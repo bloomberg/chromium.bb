bloomberg/chromium.bb

commit 5b763d9226b2522b550573cd8f692ee6caf8ae1b

Fixed fallback to software for tests
In ui/gl/init/gl_factory.cc, InitializeGLOneOffImplementation() has
functionality to fallback to software OpenGL, which wasn't working for
SwiftShader, since it uses the same initializer as Angle (which is EGL
and GLESv2). Currently, the way to distinguish between the two was
through command line flags, but the flags stay unchanged when falling
back to software OpenGL, so an extra argument was added in order to
know if the EGL/GLESv2 initialization has to use SwiftShader.

Bug: 726075
Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
Change-Id: Ic4480f0e1e9ac385b90f2760408b10a9738a2498
Reviewed-on: https://chromium-review.googlesource.com/517912
Commit-Queue: Kenneth Russell <kbr@chromium.org>
Reviewed-by: Kenneth Russell <kbr@chromium.org>
Cr-Commit-Position: refs/heads/master@{#476162}
Former-commit-id: 06a905b558faf63b9f867f200b88379d93485bf3

ui/gl/init/gl_factory.cc
@@ -37,12 +37,12 @@ bool InitializeGLOneOff() {
         cmd->GetSwitchValueASCII(switches::kUseGL);
     if (requested_implementation_name == "any") {
       fallback_to_software_gl = true;
-    } else if (requested_implementation_name ==
-               kGLImplementationSwiftShaderName) {
+    } else if ((requested_implementation_name ==
+                kGLImplementationSwiftShaderName) ||
+               (requested_implementation_name ==
+                kGLImplementationSwiftShaderForWebGLName)) {
       impl = kGLImplementationSwiftShaderGL;
-    } else if (requested_implementation_name ==
-                   kGLImplementationSwiftShaderForWebGLName ||
-               requested_implementation_name == kGLImplementationANGLEName) {
+    } else if (requested_implementation_name == kGLImplementationANGLEName) {
       impl = kGLImplementationEGLGLES2;
     } else {
       impl = GetNamedGLImplementation(requested_implementation_name);

ui/gl/init/gl_initializer_x11.cc
@@ -81,23 +81,11 @@ bool InitializeStaticGLXInternal() {
   return true;
 }
 
-bool InitializeStaticEGLInternal() {
+bool InitializeStaticEGLInternal(GLImplementation implementation) {
   base::FilePath glesv2_path(kGLESv2LibraryName);
   base::FilePath egl_path(kEGLLibraryName);
 
-  const base::CommandLine* command_line =
-      base::CommandLine::ForCurrentProcess();
-  const std::string use_gl =
-      command_line->GetSwitchValueASCII(switches::kUseGL);
-  if (use_gl == kGLImplementationANGLEName) {
-    base::FilePath module_path;
-    if (!PathService::Get(base::DIR_MODULE, &module_path))
-      return false;
-
-    glesv2_path = module_path.Append(kGLESv2ANGLELibraryName);
-    egl_path = module_path.Append(kEGLANGLELibraryName);
-  } else if ((use_gl == kGLImplementationSwiftShaderName) ||
-             (use_gl == kGLImplementationSwiftShaderForWebGLName)) {
+  if (implementation == kGLImplementationSwiftShaderGL) {
 #if BUILDFLAG(ENABLE_SWIFTSHADER)
     base::FilePath module_path;
     if (!PathService::Get(base::DIR_MODULE, &module_path))
@@ -109,6 +97,13 @@ bool InitializeStaticEGLInternal() {
 #else
     return false;
 #endif
+  } else {
+    base::FilePath module_path;
+    if (!PathService::Get(base::DIR_MODULE, &module_path))
+      return false;
+
+    glesv2_path = module_path.Append(kGLESv2ANGLELibraryName);
+    egl_path = module_path.Append(kEGLANGLELibraryName);
   }
 
   base::NativeLibrary gles_library = LoadLibraryAndPrintError(glesv2_path);
@@ -194,7 +189,7 @@ bool InitializeStaticGLBindings(GLImplementation implementation) {
       return InitializeStaticGLXInternal();
     case kGLImplementationSwiftShaderGL:
     case kGLImplementationEGLGLES2:
-      return InitializeStaticEGLInternal();
+      return InitializeStaticEGLInternal(implementation);
     case kGLImplementationMockGL:
     case kGLImplementationStubGL:
       SetGLImplementation(implementation);

ui/gl/init/gl_initializer_win.cc
@@ -9,7 +9,6 @@
 #include "base/at_exit.h"
 #include "base/base_paths.h"
 #include "base/bind.h"
-#include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/native_library.h"
@@ -84,7 +83,7 @@ bool InitializeStaticOSMesaInternal() {
   return true;
 }
 
-bool InitializeStaticEGLInternal() {
+bool InitializeStaticEGLInternal(GLImplementation implementation) {
   base::FilePath module_path;
   if (!PathService::Get(base::DIR_MODULE, &module_path))
     return false;
@@ -96,14 +95,7 @@ bool InitializeStaticEGLInternal() {
   LoadD3DXLibrary(module_path, kD3DCompiler);
 
   base::FilePath gles_path;
-  const base::CommandLine* command_line =
-      base::CommandLine::ForCurrentProcess();
-  const std::string use_gl =
-      command_line->GetSwitchValueASCII(switches::kUseGL);
-  bool using_swift_shader =
-      (use_gl == kGLImplementationSwiftShaderName) ||
-      (use_gl == kGLImplementationSwiftShaderForWebGLName);
-  if (using_swift_shader) {
+  if (implementation == kGLImplementationSwiftShaderGL) {
 #if BUILDFLAG(ENABLE_SWIFTSHADER)
     gles_path = module_path.Append(L"swiftshader/");
     // Preload library
@@ -268,7 +260,7 @@ bool InitializeStaticGLBindings(GLImplementation implementation) {
       return InitializeStaticOSMesaInternal();
     case kGLImplementationSwiftShaderGL:
     case kGLImplementationEGLGLES2:
-      return InitializeStaticEGLInternal();
+      return InitializeStaticEGLInternal(implementation);
     case kGLImplementationDesktopGL:
       return InitializeStaticWGLInternal();
     case kGLImplementationMockGL:

commit edbbd6f7e7d65eb24a173e2a839f30ea5868acbd

Fix regression --use-gl=egl does not select Mesa anymore
Instead it always selects angle, like if --use-gl=angle was set
which makes --use-gl=egl useless.

My understanding is that --use-gl=egl is supposed to select
the defaults libEGL.so.1/libGLESv2.so.2 available on the system.

Regression introduced by 5b763d9226b2522b550573cd8f692ee6caf8ae1b [formerly 06a905b558faf63b9f867f200b88379d93485bf3]
"Fixed fallback to software for tests"
https://chromium-review.googlesource.com/517912

BUG=726075

Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
Change-Id: Idf3cfb1b5480a112f73b7e7c20f4d4ade1cc2a51
Reviewed-on: https://chromium-review.googlesource.com/690074
Reviewed-by: Alexis Hétu <sugoi@chromium.org>
Reviewed-by: Kenneth Russell <kbr@chromium.org>
Commit-Queue: Julien Isorce <julien.isorce@chromium.org>
Cr-Commit-Position: refs/heads/master@{#505331}
Former-commit-id: 7a1443f8a4606ba302c36f380415c098259fa6c3

ui/gl/init/gl_factory.cc
@@ -24,7 +24,7 @@ bool InitializeGLOneOffHelper(bool init_extensions) {
   std::vector<GLImplementation> allowed_impls = GetAllowedGLImplementations();
   DCHECK(!allowed_impls.empty());
 
-  base::CommandLine* cmd = base::CommandLine::ForCurrentProcess();
+  const base::CommandLine* cmd = base::CommandLine::ForCurrentProcess();
 
   // The default implementation is always the first one in list.
   GLImplementation impl = allowed_impls[0];

ui/gl/init/gl_initializer_x11.cc
@@ -85,6 +85,8 @@ bool InitializeStaticEGLInternal(GLImplementation implementation) {
   base::FilePath glesv2_path(kGLESv2LibraryName);
   base::FilePath egl_path(kEGLLibraryName);
 
+  const base::CommandLine* cmd = base::CommandLine::ForCurrentProcess();
+
   if (implementation == kGLImplementationSwiftShaderGL) {
 #if BUILDFLAG(ENABLE_SWIFTSHADER)
     base::FilePath module_path;
@@ -97,7 +99,8 @@ bool InitializeStaticEGLInternal(GLImplementation implementation) {
 #else
     return false;
 #endif
-  } else {
+  } else if (cmd->GetSwitchValueASCII(switches::kUseGL) ==
+             kGLImplementationANGLEName) {
     base::FilePath module_path;
     if (!PathService::Get(base::DIR_MODULE, &module_path))
       return false;