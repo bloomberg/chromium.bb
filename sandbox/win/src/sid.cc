3fbd7b9e70f2b630ac16f29dc1217763152961dc
James Forshaw <forshaw@chromium.org>
2017-09-29 18:40:52 +0000

    Reland "Implemented changes to Sid class for AC support."
    This is a reland of 112cc910b085278c19ba2c0eb65fb5ca4fd5cc49 [formerly 251a3311605b4f065b84d26a150ff1a3feb4abd0], possible now
    that the Creators Update SDK (10.0.15036) is now required when building
    Chrome.
    
    Original change's description:
    > Implemented changes to Sid class for AC support.
    > 
    > This CL implements changes to the sandbox Sid class to support the creation
    > of Capability Sid objects. It also contains a few refactoring efforts to
    > cleanup up older code.
    > 
    > Bug: 760977
    > Cq-Include-Trybots: master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    > Change-Id: Id9c05b8017abb81896c1bc30f23599441812d1fd
    > Reviewed-on: https://chromium-review.googlesource.com/649026
    > Commit-Queue: James Forshaw <forshaw@chromium.org>
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Reviewed-by: Penny MacNeil <pennymac@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#501229}
    
    Bug: 760977
    Change-Id: I4d3f61190f8f34bc638cf2c48646bfc66a6508af
    Cq-Include-Trybots: master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    Reviewed-on: https://chromium-review.googlesource.com/691114
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505430}
    Former-commit-id: eba36b241ed357ca5d78047ae80495af3a2c37e3

--- sandbox/win/src/sid.cc
+++ sandbox/win/src/sid.cc
@@ -4,23 +4,123 @@
 
 #include "sandbox/win/src/sid.h"
 
+#include <sddl.h>
+
 #include "base/logging.h"
+#include "base/win/windows_version.h"
 
 namespace sandbox {
 
-Sid::Sid(const SID *sid) {
-  ::CopySid(SECURITY_MAX_SID_SIZE, sid_, const_cast<SID*>(sid));
+namespace {
+
+typedef decltype(
+    ::DeriveCapabilitySidsFromName) DeriveCapabilitySidsFromNameFunc;
+
+class SidArray {
+ public:
+  SidArray() : count_(0), sids_(nullptr) {}
+
+  ~SidArray() {
+    if (sids_) {
+      for (size_t index = 0; index < count_; ++index) {
+        ::LocalFree(sids_[index]);
+      }
+      ::LocalFree(sids_);
+    }
+  }
+
+  DWORD count() { return count_; }
+  PSID* sids() { return sids_; }
+  PDWORD count_ptr() { return &count_; }
+  PSID** sids_ptr() { return &sids_; }
+
+ private:
+  DWORD count_;
+  PSID* sids_;
 };
+
+const wchar_t* WellKnownCapabilityToName(WellKnownCapabilities capability) {
+  switch (capability) {
+    case kInternetClient:
+      return L"internetClient";
+    case kInternetClientServer:
+      return L"internetClientServer";
+    case kRegistryRead:
+      return L"registryRead";
+    case kLpacCryptoServices:
+      return L"lpacCryptoServices";
+    case kEnterpriseAuthentication:
+      return L"enterpriseAuthentication";
+    case kPrivateNetworkClientServer:
+      return L"privateNetworkClientServer";
+    default:
+      return nullptr;
+  }
+}
+
+}  // namespace
+
+Sid::Sid() : sid_() {}
+
+Sid::Sid(PSID sid) : sid_() {
+  ::CopySid(SECURITY_MAX_SID_SIZE, sid_, sid);
+}
+
+Sid::Sid(const SID* sid) : sid_() {
+  ::CopySid(SECURITY_MAX_SID_SIZE, sid_, const_cast<SID*>(sid));
+}
 
 Sid::Sid(WELL_KNOWN_SID_TYPE type) {
   DWORD size_sid = SECURITY_MAX_SID_SIZE;
-  BOOL result = ::CreateWellKnownSid(type, NULL, sid_, &size_sid);
+  BOOL result = ::CreateWellKnownSid(type, nullptr, sid_, &size_sid);
   DCHECK(result);
   (void)result;
 }
 
-const SID *Sid::GetPSID() const {
-  return reinterpret_cast<SID*>(const_cast<BYTE*>(sid_));
+Sid Sid::FromKnownCapability(WellKnownCapabilities capability) {
+  return Sid::FromNamedCapability(WellKnownCapabilityToName(capability));
+}
+
+Sid Sid::FromNamedCapability(const wchar_t* capability_name) {
+  DeriveCapabilitySidsFromNameFunc* derive_capablity_sids =
+      (DeriveCapabilitySidsFromNameFunc*)GetProcAddress(
+          GetModuleHandle(L"kernelbase"), "DeriveCapabilitySidsFromName");
+  if (!derive_capablity_sids)
+    return Sid();
+
+  if (capability_name == nullptr || ::wcslen(capability_name) == 0)
+    return Sid();
+
+  SidArray capability_group_sids;
+  SidArray capability_sids;
+
+  if (!derive_capablity_sids(capability_name, capability_group_sids.sids_ptr(),
+                             capability_group_sids.count_ptr(),
+                             capability_sids.sids_ptr(),
+                             capability_sids.count_ptr())) {
+    return Sid();
+  }
+
+  if (capability_sids.count() < 1)
+    return Sid();
+
+  return Sid(capability_sids.sids()[0]);
+}
+
+Sid Sid::FromSddlString(const wchar_t* sddl_sid) {
+  PSID converted_sid;
+  if (!::ConvertStringSidToSid(sddl_sid, &converted_sid))
+    return Sid();
+
+  return Sid(converted_sid);
+}
+
+PSID Sid::GetPSID() const {
+  return const_cast<BYTE*>(sid_);
+}
+
+bool Sid::IsValid() const {
+  return !!::IsValidSid(GetPSID());
 }
 
 }  // namespace sandbox
