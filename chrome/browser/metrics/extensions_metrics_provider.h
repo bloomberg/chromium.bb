95f10aceb10792c99b3cda8036fb389437850b3f
Devlin Cronin <rdevlin.cronin@chromium.org>
2017-09-29 02:27:59 +0000

    [Extensions][Metrics] Add and use extension_install.proto
    Add and use an extension_install.proto file. This will allow us to
    gather more detailed metrics on the types of extensions that users
    have installed, and determine the answers to more structured queries
    than could be determined by UMA histograms alone.
    
    Add the extension_install.proto and an ExtensionInstallMetricsProvider
    to gather these metrics, and hook these up to the metrics service.
    
    Add unittests for the provider.
    
    Bug: 673875
    Change-Id: Ic0bdded27cafe3de75f2edeec2e7d927e0e2ecb6
    Reviewed-on: https://chromium-review.googlesource.com/621672
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505259}
    Former-commit-id: 9944b6e56a865c8e7e8a92ae4f408eba214d261a

--- chrome/browser/metrics/extensions_metrics_provider.h
+++ chrome/browser/metrics/extensions_metrics_provider.h
@@ -9,13 +9,18 @@
 
 #include <memory>
 #include <string>
+#include <vector>
 
 #include "base/macros.h"
+#include "base/time/time.h"
 #include "components/metrics/metrics_provider.h"
+#include "components/metrics/proto/extension_install.pb.h"
 
 class Profile;
 
 namespace extensions {
+class Extension;
+class ExtensionPrefs;
 class ExtensionSet;
 }
 
@@ -38,6 +43,14 @@ class ExtensionsMetricsProvider : public metrics::MetricsProvider {
   // metrics::MetricsProvider:
   void ProvideSystemProfileMetrics(
       metrics::SystemProfileProto* system_profile) override;
+
+  static metrics::ExtensionInstallProto ConstructInstallProtoForTesting(
+      const extensions::Extension& extension,
+      extensions::ExtensionPrefs* prefs,
+      base::Time last_sample_time);
+  static std::vector<metrics::ExtensionInstallProto>
+  GetInstallsForProfileForTesting(Profile* profile,
+                                  base::Time last_sample_time);
 
  protected:
   // Exposed for the sake of mocking in test code.
@@ -68,6 +81,11 @@ class ExtensionsMetricsProvider : public metrics::MetricsProvider {
   // SystemProfileProto object.
   void ProvideOccupiedBucketMetric(metrics::SystemProfileProto* system_profile);
 
+  // Writes information about the installed extensions for all profiles into
+  // the proto.
+  void ProvideExtensionInstallsMetrics(
+      metrics::SystemProfileProto* system_profile);
+
   // The MetricsStateManager from which the client ID is obtained.
   metrics::MetricsStateManager* metrics_state_manager_;
 
@@ -75,6 +93,9 @@ class ExtensionsMetricsProvider : public metrics::MetricsProvider {
   // metric. Once a profile is found its value is cached here so that
   // GetMetricsProfile() can return a consistent value.
   Profile* cached_profile_;
+
+  // The time of our last recorded sample.
+  base::Time last_sample_time_;
 
   DISALLOW_COPY_AND_ASSIGN(ExtensionsMetricsProvider);
 };
