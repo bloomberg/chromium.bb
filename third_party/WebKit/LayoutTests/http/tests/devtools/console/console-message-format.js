61c080ccdb0312d0f0d211c081171852f803097b
Erik Luo <luoe@chromium.org>
2017-09-29 08:50:39 +0000

    DevTools: align console %o optimal formatting specifier with spec
    The console spec says that the %o specifier should produce
    "optimally useful formatting" as opposed to the %O
    "generic JavaScript object formatting". To make this distinction clearer
    and align log() to %o and dir() to %O, this CL will format
    arrays/objects with previews when using %o.
    
    Please see crbug for screenshot.
    
    Bug: 751850
    Change-Id: I927e1c34667f1d11bc13e98a86d38419b7012940
    Reviewed-on: https://chromium-review.googlesource.com/607576
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505333}
    Former-commit-id: 71d5ed5b2493fc0d5a32211b2d6bd8b770bc8cf7

--- third_party/WebKit/LayoutTests/http/tests/devtools/console/console-message-format.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/console/console-message-format.js
@@ -7,13 +7,13 @@
 
   await TestRunner.loadModule('console_test_runner');
   await TestRunner.showPanel('console');
-  await TestRunner.loadHTML(`
-    <p id="p"></p>
-  `);
   await TestRunner.evaluateInPagePromise(`
     console.log('Message format number %i, %d and %f', 1, 2, 3.5);
     console.log('Message %s for %s', 'format', 'string');
     console.log('Object %o', {'foo' : 'bar' });
+    console.log('Array %o', ['foo', 'bar']);
+    console.log('Object as object: %O', {'foo' : 'bar' });
+    console.log('Array as object: %O', ['foo', 'bar']);
     console.log("Floating as integers: %d %i", 42.5, 42.5);
     console.log("Floating as is: %f", 42.5);
     console.log("Non-numbers as numbers: %d %i %f", document, null, "document");
