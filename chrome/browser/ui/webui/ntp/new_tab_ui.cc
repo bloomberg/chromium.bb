c6b8136d6aa3cadb6afcb67cd4955c0548a1b828
Lukasz Anforowicz <lukasza@chromium.org>
2017-09-29 18:13:45 +0000

    Removing callers of the WebContents::GetRenderProcessHost() method.
    WebContents::GetRenderProcessHost is an API that doesn't behave as
    expected with out-of-process iframes, as multiple processes can be
    associated with a single WebContents.  This CL replaces callers of this
    API, so that they go through an equivalent API that explicitly selects
    which frame's process is needed.
    
    This CL was started by using an ad-hoc clang-plugin that replaced
    existing callsites of WebContents::GetRenderProcessHost() with a call to
    something like wc->GetMainFrame()->GetProcess().  This was followed-up
    by manually adding an include of render_frame_host.h and git cl format
    and git cl lint and then a self-review with small tweaks (e.g. sometimes
    using a wc->GetRenderViewHost()->GetProcess() is more appropriate).
    
    This CL was uploaded by git cl split.
    
    R=estade@chromium.org
    
    Bug: 666525
    Change-Id: Ia364e50c35362c9fd1f4bfecd59d022e1dd42dc2
    Reviewed-on: https://chromium-review.googlesource.com/688229
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505423}
    Former-commit-id: 1b3182253006df2ca3297a5182da86627cfd37b0

--- chrome/browser/ui/webui/ntp/new_tab_ui.cc
+++ chrome/browser/ui/webui/ntp/new_tab_ui.cc
@@ -24,6 +24,7 @@
 #include "components/prefs/pref_service.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
@@ -169,7 +170,7 @@ void NewTabUI::NewTabHTMLSource::StartDataRequest(
 
   content::WebContents* web_contents = wc_getter.Run();
   content::RenderProcessHost* render_host =
-      web_contents ? web_contents->GetRenderProcessHost() : nullptr;
+      web_contents ? web_contents->GetMainFrame()->GetProcess() : nullptr;
   NTPResourceCache::WindowType win_type = NTPResourceCache::GetWindowType(
       profile_, render_host);
   scoped_refptr<base::RefCountedMemory> html_bytes(
