04f1b19e5317a260c814aec7bc04fcc7a15428eb
Xianzhu Wang <wangxianzhu@chromium.org>
2017-09-29 03:28:09 +0000

    Fix false-positives of under-invalidation checking in layout tests
    In the following few cases we intentionally allow under-invalidations in
    cached subsequences:
    - offscreen image animation
    - media buffered range
    
    We intentionally don't update each time the contents change to improve
    performance or avoid complex implementation of real time change
    notification.
    
    Now allow cache skipping in cached subsequences.
    
    Enable under-invalidation checking for tests that would have reported
    under-invalidation with the checking enabled.
    
    This also helps clusterfuzz not to trigger under-invaldiation checking
    failures when it creates a test for the above cases.
    
    Bug: 769729
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I2149e9d2304dbad5d7486c822d5452c5dba237fe
    Reviewed-on: https://chromium-review.googlesource.com/690851
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505281}
    Former-commit-id: 206bdc858ad26bc02172a308e6836921a1e41b38

--- third_party/WebKit/Source/platform/graphics/paint/PaintControllerTest.cpp
+++ third_party/WebKit/Source/platform/graphics/paint/PaintControllerTest.cpp
@@ -11,6 +11,7 @@
 #include "platform/graphics/paint/ClipPathRecorder.h"
 #include "platform/graphics/paint/ClipRecorder.h"
 #include "platform/graphics/paint/CompositingRecorder.h"
+#include "platform/graphics/paint/DisplayItemCacheSkipper.h"
 #include "platform/graphics/paint/DrawingDisplayItem.h"
 #include "platform/graphics/paint/DrawingRecorder.h"
 #include "platform/graphics/paint/SubsequenceRecorder.h"
@@ -2074,7 +2075,10 @@ TEST_P(PaintControllerTest, PartialSkipCache) {
                          .GetPaintRecord());
 }
 
-TEST_F(PaintControllerTestBase, OptimizeNoopPairs) {
+TEST_P(PaintControllerTest, OptimizeNoopPairs) {
+  if (RuntimeEnabledFeatures::SlimmingPaintV175Enabled())
+    return;
+
   FakeDisplayItemClient first("first");
   FakeDisplayItemClient second("second");
   FakeDisplayItemClient third("third");
@@ -2128,7 +2132,7 @@ TEST_F(PaintControllerTestBase, OptimizeNoopPairs) {
                       TestControllerDisplayItem(third, kBackgroundDrawingType));
 }
 
-TEST_F(PaintControllerTestBase, SmallPaintControllerHasOnePaintChunk) {
+TEST_P(PaintControllerTest, SmallPaintControllerHasOnePaintChunk) {
   ScopedSlimmingPaintV2ForTest enable_s_pv2(true);
   FakeDisplayItemClient client("test client");
 
@@ -2168,7 +2172,7 @@ void DrawPath(GraphicsContext& context,
     context.DrawPath(path, flags);
 }
 
-TEST_F(PaintControllerTestBase, BeginAndEndFrame) {
+TEST_P(PaintControllerTest, BeginAndEndFrame) {
   class FakeFrame {};
 
   // PaintController should have one null frame in the stack since beginning.
@@ -2443,6 +2447,30 @@ class PaintControllerUnderInvalidationTest
     }
     GetPaintController().CommitNewDisplayItems();
   }
+
+  void TestSkipCacheInSubsequence() {
+    FakeDisplayItemClient container("container");
+    FakeDisplayItemClient content("content");
+    GraphicsContext context(GetPaintController());
+
+    {
+      SubsequenceRecorder r(context, container);
+      DisplayItemCacheSkipper cache_skipper(context);
+      DrawRect(context, content, kBackgroundDrawingType,
+               FloatRect(100, 100, 300, 300));
+    }
+    GetPaintController().CommitNewDisplayItems();
+
+    {
+      EXPECT_FALSE(SubsequenceRecorder::UseCachedSubsequenceIfPossible(
+          context, container));
+      SubsequenceRecorder r(context, container);
+      DisplayItemCacheSkipper cache_skipper(context);
+      DrawRect(context, content, kBackgroundDrawingType,
+               FloatRect(200, 200, 400, 400));
+    }
+    GetPaintController().CommitNewDisplayItems();
+  }
 };
 
 TEST_F(PaintControllerUnderInvalidationTest, ChangeDrawing) {
@@ -2494,6 +2522,10 @@ TEST_F(PaintControllerUnderInvalidationTest, InvalidationInSubsequence) {
 
 TEST_F(PaintControllerUnderInvalidationTest, SubsequenceBecomesEmpty) {
   EXPECT_DEATH(TestSubsequenceBecomesEmpty(), "");
+}
+
+TEST_F(PaintControllerUnderInvalidationTest, SkipCacheInSubsequence) {
+  TestSkipCacheInSubsequence();
 }
 
 #endif  // defined(GTEST_HAS_DEATH_TEST) && !defined(OS_ANDROID)
