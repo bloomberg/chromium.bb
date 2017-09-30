7dd0b80931e19e894e3aca6b156dcae7c2b91259
Tom Finegan <tomfinegan@google.com>
2017-09-29 20:11:03 -0700

    Avoid overflows in Block::GetTimeCode().
    BUG=b/66854237
    
    Change-Id: I721bfa0579597995ae0b488f69bcf17aba6c32c6

--- mkvparser/mkvparser.cc
+++ mkvparser/mkvparser.cc
@@ -7976,6 +7976,11 @@ long long Block::GetTimeCode(const Cluster* pCluster) const {
   const long long tc0 = pCluster->GetTimeCode();
   assert(tc0 >= 0);
 
+  // Check if tc0 + m_timecode would overflow.
+  if (tc0 < 0 || LLONG_MAX - tc0 < m_timecode) {
+    return -1;
+  }
+
   const long long tc = tc0 + m_timecode;
 
   return tc;  // unscaled timecode units
