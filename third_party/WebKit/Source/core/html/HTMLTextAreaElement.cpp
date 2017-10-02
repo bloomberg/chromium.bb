49dd977a3fecc5410c1a83eefcc05bce9f11cc09
sebsg <sebsg@chromium.org>
2017-10-02 19:57:41 +0000

    [Autofill] Use ShadowDom placeholder to preview suggestions.
    Bug: 753645
    Change-Id: Idabb4d01b45aa08a71f9fc8ad1dd89b192cfb0c5
    Reviewed-on: https://chromium-review.googlesource.com/646754
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505743}
    Former-commit-id: 0727466e09c7285f3c4fe6a7974bfc68fd9bbccb

--- third_party/WebKit/Source/core/html/HTMLTextAreaElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLTextAreaElement.cpp
@@ -391,6 +391,10 @@ void HTMLTextAreaElement::SetValueCommon(
   normalized_value.Replace("\r\n", "\n");
   normalized_value.Replace('\r', '\n');
 
+  // Clear the suggested value. Use the base class version to not trigger a view
+  // update.
+  TextControlElement::SetSuggestedValue(String());
+
   // Return early because we don't want to trigger other side effects when the
   // value isn't changing. This is interoperable.
   if (normalized_value == value())
@@ -408,7 +412,6 @@ void HTMLTextAreaElement::SetValueCommon(
   SetNeedsStyleRecalc(
       kSubtreeStyleChange,
       StyleChangeReasonForTracing::Create(StyleChangeReason::kControlValue));
-  suggested_value_ = String();
   SetNeedsValidityCheck();
   if (IsFinishedParsingChildren() &&
       selection == TextControlSetValueSelection::kSetSelectionToEnd) {
@@ -468,18 +471,8 @@ void HTMLTextAreaElement::setDefaultValue(const String& default_value) {
     SetNonDirtyValue(value);
 }
 
-String HTMLTextAreaElement::SuggestedValue() const {
-  return suggested_value_;
-}
-
 void HTMLTextAreaElement::SetSuggestedValue(const String& value) {
-  suggested_value_ = value;
-
-  if (!value.IsNull())
-    SetInnerEditorValue(suggested_value_);
-  else
-    SetInnerEditorValue(value_);
-  UpdatePlaceholderVisibility();
+  TextControlElement::SetSuggestedValue(value);
   SetNeedsStyleRecalc(
       kSubtreeStyleChange,
       StyleChangeReasonForTracing::Create(StyleChangeReason::kControlValue));
@@ -588,7 +581,7 @@ void HTMLTextAreaElement::SetPlaceholderVisibility(bool visible) {
 
 void HTMLTextAreaElement::UpdatePlaceholderText() {
   HTMLElement* placeholder = PlaceholderElement();
-  const AtomicString& placeholder_text = FastGetAttribute(placeholderAttr);
+  const String placeholder_text = GetPlaceholderValue();
   if (placeholder_text.IsEmpty()) {
     if (placeholder)
       UserAgentShadowRoot()->RemoveChild(placeholder);
@@ -605,6 +598,11 @@ void HTMLTextAreaElement::UpdatePlaceholderText() {
     UserAgentShadowRoot()->InsertBefore(placeholder, InnerEditorElement());
   }
   placeholder->setTextContent(placeholder_text);
+}
+
+String HTMLTextAreaElement::GetPlaceholderValue() const {
+  return !SuggestedValue().IsEmpty() ? SuggestedValue()
+                                     : FastGetAttribute(placeholderAttr);
 }
 
 bool HTMLTextAreaElement::IsInteractiveContent() const {
