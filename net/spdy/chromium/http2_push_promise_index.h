6432f3f8facfa5f7b5608cb888c2cc6b0f82ad1f
Bence Béky <bnc@chromium.org>
2017-10-02 16:51:54 +0000

    Create Http2PushPromiseIndex class.
    Factor out SpdySessionPool::unclaimed_pushed_streams_ into its own
    class, similar to QuicClientPushPromiseIndex.  This class will handle
    async push promise validation after some future CLs.
    
    No functional change.
    
    Bug: 554220
    Change-Id: If42d077c6d258fc419b822db7511623904629aae
    Reviewed-on: https://chromium-review.googlesource.com/692935
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505647}
    Former-commit-id: 824e493803f18649da0e7961bfa6d027e8e2700d

---
+++ net/spdy/chromium/http2_push_promise_index.h
@@ -0,0 +1,51 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_SPDY_CHROMIUM_HTTP2_PUSH_PROMISE_INDEX_H_
+#define NET_SPDY_CHROMIUM_HTTP2_PUSH_PROMISE_INDEX_H_
+
+#include <map>
+#include <vector>
+
+#include "base/memory/weak_ptr.h"
+#include "net/base/net_export.h"
+#include "net/spdy/chromium/spdy_session_key.h"
+#include "url/gurl.h"
+
+namespace net {
+
+class SpdySession;
+
+class NET_EXPORT Http2PushPromiseIndex {
+ public:
+  Http2PushPromiseIndex();
+  ~Http2PushPromiseIndex();
+  Http2PushPromiseIndex(const Http2PushPromiseIndex&) = delete;
+
+  // If there is a session for |key| that has an unclaimed push stream for
+  // |url|, return it.  Otherwise return nullptr.
+  base::WeakPtr<SpdySession> Find(const SpdySessionKey& key, const GURL& url);
+
+  // (Un)register a SpdySession with an unclaimed pushed stream for |url|, so
+  // that the right SpdySession can be served by FindAvailableSession.
+  void RegisterUnclaimedPushedStream(GURL url,
+                                     base::WeakPtr<SpdySession> spdy_session);
+  void UnregisterUnclaimedPushedStream(const GURL& url,
+                                       SpdySession* spdy_session);
+
+ private:
+  typedef std::vector<base::WeakPtr<SpdySession>> WeakSessionList;
+  typedef std::map<GURL, WeakSessionList> UnclaimedPushedStreamMap;
+
+  // A map of all SpdySessions owned by |this| that have an unclaimed pushed
+  // streams for a GURL.  Might contain invalid WeakPtr's.
+  // A single SpdySession can only have at most one pushed stream for each GURL,
+  // but it is possible that multiple SpdySessions have pushed streams for the
+  // same GURL.
+  UnclaimedPushedStreamMap unclaimed_pushed_streams_;
+};
+
+}  // namespace net
+
+#endif  // NET_SPDY_CHROMIUM_HTTP2_PUSH_PROMISE_INDEX_H_
--- net/spdy/chromium/spdy_session_pool.cc
+++ net/spdy/chromium/spdy_session_pool.cc
@@ -136,33 +136,9 @@ base::WeakPtr<SpdySession> SpdySessionPool::FindAvailableSession(
     const GURL& url,
     bool enable_ip_based_pooling,
     const NetLogWithSource& net_log) {
-  UnclaimedPushedStreamMap::iterator url_it =
-      unclaimed_pushed_streams_.find(url);
-  if (!url.is_empty() && url_it != unclaimed_pushed_streams_.end()) {
-    DCHECK(url.SchemeIsCryptographic());
-    for (WeakSessionList::iterator it = url_it->second.begin();
-         it != url_it->second.end();) {
-      base::WeakPtr<SpdySession> spdy_session = *it;
-      // Lazy deletion of destroyed SpdySessions.
-      if (!spdy_session) {
-        it = url_it->second.erase(it);
-        continue;
-      }
-      ++it;
-      const SpdySessionKey& spdy_session_key = spdy_session->spdy_session_key();
-      if (!(spdy_session_key.proxy_server() == key.proxy_server()) ||
-          !(spdy_session_key.privacy_mode() == key.privacy_mode())) {
-        continue;
-      }
-      if (!spdy_session->VerifyDomainAuthentication(
-              key.host_port_pair().host())) {
-        continue;
-      }
-      return spdy_session;
-    }
-    if (url_it->second.empty()) {
-      unclaimed_pushed_streams_.erase(url_it);
-    }
+  base::WeakPtr<SpdySession> session = push_promise_index_.Find(key, url);
+  if (session) {
+    return session;
   }
 
   AvailableSessionMap::iterator it = LookupAvailableSessionByKey(key);
@@ -303,52 +279,6 @@ void SpdySessionPool::CloseAllSessions() {
     CloseCurrentSessionsHelper(ERR_ABORTED, "Closing all sessions.",
                                false /* idle_only */);
   }
-}
-
-void SpdySessionPool::RegisterUnclaimedPushedStream(
-    GURL url,
-    base::WeakPtr<SpdySession> spdy_session) {
-  DCHECK(!url.is_empty());
-  // This SpdySessionPool must own |spdy_session|.
-  DCHECK(base::ContainsKey(sessions_, spdy_session.get()));
-  UnclaimedPushedStreamMap::iterator url_it =
-      unclaimed_pushed_streams_.lower_bound(url);
-  if (url_it == unclaimed_pushed_streams_.end() || url_it->first != url) {
-    WeakSessionList list;
-    list.push_back(std::move(spdy_session));
-    UnclaimedPushedStreamMap::value_type value(std::move(url), std::move(list));
-    unclaimed_pushed_streams_.insert(url_it, std::move(value));
-    return;
-  }
-  url_it->second.push_back(spdy_session);
-}
-
-void SpdySessionPool::UnregisterUnclaimedPushedStream(
-    const GURL& url,
-    SpdySession* spdy_session) {
-  DCHECK(!url.is_empty());
-  UnclaimedPushedStreamMap::iterator url_it =
-      unclaimed_pushed_streams_.find(url);
-  DCHECK(url_it != unclaimed_pushed_streams_.end());
-  size_t removed = 0;
-  for (WeakSessionList::iterator it = url_it->second.begin();
-       it != url_it->second.end();) {
-    // Lazy deletion of destroyed SpdySessions.
-    if (!*it) {
-      it = url_it->second.erase(it);
-      continue;
-    }
-    if (it->get() == spdy_session) {
-      it = url_it->second.erase(it);
-      ++removed;
-      break;
-    }
-    ++it;
-  }
-  if (url_it->second.empty()) {
-    unclaimed_pushed_streams_.erase(url_it);
-  }
-  DCHECK_EQ(1u, removed);
 }
 
 std::unique_ptr<base::Value> SpdySessionPool::SpdySessionPoolInfoToValue()
