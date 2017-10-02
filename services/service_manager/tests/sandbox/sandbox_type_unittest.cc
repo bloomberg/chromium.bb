3fd73626a7dfeabba3f3ca5fb332fc9d4526adea
Tom Sepez <tsepez@chromium.org>
2017-10-02 21:53:47 +0000

    build sandbox_type_unitttest.cc
    This seems to have been missed when these sources moved to
    the service_manager directory tree.
    
    Change-Id: I4443527ca72086bfacfa15b1593c514e669c7567
    Reviewed-on: https://chromium-review.googlesource.com/695823
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505803}
    Former-commit-id: d27f92a4906e3846bf4e0435a1569a9052c86cd6

--- services/service_manager/tests/sandbox/sandbox_type_unittest.cc
+++ services/service_manager/tests/sandbox/sandbox_type_unittest.cc
@@ -8,7 +8,7 @@
 #include "services/service_manager/sandbox/switches.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace content {
+namespace service_manager {
 
 TEST(SandboxTypeTest, Empty) {
   base::CommandLine command_line(base::CommandLine::NO_PROGRAM);
@@ -120,4 +120,4 @@ TEST(SandboxTypeTest, Nonesuch) {
   EXPECT_EQ(SANDBOX_TYPE_NO_SANDBOX, SandboxTypeFromCommandLine(command_line));
 }
 
-}  // namespace content
+}  // namespace service_manager
