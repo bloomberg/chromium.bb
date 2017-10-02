423d39bdf16cea93befacdd41bc4b0589d2d6487
spqchan <spqchan@chromium.org>
2017-10-02 19:48:13 +0000

    [Mac] Touch Bar Browser Tests
    Add browser tests to check if the touch bar gets
    updated.
    
    Bug: 764295
    Change-Id: I9c579319050360e6bbc440137fd3e452f5142c6c
    Reviewed-on: https://chromium-review.googlesource.com/688803
    Commit-Queue: Sarah Chan <spqchan@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505733}
    Former-commit-id: a1487ab0a80de7bcbe522fea1102576a58aa2f14

--- chrome/browser/ui/cocoa/browser_window_touch_bar.h
+++ chrome/browser/ui/cocoa/browser_window_touch_bar.h
@@ -32,4 +32,19 @@ class Browser;
 
 @end
 
+// Private methods exposed for testing.
+@interface BrowserWindowTouchBar (ExposedForTesting)
+
+// Methods to update controls on the touch bar. Called when creating the
+// touch bar or the page load state has been updated. Exposed for
+// testing.
+- (void)updateReloadStopButton;
+- (void)updateBackForwardControl;
+- (void)updateStarredButton;
+
+// Returns the reload/stop button on the touch bar.
+- (NSButton*)reloadStopButton;
+
+@end
+
 #endif  // CHROME_BROWSER_UI_COCOA_BROWSER_WINDOW_TOUCH_BAR_H_
