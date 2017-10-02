38de1a5aa770bf7ac964fd13942a6f8be6a330ed
Mostyn Bramley-Moore <mostynb@vewd.com>
2017-10-02 21:10:29 +0000

    [jumbo] rename FontMessageFilter to avoid symbol clash
    There is another FontMessageFilter in pepper_browser_font_singleton_host.cc
    
    Bug: 746953
    Change-Id: I2ee0188ac38637b94f9e9aa792dccd42be7d8e9a
    Reviewed-on: https://chromium-review.googlesource.com/696101
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Commit-Queue: Mostyn Bramley-Moore <mostynb@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#505783}
    Former-commit-id: c61bf647f03d5683cd5959295b8f72159a51b803

--- content/browser/renderer_host/pepper/pepper_truetype_font_list_host.cc
+++ content/browser/renderer_host/pepper/pepper_truetype_font_list_host.cc
@@ -23,9 +23,9 @@ namespace content {
 namespace {
 
 // Handles the font list request on the blocking pool.
-class FontMessageFilter : public ppapi::host::ResourceMessageFilter {
+class TrueTypeFontMessageFilter : public ppapi::host::ResourceMessageFilter {
  public:
-  FontMessageFilter();
+  TrueTypeFontMessageFilter();
 
   // ppapi::host::ResourceMessageFilter implementation.
   scoped_refptr<base::TaskRunner> OverrideTaskRunnerForMessage(
@@ -35,21 +35,22 @@ class FontMessageFilter : public ppapi::host::ResourceMessageFilter {
       ppapi::host::HostMessageContext* context) override;
 
  private:
-  ~FontMessageFilter() override;
+  ~TrueTypeFontMessageFilter() override;
 
   // Message handlers.
   int32_t OnHostMsgGetFontFamilies(ppapi::host::HostMessageContext* context);
   int32_t OnHostMsgGetFontsInFamily(ppapi::host::HostMessageContext* context,
                                     const std::string& family);
 
-  DISALLOW_COPY_AND_ASSIGN(FontMessageFilter);
+  DISALLOW_COPY_AND_ASSIGN(TrueTypeFontMessageFilter);
 };
 
-FontMessageFilter::FontMessageFilter() {}
+TrueTypeFontMessageFilter::TrueTypeFontMessageFilter() {}
 
-FontMessageFilter::~FontMessageFilter() {}
+TrueTypeFontMessageFilter::~TrueTypeFontMessageFilter() {}
 
-scoped_refptr<base::TaskRunner> FontMessageFilter::OverrideTaskRunnerForMessage(
+scoped_refptr<base::TaskRunner>
+TrueTypeFontMessageFilter::OverrideTaskRunnerForMessage(
     const IPC::Message& msg) {
   // Use the font list SequencedTaskRunner to get the font list (currently the
   // only message) since getting the font list is non-threadsafe on Linux (for
@@ -57,10 +58,10 @@ scoped_refptr<base::TaskRunner> FontMessageFilter::OverrideTaskRunnerForMessage(
   return GetFontListTaskRunner();
 }
 
-int32_t FontMessageFilter::OnResourceMessageReceived(
+int32_t TrueTypeFontMessageFilter::OnResourceMessageReceived(
     const IPC::Message& msg,
     ppapi::host::HostMessageContext* context) {
-  PPAPI_BEGIN_MESSAGE_MAP(FontMessageFilter, msg)
+  PPAPI_BEGIN_MESSAGE_MAP(TrueTypeFontMessageFilter, msg)
     PPAPI_DISPATCH_HOST_RESOURCE_CALL_0(
         PpapiHostMsg_TrueTypeFontSingleton_GetFontFamilies,
         OnHostMsgGetFontFamilies)
@@ -71,7 +72,7 @@ int32_t FontMessageFilter::OnResourceMessageReceived(
   return PP_ERROR_FAILED;
 }
 
-int32_t FontMessageFilter::OnHostMsgGetFontFamilies(
+int32_t TrueTypeFontMessageFilter::OnHostMsgGetFontFamilies(
     ppapi::host::HostMessageContext* context) {
   // OK to use "slow blocking" version since we're on the blocking pool.
   std::vector<std::string> font_families;
@@ -84,7 +85,7 @@ int32_t FontMessageFilter::OnHostMsgGetFontFamilies(
   return base::checked_cast<int32_t>(font_families.size());
 }
 
-int32_t FontMessageFilter::OnHostMsgGetFontsInFamily(
+int32_t TrueTypeFontMessageFilter::OnHostMsgGetFontsInFamily(
     ppapi::host::HostMessageContext* context,
     const std::string& family) {
   // OK to use "slow blocking" version since we're on the blocking pool.
@@ -104,7 +105,7 @@ PepperTrueTypeFontListHost::PepperTrueTypeFontListHost(BrowserPpapiHost* host,
                                                        PP_Resource resource)
     : ResourceHost(host->GetPpapiHost(), instance, resource) {
   AddFilter(scoped_refptr<ppapi::host::ResourceMessageFilter>(
-      new FontMessageFilter()));
+      new TrueTypeFontMessageFilter()));
 }
 
 PepperTrueTypeFontListHost::~PepperTrueTypeFontListHost() {}
