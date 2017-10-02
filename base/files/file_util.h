61ab8c20f3cafc25e3a04fb5fc4ecf4df1508191
Lei Zhang <thestig@chromium.org>
2017-10-02 23:02:16 +0000

    Add tests to document base::CopyFile() behavior.
    - Always overwrites the destination, if it is a file.
    - Does not touch destination permissions when overwriting.
    - On POSIX, preserves and follows destinations that are symlinks.
    - On POSIX, file permissions for the destination is platform dependent.
    
    Then document these in the header, since these details may be important
    to the caller.
    
    Change-Id: Ibd47fa95aac29a35d798e5c19d1da1a64df63754
    Reviewed-on: https://chromium-review.googlesource.com/686001
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505836}
    Former-commit-id: 6a0e47cad50b996e668623e6bdeff8dec591203c

--- base/files/file_util.h
+++ base/files/file_util.h
@@ -95,11 +95,26 @@ BASE_EXPORT bool ReplaceFile(const FilePath& from_path,
                              const FilePath& to_path,
                              File::Error* error);
 
-// Copies a single file. Use CopyDirectory to copy directories.
+// Copies a single file. Use CopyDirectory() to copy directories.
 // This function fails if either path contains traversal components ('..').
+// This function also fails if |to_path| is a directory.
 //
-// This function keeps the metadata on Windows. The read only bit on Windows is
-// not kept.
+// On POSIX, if |to_path| is a symlink, CopyFile() will follow the symlink. This
+// may have security implications. Use with care.
+//
+// If |to_path| already exists and is a regular file, it will be overwritten,
+// though its permissions will stay the same.
+//
+// If |to_path| does not exist, it will be created. The new file's permissions
+// varies per platform:
+//
+// - This function keeps the metadata on Windows. The read only bit is not kept.
+// - On Mac and iOS, |to_path| retains |from_path|'s permissions, except user
+//   read/write permissions are always set.
+// - On Linux and Android, |to_path| has user read/write permissions only. i.e.
+//   Always 0600.
+// - On ChromeOS, |to_path| has user read/write permissions and group/others
+//   read permissions. i.e. Always 0644.
 BASE_EXPORT bool CopyFile(const FilePath& from_path, const FilePath& to_path);
 
 // Copies the given path, and optionally all subdirectories and their contents
