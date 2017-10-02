bb89be9d47dc64850c038b5a26bddc9e3b7f385e
spqchan <spqchan@chromium.org>
2017-10-02 09:00:11 +0000

    [Mac] Fullscreen Metrics
    Add the following metrics for Chrome Mac Fullscreen:
    - Fullscreen source (Browser, Tab, Extension)
    - Fullscreen toolbar style (Shown, Hidden, None)
    
    This CL also refactors and renames a couple of
    confusing methods in FullscreenToolbarController.
    
    Testing: BrowserWindowControllerTest.FullscreenHistogram
    
    Bug: 766268
    Change-Id: Iff36e448a0fc1385c7392088699d91a2be94fd91
    Reviewed-on: https://chromium-review.googlesource.com/688224
    Commit-Queue: Sarah Chan <spqchan@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505561}
    Former-commit-id: c43a18e85fe1c6f718273977c1b68c9d1fbcf51a

--- chrome/browser/ui/cocoa/fullscreen/fullscreen_toolbar_controller.h
+++ chrome/browser/ui/cocoa/fullscreen/fullscreen_toolbar_controller.h
@@ -22,18 +22,24 @@ class WebContents;
 }
 
 // This enum class represents the appearance of the fullscreen toolbar, which
-// includes the tab strip and omnibox.
+// includes the tab strip and omnibox. These values are logged in a histogram
+// and shouldn't be renumbered or removed.
 enum class FullscreenToolbarStyle {
   // The toolbar is present. Moving the cursor to the top
   // causes the menubar to appear and the toolbar to slide down.
-  TOOLBAR_PRESENT,
+  TOOLBAR_PRESENT = 0,
   // The toolbar is hidden. Moving cursor to top shows the
   // toolbar and menubar.
   TOOLBAR_HIDDEN,
   // Toolbar is hidden. Moving cursor to top causes the menubar
   // to appear, but not the toolbar.
   TOOLBAR_NONE,
+  // The last enum value. Used for logging in a histogram.
+  TOOLBAR_LAST = TOOLBAR_NONE
 };
+
+static constexpr int kFullscreenToolbarStyleCount =
+    (int)FullscreenToolbarStyle::TOOLBAR_LAST + 1;
 
 // This struct contains the calculated values of the fullscreen toolbar layout.
 struct FullscreenToolbarLayout {
@@ -118,10 +124,10 @@ struct FullscreenToolbarLayout {
 
 // Updates the toolbar style. If the style has changed, then the toolbar will
 // relayout.
-- (void)updateToolbarStyleExitingTabFullscreen:(BOOL)isExitingTabFullscreen;
+- (void)layoutToolbarStyleIsExitingTabFullscreen:(BOOL)isExitingTabFullscreen;
 
 // Updates the toolbar by updating the layout.
-- (void)updateToolbarLayout;
+- (void)layoutToolbar;
 
 // Returns YES if the browser in in fullscreen.
 - (BOOL)isInFullscreen;
