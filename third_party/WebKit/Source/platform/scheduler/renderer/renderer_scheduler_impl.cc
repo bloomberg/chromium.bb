32cd0c3246106ff60aab33ed2b5033c0b2f79721
Greg Kraynov <kraynov@chromium.org>
2017-10-03 13:48:15 +0000

    [Reland] Blink Scheduler: Tracing refactoring.
    Added PLATFORM_EXPORT macro missing what caused the revert.
    
    - Support different categories in StateTracer.
    - Distinct TRACE_EVENT stubs for each StateTracer category.
    - Fixed bug when StateTracer didn't emit initial slice.
    - AreVerboseSnaphotsEnabled() helper method.
    - WarmupCategories() helper method.
    - Removed abandoned worker.scheduler.debug category.
    
    Original CL: https://chromium-review.googlesource.com/692376
    
    Bug: 765660
    Change-Id: Ifd0df2216b7b3e042c56966756be64ab41d4cb3c
    Reviewed-on: https://chromium-review.googlesource.com/697446
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Grigoriy Kraynov <kraynov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506020}
    Former-commit-id: 413046b389220ea6bdffc2a0794a1457c3be0943

--- third_party/WebKit/Source/platform/scheduler/renderer/renderer_scheduler_impl.cc
+++ third_party/WebKit/Source/platform/scheduler/renderer/renderer_scheduler_impl.cc
@@ -2086,11 +2086,11 @@ TimeDomain* RendererSchedulerImpl::GetActiveTimeDomain() {
 void RendererSchedulerImpl::OnTraceLogEnabled() {
   CreateTraceEventObjectSnapshot();
 
-  main_thread_only().use_case_tracer.Start(
+  main_thread_only().use_case_tracer.SetState(
       UseCaseToString(main_thread_only().current_use_case));
-  main_thread_only().backgrounding_tracer.Start(
+  main_thread_only().backgrounding_tracer.SetState(
       BackgroundStateToString(main_thread_only().renderer_backgrounded));
-  main_thread_only().audio_playing_tracer.Start(
+  main_thread_only().audio_playing_tracer.SetState(
       AudioPlayingToString(main_thread_only().is_audio_playing));
 }
 
---
+++ third_party/WebKit/Source/platform/scheduler/util/tracing_helper.cc
@@ -0,0 +1,39 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/scheduler/util/tracing_helper.h"
+
+namespace blink {
+namespace scheduler {
+
+const char kTracingCategoryNameDefault[] = "renderer.scheduler";
+const char kTracingCategoryNameInfo[] =
+    TRACE_DISABLED_BY_DEFAULT("renderer.scheduler");
+const char kTracingCategoryNameDebug[] =
+    TRACE_DISABLED_BY_DEFAULT("renderer.scheduler.debug");
+
+namespace {
+
+// No trace events should be created with this category.
+const char kTracingCategoryNameVerboseSnapshots[] =
+    TRACE_DISABLED_BY_DEFAULT("renderer.scheduler.enable_verbose_snapshots");
+
+}  // namespace
+
+bool AreVerboseSnapshotsEnabled() {
+  bool result = false;
+  TRACE_EVENT_CATEGORY_GROUP_ENABLED(kTracingCategoryNameVerboseSnapshots,
+                                     &result);
+  return result;
+}
+
+void WarmupTracingCategories() {
+  TRACE_EVENT_WARMUP_CATEGORY(kTracingCategoryNameDefault);
+  TRACE_EVENT_WARMUP_CATEGORY(kTracingCategoryNameInfo);
+  TRACE_EVENT_WARMUP_CATEGORY(kTracingCategoryNameDebug);
+  TRACE_EVENT_WARMUP_CATEGORY(kTracingCategoryNameVerboseSnapshots);
+}
+
+}  // namespace scheduler
+}  // namespace blink
