deb65e08e11f40df127d9370263a692cb71a16c4
Bill Budge <bbudge@chromium.org>
2017-10-03 01:06:28 +0000

    [Memory] Adds ASLR handling for OS'es and Architectures supported by V8.
    - Refactors GetRandomPageBase to make it easier to understand.
    - Adds handling for architectures and OS'es needed by V8.
    - Increases random bits on Windows 64 bit to 48 for 8.10 or greater,
      44 for older versions of Windows.
    - Adds unit tests.
    
    Bug: chromium:756050
    Change-Id: I3d27aeb9e243cfdda005662dc6df710bdbcabaaa
    Reviewed-on: https://chromium-review.googlesource.com/688741
    Commit-Queue: Bill Budge <bbudge@chromium.org>
    Reviewed-by: Albert J. Wong <ajwong@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505900}
    Former-commit-id: 3f096d724aaadaa8c321a48be12b85c7f2c2c175

--- base/allocator/partition_allocator/address_space_randomization.cc
+++ base/allocator/partition_allocator/address_space_randomization.cc
@@ -90,14 +90,11 @@ static LazyInstance<ranctx>::Leaky s_ranctx = LAZY_INSTANCE_INITIALIZER;
 void* GetRandomPageBase() {
   uintptr_t random = static_cast<uintptr_t>(ranval(s_ranctx.Pointer()));
 
-#if defined(ARCH_CPU_X86_64)
-  random <<= 32UL;
+#if defined(ARCH_CPU_64_BITS)
+  random <<= 32ULL;
   random |= static_cast<uintptr_t>(ranval(s_ranctx.Pointer()));
 
-// This address mask gives a low likelihood of address space collisions. We
-// handle the situation gracefully if there is a collision.
 #if defined(OS_WIN)
-  random &= 0x3ffffffffffUL;
   // Windows >= 8.1 has the full 47 bits. Use them where available.
   static bool windows_81 = false;
   static bool windows_81_initialized = false;
@@ -106,23 +103,16 @@ void* GetRandomPageBase() {
     windows_81_initialized = true;
   }
   if (!windows_81) {
-    random += 0x10000000000UL;
+    random &= internal::kASLRMaskBefore8_10;
+  } else {
+    random &= internal::kASLRMask;
   }
-#elif defined(MEMORY_TOOL_REPLACES_ALLOCATOR)
-  // This range is copied from the TSan source, but works for all tools.
-  random &= 0x007fffffffffUL;
-  random += 0x7e8000000000UL;
-#else
-  // Linux and OS X support the full 47-bit user space of x64 processors.
-  random &= 0x3fffffffffffUL;
-#endif  // defined(OS_WIN)
-
-#elif defined(ARCH_CPU_ARM64)
-  // ARM64 on Linux has 39-bit user space.
-  random &= 0x3fffffffffUL;
-  random += 0x1000000000UL;
-#else  // !defined(ARCH_CPU_X86_64) && !defined(ARCH_CPU_ARM64)
-
+  random += internal::kASLROffset;
+#else   // defined(OS_POSIX)
+  random &= internal::kASLRMask;
+  random += internal::kASLROffset;
+#endif  // defined(OS_POSIX)
+#else   // defined(ARCH_CPU_32_BITS)
 #if defined(OS_WIN)
   // On win32 host systems the randomization plus huge alignment causes
   // excessive fragmentation. Plus most of these systems lack ASLR, so the
@@ -133,31 +123,12 @@ void* GetRandomPageBase() {
     isWow64 = FALSE;
   if (!isWow64)
     return nullptr;
-#elif defined(OS_MACOSX)
-  // macOS as of 10.12.5 does not clean up entries in page map levels 3/4
-  // [PDP/PML4] created from mmap or mach_vm_allocate, even after the region is
-  // destroyed. Using a virtual address space that is too large causes a leak of
-  // about 1 wired [can never be paged out] page per call to mmap(). The page is
-  // only reclaimed when the process is killed. Confine the hint to a 39-bit
-  // section of the virtual address space.
-  //
-  // This implementation adapted from
-  // https://chromium-review.googlesource.com/c/v8/v8/+/557958. The difference
-  // is that here we clamp to 39 bits, not 32.
-  //
-  // TODO(crbug.com/738925): Remove this limitation if/when the macOS behavior
-  // changes.
-  random &= 0x3fffffffffUL;
-  random += 0x1000000000UL;
 #endif  // defined(OS_WIN)
+  random &= internal::kASLRMask;
+  random += internal::kASLROffset;
+#endif  // defined(ARCH_CPU_32_BITS)
 
-  // This is a good range on Windows, Linux and Mac.
-  // Allocates in the 0.5-1.5GB region.
-  random &= 0x3fffffff;
-  random += 0x20000000;
-#endif  // defined(ARCH_CPU_X86_64)
-
-  random &= kPageAllocationGranularityBaseMask;
+  DCHECK_EQ(0ULL, (random & kPageAllocationGranularityOffsetMask));
   return reinterpret_cast<void*>(random);
 }
 
