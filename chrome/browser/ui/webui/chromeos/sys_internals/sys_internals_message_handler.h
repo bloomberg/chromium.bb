e9175fb9a9b7b648a079c895b8d1306dc1d16038
Lei Zhang <thestig@chromium.org>
2017-10-03 02:22:15 +0000

    Clean up nits in SysInternalsMessageHandler.
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Id7c3b796f1f9491214c5067ad954ac91622f4cf9
    Reviewed-on: https://chromium-review.googlesource.com/689954
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505928}
    Former-commit-id: 7b06e3ad59c5a570743613ceb191b5ec0e9e8599

--- chrome/browser/ui/webui/chromeos/sys_internals/sys_internals_message_handler.h
+++ chrome/browser/ui/webui/chromeos/sys_internals/sys_internals_message_handler.h
@@ -6,12 +6,10 @@
 #define CHROME_BROWSER_UI_WEBUI_CHROMEOS_SYS_INTERNALS_SYS_INTERNALS_MESSAGE_HANDLER_H_
 
 #include <stdint.h>
-#include <memory>
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/task_scheduler/post_task.h"
-#include "base/threading/thread_checker.h"
 #include "base/values.h"
 #include "content/public/browser/web_ui_message_handler.h"
 
@@ -24,16 +22,7 @@ class SysInternalsMessageHandler : public content::WebUIMessageHandler {
   // content::WebUIMessageHandler methods:
   void RegisterMessages() override;
 
-  // Because the base::Value API only supports 32-bit signed integer,
-  // we need to make sure every counter is less than the maximum value.
-  // See ToCounter() in sys_internals_message_handler.cc.
-  static const uint32_t COUNTER_MAX = 0x7FFFFFFFu;
-
  private:
-  THREAD_CHECKER(thread_checker_);
-
-  base::WeakPtrFactory<SysInternalsMessageHandler> weak_ptr_factory_;
-
   // Handle the Javascript message |getSysInfo|. The message is sent to get
   // system information.
   void HandleGetSysInfo(const base::ListValue* args);
@@ -68,8 +57,9 @@ class SysInternalsMessageHandler : public content::WebUIMessageHandler {
   //   total (counter)
   // }
   //
-  void ReplySysInfo(std::unique_ptr<base::Value> callback_id,
-                    std::unique_ptr<base::Value> result);
+  void ReplySysInfo(base::Value callback_id, base::Value result);
+
+  base::WeakPtrFactory<SysInternalsMessageHandler> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(SysInternalsMessageHandler);
 };
