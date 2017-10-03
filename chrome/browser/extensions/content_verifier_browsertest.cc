0f571698f3289a763806487a6aff72f59e26293e
proberge <proberge@chromium.org>
2017-10-03 22:19:33 +0000

    Extension content verification: disable from most tests and update testing config
    Most extension tests use on-disk or generated extensions which are not in the CWS 
    and would fail strict content checks once
    https://chromium-review.googlesource.com/c/chromium/src/+/677403 lands. 
    
    For fieldtrial_testing_config: EnforceStrict has been the Chrome behavior for 
    several versions. This change prevents changes which will fail on the official
    trybots to pass on the Chromium trybots and land.
    
    TBR=atwilson (Trivial component_cloud_policy_browsertest.cc change: removing
    duplicate call to ExtensionBrowserTest::SetUpOnMainThread())
    
    Bug: 766806
    Change-Id: Ifca0440b57952eae51854b059d713b2fcdf80a8f
    Reviewed-on: https://chromium-review.googlesource.com/692554
    Reviewed-by: proberge <proberge@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: proberge <proberge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506206}
    Former-commit-id: e8f8c750c9d97800aa131a65f2406c0276eafc41

--- chrome/browser/extensions/content_verifier_browsertest.cc
+++ chrome/browser/extensions/content_verifier_browsertest.cc
@@ -476,6 +476,8 @@ class ContentVerifierTest : public ExtensionBrowserTest {
         switches::kExtensionContentVerificationEnforce);
   }
 
+  bool ShouldEnableContentVerification() override { return true; }
+
   virtual void OpenPageAndWaitForUnload() {
     ScopedContentVerifyJobDelegateOverride scoped_delegate(&delegate_);
     std::string id = "npnbmohejbjohgpjnmjagbafnjhkmgko";
