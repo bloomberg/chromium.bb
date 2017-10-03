d67dcc4691b3d6f8b06f1db57c5df3831619d7f3
Will Chen <chenwilliam@chromium.org>
2017-10-03 05:06:07 +0000

    DevTools: migrate http/tests/devtools/sources/*.html tests
    This converts a batch of tests in http/tests/devtools/sources from the 
    old test framework (html file) into the new test framework (js file).
    
    Most of it was done through an automated transformation script, with
    some manual clean-up.
    
    Bug: 667560
    Change-Id: I74312a2099c3da04bef8bd1c85df13339cfde00f
    Reviewed-on: https://chromium-review.googlesource.com/688833
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505947}
    Former-commit-id: 3a507452d037e6e7482a064086868523279b82ec

---
+++ third_party/WebKit/LayoutTests/http/tests/devtools/sources/js-sourcemaps-toggle-enabled.js
@@ -0,0 +1,37 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+  TestRunner.addResult(`Verify that JavaScript sourcemap enabling and disabling adds/removes sourcemap sources.\n`);
+  await TestRunner.loadModule('sources_test_runner');
+  await TestRunner.showPanel('sources');
+  await TestRunner.evaluateInPagePromise(`
+      function addScript() {
+          var script = document.createElement('script');
+          script.src = relativeToTest('resources/sourcemap-script.js');
+          document.head.appendChild(script);
+      }
+  `);
+
+  var sourcesNavigator = new Sources.SourcesNavigatorView();
+  sourcesNavigator.show(UI.inspectorView.element);
+
+  Common.moduleSetting('jsSourceMapsEnabled').set(true);
+  TestRunner.evaluateInPagePromise('addScript()');
+  await TestRunner.waitForUISourceCode('sourcemap-typescript.ts'),
+
+      TestRunner.markStep('dumpInitialNavigator');
+  SourcesTestRunner.dumpNavigatorView(sourcesNavigator, false);
+
+  TestRunner.markStep('disableJSSourceMaps');
+  Common.moduleSetting('jsSourceMapsEnabled').set(false);
+  SourcesTestRunner.dumpNavigatorView(sourcesNavigator, false);
+
+  TestRunner.markStep('enableJSSourceMaps');
+  Common.moduleSetting('jsSourceMapsEnabled').set(true);
+  await TestRunner.waitForUISourceCode('sourcemap-typescript.ts'),
+      SourcesTestRunner.dumpNavigatorView(sourcesNavigator, false);
+
+  TestRunner.completeTest();
+})();
