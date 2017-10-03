453ffd6393e99e66c0592a958486c919beac7612
Khushal <khushalsagar@chromium.org>
2017-10-03 00:36:30 +0000

    Reland cc: Decode discardable images in PaintRecord backed shaders.
    This reverts commit 3ce0c258fc73a34e7f2ff09e35a88ae562a80328 [formerly fe24d7f3ac1b5bcfb840a5ada01627df90a099eb]. Since we
    rasterize the SkPicture with decoded images in the test, we need to
    initialize its pixels. Fix that.
    
    R=enne@chromium.org
    TBR=chrishtr@chromium.org
    
    Bug: 735741,728359
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I10989e2bc28594c9fa8e291559ae2753e65de29d
    Reviewed-on: https://chromium-review.googlesource.com/695449
    Reviewed-by: enne <enne@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505885}
    Former-commit-id: 53b8ed642644f245e3cb8d5cf2541bf3778ae7de

--- cc/paint/discardable_image_map.cc
+++ cc/paint/discardable_image_map.cc
@@ -27,6 +27,8 @@ SkRect MapRect(const SkMatrix& matrix, const SkRect& src) {
   return dst;
 }
 
+// This canvas is used only for tracking transform/clip/filter state from the
+// non-drawing ops.
 class PaintTrackingCanvas final : public SkNoDrawCanvas {
  public:
   PaintTrackingCanvas(int width, int height) : SkNoDrawCanvas(width, height) {}
@@ -75,51 +77,13 @@ class PaintTrackingCanvas final : public SkNoDrawCanvas {
 
 class DiscardableImageGenerator {
  public:
-  DiscardableImageGenerator(int width, int height) : canvas_(width, height) {}
-  ~DiscardableImageGenerator() = default;
-
-  void GatherDiscardableImages(const PaintOpBuffer* buffer) {
-    if (!buffer->HasDiscardableImages())
-      return;
-
-    PlaybackParams params(nullptr, canvas_.getTotalMatrix());
-    canvas_.save();
-    // TODO(khushalsagar): Optimize out save/restore blocks if there are no
-    // images in the draw ops between them.
-    for (auto* op : PaintOpBuffer::Iterator(buffer)) {
-      if (op->IsDrawOp()) {
-        SkRect op_rect;
-        if (op->IsPaintOpWithFlags() && PaintOp::GetBounds(op, &op_rect)) {
-          AddImageFromFlags(op_rect,
-                            static_cast<const PaintOpWithFlags*>(op)->flags);
-        }
-
-        PaintOpType op_type = static_cast<PaintOpType>(op->type);
-        if (op_type == PaintOpType::DrawImage) {
-          auto* image_op = static_cast<DrawImageOp*>(op);
-          auto* sk_image = image_op->image.GetSkImage().get();
-          AddImage(image_op->image,
-                   SkRect::MakeIWH(sk_image->width(), sk_image->height()),
-                   SkRect::MakeXYWH(image_op->left, image_op->top,
-                                    sk_image->width(), sk_image->height()),
-                   nullptr, image_op->flags);
-        } else if (op_type == PaintOpType::DrawImageRect) {
-          auto* image_rect_op = static_cast<DrawImageRectOp*>(op);
-          SkMatrix matrix;
-          matrix.setRectToRect(image_rect_op->src, image_rect_op->dst,
-                               SkMatrix::kFill_ScaleToFit);
-          AddImage(image_rect_op->image, image_rect_op->src, image_rect_op->dst,
-                   &matrix, image_rect_op->flags);
-        } else if (op_type == PaintOpType::DrawRecord) {
-          GatherDiscardableImages(
-              static_cast<const DrawRecordOp*>(op)->record.get());
-        }
-      } else {
-        op->Raster(&canvas_, params);
-      }
-    }
-    canvas_.restore();
+  DiscardableImageGenerator(int width,
+                            int height,
+                            const PaintOpBuffer* buffer) {
+    PaintTrackingCanvas canvas(width, height);
+    GatherDiscardableImages(buffer, nullptr, &canvas);
   }
+  ~DiscardableImageGenerator() = default;
 
   std::vector<std::pair<DrawImage, gfx::Rect>> TakeImages() {
     return std::move(image_set_);
@@ -153,47 +117,111 @@ class DiscardableImageGenerator {
   }
 
  private:
-  void AddImageFromFlags(const SkRect& rect, const PaintFlags& flags) {
-    if (!flags.HasShader() ||
-        flags.getShader()->shader_type() != PaintShader::Type::kImage)
+  // Adds discardable images from |buffer| to the set of images tracked by
+  // this generator. If |buffer| is being used in a DrawOp that requires
+  // rasterization of the buffer as a pre-processing step for execution of the
+  // op (for instance, with PaintRecord backed PaintShaders),
+  // |top_level_op_rect| is set to the rect for that op. If provided, the
+  // |top_level_op_rect| will be used as the rect for tracking the position of
+  // this image in the top-level buffer.
+  void GatherDiscardableImages(const PaintOpBuffer* buffer,
+                               const gfx::Rect* top_level_op_rect,
+                               PaintTrackingCanvas* canvas) {
+    if (!buffer->HasDiscardableImages())
       return;
 
-    const PaintImage& paint_image = flags.getShader()->paint_image();
-    SkMatrix local_matrix = flags.getShader()->GetLocalMatrix();
-    AddImage(paint_image,
-             SkRect::MakeWH(paint_image.width(), paint_image.height()), rect,
-             &local_matrix, flags);
+    // Prevent PaintOpBuffers from having side effects back into the canvas.
+    SkAutoCanvasRestore save_restore(canvas, true);
+
+    PlaybackParams params(nullptr, canvas->getTotalMatrix());
+    // TODO(khushalsagar): Optimize out save/restore blocks if there are no
+    // images in the draw ops between them.
+    for (auto* op : PaintOpBuffer::Iterator(buffer)) {
+      if (!op->IsDrawOp()) {
+        op->Raster(canvas, params);
+        continue;
+      } else if (!PaintOp::OpHasDiscardableImages(op)) {
+        continue;
+      }
+
+      gfx::Rect op_rect;
+      base::Optional<gfx::Rect> local_op_rect;
+
+      if (top_level_op_rect) {
+        op_rect = *top_level_op_rect;
+      } else {
+        local_op_rect = ComputePaintRect(op, canvas);
+        if (local_op_rect.value().IsEmpty())
+          continue;
+
+        op_rect = local_op_rect.value();
+      }
+
+      const SkMatrix& ctm = canvas->getTotalMatrix();
+      if (op->IsPaintOpWithFlags()) {
+        AddImageFromFlags(op_rect,
+                          static_cast<const PaintOpWithFlags*>(op)->flags, ctm);
+      }
+
+      PaintOpType op_type = static_cast<PaintOpType>(op->type);
+      if (op_type == PaintOpType::DrawImage) {
+        auto* image_op = static_cast<DrawImageOp*>(op);
+        auto* sk_image = image_op->image.GetSkImage().get();
+        AddImage(image_op->image,
+                 SkRect::MakeIWH(sk_image->width(), sk_image->height()),
+                 op_rect, ctm, image_op->flags.getFilterQuality());
+      } else if (op_type == PaintOpType::DrawImageRect) {
+        auto* image_rect_op = static_cast<DrawImageRectOp*>(op);
+        SkMatrix matrix = ctm;
+        matrix.postConcat(SkMatrix::MakeRectToRect(image_rect_op->src,
+                                                   image_rect_op->dst,
+                                                   SkMatrix::kFill_ScaleToFit));
+        AddImage(image_rect_op->image, image_rect_op->src, op_rect, matrix,
+                 image_rect_op->flags.getFilterQuality());
+      } else if (op_type == PaintOpType::DrawRecord) {
+        GatherDiscardableImages(
+            static_cast<const DrawRecordOp*>(op)->record.get(),
+            top_level_op_rect, canvas);
+      }
+    }
   }
 
-  void AddImage(PaintImage paint_image,
-                const SkRect& src_rect,
-                const SkRect& rect,
-                const SkMatrix* local_matrix,
-                const PaintFlags& flags) {
-    if (!paint_image.IsLazyGenerated())
-      return;
+  // Given the |op_rect|, which is the rect for the draw op, returns the
+  // transformed rect accounting for the current transform, clip and paint
+  // state on |canvas_|.
+  gfx::Rect ComputePaintRect(const PaintOp* op, PaintTrackingCanvas* canvas) {
+    const SkRect& clip_rect = SkRect::Make(canvas->getDeviceClipBounds());
+    const SkMatrix& ctm = canvas->getTotalMatrix();
 
-    const SkRect& clip_rect = SkRect::Make(canvas_.getDeviceClipBounds());
-    const SkMatrix& ctm = canvas_.getTotalMatrix();
+    gfx::Rect transformed_rect;
+    SkRect op_rect;
+    if (!PaintOp::GetBounds(op, &op_rect)) {
+      // If we can't provide a conservative bounding rect for the op, assume it
+      // covers the complete current clip.
+      transformed_rect = gfx::ToEnclosingRect(gfx::SkRectToRectF(clip_rect));
+    } else {
+      const PaintFlags* flags =
+          op->IsPaintOpWithFlags()
+              ? &static_cast<const PaintOpWithFlags*>(op)->flags
+              : nullptr;
+      SkPaint paint;
+      if (flags)
+        paint = flags->ToSkPaint();
 
-    SkRect paint_rect = MapRect(ctm, rect);
-    SkPaint paint = flags.ToSkPaint();
-    bool computed_paint_bounds =
-        canvas_.ComputePaintBounds(paint_rect, &paint, &paint_rect);
-    if (!computed_paint_bounds) {
-      // TODO(vmpstr): UMA this case.
-      paint_rect = clip_rect;
+      SkRect paint_rect = MapRect(ctm, op_rect);
+      bool computed_paint_bounds =
+          canvas->ComputePaintBounds(paint_rect, &paint, &paint_rect);
+      if (!computed_paint_bounds) {
+        // TODO(vmpstr): UMA this case.
+        paint_rect = clip_rect;
+      }
+
+      // Clamp the image rect by the current clip rect.
+      if (!paint_rect.intersect(clip_rect))
+        return gfx::Rect();
+
+      transformed_rect = gfx::ToEnclosingRect(gfx::SkRectToRectF(paint_rect));
     }
-
-    // Clamp the image rect by the current clip rect.
-    if (!paint_rect.intersect(clip_rect))
-      return;
-
-    SkFilterQuality filter_quality = flags.getFilterQuality();
-
-    SkIRect src_irect;
-    src_rect.roundOut(&src_irect);
-    gfx::Rect image_rect = gfx::ToEnclosingRect(gfx::SkRectToRectF(paint_rect));
 
     // During raster, we use the device clip bounds on the canvas, which outsets
     // the actual clip by 1 due to the possibility of antialiasing. Account for
@@ -205,7 +233,52 @@ class DiscardableImageGenerator {
     // raster time, since we might be sending a larger-than-one-item display
     // item to skia, which means that skia will internally determine whether to
     // raster the picture (using device clip bounds that are outset).
-    image_rect.Inset(-1, -1);
+    transformed_rect.Inset(-1, -1);
+    return transformed_rect;
+  }
+
+  void AddImageFromFlags(const gfx::Rect& op_rect,
+                         const PaintFlags& flags,
+                         const SkMatrix& ctm) {
+    if (!flags.getShader())
+      return;
+
+    if (flags.getShader()->shader_type() == PaintShader::Type::kImage) {
+      const PaintImage& paint_image = flags.getShader()->paint_image();
+      SkMatrix matrix = ctm;
+      matrix.postConcat(flags.getShader()->GetLocalMatrix());
+      AddImage(paint_image,
+               SkRect::MakeWH(paint_image.width(), paint_image.height()),
+               op_rect, matrix, flags.getFilterQuality());
+    } else if (flags.getShader()->shader_type() ==
+                   PaintShader::Type::kPaintRecord &&
+               flags.getShader()->paint_record()->HasDiscardableImages()) {
+      SkRect scaled_tile_rect;
+      if (!flags.getShader()->GetRasterizationTileRect(ctm,
+                                                       &scaled_tile_rect)) {
+        return;
+      }
+
+      PaintTrackingCanvas canvas(scaled_tile_rect.width(),
+                                 scaled_tile_rect.height());
+      canvas.setMatrix(SkMatrix::MakeRectToRect(flags.getShader()->tile(),
+                                                scaled_tile_rect,
+                                                SkMatrix::kFill_ScaleToFit));
+      GatherDiscardableImages(flags.getShader()->paint_record().get(), &op_rect,
+                              &canvas);
+    }
+  }
+
+  void AddImage(PaintImage paint_image,
+                const SkRect& src_rect,
+                const gfx::Rect& image_rect,
+                const SkMatrix& matrix,
+                SkFilterQuality filter_quality) {
+    if (!paint_image.IsLazyGenerated())
+      return;
+
+    SkIRect src_irect;
+    src_rect.roundOut(&src_irect);
 
     // Make a note if any image was originally specified in a non-sRGB color
     // space.
@@ -216,10 +289,6 @@ class DiscardableImageGenerator {
       color_stats_srgb_pixel_count_ += image_rect.size().GetCheckedArea();
       color_stats_srgb_image_count_++;
     }
-
-    SkMatrix matrix = ctm;
-    if (local_matrix)
-      matrix.postConcat(*local_matrix);
 
     image_id_to_rect_[paint_image.stable_id()].Union(image_rect);
 
@@ -235,9 +304,6 @@ class DiscardableImageGenerator {
         image_rect);
   }
 
-  // This canvas is used only for tracking transform/clip/filter state from the
-  // non-drawing ops.
-  PaintTrackingCanvas canvas_;
   std::vector<std::pair<DrawImage, gfx::Rect>> image_set_;
   base::flat_map<PaintImage::Id, gfx::Rect> image_id_to_rect_;
   std::vector<DiscardableImageMap::AnimatedImageMetadata>
@@ -263,8 +329,8 @@ void DiscardableImageMap::Generate(const PaintOpBuffer* paint_op_buffer,
   if (!paint_op_buffer->HasDiscardableImages())
     return;
 
-  DiscardableImageGenerator generator(bounds.right(), bounds.bottom());
-  generator.GatherDiscardableImages(paint_op_buffer);
+  DiscardableImageGenerator generator(bounds.right(), bounds.bottom(),
+                                      paint_op_buffer);
   generator.RecordColorHistograms();
   image_id_to_rect_ = generator.TakeImageIdToRectMap();
   animated_images_metadata_ = generator.TakeAnimatedImagesMetadata();
