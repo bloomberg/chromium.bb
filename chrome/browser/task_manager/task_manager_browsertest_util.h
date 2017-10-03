672b22b4d590404f6b67d80ec4f12059f0689267
Erik Chen <erikchen@chromium.org>
2017-10-03 22:59:57 +0000

    Switch task manager to use the memory_instrumentation interface.
    The memory_instrumentation interface on the content_browser service exposes
    cross-platform, consistent memory metrics that should also be used by the task
    manager.
    
    This CL adds a new column to the task manager: Memory Footprint. See
    https://docs.google.com/document/d/1PZyRzChnvkUNUB85Op46aqkFXuAGUJi751DJuB6O40g/edit#
    for more details.
    
    Bug: 756276
    Change-Id: I68fdd6868a231840a1a8401dcd7ec50ffb0af068
    Reviewed-on: https://chromium-review.googlesource.com/646047
    Reviewed-by: Nick Carter <nick@chromium.org>
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506220}
    Former-commit-id: 4925a99f1f1c95509e319a4a86bd0ff63d0a0a6f

--- chrome/browser/task_manager/task_manager_browsertest_util.h
+++ chrome/browser/task_manager/task_manager_browsertest_util.h
@@ -22,6 +22,7 @@ namespace browsertest_util {
 // manager model. Please add more here as needed by tests.
 enum class ColumnSpecifier {
   PROCESS_ID,
+  MEMORY_FOOTPRINT,
   PHYSICAL_MEMORY,
   V8_MEMORY,
   V8_MEMORY_USED,
