3ca6baff0010da4aa97c6700d088201efcf99ec9
Becca Hughes <beccahughes@chromium.org>
2017-10-03 08:42:02 +0000

    Reland "Media Controls: Load CSS directly in Blink"
    This is a reland of 77d22f23c6b11715516e1802631f97a863034ca3 [formerly 3255bf706d4b958b9ac13515faef6270f7ae4f37]
    Original change's description:
    > Media Controls: Load CSS directly in Blink
    >
    > Now that MediaControlsPainter has been removed, we should move
    > resource loading for media controls to the media_controls
    > module in Blink.
    >
    > BUG=757445
    >
    > Change-Id: Ic756e6dd0cf06109b7bae1f62dddd0e02c5dfb06
    > Reviewed-on: https://chromium-review.googlesource.com/628171
    > Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#505569}
    
    TBR=haraken@chromium.org,mlamouri@chromium.org,jochen@chromium.org,beccahughes@chromium.org
    
    Bug: 757445
    Change-Id: If0fe56bb489cfd0b1e1b27c7da304bf75dc23be7
    Reviewed-on: https://chromium-review.googlesource.com/695328
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505985}
    Former-commit-id: 3b0c080b0bb28d3d8616d863f0302053dd1dbf42

--- content/test/test_content_client.h
+++ content/test/test_content_client.h
@@ -11,7 +11,6 @@
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "content/public/common/content_client.h"
-#include "ui/base/resource/data_pack.h"
 
 namespace content {
 
@@ -27,8 +26,6 @@ class TestContentClient : public ContentClient {
       ui::ScaleFactor scale_factor) const override;
 
  private:
-  ui::DataPack data_pack_;
-
   DISALLOW_COPY_AND_ASSIGN(TestContentClient);
 };
 
