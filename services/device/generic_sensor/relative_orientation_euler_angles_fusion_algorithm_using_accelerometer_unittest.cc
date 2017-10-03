da0ddb8340740120c0f0fc55daa7f7ffc4a7368b
Jun Cai <juncai@chromium.org>
2017-10-03 01:48:48 +0000

    Set unavailable sensor data field Not-A-Number
    Currently the generic_sensor code doesn't provide a mechanism to mark
    which sensor data field is unavailable. This CL uses Not-A-Number to
    mark that.
    
    Bug: 768473
    Change-Id: I7debdfde015cd5bec83c01f4a01b781a419e74e7
    Reviewed-on: https://chromium-review.googlesource.com/682557
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505918}
    Former-commit-id: 05201d651839d0ed5498bbb3e4b17a3d52a69950

--- services/device/generic_sensor/relative_orientation_euler_angles_fusion_algorithm_using_accelerometer_unittest.cc
+++ services/device/generic_sensor/relative_orientation_euler_angles_fusion_algorithm_using_accelerometer_unittest.cc
@@ -32,7 +32,6 @@ class RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometerTest
   void VerifyRelativeOrientationEulerAngles(double acceleration_x,
                                             double acceleration_y,
                                             double acceleration_z,
-                                            double expected_alpha_in_degrees,
                                             double expected_beta_in_degrees,
                                             double expected_gamma_in_degrees) {
     SensorReading reading;
@@ -48,8 +47,8 @@ class RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometerTest
     EXPECT_TRUE(fusion_algorithm_->GetFusedData(
         mojom::SensorType::ACCELEROMETER, &fused_reading));
 
-    EXPECT_DOUBLE_EQ(expected_alpha_in_degrees,
-                     fused_reading.orientation_euler.z /* alpha */);
+    EXPECT_TRUE(
+        std::isnan(fused_reading.orientation_euler.z.value() /* alpha */));
     EXPECT_DOUBLE_EQ(expected_beta_in_degrees,
                      fused_reading.orientation_euler.x /* beta */);
     EXPECT_DOUBLE_EQ(expected_gamma_in_degrees,
@@ -82,13 +81,12 @@ TEST_F(RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometerTest,
   double acceleration_y = 0.0;
   double acceleration_z = kMeanGravity;
 
-  double expected_alpha_in_degrees = 0.0;
   double expected_beta_in_degrees = 0.0;
   double expected_gamma_in_degrees = 0.0;
 
-  VerifyRelativeOrientationEulerAngles(
-      acceleration_x, acceleration_y, acceleration_z, expected_alpha_in_degrees,
-      expected_beta_in_degrees, expected_gamma_in_degrees);
+  VerifyRelativeOrientationEulerAngles(acceleration_x, acceleration_y,
+                                       acceleration_z, expected_beta_in_degrees,
+                                       expected_gamma_in_degrees);
 }
 
 // Tests an upside-down device, such that the W3C boundary [-180, 180) causes
@@ -99,13 +97,12 @@ TEST_F(RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometerTest,
   double acceleration_y = 0.0;
   double acceleration_z = -kMeanGravity;
 
-  double expected_alpha_in_degrees = 0.0;
   double expected_beta_in_degrees = -180.0;
   double expected_gamma_in_degrees = 0.0;
 
-  VerifyRelativeOrientationEulerAngles(
-      acceleration_x, acceleration_y, acceleration_z, expected_alpha_in_degrees,
-      expected_beta_in_degrees, expected_gamma_in_degrees);
+  VerifyRelativeOrientationEulerAngles(acceleration_x, acceleration_y,
+                                       acceleration_z, expected_beta_in_degrees,
+                                       expected_gamma_in_degrees);
 }
 
 // Tests for positive beta value before the device is completely upside-down.
@@ -115,13 +112,12 @@ TEST_F(RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometerTest,
   double acceleration_y = -kMeanGravity / 2.0;
   double acceleration_z = -kMeanGravity / 2.0;
 
-  double expected_alpha_in_degrees = 0.0;
   double expected_beta_in_degrees = 135.0;
   double expected_gamma_in_degrees = 0.0;
 
-  VerifyRelativeOrientationEulerAngles(
-      acceleration_x, acceleration_y, acceleration_z, expected_alpha_in_degrees,
-      expected_beta_in_degrees, expected_gamma_in_degrees);
+  VerifyRelativeOrientationEulerAngles(acceleration_x, acceleration_y,
+                                       acceleration_z, expected_beta_in_degrees,
+                                       expected_gamma_in_degrees);
 }
 
 // Tests a device lying on its top-edge.
@@ -131,13 +127,12 @@ TEST_F(RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometerTest,
   double acceleration_y = kMeanGravity;
   double acceleration_z = 0.0;
 
-  double expected_alpha_in_degrees = 0.0;
   double expected_beta_in_degrees = -90.0;
   double expected_gamma_in_degrees = 0.0;
 
-  VerifyRelativeOrientationEulerAngles(
-      acceleration_x, acceleration_y, acceleration_z, expected_alpha_in_degrees,
-      expected_beta_in_degrees, expected_gamma_in_degrees);
+  VerifyRelativeOrientationEulerAngles(acceleration_x, acceleration_y,
+                                       acceleration_z, expected_beta_in_degrees,
+                                       expected_gamma_in_degrees);
 }
 
 // Tests before a device is completely on its top-edge.
@@ -147,13 +142,12 @@ TEST_F(RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometerTest,
   double acceleration_y = kMeanGravity / 2.0;
   double acceleration_z = kMeanGravity / 2.0;
 
-  double expected_alpha_in_degrees = 0.0;
   double expected_beta_in_degrees = -45.0;
   double expected_gamma_in_degrees = 0.0;
 
-  VerifyRelativeOrientationEulerAngles(
-      acceleration_x, acceleration_y, acceleration_z, expected_alpha_in_degrees,
-      expected_beta_in_degrees, expected_gamma_in_degrees);
+  VerifyRelativeOrientationEulerAngles(acceleration_x, acceleration_y,
+                                       acceleration_z, expected_beta_in_degrees,
+                                       expected_gamma_in_degrees);
 }
 
 // Tests a device lying on its bottom-edge.
@@ -163,13 +157,12 @@ TEST_F(RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometerTest,
   double acceleration_y = -kMeanGravity;
   double acceleration_z = 0.0;
 
-  double expected_alpha_in_degrees = 0.0;
   double expected_beta_in_degrees = 90.0;
   double expected_gamma_in_degrees = 0.0;
 
-  VerifyRelativeOrientationEulerAngles(
-      acceleration_x, acceleration_y, acceleration_z, expected_alpha_in_degrees,
-      expected_beta_in_degrees, expected_gamma_in_degrees);
+  VerifyRelativeOrientationEulerAngles(acceleration_x, acceleration_y,
+                                       acceleration_z, expected_beta_in_degrees,
+                                       expected_gamma_in_degrees);
 }
 
 // Tests before a device is completely on its bottom-edge.
@@ -179,13 +172,12 @@ TEST_F(RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometerTest,
   double acceleration_y = -kMeanGravity / 2.0;
   double acceleration_z = kMeanGravity / 2.0;
 
-  double expected_alpha_in_degrees = 0.0;
   double expected_beta_in_degrees = 45.0;
   double expected_gamma_in_degrees = 0.0;
 
-  VerifyRelativeOrientationEulerAngles(
-      acceleration_x, acceleration_y, acceleration_z, expected_alpha_in_degrees,
-      expected_beta_in_degrees, expected_gamma_in_degrees);
+  VerifyRelativeOrientationEulerAngles(acceleration_x, acceleration_y,
+                                       acceleration_z, expected_beta_in_degrees,
+                                       expected_gamma_in_degrees);
 }
 
 // Tests a device lying on its left-edge.
@@ -195,13 +187,12 @@ TEST_F(RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometerTest,
   double acceleration_y = 0.0;
   double acceleration_z = 0.0;
 
-  double expected_alpha_in_degrees = 0.0;
   double expected_beta_in_degrees = 0.0;
   double expected_gamma_in_degrees = -90.0;
 
-  VerifyRelativeOrientationEulerAngles(
-      acceleration_x, acceleration_y, acceleration_z, expected_alpha_in_degrees,
-      expected_beta_in_degrees, expected_gamma_in_degrees);
+  VerifyRelativeOrientationEulerAngles(acceleration_x, acceleration_y,
+                                       acceleration_z, expected_beta_in_degrees,
+                                       expected_gamma_in_degrees);
 }
 
 // Tests for negative gamma value before the device is completely on its left
@@ -212,13 +203,12 @@ TEST_F(RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometerTest,
   double acceleration_y = 0.0;
   double acceleration_z = kMeanGravity / std::sqrt(2.0);
 
-  double expected_alpha_in_degrees = 0.0;
   double expected_beta_in_degrees = 0.0;
   double expected_gamma_in_degrees = -45.0;
 
-  VerifyRelativeOrientationEulerAngles(
-      acceleration_x, acceleration_y, acceleration_z, expected_alpha_in_degrees,
-      expected_beta_in_degrees, expected_gamma_in_degrees);
+  VerifyRelativeOrientationEulerAngles(acceleration_x, acceleration_y,
+                                       acceleration_z, expected_beta_in_degrees,
+                                       expected_gamma_in_degrees);
 }
 
 // Tests a device lying on its right-edge, such that the W3C boundary [-90, 90)
@@ -229,13 +219,12 @@ TEST_F(RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometerTest,
   double acceleration_y = 0.0;
   double acceleration_z = 0.0;
 
-  double expected_alpha_in_degrees = 0.0;
   double expected_beta_in_degrees = 0.0;
   double expected_gamma_in_degrees = -90.0;
 
-  VerifyRelativeOrientationEulerAngles(
-      acceleration_x, acceleration_y, acceleration_z, expected_alpha_in_degrees,
-      expected_beta_in_degrees, expected_gamma_in_degrees);
+  VerifyRelativeOrientationEulerAngles(acceleration_x, acceleration_y,
+                                       acceleration_z, expected_beta_in_degrees,
+                                       expected_gamma_in_degrees);
 }
 
 // Tests for positive gamma value before the device is completely on its right
@@ -246,13 +235,12 @@ TEST_F(RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometerTest,
   double acceleration_y = 0.0;
   double acceleration_z = kMeanGravity / std::sqrt(2.0);
 
-  double expected_alpha_in_degrees = 0.0;
   double expected_beta_in_degrees = 0.0;
   double expected_gamma_in_degrees = 45.0;
 
-  VerifyRelativeOrientationEulerAngles(
-      acceleration_x, acceleration_y, acceleration_z, expected_alpha_in_degrees,
-      expected_beta_in_degrees, expected_gamma_in_degrees);
+  VerifyRelativeOrientationEulerAngles(acceleration_x, acceleration_y,
+                                       acceleration_z, expected_beta_in_degrees,
+                                       expected_gamma_in_degrees);
 }
 
 }  // namespace device
