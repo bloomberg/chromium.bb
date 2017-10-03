53adc4bd7d127969a0670df78e564982d8fdf181
Evan Stade <estade@chromium.org>
2017-10-03 18:52:31 +0000

    Remove an ash reference from webui settings.
    Bug: 756095
    Change-Id: Ieb207278d7b38228b7a5361aafd8062558869431
    Reviewed-on: https://chromium-review.googlesource.com/696286
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506123}
    Former-commit-id: cae9d30cb818886a654ad21a3663f526a26a642b

--- ash/new_window_controller.h
+++ ash/new_window_controller.h
@@ -5,6 +5,7 @@
 #ifndef ASH_NEW_WINDOW_CONTROLLER_H_
 #define ASH_NEW_WINDOW_CONTROLLER_H_
 
+#include "ash/ash_export.h"
 #include "ash/public/interfaces/new_window.mojom.h"
 #include "base/macros.h"
 #include "mojo/public/cpp/bindings/associated_binding.h"
@@ -15,29 +16,28 @@ namespace ash {
 // Provides the NewWindowController interface to the outside world. This lets a
 // consumer of ash provide a NewWindowClient, which we will dispatch to if one
 // has been provided to us.
-class NewWindowController : public mojom::NewWindowController,
-                            public mojom::NewWindowClient {
+class ASH_EXPORT NewWindowController : public mojom::NewWindowController {
  public:
   NewWindowController();
   ~NewWindowController() override;
 
   void BindRequest(mojom::NewWindowControllerRequest request);
 
-  // NewWindowClient:
-  void NewTab() override;
-  void NewWindow(bool incognito) override;
-  void OpenFileManager() override;
-  void OpenCrosh() override;
-  void OpenGetHelp() override;
-  void RestoreTab() override;
-  void ShowKeyboardOverlay() override;
-  void ShowTaskManager() override;
-  void OpenFeedbackPage() override;
-
- private:
   // NewWindowController:
   void SetClient(mojom::NewWindowClientAssociatedPtrInfo client) override;
+  void ShowKeyboardOverlay() override;
 
+  // Pass throughs for methods of the same name on |client_|.
+  void NewTab();
+  void NewWindow(bool incognito);
+  void OpenFileManager();
+  void OpenCrosh();
+  void OpenGetHelp();
+  void RestoreTab();
+  void ShowTaskManager();
+  void OpenFeedbackPage();
+
+ private:
   mojo::BindingSet<mojom::NewWindowController> bindings_;
 
   mojom::NewWindowClientAssociatedPtr client_;
