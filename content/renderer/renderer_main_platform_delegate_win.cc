9484c72b71fe8b8d91f4cb146aac3e54f28ecf47
Wei Li <weili@chromium.org>
2017-10-03 20:00:52 +0000

    Add access to direct write font proxy in pdf compositor service
    Pdf compositor service needs to access direct write font proxy to
    be able to use skia to draw properly on Windows. This CL move font
    proxy init header to public so more processes (including ours)
    can use it, also it adds font proxy initialization in pdf
    compositor service.
    
    TBR=thestig@chromium.org
    BUG=455764
    
    Change-Id: Ia7bf3c7804ae0fea671087a53cf177615ca4d59e
    Reviewed-on: https://chromium-review.googlesource.com/685271
    Commit-Queue: Wei Li <weili@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506150}
    Former-commit-id: bf941b2b1f9a53ecb2d3263b80a21f42340d51bb

--- content/renderer/renderer_main_platform_delegate_win.cc
+++ content/renderer/renderer_main_platform_delegate_win.cc
@@ -14,7 +14,7 @@
 #include "base/win/scoped_comptr.h"
 #include "base/win/win_util.h"
 #include "base/win/windows_version.h"
-#include "content/child/dwrite_font_proxy/dwrite_font_proxy_init_win.h"
+#include "content/child/dwrite_font_proxy/dwrite_font_proxy_init_impl_win.h"
 #include "content/child/font_warmup_win.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/injection_test_win.h"
