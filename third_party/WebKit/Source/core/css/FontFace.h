c6825342a7616dfc20d91505c46a0d373993ddc1
Nate Chapin <japhet@chromium.org>
2017-10-04 18:29:08 +0000

    Early-exit in FontFace::SetLoadStatus when there's no ExecutionContext
    GetTaskRunner() will crash with a nullptr ExecutionContext, and all the
    other work is nonsensical in the absence of an ExecutionContext.
    
    Bug: 763040
    Change-Id: Ide915c02265e35e55c7dc5aa234652efc5016b55
    Reviewed-on: https://chromium-review.googlesource.com/698606
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506453}
    Former-commit-id: af33ce6848037fa0e000cd210a1924f35f8fc59a

--- third_party/WebKit/Source/core/css/FontFace.h
+++ third_party/WebKit/Source/core/css/FontFace.h
@@ -54,7 +54,6 @@ class FontFaceDescriptors;
 class StringOrArrayBufferOrArrayBufferView;
 class StylePropertySet;
 class StyleRuleFontFace;
-class WebTaskRunner;
 
 class CORE_EXPORT FontFace : public GarbageCollectedFinalized<FontFace>,
                              public ScriptWrappable,
@@ -157,7 +156,6 @@ class CORE_EXPORT FontFace : public GarbageCollectedFinalized<FontFace>,
   bool SetPropertyValue(const CSSValue*, CSSPropertyID);
   bool SetFamilyValue(const CSSValue&);
   ScriptPromise FontStatusPromise(ScriptState*);
-  WebTaskRunner* GetTaskRunner();
   void RunCallbacks();
 
   using LoadedProperty = ScriptPromiseProperty<Member<FontFace>,
