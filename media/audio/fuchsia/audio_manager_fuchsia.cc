d080bd6fc1ef58467263176bba58b35e83e1e0e9
Sergey Ulanov <sergeyu@chromium.org>
2017-10-04 20:20:48 +0000

    Implement AudioOutputStream for Fuchsia
    Added AudioOutputStreamFuchsia which implemented audio output using
    Fuchsia's media_client library. Also updated AudioManagerFuchsia to
    enumerate output devices and create AudioOutputStream.
    
    Bug: 737802
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I344ed5ad885d602574dd03ad31e85d7b76e0c2b8
    Reviewed-on: https://chromium-review.googlesource.com/690622
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506500}
    Former-commit-id: cae560d544c1d5f82d0f40e664d2e40b33d145e7

--- media/audio/fuchsia/audio_manager_fuchsia.cc
+++ media/audio/fuchsia/audio_manager_fuchsia.cc
@@ -4,21 +4,24 @@
 
 #include "media/audio/fuchsia/audio_manager_fuchsia.h"
 
+#include <media/audio.h>
+
 #include "base/memory/ptr_util.h"
+#include "media/audio/fuchsia/audio_output_stream_fuchsia.h"
 
 namespace media {
-
-// TODO(fuchsia): Implement this class.
 
 AudioManagerFuchsia::AudioManagerFuchsia(
     std::unique_ptr<AudioThread> audio_thread,
     AudioLogFactory* audio_log_factory)
-    : AudioManagerBase(std::move(audio_thread), audio_log_factory) {}
+    : AudioManagerBase(std::move(audio_thread), audio_log_factory),
+      fuchsia_audio_manager_(fuchsia_audio_manager_create()) {}
+
 AudioManagerFuchsia::~AudioManagerFuchsia() {}
 
 bool AudioManagerFuchsia::HasAudioOutputDevices() {
-  NOTIMPLEMENTED();
-  return false;
+  return fuchsia_audio_manager_get_output_devices(fuchsia_audio_manager_,
+                                                  nullptr, 0) > 0;
 }
 
 bool AudioManagerFuchsia::HasAudioInputDevices() {
@@ -35,7 +38,32 @@ void AudioManagerFuchsia::GetAudioInputDeviceNames(
 void AudioManagerFuchsia::GetAudioOutputDeviceNames(
     AudioDeviceNames* device_names) {
   device_names->clear();
-  NOTIMPLEMENTED();
+
+  std::vector<fuchsia_audio_device_description> descriptions;
+  descriptions.resize(16);
+  bool try_again = true;
+  while (try_again) {
+    int result = fuchsia_audio_manager_get_output_devices(
+        fuchsia_audio_manager_, descriptions.data(), descriptions.size());
+    if (result < 0) {
+      LOG(ERROR) << "fuchsia_audio_manager_get_output_devices() returned "
+                 << result;
+      device_names->clear();
+      return;
+    }
+
+    // Try again if the buffer was too small.
+    try_again = static_cast<size_t>(result) > descriptions.size();
+    descriptions.resize(result);
+  }
+
+  // Create default device if we have any output devices present.
+  if (!descriptions.empty())
+    device_names->push_back(AudioDeviceName::CreateDefault());
+
+  for (auto& desc : descriptions) {
+    device_names->push_back(AudioDeviceName(desc.name, desc.id));
+  }
 }
 
 AudioParameters AudioManagerFuchsia::GetInputStreamParameters(
@@ -47,8 +75,32 @@ AudioParameters AudioManagerFuchsia::GetInputStreamParameters(
 AudioParameters AudioManagerFuchsia::GetPreferredOutputStreamParameters(
     const std::string& output_device_id,
     const AudioParameters& input_params) {
-  NOTREACHED();
-  return AudioParameters();
+  fuchsia_audio_parameters device_params;
+  int result = fuchsia_audio_manager_get_output_device_default_parameters(
+      fuchsia_audio_manager_,
+      output_device_id == AudioDeviceDescription::kDefaultDeviceId
+          ? nullptr
+          : const_cast<char*>(output_device_id.c_str()),
+      &device_params);
+  if (result < 0) {
+    LOG(ERROR) << "fuchsia_audio_manager_get_default_output_device_parameters()"
+                  " returned "
+               << result;
+
+    return AudioParameters();
+  }
+
+  int user_buffer_size = GetUserBufferSize();
+  if (user_buffer_size > 0)
+    device_params.buffer_size = user_buffer_size;
+
+  int sample_rate = input_params.sample_rate();
+  if (sample_rate < 8000 || sample_rate > 96000)
+    sample_rate = device_params.sample_rate;
+
+  return AudioParameters(AudioParameters::AUDIO_PCM_LOW_LATENCY,
+                         GuessChannelLayout(device_params.num_channels),
+                         sample_rate, 24, device_params.buffer_size);
 }
 
 const char* AudioManagerFuchsia::GetName() {
@@ -66,8 +118,8 @@ AudioOutputStream* AudioManagerFuchsia::MakeLowLatencyOutputStream(
     const AudioParameters& params,
     const std::string& device_id,
     const LogCallback& log_callback) {
-  NOTREACHED();
-  return nullptr;
+  DCHECK_EQ(AudioParameters::AUDIO_PCM_LOW_LATENCY, params.format());
+  return new AudioOutputStreamFuchsia(this, device_id, params);
 }
 
 AudioInputStream* AudioManagerFuchsia::MakeLinearInputStream(
