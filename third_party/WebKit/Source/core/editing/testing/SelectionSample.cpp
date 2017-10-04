30dcec46516ebe1c2b39f513efe242ef54183c7b
Yoshifumi Inoue <yosin@chromium.org>
2017-10-04 02:13:23 +0000

    Untemplatize Parser class in SelectionSample.cpp
    This patch changes |Parser| class in "SelectionSample.cpp" as non-template
    class since |Parser| class is used only for DOM tree traversal to simplify
    code for improving code health.
    
    Change-Id: I58f3749fab4e841686f7eedd64e3b2ad589363ec
    Reviewed-on: https://chromium-review.googlesource.com/694722
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506277}
    Former-commit-id: 654abc5bddd234f11f3122113e7db443fc693d01

--- third_party/WebKit/Source/core/editing/testing/SelectionSample.cpp
+++ third_party/WebKit/Source/core/editing/testing/SelectionSample.cpp
@@ -49,7 +49,6 @@ void ConvertTemplatesToShadowRoots(HTMLElement& element) {
 }
 
 // Parse selection text notation into Selection object.
-template <typename Strategy>
 class Parser final {
   STACK_ALLOCATED();
 
@@ -58,22 +57,21 @@ class Parser final {
   ~Parser() = default;
 
   // Set |selection_text| as inner HTML of |element| and returns
-  // |SelectionTemplate<Strategy>| marked up within |selection_text|.
-  SelectionTemplate<Strategy> SetSelectionText(
-      HTMLElement* element,
-      const std::string& selection_text) {
+  // |SelectionInDOMTree| marked up within |selection_text|.
+  SelectionInDOMTree SetSelectionText(HTMLElement* element,
+                                      const std::string& selection_text) {
     element->SetInnerHTMLFromString(String::FromUTF8(selection_text.c_str()));
     ConvertTemplatesToShadowRoots(*element);
     Traverse(element);
     if (anchor_node_ && focus_node_) {
-      return typename SelectionTemplate<Strategy>::Builder()
-          .Collapse(PositionTemplate<Strategy>(anchor_node_, anchor_offset_))
-          .Extend(PositionTemplate<Strategy>(focus_node_, focus_offset_))
+      return typename SelectionInDOMTree::Builder()
+          .Collapse(Position(anchor_node_, anchor_offset_))
+          .Extend(Position(focus_node_, focus_offset_))
           .Build();
     }
     DCHECK(focus_node_) << "Need just '|', or '^' and '|'";
-    return typename SelectionTemplate<Strategy>::Builder()
-        .Collapse(PositionTemplate<Strategy>(focus_node_, focus_offset_))
+    return typename SelectionInDOMTree::Builder()
+        .Collapse(Position(focus_node_, focus_offset_))
         .Build();
   }
 
@@ -354,7 +352,7 @@ SelectionInDOMTree SelectionSample::SetSelectionText(
     HTMLElement* element,
     const std::string& selection_text) {
   SelectionInDOMTree selection =
-      Parser<EditingStrategy>().SetSelectionText(element, selection_text);
+      Parser().SetSelectionText(element, selection_text);
   DCHECK(!selection.IsNone()) << "|selection_text| should container caret "
                                  "marker '|' or selection marker '^' and "
                                  "'|'.";
