d28847e42d483894ebcc57ae1ccad1cf586d24a7
Lei Zhang <thestig@chromium.org>
2017-10-04 05:45:41 +0000

    Make headless mode understand /path/to/file.
    BUG=767314
    
    Change-Id: If738ae440ae0adb1c266a21a09f2cba32d91724e
    Reviewed-on: https://chromium-review.googlesource.com/696492
    Reviewed-by: David Vallet <dvallet@chromium.org>
    Commit-Queue: David Vallet <dvallet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506315}
    Former-commit-id: a2b4bf980cff7aa2792c843026c37d22cfa79af8

--- headless/app/headless_shell.h
+++ headless/app/headless_shell.h
@@ -7,6 +7,7 @@
 
 #include <memory>
 #include <string>
+#include <vector>
 
 #include "base/files/file_proxy.h"
 #include "base/memory/weak_ptr.h"
@@ -21,6 +22,8 @@
 #include "headless/public/headless_web_contents.h"
 #include "headless/public/util/deterministic_dispatcher.h"
 #include "net/base/file_stream.h"
+
+class GURL;
 
 namespace headless {
 
@@ -59,6 +62,8 @@ class HeadlessShell : public HeadlessWebContents::Observer,
   virtual void Shutdown();
 
   void FetchTimeout();
+
+  void OnGotURLs(const std::vector<GURL>& urls);
 
   void PollReadyState();
 
