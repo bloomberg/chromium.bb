fa3bdf25f28fa8327bae3e568d2d6bbd74b557f1
Theresa Wellington <twellington@google.com>
2017-10-04 20:19:39 +0000

    [Home] More accessibility polish
     * Update content description for expand button
     * Update promo dialog text when accessibility is enabled
    
    BUG=767738
    
    Change-Id: I0d33304803855e6fe894f26a83d10d9c3c361244
    Reviewed-on: https://chromium-review.googlesource.com/699916
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506498}
    Former-commit-id: ad7eeea3dc36a32ff5b40619a0c355873c235006

--- chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/ChromeHomePromoDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/ChromeHomePromoDialog.java
@@ -15,6 +15,7 @@ import org.chromium.base.ApplicationStatus;
 import org.chromium.base.SysUtils;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.multiwindow.MultiWindowUtils;
+import org.chromium.chrome.browser.util.AccessibilityUtil;
 import org.chromium.chrome.browser.util.FeatureUtilities;
 import org.chromium.chrome.browser.widget.PromoDialog;
 
@@ -40,7 +41,9 @@ public class ChromeHomePromoDialog extends PromoDialog {
     protected DialogParams getDialogParams() {
         PromoDialog.DialogParams params = new PromoDialog.DialogParams();
         params.headerStringResource = R.string.chrome_home_promo_dialog_title;
-        params.subheaderStringResource = R.string.chrome_home_promo_dialog_message;
+        params.subheaderStringResource = AccessibilityUtil.isAccessibilityEnabled()
+                ? R.string.chrome_home_promo_dialog_message_accessibility
+                : R.string.chrome_home_promo_dialog_message;
         params.primaryButtonStringResource = R.string.ok;
         if (SysUtils.isLowEndDevice()) {
             params.drawableResource = R.drawable.chrome_home_promo_static;
