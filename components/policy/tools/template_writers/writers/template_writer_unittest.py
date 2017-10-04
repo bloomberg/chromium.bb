ac6f88f9c19cfb277dedde373abf2e9381630615
Owen Min <zmin@chromium.org>
2017-10-04 17:16:46 +0000

    TemplateWriter: checks supported version on each platform
    TemplateWriter.IsPolicySupported and
    TemplateWriter.IsPolicySupportedOnPlatform are now checking whether the
    currect version is supported or not. It will affect the generation of
    html documentation, adm/admx file, etc.
    
    TEST: components/policy/tools/template_writers/test_suite_all.py
    Bug: 770874
    
    
    Change-Id: I6fcecfaae5c87197b7829bb102afc407d2120779
    Reviewed-on: https://chromium-review.googlesource.com/687870
    Commit-Queue: Owen Min <zmin@chromium.org>
    Reviewed-by: Lutz Justen <ljusten@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506423}
    Former-commit-id: adab685e64d889724b76e4c3660992d8647cab83

--- components/policy/tools/template_writers/writers/template_writer_unittest.py
+++ components/policy/tools/template_writers/writers/template_writer_unittest.py
@@ -68,6 +68,12 @@ IGNORE_GROUPS_SORTED_POLICY_DEFS = [
 
 class TemplateWriterUnittests(unittest.TestCase):
   '''Unit tests for templater_writer.py.'''
+  def _IsPolicySupported(self, platform, version, policy):
+    tw = template_writer.TemplateWriter([platform], {'major_version': version})
+    if platform != '*':
+        self.assertEqual(tw.IsPolicySupported(policy),
+                         tw.IsPolicySupportedOnPlatform(policy, platform))
+    return tw.IsPolicySupported(policy)
 
   def testSortingGroupsFirst(self):
     tw = template_writer.TemplateWriter(None, None)
@@ -79,6 +85,36 @@ class TemplateWriterUnittests(unittest.TestCase):
     sorted_list = tw.FlattenGroupsAndSortPolicies(POLICY_DEFS)
     self.assertEqual(sorted_list, IGNORE_GROUPS_SORTED_POLICY_DEFS)
 
+  def testPoliciesIsNotSupported(self):
+    tw = template_writer.TemplateWriter(None, None)
+    self.assertFalse(tw.IsPolicySupported({'future': True}))
+    self.assertFalse(tw.IsPolicySupported({'deprecated': True}))
+
+  def testPoliciesIsSupportedOnCertainVersion(self):
+    platform = 'win'
+    policy = {'supported_on' : [{'platforms' : ['win'], 'since_version': '11',
+                                 'until_version': '12'}]}
+    self.assertFalse(self._IsPolicySupported(platform, 10, policy))
+    self.assertTrue(self._IsPolicySupported(platform, 11, policy))
+    self.assertTrue(self._IsPolicySupported(platform, 12, policy))
+    self.assertFalse(self._IsPolicySupported(platform, 13, policy))
+
+    policy = {'supported_on' : [{'platforms' : ['win'], 'since_version': '11',
+                                 'until_version': ''}]}
+    self.assertFalse(self._IsPolicySupported(platform, 10, policy))
+    self.assertTrue(self._IsPolicySupported(platform, 11, policy))
+    self.assertTrue(self._IsPolicySupported(platform, 12, policy))
+    self.assertTrue(self._IsPolicySupported(platform, 13, policy))
+
+  def testPoliciesIsSupportedOnMulitplePlatform(self):
+    policy = {'supported_on' : [{'platforms' : ['win'], 'since_version': '12',
+                                 'until_version': ''},
+                                {'platforms' : ['mac'], 'since_version': '11',
+                                 'until_version': ''}]}
+    self.assertFalse(self._IsPolicySupported('win', 11, policy))
+    self.assertTrue(self._IsPolicySupported('mac', 11, policy))
+    self.assertTrue(self._IsPolicySupported('*', 11, policy))
+    self.assertFalse(self._IsPolicySupported('*', 10, policy))
 
 if __name__ == '__main__':
   unittest.main()
