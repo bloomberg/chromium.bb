3f8af572888d266ef20a4445e5916afbd412b92c
Tom Anderson <thomasanderson@chromium.org>
2017-10-04 19:26:36 +0000

    Linux packager: Various manpage fixes
    This CL:
    * Fixes some manpage syntax.
    * Compresses the manpages.
    * Adds manpage entries for google-chrome-stable and google-chrome for
      the stable channel.
    * Fixes a reference to google-chrome in the RPM spec.
    * Handles rpmlint suppressions using a conf file instead of just
      filtering out what we don't want.
    
    This fixes the following lint errors:
    * lintian:
      * E: chromium-browser-stable: manpage-not-compressed
        usr/share/man/man1/chromium-browser.1
      * W: chromium-browser-stable: manpage-has-errors-from-man
        usr/share/man/man1/chromium-browser.1 1: warning: macro `"' not
        defined
    * rpmlint:
      * chromium-browser-stable.x86_64: W: manpage-not-compressed bz2
        /usr/share/man/man1/chromium-browser.1
      * chromium-browser-stable.x86_64: W: manual-page-warning
        /usr/share/man/man1/chromium-browser.1 1: warning: macro `"' not
        defined
      * chromium-browser-stable.x86_64: W: no-manual-page-for-binary
        google-chrome
      * chromium-browser-stable.x86_64: W: no-manual-page-for-binary
        chromium-browser-stable
    
    BUG=771373
    R=thestig@chromium.org
    
    Change-Id: I5d1921150945123b72b5f2a50714c3094a526648
    Reviewed-on: https://chromium-review.googlesource.com/699855
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506477}
    Former-commit-id: 7f1ac3735dace9ee652ef51a68102924500e0408

--- chrome/installer/linux/rpm/lint_package.py
+++ chrome/installer/linux/rpm/lint_package.py
@@ -10,36 +10,8 @@ rpmlint.
 import argparse
 import os
 import subprocess
-import sys
 
-
-SUPPRESSIONS = [
-    # Summary message that we would like to filter out.
-    'checked;',
-    # Google Chrome is not software available on a distro by default,
-    # so installing to /opt is correct behavior.
-    'dir-or-file-in-opt',
-    # Development builds won't be signed.
-    'no-signature',
-    # The setuid sandbox has permissions 4755.
-    'non-standard-executable-perm',
-    # The setuid sandbox is a setuid binary.
-    'setuid-binary',
-    # Some nacl binaries are statically linked but don't have "static"
-    # in their name.
-    'statically-linked-binary'
-]
-
-
-def line_is_suppressed(line):
-  global SUPPRESSIONS
-  if line == '':
-    return True
-  for suppression in SUPPRESSIONS:
-    if suppression in line:
-      return True
-  return False
-
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 parser = argparse.ArgumentParser()
 parser.add_argument('package', help='path/to/package.rpm')
@@ -48,12 +20,10 @@ package = os.path.abspath(args.package)
 
 cmd = [
     'rpmlint',
+    '-f',
+    os.path.join(SCRIPT_DIR, 'rpmlint.conf'),
+    '-o',
+    'CompressExtension \'gz\'',
     str(package),
 ]
-rpmlint = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-stdout, _ = rpmlint.communicate()
-filtered = '\n'.join(
-    [line for line in stdout.split('\n') if not line_is_suppressed(line)])
-if filtered != '':
-  print filtered
-sys.exit(filtered != '')
+subprocess.check_call(cmd)
