58c8b2ba73fc52a638e7e170cd26a8f13aacfc1a
Lukasz Anforowicz <lukasza@chromium.org>
2017-10-04 15:05:04 +0000

    Removing callers of the WebContents::GetRenderProcessHost() method.
    WebContents::GetRenderProcessHost is an API that doesn't behave as
    expected with out-of-process iframes, as multiple processes can be
    associated with a single WebContents.  This CL replaces callers of this
    API, so that they go through an equivalent API that explicitly selects
    which frame's process is needed.
    
    This CL was started by using an ad-hoc clang-plugin that replaced
    existing callsites of WebContents::GetRenderProcessHost() with a call to
    something like wc->GetMainFrame()->GetProcess().  This was followed-up
    by manually adding an include of render_frame_host.h and git cl format
    and git cl lint and then a self-review with small tweaks (e.g. sometimes
    using a wc->GetRenderViewHost()->GetProcess() is more appropriate).
    
    This CL was uploaded by git cl split.
    
    R=samarth@chromium.org
    
    Bug: 666525
    Change-Id: Id663ba6297c2513f71343452a1b2f56a1528c17d
    Reviewed-on: https://chromium-review.googlesource.com/689140
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506389}
    Former-commit-id: 7d7fc4b795414b438ee7f22515e4c0683918270f

--- chrome/browser/ui/browser_instant_controller.cc
+++ chrome/browser/ui/browser_instant_controller.cc
@@ -17,6 +17,7 @@
 #include "chrome/common/url_constants.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_controller.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_user_data.h"
@@ -104,7 +105,7 @@ void BrowserInstantController::OnSearchEngineBaseURLChanged(
       continue;
 
     // Send new search URLs to the renderer.
-    content::RenderProcessHost* rph = contents->GetRenderProcessHost();
+    content::RenderProcessHost* rph = contents->GetMainFrame()->GetProcess();
     instant_service->SendSearchURLsToRenderer(rph);
 
     if (!instant_service->IsInstantProcess(rph->GetID()))
