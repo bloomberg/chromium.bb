30b81bc7e112ae8438008d8f68a98e6e64821283
Robert Phillips <robertphillips@google.com>
2017-10-04 15:12:41 +0000

    Readd SK_DISABLE_DEFERRED_PROXIES flag
    This guard is required by the upcoming Skia CL https://skia-review.googlesource.com/c/skia/+/55100 (Use SK_DISABLE_DEFERRED_PROXIES to only disable deferred proxies for Chrome)
    
    Change-Id: Ib5972340b8db490af913d5d3bf0889fd364e90cc
    Reviewed-on: https://chromium-review.googlesource.com/700475
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Leon Scroggins <scroggo@chromium.org>
    Commit-Queue: Robert Phillips <robertphillips@google.com>
    Cr-Commit-Position: refs/heads/master@{#506391}
    Former-commit-id: a43b5a614a12858e7bc52359ec7e114379a662cd

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -204,6 +204,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_USE_LEGACY_DISTANCE_FIELDS
 #endif
 
+#ifndef SK_DISABLE_DEFERRED_PROXIES
+#define SK_DISABLE_DEFERRED_PROXIES
+#endif
+
 #ifndef SK_SUPPORT_LEGACY_DELTA_AA
 #define SK_SUPPORT_LEGACY_DELTA_AA
 #endif
