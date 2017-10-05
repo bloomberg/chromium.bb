ae5e76f1d3c58dd2dbb84c0e9beaf0be71254b04
Alex Clarke <alexclarke@chromium.org>
2017-10-05 18:11:54 +0000

    Page.frameScheduledNavigation to report form submission method
    This will allow devtools protocol users to distinguish between forms that
    use GET and POST.
    
    Bug: 
    Change-Id: Id27f36cb675aaea02434a76eae492faac320f01e
    Reviewed-on: https://chromium-review.googlesource.com/700518
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506788}
    Former-commit-id: 8ff74450b0041c638a7014e15c6ff3c8863d802c

--- third_party/WebKit/Source/core/inspector/InspectorPageAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorPageAgent.cpp
@@ -104,8 +104,10 @@ String ScheduledNavigationReasonToProtocol(ScheduledNavigation::Reason reason) {
   using ReasonEnum =
       protocol::Page::FrameScheduledNavigationNotification::ReasonEnum;
   switch (reason) {
-    case ScheduledNavigation::Reason::kFormSubmission:
-      return ReasonEnum::FormSubmission;
+    case ScheduledNavigation::Reason::kFormSubmissionGet:
+      return ReasonEnum::FormSubmissionGet;
+    case ScheduledNavigation::Reason::kFormSubmissionPost:
+      return ReasonEnum::FormSubmissionPost;
     case ScheduledNavigation::Reason::kHttpHeaderRefresh:
       return ReasonEnum::HttpHeaderRefresh;
     case ScheduledNavigation::Reason::kFrameNavigation:
