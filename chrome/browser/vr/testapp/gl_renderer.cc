ce7280dd9af6b95100bd875c7832127f423daaa0
Ian Vollick <vollick@chromium.org>
2017-10-05 16:05:54 +0000

    [vr] Throttle framerate in vr test app
    With this change, we pump frames at 60Hz.
    
    Bug: None
    Change-Id: I9aa814c1ff8d3ae88a85723a4193a2b221493d96
    Reviewed-on: https://chromium-review.googlesource.com/702043
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506744}
    Former-commit-id: 78b8c8d08275ea3ccb0635b4bab265a468ccba16

--- chrome/browser/vr/testapp/gl_renderer.cc
+++ chrome/browser/vr/testapp/gl_renderer.cc
@@ -5,6 +5,7 @@
 #include "chrome/browser/vr/testapp/gl_renderer.h"
 
 #include "base/threading/thread_task_runner_handle.h"
+#include "base/time/time.h"
 #include "chrome/browser/vr/testapp/vr_test_context.h"
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_context.h"
@@ -48,9 +49,10 @@ void GlRenderer::RenderFrame() {
 }
 
 void GlRenderer::PostRenderFrameTask(gfx::SwapResult result) {
-  base::ThreadTaskRunnerHandle::Get()->PostTask(
+  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
       FROM_HERE,
-      base::Bind(&GlRenderer::RenderFrame, weak_ptr_factory_.GetWeakPtr()));
+      base::Bind(&GlRenderer::RenderFrame, weak_ptr_factory_.GetWeakPtr()),
+      base::TimeDelta::FromSecondsD(1.0 / 60));
 }
 
 }  // namespace vr
