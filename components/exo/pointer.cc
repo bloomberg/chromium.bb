650a02943f1a767906e48be275975b231df73dcf
Sean O'Brien <seobrien@google.com>
2017-10-05 19:44:58 +0000

    Add and implement wayland protocol for pinch gestures
    Extends the wl_pointer protocol using the wayland upstream
    zwp_pointer_gestures protocol to pass information about pinch gestures.
    
    BUG=b:25668308
    
    Change-Id: I9f90ebebec3f8476181e0426401827a3cf1064e4
    Reviewed-on: https://chromium-review.googlesource.com/699017
    Commit-Queue: Sean O'Brien <seobrien@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506828}
    Former-commit-id: 1bb3d17c2fc66ea3223b839c7bd5171dbbcd4fd0

--- components/exo/pointer.cc
+++ components/exo/pointer.cc
@@ -8,7 +8,7 @@
 
 #include "ash/public/cpp/shell_window_ids.h"
 #include "components/exo/pointer_delegate.h"
-#include "components/exo/pointer_stylus_delegate.h"
+#include "components/exo/pointer_gesture_pinch_delegate.h"
 #include "components/exo/surface.h"
 #include "components/exo/wm_helper.h"
 #include "components/viz/common/frame_sinks/copy_output_request.h"
@@ -96,6 +96,8 @@ Pointer::~Pointer() {
     focus_surface_->RemoveSurfaceObserver(this);
     focus_surface_->UnregisterCursorProvider(this);
   }
+  if (pinch_delegate_)
+    pinch_delegate_->OnPointerDestroying(this);
   auto* helper = WMHelper::GetInstance();
   helper->RemoveDisplayConfigurationObserver(this);
   helper->RemoveCursorObserver(this);
@@ -149,6 +151,10 @@ void Pointer::SetCursor(Surface* surface, const gfx::Point& hotspot) {
 
 gfx::NativeCursor Pointer::GetCursor() {
   return cursor_;
+}
+
+void Pointer::SetGesturePinchDelegate(PointerGesturePinchDelegate* delegate) {
+  pinch_delegate_ = delegate;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -291,6 +297,36 @@ void Pointer::OnMouseEvent(ui::MouseEvent* event) {
 
 void Pointer::OnScrollEvent(ui::ScrollEvent* event) {
   OnMouseEvent(event);
+}
+
+void Pointer::OnGestureEvent(ui::GestureEvent* event) {
+  // We don't want to handle gestures generated from touchscreen events,
+  // we handle touch events in touch.cc
+  if (event->details().device_type() != ui::GestureDeviceType::DEVICE_TOUCHPAD)
+    return;
+
+  if (!focus_surface_ || !pinch_delegate_)
+    return;
+
+  switch (event->type()) {
+    case ui::ET_GESTURE_PINCH_BEGIN:
+      pinch_delegate_->OnPointerPinchBegin(event->unique_touch_event_id(),
+                                           event->time_stamp(), focus_surface_);
+      delegate_->OnPointerFrame();
+      break;
+    case ui::ET_GESTURE_PINCH_UPDATE:
+      pinch_delegate_->OnPointerPinchUpdate(event->time_stamp(),
+                                            event->details().scale());
+      delegate_->OnPointerFrame();
+      break;
+    case ui::ET_GESTURE_PINCH_END:
+      pinch_delegate_->OnPointerPinchEnd(event->unique_touch_event_id(),
+                                         event->time_stamp());
+      delegate_->OnPointerFrame();
+      break;
+    default:
+      break;
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
--- components/exo/wayland/server.cc
+++ components/exo/wayland/server.cc
@@ -12,6 +12,7 @@
 #include <keyboard-configuration-unstable-v1-server-protocol.h>
 #include <keyboard-extension-unstable-v1-server-protocol.h>
 #include <linux/input.h>
+#include <pointer-gestures-unstable-v1-server-protocol.h>
 #include <presentation-time-server-protocol.h>
 #include <remote-shell-unstable-v1-server-protocol.h>
 #include <secure-output-unstable-v1-server-protocol.h>
@@ -71,6 +72,7 @@
 #include "components/exo/notification_surface_manager.h"
 #include "components/exo/pointer.h"
 #include "components/exo/pointer_delegate.h"
+#include "components/exo/pointer_gesture_pinch_delegate.h"
 #include "components/exo/shared_memory.h"
 #include "components/exo/shell_surface.h"
 #include "components/exo/sub_surface.h"
@@ -4094,6 +4096,95 @@ void bind_stylus_v2(wl_client* client,
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+// pointer_gesture_swipe_v1 interface:
+
+void pointer_gestures_get_swipe_gesture(wl_client* client,
+                                        wl_resource* resource,
+                                        uint32_t id,
+                                        wl_resource* pointer_resource) {
+  NOTIMPLEMENTED();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// pointer_gesture_pinch_v1 interface:
+
+class WaylandPointerGesturePinchDelegate : public PointerGesturePinchDelegate {
+ public:
+  WaylandPointerGesturePinchDelegate(wl_resource* resource, Pointer* pointer)
+      : resource_(resource), pointer_(pointer) {
+    pointer_->SetGesturePinchDelegate(this);
+  }
+
+  ~WaylandPointerGesturePinchDelegate() override {
+    if (pointer_)
+      pointer_->SetGesturePinchDelegate(nullptr);
+  }
+  void OnPointerDestroying(Pointer* pointer) override { pointer_ = nullptr; }
+  void OnPointerPinchBegin(uint32_t unique_touch_event_id,
+                           base::TimeTicks time_stamp,
+                           Surface* surface) override {
+    wl_resource* surface_resource = GetSurfaceResource(surface);
+    DCHECK(surface_resource);
+    zwp_pointer_gesture_pinch_v1_send_begin(resource_, unique_touch_event_id,
+                                            TimeTicksToMilliseconds(time_stamp),
+                                            surface_resource, 2);
+  }
+  void OnPointerPinchUpdate(base::TimeTicks time_stamp, float scale) override {
+    zwp_pointer_gesture_pinch_v1_send_update(
+        resource_, TimeTicksToMilliseconds(time_stamp), 0, 0,
+        wl_fixed_from_double(scale), 0);
+  }
+  void OnPointerPinchEnd(uint32_t unique_touch_event_id,
+                         base::TimeTicks time_stamp) override {
+    zwp_pointer_gesture_pinch_v1_send_end(resource_, unique_touch_event_id,
+                                          TimeTicksToMilliseconds(time_stamp),
+                                          0);
+  }
+
+ private:
+  wl_resource* const resource_;
+  Pointer* pointer_;
+
+  DISALLOW_COPY_AND_ASSIGN(WaylandPointerGesturePinchDelegate);
+};
+
+void pointer_gesture_pinch_destroy(wl_client* client, wl_resource* resource) {
+  wl_resource_destroy(resource);
+}
+
+const struct zwp_pointer_gesture_pinch_v1_interface
+    pointer_gesture_pinch_implementation = {pointer_gesture_pinch_destroy};
+
+void pointer_gestures_get_pinch_gesture(wl_client* client,
+                                        wl_resource* resource,
+                                        uint32_t id,
+                                        wl_resource* pointer_resource) {
+  Pointer* pointer = GetUserDataAs<Pointer>(pointer_resource);
+  wl_resource* pointer_gesture_pinch_resource = wl_resource_create(
+      client, &zwp_pointer_gesture_pinch_v1_interface, 1, id);
+  SetImplementation(pointer_gesture_pinch_resource,
+                    &pointer_gesture_pinch_implementation,
+                    base::MakeUnique<WaylandPointerGesturePinchDelegate>(
+                        pointer_gesture_pinch_resource, pointer));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// pointer_gestures_v1 interface:
+
+const struct zwp_pointer_gestures_v1_interface pointer_gestures_implementation =
+    {pointer_gestures_get_swipe_gesture, pointer_gestures_get_pinch_gesture};
+
+void bind_pointer_gestures(wl_client* client,
+                           void* data,
+                           uint32_t version,
+                           uint32_t id) {
+  wl_resource* resource = wl_resource_create(
+      client, &zwp_pointer_gestures_v1_interface, version, id);
+  wl_resource_set_implementation(resource, &pointer_gestures_implementation,
+                                 data, nullptr);
+}
+
+////////////////////////////////////////////////////////////////////////////////
 // pointer_stylus interface (deprecated)
 // TODO(denniskempin): Remove once client no longer depends on this interface.
 
@@ -4440,6 +4531,8 @@ Server::Server(Display* display)
                    bind_stylus_v1_DEPRECATED);
   wl_global_create(wl_display_.get(), &zcr_stylus_v2_interface, 1, display_,
                    bind_stylus_v2);
+  wl_global_create(wl_display_.get(), &zwp_pointer_gestures_v1_interface, 1,
+                   display_, bind_pointer_gestures);
   wl_global_create(wl_display_.get(), &zcr_keyboard_configuration_v1_interface,
                    2, display_, bind_keyboard_configuration);
   wl_global_create(wl_display_.get(), &zcr_stylus_tools_v1_interface, 1,
