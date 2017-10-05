10f60ed3815a1a3a811c1ed20ffe07545c2c874b
Siddhartha <ssid@chromium.org>
2017-10-05 07:56:29 +0000

    Whitelist tracing metadata events for heap profiling
    Bug: 700245
    Change-Id: Id316cb680cecc5755ab52026ccff790e12d1d133
    Reviewed-on: https://chromium-review.googlesource.com/702019
    Reviewed-by: oysteine <oysteine@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506675}
    Former-commit-id: af517105b92b50274ce33f85933bfbd430f93b40

--- chrome/common/trace_event_args_whitelist.cc
+++ chrome/common/trace_event_args_whitelist.cc
@@ -25,6 +25,8 @@ const WhitelistEntry kEventArgsWhitelist[] = {
     {"__metadata", "thread_name", nullptr},
     {"__metadata", "process_name", nullptr},
     {"__metadata", "process_uptime_seconds", nullptr},
+    {"__metadata", "stackFrames", nullptr},
+    {"__metadata", "typeNames", nullptr},
     {"ipc", "SyncChannel::Send", nullptr},
     {"toplevel", "*", nullptr},
     {"latencyInfo", "*", kInputLatencyAllowedArgs},
