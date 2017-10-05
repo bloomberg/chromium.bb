3a4b7a2be86358a6f4a707e11e9db2456c6ad3ea
Yoland Yan <yolandyan@chromium.org>
2017-10-05 00:06:46 +0000

    Convert MultiWindow Test to JUnit4
    For more on JUnit4 migration, please check
    src/testing/android/docs/junit4.md
    
    Bug: 640116
    Change-Id: Icbd7c78acda5897a983cfe8fcafb2ffc4ead94d8
    Reviewed-on: https://chromium-review.googlesource.com/701596
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Yoland Yan <yolandyan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506578}
    Former-commit-id: 38ca7a67c23de765abc3dad12d35afcd4e8e3087

--- chrome/android/javatests/src/org/chromium/chrome/browser/multiwindow/MultiWindowUtilsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/multiwindow/MultiWindowUtilsTest.java
@@ -10,15 +10,25 @@ import android.content.Intent;
 import android.os.Build;
 import android.support.test.filters.SmallTest;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import org.chromium.base.ActivityState;
 import org.chromium.base.ApplicationStatus;
+import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.ChromeTabbedActivity2;
 import org.chromium.chrome.browser.IntentHandler;
-import org.chromium.chrome.test.ChromeTabbedActivityTestBase;
+import org.chromium.chrome.test.ChromeActivityTestRule;
+import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
+import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.content.browser.test.util.Criteria;
 import org.chromium.content.browser.test.util.CriteriaHelper;
 
@@ -28,45 +38,54 @@ import java.util.concurrent.Callable;
 /**
  * Class for testing MultiWindowUtils.
  */
+@RunWith(ChromeJUnit4ClassRunner.class)
+@CommandLineFlags.Add({
+        ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE,
+        ChromeActivityTestRule.DISABLE_NETWORK_PREDICTION_FLAG,
+})
 @MinAndroidSdkLevel(Build.VERSION_CODES.N)
-public class MultiWindowUtilsTest extends  ChromeTabbedActivityTestBase {
+public class MultiWindowUtilsTest {
+    @Rule
+    public ChromeTabbedActivityTestRule mActivityTestRule = new ChromeTabbedActivityTestRule();
 
-    @Override
-    public void startMainActivity() throws InterruptedException {
-        startMainActivityOnBlankPage();
+    @Before
+    public void setUp() throws InterruptedException {
+        mActivityTestRule.startMainActivityOnBlankPage();
     }
 
     /**
      * Tests that ChromeTabbedActivity2 is used for intents when EXTRA_WINDOW_ID is set to 2.
      */
+    @Test
     @SmallTest
     @Feature("MultiWindow")
     public void testTabbedActivityForIntentWithExtraWindowId() {
-        ChromeTabbedActivity activity1 = getActivity();
+        ChromeTabbedActivity activity1 = mActivityTestRule.getActivity();
         createSecondChromeTabbedActivity(activity1);
 
         Intent intent = activity1.getIntent();
         intent.putExtra(IntentHandler.EXTRA_WINDOW_ID, 2);
 
-        assertEquals("ChromeTabbedActivity2 should be used when EXTRA_WINDOW_ID is set to 2.",
+        Assert.assertEquals(
+                "ChromeTabbedActivity2 should be used when EXTRA_WINDOW_ID is set to 2.",
                 ChromeTabbedActivity2.class,
-                MultiWindowUtils.getInstance().getTabbedActivityForIntent(
-                        intent, activity1));
+                MultiWindowUtils.getInstance().getTabbedActivityForIntent(intent, activity1));
     }
 
     /**
      * Tests that if two ChromeTabbedActivities are running the one that was resumed most recently
      * is used as the class name for new intents.
      */
+    @Test
     @SmallTest
     @Feature("MultiWindow")
     public void testTabbedActivityForIntentLastResumedActivity() {
-        ChromeTabbedActivity activity1 = getActivity();
+        ChromeTabbedActivity activity1 = mActivityTestRule.getActivity();
         final ChromeTabbedActivity2 activity2 = createSecondChromeTabbedActivity(activity1);
 
-        assertFalse("ChromeTabbedActivity should not be resumed",
+        Assert.assertFalse("ChromeTabbedActivity should not be resumed",
                 ApplicationStatus.getStateForActivity(activity1) == ActivityState.RESUMED);
-        assertTrue("ChromeTabbedActivity2 should be resumed",
+        Assert.assertTrue("ChromeTabbedActivity2 should be resumed",
                 ApplicationStatus.getStateForActivity(activity2) == ActivityState.RESUMED);
 
         // Open settings and wait for ChromeTabbedActivity2 to pause.
@@ -79,7 +98,8 @@ public class MultiWindowUtilsTest extends  ChromeTabbedActivityTestBase {
             }
         }));
 
-        assertEquals("The most recently resumed ChromeTabbedActivity should be used for intents.",
+        Assert.assertEquals(
+                "The most recently resumed ChromeTabbedActivity should be used for intents.",
                 ChromeTabbedActivity2.class,
                 MultiWindowUtils.getInstance().getTabbedActivityForIntent(
                         activity1.getIntent(), activity1));
@@ -88,16 +108,18 @@ public class MultiWindowUtilsTest extends  ChromeTabbedActivityTestBase {
     /**
      * Tests that if only ChromeTabbedActivity is running it is used as the class name for intents.
      */
+    @Test
     @SmallTest
     @Feature("MultiWindow")
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public void testTabbedActivityForIntentOnlyActivity1IsRunning() {
-        ChromeTabbedActivity activity1 = getActivity();
+        ChromeTabbedActivity activity1 = mActivityTestRule.getActivity();
         ChromeTabbedActivity2 activity2 = createSecondChromeTabbedActivity(activity1);
         activity2.finishAndRemoveTask();
 
-        assertEquals("ChromeTabbedActivity should be used for intents if ChromeTabbedActivity2 is "
-                + "not running.",
+        Assert.assertEquals(
+                "ChromeTabbedActivity should be used for intents if ChromeTabbedActivity2 is "
+                        + "not running.",
                 ChromeTabbedActivity.class,
                 MultiWindowUtils.getInstance().getTabbedActivityForIntent(
                         activity1.getIntent(), activity1));
@@ -106,16 +128,18 @@ public class MultiWindowUtilsTest extends  ChromeTabbedActivityTestBase {
     /**
      * Tests that if only ChromeTabbedActivity2 is running it is used as the class name for intents.
      */
+    @Test
     @SmallTest
     @Feature("MultiWindow")
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public void testTabbedActivityForIntentOnlyActivity2IsRunning() {
-        ChromeTabbedActivity activity1 = getActivity();
+        ChromeTabbedActivity activity1 = mActivityTestRule.getActivity();
         createSecondChromeTabbedActivity(activity1);
         activity1.finishAndRemoveTask();
 
-        assertEquals("ChromeTabbedActivity2 should be used for intents if ChromeTabbedActivity is "
-                + "not running.",
+        Assert.assertEquals(
+                "ChromeTabbedActivity2 should be used for intents if ChromeTabbedActivity is "
+                        + "not running.",
                 ChromeTabbedActivity2.class,
                 MultiWindowUtils.getInstance().getTabbedActivityForIntent(
                         activity1.getIntent(), activity1));
@@ -125,14 +149,16 @@ public class MultiWindowUtilsTest extends  ChromeTabbedActivityTestBase {
      * Tests that if no ChromeTabbedActivities are running ChromeTabbedActivity is used as the
      * default for intents.
      */
+    @Test
     @SmallTest
     @Feature("MultiWindow")
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public void testTabbedActivityForIntentNoActivitiesAlive() {
-        ChromeTabbedActivity activity1 = getActivity();
+        ChromeTabbedActivity activity1 = mActivityTestRule.getActivity();
         activity1.finishAndRemoveTask();
 
-        assertEquals("ChromeTabbedActivity should be used as the default for external intents.",
+        Assert.assertEquals(
+                "ChromeTabbedActivity should be used as the default for external intents.",
                 ChromeTabbedActivity.class,
                 MultiWindowUtils.getInstance().getTabbedActivityForIntent(
                         activity1.getIntent(), activity1));
@@ -141,17 +167,19 @@ public class MultiWindowUtilsTest extends  ChromeTabbedActivityTestBase {
     /**
      * Tests that MultiWindowUtils properly tracks whether ChromeTabbedActivity2 is running.
      */
+    @Test
     @SmallTest
     @Feature("MultiWindow")
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public void testTabbedActivity2TaskRunning() {
-        ChromeTabbedActivity activity2 = createSecondChromeTabbedActivity(getActivity());
-        assertTrue(MultiWindowUtils.getInstance().getTabbedActivity2TaskRunning());
+        ChromeTabbedActivity activity2 =
+                createSecondChromeTabbedActivity(mActivityTestRule.getActivity());
+        Assert.assertTrue(MultiWindowUtils.getInstance().getTabbedActivity2TaskRunning());
 
         activity2.finishAndRemoveTask();
         MultiWindowUtils.getInstance().getTabbedActivityForIntent(
-                getActivity().getIntent(), getActivity());
-        assertFalse(MultiWindowUtils.getInstance().getTabbedActivity2TaskRunning());
+                mActivityTestRule.getActivity().getIntent(), mActivityTestRule.getActivity());
+        Assert.assertFalse(MultiWindowUtils.getInstance().getTabbedActivity2TaskRunning());
     }
 
     public static ChromeTabbedActivity2 createSecondChromeTabbedActivity(Activity activity) {
@@ -165,7 +193,8 @@ public class MultiWindowUtilsTest extends  ChromeTabbedActivityTestBase {
         // for initializing things in MultiWindowUtils.java.
         Class<? extends Activity> secondActivityClass =
                 MultiWindowUtils.getInstance().getOpenInOtherWindowActivity(activity);
-        assertEquals("ChromeTabbedActivity2 should be used as the 'open in other window' activity.",
+        Assert.assertEquals(
+                "ChromeTabbedActivity2 should be used as the 'open in other window' activity.",
                 ChromeTabbedActivity2.class, secondActivityClass);
 
         // Create an intent and start the second ChromeTabbedActivity.
@@ -190,7 +219,7 @@ public class MultiWindowUtilsTest extends  ChromeTabbedActivityTestBase {
                 returnActivity = (ChromeTabbedActivity2) runningActivity;
             }
         }
-        assertTrue(returnActivity != null);
+        Assert.assertTrue(returnActivity != null);
         return returnActivity;
     }
 }
