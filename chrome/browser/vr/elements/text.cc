46629ddc169a766166bfd8fe469f21028f7a959e
Tibor Goldschwendt <tiborg@chromium.org>
2017-10-05 22:41:58 +0000

    [vr] Move TextTexture into its own file
    Change-Id: I0ac74f33ceb8f61593a16a957ee8b9c7c93f51fd
    Reviewed-on: https://chromium-review.googlesource.com/702775
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#506893}
    Former-commit-id: 6003385719f84349cac49dac3b87074e7e986047

--- chrome/browser/vr/elements/text.cc
+++ chrome/browser/vr/elements/text.cc
@@ -5,48 +5,9 @@
 #include "chrome/browser/vr/elements/text.h"
 
 #include "base/memory/ptr_util.h"
-#include "cc/paint/skia_paint_canvas.h"
-#include "chrome/browser/vr/elements/ui_texture.h"
-#include "ui/gfx/canvas.h"
-#include "ui/gfx/font_list.h"
-#include "ui/gfx/geometry/rect.h"
-#include "ui/gfx/geometry/vector2d.h"
-#include "ui/gfx/render_text.h"
+#include "chrome/browser/vr/elements/text_texture.h"
 
 namespace vr {
-
-class TextTexture : public UiTexture {
- public:
-  TextTexture(const base::string16& text, float font_height, float text_width)
-      : text_(text), font_height_(font_height), text_width_(text_width) {}
-  ~TextTexture() override {}
-
-  void SetColor(SkColor color) {
-    if (color_ == color)
-      return;
-    color_ = color;
-    set_dirty();
-  }
-
- private:
-  gfx::Size GetPreferredTextureSize(int width) const override {
-    return gfx::Size(width, width);
-  }
-
-  gfx::SizeF GetDrawnSize() const override { return size_; }
-
-  void Draw(SkCanvas* sk_canvas, const gfx::Size& texture_size) override;
-
-  gfx::SizeF size_;
-  base::string16 text_;
-  // These widths are in meters.
-  float font_height_;
-  float text_width_;
-
-  SkColor color_ = SK_ColorBLACK;
-
-  DISALLOW_COPY_AND_ASSIGN(TextTexture);
-};
 
 Text::Text(int maximum_width_pixels,
            float font_height_meters,
@@ -58,36 +19,12 @@ Text::Text(int maximum_width_pixels,
                                              text_width_meters)) {}
 Text::~Text() {}
 
-UiTexture* Text::GetTexture() const {
-  return texture_.get();
-}
-
 void Text::SetColor(SkColor color) {
   texture_->SetColor(color);
 }
 
-void TextTexture::Draw(SkCanvas* sk_canvas, const gfx::Size& texture_size) {
-  cc::SkiaPaintCanvas paint_canvas(sk_canvas);
-  gfx::Canvas gfx_canvas(&paint_canvas, 1.0f);
-  gfx::Canvas* canvas = &gfx_canvas;
-
-  gfx::FontList fonts;
-  float pixels_per_meter = texture_size.width() / text_width_;
-  int pixel_font_height = static_cast<int>(font_height_ * pixels_per_meter);
-  GetFontList(pixel_font_height, text_, &fonts);
-  gfx::Rect text_bounds(texture_size.width(), 0);
-
-  std::vector<std::unique_ptr<gfx::RenderText>> lines =
-      // TODO(vollick): if this subsumes all text, then we should probably move
-      // this function into this class.
-      PrepareDrawStringRect(text_, fonts, color_, &text_bounds,
-                            kTextAlignmentCenter, kWrappingBehaviorWrap);
-  // Draw the text.
-  for (auto& render_text : lines)
-    render_text->Draw(canvas);
-
-  // Note, there is no padding here whatsoever.
-  size_ = gfx::SizeF(text_bounds.size());
+UiTexture* Text::GetTexture() const {
+  return texture_.get();
 }
 
 }  // namespace vr
