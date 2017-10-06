4464101d866db2b2a6bbc2bdb36d448698ab3385
Erik Chen <erikchen@chromium.org>
2017-10-06 22:18:15 +0000

    Revert "Revert "Avoid checking overlay ResourcePool in non CA Layer""
    This reverts commit 69e5a22024e8795ae8cadd283e88f31bac87e512 [formerly bb57d0bd433908d79df0f0b39a5f12c7d44b2d14].
    
    Reason for revert: The original CL was correct and fixed a broken cache on macOS [which increases memory usage on a very specific test, which is WAI]. 
    
    Original change's description:
    > Revert "Avoid checking overlay ResourcePool in non CA Layer"
    > 
    > This reverts commit 3796fa0a35c567a388dccf4024678e128f9da64b [formerly afd24343fb79bb4212e26be973a51f6d9713e556].
    > 
    > Reason for revert: Caused a memory regression on Mac, for some reason: https://bugs.chromium.org/p/chromium/issues/detail?id=771766
    > 
    > Original change's description:
    > > Avoid checking overlay ResourcePool in non CA Layer
    > > 
    > > Overlay ResourcePool is only used for CA layer.
    > > BUG=None
    > > 
    > > Change-Id: Id37572e65d1b7a2c3307fa80be9820a94d693f7a
    > > Reviewed-on: https://chromium-review.googlesource.com/685947
    > > Reviewed-by: Antoine Labour <piman@chromium.org>
    > > Commit-Queue: Xing Xu <xing.xu@intel.com>
    > > Cr-Commit-Position: refs/heads/master@{#505913}
    > 
    > TBR=xing.xu@intel.com,piman@chromium.org,danakj@google.com
    > 
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    > 
    > Bug: None
    > Change-Id: I0543fa78a715438d9d27c9eb38f643008745679f
    > Reviewed-on: https://chromium-review.googlesource.com/703714
    > Reviewed-by: Antoine Labour <piman@chromium.org>
    > Commit-Queue: Antoine Labour <piman@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#506907}
    
    TBR=xing.xu@intel.com,piman@chromium.org,danakj@google.com
    
    Change-Id: Ib2c9e8d83e76df69805f46fe652178cd4da53ecb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/706294
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507193}
    Former-commit-id: 32ea531671b06e5005c6b8ffe33085f6a95a0e76

--- components/viz/service/display/gl_renderer.cc
+++ components/viz/service/display/gl_renderer.cc
@@ -3375,12 +3375,7 @@ void GLRenderer::ScheduleCALayers() {
 }
 
 void GLRenderer::ScheduleDCLayers() {
-  if (overlay_resource_pool_) {
-    overlay_resource_pool_->CheckBusyResources();
-  }
-
   scoped_refptr<DCLayerOverlaySharedState> shared_state;
-  size_t copied_render_pass_count = 0;
   for (DCLayerOverlay& dc_layer_overlay :
        current_frame()->dc_layer_overlay_list) {
     DCHECK(!dc_layer_overlay.rpdq);
@@ -3434,13 +3429,6 @@ void GLRenderer::ScheduleDCLayers() {
                                  dc_layer_overlay.background_color,
                                  dc_layer_overlay.edge_aa_mask, bounds_rect,
                                  filter);
-  }
-
-  // Take the number of copied render passes in this frame, and use 3 times that
-  // amount as the cache limit.
-  if (overlay_resource_pool_) {
-    overlay_resource_pool_->SetResourceUsageLimits(
-        std::numeric_limits<std::size_t>::max(), copied_render_pass_count * 5);
   }
 }
 
