c3e7ca6f19f2395fc4e224660315ce18b659fa83
Emircan Uysaler <emircan@chromium.org>
2017-10-06 23:54:31 +0000

    Fix loadedmetadata checks for MediaStreams in HTMLMediaElementCapture
    This CL handles the case where a MediaStream is assigned after loadedmetadata.
    We should clone the newly generated tracks to the returned media stream.
    
    Bug: 771117
    Change-Id: I7d7625133fcff6fc91c5473aafa4d7895c81c311
    Reviewed-on: https://chromium-review.googlesource.com/699747
    Commit-Queue: Emircan Uysaler <emircan@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507233}
    Former-commit-id: fe8edc0fafdd3c94b1f8a944a87421baa125783d

--- third_party/WebKit/Source/modules/mediacapturefromelement/HTMLMediaElementCapture.cpp
+++ third_party/WebKit/Source/modules/mediacapturefromelement/HTMLMediaElementCapture.cpp
@@ -59,7 +59,7 @@ void MediaElementEventListener::handleEvent(ExecutionContext* context,
   DCHECK(media_stream_);
 
   if (event->type() == EventTypeNames::ended) {
-    MediaStreamTrackVector tracks = media_stream_->getTracks();
+    const MediaStreamTrackVector tracks = media_stream_->getTracks();
     for (const auto& track : tracks) {
       track->stopTrack(ASSERT_NO_EXCEPTION);
       media_stream_->RemoveTrackByComponent(track->Component());
@@ -70,6 +70,27 @@ void MediaElementEventListener::handleEvent(ExecutionContext* context,
   }
   if (event->type() != EventTypeNames::loadedmetadata)
     return;
+
+  // If |media_element_| is a MediaStream, clone the new tracks.
+  if (media_element_->GetLoadType() == WebMediaPlayer::kLoadTypeMediaStream) {
+    const MediaStreamTrackVector tracks = media_stream_->getTracks();
+    for (const auto& track : tracks) {
+      track->stopTrack(ASSERT_NO_EXCEPTION);
+      media_stream_->RemoveTrackByComponent(track->Component());
+    }
+    MediaStreamDescriptor* const descriptor =
+        media_element_->currentSrc().IsEmpty()
+            ? media_element_->GetSrcObject()
+            : MediaStreamRegistry::Registry().LookupMediaStreamDescriptor(
+                  media_element_->currentSrc().GetString());
+    DCHECK(descriptor);
+    for (size_t i = 0; i < descriptor->NumberOfAudioComponents(); i++)
+      media_stream_->AddTrackByComponent(descriptor->AudioComponent(i));
+    for (size_t i = 0; i < descriptor->NumberOfVideoComponents(); i++)
+      media_stream_->AddTrackByComponent(descriptor->VideoComponent(i));
+    UpdateSources(context);
+    return;
+  }
 
   WebMediaStream web_stream;
   web_stream.Initialize(WebVector<WebMediaStreamTrack>(),
