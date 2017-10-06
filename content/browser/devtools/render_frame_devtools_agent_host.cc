8e292957c41587858649121fdca65399d19b677b
arthursonzogni <arthursonzogni@chromium.org>
2017-10-06 09:13:01 +0000

    Prevent RenderFrameDevtoolsAgentHost to hold a stale pointer.
    RenderFrameDevtoolsAgentHost keeps track of the RenderFrameHost a
    navigation is using after WebContentsObserver::ReadyToCommitNavigation().
    
    After WebContentsObserver::DidFinishNavigation(), there are no guarantee
    that it will continue to exist. This CL reset |frame_host_| to
    frame_tree_node_->current_frame_host() when a navigation is canceled.
    
    Bug: 742955
    Change-Id: I7615d6bef56680769a652587bf1471d191697237
    Reviewed-on: https://chromium-review.googlesource.com/695203
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507012}
    Former-commit-id: a63f35632f1076978fc85a33b5cdb25679645b29

--- content/browser/devtools/render_frame_devtools_agent_host.cc
+++ content/browser/devtools/render_frame_devtools_agent_host.cc
@@ -698,9 +698,9 @@ void RenderFrameDevToolsAgentHost::DidFinishNavigation(
 
   // UpdateFrameHost may destruct |this|.
   scoped_refptr<RenderFrameDevToolsAgentHost> protect(this);
-  if (handle->HasCommitted() && !handle->IsErrorPage())
-    UpdateFrameHost(handle->GetRenderFrameHost());
+  UpdateFrameHost(frame_tree_node_->current_frame_host());
   DCHECK(CheckConsistency());
+
   if (navigation_handles_.empty()) {
     for (auto& pair : suspended_messages_by_session_id_) {
       int session_id = pair.first;
