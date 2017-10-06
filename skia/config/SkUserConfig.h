822ff050d3e886651bba6babdbdbe50b035f10ec
Mike Reed <reed@google.com>
2017-10-06 18:50:11 +0000

    add guard for future Skia change
    See https://skia-review.googlesource.com/c/skia/+/56620
    
    Bug: Skia:7132
    Change-Id: I2665b854daef351e97e26170f4420a82ffb5ad19
    Reviewed-on: https://chromium-review.googlesource.com/705454
    Commit-Queue: Mike Reed <reed@chromium.org>
    Reviewed-by: Ben Wagner <bungeman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507131}
    Former-commit-id: 14d88726bcc9beedc31e56467bf595e3aa34027d

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -228,6 +228,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_COLOR_SPACE_XFORM_LEGACY_PIPELINE
 #endif
 
+#ifndef SK_SUPPORT_LEGACY_COMPUTEBYTESIZE_RET_0
+#define SK_SUPPORT_LEGACY_COMPUTEBYTESIZE_RET_0
+#endif
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
