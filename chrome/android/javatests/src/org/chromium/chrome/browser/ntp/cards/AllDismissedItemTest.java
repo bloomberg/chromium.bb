c2f51be163838f97b3b63ba61f75496b90eb0744
Ted Choc <tedchoc@google.com>
2017-10-06 20:52:00 +0000

    Restrict AllDismissedItemTest#testChromeHomeAppearance to Phones.
    ChromeHome is disabled for tablets, so ChromeHome#setPrefs will fail
    to enable ChromeHome as FeatureUtilities#isChromeHomeEnabled returns
    false always for tablets.
    
    TBR=peconn@chromium.org
    BUG=
    
    Change-Id: Ie96def69287da15f14a9640f5514ba0e4936fdbe
    Reviewed-on: https://chromium-review.googlesource.com/705929
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507173}
    Former-commit-id: 2216977453350064bfb0efb0a84f31f8daed29fe

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/cards/AllDismissedItemTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/cards/AllDismissedItemTest.java
@@ -16,6 +16,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.ntp.cards.AllDismissedItem.ViewHolder;
@@ -23,6 +24,7 @@ import org.chromium.chrome.test.ChromeActivityTestRule;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.util.RenderTestRule;
 import org.chromium.chrome.test.util.browser.ChromeHome;
+import org.chromium.ui.test.util.UiRestriction;
 
 import java.io.IOException;
 
@@ -75,6 +77,7 @@ public class AllDismissedItemTest {
     @MediumTest
     @Feature({"Cards", "RenderTest"})
     @ChromeHome
+    @Restriction(UiRestriction.RESTRICTION_TYPE_PHONE)
     public void testChromeHomeAppearance() throws IOException {
         renderAtHour(new ViewHolder(mContentView, null), 0, "modern");
     }
