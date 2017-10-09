9b32d6c3677af787b77e539fe53a267968f100a8
Henrik Grunell <grunell@chromium.org>
2017-10-09 15:06:42 +0000

    Revert "Don't recenter image document when URL bar hides"
    This reverts commit 06cffeb5a98b626902476f1d1819c6a70eb38195 [formerly a38acc8557b35c676bd5c936db0e1187bfa4ee57].
    
    Reason for revert: breaks Android bot: https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Android%20%28Nexus4%29?numbuilds=200
    http://crbug.com/772860
    
    Original change's description:
    > Don't recenter image document when URL bar hides
    > 
    > This patch changes the ImageDocument so that hiding the URL bar doesn't
    > attempt to vertically re-center an image that's wider than the viewport.
    > 
    > Bug: 718515
    > Change-Id: I0b45beb011c2c8dbc6f095a0456774f6f5e041ff
    > Reviewed-on: https://chromium-review.googlesource.com/700617
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Commit-Queue: David Bokan <bokan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#507079}
    
    TBR=bokan@chromium.org,dtrainor@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 718515, 772860
    Change-Id: I5d82ae26c29068597bf90b29dbddcd9a46ba153d
    Reviewed-on: https://chromium-review.googlesource.com/707075
    Reviewed-by: Henrik Grunell <grunell@chromium.org>
    Commit-Queue: Henrik Grunell <grunell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507376}
    Former-commit-id: 0400d0014243549fb4fd84d6d4af1db3fc8fb252

--- third_party/WebKit/Source/core/testing/sim/SimRequest.h
+++ third_party/WebKit/Source/core/testing/sim/SimRequest.h
@@ -5,7 +5,6 @@
 #ifndef SimRequest_h
 #define SimRequest_h
 
-#include "platform/wtf/Vector.h"
 #include "platform/wtf/text/WTFString.h"
 #include "public/platform/WebURLError.h"
 #include "public/platform/WebURLResponse.h"
@@ -30,14 +29,12 @@ class SimRequest final {
 
   // Write a chunk of the response body.
   void Write(const String& data);
-  void Write(const Vector<char>& data);
 
   // Finish the response, this is as if the server closed the connection.
   void Finish();
 
   // Shorthand to complete a request (start/write/finish) sequence in order.
   void Complete(const String& data = String());
-  void Complete(const Vector<char>& data);
 
   const String& Url() const { return url_; }
   const WebURLError& GetError() const { return error_; }
