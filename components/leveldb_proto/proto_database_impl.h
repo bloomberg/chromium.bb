8a8f2edb2533689ad78fc3ef4a7ef3a3754eb32b
Tommy Nyquist <nyquist@chromium.org>
2017-10-09 21:14:53 +0000

    Fix handling of not found entries when invoking LevelDB::Get
    The ProtoDatabaseImpl of //components/leveldb_proto uses the underlying
    DB to look for items. Whenever found is set to false, it still tries to
    parse the entries, which leads to unnecessary error-log messages for
    parsing.
    
    In addition, instead of checking the value of *success, it checks
    whether there is a pointer to success (which is already DCHECKed in the
    start of the method).
    
    This CL fixes the check of *success and also adds an early bail out if
    the entry is not found to ensure no parsing happens. |found| is also
    forcefully set to false if we failed the lookup (i.e. |success| was
    false).
    
    BUG=772125
    
    Change-Id: Ie23c46e0c0e9d48fbd2746348f19f694d82a0b68
    Reviewed-on: https://chromium-review.googlesource.com/703466
    Reviewed-by: Chris Mumford <cmumford@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507489}
    Former-commit-id: c84784ce85d0fada9677e00d95a25b76237b2028

--- components/leveldb_proto/proto_database_impl.h
+++ components/leveldb_proto/proto_database_impl.h
@@ -198,7 +198,15 @@ void GetEntryFromTaskRunner(LevelDB* database,
   std::string serialized_entry;
   *success = database->Get(key, found, &serialized_entry);
 
-  if (success && !entry->ParseFromString(serialized_entry)) {
+  if (!*success) {
+    *found = false;
+    return;
+  }
+
+  if (!*found)
+    return;
+
+  if (!entry->ParseFromString(serialized_entry)) {
     *found = false;
     DLOG(WARNING) << "Unable to parse leveldb_proto entry";
     // TODO(cjhopman): Decide what to do about un-parseable entries.
