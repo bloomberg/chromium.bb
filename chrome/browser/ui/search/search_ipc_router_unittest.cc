113288523cd6ff099a6b82073d7a66dc2660563f
Marc Treib <treib@chromium.org>
2017-10-09 08:41:19 +0000

    Instant IPC: Always run Mojo return value callbacks
    If we don't, Mojo gets unhappy and DCHECKs.
    Followup to crrev.com/c/695227, which introduced the Mojo return values.
    
    Bug: 772191
    Change-Id: I55daeff424217cfe622c5364ae960bea723f94f4
    Reviewed-on: https://chromium-review.googlesource.com/704734
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507339}
    Former-commit-id: 16c803a1c68bf3a1c46d7d524350feac4a743c91

--- chrome/browser/ui/search/search_ipc_router_unittest.cc
+++ chrome/browser/ui/search/search_ipc_router_unittest.cc
@@ -330,6 +330,7 @@ TEST_F(SearchIPCRouterTest, IgnoreChromeIdentityCheckMsg) {
       .WillOnce(Return(false));
 
   base::MockCallback<SearchIPCRouter::ChromeIdentityCheckCallback> callback;
+  EXPECT_CALL(callback, Run(false));
   GetSearchIPCRouter().ChromeIdentityCheck(GetSearchIPCRouterSeqNo(),
                                            test_identity, callback.Get());
 }
@@ -362,6 +363,7 @@ TEST_F(SearchIPCRouterTest, IgnoreHistorySyncCheckMsg) {
       .WillOnce(Return(false));
 
   base::MockCallback<SearchIPCRouter::ChromeIdentityCheckCallback> callback;
+  EXPECT_CALL(callback, Run(false));
   GetSearchIPCRouter().HistorySyncCheck(GetSearchIPCRouterSeqNo(),
                                         callback.Get());
 }
