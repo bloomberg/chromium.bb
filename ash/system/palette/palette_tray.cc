25d6f7978a20d98c1acab5af1a88a8fcfcd3dc52
MinChen <minch@chromium.org>
2017-10-09 23:57:46 +0000

    Add tooltips to stylus/ IME/ notifications button in shelf.
    Bug: 758386
    Change-Id: I300bd76fdd09d8bb6c61d235dc3b9abb525b53d9
    Reviewed-on: https://chromium-review.googlesource.com/707760
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Commit-Queue: min c <minch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507545}
    Former-commit-id: f492034f45d22a21f3108f4723949014219820ec

--- ash/system/palette/palette_tray.cc
+++ ash/system/palette/palette_tray.cc
@@ -181,6 +181,7 @@ PaletteTray::PaletteTray(Shelf* shelf)
   SetInkDropMode(InkDropMode::ON);
   SetLayoutManager(new views::FillLayout());
   icon_ = new views::ImageView();
+  icon_->SetTooltipText(l10n_util::GetStringUTF16(IDS_ASH_STYLUS_TOOLS_TITLE));
   UpdateTrayIcon();
 
   tray_container()->SetMargin(kTrayIconMainAxisInset, kTrayIconCrossAxisInset);
