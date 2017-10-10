287423ce29ea293e0517411f9c565d4d312ee503
Daniele Castagna <dcastagna@chromium.org>
2017-10-10 17:19:09 +0000

    ash: Add UMA stats to DisplayColorManager.
    Add three UMA stats to DisplayColorManager:
    - Whether a display has "CTM" property.
    - Whether we could parse a valid product ID.
    - If we could find an ICC file given a valid product ID.
    
    The goal of these UMA stats is to investigate the effectiveness
    of the ICC service ChromeOS currently use for color management.
    
    Bug: 771345
    Change-Id: Ib78f7c0c1661393dd5a93f99147a12aeeaa55281
    Reviewed-on: https://chromium-review.googlesource.com/706664
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Commit-Queue: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507702}
    Former-commit-id: c08fc3300a7dbc92472c16dcc2cd8983a3261e54

--- ash/display/display_color_manager_chromeos.cc
+++ ash/display/display_color_manager_chromeos.cc
@@ -11,6 +11,7 @@
 #include "base/bind.h"
 #include "base/files/file_util.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/sequenced_task_runner.h"
 #include "base/task_runner_util.h"
 #include "base/task_scheduler/post_task.h"
@@ -176,10 +177,16 @@ void DisplayColorManager::OnDisplayModeChanged(
         state->display_id(), std::vector<display::GammaRampRGBEntry>(),
         std::vector<display::GammaRampRGBEntry>(), std::vector<float>());
 
+    UMA_HISTOGRAM_BOOLEAN("Ash.DisplayColorManager.HasColorCorrectionMatrix",
+                          state->has_color_correction_matrix());
     if (calibration_map_[state->product_id()]) {
       ApplyDisplayColorCalibration(state->display_id(), state->product_id());
     } else {
-      if (state->product_id() != display::DisplaySnapshot::kInvalidProductID)
+      const bool valid_product_id =
+          state->product_id() != display::DisplaySnapshot::kInvalidProductID;
+      UMA_HISTOGRAM_BOOLEAN("Ash.DisplayColorManager.ValidProductId",
+                            valid_product_id);
+      if (valid_product_id)
         LoadCalibrationForDisplay(state);
     }
   }
@@ -226,6 +233,8 @@ void DisplayColorManager::FinishLoadCalibrationForDisplay(
     const base::FilePath& path,
     bool file_downloaded) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  UMA_HISTOGRAM_BOOLEAN("Ash.DisplayColorManager.IccFileFound",
+                        !path.empty() && file_downloaded);
   std::string product_string = quirks::IdToHexString(product_id);
   if (path.empty()) {
     VLOG(1) << "No ICC file found with product id: " << product_string
