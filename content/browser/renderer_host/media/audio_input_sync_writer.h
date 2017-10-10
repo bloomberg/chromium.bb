3ea346e14f3a1018db2ced96d98a5c3b615ff301
k2.nagaraju <k2.nagaraju@samsung.com>
2017-10-10 09:46:55 +0000

    Clean up storage of overflow data in AudioInputSyncWriter
    Bug: 768562
    Change-Id: Ied472734f32e09ff0aa15d54dcde3f74eb6b2b80
    Reviewed-on: https://chromium-review.googlesource.com/704495
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Commit-Queue: srirama chandra sekhar <srirama.m@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#507625}
    Former-commit-id: 46a011e1dade438944de918f63b26df678416a42

--- content/browser/renderer_host/media/audio_input_sync_writer.h
+++ content/browser/renderer_host/media/audio_input_sync_writer.h
@@ -164,13 +164,24 @@ class CONTENT_EXPORT AudioInputSyncWriter
   // It should ideally be rare, but we need to guarantee that the data arrives
   // since audio processing such as echo cancelling requires that to perform
   // properly.
-  std::vector<std::unique_ptr<media::AudioBus>> overflow_buses_;
-  struct OverflowParams {
-    double volume;
-    bool key_pressed;
-    base::TimeTicks capture_time;
+  struct OverflowData {
+    OverflowData(double volume,
+                 bool key_pressed,
+                 base::TimeTicks capture_time,
+                 std::unique_ptr<media::AudioBus> audio_bus);
+    ~OverflowData();
+    OverflowData(OverflowData&&);
+    OverflowData& operator=(OverflowData&& other);
+    double volume_;
+    bool key_pressed_;
+    base::TimeTicks capture_time_;
+    std::unique_ptr<media::AudioBus> audio_bus_;
+
+   private:
+    DISALLOW_COPY_AND_ASSIGN(OverflowData);
   };
-  base::circular_deque<OverflowParams> overflow_params_;
+
+  std::vector<OverflowData> overflow_data_;
 
   DISALLOW_IMPLICIT_CONSTRUCTORS(AudioInputSyncWriter);
 };
