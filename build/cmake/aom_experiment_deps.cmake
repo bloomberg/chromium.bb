93e9d1307edbed17ec7b76485f21730484d6968c
Zoe Liu <zoeliu@google.com>
2017-10-10 15:13:42 -0700

    Fix cmake setup for frame-sign-bias
    Change-Id: I9cab533c3395cbb5cfb9c0b5464a566258f2cbd9

--- build/cmake/aom_experiment_deps.cmake
+++ build/cmake/aom_experiment_deps.cmake
@@ -107,6 +107,12 @@ macro (fix_experiment_configs)
     endif ()
   endif ()
 
+  if (CONFIG_FRAME_SIGN_BIAS)
+    if (NOT CONFIG_FRAME_MARKER)
+      change_config_and_warn(CONFIG_FRAME_MARKER 1 CONFIG_FRAME_SIGN_BIAS)
+    endif ()
+  endif ()
+
   if (CONFIG_AMVR)
     change_config_and_warn(CONFIG_HASH_ME 1 CONFIG_AMVR)
   endif ()
