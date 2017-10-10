46e98777ba469092bc9dab44d75ca96b3c84e22e
Emily Stark <estark@google.com>
2017-10-10 00:10:39 +0000

    Add UMA histogram for connection CT compliance status
    This metric will help us gauge readiness for the CT requirement date. As the CT
    requirement date approaches, we would hope to see nearly 100% of TLS connections
    comply with the Chrome CT policy.
    
    Bug: 772534
    Change-Id: Iafbe5b9e443e37e925548deac3a2949d203a38ab
    Reviewed-on: https://chromium-review.googlesource.com/706377
    Commit-Queue: Emily Stark <estark@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Eran Messeri <eranm@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507549}
    Former-commit-id: c96e9bcbf95e79be3c9dd17d29fde20cb9aa8019

--- net/cert/ct_policy_status.h
+++ net/cert/ct_policy_status.h
@@ -10,19 +10,21 @@ namespace net {
 namespace ct {
 
 // Information about the connection's compliance with the CT
-// certificate policy.
+// certificate policy. This value is histogrammed, so do not re-order or change
+// values, and add new values at the end.
 enum class CertPolicyCompliance {
   // The connection complied with the certificate policy by
   // including SCTs that satisfy the policy.
   CERT_POLICY_COMPLIES_VIA_SCTS = 0,
   // The connection did not have enough SCTs to comply.
-  CERT_POLICY_NOT_ENOUGH_SCTS,
+  CERT_POLICY_NOT_ENOUGH_SCTS = 1,
   // The connection did not have diverse enough SCTs to comply.
-  CERT_POLICY_NOT_DIVERSE_SCTS,
+  CERT_POLICY_NOT_DIVERSE_SCTS = 2,
   // The connection cannot be considered compliant because the build
   // isn't timely and therefore log information might be out of date
   // (for example a log might no longer be considered trustworthy).
-  CERT_POLICY_BUILD_NOT_TIMELY,
+  CERT_POLICY_BUILD_NOT_TIMELY = 3,
+  CERT_POLICY_MAX
 };
 
 }  // namespace ct
