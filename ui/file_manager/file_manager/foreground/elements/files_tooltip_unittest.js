3d6fcdd1898f631dbd2eac396b56de11f1ff06d6
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2017-10-10 08:17:22 +0000

    Do not show tooltip when focus moved as a result of mouse operation.
    Focus is also moved a menu-button after executing or dismissing menu
    by mouse/touch. In such case we should not show the tooltip unlike when
    manually focusing it by keyboard.
    
    As a side effect, tooltip will not appear when focusing by keyboard
    when button focus is made invisible by Issue 748489. It will be resolved
    when Issue 748489 is fixed.
    
    Bug: 769593
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ieda9e391117103d91ddad584705173674b3470ae
    Reviewed-on: https://chromium-review.googlesource.com/700116
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507616}
    Former-commit-id: 10a97df6559061ad5b81e9be7b59dd616fb44fe0

--- ui/file_manager/file_manager/foreground/elements/files_tooltip_unittest.js
+++ ui/file_manager/file_manager/foreground/elements/files_tooltip_unittest.js
@@ -7,6 +7,8 @@ var cherriesButton;
 var otherButton;
 var tooltip;
 
+var previousFocus = null;
+
 function waitForMutation(target) {
   return new Promise(function(fulfill, reject) {
     var observer = new MutationObserver(function(mutations) {
@@ -15,6 +17,23 @@ function waitForMutation(target) {
     });
     observer.observe(target, {attributes: true});
   });
+}
+
+function moveFocus(target) {
+  if (previousFocus && previousFocus != target) {
+    // Emulate blur event which would be issued in actual usage.
+    // previousFocus.blur();
+    // does not work here because it's not actually focused in this test.
+    var blurEvent = new UIEvent('blur', {target: previousFocus});
+    previousFocus.dispatchEvent(blurEvent);
+  }
+  previousFocus = target;
+  // Emulate focus event which would be issued in actual usage.
+  // target.focus();
+  // does not work here because it doesn't fill sourceCapabilities in Event.
+  var focusEvent =
+      new UIEvent('focus', {sourceCapabilities: new InputDeviceCapabilities()});
+  target.dispatchEvent(focusEvent);
 }
 
 function setUp() {
@@ -26,7 +45,7 @@ function setUp() {
 }
 
 function testFocus(callback) {
-  chocolateButton.focus();
+  moveFocus(chocolateButton);
 
   return reportPromise(
     waitForMutation(tooltip).then(function() {
@@ -35,7 +54,7 @@ function testFocus(callback) {
       assertEquals('4px', tooltip.style.left);
       assertEquals('70px', tooltip.style.top);
 
-      cherriesButton.focus();
+      moveFocus(cherriesButton);
       return waitForMutation(tooltip);
     }).then(function() {
       assertEquals('Cherries!', tooltip.textContent.trim());
@@ -44,7 +63,7 @@ function testFocus(callback) {
       assertEquals(expectedLeft, tooltip.style.left);
       assertEquals('70px', tooltip.style.top);
 
-      otherButton.focus();
+      moveFocus(otherButton);
       return waitForMutation(tooltip);
     }).then(function() {
       assertFalse(!!tooltip.getAttribute('visible'));
