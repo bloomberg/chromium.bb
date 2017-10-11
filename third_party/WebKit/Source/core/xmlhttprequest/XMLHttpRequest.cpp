87aac1e219562b710e963cc5c6b3398bcbd8f0ae
Yutaka Hirano <yhirano@chromium.org>
2017-10-11 07:03:53 +0000

    [XHR] Use correct TextResourceDecoderOption
    Blink enabled HTML / XML charset detection for HTML-ish / XML-ish
    responses even when response type is text. This CL fixes the issue.
    
    This CL removes some layout tests. The behavior is tested by
    wpt/XMLHttpRequest/responsetext-decoding.htm,
    wpt/XMLHttpRequest/responsedocument-decoding.htm,
    wpt/XMLHttpRequest/overridemimetype-open-state-force-utf-8.htm and
    wpt/XMLHttpRequest/overridemimetype-open-state-force-xml.
    
    Bug: 695045
    Change-Id: I82b223dd85907350ce1e7787969bd1d4de85aa3a
    Reviewed-on: https://chromium-review.googlesource.com/701899
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Takeshi Yoshino <tyoshino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507915}
    Former-commit-id: 47e4fc53e6d68c0a788fcc26de598b9e3848033f

--- third_party/WebKit/Source/core/xmlhttprequest/XMLHttpRequest.cpp
+++ third_party/WebKit/Source/core/xmlhttprequest/XMLHttpRequest.cpp
@@ -1786,37 +1786,48 @@ void XMLHttpRequest::ParseDocumentChunk(const char* data, unsigned len) {
 }
 
 std::unique_ptr<TextResourceDecoder> XMLHttpRequest::CreateDecoder() const {
-  if (response_type_code_ == kResponseTypeJSON) {
-    return TextResourceDecoder::Create(TextResourceDecoderOptions(
-        TextResourceDecoderOptions::kPlainTextContent, UTF8Encoding()));
-  }
+  const TextResourceDecoderOptions decoder_options_for_utf8_plain_text(
+      TextResourceDecoderOptions::kPlainTextContent, UTF8Encoding());
+  if (response_type_code_ == kResponseTypeJSON)
+    return TextResourceDecoder::Create(decoder_options_for_utf8_plain_text);
 
   String final_response_charset = FinalResponseCharset();
   if (!final_response_charset.IsEmpty()) {
+    // If the final charset is given, use the charset without sniffing the
+    // content.
     return TextResourceDecoder::Create(TextResourceDecoderOptions(
         TextResourceDecoderOptions::kPlainTextContent,
         WTF::TextEncoding(final_response_charset)));
   }
 
-  // allow TextResourceDecoder to look inside the m_response if it's XML or HTML
-  if (ResponseIsXML()) {
-    TextResourceDecoderOptions options(TextResourceDecoderOptions::kXMLContent);
+  TextResourceDecoderOptions decoder_options_for_xml(
+      TextResourceDecoderOptions::kXMLContent);
+  // Don't stop on encoding errors, unlike it is done for other kinds
+  // of XML resources. This matches the behavior of previous WebKit
+  // versions, Firefox and Opera.
+  decoder_options_for_xml.SetUseLenientXMLDecoding();
 
-    // Don't stop on encoding errors, unlike it is done for other kinds
-    // of XML resources. This matches the behavior of previous WebKit
-    // versions, Firefox and Opera.
-    options.SetUseLenientXMLDecoding();
-
-    return TextResourceDecoder::Create(options);
+  switch (response_type_code_) {
+    case kResponseTypeDefault:
+      if (ResponseIsXML())
+        return TextResourceDecoder::Create(decoder_options_for_xml);
+    // fall through
+    case kResponseTypeText:
+      return TextResourceDecoder::Create(decoder_options_for_utf8_plain_text);
+    case kResponseTypeDocument:
+      if (ResponseIsHTML()) {
+        return TextResourceDecoder::Create(TextResourceDecoderOptions(
+            TextResourceDecoderOptions::kHTMLContent, UTF8Encoding()));
+      }
+      return TextResourceDecoder::Create(decoder_options_for_xml);
+    case kResponseTypeJSON:
+    case kResponseTypeBlob:
+    case kResponseTypeArrayBuffer:
+      NOTREACHED();
+      break;
   }
-
-  if (ResponseIsHTML()) {
-    return TextResourceDecoder::Create(TextResourceDecoderOptions(
-        TextResourceDecoderOptions::kHTMLContent, UTF8Encoding()));
-  }
-
-  return TextResourceDecoder::Create(TextResourceDecoderOptions(
-      TextResourceDecoderOptions::kPlainTextContent, UTF8Encoding()));
+  NOTREACHED();
+  return nullptr;
 }
 
 void XMLHttpRequest::DidReceiveData(const char* data, unsigned len) {
