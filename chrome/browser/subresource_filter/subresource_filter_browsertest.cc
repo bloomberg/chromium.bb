8d64efea3fe802706a981f383c7092da838f086e
Charles Harrison <csharrison@chromium.org>
2017-10-11 23:20:21 +0000

    [subresource_filter] Make sure forced activation does not trigger messages
    Bug: None
    Change-Id: I5d4c86e98c2061a85817f28c36f2a73a6b65c28f
    Reviewed-on: https://chromium-review.googlesource.com/707598
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508167}
    Former-commit-id: 9016e874635d3ad30d96252a0cf6140992331791

--- chrome/browser/subresource_filter/subresource_filter_browsertest.cc
+++ chrome/browser/subresource_filter/subresource_filter_browsertest.cc
@@ -12,7 +12,6 @@
 
 #include "base/bind.h"
 #include "base/command_line.h"
-#include "base/json/json_string_value_serializer.h"
 #include "base/location.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
@@ -53,8 +52,6 @@
 #include "components/subresource_filter/core/common/test_ruleset_creator.h"
 #include "components/subresource_filter/core/common/test_ruleset_utils.h"
 #include "components/url_pattern_index/proto/rules.pb.h"
-#include "content/public/browser/devtools_agent_host.h"
-#include "content/public/browser/devtools_agent_host_client.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_types.h"
 #include "content/public/browser/page_navigator.h"
@@ -135,16 +132,6 @@ GURL GetURLWithFragment(const GURL& url, base::StringPiece fragment) {
   replacements.SetRefStr(fragment);
   return url.ReplaceComponents(replacements);
 }
-
-class TestClient : public content::DevToolsAgentHostClient {
- public:
-  TestClient() {}
-  ~TestClient() override {}
-  void DispatchProtocolMessage(content::DevToolsAgentHost* agent_host,
-                               const std::string& message) override {}
-  void AgentHostClosed(content::DevToolsAgentHost* agent_host,
-                       bool replaced_with_another_client) override {}
-};
 
 }  // namespace
 
@@ -485,104 +472,6 @@ IN_PROC_BROWSER_TEST_F(SubresourceFilterBrowserTest,
     EXPECT_EQ(allowed_empty_subdocument_url, frame->GetLastCommittedURL());
     ExpectFramesIncludedInLayout(kSubframeNames, kExpectFirstAndSecondSubframe);
   }
-}
-
-IN_PROC_BROWSER_TEST_F(SubresourceFilterBrowserTest,
-                       ForceActivation_RequiresDevtools) {
-  const GURL url(
-      GetTestUrl("subresource_filter/frame_with_included_script.html"));
-  ASSERT_NO_FATAL_FAILURE(
-      SetRulesetToDisallowURLsWithPathSuffix("included_script.js"));
-
-  // Should not trigger activation, the URL is not on the blacklist.
-  ui_test_utils::NavigateToURL(browser(), url);
-  EXPECT_TRUE(WasParsedScriptElementLoaded(web_contents()->GetMainFrame()));
-
-  // Open up devtools and trigger forced activation.
-  scoped_refptr<content::DevToolsAgentHost> agent_host =
-      content::DevToolsAgentHost::GetOrCreateFor(web_contents());
-  EXPECT_TRUE(agent_host);
-  TestClient test_client;
-  agent_host->AttachClient(&test_client);
-
-  // Send Page.enable, which is required before any Page methods.
-  agent_host->DispatchProtocolMessage(
-      &test_client, "{\"id\": 0, \"method\": \"Page.enable\"}");
-
-  // Send Page.setAdBlockingEnabled, should force activation.
-  base::DictionaryValue ad_blocking_command;
-  ad_blocking_command.SetInteger("id", 1);
-  ad_blocking_command.SetString("method", "Page.setAdBlockingEnabled");
-  auto params = base::MakeUnique<base::DictionaryValue>();
-  params->SetBoolean("enabled", true);
-  ad_blocking_command.SetDictionary("params", std::move(params));
-  std::string json_string;
-  JSONStringValueSerializer serializer(&json_string);
-  ASSERT_TRUE(serializer.Serialize(ad_blocking_command));
-  agent_host->DispatchProtocolMessage(&test_client, json_string);
-
-  ui_test_utils::NavigateToURL(browser(), url);
-  EXPECT_FALSE(WasParsedScriptElementLoaded(web_contents()->GetMainFrame()));
-
-  // Close devtools, should stop forced activation.
-  agent_host->DetachAllClients();
-  ui_test_utils::NavigateToURL(browser(), url);
-  EXPECT_TRUE(WasParsedScriptElementLoaded(web_contents()->GetMainFrame()));
-}
-
-IN_PROC_BROWSER_TEST_F(SubresourceFilterListInsertingBrowserTest,
-                       WarningSiteWithForceActivation_LogsWarning) {
-  const GURL url(
-      GetTestUrl("subresource_filter/frame_with_included_script.html"));
-  ASSERT_NO_FATAL_FAILURE(
-      SetRulesetToDisallowURLsWithPathSuffix("included_script.js"));
-  ConfigureURLWithWarning(url,
-                          {safe_browsing::SubresourceFilterType::BETTER_ADS});
-
-  Configuration config(subresource_filter::ActivationLevel::ENABLED,
-                       subresource_filter::ActivationScope::ACTIVATION_LIST,
-                       subresource_filter::ActivationList::BETTER_ADS);
-  ResetConfiguration(std::move(config));
-
-  // Should not trigger activation, the URL is not on the blacklist.
-  ui_test_utils::NavigateToURL(browser(), url);
-  EXPECT_TRUE(WasParsedScriptElementLoaded(web_contents()->GetMainFrame()));
-
-  // Open up devtools and trigger forced activation.
-  scoped_refptr<content::DevToolsAgentHost> agent_host =
-      content::DevToolsAgentHost::GetOrCreateFor(web_contents());
-  EXPECT_TRUE(agent_host);
-  TestClient test_client;
-  agent_host->AttachClient(&test_client);
-
-  // Send Page.enable, which is required before any Page methods.
-  agent_host->DispatchProtocolMessage(
-      &test_client, "{\"id\": 0, \"method\": \"Page.enable\"}");
-
-  // Send Page.setAdBlockingEnabled, should force activation.
-  base::DictionaryValue ad_blocking_command;
-  ad_blocking_command.SetInteger("id", 1);
-  ad_blocking_command.SetString("method", "Page.setAdBlockingEnabled");
-  auto params = base::MakeUnique<base::DictionaryValue>();
-  params->SetBoolean("enabled", true);
-  ad_blocking_command.SetDictionary("params", std::move(params));
-  std::string json_string;
-  JSONStringValueSerializer serializer(&json_string);
-  ASSERT_TRUE(serializer.Serialize(ad_blocking_command));
-  agent_host->DispatchProtocolMessage(&test_client, json_string);
-
-  content::ConsoleObserverDelegate console_observer(
-      web_contents(), kActivationWarningConsoleMessage);
-  web_contents()->SetDelegate(&console_observer);
-  ui_test_utils::NavigateToURL(browser(), url);
-  EXPECT_FALSE(WasParsedScriptElementLoaded(web_contents()->GetMainFrame()));
-  console_observer.Wait();
-  EXPECT_EQ(console_observer.message(), kActivationWarningConsoleMessage);
-
-  // Close devtools, should stop forced activation.
-  agent_host->DetachAllClients();
-  ui_test_utils::NavigateToURL(browser(), url);
-  EXPECT_TRUE(WasParsedScriptElementLoaded(web_contents()->GetMainFrame()));
 }
 
 IN_PROC_BROWSER_TEST_F(SubresourceFilterBrowserTest,
