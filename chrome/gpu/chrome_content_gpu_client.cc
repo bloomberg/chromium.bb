9517101a7c28a91fe24c19a81c991ca370b30352
Jamie Madill <jmadill@chromium.org>
2017-10-11 14:20:37 +0000

    Revert "[Sampling Profiler] Start sampling as early during initialization as possible."
    This reverts commit 52ee6241004dadfdbf21ae9594966f55559f1da4 [formerly 65e27b1b16478877de9c5c31a12569a7f8c88a5b].
    
    Reason for revert:
    Causes failures in the GPU FYI x64 bots and ANGLE CQ bots.
    
    See for example 
    https://build.chromium.org/p/chromium.gpu.fyi/builders/Win7%20x64%20Release%20%28NVIDIA%29/builds/12511
    
    [1010/202058.449:FATAL:at_exit.cc(58)] Check failed: false. Tried to RegisterCallback without an AtExitManager
    Backtrace:
    	base::debug::StackTrace::StackTrace [0x000007FEEF138495+69]
    	base::debug::StackTrace::StackTrace [0x000007FEEF112EC3+19]
    	logging::LogMessage::~LogMessage [0x000007FEEF08BB3D+253]
    	base::AtExitManager::RegisterTask [0x000007FEEF0FE469+105]
    	base::AtExitManager::RegisterCallback [0x000007FEEF0FE3E2+258]
    	base::trace_event::TraceEventETWExport::GetInstance [0x000007FEEF139EA0+112]
    	base::trace_event::TraceEventETWExport::IsCategoryGroupEnabled [0x000007FEEF13A31D+125]
    	base::trace_event::TraceLog::UpdateCategoryState [0x000007FEEF0A9333+371]
    	base::trace_event::CategoryRegistry::GetOrCreateCategoryLocked [0x000007FEEF14105F+655]
    	base::trace_event::TraceLog::GetCategoryGroupEnabled [0x000007FEEF0A7092+210]
    	base::debug::TaskAnnotator::DidQueueTask [0x000007FEEF167705+53]
    	base::internal::IncomingTaskQueue::AddToIncomingQueue [0x000007FEEF150E56+982]
    	base::internal::MessageLoopTaskRunner::PostDelayedTask [0x000007FEEF151828+328]
    	base::TaskRunner::PostTask [0x000007FEEF0C6043+67]
    	base::StackSamplingProfiler::SamplingThread::Add [0x000007FEEF0ADAF2+354]
    	base::StackSamplingProfiler::Start [0x000007FEEF0AF986+310]
    
    BUG=773681
    
    Original change's description:
    > [Sampling Profiler] Start sampling as early during initialization as possible.
    > 
    > This will give us visibility into the performance of the earliest parts of
    > startup as well as the later parts, which already have coverage.
    > 
    > R=​asvitkine@chromium.org
    > 
    > Bug: 700083
    > Change-Id: I2fd2e76c00ca46754a57574dc3713ee641494a66
    > Reviewed-on: https://chromium-review.googlesource.com/691239
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Mike Wittman <wittman@chromium.org>
    > Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    > Commit-Queue: Ilya Sherman <isherman@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#507843}
    
    TBR=sky@chromium.org,wittman@chromium.org,isherman@chromium.org,asvitkine@chromium.org
    
    Change-Id: I28d3080b0fa47b953e9a8707511e8cf9bd05ed2d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 700083
    Reviewed-on: https://chromium-review.googlesource.com/712854
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Commit-Queue: Jamie Madill <jmadill@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507976}
    Former-commit-id: aba2fd20b6636eab41e7cb7f879ff511b598b11f

--- chrome/gpu/chrome_content_gpu_client.cc
+++ chrome/gpu/chrome_content_gpu_client.cc
@@ -7,8 +7,11 @@
 #include <utility>
 
 #include "base/command_line.h"
+#include "base/lazy_instance.h"
 #include "base/memory/ptr_util.h"
+#include "base/threading/platform_thread.h"
 #include "base/time/time.h"
+#include "chrome/common/stack_sampling_configuration.h"
 #include "components/metrics/child_call_stack_profile_collector.h"
 #include "content/public/child/child_thread.h"
 #include "content/public/common/content_switches.h"
@@ -23,7 +26,28 @@
 #include "services/service_manager/public/cpp/binder_registry.h"
 #endif
 
-ChromeContentGpuClient::ChromeContentGpuClient() {}
+namespace {
+
+base::LazyInstance<metrics::ChildCallStackProfileCollector>::Leaky
+    g_call_stack_profile_collector = LAZY_INSTANCE_INITIALIZER;
+
+}  // namespace
+
+ChromeContentGpuClient::ChromeContentGpuClient()
+    : stack_sampling_profiler_(
+          base::PlatformThread::CurrentId(),
+          StackSamplingConfiguration::Get()
+              ->GetSamplingParamsForCurrentProcess(),
+          g_call_stack_profile_collector.Get().GetProfilerCallback(
+              metrics::CallStackProfileParams(
+                  metrics::CallStackProfileParams::GPU_PROCESS,
+                  metrics::CallStackProfileParams::GPU_MAIN_THREAD,
+                  metrics::CallStackProfileParams::PROCESS_STARTUP,
+                  metrics::CallStackProfileParams::MAY_SHUFFLE))) {
+  if (StackSamplingConfiguration::Get()->IsProfilerEnabledForCurrentProcess())
+    stack_sampling_profiler_.Start();
+}
+
 ChromeContentGpuClient::~ChromeContentGpuClient() {}
 
 void ChromeContentGpuClient::InitializeRegistry(
@@ -46,11 +70,10 @@ void ChromeContentGpuClient::GpuServiceInitialized(
   gpu_preferences_ = gpu_preferences;
 #endif
 
-  // Note: The call stack profile collection is initiated in ChromeMain.
   metrics::mojom::CallStackProfileCollectorPtr browser_interface;
   content::ChildThread::Get()->GetConnector()->BindInterface(
       content::mojom::kBrowserServiceName, &browser_interface);
-  metrics::ChildCallStackProfileCollector::Get()->SetParentProfileCollector(
+  g_call_stack_profile_collector.Get().SetParentProfileCollector(
       std::move(browser_interface));
 }
 
