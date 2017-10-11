c0ffc827c53fe9f961e081b311d2aa4040291bea
Jamie Madill <jmadill@chromium.org>
2017-10-11 17:18:26 +0000

    Increase webgl conformance shard counts.
    This increases the shard count for the webgl conformance tests from
    one to two, which should halve the 10 minute run time. It also
    increases the shard count for the webgl 2 conformance ANGLE tests
    from 15 to 20, which should reduce the time spent from about 9-10
    minutes to 6-7.
    
    Hold off on increasing the shard count for the Chrome CQ version of
    the webgl 2 conformance tests until we can confirm the bots are stable
    with the increased shards count.
    
    Also fixes the webgl2_conformance_gl_tests, which was accidentally
    testing webgl 1.
    
    BUG=765321
    R=kbr@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I9ac1a0b649556b4797cc6e9d2f885921d55692ca
    Reviewed-on: https://chromium-review.googlesource.com/709460
    Commit-Queue: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508012}
    Former-commit-id: dd17a1699b1cb628aaa1479f6bab4d61a73d8f3d

--- content/test/gpu/generate_buildbot_json.py
+++ content/test/gpu/generate_buildbot_json.py
@@ -1966,6 +1966,9 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
       '--use-angle=d3d9',
     ],
     'asan_args': ['--is-asan'],
+    'swarming': {
+      'shards': 2,
+    },
   },
   'webgl_conformance_gl_tests': {
     'tester_configs': [
@@ -2014,6 +2017,9 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
       '--use-angle=gl',
     ],
     'asan_args': ['--is-asan'],
+    'swarming': {
+      'shards': 2,
+    },
   },
   'webgl_conformance_d3d11_passthrough': {
     'tester_configs': [
@@ -2038,6 +2044,9 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
       '--use-passthrough-cmd-decoder',
     ],
     'asan_args': ['--is-asan'],
+    'swarming': {
+      'shards': 2,
+    },
   },
   'webgl_conformance_gl_passthrough': {
     'tester_configs': [
@@ -2062,6 +2071,9 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
       '--use-passthrough-cmd-decoder',
     ],
     'asan_args': ['--is-asan'],
+    'swarming': {
+      'shards': 2,
+    },
   },
   'webgl2_conformance_tests': {
     'tester_configs': [
@@ -2155,8 +2167,8 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
     'asan_args': ['--is-asan'],
     'swarming': {
       # These tests currently take about an hour and fifteen minutes
-      # to run. Split them into roughly 5-minute shards.
-      'shards': 15,
+      # to run serially.
+      'shards': 20,
     },
   },
   'webgl2_conformance_gl_tests': {
@@ -2187,6 +2199,20 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
     'extra_browser_args': [
       '--use-angle=gl',
     ],
+    'args': [
+      '--webgl-conformance-version=2.0.1',
+      # The current working directory when run via isolate is
+      # out/Debug or out/Release. Reference this file relatively to
+      # it.
+      '--read-abbreviated-json-results-from=' + \
+      '../../content/test/data/gpu/webgl2_conformance_tests_output.json',
+    ],
+    'asan_args': ['--is-asan'],
+    'swarming': {
+      # These tests currently take about an hour and fifteen minutes
+      # to run serially.
+      'shards': 20,
+    },
   },
   'webgl2_conformance_d3d11_passthrough_tests': {
     'tester_configs': [
@@ -2222,7 +2248,7 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
     'swarming': {
       # These tests currently take about an hour and fifteen minutes
       # to run. Split them into roughly 5-minute shards.
-      'shards': 15,
+      'shards': 20,
     },
   },
 }
