e5764b3cd9747f9f5b40583d51497bb4df41b4b7
Toni Barzic <tbarzic@google.com>
2017-10-11 20:16:28 +0000

    Add lock screen note background controller
    The controller will be used to control the lock action app window
    background visibility - the background will be shown when a lock screen
    action handler app is launched (e.g. lock screen note taking app).
    The background is intended to be shown with a ink drop ripple animation,
    thus, in addition to hidden and shown state, the controller will support
    showing and hiding states - states indicating that a background ink drop
    animation is in progress. The background should be visible while the
    lock screen note action is in launching or active state.
    
    Additionally, the lock screen action app window should not be shown
    until the background is fully shown (i.e. until the show animation
    finishes).
    To achieve this, lock_screen_action_background_controller exposes an
    observer interface for the background state. This interface can be used
    to observe whether the ink drop animation is in progress and manage lock
    action handler window visibility accordingly.
    
    Initially, observer interface will be used only by
    lock_action_handler_layout manager to delay showing of lock action
    handler app windows until the background widget animation is done, but
    other observers might be added  - e.g. lock screen shelf / lock screen
    UI to enable them to update their UI state according to the lock screen
    handler background state.
    
    This cl uses stub background controller implementation (the
    implementation that does not show a background) - the actual
    implementation will be added in subsequent cls.
    
    Bug:746596
    
    Change-Id: I05be806ddf35ab29b70e7754a1a47e82e5b87453
    Reviewed-on: https://chromium-review.googlesource.com/689034
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508089}
    Former-commit-id: bbe776a20641c354b23e9d79c14c7a2392ec86e1

--- ash/wm/lock_action_handler_layout_manager.cc
+++ ash/wm/lock_action_handler_layout_manager.cc
@@ -4,8 +4,10 @@
 
 #include "ash/wm/lock_action_handler_layout_manager.h"
 
+#include <utility>
 #include <vector>
 
+#include "ash/lock_screen_action/lock_screen_action_background_controller.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/public/interfaces/tray_action.mojom.h"
 #include "ash/shell.h"
@@ -14,27 +16,52 @@
 #include "ash/wm/window_state.h"
 #include "ash/wm/window_util.h"
 #include "ash/wm/wm_event.h"
+#include "base/bind.h"
 #include "ui/wm/core/window_animations.h"
 
 namespace ash {
 
+namespace {
+
+// Whether child windows should be shown depending on lock screen note action
+// state and lock screen action background state.
+// This should not be used for lock screen background windows.
+bool ShowChildWindows(mojom::TrayActionState action_state,
+                      LockScreenActionBackgroundState background_state) {
+  return action_state == mojom::TrayActionState::kActive &&
+         (background_state == LockScreenActionBackgroundState::kShown ||
+          background_state == LockScreenActionBackgroundState::kHidden);
+}
+
+}  // namespace
+
 LockActionHandlerLayoutManager::LockActionHandlerLayoutManager(
     aura::Window* window,
     Shelf* shelf)
-    : LockLayoutManager(window, shelf), tray_action_observer_(this) {
+    : LockLayoutManager(window, shelf),
+      action_background_controller_(
+          LockScreenActionBackgroundController::Create()),
+      tray_action_observer_(this),
+      action_background_observer_(this) {
+  action_background_controller_->SetParentWindow(window);
+
   TrayAction* tray_action = Shell::Get()->tray_action();
   tray_action_observer_.Add(tray_action);
+  action_background_observer_.Add(action_background_controller_.get());
 }
 
 LockActionHandlerLayoutManager::~LockActionHandlerLayoutManager() = default;
 
 void LockActionHandlerLayoutManager::OnWindowAddedToLayout(
     aura::Window* child) {
-  ::wm::SetWindowVisibilityAnimationType(
-      child, ::wm::WINDOW_VISIBILITY_ANIMATION_TYPE_FADE);
+  ::wm::SetWindowVisibilityAnimationTransition(child, ::wm::ANIMATE_NONE);
 
+  // The lock action background should be shown behind the shelf (which is
+  // transparent on the lock screen), unlike lock action handler windows.
   wm::WindowState* window_state =
-      LockWindowState::SetLockWindowStateWithShelfExcluded(child);
+      action_background_controller_->IsBackgroundWindow(child)
+          ? LockWindowState::SetLockWindowState(child)
+          : LockWindowState::SetLockWindowStateWithShelfExcluded(child);
   wm::WMEvent event(wm::WM_EVENT_ADDED_TO_WORKSPACE);
   window_state->OnWMEvent(&event);
 }
@@ -42,13 +69,56 @@ void LockActionHandlerLayoutManager::OnWindowAddedToLayout(
 void LockActionHandlerLayoutManager::OnChildWindowVisibilityChanged(
     aura::Window* child,
     bool visible) {
+  if (action_background_controller_->IsBackgroundWindow(child)) {
+    window()->StackChildAtBottom(child);
+    return;
+  }
+
   // Windows should be shown only in active state.
-  if (visible && !Shell::Get()->tray_action()->IsLockScreenNoteActive())
+  if (visible &&
+      !ShowChildWindows(Shell::Get()->tray_action()->GetLockScreenNoteState(),
+                        action_background_controller_->state())) {
     child->Hide();
+  }
 }
 
 void LockActionHandlerLayoutManager::OnLockScreenNoteStateChanged(
     mojom::TrayActionState state) {
+  // Update the background controller state first.
+  bool background_changed = false;
+  switch (state) {
+    case mojom::TrayActionState::kNotAvailable:
+      background_changed =
+          action_background_controller_->HideBackgroundImmediately();
+      break;
+    case mojom::TrayActionState::kAvailable:
+      background_changed = action_background_controller_->HideBackground();
+      break;
+    case mojom::TrayActionState::kLaunching:
+    case mojom::TrayActionState::kActive:
+    case mojom::TrayActionState::kBackground:
+      background_changed = action_background_controller_->ShowBackground();
+      break;
+  }
+
+  // Given that background state changes invoke the background controller
+  // observers (one of which is |this|), and |UpdateChildren| is called as part
+  // of handling background state changes, the child windows state has alreday
+  // been updated if |background_changed| is true - no need to do it again.
+  if (background_changed)
+    return;
+
+  UpdateChildren(state, action_background_controller_->state());
+}
+
+void LockActionHandlerLayoutManager::OnLockScreenActionBackgroundStateChanged(
+    LockScreenActionBackgroundState state) {
+  UpdateChildren(Shell::Get()->tray_action()->GetLockScreenNoteState(), state);
+}
+
+void LockActionHandlerLayoutManager::UpdateChildren(
+    mojom::TrayActionState action_state,
+    LockScreenActionBackgroundState background_state) {
   // Update children state:
   // * a child can be visible only in active state
   // * on transition to active state:
@@ -56,18 +126,21 @@ void LockActionHandlerLayoutManager::OnLockScreenNoteStateChanged(
   //       in active state are shown
   //     * activate a container child to ensure the container gets focus when
   //       moving from background state.
+  bool show_children = ShowChildWindows(action_state, background_state);
+  aura::Window* child_to_activate = nullptr;
   for (aura::Window* child : window()->children()) {
-    if (state == mojom::TrayActionState::kActive) {
+    if (action_background_controller_->IsBackgroundWindow(child))
+      continue;
+    if (show_children) {
       child->Show();
+      child_to_activate = child;
     } else {
       child->Hide();
     }
   }
 
-  if (!window()->children().empty() &&
-      state == mojom::TrayActionState::kActive) {
-    wm::ActivateWindow(window()->children().back());
-  }
+  if (child_to_activate)
+    wm::ActivateWindow(child_to_activate);
 }
 
 }  // namespace ash
