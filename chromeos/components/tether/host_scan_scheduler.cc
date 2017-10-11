32661644308daaed5ea8a1bc173eaa1f8511c52f
Kyle Horimoto <khorimoto@google.com>
2017-10-11 19:21:54 +0000

    [CrOS Tether] Add a metric for the length of a host scan batch.
    Here, a batch refers to one or more host scans in close succession. This
    metric will provide us with a way to quantify how much users are
    scanning, which will be helpful in determining Bluetooth stability.
    
    Bug: 772536, 672263
    Change-Id: I33f97bdca7a25b5f0a66c1e89be28718176a2d40
    Reviewed-on: https://chromium-review.googlesource.com/711097
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508059}
    Former-commit-id: fb813a421a3821c32b2d95ac32cfce3752cab6b4

--- chromeos/components/tether/host_scan_scheduler.cc
+++ chromeos/components/tether/host_scan_scheduler.cc
@@ -6,6 +6,8 @@
 
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram_macros.h"
+#include "base/time/default_clock.h"
 #include "chromeos/network/network_handler.h"
 #include "chromeos/network/network_state.h"
 #include "chromeos/network/network_state_handler.h"
@@ -15,10 +17,35 @@ namespace chromeos {
 
 namespace tether {
 
+namespace {
+
+// The InstantTethering.HostScanBatchDuration metric meaures the duration of a
+// batch of host scans. A "batch" of host scans here refers to a group of host
+// scans which occur just after each other. For example, two 30-second scans
+// with a 5-second gap between them are considered a single 65-second batch host
+// scan for this metric. For two back-to-back scans to be considered part of the
+// same batch metric, they must be at most kMaxNumSecondsBetweenBatchScans
+// seconds apart.
+const int64_t kMaxNumSecondsBetweenBatchScans = 60;
+
+// Minimum value for the scan length metric.
+const int64_t kMinScanMetricSeconds = 1;
+
+// Maximum value for the scan length metric.
+const int64_t kMaxScanMetricsDays = 1;
+
+// Number of buckets in the metric.
+const int kNumMetricsBuckets = 1000;
+
+}  // namespace
+
 HostScanScheduler::HostScanScheduler(NetworkStateHandler* network_state_handler,
                                      HostScanner* host_scanner)
     : network_state_handler_(network_state_handler),
-      host_scanner_(host_scanner) {
+      host_scanner_(host_scanner),
+      timer_(base::MakeUnique<base::OneShotTimer>()),
+      clock_(base::MakeUnique<base::DefaultClock>()),
+      weak_ptr_factory_(this) {
   network_state_handler_->AddObserver(this, FROM_HERE);
   host_scanner_->AddObserver(this);
 }
@@ -27,6 +54,19 @@ HostScanScheduler::~HostScanScheduler() {
   network_state_handler_->SetTetherScanState(false);
   network_state_handler_->RemoveObserver(this, FROM_HERE);
   host_scanner_->RemoveObserver(this);
+
+  // If the most recent batch of host scans has already been logged, return
+  // early.
+  if (!host_scanner_->IsScanActive() && !timer_->IsRunning())
+    return;
+
+  // If a scan is still active during shutdown, there is not enough time to wait
+  // for the scan to finish before logging its full duration. Instead, mark the
+  // current time as the end of the scan so that it can be logged.
+  if (host_scanner_->IsScanActive())
+    last_scan_end_timestamp_ = clock_->Now();
+
+  LogHostScanBatchMetric();
 }
 
 void HostScanScheduler::ScheduleScan() {
@@ -34,7 +74,7 @@ void HostScanScheduler::ScheduleScan() {
 }
 
 void HostScanScheduler::DefaultNetworkChanged(const NetworkState* network) {
-  if (!IsNetworkConnectingOrConnected(network) &&
+  if ((!network || !network->IsConnectingOrConnected()) &&
       !IsTetherNetworkConnectingOrConnected()) {
     EnsureScan();
   }
@@ -46,20 +86,36 @@ void HostScanScheduler::ScanRequested() {
 
 void HostScanScheduler::ScanFinished() {
   network_state_handler_->SetTetherScanState(false);
+
+  last_scan_end_timestamp_ = clock_->Now();
+  timer_->Start(FROM_HERE,
+                base::TimeDelta::FromSeconds(kMaxNumSecondsBetweenBatchScans),
+                base::Bind(&HostScanScheduler::LogHostScanBatchMetric,
+                           weak_ptr_factory_.GetWeakPtr()));
+}
+
+void HostScanScheduler::SetTestDoubles(
+    std::unique_ptr<base::Timer> test_timer,
+    std::unique_ptr<base::Clock> test_clock) {
+  timer_ = std::move(test_timer);
+  clock_ = std::move(test_clock);
 }
 
 void HostScanScheduler::EnsureScan() {
   if (host_scanner_->IsScanActive())
     return;
 
+  // If the timer is running, this new scan is part of the same batch as the
+  // previous scan, so the timer should be stopped (it will be restarted after
+  // the new scan finishes). If the timer is not running, the new scan is part
+  // of a new batch, so the start timestamp should be recorded.
+  if (timer_->IsRunning())
+    timer_->Stop();
+  else
+    last_scan_batch_start_timestamp_ = clock_->Now();
+
   host_scanner_->StartScan();
   network_state_handler_->SetTetherScanState(true);
-}
-
-bool HostScanScheduler::IsNetworkConnectingOrConnected(
-    const NetworkState* network) {
-  return network &&
-         (network->IsConnectingState() || network->IsConnectedState());
 }
 
 bool HostScanScheduler::IsTetherNetworkConnectingOrConnected() {
@@ -67,6 +123,22 @@ bool HostScanScheduler::IsTetherNetworkConnectingOrConnected() {
              NetworkTypePattern::Tether()) ||
          network_state_handler_->ConnectedNetworkByType(
              NetworkTypePattern::Tether());
+}
+
+void HostScanScheduler::LogHostScanBatchMetric() {
+  DCHECK(!last_scan_batch_start_timestamp_.is_null());
+  DCHECK(!last_scan_end_timestamp_.is_null());
+
+  base::TimeDelta batch_duration =
+      last_scan_end_timestamp_ - last_scan_batch_start_timestamp_;
+  UMA_HISTOGRAM_CUSTOM_TIMES(
+      "InstantTethering.HostScanBatchDuration", batch_duration,
+      base::TimeDelta::FromSeconds(kMinScanMetricSeconds) /* min */,
+      base::TimeDelta::FromDays(kMaxScanMetricsDays) /* max */,
+      kNumMetricsBuckets /* bucket_count */);
+
+  PA_LOG(INFO) << "Logging host scan batch duration. Duration was "
+               << batch_duration.InSeconds() << " seconds.";
 }
 
 }  // namespace tether
