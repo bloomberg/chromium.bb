c88b8b1e21e0f3d33b71584e039920b4946d769f
Ted Choc <tedchoc@google.com>
2017-10-11 22:51:47 +0000

    Fix alpha animation on chrome home promo illustration.
    Add proper calls to invalidateSelf() in the overridden
    setAlpha calls to ensure the promo updates in the animation
    correctly.
    
    BUG=772547
    
    Change-Id: I7444a01701f19c72f5e776b8160cfb356a06bdf4
    Reviewed-on: https://chromium-review.googlesource.com/713972
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508150}
    Former-commit-id: aa79827bc734f1bee52dbffcf1d0472a63649dc6

--- chrome/android/java/src/org/chromium/chrome/browser/widget/animation/AnimatorProperties.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/animation/AnimatorProperties.java
@@ -5,7 +5,6 @@
 package org.chromium.chrome.browser.widget.animation;
 
 import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.util.Property;
 
 /**
@@ -25,7 +24,6 @@ public class AnimatorProperties {
         @Override
         public void set(Drawable d, Integer alpha) {
             d.setAlpha(alpha);
-            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) d.invalidateSelf();
         }
     };
 }
