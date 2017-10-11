88a94eb74dc538e0ed224271bda0da9b4fac5a5f
Greg Kraynov <kraynov@chromium.org>
2017-10-11 15:18:02 +0000

    Blink Scheduler: TraceableState class for automatic tracing.
    Bug: 765660
    Change-Id: I8a46111cd13577d2e999e5038d1d10e90d3d2ebc
    Reviewed-on: https://chromium-review.googlesource.com/709554
    Commit-Queue: Grigoriy Kraynov <kraynov@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#507985}
    Former-commit-id: 89b6ce1ae3701d825a5519a156d6110c0b5e8e9a

--- third_party/WebKit/Source/platform/scheduler/util/tracing_helper.h
+++ third_party/WebKit/Source/platform/scheduler/util/tracing_helper.h
@@ -5,7 +5,6 @@
 #ifndef THIRD_PARTY_WEBKIT_SOURCE_PLATFORM_SCHEDULER_UTIL_TRACING_HELPER_H_
 #define THIRD_PARTY_WEBKIT_SOURCE_PLATFORM_SCHEDULER_UTIL_TRACING_HELPER_H_
 
-#include <memory>
 #include "base/macros.h"
 #include "base/trace_event/trace_event.h"
 #include "platform/PlatformExport.h"
@@ -28,25 +27,54 @@ bool AreVerboseSnapshotsEnabled();
 // TRACE_EVENT macros define static variable to cache a pointer to the state
 // of category. Hence, we need distinct version for each category in order to
 // prevent unintended leak of state.
-template <const char* category>
-class StateTracer {
+template <typename T, const char* category>
+class TraceableState {
  public:
-  StateTracer(const char* name, const void* object)
-      : name_(name), object_(object), started_(false) {
-    // StateTracer category must be a constant defined in tracing_helper.
-    // Unfortunately, static_assert won't work here because inequality (!=) of
-    // linker symbols is undefined in compile-time.
+  TraceableState(T initial_state,
+                 const char* name,
+                 const void* object,
+                 const char* (*converter)(T))
+      : name_(name),
+        object_(object),
+        converter_(converter),
+        state_(initial_state),
+        started_(false) {
+    // Category must be a constant defined in tracing_helper.
+    // Unfortunately, static_assert won't work here because inequality (!=)
+    // of linker symbols is undefined in compile-time.
     DCHECK(category == kTracingCategoryNameDefault ||
            category == kTracingCategoryNameInfo ||
            category == kTracingCategoryNameDebug);
   }
 
-  ~StateTracer() {
+  ~TraceableState() {
     if (started_)
       TRACE_EVENT_ASYNC_END0(category, name_, object_);
   }
 
-  void SetState(const char* state) {
+  TraceableState& operator =(const T& value) {
+    Assign(value);
+    return *this;
+  }
+  TraceableState& operator =(const TraceableState& another) {
+    Assign(another.state_);
+    return *this;
+  }
+
+  operator T() const {
+    return state_;
+  }
+
+  void OnTraceLogEnabled() {
+    Assign(state_, true);
+  }
+
+ private:
+  void Assign(T new_state, bool force_trace = false) {
+    if (!force_trace && new_state == state_)
+      return;
+    state_ = new_state;
+
     if (started_)
       TRACE_EVENT_ASYNC_END0(category, name_, object_);
 
@@ -57,12 +85,11 @@ class StateTracer {
       base::TimeTicks now = base::TimeTicks::Now();
       TRACE_EVENT_ASYNC_BEGIN_WITH_TIMESTAMP0(category, name_, object_, now);
       TRACE_EVENT_ASYNC_STEP_INTO_WITH_TIMESTAMP0(category, name_, object_,
-                                                  state, now);
+                                                  converter_(state_), now);
     }
   }
 
- private:
-  bool is_enabled() {
+  bool is_enabled() const {
     bool result = false;
     TRACE_EVENT_CATEGORY_GROUP_ENABLED(category, &result);  // Cached.
     return result;
@@ -70,13 +97,14 @@ class StateTracer {
 
   const char* const name_;  // Not owned.
   const void* const object_;  // Not owned.
+  const char* (*converter_)(T);
 
+  T state_;
   // We have to track |started_| state to avoid race condition since SetState
   // might be called before OnTraceLogEnabled notification.
   bool started_;
 
-  DISALLOW_COPY_AND_ASSIGN(StateTracer);
-  // TODO(kraynov): Tests.
+  DISALLOW_COPY(TraceableState);
 };
 
 }  // namespace scheduler
