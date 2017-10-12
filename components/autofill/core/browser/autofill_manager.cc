b504d74b5167c17e3cb39879dd2136f1f108bb1f
Shanfeng Zhang <szhangcs@google.com>
2017-10-12 20:23:05 +0000

    Credit card autofill ablation experiment setup
    This CL is patched from  https://chromium-review.googlesource.com/c/chromium/src/+/615603
    Handling this since Kumar doesn't have enough time to finish this in this week.
    
    Bug:755337
    
    Abalation doc - https://docs.google.com/document/d/1vMGO7sfzYDXpev_5ftyLH-WoKpCHA4I9lVLYuesVT9c/edit?ts=59826c12#heading=h.k6sbad93o9zk
    
    Conditional Metrics to log in control & ablation arm
    >>
    Submitted with suggestion shown (once)
    >>
    
    Change-Id: Ie1be217bc9dfcca36419e6635f9f8459be1ec0b5
    Reviewed-on: https://chromium-review.googlesource.com/711021
    Commit-Queue: Shanfeng Zhang <szhangcs@google.com>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508441}
    Former-commit-id: 3d541cac26d562d9a6f621362faef99d807056f6

--- components/autofill/core/browser/autofill_manager.cc
+++ components/autofill/core/browser/autofill_manager.cc
@@ -247,6 +247,7 @@ AutofillManager::AutofillManager(
       found_cvc_field_(false),
       found_value_in_cvc_field_(false),
       found_cvc_value_in_non_cvc_field_(false),
+      enable_ablation_logging_(false),
       external_delegate_(NULL),
       test_delegate_(NULL),
 #if defined(OS_ANDROID) || defined(OS_IOS)
@@ -442,9 +443,9 @@ bool AutofillManager::OnFormSubmitted(const FormData& form) {
     }
   }
 
-  address_form_event_logger_->OnFormSubmitted();
+  address_form_event_logger_->OnFormSubmitted(/*force_logging=*/false);
   if (IsCreditCardAutofillEnabled())
-    credit_card_form_event_logger_->OnFormSubmitted();
+    credit_card_form_event_logger_->OnFormSubmitted(enable_ablation_logging_);
 
   // Update Personal Data with the form's submitted data.
   if (submitted_form->IsAutofillable())
@@ -627,6 +628,16 @@ void AutofillManager::OnQueryFormFieldAutofillImpl(
     } else {
       suggestions =
           GetProfileSuggestions(*form_structure, field, *autofill_field);
+    }
+
+    // Logic for disabling/ablating credit card autofill.
+    if (base::FeatureList::IsEnabled(kAutofillCreditCardAblationExperiment) &&
+        is_filling_credit_card && !suggestions.empty()) {
+      suggestions.clear();
+      autocomplete_history_manager_->CancelPendingQuery();
+      external_delegate_->OnSuggestionsReturned(query_id, suggestions);
+      enable_ablation_logging_ = true;
+      return;
     }
 
     if (!suggestions.empty()) {
@@ -1609,6 +1620,7 @@ void AutofillManager::Reset() {
   user_did_type_ = false;
   user_did_autofill_ = false;
   user_did_edit_autofilled_field_ = false;
+  enable_ablation_logging_ = false;
   masked_card_ = CreditCard();
   unmasking_query_id_ = -1;
   unmasking_form_ = FormData();
