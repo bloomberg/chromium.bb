169f05fe3bd9da0167b4f1b3fde62106659b2d06
Yutaka Hirano <yhirano@chromium.org>
2017-10-12 08:04:11 +0000

    Record TeeHelper::Destination::client_'s name
    To investigate crashes this CL records the client's name.
    
    Bug: 756406, 731019
    Change-Id: I6374c05f5e408ad93480f974cbe775092450fe01
    Reviewed-on: https://chromium-review.googlesource.com/715296
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508279}
    Former-commit-id: eebfe8db321b682bc5f8609a0175cdbf734d0edc

--- third_party/WebKit/Source/modules/fetch/BytesConsumer.cpp
+++ third_party/WebKit/Source/modules/fetch/BytesConsumer.cpp
@@ -13,6 +13,7 @@
 #include "platform/blob/BlobData.h"
 #include "platform/wtf/Functional.h"
 #include "platform/wtf/RefPtr.h"
+#include "platform/wtf/debug/Alias.h"
 #include "v8/include/v8.h"
 
 namespace blink {
@@ -200,9 +201,13 @@ class TeeHelper final : public GarbageCollectedFinalized<TeeHelper>,
       if (state == PublicState::kClosed || state == PublicState::kErrored)
         return;
       client_ = client;
+      client_name_ = client->DebugName().Utf8();
     }
 
-    void ClearClient() override { client_ = nullptr; }
+    void ClearClient() override {
+      client_ = nullptr;
+      client_name_ = CString();
+    }
 
     void Cancel() override {
       DCHECK(!chunk_in_use_);
@@ -250,6 +255,11 @@ class TeeHelper final : public GarbageCollectedFinalized<TeeHelper>,
         return;
       }
       if (client_) {
+        char client_name[32];
+        strncpy(client_name, client_name_.data(), sizeof(client_name) - 1);
+        client_name[sizeof(client_name) - 1] = '\0';
+        WTF::debug::Alias(client_name);
+
         client_->OnStateChange();
         if (GetPublicState() == PublicState::kErrored)
           ClearClient();
@@ -259,6 +269,11 @@ class TeeHelper final : public GarbageCollectedFinalized<TeeHelper>,
     bool IsCancelled() const { return is_cancelled_; }
 
     DEFINE_INLINE_TRACE() {
+      char client_name[32];
+      strncpy(client_name, client_name_.data(), sizeof(client_name) - 1);
+      client_name[sizeof(client_name) - 1] = '\0';
+      WTF::debug::Alias(client_name);
+
       visitor->Trace(execution_context_);
       visitor->Trace(tee_);
       visitor->Trace(client_);
@@ -287,6 +302,8 @@ class TeeHelper final : public GarbageCollectedFinalized<TeeHelper>,
     Member<ExecutionContext> execution_context_;
     Member<TeeHelper> tee_;
     Member<BytesConsumer::Client> client_;
+    // TODO(yhirano): Remove this member once the investigation finishes.
+    CString client_name_;
     HeapDeque<Member<Chunk>> chunks_;
     Member<Chunk> chunk_in_use_;
     size_t offset_ = 0;
