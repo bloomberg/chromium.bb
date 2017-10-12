4063f529665c997c6792eb1b10885c01dfdb4727
Xiaocheng Hu <xiaochengh@chromium.org>
2017-10-12 03:33:32 +0000

    Remove focus check from copy action
    This is a partial revert of https://codereview.chromium.org/2894473003
    for copy. With this patch, we stop checking focus when doing a copy
    action. Reasons:
    
    1. Copying unfocused content still makes sense
    2. Spec (*) doesn't require selection to have focus to be copied
    3. A lot of sites rely on copying without focus
    
    (*) https://w3c.github.io/clipboard-apis/#clipboard-actions
    
    Bug: 771604
    Change-Id: I50caa4afb381f324cf86f168456e6bcecb24b082
    Reviewed-on: https://chromium-review.googlesource.com/713742
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508247}
    Former-commit-id: 75a47d437c69a18e12e6b419fbe9f4f527a74e29

--- third_party/WebKit/Source/core/editing/commands/EditorCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/EditorCommand.cpp
@@ -2133,9 +2133,6 @@ static bool EnableCaretInEditableText(LocalFrame& frame,
 static bool EnabledCopy(LocalFrame& frame, Event*, EditorCommandSource source) {
   if (!CanWriteClipboard(frame, source))
     return false;
-  if (source == kCommandFromMenuOrKeyBinding &&
-      !frame.Selection().SelectionHasFocus())
-    return false;
   return frame.GetEditor().CanDHTMLCopy() || frame.GetEditor().CanCopy();
 }
 
