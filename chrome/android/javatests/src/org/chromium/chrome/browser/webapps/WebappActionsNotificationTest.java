85f6f91178d74f8478e799eedbd0ecb5737afb90
Piotr Swigon <piotrs@chromium.org>
2017-10-12 10:22:54 +0000

    [WebApps] Copies the URL when the user taps on a webapp notification.
    Bug: 471623
    Change-Id: I78662fd34694c4cfd3f3fc6754eecae08b194a07
    Reviewed-on: https://chromium-review.googlesource.com/715076
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Piotr Swigon <piotrs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508303}
    Former-commit-id: 6dc04ca230c2cb1f576bdd9420607bb8c369eeaf

--- chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappActionsNotificationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappActionsNotificationTest.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.webapps;
 import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.os.Build;
 import android.service.notification.StatusBarNotification;
@@ -20,6 +21,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.ApplicationStatus;
+import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
@@ -60,7 +62,7 @@ public class WebappActionsNotificationTest {
     @RetryOnFailure
     @MinAndroidSdkLevel(Build.VERSION_CODES.M) // NotificationManager.getActiveNotifications
     @CommandLineFlags.Add({"enable-features=" + ChromeFeatureList.PWA_PERSISTENT_NOTIFICATION})
-    public void testNotification() throws Exception {
+    public void testNotification_openInChrome() throws Exception {
         Notification notification = getWebappNotification();
 
         Assert.assertNotNull(notification);
@@ -82,6 +84,27 @@ public class WebappActionsNotificationTest {
         });
 
         Assert.assertNull("Notification should no longer be shown", getWebappNotification());
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"Webapps"})
+    @RetryOnFailure
+    @MinAndroidSdkLevel(Build.VERSION_CODES.M) // NotificationManager.getActiveNotifications
+    @CommandLineFlags.Add({"enable-features=" + ChromeFeatureList.PWA_PERSISTENT_NOTIFICATION})
+    public void testNotification_copyUrl() throws Exception {
+        Notification notification = getWebappNotification();
+        Assert.assertNotNull(notification);
+
+        notification.contentIntent.send();
+
+        ThreadUtils.runOnUiThreadBlocking(() -> {
+            ClipboardManager clipboard =
+                    (ClipboardManager) mActivityTestRule.getActivity().getSystemService(
+                            Context.CLIPBOARD_SERVICE);
+            Assert.assertEquals(mActivityTestRule.getUrlFromTestServer(WEB_APP_PATH),
+                    clipboard.getPrimaryClip().getItemAt(0).getText().toString());
+        });
     }
 
     @Test
