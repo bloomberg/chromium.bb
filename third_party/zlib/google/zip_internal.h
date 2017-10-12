af718f29ad63d5b9a775f42352e66270d8fbc534
Keishi Hattori <keishi@chromium.org>
2017-10-12 05:00:26 +0000

    Revert "Abstrating file access operation in zip creation."
    This reverts commit 400faf00038fbfc82c0f7017167a0de713cd8757 [formerly 91ac47fceb859a3804b38ec4f3befcef0f29b019].
    
    Reason for revert: Failing unit_tests ZipTest.UnzipUncompressed and others
    
    crbug.com/773963
    
    Original change's description:
    > Abstrating file access operation in zip creation.
    > 
    > Abstracting file access operations when creating ZIP files. This is
    > going to be useful when ZIP operations are done by a Mojo service in a
    > sandboxed environment and can only access files through IPC.
    > 
    > In order to make the API cleaner, introduced a parameter class that
    > lets caller specify which options to use.
    > 
    > Bug: 772815
    > Change-Id: I229291a0c37bfee8fd29fe894a8118c91afab074
    > Reviewed-on: https://chromium-review.googlesource.com/706282
    > Commit-Queue: Jay Civelli <jcivelli@chromium.org>
    > Reviewed-by: Ilya Sherman <isherman@chromium.org>
    > Reviewed-by: Chris Blume <cblume@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#508233}
    
    TBR=jcivelli@chromium.org,isherman@chromium.org,satorux@chromium.org,cblume@chromium.org
    
    Change-Id: I91fb4aa3b5920eac88e1ba6900a441961117c632
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 772815
    Reviewed-on: https://chromium-review.googlesource.com/715036
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508256}
    Former-commit-id: b092fa9950a914b79d01ad59e6ec3f9c8e18b0db

--- third_party/zlib/google/zip_internal.h
+++ third_party/zlib/google/zip_internal.h
@@ -7,7 +7,6 @@
 
 #include <string>
 
-#include "base/time/time.h"
 #include "build/build_config.h"
 
 #if defined(OS_WIN)
@@ -60,10 +59,13 @@ zipFile OpenForZipping(const std::string& file_name_utf8, int append_flag);
 zipFile OpenFdForZipping(int zip_fd, int append_flag);
 #endif
 
+// Returns a zip_fileinfo with the last modification date of |path| set.
+zip_fileinfo GetFileInfoForZipping(const base::FilePath& path);
+
 // Wrapper around zipOpenNewFileInZip4 which passes most common options.
 bool ZipOpenNewFileInZip(zipFile zip_file,
                          const std::string& str_path,
-                         base::Time last_modified_time);
+                         const zip_fileinfo* file_info);
 
 const int kZipMaxPath = 256;
 const int kZipBufSize = 8192;
