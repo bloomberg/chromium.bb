10ed7baa2fd8f047f4c30befbf9f440dd5d07e66
Tarun Bansal <tbansal@chromium.org>
2017-10-12 20:14:35 +0000

    Add histograms to record result of warmup URL fetching in data reduction proxy (DRP).
    Bug: 760294
    Change-Id: Ibd0085ed90aa800d7d1def8ea2f06c95dc632448
    Reviewed-on: https://chromium-review.googlesource.com/710737
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508430}
    Former-commit-id: 89cb8de76b47a274111b8b6b013322f1c8779a2c

--- components/data_reduction_proxy/core/common/data_reduction_proxy_util.h
+++ components/data_reduction_proxy/core/common/data_reduction_proxy_util.h
@@ -46,6 +46,16 @@ enum class Client {
   CHROME_QNX,
 };
 
+// Scheme of the proxy used.
+enum ProxyScheme {
+  PROXY_SCHEME_UNKNOWN = 0,
+  PROXY_SCHEME_HTTP,
+  PROXY_SCHEME_HTTPS,
+  PROXY_SCHEME_QUIC,
+  PROXY_SCHEME_DIRECT,
+  PROXY_SCHEME_MAX
+};
+
 namespace util {
 
 // Returns the version of Chromium that is being used, e.g. "1.2.3.4".
@@ -98,6 +108,10 @@ int64_t CalculateEffectiveOCL(const net::URLRequest& request);
 int64_t EstimateOriginalReceivedBytes(const net::URLRequest& request,
                                       bool used_drp,
                                       const LoFiDecider* lofi_decider);
+
+// Converts net::ProxyServer::Scheme to type ProxyScheme.
+ProxyScheme ConvertNetProxySchemeToProxyScheme(net::ProxyServer::Scheme scheme);
+
 }  // namespace util
 
 namespace protobuf_parser {
