c819e4d7452fbefaf6a4d064c2cfe75c6da28062
Kenneth Russell <kbr@chromium.org>
2017-10-12 22:51:46 +0000

    Wait for subordinate windows to be ready in SnapshotBrowserTest.
    In order to force all the windows to share the same renderer process,
    the test calls window.open() from JavaScript, which doesn't have the
    same synchronization guarantees as content_browser_test_utils'
    NavigateToURL. Wait for the script to execute in each window before
    proceeding with the test.
    
    BUG=772379
    
    Change-Id: Id5aff8aca03e22c65d06e5ed35c4aeff13997ac2
    Reviewed-on: https://chromium-review.googlesource.com/714684
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508514}
    Former-commit-id: 745308be7bd9df53c1834d674220d885adf40b00

--- content/browser/snapshot_browsertest.cc
+++ content/browser/snapshot_browsertest.cc
@@ -50,6 +50,7 @@ static const char kCanvasPageString[] =
     "      offset += 50;"
     "      window.domAutomationController.send(true);"
     "    }"
+    "    window.document.title = \"Ready\";"
     "  </script>"
     "</body>";
 }
@@ -105,6 +106,18 @@ class SnapshotBrowserTest : public ContentBrowserTest {
     http_response->set_content(kCanvasPageString);
     http_response->set_content_type("text/html");
     return http_response;
+  }
+
+  void WaitForAllWindowsToBeReady() {
+    const base::string16 expected_title = base::UTF8ToUTF16("Ready");
+    // The subordinate windows may load asynchronously. Wait for all of
+    // them to execute their script before proceeding.
+    auto browser_list = Shell::windows();
+    for (Shell* browser : browser_list) {
+      TitleWatcher watcher(GetWebContents(browser), expected_title);
+      const base::string16& actual_title = watcher.WaitAndGetTitle();
+      EXPECT_EQ(expected_title, actual_title);
+    }
   }
 
   struct ExpectedColor {
@@ -254,6 +267,8 @@ IN_PROC_BROWSER_TEST_F(SnapshotBrowserTest, MAYBE_SyncMultiWindowTest) {
 
   base::RunLoop().RunUntilIdle();
 
+  WaitForAllWindowsToBeReady();
+
   auto browser_list = Shell::windows();
   EXPECT_EQ(4u, browser_list.size());
 
@@ -297,7 +312,7 @@ IN_PROC_BROWSER_TEST_F(SnapshotBrowserTest, MAYBE_SyncMultiWindowTest) {
 
 // Seen to time out / fail on Mac and Win bots; crbug.com/772379,
 // crbug.com/771119.
-IN_PROC_BROWSER_TEST_F(SnapshotBrowserTest, DISABLED_AsyncMultiWindowTest) {
+IN_PROC_BROWSER_TEST_F(SnapshotBrowserTest, AsyncMultiWindowTest) {
   SetupTestServer();
 
   for (int i = 0; i < 3; ++i) {
@@ -308,6 +323,8 @@ IN_PROC_BROWSER_TEST_F(SnapshotBrowserTest, DISABLED_AsyncMultiWindowTest) {
   }
 
   base::RunLoop().RunUntilIdle();
+
+  WaitForAllWindowsToBeReady();
 
   auto browser_list = Shell::windows();
   EXPECT_EQ(4u, browser_list.size());
