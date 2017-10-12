37c58bfdd8baf2a08bfef6c7cbf1a23cc220c32f
Takashi Toyoshima <toyoshim@chromium.org>
2017-10-12 10:03:17 +0000

    Add spec links for fetch related enums
    NOTRY=true
    
    Bug: 736308
    Change-Id: Ie63efe6e1270077ec32f7aaf71a37fd0ce947b6d
    Reviewed-on: https://chromium-review.googlesource.com/701901
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508295}
    Former-commit-id: 916c6701b399029e0c610161628659d30a177d5b

--- third_party/WebKit/public/platform/WebURLRequest.h
+++ third_party/WebKit/public/platform/WebURLRequest.h
@@ -105,6 +105,8 @@ class WebURLRequest {
     kFrameTypeTopLevel
   };
 
+  // Corresponds to Fetch request's "mode" and "use-CORS-preflight flag":
+  // https://fetch.spec.whatwg.org/#concept-request-mode
   enum FetchRequestMode : uint8_t {
     kFetchRequestModeSameOrigin,
     kFetchRequestModeNoCORS,
@@ -113,13 +115,22 @@ class WebURLRequest {
     kFetchRequestModeNavigate
   };
 
+  // Corresponds to Fetch request's "credentials mode":
+  // https://fetch.spec.whatwg.org/#concept-request-credentials-mode
   enum FetchCredentialsMode : uint8_t {
     kFetchCredentialsModeOmit,
     kFetchCredentialsModeSameOrigin,
     kFetchCredentialsModeInclude,
+
+    // "password" mode is not defined in the Fetch spec, but can be seen in
+    // an example code for the Credential Management Level 1.
+    // https://w3c.github.io/webappsec-credential-management/#passwords
+    // TODO(battre): Remove kFetchCredentialsModePassword.
     kFetchCredentialsModePassword
   };
 
+  // Corresponds to Fetch request's "cache mode":
+  // https://fetch.spec.whatwg.org/#concept-request-cache-mode
   enum FetchRequestCacheMode : uint8_t {
     kFetchRequestCacheModeDefault,
     kFetchRequestCacheModeNoStore,
@@ -129,6 +140,8 @@ class WebURLRequest {
     kFetchRequestCacheModeOnlyIfCached
   };
 
+  // Corresponds to Fetch request's "redirect mode":
+  // https://fetch.spec.whatwg.org/#concept-request-redirect-mode
   enum FetchRedirectMode : uint8_t {
     kFetchRedirectModeFollow,
     kFetchRedirectModeError,
