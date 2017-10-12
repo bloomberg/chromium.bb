46cd212365056abc395827e130c08b1e1df5a93b
Darren Shen <shend@chromium.org>
2017-10-12 03:13:24 +0000

    Add fuzzing target for CSSParserFastPaths.cpp.
    CSSParserFastPaths is a class that parses very specific cases of CSS
    property values e.g. "width:10px". It scans the string at a character
    level instead of tokenizing which makes it fast, but it means that
    not everything can be parsed via the fast path (e.g. "width:10em").
    
    The fast path code performs a lot of array manipulation and branches,
    so it's a good candidate for fuzzing.
    
    Change-Id: Iaac16dfc971df96036d9ae63dd514aa82bb46f0d
    Reviewed-on: https://chromium-review.googlesource.com/706450
    Reviewed-by: Max Moroz <mmoroz@chromium.org>
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508238}
    Former-commit-id: 4491ba9d10af731964683f577822e7f29c339bf3

---
+++ third_party/WebKit/Source/core/css/parser/CSSParserFastPathsFuzzer.cpp
@@ -0,0 +1,39 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/parser/CSSParserFastPaths.h"
+
+#include "platform/testing/BlinkFuzzerTestSupport.h"
+#include "platform/wtf/text/WTFString.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  static blink::BlinkFuzzerTestSupport test_support =
+      blink::BlinkFuzzerTestSupport();
+
+  // MaybeParseValue assumes the string is not empty.
+  if (size == 0)
+    return 0;
+
+  const std::string data_string(reinterpret_cast<const char*>(data), size);
+  const std::size_t data_hash = std::hash<std::string>()(data_string);
+
+  const auto property_id =
+      static_cast<blink::CSSPropertyID>(data_hash % blink::numCSSPropertyIDs);
+
+  for (unsigned parser_mode = 0;
+       parser_mode < blink::CSSParserMode::kNumCSSParserModes; parser_mode++) {
+    blink::CSSParserFastPaths::MaybeParseValue(
+        property_id, String::FromUTF8WithLatin1Fallback(data, size),
+        static_cast<blink::CSSParserMode>(parser_mode));
+  }
+
+#if defined(ADDRESS_SANITIZER)
+  // LSAN needs unreachable objects to be released to avoid reporting them
+  // incorrectly as a memory leak.
+  blink::ThreadState* currentThreadState = blink::ThreadState::Current();
+  currentThreadState->CollectAllGarbage();
+#endif
+
+  return 0;
+}
