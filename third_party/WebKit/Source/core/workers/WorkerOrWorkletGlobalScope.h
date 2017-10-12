0a1f0667a9e2406c26573e68553e60722e635fc7
Fernando Serboncini <fserb@chromium.org>
2017-10-12 14:16:55 +0000

    Makes Fetcher part of ExecutionContext
    This is needed so we can share font loading code between Document and
    Worker.
    
    We need to keep the const-ness from Document's Fetcher(), since this 
    is required everywhere. Unfortunately, Worker's fetcher is lazy and
    mutable. So we have to hack around it.
    
    Bug: 722511
    Change-Id: I6b8332f0f0ac98ad892c8d57e59511abc7c23214
    Reviewed-on: https://chromium-review.googlesource.com/702055
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508344}
    Former-commit-id: 96d81513f28ee7b566740847ad97dc079245d617

--- third_party/WebKit/Source/core/workers/WorkerOrWorkletGlobalScope.h
+++ third_party/WebKit/Source/core/workers/WorkerOrWorkletGlobalScope.h
@@ -64,7 +64,8 @@ class CORE_EXPORT WorkerOrWorkletGlobalScope : public ExecutionContext {
   virtual WorkerThread* GetThread() const = 0;
 
   // Available only when off-main-thread-fetch is enabled.
-  ResourceFetcher* GetResourceFetcher();
+  ResourceFetcher* Fetcher() const override;
+  ResourceFetcher* EnsureFetcher();
 
   WorkerClients* Clients() const { return worker_clients_.Get(); }
 
