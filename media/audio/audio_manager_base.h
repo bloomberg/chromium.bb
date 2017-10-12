9ab476037505759470a77b5aaf4ba437c4ed051b
Max Morin <maxmorin@chromium.org>
2017-10-12 11:18:18 +0000

    (cros) Associate "default" input to "default" output.
    The current implementation, associating "default" input to output based
    on hardware, has unwanted consequences. Thus, we associate "default" to
    "" instead. We also override "Get{In|Out}putGroupId" in
    AudioManagerCras to make sure it's still based on the underlying
    hardware device.
    
    BUG=768792
    TBR=cychiang
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I8c7ba3b0c2ab16645352b8e32b499f71e219fcc8
    Reviewed-on: https://chromium-review.googlesource.com/688797
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Commit-Queue: Max Morin <maxmorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508311}
    Former-commit-id: ceb41ed3cea56d876f40c720c78f4f40a79bfe86

--- media/audio/audio_manager_base.h
+++ media/audio/audio_manager_base.h
@@ -159,6 +159,14 @@ class MEDIA_EXPORT AudioManagerBase : public AudioManager {
   CreateAudioDebugRecordingManager(
       scoped_refptr<base::SingleThreadTaskRunner> task_runner);
 
+  // These functions assign group ids to devices based on their device ids. The
+  // default implementation is an attempt to do this based on
+  // GetAssociatedOutputDeviceID. They may be overridden by subclasses that want
+  // a different logic for assigning group ids. Must be called on the audio
+  // worker thread (see GetTaskRunner()).
+  virtual std::string GetGroupIDOutput(const std::string& output_device_id);
+  virtual std::string GetGroupIDInput(const std::string& input_device_id);
+
  private:
   FRIEND_TEST_ALL_PREFIXES(AudioManagerTest, AudioDebugRecording);
 
@@ -169,13 +177,6 @@ class MEDIA_EXPORT AudioManagerBase : public AudioManager {
 
   // AudioManager:
   void InitializeOutputDebugRecording() final;
-
-  // These functions assign group ids to devices based on their device ids.
-  // The default implementation is an attempt to do this based on
-  // GetAssociatedOutputDeviceID. Must be called on the audio worker thread
-  // (see GetTaskRunner()).
-  std::string GetGroupIDOutput(const std::string& output_device_id);
-  std::string GetGroupIDInput(const std::string& input_device_id);
 
   // Max number of open output streams, modified by
   // SetMaxOutputStreamsAllowed().
