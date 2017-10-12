4919199a807cc475a90964a692358a920a3ad512
Xiaohui Chen <xiaohuic@google.com>
2017-10-12 20:41:24 +0000

    Add histogram to measure voice interaction duration
    BUG=b:63761029
    TEST=build and flash, watch chrome://histograms
    
    Change-Id: I7ed058f20b3346db82ef9b8d6532f0e888793d41
    Reviewed-on: https://chromium-review.googlesource.com/714483
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508451}
    Former-commit-id: d8ee494c10efc78fdb90ef6c130398bd905ced2e

--- ash/shelf/app_list_button.cc
+++ ash/shelf/app_list_button.cc
@@ -20,6 +20,7 @@
 #include "ash/system/tray/tray_popup_utils.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "base/command_line.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
 #include "base/timer/timer.h"
@@ -495,6 +496,9 @@ void AppListButton::OnVoiceInteractionStatusChanged(
 
   switch (state) {
     case ash::VoiceInteractionState::STOPPED:
+      UMA_HISTOGRAM_TIMES(
+          "VoiceInteraction.OpenDuration",
+          base::TimeTicks::Now() - voice_interaction_start_timestamp_);
       break;
     case ash::VoiceInteractionState::NOT_READY:
       // If we are showing the bursting or waiting animation, no need to do
@@ -515,6 +519,8 @@ void AppListButton::OnVoiceInteractionStatusChanged(
             base::Bind(&VoiceInteractionOverlay::HideAnimation,
                        base::Unretained(voice_interaction_overlay_)));
       }
+
+      voice_interaction_start_timestamp_ = base::TimeTicks::Now();
       break;
   }
 }
