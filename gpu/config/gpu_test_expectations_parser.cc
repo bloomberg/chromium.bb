daa53c96688f9c8a7913d3429e96c09653d4043c
Corentin Wallez <cwallez@chromium.org>
2017-10-13 21:01:03 +0000

    gpu/config: Add support for macOS High Sierra
    BUG=774309
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Iadfc74843630ef78e708412922eccd0fb2cc137a
    Reviewed-on: https://chromium-review.googlesource.com/717658
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508814}
    Former-commit-id: 443334af046a516e6bcd5517ebdd0a7803575fe0

--- gpu/config/gpu_test_expectations_parser.cc
+++ gpu/config/gpu_test_expectations_parser.cc
@@ -44,6 +44,7 @@ enum Token {
   kConfigMacYosemite,
   kConfigMacElCapitan,
   kConfigMacSierra,
+  kConfigMacHighSierra,
   kConfigMac,
   kConfigLinux,
   kConfigChromeOS,
@@ -99,6 +100,7 @@ const TokenInfo kTokenData[] = {
     {"yosemite", GPUTestConfig::kOsMacYosemite},
     {"elcapitan", GPUTestConfig::kOsMacElCapitan},
     {"sierra", GPUTestConfig::kOsMacSierra},
+    {"highsierra", GPUTestConfig::kOsMacHighSierra},
     {"mac", GPUTestConfig::kOsMac},
     {"linux", GPUTestConfig::kOsLinux},
     {"chromeos", GPUTestConfig::kOsChromeOS},
@@ -262,6 +264,7 @@ bool GPUTestExpectationsParser::ParseConfig(
       case kConfigMacYosemite:
       case kConfigMacElCapitan:
       case kConfigMacSierra:
+      case kConfigMacHighSierra:
       case kConfigMac:
       case kConfigLinux:
       case kConfigChromeOS:
@@ -322,6 +325,7 @@ bool GPUTestExpectationsParser::ParseLine(
       case kConfigMacYosemite:
       case kConfigMacElCapitan:
       case kConfigMacSierra:
+      case kConfigMacHighSierra:
       case kConfigMac:
       case kConfigLinux:
       case kConfigChromeOS:
@@ -445,6 +449,7 @@ bool GPUTestExpectationsParser::UpdateTestConfig(GPUTestConfig* config,
     case kConfigMacYosemite:
     case kConfigMacElCapitan:
     case kConfigMacSierra:
+    case kConfigMacHighSierra:
     case kConfigMac:
     case kConfigLinux:
     case kConfigChromeOS:
