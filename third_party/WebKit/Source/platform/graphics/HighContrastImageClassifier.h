13f7db605c0e693e6d286976fdab16e4d7d44ce8
Ramin Halavati <rhalavati@chromium.org>
2017-10-13 08:01:10 +0000

    Decision tree with Simple features added to HighContrast Classifier.
    A simple color buckets count feature added to the classifier. This
    feature, along with color/grayscale flag is given to a shallow, human
    comprehensible decision tree to do the classification.
    
    Bug: 685242
    Change-Id: I848c8260f66c2a3f53fe8ad0d903cc2b479df1c9
    Reviewed-on: https://chromium-review.googlesource.com/692394
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508640}
    Former-commit-id: b276eb50222e6a1125debfb76a17c5495ca65c81

--- third_party/WebKit/Source/platform/graphics/HighContrastImageClassifier.h
+++ third_party/WebKit/Source/platform/graphics/HighContrastImageClassifier.h
@@ -20,20 +20,31 @@ class PLATFORM_EXPORT HighContrastImageClassifier {
   // Decides if a high contrast filter should be applied to the image or not.
   bool ShouldApplyHighContrastFilterToImage(Image&);
 
+  bool ComputeImageFeaturesForTesting(Image& image,
+                                      std::vector<float>* features) {
+    return ComputeImageFeatures(image, features);
+  }
+
  private:
+  // Computes the features vector for a given image.
+  bool ComputeImageFeatures(Image&, std::vector<float>*);
+
   // Converts image to SkBitmap and returns true if successful.
   bool GetBitmap(Image&, SkBitmap*);
 
   // Extracts sample pixels and transparency ratio from the given bitmap.
   void GetSamples(const SkBitmap&, std::vector<SkColor>*, float*);
 
-  // Computes the features given sample pixels and transparency ratio.
+  // Computes the features, given sampled pixels and transparency ratio.
   void GetFeatures(const std::vector<SkColor>&,
                    const float,
                    std::vector<float>*);
 
   // Makes a decision about image given its features.
   HighContrastClassification ClassifyImage(const std::vector<float>&);
+
+  // Receives sampled pixels, and returns the number of color buckets.
+  int CountColorBuckets(const std::vector<SkColor>&);
 };
 
 }  // namespace blink
