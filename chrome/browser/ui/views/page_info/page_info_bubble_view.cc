fbbdf1acdfcbf68632b2d96f37d1c1bc9c9120a7
Jialiu Lin <jialiul@chromium.org>
2017-10-13 00:43:47 +0000

    Fix PhishGuard PageInfo button layout
    1. For Mac and Linux, Site is legitimate | Change password
       For Win | ChromeOs, Change password | Site is legitimate
      (Flip button order for RTL)
    2. If 2 buttons cannot fit in one line, stack them vertically
    
    Bug: 772913, 772907
    Change-Id: I2f9417b8c2b5980ae01f2eac5b7a761bbac77293
    Reviewed-on: https://chromium-review.googlesource.com/707719
    Reviewed-by: Lucas Garron <lgarron@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Jialiu Lin (Slow Oct 13-20, OOO Oct  21-29) <jialiul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508567}
    Former-commit-id: ce869eff711d30669fd41f3219a1200c9a96d711

--- chrome/browser/ui/views/page_info/page_info_bubble_view.cc
+++ chrome/browser/ui/views/page_info/page_info_bubble_view.cc
@@ -297,8 +297,6 @@ BubbleHeaderView::BubbleHeaderView(
 
   layout->StartRow(0, label_column_status);
   password_reuse_button_container_ = new views::View();
-  password_reuse_button_container_->SetLayoutManager(
-      new views::BoxLayout(views::BoxLayout::kHorizontal, gfx::Insets(), 8));
   layout->AddView(password_reuse_button_container_, 1, 1,
                   views::GridLayout::FILL, views::GridLayout::LEADING);
 
@@ -374,9 +372,34 @@ void BubbleHeaderView::AddPasswordReuseButtons() {
                                 IDS_PAGE_INFO_WHITELIST_PASSWORD_REUSE_BUTTON));
   whitelist_password_reuse_button_->set_id(
       PageInfoBubbleView::VIEW_ID_PAGE_INFO_BUTTON_WHITELIST_PASSWORD_REUSE);
+
+  int kSpacingBetweenButtons = 8;
+
+  // If these two buttons cannot fit into a single line, stack them vertically.
+  bool can_fit_in_one_line =
+      (password_reuse_button_container_->width() - kSpacingBetweenButtons) >=
+      (change_password_button_->CalculatePreferredSize().width() +
+       whitelist_password_reuse_button_->CalculatePreferredSize().width());
+  views::BoxLayout* layout =
+      new views::BoxLayout(can_fit_in_one_line ? views::BoxLayout::kHorizontal
+                                               : views::BoxLayout::kVertical,
+                           gfx::Insets(), kSpacingBetweenButtons);
+  // Make buttons left-aligned. For RTL languages, buttons will automatically
+  // become right-aligned.
+  layout->set_main_axis_alignment(views::BoxLayout::MAIN_AXIS_ALIGNMENT_START);
+  password_reuse_button_container_->SetLayoutManager(layout);
+
+#if defined(OS_WIN) || defined(OS_CHROMEOS)
   password_reuse_button_container_->AddChildView(change_password_button_);
   password_reuse_button_container_->AddChildView(
       whitelist_password_reuse_button_);
+#else
+  password_reuse_button_container_->AddChildView(
+      whitelist_password_reuse_button_);
+  password_reuse_button_container_->AddChildView(change_password_button_);
+#endif
+
+  // Add padding at the top.
   password_reuse_button_container_->SetBorder(
       views::CreateEmptyBorder(8, 0, 0, 0));
 
