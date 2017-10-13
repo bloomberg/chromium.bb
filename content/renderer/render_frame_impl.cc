43956786c2e486006331d751e421b84c13066564
Nasko Oskov <nasko@chromium.org>
2017-10-13 01:01:43 +0000

    Check weak_this after calling DidFailProvisionalLoad.
    This CL fixes an UaF bug in RenderFrameImpl::OnFailedNavigation. It is
    a followup to r503419 since DidFailProvisionalLoad calls
    LoadNavigationErrorPage internally and it should also be guarded by a
    check of weak_this.
    
    Bug: 766208, 766250
    Change-Id: Ia359607ce35ce282330a30f5f5b9c79a2656c5f3
    Reviewed-on: https://chromium-review.googlesource.com/717460
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508575}
    Former-commit-id: 45d4e94f5b511114536b8445e2327b838642ba20

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -5395,23 +5395,26 @@ void RenderFrameImpl::OnFailedNavigation(
   if (request_params.page_state.IsValid())
     history_entry = PageStateToHistoryEntry(request_params.page_state);
 
+  // The load of the error page can result in this frame being removed.
+  // Use a WeakPtr as an easy way to detect whether this has occured. If so,
+  // this method should return immediately and not touch any part of the object,
+  // otherwise it will result in a use-after-free bug.
+  base::WeakPtr<RenderFrameImpl> weak_this = weak_factory_.GetWeakPtr();
+
   // For renderer initiated navigations, we send out a didFailProvisionalLoad()
   // notification.
   bool had_provisional_document_loader = frame_->GetProvisionalDocumentLoader();
   if (request_params.nav_entry_id == 0) {
     DidFailProvisionalLoad(error, replace ? blink::kWebHistoryInertCommit
                                           : blink::kWebStandardCommit);
+    if (!weak_this)
+      return;
   }
 
   // If we didn't call didFailProvisionalLoad or there wasn't a
   // GetProvisionalDocumentLoader(), LoadNavigationErrorPage wasn't called, so
   // do it now.
-  // Note: the load of the error page can result in this frame being removed.
-  // Use a WeakPtr as an easy way to detect whether this has occured. If so,
-  // this method should return immediately and not touch any part of the object,
-  // otherwise it will result in a use-after-free bug.
   if (request_params.nav_entry_id != 0 || !had_provisional_document_loader) {
-    base::WeakPtr<RenderFrameImpl> weak_this = weak_factory_.GetWeakPtr();
     LoadNavigationErrorPage(failed_request, error, replace,
                             history_entry.get());
     if (!weak_this)
