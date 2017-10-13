ad01b1c66354449156163ce1987a3cb423d5a767
David Reveman <reveman@chromium.org>
2017-10-13 04:49:23 +0000

    Revert "Change Picture: Policy to disable user avatar videos"
    This reverts commit 3da29a7caa39d5b9855a9a279dbaa53c7752946c [formerly 0235aec2ea6e780e2712a45b6d31156a64a0b370].
    
    Reason for revert: Accidentally disabled motion stills by default
    and not clear if we want to add this policy or not.
    
    Original change's description:
    > Change Picture: Policy to disable user avatar videos
    > 
    > Adds a policy to control whether recording of videos for user
    > avatar should be allowed.
    > 
    > Bug: 763735
    > Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    > Change-Id: I30ba35f867533acb7431914b7eea7e9bf443b36b
    > Reviewed-on: https://chromium-review.googlesource.com/704199
    > Commit-Queue: David Reveman <reveman@chromium.org>
    > Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    > Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#507891}
    
    TBR=stevenjb@chromium.org,reveman@chromium.org,emaxx@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 763735
    Change-Id: I8b1c14259c563593854e28ac82c5bf4ee2e9597b
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Reviewed-on: https://chromium-review.googlesource.com/716343
    Reviewed-by: David Reveman <reveman@chromium.org>
    Commit-Queue: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508594}
    Former-commit-id: 0ab69aae8867b2ed55c6634acae3c0519e58ca38

--- chrome/browser/ui/webui/chromeos/login/user_image_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/user_image_screen_handler.cc
@@ -39,11 +39,7 @@ const char kJsScreenPath[] = "login.UserImageScreen";
 namespace chromeos {
 
 UserImageScreenHandler::UserImageScreenHandler()
-    : BaseScreenHandler(kScreenId),
-      device_settings_observer_(CrosSettings::Get()->AddSettingsObserver(
-          kAllowUserAvatarVideos,
-          base::Bind(&UserImageScreenHandler::UpdateAllowVideoMode,
-                     base::Unretained(this)))) {
+    : BaseScreenHandler(kScreenId) {
   set_call_js_prefix(kJsScreenPath);
   ui::ResourceBundle& bundle = ui::ResourceBundle::GetSharedInstance();
   media::SoundsManager* manager = media::SoundsManager::Get();
@@ -127,7 +123,6 @@ void UserImageScreenHandler::HandleGetImages() {
       default_user_image::GetAsDictionary(true /* all */);
   result.Set("images", std::move(default_images));
   CallJS("setDefaultImages", result);
-  UpdateAllowVideoMode();
 }
 
 void UserImageScreenHandler::HandleScreenReady() {
@@ -175,12 +170,6 @@ void UserImageScreenHandler::HandleScreenShown() {
 
 void UserImageScreenHandler::HideCurtain() {
   CallJS("hideCurtain");
-}
-
-void UserImageScreenHandler::UpdateAllowVideoMode() {
-  bool allow_video_mode = false;
-  CrosSettings::Get()->GetBoolean(kAllowUserAvatarVideos, &allow_video_mode);
-  CallJS("setAllowVideoMode", base::Value(allow_video_mode));
 }
 
 }  // namespace chromeos
--- chrome/browser/ui/webui/chromeos/login/user_image_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/user_image_screen_handler.h
@@ -11,7 +11,6 @@
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "chrome/browser/chromeos/login/screens/user_image_view.h"
-#include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chrome/browser/ui/webui/chromeos/login/base_screen_handler.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/native_widget_types.h"
@@ -64,9 +63,6 @@ class UserImageScreenHandler : public UserImageView, public BaseScreenHandler {
   // Called when the user image screen has been loaded and shown.
   void HandleScreenShown();
 
-  // Called when video mode allowance changed.
-  void UpdateAllowVideoMode();
-
   UserImageScreen* screen_ = nullptr;
 
   // Keeps whether screen should be shown right after initialization.
@@ -76,8 +72,6 @@ class UserImageScreenHandler : public UserImageView, public BaseScreenHandler {
   bool is_ready_ = false;
 
   base::Time screen_show_time_;
-
-  std::unique_ptr<CrosSettings::ObserverSubscription> device_settings_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(UserImageScreenHandler);
 };
