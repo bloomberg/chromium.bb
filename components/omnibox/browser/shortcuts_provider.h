5405023e5352c1b6d86dcaa17f80d566e1a1cd1e
Gheorghe Comanici <gcomanici@chromium.org>
2017-10-13 17:38:44 +0000

    Fix match classification of queries from ShortcutsProvider
    This CL adds consistency between match classification for query
    suggestions coming from ShortcutsProvider and query suggestions
    coming from SearchProvider. Both of them are now bolding the non-matching
    component of a prefix-match.
    
    Bug: 341139
    Change-Id: I0020e6cc1236c7915e64a0520820400eae001082
    Reviewed-on: https://chromium-review.googlesource.com/713762
    Commit-Queue: Gheorghe Comanici <gcomanici@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508744}
    Former-commit-id: 15b9b104009576d92e62fc087e1460e74670c804

--- components/omnibox/browser/shortcuts_provider.h
+++ components/omnibox/browser/shortcuts_provider.h
@@ -47,21 +47,34 @@ class ShortcutsProvider : public AutocompleteProvider,
   // equal_range().
   static WordMap CreateWordMapForString(const base::string16& text);
 
-  // Given |text| and a corresponding base set of classifications
-  // |original_class|, adds ACMatchClassification::MATCH markers for all
-  // instances of the words from |find_words| within |text| and returns the
-  // resulting classifications.  (|find_text| is provided as the original string
-  // used to create |find_words|.  This is supplied because it's common for this
-  // to be a prefix of |text|, so we can quickly check for that and mark that
-  // entire substring as a match before proceeding with the more generic
-  // algorithm.)
+  // Finds all instances of the words from |find_words| within |text|, adds
+  // classifications to |original_class| according to the logic described below,
+  // and returns the result.
   //
-  // For example, given the |text|
-  // "Sports and News at sports.somesite.com - visit us!" and |original_class|
-  // {{0, NONE}, {18, URL}, {37, NONE}} (marking "sports.somesite.com" as a
-  // URL), calling with |find_text| set to "sp ew" would return
-  // {{0, MATCH}, {2, NONE}, {12, MATCH}, {14, NONE}, {18, URL|MATCH},
-  // {20, URL}, {37, NONE}}.
+  //   - if |text_is_search_query| is false, the function adds
+  //   ACMatchClassification::MATCH markers for all such instances.
+  //
+  //   For example, given the |text|
+  //   "Sports and News at sports.somesite.com - visit us!" and |original_class|
+  //   {{0, NONE}, {18, URL}, {37, NONE}} (marking "sports.somesite.com" as a
+  //   URL), calling with |find_text| set to "sp ew" would return
+  //   {{0, MATCH}, {2, NONE}, {12, MATCH}, {14, NONE}, {18, URL|MATCH},
+  //   {20, URL}, {37, NONE}}.
+  //
+  //
+  //   - if |text_is_search_query| is true, applies the same logic, but uses
+  //   NONE for the matching text and MATCH for the non-matching text. This is
+  //   done to mimic the behavior of SearchProvider which decorates matches
+  //   according to the approach used by Google Suggest.
+  //
+  //   For example, given that |text| corresponds to a search query "panama
+  //   canal" and |original class| is {{0, NONE}}, calling with |find_text| set
+  //   to "canal" would return {{0,MATCH}, {7, NONE}}.
+  //
+  // |find_text| is provided as the original string used to create
+  // |find_words|.  This is supplied because it's common for this to be a prefix
+  // of |text|, so we can quickly check for that and mark that entire substring
+  // as a match before proceeding with the more generic algorithm.
   //
   // |find_words| should be as constructed by CreateWordMapForString(find_text).
   //
@@ -71,6 +84,7 @@ class ShortcutsProvider : public AutocompleteProvider,
       const base::string16& find_text,
       const WordMap& find_words,
       const base::string16& text,
+      const bool text_is_search_query,
       const ACMatchClassifications& original_class);
 
   // ShortcutsBackendObserver:
