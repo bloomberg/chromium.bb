1104dabb17ad2891aace4f78a161e2bd648c05ba
chaopeng <chaopeng@chromium.org>
2017-10-13 20:30:03 +0000

    Invalid viewport meta width/height value should fall back to auto.
    Currently invalid viewport meta width/height value fall back to 0.0.
    But fall back to auto make more sense to developer.
    
    Bug: 773653
    Change-Id: I3a9243a171101cfdbd10f9f71c9fe7288b44d7c7
    Reviewed-on: https://chromium-review.googlesource.com/713876
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Jianpeng Chao <chaopeng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508800}
    Former-commit-id: 145660d6a05041e0da6a412598a29741b9efe816

--- third_party/WebKit/Source/core/html/HTMLMetaElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLMetaElement.cpp
@@ -179,15 +179,20 @@ Length HTMLMetaElement::ParseViewportValueAsLength(Document* document,
   // 1) Non-negative number values are translated to px lengths.
   // 2) Negative number values are translated to auto.
   // 3) device-width and device-height are used as keywords.
-  // 4) Other keywords and unknown values translate to 0.0.
+  // 4) Other keywords and unknown values translate to auto.
 
   if (DeprecatedEqualIgnoringCase(value_string, "device-width"))
     return Length(kDeviceWidth);
   if (DeprecatedEqualIgnoringCase(value_string, "device-height"))
     return Length(kDeviceHeight);
 
-  float value =
-      ParsePositiveNumber(document, report_warnings, key_string, value_string);
+  bool ok;
+
+  float value = ParsePositiveNumber(document, report_warnings, key_string,
+                                    value_string, &ok);
+
+  if (!ok)
+    return Length();  // auto
 
   if (value < 0)
     return Length();  // auto
