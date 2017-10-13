ef9c0254ad8cbda03d79fbbde57d88ed407147b5
Mythri Alle <mythria@chromium.org>
2017-10-13 13:42:58 +0000

    Add GetExpectations class to v8.browsing* benchmarks
    v8.browsing_desktop, v8.browsing_mobile, v8.runtimestats.browsing_desktop
    were using the expectations from system_health/expectations.py file. This
    cl moves them to their own class.
    
    Bug: chromium:773077
    Change-Id: I049361516407455016ee11558d1d2eb9d8e57abc
    Reviewed-on: https://chromium-review.googlesource.com/716678
    Reviewed-by: rnephew <rnephew@chromium.org>
    Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Mythri Alle <mythria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508690}
    Former-commit-id: 2aff6db2e54003949511454f195acf42c3ae357d

--- tools/perf/page_sets/system_health/expectations.py
+++ tools/perf/page_sets/system_health/expectations.py
@@ -117,48 +117,6 @@ class SystemHealthMobileMemoryExpectations(expectations.StoryExpectations):
                       'Webview does not have tabs')
 
 
-# Should only include browse:*:* stories.
-class V8BrowsingDesktopExpecations(expectations.StoryExpectations):
-  def SetExpectations(self):
-    self.DisableStory('browse:news:hackernews',
-                      [expectations.ALL_WIN, expectations.ALL_MAC],
-                      'crbug.com/676336')
-    self.DisableStory('browse:news:reddit', [expectations.ALL_DESKTOP],
-                      'crbug.com/759777')
-    self.DisableStory('browse:news:cnn', [expectations.ALL_DESKTOP],
-                      'mac:crbug.com/728576, all:crbug.com/759777')
-    self.DisableStory('browse:tools:maps', [expectations.ALL_MAC],
-                      'crbug.com/773084')
-
-
-# Should only include browse:*:* stories.
-class V8BrowsingMobileExpecations(expectations.StoryExpectations):
-  def SetExpectations(self):
-    self.DisableStory('browse:shopping:flipkart', [expectations.ALL_ANDROID],
-                      'crbug.com/708300')
-    self.DisableStory('browse:news:cnn', [expectations.ANDROID_NEXUS5X],
-                      'crbug.com/714650')
-    self.DisableStory('browse:news:globo', [expectations.ALL_ANDROID],
-                      'crbug.com/714650')
-    self.DisableStory('browse:news:toi', [expectations.ALL_ANDROID],
-                      'crbug.com/728081')
-    # TODO(rnephew): This disabling should move to CanRunOnBrowser.
-    self.DisableStory('browse:chrome:omnibox',
-                      [expectations.ANDROID_WEBVIEW],
-                      'Webview does not have omnibox')
-    # TODO(rnephew): This disabling should move to CanRunOnBrowser.
-    self.DisableStory('browse:chrome:newtab',
-                      [expectations.ANDROID_WEBVIEW],
-                      'Webview does not have NTP')
-    self.DisableStory('browse:news:cnn',
-                      [expectations.ANDROID_WEBVIEW],
-                      'Crash: crbug.com/767595')
-    self.DisableStory('browse:news:toi',
-                      [expectations.ANDROID_NEXUS5X,
-                       expectations.ANDROID_NEXUS6],
-                      'Crash: crbug.com/770920')
-
-
 class SystemHealthWebviewStartupExpectations(expectations.StoryExpectations):
   def SetExpectations(self):
     pass
