c5e20f4a041b455d23b31755be16ad39cf4b0674
Mitsuru Oshima <oshima@chromium.org>
2017-10-13 23:38:13 +0000

    Fix mash crash in ArcRotationLockBridge
    BUG=774713
    
    Change-Id: I70179391de854c57c9b7249d5bdab2557f63e7a7
    Reviewed-on: https://chromium-review.googlesource.com/719430
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508867}
    Former-commit-id: addcbedae6fd53756ae328e4f491e6fcc6958cdd

--- components/arc/rotation_lock/arc_rotation_lock_bridge.cc
+++ components/arc/rotation_lock/arc_rotation_lock_bridge.cc
@@ -48,13 +48,17 @@ ArcRotationLockBridge::ArcRotationLockBridge(content::BrowserContext* context,
                                              ArcBridgeService* bridge_service)
     : arc_bridge_service_(bridge_service) {
   arc_bridge_service_->rotation_lock()->AddObserver(this);
-  ash::Shell::Get()->screen_orientation_controller()->AddObserver(this);
-  ash::Shell::Get()->tablet_mode_controller()->AddObserver(this);
+  // TODO(mash): Support this functionality without ash::Shell access in Chrome.
+  if (ash::Shell::HasInstance()) {
+    ash::Shell::Get()->screen_orientation_controller()->AddObserver(this);
+    ash::Shell::Get()->tablet_mode_controller()->AddObserver(this);
+  }
 }
 
 ArcRotationLockBridge::~ArcRotationLockBridge() {
   arc_bridge_service_->rotation_lock()->RemoveObserver(this);
-  // TODO(mash): mash needs proper shutdown process.
+  // TODO(mus): mus needs proper shutdown process.
+  // TODO(mash): Support this functionality without ash::Shell access in Chrome.
   if (ash::Shell::HasInstance()) {
     ash::Shell::Get()->screen_orientation_controller()->RemoveObserver(this);
     ash::Shell::Get()->tablet_mode_controller()->RemoveObserver(this);
@@ -78,6 +82,10 @@ void ArcRotationLockBridge::OnTabletModeEnded() {
 }
 
 void ArcRotationLockBridge::SendRotationLockState() {
+  // TODO(mash): Support this functionality without ash::Shell access in Chrome.
+  if (!ash::Shell::HasInstance())
+    return;
+
   mojom::RotationLockInstance* rotation_lock_instance =
       ARC_GET_INSTANCE_FOR_METHOD(arc_bridge_service_->rotation_lock(),
                                   OnRotationLockStateChanged);
