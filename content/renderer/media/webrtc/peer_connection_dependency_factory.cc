bc7810bf9ed742c1443ab8ab141679ff2be0622f
Karl Wiberg <kwiberg@chromium.org>
2017-10-13 09:06:28 +0000

    Explicitly list the WebRTC audio codecs we want to use
    Instead of just taking the pre-packaged bargain deal that the WebRTC
    project maintains.
    
    This CL attempts to exactly duplicate the set of codecs that were
    included previously.
    
    BUG=webrtc:8343
    
    Change-Id: I4d4f82b2ed4a204b236ae850d2c6750fa5bf7fd7
    Reviewed-on: https://chromium-review.googlesource.com/712596
    Reviewed-by: Dimitri Glazkov <dglazkov@chromium.org>
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Commit-Queue: Karl Wiberg <kwiberg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508649}
    Former-commit-id: 8bec2ccaf446a1f98c0474b8b9516ab9447b5528

--- content/renderer/media/webrtc/peer_connection_dependency_factory.cc
+++ content/renderer/media/webrtc/peer_connection_dependency_factory.cc
@@ -36,6 +36,7 @@
 #include "content/renderer/media/media_stream_video_source.h"
 #include "content/renderer/media/media_stream_video_track.h"
 #include "content/renderer/media/rtc_peer_connection_handler.h"
+#include "content/renderer/media/webrtc/audio_codec_factory.h"
 #include "content/renderer/media/webrtc/stun_field_trial.h"
 #include "content/renderer/media/webrtc/webrtc_video_capturer_adapter.h"
 #include "content/renderer/media/webrtc_audio_device_impl.h"
@@ -61,8 +62,6 @@
 #include "third_party/WebKit/public/platform/WebURL.h"
 #include "third_party/WebKit/public/web/WebDocument.h"
 #include "third_party/WebKit/public/web/WebLocalFrame.h"
-#include "third_party/webrtc/api/audio_codecs/builtin_audio_decoder_factory.h"
-#include "third_party/webrtc/api/audio_codecs/builtin_audio_encoder_factory.h"
 #include "third_party/webrtc/api/mediaconstraintsinterface.h"
 #include "third_party/webrtc/api/videosourceproxy.h"
 #include "third_party/webrtc/modules/video_coding/codecs/h264/include/h264.h"
@@ -255,9 +254,8 @@ void PeerConnectionDependencyFactory::InitializeSignalingThread(
 
   pc_factory_ = webrtc::CreatePeerConnectionFactory(
       worker_thread_, signaling_thread_, audio_device_.get(),
-      webrtc::CreateBuiltinAudioEncoderFactory(),
-      webrtc::CreateBuiltinAudioDecoderFactory(), encoder_factory.release(),
-      decoder_factory.release());
+      CreateWebrtcAudioEncoderFactory(), CreateWebrtcAudioDecoderFactory(),
+      encoder_factory.release(), decoder_factory.release());
   CHECK(pc_factory_.get());
 
   webrtc::PeerConnectionFactoryInterface::Options factory_options;
