236f6c84266e28c901b02581bd9a301f9b5e4214
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2017-10-13 21:05:13 +0000

    Announce all InfoBars
    For a normal infobar, an announcement was made only when a proper TextView
    (R.id.infobar_message) exists in it. Therefore, it was hard to guarantee
    all infobars are easily discoverable for accessibility users.
    
    Now all the infobars are announced.
    
    Change-Id: I5322815fe13f43e7028eb07e7a37147ad94c0c23
    Reviewed-on: https://chromium-review.googlesource.com/719398
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508816}
    Former-commit-id: 914e2fe282d3e975a699a9bf40808ade97a480c3

--- chrome/android/java/src/org/chromium/chrome/browser/infobar/InfoBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/infobar/InfoBar.java
@@ -103,7 +103,10 @@ public abstract class InfoBar implements InfoBarView {
         return mView;
     }
 
-    /** If this returns true, the infobar contents will be replaced with a one-line layout. */
+    /**
+     * If this returns true, the infobar contents will be replaced with a one-line layout.
+     * When overriding this, also override {@link #getAccessibilityMessage}.
+     */
     protected boolean usesCompactLayout() {
         return false;
     }
@@ -134,6 +137,8 @@ public abstract class InfoBar implements InfoBarView {
 
     /**
      * Returns the accessibility message to announce when this infobar is first shown.
+     * Override this if the InfoBar doesn't have {@link R.id.infobar_message}. It is usually the
+     * case when it is in CompactLayout.
      */
     protected CharSequence getAccessibilityMessage(CharSequence defaultTitle) {
         return defaultTitle == null ? "" : defaultTitle;
@@ -144,14 +149,12 @@ public abstract class InfoBar implements InfoBarView {
         if (mView == null) return "";
 
         CharSequence title = null;
-        if (usesCompactLayout()) {
-            title = getAccessibilityMessage(title);
-        } else {
-            // For normal infobar, make an announcement only when a proper TextView is assigned.
-            TextView messageView = (TextView) mView.findViewById(R.id.infobar_message);
-            if (messageView == null) return "";
-            title = getAccessibilityMessage(messageView.getText());
+        TextView messageView = (TextView) mView.findViewById(R.id.infobar_message);
+        if (messageView != null) {
+            title = messageView.getText();
         }
+        title = getAccessibilityMessage(title);
+        // TODO(crbug/773717): Avoid string concatenation due to i18n.
         return title + mContext.getString(R.string.bottom_bar_screen_position);
     }
 
