a9bac57ce6c9d390a52ebaad3259f5fdb871210e
Chris Nardi <hichris123@gmail.com>
2017-10-14 16:47:23 -0400

    Update English Hunspell dictionaries
    Update English Hunspell dictionaries from upstream (SCOWL). Add back in en_AU.dic_delta (copy of en_GB.dic_delta) as SCOWL added en_AU dictionary. Bump version of all dictionaries to 8.0.
    
    Bug:774804
    Change-Id: Idc4dc4364641a56315a31c5c143fcf4eb1f828f8

--- update_dictionaries.py
+++ update_dictionaries.py
@@ -16,25 +16,31 @@ def main():
   if not os.getcwd().endswith("hunspell_dictionaries"):
     print "Please run this file from the hunspell_dictionaries directory"
   dictionaries = (
-      ("http://downloads.sourceforge.net/project/wordlist/speller/2016.01.19/"
-       "hunspell-en_US-2016.01.19.zip",
+      ("https://sourceforge.net/projects/wordlist/files/speller/2017.01.22/"
+       "hunspell-en_US-2017.01.22.zip",
        "en_US.zip"),
-      ("http://downloads.sourceforge.net/project/wordlist/speller/2016.01.19/"
-       "hunspell-en_CA-2016.01.19.zip",
+      ("https://sourceforge.net/projects/wordlist/files/speller/2017.01.22/"
+       "hunspell-en_CA-2017.01.22.zip",
        "en_CA.zip"),
-      ("http://downloads.sourceforge.net/project/wordlist/speller/2016.01.19/"
-       "hunspell-en_GB-ise-2016.01.19.zip",
+      ("https://sourceforge.net/projects/wordlist/files/speller/2017.01.22/"
+       "hunspell-en_GB-ise-2017.01.22.zip",
        "en_GB.zip"),
+      ("https://sourceforge.net/projects/wordlist/files/speller/2017.01.22/"
+       "hunspell-en_AU-2017.01.22.zip",
+       "en_AU.zip"),
       ("http://downloads.sourceforge.net/lilak/"
        "lilak_fa-IR_2-1.zip",
        "fa_IR.zip")
   )
   for pair in dictionaries:
-    urllib.urlretrieve(pair[0], pair[1])
-    ZipFile(pair[1]).extractall()
+    url = pair[0]
+    file_name = pair[1]
+
+    urllib.urlretrieve(url, file_name)
+    ZipFile(file_name).extractall()
     for name in glob.glob("*en_GB-ise*"):
       os.rename(name, name.replace("-ise", ""))
-    os.remove(pair[1])
+    os.remove(file_name)
   return 0
 
 if __name__ == "__main__":
