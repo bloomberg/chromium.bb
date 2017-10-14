ab1ffdf9ccc314a75fc2603bfba27933d20a48fb
Will Chen <chenwilliam@chromium.org>
2017-10-14 06:11:47 +0000

    DevTools: re-use new test runner modules in legacy test helpers (part 1)
    This eliminates a lot of the code duplication between the new and legacy
    integration test frameworks by making the legacy test helper files small
    wrappers to load the new *test_runner modules.
    
    * syntax-highlight.js - no longer needed because it's in TestRunner.js
    * Line numbers changed in indexed DB tests because inspected page helper
    functions are executed in anonymous scripts with initAsync
    
    Drive-by: This also removes unused files in http/tests/inspector
    that are no longer needed now that the tests have all been moved to
    http/tests/devtools.
    
    Bug: 667560
    Change-Id: Ifc6655dbf76d9b56d6e6de0fff3103a533cf6e31
    Reviewed-on: https://chromium-review.googlesource.com/719711
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#508931}
    Former-commit-id: 1c2edfba58aea9a316c796649fb932636a491932

--- third_party/WebKit/LayoutTests/http/tests/inspector/live-edit-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/live-edit-test.js
@@ -1,27 +1,3 @@
 var initialize_LiveEditTest = function() {
-
-InspectorTest.replaceInSource = function(sourceFrame, string, replacement)
-{
-    sourceFrame._textEditor.setReadOnly(false);
-    for (var i = 0; i < sourceFrame._textEditor.linesCount; ++i) {
-        var line = sourceFrame._textEditor.line(i);
-        var column = line.indexOf(string);
-        if (column === -1)
-            continue;
-        range = new TextUtils.TextRange(i, column, i, column + string.length);
-        var newRange = sourceFrame._textEditor.editRange(range, replacement);
-        break;
-    }
-}
-
-InspectorTest.commitSource = function(sourceFrame)
-{
-    sourceFrame.commitEditing();
-}
-
-InspectorTest.undoSourceEditing = function(sourceFrame)
-{
-    sourceFrame._textEditor.undo();
-}
-
+    InspectorTest.preloadModule('sources_test_runner');
 };
