ba396ab355dfcb33bb585029f2df8af63e202820
Francois Doray <fdoray@chromium.org>
2017-10-16 15:51:47 +0000

    Return ThreadTaskRunnerHandle::Get() last in SequencedTaskRunnerHandle::Get().
    SequencedTaskRunnerHandle::Get() temporarily always returned
    ThreadTaskRunnerHandle::Get() when ThreadTaskRunnerHandle::IsSet() was
    true. This avoided a crash in RLZ.
    
    Now that the RLZ code is fixed, this CL brings back
    SequencedTaskRunnerHandle::Get() to its old state (i.e. only return
    ThreadTaskRunnerHandle::Get() if no handle is stored in TLS and
    if the call is not made from a SequencedWorkerPool).
    
    Bug: 618530
    Change-Id: I59ac8a9e92162655ec5e151892bd222e0c3045cd
    Reviewed-on: https://chromium-review.googlesource.com/721063
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509061}
    Former-commit-id: ff31ca8db7305be4cd4bb545dec1cef743c45e75

--- base/threading/sequenced_task_runner_handle.cc
+++ base/threading/sequenced_task_runner_handle.cc
@@ -23,21 +23,6 @@ LazyInstance<ThreadLocalPointer<SequencedTaskRunnerHandle>>::Leaky
 
 // static
 scoped_refptr<SequencedTaskRunner> SequencedTaskRunnerHandle::Get() {
-  // Return the registered SingleThreadTaskRunner, if any. This must be at the
-  // top so that a SingleThreadTaskRunner has priority over a
-  // SequencedTaskRunner (RLZ registers both on the same thread despite that
-  // being prevented by DCHECKs).
-  // TODO(fdoray): Move this to the bottom once RLZ stops registering a
-  // SingleThreadTaskRunner and a SequencedTaskRunner on the same thread.
-  // https://crbug.com/618530#c14
-  if (ThreadTaskRunnerHandle::IsSet()) {
-    // Various modes of setting SequencedTaskRunnerHandle don't combine.
-    DCHECK(!lazy_tls_ptr.Pointer()->Get());
-    DCHECK(!SequencedWorkerPool::GetSequenceTokenForCurrentThread().IsValid());
-
-    return ThreadTaskRunnerHandle::Get();
-  }
-
   // Return the registered SequencedTaskRunner, if any.
   const SequencedTaskRunnerHandle* handle = lazy_tls_ptr.Pointer()->Get();
   if (handle) {
@@ -47,22 +32,26 @@ scoped_refptr<SequencedTaskRunner> SequencedTaskRunnerHandle::Get() {
     return handle->task_runner_;
   }
 
-  // If we are on a worker thread for a SequencedBlockingPool that is running a
+  // If we are on a worker thread for a SequencedWorkerPool that is running a
   // sequenced task, return a SequencedTaskRunner for it.
   scoped_refptr<SequencedWorkerPool> pool =
       SequencedWorkerPool::GetWorkerPoolForCurrentThread();
-  // Note if you hit this: the problem isn't the lack of a |pool|, it's the lack
-  // of a sequenced context above. The |pool| is just the last desperate attempt
-  // at finding such a context from the deprecated SequencedWorkerPool.
-  CHECK(pool) << "Error: This caller requires a sequenced context (i.e. the "
-                 "current task needs to run from a SequencedTaskRunner).";
-  SequencedWorkerPool::SequenceToken sequence_token =
-      SequencedWorkerPool::GetSequenceTokenForCurrentThread();
-  DCHECK(sequence_token.IsValid());
-  scoped_refptr<SequencedTaskRunner> sequenced_task_runner(
-      pool->GetSequencedTaskRunner(sequence_token));
-  DCHECK(sequenced_task_runner->RunsTasksInCurrentSequence());
-  return sequenced_task_runner;
+  if (pool) {
+    SequencedWorkerPool::SequenceToken sequence_token =
+        SequencedWorkerPool::GetSequenceTokenForCurrentThread();
+    DCHECK(sequence_token.IsValid());
+    scoped_refptr<SequencedTaskRunner> sequenced_task_runner(
+        pool->GetSequencedTaskRunner(sequence_token));
+    DCHECK(sequenced_task_runner->RunsTasksInCurrentSequence());
+    return sequenced_task_runner;
+  }
+
+  // Note if you hit this: the problem is the lack of a sequenced context. The
+  // ThreadTaskRunnerHandle is just the last attempt at finding such a context.
+  CHECK(ThreadTaskRunnerHandle::IsSet())
+      << "Error: This caller requires a sequenced context (i.e. the "
+         "current task needs to run from a SequencedTaskRunner).";
+  return ThreadTaskRunnerHandle::Get();
 }
 
 // static
