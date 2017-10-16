4181b4b4f5e374588326a07c047d4c3111fe7d00
Florent Castelli <orphis@chromium.org>
2017-10-16 12:06:59 +0000

    Add missing sequence check to remaining MSVCS function.
    Bug: 739393
    Change-Id: I923995bda4576ab921f1d122eacb663ba8568873
    Reviewed-on: https://chromium-review.googlesource.com/716484
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Florent Castelli <orphis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509024}
    Former-commit-id: 5aa3813736e571bd3091a89e4a401eca1082a0d6

--- content/renderer/media/media_stream_video_capturer_source.cc
+++ content/renderer/media/media_stream_video_capturer_source.cc
@@ -224,6 +224,7 @@ void MediaStreamVideoCapturerSource::StopSourceImpl() {
 }
 
 void MediaStreamVideoCapturerSource::StopSourceForRestartImpl() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   if (state_ != STARTED) {
     OnStopForRestartDone(false);
     return;
