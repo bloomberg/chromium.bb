4c002be4e0dae07bc09163785134d00157749f5a
Ned Nguyen <nednguyen@google.com>
2017-10-16 16:22:49 +0000

    Remove oortonline benchmark
    Bug: 774456
    Change-Id: I667eb5540bf81a0d5c33f0c269b7757d7bba068f
    Reviewed-on: https://chromium-review.googlesource.com/718776
    Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#509071}
    Former-commit-id: 3d373b4c6a5b7aaa56682e0668c89e9897b8634d

--- tools/perf/benchmarks/oortonline.py
+++ tools/perf/benchmarks/oortonline.py
@@ -9,52 +9,10 @@ import page_sets
 from core import perf_benchmark
 from telemetry import benchmark
 from telemetry import story
-from telemetry.page import legacy_page_test
-from telemetry.value import scalar
-from telemetry.value import improvement_direction
 from telemetry.timeline import chrome_trace_category_filter
 from telemetry.timeline import chrome_trace_config
 from telemetry.web_perf import timeline_based_measurement
 
-
-class _OortOnlineMeasurement(legacy_page_test.LegacyPageTest):
-
-  def __init__(self):
-    super(_OortOnlineMeasurement, self).__init__()
-
-  def ValidateAndMeasurePage(self, page, tab, results):
-    del page  # unused
-    tab.WaitForJavaScriptCondition('window.benchmarkFinished', timeout=1000)
-    scores = tab.EvaluateJavaScript('window.benchmarkScore')
-    for score in scores:
-      valid = score['valid']
-      if valid:
-        results.AddValue(scalar.ScalarValue(
-            results.current_page, score['name'], 'score', score['score'],
-            important=True, improvement_direction=improvement_direction.UP))
-
-
-@benchmark.Owner(emails=['ulan@chromium.org'])
-class OortOnline(perf_benchmark.PerfBenchmark):
-  """OortOnline benchmark that measures WebGL and V8 performance.
-  URL: http://oortonline.gl/#run
-  Info: http://v8project.blogspot.de/2015/10/jank-busters-part-one.html
-  """
-  test = _OortOnlineMeasurement
-  SUPPORTED_PLATFORMS = [story.expectations.ALL_DESKTOP]
-
-  @classmethod
-  def Name(cls):
-    return 'oortonline'
-
-  def CreateStorySet(self, options):
-    return page_sets.OortOnlinePageSet()
-
-  def GetExpectations(self):
-    class StoryExpectations(story.expectations.StoryExpectations):
-      def SetExpectations(self):
-        pass
-    return StoryExpectations()
 
 @benchmark.Owner(emails=['ulan@chromium.org'])
 class OortOnlineTBMv2(perf_benchmark.PerfBenchmark):
