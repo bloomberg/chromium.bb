15ddfc9a4cebe1ae54f1ac20e245deeaac94a2f6
Darren Shen <shend@chromium.org>
2017-10-17 10:44:55 +0000

    Prevent counter values from over/underflowing.
    Currently we over/underflow if a counter value goes beyond the limits
    of a 32 bit integer. The spec [1] says we can ignore the increment
    if it would cause an over/underflow. This patch implements this
    behaviour.
    
    Firefox follows the spec in this.
    
    [1] https://drafts.csswg.org/css-lists-3/#valdef-counter-reset-custom-ident-integer
    
    Bug: 655473
    Change-Id: I6e10b6e4f5f672d723a0ccb69fd309d52d1d1204
    Reviewed-on: https://chromium-review.googlesource.com/708105
    Reviewed-by: Justin Schuh <jschuh@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509351}
    Former-commit-id: e18ac62b00618f26ef125554dd8241b2aed711e9

--- third_party/WebKit/Source/core/layout/CounterNode.cpp
+++ third_party/WebKit/Source/core/layout/CounterNode.cpp
@@ -22,6 +22,7 @@
 #include "core/layout/CounterNode.h"
 
 #include "core/layout/LayoutCounter.h"
+#include "platform/wtf/CheckedNumeric.h"
 
 #ifndef NDEBUG
 #include <stdio.h>
@@ -142,11 +143,17 @@ CounterNode* CounterNode::PreviousInPreOrder() const {
 }
 
 int CounterNode::ComputeCountInParent() const {
+  // According to the spec, if an increment would overflow or underflow the
+  // counter, we are allowed to ignore the increment.
+  // https://drafts.csswg.org/css-lists-3/#valdef-counter-reset-custom-ident-integer
   int increment = ActsAsReset() ? 0 : value_;
-  if (previous_sibling_)
-    return previous_sibling_->count_in_parent_ + increment;
+  if (previous_sibling_) {
+    return WTF::CheckAdd(previous_sibling_->count_in_parent_, increment)
+        .ValueOrDefault(previous_sibling_->count_in_parent_);
+  }
   DCHECK_EQ(parent_->first_child_, this);
-  return parent_->value_ + increment;
+  return WTF::CheckAdd(parent_->value_, increment)
+      .ValueOrDefault(parent_->value_);
 }
 
 void CounterNode::AddLayoutObject(LayoutCounter* value) {
