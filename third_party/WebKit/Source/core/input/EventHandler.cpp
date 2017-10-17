667463c97a47b2be0d66bbe355a9fb081fc09ade
Hugo Holgersson <hugoh@vewd.com>
2017-10-17 17:57:35 +0000

    Unit test that contenteditable links show an I-beam mouse cursor
    Bug: 774058
    Change-Id: I05e14895d59609144b76264f04d081ee8657caeb
    Reviewed-on: https://chromium-review.googlesource.com/718936
    Commit-Queue: Hugo Holgersson <hugoh@vewd.com>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509430}
    Former-commit-id: 39ddbd6c77f761e13a38bf4d9409452e1ca3b7f3

--- third_party/WebKit/Source/core/input/EventHandler.cpp
+++ third_party/WebKit/Source/core/input/EventHandler.cpp
@@ -140,25 +140,6 @@ static const double kMinimumCursorScale = 0.001;
 // This is roughly 9 frames, which should be long enough to be noticeable.
 constexpr TimeDelta kMinimumActiveInterval = TimeDelta::FromSecondsD(0.15);
 
-enum NoCursorChangeType { kNoCursorChange };
-
-class OptionalCursor {
- public:
-  OptionalCursor(NoCursorChangeType) : is_cursor_change_(false) {}
-  OptionalCursor(const Cursor& cursor)
-      : is_cursor_change_(true), cursor_(cursor) {}
-
-  bool IsCursorChange() const { return is_cursor_change_; }
-  const Cursor& GetCursor() const {
-    DCHECK(is_cursor_change_);
-    return cursor_;
-  }
-
- private:
-  bool is_cursor_change_;
-  Cursor cursor_;
-};
-
 EventHandler::EventHandler(LocalFrame& frame)
     : frame_(frame),
       selection_controller_(SelectionController::Create(frame)),
@@ -363,7 +344,7 @@ void EventHandler::UpdateCursor() {
   layout_view_item.HitTest(result);
 
   if (LocalFrame* frame = result.InnerNodeFrame()) {
-    OptionalCursor optional_cursor =
+    EventHandler::OptionalCursor optional_cursor =
         frame->GetEventHandler().SelectCursor(result);
     if (optional_cursor.IsCursorChange()) {
       view->SetCursor(optional_cursor.GetCursor());
@@ -410,7 +391,8 @@ bool EventHandler::ShouldShowIBeamForNode(const Node* node,
   return HasEditableStyle(*node);
 }
 
-OptionalCursor EventHandler::SelectCursor(const HitTestResult& result) {
+EventHandler::OptionalCursor EventHandler::SelectCursor(
+    const HitTestResult& result) {
   if (scroll_manager_->InResizeMode())
     return kNoCursorChange;
 
@@ -559,9 +541,10 @@ OptionalCursor EventHandler::SelectCursor(const HitTestResult& result) {
   return PointerCursor();
 }
 
-OptionalCursor EventHandler::SelectAutoCursor(const HitTestResult& result,
-                                              Node* node,
-                                              const Cursor& i_beam) {
+EventHandler::OptionalCursor EventHandler::SelectAutoCursor(
+    const HitTestResult& result,
+    Node* node,
+    const Cursor& i_beam) {
   if (ShouldShowIBeamForNode(node, result))
     return i_beam;
 
@@ -915,7 +898,8 @@ WebInputEventResult EventHandler::HandleMouseMoveOrLeaveEvent(
       scrollbar->MouseMoved(mev.Event());
     }
     if (LocalFrameView* view = frame_->View()) {
-      OptionalCursor optional_cursor = SelectCursor(mev.GetHitTestResult());
+      EventHandler::OptionalCursor optional_cursor =
+          SelectCursor(mev.GetHitTestResult());
       if (optional_cursor.IsCursorChange()) {
         view->SetCursor(optional_cursor.GetCursor());
       }
--- third_party/WebKit/Source/core/input/EventHandler.h
+++ third_party/WebKit/Source/core/input/EventHandler.h
@@ -67,7 +67,6 @@ class HitTestResult;
 class LayoutObject;
 class LocalFrame;
 class Node;
-class OptionalCursor;
 class ScrollableArea;
 class Scrollbar;
 class SelectionController;
@@ -272,6 +271,25 @@ class CORE_EXPORT EventHandler final
   void ClearDragState();
 
  private:
+  enum NoCursorChangeType { kNoCursorChange };
+
+  class OptionalCursor {
+   public:
+    OptionalCursor(NoCursorChangeType) : is_cursor_change_(false) {}
+    OptionalCursor(const Cursor& cursor)
+        : is_cursor_change_(true), cursor_(cursor) {}
+
+    bool IsCursorChange() const { return is_cursor_change_; }
+    const Cursor& GetCursor() const {
+      DCHECK(is_cursor_change_);
+      return cursor_;
+    }
+
+   private:
+    bool is_cursor_change_;
+    Cursor cursor_;
+  };
+
   WebInputEventResult HandleMouseMoveOrLeaveEvent(
       const WebMouseEvent&,
       const Vector<WebMouseEvent>&,
@@ -414,6 +432,8 @@ class CORE_EXPORT EventHandler final
   FRIEND_TEST_ALL_PREFIXES(EventHandlerTest, InputFieldsCanStartSelection);
   FRIEND_TEST_ALL_PREFIXES(EventHandlerTest, ImagesCannotStartSelection);
   FRIEND_TEST_ALL_PREFIXES(EventHandlerTest, AnchorTextCannotStartSelection);
+  FRIEND_TEST_ALL_PREFIXES(EventHandlerTest,
+                           EditableAnchorTextCanStartSelection);
   FRIEND_TEST_ALL_PREFIXES(EventHandlerTest,
                            ReadOnlyInputDoesNotInheritUserSelect);
 };
