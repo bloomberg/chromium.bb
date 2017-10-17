a9bad4fae5d1057967c7dae873365bb4070daef4
Gauthier Ambard <gambard@chromium.org>
2017-10-17 14:33:26 +0000

    Change assign to copy for block property
    RecentTabsHandsetCoordinator should retain its block property.
    
    Bug: 775508
    Change-Id: Ifd9bd829786592adde9057bec68bf3a73b6c9fc2
    Reviewed-on: https://chromium-review.googlesource.com/723304
    Reviewed-by: Jean-François Geyelin <jif@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509383}
    Former-commit-id: 51058e681025614621ca3c6cfb30e8e7c1dd8ab5

--- ios/chrome/browser/ui/ntp/recent_tabs/recent_tabs_handset_coordinator.mm
+++ ios/chrome/browser/ui/ntp/recent_tabs/recent_tabs_handset_coordinator.mm
@@ -20,7 +20,7 @@
     RecentTabsHandsetViewController* recentTabsViewController;
 @property(nonatomic, strong) RecentTabsTableCoordinator* tableCoordinator;
 // Completion block called once the recentTabsViewController is dismissed.
-@property(nonatomic, assign) ProceduralBlock completion;
+@property(nonatomic, copy) ProceduralBlock completion;
 
 @end
 
