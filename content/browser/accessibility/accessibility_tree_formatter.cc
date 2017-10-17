4ddd81c1ac8fb6ae4df5962569dbcf3ae61e8dd1
Aaron Leventhal <aleventhal@chromium.org>
2017-10-17 20:00:50 +0000

    ax_dump_tree
    Standalone executable tool that helps developers view IAccessible2 object trees from any application.
    This tool is an alternative to AccProbe, which is buggy, and AViewer, which is not feature rich.
    The first version of ax_dump_tree is barebones and simply displays events on console, but has
    a lot of room for future expansion (e.g., serve info to a GUI, dump the tree structure etc.).
    The tool is similar is use to ax_dump_events.
    
    As an example of how this makes easier, when there is a bug with NVDA/JAWS in Chrome that doesn't 
    occur with Firefox, we can look at the object tree emitted by Firefox for the same content.
    
    The ax_dump_tree tool shares code and formatting with the DumpAccessibilityTree browsertests. 
    
    Use as follows:
    ax_dump_tree --pid=[processid]
    ax_dump_tree --window=[hwnd]   (start with 0x to use hex value)
    
    Bug: 771747
    Change-Id: I330d2aba8b004e679e9a8d15b1c288ae5460e83e
    Reviewed-on: https://chromium-review.googlesource.com/700725
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509495}
    Former-commit-id: 96754161358a5257a98e8f2fc5e258d2ed835812

--- content/browser/accessibility/accessibility_tree_formatter.cc
+++ content/browser/accessibility/accessibility_tree_formatter.cc
@@ -45,6 +45,12 @@ void AccessibilityTreeFormatter::FormatAccessibilityTree(
   RecursiveFormatAccessibilityTree(*(dict.get()), contents);
 }
 
+void AccessibilityTreeFormatter::FormatAccessibilityTree(
+    const base::DictionaryValue& dict,
+    base::string16* contents) {
+  RecursiveFormatAccessibilityTree(dict, contents);
+}
+
 void AccessibilityTreeFormatter::RecursiveFormatAccessibilityTree(
     const base::DictionaryValue& dict, base::string16* contents, int depth) {
   base::string16 indent = base::string16(depth * kIndentSymbolCount,
@@ -64,7 +70,8 @@ void AccessibilityTreeFormatter::RecursiveFormatAccessibilityTree(
     return;
 
   const base::ListValue* children;
-  dict.GetList(kChildrenDictAttr, &children);
+  if (!dict.GetList(kChildrenDictAttr, &children))
+    return;
   const base::DictionaryValue* child_dict;
   for (size_t i = 0; i < children->GetSize(); i++) {
     children->GetDictionary(i, &child_dict);
