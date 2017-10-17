84f9cc08ef0a3ec3894aad7a28676f1d046730d7
Peng Huang <penghuang@chromium.org>
2017-10-17 15:34:35 +0000

    exo: Fix crash during chrome shutdown
    The crash is because wayland resources are leaked. This CL fixes the
    problem by releasing all wayland resources with wl_display.
    
    Bug: 774920
    Change-Id: I91e7085e98551aea637da72e4237b6035831f689
    Reviewed-on: https://chromium-review.googlesource.com/721737
    Reviewed-by: David Reveman <reveman@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509394}
    Former-commit-id: cc1ff3dda1bcbfb310cbd0299dc0b19e1b7d684e

--- components/exo/wayland/scoped_wl.cc
+++ components/exo/wayland/scoped_wl.cc
@@ -10,6 +10,16 @@ namespace exo {
 namespace wayland {
 
 void WlDisplayDeleter::operator()(wl_display* display) const {
+  // Destroy all clients in the display manually, because wl_display_destroy()
+  // doesn't do it.
+  // TODO(penghuang): Remove below manual client tear down code if the
+  // wl_display_destroy() is fixed upstream.
+  wl_list* client_list = wl_display_get_client_list(display);
+  while (!wl_list_empty(client_list)) {
+    wl_client* client = wl_client_from_link(client_list->next);
+    wl_client_destroy(client);
+  }
+
   wl_display_destroy(display);
 }
 
