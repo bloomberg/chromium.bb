451220612f7b43a74051992a1e0c55a1d7e70d2f
Sammie Quon <sammiequon@google.com>
2017-10-17 21:23:50 +0000

    splitview: keep split view overview overlay show until phantom shows up
    Bug: 772201
    Test: ash_unittests SplitViewWindowSelectorTest.*
    Change-Id: I2bd02c4cde73247e1260064035401bf5069150ac
    Reviewed-on: https://chromium-review.googlesource.com/717264
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509538}
    Former-commit-id: be12f0badb3cf6f14e57f7800857bc6a95e15d5c

--- ash/wm/overview/overview_window_drag_controller.h
+++ ash/wm/overview/overview_window_drag_controller.h
@@ -23,6 +23,10 @@ class WindowSelectorItem;
 // and shows/hides the phantom window accordingly.
 class ASH_EXPORT OverviewWindowDragController {
  public:
+  // Snapping distance between the dragged window with the screen edge. It's
+  // useful especially for touch events.
+  static constexpr int kScreenEdgeInsetForDrag = 200;
+
   explicit OverviewWindowDragController(WindowSelector* window_selector);
   ~OverviewWindowDragController();
 
@@ -38,6 +42,10 @@ class ASH_EXPORT OverviewWindowDragController {
   void ResetWindowSelector();
 
   WindowSelectorItem* item() { return item_; }
+
+  bool IsPhantomWindowShowing() const {
+    return phantom_window_controller_ != nullptr;
+  }
 
  private:
   // Updates visuals for the user while dragging items around.
