3a4d933a3998d5cdc10111239236108d27487568
Boris Sazonov <bsazonov@chromium.org>
2017-10-17 18:43:29 +0000

    [Android] Fix account list invalidation in AccountTrackerService
    This CL fixes a crash introduced by https://crrev.com/c/675684, which
    added a cache of the account list to AccountManagerFacade.
    
    Since the cache is updated asynchronously, the following situation
    could occur:
    1. Account A is added to the account list.
    2. AccountManagerFacade starts updating the cache asynchronously.
    3. AccountsChangedReceiver notifies AccountTrackerService about
      the change to the account list.
    4. AccountTrackerService gets account list from AccountManagerFacade.
      AccountManagerFacade returns the cached list (without the account A).
    5. AccountTrackerService seeds the old account list and clears the
      mSystemAccountsChanged flag.
    6. AccountManagerFacade finishes updating account list.
    7. Now AccountTrackerService considers the account list to be up-to-date
      and never seeds account A.
    8. Some code path tries to get account info for account A and crashes.
    
    This CL fixes the issue by adding an AccountsChangeObserver to
    AccountTrackerService that will invalidate seeding status whenever the
    cached account list in AccountManagerFacade changes.
    
    Bug: 768366
    Change-Id: Ia1c8dcb6ac5b4033d6ed1ee204db50aaf8e02964
    Reviewed-on: https://chromium-review.googlesource.com/721439
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509449}
    Former-commit-id: 1a675ac69b6c0012eb0a2649d480899689480744

--- chrome/android/java/src/org/chromium/chrome/browser/signin/AccountTrackerService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/AccountTrackerService.java
@@ -12,6 +12,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.components.signin.AccountManagerFacade;
+import org.chromium.components.signin.AccountsChangeObserver;
 
 /**
 * Android wrapper of AccountTrackerService which provides access from the java layer.
@@ -26,6 +27,7 @@ public class AccountTrackerService {
     private SystemAccountsSeedingStatus mSystemAccountsSeedingStatus;
     private boolean mSystemAccountsChanged;
     private boolean mSyncForceRefreshedForTest;
+    private AccountsChangeObserver mAccountsChangeObserver;
 
     private enum SystemAccountsSeedingStatus {
         SEEDING_NOT_STARTED,
@@ -104,6 +106,7 @@ public class AccountTrackerService {
         ThreadUtils.assertOnUiThread();
         mSystemAccountsChanged = false;
         mSyncForceRefreshedForTest = false;
+
         final AccountIdProvider accountIdProvider = AccountIdProvider.getInstance();
         if (accountIdProvider.canBeUsed()) {
             mSystemAccountsSeedingStatus = SystemAccountsSeedingStatus.SEEDING_IN_PROGRESS;
@@ -111,6 +114,13 @@ public class AccountTrackerService {
             mSystemAccountsSeedingStatus = SystemAccountsSeedingStatus.SEEDING_NOT_STARTED;
             return;
         }
+
+        if (mAccountsChangeObserver == null) {
+            mAccountsChangeObserver =
+                    () -> invalidateAccountSeedStatus(false /* don't reseed right now */);
+            AccountManagerFacade.get().addObserver(mAccountsChangeObserver);
+        }
+
         AccountManagerFacade.get().tryGetGoogleAccounts(accounts -> {
             new AsyncTask<Void, Void, String[][]>() {
                 @Override
@@ -163,6 +173,7 @@ public class AccountTrackerService {
     public void syncForceRefreshForTest(String[] accountIds, String[] accountNames) {
         ThreadUtils.assertOnUiThread();
         mSystemAccountsSeedingStatus = SystemAccountsSeedingStatus.SEEDING_IN_PROGRESS;
+        mSystemAccountsChanged = false;
         mSyncForceRefreshedForTest = true;
         nativeSeedAccountsInfo(accountIds, accountNames);
         mSystemAccountsSeedingStatus = SystemAccountsSeedingStatus.SEEDING_DONE;
