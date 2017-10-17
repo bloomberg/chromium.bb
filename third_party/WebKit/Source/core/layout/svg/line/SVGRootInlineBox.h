13aa55bd67fcd6e4e8111c01ede584269533f782
Fredrik Söderquist <fs@opera.com>
2017-10-17 14:59:19 +0000

    Propagate float rects further in SVGRootInlineBox::LayoutInlineBoxes
    Compute the union of the children of a flow box using FloatRect too,
    then convert to a LayoutRect for setting the InlineBox's logical rect.
    
    Also restructure the physical->logical conversion a bit.
    
    Bug: 757436
    Change-Id: I6c8f3a781c3c02dfb28d87eafa3df6cd5fb864f3
    Reviewed-on: https://chromium-review.googlesource.com/721320
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#509386}
    Former-commit-id: 464ff308cb7cf8cad6f5201b192e87528cd60c9c

--- third_party/WebKit/Source/core/layout/svg/line/SVGRootInlineBox.h
+++ third_party/WebKit/Source/core/layout/svg/line/SVGRootInlineBox.h
@@ -55,7 +55,7 @@ class SVGRootInlineBox final : public RootInlineBox {
 
  private:
   void ReorderValueLists();
-  LayoutRect LayoutInlineBoxes(InlineBox&);
+  FloatRect LayoutInlineBoxes(InlineBox&);
 
   LayoutUnit logical_height_;
 };
