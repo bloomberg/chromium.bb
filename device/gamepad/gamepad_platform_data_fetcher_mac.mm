9a99427740805610aa51ad1775c9c1f01f772276
Matt Reynolds <mattreynolds@google.com>
2017-10-17 19:00:29 +0000

    Blacklist SteelSeries Nimbus in platform data fetcher
    The SteelSeries Nimbus is a Made For iOS (MFi) gamepad. In Chrome for
    Mac, MFi gamepads are enumerated through the GameController framework.
    The Nimbus is also detected as a generic USB HID device, which are
    enumerated by GamepadPlatformDataFetcherMac. To prevent the gamepad
    from being enumerated by both data fetchers and taking an extra gamepad
    slot, the vendor/product ID pair for the Nimbus is blacklisted in the
    platform data fetcher.
    
    BUG=775268
    
    Change-Id: Idf8510663ab15b0c11c1b0dd712f3889383e6894
    Reviewed-on: https://chromium-review.googlesource.com/722522
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509470}
    Former-commit-id: 35f6d48ddeba543ffdd46fe0c91d6c0b358e8bbf

--- device/gamepad/gamepad_platform_data_fetcher_mac.mm
+++ device/gamepad/gamepad_platform_data_fetcher_mac.mm
@@ -66,6 +66,9 @@ const uint32_t kGameUsageNumber = 0x05;
 const uint32_t kMultiAxisUsageNumber = 0x08;
 const uint32_t kAxisMinimumUsageNumber = 0x30;
 
+const int kVendorSteelSeries = 0x1038;
+const int kProductNimbus = 0x1420;
+
 }  // namespace
 
 GamepadPlatformDataFetcherMac::GamepadPlatformDataFetcherMac()
@@ -318,6 +321,12 @@ void GamepadPlatformDataFetcherMac::DeviceAdd(IOHIDDeviceRef device) {
   int vendor_int = [vendor_id intValue];
   int product_int = [product_id intValue];
   int version_int = [version_number intValue];
+
+  // The SteelSeries Nimbus and other Made for iOS gamepads should be handled
+  // through the GameController interface. Blacklist it here so it doesn't
+  // take up an additional gamepad slot.
+  if (vendor_int == kVendorSteelSeries && product_int == kProductNimbus)
+    return;
 
   char vendor_as_str[5], product_as_str[5], version_as_str[5];
   snprintf(vendor_as_str, sizeof(vendor_as_str), "%04x", vendor_int);
