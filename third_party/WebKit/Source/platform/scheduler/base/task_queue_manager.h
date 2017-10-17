c45a577869e629f370d3896cb185a6e29b7e3efa
tzik <tzik@chromium.org>
2017-10-17 08:16:59 +0000

    Replace ref-counted TaskQueueManager::DeletionSentinel with a WeakPtr
    DeletionSentinel may fail to detect TaskQueueManager deletion on a
    nested message loop:
    If there's a nested message loop that spins in task_annotator_.RunTask()
    at line 531, ProcessTaskFromWorkQueue() is reentered. As both outer and
    inner PTFWQ hold a ref to the DeletionSentinel instance,
    protect->HasOneRef() at line 534 doesn't hit even when the
    TaskQueueManager is gone.
    
    This CL replaces it with WeakPtr that covers the reentered case.
    
    Change-Id: Ia15947fcac399dc45994b0df7514a3872c6ba25c
    Reviewed-on: https://chromium-review.googlesource.com/721219
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509322}
    Former-commit-id: 99fb6b82a52f4b024c4028c42f2d004073e2355c

--- third_party/WebKit/Source/platform/scheduler/base/task_queue_manager.h
+++ third_party/WebKit/Source/platform/scheduler/base/task_queue_manager.h
@@ -11,6 +11,7 @@
 #include "base/cancelable_callback.h"
 #include "base/debug/task_annotator.h"
 #include "base/macros.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/message_loop/message_loop.h"
 #include "base/pending_task.h"
@@ -229,12 +230,6 @@ class PLATFORM_EXPORT TaskQueueManager
     TimeDomain* time_domain_;
   };
 
-  class DeletionSentinel : public base::RefCounted<DeletionSentinel> {
-   private:
-    friend class base::RefCounted<DeletionSentinel>;
-    ~DeletionSentinel() {}
-  };
-
   // TaskQueueSelector::Observer implementation:
   void OnTaskQueueEnabled(internal::TaskQueueImpl* queue) override;
   void OnTriedToSelectBlockedWorkQueue(
@@ -382,7 +377,6 @@ class PLATFORM_EXPORT TaskQueueManager
   internal::TaskQueueImpl* currently_executing_task_queue_;  // NOT OWNED
 
   Observer* observer_;  // NOT OWNED
-  scoped_refptr<DeletionSentinel> deletion_sentinel_;
   base::WeakPtrFactory<TaskQueueManager> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(TaskQueueManager);
