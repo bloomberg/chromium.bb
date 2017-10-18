1b89736976c90c414239dfb5dcc31fedefd7f24c
Mathieu Perreault <mathp@chromium.org>
2017-10-18 21:54:44 +0000

    [Autofill] Cleaning up of AddressNormalizationManager
    Changing the method names in anticipation of future change. Moving a
    class to the implementation
    
    Bug: 767931
    Change-Id: Id386a7708f05c501c7f44321dfa0b116215118cd
    Tbr: mahmadi@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/726370
    Commit-Queue: Mathieu Perreault <mathp@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509888}
    Former-commit-id: cf67deeb3b5b0b69fc7ef512124dca0c0ae31ef4

--- components/autofill/core/browser/address_normalization_manager.h
+++ components/autofill/core/browser/address_normalization_manager.h
@@ -11,7 +11,6 @@
 
 #include "base/callback.h"
 #include "base/threading/thread_checker.h"
-#include "components/autofill/core/browser/address_normalizer.h"
 
 namespace autofill {
 
@@ -30,53 +29,19 @@ class AddressNormalizationManager {
 
   ~AddressNormalizationManager();
 
+  // Multiple address version of NormalizeAddressWithCallback. Starts the
+  // normalization of |profile|. Callers should call
+  // FinalizeWithCompletionCallback. On completion, the address in |profile|
+  // will be updated with the normalized address.
+  void NormalizeAddressUntilFinalized(AutofillProfile* profile);
+
   // Stops accepting normalization requests until all pending requests complete.
   // If all the address normalization requests have already completed,
   // |completion_callback| will be called before this method returns. Otherwise,
   // it will be called as soon as the last pending request completes.
-  void FinalizePendingRequestsWithCompletionCallback(
-      base::OnceClosure completion_callback);
-
-  // Normalizes the address in |profile|. This may or may not happen
-  // asynchronously. On completion, the address in |profile| will be updated
-  // with the normalized address.
-  void StartNormalizingAddress(AutofillProfile* profile);
+  void FinalizeWithCompletionCallback(base::OnceClosure completion_callback);
 
  private:
-  // Implements the payments::AddressNormalizer::Delegate interface, and
-  // notifies its parent AddressNormalizationManager when normalization has
-  // completed.
-  class NormalizerDelegate : public autofill::AddressNormalizer::Delegate {
-   public:
-    // |owner| is the parent AddressNormalizationManager, |address_normalizer|
-    // is a pointer to an instance of AddressNormalizer which will handle
-    // normalization of |profile|. |profile| will be updated when normalization
-    // is complete.
-    NormalizerDelegate(AddressNormalizationManager* owner,
-                       AddressNormalizer* address_normalizer,
-                       AutofillProfile* profile);
-
-    // Returns whether this delegate has completed or not.
-    bool has_completed() const { return has_completed_; }
-
-    // AddressNormalizer::Delegate:
-    void OnAddressNormalized(
-        const AutofillProfile& normalized_profile) override;
-    void OnCouldNotNormalize(const AutofillProfile& profile) override;
-
-   private:
-    // Helper method that handles when normalization has completed.
-    void OnCompletion(const AutofillProfile& profile);
-
-    bool has_completed_ = false;
-    AddressNormalizationManager* owner_ = nullptr;
-    autofill::AutofillProfile* profile_ = nullptr;
-
-    DISALLOW_COPY_AND_ASSIGN(NormalizerDelegate);
-  };
-
-  friend class NormalizerDelegate;
-
   // Runs the completion callback if all the delegates have completed.
   void MaybeRunCompletionCallback();
 
@@ -91,6 +56,7 @@ class AddressNormalizationManager {
   base::OnceClosure completion_callback_;
 
   // Storage for all the delegates that handle the normalization requests.
+  class NormalizerDelegate;
   std::vector<std::unique_ptr<NormalizerDelegate>> delegates_;
 
   // An unowned raw pointer to the AddressNormalizer to use.
--- components/autofill/core/browser/address_normalization_manager_unittest.cc
+++ components/autofill/core/browser/address_normalization_manager_unittest.cc
@@ -23,7 +23,7 @@ class AddressNormalizationManagerTest : public testing::Test {
   }
 
   void Finalize() {
-    manager_->FinalizePendingRequestsWithCompletionCallback(
+    manager_->FinalizeWithCompletionCallback(
         base::BindOnce(&AddressNormalizationManagerTest::CompletionCallback,
                        base::Unretained(this)));
   }
@@ -39,7 +39,7 @@ TEST_F(AddressNormalizationManagerTest, SynchronousResult) {
   Initialize("en-US");
 
   AutofillProfile profile_to_normalize;
-  manager_->StartNormalizingAddress(&profile_to_normalize);
+  manager_->NormalizeAddressUntilFinalized(&profile_to_normalize);
 
   EXPECT_FALSE(completion_callback_called_);
   Finalize();
@@ -51,7 +51,7 @@ TEST_F(AddressNormalizationManagerTest, AsynchronousResult) {
   address_normalizer_.DelayNormalization();
 
   AutofillProfile profile_to_normalize;
-  manager_->StartNormalizingAddress(&profile_to_normalize);
+  manager_->NormalizeAddressUntilFinalized(&profile_to_normalize);
 
   EXPECT_FALSE(completion_callback_called_);
   Finalize();
