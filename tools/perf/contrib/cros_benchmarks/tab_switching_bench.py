fa24fbc90cf396dc2121e42c1491d55b154e9a6f
Vovo Yang <vovoy@google.com>
2017-10-18 15:07:28 +0000

    Add option --pause-after-creation
    Add option to specify the pause between tab creation and tab switch
    so that tester can manually do something during this pause.
    
    Bug: 724381
    Change-Id: I3440edab585c64aef4c74b64a86162d75e3f6173
    Reviewed-on: https://chromium-review.googlesource.com/721105
    Commit-Queue: Vovo Yang <vovoy@chromium.org>
    Reviewed-by: Cheng-Yu Lee <cylee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509771}
    Former-commit-id: 26e771d2dd407e77f8b618d98ef66eb09fcb86fa

--- tools/perf/contrib/cros_benchmarks/tab_switching_bench.py
+++ tools/perf/contrib/cros_benchmarks/tab_switching_bench.py
@@ -25,6 +25,7 @@ class CrosTabSwitchingTypical24(perf_benchmark.PerfBenchmark):
 
   Benchmark specific option:
     --tabset-repeat=N: Duplicate tab set for N times.
+    --pause-after-creation=N: Pauses N secs after tab creation.
   The following usage example opens 120 tabs.
   $ ./run_benchmark --browser=cros-chrome --remote=DUT_IP
   cros_tab_switching.typical_24 --tabset-repeat=5
@@ -37,6 +38,8 @@ class CrosTabSwitchingTypical24(perf_benchmark.PerfBenchmark):
   def AddBenchmarkCommandLineArgs(cls, parser):
     parser.add_option('--tabset-repeat', type='int', default=1,
                       help='repeat tab page set')
+    parser.add_option('--pause-after-creation', type='int', default=0,
+                      help='pause between tab creation and tab switch')
 
   def CreateStorySet(self, options):
     if not options.cros_remote:
@@ -46,8 +49,11 @@ class CrosTabSwitchingTypical24(perf_benchmark.PerfBenchmark):
         archive_data_file='data/tab_switching.json',
         base_dir=os.path.dirname(os.path.abspath(__file__)),
         cloud_storage_bucket=story.PARTNER_BUCKET)
+    # May not have pause_after_creation attribute in presubmit check.
+    pause_after_creation = getattr(options, 'pause_after_creation', 0)
     story_set.AddStory(tab_switching_stories.CrosMultiTabTypical24Story(
-        story_set, options.cros_remote, options.tabset_repeat))
+        story_set, options.cros_remote, options.tabset_repeat,
+        pause_after_creation))
     return story_set
 
   @classmethod
