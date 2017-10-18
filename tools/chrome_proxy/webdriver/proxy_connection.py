2520bcc18a4292cef3cad0aeade21e38af5bbbc5
Robert Ogden <robertogden@chromium.org>
2017-10-18 20:57:02 +0000

    Add test for hijacked secure proxy connections
    Also adds basic framework for a connection emulation server for future use.
    
    Bug: 766866
    Change-Id: I8c34a80d65cb614f4833d962b9cafbe02b0075b2
    Reviewed-on: https://chromium-review.googlesource.com/721696
    Commit-Queue: Robert Ogden <robertogden@google.com>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509869}
    Former-commit-id: 982fc026308e4233b1579c01d68dbd42615db9e7

---
+++ tools/chrome_proxy/webdriver/proxy_connection.py
@@ -0,0 +1,36 @@
+ # Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import common
+from common import TestDriver
+from common import IntegrationTest
+from decorators import ChromeVersionEqualOrAfterM
+from emulation_server import InvalidTLSHandler
+
+class ProxyConnection(IntegrationTest):
+
+  @ChromeVersionEqualOrAfterM(64)
+  def testTLSInjectionAfterHandshake(self):
+    port = common.GetOpenPort()
+    with TestDriver() as t:
+      t.AddChromeArg('--enable-spdy-proxy-auth')
+      # The server should be 127.0.0.1, not localhost because the two are
+      # treated differently in Chrome internals. Using localhost invalidates the
+      # test.
+      t.AddChromeArg(
+        '--data-reduction-proxy-http-proxies=https://127.0.0.1:%d' % port)
+      t.AddChromeArg(
+        '--force-fieldtrials=DataReductionProxyConfigService/Disabled')
+      t.UseEmulationServer(InvalidTLSHandler, port=port)
+
+      t.LoadURL('http://check.googlezip.net/test.html')
+      responses = t.GetHTTPResponses()
+      # Expect responses with a bypass on a bad proxy. If the test failed, the
+      # next assertion will fail because there will be no responses.
+      self.assertEqual(2, len(responses))
+      for response in responses:
+        self.assertNotHasChromeProxyViaHeader(response)
+
+if __name__ == '__main__':
+  IntegrationTest.RunAllTests()
