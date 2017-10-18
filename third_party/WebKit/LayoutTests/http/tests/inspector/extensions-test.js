8c3aeec11e07d0cc370741fe1de01997221b55ed
Will Chen <chenwilliam@chromium.org>
2017-10-18 17:48:19 +0000

    DevTools: extensions test mostly relies on extensions_test_runner module
    This eliminates a lot of the code duplication between the new and legacy
    integration test frameworks by making the legacy test helper files small
    wrappers to load the new extensions_test_runner modules.
    
    Bug: 667560
    Change-Id: Id78d1224eb460dd9a7b8aade42a7c2b710cc4774
    Reviewed-on: https://chromium-review.googlesource.com/724160
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509807}
    Former-commit-id: 72fb23406defaf7e05991d9e6ae04083c7a3c93c

--- third_party/WebKit/LayoutTests/http/tests/inspector/extensions-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/extensions-test.js
@@ -1,3 +1,10 @@
+var initialize_ExtensionsTest = function()
+{
+    InspectorTest.preloadModule('extensions_test_runner');
+}
+
+// TODO(chenwilliam): these should be in ExtensionsTestRunner.js initAsync(...)
+// however these are here because they need to be executed when the inspected page reloads
 function extensionFunctions()
 {
     var functions = "";
@@ -11,65 +18,6 @@ function extensionFunctions()
 
 var extensionsOrigin = "http://devtools-extensions.oopif.test:8000";
 
-var initialize_ExtensionsTest = function()
-{
-
-var extensionsHost = "devtools-extensions.oopif.test";
-var extensionsOrigin = `http://${extensionsHost}:8000`;
-
-Extensions.extensionServer._registerHandler("evaluateForTestInFrontEnd", onEvaluate);
-
-Extensions.extensionServer._extensionAPITestHook = function(extensionServerClient, coreAPI)
-{
-    window.webInspector = coreAPI;
-    window._extensionServerForTests = extensionServerClient;
-    coreAPI.panels.themeName = "themeNameForTest";
-}
-
-InspectorTest._replyToExtension = function(requestId, port)
-{
-    Extensions.extensionServer._dispatchCallback(requestId, port);
-}
-
-function onEvaluate(message, port)
-{
-    function reply(param)
-    {
-        Extensions.extensionServer._dispatchCallback(message.requestId, port, param);
-    }
-
-    try {
-        eval(message.expression);
-    } catch (e) {
-        InspectorTest.addResult("Exception while running: " + message.expression + "\n" + (e.stack || e));
-        InspectorTest.completeTest();
-    }
-}
-
-InspectorTest.showPanel = function(panelId)
-{
-    if (panelId === "extension")
-        panelId = UI.inspectorView._tabbedPane._tabs[UI.inspectorView._tabbedPane._tabs.length - 1].id;
-    return UI.inspectorView.showPanel(panelId);
-}
-
-InspectorTest.runExtensionTests = async function()
-{
-    var result = await InspectorTest.RuntimeAgent.evaluate("location.href", "console", false);
-    if (!result)
-        return;
-    var pageURL = result.value;
-    var extensionURL = (/^https?:/.test(pageURL) ?
-        pageURL.replace(/^(https?:\/\/[^/]*\/).*$/,"$1") :
-        pageURL.replace(/\/devtools\/extensions\/[^/]*$/, "/http/tests")) +
-        "devtools/resources/extension-main.html";
-    extensionURL = extensionURL.replace("127.0.0.1", extensionsHost);
-    InspectorFrontendAPI.addExtensions([{ startPage: extensionURL, name: "test extension", exposeWebInspectorNamespace: true }]);
-    Extensions.extensionServer.initializeExtensions();
-}
-
-}
-
 function extension_showPanel(panelId, callback)
 {
     evaluateOnFrontend("InspectorTest.showPanel(unescape('" + escape(panelId) + "')).then(function() { reply(); });", callback);
@@ -78,5 +26,5 @@ function extension_showPanel(panelId, callback)
 var test = function()
 {
     Common.moduleSetting("shortcutPanelSwitch").set(true);
-    InspectorTest.runExtensionTests();
+    ExtensionsTestRunner.runExtensionTests();
 }
