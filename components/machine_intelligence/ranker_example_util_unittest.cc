2df9be9168ac1f74747e27414be1694550b08774
Philippe Hamel <hamelphi@chromium.org>
2017-10-18 19:51:28 +0000

    RankerExample proto.
    Bug: 759161
    Change-Id: I6688aaa98e819122673c471f6094bc85b1d836e9
    Reviewed-on: https://chromium-review.googlesource.com/723589
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Commit-Queue: Philippe Hamel <hamelphi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509844}
    Former-commit-id: cb63f2aa96886656db555260efc65065473192ac

---
+++ components/machine_intelligence/ranker_example_util_unittest.cc
@@ -0,0 +1,106 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/machine_intelligence/ranker_example_util.h"
+
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace machine_intelligence {
+
+using ::testing::ElementsAreArray;
+
+class RankerExampleUtilTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    auto& features = *example_.mutable_features();
+    features[bool_name_].set_bool_value(bool_value_);
+    features[int64_name_].set_int64_value(int64_value_);
+    features[float_name_].set_float_value(float_value_);
+    features[one_hot_name_].set_string_value(one_hot_value_);
+  }
+
+  RankerExample example_;
+  const std::string bool_name_ = "bool_feature";
+  const bool bool_value_ = true;
+  const std::string int64_name_ = "int64_feature";
+  const int int64_value_ = 2;
+  const std::string float_name_ = "float_feature";
+  const float float_value_ = 3.0f;
+  const std::string one_hot_name_ = "one_hot_feature";
+  const std::string elem1_ = "elem1";
+  const std::string elem2_ = "elem2";
+  const std::string one_hot_value_ = elem1_;
+  const float epsilon_ = 0.00000001f;
+};
+
+TEST_F(RankerExampleUtilTest, HasFeature) {
+  EXPECT_TRUE(HasFeature(bool_name_, example_));
+  EXPECT_TRUE(HasFeature(int64_name_, example_));
+  EXPECT_TRUE(HasFeature(float_name_, example_));
+  EXPECT_TRUE(HasFeature(one_hot_name_, example_));
+  EXPECT_FALSE(HasFeature("", example_));
+  EXPECT_FALSE(HasFeature("foo", example_));
+}
+
+TEST_F(RankerExampleUtilTest, SafeGetFeature) {
+  Feature feature;
+
+  EXPECT_TRUE(SafeGetFeature(bool_name_, example_, &feature));
+  EXPECT_TRUE(feature.bool_value());
+  feature.Clear();
+
+  EXPECT_TRUE(SafeGetFeature(int64_name_, example_, &feature));
+  EXPECT_EQ(int64_value_, feature.int64_value());
+  feature.Clear();
+
+  EXPECT_TRUE(SafeGetFeature(float_name_, example_, &feature));
+  EXPECT_NEAR(float_value_, feature.float_value(), epsilon_);
+  feature.Clear();
+
+  EXPECT_TRUE(SafeGetFeature(one_hot_name_, example_, &feature));
+  EXPECT_EQ(one_hot_value_, feature.string_value());
+  feature.Clear();
+
+  EXPECT_FALSE(SafeGetFeature("", example_, &feature));
+  EXPECT_FALSE(SafeGetFeature("foo", example_, &feature));
+}
+
+TEST_F(RankerExampleUtilTest, GetFeatureValueAsFloat) {
+  float value;
+
+  EXPECT_TRUE(GetFeatureValueAsFloat(bool_name_, example_, &value));
+  EXPECT_NEAR(1.0f, value, epsilon_);
+
+  EXPECT_TRUE(GetFeatureValueAsFloat(int64_name_, example_, &value));
+  EXPECT_NEAR(2.0f, value, epsilon_);
+
+  EXPECT_TRUE(GetFeatureValueAsFloat(float_name_, example_, &value));
+  EXPECT_NEAR(3.0f, value, epsilon_);
+
+  EXPECT_FALSE(GetFeatureValueAsFloat(one_hot_name_, example_, &value));
+  // Value remains unchanged if GetFeatureValueAsFloat returns false.
+  EXPECT_NEAR(3.0f, value, epsilon_);
+
+  EXPECT_FALSE(GetFeatureValueAsFloat("", example_, &value));
+  EXPECT_FALSE(GetFeatureValueAsFloat("foo", example_, &value));
+}
+
+TEST_F(RankerExampleUtilTest, GetOneHotValue) {
+  std::string value;
+
+  EXPECT_FALSE(GetOneHotValue(bool_name_, example_, &value));
+
+  EXPECT_FALSE(GetOneHotValue(int64_name_, example_, &value));
+
+  EXPECT_FALSE(GetOneHotValue(float_name_, example_, &value));
+
+  EXPECT_TRUE(GetOneHotValue(one_hot_name_, example_, &value));
+  EXPECT_EQ(one_hot_value_, value);
+
+  EXPECT_FALSE(GetOneHotValue("", example_, &value));
+  EXPECT_FALSE(GetOneHotValue("foo", example_, &value));
+}
+
+}  // namespace machine_intelligence
