ca955fad4c22cd2d3bf7d93faf1e4e521d6f311b
Darren Shen <shend@chromium.org>
2017-10-18 22:56:41 +0000

    Use FuzzedDataProvider for CSSParserFastPathsFuzzer.
    This patch changes CSSParserFastPathsFuzzer to use the last 4 bytes
    for the property ID rather than a hash to get more coverage. See
    comments at end of [1] for context.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/706450#message-eecf2ab42f1c020fbbce3cb064d03718f9fa6eeb
    
    Change-Id: I1375add835cac612e8ce6edf298c1a3ff1601435
    Reviewed-on: https://chromium-review.googlesource.com/724559
    Reviewed-by: Max Moroz <mmoroz@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509908}
    Former-commit-id: 8b63664ae39d6d390df10d6871f0d96c9341a20a

--- third_party/WebKit/Source/core/css/parser/CSSParserFastPathsFuzzer.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSParserFastPathsFuzzer.cpp
@@ -5,26 +5,29 @@
 #include "core/css/parser/CSSParserFastPaths.h"
 
 #include "platform/testing/BlinkFuzzerTestSupport.h"
+#include "platform/testing/FuzzedDataProvider.h"
 #include "platform/wtf/text/WTFString.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   static blink::BlinkFuzzerTestSupport test_support =
       blink::BlinkFuzzerTestSupport();
 
-  // MaybeParseValue assumes the string is not empty.
-  if (size == 0)
+  if (size <= 4)
     return 0;
 
-  const std::string data_string(reinterpret_cast<const char*>(data), size);
-  const std::size_t data_hash = std::hash<std::string>()(data_string);
+  blink::FuzzedDataProvider provider(data, size);
 
-  const auto property_id = blink::convertToCSSPropertyID(
-      (data_hash % blink::numCSSProperties) + blink::firstCSSProperty);
+  const auto property_id =
+      blink::convertToCSSPropertyID(provider.ConsumeInt32InRange(
+          blink::firstCSSProperty, blink::lastCSSProperty));
+  const auto data_string = provider.ConsumeRemainingBytes();
 
   for (unsigned parser_mode = 0;
        parser_mode < blink::CSSParserMode::kNumCSSParserModes; parser_mode++) {
     blink::CSSParserFastPaths::MaybeParseValue(
-        property_id, String::FromUTF8WithLatin1Fallback(data, size),
+        property_id,
+        String::FromUTF8WithLatin1Fallback(data_string.data(),
+                                           data_string.length()),
         static_cast<blink::CSSParserMode>(parser_mode));
   }
 
