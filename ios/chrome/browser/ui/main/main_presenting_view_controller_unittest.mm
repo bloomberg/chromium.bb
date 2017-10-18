9a23b19557e6f181ee2bf269f7b1c7b5de693355
Rohit Rao <rohitrao@google.com>
2017-10-18 23:22:46 +0000

    [ios] Updates the internal structure of MainPresentingViewController.
    The TabSwitcher is now displayed as a contained child, while BVCs are presented
    inside a container VC.  This structure allows for BVCs to be presented even
    before a TabSwitcher is created, and it allows incognito/non-incognito BVCs to
    be swapped without having to dismiss and re-present view controllers.
    
    More details can be found in the design doc at
    https://docs.google.com/document/d/11kI9oH8NUl0oJU_ZRHt6amblGyzPSwhvB_7JWsKayGY/view
    
    BUG=768563
    
    Change-Id: I971cf80c1c2714d801f9885369ce1d0fb95afb28
    Reviewed-on: https://chromium-review.googlesource.com/723940
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Rohit Rao (ping after 24h) <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509919}
    Former-commit-id: 2e47cfb5468c17703bef672f90095509c17dad68

--- ios/chrome/browser/ui/main/main_presenting_view_controller_unittest.mm
+++ ios/chrome/browser/ui/main/main_presenting_view_controller_unittest.mm
@@ -18,52 +18,144 @@
 
 namespace {
 
-using MainPresentingViewControllerTest = MainViewControllerTest;
+class MainPresentingViewControllerTest : public MainViewControllerTest {
+ public:
+  MainPresentingViewControllerTest() {
+    main_view_controller_ = [[MainPresentingViewController alloc] init];
+    SetRootViewController(main_view_controller_);
+    CGRect windowRect = CGRectMake(0, 0, 200, 200);
+    main_view_controller_.view.frame = windowRect;
+
+    tab_switcher_ = CreateTestTabSwitcher();
+    tab_switcher_.view.frame = CGRectMake(0, 0, 10, 10);
+
+    normal_tab_view_controller_ = [[UIViewController alloc] init];
+    normal_tab_view_controller_.view.frame = CGRectMake(20, 20, 10, 10);
+
+    incognito_tab_view_controller_ = [[UIViewController alloc] init];
+    incognito_tab_view_controller_.view.frame = CGRectMake(40, 40, 10, 10);
+  }
+
+  ~MainPresentingViewControllerTest() override {}
+
+ protected:
+  // The MainPresentingViewController that is under test.  The test fixture sets
+  // this VC as the root VC for the window.
+  MainPresentingViewController* main_view_controller_;
+
+  // The following view controllers are created by the test fixture and are
+  // available for use in tests.
+  UIViewController<TabSwitcher>* tab_switcher_;
+  UIViewController* normal_tab_view_controller_;
+  UIViewController* incognito_tab_view_controller_;
+};
+
+// Tests that there is no activeViewController for a newly constructed instance.
+TEST_F(MainPresentingViewControllerTest, NoActiveViewController) {
+  EXPECT_EQ(nil, main_view_controller_.activeViewController);
+}
+
+// Tests that it is possible to set a TabViewController without first setting a
+// TabSwitcher.
+TEST_F(MainPresentingViewControllerTest, TabViewControllerBeforeTabSwitcher) {
+  [main_view_controller_ showTabViewController:normal_tab_view_controller_
+                                    completion:nil];
+  EXPECT_EQ(normal_tab_view_controller_,
+            main_view_controller_.activeViewController);
+
+  // Now setting a TabSwitcher will make the switcher active.
+  [main_view_controller_ showTabSwitcher:tab_switcher_ completion:nil];
+  EXPECT_EQ(tab_switcher_, main_view_controller_.activeViewController);
+}
+
+// Tests that it is possible to set a TabViewController after setting a
+// TabSwitcher.
+TEST_F(MainPresentingViewControllerTest, TabViewControllerAfterTabSwitcher) {
+  [main_view_controller_ showTabSwitcher:tab_switcher_ completion:nil];
+  EXPECT_EQ(tab_switcher_, main_view_controller_.activeViewController);
+
+  [main_view_controller_ showTabViewController:normal_tab_view_controller_
+                                    completion:nil];
+  EXPECT_EQ(normal_tab_view_controller_,
+            main_view_controller_.activeViewController);
+
+  // Showing the TabSwitcher again will make it active.
+  [main_view_controller_ showTabSwitcher:tab_switcher_ completion:nil];
+  EXPECT_EQ(tab_switcher_, main_view_controller_.activeViewController);
+}
+
+// Tests swapping between two TabViewControllers.
+TEST_F(MainPresentingViewControllerTest, SwapTabViewControllers) {
+  [main_view_controller_ showTabViewController:normal_tab_view_controller_
+                                    completion:nil];
+  EXPECT_EQ(normal_tab_view_controller_,
+            main_view_controller_.activeViewController);
+
+  [main_view_controller_ showTabViewController:incognito_tab_view_controller_
+                                    completion:nil];
+  EXPECT_EQ(incognito_tab_view_controller_,
+            main_view_controller_.activeViewController);
+}
+
+// Tests calling showTabSwitcher twice in a row with the same VC.
+TEST_F(MainPresentingViewControllerTest, ShowTabSwitcherTwice) {
+  [main_view_controller_ showTabSwitcher:tab_switcher_ completion:nil];
+  EXPECT_EQ(tab_switcher_, main_view_controller_.activeViewController);
+
+  [main_view_controller_ showTabSwitcher:tab_switcher_ completion:nil];
+  EXPECT_EQ(tab_switcher_, main_view_controller_.activeViewController);
+}
+
+// Tests calling showTabViewController twice in a row with the same VC.
+TEST_F(MainPresentingViewControllerTest, ShowTabViewControllerTwice) {
+  [main_view_controller_ showTabViewController:normal_tab_view_controller_
+                                    completion:nil];
+  EXPECT_EQ(normal_tab_view_controller_,
+            main_view_controller_.activeViewController);
+
+  [main_view_controller_ showTabViewController:normal_tab_view_controller_
+                                    completion:nil];
+  EXPECT_EQ(normal_tab_view_controller_,
+            main_view_controller_.activeViewController);
+}
 
 // Tests that setting the active view controller work and that completion
 // handlers are called properly after the new view controller is made active.
-TEST_F(MainPresentingViewControllerTest, SetActiveVCAndCompletionHandlers) {
-  MainPresentingViewController* main_view_controller =
-      [[MainPresentingViewController alloc] init];
-  SetRootViewController(main_view_controller);
-  CGRect windowRect = CGRectMake(0, 0, 200, 200);
-  [main_view_controller view].frame = windowRect;
-
-  UIViewController<TabSwitcher>* child_view_controller_1 =
-      CreateTestTabSwitcher();
-  [child_view_controller_1 view].frame = CGRectMake(0, 0, 10, 10);
-
-  UIViewController<TabSwitcher>* child_view_controller_2 =
-      CreateTestTabSwitcher();
-  [child_view_controller_2 view].frame = CGRectMake(20, 20, 10, 10);
-
-  // Tests that the completion handler is called when there is no preexisting
-  // active view controller.
+TEST_F(MainPresentingViewControllerTest, CompletionHandlers) {
+  // Tests that the completion handler is called when showing the switcher.
   __block BOOL completion_handler_was_called = false;
-  [main_view_controller showTabSwitcher:child_view_controller_1
-                             completion:^{
-                               completion_handler_was_called = YES;
-                             }];
+  [main_view_controller_ showTabSwitcher:tab_switcher_
+                              completion:^{
+                                completion_handler_was_called = YES;
+                              }];
   base::test::ios::WaitUntilCondition(^bool() {
     return completion_handler_was_called;
   });
   ASSERT_TRUE(completion_handler_was_called);
-  EXPECT_EQ(main_view_controller.presentedViewController,
-            child_view_controller_1);
 
-  // Tests that the completion handler is called when replacing an existing
-  // active view controller.
+  // Tests that the completion handler is called when showing a tab view
+  // controller.
   completion_handler_was_called = NO;
-  [main_view_controller showTabViewController:child_view_controller_2
-                                   completion:^{
-                                     completion_handler_was_called = YES;
-                                   }];
+  [main_view_controller_ showTabViewController:normal_tab_view_controller_
+                                    completion:^{
+                                      completion_handler_was_called = YES;
+                                    }];
   base::test::ios::WaitUntilCondition(^bool() {
     return completion_handler_was_called;
   });
   ASSERT_TRUE(completion_handler_was_called);
-  EXPECT_EQ(main_view_controller.presentedViewController,
-            child_view_controller_2);
+
+  // Tests that the completion handler is called when replacing an existing tab
+  // view controller.
+  completion_handler_was_called = NO;
+  [main_view_controller_ showTabViewController:incognito_tab_view_controller_
+                                    completion:^{
+                                      completion_handler_was_called = YES;
+                                    }];
+  base::test::ios::WaitUntilCondition(^bool() {
+    return completion_handler_was_called;
+  });
+  ASSERT_TRUE(completion_handler_was_called);
 }
 
 }  // namespace
