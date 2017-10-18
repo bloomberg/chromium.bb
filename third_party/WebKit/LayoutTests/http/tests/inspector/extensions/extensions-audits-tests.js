8fdb08d17832e87c8e026b62b9d60420840f4824
Will Chen <chenwilliam@chromium.org>
2017-10-18 17:48:19 +0000

    DevTools: extensions test mostly relies on extensions_test_runner module
    This eliminates a lot of the code duplication between the new and legacy
    integration test frameworks by making the legacy test helper files small
    wrappers to load the new extensions_test_runner modules.
    
    Bug: 667560
    Change-Id: Id78d1224eb460dd9a7b8aade42a7c2b710cc4774
    Reviewed-on: https://chromium-review.googlesource.com/724160
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509807}
    Former-commit-id: 72fb23406defaf7e05991d9e6ae04083c7a3c93c

--- third_party/WebKit/LayoutTests/http/tests/inspector/extensions/extensions-audits-tests.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/extensions/extensions-audits-tests.js
@@ -1,40 +1,6 @@
-function extension_runAudits(callback)
-{
-    evaluateOnFrontend("InspectorTest.startExtensionAudits(reply);", callback);
-}
-
-// runs in front-end
 var initialize_ExtensionsAuditsTest = function()
 {
-    InspectorTest.startExtensionAudits = function(callback)
-    {
-        const launcherView = UI.panels.audits._launcherView;
-        launcherView._selectAllClicked(false);
-        launcherView._auditPresentStateElement.checked = true;
-
-        var extensionCategories = document.querySelectorAll(".audit-categories-container > label");
-        for (var i = 0; i < extensionCategories.length; ++i) {
-            var shouldBeEnabled = extensionCategories[i].textContent.includes("Extension");
-            if (!shouldBeEnabled && extensionCategories[i].textElement)
-                shouldBeEnabled = extensionCategories[i].textElement.textContent.includes("Extension");
-            if (shouldBeEnabled !== extensionCategories[i].checkboxElement.checked)
-                extensionCategories[i].checkboxElement.click();
-        }
-
-        function onAuditsDone()
-        {
-            InspectorTest.collectAuditResults(callback);
-        }
-        InspectorTest.addSniffer(UI.panels.audits, "auditFinishedCallback", onAuditsDone, true);
-
-        launcherView._launchButtonClicked();
-    }
-
-    InspectorTest.dumpAuditProgress = function()
-    {
-        var progress = document.querySelector(".progress-indicator").shadowRoot.querySelector("progress");
-        InspectorTest.addResult("Progress: " + Math.round(100 * progress.value / progress.max) + "%");
-    }
+    InspectorTest.preloadModule('extensions_test_runner');
 
     // We will render DOM node results, so preload elements.
     InspectorTest.preloadPanel("elements");
