3a9d683c589ba8300835925799e75434bde3598a
Ryan Hamilton <rch@chromium.org>
2017-10-18 04:39:20 +0000

    Add TaskScheduler to quic_client and quic_server
    Change-Id: I8f5a6e787a1888b81f1a265cda788a034267b885
    Reviewed-on: https://chromium-review.googlesource.com/724961
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509682}
    Former-commit-id: 79dd8e5bd1d8ad9bf57b5e9ed8d9dcdf0f5b49a9

--- net/tools/quic/quic_server_bin.cc
+++ net/tools/quic/quic_server_bin.cc
@@ -13,6 +13,7 @@
 #include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/task_scheduler/task_scheduler.h"
 #include "net/quic/chromium/crypto/proof_source_chromium.h"
 #include "net/quic/core/quic_packets.h"
 #include "net/quic/platform/api/quic_socket_address.h"
@@ -32,6 +33,7 @@ std::unique_ptr<net::ProofSource> CreateProofSource(
 }
 
 int main(int argc, char* argv[]) {
+  base::TaskScheduler::CreateAndStartWithDefaultParams("quic_server");
   base::AtExitManager exit_manager;
   base::MessageLoopForIO message_loop;
 
