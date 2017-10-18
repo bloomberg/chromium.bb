8db48b0445aa9d1a093fbd9759b37483a596fe9d
Thanh Pham <thanhph@chromium.org>
2017-10-18 20:40:23 +0000

    IME for Mus: Candidate list popup should be visible and move with the browser when the browser window is dragged in mash and non-mash.
    Candidate list popup can ask window manager to find its parent by
    setting the mus property WindowManager::kContainerId_InitProperty as 
    ash::kShellWindowId_SettingBubbleContainer. This will maintain the pop
    up window while the browser is dragging.
    
    Bug: 637416
    Change-Id: I3af237f68f6b53f6cc7d26384e2d9693bfe66212
    Reviewed-on: https://chromium-review.googlesource.com/685497
    Commit-Queue: Thanh Pham <thanhph@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509859}
    Former-commit-id: 3a3efda772bd2a07fa66ebb9ff41ef4796739ae2

--- ui/chromeos/ime/candidate_window_view.h
+++ ui/chromeos/ime/candidate_window_view.h
@@ -32,7 +32,10 @@ class UI_CHROMEOS_EXPORT CandidateWindowView
     virtual void OnCandidateCommitted(int index) = 0;
   };
 
-  explicit CandidateWindowView(gfx::NativeView parent);
+  explicit CandidateWindowView(
+      gfx::NativeView parent,
+      int window_shell_id =
+          -1 /* equals ash::ShellWindowId::kShellWindowId_Invalid */);
   ~CandidateWindowView() override;
   views::Widget* InitWidget();
 
@@ -80,6 +83,8 @@ class UI_CHROMEOS_EXPORT CandidateWindowView
   // views::BubbleDialogDelegateView:
   const char* GetClassName() const override;
   int GetDialogButtons() const override;
+  void OnBeforeBubbleWidgetInit(views::Widget::InitParams* params,
+                                views::Widget* widget) const override;
 
   // Overridden from views::ButtonListener:
   void ButtonPressed(views::Button* sender, const ui::Event& event) override;
@@ -131,6 +136,9 @@ class UI_CHROMEOS_EXPORT CandidateWindowView
   // True if the candidate window was open.  This is used to determine when to
   // send OnCandidateWindowOpened and OnCandidateWindowClosed events.
   bool was_candidate_window_open_;
+
+  // Corresponds to ash::ShellWindowId.
+  const int window_shell_id_;
 
   DISALLOW_COPY_AND_ASSIGN(CandidateWindowView);
 };
