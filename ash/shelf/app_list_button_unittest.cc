04134254fb0048d7be0f1d790f2c8c3730741632
Mike Wasserman <msw@chromium.org>
2017-10-19 01:24:16 +0000

    mash: Fix and enable AppListButton unit test.
    The app list button test should start drags on the app list button.
    It was actually targetting the ShelfBezelEventHandler.
    Fix and enable the test, make other cleanup changes.
    
    Enable ShelfLayoutManager test fixtures that work as-is in mash.
    Move remaining ShelfLayoutManager test failures to filter file.
    
    Bug: 695686, 775177
    Change-Id: I7638c6294bc8ce6e2c6b91d459d462a5b357db5d
    Reviewed-on: https://chromium-review.googlesource.com/723863
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509959}
    Former-commit-id: 1de4bccb0ea32f5c5bd82d7d48553c11f88659a3

--- ash/shelf/app_list_button_unittest.cc
+++ ash/shelf/app_list_button_unittest.cc
@@ -38,8 +38,6 @@ class AppListButtonTest : public AshTestBase {
 
   // AshTestBase:
   void SetUp() override {
-    command_line_ = std::make_unique<base::test::ScopedCommandLine>();
-    SetupCommandLine(command_line_->GetProcessCommandLine());
     AshTestBase::SetUp();
     app_list_button_ =
         GetPrimaryShelf()->GetShelfViewForTesting()->GetAppListButton();
@@ -47,21 +45,22 @@ class AppListButtonTest : public AshTestBase {
         test_app_list_presenter.CreateInterfacePtrAndBind());
   }
 
-  virtual void SetupCommandLine(base::CommandLine* command_line) {}
-
   void SendGestureEvent(ui::GestureEvent* event) {
     app_list_button_->OnGestureEvent(event);
+    Shell::Get()->app_list()->FlushForTesting();
+    RunAllPendingInMessageLoop();
   }
 
   void SendGestureEventToSecondaryDisplay(ui::GestureEvent* event) {
     // Add secondary display.
     UpdateDisplay("1+1-1000x600,1002+0-600x400");
     // Send the gesture event to the secondary display.
-    Shell::GetRootWindowControllerWithDisplayId(GetSecondaryDisplay().id())
-        ->shelf()
+    Shelf::ForWindow(Shell::GetAllRootWindows()[1])
         ->GetShelfViewForTesting()
         ->GetAppListButton()
         ->OnGestureEvent(event);
+    Shell::Get()->app_list()->FlushForTesting();
+    RunAllPendingInMessageLoop();
   }
 
   const AppListButton* app_list_button() const { return app_list_button_; }
@@ -70,9 +69,7 @@ class AppListButtonTest : public AshTestBase {
   app_list::test::TestAppListPresenter test_app_list_presenter;
 
  private:
-  AppListButton* app_list_button_;
-
-  std::unique_ptr<base::test::ScopedCommandLine> command_line_;
+  AppListButton* app_list_button_ = nullptr;
 
   DISALLOW_COPY_AND_ASSIGN(AppListButtonTest);
 };
@@ -87,40 +84,43 @@ TEST_F(AppListButtonTest, LongPressGestureWithoutVoiceInteractionFlag) {
   ui::GestureEvent long_press =
       CreateGestureEvent(ui::GestureEventDetails(ui::ET_GESTURE_LONG_PRESS));
   SendGestureEvent(&long_press);
-  RunAllPendingInMessageLoop();
   EXPECT_EQ(0u, test_app_list_presenter.voice_session_count());
 
   // Test long press gesture on secondary display.
   SendGestureEventToSecondaryDisplay(&long_press);
-  RunAllPendingInMessageLoop();
   EXPECT_EQ(0u, test_app_list_presenter.voice_session_count());
 }
 
-TEST_F(AppListButtonTest, SwipingupToOpenFullscreenAppList) {
-  // TODO: investigate failure in mash, http://crbug.com/695686.
-  if (Shell::GetAshConfig() == Config::MASH)
-    return;
-
+TEST_F(AppListButtonTest, SwipeUpToOpenFullscreenAppList) {
   Shelf* shelf = GetPrimaryShelf();
   EXPECT_EQ(SHELF_ALIGNMENT_BOTTOM, shelf->alignment());
 
-  // Start the drag from the center of the applist button's bottom.
-  gfx::Point center_point = app_list_button()->GetAppListButtonCenterPoint();
-  gfx::Point start(center_point.x(),
-                   center_point.y() + app_list_button()->height() / 2.f);
+  // Start the drags from the center of the app list button.
+  gfx::Point start = app_list_button()->GetAppListButtonCenterPoint();
   views::View::ConvertPointToScreen(app_list_button(), &start);
-  // Swiping up less than peeking threshold should keep the app list at PEEKING
-  // state.
-  gfx::Point end =
-      start -
-      gfx::Vector2d(
-          0, ShelfLayoutManager::kAppListDragSnapToPeekingThreshold - 10);
+  // Swiping up less than the threshold should trigger a peeking app list.
+  gfx::Point end = start;
+  end.set_y(shelf->GetIdealBounds().bottom() -
+            ShelfLayoutManager::kAppListDragSnapToPeekingThreshold + 10);
   GetEventGenerator().GestureScrollSequence(
       start, end, base::TimeDelta::FromMilliseconds(100), 4 /* steps */);
   RunAllPendingInMessageLoop();
+  Shell::Get()->app_list()->FlushForTesting();
   EXPECT_EQ(1u, test_app_list_presenter.show_count());
   EXPECT_GE(test_app_list_presenter.set_y_position_count(), 1u);
   EXPECT_EQ(app_list::mojom::AppListState::PEEKING,
+            test_app_list_presenter.app_list_state());
+
+  // Swiping above the threshold should trigger a fullscreen app list.
+  end.set_y(shelf->GetIdealBounds().bottom() -
+            ShelfLayoutManager::kAppListDragSnapToPeekingThreshold - 10);
+  GetEventGenerator().GestureScrollSequence(
+      start, end, base::TimeDelta::FromMilliseconds(100), 4 /* steps */);
+  RunAllPendingInMessageLoop();
+  Shell::Get()->app_list()->FlushForTesting();
+  EXPECT_EQ(2u, test_app_list_presenter.show_count());
+  EXPECT_GE(test_app_list_presenter.set_y_position_count(), 1u);
+  EXPECT_EQ(app_list::mojom::AppListState::FULLSCREEN_ALL_APPS,
             test_app_list_presenter.app_list_state());
 }
 
@@ -128,19 +128,22 @@ class VoiceInteractionAppListButtonTest : public AppListButtonTest {
  public:
   VoiceInteractionAppListButtonTest() {}
 
-  void SetupCommandLine(base::CommandLine* command_line) override {
-    command_line->AppendSwitch(chromeos::switches::kEnableVoiceInteraction);
+  // AppListButtonTest:
+  void SetUp() override {
+    command_line_ = std::make_unique<base::test::ScopedCommandLine>();
+    command_line_->GetProcessCommandLine()->AppendSwitch(
+        chromeos::switches::kEnableVoiceInteraction);
+    EXPECT_TRUE(chromeos::switches::IsVoiceInteractionFlagsEnabled());
+    AppListButtonTest::SetUp();
   }
 
  private:
+  std::unique_ptr<base::test::ScopedCommandLine> command_line_;
   DISALLOW_COPY_AND_ASSIGN(VoiceInteractionAppListButtonTest);
 };
 
 TEST_F(VoiceInteractionAppListButtonTest,
        LongPressGestureWithVoiceInteractionFlag) {
-  EXPECT_TRUE(base::CommandLine::ForCurrentProcess()->HasSwitch(
-      chromeos::switches::kEnableVoiceInteraction));
-
   // Simulate two user with primary user as active.
   CreateUserSessions(2);
 
@@ -150,19 +153,14 @@ TEST_F(VoiceInteractionAppListButtonTest,
   ui::GestureEvent long_press =
       CreateGestureEvent(ui::GestureEventDetails(ui::ET_GESTURE_LONG_PRESS));
   SendGestureEvent(&long_press);
-  RunAllPendingInMessageLoop();
   EXPECT_EQ(1u, test_app_list_presenter.voice_session_count());
 
   // Test long press gesture on secondary display.
   SendGestureEventToSecondaryDisplay(&long_press);
-  RunAllPendingInMessageLoop();
   EXPECT_EQ(2u, test_app_list_presenter.voice_session_count());
 }
 
 TEST_F(VoiceInteractionAppListButtonTest, LongPressGestureWithSecondaryUser) {
-  EXPECT_TRUE(base::CommandLine::ForCurrentProcess()->HasSwitch(
-      chromeos::switches::kEnableVoiceInteraction));
-
   // Simulate two user with secondary user as active.
   SimulateUserLogin("user1@test.com");
   SimulateUserLogin("user2@test.com");
@@ -173,26 +171,17 @@ TEST_F(VoiceInteractionAppListButtonTest, LongPressGestureWithSecondaryUser) {
   ui::GestureEvent long_press =
       CreateGestureEvent(ui::GestureEventDetails(ui::ET_GESTURE_LONG_PRESS));
   SendGestureEvent(&long_press);
-  RunAllPendingInMessageLoop();
   // Voice interaction is disabled for secondary user, so the count here should
   // be 0.
   EXPECT_EQ(0u, test_app_list_presenter.voice_session_count());
 
   // Test long press gesture on secondary display.
   SendGestureEventToSecondaryDisplay(&long_press);
-  RunAllPendingInMessageLoop();
   EXPECT_EQ(0u, test_app_list_presenter.voice_session_count());
 }
 
 TEST_F(VoiceInteractionAppListButtonTest,
        LongPressGestureWithSettingsDisabled) {
-  app_list::test::TestAppListPresenter test_app_list_presenter;
-  Shell::Get()->app_list()->SetAppListPresenter(
-      test_app_list_presenter.CreateInterfacePtrAndBind());
-
-  EXPECT_TRUE(base::CommandLine::ForCurrentProcess()->HasSwitch(
-      chromeos::switches::kEnableVoiceInteraction));
-
   // Simulate two user with primary user as active.
   CreateUserSessions(2);
 
@@ -204,26 +193,17 @@ TEST_F(VoiceInteractionAppListButtonTest,
   ui::GestureEvent long_press =
       CreateGestureEvent(ui::GestureEventDetails(ui::ET_GESTURE_LONG_PRESS));
   SendGestureEvent(&long_press);
-  RunAllPendingInMessageLoop();
   // After value prop has been accepted, if voice interaction is disalbed in
   // settings we should not handle long press action in app list button.
   EXPECT_EQ(0u, test_app_list_presenter.voice_session_count());
 
   // Test long press gesture on secondary display.
   SendGestureEventToSecondaryDisplay(&long_press);
-  RunAllPendingInMessageLoop();
   EXPECT_EQ(0u, test_app_list_presenter.voice_session_count());
 }
 
 TEST_F(VoiceInteractionAppListButtonTest,
        LongPressGestureBeforeSetupCompleted) {
-  app_list::test::TestAppListPresenter test_app_list_presenter;
-  Shell::Get()->app_list()->SetAppListPresenter(
-      test_app_list_presenter.CreateInterfacePtrAndBind());
-
-  EXPECT_TRUE(base::CommandLine::ForCurrentProcess()->HasSwitch(
-      chromeos::switches::kEnableVoiceInteraction));
-
   // Simulate two user with primary user as active.
   CreateUserSessions(2);
 
@@ -233,14 +213,12 @@ TEST_F(VoiceInteractionAppListButtonTest,
   ui::GestureEvent long_press =
       CreateGestureEvent(ui::GestureEventDetails(ui::ET_GESTURE_LONG_PRESS));
   SendGestureEvent(&long_press);
-  RunAllPendingInMessageLoop();
   // Before setup flow completed we should show the animation even if the
   // settings are disabled.
   EXPECT_EQ(1u, test_app_list_presenter.voice_session_count());
 
   // Test long press gesture on secondary display.
   SendGestureEventToSecondaryDisplay(&long_press);
-  RunAllPendingInMessageLoop();
   EXPECT_EQ(2u, test_app_list_presenter.voice_session_count());
 }
 
