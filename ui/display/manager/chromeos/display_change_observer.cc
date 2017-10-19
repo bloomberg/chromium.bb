41c739dc0dc5da53437605f49abe949f5f12d580
Mitsuru Oshima <oshima@chromium.org>
2017-10-19 22:10:07 +0000

    Do not re-register dislay info if exists
    UpdateInternalDisplay should register the display info
    only if
    1) no internal display info  is registered. (it'll be
      treated as new display)
    2) it's disconnected
    
    BUG=774383
    TEST=covered by unit test. manually tested on kevin.
    
    Change-Id: Ifdf74ad7e61a0d5ffa617e0a72622e2ddb9bbebd
    Reviewed-on: https://chromium-review.googlesource.com/726262
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510235}
    Former-commit-id: ca1309e1130def3f01b093f53e083258ea214e78

--- ui/display/manager/chromeos/display_change_observer.cc
+++ ui/display/manager/chromeos/display_change_observer.cc
@@ -222,7 +222,15 @@ void DisplayChangeObserver::UpdateInternalDisplay(
         DCHECK_EQ(Display::InternalDisplayId(), state->display_id());
       Display::SetInternalDisplayId(state->display_id());
 
-      if (state->native_mode()) {
+      if (state->native_mode() &&
+          (!display_manager_->IsDisplayIdValid(state->display_id()) ||
+           !state->current_mode())) {
+        // Register the internal display info if
+        // 1) If it's not already registered. It'll be treated as
+        // new display in |UpdateDisplaysWith()|.
+        // 2) If it's not connected, because the display info will not
+        // be updated in |UpdateDisplaysWith()|, which will skips the
+        // disconnected displays.
         ManagedDisplayInfo new_info =
             CreateManagedDisplayInfo(state, state->native_mode());
         display_manager_->UpdateInternalDisplay(new_info);
