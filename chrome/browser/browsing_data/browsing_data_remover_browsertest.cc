7bd8560011640346acedbd6051606da51a7ff133
Christian Dullweber <dullweber@chromium.org>
2017-10-19 12:19:39 +0000

    Add browser_test for counting and deleting site data
    This test creates site data entries via javascript and tests that they
    are counted and deleted correctly.
    
    Bug: 769425
    Change-Id: I81178ade64a5d76fd19ee7e79c6a55c4c5ae32cc
    Reviewed-on: https://chromium-review.googlesource.com/725385
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510059}
    Former-commit-id: ab39f103c1d4da5ca51e6ea8517e17f5316def09

--- chrome/browser/browsing_data/browsing_data_remover_browsertest.cc
+++ chrome/browser/browsing_data/browsing_data_remover_browsertest.cc
@@ -14,7 +14,9 @@
 #include "chrome/browser/browsing_data/browsing_data_helper.h"
 #include "chrome/browser/browsing_data/cache_counter.h"
 #include "chrome/browser/browsing_data/chrome_browsing_data_remover_delegate.h"
+#include "chrome/browser/browsing_data/site_data_counting_helper.h"
 #include "chrome/browser/chrome_notification_types.h"
+#include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/external_protocol/external_protocol_handler.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -23,6 +25,7 @@
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/browsing_data/core/browsing_data_utils.h"
+#include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_thread.h"
@@ -63,6 +66,13 @@ class BrowsingDataRemoverBrowserTest : public InProcessBrowserTest {
     ASSERT_TRUE(content::ExecuteScriptAndExtractString(
         browser()->tab_strip_model()->GetActiveWebContents(), script, &data));
     ASSERT_EQ(data, result);
+  }
+
+  bool RunScriptAndGetBool(const std::string& script) {
+    bool data;
+    EXPECT_TRUE(content::ExecuteScriptAndExtractBool(
+        browser()->tab_strip_model()->GetActiveWebContents(), script, &data));
+    return data;
   }
 
   void VerifyDownloadCount(size_t expected) {
@@ -137,6 +147,54 @@ class BrowsingDataRemoverBrowserTest : public InProcessBrowserTest {
     completion_observer.BlockUntilCompletion();
   }
 
+  // Test a data type by creating a value and checking it is counted by the
+  // cookie counter. Then it deletes the value and checks that it has been
+  // deleted and the cookie counter is back to zero.
+  void TestSiteData(const std::string& type) {
+    EXPECT_EQ(0, GetSiteDataCount());
+    GURL url = embedded_test_server()->GetURL("/site_data.html");
+    ui_test_utils::NavigateToURL(browser(), url);
+    // We don't want to measure site engagement entries.
+    RemoveSiteEngagement();
+    EXPECT_EQ(0, GetSiteDataCount());
+    EXPECT_FALSE(HasDataForType(type));
+
+    SetDataForType(type);
+    EXPECT_EQ(1, GetSiteDataCount());
+    EXPECT_TRUE(HasDataForType(type));
+
+    RemoveAndWait(ChromeBrowsingDataRemoverDelegate::DATA_TYPE_SITE_DATA);
+    EXPECT_EQ(0, GetSiteDataCount());
+    EXPECT_FALSE(HasDataForType(type));
+  }
+
+  bool HasDataForType(const std::string& type) {
+    return RunScriptAndGetBool("has" + type + "()");
+  }
+
+  void SetDataForType(const std::string& type) {
+    ASSERT_TRUE(RunScriptAndGetBool("set" + type + "()"));
+  }
+
+  void RemoveSiteEngagement() {
+    HostContentSettingsMapFactory::GetForProfile(browser()->profile())
+        ->ClearSettingsForOneType(CONTENT_SETTINGS_TYPE_SITE_ENGAGEMENT);
+  }
+
+  int GetSiteDataCount() {
+    base::RunLoop run_loop;
+    int count = -1;
+    (new SiteDataCountingHelper(
+         browser()->profile(), base::Time(),
+         base::Bind(&BrowsingDataRemoverBrowserTest::OnCookieCountResult,
+                    base::Unretained(this), base::Unretained(&run_loop),
+                    base::Unretained(&count))))
+        ->CountAndDestroySelfWhenFinished();
+
+    run_loop.Run();
+    return count;
+  }
+
  private:
   void OnCacheSizeResult(
       base::RunLoop* run_loop,
@@ -149,6 +207,11 @@ class BrowsingDataRemoverBrowserTest : public InProcessBrowserTest {
         static_cast<browsing_data::BrowsingDataCounter::FinishedResult*>(
             result.get())
             ->Value();
+    run_loop->Quit();
+  }
+
+  void OnCookieCountResult(base::RunLoop* run_loop, int* out_count, int count) {
+    *out_count = count;
     run_loop->Quit();
   }
 };
@@ -186,7 +249,6 @@ IN_PROC_BROWSER_TEST_F(BrowsingDataRemoverBrowserTest, DownloadProhibited) {
 // Verify can modify database after deleting it.
 IN_PROC_BROWSER_TEST_F(BrowsingDataRemoverBrowserTest, Database) {
   GURL url = embedded_test_server()->GetURL("/simple_database.html");
-  LOG(ERROR) << url;
   ui_test_utils::NavigateToURL(browser(), url);
 
   RunScriptAndCheckResult("createTable()", "done");
@@ -258,4 +320,32 @@ IN_PROC_BROWSER_TEST_F(BrowsingDataRemoverBrowserTest,
   RemoveAndWait(ChromeBrowsingDataRemoverDelegate::DATA_TYPE_SITE_DATA);
   block_state = ExternalProtocolHandler::GetBlockState("tel", profile);
   ASSERT_EQ(ExternalProtocolHandler::UNKNOWN, block_state);
+}
+// Visiting a site creates an site engagement entry. Test that it is counted and
+// deleted properly.
+IN_PROC_BROWSER_TEST_F(BrowsingDataRemoverBrowserTest, SiteEngagementDeletion) {
+  EXPECT_EQ(0, GetSiteDataCount());
+  GURL url = embedded_test_server()->GetURL("/site_data.html");
+  ui_test_utils::NavigateToURL(browser(), url);
+  EXPECT_EQ(1, GetSiteDataCount());
+  RemoveAndWait(ChromeBrowsingDataRemoverDelegate::DATA_TYPE_SITE_DATA);
+  EXPECT_EQ(0, GetSiteDataCount());
+}
+
+IN_PROC_BROWSER_TEST_F(BrowsingDataRemoverBrowserTest, CookieDeletion) {
+  TestSiteData("Cookie");
+}
+
+IN_PROC_BROWSER_TEST_F(BrowsingDataRemoverBrowserTest, LocalStorageDeletion) {
+  TestSiteData("LocalStorage");
+}
+
+// TODO(crbug.com/772337): DISABLED until session storage is working correctly.
+IN_PROC_BROWSER_TEST_F(BrowsingDataRemoverBrowserTest,
+                       DISABLED_SessionStorageDeletion) {
+  TestSiteData("SessionStorage");
+}
+
+IN_PROC_BROWSER_TEST_F(BrowsingDataRemoverBrowserTest, FileSystemDeletion) {
+  TestSiteData("FileSystem");
 }
