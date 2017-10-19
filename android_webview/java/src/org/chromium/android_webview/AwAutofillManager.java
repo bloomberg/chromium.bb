bab5a128f4670e81e2991d7bf0ae6bdd1d03f567
Tao Bai <michaelbai@chromium.org>
2017-10-19 20:29:38 +0000

    [WebView autofill] Disable the autofill on application context
    Android autofill won't work with application context, disable
    Autofill if WebView is created with application context.
    
    Also add a warning log.
    
    Bug: 776152
    Change-Id: I3b9b05b43c6359b4f5f4339699b4143b1d0b84ad
    Reviewed-on: https://chromium-review.googlesource.com/726968
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Commit-Queue: Tao Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510188}
    Former-commit-id: aeb2dd08d7deec2998db3f0c37843cb1103e9954

--- android_webview/java/src/org/chromium/android_webview/AwAutofillManager.java
+++ android_webview/java/src/org/chromium/android_webview/AwAutofillManager.java
@@ -19,7 +19,6 @@ import java.lang.ref.WeakReference;
 /**
  * The class to call Android's AutofillManager.
  */
-// TODO(michaelbai): Extend this class to provide instrumentation test. http://crbug.com/717658.
 @TargetApi(Build.VERSION_CODES.O)
 public class AwAutofillManager {
     private static final String TAG = "AwAutofillManager";
@@ -42,59 +41,67 @@ public class AwAutofillManager {
     private AutofillManager mAutofillManager;
     private boolean mIsAutofillInputUIShowing;
     private AutofillInputUIMonitor mMonitor;
+    private boolean mDestroyed;
 
     public AwAutofillManager(Context context) {
+        if (AwContents.activityFromContext(context) == null) {
+            Log.w(TAG,
+                    "WebView autofill is disabled because WebView isn't created with "
+                            + "activity context.");
+            return;
+        }
         mAutofillManager = context.getSystemService(AutofillManager.class);
         mMonitor = new AutofillInputUIMonitor(this);
         mAutofillManager.registerCallback(mMonitor);
     }
 
     public void notifyVirtualValueChanged(View parent, int childId, AutofillValue value) {
-        if (isDestroyed()) return;
+        if (isDestroyed() || mAutofillManager == null) return;
         mAutofillManager.notifyValueChanged(parent, childId, value);
     }
 
     public void commit() {
-        if (isDestroyed()) return;
+        if (isDestroyed() || mAutofillManager == null) return;
         mAutofillManager.commit();
     }
 
     public void cancel() {
-        if (isDestroyed()) return;
+        if (isDestroyed() || mAutofillManager == null) return;
         mAutofillManager.cancel();
     }
 
     public void notifyVirtualViewEntered(View parent, int childId, Rect absBounds) {
-        if (isDestroyed()) return;
+        if (isDestroyed() || mAutofillManager == null) return;
         mAutofillManager.notifyViewEntered(parent, childId, absBounds);
     }
 
     public void notifyVirtualViewExited(View parent, int childId) {
-        if (isDestroyed()) return;
+        if (isDestroyed() || mAutofillManager == null) return;
         mAutofillManager.notifyViewExited(parent, childId);
     }
 
     public void requestAutofill(View parent, int virtualId, Rect absBounds) {
-        if (isDestroyed()) return;
+        if (isDestroyed() || mAutofillManager == null) return;
         mAutofillManager.requestAutofill(parent, virtualId, absBounds);
     }
 
     public boolean isAutofillInputUIShowing() {
-        if (isDestroyed()) return false;
+        if (isDestroyed() || mAutofillManager == null) return false;
         return mIsAutofillInputUIShowing;
     }
 
     public void destroy() {
-        if (isDestroyed()) return;
+        if (isDestroyed() || mAutofillManager == null) return;
         mAutofillManager.unregisterCallback(mMonitor);
         mAutofillManager = null;
+        mDestroyed = true;
     }
 
     private boolean isDestroyed() {
-        if (mAutofillManager == null) {
+        if (mDestroyed) {
             Log.w(TAG, "Application attempted to call on a destroyed AwAutofillManager",
                     new Throwable());
         }
-        return mAutofillManager == null;
+        return mDestroyed;
     }
 }
