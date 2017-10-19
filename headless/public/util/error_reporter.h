1ada4632726cd0002f65df2c0c2d7b1160baf9db
François Degros <fdegros@chromium.org>
2017-10-19 01:36:36 +0000

    Trivial code cleanup. No functional change.
    Change-Id: Iae4b1a807c0984ea41dff9ffeaa45337dc8b9cdc
    Reviewed-on: https://chromium-review.googlesource.com/725099
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: David Vallet <dvallet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#509964}
    Former-commit-id: 8941eabbec64c84e8123f5743edaad261bd6c772

--- headless/public/util/error_reporter.h
+++ headless/public/util/error_reporter.h
@@ -42,13 +42,13 @@ class HEADLESS_EXPORT ErrorReporter {
 
   // Returns a string containing all the errors concatenated together.
   std::string ToString() const;
-#else  // DCHECK_IS_ON()
-  inline void Push() {}
-  inline void Pop() {}
-  inline void SetName(const char* name) {}
-  inline void AddError(base::StringPiece description) {}
-  inline bool HasErrors() const { return false; }
-  std::vector<std::string> errors() const { return std::vector<std::string>(); }
+#else   // DCHECK_IS_ON()
+  void Push() {}
+  void Pop() {}
+  void SetName(const char* name) {}
+  void AddError(base::StringPiece description) {}
+  bool HasErrors() const { return false; }
+  std::vector<std::string> errors() const { return {}; }
   std::string ToString() const { return ""; }
 #endif  // DCHECK_IS_ON()
 
