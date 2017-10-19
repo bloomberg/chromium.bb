04db55fe8b4bb83f1aafd89f3e3f75ab2128b241
Xiaocheng Hu <xiaochengh@chromium.org>
2017-10-19 17:54:56 +0000

    [LayoutNG] Add NG alternatives of LayoutText::ContainsCaretOffset
    This patch adds LayoutNG alternatives of LayoutText::ContainsCaretOffset for
    both "common" LayoutText and ::first-letter LayoutTextFragments, with an
    extensive set of unit tests.
    
    Bug: 771398
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I59b17046c271bf8c5c1fb4fdf7421b37a3183073
    Reviewed-on: https://chromium-review.googlesource.com/724262
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510130}
    Former-commit-id: 63d12e3af49b452a4ed2e0e01196ba6a249af9b2

--- third_party/WebKit/Source/core/layout/LayoutText.h
+++ third_party/WebKit/Source/core/layout/LayoutText.h
@@ -206,7 +206,7 @@ class CORE_EXPORT LayoutText : public LayoutObject {
   // or segment break in node with style white-space: pre/pre-line/pre-wrap).
   // TODO(editing-dev): The behavior is introduced by crrev.com/e3eb4e in
   // InlineTextBox::ContainsCaretOffset(). Try to understand it.
-  bool ContainsCaretOffset(int) const;
+  virtual bool ContainsCaretOffset(int) const;
 
   int CaretMinOffset() const override;
   int CaretMaxOffset() const override;
--- third_party/WebKit/Source/core/layout/LayoutTextFragment.h
+++ third_party/WebKit/Source/core/layout/LayoutTextFragment.h
@@ -51,6 +51,7 @@ class CORE_EXPORT LayoutTextFragment final : public LayoutText {
 
   bool IsTextFragment() const override { return true; }
 
+  bool ContainsCaretOffset(int) const override;
   int CaretMinOffset() const override;
   int CaretMaxOffset() const override;
   unsigned ResolvedTextLength() const override;
