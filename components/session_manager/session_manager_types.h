03fb39a415b58be98712d2f44269f1d33c31c086
Qiang Xu <warx@chromium.org>
2017-10-20 21:23:02 +0000

    cros: make post logged in UI flow as part of login
    changes:
    After user sign in, there might be other session initialization or UI
    flow which is still on the same login UI. Make this state part of login
    session state.  And make LOGGED_IN_NOT_ACTIVE session state just used
    for UserSessionManager::DoBrowserLaunch().
    
    Bug: 772586
    Test: tested on eve for crbug.com/775286
    Change-Id: Idb244e6970da552ace4bc8e16d8b2cad4ce0d491
    Reviewed-on: https://chromium-review.googlesource.com/729748
    Commit-Queue: Qiang(Joe) Xu <warx@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510564}
    Former-commit-id: 4d137301a986a5cfd5cf3025500135774fef09b5

--- components/session_manager/session_manager_types.h
+++ components/session_manager/session_manager_types.h
@@ -17,14 +17,13 @@ enum class SessionState {
   // Running out of box UI.
   OOBE,
 
-  // Running login UI (primary user) but user sign in hasn't completed yet.
+  // Running login UI (primary user), including the post login steps such as
+  // selecting avatar, agreeing to terms of service etc.
   LOGIN_PRIMARY,
 
-  // Running login UI (primary or secondary user), user sign in has been
-  // completed but login UI hasn't been hidden yet. This means that either
-  // some session initialization is happening or user has to go through some
-  // UI flow on the same login UI like select avatar, agree to terms of
-  // service etc.
+  // A transient state between LOGIN_PRIMARY/LOGIN_SECONDARY and ACTIVE to
+  // prepare user desktop environment (e.g. launching browser windows) while the
+  // login screen is still visible.
   LOGGED_IN_NOT_ACTIVE,
 
   // A user(s) has logged in *and* login UI is hidden i.e. user session is
