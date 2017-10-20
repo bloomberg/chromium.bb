e09cf4646cf6fe0f2efeffe0e4c841a286e13272
Vadym Doroshenko <dvadym@chromium.org>
2017-10-20 15:57:06 +0000

    Moving password-manager-internals resources to components/.
    This is the first CL for implementing password-manager-internals in Bling.
    In order to reuse resources on IOS they should be moved from
    chrome/browser/ to components/.
    
    Bug: 775894
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I4b9cc6096bba1ce215821be13f55ed8b0659fad9
    Reviewed-on: https://chromium-review.googlesource.com/725709
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510451}
    Former-commit-id: 15fb1d7e0f94d7f4cfa3be036f615354000a51d6

--- chrome/browser/ui/webui/password_manager_internals/password_manager_internals_ui.cc
+++ chrome/browser/ui/webui/password_manager_internals/password_manager_internals_ui.cc
@@ -12,7 +12,7 @@
 #include "base/values.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/url_constants.h"
-#include "chrome/grit/password_manager_internals_resources.h"
+#include "components/grit/components_resources.h"
 #include "components/password_manager/content/browser/password_manager_internals_service_factory.h"
 #include "components/password_manager/core/browser/password_manager_internals_service.h"
 #include "components/password_manager/core/common/password_manager_pref_names.h"
