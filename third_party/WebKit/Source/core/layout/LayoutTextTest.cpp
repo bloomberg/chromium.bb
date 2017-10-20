a7e61b7ac9e06dd30435224f5a71dc262704c13a
Xiaocheng Hu <xiaochengh@chromium.org>
2017-10-20 02:06:30 +0000

    Parameterize LayoutText[Fragment]Test with LayoutNG
    This patch parameterize the two test base classes to reduce code
    duplication.
    
    Bug: 
    Change-Id: I42d64ef2028458b437b7471faf49f90562554cee
    Reviewed-on: https://chromium-review.googlesource.com/729216
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510296}
    Former-commit-id: 6a534a5430beb7d6399423a7d3298b4625242f86

--- third_party/WebKit/Source/core/layout/LayoutTextTest.cpp
+++ third_party/WebKit/Source/core/layout/LayoutTextTest.cpp
@@ -28,6 +28,22 @@ class LayoutTextTest : public RenderingTest {
 
 const char kTacoText[] = "Los Compadres Taco Truck";
 
+// Helper class to run the same test code with and without LayoutNG
+class ParameterizedLayoutTextTest : public ::testing::WithParamInterface<bool>,
+                                    private ScopedLayoutNGForTest,
+                                    private ScopedLayoutNGPaintFragmentsForTest,
+                                    public LayoutTextTest {
+ public:
+  ParameterizedLayoutTextTest()
+      : ScopedLayoutNGForTest(GetParam()),
+        ScopedLayoutNGPaintFragmentsForTest(GetParam()) {}
+
+ protected:
+  bool LayoutNGEnabled() const { return GetParam(); }
+};
+
+INSTANTIATE_TEST_CASE_P(All, ParameterizedLayoutTextTest, ::testing::Bool());
+
 }  // namespace
 
 TEST_F(LayoutTextTest, WidthZeroFromZeroLength) {
@@ -96,10 +112,7 @@ TEST_F(LayoutTextTest, WidthLengthBeyondLength) {
   ASSERT_LE(width, 20.f);
 }
 
-TEST_F(LayoutTextTest, CaretMinMaxOffsetNG) {
-  ScopedLayoutNGForTest layout_ng(true);
-  ScopedLayoutNGPaintFragmentsForTest layout_ng_paint_fragments(true);
-
+TEST_P(ParameterizedLayoutTextTest, CaretMinMaxOffset) {
   SetBasicBody("foo");
   EXPECT_EQ(0, GetBasicText()->CaretMinOffset());
   EXPECT_EQ(3, GetBasicText()->CaretMaxOffset());
@@ -117,10 +130,7 @@ TEST_F(LayoutTextTest, CaretMinMaxOffsetNG) {
   EXPECT_EQ(4, GetBasicText()->CaretMaxOffset());
 }
 
-TEST_F(LayoutTextTest, ResolvedTextLengthNG) {
-  ScopedLayoutNGForTest layout_ng(true);
-  ScopedLayoutNGPaintFragmentsForTest layout_ng_paint_fragments(true);
-
+TEST_P(ParameterizedLayoutTextTest, ResolvedTextLength) {
   SetBasicBody("foo");
   EXPECT_EQ(3u, GetBasicText()->ResolvedTextLength());
 
@@ -134,60 +144,7 @@ TEST_F(LayoutTextTest, ResolvedTextLengthNG) {
   EXPECT_EQ(3u, GetBasicText()->ResolvedTextLength());
 }
 
-TEST_F(LayoutTextTest, ContainsCaretOffset) {
-  // This test records the behavior introduced in crrev.com/e3eb4e
-  SetBasicBody(" foo   bar ");
-  EXPECT_FALSE(GetBasicText()->ContainsCaretOffset(0));   // "| foo   bar "
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(1));    // " |foo   bar "
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(2));    // " f|oo   bar "
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(3));    // " fo|o   bar "
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(4));    // " foo|   bar "
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(5));    // " foo |  bar "
-  EXPECT_FALSE(GetBasicText()->ContainsCaretOffset(6));   // " foo  | bar "
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(7));    // " foo   |bar "
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(8));    // " foo   b|ar "
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(9));    // " foo   ba|r "
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(10));   // " foo   bar| "
-  EXPECT_FALSE(GetBasicText()->ContainsCaretOffset(11));  // " foo   bar |"
-}
-
-TEST_F(LayoutTextTest, ContainsCaretOffsetInPre) {
-  // These tests record the behavior introduced in crrev.com/e3eb4e
-
-  SetBodyInnerHTML("<pre id='target'>foo   bar</pre>");
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(0));  // "|foo   bar"
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(1));  // "f|oo   bar"
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(2));  // "fo|o   bar"
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(3));  // "foo|   bar"
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(4));  // "foo |  bar"
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(5));  // "foo  | bar"
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(6));  // "foo   |bar"
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(7));  // "foo   b|ar"
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(8));  // "foo   ba|r"
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(9));  // "foo   bar|"
-
-  SetBodyInnerHTML("<pre id='target'>foo\n</div>");
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(0));   // "|foo\n"
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(1));   // "f|oo\n"
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(2));   // "fo|o\n"
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(3));   // "foo|\n"
-  EXPECT_FALSE(GetBasicText()->ContainsCaretOffset(4));  // "foo\n|"
-
-  SetBodyInnerHTML("<pre id='target'>foo\nbar</pre>");
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(0));  // "|foo\nbar"
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(1));  // "f|oo\nbar"
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(2));  // "fo|o\nbar"
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(3));  // "foo|\nbar"
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(4));  // "foo\n|bar"
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(5));  // "foo\nb|ar"
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(6));  // "foo\nba|r"
-  EXPECT_TRUE(GetBasicText()->ContainsCaretOffset(7));  // "foo\nbar|"
-}
-
-TEST_F(LayoutTextTest, ContainsCaretOffsetNG) {
-  ScopedLayoutNGForTest layout_ng(true);
-  ScopedLayoutNGPaintFragmentsForTest layout_ng_paint_fragments(true);
-
+TEST_P(ParameterizedLayoutTextTest, ContainsCaretOffset) {
   // This test records the behavior introduced in crrev.com/e3eb4e
   SetBasicBody(" foo   bar ");
   EXPECT_FALSE(GetBasicText()->ContainsCaretOffset(0));   // "| foo   bar "
@@ -205,10 +162,7 @@ TEST_F(LayoutTextTest, ContainsCaretOffsetNG) {
   EXPECT_FALSE(GetBasicText()->ContainsCaretOffset(12));  // out of range
 }
 
-TEST_F(LayoutTextTest, ContainsCaretOffsetNGInPre) {
-  ScopedLayoutNGForTest layout_ng(true);
-  ScopedLayoutNGPaintFragmentsForTest layout_ng_paint_fragments(true);
-
+TEST_P(ParameterizedLayoutTextTest, ContainsCaretOffsetInPre) {
   // These tests record the behavior introduced in crrev.com/e3eb4e
 
   SetBodyInnerHTML("<pre id='target'>foo   bar</pre>");
