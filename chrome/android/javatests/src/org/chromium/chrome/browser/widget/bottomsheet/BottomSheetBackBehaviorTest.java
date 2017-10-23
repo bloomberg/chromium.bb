06b42aa9e652510e3d9750632875faf71c849963
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2017-10-23 20:30:51 +0000

    Fix flaky testBackButton_backFromInternalNewTab
    Chrome wouldn't immediately close after clicking back, so we need
    to wait a bit longer.
    
    Bug: 764950
    Change-Id: I5daecbf556992afdcd94f5fb9c1d03d6230e4ca4
    Reviewed-on: https://chromium-review.googlesource.com/729300
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510903}
    Former-commit-id: f232ce9c11c1580bd9a45bd3517f2c94514c4f96

--- chrome/android/javatests/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheetBackBehaviorTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheetBackBehaviorTest.java
@@ -128,7 +128,7 @@ public class BottomSheetBackBehaviorTest {
 
         assertEquals("The bottom sheet should be peeking.", BottomSheet.SHEET_STATE_PEEK,
                 mBottomSheet.getSheetState());
-        assertFalse("Chrome should no longer have focus.", mActivity.hasWindowFocus());
+        waitForClose();
     }
 
     @Test
@@ -164,7 +164,7 @@ public class BottomSheetBackBehaviorTest {
 
         assertEquals("The bottom sheet should be peeking.", BottomSheet.SHEET_STATE_PEEK,
                 mBottomSheet.getSheetState());
-        assertFalse("Chrome should no longer have focus.", mActivity.hasWindowFocus());
+        waitForClose();
     }
 
     @Test
@@ -241,7 +241,7 @@ public class BottomSheetBackBehaviorTest {
 
         assertEquals("The bottom sheet should be peeking.", BottomSheet.SHEET_STATE_PEEK,
                 mBottomSheet.getSheetState());
-        assertFalse("Chrome should no longer have focus.", mActivity.hasWindowFocus());
+        waitForClose();
     }
 
     /**
@@ -333,6 +333,17 @@ public class BottomSheetBackBehaviorTest {
             @Override
             public void run() {
                 mBottomSheet.endAnimations();
+            }
+        });
+    }
+
+    /** Wait until Chrome doesn't have window focus. */
+    private void waitForClose() {
+        // It takes some time for Chrome to completely close.
+        CriteriaHelper.pollUiThread(new Criteria() {
+            @Override
+            public boolean isSatisfied() {
+                return !mActivity.hasWindowFocus();
             }
         });
     }
