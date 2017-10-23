7821a961448f36a48d6a15c35353daf51b8a696b
Hans Wennborg <hans@chromium.org>
2017-10-23 17:58:44 +0000

    Fix Win/Clang -Wsign-compare warning
    enums are backed by signed integers by default on Windows, and Clang
    (after r316268) would warn about comparing 'code', which is signed, with
    sizeof(..) which is unsigned.
    
    Bug: 777363
    Change-Id: I4f69d92e9f770a1a9011dbd8b76ffaea1fadc469
    Reviewed-on: https://chromium-review.googlesource.com/733225
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510842}
    Former-commit-id: 7378f29a80837745d43076cb5b02a13587a72f30

--- third_party/expat/files/lib/xmlparse.c
+++ third_party/expat/files/lib/xmlparse.c
@@ -2426,7 +2426,7 @@ XML_ErrorString(enum XML_Error code)
     XML_L("reserved prefix (xmlns) must not be declared or undeclared"),
     XML_L("prefix must not be bound to one of the reserved namespace names")
   };
-  if (code > 0 && code < sizeof(message)/sizeof(message[0]))
+  if (code > 0 && (size_t)code < sizeof(message)/sizeof(message[0]))
     return message[code];
   return NULL;
 }
