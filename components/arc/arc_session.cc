e2309170113d91438bba2089d6c18648763cd6f7
Hidehiko Abe <hidehiko@chromium.org>
2017-10-23 08:06:05 +0000

    Fix container id assignment.
    https://chromium-review.googlesource.com/c/chromium/src/+/723679
    mistakenly skiped to assign container_id for full instance.
    Specifically, this caused to fail "adb reboot", because
    ArcInstanceStop() call for the instance will be ignored
    unexpectedly, so that restarting wasn't be triggered.
    
    BUG=b/68112711
    TEST=Manually made sure "adb reboot" works.
    
    Change-Id: I25aff171408a2d8fc27b405b78c561300892583f
    Reviewed-on: https://chromium-review.googlesource.com/732800
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510733}
    Former-commit-id: 3b20f40fcebc96b3465ece8486d39d3b53b49519

--- components/arc/arc_session.cc
+++ components/arc/arc_session.cc
@@ -370,7 +370,8 @@ void ArcSessionImpl::OnMiniInstanceStarted(
 
   DCHECK(!container_instance_id.empty());
   container_instance_id_ = container_instance_id;
-  VLOG(2) << "ARC mini instance is successfully started.";
+  VLOG(2) << "ARC mini instance is successfully started: "
+          << container_instance_id_;
 
   if (stop_requested_) {
     // The ARC instance has started to run. Request to stop.
@@ -443,7 +444,9 @@ void ArcSessionImpl::OnFullInstanceStarted(
     DCHECK(!container_instance_id_.empty());
   } else {
     // This was a request to start a full instance from scratch.
-    VLOG(2) << "ARC instance is successfully started.";
+    container_instance_id_ = container_instance_id;
+    VLOG(2) << "ARC instance is successfully started: "
+            << container_instance_id_;
   }
 
   if (stop_requested_) {
