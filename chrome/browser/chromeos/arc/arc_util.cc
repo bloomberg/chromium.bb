c42c53f98dd44a97d5c82c50bb811ea5e40c1660
khmel <khmel@google.com>
2017-10-23 21:37:08 +0000

    Revert "Do not start ARC cotnainer if chrome will be restarted due to flag"
    This reverts commit e5afdfce673b4491ba930624beb9a1d45de2f2a8 [formerly a26daee641b2f2b02e621d44f224d268a2ebc87e].
    
    This CL looks dangerous once IsArcAllowedForProfile needs to be
    persistent for all calls during the user session lifetime. Otherwise
    some ARC component may have wrong assumption. Crashes in go/rfunq
    contain in log "ARC because chrome will restart" so this looks as the
    reason of this.
    
    Based on offline discussion with oshima@ now this CL is less important
    starting from M62. Let merge this revert and see how this affects
    stability.
    
    Test: N/A
    Bug: 775861
    Bug: 758820
    Change-Id: Icab0dfe95402c1fdcfde11333522f74ae8236fb1
    Reviewed-on: https://chromium-review.googlesource.com/734283
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Yury Khmel <khmel@google.com>
    Cr-Commit-Position: refs/heads/master@{#510928}
    Former-commit-id: 0fc86dec47963d4c4deb6e945a58aa8366d42eee

--- chrome/browser/chromeos/arc/arc_util.cc
+++ chrome/browser/chromeos/arc/arc_util.cc
@@ -20,7 +20,6 @@
 #include "base/threading/thread_restrictions.h"
 #include "chrome/browser/chromeos/arc/arc_session_manager.h"
 #include "chrome/browser/chromeos/arc/policy/arc_policy_util.h"
-#include "chrome/browser/chromeos/login/session/user_session_manager.h"
 #include "chrome/browser/chromeos/login/user_flow.h"
 #include "chrome/browser/chromeos/login/users/chrome_user_manager.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
@@ -223,13 +222,6 @@ bool IsArcAllowedForProfile(const Profile* profile) {
   if (!user_flow || !user_flow->CanStartArc()) {
     VLOG_IF(1, IsReportingFirstTimeForProfile(profile))
         << "ARC is not allowed in the current user flow.";
-    return false;
-  }
-
-  if (chromeos::UserSessionManager::NeedRestartToApplyPerSessionFlagsForProfile(
-          profile)) {
-    // Quickly restarting ARC instance can cause black screen. crbug.com/758820.
-    VLOG(1) << "Do not start ARC because chrome will restart";
     return false;
   }
 
