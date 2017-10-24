d98a9ed7599c68bc279e02f12280985cb22a2ea8
Daniel Cheng <dcheng@chromium.org>
2017-10-24 16:55:16 +0000

    Automated fixes from the Chrome clang plugin in //third_party/WebKit/Source/modules/broadcastchannel
    - normalizes uses of virtual / override / final
    - ensures that auto is const / pointer / reference qualified as
      appropriate
    
    This CL was uploaded by git cl split.
    
    R=mek@chromium.org
    
    Bug: 706466
    Change-Id: I3770d73f9fbd8607e5acf6233a84992d3aac6ef0
    Reviewed-on: https://chromium-review.googlesource.com/735500
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511179}
    Former-commit-id: e45bd89263306db3eac9599a1cf51f95cbf95296

--- third_party/WebKit/Source/modules/broadcastchannel/BroadcastChannel.h
+++ third_party/WebKit/Source/modules/broadcastchannel/BroadcastChannel.h
@@ -51,7 +51,7 @@ class BroadcastChannel final : public EventTargetWithInlineData,
   // ContextLifecycleObserver:
   void ContextDestroyed(ExecutionContext*) override;
 
-  virtual void Trace(blink::Visitor*);
+  void Trace(blink::Visitor*) override;
 
  private:
   BroadcastChannel(ExecutionContext*, const String& name);
