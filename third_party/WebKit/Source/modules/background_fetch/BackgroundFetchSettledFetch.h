4e55f8b78929aea3d908c29de979cd6f8ad64791
Daniel Cheng <dcheng@chromium.org>
2017-10-24 12:16:31 +0000

    Automated fixes from the Chrome clang plugin in //third_party/WebKit/Source/modules/background_fetch
    - normalizes uses of virtual / override / final
    - ensures that auto is const / pointer / reference qualified as
      appropriate
    
    This CL was uploaded by git cl split.
    
    R=peter@chromium.org
    
    Bug: 706466
    Change-Id: I08b8f6b57fc9ddf006439f8760850f33812eef15
    Reviewed-on: https://chromium-review.googlesource.com/734843
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511112}
    Former-commit-id: 2ef3e9c91f808f922b2310405a62122ce0eabda4

--- third_party/WebKit/Source/modules/background_fetch/BackgroundFetchSettledFetch.h
+++ third_party/WebKit/Source/modules/background_fetch/BackgroundFetchSettledFetch.h
@@ -28,7 +28,7 @@ class BackgroundFetchSettledFetch final : public BackgroundFetchFetch {
   // Web Exposed attribute defined in the IDL file.
   Response* response() const;
 
-  virtual void Trace(blink::Visitor*);
+  void Trace(blink::Visitor*) override;
 
  private:
   BackgroundFetchSettledFetch(Request*, Response*);
--- third_party/WebKit/Source/modules/background_fetch/BackgroundFetchedEvent.h
+++ third_party/WebKit/Source/modules/background_fetch/BackgroundFetchedEvent.h
@@ -54,7 +54,7 @@ class MODULES_EXPORT BackgroundFetchedEvent final
   // ExtendableEvent interface.
   const AtomicString& InterfaceName() const override;
 
-  virtual void Trace(blink::Visitor*);
+  void Trace(blink::Visitor*) override;
 
  private:
   BackgroundFetchedEvent(const AtomicString& type,
