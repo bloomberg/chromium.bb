f8735af426db4d5b2742030db1a30ccb617fbd1e
Sergey Ulanov <sergeyu@chromium.org>
2017-10-24 17:11:43 +0000

    Add BASE_EXPORT for GetDefaultJob() and SetDefaultJob()
    //content uses SetDefaultJob(), so component build fails on Fuchsia
    
    Change-Id: I00f15e841839c0d7fc1dd396c9cc28e4acfd5dfe
    Reviewed-on: https://chromium-review.googlesource.com/734704
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511186}
    Former-commit-id: 481a04890e2f3d606b23da375eb1e9d4f8d37b90

--- base/fuchsia/default_job.h
+++ base/fuchsia/default_job.h
@@ -5,6 +5,7 @@
 #ifndef BASE_FUCHSIA_DEFAULT_JOB_H_
 #define BASE_FUCHSIA_DEFAULT_JOB_H_
 
+#include "base/base_export.h"
 #include "base/fuchsia/scoped_zx_handle.h"
 
 namespace base {
@@ -13,8 +14,8 @@ namespace base {
 // and looking them up by their process IDs.
 // zx_job_default() will be returned if no job is explicitly set here.
 // Only valid handles may be passed to SetDefaultJob().
-zx_handle_t GetDefaultJob();
-void SetDefaultJob(ScopedZxHandle job);
+BASE_EXPORT zx_handle_t GetDefaultJob();
+BASE_EXPORT void SetDefaultJob(ScopedZxHandle job);
 
 }  // namespace base
 
