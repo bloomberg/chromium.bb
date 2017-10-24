e68126aedd3196b32a2b48a95c016d36acbef5c3
Mike Klein <mtklein@chromium.org>
2017-10-24 01:37:12 +0000

    Define SK_LEGACY_LOWP_STAGES.
    This guards https://skia-review.googlesource.com/c/skia/+/62520,
    which will change about 20 layout tests.
    
    Change-Id: Ia20bea78b64133cdd410f04d0260dc9d6f037eb1
    Reviewed-on: https://chromium-review.googlesource.com/733616
    Commit-Queue: Mike Klein <mtklein@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#510993}
    Former-commit-id: 032c90c0a119f2b78645416bf1734ce55c6086b1

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -232,6 +232,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_AA_CHOICE
 #endif
 
+#ifndef SK_LEGACY_LOWP_STAGES
+#define SK_LEGACY_LOWP_STAGES
+#endif
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
