471702dd13131ffdb4d1919e9555deaa2227193d
Ian Clelland <iclelland@google.com>
2017-10-24 20:10:21 +0000

    Count uses of navigator.getBattery which would be blocked by spec
    The most recent Battery Status API specification says that calls to getBattery
    from secure contexts, or from frames which are cross-origin with the top-level
    document, should return a promise which rejects. This doesn't match Chrome's
    implementation, so this CL adds use counters to evaluate the real-world effects
    that aligning with the spec would have.
    
    Bug: 747610
    Change-Id: I2b0a387b1e61510700191946e345d9cef9a10e69
    Reviewed-on: https://chromium-review.googlesource.com/733865
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511243}
    Former-commit-id: 40b9767c09612064d25a3470caf175d093f3c94a

--- third_party/WebKit/Source/core/frame/UseCounter.cpp
+++ third_party/WebKit/Source/core/frame/UseCounter.cpp
@@ -30,6 +30,7 @@
 #include "core/dom/Document.h"
 #include "core/dom/ExecutionContext.h"
 #include "core/frame/Deprecation.h"
+#include "core/frame/Frame.h"
 #include "core/frame/FrameConsole.h"
 #include "core/frame/LocalFrame.h"
 #include "core/frame/LocalFrameClient.h"
@@ -1204,6 +1205,36 @@ bool UseCounter::HasRecordedMeasurement(WebFeature feature) const {
   DCHECK_GE(WebFeature::kNumberOfFeatures, feature);
 
   return features_recorded_.QuickGet(static_cast<int>(feature));
+}
+
+// Static
+void UseCounter::CountIfFeatureWouldBeBlockedByFeaturePolicy(
+    const LocalFrame& frame,
+    WebFeature blocked_cross_origin,
+    WebFeature blocked_same_origin) {
+  // Get the origin of the top-level document
+  SecurityOrigin* topOrigin =
+      frame.Tree().Top().GetSecurityContext()->GetSecurityOrigin();
+
+  // Check if this frame is same-origin with the top-level
+  if (!frame.GetSecurityContext()->GetSecurityOrigin()->CanAccess(topOrigin)) {
+    // This frame is cross-origin with the top-level frame, and so would be
+    // blocked without a feature policy.
+    UseCounter::Count(&frame, blocked_cross_origin);
+    return;
+  }
+
+  // Walk up the frame tree looking for any cross-origin embeds. Even if this
+  // frame is same-origin with the top-level, if it is embedded by a cross-
+  // origin frame (like A->B->A) it would be blocked without a feature policy.
+  const Frame* f = &frame;
+  while (!f->IsMainFrame()) {
+    if (!f->GetSecurityContext()->GetSecurityOrigin()->CanAccess(topOrigin)) {
+      UseCounter::Count(&frame, blocked_same_origin);
+      return;
+    }
+    f = f->Tree().Parent();
+  }
 }
 
 void UseCounter::Trace(blink::Visitor* visitor) {
