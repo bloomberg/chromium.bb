4acfdd0ac31eafff43bc3181a1ac7f131d0662b9
Michael Hablich <hablich@chromium.org>
2017-10-24 19:13:03 +0000

    [benchmarks] Add --future variants for Speedometer1/2
    This enables V8 developers to see the performance
    of upcoming VM features that are not yet ready
    to be shipped.
    
    R=nednguyen@google.com
    
    Bug: 772325, 777818
    Change-Id: I718c7494609dd89833c5a27cd507b5e57981435a
    Reviewed-on: https://chromium-review.googlesource.com/735148
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Michael Hablich <hablich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511227}
    Former-commit-id: c3a375964a43a0e4bbc9bed4c318abee8d38c6b3

--- tools/perf/benchmarks/speedometer2.py
+++ tools/perf/benchmarks/speedometer2.py
@@ -96,3 +96,18 @@ class Speedometer2(perf_benchmark.PerfBenchmark):
     ps.AddStory(page_module.Page(
        'file://InteractiveRunner.html', ps, ps.base_dir, name='Speedometer2'))
     return ps
+
+
+@benchmark.Owner(emails=['hablich@chromium.org'])
+class V8Speedometer2Future(Speedometer2):
+  """Speedometer2 benchmark with the V8 flag --future.
+
+  Shows the performance of upcoming V8 VM features.
+  """
+
+  @classmethod
+  def Name(cls):
+    return 'speedometer2-future'
+
+  def SetExtraBrowserOptions(self, options):
+    options.AppendExtraBrowserArgs('--enable-features=V8VmFuture')
