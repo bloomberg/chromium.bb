ee0f18a958e39d2e82e5acbc56e810f309906be6
Vaclav Brozek <vabr@chromium.org>
2017-10-24 15:38:32 +0000

    Separate authentication logic from PasswordManagerPresenter
    Currently, PasswordManagerPresenter offers OS-level reauthentication to
    be used before revealing passwords in settings. This reauthentication
    has a platform independent logic concerning time spans of
    reauthentication attempt validity, and platform dependent OS calls to
    show appropriate UI. This is all inside one method within
    PasswordManagerPresenter. That makes it hard to test both the
    time-handling logic, and the fact that the rest of
    PasswordManagerPresenter invokes reauthentication as needed.
    
    Therefore, this CL separates that logic into a class,
    PasswordAccessAuthenticator, which contains precisely the
    platform-independent logic. An object of that class gets a callback to
    invoke the OS-dependent UI calls, which allows mocking it. Therefore the
    logic itself becomes easily testable, and also PasswordManagerPresenter
    can be tested for using reauthentication.
    
    Bug: 700003
    Change-Id: Ie694895c9e730be095712b95e64f320601fc0f8f
    Reviewed-on: https://chromium-review.googlesource.com/733131
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511158}
    Former-commit-id: 885a93b7999b908a2440bc5d330b94dde368318e

---
+++ chrome/browser/ui/passwords/password_access_authenticator.h
@@ -0,0 +1,62 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_PASSWORDS_PASSWORD_ACCESS_AUTHENTICATOR_H_
+#define CHROME_BROWSER_UI_PASSWORDS_PASSWORD_ACCESS_AUTHENTICATOR_H_
+
+#include <memory>
+
+#include "base/callback.h"
+#include "base/macros.h"
+#include "base/optional.h"
+#include "base/time/clock.h"
+#include "base/time/time.h"
+
+// This class takes care of reauthentication used for accessing passwords
+// through the settings page. It is used on all platforms but iOS and Android
+// (see //ios/chrome/browser/ui/settings/reauthentication_module.* for iOS and
+// PasswordEntryEditor.java and PasswordReauthenticationFragment.java in
+// chrome/android/java/src/org/chromium/chrome/browser/preferences/password/
+// for Android).
+class PasswordAccessAuthenticator {
+ public:
+  // For how long after the last successful authentication a user is considered
+  // authenticated without repeating the challenge.
+  constexpr static int kAuthValidityPeriodSeconds = 60;
+
+  // |os_reauth_call| is passed to |os_reauth_call_|, see the latter for
+  // explanation.
+  explicit PasswordAccessAuthenticator(
+      base::RepeatingCallback<bool()> os_reauth_call);
+
+  ~PasswordAccessAuthenticator();
+
+  // Returns whether the user is able to pass the authentication challenge,
+  // which is represented by |os_reauth_call_| returning true. A successful
+  // result of |os_reauth_call_| is cached for |kAuthValidityPeriodSeconds|
+  // seconds.
+  bool EnsureUserIsAuthenticated();
+
+  // Use this in tests to mock the OS-level reauthentication.
+  void SetOsReauthCallForTesting(
+      base::RepeatingCallback<bool()> os_reauth_call);
+
+  // Use this to manipulate time in tests.
+  void SetClockForTesting(std::unique_ptr<base::Clock> clock);
+
+ private:
+  // The last time the user was successfully authenticated.
+  base::Optional<base::Time> last_authentication_time_;
+
+  // Used to measure the time since the last authentication.
+  std::unique_ptr<base::Clock> clock_;
+
+  // Used to directly present the authentication challenge (such as the login
+  // prompt) to the user.
+  base::RepeatingCallback<bool()> os_reauth_call_;
+
+  DISALLOW_COPY_AND_ASSIGN(PasswordAccessAuthenticator);
+};
+
+#endif  // CHROME_BROWSER_UI_PASSWORDS_PASSWORD_ACCESS_AUTHENTICATOR_H_
--- chrome/browser/ui/passwords/password_manager_presenter_unittest.cc
+++ chrome/browser/ui/passwords/password_manager_presenter_unittest.cc
@@ -7,6 +7,7 @@
 #include <stddef.h>
 #include <utility>
 
+#include "base/bind.h"
 #include "base/macros.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
@@ -20,6 +21,7 @@
 #include "testing/gtest/include/gtest/gtest.h"
 
 using base::ASCIIToUTF16;
+using testing::_;
 using testing::Eq;
 using testing::Property;
 
@@ -345,5 +347,37 @@ TEST_F(PasswordManagerPresenterTest, Sorting_SpecialCharacters) {
   SortAndCheckPositions(test_cases, arraysize(test_cases),
                         PasswordEntryType::SAVED);
 }
+
+#if !defined(OS_ANDROID)  // Reauthentication is handled differently on Android.
+enum class ReauthResult { PASS, FAIL };
+bool FakeOsReauthCall(bool* reauth_called, ReauthResult result) {
+  *reauth_called = true;
+  return result == ReauthResult::PASS;
+}
+
+TEST_F(PasswordManagerPresenterTest, TestPassedReauthOnView) {
+  bool reauth_called = false;
+  GetUIController()->GetPasswordManagerPresenter()->SetOsReauthCallForTesting(
+      base::BindRepeating(&FakeOsReauthCall, &reauth_called,
+                          ReauthResult::PASS));
+
+  AddPasswordEntry(GURL("http://abc1.com"), "test@gmail.com", "test");
+  EXPECT_CALL(*GetUIController(), ShowPassword(0, base::ASCIIToUTF16("test")));
+  GetUIController()->GetPasswordManagerPresenter()->RequestShowPassword(0);
+  EXPECT_TRUE(reauth_called);
+}
+
+TEST_F(PasswordManagerPresenterTest, TestFailedReauthOnView) {
+  bool reauth_called = false;
+  GetUIController()->GetPasswordManagerPresenter()->SetOsReauthCallForTesting(
+      base::BindRepeating(&FakeOsReauthCall, &reauth_called,
+                          ReauthResult::FAIL));
+
+  AddPasswordEntry(GURL("http://abc1.com"), "test@gmail.com", "test");
+  EXPECT_CALL(*GetUIController(), ShowPassword(_, _)).Times(0);
+  GetUIController()->GetPasswordManagerPresenter()->RequestShowPassword(0);
+  EXPECT_TRUE(reauth_called);
+}
+#endif  // !defined(OS_ANDROID)
 
 }  // namespace
