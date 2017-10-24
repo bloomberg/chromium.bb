393ad21b0f9069b542e9a50fd61e27042af1eb2f
Nico Weber <thakis@chromium.org>
2017-10-24 17:33:50 +0000

    Revert "Disable TabStackTraceTest.testValidDump on Windows."
    This reverts commit 719bb7e83578ddd2fa2e30f5fc49ce71f818d91c [formerly 6c789de26df203e92748d3c0e4a9a80b2a6101ae].
    
    Reason for revert: The test should work again.
    
    Original change's description:
    > Disable TabStackTraceTest.testValidDump on Windows.
    > 
    > It seems chrome://crash has started to behave differently after we switch to
    > clang-cl on Windows. This telemetry test started to fail due to that.
    > 
    > TBR=battre@chromium.org,rnephew@chromium.org
    > 
    > Bug: 777741
    > Change-Id: I3395426d3c798b8bdd563162567352806d7b30b9
    > Reviewed-on: https://chromium-review.googlesource.com/735300
    > Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    > Reviewed-by: Dominic Battré <battre@chromium.org>
    > Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    > Commit-Queue: Dominic Battré <battre@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#511083}
    
    TBR=battre@chromium.org,yutak@chromium.org,rnephew@chromium.org
    
    Change-Id: I411fe57fa374906ad8970ca671a47fede878d2a3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 777741
    Reviewed-on: https://chromium-review.googlesource.com/735879
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511195}
    Former-commit-id: ea9b1a8af2271c66c9093970c8c75ab6115dcac9

--- tools/perf/core/stacktrace_unittest.py
+++ tools/perf/core/stacktrace_unittest.py
@@ -15,8 +15,7 @@ class TabStackTraceTest(tab_test_case.TabTestCase):
   # Stack traces do not currently work on 10.6, but they are also being
   # disabled shortly so just disable it for now.
   # All platforms except chromeos should at least have a valid minidump.
-  # crbug.com/777741: Switch to clang-cl caused this test to fail on Windows.
-  @decorators.Disabled('snowleopard', 'chromeos', 'win')
+  @decorators.Disabled('snowleopard', 'chromeos')
   def testValidDump(self):
     with self.assertRaises(exceptions.DevtoolsTargetCrashException) as c:
       self._tab.Navigate('chrome://crash', timeout=5)
