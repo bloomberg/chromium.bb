7d480480dd8a6729f54618e87e60373b763e869d
David Tseng <dtseng@chromium.org>
2017-10-24 22:27:52 +0000

    Provide the ability to assign an Aura window an accessible role
    Bug: 761124
    Change-Id: Ic2389f4122054c39757463f614ccd0adb756cf90
    Reviewed-on: https://chromium-review.googlesource.com/671534
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511283}
    Former-commit-id: 6197a3b7a1f096927e0f42d4ac67c7e6cc6af3b7

--- ui/accessibility/platform/aura_window_properties.h
+++ ui/accessibility/platform/aura_window_properties.h
@@ -5,6 +5,7 @@
 #ifndef UI_ACCESSIBILITY_PLATFORM_AURA_WINDOW_PROPERTIES_H_
 #define UI_ACCESSIBILITY_PLATFORM_AURA_WINDOW_PROPERTIES_H_
 
+#include "ui/accessibility/ax_enums.h"
 #include "ui/accessibility/ax_export.h"
 #include "ui/accessibility/ax_tree_id_registry.h"
 #include "ui/aura/window.h"
@@ -13,6 +14,8 @@ namespace ui {
 
 AX_EXPORT extern const aura::WindowProperty<AXTreeIDRegistry::AXTreeID>* const
     kChildAXTreeID;
+
+AX_EXPORT extern const aura::WindowProperty<AXRole>* const kAXRoleOverride;
 
 }  // namespace ui
 
