ff6e9a7f3b3b83fcf43bd60cee60a248b02714b8
Nico Weber <thakis@chromium.org>
2017-10-24 13:15:02 +0000

    crashpad: Revert inlin-asm-for-SafeTerminateProcess(); call TerminateProcess() in cross builds instead.
    The inline asm doesn't work in clang builds for reasons I don't yet understand.
    Since we just switched to clang by default, lets get back to a good state first.
    
    TBR=mark@chromium.org
    
    Bug: 777579,777741,762167
    Change-Id: I928ebaeaca033da69eaf1d832f477fb3fdca0283
    Reviewed-on: https://chromium-review.googlesource.com/735619
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511127}
    Former-commit-id: af5f31ed615bdbf0ad246cff643a2802a1e0d702

--- third_party/crashpad/crashpad/util/win/safe_terminate_process_broken.cc
+++ third_party/crashpad/crashpad/util/win/safe_terminate_process_broken.cc
@@ -17,34 +17,11 @@
 #if defined(ARCH_CPU_X86)
 namespace crashpad {
 
-// This function is written in assembler source because it’s important for it
-// to not be inlined, for it to allocate a stack frame, and most critically,
-// for it to not trust esp on return from TerminateProcess().
-// __declspec(naked) conveniently prevents inlining and allows control of stack
-// layout.
-__declspec(naked)
 bool SafeTerminateProcess(HANDLE process, UINT exit_code) {
-  __asm {
-    push ebp
-    mov ebp, esp
-
-    push [ebp+12]
-    push [ebp+8]
-    call TerminateProcess
-
-    // Convert from BOOL to bool.
-    test eax, eax
-    setne al
-
-    // TerminateProcess() is supposed to be stdcall (callee clean-up), and esp
-    // and ebp are expected to already be equal. But if it’s been patched badly
-    // by something that’s cdecl (caller clean-up), this next move will get
-    // things back on track.
-    mov esp, ebp
-    pop ebp
-
-    ret
-  }
+  // Third-party software that hooks TerminateProcess() incorrectly has been
+  // encountered in the wild.  This version of SafeTerminateProcess() lacks
+  // protection against that, so don't use it in production.
+  return TerminateProcess(process, exit_code) != FALSE;
 }
 
 }  // namespace crashpad
