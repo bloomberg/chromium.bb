746016a2c939c4bb5552cf927541e6b41ad03617
Ned Nguyen <nednguyen@google.com>
2017-10-24 01:15:40 +0000

    Revert "Disable failing memory_desktop.browse:search:google smoke test (telemetry_perf_unittests)."
    This reverts commit 9dd72ef1d2ad60ee0ed93a4a02c105a936f20fea [formerly 262c05e23b85a9a2cfd44a44250ea402080080b7].
    
    Reason for revert: try reenabling to see if this is still failing.
    
    Original change's description:
    > Disable failing memory_desktop.browse:search:google smoke test (telemetry_perf_unittests).
    > 
    > By the authority of Chromium sheriff.
    > 
    > Bug: 755969
    > Change-Id: If27affaa5bd0904f3a4d0cc89179ef9964966cef
    > No-Try: true
    > Tbr: nednguyen@google.com
    > Reviewed-on: https://chromium-review.googlesource.com/616708
    > Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    > Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#494762}
    
    TBR=jbroman@chromium.org,nednguyen@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 755969
    Change-Id: I853d0b816f1910a6ff7ca9489dd4df24adcd05d4
    Reviewed-on: https://chromium-review.googlesource.com/734020
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#510975}
    Former-commit-id: 5a49151320d67bf460014f01e12a40c709bccf3f

--- tools/perf/benchmarks/system_health_smoke_test.py
+++ tools/perf/benchmarks/system_health_smoke_test.py
@@ -66,9 +66,6 @@ _DISABLED_TESTS = frozenset({
 
   # crbug.com/725386
   'benchmarks.system_health_smoke_test.SystemHealthBenchmarkSmokeTest.system_health.memory_desktop.browse:social:twitter', # pylint: disable=line-too-long
-
-  # crbug.com/755969
-  'benchmarks.system_health_smoke_test.SystemHealthBenchmarkSmokeTest.system_health.memory_desktop.browse:search:google', # pylint: disable=line-too-long
 })
 
 
