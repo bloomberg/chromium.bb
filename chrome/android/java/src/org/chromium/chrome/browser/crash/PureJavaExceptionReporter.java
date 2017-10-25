9d9db54d6e38fb97733f0814819054c0c0bbbeeb
Eric Stevenson <estevenson@chromium.org>
2017-10-25 23:24:33 +0000

    Android: Make PureJavaExceptionReporter easier to use.
    Now has an interface that's similar to JavaExceptionReporter.
    
    Bug: 
    Change-Id: I74e7d895aef50bb9749bfb24fd75dcfd056e9fd0
    Reviewed-on: https://chromium-review.googlesource.com/730650
    Commit-Queue: Eric Stevenson <estevenson@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511637}
    Former-commit-id: 402a61e04c1f64a866a16a2d3f9b601a991a175c

--- chrome/android/java/src/org/chromium/chrome/browser/crash/PureJavaExceptionReporter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/crash/PureJavaExceptionReporter.java
@@ -55,11 +55,18 @@ public class PureJavaExceptionReporter {
     private final String mBoundary = "------------" + UUID.randomUUID() + RN;
 
     /**
-     * Report and upload the device info and stack trace as if it was a crash.
+     * Report and upload the device info and stack trace as if it was a crash. Runs synchronously
+     * and results in I/O on the main thread.
      *
      * @param javaException The exception to report.
      */
-    public void createAndUploadReport(Throwable javaException) {
+    public static void reportJavaException(Throwable javaException) {
+        PureJavaExceptionReporter reporter = new PureJavaExceptionReporter();
+        reporter.createAndUploadReport(javaException);
+    }
+
+    @VisibleForTesting
+    void createAndUploadReport(Throwable javaException) {
         // It is OK to do IO in main thread when we know there is a crash happens.
         try (StrictModeContext unused = StrictModeContext.allowDiskWrites()) {
             createReport(javaException);
@@ -69,7 +76,7 @@ public class PureJavaExceptionReporter {
     }
 
     @VisibleForTesting
-    public File getMinidumpFile() {
+    File getMinidumpFile() {
         return mMinidumpFile;
     }
 
