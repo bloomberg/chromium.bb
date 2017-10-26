5e3aa662df56f8ef4f8c8a4b1862454e8c63112f
Danyao Wang <danyao@chromium.org>
2017-10-26 20:29:34 +0000

    [Nav Experiment] Add about:blank workaround to fix WebTestWithWebState.
    These unit tests call [CRWWebController -loadHTML] on an empty WKWebView
    This breaks the collective assumption in CRWWebController and
    WKBasedNavigationManager that -loadHTML is always called on an existing
    WKBackForwardListItem. This CL works around it by loading about:blank
    before calling -loadHTML if WKBackForwardList is empty.
    
    The only other user of [CRWWebController -loadHTML] is WebUI, which
    already has a similar hack implemented in
    [CRWWebController -loadHTML:forAppSpecificURL].
    
    Bug: 776378
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I4d529011b8a9eea906632c13edc1da20e31b52a9
    Reviewed-on: https://chromium-review.googlesource.com/735128
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511945}
    Former-commit-id: a965ace552fb3a97de878b53573e17d96ea5eb36

--- ios/web/public/test/web_test_with_web_state.mm
+++ ios/web/public/test/web_test_with_web_state.mm
@@ -8,6 +8,7 @@
 #include "base/strings/sys_string_conversions.h"
 #import "base/test/ios/wait_util.h"
 #import "ios/web/navigation/navigation_manager_impl.h"
+#import "ios/web/public/web_client.h"
 #include "ios/web/public/web_state/url_verification_constants.h"
 #include "ios/web/public/web_state/web_state_observer.h"
 #import "ios/web/web_state/ui/crw_web_controller.h"
@@ -78,6 +79,22 @@ void WebTestWithWebState::LoadHtml(NSString* html, const GURL& url) {
   // Initiate asynchronous HTML load.
   CRWWebController* web_controller = GetWebController(web_state());
   ASSERT_EQ(PAGE_LOADED, web_controller.loadPhase);
+
+  // If the underlying WKWebView is empty, first load a placeholder about:blank
+  // to create a WKBackForwardListItem to store the NavigationItem associated
+  // with the |-loadHTML|.
+  // TODO(crbug.com/777884): consider changing |-loadHTML| to match WKWebView's
+  // |-loadHTMLString:baseURL| that doesn't create a navigation entry.
+  if (web::GetWebClient()->IsSlimNavigationManagerEnabled() &&
+      !web_state()->GetNavigationManager()->GetItemCount()) {
+    GURL url(url::kAboutBlankURL);
+    NavigationManager::WebLoadParams params(url);
+    web_state()->GetNavigationManager()->LoadURLWithParams(params);
+    base::test::ios::WaitUntilCondition(^{
+      return web_controller.loadPhase == PAGE_LOADED;
+    });
+  }
+
   [web_controller loadHTML:html forURL:url];
   ASSERT_EQ(LOAD_REQUESTED, web_controller.loadPhase);
 
