03e272fc6b1e1801fcace328631df706787616c4
Scott Chen <scottchen@chromium.org>
2017-10-26 15:15:29 +0000

    MD Extensions: reroute legacy paths to new paths.
    This CL adds code to reroute legacy paths to their equivalent MD Extensions
    paths. It also moves route-related code in NavigationHelper's constructor
    to a separate helper method, since it's growing to be quite a few lines.
    
    Bug: 775698
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I473ff4c2e3fb4bedb814b9538ee2a46f11459b13
    Reviewed-on: https://chromium-review.googlesource.com/724271
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Commit-Queue: Scott Chen <scottchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511822}
    Former-commit-id: 3c45abea5a4e62a95c08331d5d88b4c8b1e8e3bb

--- chrome/test/data/webui/extensions/extension_navigation_helper_test.js
+++ chrome/test/data/webui/extensions/extension_navigation_helper_test.js
@@ -158,23 +158,37 @@ cr.define('extension_navigation_helper_tests', function() {
     });
 
     test(assert(TestNames.SupportedRoutes), function() {
-      function testRedirect(url, redirected) {
+      function removeEndSlash(url) {
+        const CANONICAL_PATH_REGEX = /([\/-\w]+)\/$/;
+        return url.replace(CANONICAL_PATH_REGEX, '$1');
+      }
+
+      // If it should not redirect, leave newUrl as undefined.
+      function testIfRedirected(url, newUrl) {
         history.pushState({}, '', url);
         const testNavigationHelper = new extensions.NavigationHelper();
-        expectEquals(redirected, window.location.href !== url);
+        expectEquals(
+            removeEndSlash(window.location.href),
+            removeEndSlash(newUrl || url));
       }
 
       loadTimeData.overrideValues({isGuest: false});
-      testRedirect('chrome://extensions/', false);
-      testRedirect('chrome://extensions/shortcuts', false);
-      testRedirect('chrome://extensions/fake-route', true);
+      testIfRedirected('chrome://extensions');
+      testIfRedirected('chrome://extensions/');
+      testIfRedirected('chrome://extensions/shortcuts');
+      testIfRedirected('chrome://extensions/shortcuts/');
+      testIfRedirected('chrome://extensions/fake-route', 'chrome://extensions');
       // Test trailing slash works.
-      testRedirect('chrome://extensions/shortcuts/', false);
+
+      // Test legacy paths
+      testIfRedirected(
+          'chrome://extensions/configureCommands',
+          'chrome://extensions/shortcuts');
 
       loadTimeData.overrideValues({isGuest: true});
-      testRedirect('chrome://extensions/', false);
-      testRedirect('chrome://extensions/shortcuts', true);
-      testRedirect('chrome://extensions/fake-route', true);
+      testIfRedirected('chrome://extensions/');
+      testIfRedirected('chrome://extensions/shortcuts', 'chrome://extensions');
+      testIfRedirected('chrome://extensions/fake-route', 'chrome://extensions');
     });
   });
 
