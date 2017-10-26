d693adc93fdda6a521670715581b619c8698d337
Jeremy Roman <jbroman@chromium.org>
2017-10-26 17:52:07 +0000

    Catch make_unique and WrapUnique applied to GC objects in the Blink GC plugin.
    Implemented using Clang AST matchers for concise code.
    
    Bug: 777751
    Change-Id: I37b5d14205314c5ada88e45f06b997006bdfda85
    Reviewed-on: https://chromium-review.googlesource.com/738401
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511876}
    Former-commit-id: 9624ee2a5b59e1a5a6053e95ec98a01f76cf92d3

--- tools/clang/blink_gc_plugin/tests/heap/stubs.h
+++ tools/clang/blink_gc_plugin/tests/heap/stubs.h
@@ -155,7 +155,21 @@ public:
     T* operator->() { return 0; }
 };
 
+template <typename T, typename... Args>
+unique_ptr<T> make_unique(Args&&... args) {
+  return unique_ptr<T>();
 }
+
+}  // namespace std
+
+namespace base {
+
+template <typename T>
+std::unique_ptr<T> WrapUnique(T* ptr) {
+  return std::unique_ptr<T>();
+}
+
+}  // namespace base
 
 namespace blink {
 
