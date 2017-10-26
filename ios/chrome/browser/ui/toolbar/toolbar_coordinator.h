560ddb883728ad4aa141a33f5553d858626a9f88
Gauthier Ambard <gambard@chromium.org>
2017-10-26 15:59:05 +0000

    Remove WebToolbarController references to BVC
    This CL removes the direct references to the WebToolbarController in
    BVC, except for the reference about the UI needed for the iPhone X fixes
    which will be removed in a separate CL.
    
    Bug: 778226
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ib5e7c34fc61c7cbeb73e14df88355239a907faa0
    Reviewed-on: https://chromium-review.googlesource.com/738197
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Rohit Rao (ping after 24h) <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511832}
    Former-commit-id: 82c8cc5aab02c4bd2e4504620d7f0414d04d9823

--- ios/chrome/browser/ui/toolbar/toolbar_coordinator.h
+++ ios/chrome/browser/ui/toolbar/toolbar_coordinator.h
@@ -12,10 +12,17 @@
 #import "ios/chrome/browser/ui/ntp/incognito_view_controller_delegate.h"
 #import "ios/chrome/browser/ui/toolbar/omnibox_focuser.h"
 
+@protocol ActivityServicePositioner;
+@protocol QRScannerResultLoading;
+@protocol TabHistoryPositioner;
+@protocol TabHistoryUIUpdater;
 @class TabModel;
+@class ToolbarController;
 @class ToolsMenuConfiguration;
 @class ToolsPopupController;
+@protocol VoiceSearchControllerDelegate;
 @class WebToolbarController;
+@protocol WebToolbarDelegate;
 
 @interface LegacyToolbarCoordinator
     : ChromeCoordinator<BubbleViewAnchorPointProvider,
@@ -24,7 +31,23 @@
 
 @property(nonatomic, weak) TabModel* tabModel;
 @property(nonatomic, readonly, strong) UIView* view;
+// TODO(crbug.com/778226): Remove this property.
 @property(nonatomic, strong) WebToolbarController* webToolbarController;
+
+// Returns the different protocols and superclass now implemented by the
+// WebToolbarController to avoid using the toolbar directly.
+- (ToolbarController*)toolbarController;
+- (id<VoiceSearchControllerDelegate>)voiceSearchDelegate;
+- (id<ActivityServicePositioner>)activityServicePositioner;
+- (id<TabHistoryPositioner>)tabHistoryPositioner;
+- (id<TabHistoryUIUpdater>)tabHistoryUIUpdater;
+- (id<QRScannerResultLoading>)QRScannerResultLoader;
+
+// Sets the WebToolbarController for this coordinator.
+- (void)setWebToolbar:(WebToolbarController*)webToolbarController;
+
+// Sets the delegate for the toolbar.
+- (void)setToolbarDelegate:(id<WebToolbarDelegate>)delegate;
 
 // TabModel callbacks.
 - (void)selectedTabChanged;
