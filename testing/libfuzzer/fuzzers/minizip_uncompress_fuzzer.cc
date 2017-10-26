982a5cea03bb464efe91926c0e0c4f89bdd362d4
Andrew R. Whalley <awhalley@chromium.org>
2017-10-26 04:23:44 +0000

    Update and add seed corpus for minizip_uncompress_fuzzer
    This fuzzer was flagged as not being able to achieve new coverage.
    It appears that unzOpen2() will quickly return for many inputs since
    the function set for zopen_file was returning null.
    
    Modify the fuzzer to use minizip's fill_memory_filefunc() to read
    from a buffer, rather than creating the zlib_filefunc_def's manually.
    
    Added a corpus of some simple zip files.
    
    Change-Id: I4d4eea123d82b341f4823ff1d6ca13328dfce40b
    Reviewed-on: https://chromium-review.googlesource.com/738909
    Reviewed-by: Oliver Chang <ochang@chromium.org>
    Reviewed-by: Yuki Awano <yawano@chromium.org>
    Commit-Queue: Andrew Whalley <awhalley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#511735}
    Former-commit-id: 9d1617cb0a8ff76d106ac7d8b4eca1d561c010c8

--- testing/libfuzzer/fuzzers/minizip_uncompress_fuzzer.cc
+++ testing/libfuzzer/fuzzers/minizip_uncompress_fuzzer.cc
@@ -7,53 +7,20 @@
 #include <stdint.h>
 
 #include "third_party/minizip/src/ioapi.h"
+#include "third_party/minizip/src/ioapi_mem.h"
 #include "third_party/minizip/src/unzip.h"
 
-zlib_filefunc_def test_filefunc;
-
-uint32_t current_offset = 0;
-size_t test_data_size;
-const uint8_t* test_data;
-
-void* Open(void* /* opaque */, const char* /* filename */, int /* mode */) {
-  return nullptr;
-}
-
-uint32_t Read(void* /* opaque */,
-              void* /* stream */,
-              void* buffer,
-              uint32_t size) {
-  memcpy(buffer, test_data, size);
-  return size;
-}
-
-long Seek(void* /* opaque */, void* /* stream */, uint32_t offset, int origin) {
-  switch (origin) {
-    case ZLIB_FILEFUNC_SEEK_SET:
-      current_offset = offset;
-      break;
-    case ZLIB_FILEFUNC_SEEK_CUR:
-      current_offset += offset;
-    case ZLIB_FILEFUNC_SEEK_END:
-      current_offset = test_data_size + offset;
-  }
-  return current_offset;
-}
-
-long Tell(void* /* opaque */, void* /* stream */) {
-  return current_offset;
-}
-
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  test_data = data;
-  test_data_size = size;
+  zlib_filefunc_def filefunc32 = {0};
+  ourmemory_t unzmem = {0};
+  unzFile handle;
+  unzmem.size = size;
+  unzmem.base = (char*)data;
+  unzmem.grow = 0;
 
-  test_filefunc.zopen_file = Open;
-  test_filefunc.zread_file = Read;
-  test_filefunc.zseek_file = Seek;
-  test_filefunc.ztell_file = Tell;
-
-  unzOpen2(nullptr /* filename */, &test_filefunc);
+  fill_memory_filefunc(&filefunc32, &unzmem);
+  handle = unzOpen2(nullptr, &filefunc32);
+  unzClose(handle);
 
   return 0;
 }
