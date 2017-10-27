b1c58eac344c8d1ace3772f8ecbc6628f9e7a4ab
Robert Liao <robliao@chromium.org>
2017-10-27 21:14:52 +0000

    Introduce ScopedWinrtInitializer and ScopedWindowsThreadEnvironment
    This adds ScopedWinrtInitializer to allow for paired
    base::win::RoInitialize() and base::win::RoUninitialize() calls.
    
    ScopedWindowsThreadEnvironment serves as the new root class for both
    ScopedComInitializer and ScopedWinrtInitializer so that consumers can use the
    same field for conditional initialization between ScopedComInitializer or
    ScopedWinrtInitializer.
    
    BUG=706479
    TBR=dcheng@chromium.org
    base/BUILD.gn change
    
    Change-Id: I9550d02f82a32657041bbc898ea0789b17c5bcf0
    Reviewed-on: https://chromium-review.googlesource.com/741026
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512292}
    Former-commit-id: 9c323eff7ad2bd1a9d62432df4b0bf84c6b0bd29

--- base/win/scoped_com_initializer.h
+++ base/win/scoped_com_initializer.h
@@ -7,10 +7,10 @@
 
 #include <objbase.h>
 
-#include "base/logging.h"
+#include "base/base_export.h"
 #include "base/macros.h"
 #include "base/threading/thread_checker.h"
-#include "build/build_config.h"
+#include "base/win/scoped_windows_thread_environment.h"
 
 namespace base {
 namespace win {
@@ -22,35 +22,24 @@ namespace win {
 // similar lifetime as the thread itself.  You should not be using this in
 // random utility functions that make COM calls -- instead ensure these
 // functions are running on a COM-supporting thread!
-class ScopedCOMInitializer {
+class BASE_EXPORT ScopedCOMInitializer : public ScopedWindowsThreadEnvironment {
  public:
   // Enum value provided to initialize the thread as an MTA instead of STA.
   enum SelectMTA { kMTA };
 
   // Constructor for STA initialization.
-  ScopedCOMInitializer() {
-    Initialize(COINIT_APARTMENTTHREADED);
-  }
+  ScopedCOMInitializer();
 
   // Constructor for MTA initialization.
-  explicit ScopedCOMInitializer(SelectMTA mta) {
-    Initialize(COINIT_MULTITHREADED);
-  }
+  explicit ScopedCOMInitializer(SelectMTA mta);
 
-  ~ScopedCOMInitializer() {
-    DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
-    if (succeeded())
-      CoUninitialize();
-  }
+  ~ScopedCOMInitializer() override;
 
-  bool succeeded() const { return SUCCEEDED(hr_); }
+  // ScopedWindowsThreadEnvironment:
+  bool succeeded() const override;
 
  private:
-  void Initialize(COINIT init) {
-    DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
-    hr_ = CoInitializeEx(NULL, init);
-    DCHECK_NE(RPC_E_CHANGED_MODE, hr_) << "Invalid COM thread model change";
-  }
+  void Initialize(COINIT init);
 
   HRESULT hr_;
   THREAD_CHECKER(thread_checker_);
---
+++ base/win/scoped_winrt_initializer.cc
@@ -0,0 +1,36 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/win/scoped_winrt_initializer.h"
+
+#include "base/logging.h"
+#include "base/win/com_init_util.h"
+#include "base/win/core_winrt_util.h"
+#include "base/win/windows_version.h"
+
+namespace base {
+namespace win {
+
+ScopedWinrtInitializer::ScopedWinrtInitializer()
+    : hr_(base::win::RoInitialize(RO_INIT_MULTITHREADED)) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  DCHECK_GE(GetVersion(), VERSION_WIN8);
+#if DCHECK_IS_ON()
+  if (SUCCEEDED(hr_))
+    AssertComApartmentType(ComApartmentType::MTA);
+#endif
+}
+
+ScopedWinrtInitializer::~ScopedWinrtInitializer() {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  if (SUCCEEDED(hr_))
+    base::win::RoUninitialize();
+}
+
+bool ScopedWinrtInitializer::succeeded() const {
+  return SUCCEEDED(hr_);
+}
+
+}  // namespace win
+}  // namespace base
