968cbd8652bbff90d5b513f31cf86e5dbf36d001
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2017-10-27 02:49:04 +0000

    Save Reader Mode for Accessibility preference
    The setting UI to toggle Reader Mode for Accessibility is now saved
    in the preference. The triggering logic hasn't been linked to the
    preference though.
    
    TBR=bauerb@chromium.org
    
    Bug: 777962
    Change-Id: Ibdee6a12557885aec0be3ee0111197e273456fcc
    Reviewed-on: https://chromium-review.googlesource.com/736182
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512068}
    Former-commit-id: d174a525391c53028e3c5b53496f0e0f26e30219

--- chrome/browser/android/preferences/prefs.h
+++ chrome/browser/android/preferences/prefs.h
@@ -9,6 +9,7 @@
 
 #include "build/build_config.h"
 #include "chrome/common/pref_names.h"
+#include "components/dom_distiller/core/pref_names.h"
 
 // A preference exposed to Java.
 // A Java counterpart will be generated for this enum.
@@ -16,12 +17,16 @@
 enum Pref {
   ALLOW_DELETING_BROWSER_HISTORY,
   INCOGNITO_MODE_AVAILABILITY,
+  READER_FOR_ACCESSIBILITY_ENABLED,
+  // PREF_NUM_PREFS must be the last entry.
   PREF_NUM_PREFS
 };
 
 // The indices must match value of Pref.
+// Remember to update prefs_unittest.cc as well.
 const char* const kPrefsExposedToJava[] = {
     prefs::kAllowDeletingBrowserHistory, prefs::kIncognitoModeAvailability,
+    dom_distiller::prefs::kReaderForAccessibility,
 };
 
 #endif  // CHROME_BROWSER_ANDROID_PREFERENCES_PREFS_H_
