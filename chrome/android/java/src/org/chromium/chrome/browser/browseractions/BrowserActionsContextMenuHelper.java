01d13249f0e3e40cfce899ce98ddfda6e66602c2
Lei Tian <ltian@google.com>
2017-10-27 23:46:44 +0000

    Add histogram of selected option for Browser Actions
    Add the histogram metric to measure the selected option from a Browser
    Actions context menu.
    
    Bug: 776967
    Change-Id: I98787fe1fedcc3d4b7c08e06f9138c8eec57345d
    Reviewed-on: https://chromium-review.googlesource.com/731831
    Commit-Queue: Lei Tian <ltian@google.com>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512352}
    Former-commit-id: 07c7037e620a5cc2a4a209a3b9454729ae5591ee

--- chrome/android/java/src/org/chromium/chrome/browser/browseractions/BrowserActionsContextMenuHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/browseractions/BrowserActionsContextMenuHelper.java
@@ -10,6 +10,7 @@ import android.app.PendingIntent.CanceledException;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.IntDef;
 import android.support.customtabs.browseractions.BrowserActionItem;
 import android.support.customtabs.browseractions.BrowserActionsIntent;
 import android.support.customtabs.browseractions.BrowserActionsIntent.BrowserActionsItemId;
@@ -24,6 +25,7 @@ import android.view.View.OnCreateContextMenuListener;
 import org.chromium.base.Callback;
 import org.chromium.base.Log;
 import org.chromium.base.VisibleForTesting;
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.contextmenu.ChromeContextMenuItem;
@@ -35,6 +37,8 @@ import org.chromium.chrome.browser.contextmenu.ShareContextMenuItem;
 import org.chromium.chrome.browser.contextmenu.TabularContextMenuUi;
 import org.chromium.ui.base.WindowAndroid.OnCloseContextMenuListener;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -65,6 +69,23 @@ public class BrowserActionsContextMenuHelper implements OnCreateContextMenuListe
                 ProgressDialog progressDialog);
     }
 
+    /**
+     * Defines the actions shown on the Browser Actions context menu.
+     * Note: these values must match the BrowserActionsMenuOption enum in enums.xml.
+     * And the values are persisted to logs so they cannot be renumbered or reused.
+     */
+    @IntDef({ACTION_OPEN_IN_NEW_CHROME_TAB, ACTION_OPEN_IN_INCOGNITO_TAB, ACTION_DOWNLOAD_PAGE,
+            ACTION_COPY_LINK, ACTION_SHARE, ACTION_APP_PROVIDED})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface BrowserActionsActionId {}
+    private static final int ACTION_OPEN_IN_NEW_CHROME_TAB = 0;
+    private static final int ACTION_OPEN_IN_INCOGNITO_TAB = 1;
+    private static final int ACTION_DOWNLOAD_PAGE = 2;
+    private static final int ACTION_COPY_LINK = 3;
+    private static final int ACTION_SHARE = 4;
+    // Actions for selecting custom items.
+    private static final int ACTION_APP_PROVIDED = 5;
+    private static final int NUM_ACTIONS = 6;
 
     static final List<Integer> CUSTOM_BROWSER_ACTIONS_ID_GROUP =
             Arrays.asList(R.id.browser_actions_custom_item_one,
@@ -190,31 +211,42 @@ public class BrowserActionsContextMenuHelper implements OnCreateContextMenuListe
     boolean onItemSelected(int itemId) {
         if (itemId == R.id.browser_actions_open_in_background) {
             if (mIsNativeInitialized) {
+                recordBrowserActionsSelection(ACTION_OPEN_IN_NEW_CHROME_TAB);
                 handleOpenInBackground();
             } else {
                 mPendingItemId = itemId;
                 waitNativeInitialized();
             }
         } else if (itemId == R.id.browser_actions_open_in_incognito_tab) {
+            recordBrowserActionsSelection(ACTION_OPEN_IN_INCOGNITO_TAB);
             mMenuItemDelegate.onOpenInIncognitoTab(mCurrentContextMenuParams.getLinkUrl());
             notifyBrowserActionSelected(BrowserActionsIntent.ITEM_OPEN_IN_INCOGNITO);
         } else if (itemId == R.id.browser_actions_save_link_as) {
             if (mIsNativeInitialized) {
+                recordBrowserActionsSelection(ACTION_DOWNLOAD_PAGE);
                 handleDownload();
             } else {
                 mPendingItemId = itemId;
                 waitNativeInitialized();
             }
         } else if (itemId == R.id.browser_actions_copy_address) {
+            recordBrowserActionsSelection(ACTION_COPY_LINK);
             mMenuItemDelegate.onSaveToClipboard(mCurrentContextMenuParams.getLinkUrl());
             notifyBrowserActionSelected(BrowserActionsIntent.ITEM_COPY);
         } else if (itemId == R.id.browser_actions_share) {
+            recordBrowserActionsSelection(ACTION_SHARE);
             mMenuItemDelegate.share(false, mCurrentContextMenuParams.getLinkUrl());
             notifyBrowserActionSelected(BrowserActionsIntent.ITEM_SHARE);
         } else if (mCustomItemActionMap.indexOfKey(itemId) >= 0) {
+            recordBrowserActionsSelection(ACTION_APP_PROVIDED);
             mMenuItemDelegate.onCustomItemSelected(mCustomItemActionMap.get(itemId));
         }
         return true;
+    }
+
+    private void recordBrowserActionsSelection(@BrowserActionsActionId int itemId) {
+        RecordHistogram.recordEnumeratedHistogram(
+                "BrowserActions.SelectedOption", itemId, NUM_ACTIONS);
     }
 
     private void notifyBrowserActionSelected(@BrowserActionsItemId int menuId) {
