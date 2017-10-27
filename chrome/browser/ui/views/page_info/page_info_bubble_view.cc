e6dfa7fcacd1f2f4978e92790548da62745c69e1
Timothy Loh <timloh@chromium.org>
2017-10-27 06:08:18 +0000

    Desktop Page Info: Add tooltips for site settings and cookies
    This patch adds tooltips (and thus accessibility info) for the site
    settings and cookies (N in use) labels ("Open site settings" and "Show
    cookies") in the views implementation of page info.
    
    Bug: 535074
    Change-Id: Idac98d25bac6498e2252305c0d03723d54df13d3
    Reviewed-on: https://chromium-review.googlesource.com/737309
    Reviewed-by: Patti <patricialor@chromium.org>
    Reviewed-by: Lucas Garron <lgarron@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512103}
    Former-commit-id: 89cdd4ac646bcdb2b3f9aa12d70870dc416f7483

--- chrome/browser/ui/views/page_info/page_info_bubble_view.cc
+++ chrome/browser/ui/views/page_info/page_info_bubble_view.cc
@@ -150,6 +150,8 @@ views::View* CreateSiteSettingsLink(const int side_margin,
       l10n_util::GetStringUTF16(IDS_PAGE_INFO_SITE_SETTINGS_LINK));
   site_settings_link->set_id(
       PageInfoBubbleView::VIEW_ID_PAGE_INFO_LINK_SITE_SETTINGS);
+  site_settings_link->SetTooltipText(
+      l10n_util::GetStringUTF16(IDS_PAGE_INFO_SITE_SETTINGS_TOOLTIP));
   site_settings_link->set_listener(listener);
   site_settings_link->SetUnderline(false);
   views::View* link_section = new views::View();
@@ -688,6 +690,8 @@ void PageInfoBubbleView::SetCookieInfo(const CookieInfoList& cookie_info_list) {
   base::string16 label_text = l10n_util::GetPluralStringFUTF16(
       IDS_PAGE_INFO_NUM_COOKIES, total_allowed);
   cookie_dialog_link_->SetText(label_text);
+  cookie_dialog_link_->SetTooltipText(
+      l10n_util::GetStringUTF16(IDS_PAGE_INFO_COOKIES_TOOLTIP));
 
   Layout();
   SizeToContents();
