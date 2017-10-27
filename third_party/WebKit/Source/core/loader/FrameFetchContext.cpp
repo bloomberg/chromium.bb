25a0677a5d25ae1e9658cc09759cde3f96220005
Hiroshige Hayashizaki <hiroshige@chromium.org>
2017-10-27 19:27:04 +0000

    Revert "Implement Time to Interactive Detector"
    This reverts commit 4bbe22b6d5560af351c03dc56e70bf131c206a01 [formerly 3b4bb95c40fccb9dfe732085d736fb71201fbdba].
    
    Reason for revert: Build failure on Windows dbg builder:
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Win%20Builder%20%28dbg%29/builds/126465
    
    Original change's description:
    > Implement Time to Interactive Detector
    > 
    > This patch introduces the InteractiveDetector class, which currently detects
    > Time to Interactive. Time to Interactive is intended to approximate the first
    > point in time a page is consistently jank free for a significant time window
    > after load.
    > 
    > We currently output a trace event at the Interactive timestamp. Future patches
    > will report the metric as UMA/UKM, and add First Idle detection.
    > 
    > Design doc: https://goo.gl/SYt55W
    > 
    > Bug: 631203
    > Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    > Change-Id: Ic9f3cf8089f43d313c255c8df1aa1f311163b2e5
    > Reviewed-on: https://chromium-review.googlesource.com/720213
    > Commit-Queue: Deepanjan Roy <dproy@chromium.org>
    > Reviewed-by: Nate Chapin <japhet@chromium.org>
    > Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#512247}
    
    TBR=japhet@chromium.org,tdresser@chromium.org,dproy@chromium.org
    
    Change-Id: I55b855c7a099c92437e740d310b07acf4d673071
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 631203
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Reviewed-on: https://chromium-review.googlesource.com/742241
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512265}
    Former-commit-id: 318723cc573ab5b141c2a40cf1874a8324f0d01e

--- third_party/WebKit/Source/core/loader/FrameFetchContext.cpp
+++ third_party/WebKit/Source/core/loader/FrameFetchContext.cpp
@@ -52,7 +52,6 @@
 #include "core/loader/DocumentLoader.h"
 #include "core/loader/FrameLoader.h"
 #include "core/loader/IdlenessDetector.h"
-#include "core/loader/InteractiveDetector.h"
 #include "core/loader/MixedContentChecker.h"
 #include "core/loader/NetworkHintsInterface.h"
 #include "core/loader/PingLoader.h"
@@ -82,7 +81,6 @@
 #include "platform/scheduler/child/web_scheduler.h"
 #include "platform/scheduler/renderer/web_view_scheduler.h"
 #include "platform/weborigin/SchemeRegistry.h"
-#include "platform/wtf/Optional.h"
 #include "platform/wtf/Vector.h"
 #include "public/platform/Platform.h"
 #include "public/platform/WebApplicationCacheHost.h"
@@ -468,13 +466,6 @@ void FrameFetchContext::DispatchWillSendRequest(
                          initiator_info, resource_type);
   if (IdlenessDetector* idleness_detector = GetFrame()->GetIdlenessDetector())
     idleness_detector->OnWillSendRequest(MasterDocumentLoader()->Fetcher());
-  if (document_) {
-    InteractiveDetector* interactive_detector(
-        InteractiveDetector::From(*document_));
-    if (interactive_detector) {
-      interactive_detector->OnResourceLoadBegin(WTF::nullopt);
-    }
-  }
   if (GetFrame()->FrameScheduler())
     GetFrame()->FrameScheduler()->DidStartLoading(identifier);
 }
@@ -584,14 +575,6 @@ void FrameFetchContext::DispatchDidFinishLoading(unsigned long identifier,
   probe::didFinishLoading(GetFrame()->GetDocument(), identifier,
                           MasterDocumentLoader(), finish_time,
                           encoded_data_length, decoded_body_length);
-  if (document_) {
-    InteractiveDetector* interactive_detector(
-        InteractiveDetector::From(*document_));
-    if (interactive_detector) {
-      interactive_detector->OnResourceLoadEnd(finish_time);
-    }
-  }
-
   if (GetFrame()->FrameScheduler())
     GetFrame()->FrameScheduler()->DidStopLoading(identifier);
 }
@@ -606,15 +589,6 @@ void FrameFetchContext::DispatchDidFail(unsigned long identifier,
   GetFrame()->Loader().Progress().CompleteProgress(identifier);
   probe::didFailLoading(GetFrame()->GetDocument(), identifier,
                         MasterDocumentLoader(), error);
-  if (document_) {
-    InteractiveDetector* interactive_detector(
-        InteractiveDetector::From(*document_));
-    if (interactive_detector) {
-      // We have not yet recorded load_finish_time. Pass nullopt here; we will
-      // call MonotonicallyIncreasingTime lazily when we need it.
-      interactive_detector->OnResourceLoadEnd(WTF::nullopt);
-    }
-  }
   // Notification to FrameConsole should come AFTER InspectorInstrumentation
   // call, DevTools front-end relies on this.
   if (!is_internal_request)
