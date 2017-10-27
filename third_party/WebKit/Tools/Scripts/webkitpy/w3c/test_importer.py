3c88008e6204d62db34f522ac564932e4ccc3bd9
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2017-10-27 12:10:28 +0000

    Reland "wpt-import: TBR current ecosystem infra sheriff"
    This is a reland of https://chromium-review.googlesource.com/729639 combined
    with the fix for non-chromium.org email addresses uploaded in
    https://chromium-review.googlesource.com/739393. They are being landed
    together to avoid having a small window where having the former and not the
    latter can break WPT imports.
    
    First CL's original commit message:
    
      wpt-import: TBR current ecosystem infra sheriff
    
    Second CL's original commit message:
    
      wpt-import: Do not always append chromium.org to TBR email addresses.
    
      The rotation JSON file contains full email addresses for users with a
      non-chromium.org email (such as myself).
    
      The code was previously failing in those cases, as it generated a TBR line
      such as "raphael.kubo.da.costa@intel.com@chromium.org", which causes
      depot_tools' owners.py to bail out.
    
    Bug: 755753
    Change-Id: I5306f137dbc740452ec9e28497da076a146a78fe
    Reviewed-on: https://chromium-review.googlesource.com/741593
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Raphael Kubo da Costa (rakuco) <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/master@{#512153}
    Former-commit-id: 78e97a5517a3064d8738b5bbd06e5df0c9f0dc27

--- third_party/WebKit/Tools/Scripts/webkitpy/w3c/test_importer.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/test_importer.py
@@ -12,6 +12,8 @@ If this script is given the argument --auto-update, it will also:
 """
 
 import argparse
+import datetime
+import json
 import logging
 
 from webkitpy.common.net.buildbot import current_build_link
@@ -32,6 +34,9 @@ from webkitpy.w3c.wpt_manifest import WPTManifest
 # Settings for how often to check try job results and how long to wait.
 POLL_DELAY_SECONDS = 2 * 60
 TIMEOUT_SECONDS = 210 * 60
+
+# Sheriff calendar URL, used for getting the ecosystem infra sheriff to TBR.
+ROTATIONS_URL = 'http://chromium-build.appspot.com/p/chromium/all_rotations.js'
 
 _log = logging.getLogger(__file__)
 
@@ -427,18 +432,16 @@ class TestImporter(object):
         _log.info('Uploading change list.')
         directory_owners = self.get_directory_owners()
         description = self._cl_description(directory_owners)
+        sheriff_email = self.tbr_reviewer()
         self.git_cl.run([
             'upload',
             '-f',
             '--gerrit',
-            '-m',
-            description,
-            '--tbrs',
-            'qyearsley@chromium.org',
+            '-m', description,
+            '--tbrs', sheriff_email,
             # Note: we used to CC all the directory owners, but have stopped
             # in search of a better notification mechanism. (crbug.com/765334)
-            '--cc',
-            'robertma@chromium.org',
+            '--cc', 'robertma@chromium.org',
         ])
 
     def get_directory_owners(self):
@@ -483,6 +486,38 @@ class TestImporter(object):
             message_lines.append(', '.join(owner_tuple) + ':')
             message_lines.extend('  ' + d for d in directories)
         return '\n'.join(message_lines)
+
+    def tbr_reviewer(self):
+        """Returns the email address to use as the reviewer.
+
+        This tries to fetch the current ecosystem infra sheriff,
+        but falls back in case of error.
+        """
+        username = ''
+        try:
+            username = self._fetch_ecosystem_infra_sheriff_username()
+        except (IOError, KeyError, ValueError) as error:
+            _log.error('Exception while fetching current sheriff: %s', error)
+        if not username:
+            username = 'qyearsley'  # Fallback in case of failure.
+        # Users with an email address other than chromium.org have a full email
+        # address in the JSON file.
+        if '@' in username:
+            return username
+        return username + '@chromium.org'
+
+    def _fetch_ecosystem_infra_sheriff_username(self):
+        content = self.host.web.get_binary(ROTATIONS_URL)
+        data = json.loads(content)
+        today = datetime.date.fromtimestamp(self.host.time()).isoformat()
+        index = data['rotations'].index('ecosystem_infra')
+        calendar = data['calendar']
+        for entry in calendar:
+            if entry['date'] == today:
+                if not entry['participants'][index]:
+                    return ''
+                return entry['participants'][index][0]
+        return ''
 
     def fetch_new_expectations_and_baselines(self):
         """Modifies expectation lines and baselines based on try job results.
