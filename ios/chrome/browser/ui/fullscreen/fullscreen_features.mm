a0c90e54a0410014602e9a755b40f0e98d0eb73b
Kurt Horimoto <kkhorimoto@chromium.org>
2017-10-28 01:20:37 +0000

    [iOS] Created feature flag for new Fullscreen.
    This CL creates a new base::Feature that can be used to enable the new
    fullscreen implementation.
    
    Bug: 778017
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Id54fc69e607e423c37cbfff0a831b4e2555ad1f9
    Reviewed-on: https://chromium-review.googlesource.com/736690
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512364}
    Former-commit-id: 803840623f51cc616d37cc2f473e55dd1a320fce

---
+++ ios/chrome/browser/ui/fullscreen/fullscreen_features.mm
@@ -0,0 +1,16 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/fullscreen/fullscreen_features.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+namespace features {
+
+const base::Feature kNewFullscreen{"NewFullscreen",
+                                   base::FEATURE_DISABLED_BY_DEFAULT};
+
+}  // namespace features
