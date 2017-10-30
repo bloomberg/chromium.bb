b5b35dab18c31f75f7ff8c832e35494ef9f8f5e2
Danyao Wang <danyao@chromium.org>
2017-10-30 16:50:44 +0000

    [Nav Experiment] Fix NativeContent unit test to wait after load.
    With WKBasedNavigationManager, native URLs are loaded asynchronously.
    Added WebStateObserver to wait for load to finish before asserting in
    tests.
    
    Bug: 776467
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I2d0da5770ea09988a57e9af3e3561d1dc5bd3ebf
    Reviewed-on: https://chromium-review.googlesource.com/740370
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512519}
    Former-commit-id: 449262de3deed2091f6f4a02768b45d126ba3d41

--- ios/web/web_state/ui/crw_web_controller_unittest.mm
+++ ios/web/web_state/ui/crw_web_controller_unittest.mm
@@ -15,6 +15,7 @@
 #include "base/strings/utf_string_conversions.h"
 #import "base/test/ios/wait_util.h"
 #import "ios/testing/ocmock_complex_type_helper.h"
+#import "ios/testing/wait_util.h"
 #import "ios/web/navigation/crw_session_controller.h"
 #import "ios/web/navigation/navigation_item_impl.h"
 #import "ios/web/navigation/navigation_manager_impl.h"
@@ -645,6 +646,7 @@ class CRWWebControllerNativeContentTest : public WebTestWithWebController {
   }
 
   void Load(const GURL& URL) {
+    TestWebStateObserver observer(web_state());
     NavigationManagerImpl& navigation_manager =
         [web_controller() webStateImpl]->GetNavigationManagerImpl();
     navigation_manager.AddPendingItem(
@@ -652,6 +654,16 @@ class CRWWebControllerNativeContentTest : public WebTestWithWebController {
         NavigationInitiationType::USER_INITIATED,
         NavigationManager::UserAgentOverrideOption::INHERIT);
     [web_controller() loadCurrentURL];
+
+    // Native URL is loaded asynchronously with WKBasedNavigationManager. Wait
+    // for navigation to finish before asserting.
+    if (GetWebClient()->IsSlimNavigationManagerEnabled()) {
+      TestWebStateObserver* observer_ptr = &observer;
+      ASSERT_TRUE(testing::WaitUntilConditionOrTimeout(
+          testing::kWaitForPageLoadTimeout, ^{
+            return observer_ptr->did_finish_navigation_info() != nullptr;
+          }));
+    }
   }
 
   TestNativeContentProvider* mock_native_provider_;
