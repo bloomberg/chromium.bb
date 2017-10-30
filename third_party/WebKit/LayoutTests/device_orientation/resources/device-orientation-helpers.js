1487b55d9ac93589efbfc78e8b407fd1846ed000
Jun Cai <juncai@chromium.org>
2017-10-30 18:44:21 +0000

    Update device orientation layouttests using testharness.js instead of js-test.js
    This CL updates //third_party/WebKit/LayoutTests/device_orientation/orientation
    to use testharness.js instead of js-test.js.
    
    Bug: 758628
    Change-Id: I8be7a93cdd9e0bca1a168f91c165c119e02ca1cd
    Reviewed-on: https://chromium-review.googlesource.com/677634
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512550}
    Former-commit-id: db41a37062a493327eb216867fca603afaa553a0

--- third_party/WebKit/LayoutTests/device_orientation/resources/device-orientation-helpers.js
+++ third_party/WebKit/LayoutTests/device_orientation/resources/device-orientation-helpers.js
@@ -24,6 +24,14 @@ function generateMotionData(accelerationX, accelerationY, accelerationZ,
   return motionData;
 }
 
+function generateOrientationData(alpha, beta, gamma, absolute) {
+  var orientationData = {alpha: alpha,
+                         beta: beta,
+                         gamma: gamma,
+                         absolute: absolute};
+  return orientationData;
+}
+
 function setMockMotion(motionData) {
   testRunner.setMockDeviceMotion(null != motionData.accelerationX,
       null == motionData.accelerationX ? 0 : motionData.accelerationX,
@@ -46,6 +54,16 @@ function setMockMotion(motionData) {
       motionData.interval);
 }
 
+function setMockOrientation(orientationData) {
+  testRunner.setMockDeviceOrientation(null != orientationData.alpha,
+      null == orientationData.alpha ? 0 : orientationData.alpha,
+      null != orientationData.beta,
+      null == orientationData.beta ? 0 : orientationData.beta,
+      null != orientationData.gamma,
+      null == orientationData.gamma ? 0 : orientationData.gamma,
+      orientationData.absolute);
+}
+
 function checkMotion(event, expectedMotionData) {
   assert_equals(event.acceleration.x, expectedMotionData.accelerationX, "acceleration.x");
   assert_equals(event.acceleration.y, expectedMotionData.accelerationY, "acceleration.y");
@@ -60,4 +78,12 @@ function checkMotion(event, expectedMotionData) {
   assert_equals(event.rotationRate.gamma, expectedMotionData.rotationRateGamma, "rotationRate.gamma");
 
   assert_equals(event.interval, expectedMotionData.interval, "interval");
+}
+
+function checkOrientation(event, expectedOrientationData) {
+  assert_equals(event.alpha, expectedOrientationData.alpha, "alpha");
+  assert_equals(event.beta, expectedOrientationData.beta, "beta");
+  assert_equals(event.gamma, expectedOrientationData.gamma, "gamma");
+
+  assert_equals(event.absolute, expectedOrientationData.absolute, "absolute");
 }
