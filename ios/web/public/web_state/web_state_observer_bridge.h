9f3e89889c421761ff8fbee4a2aad212c7513b91
Sylvain Defresne <sdefresne@chromium.org>
2017-10-30 15:23:05 +0000

    Allow WebStateObserver to observe N WebStates [2/N].
    Convert WebStateObserverBridge to allow client code to control
    which WebStates are observed. Still support the constructor to
    let WebStateObserverBridge itself manages the registration.
    
    Bug: 775684
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I06cc3f8d84f708f493221042a0257ede143b91d0
    Reviewed-on: https://chromium-review.googlesource.com/738864
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512498}
    Former-commit-id: cbb6a6244c1cbfeb7242043aa7eb19e156751b1e

--- ios/web/public/web_state/web_state_observer_bridge.h
+++ ios/web/public/web_state/web_state_observer_bridge.h
@@ -98,11 +98,22 @@ namespace web {
 class WebState;
 
 // Bridge to use an id<CRWWebStateObserver> as a web::WebStateObserver.
-// Will be added/removed as an observer of the underlying WebState during
-// construction/destruction. Instances should be owned by instances of the
-// class they're bridging.
 class WebStateObserverBridge : public web::WebStateObserver {
  public:
+  // Returns the web state associated with this observer.
+  // TODO(crbug.com/775684): this is deprecated. Remove once all observer
+  // have been converted to manage the registration with WebState directly.
+  WebState* web_state() const { return web_state_; }
+
+  // Use this constructor to control which WebStates to observe. It it the
+  // responsibility of calling code to add/remove the instance from the
+  // WebStates observer lists.
+  WebStateObserverBridge(id<CRWWebStateObserver> observer);
+  // Use this constructor when using automatic registration/unregistration
+  // of the WebStateObserver. Deprecated. TODO(crbug.com/775684): remove
+  // once all observer have been converted.
+  // TODO(crbug.com/775684): this is deprecated. Remove once all observer
+  // have been converted to manage the registration with WebState directly.
   WebStateObserverBridge(web::WebState* web_state,
                          id<CRWWebStateObserver> observer);
   ~WebStateObserverBridge() override;
@@ -143,7 +154,19 @@ class WebStateObserverBridge : public web::WebStateObserver {
   void DidStartLoading(web::WebState* web_state) override;
   void DidStopLoading(web::WebState* web_state) override;
 
+ protected:
+  // Start observing a different WebState; used with the default constructor.
+  // TODO(crbug.com/775684): this is deprecated. Remove once all observer
+  // have been converted to manage the registration with WebState directly.
+  void Observe(WebState* web_state);
+
  private:
+  // The WebState this instance is observing. Will be null after
+  // WebStateDestroyed has been called or if registration is manual.
+  // TODO(crbug.com/775684): this is deprecated. Remove once all observer
+  // have been converted to manage the registration with WebState directly.
+  WebState* web_state_ = nullptr;
+
   __weak id<CRWWebStateObserver> observer_ = nil;
   DISALLOW_COPY_AND_ASSIGN(WebStateObserverBridge);
 };
