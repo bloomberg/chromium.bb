1b5ecac476fdccea28f9488d457482d0580b2b73
Ken Rockot <rockot@chromium.org>
2017-10-30 09:25:40 +0000

    Always send subresource ULF on navigation
    This changes the Network Service-enabled browser-side navigation path
    to always send a default subresource URLLoaderFactory to the render
    frame on navigation commit.
    
    In addition to sending a default ULF, we also send one for blobs.
    Ultimately the idea here is to have the browser, at navigation time,
    send a frame everything it will need to request any accessible
    subresources, making it easier to reason about what subresource
    capabilities any given frame has.
    
    This set of capabilities is encapsulated by a new
    URLLoaderFactoryBundle type, which can be serialized over mojom and is
    now passed in new-document navigation commit messages.
    
    Follow-up usage of URLLoaderFactoryBundle will support loading things
    like file:// and chrome-extension:// subresources when appropriate.
    
    Note that this does not yet change how workers acquire ULFs for their
    own resource requests.
    
    Bug: 721414,759230
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I47b0897c7c012292633d5f08c874a18cc0fb1dcb
    Reviewed-on: https://chromium-review.googlesource.com/740904
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512459}
    Former-commit-id: bba21f276796f75e0d427fe3a224088e48f375e6

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -742,9 +742,15 @@ class RenderFrameImpl::FrameURLLoaderFactory
     DCHECK(frame_);
     frame_->UpdatePeakMemoryStats();
 
-    mojom::URLLoaderFactory* factory =
-        frame_->GetDefaultURLLoaderFactoryGetter()->GetFactoryForURL(
-            request.Url(), frame_->custom_url_loader_factory());
+    mojom::URLLoaderFactory* factory;
+    if (base::FeatureList::IsEnabled(features::kNetworkService)) {
+      DCHECK(frame_->subresource_loader_factories_);
+      factory = frame_->subresource_loader_factories_->GetFactoryForRequest(
+          request.Url());
+    } else {
+      factory = frame_->GetDefaultURLLoaderFactoryGetter()->GetFactoryForURL(
+          request.Url(), frame_->custom_url_loader_factory());
+    }
     DCHECK(factory);
 
     mojom::KeepAliveHandlePtr keep_alive_handle;
@@ -1815,7 +1821,8 @@ void RenderFrameImpl::OnNavigate(
   TRACE_EVENT2("navigation,rail", "RenderFrameImpl::OnNavigate", "id",
                routing_id_, "url", common_params.url.possibly_invalid_spec());
   NavigateInternal(common_params, start_params, request_params,
-                   std::unique_ptr<StreamOverrideParameters>());
+                   std::unique_ptr<StreamOverrideParameters>(),
+                   /*subresource_loader_factories=*/base::nullopt);
 }
 
 void RenderFrameImpl::BindEngagement(
@@ -3010,7 +3017,7 @@ void RenderFrameImpl::CommitNavigation(
     const CommonNavigationParams& common_params,
     const RequestNavigationParams& request_params,
     mojo::ScopedDataPipeConsumerHandle body_data,
-    mojom::URLLoaderFactoryPtr default_subresource_url_loader_factory) {
+    base::Optional<URLLoaderFactoryBundle> subresource_loader_factories) {
   CHECK(IsBrowserSideNavigationEnabled());
   // If this was a renderer-initiated navigation (nav_entry_id == 0) from this
   // frame, but it was aborted, then ignore it.
@@ -3045,8 +3052,6 @@ void RenderFrameImpl::CommitNavigation(
       },
       weak_factory_.GetWeakPtr());
 
-  SetCustomURLLoaderFactory(std::move(default_subresource_url_loader_factory));
-
   // If the request was initiated in the context of a user gesture then make
   // sure that the navigation also executes in the context of a user gesture.
   std::unique_ptr<blink::WebScopedUserGesture> gesture(
@@ -3057,7 +3062,8 @@ void RenderFrameImpl::CommitNavigation(
   browser_side_navigation_pending_url_ = GURL();
 
   NavigateInternal(common_params, StartNavigationParams(), request_params,
-                   std::move(stream_override));
+                   std::move(stream_override),
+                   std::move(subresource_loader_factories));
 
   // Don't add code after this since NavigateInternal may have destroyed this
   // RenderFrameImpl.
@@ -6159,7 +6165,8 @@ void RenderFrameImpl::NavigateInternal(
     const CommonNavigationParams& common_params,
     const StartNavigationParams& start_params,
     const RequestNavigationParams& request_params,
-    std::unique_ptr<StreamOverrideParameters> stream_params) {
+    std::unique_ptr<StreamOverrideParameters> stream_params,
+    base::Optional<URLLoaderFactoryBundle> subresource_loader_factories) {
   bool browser_side_navigation = IsBrowserSideNavigationEnabled();
 
   // PlzNavigate
@@ -6222,6 +6229,15 @@ void RenderFrameImpl::NavigateInternal(
   bool is_same_document =
       IsBrowserSideNavigationEnabled() &&
       FrameMsg_Navigate_Type::IsSameDocument(common_params.navigation_type);
+
+  // The browser only provides new subresource loader factories on
+  // cross-document navigations. In other cases we are free to keep reusing the
+  // ones we have.
+  if (!is_same_document &&
+      base::FeatureList::IsEnabled(features::kNetworkService)) {
+    DCHECK(subresource_loader_factories);
+    subresource_loader_factories_ = std::move(subresource_loader_factories);
+  }
 
   WebURLRequest request = CreateURLRequestForNavigation(
       common_params, request_params, std::move(stream_params),
@@ -6497,7 +6513,15 @@ void RenderFrameImpl::SyncSelectionIfRequired() {
 
 void RenderFrameImpl::SetCustomURLLoaderFactory(
     mojom::URLLoaderFactoryPtr factory) {
-  custom_url_loader_factory_ = std::move(factory);
+  if (base::FeatureList::IsEnabled(features::kNetworkService)) {
+    // When the network service is enabled, all subresource loads go through
+    // a factory from |subresource_loader_factories|. In this case we simply
+    // replace the existing default factory within the bundle.
+    DCHECK(subresource_loader_factories_);
+    subresource_loader_factories_->SetDefaultFactory(std::move(factory));
+  } else {
+    custom_url_loader_factory_ = std::move(factory);
+  }
 }
 
 void RenderFrameImpl::InitializeUserMediaClient() {
