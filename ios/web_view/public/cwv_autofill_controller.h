d2024dd384d8b571d58cbc8e6c5dcef4caad7daf
John Z Wu <jzw@chromium.org>
2017-10-30 02:09:54 +0000

    Public headers for //ios/web_view autofill API.
    Nothing will be compiled yet.
    
    Bug: 763797
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I2fd69181b3bbb36bcb913dd540bcc9e8ef37454d
    Reviewed-on: https://chromium-review.googlesource.com/741091
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Hiroshi Ichikawa <ichikawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512431}
    Former-commit-id: 7f0766ac417583c6c404588ff6c36a7a507c1195

---
+++ ios/web_view/public/cwv_autofill_controller.h
@@ -0,0 +1,52 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_WEB_VIEW_PUBLIC_CWV_AUTOFILL_CONTROLLER_H_
+#define IOS_WEB_VIEW_PUBLIC_CWV_AUTOFILL_CONTROLLER_H_
+
+#import <Foundation/Foundation.h>
+
+#import "cwv_export.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class CWVAutofillSuggestion;
+@protocol CWVAutofillControllerDelegate;
+
+CWV_EXPORT
+// Exposes features that allow autofilling html forms. Forms maybe include
+// single fields, address forms, or credit card forms.
+@interface CWVAutofillController : NSObject
+
+// Delegate to receive autofill callbacks.
+@property(nonatomic, weak, nullable) id<CWVAutofillControllerDelegate> delegate;
+
+// Clears the html form element with the 'name' attribute equal to |formName|.
+// No-op if no such form is found.
+// |completionHandler| will only be called on success.
+- (void)clearFormWithName:(NSString*)formName
+        completionHandler:(nullable void (^)(void))completionHandler;
+
+// For the field named |fieldName| in the form named |formName|, fetches
+// suggestions that can be used to autofill.
+// No-op if no such form and field can be found in the current page.
+// |completionHandler| will only be called on success.
+- (void)fetchSuggestionsForFormWithName:(NSString*)formName
+                              fieldName:(NSString*)fieldName
+                      completionHandler:
+                          (void (^)(NSArray<CWVAutofillSuggestion*>*))
+                              completionHandler;
+
+// Takes the |suggestion| and finds the form matching its |formName| and
+// |fieldName| property. If found, autofills the values in to the page.
+// No-op if no such form and field can be found in the current page.
+// |completionHandler| will only be called on success.
+- (void)fillSuggestion:(CWVAutofillSuggestion*)suggestion
+     completionHandler:(nullable void (^)(void))completionHandler;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#endif  // IOS_WEB_VIEW_PUBLIC_CWV_AUTOFILL_CONTROLLER_H_
