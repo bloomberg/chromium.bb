efdcfff66f92b81e4c982a2e1230c6f8a5b37311
Juan A. Navarro Perez <perezju@chromium.org>
2017-10-30 18:35:55 +0000

    [tools/perf] Remove obsolete memory.MemoryMetric
    The metric is no longer used, should be dead code now.
    
    Bug: 632021
    Change-Id: Ic6615654a74128b2782ae93fbf7c05dc95e4aaa9
    Reviewed-on: https://chromium-review.googlesource.com/744201
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512537}
    Former-commit-id: 04940300e3c098edd27ba3360113f54836fa7bcb

--- tools/perf/metrics/system_memory.py
+++ tools/perf/metrics/system_memory.py
@@ -2,13 +2,14 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import sys
+
 from telemetry.value import scalar
 
-from metrics import memory
-from metrics import Metric
+import metrics
 
 
-class SystemMemoryMetric(Metric):
+class SystemMemoryMetric(metrics.Metric):
   """SystemMemoryMetric gathers system memory statistic.
 
   This metric collects system memory stats per test.  It reports the difference
@@ -49,7 +50,7 @@ class SystemMemoryMetric(Metric):
     Args:
       trace_name: Trace name to identify the summary results for current page.
       exclude_metrics: List of memory metrics to exclude from results,
-                       e.g. VM, VMPeak, etc. See AddResultsForProcesses().
+                       e.g. VM, VMPeak, etc. See _AddResultsForProcesses().
     """
     assert self._memory_stats_end, 'Must call Stop() first'
     memory_stats = _SubtractMemoryStats(self._memory_stats_end,
@@ -57,7 +58,7 @@ class SystemMemoryMetric(Metric):
     if not memory_stats['Browser']:
       return
     exclude_metrics = exclude_metrics or {}
-    memory.AddResultsForProcesses(
+    _AddResultsForProcesses(
         results, memory_stats,
         metric_trace_name=trace_name, chart_trace_name='delta',
         exclude_metrics=exclude_metrics)
@@ -124,3 +125,93 @@ def _SubtractMemoryStats(end_memory_stats, start_memory_stats):
         else:
           memory_stats[process_type][metric] = end_value - start_value
   return memory_stats
+
+
+def _AddResultsForProcesses(results, memory_stats, chart_trace_name='final',
+                           metric_trace_name=None,
+                           exclude_metrics=None):
+  """Adds memory stats for browser, renderer and gpu processes.
+
+  Args:
+    results: A telemetry.results.PageTestResults object.
+    memory_stats: System memory stats collected.
+    chart_trace_name: Trace to identify memory metrics. Default is 'final'.
+    metric_trace_name: Trace to identify the metric results per test page.
+    exclude_metrics: List of memory metrics to exclude from results,
+                     e.g. VM, WorkingSetSize, etc.
+  """
+  metric = 'resident_set_size'
+  if sys.platform == 'win32':
+    metric = 'working_set'
+
+  exclude_metrics = exclude_metrics or {}
+
+  def AddResultsForProcessTypes(process_types_memory, process_type_trace):
+    """Add all results for a given set of process types.
+
+    Args:
+      process_types_memory: A list of process types, e.g. Browser, 'Renderer'.
+      process_type_trace: The name of this set of process types in the output.
+    """
+    def AddResult(value_name_memory, value_name_trace, description):
+      """Add a result for a given statistic.
+
+      Args:
+        value_name_memory: Name of some statistic, e.g. VM, WorkingSetSize.
+        value_name_trace: Name of this statistic to be used in the output.
+      """
+      if value_name_memory in exclude_metrics:
+        return
+      if len(process_types_memory) > 1 and value_name_memory.endswith('Peak'):
+        return
+      values = []
+      for process_type_memory in process_types_memory:
+        stats = memory_stats[process_type_memory]
+        if value_name_memory in stats:
+          values.append(stats[value_name_memory])
+      if values:
+        if metric_trace_name:
+          current_trace = '%s_%s' % (metric_trace_name, process_type_trace)
+          chart_name = value_name_trace
+        else:
+          current_trace = '%s_%s' % (value_name_trace, process_type_trace)
+          chart_name = current_trace
+        results.AddValue(scalar.ScalarValue(
+            results.current_page, '%s.%s' % (chart_name, current_trace), 'kb',
+            sum(values) / 1024, important=False, description=description))
+
+    AddResult('VM', 'vm_%s_size' % chart_trace_name,
+              'Virtual Memory Size (address space allocated).')
+    AddResult('WorkingSetSize', 'vm_%s_%s_size' % (metric, chart_trace_name),
+              'Working Set Size (Windows) or Resident Set Size (other '
+              'platforms).')
+    AddResult('PrivateDirty', 'vm_private_dirty_%s' % chart_trace_name,
+              'Private Dirty is basically the amount of RAM inside the '
+              'process that can not be paged to disk (it is not backed by the '
+              'same data on disk), and is not shared with any other '
+              'processes. Another way to look at this is the RAM that will '
+              'become available to the system when that process goes away '
+              '(and probably quickly subsumed into caches and other uses of '
+              'it).')
+    AddResult('ProportionalSetSize',
+              'vm_proportional_set_size_%s' % chart_trace_name,
+              'The Proportional Set Size (PSS) number is a metric the kernel '
+              'computes that takes into account memory sharing -- basically '
+              'each page of RAM in a process is scaled by a ratio of the '
+              'number of other processes also using that page. This way you '
+              'can (in theory) add up the PSS across all processes to see '
+              'the total RAM they are using, and compare PSS between '
+              'processes to get a rough idea of their relative weight.')
+    AddResult('SharedDirty', 'vm_shared_dirty_%s' % chart_trace_name,
+              'Shared Dirty is the amount of RAM outside the process that can '
+              'not be paged to disk, and is shared with other processes.')
+    AddResult('VMPeak', 'vm_peak_size',
+              'The peak Virtual Memory Size (address space allocated) usage '
+              'achieved by the * process.')
+    AddResult('WorkingSetSizePeak', '%s_peak_size' % metric,
+              'Peak Working Set Size.')
+
+  AddResultsForProcessTypes(['Browser'], 'browser')
+  AddResultsForProcessTypes(['Renderer'], 'renderer')
+  AddResultsForProcessTypes(['Gpu'], 'gpu')
+  AddResultsForProcessTypes(['Browser', 'Renderer', 'Gpu'], 'total')
