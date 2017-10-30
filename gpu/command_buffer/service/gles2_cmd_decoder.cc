944e6a34313898737415db32b8e4f5dce7ee04b6
Sadrul Habib Chowdhury <sadrul@chromium.org>
2017-10-30 22:42:17 +0000

    glx: Fix a crash during teardown.
    Two notable changes:
     . Do not immediately destroy the GLContextGLX when MakeCurrent() fails.
       This follows the pattern in GLContextEGL, where MakeCurrent() does
       not immediately destroy the context on failure.
     . Notify TextureManager about lost context early during tear down. This
       is necessary because FramebufferManager is destroyed first, and
       during its destruction, some gpu::gles2::Texture objects can be
       destroyed. But TextureManager is destroyed later, and is notified of
       the lost context only during destruction. As a result, Texture
       destruction does glDeleteTextures() while there's no context, causing
       a crash.
    
    BUG=777594
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I664e43c297cded17c223d3ee1ac4e32ca28ffd42
    Reviewed-on: https://chromium-review.googlesource.com/734945
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512651}
    Former-commit-id: c74bc22553928bd0c920a25118308f56c5cac432

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -4952,6 +4952,9 @@ void GLES2DecoderImpl::Destroy(bool have_context) {
     for (auto& fence : deschedule_until_finished_fences_) {
       fence->Invalidate();
     }
+
+    if (group_->texture_manager())
+      group_->texture_manager()->MarkContextLost();
   }
   deschedule_until_finished_fences_.clear();
 
