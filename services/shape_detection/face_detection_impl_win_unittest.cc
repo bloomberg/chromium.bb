0ac0284218cc652cd2ad850817ccaac13cd8e50b
junweifu <junwei.fu@intel.com>
2017-10-30 02:42:11 +0000

    ShapeDetection: Start with the minimum Win implementation
    Load com base functions dynamically at runtime with core_winrt_util.
    Request face detection failed if it's not supported.
    Add unittest and make sure work in win10 bots with using win10_chromium_x64_rel_ng
    in PRESUBMIT.py .
    Link Face Detection demo[2] here.
    
    Split original large CL[1] up in smaller subpatches including this CL.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/708336
    [2] https://codepen.io/miguelao/pen/PmJWro
    
    BUG=767021
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win10_chromium_x64_rel_ng;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ice003ac398379b60e0668c14a7cec75195be0441
    Reviewed-on: https://chromium-review.googlesource.com/724745
    Commit-Queue: Junwei Fu <junwei.fu@intel.com>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512433}
    Former-commit-id: 71716930f260e22871135e8657f85f7e383e6dc8

---
+++ services/shape_detection/face_detection_impl_win_unittest.cc
@@ -0,0 +1,28 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/shape_detection/face_detection_impl_win.h"
+
+#include "base/win/scoped_com_initializer.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace shape_detection {
+
+class FaceDetectionImplWinTest : public testing::Test {
+ protected:
+  void SetUp() override {
+    scoped_com_initializer_ = base::MakeUnique<base::win::ScopedCOMInitializer>(
+        base::win::ScopedCOMInitializer::kMTA);
+    ASSERT_TRUE(scoped_com_initializer_->Succeeded());
+  }
+
+ private:
+  std::unique_ptr<base::win::ScopedCOMInitializer> scoped_com_initializer_;
+};
+
+TEST_F(FaceDetectionImplWinTest, CreateAndDestroy) {
+  auto impl = FaceDetectionImplWin::Create();
+}
+
+}  // namespace shape_detection
