9ddd409f098b32abcf20970e97753e8787d06fd1
Yue Chen <yuec@google.com>
2017-10-30 16:13:39 -0700

    Disable interintra in >32x32 blocks
    Disabling such cases will reduce search time without hurting
    coding performance (-0.001%).
    
    Change-Id: Iaa4385053fcf5bd59fb1f94d5583eb19cf792242

--- av1/common/blockd.h
+++ av1/common/blockd.h
@@ -1298,8 +1298,7 @@ void av1_set_contexts(const MACROBLOCKD *xd, struct macroblockd_plane *pd,
 
 static INLINE int is_interintra_allowed_bsize(const BLOCK_SIZE bsize) {
 #if CONFIG_INTERINTRA
-  // TODO(debargha): Should this be bsize < BLOCK_LARGEST?
-  return (bsize >= BLOCK_8X8) && (bsize < BLOCK_64X64);
+  return (bsize >= BLOCK_8X8) && (bsize <= BLOCK_32X32);
 #else
   (void)bsize;
   return 0;
