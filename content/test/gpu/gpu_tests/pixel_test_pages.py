68b43822fb57a08200225acf6661f3a443a79cb7
sunxd <sunxd@chromium.org>
2017-10-30 20:36:28 +0000

    Rebaseline Pixel_CSS3DBlueBox
    The test failed because we fix a cc picture layer tiling issue. It used
    to ignore any scale factors, so fixing it would bring pixel differences
    in the test. This CL increase the version number for rebaseline.
    
    Bug: 774354
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I38fee772dd9a8ed779c420acc6508077b45ebe1f
    Reviewed-on: https://chromium-review.googlesource.com/735074
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Xianda Sun <sunxd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512609}
    Former-commit-id: 8111786e36f6d6fa6a787509a690379e60afdd0b

--- content/test/gpu/gpu_tests/pixel_test_pages.py
+++ content/test/gpu/gpu_tests/pixel_test_pages.py
@@ -64,7 +64,7 @@ def DefaultPages(base_name):
       'pixel_css3d.html',
       base_name + '_CSS3DBlueBox',
       test_rect=[0, 0, 300, 300],
-      revision=16),
+      revision=17),
 
     PixelTestPage(
       'pixel_webgl_aa_alpha.html',
