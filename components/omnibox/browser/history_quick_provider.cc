e3761a466f17cbdb0d0a4be5206c17ed7c7fa292
Kevin Bailey <krb@chromium.org>
2017-10-31 23:58:20 +0000

    [omnibox] Factor HistoryQuickProvider::DoAutocomplete()
    There is a large piece of code within |HQP::DoAutocomplete()| that can
    be on its own trivially. This change moves it to another method, and
    should not change behavior. This is to facilitate a downstream change.
    
    Bug: 
    Change-Id: Ifdbbac697aee1efd0f42068f2e0825bb176a8076
    Reviewed-on: https://chromium-review.googlesource.com/747064
    Commit-Queue: Kevin Bailey <krb@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513013}
    Former-commit-id: 61437c926d05c6bcde3cc59d7303d9f9567e75ab

--- components/omnibox/browser/history_quick_provider.cc
+++ components/omnibox/browser/history_quick_provider.cc
@@ -74,6 +74,24 @@ void HistoryQuickProvider::DoAutocomplete() {
   if (matches.empty())
     return;
 
+  // Loop over every result and add it to matches_.  In the process,
+  // guarantee that scores are decreasing.  |max_match_score| keeps
+  // track of the highest score we can assign to any later results we
+  // see.
+  int max_match_score = FindMaxMatchScore(matches);
+  for (ScoredHistoryMatches::const_iterator match_iter = matches.begin();
+       match_iter != matches.end(); ++match_iter) {
+    const ScoredHistoryMatch& history_match(*match_iter);
+    // Set max_match_score to the score we'll assign this result.
+    max_match_score = std::min(max_match_score, history_match.raw_score);
+    matches_.push_back(QuickMatchToACMatch(history_match, max_match_score));
+    // Mark this max_match_score as being used.
+    max_match_score--;
+  }
+}
+
+int HistoryQuickProvider::FindMaxMatchScore(
+    const ScoredHistoryMatches& matches) {
   // Figure out if HistoryURL provider has a URL-what-you-typed match
   // that ought to go first and what its score will be.
   bool will_have_url_what_you_typed_match_first = false;
@@ -160,30 +178,17 @@ void HistoryQuickProvider::DoAutocomplete() {
       }
     }
   }
-
-  // Loop over every result and add it to matches_.  In the process,
-  // guarantee that scores are decreasing.  |max_match_score| keeps
-  // track of the highest score we can assign to any later results we
-  // see.  Also, reduce |max_match_score| if we think there will be
-  // a URL-what-you-typed match.  (We want URL-what-you-typed matches for
-  // visited URLs to beat out any longer URLs, no matter how frequently
-  // they're visited.)  The strength of this reduction depends on the
-  // likely score for the URL-what-you-typed result.
-
+  // Return a |max_match_score| that is the raw score for the first match, but
+  // reduce it if we think there will be a URL-what-you-typed match.  (We want
+  // URL-what-you-typed matches for visited URLs to beat out any longer URLs, no
+  // matter how frequently they're visited.)  The strength of this reduction
+  // depends on the likely score for the URL-what-you-typed result.
   int max_match_score = matches.begin()->raw_score;
   if (will_have_url_what_you_typed_match_first) {
     max_match_score = std::min(max_match_score,
         url_what_you_typed_match_score - 1);
   }
-  for (ScoredHistoryMatches::const_iterator match_iter = matches.begin();
-       match_iter != matches.end(); ++match_iter) {
-    const ScoredHistoryMatch& history_match(*match_iter);
-    // Set max_match_score to the score we'll assign this result.
-    max_match_score = std::min(max_match_score, history_match.raw_score);
-    matches_.push_back(QuickMatchToACMatch(history_match, max_match_score));
-    // Mark this max_match_score as being used.
-    max_match_score--;
-  }
+  return max_match_score;
 }
 
 AutocompleteMatch HistoryQuickProvider::QuickMatchToACMatch(
