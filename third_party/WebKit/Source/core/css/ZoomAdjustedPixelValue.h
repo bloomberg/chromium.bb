44fdb6953e04f38af2d69674d72eb409418d829f
Eddy Mead <meade@chromium.org>
2017-10-31 03:44:04 +0000

    Rename zoomAdjustedPixelValue.h to ZoomAdjustedPixelValue.h
    This makes it consistent with the rest of the code.
    
    Also add it to the BUILD file, so that any changes in it are
    automatically picked up when building.
    
    Change-Id: I4a82d3c7c1f25e4f176d111615a8a66f2304d6c8
    Reviewed-on: https://chromium-review.googlesource.com/742764
    Reviewed-by: Bugs Nash <bugsnash@chromium.org>
    Commit-Queue: meade_UTC10 <meade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512735}
    Former-commit-id: f10e1c0af6be1811ee021e838dd2c49ae01bdd21

--- third_party/WebKit/Source/core/css/ZoomAdjustedPixelValue.h
+++ third_party/WebKit/Source/core/css/ZoomAdjustedPixelValue.h
@@ -2,19 +2,20 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef zoomAdjustedPixelValue_h
-#define zoomAdjustedPixelValue_h
+#ifndef ZoomAdjustedPixelValue_h
+#define ZoomAdjustedPixelValue_h
 
 #include "core/css/CSSPrimitiveValue.h"
 #include "core/style/ComputedStyle.h"
 
 namespace blink {
-class ComputedStyle;
 
 inline CSSPrimitiveValue* ZoomAdjustedPixelValue(double value,
                                                  const ComputedStyle& style) {
   return CSSPrimitiveValue::Create(AdjustFloatForAbsoluteZoom(value, style),
                                    CSSPrimitiveValue::UnitType::kPixels);
 }
-}
-#endif  // zoomAdjustedPixelValue_h
+
+}  // namespace blink
+
+#endif  // ZoomAdjustedPixelValue_h
