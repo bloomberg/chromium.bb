1cd60e4c675328779d9b9f14b57572e8b25388dd
tzik <tzik@chromium.org>
2017-10-31 10:50:31 +0000

    Remove WTF::BindInternal
    As WTF::BindInternal no longer does lots of things, this CL inlines it
    into WTF::Bind and blink::CrossThreadBind. As a preparation to remove
    WTF::Function for same thread usage.
    
    Bug: 771087
    Change-Id: I7b24c106d3ce9676b074a19521628107b7fe7b89
    Reviewed-on: https://chromium-review.googlesource.com/745742
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#512799}
    Former-commit-id: d53727e2c524a1c67bb666a19aa9b35f8e16f871

--- third_party/WebKit/Source/platform/CrossThreadFunctional.h
+++ third_party/WebKit/Source/platform/CrossThreadFunctional.h
@@ -32,9 +32,14 @@ template <typename FunctionType, typename... Ps>
 Function<base::MakeUnboundRunType<FunctionType, Ps...>,
          WTF::kCrossThreadAffinity>
 CrossThreadBind(FunctionType function, Ps&&... parameters) {
-  return WTF::BindInternal<WTF::kCrossThreadAffinity>(
-      function, CrossThreadCopier<typename std::decay<Ps>::type>::Copy(
-                    std::forward<Ps>(parameters))...);
+  static_assert(
+      WTF::internal::CheckGCedTypeRestrictions<std::index_sequence_for<Ps...>,
+                                               std::decay_t<Ps>...>::ok,
+      "A bound argument uses a bad pattern.");
+  using UnboundRunType = base::MakeUnboundRunType<FunctionType, Ps...>;
+  return WTF::Function<UnboundRunType, WTF::kCrossThreadAffinity>(
+      base::Bind(function, CrossThreadCopier<std::decay_t<Ps>>::Copy(
+                               std::forward<Ps>(parameters))...));
 }
 
 }  // namespace blink
