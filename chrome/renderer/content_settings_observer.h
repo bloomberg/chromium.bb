bc797335731390db751e15736d0d4dec9e3fc187
Lei Zhang <thestig@chromium.org>
2017-10-31 23:56:29 +0000

    Use flat containers in ContentSettingsObserver.
    Change-Id: Ic6489f81849456c7ce225cda73c5c08ec41e8e29
    Reviewed-on: https://chromium-review.googlesource.com/747161
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513012}
    Former-commit-id: a3774c1640a918643a2a68fc43fb8ec3a688b484

--- chrome/renderer/content_settings_observer.h
+++ chrome/renderer/content_settings_observer.h
@@ -5,12 +5,11 @@
 #ifndef CHROME_RENDERER_CONTENT_SETTINGS_OBSERVER_H_
 #define CHROME_RENDERER_CONTENT_SETTINGS_OBSERVER_H_
 
-#include <map>
-#include <set>
 #include <string>
-#include <unordered_map>
 #include <utility>
 
+#include "base/containers/flat_map.h"
+#include "base/containers/flat_set.h"
 #include "base/gtest_prod_util.h"
 #include "base/time/time.h"
 #include "chrome/common/insecure_content_renderer.mojom.h"
@@ -164,22 +163,20 @@ class ContentSettingsObserver
   const RendererContentSettingRules* content_setting_rules_;
 
   // Stores if images, scripts, and plugins have actually been blocked.
-  std::map<ContentSettingsType, bool> content_blocked_;
+  base::flat_set<ContentSettingsType> content_blocked_;
 
   // Caches the result of AllowStorage.
   using StoragePermissionsKey = std::pair<GURL, bool>;
-  std::map<StoragePermissionsKey, bool> cached_storage_permissions_;
+  base::flat_map<StoragePermissionsKey, bool> cached_storage_permissions_;
 
   // Caches the result of AllowScript.
-  std::unordered_map<blink::WebFrame*, bool> cached_script_permissions_;
+  base::flat_map<blink::WebFrame*, bool> cached_script_permissions_;
 
-  std::set<std::string> temporarily_allowed_plugins_;
+  base::flat_set<std::string> temporarily_allowed_plugins_;
   bool is_interstitial_page_;
 
   int current_request_id_;
-  using PermissionRequestMap =
-      std::unordered_map<int, blink::WebContentSettingCallbacks>;
-  PermissionRequestMap permission_requests_;
+  base::flat_map<int, blink::WebContentSettingCallbacks> permission_requests_;
 
   // If true, IsWhitelistedForContentSettings will always return true.
   const bool should_whitelist_;
