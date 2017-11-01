678cfb171298fabd65d790fc690e348793d67e9a
Hajime Hoshi <hajimehoshi@chromium.org>
2017-11-01 05:10:50 +0000

    Remove TaskRunnerHelper::Get except for WorkerThread
    This is one step of all replacings at https://chromium-review.googlesource.com/c/chromium/src/+/743663
    
    Bug: 777775
    Change-Id: Ib388098d7a6f3d1baa3b144734f470b0263f74f7
    Reviewed-on: https://chromium-review.googlesource.com/748741
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513078}
    Former-commit-id: 3763897898cb65f4e60e1e33f7def429b4293259

--- third_party/WebKit/Source/core/dom/TaskRunnerHelper.cpp
+++ third_party/WebKit/Source/core/dom/TaskRunnerHelper.cpp
@@ -4,39 +4,9 @@
 
 #include "core/dom/TaskRunnerHelper.h"
 
-#include "core/dom/Document.h"
-#include "core/dom/ExecutionContext.h"
-#include "core/frame/LocalFrame.h"
-#include "core/workers/WorkerOrWorkletGlobalScope.h"
 #include "core/workers/WorkerThread.h"
-#include "platform/WebFrameScheduler.h"
-#include "platform/WebTaskRunner.h"
-#include "public/platform/Platform.h"
-#include "public/platform/WebThread.h"
 
 namespace blink {
-
-scoped_refptr<WebTaskRunner> TaskRunnerHelper::Get(TaskType type,
-                                                   LocalFrame* frame) {
-  return frame->GetTaskRunner(type);
-}
-
-scoped_refptr<WebTaskRunner> TaskRunnerHelper::Get(TaskType type,
-                                                   Document* document) {
-  return document->GetTaskRunner(type);
-}
-
-scoped_refptr<WebTaskRunner> TaskRunnerHelper::Get(
-    TaskType type,
-    ExecutionContext* execution_context) {
-  return execution_context->GetTaskRunner(type);
-}
-
-scoped_refptr<WebTaskRunner> TaskRunnerHelper::Get(
-    TaskType type,
-    WorkerOrWorkletGlobalScope* global_scope) {
-  return global_scope->GetTaskRunner(type);
-}
 
 scoped_refptr<WebTaskRunner> TaskRunnerHelper::Get(
     TaskType type,
