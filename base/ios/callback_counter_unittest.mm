93f17f53823580e8078c3a2cf052484b54e51d77
mrefaat <mrefaat@chromium.org>
2017-11-01 21:04:16 +0000

    Move CallbackCounter from ios/chrome/browser to base/ios
    This is more suitable place for it, as it will be used from different layers.
    
    Bug: 
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Iaa3a11f53730216d7c3794fc5cd771fd936c17be
    Reviewed-on: https://chromium-review.googlesource.com/748601
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513265}
    Former-commit-id: 6687bb79942d0ebd85edb0533d057647b2f6b317

--- base/ios/callback_counter_unittest.mm
+++ base/ios/callback_counter_unittest.mm
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ios/chrome/browser/callback_counter.h"
+#include "base/ios/callback_counter.h"
 
 #include "base/bind.h"
 #include "base/mac/bind_objc_block.h"
@@ -10,17 +10,13 @@
 #import "base/test/ios/wait_util.h"
 #include "testing/platform_test.h"
 
-#if !defined(__has_feature) || !__has_feature(objc_arc)
-#error "This file requires ARC support."
-#endif
-
 using CallbackCounterTest = PlatformTest;
 
 // Tests that CallbackCounter works with 2 callbacks.
 TEST_F(CallbackCounterTest, Basic) {
   __block BOOL block_was_called = NO;
   scoped_refptr<CallbackCounter> callback_counter =
-      new CallbackCounter(base::BindBlockArc(^{
+      new CallbackCounter(base::BindBlock(^{
         block_was_called = YES;
       }));
 
