c2365c3e49524f5d181a2144a40e780f2c89fa74
Edward Jung <edwardjung@chromium.org>
2017-11-01 10:15:56 +0000

    Interstitial tweaks for small screen / low bit depth screens
    + Remove the gradient from behind the fix button nav as this is poorly
      rendered on low bit depth screens.
    + Reduce the margin needed for the details button.
    + Reduced the margin for the dino game. This allows all the text to fit
      without scrolling.
    
    Bug: 755714
    Change-Id: Ia90afd5ab621fd0c0c9781cb13f7a9e003c053a0
    Reviewed-on: https://chromium-review.googlesource.com/741741
    Commit-Queue: Edward Jung <edwardjung@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513103}
    Former-commit-id: a507c0434dff8f56698202494ffa46a3d434971a

--- components/security_interstitials/core/common/resources/interstitial_mobile_nav.js
+++ components/security_interstitials/core/common/resources/interstitial_mobile_nav.js
@@ -42,6 +42,8 @@ function onResize() {
 }
 
 function setupMobileNav() {
+  // Check for screens with a low colour bit depth.
+  document.body.classList.toggle('low-color-depth', screen.colorDepth < 24);
   window.addEventListener('resize', onResize);
   onResize();
 }
