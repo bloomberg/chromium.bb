db5bd469b6826434767c0471e45376acf1292633
Nicolas Dossou-gbete <dgn@chromium.org>
2017-11-01 20:51:38 +0000

    📰 Remove deprecated SiteExploreUI code
    Bug: 740905
    Change-Id: I8b052887895bee3038df20517d8cc68f36389962
    Reviewed-on: https://chromium-review.googlesource.com/746803
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513259}
    Former-commit-id: 7e95a9a243b1e3c197236703501a9f40a8bdbf67

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/SiteSection.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/SiteSection.java
@@ -49,9 +49,7 @@ public class SiteSection extends OptionalLeaf implements TileGroup.Observer {
     }
 
     public static SiteSectionViewHolder createViewHolder(ViewGroup view, UiConfig uiConfig) {
-        return SuggestionsConfig.useSitesExplorationUi()
-                ? new SiteExploreViewHolder(view, MAX_TILE_COLUMNS)
-                : new TileGridViewHolder(view, getMaxTileRows(), MAX_TILE_COLUMNS, uiConfig);
+        return new TileGridViewHolder(view, getMaxTileRows(), MAX_TILE_COLUMNS, uiConfig);
     }
 
     public SiteSection(SuggestionsUiDelegate uiDelegate, ContextMenuManager contextMenuManager,
@@ -127,9 +125,6 @@ public class SiteSection extends OptionalLeaf implements TileGroup.Observer {
 
     @LayoutRes
     private static int getLayout() {
-        if (SuggestionsConfig.useSitesExplorationUi()) {
-            return R.layout.suggestions_site_explore;
-        }
         if (FeatureUtilities.isChromeHomeEnabled()) {
             return R.layout.suggestions_site_tile_grid_modern;
         }
