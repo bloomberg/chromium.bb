a93f063f020530b4391e570935316a7ba411c14c
Dale Curtis <dalecurtis@chromium.org>
2017-10-31 17:06:30 -0700

    Revert intmath.h changes, seem no longer necessary.
    BUG=none
    TEST=arm android build succeeds
    
    Change-Id: Ic32746ec8e5eccae981766d86004df0258e591f3
    Reviewed-on: https://chromium-review.googlesource.com/748211
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>

--- libavutil/arm/intmath.h
+++ libavutil/arm/intmath.h
@@ -62,13 +62,6 @@ static av_always_inline av_const int av_clip_int16_arm(int a)
     return x;
 }
 
-/*
-TODO(dalecurtis): This causes compilation to fail if the function can't be
-inlined because the 2nd operand in usat must be an immediate.  So disable since
-av_always_inline doesn't seem to really mean always...
-TODO(watk): Upstream added av_clip_intp2_arm, which uses the similar ssat, so I
-commented it out too.
-
 #define av_clip_intp2 av_clip_intp2_arm
 static av_always_inline av_const int av_clip_intp2_arm(int a, int p)
 {
@@ -84,7 +77,6 @@ static av_always_inline av_const unsigned av_clip_uintp2_arm(int a, int p)
     __asm__ ("usat %0, %2, %1" : "=r"(x) : "r"(a), "i"(p));
     return x;
 }
-*/
 
 #define av_sat_add32 av_sat_add32_arm
 static av_always_inline int av_sat_add32_arm(int a, int b)
