567567f1a7cc4143e3fd7d16776676a006c9f886
Yuzhu Shen <yzshen@chromium.org>
2017-11-02 17:20:00 +0000

    Remove unused method define() in the private mojoPrivate extensions API.
    Bug: 699569,718047
    Change-Id: I2ebda3e66b6cb7f74cb6bc3f577f978022374a14
    Reviewed-on: https://chromium-review.googlesource.com/750191
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513529}
    Former-commit-id: eca9c1de14c95fc5c4f23f1b2b144349de5f2817

--- extensions/renderer/api/mojo_private/mojo_private_unittest.cc
+++ extensions/renderer/api/mojo_private/mojo_private_unittest.cc
@@ -7,7 +7,6 @@
 #include "base/macros.h"
 #include "extensions/common/extension_builder.h"
 #include "extensions/common/value_builder.h"
-#include "gin/modules/module_registry.h"
 
 // A test launcher for tests for the mojoPrivate API defined in
 // extensions/test/data/mojo_private_unittest.js.
@@ -17,10 +16,6 @@ namespace extensions {
 class MojoPrivateApiTest : public ApiTestBase {
  public:
   MojoPrivateApiTest() = default;
-
-  gin::ModuleRegistry* module_registry() {
-    return gin::ModuleRegistry::From(env()->context()->v8_context());
-  }
 
   scoped_refptr<const Extension> CreateExtension() override {
     std::unique_ptr<base::DictionaryValue> manifest =
@@ -40,36 +35,13 @@ class MojoPrivateApiTest : public ApiTestBase {
   DISALLOW_COPY_AND_ASSIGN(MojoPrivateApiTest);
 };
 
-TEST_F(MojoPrivateApiTest, Define) {
-  ASSERT_NO_FATAL_FAILURE(RunTest("mojo_private_unittest.js", "testDefine"));
-  EXPECT_EQ(1u, module_registry()->available_modules().count("testModule"));
-}
-
-TEST_F(MojoPrivateApiTest, DefineRegistersModule) {
-  ASSERT_NO_FATAL_FAILURE(
-      RunTest("mojo_private_unittest.js", "testDefineRegistersModule"));
-  EXPECT_EQ(1u, module_registry()->available_modules().count("testModule"));
-  EXPECT_EQ(1u, module_registry()->available_modules().count("dependency"));
-}
-
-TEST_F(MojoPrivateApiTest, DefineModuleDoesNotExist) {
-  ASSERT_NO_FATAL_FAILURE(
-      RunTest("mojo_private_unittest.js", "testDefineModuleDoesNotExist"));
-  EXPECT_EQ(0u, module_registry()->available_modules().count("testModule"));
-  EXPECT_EQ(0u,
-            module_registry()->available_modules().count("does not exist!"));
-  EXPECT_EQ(1u, module_registry()->unsatisfied_dependencies().count(
-                    "does not exist!"));
-}
-
 TEST_F(MojoPrivateApiTest, RequireAsync) {
+  env()->RegisterModule("add",
+                        "define('add', [], function() {"
+                        "  return { Add: function(x, y) { return x + y; } };"
+                        "});");
   ASSERT_NO_FATAL_FAILURE(
       RunTest("mojo_private_unittest.js", "testRequireAsync"));
-}
-
-TEST_F(MojoPrivateApiTest, DefineAndRequire) {
-  ASSERT_NO_FATAL_FAILURE(
-      RunTest("mojo_private_unittest.js", "testDefineAndRequire"));
 }
 
 }  // namespace extensions
