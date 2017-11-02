1e10a15eac618ab4558c542c4ca6623ff7c1e002
Michael van Ouwerkerk <mvanouwerkerk@google.com>
2017-11-02 16:59:42 +0000

    Add UiCapture test for having no tile suggestions.
    Bug: 761813
    Change-Id: Icd5c2f51d6831167745f63ccb9e635bad7b89a84
    Reviewed-on: https://chromium-review.googlesource.com/751270
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513521}
    Former-commit-id: ad1b6632ed8c2ca34a9cdb0603167fec8dae76ac

---
+++ chrome/android/javatests/src/org/chromium/chrome/browser/suggestions/HomeSheetNoTilesUiCaptureTest.java
@@ -0,0 +1,65 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.suggestions;
+
+import static org.chromium.chrome.test.BottomSheetTestRule.waitForWindowUpdates;
+
+import android.support.test.filters.MediumTest;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.Restriction;
+import org.chromium.chrome.browser.ntp.NtpUiCaptureTestData;
+import org.chromium.chrome.browser.preferences.ChromePreferenceManager;
+import org.chromium.chrome.browser.test.ScreenShooter;
+import org.chromium.chrome.browser.widget.bottomsheet.BottomSheet;
+import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
+import org.chromium.chrome.test.util.browser.suggestions.FakeMostVisitedSites;
+import org.chromium.chrome.test.util.browser.suggestions.FakeSuggestionsSource;
+import org.chromium.chrome.test.util.browser.suggestions.SuggestionsDependenciesRule;
+import org.chromium.ui.test.util.UiRestriction;
+
+/**
+ * Tests for the appearance of the home sheet when there are no tiles.
+ */
+@RunWith(ChromeJUnit4ClassRunner.class)
+@Restriction(UiRestriction.RESTRICTION_TYPE_PHONE) // ChromeHome is only enabled on phones
+public class HomeSheetNoTilesUiCaptureTest {
+    @Rule
+    public SuggestionsBottomSheetTestRule mActivityRule = new SuggestionsBottomSheetTestRule();
+
+    @Rule
+    public SuggestionsDependenciesRule setupSuggestions() {
+        SuggestionsDependenciesRule.TestFactory depsFactory = NtpUiCaptureTestData.createFactory();
+        FakeSuggestionsSource suggestionsSource = new FakeSuggestionsSource();
+        NtpUiCaptureTestData.registerArticleSamples(suggestionsSource);
+        depsFactory.suggestionsSource = suggestionsSource;
+        ((FakeMostVisitedSites) depsFactory.mostVisitedSites).setTileSuggestions(new String[] {});
+        return new SuggestionsDependenciesRule(depsFactory);
+    }
+
+    @Rule
+    public ScreenShooter mScreenShooter = new ScreenShooter();
+
+    @Before
+    public void setup() throws InterruptedException {
+        ChromePreferenceManager.getInstance().setNewTabPagePersonalizedSigninPromoDismissed(true);
+        mActivityRule.startMainActivityOnBlankPage();
+    }
+
+    @Test
+    @MediumTest
+    @Feature({"UiCatalogue"})
+    @ScreenShooter.Directory("HomeSheetTiles")
+    public void testNoTiles() {
+        mActivityRule.setSheetState(BottomSheet.SHEET_STATE_FULL, false);
+        waitForWindowUpdates();
+        mScreenShooter.shoot("NoTiles");
+    }
+}
