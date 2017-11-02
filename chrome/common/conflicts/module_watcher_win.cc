1f026f0b08dd7b760301e0c4c2464fa6cd6b66d0
Patrick Monette <pmonette@chromium.org>
2017-11-02 22:47:19 +0000

    Do the module enumeration on a background task to improve startup
    Bug: 751136
    Change-Id: Id8f492cb0ed9bacc894be970e5d952d6ba20f4e4
    Reviewed-on: https://chromium-review.googlesource.com/747726
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513619}
    Former-commit-id: bd7984c8bf69c1cfe379a95168b2a62628b958b2

--- chrome/common/conflicts/module_watcher_win.cc
+++ chrome/common/conflicts/module_watcher_win.cc
@@ -11,11 +11,16 @@
 #include <string>
 #include <utility>
 
+#include "base/bind.h"
 #include "base/lazy_instance.h"
+#include "base/location.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/synchronization/lock.h"
+#include "base/task_scheduler/post_task.h"
+#include "base/task_scheduler/task_traits.h"
+#include "base/threading/sequenced_task_runner_handle.h"
 #include "base/win/scoped_handle.h"
 
 // These structures and functions are documented in MSDN, see
@@ -150,11 +155,23 @@ ModuleWatcher::~ModuleWatcher() {
   g_module_watcher_instance = nullptr;
 }
 
+ModuleWatcher::ModuleWatcher() : weak_ptr_factory_(this) {}
+
 // Initializes the ModuleWatcher instance.
 void ModuleWatcher::Initialize(OnModuleEventCallback callback) {
   callback_ = std::move(callback);
   RegisterDllNotificationCallback();
-  EnumerateAlreadyLoadedModules();
+
+  // The enumeration of modules is done on a background task to make sure it
+  // doesn't slow down startup.
+  base::PostTaskWithTraits(
+      FROM_HERE,
+      {base::TaskPriority::BACKGROUND,
+       base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN},
+      base::BindOnce(&ModuleWatcher::EnumerateAlreadyLoadedModules,
+                     base::SequencedTaskRunnerHandle::Get(),
+                     base::BindRepeating(&ModuleWatcher::RunCallback,
+                                         weak_ptr_factory_.GetWeakPtr())));
 }
 
 void ModuleWatcher::RegisterDllNotificationCallback() {
@@ -177,7 +194,10 @@ void ModuleWatcher::UnregisterDllNotificationCallback() {
     unreg_fn(dll_notification_cookie_);
 }
 
-void ModuleWatcher::EnumerateAlreadyLoadedModules() {
+// static
+void ModuleWatcher::EnumerateAlreadyLoadedModules(
+    scoped_refptr<base::SequencedTaskRunner> task_runner,
+    OnModuleEventCallback callback) {
   // Get all modules in the current process. According to MSDN,
   // CreateToolhelp32Snapshot should be retried as long as its returning
   // ERROR_BAD_LENGTH. To avoid locking up here a retry limit is enforced.
@@ -201,7 +221,7 @@ void ModuleWatcher::EnumerateAlreadyLoadedModules() {
     ModuleEvent event(mojom::ModuleEventType::MODULE_ALREADY_LOADED,
                       base::FilePath(module.szExePath), module.modBaseAddr,
                       module.modBaseSize);
-    callback_.Run(event);
+    task_runner->PostTask(FROM_HERE, base::BindOnce(callback, event));
   }
 }
 
@@ -240,4 +260,6 @@ void __stdcall ModuleWatcher::LoaderNotificationCallback(
   }
 }
 
-ModuleWatcher::ModuleWatcher() = default;
+void ModuleWatcher::RunCallback(const ModuleEvent& event) {
+  callback_.Run(event);
+}
