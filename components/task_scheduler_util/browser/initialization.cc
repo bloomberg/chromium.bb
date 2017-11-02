c823227fd6082b894c04db42fe3d7e7717739f17
Robert Liao <robliao@chromium.org>
2017-11-02 21:22:16 +0000

    Enable Shared Worker Pool COM MTA in the Browser Process
    BUG=706479
    TBR=jam@chromium.org
    Task Scheduler parameter change for the browser process.
    
    Change-Id: I0fa1712a4696db1845e4d4c57e68c3f7af49cec4
    Reviewed-on: https://chromium-review.googlesource.com/749629
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513604}
    Former-commit-id: 8d3b75514c405135e8bbc5e4a9c1b8576fb4d419

--- components/task_scheduler_util/browser/initialization.cc
+++ components/task_scheduler_util/browser/initialization.cc
@@ -7,6 +7,7 @@
 #include <map>
 #include <string>
 
+#include "build/build_config.h"
 #include "components/task_scheduler_util/common/variations_util.h"
 #include "components/variations/variations_associated_data.h"
 
@@ -24,8 +25,14 @@ GetBrowserTaskSchedulerInitParamsFromVariations() {
   if (!::variations::GetVariationParams(kFieldTrialName, &variation_params))
     return nullptr;
 
-  return GetTaskSchedulerInitParams(
-      "", variation_params, base::SchedulerBackwardCompatibility::DISABLED);
+  std::unique_ptr<base::TaskScheduler::InitParams> init_params =
+      GetTaskSchedulerInitParams(
+          "", variation_params, base::SchedulerBackwardCompatibility::DISABLED);
+#if defined(OS_WIN)
+  init_params->shared_worker_pool_environment =
+      base::TaskScheduler::InitParams::SharedWorkerPoolEnvironment::COM_MTA;
+#endif  // defined(OS_WIN)
+  return init_params;
 }
 
 }  // namespace task_scheduler_util
