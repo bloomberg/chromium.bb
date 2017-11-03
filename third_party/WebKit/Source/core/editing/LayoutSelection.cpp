b2d64f9e676c5e19b36f542906cc2a3c0c02f7d7
Yoichi Osato <yoichio@chromium.org>
2017-11-03 07:11:40 +0000

    Revert "Reset SelectionState w/o propagation when clear old SeletionState."
    This reverts commit 903f78ca8e977871ea5ecb430a740863cd0d1456 [formerly 090ef9237a1e1c0c986532ca76d4b85825d7b496].
    
    Reason for revert: crbug.com/780558
    
    Original change's description:
    > Reset SelectionState w/o propagation when clear old SeletionState.
    > 
    > We clear old selected but not selected LayoutObject's SelectionState
    > to kNone using LayoutObject::SetSelectionStateIfNeeded().
    > However the function calls virtual LayoutObject::SetSelectionState(),
    > which change containing blocks' SelectionState.
    > This causes inconsistency that marked not kNone LayoutObject can
    > accidentally marked kNone.
    > 
    > This patch changes it to call non virtual
    > LayoutObject::SetSelectionState() and assign kNone SelectionState
    > to only target old selected LayoutObjects.
    > 
    > Bug: 739062
    > 
    > Change-Id: I9ca736de43a2a982b1e29a1607325a1b77e3c35f
    > Reviewed-on: https://chromium-review.googlesource.com/748841
    > Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    > Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#513096}
    
    TBR=yosin@chromium.org,yoichio@chromium.org,xiaochengh@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 739062, 780558
    Change-Id: Ibe0945e61bb87079a4a925799e369e4f1e38662d
    Reviewed-on: https://chromium-review.googlesource.com/752781
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513715}
    Former-commit-id: 68a0980afbb3a5abcbd433c07f6774b6d732ddc5

--- third_party/WebKit/Source/core/editing/LayoutSelection.cpp
+++ third_party/WebKit/Source/core/editing/LayoutSelection.cpp
@@ -258,7 +258,7 @@ static void SetShouldInvalidateSelection(
   // above.
   for (LayoutObject* layout_object : old_invalidation_set) {
     const SelectionState old_state = layout_object->GetSelectionState();
-    layout_object->LayoutObject::SetSelectionState(SelectionState::kNone);
+    layout_object->SetSelectionStateIfNeeded(SelectionState::kNone);
     if (layout_object->GetSelectionState() == old_state)
       continue;
     SetShouldInvalidateIfNeeds(layout_object);
@@ -598,15 +598,43 @@ void LayoutSelection::Commit() {
   DCHECK(frame_selection_->GetDocument().GetLayoutView()->GetFrameView());
   SetShouldInvalidateSelection(new_range, paint_range_);
   paint_range_ = new_paint_range;
-  if (paint_range_.StartLayoutObject() == paint_range_.EndLayoutObject()) {
-    DCHECK_EQ(paint_range_.StartLayoutObject()->GetSelectionState(),
-              SelectionState::kStartAndEnd);
-    return;
+  // TODO(yoichio): Remove this if state.
+  // This SelectionState reassignment is ad-hoc patch for
+  // prohibiting use-after-free(crbug.com/752715).
+  // LayoutText::setSelectionState(state) propergates |state| to ancestor
+  // LayoutObjects, which can accidentally change start/end LayoutObject state
+  // then LayoutObject::IsSelectionBorder() returns false although we should
+  // clear selection at LayoutObject::WillBeRemoved().
+  // We should make LayoutObject::setSelectionState() trivial and remove
+  // such propagation or at least do it in LayoutSelection.
+  if ((paint_range_.StartLayoutObject()->GetSelectionState() !=
+           SelectionState::kStart &&
+       paint_range_.StartLayoutObject()->GetSelectionState() !=
+           SelectionState::kStartAndEnd) ||
+      (paint_range_.EndLayoutObject()->GetSelectionState() !=
+           SelectionState::kEnd &&
+       paint_range_.EndLayoutObject()->GetSelectionState() !=
+           SelectionState::kStartAndEnd)) {
+    if (paint_range_.StartLayoutObject() == paint_range_.EndLayoutObject()) {
+      paint_range_.StartLayoutObject()->SetSelectionStateIfNeeded(
+          SelectionState::kStartAndEnd);
+    } else {
+      paint_range_.StartLayoutObject()->SetSelectionStateIfNeeded(
+          SelectionState::kStart);
+      paint_range_.EndLayoutObject()->SetSelectionStateIfNeeded(
+          SelectionState::kEnd);
+    }
   }
-  DCHECK_EQ(paint_range_.StartLayoutObject()->GetSelectionState(),
-            SelectionState::kStart);
-  DCHECK_EQ(paint_range_.EndLayoutObject()->GetSelectionState(),
-            SelectionState::kEnd);
+  // TODO(yoichio): If start == end, they should be kStartAndEnd.
+  // If not, start.SelectionState == kStart and vice versa.
+  DCHECK(paint_range_.StartLayoutObject()->GetSelectionState() ==
+             SelectionState::kStart ||
+         paint_range_.StartLayoutObject()->GetSelectionState() ==
+             SelectionState::kStartAndEnd);
+  DCHECK(paint_range_.EndLayoutObject()->GetSelectionState() ==
+             SelectionState::kEnd ||
+         paint_range_.EndLayoutObject()->GetSelectionState() ==
+             SelectionState::kStartAndEnd);
 }
 
 void LayoutSelection::OnDocumentShutdown() {
