19f04d0d8cf5201bff57b3e1c0b2f031161ee736
Daniel Bratell <bratell@opera.com>
2017-11-03 15:24:06 +0000

    Use PostMessageW instead of PostMessage to avoid undefs
    There are code that does #undef PostMessage to avoid
    the win32 defines and if such code/headers get included in
    render_widget_host_view_event_handler.cc, then there won't be
    a PostMessage to call.
    
    The easiest solution is to call PostMessageW instead (the Unicode
    version of the API) since that is the function we want to call
    anyway.
    
    Bug: 746953
    Change-Id: I6bb61404e2685846b4298223fe588d51fa0704d5
    Reviewed-on: https://chromium-review.googlesource.com/753582
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513785}
    Former-commit-id: a309b574099ec832c98a16686ac365173631fc60

--- content/browser/renderer_host/render_widget_host_view_event_handler.cc
+++ content/browser/renderer_host/render_widget_host_view_event_handler.cc
@@ -55,7 +55,7 @@ BOOL CALLBACK DismissOwnedPopups(HWND window, LPARAM arg) {
   if (::IsWindowVisible(window)) {
     const HWND owner = ::GetWindow(window, GW_OWNER);
     if (toplevel_hwnd == owner) {
-      ::PostMessage(window, WM_CANCELMODE, 0, 0);
+      ::PostMessageW(window, WM_CANCELMODE, 0, 0);
     }
   }
 
