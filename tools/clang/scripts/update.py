03fc558046bbcf943e6bb24852c75f34fac587dc
Peter Collingbourne <pcc@chromium.org>
2017-11-03 17:10:16 +0000

    update.py: Build lld with threading support unconditionally.
    This removes the condition that we need to be doing a bootstrap build.
    With this change we will get an lld built with threads on Windows as
    well as Linux ToT builds.
    
    Bug: 779639
    Change-Id: I961920d0f04f4aa2607edeab44a02e3857a2fd52
    Reviewed-on: https://chromium-review.googlesource.com/745525
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513810}
    Former-commit-id: cd776f006a0fe2e35e179944fa475c97457069ae

--- tools/clang/scripts/update.py
+++ tools/clang/scripts/update.py
@@ -35,7 +35,7 @@ if use_head_revision:
   CLANG_REVISION = 'HEAD'
 
 # This is incremented when pushing a new build of Clang at the same revision.
-CLANG_SUB_REVISION=1
+CLANG_SUB_REVISION=2
 
 PACKAGE_VERSION = "%s-%s" % (CLANG_REVISION, CLANG_SUB_REVISION)
 
@@ -48,10 +48,10 @@ LLVM_DIR = os.path.join(THIRD_PARTY_DIR, 'llvm')
 LLVM_BOOTSTRAP_DIR = os.path.join(THIRD_PARTY_DIR, 'llvm-bootstrap')
 LLVM_BOOTSTRAP_INSTALL_DIR = os.path.join(THIRD_PARTY_DIR,
                                           'llvm-bootstrap-install')
-LLVM_LTO_LLD_DIR = os.path.join(THIRD_PARTY_DIR, 'llvm-lto-lld')
 CHROME_TOOLS_SHIM_DIR = os.path.join(LLVM_DIR, 'tools', 'chrometools')
 LLVM_BUILD_DIR = os.path.join(CHROMIUM_DIR, 'third_party', 'llvm-build',
                               'Release+Asserts')
+LLD_BUILD_DIR = os.path.join(LLVM_BUILD_DIR, 'lld')
 COMPILER_RT_BUILD_DIR = os.path.join(LLVM_BUILD_DIR, 'compiler-rt')
 CLANG_DIR = os.path.join(LLVM_DIR, 'tools', 'clang')
 LLD_DIR = os.path.join(LLVM_DIR, 'tools', 'lld')
@@ -569,37 +569,6 @@ def UpdateClang(args):
       cxxflags = ['--gcc-toolchain=' + args.gcc_toolchain]
     print 'Building final compiler'
 
-  # Build lld with LTO. That speeds up the linker by ~10%.
-  # We only use LTO for Linux now.
-  if args.bootstrap and args.lto_lld:
-    print 'Building LTO lld'
-    if os.path.exists(LLVM_LTO_LLD_DIR):
-      RmTree(LLVM_LTO_LLD_DIR)
-    EnsureDirExists(LLVM_LTO_LLD_DIR)
-    os.chdir(LLVM_LTO_LLD_DIR)
-
-    # The linker expects all archive members to have symbol tables, so the
-    # archiver needs to be able to create symbol tables for bitcode files.
-    # GNU ar and ranlib don't understand bitcode files, but llvm-ar and
-    # llvm-ranlib do, so use them.
-    ar = os.path.join(LLVM_BOOTSTRAP_INSTALL_DIR, 'bin', 'llvm-ar')
-    ranlib = os.path.join(LLVM_BOOTSTRAP_INSTALL_DIR, 'bin', 'llvm-ranlib')
-
-    lto_cmake_args = base_cmake_args + [
-        '-DCMAKE_C_COMPILER=' + cc,
-        '-DCMAKE_CXX_COMPILER=' + cxx,
-        '-DCMAKE_AR=' + ar,
-        '-DCMAKE_RANLIB=' + ranlib,
-        '-DLLVM_ENABLE_LTO=thin',
-        '-DLLVM_USE_LINKER=lld',
-        '-DCMAKE_C_FLAGS=' + ' '.join(cflags),
-        '-DCMAKE_CXX_FLAGS=' + ' '.join(cxxflags)]
-
-    RmCmakeCache('.')
-    RunCommand(['cmake'] + lto_cmake_args + [LLVM_DIR])
-    RunCommand(['ninja', 'lld'])
-
-
   # LLVM uses C++11 starting in llvm 3.5. On Linux, this means libstdc++4.7+ is
   # needed, on OS X it requires libc++. clang only automatically links to libc++
   # when targeting OS X 10.9+, so add stdlib=libc++ explicitly so clang can run
@@ -619,7 +588,6 @@ def UpdateClang(args):
     # This makes running package.py over 10% faster (30 min instead of 34 min)
     RmTree(LIBCXX_DIR)
 
-  # Build clang.
 
   # If building at head, define a macro that plugins can use for #ifdefing
   # out code that builds at head, but not at CLANG_REVISION or vice versa.
@@ -635,12 +603,51 @@ def UpdateClang(args):
     cxxflags += ['/Zi', '/GS-']
     ldflags += ['/DEBUG', '/OPT:REF', '/OPT:ICF']
 
-  CreateChromeToolsShim()
-
   deployment_env = None
   if deployment_target:
     deployment_env = os.environ.copy()
     deployment_env['MACOSX_DEPLOYMENT_TARGET'] = deployment_target
+
+  # Build lld. This is done separately from the rest of the build because lld
+  # requires threading support.
+  print 'Building lld'
+  if os.path.exists(LLD_BUILD_DIR):
+    RmTree(LLD_BUILD_DIR)
+  EnsureDirExists(LLD_BUILD_DIR)
+  os.chdir(LLD_BUILD_DIR)
+
+  lld_cmake_args = base_cmake_args + [
+      '-DCMAKE_C_COMPILER=' + cc,
+      '-DCMAKE_CXX_COMPILER=' + cxx,
+      '-DCMAKE_C_FLAGS=' + ' '.join(cflags),
+      '-DCMAKE_CXX_FLAGS=' + ' '.join(cxxflags),
+      '-DCMAKE_EXE_LINKER_FLAGS=' + ' '.join(ldflags),
+      '-DCMAKE_SHARED_LINKER_FLAGS=' + ' '.join(ldflags),
+      '-DCMAKE_MODULE_LINKER_FLAGS=' + ' '.join(ldflags)]
+
+  if args.lto_lld:
+    # Build lld with LTO. That speeds up the linker by ~10%.
+    # We only use LTO for Linux now.
+    #
+    # The linker expects all archive members to have symbol tables, so the
+    # archiver needs to be able to create symbol tables for bitcode files.
+    # GNU ar and ranlib don't understand bitcode files, but llvm-ar and
+    # llvm-ranlib do, so use them.
+    ar = os.path.join(LLVM_BOOTSTRAP_INSTALL_DIR, 'bin', 'llvm-ar')
+    ranlib = os.path.join(LLVM_BOOTSTRAP_INSTALL_DIR, 'bin', 'llvm-ranlib')
+    lld_cmake_args += [
+        '-DCMAKE_AR=' + ar,
+        '-DCMAKE_RANLIB=' + ranlib,
+        '-DLLVM_ENABLE_LTO=thin',
+        '-DLLVM_USE_LINKER=lld']
+
+  RmCmakeCache('.')
+  RunCommand(['cmake'] + lld_cmake_args + [LLVM_DIR], msvc_arch='x64',
+             env=deployment_env)
+  RunCommand(['ninja', 'lld'], msvc_arch='x64')
+
+  # Build clang and other tools.
+  CreateChromeToolsShim()
 
   cmake_args = []
   # TODO(thakis): Unconditionally append this to base_cmake_args instead once
@@ -668,9 +675,14 @@ def UpdateClang(args):
              msvc_arch='x64', env=deployment_env)
   RunCommand(['ninja'], msvc_arch='x64')
 
-  # Copy LTO-optimized lld, if any.
-  if args.bootstrap and args.lto_lld:
-    CopyFile(os.path.join(LLVM_LTO_LLD_DIR, 'bin', 'lld'),
+  # Copy in the threaded version of lld.
+  if sys.platform == 'win32':
+    CopyFile(os.path.join(LLD_BUILD_DIR, 'bin', 'lld-link.exe'),
+             os.path.join(LLVM_BUILD_DIR, 'bin'))
+    CopyFile(os.path.join(LLD_BUILD_DIR, 'bin', 'lld.pdb'),
+             os.path.join(LLVM_BUILD_DIR, 'bin'))
+  else:
+    CopyFile(os.path.join(LLD_BUILD_DIR, 'bin', 'lld'),
              os.path.join(LLVM_BUILD_DIR, 'bin'))
 
   if chrome_tools:
