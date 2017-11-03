4c0323924b6a15da8b196d9fd5b0465fd35941a6
Charles Harrison <csharrison@chromium.org>
2017-11-03 16:59:32 +0000

    [subresource_filter] Add metric for total SB check time
    We have some ideas to make this feature a bit more async, to avoid
    thread descheds janking navigation code on the IO thread. This metric
    can help us evaluate that impact.
    
    This CL also adds a histogram for the time it took to dispatch the
    safe browsing check, which could be substantial on Android where we
    are seeing IPC descheduling.
    
    Bug: 779914
    Change-Id: I2cabba3c5979a7ea8008d294d02d1bfbf7004ebb
    Reviewed-on: https://chromium-review.googlesource.com/747441
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513807}
    Former-commit-id: 78ff41eab058af57f7629b6b1beec3628f7ce719

--- components/subresource_filter/content/browser/subresource_filter_safe_browsing_client_request.cc
+++ components/subresource_filter/content/browser/subresource_filter_safe_browsing_client_request.cc
@@ -13,6 +13,7 @@
 #include "base/metrics/histogram_macros.h"
 #include "base/single_thread_task_runner.h"
 #include "components/subresource_filter/content/browser/subresource_filter_safe_browsing_client.h"
+#include "components/subresource_filter/core/common/time_measurements.h"
 #include "content/public/browser/browser_thread.h"
 #include "url/gurl.h"
 
@@ -46,7 +47,11 @@ SubresourceFilterSafeBrowsingClientRequest::
 void SubresourceFilterSafeBrowsingClientRequest::Start(const GURL& url) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
   start_time_ = base::TimeTicks::Now();
-  if (database_manager_->CheckUrlForSubresourceFilter(url, this)) {
+  bool synchronous_finish =
+      database_manager_->CheckUrlForSubresourceFilter(url, this);
+  UMA_HISTOGRAM_MICRO_TIMES("SubresourceFilter.SafeBrowsing.CheckDispatchTime",
+                            (base::TimeTicks::Now() - start_time_));
+  if (synchronous_finish) {
     request_completed_ = true;
     SendCheckResultToClient(false /* served_from_network */,
                             safe_browsing::SB_THREAT_TYPE_SAFE,
