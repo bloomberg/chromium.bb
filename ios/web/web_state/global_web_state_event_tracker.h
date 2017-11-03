dd8718e3cb2eebdcbeaae58803cab88d4f90b318
Sylvain Defresne <sdefresne@chromium.org>
2017-11-03 12:43:23 +0000

    Allow WebStateObserver to observe N WebStates [5/N].
    Convert GlobalWebStateEventTracker to directly observe the
    WebStates instead of using a separate WebStateObserver for
    each WebState.
    
    Bug: 775684
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ibccd2b28aa6394e32138629d132cd4075af0155e
    Reviewed-on: https://chromium-review.googlesource.com/738918
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513753}
    Former-commit-id: 0f50f26f8d1969ae6b21c2b2951cc2a3e9e45b27

--- ios/web/web_state/global_web_state_event_tracker.h
+++ ios/web/web_state/global_web_state_event_tracker.h
@@ -9,7 +9,9 @@
 
 #include "base/macros.h"
 #include "base/observer_list.h"
+#include "base/scoped_observer.h"
 #include "ios/web/public/web_state/global_web_state_observer.h"
+#include "ios/web/public/web_state/web_state_observer.h"
 
 namespace base {
 template <typename T>
@@ -20,7 +22,7 @@ namespace web {
 
 // This singleton serves as the mechanism via which GlobalWebStateObservers get
 // informed of relevant events from all WebState instances.
-class GlobalWebStateEventTracker {
+class GlobalWebStateEventTracker : public WebStateObserver {
  public:
   // Returns the instance of GlobalWebStateEventTracker.
   static GlobalWebStateEventTracker* GetInstance();
@@ -37,20 +39,25 @@ class GlobalWebStateEventTracker {
   // Should be called whenever a WebState instance is created.
   void OnWebStateCreated(WebState* web_state);
 
-  // Forward to the registered observers.
-  void NavigationItemsPruned(WebState* web_state, size_t pruned_item_count);
-  void NavigationItemChanged(WebState* web_state);
-  void NavigationItemCommitted(WebState* web_state,
-                               const LoadCommittedDetails& load_details);
-  void WebStateDidStartLoading(WebState* web_state);
-  void WebStateDidStopLoading(WebState* web_state);
+  // WebStateObserver implementation.
+  void NavigationItemsPruned(WebState* web_state,
+                             size_t pruned_item_count) override;
+  void NavigationItemChanged(WebState* web_state) override;
+  void NavigationItemCommitted(
+      WebState* web_state,
+      const LoadCommittedDetails& load_details) override;
+  void DidStartLoading(WebState* web_state) override;
+  void DidStopLoading(WebState* web_state) override;
   void PageLoaded(WebState* web_state,
-                  PageLoadCompletionStatus load_completion_status);
-  void RenderProcessGone(WebState* web_state);
-  void WebStateDestroyed(WebState* web_state);
+                  PageLoadCompletionStatus load_completion_status) override;
+  void RenderProcessGone(WebState* web_state) override;
+  void WebStateDestroyed(WebState* web_state) override;
 
   GlobalWebStateEventTracker();
-  ~GlobalWebStateEventTracker();
+  ~GlobalWebStateEventTracker() override;
+
+  // ScopedObserver used to track registration with WebState.
+  ScopedObserver<WebState, WebStateObserver> scoped_observer_;
 
   // List of observers currently registered with the tracker.
   base::ObserverList<GlobalWebStateObserver, true> observer_list_;
