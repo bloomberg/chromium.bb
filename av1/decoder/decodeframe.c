b44deca918c7d8207dca622f3a54645ef26e40d3
David Barker <david.barker@argondesign.com>
2017-11-03 17:06:10 +0000

    Fix bug introduced by commit 0156308
    When fixing one bug in av1_decode_tg_tiles_and_wrapup, I seem to have
    introduced another bug. This was due to checking the wrong condition
    on whether to update the frame context at the end of the frame.
    
    BUG=aomedia:1001
    
    Change-Id: I929a710e2de31a89cc7899fb1605ca7edf968a87

--- av1/decoder/decodeframe.c
+++ av1/decoder/decodeframe.c
@@ -3887,7 +3887,8 @@ void av1_decode_tg_tiles_and_wrapup(AV1Decoder *pbi, const uint8_t *data,
 #endif
 
   // Non frame parallel update frame context here.
-  if (cm->refresh_frame_context != REFRESH_FRAME_CONTEXT_FORWARD) {
+  if (!cm->frame_parallel_decode ||
+      cm->refresh_frame_context != REFRESH_FRAME_CONTEXT_FORWARD) {
 #if CONFIG_NO_FRAME_CONTEXT_SIGNALING
     cm->frame_contexts[cm->new_fb_idx] = *cm->fc;
 #else
