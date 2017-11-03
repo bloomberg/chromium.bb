01588c5801c2bedee1332b4bd2b058d2f176aa1c
junweifu <junwei.fu@intel.com>
2017-11-03 06:37:09 +0000

    Move Storage functions to new file winrt_storage_util
    Windows::Streams::Storage is definitely outside of Core WinRT,
    core_winrt_util.h is intended for Core WinRT Functions, so move
    to separate file.
    
    BUG=767021
    
    Change-Id: If42203f7e3a55f1f4d35e955662e682ac37eeceb
    Reviewed-on: https://chromium-review.googlesource.com/729306
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Junwei Fu <junwei.fu@intel.com>
    Cr-Commit-Position: refs/heads/master@{#513713}
    Former-commit-id: b7c153bd16db1cffae948a57315df053531a530a

--- base/win/core_winrt_util_unittest.cc
+++ base/win/core_winrt_util_unittest.cc
@@ -4,22 +4,13 @@
 
 #include "base/win/core_winrt_util.h"
 
-#include <string.h>
-#include <wrl/client.h>
-
-#include "base/strings/string_util.h"
 #include "base/win/com_init_util.h"
 #include "base/win/scoped_com_initializer.h"
-#include "base/win/scoped_hstring.h"
 #include "base/win/windows_version.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace base {
 namespace win {
-
-namespace {
-const std::vector<uint8_t> KTestBufferData = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-}
 
 TEST(CoreWinrtUtilTest, PreloadFunctions) {
   if (GetVersion() < VERSION_WIN8)
@@ -37,28 +28,6 @@ TEST(CoreWinrtUtilTest, RoInitializeAndUninitialize) {
   AssertComApartmentType(ComApartmentType::MTA);
   base::win::RoUninitialize();
   AssertComApartmentType(ComApartmentType::NONE);
-}
-
-TEST(CoreWinrtUtilTest, CreateBufferFromData) {
-  ScopedCOMInitializer com_initializer(ScopedCOMInitializer::kMTA);
-
-  if (GetVersion() < VERSION_WIN8) {
-    EXPECT_FALSE(ScopedHString::ResolveCoreWinRTStringDelayload());
-    return;
-  }
-
-  EXPECT_TRUE(ScopedHString::ResolveCoreWinRTStringDelayload());
-
-  Microsoft::WRL::ComPtr<ABI::Windows::Storage::Streams::IBuffer> buffer;
-  ASSERT_HRESULT_SUCCEEDED(CreateIBufferFromData(
-      KTestBufferData.data(), KTestBufferData.size(), buffer.GetAddressOf()));
-  ASSERT_TRUE(buffer);
-
-  uint8_t* p_buffer_data;
-  ASSERT_HRESULT_SUCCEEDED(
-      GetPointerToBufferData(buffer.Get(), &p_buffer_data));
-  EXPECT_EQ(
-      0, memcmp(p_buffer_data, KTestBufferData.data(), KTestBufferData.size()));
 }
 
 }  // namespace win
