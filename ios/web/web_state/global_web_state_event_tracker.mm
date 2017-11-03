8fc251bef43c3cbf95a025d9b99b13b8d6a6047e
Sylvain Defresne <sdefresne@chromium.org>
2017-11-03 12:43:23 +0000

    Allow WebStateObserver to observe N WebStates [5/N].
    Convert GlobalWebStateEventTracker to directly observe the
    WebStates instead of using a separate WebStateObserver for
    each WebState.
    
    Bug: 775684
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ibccd2b28aa6394e32138629d132cd4075af0155e
    Reviewed-on: https://chromium-review.googlesource.com/738918
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#513753}
    Former-commit-id: 0f50f26f8d1969ae6b21c2b2951cc2a3e9e45b27

--- ios/web/web_state/global_web_state_event_tracker.mm
+++ ios/web/web_state/global_web_state_event_tracker.mm
@@ -8,7 +8,6 @@
 
 #include "base/macros.h"
 #include "base/memory/singleton.h"
-#include "ios/web/public/web_state/web_state_observer.h"
 #import "ios/web/public/web_state/web_state_user_data.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -17,96 +16,17 @@
 
 namespace web {
 
-class WebStateEventForwarder;
-DEFINE_WEB_STATE_USER_DATA_KEY(WebStateEventForwarder);
-
-// Helper class that forwards events from a single associated WebState instance
-// to the GlobalWebStateEventTracker singleton.
-class WebStateEventForwarder : public WebStateUserData<WebStateEventForwarder>,
-                               public WebStateObserver {
- public:
-  ~WebStateEventForwarder() override;
-
- private:
-  explicit WebStateEventForwarder(WebState* web_state);
-  friend class WebStateUserData<WebStateEventForwarder>;
-
-  // WebContentsObserver:
-  void NavigationItemsPruned(WebState* web_state,
-                             size_t pruned_item_count) override;
-  void NavigationItemChanged(WebState* web_state) override;
-  void NavigationItemCommitted(
-      WebState* web_state,
-      const LoadCommittedDetails& load_details) override;
-  void DidStartLoading(WebState* web_state) override;
-  void DidStopLoading(WebState* web_state) override;
-  void PageLoaded(WebState* web_state,
-                  PageLoadCompletionStatus load_completion_status) override;
-  void RenderProcessGone(WebState* web_state) override;
-  void WebStateDestroyed(WebState* web_state) override;
-
-  DISALLOW_COPY_AND_ASSIGN(WebStateEventForwarder);
-};
-
-#pragma mark - WebStateEventForwarder
-
-WebStateEventForwarder::WebStateEventForwarder(web::WebState* web_state)
-    : WebStateObserver(web_state) {}
-
-WebStateEventForwarder::~WebStateEventForwarder() {}
-
-void WebStateEventForwarder::NavigationItemsPruned(WebState* web_state,
-                                                   size_t pruned_item_count) {
-  GlobalWebStateEventTracker::GetInstance()->NavigationItemsPruned(
-      web_state, pruned_item_count);
-}
-
-void WebStateEventForwarder::NavigationItemChanged(WebState* web_state) {
-  GlobalWebStateEventTracker::GetInstance()->NavigationItemChanged(web_state);
-}
-
-void WebStateEventForwarder::NavigationItemCommitted(
-    WebState* web_state,
-    const LoadCommittedDetails& load_details) {
-  GlobalWebStateEventTracker::GetInstance()->NavigationItemCommitted(
-      web_state, load_details);
-}
-
-void WebStateEventForwarder::DidStartLoading(WebState* web_state) {
-  GlobalWebStateEventTracker::GetInstance()->WebStateDidStartLoading(web_state);
-}
-
-void WebStateEventForwarder::DidStopLoading(WebState* web_state) {
-  GlobalWebStateEventTracker::GetInstance()->WebStateDidStopLoading(web_state);
-}
-
-void WebStateEventForwarder::PageLoaded(
-    WebState* web_state,
-    PageLoadCompletionStatus load_completion_status) {
-  GlobalWebStateEventTracker::GetInstance()->PageLoaded(web_state,
-                                                        load_completion_status);
-}
-
-void WebStateEventForwarder::RenderProcessGone(WebState* web_state) {
-  GlobalWebStateEventTracker::GetInstance()->RenderProcessGone(web_state);
-}
-
-void WebStateEventForwarder::WebStateDestroyed(WebState* web_state) {
-  GlobalWebStateEventTracker::GetInstance()->WebStateDestroyed(web_state);
-}
-
-#pragma mark - GlobalWebStateEventTracker
-
 GlobalWebStateEventTracker* GlobalWebStateEventTracker::GetInstance() {
   return base::Singleton<GlobalWebStateEventTracker>::get();
 }
 
-GlobalWebStateEventTracker::GlobalWebStateEventTracker() {}
+GlobalWebStateEventTracker::GlobalWebStateEventTracker()
+    : scoped_observer_(this) {}
 
-GlobalWebStateEventTracker::~GlobalWebStateEventTracker() {}
+GlobalWebStateEventTracker::~GlobalWebStateEventTracker() = default;
 
 void GlobalWebStateEventTracker::OnWebStateCreated(WebState* web_state) {
-  WebStateEventForwarder::CreateForWebState(web_state);
+  scoped_observer_.Add(web_state);
 }
 
 void GlobalWebStateEventTracker::AddObserver(GlobalWebStateObserver* observer) {
@@ -137,12 +57,12 @@ void GlobalWebStateEventTracker::NavigationItemCommitted(
     observer.NavigationItemCommitted(web_state, load_details);
 }
 
-void GlobalWebStateEventTracker::WebStateDidStartLoading(WebState* web_state) {
+void GlobalWebStateEventTracker::DidStartLoading(WebState* web_state) {
   for (auto& observer : observer_list_)
     observer.WebStateDidStartLoading(web_state);
 }
 
-void GlobalWebStateEventTracker::WebStateDidStopLoading(WebState* web_state) {
+void GlobalWebStateEventTracker::DidStopLoading(WebState* web_state) {
   for (auto& observer : observer_list_)
     observer.WebStateDidStopLoading(web_state);
 }
@@ -162,6 +82,7 @@ void GlobalWebStateEventTracker::RenderProcessGone(WebState* web_state) {
 void GlobalWebStateEventTracker::WebStateDestroyed(WebState* web_state) {
   for (auto& observer : observer_list_)
     observer.WebStateDestroyed(web_state);
+  scoped_observer_.Remove(web_state);
 }
 
 }  // namespace web
