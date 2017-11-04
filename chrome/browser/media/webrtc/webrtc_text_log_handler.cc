4e682f002f563af5bd15b06f245b8239e1581d7d
Tommi <tommi@chromium.org>
2017-11-04 13:50:46 +0000

    Checking log_buffer_ and meta_data_ before detaching in WebRtcTextLogHandler. Looks like ReleaseLog might be called more than once or that ReleaseLog might be called when not started.
    BUG=699960
    
    Change-Id: I70714399ee3cefe86046ea72d921cbcd931cbb02
    Reviewed-on: https://chromium-review.googlesource.com/751244
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Tommi <tommi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514048}
    Former-commit-id: d92a5c0ce928d1e9ecc828a36ff95fc9968eb6a7

--- chrome/browser/media/webrtc/webrtc_text_log_handler.cc
+++ chrome/browser/media/webrtc/webrtc_text_log_handler.cc
@@ -301,11 +301,16 @@ void WebRtcTextLogHandler::ReleaseLog(
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
   DCHECK(logging_state_ == STOPPED || logging_state_ == CHANNEL_CLOSING);
 
-  log_buffer_->SetComplete();
-  *log_buffer = std::move(log_buffer_);
-  *meta_data = std::move(meta_data_);
-  log_buffer_.reset();
-  meta_data_.reset();
+  // Checking log_buffer_ here due to seeing some crashes out in the wild.
+  // See crbug/699960 for more details.
+  if (log_buffer_) {
+    log_buffer_->SetComplete();
+    *log_buffer = std::move(log_buffer_);
+  }
+
+  if (meta_data_)
+    *meta_data = std::move(meta_data_);
+
   if (logging_state_ != CHANNEL_CLOSING)
     logging_state_ = LoggingState::CLOSED;
 }
