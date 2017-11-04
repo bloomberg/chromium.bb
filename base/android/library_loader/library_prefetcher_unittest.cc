5e10cde2c058862cff8706b7001d57be998be02e
Peter Collingbourne <pcc@chromium.org>
2017-11-04 02:37:47 +0000

    Reland "Compute base addresses from program headers while reading /proc/self/maps."
    This is a reland of 993ff83be2795775ddf515aab26bf47bc2ee0161 [formerly 6f903f3a228bc8e10cd7bda76342954feb3000c5]
    
    Fixed Android build issue.
    
    Original change's description:
    > Compute base addresses from program headers while reading /proc/self/maps.
    >
    > This cherry picks this glog change:
    > https://github.com/google/glog/pull/261
    > with a reimplementation of the program header reading logic for the
    > sandboxed symbolizer.
    >
    > This should cause unsymbolized stack traces to contain the correct
    > addresses for binaries linked with lld.
    >
    > Bug: 772559
    > Change-Id: Ief9cbb463b3b4c32149da893c89c2eefd76b05d4
    > Reviewed-on: https://chromium-review.googlesource.com/752753
    > Commit-Queue: Peter Collingbourne <pcc@chromium.org>
    > Reviewed-by: Mark Mentovai <mark@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#513956}
    
    Bug: 772559
    Change-Id: Id6a8c62c0b05a7d12f817e389c7b96223fec8073
    Reviewed-on: https://chromium-review.googlesource.com/754232
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Peter Collingbourne <pcc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514018}
    Former-commit-id: 1a3ed318cbbe2b53d9d6410a376e75439b9e6007

--- base/android/library_loader/library_prefetcher_unittest.cc
+++ base/android/library_loader/library_prefetcher_unittest.cc
@@ -27,12 +27,13 @@ const uint8_t kExecutePrivate = base::debug::MappedMemoryRegion::EXECUTE |
 
 TEST(NativeLibraryPrefetcherTest, TestIsGoodToPrefetchNoRange) {
   const base::debug::MappedMemoryRegion regions[4] = {
-      base::debug::MappedMemoryRegion{0x4000, 0x5000, 10, kReadPrivate, ""},
-      base::debug::MappedMemoryRegion{0x4000, 0x5000, 10, kReadPrivate, "foo"},
-      base::debug::MappedMemoryRegion{
-          0x4000, 0x5000, 10, kReadPrivate, "foobar.apk"},
-      base::debug::MappedMemoryRegion{
-          0x4000, 0x5000, 10, kReadPrivate, "libchromium.so"}};
+      base::debug::MappedMemoryRegion{0x4000, 0x5000, 10, 0, kReadPrivate, ""},
+      base::debug::MappedMemoryRegion{0x4000, 0x5000, 10, 0, kReadPrivate,
+                                      "foo"},
+      base::debug::MappedMemoryRegion{0x4000, 0x5000, 10, 0, kReadPrivate,
+                                      "foobar.apk"},
+      base::debug::MappedMemoryRegion{0x4000, 0x5000, 10, 0, kReadPrivate,
+                                      "libchromium.so"}};
   for (int i = 0; i < 4; ++i) {
     ASSERT_FALSE(NativeLibraryPrefetcher::IsGoodToPrefetch(regions[i]));
   }
@@ -40,35 +41,35 @@ TEST(NativeLibraryPrefetcherTest, TestIsGoodToPrefetchNoRange) {
 
 TEST(NativeLibraryPrefetcherTest, TestIsGoodToPrefetchUnreadableRange) {
   const base::debug::MappedMemoryRegion region = {
-      0x4000, 0x5000, 10, kExecutePrivate, "base.apk"};
+      0x4000, 0x5000, 10, 0, kExecutePrivate, "base.apk"};
   ASSERT_FALSE(NativeLibraryPrefetcher::IsGoodToPrefetch(region));
 }
 
 TEST(NativeLibraryPrefetcherTest, TestIsGoodToPrefetchSkipSharedRange) {
   const base::debug::MappedMemoryRegion region = {
-      0x4000, 0x5000, 10, kRead, "base.apk"};
+      0x4000, 0x5000, 10, 0, kRead, "base.apk"};
   ASSERT_FALSE(NativeLibraryPrefetcher::IsGoodToPrefetch(region));
 }
 
 TEST(NativeLibraryPrefetcherTest, TestIsGoodToPrefetchLibchromeRange) {
   const base::debug::MappedMemoryRegion region = {
-      0x4000, 0x5000, 10, kReadPrivate, "libchrome.so"};
+      0x4000, 0x5000, 10, 0, kReadPrivate, "libchrome.so"};
   ASSERT_TRUE(NativeLibraryPrefetcher::IsGoodToPrefetch(region));
 }
 
 TEST(NativeLibraryPrefetcherTest, TestIsGoodToPrefetchBaseApkRange) {
   const base::debug::MappedMemoryRegion region = {
-      0x4000, 0x5000, 10, kReadPrivate, "base.apk"};
+      0x4000, 0x5000, 10, 0, kReadPrivate, "base.apk"};
   ASSERT_TRUE(NativeLibraryPrefetcher::IsGoodToPrefetch(region));
 }
 
 TEST(NativeLibraryPrefetcherTest,
      TestFilterLibchromeRangesOnlyIfPossibleNoLibchrome) {
   std::vector<base::debug::MappedMemoryRegion> regions;
-  regions.push_back(
-      base::debug::MappedMemoryRegion{0x1, 0x2, 0, kReadPrivate, "base.apk"});
-  regions.push_back(
-      base::debug::MappedMemoryRegion{0x3, 0x4, 0, kReadPrivate, "base.apk"});
+  regions.push_back(base::debug::MappedMemoryRegion{0x1, 0x2, 0, 0,
+                                                    kReadPrivate, "base.apk"});
+  regions.push_back(base::debug::MappedMemoryRegion{0x3, 0x4, 0, 0,
+                                                    kReadPrivate, "base.apk"});
   std::vector<NativeLibraryPrefetcher::AddressRange> ranges;
   NativeLibraryPrefetcher::FilterLibchromeRangesOnlyIfPossible(regions,
                                                                &ranges);
@@ -82,12 +83,12 @@ TEST(NativeLibraryPrefetcherTest,
 TEST(NativeLibraryPrefetcherTest,
      TestFilterLibchromeRangesOnlyIfPossibleHasLibchrome) {
   std::vector<base::debug::MappedMemoryRegion> regions;
-  regions.push_back(
-      base::debug::MappedMemoryRegion{0x1, 0x2, 0, kReadPrivate, "base.apk"});
+  regions.push_back(base::debug::MappedMemoryRegion{0x1, 0x2, 0, 0,
+                                                    kReadPrivate, "base.apk"});
   regions.push_back(base::debug::MappedMemoryRegion{
-      0x6, 0x7, 0, kReadPrivate, "libchrome.so"});
-  regions.push_back(
-      base::debug::MappedMemoryRegion{0x3, 0x4, 0, kReadPrivate, "base.apk"});
+      0x6, 0x7, 0, 0, kReadPrivate, "libchrome.so"});
+  regions.push_back(base::debug::MappedMemoryRegion{0x3, 0x4, 0, 0,
+                                                    kReadPrivate, "base.apk"});
   std::vector<NativeLibraryPrefetcher::AddressRange> ranges;
   NativeLibraryPrefetcher::FilterLibchromeRangesOnlyIfPossible(regions,
                                                                &ranges);
