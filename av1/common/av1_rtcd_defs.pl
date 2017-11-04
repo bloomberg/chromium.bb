wjdz99/aom

Use rtcd script to choose between implementations
Change-Id: I752ad96a8b4349d4a437a97e30edc8e4c22f81b5

av1/common/av1_rtcd_defs.pl
@@ -64,7 +64,9 @@ if (aom_config("CONFIG_HIGHBITDEPTH") eq "yes") {
 #
 if (aom_config("CONFIG_HIGHBITDEPTH") eq "yes") {
   add_proto qw/void av1_iht4x4_16_add/, "const tran_low_t *input, uint8_t *dest, int dest_stride, const struct txfm_param *param";
-  specialize qw/av1_iht4x4_16_add sse2/;
+  if (aom_config("CONFIG_LGT") ne "yes" && aom_config("CONFIG_DAALA_DCT4") ne "yes") {
+    specialize qw/av1_iht4x4_16_add sse2/;
+  }
 
   add_proto qw/void av1_iht4x8_32_add/, "const tran_low_t *input, uint8_t *dest, int dest_stride, const struct txfm_param *param";
   specialize qw/av1_iht4x8_32_add sse2/;
@@ -93,15 +95,19 @@ if (aom_config("CONFIG_HIGHBITDEPTH") eq "yes") {
   add_proto qw/void av1_iht32x8_256_add/, "const tran_low_t *input, uint8_t *dest, int dest_stride, const struct txfm_param *param";
 
   add_proto qw/void av1_iht8x8_64_add/, "const tran_low_t *input, uint8_t *dest, int dest_stride, const struct txfm_param *param";
-  specialize qw/av1_iht8x8_64_add sse2/;
+  if (aom_config("CONFIG_LGT") ne "yes" && aom_config("CONFIG_DAALA_DCT8") ne "yes") {
+    specialize qw/av1_iht8x8_64_add sse2/;
+  }
 
   add_proto qw/void av1_iht16x16_256_add/, "const tran_low_t *input, uint8_t *output, int pitch, const struct txfm_param *param";
-  specialize qw/av1_iht16x16_256_add sse2 avx2/;
+  if (aom_config("CONFIG_DAALA_DCT16") ne "yes") {
+    specialize qw/av1_iht16x16_256_add sse2 avx2/;
+  }
 
   add_proto qw/void av1_iht32x32_1024_add/, "const tran_low_t *input, uint8_t *output, int pitch, const struct txfm_param *param";
 } else {
   add_proto qw/void av1_iht4x4_16_add/, "const tran_low_t *input, uint8_t *dest, int dest_stride, const struct txfm_param *param";
-  if (aom_config("CONFIG_DAALA_DCT4") ne "yes") {
+  if (aom_config("CONFIG_LGT") ne "yes" && aom_config("CONFIG_DAALA_DCT4") ne "yes") {
     specialize qw/av1_iht4x4_16_add sse2 neon/;
   }
 
@@ -132,7 +138,7 @@ if (aom_config("CONFIG_HIGHBITDEPTH") eq "yes") {
   add_proto qw/void av1_iht32x8_256_add/, "const tran_low_t *input, uint8_t *dest, int dest_stride, const struct txfm_param *param";
 
   add_proto qw/void av1_iht8x8_64_add/, "const tran_low_t *input, uint8_t *dest, int dest_stride, const struct txfm_param *param";
-  if (aom_config("CONFIG_DAALA_DCT8") ne "yes") {
+  if (aom_config("CONFIG_LGT") ne "yes" && aom_config("CONFIG_DAALA_DCT8") ne "yes") {
     specialize qw/av1_iht8x8_64_add sse2 neon/;
   }
 

bloomberg/chromium.bb

av1/common/av1_rtcd_defs.pl
@@ -70,10 +70,10 @@ if (aom_config("CONFIG_HIGHBITDEPTH") eq "yes") {
 # Inverse dct
 #
 add_proto qw/void av1_iht4x4_16_add/, "const tran_low_t *input, uint8_t *dest, int dest_stride, const struct txfm_param *param";
-if (aom_config("CONFIG_HIGHBITDEPTH") eq "yes") {
-  specialize qw/av1_iht4x4_16_add sse2/;
-} else {
-  if (aom_config("CONFIG_DAALA_TX4") ne "yes") {
+if (aom_config("CONFIG_DAALA_TX4") ne "yes" && aom_config("CONFIG_LGT") ne "yes") {
+  if (aom_config("CONFIG_HIGHBITDEPTH") eq "yes") {
+    specialize qw/av1_iht4x4_16_add sse2/;
+  } else {
     specialize qw/av1_iht4x4_16_add sse2 neon/;
   }
 }
@@ -105,19 +105,20 @@ add_proto qw/void av1_iht8x32_256_add/, "const tran_low_t *input, uint8_t *dest,
 add_proto qw/void av1_iht32x8_256_add/, "const tran_low_t *input, uint8_t *dest, int dest_stride, const struct txfm_param *param";
 
 add_proto qw/void av1_iht8x8_64_add/, "const tran_low_t *input, uint8_t *dest, int dest_stride, const struct txfm_param *param";
-if (aom_config("CONFIG_HIGHBITDEPTH") eq "yes") {
-  specialize qw/av1_iht8x8_64_add sse2/;
-} else {
-  if (aom_config("CONFIG_DAALA_TX8") ne "yes") {
+if (aom_config("CONFIG_DAALA_TX8") ne "yes" && aom_config("CONFIG_LGT") ne "yes") {
+  if (aom_config("CONFIG_HIGHBITDEPTH") eq "yes") {
+    specialize qw/av1_iht8x8_64_add sse2/;
+  } else {
     specialize qw/av1_iht8x8_64_add sse2 neon/;
   }
 }
 
 add_proto qw/void av1_iht16x16_256_add/, "const tran_low_t *input, uint8_t *output, int pitch, const struct txfm_param *param";
-if (aom_config("CONFIG_HIGHBITDEPTH") eq "yes") {
-  specialize qw/av1_iht16x16_256_add sse2 avx2/;
-} else {
-  if (aom_config("CONFIG_DAALA_TX16") ne "yes") {
+
+if (aom_config("CONFIG_DAALA_TX16") ne "yes" && aom_config("CONFIG_LGT") ne "yes") {
+  if (aom_config("CONFIG_HIGHBITDEPTH") eq "yes") {
+    specialize qw/av1_iht16x16_256_add sse2 avx2/;
+  } else {
     specialize qw/av1_iht16x16_256_add sse2 avx2/;
   }
 }