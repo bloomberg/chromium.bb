9d7e25dc38510c0883c143f423a50c0e453e96af
Min Qin <qinmin@chromium.org>
2017-11-04 04:50:25 +0000

    Only allow googlechrome protocol for http(s) urls
    The protocol should only be used to navigate to http(s) urls, no content or
    file uri.
    
    BUG=780708
    
    Change-Id: I00ea7e0fd31ec9e379ddcb5126265a6744de822d
    Reviewed-on: https://chromium-review.googlesource.com/752202
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514030}
    Former-commit-id: e29f7184a20c69e484adfd115b7dcdc4f774ef5b

--- chrome/android/javatests/src/org/chromium/chrome/browser/IntentHandlerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/IntentHandlerTest.java
@@ -49,22 +49,10 @@ public class IntentHandlerTest {
     private static final String VOICE_URL_QUERY = "www.google.com";
     private static final String VOICE_URL_QUERY_URL = "INVALID_URLZ";
 
-    private static final String[] ACCEPTED_INTENT_URLS = {"http://www.google.com",
-            "http://movies.nytimes.com/movie/review?"
-                    + "res=9405EFDB1E3BE23BBC4153DFB7678382659EDE&partner=Rotten Tomatoes",
-            "chrome://newtab", "file://foo.txt", "ftp://www.foo.com", "https://www.gmail.com", "",
-            "http://www.example.com/\u00FCmlat.html&q=name", "http://www.example.com/quotation_\"",
-            "http://www.example.com/lessthansymbol_<", "http://www.example.com/greaterthansymbol_>",
-            "http://www.example.com/poundcharacter_#", "http://www.example.com/percentcharacter_%",
-            "http://www.example.com/leftcurlybrace_{", "http://www.example.com/rightcurlybrace_}",
-            "http://www.example.com/verticalpipe_|", "http://www.example.com/backslash_\\",
-            "http://www.example.com/caret_^", "http://www.example.com/tilde_~",
-            "http://www.example.com/leftsquarebracket_[",
-            "http://www.example.com/rightsquarebracket_]", "http://www.example.com/graveaccent_`",
-            "www.example.com", "www.google.com", "www.bing.com", "notreallyaurl",
-            "://javascript:80/hello", "https:awesome@google.com/haha.gif",
+    private static final String[] ACCEPTED_NON_HTTP_AND_HTTPS_URLS = {"chrome://newtab",
+            "file://foo.txt", "ftp://www.foo.com", "", "://javascript:80/hello",
             "ftp@https://confusing:@something.example:5/goat?sayit", "://www.google.com/",
-            "//www.google.com", "chrome-search://food",
+            "chrome-search://food",
             "java-scr\nipt://alert", // - is significant
             "java.scr\nipt://alert", // . is significant
             "java+scr\nipt://alert", // + is significant
@@ -79,10 +67,26 @@ public class IntentHandlerTest {
             "javascript:http//bar.net:javascript/yes.no", " javascript:://window.open(1)",
             " java script:alert(1)", "~~~javascript://alert"};
 
+    private static final String[] VALID_HTTP_AND_HTTPS_URLS = {"http://www.google.com",
+            "http://movies.nytimes.com/movie/review?"
+                    + "res=9405EFDB1E3BE23BBC4153DFB7678382659EDE&partner=Rotten Tomatoes",
+            "https://www.gmail.com", "http://www.example.com/\u00FCmlat.html&q=name",
+            "http://www.example.com/quotation_\"", "http://www.example.com/lessthansymbol_<",
+            "http://www.example.com/greaterthansymbol_>", "http://www.example.com/poundcharacter_#",
+            "http://www.example.com/percentcharacter_%", "http://www.example.com/leftcurlybrace_{",
+            "http://www.example.com/rightcurlybrace_}", "http://www.example.com/verticalpipe_|",
+            "http://www.example.com/backslash_\\", "http://www.example.com/caret_^",
+            "http://www.example.com/tilde_~", "http://www.example.com/leftsquarebracket_[",
+            "http://www.example.com/rightsquarebracket_]", "http://www.example.com/graveaccent_`",
+            "www.example.com", "www.google.com", "www.bing.com", "notreallyaurl",
+            "https:awesome@google.com/haha.gif", "//www.google.com"};
+
     private static final String[] REJECTED_GOOGLECHROME_URLS = {
             IntentHandler.GOOGLECHROME_SCHEME + "://reddit.com",
             IntentHandler.GOOGLECHROME_SCHEME + "://navigate?reddit.com",
             IntentHandler.GOOGLECHROME_SCHEME + "://navigate?urlreddit.com",
+            IntentHandler.GOOGLECHROME_SCHEME
+                    + "://navigate?url=content://com.android.chrome.FileProvider",
     };
 
     private static final String GOOGLE_URL = "https://www.google.com";
@@ -113,7 +117,8 @@ public class IntentHandlerTest {
     @SmallTest
     @Feature({"Android-AppBase"})
     public void testAcceptedUrls() {
-        processUrls(ACCEPTED_INTENT_URLS, true);
+        processUrls(ACCEPTED_NON_HTTP_AND_HTTPS_URLS, true);
+        processUrls(VALID_HTTP_AND_HTTPS_URLS, true);
     }
 
     @Test
@@ -127,11 +132,10 @@ public class IntentHandlerTest {
     @SmallTest
     @Feature({"Android-AppBase"})
     public void testAcceptedGoogleChromeSchemeNavigateUrls() {
-        // Test all of the accepted URLs after prepending googlechrome://navigate?url.
-        String[] expectedAccepts = new String[ACCEPTED_INTENT_URLS.length];
-        for (int i = 0; i < ACCEPTED_INTENT_URLS.length; ++i) {
+        String[] expectedAccepts = new String[VALID_HTTP_AND_HTTPS_URLS.length];
+        for (int i = 0; i < VALID_HTTP_AND_HTTPS_URLS.length; ++i) {
             expectedAccepts[i] =
-                    IntentHandler.GOOGLECHROME_NAVIGATE_PREFIX + ACCEPTED_INTENT_URLS[i];
+                    IntentHandler.GOOGLECHROME_NAVIGATE_PREFIX + VALID_HTTP_AND_HTTPS_URLS[i];
         }
         processUrls(expectedAccepts, true);
     }
