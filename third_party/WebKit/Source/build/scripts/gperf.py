e4bd77302ab47e4dab55af12cbf32d2ca1cf63e4
Eddy Mead <meade@chromium.org>
2017-11-06 08:39:07 +0000

    Move jinja-gperf template expansion into gperf.py
    Bug: 752745
    Change-Id: Ide15412e8bb8397ca40e3b238c6eec56abb84c1a
    Reviewed-on: https://chromium-review.googlesource.com/750483
    Commit-Queue: meade_UTC10 <meade@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514099}
    Former-commit-id: 8c187aca591375ac9e0b795d497b3f2ccfe2b6cf

--- third_party/WebKit/Source/build/scripts/gperf.py
+++ third_party/WebKit/Source/build/scripts/gperf.py
@@ -11,6 +11,45 @@ import argparse
 import os
 import subprocess
 import sys
+import template_expander
+
+
+def generate_gperf(gperf_path, gperf_input, extra_args=None):
+    # FIXME: If we could depend on Python 2.7, we would use
+    # subprocess.check_output
+    gperf_args = [gperf_path, '--key-positions=*', '-P', '-n']
+    gperf_args.extend(['-m', '50'])  # Pick best of 50 attempts.
+    gperf_args.append('-D')  # Allow duplicate hashes -> More compact code.
+    if extra_args:
+        gperf_args.extend(extra_args)
+
+    # If gperf isn't in the path we get an OSError. We don't want to use
+    # the normal solution of shell=True (as this has to run on many
+    # platforms), so instead we catch the error and raise a
+    # CalledProcessError like subprocess would do when shell=True is set.
+    try:
+        gperf = subprocess.Popen(
+            gperf_args,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            universal_newlines=True)
+        return gperf.communicate(gperf_input)[0]
+    except OSError:
+        raise subprocess.CalledProcessError(
+            127, gperf_args, output='Command not found.')
+
+
+def use_jinja_gperf_template(template_path, gperf_extra_args=None):
+    def wrapper(generator):
+        def generator_internal(*args, **kwargs):
+            parameters = generator(*args, **kwargs)
+            gperf_path = parameters['gperf_path']
+            gperf_input = template_expander.apply_template(template_path,
+                                                           parameters)
+            return generate_gperf(gperf_path, gperf_input, gperf_extra_args)
+        generator_internal.func_name = generator.func_name
+        return generator_internal
+    return wrapper
 
 
 def main():
