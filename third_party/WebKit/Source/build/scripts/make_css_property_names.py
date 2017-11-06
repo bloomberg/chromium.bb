d298969dcdd6e733a7ad4462f9a9ec2355a88607
Eddy Mead <meade@chromium.org>
2017-11-06 05:44:57 +0000

    Move CSSPropertyNames.cpp template into a jinja template file
    This will help us eventually move gperf code into a common location.
    
    Diff between string template and jinja template:
    https://www.diffchecker.com/086dDizU
    
    Bug: 752745
    Change-Id: I2b278ff4be26331ab074eee294a92a1635ed7bc6
    Reviewed-on: https://chromium-review.googlesource.com/750441
    Commit-Queue: meade_UTC10 <meade@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514092}
    Former-commit-id: fbc8ea4b69a6bb6c1ae8c1169130d31ee1a970d2

--- third_party/WebKit/Source/build/scripts/make_css_property_names.py
+++ third_party/WebKit/Source/build/scripts/make_css_property_names.py
@@ -6,122 +6,6 @@ import sys
 from core.css import css_properties
 import json5_generator
 import template_expander
-import license
-
-
-GPERF_TEMPLATE = """
-%%{
-%(license)s
-
-#include "%(class_name)s.h"
-#include "core/css/HashTools.h"
-#include <string.h>
-
-#include "platform/wtf/ASCIICType.h"
-#include "platform/wtf/text/AtomicString.h"
-#include "platform/wtf/text/WTFString.h"
-
-#ifdef _MSC_VER
-// Disable the warnings from casting a 64-bit pointer to 32-bit long
-// warning C4302: 'type cast': truncation from 'char (*)[28]' to 'long'
-// warning C4311: 'type cast': pointer truncation from 'char (*)[18]' to 'long'
-#pragma warning(disable : 4302 4311)
-#endif
-
-#if defined(__clang__)
-#pragma clang diagnostic push
-// TODO(thakis): Remove once we use a gperf that no longer produces "register".
-#pragma clang diagnostic ignored "-Wdeprecated-register"
-#endif
-
-namespace blink {
-static const char propertyNameStringsPool[] = {
-%(property_name_strings)s
-};
-
-static const unsigned short propertyNameStringsOffsets[] = {
-%(property_name_offsets)s
-};
-
-%%}
-%%struct-type
-struct Property;
-%%omit-struct-type
-%%language=C++
-%%readonly-tables
-%%global-table
-%%compare-strncmp
-%%define class-name %(class_name)sHash
-%%define lookup-function-name findPropertyImpl
-%%define hash-function-name property_hash_function
-%%define slot-name name_offset
-%%define word-array-name property_word_list
-%%enum
-%%%%
-%(property_to_enum_map)s
-%%%%
-
-#if defined(__clang__)
-#pragma clang diagnostic pop
-#endif
-
-const Property* FindProperty(const char* str, unsigned int len) {
-  return %(class_name)sHash::findPropertyImpl(str, len);
-}
-
-const char* getPropertyName(CSSPropertyID id) {
-  DCHECK(isCSSPropertyIDWithName(id));
-  int index = id - firstCSSProperty;
-  return propertyNameStringsPool + propertyNameStringsOffsets[index];
-}
-
-const AtomicString& getPropertyNameAtomicString(CSSPropertyID id) {
-  DCHECK(isCSSPropertyIDWithName(id));
-  int index = id - firstCSSProperty;
-  static AtomicString* propertyStrings =
-      new AtomicString[lastUnresolvedCSSProperty]; // Leaked.
-  AtomicString& propertyString = propertyStrings[index];
-  if (propertyString.IsNull()) {
-    propertyString = AtomicString(propertyNameStringsPool +
-                     propertyNameStringsOffsets[index]);
-  }
-  return propertyString;
-}
-
-String getPropertyNameString(CSSPropertyID id) {
-  // We share the StringImpl with the AtomicStrings.
-  return getPropertyNameAtomicString(id).GetString();
-}
-
-String getJSPropertyName(CSSPropertyID id) {
-  char result[maxCSSPropertyNameLength + 1];
-  const char* cssPropertyName = getPropertyName(id);
-  const char* propertyNamePointer = cssPropertyName;
-  if (!propertyNamePointer)
-    return g_empty_string;
-
-  char* resultPointer = result;
-  while (char character = *propertyNamePointer++) {
-    if (character == '-') {
-      char nextCharacter = *propertyNamePointer++;
-      if (!nextCharacter)
-        break;
-      character = (propertyNamePointer - 2 != cssPropertyName)
-                      ? ToASCIIUpper(nextCharacter) : nextCharacter;
-    }
-    *resultPointer++ = character;
-  }
-  *resultPointer = '\\0';
-  return String(result);
-}
-
-CSSPropertyID cssPropertyID(const String& string)
-{
-    return resolveCSSPropertyID(unresolvedCSSPropertyID(string));
-}
-
-} // namespace blink
-"""
 
 
 class CSSPropertyNamesWriter(json5_generator.Writer):
@@ -139,7 +23,8 @@ class CSSPropertyNamesWriter(json5_generator.Writer):
         return "    %(property_id)s = %(enum_value)s," % property_
 
     def _array_item(self, property_):
-        return "    static_cast<CSSPropertyID>(%(enum_value)s), // %(property_id)s" % property_
+        return "    static_cast<CSSPropertyID>(%(enum_value)s), " \
+            "// %(property_id)s" % property_
 
     @template_expander.use_jinja('templates/CSSPropertyNames.h.tmpl')
     def generate_header(self):
@@ -180,14 +65,19 @@ class CSSPropertyNamesWriter(json5_generator.Writer):
             (property_['name'], property_['property_id'])
             for property_ in self._css_properties.properties_including_aliases]
 
-        gperf_input = GPERF_TEMPLATE % {
-            'license': license.license_for_generated_cpp(),
-            'class_name': self.class_name,
-            'property_name_strings': '\n'.join('    "%s\\0"' % name for name in property_names),
-            'property_name_offsets': '\n'.join('    %d,' % offset for offset in property_offsets),
-            'property_to_enum_map': '\n'.join('%s, %s' % property for property in css_name_and_enum_pairs),
+        template_params = {
+            'class_name': 'CSSPropertyNames',
+            'property_names': property_names,
+            'property_offsets': property_offsets,
+            'property_to_enum_map':
+                '\n'.join('%s, %s' % property_
+                          for property_ in css_name_and_enum_pairs),
         }
-        # FIXME: If we could depend on Python 2.7, we would use subprocess.check_output
+        gperf_input = template_expander.apply_template(
+            'templates/CSSPropertyNames.cpp.tmpl', template_params)
+
+        # FIXME: If we could depend on Python 2.7, we would use
+        # subprocess.check_output
         gperf_args = [self.gperf_path, '--key-positions=*', '-P', '-n']
         gperf_args.extend(['-m', '50'])  # Pick best of 50 attempts.
         gperf_args.append('-D')  # Allow duplicate hashes -> More compact code.
@@ -198,10 +88,15 @@ class CSSPropertyNamesWriter(json5_generator.Writer):
         # platforms), so instead we catch the error and raise a
         # CalledProcessError like subprocess would do when shell=True is set.
         try:
-            gperf = subprocess.Popen(gperf_args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
+            gperf = subprocess.Popen(
+                gperf_args,
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                universal_newlines=True)
             return gperf.communicate(gperf_input)[0]
         except OSError:
-            raise subprocess.CalledProcessError(127, gperf_args, output='Command not found.')
+            raise subprocess.CalledProcessError(
+                127, gperf_args, output='Command not found.')
 
 
 if __name__ == "__main__":
