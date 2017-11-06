63b8591c8f4e7ff07b285eea23aaa411b516b459
Ian Vollick <vollick@chromium.org>
2017-11-06 15:37:55 +0000

    [vr] Ensure that hit testing uses the same stacking order as rendering
    The rendering code was updated to sort elements in tree rather than
    depth order in this cl:
    https://chromium-review.googlesource.com/c/chromium/src/+/727342
    
    The hit testing code, however, was not similarly updated. Consequently,
    there can be discrepancies. This CL updates the hit testing code to
    use nearly the same stacking order as rendering. Note: it is not quite
    perfect because the ui renderer still summons swaths of elements from
    the scene explicitly. Eventually, the ui renderer will simply sort
    and render all elements in the tree and these will be truly identical.
    However, until then, this ensures we do use the same element sorting
    routines.
    
    Bug: None
    Change-Id: I12483713f19b678aa9a69b91e5d910e408b73007
    Reviewed-on: https://chromium-review.googlesource.com/754554
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514136}
    Former-commit-id: 053d939f842b119b0b0abeace21305440d96abc5

--- chrome/browser/vr/ui_input_manager.cc
+++ chrome/browser/vr/ui_input_manager.cc
@@ -6,10 +6,12 @@
 
 #include <algorithm>
 
+#include "base/containers/adapters.h"
 #include "base/macros.h"
 #include "chrome/browser/vr/elements/ui_element.h"
 #include "chrome/browser/vr/model/controller_model.h"
 #include "chrome/browser/vr/model/reticle_model.h"
+#include "chrome/browser/vr/ui_renderer.h"
 #include "chrome/browser/vr/ui_scene.h"
 // TODO(tiborg): Remove include once we use a generic type to pass scroll/fling
 // gestures.
@@ -51,7 +53,6 @@ bool IsScrollEvent(const GestureList& list) {
 
 bool GetTargetLocalPoint(const gfx::Vector3dF& eye_to_target,
                          const UiElement& element,
-                         float max_distance_to_plane,
                          gfx::PointF* out_target_local_point,
                          gfx::Point3F* out_target_point,
                          float* out_distance_to_plane) {
@@ -59,8 +60,7 @@ bool GetTargetLocalPoint(const gfx::Vector3dF& eye_to_target,
     return false;
   }
 
-  if (*out_distance_to_plane < 0 ||
-      *out_distance_to_plane >= max_distance_to_plane) {
+  if (*out_distance_to_plane < 0) {
     return false;
   }
 
@@ -72,37 +72,6 @@ bool GetTargetLocalPoint(const gfx::Vector3dF& eye_to_target,
   out_target_local_point->set_x(0.5f + unit_xy_point.x());
   out_target_local_point->set_y(0.5f - unit_xy_point.y());
   return true;
-}
-
-void HitTestElements(UiElement* element,
-                     ReticleModel* reticle_model,
-                     gfx::Vector3dF* out_eye_to_target,
-                     float* out_closest_element_distance) {
-  for (auto& child : element->children()) {
-    HitTestElements(child.get(), reticle_model, out_eye_to_target,
-                    out_closest_element_distance);
-  }
-
-  if (!element->IsHitTestable()) {
-    return;
-  }
-
-  gfx::PointF local_point;
-  gfx::Point3F plane_intersection_point;
-  float distance_to_plane;
-  if (!GetTargetLocalPoint(*out_eye_to_target, *element,
-                           *out_closest_element_distance, &local_point,
-                           &plane_intersection_point, &distance_to_plane)) {
-    return;
-  }
-  if (!element->HitTest(local_point)) {
-    return;
-  }
-
-  *out_closest_element_distance = distance_to_plane;
-  reticle_model->target_point = plane_intersection_point;
-  reticle_model->target_element_id = element->id();
-  reticle_model->target_local_point = local_point;
 }
 
 }  // namespace
@@ -130,7 +99,6 @@ void UiInputManager::HandleInput(base::TimeTicks current_time,
       gfx::Point3F plane_intersection_point;
       float distance_to_plane;
       if (!GetTargetLocalPoint(eye_to_target, *target_element,
-                               2 * scene_->background_distance(),
                                &reticle_model->target_local_point,
                                &plane_intersection_point, &distance_to_plane)) {
         reticle_model->target_local_point = kInvalidTargetPoint;
@@ -403,13 +371,37 @@ void UiInputManager::GetVisualTargetElement(
   *out_eye_to_target = reticle_model->target_point - kOrigin;
   out_eye_to_target->GetNormalized(out_eye_to_target);
 
-  // Determine which UI element (if any) intersects the line between the eyes
-  // and the controller target position.
-  float closest_element_distance =
-      (reticle_model->target_point - kOrigin).Length();
+  std::vector<const UiElement*> elements;
+  for (auto& element : scene_->root_element()) {
+    if (element.IsVisible()) {
+      elements.push_back(&element);
+    }
+  }
 
-  HitTestElements(&scene_->root_element(), reticle_model, out_eye_to_target,
-                  &closest_element_distance);
+  std::vector<const UiElement*> sorted =
+      UiRenderer::GetElementsInDrawOrder(elements);
+
+  for (const auto* element : base::Reversed(sorted)) {
+    if (!element->IsHitTestable()) {
+      continue;
+    }
+
+    gfx::PointF local_point;
+    gfx::Point3F plane_intersection_point;
+    float distance_to_plane;
+    if (!GetTargetLocalPoint(*out_eye_to_target, *element, &local_point,
+                             &plane_intersection_point, &distance_to_plane)) {
+      continue;
+    }
+    if (!element->HitTest(local_point)) {
+      continue;
+    }
+
+    reticle_model->target_point = plane_intersection_point;
+    reticle_model->target_element_id = element->id();
+    reticle_model->target_local_point = local_point;
+    break;
+  }
 }
 
 void UiInputManager::UpdateQuiescenceState(
