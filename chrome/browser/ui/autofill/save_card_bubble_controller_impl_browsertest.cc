d52bde519798e489a4046e723ade2ea5fde5aa15
Mathieu Perreault <mathp@chromium.org>
2017-11-06 17:16:34 +0000

    [Autofill] Add browsertest cases for credit card save dialog
    Bug: 780989
    Test: out/Default/browser_tests --gtest_filter=BrowserDialogTest.Invoke --dialog=SaveCardBubbleControllerImplTest.InvokeDialog_Local
    Change-Id: Ib883056069985a39ff6d6cf3115f3df2edb3ee28
    Reviewed-on: https://chromium-review.googlesource.com/754732
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514158}
    Former-commit-id: ef522e8e25813f30e28f0a5d72cebc16372555ca

--- chrome/browser/ui/autofill/save_card_bubble_controller_impl_browsertest.cc
+++ chrome/browser/ui/autofill/save_card_bubble_controller_impl_browsertest.cc
@@ -4,17 +4,35 @@
 
 #include "chrome/browser/ui/autofill/save_card_bubble_controller_impl.h"
 
+#include <memory>
+
+#include "base/json/json_reader.h"
+#include "base/macros.h"
+#include "base/values.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
+#include "testing/gtest/include/gtest/gtest.h"
 
 namespace autofill {
 
 class SaveCardBubbleControllerImplTest : public DialogBrowserTest {
  public:
   SaveCardBubbleControllerImplTest() {}
+
+  std::unique_ptr<base::DictionaryValue> GetTestLegalMessage() {
+    std::unique_ptr<base::Value> value(base::JSONReader::Read(
+        "{"
+        "  \"line\" : [ {"
+        "     \"template\": \"This is the entire message.\""
+        "  } ]"
+        "}"));
+    base::DictionaryValue* dictionary;
+    value->GetAsDictionary(&dictionary);
+    return dictionary->CreateDeepCopy();
+  }
 
   // DialogBrowserTest:
   void ShowDialog(const std::string& name) override {
@@ -27,18 +45,42 @@ class SaveCardBubbleControllerImplTest : public DialogBrowserTest {
     SaveCardBubbleControllerImpl* controller =
         SaveCardBubbleControllerImpl::FromWebContents(web_contents);
     DCHECK(controller);
-    CreditCard card = test::GetCreditCard();
-    controller->ShowBubbleForLocalSave(card, base::Bind(&base::DoNothing));
+
+    // Behavior depends on the test case name (not the most robust, but it will
+    // do).
+    if (name.find("Local") != std::string::npos) {
+      controller->ShowBubbleForLocalSave(test::GetCreditCard(),
+                                         base::Bind(&base::DoNothing));
+    } else {
+      CreditCard card = test::GetMaskedServerCard();
+      bool should_cvc_be_requested = name.find("Cvc") != std::string::npos;
+      controller->ShowBubbleForUpload(card, GetTestLegalMessage(),
+                                      should_cvc_be_requested,
+                                      base::Bind(&base::DoNothing));
+    }
   }
 
  private:
   DISALLOW_COPY_AND_ASSIGN(SaveCardBubbleControllerImplTest);
 };
 
-// Invokes a bubble asking the user if they want to save a credit card. See
-// test_browser_dialog.h.
+// Invokes a bubble asking the user if they want to save a credit card locally.
+// See test_browser_dialog.h for instructions on how to run.
+IN_PROC_BROWSER_TEST_F(SaveCardBubbleControllerImplTest, InvokeDialog_Local) {
+  RunDialog();
+}
+
+// Invokes a bubble asking the user if they want to save a credit card to the
+// server. See test_browser_dialog.h for instructions on how to run.
+IN_PROC_BROWSER_TEST_F(SaveCardBubbleControllerImplTest, InvokeDialog_Server) {
+  RunDialog();
+}
+
+// Invokes a bubble asking the user if they want to save a credit card to the
+// server, with an added CVC step. See test_browser_dialog.h for instructions on
+// how to run.
 IN_PROC_BROWSER_TEST_F(SaveCardBubbleControllerImplTest,
-                       InvokeDialog_LocalSave) {
+                       InvokeDialog_Server_WithCvcStep) {
   RunDialog();
 }
 
