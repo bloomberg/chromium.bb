51769a5395b2493b05472e01778bdeda6f675a06
Pavol Marko <pmarko@chromium.org>
2017-11-07 12:43:56 +0000

    ClientCertResolver: Speed up cert filtering by evaluation order change
    When determining which certificates can be used as client certificates,
    check if the certificate is hardware-backed first and if it has a
    private key second.
    Background: The check if a certificate is hardware-backed is less
    expensive:
    IsCertificateHardwareBacked extracts the slot id (struct member access)
    and then calls PK11_IsHW (struct member access).
    HasPrivateKey calls PK11_KeyForCertExists, which iterates all tokens
    and tries to look up the private key inside each token.
    
    BUG=781275
    
    Change-Id: Ie68286e01790cbde305d50227a0e72a0ecacb173
    Reviewed-on: https://chromium-review.googlesource.com/753689
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514454}
    Former-commit-id: cf59b09417034d9d5f4cb850d9ac056dbb1635f6

--- chromeos/network/client_cert_resolver.cc
+++ chromeos/network/client_cert_resolver.cc
@@ -73,6 +73,8 @@ bool ContainsValue(const std::vector<T>& vector, const T& value) {
 }
 
 // Returns true if a private key for certificate |cert| is installed.
+// Note that HasPrivateKey is not a cheap operation: it iterates all tokens and
+// attempts to look up the private key.
 bool HasPrivateKey(CERTCertificate* cert) {
   PK11SlotInfo* slot = PK11_KeyForCertExists(cert, nullptr, nullptr);
   if (!slot)
@@ -180,14 +182,21 @@ std::vector<CertAndIssuer> CreateSortedCertAndIssuerList(
     base::Time now) {
   // Filter all client certs and determines each certificate's issuer, which is
   // required for the pattern matching.
+  // TODO(pmarko): Consider moving the filtering of client certs into
+  // CertLoader. It should not be in ClientCertResolver's responsibility to
+  // decide if a certificate is a valid client certificate or not. Other
+  // consumers of CertLoader could also use a pre-filtered list (e.g.
+  // NetworkCertMigrator). See crbug.com/781693.
   std::vector<CertAndIssuer> client_certs;
   for (net::ScopedCERTCertificateList::iterator it = certs.begin();
        it != certs.end(); ++it) {
     CERTCertificate* cert = it->get();
     base::Time not_after;
+    // HasPrivateKey should be invoked after IsCertificateHardwareBacked for
+    // performance reasons.
     if (!net::x509_util::GetValidityTimes(cert, nullptr, &not_after) ||
-        now > not_after || !HasPrivateKey(cert) ||
-        !CertLoader::IsCertificateHardwareBacked(cert)) {
+        now > not_after || !CertLoader::IsCertificateHardwareBacked(cert) ||
+        !HasPrivateKey(cert)) {
       continue;
     }
     std::string pem_encoded_issuer = GetPEMEncodedIssuer(cert);
