71e30009e9111aa017dbf876f50836a382b80726
Theresa Wellington <twellington@google.com>
2017-11-07 23:13:02 +0000

    Disable two failing BookmarkPersonalizedSigninPromoTests
    BUG=782409
    TBR=bauerb@chromium.org, bsazonov@chromium.org
    
    Change-Id: Ic1c278afcc976dac236ab7c079a63b3a2e1154fb
    Reviewed-on: https://chromium-review.googlesource.com/757664
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514643}
    Former-commit-id: db8f04787fe546230612bd32f499194d3219f15c

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedSigninPromoTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedSigninPromoTest.java
@@ -32,6 +32,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeActivity;
@@ -100,6 +101,7 @@ public class BookmarkPersonalizedSigninPromoTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/782409")
     public void testSigninButtonDefaultAccount() throws Exception {
         addTestAccount();
         openBookmarkManager();
@@ -121,6 +123,7 @@ public class BookmarkPersonalizedSigninPromoTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/782409")
     public void testSigninButtonNotDefaultAccount() throws Exception {
         addTestAccount();
         openBookmarkManager();
