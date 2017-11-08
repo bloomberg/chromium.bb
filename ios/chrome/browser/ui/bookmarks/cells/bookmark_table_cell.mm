c0fe7ada878862be38c0b28b9841ba1e1e2dcfd5
Marti Wong <martiw@chromium.org>
2017-11-08 08:51:59 +0000

    Fix the incorrect accessibility type issue (new iOS bookmarks)
    Override the accessibility methods in bookmark_table_cell so that we can
    hide the title textField from UIAccessibility when it is not being
    edited.
    
    Bug: 781213
    Change-Id: I6f35c6ad8a060f09a8dfc5576123f89bc9ffa114
    Reviewed-on: https://chromium-review.googlesource.com/754466
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514783}
    Former-commit-id: 5213e481438d7d9a058cf49d17542c295e0733d0

--- ios/chrome/browser/ui/bookmarks/cells/bookmark_table_cell.mm
+++ ios/chrome/browser/ui/bookmarks/cells/bookmark_table_cell.mm
@@ -36,6 +36,9 @@ const CGFloat kBookmarkTableCellImagePadding = 16.0;
 // Separator view. Displayed at 1 pixel height at the bottom.
 @property(nonatomic, strong) UIView* separatorView;
 
+// Lists the accessibility elements that are to be seen by UIAccessibility.
+@property(nonatomic, readonly) NSMutableArray* accessibilityElements;
+
 @end
 
 @implementation BookmarkTableCell
@@ -44,6 +47,7 @@ const CGFloat kBookmarkTableCellImagePadding = 16.0;
 @synthesize titleText = _titleText;
 @synthesize textDelegate = _textDelegate;
 @synthesize separatorView = _separatorView;
+@synthesize accessibilityElements = _accessibilityElements;
 
 #pragma mark - Initializer
 
@@ -118,6 +122,12 @@ const CGFloat kBookmarkTableCellImagePadding = 16.0;
     ]];
     _separatorView.translatesAutoresizingMaskIntoConstraints = NO;
     _separatorView.backgroundColor = [UIColor colorWithWhite:0.0 alpha:.12];
+
+    // Setup accessibility elements.
+    _accessibilityElements = [[NSMutableArray alloc] init];
+    self.contentView.isAccessibilityElement = YES;
+    self.contentView.accessibilityTraits |= UIAccessibilityTraitButton;
+    [_accessibilityElements addObject:self.contentView];
   }
   return self;
 }
@@ -126,7 +136,7 @@ const CGFloat kBookmarkTableCellImagePadding = 16.0;
 
 - (void)setNode:(const bookmarks::BookmarkNode*)node {
   self.titleText.text = bookmark_utils_ios::TitleForBookmarkNode(node);
-  self.titleText.accessibilityIdentifier = self.titleText.text;
+  [self updateAccessibilityValues];
 
   self.iconView.image = [UIImage imageNamed:@"bookmark_gray_folder_new"];
   if (node->is_folder()) {
@@ -151,6 +161,9 @@ const CGFloat kBookmarkTableCellImagePadding = 16.0;
       [self.titleText selectAll:nil];
     }
   });
+  if (![self.accessibilityElements containsObject:self.titleText]) {
+    [self.accessibilityElements addObject:self.titleText];
+  }
   self.titleText.delegate = self;
 }
 
@@ -158,6 +171,7 @@ const CGFloat kBookmarkTableCellImagePadding = 16.0;
   [self.textDelegate textDidChangeTo:self.titleText.text];
   self.titleText.userInteractionEnabled = NO;
   [self.titleText endEditing:YES];
+  [self.accessibilityElements removeObject:self.titleText];
 }
 
 + (NSString*)reuseIdentifier {
@@ -214,6 +228,30 @@ const CGFloat kBookmarkTableCellImagePadding = 16.0;
   UIColor* backgroundColor = self.placeholderLabel.backgroundColor;
   [super setSelected:selected animated:animated];
   self.placeholderLabel.backgroundColor = backgroundColor;
+  if (selected) {
+    self.contentView.accessibilityTraits |= UIAccessibilityTraitSelected;
+  } else {
+    self.contentView.accessibilityTraits &= ~UIAccessibilityTraitSelected;
+  }
+}
+
+#pragma mark - Accessibility
+
+- (void)updateAccessibilityValues {
+  self.contentView.accessibilityLabel = self.titleText.text;
+  self.contentView.accessibilityIdentifier = self.titleText.text;
+}
+
+- (NSInteger)accessibilityElementCount {
+  return [self.accessibilityElements count];
+}
+
+- (id)accessibilityElementAtIndex:(NSInteger)index {
+  return [self.accessibilityElements objectAtIndex:index];
+}
+
+- (NSInteger)indexOfAccessibilityElement:(id)element {
+  return [self.accessibilityElements indexOfObject:element];
 }
 
 #pragma mark - UITextFieldDelegate
