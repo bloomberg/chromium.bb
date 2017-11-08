b912fb24e720e3e47aed75c0bdb2ea372dc34744
Gregory Chatzinoff <gchatz@chromium.org>
2017-11-08 02:32:58 +0000

    Introduce the RetrieveCanonicalURL function.
    Introduces the RetrieveCanonicalURL function which can access the
    canonical URL from a web page.
    
    Bug: none
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I0bc33e44679cb1332a37d000b331ea4966ca7e64
    Reviewed-on: https://chromium-review.googlesource.com/752504
    Commit-Queue: Gregory Chatzinoff <gchatz@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514717}
    Former-commit-id: fdddf1a0010dd9c9f8da053f112030745be5d0c8

---
+++ ios/chrome/browser/ui/activity_services/canonical_url_retriever_unittest.mm
@@ -0,0 +1,140 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/activity_services/canonical_url_retriever.h"
+
+#import <Foundation/Foundation.h>
+
+#include "base/macros.h"
+#import "ios/testing/wait_util.h"
+#import "ios/web/public/test/web_test_with_web_state.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+// Test fixture for the retrieving canonical URLs.
+class CanonicalURLRetrieverTest : public web::WebTestWithWebState {
+ public:
+  CanonicalURLRetrieverTest() = default;
+  ~CanonicalURLRetrieverTest() override = default;
+
+ protected:
+  // Retrieves the canonical URL and returns it through the |url| out parameter.
+  // Returns whether the operation was successful.
+  bool RetrieveCanonicalUrl(GURL* url) {
+    __block GURL result;
+    __block bool canonical_url_received = false;
+    activity_services::RetrieveCanonicalUrl(web_state(), ^(const GURL& url) {
+      result = url;
+      canonical_url_received = true;
+    });
+
+    bool success = testing::WaitUntilConditionOrTimeout(
+        testing::kWaitForJSCompletionTimeout, ^{
+          return canonical_url_received;
+        });
+
+    *url = result;
+    return success;
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(CanonicalURLRetrieverTest);
+};
+
+// Validates that if there is one canonical URL, it is found and given to the
+// completion block.
+TEST_F(CanonicalURLRetrieverTest, TestOneCanonicalURLFound) {
+  LoadHtml(@"<link rel=\"canonical\" href=\"https://chromium.test\">",
+           GURL("https://m.chromium.test/"));
+
+  GURL url = GURL("garbage");
+  bool success = RetrieveCanonicalUrl(&url);
+
+  ASSERT_TRUE(success);
+  EXPECT_EQ("https://chromium.test/", url);
+}
+
+// Validates that if there is no canonical URL, an empty GURL is given to the
+// completion block.
+TEST_F(CanonicalURLRetrieverTest, TestNoCanonicalURLFound) {
+  LoadHtml(@"No canonical link on this page.",
+           GURL("https://m.chromium.test/"));
+
+  GURL url = GURL("garbage");
+  bool success = RetrieveCanonicalUrl(&url);
+
+  ASSERT_TRUE(success);
+  EXPECT_TRUE(url.is_empty());
+}
+
+// Validates that if the found canonical URL is invalid, an empty GURL is
+// given to the completion block.
+TEST_F(CanonicalURLRetrieverTest, TestInvalidCanonicalFound) {
+  LoadHtml(@"<link rel=\"canonical\" href=\"chromium\">",
+           GURL("https://m.chromium.test/"));
+
+  GURL url = GURL("garbage");
+  bool success = RetrieveCanonicalUrl(&url);
+
+  ASSERT_TRUE(success);
+  EXPECT_TRUE(url.is_empty());
+}
+
+// Validates that if multiple canonical URLs are found, the first one is given
+// to the completion block.
+TEST_F(CanonicalURLRetrieverTest, TestMultipleCanonicalURLsFound) {
+  LoadHtml(
+      @"<link rel=\"canonical\" href=\"https://chromium.test\">"
+      @"<link rel=\"canonical\" href=\"https://chromium1.test\">",
+      GURL("https://m.chromium.test/"));
+
+  GURL url = GURL("garbage");
+  bool success = RetrieveCanonicalUrl(&url);
+
+  ASSERT_TRUE(success);
+  EXPECT_EQ("https://chromium.test/", url);
+}
+
+// Validates that if the visible and canonical URLs are http, an empty GURL is
+// given to the completion block.
+TEST_F(CanonicalURLRetrieverTest, TestCanonicalURLHTTP) {
+  LoadHtml(@"<link rel=\"canonical\" href=\"http://chromium.test\">",
+           GURL("http://m.chromium.test/"));
+
+  GURL url = GURL("garbage");
+  bool success = RetrieveCanonicalUrl(&url);
+
+  ASSERT_TRUE(success);
+  EXPECT_TRUE(url.is_empty());
+}
+
+// Validates that if the visible URL is HTTP but the canonical URL is HTTPS, an
+// empty GURL is given to the completion block.
+TEST_F(CanonicalURLRetrieverTest, TestCanonicalURLHTTPSUpgrade) {
+  LoadHtml(@"<link rel=\"canonical\" href=\"https://chromium.test\">",
+           GURL("http://m.chromium.test/"));
+
+  GURL url = GURL("garbage");
+  bool success = RetrieveCanonicalUrl(&url);
+
+  ASSERT_TRUE(success);
+  EXPECT_TRUE(url.is_empty());
+}
+
+// Validates that if the visible URL is HTTPS but the canonical URL is HTTP, an
+// empty GURL is given to the completion block.
+TEST_F(CanonicalURLRetrieverTest, TestCanonicalLinkHTTPSDowngrade) {
+  LoadHtml(@"<link rel=\"canonical\" href=\"http://chromium.test\">",
+           GURL("https://m.chromium.test/"));
+
+  GURL url = GURL("garbage");
+  bool success = RetrieveCanonicalUrl(&url);
+
+  ASSERT_TRUE(success);
+  EXPECT_TRUE(url.is_empty());
+}
