40ad45874b8857580eee134d24d6b1362a3f7289
Finnur Thorarinsson <finnur@chromium.org>
2017-11-08 23:48:47 +0000

    Win10 Native Notifications: Implement group and tag.
    Bug: 734095
    Change-Id: I1f9bf2398559b7241ad81a9a6f83224a2236406b
    Reviewed-on: https://chromium-review.googlesource.com/753622
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515010}
    Former-commit-id: 99ebc04a362941e0ff0aff66c0c33514f2e049ae

---
+++ chrome/browser/notifications/notification_platform_bridge_win_unittest.cc
@@ -0,0 +1,90 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/notifications/notification_platform_bridge_win.h"
+
+#include <wrl/client.h>
+#include <wrl/wrappers/corewrappers.h>
+#include <memory>
+
+#include "base/hash.h"
+#include "base/memory/ptr_util.h"
+#include "base/strings/string_number_conversions.h"
+#include "base/win/scoped_com_initializer.h"
+#include "base/win/scoped_hstring.h"
+#include "base/win/windows_version.h"
+#include "chrome/browser/notifications/notification_template_builder.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "ui/message_center/notification.h"
+#include "ui/message_center/notifier_id.h"
+
+namespace mswr = Microsoft::WRL;
+namespace winui = ABI::Windows::UI;
+
+using message_center::Notification;
+
+namespace {
+
+const char kOrigin[] = "https://www.google.com/";
+const char kNotificationId[] = "id";
+
+}  // namespace
+
+class NotificationPlatformBridgeWinTest : public testing::Test {
+ public:
+  NotificationPlatformBridgeWinTest()
+      : notification_platform_bridge_win_(
+            std::make_unique<NotificationPlatformBridgeWin>()) {}
+  ~NotificationPlatformBridgeWinTest() override = default;
+
+  HRESULT GetToast(
+      ABI::Windows::UI::Notifications::IToastNotification** toast) {
+    GURL origin(kOrigin);
+    auto notification = std::make_unique<message_center::Notification>(
+        message_center::NOTIFICATION_TYPE_SIMPLE, kNotificationId, L"title",
+        L"message", gfx::Image(), L"display_source", origin,
+        message_center::NotifierId(origin),
+        message_center::RichNotificationData(), nullptr /* delegate */);
+    std::unique_ptr<NotificationTemplateBuilder> builder =
+        NotificationTemplateBuilder::Build(*notification);
+
+    return notification_platform_bridge_win_->GetToastNotification(
+        *notification, *builder, toast);
+  }
+
+ protected:
+  std::unique_ptr<NotificationPlatformBridgeWin>
+      notification_platform_bridge_win_;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NotificationPlatformBridgeWinTest);
+};
+
+TEST_F(NotificationPlatformBridgeWinTest, GroupAndTag) {
+  // This test requires WinRT core functions, which are not available in
+  // older versions of Windows.
+  if (base::win::GetVersion() < base::win::VERSION_WIN8)
+    return;
+
+  base::win::ScopedCOMInitializer com_initializer;
+
+  mswr::ComPtr<winui::Notifications::IToastNotification> toast;
+  ASSERT_HRESULT_SUCCEEDED(GetToast(&toast));
+  mswr::ComPtr<winui::Notifications::IToastNotification2> toast2;
+  ASSERT_HRESULT_SUCCEEDED(
+      toast.As<winui::Notifications::IToastNotification2>(&toast2));
+
+  HSTRING hstring_group;
+  ASSERT_HRESULT_SUCCEEDED(toast2->get_Group(&hstring_group));
+  base::win::ScopedHString group(hstring_group);
+  GURL origin(kOrigin);
+  ASSERT_STREQ(base::UintToString16(base::Hash(origin.spec())).c_str(),
+               group.Get().as_string().c_str());
+
+  HSTRING hstring_tag;
+  ASSERT_HRESULT_SUCCEEDED(toast2->get_Tag(&hstring_tag));
+  base::win::ScopedHString tag(hstring_tag);
+  ASSERT_STREQ(base::UintToString16(base::Hash(kNotificationId)).c_str(),
+               tag.Get().as_string().c_str());
+}
