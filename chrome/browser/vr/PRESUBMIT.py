3bcfe07d00cf9e33700d914de6fd246b47a80267
Kenneth Russell <kbr@chromium.org>
2017-11-08 22:55:01 +0000

    Run optional GPU tryservers against VR changes.
    These changes have the possibility of breaking normal operation of the
    browser and it's important that we run all of the available GPU tests
    against them.
    
    BUG=782573
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: If19c13e91ee4a0757abbd6b7facb84f5fcb5586f
    Reviewed-on: https://chromium-review.googlesource.com/759114
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#514983}
    Former-commit-id: 8e8300ba82e9758863bb4a46dd747dc3ed4a610c

--- chrome/browser/vr/PRESUBMIT.py
+++ chrome/browser/vr/PRESUBMIT.py
@@ -39,3 +39,25 @@ def CheckChangeOnCommit(input_api, output_api):
   results = []
   results.extend(_CheckChangeLintsClean(input_api, output_api))
   return results
+
+def PostUploadHook(cl, change, output_api):
+  """git cl upload will call this hook after the issue is created/modified.
+
+  This hook modifies the CL description in order to run extra GPU
+  tests (in particular, the WebGL 2.0 conformance tests) in addition
+  to the regular CQ try bots. This test suite is too large to run
+  against all Chromium commits, but should be run against changes
+  likely to affect these tests.
+
+  When adding/removing tests here, ensure that both gpu/PRESUBMIT.py and
+  ui/gl/PRESUBMIT.py are updated.
+  """
+  return output_api.EnsureCQIncludeTrybotsAreAdded(
+    cl,
+    [
+      'master.tryserver.chromium.linux:linux_optional_gpu_tests_rel',
+      'master.tryserver.chromium.mac:mac_optional_gpu_tests_rel',
+      'master.tryserver.chromium.win:win_optional_gpu_tests_rel',
+      'master.tryserver.chromium.android:android_optional_gpu_tests_rel',
+    ],
+    'Automatically added optional GPU tests to run on CQ.')
