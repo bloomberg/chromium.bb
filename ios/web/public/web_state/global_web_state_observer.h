2a43f2f88cdaf698f125fd070a3fb326fc9bb50c
Danyao Wang <danyao@google.com>
2017-11-09 17:43:25 +0000

    Add deprecation notes to GlobalWebStateObserver.
    Bug: 720786
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I80e08d02f49e452596a21e4a91f0d0d7f3810236
    Reviewed-on: https://chromium-review.googlesource.com/758484
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515200}
    Former-commit-id: 3f178c9f93a105eba880f285a54ae2dde1b9f552

--- ios/web/public/web_state/global_web_state_observer.h
+++ ios/web/public/web_state/global_web_state_observer.h
@@ -21,35 +21,55 @@ class WebState;
 // WARNING: This class exists only to mimic //chrome-specific flows that
 // listen for notifications from all WebContents or NavigationController
 // instances. Do not add new subclasses of this class for any other reason.
+// DEPRECATED. Use TabHelper to observe WebState creation. Use WebStateObserver
+// to observe WebState changes.
+// TODO(crbug.com/782269): Remove this class.
 class GlobalWebStateObserver {
  public:
+  // DEPRECATED. Use WebStateObserver's |NavigationItemsPruned| instead.
   // Called when navigation items have been pruned in |web_state|.
+  // TODO(crbug.com/782269): Remove this method.
   virtual void NavigationItemsPruned(WebState* web_state,
                                      size_t pruned_item_count) {}
 
   // Called when a navigation item has changed in |web_state|.
+  // DEPRECATED. Use WebStateObserver's |TitleWasSet| to listen for title
+  // changes and |DidFinishNavigation| for |window.location.replace|.
+  // TODO(crbug.com/782269): Remove this method.
   virtual void NavigationItemChanged(WebState* web_state) {}
 
   // Called when a navigation item has been committed in |web_state|.
+  // DEPRECATED. Use WebStateObserver's |DidFinishNavigation| instead.
+  // TODO(crbug.com/782269): Remove this method.
   virtual void NavigationItemCommitted(
       WebState* web_state,
       const LoadCommittedDetails& load_details) {}
 
   // Called when |web_state| has started loading a page.
+  // DEPRECATED. Use WebStateObserver's |DidStartLoading| instead.
+  // TODO(crbug.com/782269): Remove this method.
   virtual void WebStateDidStartLoading(WebState* web_state) {}
 
   // Called when |web_state| has stopped loading a page.
+  // DEPRECATED. Use WebStateObserver's |DidStopLoading| instead.
+  // TODO(crbug.com/782269): Remove this method.
   virtual void WebStateDidStopLoading(WebState* web_state) {}
 
   // Called when the current page is loaded in |web_state|.
+  // DEPRECATED. Use WebStateObserver's |PageLoaded| instead.
+  // TODO(crbug.com/782269): Remove this method.
   virtual void PageLoaded(WebState* web_state,
                           PageLoadCompletionStatus load_completion_status) {}
 
   // Called when the web process is terminated (usually by crashing, though
   // possibly by other means).
+  // DEPRECATED. Use WebStateObserver's |RendererProcessGone| instead.
+  // TODO(crbug.com/782269): Remove this method.
   virtual void RenderProcessGone(WebState* web_state) {}
 
   // Called when |web_state| is being destroyed.
+  // DEPRECATED. Use WebStateObserver's |WebStateDestroyed| instead.
+  // TODO(crbug.com/782269): Remove this method.
   virtual void WebStateDestroyed(WebState* web_state) {}
 
  protected:
