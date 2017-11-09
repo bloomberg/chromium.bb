7d91c12692a6e549d2f6c79b172c710f078e3026
Eddy Mead <meade@chromium.org>
2017-11-09 05:58:33 +0000

    Move templates for generating CSSValueKeywords into jinja files
    String template and jinja template diff:
    https://www.diffchecker.com/kpI6KiZh
    
    gist diff:
    https://gist.github.com/0ce0266da324aeb06682d8d711f4c1c3/revisions
    
    Bug: 752745
    Change-Id: I2adf5eca99975bdf66b69bf09a42e9127260687b
    Reviewed-on: https://chromium-review.googlesource.com/750443
    Commit-Queue: meade_UTC10 <meade@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515111}
    Former-commit-id: e12ef9fda60be4d335a9ad68ef9dd7c28f768f43

--- third_party/WebKit/Source/build/scripts/core/css/make_css_value_keywords.py
+++ third_party/WebKit/Source/build/scripts/core/css/make_css_value_keywords.py
@@ -8,119 +8,18 @@ import os.path
 import subprocess
 from name_utilities import enum_for_css_keyword
 import json5_generator
-from license import license_for_generated_cpp
 
-
-HEADER_TEMPLATE = """
-%(license)s
-
-#ifndef %(class_name)s_h
-#define %(class_name)s_h
-
-#include "core/css/parser/CSSParserMode.h"
-#include <string.h>
-
-namespace blink {
-
-enum CSSValueID {
-%(value_keyword_enums)s
-};
-
-const int numCSSValueKeywords = %(value_keywords_count)d;
-const size_t maxCSSValueKeywordLength = %(max_value_keyword_length)d;
-
-const char* getValueName(CSSValueID);
-bool isValueAllowedInMode(unsigned short id, CSSParserMode mode);
-
-} // namespace blink
-
-#endif // %(class_name)s_h
-"""
-
-GPERF_TEMPLATE = """
-%%{
-%(license)s
-
-#include "%(class_name)s.h"
-#include "core/css/HashTools.h"
-#include <string.h>
-
-#ifdef _MSC_VER
-// Disable the warnings from casting a 64-bit pointer to 32-bit long
-// warning C4302: 'type cast': truncation from 'char (*)[28]' to 'long'
-// warning C4311: 'type cast': pointer truncation from 'char (*)[18]' to 'long'
-#pragma warning(disable : 4302 4311)
-#endif
-
-#if defined(__clang__)
-#pragma clang diagnostic push
-// TODO(thakis): Remove once we use a gperf that no longer produces "register".
-#pragma clang diagnostic ignored "-Wdeprecated-register"
-#endif
-
-namespace blink {
-static const char valueListStringPool[] = {
-%(value_keyword_strings)s
-};
-
-static const unsigned short valueListStringOffsets[] = {
-%(value_keyword_offsets)s
-};
-
-%%}
-%%struct-type
-struct Value;
-%%omit-struct-type
-%%language=C++
-%%readonly-tables
-%%compare-strncmp
-%%define class-name %(class_name)sHash
-%%define lookup-function-name findValueImpl
-%%define hash-function-name value_hash_function
-%%define slot-name name_offset
-%%define word-array-name value_word_list
-%%pic
-%%enum
-%%%%
-%(value_keyword_to_enum_map)s
-%%%%
-
-#if defined(__clang__)
-#pragma clang diagnostic pop
-#endif
-
-const Value* FindValue(const char* str, unsigned int len) {
-  return CSSValueKeywordsHash::findValueImpl(str, len);
-}
-
-const char* getValueName(CSSValueID id) {
-  DCHECK_GT(id, 0);
-  DCHECK_LT(id, numCSSValueKeywords);
-  return valueListStringPool + valueListStringOffsets[id - 1];
-}
-
-bool isValueAllowedInMode(unsigned short id, CSSParserMode mode) {
-  switch (id) {
-    %(ua_sheet_mode_values_keywords)s
-      return IsUASheetBehavior(mode);
-    %(quirks_mode_or_ua_sheet_mode_values_keywords)s
-      return IsUASheetBehavior(mode) || IsQuirksModeBehavior(mode);
-    default:
-      return true;
-  }
-}
-
-} // namespace blink
-"""
+import template_expander
+import gperf
 
 
 class CSSValueKeywordsWriter(json5_generator.Writer):
-    class_name = "CSSValueKeywords"
-
     def __init__(self, file_paths):
         json5_generator.Writer.__init__(self, file_paths)
-        self._outputs = {(self.class_name + ".h"): self.generate_header,
-                         (self.class_name + ".cpp"): self.generate_implementation}
+        self._outputs = {
+            "CSSValueKeywords.h": self.generate_header,
+            "CSSValueKeywords.cpp": self.generate_implementation
+        }
 
         self._value_keywords = self.json5_file.name_dictionaries
         first_keyword_id = 1
@@ -129,30 +28,32 @@ class CSSValueKeywordsWriter(json5_generator.Writer):
             keyword['enum_name'] = enum_for_css_keyword(keyword['name'])
             keyword['enum_value'] = first_keyword_id + offset
             if keyword['name'].startswith('-internal-'):
-                assert keyword['mode'] is None, 'Can\'t specify mode for value keywords with the prefix "-internal-".'
+                assert keyword['mode'] is None, 'Can\'t specify mode for ' \
+                    'value keywords with the prefix "-internal-".'
                 keyword['mode'] = 'UASheet'
             else:
-                assert keyword['mode'] != 'UASheet', 'UASheet mode only value keywords should have the prefix "-internal-".'
+                assert keyword['mode'] != 'UASheet', 'UASheet mode only ' \
+                    'value keywords should have the prefix "-internal-".'
+        self._keyword_count = len(self._value_keywords) + first_keyword_id
 
-    def _enum_declaration(self, keyword):
-        return "    %(enum_name)s = %(enum_value)s," % keyword
-
-    def _case_value_keyword(self, keyword):
-        return "case %(enum_name)s:" % keyword
-
+    @template_expander.use_jinja('core/css/templates/CSSValueKeywords.h.tmpl')
     def generate_header(self):
-        enum_enties = map(self._enum_declaration, [{'enum_name': 'CSSValueInvalid', 'enum_value': 0}] + self._value_keywords)
-        return HEADER_TEMPLATE % {
-            'license': license_for_generated_cpp(),
-            'class_name': self.class_name,
-            'value_keyword_enums': "\n".join(enum_enties),
-            'value_keywords_count': len(enum_enties),
-            'max_value_keyword_length': max(len(keyword['name']) for keyword in self._value_keywords),
+        return {
+            'value_keywords': self._value_keywords,
+            'value_keywords_count': self._keyword_count,
+            'max_value_keyword_length':
+                max(len(keyword['name']) for keyword in self._value_keywords),
         }
 
     def _value_keywords_with_mode(self, mode):
-        return [keyword for keyword in self._value_keywords if keyword['mode'] == mode]
+        return [
+            keyword for keyword in self._value_keywords
+            if keyword['mode'] == mode
+        ]
 
+    @gperf.use_jinja_gperf_template(
+        'core/css/templates/CSSValueKeywords.cpp.tmpl',
+        ['-Q', 'CSSValueStringPool'])
     def generate_implementation(self):
         keyword_offsets = []
         current_offset = 0
@@ -160,35 +61,15 @@ class CSSValueKeywordsWriter(json5_generator.Writer):
             keyword_offsets.append(current_offset)
             current_offset += len(keyword["name"]) + 1
 
-        gperf_input = GPERF_TEMPLATE % {
-            'license': license_for_generated_cpp(),
-            'class_name': self.class_name,
-            'value_keyword_strings': '\n'.join('    "%(name)s\\0"' % keyword for keyword in self._value_keywords),
-            'value_keyword_offsets': '\n'.join('  %d,' % offset for offset in keyword_offsets),
-            'value_keyword_to_enum_map': '\n'.join('%(lower_name)s, %(enum_name)s' % keyword for keyword in self._value_keywords),
-            'ua_sheet_mode_values_keywords': '\n        '.join(
-                map(self._case_value_keyword, self._value_keywords_with_mode('UASheet'))
-            ),
-            'quirks_mode_or_ua_sheet_mode_values_keywords': '\n    '.join(
-                map(self._case_value_keyword, self._value_keywords_with_mode('QuirksOrUASheet'))
-            ),
+        return {
+            'value_keywords': self._value_keywords,
+            'value_keyword_offsets': keyword_offsets,
+            'ua_sheet_mode_values_keywords':
+                self._value_keywords_with_mode('UASheet'),
+            'quirks_mode_or_ua_sheet_mode_values_keywords':
+                self._value_keywords_with_mode('QuirksOrUASheet'),
+            'gperf_path': self.gperf_path,
         }
-        # FIXME: If we could depend on Python 2.7, we would use subprocess.check_output
-        gperf_args = [self.gperf_path, '--key-positions=*', '-P', '-n']
-        gperf_args.extend(['-m', '50'])  # Pick best of 50 attempts.
-        gperf_args.append('-D')  # Allow duplicate hashes -> More compact code.
-        gperf_args.extend(['-Q', 'CSSValueStringPool'])  # Unique var names.
-
-        # If gperf isn't in the path we get an OSError. We don't want to use
-        # the normal solution of shell=True (as this has to run on many
-        # platforms), so instead we catch the error and raise a
-        # CalledProcessError like subprocess would do when shell=True is set.
-        try:
-            gperf = subprocess.Popen(gperf_args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
-            return gperf.communicate(gperf_input)[0]
-        except OSError:
-            raise subprocess.CalledProcessError(127, gperf_args, output='Command not found.')
-
 
 if __name__ == "__main__":
     json5_generator.Maker(CSSValueKeywordsWriter).main()
