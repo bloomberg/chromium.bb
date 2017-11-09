wjdz99/aom

Add av1_get_base_level_counts()
which calculates base level counts in block.

Change-Id: Ia975faad4afbfc0c587bdcd403de3d4dc3f5f411

av1/common/txb_common.c
@@ -282,3 +282,17 @@ int16_t get_eob_pos_token(int eob, int16_t *extra) {
 
   return t;
 }
+
+void av1_get_base_level_counts(const uint8_t *const levels,
+                               const int level_minus_1, const int width,
+                               const int height, uint8_t *const level_counts) {
+  const int stride = width + MAX_TX_PAD_HOR;
+
+  for (int row = 0; row < height; ++row) {
+    for (int col = 0; col < width; ++col) {
+      level_counts[row * width + col] =
+          get_level_count(levels, stride, row, col, level_minus_1,
+                          base_ref_offset, BASE_CONTEXT_POSITION_NUM);
+    }
+  }
+}

bloomberg/chromium.bb

av1/common/txb_common.c
@@ -282,3 +282,17 @@ int16_t get_eob_pos_token(int eob, int16_t *extra) {
 
   return t;
 }
+
+void av1_get_base_level_counts(const uint8_t *const levels,
+                               const int level_minus_1, const int width,
+                               const int height, uint8_t *const level_counts) {
+  const int stride = width + TX_PAD_HOR;
+
+  for (int row = 0; row < height; ++row) {
+    for (int col = 0; col < width; ++col) {
+      level_counts[row * width + col] =
+          get_level_count(levels, stride, row, col, level_minus_1,
+                          base_ref_offset, BASE_CONTEXT_POSITION_NUM);
+    }
+  }
+}