88faa53dd4e573643c309dcd14f55ab6746fd73e
Adithya Srinivasan <adithyas@chromium.org>
2017-11-10 17:53:34 +0000

    Revert "Re-enabled DefaultKeyboardExtensionBrowserTest.EndToEndTest."
    This reverts commit 2f2403fc44c77e812bb9fe4978060b5731a8acb5 [formerly 9563643319e5caebdc5f88e09872c9bf6ff48806].
    
    Reason for revert: This test still appears to time out on Linux ChromiumOS MSAN, so I'm disabling it again.
    
    Original change's description:
    > Re-enabled DefaultKeyboardExtensionBrowserTest.EndToEndTest.
    > 
    > The cause of the failure was fixed in crrev.com/c/645586.
    > 
    > Bug:515596
    > Test: Run the test 100 times on local machine
    > 
    > Change-Id: I64c613b23f3147f5c8cdb69f9c23adb1cb7bff21
    > Reviewed-on: https://chromium-review.googlesource.com/667556
    > Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    > Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#515454}
    
    TBR=tbarzic@chromium.org,yhanada@chromium.org
    
    Change-Id: I4c15620c1aa1cad6e61aa58040e052d5546da3ef
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 515596
    Reviewed-on: https://chromium-review.googlesource.com/763848
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515592}
    Former-commit-id: 9e95d795a2930525192754a9832ff34023309fdf

--- chrome/browser/chromeos/extensions/default_keyboard_extension_browser_test.cc
+++ chrome/browser/chromeos/extensions/default_keyboard_extension_browser_test.cc
@@ -158,7 +158,9 @@ IN_PROC_BROWSER_TEST_F(DefaultKeyboardExtensionBrowserTest, IsKeyboardLoaded) {
   ASSERT_TRUE(loaded);
 }
 
-IN_PROC_BROWSER_TEST_F(DefaultKeyboardExtensionBrowserTest, EndToEndTest) {
+// Disabled; http://crbug.com/515596
+IN_PROC_BROWSER_TEST_F(DefaultKeyboardExtensionBrowserTest,
+                       DISABLED_EndToEndTest) {
   // Get the virtual keyboard's render view host.
   content::RenderViewHost* keyboard_rvh =
       GetKeyboardRenderViewHost(kExtensionId);
