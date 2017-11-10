ab5191543152dfd3b53ce948584d7e38ee5ce86a
Gregory Chatzinoff <gchatz@chromium.org>
2017-11-10 03:37:12 +0000

    Add Metrics to Record Result of Canonical URL Retrieval.
    Bug: none
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I11e058b4dbb86a547ffbb7d96fd5c11286ec80d2
    Reviewed-on: https://chromium-review.googlesource.com/759141
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Gregory Chatzinoff <gchatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515442}
    Former-commit-id: 2e7619405ff3e99d6e24758165d89e307310e1c2

--- ios/chrome/browser/ui/activity_services/canonical_url_retriever.mm
+++ ios/chrome/browser/ui/activity_services/canonical_url_retriever.mm
@@ -3,8 +3,10 @@
 // found in the LICENSE file.
 
 #import "ios/chrome/browser/ui/activity_services/canonical_url_retriever.h"
+#import "ios/chrome/browser/ui/activity_services/canonical_url_retriever_private.h"
 
 #include "base/mac/bind_objc_block.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
 #import "ios/chrome/browser/procedural_block_types.h"
@@ -17,20 +19,55 @@
 
 namespace {
 // Script to access the canonical URL from a web page.
-char kCanonicalURLScript[] =
+const char kCanonicalURLScript[] =
     "(function() {"
     "  var linkNode = document.querySelector(\"link[rel='canonical']\");"
     "  return linkNode ? linkNode.getAttribute(\"href\") : \"\";"
     "})()";
 
+// Logs |result| in the IOS.CanonicalURLResult histogram.
+void LogCanonicalUrlResultHistogram(
+    activity_services::CanonicalURLResult result) {
+  UMA_HISTOGRAM_ENUMERATION(activity_services::kCanonicalURLResultHistogram,
+                            result,
+                            activity_services::CANONICAL_URL_RESULT_COUNT);
+}
+
 // Converts a |value| to a GURL. Returns an empty GURL if |value| is not a valid
-// URL.
+// HTTPS URL, indicating that retrieval failed. This function also handles
+// logging retrieval failures if applicable.
 GURL UrlFromValue(const base::Value* value) {
   GURL canonical_url;
-  if (value && value->is_string()) {
+  bool canonical_url_found = false;
+
+  if (value && value->is_string() && !value->GetString().empty()) {
     canonical_url = GURL(value->GetString());
+
+    // This variable is required for metrics collection in order to distinguish
+    // between the no canonical URL found and the invalid canonical URL found
+    // cases. The |canonical_url| GURL cannot be relied upon to distinguish
+    // between these cases because GURLs created with invalid URLs can be
+    // constructed as empty GURLs.
+    canonical_url_found = true;
   }
-  return canonical_url.is_valid() ? canonical_url : GURL::EmptyGURL();
+
+  if (!canonical_url_found) {
+    // Log result if no canonical URL is found.
+    LogCanonicalUrlResultHistogram(
+        activity_services::FAILED_NO_CANONICAL_URL_DEFINED);
+  } else if (!canonical_url.is_valid()) {
+    // Log result if an invalid canonical URL is found.
+    LogCanonicalUrlResultHistogram(
+        activity_services::FAILED_CANONICAL_URL_INVALID);
+  } else if (!canonical_url.SchemeIsCryptographic()) {
+    // Logs result if the found canonical URL is not HTTPS.
+    LogCanonicalUrlResultHistogram(
+        activity_services::FAILED_CANONICAL_URL_NOT_HTTPS);
+  }
+
+  return canonical_url.is_valid() && canonical_url.SchemeIsCryptographic()
+             ? canonical_url
+             : GURL::EmptyGURL();
 }
 }  // namespace
 
@@ -38,7 +75,10 @@ namespace activity_services {
 void RetrieveCanonicalUrl(web::WebState* web_state,
                           ProceduralBlockWithURL completion) {
   // Do not use the canonical URL if the page is not secured with HTTPS.
-  if (!web_state->GetVisibleURL().SchemeIsCryptographic()) {
+  const GURL visible_url = web_state->GetVisibleURL();
+  if (!visible_url.SchemeIsCryptographic()) {
+    LogCanonicalUrlResultHistogram(
+        activity_services::FAILED_VISIBLE_URL_NOT_HTTPS);
     completion(GURL::EmptyGURL());
     return;
   }
@@ -47,10 +87,17 @@ void RetrieveCanonicalUrl(web::WebState* web_state,
       ^(const base::Value* value) {
         GURL canonical_url = UrlFromValue(value);
 
-        // If the canonical URL is not HTTPS, pass an empty GURL so that there
-        // is not a downgrade from the HTTPS visible URL.
-        completion(canonical_url.SchemeIsCryptographic() ? canonical_url
-                                                         : GURL::EmptyGURL());
+        // If the canonical URL is not empty, then the retrieval was successful,
+        // and the success can be logged.
+        if (!canonical_url.is_empty()) {
+          LogCanonicalUrlResultHistogram(
+              visible_url == canonical_url
+                  ? activity_services::SUCCESS_CANONICAL_URL_SAME_AS_VISIBLE
+                  : activity_services::
+                        SUCCESS_CANONICAL_URL_DIFFERENT_FROM_VISIBLE);
+        }
+
+        completion(canonical_url);
       };
 
   web_state->ExecuteJavaScript(base::UTF8ToUTF16(kCanonicalURLScript),
