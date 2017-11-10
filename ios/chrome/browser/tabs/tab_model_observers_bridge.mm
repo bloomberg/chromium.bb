e63b3df5da4434b07ac0a38404157a0d00d049f1
Sylvain Defresne <sdefresne@chromium.org>
2017-11-10 19:55:15 +0000

    Extract FaviconObserver to a separate class.
    Extract FaviconDriverObserverBridge out of TabModelObserversBridge
    into a separate class TabModelFaviconDriverObserver and refactor
    it to avoid casting the FaviconDriver to WebFaviconDriver (using
    a map to get the WebState associated with a FaviconDriver).
    
    The cast was the reason for reverting http://crrev.com/c/743627.
    
    Bug: 775684
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I58f2040642b93142ff299a6d6594bc1cca353092
    Reviewed-on: https://chromium-review.googlesource.com/758843
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Rohit Rao (ping after 24h) <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515648}
    Former-commit-id: 17b7b33f3fa3cfaf221163f9ec020b7d61e9c078

--- ios/chrome/browser/tabs/tab_model_observers_bridge.mm
+++ ios/chrome/browser/tabs/tab_model_observers_bridge.mm
@@ -4,97 +4,13 @@
 
 #import "ios/chrome/browser/tabs/tab_model_observers_bridge.h"
 
-#include <map>
-#include <memory>
-
 #include "base/logging.h"
-#include "base/macros.h"
-#include "base/scoped_observer.h"
-#include "components/favicon/ios/web_favicon_driver.h"
 #import "ios/chrome/browser/tabs/legacy_tab_helper.h"
-#import "ios/chrome/browser/tabs/tab_model.h"
 #import "ios/chrome/browser/tabs/tab_model_observers.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
-
-@interface TabModelObserversBridge ()
-
-// Called when the given WebState favicon has been changed.
-- (void)webStateFaviconChanged:(web::WebState*)webState;
-
-@end
-
-namespace {
-
-// Listen to multiple WebFaviconDriver for notification that their WebState's
-// favicon has changed and forward the notification to TabModelObserversBridge.
-class FaviconDriverObserverBridge : public favicon::FaviconDriverObserver {
- public:
-  explicit FaviconDriverObserverBridge(TabModelObserversBridge* bridge);
-  ~FaviconDriverObserverBridge() override;
-
-  // Starts/stops listening to a given WebFaviconDriver notifications. The
-  // |source| can be null (methods are no-op in that case).
-  void Add(favicon::WebFaviconDriver* source);
-  void Remove(favicon::WebFaviconDriver* source);
-
-  // favicon::FaviconDriverObserver implementation.
-  void OnFaviconUpdated(favicon::FaviconDriver* favicon_driver,
-                        NotificationIconType notification_icon_type,
-                        const GURL& icon_url,
-                        bool icon_url_changed,
-                        const gfx::Image& image) override;
-
- private:
-  // The owning TabModelObserversBridge. Notifications will be forwarded to
-  // this object.
-  __weak TabModelObserversBridge* bridge_;
-
-  // Scoped observer to track the WebFaviconDriver that are listened to.
-  ScopedObserver<favicon::FaviconDriver, favicon::FaviconDriverObserver>
-      scoped_observer_;
-
-  DISALLOW_COPY_AND_ASSIGN(FaviconDriverObserverBridge);
-};
-
-FaviconDriverObserverBridge::FaviconDriverObserverBridge(
-    TabModelObserversBridge* bridge)
-    : bridge_(bridge), scoped_observer_(this) {}
-
-FaviconDriverObserverBridge::~FaviconDriverObserverBridge() = default;
-
-void FaviconDriverObserverBridge::Add(favicon::WebFaviconDriver* source) {
-  if (source) {
-    DCHECK(!scoped_observer_.IsObserving(source));
-    scoped_observer_.Add(source);
-  }
-}
-
-void FaviconDriverObserverBridge::Remove(favicon::WebFaviconDriver* source) {
-  if (source) {
-    DCHECK(scoped_observer_.IsObserving(source));
-    scoped_observer_.Remove(source);
-  }
-}
-
-void FaviconDriverObserverBridge::OnFaviconUpdated(
-    favicon::FaviconDriver* favicon_driver,
-    favicon::FaviconDriverObserver::NotificationIconType notification_icon_type,
-    const GURL& icon_url,
-    bool icon_url_changed,
-    const gfx::Image& image) {
-  // It is safe to cast the driver to favicon::WebFaviconDriver* if it is
-  // observed as only favicon::WebFaviconDrivers can be added to the
-  // ScopedObserver.
-  DCHECK(scoped_observer_.IsObserving(favicon_driver));
-  web::WebState* web_state =
-      static_cast<favicon::WebFaviconDriver*>(favicon_driver)->web_state();
-  [bridge_ webStateFaviconChanged:web_state];
-}
-
-}  // namespace
 
 @implementation TabModelObserversBridge {
   // The TabModel owning self.
@@ -103,10 +19,6 @@ void FaviconDriverObserverBridge::OnFaviconUpdated(
   // The TabModelObservers that forward events to TabModelObserver instances
   // registered with owning TabModel.
   __weak TabModelObservers* _tabModelObservers;
-
-  // The FaviconDriverObserverBridge used to listen to all the WebState's
-  // favicon changed notifications.
-  std::unique_ptr<FaviconDriverObserverBridge> _faviconObserver;
 }
 
 - (instancetype)initWithTabModel:(TabModel*)tabModel
@@ -116,7 +28,6 @@ void FaviconDriverObserverBridge::OnFaviconUpdated(
   if ((self = [super init])) {
     _tabModel = tabModel;
     _tabModelObservers = tabModelObservers;
-    _faviconObserver = std::make_unique<FaviconDriverObserverBridge>(self);
   }
   return self;
 }
@@ -128,7 +39,6 @@ void FaviconDriverObserverBridge::OnFaviconUpdated(
               atIndex:(int)atIndex
            activating:(BOOL)activating {
   DCHECK_GE(atIndex, 0);
-  [self webStateInserted:webState];
   [_tabModelObservers tabModel:_tabModel
                   didInsertTab:LegacyTabHelper::GetTabForWebState(webState)
                        atIndex:static_cast<NSUInteger>(atIndex)
@@ -153,14 +63,10 @@ void FaviconDriverObserverBridge::OnFaviconUpdated(
           withWebState:(web::WebState*)newWebState
                atIndex:(int)atIndex {
   DCHECK_GE(atIndex, 0);
-  [self webStateInserted:newWebState];
-
   [_tabModelObservers tabModel:_tabModel
                  didReplaceTab:LegacyTabHelper::GetTabForWebState(oldWebState)
                        withTab:LegacyTabHelper::GetTabForWebState(newWebState)
                        atIndex:static_cast<NSUInteger>(atIndex)];
-
-  [self webStateDetached:oldWebState];
 }
 
 - (void)webStateList:(WebStateList*)webStateList
@@ -171,7 +77,6 @@ void FaviconDriverObserverBridge::OnFaviconUpdated(
                   didRemoveTab:LegacyTabHelper::GetTabForWebState(webState)
                        atIndex:static_cast<NSUInteger>(atIndex)];
   [_tabModelObservers tabModelDidChangeTabCount:_tabModel];
-  [self webStateDetached:webState];
 }
 
 - (void)webStateList:(WebStateList*)webStateList
@@ -201,22 +106,6 @@ void FaviconDriverObserverBridge::OnFaviconUpdated(
   DCHECK_GE(atIndex, 0);
   [_tabModelObservers tabModel:_tabModel
                  willRemoveTab:LegacyTabHelper::GetTabForWebState(webState)];
-}
-
-#pragma mark Private methods
-
-- (void)webStateFaviconChanged:(web::WebState*)webState {
-  DCHECK(webState);
-  [_tabModelObservers tabModel:_tabModel
-                  didChangeTab:LegacyTabHelper::GetTabForWebState(webState)];
-}
-
-- (void)webStateInserted:(web::WebState*)webState {
-  _faviconObserver->Add(favicon::WebFaviconDriver::FromWebState(webState));
-}
-
-- (void)webStateDetached:(web::WebState*)webState {
-  _faviconObserver->Remove(favicon::WebFaviconDriver::FromWebState(webState));
 }
 
 @end
