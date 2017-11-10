4194f4222b809dadf11a1c1713022004aff64e66
Lily Houghton <lilyhoughton@chromium.org>
2017-11-10 05:37:59 +0000

    [Cronet] Add CronetMetricsDelegate and add/remove methods to Cronet
    This adds a CronetMetricsDelegate interface, as well as addMetricsDelegate
    and removeMetricsDelegate methods, which are used to register/unregister
    metrics callbacks.
    
    Currently the callback is never actually called; this is simply defining the
    interface and registration methods.
    
    Bug: 702796
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I0a3933bf623265d675e3a10ef5ed25149c89e101
    Reviewed-on: https://chromium-review.googlesource.com/748819
    Commit-Queue: Lily Houghton <lilyhoughton@chromium.org>
    Reviewed-by: Andrei Kapishnikov <kapishnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515456}
    Former-commit-id: adfd430821e51b6d8fa9d3d7e693b6b246c45fb0

--- components/cronet/ios/test/cronet_metrics_test.mm
+++ components/cronet/ios/test/cronet_metrics_test.mm
@@ -6,11 +6,26 @@
 
 #include "components/cronet/ios/cronet_metrics.h"
 #include "components/cronet/ios/test/cronet_test_base.h"
+
 #include "testing/gtest_mac.h"
+
+@interface TestMetricsDelegate : NSObject<CronetMetricsDelegate>
+@end
+
+@implementation TestMetricsDelegate
+- (void)URLSession:(NSURLSession*)session task:(NSURLSessionTask*)task
+                    didFinishCollectingMetrics:(NSURLSessionTaskMetrics*)metrics
+    NS_AVAILABLE_IOS(10.0) {
+  // This is never actually called, so its definition currently doesn't matter.
+}
+@end
 
 namespace cronet {
 
 class CronetMetricsTest : public CronetTestBase {
+ protected:
+  CronetMetricsTest() {}
+  ~CronetMetricsTest() override {}
 };
 
 TEST_F(CronetMetricsTest, Setters) {
@@ -69,6 +84,17 @@ TEST_F(CronetMetricsTest, Setters) {
     EXPECT_EQ([metrics isReusedConnection], YES);
     EXPECT_EQ([metrics resourceFetchType],
               NSURLSessionTaskMetricsResourceFetchTypeNetworkLoad);
+  }
+}
+
+TEST_F(CronetMetricsTest, Delegate) {
+  if (@available(iOS 10, *)) {
+    TestMetricsDelegate* metrics_delegate =
+        [[TestMetricsDelegate alloc] init];
+    EXPECT_TRUE([Cronet addMetricsDelegate:metrics_delegate]);
+    EXPECT_FALSE([Cronet addMetricsDelegate:metrics_delegate]);
+    EXPECT_TRUE([Cronet removeMetricsDelegate:metrics_delegate]);
+    EXPECT_FALSE([Cronet removeMetricsDelegate:metrics_delegate]);
   }
 }
 
