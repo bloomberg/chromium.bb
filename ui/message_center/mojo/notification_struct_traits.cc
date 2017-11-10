cd3b07f9d7bdba0154c89815e6b1315caaf9447e
Findit <findit-for-me@appspot.gserviceaccount.com>
2017-11-10 04:17:32 +0000

    Revert "Serialize more of message_center::Notification for mojo."
    This reverts commit 4c82f0930401ae9bde5ec5b30f5bbed660526077 [formerly c4ea951f36ea1c1960d5694942da99c37c6817af].
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 515427 as the
    culprit for failures in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2M0ZWE5NTFmMzZlYTFjMTk2MGQ1Njk0OTQyZGE5OWMzN2M2ODE3YWYM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium/Mac/34475
    
    Original change's description:
    > Serialize more of message_center::Notification for mojo.
    > 
    > Adds NotificationType and (partial) RichNotificationData. Now you can
    > see progress bars in download item notifications on Chrome OS when
    > using --enable-features=NativeNotifications
    > 
    > Bug: 578868
    > Change-Id: I781c10387479ed921f0a7b4efce262b18f04844f
    > Reviewed-on: https://chromium-review.googlesource.com/755317
    > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Commit-Queue: Evan Stade <estade@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#515427}
    
    Change-Id: I3eac7eb2400781c22fd59c8f6eb7fc2cdd80e1b3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 578868
    Reviewed-on: https://chromium-review.googlesource.com/762881
    Cr-Commit-Position: refs/heads/master@{#515450}
    Former-commit-id: ebb92497e38ff5308f0ad678aabd980b84c1f746

--- ui/message_center/mojo/notification_struct_traits.cc
+++ ui/message_center/mojo/notification_struct_traits.cc
@@ -10,136 +10,61 @@
 
 namespace mojo {
 
-using message_center::mojom::NotificationDataView;
-using message_center::mojom::RichNotificationDataDataView;
-using message_center::Notification;
-using message_center::RichNotificationData;
-using NotificationStructTraits =
-    StructTraits<NotificationDataView, Notification>;
-using RichNotificationDataStructTraits =
-    StructTraits<RichNotificationDataDataView, RichNotificationData>;
-
 // static
-int RichNotificationDataStructTraits::progress(
-    const message_center::RichNotificationData& r) {
-  return r.progress;
-}
-
-// static
-const base::string16& RichNotificationDataStructTraits::progress_status(
-    const message_center::RichNotificationData& r) {
-  return r.progress_status;
-}
-
-// static
-bool RichNotificationDataStructTraits::
-    should_make_spoken_feedback_for_popup_updates(
-        const message_center::RichNotificationData& r) {
-  return r.should_make_spoken_feedback_for_popup_updates;
-}
-
-// static
-bool RichNotificationDataStructTraits::clickable(
-    const message_center::RichNotificationData& r) {
-  return r.clickable;
-}
-
-// static
-bool RichNotificationDataStructTraits::pinned(
-    const message_center::RichNotificationData& r) {
-  return r.pinned;
-}
-
-// static
-const base::string16& RichNotificationDataStructTraits::accessible_name(
-    const message_center::RichNotificationData& r) {
-  return r.accessible_name;
-}
-
-// static
-SkColor RichNotificationDataStructTraits::accent_color(
-    const message_center::RichNotificationData& r) {
-  return r.accent_color;
-}
-
-// static
-bool RichNotificationDataStructTraits::use_image_as_icon(
-    const message_center::RichNotificationData& r) {
-  return r.use_image_as_icon;
-}
-
-// static
-bool RichNotificationDataStructTraits::Read(RichNotificationDataDataView data,
-                                            RichNotificationData* out) {
-  out->progress = data.progress();
-  out->should_make_spoken_feedback_for_popup_updates =
-      data.should_make_spoken_feedback_for_popup_updates();
-  out->clickable = data.clickable();
-  out->pinned = data.pinned();
-  out->accent_color = data.accent_color();
-  out->use_image_as_icon = data.use_image_as_icon();
-  return data.ReadProgressStatus(&out->progress_status) &&
-         data.ReadAccessibleName(&out->accessible_name);
-}
-
-// static
-message_center::NotificationType NotificationStructTraits::type(
-    const Notification& n) {
-  return n.type();
-}
-
-// static
-const std::string& NotificationStructTraits::id(const Notification& n) {
+const std::string& StructTraits<
+    message_center::mojom::NotificationDataView,
+    message_center::Notification>::id(const message_center::Notification& n) {
   return n.id();
 }
 
 // static
-const base::string16& NotificationStructTraits::title(const Notification& n) {
+const base::string16& StructTraits<message_center::mojom::NotificationDataView,
+                                   message_center::Notification>::
+    title(const message_center::Notification& n) {
   return n.title();
 }
 
 // static
-const base::string16& NotificationStructTraits::message(const Notification& n) {
+const base::string16& StructTraits<message_center::mojom::NotificationDataView,
+                                   message_center::Notification>::
+    message(const message_center::Notification& n) {
   return n.message();
 }
 
 // static
-gfx::ImageSkia NotificationStructTraits::icon(const Notification& n) {
+gfx::ImageSkia StructTraits<
+    message_center::mojom::NotificationDataView,
+    message_center::Notification>::icon(const message_center::Notification& n) {
   return n.icon().AsImageSkia();
 }
 
 // static
-const base::string16& NotificationStructTraits::display_source(
-    const Notification& n) {
+const base::string16& StructTraits<message_center::mojom::NotificationDataView,
+                                   message_center::Notification>::
+    display_source(const message_center::Notification& n) {
   return n.display_source();
 }
 
 // static
-const GURL& NotificationStructTraits::origin_url(const Notification& n) {
+const GURL& StructTraits<message_center::mojom::NotificationDataView,
+                         message_center::Notification>::
+    origin_url(const message_center::Notification& n) {
   return n.origin_url();
 }
 
 // static
-const RichNotificationData& NotificationStructTraits::optional_fields(
-    const Notification& n) {
-  return n.rich_notification_data();
-}
-
-// static
-bool NotificationStructTraits::Read(NotificationDataView data,
-                                    Notification* out) {
+bool StructTraits<message_center::mojom::NotificationDataView,
+                  message_center::Notification>::
+    Read(message_center::mojom::NotificationDataView data,
+         message_center::Notification* out) {
   gfx::ImageSkia icon;
   if (!data.ReadIcon(&icon))
     return false;
-  out->set_icon(gfx::Image(icon));
-  return EnumTraits<message_center::mojom::NotificationType,
-                    message_center::NotificationType>::FromMojom(data.type(),
-                                                                 &out->type_) &&
-         data.ReadId(&out->id_) && data.ReadTitle(&out->title_) &&
+  out->icon_ = gfx::Image(icon);
+  return data.ReadId(&out->id_) && data.ReadTitle(&out->title_) &&
          data.ReadMessage(&out->message_) &&
          data.ReadDisplaySource(&out->display_source_) &&
-         data.ReadOriginUrl(&out->origin_url_) &&
-         data.ReadOptionalFields(&out->optional_fields_);
+         data.ReadOriginUrl(&out->origin_url_);
 }
 
 }  // namespace mojo
