0e751a43e6af57559e47667b3a6584262a1b17eb
Yutaka Hirano <yhirano@chromium.org>
2017-11-10 15:52:41 +0000

    [Streams] Fix crash when cancel is called after ContextDestroyed
    Bug: 757378, 783392
    Change-Id: Iab45582c907cb8613dd66e6d1fd635eeed3ece86
    Reviewed-on: https://chromium-review.googlesource.com/763151
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515560}
    Former-commit-id: af7c874f836329ae36d5db9af61a9157cd96c6d7

--- third_party/WebKit/Source/modules/fetch/BodyStreamBuffer.cpp
+++ third_party/WebKit/Source/modules/fetch/BodyStreamBuffer.cpp
@@ -225,7 +225,9 @@ ScriptPromise BodyStreamBuffer::pull(ScriptState* script_state) {
 ScriptPromise BodyStreamBuffer::Cancel(ScriptState* script_state,
                                        ScriptValue reason) {
   DCHECK_EQ(script_state, script_state_.get());
-  Close();
+  if (Controller())
+    Controller()->Close();
+  CancelConsumer();
   return ScriptPromise::CastUndefined(script_state);
 }
 
