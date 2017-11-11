c02cb5db25df2961ba4e39aa6fee03f982a769b1
Eugene But <eugenebut@google.com>
2017-11-11 01:55:58 +0000

    Use Fake name instead of Test for ios/web/test/fakes classes.
    Bug: 688063
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: If9b10b4e9ab8b975dbce21aec6cf287ef82a6ab9
    Reviewed-on: https://chromium-review.googlesource.com/759301
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515788}
    Former-commit-id: d20a96abc5584eff170e6ab1088084a0f35e3b1e

--- ios/web/net/crw_ssl_status_updater_unittest.mm
+++ ios/web/net/crw_ssl_status_updater_unittest.mm
@@ -14,7 +14,7 @@
 #import "ios/web/public/navigation_item.h"
 #include "ios/web/public/ssl_status.h"
 #include "ios/web/public/test/web_test.h"
-#import "ios/web/test/fakes/test_navigation_manager_delegate.h"
+#import "ios/web/test/fakes/fake_navigation_manager_delegate.h"
 #import "ios/web/web_state/wk_web_view_security_util.h"
 #include "net/cert/x509_util_ios_and_mac.h"
 #include "net/test/cert_test_util.h"
@@ -136,7 +136,7 @@ class CRWSSLStatusUpdaterTest : public web::WebTest {
   CRWSSLStatusUpdaterTestDataSource* data_source_;
   id delegate_;
   std::unique_ptr<web::NavigationManagerImpl> nav_manager_;
-  TestNavigationManagerDelegate nav_delegate_;
+  FakeNavigationManagerDelegate nav_delegate_;
   CRWSSLStatusUpdater* ssl_status_updater_;
   base::ScopedCFTypeRef<SecTrustRef> trust_;
 };
