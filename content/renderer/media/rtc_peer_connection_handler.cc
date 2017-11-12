d2d90fdc948d29f6dc7f083bf938bbe81fafd1cd
Benjamin Joyce <bjoyce@google.com>
2017-11-12 21:52:50 +0000

    Revert "Unregister RTCPeerConnectionHandler upon native PC's Close()"
    This reverts commit 9723a4cac189e28bceb0163fbf7ae6dd3669ecd2 [formerly d138e38ab67e6badaacbd041dcdd67eeeaadd735].
    
    Reason for revert: assessing if cause for https://bugs.chromium.org/p/chromium/issues/detail?id=783010
    
    Original change's description:
    > Unregister RTCPeerConnectionHandler upon native PC's Close()
    > 
    > The existing code only unregisters the handler it is destructed. 
    > However, garbage collection might not destroy the object for a while.
    > 
    > Bug: 780855
    > Change-Id: I68d2c1b49cc5042d53464878977400bfd3b75c81
    > Reviewed-on: https://chromium-review.googlesource.com/754838
    > Commit-Queue: Elad Alon <eladalon@chromium.org>
    > Reviewed-by: Henrik Boström <hbos@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#514466}
    
    TBR=hbos@chromium.org,guidou@chromium.org,terelius@chromium.org,eladalon@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 780855
    Change-Id: I567bfb7f5aa7f2658b1dbee06a3fe5b66f3dec4b
    Reviewed-on: https://chromium-review.googlesource.com/765147
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Benjamin Joyce <bjoyce@google.com>
    Cr-Commit-Position: refs/heads/master@{#515870}
    Former-commit-id: 443c81f945a354dd3b11d220895b9c98009552f0

--- content/renderer/media/rtc_peer_connection_handler.cc
+++ content/renderer/media/rtc_peer_connection_handler.cc
@@ -1199,6 +1199,10 @@ RTCPeerConnectionHandler::~RTCPeerConnectionHandler() {
 
   Stop();
 
+  GetPeerConnectionHandlers()->erase(this);
+  if (peer_connection_tracker_)
+    peer_connection_tracker_->UnregisterPeerConnection(this);
+
   UMA_HISTOGRAM_COUNTS_10000(
       "WebRTC.NumDataChannelsPerPeerConnection", num_data_channels_created_);
 }
@@ -1923,11 +1927,6 @@ void RTCPeerConnectionHandler::Stop() {
     peer_connection_tracker_->TrackStop(this);
 
   native_peer_connection_->Close();
-
-  GetPeerConnectionHandlers()->erase(this);
-  if (peer_connection_tracker_)
-    peer_connection_tracker_->UnregisterPeerConnection(this);
-  peer_connection_tracker_ = nullptr;
 
   // This object may no longer forward call backs to blink.
   is_closed_ = true;
