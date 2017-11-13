4e6ab7a0a71c7c2e36cb34aa8f18d6870a65ad49
Lei Tian <ltian@chromium.org>
2017-11-13 20:50:32 +0000

    Chrome retrieves Browser Actions' custom item icons through resource ids
    Browser Actions will pass only the resource ids of the custom items'
    icons through the Intent, instead of passing the whole Bitmaps. Then
    with the source package name, Chrome can retrieve the client app's
    Resources by PackageManager.getResourcesForApplication() and use it to
    get the icons.
    
    Bug: 775798
    Change-Id: Ia63f07a6245673df63e71b550ffff2f63753964f
    Reviewed-on: https://chromium-review.googlesource.com/758745
    Commit-Queue: Lei Tian <ltian@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516036}
    Former-commit-id: fa849f85fa78faa965b2b392a9b0428ef743893c

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabsTestUtils.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabsTestUtils.java
@@ -32,7 +32,7 @@ public class CustomTabsTestUtils {
     public static Intent createMinimalCustomTabIntent(
             Context context, String url) {
         CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder(
-                CustomTabsSession.createDummySessionForTesting(
+                CustomTabsSession.createMockSessionForTesting(
                         new ComponentName(context, ChromeLauncherActivity.class)));
         CustomTabsIntent customTabsIntent = builder.build();
         Intent intent = customTabsIntent.intent;
