1c1c881a0c4041ea149756f28df665510a96370c
Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com>
2017-11-13 10:42:41 +0000

    [sensors] Check shared buffer mapping for PlatformSensorFusion
    This patch adds the missing DCHECK(mapping) to the PlatformSensorFusion::Factory
    constructor, and fixes implementation of FakePlatformSensorProvider::GetMapping()
    which otherwise caused the assertion hit on some test cases.
    
    Change-Id: I6bd2d17c66bcc2bfed766a3a3cbc0ca7c5c846f9
    Reviewed-on: https://chromium-review.googlesource.com/765487
    Reviewed-by: Alexander Shalamov <alexander.shalamov@intel.com>
    Commit-Queue: Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com>
    Cr-Commit-Position: refs/heads/master@{#515918}
    Former-commit-id: ea61b184ba69d22618dd74bc67dfc313498ede1b

--- services/device/generic_sensor/platform_sensor_fusion.cc
+++ services/device/generic_sensor/platform_sensor_fusion.cc
@@ -40,6 +40,7 @@ class PlatformSensorFusion::Factory : public base::RefCounted<Factory> {
     // Make sure there are no dups.
     DCHECK(std::adjacent_find(types.begin(), types.end()) == types.end());
     DCHECK(result_callback_);
+    DCHECK(mapping_);
     DCHECK(provider_);
   }
 
@@ -69,8 +70,8 @@ class PlatformSensorFusion::Factory : public base::RefCounted<Factory> {
       std::move(result_callback_).Run(nullptr);
       return;
     }
-
-    sources_map_[sensor->GetType()] = std::move(sensor);
+    mojom::SensorType type = sensor->GetType();
+    sources_map_[type] = std::move(sensor);
     if (sources_map_.size() == fusion_algorithm_->source_types().size()) {
       scoped_refptr<PlatformSensor> fusion_sensor(new PlatformSensorFusion(
           std::move(mapping_), provider_, std::move(fusion_algorithm_),
