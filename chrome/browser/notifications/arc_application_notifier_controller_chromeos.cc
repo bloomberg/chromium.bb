dbef8206d44a47e4337f0e46df801f585cb371c4
yoshiki iguchi <yoshiki@chromium.org>
2017-11-13 09:58:44 +0000

    Prevent crash on ArcApplicationNotifierController on shutdown
    The clean-up code touched a profile after the profiles were destroyed. This
    patch fixes this issue.
    
    Bug: 783538
    Test: none
    Change-Id: If010a424e0fbcac2188eb5546c1e4917214dcbc2
    Reviewed-on: https://chromium-review.googlesource.com/765635
    Reviewed-by: Daichi Hirono <hirono@chromium.org>
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515908}
    Former-commit-id: 967c6b4b9531d82ed8e5f700cd7fe97d4ae8fa04

--- chrome/browser/notifications/arc_application_notifier_controller_chromeos.cc
+++ chrome/browser/notifications/arc_application_notifier_controller_chromeos.cc
@@ -7,7 +7,9 @@
 #include <set>
 
 #include "base/strings/utf_string_conversions.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "ui/base/layout.h"
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
@@ -110,8 +112,14 @@ void ArcApplicationNotifierControllerChromeOS::OnIconUpdated(ArcAppIcon* icon) {
 void ArcApplicationNotifierControllerChromeOS::StopObserving() {
   if (!last_profile_)
     return;
-  ArcAppListPrefs* const app_list = ArcAppListPrefs::Get(last_profile_);
-  app_list->RemoveObserver(this);
+
+  // This is also called from the destructor during shutdown. In that case, the
+  // profile manager and the profiles are already destroyed. (see Issue 783538)
+  if (g_browser_process->profile_manager() &&
+      g_browser_process->profile_manager()->IsValidProfile(last_profile_)) {
+    ArcAppListPrefs* const app_list = ArcAppListPrefs::Get(last_profile_);
+    app_list->RemoveObserver(this);
+  }
   last_profile_ = nullptr;
 }
 
