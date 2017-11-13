7ba22035727a26e3b41508bc6d98abad1a724b73
Rune Lillesveen <futhark@chromium.org>
2017-11-13 23:36:24 +0000

    Update WhitespaceLayoutObjects.md documentation.
    Corrected documentation for attaching LayoutObjects to whitespace text
    nodes after landing crrev.com/18c851b1540c54a74a36fb9493af0059d4aa7151 [formerly 7c44da721a59e6aa0b9fdcddb314175cb1e0123f]
    
    Bug: 756340
    Change-Id: I1ed807b170f613f6b7b7dfed50b58b121af0b564
    Reviewed-on: https://chromium-review.googlesource.com/766349
    Reviewed-by: nainar <nainar@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516098}
    Former-commit-id: 6396abe0f83f7e76cb0b4ac3090ab3ec2877b07c

--- third_party/WebKit/Source/core/dom/WhitespaceAttacher.h
+++ third_party/WebKit/Source/core/dom/WhitespaceAttacher.h
@@ -18,7 +18,6 @@ class Text;
 // attach whitespace LayoutObjects when necessary. For more details about white-
 // space LayoutObjects, see the WhitespaceLayoutObjects.md file in this
 // directory.
-// TODO(rune@opera.com): Update WhitespaceLayoutObjects.md documentation.
 //
 // As RebuildLayoutTree walks from last to first child, we track the last text
 // node, or the last skipped display:contents element we have seen. These are
