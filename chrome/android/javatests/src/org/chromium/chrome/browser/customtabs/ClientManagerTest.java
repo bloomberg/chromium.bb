ed477056cff1c1f79a174df1cb27d1ee336df07f
Lei Tian <ltian@chromium.org>
2017-11-13 20:50:32 +0000

    Chrome retrieves Browser Actions' custom item icons through resource ids
    Browser Actions will pass only the resource ids of the custom items'
    icons through the Intent, instead of passing the whole Bitmaps. Then
    with the source package name, Chrome can retrieve the client app's
    Resources by PackageManager.getResourcesForApplication() and use it to
    get the icons.
    
    Bug: 775798
    Change-Id: Ia63f07a6245673df63e71b550ffff2f63753964f
    Reviewed-on: https://chromium-review.googlesource.com/758745
    Commit-Queue: Lei Tian <ltian@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516036}
    Former-commit-id: fa849f85fa78faa965b2b392a9b0428ef743893c

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/ClientManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/ClientManagerTest.java
@@ -42,7 +42,7 @@ public class ClientManagerTest {
 
     private ClientManager mClientManager;
     private CustomTabsSessionToken mSession =
-            CustomTabsSessionToken.createDummySessionTokenForTesting();
+            CustomTabsSessionToken.createMockSessionTokenForTesting();
     private int mUid = Process.myUid();
 
     @Before
@@ -123,7 +123,7 @@ public class ClientManagerTest {
         mClientManager.newSession(mSession, mUid, null, null);
         Assert.assertEquals(ClientManager.SESSION_WARMUP, mClientManager.getWarmupState(mSession));
 
-        CustomTabsSessionToken token = CustomTabsSessionToken.createDummySessionTokenForTesting();
+        CustomTabsSessionToken token = CustomTabsSessionToken.createMockSessionTokenForTesting();
         mClientManager.newSession(token, mUid, null, null);
         Assert.assertEquals(ClientManager.SESSION_WARMUP, mClientManager.getWarmupState(token));
     }
