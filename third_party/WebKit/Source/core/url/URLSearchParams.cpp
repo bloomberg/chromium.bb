a84fa02d2304408c3b7eb9d65960ce95c4d9a8e9
Yutaka Hirano <yhirano@chromium.org>
2017-11-13 13:14:38 +0000

    [URL] Do not run update steps when URLSearchParam is created
    See https://github.com/whatwg/url/issues/350 and
    https://github.com/whatwg/url/issues/18.
    
    Bug: 776344
    Change-Id: I7881add2fc5b858874da42cef7b14a9e8ee10262
    Reviewed-on: https://chromium-review.googlesource.com/756537
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Takeshi Yoshino <tyoshino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#515937}
    Former-commit-id: 45d0c725191542163217bbea46916e7d5e75830a

--- third_party/WebKit/Source/core/url/URLSearchParams.cpp
+++ third_party/WebKit/Source/core/url/URLSearchParams.cpp
@@ -82,14 +82,13 @@ URLSearchParams* URLSearchParams::Create(const Vector<Vector<String>>& init,
     }
     instance->AppendWithoutUpdate(pair[0], pair[1]);
   }
-  instance->RunUpdateSteps();
   return instance;
 }
 
 URLSearchParams::URLSearchParams(const String& query_string, DOMURL* url_object)
     : url_object_(url_object) {
   if (!query_string.IsEmpty())
-    SetInput(query_string);
+    SetInputWithoutUpdate(query_string);
 }
 
 URLSearchParams* URLSearchParams::Create(
@@ -100,7 +99,6 @@ URLSearchParams* URLSearchParams::Create(
     return instance;
   for (const auto& item : init)
     instance->AppendWithoutUpdate(item.first, item.second);
-  instance->RunUpdateSteps();
   return instance;
 }
 
@@ -131,7 +129,7 @@ static String DecodeString(String input) {
   return DecodeURLEscapeSequences(input.Replace('+', ' '));
 }
 
-void URLSearchParams::SetInput(const String& query_string) {
+void URLSearchParams::SetInputWithoutUpdate(const String& query_string) {
   params_.clear();
 
   size_t start = 0;
@@ -157,7 +155,6 @@ void URLSearchParams::SetInput(const String& query_string) {
     }
     start = name_value_end + 1;
   }
-  RunUpdateSteps();
 }
 
 String URLSearchParams::toString() const {
