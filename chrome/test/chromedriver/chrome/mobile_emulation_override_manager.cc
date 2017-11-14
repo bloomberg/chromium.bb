ecef646c73dfce495117d0fe48db251deb4774b3
John Chen <johnchen@chromium.org>
2017-11-14 23:22:40 +0000

    [ChromeDriver] Fix mobile emulation on v61
    https://crrev.com/516159 broke mobile emulation on Chrome v61, since it
    calls unavailable DevTools method Emulation.setEmitTouchEventsForMouse.
    Temporarily allow this error until ChromeDriver stops supporting v61.
    
    Bug: 784284, chromedriver:2103
    Change-Id: I3cf2c38164512632e717ed0b5d02eb9da0664ae3
    Reviewed-on: https://chromium-review.googlesource.com/769668
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516492}
    Former-commit-id: ad567264b2cdf5c758a33f52b784cf088fbdff17

--- chrome/test/chromedriver/chrome/mobile_emulation_override_manager.cc
+++ chrome/test/chromedriver/chrome/mobile_emulation_override_manager.cc
@@ -73,7 +73,14 @@ Status MobileEmulationOverrideManager::ApplyOverrideIfNeeded() {
     emit_touch_for_mouse_params.SetString("configuration", "mobile");
     status = client_->SendCommand("Emulation.setEmitTouchEventsForMouse",
                                   emit_touch_for_mouse_params);
-    if (status.IsError())
+    // OK if Emulation.setEmitTouchEventsForMouse isn't available, as it was
+    // added in Chrome v62. TODO(johnchen@chromium.org): Remove check for
+    // Emulation.setEmitTouchEventsForMouse not found error when ChromeDriver
+    // stops supporting v61.
+    if (status.IsError() &&
+        status.message().find(
+            "'Emulation.setEmitTouchEventsForMouse' wasn't found") ==
+            std::string::npos)
       return status;
   }
 
