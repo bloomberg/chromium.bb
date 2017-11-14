3ac0c93df571e2034be8859d344df51803ef268a
Peter Collingbourne <pcc@chromium.org>
2017-11-14 22:10:49 +0000

    android: Have native test runner configure ubsan to print stack traces.
    This change configures ubsan to print stack traces in the format
    understood by "stack" so that they will be symbolized.
    
    The ubsan runtime library reads its configuration from $UBSAN_OPTIONS
    when the library is loaded, so we need to set the environment
    variable before the test runner loads any native libraries. I'm open
    to suggestions on better places to do this.
    
    Bug: 469376
    Change-Id: I166f601ac5b9de6f2c33af8d5405dfd14c0c2961
    Reviewed-on: https://chromium-review.googlesource.com/762125
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Commit-Queue: Peter Collingbourne <pcc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516452}
    Former-commit-id: c3e01aae1bebb26144856a35db270a5bae473fcc

--- testing/android/native_test/java/src/org/chromium/native_test/NativeUnitTest.java
+++ testing/android/native_test/java/src/org/chromium/native_test/NativeUnitTest.java
@@ -5,6 +5,8 @@
 package org.chromium.native_test;
 
 import android.app.Activity;
+import android.os.Build;
+import android.system.Os;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
@@ -30,6 +32,21 @@ public class NativeUnitTest extends NativeTest {
 
         // Needed by system_monitor_unittest.cc
         PowerMonitor.createForTests();
+
+        // Configure ubsan to print stack traces in the format understood by "stack" so that they
+        // will be symbolized. This needs to happen here because ubsan reads its configuration from
+        // $UBSAN_OPTIONS when the native library is loaded.
+        //
+        // The setenv API was added in L. On older versions of Android, we should still see ubsan
+        // reports, but they will not have stack traces.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            try {
+                Os.setenv("UBSAN_OPTIONS", "print_stacktrace=1:stack_trace_format='#%n pc %o %m'",
+                        true);
+            } catch (Exception e) {
+                Log.w(TAG, "failed to set UBSAN_OPTIONS", e);
+            }
+        }
 
         // For NativeActivity based tests,
         // dependency libraries must be loaded before NativeActivity::OnCreate,
