25b13c0a54a171203b43fa7859bca4719f738031
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2017-11-14 23:29:22 +0000

    Allow chrome:// scheme in partner homepage setting
    chrome://newtab is also a very common partner homepage setting, so
    add it to the allowed schemes.
    
    Robolectric unit tests are added.
    
    Bug: 783048
    Change-Id: I218e0a1c26222b7c3d91b6418c5f9efa96f565e6
    Reviewed-on: https://chromium-review.googlesource.com/769235
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516494}
    Former-commit-id: fa64efdaee5858e87195ae1efc752d50ace2a3fb

---
+++ chrome/android/junit/src/org/chromium/chrome/browser/partnercustomizations/PartnerBrowserCustomizationsUnitTest.java
@@ -0,0 +1,35 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.partnercustomizations;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+
+import org.chromium.base.test.util.Feature;
+import org.chromium.testing.local.LocalRobolectricTestRunner;
+
+/**
+ * Unit tests for {@link PartnerBrowserCustomizations}.
+ */
+@RunWith(LocalRobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public class PartnerBrowserCustomizationsUnitTest {
+    @Test
+    @Feature({"Homepage"})
+    public void testIsValidHomepage() {
+        Assert.assertTrue(PartnerBrowserCustomizations.isValidHomepage("chrome://newtab"));
+        Assert.assertTrue(PartnerBrowserCustomizations.isValidHomepage("http://example.com"));
+        Assert.assertTrue(PartnerBrowserCustomizations.isValidHomepage("https:example.com"));
+
+        Assert.assertFalse(PartnerBrowserCustomizations.isValidHomepage("example.com"));
+        Assert.assertFalse(PartnerBrowserCustomizations.isValidHomepage(
+                "content://com.android.providers.media.documents/document/video:113"));
+        Assert.assertFalse(PartnerBrowserCustomizations.isValidHomepage("ftp://example.com"));
+        Assert.assertFalse(PartnerBrowserCustomizations.isValidHomepage(""));
+        Assert.assertFalse(PartnerBrowserCustomizations.isValidHomepage(null));
+    }
+}
