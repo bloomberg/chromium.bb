52c3ead9098439f5ba51f35fee638e92ba21b038
Joel Einbinder <einbinder@chromium.org>
2017-11-15 03:15:33 +0000

    DevTools: Migrate some elements tests
    Bug: 667560
    Change-Id: I8817cabc284c61c6cf25684bffaf5038ef37132f
    Reviewed-on: https://chromium-review.googlesource.com/767847
    Commit-Queue: Joel Einbinder <einbinder@chromium.org>
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Reviewed-by: Will Chen <chenwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516580}
    Former-commit-id: 3f049c669d944abd65701ed4b7d613b39a00f32d

--- third_party/WebKit/LayoutTests/http/tests/devtools/elements/elements-panel-rewrite-href.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/elements/elements-panel-rewrite-href.js
@@ -1,13 +1,18 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<link rel="stylesheet" src="/bogusSheet1.css">
-<link rel="stylesheet" src="bogusSheet2.css">
-<script src="../../inspector/inspector-test.js"></script>
-<script src="../../inspector/elements-test.js"></script>
-<script>
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-function test() {
+(async function() {
+  TestRunner.addResult(`Tests that src and href element targets are rewritten properly.\n`);
+  await TestRunner.loadModule('elements_test_runner');
+  await TestRunner.showPanel('elements');
+  await TestRunner.loadHTML(`
+      <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+      <a style="display:none" href=" javascript:alert('foo') "></a>
+    `);
+
+  await TestRunner.addIframe('resources/elements-panel-rewrite-href-iframe.html');
+
   ElementsTestRunner.expandElementsTree(step1);
 
   function step1() {
@@ -44,17 +49,4 @@ function test() {
     TestRunner.addResult(outputLines.join('\n'));
     TestRunner.completeTest();
   }
-}
-
-</script>
-</head>
-
-<body>
-<p>
-Tests that src and href element targets are rewritten properly.
-</p>
-
-<a style="display:none" href=" javascript:alert('foo') "></a>
-<iframe src="resources/elements-panel-rewrite-href-iframe.html" onload="runTest()"></iframe>
-</body>
-</html>
+})();
