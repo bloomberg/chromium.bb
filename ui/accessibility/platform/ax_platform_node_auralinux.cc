ac1703ca9c35ca6ea26fc35cbbbbb0b171132fca
Dirk Pranke <dpranke@chromium.org>
2017-11-15 02:12:17 +0000

    Revert "A11y: prepare AXPlatformNodeAuraLinux to implement multiple Atk interfaces."
    This reverts commit ad5c5cf5177632f9bfb1c8544f80f19dcce77c56 [formerly 27e815da65ec253132b1ca60883a576f5ff0785d].
    
    Reason for revert: This appears to cause the system mesa (GL) library to get loaded at runtime, which breaks the layout tests when run under MSAN :(. See crbug.com/785106. 
    
    Original change's description:
    > A11y: prepare AXPlatformNodeAuraLinux to implement multiple Atk interfaces.
    > 
    > As a first step towards moving all the ATK object implementation to
    > ui/accessibility, we will make AXPlatformNodeAuraLinux implement as much
    > as possible of the features implemented only in
    > BrowserAccessibilityAuraLinux ATK object.
    > 
    > This patch prepares AtkPlatformNodeAuraLinux to load different Atk
    > interfaces.
    > 
    > Bug: 784583
    > Change-Id: I056ebcc6112743b99b1bc5017dbb226b3f5cbe60
    > Reviewed-on: https://chromium-review.googlesource.com/767427
    > Commit-Queue: José Dapena Paz <jose.dapena@lge.com>
    > Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#516429}
    
    TBR=dmazzoni@chromium.org,jdapena@gmail.com,jose.dapena@lge.com
    
    Change-Id: I62466e84a96902873aecb18bfcba233aec6a6c96
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 784583
    Reviewed-on: https://chromium-review.googlesource.com/770452
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Dirk Pranke <dpranke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516563}
    Former-commit-id: 8e1966b926d5aa94ae21a22c9c026d9fc7449292

--- ui/accessibility/platform/ax_platform_node_auralinux.cc
+++ ui/accessibility/platform/ax_platform_node_auralinux.cc
@@ -1,4 +1,4 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
+// Copyright 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -73,11 +73,6 @@ static const gchar* ax_platform_node_auralinux_get_name(AtkObject* atk_object) {
   ui::AXPlatformNodeAuraLinux* obj =
       AtkObjectToAXPlatformNodeAuraLinux(atk_object);
   if (!obj)
-    return nullptr;
-
-  if (obj->GetStringAttribute(ui::AX_ATTR_NAME).empty() &&
-      !(obj->GetIntAttribute(ui::AX_ATTR_NAME_FROM) ==
-        ui::AX_NAME_FROM_ATTRIBUTE_EXPLICITLY_EMPTY))
     return nullptr;
 
   return obj->GetStringAttribute(ui::AX_ATTR_NAME).c_str();
@@ -322,30 +317,43 @@ static void ax_platform_node_auralinux_class_init(AtkObjectClass* klass) {
 GType ax_platform_node_auralinux_get_type() {
   static volatile gsize type_volatile = 0;
 
-#if !GLIB_CHECK_VERSION(2, 36, 0)
-  g_type_init();
-#endif
-
   if (g_once_init_enter(&type_volatile)) {
     static const GTypeInfo tinfo = {
-        sizeof(AXPlatformNodeAuraLinuxClass),
-        (GBaseInitFunc) nullptr,
-        (GBaseFinalizeFunc) nullptr,
-        (GClassInitFunc)ax_platform_node_auralinux_class_init,
-        (GClassFinalizeFunc) nullptr,
-        nullptr,                               /* class data */
-        sizeof(AXPlatformNodeAuraLinuxObject), /* instance size */
-        0,                                     /* nb preallocs */
-        (GInstanceInitFunc) nullptr,
-        nullptr /* value table */
+      sizeof(AXPlatformNodeAuraLinuxClass),
+      (GBaseInitFunc) 0,
+      (GBaseFinalizeFunc) 0,
+      (GClassInitFunc) ax_platform_node_auralinux_class_init,
+      (GClassFinalizeFunc) 0,
+      0, /* class data */
+      sizeof(AXPlatformNodeAuraLinuxObject), /* instance size */
+      0, /* nb preallocs */
+      (GInstanceInitFunc) 0,
+      0 /* value table */
     };
 
     GType type = g_type_register_static(
         ATK_TYPE_OBJECT, "AXPlatformNodeAuraLinux", &tinfo, GTypeFlags(0));
+    g_type_add_interface_static(type, ATK_TYPE_COMPONENT, &ComponentInfo);
     g_once_init_leave(&type_volatile, type);
   }
 
   return type_volatile;
+}
+
+AXPlatformNodeAuraLinuxObject* ax_platform_node_auralinux_new(
+    ui::AXPlatformNodeAuraLinux* obj) {
+  #if !GLIB_CHECK_VERSION(2, 36, 0)
+  static bool first_time = true;
+  if (first_time) {
+    g_type_init();
+    first_time = false;
+  }
+  #endif
+
+  GType type = AX_PLATFORM_NODE_AURALINUX_TYPE;
+  AtkObject* atk_object = static_cast<AtkObject*>(g_object_new(type, 0));
+  atk_object_initialize(atk_object, obj);
+  return AX_PLATFORM_NODE_AURALINUX(atk_object);
 }
 
 void ax_platform_node_auralinux_detach(
@@ -356,88 +364,6 @@ void ax_platform_node_auralinux_detach(
 G_END_DECLS
 
 namespace ui {
-
-const char* AXPlatformNodeAuraLinux::GetUniqueAccessibilityGTypeName(
-    int interface_mask) {
-  // 37 characters is enough for "AXPlatformNodeAuraLinux%x" with any integer
-  // value.
-  static char name[37];
-  snprintf(name, sizeof(name), "AXPlatformNodeAuraLinux%x", interface_mask);
-  return name;
-}
-
-int AXPlatformNodeAuraLinux::GetGTypeInterfaceMask() {
-  int interface_mask = 0;
-
-  // Component interface is always supported.
-  interface_mask |= 1 << ATK_COMPONENT_INTERFACE;
-
-  // Action interface is basic one. It just relays on executing default action
-  // for each object.
-  interface_mask |= 1 << ATK_ACTION_INTERFACE;
-
-  // Value Interface
-  int role = GetAtkRole();
-  if (role == ui::AX_ROLE_PROGRESS_INDICATOR ||
-      role == ui::AX_ROLE_SCROLL_BAR || role == ui::AX_ROLE_SLIDER) {
-    interface_mask |= 1 << ATK_VALUE_INTERFACE;
-  }
-
-  // Document Interface
-  if (role == ui::AX_ROLE_DOCUMENT || role == ui::AX_ROLE_ROOT_WEB_AREA ||
-      role == ui::AX_ROLE_WEB_AREA)
-    interface_mask |= 1 << ATK_DOCUMENT_INTERFACE;
-
-  // Image Interface
-  if (role == ui::AX_ROLE_IMAGE || role == ui::AX_ROLE_IMAGE_MAP)
-    interface_mask |= 1 << ATK_IMAGE_INTERFACE;
-
-  return interface_mask;
-}
-
-GType AXPlatformNodeAuraLinux::GetAccessibilityGType() {
-  static const GTypeInfo type_info = {
-      sizeof(AXPlatformNodeAuraLinuxClass),
-      (GBaseInitFunc) nullptr,
-      (GBaseFinalizeFunc) nullptr,
-      (GClassInitFunc) nullptr,
-      (GClassFinalizeFunc) nullptr,
-      nullptr,                               /* class data */
-      sizeof(AXPlatformNodeAuraLinuxObject), /* instance size */
-      0,                                     /* nb preallocs */
-      (GInstanceInitFunc) nullptr,
-      nullptr /* value table */
-  };
-
-  int interface_mask = GetGTypeInterfaceMask();
-  const char* atk_type_name = GetUniqueAccessibilityGTypeName(interface_mask);
-  GType type = g_type_from_name(atk_type_name);
-  if (type)
-    return type;
-
-  type = g_type_register_static(AX_PLATFORM_NODE_AURALINUX_TYPE, atk_type_name,
-                                &type_info, GTypeFlags(0));
-  if (interface_mask & (1 << ATK_COMPONENT_INTERFACE))
-    g_type_add_interface_static(type, ATK_TYPE_COMPONENT, &ComponentInfo);
-
-  return type;
-}
-
-AtkObject* AXPlatformNodeAuraLinux::CreateAtkObject() {
-#if !GLIB_CHECK_VERSION(2, 36, 0)
-  static bool first_time = true;
-  if (first_time) {
-    g_type_init();
-    first_time = false;
-  }
-#endif
-  GType type = GetAccessibilityGType();
-  AtkObject* atk_object = static_cast<AtkObject*>(g_object_new(type, nullptr));
-
-  atk_object_initialize(atk_object, this);
-
-  return ATK_OBJECT(atk_object);
-}
 
 // static
 AXPlatformNode* AXPlatformNode::Create(AXPlatformNodeDelegate* delegate) {
@@ -473,8 +399,6 @@ AtkRole AXPlatformNodeAuraLinux::GetAtkRole() {
   switch (GetData().role) {
     case ui::AX_ROLE_ALERT:
       return ATK_ROLE_ALERT;
-    case ui::AX_ROLE_ALERT_DIALOG:
-      return ATK_ROLE_ALERT;
     case ui::AX_ROLE_APPLICATION:
       return ATK_ROLE_APPLICATION;
     case ui::AX_ROLE_AUDIO:
@@ -489,28 +413,12 @@ AtkRole AXPlatformNodeAuraLinux::GetAtkRole() {
 #endif
     case ui::AX_ROLE_BUTTON:
       return ATK_ROLE_PUSH_BUTTON;
-    case ui::AX_ROLE_CANVAS:
-      return ATK_ROLE_CANVAS;
-    case ui::AX_ROLE_CAPTION:
-      return ATK_ROLE_CAPTION;
     case ui::AX_ROLE_CHECK_BOX:
       return ATK_ROLE_CHECK_BOX;
-    case ui::AX_ROLE_COLOR_WELL:
-      return ATK_ROLE_COLOR_CHOOSER;
-    case ui::AX_ROLE_COLUMN_HEADER:
-      return ATK_ROLE_COLUMN_HEADER;
     case ui::AX_ROLE_COMBO_BOX:
       return ATK_ROLE_COMBO_BOX;
-    case ui::AX_ROLE_DATE:
-      return ATK_ROLE_DATE_EDITOR;
-    case ui::AX_ROLE_DATE_TIME:
-      return ATK_ROLE_DATE_EDITOR;
     case ui::AX_ROLE_DIALOG:
       return ATK_ROLE_DIALOG;
-    case ui::AX_ROLE_DOCUMENT:
-      return ATK_ROLE_DOCUMENT_WEB;
-    case ui::AX_ROLE_FORM:
-      return ATK_ROLE_FORM;
     case ui::AX_ROLE_GENERIC_CONTAINER:
       return ATK_ROLE_PANEL;
     case ui::AX_ROLE_GROUP:
@@ -519,18 +427,6 @@ AtkRole AXPlatformNodeAuraLinux::GetAtkRole() {
       return ATK_ROLE_REDUNDANT_OBJECT;
     case ui::AX_ROLE_IMAGE:
       return ATK_ROLE_IMAGE;
-    case ui::AX_ROLE_IMAGE_MAP:
-      return ATK_ROLE_IMAGE_MAP;
-    case ui::AX_ROLE_LABEL_TEXT:
-      return ATK_ROLE_LABEL;
-    case ui::AX_ROLE_LINK:
-      return ATK_ROLE_LINK;
-    case ui::AX_ROLE_LIST:
-      return ATK_ROLE_LIST;
-    case ui::AX_ROLE_LIST_BOX:
-      return ATK_ROLE_LIST_BOX;
-    case ui::AX_ROLE_LIST_ITEM:
-      return ATK_ROLE_LIST_ITEM;
     case ui::AX_ROLE_MATH:
 #if defined(ATK_CHECK_VERSION)
 #if ATK_CHECK_VERSION(2, 12, 0)
@@ -541,58 +437,24 @@ AtkRole AXPlatformNodeAuraLinux::GetAtkRole() {
 #else
       return ATK_ROLE_TEXT;
 #endif
-    case ui::AX_ROLE_MENU:
-      return ATK_ROLE_MENU;
-    case ui::AX_ROLE_MENU_BAR:
-      return ATK_ROLE_MENU_BAR;
     case ui::AX_ROLE_MENU_ITEM:
       return ATK_ROLE_MENU_ITEM;
-    case ui::AX_ROLE_MENU_ITEM_CHECK_BOX:
-      return ATK_ROLE_CHECK_MENU_ITEM;
-    case ui::AX_ROLE_MENU_ITEM_RADIO:
-      return ATK_ROLE_RADIO_MENU_ITEM;
-    case ui::AX_ROLE_METER:
-      return ATK_ROLE_PROGRESS_BAR;
-    case ui::AX_ROLE_PARAGRAPH:
-      return ATK_ROLE_PARAGRAPH;
-    case ui::AX_ROLE_RADIO_BUTTON:
-      return ATK_ROLE_RADIO_BUTTON;
-    case ui::AX_ROLE_ROW_HEADER:
-      return ATK_ROLE_ROW_HEADER;
-    case ui::AX_ROLE_ROOT_WEB_AREA:
-      return ATK_ROLE_DOCUMENT_WEB;
     case ui::AX_ROLE_SCROLL_BAR:
       return ATK_ROLE_SCROLL_BAR;
     case ui::AX_ROLE_SLIDER:
       return ATK_ROLE_SLIDER;
-    case ui::AX_ROLE_SPIN_BUTTON:
-      return ATK_ROLE_SPIN_BUTTON;
     case ui::AX_ROLE_SPLITTER:
       return ATK_ROLE_SEPARATOR;
     case ui::AX_ROLE_STATIC_TEXT:
       return ATK_ROLE_TEXT;
-    case ui::AX_ROLE_STATUS:
-      return ATK_ROLE_STATUSBAR;
     case ui::AX_ROLE_TAB:
       return ATK_ROLE_PAGE_TAB;
-    case ui::AX_ROLE_TABLE:
-      return ATK_ROLE_TABLE;
     case ui::AX_ROLE_TAB_LIST:
       return ATK_ROLE_PAGE_TAB_LIST;
     case ui::AX_ROLE_TEXT_FIELD:
       return ATK_ROLE_ENTRY;
-    case ui::AX_ROLE_TOGGLE_BUTTON:
-      return ATK_ROLE_TOGGLE_BUTTON;
     case ui::AX_ROLE_TOOLBAR:
       return ATK_ROLE_TOOL_BAR;
-    case ui::AX_ROLE_TOOLTIP:
-      return ATK_ROLE_TOOL_TIP;
-    case ui::AX_ROLE_TREE:
-      return ATK_ROLE_TREE;
-    case ui::AX_ROLE_TREE_ITEM:
-      return ATK_ROLE_TREE_ITEM;
-    case ui::AX_ROLE_TREE_GRID:
-      return ATK_ROLE_TREE_TABLE;
     case ui::AX_ROLE_VIDEO:
 #if defined(ATK_CHECK_VERSION)
 #if ATK_CHECK_VERSION(2, 12, 0)
@@ -603,8 +465,6 @@ AtkRole AXPlatformNodeAuraLinux::GetAtkRole() {
 #else
       return ATK_ROLE_SECTION;
 #endif
-    case ui::AX_ROLE_WEB_AREA:
-      return ATK_ROLE_DOCUMENT_WEB;
     case ui::AX_ROLE_WINDOW:
       return ATK_ROLE_WINDOW;
     default:
@@ -655,11 +515,10 @@ void AXPlatformNodeAuraLinux::GetAtkState(AtkStateSet* atk_state_set) {
       atk_state_set_add_state(atk_state_set, ATK_STATE_ENABLED);
       break;
     case ui::AX_RESTRICTION_READ_ONLY:
-#if defined(ATK_CHECK_VERSION)
-#if ATK_CHECK_VERSION(2, 16, 0)
-      atk_state_set_add_state(atk_state_set, ATK_STATE_READ_ONLY);
-#endif
-#endif
+      // The following would require ATK 2.16 or later, which many
+      // systems do not have. Since we aren't officially supporting ATK
+      // it's best to leave this out rather than break people's builds:
+      // atk_state_set_add_state(atk_state_set, ATK_STATE_READ_ONLY);
       break;
   }
 
@@ -682,7 +541,7 @@ AXPlatformNodeAuraLinux::~AXPlatformNodeAuraLinux() {
 void AXPlatformNodeAuraLinux::Init(AXPlatformNodeDelegate* delegate) {
   // Initialize ATK.
   AXPlatformNodeBase::Init(delegate);
-  atk_object_ = CreateAtkObject();
+  atk_object_ = ATK_OBJECT(ax_platform_node_auralinux_new(this));
 }
 
 gfx::NativeViewAccessible AXPlatformNodeAuraLinux::GetNativeViewAccessible() {
@@ -731,7 +590,7 @@ void AXPlatformNodeAuraLinux::GetExtents(gint* x, gint* y,
 void AXPlatformNodeAuraLinux::GetPosition(gint* x, gint* y,
                                           AtkCoordType coord_type) {
   SetExtentsRelativeToAtkCoordinateType(x, y,
-                                        nullptr, nullptr,
+                                        nullptr,nullptr,
                                         coord_type);
 }
 
