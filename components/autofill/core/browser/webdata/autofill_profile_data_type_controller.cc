5da6bbdf615005095f102eff81607ea5936f45f6
Cait Phillips <caitkp@chromium.org>
2017-11-15 18:50:58 +0000

    Do not sync autofill data if autofill is disabled.
    Bug: 
    Change-Id: Ie6a3beeede934b7be90b1ab544774eef29ec6ebd
    Reviewed-on: https://chromium-review.googlesource.com/738592
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Reviewed-by: Nicolas Zea <zea@chromium.org>
    Commit-Queue: Cait Phillips <caitkp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516764}
    Former-commit-id: 5f3db5dd00b504cc7e232a4e03941c23e8512b00

--- components/autofill/core/browser/webdata/autofill_profile_data_type_controller.cc
+++ components/autofill/core/browser/webdata/autofill_profile_data_type_controller.cc
@@ -9,12 +9,15 @@
 #include "base/bind.h"
 #include "base/metrics/histogram.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
+#include "components/autofill/core/browser/webdata/autocomplete_syncable_service.h"
 #include "components/autofill/core/browser/webdata/autofill_webdata_service.h"
+#include "components/autofill/core/common/autofill_pref_names.h"
+#include "components/prefs/pref_service.h"
+#include "components/sync/base/experiments.h"
 #include "components/sync/driver/sync_client.h"
+#include "components/sync/driver/sync_service.h"
 #include "components/sync/model/sync_error.h"
 #include "components/sync/model/syncable_service.h"
-
-using autofill::AutofillWebDataService;
 
 namespace browser_sync {
 
@@ -30,7 +33,14 @@ AutofillProfileDataTypeController::AutofillProfileDataTypeController(
                                    std::move(db_thread)),
       sync_client_(sync_client),
       web_data_service_(web_data_service),
-      callback_registered_(false) {}
+      callback_registered_(false),
+      currently_enabled_(IsEnabled()) {
+  pref_registrar_.Init(sync_client_->GetPrefService());
+  pref_registrar_.Add(
+      autofill::prefs::kAutofillEnabled,
+      base::Bind(&AutofillProfileDataTypeController::OnUserPrefChanged,
+                 base::AsWeakPtr(this)));
+}
 
 void AutofillProfileDataTypeController::WebDatabaseLoaded() {
   DCHECK(CalledOnValidThread());
@@ -61,6 +71,12 @@ AutofillProfileDataTypeController::~AutofillProfileDataTypeController() {}
 bool AutofillProfileDataTypeController::StartModels() {
   DCHECK(CalledOnValidThread());
   DCHECK_EQ(state(), MODEL_STARTING);
+
+  if (!IsEnabled()) {
+    DisableForPolicy();
+    return false;
+  }
+
   // Waiting for the personal data is subtle:  we do this as the PDM resets
   // its cache of unique IDs once it gets loaded. If we were to proceed with
   // association, the local ids in the mappings would wind up colliding.
@@ -90,6 +106,45 @@ bool AutofillProfileDataTypeController::StartModels() {
 void AutofillProfileDataTypeController::StopModels() {
   DCHECK(CalledOnValidThread());
   sync_client_->GetPersonalDataManager()->RemoveObserver(this);
+}
+
+bool AutofillProfileDataTypeController::ReadyForStart() const {
+  DCHECK(CalledOnValidThread());
+  return currently_enabled_;
+}
+
+void AutofillProfileDataTypeController::OnUserPrefChanged() {
+  DCHECK(CalledOnValidThread());
+
+  bool new_enabled = IsEnabled();
+  if (currently_enabled_ == new_enabled)
+    return;  // No change to sync state.
+  currently_enabled_ = new_enabled;
+
+  if (currently_enabled_) {
+    // The preference was just enabled. Trigger a reconfiguration. This will do
+    // nothing if the type isn't preferred.
+    syncer::SyncService* sync_service = sync_client_->GetSyncService();
+    sync_service->ReenableDatatype(type());
+  } else {
+    DisableForPolicy();
+  }
+}
+
+bool AutofillProfileDataTypeController::IsEnabled() {
+  DCHECK(CalledOnValidThread());
+
+  // Require the user-visible pref to be enabled to sync Autofill Profile data.
+  PrefService* ps = sync_client_->GetPrefService();
+  return ps->GetBoolean(autofill::prefs::kAutofillEnabled);
+}
+
+void AutofillProfileDataTypeController::DisableForPolicy() {
+  if (state() != NOT_RUNNING && state() != STOPPING) {
+    CreateErrorHandler()->OnUnrecoverableError(
+        syncer::SyncError(FROM_HERE, syncer::SyncError::DATATYPE_POLICY_ERROR,
+                          "Profile syncing is disabled by policy.", type()));
+  }
 }
 
 }  // namespace browser_sync
