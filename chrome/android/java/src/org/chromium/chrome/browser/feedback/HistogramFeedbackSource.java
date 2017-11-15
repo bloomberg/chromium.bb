2557a4b62fcb665df693d9220266c1e06540ba8b
David Trainor <dtrainor@chromium.org>
2017-11-15 21:00:51 +0000

    Revert "Revert "Rework FeedbackCollector""
    This reverts commit 3698df639d8d71579ce7f21b93609f6a27c91d5a [formerly 9b92c53200e26ded2a7ed716a40f0c0728acaa7b].
    
    Had to add back an API to FeedbackCollector called
    setScreenshot(Bitmap).  To do this properly I added a new implementation
    of ScreenshotSource called StaticScreenshotSource that just takes a
    Bitmap and returns it back when queried.  Also added a host of tests to
    validate the new API:
    
    testNullScreenshotOverrideStillTriggersCallback
    testScreenshotOverrideStillTriggersCallback
    testScreenshotOverrideWithNoOriginalScreenshot
    testScreenshotOverrideAfterCallback
    testOldScreenshotDoesNotOverrideNewOne
    
    Bug: 
    Change-Id: I2f40ce0f9c533d6d8b38d7435adf3c0143b8133e
    Reviewed-on: https://chromium-review.googlesource.com/772087
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516820}
    Former-commit-id: 3364335d90aa70d93e187c69f6dab1269bdf476a

---
+++ chrome/android/java/src/org/chromium/chrome/browser/feedback/HistogramFeedbackSource.java
@@ -0,0 +1,29 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.feedback;
+
+import android.util.Pair;
+
+import org.chromium.base.metrics.StatisticsRecorderAndroid;
+import org.chromium.chrome.browser.profiles.Profile;
+
+/** Grabs feedback about the UMA histograms. */
+// TODO(dtrainor): Make this class protected and HISTOGRAMS_KEY private once grabbing specific log
+// sources is no longer required.
+public class HistogramFeedbackSource implements FeedbackSource {
+    public static final String HISTOGRAMS_KEY = "histograms";
+
+    private final boolean mIsOffTheRecord;
+
+    HistogramFeedbackSource(Profile profile) {
+        mIsOffTheRecord = profile.isOffTheRecord();
+    }
+
+    @Override
+    public Pair<String, String> getLogs() {
+        if (mIsOffTheRecord) return null;
+        return Pair.create(HISTOGRAMS_KEY, StatisticsRecorderAndroid.toJson());
+    }
+}
