58610a830f1e29c945fcece48b38e32e3dcd56ad
Will Chen <chenwilliam@chromium.org>
2017-11-16 03:35:48 +0000

    DevTools: automatically migrate the rest of network tests
    This converts a batch of tests from the old test framework (html file)
    into the new test framework (js file).
    
    Most of it was done through an automated transformation script, with
    a small amount of manual clean-up.
    
    R=allada@chromium.org
    
    Bug: 667560
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Ia427b15096c06c72d7fa625b9660476b14705ab6
    Reviewed-on: https://chromium-review.googlesource.com/758508
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Reviewed-by: Blaise Bruer <allada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#516970}
    Former-commit-id: da4699e0ed2b9a0ece35419dc4fc886d376fd28a

---
+++ third_party/WebKit/LayoutTests/http/tests/devtools/network/script-as-text-loading-long-url.js
@@ -0,0 +1,28 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+  TestRunner.addResult(`Tests that long URLs are correctly trimmed in anchor links.\n`);
+  await TestRunner.loadModule('console_test_runner');
+  await TestRunner.showPanel('network');
+  await TestRunner.loadHTML(`
+      <a href="https://bugs.webkit.org/show_bug.cgi?id=100095">Bug 100095</a>
+    `);
+  await TestRunner.evaluateInPagePromise(`
+      function loadScript()
+      {
+          var i = document.createElement('script');
+          i.src = "http://127.0.0.1:8000/inspector/network/resources/script-as-text-with-a-very-very-very-very-very-very-very-very-very-very-very-very-very-very-long-url.php";
+          document.body.appendChild(i);
+      }
+  `);
+
+  ConsoleTestRunner.addConsoleSniffer(step1);
+  TestRunner.evaluateInPage('loadScript()');
+
+  function step1() {
+    ConsoleTestRunner.dumpConsoleMessages();
+    TestRunner.completeTest();
+  }
+})();
