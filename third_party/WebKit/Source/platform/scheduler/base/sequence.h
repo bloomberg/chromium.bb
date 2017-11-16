6c7f930065975df1257a8fcef89ed76364a777df
Alexander Timin <altimin@chromium.org>
2017-11-16 15:49:54 +0000

    [scheduler] Introduce ThreadController.
    ThreadController is a designed replacement for TaskQueueManagerDelegate
    in a SequenceManager world.
    
    Current implementation of a ThreadController allows for drop-in
    replacement of TaskQueueManagerDelegate. Further refactorings are
    planned after complete deletion of TaskQueueManagerDelegate.
    
    Full replacement code can be found at
    https://chromium-review.googlesource.com/c/chromium/src/+/763511
    
    R=skyostil@chromium.org,alexclarke@chromium.org,robliao@chromium.org,fdoray@chromium.org,gab@chromium.org
    
    Bug: 783309
    
    Change-Id: Id424da3cf43ac20b40c93ab7f20d79294d854df9
    Reviewed-on: https://chromium-review.googlesource.com/765267
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517093}
    Former-commit-id: 37ce9b49795d7b4e7eda0f25c6851d3fa67eb660

---
+++ third_party/WebKit/Source/platform/scheduler/base/sequence.h
@@ -0,0 +1,31 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_WEBKIT_SOURCE_PLATFORM_SCHEDULER_BASE_SEQUENCE_H_
+#define THIRD_PARTY_WEBKIT_SOURCE_PLATFORM_SCHEDULER_BASE_SEQUENCE_H_
+
+namespace blink {
+namespace scheduler {
+namespace internal {
+
+// This is temporary interface for ThreadController to be able to run tasks
+// from TaskQueueManager.
+class Sequence {
+ public:
+  enum class WorkType { kImmediate, kDelayed };
+
+  // Take a next task to run from a sequence.
+  // TODO(altimin): Do not pass |work_type| here.
+  virtual base::PendingTask TakeTask(WorkType work_type) = 0;
+
+  // Notify a sequence that a taken task has been completed.
+  // Returns true if sequence has more work to do.
+  virtual bool DidRunTask() = 0;
+};
+
+}  // namespace internal
+}  // namespace scheduler
+}  // namespace blink
+
+#endif  // THIRD_PARTY_WEBKIT_SOURCE_PLATFORM_SCHEDULER_BASE_SEQUENCE_H_
---
+++ third_party/WebKit/Source/platform/scheduler/base/thread_controller_impl.cc
@@ -0,0 +1,119 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/scheduler/base/thread_controller_impl.h"
+
+#include "base/bind.h"
+#include "base/memory/ptr_util.h"
+#include "base/message_loop/message_loop.h"
+#include "base/run_loop.h"
+#include "base/time/tick_clock.h"
+#include "platform/scheduler/base/sequence.h"
+
+namespace blink {
+namespace scheduler {
+namespace internal {
+
+ThreadControllerImpl::ThreadControllerImpl(
+    base::MessageLoop* message_loop,
+    scoped_refptr<base::SingleThreadTaskRunner> task_runner,
+    std::unique_ptr<base::TickClock> time_source)
+    : message_loop_(message_loop),
+      task_runner_(task_runner),
+      message_loop_task_runner_(message_loop ? message_loop->task_runner()
+                                             : nullptr),
+      time_source_(std::move(time_source)),
+      weak_factory_(this) {
+  immediate_do_work_closure_ = base::BindRepeating(
+      &ThreadControllerImpl::DoWork, weak_factory_.GetWeakPtr(),
+      Sequence::WorkType::kImmediate);
+  delayed_do_work_closure_ = base::BindRepeating(&ThreadControllerImpl::DoWork,
+                                                 weak_factory_.GetWeakPtr(),
+                                                 Sequence::WorkType::kDelayed);
+}
+
+ThreadControllerImpl::~ThreadControllerImpl() {}
+
+std::unique_ptr<ThreadControllerImpl> ThreadControllerImpl::Create(
+    base::MessageLoop* message_loop,
+    std::unique_ptr<base::TickClock> time_source) {
+  return base::WrapUnique(new ThreadControllerImpl(
+      message_loop, message_loop->task_runner(), std::move(time_source)));
+}
+
+void ThreadControllerImpl::SetSequence(Sequence* sequence) {
+  DCHECK(sequence);
+  DCHECK(!sequence_);
+  sequence_ = sequence;
+}
+
+void ThreadControllerImpl::ScheduleWork() {
+  DCHECK(sequence_);
+  task_runner_->PostTask(FROM_HERE, immediate_do_work_closure_);
+}
+
+void ThreadControllerImpl::ScheduleDelayedWork(base::TimeDelta delay) {
+  DCHECK(sequence_);
+  cancelable_delayed_do_work_closure_.Reset(delayed_do_work_closure_);
+  task_runner_->PostDelayedTask(
+      FROM_HERE, cancelable_delayed_do_work_closure_.callback(), delay);
+}
+
+void ThreadControllerImpl::CancelDelayedWork() {
+  DCHECK(sequence_);
+  cancelable_delayed_do_work_closure_.Cancel();
+}
+
+void ThreadControllerImpl::PostNonNestableTask(const base::Location& from_here,
+                                               base::OnceClosure task) {
+  task_runner_->PostNonNestableTask(from_here, std::move(task));
+}
+
+bool ThreadControllerImpl::RunsTasksInCurrentSequence() {
+  return task_runner_->RunsTasksInCurrentSequence();
+}
+
+base::TickClock* ThreadControllerImpl::GetClock() {
+  return time_source_.get();
+}
+
+void ThreadControllerImpl::SetDefaultTaskRunner(
+    scoped_refptr<base::SingleThreadTaskRunner> task_runner) {
+  if (!message_loop_)
+    return;
+  message_loop_->SetTaskRunner(task_runner);
+}
+
+void ThreadControllerImpl::RestoreDefaultTaskRunner() {
+  if (!message_loop_)
+    return;
+  message_loop_->SetTaskRunner(message_loop_task_runner_);
+}
+bool ThreadControllerImpl::IsNested() {
+  DCHECK(RunsTasksInCurrentSequence());
+  return base::RunLoop::IsNestedOnCurrentThread();
+}
+
+void ThreadControllerImpl::DoWork(Sequence::WorkType work_type) {
+  DCHECK(sequence_);
+  base::PendingTask task = sequence_->TakeTask(work_type);
+  base::WeakPtr<ThreadControllerImpl> weak_ptr = weak_factory_.GetWeakPtr();
+  task_annotator_.RunTask("ThreadControllerImpl::DoWork", &task);
+  if (weak_ptr)
+    sequence_->DidRunTask();
+}
+
+void ThreadControllerImpl::AddNestingObserver(
+    base::RunLoop::NestingObserver* observer) {
+  base::RunLoop::AddNestingObserverOnCurrentThread(observer);
+}
+
+void ThreadControllerImpl::RemoveNestingObserver(
+    base::RunLoop::NestingObserver* observer) {
+  base::RunLoop::RemoveNestingObserverOnCurrentThread(observer);
+}
+
+}  // namespace internal
+}  // namespace scheduler
+}  // namespace blink
