c72cc4d3fa229e254424de64f07c6f66e835ef24
Anita Woodruff <awdf@chromium.org>
2017-11-16 13:25:52 +0000

    [Android O] Clean up notification settings on channel migration
    - Previously when migrating to channels, the content settings were
    simply copied over to the NotificationChannelsProviderAndroid from
    the PrefProvider.
    
    - Now they are deleted from the PrefProvider after they are copied over
    to the NCPA.
    
    - This avoids old content settings hanging around and getting
    reinstated on channel deletion.
    
    Bug: 781524,784823
    Change-Id: I01ec7974831f6a2be192c7e6e2a587148e736b9e
    Reviewed-on: https://chromium-review.googlesource.com/768722
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Anita Woodruff <awdf@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517071}
    Former-commit-id: 138706e38fcd103ba462dbbc13785235a591c4f3

--- chrome/browser/notifications/notification_channels_provider_android.cc
+++ chrome/browser/notifications/notification_channels_provider_android.cc
@@ -185,11 +185,31 @@ void NotificationChannelsProviderAndroid::MigrateToChannelsIfNecessary(
     return;
   }
   InitCachedChannels();
-  std::unique_ptr<content_settings::RuleIterator> it(
-      pref_provider->GetRuleIterator(CONTENT_SETTINGS_TYPE_NOTIFICATIONS,
-                                     std::string(), false /* incognito */));
-  while (it && it->HasNext())
-    CreateChannelForRule(it->Next());
+
+  std::vector<content_settings::Rule> rules;
+
+  // Collect the existing rules and create channels for them.
+  {
+    std::unique_ptr<content_settings::RuleIterator> it(
+        pref_provider->GetRuleIterator(CONTENT_SETTINGS_TYPE_NOTIFICATIONS,
+                                       std::string(), false /* incognito */));
+
+    while (it && it->HasNext()) {
+      content_settings::Rule rule = it->Next();
+      rules.push_back(rule);
+
+      CreateChannelForRule(rule);
+    }
+  }
+
+  // Remove the existing |rules| from the preference provider.
+  for (const auto& rule : rules) {
+    pref_provider->SetWebsiteSetting(
+        rule.primary_pattern, rule.secondary_pattern,
+        CONTENT_SETTINGS_TYPE_NOTIFICATIONS,
+        content_settings::ResourceIdentifier(), nullptr);
+  }
+
   prefs->SetBoolean(prefs::kMigratedToSiteNotificationChannels, true);
 }
 
