9df448e9316b95badcdec6e8454e68c69229fdbb
Sylvain Defresne <sdefresne@chromium.org>
2017-11-16 10:29:57 +0000

    Allow WebStateObserver to observe N WebStates [26/N].
    Convert JavaScriptDialogBlockingState to directly track
    registration with the observed WebState instead of
    relying on the deprecated code in WebStateObserver.
    
    Bug: 775684
    Change-Id: I5add60c72e6046400c2dc1c4a609e9f443ecb782
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Reviewed-on: https://chromium-review.googlesource.com/768420
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517049}
    Former-commit-id: e110ef5939f209aa91f56a6acc1505d51c5e7c1f

--- ios/chrome/browser/ui/dialogs/java_script_dialog_blocking_state.mm
+++ ios/chrome/browser/ui/dialogs/java_script_dialog_blocking_state.mm
@@ -4,6 +4,7 @@
 
 #import "ios/chrome/browser/ui/dialogs/java_script_dialog_blocking_state.h"
 
+#include "base/logging.h"
 #import "ios/web/public/navigation_item.h"
 #import "ios/web/public/navigation_manager.h"
 #import "ios/web/public/web_state/navigation_context.h"
@@ -16,24 +17,25 @@ DEFINE_WEB_STATE_USER_DATA_KEY(JavaScriptDialogBlockingState);
 
 JavaScriptDialogBlockingState::JavaScriptDialogBlockingState(
     web::WebState* web_state)
-    : web::WebStateObserver(web_state) {
-  DCHECK(web_state);
+    : web_state_(web_state) {
+  web_state_->AddObserver(this);
 }
 
 JavaScriptDialogBlockingState::~JavaScriptDialogBlockingState() {
   // It is expected that WebStateDestroyed() will be received before this state
   // is deallocated.
-  DCHECK(!web_state());
+  DCHECK(!web_state_);
 }
 
 void JavaScriptDialogBlockingState::JavaScriptDialogBlockingOptionSelected() {
-  blocked_item_ = web_state()->GetNavigationManager()->GetLastCommittedItem();
+  blocked_item_ = web_state_->GetNavigationManager()->GetLastCommittedItem();
   DCHECK(blocked_item_);
 }
 
 void JavaScriptDialogBlockingState::DidStartNavigation(
     web::WebState* web_state,
     web::NavigationContext* navigation_context) {
+  DCHECK_EQ(web_state_, web_state);
   web::NavigationItem* item =
       web_state->GetNavigationManager()->GetLastCommittedItem();
   // The dialog blocking state should be reset for user-initiated loads or for
@@ -47,5 +49,7 @@ void JavaScriptDialogBlockingState::DidStartNavigation(
 
 void JavaScriptDialogBlockingState::WebStateDestroyed(
     web::WebState* web_state) {
-  Observe(nullptr);
+  DCHECK_EQ(web_state_, web_state);
+  web_state_->RemoveObserver(this);
+  web_state_ = nullptr;
 }
