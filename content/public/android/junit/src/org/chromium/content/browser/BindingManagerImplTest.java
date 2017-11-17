0aaaee0c174956e07c35957804ee90c00181d38a
Bo Liu <boliu@chromium.org>
2017-11-17 15:34:23 +0000

    android: Remove 1s delay to remove strong binding
    This is needed to avoid dropping the strong binding when there were two
    racy signals to drop and add strong binding. Now the the "last
    foreground connection" has long been removed, it should be safe to just
    remove this delay.
    
    Bug: 689758
    Change-Id: I874b7762f35c208f497189f142797200de307cd9
    Reviewed-on: https://chromium-review.googlesource.com/767651
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517394}
    Former-commit-id: 80943ada381be0b05562a02af9db38d6d426fe74

--- content/public/android/junit/src/org/chromium/content/browser/BindingManagerImplTest.java
+++ content/public/android/junit/src/org/chromium/content/browser/BindingManagerImplTest.java
@@ -150,14 +150,8 @@ public class BindingManagerImplTest {
         manager.setPriority(connection.getPid(), true /* foreground */, false /* boost */);
         Assert.assertTrue(connection.isStrongBindingBound());
 
-        // Remove the strong binding, verify that the strong binding is not removed
-        // immediately.
+        // Remove the strong binding, verify that the strong binding was removed.
         manager.setPriority(connection.getPid(), false /* foreground */, false /* boost */);
-        Assert.assertTrue(connection.isStrongBindingBound());
-
-        // Wait until the posted unbinding tasks get executed and verify that the strong binding was
-        // removed while the initial binding is not affected.
-        ShadowLooper.runUiThreadTasksIncludingDelayedTasks();
         Assert.assertFalse(connection.isStrongBindingBound());
     }
 
@@ -183,15 +177,9 @@ public class BindingManagerImplTest {
         Assert.assertTrue(connection.isStrongBindingBound());
         Assert.assertFalse(connection.isModerateBindingBound());
 
-        // Remove the strong binding, verify that the strong binding is not removed
-        // immediately.
+        // Remove the strong binding, verify that the strong binding was removed while the
+        // initial binding is not affected, and the moderate binding is bound.
         manager.setPriority(connection.getPid(), false /* foreground */, false /* boost */);
-        Assert.assertTrue(connection.isStrongBindingBound());
-        Assert.assertFalse(connection.isModerateBindingBound());
-
-        // Wait until the posted unbinding tasks get executed and verify that the strong binding was
-        // removed while the initial binding is not affected, and the moderate binding is bound.
-        ShadowLooper.runUiThreadTasksIncludingDelayedTasks();
         Assert.assertFalse(connection.isStrongBindingBound());
         Assert.assertTrue(connection.isModerateBindingBound());
     }
