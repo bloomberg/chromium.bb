3b3b39605dff1d5384ec9e124a16192a2f5e04c1
Marc Treib <treib@chromium.org>
2017-11-17 16:05:48 +0000

    NTP: Avoid supervised user URL filter checks for non-SUs
    These checks can be fairly expensive, and they're called on the critical
    path for all navigations. No reason to pay the cost for non-SUs.
    
    Bug: 786035
    Change-Id: Id47d0b2fe4c86371fd4c277e363a4f7fcfd49194
    Reviewed-on: https://chromium-review.googlesource.com/775959
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517413}
    Former-commit-id: 3ac2b20f294ae4bbbaa142c2fdee9c588f0412ce

--- chrome/browser/search/search_unittest.cc
+++ chrome/browser/search/search_unittest.cc
@@ -350,6 +350,8 @@ TEST_F(SearchTest, UseLocalNTPIfNTPURLIsNotSet) {
 
 #if BUILDFLAG(ENABLE_SUPERVISED_USERS)
 TEST_F(SearchTest, UseLocalNTPIfNTPURLIsBlockedForSupervisedUser) {
+  // Mark the profile as supervised, otherwise the URL filter won't be checked.
+  profile()->SetSupervisedUserId("supervised");
   // Block access to foo.com in the URL filter.
   SupervisedUserService* supervised_user_service =
       SupervisedUserServiceFactory::GetForProfile(profile());
