72dfddaeb990d174cdaf29e4c3e62ee2dd2cdd8e
Ioana Pandele <ioanap@chromium.org>
2017-11-17 11:11:07 +0000

    Remove deletedForms_ from SavePasswordsCollectionViewController
    PasswordStore::RemoveLogin passes the form through base::Bind which makes a copy, so this instance
    doesn't need to be kept around.
    
    Bug: 784788
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I47e02f82fa668b643d7d8c4a52907aa9b25aa404
    Reviewed-on: https://chromium-review.googlesource.com/771550
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517373}
    Former-commit-id: 644e22dc09fbb1031462301458f6c4db5437c231

--- ios/chrome/browser/ui/settings/save_passwords_collection_view_controller.mm
+++ ios/chrome/browser/ui/settings/save_passwords_collection_view_controller.mm
@@ -129,10 +129,6 @@ void SavePasswordsConsumer::OnGetPasswordStoreResults(
   std::vector<std::unique_ptr<autofill::PasswordForm>> savedForms_;
   // The list of the user's blacklisted sites.
   std::vector<std::unique_ptr<autofill::PasswordForm>> blacklistedForms_;
-  // Deletion of password being asynchronous, and the method taking a reference
-  // to the PasswordForm, the PasswordForm must outlive the calls to
-  // RemoveLogin. This vector will ensure this.
-  std::vector<std::unique_ptr<autofill::PasswordForm>> deletedForms_;
   // Map containing duplicates of saved passwords.
   password_manager::DuplicatesMap savedPasswordDuplicates_;
   // Map containing duplicates of blacklisted passwords.
@@ -543,7 +539,6 @@ void SavePasswordsConsumer::OnGetPasswordStoreResults(
 
     forms.erase(formIterator);
     passwordStore_->RemoveLogin(*form);
-    deletedForms_.push_back(std::move(form));
     if (blacklisted) {
       ++blacklistedDeleted;
     } else {
