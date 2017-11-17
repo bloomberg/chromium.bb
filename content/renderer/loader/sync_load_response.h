8f5896a40b155a9a026005e62e43c8b8f5e71675
Takashi Toyoshima <toyoshim@chromium.org>
2017-11-17 13:03:05 +0000

    Plumb extra errors from ResourceRequestCompletionStatus to Blink
    This patch plumbs CORSError and HTTP response headers
    from ResourceRequestCompletinoStatus to Blink's ResourceError.
    
    Test: ./Tools/Scripts/run-webkit-tests virtual/outofblink-cors
    Bug: 736308
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I20e4ec9a2e7ca93ce692a5a8312dadf59eb0f345
    Reviewed-on: https://chromium-review.googlesource.com/749024
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517388}
    Former-commit-id: 0326c088ba772ae53bd426f0eb4ef478053b88d3

--- content/renderer/loader/sync_load_response.h
+++ content/renderer/loader/sync_load_response.h
@@ -7,7 +7,10 @@
 
 #include <string>
 
+#include "base/optional.h"
+#include "content/common/content_export.h"
 #include "content/public/common/resource_response_info.h"
+#include "services/network/public/interfaces/cors.mojom.h"
 #include "url/gurl.h"
 
 namespace content {
@@ -19,7 +22,10 @@ struct CONTENT_EXPORT SyncLoadResponse : ResourceResponseInfo {
   ~SyncLoadResponse();
 
   // The response error code.
-  int error_code;
+  int error_code = 0;
+
+  // Optional CORS error details.
+  base::Optional<network::mojom::CORSError> cors_error;
 
   // The final URL of the response.  This may differ from the request URL in
   // the case of a server redirect.
