faa8f7ec5391f70890613a0d2aa213a4aa86f272
tanvir.rizvi <tanvir.rizvi@samsung.com>
2017-11-17 17:13:08 +0000

    [ClusterFuzz] crash in replaceSelectionCommand
    ReplaceSelectionCommand crashes if InsertHTML content have
    trailing non visible content.
    This happens as the nextAncesstorSibling comes as null
    This CL does the safety check to prevent the crash
    observed in this scenario.
    
    Bug: 781282
    Change-Id: Ibb886956dafcdfaadac4dd2ee6b6c1ef70ad8340
    Reviewed-on: https://chromium-review.googlesource.com/768550
    Commit-Queue: Tanvir Rizvi <tanvir.rizvi@samsung.com>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517437}
    Former-commit-id: 4306d8180795bb89b22ccec20c6ff9a1b2acfaa1

--- third_party/WebKit/Source/core/editing/commands/ReplaceSelectionCommandTest.cpp
+++ third_party/WebKit/Source/core/editing/commands/ReplaceSelectionCommandTest.cpp
@@ -154,4 +154,21 @@ TEST_F(ReplaceSelectionCommandTest, TextAutosizingDoesntInflateText) {
   EXPECT_EQ(1u, div->CountChildren());
 }
 
+// This is a regression test for https://crbug.com/781282
+TEST_F(ReplaceSelectionCommandTest, TrailingNonVisibleTextCrash) {
+  GetDocument().setDesignMode("on");
+  Selection().SetSelection(SetSelectionTextToBody("<div>^foo|</div>"));
+
+  DocumentFragment* fragment = GetDocument().createDocumentFragment();
+  fragment->ParseHTML("<div>bar</div> ", GetDocument().QuerySelector("div"));
+  ReplaceSelectionCommand::CommandOptions options = 0;
+  ReplaceSelectionCommand* command =
+      ReplaceSelectionCommand::Create(GetDocument(), fragment, options);
+
+  // Crash should not occur on applying ReplaceSelectionCommand
+  EXPECT_FALSE(command->Apply());
+  EXPECT_EQ("<div>bar</div>|<br>",
+            GetSelectionTextFromBody(Selection().GetSelectionInDOMTree()));
+}
+
 }  // namespace blink
