58b06b1cb386616a2baf3015d20520e8de96a05b
Robert Ma <robertma@chromium.org>
2017-11-17 20:22:47 +0000

    Fix some docs for all-PASS baseline checking
    Bug: 613387
    Change-Id: I3696db30690ea6bd7de673989e2846ec186bea25
    Reviewed-on: https://chromium-review.googlesource.com/775758
    Commit-Queue: Robert Ma <robertma@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517534}
    Former-commit-id: 77a8696a4ddaa997b7047fc740aa7c57f799208b

--- third_party/WebKit/LayoutTests/PRESUBMIT.py
+++ third_party/WebKit/LayoutTests/PRESUBMIT.py
@@ -12,12 +12,14 @@ import filecmp
 
 
 def _CheckTestharnessResults(input_api, output_api):
-    """Checks for testharness.js test baseline files that contain only PASS lines.
+    """Checks for all-PASS generic baselines for testharness.js tests.
 
-    In general these files are unnecessary because for testharness.js tests, if there is
-    no baseline file then the test is considered to pass when the output is all PASS.
+    These files are unnecessary because for testharness.js tests, if there is no
+    baseline file then the test is considered to pass when the output is all
+    PASS. Note that only generic baselines are checked because platform specific
+    and virtual baselines might be needed to prevent fallback.
     """
-    baseline_files = _TestharnessBaselineFilesToCheck(input_api)
+    baseline_files = _TestharnessGenericBaselinesToCheck(input_api)
     if not baseline_files:
         return []
 
@@ -34,8 +36,8 @@ def _CheckTestharnessResults(input_api, output_api):
     return []
 
 
-def _TestharnessBaselineFilesToCheck(input_api):
-    """Returns a list of paths of -expected.txt files for testharness.js tests."""
+def _TestharnessGenericBaselinesToCheck(input_api):
+    """Returns a list of paths of generic baselines for testharness.js tests."""
     baseline_files = []
     for f in input_api.AffectedFiles():
         if f.Action() == 'D':
@@ -45,11 +47,6 @@ def _TestharnessBaselineFilesToCheck(input_api):
             continue
         if (input_api.os_path.join('LayoutTests', 'platform') in path or
             input_api.os_path.join('LayoutTests', 'virtual') in path):
-            # We want to ignore files in LayoutTests/platform, because some all-PASS
-            # platform specific baselines may be necessary to prevent fallback to a
-            # more general baseline; we also ignore files in LayoutTests/virtual
-            # for a similar reason; some all-pass baselines are necessary to
-            # prevent fallback to the corresponding non-virtual test baseline.
             continue
         baseline_files.append(path)
     return baseline_files
