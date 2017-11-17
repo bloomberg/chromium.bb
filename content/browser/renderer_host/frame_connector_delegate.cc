e0ebbff5d3e57ca83460cc224878f464ba8d6964
Fady Samuel <fsamuel@chromium.org>
2017-11-17 04:18:53 +0000

    Surface synchronization: Use the frame rect in pixels for physical backing size
    Prior to this CL, RenderWidgetHostViewChildFrame converted the frame rect
    in DIP to physical pixels for GetPhysicalBackingSize. The frame rect in DIP
    is computed using ScaleToEnclosingRect which can change the size of the rect
    depending on its position. This occasionally results in a surface invariants
    violation when surface synchronization is turned on.
    
    The solution is to use the frame rect in physical pixels from the parent
    and pass that along in RWHVChildFrame::GetPhysicalSize().
    
    Bug: 672962
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: Ic1ec779b8330e77c04fe3a3c0a73b90d57b4beaf
    Reviewed-on: https://chromium-review.googlesource.com/753832
    Commit-Queue: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517294}
    Former-commit-id: 9bbb082b90b9977b7e583b0321341508a005adf5

--- content/browser/renderer_host/frame_connector_delegate.cc
+++ content/browser/renderer_host/frame_connector_delegate.cc
@@ -4,6 +4,8 @@
 
 #include "content/browser/renderer_host/frame_connector_delegate.h"
 
+#include "content/common/content_switches_internal.h"
+
 namespace content {
 
 RenderWidgetHostViewBase*
@@ -14,10 +16,6 @@ FrameConnectorDelegate::GetParentRenderWidgetHostView() {
 RenderWidgetHostViewBase*
 FrameConnectorDelegate::GetRootRenderWidgetHostView() {
   return nullptr;
-}
-
-gfx::Rect FrameConnectorDelegate::ChildFrameRect() {
-  return gfx::Rect();
 }
 
 gfx::PointF FrameConnectorDelegate::TransformPointToRootCoordSpace(
@@ -65,5 +63,21 @@ bool FrameConnectorDelegate::IsThrottled() const {
 bool FrameConnectorDelegate::IsSubtreeThrottled() const {
   return false;
 }
+
+void FrameConnectorDelegate::SetRect(const gfx::Rect& frame_rect) {
+  if (use_zoom_for_device_scale_factor_) {
+    frame_rect_in_pixels_ = frame_rect;
+    frame_rect_in_dip_ = gfx::ScaleToEnclosingRect(
+        frame_rect, 1.f / screen_info_.device_scale_factor);
+  } else {
+    frame_rect_in_dip_ = frame_rect;
+    frame_rect_in_pixels_ =
+        gfx::ScaleToEnclosingRect(frame_rect, screen_info_.device_scale_factor);
+  }
+}
+
+FrameConnectorDelegate::FrameConnectorDelegate(
+    bool use_zoom_for_device_scale_factor)
+    : use_zoom_for_device_scale_factor_(use_zoom_for_device_scale_factor) {}
 
 }  // namespace content
