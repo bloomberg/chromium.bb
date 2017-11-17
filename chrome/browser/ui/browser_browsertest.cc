87a6f44eceef04b77b20c49db9b0263f3a26b55c
Jochen Eisinger <jochen@chromium.org>
2017-11-17 10:22:47 +0000

    Check whether timer throttling makes BrowserTest.WindowOpenClose flaky
    BUG=753691
    R=skyostil@chromium.org
    
    Change-Id: I3cfb174a725cdb659bf220ac392b7584a95131c0
    Reviewed-on: https://chromium-review.googlesource.com/775158
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517358}
    Former-commit-id: 91d4c55c32a7a2fc775612e50a8763a8d079d70c

--- chrome/browser/ui/browser_browsertest.cc
+++ chrome/browser/ui/browser_browsertest.cc
@@ -98,6 +98,7 @@
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/common/browser_side_navigation_policy.h"
+#include "content/public/common/content_switches.h"
 #include "content/public/common/frame_navigate_params.h"
 #include "content/public/common/renderer_preferences.h"
 #include "content/public/common/url_constants.h"
@@ -2003,6 +2004,8 @@ IN_PROC_BROWSER_TEST_F(BrowserTest2, NoTabsInPopups) {
 IN_PROC_BROWSER_TEST_F(BrowserTest, WindowOpenClose) {
   base::CommandLine::ForCurrentProcess()->AppendSwitch(
       switches::kDisablePopupBlocking);
+  base::CommandLine::ForCurrentProcess()->AppendSwitch(
+      switches::kDisableBackgroundTimerThrottling);
   GURL url = ui_test_utils::GetTestUrl(
       base::FilePath(), base::FilePath().AppendASCII("window.close.html"));
 
