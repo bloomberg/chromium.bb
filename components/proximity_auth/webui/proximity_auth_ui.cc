564c42678e5e13d14e965e469fb4c5c2fc957142
Reilly Grant <reillyg@chromium.org>
2017-11-17 22:04:28 +0000

    Compress shared WebUI resource files
    This reduces the size of an official build of ChromePublic.apk by
    403 KiB and the size of resources.pak on a Chrome OS build by 1.46 MiB.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation;master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I7f6502cc17136dd15a2b3e026bbe5c84e3fb337d
    Bug: 619091
    Reviewed-on: https://chromium-review.googlesource.com/770550
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517578}
    Former-commit-id: 89a01f157a882f2a8d7cc36d323ef6d62645ae29

--- components/proximity_auth/webui/proximity_auth_ui.cc
+++ components/proximity_auth/webui/proximity_auth_ui.cc
@@ -12,7 +12,6 @@
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
-#include "ui/resources/grit/webui_resources.h"
 
 namespace proximity_auth {
 
