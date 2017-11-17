f41f121c537d00c0d2a4f2ae59efe3a877e77afb
Christos Froussios <cfroussios@chromium.org>
2017-11-17 17:18:01 +0000

    🔐 Introduce DestinationFileSystem
    DestinationFileSystem is the desktop implemenation of Destination, which
    sends the data to the file system.
    
    Bug: 785237
    Change-Id: I034fab562eafde2240dc75743cb2ebbc6f7ec9ac
    Reviewed-on: https://chromium-review.googlesource.com/776596
    Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517442}
    Former-commit-id: 0126d27663187dfcecc60c35159155a836a3e738

---
+++ chrome/browser/ui/passwords/destination_file_system.cc
@@ -0,0 +1,16 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/passwords/destination_file_system.h"
+
+#include <utility>
+
+#include "base/files/file_util.h"
+
+DestinationFileSystem::DestinationFileSystem(base::FilePath destination_path)
+    : destination_path_(std::move(destination_path)) {}
+
+bool DestinationFileSystem::Write(const std::string& data) {
+  return base::WriteFile(destination_path_, data.c_str(), data.size());
+}
---
+++ chrome/browser/ui/passwords/destination_file_system.h
@@ -0,0 +1,29 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_PASSWORDS_DESTINATION_FILE_SYSTEM_H_
+#define CHROME_BROWSER_UI_PASSWORDS_DESTINATION_FILE_SYSTEM_H_
+
+#include <string>
+
+#include "base/files/file_path.h"
+#include "base/macros.h"
+#include "components/password_manager/core/browser/export/destination.h"
+
+class DestinationFileSystem : public password_manager::Destination {
+ public:
+  explicit DestinationFileSystem(base::FilePath destination_path);
+  virtual ~DestinationFileSystem() = default;
+
+  // password_manager::Destination
+  bool Write(const std::string& data) override;
+
+ private:
+  // The file, to which the data will be written.
+  const base::FilePath destination_path_;
+
+  DISALLOW_COPY_AND_ASSIGN(DestinationFileSystem);
+};
+
+#endif  // CHROME_BROWSER_UI_PASSWORDS_DESTINATION_FILE_SYSTEM_H_
