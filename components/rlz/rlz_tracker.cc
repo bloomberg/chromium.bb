66af5a04eb4ff968d56c10beaa537908ed59be94
Francois Doray <fdoray@chromium.org>
2017-11-17 22:41:12 +0000

    Remove WithBaseSyncPrimitives() from rlz_tracker.cc.
    The WithBaseSyncPrimitives() trait is deprecated.
    
    ScopedAllowBaseSyncPrimitives(ForTesting|OutsideBlockingScope)
    should be used instead.
    
    Bug: 766678
    Change-Id: Ib8e512a0c4f32ab7eec0ade8764495614bf3fb69
    Reviewed-on: https://chromium-review.googlesource.com/703200
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517594}
    Former-commit-id: 6f3478dfd7d29b9872871718bd08493ed0c8bc8e

--- components/rlz/rlz_tracker.cc
+++ components/rlz/rlz_tracker.cc
@@ -171,7 +171,7 @@ RLZTracker::RLZTracker()
       min_init_delay_(kMinInitDelay),
       background_task_runner_(base::CreateSequencedTaskRunnerWithTraits(
           {base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN, base::MayBlock(),
-           base::WithBaseSyncPrimitives(), base::TaskPriority::BACKGROUND})) {
+           base::TaskPriority::BACKGROUND})) {
   DETACH_FROM_SEQUENCE(sequence_checker_);
 }
 
