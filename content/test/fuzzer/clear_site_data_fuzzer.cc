9c2c4abe4108e5d8724c604c307cbedcb7ed7cbf
Martin Sramek <msramek@chromium.org>
2017-11-17 16:43:03 +0000

    Initialize the command line in clear_site_data_fuzzer.cc
    After https://chromium-review.googlesource.com/771890, the functionality
    tested by this fuzzer depends on base::CommandLine. This was not
    initialized in the fuzzer, making it crash.
    
    Bug: 786023
    Change-Id: I1c599e3231d5236b6ffb59e20373c2243ee0d026
    Reviewed-on: https://chromium-review.googlesource.com/776874
    Reviewed-by: Max Moroz <mmoroz@chromium.org>
    Commit-Queue: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517427}
    Former-commit-id: 8667d1b179aa31f6a5165c2e79febae070d21597

--- content/test/fuzzer/clear_site_data_fuzzer.cc
+++ content/test/fuzzer/clear_site_data_fuzzer.cc
@@ -6,9 +6,15 @@
 #include <stdint.h>
 #include <string>
 
+#include "base/command_line.h"
 #include "content/browser/browsing_data/clear_site_data_throttle.h"  // nogncheck
 
 namespace content {
+
+extern "C" int LLVMFuzzerInitialize(int* argc, char*** argv) {
+  base::CommandLine::Init(*argc, *argv);
+  return 0;
+}
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   std::string header(reinterpret_cast<const char*>(data), size);
