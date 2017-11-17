dd7a182d1f2228dc04ce7b976b447543086da881
Jun Cai <juncai@chromium.org>
2017-11-17 20:54:29 +0000

    NetworkService: Fix some ErrorPageTest that was using URLRequestMockHTTPJob::GetMockUrl
    This CL fixes some ErrorPageTest by using test server instead of
    URLRequestMockHTTPJob::GetMockUrl().
    
    Bug: 776589
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I4c14d6e6f15b2435eb6bd3ca22afe3ad6b7a8a11
    Reviewed-on: https://chromium-review.googlesource.com/772872
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517548}
    Former-commit-id: f6c24adb89d5c8aa0741da20484adcac4cd30e1b

--- chrome/browser/net/errorpage_browsertest.cc
+++ chrome/browser/net/errorpage_browsertest.cc
@@ -862,8 +862,12 @@ IN_PROC_BROWSER_TEST_F(ErrorPageTest, IFrameDNSError_Basic) {
 // Test that a DNS error occuring in an iframe does not result in an
 // additional session history entry.
 IN_PROC_BROWSER_TEST_F(ErrorPageTest, MAYBE_IFrameDNSError_GoBack) {
-  NavigateToFileURL("title2.html");
-  NavigateToFileURL("iframe_dns_error.html");
+  ASSERT_TRUE(embedded_test_server()->Start());
+
+  ui_test_utils::NavigateToURL(browser(),
+                               embedded_test_server()->GetURL("/title2.html"));
+  ui_test_utils::NavigateToURL(
+      browser(), embedded_test_server()->GetURL("/iframe_dns_error.html"));
   GoBackAndWaitForTitle("Title Of Awesomeness", 1);
   EXPECT_EQ(0, link_doctor_interceptor()->num_requests());
 }
@@ -891,13 +895,16 @@ IN_PROC_BROWSER_TEST_F(ErrorPageTest, MAYBE_IFrameDNSError_GoBackAndForward) {
 // To ensure that the main document has completed loading, JavaScript is used to
 // inject an iframe after loading is done.
 IN_PROC_BROWSER_TEST_F(ErrorPageTest, IFrameDNSError_JavaScript) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+
   content::WebContents* wc =
       browser()->tab_strip_model()->GetActiveWebContents();
   GURL fail_url =
       URLRequestFailedJob::GetMockHttpUrl(net::ERR_NAME_NOT_RESOLVED);
 
   // Load a regular web page, in which we will inject an iframe.
-  NavigateToFileURL("title2.html");
+  ui_test_utils::NavigateToURL(browser(),
+                               embedded_test_server()->GetURL("/title2.html"));
 
   // We expect to have two history entries, since we started off with navigation
   // to "about:blank" and then navigated to "title2.html".
@@ -977,7 +984,10 @@ IN_PROC_BROWSER_TEST_F(ErrorPageTest, Empty404) {
 // Checks that a local error page is shown in response to a 500 error page
 // without a body.
 IN_PROC_BROWSER_TEST_F(ErrorPageTest, Empty500) {
-  NavigateToFileURL("errorpage/empty500.html");
+  ASSERT_TRUE(embedded_test_server()->Start());
+
+  ui_test_utils::NavigateToURL(
+      browser(), embedded_test_server()->GetURL("/errorpage/empty500.html"));
   // This depends on the non-internationalized error ID string in
   // localized_error.cc.
   ExpectDisplayingLocalErrorPage(browser(), "HTTP ERROR 500");
