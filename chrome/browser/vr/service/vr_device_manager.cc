4cf6790c6b08391de4ece627f332ed96ea5f073b
Bill Orr <billorr@chromium.org>
2017-11-18 02:39:38 +0000

    Enable OpenVR devices behind a runtime flag on Windows
    BUG=782445
    
    Change-Id: Iec695a4db2bf2851f8f23b2c8ab1568a5bbfa588
    Reviewed-on: https://chromium-review.googlesource.com/757859
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Commit-Queue: Bill Orr <billorr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517684}
    Former-commit-id: 14d631ca392fa139a6f6b89d052d089b99758e33

--- chrome/browser/vr/service/vr_device_manager.cc
+++ chrome/browser/vr/service/vr_device_manager.cc
@@ -10,6 +10,7 @@
 #include "base/memory/ptr_util.h"
 #include "base/memory/singleton.h"
 #include "build/build_config.h"
+#include "chrome/common/chrome_features.h"
 #include "device/vr/features/features.h"
 
 #if defined(OS_ANDROID)
@@ -35,7 +36,8 @@ VRDeviceManager* VRDeviceManager::GetInstance() {
 #endif
 
 #if BUILDFLAG(ENABLE_OPENVR)
-    providers.emplace_back(std::make_unique<device::OpenVRDeviceProvider>());
+    if (base::FeatureList::IsEnabled(features::kOpenVR))
+      providers.emplace_back(std::make_unique<device::OpenVRDeviceProvider>());
 #endif
     new VRDeviceManager(std::move(providers));
   }
