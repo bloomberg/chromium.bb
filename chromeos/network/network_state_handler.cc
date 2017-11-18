f09dfc27a9c9dccb9f092bf1e3214f3852c01608
Kevin Cernekee <cernekee@chromium.org>
2017-11-18 01:55:41 +0000

    Fix lint warnings in network_state_handler.cc
    BUG=none
    TEST=unit tests
    
    Change-Id: Ia734c40400e722cf979231af398fa4a38611cfb7
    Reviewed-on: https://chromium-review.googlesource.com/777971
    Commit-Queue: Kevin Cernekee <cernekee@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517676}
    Former-commit-id: 0598d65f3ba6219042f0eadc1d7990b7ff6554ad

--- chromeos/network/network_state_handler.cc
+++ chromeos/network/network_state_handler.cc
@@ -6,7 +6,9 @@
 
 #include <stddef.h>
 
+#include <limits>
 #include <memory>
+#include <utility>
 
 #include "base/bind.h"
 #include "base/command_line.h"
@@ -515,7 +517,7 @@ void NetworkStateHandler::AppendTetherNetworksToList(bool get_active,
                                                      size_t limit,
                                                      NetworkStateList* list) {
   DCHECK(list);
-  DCHECK(limit != 0);
+  DCHECK_NE(0U, limit);
   if (!IsTechnologyEnabled(NetworkTypePattern::Tether()))
     return;
 
