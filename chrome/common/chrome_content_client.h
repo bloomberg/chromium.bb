35b166c0c7fed3896b4de8e1892b7e2f8c93ca22
Ben Wells <benwells@chromium.org>
2017-11-20 02:00:58 +0000

    Remove the internal extensions content settings provider.
    This was only used for plugin settings, which are not needed any more.
    
    Bug: 754178
    Change-Id: I24a9d1d08fd5a00fca088ed7ac212148af6c30e0
    Reviewed-on: https://chromium-review.googlesource.com/773898
    Commit-Queue: Ben Wells <benwells@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517742}
    Former-commit-id: 7bfd0bc891f489604cfe27d1a14f98450c84a420

--- chrome/common/chrome_content_client.h
+++ chrome/common/chrome_content_client.h
@@ -38,7 +38,6 @@ class ChromeContentClient : public content::ContentClient {
   static const char kPDFExtensionPluginName[];
   static const char kPDFInternalPluginName[];
   static const char kPDFPluginPath[];
-  static const char kRemotingViewerPluginPath[];
 
   ChromeContentClient();
   ~ChromeContentClient() override;
