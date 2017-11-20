3ff66d88396b31bb647ad8132b81029f23c5aea6
Takeshi Yoshino <tyoshino@chromium.org>
2017-11-20 07:45:24 +0000

    Revert "Enable video capture service by default on platforms where it is rolling"
    This reverts commit 572173da92aaec7e96188f891c4d0f4693255596 [formerly 1e2e99ef1f41d6f22f2f027dee2cc851d095eb2d].
    
    Reason for revert: Suspected to be the cause of fast/mediastream/MediaStreamTrack-gc-no-crash.html failure
    
    Original change's description:
    > Enable video capture service by default on platforms where it is rolling
    > out with M61.
    > 
    > Bug: 721812
    > Change-Id: I1de0f625a1fb106e2dea664675e94267e66fc2fd
    > Reviewed-on: https://chromium-review.googlesource.com/719416
    > Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    > Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#517761}
    
    TBR=emircan@chromium.org,chfremer@chromium.org
    
    Change-Id: Icdb2af1cf68e1774d57c1d8b3c529e86e0a2a48e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 721812
    Reviewed-on: https://chromium-review.googlesource.com/778603
    Reviewed-by: Takeshi Yoshino <tyoshino@chromium.org>
    Commit-Queue: Takeshi Yoshino <tyoshino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517776}
    Former-commit-id: edbcd5e7e3fa7ac24538d29244fa81405ff22581

--- services/video_capture/public/cpp/constants.cc
+++ services/video_capture/public/cpp/constants.cc
@@ -4,18 +4,9 @@
 
 #include "services/video_capture/public/cpp/constants.h"
 
-#include "build/build_config.h"
-
 namespace video_capture {
 
-const base::Feature kMojoVideoCapture {
-  "MojoVideoCapture",
-#if (defined(OS_LINUX) && !defined(OS_CHROMEOS)) || defined(OS_MACOSX) || \
-    defined(OS_WIN)
-      base::FEATURE_ENABLED_BY_DEFAULT
-#else
-      base::FEATURE_DISABLED_BY_DEFAULT
-#endif
-};
+const base::Feature kMojoVideoCapture{"MojoVideoCapture",
+                                      base::FEATURE_DISABLED_BY_DEFAULT};
 
 }  // namespace video_capture
