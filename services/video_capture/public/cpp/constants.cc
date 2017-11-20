572173da92aaec7e96188f891c4d0f4693255596
Christian Fremerey <chfremer@chromium.org>
2017-11-20 06:20:45 +0000

    Enable video capture service by default on platforms where it is rolling out with M61.
    Bug: 721812
    Change-Id: I1de0f625a1fb106e2dea664675e94267e66fc2fd
    Reviewed-on: https://chromium-review.googlesource.com/719416
    Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517761}
    Former-commit-id: 1e2e99ef1f41d6f22f2f027dee2cc851d095eb2d

--- services/video_capture/public/cpp/constants.cc
+++ services/video_capture/public/cpp/constants.cc
@@ -4,9 +4,18 @@
 
 #include "services/video_capture/public/cpp/constants.h"
 
+#include "build/build_config.h"
+
 namespace video_capture {
 
-const base::Feature kMojoVideoCapture{"MojoVideoCapture",
-                                      base::FEATURE_DISABLED_BY_DEFAULT};
+const base::Feature kMojoVideoCapture {
+  "MojoVideoCapture",
+#if (defined(OS_LINUX) && !defined(OS_CHROMEOS)) || defined(OS_MACOSX) || \
+    defined(OS_WIN)
+      base::FEATURE_ENABLED_BY_DEFAULT
+#else
+      base::FEATURE_DISABLED_BY_DEFAULT
+#endif
+};
 
 }  // namespace video_capture
