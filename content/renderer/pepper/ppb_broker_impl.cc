e314d2b52c0bdd12ca105c5e618a0fb97b2be989
Raymes Khoury <raymes@chromium.org>
2017-11-20 01:27:57 +0000

    Add metric to record actions associated with the Pepper Broker
    Records creation of the broker resource and calls to the Connect
    function.
    
    Bug: 756138
    Change-Id: Ib5624a6469123e156677d75d7dfe59c5b3a70677
    Reviewed-on: https://chromium-review.googlesource.com/773919
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Raymes Khoury <raymes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517741}
    Former-commit-id: 27d7d5f523cbee15622c69ff8bc37ef6eeffd52e

--- content/renderer/pepper/ppb_broker_impl.cc
+++ content/renderer/pepper/ppb_broker_impl.cc
@@ -5,6 +5,7 @@
 #include "content/renderer/pepper/ppb_broker_impl.h"
 
 #include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
 #include "content/common/frame_messages.h"
 #include "content/common/view_messages.h"
 #include "content/renderer/pepper/host_globals.h"
@@ -24,6 +25,8 @@ using ppapi::TrackedCallback;
 
 namespace content {
 
+enum class PepperBrokerAction { CREATE = 0, CONNECT = 1, NUM };
+
 // PPB_Broker_Impl ------------------------------------------------------
 
 PPB_Broker_Impl::PPB_Broker_Impl(PP_Instance instance)
@@ -33,6 +36,9 @@ PPB_Broker_Impl::PPB_Broker_Impl(PP_Instance instance)
       pipe_handle_(PlatformFileToInt(base::SyncSocket::kInvalidHandle)),
       routing_id_(RenderThreadImpl::current()->GenerateRoutingID()) {
   ChildThreadImpl::current()->GetRouter()->AddRoute(routing_id_, this);
+
+  UMA_HISTOGRAM_ENUMERATION("Pepper.BrokerAction", PepperBrokerAction::CREATE,
+                            PepperBrokerAction::NUM);
 }
 
 PPB_Broker_Impl::~PPB_Broker_Impl() {
@@ -51,6 +57,9 @@ PPB_Broker_API* PPB_Broker_Impl::AsPPB_Broker_API() { return this; }
 int32_t PPB_Broker_Impl::Connect(
     scoped_refptr<TrackedCallback> connect_callback) {
   // TODO(ddorwin): Return PP_ERROR_FAILED if plugin is in-process.
+
+  UMA_HISTOGRAM_ENUMERATION("Pepper.BrokerAction", PepperBrokerAction::CONNECT,
+                            PepperBrokerAction::NUM);
 
   if (broker_) {
     // May only be called once.
