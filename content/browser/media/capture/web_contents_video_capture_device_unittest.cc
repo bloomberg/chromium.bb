c9c86ff1de95d11a286b8aa564b26280f0f96464
Dave Tapuska <dtapuska@chromium.org>
2017-11-20 18:49:00 +0000

    Adjust RenderFrameHostManagerTest to use mojo input.
    Reland commit fd05dfb88ee1ca21821c121152f699ac9747d72e [formerly 26bf6f9f72276a3020941f422e54c8655d08fa08]. Fixed
    the WebContentsVideoCapture device test, it is the only other
    class directly creating a TestRenderViewHost which caused a
    CFI failure.
    
    SetFocus for non-proxy frames are sent input message over mojo.
    RenderFrameProxyHost based messages are still sent via
    InputMsg_SetFocus chrome IPC since there is no widget input handler
    for these situations.
    
    In order to grab the mojo input message we are required to subclass
    the RenderWidgetHostImpl to be able to provide a mock interface.
    
    BUG=722928
    TBR=nick@chromium.org
    
    Change-Id: I80e2fd8fee12619b87031ea18fe869de70ce01ac
    Reviewed-on: https://chromium-review.googlesource.com/779242
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517874}
    Former-commit-id: 8eccc8c7a2c0de051dea4b85309266c3bc7d8ee5

--- content/browser/media/capture/web_contents_video_capture_device_unittest.cc
+++ content/browser/media/capture/web_contents_video_capture_device_unittest.cc
@@ -22,6 +22,7 @@
 #include "content/browser/browser_thread_impl.h"
 #include "content/browser/frame_host/render_frame_host_impl.h"
 #include "content/browser/renderer_host/render_view_host_factory.h"
+#include "content/browser/renderer_host/render_widget_host_factory.h"
 #include "content/browser/renderer_host/render_widget_host_impl.h"
 #include "content/browser/renderer_host/render_widget_host_view_frame_subscriber.h"
 #include "content/browser/web_contents/web_contents_impl.h"
@@ -32,6 +33,7 @@
 #include "content/public/test/test_utils.h"
 #include "content/test/test_render_frame_host_factory.h"
 #include "content/test/test_render_view_host.h"
+#include "content/test/test_render_widget_host_factory.h"
 #include "content/test/test_web_contents.h"
 #include "media/base/video_frame.h"
 #include "media/base/video_util.h"
@@ -187,11 +189,10 @@ class CaptureTestRenderViewHost : public TestRenderViewHost {
                             int32_t main_frame_routing_id,
                             bool swapped_out)
       : TestRenderViewHost(instance,
-                           std::make_unique<RenderWidgetHostImpl>(
+                           TestRenderWidgetHost::Create(
                                widget_delegate,
                                instance->GetProcess(),
                                routing_id,
-                               nullptr,
                                false /* This means: "Is not hidden." */),
                            delegate,
                            main_frame_routing_id,
@@ -469,11 +470,15 @@ class WebContentsVideoCaptureDeviceTest : public testing::Test {
     // CopyFromSurfaceToVideoFrame functionality into TestRenderWidgetHostView
     // itself.
 
-    render_process_host_factory_.reset(new MockRenderProcessHostFactory());
+    render_process_host_factory_ =
+        std::make_unique<MockRenderProcessHostFactory>();
     // Create our (self-registering) RVH factory, so that when we create a
     // WebContents, it in turn creates CaptureTestRenderViewHosts.
-    render_view_host_factory_.reset(new CaptureTestRenderViewHostFactory());
-    render_frame_host_factory_.reset(new TestRenderFrameHostFactory());
+    render_view_host_factory_ =
+        std::make_unique<CaptureTestRenderViewHostFactory>();
+    render_frame_host_factory_ = std::make_unique<TestRenderFrameHostFactory>();
+    render_widget_host_factory_ =
+        std::make_unique<TestRenderWidgetHostFactory>();
 
     browser_context_.reset(new TestBrowserContext());
 
@@ -631,6 +636,9 @@ class WebContentsVideoCaptureDeviceTest : public testing::Test {
 
   // Self-registering RenderFrameHostFactory.
   std::unique_ptr<TestRenderFrameHostFactory> render_frame_host_factory_;
+
+  // Self-registering RenderWidgetHostFactory.
+  std::unique_ptr<TestRenderWidgetHostFactory> render_widget_host_factory_;
 
   // A mocked-out browser and tab.
   std::unique_ptr<TestBrowserContext> browser_context_;
