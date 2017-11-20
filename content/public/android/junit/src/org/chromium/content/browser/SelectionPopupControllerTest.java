48be680306e7fcf0bbc8f3e182e3f200a14e72df
Shimi Zhang <ctzsm@chromium.org>
2017-11-20 19:09:04 +0000

    Fix error prone warnings
    Since we switched to error prone to do static analysis, fixing warnings
    introduced by my changes.
    
    Bug: None
    Change-Id: I9b4e2593f217b424658e344957f8f4aadff2d2cd
    Reviewed-on: https://chromium-review.googlesource.com/777947
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517882}
    Former-commit-id: 7f395224ae1f018f5fc32e71b12e07a891ecde34

--- content/public/android/junit/src/org/chromium/content/browser/SelectionPopupControllerTest.java
+++ content/public/android/junit/src/org/chromium/content/browser/SelectionPopupControllerTest.java
@@ -55,7 +55,7 @@ public class SelectionPopupControllerTest {
     private static final String AMPHITHEATRE_FULL = "1600 Amphitheatre Parkway";
     private static final String AMPHITHEATRE = "Amphitheatre";
 
-    private class TestSelectionClient implements SelectionClient {
+    private static class TestSelectionClient implements SelectionClient {
         private SelectionClient.Result mResult;
         private SelectionClient.ResultCallback mResultCallback;
 
@@ -94,7 +94,7 @@ public class SelectionPopupControllerTest {
         }
     }
 
-    class SelectionClientOnlyReturnTrue extends TestSelectionClient {
+    private static class SelectionClientOnlyReturnTrue extends TestSelectionClient {
         @Override
         public boolean requestSelectionPopupUpdates(boolean shouldSuggest) {
             return true;
