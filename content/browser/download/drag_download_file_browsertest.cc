1967c62826d41e433991cb6534888a0da4ec8af1
Min Qin <qinmin@chromium.org>
2017-11-20 23:27:52 +0000

    Switch to use EmbeddedTestServer for DragDownloadFileTest
    URLRequestMockHTTPJob doesn't work with network service
    This CL migrates the test to use EmbeddedTestServer.
    So that the test can pass with/wihout network service enabled
    
    BUG=715630
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I4d380ebbe78f83ca59a921c8c1ff18fc5a598f16
    Reviewed-on: https://chromium-review.googlesource.com/776055
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517986}
    Former-commit-id: d5ec7d1e5b842420620d10b94eb8392cdaec5cf2

--- content/browser/download/drag_download_file_browsertest.cc
+++ content/browser/download/drag_download_file_browsertest.cc
@@ -7,6 +7,7 @@
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/message_loop/message_loop.h"
+#include "base/path_service.h"
 #include "content/browser/download/download_file_factory.h"
 #include "content/browser/download/download_file_impl.h"
 #include "content/browser/download/download_item_impl.h"
@@ -16,6 +17,7 @@
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/common/content_client.h"
+#include "content/public/common/content_paths.h"
 #include "content/public/test/content_browser_test.h"
 #include "content/public/test/content_browser_test_utils.h"
 #include "content/public/test/download_test_observer.h"
@@ -69,13 +71,10 @@ class DragDownloadFileTest : public ContentBrowserTest {
             shell()->web_contents()->GetBrowserContext()
             ->GetDownloadManagerDelegate());
     delegate->SetDownloadBehaviorForTesting(downloads_directory());
-  }
-
-  void SetUpServer() {
-    base::FilePath mock_base(GetTestFilePath("download", ""));
-    BrowserThread::PostTask(
-        BrowserThread::IO, FROM_HERE,
-        base::BindOnce(&net::URLRequestMockHTTPJob::AddUrlHandlers, mock_base));
+    base::FilePath test_data_dir;
+    ASSERT_TRUE(PathService::Get(content::DIR_TEST_DATA, &test_data_dir));
+    embedded_test_server()->ServeFilesFromDirectory(test_data_dir);
+    ASSERT_TRUE(embedded_test_server()->Start());
   }
 
   const base::FilePath& downloads_directory() const {
@@ -91,7 +90,8 @@ class DragDownloadFileTest : public ContentBrowserTest {
 IN_PROC_BROWSER_TEST_F(DragDownloadFileTest, DragDownloadFileTest_NetError) {
   base::FilePath name(downloads_directory().AppendASCII(
       "DragDownloadFileTest_NetError.txt"));
-  GURL url(net::URLRequestMockHTTPJob::GetMockUrl("download-test.lib"));
+  GURL url = embedded_test_server()->GetURL("/download/download-test.lib");
+  ASSERT_TRUE(embedded_test_server()->ShutdownAndWaitUntilComplete());
   Referrer referrer;
   std::string referrer_encoding;
   scoped_refptr<DragDownloadFile> file(
@@ -110,10 +110,9 @@ IN_PROC_BROWSER_TEST_F(DragDownloadFileTest, DragDownloadFileTest_NetError) {
 IN_PROC_BROWSER_TEST_F(DragDownloadFileTest, DragDownloadFileTest_Complete) {
   base::FilePath name(downloads_directory().AppendASCII(
         "DragDownloadFileTest_Complete.txt"));
-  GURL url(net::URLRequestMockHTTPJob::GetMockUrl("download-test.lib"));
+  GURL url = embedded_test_server()->GetURL("/download/download-test.lib");
   Referrer referrer;
   std::string referrer_encoding;
-  SetUpServer();
   scoped_refptr<DragDownloadFile> file(new DragDownloadFile(
       name, base::File(), url, referrer,
       referrer_encoding, shell()->web_contents()));
