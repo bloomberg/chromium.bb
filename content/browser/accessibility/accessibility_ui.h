33108b42666f6883b79f36bc909d9507601e8550
Dominic Mazzoni <dmazzoni@chromium.org>
2017-11-20 18:43:31 +0000

    Make the Accessibility WebUI page use a WebUIMessageHandler.
    No user-visible change, just modernizing the code behind the
    chrome://accessibility Accessibility Internals page.
    We were calling CallJavascriptFunctionUnsafe, which was unsafe because
    the page might not have been ready. Switch to using a WebUIMessageHandler
    so we can call AllowJavascript() when we get a message from the page,
    indicating we're sure it's ready, then future calls to CallJavascriptFunction
    will succeed. See https://goo.gl/qivavq for details.
    
    Bug: 785493
    Change-Id: I1623aae2be16b968ae8d7b17058348980637f02c
    Reviewed-on: https://chromium-review.googlesource.com/772876
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#517869}
    Former-commit-id: 35753f614a9fe0623c2cd778c8084156d153b0a4

--- content/browser/accessibility/accessibility_ui.h
+++ content/browser/accessibility/accessibility_ui.h
@@ -8,6 +8,7 @@
 #include "base/macros.h"
 #include "content/public/browser/web_ui_controller.h"
 #include "content/public/browser/web_ui_data_source.h"
+#include "content/public/browser/web_ui_message_handler.h"
 
 namespace base {
   class ListValue;
@@ -19,13 +20,21 @@ class AccessibilityUI : public WebUIController {
  public:
   explicit AccessibilityUI(WebUI* web_ui);
   ~AccessibilityUI() override;
+};
+
+class AccessibilityUIMessageHandler : public content::WebUIMessageHandler {
+ public:
+  AccessibilityUIMessageHandler();
+  ~AccessibilityUIMessageHandler() override;
+
+  void RegisterMessages() override;
 
  private:
   void ToggleAccessibility(const base::ListValue* args);
   void SetGlobalFlag(const base::ListValue* args);
   void RequestAccessibilityTree(const base::ListValue* args);
 
-  DISALLOW_COPY_AND_ASSIGN(AccessibilityUI);
+  DISALLOW_COPY_AND_ASSIGN(AccessibilityUIMessageHandler);
 };
 
 }  // namespace content
