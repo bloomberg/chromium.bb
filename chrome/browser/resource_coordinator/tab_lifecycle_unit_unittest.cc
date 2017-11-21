5138b53500ef873c567bdbeea0968e5f4255588e
Francois Doray <fdoray@chromium.org>
2017-11-21 22:03:17 +0000

    Add resource_coordinator::TabLifecycleUnit.
    A TabLifecycleUnit represents a tab.
    
    The implementation will be in a separate class (TabLifecyleUnitImpl)
    because we want to expose only part of the it outside of
    chrome/browser/resource_coordinator/.
    
    E.g. the extensions system is allowed call:
    
    TabLifecycleUnit::FromWebContents(contents)->Discard(condition);
    
    but we don't want it to change the TabStripModel associated with
    a tab (TabLifecycleUnitImpl::set_tab_strip_model()).
    
    Bug: 775644
    Change-Id: Idf2ad85bf2969871be1dc09eb1eb3776bc922959
    Reviewed-on: https://chromium-review.googlesource.com/774979
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518410}
    Former-commit-id: 414d24f7f85dd165674e4ab0c861b7d32971495f

---
+++ chrome/browser/resource_coordinator/tab_lifecycle_unit_unittest.cc
@@ -0,0 +1,73 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/resource_coordinator/tab_lifecycle_unit.h"
+
+#include "base/macros.h"
+#include "chrome/test/base/chrome_render_view_host_test_harness.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace resource_coordinator {
+
+namespace {
+
+class DummyTabLifecycleUnit : public TabLifecycleUnit {
+ public:
+  explicit DummyTabLifecycleUnit(content::WebContents* contents)
+      : contents_(contents) {
+    TabLifecycleUnit::SetForWebContents(contents_, this);
+  }
+
+  ~DummyTabLifecycleUnit() override {
+    TabLifecycleUnit::SetForWebContents(contents_, nullptr);
+  }
+
+  // TabLifecycleUnit:
+  content::WebContents* GetWebContents() const override { return nullptr; }
+  content::RenderProcessHost* GetRenderProcessHost() const override {
+    return nullptr;
+  }
+  bool IsMediaTab() const override { return false; }
+  bool IsAutoDiscardable() const override { return false; }
+  void SetAutoDiscardable(bool) override {}
+
+  // LifecycleUnit:
+  base::string16 GetTitle() const override { return base::string16(); }
+  std::string GetIconURL() const override { return std::string(); }
+  SortKey GetSortKey() const override { return SortKey(); }
+  State GetState() const override { return State::LOADED; }
+  int GetEstimatedMemoryFreedOnDiscardKB() const override { return 0; }
+  bool CanDiscard(DiscardCondition) const override { return false; }
+  bool Discard(DiscardCondition) override { return false; }
+
+ private:
+  content::WebContents* const contents_;
+
+  DISALLOW_COPY_AND_ASSIGN(DummyTabLifecycleUnit);
+};
+
+}  // namespace
+
+using TabLifecycleUnitTest = ChromeRenderViewHostTestHarness;
+
+TEST_F(TabLifecycleUnitTest, FromWebContentsNullByDefault) {
+  EXPECT_FALSE(TabLifecycleUnit::FromWebContents(web_contents()));
+}
+
+TEST_F(TabLifecycleUnitTest, SetForWebContents) {
+  {
+    DummyTabLifecycleUnit tab_lifecycle_unit(web_contents());
+    EXPECT_EQ(&tab_lifecycle_unit,
+              TabLifecycleUnit::FromWebContents(web_contents()));
+  }
+  EXPECT_FALSE(TabLifecycleUnit::FromWebContents(web_contents()));
+  {
+    DummyTabLifecycleUnit tab_lifecycle_unit(web_contents());
+    EXPECT_EQ(&tab_lifecycle_unit,
+              TabLifecycleUnit::FromWebContents(web_contents()));
+  }
+  EXPECT_FALSE(TabLifecycleUnit::FromWebContents(web_contents()));
+}
+
+}  // namespace resource_coordinator
