462a04acef84ed560ae4aee5336696c78e23f2f5
Eugene But <eugenebut@google.com>
2017-11-21 23:14:10 +0000

    Integrated DownloadController into CRWWebController.
    This CL completes ios/web Download API implementation. Integration test
    will be landed in a separate CL.
    
    Embedder does not set DownloadController delegate yet, so this CL does
    not trigger the second download.
    
    Design doc: http://go/ios-web-download-api
    
    Bug: 780646
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Icc9d5c5432a038237f80e7f35ddaa787dbb165fe
    Reviewed-on: https://chromium-review.googlesource.com/764890
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518431}
    Former-commit-id: d08f91c6044968b1472234f260a2e02ea1407e30

--- ios/web/web_state/ui/crw_web_controller_unittest.mm
+++ ios/web/web_state/ui/crw_web_controller_unittest.mm
@@ -22,7 +22,10 @@
 #import "ios/web/navigation/navigation_manager_impl.h"
 #import "ios/web/public/crw_navigation_item_storage.h"
 #import "ios/web/public/crw_session_storage.h"
+#import "ios/web/public/download/download_controller.h"
+#import "ios/web/public/download/download_task.h"
 #include "ios/web/public/referrer.h"
+#include "ios/web/public/test/fakes/fake_download_controller_delegate.h"
 #import "ios/web/public/test/fakes/test_native_content.h"
 #import "ios/web/public/test/fakes/test_native_content_provider.h"
 #import "ios/web/public/test/fakes/test_web_client.h"
@@ -37,6 +40,7 @@
 #include "ios/web/public/web_state/url_verification_constants.h"
 #include "ios/web/public/web_state/web_state_observer.h"
 #import "ios/web/test/fakes/crw_fake_back_forward_list.h"
+#import "ios/web/test/fakes/crw_fake_wk_navigation_response.h"
 #import "ios/web/test/web_test_with_web_controller.h"
 #import "ios/web/test/wk_web_view_crash_utils.h"
 #import "ios/web/web_state/ui/crw_web_controller_container_view.h"
@@ -59,6 +63,9 @@
 #error "This file requires ARC support."
 #endif
 
+using testing::WaitUntilConditionOrTimeout;
+using testing::kWaitForPageLoadTimeout;
+
 @interface CRWWebController (PrivateAPI)
 @property(nonatomic, readwrite) web::PageDisplayState pageDisplayState;
 @end
@@ -71,6 +78,8 @@ const char kInvalidURL[] = "http://%3";
 
 const char kTestURLString[] = "http://www.google.com/";
 const char kTestAppSpecificURL[] = "testwebui://test/";
+
+const char kTestMimeType[] = "application/vnd.test";
 
 // Returns HTML for an optionally zoomable test page with |zoom_state|.
 enum PageScalabilityType {
@@ -529,6 +538,88 @@ TEST_F(CRWWebControllerJSExecutionTest, WindowIdMissmatch) {
   EXPECT_FALSE(ExecuteJavaScript(@"window.test2"));
 }
 
+// Test fixture to test that DownloadControllerDelegate::OnDownloadCreated
+// callback is trigerred if CRWWebController can not display the response.
+class CRWWebControllerDownloadTest : public CRWWebControllerTest {
+ protected:
+  CRWWebControllerDownloadTest() : delegate_(download_controller()) {}
+
+  // Calls webView:decidePolicyForNavigationResponse:decisionHandler: callback
+  // and waits for decision handler call. Returns false if decision handler call
+  // times out.
+  bool CallDecidePolicyForNavigationResponseWithResponse(
+      NSURLResponse* response) WARN_UNUSED_RESULT {
+    CRWFakeWKNavigationResponse* navigation_response =
+        [[CRWFakeWKNavigationResponse alloc] init];
+    navigation_response.response = response;
+
+    // Wait for decidePolicyForNavigationResponse: callback.
+    __block bool callback_called = false;
+    [navigation_delegate_ webView:mock_web_view_
+        decidePolicyForNavigationResponse:navigation_response
+                          decisionHandler:^(WKNavigationResponsePolicy policy) {
+                            callback_called = true;
+                          }];
+    return WaitUntilConditionOrTimeout(kWaitForPageLoadTimeout, ^{
+      return callback_called;
+    });
+  }
+
+  DownloadController* download_controller() {
+    return DownloadController::FromBrowserState(GetBrowserState());
+  }
+
+  FakeDownloadControllerDelegate delegate_;
+};
+
+// Tests that webView:decidePolicyForNavigationResponse:decisionHandler: creates
+// the DownloadTask for NSURLResponse.
+TEST_F(CRWWebControllerDownloadTest, CreationWithNSURLResponse) {
+  // Simulate download response.
+  int64_t content_length = 10;
+  NSURLResponse* response =
+      [[NSURLResponse alloc] initWithURL:[NSURL URLWithString:@(kTestURLString)]
+                                MIMEType:@(kTestMimeType)
+                   expectedContentLength:content_length
+                        textEncodingName:nil];
+  ASSERT_TRUE(CallDecidePolicyForNavigationResponseWithResponse(response));
+
+  // Verify that download task was created.
+  ASSERT_EQ(1U, delegate_.alive_download_tasks().size());
+  DownloadTask* task = delegate_.alive_download_tasks()[0].second.get();
+  ASSERT_TRUE(task);
+  EXPECT_TRUE(task->GetIndentifier());
+  EXPECT_EQ(kTestURLString, task->GetOriginalUrl());
+  EXPECT_EQ(content_length, task->GetTotalBytes());
+  EXPECT_EQ("", task->GetContentDisposition());
+  EXPECT_EQ(kTestMimeType, task->GetMimeType());
+}
+
+// Tests that webView:decidePolicyForNavigationResponse:decisionHandler: creates
+// the DownloadTask for NSHTTPURLResponse.
+TEST_F(CRWWebControllerDownloadTest, CreationWithNSHTTPURLResponse) {
+  // Simulate download response.
+  const char kContentDisposition[] = "attachment; filename=download.test";
+  NSURLResponse* response = [[NSHTTPURLResponse alloc]
+       initWithURL:[NSURL URLWithString:@(kTestURLString)]
+        statusCode:200
+       HTTPVersion:nil
+      headerFields:@{
+        @"content-disposition" : @(kContentDisposition),
+      }];
+  ASSERT_TRUE(CallDecidePolicyForNavigationResponseWithResponse(response));
+
+  // Verify that download task was created.
+  ASSERT_EQ(1U, delegate_.alive_download_tasks().size());
+  DownloadTask* task = delegate_.alive_download_tasks()[0].second.get();
+  ASSERT_TRUE(task);
+  EXPECT_TRUE(task->GetIndentifier());
+  EXPECT_EQ(kTestURLString, task->GetOriginalUrl());
+  EXPECT_EQ(-1, task->GetTotalBytes());
+  EXPECT_EQ(kContentDisposition, task->GetContentDisposition());
+  EXPECT_EQ("", task->GetMimeType());
+}
+
 // Tests |currentURLWithTrustLevel:| method.
 TEST_F(CRWWebControllerTest, CurrentUrlWithTrustLevel) {
   AddPendingItem(GURL("http://chromium.test"), ui::PAGE_TRANSITION_TYPED);
@@ -576,10 +667,9 @@ class CRWWebControllerNativeContentTest : public WebTestWithWebController {
     // for navigation to finish before asserting.
     if (GetWebClient()->IsSlimNavigationManagerEnabled()) {
       TestWebStateObserver* observer_ptr = &observer;
-      ASSERT_TRUE(testing::WaitUntilConditionOrTimeout(
-          testing::kWaitForPageLoadTimeout, ^{
-            return observer_ptr->did_finish_navigation_info() != nullptr;
-          }));
+      ASSERT_TRUE(WaitUntilConditionOrTimeout(kWaitForPageLoadTimeout, ^{
+        return observer_ptr->did_finish_navigation_info() != nullptr;
+      }));
     }
   }
 
