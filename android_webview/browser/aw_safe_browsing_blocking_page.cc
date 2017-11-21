cc4b71b5f79d09b2051e87d28ba9cb9cb86e8a56
Nate Fischer <ntfschr@chromium.org>
2017-11-21 03:27:50 +0000

    AW: UMA metrics for Quiet vs Loud interstitials
    This CL adds UMA metrics to track how often we see particular
    interstitial types. Currently these are for QUIET_GIANT, QUIET_SMALL,
    and LOUD.
    
    This adds the metric for WebView only, since Chrome currently only uses
    loud interstitials.
    
    Bug: 713724
    Test: check that metrics are logged under chrome://histograms/SafeBrowsing
    Change-Id: I36af496078bf5a502a76b2cbdf481acf30a208bc
    Reviewed-on: https://chromium-review.googlesource.com/777945
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518081}
    Former-commit-id: 32bbd06a56b6048f101bcbe0e1d4763cc8718c54

--- android_webview/browser/aw_safe_browsing_blocking_page.cc
+++ android_webview/browser/aw_safe_browsing_blocking_page.cc
@@ -7,6 +7,7 @@
 #include "android_webview/browser/aw_browser_context.h"
 #include "android_webview/browser/aw_safe_browsing_ui_manager.h"
 #include "android_webview/browser/net/aw_url_request_context_getter.h"
+#include "base/metrics/histogram_macros.h"
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing/browser/threat_details.h"
 #include "components/safe_browsing/triggers/trigger_manager.h"
@@ -41,6 +42,8 @@ AwSafeBrowsingBlockingPage::AwSafeBrowsingBlockingPage(
                        std::move(controller_client),
                        display_options),
       threat_details_in_progress_(false) {
+  UMA_HISTOGRAM_ENUMERATION("SafeBrowsing.Interstitial.Type", errorUiType,
+                            ErrorUiType::COUNT);
   if (errorUiType == ErrorUiType::QUIET_SMALL ||
       errorUiType == ErrorUiType::QUIET_GIANT) {
     set_sb_error_ui(base::MakeUnique<SafeBrowsingQuietErrorUI>(
