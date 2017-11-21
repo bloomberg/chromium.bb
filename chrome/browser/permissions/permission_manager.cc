18c9ab360ab3a11c5fce5546346011c9c9791dbf
Dominick Ng <dominickn@chromium.org>
2017-11-21 04:31:39 +0000

    Remove unnecessary weak pointer in PermissionResponseCallback.
    This CL partially reverts crrev.com/c/773795, as the crash that CL was
    fixing was actually fixed in crbug.com/757394. Additionally, this CL
    removes the unnecessary weak pointer in PermissionResponseCallback, and
    replaces it with a raw pointer. This is valid as the weak pointer is
    misleading; PermissionManager must outline PermissionResponseCallback,
    and any implication that it does not is a potential bug.
    
    The removal of the weak pointer argument in PermissionResponseCallback
    also allows for the removal of the WeakPtrFactory in PermissionManager.
    
    BUG=751020
    
    Change-Id: I8b089d8126e3b34badb4460bf0e8297003a3bac5
    Reviewed-on: https://chromium-review.googlesource.com/778721
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518110}
    Former-commit-id: c12a0d32cb5a83b28a2295057e8870f7b0fb1c32

--- chrome/browser/permissions/permission_manager.cc
+++ chrome/browser/permissions/permission_manager.cc
@@ -210,20 +210,20 @@ class PermissionManager::PendingRequest {
 // be run when a permission prompt has been ignored, but it's important that we
 // know when a prompt is ignored to clean up |pending_requests_| correctly.
 // If the callback is destroyed without being run, the destructor here will
-// cancel the request to clean up.
+// cancel the request to clean up. |permission_manager| must outlive this
+// object.
 class PermissionManager::PermissionResponseCallback {
  public:
-  PermissionResponseCallback(
-      const base::WeakPtr<PermissionManager>& permission_manager,
-      int request_id,
-      int permission_id)
+  PermissionResponseCallback(PermissionManager* permission_manager,
+                             int request_id,
+                             int permission_id)
       : permission_manager_(permission_manager),
         request_id_(request_id),
         permission_id_(permission_id),
         request_answered_(false) {}
 
   ~PermissionResponseCallback() {
-    if (!request_answered_ && permission_manager_.get() &&
+    if (!request_answered_ &&
         permission_manager_->pending_requests_.Lookup(request_id_)) {
       permission_manager_->pending_requests_.Remove(request_id_);
     }
@@ -231,17 +231,14 @@ class PermissionManager::PermissionResponseCallback {
 
   void OnPermissionsRequestResponseStatus(ContentSetting content_setting) {
     request_answered_ = true;
-    if (permission_manager_.get()) {
-      permission_manager_->OnPermissionsRequestResponseStatus(
-          request_id_, permission_id_, content_setting);
-    }
+    permission_manager_->OnPermissionsRequestResponseStatus(
+        request_id_, permission_id_, content_setting);
   }
 
  private:
-  base::WeakPtr<PermissionManager> permission_manager_;
+  PermissionManager* permission_manager_;
   int request_id_;
   int permission_id_;
-
   bool request_answered_;
 
   DISALLOW_COPY_AND_ASSIGN(PermissionResponseCallback);
@@ -260,9 +257,7 @@ PermissionManager* PermissionManager::Get(Profile* profile) {
   return PermissionManagerFactory::GetForProfile(profile);
 }
 
-PermissionManager::PermissionManager(Profile* profile)
-    : profile_(profile),
-      weak_ptr_factory_(this) {
+PermissionManager::PermissionManager(Profile* profile) : profile_(profile) {
   permission_contexts_[CONTENT_SETTINGS_TYPE_MIDI_SYSEX] =
       base::MakeUnique<MidiSysexPermissionContext>(profile);
   permission_contexts_[CONTENT_SETTINGS_TYPE_MIDI] =
@@ -371,8 +366,8 @@ int PermissionManager::RequestPermissions(
 
     PermissionContextBase* context = GetPermissionContext(permission);
     DCHECK(context);
-    auto callback = base::MakeUnique<PermissionResponseCallback>(
-        weak_ptr_factory_.GetWeakPtr(), request_id, i);
+    auto callback =
+        base::MakeUnique<PermissionResponseCallback>(this, request_id, i);
     context->RequestPermission(
         web_contents, request, canonical_requesting_origin, user_gesture,
         base::Bind(
