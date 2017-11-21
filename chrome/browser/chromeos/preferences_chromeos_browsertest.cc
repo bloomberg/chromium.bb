9d9d529b2a8e142c092bc824fc46ddcbf96f03b0
Jonathan <jonross@chromium.org>
2017-11-21 17:12:40 +0000

    Disable flaking PreferencesTest.MultiProfiles Everywhere
    PreferencesTest.MultiProfiles is flaking both on ChromeOS as well as with mash.
    This change disables the test everywhere until the flake can be resolved.
    
    This also reverts having the test filtered out for mash.
    
    TBR=jamescook@chromium.org
    TEST=PreferencesTest,MultiProfiles
    
    Bug: 787050
    Change-Id: Iaa651e1dd23b836f6d8354fbe9e9d2834f855e0b
    Reviewed-on: https://chromium-review.googlesource.com/782041
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518285}
    Former-commit-id: 84c9ecb20735c69ed258ac33b8208b8c99365866

--- chrome/browser/chromeos/preferences_chromeos_browsertest.cc
+++ chrome/browser/chromeos/preferences_chromeos_browsertest.cc
@@ -213,7 +213,8 @@ IN_PROC_BROWSER_TEST_F(PreferencesTest, PRE_MultiProfiles) {
   chromeos::StartupUtils::MarkOobeCompleted();
 }
 
-IN_PROC_BROWSER_TEST_F(PreferencesTest, MultiProfiles) {
+// This test is flaking both with and without mash. http://crbug.com/787050
+IN_PROC_BROWSER_TEST_F(PreferencesTest, DISABLED_MultiProfiles) {
   user_manager::UserManager* user_manager = user_manager::UserManager::Get();
 
   // Add first user and init its preferences. Check that corresponding
