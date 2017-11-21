880b740f15871908202286b2f11d2609991c9ae0
Adrian Elder <aelder@chromium.org>
2017-11-21 20:55:46 +0000

    Permit some apps to call startAudioDebugRecordings without a flag.
    Before this change, webrtcLoggingPrivate.startAudioDebugRecordings was only
    available if switches::kEnableAudioDebugRecordingsFromExtension was present.
    
    This change adds new API permissions that permit creating audio debug recordings
    without that flag. These permissions are only granted to whitelisted apps
    installed on internal Hotrods.
    
    Bug: 777906
    Change-Id: I42d583f1a58d86c351cc41e2cd261bff4b96be9f
    Reviewed-on: https://chromium-review.googlesource.com/766708
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Greg Levin <glevin@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Adrian Elder <aelder@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518385}
    Former-commit-id: cf947e084cfb8f06bf8b75fa3bab4e3b90fda696

--- extensions/common/permissions/api_permission.h
+++ extensions/common/permissions/api_permission.h
@@ -249,6 +249,7 @@ class APIPermission {
     kNewTabPageOverride,
     kDeclarativeNetRequest,
     kLockWindowFullscreenPrivate,
+    kWebrtcLoggingPrivateAudioDebug,
     // Last entry: Add new entries above and ensure to update the
     // "ExtensionPermission3" enum in tools/metrics/histograms/histograms.xml
     // (by running update_extension_permission.py).
