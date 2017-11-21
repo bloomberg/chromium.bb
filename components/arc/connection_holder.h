f79c69d4759886f16fe5e487ab683ad439b691f2
Hidehiko Abe <hidehiko@chromium.org>
2017-11-21 13:27:49 +0000

    Extract ConnectionHolderImpl.
    ConnectionHolder will support full duplex connection in a following
    CL. To switch the implementation between single direction connection
    and full duplex connection, this CL introduces Impl class.
    
    BUG=750563
    TEST=Ran bots.
    
    Change-Id: I2037f541b12a91ca5fe8e2b32b6b1fb59a5010a4
    Reviewed-on: https://chromium-review.googlesource.com/776734
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518232}
    Former-commit-id: 61350cba299e84239d215ff86c7cce2630b5f0e2

--- components/arc/connection_holder.h
+++ components/arc/connection_holder.h
@@ -29,21 +29,72 @@
 
 namespace arc {
 
+namespace internal {
+
+// This is the core part of ConnectionHolder, which implementes the connection
+// initialization.
+// Currently, this supports only single direction connection. When Instance
+// connection is established from ARC container via ArcBridgeService,
+// the Instance proxy will be set.
+// TODO(hidehiko): Support full-duplex connection.
+template <typename InstanceType>
+class ConnectionHolderImpl {
+ public:
+  explicit ConnectionHolderImpl(ConnectionNotifier* connection_notifier)
+      : connection_notifier_(connection_notifier) {}
+
+  InstanceType* instance() { return instance_; }
+  uint32_t instance_version() const { return instance_version_; }
+
+  // For single direction connection, when Instance proxy is obtained,
+  // it means connected.
+  bool IsConnected() const { return instance_; }
+
+  // Sets (or resets if |instance| is nullptr) the instance.
+  void SetInstance(InstanceType* instance,
+                   uint32_t version = InstanceType::version_) {
+    DCHECK(instance == nullptr || instance_ == nullptr);
+
+    // Note: This can be called with nullptr even if |instance_| is still
+    // nullptr for just in case clean up purpose. No-op in such a case.
+    if (instance == instance_)
+      return;
+
+    instance_ = instance;
+    instance_version_ = version;
+    if (instance_)
+      connection_notifier_->NotifyConnectionReady();
+    else
+      connection_notifier_->NotifyConnectionClosed();
+  }
+
+ private:
+  // This class does not have ownership. The pointers should be managed by the
+  // caller.
+  ConnectionNotifier* const connection_notifier_;
+  InstanceType* instance_ = nullptr;
+  uint32_t instance_version_ = 0;
+
+  DISALLOW_COPY_AND_ASSIGN(ConnectionHolderImpl);
+};
+
+}  // namespace internal
+
 // Holds a Mojo instance+version pair. This also allows for listening for state
-// changes for the particular instance. T should be a Mojo interface type
-// (arc::mojom::XxxInstance).
-template <typename T>
+// changes for the particular instance. InstanceType should be a Mojo
+// interface type (arc::mojom::XxxInstance).
+template <typename InstanceType>
 class ConnectionHolder {
  public:
-  using Observer = ConnectionObserver<T>;
-  using Instance = T;
+  using Observer = ConnectionObserver<InstanceType>;
+  using Instance = InstanceType;
 
   ConnectionHolder() = default;
 
   // Returns true if the Mojo interface is ready at least for its version 0
   // interface. Use an Observer if you want to be notified when this is ready.
   // This can only be called on the thread that this class was created on.
-  bool IsConnected() const { return instance_; }
+  bool IsConnected() const { return impl_.IsConnected(); }
 
   // Gets the Mojo interface that's intended to call for
   // |method_name_for_logging|, but only if its reported version is at least
@@ -51,21 +102,21 @@ class ConnectionHolder {
   // not have the requested version, and logs appropriately.
   // This function should not be called directly. Instead, use the
   // ARC_GET_INSTANCE_FOR_METHOD() macro.
-  T* GetInstanceForVersionDoNotCallDirectly(
+  InstanceType* GetInstanceForVersionDoNotCallDirectly(
       uint32_t min_version,
       const char method_name_for_logging[]) {
-    if (!instance_) {
-      VLOG(1) << "Instance " << T::Name_ << " not available.";
+    if (!impl_.IsConnected()) {
+      VLOG(1) << "Instance " << InstanceType::Name_ << " not available.";
       return nullptr;
     }
-    if (version_ < min_version) {
-      LOG(ERROR) << "Instance for " << T::Name_
+    if (impl_.instance_version() < min_version) {
+      LOG(ERROR) << "Instance for " << InstanceType::Name_
                  << "::" << method_name_for_logging
                  << " version mismatch. Expected " << min_version << " got "
-                 << version_;
+                 << impl_.instance_version();
       return nullptr;
     }
-    return instance_;
+    return impl_.instance();
   }
 
   // Adds or removes observers. This can only be called on the thread that this
@@ -73,7 +124,7 @@ class ConnectionHolder {
   // the list.
   void AddObserver(Observer* observer) {
     connection_notifier_.AddObserver(observer);
-    if (IsConnected())
+    if (impl_.IsConnected())
       connection_notifier_.NotifyConnectionReady();
   }
 
@@ -84,31 +135,16 @@ class ConnectionHolder {
   // Sets |instance| with |version|.
   // This can be called in both case; on ready, and on closed.
   // Passing nullptr to |instance| means closing.
-  void SetInstance(T* instance, uint32_t version = T::Version_) {
+  void SetInstance(InstanceType* instance,
+                   uint32_t version = InstanceType::Version_) {
     DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
-    DCHECK(instance == nullptr || instance_ == nullptr);
-
-    // Note: This can be called with nullptr even if |instance_| is still
-    // nullptr for just in case clean up purpose. No-op in such a case.
-    if (instance == instance_)
-      return;
-
-    instance_ = instance;
-    version_ = version;
-    if (instance_)
-      connection_notifier_.NotifyConnectionReady();
-    else
-      connection_notifier_.NotifyConnectionClosed();
+    impl_.SetInstance(instance, version);
   }
 
  private:
-  // This class does not have ownership. The pointers should be managed by the
-  // callee.
-  T* instance_ = nullptr;
-  uint32_t version_ = 0;
-
   THREAD_CHECKER(thread_checker_);
   internal::ConnectionNotifier connection_notifier_;
+  internal::ConnectionHolderImpl<InstanceType> impl_{&connection_notifier_};
 
   DISALLOW_COPY_AND_ASSIGN(ConnectionHolder);
 };
