6ea84b916de9c45307a0b5368882de823b2da365
Francois Doray <fdoray@chromium.org>
2017-11-21 21:46:53 +0000

    Add resource_coordinator::LifecycleUnit.
    A LifecycleUnit represents a unit that can switch between the "alive" and
    "discarded" states. When it is alive, the unit uses system resources and
    provides functionality to the user. When it is discarded, the unit doesn't
    use any system resource.
    
    Bug: 775644
    Change-Id: I32dcf6a2a5a06919154ebcc48a73f88eecca9a8c
    Reviewed-on: https://chromium-review.googlesource.com/773078
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518405}
    Former-commit-id: 306bae8fe866545fe64561ef2edec471f86276fc

---
+++ chrome/browser/resource_coordinator/lifecycle_unit.h
@@ -0,0 +1,93 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_RESOURCE_COORDINATOR_LIFECYCLE_UNIT_H_
+#define CHROME_BROWSER_RESOURCE_COORDINATOR_LIFECYCLE_UNIT_H_
+
+#include <string>
+
+#include "base/containers/flat_set.h"
+#include "base/strings/string16.h"
+#include "base/time/time.h"
+#include "chrome/browser/resource_coordinator/discard_condition.h"
+
+namespace resource_coordinator {
+
+// A LifecycleUnit represents a unit that can switch between the "loaded" and
+// "discarded" states. When it is loaded, the unit uses system resources and
+// provides functionality to the user. When it is discarded, the unit doesn't
+// use any system resource.
+class LifecycleUnit {
+ public:
+  enum class State {
+    // The LifecycleUnit is using system resources.
+    LOADED,
+    // The LifecycleUnit is not using system resources.
+    DISCARDED,
+  };
+
+  // Used to sort LifecycleUnit by importance. The most important LifecycleUnit
+  // has the greatest SortKey.
+  struct SortKey {
+    SortKey();
+    explicit SortKey(base::TimeTicks last_focused_time);
+
+    bool operator<(const SortKey& other) const;
+    bool operator>(const SortKey& other) const;
+
+    // Last time at which the LifecycleUnit was focused. base::TimeTicks::Max()
+    // if the LifecycleUnit is currently focused.
+    base::TimeTicks last_focused_time;
+  };
+
+  virtual ~LifecycleUnit() = default;
+
+  // Returns a title describing this LifecycleUnit, or an empty string if no
+  // title is available.
+  virtual base::string16 GetTitle() const = 0;
+
+  // Returns the URL of an icon for this LifecycleUnit, or an empty string if no
+  // icon is available.
+  virtual std::string GetIconURL() const = 0;
+
+  // Returns a key that can be used to evaluate the relative importance of this
+  // LifecycleUnit.
+  //
+  // TODO(fdoray): Figure out if GetSortKey() and CanDiscard() should be
+  // replaced with a method that returns a numeric value representing the
+  // expected user pain caused by a discard. A values above a given threshold
+  // would be equivalent to CanDiscard() returning false for a given
+  // DiscardCondition. https://crbug.com/775644
+  virtual SortKey GetSortKey() const = 0;
+
+  // Returns the current state of this LifecycleUnit.
+  virtual State GetState() const = 0;
+
+  // Returns the estimated number of kilobytes that would be freed if this
+  // LifecycleUnit was discarded.
+  //
+  // TODO(fdoray): Consider exposing this only on a new class that represents a
+  // group of LifecycleUnits. It is easier to compute memory consumption
+  // accurately for a group of LifecycleUnits that live in the same process(es)
+  // than for individual LifecycleUnits. https://crbug.com/775644
+  virtual int GetEstimatedMemoryFreedOnDiscardKB() const = 0;
+
+  // Returns true if this LifecycleUnit can be discared.
+  virtual bool CanDiscard(DiscardCondition discard_condition) const = 0;
+
+  // Discards this LifecycleUnit.
+  //
+  // TODO(fdoray): Consider handling urgent discard with groups of
+  // LifecycleUnits. On urgent discard, we want to minimize memory accesses. It
+  // is easier to achieve that if we discard a group of LifecycleUnits that live
+  // in the same process(es) than if we discard individual LifecycleUnits.
+  // https://crbug.com/775644
+  virtual bool Discard(DiscardCondition discard_condition) = 0;
+};
+
+using LifecycleUnitSet = base::flat_set<LifecycleUnit*>;
+
+}  // namespace resource_coordinator
+
+#endif  // CHROME_BROWSER_RESOURCE_COORDINATOR_LIFECYCLE_UNIT_H_
