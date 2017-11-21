d33088a4150d8026140e76bb96020bace4f6752f
Daichi Hirono <hirono@chromium.org>
2017-11-21 06:52:12 +0000

    Add ScopedDataOffer helper.
    ScopedDataOffer is a pointer for DataOffer with observing target
    events.  This is useful because DataOffer in DataDevice is not const and
    needs to register/unregister observer everytime the pointer refers
    different object.
    
    Bug: 773978
    Test: exo_unittests
    Change-Id: I48586a70f5f234cd0f874faa58652820a2543fd9
    Reviewed-on: https://chromium-review.googlesource.com/776340
    Commit-Queue: Daichi Hirono <hirono@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518160}
    Former-commit-id: 4252b6f6e11eca63f0ee895744566d19f09ba102

--- components/exo/data_device.cc
+++ components/exo/data_device.cc
@@ -14,15 +14,30 @@
 
 namespace exo {
 
+class ScopedDataOffer {
+ public:
+  ScopedDataOffer(DataOffer* data_offer, DataOfferObserver* observer)
+      : data_offer_(data_offer), observer_(observer) {
+    data_offer_->AddObserver(observer_);
+  }
+  ~ScopedDataOffer() { data_offer_->RemoveObserver(observer_); }
+  DataOffer* get() { return data_offer_; }
+
+ private:
+  DataOffer* const data_offer_;
+  DataOfferObserver* const observer_;
+
+  DISALLOW_COPY_AND_ASSIGN(ScopedDataOffer);
+};
+
 DataDevice::DataDevice(DataDeviceDelegate* delegate, FileHelper* file_helper)
-    : delegate_(delegate), file_helper_(file_helper), data_offer_(nullptr) {
+    : delegate_(delegate), file_helper_(file_helper) {
   WMHelper::GetInstance()->AddDragDropObserver(this);
 }
 
 DataDevice::~DataDevice() {
   delegate_->OnDataDeviceDestroying(this);
   WMHelper::GetInstance()->RemoveDragDropObserver(this);
-  ClearDataOffer();
 }
 
 void DataDevice::StartDrag(const DataSource* source_resource,
@@ -56,12 +71,12 @@ void DataDevice::OnDragEntered(const ui::DropTargetEvent& event) {
     dnd_actions.insert(DndAction::kAsk);
   }
 
-  data_offer_ = delegate_->OnDataOffer();
-  data_offer_->AddObserver(this);
-  data_offer_->SetDropData(file_helper_, event.data());
-  data_offer_->SetSourceActions(dnd_actions);
-  data_offer_->SetActions(base::flat_set<DndAction>(), DndAction::kAsk);
-  delegate_->OnEnter(surface, event.location_f(), *data_offer_);
+  data_offer_ =
+      std::make_unique<ScopedDataOffer>(delegate_->OnDataOffer(), this);
+  data_offer_->get()->SetDropData(file_helper_, event.data());
+  data_offer_->get()->SetSourceActions(dnd_actions);
+  data_offer_->get()->SetActions(base::flat_set<DndAction>(), DndAction::kAsk);
+  delegate_->OnEnter(surface, event.location_f(), *data_offer_->get());
 }
 
 int DataDevice::OnDragUpdated(const ui::DropTargetEvent& event) {
@@ -72,7 +87,7 @@ int DataDevice::OnDragUpdated(const ui::DropTargetEvent& event) {
 
   // TODO(hirono): dnd_action() here may not be updated. Chrome needs to provide
   // a way to update DND action asynchronously.
-  switch (data_offer_->dnd_action()) {
+  switch (data_offer_->get()->dnd_action()) {
     case DndAction::kMove:
       return ui::DragDropTypes::DRAG_MOVE;
     case DndAction::kCopy:
@@ -89,7 +104,7 @@ void DataDevice::OnDragExited() {
     return;
 
   delegate_->OnLeave();
-  ClearDataOffer();
+  data_offer_.reset();
 }
 
 int DataDevice::OnPerformDrop(const ui::DropTargetEvent& event) {
@@ -97,13 +112,13 @@ int DataDevice::OnPerformDrop(const ui::DropTargetEvent& event) {
     return ui::DragDropTypes::DRAG_NONE;
 
   delegate_->OnDrop();
-  ClearDataOffer();
+  data_offer_.reset();
   return ui::DragDropTypes::DRAG_NONE;
 }
 
 void DataDevice::OnDataOfferDestroying(DataOffer* data_offer) {
-  if (data_offer_ == data_offer)
-    ClearDataOffer();
+  if (data_offer_ && data_offer_->get() == data_offer)
+    data_offer_.reset();
 }
 
 Surface* DataDevice::GetEffectiveTargetForEvent(
@@ -116,13 +131,6 @@ Surface* DataDevice::GetEffectiveTargetForEvent(
     return nullptr;
 
   return delegate_->CanAcceptDataEventsForSurface(target) ? target : nullptr;
-}
-
-void DataDevice::ClearDataOffer() {
-  if (!data_offer_)
-    return;
-  data_offer_->RemoveObserver(this);
-  data_offer_ = nullptr;
 }
 
 }  // namespace exo
