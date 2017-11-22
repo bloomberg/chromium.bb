93b5a923c7092857ee78bcc836fd57f668d5004c
Christopher Grant <cjgrant@chromium.org>
2017-11-22 22:02:05 +0000

    VR: Introduce an initial TextInput UI element
    This element will evolve, but initially it is a bridge between the
    keyboard and omnibox functionality.
    
    BUG=
    R=ymalik
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ia697f0d651948a591cdc5378c21df4bcfa9a0333
    Reviewed-on: https://chromium-review.googlesource.com/786112
    Reviewed-by: Yash Malik <ymalik@chromium.org>
    Commit-Queue: Yash Malik <ymalik@chromium.org>
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518764}
    Former-commit-id: d27faaacef94deeae435c2d1e3f257497a3a7901

---
+++ chrome/browser/vr/elements/text_input.cc
@@ -0,0 +1,126 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/vr/elements/text_input.h"
+
+#include "base/memory/ptr_util.h"
+#include "cc/paint/skia_paint_canvas.h"
+#include "chrome/browser/vr/elements/ui_texture.h"
+#include "ui/gfx/canvas.h"
+#include "ui/gfx/font_list.h"
+#include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/geometry/rect_f.h"
+#include "ui/gfx/render_text.h"
+
+namespace vr {
+
+class TextInputTexture : public UiTexture {
+ public:
+  TextInputTexture(float font_height, float text_width)
+      : font_height_(font_height), text_width_(text_width) {}
+  ~TextInputTexture() override {}
+
+  void SetText(const base::string16& text) { SetAndDirty(&text_, text); }
+
+  void SetCursorPosition(int position) {
+    SetAndDirty(&cursor_position_, position);
+  }
+
+  void SetColor(SkColor color) { SetAndDirty(&color_, color); }
+
+  void SetCursorVisible(bool visible) {
+    SetAndDirty(&cursor_visible_, visible);
+  }
+
+  void Draw(SkCanvas* sk_canvas, const gfx::Size& texture_size) override {
+    cc::SkiaPaintCanvas paint_canvas(sk_canvas);
+    gfx::Canvas gfx_canvas(&paint_canvas, 1.0f);
+    gfx::Canvas* canvas = &gfx_canvas;
+
+    gfx::FontList font_list;
+    float pixels_per_meter = texture_size.width() / text_width_;
+    int pixel_font_height = static_cast<int>(font_height_ * pixels_per_meter);
+    GetFontList(pixel_font_height, text_, &font_list);
+    gfx::Rect text_bounds(texture_size.width(), pixel_font_height);
+    size_ = gfx::SizeF(text_bounds.size());
+
+    std::unique_ptr<gfx::RenderText> render_text(CreateRenderText());
+    render_text->SetText(text_);
+    render_text->SetFontList(font_list);
+    render_text->SetColor(color_);
+    render_text->SetHorizontalAlignment(gfx::ALIGN_LEFT);
+    render_text->SetDisplayRect(text_bounds);
+    if (cursor_visible_) {
+      render_text->SetCursorEnabled(true);
+      render_text->SetCursorPosition(cursor_position_);
+    }
+    render_text->Draw(canvas);
+
+    if (cursor_visible_) {
+      auto bounds = render_text->GetUpdatedCursorBounds();
+      canvas->DrawRect(gfx::RectF(bounds), 0xFF000080);
+    }
+  }
+
+ private:
+  gfx::Size GetPreferredTextureSize(int width) const override {
+    return gfx::Size(width, width * font_height_ / text_width_);
+  }
+
+  gfx::SizeF GetDrawnSize() const override { return size_; }
+
+  gfx::SizeF size_;
+  base::string16 text_;
+  int cursor_position_ = 0;
+  float font_height_;
+  float text_width_;
+  SkColor color_ = SK_ColorBLACK;
+  bool cursor_visible_ = false;
+
+  DISALLOW_COPY_AND_ASSIGN(TextInputTexture);
+};
+
+TextInput::TextInput(int maximum_width_pixels,
+                     float font_height_meters,
+                     float text_width_meters)
+    : TexturedElement(maximum_width_pixels),
+      texture_(base::MakeUnique<TextInputTexture>(font_height_meters,
+                                                  text_width_meters)) {
+  SetSize(text_width_meters, font_height_meters);
+}
+TextInput::~TextInput() {}
+
+void TextInput::SetText(const base::string16& text) {
+  if (text_ == text)
+    return;
+  text_ = text;
+  texture_->SetText(text);
+  if (text_changed_callback_)
+    text_changed_callback_.Run(text);
+}
+
+void TextInput::SetCursorPosition(int position) {
+  texture_->SetCursorPosition(position);
+}
+
+void TextInput::SetTextChangedCallback(const TextInputCallback& callback) {
+  text_changed_callback_ = callback;
+}
+
+void TextInput::SetColor(SkColor color) {
+  texture_->SetColor(color);
+}
+
+bool TextInput::OnBeginFrame(const base::TimeTicks& time,
+                             const gfx::Vector3dF& look_at) {
+  base::TimeDelta delta = time - base::TimeTicks();
+  texture_->SetCursorVisible(delta.InMilliseconds() / 500 % 2);
+  return false;
+}
+
+UiTexture* TextInput::GetTexture() const {
+  return texture_.get();
+}
+
+}  // namespace vr
