402dc4b4d405dbb2194445cfab60069c46e63f12
Wez <wez@chromium.org>
2017-11-22 00:17:45 +0000

    Add AltGraph to the set of modifier flags known to Accelerators.
    ui::Accelerator instances built from ui::Events with the AltGraph flag
    set lose that modifier, leading to potential for confusion with other
    accelerator keys, e.g: both the Alt and AltGraph keys generate events
    with key_code=VKEY_MENU, and may only be distinguished by the modifiers
    set on the events.
    
    This CL adds knowledge of AltGraph to the ui::Accelerator constructors,
    and migrates the CreateAcceleratorFromNativeKeyboardEvent() helper to
    use the //ui/events/blink utility functions to translate modifier
    flags.
    
    Bug: 787137, 25503
    Change-Id: I8bde31947b99d9ae113ed5a38053ae8c5e46d32f
    Reviewed-on: https://chromium-review.googlesource.com/780449
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518472}
    Former-commit-id: e66269c362f753cd57f5bf5bc8d33c7c81bbbdce

--- ui/content_accelerators/accelerator_util.cc
+++ ui/content_accelerators/accelerator_util.cc
@@ -6,40 +6,17 @@
 
 #include "build/build_config.h"
 #include "third_party/WebKit/public/platform/WebInputEvent.h"
+#include "ui/events/blink/blink_event_util.h"
 #include "ui/events/event.h"
 #include "ui/events/event_constants.h"
 
 namespace ui {
 
-namespace {
-
-int GetModifiersFromNativeWebKeyboardEvent(
-    const content::NativeWebKeyboardEvent& event) {
-  int modifiers = ui::EF_NONE;
-  if (event.GetModifiers() & content::NativeWebKeyboardEvent::kShiftKey)
-    modifiers |= ui::EF_SHIFT_DOWN;
-  if (event.GetModifiers() & content::NativeWebKeyboardEvent::kControlKey)
-    modifiers |= ui::EF_CONTROL_DOWN;
-  if (event.GetModifiers() & content::NativeWebKeyboardEvent::kAltKey)
-    modifiers |= ui::EF_ALT_DOWN;
-#if defined(OS_MACOSX) || defined(OS_CHROMEOS)
-  if (event.GetModifiers() & content::NativeWebKeyboardEvent::kMetaKey)
-    modifiers |= ui::EF_COMMAND_DOWN;
-#endif
-#if defined(USE_AURA)
-  if (event.os_event && static_cast<ui::KeyEvent*>(event.os_event)->is_repeat())
-    modifiers |= ui::EF_IS_REPEAT;
-#endif
-  return modifiers;
-}
-
-}  // namespace
-
 ui::Accelerator GetAcceleratorFromNativeWebKeyboardEvent(
     const content::NativeWebKeyboardEvent& event) {
   ui::Accelerator accelerator(
       static_cast<ui::KeyboardCode>(event.windows_key_code),
-      GetModifiersFromNativeWebKeyboardEvent(event));
+      WebEventModifiersToEventFlags(event.GetModifiers()));
   if (event.GetType() == blink::WebInputEvent::kKeyUp)
     accelerator.set_key_state(Accelerator::KeyState::RELEASED);
   return accelerator;
