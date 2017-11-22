96f2596b3e9d29715e5da165f6cd2f903d9e89de
Andrew Grieve <agrieve@chromium.org>
2017-11-22 17:04:46 +0000

    Move RevenueStats.getInstance() back to being lazy
    I had missed that there is actually a public static method on the class
    that doesn't call getInstance(), and it's causing the class to be
    initialized on a background thread (and thus crash sometimes)
    
    Bug: 786967
    Change-Id: I602fd97f8bb7d1ad30ee6e69599e22f426e50c29
    Reviewed-on: https://chromium-review.googlesource.com/784210
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518660}
    Former-commit-id: 8e2dc71bc861313bf465469b81639545723d38c3

--- chrome/android/java/src/org/chromium/chrome/browser/rlz/RevenueStats.java
+++ chrome/android/java/src/org/chromium/chrome/browser/rlz/RevenueStats.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 import org.chromium.base.ContextUtils;
+import org.chromium.base.ThreadUtils;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.chrome.browser.AppHooks;
 import org.chromium.chrome.browser.tab.Tab;
@@ -19,12 +20,17 @@ import org.chromium.chrome.browser.tab.Tab;
 public class RevenueStats {
     private static final String PREF_RLZ_NOTIFIED = "rlz_first_search_notified";
 
-    private static final RevenueStats sInstance = AppHooks.get().createRevenueStatsInstance();
+    private static RevenueStats sInstance;
 
     /**
      * Returns the singleton instance of ExternalAuthUtils, creating it if needed.
      */
     public static RevenueStats getInstance() {
+        assert ThreadUtils.runningOnUiThread();
+        if (sInstance == null) {
+            sInstance = AppHooks.get().createRevenueStatsInstance();
+        }
+
         return sInstance;
     }
 
@@ -33,10 +39,18 @@ public class RevenueStats {
      */
     public void tabCreated(Tab tab) {}
 
+    protected static boolean getRlzNotified(Context context) {
+        return getRlzNotified(); // TODO(agrieve): Delete once downstream is updated.
+    }
+
+    protected static void setRlzNotified(Context context, boolean notified) {
+        setRlzNotified(notified); // TODO(agrieve): Delete once downstream is updated.
+    }
+
     /**
      * Returns whether the RLZ provider has been notified that the first search has occurred.
      */
-    protected static boolean getRlzNotified(Context context) {
+    protected static boolean getRlzNotified() {
         return ContextUtils.getAppSharedPreferences().getBoolean(
                 PREF_RLZ_NOTIFIED, false);
     }
@@ -45,7 +59,7 @@ public class RevenueStats {
      * Stores whether the RLZ provider has been notified that the first search has occurred as
      * shared preference.
      */
-    protected static void setRlzNotified(Context context, boolean notified) {
+    protected static void setRlzNotified(boolean notified) {
         SharedPreferences.Editor sharedPreferencesEditor =
                 ContextUtils.getAppSharedPreferences().edit();
         sharedPreferencesEditor.putBoolean(PREF_RLZ_NOTIFIED, notified);
