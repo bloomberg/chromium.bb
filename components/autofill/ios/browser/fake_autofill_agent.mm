da55ca3638da498b60b213e68ddd40d27a6f86e9
John Z Wu <jzw@chromium.org>
2017-11-22 00:03:31 +0000

    Fake autofillagent for use in testing.
    //ios/web_view needs a fake autofill agent in order to unit test
    CWVAutofillController, which depends on an autofill_agent for
    fetching suggestions.
    
    Bug: 778764
    Change-Id: If6f552dc049739ea0ab322ffb50a32334ab5ce74
    Reviewed-on: https://chromium-review.googlesource.com/779628
    Commit-Queue: John Wu <jzw@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518464}
    Former-commit-id: 5bd9766ef73088a648794bd8077ad8d456ecdc5e

---
+++ components/autofill/ios/browser/fake_autofill_agent.mm
@@ -0,0 +1,101 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "components/autofill/ios/browser/fake_autofill_agent.h"
+
+#import "base/mac/bind_objc_block.h"
+#include "ios/web/public/web_thread.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@implementation FakeAutofillAgent {
+  NSMutableDictionary<NSString*, NSMutableArray<FormSuggestion*>*>*
+      _suggestionsByFormAndFieldName;
+  NSMutableDictionary<NSString*, FormSuggestion*>*
+      _selectedSuggestionByFormAndFieldName;
+}
+
+- (instancetype)initWithPrefService:(PrefService*)prefService
+                           webState:(web::WebState*)webState {
+  self = [super initWithPrefService:prefService webState:webState];
+  if (self) {
+    _suggestionsByFormAndFieldName = [NSMutableDictionary dictionary];
+    _selectedSuggestionByFormAndFieldName = [NSMutableDictionary dictionary];
+  }
+  return self;
+}
+
+#pragma mark - Public Methods
+
+- (void)addSuggestion:(FormSuggestion*)suggestion
+          forFormName:(NSString*)formName
+            fieldName:(NSString*)fieldName {
+  NSString* key = [self keyForFormName:formName fieldName:fieldName];
+  NSMutableArray* suggestions = _suggestionsByFormAndFieldName[key];
+  if (!suggestions) {
+    suggestions = [NSMutableArray array];
+    _suggestionsByFormAndFieldName[key] = suggestions;
+  }
+  [suggestions addObject:suggestion];
+}
+
+- (FormSuggestion*)selectedSuggestionForFormName:(NSString*)formName
+                                       fieldName:(NSString*)fieldName {
+  NSString* key = [self keyForFormName:formName fieldName:fieldName];
+  return _selectedSuggestionByFormAndFieldName[key];
+}
+
+#pragma mark - FormSuggestionProvider
+
+- (void)checkIfSuggestionsAvailableForForm:(NSString*)formName
+                                     field:(NSString*)fieldName
+                                 fieldType:(NSString*)fieldType
+                                      type:(NSString*)type
+                                typedValue:(NSString*)typedValue
+                                  webState:(web::WebState*)webState
+                         completionHandler:
+                             (SuggestionsAvailableCompletion)completion {
+  web::WebThread::PostTask(
+      web::WebThread::UI, FROM_HERE, base::BindBlockArc(^{
+        NSString* key = [self keyForFormName:formName fieldName:fieldName];
+        completion([_suggestionsByFormAndFieldName[key] count] ? YES : NO);
+      }));
+}
+
+- (void)retrieveSuggestionsForForm:(NSString*)formName
+                             field:(NSString*)fieldName
+                         fieldType:(NSString*)fieldType
+                              type:(NSString*)type
+                        typedValue:(NSString*)typedValue
+                          webState:(web::WebState*)webState
+                 completionHandler:(SuggestionsReadyCompletion)completion {
+  web::WebThread::PostTask(
+      web::WebThread::UI, FROM_HERE, base::BindBlockArc(^{
+        NSString* key = [self keyForFormName:formName fieldName:fieldName];
+        completion(_suggestionsByFormAndFieldName[key], self);
+      }));
+}
+
+- (void)didSelectSuggestion:(FormSuggestion*)suggestion
+                   forField:(NSString*)fieldName
+                       form:(NSString*)formName
+          completionHandler:(SuggestionHandledCompletion)completion {
+  web::WebThread::PostTask(
+      web::WebThread::UI, FROM_HERE, base::BindBlockArc(^{
+        NSString* key = [self keyForFormName:formName fieldName:fieldName];
+        _selectedSuggestionByFormAndFieldName[key] = suggestion;
+        completion();
+      }));
+}
+
+#pragma mark - Private Methods
+
+- (NSString*)keyForFormName:(NSString*)formName fieldName:(NSString*)fieldName {
+  // Uniqueness ensured because spaces are not allowed in html name attributes.
+  return [NSString stringWithFormat:@"%@ %@", formName, fieldName];
+}
+
+@end
