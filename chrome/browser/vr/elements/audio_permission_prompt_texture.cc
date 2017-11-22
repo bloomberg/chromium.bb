e3a44e8a8729dc47d24a10b05a864b30bae0129c
Amirhossein Simjour <asimjour@chromium.org>
2017-11-22 17:44:20 +0000

    VR: Fix text on buttons for AudioPermissionPrompt and ExitPrompt
    Load the font before drawing the text on the buttons.
    This fixes the problem when the font for that text was not loaded before.
    
    Bug: 786489
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I296e3c21049974a8502f7a5b96205ea1675f6280
    Reviewed-on: https://chromium-review.googlesource.com/785891
    Reviewed-by: Biao She <bshe@chromium.org>
    Commit-Queue: Amirhossein Simjour <asimjour@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518672}
    Former-commit-id: d35e19881920f5c6a63b339e0a4f348891fdfc46

--- chrome/browser/vr/elements/audio_permission_prompt_texture.cc
+++ chrome/browser/vr/elements/audio_permission_prompt_texture.cc
@@ -86,11 +86,11 @@ void AudioPermissionPromptTexture::Draw(SkCanvas* sk_canvas,
   SkPaint paint;
   gfx::Rect button_text_size(ToPixels(kButtonWidth), 0);
   float radius = ToPixels(kButtonRadius);
-  GetFontList(ToPixels(kFontSizePromptButtonText), text, &fonts);
 
   // Secondary button area.
   text = base::i18n::ToUpper(l10n_util::GetStringUTF16(
       IDS_VR_SHELL_AUDIO_PERMISSION_PROMPT_ABORT_BUTTON));
+  GetFontList(ToPixels(kFontSizePromptButtonText), text, &fonts);
   lines = PrepareDrawStringRect(
       text, fonts, secondary_button_colors_.foreground, &button_text_size,
       kTextAlignmentCenter, kWrappingBehaviorWrap);
@@ -115,6 +115,7 @@ void AudioPermissionPromptTexture::Draw(SkCanvas* sk_canvas,
   // Primary button area.
   text = base::i18n::ToUpper(l10n_util::GetStringUTF16(
       IDS_VR_SHELL_AUDIO_PERMISSION_PROMPT_CONTINUE_BUTTON));
+  GetFontList(ToPixels(kFontSizePromptButtonText), text, &fonts);
   button_text_size.set_size(gfx::Size(ToPixels(kButtonWidth), 0));
   lines = PrepareDrawStringRect(text, fonts, primary_button_colors_.foreground,
                                 &button_text_size, kTextAlignmentCenter,
