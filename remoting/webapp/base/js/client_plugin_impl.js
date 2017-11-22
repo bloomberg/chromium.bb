e0da853c938369639812360bf51ef2c2dd0f08f8
Jamie Walch <jamiewalch@chromium.org>
2017-11-22 22:55:55 +0000

    Enable stuck modifier detection on Linux.
    Note that this is not something we want enabled on all platforms due to
    bug 492414 but I plan on pushing this to dogfood to unblock Rodete users
    currently affected by missing modifier keyup events.
    
    Bug: 787523
    Change-Id: Ice97bd74eb293298d36fc52ef32fa37f2e272904
    Reviewed-on: https://chromium-review.googlesource.com/786391
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518782}
    Former-commit-id: ae62e366496fdf2b31987e4592fa9d7650a2875a

--- remoting/webapp/base/js/client_plugin_impl.js
+++ remoting/webapp/base/js/client_plugin_impl.js
@@ -456,6 +456,17 @@ remoting.ClientPluginImpl.prototype.connectWithExperiments_ = function(
 
   this.plugin_.postMessage(JSON.stringify(
       { method: 'delegateLargeCursors', data: {} }));
+
+  // Enable stuck modifier detection for Linux, but not for the public app.
+  // TODO(jamiewalch): Revert this once crbug.com/787523 is fixed.
+  this.plugin_.postMessage(JSON.stringify({
+    method: 'enableStuckModifierKeyDetection',
+    data: {
+      'enable': remoting.platformIsLinux() &&
+          chrome.runtime.id != 'gbchcmhmhahfdphkhkmpfmihenigjmpp'
+    }
+  }));
+
   this.credentials_ = credentialsProvider;
   this.useAsyncPinDialog_();
   this.plugin_.postMessage(JSON.stringify({
