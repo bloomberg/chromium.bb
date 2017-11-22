b00a52a2e5ecb038c2add5e064ba23e6149faaba
Jialiu Lin <jialiul@chromium.org>
2017-11-22 20:39:56 +0000

    Close Phishguard modal dialog when webcontents is gone
    There seems to be ways to close the target tab when modal dialog
    is showing (see crbug.com/784477 for details). As a result, we
    should handle the webcontents destroy event gracefully.
    
    Bug: 784477
    Change-Id: Ie17ac98ddb890a8b91f8cb02693e1c215364aa0b
    Reviewed-on: https://chromium-review.googlesource.com/783665
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Commit-Queue: Jialiu Lin <jialiul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518732}
    Former-commit-id: bf98cd8c071b5c04b7f509a277867f98456d968f

--- chrome/browser/ui/views/safe_browsing/password_reuse_modal_warning_dialog_browsertest.cc
+++ chrome/browser/ui/views/safe_browsing/password_reuse_modal_warning_dialog_browsertest.cc
@@ -39,6 +39,14 @@ class PasswordReuseModalWarningTest : public DialogBrowserTest {
         ->Show();
   }
 
+  void CloseActiveWebContents() {
+    TabStripModel* tab_strip_model = browser()->tab_strip_model();
+    tab_strip_model->CloseWebContentsAt(
+        tab_strip_model->GetIndexOfWebContents(
+            tab_strip_model->GetActiveWebContents()),
+        TabStripModel::CLOSE_NONE);
+  }
+
   void DialogCallback(PasswordProtectionService::WarningAction action) {
     latest_user_action_ = action;
   }
@@ -70,18 +78,22 @@ IN_PROC_BROWSER_TEST_F(PasswordReuseModalWarningTest, TestBasicDialogBehavior) {
   // CHANGE_PASSWORD action.
   ShowDialog(std::string());
   dialog_->GetDialogClientView()->AcceptWindow();
-  base::RunLoop().RunUntilIdle();
   EXPECT_EQ(PasswordProtectionService::CHANGE_PASSWORD, latest_user_action_);
 
   // Simulating a click on ui::DIALOG_BUTTON_CANCEL button results in an
   // IGNORE_WARNING action.
   ShowDialog(std::string());
   dialog_->GetDialogClientView()->CancelWindow();
-  base::RunLoop().RunUntilIdle();
   EXPECT_EQ(PasswordProtectionService::IGNORE_WARNING, latest_user_action_);
 }
 
-// TODO(jialiul): Add true end-to-end tests when this dialog is wired into
-// password protection service.
+IN_PROC_BROWSER_TEST_F(PasswordReuseModalWarningTest,
+                       CloseDialogWhenWebContentsDestroyed) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+
+  ShowDialog(std::string());
+  CloseActiveWebContents();
+  EXPECT_EQ(PasswordProtectionService::CLOSE, latest_user_action_);
+}
 
 }  // namespace safe_browsing
