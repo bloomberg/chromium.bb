31611dde8653b5fe9caf4c60e1a835544364a9eb
Will Chen <chenwilliam@chromium.org>
2017-11-22 01:29:09 +0000

    Reland "DevTools: migrate a handful of assorted DevTools tests"
    Deflaked the flaky test: workers-on-navigation.js
    
    This is a reland of 73b65a807984d27bf5f936c20fee6fc2a0419dce [formerly 614a066b19391e76a9637211012150c4bebe4e72]
    Original change's description:
    > DevTools: migrate a handful of assorted DevTools tests
    >
    > Migrates a couple websocket tests and assorted tests in http/tests/devtools/*.html
    >
    > Bug: 667560
    > Change-Id: I9d23517e8be6ed2282bd111b37bd0fa64f28b0c1
    > Reviewed-on: https://chromium-review.googlesource.com/777738
    > Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    > Commit-Queue: Will Chen <chenwilliam@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#518067}
    
    TBR=dgozman@chromium.org
    
    Bug: 667560
    Change-Id: If0148921e6133f2b5a7d351be9b59068738664fb
    Reviewed-on: https://chromium-review.googlesource.com/782743
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Reviewed-by: Will Chen <chenwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518486}
    Former-commit-id: 57df29264aaef643e8c48bead8701bacd94d1fbc

---
+++ third_party/WebKit/LayoutTests/http/tests/devtools/console-cross-origin-iframe-logging.js
@@ -0,0 +1,43 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+  TestRunner.addResult(`Tests that cross origin errors are logged with source url and line number.\n`);
+  await TestRunner.loadModule('console_test_runner');
+  await TestRunner.loadHTML(`
+      <iframe src="http://localhost:8000/devtools/resources/cross-origin-iframe.html"></iframe>
+    `);
+  await TestRunner.evaluateInPagePromise(`
+      function accessFrame()
+      {
+          // Should fail.
+          try {
+              var host = frames[0].location.host;
+          } catch (e) {}
+
+          // Should fail.
+          try {
+              frames[0].location.reload();
+          } catch (e) {}
+
+          // Should fail.
+          frames[0].postMessage("fail", "http://127.0.0.1:8000");
+      }
+  `);
+
+  var finishAttemptsLeft = 2;
+  ConsoleTestRunner.addConsoleSniffer(maybeFinish, true);
+  Common.settingForTest('monitoringXHREnabled').set(true);
+  TestRunner.evaluateInPage('accessFrame()', maybeFinish);
+
+  function maybeFinish() {
+    --finishAttemptsLeft;
+    if (finishAttemptsLeft)
+      return;
+
+    Common.settingForTest('monitoringXHREnabled').set(false);
+    ConsoleTestRunner.dumpConsoleMessages();
+    TestRunner.completeTest();
+  }
+})();
