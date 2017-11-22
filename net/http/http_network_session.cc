37f886c297918371d6e596b7bfe533ccb0d87d3c
yucliu <yucliu@chromium.org>
2017-11-22 02:41:23 +0000

    Close idle sockets only on memory pressure critical
    Closing idle sockets on memory pressure moderate is too aggressive. In
    some cases (e.g. media streaming on Cast), the socket connecting to
    the same server is closed and re-opened multiple times, if we close
    idle sockets when moderate memory pressure happens.
    
    Make it less aggressive by keeping the logic only for memory pressure
    critical.
    
    BUG=786585
    TEST=Stream on Cast
    
    Change-Id: I56fe7f53aa974125e9acada43f853ed6c28fd0fe
    Reviewed-on: https://chromium-review.googlesource.com/780779
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Yuchen Liu <yucliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518508}
    Former-commit-id: 9462d916a795eee44483d0fa695b1fca516e0aa1

--- net/http/http_network_session.cc
+++ net/http/http_network_session.cc
@@ -473,8 +473,8 @@ void HttpNetworkSession::OnMemoryPressure(
     base::MemoryPressureListener::MemoryPressureLevel memory_pressure_level) {
   switch (memory_pressure_level) {
     case base::MemoryPressureListener::MEMORY_PRESSURE_LEVEL_NONE:
-      break;
     case base::MemoryPressureListener::MEMORY_PRESSURE_LEVEL_MODERATE:
+      break;
     case base::MemoryPressureListener::MEMORY_PRESSURE_LEVEL_CRITICAL:
       CloseIdleConnections();
       break;
