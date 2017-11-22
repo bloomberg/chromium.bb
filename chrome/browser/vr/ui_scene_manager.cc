9e46f64f2d992460d9b82e51b491475e7fa8fadf
David Dorwin <ddorwin@chromium.org>
2017-11-22 23:10:13 +0000

    Make Exit VR prompt button text to be uppercase
    The uppercase styling should be applied in code and not depend on the
    actual strings. Leave the "EXIT VR" string unchanged for now.
    
    Bug: 787654
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: If265f4f6a6584f95bd0f6cdb39f26632875967a4
    Reviewed-on: https://chromium-review.googlesource.com/780692
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: David Dorwin <ddorwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#518788}
    Former-commit-id: f2848d15b9bd2d1c97333568ccb3730c59cf43c3

--- chrome/browser/vr/ui_scene_manager.cc
+++ chrome/browser/vr/ui_scene_manager.cc
@@ -548,6 +548,7 @@ void UiSceneManager::CreateSplashScreen() {
 
   timeout_text = base::MakeUnique<Text>(512, kTimeoutMessageTextFontHeightM,
                                         kTimeoutButtonTextWidth);
+  // Disk-style button text is not uppercase. See crbug.com/787654.
   timeout_text->SetText(
       l10n_util::GetStringUTF16(IDS_VR_WEB_VR_EXIT_BUTTON_LABEL));
   timeout_text->set_name(kWebVrTimeoutMessageButtonText);
