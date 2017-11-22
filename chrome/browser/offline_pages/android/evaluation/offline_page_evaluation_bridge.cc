bloomberg/chromium.bb

commit 4a8c00f80280b63efc9b8ab79b3bf72803a328df

Add the classname to JNI method names to make them unique
Our JNI code generator generated a lot of Init(...) methods which
collided in jumbo builds. This patch adds a class name prefix to
them, and all other JNI methods, so that they have locally unique names.

TBR=erikchen@chromium.org,fgorski@chromium.org,sergeyu@chromium.org,dtrainor@chromium.org,rouslan@chromium.org,vakh@chromium.org,rogerta@chromium.org,holte@chromium.org,bajones@chromium.org

Bug: 779013
Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.linux:linux_site_isolation;master.tryserver.chromium.mac:ios-simulator-cronet
Change-Id: I86b6ac83c8e0a3507bd1d0efc48d625c12b4bb2b
Reviewed-on: https://chromium-review.googlesource.com/765973
Commit-Queue: Daniel Bratell <bratell@opera.com>
Reviewed-by: Daniel Bratell <bratell@opera.com>
Reviewed-by: Peter Beverloo <peter@chromium.org>
Reviewed-by: Avi Drissman <avi@chromium.org>
Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
Reviewed-by: Sky Malice <skym@chromium.org>
Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
Reviewed-by: Luke Halliwell <halliwell@chromium.org>
Reviewed-by: Ken Rockot <rockot@chromium.org>
Reviewed-by: Richard Coles <torne@chromium.org>
Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
Reviewed-by: Miguel Casas <mcasas@chromium.org>
Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
Reviewed-by: Paul Jensen <pauljensen@chromium.org>
Reviewed-by: Peter Kasting <pkasting@chromium.org>
Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
Reviewed-by: agrieve <agrieve@chromium.org>
Cr-Commit-Position: refs/heads/master@{#518300}
Former-commit-id: 7aacf9523d3616f046ee93e902ea5f95c874f609

chrome/browser/offline_pages/android/evaluation/offline_page_evaluation_bridge.cc
@@ -54,9 +54,10 @@ const char kNativeTag[] = "OPNative";
 const base::FilePath::CharType kTestRequestQueueDirname[] =
     FILE_PATH_LITERAL("Offline Pages/test_request_queue");
 
-void ToJavaOfflinePageList(JNIEnv* env,
-                           const JavaRef<jobject>& j_result_obj,
-                           const std::vector<OfflinePageItem>& offline_pages) {
+void JNI_OfflinePageEvaluationBridge_ToJavaOfflinePageList(
+    JNIEnv* env,
+    const JavaRef<jobject>& j_result_obj,
+    const std::vector<OfflinePageItem>& offline_pages) {
   for (const OfflinePageItem& offline_page : offline_pages) {
     Java_OfflinePageEvaluationBridge_createOfflinePageAndAddToList(
         env, j_result_obj,
@@ -72,7 +73,8 @@ void ToJavaOfflinePageList(JNIEnv* env,
   }
 }
 
-ScopedJavaLocalRef<jobject> ToJavaSavePageRequest(
+ScopedJavaLocalRef<jobject>
+JNI_OfflinePageEvaluationBridge_ToJavaSavePageRequest(
     JNIEnv* env,
     const SavePageRequest& request) {
   return Java_OfflinePageEvaluationBridge_createSavePageRequest(
@@ -82,7 +84,8 @@ ScopedJavaLocalRef<jobject> ToJavaSavePageRequest(
       ConvertUTF8ToJavaString(env, request.client_id().id));
 }
 
-ScopedJavaLocalRef<jobjectArray> CreateJavaSavePageRequests(
+ScopedJavaLocalRef<jobjectArray>
+JNI_OfflinePageEvaluationBridge_CreateJavaSavePageRequests(
     JNIEnv* env,
     std::vector<std::unique_ptr<SavePageRequest>> requests) {
   ScopedJavaLocalRef<jclass> save_page_request_clazz = base::android::GetClass(
@@ -190,10 +193,11 @@ RequestCoordinator* GetRequestCoordinator(Profile* profile,
 
 }  // namespace
 
-static jlong CreateBridgeForProfile(JNIEnv* env,
-                                    const JavaParamRef<jobject>& obj,
-                                    const JavaParamRef<jobject>& j_profile,
-                                    const jboolean j_use_evaluation_scheduler) {
+static jlong JNI_OfflinePageEvaluationBridge_CreateBridgeForProfile(
+    JNIEnv* env,
+    const JavaParamRef<jobject>& obj,
+    const JavaParamRef<jobject>& j_profile,
+    const jboolean j_use_evaluation_scheduler) {
   Profile* profile = ProfileAndroid::FromProfileAndroid(j_profile);
 
   OfflinePageModel* offline_page_model =

commit 2763f0f8a193c2db11c114b399745b80518f9d4a

[Offline Pages] Fixing evaluation bridge.
Evaluation bridge build was broken by the commit:
4a8c00f80280b63efc9b8ab79b3bf72803a328df [formerly 7aacf9523d3616f046ee93e902ea5f95c874f609] from CL:
https://chromium-review.googlesource.com/765973

This CL fixed the build error by using the renamed methods introduced in
the CL mentioned above.

Bug: NONE
Change-Id: I22c932327b7ac5072da2151257024a2e9e2a87fd
Reviewed-on: https://chromium-review.googlesource.com/786278
Reviewed-by: Peter Williamson <petewil@chromium.org>
Commit-Queue: Yafei Duan <romax@chromium.org>
Cr-Commit-Position: refs/heads/master@{#518778}
Former-commit-id: 69b2617507f821755d95c734fd4d249128579355

chrome/browser/offline_pages/android/evaluation/offline_page_evaluation_bridge.cc
@@ -97,7 +97,7 @@ JNI_OfflinePageEvaluationBridge_CreateJavaSavePageRequests(
   for (size_t i = 0; i < requests.size(); i++) {
     SavePageRequest request = *(requests[i]);
     ScopedJavaLocalRef<jobject> j_save_page_request =
-        ToJavaSavePageRequest(env, request);
+        JNI_OfflinePageEvaluationBridge_ToJavaSavePageRequest(env, request);
     env->SetObjectArrayElement(joa, i, j_save_page_request.obj());
   }
 
@@ -109,7 +109,8 @@ void GetAllPagesCallback(
     const ScopedJavaGlobalRef<jobject>& j_callback_obj,
     const OfflinePageModel::MultipleOfflinePageItemResult& result) {
   JNIEnv* env = base::android::AttachCurrentThread();
-  ToJavaOfflinePageList(env, j_result_obj, result);
+  JNI_OfflinePageEvaluationBridge_ToJavaOfflinePageList(env, j_result_obj,
+                                                        result);
   base::android::RunCallbackAndroid(j_callback_obj, j_result_obj);
 }
 
@@ -124,7 +125,8 @@ void OnGetAllRequestsDone(
   JNIEnv* env = base::android::AttachCurrentThread();
 
   ScopedJavaLocalRef<jobjectArray> j_result_obj =
-      CreateJavaSavePageRequests(env, std::move(all_requests));
+      JNI_OfflinePageEvaluationBridge_CreateJavaSavePageRequests(
+          env, std::move(all_requests));
   base::android::RunCallbackAndroid(j_callback_obj, j_result_obj);
 }
 
@@ -264,7 +266,8 @@ void OfflinePageEvaluationBridge::OnAdded(const SavePageRequest& request) {
   if (obj.is_null())
     return;
   Java_OfflinePageEvaluationBridge_savePageRequestAdded(
-      env, obj, ToJavaSavePageRequest(env, request));
+      env, obj,
+      JNI_OfflinePageEvaluationBridge_ToJavaSavePageRequest(env, request));
 }
 
 void OfflinePageEvaluationBridge::OnCompleted(
@@ -275,7 +278,9 @@ void OfflinePageEvaluationBridge::OnCompleted(
   if (obj.is_null())
     return;
   Java_OfflinePageEvaluationBridge_savePageRequestCompleted(
-      env, obj, ToJavaSavePageRequest(env, request), static_cast<int>(status));
+      env, obj,
+      JNI_OfflinePageEvaluationBridge_ToJavaSavePageRequest(env, request),
+      static_cast<int>(status));
 }
 
 void OfflinePageEvaluationBridge::OnChanged(const SavePageRequest& request) {
@@ -284,7 +289,8 @@ void OfflinePageEvaluationBridge::OnChanged(const SavePageRequest& request) {
   if (obj.is_null())
     return;
   Java_OfflinePageEvaluationBridge_savePageRequestChanged(
-      env, obj, ToJavaSavePageRequest(env, request));
+      env, obj,
+      JNI_OfflinePageEvaluationBridge_ToJavaSavePageRequest(env, request));
 }
 
 void OfflinePageEvaluationBridge::OnNetworkProgress(