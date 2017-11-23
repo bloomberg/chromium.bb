065671221956c270c8f4784da309b099bf24cd27
Fredrik Söderquist <fs@opera.com>
2017-11-23 10:08:33 +0000

    Use the same bounding box computation even with fallback path
    LayoutSVGShape::UpdateShapeFromElement() creates a Path for the shape as
    well as update the bounding box (fill/object and stroke.)
    When we fallback to using a Path rather than the fast path
    representation for the shape, we can still use the same code for
    computing the bounding boxes. So instead of calling LayoutSVGShape's
    UpdateShapeFromElement from LayoutSVGRect and LayoutSVGEllipse, just
    call CreatePath() in the codepaths that transitions to the fallback/Path
    codepath. Non-scaling stroke still require a "precise" stroke bbox.
    
    This should make stroke bounding boxes for <rect>, <circle> and
    <ellipse> tighter again.
    
    Bug: 435097
    Change-Id: I5361689371cb78eb405e0403f3be165bd254aab2
    Reviewed-on: https://chromium-review.googlesource.com/779460
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#518880}
    Former-commit-id: 8534729a4acea2d5064a3e5d39baabdc84ff3ba0

--- third_party/WebKit/Source/core/layout/svg/LayoutSVGEllipse.cpp
+++ third_party/WebKit/Source/core/layout/svg/LayoutSVGEllipse.cpp
@@ -56,14 +56,22 @@ void LayoutSVGEllipse::UpdateShapeFromElement() {
   if (!radii_.IsEmpty()) {
     // Fall back to LayoutSVGShape and path-based hit detection if the ellipse
     // has a non-scaling or discontinuous stroke.
-    if (HasNonScalingStroke() || !HasContinuousStroke()) {
+    // However, only use LayoutSVGShape bounding-box calculations for the
+    // non-scaling stroke case, since the computation below should be accurate
+    // for the other cases.
+    if (HasNonScalingStroke()) {
       LayoutSVGShape::UpdateShapeFromElement();
       use_path_fallback_ = true;
       return;
     }
+    if (!HasContinuousStroke()) {
+      CreatePath();
+      use_path_fallback_ = true;
+    }
   }
 
-  ClearPath();
+  if (!use_path_fallback_)
+    ClearPath();
 
   fill_bounding_box_ =
       FloatRect(center_.X() - radii_.Width(), center_.Y() - radii_.Height(),
