8d436e55b44520596188f6850bd0a8d920598210
Paul Meyer <paulmeyer@chromium.org>
2017-11-24 20:47:15 +0000

    Refactor DeprecationReport / InterventionReport
    This patch refactors the common code in DeprecationReport and
    InterventionReport out to a new type called MessageReport.
    
    Change-Id: Idd87c045256c373440e835b85f1981f159398e25
    Reviewed-on: https://chromium-review.googlesource.com/786611
    Commit-Queue: Paul Meyer <paulmeyer@chromium.org>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519160}
    Former-commit-id: 42069224b96619ad82c8eaf7bd0b7322ce524482

--- third_party/WebKit/Source/core/frame/InterventionReport.h
+++ third_party/WebKit/Source/core/frame/InterventionReport.h
@@ -6,32 +6,19 @@
 #define InterventionReport_h
 
 #include "bindings/core/v8/SourceLocation.h"
-#include "core/frame/ReportBody.h"
+#include "core/frame/MessageReport.h"
 
 namespace blink {
 
-// TODO(paulmeyer): Consider refactor with DeprecationReport.h.
-class CORE_EXPORT InterventionReport : public ReportBody {
+class CORE_EXPORT InterventionReport : public MessageReport {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
   InterventionReport(const String& message,
                      std::unique_ptr<SourceLocation> location)
-      : message_(message), location_(std::move(location)) {}
+      : MessageReport(message, std::move(location)) {}
 
   ~InterventionReport() override {}
-
-  String message() const { return message_; }
-  long lineNumber() const { return location_->LineNumber(); }
-  String sourceFile() const {
-    return location_->Url().IsNull() ? "" : location_->Url();
-  }
-
-  virtual void Trace(blink::Visitor* visitor) { ReportBody::Trace(visitor); }
-
- private:
-  const String message_;
-  std::unique_ptr<SourceLocation> location_;
 };
 
 }  // namespace blink
