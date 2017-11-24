86c1e179f4c4f8270c425e59b8aa08d681f84332
Marti Wong <martiw@chromium.org>
2017-11-24 05:19:57 +0000

    Add experiment flag for new iOS bookmarks edit page and folder picker.
    Bug: 787668
    Change-Id: Ia6324cdf292f2dcd2cc4edeb81a137fb3dcf26e4
    Reviewed-on: https://chromium-review.googlesource.com/784370
    Reviewed-by: Eric Noyau <noyau@chromium.org>
    Commit-Queue: Marti Wong <martiw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519060}
    Former-commit-id: 1b885ad68e246f0fbca1e12d4ff16dc298121601

--- ios/chrome/browser/bookmarks/bookmark_new_generation_features.h
+++ ios/chrome/browser/bookmarks/bookmark_new_generation_features.h
@@ -8,7 +8,10 @@
 #include "base/feature_list.h"
 #include "build/build_config.h"
 
-// Feature flag for the new bookmark UI
+// Feature flag for the new bookmark UI.
 extern const base::Feature kBookmarkNewGeneration;
+
+// Feature flag for the new edit page and folder picker of the new bookmark UI.
+extern const base::Feature kBookmarkNewEditPage;
 
 #endif  // IOS_CHROME_BROWSER_BOOKMARKS_BOOKMARK_NEW_GENERATION_FEATURES_H
