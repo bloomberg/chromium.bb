cbfc5333d30b6d73966c8c356aa9626515eada84
Henrik Grunell <grunell@chromium.org>
2017-11-27 16:41:08 +0000

    Disable WebRtcSimulcastBrowserTest.TestVgaReturnsTwoSimulcastStreams on Mac.
    This test is problematic on all other platforms already. It started failing
    on Mac bots, but is not reproducible locally.
    
    NOTRY=True
    TBR=phoglund@chromium.org
    
    Bug: 452892
    Change-Id: I4f55753744b35c3a739b5418cc8e7d35060bd907
    Reviewed-on: https://chromium-review.googlesource.com/790517
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Henrik Grunell <grunell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519315}
    Former-commit-id: 771ff796194e75139d25833928c55fd74ea30b22

--- chrome/browser/media/webrtc/webrtc_simulcast_browsertest.cc
+++ chrome/browser/media/webrtc/webrtc_simulcast_browsertest.cc
@@ -43,10 +43,11 @@ class WebRtcSimulcastBrowserTest : public WebRtcTestBase {
   }
 };
 
-// Fails/times out on Windows and Chrome OS. Flaky on Linux.
+// Fails/times out on Windows, Mac and Chrome OS. Flaky on Linux.
 // http://crbug.com/452623
 // MSan reports errors. http://crbug.com/452892
-#if defined(OS_WIN) || defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(MEMORY_SANITIZER)
+#if defined(OS_WIN) || defined(OS_LINUX) || defined(OS_CHROMEOS) || \
+    defined(MEMORY_SANITIZER) || defined(OS_MACOSX)
 #define MAYBE_TestVgaReturnsTwoSimulcastStreams DISABLED_TestVgaReturnsTwoSimulcastStreams
 #else
 #define MAYBE_TestVgaReturnsTwoSimulcastStreams TestVgaReturnsTwoSimulcastStreams
