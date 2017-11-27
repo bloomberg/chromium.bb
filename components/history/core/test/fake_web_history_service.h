7c5c5d069ff231e4018e6b72ee5daaca697f0386
Sky Malice <skym@chromium.org>
2017-11-27 22:08:45 +0000

    Stop assuming web history results are ordered.
    The previous code would simply take the last result from both local and
    remote sources and use that as a potential cutoff time for holding
    results back. However, web history in particular, is not guaranteed or
    even commonly ordered (reverse chronologically). This also occasionally
    would hit a DCHECK, and could have caused duplicated remote results and
    extra queries to web history.
    
    Additionally, changed youngest_* to oldest_*, referring to time stamps
    that happened the longest ago in the past, which now have the smallest
    numerical value as internal representation.
    
    Bug: 787928
    Change-Id: I8b8b287599c6e4ad06ced1546974d8a4d167bb84
    Reviewed-on: https://chromium-review.googlesource.com/786642
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519422}
    Former-commit-id: bc081b5d27cff309f24c36b82745cb56e17bb7e0

--- components/history/core/test/fake_web_history_service.h
+++ components/history/core/test/fake_web_history_service.h
@@ -57,20 +57,23 @@ class FakeWebHistoryService : public WebHistoryService {
   bool AreOtherFormsOfBrowsingHistoryPresent();
   void SetOtherFormsOfBrowsingHistoryPresent(bool present);
 
- private:
-  class FakeRequest;
+ protected:
   typedef std::pair<std::string, base::Time> Visit;
 
   // Returns up to |count| results from |visits_| between |begin| and |end.
   // Results are sorted from most recent to least recent, prioritizing more
   // recent results when some need to be omitted. |more_results_left| will be
   // set to true only if there are results from |visits_| that were not included
-  // because of |count| limitations, but were also within time range.
-  std::vector<FakeWebHistoryService::Visit> GetVisitsBetween(
+  // because of |count| limitations, but were also within time range. Virtual to
+  // allow subclasses to modify.
+  virtual std::vector<FakeWebHistoryService::Visit> GetVisitsBetween(
       base::Time begin,
       base::Time end,
       size_t count,
       bool* more_results_left);
+
+ private:
+  class FakeRequest;
 
   base::Time GetTimeForKeyInQuery(const GURL& url, const std::string& key);
 
