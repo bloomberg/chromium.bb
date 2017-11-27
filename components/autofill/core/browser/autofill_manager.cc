5f0280a1f332962d82b7303665be7cbdbb5b19a2
Shanfeng Zhang <szhangcs@google.com>
2017-11-27 22:51:20 +0000

    Add UMA events to log how many fields were autofilled overall.
    Bug: 
    Change-Id: Id706ff7666dde88a68381897af5cdc8370e8efb6
    Reviewed-on: https://chromium-review.googlesource.com/786637
    Commit-Queue: Shanfeng Zhang <szhangcs@google.com>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519435}
    Former-commit-id: 24b0659db263b40a7e1f85b2810e9e2b447e6fab

--- components/autofill/core/browser/autofill_manager.cc
+++ components/autofill/core/browser/autofill_manager.cc
@@ -1924,6 +1924,8 @@ void AutofillManager::FillFieldWithValue(AutofillField* autofill_field,
     // it. This allows the renderer to distinguish autofilled fields from
     // fields with non-empty values, such as select-one fields.
     field_data->is_autofilled = true;
+    AutofillMetrics::LogUserHappinessMetric(
+        AutofillMetrics::FIELD_WAS_AUTOFILLED, autofill_field->Type().group());
 
     if (should_notify) {
       client_->DidFillOrPreviewField(
