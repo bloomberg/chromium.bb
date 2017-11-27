d8f0e4d16be29d90059e7d9cb5a51f5814207c50
David Bokan <bokan@chromium.org>
2017-11-27 23:50:25 +0000

    Compensate for page scale in animated scrolls
    When the user zooms into the page, scrolling should be proportionally
    reduced so the page appears to move at the same rate. This is currently
    done for various kinds of scrolling.
    
    An animated scroll that updates an existing scroll animation simply
    adjusts the animation curve's target delta. However, this path forgot
    to adjust by the page scale factor like the "new animation" and "non-
    animated" paths. This CL simply adds that in.
    
    Bug: 610021
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Ic1a6421131915ad739d39ad3ef36792dd3be53aa
    Reviewed-on: https://chromium-review.googlesource.com/788134
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519456}
    Former-commit-id: 2a96df7c3a68dd9c0ac1cf1263a609bf4824f7f3

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -4419,8 +4419,11 @@ bool LayerTreeHostImpl::ScrollAnimationUpdateTarget(
     ScrollNode* scroll_node,
     const gfx::Vector2dF& scroll_delta,
     base::TimeDelta delayed_by) {
+  float scale_factor = active_tree()->current_page_scale_factor();
+  gfx::Vector2dF scaled_delta =
+      gfx::ScaleVector2d(scroll_delta, 1.f / scale_factor);
   return mutator_host_->ImplOnlyScrollAnimationUpdateTarget(
-      scroll_node->element_id, scroll_delta,
+      scroll_node->element_id, scaled_delta,
       active_tree_->property_trees()->scroll_tree.MaxScrollOffset(
           scroll_node->id),
       CurrentBeginFrameArgs().frame_time, delayed_by);
