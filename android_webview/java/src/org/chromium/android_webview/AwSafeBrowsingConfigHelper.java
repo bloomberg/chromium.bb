ca76ffe7bf297d90634154de7c60f4eb31fbccbb
James Wallace-Lee <jamwalla@chromium.org>
2017-11-27 22:40:39 +0000

    WebView: move statics into PlatformServiceBridge
    (Part 2 of a 3-sided patch.)
    Depends on crrev.com/i/502732
    
    Downstream, we are moving static SafeBrowsing methods into
    PlatformServiceBridge. This CL replaces upstream calls to SafeBrowsing
    methods, which previously used reflections, with calls to new instance
    methods in PlatformServiceBridge.
    
    BUG=682070
    
    Change-Id: Id457a6065aa1d16b2acb8404a2fea4070b0879be
    Reviewed-on: https://chromium-review.googlesource.com/765092
    Reviewed-by: Paul Miller <paulmiller@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: James Wallace-Lee <jamwalla@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519432}
    Former-commit-id: 879fb124ce96a93d0ea96947b14eeebbe3cec6c6

--- android_webview/java/src/org/chromium/android_webview/AwSafeBrowsingConfigHelper.java
+++ android_webview/java/src/org/chromium/android_webview/AwSafeBrowsingConfigHelper.java
@@ -14,9 +14,6 @@ import org.chromium.base.CommandLine;
 import org.chromium.base.Log;
 import org.chromium.base.annotations.JNINamespace;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 /**
  * Helper class for getting the configuration settings related to safebrowsing in WebView.
  */
@@ -37,28 +34,8 @@ public class AwSafeBrowsingConfigHelper {
         AwContentsStatics.setSafeBrowsingEnabledByManifest(
                 appOptIn == null ? getCommandLineOptIn() : appOptIn);
 
-        // If GMS is available, we will figure out if the user has opted-in to Safe Browsing and set
-        // the correct value for sSafeBrowsingUserOptIn.
-        final String getUserOptInPreferenceMethodName = "getUserOptInPreference";
-        try {
-            Class awSafeBrowsingApiHelperClass =
-                    Class.forName("com.android.webview.chromium.AwSafeBrowsingApiHandler");
-            Method getUserOptInPreference = awSafeBrowsingApiHelperClass.getDeclaredMethod(
-                    getUserOptInPreferenceMethodName, Context.class, Callback.class);
-            // TODO(ntfschr): remove clang-format directives once crbug/764581 is resolved
-            // clang-format off
-            getUserOptInPreference.invoke(null, appContext,
-                    (Callback<Boolean>) optin -> setSafeBrowsingUserOptIn(
-                            optin == null ? false : optin));
-            // clang-format on
-        } catch (ClassNotFoundException e) {
-            // This is not an error; it just means this device doesn't have specialized services.
-        } catch (IllegalAccessException | IllegalArgumentException | NoSuchMethodException e) {
-            Log.e(TAG, "Failed to invoke " + getUserOptInPreferenceMethodName + ": " + e);
-        } catch (InvocationTargetException e) {
-            Log.e(TAG, "Failed invocation for " + getUserOptInPreferenceMethodName + ": ",
-                    e.getCause());
-        }
+        Callback<Boolean> cb = optin -> setSafeBrowsingUserOptIn(optin == null ? false : optin);
+        PlatformServiceBridge.getInstance().querySafeBrowsingUserConsent(appContext, cb);
     }
 
     private static boolean getCommandLineOptIn() {
