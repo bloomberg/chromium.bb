aa3ea6ad96af28bcb5f418d0c7fbd4e15f243a57
khmel <khmel@google.com>
2017-11-27 23:00:19 +0000

    arc: Fix issues with ARC migration notifications.
    Clicking need migration notification should cause session exit. We had
    only handle for old style notifications. This suppors new style as well.
    Clicking on migration completed notification should start OptIn flow.
    
    Test: Manually for both cases.
    Bug: b/69622523
    Change-Id: If2a56ec81b5e9cba797e081a4c97babd8d00951c
    Reviewed-on: https://chromium-review.googlesource.com/783832
    Commit-Queue: Yury Khmel <khmel@google.com>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519439}
    Former-commit-id: a02133bb92730213a93d13cfd48c0a5ef266be0f

--- chrome/browser/chromeos/arc/arc_migration_guide_notification.cc
+++ chrome/browser/chromeos/arc/arc_migration_guide_notification.cc
@@ -47,11 +47,36 @@ class ArcMigrationGuideNotificationDelegate
 
   // message_center::NotificationDelegate
   void ButtonClick(int button_index) override { chrome::AttemptUserExit(); }
+  void Click() override { chrome::AttemptUserExit(); }
 
  private:
   ~ArcMigrationGuideNotificationDelegate() override = default;
 
   DISALLOW_COPY_AND_ASSIGN(ArcMigrationGuideNotificationDelegate);
+};
+
+class ArcMigrationCompletedNotificationDelegate
+    : public message_center::NotificationDelegate {
+ public:
+  explicit ArcMigrationCompletedNotificationDelegate(Profile* profile)
+      : profile_(profile) {}
+
+  // message_center::NotificationDelegate
+  void ButtonClick(int button_index) override {
+    arc::SetArcPlayStoreEnabledForProfile(profile_, true);
+  }
+
+  void Click() override {
+    arc::SetArcPlayStoreEnabledForProfile(profile_, true);
+  }
+
+ private:
+  ~ArcMigrationCompletedNotificationDelegate() override = default;
+
+  // Unowned pointer.
+  Profile* const profile_;
+
+  DISALLOW_COPY_AND_ASSIGN(ArcMigrationCompletedNotificationDelegate);
 };
 
 void DoShowArcMigrationSuccessNotification(Profile* profile) {
@@ -67,7 +92,9 @@ void DoShowArcMigrationSuccessNotification(Profile* profile) {
         l10n_util::GetStringUTF16(IDS_ARC_MIGRATE_ENCRYPTION_SUCCESS_TITLE),
         l10n_util::GetStringUTF16(IDS_ARC_MIGRATE_ENCRYPTION_SUCCESS_MESSAGE),
         gfx::Image(), base::string16(), GURL(), notifier_id,
-        message_center::RichNotificationData(), nullptr,
+        message_center::RichNotificationData(),
+        scoped_refptr<message_center::NotificationDelegate>(
+            new ArcMigrationCompletedNotificationDelegate(profile)),
         ash::kNotificationSettingsIcon,
         message_center::SystemNotificationWarningLevel::NORMAL);
   } else {
@@ -78,7 +105,9 @@ void DoShowArcMigrationSuccessNotification(Profile* profile) {
         gfx::Image(gfx::CreateVectorIcon(kArcMigrateEncryptionNotificationIcon,
                                          gfx::kPlaceholderColor)),
         base::string16(), GURL(), notifier_id,
-        message_center::RichNotificationData(), nullptr);
+        message_center::RichNotificationData(),
+        scoped_refptr<message_center::NotificationDelegate>(
+            new ArcMigrationCompletedNotificationDelegate(profile)));
   }
 
   NotificationDisplayService::GetForProfile(profile)->Display(
@@ -119,7 +148,8 @@ void ShowArcMigrationGuideNotification(Profile* profile) {
             IDS_ARC_MIGRATE_ENCRYPTION_NOTIFICATION_TITLE),
         message, gfx::Image(), base::string16(), GURL(), notifier_id,
         message_center::RichNotificationData(),
-        new ArcMigrationGuideNotificationDelegate(),
+        scoped_refptr<message_center::NotificationDelegate>(
+            new ArcMigrationGuideNotificationDelegate()),
         ash::kNotificationSettingsIcon,
         message_center::SystemNotificationWarningLevel::NORMAL);
   } else {
@@ -134,7 +164,8 @@ void ShowArcMigrationGuideNotification(Profile* profile) {
         gfx::Image(gfx::CreateVectorIcon(kArcMigrateEncryptionNotificationIcon,
                                          gfx::kPlaceholderColor)),
         base::string16(), GURL(), notifier_id, data,
-        new ArcMigrationGuideNotificationDelegate());
+        scoped_refptr<message_center::NotificationDelegate>(
+            new ArcMigrationGuideNotificationDelegate()));
   }
 
   NotificationDisplayService::GetForProfile(profile)->Display(
