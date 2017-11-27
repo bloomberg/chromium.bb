9920172d8a582a7c9805b94bcedc94f9f67cfe7b
Ehsan Chiniforooshan <chiniforooshan@chromium.org>
2017-11-27 21:27:17 +0000

    tracing: fix a bug when no endpoint is given
    When no trace endpoint is given to StopTracing, we don't reset
    filtered_metadata_ which causes a DCHECK failure in the next StopTracing call.
    
    Bug: 788698
    Change-Id: Ifb76f0d68ffc92a6d34498b9e574e31f0644b2dc
    Reviewed-on: https://chromium-review.googlesource.com/791210
    Reviewed-by: oysteine <oysteine@chromium.org>
    Commit-Queue: Ehsan Chiniforooshan <chiniforooshan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519411}
    Former-commit-id: 87a4dbb155d94f4854a1adeb2db07770f670cbb7

--- content/browser/tracing/tracing_controller_impl.cc
+++ content/browser/tracing/tracing_controller_impl.cc
@@ -381,6 +381,7 @@ void TracingControllerImpl::CompleteFlush() {
     trace_data_endpoint_->ReceiveTraceFinalContents(
         std::move(filtered_metadata_));
   }
+  filtered_metadata_.reset(nullptr);
   trace_data_endpoint_ = nullptr;
   trace_config_ = nullptr;
 }
