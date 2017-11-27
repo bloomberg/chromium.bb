c1d0950eca28374b3e7d6c7819d44ad1db727c1d
Benoit Lize <lizeb@chromium.org>
2017-11-27 10:43:54 +0000

    android: Record ordering in lightweight_cygprofile.cc.
    lightweight_cygprofile.cc doesn't take ordering into account, as it uses
    a simple bitfield to record which locations have been reached in the
    executable code. This is detrimental to startup performance, so this CL
    adds ordering to it.
    
    Changes:
    - Adds an ordered list of reached locations "behind" the bitfield. When
      a new element is inserted in the bitfield, add it to the list of reached locations.
    - Updates the processing to keep ordering, and deal with the new output
      format.
    
    This increases memory usage by ~4MB per process, as the reached location
    list is preallocated, but only for profiling builds. This would
    drastically shrink once post-inlining instrumentation is available in
    clang, as Chrome has <<1M functions reached per process.
    
    Bug: 776702
    Change-Id: Ib6f0fd2fd83d91167073453bac47f79050249248
    Reviewed-on: https://chromium-review.googlesource.com/782220
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519269}
    Former-commit-id: 328af70306c1e9a992bda8b4cd3af88b03c1b9df

--- tools/cygprofile/lightweight_cygprofile.cc
+++ tools/cygprofile/lightweight_cygprofile.cc
@@ -10,102 +10,158 @@
 #include <thread>
 #include <vector>
 
+#include "base/android/library_loader/anchor_functions.h"
 #include "base/files/file.h"
+#include "base/format_macros.h"
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/strings/stringprintf.h"
+#include "build/build_config.h"
 
-// These functions are here to, respectively:
-// 1. Check that functions are ordered
-// 2. Delimit the start of .text
-// 3. Delimit the end of .text
-//
-// (2) and (3) require a suitably constructed orderfile, with these
-// functions at the beginning and end. (1) doesn't need to be in it.
-//
-// Also note that we have "load-bearing" CHECK()s below: they have side-effects.
-// Without any code taking the address of a function, Identical Code Folding
-// would merge these functions with other empty ones, defeating their purpose.
-extern "C" {
-void dummy_function_to_check_ordering() {}
-void dummy_function_to_anchor_text() {}
-void dummy_function_at_the_end_of_text() {}
-}
+#if !defined(ARCH_CPU_ARMEL)
+#error Only supported on ARM.
+#endif  // !defined(ARCH_CPU_ARMEL)
 
 namespace {
 
-constexpr size_t kMaxTextSizeInBytes = 280000000;  // Must be an overestimate.
-constexpr size_t kArraySize = kMaxTextSizeInBytes / (4 * 32);
+// These are large overestimates, which is not an issue, as the data is
+// allocated in .bss, and on linux doesn't take any actual memory when it's not
+// touched.
+constexpr size_t kBitfieldSize = 1 << 22;
+constexpr size_t kMaxTextSizeInBytes = kBitfieldSize * (4 * 32);
+constexpr size_t kMaxElements = 1 << 20;
 
-// Allocated in .bss. std::atomic<uint32_t> is guaranteed to behave as uint32_t
-// with respect to size and initialization.
-// Having the array in .bss means that instrumentation starts from the very
-// first code executed within the binary.
-std::atomic<uint32_t> g_return_offsets[kArraySize];
+// |RecordAddress()| adds an element to a concurrent bitset and to a concurrent
+// append-only list of offsets.
+//
+// Ordering:
+// Two consecutive calls to |RecordAddress()| from the same thread will be
+// ordered in the same way in the result, as written by
+// |StopAndDumpToFile()|. The result will contain exactly one instance of each
+// unique offset relative to |kStartOfText| passed to |RecordAddress()|.
+//
+// Implementation:
+// The "set" part is implemented with a bitfield, |g_offset|. The insertion
+// order is recorded in |g_ordered_offsets|.
+// This is not a class to make sure there isn't a static constructor, as it
+// would cause issue with an instrumented static constructor calling this code.
+//
+// Limitations:
+// - Only records offsets to addresses between |kStartOfText| and |kEndOfText|.
+// - Capacity of the set is limited by |kMaxElements|.
+// - Some insertions at the end of collection may be lost.
 
-// Non-null iff collection is enabled. Also used as the pointer to the array
-// to save a global load in the instrumentation function.
-std::atomic<std::atomic<uint32_t>*> g_enabled_and_array = {g_return_offsets};
+// |g_offsets| and |g_ordered_offsets| are allocated in .bss, as
+// std::atomic<uint32_t> and std::atomic<size_t> are guarangeed to behave like
+// their respective template type parameters with respect to size and
+// initialization.
+// Having these arrays in .bss simplifies initialization.
+std::atomic<uint32_t> g_offsets[kBitfieldSize];
+// Non-null iff collection is enabled. Also use to as the pointer to the offset
+// array to save a global load in the instrumentation.
+std::atomic<std::atomic<uint32_t>*> g_enabled_and_offsets = {g_offsets};
 
-// Used to compute the offset of any return address inside .text, as relative
-// to this function.
-const size_t kStartOfText =
-    reinterpret_cast<size_t>(dummy_function_to_anchor_text);
-const size_t kEndOfText =
-    reinterpret_cast<size_t>(dummy_function_at_the_end_of_text);
+// Ordered list of recorded offsets.
+std::atomic<size_t> g_ordered_offsets[kMaxElements];
+// Next free slot.
+std::atomic<size_t> g_ordered_offset_index;
+
+// Stops recording.
+void Disable() {
+  g_enabled_and_offsets.store(nullptr, std::memory_order_relaxed);
+}
+
+// Records that |address| has been reached, if recording is enabled.
+// To avoid any risk of infinite recursion, this *must* *never* call any
+// instrumented function.
+void RecordAddress(size_t address) {
+  auto* offsets = g_enabled_and_offsets.load(std::memory_order_relaxed);
+  if (!offsets)
+    return;
+
+  if (UNLIKELY(address < base::android::kStartOfText ||
+               address > base::android::kEndOfText)) {
+    Disable();
+    LOG(FATAL) << "Return address out of bounds (are dummy functions in the "
+               << "orderfile?)";
+  }
+
+  size_t offset = address - base::android::kStartOfText;
+  static_assert(sizeof(int) == 4,
+                "Collection and processing code assumes that sizeof(int) == 4");
+  size_t index = offset / 4;
+
+  // Atomically set the corresponding bit in the array.
+  std::atomic<uint32_t>* element = offsets + (index / 32);
+  // First, a racy check. This saves a CAS if the bit is already set, and
+  // allows the cache line to remain shared acoss CPUs in this case.
+  uint32_t value = element->load(std::memory_order_relaxed);
+  uint32_t mask = 1 << (index % 32);
+  if (value & mask)
+    return;
+
+  auto before = element->fetch_or(mask, std::memory_order_relaxed);
+  if (before & mask)
+    return;
+
+  // We were the first one to set the element, record it in the ordered
+  // elements list.
+  // Use relaxed ordering, as the value is not published, or used for
+  // synchronization.
+  size_t ordered_offsets_index =
+      g_ordered_offset_index.fetch_add(1, std::memory_order_relaxed);
+  if (UNLIKELY(ordered_offsets_index >= kMaxElements)) {
+    Disable();
+    LOG(FATAL) << "Too many reached offsets";
+  }
+  g_ordered_offsets[ordered_offsets_index].store(offset,
+                                                 std::memory_order_relaxed);
+}
+
+// Stops recording, and outputs the data to |path|.
+void StopAndDumpToFile(const base::FilePath& path) {
+  Disable();
+  auto file = base::File(base::FilePath(path), base::File::FLAG_CREATE_ALWAYS |
+                                                   base::File::FLAG_WRITE);
+  if (!file.IsValid()) {
+    PLOG(ERROR) << "Could not open " << path;
+    return;
+  }
+
+  std::atomic_thread_fence(std::memory_order_seq_cst);
+  // |g_ordered_offset_index| is the index of the next insertion.
+  size_t count = g_ordered_offset_index.load(std::memory_order_relaxed) - 1;
+  for (size_t i = 0; i < count; i++) {
+    // |g_ordered_offsets| is initialized to 0, so a 0 in the middle of it
+    // indicates a case where the index was incremented, but the write is not
+    // visible in this thread yet. Safe to skip, also because the function at
+    // the start of text is never called.
+    auto offset = g_ordered_offsets[i].load(std::memory_order_relaxed);
+    if (!offset)
+      continue;
+    auto offset_str = base::StringPrintf("%" PRIuS "\n", offset);
+    file.WriteAtCurrentPos(offset_str.c_str(),
+                           static_cast<int>(offset_str.size()));
+  }
+}
 
 // Disables the logging and dumps the result after |kDelayInSeconds|.
 class DelayedDumper {
  public:
   DelayedDumper() {
-    // The linker usually keeps the input file ordering for symbols.
-    // dummy_function_to_anchor_text() should then be after
-    // dummy_function_to_check_ordering() without ordering.
-    // This check is thus intended to catch the lack of ordering.
-    CHECK_LT(kStartOfText,
-             reinterpret_cast<size_t>(&dummy_function_to_check_ordering));
-    CHECK_LT(kStartOfText, kEndOfText);
-    CHECK_LT(kEndOfText - kStartOfText, kMaxTextSizeInBytes);
-    CHECK_LT(kStartOfText,
-             reinterpret_cast<size_t>(&dummy_function_to_check_ordering));
+    CHECK_LT(base::android::kEndOfText - base::android::kStartOfText,
+             kMaxTextSizeInBytes);
+    base::android::CheckOrderingSanity();
 
     std::thread([]() {
       sleep(kDelayInSeconds);
-      // As Dump() is called from the same thread, ensures that the
-      // base functions called in the dumping code will not spuriously appear
-      // in the profile.
-      g_enabled_and_array.store(nullptr, std::memory_order_relaxed);
-      Dump();
+      auto path = base::StringPrintf(
+          "/data/local/tmp/chrome/cyglog/"
+          "cygprofile-instrumented-code-hitmap-%d.txt",
+          getpid());
+      StopAndDumpToFile(base::FilePath(path));
     })
         .detach();
-  }
-
- private:
-  // Dumps the data to disk.
-  static void Dump() {
-    CHECK(!g_enabled_and_array.load(std::memory_order_relaxed));
-
-    auto path = base::StringPrintf(
-        "/data/local/tmp/chrome/cyglog/"
-        "cygprofile-instrumented-code-hitmap-%d.txt",
-        getpid());
-    auto file =
-        base::File(base::FilePath(path),
-                   base::File::FLAG_CREATE_ALWAYS | base::File::FLAG_WRITE);
-    if (!file.IsValid()) {
-      PLOG(ERROR) << "Could not open " << path;
-      return;
-    }
-
-    auto data = std::vector<char>(kArraySize * 32 + 1, '\0');
-    for (size_t i = 0; i < kArraySize; i++) {
-      uint32_t value = g_return_offsets[i].load(std::memory_order_relaxed);
-      for (int bit = 0; bit < 32; bit++) {
-        data[32 * i + bit] = value & (1 << bit) ? '1' : '0';
-      }
-    }
-    data[32 * kArraySize] = '\n';
-    file.WriteAtCurrentPos(&data[0], static_cast<int>(data.size()));
   }
 
   static constexpr int kDelayInSeconds = 30;
@@ -115,6 +171,8 @@ class DelayedDumper {
 // |kDelayInSeconds|.
 DelayedDumper g_dump_later;
 
+}  // namespace
+
 extern "C" {
 
 // Since this function relies on the return address, if it's not inlined and
@@ -123,32 +181,7 @@ extern "C" {
 // We cannot use ALWAYS_INLINE from src/base/compiler_specific.h, as it doesn't
 // always map to always_inline, for instance when NDEBUG is not defined.
 __attribute__((__always_inline__)) void mcount() {
-  // To avoid any risk of infinite recusion, this *must* *never* call any
-  // instrumented function.
-  auto* array = g_enabled_and_array.load(std::memory_order_relaxed);
-  if (!array)
-    return;
-
-  size_t return_address = reinterpret_cast<size_t>(__builtin_return_address(0));
-  // Not a CHECK() to avoid the possibility of calling an instrumented function.
-  if (UNLIKELY(return_address < kStartOfText || return_address > kEndOfText)) {
-    g_enabled_and_array.store(nullptr, std::memory_order_relaxed);
-    LOG(FATAL) << "Return address out of bounds (wrong ordering?)";
-  }
-
-  size_t index = (return_address - kStartOfText) / sizeof(int);
-  // Atomically set the corresponding bit in the array.
-  std::atomic<uint32_t>* element = array + (index / 32);
-  // First, a racy check. This saves a CAS if the bit is already set, and allows
-  // the cache line to remain shared acoss CPUs in this case.
-  uint32_t value = element->load(std::memory_order_relaxed);
-  uint32_t mask = (1 << (index % 32));
-  if (value & mask)
-    return;
-
-  // We do not need any barrier because no other memory location is
-  // read/modified in this thread, hence the relaxed memory ordering.
-  std::atomic_fetch_or_explicit(element, mask, std::memory_order_relaxed);
+  RecordAddress(reinterpret_cast<size_t>(__builtin_return_address(0)));
 }
 
 void __cyg_profile_func_enter(void* unused1, void* unused2) {
@@ -159,5 +192,3 @@ void __cyg_profile_func_enter(void* unused1, void* unused2) {
 void __cyg_profile_func_exit(void* unused1, void* unused2) {}
 
 }  // extern "C"
-
-}  // namespace
