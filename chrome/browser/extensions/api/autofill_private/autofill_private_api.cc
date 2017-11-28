19249ada578f5177841db69b5d3fb347dd5d1e67
sebsg <sebsg@chromium.org>
2017-11-28 20:06:58 +0000

    [Autofill] Fix bug in settings when card has no billing address.
    Bug: 789059
    Change-Id: I60aee9281d367fe8df545fef59ad920462d10a87
    Reviewed-on: https://chromium-review.googlesource.com/794095
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519808}
    Former-commit-id: fd7a8ba336b85a78fdc4ee85b90d5b4c32664c24

--- chrome/browser/extensions/api/autofill_private/autofill_private_api.cc
+++ chrome/browser/extensions/api/autofill_private/autofill_private_api.cc
@@ -399,7 +399,7 @@ ExtensionFunction::ResponseAction AutofillPrivateSaveCreditCardFunction::Run() {
         base::UTF8ToUTF16(*card->expiration_year));
   }
 
-  if (!card->billing_address_id->empty()) {
+  if (card->billing_address_id) {
     credit_card.set_billing_address_id(*card->billing_address_id);
   }
 
