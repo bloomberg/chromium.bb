d1c9ff33072c9624f379e79cab88bec3dea9f9c2
Myung-jong Kim <mjkim610@gmail.com>
2017-11-28 19:36:05 +0000

    Remove gc from worker-event-listener.js
    Bug: 706285
    Change-Id: I21524ca6076351b4fd056443add699dbad62841b
    Reviewed-on: https://chromium-review.googlesource.com/789734
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519797}
    Former-commit-id: b4c579cc9e9c123839a0674bda83b4a91276c905

--- third_party/WebKit/LayoutTests/fast/workers/resources/shared-worker-common.js
+++ third_party/WebKit/LayoutTests/fast/workers/resources/shared-worker-common.js
@@ -8,13 +8,6 @@ onconnect = function(event) {
     event.ports[0].onmessage = function(evt) { handleMessage(evt, event.ports[0]); };
 };
 
-function gc()
-{
-    for (var i = 0; i < 10000; i++) { // force garbage collection (FF requires about 9K allocations before a collect)
-        var s = new String("abc");
-    }
-}
-
 function handleMessage(event, port) {
     self.port = port;
     if (event.data == "ping")
