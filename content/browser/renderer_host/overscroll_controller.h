f37adee2756be88b46f51e4159830b034b6a81f1
Jose Dapena Paz <jose.dapena@lge.com>
2017-11-28 15:58:21 +0000

    Build: fix content_browsertests aura component build.
    MockOverscrollControllerImpl in browser_tests inherits from
    OverscrollController in content/browser, but the class was
    not exported, causing a build failure with is_component_build.
    
    Change-Id: I9e3b893a23243e292fa94cc4441821631dafcca2
    Reviewed-on: https://chromium-review.googlesource.com/788213
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: José Dapena Paz <jose.dapena@lge.com>
    Cr-Commit-Position: refs/heads/master@{#519699}
    Former-commit-id: db116c0234b23722f603687a3288417a0d607207

--- content/browser/renderer_host/overscroll_controller.h
+++ content/browser/renderer_host/overscroll_controller.h
@@ -37,13 +37,13 @@ enum class OverscrollSource {
 // overscroll gesture. This controller receives the events that are dispatched
 // to the renderer, and the ACKs of events, and updates the overscroll gesture
 // status accordingly.
-class OverscrollController {
+class CONTENT_EXPORT OverscrollController {
  public:
   // Exported for testing.
   // TODO(mcnee): Tests needing CONTENT_EXPORT are BrowserPlugin specific.
   // Remove after removing BrowserPlugin (crbug.com/533069).
-  CONTENT_EXPORT OverscrollController();
-  CONTENT_EXPORT virtual ~OverscrollController();
+  OverscrollController();
+  virtual ~OverscrollController();
 
   // This must be called when dispatching any event from the
   // RenderWidgetHostView so that the state of the overscroll gesture can be
@@ -56,9 +56,8 @@ class OverscrollController {
   // Virtual and exported for testing.
   // TODO(mcnee): Tests needing CONTENT_EXPORT and virtual are BrowserPlugin
   // specific. Remove after removing BrowserPlugin (crbug.com/533069).
-  CONTENT_EXPORT virtual void ReceivedEventACK(
-      const blink::WebInputEvent& event,
-      bool processed);
+  virtual void ReceivedEventACK(const blink::WebInputEvent& event,
+                                bool processed);
 
   OverscrollMode overscroll_mode() const { return overscroll_mode_; }
 
