a09af6244cb03f4e4a0f2cf30e1faa72257a593e
Lei Tian <ltian@chromium.org>
2017-11-28 01:24:19 +0000

    Reland "Use TabPersistentStoreObserver.onMetadataSavedAsynchronously to background the Browser Actions tab creation service"
    This relands commit 7a5d1c035de49882aeadbccc958349cc1a4e563e [formerly 64565fd707ff5a4e2d1b378b26dc2e28e35e4806].
    
    Original change's description:
    > Use TabPersistentStoreObserver.onMetadataSavedAsynchronously to
    background the Browser Actions tab creation service
    >
    > Change the logic to background the Browser Actions tab creation
    service
    > based on TabPersistentStoreObserver.onMetadataSavedAsynchronously. The
    > callback indicates that tab model lists are backuped so the saved tabs
    > can be restored succesfully.
    >
    > Also for the notification when a new tab has not been created (tabId
    is
    > not available), the Intent to bind the notification will be a VIEW
    > intent. This notification is necessary to make the service foreground.
    >
    > Bug: 766349
    > Change-Id: I73d49e2a1e26410b2dd6e51a07f35e9965b9a38a
    > Reviewed-on: https://chromium-review.googlesource.com/769852
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Commit-Queue: Lei Tian <ltian@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#517492}
    
    Bug: 766349
    Change-Id: Icb35c3b0e3ed2186e48c7f0e77ceb6a07bc1f39f
    Reviewed-on: https://chromium-review.googlesource.com/791392
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Lei Tian <ltian@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519509}
    Former-commit-id: 1c3e620eee0877d415e9f8c651dea8c77d399cce

--- chrome/android/java/src/org/chromium/chrome/browser/browseractions/BrowserActionsTabModelSelector.java
+++ chrome/android/java/src/org/chromium/chrome/browser/browseractions/BrowserActionsTabModelSelector.java
@@ -6,6 +6,7 @@ package org.chromium.chrome.browser.browseractions;
 
 import android.os.AsyncTask;
 
+import org.chromium.base.Callback;
 import org.chromium.base.ThreadUtils;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.browseractions.BrowserActionsTabCreatorManager.BrowserActionsTabCreator;
@@ -126,11 +127,12 @@ public class BrowserActionsTabModelSelector
     /**
      * Creates a new Tab with given url in Browser Actions tab model.
      * @param loadUrlParams The url params to be opened.
+     * @param tabCreatedCallback The {@link Callback} to run when tab is created.
      */
-    public void openNewTab(LoadUrlParams loadUrlParams) {
+    public void openNewTab(LoadUrlParams loadUrlParams, Callback<Integer> tabCreatedCallback) {
         // If tab model is restored, directly create a new tab.
         if (isTabStateInitialized()) {
-            createNewTab(loadUrlParams);
+            createNewTab(loadUrlParams, tabCreatedCallback);
             return;
         }
         if (mTabCreationRunnable == null) {
@@ -138,7 +140,7 @@ public class BrowserActionsTabModelSelector
                 @Override
                 public void run() {
                     for (int i = 0; i < mPendingUrls.size(); i++) {
-                        createNewTab(mPendingUrls.get(i));
+                        createNewTab(mPendingUrls.get(i), tabCreatedCallback);
                     }
                     mPendingUrls.clear();
                 }
@@ -156,8 +158,9 @@ public class BrowserActionsTabModelSelector
         mPendingUrls.add(loadUrlParams);
     }
 
-    private void createNewTab(LoadUrlParams params) {
-        openNewTab(params, TabLaunchType.FROM_BROWSER_ACTIONS, null, false);
+    private void createNewTab(LoadUrlParams params, Callback<Integer> tabCreatedCallback) {
+        Tab tab = openNewTab(params, TabLaunchType.FROM_BROWSER_ACTIONS, null, false);
+        tabCreatedCallback.onResult(tab.getId());
     }
 
     @Override
@@ -241,5 +244,13 @@ public class BrowserActionsTabModelSelector
      */
     public void addTabPersistentStoreObserver(TabPersistentStoreObserver observer) {
         mTabSaver.addObserver(observer);
+    }
+
+    /**
+     * Remove a {@link TabPersistentStoreObserver} from {@link TabPersistentStore}.
+     * @param observer The observer to remove.
+     */
+    public void removeTabPersistentStoreObserver(TabPersistentStoreObserver observer) {
+        mTabSaver.removeObserver(observer);
     }
 }
