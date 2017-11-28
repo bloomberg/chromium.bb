b84ad7c54c514ac7332469967cf0134c8729ab28
Vaclav Brozek <vabr@chromium.org>
2017-11-28 08:29:05 +0000

    [Android settings] Add a menu item to export passwords
    This CL adds a menu item, behind an off-by-default feature, for
    exporting passwords from Chrome's settings. The menu item
    currently does not perform any action.
    
    Design: https://docs.google.com/a/chromium.org/document/d/1miKr2x0PTNIKgt3RQeur51ICQ66uszlYAmFXJONbG_0/edit?usp=sharing
    
    Mocks: go chrome-pwd-export-mocks-android (Google-internal only)
    
    Screenshots: https://crbug.com/788701#c1
    
    Bug: 788701
    Change-Id: I7e8b515596db4be061d494019a34220b9c019caf
    Reviewed-on: https://chromium-review.googlesource.com/776681
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519607}
    Former-commit-id: dcd1fe27c6a6d37766c55a0a26b6db7f655ecd39

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/password/SavePasswordsPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/password/SavePasswordsPreferences.java
@@ -14,9 +14,13 @@ import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.PasswordUIView;
 import org.chromium.chrome.browser.PasswordUIView.PasswordListObserver;
 import org.chromium.chrome.browser.preferences.ChromeBaseCheckBoxPreference;
@@ -52,6 +56,9 @@ public class SavePasswordsPreferences extends PreferenceFragment
     private static final String PREF_CATEGORY_EXCEPTIONS = "exceptions";
     private static final String PREF_MANAGE_ACCOUNT_LINK = "manage_account_link";
     private static final String PREF_CATEGORY_SAVED_PASSWORDS_NO_TEXT = "saved_passwords_no_text";
+
+    // Name of the feature controlling the password export functionality.
+    private static final String EXPORT_PASSWORDS = "password-export";
 
     private static final int ORDER_SWITCH = 0;
     private static final int ORDER_AUTO_SIGNIN_CHECKBOX = 1;
@@ -89,6 +96,25 @@ public class SavePasswordsPreferences extends PreferenceFragment
         getActivity().setTitle(R.string.prefs_saved_passwords);
         setPreferenceScreen(getPreferenceManager().createPreferenceScreen(getActivity()));
         mPasswordManagerHandler.addObserver(this);
+        if (ChromeFeatureList.isEnabled(EXPORT_PASSWORDS)) {
+            setHasOptionsMenu(true);
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.clear();
+        inflater.inflate(R.menu.save_password_preferences_action_bar_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.export_passwords) {
+            // TODO(crbug.com/788701): Trigger the exporting dialogue here.
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     /**
