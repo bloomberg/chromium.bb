9f54f37b5d240cc01bed02a0e9691bbb146055a4
Antonio Gomes <tonikitoo@igalia.com>
2017-11-28 17:37:47 +0000

    Untangle ENABLE_MUS from OS_CHROMEOS in chrome_main.cc
    CL is a first step to untangle ENABLE_MUS from OS_CHROMEOS.
    
    This does not change any ChromeOS behavior, since in ui/base/ui_features.gni
    there is
    
      'enable_mus = is_chromeos'
    
    .. but makes it easier to run Mus on non-ChromeOS builds.
    
    BUG=789043
    
    Change-Id: I0febf99ee8352af9f39c4e3411864e0a1baf3460
    Reviewed-on: https://chromium-review.googlesource.com/793952
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#519728}
    Former-commit-id: 0bd5352f4973f3d84a8e09c0a5898cbb993d3310

--- chrome/app/chrome_main.cc
+++ chrome/app/chrome_main.cc
@@ -104,7 +104,7 @@ int ChromeMain(int argc, const char** argv) {
   }
 #endif  // defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_WIN)
 
-#if defined(OS_CHROMEOS) && BUILDFLAG(ENABLE_MUS)
+#if BUILDFLAG(ENABLE_MUS)
   if (service_manager::ServiceManagerIsRemote() ||
       command_line->HasSwitch(switches::kMash)) {
     params.create_discardable_memory = false;
