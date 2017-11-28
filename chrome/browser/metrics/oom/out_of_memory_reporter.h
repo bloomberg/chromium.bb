68c776663897520f340e2e6c4cdb78a3246739ac
Maria Khomenko <mariakhomenko@chromium.org>
2017-11-28 01:08:38 +0000

    Record UKM for Renderer OOM
    Records the time since last main frame navigation when a renderer OOM is
    detected. On Android record only foreground OOMs.
    
    BUG=736892
    
    Change-Id: I71424844a7bfc9528a6ec865249b046e65275e07
    Reviewed-on: https://chromium-review.googlesource.com/769234
    Commit-Queue: Maria Khomenko <mariakhomenko@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519498}
    Former-commit-id: 11852d9e9d9f090111951887b1c598888198f365

--- chrome/browser/metrics/oom/out_of_memory_reporter.h
+++ chrome/browser/metrics/oom/out_of_memory_reporter.h
@@ -5,9 +5,13 @@
 #ifndef CHROME_BROWSER_METRICS_OOM_OUT_OF_MEMORY_REPORTER_H_
 #define CHROME_BROWSER_METRICS_OOM_OUT_OF_MEMORY_REPORTER_H_
 
+#include <memory>
+
 #include "base/macros.h"
 #include "base/observer_list.h"
 #include "base/scoped_observer.h"
+#include "base/time/tick_clock.h"
+#include "base/time/time.h"
 #include "build/build_config.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
@@ -18,6 +22,8 @@
 #if defined(OS_ANDROID)
 #include "components/crash/content/browser/crash_dump_manager_android.h"
 #endif
+
+class OutOfMemoryReporterTest;
 
 // This class listens for OOM notifications from WebContentsObserver and
 // breakpad::CrashDumpManager::Observer methods. It forwards foreground OOM
@@ -43,29 +49,36 @@ class OutOfMemoryReporter
 
  private:
   friend class content::WebContentsUserData<OutOfMemoryReporter>;
+  friend class OutOfMemoryReporterTest;
 
   OutOfMemoryReporter(content::WebContents* web_contents);
   void OnForegroundOOMDetected(const GURL& url, ukm::SourceId source_id);
+
+  // Used by tests to deterministically control time.
+  void SetTickClockForTest(std::unique_ptr<base::TickClock> tick_clock);
 
   // content::WebContentsObserver:
   void DidFinishNavigation(content::NavigationHandle* handle) override;
   void RenderProcessGone(base::TerminationStatus termination_status) override;
 
-// breakpad::CrashDumpManager::Observer:
 #if defined(OS_ANDROID)
+  // breakpad::CrashDumpManager::Observer:
   void OnCrashDumpProcessed(
       const breakpad::CrashDumpManager::CrashDumpDetails& details) override;
-
-  ScopedObserver<breakpad::CrashDumpManager,
-                 breakpad::CrashDumpManager::Observer>
-      scoped_observer_;
 #endif  // defined(OS_ANDROID)
 
   base::ObserverList<Observer> observers_;
 
   base::Optional<ukm::SourceId> last_committed_source_id_;
+  base::TimeTicks last_navigation_timestamp_;
+  std::unique_ptr<base::TickClock> tick_clock_;
   int crashed_render_process_id_ = content::ChildProcessHost::kInvalidUniqueID;
 
+#if defined(OS_ANDROID)
+  ScopedObserver<breakpad::CrashDumpManager,
+                 breakpad::CrashDumpManager::Observer>
+      scoped_observer_;
+#endif
   DISALLOW_COPY_AND_ASSIGN(OutOfMemoryReporter);
 };
 
