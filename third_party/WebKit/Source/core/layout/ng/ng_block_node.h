e7182367e278fa1c68424af2add77b31ea49c7f7
Yoshifumi Inoue <yosin@chromium.org>
2017-11-29 02:27:01 +0000

    Mark NGBlockNode::FirstChild() as const member function
    This patch marks |NGBlockNode::FirstChild()| as const member function since it
    doesn't mutate its member for improving code health.
    
    Note: This patch is a preparation for using |FirstChild()| in |CanNewLayout()|
    for disable content editable in LayoutNG[1].
    
    [1] http://crrev.com/c/792814  [LayoutNG] Disable LayoutNG for contenteditable
    
    Bug: 591099
    Change-Id: I205aea86bc15c47830c9c86e5949bd688c9c38e5
    Reviewed-on: https://chromium-review.googlesource.com/792813
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519976}
    Former-commit-id: 7ae1c8fc5725217f1555e17a8214915a388a7fa2

--- third_party/WebKit/Source/core/layout/ng/ng_block_node.h
+++ third_party/WebKit/Source/core/layout/ng/ng_block_node.h
@@ -44,7 +44,7 @@ class CORE_EXPORT NGBlockNode final : public NGLayoutInputNode {
 
   NGBoxStrut GetScrollbarSizes() const;
 
-  NGLayoutInputNode FirstChild();
+  NGLayoutInputNode FirstChild() const;
 
   // Layout an atomic inline; e.g., inline block.
   scoped_refptr<NGLayoutResult> LayoutAtomicInline(const NGConstraintSpace&,
