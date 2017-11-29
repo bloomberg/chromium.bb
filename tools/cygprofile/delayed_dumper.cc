e6c2a9ff5422a9e2d609d5754f28f68a6c882a14
Benoit Lize <lizeb@chromium.org>
2017-11-29 09:57:28 +0000

    tools/cygprofile: Order dumps by timestamp.
    Add the timestamp to the filenames, and process them in increasing process
    creation time order. This changes the ordering so that symbols accessed by the
    browser process are at the beginning.
    
    Bug: 776702
    Change-Id: I5e6b9eb61364ed194186900afa64fdd459e65fbe
    Reviewed-on: https://chromium-review.googlesource.com/790991
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Reviewed-by: Matthew Cary <mattcary@chromium.org>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520057}
    Former-commit-id: e24c2fd0c9054d0d971978a981278c64f0c4a086

--- tools/cygprofile/delayed_dumper.cc
+++ tools/cygprofile/delayed_dumper.cc
@@ -2,12 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <time.h>
 #include <unistd.h>
 
 #include <thread>
 
 #include "base/android/library_loader/anchor_functions.h"
 #include "base/files/file.h"
+#include "base/format_macros.h"
 #include "base/logging.h"
 #include "base/strings/stringprintf.h"
 #include "build/build_config.h"
@@ -25,13 +27,20 @@ namespace {
 class DelayedDumper {
  public:
   DelayedDumper() {
-    SanityChecks();
-    std::thread([]() {
+    // Not using base::TimeTicks() to not call too many base:: symbol that would
+    // pollute the reached symbols dumps.
+    struct timespec ts;
+    if (clock_gettime(CLOCK_MONOTONIC, &ts))
+      PLOG(FATAL) << "clock_gettime.";
+    uint64_t start_ns_since_epoch =
+        static_cast<uint64_t>(ts.tv_sec) * 1000 * 1000 * 1000 + ts.tv_nsec;
+
+    std::thread([start_ns_since_epoch]() {
       sleep(kDelayInSeconds);
       auto path = base::StringPrintf(
           "/data/local/tmp/chrome/cyglog/"
-          "cygprofile-instrumented-code-hitmap-%d.txt",
-          getpid());
+          "cygprofile-instrumented-code-hitmap-%d-%" PRIu64 ".txt",
+          getpid(), start_ns_since_epoch);
       StopAndDumpToFile(base::FilePath(path));
     })
         .detach();
