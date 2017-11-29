9ea490c02e993154ba0d3c6c0ec97f6e3ad2d5b6
Henrik Boström <hbos@chromium.org>
2017-11-29 15:42:20 +0000

    Disable flaky testAutoDismissPromo() (attempt #2)
    TBR=bsazonov@chromium.org,bauerb@chromium.org,iuliah@google.com
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 789531
    Change-Id: Iffeb007f1c4181cb72553298d98c9f332ce19ea3
    Reviewed-on: https://chromium-review.googlesource.com/797030
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520123}
    Former-commit-id: 840e0f72691f6dd93fb5a1ac68ad39094ce7df3a

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedSigninPromoTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedSigninPromoTest.java
@@ -31,6 +31,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.ChromeSwitches;
@@ -84,6 +85,7 @@ public class BookmarkPersonalizedSigninPromoTest {
     @Test
     @LargeTest
     @DisableIf.Device(type = {UiDisableIf.TABLET}) // https://crbug.com/776405.
+    @DisabledTest(message = "Flaky. See crbug.com/789531")
     public void testAutoDismissPromo() throws Exception {
         int impressionCap = SigninPromoController.getMaxImpressionsBookmarksForTests();
         for (int impression = 0; impression < impressionCap; impression++) {
