7399048dbde0eee196f2ba8e8aef68d7df1c1f0f
Avi Drissman <avi@chromium.org>
2017-11-29 22:32:29 +0000

    Refactor titling of JavaScript alerts.
    No functional change, just making it testable.
    
    BUG=696454
    TEST=The new test!
    TBR=caitkp@chromium.org
    
    Change-Id: Iea28648b76856a7753776e21eeed809ef85460ec
    Reviewed-on: https://chromium-review.googlesource.com/794171
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Cait Phillips <caitkp@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520277}
    Former-commit-id: 82c4500f8a46069fb67416357a77f8ae04d89cda

--- components/app_modal/javascript_dialog_manager.h
+++ components/app_modal/javascript_dialog_manager.h
@@ -7,6 +7,7 @@
 
 #include <memory>
 
+#include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/memory/singleton.h"
 #include "base/time/time.h"
@@ -60,6 +61,7 @@ class JavaScriptDialogManager : public content::JavaScriptDialogManager {
                      bool reset_state) override;
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(JavaScriptDialogManagerTest, GetTitle);
   friend struct base::DefaultSingletonTraits<JavaScriptDialogManager>;
 
   JavaScriptDialogManager();
@@ -77,6 +79,9 @@ class JavaScriptDialogManager : public content::JavaScriptDialogManager {
                       DialogClosedCallback callback,
                       bool success,
                       const base::string16& user_input);
+
+  static base::string16 GetTitleImpl(const GURL& parent_frame_url,
+                                     const GURL& alerting_frame_url);
 
   // Mapping between the WebContents and their extra data. The key
   // is a void* because the pointer is just a cookie and is never dereferenced.
