caa48c7749475fdf20ee86f17c25715d33cc18b2
Xiaocheng Hu <xiaochengh@chromium.org>
2017-11-29 05:30:04 +0000

    Simplify TextControlElement::ComputeSelectionStart/End
    We used to need such handling for different selection granularities
    in the above mentioned functions because when granularity is not
    character, we used to have a mismatch between:
    - FrameSelection::SelectionInDOMTree::ComputeStart/EndPosition()
    - FrameSelection::ComputeVisibleSelectionInDOMTree::Start/End()
    
    Now that granularity is completely moved to FrameSelection, there is
    no longer such mismatch. So we can use the same logic for all
    granularities. As a side effect, this also allows us to remove some
    call sites of ComputeVisibleSelectionInDOMTreeDeprecated().
    
    Note that this patch changes the calculation of IndexForPosition from
    using the canonicalized position to the un-canonicalized position.
    This doesn't change the result, because there is only plain text in
    text control elements, while canonicalization doesn't skip any plain
    text (unless there is bug in canonicalization).
    
    Behavior covered by layout tests:
    - fast/forms/onselect-textfield.html
    - fast/forms/textarea/onselect-textarea.html
    
    Bug: 698633,740804
    Change-Id: Id19d694284a5b439dd4585b88c0e40ad99f33f75
    Reviewed-on: https://chromium-review.googlesource.com/786397
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520020}
    Former-commit-id: 8bb1cbaee778761d1716d455e51e033e32687022

--- third_party/WebKit/Source/core/html/forms/TextControlElement.cpp
+++ third_party/WebKit/Source/core/html/forms/TextControlElement.cpp
@@ -514,22 +514,16 @@ unsigned TextControlElement::ComputeSelectionStart() const {
   LocalFrame* frame = GetDocument().GetFrame();
   if (!frame)
     return 0;
-  {
-    // To avoid regression on speedometer benchmark[1] test, we should not
-    // update layout tree in this code block.
-    // [1] http://browserbench.org/Speedometer/
-    DocumentLifecycle::DisallowTransitionScope disallow_transition(
-        GetDocument().Lifecycle());
-    const SelectionInDOMTree& selection =
-        frame->Selection().GetSelectionInDOMTree();
-    if (frame->Selection().Granularity() == TextGranularity::kCharacter) {
-      return IndexForPosition(InnerEditorElement(),
-                              selection.ComputeStartPosition());
-    }
-  }
-  const VisibleSelection& visible_selection =
-      frame->Selection().ComputeVisibleSelectionInDOMTreeDeprecated();
-  return IndexForPosition(InnerEditorElement(), visible_selection.Start());
+
+  // To avoid regression on speedometer benchmark[1] test, we should not
+  // update layout tree in this code block.
+  // [1] http://browserbench.org/Speedometer/
+  DocumentLifecycle::DisallowTransitionScope disallow_transition(
+      GetDocument().Lifecycle());
+  const SelectionInDOMTree& selection =
+      frame->Selection().GetSelectionInDOMTree();
+  return IndexForPosition(InnerEditorElement(),
+                          selection.ComputeStartPosition());
 }
 
 unsigned TextControlElement::selectionEnd() const {
@@ -545,22 +539,15 @@ unsigned TextControlElement::ComputeSelectionEnd() const {
   LocalFrame* frame = GetDocument().GetFrame();
   if (!frame)
     return 0;
-  {
-    // To avoid regression on speedometer benchmark[1] test, we should not
-    // update layout tree in this code block.
-    // [1] http://browserbench.org/Speedometer/
-    DocumentLifecycle::DisallowTransitionScope disallow_transition(
-        GetDocument().Lifecycle());
-    const SelectionInDOMTree& selection =
-        frame->Selection().GetSelectionInDOMTree();
-    if (frame->Selection().Granularity() == TextGranularity::kCharacter) {
-      return IndexForPosition(InnerEditorElement(),
-                              selection.ComputeEndPosition());
-    }
-  }
-  const VisibleSelection& visible_selection =
-      frame->Selection().ComputeVisibleSelectionInDOMTreeDeprecated();
-  return IndexForPosition(InnerEditorElement(), visible_selection.End());
+
+  // To avoid regression on speedometer benchmark[1] test, we should not
+  // update layout tree in this code block.
+  // [1] http://browserbench.org/Speedometer/
+  DocumentLifecycle::DisallowTransitionScope disallow_transition(
+      GetDocument().Lifecycle());
+  const SelectionInDOMTree& selection =
+      frame->Selection().GetSelectionInDOMTree();
+  return IndexForPosition(InnerEditorElement(), selection.ComputeEndPosition());
 }
 
 static const AtomicString& DirectionString(
