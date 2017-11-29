b15d01b53fca227b103f8eca24cf1bc83838c2bc
Tommy Steimel <steimel@chromium.org>
2017-11-29 19:41:45 +0000

    Add ability to mute chrome:// pages
    Bug: 787743, 785711, 782134
    Change-Id: I77fe9a7192697ebe82734231ae709c2094b57b62
    Reviewed-on: https://chromium-review.googlesource.com/791813
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520203}
    Former-commit-id: 37112950bc5ed3dbd06fdf42c0367ff246939287

--- chrome/browser/ui/tabs/tab_utils.h
+++ chrome/browser/ui/tabs/tab_utils.h
@@ -37,12 +37,13 @@ enum class TabAlertState {
 };
 
 enum class TabMutedReason {
-  NONE,             // The tab has never been muted or unmuted.
-  CONTEXT_MENU,     // Mute/Unmute chosen from tab context menu.
-  AUDIO_INDICATOR,  // Mute toggled via tab-strip audio icon.
-  MEDIA_CAPTURE,    // Media recording/capture was started.
-  EXTENSION,        // Mute state changed via extension API.
-  CONTENT_SETTING,  // The sound content setting was set to BLOCK.
+  NONE,                    // The tab has never been muted or unmuted.
+  CONTEXT_MENU,            // Mute/Unmute chosen from tab context menu.
+  AUDIO_INDICATOR,         // Mute toggled via tab-strip audio icon.
+  MEDIA_CAPTURE,           // Media recording/capture was started.
+  EXTENSION,               // Mute state changed via extension API.
+  CONTENT_SETTING,         // The sound content setting was set to BLOCK.
+  CONTENT_SETTING_CHROME,  // Mute toggled on chrome:// URL.
 };
 
 enum class TabMutedResult {
