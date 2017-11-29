e2020175b7230a3beab4bd5a3a152bb5098e5984
James Zern <jzern@chromium.org>
2017-11-29 19:42:02 +0000

    libwebp: upgrade to libwebp-0.6.1
    BUG=788275
    
    Change-Id: Ied2077ce95dd3a0eb1d97ebf5f063c1da4975025
    Reviewed-on: https://chromium-review.googlesource.com/792633
    Commit-Queue: James Zern <jzern@google.com>
    Reviewed-by: James Zern <jzern@google.com>
    Reviewed-by: Tom Finegan <tomfinegan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520204}
    Former-commit-id: 0647aba12e2e59f823120af5a13c90d963e65b3f

--- third_party/libwebp/examples/cwebp.c
+++ third_party/libwebp/examples/cwebp.c
@@ -463,8 +463,9 @@ static int WriteWebPWithMetadata(FILE* const out,
     } else {
       const int is_lossless = !memcmp(webp, "VP8L", kTagSize);
       if (is_lossless) {
-        // Presence of alpha is stored in the 29th bit of VP8L data.
-        if (webp[kChunkHeaderSize + 3] & (1 << 5)) flags |= kAlphaFlag;
+        // Presence of alpha is stored in the 37th bit (29th after the
+        // signature) of VP8L data.
+        if (webp[kChunkHeaderSize + 4] & (1 << 4)) flags |= kAlphaFlag;
       }
       ok = ok && (fwrite(kVP8XHeader, kChunkHeaderSize, 1, out) == 1);
       ok = ok && WriteLE32(out, flags);
@@ -486,10 +487,10 @@ static int WriteWebPWithMetadata(FILE* const out,
       *metadata_written |= METADATA_XMP;
     }
     return ok;
-  } else {
-    // No metadata, just write the original image file.
-    return (fwrite(webp, webp_size, 1, out) == 1);
   }
+
+  // No metadata, just write the original image file.
+  return (fwrite(webp, webp_size, 1, out) == 1);
 }
 
 //------------------------------------------------------------------------------
