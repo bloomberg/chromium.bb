cfacfcd43dc7e0b1731a42c1573290fcf2a9bb5f
Ken Buchanan <kenrb@chromium.org>
2017-11-29 19:28:57 +0000

    Correctly register RenderFrames with their associated RenderWidgets
    Currently all RenderFrameImpls call RegisterRenderFrame on the
    RenderView, and additionally, out-of-process iframe RFIs call it on the
    RenderWidgets that they own.
    
    This is incorrect for local child frames of OOPIFs, because they need
    to register with the OOPIF's RenderWidget also.
    
    This patch corrects and simplifies RenderFrameImpl widget registration,
    making all of them register with their associated widgets after their
    WebFrame is created, and unregister just before their WebFrame is
    destroyed.
    
    Bug: 770622
    Change-Id: I51be01147d3a810bf6e6c0214150b3714ea1c181
    Reviewed-on: https://chromium-review.googlesource.com/791071
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520196}
    Former-commit-id: c398e285eea601718751f6b6cac5a126bebba724

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -1193,11 +1193,6 @@ void RenderFrameImpl::CreateFrame(
     render_frame->render_widget_ = RenderWidget::CreateForFrame(
         widget_params.routing_id, widget_params.hidden,
         render_frame->render_view_->screen_info(), compositor_deps, web_frame);
-    // TODO(avi): The main frame re-uses the RenderViewImpl as its widget, so
-    // avoid double-registering the frame as an observer.
-    // https://crbug.com/545684
-    if (web_frame->Parent())
-      render_frame->render_widget_->RegisterRenderFrame(render_frame);
   }
 
   render_frame->Initialize();
@@ -1348,8 +1343,6 @@ RenderFrameImpl::RenderFrameImpl(CreateParams params)
 
   RenderThread::Get()->AddRoute(routing_id_, this);
 
-  render_view_->RegisterRenderFrame(this);
-
   // Everything below subclasses RenderFrameObserver and is automatically
   // deleted when the RenderFrame gets deleted.
 #if defined(OS_ANDROID)
@@ -1407,13 +1400,14 @@ RenderFrameImpl::~RenderFrameImpl() {
     render_view_->main_render_frame_ = nullptr;
   }
 
-  render_view_->UnregisterRenderFrame(this);
   g_routing_id_frame_map.Get().erase(routing_id_);
   RenderThread::Get()->RemoveRoute(routing_id_);
 }
 
 void RenderFrameImpl::Initialize() {
   is_main_frame_ = !frame_->Parent();
+
+  GetRenderWidget()->RegisterRenderFrame(this);
 
   RenderFrameImpl* parent_frame =
       RenderFrameImpl::FromWebFrame(frame_->Parent());
@@ -3518,10 +3512,9 @@ void RenderFrameImpl::FrameDetached(DetachType type) {
     Send(new FrameHostMsg_Detach(routing_id_));
 
   // Clean up the associated RenderWidget for the frame, if there is one.
-  if (render_widget_) {
-    render_widget_->UnregisterRenderFrame(this);
+  GetRenderWidget()->UnregisterRenderFrame(this);
+  if (render_widget_)
     render_widget_->CloseForFrame();
-  }
 
   // We need to clean up subframes by removing them from the map and deleting
   // the RenderFrameImpl.  In contrast, the main frame is owned by its
