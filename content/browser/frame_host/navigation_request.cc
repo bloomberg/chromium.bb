b26e35a732f271b41c551d74dc4c5cc0a5cb0413
clamy <clamy@chromium.org>
2017-11-29 16:14:13 +0000

    PlzNavigate: fix issue with missing upgrade insecure request header
    This CL ensures that the upgrade-insecure-request header is properly set
    on navigations when PlzNavigate is on and that it is before the
    user-agent one as some websites (e.g. hertz.com) seem to expect this.
    
    BUG=776164
    
    Change-Id: Ib388dcd640c8042c458b21c5edfb6768e145e278
    Reviewed-on: https://chromium-review.googlesource.com/793712
    Commit-Queue: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520130}
    Former-commit-id: 31c3332e3c10c64ec14f4920bda83776941c5937

--- content/browser/frame_host/navigation_request.cc
+++ content/browser/frame_host/navigation_request.cc
@@ -164,6 +164,11 @@ void AddAdditionalRequestHeaders(net::HttpRequestHeaders* headers,
   if (embedder_additional_headers)
     headers->MergeFrom(*(embedder_additional_headers.get()));
 
+  // Tack an 'Upgrade-Insecure-Requests' header to outgoing navigational
+  // requests, as described in
+  // https://w3c.github.io/webappsec/specs/upgrade/#feature-detect
+  headers->SetHeaderIfMissing("Upgrade-Insecure-Requests", "1");
+
   headers->SetHeaderIfMissing(net::HttpRequestHeaders::kUserAgent,
                               user_agent_override.empty()
                                   ? GetContentClient()->GetUserAgent()
@@ -172,11 +177,6 @@ void AddAdditionalRequestHeaders(net::HttpRequestHeaders* headers,
   // Check whether DevTools wants to override user agent for this request
   // after setting the default user agent, or append throttling control header.
   RenderFrameDevToolsAgentHost::AppendDevToolsHeaders(frame_tree_node, headers);
-
-  // Tack an 'Upgrade-Insecure-Requests' header to outgoing navigational
-  // requests, as described in
-  // https://w3c.github.io/webappsec/specs/upgrade/#feature-detect
-  headers->AddHeaderFromString("Upgrade-Insecure-Requests: 1");
 
   // Next, set the HTTP Origin if needed.
   if (!NeedsHTTPOrigin(headers, method))
