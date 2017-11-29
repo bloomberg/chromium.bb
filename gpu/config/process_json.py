7040b786f9bcacf9cbf49177e010ea83e0123bd0
Bruce Dawson <brucedawson@chromium.org>
2017-11-29 05:27:33 +0000

    Add OS type checking to process_json.py
    File adding a workaround to a GPU driver bug the os type "win" was
    misspelled as "windows". This caused a one-week delay in testing the
    workaround. This adds an OS type check to avoid this error in the
    future. It was confirmed that this check would have detected and
    prevented the recent error, with this error message:
    
        Exception: Unknown OS type "windows" for entry 248
    
    Bug: 775898,785648
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Idd1a4266087acbae24cfc6e60ac873f17c7ac7ef
    Reviewed-on: https://chromium-review.googlesource.com/791930
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520018}
    Former-commit-id: f0980d12d3c008a78a2a03109e2b63954f60b680

--- gpu/config/process_json.py
+++ gpu/config/process_json.py
@@ -26,6 +26,15 @@ _DO_NOT_EDIT_WARNING = """// This file is auto-generated from
 
 """
 
+_OS_TYPE_MAP = {
+    'win': 'kOsWin',
+    'macosx': 'kOsMacosx',
+    'android': 'kOsAndroid',
+    'linux': 'kOsLinux',
+    'chromeos': 'kOsChromeOS',
+    '': 'kOsAny',
+  }
+
 
 def load_software_rendering_list_features(feature_type_filename):
   header_file = open(feature_type_filename, 'rb')
@@ -286,16 +295,8 @@ def write_machine_model_info(entry_id, is_exception, exception_id,
 
 
 def write_os_type(os_type, data_file):
-  map = {
-    'win': 'kOsWin',
-    'macosx': 'kOsMacosx',
-    'android': 'kOsAndroid',
-    'linux': 'kOsLinux',
-    'chromeos': 'kOsChromeOS',
-    '': 'kOsAny',
-  }
-  assert map.has_key(os_type)
-  data_file.write('GpuControlList::%s,  // os_type\n' % map[os_type])
+  assert _OS_TYPE_MAP.has_key(os_type)
+  data_file.write('GpuControlList::%s,  // os_type\n' % _OS_TYPE_MAP[os_type])
 
 
 def write_multi_gpu_category(multi_gpu_category, data_file):
@@ -618,8 +619,14 @@ def process_json_file(json_filepath, list_tag,
     entry_id = entry['id']
     assert entry_id not in ids
     ids.append(entry_id)
-    if os_filter != None and 'os' in entry and entry['os']['type'] != os_filter:
-      continue
+    if 'os' in entry:
+      os_type = entry['os']['type']
+      # Check for typos in the .json data
+      if not _OS_TYPE_MAP.has_key(os_type):
+        raise Exception('Unknown OS type "%s" for entry %d' %
+                        (os_type, entry_id))
+      if os_filter != None and os_type != os_filter:
+        continue
     entry_count += 1
     write_entry(entry, total_features, feature_tag,
                 data_file, data_helper_file, data_exception_file)
