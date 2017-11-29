c37554db6bdc399f7e2bf9700c4da3953634cb4e
Becca Hughes <beccahughes@chromium.org>
2017-11-29 11:52:05 +0000

    Media Engagement: Short playback detection
    Measures playback and ignores any playbacks under a certain threshold.
    Triggers a UKM "Media.Engagement.Ignored" event if the value was
    ignored.
    
    BUG=781726
    
    Change-Id: I368a1ab514ff73c064b84134984278d52f49142a
    Reviewed-on: https://chromium-review.googlesource.com/758581
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520078}
    Former-commit-id: e1dc0700afc71ac232659e1dad6c77e5df1d3eb8

--- chrome/browser/media/media_engagement_browsertest.cc
+++ chrome/browser/media/media_engagement_browsertest.cc
@@ -6,6 +6,7 @@
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/test/simple_test_clock.h"
 #include "base/test/test_mock_time_task_runner.h"
 #include "base/time/time.h"
 #include "chrome/browser/media/media_engagement_contents_observer.h"
@@ -70,7 +71,8 @@ class WasRecentlyAudibleWatcher {
 class MediaEngagementBrowserTest : public InProcessBrowserTest {
  public:
   MediaEngagementBrowserTest()
-      : task_runner_(new base::TestMockTimeTaskRunner()) {
+      : test_clock_(new base::SimpleTestClock()),
+        task_runner_(new base::TestMockTimeTaskRunner()) {
     http_server_.ServeFilesFromSourceDirectory(kMediaEngagementTestDataPath);
     http_server_origin2_.ServeFilesFromSourceDirectory(
         kMediaEngagementTestDataPath);
@@ -86,6 +88,8 @@ class MediaEngagementBrowserTest : public InProcessBrowserTest {
         media::kRecordMediaEngagementScores);
 
     InProcessBrowserTest::SetUp();
+
+    injected_clock_ = false;
   };
 
   void LoadTestPage(const std::string& page) {
@@ -110,7 +114,10 @@ class MediaEngagementBrowserTest : public InProcessBrowserTest {
   };
 
   void Advance(base::TimeDelta time) {
+    DCHECK(injected_clock_);
     task_runner_->FastForwardBy(time);
+    static_cast<base::SimpleTestClock*>(GetService()->clock_.get())
+        ->Advance(time);
     base::RunLoop().RunUntilIdle();
   }
 
@@ -199,6 +206,11 @@ class MediaEngagementBrowserTest : public InProcessBrowserTest {
   }
 
   void InjectTimerTaskRunner() {
+    if (!injected_clock_) {
+      GetService()->clock_ = std::move(test_clock_);
+      injected_clock_ = true;
+    }
+
     contents_observer()->SetTaskRunnerForTest(task_runner_);
   }
 
@@ -211,6 +223,10 @@ class MediaEngagementBrowserTest : public InProcessBrowserTest {
     DCHECK(mei_service->contents_observers_.size() == 1);
     return *mei_service->contents_observers_.begin();
   }
+
+  bool injected_clock_ = false;
+
+  std::unique_ptr<base::SimpleTestClock> test_clock_;
 
   net::EmbeddedTestServer http_server_;
   net::EmbeddedTestServer http_server_origin2_;
@@ -431,4 +447,12 @@ IN_PROC_BROWSER_TEST_F(MediaEngagementBrowserTest, MultipleElements) {
   AdvanceMeaningfulPlaybackTime();
   CloseTab();
   ExpectScores(1, 1, 3, 2);
+}
+
+IN_PROC_BROWSER_TEST_F(MediaEngagementBrowserTest,
+                       RecordAudibleBasedOnShortTime) {
+  LoadTestPageAndWaitForPlayAndAudible("engagement_test.html", false);
+  Advance(base::TimeDelta::FromSeconds(4));
+  CloseTab();
+  ExpectScores(1, 0, 1, 0);
 }
