83e34a156b7c401c99474c94371a7dd17da2beba
Greg Thompson <grt@chromium.org>
2017-11-29 20:14:30 +0000

    Don't run inactive user experiments for in-use updates.
    BUG=717091
    
    Change-Id: Ie5f53cabef12dd0f8cd4ac368789bb19f381d10c
    Reviewed-on: https://chromium-review.googlesource.com/795930
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520216}
    Former-commit-id: b973d70e2daa13b59650f87186a4f3cb1e268c14

--- chrome/installer/setup/setup_main.cc
+++ chrome/installer/setup/setup_main.cc
@@ -1437,9 +1437,10 @@ int WINAPI wWinMain(HINSTANCE instance, HINSTANCE prev_instance,
                         &installer_state, &installer_directory);
     DoLegacyCleanups(installer_state, install_status);
 
-    // It may be time to kick off an experiment if this was a successful update.
-    if ((install_status == installer::NEW_VERSION_UPDATED ||
-         install_status == installer::IN_USE_UPDATED) &&
+    // It may be time to kick off an experiment if this was a successful update
+    // and Chrome was not in use (since the experiment only applies to inactive
+    // installs).
+    if (install_status == installer::NEW_VERSION_UPDATED &&
         installer::ShouldRunUserExperiment(installer_state)) {
       installer::BeginUserExperiment(
           installer_state, installer_directory.Append(setup_exe.BaseName()),
