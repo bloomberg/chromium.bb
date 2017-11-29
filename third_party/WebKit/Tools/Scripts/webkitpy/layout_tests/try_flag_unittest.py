b92842310fd1b2cdfee2f23da569fbb973135bab
Steve Kobes <skobes@chromium.org>
2017-11-29 01:45:20 +0000

    try-flag: Intersect unexpected passes with the flag expectation file.
    Previously, try-flag reported all unexpected passes, even if the failure
    expectation came from the main TestExpectations file.  This produced a lot of
    irrelevant noise.
    
    Now, try-flag only reports unexpected passes of tests that have flag-specific
    failure expectations.
    
    The --flag argument is no longer optional.
    
    Bug: 755401
    Change-Id: Ifa507e19159b8cdadf63752f5c8aa2635de014d5
    Reviewed-on: https://chromium-review.googlesource.com/794877
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#519950}
    Former-commit-id: fd3e46e8e7e3be4b07482340b3bb7bc3ddfcb2f5

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/try_flag_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/try_flag_unittest.py
@@ -19,6 +19,11 @@ class TryFlagTest(unittest.TestCase):
         self.linux_build = Build('linux_chromium_rel_ng', 100)
         self.mac_build = Build('mac_chromium_rel_ng', 101)
         self.win_build = Build('win7_chromium_rel_ng', 102)
+        self.mock_try_results = {
+            self.linux_build: TryJobStatus('COMPLETED', 'SUCCESS'),
+            self.win_build: TryJobStatus('COMPLETED', 'SUCCESS'),
+            self.mac_build: TryJobStatus('COMPLETED', 'SUCCESS')
+        }
         super(TryFlagTest, self).__init__(*args, **kwargs)
 
     def _run_trigger_test(self, regenerate):
@@ -115,13 +120,18 @@ class TryFlagTest(unittest.TestCase):
 
     def test_update(self):
         host = MockHost()
-        try_jobs = {
-            self.linux_build: TryJobStatus('COMPLETED', 'SUCCESS'),
-            self.win_build: TryJobStatus('COMPLETED', 'SUCCESS'),
-            self.mac_build: TryJobStatus('COMPLETED', 'SUCCESS')
-        }
+        filesystem = host.filesystem
+        finder = PathFinder(filesystem)
+
+        flag_expectations_file = finder.path_from_layout_tests(
+            'FlagExpectations', 'foo')
+        filesystem.write_text_file(
+            flag_expectations_file,
+            'something/pass-unexpectedly-mac.html [ Fail ]')
+
         self._setup_mock_results(host.buildbot)
-        TryFlag(['update'], host, MockGitCL(host, try_jobs)).run()
+        cmd = ['update', '--flag=--foo']
+        TryFlag(cmd, host, MockGitCL(host, self.mock_try_results)).run()
 
         def results_url(build):
             return '%s/%s/%s/layout-test-results/results.html' % (
@@ -146,8 +156,24 @@ class TryFlagTest(unittest.TestCase):
             ''
         ]))
 
+    def test_update_irrelevant_unexpected_pass(self):
+        host = MockHost()
+        filesystem = host.filesystem
+        finder = PathFinder(filesystem)
+        flag_expectations_file = finder.path_from_layout_tests(
+            'FlagExpectations', 'foo')
+        self._setup_mock_results(host.buildbot)
+        cmd = ['update', '--flag=--foo']
+
+        # Unexpected passes that don't have flag-specific failure expectations
+        # should not be reported.
+        filesystem.write_text_file(flag_expectations_file, '')
+        TryFlag(cmd, host, MockGitCL(host, self.mock_try_results)).run()
+        self.assertTrue('### 0 unexpected passes' in host.stdout.getvalue())
+
     def test_invalid_action(self):
         host = MockHost()
-        TryFlag(['invalid'], host, MockGitCL(host)).run()
+        cmd = ['invalid', '--flag=--foo']
+        TryFlag(cmd, host, MockGitCL(host)).run()
         self.assertEqual(host.stderr.getvalue(),
                          'specify "trigger" or "update"\n')
