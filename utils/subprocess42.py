b3b5162f8b8005bf556a9656fc6d2154cf34a1da
Marc-Antoine Ruel <maruel@chromium.org>
2017-11-29 19:00:21 -0500

    subprocess42: be more tolerant to unicode.
    Make it automatically encode unicode strings to str as UTF-8. This simplifies
    everyone's life. Asserting that all call sites are using str would be painful
    and labour intensive so it's not worth.
    
    R=vadimsh@chromium.org
    Bug: 787531
    Change-Id: Ib683dbccad98d4d57f1ebd5bd627d4b1874062b2
    Reviewed-on: https://chromium-review.googlesource.com/798150
    Commit-Queue: Marc-Antoine Ruel <maruel@chromium.org>
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    
    Cr-Mirrored-From: https://chromium.googlesource.com/infra/luci/luci-py
    Cr-Mirrored-Commit: dcf287d892186f4adcc1d0e5787cc451f881c1b3

--- utils/subprocess42.py
+++ utils/subprocess42.py
@@ -213,7 +213,12 @@ class Popen(subprocess.Popen):
   Unlike subprocess, yield_any(), recv_*(), communicate() will close stdout and
   stderr once the child process closes them, after all the data is read.
 
-  Arguments:
+  Mutated behavior:
+  - args: transparently encode('utf-8') any unicode items.
+  - cwd: transparently encode('utf-8') if unicode.
+  - env: transparently encode('utf-8') any unicode keys or values.
+
+  Additional arguments:
   - detached: If True, the process is created in a new process group. On
     Windows, use CREATE_NEW_PROCESS_GROUP. On posix, use os.setpgid(0, 0).
 
@@ -240,6 +245,19 @@ class Popen(subprocess.Popen):
   def __init__(self, args, **kwargs):
     assert 'creationflags' not in kwargs
     assert 'preexec_fn' not in kwargs, 'Use detached=True instead'
+
+    # Windows version of subprocess.Popen() really doens't like unicode. In
+    # practice we should use the current ANSI code page, but settle for utf-8
+    # across all OSes for consistency.
+    to_str = lambda i: i if isinstance(i, str) else i.encode('utf-8')
+    args = [to_str(i) for i in args]
+    if kwargs.get('cwd') is not None:
+      kwargs['cwd'] = to_str(kwargs['cwd'])
+    if kwargs.get('env'):
+      kwargs['env'] = {
+        to_str(k): to_str(v) for k, v in kwargs['env'].iteritems()
+      }
+
     self.start = time.time()
     self.end = None
     self.gid = None
