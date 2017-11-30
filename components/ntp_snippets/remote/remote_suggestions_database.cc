108acf653c81d034c11ae7eb24112dbf1c014842
Jan Krcal <jkrcal@chromium.org>
2017-11-30 16:29:52 +0000

    [Remote suggestions] Reduce database memory use for low end devices
    This CL changes the buffer size for leveldb database for remote
    suggestions. This smaller buffer size is used only for devices with
    memory pressure. The change saves up to 500 KB of memory.
    
    Similarly as the previous buffer size of 512 KB, the new buffer size
    of 128 KB was chosen arbitrarily (the largest size that still shows
    considerable memory savings when tracing the process).
    
    The CL also makes small changes to avoid empty calls to the database.
    
    Bug: 716170
    Change-Id: Iccb398d50b95d8d1441ceb4bb731cb1d26b0fe20
    Reviewed-on: https://chromium-review.googlesource.com/800271
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520567}
    Former-commit-id: 1fa06605790c5c914f474346417f7eaa8ee23884

--- components/ntp_snippets/remote/remote_suggestions_database.cc
+++ components/ntp_snippets/remote/remote_suggestions_database.cc
@@ -8,6 +8,7 @@
 
 #include "base/files/file_path.h"
 #include "base/memory/ptr_util.h"
+#include "base/sys_info.h"
 #include "base/task_scheduler/post_task.h"
 #include "components/leveldb_proto/proto_database_impl.h"
 #include "components/ntp_snippets/remote/proto/ntp_snippets.pb.h"
@@ -27,6 +28,7 @@ const char kSnippetDatabaseFolder[] = "snippets";
 const char kImageDatabaseFolder[] = "images";
 
 const size_t kDatabaseWriteBufferSizeBytes = 512 << 10;
+const size_t kDatabaseWriteBufferSizeBytesForLowEndDevice = 128 << 10;
 }  // namespace
 
 namespace ntp_snippets {
@@ -58,7 +60,11 @@ RemoteSuggestionsDatabase::RemoteSuggestionsDatabase(
       weak_ptr_factory_(this) {
   base::FilePath snippet_dir = database_dir.AppendASCII(kSnippetDatabaseFolder);
   leveldb_env::Options options = leveldb_proto::CreateSimpleOptions();
-  options.write_buffer_size = kDatabaseWriteBufferSizeBytes;
+  if (base::SysInfo::IsLowEndDevice()) {
+    options.write_buffer_size = kDatabaseWriteBufferSizeBytesForLowEndDevice;
+  } else {
+    options.write_buffer_size = kDatabaseWriteBufferSizeBytes;
+  }
   database_->Init(kDatabaseUMAClientName, snippet_dir, options,
                   base::Bind(&RemoteSuggestionsDatabase::OnDatabaseInited,
                              weak_ptr_factory_.GetWeakPtr()));
@@ -343,6 +349,8 @@ void RemoteSuggestionsDatabase::DeleteUnreferencedImages(
       keys_to_remove->emplace_back(key);
     }
   }
+  if (keys_to_remove->empty())
+    return;
   DeleteImages(std::move(keys_to_remove));
 }
 
