aa1873ffafe61360d26381ec318bc5787980a505
Martin Sramek <msramek@chromium.org>
2017-11-30 17:21:40 +0000

    Clean up the old Desktop Incognito NTP
    1. Remove the code of the old ("non-MD") Incognito NTP
    2. Remove the qualifier "MD" from variable names (no longer necessary,
       as the "non-MD" version is removed)
    3. Remove the qualifier "MD" from file names (as above; and thus
       overwrite the previous files).
    
    Note: #3 was executed as:
    > mv md_incognito_tab.css incognito_tab.css
    > mv md_incognito_tabsjs incognito_tab.js
    > mv md_incognito_tabshtml incognito_tab.html
    And then inside md_incognito_tabs.html:
    > s/md_incognito_tab/incognito_tab/
    Although git understood this as delete+rename, the diffs in the code
    compare the old files with the new ones, so it's not as well visible :(
    
    Bug: 693525
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I8e4096337dff097aaf400f15b2c2b7b8e1c9c475
    Reviewed-on: https://chromium-review.googlesource.com/777319
    Commit-Queue: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520584}
    Former-commit-id: b3cb619de0b9c1e7808ab9dac7104b6b4b99f024

--- chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
+++ chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
@@ -6,7 +6,6 @@
 
 #include <string>
 
-#include "base/feature_list.h"
 #include "base/memory/ref_counted_memory.h"
 #include "base/strings/string16.h"
 #include "base/strings/string_number_conversions.h"
@@ -25,7 +24,6 @@
 #include "chrome/browser/ui/bookmarks/bookmark_bar_constants.h"
 #include "chrome/browser/ui/webui/app_launcher_login_handler.h"
 #include "chrome/browser/ui/webui/ntp/app_launcher_handler.h"
-#include "chrome/common/chrome_features.h"
 #include "chrome/common/features.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
@@ -247,9 +245,6 @@ void NTPResourceCache::Invalidate() {
 }
 
 void NTPResourceCache::CreateNewTabIncognitoHTML() {
-  const bool is_md_incognito_ntp_enabled =
-      base::FeatureList::IsEnabled(features::kMaterialDesignIncognitoNTP);
-
   ui::TemplateReplacements replacements;
   // Note: there's specific rules in CSS that look for this attribute's content
   // being equal to "true" as a string.
@@ -258,27 +253,16 @@ void NTPResourceCache::CreateNewTabIncognitoHTML() {
           ? "true"
           : "false";
 
-  if (is_md_incognito_ntp_enabled) {
-    replacements["incognitoTabDescription"] =
-        l10n_util::GetStringUTF8(IDS_NEW_TAB_OTR_SUBTITLE);
-    replacements["incognitoTabHeading"] =
-        l10n_util::GetStringUTF8(IDS_NEW_TAB_OTR_TITLE);
-    replacements["incognitoTabWarning"] =
-        l10n_util::GetStringUTF8(IDS_NEW_TAB_OTR_VISIBLE);
-    replacements["learnMore"] =
-        l10n_util::GetStringUTF8(IDS_NEW_TAB_OTR_LEARN_MORE_LINK);
-    replacements["incognitoTabFeatures"] =
-        l10n_util::GetStringUTF8(IDS_NEW_TAB_OTR_NOT_SAVED);
-  } else {
-    replacements["incognitoTabDescription"] =
-        l10n_util::GetStringUTF8(IDS_NEW_TAB_OTR_DESCRIPTION);
-    replacements["incognitoTabHeading"] =
-        l10n_util::GetStringUTF8(IDS_NEW_TAB_OTR_HEADING);
-    replacements["incognitoTabWarning"] =
-        l10n_util::GetStringUTF8(IDS_NEW_TAB_OTR_MESSAGE_WARNING);
-    replacements["learnMore"] =
-        l10n_util::GetStringUTF8(IDS_NEW_TAB_OTR_LEARN_MORE_LINK);
-  }
+  replacements["incognitoTabDescription"] =
+      l10n_util::GetStringUTF8(IDS_NEW_TAB_OTR_SUBTITLE);
+  replacements["incognitoTabHeading"] =
+      l10n_util::GetStringUTF8(IDS_NEW_TAB_OTR_TITLE);
+  replacements["incognitoTabWarning"] =
+      l10n_util::GetStringUTF8(IDS_NEW_TAB_OTR_VISIBLE);
+  replacements["learnMore"] =
+      l10n_util::GetStringUTF8(IDS_NEW_TAB_OTR_LEARN_MORE_LINK);
+  replacements["incognitoTabFeatures"] =
+      l10n_util::GetStringUTF8(IDS_NEW_TAB_OTR_NOT_SAVED);
   replacements["learnMoreLink"] = kLearnMoreIncognitoUrl;
   replacements["title"] = l10n_util::GetStringUTF8(IDS_NEW_TAB_TITLE);
 
@@ -292,8 +276,7 @@ void NTPResourceCache::CreateNewTabIncognitoHTML() {
 
   static const base::StringPiece incognito_tab_html(
       ui::ResourceBundle::GetSharedInstance().GetRawDataResource(
-          is_md_incognito_ntp_enabled ? IDR_MD_INCOGNITO_TAB_HTML
-                                      : IDR_INCOGNITO_TAB_HTML));
+          IDR_INCOGNITO_TAB_HTML));
 
   std::string full_html =
       ui::ReplaceTemplateExpressions(incognito_tab_html, replacements);
