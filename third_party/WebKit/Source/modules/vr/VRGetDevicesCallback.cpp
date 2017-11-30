fd3d704a3926146fe1cb8f10e617764ea8e5983d
Bill Orr <billorr@chromium.org>
2017-11-30 01:11:37 +0000

    Add metric(s) to track vrdisplays result
    This change adds a new VRGetDisplaysSupportsPresent event to report
    when VRGetDisplays returns a display capable of presentation.
    
    BUG=787951
    
    Change-Id: I0b27de1eac43164aa0caa75f127183c8bcc4b6a5
    Reviewed-on: https://chromium-review.googlesource.com/795119
    Commit-Queue: Bill Orr <billorr@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520365}
    Former-commit-id: 841ae3ae2fe9b35c68948fb2597f26d3764d0a6a

--- third_party/WebKit/Source/modules/vr/VRGetDevicesCallback.cpp
+++ third_party/WebKit/Source/modules/vr/VRGetDevicesCallback.cpp
@@ -14,6 +14,20 @@ VRGetDevicesCallback::VRGetDevicesCallback(ScriptPromiseResolver* resolver)
 VRGetDevicesCallback::~VRGetDevicesCallback() {}
 
 void VRGetDevicesCallback::OnSuccess(VRDisplayVector displays) {
+  bool display_supports_presentation = false;
+  for (auto display : displays) {
+    if (display->capabilities()->canPresent()) {
+      display_supports_presentation = true;
+    }
+  }
+
+  if (display_supports_presentation) {
+    ExecutionContext* execution_context =
+        ExecutionContext::From(resolver_->GetScriptState());
+    UseCounter::Count(execution_context,
+                      WebFeature::kVRGetDisplaysSupportsPresent);
+  }
+
   resolver_->Resolve(displays);
 }
 
