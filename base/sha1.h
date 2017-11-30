295b40550255b981bc7b9dff2e10a652f1ab882d
Henrik Grunell <grunell@chromium.org>
2017-11-30 11:56:44 +0000

    Revert "base::SHA1HashString should take a StringPiece rather than a std::string"
    This reverts commit 8c5f13c8ef66221d95c12c9ea0a293ae425a8b7f [formerly bb2075717cee645d820129b4e5ef7872dfd22661].
    
    Reason for revert: Breaks the Mac installer build. Seen first in https://build.chromium.org/p/chromium.webrtc.fyi/builders/Mac%20Builder%20%28dbg%29/builds/3358
    
    Looks like that target is not built on other bots.
    
    -----
    [140/15026] LINK ./goobsdiff
    FAILED: goobsdiff 
    export DEVELOPER_DIR=/b/c/b/Mac_Builder__dbg_/src/build/mac_files/Xcode.app;  TOOL_VERSION=1511400028 ../../build/toolchain/mac/linker_driver.py ../../third_party/llvm-build/Release+Asserts/bin/clang++  -stdlib=libc++ -arch x86_64 -segprot PROTECTED_MEMORY rw r -isysroot ../../build/mac_files/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.12.sdk -mmacosx-version-min=10.9.0 -Wl,-ObjC -Wl,-rpath,@loader_path/. -Wl,-rpath,@loader_path/../../.. -Werror -o "./goobsdiff" -Wl,-filelist,"./goobsdiff.rsp"  -lbz2 -lz
    Undefined symbols for architecture x86_64:
      "base::BasicStringPiece<std::__1::basic_string<char, std::__1::char_traits<char>, std::__1::allocator<char> > >::data() const", referenced from:
          base::SHA1HashString(base::BasicStringPiece<std::__1::basic_string<char, std::__1::char_traits<char>, std::__1::allocator<char> > >) in libgoobsdiff_sha1_adapter.a(sha1.o)
      "base::BasicStringPiece<std::__1::basic_string<char, std::__1::char_traits<char>, std::__1::allocator<char> > >::length() const", referenced from:
          base::SHA1HashString(base::BasicStringPiece<std::__1::basic_string<char, std::__1::char_traits<char>, std::__1::allocator<char> > >) in libgoobsdiff_sha1_adapter.a(sha1.o)
    ld: symbol(s) not found for architecture x86_64
    clang: error: linker command failed with exit code 1 (use -v to see invocation)
    Traceback (most recent call last):
      File "../../build/toolchain/mac/linker_driver.py", line 229, in <module>
        Main(sys.argv)
      File "../../build/toolchain/mac/linker_driver.py", line 79, in Main
        subprocess.check_call(compiler_driver_args)
      File "/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/subprocess.py", line 540, in check_call
        raise CalledProcessError(retcode, cmd)
    subprocess.CalledProcessError: Command '['../../third_party/llvm-build/Release+Asserts/bin/clang++', '-stdlib=libc++', '-arch', 'x86_64', '-segprot', 'PROTECTED_MEMORY', 'rw', 'r', '-isysroot', '../../build/mac_files/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.12.sdk', '-mmacosx-version-min=10.9.0', '-Wl,-ObjC', '-Wl,-rpath,@loader_path/.', '-Wl,-rpath,@loader_path/../../..', '-Werror', '-o', './goobsdiff', '-Wl,-filelist,./goobsdiff.rsp', '-lbz2', '-lz']' returned non-zero exit status 1
    -----
    
    Original change's description:
    > base::SHA1HashString should take a StringPiece rather than a std::string
    > 
    > Change-Id: I6fba1ccacfe9bc9b3ba3cc84b3157876c97c88b7
    > Reviewed-on: https://chromium-review.googlesource.com/798065
    > Commit-Queue: Matt Mueller <mattm@chromium.org>
    > Reviewed-by: Mark Mentovai <mark@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#520331}
    
    TBR=mattm@chromium.org,mark@chromium.org
    
    Change-Id: I4d87fd1d27297d46b36da432534f40e427d35cd2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/800210
    Reviewed-by: Henrik Grunell <grunell@chromium.org>
    Commit-Queue: Henrik Grunell <grunell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520514}
    Former-commit-id: fc4ffc7c1c6e1e7328f52af553d5f2072bee36a9

--- base/sha1.h
+++ base/sha1.h
@@ -7,8 +7,9 @@
 
 #include <stddef.h>
 
+#include <string>
+
 #include "base/base_export.h"
-#include "base/strings/string_piece.h"
 
 namespace base {
 
@@ -18,7 +19,7 @@ static const size_t kSHA1Length = 20;  // Length in bytes of a SHA-1 hash.
 
 // Computes the SHA-1 hash of the input string |str| and returns the full
 // hash.
-BASE_EXPORT std::string SHA1HashString(base::StringPiece str);
+BASE_EXPORT std::string SHA1HashString(const std::string& str);
 
 // Computes the SHA-1 hash of the |len| bytes in |data| and puts the hash
 // in |hash|. |hash| must be kSHA1Length bytes long.
