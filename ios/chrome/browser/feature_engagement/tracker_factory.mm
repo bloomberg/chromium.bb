fa7a520ba983d45c884c394082d890e52b19ab36
Gregory Chatzinoff <gchatz@chromium.org>
2017-11-30 02:13:04 +0000

    Add initial EG tests for Badged Reading List.
    Bug: 780984
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I66ab9b801a6dbe34ea70eb5866f5d323d89d9299
    Reviewed-on: https://chromium-review.googlesource.com/778102
    Reviewed-by: Mike Baxley <baxley@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: Gregory Chatzinoff <gchatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520390}
    Former-commit-id: 4e14eca63472e9785f405c842042359fed7ca481

--- ios/chrome/browser/feature_engagement/tracker_factory.mm
+++ ios/chrome/browser/feature_engagement/tracker_factory.mm
@@ -4,27 +4,16 @@
 
 #include "ios/chrome/browser/feature_engagement/tracker_factory.h"
 
-#include "base/memory/ptr_util.h"
-#include "base/memory/ref_counted.h"
 #include "base/memory/singleton.h"
-#include "base/sequenced_task_runner.h"
-#include "base/task_scheduler/post_task.h"
 #include "components/feature_engagement/public/tracker.h"
 #include "components/keyed_service/ios/browser_state_dependency_manager.h"
 #include "ios/chrome/browser/browser_state/browser_state_otr_helper.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
-#include "ios/chrome/browser/chrome_constants.h"
+#include "ios/chrome/browser/feature_engagement/tracker_factory_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
-
-namespace {
-
-const base::FilePath::CharType kIOSFeatureEngagementTrackerStorageDirname[] =
-    FILE_PATH_LITERAL("Feature Engagement Tracker");
-
-}  // namespace
 
 namespace feature_engagement {
 
@@ -49,18 +38,7 @@ TrackerFactory::~TrackerFactory() = default;
 
 std::unique_ptr<KeyedService> TrackerFactory::BuildServiceInstanceFor(
     web::BrowserState* context) const {
-  ios::ChromeBrowserState* browser_state =
-      ios::ChromeBrowserState::FromBrowserState(context);
-
-  scoped_refptr<base::SequencedTaskRunner> background_task_runner =
-      base::CreateSequencedTaskRunnerWithTraits(
-          {base::MayBlock(), base::TaskPriority::BACKGROUND});
-
-  base::FilePath storage_dir = browser_state->GetStatePath().Append(
-      kIOSFeatureEngagementTrackerStorageDirname);
-
-  return base::WrapUnique(
-      feature_engagement::Tracker::Create(storage_dir, background_task_runner));
+  return CreateFeatureEngagementTracker(context);
 }
 
 // Finds which browser state to use. If |context| is an incognito browser
