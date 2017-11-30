d15778a8e02992a10d4a76e8a526c02c406af411
Becca Hughes <beccahughes@chromium.org>
2017-11-30 11:25:21 +0000

    Feature Policy: Enable "autoplay" feature
    Add an "autoplay" feature that controls autoplay and delegate
    activation state through frames.
    
    BUG=788390
    
    Change-Id: Ifeee15cf526d8a27e1b73fa62784e860c73af1ee
    Reviewed-on: https://chromium-review.googlesource.com/788858
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520507}
    Former-commit-id: 6b3eec088008a5e8bb516acf07d71f4924580720

---
+++ third_party/WebKit/LayoutTests/http/tests/media/autoplay/resources/autoplay-test.js
@@ -0,0 +1,88 @@
+/**
+ * Tests autoplay on a page and its subframes.
+ */
+
+// Tracks the results and how many active tests we have running.
+let activeTests;
+let testExpectations = {};
+let callback;
+
+function tearDown(result) {
+  // Reset the flag state.
+  window.internals.settings.setAutoplayPolicy('no-user-gesture-required');
+
+  receivedResult({
+    url: window.location.href,
+    message: result == undefined
+  });
+}
+
+function receivedResult(data) {
+  // Forward the result to the top frame.
+  if (!callback) {
+    top.postMessage(data, '*');
+    return;
+  }
+
+  activeTests--;
+  assert_equals(testExpectations[data.url], data.message);
+
+  if (!activeTests)
+    callback();
+}
+
+function runVideoTest() {
+  const video = document.createElement('video');
+  video.src = findMediaFile('video', '/media-resources/content/test');
+  video.play().then(tearDown, tearDown);
+}
+
+function simulateViewportClick() {
+  chrome.gpuBenchmarking.pointerActionSequence([
+      {"source": "mouse",
+       "actions": [
+       { "name": "pointerDown", "x": 0, "y": 0 },
+       { "name": "pointerUp" } ]}]);
+}
+
+function simulateFrameClick() {
+  const frame = document.getElementsByTagName('iframe')[0];
+  const rect = frame.getBoundingClientRect();
+
+  chrome.gpuBenchmarking.pointerActionSequence([
+      {"source": "mouse",
+       "actions": [
+       {
+         "name": "pointerDown",
+         "x": rect.left + (rect.width / 2),
+         "y": rect.top + (rect.height / 2)
+       },
+       { "name": "pointerUp" } ]}]);
+}
+
+function runTest(expectations) {
+  async_test((t) => {
+    testExpectations = expectations;
+    activeTests = Object.keys(expectations).length;
+    callback = t.step_func_done();
+  });
+}
+
+window.addEventListener('load', () => {
+  if (!window.testRunner)
+    return;
+
+  // Setup the flags before the test is run.
+  window.internals.settings.setAutoplayPolicy('document-user-activation-required');
+
+  // Setup the event listener to forward messages.
+  window.addEventListener('message', (e) => {
+    if (callback) {
+      receivedResult(e.data);
+    } else {
+      top.postMessage(e.data, '*');
+    }
+  });
+
+  runVideoTest();
+}, { once: true });
