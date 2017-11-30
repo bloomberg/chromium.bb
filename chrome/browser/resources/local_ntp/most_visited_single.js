57d86a68309027daff4a9d8ab04fe71af69e8ea2
Marc Treib <treib@chromium.org>
2017-11-30 13:14:53 +0000

    Most Visited iframe: Set theme info via a css class and variable
    instead of manually constructing a css string.
    
    Bug: none
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I5f861efe80a87bae39c4116c2b798b04b462d4eb
    Reviewed-on: https://chromium-review.googlesource.com/796418
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Chris Pickel <sfiera@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520526}
    Former-commit-id: 03d083a82b9174dfe11ef90d000eaad2c7ccdcf6

--- chrome/browser/resources/local_ntp/most_visited_single.js
+++ chrome/browser/resources/local_ntp/most_visited_single.js
@@ -223,41 +223,8 @@ var showTiles = function(info) {
  * @param {object} info Data received in the message.
  */
 var updateTheme = function(info) {
-  var themeStyle = [];
-
-  if (info.isThemeDark) {
-    themeStyle.push(
-        '.mv-tile, .mv-empty-tile { ' +
-        'background: rgb(51,51,51); }');
-    themeStyle.push(
-        '.mv-thumb.failed-img { ' +
-        'background-color: #555; }');
-    themeStyle.push(
-        '.mv-thumb.failed-img::after { ' +
-        'border-color: #333; }');
-    themeStyle.push(
-        '.mv-x { ' +
-        'background: linear-gradient(to left, ' +
-        'rgb(51,51,51) 60%, transparent); }');
-    themeStyle.push(
-        'html[dir=rtl] .mv-x { ' +
-        'background: linear-gradient(to right, ' +
-        'rgb(51,51,51) 60%, transparent); }');
-    themeStyle.push(
-        '.mv-x::after { ' +
-        'background-color: rgba(255,255,255,0.7); }');
-    themeStyle.push(
-        '.mv-x:hover::after { ' +
-        'background-color: #fff; }');
-    themeStyle.push(
-        '.mv-x:active::after { ' +
-        'background-color: rgba(255,255,255,0.5); }');
-  }
-  if (info.tileTitleColor) {
-    themeStyle.push('body { color: ' + info.tileTitleColor + '; }');
-  }
-
-  document.querySelector('#custom-theme').textContent = themeStyle.join('\n');
+  document.body.style.setProperty('--tile-title-color', info.tileTitleColor);
+  document.body.classList.toggle('dark-theme', info.isThemeDark);
 };
 
 
