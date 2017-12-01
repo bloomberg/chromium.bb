2b1c193f01801c5dc1fdc5e7115a771dd85dc565
Matthew Cary <mattcary@chromium.org>
2017-12-01 17:19:42 +0000

    CCT: enable background tab feature by default.
    The finch experiment has been running as a kill-switch for a quarter,
    which is not the right way to run this. We should just enable the feature,
    and if it needs to be killed only at that point push out an experiment.
    
    Bug: 710720
    Change-Id: Id9b0ae2d0d66e8082a98346921a51838e3c6a381
    Reviewed-on: https://chromium-review.googlesource.com/803815
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Commit-Queue: Matthew Cary <mattcary@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520970}
    Former-commit-id: 29de08401a3679af8a5156396e5237c24792ee3b

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -170,7 +170,7 @@ const base::Feature kAndroidSigninPromos{"AndroidSigninPromos",
                                          base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kCCTBackgroundTab{"CCTBackgroundTab",
-                                      base::FEATURE_DISABLED_BY_DEFAULT};
+                                      base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kCCTExternalLinkHandling{"CCTExternalLinkHandling",
                                              base::FEATURE_ENABLED_BY_DEFAULT};
