9f76d0f620036cc45b309a6694030062a9318371
Scott Violet <sky@chromium.org>
2017-12-01 07:04:52 +0000

    chromeos: update content to deal with ui-service not implying viz
    When the ui-service is running without viz we need to run some of the
    classic behavior (around creating frame ids). This patch updates a
    handful of sites. This gets the login case working, but pdfs (webview
    guest) still don't work. Will continue looking at it.
    
    BUG=786453
    TEST=none
    
    Change-Id: I0ce2934a667195608bcecb0c00a0f181f249bd74
    Reviewed-on: https://chromium-review.googlesource.com/802182
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520880}
    Former-commit-id: 30daff6502d23b98a544c110e598f42c477a390c

--- content/browser/browser_plugin/browser_plugin_guest.cc
+++ content/browser/browser_plugin/browser_plugin_guest.cc
@@ -54,6 +54,7 @@
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/common/drop_data.h"
+#include "ui/base/ui_base_switches_util.h"
 #include "ui/events/blink/web_input_event_traits.h"
 #include "ui/gfx/geometry/size_conversions.h"
 
@@ -436,7 +437,7 @@ void BrowserPluginGuest::SetChildFrameSurface(
     const viz::SurfaceInfo& surface_info,
     const viz::SurfaceSequence& sequence) {
   has_attached_since_surface_set_ = false;
-  if (!IsUsingMus()) {
+  if (!switches::IsMusHostingViz()) {
     SendMessageToEmbedder(
         std::make_unique<BrowserPluginMsg_SetChildFrameSurface>(
             browser_plugin_instance_id(), surface_info, sequence));
@@ -703,7 +704,7 @@ void BrowserPluginGuest::RenderViewReady() {
   // In case we've created a new guest render process after a crash, let the
   // associated BrowserPlugin know. We only need to send this if we're attached,
   // as guest_crashed_ is cleared automatically on attach anyways.
-  if (attached() && !IsUsingMus()) {
+  if (attached() && !switches::IsMusHostingViz()) {
     RenderWidgetHostViewGuest* rwhv = static_cast<RenderWidgetHostViewGuest*>(
         web_contents()->GetRenderWidgetHostView());
     if (rwhv) {
