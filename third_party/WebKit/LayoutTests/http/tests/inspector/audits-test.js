a6f557caa48e8788ecf2f9de68f0735698e280b3
Will Chen <chenwilliam@chromium.org>
2017-12-01 22:54:41 +0000

    DevTools: remove legacy audits & extension audits
    Now that lighthouse has succeeded our legacy audits panel, it's time to
    remove audits, the extension audits API, extension audits-related
    docs, and relevant tests.
    
    Bug: 790541
    Change-Id: Ie2cf3fb439dab768a8511b151d81d6066b41a3a9
    Reviewed-on: https://chromium-review.googlesource.com/798633
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521101}
    Former-commit-id: ebdba3f23171157c8d2ce3bcd1072b596fe2a03b

--- third_party/WebKit/LayoutTests/http/tests/inspector/audits-test.js
+++
@@ -1,4 +0,0 @@
-function initialize_AuditTests() {
-    InspectorTest.preloadModule('audits_test_runner');
-    InspectorTest.preloadPanel("audits");
-}
--- third_party/WebKit/LayoutTests/http/tests/inspector/extensions/extensions-audits-tests.js
+++
@@ -1,8 +0,0 @@
-var initialize_ExtensionsAuditsTest = function()
-{
-    InspectorTest.preloadModule('extensions_test_runner');
-
-    // We will render DOM node results, so preload elements.
-    InspectorTest.preloadPanel("elements");
-    InspectorTest.preloadPanel("audits");
-}
