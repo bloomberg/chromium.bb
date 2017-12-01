809cf30c26bb33e821abdb740823a93c639b6ad9
Xiaocheng Hu <xiaochengh@chromium.org>
2017-12-01 23:20:40 +0000

    [LayoutNG] Fix NGOffsetMapping::GetFor() for positions anchored to inline blocks
    NGOffsetMapping::GetFor() used to return the wrong mapping for positions before
    or after an inline block. Example:
    
    <div><span style='display: inline-block'>foo</span></div>
    
    When taking SPAN@BeforeAnchor, GetFor() returns the offset mapping of the SPAN,
    but the correct result should be that of the DIV since the position is outside
    of the SPAN. The root cause is that EnclosingNGBlockFlow() on SPAN returns the
    layout object of SPAN itself, since it is a LayoutNGBlockFlow by itself.
    
    This patch fixes the issue by returning EnclosingNGBlockFlow() of its parent
    instead when the given position is before/after an atomic inline.
    
    Bug: 699017
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Idf2cdda0fd9c5ed533e5d516fe459374e0383b29
    Reviewed-on: https://chromium-review.googlesource.com/802628
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521110}
    Former-commit-id: bcd58929f5dd0276306e9fdd308a5c9b850fe880

--- third_party/WebKit/Source/core/layout/ng/inline/ng_offset_mapping_test.cc
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_offset_mapping_test.cc
@@ -740,4 +740,32 @@ TEST_P(ParameterizedNGOffsetMappingTest, Table) {
   TEST_RANGE(result.GetRanges(), foo_node, 0u, 3u);
 }
 
+TEST_P(ParameterizedNGOffsetMappingTest, GetMappingForInlineBlock) {
+  SetupHtml("t",
+            "<div id=t>foo"
+            "<span style='display: inline-block' id=span> bar </span>"
+            "baz</div>");
+
+  const Element* div = GetElementById("t");
+  const Element* span = GetElementById("span");
+
+  const NGOffsetMapping* div_mapping =
+      NGOffsetMapping::GetFor(Position(div->firstChild(), 0));
+  const NGOffsetMapping* span_mapping =
+      NGOffsetMapping::GetFor(Position(span->firstChild(), 0));
+
+  // NGOffsetMapping::GetFor for Before/AfterAnchor of an inline block should
+  // return the mapping of the containing block, not of the inline block itself.
+
+  const NGOffsetMapping* span_before_mapping =
+      NGOffsetMapping::GetFor(Position::BeforeNode(*span));
+  EXPECT_EQ(div_mapping, span_before_mapping);
+  EXPECT_NE(span_mapping, span_before_mapping);
+
+  const NGOffsetMapping* span_after_mapping =
+      NGOffsetMapping::GetFor(Position::AfterNode(*span));
+  EXPECT_EQ(div_mapping, span_after_mapping);
+  EXPECT_NE(span_mapping, span_after_mapping);
+}
+
 }  // namespace blink
