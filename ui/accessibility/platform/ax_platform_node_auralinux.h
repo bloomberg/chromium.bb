e8823fe06a4cabfc1177244321a1c77899e789f9
Jose Dapena Paz <jose.dapena@lge.com>
2017-12-01 15:49:36 +0100

    A11y: add ATK_STATE_FOCUSED support.
    Map AX_EVENT_FOCUS to state updates of ATK_STATE_FOCUSED. As Blur events
    are expected to be removed, this change adds a global static variable
    pointing to current focus. This is needed to be able to emit setting
    ATK_STATE_FOCUSED to false for the node that becomes unfocused.
    
    Bug: 791645
    Change-Id: If1c675279136e66bfc2b489552f505f4a0a38fe7
    Reviewed-on: https://chromium-review.googlesource.com/806039
    Commit-Queue: José Dapena Paz <jose.dapena@lge.com>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525721}
    Former-commit-id: 5c59ff982aa87c90415a34204b8d7e013b246e05

--- ui/accessibility/platform/ax_platform_node_auralinux.h
+++ ui/accessibility/platform/ax_platform_node_auralinux.h
@@ -72,6 +72,9 @@ class AXPlatformNodeAuraLinux : public AXPlatformNodeBase {
   // Misc helpers
   void GetFloatAttributeInGValue(AXFloatAttribute attr, GValue* value);
 
+  // Event helpers
+  void OnFocused();
+
   // AXPlatformNode overrides.
   gfx::NativeViewAccessible GetNativeViewAccessible() override;
   void NotifyAccessibilityEvent(ui::AXEvent event_type) override;
@@ -111,6 +114,10 @@ class AXPlatformNodeAuraLinux : public AXPlatformNodeBase {
   // The root-level Application object that's the parent of all
   // top-level windows.
   static AXPlatformNode* application_;
+
+  // The last AtkObject with keyboard focus. Tracking this is required
+  // to emit the ATK_STATE_FOCUSED change to false.
+  static AtkObject* current_focused_;
 
   DISALLOW_COPY_AND_ASSIGN(AXPlatformNodeAuraLinux);
 };
