5c36ddcd97e9a4a0358fe4b24b8c97c87d0a06e5
Matthew Cary <mattcary@chromium.org>
2017-12-01 15:11:06 +0000

    Disable flaky ClipboardTest.testCopyDocumentFragment
    Bug: 791021
    Change-Id: Ibd01847b97922ea1527de149be7086c86db1b641
    TBR: jinsukkim@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/803439
    Reviewed-by: Matthew Cary <mattcary@chromium.org>
    Commit-Queue: Matthew Cary <mattcary@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520940}
    Former-commit-id: 16a18e19f015d1371938380f5ea1428c1038f89e

--- content/public/android/javatests/src/org/chromium/content/browser/ClipboardTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ClipboardTest.java
@@ -18,6 +18,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.content.browser.test.util.Criteria;
@@ -60,6 +61,7 @@ public class ClipboardTest {
     @LargeTest
     @Feature({"Clipboard", "TextInput"})
     @RerunWithUpdatedContainerView
+    @DisabledTest(message = "https://crbug.com/791021")
     public void testCopyDocumentFragment() throws Throwable {
         ClipboardManager clipboardManager =
                 ThreadUtils.runOnUiThreadBlockingNoException(new Callable<ClipboardManager>() {
