341f0e99872fe9f5a8ef85f72be05a126768ca91
Avi Drissman <avi@chromium.org>
2017-12-01 14:57:39 +0000

    Revert "Introduce RtcEventLogManager"
    This reverts commit bfce571513bf5fc95758516c79c2beb9a47cad7b [formerly 437fa566ed89d7eafc803ae1c304e5e1f34040fb].
    
    Reason for revert:
    
    WebRtcEventLogManagerTest.LocalLogMayNotBeStartedTwice fails across multiple bots (https://ci.chromium.org/buildbot/chromium.linux/Linux%20Tests/65151) (https://ci.chromium.org/buildbot/chromium.memory/Linux%20TSan%20Tests/14934) with this change.
    
    Original change's description:
    > Introduce RtcEventLogManager
    > 
    > RtcEventLogManager will manage both:
    > 1. Writing RTC event logs to local files, based on user input via
    > WebRTCInternals. (Current CL.)
    > 2. Uploading RTC event logs to remote servers, based on JS-triggered
    > configuration. (Upcoming CLs.)
    > 
    > The latter of the two is the use case which motivated the creation of
    > RtcEventLogManager, since we need this manager to outlive the call. We
    > also want RtcEventLogManager to run in new Chrome instances, even if
    > now WebRTC session exists, so that it may upload still-pending logs
    > from previous sessions. (This functionality will be added in upcoming
    > CLs.)
    > 
    > Bug: 775415, 780863
    > Change-Id: I7504857ebf662fd914f5988ecbc1fae051f12275
    > Reviewed-on: https://chromium-review.googlesource.com/760816
    > Commit-Queue: Elad Alon <eladalon@chromium.org>
    > Reviewed-by: Chris Palmer <palmer@chromium.org>
    > Reviewed-by: Tommi <tommi@chromium.org>
    > Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    > Reviewed-by: Camille Lamy <clamy@chromium.org>
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Reviewed-by: Henrik Boström <hbos@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#520926}
    
    TBR=palmer@chromium.org,tommi@chromium.org,grunell@chromium.org,rdevlin.cronin@chromium.org,clamy@chromium.org,msramek@chromium.org,hbos@chromium.org,guidou@chromium.org,terelius@chromium.org,eladalon@chromium.org
    
    Change-Id: I97ff60cccac1be13dc250f6a3bb129c09265e212
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 775415, 780863
    Reviewed-on: https://chromium-review.googlesource.com/803614
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#520938}
    Former-commit-id: 67dc496d6933b65092528bed4a995eef44a24ac9

--- content/browser/webrtc/webrtc_internals.h
+++ content/browser/webrtc/webrtc_internals.h
@@ -101,8 +101,8 @@ class CONTENT_EXPORT WebRTCInternals : public RenderProcessHostObserver,
   const base::FilePath& GetAudioDebugRecordingsFilePath() const;
 
   // Enables or disables diagnostic event log.
-  void EnableLocalEventLogRecordings(content::WebContents* web_contents);
-  void DisableLocalEventLogRecordings();
+  void EnableEventLogRecordings(content::WebContents* web_contents);
+  void DisableEventLogRecordings();
 
   bool IsEventLogRecordingsEnabled() const;
   const base::FilePath& GetEventLogFilePath() const;
@@ -153,9 +153,9 @@ class CONTENT_EXPORT WebRTCInternals : public RenderProcessHostObserver,
   // |audio_debug_recordings_file_path_|.
   void EnableAudioDebugRecordingsOnAllRenderProcessHosts();
 
-  // Enables local WebRTC event log recordings on all render process hosts using
+  // Enables event log recordings on all render process hosts using
   // |event_log_recordings_file_path_|.
-  void EnableLocalEventLogRecordingsOnAllRenderProcessHosts();
+  void EnableEventLogRecordingsOnAllRenderProcessHosts();
 #endif
 
   // Updates the number of open PeerConnections. Called when a PeerConnection
--- content/common/media/peer_connection_tracker_messages.h
+++ content/common/media/peer_connection_tracker_messages.h
@@ -41,18 +41,13 @@ IPC_MESSAGE_CONTROL5(PeerConnectionTrackerHost_GetUserMedia,
                      // not be parsed by the browser for security reasons.
                      std::string /* audio_constraints */,
                      std::string /* video_constraints */)
-IPC_MESSAGE_CONTROL2(PeerConnectionTrackerHost_WebRtcEventLogWrite,
-                     int /* lid */,
-                     std::string /* output */)
 
 // Messages sent to PeerConnectionTracker.
 IPC_MESSAGE_CONTROL0(PeerConnectionTracker_GetAllStats)
 IPC_MESSAGE_CONTROL0(PeerConnectionTracker_OnSuspend)
-IPC_MESSAGE_CONTROL2(PeerConnectionTracker_StartEventLogFile,
+IPC_MESSAGE_CONTROL2(PeerConnectionTracker_StartEventLog,
                      int /* peer_connection_local_id */,
                      IPC::PlatformFileForTransit /* file */)
-IPC_MESSAGE_CONTROL1(PeerConnectionTracker_StartEventLogOutput,
-                     int /* peer_connection_local_id */)
 IPC_MESSAGE_CONTROL1(PeerConnectionTracker_StopEventLog,
                      int /* peer_connection_local_id */)
 
