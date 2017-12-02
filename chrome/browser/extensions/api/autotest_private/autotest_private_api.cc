4cb85ab0cea8cf0af57cdfbf43956be621978432
Sarah Hu <xiaoyinh@chromium.org>
2017-12-02 00:14:31 +0000

    Modify Autotest API to support views based lockscreen
    Bug: 781998
    Change-Id: I11a456b1341d044b1370652c337f105e0bef649a
    Reviewed-on: https://chromium-review.googlesource.com/803622
    Commit-Queue: Xiaoyin Hu <xiaoyinh@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521137}
    Former-commit-id: 7df02c0de4bab86ea229a6a09bb8cc65b1782593

--- chrome/browser/extensions/api/autotest_private/autotest_private_api.cc
+++ chrome/browser/extensions/api/autotest_private/autotest_private_api.cc
@@ -109,8 +109,12 @@ ExtensionFunction::ResponseAction AutotestPrivateLoginStatusFunction::Run() {
 #if defined(OS_CHROMEOS)
   const user_manager::UserManager* user_manager =
       user_manager::UserManager::Get();
+
+  // default_screen_locker()->locked() is set when the UI is ready, so this
+  // tells us both views based lockscreen UI and screenlocker are ready.
   const bool is_screen_locked =
-      !!chromeos::ScreenLocker::default_screen_locker();
+      !!chromeos::ScreenLocker::default_screen_locker() &&
+      chromeos::ScreenLocker::default_screen_locker()->locked();
 
   if (user_manager) {
     result->SetBoolean("isLoggedIn", user_manager->IsUserLoggedIn());
