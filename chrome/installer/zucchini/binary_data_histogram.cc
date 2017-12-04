cd679ddd926be9e18767e83d3adce8a5fb269f6a
Samuel Huang <huangs@chromium.org>
2017-12-04 15:22:15 +0000

    [Zucchini] Introduce OutlierDetector.
    OutlierDetector is a component of (upcoming) ensemble patching. Its goal
    is to reduce the likelyhood of false-positive matches by applying simple
    statistical tests on BinaryDataHistogram scores.
    
    Bug: 778316
    Change-Id: I57e9859b73b9fc62ffffe973ca47ac723e64f6fd
    Reviewed-on: https://chromium-review.googlesource.com/798899
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521342}
    Former-commit-id: 7f1542b75645fb2e269d4aee46b5a41d49442ac8

--- chrome/installer/zucchini/binary_data_histogram.cc
+++ chrome/installer/zucchini/binary_data_histogram.cc
@@ -4,13 +4,58 @@
 
 #include "chrome/installer/zucchini/binary_data_histogram.h"
 
+#include <algorithm>
 #include <cmath>
 #include <limits>
 
+#include "base/format_macros.h"
 #include "base/logging.h"
-#include "base/memory/ptr_util.h"
+#include "base/strings/stringprintf.h"
 
 namespace zucchini {
+
+/******** OutlierDetector ********/
+
+OutlierDetector::OutlierDetector() = default;
+
+OutlierDetector::~OutlierDetector() = default;
+
+// For BinaryDataHistogram, |sample| is typically in interval [0, 1].
+void OutlierDetector::Add(double sample) {
+  ++n_;
+  sum_ += sample;
+  sum_of_squares_ += sample * sample;
+}
+
+void OutlierDetector::Prepare() {
+  if (n_ > 0) {
+    mean_ = sum_ / n_;
+    standard_deviation_ = ::sqrt((sum_of_squares_ - sum_ * mean_) /
+                                 std::max(static_cast<size_t>(1), n_ - 1));
+  }
+}
+
+std::string OutlierDetector::RenderStats() {
+  return base::StringPrintf("Mean = %.5f, StdDev = %.5f over %" PRIuS
+                            " samples",
+                            mean_, standard_deviation_, n_);
+}
+
+// Constants are chosen for BinaryDataHistogram, where |sample| is typically in
+// [0, 1].
+int OutlierDetector::DecideOutlier(double sample) {
+  // Lower bound to avoid divide-by-zero and penalizing tight clusters.
+  constexpr double kMinTolerance = 0.1;
+  // Number of standard deviations away from mean for value to become outlier.
+  constexpr double kSigmaBound = 1.9;
+  if (n_ <= 1)
+    return 0;
+  double tolerance = std::max(kMinTolerance, standard_deviation_);
+  double num_sigma = (sample - mean_) / tolerance;
+  return num_sigma > kSigmaBound ? 1 : num_sigma < -kSigmaBound ? -1 : 0;
+}
+
+/******** BinaryDataHistogram ********/
 
 BinaryDataHistogram::BinaryDataHistogram() = default;
 
@@ -24,7 +69,7 @@ bool BinaryDataHistogram::Compute(ConstBufferView region) {
   DCHECK_LE(region.size(),
             static_cast<size_t>(std::numeric_limits<int32_t>::max()));
 
-  histogram_ = base::MakeUnique<int32_t[]>(kNumBins);
+  histogram_ = std::make_unique<int32_t[]>(kNumBins);
   size_ = region.size();
   // Number of 2-byte intervals fully contained in |region|.
   size_t bound = size_ - sizeof(uint16_t) + 1;
