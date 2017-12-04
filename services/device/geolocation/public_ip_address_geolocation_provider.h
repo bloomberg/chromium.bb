ccfcd001122af5ff071518530accccdc60f6f3d8
Renjie Liu <renjieliu@chromium.org>
2017-12-04 23:37:09 +0000

    PublicIpAddressGeolocationProvider interface and implementation
    original owner: amoylan@chromium.org
    original cl: https://chromium-review.googlesource.com/c/chromium/src/+/754255
    
    This CL is part of the implementation of public-IP-only geolocation
    as described in the design doc:
    https://docs.google.com/document/d/1oSQM3GEMTL1raWLYv8S6_Yyt93FF2zQbANI4bKJ6LDs
    
    This part is a new Mojo interface PublicIpAddressGeolocationProvider
    that provides mojom::Geolocation implementations using the
    PublicIpAddressGeolocator (from the upstream dependent CL).
    
    In a subsequent CL, DeviceService will provide this implementation
    of PublicIpAddressGeolocationProvider via ServiceManager.
    
    Change-Id: I2458c816b5a861a409ab2b3a5defa8d80444c59e
    Reviewed-on: https://chromium-review.googlesource.com/784533
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Renjie Liu <renjieliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521521}
    Former-commit-id: 8668849b1d0f5dc561e5ab1659b89fe56bb0472b

---
+++ services/device/geolocation/public_ip_address_geolocation_provider.h
@@ -0,0 +1,76 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_DEVICE_GEOLOCATION_PUBLIC_IP_ADDRESS_GEOLOCATION_PROVIDER_H_
+#define SERVICES_DEVICE_GEOLOCATION_PUBLIC_IP_ADDRESS_GEOLOCATION_PROVIDER_H_
+
+#include <string>
+
+#include "base/macros.h"
+#include "device/geolocation/public/interfaces/geolocation.mojom.h"
+#include "mojo/public/cpp/bindings/binding_set.h"
+#include "mojo/public/cpp/bindings/strong_binding_set.h"
+#include "net/traffic_annotation/network_traffic_annotation.h"
+#include "services/device/geolocation/public_ip_address_geolocator.h"
+#include "services/device/geolocation/public_ip_address_location_notifier.h"
+#include "services/device/public/interfaces/public_ip_address_geolocation_provider.mojom.h"
+
+namespace device {
+
+// Implementation of PublicIpAddressGeolocationProvider Mojo interface that will
+// provide mojom::Geolocation implementations that use IP-only geolocation.
+// Binds multiple PublicIpAddressGeolocationProvider requests.
+//
+// Sequencing:
+// * Must be used and destroyed on the same sequence.
+// * Provides mojom::Geolocation instances that are bound on the same sequence.
+// * Requires two-step construction: Construct on any sequence, then invoke
+//   Initialize() on the sequence on which this object will run.
+class PublicIpAddressGeolocationProvider
+    : public mojom::PublicIpAddressGeolocationProvider {
+ public:
+  // After construction, invoke Initialize() (on the appropriate sequence) to
+  // finish initialization.
+  PublicIpAddressGeolocationProvider();
+  ~PublicIpAddressGeolocationProvider() override;
+
+  // Finishes initialization, using the specified Google |api_key| and a URL
+  // request context produced by |request_context_producer| for network location
+  // requests.
+  // After this call is made, this object must be used only on the sequence on
+  // which this call was made.
+  void Initialize(
+      GeolocationProvider::RequestContextProducer request_context_producer,
+      const std::string& api_key);
+
+  // Binds a PublicIpAddressGeolocationProvider request to this instance.
+  void Bind(mojom::PublicIpAddressGeolocationProviderRequest request);
+
+ private:
+  SEQUENCE_CHECKER(sequence_checker_);
+
+  // mojom::PublicIpAddressGeolocationProvider implementation:
+  // Provides a Geolocation instance that performs IP-geolocation only.
+  void CreateGeolocation(
+      const net::MutablePartialNetworkTrafficAnnotationTag& tag,
+      mojom::GeolocationRequest request) override;
+
+  // Central PublicIpAddressLocationNotifier for use by all implementations of
+  // mojom::Geolocation provided by the CreateGeolocation method.
+  // Note that this must be before the StrongBindingSet<mojom::Geolocation> as
+  // it must outlive the Geolocation implementations.
+  std::unique_ptr<PublicIpAddressLocationNotifier>
+      public_ip_address_location_notifier_;
+
+  mojo::BindingSet<mojom::PublicIpAddressGeolocationProvider>
+      provider_binding_set_;
+
+  mojo::StrongBindingSet<mojom::Geolocation> geolocation_binding_set_;
+
+  DISALLOW_COPY_AND_ASSIGN(PublicIpAddressGeolocationProvider);
+};
+
+}  // namespace device
+
+#endif  // SERVICES_DEVICE_GEOLOCATION_PUBLIC_IP_ADDRESS_GEOLOCATION_PROVIDER_H_
