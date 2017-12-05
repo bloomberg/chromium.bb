444bebeff65d2630fb98af47e75a25d64f226edb
Takumi Fujimoto <takumif@chromium.org>
2017-12-05 00:33:00 +0000

    [Media Router] Better sink availability tracking
    - When sink availability changes for an MRP, start/stop observing sinks only for that MRP
    - Make WiredDisplayMRP report sink availability updates
    
    Bug: 789277,777650
    Change-Id: I3d1a087ca39a4830403d6d2f0f00bf1411388f33
    Reviewed-on: https://chromium-review.googlesource.com/794106
    Reviewed-by: Derek Cheng <imcheng@chromium.org>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521550}
    Former-commit-id: 52dda1185f5f801b7f16bc79be6696b297cbf3d4

--- chrome/browser/media/router/mojo/media_router_mojo_impl.h
+++ chrome/browser/media/router/mojo/media_router_mojo_impl.h
@@ -296,6 +296,10 @@ class MediaRouterMojoImpl : public MediaRouterBase,
     bool SetAvailabilityForProvider(MediaRouteProviderId provider_id,
                                     SinkAvailability availability);
 
+    // Returns true if the availability for the provider is not UNAVAILABLE.
+    bool IsAvailableForProvider(
+        mojom::MediaRouteProvider::Id provider_id) const;
+
     // Returns true if there is a provider whose sink availability isn't
     // UNAVAILABLE.
     bool IsAvailable() const;
@@ -407,7 +411,7 @@ class MediaRouterMojoImpl : public MediaRouterBase,
   std::string instance_id_;
 
   // The last reported sink availability from the media route providers.
-  ProviderSinkAvailability availability_;
+  ProviderSinkAvailability sink_availability_;
 
   // Bindings for Mojo pointers to |this| held by media route providers.
   mojo::BindingSet<mojom::MediaRouter> bindings_;
