9d7ce6d1e1c6c3cc2ede00b3ecc5339e89d3e21b
Chris Nardi <hichris123@gmail.com>
2017-12-05 15:44:50 +0000

    Update parsing of RGB() and RGBA() functions to match CSS Color 4
    In CSS Color 4, RGB() and RGBA() were synonymized, along with allowing
    whitespace to separate the channels instead of commas, introducing
    a backslash as a new way to separate the alpha parameter, and allowing
    percent values for the alpha parameter. This updates our parsing
    behavior to reflect these changes, and enables now-passing WPT tests.
    The new spec is at https://drafts.csswg.org/css-color/#rgb-functions.
    
    Bug: 788707
    Change-Id: Ib0f29009e477fc88cd1ceda03ff0ebb947a36ea3
    Reviewed-on: https://chromium-review.googlesource.com/798831
    Commit-Queue: Chris Nardi <hichris123@gmail.com>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521703}
    Former-commit-id: 106b2819bfca32edee99e803364395453b31a2ca

--- third_party/WebKit/Source/core/css/parser/CSSParserFastPathsTest.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSParserFastPathsTest.cpp
@@ -118,4 +118,45 @@ TEST(CSSParserFastPathsTest, ParseColorWithLargeAlpha) {
   EXPECT_EQ(Color::kBlack, ToCSSColorValue(*value).Value());
 }
 
+TEST(CSSParserFastPathsTest, ParseColorWithNewSyntax) {
+  CSSValue* value =
+      CSSParserFastPaths::ParseColor("rgba(0 0 0)", kHTMLStandardMode);
+  EXPECT_NE(nullptr, value);
+  EXPECT_TRUE(value->IsColorValue());
+  EXPECT_EQ(Color::kBlack, ToCSSColorValue(*value).Value());
+
+  value = CSSParserFastPaths::ParseColor("rgba(0 0 0 / 1)", kHTMLStandardMode);
+  EXPECT_NE(nullptr, value);
+  EXPECT_TRUE(value->IsColorValue());
+  EXPECT_EQ(Color::kBlack, ToCSSColorValue(*value).Value());
+
+  value = CSSParserFastPaths::ParseColor("rgba(0, 0, 0, 1)", kHTMLStandardMode);
+  EXPECT_NE(nullptr, value);
+  EXPECT_TRUE(value->IsColorValue());
+  EXPECT_EQ(Color::kBlack, ToCSSColorValue(*value).Value());
+
+  value = CSSParserFastPaths::ParseColor("RGBA(0 0 0 / 1)", kHTMLStandardMode);
+  EXPECT_NE(nullptr, value);
+  EXPECT_TRUE(value->IsColorValue());
+  EXPECT_EQ(Color::kBlack, ToCSSColorValue(*value).Value());
+
+  value = CSSParserFastPaths::ParseColor("RGB(0 0 0 / 1)", kHTMLStandardMode);
+  EXPECT_NE(nullptr, value);
+  EXPECT_TRUE(value->IsColorValue());
+  EXPECT_EQ(Color::kBlack, ToCSSColorValue(*value).Value());
+
+  value = CSSParserFastPaths::ParseColor("rgba(0 0 0 0)", kHTMLStandardMode);
+  EXPECT_EQ(nullptr, value);
+
+  value = CSSParserFastPaths::ParseColor("rgba(0, 0 0 1)", kHTMLStandardMode);
+  EXPECT_EQ(nullptr, value);
+
+  value =
+      CSSParserFastPaths::ParseColor("rgba(0, 0, 0 / 1)", kHTMLStandardMode);
+  EXPECT_EQ(nullptr, value);
+
+  value = CSSParserFastPaths::ParseColor("rgba(0 0 0, 1)", kHTMLStandardMode);
+  EXPECT_EQ(nullptr, value);
+}
+
 }  // namespace blink
