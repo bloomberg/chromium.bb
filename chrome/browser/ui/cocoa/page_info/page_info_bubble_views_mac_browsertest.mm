76624364267f1f5020e3bb4dc381d387dc4646a6
Patricia Lor <patricialor@chromium.org>
2017-12-05 08:40:28 +0000

    Desktop Page Info/Mac: Dismiss internal page info bubble on tab switches.
    Follow up to r475019, which dismisses the (non-internal) page info bubble when
    using global browser hotkeys. This fixes the same issue, but for the internal
    page info bubble.
    
    Bug: 786361
    Change-Id: If0a04f057e023871ff2b665d98941b836920625a
    Reviewed-on: https://chromium-review.googlesource.com/799651
    Commit-Queue: Patti <patricialor@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521641}
    Former-commit-id: 6ed69ac878454fe3606b867eb4a15145baed701c

--- chrome/browser/ui/cocoa/page_info/page_info_bubble_views_mac_browsertest.mm
+++ chrome/browser/ui/cocoa/page_info/page_info_bubble_views_mac_browsertest.mm
@@ -5,7 +5,9 @@
 #import "chrome/browser/ui/cocoa/page_info/page_info_bubble_controller.h"
 
 #include "base/command_line.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/exclusive_access/exclusive_access_manager.h"
 #include "chrome/browser/ui/exclusive_access/fullscreen_controller.h"
 #include "chrome/browser/ui/page_info/page_info_dialog.h"
@@ -14,6 +16,7 @@
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "ui/base/material_design/material_design_controller.h"
+#include "ui/base/page_transition_types.h"
 #import "ui/base/test/scoped_fake_nswindow_fullscreen.h"
 #include "ui/base/ui_base_switches.h"
 #include "ui/views/bubble/bubble_dialog_delegate.h"
@@ -27,15 +30,33 @@
 
 namespace {
 
-using PageInfoBubbleViewsMacTest = InProcessBrowserTest;
+struct GURLBubbleTypePair {
+  const char* const url;
+  const PageInfoBubbleView::BubbleType bubble_type;
+};
+
+constexpr GURLBubbleTypePair kGurlBubbleTypePairs[] = {
+    {url::kAboutBlankURL, PageInfoBubbleView::BUBBLE_PAGE_INFO},
+    {"chrome://settings", PageInfoBubbleView::BUBBLE_INTERNAL_PAGE},
+};
+
+class PageInfoBubbleViewsMacTest
+    : public InProcessBrowserTest,
+      public testing::WithParamInterface<GURLBubbleTypePair> {
+ protected:
+  PageInfoBubbleViewsMacTest() {}
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(PageInfoBubbleViewsMacTest);
+};
 
 }  // namespace
 
 // Test the Page Info bubble doesn't crash upon entering full screen mode while
 // it is open. This may occur when the bubble is trying to reanchor itself.
-IN_PROC_BROWSER_TEST_F(PageInfoBubbleViewsMacTest, NoCrashOnFullScreenToggle) {
+IN_PROC_BROWSER_TEST_P(PageInfoBubbleViewsMacTest, NoCrashOnFullScreenToggle) {
   ui::test::ScopedFakeNSWindowFullscreen fake_fullscreen;
-  ui_test_utils::NavigateToURL(browser(), GURL(url::kAboutBlankURL));
+  ui_test_utils::NavigateToURL(browser(), GURL(GetParam().url));
   ShowPageInfoDialog(browser()->tab_strip_model()->GetWebContentsAt(0));
   ExclusiveAccessManager* access_manager =
       browser()->exclusive_access_manager();
@@ -49,8 +70,7 @@ IN_PROC_BROWSER_TEST_F(PageInfoBubbleViewsMacTest, NoCrashOnFullScreenToggle) {
     EXPECT_TRUE(page_info);
     views::Widget* page_info_bubble = page_info->GetWidget();
     EXPECT_TRUE(page_info_bubble);
-    EXPECT_EQ(PageInfoBubbleView::BUBBLE_PAGE_INFO,
-              PageInfoBubbleView::GetShownBubbleType());
+    EXPECT_EQ(GetParam().bubble_type, PageInfoBubbleView::GetShownBubbleType());
     EXPECT_TRUE(page_info_bubble->IsVisible());
   } else {
     EXPECT_TRUE([PageInfoBubbleController getPageInfoBubbleForTest]);
@@ -62,3 +82,42 @@ IN_PROC_BROWSER_TEST_F(PageInfoBubbleViewsMacTest, NoCrashOnFullScreenToggle) {
   // transitioning into full screen.
   fake_fullscreen.FinishTransition();
 }
+
+// Test |PageInfoBubbleView| or |InternalPageInfoBubbleView| is hidden on tab
+// switches via keyboard shortcuts.
+IN_PROC_BROWSER_TEST_P(PageInfoBubbleViewsMacTest,
+                       BubbleClosesOnKeyboardTabSwitch) {
+  ui_test_utils::NavigateToURL(browser(), GURL(GetParam().url));
+  // Add a second tab, but make sure the first is selected.
+  AddTabAtIndex(1, GURL("https://test_url.com"),
+                ui::PageTransition::PAGE_TRANSITION_LINK);
+  browser()->tab_strip_model()->ActivateTabAt(0, true);
+  EXPECT_EQ(0, browser()->tab_strip_model()->active_index());
+
+  // Show the (internal or external) Page Info bubble and check it's visible.
+  ShowPageInfoDialog(browser()->tab_strip_model()->GetWebContentsAt(0));
+  EXPECT_EQ(GetParam().bubble_type, PageInfoBubbleView::GetShownBubbleType());
+  views::BubbleDialogDelegateView* page_info =
+      PageInfoBubbleView::GetPageInfoBubble();
+  EXPECT_TRUE(page_info);
+  EXPECT_TRUE(page_info->GetWidget()->IsVisible());
+
+  // Switch to the second tab without clicking (which would normally dismiss the
+  // bubble).
+  chrome::SelectNextTab(browser());
+  EXPECT_EQ(1, browser()->tab_strip_model()->active_index());
+
+  page_info = PageInfoBubbleView::GetPageInfoBubble();
+  // Check the bubble is no longer visible. BubbleDialogDelegateView's Widget is
+  // destroyed when the native widget is destroyed, so it should still be alive.
+  EXPECT_TRUE(page_info);
+  EXPECT_TRUE(page_info->GetWidget());
+  EXPECT_FALSE(page_info->GetWidget()->IsVisible());
+
+  base::RunLoop().RunUntilIdle();
+  EXPECT_FALSE(PageInfoBubbleView::GetPageInfoBubble());
+}
+
+INSTANTIATE_TEST_CASE_P(,
+                        PageInfoBubbleViewsMacTest,
+                        testing::ValuesIn(kGurlBubbleTypePairs));
