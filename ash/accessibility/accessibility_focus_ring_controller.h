b81b11a24b0c780cd8225e6ae765f1ddf799be7d
Katie Dektar <katydek@google.com>
2017-12-06 23:35:21 +0000

    Adds test to ensure Select-to-Speak gets mouse movements.
    This would catch a regression that happened last week where mousemove
    events were not passed properly to the background extension, should
    it re-occur in the future.
    
    Bug: 789598
    Change-Id: Idd61c1e63f9dfa4f0e87603b62b0cc0a31a34902
    Reviewed-on: https://chromium-review.googlesource.com/806647
    Commit-Queue: Katie D <katie@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522252}
    Former-commit-id: c124b83383cd929e10b26d545eaa6e7e323d138a

--- ash/accessibility/accessibility_focus_ring_controller.h
+++ ash/accessibility/accessibility_focus_ring_controller.h
@@ -12,6 +12,7 @@
 #include "ash/accessibility/accessibility_focus_ring_layer.h"
 #include "ash/accessibility/accessibility_highlight_layer.h"
 #include "ash/ash_export.h"
+#include "base/bind.h"
 #include "base/macros.h"
 #include "base/memory/singleton.h"
 #include "base/optional.h"
@@ -61,11 +62,25 @@ class ASH_EXPORT AccessibilityFocusRingController
   // Don't fade in / out, for testing.
   void SetNoFadeForTesting();
 
+  // Get accessibility layers, for testing.
   AccessibilityCursorRingLayer* cursor_layer_for_testing() {
     return cursor_layer_.get();
   }
   AccessibilityCursorRingLayer* caret_layer_for_testing() {
     return caret_layer_.get();
+  }
+  const std::vector<std::unique_ptr<AccessibilityFocusRingLayer>>&
+  focus_ring_layers_for_testing() {
+    return focus_layers_;
+  }
+  AccessibilityHighlightLayer* highlight_layer_for_testing() {
+    return highlight_layer_.get();
+  }
+
+  // Sets an observer of focus ring layer changes.
+  void set_focus_ring_observer_for_testing(
+      base::RepeatingCallback<void()> observer) {
+    focus_ring_observer_for_testing_ = observer;
   }
 
  protected:
@@ -144,6 +159,8 @@ class ASH_EXPORT AccessibilityFocusRingController
   float highlight_opacity_;
 
   friend struct base::DefaultSingletonTraits<AccessibilityFocusRingController>;
+
+  base::RepeatingCallback<void()> focus_ring_observer_for_testing_;
 
   DISALLOW_COPY_AND_ASSIGN(AccessibilityFocusRingController);
 };
