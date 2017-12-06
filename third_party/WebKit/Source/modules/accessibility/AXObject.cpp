fdfb2f4459e3ed70643935f925f2f79a906a81fd
Katie Dektar <katydek@google.com>
2017-12-06 19:25:41 +0000

    Fixes clipping on fixed and absolutely positioned elements.
    Fixed positioned elements are not clipped by their ancestors,
    except by the rootWebArea. Similarly with absolutely positioned
    elements, except if they have an ancestor with some non-static
    positioning.
    
    Bug: 792124
    Change-Id: I9c31c90fed89179434ae7f89aa18528e929348da
    Reviewed-on: https://chromium-review.googlesource.com/809828
    Commit-Queue: Katie D <katie@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522162}
    Former-commit-id: 9b4b71e58087875b5408dc5f91d18c4bb91969db

--- third_party/WebKit/Source/modules/accessibility/AXObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXObject.cpp
@@ -1936,16 +1936,32 @@ void AXObject::GetRelativeBounds(AXObject** out_container,
   // layer.
   AXObject* container = ParentObjectUnignored();
   LayoutObject* container_layout_object = nullptr;
-  while (container) {
-    container_layout_object = container->GetLayoutObject();
-    if (container_layout_object && container_layout_object->IsBox() &&
-        layout_object->IsDescendantOf(container_layout_object)) {
-      if (container->IsScrollableContainer() ||
-          container_layout_object->HasLayer())
-        break;
-    }
+  if (layout_object->IsFixedPositioned()) {
+    // If it's a fixed position element, the container should simply be the
+    // root web area.
+    container = AXObjectCache().GetOrCreate(GetDocument());
+  } else {
+    while (container) {
+      container_layout_object = container->GetLayoutObject();
+      if (container_layout_object && container_layout_object->IsBox() &&
+          layout_object->IsDescendantOf(container_layout_object)) {
+        if (container->IsScrollableContainer() ||
+            container_layout_object->HasLayer()) {
+          if (layout_object->IsAbsolutePositioned()) {
+            // If it's absolutely positioned, the container must be the
+            // nearest positioned container, or the root.
+            if (container->IsWebArea())
+              break;
+            if (container_layout_object->IsPositioned())
+              break;
+          } else {
+            break;
+          }
+        }
+      }
 
-    container = container->ParentObjectUnignored();
+      container = container->ParentObjectUnignored();
+    }
   }
 
   if (!container)
