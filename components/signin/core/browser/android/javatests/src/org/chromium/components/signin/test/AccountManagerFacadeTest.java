f3ca756382f0a89ceca22b2170f8243cb81d154e
Boris Sazonov <bsazonov@chromium.org>
2017-12-07 18:13:21 +0000

    [Signin][Android] Remove SimpleFuture from signin tests
    This CL removes AccountManagerFacadeTest.hasAccountForName as the same
    method in AccountManagerFacade can be invoked from any thread. It was
    the last usage of org.chromium.components.signin.test.util.SimpleFuture,
    it is removed as well.
    
    Bug: 698258
    Change-Id: I62271d79e469ae966ea500dbb058fb54baf36a54
    Reviewed-on: https://chromium-review.googlesource.com/814514
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522476}
    Former-commit-id: ec4490c0ec5ed8d4543a6680a3f2cbfed3a11259

--- components/signin/core/browser/android/javatests/src/org/chromium/components/signin/test/AccountManagerFacadeTest.java
+++ components/signin/core/browser/android/javatests/src/org/chromium/components/signin/test/AccountManagerFacadeTest.java
@@ -8,20 +8,17 @@ import android.accounts.Account;
 import android.support.test.filters.SmallTest;
 import android.support.test.rule.UiThreadTestRule;
 
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.components.signin.AccountManagerFacade;
 import org.chromium.components.signin.ProfileDataSource;
 import org.chromium.components.signin.test.util.AccountHolder;
 import org.chromium.components.signin.test.util.FakeAccountManagerDelegate;
-import org.chromium.components.signin.test.util.SimpleFuture;
 
 /**
  * Test class for {@link AccountManagerFacade}.
@@ -32,7 +29,7 @@ public class AccountManagerFacadeTest {
     public UiThreadTestRule mRule = new UiThreadTestRule();
 
     private FakeAccountManagerDelegate mDelegate;
-    private AccountManagerFacade mHelper;
+    private AccountManagerFacade mFacade;
 
     @Before
     public void setUp() throws Exception {
@@ -41,12 +38,7 @@ public class AccountManagerFacadeTest {
         Assert.assertFalse(mDelegate.isRegisterObserversCalled());
         AccountManagerFacade.overrideAccountManagerFacadeForTests(mDelegate);
         Assert.assertTrue(mDelegate.isRegisterObserversCalled());
-        mHelper = AccountManagerFacade.get();
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        AccountManagerFacade.resetAccountManagerFacadeForTests();
+        mFacade = AccountManagerFacade.get();
     }
 
     @Test
@@ -54,9 +46,9 @@ public class AccountManagerFacadeTest {
     public void testCanonicalAccount() {
         addTestAccount("test@gmail.com");
 
-        Assert.assertTrue(hasAccountForName("test@gmail.com"));
-        Assert.assertTrue(hasAccountForName("Test@gmail.com"));
-        Assert.assertTrue(hasAccountForName("te.st@gmail.com"));
+        Assert.assertTrue(mFacade.hasAccountForName("test@gmail.com"));
+        Assert.assertTrue(mFacade.hasAccountForName("Test@gmail.com"));
+        Assert.assertTrue(mFacade.hasAccountForName("te.st@gmail.com"));
     }
 
     // If this test starts flaking, please re-open crbug.com/568636 and make sure there is some sort
@@ -66,10 +58,10 @@ public class AccountManagerFacadeTest {
     public void testNonCanonicalAccount() {
         addTestAccount("test.me@gmail.com");
 
-        Assert.assertTrue(hasAccountForName("test.me@gmail.com"));
-        Assert.assertTrue(hasAccountForName("testme@gmail.com"));
-        Assert.assertTrue(hasAccountForName("Testme@gmail.com"));
-        Assert.assertTrue(hasAccountForName("te.st.me@gmail.com"));
+        Assert.assertTrue(mFacade.hasAccountForName("test.me@gmail.com"));
+        Assert.assertTrue(mFacade.hasAccountForName("testme@gmail.com"));
+        Assert.assertTrue(mFacade.hasAccountForName("Testme@gmail.com"));
+        Assert.assertTrue(mFacade.hasAccountForName("te.st.me@gmail.com"));
     }
 
     @Test
@@ -99,20 +91,5 @@ public class AccountManagerFacadeTest {
         AccountHolder holder = AccountHolder.builder(account).alwaysAccept(true).build();
         mDelegate.addAccountHolderBlocking(holder);
         return account;
-    }
-
-    private boolean hasAccountForName(final String accountName) {
-        final SimpleFuture<Boolean> result = new SimpleFuture<Boolean>();
-        ThreadUtils.postOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                mHelper.hasAccountForName(accountName, result.createCallback());
-            }
-        });
-        try {
-            return result.get();
-        } catch (InterruptedException e) {
-            throw new RuntimeException("Exception occurred while waiting for future", e);
-        }
     }
 }
