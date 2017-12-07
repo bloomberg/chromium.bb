4c31da76733ed7a201bd68db6dcb796f9c6502f3
Dave Tapuska <dtapuska@chromium.org>
2017-12-07 14:31:55 +0000

    Fix flaky SyntheticMouseEventTest.MouseEventAck
    Reland https://chromium-review.googlesource.com/804357. This time ensure
    that we are using a MouseDown event instead of MouseMove event as
    MouseMove events can be sent from the UI synthetically and that causes
    the InputMsgWatcher to be in a different state sometimes.
    
    Devtools was not correctly handling input that is not generated by itself
    and invoking callbacks when the input wasn't actually processed.
    
    Add a modifier indicating content was injected using the debugger so that
    we can then query for that to determine if we should release the event.
    This is a stop gap solution until we can actually bind callbacks into the
    sending of events and devtools wouldn't need its own queues of callbacks.
    
    BUG=789869
    TBR=pfeldman@chromium.org
    
    Change-Id: Id769fa9363debb196d787a73920f24e3a409c68f
    Reviewed-on: https://chromium-review.googlesource.com/811833
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522417}
    Former-commit-id: 47740515e5b7e5e2df5db5f7f2685f017eec18d9

--- content/browser/devtools/protocol/devtools_protocol_browsertest.cc
+++ content/browser/devtools/protocol/devtools_protocol_browsertest.cc
@@ -514,11 +514,19 @@ class SyntheticKeyEventTest : public DevToolsProtocolTest {
 
 class SyntheticMouseEventTest : public DevToolsProtocolTest {
  protected:
-  void SendMouseEvent(const std::string& type, int x, int y, bool wait) {
+  void SendMouseEvent(const std::string& type,
+                      int x,
+                      int y,
+                      const std::string& button,
+                      bool wait) {
     std::unique_ptr<base::DictionaryValue> params(new base::DictionaryValue());
     params->SetString("type", type);
     params->SetInteger("x", x);
     params->SetInteger("y", y);
+    if (!button.empty()) {
+      params->SetString("button", button);
+      params->SetInteger("clickCount", 1);
+    }
     SendCommand("Input.dispatchMouseEvent", std::move(params), wait);
   }
 };
@@ -582,20 +590,20 @@ IN_PROC_BROWSER_TEST_F(SyntheticKeyEventTest, KeyboardEventAck) {
   EXPECT_EQ(3u, result_ids_.size());
 }
 
-IN_PROC_BROWSER_TEST_F(SyntheticMouseEventTest, DISABLED_MouseEventAck) {
+IN_PROC_BROWSER_TEST_F(SyntheticMouseEventTest, MouseEventAck) {
   NavigateToURLBlockUntilNavigationsComplete(shell(), GURL("about:blank"), 1);
   Attach();
   ASSERT_TRUE(content::ExecuteScript(
       shell()->web_contents()->GetRenderViewHost(),
-      "document.body.addEventListener('mousemove', () => {debugger;});"));
+      "document.body.addEventListener('mousedown', () => {debugger;});"));
 
   auto filter = std::make_unique<InputMsgWatcher>(
       RenderWidgetHostImpl::From(
           shell()->web_contents()->GetRenderViewHost()->GetWidget()),
-      blink::WebInputEvent::kMouseMove);
+      blink::WebInputEvent::kMouseDown);
 
   SendCommand("Debugger.enable", nullptr);
-  SendMouseEvent("mouseMoved", 15, 15, false);
+  SendMouseEvent("mousePressed", 15, 15, "left", false);
 
   // We expect that the debugger message event arrives *before* the input
   // event ack, and the subsequent command response for
