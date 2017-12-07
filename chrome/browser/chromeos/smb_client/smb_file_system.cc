28c86fc306c401c01e124814101e0e11ff010e00
Bailey Berro <baileyberro@chromium.org>
2017-12-07 23:35:24 +0000

    Implementing Mount and Unmount for SmbFileSystem.
    Make SmbService::Mount api asynchronous.
    
    Bug: 778791
    Change-Id: I219214f381c1c78dd4d2a018ad909286a89e3719
    Reviewed-on: https://chromium-review.googlesource.com/812107
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522614}
    Former-commit-id: f5212d9f2c2d684939b9ade5e1c2f6adad232d70

--- chrome/browser/chromeos/smb_client/smb_file_system.cc
+++ chrome/browser/chromeos/smb_client/smb_file_system.cc
@@ -4,6 +4,10 @@
 
 #include "chrome/browser/chromeos/smb_client/smb_file_system.h"
 
+#include "base/strings/string_number_conversions.h"
+#include "chromeos/dbus/dbus_thread_manager.h"
+#include "chromeos/dbus/smb_provider_client.h"
+
 namespace chromeos {
 namespace smb_client {
 
@@ -15,10 +19,81 @@ SmbFileSystem::SmbFileSystem(
 
 SmbFileSystem::~SmbFileSystem() {}
 
+// static
+base::File::Error SmbFileSystem::TranslateError(smbprovider::ErrorType error) {
+  DCHECK_NE(smbprovider::ERROR_NONE, error);
+
+  switch (error) {
+    case smbprovider::ERROR_OK:
+      return base::File::FILE_OK;
+    case smbprovider::ERROR_FAILED:
+      return base::File::FILE_ERROR_FAILED;
+    case smbprovider::ERROR_IN_USE:
+      return base::File::FILE_ERROR_IN_USE;
+    case smbprovider::ERROR_EXISTS:
+      return base::File::FILE_ERROR_EXISTS;
+    case smbprovider::ERROR_NOT_FOUND:
+      return base::File::FILE_ERROR_NOT_FOUND;
+    case smbprovider::ERROR_ACCESS_DENIED:
+      return base::File::FILE_ERROR_ACCESS_DENIED;
+    case smbprovider::ERROR_TOO_MANY_OPENED:
+      return base::File::FILE_ERROR_TOO_MANY_OPENED;
+    case smbprovider::ERROR_NO_MEMORY:
+      return base::File::FILE_ERROR_NO_MEMORY;
+    case smbprovider::ERROR_NO_SPACE:
+      return base::File::FILE_ERROR_NO_SPACE;
+    case smbprovider::ERROR_NOT_A_DIRECTORY:
+      return base::File::FILE_ERROR_NOT_A_DIRECTORY;
+    case smbprovider::ERROR_INVALID_OPERATION:
+      return base::File::FILE_ERROR_INVALID_OPERATION;
+    case smbprovider::ERROR_SECURITY:
+      return base::File::FILE_ERROR_SECURITY;
+    case smbprovider::ERROR_ABORT:
+      return base::File::FILE_ERROR_ABORT;
+    case smbprovider::ERROR_NOT_A_FILE:
+      return base::File::FILE_ERROR_NOT_A_FILE;
+    case smbprovider::ERROR_NOT_EMPTY:
+      return base::File::FILE_ERROR_NOT_EMPTY;
+    case smbprovider::ERROR_INVALID_URL:
+      return base::File::FILE_ERROR_INVALID_URL;
+    case smbprovider::ERROR_IO:
+      return base::File::FILE_ERROR_IO;
+    case smbprovider::ERROR_DBUS_PARSE_FAILED:
+      // DBUS_PARSE_FAILED maps to generic ERROR_FAILED
+      LOG(ERROR) << "DBUS PARSE FAILED";
+      return base::File::FILE_ERROR_FAILED;
+    default:
+      break;
+  }
+
+  NOTREACHED();
+  return base::File::FILE_ERROR_FAILED;
+}
+
+int32_t SmbFileSystem::GetMountId() const {
+  int32_t mount_id;
+  bool result =
+      base::StringToInt(file_system_info_.file_system_id(), &mount_id);
+  DCHECK(result);
+  return mount_id;
+}
+
+SmbProviderClient* SmbFileSystem::GetSmbProviderClient() const {
+  return chromeos::DBusThreadManager::Get()->GetSmbProviderClient();
+}
+
 AbortCallback SmbFileSystem::RequestUnmount(
     const storage::AsyncFileUtil::StatusCallback& callback) {
-  NOTIMPLEMENTED();
+  GetSmbProviderClient()->Unmount(
+      GetMountId(), base::BindOnce(&SmbFileSystem::HandleRequestUnmountCallback,
+                                   weak_ptr_factory_.GetWeakPtr(), callback));
   return AbortCallback();
+}
+
+void SmbFileSystem::HandleRequestUnmountCallback(
+    const storage::AsyncFileUtil::StatusCallback& callback,
+    smbprovider::ErrorType smb_error) const {
+  callback.Run(TranslateError(smb_error));
 }
 
 AbortCallback SmbFileSystem::GetMetadata(
