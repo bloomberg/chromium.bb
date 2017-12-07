25ff47908941f937ef2b5c01259dc3aa9f73f35f
tzik <tzik@chromium.org>
2017-12-07 16:17:55 +0000

    Remove thread check in WebTaskRunner
    The DCHECK in WebTaskRunner::PostTask used to ensure that a same thread
    WTF::Function is posted to the original thread. However, after it is
    merged to base::OnceCallback, it wrongly rejects base::OnceCallback
    which may be passed through base::SingleThreadTaskRunner::PostTask.
    
    This CL removes the DCHECK to prevent the potential failure. The
    restriction itself is still checked by a callback wrapper made by
    WTF::Bind.
    
    Change-Id: I5b0cf0a6beb3b88a66a1ce89a8df466379cb9c74
    Reviewed-on: https://chromium-review.googlesource.com/813574
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522441}
    Former-commit-id: bac5ecab249b9a4ada209ec61e794f85da73c559

--- third_party/WebKit/Source/platform/WebTaskRunner.cpp
+++ third_party/WebKit/Source/platform/WebTaskRunner.cpp
@@ -132,7 +132,6 @@ void WebTaskRunner::PostDelayedTask(const WebTraceLocation& location,
 
 void WebTaskRunner::PostTask(const WebTraceLocation& location,
                              WTF::Closure task) {
-  DCHECK(RunsTasksInCurrentSequence());
   PostDelayedTask(location, std::move(task), base::TimeDelta());
 }
 
