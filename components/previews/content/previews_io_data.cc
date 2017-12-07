bd7d6054f12393fa1cc7b3d2f358bc8717ac33fd
Thanh Le <thanhdle@chromium.org>
2017-12-07 21:59:06 +0000

    Fix crash ShouldAllowPreviewAtECT.
    Check PreviewsUserData before retrieve page_id. Use page_id = 0, which
    means that log messages will not be grouped for offline page to fix
    the crash.
    
    Bug: 793017
    Change-Id: I2f0359a054b20e93df1df245a43687eb1d10ded7
    Reviewed-on: https://chromium-review.googlesource.com/815184
    Commit-Queue: Thanh Le <thanhdle@chromium.org>
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522567}
    Former-commit-id: 3f67ba69a9b86add692274c1bccbe1195b997041

--- components/previews/content/previews_io_data.cc
+++ components/previews/content/previews_io_data.cc
@@ -207,7 +207,7 @@ bool PreviewsIOData::ShouldAllowPreviewAtECT(
     PreviewsType type,
     net::EffectiveConnectionType effective_connection_type_threshold,
     const std::vector<std::string>& host_blacklist_from_server) const {
-  if (!request.url().has_host()) {
+  if (!request.url().has_host() || !PreviewsUserData::GetData(request)) {
     // Don't capture UMA on this case, as it is not important and can happen
     // when navigating to files on disk, etc.
     return false;
