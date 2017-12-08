a8c7785422a06687a67dc0a97ef584de033b6803
Jungshik Shin <jshin@chromium.org>
2017-12-08 23:46:08 +0000

    Use ICU API to map Olson timezone id to country code
    ICU's TimeZone class has an API for that. No need to use
    a hard-coded table. Saves about 10kB in the binary size.
    
    Bug: None
    Test: base_unittests --gtest_filter=Timezone*
    Change-Id: I148fa56332fb74d125121e37071eee4899f236f9
    Reviewed-on: https://chromium-review.googlesource.com/513562
    Commit-Queue: Jungshik Shin <jshin@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522922}
    Former-commit-id: 2396b834a06a9bd6851e09368db932aaf6ef5367

--- base/i18n/timezone_unittest.cc
+++ base/i18n/timezone_unittest.cc
@@ -11,10 +11,16 @@ namespace {
 
 TEST(TimezoneTest, CountryCodeForCurrentTimezone) {
   std::string country_code = CountryCodeForCurrentTimezone();
-  // On some systems (such as Android or some flavors of Linux), icu may come up
-  // empty.
+  // On some systems (such as Android or some flavors of Linux), ICU may come up
+  // empty. With https://chromium-review.googlesource.com/c/512282/ , ICU will
+  // not fail any more. See also http://bugs.icu-project.org/trac/ticket/13208 .
+  // Even with that, ICU returns '001' (world) for region-agnostic timezones
+  // such as Etc/UTC and |CountryCodeForCurrentTimezone| returns an empty
+  // string so that the next fallback can be tried by a customer.
+  // TODO(jshin): Revise this to test for actual timezones using
+  // use ScopedRestoreICUDefaultTimezone.
   if (!country_code.empty())
-    EXPECT_EQ(2U, country_code.size());
+    EXPECT_EQ(2U, country_code.size()) << "country_code = " << country_code;
 }
 
 }  // namespace
