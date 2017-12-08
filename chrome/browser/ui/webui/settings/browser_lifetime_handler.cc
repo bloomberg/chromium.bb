970b11c375a2ff3ddb7e90ed12bc618d89ddf174
Mattias Nissler <mnissler@chromium.org>
2017-12-08 11:20:17 +0000

    Handle TPM firmware update request in HandleFactoryReset()
    Adjust the logic to decide whether a factory reset is allowed when
    requesting a TPM firmware update via chrome://chrome. Factory resets
    are not allowed on enrolled devices in general, but we should allow
    them if a TPM firmware update is requested and the administrative
    policy to allow users to update TPM firmware via factory reset is
    present.
    
    BUG=chromium:792023
    TEST=Trigger TPM firmware update via chrome://chrome on enrolled device.
    
    Change-Id: Ifcb854195637284b1f9eed247382daa5ffa3ac0b
    Reviewed-on: https://chromium-review.googlesource.com/812225
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Mattias Nissler <mnissler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522760}
    Former-commit-id: ac57fe328ab67b3dd8e1e17fdacace0a4b5ba628

--- chrome/browser/ui/webui/settings/browser_lifetime_handler.cc
+++ chrome/browser/ui/webui/settings/browser_lifetime_handler.cc
@@ -12,6 +12,7 @@
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_process_platform_part.h"
 #include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
+#include "chrome/browser/chromeos/tpm_firmware_update.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "components/user_manager/user_manager.h"
@@ -58,6 +59,24 @@ void BrowserLifetimeHandler::HandleSignOutAndRestart(
 
 void BrowserLifetimeHandler::HandleFactoryReset(
     const base::ListValue* args) {
+  bool tpm_firmware_update_requested;
+  args->GetBoolean(0, &tpm_firmware_update_requested);
+  if (tpm_firmware_update_requested) {
+    chromeos::tpm_firmware_update::ShouldOfferUpdateViaPowerwash(
+        base::BindOnce([](bool offer_update) {
+          if (!offer_update)
+            return;
+
+          PrefService* prefs = g_browser_process->local_state();
+          prefs->SetBoolean(prefs::kFactoryResetRequested, true);
+          prefs->SetBoolean(prefs::kFactoryResetTPMFirmwareUpdateRequested,
+                            true);
+          prefs->CommitPendingWrite();
+          chrome::AttemptRelaunch();
+        }));
+    return;
+  }
+
   policy::BrowserPolicyConnectorChromeOS* connector =
       g_browser_process->platform_part()->browser_policy_connector_chromeos();
   bool allow_powerwash = !connector->IsEnterpriseManaged() &&
