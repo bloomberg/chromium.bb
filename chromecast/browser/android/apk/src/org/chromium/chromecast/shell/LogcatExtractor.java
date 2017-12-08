b00b19934dc07b8eb589cd6b044af31520521f0d
Simeon Anfinrud <sanfin@chromium.org>
2017-12-08 19:55:25 +0000

    [Chromecast] Fix lint warnings in LogcatExtractor.
    Refactors LogcatExtractor to separate the elision concern into
    its own class with its own unittest. Use a CircularBuffer
    Iterable object to store the raw logcat more elegantly, without
    having to use a LinkedList (with its associated Lint warnings)
    to remove the head of the list in constant time.
    
    This fixes Java Errorprone warnings for:
      * JdkObsolete
      * DefaultCharset
    
          cast_shell_junit_tests
    
    Bug: Internal b/70158727
    Bug: Internal b/70164086
    Test: cast_base_junit_tests (new suite)
    Change-Id: I6b88d2e51c994a37e134824a41a26c4d613fa293
    Reviewed-on: https://chromium-review.googlesource.com/809918
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Simeon Anfinrud <sanfin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522851}
    Former-commit-id: 927ecc5f8db7ef33f497562ba7dced65b63ceece

--- chromecast/browser/android/apk/src/org/chromium/chromecast/shell/LogcatExtractor.java
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/LogcatExtractor.java
@@ -1,126 +1,57 @@
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-package org.chromium.chromecast.shell;
 
-import android.util.Patterns;
+package org.chromium.chromecast.shell;
 
 import org.chromium.base.Log;
 import org.chromium.base.VisibleForTesting;
+import org.chromium.chromecast.base.CircularBuffer;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.nio.charset.Charset;
+import java.nio.charset.UnsupportedCharsetException;
 
 /**
  * Extracts logcat out of Android devices and elide PII sensitive info from it.
  *
  * <p>Elided information includes: Emails, IP address, MAC address, URL/domains as well as
  * Javascript console messages.
- *
- * <p>Caller provides a list of minidump files as well as an intent. This Callable will then extract
- * the most recent logcat and save a copy for each minidump.
- *
- * <p>Upon completion, each minidump + logcat pairs will be passed to the MinidumpPreparationService
- * along with the intent provided here.
  */
 class LogcatExtractor {
     private static final String TAG = "LogcatExtractor";
     private static final long HALF_SECOND = 500;
-    protected static final String EMAIL_ELISION = "XXX@EMAIL.ELIDED";
-    @VisibleForTesting
-    protected static final String URL_ELISION = "HTTP://WEBADDRESS.ELIDED";
-
-    private static final String GOOD_IRI_CHAR = "a-zA-Z0-9\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF";
-
-    private static final Pattern IP_ADDRESS = Pattern.compile(
-            "((25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\\.(25[0-5]|2[0-4]"
-            + "[0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1]"
-            + "[0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}"
-            + "|[1-9][0-9]|[0-9]))");
-
-    private static final String IRI =
-            "[" + GOOD_IRI_CHAR + "]([" + GOOD_IRI_CHAR + "\\-]{0,61}[" + GOOD_IRI_CHAR + "]){0,1}";
-
-    private static final String GOOD_GTLD_CHAR = "a-zA-Z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF";
-    private static final String GTLD = "[" + GOOD_GTLD_CHAR + "]{2,63}";
-    private static final String HOST_NAME = "(" + IRI + "\\.)+" + GTLD;
-
-    private static final Pattern DOMAIN_NAME =
-            Pattern.compile("(" + HOST_NAME + "|" + IP_ADDRESS + ")");
-
-    private static final Pattern WEB_URL =
-            Pattern.compile("(?:\\b|^)((?:(http|https|Http|Https|rtsp|Rtsp):"
-                    + "\\/\\/(?:(?:[a-zA-Z0-9\\$\\-\\_\\.\\+\\!\\*\\'\\(\\)"
-                    + "\\,\\;\\?\\&\\=]|(?:\\%[a-fA-F0-9]{2})){1,64}(?:\\:(?:[a-zA-Z0-9\\$\\-\\_"
-                    + "\\.\\+\\!\\*\\'\\(\\)\\,\\;\\?\\&\\=]|(?:\\%[a-fA-F0-9]{2})){1,25})?\\@)?)?"
-                    + "(?:" + DOMAIN_NAME + ")"
-                    + "(?:\\:\\d{1,5})?)"
-                    + "(\\/(?:(?:[" + GOOD_IRI_CHAR + "\\;\\/\\?\\:\\@\\&\\=\\#\\~"
-                    + "\\-\\.\\+\\!\\*\\'\\(\\)\\,\\_])|(?:\\%[a-fA-F0-9]{2}))*)?"
-                    + "(?:\\b|$)");
-
-    @VisibleForTesting
-    protected static final String IP_ELISION = "1.2.3.4";
-    @VisibleForTesting
-    protected static final String MAC_ELISION = "01:23:45:67:89:AB";
-
-    @VisibleForTesting
-    protected static final String CONSOLE_ELISION = "[ELIDED:CONSOLE(0)] ELIDED CONSOLE MESSAGE";
-
-    private static final Pattern MAC_ADDRESS =
-            Pattern.compile("([0-9a-fA-F]{2}[-:]+){5}[0-9a-fA-F]{2}");
-    private static final Pattern CONSOLE_MSG = Pattern.compile("\\[\\w*:CONSOLE.*\\].*");
-
-    private static final String[] CHROME_NAMESPACE = new String[] {"org.chromium.", "com.google."};
-
-    private static final String[] CAST_NAMESPACE = new String[] {"Cast.", "CastV2."};
-
-    private static final String[] SYSTEM_NAMESPACE = new String[] {"android.accessibilityservice",
-            "android.accounts", "android.animation", "android.annotation", "android.app",
-            "android.appwidget", "android.bluetooth", "android.content", "android.database",
-            "android.databinding", "android.drm", "android.gesture", "android.graphics",
-            "android.hardware", "android.inputmethodservice", "android.location", "android.media",
-            "android.mtp", "android.net", "android.nfc", "android.opengl", "android.os",
-            "android.preference", "android.print", "android.printservice", "android.provider",
-            "android.renderscript", "android.sax", "android.security", "android.service",
-            "android.speech", "android.support", "android.system", "android.telecom",
-            "android.telephony", "android.test", "android.text", "android.transition",
-            "android.util", "android.view", "android.webkit", "android.widget", "com.android.",
-            "dalvik.", "java.", "javax.", "org.apache.", "org.json.", "org.w3c.dom.", "org.xml.",
-            "org.xmlpull."};
-
-    private static final Pattern JAVA_FILE = Pattern.compile(".java:[0-9]+$");
 
     public static String getElidedLogcat() throws IOException, InterruptedException {
-        List<String> rawLogcat = getLogcat();
-        List<String> elidedLogcat = Collections.unmodifiableList(processLogcat(rawLogcat));
-        return joinLines(elidedLogcat);
+        return getElidedLogcat(getLogcat());
     }
 
-    private static List<String> getLogcat() throws IOException, InterruptedException {
-        LinkedList<String> rawLogcat = new LinkedList<String>();
+    @VisibleForTesting
+    static String getElidedLogcat(Iterable<String> rawLogcat) {
+        StringBuilder builder = new StringBuilder();
+        for (String line : rawLogcat) {
+            builder.append(LogcatElision.elide(line));
+            builder.append("\n");
+        }
+        return builder.toString();
+    }
+
+    private static Iterable<String> getLogcat() throws IOException, InterruptedException {
+        CircularBuffer<String> rawLogcat = new CircularBuffer<>(BuildConfig.LOGCAT_SIZE);
         String logLn = null;
         Integer exitValue = null;
 
         Process p = Runtime.getRuntime().exec("logcat -d");
         BufferedReader bReader = null;
         try {
-            bReader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            bReader = new BufferedReader(
+                    new InputStreamReader(p.getInputStream(), Charset.forName("UTF-8")));
             while (exitValue == null) {
                 while ((logLn = bReader.readLine()) != null) {
-                    // Add each new string to the end of the queue.
+                    // Add each new string to the end of the buffer.
                     rawLogcat.add(logLn);
-                    if (rawLogcat.size() > BuildConfig.LOGCAT_SIZE) {
-                        // Remove the head of the queue when it gets too large.
-                        rawLogcat.remove();
-                    }
                 }
                 try {
                     exitValue = p.exitValue();
@@ -134,130 +65,14 @@ class LogcatExtractor {
                 throw new IOException(msg);
             }
             return rawLogcat;
+        } catch (UnsupportedCharsetException e) {
+            // Should never happen; all Java implementations are required to support UTF-8.
+            Log.wtf(TAG, "UTF-8 not supported", e);
+            return rawLogcat;
         } finally {
             if (bReader != null) {
                 bReader.close();
             }
         }
-    }
-
-    private static String joinLines(List<String> elidedLogcat) {
-        StringBuilder sBuilder = new StringBuilder();
-        for (String ln : elidedLogcat) {
-            sBuilder.append(ln);
-            sBuilder.append("\n");
-        }
-        return sBuilder.toString();
-    }
-
-    @VisibleForTesting
-    protected static List<String> processLogcat(List<String> rawLogcat) {
-        List<String> out = new ArrayList<String>(rawLogcat.size());
-        for (String ln : rawLogcat) {
-            ln = elideEmail(ln);
-            ln = elideUrl(ln);
-            ln = elideIp(ln);
-            ln = elideMac(ln);
-            ln = elideConsole(ln);
-            out.add(ln);
-        }
-        return out;
-    }
-    /**
-     * Elides any emails in the specified {@link String} with {@link #EMAIL_ELISION}.
-     *
-     * @param original String potentially containing emails.
-     * @return String with elided emails.
-     */
-    @VisibleForTesting
-    protected static String elideEmail(String original) {
-        return Patterns.EMAIL_ADDRESS.matcher(original).replaceAll(EMAIL_ELISION);
-    }
-    /**
-     * Elides any URLs in the specified {@link String} with
-     * {@link #URL_ELISION}.
-     *
-     * @param original String potentially containing URLs.
-     * @return String with elided URLs.
-     */
-    @VisibleForTesting
-    protected static String elideUrl(String original) {
-        StringBuilder buffer = new StringBuilder(original);
-        Matcher matcher = WEB_URL.matcher(buffer);
-        int start = 0;
-        while (matcher.find(start)) {
-            start = matcher.start();
-            int end = matcher.end();
-            String url = buffer.substring(start, end);
-            if (!likelyToBeChromeNamespace(url) && !likelyToBeSystemNamespace(url)
-                    && !likelyToBeCastNamespace(url) && !likelyToBeJavaFile(url)) {
-                buffer.replace(start, end, URL_ELISION);
-                end = start + URL_ELISION.length();
-                matcher = WEB_URL.matcher(buffer);
-            }
-            start = end;
-        }
-        return buffer.toString();
-    }
-
-    public static boolean likelyToBeChromeNamespace(String url) {
-        for (String ns : CHROME_NAMESPACE) {
-            if (url.startsWith(ns)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static boolean likelyToBeSystemNamespace(String url) {
-        for (String ns : SYSTEM_NAMESPACE) {
-            if (url.startsWith(ns)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static boolean likelyToBeCastNamespace(String url) {
-        for (String ns : CAST_NAMESPACE) {
-            if (url.startsWith(ns)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static boolean likelyToBeJavaFile(String url) {
-        return JAVA_FILE.matcher(url).find();
-    }
-    /**
-     * Elides any IP addresses in the specified {@link String} with {@link #IP_ELISION}.
-     *
-     * @param original String potentially containing IPs.
-     * @return String with elided IPs.
-     */
-    @VisibleForTesting
-    protected static String elideIp(String original) {
-        return Patterns.IP_ADDRESS.matcher(original).replaceAll(IP_ELISION);
-    }
-    /**
-     * Elides any MAC addresses in the specified {@link String} with {@link #MAC_ELISION}.
-     *
-     * @param original String potentially containing MACs.
-     * @return String with elided MACs.
-     */
-    @VisibleForTesting
-    protected static String elideMac(String original) {
-        return MAC_ADDRESS.matcher(original).replaceAll(MAC_ELISION);
-    }
-    /**
-     * Elides any console messages in the specified {@link String} with {@link #CONSOLE_ELISION}.
-     *
-     * @param original String potentially containing console messages.
-     * @return String with elided console messages.
-     */
-    @VisibleForTesting
-    protected static String elideConsole(String original) {
-        return CONSOLE_MSG.matcher(original).replaceAll(CONSOLE_ELISION);
     }
 }
--- chromecast/browser/android/junit/src/org/chromium/chromecast/shell/LogcatExtractorUnitTest.java
+++ chromecast/browser/android/junit/src/org/chromium/chromecast/shell/LogcatExtractorUnitTest.java
@@ -4,145 +4,57 @@
 
 package org.chromium.chromecast.shell;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.annotation.Config;
+import org.junit.runners.BlockJUnit4ClassRunner;
 
-import org.chromium.testing.local.LocalRobolectricTestRunner;
-
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+// TODO(sandv): Add test cases as need arises.
 /**
- * junit tests for {@link LogcatExtractor}.
+ * Tests for LogcatExtractor.
+ *
+ * Full testing of elision of PII is done in LogcatElisionUnitTest.
  */
-@RunWith(LocalRobolectricTestRunner.class)
-@Config(manifest = Config.NONE)
+@RunWith(BlockJUnit4ClassRunner.class)
 public class LogcatExtractorUnitTest {
-    private static final int MAX_LINES = 5;
-
     @Test
-    public void testElideEmail() {
-        String original = "email me at someguy@mailservice.com";
-        String expected = "email me at XXX@EMAIL.ELIDED";
-        assertEquals(expected, LogcatExtractor.elideEmail(original));
+    public void testEmptyLogcat() {
+        List<String> lines = new ArrayList<String>();
+        assertEquals("", LogcatExtractor.getElidedLogcat(lines));
     }
 
     @Test
-    public void testElideUrl() {
-        String original = "file bugs at crbug.com";
-        String expected = "file bugs at HTTP://WEBADDRESS.ELIDED";
-        assertEquals(expected, LogcatExtractor.elideUrl(original));
+    public void testLogcatOfEmptyString() {
+        List<String> lines = Arrays.asList("");
+        assertEquals("\n", LogcatExtractor.getElidedLogcat(lines));
     }
 
     @Test
-    public void testElideUrl2() {
-        String original = "exception at org.chromium.chrome.browser.crash.LogcatExtractorUnitTest";
-        assertEquals(original, LogcatExtractor.elideUrl(original));
+    public void testJoinsLines() {
+        List<String> lines = Arrays.asList("a", "b", "c");
+        assertEquals("a\nb\nc\n", LogcatExtractor.getElidedLogcat(lines));
     }
 
     @Test
-    public void testElideUrl3() {
-        String original = "file bugs at crbug.com or code.google.com";
-        String expected = "file bugs at HTTP://WEBADDRESS.ELIDED or HTTP://WEBADDRESS.ELIDED";
-        assertEquals(expected, LogcatExtractor.elideUrl(original));
-    }
-
-    @Test
-    public void testElideUrl4() {
-        String original = "test shorturl.com !!!";
-        String expected = "test HTTP://WEBADDRESS.ELIDED !!!";
-        assertEquals(expected, LogcatExtractor.elideUrl(original));
-    }
-
-    @Test
-    public void testElideUrl5() {
-        String original = "test just.the.perfect.len.url !!!";
-        String expected = "test HTTP://WEBADDRESS.ELIDED !!!";
-        assertEquals(expected, LogcatExtractor.elideUrl(original));
-    }
-
-    @Test
-    public void testElideUrl6() {
-        String original = "test a.very.very.very.very.very.long.url !!!";
-        String expected = "test HTTP://WEBADDRESS.ELIDED !!!";
-        assertEquals(expected, LogcatExtractor.elideUrl(original));
-    }
-
-    @Test
-    public void testElideUrl7() {
-        String original = " at android.content.Intent \n at java.util.ArrayList";
-        assertEquals(original, LogcatExtractor.elideUrl(original));
-    }
-
-    @Test
-    public void testDontElideFileSuffixes() {
-        String original = "chromium_android_linker.so";
-        assertEquals(original, LogcatExtractor.elideUrl(original));
-    }
-
-    @Test
-    public void testElideIp() {
-        String original = "traceroute 127.0.0.1";
-        String expected = "traceroute 1.2.3.4";
-        assertEquals(expected, LogcatExtractor.elideIp(original));
-    }
-
-    @Test
-    public void testElideMac1() {
-        String original = "MAC: AB-AB-AB-AB-AB-AB";
-        String expected = "MAC: 01:23:45:67:89:AB";
-        assertEquals(expected, LogcatExtractor.elideMac(original));
-    }
-
-    @Test
-    public void testElideMac2() {
-        String original = "MAC: AB:AB:AB:AB:AB:AB";
-        String expected = "MAC: 01:23:45:67:89:AB";
-        assertEquals(expected, LogcatExtractor.elideMac(original));
-    }
-
-    @Test
-    public void testElideConsole() {
-        String original = "I/chromium(123): [INFO:CONSOLE(2)] hello!";
-        String expected = "I/chromium(123): [ELIDED:CONSOLE(0)] ELIDED CONSOLE MESSAGE";
-        assertEquals(expected, LogcatExtractor.elideConsole(original));
-    }
-
-    @Test
-    public void testLogTagNotElided() {
-        List<String> original = Arrays.asList(new String[] {"I/cr_FooBar(123): Some message"});
-        assertEquals(original, LogcatExtractor.processLogcat(original));
-    }
-
-    @Test
-    public void testLogCastNotElided() {
-        String original = "09-08 11:52:12.569  4406  4406 I chromium: "
-                + "Cast.Discovery.Mdns.Request.AppId.In=25";
-        assertEquals(original, LogcatExtractor.elideUrl(original));
-    }
-
-    @Test
-    public void testLogAndroidNotElided() {
-        String original = "09-08 11:54:38.053  4406  4406 D cr_AvSettingsAndroid:"
-                + "[AvSettingsAndroid.java:180] HDMI plug update: "
-                + "action=android.media.action.HDMI_AUDIO_PLUG, plug=1";
-        assertEquals(original, LogcatExtractor.elideUrl(original));
-    }
-
-    @Test
-    public void testLogJavaNotElided() {
-        String original = "09-08 11:54:38.053  4406  4406 D cr_AvSettingsAndroid: "
-                + " [AvSettingsAndroid.java:187] Max channel count = 8";
-        assertEquals(original, LogcatExtractor.elideUrl(original));
-    }
-
-    @Test
-    public void testLogCcNotElided() {
-        String original = "09-08 11:59:12.335  4406  4443 I chromium: "
-                + "[4406:4443:INFO:wifi_util.cc(113)]   No peers:";
-        assertEquals(original, LogcatExtractor.elideUrl(original));
+    public void testElidesPii() {
+        List<String> lines = Arrays.asList("email me at someguy@mailservice.com",
+                "file bugs at crbug.com", "at android.content.Intent", "at java.util.ArrayList",
+                "mac address: AB-AB-AB-AB-AB-AB");
+        String elided = LogcatExtractor.getElidedLogcat(lines);
+        // PII like email addresses, web addresses, and MAC addresses are elided.
+        assertThat(elided, not(containsString("someguy@mailservice.com")));
+        assertThat(elided, not(containsString("crbug.com")));
+        assertThat(elided, not(containsString("AB-AB-AB-AB-AB-AB")));
+        // Tags for class names relevant for debugging should not be elided.
+        assertThat(elided, containsString("android.content.Intent"));
+        assertThat(elided, containsString("java.util.ArrayList"));
     }
 }
