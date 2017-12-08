0638ad9c0c1f69f28beddf7135c6f322ccac4acf
Jungshik Shin <jshin@chromium.org>
2017-12-08 23:46:08 +0000

    Use ICU API to map Olson timezone id to country code
    ICU's TimeZone class has an API for that. No need to use
    a hard-coded table. Saves about 10kB in the binary size.
    
    Bug: None
    Test: base_unittests --gtest_filter=Timezone*
    Change-Id: I148fa56332fb74d125121e37071eee4899f236f9
    Reviewed-on: https://chromium-review.googlesource.com/513562
    Commit-Queue: Jungshik Shin <jshin@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522922}
    Former-commit-id: 2396b834a06a9bd6851e09368db932aaf6ef5367

--- base/i18n/timezone.h
+++ base/i18n/timezone.h
@@ -11,9 +11,12 @@
 
 namespace base {
 
-// Checks the system timezone and turns it into a two-character ASCII country
-// code. This may fail (for example, it will always fail on Android), in which
-// case it will return an empty string.
+// Checks the system timezone and turns it into a two-character ISO 3166 country
+// code. This may fail (for example, it used to always fail on Android), in
+// which case it will return an empty string. It'll also return an empty string
+// when the timezone is Etc/UTC or Etc/UCT, but will return 'GB" for Etc/GMT
+// because people in the UK tends to select Etc/GMT by mistake instead of
+// Europe/London (British Time).
 BASE_I18N_EXPORT std::string CountryCodeForCurrentTimezone();
 
 }  // namespace base
