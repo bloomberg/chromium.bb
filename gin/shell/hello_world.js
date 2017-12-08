428fe50c311eed4fae1a4cc16762cc73c32e2591
Yuzhu Shen <yzshen@chromium.org>
2017-12-08 23:37:45 +0000

    Revert "Remove AMD-style module support from gin/."
    This reverts commit e66669220346aa7399ba0df3c13bb5d46bd27b76 [formerly c1ab49a9e23036de2f94da601782da9985a9a481].
    
    Reason for revert: Likely the cause of
    RunnerTest.Run failure in gin_unittests: Check failed: (isolate->snapshot_blob()) == nullptr.
    
    Original change's description:
    > Remove AMD-style module support from gin/.
    > 
    > Bug: 718047
    > Change-Id: If480fff65a2e5993ed5143eede8354f890de3a3b
    > Reviewed-on: https://chromium-review.googlesource.com/812172
    > Commit-Queue: Yuzhu Shen <yzshen@chromium.org>
    > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#522850}
    
    TBR=yzshen@chromium.org,jochen@chromium.org
    
    Change-Id: I4a6efb21dd7972994a06ddab1316fa43e5d3e060
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 718047, 793480
    Reviewed-on: https://chromium-review.googlesource.com/817373
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Commit-Queue: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522921}
    Former-commit-id: a8f25f1e37e561d17222773d869ca460e00d8fe1

--- gin/shell/hello_world.js
+++ gin/shell/hello_world.js
@@ -2,4 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-log("Hello World");
+define(["console"], function(console) {
+  console.log("Hello World");
+});
---
+++ gin/test/file_runner.cc
@@ -0,0 +1,94 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "gin/test/file_runner.h"
+
+#include "base/files/file_util.h"
+#include "base/path_service.h"
+#include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
+#include "base/threading/thread_task_runner_handle.h"
+#include "gin/array_buffer.h"
+#include "gin/converter.h"
+#include "gin/modules/console.h"
+#include "gin/modules/module_registry.h"
+#include "gin/public/context_holder.h"
+#include "gin/public/isolate_holder.h"
+#include "gin/test/file.h"
+#include "gin/test/gc.h"
+#include "gin/test/gtest.h"
+#include "gin/try_catch.h"
+#include "gin/v8_initializer.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace gin {
+
+namespace {
+
+std::vector<base::FilePath> GetModuleSearchPaths() {
+  std::vector<base::FilePath> search_paths(2);
+  PathService::Get(base::DIR_SOURCE_ROOT, &search_paths[0]);
+  PathService::Get(base::DIR_EXE, &search_paths[1]);
+  search_paths[1] = search_paths[1].AppendASCII("gen");
+  return search_paths;
+}
+
+}  // namespace
+
+FileRunnerDelegate::FileRunnerDelegate()
+    : ModuleRunnerDelegate(GetModuleSearchPaths()) {
+  AddBuiltinModule(Console::kModuleName, Console::GetModule);
+  AddBuiltinModule(GTest::kModuleName, GTest::GetModule);
+  AddBuiltinModule(GC::kModuleName, GC::GetModule);
+  AddBuiltinModule(File::kModuleName, File::GetModule);
+}
+
+FileRunnerDelegate::~FileRunnerDelegate() = default;
+
+void FileRunnerDelegate::UnhandledException(ShellRunner* runner,
+                                            TryCatch& try_catch) {
+  ModuleRunnerDelegate::UnhandledException(runner, try_catch);
+  FAIL() << try_catch.GetStackTrace();
+}
+
+void RunTestFromFile(const base::FilePath& path, FileRunnerDelegate* delegate,
+                     bool run_until_idle) {
+  ASSERT_TRUE(base::PathExists(path)) << path.LossyDisplayName();
+  std::string source;
+  ASSERT_TRUE(ReadFileToString(path, &source));
+
+  base::test::ScopedTaskEnvironment scoped_task_environment;
+
+#ifdef V8_USE_EXTERNAL_STARTUP_DATA
+  gin::V8Initializer::LoadV8Snapshot();
+  gin::V8Initializer::LoadV8Natives();
+#ifdef USE_V8_CONTEXT_SNAPSHOT
+  gin::V8Initializer::LoadV8ContextSnapshot();
+#endif  // USE_V8_CONTEXT_SNAPSHOT
+#endif  // V8_USE_EXTERNAL_STARTUP_DATA
+
+  gin::IsolateHolder::Initialize(gin::IsolateHolder::kStrictMode,
+                                 gin::IsolateHolder::kStableV8Extras,
+                                 gin::ArrayBufferAllocator::SharedInstance());
+
+  gin::IsolateHolder instance(base::ThreadTaskRunnerHandle::Get());
+  gin::ShellRunner runner(delegate, instance.isolate());
+  {
+    gin::Runner::Scope scope(&runner);
+    instance.isolate()->SetCaptureStackTraceForUncaughtExceptions(true);
+    runner.Run(source, path.AsUTF8Unsafe());
+
+    if (run_until_idle) {
+      base::RunLoop().RunUntilIdle();
+    } else {
+      base::RunLoop().Run();
+    }
+
+    v8::Local<v8::Value> result = runner.global()->Get(
+        StringToSymbol(runner.GetContextHolder()->isolate(), "result"));
+    EXPECT_EQ("PASS", V8ToString(result));
+  }
+}
+
+}  // namespace gin
