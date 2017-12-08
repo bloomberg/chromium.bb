a25d893640c9ff3ccc82eea0a2a58b4ad881dd32
Brian Salomon <bsalomon@google.com>
2017-12-08 17:18:53 +0000

    Update ganesh_surface_provider to pass GrGLInterface as sk_sp
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I32edb0a7661e80d52beab94450c4631a87a8afb0
    Reviewed-on: https://chromium-review.googlesource.com/817615
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Brian Salomon <bsalomon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522807}
    Former-commit-id: ed28bea93c7f654bb95f94e3c23a7b21a4637286

--- chrome/browser/vr/ganesh_surface_provider.cc
+++ chrome/browser/vr/ganesh_surface_provider.cc
@@ -26,7 +26,7 @@ GaneshSurfaceProvider::GaneshSurfaceProvider() {
   sk_sp<const GrGLInterface> gr_interface =
       gl::init::CreateGrGLInterface(gl_version_info);
   DCHECK(gr_interface.get());
-  gr_context_ = GrContext::MakeGL(gr_interface.get());
+  gr_context_ = GrContext::MakeGL(std::move(gr_interface));
   DCHECK(gr_context_.get());
   glGetIntegerv(GL_FRAMEBUFFER_BINDING, &main_fbo_);
 }
