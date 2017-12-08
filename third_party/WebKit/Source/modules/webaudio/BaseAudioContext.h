20b0ed12bbe35fd5c90c4b46efc4631e96ba9562
Yuki Shiino <yukishiino@chromium.org>
2017-12-08 16:41:23 +0000

    v8binding: Fixes wrapper-tracing of BaseAudioContext's callbacks.
    BaseAudioContext has no parent object that does wrapper-tracing,
    and author script does not hold the V8 wrapper object in general.
    The only thing that makes the BaseAudioContext alive is a closure
    posted to a task queue, which doesn't support wrapper-tracing.
    
    Thus, this patch uses persistent handles to make the callback
    functions in BaseAudioContext alive.
    
    This is acceptable because it's guaranteed that each callback will
    be removed once their task gets done regardless of whether it's
    successful or not.
    
    Bug: 791436
    Change-Id: Ibfd16c6806734a562c3a59b263bc95072fa0eb2c
    Reviewed-on: https://chromium-review.googlesource.com/816642
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522795}
    Former-commit-id: 182dd1a3b8d8a57d9eb796b6027bd6ed2009a2a7

--- third_party/WebKit/Source/modules/webaudio/BaseAudioContext.h
+++ third_party/WebKit/Source/modules/webaudio/BaseAudioContext.h
@@ -30,6 +30,8 @@
 #include "bindings/core/v8/ActiveScriptWrappable.h"
 #include "bindings/core/v8/ScriptPromise.h"
 #include "bindings/core/v8/ScriptPromiseResolver.h"
+#include "bindings/modules/v8/v8_decode_error_callback.h"
+#include "bindings/modules/v8/v8_decode_success_callback.h"
 #include "core/dom/PausableObject.h"
 #include "core/dom/events/EventListener.h"
 #include "core/html/media/AutoplayPolicy.h"
@@ -42,7 +44,6 @@
 #include "modules/webaudio/DeferredTaskHandler.h"
 #include "modules/webaudio/IIRFilterNode.h"
 #include "platform/audio/AudioBus.h"
-#include "platform/bindings/TraceWrapperMember.h"
 #include "platform/heap/Handle.h"
 #include "platform/wtf/HashSet.h"
 #include "platform/wtf/Threading.h"
@@ -111,9 +112,7 @@ class MODULES_EXPORT BaseAudioContext
 
   ~BaseAudioContext() override;
 
-  virtual void Trace(blink::Visitor*);
-
-  virtual void TraceWrappers(const ScriptWrappableVisitor*) const;
+  void Trace(blink::Visitor*) override;
 
   // Is the destination node initialized and ready to handle audio?
   bool IsDestinationInitialized() const {
@@ -494,8 +493,18 @@ class MODULES_EXPORT BaseAudioContext
   // Hold references to the |decodeAudioData| callbacks so that they
   // don't get prematurely GCed by v8 before |decodeAudioData| returns
   // and calls them.
-  HeapVector<TraceWrapperMember<V8DecodeSuccessCallback>> success_callbacks_;
-  HeapVector<TraceWrapperMember<V8DecodeErrorCallback>> error_callbacks_;
+  //
+  // Note that BaseAudioContext has no parent object that does wrapper-tracing,
+  // and author script does not hold the V8 wrapper object in general. The only
+  // thing that makes the BaseAudioContext alive is a closure posted to a task
+  // queue, which doesn't support wrapper-tracing. So this object holds the
+  // callback functions as persistent handles. This is acceptable because it's
+  // guaranteed that each callback will be removed once their task gets done
+  // regardless of whether it's successful or not.
+  Vector<V8DecodeSuccessCallback::Persistent<V8DecodeSuccessCallback>>
+      success_callbacks_;
+  Vector<V8DecodeErrorCallback::Persistent<V8DecodeErrorCallback>>
+      error_callbacks_;
 
   // When a context is closed, the sample rate is cleared.  But decodeAudioData
   // can be called after the context has been closed and it needs the sample
