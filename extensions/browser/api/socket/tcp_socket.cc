2c53b526ebb85df4a9c37cc4f679625a67aca8b4
rhalavati@google.com <rhalavati@google.com>
2017-12-08 09:06:19 +0000

    Remove default traffic annotation value from TCPClientSocket::Write().
    The default network traffic annotation value is removed from the
    TCPClientSocket::Write() function, propagating the required value to
    other classes and tests.
    
    Bug: 656607
    Change-Id: I960b511edfe4cb8d09e64c9804591098b2e1e381
    Reviewed-on: https://chromium-review.googlesource.com/809111
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#522742}
    Former-commit-id: ac28b5d25ae942bea4c5d5110cf33b3e159f5827

--- extensions/browser/api/socket/tcp_socket.cc
+++ extensions/browser/api/socket/tcp_socket.cc
@@ -16,6 +16,7 @@
 #include "net/base/rand_callback.h"
 #include "net/log/net_log_source.h"
 #include "net/socket/tcp_client_socket.h"
+#include "net/traffic_annotation/network_traffic_annotation.h"
 
 namespace extensions {
 
@@ -269,12 +270,14 @@ Socket::SocketType TCPSocket::GetSocketType() const { return Socket::TYPE_TCP; }
 int TCPSocket::WriteImpl(net::IOBuffer* io_buffer,
                          int io_buffer_size,
                          const net::CompletionCallback& callback) {
+  // TODO(crbug.com/656607): Add proper annotation.
   if (socket_mode_ != CLIENT)
     return net::ERR_FAILED;
   else if (!socket_.get() || !IsConnected())
     return net::ERR_SOCKET_NOT_CONNECTED;
   else
-    return socket_->Write(io_buffer, io_buffer_size, callback);
+    return socket_->Write(io_buffer, io_buffer_size, callback,
+                          NO_TRAFFIC_ANNOTATION_BUG_656607);
 }
 
 void TCPSocket::RefreshConnectionStatus() {
