07d827b0858dd3d10259ea736283f1408920eac8
Tim Brown <timbrown@chromium.org>
2017-12-11 22:19:11 +0000

    Update distro histogram
    For supported distros, group the versions together so that we can more
    easily see them together (include some spare ID range for each distro
    too). Also add in a couple of future versions so that we don't have to
    keep on updating the metrics.
    
    Downside is that the old histogram values are deprecated and will take
    a while to settle on the new values as newer versions roll out.
    
    Bug: 724244
    Change-Id: I74f1c704529856ccb946178f035e8baed5212ecc
    Reviewed-on: https://chromium-review.googlesource.com/818203
    Commit-Queue: Tim Brown <timbrown@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523230}
    Former-commit-id: 9ae5e682de79abafd24549e3aed76ce523562629

--- chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
+++ chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
@@ -71,26 +71,52 @@ namespace {
 // enums must never be renumbered or deleted and reused.
 enum UMALinuxDistro {
   UMA_LINUX_DISTRO_UNKNOWN = 0,
-  UMA_LINUX_DISTRO_UBUNTU_OTHER = 1,
-  UMA_LINUX_DISTRO_UBUNTU_14_04 = 2,
-  UMA_LINUX_DISTRO_UBUNTU_16_04 = 3,
-  UMA_LINUX_DISTRO_UBUNTU_16_10 = 4,
-  UMA_LINUX_DISTRO_UBUNTU_17_04 = 5,
-  UMA_LINUX_DISTRO_DEBIAN_OTHER = 6,
-  UMA_LINUX_DISTRO_DEBIAN_8 = 7,
-  UMA_LINUX_DISTRO_OPENSUSE_OTHER = 8,
-  UMA_LINUX_DISTRO_OPENSUSE_LEAP_42_2 = 9,
-  UMA_LINUX_DISTRO_FEDORA_OTHER = 10,
-  UMA_LINUX_DISTRO_FEDORA_24 = 11,
-  UMA_LINUX_DISTRO_FEDORA_25 = 12,
-  UMA_LINUX_DISTRO_FEDORA_26 = 13,
-  UMA_LINUX_DISTRO_DEBIAN_9 = 14,
+  UMA_LINUX_DISTRO_UBUNTU_OTHER_DEPRECATED = 1,
+  UMA_LINUX_DISTRO_UBUNTU_14_04_DEPRECATED = 2,
+  UMA_LINUX_DISTRO_UBUNTU_16_04_DEPRECATED = 3,
+  UMA_LINUX_DISTRO_UBUNTU_16_10_DEPRECATED = 4,
+  UMA_LINUX_DISTRO_UBUNTU_17_04_DEPRECATED = 5,
+  UMA_LINUX_DISTRO_DEBIAN_OTHER_DEPRECATED = 6,
+  UMA_LINUX_DISTRO_DEBIAN_8_DEPRECATED = 7,
+  UMA_LINUX_DISTRO_OPENSUSE_OTHER_DEPRECATED = 8,
+  UMA_LINUX_DISTRO_OPENSUSE_LEAP_42_2_DEPRECATED = 9,
+  UMA_LINUX_DISTRO_FEDORA_OTHER_DEPRECATED = 10,
+  UMA_LINUX_DISTRO_FEDORA_24_DEPRECATED = 11,
+  UMA_LINUX_DISTRO_FEDORA_25_DEPRECATED = 12,
+  UMA_LINUX_DISTRO_FEDORA_26_DEPRECATED = 13,
+  UMA_LINUX_DISTRO_DEBIAN_9_DEPRECATED = 14,
   UMA_LINUX_DISTRO_ARCH = 15,
   UMA_LINUX_DISTRO_CENTOS = 16,
   UMA_LINUX_DISTRO_ELEMENTARY = 17,
   UMA_LINUX_DISTRO_MINT = 18,
   UMA_LINUX_DISTRO_RHEL = 19,
   UMA_LINUX_DISTRO_SUSE_ENTERPRISE = 20,
+  // Debian
+  UMA_LINUX_DISTRO_DEBIAN_OTHER = 50,
+  UMA_LINUX_DISTRO_DEBIAN_8 = 51,
+  UMA_LINUX_DISTRO_DEBIAN_9 = 52,
+  UMA_LINUX_DISTRO_DEBIAN_10 = 53,
+  // Fedora
+  UMA_LINUX_DISTRO_FEDORA_OTHER = 100,
+  UMA_LINUX_DISTRO_FEDORA_24 = 101,
+  UMA_LINUX_DISTRO_FEDORA_25 = 102,
+  UMA_LINUX_DISTRO_FEDORA_26 = 103,
+  UMA_LINUX_DISTRO_FEDORA_27 = 104,
+  UMA_LINUX_DISTRO_FEDORA_28 = 105,
+  // openSUSE
+  UMA_LINUX_DISTRO_OPENSUSE_OTHER = 150,
+  UMA_LINUX_DISTRO_OPENSUSE_LEAP_42_2 = 151,
+  UMA_LINUX_DISTRO_OPENSUSE_LEAP_42_3 = 152,
+  UMA_LINUX_DISTRO_OPENSUSE_LEAP_15 = 153,
+  // Ubuntu
+  UMA_LINUX_DISTRO_UBUNTU_OTHER = 200,
+  UMA_LINUX_DISTRO_UBUNTU_14_04 = 201,
+  UMA_LINUX_DISTRO_UBUNTU_16_04 = 202,
+  UMA_LINUX_DISTRO_UBUNTU_16_10 = 203,
+  UMA_LINUX_DISTRO_UBUNTU_17_04 = 204,
+  UMA_LINUX_DISTRO_UBUNTU_17_10 = 205,
+  UMA_LINUX_DISTRO_UBUNTU_18_04 = 206,
+  UMA_LINUX_DISTRO_UBUNTU_18_10 = 207,
   // Note: Add new distros to the list above this line, and update LinuxDistro
   // in tools/metrics/histograms/enums.xml accordingly.
   UMA_LINUX_DISTRO_MAX
@@ -231,15 +257,26 @@ void RecordLinuxDistro() {
             distro_result = UMA_LINUX_DISTRO_UBUNTU_16_10;
           } else if (version.CompareToWildcardString("17.04.*") == 0) {
             distro_result = UMA_LINUX_DISTRO_UBUNTU_17_04;
+          } else if (version.CompareToWildcardString("17.10.*") == 0) {
+            distro_result = UMA_LINUX_DISTRO_UBUNTU_17_10;
+          } else if (version.CompareToWildcardString("18.04.*") == 0) {
+            distro_result = UMA_LINUX_DISTRO_UBUNTU_18_04;
+          } else if (version.CompareToWildcardString("18.10.*") == 0) {
+            distro_result = UMA_LINUX_DISTRO_UBUNTU_18_10;
           }
         }
       }
     } else if (distro_tokens[0] == "openSUSE") {
       // Format: openSUSE Leap RR.R
       distro_result = UMA_LINUX_DISTRO_OPENSUSE_OTHER;
-      if (distro_tokens.size() >= 3 && distro_tokens[1] == "Leap" &&
-          distro_tokens[2] == "42.2") {
-        distro_result = UMA_LINUX_DISTRO_OPENSUSE_LEAP_42_2;
+      if (distro_tokens.size() >= 3 && distro_tokens[1] == "Leap") {
+        if (distro_tokens[2] == "42.2") {
+          distro_result = UMA_LINUX_DISTRO_OPENSUSE_LEAP_42_2;
+        } else if (distro_tokens[2] == "42.3") {
+          distro_result = UMA_LINUX_DISTRO_OPENSUSE_LEAP_42_3;
+        } else if (distro_tokens[2] == "15") {
+          distro_result = UMA_LINUX_DISTRO_OPENSUSE_LEAP_15;
+        }
       }
     } else if (distro_tokens[0] == "Debian") {
       // Format: Debian GNU/Linux R.P (<codename>)
@@ -252,6 +289,8 @@ void RecordLinuxDistro() {
             distro_result = UMA_LINUX_DISTRO_DEBIAN_8;
           } else if (version.CompareToWildcardString("9.*")) {
             distro_result = UMA_LINUX_DISTRO_DEBIAN_9;
+          } else if (version.CompareToWildcardString("10.*")) {
+            distro_result = UMA_LINUX_DISTRO_DEBIAN_10;
           }
         }
       }
@@ -265,6 +304,10 @@ void RecordLinuxDistro() {
           distro_result = UMA_LINUX_DISTRO_FEDORA_25;
         } else if (distro_tokens[2] == "26") {
           distro_result = UMA_LINUX_DISTRO_FEDORA_26;
+        } else if (distro_tokens[2] == "27") {
+          distro_result = UMA_LINUX_DISTRO_FEDORA_27;
+        } else if (distro_tokens[2] == "28") {
+          distro_result = UMA_LINUX_DISTRO_FEDORA_28;
         }
       }
     } else if (distro_tokens[0] == "Arch") {
@@ -292,8 +335,7 @@ void RecordLinuxDistro() {
     }
   }
 
-  UMA_HISTOGRAM_ENUMERATION("Linux.Distro", distro_result,
-                            UMA_LINUX_DISTRO_MAX);
+  UMA_HISTOGRAM_SPARSE_SLOWLY("Linux.Distro", distro_result);
 }
 #endif  // defined(OS_LINUX) && !defined(OS_CHROMEOS)
 
