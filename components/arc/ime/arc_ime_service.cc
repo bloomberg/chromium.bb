2a66bf958a125fe9fbd3eabcdcab176a5dd0838c
Blake O'Hare <blakeo@chromium.org>
2017-12-11 06:42:43 +0000

    Replace ArcImeService's usage of OnKeyboardBoundsChanging
    OnKeyboardBoundsChanging is being split into several more specific
    methods that have a stronger contract of how they are being used by the
    implementer. More information about why this is being done is in the
    associated bug.
    
    OnKeyboardAppearanceChanging is called when any time the keyboard's
    bounds or availability changes and has a struct as its parameter that
    includes several properties of the keyboard appearance.
    
    Currently the IME Bridge does not support any additional params other
    than the bounds and then uses the bounds to check if the keyboard is
    available by checking for empty bounds. I've opened b/70251261 to
    change this, since empty bounds can still mean the keyboard is
    available. The KeyboardStateDescriptor struct contains a boolean that
    indicates if the keyboard is truly available for use. This value needs
    to be plumbed into the ime bridge.
    
    Bug: 786290
    Change-Id: I256c348418ec792879e2687064b1a16468432908
    Reviewed-on: https://chromium-review.googlesource.com/778740
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Blake O'Hare <blakeo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523048}
    Former-commit-id: 3ef446508b5a059e28d450007307c5cffe483fd1

--- components/arc/ime/arc_ime_service.cc
+++ components/arc/ime/arc_ime_service.cc
@@ -279,13 +279,18 @@ void ArcImeService::OnCursorRectChangedWithSurroundingText(
 
 ////////////////////////////////////////////////////////////////////////////////
 // Overridden from keyboard::KeyboardControllerObserver
-void ArcImeService::OnKeyboardBoundsChanging(const gfx::Rect& new_bounds) {
+void ArcImeService::OnKeyboardAppearanceChanging(
+    const keyboard::KeyboardStateDescriptor& state) {
   if (!focused_arc_window_)
     return;
   aura::Window* window = focused_arc_window_;
+  gfx::Rect new_bounds = state.occluded_bounds;
   // Multiply by the scale factor. To convert from DPI to physical pixels.
   gfx::Rect bounds_in_px = gfx::ScaleToEnclosingRect(
       new_bounds, window->layer()->device_scale_factor());
+
+  // TODO(b/70251261): In addition to the bounds, the state.is_available value
+  // needs to be conveyed to the ime bridge as well.
   ime_bridge_->SendOnKeyboardBoundsChanging(bounds_in_px);
 }
 
