21e4a5bdff15d43d4c985dbbf7ae329fbfedb600
Blake O'Hare <blakeo@chromium.org>
2017-12-11 06:42:43 +0000

    Replace ArcImeService's usage of OnKeyboardBoundsChanging
    OnKeyboardBoundsChanging is being split into several more specific
    methods that have a stronger contract of how they are being used by the
    implementer. More information about why this is being done is in the
    associated bug.
    
    OnKeyboardAppearanceChanging is called when any time the keyboard's
    bounds or availability changes and has a struct as its parameter that
    includes several properties of the keyboard appearance.
    
    Currently the IME Bridge does not support any additional params other
    than the bounds and then uses the bounds to check if the keyboard is
    available by checking for empty bounds. I've opened b/70251261 to
    change this, since empty bounds can still mean the keyboard is
    available. The KeyboardStateDescriptor struct contains a boolean that
    indicates if the keyboard is truly available for use. This value needs
    to be plumbed into the ime bridge.
    
    Bug: 786290
    Change-Id: I256c348418ec792879e2687064b1a16468432908
    Reviewed-on: https://chromium-review.googlesource.com/778740
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Blake O'Hare <blakeo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523048}
    Former-commit-id: 3ef446508b5a059e28d450007307c5cffe483fd1

--- components/arc/ime/arc_ime_service.h
+++ components/arc/ime/arc_ime_service.h
@@ -95,7 +95,8 @@ class ArcImeService : public KeyedService,
       const gfx::Range& selection_range) override;
 
   // Overridden from keyboard::KeyboardControllerObserver.
-  void OnKeyboardBoundsChanging(const gfx::Rect& rect) override;
+  void OnKeyboardAppearanceChanging(
+      const keyboard::KeyboardStateDescriptor& state) override;
 
   // Overridden from ui::TextInputClient:
   void SetCompositionText(const ui::CompositionText& composition) override;
