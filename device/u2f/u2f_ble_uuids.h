fa624854e84a0b665a3cba5cfa47aea0334fd998
jdoerrie <jdoerrie@chromium.org>
2017-12-11 13:42:41 +0000

    [u2f] Introduce U2F BLE Connection
    This change introduces a U2F BLE connection abstraction. Given an device
    address it attempts to connect to the device, checking if expected
    services and characteristics are present. Furthermore it provides APIs
    to read from and write to the device.
    
    Bug: 763303
    Change-Id: I586f0b2150d676959e119b0ce3c775b47a20c003
    Reviewed-on: https://chromium-review.googlesource.com/774742
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Vincent Scheib <scheib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523086}
    Former-commit-id: c9396bf17a48277285fc9fde83b362943f292bd3

--- device/u2f/u2f_ble_uuids.h
+++ device/u2f/u2f_ble_uuids.h
@@ -10,8 +10,8 @@ namespace device {
 // U2F GATT Service's UUIDs as defined by the standard:
 // https://fidoalliance.org/specs/fido-u2f-v1.2-ps-20170411/fido-u2f-bt-protocol-v1.2-ps-20170411.html#h3_u2f-service
 //
-// For details on how the short U2F Service UUID (0xfffd) was converted to the
-// long one below, see
+// For details on how the short UUIDs for U2F Service (0xFFFD) and U2F Service
+// Revision (0x2A28) were converted to the long ones below, see
 // https://www.bluetooth.com/specifications/assigned-numbers/service-discovery
 static constexpr const char U2F_SERVICE_UUID[] =
     "0000fffd-0000-1000-8000-00805f9b34fb";
@@ -21,6 +21,10 @@ static constexpr const char U2F_STATUS_UUID[] =
     "f1d0fff2-deaa-ecee-b42f-c9ba7ed623bb";
 static constexpr const char U2F_CONTROL_POINT_LENGTH_UUID[] =
     "f1d0fff3-deaa-ecee-b42f-c9ba7ed623bb";
+static constexpr const char U2F_SERVICE_REVISION_UUID[] =
+    "00002a28-0000-1000-8000-00805f9b34fb";
+static constexpr const char U2F_SERVICE_REVISION_BITFIELD_UUID[] =
+    "f1d0fff4-deaa-ecee-b42f-c9ba7ed623bb";
 
 }  // namespace device
 
