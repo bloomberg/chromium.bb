fe7207bb30132913d2adbd20da522c1dfe2d3e7c
Hajime Hoshi <hajimehoshi@chromium.org>
2017-12-11 08:51:09 +0000

    Disable flaky tests in autofill_metrics_browsertests.cc
    TBR=hajimehoshi@chromium.org
    NOTRY=true
    
    Bug: 793672, 793638, 793634
    Change-Id: I9eb80f8ea1712792604bcdba5d5782e2acc0a111
    Reviewed-on: https://chromium-review.googlesource.com/818889
    Reviewed-by: Hajime Hoshi <hajimehoshi@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523058}
    Former-commit-id: 26ca32816241a700b984dbc7a7d16cd8b71b7bd2

--- chrome/browser/autofill/autofill_metrics_browsertest.cc
+++ chrome/browser/autofill/autofill_metrics_browsertest.cc
@@ -114,8 +114,9 @@ IN_PROC_BROWSER_TEST_F(AutofillMetricsMetricsBrowserTest,
   }
 }
 
+// Flaky test, see crbug.com/793672
 IN_PROC_BROWSER_TEST_F(AutofillMetricsMetricsBrowserTest,
-                       CorrectSourceForUnownedAddressCheckout) {
+                       DISABLED_CorrectSourceForUnownedAddressCheckout) {
   GURL main_frame_url =
       https_server_->GetURL("a.com", "/autofill_unowned_address_checkout.html");
   ui_test_utils::NavigateToURL(browser(), main_frame_url);
@@ -126,8 +127,9 @@ IN_PROC_BROWSER_TEST_F(AutofillMetricsMetricsBrowserTest,
   }
 }
 
+// Flaky test, see crbug.com/793638
 IN_PROC_BROWSER_TEST_F(AutofillMetricsMetricsBrowserTest,
-                       CorrectSourceForUnownedCreditCardCheckout) {
+                       DISABLED_CorrectSourceForUnownedCreditCardCheckout) {
   GURL main_frame_url = https_server_->GetURL(
       "a.com", "/autofill_unowned_credit_card_checkout.html");
   ui_test_utils::NavigateToURL(browser(), main_frame_url);
@@ -265,8 +267,9 @@ IN_PROC_BROWSER_TEST_F(SitePerProcessAutofillMetricsMetricsBrowserTest,
   }
 }
 
+// Flaky test, see crbug.com/793634
 IN_PROC_BROWSER_TEST_F(SitePerProcessAutofillMetricsMetricsBrowserTest,
-                       CorrectSourceForUnownedCreditCardCheckout) {
+                       DISABLED_CorrectSourceForUnownedCreditCardCheckout) {
   GURL main_frame_url = https_server_->GetURL(
       "a.com", "/autofill_unowned_credit_card_checkout.html");
   ui_test_utils::NavigateToURL(browser(), main_frame_url);
