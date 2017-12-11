b81ae23bdec8b69fb2edea49965f652e2d79800e
Mathias Bynens <mathias@chromium.org>
2017-12-11 19:04:37 +0000

    Remove V8 use counter for U+2028 & U+2029
    The plumbing was added in 6d8b82ac0b1be6dccbd97e09c6ab6d19cc0fd023 [formerly 94f2d621c52bcc0773a6754d3c52480d88fd50fe]
    (https://chromium-review.googlesource.com/c/chromium/src/+/693518)
    
    BUG=v8:6827
    
    Change-Id: Id29b36d702eb0857fdfb63d52c3494871ee06501
    Reviewed-on: https://chromium-review.googlesource.com/819632
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523143}
    Former-commit-id: be6ef64d5d2f11a8c52de921e81165ecac76e675

--- third_party/WebKit/Source/bindings/core/v8/UseCounterCallback.cpp
+++ third_party/WebKit/Source/bindings/core/v8/UseCounterCallback.cpp
@@ -130,9 +130,6 @@ void UseCounterCallback(v8::Isolate* isolate,
     case v8::Isolate::kLabeledExpressionStatement:
       blink_feature = WebFeature::kV8LabeledExpressionStatement;
       break;
-    case v8::Isolate::kLineOrParagraphSeparatorAsLineTerminator:
-      blink_feature = WebFeature::kV8LineOrParagraphSeparatorAsLineTerminator;
-      break;
     case v8::Isolate::kErrorCaptureStackTrace:
       blink_feature = WebFeature::kV8ErrorCaptureStackTrace;
       break;
