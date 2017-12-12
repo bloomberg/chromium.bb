wjdz99/aom

Make mv storage independent of ref frame index
The motion vector storage location in the reference frame is set
to be dependent on the relative frame position, instead of the
reference frame index.

Change-Id: Id8f7d1e6ba32a5ed9e182194178fd2a70094a309

av1/common/onyxc_int.h
@@ -581,6 +581,8 @@ typedef struct AV1Common {
 #endif
 #if CONFIG_MFMV
   TPL_MV_REF *tpl_mvs;
+  // TODO(jingning): This can be combined with sign_bias later.
+  uint8_t ref_frame_side[TOTAL_REFS_PER_FRAME];
 #endif
 
 #if TXCOEFF_TIMER

bloomberg/chromium.bb

av1/common/onyxc_int.h
@@ -577,6 +577,8 @@ typedef struct AV1Common {
 #endif
 #if CONFIG_MFMV
   TPL_MV_REF *tpl_mvs;
+  // TODO(jingning): This can be combined with sign_bias later.
+  int8_t ref_frame_side[TOTAL_REFS_PER_FRAME];
 #endif
 
 #if TXCOEFF_TIMER