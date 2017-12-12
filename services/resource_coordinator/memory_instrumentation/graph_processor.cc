a7f3d7dcac42c89e5bef1044892adcac3d96238d
Lalit Maganti <lalitm@chromium.org>
2017-12-12 12:41:21 +0000

    memory-infra: enable full graph computation pipeline
    Bug: 728199
    Change-Id: I440814aa37c687ab68fea5717cee36a051a4908d
    Reviewed-on: https://chromium-review.googlesource.com/800553
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Commit-Queue: Lalit Maganti <lalitm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523413}
    Former-commit-id: 1220248209411cf32441e865677bd2fd4240c0de

--- services/resource_coordinator/memory_instrumentation/graph_processor.cc
+++ services/resource_coordinator/memory_instrumentation/graph_processor.cc
@@ -566,7 +566,10 @@ void GraphProcessor::CalculateSizeForNode(Node* node) {
 
   // Check that if both aggregated and node sizes exist that the node size
   // is bigger than the aggregated.
-  DCHECK(!node_size || !aggregated_size || *node_size >= *aggregated_size);
+  // TODO(lalitm): the following condition is triggered very often even though
+  // it is a warning in JS code. Find a way to add the warning to display in UI
+  // or to fix all instances where this is violated and then enable this check.
+  // DCHECK(!node_size || !aggregated_size || *node_size >= *aggregated_size);
 
   // Calculate the maximal size of an owner node.
   base::Optional<uint64_t> max_owner_size;
@@ -580,7 +583,10 @@ void GraphProcessor::CalculateSizeForNode(Node* node) {
 
   // Check that if both owner and node sizes exist that the node size
   // is bigger than the owner.
-  DCHECK(!node_size || !max_owner_size || *node_size >= *max_owner_size);
+  // TODO(lalitm): the following condition is triggered very often even though
+  // it is a warning in JS code. Find a way to add the warning to display in UI
+  // or to fix all instances where this is violated and then enable this check.
+  // DCHECK(!node_size || !max_owner_size || *node_size >= *max_owner_size);
 
   // Clear out any existing size entry which may exist.
   node->entries()->erase(kSizeEntryName);
