6d479daacdf5e6aea110ee9c426bd7aae4832708
John Z Wu <jzw@chromium.org>
2017-12-12 16:46:39 +0000

    Remove IOSSigninClient and have IOSWebViewSigninClient and IOSChromeSigninClient directly inherit from SigninClient.
    Partial implementations are not preferred when sharing code between
    //ios/chrome and //ios/web_view. Instead, convert to using helper
    classes (using composition) wherever possible.
    
    Bug: 774517
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I0fa0ad558ee518b0eefb110f34f9480d69b59a6a
    Reviewed-on: https://chromium-review.googlesource.com/792252
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523451}
    Former-commit-id: bda65f4e1fceadb745a005f14c36886be4730cd0

--- ios/chrome/browser/signin/ios_chrome_signin_client.h
+++ ios/chrome/browser/signin/ios_chrome_signin_client.h
@@ -5,14 +5,23 @@
 #ifndef IOS_CHROME_BROWSER_SIGNIN_IOS_CHROME_SIGNIN_CLIENT_H_
 #define IOS_CHROME_BROWSER_SIGNIN_IOS_CHROME_SIGNIN_CLIENT_H_
 
-#include "components/signin/ios/browser/ios_signin_client.h"
+#include <memory>
+
+#include "base/macros.h"
+#include "components/content_settings/core/browser/cookie_settings.h"
+#include "components/content_settings/core/browser/host_content_settings_map.h"
+#include "components/signin/core/browser/signin_client.h"
+#include "components/signin/core/browser/signin_error_controller.h"
+#include "components/signin/ios/browser/wait_for_network_callback_helper.h"
+#include "net/url_request/url_request_context_getter.h"
 
 namespace ios {
 class ChromeBrowserState;
 }
 
-// Concrete implementation of IOSSigninClient for //ios/chrome.
-class IOSChromeSigninClient : public IOSSigninClient {
+// Concrete implementation of SigninClient for //ios/chrome.
+class IOSChromeSigninClient : public SigninClient,
+                              public SigninErrorController::Observer {
  public:
   IOSChromeSigninClient(
       ios::ChromeBrowserState* browser_state,
@@ -20,7 +29,10 @@ class IOSChromeSigninClient : public IOSSigninClient {
       scoped_refptr<content_settings::CookieSettings> cookie_settings,
       scoped_refptr<HostContentSettingsMap> host_content_settings_map,
       scoped_refptr<TokenWebData> token_web_data);
-  ~IOSChromeSigninClient() override = default;
+  ~IOSChromeSigninClient() override;
+
+  // KeyedService implementation.
+  void Shutdown() override;
 
   // SigninClient implementation.
   base::Time GetInstallDate() override;
@@ -34,6 +46,24 @@ class IOSChromeSigninClient : public IOSSigninClient {
       const std::string& source,
       net::URLRequestContextGetter* getter) override;
   void PreGaiaLogout(base::OnceClosure callback) override;
+  scoped_refptr<TokenWebData> GetDatabase() override;
+  PrefService* GetPrefs() override;
+  net::URLRequestContextGetter* GetURLRequestContext() override;
+  void DoFinalInit() override;
+  bool CanRevokeCredentials() override;
+  std::string GetSigninScopedDeviceId() override;
+  bool ShouldMergeSigninCredentialsIntoCookieJar() override;
+  bool IsFirstRun() const override;
+  bool AreSigninCookiesAllowed() override;
+  void AddContentSettingsObserver(
+      content_settings::Observer* observer) override;
+  void RemoveContentSettingsObserver(
+      content_settings::Observer* observer) override;
+  std::unique_ptr<CookieChangedSubscription> AddCookieChangedCallback(
+      const GURL& url,
+      const std::string& name,
+      const net::CookieStore::CookieChangedCallback& callback) override;
+  void DelayNetworkCall(const base::Closure& callback) override;
 
   // SigninErrorController::Observer implementation.
   void OnErrorChanged() override;
@@ -42,8 +72,19 @@ class IOSChromeSigninClient : public IOSSigninClient {
   // SigninClient private implementation.
   void OnSignedOut() override;
 
+  // Helper to delay callbacks until connection becomes online again.
+  std::unique_ptr<WaitForNetworkCallbackHelper> network_callback_helper_;
+  // The browser state associated with this service.
   ios::ChromeBrowserState* browser_state_;
+  // Used to check for errors related to signing in.
   SigninErrorController* signin_error_controller_;
+  // Used to check if sign in cookies are allowed.
+  scoped_refptr<content_settings::CookieSettings> cookie_settings_;
+  // Used to add and remove content settings observers.
+  scoped_refptr<HostContentSettingsMap> host_content_settings_map_;
+  // The TokenWebData associated with this service.
+  scoped_refptr<TokenWebData> token_web_data_;
+
   DISALLOW_COPY_AND_ASSIGN(IOSChromeSigninClient);
 };
 
