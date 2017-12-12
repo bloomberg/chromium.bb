56b47c51b0db35b1a5966bbc45fdaf2f35052431
Charles Harrison <csharrison@chromium.org>
2017-12-12 17:56:58 +0000

    Predictor: Dispatch all predictions async from the resource throttle
    This is a small refactor that should have some performance wins. Now
    that PlzNavigate has launched, we should be dispatching our preconnects
    after the initial navigation request has gone out, not before. This is
    because frequently (especially on Android), dispatching preconnects
    tends to hog the IO thread, delaying the initial request.
    
    Note that this predictor is probably going away, but this CPU work
    is in the critical path of some other work I am planning on tackling.
    
    Bug: 792524
    Change-Id: I87e3c235be0480d766e3e45c9a97c4a26b15f60d
    Reviewed-on: https://chromium-review.googlesource.com/821090
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523473}
    Former-commit-id: 24e730d2d5a98e1ec4fe01cf35f62f9d171e6503

--- chrome/browser/net/predictor_tab_helper.h
+++ chrome/browser/net/predictor_tab_helper.h
@@ -6,15 +6,8 @@
 #define CHROME_BROWSER_NET_PREDICTOR_TAB_HELPER_H_
 
 #include "base/macros.h"
-#include "content/public/browser/notification_observer.h"
-#include "content/public/browser/notification_registrar.h"
-#include "content/public/browser/reload_type.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
-
-namespace content {
-class NavigationHandle;
-}
 
 namespace chrome_browser_net {
 
@@ -25,27 +18,11 @@ class PredictorTabHelper
   ~PredictorTabHelper() override;
 
   // content::WebContentsObserver:
-  void DidStartNavigation(
-      content::NavigationHandle* navigation_handle) override;
-  void DidStartNavigationToPendingEntry(
-      const GURL& url,
-      content::ReloadType reload_type) override;
   void DocumentOnLoadCompletedInMainFrame() override;
 
  private:
   explicit PredictorTabHelper(content::WebContents* web_contents);
   friend class content::WebContentsUserData<PredictorTabHelper>;
-
-  void PreconnectUrl(const GURL& url);
-
-  // This boolean is set to true after a call to
-  // DidStartNavigationToPendingEntry, which fires off predictive preconnects.
-  // This ensures that the resulting call to DidStartNavigation does not
-  // duplicate these preconnects. The tab helper spawns preconnects on the
-  // navigation to the pending entry because DidStartNavigation is called after
-  // render process spawn (pre-PlzNavigate), which can take substantial time
-  // especially on Android.
-  bool predicted_from_pending_entry_;
 
   DISALLOW_COPY_AND_ASSIGN(PredictorTabHelper);
 };
