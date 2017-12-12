3c7f75d89b93929bce9f06607181f6908993c4d1
Darren Shen <shend@chromium.org>
2017-12-12 04:21:51 +0000

    [css-counters] Update counters correctly under counter-reset changes.
    Currently we are not updating counter values correctly when we change
    or add counter-resets. For example, if we have something like:
    
    <div style="counter-reset: c">
      <p style="counter-increment: c">First</p>
      <p style="counter-increment: c">Second</p>
    </div>
    
    we would construct the following counter tree (approximately):
    
    reset
      increment // first
      increment // second
    
    When we insert a new reset node after the first item, we would get
    something like:
    
    reset
      increment // first
      increment // second
    reset
    
    The correct behaviour would be to move every (non-reset) counter
    node after the first node to be a child of the new node:
    
    reset
      increment // first
    reset
      increment // second
    
    Bug: 463513, 599359, 591267
    Change-Id: I28cbf3c13c86336ad3f6f44bf865c59d9f82d98a
    Reviewed-on: https://chromium-review.googlesource.com/809984
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523333}
    Former-commit-id: 03c0c40d9fd01a01d3ea7992fa3779a876a3740e

--- third_party/WebKit/Source/core/layout/CounterNode.cpp
+++ third_party/WebKit/Source/core/layout/CounterNode.cpp
@@ -359,6 +359,31 @@ void CounterNode::RemoveChild(CounterNode* old_child) {
     next->Recount();
 }
 
+void CounterNode::MoveNonResetSiblingsToChildOf(
+    CounterNode* first_node,
+    CounterNode& new_parent,
+    const AtomicString& identifier) {
+  if (!first_node)
+    return;
+
+  scoped_refptr<CounterNode> cur_node = first_node;
+  scoped_refptr<CounterNode> old_parent = first_node->Parent();
+  while (cur_node && !cur_node->ActsAsReset()) {
+    scoped_refptr<CounterNode> next = cur_node->NextSibling();
+    old_parent->RemoveChild(cur_node.get());
+    new_parent.InsertAfter(cur_node.get(), new_parent.LastChild(), identifier);
+    cur_node = next;
+  }
+
+  // We assume that a reset node cannot have a non-reset node as its next
+  // sibling, but we're not sure this is always true, so we add a DCHECK to be
+  // safe.
+  while (cur_node) {
+    DCHECK(cur_node->ActsAsReset());
+    cur_node = cur_node->NextSibling();
+  }
+}
+
 #ifndef NDEBUG
 
 static void ShowTreeAndMark(const CounterNode* node) {
