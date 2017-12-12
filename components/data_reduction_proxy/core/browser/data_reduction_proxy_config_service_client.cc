4d039631ed59bb211b77a5c1b5481a7108f04105
Ryan Sturm <ryansturm@chromium.org>
2017-12-12 19:34:40 +0000

    Adding MCC/MNC to Data_reduction_proxy ClientConfig
    Bug: 793137
    Change-Id: Ifb14055f98fbe75523aaca6d61cf0ed85b9ef70c
    Reviewed-on: https://chromium-review.googlesource.com/820560
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523509}
    Former-commit-id: 63576d29e15e56eee6e2c50364c4e5a92d5b74df

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_config_service_client.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_config_service_client.cc
@@ -18,6 +18,7 @@
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/values.h"
+#include "build/build_config.h"
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_config.h"
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_io_data.h"
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_mutable_config_values.h"
@@ -42,6 +43,10 @@
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_request_status.h"
+
+#if defined(OS_ANDROID)
+#include "net/android/network_library.h"
+#endif
 
 namespace data_reduction_proxy {
 
@@ -372,6 +377,10 @@ void DataReductionProxyConfigServiceClient::RetrieveRemoteConfig() {
   DCHECK(thread_checker_.CalledOnValidThread());
   CreateClientConfigRequest request;
   std::string serialized_request;
+#if defined(OS_ANDROID)
+  request.set_telephony_network_operator(
+      net::android::GetTelephonyNetworkOperator());
+#endif
   const std::string& session_key = request_options_->GetSecureSession();
   if (!session_key.empty())
     request.set_session_key(request_options_->GetSecureSession());
