e0888030b38856ff6c24d2acb3705a23c5044c10
Devlin Cronin <rdevlin.cronin@chromium.org>
2017-12-12 21:02:12 +0000

    [Extensions Cleanup] Remove unused ModuleSystem::RunString variant
    One of the variants of ModuleSystem::RunString() was unused. Remove it.
    
    Bug: None
    
    Change-Id: I551b5b2baec597d20d8923578b52ea6f02a169ce
    Reviewed-on: https://chromium-review.googlesource.com/822730
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523536}
    Former-commit-id: 9520019b96116dac669ba2cae0a9fb0275b14b25

--- extensions/renderer/module_system.cc
+++ extensions/renderer/module_system.cc
@@ -355,18 +355,6 @@ void ModuleSystem::OverrideNativeHandlerForTest(const std::string& name) {
   overridden_native_handlers_.insert(name);
 }
 
-void ModuleSystem::RunString(const std::string& code, const std::string& name) {
-  v8::HandleScope handle_scope(GetIsolate());
-  v8::Local<v8::String> v8_code;
-  v8::Local<v8::String> v8_name;
-  if (!ToV8String(GetIsolate(), code.c_str(), &v8_code) ||
-      !ToV8String(GetIsolate(), name.c_str(), &v8_name)) {
-    Warn(GetIsolate(), "Too long code or name.");
-    return;
-  }
-  RunString(v8_code, v8_name);
-}
-
 // static
 void ModuleSystem::NativeLazyFieldGetter(
     v8::Local<v8::Name> property,
