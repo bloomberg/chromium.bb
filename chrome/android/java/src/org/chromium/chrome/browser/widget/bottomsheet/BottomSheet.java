8f512928afe3599800fcfbc3747755c23f1cf085
Matthew Jones <mdjones@chromium.org>
2017-12-12 23:09:50 +0000

    [Home] Increase maximum recorded reciprocal swipe velocity
    This change increases the maximum recorded value for reciprocal
    velocity in the bottom sheet. Early metrics indicate a high number
    of swipes in the 20k - 60k range. Reproducing these values is possible
    by idly keeping your thumb at near the bottom of the screen; moving
    very slightly (thus triggering a swipe).
    
    BUG=779615
    
    Change-Id: I9662d9d8a8ceb0d9853adea30b296bde23b0da3a
    Reviewed-on: https://chromium-review.googlesource.com/823220
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523594}
    Former-commit-id: ba40d2e3975993961ae37c3055c745c545296e8b

--- chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
@@ -425,7 +425,7 @@ public class BottomSheet
      * @param microsPerDp The microseconds per dp being recorded.
      */
     private void recordSwipeVelocity(String name, int microsPerDp) {
-        RecordHistogram.recordCustomCountHistogram(name, microsPerDp, 1, 20000, 50);
+        RecordHistogram.recordCustomCountHistogram(name, microsPerDp, 1, 60000, 50);
     }
 
     /**
