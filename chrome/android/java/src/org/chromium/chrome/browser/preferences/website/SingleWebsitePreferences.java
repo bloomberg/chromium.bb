29b384da566675324e4a0e3f38d083e1b7927d75
Raymes Khoury <raymes@chromium.org>
2017-12-12 23:43:05 +0000

    Update the UI to reflect the notifications permission for the DSE
    This updates 2 UI surfaces to reflect notifications permissions set for
    the DSE. Specifically:
    1) In android Site Details, when toggle the DSE permission for
    notififications, it will show the options "Allow/Block for the current
    search engine". 2 cases are handled here: one when that setting links
    directly into Notification Channels on Android O+ and the other in the
    case where the permission is handled directly in Chrome.
    
    2) In Settings>Search Engines, underneath the currently selected search
    engine, it will display a link indicating whether
    notifications/geolocation are allowed. The link will take the user to
    the Site Details for the search engine. Several changes have been made
    to the behavior here:
    -The link used to indicate if location was disabled. Now, if
    notifications are location are disabled, nothing will be shown in the
    text for those permissions.
    -The link used to take users directly to Android Location Settings when
    the text indicated that system location was disabled. But now due to the
    link handling combined permissions, it always takes the user directly to
    Site Details for the search engine, where the user can follow a link
    into Android Location Settings.
    
    Bug: 780344
    Change-Id: I41e0dd1028ad289141d1632875d30373b528f17c
    Reviewed-on: https://chromium-review.googlesource.com/799534
    Commit-Queue: Raymes Khoury <raymes@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523606}
    Former-commit-id: 6deaae488ed3eb805a385c453507a1ebac0edf80

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SingleWebsitePreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SingleWebsitePreferences.java
@@ -368,9 +368,9 @@ public class SingleWebsitePreferences extends PreferenceFragment
     }
 
     private void setUpNotificationsPreference(Preference preference) {
+        final ContentSetting value = mSite.getNotificationPermission();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
                 && ChromeFeatureList.isEnabled(ChromeFeatureList.SITE_NOTIFICATION_CHANNELS)) {
-            final ContentSetting value = mSite.getNotificationPermission();
             if (!(value == ContentSetting.ALLOW || value == ContentSetting.BLOCK)) {
                 // TODO(crbug.com/735110): Figure out if this is the correct thing to do, for values
                 // that are non-null, but not ALLOW or BLOCK either. (In setupListPreference we
@@ -384,8 +384,16 @@ public class SingleWebsitePreferences extends PreferenceFragment
             newPreference.setKey(preference.getKey());
             setUpPreferenceCommon(newPreference);
 
-            newPreference.setSummary(
-                    getResources().getString(ContentSettingsResources.getSiteSummary(value)));
+            if (isPermissionControlledByDSE(
+                        ContentSettingsType.CONTENT_SETTINGS_TYPE_NOTIFICATIONS)) {
+                newPreference.setSummary(getResources().getString(value == ContentSetting.ALLOW
+                                ? R.string.website_settings_permissions_allow_dse
+                                : R.string.website_settings_permissions_block_dse));
+            } else {
+                newPreference.setSummary(
+                        getResources().getString(ContentSettingsResources.getSiteSummary(value)));
+            }
+
             newPreference.setDefaultValue(value);
 
             // This preference is read-only so should not attempt to persist to shared prefs.
@@ -410,7 +418,11 @@ public class SingleWebsitePreferences extends PreferenceFragment
             getPreferenceScreen().removePreference(preference);
             getPreferenceScreen().addPreference(newPreference);
         } else {
-            setUpListPreference(preference, mSite.getNotificationPermission());
+            setUpListPreference(preference, value);
+            if (isPermissionControlledByDSE(ContentSettingsType.CONTENT_SETTINGS_TYPE_NOTIFICATIONS)
+                    && value != null) {
+                updatePreferenceForDSESetting(preference);
+            }
         }
     }
 
@@ -620,7 +632,7 @@ public class SingleWebsitePreferences extends PreferenceFragment
         setUpListPreference(preference, permission);
         if (isPermissionControlledByDSE(ContentSettingsType.CONTENT_SETTINGS_TYPE_GEOLOCATION)
                 && permission != null) {
-            updateLocationPreferenceForDSESetting(preference);
+            updatePreferenceForDSESetting(preference);
         }
     }
 
@@ -684,7 +696,7 @@ public class SingleWebsitePreferences extends PreferenceFragment
      * for searches that happen from the omnibox.
      * @param preference The Location preference to modify.
      */
-    private void updateLocationPreferenceForDSESetting(Preference preference) {
+    private void updatePreferenceForDSESetting(Preference preference) {
         ListPreference listPreference = (ListPreference) preference;
         Resources res = getResources();
         listPreference.setEntries(new String[] {
