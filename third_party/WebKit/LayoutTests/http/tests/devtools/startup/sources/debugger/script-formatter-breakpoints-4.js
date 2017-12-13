adb593d6bd0a0b222265cd370d22746ab716a829
Will Chen <chenwilliam@chromium.org>
2017-12-13 21:39:32 +0000

    DevTools: migrate most of the remaining startup tests
    After this, there's a few tests that will need to be ported into protocol
    test, browser test, or removed.
    
    This is a part-automated / part-manual process to migrate these /startup/
    tests to the new integration test framework:
    
    1. extract the precondition activities into an html file for the inspected
    window.
    2. add TestRunner.setupStartupTest(...) as the first line of the js test to
    navigate to the inspected window before the DevTools session is started.
    3. add testRunner.navigateSecondaryWindow(...) once the inspected page is
    done w/ the precondition activities.
    
    Bug: 667560
    Change-Id: Ie988b6767f98759e7008e9306cd166776196197b
    Reviewed-on: https://chromium-review.googlesource.com/823611
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523887}
    Former-commit-id: 0d705f66ef33710c57e875543c3a4634c63a31c1

--- third_party/WebKit/LayoutTests/http/tests/devtools/startup/sources/debugger/script-formatter-breakpoints-4.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/startup/sources/debugger/script-formatter-breakpoints-4.js
@@ -1,28 +1,13 @@
-<html>
-<head>
-<script src="../../../../inspector/inspector-test.js"></script>
-<script src="../../../../inspector/debugger-test.js"></script>
-<script src="../../../../inspector/sources/debugger/resources/unformatted4.js"></script>
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-<script>
+(async function() {
+  await TestRunner.setupStartupTest('resources/script-formatter-breakpoints-4.html');
+  TestRunner.addResult(`Tests the script formatting is working fine with breakpoints.\n`);
+  await TestRunner.loadModule('sources_test_runner');
+  await TestRunner.showPanel('sources');
 
-function f1()
-{
-    var a=0;var b=1;var c=3;var d=4;var e=5;
-    var f=0;
-    return 0;
-}
-
-function onload()
-{
-    if (window.testRunner) {
-        testRunner.waitUntilDone();
-        testRunner.showWebInspector();
-    }
-    runTest();
-}
-
-var test = function() {
   Bindings.breakpointManager._storage._breakpoints = {};
   var panel = UI.panels.sources;
   var scriptFormatter;
@@ -42,7 +27,7 @@ var test = function() {
         TestRunner.addResult('Adding breakpoint.');
         TestRunner.addSniffer(
             Bindings.BreakpointManager.ModelBreakpoint.prototype, '_addResolvedLocation', breakpointResolved);
-        SourcesTestRunner.setBreakpoint(sourceFrame, 11, '', true);
+        SourcesTestRunner.setBreakpoint(sourceFrame, 9, '', true);
       }
 
       function breakpointResolved() {
@@ -55,7 +40,7 @@ var test = function() {
       function uiSourceCodeScriptFormatted() {
         TestRunner.addResult('Removing breakpoint.');
         var formattedSourceFrame = panel.visibleView;
-        SourcesTestRunner.removeBreakpoint(formattedSourceFrame, 13);
+        SourcesTestRunner.removeBreakpoint(formattedSourceFrame, 11);
         TestRunner.addResult('Unformatting.');
         Sources.sourceFormatter.discardFormattedUISourceCode(panel.visibleView.uiSourceCode());
         var breakpoints = Bindings.breakpointManager._storage._setting.get();
@@ -64,12 +49,4 @@ var test = function() {
       }
     }
   ]);
-
-};
-</script>
-</head>
-<body onload="onload()">
-<p>Tests the script formatting is working fine with breakpoints.
-</p>
-</body>
-</html>
+})();
