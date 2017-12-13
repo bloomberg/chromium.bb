111155e281dca9b07fe1618e01e0237c06cd7d36
Vincent Palatin <vpalatin@chromium.org>
2017-12-13 16:58:43 +0000

    Mojo bridge interface for the USB Host
    Define the bridge interface to connect the machine USB devices to the
    Android USB host API.
    Implement the API using /device/usb.
    
    The permission request for the USB devices is currently stubbed and
    fails closed. So until the Permission UI and settings are submitted, the
    API basically rejects all the requests.
    
    Bug: 24572867
    Test: Manual, run with ARC++ app using android.hardware.usb to use USB
    devices.
    
    Change-Id: Ibce0a45e4359a97ee06d8d49981169bd5f18176f
    Reviewed-on: https://chromium-review.googlesource.com/731284
    Reviewed-by: Mattias Nissler <mnissler@chromium.org>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Vincent Palatin <vpalatin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523790}
    Former-commit-id: 63f4e4ef798cbe00944b5842cf56d5aab94a1fb2

--- chrome/browser/chromeos/arc/arc_service_launcher.cc
+++ chrome/browser/chromeos/arc/arc_service_launcher.cc
@@ -55,6 +55,7 @@
 #include "components/arc/power/arc_power_bridge.h"
 #include "components/arc/rotation_lock/arc_rotation_lock_bridge.h"
 #include "components/arc/storage_manager/arc_storage_manager.h"
+#include "components/arc/usb/usb_host_bridge.h"
 #include "components/arc/volume_mounter/arc_volume_mounter_bridge.h"
 #include "components/prefs/pref_member.h"
 #include "ui/arc/notification/arc_notification_manager.h"
@@ -163,6 +164,7 @@ void ArcServiceLauncher::OnPrimaryUserProfilePrepared(Profile* profile) {
   ArcSettingsService::GetForBrowserContext(profile);
   ArcTracingBridge::GetForBrowserContext(profile);
   ArcTtsService::GetForBrowserContext(profile);
+  ArcUsbHostBridge::GetForBrowserContext(profile);
   ArcUserSessionService::GetForBrowserContext(profile);
   ArcVoiceInteractionArcHomeService::GetForBrowserContext(profile);
   ArcVoiceInteractionFrameworkService::GetForBrowserContext(profile);
