4960468c21c3ac9565f143fe6d2b5aace603b87a
Ross McIlroy <rmcilroy@chromium.org>
2017-12-13 12:17:58 +0000

    [Content] Add V8BackgroundCompile feature
    Adds a V8BackgroundCompile feature to content to enable control of enabling
    the background compile feature in V8.
    
    BUG=v8:5203
    
    Change-Id: I3c872d619db628e795fa8a817222774b238d4342
    Reviewed-on: https://chromium-review.googlesource.com/822337
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Ross McIlroy <rmcilroy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523752}
    Former-commit-id: 824a4048044d2b162da34bbfa8545324da8fe7ec

--- content/renderer/render_process_impl.cc
+++ content/renderer/render_process_impl.cc
@@ -156,6 +156,7 @@ RenderProcessImpl::RenderProcessImpl(
   SetV8FlagIfNotFeature(features::kAsmJsToWebAssembly, "--no-validate-asm");
   SetV8FlagIfNotFeature(features::kWebAssembly,
                         "--wasm-disable-structured-cloning");
+  SetV8FlagIfFeature(features::kV8BackgroundCompile, "--background-compile");
 
   SetV8FlagIfFeature(features::kV8VmFuture, "--future");
   SetV8FlagIfNotFeature(features::kV8VmFuture, "--no-future");
