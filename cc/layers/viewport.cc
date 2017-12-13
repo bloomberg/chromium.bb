9f91f843ccef2994197e7066f489784b634aa4c7
W. James MacLean <wjmaclean@chromium.org>
2017-12-13 21:34:38 +0000

    OOPIFs shouldn't invoke snap-to-min on PinchEnd.
    Now that (i) we route GesturePinchX events to the renderer on Mac, and
    (ii) ViewPort::PinchEnd() may attempt a PageScaleAnimation for
    snap-to-min, which only makes sense on the main frame, we should early
    out to avoid calling this function for OOPIFs.
    
    Bug: 791885
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I48d11add4694eeeac7be44d0d7430c6d04d7f94b
    Reviewed-on: https://chromium-review.googlesource.com/823097
    Commit-Queue: James MacLean <wjmaclean@chromium.org>
    Reviewed-by: weiliangc <weiliangc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523886}
    Former-commit-id: 1bb05e1810bb6117d6e091ceefe3b4ea17572971

--- cc/layers/viewport.cc
+++ cc/layers/viewport.cc
@@ -213,8 +213,13 @@ void Viewport::PinchUpdate(float magnify_delta, const gfx::Point& anchor) {
 }
 
 void Viewport::PinchEnd(const gfx::Point& anchor, bool snap_to_min) {
-  if (snap_to_min) {
-    LayerTreeImpl* active_tree = host_impl_->active_tree();
+  LayerTreeImpl* active_tree = host_impl_->active_tree();
+  // TODO(mcnee): Remove the InnerViewportScrollLayer() check (and add a
+  // DCHECK instead), after we no longer send GesturePinch events to
+  // OOPIF renderers. https://crbug.com/787924
+  // Snap-to-min only makes sense for mainframes, so we use the lack of an
+  // InnerViewportScrollLayer() to detect if this is an OOPIF.
+  if (snap_to_min && active_tree->InnerViewportScrollLayer()) {
     const float kMaxZoomForSnapToMin = 1.05f;
     const base::TimeDelta kSnapToMinZoomAnimationDuration =
         base::TimeDelta::FromMilliseconds(200);
