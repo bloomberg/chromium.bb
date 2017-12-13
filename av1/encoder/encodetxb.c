a7a6f4e1ebc79fb7570035d00b3b05e817a09b4f
Jingning Han <jingning@google.com>
2017-12-13 14:44:08 -0800

    Update levels in coeff optimization
    When reducing the coefficient level gives better rate-distortion
    results, update the the corresponding levels for next coefficient
    context modeling.
    
    Change-Id: Ia04e287a576f2a4afd1e8ecc9bcb311d067f02e5

--- av1/encoder/encodetxb.c
+++ av1/encoder/encodetxb.c
@@ -1810,7 +1810,7 @@ static int optimize_txb(TxbInfo *txb_info, const LV_MAP_COEFF_COST *txb_costs,
 
       int bUpdCoeff = 0;
       if (stats.rd_low < stats.rd) {
-        if ((stats.low_qc != 0) || (si < txb_info->eob - 1)) {
+        if (si < txb_info->eob - 1) {
           bUpdCoeff = 1;
           update = 1;
         }
@@ -1826,6 +1826,7 @@ static int optimize_txb(TxbInfo *txb_info, const LV_MAP_COEFF_COST *txb_costs,
       }
     }
   }  // for (si)
+
   int non_zero_blk_rate =
       txb_costs->txb_skip_cost[txb_info->txb_ctx->txb_skip_ctx][0];
   prev_eob_rd_cost =
