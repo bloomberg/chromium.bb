fe67b05bc3507f7e6f011956439f6a27fd9636a0
Xi Han <hanxi@google.com>
2017-12-14 14:54:43 +0000

    Webapp offline dialog handles more network errors
    Currently the offline dialog only handles ERR_INTERNET_DISCONNECTED error, and
    we would like it to catch up more network errors. A generic message is added
    to shown on the dialog.
    
    Bug: 789566
    Change-Id: I9c36319241f8754822e6336eca8ae13379edc764
    Reviewed-on: https://chromium-review.googlesource.com/803835
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524066}
    Former-commit-id: 0f70028fac90a82ed3b3e9ff648bcca2341dabeb

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappOfflineDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappOfflineDialog.java
@@ -7,11 +7,11 @@ package org.chromium.chrome.browser.webapps;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.support.annotation.StringRes;
 import android.support.v7.app.AlertDialog;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
+import org.chromium.net.NetError;
 
 /**
  * A dialog to notify users that WebAPKs and Trusted Web Activities need a network connection to
@@ -25,19 +25,17 @@ public class WebappOfflineDialog {
      * @param activity Activity that will be used for {@link Dialog#show()}.
      * @param appName The name of the Android native client for which the dialog is shown.
      * @param isWebApk Whether the app above is a WebAPK.
+     * @param errorCode The network error code.
      */
-    public void show(final Activity activity, String appName, boolean isWebApk) {
-        @StringRes int messageID = isWebApk
-                ? R.string.webapk_offline_dialog : R.string.webapp_twa_offline_dialog;
+    public void show(final Activity activity, String appName, boolean isWebApk, int errorCode) {
         AlertDialog.Builder builder = new AlertDialog.Builder(activity, R.style.AlertDialogTheme);
-        builder.setMessage(activity.getString(messageID, appName))
-                .setPositiveButton(R.string.ok,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                ApiCompatibilityUtils.finishAndRemoveTask(activity);
-                            }
-                        });
+        builder.setMessage(getErrorDescription(activity, appName, isWebApk, errorCode))
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ApiCompatibilityUtils.finishAndRemoveTask(activity);
+                    }
+                });
 
         mDialog = builder.create();
         mDialog.setCanceledOnTouchOutside(false);
@@ -47,5 +45,20 @@ public class WebappOfflineDialog {
     /** Closes the dialog. */
     public void cancel() {
         mDialog.cancel();
+    }
+
+    private String getErrorDescription(
+            Activity activity, String appName, boolean isWebApk, int errorCode) {
+        switch (errorCode) {
+            case NetError.ERR_INTERNET_DISCONNECTED:
+                int messageID = isWebApk ? R.string.webapk_offline_dialog
+                                         : R.string.webapp_twa_offline_dialog;
+                return activity.getString(messageID, appName);
+            case NetError.ERR_TUNNEL_CONNECTION_FAILED:
+                return activity.getString(
+                        R.string.webapp_network_error_message_tunnel_connection_failed);
+            default:
+                return activity.getString(R.string.webapp_cannot_connect_to_site);
+        }
     }
 }
