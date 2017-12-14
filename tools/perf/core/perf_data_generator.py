7d2c65c97ea0f3c11fd484380ce1c1ddb056a43d
Charlie Andrews <charliea@chromium.org>
2017-12-14 19:50:43 +0000

    Increase the system_health.memory_mobile timeout from 3h to 4h
    The benchmark regularly takes around 2h50m-3h on Android One, and has
    been regularly timing out for the past several months on the waterfall.
    
    NOTRY=true
    TBR=nednguyen@google.com
    
    Bug: 775242
    Change-Id: I2945eef4c12bcf9540c93760c33f54df66dffc9a
    Reviewed-on: https://chromium-review.googlesource.com/827605
    Commit-Queue: Charlie Andrews <charliea@chromium.org>
    Reviewed-by: Charlie Andrews <charliea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524150}
    Former-commit-id: a79793994cb8f772fd9f6039a4c45535d53091d6

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -844,7 +844,7 @@ def generate_telemetry_tests(name, tester_config, benchmarks,
 BENCHMARK_SWARMING_TIMEOUTS = {
     'loading.desktop': 14400, # 4 hours (crbug.com/753798)
     'loading.mobile': 16200, # 4.5 hours
-    'system_health.memory_mobile': 10800, # 3 hours
+    'system_health.memory_mobile': 14400, # 4 hours (crbug.com/775242)
     'system_health.memory_desktop': 10800, # 3 hours
 }
 
