9fce213bdbb8512c571c6e14b0302dbb5ecd653e
Takuto Ikuta <tikuta@google.com>
2017-12-14 10:44:28 +0900

    Partial revert of "git_cache: lower max num of .pack files before re-bootstrap on Win."
    This partially reverts commit d51ed57edb595cd0e13e398925dba8bf2324e77e.
    
    Reason for revert:
    New git client for windows was rolled including fix for slow `git fetch`.
    I guess smaller pack limit causes frequent bootstrap taking 2~3 minutes longer than
    the case it does not happen.
    Let me see what happen if we increase pack limit 9 -> 30.
    I will increase this to 50 if this won't cause regression again.
    
    Original change's description:
    > git_cache: lower max num of .pack files before re-bootstrap on Win.
    >
    > It used to be 50, I think ~9 gives best results for Chromium on Win:
    >   on golo VM, it takes <4 minutes to re-boostrap + git fetch small
    >   delta, assuming zipped git checkout for bootstrap is fresh (~1day).
    >
    > For other repos, which are significantly smaller, this change should
    > have minor effect if at all.
    >
    > Test: I tested this using `led` tool on Win7 machines running LUCI
    > stack extensively. For example,
    >
    >   * https://ci.chromium.org/swarming/task/3a0102e8c8657410
    >     shows case with few .pack files, hence just 1 fetch
    >
    >   * https://ci.chromium.org/swarming/task/3a010282f9fd8010
    >     shows case with 39 .pack files and so bootstrapping + fetch.
    >     If you look at prior tasks on the same VM, you'd find this:
    >     https://ci.chromium.org/swarming/task/39ffe843d01ed010
    >     which spent 8 minutes doing 1 incremental fetch with 39 .pack
    >     files.
    >
    > **Troopers/Sheriffs**: This change is safe to revert.
    > However, beware that you should also at the same time revert the recipe
    > roll of this CL to the repo, in which the failed builder's recipe is
    > located, typically `chromium/tools/build`.
    >
    > Bug: 749709
    > Change-Id: I18e2b63283100d466e9fb981a9094862463f6909
    > Reviewed-on: https://chromium-review.googlesource.com/787174
    > Commit-Queue: Andrii Shyshkalov <tandrii@chromium.org>
    > Reviewed-by: Takuto Ikuta <tikuta@google.com>
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 749709
    Change-Id: I3052abe4a9b53277a60c0791a85355e7a0bbdf8f
    Reviewed-on: https://chromium-review.googlesource.com/823544
    Commit-Queue: Andrii Shyshkalov <tandrii@chromium.org>
    Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@google.com>

--- git_cache.py
+++ git_cache.py
@@ -26,8 +26,10 @@ import subcommand
 
 # Analogous to gc.autopacklimit git config.
 GC_AUTOPACKLIMIT = 50
+
+# TODO(tikuta): remove or revert this (crbug.com/749709)
 if sys.platform.startswith('win'):
-  GC_AUTOPACKLIMIT = 9
+  GC_AUTOPACKLIMIT = 30
 
 GIT_CACHE_CORRUPT_MESSAGE = 'WARNING: The Git cache is corrupt.'
 
