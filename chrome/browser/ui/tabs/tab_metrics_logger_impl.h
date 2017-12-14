5f42899b67475f355fd0bf6e23d909eaaa162ec6
Michael Giuffrida <michaelpg@chromium.org>
2017-12-14 00:08:00 +0000

    Add DefaultProtocolHandlers to tab activity UKMs
    The DefaultProtocolHandlers TabMetrics field is limited to schemes
    the user has accepted the site as the default protocol handler for.
    
    Bug: 784639
    Change-Id: I21f5c924a668c1b983744fcec4656c6b04245452
    Reviewed-on: https://chromium-review.googlesource.com/797355
    Commit-Queue: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Matt Giuca <mgiuca@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#523943}
    Former-commit-id: 33201b4fdc62be5c079bf7599dcca2d354af9f62

--- chrome/browser/ui/tabs/tab_metrics_logger_impl.h
+++ chrome/browser/ui/tabs/tab_metrics_logger_impl.h
@@ -5,7 +5,11 @@
 #ifndef CHROME_BROWSER_UI_TABS_TAB_METRICS_LOGGER_IMPL_H_
 #define CHROME_BROWSER_UI_TABS_TAB_METRICS_LOGGER_IMPL_H_
 
+#include <string>
+
+#include "base/gtest_prod_util.h"
 #include "base/macros.h"
+#include "chrome/browser/ui/tabs/tab_metrics_event.pb.h"
 #include "chrome/browser/ui/tabs/tab_metrics_logger.h"
 
 // Logs a TabManager.TabMetrics UKM for a tab when requested. Includes
@@ -21,6 +25,13 @@ class TabMetricsLoggerImpl : public TabMetricsLogger {
                         const TabMetrics& tab_metrics) override;
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(TabMetricsLoggerImplTest, Schemes);
+  FRIEND_TEST_ALL_PREFIXES(TabMetricsLoggerImplTest, NonWhitelistedSchemes);
+
+  // Returns the ProtocolHandlerScheme enumerator matching the string.
+  static metrics::TabMetricsEvent::ProtocolHandlerScheme
+  GetSchemeValueFromString(const std::string& scheme);
+
   // A counter to be incremented and logged with each UKM entry, used to
   // indicate the order that events within the same report were logged.
   int sequence_id_ = 0;
