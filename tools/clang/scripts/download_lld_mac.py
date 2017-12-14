7a198182542d9b1b76394bc2bf412406a05661f0
Nico Weber <thakis@chromium.org>
2017-12-14 16:34:22 +0000

    mac: Remove now-unused code from lld downlod script.
    No intended behavior change.
    
    Bug: 774209
    Change-Id: Iffa593253058c5ab8a81712be216b903be23b0c4
    Reviewed-on: https://chromium-review.googlesource.com/826173
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524083}
    Former-commit-id: 9ddc34d682f7318225e1d3591bf081a6f393d476

--- tools/clang/scripts/download_lld_mac.py
+++ tools/clang/scripts/download_lld_mac.py
@@ -13,17 +13,13 @@ import sys
 import update
 
 LLVM_BUILD_DIR = update.LLVM_BUILD_DIR
-LLD_PATH = os.path.join(LLVM_BUILD_DIR, 'bin', 'lld')
-LD_LLD_PATH = os.path.join(LLVM_BUILD_DIR, 'bin', 'ld.lld')
 LLD_LINK_PATH = os.path.join(LLVM_BUILD_DIR, 'bin', 'lld-link')
 
 
 def AlreadyUpToDate():
-  if not os.path.exists(LLD_PATH):
+  if not os.path.exists(LLD_LINK_PATH):
     return False
-  # lld-link seems to have no flag to query the version; go to ld.lld instead.
-  # TODO(thakis): Remove ld.lld workaround if https://llvm.org/PR34955 is fixed.
-  lld_rev = subprocess.check_output([LD_LLD_PATH, '--version'])
+  lld_rev = subprocess.check_output([LLD_LINK_PATH, '--version'])
   return (re.match(r'LLD.*\(trunk (\d+)\)', lld_rev).group(1) ==
              update.CLANG_REVISION)
 
@@ -31,17 +27,10 @@ def AlreadyUpToDate():
 def main():
   if AlreadyUpToDate():
     return 0
-
   remote_path = '%s/Mac/lld-%s.tgz' % (update.CDS_URL, update.PACKAGE_VERSION)
   update.DownloadAndUnpack(remote_path, update.LLVM_BUILD_DIR)
-
-  # TODO(thakis): Remove this once the lld tgz includes the symlink.
-  if os.path.exists(LLD_LINK_PATH):
-    os.remove(LLD_LINK_PATH)
-    os.remove(LD_LLD_PATH)
-  os.symlink('lld', LLD_LINK_PATH)
-  os.symlink('lld', LD_LLD_PATH)
   return 0
+
 
 if __name__ == '__main__':
   sys.exit(main())
