872b752989c91c36360d5abd37aefce10fdf4a0c
Peter Laurens <peterlaurens@chromium.org>
2017-12-14 22:59:00 +0000

    Revert "Unescape fragment for display in Omnibox"
    This reverts commit 32dbd81a78c7e132f2e6f02e10c036dcc1fa053c [formerly f8625da2e7215aaac6c0c76a636ab8483090943d].
    
    Reason for revert: This change breaks EGTests for iOS Simulator and Device, see:
    
    https://logs.chromium.org/v/?s=chromium%2Fbb%2Fchromium.mac%2Fios-simulator-full-configs%2F1434%2F%2B%2Frecipes%2Fsteps%2Fios_chrome_web_egtests__iPhone_6s_Plus_iOS_10.0_%2F0%2Fstdout
    
    Original change's description:
    > Unescape fragment for display in Omnibox
    > 
    > A change in Chrome 63 causes Chrome to urlencode the fragment component
    > of URLs. The Chrome omnibox should decode the fragment component as it
    > does for other URL components and matching Firefox.
    > 
    > Bug: 789163
    > Change-Id: If82d72445d904495c8d3e4129ab63be141745328
    > Reviewed-on: https://chromium-review.googlesource.com/822819
    > Commit-Queue: Eric Lawrence <elawrence@chromium.org>
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#524123}
    
    TBR=pkasting@chromium.org,elawrence@chromium.org
    
    Change-Id: I9572e8a5d66381478c1faca0f78a66b878a4b059
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 789163
    Reviewed-on: https://chromium-review.googlesource.com/828080
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524221}
    Former-commit-id: e486ef03b1649a5749cd875b03898b2b1600c055

--- components/url_formatter/url_formatter.cc
+++ components/url_formatter/url_formatter.cc
@@ -586,10 +586,11 @@ base::string16 FormatUrlWithAdjustments(
                              NonHostComponentTransform(unescape_rules),
                              &url_string, &new_parsed->query, adjustments);
 
+    // Ref.  This is valid, unescaped UTF-8, so we can just convert.
     if (parsed.ref.is_valid())
       url_string.push_back('#');
     AppendFormattedComponent(spec, parsed.ref,
-                             NonHostComponentTransform(unescape_rules),
+                             NonHostComponentTransform(net::UnescapeRule::NONE),
                              &url_string, &new_parsed->ref, adjustments);
   }
 
