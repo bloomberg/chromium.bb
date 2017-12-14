5c5f8812facc5ac9178a9b1afe3ddcd5921fdcce
Steven Bennetts <stevenjb@chromium.org>
2017-12-14 17:54:38 +0000

    AppWindowApiTest.Properties: Elim flakiness
    This test was assuming that calling minimize() and maximize() followed
    by restore() without waiting for onMinimized or onMaximized would
    trigger onRestored. This is not the case on Mash, and is also a likely
    cause of flake on other platforms.
    
    Bug: 232330,678687,794771
    Change-Id: I2a6a0f1e0b527583add555ed5ab8bfdc3a073df6
    Reviewed-on: https://chromium-review.googlesource.com/821310
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524110}
    Former-commit-id: eefdc769289d2fe0de931f6626c94aa34d0292bd

--- chrome/test/data/extensions/platform_apps/windows_api_properties/main.js
+++ chrome/test/data/extensions/platform_apps/windows_api_properties/main.js
@@ -33,34 +33,73 @@ function makeEventTest(eventName, startFunction) {
 }
 
 
-var tests = [
-
-  makeEventTest('onMinimized', function() { current.minimize(); }),
-  makeEventTest('onMaximized', function() { current.maximize(); }),
-  makeEventTest('onRestored', function() {
-    current.minimize();
-    current.restore();
-  }),
-  makeEventTest('onRestored', function() {
-    current.maximize();
-    current.restore();
-  }),
-  makeEventTest('onBoundsChanged', function() {
-    current.outerBounds.setPosition(5, 5);
-  }),
-  makeEventTest('onBoundsChanged', function() {
-    current.outerBounds.setSize(150, 150);
-  }),
-  makeEventTest('onBoundsChanged', function() {
-    current.innerBounds.setPosition(40, 40);
-  }),
-  makeEventTest('onBoundsChanged', function() {
-    current.innerBounds.setSize(100, 100);
-  })
-
-];
+var tests = {
+  minimized: [
+    makeEventTest(
+        'onMinimized',
+        function() {
+          current.minimize();
+        }),
+  ],
+  maximized: [
+    makeEventTest(
+        'onMaximized',
+        function() {
+          current.maximize();
+        }),
+  ],
+  restored: [
+    makeEventTest(
+        'onRestored',
+        function() {
+          var doRestore = function() {
+            current.onMinimized.removeListener(doRestore);
+            current.restore();
+          };
+          current.onMinimized.addListener(doRestore);
+          current.minimize();
+        }),
+    makeEventTest(
+        'onRestored',
+        function() {
+          var doRestore = function() {
+            current.onMaximized.removeListener(doRestore);
+            current.restore();
+          };
+          current.onMaximized.addListener(doRestore);
+          current.maximize();
+        })
+  ],
+  boundsChanged: [
+    makeEventTest(
+        'onBoundsChanged',
+        function() {
+          current.outerBounds.setPosition(5, 5);
+        }),
+    makeEventTest(
+        'onBoundsChanged',
+        function() {
+          current.outerBounds.setSize(150, 150);
+        }),
+    makeEventTest(
+        'onBoundsChanged',
+        function() {
+          current.innerBounds.setPosition(40, 40);
+        }),
+    makeEventTest(
+        'onBoundsChanged',
+        function() {
+          current.innerBounds.setSize(100, 100);
+        })
+  ],
+};
 
 chrome.runtime.getBackgroundPage(function(page) {
   bg = page;
-  chrome.test.runTests(tests);
+  chrome.test.getConfig(function(config) {
+    if (config.customArg in tests)
+      chrome.test.runTests(tests[config.customArg]);
+    else
+      chrome.test.fail('Test "' + config.customArg + '"" doesnt exist!');
+  });
 });
