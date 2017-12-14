60fac781c9b2f28e270e3dcbcb6fc194b57c8aa6
Mikel Astiz <mastiz@chromium.org>
2017-12-14 16:20:42 +0100

    Clean up feature toggle PropagateFaviconsAcrossClientRedirects
    This particular logic doesn't seem responsible for the issues reported
    by users (linked bugs) and the UMA data for the two variations is rather
    neutral, with a minor (noise-level) increase in the number of mixups
    in the enabled group, according to the UMA metric we instrumented for
    this particular purpose.
    
    Hence, let's go ahead and remove the feature toggle.
    
    Bug: 759631
    Bug: 776346
    
    Change-Id: I5aa3c9f1f412faeb09734ce26cf59b1308cccda8
    Reviewed-on: https://chromium-review.googlesource.com/827077
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527613}
    Former-commit-id: 7922d8e220c93440eff1a98d556e0ee7810d2069

--- components/history/core/browser/history_backend.cc
+++ components/history/core/browser/history_backend.cc
@@ -77,10 +77,6 @@ namespace history {
 const base::Feature kAvoidStrippingRefFromFaviconPageUrls{
     "AvoidStrippingRefFromFaviconPageUrls", base::FEATURE_DISABLED_BY_DEFAULT};
 
-// TODO(crbug.com/759631): Clean this up after impact is measured via Finch.
-const base::Feature kPropagateFaviconsAcrossClientRedirects{
-    "PropagateFaviconsAcrossClientRedirects", base::FEATURE_ENABLED_BY_DEFAULT};
-
 namespace {
 
 void RunUnlessCanceled(
@@ -549,11 +545,7 @@ void HistoryBackend::AddPage(const HistoryAddPageArgs& request) {
             db_->UpdateVisitRow(visit_row);
           }
 
-          if (base::FeatureList::IsEnabled(
-                  kPropagateFaviconsAcrossClientRedirects)) {
-            extended_redirect_chain =
-                GetCachedRecentRedirects(request.referrer);
-          }
+          extended_redirect_chain = GetCachedRecentRedirects(request.referrer);
         }
       }
     }
