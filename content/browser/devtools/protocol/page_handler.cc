d6be64b4d0ff5a51f932ea5e76ccd6b91f2ca72f
Andrey Lushnikov <lushnikov@chromium.org>
2017-12-14 19:38:03 +0000

    DevTools: fix reload for data URLs.
    This patchs starts running browser-side reload for data URLs.
    Note: this means that "scriptToEvaluateOnLoad" argument will be ignored
    when reloading data URLs.
    
    BUG=754470
    R=dgozman
    
    Change-Id: Ib0a07a4395ea2aca881958587e82d18dded81d95
    Reviewed-on: https://chromium-review.googlesource.com/823702
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524144}
    Former-commit-id: bcd23d6f4f2986c65794188c2a0cc94e87d32b52

--- content/browser/devtools/protocol/page_handler.cc
+++ content/browser/devtools/protocol/page_handler.cc
@@ -291,8 +291,8 @@ Response PageHandler::Reload(Maybe<bool> bypassCache,
   WebContentsImpl* web_contents = GetWebContents();
   if (!web_contents)
     return Response::InternalError();
-
   if (web_contents->IsCrashed() ||
+      web_contents->GetURL().scheme() == url::kDataScheme ||
       (web_contents->GetController().GetVisibleEntry() &&
        web_contents->GetController().GetVisibleEntry()->IsViewSourceMode())) {
     web_contents->GetController().Reload(bypassCache.fromMaybe(false)
