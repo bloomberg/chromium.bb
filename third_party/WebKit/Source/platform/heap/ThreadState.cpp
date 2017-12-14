cea2e3de8bd5b3c8e237fb7d9a10f284e55e3b81
Robert Flack <flackr@chromium.org>
2017-12-14 13:52:38 +0000

    Revert "[oilpan] Move incremental marking behind a build-time flag"
    This reverts commit 6b5514c7fe9e46ca1dd4c10a30adef77efc6173f [formerly 9966b73a986a5de99d3ba51d0d587b17fd0b938d].
    
    Reason for revert: I suspect this caused the compile failure here:
    https://ci.chromium.org/buildbot/chromium.webkit/WebKit%20Win%20x64%20Builder%20(dbg)/116281
    
    Original change's description:
    > [oilpan] Move incremental marking behind a build-time flag
    > 
    > Move Oilpan's incremental marking work behind a build-time flag
    >   enable_blink_heap_incremental_marking
    > 
    > Bug: chromium:757440
    > Change-Id: Id4adddcf0669dc165935eced90e8caefbb76a094
    > Reviewed-on: https://chromium-review.googlesource.com/822492
    > Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    > Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#524051}
    
    TBR=jbroman@chromium.org,dpranke@chromium.org,haraken@chromium.org,mlippautz@chromium.org
    
    Change-Id: I15da11c96b51ede50f4fb0742d6859117b2d4175
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:757440
    Reviewed-on: https://chromium-review.googlesource.com/826699
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524055}
    Former-commit-id: 75b6ebb7b08a088bae14304c6ecdae6dab66dd6f

--- third_party/WebKit/Source/platform/heap/ThreadState.cpp
+++ third_party/WebKit/Source/platform/heap/ThreadState.cpp
@@ -44,7 +44,6 @@
 #include "platform/heap/Handle.h"
 #include "platform/heap/Heap.h"
 #include "platform/heap/HeapCompact.h"
-#include "platform/heap/IncrementalMarking.h"
 #include "platform/heap/PagePool.h"
 #include "platform/heap/SafePoint.h"
 #include "platform/heap/Visitor.h"
@@ -365,14 +364,15 @@ bool ThreadState::JudgeGCThreshold(size_t allocated_object_size_threshold,
 }
 
 bool ThreadState::ShouldScheduleIncrementalMarking() const {
-#if BUILDFLAG(BLINK_HEAP_INCREMENTAL_MARKING)
+// TODO(mlippautz): Replace with proper build flag.
+#if 0
   // TODO(mlippautz): For now immediately schedule incremental marking if
   // the runtime flag is provided, basically exercising a stress test.
   return (GcState() == kNoGCScheduled || GcState() == kSweeping) &&
          RuntimeEnabledFeatures::HeapIncrementalMarkingEnabled();
 #else
   return false;
-#endif  // BUILDFLAG(BLINK_HEAP_INCREMENTAL_MARKING)
+#endif
 }
 
 bool ThreadState::ShouldScheduleIdleGC() {
