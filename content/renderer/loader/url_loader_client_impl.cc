d28b95b99dfcf529223f249a68ef5e5216543c1c
Yutaka Hirano <yhirano@chromium.org>
2017-12-14 06:48:16 +0000

    Remove IPC::Message usages from content::URLLoaderClientImpl
    We've been using IPC::Message in URLLoaderClientImpl to implement the
    "deferred loading" feature. This CL replaces it with custom containers
    having callback parameters, in order to remove IPC messages defined in
    resources_messages.h.
    
    Bug: 788241
    Change-Id: I3b5863aa2604c2f3d56c3c0481d88ef865f91b28
    Reviewed-on: https://chromium-review.googlesource.com/826882
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524013}
    Former-commit-id: 3dad2835bcb87db92086ea0d5e431b56c36479b2

--- content/renderer/loader/url_loader_client_impl.cc
+++ content/renderer/loader/url_loader_client_impl.cc
@@ -4,6 +4,8 @@
 
 #include "content/renderer/loader/url_loader_client_impl.h"
 
+#include <iterator>
+
 #include "base/callback.h"
 #include "base/single_thread_task_runner.h"
 #include "content/common/resource_messages.h"
@@ -12,6 +14,112 @@
 #include "net/url_request/redirect_info.h"
 
 namespace content {
+
+class URLLoaderClientImpl::DeferredMessage {
+ public:
+  DeferredMessage() = default;
+  virtual void HandleMessage(ResourceDispatcher* dispatcher,
+                             int request_id) = 0;
+  virtual bool IsCompletionMessage() const = 0;
+  virtual ~DeferredMessage() = default;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(DeferredMessage);
+};
+
+class URLLoaderClientImpl::DeferredOnReceiveResponse final
+    : public DeferredMessage {
+ public:
+  explicit DeferredOnReceiveResponse(const ResourceResponseHead& response_head)
+      : response_head_(response_head) {}
+
+  void HandleMessage(ResourceDispatcher* dispatcher, int request_id) override {
+    dispatcher->OnReceivedResponse(request_id, response_head_);
+  }
+  bool IsCompletionMessage() const override { return false; }
+
+ private:
+  const ResourceResponseHead response_head_;
+};
+
+class URLLoaderClientImpl::DeferredOnReceiveRedirect final
+    : public DeferredMessage {
+ public:
+  DeferredOnReceiveRedirect(const net::RedirectInfo& redirect_info,
+                            const ResourceResponseHead& response_head)
+      : redirect_info_(redirect_info), response_head_(response_head) {}
+
+  void HandleMessage(ResourceDispatcher* dispatcher, int request_id) override {
+    dispatcher->OnReceivedRedirect(request_id, redirect_info_, response_head_);
+  }
+  bool IsCompletionMessage() const override { return false; }
+
+ private:
+  const net::RedirectInfo redirect_info_;
+  const ResourceResponseHead response_head_;
+};
+
+class URLLoaderClientImpl::DeferredOnDataDownloaded final
+    : public DeferredMessage {
+ public:
+  DeferredOnDataDownloaded(int64_t data_length, int64_t encoded_data_length)
+      : data_length_(data_length), encoded_data_length_(encoded_data_length) {}
+
+  void HandleMessage(ResourceDispatcher* dispatcher, int request_id) override {
+    dispatcher->OnDownloadedData(request_id, data_length_,
+                                 encoded_data_length_);
+  }
+  bool IsCompletionMessage() const override { return false; }
+
+ private:
+  const int64_t data_length_;
+  const int64_t encoded_data_length_;
+};
+
+class URLLoaderClientImpl::DeferredOnUploadProgress final
+    : public DeferredMessage {
+ public:
+  DeferredOnUploadProgress(int64_t current, int64_t total)
+      : current_(current), total_(total) {}
+
+  void HandleMessage(ResourceDispatcher* dispatcher, int request_id) override {
+    dispatcher->OnDownloadedData(request_id, current_, total_);
+  }
+  bool IsCompletionMessage() const override { return false; }
+
+ private:
+  const int64_t current_;
+  const int64_t total_;
+};
+
+class URLLoaderClientImpl::DeferredOnReceiveCachedMetadata final
+    : public DeferredMessage {
+ public:
+  explicit DeferredOnReceiveCachedMetadata(const std::vector<uint8_t>& data)
+      : data_(data) {}
+
+  void HandleMessage(ResourceDispatcher* dispatcher, int request_id) override {
+    dispatcher->OnReceivedCachedMetadata(request_id, data_);
+  }
+  bool IsCompletionMessage() const override { return false; }
+
+ private:
+  const std::vector<uint8_t> data_;
+};
+
+class URLLoaderClientImpl::DeferredOnComplete final : public DeferredMessage {
+ public:
+  explicit DeferredOnComplete(const network::URLLoaderCompletionStatus& status)
+      : status_(status) {}
+
+  void HandleMessage(ResourceDispatcher* dispatcher, int request_id) override {
+    dispatcher->OnRequestComplete(request_id, status_);
+  }
+  bool IsCompletionMessage() const override { return true; }
+
+ private:
+  const network::URLLoaderCompletionStatus status_;
+};
 
 URLLoaderClientImpl::URLLoaderClientImpl(
     int request_id,
@@ -40,7 +148,7 @@ void URLLoaderClientImpl::UnsetDefersLoading() {
 
 void URLLoaderClientImpl::FlushDeferredMessages() {
   DCHECK(!is_deferred_);
-  std::vector<IPC::Message> messages;
+  std::vector<std::unique_ptr<DeferredMessage>> messages;
   messages.swap(deferred_messages_);
   bool has_completion_message = false;
   base::WeakPtr<URLLoaderClientImpl> weak_this = weak_factory_.GetWeakPtr();
@@ -49,19 +157,22 @@ void URLLoaderClientImpl::FlushDeferredMessages() {
   //  - transfer size change (dispatched later)
   //  - completion (dispatched by |body_consumer_| or dispatched later)
   for (size_t index = 0; index < messages.size(); ++index) {
-    if (messages[index].type() == ResourceMsg_RequestComplete::ID) {
+    if (messages[index]->IsCompletionMessage()) {
       // The completion message arrives at the end of the message queue.
       DCHECK(!has_completion_message);
       DCHECK_EQ(index, messages.size() - 1);
       has_completion_message = true;
       break;
     }
-    resource_dispatcher_->DispatchMessage(messages[index]);
+
+    messages[index]->HandleMessage(resource_dispatcher_, request_id_);
     if (!weak_this)
       return;
     if (is_deferred_) {
-      deferred_messages_.insert(deferred_messages_.begin(),
-                                messages.begin() + index + 1, messages.end());
+      deferred_messages_.insert(
+          deferred_messages_.begin(),
+          std::make_move_iterator(messages.begin()) + index + 1,
+          std::make_move_iterator(messages.end()));
       return;
     }
   }
@@ -77,8 +188,7 @@ void URLLoaderClientImpl::FlushDeferredMessages() {
     if (is_deferred_) {
       if (has_completion_message) {
         DCHECK_GT(messages.size(), 0u);
-        DCHECK_EQ(messages.back().type(),
-                  static_cast<uint32_t>(ResourceMsg_RequestComplete::ID));
+        DCHECK(messages.back()->IsCompletionMessage());
         deferred_messages_.emplace_back(std::move(messages.back()));
       }
       return;
@@ -97,10 +207,8 @@ void URLLoaderClientImpl::FlushDeferredMessages() {
   // Dispatch the completion message.
   if (has_completion_message) {
     DCHECK_GT(messages.size(), 0u);
-    DCHECK_EQ(messages.back().type(),
-              static_cast<uint32_t>(ResourceMsg_RequestComplete::ID));
-
-    resource_dispatcher_->DispatchMessage(messages.back());
+    DCHECK(messages.back()->IsCompletionMessage());
+    messages.back()->HandleMessage(resource_dispatcher_, request_id_);
   }
 }
 
@@ -117,10 +225,12 @@ void URLLoaderClientImpl::OnReceiveResponse(
     mojom::DownloadedTempFilePtr downloaded_file) {
   has_received_response_ = true;
   downloaded_file_ = std::move(downloaded_file);
-  if (NeedsStoringMessage())
-    StoreAndDispatch(ResourceMsg_ReceivedResponse(request_id_, response_head));
-  else
+  if (NeedsStoringMessage()) {
+    StoreAndDispatch(
+        std::make_unique<DeferredOnReceiveResponse>(response_head));
+  } else {
     resource_dispatcher_->OnReceivedResponse(request_id_, response_head);
+  }
 }
 
 void URLLoaderClientImpl::OnReceiveRedirect(
@@ -129,8 +239,8 @@ void URLLoaderClientImpl::OnReceiveRedirect(
   DCHECK(!has_received_response_);
   DCHECK(!body_consumer_);
   if (NeedsStoringMessage()) {
-    StoreAndDispatch(ResourceMsg_ReceivedRedirect(request_id_, redirect_info,
-                                                  response_head));
+    StoreAndDispatch(std::make_unique<DeferredOnReceiveRedirect>(
+        redirect_info, response_head));
   } else {
     resource_dispatcher_->OnReceivedRedirect(request_id_, redirect_info,
                                              response_head);
@@ -141,17 +251,31 @@ void URLLoaderClientImpl::OnDataDownloaded(int64_t data_len,
                                            int64_t encoded_data_len) {
   if (NeedsStoringMessage()) {
     StoreAndDispatch(
-        ResourceMsg_DataDownloaded(request_id_, data_len, encoded_data_len));
+        std::make_unique<DeferredOnDataDownloaded>(data_len, encoded_data_len));
   } else {
     resource_dispatcher_->OnDownloadedData(request_id_, data_len,
                                            encoded_data_len);
   }
 }
 
+void URLLoaderClientImpl::OnUploadProgress(
+    int64_t current_position,
+    int64_t total_size,
+    OnUploadProgressCallback ack_callback) {
+  if (NeedsStoringMessage()) {
+    StoreAndDispatch(std::make_unique<DeferredOnUploadProgress>(
+        current_position, total_size));
+  } else {
+    resource_dispatcher_->OnUploadProgress(request_id_, current_position,
+                                           total_size);
+  }
+  std::move(ack_callback).Run();
+}
+
 void URLLoaderClientImpl::OnReceiveCachedMetadata(
     const std::vector<uint8_t>& data) {
   if (NeedsStoringMessage()) {
-    StoreAndDispatch(ResourceMsg_ReceivedCachedMetadata(request_id_, data));
+    StoreAndDispatch(std::make_unique<DeferredOnReceiveCachedMetadata>(data));
   } else {
     resource_dispatcher_->OnReceivedCachedMetadata(request_id_, data);
   }
@@ -186,7 +310,7 @@ void URLLoaderClientImpl::OnComplete(
   has_received_complete_ = true;
   if (!body_consumer_) {
     if (NeedsStoringMessage()) {
-      StoreAndDispatch(ResourceMsg_RequestComplete(request_id_, status));
+      StoreAndDispatch(std::make_unique<DeferredOnComplete>(status));
     } else {
       resource_dispatcher_->OnRequestComplete(request_id_, status);
     }
@@ -199,30 +323,17 @@ bool URLLoaderClientImpl::NeedsStoringMessage() const {
   return is_deferred_ || deferred_messages_.size() > 0;
 }
 
-void URLLoaderClientImpl::StoreAndDispatch(const IPC::Message& message) {
+void URLLoaderClientImpl::StoreAndDispatch(
+    std::unique_ptr<DeferredMessage> message) {
   DCHECK(NeedsStoringMessage());
   if (is_deferred_) {
-    deferred_messages_.push_back(message);
+    deferred_messages_.push_back(std::move(message));
   } else if (deferred_messages_.size() > 0) {
-    deferred_messages_.push_back(message);
+    deferred_messages_.push_back(std::move(message));
     FlushDeferredMessages();
   } else {
     NOTREACHED();
   }
-}
-
-void URLLoaderClientImpl::OnUploadProgress(
-    int64_t current_position,
-    int64_t total_size,
-    OnUploadProgressCallback ack_callback) {
-  if (NeedsStoringMessage()) {
-    StoreAndDispatch(
-        ResourceMsg_UploadProgress(request_id_, current_position, total_size));
-  } else {
-    resource_dispatcher_->OnUploadProgress(request_id_, current_position,
-                                           total_size);
-  }
-  std::move(ack_callback).Run();
 }
 
 void URLLoaderClientImpl::OnConnectionClosed() {
