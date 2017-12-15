023a9e669f4f73c56206d1840630f4d114cacb81
Alexander Alekseev <alemate@chromium.org>
2017-12-15 02:06:33 +0000

    Chrome OS Network UI: forget selected network when it disappears from the list.
    This CL fixes error message:
    
        Error in response to networkingPrivate.getNetworks: TypeError: Cannot read
        property 'GUID' of null", source: chrome://oobe/oobe (0)
    
    and clears default network if it disappears from the list of networks.
    
    Bug: 793262
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ia72e650b73202de56b8ec842dbcee81f65366793
    Reviewed-on: https://chromium-review.googlesource.com/822171
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524287}
    Former-commit-id: d0b408d6d09a69f9ef3e618530fe87540ec00fc7

--- chrome/browser/resources/chromeos/login/oobe_welcome.js
+++ chrome/browser/resources/chromeos/login/oobe_welcome.js
@@ -326,7 +326,7 @@ Polymer({
    */
   onDefaultNetworkChanged_: function(event) {
     var state = event.detail;
-    this.defaultNetworkGuid_ = state.GUID;
+    this.defaultNetworkGuid_ = (state ? state.GUID : '');
     this.isConnected_ =
         !!state && state.ConnectionState == CrOnc.ConnectionState.CONNECTED;
     if (!state || state.GUID != this.networkLastSelectedGuid_)
@@ -346,6 +346,7 @@ Polymer({
    */
   onNetworkListNetworkItemSelected_: function(event) {
     var state = event.detail;
+    assert(state);
     // If the user has not previously made a selection and the default network
     // is selected and connected, continue to the next screen.
     if (this.networkLastSelectedGuid_ == '' &&
@@ -359,7 +360,10 @@ Polymer({
     // is pending connection attempt. So even if new selection is currently
     // connected, it may get disconnected at any time.
     // So just send one more connection request to cancel current attempts.
-    this.networkLastSelectedGuid_ = state.GUID;
+    this.networkLastSelectedGuid_ = (state ? state.GUID : '');
+
+    if (!state)
+      return;
 
     var self = this;
     var networkStateCopy = Object.assign({}, state);
