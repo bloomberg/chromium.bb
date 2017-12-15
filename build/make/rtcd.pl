aecbba6d678356299663a18560c8fa2c36aaeefe
Johann <johannkoenig@google.com>
2017-12-15 09:03:23 -0800

    add copyright to rtcd files
    Allows them to pass the license check in chromium.
    
    Based on libvpx e4b3f03
    
    BUG=chromium:795297
    
    Change-Id: I2bb49ecb62f20d7bc5093a1732b6a8228ef5c87f

--- build/make/rtcd.pl
+++ build/make/rtcd.pl
@@ -1,5 +1,14 @@
 #!/usr/bin/env perl
-
+##
+## Copyright (c) 2017, Alliance for Open Media. All rights reserved
+##
+## This source code is subject to the terms of the BSD 2 Clause License and
+## the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
+## was not distributed with this source code in the LICENSE file, you can
+## obtain it at www.aomedia.org/license/software. If the Alliance for Open
+## Media Patent License 1.0 was not distributed with this source code in the
+## PATENTS file, you can obtain it at www.aomedia.org/license/patent.
+##
 no strict 'refs';
 use warnings;
 use Getopt::Long;
