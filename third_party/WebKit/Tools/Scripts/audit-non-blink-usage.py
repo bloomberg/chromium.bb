0298b82ba511ecabe49616595bb15a33fdbfe65f
Yuta Kitamura <yutak@chromium.org>
2017-12-15 11:22:49 +0000

    Allow base::Location in Blink.
    We'd like to use base::Location and FROM_HERE in Blink instead of
    blink::WebTraceLocation and BLINK_FROM_HERE.
    
    Regarding a DEPS change in public/platform/scheduler, the only reference
    of those names in public/ is in that directory, so I updated the DEPS
    file there instead of the one in public/.
    
    Bug: 794845
    Change-Id: I4249abcb727186281229f41a0c81df649de8fbf7
    Reviewed-on: https://chromium-review.googlesource.com/826924
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Taiju Tsuiki <tzik@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524359}
    Former-commit-id: 137c61be2db3cbbbdf512f5d9a3be7ef302c08df

--- third_party/WebKit/Tools/Scripts/audit-non-blink-usage.py
+++ third_party/WebKit/Tools/Scripts/audit-non-blink-usage.py
@@ -31,6 +31,7 @@ _CONFIG = [
 
             # //base constructs that are allowed everywhere
             'base::AdoptRef',
+            'base::Location',
             'base::MakeRefCounted',
             'base::Optional',
             'base::SingleThreadTaskRunner',
