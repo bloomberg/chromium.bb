403003fa2d40c1efb12d277fbec33b49d4d10042
David Benjamin <davidben@chromium.org>
2017-12-15 21:23:17 +0000

    Add a regression test for normalizing custom attributes.
    A recent BoringSSL change broke this. Add a regression test for it
    before I try to make the change again.
    
    Bug: 789802
    Change-Id: Ibe021424fc6b36b2ef71d91b30f523a2cfae1482
    Reviewed-on: https://chromium-review.googlesource.com/828381
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524467}
    Former-commit-id: c4dd10518c291c20a9881e6720d8b9a9505c2010

--- net/cert/internal/verify_name_match_unittest.cc
+++ net/cert/internal/verify_name_match_unittest.cc
@@ -597,4 +597,16 @@ TEST(NameNormalizationTest, TestEverything) {
   EXPECT_EQ(normalized_der, renormalized_der);
 }
 
+// Unknown AttributeValue types normalize as-is, even non-primitive tags.
+TEST(NameNormalizationTest, NormalizeCustom) {
+  std::string raw_der;
+  ASSERT_TRUE(LoadTestData("custom", "custom", "normalized", &raw_der));
+
+  std::string normalized_der;
+  CertErrors errors;
+  ASSERT_TRUE(NormalizeName(SequenceValueFromString(&raw_der), &normalized_der,
+                            &errors));
+  EXPECT_EQ(SequenceValueFromString(&raw_der), der::Input(&normalized_der));
+}
+
 }  // namespace net
