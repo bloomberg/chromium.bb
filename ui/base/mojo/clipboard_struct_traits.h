40a1df14fe166e6521219d4eb55108d2f9a85a94
Stuart Langley <slangley@chromium.org>
2017-12-16 05:27:56 +0000

    Use typemap to convert blink::mojom::ClipboardBuffer to ui::ClipboardType.
    We removed this typemap when moving the mojom into WebKit, now we add it
    back to do the marshaling between ClipboardBuffer and ClipboardType on
    the browser side.
    
    As we don't intend at this time to send messages of this type in the
    opposite direction we only implement the FromMojom method in
    clipboard_struct_traits.
    
    Bug: 775830
    Change-Id: Ib3dacbcc5b3c029167e902afbf7c8c5c809f1ee9
    Reviewed-on: https://chromium-review.googlesource.com/823542
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524593}
    Former-commit-id: edf12cc9a016371ca930e0771acf164bdffbcd17

---
+++ ui/base/mojo/clipboard_struct_traits.h
@@ -0,0 +1,42 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_BASE_MOJO_CLIPBOARD_STRUCT_TRAITS_H_
+#define UI_BASE_MOJO_CLIPBOARD_STRUCT_TRAITS_H_
+
+#include "third_party/WebKit/common/clipboard/clipboard.mojom-shared.h"
+#include "ui/base/clipboard/clipboard_types.h"
+
+namespace mojo {
+
+template <>
+struct EnumTraits<blink::mojom::ClipboardBuffer, ui::ClipboardType> {
+  static blink::mojom::ClipboardBuffer ToMojom(ui::ClipboardType) {
+    // We never intend on converting from ui::Clipboardtype to
+    // blink::mojom::ClipboardBuffer.
+    NOTREACHED();
+    return blink::mojom::ClipboardBuffer::kStandard;
+  }
+
+  static bool FromMojom(blink::mojom::ClipboardBuffer clipboard_type,
+                        ui::ClipboardType* out) {
+    switch (clipboard_type) {
+      case blink::mojom::ClipboardBuffer::kStandard:
+        *out = ui::CLIPBOARD_TYPE_COPY_PASTE;
+        return true;
+      case blink::mojom::ClipboardBuffer::kSelection:
+#if defined(USE_X11)
+        *out = ui::CLIPBOARD_TYPE_SELECTION;
+        return true;
+#else
+        return false;
+#endif
+    }
+    return false;
+  }
+};
+
+}  // namespace mojo
+
+#endif  // UI_BASE_MOJO_CLIPBOARD_STRUCT_TRAITS_H_
