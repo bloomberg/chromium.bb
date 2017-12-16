0f661e3e3b8b5ad3a71b36245dc5b116fe33861d
Tom Anderson <thomasanderson@chromium.org>
2017-12-16 00:19:13 +0000

    Fix freetype unbundle
    The freetype unbundle build file is out-of-date, and trying to use it
    will result in errors like this:
    
    ERROR Unresolved dependencies.
    //third_party:freetype_harfbuzz(//build/toolchain/linux/unbundle:default)
      needs //third_party/freetype:freetype_source(//build/toolchain/linux/unbundle:default)
    
    This CL changes the unbundled build file to simply toggle the
    use_system_freetype flag.
    
    R=thestig@chromium.org
    CC=phajdan.jr@chromium.org
    
    Change-Id: If38ccc51098857cd479707879562b23f8f32e85f
    Reviewed-on: https://chromium-review.googlesource.com/830974
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524535}
    Former-commit-id: a7b961302726f6accc1d70682017b6c36127ae7a

--- build/linux/unbundle/replace_gn_files.py
+++ build/linux/unbundle/replace_gn_files.py
@@ -20,7 +20,7 @@ REPLACEMENTS = {
   'ffmpeg': 'third_party/ffmpeg/BUILD.gn',
   'flac': 'third_party/flac/BUILD.gn',
   'fontconfig': 'third_party/fontconfig/BUILD.gn',
-  'freetype': 'third_party/freetype/BUILD.gn',
+  'freetype': 'build/config/freetype/freetype.gni',
   'harfbuzz-ng': 'third_party/harfbuzz-ng/BUILD.gn',
   'icu': 'third_party/icu/BUILD.gn',
   'libdrm': 'third_party/libdrm/BUILD.gn',
