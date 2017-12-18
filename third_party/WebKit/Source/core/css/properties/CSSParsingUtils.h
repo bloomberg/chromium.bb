1effbf008eb6e0a42f9a98ce0cf9e6da9dd10263
Renee Wright <rjwright@chromium.org>
2017-12-18 13:05:08 +0000

    [Ribbon] Remove CSSProperty groups
    Gist for gen diff here:
    
    https: //gist.github.com/rjwright/d32835d21f8b313c46ec7a1c6d4d0155/revisions
    Bug: 788554
    Change-Id: Ia19d78686e9742a91455fa044c69c22c94c05552
    Reviewed-on: https://chromium-review.googlesource.com/804645
    Commit-Queue: Renée Wright <rjwright@chromium.org>
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524692}
    Former-commit-id: e9cc36e4b58dfa03c0e882d6a9a9c8322e03a8f3

--- third_party/WebKit/Source/core/css/properties/CSSParsingUtils.h
+++ third_party/WebKit/Source/core/css/properties/CSSParsingUtils.h
@@ -73,6 +73,16 @@ bool ConsumeBackgroundPosition(CSSParserTokenRange&,
                                CSSValue*& result_x,
                                CSSValue*& result_y);
 CSSValue* ConsumePrefixedBackgroundBox(CSSParserTokenRange&, AllowTextValue);
+CSSValue* ParseBackgroundBox(CSSParserTokenRange&,
+                             const CSSParserLocalContext&);
+CSSValue* ParseBackgroundOrMaskSize(CSSParserTokenRange&,
+                                    const CSSParserContext&,
+                                    const CSSParserLocalContext&);
+bool ParseBackgroundOrMask(bool,
+                           CSSParserTokenRange&,
+                           const CSSParserContext&,
+                           const CSSParserLocalContext&,
+                           HeapVector<CSSPropertyValue, 256>&);
 
 bool ConsumeRepeatStyleComponent(CSSParserTokenRange&,
                                  CSSValue*& value1,
@@ -97,6 +107,12 @@ CSSValue* ConsumeBorderImageRepeat(CSSParserTokenRange&);
 CSSValue* ConsumeBorderImageSlice(CSSParserTokenRange&, DefaultFill);
 CSSValue* ConsumeBorderImageWidth(CSSParserTokenRange&);
 CSSValue* ConsumeBorderImageOutset(CSSParserTokenRange&);
+
+CSSValue* ParseBorderRadiusCorner(CSSParserTokenRange&,
+                                  const CSSParserContext&);
+CSSValue* ParseBorderWidthSide(CSSParserTokenRange&,
+                               const CSSParserContext&,
+                               const CSSParserLocalContext&);
 
 CSSValue* ConsumeShadow(CSSParserTokenRange&,
                         CSSParserMode,
@@ -203,6 +219,8 @@ CSSValue* ConsumeBorderColorSide(CSSParserTokenRange&,
 CSSValue* ConsumeBorderWidth(CSSParserTokenRange&,
                              CSSParserMode,
                              CSSPropertyParserHelpers::UnitlessQuirk);
+CSSValue* ParsePaintStroke(CSSParserTokenRange&, const CSSParserContext&);
+CSSValue* ParseSpacing(CSSParserTokenRange&, const CSSParserContext&);
 
 template <CSSValueID start, CSSValueID end>
 CSSValue* ConsumePositionLonghand(CSSParserTokenRange& range,
