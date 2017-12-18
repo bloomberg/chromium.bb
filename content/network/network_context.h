761b29704d2a903b36dbcf5176941d32b4ddd66b
John Abd-El-Malek <jam@chromium.org>
2017-12-18 17:38:06 +0000

    Remove obsolete NetworkContext::DiskCheck code.
    Bug: 711579
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: If272f72850da85b7959df00de5c1b5f97600f520
    Reviewed-on: https://chromium-review.googlesource.com/830653
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524734}
    Former-commit-id: 55d8326fa7f2c875b4c6c40fd4fc1276fc90c1da

--- content/network/network_context.h
+++ content/network/network_context.h
@@ -13,7 +13,6 @@
 #include "base/callback.h"
 #include "base/macros.h"
 #include "base/time/time.h"
-#include "base/timer/timer.h"
 #include "content/common/content_export.h"
 #include "content/network/cookie_manager.h"
 #include "content/public/common/network_service.mojom.h"
@@ -144,24 +143,6 @@ class CONTENT_EXPORT NetworkContext : public mojom::NetworkContext {
   mojo::Binding<mojom::NetworkContext> binding_;
 
   std::unique_ptr<CookieManager> cookie_manager_;
-
-  // Temporary class to help diagnose the impact of https://crbug.com/711579.
-  // Every 24-hours, measures the size of the network cache and emits an UMA
-  // metric.
-  class DiskChecker {
-   public:
-    explicit DiskChecker(const base::FilePath& cache_path);
-    ~DiskChecker();
-
-   private:
-    void CheckDiskSize();
-    static void CheckDiskSizeOnBackgroundThread(
-        const base::FilePath& cache_path);
-    base::RepeatingTimer timer_;
-    base::FilePath cache_path_;
-    DISALLOW_COPY_AND_ASSIGN(DiskChecker);
-  };
-  std::unique_ptr<DiskChecker> disk_checker_;
 
   DISALLOW_COPY_AND_ASSIGN(NetworkContext);
 };
