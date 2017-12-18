158b1f77a63a7aea3619ff6bd99b1671ba45362a
Renee Wright <rjwright@chromium.org>
2017-12-18 14:02:16 +0000

    [Ribbon] Make unneeded constructors protected & leaf classes final
    Would have been nice to make the leaf constructors protected &
    singleton but they are called by global level declarations
    so too much messing around to be worth it.
    
    Bug: 
    Change-Id: I2b96460912af5a3ea6b4049ccb49c67b2d394a27
    Reviewed-on: https://chromium-review.googlesource.com/816780
    Commit-Queue: Renée Wright <rjwright@chromium.org>
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524701}
    Former-commit-id: b5d56567762b58996c1d1d1f045607e93984a382

--- third_party/WebKit/Source/core/css/properties/Shorthand.h
+++ third_party/WebKit/Source/core/css/properties/Shorthand.h
@@ -13,8 +13,6 @@ class CSSPropertyValue;
 
 class Shorthand : public CSSProperty {
  public:
-  constexpr Shorthand() : CSSProperty() {}
-
   // Parses and consumes entire shorthand value from the token range and adds
   // all constituent parsed longhand properties to the 'properties' set.
   // Returns false if the input is invalid.
@@ -28,6 +26,9 @@ class Shorthand : public CSSProperty {
     return false;
   }
   bool IsShorthand() const override { return true; }
+
+ protected:
+  constexpr Shorthand() : CSSProperty() {}
 };
 
 DEFINE_TYPE_CASTS(Shorthand,
