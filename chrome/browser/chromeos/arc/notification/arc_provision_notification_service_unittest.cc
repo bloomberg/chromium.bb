14ce7f98d85789cf02a50b358722b28a90e7fc0f
Evan Stade <estade@chromium.org>
2017-12-19 14:17:04 +0000

    Update ArcProvisionNotification for NotificationDisplayService.
    also,
    - simplify production code by removing unit test delegate
    - streamline unit test with use of BrowserWithTestWindowTest
    
    Bug: 783018
    Change-Id: I997312523f48bdd7a0cdeb20c563412afac526d6
    Reviewed-on: https://chromium-review.googlesource.com/823134
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525016}
    Former-commit-id: 627f0f0e3ca12a1e37e8724fc3ca3df33443267c

--- chrome/browser/chromeos/arc/notification/arc_provision_notification_service_unittest.cc
+++ chrome/browser/chromeos/arc/notification/arc_provision_notification_service_unittest.cc
@@ -9,15 +9,14 @@
 
 #include "base/bind.h"
 #include "base/command_line.h"
-#include "base/files/scoped_temp_dir.h"
 #include "base/values.h"
 #include "chrome/browser/chromeos/arc/arc_auth_notification.h"
 #include "chrome/browser/chromeos/arc/arc_optin_uma.h"
 #include "chrome/browser/chromeos/arc/arc_session_manager.h"
 #include "chrome/browser/chromeos/login/users/fake_chrome_user_manager.h"
+#include "chrome/browser/notifications/notification_display_service_tester.h"
+#include "chrome/test/base/browser_with_test_window_test.h"
 #include "chrome/test/base/testing_profile.h"
-#include "chromeos/dbus/dbus_thread_manager.h"
-#include "chromeos/dbus/fake_session_manager_client.h"
 #include "components/arc/arc_prefs.h"
 #include "components/arc/arc_service_manager.h"
 #include "components/arc/arc_util.h"
@@ -26,60 +25,38 @@
 #include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "components/user_manager/scoped_user_manager.h"
 #include "components/user_manager/user_manager.h"
-#include "content/public/test/test_browser_thread_bundle.h"
-#include "testing/gmock/include/gmock/gmock.h"
-#include "testing/gtest/include/gtest/gtest.h"
-
-using testing::AnyNumber;
-using testing::AtLeast;
-using testing::Mock;
-using testing::StrictMock;
+#include "ui/message_center/notification.h"
 
 namespace arc {
 
 namespace {
 
-class MockArcProvisionNotificationServiceDelegate
-    : public ArcProvisionNotificationService::Delegate {
- public:
-  MOCK_METHOD0(ShowManagedProvisionNotification, void());
-  MOCK_METHOD0(RemoveManagedProvisionNotification, void());
-};
+const char kArcManagedProvisionNotificationId[] = "arc_managed_provision";
 
-class ArcProvisionNotificationServiceTest : public testing::Test {
+class ArcProvisionNotificationServiceTest : public BrowserWithTestWindowTest {
  protected:
   ArcProvisionNotificationServiceTest()
       : user_manager_enabler_(
             std::make_unique<chromeos::FakeChromeUserManager>()) {}
 
   void SetUp() override {
-    chromeos::DBusThreadManager::GetSetterForTesting()->SetSessionManagerClient(
-        std::make_unique<chromeos::FakeSessionManagerClient>());
-    chromeos::DBusThreadManager::Initialize();
-
     SetArcAvailableCommandLineForTesting(
         base::CommandLine::ForCurrentProcess());
     ArcSessionManager::DisableUIForTesting();
     ArcAuthNotification::DisableForTesting();
 
-    ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
-    TestingProfile::Builder profile_builder;
-    profile_builder.SetProfileName("user@gmail.com");
-    profile_builder.SetPath(temp_dir_.GetPath().AppendASCII("TestArcProfile"));
-    profile_ = profile_builder.Build();
-
     arc_service_manager_ = std::make_unique<ArcServiceManager>();
     arc_session_manager_ = std::make_unique<ArcSessionManager>(
         std::make_unique<ArcSessionRunner>(base::Bind(FakeArcSession::Create)));
-    std::unique_ptr<MockArcProvisionNotificationServiceDelegate>
-        mock_arc_provision_notification_service_delegate = std::make_unique<
-            StrictMock<MockArcProvisionNotificationServiceDelegate>>();
-    arc_provision_notification_service_delegate_ =
-        mock_arc_provision_notification_service_delegate.get();
-    arc_provision_notification_service_ =
-        ArcProvisionNotificationService::CreateForTesting(
-            profile_.get(),
-            std::move(mock_arc_provision_notification_service_delegate));
+
+    // This creates |profile()|, so it has to come after the arc managers.
+    BrowserWithTestWindowTest::SetUp();
+
+    arc_service_manager_->set_browser_context(profile());
+    display_service_ =
+        std::make_unique<NotificationDisplayServiceTester>(profile());
+    // Create the service (normally handled by ArcServiceLauncher).
+    ArcProvisionNotificationService::GetForBrowserContext(profile());
 
     const AccountId account_id(AccountId::FromUserEmailGaiaId(
         profile()->GetProfileUserName(), "1234567890"));
@@ -88,47 +65,28 @@ class ArcProvisionNotificationServiceTest : public testing::Test {
   }
 
   void TearDown() override {
-    EXPECT_CALL(*arc_provision_notification_service_delegate(),
-                RemoveManagedProvisionNotification())
-        .Times(AtLeast(1));
-    arc_provision_notification_service_.reset();
-    arc_provision_notification_service_delegate_ = nullptr;
+    // The session manager has to be shutdown before the profile is destroyed so
+    // it stops observing prefs, but can't be reset completely because some
+    // profile keyed services call into it.
+    arc_session_manager_->Shutdown();
+    display_service_.reset();
+    arc_service_manager_->set_browser_context(nullptr);
+    BrowserWithTestWindowTest::TearDown();
     arc_session_manager_.reset();
     arc_service_manager_.reset();
-    profile_.reset();
-    chromeos::DBusThreadManager::Shutdown();
   }
 
-  TestingProfile* profile() { return profile_.get(); }
-  ArcServiceManager* arc_service_manager() {
-    return arc_service_manager_.get();
-  }
-  ArcSessionManager* arc_session_manager() {
-    return arc_session_manager_.get();
-  }
-  MockArcProvisionNotificationServiceDelegate*
-  arc_provision_notification_service_delegate() {
-    return arc_provision_notification_service_delegate_;
-  }
-  ArcProvisionNotificationService* arc_provision_notification_service() {
-    return arc_provision_notification_service_.get();
-  }
   chromeos::FakeChromeUserManager* GetFakeUserManager() {
     return static_cast<chromeos::FakeChromeUserManager*>(
         user_manager::UserManager::Get());
   }
 
- private:
-  content::TestBrowserThreadBundle thread_bundle_;
-  user_manager::ScopedUserManager user_manager_enabler_;
-  base::ScopedTempDir temp_dir_;
-  std::unique_ptr<TestingProfile> profile_;
   std::unique_ptr<ArcServiceManager> arc_service_manager_;
   std::unique_ptr<ArcSessionManager> arc_session_manager_;
-  std::unique_ptr<ArcProvisionNotificationService>
-      arc_provision_notification_service_;
-  MockArcProvisionNotificationServiceDelegate*
-      arc_provision_notification_service_delegate_ = nullptr;
+  std::unique_ptr<NotificationDisplayServiceTester> display_service_;
+
+ private:
+  user_manager::ScopedUserManager user_manager_enabler_;
 
   DISALLOW_COPY_AND_ASSIGN(ArcProvisionNotificationServiceTest);
 };
@@ -147,38 +105,29 @@ TEST_F(ArcProvisionNotificationServiceTest,
   profile()->GetTestingPrefService()->SetManagedPref(
       prefs::kArcLocationServiceEnabled, std::make_unique<base::Value>(false));
 
-  arc_session_manager()->SetProfile(profile());
-  arc_session_manager()->Initialize();
+  arc_session_manager_->SetProfile(profile());
+  arc_session_manager_->Initialize();
 
   // Trigger opt-in flow. The notification gets shown.
-  EXPECT_CALL(*arc_provision_notification_service_delegate(),
-              ShowManagedProvisionNotification());
-  arc_session_manager()->RequestEnable();
-  Mock::VerifyAndClearExpectations(
-      arc_provision_notification_service_delegate());
+  arc_session_manager_->RequestEnable();
+  EXPECT_TRUE(
+      display_service_->GetNotification(kArcManagedProvisionNotificationId));
   EXPECT_EQ(ArcSessionManager::State::CHECKING_ANDROID_MANAGEMENT,
-            arc_session_manager()->state());
-  arc_session_manager()->StartArcForTesting();
-  EXPECT_EQ(ArcSessionManager::State::ACTIVE, arc_session_manager()->state());
+            arc_session_manager_->state());
+  arc_session_manager_->StartArcForTesting();
+  EXPECT_EQ(ArcSessionManager::State::ACTIVE, arc_session_manager_->state());
 
   // Emulate successful provisioning. The notification gets removed.
-  EXPECT_CALL(*arc_provision_notification_service_delegate(),
-              RemoveManagedProvisionNotification())
-      .Times(AtLeast(1));
-  arc_session_manager()->OnProvisioningFinished(ProvisioningResult::SUCCESS);
-  Mock::VerifyAndClearExpectations(
-      arc_provision_notification_service_delegate());
+  arc_session_manager_->OnProvisioningFinished(ProvisioningResult::SUCCESS);
+  EXPECT_FALSE(
+      display_service_->GetNotification(kArcManagedProvisionNotificationId));
 }
 
 // The managed provision notification is not displayed after the restart if the
 // provisioning was successful.
 TEST_F(ArcProvisionNotificationServiceTest,
        ManagedProvisionNotification_Restart) {
-  // No notifications are expected to be shown in this test, but the delegate
-  // method that removes the notification is allowed to be called.
-  EXPECT_CALL(*arc_provision_notification_service_delegate(),
-              RemoveManagedProvisionNotification())
-      .Times(AnyNumber());
+  // No notifications are expected to be shown in this test.
 
   // Set up managed ARC and assign managed values to all opt-in prefs.
   profile()->GetTestingPrefService()->SetManagedPref(
@@ -191,14 +140,18 @@ TEST_F(ArcProvisionNotificationServiceTest,
   // performed.
   profile()->GetPrefs()->SetBoolean(prefs::kArcSignedIn, true);
 
-  arc_session_manager()->SetProfile(profile());
-  arc_session_manager()->Initialize();
+  arc_session_manager_->SetProfile(profile());
+  arc_session_manager_->Initialize();
 
   // Enable ARC. The opt-in flow doesn't take place, and no notification is
   // shown.
-  arc_session_manager()->RequestEnable();
-  EXPECT_EQ(ArcSessionManager::State::ACTIVE, arc_session_manager()->state());
-  arc_session_manager()->OnProvisioningFinished(ProvisioningResult::SUCCESS);
+  arc_session_manager_->RequestEnable();
+  EXPECT_FALSE(
+      display_service_->GetNotification(kArcManagedProvisionNotificationId));
+  EXPECT_EQ(ArcSessionManager::State::ACTIVE, arc_session_manager_->state());
+  arc_session_manager_->OnProvisioningFinished(ProvisioningResult::SUCCESS);
+  EXPECT_FALSE(
+      display_service_->GetNotification(kArcManagedProvisionNotificationId));
 }
 
 // The managed provision notification is displayed from the beginning of the
@@ -213,29 +166,24 @@ TEST_F(ArcProvisionNotificationServiceTest,
   profile()->GetTestingPrefService()->SetManagedPref(
       prefs::kArcLocationServiceEnabled, std::make_unique<base::Value>(false));
 
-  arc_session_manager()->SetProfile(profile());
-  arc_session_manager()->Initialize();
+  arc_session_manager_->SetProfile(profile());
+  arc_session_manager_->Initialize();
 
   // Trigger opt-in flow. The notification gets shown.
-  EXPECT_CALL(*arc_provision_notification_service_delegate(),
-              ShowManagedProvisionNotification());
-  arc_session_manager()->RequestEnable();
-  Mock::VerifyAndClearExpectations(
-      arc_provision_notification_service_delegate());
+  arc_session_manager_->RequestEnable();
+  EXPECT_TRUE(
+      display_service_->GetNotification(kArcManagedProvisionNotificationId));
   EXPECT_EQ(ArcSessionManager::State::CHECKING_ANDROID_MANAGEMENT,
-            arc_session_manager()->state());
-  arc_session_manager()->StartArcForTesting();
-  EXPECT_EQ(ArcSessionManager::State::ACTIVE, arc_session_manager()->state());
+            arc_session_manager_->state());
+  arc_session_manager_->StartArcForTesting();
+  EXPECT_EQ(ArcSessionManager::State::ACTIVE, arc_session_manager_->state());
 
   // Emulate provisioning failure that leads to stopping ARC. The notification
   // gets removed.
-  EXPECT_CALL(*arc_provision_notification_service_delegate(),
-              RemoveManagedProvisionNotification())
-      .Times(AtLeast(1));
-  arc_session_manager()->OnProvisioningFinished(
+  arc_session_manager_->OnProvisioningFinished(
       ProvisioningResult::CHROME_SERVER_COMMUNICATION_ERROR);
-  Mock::VerifyAndClearExpectations(
-      arc_provision_notification_service_delegate());
+  EXPECT_FALSE(
+      display_service_->GetNotification(kArcManagedProvisionNotificationId));
 }
 
 // The managed provision notification is displayed from the beginning of the
@@ -250,60 +198,57 @@ TEST_F(ArcProvisionNotificationServiceTest,
   profile()->GetTestingPrefService()->SetManagedPref(
       prefs::kArcLocationServiceEnabled, std::make_unique<base::Value>(false));
 
-  arc_session_manager()->SetProfile(profile());
-  arc_session_manager()->Initialize();
+  arc_session_manager_->SetProfile(profile());
+  arc_session_manager_->Initialize();
 
   // Trigger opt-in flow. The notification gets shown.
-  EXPECT_CALL(*arc_provision_notification_service_delegate(),
-              ShowManagedProvisionNotification());
-  arc_session_manager()->RequestEnable();
-  Mock::VerifyAndClearExpectations(
-      arc_provision_notification_service_delegate());
+  arc_session_manager_->RequestEnable();
+  EXPECT_TRUE(
+      display_service_->GetNotification(kArcManagedProvisionNotificationId));
   EXPECT_EQ(ArcSessionManager::State::CHECKING_ANDROID_MANAGEMENT,
-            arc_session_manager()->state());
-  arc_session_manager()->StartArcForTesting();
-  EXPECT_EQ(ArcSessionManager::State::ACTIVE, arc_session_manager()->state());
+            arc_session_manager_->state());
+  arc_session_manager_->StartArcForTesting();
+  EXPECT_EQ(ArcSessionManager::State::ACTIVE, arc_session_manager_->state());
 
   // Emulate provisioning failure that leads to showing an error screen without
   // shutting ARC down. The notification gets removed.
-  EXPECT_CALL(*arc_provision_notification_service_delegate(),
-              RemoveManagedProvisionNotification())
-      .Times(AtLeast(1));
-  arc_session_manager()->OnProvisioningFinished(
+  arc_session_manager_->OnProvisioningFinished(
       ProvisioningResult::NO_NETWORK_CONNECTION);
-  Mock::VerifyAndClearExpectations(
-      arc_provision_notification_service_delegate());
+  EXPECT_FALSE(
+      display_service_->GetNotification(kArcManagedProvisionNotificationId));
 }
 
 // The managed provision notification is not displayed when opt-in prefs are not
 // managed.
 TEST_F(ArcProvisionNotificationServiceTest,
        ManagedProvisionNotification_NotSilent) {
-  // No notifications are expected to be shown in this test, but the delegate
-  // method that removes the notification is allowed to be called.
-  EXPECT_CALL(*arc_provision_notification_service_delegate(),
-              RemoveManagedProvisionNotification())
-      .Times(AnyNumber());
+  // No notifications are expected to be shown in this test.
 
   // Set ARC to be managed.
   profile()->GetTestingPrefService()->SetManagedPref(
       prefs::kArcEnabled, std::make_unique<base::Value>(true));
 
-  arc_session_manager()->SetProfile(profile());
-  arc_session_manager()->Initialize();
+  arc_session_manager_->SetProfile(profile());
+  arc_session_manager_->Initialize();
 
   // Trigger opt-in flow. The notification is not shown.
-  arc_session_manager()->RequestEnable();
+  arc_session_manager_->RequestEnable();
+  EXPECT_FALSE(
+      display_service_->GetNotification(kArcManagedProvisionNotificationId));
   EXPECT_EQ(ArcSessionManager::State::NEGOTIATING_TERMS_OF_SERVICE,
-            arc_session_manager()->state());
+            arc_session_manager_->state());
 
   // Emulate accepting the terms of service.
-  arc_session_manager()->OnTermsOfServiceNegotiatedForTesting(true);
-  arc_session_manager()->StartArcForTesting();
-  EXPECT_EQ(ArcSessionManager::State::ACTIVE, arc_session_manager()->state());
+  arc_session_manager_->OnTermsOfServiceNegotiatedForTesting(true);
+  arc_session_manager_->StartArcForTesting();
+  EXPECT_EQ(ArcSessionManager::State::ACTIVE, arc_session_manager_->state());
 
   // Emulate successful provisioning.
-  arc_session_manager()->OnProvisioningFinished(ProvisioningResult::SUCCESS);
+  EXPECT_FALSE(
+      display_service_->GetNotification(kArcManagedProvisionNotificationId));
+  arc_session_manager_->OnProvisioningFinished(ProvisioningResult::SUCCESS);
+  EXPECT_FALSE(
+      display_service_->GetNotification(kArcManagedProvisionNotificationId));
 }
 
 }  // namespace arc
