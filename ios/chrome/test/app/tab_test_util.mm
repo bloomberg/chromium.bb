43cae49d3fe571bf0cb81d6ff4b40a8afac196b9
Sylvain Defresne <sdefresne@chromium.org>
2017-12-19 18:12:11 +0000

    Remove obsolete #include.
    Bug: none
    Change-Id: Ic21288bc13f72cf0b99658df7195d2a839bbf4b4
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Reviewed-on: https://chromium-review.googlesource.com/834209
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Louis Romero <lpromero@chromium.org>
    Reviewed-by: Louis Romero <lpromero@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525080}
    Former-commit-id: 64f90e0d2e6799112d90e03eeea68780bc6bfa20

--- ios/chrome/test/app/tab_test_util.mm
+++ ios/chrome/test/app/tab_test_util.mm
@@ -13,7 +13,6 @@
 #import "ios/chrome/browser/metrics/tab_usage_recorder.h"
 #import "ios/chrome/browser/tabs/tab.h"
 #import "ios/chrome/browser/tabs/tab_model.h"
-#import "ios/chrome/browser/tabs/tab_private.h"
 #import "ios/chrome/browser/ui/browser_view_controller.h"
 #import "ios/chrome/browser/ui/commands/browser_commands.h"
 #import "ios/chrome/browser/ui/commands/open_new_tab_command.h"
