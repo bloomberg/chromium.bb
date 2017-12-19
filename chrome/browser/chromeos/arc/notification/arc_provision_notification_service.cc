05a03fabc3b3c50861434bab310915079b463291
Christian Dullweber <dullweber@chromium.org>
2017-12-19 15:53:42 +0000

    Revert "Update ArcProvisionNotification for NotificationDisplayService."
    This reverts commit 789fa0ecf9f3040fd1c323bc3f95aab4d3d2ace6 [formerly 627f0f0e3ca12a1e37e8724fc3ca3df33443267c].
    
    Reason for revert: The test is failing on linux-chromeos-rel https://crbug.com/796198
    
    Original change's description:
    > Update ArcProvisionNotification for NotificationDisplayService.
    > 
    > also,
    > - simplify production code by removing unit test delegate
    > - streamline unit test with use of BrowserWithTestWindowTest
    > 
    > Bug: 783018
    > Change-Id: I997312523f48bdd7a0cdeb20c563412afac526d6
    > Reviewed-on: https://chromium-review.googlesource.com/823134
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Commit-Queue: Evan Stade <estade@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#525016}
    
    TBR=yoshiki@chromium.org,estade@chromium.org,hidehiko@chromium.org
    
    Change-Id: Ie3964e9c4c810c24e13751ec5733f27d2e5bd211
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 783018
    Reviewed-on: https://chromium-review.googlesource.com/832689
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525042}
    Former-commit-id: dd75c2543912123c4353e0d125351797d7d53c53

--- chrome/browser/chromeos/arc/notification/arc_provision_notification_service.cc
+++ chrome/browser/chromeos/arc/notification/arc_provision_notification_service.cc
@@ -9,18 +9,18 @@
 #include "base/memory/singleton.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/chromeos/arc/arc_util.h"
-#include "chrome/browser/notifications/notification_display_service.h"
-#include "chrome/browser/notifications/notification_display_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/ash/multi_user/multi_user_util.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/theme_resources.h"
 #include "components/arc/arc_browser_context_keyed_service_factory_base.h"
 #include "components/signin/core/account_id/account_id.h"
+#include "components/user_manager/user.h"
+#include "components/user_manager/user_manager.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/chromeos/devicetype_utils.h"
 #include "ui/gfx/image/image.h"
+#include "ui/message_center/message_center.h"
 #include "ui/message_center/notification.h"
 #include "ui/message_center/notification_types.h"
 #include "ui/message_center/notifier_id.h"
@@ -32,6 +32,42 @@ namespace {
 
 constexpr char kManagedProvisionNotificationId[] = "arc_managed_provision";
 constexpr char kManagedProvisionNotifierId[] = "arc_managed_provision";
+
+class DelegateImpl : public ArcProvisionNotificationService::Delegate {
+ public:
+  void ShowManagedProvisionNotification() override;
+  void RemoveManagedProvisionNotification() override;
+};
+
+void DelegateImpl::ShowManagedProvisionNotification() {
+  message_center::NotifierId notifier_id(
+      message_center::NotifierId::SYSTEM_COMPONENT,
+      kManagedProvisionNotifierId);
+  const AccountId& account_id =
+      user_manager::UserManager::Get()->GetPrimaryUser()->GetAccountId();
+  notifier_id.profile_id = account_id.GetUserEmail();
+  message_center::RichNotificationData optional_fields;
+  optional_fields.never_timeout = true;
+
+  message_center::MessageCenter::Get()->AddNotification(
+      std::make_unique<message_center::Notification>(
+          message_center::NOTIFICATION_TYPE_SIMPLE,
+          kManagedProvisionNotificationId,
+          l10n_util::GetStringUTF16(
+              IDS_ARC_MANAGED_PROVISION_NOTIFICATION_TITLE),
+          l10n_util::GetStringFUTF16(
+              IDS_ARC_MANAGED_PROVISION_NOTIFICATION_MESSAGE,
+              ui::GetChromeOSDeviceName()),
+          gfx::Image(ui::ResourceBundle::GetSharedInstance().GetImageNamed(
+              IDR_ARC_PLAY_STORE_OPTIN_IN_PROGRESS_NOTIFICATION)),
+          l10n_util::GetStringUTF16(IDS_ARC_NOTIFICATION_DISPLAY_SOURCE),
+          GURL(), notifier_id, optional_fields, nullptr));
+}
+
+void DelegateImpl::RemoveManagedProvisionNotification() {
+  message_center::MessageCenter::Get()->RemoveNotification(
+      kManagedProvisionNotificationId, false);
+}
 
 // Singleton factory for ArcProvisionNotificationService.
 class ArcProvisionNotificationServiceFactory
@@ -48,13 +84,15 @@ class ArcProvisionNotificationServiceFactory
 
  private:
   friend base::DefaultSingletonTraits<ArcProvisionNotificationServiceFactory>;
-  ArcProvisionNotificationServiceFactory() {
-    DependsOn(NotificationDisplayServiceFactory::GetInstance());
-  }
+  ArcProvisionNotificationServiceFactory() = default;
   ~ArcProvisionNotificationServiceFactory() override = default;
 };
 
 }  // namespace
+
+ArcProvisionNotificationService::Delegate::Delegate() = default;
+
+ArcProvisionNotificationService::Delegate::~Delegate() = default;
 
 // static
 ArcProvisionNotificationService*
@@ -66,75 +104,63 @@ ArcProvisionNotificationService::GetForBrowserContext(
 ArcProvisionNotificationService::ArcProvisionNotificationService(
     content::BrowserContext* context,
     ArcBridgeService* bridge_service)
-    : context_(context) {
-  ArcSessionManager::Get()->AddObserver(this);
-}
+    : ArcProvisionNotificationService(context,
+                                      std::make_unique<DelegateImpl>()) {}
 
 ArcProvisionNotificationService::~ArcProvisionNotificationService() {
+  // Make sure no notification is left being shown.
+  delegate_->RemoveManagedProvisionNotification();
+
   ArcSessionManager::Get()->RemoveObserver(this);
 }
 
-void ArcProvisionNotificationService::ShowNotification() {
-  Profile* profile = Profile::FromBrowserContext(context_);
-  message_center::NotifierId notifier_id(
-      message_center::NotifierId::SYSTEM_COMPONENT,
-      kManagedProvisionNotifierId);
-  notifier_id.profile_id =
-      multi_user_util::GetAccountIdFromProfile(profile).GetUserEmail();
-  message_center::RichNotificationData optional_fields;
-  optional_fields.never_timeout = true;
-
-  message_center::Notification notification(
-      message_center::NOTIFICATION_TYPE_SIMPLE, kManagedProvisionNotificationId,
-      l10n_util::GetStringUTF16(IDS_ARC_MANAGED_PROVISION_NOTIFICATION_TITLE),
-      l10n_util::GetStringFUTF16(IDS_ARC_MANAGED_PROVISION_NOTIFICATION_MESSAGE,
-                                 ui::GetChromeOSDeviceName()),
-      gfx::Image(ui::ResourceBundle::GetSharedInstance().GetImageNamed(
-          IDR_ARC_PLAY_STORE_OPTIN_IN_PROGRESS_NOTIFICATION)),
-      l10n_util::GetStringUTF16(IDS_ARC_NOTIFICATION_DISPLAY_SOURCE), GURL(),
-      notifier_id, optional_fields, nullptr);
-
-  NotificationDisplayService::GetForProfile(profile)->Display(
-      NotificationHandler::Type::TRANSIENT, notification);
+// static
+std::unique_ptr<ArcProvisionNotificationService>
+ArcProvisionNotificationService::CreateForTesting(
+    content::BrowserContext* context,
+    std::unique_ptr<Delegate> delegate) {
+  return base::WrapUnique<ArcProvisionNotificationService>(
+      new ArcProvisionNotificationService(context, std::move(delegate)));
 }
 
-void ArcProvisionNotificationService::HideNotification() {
-  NotificationDisplayService::GetForProfile(
-      Profile::FromBrowserContext(context_))
-      ->Close(NotificationHandler::Type::TRANSIENT,
-              kManagedProvisionNotificationId);
+ArcProvisionNotificationService::ArcProvisionNotificationService(
+    content::BrowserContext* context,
+    std::unique_ptr<Delegate> delegate)
+    : context_(context), delegate_(std::move(delegate)) {
+  ArcSessionManager::Get()->AddObserver(this);
 }
 
 void ArcProvisionNotificationService::OnArcPlayStoreEnabledChanged(
     bool enabled) {
-  // Make sure no notification is shown after ARC gets disabled.
-  if (!enabled)
-    HideNotification();
+  if (!enabled) {
+    // Make sure no notification is shown after ARC gets disabled.
+    delegate_->RemoveManagedProvisionNotification();
+  }
 }
 
 void ArcProvisionNotificationService::OnArcOptInManagementCheckStarted() {
   // This observer is notified at an early phase of the opt-in flow, so start
   // showing the notification if the opt-in flow happens silently (due to the
   // managed prefs), or ensure that no notification is shown otherwise.
-  Profile* profile = Profile::FromBrowserContext(context_);
+  const Profile* const profile = Profile::FromBrowserContext(context_);
   if (IsArcPlayStoreEnabledPreferenceManagedForProfile(profile) &&
       AreArcAllOptInPreferencesIgnorableForProfile(profile)) {
-    ShowNotification();
+    delegate_->ShowManagedProvisionNotification();
   } else {
-    HideNotification();
+    delegate_->RemoveManagedProvisionNotification();
   }
 }
 
 void ArcProvisionNotificationService::OnArcInitialStart() {
   // The opt-in flow finished successfully, so remove the notification.
-  HideNotification();
+  delegate_->RemoveManagedProvisionNotification();
 }
 
 void ArcProvisionNotificationService::OnArcSessionStopped(
     ArcStopReason stop_reason) {
   // One of the reasons of ARC being stopped is a failure of the opt-in flow.
   // Therefore remove the notification if it is shown.
-  HideNotification();
+  delegate_->RemoveManagedProvisionNotification();
 }
 
 void ArcProvisionNotificationService::OnArcErrorShowRequested(
@@ -143,7 +169,7 @@ void ArcProvisionNotificationService::OnArcErrorShowRequested(
   // the notification should be therefore removed if it is shown. Do this here
   // unconditionally as there should be no notification displayed otherwise
   // anyway.
-  HideNotification();
+  delegate_->RemoveManagedProvisionNotification();
 }
 
 }  // namespace arc
--- chrome/browser/chromeos/arc/notification/arc_provision_notification_service.h
+++ chrome/browser/chromeos/arc/notification/arc_provision_notification_service.h
@@ -22,19 +22,40 @@ namespace arc {
 class ArcProvisionNotificationService : public KeyedService,
                                         public ArcSessionManager::Observer {
  public:
+  // The delegate whose methods are used by the service for showing/hiding the
+  // notifications. The delegate exists for unit testing purposes.
+  class Delegate {
+   public:
+    Delegate();
+    virtual ~Delegate();
+    virtual void ShowManagedProvisionNotification() = 0;
+    virtual void RemoveManagedProvisionNotification() = 0;
+
+   private:
+    DISALLOW_COPY_AND_ASSIGN(Delegate);
+  };
+
   // Returns singleton instance for the given BrowserContext,
   // or nullptr if the browser |context| is not allowed to use ARC.
   static ArcProvisionNotificationService* GetForBrowserContext(
       content::BrowserContext* context);
 
+  // Constructs with the default delegate implementation that uses message
+  // center for showing the notifications.
   ArcProvisionNotificationService(content::BrowserContext* context,
                                   ArcBridgeService* bridge_service);
+
   ~ArcProvisionNotificationService() override;
 
+  // Constructs an instance with the supplied delegate.
+  static std::unique_ptr<ArcProvisionNotificationService> CreateForTesting(
+      content::BrowserContext* context,
+      std::unique_ptr<Delegate> delegate);
+
  private:
-  // Shows/hides the notification.
-  void ShowNotification();
-  void HideNotification();
+  // Constructs with the supplied delegate.
+  ArcProvisionNotificationService(content::BrowserContext* context,
+                                  std::unique_ptr<Delegate> delegate);
 
   // ArcSessionManager::Observer:
   void OnArcPlayStoreEnabledChanged(bool enabled) override;
@@ -44,6 +65,7 @@ class ArcProvisionNotificationService : public KeyedService,
   void OnArcErrorShowRequested(ArcSupportHost::Error error) override;
 
   content::BrowserContext* const context_;
+  std::unique_ptr<Delegate> delegate_;
 
   DISALLOW_COPY_AND_ASSIGN(ArcProvisionNotificationService);
 };
