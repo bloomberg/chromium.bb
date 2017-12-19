8265ae653e19b023795d9d894ded05a375e00948
Scott Graham <scottmg@chromium.org>
2017-12-19 07:47:06 +0000

    fuchsia: Move runner shutdown to after flush delay
    The delay before VM termination was accidentally moved in
    https://chromium-review.googlesource.com/c/chromium/src/+/734128 and so
    on fast shutdown the results .json file might not be flushed to the disk
    image. This could result in a truncated output.json (see linked bug)
    causing bot failures.
    
    Also, unify delay to longer of current values as they're arbitrary
    anyway.
    
    TBR=wez@chromium.org
    
    Bug: 796026
    Change-Id: Ie8986ec928e37d251571590c9f05b58109df0f1f
    Reviewed-on: https://chromium-review.googlesource.com/833757
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524966}
    Former-commit-id: c552dad1797351aa6b54998e2a8fca21d0c07758

--- build/fuchsia/runner_common.py
+++ build/fuchsia/runner_common.py
@@ -330,20 +330,19 @@ def WriteAutorun(bin_name, child_args, summary_output, shutdown_machine,
   for arg in child_args:
     autorun_file.write(' "%s"' % arg);
   autorun_file.write('\n')
-  autorun_file.write('echo \"%s\"\n' % ALL_DONE_MESSAGE)
 
   if shutdown_machine:
     autorun_file.write('echo Sleeping and shutting down...\n')
 
     # A delay is required to give the guest OS or remote device a chance to
     # flush its output before it terminates.
+    autorun_file.write('msleep 8000\n')
     if use_device:
-      autorun_file.write('msleep 8000\n')
       autorun_file.write('dm reboot\n')
     else:
-      autorun_file.write('msleep 3000\n')
       autorun_file.write('dm poweroff\n')
 
+  autorun_file.write('echo \"%s\"\n' % ALL_DONE_MESSAGE)
 
   autorun_file.flush()
   os.chmod(autorun_file.name, 0750)
