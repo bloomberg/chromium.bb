d03ad0aa67d7e516a56068e699550e8191530c79
Takashi Toyoshima <toyoshim@chromium.org>
2017-12-19 09:08:34 +0000

    MidiManager: remove dynamic instantiation study
    Now that the mode is enabled on all platforms, let's remove
    the study flag, and enable it always on production code.
    
    Unit tests still use the legacy mode in some cases, but it
    should be modified in follow-up changes soon.
    
    Bug: 672793
    Change-Id: I99cf9d15e9cf8c3305d5bf73f2e7350159f45e56
    Tbr: mpearson@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/821803
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#524976}
    Former-commit-id: 3cc949e9877ba93f05feaaf0bc5f75cb90365b68

--- media/midi/midi_switches.cc
+++ media/midi/midi_switches.cc
@@ -8,11 +8,6 @@
 namespace midi {
 namespace features {
 
-#if defined(OS_ANDROID)
-const base::Feature kMidiManagerDynamicInstantiation{
-    "MidiManagerDynamicInstantiation", base::FEATURE_ENABLED_BY_DEFAULT};
-#endif
-
 #if defined(OS_WIN)
 const base::Feature kMidiManagerWinrt{"MidiManagerWinrt",
                                       base::FEATURE_DISABLED_BY_DEFAULT};
