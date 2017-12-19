ae5d3311c814ddc0d547e6e7dca5dabe938aabb5
Colin Blundell <blundell@chromium.org>
2017-12-19 23:01:40 +0000

    Identity Service: Add //chrome KeyedService that wraps IdentityManager
    This CL makes IdentityManager available in //chrome via a
    KeyedServiceFactory. KeyedService is conceptually the right match for
    IdentityManager: There should be one IdentityManager instance per
    Identity Service instance, and as the Identity Service is a per-user
    service, there will be one Identity Service instance per Profile.
    
    Note that IdentityManager itself cannot be a KeyedService directly as it
    is intended for usage from generic client processes (e.g., the renderer);
    to introduce KeyedService in this generic context would be a layering
    violation. Hence, IdentityManagerFactory maintains a small wrapper class
    internally that holds IdentityManager instances.
    
    A followup change will introduce the same access for //ios/chrome.
    
    Bug: 654990
    Change-Id: I5bf924083e4764fb3bc9e9d0d12b16b18937d8d1
    Reviewed-on: https://chromium-review.googlesource.com/831516
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525171}
    Former-commit-id: 28bba9f661b4680c182972388737b27a2edca286

---
+++ chrome/browser/signin/identity_manager_factory.cc
@@ -0,0 +1,56 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/signin/identity_manager_factory.h"
+
+#include "build/build_config.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/signin/signin_manager_factory.h"
+#include "components/keyed_service/content/browser_context_dependency_manager.h"
+#include "components/keyed_service/core/keyed_service.h"
+#include "components/signin/core/browser/signin_manager.h"
+#include "services/identity/public/cpp/identity_manager.h"
+
+// Class that wraps IdentityManager in a KeyedService (as IdentityManager is a
+// client-side library intended for use by any process, it would be a layering
+// violation for IdentityManager itself to have direct knowledge of
+// KeyedService).
+class IdentityManagerHolder : public KeyedService {
+ public:
+  explicit IdentityManagerHolder(Profile* profile)
+      : identity_manager_(SigninManagerFactory::GetForProfile(profile)) {}
+
+  identity::IdentityManager* identity_manager() { return &identity_manager_; }
+
+ private:
+  identity::IdentityManager identity_manager_;
+};
+
+IdentityManagerFactory::IdentityManagerFactory()
+    : BrowserContextKeyedServiceFactory(
+          "IdentityManager",
+          BrowserContextDependencyManager::GetInstance()) {
+  DependsOn(SigninManagerFactory::GetInstance());
+}
+
+IdentityManagerFactory::~IdentityManagerFactory() {}
+
+// static
+identity::IdentityManager* IdentityManagerFactory::GetForProfile(
+    Profile* profile) {
+  IdentityManagerHolder* holder = static_cast<IdentityManagerHolder*>(
+      GetInstance()->GetServiceForBrowserContext(profile, true));
+
+  return holder->identity_manager();
+}
+
+// static
+IdentityManagerFactory* IdentityManagerFactory::GetInstance() {
+  return base::Singleton<IdentityManagerFactory>::get();
+}
+
+KeyedService* IdentityManagerFactory::BuildServiceInstanceFor(
+    content::BrowserContext* context) const {
+  return new IdentityManagerHolder(Profile::FromBrowserContext(context));
+}
