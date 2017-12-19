1173fd1f4ffea5cd4896557997c6be3dcc9af35e
Will Chen <chenwilliam@chromium.org>
2017-12-19 18:15:03 +0000

    DevTools: clean up inspector-related webkitpy code
    * removes run-inspector-tests script which was part of an (abandoned)
    experiment to move devtools tests next to its non-test code.
    * removes /inspector/ specific code which is no longer applicable
    since all the devtools integration tests are in /http/tests/devtools/
    
    Bug: 667560
    Change-Id: I66e5ef91cb8be81dc2501a69595687372fe22b54
    Reviewed-on: https://chromium-review.googlesource.com/833345
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525083}
    Former-commit-id: 384520286f53e300069fa9ab9fcc572595fcde87

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/controllers/manager.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/controllers/manager.py
@@ -66,7 +66,6 @@ class Manager(object):
     """A class for managing running a series of layout tests."""
 
     HTTP_SUBDIR = 'http'
-    INSPECTOR_SUBDIR = 'inspector'
     PERF_SUBDIR = 'perf'
     WEBSOCKET_SUBDIR = 'websocket'
     ARCHIVED_RESULTS_LIMIT = 25
@@ -277,9 +276,6 @@ class Manager(object):
             self._port.TEST_PATH_SEPARATOR + self.HTTP_SUBDIR + self._port.TEST_PATH_SEPARATOR in test
         )
 
-    def _is_inspector_test(self, test):
-        return self.INSPECTOR_SUBDIR + self._port.TEST_PATH_SEPARATOR in test
-
     def _is_websocket_test(self, test):
         if self._port.should_use_wptserve(test):
             return False
@@ -399,8 +395,7 @@ class Manager(object):
             self._port.start_wptserve()
             self._wptserve_started = True
 
-        if self._port.requires_http_server() or any((self._is_http_test(test) or self._is_inspector_test(test))
-                                                    for test in tests_to_run):
+        if self._port.requires_http_server() or any(self._is_http_test(test) for test in tests_to_run):
             self._printer.write_update('Starting HTTP server ...')
             self._port.start_http_server(additional_dirs={}, number_of_drivers=self._options.max_locked_shards)
             self._http_server_started = True
