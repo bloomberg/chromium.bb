2fb7ee3a8e987e0ed6efd3a3d035c1a17f3a4a64
Mike Klein <mtklein@chromium.org>
2017-12-19 20:04:37 +0000

    Guard removing the [0,a] clamp from bicubic image sampling.
    While we're here, clean up SK_COLOR_SPACE_XFORM_LEGACY_PIPELINE,
    which doesn't do anything.
    
    Bug: skia:5733
    Change-Id: Ibe9f5d558cacb9359a99e7a1ecb42e6b23f06c48
    Reviewed-on: https://chromium-review.googlesource.com/834329
    Commit-Queue: Mike Klein <mtklein@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525123}
    Former-commit-id: 46c8bf4a955116f20f839f88720b28de9571a7ca

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -228,8 +228,12 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_TILED_BITMAPS
 #endif
 
-#ifndef SK_COLOR_SPACE_XFORM_LEGACY_PIPELINE
-#define SK_COLOR_SPACE_XFORM_LEGACY_PIPELINE
+#ifndef SK_SUPPORT_LEGACY_ADOBE_XYZ
+#define SK_SUPPORT_LEGACY_ADOBE_XYZ
+#endif
+
+#ifndef SK_LEGACY_HIGH_QUALITY_SCALING_CLAMP
+#define SK_LEGACY_HIGH_QUALITY_SCALING_CLAMP
 #endif
 
 // remove after rebaselining svg layout tests
