c9e513b101bbe05e994d0420271b881a99653d81
Tarun Bansal <tbansal@chromium.org>
2017-12-19 19:59:18 +0000

    Remove code for recording correlation metric in Network Quality Estimator
    The correlation metric histogram is not in use anymore, and can be
    removed.
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Bug: 795010
    Change-Id: I512bd88152ae4d22806c6b5d12ce910440787ebe
    TBR: holte@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/822182
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525120}
    Former-commit-id: 2d3d329540541ffc28945b3ef438c2cc0d41aa0f

--- net/nqe/network_quality_estimator_params.h
+++ net/nqe/network_quality_estimator_params.h
@@ -88,12 +88,6 @@ class NET_EXPORT NetworkQualityEstimatorParams {
     return weight_multiplier_per_signal_strength_level_;
   }
 
-  // Returns the fraction of URL requests that should record the correlation
-  // UMA.
-  double correlation_uma_logging_probability() const {
-    return correlation_uma_logging_probability_;
-  }
-
   // Returns an unset value if the effective connection type has not been forced
   // via the |params| provided to this class. Otherwise, returns a value set to
   // the effective connection type that has been forced. Forced ECT can be
@@ -269,7 +263,6 @@ class NET_EXPORT NetworkQualityEstimatorParams {
   const double throughput_hanging_requests_cwnd_size_multiplier_;
   const double weight_multiplier_per_second_;
   const double weight_multiplier_per_signal_strength_level_;
-  const double correlation_uma_logging_probability_;
   base::Optional<EffectiveConnectionType> forced_effective_connection_type_;
   const bool forced_effective_connection_type_on_cellular_only_;
   bool persistent_cache_reading_enabled_;
