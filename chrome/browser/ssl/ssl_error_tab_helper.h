0a12684d6b65f73b3f2ba30cbc0a3258c168b240
Carlos IL <carlosil@chromium.org>
2017-12-20 00:18:11 +0000

    Added mojo interface for interstitial commands.
    Added mojo interface to handle interstitial commands and implemented it
    in SSLErrorTabHelper, which passes the command to SSLBlockingPage.
    
    Bug: 779782
    Change-Id: I261dddd835a9e5360c214acd6cedd6d67d2d3710
    Reviewed-on: https://chromium-review.googlesource.com/833455
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525192}
    Former-commit-id: 2136d4f77a0e768a011f69ebf3a9e2d38d98ce45

--- chrome/browser/ssl/ssl_error_tab_helper.h
+++ chrome/browser/ssl/ssl_error_tab_helper.h
@@ -7,6 +7,9 @@
 
 #include <map>
 
+#include "components/security_interstitials/core/common/interfaces/interstitial_commands.mojom.h"
+#include "components/security_interstitials/core/controller_client.h"
+#include "content/public/browser/web_contents_binding_set.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 
@@ -22,7 +25,8 @@ class SecurityInterstitialPage;
 // Long-lived helper associated with a WebContents, for owning blocking pages.
 class SSLErrorTabHelper
     : public content::WebContentsObserver,
-      public content::WebContentsUserData<SSLErrorTabHelper> {
+      public content::WebContentsUserData<SSLErrorTabHelper>,
+      public security_interstitials::mojom::InterstitialCommands {
  public:
   ~SSLErrorTabHelper() override;
 
@@ -52,6 +56,23 @@ class SSLErrorTabHelper
       std::unique_ptr<security_interstitials::SecurityInterstitialPage>
           blocking_page);
 
+  void HandleCommand(security_interstitials::SecurityInterstitialCommand cmd);
+
+  // security_interstitials::mojom::InterstitialCommands::
+  void DontProceed() override;
+  void Proceed() override;
+  void ShowMoreSection() override;
+  void OpenHelpCenter() override;
+  void OpenDiagnostic() override;
+  void Reload() override;
+  void OpenDateSettings() override;
+  void OpenLogin() override;
+  void DoReport() override;
+  void DontReport() override;
+  void OpenReportingPrivacy() override;
+  void OpenWhitepaper() override;
+  void ReportPhishingError() override;
+
   // Keeps track of blocking pages for navigations that have encountered
   // certificate errors in this WebContents. When a navigation commits, the
   // corresponding blocking page is moved out and stored in
@@ -64,6 +85,10 @@ class SSLErrorTabHelper
   // blocking page) or reset on every committed navigation.
   std::unique_ptr<security_interstitials::SecurityInterstitialPage>
       blocking_page_for_currently_committed_navigation_;
+
+  content::WebContentsFrameBindingSet<
+      security_interstitials::mojom::InterstitialCommands>
+      binding_;
 
   DISALLOW_COPY_AND_ASSIGN(SSLErrorTabHelper);
 };
