78416207866dac431d1ae911396f37e77c61e2e2
rbpotter <rbpotter@chromium.org>
2017-12-20 01:02:33 +0000

    Printing: Fix problems with system dialog and hidden preview
    Bug: 796057
    Change-Id: Ifda86af778af0fabf1153189a193c92b36727a1c
    Reviewed-on: https://chromium-review.googlesource.com/834690
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525207}
    Former-commit-id: 3276e3b9c843443403653c84cc02231424eccd6b

--- chrome/browser/printing/print_view_manager_base.cc
+++ chrome/browser/printing/print_view_manager_base.cc
@@ -196,6 +196,20 @@ void PrintViewManagerBase::OnPrintSettingsDone(
     scoped_refptr<printing::PrinterQuery> printer_query) {
   queue_->QueuePrinterQuery(printer_query.get());
 
+  // Check if the job was cancelled. This should only happen on Windows when
+  // the system dialog is cancelled.
+  if (printer_query &&
+      printer_query->last_status() == PrintingContext::CANCEL) {
+#if defined(OS_WIN)
+    content::BrowserThread::PostTask(
+        content::BrowserThread::UI, FROM_HERE,
+        base::BindOnce(&PrintViewManagerBase::SystemDialogCancelled,
+                       base::Unretained(this)));
+#endif
+    std::move(callback).Run(base::Value());
+    return;
+  }
+
   // Post task so that the query has time to reset the callback before calling
   // OnDidGetPrintedPagesCount.
   content::BrowserThread::PostTask(
@@ -380,6 +394,7 @@ void PrintViewManagerBase::RenderFrameDeleted(
 void PrintViewManagerBase::SystemDialogCancelled() {
   // System dialog was cancelled. Clean up the print job and notify the
   // BackgroundPrintingManager.
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   ReleasePrinterQuery();
   TerminatePrintJob(true);
   content::NotificationService::current()->Notify(
