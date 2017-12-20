355dbc30923c4f0cdbbfdac7bffcac5686593bc7
Ross McIlroy <rmcilroy@chromium.org>
2017-12-20 12:59:16 +0000

    [Gin] Add feature to do extra masking in V8.
    Change-Id: Iba70082c0c8cb01c8bcb8c5c6e7f0fa237184f30
    Reviewed-on: https://chromium-review.googlesource.com/834088
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Ross McIlroy <rmcilroy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525316}
    Former-commit-id: e265c9cdeeca2e7bcaf94e20f5e32d12d248a606

---
+++ gin/gin_features.cc
@@ -0,0 +1,13 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "gin/gin_features.h"
+
+namespace features {
+
+// Enables extra masking and integrity checking in V8.
+const base::Feature kV8ExtraMasking{"V8ExtraMasking",
+                                    base::FEATURE_DISABLED_BY_DEFAULT};
+
+}  // namespace features
