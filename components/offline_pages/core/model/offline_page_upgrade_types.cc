2bb52e4dc26f2b6e50abf1fb53a6cf4c50dcceb5
Filip Gorski <fgorski@chromium.org>
2017-12-20 23:54:56 +0000

    [Offline pages] StartOfflinePageUpgradeTask implementation
    Patch 1/3 for upgrading offline pages. Provides the following:
    * Capability to verify that item can be upgraded
    * Reducing the number of remaining attempts in case above is true
    * Delivering file path and digest as a result of starting,
      data which is later consumed by the upgrade process.
    
    Bug: 758690
    Change-Id: I878d5b8586d3cfb52184f2c540fbb9ddc45082bf
    Reviewed-on: https://chromium-review.googlesource.com/837154
    Commit-Queue: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Yafei Duan <romax@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525519}
    Former-commit-id: b22c1b9bc3f71df92b2eca9e4543c6be4b304442

---
+++ components/offline_pages/core/model/offline_page_upgrade_types.cc
@@ -0,0 +1,22 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/offline_pages/core/model/offline_page_upgrade_types.h"
+
+namespace offline_pages {
+
+StartUpgradeResult::StartUpgradeResult()
+    : status(StartUpgradeStatus::DB_ERROR) {}
+
+StartUpgradeResult::StartUpgradeResult(StartUpgradeStatus status)
+    : status(status) {}
+
+StartUpgradeResult::StartUpgradeResult(StartUpgradeStatus status,
+                                       std::string digest,
+                                       base::FilePath file_path)
+    : status(status),
+      digest(std::move(digest)),
+      file_path(std::move(file_path)) {}
+
+}  // namespace offline_pages
---
+++ components/offline_pages/core/model/offline_page_upgrade_types.h
@@ -0,0 +1,46 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_OFFLINE_PAGES_CORE_MODEL_OFFLINE_PAGE_UPRGRADE_TYPES_H_
+#define COMPONENTS_OFFLINE_PAGES_CORE_MODEL_OFFLINE_PAGE_UPRGRADE_TYPES_H_
+
+#include <string>
+
+#include "base/callback_forward.h"
+#include "base/files/file_path.h"
+
+namespace offline_pages {
+
+// Enumeration of possible results for starting the upgrade process.
+enum class StartUpgradeStatus {
+  SUCCESS,
+  DB_ERROR,
+  ITEM_MISSING,
+  FILE_MISSING,
+  NOT_ENOUGH_STORAGE,
+};
+
+// Result of starting the upgrade.
+struct StartUpgradeResult {
+  StartUpgradeResult();
+  explicit StartUpgradeResult(StartUpgradeStatus status);
+  StartUpgradeResult(StartUpgradeStatus status,
+                     std::string digest,
+                     base::FilePath file_path);
+
+  // Support for move semantics.
+  StartUpgradeResult(StartUpgradeResult&& other) = default;
+  StartUpgradeResult& operator=(StartUpgradeResult&& other) = default;
+
+  StartUpgradeStatus status;
+  std::string digest;
+  base::FilePath file_path;
+};
+
+// Callback delivering results of starting the upgrade.
+typedef base::OnceCallback<void(StartUpgradeResult)> StartUpgradeCallback;
+
+}  // namespace offline_pages
+
+#endif  // COMPONENTS_OFFLINE_PAGES_CORE_MODEL_OFFLINE_PAGE_UPRGRADE_TYPES_H_
