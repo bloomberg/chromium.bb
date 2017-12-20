1b0b6ea88e0daa229380d1847910ee0e520d29f3
Rohit Rao <rohitrao@chromium.org>
2017-12-20 17:25:43 +0000

    [ios] Flip the tab switcher presentation flag on by default.
    Bug: 787821
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I3187e7175426f8453469283b58414618b3d4d3fb
    Reviewed-on: https://chromium-review.googlesource.com/758117
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525357}
    Former-commit-id: 4b02eda5f48d92a890bb0716c1bbf275dbf19298

--- ios/chrome/browser/ui/main/main_feature_flags.cc
+++ ios/chrome/browser/ui/main/main_feature_flags.cc
@@ -5,7 +5,7 @@
 #include "ios/chrome/browser/ui/main/main_feature_flags.h"
 
 const base::Feature kTabSwitcherPresentsBVC{"TabSwitcherPresentsBVC",
-                                            base::FEATURE_DISABLED_BY_DEFAULT};
+                                            base::FEATURE_ENABLED_BY_DEFAULT};
 
 bool TabSwitcherPresentsBVCEnabled() {
   return base::FeatureList::IsEnabled(kTabSwitcherPresentsBVC);
