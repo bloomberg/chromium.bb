dc280555e5ebe25412469a6d091bbececb4c1cc2
Darren Shen <shend@chromium.org>
2017-12-20 22:58:54 +0000

    [css-typed-om] Update tests for CSSTransformValue.
    Update tests to reflect new spec. We have to update behaviour
    to throw error when there's no components.
    
    Spec:
    https://github.com/w3c/css-houdini-drafts/issues/534
    
    Bug: 545318
    Change-Id: Ifba7a1e6142bf19ff304d09eb4e45317d9dbc669
    Reviewed-on: https://chromium-review.googlesource.com/826825
    Reviewed-by: nainar <nainar@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525503}
    Former-commit-id: f34212c7732678b2d07691c149181987ff98a038

--- third_party/WebKit/Source/core/css/cssom/CSSTransformValue.cpp
+++ third_party/WebKit/Source/core/css/cssom/CSSTransformValue.cpp
@@ -10,6 +10,25 @@
 
 namespace blink {
 
+CSSTransformValue* CSSTransformValue::Create(
+    const HeapVector<Member<CSSTransformComponent>>& transform_components,
+    ExceptionState& exception_state) {
+  CSSTransformValue* value = Create(transform_components);
+  if (!value) {
+    exception_state.ThrowTypeError(
+        "CSSTransformValue must have at least one component");
+    return nullptr;
+  }
+  return value;
+}
+
+CSSTransformValue* CSSTransformValue::Create(
+    const HeapVector<Member<CSSTransformComponent>>& transform_components) {
+  if (transform_components.IsEmpty())
+    return nullptr;
+  return new CSSTransformValue(transform_components);
+}
+
 CSSTransformValue* CSSTransformValue::FromCSSValue(const CSSValue& css_value) {
   if (!css_value.IsValueList()) {
     // TODO(meade): Also need to check the separator here if we care.
@@ -27,12 +46,8 @@ CSSTransformValue* CSSTransformValue::FromCSSValue(const CSSValue& css_value) {
 }
 
 bool CSSTransformValue::is2D() const {
-  for (size_t i = 0; i < transform_components_.size(); i++) {
-    if (!transform_components_[i]->is2D()) {
-      return false;
-    }
-  }
-  return true;
+  return std::all_of(transform_components_.begin(), transform_components_.end(),
+                     [](const auto& component) { return component->is2D(); });
 }
 
 DOMMatrix* CSSTransformValue::toMatrix(ExceptionState& exception_state) const {
--- third_party/WebKit/Source/core/css/cssom/CSSTransformValue.h
+++ third_party/WebKit/Source/core/css/cssom/CSSTransformValue.h
@@ -20,12 +20,13 @@ class CORE_EXPORT CSSTransformValue final : public CSSStyleValue {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  static CSSTransformValue* Create() { return new CSSTransformValue(); }
-
   static CSSTransformValue* Create(
-      const HeapVector<Member<CSSTransformComponent>>& transform_components) {
-    return new CSSTransformValue(transform_components);
-  }
+      const HeapVector<Member<CSSTransformComponent>>& transform_components,
+      ExceptionState&);
+
+  // Blink-internal constructor
+  static CSSTransformValue* Create(
+      const HeapVector<Member<CSSTransformComponent>>& transform_components);
 
   static CSSTransformValue* FromCSSValue(const CSSValue&);
 
@@ -49,7 +50,6 @@ class CORE_EXPORT CSSTransformValue final : public CSSStyleValue {
   }
 
  private:
-  CSSTransformValue() = default;
   CSSTransformValue(
       const HeapVector<Member<CSSTransformComponent>>& transform_components)
       : CSSStyleValue(), transform_components_(transform_components) {}
