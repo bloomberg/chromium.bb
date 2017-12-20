6c832e7a1232c9592427f86e88f5c67d58bd86c8
Wez <wez@chromium.org>
2017-12-20 21:11:46 +0000

    Disable some more AliveCheckerTests.
    This disables the remaining tests which use the NotifyAliveMultipleTimes
    helper function, until they can be rewritten to use e.g. a MOCK_TIME
    ScopedTaskEnvironment instance.
    
    Bug: 789804
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I496fd5c76ebaed8df56a6c8b93225a2d3b0fe493
    Reviewed-on: https://chromium-review.googlesource.com/836736
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525454}
    Former-commit-id: 8a5ef6b01900b3ca19827854fbafd754f3e2e197

--- media/audio/alive_checker_unittest.cc
+++ media/audio/alive_checker_unittest.cc
@@ -289,7 +289,9 @@ TEST_F(AliveCheckerTest, DISABLED_NotifyThenStop) {
 
 // Start the checker, notify that the client is alive several times, then
 // run until detection. Repeat once.
-TEST_F(AliveCheckerTest, NotifyThenDetectDead) {
+// TODO(crbug.com/789804): Fix the test not to be flaky, e.g. by switching to
+// using a mocked clock, and re-enable it.
+TEST_F(AliveCheckerTest, DISABLED_NotifyThenDetectDead) {
   CreateAliveChecker(false, false);
 
   StartAliveChecker();
@@ -335,7 +337,9 @@ TEST_F(AliveCheckerTest, StopAtFirstAliveNotification_DontNotify) {
 // send alive notifications, and run until it detects dead. Start it again and
 // notify that the client is alive several times. Suspend and verify that it
 // doesn't detect dead. Resume and run until detected dead.
-TEST_F(AliveCheckerTest, SuspendResume_StartBeforeSuspend) {
+// TODO(crbug.com/789804): Fix the test not to be flaky, e.g. by switching to
+// using a mocked clock, and re-enable it.
+TEST_F(AliveCheckerTest, DISABLED_SuspendResume_StartBeforeSuspend) {
   CreateAliveChecker(false, true);
   ASSERT_TRUE(mock_power_observer_helper_);
 
