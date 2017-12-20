7b5568da9b98881334f52f46ee4ef1f49d8527a8
Filip Gorski <fgorski@chromium.org>
2017-12-20 23:54:56 +0000

    [Offline pages] StartOfflinePageUpgradeTask implementation
    Patch 1/3 for upgrading offline pages. Provides the following:
    * Capability to verify that item can be upgraded
    * Reducing the number of remaining attempts in case above is true
    * Delivering file path and digest as a result of starting,
      data which is later consumed by the upgrade process.
    
    Bug: 758690
    Change-Id: I878d5b8586d3cfb52184f2c540fbb9ddc45082bf
    Reviewed-on: https://chromium-review.googlesource.com/837154
    Commit-Queue: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Yafei Duan <romax@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525519}
    Former-commit-id: b22c1b9bc3f71df92b2eca9e4543c6be4b304442

---
+++ components/offline_pages/core/model/start_offline_page_upgrade_task.cc
@@ -0,0 +1,106 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/offline_pages/core/model/start_offline_page_upgrade_task.h"
+
+#include "base/bind.h"
+#include "base/files/file_util.h"
+#include "base/sys_info.h"
+#include "components/offline_pages/core/offline_page_metadata_store_sql.h"
+#include "components/offline_pages/core/offline_store_utils.h"
+#include "sql/connection.h"
+#include "sql/statement.h"
+#include "sql/transaction.h"
+
+namespace offline_pages {
+
+namespace {
+
+const int64_t kMinFreeDiskSpaceInBytes = 10 * 1024 * 1024;  // 10 MB
+
+StartUpgradeResult StartOfflinePageUpgradeSync(
+    int64_t offline_id,
+    const base::FilePath& target_directory,
+    sql::Connection* db) {
+  if (!db)
+    return StartUpgradeResult(StartUpgradeStatus::DB_ERROR);
+
+  sql::Transaction transaction(db);
+  if (!transaction.Begin())
+    return StartUpgradeResult(StartUpgradeStatus::DB_ERROR);
+
+  const char kSql[] =
+      "SELECT file_path, file_size, digest"
+      " FROM offlinepages_v1 WHERE offline_id = ?";
+  sql::Statement select_statement(db->GetCachedStatement(SQL_FROM_HERE, kSql));
+  select_statement.BindInt64(0, offline_id);
+  if (!select_statement.Step())
+    return StartUpgradeResult(StartUpgradeStatus::ITEM_MISSING);
+
+  base::FilePath file_path =
+      store_utils::FromDatabaseFilePath(select_statement.ColumnString(0));
+  if (!base::PathExists(file_path))
+    return StartUpgradeResult(StartUpgradeStatus::FILE_MISSING);
+
+  int64_t free_disk_space_on_target =
+      base::SysInfo::AmountOfFreeDiskSpace(target_directory);
+  if (free_disk_space_on_target < kMinFreeDiskSpaceInBytes)
+    return StartUpgradeResult(StartUpgradeStatus::NOT_ENOUGH_STORAGE);
+
+  int64_t file_size = select_statement.ColumnInt64(1);
+  if (free_disk_space_on_target < 2 * file_size)
+    return StartUpgradeResult(StartUpgradeStatus::NOT_ENOUGH_STORAGE);
+
+  // Digest will be consumed when returning.
+  std::string digest = select_statement.ColumnString(2);
+
+  // Conditions for upgrade are met here.
+  // Update remaining attempts in DB and complete task.
+  const char kUpdateSql[] =
+      "UPDATE offlinepages_v1 SET upgrade_attempt = upgrade_attempt - 1 "
+      " WHERE offline_id = ?";
+  sql::Statement update_statement(
+      db->GetCachedStatement(SQL_FROM_HERE, kUpdateSql));
+  update_statement.BindInt64(0, offline_id);
+
+  if (!update_statement.Run() || !transaction.Commit())
+    return StartUpgradeResult(StartUpgradeStatus::DB_ERROR);
+
+  return StartUpgradeResult(StartUpgradeStatus::SUCCESS, std::move(digest),
+                            std::move(file_path));
+}
+
+}  // namespace
+
+StartOfflinePageUpgradeTask::StartOfflinePageUpgradeTask(
+    OfflinePageMetadataStoreSQL* store,
+    int64_t offline_id,
+    const base::FilePath& target_directory,
+    StartUpgradeCallback callback)
+    : store_(store),
+      offline_id_(offline_id),
+      target_directory_(target_directory),
+      callback_(std::move(callback)),
+      weak_ptr_factory_(this) {
+  DCHECK(store_);
+  DCHECK(!callback_.is_null());
+}
+
+StartOfflinePageUpgradeTask::~StartOfflinePageUpgradeTask() {}
+
+void StartOfflinePageUpgradeTask::Run() {
+  store_->Execute(
+      base::BindOnce(&StartOfflinePageUpgradeSync, offline_id_,
+                     target_directory_),
+      base::BindOnce(&StartOfflinePageUpgradeTask::InformUpgradeAttemptDone,
+                     weak_ptr_factory_.GetWeakPtr()));
+}
+
+void StartOfflinePageUpgradeTask::InformUpgradeAttemptDone(
+    StartUpgradeResult result) {
+  std::move(callback_).Run(std::move(result));
+  TaskComplete();
+}
+
+}  // namespace offline_pages
---
+++ components/offline_pages/core/model/start_offline_page_upgrade_task_unittest.cc
@@ -0,0 +1,150 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/offline_pages/core/model/start_offline_page_upgrade_task.h"
+
+#include "base/files/scoped_temp_dir.h"
+#include "base/memory/ptr_util.h"
+#include "base/test/test_mock_time_task_runner.h"
+#include "base/threading/thread_task_runner_handle.h"
+#include "components/offline_pages/core/model/offline_page_item_generator.h"
+#include "components/offline_pages/core/offline_page_metadata_store_test_util.h"
+#include "components/offline_pages/core/test_task_runner.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace offline_pages {
+
+namespace {
+const char kTestDigest[] = "TestDigest==";
+}  // namespace
+
+class StartOfflinePageUpgradeTaskTest : public testing::Test {
+ public:
+  StartOfflinePageUpgradeTaskTest();
+  ~StartOfflinePageUpgradeTaskTest() override;
+
+  void SetUp() override;
+  void TearDown() override;
+
+  void StartUpgradeDone(StartUpgradeResult result);
+
+  OfflinePageMetadataStoreSQL* store() { return store_test_util_.store(); }
+  OfflinePageMetadataStoreTestUtil* store_test_util() {
+    return &store_test_util_;
+  }
+  OfflinePageItemGenerator* generator() { return &generator_; }
+  TestTaskRunner* runner() { return &runner_; }
+
+  StartUpgradeCallback callback() {
+    return base::BindOnce(&StartOfflinePageUpgradeTaskTest::StartUpgradeDone,
+                          base::Unretained(this));
+  }
+
+  StartUpgradeResult* last_result() { return &last_result_; }
+
+ private:
+  scoped_refptr<base::TestMockTimeTaskRunner> task_runner_;
+  base::ThreadTaskRunnerHandle task_runner_handle_;
+  OfflinePageMetadataStoreTestUtil store_test_util_;
+  OfflinePageItemGenerator generator_;
+  TestTaskRunner runner_;
+  StartUpgradeResult last_result_;
+};
+
+StartOfflinePageUpgradeTaskTest::StartOfflinePageUpgradeTaskTest()
+    : task_runner_(new base::TestMockTimeTaskRunner()),
+      task_runner_handle_(task_runner_),
+      store_test_util_(task_runner_),
+      runner_(task_runner_),
+      last_result_(StartUpgradeStatus::DB_ERROR) {}
+
+StartOfflinePageUpgradeTaskTest::~StartOfflinePageUpgradeTaskTest() {}
+
+void StartOfflinePageUpgradeTaskTest::SetUp() {
+  store_test_util_.BuildStoreInMemory();
+}
+
+void StartOfflinePageUpgradeTaskTest::TearDown() {
+  store_test_util_.DeleteStore();
+}
+
+void StartOfflinePageUpgradeTaskTest::StartUpgradeDone(
+    StartUpgradeResult result) {
+  last_result_ = std::move(result);
+}
+
+TEST_F(StartOfflinePageUpgradeTaskTest, StartUpgradeSuccess) {
+  base::ScopedTempDir temp_dir;
+  ASSERT_TRUE(temp_dir.CreateUniqueTempDir());
+  generator()->SetArchiveDirectory(temp_dir.GetPath());
+
+  OfflinePageItem page = generator()->CreateItemWithTempFile();
+  page.upgrade_attempt = 3;
+  page.digest = kTestDigest;
+  store_test_util()->InsertItem(page);
+
+  auto task = base::MakeUnique<StartOfflinePageUpgradeTask>(
+      store(), page.offline_id, temp_dir.GetPath(), callback());
+  runner()->RunTask(std::move(task));
+
+  EXPECT_EQ(StartUpgradeStatus::SUCCESS, last_result()->status);
+  EXPECT_EQ(kTestDigest, last_result()->digest);
+  EXPECT_EQ(page.file_path, last_result()->file_path);
+
+  auto offline_page = store_test_util()->GetPageByOfflineId(page.offline_id);
+  ASSERT_TRUE(offline_page);
+  EXPECT_EQ(2, offline_page->upgrade_attempt);
+}
+
+TEST_F(StartOfflinePageUpgradeTaskTest, StartUpgradeItemMissing) {
+  auto task = base::MakeUnique<StartOfflinePageUpgradeTask>(
+      store(), 42, base::FilePath(), callback());
+  runner()->RunTask(std::move(task));
+
+  EXPECT_EQ(StartUpgradeStatus::ITEM_MISSING, last_result()->status);
+  EXPECT_TRUE(last_result()->digest.empty());
+  EXPECT_TRUE(last_result()->file_path.empty());
+}
+
+TEST_F(StartOfflinePageUpgradeTaskTest, StartUpgradeFileMissing) {
+  OfflinePageItem page = generator()->CreateItem();
+  page.upgrade_attempt = 3;
+  store_test_util()->InsertItem(page);
+
+  auto task = base::MakeUnique<StartOfflinePageUpgradeTask>(
+      store(), page.offline_id, base::FilePath(), callback());
+  runner()->RunTask(std::move(task));
+
+  EXPECT_EQ(StartUpgradeStatus::FILE_MISSING, last_result()->status);
+  EXPECT_TRUE(last_result()->digest.empty());
+  EXPECT_TRUE(last_result()->file_path.empty());
+
+  auto offline_page = store_test_util()->GetPageByOfflineId(page.offline_id);
+  ASSERT_TRUE(offline_page);
+  EXPECT_EQ(3, offline_page->upgrade_attempt);
+}
+
+TEST_F(StartOfflinePageUpgradeTaskTest, StartUpgradeNotEnoughSpace) {
+  base::ScopedTempDir temp_dir;
+  ASSERT_TRUE(temp_dir.CreateUniqueTempDir());
+  generator()->SetArchiveDirectory(temp_dir.GetPath());
+
+  OfflinePageItem page = generator()->CreateItemWithTempFile();
+  page.upgrade_attempt = 3;
+  store_test_util()->InsertItem(page);
+
+  auto task = base::MakeUnique<StartOfflinePageUpgradeTask>(
+      store(), page.offline_id, base::FilePath(), callback());
+  runner()->RunTask(std::move(task));
+
+  EXPECT_EQ(StartUpgradeStatus::NOT_ENOUGH_STORAGE, last_result()->status);
+  EXPECT_TRUE(last_result()->digest.empty());
+  EXPECT_TRUE(last_result()->file_path.empty());
+
+  auto offline_page = store_test_util()->GetPageByOfflineId(page.offline_id);
+  ASSERT_TRUE(offline_page);
+  EXPECT_EQ(3, offline_page->upgrade_attempt);
+}
+
+}  // namespace offline_pages
