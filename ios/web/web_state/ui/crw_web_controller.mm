08e9c3c3633a41ee0af0cba87ad552f03ff79ee5
Mike Dougherty <michaeldo@chromium.org>
2017-12-20 14:15:19 -0800

    Prevent crash when determining if current navigation is a POST request.
    Bug: 795474
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I28836579c47ade716261e531e5eaeb5a81efb14d
    Reviewed-on: https://chromium-review.googlesource.com/837728
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525698}
    Former-commit-id: d2013e4a0099a66e15260b1075a88a3bf9f8ce8c

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -1317,7 +1317,13 @@ const NSTimeInterval kSnapshotOverlayTransition = 0.5;
       _pendingNavigationInfo
           ? [_pendingNavigationInfo HTTPMethod]
           : [self currentBackForwardListItemHolder]->http_method();
-  return [HTTPMethod isEqual:@"POST"] || self.currentNavItem->HasPostData();
+  if ([HTTPMethod isEqual:@"POST"]) {
+    return YES;
+  }
+  if (!self.currentNavItem) {
+    return NO;
+  }
+  return self.currentNavItem->HasPostData();
 }
 
 - (BOOL)isCurrentNavigationBackForward {
