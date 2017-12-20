e75182af8bb8388a33c77e60cbc2a41ed76c4f25
Chris Cunningham <chcunningham@chromium.org>
2017-12-20 01:17:19 +0000

    Clean up proprietary codec checks/expectations in LayoutTests
    This is mostly a reland of
    https://chromium-review.googlesource.com/c/chromium/src/+/807604
    with some improvements and now unblocked by changes to bots.
    
    Media layout tests need repair
    1) Many tests check for proprietary codec support by only asking about
       "video/mp4" without specifying a codec. This check is busted
       because MP4 is no longer considered proprietary while codecs like
       H264 and AAC still are. We currently claim "video/mp4" is entirely
       unsupported via test hacks to avoid breaking those checks.
    2) Some MSE tests focus on MP4 with H264. While many bots now have the
       needed codecs, we artificially remove their support via test-only
       hacks in order to keep expectations the same for all runners.
    
    This CL does the following
    1) Remove incomplete checks for "video/mp4" - instead hardcode use of
       the .ogv files (preserving the existing behavior).
    2) Update MSE mp4/h264 test expectations to PASS. Starting this week
       all webkit bots include the needed codecs.
    3) Remove test hacks that artificially reduce codec support.
    
    It is also not a goal to make LayoutTests try all the supported codecs.
    That is covered by unit/integration tests.
    
    Due to 3) above, some external/wpt/media-source tests now fail because
    they select mp4. Fixing these failures is tracked in Issue 794338.
    
    TBR: dalecurtis@chromium.org, peter@chromium.org
    Bug: 327115,746579,787575,568704,794338
    Change-Id: I8443d32fc9871c887d0009605cf8876521844916
    Reviewed-on: https://chromium-review.googlesource.com/827649
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525212}
    Former-commit-id: 117b9d3a698a52d96857b31caeb72c2e74f2a782

--- third_party/WebKit/LayoutTests/http/tests/media/autoplay/resources/nested-frame-with-header.php
+++ third_party/WebKit/LayoutTests/http/tests/media/autoplay/resources/nested-frame-with-header.php
@@ -3,6 +3,5 @@ Header("Feature-Policy: autoplay 'none'");
 ?>
 <!DOCTYPE html>
 <html>
-<script src="../../../media-resources/media-file.js"></script>
 <script src="autoplay-test.js"></script>
 </html>
