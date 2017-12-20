238cdf9156503ea250aca3b5800a944847880e69
bsheedy <bsheedy@chromium.org>
2017-12-20 15:06:06 -0800

    Port WebVrTabTest.java to browser tests
    Ports the one test in WebVrTabTest.java to also be a VR browser test.
    
    Bug: 795455
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ib929779c3f9c5175f43b952854c6e3d3cc6c2559
    Reviewed-on: https://chromium-review.googlesource.com/837597
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525805}
    Former-commit-id: fe2c9b07629d1f19c2a95a447ee0fdcd0e9c7937

---
+++ chrome/browser/vr/webvr_tab_browser_test.cc
@@ -0,0 +1,30 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/browser_tabstrip.h"
+#include "chrome/browser/vr/test/vr_browser_test.h"
+#include "chrome/browser/vr/test/vr_transition_utils.h"
+#include "url/gurl.h"
+#include "url/url_constants.h"
+
+// Browser test equivalent of
+// chrome/android/javatests/src/.../browser/vr_shell/WebVrTabTest.java.
+// End-to-end tests for testing WebVR's interaction with multiple tabss.
+
+namespace vr {
+
+// Tests that non-focused tabs cannot get pose information
+IN_PROC_BROWSER_TEST_F(VrBrowserTest, REQUIRES_GPU(TestPoseDataUnfocusedTab)) {
+  LoadUrlAndAwaitInitialization(
+      GetHtmlTestFile("test_pose_data_unfocused_tab"));
+  ExecuteStepAndWait("stepCheckFrameDataWhileFocusedTab()",
+                     GetFirstTabWebContents());
+  chrome::AddTabAt(browser(), GURL(url::kAboutBlankURL),
+                   -1 /* index, append to end */, true /* foreground */);
+  ExecuteStepAndWait("stepCheckFrameDataWhileNonFocusedTab()",
+                     GetFirstTabWebContents());
+  EndTest(GetFirstTabWebContents());
+}
+
+}  // namespace vr
