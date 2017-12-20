e6ee1d48652684205bbcf7eeaf141b2f4e08dba2
Carlos IL <carlosil@chromium.org>
2017-12-20 00:18:11 +0000

    Added mojo interface for interstitial commands.
    Added mojo interface to handle interstitial commands and implemented it
    in SSLErrorTabHelper, which passes the command to SSLBlockingPage.
    
    Bug: 779782
    Change-Id: I261dddd835a9e5360c214acd6cedd6d67d2d3710
    Reviewed-on: https://chromium-review.googlesource.com/833455
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525192}
    Former-commit-id: 2136d4f77a0e768a011f69ebf3a9e2d38d98ce45

--- chrome/browser/ssl/ssl_error_tab_helper.cc
+++ chrome/browser/ssl/ssl_error_tab_helper.cc
@@ -3,8 +3,9 @@
 // found in the LICENSE file.
 
 #include "chrome/browser/ssl/ssl_error_tab_helper.h"
-
+#include "base/strings/string_number_conversions.h"
 #include "components/security_interstitials/content/security_interstitial_page.h"
+#include "components/security_interstitials/core/controller_client.h"
 #include "content/public/browser/navigation_handle.h"
 
 DEFINE_WEB_CONTENTS_USER_DATA_KEY(SSLErrorTabHelper);
@@ -64,11 +65,88 @@ SSLErrorTabHelper::GetBlockingPageForCurrentlyCommittedNavigationForTesting() {
 }
 
 SSLErrorTabHelper::SSLErrorTabHelper(content::WebContents* web_contents)
-    : WebContentsObserver(web_contents) {}
+    : WebContentsObserver(web_contents), binding_(web_contents, this) {}
 
 void SSLErrorTabHelper::SetBlockingPage(
     int64_t navigation_id,
     std::unique_ptr<security_interstitials::SecurityInterstitialPage>
         blocking_page) {
   blocking_pages_for_navigations_[navigation_id] = std::move(blocking_page);
+}
+
+void SSLErrorTabHelper::HandleCommand(
+    security_interstitials::SecurityInterstitialCommand cmd) {
+  if (blocking_page_for_currently_committed_navigation_) {
+    // Currently commands need to be converted to strings before passing them
+    // to CommandReceived, which then turns them into integers again, this
+    // redundant conversion will be removed once commited interstitials are the
+    // only supported codepath.
+    blocking_page_for_currently_committed_navigation_->CommandReceived(
+        base::NumberToString(cmd));
+  }
+}
+
+void SSLErrorTabHelper::DontProceed() {
+  HandleCommand(
+      security_interstitials::SecurityInterstitialCommand::CMD_DONT_PROCEED);
+}
+
+void SSLErrorTabHelper::Proceed() {
+  HandleCommand(
+      security_interstitials::SecurityInterstitialCommand::CMD_PROCEED);
+}
+
+void SSLErrorTabHelper::ShowMoreSection() {
+  HandleCommand(security_interstitials::SecurityInterstitialCommand::
+                    CMD_SHOW_MORE_SECTION);
+}
+
+void SSLErrorTabHelper::OpenHelpCenter() {
+  HandleCommand(security_interstitials::SecurityInterstitialCommand::
+                    CMD_OPEN_HELP_CENTER);
+}
+
+void SSLErrorTabHelper::OpenDiagnostic() {
+  // SSL error pages do not implement this.
+  NOTREACHED();
+}
+
+void SSLErrorTabHelper::Reload() {
+  HandleCommand(
+      security_interstitials::SecurityInterstitialCommand::CMD_RELOAD);
+}
+
+void SSLErrorTabHelper::OpenDateSettings() {
+  HandleCommand(security_interstitials::SecurityInterstitialCommand::
+                    CMD_OPEN_DATE_SETTINGS);
+}
+
+void SSLErrorTabHelper::OpenLogin() {
+  HandleCommand(
+      security_interstitials::SecurityInterstitialCommand::CMD_OPEN_LOGIN);
+}
+
+void SSLErrorTabHelper::DoReport() {
+  HandleCommand(
+      security_interstitials::SecurityInterstitialCommand::CMD_DO_REPORT);
+}
+
+void SSLErrorTabHelper::DontReport() {
+  HandleCommand(
+      security_interstitials::SecurityInterstitialCommand::CMD_DONT_REPORT);
+}
+
+void SSLErrorTabHelper::OpenReportingPrivacy() {
+  HandleCommand(security_interstitials::SecurityInterstitialCommand::
+                    CMD_OPEN_REPORTING_PRIVACY);
+}
+
+void SSLErrorTabHelper::OpenWhitepaper() {
+  HandleCommand(
+      security_interstitials::SecurityInterstitialCommand::CMD_OPEN_WHITEPAPER);
+}
+
+void SSLErrorTabHelper::ReportPhishingError() {
+  // SSL error pages do not implement this.
+  NOTREACHED();
 }
