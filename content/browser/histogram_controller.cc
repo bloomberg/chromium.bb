f089e3468b71533beeaf2f6950b0c0bdba8bfc55
Nicholas Verne <nverne@chromium.org>
2017-12-21 00:42:54 +0000

    WrapCallbackWithDefaultInvokeIfNotRun and WrapCallbackWithDropHandler
    WrapCallbackWithDefaultInvokeIfNotRun renames media::base::ScopedCallbackRunner and
    moves it to mojo::.
    WrapCallbackWithDropHandler is function allowing users to specify
    the "drop" callback if needed.
    
    These callback helpers are intended for use on client side mojo calls.
    foo_ptr->MethodWithResponse(arg1, arg2, mojo::WrapCallbackWithDefaultInvokeIfNotRun(
       base::BindOnce(&Foo::OnResponse), default_arg)));
    
    Please see the related bug for more details.
    
    Bug: 775358
    Change-Id: Ied9d0742a260954ee670a4d8b388053c32b2665b
    Reviewed-on: https://chromium-review.googlesource.com/724763
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525536}
    Former-commit-id: af16e1187bb6300ccafe56aa4d8dfad1944ac656

--- content/browser/histogram_controller.cc
+++ content/browser/histogram_controller.cc
@@ -16,6 +16,7 @@
 #include "content/public/common/bind_interface_helpers.h"
 #include "content/public/common/child_process_host.h"
 #include "content/public/common/process_type.h"
+#include "mojo/public/cpp/bindings/callback_helpers.h"
 
 namespace content {
 
@@ -54,35 +55,6 @@ void HistogramController::OnHistogramDataCollected(
                                           pickled_histograms);
   }
 }
-
-class FetcherCallbackRunner {
- public:
-  FetcherCallbackRunner(int sequence_number)
-      : sequence_number_(sequence_number), did_run_(false) {}
-
-  ~FetcherCallbackRunner() {
-    if (!did_run_) {
-      Run(std::vector<std::string>());
-    }
-  }
-  static content::mojom::ChildHistogramFetcher::
-      GetChildNonPersistentHistogramDataCallback
-      Make(int sequence_number) {
-    return base::BindOnce(
-        &FetcherCallbackRunner::Run,
-        std::make_unique<FetcherCallbackRunner>(sequence_number));
-  }
-
-  void Run(const std::vector<std::string>& pickled_histograms) {
-    did_run_ = true;
-    HistogramController::GetInstance()->OnHistogramDataCollected(
-        sequence_number_, pickled_histograms);
-  }
-
- private:
-  int sequence_number_;
-  bool did_run_;
-};
 
 void HistogramController::Register(HistogramSubscriber* subscriber) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
@@ -187,7 +159,10 @@ void HistogramController::GetHistogramDataFromChildProcesses(
     if (auto* child_histogram_fetcher =
             GetChildHistogramFetcherInterface(iter.GetHost())) {
       child_histogram_fetcher->GetChildNonPersistentHistogramData(
-          FetcherCallbackRunner::Make(sequence_number));
+          mojo::WrapCallbackWithDefaultInvokeIfNotRun(
+              base::BindOnce(&HistogramController::OnHistogramDataCollected,
+                             base::Unretained(this), sequence_number),
+              std::vector<std::string>()));
       ++pending_processes;
     }
   }
@@ -207,7 +182,10 @@ void HistogramController::GetHistogramData(int sequence_number) {
     if (auto* child_histogram_fetcher =
             GetChildHistogramFetcherInterface(it.GetCurrentValue())) {
       child_histogram_fetcher->GetChildNonPersistentHistogramData(
-          FetcherCallbackRunner::Make(sequence_number));
+          mojo::WrapCallbackWithDefaultInvokeIfNotRun(
+              base::BindOnce(&HistogramController::OnHistogramDataCollected,
+                             base::Unretained(this), sequence_number),
+              std::vector<std::string>()));
       ++pending_processes;
     }
   }
