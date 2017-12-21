4e57f2a566b839fb53a94676765c7dff2627550d
Trent Apted <tapted@chromium.org>
2017-12-21 14:47:54 +1100

    MacViews: Speculative fix for failed CHECK([params.parent window]) showing credit card bubble
    Code higher in the stack clearly has a window, but params.parent is
    initialized from WebContents::GetNativeView(). During tab dragging, this
    can sometimes differ from the return of GetBrowserWithWebContents().
    
    To fix, pass the window we know exists, rather than one that might not.
    
    Tab dragging on Mac occurs inside a nested runloop on Mac, so this is
    hard to reproroduce or test for.
    
    Bug: 796789
    Change-Id: Ib90a34f9f83d05a9834411036ae51ae5a0d2bd32
    Reviewed-on: https://chromium-review.googlesource.com/838721
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Commit-Queue: Mathieu Perreault <mathp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525707}
    Former-commit-id: d1fbc6309775f4f6281b01eac6668c56c09124d0

--- chrome/browser/ui/cocoa/autofill/save_card_bubble_view_views.mm
+++ chrome/browser/ui/cocoa/autofill/save_card_bubble_view_views.mm
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include "base/feature_list.h"
+#include "chrome/browser/platform_util.h"
 #include "chrome/browser/ui/cocoa/autofill/save_card_bubble_view_bridge.h"
 #include "chrome/browser/ui/cocoa/browser_dialogs_views_mac.h"
 #include "chrome/browser/ui/cocoa/browser_window_cocoa.h"
@@ -23,19 +24,19 @@ SaveCardBubbleView* CreateSaveCardBubbleView(
     BrowserWindowController* browser_window_controller,
     bool user_gesture) {
   if (base::FeatureList::IsEnabled(kAutofillToolkitViewsCreditCardDialogsMac)) {
+    NSWindow* parent_window = [browser_window_controller window];
     LocationBarViewMac* location_bar =
         [browser_window_controller locationBarBridge];
     gfx::Point anchor =
         gfx::ScreenPointFromNSPoint(ui::ConvertPointFromWindowToScreen(
-            [browser_window_controller window],
-            location_bar->GetSaveCreditCardBubblePoint()));
+            parent_window, location_bar->GetSaveCreditCardBubblePoint()));
     autofill::SaveCardBubbleViews* bubble =
         new SaveCardBubbleViews(nullptr, anchor, web_contents, controller);
 
     // Usually the anchor view determines the arrow type, but there is none in
     // MacViews. So always use TOP_RIGHT, even in fullscreen.
     bubble->set_arrow(views::BubbleBorder::TOP_RIGHT);
-    bubble->set_parent_window(web_contents->GetNativeView());
+    bubble->set_parent_window(platform_util::GetViewForWindow(parent_window));
     views::BubbleDialogDelegateView::CreateBubble(bubble);
     bubble->Show(user_gesture ? autofill::SaveCardBubbleViews::USER_GESTURE
                               : autofill::SaveCardBubbleViews::AUTOMATIC);
