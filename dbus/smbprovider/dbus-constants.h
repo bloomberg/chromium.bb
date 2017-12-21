551a638c1fc92cd23d43bbd10927b2744e3eac55
Allen Vicencio <allenvic@google.com>
2017-12-21 15:44:34 -0800

    smbprovider: Add ReadFile proto
    Adding input parameter for ReadFile() as protobuf format.
    
    BUG=chromium:757625
    TEST=emerges
    Change-Id: Ia2676f8002c5d0529998477bfc0af72e35e3fc32
    Reviewed-on: https://chromium-review.googlesource.com/841527
    Commit-Ready: Zentaro Kavanagh <zentaro@chromium.org>
    Tested-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>

--- dbus/smbprovider/dbus-constants.h
+++ dbus/smbprovider/dbus-constants.h
@@ -19,6 +19,7 @@ const char kReadDirectoryMethod[] = "ReadDirectory";
 const char kGetMetadataEntryMethod[] = "GetMetadataEntry";
 const char kOpenFileMethod[] = "OpenFile";
 const char kCloseFileMethod[] = "CloseFile";
+const char kReadFileMethod[] = "ReadFile";
 
 }  // namespace smbprovider
 
