c7fadd9f38d602efd3700291914027cec3ad22c9
tanvir.rizvi <tanvir.rizvi@samsung.com>
2017-12-21 15:05:15 +0530

    Fix a crash in FormatBlock with Nth child style.
    FormatBlock execution crashes if command is
    applied with unusual HTML content like,
    applying style on the nth-child(even) with canvas
    element embedded.
    Crash happens when format block tries to
    MOveParagraphWithClones, but the calculated
    VisiblePosition comes as nill.
    Prevention mechanism is added to avoid this crash.
    
    Bug: 795280
    Change-Id: I8b55568799d9cb426326ef3f681827b0987373c8
    Reviewed-on: https://chromium-review.googlesource.com/833818
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Tanvir Rizvi <tanvir.rizvi@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#525937}
    Former-commit-id: 6483075f1938db1d02f4bb4ac09c7006442801ce

--- third_party/WebKit/Source/core/editing/commands/FormatBlockCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/FormatBlockCommand.cpp
@@ -123,9 +123,15 @@ void FormatBlockCommand::FormatRange(const Position& start,
   bool was_end_of_paragraph =
       IsEndOfParagraph(CreateVisiblePosition(last_paragraph_in_block_node));
 
-  MoveParagraphWithClones(CreateVisiblePosition(start),
-                          CreateVisiblePosition(end), block_element,
-                          outer_block, editing_state);
+  const VisiblePosition& start_of_paragraph_to_move =
+      CreateVisiblePosition(start);
+  const VisiblePosition& end_of_paragraph_to_move = CreateVisiblePosition(end);
+  // execCommand/format_block/format_block_with_nth_child_crash.html reaches
+  // here.
+  ABORT_EDITING_COMMAND_IF(start_of_paragraph_to_move.IsNull());
+  ABORT_EDITING_COMMAND_IF(end_of_paragraph_to_move.IsNull());
+  MoveParagraphWithClones(start_of_paragraph_to_move, end_of_paragraph_to_move,
+                          block_element, outer_block, editing_state);
   if (editing_state->IsAborted())
     return;
 
