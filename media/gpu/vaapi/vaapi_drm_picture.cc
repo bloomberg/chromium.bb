796209678c02140c7b29f40d14e2e783cfeff120
Julien Isorce <julien.isorce@chromium.org>
2017-12-21 10:15:36 +0000

    Enable VaapiDrmPicture when passing --use-gl=egl
    The existing VaapiTFPPicture(X11) only works with --use-gl=desktop.
    
    CQ-DEPEND=CL:817563
    Bug: 785201
    Tests: gfx_unittests --gtest_filter=*NativePixmapDmabuf*
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I304f729d3a9db04169fb3070c0382c561a659d5b
    Reviewed-on: https://chromium-review.googlesource.com/766787
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Julien Isorce <julien.isorce@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525662}
    Former-commit-id: bc7b0afd8a1be8cfee2164dead6f4bc3b1e1d3f4

--- media/gpu/vaapi/vaapi_drm_picture.cc
+++ media/gpu/vaapi/vaapi_drm_picture.cc
@@ -8,6 +8,7 @@
 #include "media/gpu/vaapi/va_surface.h"
 #include "media/gpu/vaapi/vaapi_wrapper.h"
 #include "ui/gfx/gpu_memory_buffer.h"
+#include "ui/gfx/linux/native_pixmap_dmabuf.h"
 #include "ui/gfx/native_pixmap.h"
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_image_native_pixmap.h"
@@ -74,12 +75,15 @@ bool VaapiDrmPicture::Initialize() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(pixmap_);
 
+  // Create a |va_surface_| from dmabuf fds (pixmap->GetDmaBufFd)
   va_surface_ = vaapi_wrapper_->CreateVASurfaceForPixmap(pixmap_);
   if (!va_surface_) {
     LOG(ERROR) << "Failed creating VASurface for NativePixmap";
     return false;
   }
 
+#if defined(USE_OZONE)
+  // Import dmabuf fds into the output gl texture through EGLImage.
   if (texture_id_ != 0 && !make_context_current_cb_.is_null()) {
     if (!make_context_current_cb_.Run())
       return false;
@@ -100,6 +104,11 @@ bool VaapiDrmPicture::Initialize() {
       return false;
     }
   }
+#else
+  // On non-ozone, no need to import dmabuf fds into output the gl texture
+  // because the dmabuf fds have been made from it.
+  DCHECK(pixmap_->AreDmaBufFdsValid());
+#endif
 
   if (client_texture_id_ != 0 && !bind_image_cb_.is_null()) {
     if (!bind_image_cb_.Run(client_texture_id_, texture_target_, gl_image_,
@@ -114,14 +123,56 @@ bool VaapiDrmPicture::Initialize() {
 
 bool VaapiDrmPicture::Allocate(gfx::BufferFormat format) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+// The goal of this method (ozone and non-ozone) is to allocate the
+// |pixmap_| which is a gl::GLImageNativePixmap.
 #if defined(USE_OZONE)
   ui::OzonePlatform* platform = ui::OzonePlatform::GetInstance();
   ui::SurfaceFactoryOzone* factory = platform->GetSurfaceFactoryOzone();
   pixmap_ = factory->CreateNativePixmap(gfx::kNullAcceleratedWidget, size_,
                                         format, gfx::BufferUsage::SCANOUT);
 #else
-  // TODO(jisorce): Implement non-ozone case, see https://crbug.com/785201.
-  NOTIMPLEMENTED();
+  // Export the gl texture as dmabuf.
+  if (texture_id_ != 0 && !make_context_current_cb_.is_null()) {
+    if (!make_context_current_cb_.Run())
+      return false;
+
+    scoped_refptr<gl::GLImageNativePixmap> image(new gl::GLImageNativePixmap(
+        size_, BufferFormatToInternalFormat(format)));
+
+    // Create an EGLImage from a gl texture
+    if (!image->InitializeFromTexture(texture_id_)) {
+      DLOG(ERROR) << "Failed to initialize eglimage from texture id: "
+                  << texture_id_;
+      return false;
+    }
+
+    // Export the EGLImage as dmabuf.
+    gfx::NativePixmapHandle native_pixmap_handle = image->ExportHandle();
+    if (!native_pixmap_handle.planes.size()) {
+      DLOG(ERROR) << "Failed to export EGLImage as dmabuf fds";
+      return false;
+    }
+
+    // Convert NativePixmapHandle to NativePixmapDmaBuf.
+    scoped_refptr<gfx::NativePixmap> native_pixmap_dmabuf(
+        new gfx::NativePixmapDmaBuf(size_, format, native_pixmap_handle));
+    if (!native_pixmap_dmabuf->AreDmaBufFdsValid()) {
+      DLOG(ERROR) << "Invalid dmabuf fds";
+      return false;
+    }
+
+    if (!image->BindTexImage(texture_target_)) {
+      DLOG(ERROR) << "Failed to bind texture to GLImage";
+      return false;
+    }
+
+    // The |pixmap_| takes ownership of the dmabuf fds. So the only reason to
+    // to keep a reference on the image is because the GPU service needs to
+    // track this image as it will be attached to a client texture.
+    pixmap_ = native_pixmap_dmabuf;
+    gl_image_ = image;
+  }
 #endif  // USE_OZONE
 
   if (!pixmap_) {
