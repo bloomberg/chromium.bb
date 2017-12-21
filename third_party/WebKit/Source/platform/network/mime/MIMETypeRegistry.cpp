1df4dd09a35dfc624d5e82d35719ac914fdaa8bf
Ken Rockot <rockot@chromium.org>
2017-12-21 04:40:57 +0000

    Add CHECK to track down invalid IPC
    Looks like GetMimeTypeForExtension is probably being called with a
    null string. This is an attempt to catch the offender.
    
    Bug: 791168
    Change-Id: I570926e88921175f82faad761f9a5294befcfa22
    Reviewed-on: https://chromium-review.googlesource.com/838513
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525609}
    Former-commit-id: 956d901e99587f75ac87737a5b270a32d7217bdb

--- third_party/WebKit/Source/platform/network/mime/MIMETypeRegistry.cpp
+++ third_party/WebKit/Source/platform/network/mime/MIMETypeRegistry.cpp
@@ -64,6 +64,10 @@ String MIMETypeRegistry::GetMIMETypeForExtension(const String& ext) {
   // The sandbox restricts our access to the registry, so we need to proxy
   // these calls over to the browser process.
   DEFINE_STATIC_LOCAL(MimeRegistryPtrHolder, registry_holder, ());
+
+  // TODO(https://crbug.com/791168): Remove this temporary CHECK.
+  CHECK(!ext.IsNull());
+
   String mime_type;
   if (!registry_holder.mime_registry->GetMimeTypeFromExtension(ext, &mime_type))
     return String();
