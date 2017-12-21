998956dee4c9a95b6153276f9b5b26c1d1741070
Mitsuru Oshima <oshima@chromium.org>
2017-12-21 03:54:57 +0000

    Make SystemWebDialog in login/lock state modal.
    Bug: 794776
    Test: covered by unit test
    Change-Id: Icf59f6c8fb71d8e3459a2fa26d0027f2238b022b
    Reviewed-on: https://chromium-review.googlesource.com/833528
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525592}
    Former-commit-id: 5ecb6ba426ad733bd1a2aa681df00c4c703f40be

---
+++ chrome/browser/ui/webui/chromeos/system_web_dialog_browsertest.cc
@@ -0,0 +1,59 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/webui/chromeos/system_web_dialog_delegate.h"
+
+#include "ash/shell_port.h"
+#include "chrome/browser/chromeos/login/login_manager_test.h"
+#include "chrome/browser/chromeos/login/startup_utils.h"
+#include "chrome/common/webui_url_constants.h"
+#include "components/signin/core/account_id/account_id.h"
+#include "url/gurl.h"
+
+namespace {
+
+constexpr char kTestUser[] = "test-user@gmail.com";
+constexpr char kTestUserGaiaId[] = "1234567890";
+
+class SystemWebDialogTest : public chromeos::LoginManagerTest {
+ public:
+  SystemWebDialogTest() : LoginManagerTest(false) {}
+  ~SystemWebDialogTest() override = default;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(SystemWebDialogTest);
+};
+
+class MockSystemWebDialog : public chromeos::SystemWebDialogDelegate {
+ public:
+  MockSystemWebDialog()
+      : SystemWebDialogDelegate(GURL(chrome::kChromeUIVersionURL),
+                                base::string16()) {}
+  ~MockSystemWebDialog() override = default;
+
+  std::string GetDialogArgs() const override { return std::string(); }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(MockSystemWebDialog);
+};
+
+}  // namespace
+
+IN_PROC_BROWSER_TEST_F(SystemWebDialogTest, ModalTest) {
+  chromeos::SystemWebDialogDelegate* dialog = new MockSystemWebDialog();
+  dialog->ShowSystemDialog();
+  EXPECT_TRUE(ash::ShellPort::Get()->IsSystemModalWindowOpen());
+}
+
+IN_PROC_BROWSER_TEST_F(SystemWebDialogTest, PRE_NonModalTest) {
+  RegisterUser(AccountId::FromUserEmailGaiaId(kTestUser, kTestUserGaiaId));
+  chromeos::StartupUtils::MarkOobeCompleted();
+}
+
+IN_PROC_BROWSER_TEST_F(SystemWebDialogTest, NonModalTest) {
+  LoginUser(AccountId::FromUserEmailGaiaId(kTestUser, kTestUserGaiaId));
+  chromeos::SystemWebDialogDelegate* dialog = new MockSystemWebDialog();
+  dialog->ShowSystemDialog();
+  EXPECT_FALSE(ash::ShellPort::Get()->IsSystemModalWindowOpen());
+}
