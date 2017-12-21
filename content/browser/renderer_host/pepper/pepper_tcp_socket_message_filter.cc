e48c5912e3a87a54f2f6531064cf054f9a60db2d
rhalavati@google.com <rhalavati@google.com>
2017-12-21 09:14:19 +0000

    Add network traffic annotation to PepperTCPSocketMessageFilter.
    Network traffic annotation tag is added to socket write function in
    PepperTCPSocketMessageFilter.
    
    Bug: 656607
    Change-Id: Ia9be35558f35db3bbca77ef1b58cd42452227e4c
    Reviewed-on: https://chromium-review.googlesource.com/821544
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525653}
    Former-commit-id: 6814327cbe988e35d7278977e3403d1759c7dc68

--- content/browser/renderer_host/pepper/pepper_tcp_socket_message_filter.cc
+++ content/browser/renderer_host/pepper/pepper_tcp_socket_message_filter.cc
@@ -683,22 +683,60 @@ void PepperTCPSocketMessageFilter::DoWrite(
   DCHECK(state_.IsConnected());
 
   int net_result = net::ERR_FAILED;
+  net::NetworkTrafficAnnotationTag traffic_annotation =
+      net::DefineNetworkTrafficAnnotation("pepper_tcp_socket", R"(
+        semantics {
+          sender: "Pepper TCP Socket"
+          description:
+            "Pepper plugins use this API to send and receive data over the "
+            "network using TCP connections. This inteface is used by Flash and "
+            "PDF viewer, and Chrome Apps which use plugins to send/receive TCP "
+            "traffic (require Chrome Apps TCP socket permission)."
+          trigger:
+            "A request from a Pepper plugin."
+          data: "Any data that the plugin sends."
+          destination: OTHER
+          destination_other:
+            "Data can be sent to any destination."
+        }
+        policy {
+          cookies_allowed: NO
+          setting:
+            "These requests cannot be disabled, but will not happen if user "
+            "does not use Flash, internal PDF Viewer, or Chrome Apps that use "
+            "Pepper interface."
+          chrome_policy {
+            DefaultPluginsSetting {
+              DefaultPluginsSetting: 2
+            }
+          }
+          chrome_policy {
+            AlwaysOpenPdfExternally {
+              AlwaysOpenPdfExternally: true
+            }
+          }
+          chrome_policy {
+            ExtensionInstallBlacklist {
+              ExtensionInstallBlacklist: {
+                entries: '*'
+              }
+            }
+          }
+        })");
   if (socket_) {
     DCHECK_EQ(state_.state(), TCPSocketState::CONNECTED);
-    // TODO(crbug.com/656607): Add proper annotation.
     net_result = socket_->Write(
         write_buffer_.get(), write_buffer_->BytesRemaining(),
         base::Bind(&PepperTCPSocketMessageFilter::OnWriteCompleted,
                    base::Unretained(this), context),
-        NO_TRAFFIC_ANNOTATION_BUG_656607);
+        traffic_annotation);
   } else if (ssl_socket_) {
     DCHECK_EQ(state_.state(), TCPSocketState::SSL_CONNECTED);
     net_result = ssl_socket_->Write(
-        write_buffer_.get(),
-        write_buffer_->BytesRemaining(),
+        write_buffer_.get(), write_buffer_->BytesRemaining(),
         base::Bind(&PepperTCPSocketMessageFilter::OnWriteCompleted,
-                   base::Unretained(this),
-                   context));
+                   base::Unretained(this), context),
+        traffic_annotation);
   }
   if (net_result != net::ERR_IO_PENDING)
     OnWriteCompleted(context, net_result);
