e7b9ba13f81c907c62a2aa915d329f9ac9b67518
Jennifer Apacible <apacible@chromium.org>
2017-12-21 00:33:39 +0000

    [Picture in Picture] Update window sizing and positioning.
    This change gives the OverlayWindow a fixed size. It also updates the
    initial location of the window on the primary screen on the bottom
    right hand corner, with a consistent margin.
    
    Specifics like a minimum / maximum size or behavior when switching
    between screens will be forthcoming.
    
    Bug: 726621
    Change-Id: I5f96ce876adeb1efada9f59ba82bc10eaf74024d
    Reviewed-on: https://chromium-review.googlesource.com/833336
    Commit-Queue: apacible <apacible@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525533}
    Former-commit-id: ab040fb0315f0c035d7e902805f8949bf8d1f4b5

--- chrome/browser/ui/views/overlay/overlay_window_views.cc
+++ chrome/browser/ui/views/overlay/overlay_window_views.cc
@@ -6,6 +6,8 @@
 
 #include "chrome/grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/display/manager/display_manager.h"
+#include "ui/display/screen.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/widget/widget_delegate.h"
 
@@ -24,7 +26,7 @@ class OverlayWindowWidgetDelegate : public views::WidgetDelegate {
   ~OverlayWindowWidgetDelegate() override = default;
 
   // WidgetDelegate:
-  bool CanResize() const override { return true; }
+  bool CanResize() const override { return false; }
   ui::ModalType GetModalType() const override { return ui::MODAL_TYPE_SYSTEM; }
   base::string16 GetWindowTitle() const override {
     return l10n_util::GetStringUTF16(IDS_PICTURE_IN_PICTURE_TITLE_TEXT);
@@ -46,13 +48,28 @@ OverlayWindowViews::OverlayWindowViews() {
 OverlayWindowViews::~OverlayWindowViews() = default;
 
 void OverlayWindowViews::Init() {
-  // TODO(apacible): Finalize the type of widget. http://crbug/726621
-  views::Widget::InitParams params(views::Widget::InitParams::TYPE_WINDOW);
+  views::Widget::InitParams params(
+      views::Widget::InitParams::TYPE_WINDOW_FRAMELESS);
   params.ownership = views::Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
 
-  // TODO(apacible): Update preferred sizing and positioning.
+  // These bounds are arbitrary. See OverlayWindowWidget for specified
+  // constraints. The initial positioning is on the bottom right quadrant
+  // of the primary display work area.
+  // The size is a temporary placeholder while video size is currently unused.
+  // This should also use the display of the initiating WebContents.
   // http://crbug/726621
-  params.bounds = gfx::Rect(200, 200, 700, 500);
+  gfx::Size size(500, 300);
+  gfx::Rect work_area =
+      display::Screen::GetScreen()->GetPrimaryDisplay().work_area();
+  int window_diff_width = work_area.width() - size.width();
+  int window_diff_height = work_area.height() - size.height();
+  // Keep a margin distance of 2% the average of the two window size
+  // differences, keeping the margins consistent.
+  int buffer = (window_diff_width + window_diff_height) / 2 * 0.02;
+  params.bounds = gfx::Rect(
+      gfx::Point(window_diff_width - buffer, window_diff_height - buffer),
+      size);
+
   params.keep_on_top = true;
   params.visible_on_all_workspaces = true;
 
