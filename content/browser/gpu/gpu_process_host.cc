5eb1eed5f47f810408cb2cb3fc13cd3015cec09a
Zhenyao Mo <zmo@chromium.org>
2017-12-21 03:44:52 +0000

    Add some DLOG to tell why GpuProcessHost::Get() returns nullptr
    BUG=796437
    TEST=bots (logs are not polluted by this)
    R=kbr@chromium.org
    
    Change-Id: I9a87b15801f236837d9d89e8a59deab473b23ae3
    Reviewed-on: https://chromium-review.googlesource.com/837598
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525587}
    Former-commit-id: be456d029607d8460a955e0f4af93db838d766e5

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -380,8 +380,10 @@ GpuProcessHost* GpuProcessHost::Get(GpuProcessKind kind, bool force_create) {
   // Don't grant further access to GPU if it is not allowed.
   GpuDataManagerImpl* gpu_data_manager = GpuDataManagerImpl::GetInstance();
   DCHECK(gpu_data_manager);
-  if (!gpu_data_manager->GpuAccessAllowed(nullptr))
+  if (!gpu_data_manager->GpuAccessAllowed(nullptr)) {
+    DLOG(ERROR) << "!GpuDataManagerImpl::GpuAccessAllowed()";
     return nullptr;
+  }
 
   if (g_gpu_process_hosts[kind] && ValidateHost(g_gpu_process_hosts[kind]))
     return g_gpu_process_hosts[kind];
@@ -390,8 +392,10 @@ GpuProcessHost* GpuProcessHost::Get(GpuProcessKind kind, bool force_create) {
     return nullptr;
 
   // Do not create a new process if browser is shutting down.
-  if (BrowserMainRunner::ExitedMainMessageLoop())
+  if (BrowserMainRunner::ExitedMainMessageLoop()) {
+    DLOG(ERROR) << "BrowserMainRunner::ExitedMainMessageLoop()";
     return nullptr;
+  }
 
   static int last_host_id = 0;
   int host_id;
@@ -406,6 +410,7 @@ GpuProcessHost* GpuProcessHost::Get(GpuProcessKind kind, bool force_create) {
   host->RecordProcessCrash();
 
   delete host;
+  DLOG(ERROR) << "GpuProcessHost::Init() failed";
   return nullptr;
 }
 
