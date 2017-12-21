a12a7e097b278536ec4ebeb7d70ddb28312212cb
Francois Doray <fdoray@chromium.org>
2017-12-21 11:10:17 -0500

    Remove WebContents::GetLastHiddenTime().
    This method is not used.
    
    TBR=chrisha@chromium.org
    
    Bug: 
    Change-Id: I2967cbc796b336d1badbbdbfbf093b175bdaf64a
    Reviewed-on: https://chromium-review.googlesource.com/834290
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526177}
    Former-commit-id: 9473b91ba45d89b630f9c56d768eb344922452de

--- content/browser/web_contents/web_contents_impl.h
+++ content/browser/web_contents/web_contents_impl.h
@@ -356,7 +356,6 @@ class CONTENT_EXPORT WebContentsImpl : public WebContents,
   void OnAudioStateChanged(bool is_audible) override;
   base::TimeTicks GetLastActiveTime() const override;
   void SetLastActiveTime(base::TimeTicks last_active_time) override;
-  base::TimeTicks GetLastHiddenTime() const override;
   void WasShown() override;
   void WasHidden() override;
   bool IsVisible() const override;
@@ -1495,10 +1494,6 @@ class CONTENT_EXPORT WebContentsImpl : public WebContents,
   // The time that this WebContents was last made active. The initial value is
   // the WebContents creation time.
   base::TimeTicks last_active_time_;
-
-  // The time that this WebContents was last made hidden. The initial value is
-  // zero.
-  base::TimeTicks last_hidden_time_;
 
   // See description above setter.
   bool closed_by_user_gesture_;
