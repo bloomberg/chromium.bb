389f114049f0968ea80ef85b4579acf225d896b6
Sylvain Defresne <sdefresne@chromium.org>
2017-12-21 18:19:09 +0100

    Remove circular dependencies between gn targets.
    Break the circular dependency between //ios/chrome/browser/tabs
    and //ios/chrome/browser/snapshots by using TabIdTabHelper to
    get the WebState unique identifier instead of using Tab.
    
    Extract tab_id_tab_helper.{h,mm} to a separate target
    //ios/chrome/browser/web:tab_id_tab_helper.
    
    Bug: none
    Change-Id: Ib8c194a8e80a58674e203aa7b48afb3a245709ef
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Reviewed-on: https://chromium-review.googlesource.com/839767
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525969}
    Former-commit-id: ba8520bc40a7c2e71d28770ba039ec892f8bcb51

--- ios/chrome/browser/snapshots/snapshot_cache_web_state_list_observer.mm
+++ ios/chrome/browser/snapshots/snapshot_cache_web_state_list_observer.mm
@@ -6,8 +6,7 @@
 
 #include "base/logging.h"
 #import "ios/chrome/browser/snapshots/snapshot_cache.h"
-#import "ios/chrome/browser/tabs/legacy_tab_helper.h"
-#import "ios/chrome/browser/tabs/tab.h"
+#import "ios/chrome/browser/web/tab_id_tab_helper.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -35,12 +34,12 @@ void SnapshotCacheWebStateListObserver::WebStateActivatedAt(
   NSMutableSet<NSString*>* set = [NSMutableSet set];
   if (active_index > 0) {
     web::WebState* web_state = web_state_list->GetWebStateAt(active_index - 1);
-    [set addObject:LegacyTabHelper::GetTabForWebState(web_state).tabId];
+    [set addObject:TabIdTabHelper::FromWebState(web_state)->tab_id()];
   }
 
   if (active_index + 1 < web_state_list->count()) {
     web::WebState* web_state = web_state_list->GetWebStateAt(active_index + 1);
-    [set addObject:LegacyTabHelper::GetTabForWebState(web_state).tabId];
+    [set addObject:TabIdTabHelper::FromWebState(web_state)->tab_id()];
   }
 
   snapshot_cache_.pinnedIDs = [set copy];
