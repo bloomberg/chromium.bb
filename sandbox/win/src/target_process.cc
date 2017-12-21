537253b1480fed53722cf02c1eb9d52bc66b031a
James Forshaw <forshaw@chromium.org>
2017-12-21 21:43:57 +0000

    Reland "Added AppContainerProfile support to policy."
    This is a reland of 5bce679c4920a54b78587fcd35521c32aaa70cd3 [formerly b8c94b96e65efba286cf232b7610369ae3818baa]
    Original change's description:
    > Added AppContainerProfile support to policy.
    > 
    > This CL addeds the AppContainerProfile support to the sandbox policy code
    > so that new AppContainer processes can be created.
    > 
    > Bug: 760977
    > Change-Id: Ida8028ba6146df71defe0a4072ecaea29c04f246
    > Reviewed-on: https://chromium-review.googlesource.com/804195
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Commit-Queue: James Forshaw <forshaw@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#525657}
    
    Bug: 760977
    Change-Id: I53fe426d76c24997b762135b88f6b5c84e32e9b8
    Reviewed-on: https://chromium-review.googlesource.com/841082
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: James Forshaw <forshaw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525961}
    Former-commit-id: 0c42dd04b3c81f44f1894a6c4da18fb50e8d5c7f

--- sandbox/win/src/target_process.cc
+++ sandbox/win/src/target_process.cc
@@ -9,17 +9,24 @@
 
 #include <memory>
 #include <utility>
+#include <vector>
 
 #include "base/macros.h"
 #include "base/memory/free_deleter.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/win/startup_information.h"
 #include "base/win/windows_version.h"
 #include "sandbox/win/src/crosscall_client.h"
 #include "sandbox/win/src/crosscall_server.h"
 #include "sandbox/win/src/policy_low_level.h"
+#include "sandbox/win/src/restricted_token_utils.h"
 #include "sandbox/win/src/sandbox_types.h"
+#include "sandbox/win/src/security_capabilities.h"
 #include "sandbox/win/src/sharedmem_ipc_server.h"
+#include "sandbox/win/src/sid.h"
 #include "sandbox/win/src/win_utils.h"
+
+namespace sandbox {
 
 namespace {
 
@@ -41,9 +48,53 @@ void CopyPolicyToTarget(const void* source, size_t size, void* dest) {
   }
 }
 
-}  // namespace
+bool GetTokenAppContainerSid(HANDLE token_handle,
+                             std::unique_ptr<Sid>* app_container_sid) {
+  std::vector<char> app_container_info(sizeof(TOKEN_APPCONTAINER_INFORMATION) +
+                                       SECURITY_MAX_SID_SIZE);
+  DWORD return_length;
 
-namespace sandbox {
+  if (!::GetTokenInformation(
+          token_handle, TokenAppContainerSid, app_container_info.data(),
+          base::checked_cast<DWORD>(app_container_info.size()),
+          &return_length)) {
+    return false;
+  }
+
+  PTOKEN_APPCONTAINER_INFORMATION info =
+      reinterpret_cast<PTOKEN_APPCONTAINER_INFORMATION>(
+          app_container_info.data());
+  if (!info->TokenAppContainer)
+    return false;
+  *app_container_sid = std::unique_ptr<Sid>(new Sid(info->TokenAppContainer));
+  return true;
+}
+
+bool GetProcessAppContainerSid(HANDLE process,
+                               std::unique_ptr<Sid>* app_container_sid) {
+  HANDLE token_handle;
+  if (!::OpenProcessToken(process, TOKEN_QUERY, &token_handle))
+    return false;
+  base::win::ScopedHandle process_token(token_handle);
+
+  return GetTokenAppContainerSid(process_token.Get(), app_container_sid);
+}
+
+bool GetAppContainerImpersonationToken(
+    HANDLE process,
+    HANDLE initial_token,
+    const std::vector<Sid>& capabilities,
+    base::win::ScopedHandle* impersonation_token) {
+  std::unique_ptr<Sid> app_container_sid;
+  if (!GetProcessAppContainerSid(process, &app_container_sid)) {
+    return false;
+  }
+  SecurityCapabilities security_caps(*app_container_sid, capabilities);
+  return CreateLowBoxToken(initial_token, IMPERSONATION, &security_caps,
+                           nullptr, 0, impersonation_token) == ERROR_SUCCESS;
+}
+
+}  // namespace
 
 SANDBOX_INTERCEPT HANDLE g_shared_section;
 SANDBOX_INTERCEPT size_t g_shared_IPC_size;
@@ -52,7 +103,8 @@ SANDBOX_INTERCEPT size_t g_shared_policy_size;
 TargetProcess::TargetProcess(base::win::ScopedHandle initial_token,
                              base::win::ScopedHandle lockdown_token,
                              HANDLE job,
-                             ThreadProvider* thread_pool)
+                             ThreadProvider* thread_pool,
+                             const std::vector<Sid>& impersonation_capabilities)
     // This object owns everything initialized here except thread_pool and
     // the job_ handle. The Job handle is closed by BrokerServices and results
     // eventually in a call to our dtor.
@@ -60,7 +112,8 @@ TargetProcess::TargetProcess(base::win::ScopedHandle initial_token,
       initial_token_(std::move(initial_token)),
       job_(job),
       thread_pool_(thread_pool),
-      base_address_(nullptr) {}
+      base_address_(nullptr),
+      impersonation_capabilities_(impersonation_capabilities) {}
 
 TargetProcess::~TargetProcess() {
   // Give a chance to the process to die. In most cases the JOB_KILL_ON_CLOSE
@@ -132,14 +185,20 @@ ResultCode TargetProcess::Create(
   }
 
   if (initial_token_.IsValid()) {
+    HANDLE impersonation_token = initial_token_.Get();
+    base::win::ScopedHandle app_container_token;
+    if (GetAppContainerImpersonationToken(
+            process_info.process_handle(), impersonation_token,
+            impersonation_capabilities_, &app_container_token)) {
+      impersonation_token = app_container_token.Get();
+    }
+
     // Change the token of the main thread of the new process for the
     // impersonation token with more rights. This allows the target to start;
     // otherwise it will crash too early for us to help.
     HANDLE temp_thread = process_info.thread_handle();
-    if (!::SetThreadToken(&temp_thread, initial_token_.Get())) {
+    if (!::SetThreadToken(&temp_thread, impersonation_token)) {
       *win_error = ::GetLastError();
-      // It might be a security breach if we let the target run outside the job
-      // so kill it before it causes damage.
       ::TerminateProcess(process_info.process_handle(), 0);
       return SBOX_ERROR_SET_THREAD_TOKEN;
     }
@@ -311,8 +370,9 @@ ResultCode TargetProcess::AssignLowBoxToken(
 }
 
 TargetProcess* MakeTestTargetProcess(HANDLE process, HMODULE base_address) {
-  TargetProcess* target = new TargetProcess(
-      base::win::ScopedHandle(), base::win::ScopedHandle(), nullptr, nullptr);
+  TargetProcess* target =
+      new TargetProcess(base::win::ScopedHandle(), base::win::ScopedHandle(),
+                        nullptr, nullptr, std::vector<Sid>());
   PROCESS_INFORMATION process_info = {};
   process_info.hProcess = process;
   target->sandbox_process_info_.Set(process_info);
