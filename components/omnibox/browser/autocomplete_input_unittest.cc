e9eb4d4eaefaf0542dfcb28e1a61e175e76f75fa
Dave Schuyler <dschuyler@chromium.org>
2017-12-21 15:40:49 -0800

    [omnibox] treat localhost, test, example, and invalid as top level domains.
    This CL changes a omnibox input such as foo.test to be a URL rather than
    a QUERY. The input of localhost was already being treated as a URL, but
    now foo.localhost will also be treated as a URL. The invalid TLD will be
    treated as an invalid URL.
    
    Bug: 782146
    Change-Id: I5676ff1f36fa860f6c6f654bcfa475be06a6d875
    Reviewed-on: https://chromium-review.googlesource.com/831214
    Commit-Queue: Dave Schuyler <dschuyler@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#525881}
    Former-commit-id: 9aa2101f42e1727355c43e24310dbc80c0033896

--- components/omnibox/browser/autocomplete_input_unittest.cc
+++ components/omnibox/browser/autocomplete_input_unittest.cc
@@ -174,6 +174,20 @@ TEST(AutocompleteInputTest, InputType) {
     {ASCIIToUTF16("://w"), metrics::OmniboxInputType::UNKNOWN},
     {ASCIIToUTF16(":w"), metrics::OmniboxInputType::UNKNOWN},
     {base::WideToUTF16(L".\u062A"), metrics::OmniboxInputType::UNKNOWN},
+    // These tests are for https://tools.ietf.org/html/rfc6761.
+    {ASCIIToUTF16("foo.example"), metrics::OmniboxInputType::URL},
+    {ASCIIToUTF16("foo example"), metrics::OmniboxInputType::QUERY},
+    {ASCIIToUTF16("example"), metrics::OmniboxInputType::UNKNOWN},
+    {ASCIIToUTF16("http://foo.invalid"), metrics::OmniboxInputType::UNKNOWN},
+    {ASCIIToUTF16("foo.invalid/"), metrics::OmniboxInputType::QUERY},
+    {ASCIIToUTF16("foo.invalid"), metrics::OmniboxInputType::QUERY},
+    {ASCIIToUTF16("foo invalid"), metrics::OmniboxInputType::QUERY},
+    {ASCIIToUTF16("invalid"), metrics::OmniboxInputType::UNKNOWN},
+    {ASCIIToUTF16("foo.localhost"), metrics::OmniboxInputType::URL},
+    {ASCIIToUTF16("foo localhost"), metrics::OmniboxInputType::QUERY},
+    {ASCIIToUTF16("foo.test"), metrics::OmniboxInputType::URL},
+    {ASCIIToUTF16("foo test"), metrics::OmniboxInputType::QUERY},
+    {ASCIIToUTF16("test"), metrics::OmniboxInputType::UNKNOWN},
   };
 
   for (size_t i = 0; i < arraysize(input_cases); ++i) {
