22fba1ff5c5c1ff0a84c318cafdfbfe8ccd245dc
Urvang Joshi <urvang@google.com>
2017-12-22 15:51:36 -0800

    av1_fwd_txfm1d_test: Fix when TX64X64 is off.
    Change-Id: I4658238c32ed419736cd5db2dd4fc26e9d8e50c5

--- test/av1_fwd_txfm1d_test.cc
+++ test/av1_fwd_txfm1d_test.cc
@@ -23,8 +23,17 @@ namespace {
 const int txfm_type_num = 2;
 const TYPE_TXFM txfm_type_ls[2] = { TYPE_DCT, TYPE_ADST };
 
+#if CONFIG_TX64X64
 const int txfm_size_num = 5;
-const int txfm_size_ls[5] = { 4, 8, 16, 32, 64 };
+#else
+const int txfm_size_num = 4;
+#endif  // CONFIG_TX64X64
+
+const int txfm_size_ls[] = { 4, 8, 16, 32,
+#if CONFIG_TX64X64
+                             64
+#endif  // CONFIG_TX64X64
+};
 
 const TxfmFunc fwd_txfm_func_ls[][2] = {
   { av1_fdct4_new, av1_fadst4_new },
