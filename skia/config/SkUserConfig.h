b8d448cf6fa04c915ed9c15e75f491305cf663e9
Brian Osman <brianosman@google.com>
2017-12-22 09:01:07 -0500

    Switch to new (higher precision) definition of Adobe RGB in Skia
    Bug: 
    Change-Id: I2d56d62f3635b66c7c4e3dc8882947dbdf0e8f87
    Reviewed-on: https://chromium-review.googlesource.com/836748
    Reviewed-by: Mike Klein <mtklein@chromium.org>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Cr-Commit-Position: refs/heads/master@{#526006}
    Former-commit-id: b2eac3263e143f0e9df94f55679b744d5f490b94

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -228,10 +228,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_TILED_BITMAPS
 #endif
 
-#ifndef SK_SUPPORT_LEGACY_ADOBE_XYZ
-#define SK_SUPPORT_LEGACY_ADOBE_XYZ
-#endif
-
 // remove after rebaselining svg layout tests
 #ifndef SK_SUPPORT_LEGACY_SVG_ARC_TO
 #define SK_SUPPORT_LEGACY_SVG_ARC_TO
