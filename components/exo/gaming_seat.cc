2d1ecb0f062af1607f8b33af1b7e99b2a9b7e569
Eric Willigers <ericwilligers@chromium.org>
2017-12-23 08:41:27 +1100

    [jumbo] GamepadEvent constructor is now inline
    The .cc file was referenced from two BUILD.gn files.
    
    With the constructor inline, the .cc file is not needed, and
    a duplicate definition error in jumbo builds is avoided.
    
    BUG=773275
    
    Change-Id: Ib95fb3c79be6818ef2680ff809b74ba4067bc648
    Reviewed-on: https://chromium-review.googlesource.com/842524
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526116}
    Former-commit-id: 08f3551d8b86cd9fa308013a5234b0c6c403023a

--- components/exo/gaming_seat.cc
+++ components/exo/gaming_seat.cc
@@ -90,16 +90,16 @@ void GamingSeat::OnGamepadDevicesUpdated() {
 }
 
 void GamingSeat::OnGamepadEvent(const ui::GamepadEvent& event) {
-  auto it = gamepads_.find(event.device_id());
+  auto it = gamepads_.find(event.device_id);
   if (it == gamepads_.end())
     return;
 
-  switch (event.type()) {
+  switch (event.type) {
     case ui::GamepadEventType::BUTTON:
-      it->second->OnButton(event.code(), event.value(), event.value());
+      it->second->OnButton(event.code, event.value, event.value);
       break;
     case ui::GamepadEventType::AXIS:
-      it->second->OnAxis(event.code(), event.value());
+      it->second->OnAxis(event.code, event.value);
       break;
     case ui::GamepadEventType::FRAME:
       it->second->OnFrame();
