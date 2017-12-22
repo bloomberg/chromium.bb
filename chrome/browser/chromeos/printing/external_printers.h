afccf8e68a85619e48116f1295a0f01de4e738c9
Sean Kau <skau@chromium.org>
2017-12-22 13:05:32 -0800

    Parse JSON and compute off the UI thread for policy printers.
    Change ExternalPrinters to perform slow operations off the UI thread
    to avoid introducing jank.  Also add infrastructure to observe
    preferences and manage instances by user.
    
    Bug: 758680
    Change-Id: I56f4dae9f0c2062cfe177fcdac88785978874217
    Reviewed-on: https://chromium-review.googlesource.com/826705
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Xiaoqian Dai OOO until Jan 2nd <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526087}
    Former-commit-id: 2945b4acf4c0c95c0c84472dded8b5ad094a5acf

--- chrome/browser/chromeos/printing/external_printers.h
+++ chrome/browser/chromeos/printing/external_printers.h
@@ -5,30 +5,30 @@
 #ifndef CHROME_BROWSER_CHROMEOS_PRINTING_EXTERNAL_PRINTERS_H_
 #define CHROME_BROWSER_CHROMEOS_PRINTING_EXTERNAL_PRINTERS_H_
 
+#include <map>
 #include <memory>
 #include <string>
 #include <utility>
 #include <vector>
 
-#include "base/macros.h"
-#include "chrome/browser/chromeos/policy/cloud_external_data_policy_observer.h"
+#include "chromeos/chromeos_export.h"
 #include "chromeos/printing/printer_configuration.h"
 
 namespace chromeos {
 
 // Manages download and parsing of the external policy printer configuration and
 // enforces restrictions.
-class ExternalPrinters {
+class CHROMEOS_EXPORT ExternalPrinters {
  public:
   // Choose the policy for printer access.
   enum AccessMode {
-    UNSET,
+    UNSET = -1,
     // Printers in the blacklist are disallowed.  Others are allowed.
-    BLACKLIST_ONLY,
+    BLACKLIST_ONLY = 0,
     // Only printers in the whitelist are allowed.
-    WHITELIST_ONLY,
+    WHITELIST_ONLY = 1,
     // All printers in the policy are allowed.
-    ALL_ACCESS
+    ALL_ACCESS = 2
   };
 
   // Observer is notified when the computed set of printers change.  It is
@@ -36,8 +36,12 @@ class ExternalPrinters {
   // observing.
   class Observer {
    public:
-    // Called when the computed set of printers changes.
-    virtual void OnPrintersChanged() = 0;
+    // Called when the printers have changed and should be queried.  |valid| is
+    // true if |printers| is based on a valid policy.  |printers| are the
+    // printers that should be available to the user.
+    virtual void OnPrintersChanged(
+        bool valid,
+        const std::map<const std::string, const Printer>& printers) = 0;
   };
 
   // Creates a handler for the external printer policies.
@@ -45,24 +49,35 @@ class ExternalPrinters {
 
   virtual ~ExternalPrinters() = default;
 
+  virtual void AddObserver(Observer* observer) = 0;
+  virtual void RemoveObserver(Observer* observer) = 0;
+
   // Parses |data| which is the contents of the bulk printes file and extracts
   // printer information.  The file format is assumed to be JSON.
   virtual void SetData(std::unique_ptr<std::string> data) = 0;
   // Removes all printer data and invalidates the configuration.
   virtual void ClearData() = 0;
 
-  virtual void AddObserver(Observer* observer) = 0;
-  virtual void RemoveObserver(Observer* observer) = 0;
-
+  // Set the access mode which chooses the type of filtering that is performed.
   virtual void SetAccessMode(AccessMode mode) = 0;
+  // Set the |blacklist| which excludes printers with the given id if access
+  // mode is BLACKLIST_ONLY.
   virtual void SetBlacklist(const std::vector<std::string>& blacklist) = 0;
+  // Set the |whitelist| which is the complete list of printers that are show to
+  // the user.  This is in effect if access mode is WHITELIST_ONLY.
   virtual void SetWhitelist(const std::vector<std::string>& whitelist) = 0;
 
-  // Returns true if the printer configuration has been downloaded and parsed.
+  // Returns true if the printer map has been computed from a valid policy.
+  // Returns false otherwise.  This is computed asynchronously.  Observe
+  // OnPrintersChanged() to be notified when it is updated.  This may never
+  // become true if a user does not have the appropriate printer policies.
   virtual bool IsPolicySet() const = 0;
 
-  // Returns all the printers available from the policy.
-  virtual const std::vector<Printer>& GetPrinters() const = 0;
+  // Returns a refernce to a map of the computed set of printers.  The map is
+  // empty if either the policy was empty or we are yet to receive the full
+  // policy. Use IsPolicySet() to differentiate betweeen the two.
+  virtual const std::map<const std::string, const Printer>& GetPrinters()
+      const = 0;
 };
 
 }  // namespace chromeos
