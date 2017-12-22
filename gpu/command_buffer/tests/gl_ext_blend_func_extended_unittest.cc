c11e1c4c72a2ac9b33c1f35a8c483f1384a0f589
Yuly Novikov <ynovikov@chromium.org>
2017-12-22 18:20:12 -0500

    Suppress EXTBlendFuncExtendedDrawTest.ESSL1FragColor on Linux AMD
    Likely due to AMDGPU-PRO driver bug.
    
    BUG=786219
    TBR=kbr@chromium.org,zmo@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I920e71fb110ec0ec53e96fe3ec2efa6259e74871
    Reviewed-on: https://chromium-review.googlesource.com/843168
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526091}
    Former-commit-id: e08ee6def83ce5f5a7c15b74a70d73dfbf372149

--- gpu/command_buffer/tests/gl_ext_blend_func_extended_unittest.cc
+++ gpu/command_buffer/tests/gl_ext_blend_func_extended_unittest.cc
@@ -199,6 +199,14 @@ class EXTBlendFuncExtendedDrawTest : public testing::TestWithParam<bool> {
 TEST_P(EXTBlendFuncExtendedDrawTest, ESSL1FragColor) {
   if (!IsApplicable())
     return;
+
+  // Fails on AMDGPU-PRO driver crbug.com/786219
+  gpu::GPUTestBotConfig bot_config;
+  if (bot_config.LoadCurrentConfig(nullptr) &&
+      bot_config.Matches("linux amd")) {
+    return;
+  }
+
   // clang-format off
   static const char* kFragColorShader =
       BFE_SHADER(
