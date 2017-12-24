9e28171c26e3738607b5a3b4e1515f928534373a
Rob Wu <rob@robwu.nl>
2017-12-24 14:05:07 +0100

    Fix XSS in supervised user interstitial
    BUG=797525
    
    Change-Id: Ib5cfa732b0f4de8645031c0166e4d67633a65c93
    Reviewed-on: https://chromium-review.googlesource.com/844075
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Rob Wu <rob@robwu.nl>
    Cr-Commit-Position: refs/heads/master@{#526158}
    Former-commit-id: a296eff269063ef8daa7cdacf4483d3993214a1b

--- components/supervised_user_error_page/resources/supervised_user_block_interstitial.js
+++ components/supervised_user_error_page/resources/supervised_user_block_interstitial.js
@@ -66,8 +66,8 @@ function initialize() {
       $('custodian-avatar-img').style.content =
           makeImageSet(avatarURL1x, avatarURL2x);
     }
-    $('custodian-name').innerHTML = custodianName;
-    $('custodian-email').innerHTML = loadTimeData.getString('custodianEmail');
+    $('custodian-name').textContent = custodianName;
+    $('custodian-email').textContent = loadTimeData.getString('custodianEmail');
     var secondAvatarURL1x = loadTimeData.getString('secondAvatarURL1x');
     var secondAvatarURL2x = loadTimeData.getString('secondAvatarURL2x');
     var secondCustodianName = loadTimeData.getString('secondCustodianName');
@@ -78,8 +78,8 @@ function initialize() {
         $('second-custodian-avatar-img').style.content =
             makeImageSet(secondAvatarURL1x, secondAvatarURL2x);
       }
-      $('second-custodian-name').innerHTML = secondCustodianName;
-      $('second-custodian-email').innerHTML = loadTimeData.getString(
+      $('second-custodian-name').textContent = secondCustodianName;
+      $('second-custodian-email').textContent = loadTimeData.getString(
           'secondCustodianEmail');
     }
   }
