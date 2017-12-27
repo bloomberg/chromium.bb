3232df241711cb44c7583f6aa4a075423c5d2ba6
Jonathan Metzman <metzman@chromium.org>
2017-12-27 13:53:37 -0800

    [libFuzzer] Ignore libxml's logging in xml_parser_fuzzer.
    Bug: 797811
    Change-Id: I85d682df34a5d5636b1162907efbe91ac94a00dd
    Reviewed-on: https://chromium-review.googlesource.com/845117
    Reviewed-by: Jay Civelli <jcivelli@chromium.org>
    Commit-Queue: Jonathan Metzman <metzman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526250}
    Former-commit-id: 5a31a0be2318b896d7d0a397b9f00dc4d1a8ddec

--- services/data_decoder/xml_parser_fuzzer.cc
+++ services/data_decoder/xml_parser_fuzzer.cc
@@ -11,6 +11,7 @@
 #include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "services/data_decoder/xml_parser.h"
+#include "third_party/libxml/chromium/libxml_utils.h"
 
 namespace {
 
@@ -20,7 +21,12 @@ void OnParseXml(base::Closure quit_loop,
   std::move(quit_loop).Run();
 }
 
+// Error handler to ignore spamy messages from libxml.
+void ignore(void* ctx, const char* msg, ...) {}
+
 }  // namespace
+
+static ScopedXmlErrorFunc scoped_xml_error_func(nullptr, &ignore);
 
 // Entry point for LibFuzzer.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
