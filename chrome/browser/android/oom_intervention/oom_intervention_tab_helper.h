c820b41fb5f0203baa87d9dffd7039df48905cf0
Kenichi Ishibashi <bashi@chromium.org>
2017-12-28 12:58:31 +0900

    oom_intervention: Check renderer memory usage to detect near-OOM
    Before this CL we use ComponentCallback2.onLowMemory to detect near-OOM
    situations. However, according to early results of field trials it seems
    that success estimation rate is lower than we expected. This CL adds
    heuristics to improve success estimation rate. When the browser gets
    a signal from the OS via onLowMemory(), we will start monitoring memory
    usage in renderer side. If memory workload in renderer process
    exceeds a threshold, the renderer reports that a near-OOM is detected.
    
    Bug: 776665
    Change-Id: I411e5a0b2a0eac0468133a6b843b1c613998de32
    Reviewed-on: https://chromium-review.googlesource.com/838584
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526278}
    Former-commit-id: d5f4c8889695bad4c821ff934428288fa551face

--- chrome/browser/android/oom_intervention/oom_intervention_tab_helper.h
+++ chrome/browser/android/oom_intervention/oom_intervention_tab_helper.h
@@ -5,6 +5,7 @@
 #ifndef CHROME_BROWSER_ANDROID_OOM_INTERVENTION_OOM_INTERVENTION_TAB_HELPER_H_
 #define CHROME_BROWSER_ANDROID_OOM_INTERVENTION_OOM_INTERVENTION_TAB_HELPER_H_
 
+#include "base/memory/weak_ptr.h"
 #include "base/optional.h"
 #include "base/time/time.h"
 #include "chrome/browser/android/oom_intervention/near_oom_monitor.h"
@@ -12,6 +13,7 @@
 #include "chrome/browser/ui/interventions/intervention_delegate.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
+#include "mojo/public/cpp/bindings/binding.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
 #include "third_party/WebKit/public/platform/oom_intervention.mojom.h"
 
@@ -28,11 +30,15 @@ class OomInterventionTabHelper
     : public content::WebContentsObserver,
       public content::WebContentsUserData<OomInterventionTabHelper>,
       public OutOfMemoryReporter::Observer,
+      public blink::mojom::OomInterventionHost,
       public InterventionDelegate {
  public:
   static bool IsEnabled();
 
   ~OomInterventionTabHelper() override;
+
+  // blink::mojom::OomInterventionHost:
+  void OnHighMemoryUsage(bool intervention_triggered) override;
 
   // InterventionDelegate:
   void AcceptIntervention() override;
@@ -64,11 +70,16 @@ class OomInterventionTabHelper
   // Called when NearOomMonitor detects near-OOM situation.
   void OnNearOomDetected();
 
+  // Called when we stop monitoring high memory usage in the foreground
+  // renderer.
+  void OnDetectionWindowElapsedWithoutHighMemoryUsage();
+
   void ResetInterventionState();
 
   bool navigation_started_ = false;
   base::Optional<base::TimeTicks> near_oom_detected_time_;
   std::unique_ptr<NearOomMonitor::Subscription> subscription_;
+  base::OneShotTimer renderer_detection_timer_;
 
   // Not owned. This will be nullptr in incognito mode.
   OomInterventionDecider* decider_;
@@ -87,6 +98,10 @@ class OomInterventionTabHelper
   };
 
   InterventionState intervention_state_ = InterventionState::NOT_TRIGGERED;
+
+  mojo::Binding<blink::mojom::OomInterventionHost> binding_;
+
+  base::WeakPtrFactory<OomInterventionTabHelper> weak_ptr_factory_;
 };
 
 #endif  // CHROME_BROWSER_ANDROID_OOM_INTERVENTION_OOM_INTERVENTION_TAB_HELPER_H_
--- third_party/WebKit/Source/controller/OomInterventionImplTest.cpp
+++ third_party/WebKit/Source/controller/OomInterventionImplTest.cpp
@@ -10,13 +10,35 @@
 #include "core/frame/WebLocalFrameImpl.h"
 #include "core/page/Page.h"
 #include "core/testing/DummyPageHolder.h"
+#include "mojo/public/cpp/bindings/binding.h"
+#include "platform/testing/UnitTestHelpers.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace blink {
 
+namespace {
+
+class MockOomInterventionHost : public mojom::blink::OomInterventionHost {
+ public:
+  MockOomInterventionHost(mojom::blink::OomInterventionHostRequest request)
+      : binding_(this, std::move(request)) {}
+  ~MockOomInterventionHost() = default;
+
+  void OnHighMemoryUsage(bool intervention_triggered) override {}
+
+ private:
+  mojo::Binding<mojom::blink::OomInterventionHost> binding_;
+};
+
+}  // namespace
+
 class OomInterventionImplTest : public ::testing::Test {
+ public:
+  size_t MockMemoryWorkloadCalculator() { return memory_workload_; }
+
  protected:
+  size_t memory_workload_ = 0;
   FrameTestHelpers::WebViewHelper web_view_helper_;
 };
 
@@ -25,7 +47,17 @@ TEST_F(OomInterventionImplTest, DetectedAndDeclined) {
   Page* page = web_view->MainFrameImpl()->GetFrame()->GetPage();
   EXPECT_FALSE(page->Paused());
 
-  auto intervention = std::make_unique<OomInterventionImpl>();
+  auto intervention = std::make_unique<OomInterventionImpl>(
+      WTF::BindRepeating(&OomInterventionImplTest::MockMemoryWorkloadCalculator,
+                         WTF::Unretained(this)));
+  EXPECT_FALSE(page->Paused());
+
+  memory_workload_ = OomInterventionImpl::kMemoryWorkloadThreshold + 1;
+  mojom::blink::OomInterventionHostPtr host_ptr;
+  MockOomInterventionHost mock_host(mojo::MakeRequest(&host_ptr));
+  intervention->StartDetection(std::move(host_ptr),
+                               true /*trigger_intervention*/);
+  testing::RunDelayedTasks(TimeDelta::FromSeconds(1));
   EXPECT_TRUE(page->Paused());
 
   intervention.reset();
