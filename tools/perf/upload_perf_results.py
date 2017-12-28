d7744ca4e9487f98ec5892f49bbe6eef549baaac
John Budorick <jbudorick@chromium.org>
2017-12-28 08:29:00 -0800

    Add --task-output-dir to existing merge scripts.
    Bug: 793359
    Change-Id: I874c3ed12d9de938909d89a394ca6929f5d1f494
    Reviewed-on: https://chromium-review.googlesource.com/845780
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Reviewed-by: Emily Hanley <eyaich@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526817}
    Former-commit-id: e6b01c7585ad85e5919a33d52f4d16e352e0b97e

--- tools/perf/upload_perf_results.py
+++ tools/perf/upload_perf_results.py
@@ -45,6 +45,7 @@ def main():
   parser.add_argument('-o', '--output-json', required=True)
   parser.add_argument('--build-properties', help=argparse.SUPPRESS)
   parser.add_argument('--summary-json', help=argparse.SUPPRESS)
+  parser.add_argument('--task-output-dir', help=argparse.SUPPRESS)
   parser.add_argument('--verbose', help=argparse.SUPPRESS)
   parser.add_argument('jsons_to_merge', nargs='*')
 
