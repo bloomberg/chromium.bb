41b69d7c69f71ec66ecea6e45b4547431cd07b44
Darren Shen <shend@chromium.org>
2017-12-28 16:13:03 +1100

    Remove unnecessary const_cast from CSSSelector::TagHistory().
    Not really used since we return a const CSSSelector* anyway.
    
    Change-Id: Ib28ce194c43bb43764080d17900384a759190a4c
    Reviewed-on: https://chromium-review.googlesource.com/845258
    Commit-Queue: Darren Shen <shend@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526282}
    Former-commit-id: b6f498da7d08c8f3273b2685f761feaf70882073

--- third_party/WebKit/Source/core/css/CSSSelector.h
+++ third_party/WebKit/Source/core/css/CSSSelector.h
@@ -260,8 +260,7 @@ class CORE_EXPORT CSSSelector {
   // Selectors are kept in an array by CSSSelectorList. The next component of
   // the selector is the next item in the array.
   const CSSSelector* TagHistory() const {
-    return is_last_in_tag_history_ ? nullptr
-                                   : const_cast<CSSSelector*>(this + 1);
+    return is_last_in_tag_history_ ? nullptr : this + 1;
   }
 
   static const AtomicString& UniversalSelectorAtom() { return g_null_atom; }
