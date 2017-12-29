4c4a75219355a94a8316b4b724ea95f6044c65de
Scott Chen <scottchen@chromium.org>
2017-12-28 16:54:05 -0800

    [signin] fix iron-icon path in sync confirmation dialog.
    Bug: 796536
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Icdd1b45f3c87ef18a7ece9f361788c2011746f35
    Reviewed-on: https://chromium-review.googlesource.com/837590
    Commit-Queue: Scott Chen <scottchen@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526353}
    Former-commit-id: f7e1e91b04ccaccb1c73c05cbb2c290bc3926ce5

---
+++ chrome/test/data/webui/signin/sync_confirmation_test.js
@@ -0,0 +1,20 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+cr.define('signin_sync_confirmation', function() {
+
+  suite('SigninSyncConfirmationTest', function() {
+    let app;
+    setup(function() {
+      PolymerTest.clearBody();
+      app = document.createElement('sync-confirmation-app');
+      document.body.append(app);
+    });
+
+    // Tests that no DCHECKS are thrown during initialization of the UI.
+    test('LoadPage', function() {
+      assertTrue(!!app.$.heading);
+    });
+  });
+});
