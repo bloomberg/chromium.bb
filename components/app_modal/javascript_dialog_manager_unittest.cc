4ab87298b0dedad9bffff3bddd3d83a412161d18
Peter Boström <pbos@chromium.org>
2018-01-02 15:46:34 -0800

    Update JavascriptAppModalDialog titles.
    On titlecase systems the "www.example.com Says" heading is awkward, so
    to end with the URL these are being replaced by "From www.example.com"
    and corresponding "From an Example Page at www.example.com".
    
    Bug: chromium:683346
    Change-Id: Ie520328276259f787541a02e39ed725687c1dff6
    Reviewed-on: https://chromium-review.googlesource.com/846249
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526642}
    Former-commit-id: 677891d2365eda05ec79bf5bc25f05f6187ec5a4

--- components/app_modal/javascript_dialog_manager_unittest.cc
+++ components/app_modal/javascript_dialog_manager_unittest.cc
@@ -21,83 +21,83 @@ TEST(JavaScriptDialogManagerTest, GetTitle) {
     const char* expected_android;
   } cases[] = {
       // Standard main frame alert.
-      {"http://foo.com/", "http://foo.com/", "foo.com Says", "foo.com says",
-       "foo.com says"},
+      {"http://foo.com/", "http://foo.com/", "From foo.com", "From foo.com",
+       "From foo.com"},
 
       // Subframe alert from the same origin.
-      {"http://foo.com/1", "http://foo.com/2", "foo.com Says", "foo.com says",
-       "foo.com says"},
+      {"http://foo.com/1", "http://foo.com/2", "From foo.com", "From foo.com",
+       "From foo.com"},
       // Subframe alert from a different origin.
-      {"http://foo.com/", "http://bar.com/", "An Embedded Page at bar.com Says",
-       "An embedded page at bar.com says", "An embedded page at bar.com says"},
+      {"http://foo.com/", "http://bar.com/", "From an Embedded Page at bar.com",
+       "From an embedded page at bar.com", "From an embedded page at bar.com"},
 
       // file:
       // - main frame:
       {"file:///path/to/page.html", "file:///path/to/page.html",
-       "This Page Says", "This page says", "This page says"},
+       "From This Page", "From this page", "From this page"},
       // - subframe:
       {"http://foo.com/", "file:///path/to/page.html",
-       "An Embedded Page on This Page Says",
-       "An embedded page on this page says",
-       "An embedded page on this page says"},
+       "From an Embedded Page on This Page",
+       "From an embedded page on this page",
+       "From an embedded page on this page"},
 
       // ftp:
       // - main frame:
       {"ftp://foo.com/path/to/page.html", "ftp://foo.com/path/to/page.html",
-       "foo.com Says", "foo.com says", "ftp://foo.com says"},
+       "From foo.com", "From foo.com", "From ftp://foo.com"},
       // - subframe:
       {"http://foo.com/", "ftp://foo.com/path/to/page.html",
-       "An Embedded Page at foo.com Says", "An embedded page at foo.com says",
-       "An embedded page at ftp://foo.com says"},
+       "From an Embedded Page at foo.com", "From an embedded page at foo.com",
+       "From an embedded page at ftp://foo.com"},
 
       // data:
       // - main frame:
-      {"data:blahblah", "data:blahblah", "This Page Says", "This page says",
-       "This page says"},
+      {"data:blahblah", "data:blahblah", "From This Page", "From this page",
+       "From this page"},
       // - subframe:
-      {"http://foo.com/", "data:blahblah", "An Embedded Page on This Page Says",
-       "An embedded page on this page says",
-       "An embedded page on this page says"},
+      {"http://foo.com/", "data:blahblah", "From an Embedded Page on This Page",
+       "From an embedded page on this page",
+       "From an embedded page on this page"},
 
       // javascript:
       // - main frame:
-      {"javascript:abc", "javascript:abc", "This Page Says", "This page says",
-       "This page says"},
+      {"javascript:abc", "javascript:abc", "From This Page", "From this page",
+       "From this page"},
       // - subframe:
       {"http://foo.com/", "javascript:abc",
-       "An Embedded Page on This Page Says",
-       "An embedded page on this page says",
-       "An embedded page on this page says"},
+       "From an Embedded Page on This Page",
+       "From an embedded page on this page",
+       "From an embedded page on this page"},
 
       // about:
       // - main frame:
-      {"about:blank", "about:blank", "This Page Says", "This page says",
-       "This page says"},
+      {"about:blank", "about:blank", "From This Page", "From this page",
+       "From this page"},
       // - subframe:
-      {"http://foo.com/", "about:blank", "An Embedded Page on This Page Says",
-       "An embedded page on this page says",
-       "An embedded page on this page says"},
+      {"http://foo.com/", "about:blank", "From an Embedded Page on This Page",
+       "From an embedded page on this page",
+       "From an embedded page on this page"},
 
       // blob:
       // - main frame:
       {"blob:http://foo.com/66666666-6666-6666-6666-666666666666",
        "blob:http://foo.com/66666666-6666-6666-6666-666666666666",
-       "foo.com Says", "foo.com says", "foo.com says"},
+       "From foo.com", "From foo.com", "From foo.com"},
       // - subframe:
       {"http://bar.com/",
        "blob:http://foo.com/66666666-6666-6666-6666-666666666666",
-       "An Embedded Page at foo.com Says", "An embedded page at foo.com says",
-       "An embedded page at foo.com says"},
+       "From an Embedded Page at foo.com", "From an embedded page at foo.com",
+       "From an embedded page at foo.com"},
 
       // filesystem:
       // - main frame:
       {"filesystem:http://foo.com/bar.html",
-       "filesystem:http://foo.com/bar.html", "foo.com Says", "foo.com says",
-       "foo.com says"},
+       "filesystem:http://foo.com/bar.html", "From foo.com", "From foo.com",
+       "From foo.com"},
       // - subframe:
       {"http://bar.com/", "filesystem:http://foo.com/bar.html",
-       "An Embedded Page at foo.com Says", "An embedded page at foo.com says",
-       "An embedded page at foo.com says"},
+       "From an Embedded Page at foo.com", "From an embedded page at foo.com",
+       "From an embedded page at foo.com"},
   };
 
   for (const auto& test_case : cases) {
