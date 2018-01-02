01f4ec7f269a5c38946a10ec47b257a94a0c1d2b
May Lippert <maybelle@chromium.org>
2018-01-02 13:20:55 -0800

    Add SaveLoginPassword interface constant.
    BUG=chromium:386606
    
    Change-Id: Ia0a6b727fd7b5251eeeab0a396e9d394ff2667a4
    Reviewed-on: https://chromium-review.googlesource.com/847689
    Commit-Ready: May Lippert <maybelle@chromium.org>
    Tested-by: May Lippert <maybelle@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>

--- dbus/login_manager/dbus-constants.h
+++ dbus/login_manager/dbus-constants.h
@@ -12,6 +12,7 @@ const char kSessionManagerServiceName[] = "org.chromium.SessionManager";
 // Methods
 const char kSessionManagerEmitLoginPromptVisible[] = "EmitLoginPromptVisible";
 const char kSessionManagerEnableChromeTesting[] = "EnableChromeTesting";
+const char kSessionManagerSaveLoginPassword[] = "SaveLoginPassword";
 const char kSessionManagerStartSession[] = "StartSession";
 const char kSessionManagerStopSession[] = "StopSession";
 const char kSessionManagerRestartJob[] = "RestartJob";
