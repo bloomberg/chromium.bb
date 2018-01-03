e7e1fb040af3ce102458986d9626cb13726817e9
Antoine Labour <piman@chromium.org>
2018-01-03 14:37:06 -0800

    Merge //gpu/config:crash_keys into //gpu/config
    After crrev.com/c/812525, crash_keys doesn't need to be a separate
    source_set duplicated in many components. Just make it part of the gpu
    component.
    
    Bug: 598854
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ibaa2e994087401f267a685e0a3c3c7a8f930656d
    Reviewed-on: https://chromium-review.googlesource.com/849492
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526905}
    Former-commit-id: 63875916dcf6535282293bfe590db00125815316

--- gpu/config/gpu_crash_keys.h
+++ gpu/config/gpu_crash_keys.h
@@ -7,25 +7,26 @@
 
 #include "build/build_config.h"
 #include "components/crash/core/common/crash_key.h"
+#include "gpu/gpu_export.h"
 
 namespace gpu {
 namespace crash_keys {
 
 // Keys that can be used for crash reporting.
 #if !defined(OS_ANDROID)
-extern crash_reporter::CrashKeyString<16> gpu_vendor_id;
-extern crash_reporter::CrashKeyString<16> gpu_device_id;
+extern GPU_EXPORT crash_reporter::CrashKeyString<16> gpu_vendor_id;
+extern GPU_EXPORT crash_reporter::CrashKeyString<16> gpu_device_id;
 #endif
-extern crash_reporter::CrashKeyString<64> gpu_driver_version;
-extern crash_reporter::CrashKeyString<16> gpu_pixel_shader_version;
-extern crash_reporter::CrashKeyString<16> gpu_vertex_shader_version;
+extern GPU_EXPORT crash_reporter::CrashKeyString<64> gpu_driver_version;
+extern GPU_EXPORT crash_reporter::CrashKeyString<16> gpu_pixel_shader_version;
+extern GPU_EXPORT crash_reporter::CrashKeyString<16> gpu_vertex_shader_version;
 #if defined(OS_MACOSX)
-extern crash_reporter::CrashKeyString<64> gpu_gl_version;
+extern GPU_EXPORT crash_reporter::CrashKeyString<64> gpu_gl_version;
 #elif defined(OS_POSIX)
-extern crash_reporter::CrashKeyString<256> gpu_vendor;
-extern crash_reporter::CrashKeyString<128> gpu_renderer;
+extern GPU_EXPORT crash_reporter::CrashKeyString<256> gpu_vendor;
+extern GPU_EXPORT crash_reporter::CrashKeyString<128> gpu_renderer;
 #endif
-extern crash_reporter::CrashKeyString<4> gpu_gl_context_is_virtual;
+extern GPU_EXPORT crash_reporter::CrashKeyString<4> gpu_gl_context_is_virtual;
 
 }  // namespace crash_keys
 }  // namespace gpu
