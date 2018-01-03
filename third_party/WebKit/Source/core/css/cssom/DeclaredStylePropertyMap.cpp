ab265bccbd80fb338e3839a0ca98c24d8516bc1e
Darren Shen <shend@chromium.org>
2018-01-04 10:44:36 +1100

    [css-typed-om] Implement declared style property map.
    Implements style property map for interacting with CSS rules.
    
    The tests are copied from the inline style property map tests,
    with the only change being createInlineStyle -> createDeclaredStyle.
    
    Spec: https://drafts.css-houdini.org/css-typed-om-1/#declared-stylepropertymap-objects
    
    Bug: 785132
    Change-Id: I63aedf6d02ccacc506e7a1873c17fecafe466779
    Reviewed-on: https://chromium-review.googlesource.com/823508
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526886}
    Former-commit-id: efd690f517a0758bd37bb701c3ab84656922fd39

---
+++ third_party/WebKit/Source/core/css/cssom/DeclaredStylePropertyMap.cpp
@@ -0,0 +1,79 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/cssom/DeclaredStylePropertyMap.h"
+
+#include "core/css/CSSCustomPropertyDeclaration.h"
+#include "core/css/CSSPropertyValueSet.h"
+
+#include "core/css/CSSStyleRule.h"
+#include "core/css/CSSStyleSheet.h"
+#include "core/css/StyleRule.h"
+
+namespace blink {
+
+DeclaredStylePropertyMap::DeclaredStylePropertyMap(CSSStyleRule* owner_rule)
+    : StylePropertyMap(), owner_rule_(owner_rule) {}
+
+const CSSValue* DeclaredStylePropertyMap::GetProperty(
+    CSSPropertyID property_id) {
+  if (!GetStyleRule())
+    return nullptr;
+  return GetStyleRule()->Properties().GetPropertyCSSValue(property_id);
+}
+
+const CSSValue* DeclaredStylePropertyMap::GetCustomProperty(
+    AtomicString property_name) {
+  if (!GetStyleRule())
+    return nullptr;
+  return GetStyleRule()->Properties().GetPropertyCSSValue(property_name);
+}
+
+void DeclaredStylePropertyMap::SetProperty(CSSPropertyID property_id,
+                                           const CSSValue& value) {
+  if (!GetStyleRule())
+    return;
+  CSSStyleSheet::RuleMutationScope mutation_scope(owner_rule_);
+  GetStyleRule()->MutableProperties().SetProperty(property_id, value);
+}
+
+void DeclaredStylePropertyMap::RemoveProperty(CSSPropertyID property_id) {
+  if (!GetStyleRule())
+    return;
+  CSSStyleSheet::RuleMutationScope mutation_scope(owner_rule_);
+  GetStyleRule()->MutableProperties().RemoveProperty(property_id);
+}
+
+void DeclaredStylePropertyMap::RemoveCustomProperty(
+    const AtomicString& property_name) {
+  if (!GetStyleRule())
+    return;
+  CSSStyleSheet::RuleMutationScope mutation_scope(owner_rule_);
+  GetStyleRule()->MutableProperties().RemoveProperty(property_name);
+}
+
+void DeclaredStylePropertyMap::ForEachProperty(
+    const IterationCallback& callback) {
+  const CSSPropertyValueSet& declared_style_set = GetStyleRule()->Properties();
+  for (unsigned i = 0; i < declared_style_set.PropertyCount(); i++) {
+    const auto& property_reference = declared_style_set.PropertyAt(i);
+    if (property_reference.Id() == CSSPropertyVariable) {
+      const auto& decl =
+          ToCSSCustomPropertyDeclaration(property_reference.Value());
+      callback(decl.GetName(), property_reference.Value());
+    } else {
+      const CSSProperty& property = CSSProperty::Get(property_reference.Id());
+      callback(property.GetPropertyNameAtomicString(),
+               property_reference.Value());
+    }
+  }
+}
+
+StyleRule* DeclaredStylePropertyMap::GetStyleRule() const {
+  if (!owner_rule_)
+    return nullptr;
+  return owner_rule_->GetStyleRule();
+}
+
+}  // namespace blink
