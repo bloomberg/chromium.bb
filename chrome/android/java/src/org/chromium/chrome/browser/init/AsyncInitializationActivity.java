bloomberg/chromium.bb

commit 4877446103eda70eede3aabc45753abb7228dd0d

Android: Simplify DeviceFormFactor.isTablet()
Rather than provide a setIsTablet(), just move the resource from
chrome->ui and have DeviceFormFactor use it directly.

Change-Id: Ie867fc604aa223a6c4896124e3dfe80c09675593
Reviewed-on: https://chromium-review.googlesource.com/769348
Reviewed-by: Theresa <twellington@chromium.org>
Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
Commit-Queue: agrieve <agrieve@chromium.org>
Cr-Commit-Position: refs/heads/master@{#518274}
Former-commit-id: d875cacbdc450d8594b560d4511bef1e88c5f01b

chrome/android/java/src/org/chromium/chrome/browser/init/AsyncInitializationActivity.java
@@ -111,11 +111,10 @@ public abstract class AsyncInitializationActivity extends AppCompatActivity impl
         // switcher resources. Overriding the smallestScreenWidthDp in the Configuration ensures
         // Android will load the tab strip resources. See crbug.com/588838.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            int smallestDeviceWidthDp = DeviceFormFactor.getSmallestDeviceWidthDp();
-
-            if (smallestDeviceWidthDp >= DeviceFormFactor.MINIMUM_TABLET_WIDTH_DP) {
+            if (DeviceFormFactor.isTablet()) {
                 Configuration overrideConfiguration = new Configuration();
-                overrideConfiguration.smallestScreenWidthDp = smallestDeviceWidthDp;
+                overrideConfiguration.smallestScreenWidthDp =
+                        DeviceFormFactor.getSmallestDeviceWidthDp();
                 applyOverrideConfiguration(overrideConfiguration);
             }
         }
@@ -125,14 +124,7 @@ public abstract class AsyncInitializationActivity extends AppCompatActivity impl
     @Override
     public void preInflationStartup() {
         mHadWarmStart = LibraryLoader.isInitialized();
-        // On some devices, OEM modifications have been made to the resource loader that cause the
-        // DeviceFormFactor calculation of whether a device is using tablet resources to be
-        // incorrect. Check which resources were actually loaded and set the DeviceFormFactor
-        // values. See crbug.com/662338.
-        boolean isTablet = getResources().getBoolean(R.bool.is_tablet);
-        boolean isLargeTablet = getResources().getBoolean(R.bool.is_large_tablet);
-        DeviceFormFactor.setIsTablet(isTablet, isLargeTablet);
-        mIsTablet = isTablet;
+        mIsTablet = DeviceFormFactor.isTablet();
     }
 
     @Override

ui/android/java/src/org/chromium/ui/base/DeviceFormFactor.java
@@ -11,22 +11,16 @@ import android.view.WindowManager;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.annotations.CalledByNative;
+import org.chromium.ui.R;
 
 /**
  * UI utilities for accessing form factor information.
  */
 public class DeviceFormFactor {
-
     /**
      * The minimum width that would classify the device as a tablet or a large tablet.
      */
     public static final int MINIMUM_TABLET_WIDTH_DP = 600;
-    private static final int MINIMUM_LARGE_TABLET_WIDTH_DP = 720;
-
-    private static Boolean sIsTablet;
-    private static Boolean sIsLargeTablet;
-    private static Integer sMinimumTabletWidthPx;
-    private static Float sDensity;
 
     /**
      * @return Whether the app should treat the device as a tablet for layout. This method is not
@@ -34,22 +28,21 @@ public class DeviceFormFactor {
      */
     @CalledByNative
     public static boolean isTablet() {
-        if (sIsTablet == null) {
-            sIsTablet = getSmallestDeviceWidthDp() >= MINIMUM_TABLET_WIDTH_DP;
-        }
-        return sIsTablet;
+        // On some devices, OEM modifications have been made to the resource loader that cause the
+        // DeviceFormFactor calculation of whether a device is using tablet resources to be
+        // incorrect. Check which resources were actually loaded rather than look at screen size.
+        // See crbug.com/662338.
+        return ContextUtils.getApplicationContext().getResources().getBoolean(R.bool.is_tablet);
     }
 
     /**
      * @param context {@link Context} used to get the Application Context.
      * @return True if the app should treat the device as a large (> 720dp) tablet for layout. This
-     *         method is not affected by Android N multi-window.
+     * method is not affected by Android N multi-window.
      */
     public static boolean isLargeTablet(Context context) {
-        if (sIsLargeTablet == null) {
-            sIsLargeTablet = getSmallestDeviceWidthDp() >= MINIMUM_LARGE_TABLET_WIDTH_DP;
-        }
-        return sIsLargeTablet;
+        return ContextUtils.getApplicationContext().getResources().getBoolean(
+                R.bool.is_large_tablet);
     }
 
     /**
@@ -88,34 +81,7 @@ public class DeviceFormFactor {
      *         layout.
      */
     public static int getMinimumTabletWidthPx(Context context) {
-        if (sMinimumTabletWidthPx == null) {
-            sMinimumTabletWidthPx = Math.round(MINIMUM_TABLET_WIDTH_DP
-                    * context.getResources().getDisplayMetrics().density);
-        }
-        return sMinimumTabletWidthPx;
-    }
-
-    /**
-     * Resets all cached values if the display density has changed.
-     */
-    public static void resetValuesIfNeeded(Context context) {
-        float currentDensity = context.getResources().getDisplayMetrics().density;
-        if (sDensity != null && sDensity != currentDensity) {
-            sIsTablet = null;
-            sIsLargeTablet = null;
-            sMinimumTabletWidthPx = null;
-        }
-        sDensity = currentDensity;
-    }
-
-    /**
-     * Sets whether the device is a tablet.
-     * @param isTablet Whether the app should treat the device as a tablet for layout.
-     * @param isLargeTablet Whether the app should treat the device as a large tablet for layout.
-     *                      If this is true, isTablet should also be true.
-     */
-    public static void setIsTablet(boolean isTablet, boolean isLargeTablet) {
-        sIsTablet = isTablet;
-        sIsLargeTablet = isLargeTablet;
+        return Math.round(
+                MINIMUM_TABLET_WIDTH_DP * context.getResources().getDisplayMetrics().density);
     }
 }

chrome/android/java/res/values-sw600dp/values.xml
@@ -1,17 +1,8 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!-- Copyright (C) 2011 The Android Open Source Project
+<!-- Copyright 2011 The Chromium Authors. All rights reserved.
 
-     Licensed under the Apache License, Version 2.0 (the "License");
-     you may not use this file except in compliance with the License.
-     You may obtain a copy of the License at
-
-          http://www.apache.org/licenses/LICENSE-2.0
-
-     Unless required by applicable law or agreed to in writing, software
-     distributed under the License is distributed on an "AS IS" BASIS,
-     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-     See the License for the specific language governing permissions and
-     limitations under the License.
+     Use of this source code is governed by a BSD-style license that can be
+     found in the LICENSE file.
 -->
 <resources xmlns:android="http://schemas.android.com/apk/res/android">
     <!-- Full Screen Constants -->
@@ -32,9 +23,4 @@
 
     <!-- Compositor Tab Title Text -->
     <bool name="compositor_tab_title_fake_bold_text">false</bool>
-
-    <!-- Whether the device is a tablet or large tablet. Used to determine which resources the OS
-         loaded. -->
-    <bool name="is_tablet">true</bool>
-    <bool name="is_large_tablet">false</bool>
 </resources>

chrome/android/java/res/values/values.xml
@@ -64,11 +64,6 @@
     <!-- Our manage space activity. Default pre-KitKat to be nothing. -->
     <string name="manage_space_activity"></string>
 
-    <!-- Whether the device is a tablet or large tablet. Used to determine which resources the OS
-         loaded. -->
-    <bool name="is_tablet">false</bool>
-    <bool name="is_large_tablet">false</bool>
-
     <!-- Value for scaleX to mirror drawables correctly for LTR / RTL layouts. -->
     <integer name="automirror_scale_x">1</integer>
 

chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
@@ -49,7 +49,6 @@ import org.chromium.content.browser.DeviceUtils;
 import org.chromium.content.browser.SpeechRecognition;
 import org.chromium.net.NetworkChangeNotifier;
 import org.chromium.policy.CombinedPolicyProvider;
-import org.chromium.ui.base.DeviceFormFactor;
 
 import java.io.File;
 import java.util.Locale;
@@ -411,8 +410,6 @@ public class ChromeBrowserInitializer {
                         Log.e(TAG, "Killing process because of locale change.");
                         Process.killProcess(Process.myPid());
                     }
-
-                    DeviceFormFactor.resetValuesIfNeeded(mApplication);
                 }
             }
         };

ui/android/java/res/values-sw600dp/values.xml
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!-- Copyright 2016 The Chromium Authors. All rights reserved.
+<!-- Copyright 2017 The Chromium Authors. All rights reserved.
 
      Use of this source code is governed by a BSD-style license that can be
      found in the LICENSE file.
@@ -8,5 +8,5 @@
     <!-- Whether the device is a tablet or large tablet. Used to determine which resources the OS
          loaded. -->
     <bool name="is_tablet">true</bool>
-    <bool name="is_large_tablet">true</bool>
+    <bool name="is_large_tablet">false</bool>
 </resources>

ui/android/java/res/values-sw720dp-v17/values.xml
@@ -1,14 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!-- Copyright 2013 The Chromium Authors. All rights reserved.
+<!-- Copyright 2017 The Chromium Authors. All rights reserved.
 
      Use of this source code is governed by a BSD-style license that can be
      found in the LICENSE file.
 -->
-
 <resources xmlns:android="http://schemas.android.com/apk/res/android">
-    <item type="id" name="dropdown_popup_window" />
     <!-- Whether the device is a tablet or large tablet. Used to determine which resources the OS
          loaded. -->
-    <bool name="is_tablet">false</bool>
-    <bool name="is_large_tablet">false</bool>
+    <bool name="is_tablet">true</bool>
+    <bool name="is_large_tablet">true</bool>
 </resources>

commit 19dbf554980ee95b4bf5096b9362c6bcd6be92e9

Fix crash when switching back from Samsung DeX to Phone mode.
Regressed in 4877446103eda70eede3aabc45753abb7228dd0d [formerly d875cacbdc450d8594b560d4511bef1e88c5f01b]

Bug: 780593
Change-Id: Ifee7a93ada73f5b757a35ece75b0bef61ea767c2
Reviewed-on: https://chromium-review.googlesource.com/820531
Reviewed-by: Theresa <twellington@chromium.org>
Reviewed-by: Ted Choc (back but slow, ping me) <tedchoc@chromium.org>
Commit-Queue: agrieve <agrieve@chromium.org>
Cr-Commit-Position: refs/heads/master@{#527117}
Former-commit-id: e23729f31db28a378f92a25f7672ba1a51324c5e

chrome/android/java/src/org/chromium/chrome/browser/init/AsyncInitializationActivity.java
@@ -110,14 +110,13 @@ public abstract class AsyncInitializationActivity extends AppCompatActivity impl
         // On N+, Chrome should always retain the tab strip layout on tablets. Normally in
         // multi-window, if Chrome is launched into a smaller screen Android will load the tab
         // switcher resources. Overriding the smallestScreenWidthDp in the Configuration ensures
-        // Android will load the tab strip resources. See crbug.com/588838.
+        // Android will load the tab strip resources.
+        // See crbug.com/588838, crbug.com/662338, crbug.com/780593.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            if (DeviceFormFactor.isTablet()) {
-                Configuration overrideConfiguration = new Configuration();
-                overrideConfiguration.smallestScreenWidthDp =
-                        DeviceFormFactor.getSmallestDeviceWidthDp();
-                applyOverrideConfiguration(overrideConfiguration);
-            }
+            Configuration overrideConfiguration = new Configuration();
+            overrideConfiguration.smallestScreenWidthDp =
+                    DeviceFormFactor.getSmallestDeviceWidthDp();
+            applyOverrideConfiguration(overrideConfiguration);
         }
     }
 

ui/android/java/src/org/chromium/ui/base/DeviceFormFactor.java
@@ -23,15 +23,12 @@ public class DeviceFormFactor {
     public static final int MINIMUM_TABLET_WIDTH_DP = 600;
 
     /**
-     * @return Whether the app should treat the device as a tablet for layout. This method is not
-     *         affected by Android N multi-window.
+     * @return Whether the app should currently treat the device as a tablet for layout. This method
+     *         is not affected by Android N multi-window, but can change for external displays.
+     *         E.g. http://developer.samsung.com/samsung-dex/testing
      */
     @CalledByNative
     public static boolean isTablet() {
-        // On some devices, OEM modifications have been made to the resource loader that cause the
-        // DeviceFormFactor calculation of whether a device is using tablet resources to be
-        // incorrect. Check which resources were actually loaded rather than look at screen size.
-        // See crbug.com/662338.
         return ContextUtils.getApplicationContext().getResources().getBoolean(R.bool.is_tablet);
     }
 