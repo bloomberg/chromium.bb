275100e28d8d7ed4433b50c8e08d9564714afcb3
Dominic Battre <battre@chromium.org>
2018-01-04 13:42:19 +0100

    Disabled falky ExtensionStorageMonitorTest.TwoHostedAppsInSameOrigin
    TBR=nasko@chromium.org,nick@chromium.org,rdevlin.cronin@chromium.org
    
    Bug: 799022
    Change-Id: I7db594de0aaaecda8d3d7f0bcd8cf8ea16abb3c9
    Reviewed-on: https://chromium-review.googlesource.com/850393
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#526973}
    Former-commit-id: 57dbabe710b60eab888e71afb285a0f37dfa66d5

--- chrome/browser/extensions/extension_storage_monitor_browsertest.cc
+++ chrome/browser/extensions/extension_storage_monitor_browsertest.cc
@@ -383,7 +383,9 @@ IN_PROC_BROWSER_TEST_F(ExtensionStorageMonitorTest,
 // Exercises the case where two hosted apps are same-origin but have non-
 // overlapping extents. Disabling one should not suppress storage monitoring for
 // the other.
-IN_PROC_BROWSER_TEST_F(ExtensionStorageMonitorTest, TwoHostedAppsInSameOrigin) {
+// Disabled for flakiness. crbug.com/799022
+IN_PROC_BROWSER_TEST_F(ExtensionStorageMonitorTest,
+                       DISABLED_TwoHostedAppsInSameOrigin) {
   ASSERT_TRUE(embedded_test_server()->Start());
 
   GURL url1 = embedded_test_server()->GetURL(
