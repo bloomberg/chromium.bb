d5e4d2e22f8c56b47ea980ab514307f8939ef0c1
Katie Dektar <katydek@google.com>
2018-01-04 10:25:42 -0800

    Adds speech pitch control in select-to-speak settings.
    Gives the users 6 options for pitch, from 0.55 through 1.3.
    
    Bug: 796745
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ic13c4098ff0c3629a1faa4c8a063a287492d8696
    Reviewed-on: https://chromium-review.googlesource.com/838247
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527135}
    Former-commit-id: dbbea180ec5643783dc6a41bc643bd0c36189cba

--- chrome/browser/resources/chromeos/select_to_speak/select_to_speak_options.js
+++ chrome/browser/resources/chromeos/select_to_speak/select_to_speak_options.js
@@ -22,6 +22,7 @@ SelectToSpeakOptionsPage.prototype = {
     }.bind(this));
     this.syncSelectControlToPref_('voice', 'voice');
     this.syncSelectControlToPref_('rate', 'rate');
+    this.syncSelectControlToPref_('pitch', 'pitch');
     this.syncCheckboxControlToPref_(
         'wordHighlight', 'wordHighlight', function(checked) {
           let elem = document.getElementById('highlightSubOption');
