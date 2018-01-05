4d0a1082dd95bb5507d1c6831bb17c1d5bca7f83
Juan Lang <juanlang@google.com>
2018-01-05 09:18:58 -0800

    Be more permissive handling sign errors from U2F devices.
    Since the U2F spec is a little ambiguous about the proper behavior when
    a U2F device receives a sign request with a key handle it doesn't
    generate, device vendors are predictably a little creative in how they
    behave. Hard failing when a user has devices from different vendors is a
    little unfriendly to users, especially since these devices are intended
    not to be field-updateable.
    
    This change treats most errors as "permissible", allowing all of a
    user's key handles to be attempted before failing a request.
    
    BUG=799279
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Id248317b43d5812c92d1b3f3c5713e518bb5b62f
    Reviewed-on: https://chromium-review.googlesource.com/852516
    Commit-Queue: Juan Lang <juanlang@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527695}
    Former-commit-id: 31a51606bfc0797d7c0188678725331089bb7036

--- chrome/browser/resources/cryptotoken/singlesigner.js
+++ chrome/browser/resources/cryptotoken/singlesigner.js
@@ -354,10 +354,12 @@ SingleGnubbySigner.prototype.doSign_ = function(challengeIndex) {
  *     for this gnubby.
  */
 SingleGnubbySigner.signErrorIndicatesInvalidKeyHandle = function(code) {
-  return (
-      code == DeviceStatusCodes.WRONG_DATA_STATUS ||
-      code == DeviceStatusCodes.WRONG_LENGTH_STATUS ||
-      code == DeviceStatusCodes.INVALID_DATA_STATUS);
+  // Negative errors are synthetic, device-level errors, rather than APDU-layer
+  // things. Wait for touch is the only error code defined to be a transient
+  // situation. Unfortunately the spec is ambiguous, and some devices behave
+  // oddly, so we treat all APDU-layer errors as idempotent rather than
+  // transient.
+  return code > 0 && code != DeviceStatusCodes.WAIT_TOUCH_STATUS;
 };
 
 /**
@@ -391,10 +393,6 @@ SingleGnubbySigner.prototype.signCallback_ = function(
 
   var self = this;
   switch (code) {
-    case DeviceStatusCodes.GONE_STATUS:
-      this.goToError_(code);
-      break;
-
     case DeviceStatusCodes.TIMEOUT_STATUS:
       this.gnubby_.sync(this.synced_.bind(this));
       break;
@@ -404,13 +402,6 @@ SingleGnubbySigner.prototype.signCallback_ = function(
       break;
 
     case DeviceStatusCodes.OK_STATUS:
-      // Lower bound on the minimum length, signature length can vary.
-      var MIN_SIGNATURE_LENGTH = 7;
-      if (!opt_info || opt_info.byteLength < MIN_SIGNATURE_LENGTH) {
-        console.error(UTIL_fmt(
-            'Got short response to sign request (' +
-            (opt_info ? opt_info.byteLength : 0) + ' bytes), WTF?'));
-      }
       if (this.forEnroll_) {
         this.goToError_(code);
       } else {
@@ -424,25 +415,22 @@ SingleGnubbySigner.prototype.signCallback_ = function(
       }, SingleGnubbySigner.SIGN_DELAY_MILLIS);
       break;
 
-    case DeviceStatusCodes.WRONG_DATA_STATUS:
-    case DeviceStatusCodes.WRONG_LENGTH_STATUS:
-    case DeviceStatusCodes.INVALID_DATA_STATUS:
+    default:
+      if (code < 0) {
+        // Negative errors are synthetic, device-level errors, rather than
+        // APDU-layer things. Other than the ones explicitly handled above,
+        // these are indicative of unhappy devices, so return them immediately
+        // to the caller.
+        this.goToError_(code);
+        return;
+      }
+
       if (this.challengeIndex_ < this.challenges_.length - 1) {
         this.doSign_(++this.challengeIndex_);
       } else if (this.forEnroll_) {
         this.goToSuccess_(code);
       } else {
         this.goToError_(code);
-      }
-      break;
-
-    default:
-      if (this.forEnroll_) {
-        this.goToError_(code, true);
-      } else if (this.challengeIndex_ < this.challenges_.length - 1) {
-        this.doSign_(++this.challengeIndex_);
-      } else {
-        this.goToError_(code, true);
       }
   }
 };
