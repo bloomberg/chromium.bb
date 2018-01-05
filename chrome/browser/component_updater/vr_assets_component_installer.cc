13af8ec67db350001b7debcb21a1c2c709fd777f
Tibor Goldschwendt <tiborg@chromium.org>
2018-01-05 12:31:54 -0500

    [vr] Update assets component when entering VR and component not ready
    We need the assets component in VR browsing mode to provide the best
    user experience. Thus, initiate an immediate update check when entering
    VR and no compatible component is available in order to minimize the
    time the user has to browse in the fallback browsing mode.
    
    Bug: 793975
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I7a74173807e76a457c0f8c7bfed7384596c90e35
    Reviewed-on: https://chromium-review.googlesource.com/850852
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527338}
    Former-commit-id: 8a2c2f916eb0391802d9f4d37be2651d67956f85

--- chrome/browser/component_updater/vr_assets_component_installer.cc
+++ chrome/browser/component_updater/vr_assets_component_installer.cc
@@ -22,6 +22,8 @@
 #include "chrome/browser/vr_features.h"
 #include "chrome/common/safe_browsing/file_type_policies.h"
 #include "components/component_updater/component_updater_paths.h"
+#include "components/component_updater/component_updater_service.h"
+#include "components/crx_file/id_util.h"
 
 using component_updater::ComponentUpdateService;
 
@@ -41,6 +43,20 @@ const base::FilePath::CharType kRelativeInstallDir[] =
 }  // namespace
 
 namespace component_updater {
+
+// static
+void VrAssetsComponentInstallerTraits::UpdateComponent(
+    ComponentUpdateService* cus) {
+#if BUILDFLAG(USE_VR_ASSETS_COMPONENT)
+  const std::string crx_id = crx_file::id_util::GenerateIdFromHash(
+      kPublicKeySHA256, sizeof(kPublicKeySHA256));
+  // Make sure the component is registered.
+  DCHECK(std::find(cus->GetComponentIDs().begin(), cus->GetComponentIDs().end(),
+                   crx_id) != cus->GetComponentIDs().end());
+  cus->GetOnDemandUpdater().OnDemandUpdate(
+      crx_id, base::BindOnce([](update_client::Error error) { return; }));
+#endif  // BUILDFLAG(USE_VR_ASSETS_COMPONENT)
+}
 
 bool VrAssetsComponentInstallerTraits::
     SupportsGroupPolicyEnabledComponentUpdates() const {
@@ -132,14 +148,18 @@ std::vector<std::string> VrAssetsComponentInstallerTraits::GetMimeTypes()
 }
 
 void RegisterVrAssetsComponent(ComponentUpdateService* cus) {
-#if BUILDFLAG(REGISTER_VR_ASSETS_COMPONENT)
+#if BUILDFLAG(USE_VR_ASSETS_COMPONENT)
   std::unique_ptr<ComponentInstallerPolicy> policy(
       new VrAssetsComponentInstallerTraits());
   auto installer = base::MakeRefCounted<ComponentInstaller>(std::move(policy));
   installer->Register(cus, base::Closure());
   vr::Assets::GetInstance()->GetMetricsHelper()->OnRegisteredComponent();
   VLOG(1) << "Registered VR assets component";
-#endif  // BUILDFLAG(REGISTER_VR_ASSETS_COMPONENT)
+#endif  // BUILDFLAG(USE_VR_ASSETS_COMPONENT)
+}
+
+void UpdateVrAssetsComponent(ComponentUpdateService* cus) {
+  VrAssetsComponentInstallerTraits::UpdateComponent(cus);
 }
 
 }  // namespace component_updater
