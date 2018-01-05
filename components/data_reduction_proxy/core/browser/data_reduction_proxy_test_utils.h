3514313937a62c56655174e66ffd3355cdb748e1
Vadym Doroshenko <dvadym@chromium.org>
2018-01-05 10:18:39 +0000

    Revert "Pass network quality estimator to warmup probe URL fetcher"
    This reverts commit 0019c014ebfa6e91336408078d2610190d45d1a7 [formerly 354c7688915f6f86de4dd4b6b6d3cd5df7daafc2].
    
    Reason for revert: this CL is most likely reason of failing test DataReductionProxyProtocolEmbeddedServerTest.EmbeddedTestServerBypassRetryOnPostConnectionErrors on Windows, this tests started failing on build https://uberchromegw.corp.google.com/i/chromium.win/builders/Win7%20Tests%20%28dbg%29%281%29/builds/65587
    
    Original change's description:
    > Pass network quality estimator to warmup probe URL fetcher
    > 
    > Update the tests to ensure that the network
    > quality estimator is set on the URLRequestContextGetter that
    > is passed to the warmup URL fetcher. This would be used
    > in a subsequent CL to compute the fetcher timeout.
    > 
    > Bug: 760294
    > Change-Id: I2c2081352127d7e21a00fb1935087e8555bf6fd6
    > Reviewed-on: https://chromium-review.googlesource.com/849759
    > Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    > Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#527173}
    
    TBR=tbansal@chromium.org,dougarnett@chromium.org
    
    Change-Id: I7ff5b193e2282f759469bd3cd318fcb6bb53d05c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 760294
    Reviewed-on: https://chromium-review.googlesource.com/852112
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527251}
    Former-commit-id: 1d2905e07fa0b31f53feb07d06110361a59d7ac5

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_test_utils.h
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_test_utils.h
@@ -39,7 +39,6 @@ class TestingPrefServiceSimple;
 namespace net {
 class MockClientSocketFactory;
 class NetLog;
-class TestNetworkQualityEstimator;
 class URLRequestContext;
 class URLRequestContextStorage;
 }
@@ -510,7 +509,6 @@ class DataReductionProxyTestContext {
       std::unique_ptr<TestDataReductionProxyEventStorageDelegate>
           storage_delegate,
       std::unique_ptr<TestConfigStorer> config_storer,
-      std::unique_ptr<net::TestNetworkQualityEstimator> estimator,
       TestDataReductionProxyParams* params,
       unsigned int test_context_flags);
 
@@ -523,7 +521,6 @@ class DataReductionProxyTestContext {
   std::unique_ptr<TestingPrefServiceSimple> simple_pref_service_;
   std::unique_ptr<net::TestNetLog> net_log_;
   scoped_refptr<net::URLRequestContextGetter> request_context_getter_;
-  std::unique_ptr<net::TestNetworkQualityEstimator> estimator_;
   // Non-owned pointer. Will be NULL if |this| was built without specifying a
   // |net::MockClientSocketFactory|.
   net::MockClientSocketFactory* mock_socket_factory_;
