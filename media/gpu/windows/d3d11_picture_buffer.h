4e2091d253b87f607d99277d253bfff93d1b10f3
liberato@chromium.org <liberato@chromium.org>
2018-01-04 22:01:03 -0800

    Move resource management into D3D11PictureBuffer
    This CL moves most of the code that specific to the D3D11
    PictureBuffer class into that class, from the decoder.  This makes
    it much easier to separate the D3D device contexts and associated
    threading issues.
    
    It also adds an error state to the decoder, and NotifyError() to set
    it / signal the error.
    
    Bug: 775577
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: If61e64dd19c76ebc7b2b7375fc11e4a1c92efc95
    Reviewed-on: https://chromium-review.googlesource.com/737456
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527228}
    Former-commit-id: 6e81422291cca43f41ec33393f9fbb9cc7c8f905

--- media/gpu/windows/d3d11_picture_buffer.h
+++ media/gpu/windows/d3d11_picture_buffer.h
@@ -11,61 +11,111 @@
 
 #include <vector>
 
+#include "base/memory/ref_counted.h"
+
 #include "gpu/command_buffer/service/texture_manager.h"
+#include "gpu/ipc/service/command_buffer_stub.h"
 #include "media/base/video_frame.h"
+#include "media/gpu/media_gpu_export.h"
 #include "media/video/picture.h"
+#include "third_party/angle/include/EGL/egl.h"
+#include "third_party/angle/include/EGL/eglext.h"
+#include "ui/gl/gl_image.h"
 
 namespace media {
-class D3D11H264Accelerator;
 
-// This must be freed on the main thread, since it has things like |gl_image_|
-// and |texture_refs_|.
-class D3D11PictureBuffer {
+// PictureBuffer that owns Chrome Textures to display it, and keep a reference
+// to the D3D texture that backs the image.
+//
+// This is created and owned on the decoder thread.  While currently that's the
+// gpu main thread, we still keep the decoder parts separate from the chrome GL
+// parts, in case that changes.
+//
+// This is refcounted so that VideoFrame can hold onto it indirectly.  While
+// the chrome Texture is sufficient to keep the pictures renderable, we still
+// need to guarantee that the client has time to use the mailbox.  Once it
+// does so, it would be fine if this were destroyed.  Technically, only the
+// GpuResources have to be retained until the mailbox is used, but we just
+// retain the whole thing.
+class MEDIA_GPU_EXPORT D3D11PictureBuffer
+    : public base::RefCountedThreadSafe<D3D11PictureBuffer> {
  public:
   using MailboxHolderArray = gpu::MailboxHolder[VideoFrame::kMaxPlanes];
 
-  D3D11PictureBuffer(PictureBuffer picture_buffer, size_t level);
-  D3D11PictureBuffer(
-      PictureBuffer picture_buffer,
-      size_t level,
-      const std::vector<scoped_refptr<gpu::gles2::TextureRef>>& texture_refs,
-      const MailboxHolderArray& mailbox_holders);
-  ~D3D11PictureBuffer();
+  D3D11PictureBuffer(GLenum target, gfx::Size size, size_t level);
 
-  bool Init(Microsoft::WRL::ComPtr<ID3D11VideoDevice> video_device,
+  bool Init(base::RepeatingCallback<gpu::CommandBufferStub*()> get_stub_cb,
+            Microsoft::WRL::ComPtr<ID3D11VideoDevice> video_device,
             Microsoft::WRL::ComPtr<ID3D11Texture2D> texture,
-            const GUID& decoder_guid);
+            const GUID& decoder_guid,
+            int textures_per_picture);
 
+  const gfx::Size& size() const { return size_; }
   size_t level() const { return level_; }
+  Microsoft::WRL::ComPtr<ID3D11Texture2D> texture() const { return texture_; }
 
-  PictureBuffer& picture_buffer() { return picture_buffer_; }
-
+  // Is this PictureBuffer backing a VideoFrame right now?
   bool in_client_use() const { return in_client_use_; }
+
+  // Is this PictureBuffer holding an image that's in use by the decoder?
   bool in_picture_use() const { return in_picture_use_; }
+
   void set_in_client_use(bool use) { in_client_use_ = use; }
   void set_in_picture_use(bool use) { in_picture_use_ = use; }
-  scoped_refptr<gl::GLImage> gl_image() const { return gl_image_; }
 
-  // For D3D11VideoDecoder.
+  const Microsoft::WRL::ComPtr<ID3D11VideoDecoderOutputView>& output_view()
+      const {
+    return output_view_;
+  }
+
+  // Return the mailbox holders that can be used to create a VideoFrame for us.
   const MailboxHolderArray& mailbox_holders() const { return mailbox_holders_; }
+
   // Shouldn't be here, but simpler for now.
   base::TimeDelta timestamp_;
 
  private:
-  friend class D3D11H264Accelerator;
+  ~D3D11PictureBuffer();
+  friend class base::RefCountedThreadSafe<D3D11PictureBuffer>;
 
-  PictureBuffer picture_buffer_;
-  Microsoft::WRL::ComPtr<ID3D11Texture2D> texture_;
+  GLenum target_;
+  gfx::Size size_;
   bool in_picture_use_ = false;
   bool in_client_use_ = false;
   size_t level_;
-  Microsoft::WRL::ComPtr<ID3D11VideoDecoderOutputView> output_view_;
-  EGLStreamKHR stream_;
-  scoped_refptr<gl::GLImage> gl_image_;
 
-  // For D3D11VideoDecoder.
-  std::vector<scoped_refptr<gpu::gles2::TextureRef>> texture_refs_;
+  // TODO(liberato): I don't think that we need to remember |texture_|.  The
+  // GLImage will do so, so it will last long enough for any VideoFrames that
+  // reference it.
+  Microsoft::WRL::ComPtr<ID3D11Texture2D> texture_;
+  Microsoft::WRL::ComPtr<ID3D11VideoDecoderOutputView> output_view_;
+
   MailboxHolderArray mailbox_holders_;
+
+  // Things that are to be accessed / freed only on the main thread.  In
+  // addition to setting up the textures to render from a D3D11 texture,
+  // these also hold the chrome GL Texture objects so that the client
+  // can use the mailbox.
+  class GpuResources {
+   public:
+    GpuResources();
+    ~GpuResources();
+
+    bool Init(base::RepeatingCallback<gpu::CommandBufferStub*()> get_stub_cb,
+              int level,
+              const std::vector<gpu::Mailbox> mailboxes,
+              GLenum target,
+              gfx::Size size,
+              Microsoft::WRL::ComPtr<ID3D11Texture2D> angle_texture,
+              int textures_per_picture);
+
+    std::vector<scoped_refptr<gpu::gles2::TextureRef>> texture_refs_;
+
+   private:
+    DISALLOW_COPY_AND_ASSIGN(GpuResources);
+  };
+
+  std::unique_ptr<GpuResources> gpu_resources_;
 
   DISALLOW_COPY_AND_ASSIGN(D3D11PictureBuffer);
 };
