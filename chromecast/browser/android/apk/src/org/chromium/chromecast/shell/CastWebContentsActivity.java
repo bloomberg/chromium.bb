75a90728df64eda57ca1d1be178e78a2f85707df
Thoren Paulson <thoren@chromium.org>
2018-01-05 16:34:59 -0800

    [Chromecast] Remove CVRV in detachWebContents.
    Adding these to the layout but not removing them was causing problems.
    
    Bug: internal b/70972567
    Test: repro, CQ
    Change-Id: I7abeff26c8e5e8afc47cd63a7ee2ad76ba129343
    Reviewed-on: https://chromium-review.googlesource.com/853285
    Reviewed-by: Sergey Volk <servolk@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Thoren Paulson <thoren@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527777}
    Former-commit-id: 3cc0f30aec1eb770fa0233ce79dddaed6ff76f33

--- chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastWebContentsActivity.java
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastWebContentsActivity.java
@@ -401,6 +401,7 @@ public class CastWebContentsActivity extends Activity {
         if (DEBUG) Log.d(TAG, "detachWebContentsIfAny");
         if (mContentView != null) {
             mCastWebContentsLayout.removeView(mContentView);
+            mCastWebContentsLayout.removeView(mContentViewRenderView);
             mContentViewCore.destroy();
             mContentViewRenderView.destroy();
             mWindow.destroy();
