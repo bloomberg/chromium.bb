d20f70b67bc9256076e72451fa85d39e458c933e
Peter Kasting <pkasting@chromium.org>
2018-01-05 18:39:16 -0800

    Test ExtensionDevToolsInfoBarDelegate.
    BUG=686285
    TEST=none
    
    Change-Id: Iac73a761fbd04b6119dfa81f17889afa26871b92
    Reviewed-on: https://chromium-review.googlesource.com/850867
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527509}
    Former-commit-id: 37db76f85745ee07d2836ce2f3ffda40dda539c6

--- chrome/browser/extensions/api/debugger/debugger_api.cc
+++ chrome/browser/extensions/api/debugger/debugger_api.cc
@@ -13,7 +13,6 @@
 #include <set>
 #include <utility>
 
-#include "base/callback_helpers.h"
 #include "base/command_line.h"
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
@@ -23,12 +22,11 @@
 #include "base/scoped_observer.h"
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/devtools/chrome_devtools_manager_delegate.h"
-#include "chrome/browser/devtools/global_confirm_info_bar.h"
 #include "chrome/browser/extensions/api/debugger/debugger_api_constants.h"
+#include "chrome/browser/extensions/api/debugger/extension_dev_tools_infobar.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_tab_util.h"
 #include "chrome/browser/infobars/infobar_service.h"
@@ -36,8 +34,6 @@
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/webui/chrome_web_ui_controller_factory.h"
 #include "chrome/common/chrome_switches.h"
-#include "chrome/grit/generated_resources.h"
-#include "components/infobars/core/confirm_infobar_delegate.h"
 #include "components/infobars/core/infobar.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/devtools_agent_host.h"
@@ -58,7 +54,6 @@
 #include "extensions/common/manifest_constants.h"
 #include "extensions/common/permissions/permissions_data.h"
 #include "extensions/common/switches.h"
-#include "ui/base/l10n/l10n_util.h"
 
 using content::DevToolsAgentHost;
 using content::RenderProcessHost;
@@ -87,155 +82,6 @@ void CopyDebuggee(Debuggee* dst, const Debuggee& src) {
     dst->extension_id.reset(new std::string(*src.extension_id));
   if (src.target_id)
     dst->target_id.reset(new std::string(*src.target_id));
-}
-
-
-// ExtensionDevToolsInfoBarDelegate -------------------------------------------
-
-class ExtensionDevToolsInfoBarDelegate : public ConfirmInfoBarDelegate {
- public:
-  ExtensionDevToolsInfoBarDelegate(const base::Closure& dismissed_callback,
-                                   const std::string& client_name);
-  ~ExtensionDevToolsInfoBarDelegate() override;
-
-  // ConfirmInfoBarDelegate:
-  Type GetInfoBarType() const override;
-  infobars::InfoBarDelegate::InfoBarIdentifier GetIdentifier() const override;
-  bool ShouldExpire(const NavigationDetails& details) const override;
-  void InfoBarDismissed() override;
-  base::string16 GetMessageText() const override;
-  int GetButtons() const override;
-  bool Cancel() override;
-
- private:
-  const base::string16 client_name_;
-  base::Closure dismissed_callback_;
-
-  DISALLOW_COPY_AND_ASSIGN(ExtensionDevToolsInfoBarDelegate);
-};
-
-ExtensionDevToolsInfoBarDelegate::ExtensionDevToolsInfoBarDelegate(
-    const base::Closure& dismissed_callback,
-    const std::string& client_name)
-    : ConfirmInfoBarDelegate(),
-      client_name_(base::UTF8ToUTF16(client_name)),
-      dismissed_callback_(dismissed_callback) {}
-
-ExtensionDevToolsInfoBarDelegate::~ExtensionDevToolsInfoBarDelegate() {
-}
-
-infobars::InfoBarDelegate::Type
-ExtensionDevToolsInfoBarDelegate::GetInfoBarType() const {
-  return WARNING_TYPE;
-}
-
-infobars::InfoBarDelegate::InfoBarIdentifier
-ExtensionDevToolsInfoBarDelegate::GetIdentifier() const {
-  return EXTENSION_DEV_TOOLS_INFOBAR_DELEGATE;
-}
-
-bool ExtensionDevToolsInfoBarDelegate::ShouldExpire(
-    const NavigationDetails& details) const {
-  return false;
-}
-
-void ExtensionDevToolsInfoBarDelegate::InfoBarDismissed() {
-  DCHECK(!dismissed_callback_.is_null());
-  // Use ResetAndReturn() since running the callback may delete |this|.
-  base::ResetAndReturn(&dismissed_callback_).Run();
-}
-
-base::string16 ExtensionDevToolsInfoBarDelegate::GetMessageText() const {
-  return l10n_util::GetStringFUTF16(IDS_DEV_TOOLS_INFOBAR_LABEL, client_name_);
-}
-
-int ExtensionDevToolsInfoBarDelegate::GetButtons() const {
-  return BUTTON_CANCEL;
-}
-
-bool ExtensionDevToolsInfoBarDelegate::Cancel() {
-  InfoBarDismissed();
-  // InfoBarDismissed() will have closed us already.
-  return false;
-}
-
-// ExtensionDevToolsInfoBar ---------------------------------------------------
-
-class ExtensionDevToolsInfoBar;
-using ExtensionInfoBars =
-    std::map<std::string, ExtensionDevToolsInfoBar*>;
-base::LazyInstance<ExtensionInfoBars>::Leaky g_extension_info_bars =
-    LAZY_INSTANCE_INITIALIZER;
-
-class ExtensionDevToolsInfoBar {
- public:
-  static ExtensionDevToolsInfoBar* Create(
-      const std::string& extension_id,
-      const std::string& extension_name,
-      ExtensionDevToolsClientHost* client_host,
-      const base::Closure& dismissed_callback);
-  void Remove(ExtensionDevToolsClientHost* client_host);
-
- private:
-  ExtensionDevToolsInfoBar(const std::string& extension_id,
-                           const std::string& extension_name);
-  ~ExtensionDevToolsInfoBar();
-  void InfoBarDismissed();
-
-  std::string extension_id_;
-  std::map<ExtensionDevToolsClientHost*, base::Closure> callbacks_;
-  base::WeakPtr<GlobalConfirmInfoBar> infobar_;
-};
-
-// static
-ExtensionDevToolsInfoBar* ExtensionDevToolsInfoBar::Create(
-      const std::string& extension_id,
-      const std::string& extension_name,
-      ExtensionDevToolsClientHost* client_host,
-      const base::Closure& dismissed_callback) {
-  ExtensionInfoBars::iterator it =
-      g_extension_info_bars.Get().find(extension_id);
-  ExtensionDevToolsInfoBar* infobar = nullptr;
-  if (it != g_extension_info_bars.Get().end())
-    infobar = it->second;
-  else
-    infobar = new ExtensionDevToolsInfoBar(extension_id, extension_name);
-  infobar->callbacks_[client_host] = dismissed_callback;
-  return infobar;
-}
-
-ExtensionDevToolsInfoBar::ExtensionDevToolsInfoBar(
-    const std::string& extension_id,
-    const std::string& extension_name)
-    : extension_id_(extension_id) {
-  g_extension_info_bars.Get()[extension_id] = this;
-
-  // This class closes the |infobar_|, so it's safe to pass Unretained(this).
-  std::unique_ptr<ExtensionDevToolsInfoBarDelegate> delegate(
-      new ExtensionDevToolsInfoBarDelegate(
-          base::Bind(&ExtensionDevToolsInfoBar::InfoBarDismissed,
-                     base::Unretained(this)),
-          extension_name));
-  infobar_ = GlobalConfirmInfoBar::Show(std::move(delegate));
-}
-
-ExtensionDevToolsInfoBar::~ExtensionDevToolsInfoBar() {
-  g_extension_info_bars.Get().erase(extension_id_);
-  if (infobar_)
-    infobar_->Close();
-}
-
-void ExtensionDevToolsInfoBar::Remove(
-    ExtensionDevToolsClientHost* client_host) {
-  callbacks_.erase(client_host);
-  if (callbacks_.empty())
-    delete this;
-}
-
-void ExtensionDevToolsInfoBar::InfoBarDismissed() {
-  std::map<ExtensionDevToolsClientHost*, base::Closure> copy = callbacks_;
-  for (const auto& pair : copy)
-    pair.second.Run();
 }
 
 }  // namespace
@@ -305,8 +151,6 @@ class ExtensionDevToolsClientHost : public content::DevToolsAgentHostClient,
 
   DISALLOW_COPY_AND_ASSIGN(ExtensionDevToolsClientHost);
 };
-
-// ExtensionDevToolsClientHost ------------------------------------------------
 
 ExtensionDevToolsClientHost::ExtensionDevToolsClientHost(
     Profile* profile,
