36c9b369f96ac743b870b5c607b2db7b2b9389f7
Angie Chiang <angiebird@google.com>
2018-01-05 18:09:14 -0800

    Correct 1:4 4:1 fwd rect txfm stage range
    Change-Id: I4268011bab2bd440f8fc667c7c1b1fe173deec7c

--- av1/encoder/av1_fwd_txfm1d_cfg.h
+++ av1/encoder/av1_fwd_txfm1d_cfg.h
@@ -214,20 +214,19 @@ static const int8_t fwd_cos_bit_row_dct_64x32[12] = { 13, 13, 12, 11, 10, 10,
 
 //  ---------------- 4x16 1D constants -----------------------
 #define fwd_shift_4x16 fwd_shift_16
-static const int8_t fwd_stage_range_row_dct_4x16[4] =
-    ARRAYOFFSET4(4, 0, 1, 2, 2);
-static const int8_t fwd_stage_range_row_adst_4x16[6] =
-    ARRAYOFFSET6(4, 0, 0, 1, 2, 2, 2);
+static const int8_t fwd_stage_range_row_dct_4x16[4] = { 4, 5, 5, 5 };
+static const int8_t fwd_stage_range_row_adst_4x16[6] = { 4, 4, 4, 5, 5, 5 };
 static const int8_t fwd_stage_range_row_idx_4x16[1] = { 5 };
 static const int8_t fwd_cos_bit_row_dct_4x16[6] = { 12, 12, 12, 12 };
 static const int8_t fwd_cos_bit_row_adst_4x16[6] = { 12, 12, 12, 12, 12, 12 };
 
 //  ---------------- 16x4 1D constants -----------------------
 #define fwd_shift_16x4 fwd_shift_16
-static const int8_t fwd_stage_range_row_dct_16x4[8] =
-    ARRAYOFFSET8(2, 0, 1, 2, 3, 4, 4, 4, 4);
-static const int8_t fwd_stage_range_row_adst_16x4[10] =
-    ARRAYOFFSET10(2, 0, 0, 1, 2, 2, 3, 3, 4, 4, 4);
+static const int8_t fwd_stage_range_row_dct_16x4[8] = {
+  2, 3, 4, 5, 5, 5, 5, 5
+};
+static const int8_t fwd_stage_range_row_adst_16x4[10] = { 2, 2, 2, 3, 3,
+                                                          4, 4, 5, 5, 5 };
 static const int8_t fwd_stage_range_row_idx_16x4[1] = { 4 };
 static const int8_t fwd_cos_bit_row_dct_16x4[8] = { 12, 12, 12, 12,
                                                     12, 12, 12, 12 };
@@ -236,10 +235,10 @@ static const int8_t fwd_cos_bit_row_adst_16x4[10] = { 12, 12, 12, 12, 12,
 
 //  ---------------- 8x32 1D constants -----------------------
 #define fwd_shift_8x32 fwd_shift_32
-static const int8_t fwd_stage_range_row_dct_8x32[6] =
-    ARRAYOFFSET6(5, 0, 1, 2, 3, 3, 3);
-static const int8_t fwd_stage_range_row_adst_8x32[8] =
-    ARRAYOFFSET8(5, 0, 0, 1, 2, 2, 3, 3, 3);
+static const int8_t fwd_stage_range_row_dct_8x32[6] = { 5, 6, 7, 7, 7, 7 };
+static const int8_t fwd_stage_range_row_adst_8x32[8] = {
+  5, 5, 5, 6, 6, 7, 7, 7
+};
 static const int8_t fwd_stage_range_row_idx_8x32[1] = { 6 };
 static const int8_t fwd_cos_bit_row_dct_8x32[6] = { 12, 12, 11, 11, 11, 11 };
 static const int8_t fwd_cos_bit_row_adst_8x32[8] = { 12, 12, 12, 12,
@@ -247,10 +246,10 @@ static const int8_t fwd_cos_bit_row_adst_8x32[8] = { 12, 12, 12, 12,
 
 //  ---------------- 32x8 1D constants -----------------------
 #define fwd_shift_32x8 fwd_shift_32
-static const int8_t fwd_stage_range_row_dct_32x8[10] =
-    ARRAYOFFSET10(3, 0, 1, 2, 3, 4, 5, 5, 5, 5, 5);
-static const int8_t fwd_stage_range_row_adst_32x8[12] =
-    ARRAYOFFSET12(3, 0, 0, 1, 2, 2, 3, 3, 4, 4, 5, 5, 5);
+static const int8_t fwd_stage_range_row_dct_32x8[10] = { 3, 4, 5, 6, 7,
+                                                         7, 7, 7, 7, 7 };
+static const int8_t fwd_stage_range_row_adst_32x8[12] = { 3, 3, 3, 4, 4, 5,
+                                                          5, 6, 6, 7, 7, 7 };
 static const int8_t fwd_stage_range_row_idx_32x8[1] = { 5 };
 static const int8_t fwd_cos_bit_row_dct_32x8[10] = { 12, 12, 12, 12, 11,
                                                      11, 11, 11, 11, 11 };
@@ -259,16 +258,17 @@ static const int8_t fwd_cos_bit_row_adst_32x8[12] = { 12, 12, 12, 12, 12, 12,
 
 //  ---------------- 16x64 1D constants -----------------------
 #define fwd_shift_16x64 fwd_shift_64
-static const int8_t fwd_stage_range_row_dct_16x64[8] =
-    ARRAYOFFSET8(6, 0, 1, 2, 3, 4, 4, 4, 4);
+static const int8_t fwd_stage_range_row_dct_16x64[8] = {
+  6, 7, 8, 9, 9, 9, 9, 9
+};
 static const int8_t fwd_stage_range_row_idx_16x64[1] = { 8 };
 static const int8_t fwd_cos_bit_row_dct_16x64[8] = { 12, 11, 10, 10,
                                                      10, 10, 10, 10 };
 
 //  ---------------- 64x16 1D constants -----------------------
 #define fwd_shift_64x16 fwd_shift_64
-static const int8_t fwd_stage_range_row_dct_64x16[12] =
-    ARRAYOFFSET12(4, 0, 1, 2, 3, 4, 5, 6, 6, 6, 6, 6, 6);
+static const int8_t fwd_stage_range_row_dct_64x16[12] = { 4, 5, 6, 7, 8, 9,
+                                                          9, 9, 9, 9, 9, 9 };
 static const int8_t fwd_stage_range_row_idx_64x16[1] = { 7 };
 static const int8_t fwd_cos_bit_row_dct_64x16[12] = { 13, 13, 12, 11, 11, 11,
                                                       11, 11, 11, 11, 11, 11 };
