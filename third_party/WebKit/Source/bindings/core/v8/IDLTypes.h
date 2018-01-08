f8aa04911aa65bea4c089ea94b0d088037bde7ae
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2018-01-08 16:31:24 +0100

    IDLTypes: Remove unused include.
    We have not used the export macros since commit 5254358b ("bindings: Adjust
    usage of CORE_EXPORT in IDLTypes and NativeValueTraits").
    
    R=jbroman,yukishiino,bashi
    
    Change-Id: Ieee09eeb493069bbde9d848f591a91751d2ed882
    Reviewed-on: https://chromium-review.googlesource.com/854174
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/master@{#527658}
    Former-commit-id: e26809fd875c10863cc6a70b6fee494c6fc10e63

--- third_party/WebKit/Source/bindings/core/v8/IDLTypes.h
+++ third_party/WebKit/Source/bindings/core/v8/IDLTypes.h
@@ -9,7 +9,6 @@
 #include "bindings/core/v8/IDLTypesBase.h"
 #include "bindings/core/v8/NativeValueTraits.h"
 #include "bindings/core/v8/V8BindingForCore.h"
-#include "core/CoreExport.h"
 #include "platform/heap/Handle.h"
 #include "platform/wtf/Optional.h"
 #include "platform/wtf/TypeTraits.h"
