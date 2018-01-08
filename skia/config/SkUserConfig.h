4c2a456937ee4cfc140f1b72b8847e1ea4df7710
Mike Reed <reed@google.com>
2018-01-08 14:06:16 -0500

    add flag for future Skia CL
    See https://skia-review.googlesource.com/c/skia/+/91940
    
    Bug: 
    Change-Id: Ia8cdef31de19537b713429a406869857383962e2
    Reviewed-on: https://chromium-review.googlesource.com/854598
    Commit-Queue: Mike Reed <reed@chromium.org>
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527717}
    Former-commit-id: d40dbc67fb321755d27360f420b6e6cc016a029e

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -224,6 +224,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_SMALLRECT_AA
 #endif
 
+#ifndef SK_SUPPORT_LEGACY_RECTMAKELARGEST
+#define SK_SUPPORT_LEGACY_RECTMAKELARGEST
+#endif
+
 #ifndef SK_SUPPORT_LEGACY_TILED_BITMAPS
 #define SK_SUPPORT_LEGACY_TILED_BITMAPS
 #endif
