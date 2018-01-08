c92834601e6fd6313d4e99c634454352ff26cc7b
Calder Kitagawa <ckitagawa@google.com>
2018-01-08 16:32:19 -0500

    [Zucchini] Remove failure code on invalid parameter status.
    Bug: 729154
    Change-Id: Ia55a3d8ba4808c9a1bee375198c427a84ee04be7
    Reviewed-on: https://chromium-review.googlesource.com/855060
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@google.com>
    Cr-Commit-Position: refs/heads/master@{#527767}
    Former-commit-id: 6d79d50c4c04e8fec326eade5b32425b86f616d4

--- chrome/installer/zucchini/zucchini_main.cc
+++ chrome/installer/zucchini/zucchini_main.cc
@@ -46,7 +46,9 @@ int main(int argc, const char* argv[]) {
   InitErrorHandling(command_line);
   zucchini::status::Code status =
       RunZucchiniCommand(command_line, std::cout, std::cerr);
-  if (status != zucchini::status::kStatusSuccess)
+  if (!(status == zucchini::status::kStatusSuccess ||
+        status == zucchini::status::kStatusInvalidParam)) {
     std::cerr << "Failed with code " << static_cast<int>(status) << std::endl;
+  }
   return static_cast<int>(status);
 }
