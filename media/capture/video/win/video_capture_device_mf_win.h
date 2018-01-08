fd8bc64142c28af354f3d7fe3a403e46ced9e0a3
Réda Housni Alaoui <alaoui.rda@gmail.com>
2018-01-08 17:46:39 +0100

    Reland: Win video capture: use IMFCaptureEngine for Media Foundation
    Fixes for reland number 3:
    - "Win10 Tester" browser_tests_functional failure
    - "Win10 Tester" capture_unittests failure
    - "Win8 Tester" browser_tests_functional failure
    - "Win8 Tester" capture_unittests failure
    
    Fixes for reland number 2:
    - "Win10 Tester" browser_tests_functional failure
    - "Win10 Tester" capture_unittests failure
    - "Win8 Tester" browser_tests_functional failure
    - "Win8 Tester" capture_unittests failure
    
    Fixes for reland number 1:
    - "Win8 Tester" browser_tests_functional failure
    - "Win8 Tester" capture_unittests failure
    
    Original description:
    - Full rewrite of the MediaFoundation implementation video part to use
    IMFCaptureEngine
    - Implementation of takePhoto, setPhotoOptions and getPhotoCapabilities
    - takePhoto triggers a still image capture with the highest available
    resolution without stopping the video stream thanks to IMFCaptureEngine
    
    TEST=adapted video_capture_device_unittest.cc and
    webrtc_image_capture_browsertest.cc; launch Chrome with
    --force-mediafoundation on Win8+ and capture video using
    e.g. https://webrtc.github.io/samples/src/content/getusermedia/gum/
    
    R=mcasas@chromium.org
    
    Bug: 730068
    Change-Id: I13fa632b25a345a20465e95a48162064eb2c4cae
    Reviewed-on: https://chromium-review.googlesource.com/734042
    Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Christian Fremerey <chfremer@chromium.org>
    Cr-Original-Original-Original-Commit-Position: refs/heads/master@{#521435}
    Reviewed-on: https://chromium-review.googlesource.com/810766
    Cr-Original-Original-Commit-Position: refs/heads/master@{#524417}
    Reviewed-on: https://chromium-review.googlesource.com/843974
    Cr-Original-Commit-Position: refs/heads/master@{#527139}
    Reviewed-on: https://chromium-review.googlesource.com/852455
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528005}
    Former-commit-id: 32c0a2e0d691e20e966974b1cb510b05ebb22b40

--- media/capture/video/win/video_capture_device_mf_win.h
+++ media/capture/video/win/video_capture_device_mf_win.h
@@ -3,12 +3,13 @@
 // found in the LICENSE file.
 
 // Windows specific implementation of VideoCaptureDevice.
-// DirectShow is used for capturing. DirectShow provide its own threads
-// for capturing.
+// MediaFoundation is used for capturing. MediaFoundation provides its own
+// threads for capturing.
 
 #ifndef MEDIA_CAPTURE_VIDEO_WIN_VIDEO_CAPTURE_DEVICE_MF_WIN_H_
 #define MEDIA_CAPTURE_VIDEO_WIN_VIDEO_CAPTURE_DEVICE_MF_WIN_H_
 
+#include <mfcaptureengine.h>
 #include <mfidl.h>
 #include <mfreadwrite.h>
 #include <stdint.h>
@@ -16,9 +17,9 @@
 
 #include <vector>
 
+#include "base/callback_forward.h"
 #include "base/macros.h"
 #include "base/sequence_checker.h"
-#include "base/synchronization/lock.h"
 #include "media/capture/capture_export.h"
 #include "media/capture/video/video_capture_device.h"
 
@@ -30,10 +31,7 @@ class Location;
 
 namespace media {
 
-class MFReaderCallback;
-
-const DWORD kFirstVideoStream =
-    static_cast<DWORD>(MF_SOURCE_READER_FIRST_VIDEO_STREAM);
+class MFVideoCallback;
 
 class CAPTURE_EXPORT VideoCaptureDeviceMFWin : public VideoCaptureDevice {
  public:
@@ -51,6 +49,10 @@ class CAPTURE_EXPORT VideoCaptureDeviceMFWin : public VideoCaptureDevice {
       const VideoCaptureParams& params,
       std::unique_ptr<VideoCaptureDevice::Client> client) override;
   void StopAndDeAllocate() override;
+  void TakePhoto(TakePhotoCallback callback) override;
+  void GetPhotoState(GetPhotoStateCallback callback) override;
+  void SetPhotoOptions(mojom::PhotoSettingsPtr settings,
+                       SetPhotoOptionsCallback callback) override;
 
   // Captured new video data.
   void OnIncomingCapturedData(const uint8_t* data,
@@ -58,19 +60,43 @@ class CAPTURE_EXPORT VideoCaptureDeviceMFWin : public VideoCaptureDevice {
                               int rotation,
                               base::TimeTicks reference_time,
                               base::TimeDelta timestamp);
+  void OnEvent(IMFMediaEvent* media_event);
+
+  using CreateMFPhotoCallbackCB =
+      base::RepeatingCallback<scoped_refptr<IMFCaptureEngineOnSampleCallback>(
+          VideoCaptureDevice::TakePhotoCallback callback,
+          VideoCaptureFormat format)>;
+
+  bool get_use_photo_stream_to_take_photo_for_testing() {
+    return use_photo_stream_to_take_photo_;
+  }
+
+  void set_create_mf_photo_callback_for_testing(CreateMFPhotoCallbackCB cb) {
+    create_mf_photo_callback_ = cb;
+  }
 
  private:
   void OnError(const base::Location& from_here, HRESULT hr);
 
   VideoCaptureDeviceDescriptor descriptor_;
-  Microsoft::WRL::ComPtr<IMFActivate> device_;
-  scoped_refptr<MFReaderCallback> callback_;
+  CreateMFPhotoCallbackCB create_mf_photo_callback_;
+  scoped_refptr<MFVideoCallback> video_callback_;
 
-  base::Lock lock_;  // Used to guard the below variables.
+  // Guards the below variables from concurrent access between methods running
+  // on |sequence_checker_| and calls to OnIncomingCapturedData() and OnEvent()
+  // made by MediaFoundation on threads outside of our control.
+  base::Lock lock_;
+
   std::unique_ptr<VideoCaptureDevice::Client> client_;
-  Microsoft::WRL::ComPtr<IMFSourceReader> reader_;
-  VideoCaptureFormat capture_format_;
-  bool capture_;
+  Microsoft::WRL::ComPtr<IMFCaptureEngine> engine_;
+  const GUID sink_mf_video_format_;
+  const VideoPixelFormat sink_video_pixel_format_;
+  VideoCaptureFormat sink_capture_video_format_;
+  bool is_started_;
+  DWORD video_stream_index_;
+  DWORD photo_stream_index_;
+  bool use_photo_stream_to_take_photo_;
+  base::queue<TakePhotoCallback> video_stream_take_photo_callbacks_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
