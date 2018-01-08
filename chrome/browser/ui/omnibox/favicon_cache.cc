baa38dee94967fb06317cfc81da91f74ea15197f
Tommy C. Li <tommycli@chromium.org>
2018-01-08 15:19:56 -0800

    Omnibox UI Experiments: For FaviconCache, coalesce multiple requests.
    This CL prevents FaviconCache from hitting the underlying (uncached)
    FaviconService multiple times for the same page URL. Instead it keep
    track of its pending requests and add a callback to be called when
    the original request (for the same page URL) returns.
    
    Bug: 798490
    Change-Id: Ic8104c7ef5c48facec30270b3f006e70a0030901
    Reviewed-on: https://chromium-review.googlesource.com/853213
    Reviewed-by: Kevin Bailey <krb@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527831}
    Former-commit-id: 7bddca5ed13672ceac37c9ac315d8976fe2d364e

--- chrome/browser/ui/omnibox/favicon_cache.cc
+++ chrome/browser/ui/omnibox/favicon_cache.cc
@@ -37,26 +37,39 @@ gfx::Image FaviconCache::GetFaviconForPageUrl(
   if (!favicon_service_)
     return gfx::Image();
 
+  // We have an outstanding request for this page. Add one more waiting callback
+  // and return an empty gfx::Image.
+  auto it = pending_requests_.find(page_url);
+  if (it != pending_requests_.end()) {
+    it->second.push_back(std::move(on_favicon_fetched));
+    return gfx::Image();
+  }
+
   // TODO(tommycli): Investigate using the version of this method that specifies
   // the desired size.
   favicon_service_->GetFaviconImageForPageURL(
       page_url,
       base::BindRepeating(&FaviconCache::OnFaviconFetched,
-                          weak_factory_.GetWeakPtr(), page_url,
-                          base::Passed(std::move(on_favicon_fetched))),
+                          weak_factory_.GetWeakPtr(), page_url),
       &task_tracker_);
+  pending_requests_[page_url].push_back(std::move(on_favicon_fetched));
 
   return gfx::Image();
 }
 
 void FaviconCache::OnFaviconFetched(
     const GURL& page_url,
-    FaviconFetchedCallback on_favicon_fetched,
     const favicon_base::FaviconImageResult& result) {
+  // TODO(tommycli): Also cache null image results with a reasonable expiry.
   if (result.image.IsEmpty())
     return;
 
   mru_cache_.Put(page_url, result.image);
 
-  std::move(on_favicon_fetched).Run(result.image);
+  auto it = pending_requests_.find(page_url);
+  DCHECK(it != pending_requests_.end());
+  for (auto& callback : it->second) {
+    std::move(callback).Run(result.image);
+  }
+  pending_requests_.erase(it);
 }
