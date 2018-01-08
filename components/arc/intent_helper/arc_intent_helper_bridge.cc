ca10626eea3db2020c6a0255bf35316ec46aad40
David Jacobo <djacobo@chromium.org>
2018-01-08 13:17:33 -0800

    Remove 'geo' as a valid scheme for ArcIntentHelper
    On ARC context, 'geo' scheme is useful to build intents that works with
    Maps, however if there is a need to redirect an intent with such scheme
    to Chrome, a "https://maps.google.com/maps?" prefix is added, so there
    is no need for this extra scheme on the Chrome side.
    
    More info on 'geo' scheme
    https://developers.google.com/maps/documentation/urls/android-intents
    
    Bug: None
    Test: Build
    Change-Id: Ib2818bdbc76c9fbd16e8e3b2de1e7e4f09a46d0f
    Reviewed-on: https://chromium-review.googlesource.com/852822
    Commit-Queue: David Jacobo <djacobo@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527782}
    Former-commit-id: fdb2d0e896da3c9ed688abba611ce8630d42f58f

--- components/arc/intent_helper/arc_intent_helper_bridge.cc
+++ components/arc/intent_helper/arc_intent_helper_bridge.cc
@@ -61,12 +61,8 @@ constexpr std::pair<mojom::ChromePage, const char*> kMapping[] = {
     {mojom::ChromePage::ABOUTDOWNLOADS, "about:downloads"},
     {mojom::ChromePage::ABOUTHISTORY, "about:history"}};
 
-// TODO(djacobo): Propose geo: as a constant.
-constexpr const char* kArcSchemes[] = {url::kHttpScheme,
-                                       url::kHttpsScheme,
-                                       url::kContentScheme,
-                                       url::kFileScheme,
-                                       "geo",
+constexpr const char* kArcSchemes[] = {url::kHttpScheme, url::kHttpsScheme,
+                                       url::kContentScheme, url::kFileScheme,
                                        url::kMailToScheme};
 
 // mojom::ChromePage::LAST returns the ammout of valid entries - 1.
