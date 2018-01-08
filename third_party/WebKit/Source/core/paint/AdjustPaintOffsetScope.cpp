e16b3fed079b4b0ec6e8b3a87e363c9a824b7635
Xianzhu Wang <wangxianzhu@chromium.org>
2018-01-08 14:41:47 -0800

    [SPv175+] Generate fragments for repeating table header/footer
    - When calculating the bounding box of a repeating table header/footer
      in flow thread, expand the box to cover the original and all
      repeatings, so that we'll generate fragments for the repeatings.
    
    - After fragments are generated, calculate a paint offset adjustment
      for each fragment in order to move each repeatings to its proper
      place in the fragment.
    
    - The bounding box and paint offset adjustment will be inherited by
      descendants so that they will also generate multiple fragments and
      adjust paint offset of their repeatings.
    
    - Use the paint offsets in fragments instead of the SPv1 paint offset
      during painting. All of the repeatings will be painted by the
      current fragment painting code, so the old repeating code is skipped
      for SPv175+.
    
    Bug: 797439
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I32fb9853cc0684ff90d552238938dc6ac8f4de47
    Reviewed-on: https://chromium-review.googlesource.com/846504
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#527827}
    Former-commit-id: 24176bfdc7b759743c6eacf04aaece861d92f483

--- third_party/WebKit/Source/core/paint/AdjustPaintOffsetScope.cpp
+++ third_party/WebKit/Source/core/paint/AdjustPaintOffsetScope.cpp
@@ -4,34 +4,53 @@
 
 #include "core/paint/AdjustPaintOffsetScope.h"
 
+#include "core/layout/LayoutTableSection.h"
+
 namespace blink {
 
-bool AdjustPaintOffsetScope::AdjustForPaintOffsetTranslation(
-    const LayoutBox& box) {
+bool AdjustPaintOffsetScope::AdjustPaintOffset(const LayoutBox& box) {
   DCHECK(RuntimeEnabledFeatures::SlimmingPaintV175Enabled());
 
   if (box.HasSelfPaintingLayer())
     return false;
 
+  // TODO(wangxianzhu): Expose fragment so that the client doesn't need to
+  // call FragmentToPaint() again when needed.
   const auto* fragment = old_paint_info_.FragmentToPaint(box);
-  if (!fragment)
+  if (!fragment) {
+    // TODO(wangxianzhu): The client should know the case and bail out of
+    // painting of itself.
     return false;
+  }
+
   const auto* paint_properties = fragment->PaintProperties();
-  if (!paint_properties || !paint_properties->PaintOffsetTranslation())
-    return false;
+  if (paint_properties && paint_properties->PaintOffsetTranslation()) {
+    DCHECK(fragment->LocalBorderBoxProperties());
+    contents_properties_.emplace(
+        old_paint_info_.context.GetPaintController(),
+        *fragment->LocalBorderBoxProperties(), box,
+        DisplayItem::PaintPhaseToDrawingType(old_paint_info_.phase));
 
-  DCHECK(fragment->LocalBorderBoxProperties());
-  contents_properties_.emplace(
-      old_paint_info_.context.GetPaintController(),
-      *fragment->LocalBorderBoxProperties(), box,
-      DisplayItem::PaintPhaseToDrawingType(old_paint_info_.phase));
+    new_paint_info_.emplace(old_paint_info_);
+    new_paint_info_->UpdateCullRect(paint_properties->PaintOffsetTranslation()
+                                        ->Matrix()
+                                        .ToAffineTransform());
 
-  new_paint_info_.emplace(old_paint_info_);
-  new_paint_info_->UpdateCullRect(
-      paint_properties->PaintOffsetTranslation()->Matrix().ToAffineTransform());
+    adjusted_paint_offset_ = fragment->PaintOffset();
+    return true;
+  }
 
-  adjusted_paint_offset_ = box.FirstFragment().PaintOffset();
-  return true;
+  if (box.IsTableSection()) {
+    const auto& section = ToLayoutTableSection(box);
+    if (section.IsRepeatingHeaderGroup() || section.IsRepeatingFooterGroup()) {
+      adjusted_paint_offset_ = fragment->PaintOffset();
+      return true;
+    }
+  }
+
+  // TODO(wangxianzhu): Use fragment->PaintOffset() for all cases and eliminate
+  // the paint_offset parameter of paint methods.
+  return false;
 }
 
 }  // namespace blink
--- third_party/WebKit/Source/core/paint/PaintPropertyTreeBuilder.h
+++ third_party/WebKit/Source/core/paint/PaintPropertyTreeBuilder.h
@@ -101,6 +101,11 @@ struct PaintPropertyTreeBuilderFragmentContext {
   // If the object is fragmented, it will have a fragment clip.
   Optional<LayoutRect> fragment_clip;
   LayoutUnit logical_top_in_flow_thread;
+
+  // A repeating object paints at multiple places in the flow thread, once in
+  // each fragment. The repeated paintings need to add an adjustment to the
+  // calculated paint offset to paint at the desired place.
+  LayoutSize repeating_paint_offset_adjustment;
 };
 
 struct PaintPropertyTreeBuilderContext {
@@ -131,6 +136,16 @@ struct PaintPropertyTreeBuilderContext {
 #endif
 
   PaintLayer* painting_layer = nullptr;
+
+  // In a fragmented context, some objects (e.g. repeating table headers and
+  // footers, fixed-position objects in paged media, and their descendants in
+  // paint order) repeatly paint in all fragments after the fragment where the
+  // object first appears.
+  bool is_repeating_in_fragments = false;
+
+  // The physical bounding box of all appearances of the repeating object
+  // in the flow thread.
+  LayoutRect repeating_bounding_box_in_flow_thread;
 };
 
 // |FrameViewPaintPropertyTreeBuilder| and |ObjectPaintPropertyTreeBuilder|
@@ -169,6 +184,9 @@ class ObjectPaintPropertyTreeBuilder {
   ALWAYS_INLINE void UpdateCompositedLayerPaginationOffset();
   ALWAYS_INLINE void UpdateFragments();
   ALWAYS_INLINE void UpdatePaintingLayer();
+  ALWAYS_INLINE void UpdateRepeatingPaintOffsetAdjustment();
+  ALWAYS_INLINE void UpdateRepeatingTableHeaderPaintOffsetAdjustment();
+  ALWAYS_INLINE void UpdateRepeatingTableFooterPaintOffsetAdjustment();
 
   const LayoutObject& object_;
   PaintPropertyTreeBuilderContext& context_;
