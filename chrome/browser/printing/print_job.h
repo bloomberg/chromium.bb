d4ca143ef62cfa39971655bef3f5f78cf2b6b60a
Daniel Bratell <bratell@opera.com>
2018-01-09 17:11:33 +0100

    Deduplicate printing HoldRefCallback helper
    Both print_job.cc and print_job_worker.cc wanted a helper
    function for certain callbacks. Rather than having two of them,
    let us have one.
    
    Bug: 746957
    Change-Id: If770b8f18479847489d9c2a28ba1b1d75bdc11e1
    Reviewed-on: https://chromium-review.googlesource.com/857501
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528083}
    Former-commit-id: 230062bad12dcf14ff1c1592c67e9e620a62f19b

--- chrome/browser/printing/print_job.h
+++ chrome/browser/printing/print_job.h
@@ -24,11 +24,15 @@ namespace printing {
 class JobEventDetails;
 class MetafilePlayer;
 class PrintJobWorker;
+class PrintJobWorkerOwner;
 class PrintedDocument;
 #if defined(OS_WIN)
 class PrintedPage;
 #endif
 class PrinterQuery;
+
+void HoldRefCallback(const scoped_refptr<PrintJobWorkerOwner>& owner,
+                     const base::Closure& callback);
 
 // Manages the print work for a specific document. Talks to the printer through
 // PrintingContext through PrintJobWorker. Hides access to PrintingContext in a
