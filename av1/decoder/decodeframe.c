51a2bf38363ee11b568fc6d3fbb7398e64126d92
Yue Chen <yuec@google.com>
2018-01-08 17:47:12 -0800

    Fix a missing variable to make mismatch_debug work
    Change-Id: I19e59dba2644127f8e26cf4815c55dac693b5641

--- av1/decoder/decodeframe.c
+++ av1/decoder/decodeframe.c
@@ -250,6 +250,7 @@ static void decode_reconstruct_tx(AV1_COMMON *cm, MACROBLOCKD *const xd,
                             max_scan_line, eob, cm->reduced_tx_set_used);
 #if CONFIG_MISMATCH_DEBUG
     int pixel_c, pixel_r;
+    BLOCK_SIZE bsize = txsize_to_bsize[tx_size];
     int blk_w = block_size_wide[bsize];
     int blk_h = block_size_high[bsize];
     mi_to_pixel_loc(&pixel_c, &pixel_r, mi_col, mi_row, blk_col, blk_row,
