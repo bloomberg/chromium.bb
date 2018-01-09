006d8d7cbb6cc0ce147bdeeba6a0efd1233c61b9
Lalit Maganti <lalitm@chromium.org>
2018-01-09 17:48:14 +0000

    task-manager: use all available data about memory of processes
    Even if the global dump's successs is false (which may happen if a child process is
    stuck, times out, disconnects or crashes), data about the other processes
    is still valid and can be displayed.
    
    Change the code to check if the dump is not null rather than checking
    for global success.
    
    Bug: 799537
    Change-Id: I9686b304e493529b13b2839e0cabb742e2d6280f
    Reviewed-on: https://chromium-review.googlesource.com/857466
    Commit-Queue: Lalit Maganti <lalitm@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528098}
    Former-commit-id: 7651526064768b668d0d3a4a7d21fb8179200360

--- chrome/browser/task_manager/sampling/task_manager_impl.cc
+++ chrome/browser/task_manager/sampling/task_manager_impl.cc
@@ -517,7 +517,11 @@ void TaskManagerImpl::OnReceivedMemoryDump(
     bool success,
     memory_instrumentation::mojom::GlobalMemoryDumpPtr dump) {
   waiting_for_memory_dump_ = false;
-  if (!success)
+  // We can ignore the value of success as it is a coarse grained indicator
+  // of whether the global dump was successful; usually because of a missing
+  // process or OS dumps. There may still be useful information for other
+  // processes in the global dump when success is false.
+  if (!dump)
     return;
   for (const memory_instrumentation::mojom::ProcessMemoryDumpPtr& pmd :
        dump->process_dumps) {
