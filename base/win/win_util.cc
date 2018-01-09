6b9476054156b92a949a82be47f633c147d3854b
Alexis Menard <alexis.menard@intel.com>
2018-01-09 10:18:48 -0800

    [Windows] Do not take into account Windows tablet mode when computing interaction MQs.
    The interaction media queries are about actual physical input methods.
    Windows 8 and 10 allows the user to manually trigger the tablet mode
    (settings or taskbar) but it doesn't reflect the actual input
    configuration of the device.
    
    This patch modifies isTabletDevice by extracting the code that
    is looking at the actual hardware configuration of the device
    and the tablet mode. The split is needed because IsTabletDevice
    is used by other features in Chrome so I kept the same behavior
    (IsTabletDevice is still calling the newly added method). Only the
    MQ code is now using the new method which only checks
    the hardware configuration to figure out what kind of pointer devices
    the machine has.
    
    This will avoid inconsistencies when we detect the hardware change
    and the fact that the tablet mode has been switched on/off.
    
    BUG=778845
    
    Change-Id: I13e7d4a2891c38466ed05079fb5d1aa6d4f3e35e
    Reviewed-on: https://chromium-review.googlesource.com/809811
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Alexis Menard <alexis.menard@intel.com>
    Cr-Commit-Position: refs/heads/master@{#528127}
    Former-commit-id: f3d67d226996b866053a711f59cdd465b363a63a

--- base/win/win_util.cc
+++ base/win/win_util.cc
@@ -458,6 +458,21 @@ bool IsTabletDevice(std::string* reason, HWND hwnd) {
   if (IsWindows10TabletMode(hwnd))
     return true;
 
+  return IsDeviceUsedAsATablet(reason);
+}
+
+// This method is used to set the right interactions media queries,
+// see https://drafts.csswg.org/mediaqueries-4/#mf-interaction. It doesn't
+// check the Windows 10 tablet mode because it doesn't reflect the actual
+// input configuration of the device and can be manually triggered by the user
+// independently from the hardware state.
+bool IsDeviceUsedAsATablet(std::string* reason) {
+  if (GetVersion() < VERSION_WIN8) {
+    if (reason)
+      *reason = "Tablet device detection not supported below Windows 8\n";
+    return false;
+  }
+
   if (GetSystemMetrics(SM_MAXIMUMTOUCHES) == 0) {
     if (reason) {
       *reason += "Device does not support touch.\n";
@@ -485,22 +500,16 @@ bool IsTabletDevice(std::string* reason, HWND hwnd) {
           GetModuleHandle(L"user32.dll"), "GetAutoRotationState"));
 
   if (get_auto_rotation_state_func) {
-    AR_STATE rotation_state;
-    ZeroMemory(&rotation_state, sizeof(AR_STATE));
-    if (get_auto_rotation_state_func(&rotation_state)) {
-      if ((rotation_state & AR_NOT_SUPPORTED) || (rotation_state & AR_LAPTOP) ||
-          (rotation_state & AR_NOSENSOR))
-        return false;
-    }
+    AR_STATE rotation_state = AR_ENABLED;
+    if (get_auto_rotation_state_func(&rotation_state) &&
+        (rotation_state & (AR_NOT_SUPPORTED | AR_LAPTOP | AR_NOSENSOR)) != 0)
+      return false;
   }
 
   // PlatformRoleSlate was added in Windows 8+.
   POWER_PLATFORM_ROLE role = GetPlatformRole();
-  bool mobile_power_profile = (role == PlatformRoleMobile);
-  bool slate_power_profile = (role == PlatformRoleSlate);
-
   bool is_tablet = false;
-  if (mobile_power_profile || slate_power_profile) {
+  if (role == PlatformRoleMobile || role == PlatformRoleSlate) {
     is_tablet = !GetSystemMetrics(SM_CONVERTIBLESLATEMODE);
     if (!is_tablet) {
       if (reason) {
--- base/win/win_util.h
+++ base/win/win_util.h
@@ -144,16 +144,27 @@ BASE_EXPORT void SetAbortBehaviorForCrashReporting();
 BASE_EXPORT bool IsWindows10TabletMode(HWND hwnd);
 
 // A tablet is a device that is touch enabled and also is being used
-// "like a tablet". This is used by the following:-
-// 1. Metrics:- To gain insight into how users use Chrome.
-// 2. Physical keyboard presence :- If a device is in tablet mode, it means
+// "like a tablet". This is used by the following:
+// 1. Metrics: To gain insight into how users use Chrome.
+// 2. Physical keyboard presence: If a device is in tablet mode, it means
 //    that there is no physical keyboard attached.
-// 3. To set the right interactions media queries,
-//    see https://drafts.csswg.org/mediaqueries-4/#mf-interaction
 // This function optionally sets the |reason| parameter to determine as to why
 // or why not a device was deemed to be a tablet.
-// Returns true if the device is in tablet mode.
+// Returns true if the user has set Windows 10 in tablet mode.
 BASE_EXPORT bool IsTabletDevice(std::string* reason, HWND hwnd);
+
+// Return true if the device is physically used as a tablet independently of
+// Windows tablet mode. It checks if the device:
+// - Is running Windows 8 or newer,
+// - Has a touch digitizer,
+// - Is not docked,
+// - Has a supported rotation sensor,
+// - Is not in laptop mode,
+// - prefers the mobile or slate power management profile (per OEM choice), and
+// - Is in slate mode.
+// This function optionally sets the |reason| parameter to determine as to why
+// or why not a device was deemed to be a tablet.
+BASE_EXPORT bool IsDeviceUsedAsATablet(std::string* reason);
 
 // A slate is a touch device that may have a keyboard attached. This function
 // returns true if a keyboard is attached and optionally will set the |reason|
