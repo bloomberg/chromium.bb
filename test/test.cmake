wjdz99/aom

Port EC test from Daala.
This currently only ports a small portion - the random encode/decode mixing
CDFs and bools, plus the patch initial bits tests.

The tests will break with a different CDF_PROB_TOP, as well as a different
CDF_PROB_TOP between bools and CDFs.

Change-Id: Ia289c6afd70cbc7dde5ed7a9c7c42f219c29935d

test/test.cmake
@@ -33,7 +33,8 @@ set(AOM_UNIT_TEST_COMMON_SOURCES
     "${AOM_ROOT}/test/register_state_check.h"
     "${AOM_ROOT}/test/transform_test_base.h"
     "${AOM_ROOT}/test/util.h"
-    "${AOM_ROOT}/test/video_source.h")
+    "${AOM_ROOT}/test/video_source.h"
+    "${AOM_ROOT}/test/ec_test.cc")
 
 if (NOT BUILD_SHARED_LIBS)
   set(AOM_UNIT_TEST_COMMON_SOURCES

bloomberg/chromium.bb

test/test.cmake
@@ -293,7 +293,8 @@ if (NOT BUILD_SHARED_LIBS)
     set(AOM_UNIT_TEST_COMMON_SOURCES
         ${AOM_UNIT_TEST_COMMON_SOURCES}
         "${AOM_ROOT}/test/binary_codes_test.cc"
-        "${AOM_ROOT}/test/boolcoder_test.cc")
+        "${AOM_ROOT}/test/boolcoder_test.cc"
+        "${AOM_ROOT}/test/ec_test.cc")
 
     if (CONFIG_EXT_TILE)
       set(AOM_UNIT_TEST_COMMON_SOURCES
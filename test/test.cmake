b06670782c30ee607458848cff639607c10c9f7f
Thomas Daede <tdaede@mozilla.com>
2017-11-28 16:03:46 -0800

    Port EC test from Daala.
    This currently only ports a small portion - the random encode/decode mixing
    CDFs and bools, plus the patch initial bits tests.
    
    The tests will break with a different CDF_PROB_TOP, as well as a different
    CDF_PROB_TOP between bools and CDFs.
    
    Change-Id: Ia289c6afd70cbc7dde5ed7a9c7c42f219c29935d

--- test/test.cmake
+++ test/test.cmake
@@ -293,7 +293,8 @@ if (NOT BUILD_SHARED_LIBS)
     set(AOM_UNIT_TEST_COMMON_SOURCES
         ${AOM_UNIT_TEST_COMMON_SOURCES}
         "${AOM_ROOT}/test/binary_codes_test.cc"
-        "${AOM_ROOT}/test/boolcoder_test.cc")
+        "${AOM_ROOT}/test/boolcoder_test.cc"
+        "${AOM_ROOT}/test/ec_test.cc")
 
     if (CONFIG_EXT_TILE)
       set(AOM_UNIT_TEST_COMMON_SOURCES
