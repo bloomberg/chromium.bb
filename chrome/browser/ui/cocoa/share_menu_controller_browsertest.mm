48652719e6b54c8acb74b6042f10b07028b5101b
Elly Fong-Jones <ellyjones@chromium.org>
2018-01-09 10:26:50 -0500

    cocoa: fix browser_tests build with 10.13 sdk
    These arguments became nonnull in 10.13, since they're used as out parameters.
    
    Bug: None
    Change-Id: I298bc2e04dbb7eae67512b2a2c4d3f24ecf35502
    Reviewed-on: https://chromium-review.googlesource.com/857416
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528004}
    Former-commit-id: ffce62f681e72a5ec20243eb3c1cb798f8c79b0a

--- chrome/browser/ui/cocoa/share_menu_controller_browsertest.mm
+++ chrome/browser/ui/cocoa/share_menu_controller_browsertest.mm
@@ -227,15 +227,20 @@ IN_PROC_BROWSER_TEST_F(ShareMenuControllerTest, MenuHasKeyEquivalent) {
   EXPECT_EQ([menu numberOfItems], 0);
   NSEvent* event = cocoa_test_event_utils::KeyEventWithKeyCode(
       'i', 'i', NSKeyDown, NSCommandKeyMask | NSShiftKeyMask);
+  id ignored_target;
+  SEL ignored_action;
   EXPECT_FALSE([controller_ menuHasKeyEquivalent:menu
                                         forEvent:event
-                                          target:nil
-                                          action:nil]);
+                                          target:&ignored_target
+                                          action:&ignored_action]);
   EXPECT_GT([menu numberOfItems], 0);
 
   NSMenuItem* item = [menu itemAtIndex:0];
   // |menuHasKeyEquivalent:....| shouldn't populate the menu after the first
   // time.
-  [controller_ menuHasKeyEquivalent:menu forEvent:event target:nil action:nil];
+  [controller_ menuHasKeyEquivalent:menu
+                           forEvent:event
+                             target:&ignored_target
+                             action:&ignored_action];
   EXPECT_EQ(item, [menu itemAtIndex:0]);  // Pointer equality intended.
 }
