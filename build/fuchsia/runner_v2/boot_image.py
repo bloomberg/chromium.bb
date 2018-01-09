289fde43d849d88f02019423c63fe7802f34df6a
Kevin Marshall <kmarshall@chromium.org>
2018-01-09 11:23:38 -0800

    Initial implementation of new SSH-based Fuchsia runner libraries.
    The new runner is based heavily around the use of SSH and SCP.
    Rather than build large boot filesystems with Chrome or test binaries,
    as we do now, we will instantiate a basic Fuchsia instance and use
    SSH for our binary deployment, execution, and log extraction needs.
    It can also be used as a layer to facilitate connections between the
    client and server portions of the Blink layout test system.
    
    This CL defines the abstraction layer for connecting and interacting
    with a Fuchsia target, and implementation code for using it on QEMU.
    
    
    Bug: 799309
    Change-Id: Ia2613c36d1a6f10457aa9c90435d4c95f9721e6c
    Reviewed-on: https://chromium-review.googlesource.com/851141
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528179}
    Former-commit-id: 29eea977b114eeebf54808cc1fcaa5107bd15f7b

---
+++ build/fuchsia/runner_v2/boot_image.py
@@ -0,0 +1,99 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Functions used to provision Fuchsia boot images."""
+
+import common
+import os
+import subprocess
+import tempfile
+
+_SSH_CONFIG_TEMPLATE = """
+Host *
+  CheckHostIP no
+  StrictHostKeyChecking no
+  ForwardAgent no
+  ForwardX11 no
+  GSSAPIDelegateCredentials no
+  UserKnownHostsFile {known_hosts}
+  User fuchsia
+  IdentitiesOnly yes
+  IdentityFile {identity}
+  ControlPersist yes
+  ControlMaster auto
+  ControlPath /tmp/fuchsia--%r@%h:%p
+  ServerAliveInterval 1
+  ServerAliveCountMax 1"""
+
+
+def _GetKernelPath(target_arch):
+  return os.path.join(_TargetCpuToSdkBinPath(target_arch), 'zircon.bin')
+
+
+def _TargetCpuToSdkBinPath(target_arch):
+  """Returns the path to the kernel & bootfs .bin files for |target_cpu|."""
+  return os.path.join(common.SDK_ROOT, 'target', target_arch)
+
+
+def _ProvisionSSH(output_dir):
+  """Provisions the key files used by the SSH daemon, and generates a
+  configuration file used by clients for connecting to SSH.
+
+  Returns a tuple with:
+  #0: the client configuration file
+  #1: a list of file path pairs: (<path in image>, <path on build filesystem>).
+  """
+
+  host_key_path = output_dir + '/ssh_key'
+  host_pubkey_path = host_key_path + '.pub'
+  id_key_path = output_dir + '/id_ed25519'
+  id_pubkey_path = id_key_path + '.pub'
+  known_hosts_path = output_dir + '/known_hosts'
+  ssh_config_path = output_dir + '/ssh_config'
+
+  if not os.path.isfile(host_key_path):
+    subprocess.check_call(['ssh-keygen', '-t', 'ed25519', '-h', '-f',
+                           host_key_path, '-P', '', '-N', ''],
+                          stdout=open(os.devnull))
+  if not os.path.isfile(id_key_path):
+    subprocess.check_call(['ssh-keygen', '-t', 'ed25519', '-f', id_key_path,
+                           '-P', '', '-N', ''], stdout=open(os.devnull))
+
+  with open(ssh_config_path, "w") as ssh_config:
+    ssh_config.write(
+        _SSH_CONFIG_TEMPLATE.format(identity=id_key_path,
+                                    known_hosts=known_hosts_path))
+
+  return (
+      ssh_config_path,
+      (('data/ssh/ssh_host_ed25519_key', host_key_path),
+       ('data/ssh/ssh_host_ed25519_key.pub', host_pubkey_path),
+       ('data/ssh/authorized_keys', id_pubkey_path))
+  )
+
+
+def CreateBootFS(output_dir, target_arch):
+  """Creates a bootfs image provisoned with the credentials necessary
+  for SSH remote access.
+
+  Returns a tuple with the path to SSH config and the path to the boot
+  image."""
+
+  boot_image = os.path.join(
+      _TargetCpuToSdkBinPath(target_arch), 'bootdata.bin')
+  ssh_manifest = tempfile.NamedTemporaryFile(delete=False)
+  ssh_config, ssh_data = _ProvisionSSH(output_dir)
+  for key, val in ssh_data:
+    ssh_manifest.write("%s=%s\n" % (key, val))
+  ssh_manifest.close()
+  mkbootfs_path = os.path.join(common.SDK_ROOT, 'tools', 'mkbootfs')
+  bootfs_name = output_dir + '/image.bootfs'
+  args = [mkbootfs_path, '-o', bootfs_name,
+          '--target=boot', boot_image,
+          '--target=system', ssh_manifest.name]
+
+  subprocess.check_call(args)
+  os.remove(ssh_manifest.name)
+
+  return ssh_config, bootfs_name
---
+++ build/fuchsia/runner_v2/common.py
@@ -0,0 +1,10 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+
+DIR_SOURCE_ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, os.pardir))
+SDK_ROOT = os.path.join(DIR_SOURCE_ROOT, 'third_party', 'fuchsia-sdk')
+
