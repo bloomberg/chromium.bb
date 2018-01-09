936fef94d6d1ac02aa1e901d8838254b4208d976
Kim Paulhamus <kpaulhamus@chromium.org>
2018-01-09 08:12:44 -0800

    Restructure responses from authenticator.mojom.
    This is prep work to make way for adding the call to get an assertion
    from an authenticator device.
    
    Bug: 664630
    Change-Id: I931c392b2fa523b4c05da7e6682f4c3964c5bda7
    Reviewed-on: https://chromium-review.googlesource.com/830525
    Commit-Queue: Kim Paulhamus <kpaulhamus@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528136}
    Former-commit-id: e549f84ddae929cc1d91fd5dfb3277e33c04206b

--- content/browser/webauth/authenticator_impl.cc
+++ content/browser/webauth/authenticator_impl.cc
@@ -6,6 +6,7 @@
 
 #include <string>
 #include <utility>
+#include <vector>
 
 #include "base/logging.h"
 #include "base/timer/timer.h"
@@ -33,20 +34,20 @@ bool HasValidAlgorithm(
   return false;
 }
 
-webauth::mojom::PublicKeyCredentialInfoPtr CreatePublicKeyCredentialInfo(
-    CollectedClientData client_data,
-    device::RegisterResponseData response_data) {
-  auto credential_info = webauth::mojom::PublicKeyCredentialInfo::New();
+webauth::mojom::MakeCredentialAuthenticatorResponsePtr
+CreateMakeCredentialResponse(CollectedClientData client_data,
+                             device::RegisterResponseData response_data) {
+  auto response = webauth::mojom::MakeCredentialAuthenticatorResponse::New();
+  auto common_info = webauth::mojom::CommonCredentialInfo::New();
   std::string client_data_json = client_data.SerializeToJson();
-  credential_info->client_data_json.assign(client_data_json.begin(),
-                                           client_data_json.end());
-  credential_info->raw_id = response_data.raw_id();
-  credential_info->id = response_data.GetId();
-  auto response = webauth::mojom::AuthenticatorResponse::New();
+  common_info->client_data_json.assign(client_data_json.begin(),
+                                       client_data_json.end());
+  common_info->raw_id = response_data.raw_id();
+  common_info->id = response_data.GetId();
+  response->info = std::move(common_info);
   response->attestation_object =
       response_data.GetCBOREncodedAttestationObject();
-  credential_info->response = std::move(response);
-  return credential_info;
+  return response;
 }
 }  // namespace
 
@@ -186,8 +187,8 @@ void AuthenticatorImpl::OnRegisterResponse(
       DCHECK(response_data.has_value());
       std::move(make_credential_response_callback_)
           .Run(webauth::mojom::AuthenticatorStatus::SUCCESS,
-               CreatePublicKeyCredentialInfo(std::move(client_data_),
-                                             std::move(*response_data)));
+               CreateMakeCredentialResponse(std::move(client_data_),
+                                            std::move(*response_data)));
       break;
   }
   Cleanup();
