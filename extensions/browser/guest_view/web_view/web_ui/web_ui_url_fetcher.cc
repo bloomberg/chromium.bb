b69500cc62c14829ee3106223d2cbdf427b296cf
rhalavati@google.com <rhalavati@google.com>
2018-01-10 06:40:58 +0000

    Update traffic anntoation for WebUIURLFetcher::Start().
    The cookies section of WebUIURLFetcher::Start() is updated.
    
    Bug: 656607
    Bug: 796504
    Change-Id: Ib06653ce5d15d2f83d8716f190347583dcfee722
    Reviewed-on: https://chromium-review.googlesource.com/839862
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Lucas Gadani <lfg@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528252}
    Former-commit-id: e8e880d3c8e25585160af86e842a82dae8eebcfe

--- extensions/browser/guest_view/web_view/web_ui/web_ui_url_fetcher.cc
+++ extensions/browser/guest_view/web_view/web_ui/web_ui_url_fetcher.cc
@@ -50,7 +50,8 @@ void WebUIURLFetcher::Start() {
   fetcher_->SetRequestContext(
       content::BrowserContext::GetDefaultStoragePartition(context_)->
           GetURLRequestContext());
-  fetcher_->SetLoadFlags(net::LOAD_DO_NOT_SAVE_COOKIES);
+  fetcher_->SetLoadFlags(net::LOAD_DO_NOT_SAVE_COOKIES |
+                         net::LOAD_DO_NOT_SEND_COOKIES);
 
   content::AssociateURLFetcherWithRenderFrame(
       fetcher_.get(), url::Origin::Create(url_), render_process_id_,
