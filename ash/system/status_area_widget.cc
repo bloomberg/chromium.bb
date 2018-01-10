a6a6c14fc07c6a3d5a1776406cd0d5de150ab748
Tetsui Ohkubo <tetsui@chromium.org>
2018-01-10 11:14:15 +0900

    Anchor system tray bubble to the rightmost on tablet mode.
    The system tray bubble and notification center tray bubble were not
    anchored to the rightmost on tablet mode.
    This CL adds GetSystemTrayAnchor() to StatusAreaWidget and Shelf,
    and anchor these bubbles to the TrayBackgroundView that is returned by
    the method.
    The overview button on the rightmost will be used when tablet mode is
    enabled.
    
    TEST=manual
    BUG=769512
    
    Change-Id: I3956b75ee524a31056da3a272c879997b2274e5c
    Reviewed-on: https://chromium-review.googlesource.com/844053
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528225}
    Former-commit-id: 428919da9e6fca97c109fe9f304634133b983c44

--- ash/system/status_area_widget.cc
+++ ash/system/status_area_widget.cc
@@ -129,6 +129,12 @@ void StatusAreaWidget::UpdateAfterLoginStatusChange(LoginStatus login_status) {
     overview_button_tray_->UpdateAfterLoginStatusChange(login_status);
 }
 
+TrayBackgroundView* StatusAreaWidget::GetSystemTrayAnchor() const {
+  if (overview_button_tray_->visible())
+    return overview_button_tray_;
+  return system_tray_;
+}
+
 bool StatusAreaWidget::ShouldShowShelf() const {
   // The system tray bubble may or may not want to force the shelf to be
   // visible.
@@ -183,9 +189,7 @@ void StatusAreaWidget::AddSystemTray() {
 }
 
 void StatusAreaWidget::AddWebNotificationTray() {
-  DCHECK(system_tray_);
-  web_notification_tray_ =
-      new WebNotificationTray(shelf_, GetNativeWindow(), system_tray_);
+  web_notification_tray_ = new WebNotificationTray(shelf_, GetNativeWindow());
   status_area_widget_delegate_->AddTray(web_notification_tray_);
 }
 
