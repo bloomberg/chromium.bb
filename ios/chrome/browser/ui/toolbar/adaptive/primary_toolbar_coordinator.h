8e4bba91ed30275f14ce17b3241380b9b2a622ef
Gauthier Ambard <gambard@chromium.org>
2018-01-10 14:00:09 +0000

    Create Coordinator/ViewController for AdaptiveToolbar
    This CL creates the coordinators and view controllers for the adaptive
    primary toolbar. Those objects are mainly copy-pasted from the clean
    toolbar. They are not connected to any flag yet.
    
    As the toolbar is needed to enable/disable flags, the flag will be added
    once the toolbar is reaching a state where it is usable. This CL is only
    creating the base of the adaptive toolbar.
    Other CL will follow to enable it. This will allow better readability.
    
    Bug: 799422, 799438
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I55b737e43acde8bbbe65d7d27af7ae69a93adc78
    Reviewed-on: https://chromium-review.googlesource.com/852292
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528299}
    Former-commit-id: 964f1b53dcba6d2e07a96b66f969cb4472e2f01d

---
+++ ios/chrome/browser/ui/toolbar/adaptive/primary_toolbar_coordinator.h
@@ -0,0 +1,55 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_TOOLBAR_ADAPTIVE_PRIMARY_TOOLBAR_COORDINATOR_H_
+#define IOS_CHROME_BROWSER_UI_TOOLBAR_ADAPTIVE_PRIMARY_TOOLBAR_COORDINATOR_H_
+
+#import <UIKit/UIKit.h>
+
+#import "ios/chrome/browser/ui/coordinators/chrome_coordinator.h"
+#import "ios/chrome/browser/ui/toolbar/public/primary_toolbar_coordinator.h"
+
+@protocol ApplicationCommands;
+@protocol BrowserCommands;
+@class CommandDispatcher;
+@protocol ToolbarCommands;
+@protocol ToolbarCoordinatorDelegate;
+@protocol ToolsMenuConfigurationProvider;
+@protocol UrlLoader;
+class WebStateList;
+
+// Coordinator for the primary toolbar. In an adaptive toolbar paradigm, this is
+// the toolbar always displayed.
+@interface PrimaryToolbarCoordinator
+    : ChromeCoordinator<PrimaryToolbarCoordinator>
+
+- (instancetype)
+initWithToolsMenuConfigurationProvider:
+    (id<ToolsMenuConfigurationProvider>)configurationProvider
+                            dispatcher:(CommandDispatcher*)dispatcher
+                          browserState:(ios::ChromeBrowserState*)browserState
+    NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithBaseViewController:(UIViewController*)viewController
+    NS_UNAVAILABLE;
+- (instancetype)initWithBaseViewController:(UIViewController*)viewController
+                              browserState:
+                                  (ios::ChromeBrowserState*)browserState
+    NS_UNAVAILABLE;
+
+// Dispatcher.
+@property(nonatomic, weak)
+    id<ApplicationCommands, BrowserCommands, ToolbarCommands>
+        dispatcher;
+// The web state list this ToolbarCoordinator is handling.
+@property(nonatomic, assign) WebStateList* webStateList;
+// Delegate for this coordinator.
+// TODO(crbug.com/799446): Change this.
+@property(nonatomic, weak) id<ToolbarCoordinatorDelegate> delegate;
+// URL loader for the toolbar.
+// TODO(crbug.com/799446): Remove this.
+@property(nonatomic, weak) id<UrlLoader> URLLoader;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_TOOLBAR_ADAPTIVE_PRIMARY_TOOLBAR_COORDINATOR_H_
---
+++ ios/chrome/browser/ui/toolbar/adaptive/primary_toolbar_coordinator.mm
@@ -0,0 +1,255 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/toolbar/adaptive/primary_toolbar_coordinator.h"
+
+#import <CoreLocation/CoreLocation.h>
+
+#include "base/metrics/histogram_macros.h"
+#include "base/strings/sys_string_conversions.h"
+#include "components/google/core/browser/google_util.h"
+#include "ios/chrome/browser/browser_state/chrome_browser_state.h"
+#import "ios/chrome/browser/ui/location_bar/location_bar_coordinator.h"
+#include "ios/chrome/browser/ui/omnibox/location_bar_controller_impl.h"
+#include "ios/chrome/browser/ui/omnibox/location_bar_delegate.h"
+#import "ios/chrome/browser/ui/omnibox/omnibox_popup_positioner.h"
+#import "ios/chrome/browser/ui/omnibox/popup/omnibox_popup_coordinator.h"
+#import "ios/chrome/browser/ui/toolbar/adaptive/primary_toolbar_view_controller.h"
+#import "ios/chrome/browser/ui/toolbar/clean/toolbar_button_factory.h"
+#import "ios/chrome/browser/ui/toolbar/clean/toolbar_button_visibility_configuration.h"
+#import "ios/chrome/browser/ui/toolbar/clean/toolbar_coordinator_delegate.h"
+#import "ios/chrome/browser/ui/toolbar/public/web_toolbar_controller_constants.h"
+#include "ios/chrome/browser/ui/toolbar/toolbar_model_ios.h"
+#import "ios/chrome/browser/ui/url_loader.h"
+#import "ios/chrome/browser/web_state_list/web_state_list.h"
+#include "ios/web/public/referrer.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@interface PrimaryToolbarCoordinator ()<LocationBarDelegate,
+                                        OmniboxPopupPositioner> {
+  std::unique_ptr<LocationBarControllerImpl> _locationBar;
+}
+
+// Whether this coordinator has been started.
+@property(nonatomic, assign) BOOL started;
+// The Toolbar view controller owned by this coordinator.
+@property(nonatomic, strong)
+    PrimaryToolbarViewController* toolbarViewController;
+// The coordinator for the location bar in the toolbar.
+@property(nonatomic, strong) LocationBarCoordinator* locationBarCoordinator;
+// Coordinator for the omnibox popup.
+@property(nonatomic, strong) OmniboxPopupCoordinator* omniboxPopupCoordinator;
+
+@end
+
+@implementation PrimaryToolbarCoordinator
+@synthesize delegate = _delegate;
+@synthesize dispatcher = _dispatcher;
+@synthesize locationBarCoordinator = _locationBarCoordinator;
+@synthesize omniboxPopupCoordinator = _omniboxPopupCoordinator;
+@synthesize started = _started;
+@synthesize toolbarViewController = _toolbarViewController;
+@synthesize URLLoader = _URLLoader;
+@synthesize webStateList = _webStateList;
+
+#pragma mark - ChromeCoordinator
+
+- (instancetype)
+initWithToolsMenuConfigurationProvider:(id)configurationProvider
+                            dispatcher:(id)dispatcher
+                          browserState:(ios::ChromeBrowserState*)browserState {
+  self = [super initWithBaseViewController:nil browserState:browserState];
+  if (self) {
+    // TODO(crbug.com/799422): Initialize the toolsMenu.
+    _dispatcher = dispatcher;
+  }
+  return self;
+}
+
+- (void)start {
+  if (self.started)
+    return;
+
+  self.started = YES;
+  BOOL isIncognito = self.browserState->IsOffTheRecord();
+
+  self.locationBarCoordinator = [[LocationBarCoordinator alloc] init];
+  self.locationBarCoordinator.browserState = self.browserState;
+  self.locationBarCoordinator.dispatcher = self.dispatcher;
+  [self.locationBarCoordinator start];
+
+  // TODO(crbug.com/785253): Move this to the LocationBarCoordinator once it is
+  // created.
+  _locationBar = std::make_unique<LocationBarControllerImpl>(
+      self.locationBarCoordinator.locationBarView, self.browserState, self,
+      self.dispatcher);
+  self.omniboxPopupCoordinator = _locationBar->CreatePopupCoordinator(self);
+  [self.omniboxPopupCoordinator start];
+  // End of TODO(crbug.com/785253):.
+
+  ToolbarStyle style = isIncognito ? INCOGNITO : NORMAL;
+  ToolbarButtonFactory* buttonFactory =
+      [[ToolbarButtonFactory alloc] initWithStyle:style];
+  buttonFactory.dispatcher = self.dispatcher;
+  buttonFactory.visibilityConfiguration =
+      [[ToolbarButtonVisibilityConfiguration alloc] initWithType:PRIMARY];
+
+  self.toolbarViewController = [[PrimaryToolbarViewController alloc]
+      initWithButtonFactory:buttonFactory];
+  self.toolbarViewController.dispatcher = self.dispatcher;
+  self.toolbarViewController.locationBarView =
+      self.locationBarCoordinator.locationBarView;
+}
+
+#pragma mark - Property Accessors
+
+- (UIViewController*)viewController {
+  return self.toolbarViewController;
+}
+
+#pragma mark - PrimaryToolbarCoordinator
+
+- (id<VoiceSearchControllerDelegate>)voiceSearchDelegate {
+  // TODO(crbug.com/799438): Implement that.
+  return nil;
+}
+
+- (id<QRScannerResultLoading>)QRScannerResultLoader {
+  // TODO(crbug.com/799438): Implement that.
+  return nil;
+}
+
+- (id<TabHistoryPositioner>)tabHistoryPositioner {
+  // TODO(crbug.com/799438): Implement that.
+  return nil;
+}
+
+- (id<TabHistoryUIUpdater>)tabHistoryUIUpdater {
+  // TODO(crbug.com/799438): Implement that.
+  return nil;
+}
+
+- (id<ActivityServicePositioner>)activityServicePositioner {
+  // TODO(crbug.com/799438): Implement that.
+  return nil;
+}
+
+- (void)showPrerenderingAnimation {
+  // TODO(crbug.com/799438): Implement that.
+}
+
+- (BOOL)isOmniboxFirstResponder {
+  return
+      [self.locationBarCoordinator.locationBarView.textField isFirstResponder];
+}
+
+- (BOOL)showingOmniboxPopup {
+  OmniboxViewIOS* omniboxViewIOS =
+      static_cast<OmniboxViewIOS*>(_locationBar.get()->GetLocationEntry());
+  return omniboxViewIOS->IsPopupOpen();
+}
+
+#pragma mark - OmniboxFocuser
+
+- (void)focusOmnibox {
+  // TODO(crbug.com/799438): Implement that.
+}
+
+- (void)cancelOmniboxEdit {
+  // TODO(crbug.com/799438): Implement that.
+}
+
+- (void)focusFakebox {
+  // TODO(crbug.com/799438): Implement that.
+}
+
+- (void)onFakeboxBlur {
+  // TODO(crbug.com/799438): Implement that.
+}
+
+- (void)onFakeboxAnimationComplete {
+  // TODO(crbug.com/799438): Implement that.
+}
+
+#pragma mark - SideSwipeToolbarInteracting
+
+- (UIView*)toolbarView {
+  return self.viewController.view;
+}
+
+- (BOOL)canBeginToolbarSwipe {
+  return ![self isOmniboxFirstResponder] && ![self showingOmniboxPopup];
+}
+
+- (UIImage*)toolbarSideSwipeSnapshotForTab:(Tab*)tab {
+  // TODO(crbug.com/799438): Implement that.
+  return nil;
+}
+
+// TODO(crbug.com/786940): This protocol should move to the ViewController
+// owning the Toolbar. This can wait until the omnibox and toolbar refactoring
+// is more advanced.
+#pragma mark OmniboxPopupPositioner methods.
+
+- (UIView*)popupAnchorView {
+  return self.toolbarViewController.view;
+}
+
+#pragma mark - LocationBarDelegate
+
+- (void)loadGURLFromLocationBar:(const GURL&)url
+                     transition:(ui::PageTransition)transition {
+  if (url.SchemeIs(url::kJavaScriptScheme)) {
+    // Evaluate the URL as JavaScript if its scheme is JavaScript.
+    NSString* jsToEval = [base::SysUTF8ToNSString(url.GetContent())
+        stringByRemovingPercentEncoding];
+    [self.URLLoader loadJavaScriptFromLocationBar:jsToEval];
+  } else {
+    // When opening a URL, force the omnibox to resign first responder.  This
+    // will also close the popup.
+
+    // TODO(crbug.com/785244): Is it ok to call |cancelOmniboxEdit| after
+    // |loadURL|?  It doesn't seem to be causing major problems.  If we call
+    // cancel before load, then any prerendered pages get destroyed before the
+    // call to load.
+    [self.URLLoader loadURL:url
+                   referrer:web::Referrer()
+                 transition:transition
+          rendererInitiated:NO];
+
+    if (google_util::IsGoogleSearchUrl(url)) {
+      UMA_HISTOGRAM_ENUMERATION(
+          kOmniboxQueryLocationAuthorizationStatusHistogram,
+          [CLLocationManager authorizationStatus],
+          kLocationAuthorizationStatusCount);
+    }
+  }
+  [self cancelOmniboxEdit];
+}
+
+- (void)locationBarHasBecomeFirstResponder {
+  [self.delegate locationBarDidBecomeFirstResponder];
+}
+
+- (void)locationBarHasResignedFirstResponder {
+  [self.delegate locationBarDidResignFirstResponder];
+}
+
+- (void)locationBarBeganEdit {
+  [self.delegate locationBarBeganEdit];
+}
+
+- (web::WebState*)getWebState {
+  return self.webStateList->GetActiveWebState();
+}
+
+- (ToolbarModel*)toolbarModel {
+  ToolbarModelIOS* toolbarModelIOS = [self.delegate toolbarModelIOS];
+  return toolbarModelIOS ? toolbarModelIOS->GetToolbarModel() : nullptr;
+}
+
+@end
