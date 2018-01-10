3c616890c74b6cb56975dbaec089143c5dba0ab5
Michael Giuffrida <michaelpg@chromium.org>
2018-01-10 21:08:37 +0000

    Move audio_debug_recordings_handler.h next to .cc file
    The .cc file was moved from chrome/browser/media/ to
    chrome/browser/media/webrtc/ in crrev.com/2307083002, but for some
    reason the header file stayed behind.
    
    Move it to the webrtc directory alongside its .cc file to reflect that
    the class does in fact use webrtc.
    
    No files in chrome/browser/media include this header file except for its
    .cc file, so moving it doesn't affect the original goal of the CL above.
    
    R=tommi@chromium.org
    
    Change-Id: If58ea2c860d95952b9001c29f2b6381cf4e32012
    Reviewed-on: https://chromium-review.googlesource.com/818487
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528422}
    Former-commit-id: 7e9e9b69e5cb137e56450bd72b14ae177c1a4939

--- chrome/browser/media/webrtc/audio_debug_recordings_handler.cc
+++ chrome/browser/media/webrtc/audio_debug_recordings_handler.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/media/audio_debug_recordings_handler.h"
+#include "chrome/browser/media/webrtc/audio_debug_recordings_handler.h"
 
 #include <string>
 #include <utility>
