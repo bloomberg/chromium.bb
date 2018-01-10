131372ce9434a520ca00917d82c6e6c4df6dc5b3
Francois Doray <fdoray@chromium.org>
2018-01-10 20:36:18 +0000

    Add LifecycleUnitObserver.
    This interface will be used for metrics reporting.
    
    Bug: 775644
    Change-Id: I16b68850801e8dab2edaa351a8983705e9b38753
    Reviewed-on: https://chromium-review.googlesource.com/857342
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528411}
    Former-commit-id: 5e2521fc0186ff24baa00c53935239b74213840f

---
+++ chrome/browser/resource_coordinator/lifecycle_unit_observer.h
@@ -0,0 +1,27 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_RESOURCE_COORDINATOR_LIFECYCLE_UNIT_OBSERVER_H_
+#define CHROME_BROWSER_RESOURCE_COORDINATOR_LIFECYCLE_UNIT_OBSERVER_H_
+
+namespace resource_coordinator {
+
+class LifecycleUnit;
+
+// Interface to be notified when the state of a LifecycleUnit changes.
+class LifecycleUnitObserver {
+ public:
+  virtual ~LifecycleUnitObserver() = default;
+
+  // Invoked when the state of the observed LifecycleUnit changes.
+  virtual void OnLifecycleUnitStateChanged(LifecycleUnit* lifecycle_unit) = 0;
+
+  // Invoked before the observed LifecycleUnit starts being destroyed (i.e.
+  // |lifecycle_unit| is still valid when this is inovked).
+  virtual void OnLifecycleUnitDestroyed(LifecycleUnit* lifecycle_unit) = 0;
+};
+
+}  // namespace resource_coordinator
+
+#endif  // CHROME_BROWSER_RESOURCE_COORDINATOR_LIFECYCLE_UNIT_OBSERVER_H_
