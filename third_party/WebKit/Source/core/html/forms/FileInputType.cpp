63c9615a68135f84da5101cbc5a9c50ec1f6c9c7
Eric Lawrence <elawrence@chromium.org>
2018-01-10 05:58:18 +0000

    Count interactions with file input elements
    Before enabling HTTPBad UI warnings for non-secure pages that perform
    file uploads, we need to track how often these warnings will appear.
    This CL records a UseCounter for interactions with the input element
    that open the file chooser dialog.
    
    Change-Id: Ifd69909b780e9840298bfb1129172608da9fc33e
    Reviewed-on: https://chromium-review.googlesource.com/857738
    Commit-Queue: Eric Lawrence <elawrence@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528248}
    Former-commit-id: b2e238f55317294ad47992eaed51d343cf5c1523

--- third_party/WebKit/Source/core/html/forms/FileInputType.cpp
+++ third_party/WebKit/Source/core/html/forms/FileInputType.cpp
@@ -153,6 +153,7 @@ void FileInputType::HandleDOMActivateEvent(Event* event) {
   if (ChromeClient* chrome_client = GetChromeClient()) {
     WebFileChooserParams params;
     HTMLInputElement& input = GetElement();
+    Document& document = input.GetDocument();
     bool is_directory = input.FastHasAttribute(webkitdirectoryAttr);
     params.directory = is_directory;
     params.need_local_path = is_directory;
@@ -161,10 +162,14 @@ void FileInputType::HandleDOMActivateEvent(Event* event) {
     params.selected_files = file_list_->PathsForUserVisibleFiles();
     params.use_media_capture = RuntimeEnabledFeatures::MediaCaptureEnabled() &&
                                input.FastHasAttribute(captureAttr);
-    params.requestor = input.GetDocument().Url();
+    params.requestor = document.Url();
 
-    chrome_client->OpenFileChooser(input.GetDocument().GetFrame(),
-                                   NewFileChooser(params));
+    UseCounter::Count(
+        document, document.IsSecureContext()
+                      ? WebFeature::kInputTypeFileSecureOriginOpenChooser
+                      : WebFeature::kInputTypeFileInsecureOriginOpenChooser);
+
+    chrome_client->OpenFileChooser(document.GetFrame(), NewFileChooser(params));
   }
   event->SetDefaultHandled();
 }
