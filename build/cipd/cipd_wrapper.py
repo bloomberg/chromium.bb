0f0e87a02bf9221e15e0f60a6f5a89456ca86751
George Burgess IV <gbiv@chromium.org>
2018-01-10 04:11:07 +0000

    Only download Chromium profiles for Chromium
    CL:843774 made us download these profiles for Chromium and subprojects
    (e.g. webrtc). We don't (yet?) want to apply profiles to said
    subprojects, so we shouldn't be pulling them in the first place.
    
    Bug: 799064
    Test: AFDO build of apks/Chrome.apk (among others) succeeds
    Change-Id: Id1d34e1cc442c4871191f47564d6b625cce36f42
    Reviewed-on: https://chromium-review.googlesource.com/851068
    Commit-Queue: George Burgess <gbiv@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528237}
    Former-commit-id: bfc93d720491020defa4573a0d4da4ac7918e700

--- build/cipd/cipd_wrapper.py
+++ build/cipd/cipd_wrapper.py
@@ -41,10 +41,23 @@ def cipd_ensure(root, service_url, ensure_file):
     return retcode
 
 
+def build_ensure_file(sub_files):
+  pieces = []
+  for sub in sub_files:
+    pieces.append('### Sourced from //%s' % os.path.relpath(sub, _SRC_ROOT))
+    with open(sub) as f:
+      pieces.append(f.read())
+  return '\n'.join(pieces)
+
+
 def main():
   parser = argparse.ArgumentParser(
       description='Updates CIPD-managed dependencies based on the given OS.')
 
+  parser.add_argument(
+      '--keep-generated-file',
+      action='store_true',
+      help='Don\'t delete the generated ensure file, if any, on success.')
   parser.add_argument(
       '--chromium-root',
       required=True,
@@ -55,12 +68,35 @@ def main():
       default='https://chrome-infra-packages.appspot.com')
   parser.add_argument(
       '--ensure-file',
+      action='append',
       type=os.path.realpath,
       required=True,
-      help='The path to the ensure file.')
+      help='The path to the ensure file. If given multiple times, this will '
+           'concatenate the given ensure files before passing them to cipd.')
   args = parser.parse_args()
-  return cipd_ensure(args.chromium_root, args.service_url, args.ensure_file)
 
+  build_own_ensure_file = len(args.ensure_file) > 1
+  if build_own_ensure_file:
+    ensure_file_contents = build_ensure_file(args.ensure_file)
+    with tempfile.NamedTemporaryFile(suffix='.ensure') as ensure_file:
+      ensure_file.write(ensure_file_contents)
+      ensure_file_name = ensure_file.name
+      ensure_file.delete = False
+  else:
+    ensure_file_name = args.ensure_file[0]
+
+  retcode = cipd_ensure(args.chromium_root, args.service_url, ensure_file_name)
+  if retcode:
+    if build_own_ensure_file:
+      print 'Ensure failed; generated input file is at', ensure_file_name
+    return retcode
+
+  if build_own_ensure_file:
+    if args.keep_generated_file:
+      print 'Leaving the generated .ensure file at', ensure_file_name
+    else:
+      os.unlink(ensure_file_name)
+  return 0
 
 if __name__ == '__main__':
   sys.exit(main())
