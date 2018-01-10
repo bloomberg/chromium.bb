06809575c3ab93c268c23d00503d8bba06e380f2
spqchan <spqchan@chromium.org>
2018-01-10 21:38:11 +0000

    [MacViews] Fix Cocoa Scrollbar Thumb Highlight
    The Cocoa legacy style scrollbar thumb should highlight
    when the scrollbar track is highlighted.
    
    Bug: 792373
    Change-Id: Ie64818ded49e8391dbc61c93e66fe67ca85fdcb7
    Reviewed-on: https://chromium-review.googlesource.com/858418
    Commit-Queue: Sarah Chan <spqchan@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528432}
    Former-commit-id: e3c6f21f36d71161566f1b9a6b108b3467b7ce11

--- ui/views/controls/scrollbar/cocoa_scroll_bar.mm
+++ ui/views/controls/scrollbar/cocoa_scroll_bar.mm
@@ -68,6 +68,8 @@ class CocoaScrollBarThumb : public BaseScrollBarThumb {
   // Returns true if the thumb is in pressed state.
   bool IsStatePressed() const;
 
+  void UpdateIsMouseOverTrack(bool mouse_over_track);
+
  protected:
   // View:
   gfx::Size CalculatePreferredSize() const override;
@@ -102,6 +104,15 @@ bool CocoaScrollBarThumb::IsStateHovered() const {
 
 bool CocoaScrollBarThumb::IsStatePressed() const {
   return GetState() == Button::STATE_PRESSED;
+}
+
+void CocoaScrollBarThumb::UpdateIsMouseOverTrack(bool mouse_over_track) {
+  // The state should not change if the thumb is pressed. The thumb will be
+  // set back to its hover or normal state when the mouse is released.
+  if (IsStatePressed())
+    return;
+
+  SetState(mouse_over_track ? Button::STATE_HOVERED : Button::STATE_NORMAL);
 }
 
 gfx::Size CocoaScrollBarThumb::CalculatePreferredSize() const {
@@ -286,7 +297,9 @@ void CocoaScrollBar::OnMouseReleased(const ui::MouseEvent& event) {
 }
 
 void CocoaScrollBar::OnMouseEntered(const ui::MouseEvent& event) {
-  if (scroller_style_ != NSScrollerStyleOverlay)
+  GetCocoaScrollBarThumb()->UpdateIsMouseOverTrack(true);
+
+  if (scroller_style_ == NSScrollerStyleLegacy)
     return;
 
   // If the scrollbar thumb did not completely fade away, then reshow it when
@@ -310,6 +323,7 @@ void CocoaScrollBar::OnMouseEntered(const ui::MouseEvent& event) {
 }
 
 void CocoaScrollBar::OnMouseExited(const ui::MouseEvent& event) {
+  GetCocoaScrollBarThumb()->UpdateIsMouseOverTrack(false);
   ResetOverlayScrollbar();
 }
 
