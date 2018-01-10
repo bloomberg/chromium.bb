e69115ee822796a80c22a6654448313b7688f678
Tibor Goldschwendt <tiborg@chromium.org>
2018-01-10 15:21:54 +0000

    [vr] Add presubmit to assets to check integrity of version
    The presubmit checks that
    - the version is upraded if assets files were changed,
    - the version is not downgraded.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I525d1a753282f6ecb08cf94642da24ac70f8e973
    Reviewed-on: https://chromium-review.googlesource.com/857549
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528318}
    Former-commit-id: 0870f08457a9669296c35d15201287271731467e

---
+++ chrome/browser/resources/vr/assets/PRESUBMIT.py
@@ -0,0 +1,83 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import collections
+
+
+def ParseVersion(lines):
+  version_keys = ['MAJOR', 'MINOR']
+  version_vals = {}
+  for line in lines:
+    key, val = line.strip().split('=', 1)
+    if key in version_keys:
+      if key in version_vals or not val.isdigit():
+        return None
+      version_vals[key] = int(val)
+
+  if set(version_keys) != set(version_vals):
+    # We didn't see all parts of the version.
+    return None
+
+  return collections.namedtuple('Version', ['major', 'minor'])(
+      major=version_vals['MAJOR'], minor=version_vals['MINOR'])
+
+
+def IsNewer(old_version, new_version):
+  return (old_version and new_version and
+          (old_version.major < new_version.major or
+           (old_version.major == new_version.major and
+            old_version.minor < new_version.minor)))
+
+
+def CheckVersion(input_api, output_api):
+  """Checks that
+  - the version was upraded if assets files were changed,
+  - the version was not downgraded.
+  """
+  old_version = None
+  new_version = None
+  changed_assets = False
+  changed_version = False
+  for file in input_api.AffectedFiles():
+    basename = input_api.os_path.basename(file.LocalPath())
+    extension = input_api.os_path.splitext(basename)[1][1:].strip().lower()
+    if (extension == 'sha1'):
+      changed_assets = True
+    if (basename == 'VERSION'):
+      changed_version = True
+      old_version = ParseVersion(file.OldContents())
+      new_version = ParseVersion(file.NewContents())
+
+  local_version_filename = input_api.os_path.join(
+      input_api.os_path.dirname(input_api.AffectedFiles()[0].LocalPath()),
+      'VERSION')
+
+  if changed_version and (not old_version or not new_version):
+    return [
+        output_api.PresubmitError(
+            'Cannot parse version in \'%s\'.' % local_version_filename)
+    ]
+
+  version_upgraded = IsNewer(old_version, new_version)
+  if changed_assets and not version_upgraded:
+    return [
+        output_api.PresubmitError(
+            'Must increment version in \'%s\' when '
+            'updating VR assets.' % local_version_filename)
+    ]
+  if changed_version and not version_upgraded:
+    return [
+        output_api.PresubmitError(
+            'Must not downgrade version in \'%s\'.' % local_version_filename)
+    ]
+
+  return []
+
+
+def CheckChangeOnUpload(input_api, output_api):
+  return CheckVersion(input_api, output_api)
+
+
+def CheckChangeOnCommit(input_api, output_api):
+  return CheckVersion(input_api, output_api)
