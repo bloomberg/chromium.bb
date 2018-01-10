a0f104b2f4ddf1ca4ddf29ef43d79d4dec58f94d
Daniel Bratell <bratell@opera.com>
2018-01-10 15:47:23 +0000

    Changed an html code constant kDefaultWidth to have a unique name
    In certain jumbo configurations HTMLCanvasElement.cpp and
    track/vtt/VTTRegion.cpp end up in the same translation unit
    and then the two different kDefaultWidth clash.
    
    This patch gives kDefaultWidth more specific names and
    changes the constants to be constexpr and be in an
    anonymous namespace to better match the current coding style.
    
    Change-Id: I116171382d172ff00c20eec13210494393190fc9
    Reviewed-on: https://chromium-review.googlesource.com/859781
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#528323}
    Former-commit-id: 3bed20ebf4e7d5139c490e95fd883e1ac4570df8

--- third_party/WebKit/Source/core/html/track/vtt/VTTRegion.cpp
+++ third_party/WebKit/Source/core/html/track/vtt/VTTRegion.cpp
@@ -46,32 +46,33 @@
 
 namespace blink {
 
+namespace {
 // The following values default values are defined within the WebVTT Regions
 // Spec.
 // https://dvcs.w3.org/hg/text-tracks/raw-file/default/608toVTT/region.html
 
 // The region occupies by default 100% of the width of the video viewport.
-static const double kDefaultWidth = 100;
+constexpr double kDefaultRegionWidth = 100;
 
 // The region has, by default, 3 lines of text.
-static const int kDefaultHeightInLines = 3;
+constexpr int kDefaultHeightInLines = 3;
 
 // The region and viewport are anchored in the bottom left corner.
-static const double kDefaultAnchorPointX = 0;
-static const double kDefaultAnchorPointY = 100;
+constexpr double kDefaultAnchorPointX = 0;
+constexpr double kDefaultAnchorPointY = 100;
 
 // The region doesn't have scrolling text, by default.
-static const bool kDefaultScroll = false;
+constexpr bool kDefaultScroll = false;
 
 // Default region line-height (vh units)
-static const float kLineHeight = 5.33;
+constexpr float kLineHeight = 5.33;
 
 // Default scrolling animation time period (s).
-static const float kScrollTime = 0.433;
+constexpr float kScrollTime = 0.433;
 
-static bool IsNonPercentage(double value,
-                            const char* method,
-                            ExceptionState& exception_state) {
+bool IsNonPercentage(double value,
+                     const char* method,
+                     ExceptionState& exception_state) {
   if (value < 0 || value > 100) {
     exception_state.ThrowDOMException(
         kIndexSizeError,
@@ -83,9 +84,11 @@ static bool IsNonPercentage(double value,
   return false;
 }
 
+}  // namespace
+
 VTTRegion::VTTRegion()
     : id_(g_empty_string),
-      width_(kDefaultWidth),
+      width_(kDefaultRegionWidth),
       lines_(kDefaultHeightInLines),
       region_anchor_(DoublePoint(kDefaultAnchorPointX, kDefaultAnchorPointY)),
       viewport_anchor_(DoublePoint(kDefaultAnchorPointX, kDefaultAnchorPointY)),
