96dd884a3529e4c6edb3e32f87b4932efee84b20
Friedrich Horschig <fhorschig@chromium.org>
2018-01-10 14:19:39 +0000

    Remove influence of group name on NTPPopularSites studies
    Recently, a misnamed study (see deleted test config) caused the feature
    to fall back to the default popular sites. This isn't a big problem but
    when it was finally named correctly, the names of the groups disabled
    all popular sites.
    
    Popular sites should not be so easy to disable. Therefore, remove the
    group name condition.
    
    A forced cmd line flag should cover all possible cases and make the
    intention very clear. This is already in place and remains untouched.
    
    Bug: 794329
    Change-Id: I71458d41de6d0e0d5f0d8fcde1b636b0fe18618c
    Reviewed-on: https://chromium-review.googlesource.com/827072
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528301}
    Former-commit-id: 1a544c783e00bf6adb51be7590fee71f5c4a4185

--- components/ntp_tiles/constants.h
+++ components/ntp_tiles/constants.h
@@ -26,6 +26,9 @@ extern const base::Feature kNtpMostLikelyFaviconsFromServerFeature;
 // Feature to provide site exploration tiles in addition to personal tiles.
 extern const base::Feature kSiteExplorationUiFeature;
 
+// If this feature is enabled, we enable popular sites in NTP and Chrome Hub.
+extern const base::Feature kUsePopularSitesSuggestions;
+
 }  // namespace ntp_tiles
 
 #endif  // COMPONENTS_NTP_TILES_CONSTANTS_H_
