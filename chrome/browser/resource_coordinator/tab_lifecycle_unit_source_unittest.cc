cb8231290bd3dcc76ca45b5c784246ecb0c911fd
Francois Doray <fdoray@chromium.org>
2018-01-10 21:01:36 +0000

    Add TabLifecycleUnitSourceTest.Discard.
    The purpose of this test is to verify that
    WebContents::GetLastActiveTime() is preserved when a tab is discarded.
    
    Bug: 775644
    Change-Id: I6410b05eec08bc3418e35d1fc17c59cc86244330
    Reviewed-on: https://chromium-review.googlesource.com/857577
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528420}
    Former-commit-id: a4b69f72f6f68ee747fc42a99c4a37a9e1a3bff1

--- chrome/browser/resource_coordinator/tab_lifecycle_unit_source_unittest.cc
+++ chrome/browser/resource_coordinator/tab_lifecycle_unit_source_unittest.cc
@@ -301,6 +301,36 @@ TEST_F(TabLifecycleUnitSourceTest, ReplaceWebContents) {
 // collaboration from the TabLifecycleUnitSource is required to replace the
 // WebContents in the TabLifecycleUnit.
 
+TEST_F(TabLifecycleUnitSourceTest, Discard) {
+  const base::TimeTicks kDummyLastActiveTime = base::TimeTicks() + kShortDelay;
+
+  LifecycleUnit* background_lifecycle_unit = nullptr;
+  LifecycleUnit* foreground_lifecycle_unit = nullptr;
+  CreateTwoTabs(true /* focus_tab_strip */, &background_lifecycle_unit,
+                &foreground_lifecycle_unit);
+  content::WebContents* initial_web_contents =
+      tab_strip_model_->GetWebContentsAt(0);
+  initial_web_contents->SetLastActiveTime(kDummyLastActiveTime);
+
+  // Discard the tab.
+  EXPECT_EQ(LifecycleUnit::State::LOADED,
+            background_lifecycle_unit->GetState());
+  EXPECT_CALL(tab_observer_, OnDiscardedStateChange(testing::_, true));
+  background_lifecycle_unit->Discard(DiscardReason::kProactive);
+  testing::Mock::VerifyAndClear(&tab_observer_);
+
+  // Expect the tab to be discarded and the last active time to be preserved.
+  EXPECT_EQ(LifecycleUnit::State::DISCARDED,
+            background_lifecycle_unit->GetState());
+  EXPECT_NE(initial_web_contents, tab_strip_model_->GetWebContentsAt(0));
+  EXPECT_FALSE(
+      tab_strip_model_->GetWebContentsAt(0)->GetController().GetPendingEntry());
+  EXPECT_EQ(kDummyLastActiveTime,
+            tab_strip_model_->GetWebContentsAt(0)->GetLastActiveTime());
+
+  source_.SetFocusedTabStripModelForTesting(nullptr);
+}
+
 TEST_F(TabLifecycleUnitSourceTest, DiscardAndActivate) {
   LifecycleUnit* background_lifecycle_unit = nullptr;
   LifecycleUnit* foreground_lifecycle_unit = nullptr;
