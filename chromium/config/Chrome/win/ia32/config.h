fb27c4e8357bf9fada2c0a19bb6d280074026e3a
Dan Sanders <sandersd@chromium.org>
2018-01-11 14:31:07 -0800

    Fix Windows config.h line ending.

--- chromium/config/Chrome/win/ia32/config.h
+++ chromium/config/Chrome/win/ia32/config.h
@@ -6,7 +6,7 @@
 #define CONFIG_THIS_YEAR 2018
 #define FFMPEG_DATADIR "/usr/local/share/ffmpeg"
 #define AVCONV_DATADIR "/usr/local/share/ffmpeg"
-#define CC_IDENT "Microsoft (R) C/C++ Optimizing Compiler Version 19.11.25507.1 for x86"
+#define CC_IDENT "Microsoft (R) C/C++ Optimizing Compiler Version 19.11.25507.1 for x86"
 #define av_restrict __restrict
 #define EXTERN_PREFIX "_"
 #define EXTERN_ASM _
