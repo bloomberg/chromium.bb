16a6ae8058dbaed92fd8d1cfa01f22376d85ae1c
Kevin McNee <mcnee@chromium.org>
2018-01-11 20:47:21 +0000

    OOPIF: Allow context menu on two finger tap.
    In order for the ContextMenuController to show the context menu, we
    must have a ContextMenuAllowedScope. We are missing this in
    |WebFrameWidgetImpl::HandleGestureEvent|.
    
    Bug: 795300
    Change-Id: I54f63ba594625171199cbf3213421ef2cefaa228
    Reviewed-on: https://chromium-review.googlesource.com/850992
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528746}
    Former-commit-id: d8c22f591392bea5213383662b51e35ba9d736df

--- third_party/WebKit/Source/core/frame/WebFrameWidgetImpl.cpp
+++ third_party/WebKit/Source/core/frame/WebFrameWidgetImpl.cpp
@@ -72,6 +72,7 @@
 #include "platform/graphics/Color.h"
 #include "platform/graphics/CompositorMutatorClient.h"
 #include "platform/wtf/AutoReset.h"
+#include "platform/wtf/Optional.h"
 #include "platform/wtf/PtrUtil.h"
 #include "public/web/WebAutofillClient.h"
 #include "public/web/WebPlugin.h"
@@ -826,6 +827,7 @@ WebInputEventResult WebFrameWidgetImpl::HandleGestureEvent(
   DCHECK(client_);
   WebInputEventResult event_result = WebInputEventResult::kNotHandled;
   bool event_cancelled = false;
+  WTF::Optional<ContextMenuAllowedScope> maybe_context_menu_scope;
 
   WebViewImpl* view_impl = View();
   switch (event.GetType()) {
@@ -847,9 +849,12 @@ WebInputEventResult WebFrameWidgetImpl::HandleGestureEvent(
       View()->SetLastHiddenPagePopup(nullptr);
     case WebInputEvent::kGestureShowPress:
     case WebInputEvent::kGestureDoubleTap:
+      break;
     case WebInputEvent::kGestureTwoFingerTap:
     case WebInputEvent::kGestureLongPress:
     case WebInputEvent::kGestureLongTap:
+      GetPage()->GetContextMenuController().ClearContextMenu();
+      maybe_context_menu_scope.emplace();
       break;
     case WebInputEvent::kGestureFlingStart:
     case WebInputEvent::kGestureFlingCancel:
