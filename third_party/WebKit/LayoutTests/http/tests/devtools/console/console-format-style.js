46ec750280794bda6e4845fe7215d0805b3ddd61
Erik Luo <luoe@chromium.org>
2018-01-11 02:44:45 +0000

    Reland "DevTools: ensure console links are always visible"
    Instead of using 'overflow-y: hidden' to hide overflow,
    CSS containment rules can make sure that the element's
    paint will not affect other elements.
    
    Screenshot: https://imgur.com/a/jAWI7
    
    This is a reland of 717b4038859ff1bbc000ea2e2fa6c91285b61217 [formerly 38391374b79c006e85e3b9c912267a8d4daaa7cc]
    Original change's description:
    > DevTools: ensure console links are always visible
    >
    > - The %c style formatter will not apply to links in console
    > - Messages now have 'overflow: hidden'
    >
    > Screenshot: https://imgur.com/a/7tAj9
    >
    > Bug: 798096
    > Change-Id: Ifc31314cec2a286e58ebb5625348f8dfe2827d6e
    > Reviewed-on: https://chromium-review.googlesource.com/849650
    > Commit-Queue: Erik Luo <luoe@chromium.org>
    > Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#527501}
    
    Bug: 798096
    Change-Id: I43f151a4c91e6f084c91017753c74e9cb4a7e08c
    Reviewed-on: https://chromium-review.googlesource.com/854957
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528539}
    Former-commit-id: 9495ba2aeac7df698fdcc6959a3534d34adc98c9

--- third_party/WebKit/LayoutTests/http/tests/devtools/console/console-format-style.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/console/console-format-style.js
@@ -14,6 +14,8 @@
         console.log("%cBlue!.", "color: blue;");
         console.log("%cBlue! %cRed!", "color: blue;", "color: red;");
         console.log("%cwww.google.com", "color: blue");
+        console.warn("%cwww.google.com", "color: blue; background: blue");
+        console.error("%cwww.google.com", "color: blue; background: blue");
     })();
   `);
 
