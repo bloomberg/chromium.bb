507eb07362bb7be61346a82e7dd3240585cc9d03
Eric Willigers <ericwilligers@chromium.org>
2018-01-11 01:31:49 +0000

    CSS Basic Shapes: Use counters for ellipse
    The ellipse shape spec used by clip-path, shape-outside, offset-path
    accepts <shape-radius>{2}?, i.e. zero or two <shape-radius> values.
    https://drafts.csswg.org/css-shapes-1/#funcdef-ellipse
    
    However, Blink also accepts a single <shape-radius> for clip-path and
    shape-outside. Safari similarly accepts a single <shape-radius> for
    shape-outside.
    
    CSS WG issue
    https://github.com/w3c/csswg-drafts/issues/2175
    
    We add use counters for ellipse with different numbes of <shape-radius> values.
    
    BUG=800307
    
    Change-Id: I8f05eecc4b04d860ee690dda2f55478329d23cbf
    Reviewed-on: https://chromium-review.googlesource.com/860965
    Reviewed-by: nainar <nainar@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528521}
    Former-commit-id: bf8eb5c3d6db58b0b46ebd2311b082eb5d4a0119

--- third_party/WebKit/Source/core/css/properties/CSSParsingUtils.cpp
+++ third_party/WebKit/Source/core/css/properties/CSSParsingUtils.cpp
@@ -258,10 +258,14 @@ CSSBasicShapeEllipseValue* ConsumeBasicShapeEllipse(
   // spec: https://drafts.csswg.org/css-shapes/#supported-basic-shapes
   // ellipse( [<shape-radius>{2}]? [at <position>]? )
   CSSBasicShapeEllipseValue* shape = CSSBasicShapeEllipseValue::Create();
+  WebFeature feature = WebFeature::kBasicShapeEllipseNoRadius;
   if (CSSValue* radius_x = ConsumeShapeRadius(args, context.Mode())) {
     shape->SetRadiusX(radius_x);
-    if (CSSValue* radius_y = ConsumeShapeRadius(args, context.Mode()))
+    feature = WebFeature::kBasicShapeEllipseOneRadius;
+    if (CSSValue* radius_y = ConsumeShapeRadius(args, context.Mode())) {
       shape->SetRadiusY(radius_y);
+      feature = WebFeature::kBasicShapeEllipseTwoRadius;
+    }
   }
   if (CSSPropertyParserHelpers::ConsumeIdent<CSSValueAt>(args)) {
     CSSValue* center_x = nullptr;
@@ -273,6 +277,7 @@ CSSBasicShapeEllipseValue* ConsumeBasicShapeEllipse(
     shape->SetCenterX(center_x);
     shape->SetCenterY(center_y);
   }
+  context.Count(feature);
   return shape;
 }
 
