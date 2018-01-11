8c2b2f347346ffa9afffdea59c40d8f455c111d4
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-01-11 18:50:24 +0000

    [Extensions Functions] Migrate Identity API to UIThreadExtensionFunction
    Migrate the Identity to use UIThreadExtensionFunction instead of
    ChromeUIThreadExtensionFunction.
    
    Bug: 634140
    Change-Id: I643173b32ac1a0dd898678607a57acbb1f6085da
    Reviewed-on: https://chromium-review.googlesource.com/858038
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528685}
    Former-commit-id: f7f0502c3386398a4eb0832b8ef97adfe389ca8d

--- chrome/browser/extensions/api/identity/identity_get_profile_user_info_function.cc
+++ chrome/browser/extensions/api/identity/identity_get_profile_user_info_function.cc
@@ -5,8 +5,8 @@
 #include "chrome/browser/extensions/api/identity/identity_get_profile_user_info_function.h"
 
 #include "chrome/browser/extensions/api/identity/identity_constants.h"
-#include "chrome/browser/profiles/profile.h"
 #include "chrome/common/extensions/api/identity.h"
+#include "content/public/browser/browser_context.h"
 #include "content/public/common/service_manager_connection.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/permissions/permissions_data.h"
@@ -22,7 +22,7 @@ IdentityGetProfileUserInfoFunction::~IdentityGetProfileUserInfoFunction() {
 }
 
 ExtensionFunction::ResponseAction IdentityGetProfileUserInfoFunction::Run() {
-  if (GetProfile()->IsOffTheRecord()) {
+  if (browser_context()->IsOffTheRecord()) {
     return RespondNow(Error(identity_constants::kOffTheRecord));
   }
 
@@ -32,7 +32,7 @@ ExtensionFunction::ResponseAction IdentityGetProfileUserInfoFunction::Run() {
     return RespondNow(OneArgument(profile_user_info.ToValue()));
   }
 
-  content::BrowserContext::GetConnectorFor(GetProfile())
+  content::BrowserContext::GetConnectorFor(browser_context())
       ->BindInterface(identity::mojom::kServiceName,
                       mojo::MakeRequest(&identity_manager_));
 
