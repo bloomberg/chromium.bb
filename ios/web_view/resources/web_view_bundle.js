941ae0229c8b08857f3df56da8adb610c2dad138
Mohamad Ahmadi <mahmadi@chromium.org>
2018-01-11 23:07:15 +0000

    [AF] Adds autofill_controller to the early page scripts of Chrome WebView
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I5a90e0a28acceecf7862007cf4db102078e123f1
    Reviewed-on: https://chromium-review.googlesource.com/861324
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528801}
    Former-commit-id: a02c098abe7d48f566556a14e501537c4dfab146

---
+++ ios/web_view/resources/web_view_bundle.js
@@ -0,0 +1,8 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// The set of scripts to be injected into the web view as early as possible.
+goog.provide('__crWeb.webViewBundle');
+
+goog.require('__crWeb.autofill');
