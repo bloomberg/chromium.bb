633d560a412bf1439fc34a6654680b87d3f11faa
bsheedy <bsheedy@chromium.org>
2018-01-11 17:46:17 +0000

    Add Windows VR focus lock test
    Ports WebVrInputTest#testPresentationLocksFocus to a browser test for
    use on Windows.
    
    Bug: 795455
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ic11e9bbaf7b1edd211698b7109d11908284e6ea9
    Reviewed-on: https://chromium-review.googlesource.com/861327
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528664}
    Former-commit-id: 237a467558f273d1b02a2aaf9ca844a5e9d67995

---
+++ chrome/browser/vr/webvr_input_browser_test.cc
@@ -0,0 +1,24 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/vr/test/vr_browser_test.h"
+#include "chrome/browser/vr/test/vr_transition_utils.h"
+
+// Browser test equivalent of
+// chrome/android/javatests/src/.../browser/vr_shell/WebVrInputTest.java.
+// End-to-end tests for user input interaction with WebVR.
+
+namespace vr {
+
+// Test that focus is locked to the presenting display for the purposes of VR
+// input.
+IN_PROC_BROWSER_TEST_F(VrBrowserTestStandard,
+                       REQUIRES_GPU(TestPresentationLocksFocus)) {
+  LoadUrlAndAwaitInitialization(
+      GetHtmlTestFile("test_presentation_locks_focus"));
+  EnterPresentationAndWait(GetFirstTabWebContents());
+  EndTest(GetFirstTabWebContents());
+}
+
+}  // namespace vr
