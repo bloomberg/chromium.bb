ca2e60c2650f9f80ee5229d7acc35345f0bc795d
Mike Reed <reed@google.com>
2018-01-11 21:59:22 +0000

    make input_region optional
    as follow-up to https://chromium-review.googlesource.com/c/chromium/src/+/861024
    
    Bug: 
    Change-Id: Iaf7676d126823a22c9827791e180e135cfd55788
    Reviewed-on: https://chromium-review.googlesource.com/860482
    Commit-Queue: Mike Reed <reed@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528767}
    Former-commit-id: 2c570fa74a9f4b9946fff1e7c1643b5a09a6dac9

--- components/exo/surface.h
+++ components/exo/surface.h
@@ -101,6 +101,10 @@ class Surface final : public ui::PropertyHandler {
   // events. The region is clipped to the surface bounds.
   void SetInputRegion(const cc::Region& region);
 
+  // This resets the region of the surface that can receive pointer and touch
+  // events to be wide-open. This will be clipped to the surface bounds.
+  void ResetInputRegion();
+
   // This overrides the input region to the surface bounds with an outset.
   // TODO(domlaskowski): Remove this once client-driven resizing is removed.
   void SetInputOutset(int outset);
@@ -256,7 +260,7 @@ class Surface final : public ui::PropertyHandler {
     bool operator!=(const State& other) { return !(*this == other); }
 
     cc::Region opaque_region;
-    cc::Region input_region;
+    base::Optional<cc::Region> input_region;
     int input_outset = 0;
     float buffer_scale = 1.0f;
     Transform buffer_transform = Transform::NORMAL;
