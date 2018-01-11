6d907896b88bc0ce6378290f77c1854979107815
Ivan Naydonov <samogot@gmail.com>
2018-01-11 15:14:55 +0000

    [GN] Fix TargetPath in Visual Studio projects
    GN assumes that output_dir is root_build_dir for executables.
    If that not so, Visual Studio can't find an executable to run and debug.
    To fix it we are changing TargetPath field from $(ProjectName)$(TargetExt).
    to exact path to ninja target output. Not sure about chromium, but it makes
    sense for some standalone configurations
    
    R=avakulenko@chromium.org, dpranke@chromium.org
    
    Change-Id: Id9ccb7be96cc11f9504f4ea5a6e6117e1c55247b
    Reviewed-on: https://chromium-review.googlesource.com/852459
    Commit-Queue: agrieve <agrieve@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528626}
    Former-commit-id: e3ec6398302a14ad384388a35cc1f5bd17bdbe57

--- tools/gn/visual_studio_writer.cc
+++ tools/gn/visual_studio_writer.cc
@@ -512,6 +512,8 @@ bool VisualStudioWriter::WriteProjectFileContents(
 
   project.SubElement("PropertyGroup", XmlAttributes("Label", "UserMacros"));
 
+  std::string ninja_target = GetNinjaTarget(target);
+
   {
     std::unique_ptr<XmlElementWriter> properties =
         project.SubElement("PropertyGroup");
@@ -524,8 +526,7 @@ bool VisualStudioWriter::WriteProjectFileContents(
     }
     properties->SubElement("TargetName")->Text("$(ProjectName)");
     if (target->output_type() != Target::GROUP) {
-      properties->SubElement("TargetPath")
-          ->Text("$(OutDir)\\$(ProjectName)$(TargetExt)");
+      properties->SubElement("TargetPath")->Text("$(OutDir)\\" + ninja_target);
     }
   }
 
@@ -635,8 +636,6 @@ bool VisualStudioWriter::WriteProjectFileContents(
       XmlAttributes("Project",
                     "$(VCTargetsPath)\\BuildCustomizations\\masm.targets"));
   project.SubElement("ImportGroup", XmlAttributes("Label", "ExtensionTargets"));
-
-  std::string ninja_target = GetNinjaTarget(target);
 
   {
     std::unique_ptr<XmlElementWriter> build =
