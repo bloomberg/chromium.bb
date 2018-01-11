3b68bb11bd53e2dbbde4da15b520b43c37050140
Kevin Bailey <krb@chromium.org>
2018-01-11 21:22:09 +0000

    [omnibox] Don't consider the active tab for switching
    We currently suggest the current tab for switching to, even though we
    block switching to it. This change prevents even suggesting the tab
    (but will suggest another matching tab.)
    
    Bug: 780835
    Change-Id: If10e7991f584903b0ed57f39e2c1b44a43eeb4e3
    Reviewed-on: https://chromium-review.googlesource.com/854773
    Commit-Queue: Kevin Bailey <krb@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528756}
    Former-commit-id: 26d63890c1c2981a54450932cf2e0d34f026768b

--- chrome/browser/autocomplete/chrome_autocomplete_provider_client.cc
+++ chrome/browser/autocomplete/chrome_autocomplete_provider_client.cc
@@ -344,6 +344,10 @@ void ChromeAutocompleteProviderClient::OnAutocompleteControllerResultReady(
 // TODO(crbug.com/46623): Maintain a map of URL->WebContents for fast look-up.
 bool ChromeAutocompleteProviderClient::IsTabOpenWithURL(const GURL& url) {
 #if !defined(OS_ANDROID)
+  Browser* active_browser = BrowserList::GetInstance()->GetLastActive();
+  content::WebContents* active_tab = nullptr;
+  if (active_browser)
+    active_tab = active_browser->tab_strip_model()->GetActiveWebContents();
   for (auto* browser : *BrowserList::GetInstance()) {
     // Only look at same profile (and anonymity level).
     if (browser->profile()->IsSameProfile(profile_) &&
@@ -351,7 +355,7 @@ bool ChromeAutocompleteProviderClient::IsTabOpenWithURL(const GURL& url) {
       for (int i = 0; i < browser->tab_strip_model()->count(); ++i) {
         content::WebContents* web_contents =
             browser->tab_strip_model()->GetWebContentsAt(i);
-        if (web_contents->GetLastCommittedURL() == url)
+        if (web_contents != active_tab && web_contents->GetVisibleURL() == url)
           return true;
       }
     }
