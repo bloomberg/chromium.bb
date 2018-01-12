1d849e6fc56e0e9023592936f9b912e28e174aee
Andrew Grieve <agrieve@chromium.org>
2018-01-12 19:56:12 +0000

    diagnose_bloat.py: Use symbol_level=0 when building
    Turns out that 0 != none, and that there is still enough information to
    extract symbols for supersize.
    
    Change-Id: I26bc62f54b556ba246288568328ddb1558dace25
    Reviewed-on: https://chromium-review.googlesource.com/864982
    Reviewed-by: Eric Stevenson <estevenson@chromium.org>
    Commit-Queue: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529032}
    Former-commit-id: 2dd867d81691b30bda476759fc06eafce8616ffc

--- tools/binary_size/diagnose_bloat.py
+++ tools/binary_size/diagnose_bloat.py
@@ -272,7 +272,7 @@ class _BuildHelper(object):
 
   def _GenGnCmd(self):
     gn_args = 'is_official_build=true'
-    gn_args += ' symbol_level=1'
+    gn_args += ' symbol_level=0'
     # Variables often become unused when experimenting with macros to reduce
     # size, so don't fail on warnings.
     gn_args += ' treat_warnings_as_errors=false'
