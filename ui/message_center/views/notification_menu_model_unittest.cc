5dcb0ac09c2a3ca35e9bc9e4c4605db33c5fd0d0
Tetsui Ohkubo <tetsui@chromium.org>
2018-01-12 02:23:11 +0000

    Remove notification right click menu from Chrome OS.
    This CL removes the right click menu of notification from Chrome OS.
    As inline setting is added to Notification on Chrome OS, right click
    menu is no longer necessary.
    
    TEST=manual
    BUG=796030
    
    Change-Id: I6afb4c90cb0011ad588d3182433785099b2e44ef
    Reviewed-on: https://chromium-review.googlesource.com/844048
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528851}
    Former-commit-id: 3090b8bbebb702a4f7c823a8141a4107919bc17c

--- ui/message_center/views/notification_menu_model_unittest.cc
+++ ui/message_center/views/notification_menu_model_unittest.cc
@@ -92,16 +92,7 @@ TEST_F(NotificationMenuModelTest, ContextMenuTestWithMessageCenter) {
   message_center_->AddNotification(std::move(notification));
 
   AddNotification(id3);
-#if defined(OS_CHROMEOS)
-  EXPECT_EQ(2, model->GetItemCount());
-
-  // The second item is to open the settings.
-  EXPECT_TRUE(model->IsEnabledAt(0));
-  EXPECT_TRUE(model->IsEnabledAt(1));
-  model->ActivatedAt(1);
-#else
   EXPECT_EQ(1, model->GetItemCount());
-#endif
 
   // The first item is to disable notifications from the notifier id. It also
   // removes the notification.
