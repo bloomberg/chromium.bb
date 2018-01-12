c104e0159b6d43c4694780a40509b14570f5b136
Scott Violet <sky@chromium.org>
2018-01-12 17:08:10 +0000

    chromeos: enable presentation callback on chromeos
    It's been fully implemented on Chrome OS and we would like to use it
    for metrics.
    
    BUG=776877,792214
    TEST=covered by tests
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: If4a49439eb5e8ddf75ec02ccaaa34e153f42f504
    Reviewed-on: https://chromium-review.googlesource.com/862707
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#528976}
    Former-commit-id: a95e276ba77b0f2fa606e5e01d847b8d91e2c763

--- ui/gl/gl_switches_util.cc
+++ ui/gl/gl_switches_util.cc
@@ -6,13 +6,19 @@
 
 #include "base/command_line.h"
 #include "base/macros.h"
+#include "build/build_config.h"
 #include "ui/gl/gl_switches.h"
 
 namespace gl {
 
 bool IsPresentationCallbackEnabled() {
+// TODO(peng): always enable once 776877 is fixed.
+#if defined(OS_CHROMEOS)
+  return true;
+#else
   return base::CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kEnablePresentationCallback);
+#endif
 }
 
 }  // namespace gl
