07a8dda5c76e7f6fbe1d542087dfc4ee7cc42d34
Marijn Kruisselbrink <mek@chromium.org>
2018-01-13 00:57:23 +0000

    Change android MessagePort code to no longer use blink::MessagePortChannel.
    This follows similar changes to blink::MessagePort to use mojo's Connector
    API directly, eventually paving the way to removing the now no longer needed
    several hundred lines of fragile code that is blink::MessagePortChannel.
    
    Also fixes a bug in AppWebMessagePort where if setMessageCallback is called
    with a different thread/handler than the existing hanler, it was possible
    for the old callback to be called on the new thread. With this fix a callback
    will always be called on the same thread that was passed with it to
    setMessageCallback (although there is still some race condition where the old
    callback might be called on the old thread after the callback was changed).
    
    Bug: 798449
    Change-Id: I784b23f74a41cab6e37ca6ec6d645822ee135b0d
    Reviewed-on: https://chromium-review.googlesource.com/853231
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529124}
    Former-commit-id: 323f3e4a1aa50e82dab2f79e5c2638e7ca590531

--- content/browser/android/app_web_message_port.cc
+++ content/browser/android/app_web_message_port.cc
@@ -5,6 +5,7 @@
 #include "content/browser/android/app_web_message_port.h"
 
 #include "base/android/jni_android.h"
+#include "base/android/jni_array.h"
 #include "base/android/jni_string.h"
 #include "base/bind.h"
 #include "base/threading/thread_task_runner_handle.h"
@@ -16,151 +17,49 @@ using blink::MessagePortChannel;
 namespace content {
 
 // static
-void AppWebMessagePort::CreateAndBindToJavaObject(
-    JNIEnv* env,
-    mojo::ScopedMessagePipeHandle handle,
-    const base::android::JavaRef<jobject>& jobject) {
-  AppWebMessagePort* instance =
-      new AppWebMessagePort(env, std::move(handle), jobject);
-  Java_AppWebMessagePort_setNativeAppWebMessagePort(
-      env, jobject, reinterpret_cast<jlong>(instance));
-}
-
-// static
-std::vector<MessagePortChannel> AppWebMessagePort::UnwrapJavaArray(
+std::vector<blink::MessagePortChannel> AppWebMessagePort::UnwrapJavaArray(
     JNIEnv* env,
     const base::android::JavaRef<jobjectArray>& jports) {
-  std::vector<MessagePortChannel> channels;
+  std::vector<blink::MessagePortChannel> channels;
   if (!jports.is_null()) {
     jsize num_ports = env->GetArrayLength(jports.obj());
     for (jsize i = 0; i < num_ports; ++i) {
       base::android::ScopedJavaLocalRef<jobject> jport(
           env, env->GetObjectArrayElement(jports.obj(), i));
-      jlong native_port =
-          Java_AppWebMessagePort_releaseNativePortForTransfer(env, jport);
-      // Uninitialized ports should be trapper earlier at the Java layer.
-      DCHECK(native_port != -1);
-      AppWebMessagePort* instance =
-          reinterpret_cast<AppWebMessagePort*>(native_port);
-      channels.push_back(instance->channel_);  // Transfers ownership.
-      delete instance;
+      jint native_port = Java_AppWebMessagePort_releaseNativeHandle(env, jport);
+      channels.push_back(blink::MessagePortChannel(
+          mojo::ScopedMessagePipeHandle(mojo::MessagePipeHandle(native_port))));
     }
   }
   return channels;
 }
 
-void AppWebMessagePort::CloseMessagePort(
+base::android::ScopedJavaLocalRef<jstring>
+JNI_AppWebMessagePort_DecodeStringMessage(
     JNIEnv* env,
-    const base::android::JavaParamRef<jobject>& jcaller) {
-  // Explicitly reset the port here to ensure that OnMessagesAvailable has
-  // finished before we destroy this.
-  channel_ = MessagePortChannel();
-
-  delete this;
-}
-
-void AppWebMessagePort::PostMessage(
-    JNIEnv* env,
-    const base::android::JavaParamRef<jobject>& jcaller,
-    const base::android::JavaParamRef<jstring>& jmessage,
-    const base::android::JavaParamRef<jobjectArray>& jports) {
-  std::vector<uint8_t> encoded_message =
-      EncodeStringMessage(base::android::ConvertJavaStringToUTF16(jmessage));
-  channel_.PostMessage(encoded_message.data(), encoded_message.size(),
-                       UnwrapJavaArray(env, jports));
-}
-
-jboolean AppWebMessagePort::DispatchNextMessage(
-    JNIEnv* env,
-    const base::android::JavaParamRef<jobject>& jcaller) {
-  base::android::ScopedJavaLocalRef<jobject> obj = java_ref_.get(env);
-  if (obj.is_null())
-    return false;
-
-  jmethodID app_web_message_port_constructor =
-      base::android::MethodID::Get<base::android::MethodID::TYPE_INSTANCE>(
-          env, org_chromium_content_browser_AppWebMessagePort_clazz(env),
-          "<init>", "()V");
-
+    const base::android::JavaParamRef<jclass>& jcaller,
+    const base::android::JavaParamRef<jbyteArray>& encoded_data) {
   std::vector<uint8_t> encoded_message;
-  std::vector<MessagePortChannel> channels;
-  if (!channel_.GetMessage(&encoded_message, &channels))
-    return false;
+  base::android::JavaByteArrayToByteVector(env, encoded_data.obj(),
+                                           &encoded_message);
 
   base::string16 message;
   if (!DecodeStringMessage(encoded_message, &message))
-    return false;
+    return nullptr;
 
   base::android::ScopedJavaLocalRef<jstring> jmessage =
       base::android::ConvertUTF16ToJavaString(env, message);
-
-  base::android::ScopedJavaLocalRef<jobjectArray> jports;
-  if (channels.size() > 0) {
-    jports = base::android::ScopedJavaLocalRef<jobjectArray>(
-        env, env->NewObjectArray(
-                 channels.size(),
-                 org_chromium_content_browser_AppWebMessagePort_clazz(env),
-                 nullptr));
-
-    // Instantiate the Java and C++ wrappers for the transferred ports.
-    for (size_t i = 0; i < channels.size(); ++i) {
-      base::android::ScopedJavaLocalRef<jobject> jport(
-          env, env->NewObject(
-                   org_chromium_content_browser_AppWebMessagePort_clazz(env),
-                   app_web_message_port_constructor));
-      CreateAndBindToJavaObject(env, channels[i].ReleaseHandle(), jport);
-
-      env->SetObjectArrayElement(jports.obj(), i, jport.obj());
-    }
-  }
-
-  Java_AppWebMessagePort_onReceivedMessage(env, obj, jmessage, jports);
-  return true;
+  return jmessage;
 }
 
-void AppWebMessagePort::StartReceivingMessages(
-    JNIEnv* env,
-    const base::android::JavaParamRef<jobject>& jcaller) {
-  channel_.SetCallback(base::Bind(&AppWebMessagePort::OnMessagesAvailable,
-                                  base::Unretained(this)),
-                       base::ThreadTaskRunnerHandle::Get());
-}
-
-AppWebMessagePort::AppWebMessagePort(
-    JNIEnv* env,
-    mojo::ScopedMessagePipeHandle handle,
-    const base::android::JavaRef<jobject>& jobject)
-    : channel_(std::move(handle)), java_ref_(env, jobject) {}
-
-AppWebMessagePort::~AppWebMessagePort() {
-}
-
-void AppWebMessagePort::OnMessagesAvailable() {
-  // Called on a background thread.
-  JNIEnv* env = base::android::AttachCurrentThread();
-  base::android::ScopedJavaLocalRef<jobject> obj = java_ref_.get(env);
-  if (obj.is_null())
-    return;
-  Java_AppWebMessagePort_onMessagesAvailable(env, obj);
-}
-
-void JNI_AppWebMessagePort_InitializeAppWebMessagePortPair(
+base::android::ScopedJavaLocalRef<jbyteArray>
+JNI_AppWebMessagePort_EncodeStringMessage(
     JNIEnv* env,
     const base::android::JavaParamRef<jclass>& jcaller,
-    const base::android::JavaParamRef<jobjectArray>& ports) {
-  DCHECK_EQ(2, env->GetArrayLength(ports.obj()));
-
-  mojo::MessagePipe pipe;
-
-  base::android::ScopedJavaLocalRef<jobject> jport0(
-      env, env->GetObjectArrayElement(ports.obj(), 0));
-  AppWebMessagePort::CreateAndBindToJavaObject(
-      env, std::move(pipe.handle0), jport0);
-
-  base::android::ScopedJavaLocalRef<jobject> jport1(
-      env, env->GetObjectArrayElement(ports.obj(), 1));
-  AppWebMessagePort::CreateAndBindToJavaObject(
-      env, std::move(pipe.handle1), jport1);
+    const base::android::JavaParamRef<jstring>& jmessage) {
+  std::vector<uint8_t> encoded_message =
+      EncodeStringMessage(base::android::ConvertJavaStringToUTF16(jmessage));
+  return base::android::ToJavaByteArray(env, encoded_message);
 }
 
 }  // namespace content
--- content/browser/android/app_web_message_port.h
+++ content/browser/android/app_web_message_port.h
@@ -10,48 +10,13 @@
 
 namespace content {
 
-class AppWebMessagePort {
- public:
-  static void CreateAndBindToJavaObject(
-      JNIEnv* env,
-      mojo::ScopedMessagePipeHandle handle,
-      const base::android::JavaRef<jobject>& jobject);
+namespace AppWebMessagePort {
 
-  static std::vector<blink::MessagePortChannel> UnwrapJavaArray(
-      JNIEnv* env,
-      const base::android::JavaRef<jobjectArray>& jports);
+std::vector<blink::MessagePortChannel> UnwrapJavaArray(
+    JNIEnv* env,
+    const base::android::JavaRef<jobjectArray>& jports);
 
-  // Methods called from Java.
-  void CloseMessagePort(
-      JNIEnv* env,
-      const base::android::JavaParamRef<jobject>& jcaller);
-  void PostMessage(
-      JNIEnv* env,
-      const base::android::JavaParamRef<jobject>& jcaller,
-      const base::android::JavaParamRef<jstring>& jmessage,
-      const base::android::JavaParamRef<jobjectArray>& jports);
-  jboolean DispatchNextMessage(
-      JNIEnv* env,
-      const base::android::JavaParamRef<jobject>& jcaller);
-  void StartReceivingMessages(
-      JNIEnv* env,
-      const base::android::JavaParamRef<jobject>& jcaller);
-
- private:
-  explicit AppWebMessagePort(
-      JNIEnv* env,
-      mojo::ScopedMessagePipeHandle handle,
-      const base::android::JavaRef<jobject>& jobject);
-  ~AppWebMessagePort();
-
-  void OnMessagesAvailable();
-
-  blink::MessagePortChannel channel_;
-  JavaObjectWeakGlobalRef java_ref_;
-
-  DISALLOW_COPY_AND_ASSIGN(AppWebMessagePort);
-};
-
+}  // namespace AppWebMessagePort
 }  // namespace content
 
 #endif  // CONTENT_BROWSER_ANDROID_APP_WEB_MESSAGE_PORT_H_
