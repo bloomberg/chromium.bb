7f931c1ebca6ea7007339dc371c5b7e45da4c5b5
Becky Zhou <huayinz@chromium.org>
2018-01-13 01:05:19 +0000

    Add metrics to track single list item action on Download/Bookmarks
    Record user actions on "remove" and "select" from the "more" menu
    on Download and Bookmark page to make analysis on redundant list item
    actions possible.
    
    Bug: 784016
    Change-Id: I0b776cdf1709b94337e673b5dbd1666a165abcc7
    Reviewed-on: https://chromium-review.googlesource.com/852976
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Becky Zhou <huayinz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529127}
    Former-commit-id: 28dd8986fa2bf8ea2de593aea89cc6292c7a2007

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkRow.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkRow.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.bookmarks;
 import android.content.Context;
 import android.util.AttributeSet;
 
+import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.bookmarks.BookmarkBridge.BookmarkItem;
 import org.chromium.chrome.browser.widget.ListMenuButton;
@@ -92,6 +93,7 @@ abstract class BookmarkRow extends SelectableItemView<BookmarkId>
     public void onItemSelected(Item item) {
         if (item.getTextId() == R.string.bookmark_item_select) {
             setChecked(mDelegate.getSelectionDelegate().toggleSelectionForItem(mBookmarkId));
+            RecordUserAction.record("Android.BookmarkPage.SelectFromMenu");
         } else if (item.getTextId() == R.string.bookmark_item_edit) {
             BookmarkItem bookmarkItem = mDelegate.getModel().getBookmarkById(mBookmarkId);
             if (bookmarkItem.isFolder()) {
@@ -105,6 +107,7 @@ abstract class BookmarkRow extends SelectableItemView<BookmarkId>
         } else if (item.getTextId() == R.string.bookmark_item_delete) {
             if (mDelegate != null && mDelegate.getModel() != null) {
                 mDelegate.getModel().deleteBookmarks(mBookmarkId);
+                RecordUserAction.record("Android.BookmarkPage.RemoveItem");
             }
         }
     }
