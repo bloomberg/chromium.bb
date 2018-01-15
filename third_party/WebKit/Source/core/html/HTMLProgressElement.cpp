313e199d768d8541dc921d7f8d49842c3f994803
Takayoshi Kochi <kochi@chromium.org>
2018-01-15 07:28:34 +0000

    Convert V0 UA shadow in <progress> to use V1
    This is a part of conversion from V0 UA shadow to V1.
    <progress> element doesn't have any <contents> element,
    thus the conversion is straightforward to set V1 type.
    
    The internal of <progress>'s UA shadow looks like this:
    
    <progress>
      #shadow-root
        <div pseudo="-webkit-progress-inner-element">
          <div pseudo="-webkit-progress-bar">
            <div pseudo="-webkit-progress-value">
            </div>
          </div>
        </div>
    
    Bug: 788635, 787717
    Change-Id: I743f5c99a8273f29cefe18a9e8789c43eec47f4a
    Reviewed-on: https://chromium-review.googlesource.com/866276
    Commit-Queue: Takayoshi Kochi <kochi@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529218}
    Former-commit-id: a064b39bd3c9e2be6df1f41b656e10249524f62c

--- third_party/WebKit/Source/core/html/HTMLProgressElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLProgressElement.cpp
@@ -45,7 +45,7 @@ HTMLProgressElement::~HTMLProgressElement() = default;
 
 HTMLProgressElement* HTMLProgressElement::Create(Document& document) {
   HTMLProgressElement* progress = new HTMLProgressElement(document);
-  progress->EnsureLegacyUserAgentShadowRootV0();
+  progress->EnsureUserAgentShadowRootV1();
   return progress;
 }
 
