b7e991e7792fc0d844cfe58c2415386ac6f8074c
John Chen <johnchen@chromium.org>
2018-01-15 17:38:20 +0000

    [ChromeDriver] Fix extension background page URL
    ChromeDriver used to use string concatenation to generate extension
    background page URL, and can easily generate invalid URL. For example,
    if the background page path starts with '/', the resulting URL would
    have double "//" between hostname and path.
    
    We now use GURL to generate the URL, and should be more reliable.
    
    Bug: chromedriver:2223, b/69736143
    Change-Id: Ia1031322ef5a53c8a96079c0eebaab0ef7b7c44b
    Reviewed-on: https://chromium-review.googlesource.com/863464
    Commit-Queue: John Chen <johnchen@chromium.org>
    Reviewed-by: Jonathon Kereliuk <kereliuk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529308}
    Former-commit-id: 67473c4ceb61d3c8359fcf3b2546a1b5d95d1411

--- chrome/test/chromedriver/chrome_launcher.cc
+++ chrome/test/chromedriver/chrome_launcher.cc
@@ -53,6 +53,7 @@
 #include "crypto/rsa_private_key.h"
 #include "crypto/sha2.h"
 #include "third_party/zlib/google/zip.h"
+#include "url/gurl.h"
 
 #if defined(OS_POSIX)
 #include <fcntl.h>
@@ -632,7 +633,8 @@ Status GetExtensionBackgroundPage(const base::DictionaryValue* manifest,
   manifest->GetString("background_page", &bg_page_name);
   if (bg_page_name.empty() || !persistent)
     return Status(kOk);
-  *bg_page = "chrome-extension://" + id + "/" + bg_page_name;
+  GURL baseUrl("chrome-extension://" + id + "/");
+  *bg_page = baseUrl.Resolve(bg_page_name).spec();
   return Status(kOk);
 }
 
