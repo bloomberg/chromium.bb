ebe69afd707fcceed7737c4a4926afc766e6baa0
Tommi <tommi@chromium.org>
2018-01-16 23:29:28 +0000

    Reduce the lock scope in WebRtcMediaStreamAdapterMap.
    Specifically this avoids holding the lock inside of GetOrCreateRemoteStreamAdapter
    and GetOrCreateLocalStreamAdapter while we call out to external implementations
    (CreateRemoteStreamAdapter and CreateLocalStreamAdapter).
    
    Holding the lock could cause us to deadlock with the main thread or webrtc's signaling thread
    if the lock was needed on the other thread during the nested call.
    
    Bug: 736725
    Change-Id: I121db4482bd9492de268b062f872b2af1d8484ca
    Reviewed-on: https://chromium-review.googlesource.com/868656
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Tommi <tommi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529518}
    Former-commit-id: ea0efafb2425865de79501bc3cf76dc539c995d8

--- content/renderer/media/webrtc/webrtc_media_stream_adapter_map.h
+++ content/renderer/media/webrtc/webrtc_media_stream_adapter_map.h
@@ -153,9 +153,9 @@ class CONTENT_EXPORT WebRtcMediaStreamAdapterMap
   // Pointer to a |PeerConnectionDependencyFactory| owned by the |RenderThread|.
   // It's valid for the lifetime of |RenderThread|.
   PeerConnectionDependencyFactory* const factory_;
-  scoped_refptr<base::SingleThreadTaskRunner> main_thread_;
+  const scoped_refptr<base::SingleThreadTaskRunner> main_thread_;
   // Takes care of creating and owning track adapters, used by stream adapters.
-  scoped_refptr<WebRtcMediaStreamTrackAdapterMap> track_adapter_map_;
+  const scoped_refptr<WebRtcMediaStreamTrackAdapterMap> track_adapter_map_;
 
   mutable base::Lock lock_;
   LocalStreamAdapterMap local_stream_adapters_;
