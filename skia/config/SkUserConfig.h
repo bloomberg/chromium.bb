b66646ae47116020203919a7567f24e33cac894f
Yuqian Li <liyuqian@google.com>
2018-01-16 16:53:52 +0000

    Remove SK_SUPPORT_LEGACY_SMALLRECT_AA
    Change-Id: I2004422b2227d03524a89273a00b5f001fba01fd
    Reviewed-on: https://chromium-review.googlesource.com/854817
    Commit-Queue: Yuqian Li <liyuqian@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529447}
    Former-commit-id: 8b44f53c3173fca3d2e66bde791b97de5b7826f6

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -220,9 +220,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_DELTA_AA
 #endif
 
-#ifndef SK_SUPPORT_LEGACY_SMALLRECT_AA
-#define SK_SUPPORT_LEGACY_SMALLRECT_AA
-#endif
 
 #ifndef SK_SUPPORT_LEGACY_TILED_BITMAPS
 #define SK_SUPPORT_LEGACY_TILED_BITMAPS
