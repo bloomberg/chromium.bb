52306a3e979905050928aaa5e30abd34ae6b6ea8
Evan Stade <estade@chromium.org>
2018-01-16 19:04:20 +0000

    Update RequestFileSystemNotification to use NotificationDisplayService.
    Also, there's no reason for RequestFileSystemNotification to be a
    NotificationDelegate, so remove that.
    
    Bug: 783018
    Change-Id: I8961a3457295bf6e3f1ff7b91ab9d82751f2e58d
    Reviewed-on: https://chromium-review.googlesource.com/840420
    Reviewed-by: Tomasz Mikolajewski <mtomasz@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529480}
    Former-commit-id: c4f7b0c874f9220b873c568fcd8ded6a637e9814

--- chrome/browser/extensions/api/file_system/request_file_system_notification.h
+++ chrome/browser/extensions/api/file_system/request_file_system_notification.h
@@ -8,60 +8,25 @@
 #include <memory>
 #include <string>
 
-#include "base/callback.h"
-#include "base/macros.h"
 #include "base/memory/weak_ptr.h"
-#include "chrome/browser/ui/app_icon_loader.h"
-#include "ui/message_center/notification_delegate.h"
 
 class Profile;
-
-namespace extensions {
-class Extension;
-}  // namespace extensions
 
 namespace file_manager {
 class Volume;
 }  // namespace file_manager
 
-namespace gfx {
-class Image;
-class ImageSkia;
-}  // namespace gfx
+namespace extensions {
+class Extension;
 
-namespace message_center {
-class Notification;
-}  // namespace message_center
+// Shows a notification about automatically granted access to a file system,
+// i.e. the chrome.fileSystem.requestFileSystem() API.
+void ShowNotificationForAutoGrantedRequestFileSystem(
+    Profile* profile,
+    const extensions::Extension& extension,
+    const base::WeakPtr<file_manager::Volume>& volume,
+    bool writable);
 
-// Shows notifications for the chrome.fileSystem.requestFileSystem() API.
-class RequestFileSystemNotification
-    : public message_center::NotificationDelegate,
-      public AppIconLoaderDelegate {
- public:
-  // Shows a notification about automatically granted access to a file system.
-  static void ShowAutoGrantedNotification(
-      Profile* profile,
-      const extensions::Extension& extension,
-      const base::WeakPtr<file_manager::Volume>& volume,
-      bool writable);
-
- private:
-  RequestFileSystemNotification(Profile* profile,
-                                const extensions::Extension& extension);
-  ~RequestFileSystemNotification() override;
-
-  // Shows the notification. Can be called only once.
-  void Show(std::unique_ptr<message_center::Notification> notification);
-
-  // AppIconLoaderDelegate overrides:
-  void OnAppImageUpdated(const std::string& id,
-                         const gfx::ImageSkia& image) override;
-
-  std::unique_ptr<AppIconLoader> icon_loader_;
-  std::unique_ptr<gfx::Image> extension_icon_;
-  std::unique_ptr<message_center::Notification> pending_notification_;
-
-  DISALLOW_COPY_AND_ASSIGN(RequestFileSystemNotification);
-};
+}  // namespace extensions
 
 #endif  // CHROME_BROWSER_EXTENSIONS_API_FILE_SYSTEM_REQUEST_FILE_SYSTEM_NOTIFICATION_H_
