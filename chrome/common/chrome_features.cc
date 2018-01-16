1de09fe4bfb06713c77b3c3a87a3777cff4484c1
Joy Ming <jming@chromium.org>
2018-01-16 00:40:22 +0000

    [Downloads foreground] Disable downloads foreground for M64.
    Bug: 800637,747563
    Change-Id: I54590daf0caba01abf0880f0159c6d48ba727359
    Reviewed-on: https://chromium-review.googlesource.com/865818
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Joy Ming <jming@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529349}
    Former-commit-id: 84798b4d2ab267af5e6f5a40a27efcf7b3e53033

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -200,7 +200,7 @@ const base::Feature kDoodlesOnLocalNtp{"DoodlesOnLocalNtp",
 #if defined(OS_ANDROID)
 // Enables downloads as a foreground service for all versions of Android.
 const base::Feature kDownloadsForeground{"DownloadsForeground",
-                                         base::FEATURE_ENABLED_BY_DEFAULT};
+                                         base::FEATURE_DISABLED_BY_DEFAULT};
 #endif
 
 #if defined(OS_ANDROID)
