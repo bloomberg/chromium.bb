9fe2395921af2d5cbfaae02cf21637f295b95513
Yuki Shiino <yukishiino@chromium.org>
2018-01-17 11:43:44 +0000

    v8binding: Fixes VRDisplay::HasPendingActivity.
    VRDisplay::HasPendingActivity doesn't take registered callbacks
    into considerations, so VRDisplay's wrapper object might be
    gone while callbacks are registered.  Then, there will be no
    object that invokes wrapper-tracing for callback objects.
    
    This patch makes HasPendingActivity return true if any callback
    is currently registered.
    
    Bug: 793420, 792604
    Change-Id: Iecf402c3140f521b99afe5ccc688ecb7c6531dfd
    Reviewed-on: https://chromium-review.googlesource.com/870330
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529711}
    Former-commit-id: 113dc30a074ce43885015594c0bb992bd51ba43e

--- third_party/WebKit/Source/modules/vr/VRDisplay.cpp
+++ third_party/WebKit/Source/modules/vr/VRDisplay.cpp
@@ -1005,8 +1005,9 @@ void VRDisplay::ContextDestroyed(ExecutionContext* context) {
 
 bool VRDisplay::HasPendingActivity() const {
   // Prevent V8 from garbage collecting the wrapper object if there are
-  // event listeners attached to it.
-  return GetExecutionContext() && HasEventListeners();
+  // event listeners and/or callbacks attached to it.
+  return GetExecutionContext() &&
+         (HasEventListeners() || scripted_animation_controller_->HasCallback());
 }
 
 void VRDisplay::FocusChanged() {
