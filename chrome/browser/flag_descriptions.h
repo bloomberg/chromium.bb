0c3dba3b9ca693ba36fed5ee8bae03acf66f8af0
xdai <xdai@chromium.org>
2018-01-17 22:34:16 +0000

    [Reland] Cros splitscreen: Disable the splitscreen for M65 stable but keep it enabled for all other channels.
    This CL will be reverted after M65 is cut.
    
    TBR=oshima@chromium.org
    
    Bug: 800501
    Change-Id: I35d388efcb6fc4e2ee6d1e51842e1f2bed823c12
    Reviewed-on: https://chromium-review.googlesource.com/869196
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#529845}
    Reviewed-on: https://chromium-review.googlesource.com/871504
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529924}
    Former-commit-id: ddccd3f6dba62870a038ecb7d68b28038fcbe5d9

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1539,9 +1539,6 @@ extern const char kDisableLockScreenAppsDescription[];
 extern const char kDisableTabletAutohideTitlebarsName[];
 extern const char kDisableTabletAutohideTitlebarsDescription[];
 
-extern const char kDisableTabletSplitViewName[];
-extern const char kDisableTabletSplitViewDescription[];
-
 extern const char kEnablePerUserTimezoneName[];
 extern const char kEnablePerUserTimezoneDescription[];
 
@@ -1568,6 +1565,9 @@ extern const char kEnableFloatingVirtualKeyboardDescription[];
 
 extern const char kEnableImeMenuName[];
 extern const char kEnableImeMenuDescription[];
+
+extern const char kEnableTabletSplitViewName[];
+extern const char kEnableTabletSplitViewDescription[];
 
 extern const char kEnableZipArchiverPackerName[];
 extern const char kEnableZipArchiverPackerDescription[];
