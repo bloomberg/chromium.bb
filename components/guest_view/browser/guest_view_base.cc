025774a9b00b757814e28f39e6b7c90bb81630f7
Kevin McNee <mcnee@chromium.org>
2018-01-17 19:13:00 +0000

    OOPIF guest view: Don't ACK AttachToEmbedderFrame before the attachment.
    In the OOPIF based guest version of |attachImpl$| we call
    |AttachIframeGuest| and provide a callback to be run after the
    attachment is done. However, the callback is run before the
    frame is actually swapped for a remote frame, as evidenced by
    the contentWindow still being local when the callback is run.
    
    The relative order of |AttachToOuterWebContentsFrame| and
    |GuestViewMsg_AttachToEmbedderFrame_ACK| appears to have been
    accidentally changed in https://codereview.chromium.org/2472253002
    
    We now send the ACK after the call to |AttachToOuterWebContentsFrame|
    so that the callback is run after the attachment.
    
    Similarly, we were dispatching queued events in GuestViewBase::DidAttach
    before the attachment. We now dispatch after the attachment and ACK, so
    that the correct contentWindow is available in JS when handling the
    events.
    
    Bug: 793422
    Change-Id: Ib83a985e24134dfcfd79cc474a6b30056ca66960
    Reviewed-on: https://chromium-review.googlesource.com/868277
    Reviewed-by: Lucas Gadani <lfg@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529841}
    Former-commit-id: a18303412701d46a3dd35776012b9d3578521e89

--- components/guest_view/browser/guest_view_base.cc
+++ components/guest_view/browser/guest_view_base.cc
@@ -516,7 +516,16 @@ void GuestViewBase::SetGuestHost(content::GuestHost* guest_host) {
 void GuestViewBase::WillAttach(WebContents* embedder_web_contents,
                                int element_instance_id,
                                bool is_full_page_plugin,
-                               const base::Closure& callback) {
+                               const base::Closure& completion_callback) {
+  WillAttach(embedder_web_contents, element_instance_id, is_full_page_plugin,
+             base::OnceClosure(), completion_callback);
+}
+
+void GuestViewBase::WillAttach(WebContents* embedder_web_contents,
+                               int element_instance_id,
+                               bool is_full_page_plugin,
+                               base::OnceClosure perform_attach,
+                               const base::Closure& completion_callback) {
   // Stop tracking the old embedder's zoom level.
   if (owner_web_contents())
     StopTrackingEmbedderZoomLevel();
@@ -537,9 +546,12 @@ void GuestViewBase::WillAttach(WebContents* embedder_web_contents,
 
   WillAttachToEmbedder();
 
+  if (perform_attach)
+    std::move(perform_attach).Run();
+
   // Completing attachment will resume suspended resource loads and then send
   // queued events.
-  SignalWhenReady(callback);
+  SignalWhenReady(completion_callback);
 }
 
 void GuestViewBase::SignalWhenReady(const base::Closure& callback) {
