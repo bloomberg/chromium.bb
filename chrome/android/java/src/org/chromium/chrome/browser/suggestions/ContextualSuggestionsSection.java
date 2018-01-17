35ecc356d85747fec06f4a2743d16571588df584
Theresa Wellington <twellington@google.com>
2018-01-17 22:28:02 +0000

    Contextual Suggestions prototype - preload results
    Preload contextual suggetsions every time a tab is selected or a URL is
    loaded. Also prevent sheet contents from being destroyed when the sheet
    is closed.
    
    This is intended to be a throwaway prototype, used for research purposes
    only. This approach does have increased Java memory overhead.
    
    BUG=801317
    
    Change-Id: Ie6cd8386585f4274fe2018e711f45dfac5d52350
    Reviewed-on: https://chromium-review.googlesource.com/870919
    Reviewed-by: Ted Choc (back but slow, ping me) <tedchoc@chromium.org>
    Commit-Queue: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529921}
    Former-commit-id: 4c6a357b01c51b7bd9226ec35a066cc8a318b81a

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/ContextualSuggestionsSection.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/ContextualSuggestionsSection.java
@@ -17,6 +17,12 @@ import org.chromium.chrome.browser.ntp.snippets.CategoryStatus;
 import org.chromium.chrome.browser.ntp.snippets.ContentSuggestionsCardLayout;
 import org.chromium.chrome.browser.ntp.snippets.KnownCategories;
 import org.chromium.chrome.browser.offlinepages.OfflinePageBridge;
+import org.chromium.chrome.browser.tab.EmptyTabObserver;
+import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tab.TabObserver;
+import org.chromium.chrome.browser.tabmodel.TabModel.TabSelectionType;
+import org.chromium.chrome.browser.tabmodel.TabModelSelector;
+import org.chromium.chrome.browser.tabmodel.TabModelSelectorTabModelObserver;
 import org.chromium.chrome.browser.util.UrlUtilities;
 
 /**
@@ -25,12 +31,17 @@ import org.chromium.chrome.browser.util.UrlUtilities;
 public class ContextualSuggestionsSection extends InnerNode implements SuggestionsSection.Delegate {
     private final SuggestionsUiDelegate mUiDelegate;
     private final SuggestionsSection mSection;
+    private final TabModelSelectorTabModelObserver mTabModelObserver;
+    private final TabObserver mTabObserver;
+
+    private Tab mLastTab;
 
     @Nullable
     private String mCurrentContextUrl;
 
     public ContextualSuggestionsSection(SuggestionsUiDelegate uiDelegate,
-            OfflinePageBridge offlinePageBridge, Context context) {
+            OfflinePageBridge offlinePageBridge, Context context,
+            TabModelSelector tabModelSelector) {
         mUiDelegate = uiDelegate;
 
         SuggestionsCategoryInfo info = new SuggestionsCategoryInfo(KnownCategories.CONTEXTUAL,
@@ -41,13 +52,38 @@ public class ContextualSuggestionsSection extends InnerNode implements Suggestio
                 this, mUiDelegate, mUiDelegate.getSuggestionsRanker(), offlinePageBridge, info);
         mUiDelegate.getSuggestionsRanker().registerCategory(KnownCategories.CONTEXTUAL);
         addChild(mSection);
+
+        mTabObserver = new EmptyTabObserver() {
+            @Override
+            public void onUpdateUrl(Tab tab, String url) {
+                refresh(url);
+            }
+        };
+
+        mTabModelObserver = new TabModelSelectorTabModelObserver(tabModelSelector) {
+            @Override
+            public void didSelectTab(Tab tab, TabSelectionType type, int lastId) {
+                updateCurrentTab(tab);
+            }
+        };
+
+        updateCurrentTab(tabModelSelector.getCurrentTab());
+    }
+
+    /** Destroys the section. */
+    public void destroy() {
+        if (mLastTab != null) {
+            mLastTab.removeObserver(mTabObserver);
+            mLastTab = null;
+        }
+        mTabModelObserver.destroy();
     }
 
     /**
      * Fetches new suggestions if the context URL was changed from the last time that the carousel
      * was shown.
      */
-    public void refresh(final Context context, @Nullable final String newUrl) {
+    public void refresh(@Nullable final String newUrl) {
         if (!URLUtil.isNetworkUrl(newUrl)) {
             clearSuggestions();
             return;
@@ -92,6 +128,20 @@ public class ContextualSuggestionsSection extends InnerNode implements Suggestio
     private void clearSuggestions() {
         mCurrentContextUrl = null;
         mSection.clearData();
+    }
+
+    /**
+     * Update the current tab and refresh suggestions.
+     * @param tab The current {@link Tab}.
+     */
+    private void updateCurrentTab(Tab tab) {
+        if (mLastTab != null) mLastTab.removeObserver(mTabObserver);
+
+        mLastTab = tab;
+        if (mLastTab == null) return;
+
+        mLastTab.addObserver(mTabObserver);
+        refresh(mLastTab.getUrl());
     }
 
     @Override
