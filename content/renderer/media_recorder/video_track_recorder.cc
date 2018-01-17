0a8791a66a7a0ad5dd303292dc356d41418b3925
Sheng-Hao Tsao <shenghao@google.com>
2018-01-17 10:04:35 +0000

    Enable HW VEA in MediaRecorder for ChromeOS
    We are implementing recording functionality in chrome camera app
    on ChromeOS devices with MediaRecorder APIs. Enabling HW VEA would
    greatly improve the performance. For example, FPS increases from
    7 to 30 on kevin.
    
    BUG=616659
    TEST=Tested on kevin that chrome camera app can record videos.
    
    Change-Id: I9a3bce68b71dbd98b3311b3a21cf163ff6940929
    Reviewed-on: https://chromium-review.googlesource.com/808444
    Reviewed-by: Wu-Cheng Li <wuchengli@chromium.org>
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Commit-Queue: Sheng-hao Tsao <shenghao@google.com>
    Cr-Commit-Position: refs/heads/master@{#529689}
    Former-commit-id: ecbec05c91d749e9d20ef3e0c8568cce5a87b4c5

--- content/renderer/media_recorder/video_track_recorder.cc
+++ content/renderer/media_recorder/video_track_recorder.cc
@@ -113,11 +113,6 @@ CodecEnumerator* GetCodecEnumerator() {
 }
 
 CodecEnumerator::CodecEnumerator() {
-#if defined(OS_CHROMEOS)
-  // See https://crbug.com/616659.
-  return;
-#endif
-
   content::RenderThreadImpl* const render_thread_impl =
       content::RenderThreadImpl::current();
   if (!render_thread_impl) {
