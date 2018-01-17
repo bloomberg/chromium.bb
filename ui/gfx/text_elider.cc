ba181bbadae9e056cde5ac8a6cfb10dc246ee863
Stephen McGruer <smcgruer@chromium.org>
2018-01-17 14:32:29 +0000

    Revert "Fix a corner case of text wrapping in views::StyledLabel"
    This reverts commit 236e9a9ca6122c4903c272d2bf6286a5fd209b2a [formerly 929a7aaea3d2d340ec56053cb0dc6e7eabe8ab8a].
    
    Reason for revert: TextEliderTest.ElideRectangleTextFirstWordTruncated
     fails on chromium.mac/Mac10.10 Tests:
    
    https://ci.chromium.org/buildbot/chromium.mac/Mac10.10%20Tests/28182
    
    Original change's description:
    > Fix a corner case of text wrapping in views::StyledLabel
    > 
    > Views are aligned to integer coordinates, but typesetting is not. If
    > part of a line has a stylerange, it may need to shift up to a pixel to
    > the right, compared to an earlier typesetting attempt.
    > 
    > Normally this is fine - more text will just wrap to the next line.
    > However, if this occurs for the *last* word on a line,
    > gfx::ElideRectangleText may split that word. Instead, that word should
    > just wrap on to the next line.
    > 
    > To fix, allow ElideRectangleText to signal when it splits the first
    > word in an elide attempt. StyledLabel just needs to start a new line
    > in this case rather than putting the split word at the end of the
    > current line.
    > 
    > Bug: 799300, 799213
    > Change-Id: Ibcc6134872b06c56d19bad5f7336534189fa316a
    > Reviewed-on: https://chromium-review.googlesource.com/866277
    > Reviewed-by: Michael Wasserman <msw@chromium.org>
    > Commit-Queue: Trent Apted <tapted@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#529646}
    
    TBR=msw@chromium.org,tapted@chromium.org
    
    Change-Id: Ib518babd4bf891418789951186a8bcd7442bbbb2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 799300, 799213
    Reviewed-on: https://chromium-review.googlesource.com/870611
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529736}
    Former-commit-id: db87bb2a48e652bf8db4a6c14b523e9ee055e1c4

--- ui/gfx/text_elider.cc
+++ ui/gfx/text_elider.cc
@@ -498,7 +498,12 @@ class RectangleText {
         available_pixel_height_(available_pixel_height),
         wrap_behavior_(wrap_behavior),
         typesetter_(typesetter),
-        lines_(lines) {}
+        current_width_(0),
+        current_height_(0),
+        last_line_ended_in_lf_(false),
+        lines_(lines),
+        insufficient_width_(false),
+        insufficient_height_(false) {}
 
   // Perform deferred initializions following creation.  Must be called
   // before any input can be added via AddString().
@@ -560,29 +565,26 @@ class RectangleText {
   const Typesetter typesetter_;
 
   // The current running width.
-  float current_width_ = 0;
+  float current_width_;
 
   // The current running height.
-  int current_height_ = 0;
+  int current_height_;
 
   // The current line of text.
   base::string16 current_line_;
 
   // Indicates whether the last line ended with \n.
-  bool last_line_ended_in_lf_ = false;
+  bool last_line_ended_in_lf_;
 
   // The output vector of lines.
   std::vector<base::string16>* lines_;
 
   // Indicates whether a word was so long that it had to be truncated or elided
   // to fit the available width.
-  bool insufficient_width_ = false;
+  bool insufficient_width_;
 
   // Indicates whether there were too many lines for the available height.
-  bool insufficient_height_ = false;
-
-  // Indicates whether the very first word was truncated.
-  bool first_word_truncated_ = false;
+  bool insufficient_height_;
 
   DISALLOW_COPY_AND_ASSIGN(RectangleText);
 };
@@ -617,8 +619,7 @@ int RectangleText::Finalize() {
   if (last_line_ended_in_lf_)
     lines_->push_back(base::string16());
   return (insufficient_width_ ? INSUFFICIENT_SPACE_HORIZONTAL : 0) |
-         (insufficient_height_ ? INSUFFICIENT_SPACE_VERTICAL : 0) |
-         (first_word_truncated_ ? INSUFFICIENT_SPACE_FOR_FIRST_WORD : 0);
+         (insufficient_height_ ? INSUFFICIENT_SPACE_VERTICAL : 0);
 }
 
 void RectangleText::AddLine(const base::string16& line) {
@@ -686,8 +687,6 @@ int RectangleText::AddWordOverflow(const base::string16& word) {
     if (!NewLine())
       return 0;
     lines_added++;
-  } else if (lines_->empty()) {
-    first_word_truncated_ = true;
   }
 
   if (wrap_behavior_ == IGNORE_LONG_WORDS) {
