d6b8e0ea9e5b751c14fd1610300e136c0f3f8333
Ryan Harrison <rharrison@chromium.org>
2018-01-17 15:47:23 +0000

    Add handling specific to XFA fields
    The current mouse down logic assumes there is going to be an
    annotation for a form field. This is only true if the field is an
    AcroForm field. If it is an XFA field then there is no annotation.
    
    This CL depends on
    https://pdfium-review.googlesource.com/c/pdfium/+/22677 being rolled
    into chromium.
    
    BUG=pdfium:952,chromium:763129
    
    Change-Id: I1aafb646e7fb1bfb5f4f2a499f983809ef3279cf
    Reviewed-on: https://chromium-review.googlesource.com/860876
    Reviewed-by: dsinclair <dsinclair@chromium.org>
    Commit-Queue: Ryan Harrison <rharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529752}
    Former-commit-id: 868062045852303b6536137c48eb2ca1c55d4119

--- pdf/pdfium/pdfium_engine.cc
+++ pdf/pdfium/pdfium_engine.cc
@@ -3105,7 +3105,8 @@ void PDFiumEngine::LoadForm() {
     FPDF_LoadXFA(doc_);
 #endif
 
-    FPDF_SetFormFieldHighlightColor(form_, 0, kFormHighlightColor);
+    FPDF_SetFormFieldHighlightColor(form_, FPDF_FORMFIELD_UNKNOWN,
+                                    kFormHighlightColor);
     FPDF_SetFormFieldHighlightAlpha(form_, kFormHighlightAlpha);
   }
 }  // namespace chrome_pdf
@@ -3972,9 +3973,16 @@ bool PDFiumEngine::IsPointInEditableFormTextArea(FPDF_PAGE page,
                                                  double page_x,
                                                  double page_y,
                                                  int form_type) {
+#if defined(PDF_ENABLE_XFA)
+  if (IS_XFA_FORMFIELD(form_type))
+    return form_type == FPDF_FORMFIELD_XFA_TEXTFIELD ||
+           form_type == FPDF_FORMFIELD_XFA_COMBOBOX;
+#endif  // defined(PDF_ENABLE_XFA)
+
   FPDF_ANNOTATION annot =
       FPDFAnnot_GetFormFieldAtPoint(form_, page, page_x, page_y);
-  DCHECK(annot);
+  if (!annot)
+    return false;
 
   int flags = FPDFAnnot_GetFormFieldFlags(page, annot);
   bool is_editable_form_text_area =
