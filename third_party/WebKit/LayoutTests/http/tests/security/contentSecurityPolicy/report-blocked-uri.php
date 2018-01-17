a93a20f958d5bb327520811f0a332bc2b61d6b71
Andy Paicu <andypaicu@chromium.org>
2018-01-17 07:38:48 +0000

    Fixed wpt csp reporting tests and moved over chrome internal ones
    Bug: 694525,802188
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I34cca4afc854ffb4f02fc705c4f6293fdc0d51e7
    Reviewed-on: https://chromium-review.googlesource.com/552640
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529668}
    Former-commit-id: e8a7911921a687cc31fcd51576ee9450ac6cbb1b

--- third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/report-blocked-uri.php
+++
@@ -1,7 +0,0 @@
-<?php
-header("Content-Security-Policy-Report-Only: img-src 'none'; report-uri resources/save-report.php?test=report-blocked-uri.php");
-?>
-<script src="resources/report-test.js"></script>
-The URI of this image should show up in the violation report.
-<img src="../resources/abe.png#the-fragment-should-not-be-in-report">
-<script src="resources/go-to-echo-report.js"></script>
--- third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/report-only-report-uri-missing.php
+++ third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/report-only-report-uri-missing.php
@@ -4,7 +4,14 @@ header("Content-Security-Policy-Report-Only: script-src 'unsafe-inline';");
 <!DOCTYPE html>
 <html>
 <head>
-    <script src="resources/report-test.js"></script>
+    <script>
+        if (window.testRunner) {
+          testRunner.dumpAsText();
+          testRunner.dumpPingLoaderCallbacks();
+        }
+        if (window.internals)
+          internals.settings.setExperimentalContentSecurityPolicyFeaturesEnabled(false);
+    </script>
 </head>
 <body>
     <p>This test passes if a console message is present, warning about the missing 'report-uri' directive.</p>
