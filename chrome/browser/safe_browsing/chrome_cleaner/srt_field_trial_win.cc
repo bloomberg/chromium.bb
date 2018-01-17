8a62b31c6adac805ba1868b8bcbe9f8bde7d3ef1
Fabio Tirelo <ftirelo@chromium.org>
2018-01-17 22:23:20 +0000

    Remove chrome-prompt param from cleaner dl url
    Bug: 776538
    Change-Id: I183b7f5afd8542bfc084c95dd4776e15fa3c485f
    Reviewed-on: https://chromium-review.googlesource.com/867475
    Reviewed-by: Chris Sharp <csharp@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529918}
    Former-commit-id: 826434b4506fdbee3b56868fd1f6778dd3e7e21d

--- chrome/browser/safe_browsing/chrome_cleaner/srt_field_trial_win.cc
+++ chrome/browser/safe_browsing/chrome_cleaner/srt_field_trial_win.cc
@@ -28,10 +28,10 @@ const char kDownloadRootPath[] =
 // The download links of the Software Removal Tool.
 const char kMainSRTDownloadURL[] =
     "https://dl.google.com/dl"
-    "/softwareremovaltool/win/chrome_cleanup_tool.exe?chrome-prompt=1";
+    "/softwareremovaltool/win/chrome_cleanup_tool.exe";
 const char kCanarySRTDownloadURL[] =
     "https://dl.google.com/dl"
-    "/softwareremovaltool/win/c/chrome_cleanup_tool.exe?chrome-prompt=1";
+    "/softwareremovaltool/win/c/chrome_cleanup_tool.exe";
 
 }  // namespace
 
@@ -89,7 +89,7 @@ GURL GetSRTDownloadURL() {
   // https://dl.google.com/.../win/{arch}/{group}/chrome_cleanup_tool.exe
   std::string download_url_str = std::string(kDownloadRootPath) + architecture +
                                  "/" + download_group +
-                                 "/chrome_cleanup_tool.exe?chrome-prompt=1";
+                                 "/chrome_cleanup_tool.exe";
   GURL download_url(download_url_str);
 
   // Ensure URL construction didn't change origin.
