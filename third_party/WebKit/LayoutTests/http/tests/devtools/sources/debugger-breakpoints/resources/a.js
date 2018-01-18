8744feddfe2bd9b2da8e9b5410ded7b741467100
Alexey Kozyatinskiy <kozyatinskiy@chromium.org>
2018-01-18 00:44:51 +0000

    [DevTools] remove breakpoint-manager test, part 1
    Tests set-breakpoint in separate test without using mocks.
    + fixed a bug - we used to request inline decorations for disabled
    breakpoints as well.
    
    R=dgozman@chromium.org
    
    Bug: chromium:803241
    Change-Id: I8cd262016228354accb16e84298524eef2487def
    Reviewed-on: https://chromium-review.googlesource.com/871799
    Commit-Queue: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529982}
    Former-commit-id: 6d4ff09053af834ff69b03372e83ac2dcc6e9e57

---
+++ third_party/WebKit/LayoutTests/http/tests/devtools/sources/debugger-breakpoints/resources/a.js
@@ -0,0 +1,19 @@
+function foo() {
+  return 42;
+}
+
+function boo() {
+  return 239;
+}
+
+function main() {
+  var a = 1;
+  var b = 2;
+  return a + b;
+}
+
+function process(a, b) {
+  a = a * 2;
+  b = b / 2;
+  return a + b;
+}
