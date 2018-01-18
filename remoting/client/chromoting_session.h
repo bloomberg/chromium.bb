45e6486315f07c3a1981e7241edf4146ce7538b7
Yuwei Huang <yuweih@chromium.org>
2018-01-18 20:10:12 +0000

    [Remoting Mobile] Rewire connection state reporting logic for telemetry
    This CL improves the connection state report accuracy such that:
    * If session is disconnected before the CONNECTED state, report a
      CANCEL event. Previously nothing is reported in this state.
    * If session is disconnected during the CONNECTED state, report a CLOSE
      event.
    * On iOS, report an AUTHENTICATION_FAILED error if the user tries to do
      a third party auth, which is not supported. Previously we don't
      report anything and when this happens.
    
    Bug: 793995
    Change-Id: I9155c0efe43892149179e70f52ecee17434ff88e
    Reviewed-on: https://chromium-review.googlesource.com/869457
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530249}
    Former-commit-id: 9c5c3cc45f9ee2dc3051dd41b94e98228f1d8d4e

--- remoting/client/chromoting_session.h
+++ remoting/client/chromoting_session.h
@@ -90,14 +90,18 @@ class ChromotingSession : public ClientUserInterface,
   // Must be called before destruction.
   void Disconnect();
 
-  // Requests the android app to fetch a third-party token.
+  // Similar to Disconnect(), except that this method allows you to specify the
+  // reason to disconnect, which will be reported to telemetry.
+  void DisconnectForReason(protocol::ErrorCode error);
+
+  // Requests the client to fetch a third-party token.
   void FetchThirdPartyToken(
       const std::string& host_public_key,
       const std::string& token_url,
       const std::string& scope,
       const protocol::ThirdPartyTokenFetchedCallback& token_fetched_callback);
 
-  // Called by the android app when the token is fetched.
+  // Called by the client when the token is fetched.
   void HandleOnThirdPartyTokenFetched(const std::string& token,
                                       const std::string& shared_secret);
 
@@ -211,9 +215,9 @@ class ChromotingSession : public ClientUserInterface,
   // set of capabilities for this remoting session.
   std::string capabilities_;
 
-  // Indicates whether the client is connected to the host. Used on network
-  // thread.
-  bool connected_ = false;
+  // The current session state. Used on network thread.
+  protocol::ConnectionToHost::State session_state_ =
+      protocol::ConnectionToHost::INITIALIZING;
 
   std::unique_ptr<ClientTelemetryLogger> logger_;
 
