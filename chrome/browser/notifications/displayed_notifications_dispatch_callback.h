b921b8ae6385c5c7b1a4759941293efb29c1be5a
Anita Woodruff <awdf@chromium.org>
2018-01-18 16:28:39 +0000

    [Notifications] Improve callback documentation for getDisplayed
    Change-Id: I69d99479777347a8f9036792a03e6ad096970f96
    Reviewed-on: https://chromium-review.googlesource.com/874070
    Commit-Queue: Anita Woodruff <awdf@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530161}
    Former-commit-id: 692248a91318264677b99a68667fab3a2e8afa31

--- chrome/browser/notifications/displayed_notifications_dispatch_callback.h
+++ chrome/browser/notifications/displayed_notifications_dispatch_callback.h
@@ -11,8 +11,15 @@
 
 // Callback used by the bridge and all the downstream classes that propagate
 // the callback to get displayed notifications.
+//
+// |supports_synchronization| will be true if the platform supports getting the
+// currently displayed notifications.
+//
+// If |supports_synchronization| is true, then |notification_ids| will contain
+// the ids of the currently displayed notifications, otherwise the value of
+// |notification_ids| should be ignored.
 using GetDisplayedNotificationsCallback =
-    base::Callback<void(std::unique_ptr<std::set<std::string>>,
-                        bool /* supports_synchronization */)>;
+    base::Callback<void(std::unique_ptr<std::set<std::string>> notification_ids,
+                        bool supports_synchronization)>;
 
 #endif  // CHROME_BROWSER_NOTIFICATIONS_DISPLAYED_NOTIFICATIONS_DISPATCH_CALLBACK_H_
