c4b5fca617a96d0955988fc0883acfbf55ed8dc3
Derek Cheng <imcheng@chromium.org>
2018-01-18 10:20:01 +0000

    [Media Router] Cast sink queries part 1.
    This patch defines some utility methods that will be used in subsequent
    patches:
    
    cast_message_util:
    - virtual connection and app availability requests
    - parse additional namespaces
    
    media_source_helper:
    - helper method for legacy cast urls
    
    cast_socket:
    - added missing enum value in CastDeviceCapability
    
    Bug: 698940
    Change-Id: Ie5fef9ca522ffb9375183dafb72e3209fd0fa5c6
    Reviewed-on: https://chromium-review.googlesource.com/869951
    Commit-Queue: Derek Cheng <imcheng@chromium.org>
    Reviewed-by: Bin Zhao <zhaobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530108}
    Former-commit-id: 6a28c49238f30bc180b265ec7432d5f4bd3d18f5

--- components/cast_channel/cast_message_util.cc
+++ components/cast_channel/cast_message_util.cc
@@ -10,9 +10,10 @@
 #include "base/json/json_writer.h"
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/values.h"
 #include "components/cast_channel/cast_auth_util.h"
 #include "components/cast_channel/proto/cast_channel.pb.h"
+
+using base::Value;
 
 namespace cast_channel {
 
@@ -21,6 +22,9 @@ namespace {
 constexpr char kAuthNamespace[] = "urn:x-cast:com.google.cast.tp.deviceauth";
 constexpr char kHeartbeatNamespace[] =
     "urn:x-cast:com.google.cast.tp.heartbeat";
+constexpr char kConnectionNamespace[] =
+    "urn:x-cast:com.google.cast.tp.connection";
+constexpr char kReceiverNamespace[] = "urn:x-cast:com.google.cast.receiver";
 
 // Sender and receiver IDs to use for platform messages.
 constexpr char kPlatformSenderId[] = "sender-0";
@@ -28,17 +32,28 @@ constexpr char kPlatformReceiverId[] = "receiver-0";
 
 // Text payload keys.
 constexpr char kTypeNodeId[] = "type";
+constexpr char kRequestIdNodeId[] = "requestId";
 
 // Cast application protocol message types.
 constexpr char kKeepAlivePingType[] = "PING";
 constexpr char kKeepAlivePongType[] = "PONG";
+constexpr char kGetAppAvailabilityRequestType[] = "GET_APP_AVAILABILITY";
+constexpr char kConnectionRequestType[] = "CONNECT";
+
+void FillCommonCastMessageFields(CastMessage* message,
+                                 const std::string& source_id,
+                                 const std::string& destination_id,
+                                 const std::string& message_namespace) {
+  message->set_protocol_version(CastMessage::CASTV2_1_0);
+  message->set_source_id(source_id);
+  message->set_destination_id(destination_id);
+  message->set_namespace_(message_namespace);
+}
 
 CastMessage CreateKeepAliveMessage(const char* keep_alive_type) {
   CastMessage output;
-  output.set_protocol_version(CastMessage::CASTV2_1_0);
-  output.set_source_id(kPlatformSenderId);
-  output.set_destination_id(kPlatformReceiverId);
-  output.set_namespace_(kHeartbeatNamespace);
+  FillCommonCastMessageFields(&output, kPlatformSenderId, kPlatformReceiverId,
+                              kHeartbeatNamespace);
   output.set_payload_type(
       CastMessage::PayloadType::CastMessage_PayloadType_STRING);
 
@@ -61,15 +76,26 @@ bool IsCastMessageValid(const CastMessage& message_proto) {
           message_proto.has_payload_binary());
 }
 
-CastMessageType ParseMessageType(const CastMessage& message) {
-  std::unique_ptr<base::Value> parsed_payload(
+std::unique_ptr<Value> GetDictionaryFromCastMessage(
+    const CastMessage& message) {
+  if (!message.has_payload_utf8())
+    return nullptr;
+
+  std::unique_ptr<Value> parsed_payload(
       base::JSONReader::Read(message.payload_utf8()));
-  base::DictionaryValue* payload_as_dict;
-  if (!parsed_payload || !parsed_payload->GetAsDictionary(&payload_as_dict))
+  if (!parsed_payload || !parsed_payload->is_dict())
+    return nullptr;
+
+  return parsed_payload;
+}
+
+CastMessageType ParseMessageType(const CastMessage& message) {
+  std::unique_ptr<Value> dictionary = GetDictionaryFromCastMessage(message);
+  if (!dictionary)
     return CastMessageType::kOther;
 
   const base::Value* type_string =
-      payload_as_dict->FindKeyOfType(kTypeNodeId, base::Value::Type::STRING);
+      dictionary->FindKeyOfType(kTypeNodeId, base::Value::Type::STRING);
   if (!type_string)
     return CastMessageType::kOther;
 
@@ -141,10 +167,8 @@ void CreateAuthChallengeMessage(CastMessage* message_proto,
   std::string auth_message_string;
   auth_message.SerializeToString(&auth_message_string);
 
-  message_proto->set_protocol_version(CastMessage::CASTV2_1_0);
-  message_proto->set_source_id(kPlatformSenderId);
-  message_proto->set_destination_id(kPlatformReceiverId);
-  message_proto->set_namespace_(kAuthNamespace);
+  FillCommonCastMessageFields(message_proto, kPlatformSenderId,
+                              kPlatformReceiverId, kAuthNamespace);
   message_proto->set_payload_type(CastMessage_PayloadType_BINARY);
   message_proto->set_payload_binary(auth_message_string);
 }
@@ -153,12 +177,84 @@ bool IsAuthMessage(const CastMessage& message) {
   return message.namespace_() == kAuthNamespace;
 }
 
+bool IsReceiverMessage(const CastMessage& message) {
+  return message.namespace_() == kReceiverNamespace;
+}
+
+bool IsPlatformSenderMessage(const CastMessage& message) {
+  return message.destination_id() != cast_channel::kPlatformSenderId;
+}
+
 CastMessage CreateKeepAlivePingMessage() {
   return CreateKeepAliveMessage(kKeepAlivePingType);
 }
 
 CastMessage CreateKeepAlivePongMessage() {
   return CreateKeepAliveMessage(kKeepAlivePongType);
+}
+
+CastMessage CreateVirtualConnectionRequest(const std::string& source_id,
+                                           const std::string& destination_id) {
+  CastMessage output;
+  FillCommonCastMessageFields(&output, source_id, destination_id,
+                              kConnectionNamespace);
+  output.set_payload_type(
+      CastMessage::PayloadType::CastMessage_PayloadType_STRING);
+
+  Value dict(Value::Type::DICTIONARY);
+  dict.SetKey(kTypeNodeId, Value(kConnectionRequestType));
+  // TODO(crbug.com/698940): Populate other optional fields.
+  CHECK(base::JSONWriter::Write(dict, output.mutable_payload_utf8()));
+  return output;
+}
+
+CastMessage CreateGetAppAvailabilityRequest(const std::string& source_id,
+                                            int request_id,
+                                            const std::string& app_id) {
+  CastMessage output;
+  FillCommonCastMessageFields(&output, source_id, kPlatformReceiverId,
+                              kReceiverNamespace);
+  output.set_payload_type(
+      CastMessage::PayloadType::CastMessage_PayloadType_STRING);
+
+  Value dict(Value::Type::DICTIONARY);
+  dict.SetKey(kTypeNodeId, Value(kGetAppAvailabilityRequestType));
+  Value app_id_value(Value::Type::LIST);
+  app_id_value.GetList().push_back(Value(app_id));
+  dict.SetKey("appId", std::move(app_id_value));
+  dict.SetKey(kRequestIdNodeId, Value(request_id));
+  CHECK(base::JSONWriter::Write(dict, output.mutable_payload_utf8()));
+  return output;
+}
+
+bool GetRequestIdFromResponse(const Value& payload, int* request_id) {
+  DCHECK(request_id);
+  DCHECK(payload.is_dict());
+
+  const Value* request_id_value =
+      payload.FindKeyOfType(kRequestIdNodeId, Value::Type::INTEGER);
+  if (!request_id_value)
+    return false;
+
+  *request_id = request_id_value->GetInt();
+  return true;
+}
+
+GetAppAvailabilityResult GetAppAvailabilityResultFromResponse(
+    const Value& payload,
+    const std::string& app_id) {
+  DCHECK(payload.is_dict());
+  const Value* availability_value =
+      payload.FindPathOfType({"availability", app_id}, Value::Type::STRING);
+  if (!availability_value)
+    return GetAppAvailabilityResult::kUnknown;
+
+  if (availability_value->GetString() == "APP_AVAILABLE")
+    return GetAppAvailabilityResult::kAvailable;
+  if (availability_value->GetString() == "APP_UNAVAILABLE")
+    return GetAppAvailabilityResult::kUnavailable;
+
+  return GetAppAvailabilityResult::kUnknown;
 }
 
 }  // namespace cast_channel
--- components/cast_channel/cast_message_util.h
+++ components/cast_channel/cast_message_util.h
@@ -7,6 +7,8 @@
 
 #include <string>
 
+#include "base/values.h"
+
 namespace cast_channel {
 
 class AuthContext;
@@ -18,6 +20,11 @@ enum class CastMessageType { kOther, kPing, kPong };
 
 // Checks if the contents of |message_proto| are valid.
 bool IsCastMessageValid(const CastMessage& message_proto);
+
+// Parses and returns the UTF-8 payload from |message|. Returns nullptr
+// if the UTF-8 payload doesn't exist, or if it is not a dictionary.
+std::unique_ptr<base::Value> GetDictionaryFromCastMessage(
+    const CastMessage& message);
 
 // Parses the JSON-encoded payload of |message| and returns the value in the
 // "type" field or |kUnknown| if the parse fails or the field is not found.
@@ -41,9 +48,45 @@ void CreateAuthChallengeMessage(CastMessage* message_proto,
 // Returns whether the given message is an auth handshake message.
 bool IsAuthMessage(const CastMessage& message);
 
+// Returns whether |message| is a Cast receiver message.
+bool IsReceiverMessage(const CastMessage& message);
+
+// Returns whether |message| is destined for the platform sender.
+bool IsPlatformSenderMessage(const CastMessage& message);
+
 // Creates a keep-alive message of either type PING or PONG.
 CastMessage CreateKeepAlivePingMessage();
 CastMessage CreateKeepAlivePongMessage();
+
+// Creates a virtual connection request message for |source_id| and
+// |destination_id|.
+CastMessage CreateVirtualConnectionRequest(const std::string& source_id,
+                                           const std::string& destination_id);
+
+// Creates an app availability request for |app_id| from |source_id| with
+// ID |request_id|.
+// TODO(imcheng): May not need |source_id|, just use sender-0?
+CastMessage CreateGetAppAvailabilityRequest(const std::string& source_id,
+                                            int request_id,
+                                            const std::string& app_id);
+
+// Possible results of a GET_APP_AVAILABILITY request.
+enum class GetAppAvailabilityResult {
+  kAvailable,
+  kUnavailable,
+  kUnknown,
+};
+
+// Extracts request ID from |payload| corresponding to a Cast message response.
+// If request ID is available, assigns it to |request_id|. Return |true| if
+// request ID is found.
+bool GetRequestIdFromResponse(const base::Value& payload, int* request_id);
+
+// Returns the GetAppAvailabilityResult corresponding to |app_id| in |payload|.
+// Returns kUnknown if result is not found.
+GetAppAvailabilityResult GetAppAvailabilityResultFromResponse(
+    const base::Value& payload,
+    const std::string& app_id);
 
 }  // namespace cast_channel
 
