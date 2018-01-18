dfe6a9beb7c6db4330275892893e030ec554dd20
Michael Thiessen <mthiesse@chromium.org>
2018-01-18 20:27:52 +0000

    Revert "VR: Add dynamically-toggled icon in Daydream Home"
    This reverts commit b0c52b749022fc227f1eae28f6e130b4870956fd [formerly 559f95d6420338760f285a1b726abffd8a8bcd1c].
    
    Reason for revert: Fails APK validation because of 'missing Daydream icons'.
    
    Original change's description:
    > VR: Add dynamically-toggled icon in Daydream Home
    > 
    > This CL adds the ability to show the Chrome icon in Daydream Home, which
    > launches Chrome in VR mode, behind a feature flag.
    > 
    > We're able to do this by enabling/disabling the component that adds the
    > icon at runtime.
    > 
    > Bug: 802080
    > Change-Id: I878202c110d2a7bd092173457de73ce1578d2ff6
    > Reviewed-on: https://chromium-review.googlesource.com/867512
    > Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    > Reviewed-by: Yash Malik <ymalik@chromium.org>
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#529842}
    
    TBR=mthiesse@chromium.org,dtrainor@chromium.org,ymalik@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 802080
    Change-Id: I9a01dc2dcfaa0e5de07ccc16671721d61beb6feb
    Reviewed-on: https://chromium-review.googlesource.com/874781
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530252}
    Former-commit-id: 0c357720eef65cf038f52093a5a20e25b448c8cd

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -123,7 +123,6 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kVideoPersistence,
     &kVrBrowsingFeedback,
     &kVrBrowsingInCustomTab,
-    &kVrIconInDaydreamHome,
     &kVrLaunchIntents,
     &payments::features::kWebPaymentsMethodSectionOrderV2,
     &payments::features::kWebPaymentsModifiers,
@@ -377,9 +376,6 @@ const base::Feature kVrBrowsingFeedback{"VrBrowsingFeedback",
 
 const base::Feature kVrBrowsingInCustomTab{"VrBrowsingInCustomTab",
                                            base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kVrIconInDaydreamHome{"VrIconInDaydreamHome",
-                                          base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kVrLaunchIntents{"VrLaunchIntents",
                                      base::FEATURE_DISABLED_BY_DEFAULT};
