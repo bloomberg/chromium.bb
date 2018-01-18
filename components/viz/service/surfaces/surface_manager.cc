2738c2f05351d10a28bbd9b32fda5f61da0694fc
Fady Samuel <fsamuel@chromium.org>
2018-01-18 19:51:46 +0000

    Surface synchronization: Fix devtools bug
    GetLatestInFlightSurface can return the primary surface ID. This can
    cause a race where occasionally the primary surface will be closed making
    a page unusable. This CL fixes the issue by skipping the primary surface
    in GetLatestInFlightSurface.
    
    Bug: 803243, 672962
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Icfc0b96847962e94002b8d4e2885684348232b03
    Reviewed-on: https://chromium-review.googlesource.com/872454
    Commit-Queue: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Saman Sami <samans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530233}
    Former-commit-id: 3cb232c1a3c7776aa0ddb18088eda0d5701cb9f5

--- components/viz/service/surfaces/surface_manager.cc
+++ components/viz/service/surfaces/surface_manager.cc
@@ -508,8 +508,9 @@ Surface* SurfaceManager::GetLatestInFlightSurface(
 
   const std::vector<LocalSurfaceId>& temp_surfaces = it->second;
   for (const LocalSurfaceId& local_surface_id : base::Reversed(temp_surfaces)) {
-    // The in-flight surface cannot be newer than the primary surface ID.
-    if (local_surface_id.parent_sequence_number() >
+    // The in-flight surface must be older than the primary surface ID.
+    if (local_surface_id == primary_surface_id.local_surface_id() ||
+        local_surface_id.parent_sequence_number() >
             primary_surface_id.local_surface_id().parent_sequence_number() ||
         local_surface_id.child_sequence_number() >
             primary_surface_id.local_surface_id().child_sequence_number()) {
