dd2d5c3b0f4837da2576f1bd13dba21c3542b98b
Christian Fremerey <chfremer@chromium.org>
2018-01-18 00:50:21 +0000

    Enable video capture service by default on Linux
    This was disabled in https://chromium-review.googlesource.com/c/chromium/src/+/817995,
    because it caused test failures on webrtc bots involving physical webcams.
    
    Enabling the service by default caused these failures, because it enabled the
    service for image capture test cases which are known to have issues (see
    https://bugs.chromium.org/p/chromium/issues/detail?id=733582) to run with the
    service enabled even if they are supposed to explicitly only run without the
    service enabled.
    
    The fix is to explicitly disable the service for these cases.
    
    Bug: 721812
    Change-Id: I97f21eaf725cc36235c12889d9f30146c9b9a6ae
    Reviewed-on: https://chromium-review.googlesource.com/871847
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#529988}
    Former-commit-id: 6e5e63d2a023f1582b437b80a410575cdfbc4184

--- services/video_capture/public/cpp/constants.cc
+++ services/video_capture/public/cpp/constants.cc
@@ -10,7 +10,8 @@ namespace video_capture {
 
 const base::Feature kMojoVideoCapture {
   "MojoVideoCapture",
-#if defined(OS_MACOSX) || defined(OS_WIN)
+#if (defined(OS_LINUX) && !defined(OS_CHROMEOS)) || defined(OS_MACOSX) || \
+    defined(OS_WIN)
       base::FEATURE_ENABLED_BY_DEFAULT
 #else
       base::FEATURE_DISABLED_BY_DEFAULT
