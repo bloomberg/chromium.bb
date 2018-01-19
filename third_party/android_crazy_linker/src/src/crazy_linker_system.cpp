a24c904e3070259d8845a4f441e4c28b63024da7
David 'Digit' Turner <digit@google.com>
2018-01-19 11:51:00 +0000

    crazy_linker: Reduce binary size by 24 to 64 kiB
    The libc++ implementation of 'operator new' and 'operator delete'
    drags a lot of un-needed machine code related to exception handling
    (and surprinsgly dynamic_cast as well).
    
    Since the crazy linker (as well as the rest of Chromium) is
    built with exception support disabled, it is possible to provide
    a simpler implementation that only uses ::malloc() and ::free(),
    which is what this CL does.
    
    This results in significant binary size reductions, i.e.:
    
          Sizes of libchromium_android_linker.so in bytes:
    
                before    after     saved
    
          arm    58804    34216  -> 24588  (24 kiB)
          x86    91696    54688  -> 37008  (36 kiB)
          arm64 133288    67576  -> 65712  (64 kiB)
    
    NOTE: It should be possible to do the same for base// though
          the patch will be more sophisticated (as pointed to by
          lizeb@, we catch std::bad_alloc failures in order to
          create Breadkpad minidumps in case of out-of-memory).
    
    BUG=none
    R=pasko@chromium.org,primiano@chromium.org,agrieve@chromium.org,lizeb@chromium.org
    
    Change-Id: I8877bb1480e1c4d5bb666a3a8ef090b2dd4a3428
    Reviewed-on: https://chromium-review.googlesource.com/868011
    Commit-Queue: David Turner <digit@chromium.org>
    Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530487}
    Former-commit-id: b3afb6541c4b3c29f9f7b5797be82c6456da30d2

--- third_party/android_crazy_linker/src/src/crazy_linker_system.cpp
+++ third_party/android_crazy_linker/src/src/crazy_linker_system.cpp
@@ -10,6 +10,10 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#ifdef __ANDROID__
+#include <android/log.h>
+#endif
+
 #include "crazy_linker_util.h"
 
 // Note: unit-testing support files are in crazy_linker_files_mock.cpp
@@ -49,7 +53,14 @@ void* FileDescriptor::Map(void* address,
 void FileDescriptor::Close() {
   if (fd_ != -1) {
     int old_errno = errno;
-    TEMP_FAILURE_RETRY(close(fd_));
+    // SUBTLE: Do not loop when close() returns EINTR. On Linux, this simply
+    // means that a corresponding flush operation failed, but the file
+    // descriptor will always be closed anyway.
+    //
+    // Other platforms have different behavior: e.g. on OS X, this could be
+    // the result of an interrupt, and there is no reliable way to know
+    // whether the fd was closed or not on exit :-(
+    (void)close(fd_);
     errno = old_errno;
     fd_ = -1;
   }
@@ -88,3 +99,77 @@ bool PathIsFile(const char* path) {
 #endif  // !UNIT_TESTS
 
 }  // namespace crazy
+
+// Custom implementation of new and malloc, this prevents dragging
+// the libc++ implementation, which drags exception-related machine
+// code that is not needed here. This helps reduce the size of the
+// final binary considerably.
+
+// IMPORTANT: These symbols are not exported by the crazy linker, thus this
+//            does not affect the libraries that it will load, only the
+//            linker binary itself!
+//
+void* operator new(size_t size) {
+  void* ptr = ::malloc(size);
+  if (ptr != nullptr)
+    return ptr;
+
+  // Don't assume it is possible to call any C library function like
+  // snprintf() here, since it might allocate heap memory and crash at
+  // runtime. Hence our fatal message does not contain the number of
+  // bytes requested by the allocation.
+  static const char kFatalMessage[] = "Out of memory!";
+#ifdef __ANDROID__
+  __android_log_write(ANDROID_LOG_FATAL, "crazy_linker", kFatalMessage);
+#else
+  ::write(STDERR_FILENO, kFatalMessage, sizeof(kFatalMessage) - 1);
+#endif
+  _exit(1);
+#if defined(__GNUC__)
+  __builtin_unreachable();
+#endif
+
+  // NOTE: Adding a 'return nullptr' here will make the compiler error
+  // with a message stating that 'operator new(size_t)' is not allowed
+  // to return nullptr.
+  //
+  // Indeed, an new expression like 'new T' shall never return nullptr,
+  // according to the C++ specification, and an optimizing compiler will gladly
+  // remove any null-checks after them (something the Fuschsia team had to
+  // learn the hard way when writing their kernel in C++). What is meant here
+  // is something like:
+  //
+  //   Foo* foo = new Foo(10);
+  //   if (!foo) {                             <-- entire check and branch
+  //      ... Handle out-of-memory condition.  <-- removed by an optimizing
+  //   }                                       <-- compiler.
+  //
+  // Note that some C++ library implementations (e.g. recent libc++) recognize
+  // when they are compiled with -fno-exceptions and provide a simpler version
+  // of operator new that can return nullptr. However, it is very hard to
+  // guarantee at build time that this code is linked against such a version
+  // of the runtime. Moreoever, technically disabling exceptions is completely
+  // out-of-spec regarding the C++ language, and what the compiler is allowed
+  // to do in this case is mostly implementation-defined, so better be safe
+  // than sorry here.
+  //
+  // C++ provides a non-throwing new expression that can return a nullptr
+  // value, but it must be written as 'new (std::nothrow) T' instead of
+  // 'new T', and thus nobody uses this. This ends up calling
+  // 'operator new(size_t, const std::nothrow_t&)' which is not implemented
+  // here.
+}
+
+void* operator new[](size_t size) {
+  return operator new(size);
+}
+
+void operator delete(void* ptr) {
+  // The compiler-generated code already checked that |ptr != nullptr|
+  // so don't to it a second time.
+  ::free(ptr);
+}
+
+void operator delete[](void* ptr) {
+  ::free(ptr);
+}
