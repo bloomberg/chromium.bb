d749b6eb5482cfe05281e9c2f17baac02a25399b
Eugene But <eugenebut@google.com>
2018-01-19 23:52:55 +0000

    Prevent ExecuteBlockAndWaitForLoad from hanging on the bots.
    Replace WaitUntilCondition with WaitUntilConditionOrTimeout.
    
    Bug: 784735
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I73039a4c54223d64ddab73a8ef9a7eb8e93ea1a3
    Reviewed-on: https://chromium-review.googlesource.com/874552
    Reviewed-by: Yuke Liao <liaoyuke@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530667}
    Former-commit-id: c101865c0fb75d315e42be50b2218de4b28a4a81

--- ios/web/test/web_int_test.mm
+++ ios/web/test/web_int_test.mm
@@ -8,6 +8,7 @@
 #include "base/memory/ptr_util.h"
 #include "base/scoped_observer.h"
 #import "base/test/ios/wait_util.h"
+#import "ios/testing/wait_util.h"
 #import "ios/web/public/test/http_server/http_server.h"
 #import "ios/web/public/test/js_test_util.h"
 #include "ios/web/public/web_state/web_state_observer.h"
@@ -16,6 +17,8 @@
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+using testing::WaitUntilConditionOrTimeout;
 
 namespace web {
 
@@ -91,7 +94,7 @@ id WebIntTest::ExecuteJavaScript(NSString* script) {
   return web::ExecuteJavaScript(web_state()->GetJSInjectionReceiver(), script);
 }
 
-void WebIntTest::ExecuteBlockAndWaitForLoad(const GURL& url,
+bool WebIntTest::ExecuteBlockAndWaitForLoad(const GURL& url,
                                             ProceduralBlock block) {
   DCHECK(block);
 
@@ -104,21 +107,21 @@ void WebIntTest::ExecuteBlockAndWaitForLoad(const GURL& url,
   // Need to use a pointer to |observer| as the block wants to capture it by
   // value (even if marked with __block) which would not work.
   IntTestWebStateObserver* observer_ptr = &observer;
-  base::test::ios::WaitUntilCondition(^bool {
+  return WaitUntilConditionOrTimeout(testing::kWaitForPageLoadTimeout, ^{
     return observer_ptr->IsExpectedPageLoaded();
   });
 }
 
-void WebIntTest::LoadUrl(const GURL& url) {
+bool WebIntTest::LoadUrl(const GURL& url) {
   web::NavigationManager::WebLoadParams params(url);
   params.transition_type = ui::PageTransition::PAGE_TRANSITION_TYPED;
-  LoadWithParams(params);
+  return LoadWithParams(params);
 }
 
-void WebIntTest::LoadWithParams(
+bool WebIntTest::LoadWithParams(
     const NavigationManager::WebLoadParams& params) {
   NavigationManager::WebLoadParams block_params(params);
-  ExecuteBlockAndWaitForLoad(params.url, ^{
+  return ExecuteBlockAndWaitForLoad(params.url, ^{
     navigation_manager()->LoadURLWithParams(block_params);
   });
 }
