002c42d27b177a65cf703fb70feb3fca00f76db5
Weidong Guo <weidongg@chromium.org>
2018-01-19 20:15:28 +0000

    Fix incorrect placeholder text in feedback dialog.
    Changes:
    Avoid showing description placeholder if it is undefined.
    
    BUG=803030
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Iecdfddaa53dc0cb1343b5ba66060df74cbc6da87
    Reviewed-on: https://chromium-review.googlesource.com/874550
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Weidong Guo <weidongg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530597}
    Former-commit-id: 58f6e6d431069413ac0dabd478f0b1956a1babf5

--- chrome/browser/resources/feedback/js/feedback.js
+++ chrome/browser/resources/feedback/js/feedback.js
@@ -321,7 +321,8 @@ function initialize() {
       }
 
       $('description-text').textContent = feedbackInfo.description;
-      $('description-text').placeholder = feedbackInfo.descriptionPlaceholder;
+      if (feedbackInfo.descriptionPlaceholder)
+        $('description-text').placeholder = feedbackInfo.descriptionPlaceholder;
       if (feedbackInfo.pageUrl)
         $('page-url-text').value = feedbackInfo.pageUrl;
 
