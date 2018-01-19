d393dafce9f97cf87db5d853cf49beced86e5a8b
Julia Tuttle <juliatuttle@chromium.org>
2018-01-19 23:55:25 +0000

    Network Error Logging: Check for and fix AppRequestContext isolation.
    Apps should be able to use NEL, but they shouldn't share state with the
    main request context.
    
    Bug: 803604
    Change-Id: I6533ce46e21c2b47e888524aeeeb293367919ab3
    Reviewed-on: https://chromium-review.googlesource.com/875050
    Commit-Queue: Julia Tuttle <juliatuttle@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530668}
    Former-commit-id: 102bd7a31c15ce2521b48e90e0ac2d64ab0a5466

--- chrome/browser/profiles/off_the_record_profile_io_data.cc
+++ chrome/browser/profiles/off_the_record_profile_io_data.cc
@@ -1,4 +1,3 @@
-
 // Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -34,6 +33,7 @@
 #include "net/http/http_cache.h"
 #include "net/http/http_network_session.h"
 #include "net/http/http_server_properties_impl.h"
+#include "net/network_error_logging/network_error_logging_service.h"
 #include "net/reporting/reporting_policy.h"
 #include "net/reporting/reporting_service.h"
 #include "net/ssl/channel_id_service.h"
@@ -284,7 +284,12 @@ net::URLRequestContext* OffTheRecordProfileIOData::InitializeAppRequestContext(
     context->SetReportingService(net::ReportingService::Create(
         context->reporting_service()->GetPolicy(), context));
   }
-  // TODO(juliatuttle): Should also isolate NetworkErrorLoggingDelegate.
+  if (context->network_error_logging_delegate()) {
+    context->SetNetworkErrorLoggingDelegate(
+        net::NetworkErrorLoggingService::Create());
+    context->network_error_logging_delegate()->SetReportingService(
+        context->reporting_service());
+  }
   return context;
 }
 
