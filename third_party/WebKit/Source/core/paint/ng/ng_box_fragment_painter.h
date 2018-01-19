2610aeeef4c3caac31bdacae07004a5224f45c6d
Fredrik Söderquist <fs@opera.com>
2018-01-19 18:38:48 +0000

    [CI] Share more text-clip masking code between NG and non-NG painters
    Split out a PaintTextClipMask method and make that virtual instead of
    PaintFillLayerTextFillBox. Move PaintFillLayerTextFillBox to
    BoxPainterBase and call PaintTextClipMask for the bits that differ.
    
    Bug: 803382
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_layout_ng;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ie44a64a6e70eac8a5a1029650fd206fc9b2852f7
    Reviewed-on: https://chromium-review.googlesource.com/874074
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530573}
    Former-commit-id: c966cda909ebd6ff72f33a60d0ce57b917cd8a6e

--- third_party/WebKit/Source/core/paint/ng/ng_box_fragment_painter.h
+++ third_party/WebKit/Source/core/paint/ng/ng_box_fragment_painter.h
@@ -19,7 +19,6 @@ class FillLayer;
 class HitTestLocation;
 class HitTestRequest;
 class HitTestResult;
-class Image;
 class LayoutRect;
 class NGPaintFragment;
 class NGPhysicalFragment;
@@ -50,13 +49,9 @@ class NGBoxFragmentPainter : public BoxPainterBase {
       const FillLayer&,
       BackgroundBleedAvoidance) const override;
 
-  void PaintFillLayerTextFillBox(GraphicsContext&,
-                                 const BoxPainterBase::FillLayerInfo&,
-                                 Image*,
-                                 SkBlendMode composite_op,
-                                 const BackgroundImageGeometry&,
-                                 const LayoutRect&,
-                                 LayoutRect scrolled_paint_rect) override;
+  void PaintTextClipMask(GraphicsContext&,
+                         const IntRect& mask_rect,
+                         const LayoutPoint& paint_offset) override;
   LayoutRect AdjustForScrolledContent(const PaintInfo&,
                                       const BoxPainterBase::FillLayerInfo&,
                                       const LayoutRect&) override;
