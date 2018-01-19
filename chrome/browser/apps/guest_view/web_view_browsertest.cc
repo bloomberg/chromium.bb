0035e012fc4e64e5bb8247e83fa85e7070993f86
Kevin McNee <mcnee@chromium.org>
2018-01-19 15:37:49 +0000

    Reenable WebViewTest.LoadWebviewInsideIframe under MSan.
    I believe that
    https://chromium-review.googlesource.com/c/chromium/src/+/868277
    fixes the issue which was causing this test to flake.
    
    Bug: 793422
    Change-Id: Ie74a8dc9e3b36f609210db89b453960e0d8b7286
    Reviewed-on: https://chromium-review.googlesource.com/875111
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530514}
    Former-commit-id: 1bf7fba85eb32436e2ec557a21f88e42fc3bfd86

--- chrome/browser/apps/guest_view/web_view_browsertest.cc
+++ chrome/browser/apps/guest_view/web_view_browsertest.cc
@@ -3685,13 +3685,7 @@ IN_PROC_BROWSER_TEST_P(WebViewTest, LoadWebviewInaccessibleResource) {
 
 // Tests that a webview inside an iframe can load and that it is destroyed when
 // the iframe is detached.
-// Flakily times out under MSan. crbug.com/793422
-#if defined(MEMORY_SANITIZER)
-#define MAYBE_LoadWebviewInsideIframe DISABLED_LoadWebviewInsideIframe
-#else
-#define MAYBE_LoadWebviewInsideIframe LoadWebviewInsideIframe
-#endif
-IN_PROC_BROWSER_TEST_P(WebViewTest, MAYBE_LoadWebviewInsideIframe) {
+IN_PROC_BROWSER_TEST_P(WebViewTest, LoadWebviewInsideIframe) {
   TestHelper("testLoadWebviewInsideIframe",
              "web_view/load_webview_inside_iframe", NEEDS_TEST_SERVER);
 
