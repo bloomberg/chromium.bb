0ff0df57b7ed3eba65eef84e4675f95dcd584088
Kurt Horimoto <kkhorimoto@chromium.org>
2018-01-19 23:24:56 +0000

    [iOS] Fix CRWWebController's CRWSSLStatusUpdaterDelegate implementation.
    The WebStateObserver callback specifies that the change is for the
    visible SSL status, so we should be checking agains the visible item.
    
    Bug: none
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ie0eca53ded643167e3f48a94e80ea5090277712a
    Reviewed-on: https://chromium-review.googlesource.com/877103
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530652}
    Former-commit-id: 6a0f33fbd821bf0a05c1c07a2c189aab987b09d2

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -4729,11 +4729,10 @@ registerLoadRequestForURL:(const GURL&)requestURL
 
 - (void)SSLStatusUpdater:(CRWSSLStatusUpdater*)SSLStatusUpdater
     didChangeSSLStatusForNavigationItem:(web::NavigationItem*)navigationItem {
-  web::NavigationItem* lastCommittedNavigationItem =
-      _webStateImpl->GetNavigationManager()->GetLastCommittedItem();
-  if (navigationItem == lastCommittedNavigationItem) {
+  web::NavigationItem* visibleItem =
+      _webStateImpl->GetNavigationManager()->GetVisibleItem();
+  if (navigationItem == visibleItem)
     _webStateImpl->DidChangeVisibleSecurityState();
-  }
 }
 
 #pragma mark -
