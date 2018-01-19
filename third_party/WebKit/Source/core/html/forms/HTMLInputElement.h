f57adeff7701fd68ea9ed0d9746d556fda651ff2
Shanmuga Pandi M <shanmuga.m@samsung.com>
2018-01-19 06:26:58 +0000

    Emit "input" event on activation behavior for CheckBox
    Intent to implement and ship:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/Qc-bOSyU0FQ
    
    Bug: 534245
    Change-Id: Ib97235c26b4b3ccd681e708e232a782d5612745c
    Reviewed-on: https://chromium-review.googlesource.com/861601
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Shanmuga Pandi <shanmuga.m@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#530435}
    Former-commit-id: 54f8d098b694704893f7abdbba3b1a6ee18ed687

--- third_party/WebKit/Source/core/html/forms/HTMLInputElement.h
+++ third_party/WebKit/Source/core/html/forms/HTMLInputElement.h
@@ -112,6 +112,7 @@ class CORE_EXPORT HTMLInputElement
   bool checked() const;
   void setChecked(bool, TextFieldEventBehavior = kDispatchNoEvent);
   void DispatchChangeEventIfNeeded();
+  void DispatchInputAndChangeEventIfNeeded();
 
   // 'indeterminate' is a state independent of the checked state that causes the
   // control to draw in a way that hides the actual state.
