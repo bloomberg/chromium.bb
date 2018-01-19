961a46d6b9877999e25d7aa0ea6ba801ea3c02a5
Gabriel Charette <gab@chromium.org>
2018-01-19 09:57:35 +0000

    Enable -Wglobal-constructors in //base.
    Fix an unintentional dynamic allocation in base/android.
    
    Explicitly allow dynamic allocation in two other places
    where compile-time isn't possible and I *think* are benign.
    
    Bug: 801563, 800760
    Change-Id: I183e18d21af31625a8729aff44b89be85dcd1220
    Reviewed-on: https://chromium-review.googlesource.com/866738
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530465}
    Former-commit-id: a9698a9ad30bff2bfc150fddd4b380b05d6c6eff

--- base/process/memory_win.cc
+++ base/process/memory_win.cc
@@ -10,12 +10,22 @@
 #include <psapi.h>
 #include <stddef.h>
 
+#if defined(__clang__)
+// This global constructor is trivial and non-racy (per being const).
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wglobal-constructors"
+#endif
+
 // malloc_unchecked is required to implement UncheckedMalloc properly.
 // It's provided by allocator_shim_win.cc but since that's not always present,
 // we provide a default that falls back to regular malloc.
 typedef void* (*MallocFn)(size_t);
 extern "C" void* (*const malloc_unchecked)(size_t);
 extern "C" void* (*const malloc_default)(size_t) = &malloc;
+
+#if defined(__clang__)
+#pragma clang diagnostic pop  // -Wglobal-constructors
+#endif
 
 #if defined(_M_IX86)
 #pragma comment(linker, "/alternatename:_malloc_unchecked=_malloc_default")
