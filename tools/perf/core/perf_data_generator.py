12fa421e7b53bfe90d6d14f831271e6bbd59084c
Yuri Wiitala <miu@chromium.org>
2018-01-22 12:29:34 +0000

    Revert "Disabling performance_browser_tests"
    This reverts commit 23e4cd4ef9f2670464d8aad2ceb6fb88724ebfe5 [formerly d5dd606f9d167708bc3de0c9fcd6a7fd0390eb3a].
    
    Reason for revert: Landed fix. See: https://chromium-review.googlesource.com/c/chromium/src/+/877823
    
    Original change's description:
    > Disabling performance_browser_tests
    > 
    > performance_browser_tests are failing on Mac Air 10.11 Perf, Mac Pro
    > 10.11 Perf, Mac Retina Perf, Win 7 ATI GPU Perf, Win 7 Intel GPU Perf,
    > Win 7 x64 Perf, and Win 8 Perf
    > 
    > Bug: 803455
    > Change-Id: I92856fed9176452ab07191d13db1d7f1d692bb24
    > Reviewed-on: https://chromium-review.googlesource.com/873995
    > Reviewed-by: Ned Nguyen <nednguyen@google.com>
    > Commit-Queue: Ashley Enstad <ashleymarie@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#530194}
    
    TBR=miu@chromium.org,nednguyen@google.com,ashleymarie@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 803455
    Change-Id: Iae14b0e6c51adc98ff5b4e922e24979939b8edd5
    Reviewed-on: https://chromium-review.googlesource.com/877229
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#530844}
    Former-commit-id: 1d5f7686b8165d381cb11a352bcbcd9389cd7837

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -372,8 +372,7 @@ def get_waterfall_config():
           ],
        'perf_tests': [
          ('load_library_perf_tests', 'build145-m1'),
-         # crbug.com/803455
-         # ('performance_browser_tests', 'build145-m1'),
+         ('performance_browser_tests', 'build145-m1'),
          ('media_perftests', 'build146-m1')]
       }
     ])
@@ -409,9 +408,8 @@ def get_waterfall_config():
            'build140-m1', 'build141-m1', 'build142-m1'
           ],
        'perf_tests': [
-         ('load_library_perf_tests', 'build140-m1')]
-         # crbug.com/803455
-         # ('performance_browser_tests', 'build140-m1')]
+         ('load_library_perf_tests', 'build140-m1'),
+         ('performance_browser_tests', 'build140-m1')]
       }
     ])
   waterfall = add_tester(
@@ -430,8 +428,7 @@ def get_waterfall_config():
          # crbug.com/785291
          # ('angle_perftests', 'build103-m1'),
          ('load_library_perf_tests', 'build103-m1'),
-         # crbug.com/803455
-         # ('performance_browser_tests', 'build103-m1'),
+         ('performance_browser_tests', 'build103-m1'),
          ('media_perftests', 'build104-m1')]
       }
     ])
@@ -449,9 +446,8 @@ def get_waterfall_config():
           ],
        'perf_tests': [
          ('angle_perftests', 'build166-m1'),
-         ('load_library_perf_tests', 'build166-m1')]
-         # crbug.com/803455
-         # ('performance_browser_tests', 'build166-m1')]
+         ('load_library_perf_tests', 'build166-m1'),
+         ('performance_browser_tests', 'build166-m1')]
       }
     ])
   waterfall = add_tester(
@@ -528,8 +524,7 @@ def get_waterfall_config():
            'build30-b4' # replacing build8-b1. crbug.com/724998
           ],
        'perf_tests': [
-         # crbug.com/803455
-         # ('performance_browser_tests', 'build30-b4')
+         ('performance_browser_tests', 'build30-b4')
        ]
       }
     ])
@@ -546,8 +541,7 @@ def get_waterfall_config():
            'build130-b1', 'build131-b1', 'build132-b1'
           ],
        'perf_tests': [
-         # crbug.com/803455
-         # ('performance_browser_tests', 'build132-b1')
+         ('performance_browser_tests', 'build132-b1')
        ]
       }
     ])
@@ -564,8 +558,7 @@ def get_waterfall_config():
            'build125-b1', 'build126-b1', 'build127-b1'
           ],
        'perf_tests': [
-         # crbug.com/803455
-         # ('performance_browser_tests', 'build126-b1')
+         ('performance_browser_tests', 'build126-b1')
        ]
       }
     ])
