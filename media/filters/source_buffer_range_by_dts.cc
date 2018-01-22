795ab6122d9fe9265106fc237ea175d80d57e10b
Matt Wolenetz <wolenetz@chromium.org>
2018-01-22 21:13:14 +0000

    MSE: Fix a couple fudge room usage faults
    When fudge room increases, MergeAllAdjacentRanges should merge all
    ranges that are adjacent based on the new fudge room. Previous code
    would not try to merge sets of more than 2 adjacent ranges, resulting in
    disjoint ranges which should have been coalesced. This change updates
    MergeAllAdjacentRanges to try to merge the current range with the next
    one repeatedly if the previous merge was successful.
    
    This change also stops using the approximate buffer duration (based on
    the maximum duration or continuous interbuffer DTS distance seen so far)
    as the reported range end time for ranges ending with a zero duration
    buffer. In combination with the previously broken MergeAllAdjacentRanges
    logic, approximated range end times that grow dynamically as a side
    effect of fudge room growth led to previously disjoint ranges
    overlapping each other (and failing the strengthened IsRangeListSorted()
    debug build verifications added as part of fixing bug 791095.)
    
    This change uses a constant 1 microsecond for the duration of a buffer
    that is reported as zero duration by the stream parsing and frame
    processor when calculating the buffered end time of a range.
    
    Since public base::TimeDelta::FromInternalValue usage is deprecated,
    this change also switches usage to FromMicroseconds in SBS and SBR.
    
    BUG=803018,803293,791095
    TEST=SBS.*ZeroDurationBuffersThenIncreasingFudgeRoom, and removal of a
    FrameProcessorTest that verified the obsolete fudge-room-based buffered
    range reporting. The clusterfuzz cases in bugs 803018 and 803293 also
    no longer repro locally with this change.
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I8b4adfeb5064c15e57211d2f07132e7723f7f66a
    Reviewed-on: https://chromium-review.googlesource.com/871517
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530992}
    Former-commit-id: ef5f357c1f1375f3167bc1d9ecc9b5188ad421f6

--- media/filters/source_buffer_range_by_dts.cc
+++ media/filters/source_buffer_range_by_dts.cc
@@ -432,8 +432,16 @@ DecodeTimestamp SourceBufferRangeByDts::GetEndTimestamp() const {
 DecodeTimestamp SourceBufferRangeByDts::GetBufferedEndTimestamp() const {
   DCHECK(!buffers_.empty());
   base::TimeDelta duration = buffers_.back()->duration();
-  if (duration == kNoTimestamp || duration.is_zero())
-    duration = GetApproximateDuration();
+
+  // FrameProcessor should protect against unknown buffer durations.
+  DCHECK_NE(duration, kNoTimestamp);
+
+  // Because media::Ranges<base::TimeDelta>::Add() ignores 0 duration ranges,
+  // report 1 microsecond for the last buffer's duration if it is a 0 duration
+  // buffer.
+  if (duration.is_zero())
+    duration = base::TimeDelta::FromMicroseconds(1);
+
   return GetEndTimestamp() + duration;
 }
 
