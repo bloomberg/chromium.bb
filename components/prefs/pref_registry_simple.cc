2551d659fa7c6fd6108c6314e463b1a995894f87
Ilya Sherman <isherman@chromium.org>
2018-01-22 05:36:11 +0000

    [Cleanup] Simplify the PrefRegistrySimple API and implementation.
    * Remove function overloads from the header, in favor of passing
      NO_REGISTRATION_FLAGS as a default value for the |flags| param.
    * In doing so, unify the implementations of the with- and sans-flags
      registration function implementation variants.
    * Use "= default" for the constructor and the destructor.
    * Update the method declaration order to match the definition order.
    
    R=gab@chromium.org
    
    Bug: none
    Change-Id: I0d7f5a45815f242a0746b5b31883d89b77312b75
    Reviewed-on: https://chromium-review.googlesource.com/875136
    Commit-Queue: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530812}
    Former-commit-id: 652030de396747947d3edb6ccf6f4e72788eee9a

--- components/prefs/pref_registry_simple.cc
+++ components/prefs/pref_registry_simple.cc
@@ -10,84 +10,8 @@
 #include "base/strings/string_number_conversions.h"
 #include "base/values.h"
 
-PrefRegistrySimple::PrefRegistrySimple() {
-}
-
-PrefRegistrySimple::~PrefRegistrySimple() {
-}
-
-void PrefRegistrySimple::RegisterBooleanPref(const std::string& path,
-                                             bool default_value) {
-  RegisterPreference(path, std::make_unique<base::Value>(default_value),
-                     NO_REGISTRATION_FLAGS);
-}
-
-void PrefRegistrySimple::RegisterIntegerPref(const std::string& path,
-                                             int default_value) {
-  RegisterPreference(path, std::make_unique<base::Value>(default_value),
-                     NO_REGISTRATION_FLAGS);
-}
-
-void PrefRegistrySimple::RegisterDoublePref(const std::string& path,
-                                            double default_value) {
-  RegisterPreference(path, std::make_unique<base::Value>(default_value),
-                     NO_REGISTRATION_FLAGS);
-}
-
-void PrefRegistrySimple::RegisterStringPref(const std::string& path,
-                                            const std::string& default_value) {
-  RegisterPreference(path, std::make_unique<base::Value>(default_value),
-                     NO_REGISTRATION_FLAGS);
-}
-
-void PrefRegistrySimple::RegisterFilePathPref(
-    const std::string& path,
-    const base::FilePath& default_value) {
-  RegisterPreference(path, std::make_unique<base::Value>(default_value.value()),
-                     NO_REGISTRATION_FLAGS);
-}
-
-void PrefRegistrySimple::RegisterListPref(const std::string& path) {
-  RegisterPreference(path, std::make_unique<base::ListValue>(),
-                     NO_REGISTRATION_FLAGS);
-}
-
-void PrefRegistrySimple::RegisterListPref(
-    const std::string& path,
-    std::unique_ptr<base::ListValue> default_value) {
-  RegisterPreference(path, std::move(default_value), NO_REGISTRATION_FLAGS);
-}
-
-void PrefRegistrySimple::RegisterDictionaryPref(const std::string& path) {
-  RegisterPreference(path, std::make_unique<base::DictionaryValue>(),
-                     NO_REGISTRATION_FLAGS);
-}
-
-void PrefRegistrySimple::RegisterDictionaryPref(
-    const std::string& path,
-    std::unique_ptr<base::DictionaryValue> default_value) {
-  RegisterPreference(path, std::move(default_value), NO_REGISTRATION_FLAGS);
-}
-
-void PrefRegistrySimple::RegisterInt64Pref(const std::string& path,
-                                           int64_t default_value) {
-  RegisterPreference(
-      path, std::make_unique<base::Value>(base::Int64ToString(default_value)),
-      NO_REGISTRATION_FLAGS);
-}
-
-void PrefRegistrySimple::RegisterUint64Pref(const std::string& path,
-                                            uint64_t default_value) {
-  RegisterPreference(
-      path, std::make_unique<base::Value>(base::NumberToString(default_value)),
-      NO_REGISTRATION_FLAGS);
-}
-
-void PrefRegistrySimple::RegisterTimePref(const std::string& path,
-                                          base::Time default_value) {
-  RegisterInt64Pref(path,
-                    default_value.ToDeltaSinceWindowsEpoch().InMicroseconds());
-}
+PrefRegistrySimple::PrefRegistrySimple() = default;
+PrefRegistrySimple::~PrefRegistrySimple() = default;
 
 void PrefRegistrySimple::RegisterBooleanPref(const std::string& path,
                                              bool default_value,
