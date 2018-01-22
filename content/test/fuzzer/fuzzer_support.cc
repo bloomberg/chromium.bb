a0bc768d00384526d41873eb2ebec8c5843fc719
Hitoshi Yoshida <peria@chromium.org>
2018-01-22 00:50:13 +0000

    bindings: Enable V8ContextSnasphot on fuzzers
    Currently fuzzer does not load v8_context_snapsot.bin, and
    fail to set up blink.
    This CL makes fuzzers to load v8_context_snapshot.bin
    to work with it.
    
    
    
    Bug: 789964,804073,804006
    Change-Id: I332586ccaf9d348fc5af3e22acdaf579624575e5
    Reviewed-on: https://chromium-review.googlesource.com/876744
    Reviewed-by: Oliver Chang <ochang@chromium.org>
    Commit-Queue: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#530795}
    Former-commit-id: 8b6254d72354e61e9976d1b0817477689ec9eec7

--- content/test/fuzzer/fuzzer_support.cc
+++ content/test/fuzzer/fuzzer_support.cc
@@ -14,6 +14,18 @@
 
 namespace content {
 
+namespace {
+#if defined(V8_USE_EXTERNAL_STARTUP_DATA)
+#if defined(USE_V8_CONTEXT_SNAPSHOT)
+constexpr gin::V8Initializer::V8SnapshotFileType kSnapshotType =
+    gin::V8Initializer::V8SnapshotFileType::kWithAdditionalContext;
+#else
+constexpr gin::V8Initializer::V8SnapshotFileType kSnapshotType =
+    gin::V8Initializer::V8SnapshotFileType::kDefault;
+#endif
+#endif
+}
+
 void RenderViewTestAdapter::SetUp() {
   RenderViewTest::SetUp();
 }
@@ -27,7 +39,7 @@ Env::Env() {
   blink::WebRuntimeFeatures::EnableTestOnlyFeatures(true);
 
 #ifdef V8_USE_EXTERNAL_STARTUP_DATA
-  gin::V8Initializer::LoadV8Snapshot();
+  gin::V8Initializer::LoadV8Snapshot(kSnapshotType);
   gin::V8Initializer::LoadV8Natives();
 #endif
   gin::IsolateHolder::Initialize(gin::IsolateHolder::kStrictMode,
