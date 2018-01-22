d8c08a7f97316bb5c5f1b81c0a8a5514f6ac8440
Jeremy Roman <jbroman@chromium.org>
2018-01-22 22:58:19 +0000

    [remoteobjects] Use Mockito in unit tests.
    This and subsequent tests necessitate some slightly complex logic for ensuring
    that the resposne callback is invoked appropriately. Rather than reinvent an
    ad-hoc mocking framework, make reasonable use of the one already in use in
    other content_junit_tests.
    
    Bug: 794320
    Change-Id: I4f39f5c3ca531b6c802da49e0bb2067d0b6e3045
    Reviewed-on: https://chromium-review.googlesource.com/879342
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531043}
    Former-commit-id: b3237561d68b4d2a4bfc6cacadcb8985093d62d1

--- content/public/android/junit/src/org/chromium/content/browser/remoteobjects/RemoteObjectImplTest.java
+++ content/public/android/junit/src/org/chromium/content/browser/remoteobjects/RemoteObjectImplTest.java
@@ -4,10 +4,19 @@
 
 package org.chromium.content.browser.remoteobjects;
 
+import static org.mockito.AdditionalMatchers.aryEq;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InOrder;
 
 import org.chromium.blink.mojom.RemoteInvocationArgument;
 import org.chromium.blink.mojom.RemoteInvocationResult;
@@ -18,6 +27,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.util.Arrays;
+import java.util.function.Consumer;
 
 /**
  * Tests the implementation of the Mojo object which wraps invocations
@@ -34,18 +44,6 @@ public final class RemoteObjectImplTest {
     @Retention(RetentionPolicy.RUNTIME)
     @Target({ElementType.METHOD})
     private @interface TestJavascriptInterface {}
-
-    private static class Holder<T> {
-        public T value = null;
-
-        public Holder() {}
-        public Holder(T value) {
-            this.value = value;
-        }
-        public void reset() {
-            value = null;
-        }
-    }
 
     @Test
     public void testHasMethodWithSafeAnnotationClass() {
@@ -66,31 +64,30 @@ public final class RemoteObjectImplTest {
         };
 
         RemoteObject remoteObject = new RemoteObjectImpl(target, TestJavascriptInterface.class);
-        final Holder<Boolean> hasMethodResult = new Holder<>();
+        RemoteObject.HasMethodResponse hasMethodResponse;
 
         // This method is public and annotated; it should be exposed.
-        hasMethodResult.reset();
-        remoteObject.hasMethod("exposedMethod", (Boolean result) -> hasMethodResult.value = result);
-        Assert.assertEquals(true, hasMethodResult.value);
+        hasMethodResponse = mock(RemoteObject.HasMethodResponse.class);
+        remoteObject.hasMethod("exposedMethod", hasMethodResponse);
+        verify(hasMethodResponse).call(true);
 
         // This method is private; it should not be exposed.
-        hasMethodResult.reset();
-        remoteObject.hasMethod(
-                "privateAnnotatedMethod", (Boolean result) -> hasMethodResult.value = result);
-        Assert.assertEquals(false, hasMethodResult.value);
+        hasMethodResponse = mock(RemoteObject.HasMethodResponse.class);
+        remoteObject.hasMethod("privateAnnotatedMethod", hasMethodResponse);
+        verify(hasMethodResponse).call(false);
 
         // This method is not annotated; it should not be exposed.
-        hasMethodResult.reset();
-        remoteObject.hasMethod(
-                "unannotatedMethod", (Boolean result) -> hasMethodResult.value = result);
-        Assert.assertEquals(false, hasMethodResult.value);
+        hasMethodResponse = mock(RemoteObject.HasMethodResponse.class);
+        remoteObject.hasMethod("unannotatedMethod", hasMethodResponse);
+        verify(hasMethodResponse).call(false);
 
         // getMethods should provide a result consistent with this.
         // The result must also be in sorted order and have no duplicates.
-        final Holder<String[]> getMethodsResult = new Holder<>();
-        remoteObject.getMethods((String[] result) -> getMethodsResult.value = result);
-        Assert.assertArrayEquals(
-                new String[] {"anotherExposedMethod", "exposedMethod"}, getMethodsResult.value);
+        RemoteObject.GetMethodsResponse getMethodsResponse =
+                mock(RemoteObject.GetMethodsResponse.class);
+        remoteObject.getMethods(getMethodsResponse);
+        verify(getMethodsResponse)
+                .call(aryEq(new String[] {"anotherExposedMethod", "exposedMethod"}));
     }
 
     @Test
@@ -103,78 +100,85 @@ public final class RemoteObjectImplTest {
         };
 
         RemoteObject remoteObject = new RemoteObjectImpl(target, null);
-        final Holder<Boolean> hasMethodResult = new Holder<Boolean>();
+        RemoteObject.HasMethodResponse hasMethodResponse;
 
         // This method has an annotation; it should be exposed.
-        hasMethodResult.reset();
-        remoteObject.hasMethod(
-                "annotatedMethod", (Boolean result) -> hasMethodResult.value = result);
-        Assert.assertEquals(true, hasMethodResult.value);
+        hasMethodResponse = mock(RemoteObject.HasMethodResponse.class);
+        remoteObject.hasMethod("annotatedMethod", hasMethodResponse);
+        verify(hasMethodResponse).call(true);
 
         // This method doesn't, but passing null skips the check.
-        hasMethodResult.reset();
-        remoteObject.hasMethod(
-                "unannotatedMethod", (Boolean result) -> hasMethodResult.value = result);
-        Assert.assertEquals(true, hasMethodResult.value);
+        hasMethodResponse = mock(RemoteObject.HasMethodResponse.class);
+        remoteObject.hasMethod("unannotatedMethod", hasMethodResponse);
+        verify(hasMethodResponse).call(true);
 
         // getMethods should provide a result consistent with this.
         // The result must also be in sorted order.
         // Note that this includes all of the normal java.lang.Object methods.
-        final Holder<String[]> getMethodsResult = new Holder<>();
-        remoteObject.getMethods((String[] result) -> getMethodsResult.value = result);
-        Assert.assertTrue(Arrays.asList(getMethodsResult.value).contains("annotatedMethod"));
-        Assert.assertTrue(Arrays.asList(getMethodsResult.value).contains("unannotatedMethod"));
-        Assert.assertTrue(Arrays.asList(getMethodsResult.value).contains("hashCode"));
-        String[] sortedMethods =
-                Arrays.copyOf(getMethodsResult.value, getMethodsResult.value.length);
+        RemoteObject.GetMethodsResponse getMethodsResponse =
+                mock(RemoteObject.GetMethodsResponse.class);
+        remoteObject.getMethods(getMethodsResponse);
+
+        ArgumentCaptor<String[]> methodsCaptor = ArgumentCaptor.forClass(String[].class);
+        verify(getMethodsResponse).call(methodsCaptor.capture());
+        String[] methods = methodsCaptor.getValue();
+        Assert.assertTrue(Arrays.asList(methods).contains("annotatedMethod"));
+        Assert.assertTrue(Arrays.asList(methods).contains("unannotatedMethod"));
+        Assert.assertTrue(Arrays.asList(methods).contains("hashCode"));
+        String[] sortedMethods = Arrays.copyOf(methods, methods.length);
         Arrays.sort(sortedMethods);
-        Assert.assertArrayEquals(sortedMethods, getMethodsResult.value);
+        Assert.assertArrayEquals(sortedMethods, methods);
     }
 
     @Test
     public void testInvokeMethodBasic() {
-        final Holder<Integer> frobnicateCount = new Holder<>(0);
+        final Runnable runnable = mock(Runnable.class);
         Object target = new Object() {
             @TestJavascriptInterface
             public void frobnicate() {
-                frobnicateCount.value++;
+                runnable.run();
             }
         };
 
         RemoteObject remoteObject = new RemoteObjectImpl(target, TestJavascriptInterface.class);
-        Assert.assertEquals(Integer.valueOf(0), frobnicateCount.value);
-        remoteObject.invokeMethod("frobnicate", new RemoteInvocationArgument[] {},
-                (RemoteInvocationResult result) -> {});
-        Assert.assertEquals(Integer.valueOf(1), frobnicateCount.value);
-        remoteObject.invokeMethod("frobnicate", new RemoteInvocationArgument[] {},
-                (RemoteInvocationResult result) -> {});
-        Assert.assertEquals(Integer.valueOf(2), frobnicateCount.value);
+        RemoteObject.InvokeMethodResponse response = mock(RemoteObject.InvokeMethodResponse.class);
+        remoteObject.invokeMethod("frobnicate", new RemoteInvocationArgument[] {}, response);
+        remoteObject.invokeMethod("frobnicate", new RemoteInvocationArgument[] {}, response);
+
+        verify(runnable, times(2)).run();
+        verify(response, times(2)).call(resultIsOk());
     }
 
     @Test
     public void testInvokeMethodOverloadUsingArity() {
-        final Holder<Integer> frobnicateCount = new Holder<>(0);
+        final Consumer<Integer> consumer = (Consumer<Integer>) mock(Consumer.class);
         Object target = new Object() {
             @TestJavascriptInterface
             public void frobnicate() {
-                frobnicateCount.value++;
+                consumer.accept(0);
             }
 
             @TestJavascriptInterface
             public void frobnicate(Object argument) {
-                frobnicateCount.value += 2;
+                consumer.accept(1);
             }
         };
 
         // The method overload to be called depends on the number of arguments supplied.
         // TODO(jbroman): Once it's possible to construct a non-trivial argument, do so.
         RemoteObject remoteObject = new RemoteObjectImpl(target, TestJavascriptInterface.class);
-        Assert.assertEquals(Integer.valueOf(0), frobnicateCount.value);
-        remoteObject.invokeMethod("frobnicate", new RemoteInvocationArgument[] {},
-                (RemoteInvocationResult result) -> {});
-        Assert.assertEquals(Integer.valueOf(1), frobnicateCount.value);
-        remoteObject.invokeMethod("frobnicate", new RemoteInvocationArgument[] {null},
-                (RemoteInvocationResult result) -> {});
-        Assert.assertEquals(Integer.valueOf(3), frobnicateCount.value);
+        RemoteObject.InvokeMethodResponse response = mock(RemoteObject.InvokeMethodResponse.class);
+        remoteObject.invokeMethod("frobnicate", new RemoteInvocationArgument[] {}, response);
+        remoteObject.invokeMethod("frobnicate", new RemoteInvocationArgument[] {null}, response);
+
+        InOrder inOrder = inOrder(consumer);
+        inOrder.verify(consumer).accept(0);
+        inOrder.verify(consumer).accept(1);
+        verify(response, times(2)).call(resultIsOk());
+    }
+
+    private RemoteInvocationResult resultIsOk() {
+        // TODO(jbroman): Check the error code once there is one.
+        return ArgumentMatchers.<RemoteInvocationResult>any();
     }
 }
