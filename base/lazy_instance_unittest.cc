a0a39d50dce682b043a2b7e30588618ce5fa0fb8
Daniel Cheng <dcheng@chromium.org>
2018-01-23 23:51:31 +0000

    Make DestructorAtExit LazyInstances leak.
    This means that tests may no longer rely on LazyInstance<T> to clean
    up global state between tests. Tests that depend on globals but need
    a clean test environment should use explicit test hooks to reset this
    state.
    
    Only one test depends on DestructorAtExit for correct functionality.
    cast_audio_backend_unittests creates several mocks that are owned by a
    base::LazyInstance. Since Gmock verifies expectations in destructors,
    switching all LazyInstances to leaky ones breaks this verification.
    The solution is to manually verify and check the expectations at the
    end of the test.
    
    Note that while the mock objects are annotated as leaked for Gmock,
    that does not mean the cast_audio_backend_unittests will leak
    arbitrary amounts of memory. The test fixture already configures
    StreamMixer for each test run; this setup will clear up any leftover
    objects from the previous test.
    
    DestructorAtExit will be removed in a followup.
    
    Bug: 698982
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I822ce507dbb98067a788466e7c8fcc96c3a64ef9
    Reviewed-on: https://chromium-review.googlesource.com/874994
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531385}
    Former-commit-id: 1b97e93c5a1d0bc0ed2f84d0d34581eade8315c6

--- base/lazy_instance_unittest.cc
+++ base/lazy_instance_unittest.cc
@@ -8,11 +8,11 @@
 #include <vector>
 
 #include "base/at_exit.h"
-#include "base/atomic_sequence_num.h"
-#include "base/atomicops.h"
 #include "base/barrier_closure.h"
 #include "base/bind.h"
 #include "base/lazy_instance.h"
+#include "base/logging.h"
+#include "base/synchronization/atomic_flag.h"
 #include "base/sys_info.h"
 #include "base/threading/platform_thread.h"
 #include "base/threading/simple_thread.h"
@@ -20,48 +20,111 @@
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace {
+namespace base {
 
-base::AtomicSequenceNumber constructed_seq_;
-base::AtomicSequenceNumber destructed_seq_;
+namespace {
 
 class ConstructAndDestructLogger {
  public:
   ConstructAndDestructLogger() {
-    constructed_seq_.GetNext();
+    CHECK(!constructed_->IsSet());
+    constructed_->Set();
   }
   ~ConstructAndDestructLogger() {
-    destructed_seq_.GetNext();
+    CHECK(false) << "This should be leaked and never reached!";
   }
 
+  class ScopedSetup {
+   public:
+    ScopedSetup() {
+      CHECK_EQ(nullptr, ConstructAndDestructLogger::constructed_);
+      ConstructAndDestructLogger::constructed_ = &constructed_;
+    }
+
+    ~ScopedSetup() {
+      CHECK_EQ(&constructed_, ConstructAndDestructLogger::constructed_);
+      ConstructAndDestructLogger::constructed_ = nullptr;
+    }
+
+    AtomicFlag& constructed() { return constructed_; }
+
+   private:
+    AtomicFlag constructed_;
+  };
+
  private:
+  static AtomicFlag* constructed_;
+
   DISALLOW_COPY_AND_ASSIGN(ConstructAndDestructLogger);
 };
+
+AtomicFlag* ConstructAndDestructLogger::constructed_ = nullptr;
+
+LazyInstance<ConstructAndDestructLogger>::Leaky lazy_logger =
+    LAZY_INSTANCE_INITIALIZER;
+
+}  // namespace
+
+TEST(LazyInstanceTest, Basic) {
+  ConstructAndDestructLogger::ScopedSetup test_scope;
+
+  EXPECT_FALSE(lazy_logger.IsCreated());
+  EXPECT_FALSE(test_scope.constructed().IsSet());
+
+  lazy_logger.Get();
+  EXPECT_TRUE(lazy_logger.IsCreated());
+  EXPECT_TRUE(test_scope.constructed().IsSet());
+
+  lazy_logger.Pointer();
+  EXPECT_TRUE(lazy_logger.IsCreated());
+  EXPECT_TRUE(test_scope.constructed().IsSet());
+}
+
+namespace {
 
 class SlowConstructor {
  public:
   SlowConstructor() : some_int_(0) {
     // Sleep for 1 second to try to cause a race.
-    base::PlatformThread::Sleep(base::TimeDelta::FromSeconds(1));
-    ++constructed;
+    PlatformThread::Sleep(TimeDelta::FromSeconds(1));
+    CHECK(!*constructed);
+    *constructed = true;
     some_int_ = 12;
   }
+
   int some_int() const { return some_int_; }
 
-  static int constructed;
+  class ScopedSetup {
+   public:
+    ScopedSetup() {
+      CHECK_EQ(nullptr, SlowConstructor::constructed);
+      SlowConstructor::constructed = &constructed_;
+    }
+
+    ~ScopedSetup() {
+      CHECK_EQ(&constructed_, SlowConstructor::constructed);
+      SlowConstructor::constructed = nullptr;
+    }
+
+    bool constructed() const { return constructed_; }
+
+   private:
+    bool constructed_ = false;
+  };
+
  private:
+  static bool* constructed;
   int some_int_;
 
   DISALLOW_COPY_AND_ASSIGN(SlowConstructor);
 };
 
 // static
-int SlowConstructor::constructed = 0;
+bool* SlowConstructor::constructed = nullptr;
 
-class SlowDelegate : public base::DelegateSimpleThread::Delegate {
+class SlowDelegate : public DelegateSimpleThread::Delegate {
  public:
-  explicit SlowDelegate(
-      base::LazyInstance<SlowConstructor>::DestructorAtExit* lazy)
+  explicit SlowDelegate(LazyInstance<SlowConstructor>::DestructorAtExit* lazy)
       : lazy_(lazy) {}
 
   void Run() override {
@@ -70,57 +133,29 @@ class SlowDelegate : public base::DelegateSimpleThread::Delegate {
   }
 
  private:
-  base::LazyInstance<SlowConstructor>::DestructorAtExit* lazy_;
+  LazyInstance<SlowConstructor>::DestructorAtExit* lazy_;
 
   DISALLOW_COPY_AND_ASSIGN(SlowDelegate);
 };
 
+LazyInstance<SlowConstructor>::DestructorAtExit lazy_slow =
+    LAZY_INSTANCE_INITIALIZER;
+
 }  // namespace
 
-base::LazyInstance<ConstructAndDestructLogger>::DestructorAtExit lazy_logger =
-    LAZY_INSTANCE_INITIALIZER;
-
-TEST(LazyInstanceTest, Basic) {
-  {
-    base::ShadowingAtExitManager shadow;
-
-    EXPECT_FALSE(lazy_logger.IsCreated());
-    EXPECT_EQ(0, constructed_seq_.GetNext());
-    EXPECT_EQ(0, destructed_seq_.GetNext());
-
-    lazy_logger.Get();
-    EXPECT_TRUE(lazy_logger.IsCreated());
-    EXPECT_EQ(2, constructed_seq_.GetNext());
-    EXPECT_EQ(1, destructed_seq_.GetNext());
-
-    lazy_logger.Pointer();
-    EXPECT_TRUE(lazy_logger.IsCreated());
-    EXPECT_EQ(3, constructed_seq_.GetNext());
-    EXPECT_EQ(2, destructed_seq_.GetNext());
-  }
-  EXPECT_FALSE(lazy_logger.IsCreated());
-  EXPECT_EQ(4, constructed_seq_.GetNext());
-  EXPECT_EQ(4, destructed_seq_.GetNext());
-}
-
-base::LazyInstance<SlowConstructor>::DestructorAtExit lazy_slow =
-    LAZY_INSTANCE_INITIALIZER;
-
 TEST(LazyInstanceTest, ConstructorThreadSafety) {
-  {
-    base::ShadowingAtExitManager shadow;
+  SlowConstructor::ScopedSetup test_scope;
 
-    SlowDelegate delegate(&lazy_slow);
-    EXPECT_EQ(0, SlowConstructor::constructed);
+  SlowDelegate delegate(&lazy_slow);
+  EXPECT_FALSE(test_scope.constructed());
 
-    base::DelegateSimpleThreadPool pool("lazy_instance_cons", 5);
-    pool.AddWork(&delegate, 20);
-    EXPECT_EQ(0, SlowConstructor::constructed);
+  DelegateSimpleThreadPool pool("lazy_instance_cons", 5);
+  pool.AddWork(&delegate, 20);
+  EXPECT_FALSE(test_scope.constructed());
 
-    pool.Start();
-    pool.JoinAll();
-    EXPECT_EQ(1, SlowConstructor::constructed);
-  }
+  pool.Start();
+  pool.JoinAll();
+  EXPECT_TRUE(test_scope.constructed());
 }
 
 namespace {
@@ -143,24 +178,23 @@ class DeleteLogger {
 }  // anonymous namespace
 
 TEST(LazyInstanceTest, LeakyLazyInstance) {
-  // Check that using a plain LazyInstance causes the dtor to run
-  // when the AtExitManager finishes.
+  // Check that using a *DestructorAtExit* LazyInstance makes the dtor not run
+  // when the AtExitManager finishes, since it is secretly the same thing.
   bool deleted1 = false;
   {
-    base::ShadowingAtExitManager shadow;
-    static base::LazyInstance<DeleteLogger>::DestructorAtExit test =
+    ShadowingAtExitManager shadow;
+    static LazyInstance<DeleteLogger>::DestructorAtExit test =
         LAZY_INSTANCE_INITIALIZER;
     test.Get().SetDeletedPtr(&deleted1);
   }
-  EXPECT_TRUE(deleted1);
+  EXPECT_FALSE(deleted1);
 
   // Check that using a *leaky* LazyInstance makes the dtor not run
   // when the AtExitManager finishes.
   bool deleted2 = false;
   {
-    base::ShadowingAtExitManager shadow;
-    static base::LazyInstance<DeleteLogger>::Leaky
-        test = LAZY_INSTANCE_INITIALIZER;
+    ShadowingAtExitManager shadow;
+    static LazyInstance<DeleteLogger>::Leaky test = LAZY_INSTANCE_INITIALIZER;
     test.Get().SetDeletedPtr(&deleted2);
   }
   EXPECT_FALSE(deleted2);
@@ -182,8 +216,6 @@ class AlignedData {
     EXPECT_EQ(0u, reinterpret_cast<uintptr_t>(ptr) & (align - 1))
 
 TEST(LazyInstanceTest, Alignment) {
-  using base::LazyInstance;
-
   // Create some static instances with increasing sizes and alignment
   // requirements. By ordering this way, the linker will need to do some work to
   // ensure proper alignment of the static data.
@@ -207,35 +239,46 @@ class BlockingConstructor {
  public:
   BlockingConstructor() {
     EXPECT_FALSE(WasConstructorCalled());
-    base::subtle::NoBarrier_Store(&constructor_called_, 1);
+    constructor_called_->Set();
     EXPECT_TRUE(WasConstructorCalled());
-    while (!base::subtle::NoBarrier_Load(&complete_construction_))
-      base::PlatformThread::YieldCurrentThread();
+    while (!complete_construction_->IsSet())
+      PlatformThread::YieldCurrentThread();
     done_construction_ = true;
   }
 
-  ~BlockingConstructor() {
-    // Restore static state for the next test.
-    base::subtle::NoBarrier_Store(&constructor_called_, 0);
-    base::subtle::NoBarrier_Store(&complete_construction_, 0);
-  }
-
   // Returns true if BlockingConstructor() was entered.
-  static bool WasConstructorCalled() {
-    return base::subtle::NoBarrier_Load(&constructor_called_);
-  }
+  static bool WasConstructorCalled() { return constructor_called_->IsSet(); }
 
   // Instructs BlockingConstructor() that it may now unblock its construction.
-  static void CompleteConstructionNow() {
-    base::subtle::NoBarrier_Store(&complete_construction_, 1);
-  }
+  static void CompleteConstructionNow() { complete_construction_->Set(); }
 
   bool done_construction() { return done_construction_; }
 
+  class ScopedSetup {
+   public:
+    ScopedSetup() {
+      CHECK_EQ(nullptr, BlockingConstructor::constructor_called_);
+      CHECK_EQ(nullptr, BlockingConstructor::complete_construction_);
+      BlockingConstructor::constructor_called_ = &constructor_called_;
+      BlockingConstructor::complete_construction_ = &complete_construction_;
+    }
+
+    ~ScopedSetup() {
+      CHECK_EQ(&constructor_called_, BlockingConstructor::constructor_called_);
+      CHECK_EQ(&complete_construction_,
+               BlockingConstructor::complete_construction_);
+      BlockingConstructor::constructor_called_ = nullptr;
+      BlockingConstructor::complete_construction_ = nullptr;
+    }
+
+   private:
+    AtomicFlag constructor_called_;
+    AtomicFlag complete_construction_;
+  };
+
  private:
-  // Use Atomic32 instead of AtomicFlag for them to be trivially initialized.
-  static base::subtle::Atomic32 constructor_called_;
-  static base::subtle::Atomic32 complete_construction_;
+  static AtomicFlag* constructor_called_;
+  static AtomicFlag* complete_construction_;
 
   bool done_construction_ = false;
 
@@ -244,12 +287,12 @@ class BlockingConstructor {
 
 // A SimpleThread running at |thread_priority| which invokes |before_get|
 // (optional) and then invokes Get() on the LazyInstance it's assigned.
-class BlockingConstructorThread : public base::SimpleThread {
+class BlockingConstructorThread : public SimpleThread {
  public:
   BlockingConstructorThread(
-      base::ThreadPriority thread_priority,
-      base::LazyInstance<BlockingConstructor>::DestructorAtExit* lazy,
-      base::OnceClosure before_get)
+      ThreadPriority thread_priority,
+      LazyInstance<BlockingConstructor>::DestructorAtExit* lazy,
+      OnceClosure before_get)
       : SimpleThread("BlockingConstructorThread", Options(thread_priority)),
         lazy_(lazy),
         before_get_(std::move(before_get)) {}
@@ -261,18 +304,18 @@ class BlockingConstructorThread : public base::SimpleThread {
   }
 
  private:
-  base::LazyInstance<BlockingConstructor>::DestructorAtExit* lazy_;
-  base::OnceClosure before_get_;
+  LazyInstance<BlockingConstructor>::DestructorAtExit* lazy_;
+  OnceClosure before_get_;
 
   DISALLOW_COPY_AND_ASSIGN(BlockingConstructorThread);
 };
 
 // static
-base::subtle::Atomic32 BlockingConstructor::constructor_called_ = 0;
+AtomicFlag* BlockingConstructor::constructor_called_ = nullptr;
 // static
-base::subtle::Atomic32 BlockingConstructor::complete_construction_ = 0;
+AtomicFlag* BlockingConstructor::complete_construction_ = nullptr;
 
-base::LazyInstance<BlockingConstructor>::DestructorAtExit lazy_blocking =
+LazyInstance<BlockingConstructor>::DestructorAtExit lazy_blocking =
     LAZY_INSTANCE_INITIALIZER;
 
 }  // namespace
@@ -282,28 +325,29 @@ base::LazyInstance<BlockingConstructor>::DestructorAtExit lazy_blocking =
 // to eventually complete construction.
 // This is a regression test for https://crbug.com/797129.
 TEST(LazyInstanceTest, PriorityInversionAtInitializationResolves) {
-  base::TimeTicks test_begin = base::TimeTicks::Now();
+  BlockingConstructor::ScopedSetup test_scope;
+
+  TimeTicks test_begin = TimeTicks::Now();
 
   // Construct BlockingConstructor from a background thread.
-  BlockingConstructorThread background_getter(
-      base::ThreadPriority::BACKGROUND, &lazy_blocking, base::OnceClosure());
+  BlockingConstructorThread background_getter(ThreadPriority::BACKGROUND,
+                                              &lazy_blocking, OnceClosure());
   background_getter.Start();
 
   while (!BlockingConstructor::WasConstructorCalled())
-    base::PlatformThread::Sleep(base::TimeDelta::FromMilliseconds(1));
+    PlatformThread::Sleep(TimeDelta::FromMilliseconds(1));
 
   // Spin 4 foreground thread per core contending to get the already under
   // construction LazyInstance. When they are all running and poking at it :
   // allow the background thread to complete its work.
-  const int kNumForegroundThreads = 4 * base::SysInfo::NumberOfProcessors();
-  std::vector<std::unique_ptr<base::SimpleThread>> foreground_threads;
-  base::RepeatingClosure foreground_thread_ready_callback =
-      base::BarrierClosure(
-          kNumForegroundThreads,
-          base::BindOnce(&BlockingConstructor::CompleteConstructionNow));
+  const int kNumForegroundThreads = 4 * SysInfo::NumberOfProcessors();
+  std::vector<std::unique_ptr<SimpleThread>> foreground_threads;
+  RepeatingClosure foreground_thread_ready_callback =
+      BarrierClosure(kNumForegroundThreads,
+                     BindOnce(&BlockingConstructor::CompleteConstructionNow));
   for (int i = 0; i < kNumForegroundThreads; ++i) {
     foreground_threads.push_back(std::make_unique<BlockingConstructorThread>(
-        base::ThreadPriority::NORMAL, &lazy_blocking,
+        ThreadPriority::NORMAL, &lazy_blocking,
         foreground_thread_ready_callback));
     foreground_threads.back()->Start();
   }
@@ -317,8 +361,7 @@ TEST(LazyInstanceTest, PriorityInversionAtInitializationResolves) {
 
   // Fail if this test takes more than 5 seconds (it takes 5-10 seconds on a
   // Z840 without r527445 but is expected to be fast (~30ms) with the fix).
-  EXPECT_LT(base::TimeTicks::Now() - test_begin,
-            base::TimeDelta::FromSeconds(5));
+  EXPECT_LT(TimeTicks::Now() - test_begin, TimeDelta::FromSeconds(5));
 }
 
 TEST(LazyInstanceTest, ConstExprConstructible) {
@@ -326,11 +369,12 @@ TEST(LazyInstanceTest, ConstExprConstructible) {
   // much... Declaring the variable constexpr forces the compiler to verify that
   // this is possible but renders the LazyInstance unusable as none of its
   // methods are const.
-  static constexpr base::LazyInstance<SlowConstructor>
-      kTestConstExprConstructible;
-  static constexpr base::LazyInstance<SlowConstructor>
+  static constexpr LazyInstance<SlowConstructor> kTestConstExprConstructible;
+  static constexpr LazyInstance<SlowConstructor>
       kTestConstExprConstructibleWithExplicitInitializer =
           LAZY_INSTANCE_INITIALIZER;
   ALLOW_UNUSED_LOCAL(kTestConstExprConstructible);
   ALLOW_UNUSED_LOCAL(kTestConstExprConstructibleWithExplicitInitializer);
 }
+
+}  // namespace base
