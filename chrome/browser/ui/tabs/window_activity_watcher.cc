c3868e4928b0157145de13e43892b48a1db57b1c
Michael Giuffrida <michaelpg@chromium.org>
2018-01-23 10:28:02 +0000

    Add IsActive and TabCount to browser activity UKM
    Adds two metrics to the existing TabManager.WindowMetrics UKM.
    
    WindowActivityWatcher now creates a per-browser tracker to observe each
    browser's tab strip. The tracker only logs a new UKM if its metrics
    have changed since its last UKM was logged. Having each tracker store
    its latest metrics will also be useful when new metrics are added that
    track cumulative event counts (e.g., how often a window is maximized).
    
    Bug: 791824
    Change-Id: Iacf28c0735c13cedf5cf573a9007f838707b8a98
    Reviewed-on: https://chromium-review.googlesource.com/872093
    Commit-Queue: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531200}
    Former-commit-id: 56f31113259c561a93b5b47f5dc9d6575bc6bab6

--- chrome/browser/ui/tabs/window_activity_watcher.cc
+++ chrome/browser/ui/tabs/window_activity_watcher.cc
@@ -4,21 +4,27 @@
 
 #include "chrome/browser/ui/tabs/window_activity_watcher.h"
 
+#include "base/optional.h"
+#include "base/scoped_observer.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/tabs/tab_metrics_event.pb.h"
+#include "services/metrics/public/cpp/metrics_utils.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 #include "services/metrics/public/cpp/ukm_recorder.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
 
-using WindowMetrics = WindowActivityWatcher::WindowMetrics;
 using metrics::WindowMetricsEvent;
 
-struct WindowActivityWatcher::WindowMetrics {
+namespace {
+
+struct WindowMetrics {
   bool operator==(const WindowMetrics& other) {
-    return window_id == other.window_id && show_state == other.show_state &&
-           type == other.type;
+    return window_id == other.window_id && type == other.type &&
+           show_state == other.show_state && is_active == other.is_active &&
+           tab_count == other.tab_count;
   }
 
   bool operator!=(const WindowMetrics& other) { return !operator==(other); }
@@ -28,9 +34,28 @@ struct WindowActivityWatcher::WindowMetrics {
   WindowMetricsEvent::Type type;
   // TODO(michaelpg): Observe the show state and log when it changes.
   WindowMetricsEvent::ShowState show_state;
+
+  // True if this is the active (frontmost) window.
+  bool is_active;
+
+  // Number of tabs in the tab strip.
+  int tab_count;
 };
 
-namespace {
+// Sets metric values that are dependent on the current window state.
+void UpdateMetrics(const Browser* browser, WindowMetrics* window_metrics) {
+  if (browser->window()->IsFullscreen())
+    window_metrics->show_state = WindowMetricsEvent::SHOW_STATE_FULLSCREEN;
+  else if (browser->window()->IsMinimized())
+    window_metrics->show_state = WindowMetricsEvent::SHOW_STATE_MINIMIZED;
+  else if (browser->window()->IsMaximized())
+    window_metrics->show_state = WindowMetricsEvent::SHOW_STATE_MAXIMIZED;
+  else
+    window_metrics->show_state = WindowMetricsEvent::SHOW_STATE_NORMAL;
+
+  window_metrics->is_active = browser->window()->IsActive();
+  window_metrics->tab_count = browser->tab_strip_model()->count();
+}
 
 // Returns a populated WindowMetrics for the browser.
 WindowMetrics CreateMetrics(const Browser* browser) {
@@ -46,31 +71,98 @@ WindowMetrics CreateMetrics(const Browser* browser) {
       break;
   }
 
-  if (browser->window()->IsFullscreen())
-    window_metrics.show_state = WindowMetricsEvent::SHOW_STATE_FULLSCREEN;
-  else if (browser->window()->IsMinimized())
-    window_metrics.show_state = WindowMetricsEvent::SHOW_STATE_MINIMIZED;
-  else if (browser->window()->IsMaximized())
-    window_metrics.show_state = WindowMetricsEvent::SHOW_STATE_MAXIMIZED;
-  else
-    window_metrics.show_state = WindowMetricsEvent::SHOW_STATE_NORMAL;
+  UpdateMetrics(browser, &window_metrics);
   return window_metrics;
 }
 
-// Logs a UKM entry with the corresponding metrics.
-void LogUkmEntry(const WindowMetrics& window_metrics) {
+// Logs a UKM entry with the metrics from |window_metrics|.
+void LogWindowMetricsUkmEntry(const WindowMetrics& window_metrics) {
   ukm::UkmRecorder* ukm_recorder = ukm::UkmRecorder::Get();
   if (!ukm_recorder)
     return;
 
   ukm::builders::TabManager_WindowMetrics entry(ukm::AssignNewSourceId());
   entry.SetWindowId(window_metrics.window_id)
+      .SetIsActive(window_metrics.is_active)
       .SetShowState(window_metrics.show_state)
-      .SetType(window_metrics.type)
-      .Record(ukm_recorder);
+      .SetType(window_metrics.type);
+
+  // Bucketize values for privacy considerations. Use a spacing factor that
+  // ensures values up to 3 can be logged exactly; after that, the precise
+  // number becomes less significant.
+  int tab_count = window_metrics.tab_count;
+  if (tab_count > 3)
+    tab_count = ukm::GetExponentialBucketMin(tab_count, 1.5);
+  entry.SetTabCount(tab_count);
+
+  entry.Record(ukm_recorder);
 }
 
 }  // namespace
+
+// Observes a browser window's tab strip and logs a WindowMetrics UKM event for
+// the window upon changes to metrics like TabCount.
+class WindowActivityWatcher::BrowserWatcher : public TabStripModelObserver {
+ public:
+  explicit BrowserWatcher(Browser* browser)
+      : browser_(browser), observer_(this) {
+    DCHECK(!browser->profile()->IsOffTheRecord());
+    MaybeLogWindowMetricsUkmEntry();
+    observer_.Add(browser->tab_strip_model());
+  }
+
+  ~BrowserWatcher() override = default;
+
+  // Logs a new WindowMetrics entry to the UKM recorder if the entry would be
+  // different than the last one we logged.
+  void MaybeLogWindowMetricsUkmEntry() {
+    // Do nothing if the window has no tabs (which can happen when a window is
+    // opened, before a tab is added) or is being closed.
+    if (browser_->tab_strip_model()->empty() ||
+        browser_->tab_strip_model()->closing_all()) {
+      return;
+    }
+
+    if (!last_window_metrics_) {
+      last_window_metrics_ = CreateMetrics(browser_);
+      LogWindowMetricsUkmEntry(last_window_metrics_.value());
+      return;
+    }
+
+    // Copy old state to compare with.
+    WindowMetrics old_metrics = last_window_metrics_.value();
+    UpdateMetrics(browser_, &last_window_metrics_.value());
+
+    // We only need to create a new UKM entry if the metrics have changed.
+    if (old_metrics != last_window_metrics_.value())
+      LogWindowMetricsUkmEntry(last_window_metrics_.value());
+  }
+
+ private:
+  // TabStripModelObserver:
+  void TabInsertedAt(TabStripModel* tab_strip_model,
+                     content::WebContents* contents,
+                     int index,
+                     bool foreground) override {
+    MaybeLogWindowMetricsUkmEntry();
+  }
+  void TabDetachedAt(content::WebContents* contents, int index) override {
+    MaybeLogWindowMetricsUkmEntry();
+  }
+
+  // The browser whose tab strip we track. WindowActivityWatcher should ensure
+  // this outlives us.
+  Browser* browser_;
+
+  // The most recent WindowMetrics entry logged. We only log a new UKM entry if
+  // some metric value has changed.
+  base::Optional<WindowMetrics> last_window_metrics_;
+
+  // Used to update the tab count for browser windows.
+  ScopedObserver<TabStripModel, TabStripModelObserver> observer_;
+
+  DISALLOW_COPY_AND_ASSIGN(BrowserWatcher);
+};
 
 // static
 WindowActivityWatcher* WindowActivityWatcher::GetInstance() {
@@ -78,45 +170,36 @@ WindowActivityWatcher* WindowActivityWatcher::GetInstance() {
   return &instance;
 }
 
-void WindowActivityWatcher::CreateOrUpdateWindowMetrics(
-    const Browser* browser) {
-  DCHECK(!browser->profile()->IsOffTheRecord());
-  SessionID::id_type window_id = browser->session_id().id();
-  const auto& existing_metrics_it = window_metrics_.find(window_id);
-
-  // We only need to create a new UKM entry if the metrics have changed or
-  // this browser hasn't been logged yet.
-  WindowMetrics new_metrics = CreateMetrics(browser);
-  if (existing_metrics_it == window_metrics_.end() ||
-      existing_metrics_it->second != new_metrics) {
-    LogUkmEntry(new_metrics);
-    window_metrics_[window_id] = new_metrics;
-  }
+WindowActivityWatcher::WindowActivityWatcher() {
+  BrowserList::AddObserver(this);
+  for (Browser* browser : *BrowserList::GetInstance())
+    OnBrowserAdded(browser);
 }
 
-WindowActivityWatcher::WindowActivityWatcher() : browser_list_observer_(this) {
-  browser_list_observer_.Add(BrowserList::GetInstance());
+WindowActivityWatcher::~WindowActivityWatcher() {
+  BrowserList::RemoveObserver(this);
 }
 
-WindowActivityWatcher::~WindowActivityWatcher() = default;
+bool WindowActivityWatcher::ShouldTrackBrowser(Browser* browser) {
+  // Don't track incognito browsers. This is also enforced by UKM.
+  return !browser->profile()->IsOffTheRecord();
+}
 
 void WindowActivityWatcher::OnBrowserAdded(Browser* browser) {
-  // Don't track incognito browsers. This is also enforced by UKM.
-  if (!browser->profile()->IsOffTheRecord())
-    CreateOrUpdateWindowMetrics(browser);
+  if (ShouldTrackBrowser(browser))
+    browser_watchers_[browser] = std::make_unique<BrowserWatcher>(browser);
 }
 
 void WindowActivityWatcher::OnBrowserRemoved(Browser* browser) {
-  if (!browser->profile()->IsOffTheRecord())
-    window_metrics_.erase(browser->session_id().id());
+  browser_watchers_.erase(browser);
 }
 
 void WindowActivityWatcher::OnBrowserSetLastActive(Browser* browser) {
-  if (!browser->profile()->IsOffTheRecord())
-    CreateOrUpdateWindowMetrics(browser);
+  if (ShouldTrackBrowser(browser))
+    browser_watchers_[browser]->MaybeLogWindowMetricsUkmEntry();
 }
 
 void WindowActivityWatcher::OnBrowserNoLongerActive(Browser* browser) {
-  if (!browser->profile()->IsOffTheRecord())
-    CreateOrUpdateWindowMetrics(browser);
+  if (ShouldTrackBrowser(browser))
+    browser_watchers_[browser]->MaybeLogWindowMetricsUkmEntry();
 }
--- chrome/browser/ui/tabs/window_activity_watcher.h
+++ chrome/browser/ui/tabs/window_activity_watcher.h
@@ -5,15 +5,11 @@
 #ifndef CHROME_BROWSER_UI_TABS_WINDOW_ACTIVITY_WATCHER_H_
 #define CHROME_BROWSER_UI_TABS_WINDOW_ACTIVITY_WATCHER_H_
 
-#include <stddef.h>
+#include <memory>
 
 #include "base/containers/flat_map.h"
 #include "base/macros.h"
-#include "base/scoped_observer.h"
 #include "chrome/browser/ui/browser_list_observer.h"
-#include "components/sessions/core/session_id.h"
-
-class BrowserList;
 
 // Observes browser window activity in order to log WindowMetrics UKMs for
 // browser events relative to tab activation and discarding.
@@ -22,20 +18,16 @@ class BrowserList;
 // TODO(michaelpg): Observe app and ARC++ windows as well.
 class WindowActivityWatcher : public BrowserListObserver {
  public:
-  // Represents a UKM entry for window metrics.
-  struct WindowMetrics;
+  class BrowserWatcher;
 
   // Returns the single instance, creating it if necessary.
   static WindowActivityWatcher* GetInstance();
 
-  // Ensures the window's current stats are logged.
-  // A new UKM entry will only be logged if an existing entry for the same
-  // window doesn't exist yet or has stale properties.
-  void CreateOrUpdateWindowMetrics(const Browser* browser);
-
  private:
   WindowActivityWatcher();
   ~WindowActivityWatcher() override;
+
+  bool ShouldTrackBrowser(Browser* browser);
 
   // BrowserListObserver:
   void OnBrowserAdded(Browser* browser) override;
@@ -43,11 +35,9 @@ class WindowActivityWatcher : public BrowserListObserver {
   void OnBrowserSetLastActive(Browser* browser) override;
   void OnBrowserNoLongerActive(Browser* browser) override;
 
-  ScopedObserver<BrowserList, BrowserListObserver> browser_list_observer_;
-
-  // Cache of WindowMetrics logged for each window. Used to avoid logging
-  // duplicate identical UKM events.
-  base::flat_map<SessionID::id_type, WindowMetrics> window_metrics_;
+  // Per-browser observers responsible for tracking the tab strip and logging
+  // new UKM entries on changes.
+  base::flat_map<Browser*, std::unique_ptr<BrowserWatcher>> browser_watchers_;
 
   DISALLOW_COPY_AND_ASSIGN(WindowActivityWatcher);
 };
