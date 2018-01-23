04d6018de840ea680ddc267b57cb6bcef6799bd0
Maksim Sisov <msisov@igalia.com>
2018-01-23 09:07:38 +0000

    Reland "Move and rename ConvertEventToDifferentHost to event_utils.h"
    This relands commit 0bfab7f6176 [formerly 53549f0a1c2], reverted by 31ee7d1de1 [formerly bfdecad144], due to
    https://crbug.com/794140
    
    Added a check in desktop_window_tree_host_x11, which doesn't send
    a MouseEvents, when a type of events is UNKNOWN.
    Events' type can be resolved to UNKNOWN because of a bug
    in DeviceDataManagerX11, when an event takes a path meant for
    events from cmt devices, which doesn't seem to exist on Linux,
    but ChromeOs.
    
    Original message stated that this patch doesn't bring any
    functionality changes,but moves ConvertEventToDifferentHost
    to event_utils and makes it also available for ozone x11 builds,
    but actually it has revealed a bug with coalescing XEvents.
    
    Bug: 707406, 794140, 804418
    Change-Id: Ic57be5baad2e4d9fb0543ee1b0a83294aee470f6
    Reviewed-on: https://chromium-review.googlesource.com/853953
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531189}
    Former-commit-id: e46169bf03375205b6cf62cde0d68ff88c80ab14

--- ui/views/widget/desktop_aura/desktop_window_tree_host_x11.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_x11.cc
@@ -1758,7 +1758,11 @@ void DesktopWindowTreeHostX11::DispatchMouseEvent(ui::MouseEvent* event) {
   } else {
     // Another DesktopWindowTreeHostX11 has installed itself as
     // capture. Translate the event's location and dispatch to the other.
-    ConvertEventToDifferentHost(event, g_current_capture);
+    DCHECK_EQ(ui::GetScaleFactorForNativeView(window()),
+              ui::GetScaleFactorForNativeView(g_current_capture->window()));
+    ConvertEventLocationToTargetWindowLocation(
+        g_current_capture->GetLocationOnScreenInPixels(),
+        GetLocationOnScreenInPixels(), event->AsLocatedEvent());
     g_current_capture->SendEventToSink(event);
   }
 }
@@ -1766,7 +1770,11 @@ void DesktopWindowTreeHostX11::DispatchMouseEvent(ui::MouseEvent* event) {
 void DesktopWindowTreeHostX11::DispatchTouchEvent(ui::TouchEvent* event) {
   if (g_current_capture && g_current_capture != this &&
       event->type() == ui::ET_TOUCH_PRESSED) {
-    ConvertEventToDifferentHost(event, g_current_capture);
+    DCHECK_EQ(ui::GetScaleFactorForNativeView(window()),
+              ui::GetScaleFactorForNativeView(g_current_capture->window()));
+    ConvertEventLocationToTargetWindowLocation(
+        g_current_capture->GetLocationOnScreenInPixels(),
+        GetLocationOnScreenInPixels(), event->AsLocatedEvent());
     g_current_capture->SendEventToSink(event);
   } else {
     SendEventToSink(event);
@@ -1776,20 +1784,6 @@ void DesktopWindowTreeHostX11::DispatchTouchEvent(ui::TouchEvent* event) {
 void DesktopWindowTreeHostX11::DispatchKeyEvent(ui::KeyEvent* event) {
   if (native_widget_delegate_->AsWidget()->IsActive())
     SendEventToSink(event);
-}
-
-void DesktopWindowTreeHostX11::ConvertEventToDifferentHost(
-    ui::LocatedEvent* located_event,
-    DesktopWindowTreeHostX11* host) {
-  DCHECK_NE(this, host);
-  DCHECK_EQ(ui::GetScaleFactorForNativeView(window()),
-            ui::GetScaleFactorForNativeView(host->window()));
-  gfx::Vector2d offset =
-      GetLocationOnScreenInPixels() - host->GetLocationOnScreenInPixels();
-  gfx::PointF location_in_pixel_in_host =
-      located_event->location_f() + gfx::Vector2dF(offset);
-  located_event->set_location_f(location_in_pixel_in_host);
-  located_event->set_root_location_f(location_in_pixel_in_host);
 }
 
 void DesktopWindowTreeHostX11::ResetWindowRegion() {
@@ -2092,7 +2086,15 @@ uint32_t DesktopWindowTreeHostX11::DispatchEvent(
               xev = &last_event;
           }
           ui::MouseEvent mouseev(xev);
-          DispatchMouseEvent(&mouseev);
+          // If after CoalescePendingMotionEvents the type of xev is resolved to
+          // UNKNOWN, don't dispatch the event.
+          // TODO(804418): investigate why ColescePendingMotionEvents can
+          // include mouse wheel events as well. Investigation showed that
+          // events on Linux are checked with cmt-device path, and can include
+          // DT_CMT_SCROLL_ data. See more discussion in
+          // https://crrev.com/c/853953
+          if (mouseev.type() != ui::ET_UNKNOWN)
+            DispatchMouseEvent(&mouseev);
           break;
         }
         case ui::ET_MOUSEWHEEL: {
