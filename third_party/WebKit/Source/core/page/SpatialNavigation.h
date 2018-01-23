3c829175b8b680e440392225be4cc011bd3b6155
Hugo Holgersson <hugoh@vewd.com>
2018-01-23 15:54:32 +0000

    Snav: Always search for focusables in visual viewport first
    By initializing starting_rect to one of the visual viewport's
    edges we don't need to recalculate this value every time
    AdvanceFocusDirectionallyInContainer iterates.
    
    Q: So now we always use the frame's visual viewport when the
       focused element is off screen?
    
    A: Yes. The visual viewport is the default starting point for
       snav's search. This was already the implicit behavior users
       relied on (see bug).
       When the focused element F is off screen (say JS scrolled
       <body> downwards) we want spatnav to first look for already
       visible focusables, i.e not go to focusables nearby F because
       that will cause the window to scroll (bad UX).
    
    This lets users reach things that they can _see_ first.
    
           snav-ignore-offscreen-focused-imagemap.html (NEW)
    
    Bug: 770147, 796493, 804242
    Tests: snav-use-visual-viewport.html,
    Change-Id: Iaa861d3f75effe14eac558e2214dbc3af2df365b
    Reviewed-on: https://chromium-review.googlesource.com/873645
    Commit-Queue: Hugo Holgersson <hugoh@vewd.com>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531249}
    Former-commit-id: 511906d0604cdd92df8aa077b82c51aa1a298525

--- third_party/WebKit/Source/core/page/SpatialNavigation.h
+++ third_party/WebKit/Source/core/page/SpatialNavigation.h
@@ -128,7 +128,7 @@ struct FocusCandidate {
   bool is_offscreen_after_scrolling;
 };
 
-bool HasOffscreenRect(Node*, WebFocusType = kWebFocusTypeNone);
+bool HasOffscreenRect(const Node*, WebFocusType = kWebFocusTypeNone);
 bool ScrollInDirection(LocalFrame*, WebFocusType);
 bool ScrollInDirection(Node* container, WebFocusType);
 bool IsNavigableContainer(const Node*, WebFocusType);
@@ -142,14 +142,15 @@ bool AreElementsOnSameLine(const FocusCandidate& first_candidate,
 void DistanceDataForNode(WebFocusType,
                          const FocusCandidate& current,
                          FocusCandidate&);
-LayoutRect NodeRectInAbsoluteCoordinates(Node*, bool ignore_border = false);
-LayoutRect FrameRectInAbsoluteCoordinates(LocalFrame*);
+LayoutRect NodeRectInAbsoluteCoordinates(const Node*, bool ignore_border = false);
+LayoutRect FrameRectInAbsoluteCoordinates(const LocalFrame*);
 LayoutRect VirtualRectForDirection(WebFocusType,
                                    const LayoutRect& starting_rect,
                                    LayoutUnit width = LayoutUnit());
-LayoutRect VirtualRectForAreaElementAndDirection(HTMLAreaElement&,
+LayoutRect VirtualRectForAreaElementAndDirection(const HTMLAreaElement&,
                                                  WebFocusType);
 HTMLFrameOwnerElement* FrameOwnerElement(FocusCandidate&);
+LayoutRect FindSearchStartPoint(const LocalFrame*, WebFocusType);
 
 }  // namespace blink
 
