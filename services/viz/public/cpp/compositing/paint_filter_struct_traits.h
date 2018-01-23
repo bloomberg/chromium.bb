27fc94ef803798ea457cf156fa705a6f253ca83b
Khushal <khushalsagar@chromium.org>
2018-01-23 02:17:46 +0000

    cc/ipc: Don't reject CFs in case of a filter serialization error.
    PaintOpWriter fails to serialize if the allocated memory is not enough
    to serialize the filter. Avoid terminating the renderer for a bad
    message in such cases.
    
    R=enne@chromium.org
    
    Bug: 803777
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I70492b681fff22064ea95477015e4841b76e61a3
    Reviewed-on: https://chromium-review.googlesource.com/879229
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531130}
    Former-commit-id: fb25d8a626538f7dd54f3efb381db9fe20927650

--- services/viz/public/cpp/compositing/paint_filter_struct_traits.h
+++ services/viz/public/cpp/compositing/paint_filter_struct_traits.h
@@ -14,51 +14,39 @@
 
 namespace mojo {
 
-struct PaintFilterBuffer {
-  PaintFilterBuffer();
-  PaintFilterBuffer(const PaintFilterBuffer& other);
-  ~PaintFilterBuffer();
-  std::vector<uint8_t> data;
-};
-
-template <>
-struct ArrayTraits<PaintFilterBuffer> {
-  using Element = uint8_t;
-  static size_t GetSize(const PaintFilterBuffer& b);
-  static uint8_t* GetData(PaintFilterBuffer& b);
-  static const uint8_t* GetData(const PaintFilterBuffer& b);
-  static uint8_t& GetAt(PaintFilterBuffer& b, size_t i);
-  static const uint8_t& GetAt(const PaintFilterBuffer& b, size_t i);
-  static bool Resize(PaintFilterBuffer& b, size_t size);
-};
-
 template <>
 struct StructTraits<viz::mojom::PaintFilterDataView, sk_sp<cc::PaintFilter>> {
-  static PaintFilterBuffer data(const sk_sp<cc::PaintFilter>& filter) {
-    PaintFilterBuffer buffer;
+  static base::Optional<std::vector<uint8_t>> data(
+      const sk_sp<cc::PaintFilter>& filter) {
     static const size_t kBufferSize = 8 * 1024;
-    buffer.data.resize(kBufferSize);
-    cc::PaintOpWriter writer(buffer.data.data(), kBufferSize, nullptr, nullptr,
+    std::vector<uint8_t> memory;
+    memory.resize(kBufferSize);
+    cc::PaintOpWriter writer(memory.data(), kBufferSize, nullptr, nullptr,
                              true /* enable_security_constraints */);
     writer.Write(filter.get());
     if (writer.size() == 0)
-      buffer.data.clear();
-    else
-      buffer.data.resize(writer.size());
-    return buffer;
+      return base::nullopt;
+    return memory;
   }
 
   static bool Read(viz::mojom::PaintFilterDataView data,
                    sk_sp<cc::PaintFilter>* out) {
-    PaintFilterBuffer buffer;
+    base::Optional<std::vector<uint8_t>> buffer;
     if (!data.ReadData(&buffer))
       return false;
 
-    cc::PaintOpReader reader(buffer.data.data(), buffer.data.size(), nullptr,
+    if (!buffer) {
+      // We may fail to serialize the filter if it doesn't fit in kBufferSize
+      // above, use an empty filter instead of rejecting the message.
+      *out = nullptr;
+      return true;
+    }
+
+    cc::PaintOpReader reader(buffer->data(), buffer->size(), nullptr,
                              true /* enable_security_constraints */);
     sk_sp<cc::PaintFilter> filter;
     reader.Read(&filter);
-    if (!filter) {
+    if (!reader.valid()) {
       *out = nullptr;
       return false;
     }
