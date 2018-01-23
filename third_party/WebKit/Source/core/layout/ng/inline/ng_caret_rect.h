b0a4a9da2a813a1b434733a1d6a4388d8a894cf6
Xiaocheng Hu <xiaochengh@chromium.org>
2018-01-23 07:57:41 +0000

    [LayoutNG] Introduce NGCaretPosition and its computation
    This patch introduces a new struct, NGCaretPosition, to specify a
    position of possible caret placement relative to an NGPhysicalFragment.
    
    The patch also introduces computation of NGCaretPosition in an
    inline formatting context, given a text content offset./
    
    A follow-up patch (crrev.com/c/843148) will utilize NGCaretPosition
    to compute LocalCaretRect from DOM positions.
    
    Bug: 771398
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: If437191d2def9eee33c0da79a74c27e796d98234
    Reviewed-on: https://chromium-review.googlesource.com/803015
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531177}
    Former-commit-id: eac28c5dbcd529536aae75575e33204fb6935cf4

---
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_caret_rect.h
@@ -0,0 +1,51 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NGCaretRect_h
+#define NGCaretRect_h
+
+#include "core/editing/Forward.h"
+#include "core/layout/ng/geometry/ng_physical_offset_rect.h"
+#include "core/layout/ng/ng_physical_fragment.h"
+#include "platform/wtf/Forward.h"
+#include "platform/wtf/Optional.h"
+
+namespace blink {
+
+// This file provides utility functions for computing caret rect in LayoutNG.
+
+class LayoutBlockFlow;
+
+// An NGCaretPosition indicates a caret position relative to an inline
+// NGPhysicalFragment:
+// - When |fragment| is box, |position_type| is either |kBeforeBox| or
+// |kAfterBox|, indicating either of the two caret positions by the box sides;
+// |text_offset| is |nullopt| in this case.
+// - When |fragment| is text, |position_type| is |kAtTextOffset|, and
+// |text_offset| is in the text offset range of the fragment.
+//
+// TODO(xiaochengh): Support "in empty container" caret type
+
+enum class NGCaretPositionType { kBeforeBox, kAfterBox, kAtTextOffset };
+struct NGCaretPosition {
+  DISALLOW_NEW_EXCEPT_PLACEMENT_NEW();
+
+  bool IsNull() const { return !fragment; }
+
+  scoped_refptr<const NGPhysicalFragment> fragment;
+  NGCaretPositionType position_type;
+  Optional<unsigned> text_offset;
+};
+
+// Given an inline formatting context, a text offset in the context and a text
+// affinity, returns the corresponding NGCaretPosition, or null if not found.
+// Note that in many cases, null result indicates that we have reached an
+// unexpected case that is not properly handled.
+CORE_EXPORT NGCaretPosition ComputeNGCaretPosition(const LayoutBlockFlow&,
+                                                   unsigned,
+                                                   TextAffinity);
+
+}  // namespace blink
+
+#endif  // NGCaretRect_h
