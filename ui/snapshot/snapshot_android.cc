450fb48e4c402c2ab4d319bbd002e549ceec473b
Michael Thiessen <mthiesse@chromium.org>
2018-01-23 00:03:56 +0000

    Don't crash when taking screenshot of Compositor-less window.
    Under some circumstances the window having its screenshot taken might
    not have a Compositor attached (like some of the time while in VR).
    
    Avoid crashing, and just return an empty screenshot.
    
    Bug: 793430
    
    Change-Id: Idfe6ef9937bc261e4e07b3630aa34d2a98df8ee7
    Reviewed-on: https://chromium-review.googlesource.com/874792
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531070}
    Former-commit-id: 787c991f6677df8b8816d067f21c08a7510e75dc

--- ui/snapshot/snapshot_android.cc
+++ ui/snapshot/snapshot_android.cc
@@ -38,6 +38,11 @@ static void MakeAsyncCopyRequest(
     gfx::NativeWindow window,
     const gfx::Rect& source_rect,
     viz::CopyOutputRequest::CopyOutputRequestCallback callback) {
+  if (!window->GetCompositor()) {
+    std::move(callback).Run(std::make_unique<viz::CopyOutputResult>(
+        viz::CopyOutputRequest::ResultFormat::RGBA_BITMAP, gfx::Rect()));
+    return;
+  }
   std::unique_ptr<viz::CopyOutputRequest> request =
       std::make_unique<viz::CopyOutputRequest>(
           viz::CopyOutputRequest::ResultFormat::RGBA_BITMAP,
