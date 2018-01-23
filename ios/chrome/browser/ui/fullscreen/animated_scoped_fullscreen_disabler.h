680621d1e179152d91949dc899c3a2b9896ec183
sczs <sczs@chromium.org>
2018-01-23 20:28:22 +0000

    [ios] Creates AnimatedScopedFullscreenDisabler
    In some cases the UI changes related to Fullscreen being disabled need to animate.
    AnimatedScopedFullscreenDisabler works just like ScopedFullscreenDisabler but increments the
    disabled counter inside an animation block.
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I79710443b8fa9d8137a03ce71035ccc9b077e1c6
    Reviewed-on: https://chromium-review.googlesource.com/879568
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531332}
    Former-commit-id: a85238a0323ce2e1a899eea2f6f4e040a5b95efb

---
+++ ios/chrome/browser/ui/fullscreen/animated_scoped_fullscreen_disabler.h
@@ -0,0 +1,26 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_FULLSCREEN_ANIMATED_SCOPED_FULLSCREEN_DISABLER_H_
+#define IOS_CHROME_BROWSER_UI_FULLSCREEN_ANIMATED_SCOPED_FULLSCREEN_DISABLER_H_
+
+#include "base/macros.h"
+
+class FullscreenController;
+
+// A helper object that increments FullscrenController's disabled counter for
+// its entire lifetime. Since the disabling is happening inside an animation
+// block, any UI changes related to Fullscreen being disabled will be animated.
+class AnimatedScopedFullscreenDisabler {
+ public:
+  explicit AnimatedScopedFullscreenDisabler(FullscreenController* controller);
+
+ private:
+  // The FullscreenController being disabled by this object.
+  FullscreenController* controller_;
+
+  DISALLOW_COPY_AND_ASSIGN(AnimatedScopedFullscreenDisabler);
+};
+
+#endif  // IOS_CHROME_BROWSER_UI_FULLSCREEN_ANIMATED_SCOPED_FULLSCREEN_DISABLER_H_
---
+++ ios/chrome/browser/ui/fullscreen/animated_scoped_fullscreen_disabler.mm
@@ -0,0 +1,27 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/fullscreen/animated_scoped_fullscreen_disabler.h"
+
+#import "ios/chrome/browser/ui/fullscreen/fullscreen_controller.h"
+#import "ios/chrome/common/material_timing.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+AnimatedScopedFullscreenDisabler::AnimatedScopedFullscreenDisabler(
+    FullscreenController* controller)
+    : controller_(controller) {
+  DCHECK(controller_);
+  [UIView animateWithDuration:ios::material::kDuration1
+                   animations:^{
+                     controller_->IncrementDisabledCounter();
+                   }
+                   completion:nil];
+}
+
+AnimatedScopedFullscreenDisabler::~AnimatedScopedFullscreenDisabler() {
+  controller_->DecrementDisabledCounter();
+}
