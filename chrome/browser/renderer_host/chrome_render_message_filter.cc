2c71c2799a3e1657e657381120d9434776804bb1
Daniel Bratell <bratell@opera.com>
2018-01-23 20:16:32 +0000

    Give some kFilteredMessageClasses variables unique names
    In jumbo builds multiple cc files are compiled in the same
    translation unit and then they will all share the
    anonymous namespace which can cause collisions if
    variables have the same name. That was the case for two
    kFilteredMessageClasses variables.
    
    This renames them to:
    kExtensionFilteredMessageClasses
    kRenderFilteredMessageClasses
    
    This is a followup to an equivalent change done in //content
    https://chromium-review.googlesource.com/721279
    
    Bug: 746957
    Change-Id: I959600adcc81dbd6e405c25aa0272acefc0387d9
    Reviewed-on: https://chromium-review.googlesource.com/878362
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#531325}
    Former-commit-id: ea629e7d8539feb27a9792a1d63452e7230dce3a

--- chrome/browser/renderer_host/chrome_render_message_filter.cc
+++ chrome/browser/renderer_host/chrome_render_message_filter.cc
@@ -43,7 +43,7 @@ using content::BrowserThread;
 
 namespace {
 
-const uint32_t kFilteredMessageClasses[] = {
+const uint32_t kRenderFilteredMessageClasses[] = {
     ChromeMsgStart, NetworkHintsMsgStart,
 };
 
@@ -51,8 +51,8 @@ const uint32_t kFilteredMessageClasses[] = {
 
 ChromeRenderMessageFilter::ChromeRenderMessageFilter(int render_process_id,
                                                      Profile* profile)
-    : BrowserMessageFilter(kFilteredMessageClasses,
-                           arraysize(kFilteredMessageClasses)),
+    : BrowserMessageFilter(kRenderFilteredMessageClasses,
+                           arraysize(kRenderFilteredMessageClasses)),
       render_process_id_(render_process_id),
       profile_(profile),
       predictor_(profile_->GetNetworkPredictor()),
