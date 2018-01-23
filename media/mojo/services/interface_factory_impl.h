b4fa93d77a29a78a21a6156316553d609eb881d1
Xiaohan Wang <xhwang@chromium.org>
2018-01-23 20:23:56 +0000

    media: Simplify CdmService
    - Restrict CdmService to only host library CDM. Previously CdmService
      is generic, but it's never used anywhere else, and that assumption
      has caused some unnecessary code complexity.
    - As such, also remove ENABLE_STANDALONE_CDM_SERVICE and replace it
      with ENABLE_LIBRARY_CDMS where applicable.
    - Remove the use of media::mojom::InterfaceFactory for the CdmService,
      and replace it with a dedicated media::mojom::CdmFactory.
    - Remove the use of MojoMediaClient in CdmService, and replace it with
      CdmService::Client.
    
    BUG=771791
    
    Change-Id: I49e0fc4958b18e1b619e20ab4772245b256ab454
    Reviewed-on: https://chromium-review.googlesource.com/852556
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531329}
    Former-commit-id: a3cb5eee99a8d6327fcedc517f903c8c53a92286

--- media/mojo/services/interface_factory_impl.h
+++ media/mojo/services/interface_factory_impl.h
@@ -18,7 +18,6 @@
 namespace media {
 
 class CdmFactory;
-class DelayedReleaseServiceContextRef;
 class MediaLog;
 class MojoMediaClient;
 class RendererFactory;
@@ -80,7 +79,7 @@ class InterfaceFactoryImpl : public mojom::InterfaceFactory {
   mojo::StrongBindingSet<mojom::CdmProxy> cdm_proxy_bindings_;
 #endif  // BUILDFLAG(ENABLE_LIBRARY_CDMS)
 
-  std::unique_ptr<DelayedReleaseServiceContextRef> connection_ref_;
+  std::unique_ptr<service_manager::ServiceContextRef> connection_ref_;
   MojoMediaClient* mojo_media_client_;
 
   DISALLOW_COPY_AND_ASSIGN(InterfaceFactoryImpl);
