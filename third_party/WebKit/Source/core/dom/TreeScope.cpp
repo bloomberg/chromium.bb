bbf6a3c580dea9b8773d2a998098a98359993feb
Takashi Sakamoto <tasak@google.com>
2018-01-23 08:06:40 +0000

    Revert "Fix retargeting of result in elementFromPoint and elementsFromPoint"
    This reverts commit a636d80a898626ffb10918df8beff39f2fb4f903 [formerly dd944882a245a5117b50cb417138d92f32d931d6].
    
    Reason for revert: 
    This causes WebKit Linux Trusty ASAN buildbot failure.
    https://uberchromegw.corp.google.com/i/chromium.webkit/builders/WebKit%20Linux%20Trusty%20ASAN/builds/8618
    
    23:46:29.565 3877   ==1==ERROR: AddressSanitizer: use-after-poison on address 0x7ead60c0dbf0 at pc 0x00000dca937a bp 0x7ffd86b90c10 sp 0x7ffd86b90c08
    23:46:29.565 3877   READ of size 8 at 0x7ead60c0dbf0 thread T0 (content_shell)
    23:46:29.565 3877       #0 0xdca9379 in operator==<const blink::TreeScope, const blink::TreeScope> third_party/WebKit/Source/platform/heap/Member.h:128:27
    23:46:29.565 3877       #1 0xdca9379 in blink::TreeScope::Retarget(blink::Element const&) const third_party/WebKit/Source/core/dom/TreeScope.cpp:393:0
    23:46:29.565 3877       #2 0xdca8894 in blink::TreeScope::HitTestPointInternal(blink::Node*) const third_party/WebKit/Source/core/dom/TreeScope.cpp:267:10
    23:46:29.565 3877       #3 0xdca8325 in HitTestPoint third_party/WebKit/Source/core/dom/TreeScope.cpp:254:10
    23:46:29.566 3877       #4 0xdca8325 in blink::TreeScope::ElementFromPoint(double, double) const third_party/WebKit/Source/core/dom/TreeScope.cpp:245:0
    23:46:29.566 3877       #5 0xc9353a7 in elementFromPoint third_party/WebKit/Source/core/dom/DocumentOrShadowRoot.h:38:23
    
    
    
    Original change's description:
    > Fix retargeting of result in elementFromPoint and elementsFromPoint
    > 
    > Currently elementFromPoint and elementsFromPoint are not per spec, and it may
    > return null incorrectly. This change adds retargeting of the result with
    > respect to the context object, and adds some tests that are similar to
    > elementFromPoint tests in WebKit, but with some corrected cases:
    > https://git.webkit.org/?p=WebKit-https.git;a=blob;f=LayoutTests/fast/shadow-dom/DocumentOrShadowRoot-prototype-elementFromPoint.html;h=a8dc4da2430713521b9ba77c742db10397a8e638;hb=HEAD
    > 
    > Spec:
    > https://w3c.github.io/webcomponents/spec/shadow/#extensions-to-the-documentorshadowroot-mixin
    > 
    > Bug: 759947
    > Change-Id: I6aece5e9cc826124772c6ce13c806865055b2b9b
    > Reviewed-on: https://chromium-review.googlesource.com/808446
    > Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    > Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    > Reviewed-by: Hayato Ito <hayato@chromium.org>
    > Reviewed-by: Takayoshi Kochi <kochi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#531139}
    
    TBR=kochi@chromium.org,dgozman@chromium.org,hayato@chromium.org,pfeldman@chromium.org,rakina@chromium.org
    
    Change-Id: Id62abd371d93627d3178b63ca189cecfe9ff44d4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 759947
    Reviewed-on: https://chromium-review.googlesource.com/880264
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/master@{#531178}
    Former-commit-id: 708d660d1ceac6bb5aeeb70e318f03c92bac8693

--- third_party/WebKit/Source/core/dom/TreeScope.cpp
+++ third_party/WebKit/Source/core/dom/TreeScope.cpp
@@ -251,20 +251,16 @@ Element* TreeScope::HitTestPoint(double x,
                                  const HitTestRequest& request) const {
   HitTestResult result =
       HitTestInDocument(&RootNode().GetDocument(), x, y, request);
-  return HitTestPointInternal(result.InnerNode());
-}
-
-Element* TreeScope::HitTestPointInternal(Node* node) const {
+  Node* node = result.InnerNode();
   if (!node || node->IsDocumentNode())
     return nullptr;
-  Element* element;
   if (node->IsPseudoElement() || node->IsTextNode())
-    element = node->ParentOrShadowHostElement();
-  else
-    element = ToElement(node);
-  if (!element)
+    node = node->ParentOrShadowHostNode();
+  DCHECK(!node || node->IsElementNode() || node->IsShadowRoot());
+  node = AncestorInThisScope(node);
+  if (!node || !node->IsElementNode())
     return nullptr;
-  return Retarget(*element);
+  return ToElement(node);
 }
 
 HeapVector<Member<Element>> TreeScope::ElementsFromHitTestResult(
@@ -274,11 +270,13 @@ HeapVector<Member<Element>> TreeScope::ElementsFromHitTestResult(
   Node* last_node = nullptr;
   for (const auto rect_based_node : result.ListBasedTestResult()) {
     Node* node = rect_based_node.Get();
-    // In some cases the hit test doesn't return slot elements, so we can only
-    // get it through its child and can't skip it.
-    if (!node->IsElementNode() && !IsHTMLSlotElement(node->parentNode()))
+    if (!node || !node->IsElementNode() || node->IsDocumentNode())
       continue;
-    node = HitTestPointInternal(node);
+
+    if (node->IsPseudoElement() || node->IsTextNode())
+      node = node->ParentOrShadowHostNode();
+    node = AncestorInThisScope(node);
+
     // Prune duplicate entries. A pseduo ::before content above its parent
     // node should only result in a single entry.
     if (node == last_node)
@@ -368,42 +366,7 @@ void TreeScope::AdoptIfNeeded(Node& node) {
     adopter.Execute();
 }
 
-// This method corresponds to the Retarget algorithm specified in
-// https://dom.spec.whatwg.org/#retarget
-// This retargets |target| against the root of |this|.
-// The steps are different with the spec for performance reasons,
-// but the results should be the same.
 Element* TreeScope::Retarget(const Element& target) const {
-  const TreeScope& target_scope = target.GetTreeScope();
-  if (!target_scope.RootNode().IsShadowRoot())
-    return const_cast<Element*>(&target);
-
-  HeapVector<Member<const TreeScope>> target_ancestor_scopes;
-  HeapVector<Member<const TreeScope>> context_ancestor_scopes;
-  for (const TreeScope* tree_scope = &target_scope; tree_scope;
-       tree_scope = tree_scope->ParentTreeScope())
-    target_ancestor_scopes.push_back(tree_scope);
-  for (const TreeScope* tree_scope = this; tree_scope;
-       tree_scope = tree_scope->ParentTreeScope())
-    context_ancestor_scopes.push_back(tree_scope);
-
-  auto target_ancestor_riterator = target_ancestor_scopes.rbegin();
-  auto context_ancestor_riterator = context_ancestor_scopes.rbegin();
-  while (context_ancestor_riterator != context_ancestor_scopes.rend() &&
-         *context_ancestor_riterator == *target_ancestor_riterator) {
-    ++context_ancestor_riterator;
-    ++target_ancestor_riterator;
-  }
-
-  if (target_ancestor_riterator == target_ancestor_scopes.rend())
-    return const_cast<Element*>(&target);
-  Node& first_different_scope_root =
-      (*target_ancestor_riterator).Get()->RootNode();
-  return &ToShadowRoot(first_different_scope_root).host();
-}
-
-Element* TreeScope::AdjustedFocusedElementInternal(
-    const Element& target) const {
   for (const Element* ancestor = &target; ancestor;
        ancestor = ancestor->OwnerShadowHost()) {
     if (this == ancestor->GetTreeScope())
@@ -422,7 +385,7 @@ Element* TreeScope::AdjustedFocusedElement() const {
     return nullptr;
 
   if (RootNode().IsInV1ShadowTree()) {
-    if (Element* retargeted = AdjustedFocusedElementInternal(*element)) {
+    if (Element* retargeted = Retarget(*element)) {
       return (this == &retargeted->GetTreeScope()) ? retargeted : nullptr;
     }
     return nullptr;
