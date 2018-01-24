043ea4d1b31eab08256244aa80750c994ff04975
Fredrik Söderquist <fs@opera.com>
2018-01-24 15:03:14 +0000

    [CI] Avoid using FillLayer::Size() in BackgroundImageGeometry
    FillLayer::Size() returns a FillSize, which is made up of a copy of the
    FillLayer's |size_length_| and |size_type_| fields.
    This is unnecessarily costly (both in cycles and bytes/footprint), so
    use the appropriate (const) accessors instead.
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I678095d4d9cb8a1b44a7fad7c7e7cf25c4557225
    Reviewed-on: https://chromium-review.googlesource.com/883345
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#531529}
    Former-commit-id: e8e7368f7afabdb5cb0d41e9fe83d4926b76cd08

--- third_party/WebKit/Source/core/paint/BackgroundImageGeometry.cpp
+++ third_party/WebKit/Source/core/paint/BackgroundImageGeometry.cpp
@@ -61,7 +61,7 @@ LayoutSize CalculateFillTileSize(const LayoutBoxModelObject& obj,
                                  const FillLayer& fill_layer,
                                  const LayoutSize& positioning_area_size) {
   StyleImage* image = fill_layer.GetImage();
-  EFillSizeType type = fill_layer.Size().type;
+  EFillSizeType type = fill_layer.SizeType();
 
   LayoutSize image_intrinsic_size(image->ImageSize(
       obj.GetDocument(), obj.Style()->EffectiveZoom(), positioning_area_size));
@@ -69,8 +69,8 @@ LayoutSize CalculateFillTileSize(const LayoutBoxModelObject& obj,
     case EFillSizeType::kSizeLength: {
       LayoutSize tile_size(positioning_area_size);
 
-      Length layer_width = fill_layer.Size().size.Width();
-      Length layer_height = fill_layer.Size().size.Height();
+      const Length& layer_width = fill_layer.SizeLength().Width();
+      const Length& layer_height = fill_layer.SizeLength().Height();
 
       if (layer_width.IsFixed())
         tile_size.SetWidth(LayoutUnit(layer_width.Value()));
@@ -633,7 +633,7 @@ void BackgroundImageGeometry::Calculate(const LayoutBoxModelObject* container,
     LayoutUnit rounded_width = positioning_area_size.Width() / nr_tiles;
 
     // Maintain aspect ratio if background-size: auto is set
-    if (fill_layer.Size().size.Height().IsAuto() &&
+    if (fill_layer.SizeLength().Height().IsAuto() &&
         background_repeat_y != EFillRepeat::kRoundFill) {
       fill_tile_size.SetHeight(fill_tile_size.Height() * rounded_width /
                                fill_tile_size.Width());
@@ -661,7 +661,7 @@ void BackgroundImageGeometry::Calculate(const LayoutBoxModelObject* container,
                                           fill_tile_size.Height()));
     LayoutUnit rounded_height = positioning_area_size.Height() / nr_tiles;
     // Maintain aspect ratio if background-size: auto is set
-    if (fill_layer.Size().size.Width().IsAuto() &&
+    if (fill_layer.SizeLength().Width().IsAuto() &&
         background_repeat_x != EFillRepeat::kRoundFill) {
       fill_tile_size.SetWidth(fill_tile_size.Width() * rounded_height /
                               fill_tile_size.Height());
