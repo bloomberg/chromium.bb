3a33ae7661bbebc9e3b74164fe7ab99eb75d163e
Egor Pasko <pasko@chromium.org>
2018-01-24 17:52:02 +0000

    Revert "Supports Presentation in GLSurfaceEGL"
    This reverts commit ce9edfa995b18523abc7b6f2a66611da0308a096 [formerly 2adea4d74c2e7cee0228ac74b313ed8b2c6ad61b].
    
    Reason for revert: multiple failures on devices running Android L/M: http://crbug.com/805437
    
    Observed locally on Nexus5 LMY48H that reverting this change makes the crash go away for test org.chromium.chrome.browser.sync.OpenTabsTest#testIncognitoTabGetsInvalidSyncId
    
    Original change's description:
    > Supports Presentation in GLSurfaceEGL
    > 
    > Use the GLSurfacePresenterHelper to support presentation
    > in GLSurfaceEGL.
    > 
    > Bug: 802379
    > Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    > Change-Id: I296d4bcc0d68256d0173d976a2750999f21ceebd
    > Reviewed-on: https://chromium-review.googlesource.com/870851
    > Commit-Queue: Peng Huang <penghuang@chromium.org>
    > Reviewed-by: Antoine Labour <piman@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#531291}
    
    TBR=penghuang@chromium.org,piman@chromium.org
    
    Change-Id: I1119fcc5e25f108c2ee53a127c6431f44b31ecaf
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 802379
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Reviewed-on: https://chromium-review.googlesource.com/883882
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531597}
    Former-commit-id: bdba2dd2bd4120529c5285d760baff83db9985dd

--- ui/gl/gl_surface_presentation_helper.h
+++ ui/gl/gl_surface_presentation_helper.h
@@ -9,7 +9,6 @@
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
-#include "ui/gfx/swap_result.h"
 #include "ui/gl/gl_export.h"
 #include "ui/gl/gl_surface.h"
 
@@ -36,7 +35,7 @@ class GL_EXPORT GLSurfacePresentationHelper {
 
   void OnMakeCurrent(GLContext* context, GLSurface* surface);
   void PreSwapBuffers(const GLSurface::PresentationCallback& callback);
-  void PostSwapBuffers(gfx::SwapResult result);
+  void PostSwapBuffers();
 
  private:
   struct Frame {
