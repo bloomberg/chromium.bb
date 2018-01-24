26b859916b4fcdd40a53ce1d8ba0f6ba5792860f
Mostyn Bramley-Moore <mostynb@vewd.com>
2018-01-24 18:21:41 +0000

    [jumbo] move include guards in //cc closer to the top of the file
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I53311b06ea75519ea841d2ba2eca202bf3d8a26b
    Reviewed-on: https://chromium-review.googlesource.com/882361
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Commit-Queue: Mostyn Bramley-Moore <mostynb@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#531612}
    Former-commit-id: 39abf7a95f0654028a1b738f0d416d7460e28b09

--- cc/trees/latency_info_swap_promise_monitor.h
+++ cc/trees/latency_info_swap_promise_monitor.h
@@ -2,11 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/compiler_specific.h"
-#include "cc/trees/swap_promise_monitor.h"
-
 #ifndef CC_TREES_LATENCY_INFO_SWAP_PROMISE_MONITOR_H_
 #define CC_TREES_LATENCY_INFO_SWAP_PROMISE_MONITOR_H_
+
+#include "base/compiler_specific.h"
+#include "cc/trees/swap_promise_monitor.h"
 
 namespace ui {
 class LatencyInfo;
