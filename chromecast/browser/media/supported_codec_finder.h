85e9492c770f622785ebc19436eae32ac040bba6
Mostyn Bramley-Moore <mostynb@vewd.com>
2018-01-24 14:26:41 +0000

    [jumbo] add missing include guards in //chromecast
    Change-Id: I7ea4b5b53bf5be9bf3d4c0355003867c1712afe1
    Reviewed-on: https://chromium-review.googlesource.com/882324
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Mostyn Bramley-Moore <mostynb@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#531521}
    Former-commit-id: 94d9b0af55674980a5ba536c84706573f1554f42

--- chromecast/browser/media/supported_codec_finder.h
+++ chromecast/browser/media/supported_codec_finder.h
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifndef CHROMECAST_BROWSER_MEDIA_SUPPORTED_CODEC_FINDER_H_
+#define CHROMECAST_BROWSER_MEDIA_SUPPORTED_CODEC_FINDER_H_
+
 namespace chromecast {
 namespace media {
 
@@ -15,3 +18,5 @@ class SupportedCodecFinder {
 
 }  // namespace media
 }  // namespace chromecast
+
+#endif  // CHROMECAST_BROWSER_MEDIA_SUPPORTED_CODEC_FINDER_H_
