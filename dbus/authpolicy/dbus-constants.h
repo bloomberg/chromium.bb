b669996ff781f7efd728fccc2b55865bad6217cf
Lutz Justen <ljusten@chromium.org>
2018-01-24 16:10:26 +0100

    Remove deprecated AuthPolicy D-Bus method names
    Naming convention changed and Chrome switched over a while ago.
    
    CQ-DEPEND=CL:885816
    
    BUG=None
    TEST=trybots
    
    Change-Id: Iab3d616783fa23a145e4aa983efac6147afe6222
    Reviewed-on: https://chromium-review.googlesource.com/883464
    Commit-Ready: Lutz Justen <ljusten@chromium.org>
    Tested-by: Lutz Justen <ljusten@chromium.org>
    Reviewed-by: Lutz Justen <ljusten@chromium.org>
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>

--- dbus/authpolicy/dbus-constants.h
+++ dbus/authpolicy/dbus-constants.h
@@ -12,19 +12,6 @@ const char kAuthPolicyInterface[] = "org.chromium.AuthPolicy";
 const char kAuthPolicyServicePath[] = "/org/chromium/AuthPolicy";
 const char kAuthPolicyServiceName[] = "org.chromium.AuthPolicy";
 
-// TODO(ljusten): Remove old naming style once Chrome uses the new style.
-
-// Old naming style (please don't use).
-
-// Methods
-const char kAuthPolicyAuthenticateUser[] = "AuthenticateUser";
-const char kAuthPolicyGetUserStatus[] = "GetUserStatus";
-const char kAuthPolicyJoinADDomain[] = "JoinADDomain";
-const char kAuthPolicyRefreshUserPolicy[] = "RefreshUserPolicy";
-const char kAuthPolicyRefreshDevicePolicy[] = "RefreshDevicePolicy";
-
-// New naming style.
-
 // Methods
 const char kAuthenticateUserMethod[] = "AuthenticateUser";
 const char kGetUserStatusMethod[] = "GetUserStatus";
@@ -36,6 +23,6 @@ const char kRefreshDevicePolicyMethod[] = "RefreshDevicePolicy";
 // Signals
 const char kUserKerberosFilesChangedSignal[] = "UserKerberosFilesChanged";
 
-} // namespace authpolicy
+}  // namespace authpolicy
 
-#endif // SYSTEM_API_DBUS_AUTHPOLICY_DBUS_CONSTANTS_H_
+#endif  // SYSTEM_API_DBUS_AUTHPOLICY_DBUS_CONSTANTS_H_
