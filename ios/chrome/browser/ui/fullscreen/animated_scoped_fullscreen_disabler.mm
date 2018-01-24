ca2c46f7286bdba3a974ceafbb2d11e2a31a19e4
sczs <sczs@chromium.org>
2018-01-24 16:30:39 +0000

    [ios] Adds AnimatedScoperFullscreenDisabler to Build GN
    Change-Id: I5000696cb68897869d069c2522fb846b25c1b2ba
    Reviewed-on: https://chromium-review.googlesource.com/882214
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531551}
    Former-commit-id: 5c2111bc65ea95761479e1ecc3d4bda74c404219

--- ios/chrome/browser/ui/fullscreen/animated_scoped_fullscreen_disabler.mm
+++ ios/chrome/browser/ui/fullscreen/animated_scoped_fullscreen_disabler.mm
@@ -4,6 +4,7 @@
 
 #import "ios/chrome/browser/ui/fullscreen/animated_scoped_fullscreen_disabler.h"
 
+#include "base/logging.h"
 #import "ios/chrome/browser/ui/fullscreen/fullscreen_controller.h"
 #import "ios/chrome/common/material_timing.h"
 
