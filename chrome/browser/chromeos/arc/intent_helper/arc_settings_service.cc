fd5445f51393a08127dd270caebd7adb7db77b19
Sergey Poromov <poromov@chromium.org>
2018-01-24 17:12:10 +0000

    Always disable ARC reporting for PS.
    In Public Session users never see any consent that is requred
    to report data from Android side.
    Because of that "reporting consent" is always passed as "false"
    to Android side, irrelevant of StatsReporting pref.
    
    BUG=b/72088794
    TEST=Manual
    
    Change-Id: Iab2553b868e137a3f62cb9600d4ce9dc18afdec5
    Reviewed-on: https://chromium-review.googlesource.com/883807
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531573}
    Former-commit-id: cc3a3170f6f45c988efe3ecba349bcbcab361a36

--- chrome/browser/chromeos/arc/intent_helper/arc_settings_service.cc
+++ chrome/browser/chromeos/arc/intent_helper/arc_settings_service.cc
@@ -18,6 +18,7 @@
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chrome/browser/chromeos/system/timezone_resolver_manager.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profiles_state.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
 #include "chromeos/chromeos_switches.h"
@@ -561,7 +562,10 @@ void ArcSettingsServiceImpl::SyncProxySettings() const {
 
 void ArcSettingsServiceImpl::SyncReportingConsent() const {
   bool consent = false;
-  CrosSettings::Get()->GetBoolean(chromeos::kStatsReportingPref, &consent);
+  // Public session user never saw the consent even if the admin forced
+  // the pref by a policy.
+  if (!profiles::IsPublicSession())
+    CrosSettings::Get()->GetBoolean(chromeos::kStatsReportingPref, &consent);
   base::DictionaryValue extras;
   extras.SetBoolean("reportingConsent", consent);
   SendSettingsBroadcast("org.chromium.arc.intent_helper.SET_REPORTING_CONSENT",
