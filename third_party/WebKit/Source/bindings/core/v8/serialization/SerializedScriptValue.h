e8bb3b74e4fe73ae12890cddc68efe8dd8a4c205
Malcolm White <malcolmwhite@google.com>
2018-01-24 16:46:20 +0000

    Pass SharedArrayBuffers over MessagePorts
    This is the blink side of changes, meant to follow the v8 changes at https://chromium-review.googlesource.com/c/v8/v8/+/854395
    
    This leverages the lazy serialization put in place in https://chromium-review.googlesource.com/840606.
    Before the above change, a SerializedScriptValue containing SharedArrayBuffers would be eagerly serialized by mojo. When serialized, the SSV would drop its SharedArrayBuffers.
    After the above change, MessagePort serializes SSV's lazily. This means that mojo keeps the original SSV instance alive unless the message crosses a process boundary. With this, SharedArrayBuffers are retained by their SSV for in-process MessagePort postMessage's.
    Because lazy serialization takes care of passing SharedArrayBuffers over the wire, this change only needs to set up the communication between the SSV and v8 for serialization.
    
    BUG=chromium:716320
    
    Change-Id: If8337f5a0a46aa757a6065cf0a32921df8baad70
    Reviewed-on: https://chromium-review.googlesource.com/809228
    Commit-Queue: Malcolm White <malcolmwhite@google.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Ben Smith <binji@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531560}
    Former-commit-id: 7bbf3496898be9fb8a345f1df70d4a1e92edb3d7

--- third_party/WebKit/Source/bindings/core/v8/serialization/SerializedScriptValue.h
+++ third_party/WebKit/Source/bindings/core/v8/serialization/SerializedScriptValue.h
@@ -56,14 +56,17 @@ class SharedBuffer;
 class StaticBitmapImage;
 class UnpackedSerializedScriptValue;
 class WebBlobInfo;
+class DOMSharedArrayBuffer;
 
 typedef HashMap<String, scoped_refptr<BlobDataHandle>> BlobDataHandleMap;
 typedef Vector<WebBlobInfo> WebBlobInfoArray;
+typedef HeapVector<Member<DOMSharedArrayBuffer>> SharedArrayBufferArray;
 
 class CORE_EXPORT SerializedScriptValue
     : public ThreadSafeRefCounted<SerializedScriptValue> {
  public:
   using ArrayBufferContentsArray = Vector<WTF::ArrayBufferContents, 1>;
+  using SharedArrayBufferContentsArray = Vector<WTF::ArrayBufferContents, 1>;
   using ImageBitmapContentsArray = Vector<scoped_refptr<StaticBitmapImage>, 1>;
   using TransferredWasmModulesArray =
       WTF::Vector<v8::WasmCompiledModule::TransferrableModule>;
@@ -224,6 +227,9 @@ class CORE_EXPORT SerializedScriptValue
   size_t DataLengthInBytes() const { return data_buffer_size_; }
 
   TransferredWasmModulesArray& WasmModules() { return wasm_modules_; }
+  SharedArrayBufferContentsArray& SharedArrayBuffersContents() {
+    return shared_array_buffers_contents_;
+  }
   BlobDataHandleMap& BlobDataHandles() { return blob_data_handles_; }
 
  private:
@@ -255,7 +261,7 @@ class CORE_EXPORT SerializedScriptValue
   void TransferOffscreenCanvas(v8::Isolate*,
                                const OffscreenCanvasArray&,
                                ExceptionState&);
-
+  void CloneSharedArrayBuffers(SharedArrayBufferArray&);
   DataBufferPtr data_buffer_;
   size_t data_buffer_size_ = 0;
 
@@ -267,6 +273,7 @@ class CORE_EXPORT SerializedScriptValue
   // These do not have one-use transferred contents, like the above.
   TransferredWasmModulesArray wasm_modules_;
   BlobDataHandleMap blob_data_handles_;
+  SharedArrayBufferContentsArray shared_array_buffers_contents_;
 
   bool has_registered_external_allocation_;
   bool transferables_need_external_allocation_registration_;
