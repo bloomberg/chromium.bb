7cb8ed8ea50b5b199cafd71bf5abe9cd17aaf9b1
Andrey Lushnikov <lushnikov@chromium.org>
2018-01-24 03:43:50 +0000

    DevTools: fix node.js + Workspaces + Windows
    Node.js reports scripts with file paths instead of urls. Since all of
    the devtools front-end code operates with URLs, we transform file paths
    to urls in the DebuggerModel.
    
    This patch fixes filePath->URL translation so that it works for Windows
    paths.
    
    R=dgozman, kozy
    BUG=679144
    
    Change-Id: Idf9320bd62c679277bb0689e0031ab948dbe0b72
    Reviewed-on: https://chromium-review.googlesource.com/828466
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531412}
    Former-commit-id: 2e6a86dc0fe38ca58339cd3570df3292d7497a8d

--- third_party/WebKit/LayoutTests/http/tests/devtools/sources/debugger-breakpoints/nodejs-set-breakpoint.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/sources/debugger-breakpoints/nodejs-set-breakpoint.js
@@ -5,15 +5,28 @@
 (async function() {
   TestRunner.addResult(`Verify that front-end is able to set breakpoint for node.js scripts.\n`);
   await TestRunner.loadModule('sources_test_runner');
+  await TestRunner.loadModule('sdk_test_runner');
   await TestRunner.showPanel('sources');
 
   SDK.targetManager.mainTarget().setIsNodeJSForTest();
   SourcesTestRunner.startDebuggerTest();
 
+  var debuggerModel = SDK.targetManager.mainTarget().model(SDK.DebuggerModel);
+  var originalParsedScriptSource = TestRunner.override(debuggerModel, '_parsedScriptSource', overrideSourceURL, true);
+
+  function overrideSourceURL(scriptId, sourceURL, startLine, startColumn, endLine, endColumn, executionContextId, hash,
+               executionContextAuxData, isLiveEdit, sourceMapURL, hasSourceURL, hasSyntaxError, length) {
+    // Overwrite hasSourceURL to pretend we're Node.js
+    hasSourceURL = false;
+    originalParsedScriptSource.call(this, scriptId, sourceURL, startLine, startColumn, endLine, endColumn, executionContextId, hash,
+               executionContextAuxData, isLiveEdit, sourceMapURL, hasSourceURL, hasSyntaxError, length);
+  }
+
   var functionText = 'function foobar() { \nconsole.log(\'foobar execute!\');\n}';
   var sourceURL = '\n//# sourceURL=/usr/local/home/prog/foobar.js';
   await TestRunner.evaluateInPageAnonymously(functionText + sourceURL);
   SourcesTestRunner.showScriptSource('foobar.js', didShowScriptSource);
+
 
   function didShowScriptSource(sourceFrame) {
     TestRunner.addResult('Setting breakpoint:');
