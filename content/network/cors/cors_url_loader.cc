31ea00abba8d429d727be75c9a9e876f47173ae3
Takashi Toyoshima <toyoshim@chromium.org>
2018-01-24 06:08:41 +0000

    OOR-CORS: Move CORSURLLoader from content/renderer to content/network
    This patch moves content::CORSURLLoader from content/renderer to
    the browser process, and hooks up URLLoaderFactoryImpl to use the
    CORSURLLoader when kOutOfBlinkCORS feature is enabled.
    
    With this patch, "chrome --enable-features=OutOfBlinkCORS" will handle
    basic CORS checks in the browser process.
    
    Test: ./Tools/Scripts/run-webkit-tests virtual/outofblink-cors
    Test: ./out/Debug/chrome --enable-features=OutOfBlinkCORS
    Test: ./out/Debug/content_unittests --gtest_filter="CORS*.*"
    Bug: 736308
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: If646da1f022ceb040a6069ef8942c3909b7f01a1
    Reviewed-on: https://chromium-review.googlesource.com/866657
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Takeshi Yoshino <tyoshino@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531454}
    Former-commit-id: 42926ddd2790cb10623b7a990f21736b898ac878

--- content/network/cors/cors_url_loader.cc
+++ content/network/cors/cors_url_loader.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/renderer/loader/cors_url_loader.h"
+#include "content/network/cors/cors_url_loader.h"
 
 #include "content/public/common/origin_util.h"
 #include "content/public/common/resource_type.h"
---
+++ content/network/cors/cors_url_loader_factory.cc
@@ -0,0 +1,46 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/network/cors/cors_url_loader_factory.h"
+
+#include "content/network/cors/cors_url_loader.h"
+#include "content/public/common/content_features.h"
+
+namespace content {
+
+CORSURLLoaderFactory::CORSURLLoaderFactory(
+    std::unique_ptr<network::mojom::URLLoaderFactory> network_loader_factory)
+    : network_loader_factory_(std::move(network_loader_factory)) {}
+
+CORSURLLoaderFactory::~CORSURLLoaderFactory() = default;
+
+void CORSURLLoaderFactory::CreateLoaderAndStart(
+    network::mojom::URLLoaderRequest request,
+    int32_t routing_id,
+    int32_t request_id,
+    uint32_t options,
+    const network::ResourceRequest& resource_request,
+    network::mojom::URLLoaderClientPtr client,
+    const net::MutableNetworkTrafficAnnotationTag& traffic_annotation) {
+  if (base::FeatureList::IsEnabled(features::kOutOfBlinkCORS)) {
+    loader_bindings_.AddBinding(
+        std::make_unique<CORSURLLoader>(routing_id, request_id, options,
+                                        resource_request, std::move(client),
+                                        traffic_annotation,
+                                        network_loader_factory_.get()),
+        std::move(request));
+  } else {
+    network_loader_factory_->CreateLoaderAndStart(
+        std::move(request), routing_id, request_id, options, resource_request,
+        std::move(client), traffic_annotation);
+  }
+}
+
+void CORSURLLoaderFactory::Clone(
+    network::mojom::URLLoaderFactoryRequest request) {
+  // The cloned factories stop working when this factory is destructed.
+  bindings_.AddBinding(this, std::move(request));
+}
+
+}  // namespace content
