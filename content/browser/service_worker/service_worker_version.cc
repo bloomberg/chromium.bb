ca6e5ca4e7f56b13aee0648b1407507c7a2263bd
Han Leon <leon.han@intel.com>
2018-01-25 05:24:43 +0000

    [ServiceWorker] Send clients back for ServiceWorkerHost.GetClients() anyway
    Before, we avoid sending clients back to those STOPPED/STOPPING workers
    via the legacy IPC ServiceWorkerMsg_DidGetClients, maybe this is in
    order to avoid stray IPC in the workers side.
    
    After mojofication of GetClients(), we do not need to consider stray
    message anymore, because we start to send clients back by running
    response callback of the Mojo interface method GetClients(), which is
    guaranteed to be safe no matter the other side endpoint has destroyed or
    not.
    
    BUG=772793
    
    Change-Id: I2ae3e4ccf76bc4dc12d6312c694ab90cd7b9fcd7
    Reviewed-on: https://chromium-review.googlesource.com/882348
    Commit-Queue: Han Leon <leon.han@intel.com>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531816}
    Former-commit-id: cff529b2d794c872d07f4398e8b8bb129c871f1f

--- content/browser/service_worker/service_worker_version.cc
+++ content/browser/service_worker/service_worker_version.cc
@@ -250,6 +250,14 @@ void ShowPaymentHandlerWindowOnUI(
   }
 }
 
+void DidGetClients(
+    blink::mojom::ServiceWorkerHost::GetClientsCallback callback,
+    std::unique_ptr<std::vector<blink::mojom::ServiceWorkerClientInfoPtr>>
+        clients) {
+  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  std::move(callback).Run(std::move(*clients));
+}
+
 }  // namespace
 
 constexpr base::TimeDelta ServiceWorkerVersion::kTimeoutTimerDelay;
@@ -1125,26 +1133,8 @@ void ServiceWorkerVersion::GetClients(
     GetClientsCallback callback) {
   service_worker_client_utils::GetClients(
       weak_factory_.GetWeakPtr(), std::move(options),
-      base::Bind(&ServiceWorkerVersion::OnGetClientsFinished,
-                 weak_factory_.GetWeakPtr(),
-                 base::Passed(std::move(callback))));
-}
-
-void ServiceWorkerVersion::OnGetClientsFinished(
-    GetClientsCallback callback,
-    std::unique_ptr<ServiceWorkerClientPtrs> clients) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
-
-  // When Clients.matchAll() is called on the script evaluation phase, the
-  // running status can be STARTING here.
-  if (running_status() != EmbeddedWorkerStatus::STARTING &&
-      running_status() != EmbeddedWorkerStatus::RUNNING) {
-    std::move(callback).Run(
-        std::vector<blink::mojom::ServiceWorkerClientInfoPtr>());
-    return;
-  }
-
-  std::move(callback).Run(std::move(*clients));
+      base::AdaptCallbackForRepeating(
+          base::BindOnce(&DidGetClients, std::move(callback))));
 }
 
 void ServiceWorkerVersion::OnSetCachedMetadataFinished(int64_t callback_id,
