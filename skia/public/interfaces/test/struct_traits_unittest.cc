ed75b0523f9450cca8e2e17775337407cfbcc2d2
Khushal <khushalsagar@chromium.org>
2018-01-25 02:30:31 +0000

    skia: Remove unused image filter mojom type.
    TBR=reed@google.com
    
    Bug: 800612
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I1507687375387030a354a964c1d4b6ba5415ddd0
    Reviewed-on: https://chromium-review.googlesource.com/876949
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Mike Reed <reed@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#531795}
    Former-commit-id: 927ee09a7a14cbafe10bdfedb4989517b3b0d6ee

--- skia/public/interfaces/test/struct_traits_unittest.cc
+++ skia/public/interfaces/test/struct_traits_unittest.cc
@@ -41,26 +41,11 @@ class StructTraitsTest : public testing::Test, public mojom::TraitsTestService {
     std::move(callback).Run(t);
   }
 
-  void EchoImageFilter(const sk_sp<SkImageFilter>& i,
-                       EchoImageFilterCallback callback) override {
-    std::move(callback).Run(i);
-  }
-
   base::MessageLoop loop_;
   mojo::BindingSet<TraitsTestService> traits_test_bindings_;
 
   DISALLOW_COPY_AND_ASSIGN(StructTraitsTest);
 };
-
-static sk_sp<SkImageFilter> make_scale(float amount,
-                                       sk_sp<SkImageFilter> input) {
-  SkScalar s = amount;
-  SkScalar matrix[20] = {s, 0, 0, 0, 0, 0, s, 0, 0, 0,
-                         0, 0, s, 0, 0, 0, 0, 0, s, 0};
-  sk_sp<SkColorFilter> filter(
-      SkColorFilter::MakeMatrixFilterRowMajor255(matrix));
-  return SkColorFilterImageFilter::Make(std::move(filter), std::move(input));
-}
 
 static bool colorspace_srgb_gamma(SkColorSpace* cs) {
   return cs && cs->gammaCloseToSRGB();
@@ -107,33 +92,6 @@ TEST_F(StructTraitsTest, BitmapWithExtraRowBytes) {
   EXPECT_EQ(input.height(), output.height());
   EXPECT_EQ(input.rowBytes(), output.rowBytes());
   EXPECT_TRUE(gfx::BitmapsAreEqual(input, output));
-}
-
-TEST_F(StructTraitsTest, ImageFilter) {
-  sk_sp<SkImageFilter> input(make_scale(0.5f, nullptr));
-  SkString input_str;
-  input->toString(&input_str);
-  mojom::TraitsTestServicePtr proxy = GetTraitsTestProxy();
-  sk_sp<SkImageFilter> output;
-  proxy->EchoImageFilter(input, &output);
-  SkString output_str;
-  output->toString(&output_str);
-  EXPECT_EQ(input_str, output_str);
-}
-
-TEST_F(StructTraitsTest, DropShadowImageFilter) {
-  sk_sp<SkImageFilter> input(SkDropShadowImageFilter::Make(
-      SkIntToScalar(3), SkIntToScalar(8), SkIntToScalar(4), SkIntToScalar(9),
-      SK_ColorBLACK,
-      SkDropShadowImageFilter::kDrawShadowAndForeground_ShadowMode, nullptr));
-  SkString input_str;
-  input->toString(&input_str);
-  mojom::TraitsTestServicePtr proxy = GetTraitsTestProxy();
-  sk_sp<SkImageFilter> output;
-  proxy->EchoImageFilter(input, &output);
-  SkString output_str;
-  output->toString(&output_str);
-  EXPECT_EQ(input_str, output_str);
 }
 
 TEST_F(StructTraitsTest, BlurImageFilterTileMode) {
