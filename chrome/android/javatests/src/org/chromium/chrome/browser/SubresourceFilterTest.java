d0ec5bac9b185d09e9741b1f71f4ae1fb26a3ef0
Charles Harrison <csharrison@chromium.org>
2018-01-26 17:59:37 +0000

    [subresource_filter] Add more instrumentation tests for the infobar
    This CL adds tests for:
    1. Infobar existence
    2. Clicking "learn more" on the infobar
    3. Clicking the checkbox and reloading the site
    4. Closing the infobar
    
    Bug: 800176
    Change-Id: I4cf28352c182320bcec5c9e6e8e53cd2c30d02d2
    Reviewed-on: https://chromium-review.googlesource.com/886743
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532001}
    Former-commit-id: c5bf341b89fe3181aa68766d22f28b715d8aabf8

--- chrome/android/javatests/src/org/chromium/chrome/browser/SubresourceFilterTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/SubresourceFilterTest.java
@@ -15,14 +15,23 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.chrome.browser.infobar.AdsBlockedInfoBar;
+import org.chromium.chrome.browser.infobar.InfoBar;
 import org.chromium.chrome.browser.subresource_filter.TestSubresourceFilterPublisher;
+import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tabmodel.EmptyTabModelObserver;
+import org.chromium.chrome.browser.tabmodel.TabModel;
 import org.chromium.chrome.test.ChromeActivityTestRule;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
+import org.chromium.chrome.test.util.ChromeTabUtils;
 import org.chromium.components.safe_browsing.SafeBrowsingApiBridge;
 import org.chromium.content.browser.test.util.Criteria;
 import org.chromium.content.browser.test.util.CriteriaHelper;
 import org.chromium.net.test.EmbeddedTestServer;
+
+import java.util.List;
 
 /**
  * End to end tests of SubresourceFilter ad filtering on Android.
@@ -41,6 +50,8 @@ public final class SubresourceFilterTest {
 
     private static final String PAGE_WITH_JPG =
             "/chrome/test/data/android/subresource_filter/page-with-img.html";
+    private static final String LEARN_MORE_PAGE =
+            "https://support.google.com/chrome/?p=blocked_ads";
     private static final String METADATA_FOR_ENFORCEMENT =
             "{\"matches\":[{\"threat_type\":\"13\",\"sf_bas\":\"\"}]}";
     private static final String METADATA_FOR_WARNING =
@@ -87,17 +98,112 @@ public final class SubresourceFilterTest {
 
         String loaded = mActivityTestRule.runJavaScriptCodeInCurrentTab("imgLoaded");
         Assert.assertEquals("true", loaded);
+
+        // Check that the infobar is not showing.
+        List<InfoBar> infoBars = mActivityTestRule.getInfoBars();
+        CriteriaHelper.pollUiThread(() -> infoBars.isEmpty());
     }
 
     @Test
     @MediumTest
-    public void resourceFiltered() throws Exception {
+    public void resourceFilteredClose() throws Exception {
         String url = mTestServer.getURL(PAGE_WITH_JPG);
         MockSafeBrowsingApiHandler.addMockResponse(url, METADATA_FOR_ENFORCEMENT);
         mActivityTestRule.loadUrl(url);
 
         String loaded = mActivityTestRule.runJavaScriptCodeInCurrentTab("imgLoaded");
         Assert.assertEquals("false", loaded);
+
+        // Check that the infobar is showing.
+        List<InfoBar> infoBars = mActivityTestRule.getInfoBars();
+        CriteriaHelper.pollUiThread(() -> infoBars.size() == 1);
+        AdsBlockedInfoBar infobar = (AdsBlockedInfoBar) infoBars.get(0);
+
+        // Click the link once to expand it.
+        ThreadUtils.runOnUiThreadBlocking(infobar::onLinkClicked);
+
+        // Check the checkbox and press the button to reload.
+        ThreadUtils.runOnUiThreadBlocking(() -> infobar.onCheckedChanged(null, true));
+
+        // Think better of it and just close the infobar.
+        ThreadUtils.runOnUiThreadBlocking(infobar::onCloseButtonClicked);
+        Tab tab = mActivityTestRule.getActivity().getActivityTab();
+        CriteriaHelper.pollUiThread(
+                () -> tab.getInfoBarContainer().getInfoBarsForTesting().isEmpty());
+    }
+
+    @Test
+    @MediumTest
+    public void resourceFilteredClickLearnMore() throws Exception {
+        String url = mTestServer.getURL(PAGE_WITH_JPG);
+        MockSafeBrowsingApiHandler.addMockResponse(url, METADATA_FOR_ENFORCEMENT);
+        mActivityTestRule.loadUrl(url);
+
+        String loaded = mActivityTestRule.runJavaScriptCodeInCurrentTab("imgLoaded");
+        Assert.assertEquals("false", loaded);
+
+        Tab originalTab = mActivityTestRule.getActivity().getActivityTab();
+        CallbackHelper tabCreatedCallback = new CallbackHelper();
+        TabModel tabModel = mActivityTestRule.getActivity().getTabModelSelector().getCurrentModel();
+        ThreadUtils.runOnUiThreadBlocking(() -> tabModel.addObserver(new EmptyTabModelObserver() {
+            @Override
+            public void didAddTab(Tab tab, TabModel.TabLaunchType type) {
+                if (tab.getUrl().equals(LEARN_MORE_PAGE)) tabCreatedCallback.notifyCalled();
+            }
+        }));
+
+        // Check that the infobar is showing.
+        List<InfoBar> infoBars = mActivityTestRule.getInfoBars();
+        CriteriaHelper.pollUiThread(() -> infoBars.size() == 1);
+        AdsBlockedInfoBar infobar = (AdsBlockedInfoBar) infoBars.get(0);
+
+        // Click the link once to expand it.
+        ThreadUtils.runOnUiThreadBlocking(infobar::onLinkClicked);
+
+        // Click again to navigate, which should spawn a new tab.
+        ThreadUtils.runOnUiThreadBlocking(infobar::onLinkClicked);
+
+        // Wait for the tab to be added with the correct URL. Note, do not wait for this URL to be
+        // loaded since it is not controlled by the test instrumentation. Just waiting for the
+        // navigation to start should be OK though.
+        tabCreatedCallback.waitForCallback("Never received tab created event", 0);
+
+        // The infobar should not be removed on the original tab.
+        CriteriaHelper.pollUiThread(
+                () -> !originalTab.getInfoBarContainer().getInfoBarsForTesting().isEmpty());
+    }
+
+    @Test
+    @MediumTest
+    public void resourceFilteredReload() throws Exception {
+        String url = mTestServer.getURL(PAGE_WITH_JPG);
+        MockSafeBrowsingApiHandler.addMockResponse(url, METADATA_FOR_ENFORCEMENT);
+        mActivityTestRule.loadUrl(url);
+
+        String loaded = mActivityTestRule.runJavaScriptCodeInCurrentTab("imgLoaded");
+        Assert.assertEquals("false", loaded);
+
+        // Check that the infobar is showing.
+        List<InfoBar> infoBars = mActivityTestRule.getInfoBars();
+        CriteriaHelper.pollUiThread(() -> infoBars.size() == 1);
+        AdsBlockedInfoBar infobar = (AdsBlockedInfoBar) infoBars.get(0);
+
+        // Click the link once to expand it.
+        ThreadUtils.runOnUiThreadBlocking(infobar::onLinkClicked);
+
+        // Check the checkbox and press the button to reload.
+        ThreadUtils.runOnUiThreadBlocking(() -> infobar.onCheckedChanged(null, true));
+        ThreadUtils.runOnUiThreadBlocking(() -> infobar.onButtonClicked(true));
+
+        Tab tab = mActivityTestRule.getActivity().getActivityTab();
+        ChromeTabUtils.waitForTabPageLoaded(tab, url);
+
+        CriteriaHelper.pollUiThread(
+                () -> tab.getInfoBarContainer().getInfoBarsForTesting().isEmpty());
+
+        // Reloading should whitelist the site, so resources should no longer be filtered.
+        loaded = mActivityTestRule.runJavaScriptCodeInCurrentTab("imgLoaded");
+        Assert.assertEquals("true", loaded);
     }
 
     @Test
@@ -109,5 +215,9 @@ public final class SubresourceFilterTest {
 
         String loaded = mActivityTestRule.runJavaScriptCodeInCurrentTab("imgLoaded");
         Assert.assertEquals("true", loaded);
+
+        // Check that the infobar is not showing.
+        List<InfoBar> infoBars = mActivityTestRule.getInfoBars();
+        CriteriaHelper.pollUiThread(() -> infoBars.isEmpty());
     }
 }
