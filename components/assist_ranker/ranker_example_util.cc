e3a5f7c56e79fb6e0c144194197c1baa70609f6f
Tom Anderson <thomasanderson@chromium.org>
2018-01-27 20:03:37 +0000

    Fix build with glibc 2.27
    BUG=806340
    TBR=hamelphi@chromium.org
    
    Change-Id: Ib4e5091212d874d9ad88f3e9a1fdfee3ed7e0d5e
    Reviewed-on: https://chromium-review.googlesource.com/890059
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Philippe Hamel <hamelphi@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532249}
    Former-commit-id: 9f63f94a11abc34d40ede8b8712fa15b5844a8c0

--- components/assist_ranker/ranker_example_util.cc
+++ components/assist_ranker/ranker_example_util.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <math.h>
+
 #include "components/assist_ranker/ranker_example_util.h"
 #include "base/bit_cast.h"
 #include "base/format_macros.h"
