fccf40dd79cfa6febbfe54fae5f79fd36249f369
dpapad <dpapad@chromium.org>
2018-01-27 01:57:18 +0000

    WebUI: Update Polymer paper-checkbox version 1.4.0 -> 2.0.2
    Bug: 738611
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Iad8fde7df949176ce836671916de50db6fa7099e
    Reviewed-on: https://chromium-review.googlesource.com/882465
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532123}
    Former-commit-id: 4bc89a5bc6b03143dec4d1db240f42b740149ab6

--- third_party/polymer/v1_0/components-chromium/paper-checkbox/paper-checkbox-extracted.js
+++ third_party/polymer/v1_0/components-chromium/paper-checkbox/paper-checkbox-extracted.js
@@ -30,21 +30,33 @@ Polymer({
       },
 
       attached: function() {
-        var inkSize = this.getComputedStyleValue('--calculated-paper-checkbox-ink-size');
-        // If unset, compute and set the default `--paper-checkbox-ink-size`.
-        if (inkSize === '-1px') {
-          var checkboxSize = parseFloat(this.getComputedStyleValue('--calculated-paper-checkbox-size'));
-          var defaultInkSize = Math.floor((8 / 3) * checkboxSize);
+        // Wait until styles have resolved to check for the default sentinel.
+        // See polymer#4009 for more details.
+        Polymer.RenderStatus.afterNextRender(this, function() {
+          var inkSize = this.getComputedStyleValue('--calculated-paper-checkbox-ink-size').trim();
+          // If unset, compute and set the default `--paper-checkbox-ink-size`.
+          if (inkSize === '-1px') {
+            var checkboxSizeText = this.getComputedStyleValue('--calculated-paper-checkbox-size').trim();
 
-          // The checkbox and ripple need to have the same parity so that their
-          // centers align.
-          if (defaultInkSize % 2 !== checkboxSize % 2) {
-            defaultInkSize++;
+            var units = checkboxSizeText.match(/[A-Za-z]+$/)[0] || 'px';
+            var checkboxSize = parseFloat(checkboxSizeText);
+            var defaultInkSize = (8 / 3) * checkboxSize;
+
+            if (units === 'px') {
+              defaultInkSize = Math.floor(defaultInkSize);
+
+              // The checkbox and ripple need to have the same parity so that their
+              // centers align.
+              if (defaultInkSize % 2 !== checkboxSize % 2) {
+                defaultInkSize++;
+              }
+            }
+
+            this.updateStyles({
+              '--paper-checkbox-ink-size': defaultInkSize + units,
+            });
           }
-
-          this.customStyle['--paper-checkbox-ink-size'] = defaultInkSize + 'px';
-          this.updateStyles();
-        }
+        });
       },
 
       _computeCheckboxClass: function(checked, invalid) {
