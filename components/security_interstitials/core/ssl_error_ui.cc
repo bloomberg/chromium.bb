3eea706e49bf385dbc42f09c0ede96d53664944b
Nico Weber <thakis@chromium.org>
2018-01-29 21:57:31 +0000

    Remaining changes to build all code in src.git with -Wimplicit-fallthrough.
    This CL was uploaded by git cl split.
    
    R=estark@chromium.org
    
    Bug: 177475
    Change-Id: Ie191053771d653739fa5bfb5a2a8f143306d2c47
    Reviewed-on: https://chromium-review.googlesource.com/891743
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532610}
    Former-commit-id: 93951ef292af7fe4def6b9413fc57e271cc1c7d7

--- components/security_interstitials/core/ssl_error_ui.cc
+++ components/security_interstitials/core/ssl_error_ui.cc
@@ -216,6 +216,7 @@ void SSLErrorUI::HandleCommand(SecurityInterstitialCommand command) {
     case CMD_REPORT_PHISHING_ERROR:
       // Not supported by the SSL error page.
       NOTREACHED() << "Unsupported command: " << command;
+      break;
     case CMD_ERROR:
     case CMD_TEXT_FOUND:
     case CMD_TEXT_NOT_FOUND:
