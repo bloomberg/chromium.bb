c53196eaa65ddc164b37976b82a3ade83b49697d
dpapad <dpapad@chromium.org>
2018-01-29 23:04:42 +0000

    WebUI: Update Polymer iron-location version 0.8.8 -> 2.0.3
    Bug: 738611
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Id6548d65c07bfa7217c16b6ce31c6ecb534622d5
    Reviewed-on: https://chromium-review.googlesource.com/890053
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532646}
    Former-commit-id: 81a7d54a90c9ed42a492c56b88202d550f3230e3

--- third_party/polymer/v1_0/components-chromium/iron-location/iron-query-params-extracted.js
+++ third_party/polymer/v1_0/components-chromium/iron-location/iron-query-params-extracted.js
@@ -2,12 +2,14 @@
 
   Polymer({
     is: 'iron-query-params',
+
     properties: {
       paramsString: {
         type: String,
         notify: true,
         observer: 'paramsStringChanged',
       },
+
       paramsObject: {
         type: Object,
         notify: true,
@@ -15,34 +17,44 @@
           return {};
         }
       },
+
       _dontReact: {
         type: Boolean,
         value: false
       }
     },
+
     hostAttributes: {
       hidden: true
     },
+
     observers: [
       'paramsObjectChanged(paramsObject.*)'
     ],
+
     paramsStringChanged: function() {
       this._dontReact = true;
       this.paramsObject = this._decodeParams(this.paramsString);
       this._dontReact = false;
     },
+
     paramsObjectChanged: function() {
       if (this._dontReact) {
         return;
       }
-      this.paramsString = this._encodeParams(this.paramsObject);
+      this.paramsString = this._encodeParams(this.paramsObject)
+          .replace(/%3F/g, '?').replace(/%2F/g, '/').replace(/'/g, '%27');
     },
+
     _encodeParams: function(params) {
       var encodedParams = [];
+
       for (var key in params) {
         var value = params[key];
+
         if (value === '') {
           encodedParams.push(encodeURIComponent(key));
+
         } else if (value) {
           encodedParams.push(
               encodeURIComponent(key) +
@@ -53,13 +65,12 @@
       }
       return encodedParams.join('&');
     },
+
     _decodeParams: function(paramString) {
       var params = {};
-
       // Work around a bug in decodeURIComponent where + is not
       // converted to spaces:
       paramString = (paramString || '').replace(/\+/g, '%20');
-
       var paramList = paramString.split('&');
       for (var i = 0; i < paramList.length; i++) {
         var param = paramList[i].split('=');
