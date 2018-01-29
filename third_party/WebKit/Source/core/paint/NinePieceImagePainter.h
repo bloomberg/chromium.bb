c8b3001f0363ea3efa7a1a8591dc1d86c0819bfd
Xianzhu Wang <wangxianzhu@chromium.org>
2018-01-29 18:16:29 +0000

    Remove DstIn optimization when painting mask for inline flow box
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I7cdba1e84b853eeaff281093555e8757dc1616ed
    Reviewed-on: https://chromium-review.googlesource.com/882396
    Reviewed-by: Tien-Ren Chen <trchen@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532493}
    Former-commit-id: 937677e60f4b9c525fc5b6d2e79ebb2ef86de213

--- third_party/WebKit/Source/core/paint/NinePieceImagePainter.h
+++ third_party/WebKit/Source/core/paint/NinePieceImagePainter.h
@@ -6,7 +6,6 @@
 #define NinePieceImagePainter_h
 
 #include "platform/heap/Heap.h"
-#include "third_party/skia/include/core/SkBlendMode.h"
 
 namespace blink {
 
@@ -28,8 +27,7 @@ class NinePieceImagePainter {
                     Node*,
                     const LayoutRect&,
                     const ComputedStyle&,
-                    const NinePieceImage&,
-                    SkBlendMode = SkBlendMode::kSrcOver);
+                    const NinePieceImage&);
 
  private:
   NinePieceImagePainter() = default;
