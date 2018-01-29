5e6e69d4524c1a14bb6e32e502150d926b0ffcc9
Peter Wen <wnwen@chromium.org>
2018-01-29 15:16:52 +0000

    Android: Remove unused strings
    Update suppressions from android_tools upgrades resulting in unused
    resources (mostly strings) being left behind after their feature is
    removed.
    
    Also update some suppressions as expected.
    
    BUG=799070,806060
    TBR=nyquist@chromium.org
    
    Change-Id: I30d35320ad20b318a921972529f139b3b3e65453
    Reviewed-on: https://chromium-review.googlesource.com/886863
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532433}
    Former-commit-id: 78988928ff8dd13b8fd16017c617ee5ad76c7cd8

--- chrome/test/android/javatests/src/org/chromium/chrome/test/util/TranslateUtil.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/util/TranslateUtil.java
@@ -4,12 +4,8 @@
 
 package org.chromium.chrome.test.util;
 
-import android.app.Activity;
 import android.app.Instrumentation;
-import android.text.SpannableString;
-import android.text.style.ClickableSpan;
 import android.view.View;
-import android.widget.TextView;
 
 import org.junit.Assert;
 
@@ -21,50 +17,11 @@ import org.chromium.chrome.browser.infobar.translate.TranslateMenu;
 import org.chromium.chrome.browser.infobar.translate.TranslateTabLayout;
 import org.chromium.content.browser.test.util.Criteria;
 import org.chromium.content.browser.test.util.CriteriaHelper;
-import org.chromium.content.browser.test.util.TestTouchUtils;
 
 /**
  * Utility functions for dealing with Translate InfoBars.
  */
 public class TranslateUtil {
-    /**
-     * Finds the first clickable span inside a TextView and clicks it.
-     */
-    public static void openLanguagePanel(
-            Instrumentation instrumentation, Activity activity, InfoBar infoBar) {
-        View view = infoBar.getView().findViewById(R.id.infobar_message);
-        Assert.assertNotNull(view);
-
-        TextView text = (TextView) view.findViewById(R.id.infobar_message);
-
-        SpannableString spannable = (SpannableString) text.getText();
-        ClickableSpan[] clickable =
-            spannable.getSpans(0, spannable.length() - 1, ClickableSpan.class);
-        Assert.assertTrue(clickable.length > 0);
-
-        // Find the approximate coordinates of the first link of the first line of text so we can
-        // click there. Clicking on any character of the link will work so instead of focusing on
-        // the beginning of the link we add one more character so that finding a valid coordinate
-        // is more reliable.
-        int x = spannable.getSpanStart(clickable[0]) + 1;
-        float nChars = text.getLayout().getLineVisibleEnd(0);
-
-        // Not all characters have the same width but this is a good approximation.
-        float sizePerChar =  text.getLayout().getLineWidth(0) / nChars;
-        float xPos = text.getPaddingLeft() + (sizePerChar * x);
-        float yPos = text.getHeight() / (float) 2;
-
-        TestTouchUtils.singleClickView(instrumentation, text, (int) xPos, (int) yPos);
-
-        assertInfoBarText(infoBar, activity.getString(R.string.translate_infobar_change_languages));
-    }
-
-    public static void assertInfoBarText(InfoBar infoBar, String expectedText) {
-        View view = infoBar.getView().findViewById(R.id.infobar_message);
-        Assert.assertNotNull(view);
-        String actualText = findInfoBarText(view);
-        Assert.assertEquals(expectedText, actualText);
-    }
 
     public static void assertCompactTranslateInfoBar(InfoBar infoBar) {
         Assert.assertTrue(infoBar.getView() instanceof InfoBarCompactLayout);
@@ -143,8 +100,4 @@ public class TranslateUtil {
         });
     }
 
-    private static String findInfoBarText(View view) {
-        TextView text = (TextView) view.findViewById(R.id.infobar_message);
-        return text != null ? text.getText().toString() : null;
-    }
 }
