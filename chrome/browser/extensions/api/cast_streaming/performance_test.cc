9d5e4691c6adb049ef2b057e62e2a446910a778c
Yuri Wiitala <miu@chromium.org>
2018-01-29 23:27:41 +0000

    Remove novsync variants of performance_browser_tests.
    Removes the "nosync" testing since this no longer applies to the new tab
    capture impl (because it is necessarily triggered by OnBeginFrame's).
    Also, added a call to BrowserTestBase::UseSoftwareCompositing() when the
    non-GPU variants are run to ensure the GPU compositor is not being used.
    
    TBR=xjz@chromium.org
    
    Bug: 806400, 567848
    Change-Id: I1d00a5f95ee6d3ac8caf0e5d1191c7bb03ca8064
    Reviewed-on: https://chromium-review.googlesource.com/892049
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532654}
    Former-commit-id: e179c6a28dbbb4c0e006c4726940ff4e98c2eedd

--- chrome/browser/extensions/api/cast_streaming/performance_test.cc
+++ chrome/browser/extensions/api/cast_streaming/performance_test.cc
@@ -68,13 +68,9 @@ constexpr size_t kTrimEvents = 24;  // 1 sec at 24fps, or 0.4 sec at 60 fps.
 constexpr size_t kMinDataPoints = 100;  // 1 sec of audio, or ~5 sec at 24fps.
 
 enum TestFlags {
-  // TODO(miu): Remove kUseGpu (since the GPU is required), and maybe
-  // kDisableVsync. http://crbug.com/567848
   kUseGpu = 1 << 0,           // Only execute test if --enable-gpu was given
                               // on the command line.  This is required for
                               // tests that run on GPU.
-  kDisableVsync = 1 << 1,     // Do not limit framerate to vertical refresh.
-                              // when on GPU, nor to 60hz when not on GPU.
   kSmallWindow = 1 << 2,      // Window size: 1 = 800x600, 0 = 2000x1000
   k24fps = 1 << 3,            // Use 24 fps video.
   k30fps = 1 << 4,            // Use 30 fps video.
@@ -368,8 +364,6 @@ class CastV2PerformanceTest
     std::string suffix;
     if (HasFlag(kUseGpu))
       suffix += "_gpu";
-    if (HasFlag(kDisableVsync))
-      suffix += "_novsync";
     if (HasFlag(kSmallWindow))
       suffix += "_small";
     if (HasFlag(k24fps))
@@ -406,6 +400,8 @@ class CastV2PerformanceTest
 
   void SetUp() override {
     EnablePixelOutput();
+    if (!HasFlag(kUseGpu))
+      UseSoftwareCompositing();
     ExtensionApiTest::SetUp();
   }
 
@@ -425,12 +421,10 @@ class CastV2PerformanceTest
     if (!HasFlag(kUseGpu))
       command_line->AppendSwitch(switches::kDisableGpu);
 
-    if (HasFlag(kDisableVsync))
-      command_line->AppendSwitch(switches::kDisableGpuVsync);
-
     command_line->AppendSwitchASCII(
         extensions::switches::kWhitelistedExtensionID,
         kExtensionId);
+
     ExtensionApiTest::SetUpCommandLine(command_line);
   }
 
@@ -721,7 +715,6 @@ INSTANTIATE_TEST_CASE_P(
     testing::Values(kUseGpu | k24fps,
                     kUseGpu | k30fps,
                     kUseGpu | k60fps,
-                    kUseGpu | k24fps | kDisableVsync,
                     kUseGpu | k30fps | kProxyWifi,
                     kUseGpu | k30fps | kProxyBad,
                     kUseGpu | k30fps | kSlowClock,
