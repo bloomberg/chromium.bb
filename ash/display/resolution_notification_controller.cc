4442e23fd519cdcdd87671f31d3cebf6fa89891e
Weidong Guo <weidongg@chromium.org>
2018-01-29 17:49:26 +0000

    Avoid resolution revert in Kiosk mode
    Changes:
    Assign 0 to the timeout of the notification to make resolution change
    irreversible in Kiosk mode.
    
    BUG=794135
    
    Change-Id: Icb17ba31c15fe25476674142db5ae317f6d4e6af
    Reviewed-on: https://chromium-review.googlesource.com/887224
    Commit-Queue: Weidong Guo <weidongg@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532481}
    Former-commit-id: 5f016476f6d183c2fbc4230e2270a33a0cff1cb3

--- ash/display/resolution_notification_controller.cc
+++ ash/display/resolution_notification_controller.cc
@@ -7,6 +7,7 @@
 #include <utility>
 
 #include "ash/resources/grit/ash_resources.h"
+#include "ash/session/session_controller.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/system/screen_layout_observer.h"
@@ -137,7 +138,12 @@ ResolutionNotificationController::ResolutionChangeInfo::ResolutionChangeInfo(
       timeout_count(0) {
   display::DisplayManager* display_manager = Shell::Get()->display_manager();
   if (!display::Display::HasInternalDisplay() &&
-      display_manager->num_connected_displays() == 1u) {
+      display_manager->num_connected_displays() == 1u &&
+      Shell::Get()->session_controller()->login_status() !=
+          LoginStatus::KIOSK_APP) {
+    // Introduce a timeout if we have a single external display and the device
+    // is not in Kiosk mode. (The resolution change notification is invisible in
+    // Kiosk mode, so do not introduce the timeout in this case.)
     timeout_count = kTimeoutInSec;
   }
 }
