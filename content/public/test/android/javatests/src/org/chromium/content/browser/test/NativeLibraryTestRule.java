b95cdd577f4c75b29210578436a3e79bfff79b01
Yoland Yan <yolandyan@chromium.org>
2018-01-29 20:52:30 +0000

    Merge NativeLibraryTestCommon implementation to NativeLibraryTestRule
    Merge TestCommon into TestRule since there is no long need for sharing
    the implementation with other classes
    
    Bug: 711517
    Change-Id: I40f53f07d734112fb85fb2bf8d1efe1ad4752097
    Reviewed-on: https://chromium-review.googlesource.com/887747
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Yoland Yan <yolandyan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532580}
    Former-commit-id: aebed7cbfcb7fd873caddd70851029a330ff3761

--- content/public/test/android/javatests/src/org/chromium/content/browser/test/NativeLibraryTestRule.java
+++ content/public/test/android/javatests/src/org/chromium/content/browser/test/NativeLibraryTestRule.java
@@ -4,11 +4,19 @@
 
 package org.chromium.content.browser.test;
 
-import android.support.test.InstrumentationRegistry;
-
+import org.junit.Assert;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
+
+import org.chromium.base.PathUtils;
+import org.chromium.base.ResourceExtractor;
+import org.chromium.base.ThreadUtils;
+import org.chromium.base.library_loader.LibraryLoader;
+import org.chromium.base.library_loader.LibraryProcessType;
+import org.chromium.base.library_loader.ProcessInitException;
+import org.chromium.content.browser.BrowserStartupController;
+
 
 /**
  * TestRule that adds support for loading and dealing with native libraries.
@@ -16,13 +24,13 @@ import org.junit.runners.model.Statement;
  * NativeLibraryTestRule does not interact with any Activity.
  */
 public class NativeLibraryTestRule implements TestRule {
-    private final NativeLibraryTestCommon mTestCommon = new NativeLibraryTestCommon();
+    private static final String PRIVATE_DATA_DIRECTORY_SUFFIX = "content";
 
     /**
      * Loads the native library on the activity UI thread (must not be called from the UI thread).
      */
     public void loadNativeLibraryNoBrowserProcess() {
-        mTestCommon.handleNativeInitialization(false, InstrumentationRegistry.getInstrumentation());
+        handleNativeInitialization(false);
     }
 
     /**
@@ -30,7 +38,45 @@ public class NativeLibraryTestRule implements TestRule {
      * After loading the library, this will initialize the browser process.
      */
     public void loadNativeLibraryAndInitBrowserProcess() {
-        mTestCommon.handleNativeInitialization(true, InstrumentationRegistry.getInstrumentation());
+        handleNativeInitialization(true);
+    }
+
+    private void handleNativeInitialization(final boolean initBrowserProcess) {
+        Assert.assertFalse(ThreadUtils.runningOnUiThread());
+
+        PathUtils.setPrivateDataDirectorySuffix(PRIVATE_DATA_DIRECTORY_SUFFIX);
+
+        // LibraryLoader is not in general multithreaded; as other InstrumentationTestCase code
+        // (specifically, ChromeBrowserProvider) uses it from the main thread we must do
+        // likewise.
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
+                nativeInitialization(initBrowserProcess);
+            }
+        });
+    }
+
+    private void nativeInitialization(boolean initBrowserProcess) {
+        if (initBrowserProcess) {
+            try {
+                // Extract compressed resource paks.
+                ResourceExtractor resourceExtractor = ResourceExtractor.get();
+                resourceExtractor.startExtractingResources();
+                resourceExtractor.waitForCompletion();
+
+                BrowserStartupController.get(LibraryProcessType.PROCESS_BROWSER)
+                        .startBrowserProcessesSync(false);
+            } catch (ProcessInitException e) {
+                throw new Error(e);
+            }
+        } else {
+            try {
+                LibraryLoader.get(LibraryProcessType.PROCESS_BROWSER).ensureInitialized();
+            } catch (ProcessInitException e) {
+                throw new Error(e);
+            }
+        }
     }
 
     @Override
