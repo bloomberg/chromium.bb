f5761b3f436d1eff484c2fc1de68c60278557779
Peter E Conn <peconn@chromium.org>
2018-01-29 15:12:24 +0000

    🛃 Add pending URL to BrowserSessionContentHandler.
    Bug: 806749
    Change-Id: If2867aceffe6c3ea9463eb1f5c36715d981ba97b
    Reviewed-on: https://chromium-review.googlesource.com/891222
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532431}
    Former-commit-id: 91253b09ed9a69bc3ffcc89b740443802631f0a7

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -22,6 +22,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.provider.Browser;
+import android.support.annotation.Nullable;
 import android.support.customtabs.CustomTabsCallback;
 import android.support.customtabs.CustomTabsIntent;
 import android.support.customtabs.CustomTabsSessionToken;
@@ -85,6 +86,7 @@ import org.chromium.chrome.browser.util.UrlUtilities;
 import org.chromium.chrome.browser.webapps.WebappInterceptNavigationDelegate;
 import org.chromium.components.dom_distiller.core.DomDistillerUrlUtils;
 import org.chromium.content_public.browser.LoadUrlParams;
+import org.chromium.content_public.browser.NavigationEntry;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.ui.base.PageTransition;
 import org.chromium.ui.base.WindowAndroid;
@@ -493,8 +495,17 @@ public class CustomTabActivity extends ChromeActivity {
             }
 
             @Override
+            @Nullable
             public String getCurrentUrl() {
                 return getActivityTab() == null ? null : getActivityTab().getUrl();
+            }
+
+            @Override
+            @Nullable
+            public String getPendingUrl() {
+                NavigationEntry entry = getActivityTab().getWebContents().getNavigationController()
+                        .getPendingEntry();
+                return entry != null ? entry.getUrl() : null;
             }
         };
         recordClientPackageName();
