6d45122c9cfd3c74a728501965e196abfdaf083c
Philipp Hancke <philipp.hancke@googlemail.com>
2018-01-29 15:51:06 +0000

    remove unused code from internal webrtc-adapter.js
    Removed all code in adapter.js that was not for Chrome, for
    very outdated versions of Chrome or not used anywhere.
    
    This file is a very old version of the project now residing on
        https://github.com/webrtc/adapter
    
    Bug: 690582
    Change-Id: I21fa60fd86962dadbcec1650201fc13e932b736e
    Reviewed-on: https://chromium-review.googlesource.com/889755
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Reviewed-by: Tommi <tommi@chromium.org>
    Commit-Queue: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532441}
    Former-commit-id: cfb13462393288781b461b2d911edfa75311ad06

--- chrome/test/data/webrtc/adapter.js
+++ chrome/test/data/webrtc/adapter.js
@@ -6,9 +6,6 @@
 
 var getUserMedia = null;
 var attachMediaStream = null;
-var reattachMediaStream = null;
-var webrtcDetectedBrowser = null;
-var webrtcDetectedVersion = null;
 
 function trace(text) {
   // This function is used for logging.
@@ -18,107 +15,9 @@ function trace(text) {
   console.log((performance.now() / 1000).toFixed(3) + ": " + text);
 }
 
-if (navigator.mozGetUserMedia) {
-  console.log("This appears to be Firefox");
+getUserMedia = navigator.getUserMedia.bind(navigator);
 
-  webrtcDetectedBrowser = "firefox";
-
-  webrtcDetectedVersion =
-                  parseInt(navigator.userAgent.match(/Firefox\/([0-9]+)\./)[1]);
-
-  // The RTCSessionDescription object.
-  RTCSessionDescription = mozRTCSessionDescription;
-
-  // The RTCIceCandidate object.
-  RTCIceCandidate = mozRTCIceCandidate;
-
-  // Get UserMedia (only difference is the prefix).
-  // Code from Adam Barth.
-  getUserMedia = navigator.mozGetUserMedia.bind(navigator);
-
-  // Creates iceServer from the url for FF.
-  createIceServer = function(url, username, password) {
-    var iceServer = null;
-    var url_parts = url.split(':');
-    if (url_parts[0].indexOf('stun') === 0) {
-      // Create iceServer with stun url.
-      iceServer = { 'url': url };
-    } else if (url_parts[0].indexOf('turn') === 0 &&
-               (url.indexOf('transport=udp') !== -1 ||
-                url.indexOf('?transport') === -1)) {
-      // Create iceServer with turn url.
-      // Ignore the transport parameter from TURN url.
-      var turn_url_parts = url.split("?");
-      iceServer = { 'url': turn_url_parts[0],
-                    'credential': password,
-                    'username': username };
-    }
-    return iceServer;
-  };
-
-  // Attach a media stream to an element.
-  attachMediaStream = function(element, stream) {
-    console.log("Attaching media stream");
-    element.mozSrcObject = stream;
-    element.play();
-  };
-
-  reattachMediaStream = function(to, from) {
-    console.log("Reattaching media stream");
-    to.mozSrcObject = from.mozSrcObject;
-    to.play();
-  };
-
-} else if (navigator.webkitGetUserMedia) {
-  console.log("This appears to be Chrome");
-
-  webrtcDetectedBrowser = "chrome";
-  webrtcDetectedVersion =
-             parseInt(navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./)[2]);
-
-  // Creates iceServer from the url for Chrome.
-  createIceServer = function(url, username, password) {
-    var iceServer = null;
-    var url_parts = url.split(':');
-    if (url_parts[0].indexOf('stun') === 0) {
-      // Create iceServer with stun url.
-      iceServer = { 'url': url };
-    } else if (url_parts[0].indexOf('turn') === 0) {
-      if (webrtcDetectedVersion < 28) {
-        // For pre-M28 chrome versions use old TURN format.
-        var url_turn_parts = url.split("turn:");
-        iceServer = { 'url': 'turn:' + username + '@' + url_turn_parts[1],
-                      'credential': password };
-      } else {
-        // For Chrome M28 & above use new TURN format.
-        iceServer = { 'url': url,
-                      'credential': password,
-                      'username': username };
-      }
-    }
-    return iceServer;
-  };
-
-  // Get UserMedia (only difference is the prefix).
-  // Code from Adam Barth.
-  getUserMedia = navigator.webkitGetUserMedia.bind(navigator);
-
-  // Attach a media stream to an element.
-  attachMediaStream = function(element, stream) {
-    if (typeof element.srcObject !== 'undefined') {
-      element.srcObject = stream;
-    } else if (typeof element.mozSrcObject !== 'undefined') {
-      element.mozSrcObject = stream;
-    } else if (typeof element.src !== 'undefined') {
-      element.src = URL.createObjectURL(stream);
-    } else {
-      console.log('Error attaching stream to element.');
-    }
-  };
-
-  reattachMediaStream = function(to, from) {
-    to.src = from.src;
-  };
-} else {
-  console.log("Browser does not appear to be WebRTC-capable");
+// Attach a media stream to an element.
+attachMediaStream = function(element, stream) {
+  element.srcObject = stream;
 }
