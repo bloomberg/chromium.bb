a234b2aa028525eafcb9ae63d4b41077ddc323f8
Bernhard Bauer <bauerb@chromium.org>
2018-01-30 14:40:14 +0000

    Log initialization errors in JUnit test suites.
    This helps with debugging, as the thrown exception will get mangled by
    the test runner script.
    
    Bug: 807237
    Change-Id: I74677e491c0115521b9b79c051f5213e832545ae
    Reviewed-on: https://chromium-review.googlesource.com/892863
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532862}
    Former-commit-id: adee399fe7d7084f5e040c32091cdca5ac0b98ca

--- base/test/android/javatests/src/org/chromium/base/test/BaseJUnit4ClassRunner.java
+++ base/test/android/javatests/src/org/chromium/base/test/BaseJUnit4ClassRunner.java
@@ -19,6 +19,7 @@ import org.junit.runners.model.Statement;
 
 import org.chromium.base.CollectionUtil;
 import org.chromium.base.CommandLine;
+import org.chromium.base.Log;
 import org.chromium.base.test.BaseTestResult.PreTestHook;
 import org.chromium.base.test.util.DisableIfSkipCheck;
 import org.chromium.base.test.util.MinAndroidSdkLevelSkipCheck;
@@ -39,6 +40,7 @@ import java.util.List;
  *  and {@link #isIgnored} to add SkipChecks and PreTesthook.
  */
 public class BaseJUnit4ClassRunner extends AndroidJUnit4ClassRunner {
+    private static final String TAG = "BaseJUnit4ClassRunnr";
     private final List<SkipCheck> mSkipChecks;
     private final List<PreTestHook> mPreTestHooks;
 
@@ -125,6 +127,16 @@ public class BaseJUnit4ClassRunner extends AndroidJUnit4ClassRunner {
             l.addAll(listB);
         }
         return l;
+    }
+
+    @Override
+    protected void collectInitializationErrors(List<Throwable> errors) {
+        super.collectInitializationErrors(errors);
+        // Log any initialization errors to help debugging, as the host-side test runner can get
+        // confused by the thrown exception.
+        if (!errors.isEmpty()) {
+            Log.e(TAG, "Initialization errors in %s: %s", getTestClass().getName(), errors);
+        }
     }
 
     /**
