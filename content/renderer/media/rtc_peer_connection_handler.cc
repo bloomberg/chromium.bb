bc6976c49a8162382a0d58b424f5c8c98c9b6982
Harald Alvestrand <hta@chromium.org>
2018-01-30 12:15:06 +0000

    Expose WebRTC counter for key protocol to UMA
    This allows to observe how many WebRTC users use the deprecated
    SDES key agreement style and how many use the DTLS style.
    
    Bug: 804275
    Change-Id: I0220328a0897e67fde387288ad6ec1475dca3de3
    Reviewed-on: https://chromium-review.googlesource.com/891224
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532847}
    Former-commit-id: 914c00e52f9383b4312eecc9f6624128e0d73920

--- content/renderer/media/rtc_peer_connection_handler.cc
+++ content/renderer/media/rtc_peer_connection_handler.cc
@@ -820,8 +820,13 @@ class PeerConnectionUMAObserver : public webrtc::UMAObserver {
         UMA_HISTOGRAM_EXACT_LINEAR("WebRTC.PeerConnection.IceRegatheringReason",
                                    counter, counter_max);
         break;
+      case webrtc::kEnumCounterKeyProtocol:
+        UMA_HISTOGRAM_ENUMERATION(
+            "WebRTC.PeerConnection.KeyProtocol",
+            static_cast<webrtc::KeyExchangeProtocolType>(counter),
+            webrtc::kEnumCounterKeyProtocolMax);
       default:
-        // The default clause is expected to reach when new enum types are
+        // The default clause is expected to be reached when new enum types are
         // added.
         break;
     }
