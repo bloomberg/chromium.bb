ad33884a3a45de3b7f54d474511640ff266f2070
Josh Karlin <jkarlin@chromium.org>
2018-01-30 18:19:53 +0000

    Creates a tool that converts unindexed rulesets to indexed on disk
    Creates a new binary, subresource_indexing_tool, that takes an unindexed
    ruleset as input and writes an indexed ruleset as output.
    
    Bug: 793025
    Change-Id: Ieeb10aab4ba80c83baf8058fe202e4d4fcd770e1
    Reviewed-on: https://chromium-review.googlesource.com/891140
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532952}
    Former-commit-id: a8f0905fc0786d3eb7aea956b48ea01bb8e61091

--- components/subresource_filter/core/common/test_ruleset_utils.cc
+++ components/subresource_filter/core/common/test_ruleset_utils.cc
@@ -4,6 +4,8 @@
 
 #include "components/subresource_filter/core/common/test_ruleset_utils.h"
 
+#include <utility>
+
 namespace subresource_filter {
 namespace testing {
 
@@ -12,6 +14,18 @@ namespace proto = url_pattern_index::proto;
 proto::UrlRule CreateSuffixRule(base::StringPiece suffix) {
   proto::UrlRule rule;
   rule.set_semantics(proto::RULE_SEMANTICS_BLACKLIST);
+  rule.set_source_type(proto::SOURCE_TYPE_ANY);
+  rule.set_element_types(proto::ELEMENT_TYPE_ALL);
+  rule.set_url_pattern_type(proto::URL_PATTERN_TYPE_SUBSTRING);
+  rule.set_anchor_left(proto::ANCHOR_TYPE_NONE);
+  rule.set_anchor_right(proto::ANCHOR_TYPE_BOUNDARY);
+  rule.set_url_pattern(suffix.as_string());
+  return rule;
+}
+
+proto::UrlRule CreateWhitelistSuffixRule(base::StringPiece suffix) {
+  proto::UrlRule rule;
+  rule.set_semantics(proto::RULE_SEMANTICS_WHITELIST);
   rule.set_source_type(proto::SOURCE_TYPE_ANY);
   rule.set_element_types(proto::ELEMENT_TYPE_ALL);
   rule.set_url_pattern_type(proto::URL_PATTERN_TYPE_SUBSTRING);
