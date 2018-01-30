98e6d8ad3747979a448ad8ccaa4ef811a3b148e9
Nate Chapin <japhet@chromium.org>
2018-01-30 18:47:44 +0000

    Fix detach with open()ed document leaving parent loading indefinitely
    Change-Id: I26c2a054b9f1e5eb076acd677e1223058825f6d6
    
    Bug: 803416
    Test: fast/loader/document-open-iframe-then-detach.html
    Change-Id: I26c2a054b9f1e5eb076acd677e1223058825f6d6
    Reviewed-on: https://chromium-review.googlesource.com/887298
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532967}
    Former-commit-id: fd506b0ac6c7846ae45b5034044fe85c28ee68ac

--- third_party/WebKit/Source/core/loader/DocumentLoader.cpp
+++ third_party/WebKit/Source/core/loader/DocumentLoader.cpp
@@ -773,14 +773,15 @@ void DocumentLoader::AppendRedirect(const KURL& url) {
   redirect_chain_.push_back(url);
 }
 
-void DocumentLoader::DetachFromFrame() {
-  DCHECK(frame_);
-
-  // It never makes sense to have a document loader that is detached from its
-  // frame have any loads active, so go ahead and kill all the loads.
+void DocumentLoader::StopLoading() {
   fetcher_->StopFetching();
   if (frame_ && !SentDidFinishLoad())
     LoadFailed(ResourceError::CancelledError(Url()));
+}
+
+void DocumentLoader::DetachFromFrame() {
+  DCHECK(frame_);
+  StopLoading();
   fetcher_->ClearContext();
 
   // If that load cancellation triggered another detach, leave.
