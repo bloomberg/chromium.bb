c4d22789dc001d9fb45ef56f954c0d71304ba634
tanvir.rizvi <tanvir.rizvi@samsung.com>
2018-01-30 01:47:06 +0000

    Use selection directional for commands from EditCommands.
    This CL uses selection directional value for commands
    from EditCommand SelectionIsDirectional().
    This is done because we will remove directional
    from SelectionTemplate.
    
    Bug: 740804
    Change-Id: Icb6cb608d92c6688139df713a056a7fc972d2441
    Reviewed-on: https://chromium-review.googlesource.com/891200
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532724}
    Former-commit-id: 7176fc85011c8da650f296b509bc0acf8b7bb89e

--- third_party/WebKit/Source/core/editing/commands/ApplyStyleCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/ApplyStyleCommand.cpp
@@ -183,7 +183,7 @@ void ApplyStyleCommand::UpdateStartEnd(const Position& new_start,
     use_ending_selection_ = true;
   GetDocument().UpdateStyleAndLayoutIgnorePendingStylesheets();
   const bool was_base_first =
-      StartingSelection().IsBaseFirst() || !StartingSelection().IsDirectional();
+      StartingSelection().IsBaseFirst() || !SelectionIsDirectional();
   const EphemeralRange range(new_start, new_end);
   SelectionInDOMTree::Builder builder;
   builder.SetIsDirectional(EndingSelection().IsDirectional());
