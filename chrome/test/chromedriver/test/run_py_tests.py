eeeaf978c33dbe8406fab4ddd4cba44a1df8954c
Artur Khachatryan <khachatryan@chromium.org>
2018-01-30 20:30:17 +0000

    [ChromeDriver] Disable testGoBackAndGoForward for Chrome v65
    Test testGoBackAndGoForward fails on Waterfall for Chrome v65.
    Disabling test execution in Waterfall build.
    
    Bug: 803678
    Change-Id: I0e084797f727dbd911cb16032fbb642040a1de3a
    Reviewed-on: https://chromium-review.googlesource.com/893420
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Artur Khachatryan <khachatryan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533007}
    Former-commit-id: 62c93ab55fdfb7945a8e4320dd07b8e8601b1f3e

--- chrome/test/chromedriver/test/run_py_tests.py
+++ chrome/test/chromedriver/test/run_py_tests.py
@@ -91,6 +91,12 @@ _VERSION_SPECIFIC_FILTER['HEAD'] = [
     'MobileEmulationCapabilityTest.testNetworkConnectionTypeIsAppliedToAllTabsImmediately',
 ]
 
+_VERSION_SPECIFIC_FILTER['65'] = [
+    # https://bugs.chromium.org/p/chromium/issues/detail?id=803678
+    'ChromeDriverTest.testGoBackAndGoForward',
+    'ChromeDriverTest.testAlertHandlingOnPageUnload'
+]
+
 _VERSION_SPECIFIC_FILTER['64'] = [
     # These tests are implemented to run on the latest versions of Chrome > 64
     'HeadlessInvalidCertificateTest.*',
