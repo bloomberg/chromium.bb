94d07ddb5d62ba65e7c3af6a257ca5f9c17e70cc
Steven Bennetts <stevenjb@chromium.org>
2018-01-30 21:19:32 +0000

    Elim unused NetworkConnect function
    Bug: None
    Change-Id: Ib881ec300b0fa0d3ee5666a905fb650d2dd9a9c5
    Reviewed-on: https://chromium-review.googlesource.com/887727
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533024}
    Former-commit-id: e790034a0982d9428de2b5d4d8931ed9c8a02fd4

--- chromeos/network/network_connect_unittest.cc
+++ chromeos/network/network_connect_unittest.cc
@@ -213,62 +213,6 @@ TEST_F(NetworkConnectTest,
                                                       false);
 }
 
-TEST_F(NetworkConnectTest, ShowConfigureUI) {
-  EXPECT_CALL(*mock_delegate_, ShowNetworkConfigure(kWiFi1Guid)).Times(5);
-
-  NetworkConnect::Get()->MaybeShowConfigureUI(
-      kWiFi1Guid, NetworkConnectionHandler::kErrorBadPassphrase);
-  NetworkConnect::Get()->MaybeShowConfigureUI(
-      kWiFi1Guid, NetworkConnectionHandler::kErrorPassphraseRequired);
-  NetworkConnect::Get()->MaybeShowConfigureUI(
-      kWiFi1Guid, NetworkConnectionHandler::kErrorConfigurationRequired);
-  NetworkConnect::Get()->MaybeShowConfigureUI(
-      kWiFi1Guid, NetworkConnectionHandler::kErrorAuthenticationRequired);
-  NetworkConnect::Get()->MaybeShowConfigureUI(
-      kWiFi1Guid, NetworkConnectionHandler::kErrorConnectFailed);
-}
-
-TEST_F(NetworkConnectTest, ConfigureUINotShownForTetherAssociatedWifiNetwork) {
-  // The configure UI should *not* be shown for Wi-Fi networks which serve as
-  // the underlying Wi-Fi hotspot for a Tether network.
-  EXPECT_CALL(*mock_delegate_, ShowNetworkConfigure(kWiFi1Guid)).Times(0);
-
-  AddTetherNetwork(false /* has_connected_to_host */);
-  NetworkHandler::Get()
-      ->network_state_handler()
-      ->AssociateTetherNetworkStateWithWifiNetwork(kTetherGuid, kWiFi1Guid);
-
-  NetworkConnect::Get()->MaybeShowConfigureUI(
-      kWiFi1Guid, NetworkConnectionHandler::kErrorConnectFailed);
-}
-
-TEST_F(NetworkConnectTest, ShowConfigureUI_BadErrorCode) {
-  EXPECT_CALL(*mock_delegate_, ShowNetworkConfigure(kWiFi1Guid)).Times(0);
-
-  NetworkConnect::Get()->MaybeShowConfigureUI(kWiFi1Guid,
-                                              "incorrect error code");
-}
-
-TEST_F(NetworkConnectTest, ShowConfigureUI_CertRequired_ShowEnrollNetwork) {
-  EXPECT_CALL(*mock_delegate_, ShowEnrollNetwork(_)).Times(AnyNumber());
-  EXPECT_CALL(*mock_delegate_, ShowNetworkConfigure(kWiFi1Guid)).Times(0);
-
-  NetworkConnect::Get()->MaybeShowConfigureUI(
-      kWiFi1Guid, NetworkConnectionHandler::kErrorCertificateRequired);
-}
-
-TEST_F(NetworkConnectTest,
-       ShowConfigureUI_CertRequired_DoNotShowEnrollNetwork) {
-  EXPECT_CALL(*mock_delegate_, ShowEnrollNetwork(_)).Times(AnyNumber());
-  EXPECT_CALL(*mock_delegate_, ShowNetworkConfigure(kWiFi1Guid));
-
-  ON_CALL(*mock_delegate_, ShowEnrollNetwork(kWiFi1Guid))
-      .WillByDefault(Return(false));
-
-  NetworkConnect::Get()->MaybeShowConfigureUI(
-      kWiFi1Guid, NetworkConnectionHandler::kErrorCertificateRequired);
-}
-
 TEST_F(NetworkConnectTest, ConnectThenDisconnectWiFiNetwork) {
   const NetworkState* network =
       NetworkHandler::Get()->network_state_handler()->GetNetworkStateFromGuid(
@@ -302,26 +246,6 @@ TEST_F(NetworkConnectTest, ConnectToTetherNetwork_HasNotConnectedToHost) {
   NetworkConnect::Get()->ConnectToNetworkId(kTetherGuid);
   EXPECT_TRUE(
       fake_tether_delegate_->last_connected_tether_network_guid().empty());
-}
-
-// ShowNetworkSettings only applies to cellular networks.
-TEST_F(NetworkConnectTest, ShowNetworkSettings) {
-  EXPECT_CALL(*mock_delegate_, ShowNetworkSettings(kCellular1Guid));
-
-  NetworkConnect::Get()->MaybeShowConfigureUI(
-      kCellular1Guid, NetworkConnectionHandler::kErrorConnectFailed);
-}
-
-TEST_F(NetworkConnectTest, ShowNetworkSettings_CellOutOfCredits) {
-  EXPECT_CALL(*mock_delegate_, ShowNetworkSettings(kCellular1Guid)).Times(0);
-  EXPECT_CALL(*mock_delegate_, ShowMobileSetupDialog(kCellular1Guid));
-
-  service_test_->SetServiceProperty(
-      kCellular1ServicePath, shill::kOutOfCreditsProperty, base::Value(true));
-  base::RunLoop().RunUntilIdle();
-
-  NetworkConnect::Get()->MaybeShowConfigureUI(
-      kCellular1Guid, NetworkConnectionHandler::kErrorConnectFailed);
 }
 
 TEST_F(NetworkConnectTest, ActivateCellular) {
