e540c9e15f9385441df54c9c499b0a78e24dd1e7
Ella Ge <eirage@chromium.org>
2018-01-30 17:25:23 +0000

    send mousemove instead of leave after context menu
    This CL changes mac SetShowingContextMenu to send a MouseMove instead
    of MouseLeave. And add kRelativeMotionEvent flag in the modifier to
    avoid sending the event to DOM.
    Sending MouseLeave will cause hover/active state be cleared when open
    contextmenu.
    
    Bug: 803844
    Change-Id: I38f2864b108f95a51c6e93536a1b410333fdf50e
    Reviewed-on: https://chromium-review.googlesource.com/888092
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532931}
    Former-commit-id: 0531e1c159fd0e45a8ac33f920374bae89b94cca

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -1161,8 +1161,8 @@ void RenderWidgetHostViewMac::SetShowingContextMenu(bool showing) {
                                     clickCount:0
                                       pressure:0];
   WebMouseEvent web_event = WebMouseEventBuilder::Build(event, cocoa_view_);
-  if (showing)
-    web_event.SetType(WebInputEvent::kMouseLeave);
+  web_event.SetModifiers(web_event.GetModifiers() |
+                         WebInputEvent::kRelativeMotionEvent);
   ForwardMouseEvent(web_event);
 }
 
