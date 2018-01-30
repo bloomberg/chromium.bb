17fefba162f74e6af74267dd3be9f262424e51f5
Nico Weber <thakis@chromium.org>
2018-01-30 16:30:38 +0000

    Work more on getting things built with -Wimplicit-fallthrough.
    This CL was uploaded by git cl split.
    
    R=junov@chromium.org
    
    Bug: 177475
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I377f439e850f0b26bc1c50f9bbb1f97a7d7dc8e1
    Reviewed-on: https://chromium-review.googlesource.com/890003
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532904}
    Former-commit-id: d4dc578e89f54b731b7bc23aea8d0ac0038dbf99

--- third_party/WebKit/Source/core/paint/BoxBorderPainter.cpp
+++ third_party/WebKit/Source/core/paint/BoxBorderPainter.cpp
@@ -345,7 +345,7 @@ void DrawBleedAdjustedDRRect(GraphicsContext& context,
         context.FillDRRect(adjusted_outer, inner, color);
         break;
       }
-    // fall through
+      FALLTHROUGH;
     default:
       context.FillDRRect(outer, inner, color);
       break;
