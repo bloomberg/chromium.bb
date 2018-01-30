1482edeb525cfe812855d44d8c35937ea603825a
Stephen Martinis <martiniss@chromium.org>
2018-01-30 00:25:57 +0000

    Use third party ninja in compile_db.py
    'ninja' isn't always in PATH on luci builders, so use the copy checked
    into third_party/depot_tools
    
    Bug: 731558
    Change-Id: I3f57375dc768d5266e7b80a0324abb3acf48fd49
    Reviewed-on: https://chromium-review.googlesource.com/889994
    Commit-Queue: Stephen Martinis <martiniss@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nodir Turakulov <nodir@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#532692}
    Former-commit-id: 50f07df061226f9b7a22d724ed600ede5082200d

--- tools/clang/pylib/clang/compile_db.py
+++ tools/clang/pylib/clang/compile_db.py
@@ -71,6 +71,13 @@ def _ProcessCompileDatabaseForWindows(compile_db):
   return compile_db
 
 
+def GetNinjaPath():
+  return os.path.join(
+      os.path.dirname(os.realpath(__file__)), '..', '..', '..', 'third_party',
+        'depot_tools', 'ninja')
+
+
+# FIXME: This really should be a build target, rather than generated at runtime.
 def GenerateWithNinja(path):
   """Generates a compile database using ninja.
 
@@ -84,7 +91,8 @@ def GenerateWithNinja(path):
 
   # First, generate the compile database.
   json_compile_db = subprocess.check_output([
-      'ninja', '-C', path, '-t', 'compdb', 'cc', 'cxx', 'objc', 'objcxx'])
+      GetNinjaPath(), '-C', path, '-t', 'compdb', 'cc', 'cxx', 'objc',
+      'objcxx'])
   compile_db = json.loads(json_compile_db)
 
   # TODO(dcheng): Ideally this would check target_os... but not sure there's an
