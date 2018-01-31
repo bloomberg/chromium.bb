a3c9df0d2328cc137e5d7a22811933976574018c
Maks Orlovich <morlovich@chromium.org>
2018-01-31 03:41:17 +0000

    SimpleCache: Enforce a limit on number of open files.
    Bug: 636400
    Change-Id: Ic4a49a72dcae73f1bd2d909a0e86439f7c075061
    Reviewed-on: https://chromium-review.googlesource.com/779742
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533188}
    Former-commit-id: db1f092d8f2c90207a795dc6e41f345d78c36c6c

--- net/disk_cache/simple/simple_file_tracker.cc
+++ net/disk_cache/simple/simple_file_tracker.cc
@@ -10,62 +10,98 @@
 #include <utility>
 
 #include "base/files/file.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/synchronization/lock.h"
+#include "net/disk_cache/simple/simple_histogram_enums.h"
 #include "net/disk_cache/simple/simple_synchronous_entry.h"
 
 namespace disk_cache {
 
-SimpleFileTracker::SimpleFileTracker() = default;
+SimpleFileTracker::SimpleFileTracker(int file_limit)
+    : file_limit_(file_limit), open_files_(0) {}
 
 SimpleFileTracker::~SimpleFileTracker() {
+  DCHECK(lru_.empty());
   DCHECK(tracked_files_.empty());
 }
 
 void SimpleFileTracker::Register(const SimpleSynchronousEntry* owner,
                                  SubFile subfile,
                                  std::unique_ptr<base::File> file) {
-  base::AutoLock hold_lock(lock_);
+  DCHECK(file->IsValid());
+  std::vector<std::unique_ptr<base::File>> files_to_close;
 
-  // Make sure the list exists.
-  auto insert_status = tracked_files_.insert(std::make_pair(
-      owner->entry_file_key().entry_hash, std::vector<TrackedFiles>()));
+  {
+    base::AutoLock hold_lock(lock_);
 
-  std::vector<TrackedFiles>& candidates = insert_status.first->second;
+    // Make sure the list of everything with given hash exists.
+    auto insert_status = tracked_files_.insert(
+        std::make_pair(owner->entry_file_key().entry_hash,
+                       std::vector<std::unique_ptr<TrackedFiles>>()));
 
-  // See if entry already exists, if not append.
-  TrackedFiles* owners_files = nullptr;
-  for (TrackedFiles& candidate : candidates) {
-    if (candidate.owner == owner) {
-      owners_files = &candidate;
-      break;
+    std::vector<std::unique_ptr<TrackedFiles>>& candidates =
+        insert_status.first->second;
+
+    // See if entry for |owner| already exists, if not append.
+    TrackedFiles* owners_files = nullptr;
+    for (const std::unique_ptr<TrackedFiles>& candidate : candidates) {
+      if (candidate->owner == owner) {
+        owners_files = candidate.get();
+        break;
+      }
     }
-  }
 
-  if (!owners_files) {
-    candidates.emplace_back();
-    owners_files = &candidates.back();
-    owners_files->owner = owner;
-    owners_files->key = owner->entry_file_key();
-  }
+    if (!owners_files) {
+      candidates.emplace_back(new TrackedFiles());
+      owners_files = candidates.back().get();
+      owners_files->owner = owner;
+      owners_files->key = owner->entry_file_key();
+    }
 
-  int file_index = static_cast<int>(subfile);
-  DCHECK_EQ(TrackedFiles::TF_NO_REGISTRATION, owners_files->state[file_index]);
-  owners_files->files[file_index] = std::move(file);
-  owners_files->state[file_index] = TrackedFiles::TF_REGISTERED;
+    EnsureInFrontOfLRU(owners_files);
+
+    int file_index = static_cast<int>(subfile);
+    DCHECK_EQ(TrackedFiles::TF_NO_REGISTRATION,
+              owners_files->state[file_index]);
+    owners_files->files[file_index] = std::move(file);
+    owners_files->state[file_index] = TrackedFiles::TF_REGISTERED;
+    ++open_files_;
+    CloseFilesIfTooManyOpen(&files_to_close);
+  }
 }
 
 SimpleFileTracker::FileHandle SimpleFileTracker::Acquire(
     const SimpleSynchronousEntry* owner,
     SubFile subfile) {
-  base::AutoLock hold_lock(lock_);
-  std::vector<TrackedFiles>::iterator owners_files = Find(owner);
-  int file_index = static_cast<int>(subfile);
+  std::vector<std::unique_ptr<base::File>> files_to_close;
 
-  DCHECK_EQ(TrackedFiles::TF_REGISTERED, owners_files->state[file_index]);
-  owners_files->state[file_index] = TrackedFiles::TF_ACQUIRED;
-  return FileHandle(this, owner, subfile,
-                    owners_files->files[file_index].get());
+  {
+    base::AutoLock hold_lock(lock_);
+    TrackedFiles* owners_files = Find(owner);
+    int file_index = static_cast<int>(subfile);
+
+    DCHECK_EQ(TrackedFiles::TF_REGISTERED, owners_files->state[file_index]);
+    owners_files->state[file_index] = TrackedFiles::TF_ACQUIRED;
+    EnsureInFrontOfLRU(owners_files);
+
+    // Check to see if we have to reopen the file. That might push us over the
+    // fd limit.  CloseFilesIfTooManyOpen will not close anything in
+    // |*owners_files| since it's already in the the TF_ACQUIRED state.
+    if (owners_files->files[file_index] == nullptr) {
+      ReopenFile(owners_files, subfile);
+      CloseFilesIfTooManyOpen(&files_to_close);
+    }
+
+    return FileHandle(this, owner, subfile,
+                      owners_files->files[file_index].get());
+  }
 }
+
+SimpleFileTracker::TrackedFiles::TrackedFiles() : in_lru(false) {
+  std::fill(state, state + kSimpleEntryTotalFileCount, TF_NO_REGISTRATION);
+}
+
+SimpleFileTracker::TrackedFiles::~TrackedFiles() = default;
 
 bool SimpleFileTracker::TrackedFiles::Empty() const {
   for (State s : state)
@@ -74,13 +110,20 @@ bool SimpleFileTracker::TrackedFiles::Empty() const {
   return true;
 }
 
+bool SimpleFileTracker::TrackedFiles::HasOpenFiles() const {
+  for (const std::unique_ptr<base::File>& file : files)
+    if (file != nullptr)
+      return true;
+  return false;
+}
+
 void SimpleFileTracker::Release(const SimpleSynchronousEntry* owner,
                                 SubFile subfile) {
-  std::unique_ptr<base::File> file_to_close;
+  std::vector<std::unique_ptr<base::File>> files_to_close;
 
   {
     base::AutoLock hold_lock(lock_);
-    std::vector<TrackedFiles>::iterator owners_files = Find(owner);
+    TrackedFiles* owners_files = Find(owner);
     int file_index = static_cast<int>(subfile);
 
     DCHECK(owners_files->state[file_index] == TrackedFiles::TF_ACQUIRED ||
@@ -90,13 +133,16 @@ void SimpleFileTracker::Release(const SimpleSynchronousEntry* owner,
     // Prepare to executed deferred close, if any.
     if (owners_files->state[file_index] ==
         TrackedFiles::TF_ACQUIRED_PENDING_CLOSE) {
-      file_to_close = PrepareClose(owners_files, file_index);
+      files_to_close.push_back(PrepareClose(owners_files, file_index));
     } else {
       owners_files->state[file_index] = TrackedFiles::TF_REGISTERED;
     }
-  }
 
-  // The destructor of file_to_close will close it if needed.
+    // It's possible that we were over limit and couldn't do much about it
+    // since everything was lent out, so now may be the time to close extra
+    // stuff.
+    CloseFilesIfTooManyOpen(&files_to_close);
+  }
 }
 
 void SimpleFileTracker::Close(const SimpleSynchronousEntry* owner,
@@ -105,7 +151,7 @@ void SimpleFileTracker::Close(const SimpleSynchronousEntry* owner,
 
   {
     base::AutoLock hold_lock(lock_);
-    std::vector<TrackedFiles>::iterator owners_files = Find(owner);
+    TrackedFiles* owners_files = Find(owner);
     int file_index = static_cast<int>(subfile);
 
     DCHECK(owners_files->state[file_index] == TrackedFiles::TF_ACQUIRED ||
@@ -120,10 +166,6 @@ void SimpleFileTracker::Close(const SimpleSynchronousEntry* owner,
       file_to_close = PrepareClose(owners_files, file_index);
     }
   }
-
-  // The destructor of file_to_close will close it if needed. Thing to watch
-  // for impl with stealing: race between bookkeeping above and actual
-  // close --- the FD is still alive for it.
 }
 
 void SimpleFileTracker::Doom(const SimpleSynchronousEntry* owner,
@@ -133,9 +175,10 @@ void SimpleFileTracker::Doom(const SimpleSynchronousEntry* owner,
   DCHECK(iter != tracked_files_.end());
 
   uint64_t max_doom_gen = 0;
-  for (const TrackedFiles& file_with_same_hash : iter->second) {
+  for (const std::unique_ptr<TrackedFiles>& file_with_same_hash :
+       iter->second) {
     max_doom_gen =
-        std::max(max_doom_gen, file_with_same_hash.key.doom_generation);
+        std::max(max_doom_gen, file_with_same_hash->key.doom_generation);
   }
 
   // It would take >502 years to doom the same hash enough times (at 10^9 dooms
@@ -148,44 +191,118 @@ void SimpleFileTracker::Doom(const SimpleSynchronousEntry* owner,
   key->doom_generation = new_doom_gen;
 
   // Update our own.
-  for (TrackedFiles& file_with_same_hash : iter->second) {
-    if (file_with_same_hash.owner == owner)
-      file_with_same_hash.key.doom_generation = new_doom_gen;
+  for (const std::unique_ptr<TrackedFiles>& file_with_same_hash :
+       iter->second) {
+    if (file_with_same_hash->owner == owner)
+      file_with_same_hash->key.doom_generation = new_doom_gen;
   }
 }
 
 bool SimpleFileTracker::IsEmptyForTesting() {
   base::AutoLock hold_lock(lock_);
-  return tracked_files_.empty();
+  return tracked_files_.empty() && lru_.empty();
 }
 
-std::vector<SimpleFileTracker::TrackedFiles>::iterator SimpleFileTracker::Find(
+SimpleFileTracker::TrackedFiles* SimpleFileTracker::Find(
     const SimpleSynchronousEntry* owner) {
   auto candidates = tracked_files_.find(owner->entry_file_key().entry_hash);
   DCHECK(candidates != tracked_files_.end());
-  for (std::vector<TrackedFiles>::iterator i = candidates->second.begin();
-       i != candidates->second.end(); ++i) {
-    if (i->owner == owner) {
-      return i;
+  for (const auto& candidate : candidates->second) {
+    if (candidate->owner == owner) {
+      return candidate.get();
     }
   }
   LOG(DFATAL) << "SimpleFileTracker operation on non-found entry";
-  return candidates->second.end();
+  return nullptr;
 }
 
 std::unique_ptr<base::File> SimpleFileTracker::PrepareClose(
-    std::vector<TrackedFiles>::iterator owners_files,
+    TrackedFiles* owners_files,
     int file_index) {
   std::unique_ptr<base::File> file_out =
       std::move(owners_files->files[file_index]);
   owners_files->state[file_index] = TrackedFiles::TF_NO_REGISTRATION;
   if (owners_files->Empty()) {
     auto iter = tracked_files_.find(owners_files->key.entry_hash);
-    iter->second.erase(owners_files);
+    for (auto i = iter->second.begin(); i != iter->second.end(); ++i) {
+      if ((*i).get() == owners_files) {
+        if (owners_files->in_lru)
+          lru_.erase(owners_files->position_in_lru);
+        iter->second.erase(i);
+        break;
+      }
+    }
     if (iter->second.empty())
       tracked_files_.erase(iter);
   }
+  if (file_out != nullptr)
+    --open_files_;
   return file_out;
+}
+
+void SimpleFileTracker::CloseFilesIfTooManyOpen(
+    std::vector<std::unique_ptr<base::File>>* files_to_close) {
+  std::list<TrackedFiles*>::iterator i = lru_.end();
+  while (open_files_ > file_limit_ && i != lru_.begin()) {
+    --i;  // Point to the actual entry.
+    TrackedFiles* tracked_files = *i;
+    DCHECK(tracked_files->in_lru);
+    for (int j = 0; j < kSimpleEntryTotalFileCount; ++j) {
+      if (tracked_files->state[j] == TrackedFiles::TF_REGISTERED &&
+          tracked_files->files[j] != nullptr) {
+        files_to_close->push_back(std::move(tracked_files->files[j]));
+        --open_files_;
+        UMA_HISTOGRAM_ENUMERATION("SimpleCache.FileDescriptorLimiterAction",
+                                  FD_LIMIT_CLOSE_FILE, FD_LIMIT_OP_MAX);
+      }
+    }
+
+    if (!tracked_files->HasOpenFiles()) {
+      // If there is nothing here that can possibly be closed, remove this from
+      // LRU for now so we don't have to rescan it next time we are here. If the
+      // files get re-opened (in Acquire), it will get added back in.
+      DCHECK_EQ(*tracked_files->position_in_lru, tracked_files);
+      DCHECK(i == tracked_files->position_in_lru);
+      // Note that we're erasing at i, which would make it invalid, so go back
+      // one element ahead to we can decrement from that on next iteration.
+      ++i;
+      lru_.erase(tracked_files->position_in_lru);
+      tracked_files->in_lru = false;
+    }
+  }
+}
+
+void SimpleFileTracker::ReopenFile(TrackedFiles* owners_files,
+                                   SubFile subfile) {
+  int file_index = static_cast<int>(subfile);
+  DCHECK(owners_files->files[file_index] == nullptr);
+  int flags = base::File::FLAG_OPEN | base::File::FLAG_READ |
+              base::File::FLAG_WRITE | base::File::FLAG_SHARE_DELETE;
+  base::FilePath file_path =
+      owners_files->owner->GetFilenameForSubfile(subfile);
+  owners_files->files[file_index] =
+      std::make_unique<base::File>(file_path, flags);
+  if (owners_files->files[file_index]->IsValid()) {
+    UMA_HISTOGRAM_ENUMERATION("SimpleCache.FileDescriptorLimiterAction",
+                              FD_LIMIT_REOPEN_FILE, FD_LIMIT_OP_MAX);
+
+    ++open_files_;
+  } else {
+    owners_files->files[file_index] = nullptr;
+    UMA_HISTOGRAM_ENUMERATION("SimpleCache.FileDescriptorLimiterAction",
+                              FD_LIMIT_FAIL_REOPEN_FILE, FD_LIMIT_OP_MAX);
+  }
+}
+
+void SimpleFileTracker::EnsureInFrontOfLRU(TrackedFiles* owners_files) {
+  if (!owners_files->in_lru) {
+    lru_.push_front(owners_files);
+    owners_files->position_in_lru = lru_.begin();
+    owners_files->in_lru = true;
+  } else if (owners_files->position_in_lru != lru_.begin()) {
+    lru_.splice(lru_.begin(), lru_, owners_files->position_in_lru);
+  }
+  DCHECK_EQ(*owners_files->position_in_lru, owners_files);
 }
 
 SimpleFileTracker::FileHandle::FileHandle()
--- net/disk_cache/simple/simple_file_tracker_unittest.cc
+++ net/disk_cache/simple/simple_file_tracker_unittest.cc
@@ -11,9 +11,12 @@
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_piece.h"
+#include "base/strings/string_util.h"
+#include "base/test/histogram_tester.h"
 #include "net/base/cache_type.h"
 #include "net/disk_cache/disk_cache_test_base.h"
 #include "net/disk_cache/simple/simple_file_tracker.h"
+#include "net/disk_cache/simple/simple_histogram_enums.h"
 #include "net/disk_cache/simple/simple_synchronous_entry.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -23,7 +26,14 @@ class SimpleFileTrackerTest : public DiskCacheTest {
  public:
   void DeleteSyncEntry(SimpleSynchronousEntry* entry) { delete entry; }
 
+  // We limit open files to 4 for the fixture, as this is large enough
+  // that simple tests don't have to worry about naming files normally,
+  // but small enough to test with easily.
+  static const int kFileLimit = 4;
+
  protected:
+  SimpleFileTrackerTest() : file_tracker_(kFileLimit) {}
+
   // A bit of messiness since we rely on friendship of the fixture to be able to
   // create/delete SimpleSynchronousEntry objects.
   class SyncEntryDeleter {
@@ -45,6 +55,11 @@ class SimpleFileTrackerTest : public DiskCacheTest {
         new SimpleSynchronousEntry(net::DISK_CACHE, cache_path_, "dummy", hash,
                                    /* had_index=*/true, &file_tracker_),
         SyncEntryDeleter(this));
+  }
+
+  void UpdateEntryFileKey(SimpleSynchronousEntry* sync_entry,
+                          SimpleFileTracker::EntryFileKey file_key) {
+    sync_entry->entry_file_key_ = file_key;
   }
 
   SimpleFileTracker file_tracker_;
@@ -166,10 +181,11 @@ TEST_F(SimpleFileTrackerTest, Reopen) {
                          std::move(file_1));
 
   file_tracker_.Close(entry.get(), SimpleFileTracker::SubFile::FILE_1);
-  base::File file_1b(path_1, base::File::FLAG_OPEN | base::File::FLAG_WRITE);
-  ASSERT_TRUE(file_1b.IsValid());
+  std::unique_ptr<base::File> file_1b = std::make_unique<base::File>(
+      path_1, base::File::FLAG_OPEN | base::File::FLAG_WRITE);
+  ASSERT_TRUE(file_1b->IsValid());
   file_tracker_.Register(entry.get(), SimpleFileTracker::SubFile::FILE_1,
-                         std::move(file_1));
+                         std::move(file_1b));
   file_tracker_.Close(entry.get(), SimpleFileTracker::SubFile::FILE_0);
   file_tracker_.Close(entry.get(), SimpleFileTracker::SubFile::FILE_1);
   EXPECT_TRUE(file_tracker_.IsEmptyForTesting());
@@ -248,5 +264,117 @@ TEST_F(SimpleFileTrackerTest, Doom) {
   file_tracker_.Close(entry1.get(), SimpleFileTracker::SubFile::FILE_0);
   file_tracker_.Close(entry2.get(), SimpleFileTracker::SubFile::FILE_0);
 }
+
+TEST_F(SimpleFileTrackerTest, OverLimit) {
+  base::HistogramTester histogram_tester;
+
+  const int kEntries = 10;  // want more than FD limit in fixture.
+  std::vector<SyncEntryPointer> entries;
+  std::vector<base::FilePath> names;
+  for (int i = 0; i < kEntries; ++i) {
+    SyncEntryPointer entry = MakeSyncEntry(i);
+    base::FilePath name =
+        entry->GetFilenameForSubfile(SimpleFileTracker::SubFile::FILE_0);
+    std::unique_ptr<base::File> file = std::make_unique<base::File>(
+        name, base::File::FLAG_CREATE | base::File::FLAG_WRITE |
+                  base::File::FLAG_READ);
+    ASSERT_TRUE(file->IsValid());
+    file_tracker_.Register(entry.get(), SimpleFileTracker::SubFile::FILE_0,
+                           std::move(file));
+    entries.push_back(std::move(entry));
+    names.push_back(name);
+  }
+
+  histogram_tester.ExpectBucketCount("SimpleCache.FileDescriptorLimiterAction",
+                                     disk_cache::FD_LIMIT_CLOSE_FILE,
+                                     kEntries - kFileLimit);
+  histogram_tester.ExpectBucketCount("SimpleCache.FileDescriptorLimiterAction",
+                                     disk_cache::FD_LIMIT_REOPEN_FILE, 0);
+  histogram_tester.ExpectBucketCount("SimpleCache.FileDescriptorLimiterAction",
+                                     disk_cache::FD_LIMIT_FAIL_REOPEN_FILE, 0);
+
+  // Grab the last one; we will hold it open till the end of the test. It's
+  // still open, so no change in stats after.
+  SimpleFileTracker::FileHandle borrow_last = file_tracker_.Acquire(
+      entries[kEntries - 1].get(), SimpleFileTracker::SubFile::FILE_0);
+  EXPECT_EQ(1, borrow_last->Write(0, "L", 1));
+
+  histogram_tester.ExpectBucketCount("SimpleCache.FileDescriptorLimiterAction",
+                                     disk_cache::FD_LIMIT_CLOSE_FILE,
+                                     kEntries - kFileLimit);
+  histogram_tester.ExpectBucketCount("SimpleCache.FileDescriptorLimiterAction",
+                                     disk_cache::FD_LIMIT_REOPEN_FILE, 0);
+  histogram_tester.ExpectBucketCount("SimpleCache.FileDescriptorLimiterAction",
+                                     disk_cache::FD_LIMIT_FAIL_REOPEN_FILE, 0);
+
+  // Delete file for [2], to cause error on its re-open.
+  EXPECT_TRUE(base::DeleteFile(names[2], false)) << names[2];
+
+  // Reacquire all the other files.
+  for (int i = 0; i < kEntries - 1; ++i) {
+    SimpleFileTracker::FileHandle borrow = file_tracker_.Acquire(
+        entries[i].get(), SimpleFileTracker::SubFile::FILE_0);
+    if (i != 2) {
+      EXPECT_TRUE(borrow.IsOK());
+      char c = static_cast<char>(i);
+      EXPECT_EQ(1, borrow->Write(0, &c, 1));
+    } else {
+      EXPECT_FALSE(borrow.IsOK());
+    }
+  }
+
+  histogram_tester.ExpectBucketCount("SimpleCache.FileDescriptorLimiterAction",
+                                     disk_cache::FD_LIMIT_CLOSE_FILE,
+                                     kEntries - kFileLimit + kEntries - 2);
+  histogram_tester.ExpectBucketCount("SimpleCache.FileDescriptorLimiterAction",
+                                     disk_cache::FD_LIMIT_REOPEN_FILE,
+                                     kEntries - 2);
+  histogram_tester.ExpectBucketCount("SimpleCache.FileDescriptorLimiterAction",
+                                     disk_cache::FD_LIMIT_FAIL_REOPEN_FILE, 1);
+
+  // Doom file for [1].
+  SimpleFileTracker::EntryFileKey key = entries[1]->entry_file_key();
+  file_tracker_.Doom(entries[1].get(), &key);
+  base::FilePath old_path = names[1];
+  UpdateEntryFileKey(entries[1].get(), key);
+  base::FilePath new_path =
+      entries[1]->GetFilenameForSubfile(SimpleFileTracker::SubFile::FILE_0);
+  EXPECT_TRUE(base::StartsWith(new_path.BaseName().MaybeAsASCII(), "todelete_",
+                               base::CompareCase::SENSITIVE));
+  EXPECT_TRUE(base::Move(old_path, new_path));
+
+  // Now re-acquire everything again; this time reading.
+  for (int i = 0; i < kEntries - 1; ++i) {
+    SimpleFileTracker::FileHandle borrow = file_tracker_.Acquire(
+        entries[i].get(), SimpleFileTracker::SubFile::FILE_0);
+    char read;
+    char expected = static_cast<char>(i);
+    if (i != 2) {
+      EXPECT_TRUE(borrow.IsOK());
+      EXPECT_EQ(1, borrow->Read(0, &read, 1));
+      EXPECT_EQ(expected, read);
+    } else {
+      EXPECT_FALSE(borrow.IsOK());
+    }
+  }
+
+  histogram_tester.ExpectBucketCount(
+      "SimpleCache.FileDescriptorLimiterAction",
+      disk_cache::FD_LIMIT_CLOSE_FILE,
+      kEntries - kFileLimit + 2 * (kEntries - 2));
+  histogram_tester.ExpectBucketCount("SimpleCache.FileDescriptorLimiterAction",
+                                     disk_cache::FD_LIMIT_REOPEN_FILE,
+                                     2 * (kEntries - 2));
+  histogram_tester.ExpectBucketCount("SimpleCache.FileDescriptorLimiterAction",
+                                     disk_cache::FD_LIMIT_FAIL_REOPEN_FILE, 2);
+
+  // Read from the last one, too. Should still be fine.
+  char read;
+  EXPECT_EQ(1, borrow_last->Read(0, &read, 1));
+  EXPECT_EQ('L', read);
+
+  for (const auto& entry : entries)
+    file_tracker_.Close(entry.get(), SimpleFileTracker::SubFile::FILE_0);
+};
 
 }  // namespace disk_cache
