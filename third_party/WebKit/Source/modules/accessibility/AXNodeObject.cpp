e661c098caa6686aa79d97bc72a967dfdabae9b8
Aaron Leventhal <aleventhal@chromium.org>
2018-01-31 04:40:23 +0000

    Table with role presentation should expose cells as generic containers
    When screen readers traverse a <table role="presentation">, they need
    generic container objects in place of the cells, to help them understand
    and present formatted content. Specifically, this helps present the text
    without jamming it together, as well as providing line breaks.
    
    Bug: 787664
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I53b691f0ccae3f4090ca0f8ecfcbeb622bd3dfeb
    Reviewed-on: https://chromium-review.googlesource.com/887599
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533202}
    Former-commit-id: 112c7d585aab4bffd99773897731d3df996cab4e

--- third_party/WebKit/Source/modules/accessibility/AXNodeObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXNodeObject.cpp
@@ -183,41 +183,6 @@ static bool IsListElement(Node* node) {
          IsHTMLDListElement(*node);
 }
 
-static bool IsPresentationalInTable(AXObject* parent,
-                                    HTMLElement* current_element) {
-  if (!current_element)
-    return false;
-
-  Node* parent_node = parent->GetNode();
-  if (!parent_node || !parent_node->IsHTMLElement())
-    return false;
-
-  // AXTable determines the role as checking isTableXXX.
-  // If Table has explicit role including presentation, AXTable doesn't assign
-  // implicit Role to a whole Table. That's why we should check it based on
-  // node.
-  // Normal Table Tree is that
-  // cell(its role)-> tr(tr role)-> tfoot, tbody, thead(ignored role) ->
-  //     table(table role).
-  // If table has presentation role, it will be like
-  // cell(group)-> tr(unknown) -> tfoot, tbody, thead(ignored) ->
-  //     table(presentation).
-  if (IsHTMLTableCellElement(*current_element) &&
-      IsHTMLTableRowElement(*parent_node))
-    return parent->HasInheritedPresentationalRole();
-
-  if (IsHTMLTableRowElement(*current_element) &&
-      IsHTMLTableSectionElement(ToHTMLElement(*parent_node))) {
-    // Because TableSections have ignored role, presentation should be checked
-    // with its parent node.
-    AXObject* table_object = parent->ParentObject();
-    Node* table_node = table_object ? table_object->GetNode() : nullptr;
-    return IsHTMLTableElement(table_node) &&
-           table_object->HasInheritedPresentationalRole();
-  }
-  return false;
-}
-
 static bool IsRequiredOwnedElement(AXObject* parent,
                                    AccessibilityRole current_role,
                                    HTMLElement* current_element) {
@@ -270,17 +235,9 @@ const AXObject* AXNodeObject::InheritsPresentationalRoleFrom() const {
   HTMLElement* element = nullptr;
   if (GetNode() && GetNode()->IsHTMLElement())
     element = ToHTMLElement(GetNode());
-  if (!parent->HasInheritedPresentationalRole()) {
-    if (!GetLayoutObject() || !GetLayoutObject()->IsBoxModelObject())
-      return nullptr;
+  if (!parent->HasInheritedPresentationalRole())
+    return nullptr;
 
-    LayoutBoxModelObject* css_box = ToLayoutBoxModelObject(GetLayoutObject());
-    if (!css_box->IsTableCell() && !css_box->IsTableRow())
-      return nullptr;
-
-    if (!IsPresentationalInTable(parent, element))
-      return nullptr;
-  }
   // ARIA spec says that when a parent object is presentational and this object
   // is a required owned element of that parent, then this object is also
   // presentational.
