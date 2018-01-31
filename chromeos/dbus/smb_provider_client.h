d68bcf9df200d0e9dfe7d1cc78403b7316830a66
Allen Vicencio <allenvic@chromium.org>
2018-01-31 01:55:21 +0000

    Add Truncate to SmbProviderClient
    Adds Truncate to SmbProviderClient and FakeSmbProviderClient
    
    CQ-DEPEND=CL:879387
    
    Bug: chromium:757625
    Change-Id: Iff2f62efc059a41ad4d15c3cbb18bafac277c690
    Reviewed-on: https://chromium-review.googlesource.com/879566
    Commit-Queue: Allen Vicencio <allenvic@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533156}
    Former-commit-id: 58881f7e49ba146916ad3df75b74bcca7bd07801

--- chromeos/dbus/smb_provider_client.h
+++ chromeos/dbus/smb_provider_client.h
@@ -102,6 +102,13 @@ class CHROMEOS_EXPORT SmbProviderClient : public DBusClient {
                           const base::FilePath& file_path,
                           StatusCallback callback) = 0;
 
+  // Calls Truncate. Using the corresponding mount |mount_id|, this truncates
+  // the file in |file_path| to the desired |length|.
+  virtual void Truncate(int32_t mount_id,
+                        const base::FilePath& file_path,
+                        int64_t length,
+                        StatusCallback callback) = 0;
+
  protected:
   // Create() should be used instead.
   SmbProviderClient();
