b982e485825656a83cc9f37d391fcbd235511841
Xianzhu Wang <wangxianzhu@chromium.org>
2018-01-31 22:09:44 +0000

    [PE] Correct fragment paint offset under non-self-painting paint_offset_root
    Bug: 807314
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I635c0d6303894f9aa49197edcbfe6de8990a68b6
    Reviewed-on: https://chromium-review.googlesource.com/894699
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533434}
    Former-commit-id: 8c7ac7ada27c7d4e0d4109d70c18ff78827a6285

--- third_party/WebKit/Source/core/paint/PaintPropertyTreeBuilder.cpp
+++ third_party/WebKit/Source/core/paint/PaintPropertyTreeBuilder.cpp
@@ -954,7 +954,8 @@ static LayoutPoint VisualOffsetFromPaintOffsetRoot(
   const LayoutObject* paint_offset_root = context.current.paint_offset_root;
   PaintLayer* painting_layer = paint_offset_root->PaintingLayer();
   LayoutPoint result = child->VisualOffsetFromAncestor(painting_layer);
-  if (!paint_offset_root->HasLayer()) {
+  if (!paint_offset_root->HasLayer() ||
+      ToLayoutBoxModelObject(paint_offset_root)->Layer() != painting_layer) {
     result.Move(-paint_offset_root->OffsetFromAncestorContainer(
         &painting_layer->GetLayoutObject()));
   }
