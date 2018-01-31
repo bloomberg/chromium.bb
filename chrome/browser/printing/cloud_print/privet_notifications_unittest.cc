be2f4d119bd2ea0b2b0ea9702bc1387b95cc52f3
Evan Stade <estade@chromium.org>
2018-01-31 19:53:30 +0000

    Update Privet Notifications unit test to use NotificationDisplayService.
    Bug: 783018
    Change-Id: I25cad9fae605346d13821fbd6afe300ce2abe9a8
    Reviewed-on: https://chromium-review.googlesource.com/889438
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533365}
    Former-commit-id: 5ca1233e21033c1be3a6201ebf07a19868c28be9

--- chrome/browser/printing/cloud_print/privet_notifications_unittest.cc
+++ chrome/browser/printing/cloud_print/privet_notifications_unittest.cc
@@ -9,6 +9,7 @@
 #include "base/memory/ptr_util.h"
 #include "base/message_loop/message_loop.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "chrome/browser/notifications/notification_display_service_tester.h"
 #include "chrome/browser/notifications/notification_test_util.h"
 #include "chrome/browser/printing/cloud_print/privet_http_asynchronous_factory.h"
 #include "chrome/browser/printing/cloud_print/privet_http_impl.h"
@@ -259,6 +260,8 @@ class PrivetNotificationsNotificationTest : public testing::Test {
         TestingBrowserProcess::GetGlobal());
     ASSERT_TRUE(profile_manager_->SetUp());
     profile_ = profile_manager_->CreateTestingProfile("test-user");
+    display_service_ =
+        std::make_unique<NotificationDisplayServiceTester>(profile_);
 
     TestingBrowserProcess::GetGlobal()->SetNotificationUIManager(
         base::MakeUnique<StubNotificationUIManager>());
@@ -269,21 +272,17 @@ class PrivetNotificationsNotificationTest : public testing::Test {
     testing::Test::TearDown();
   }
 
- protected:
-  StubNotificationUIManager* ui_manager() const {
-    return static_cast<StubNotificationUIManager*>(
-        TestingBrowserProcess::GetGlobal()->notification_ui_manager());
-  }
-
   Profile* profile() { return profile_; }
 
- private:
   // The thread bundle must be first so it is destroyed last.
   content::TestBrowserThreadBundle thread_bundle_;
+
+  std::unique_ptr<NotificationDisplayServiceTester> display_service_;
 
   std::unique_ptr<TestingProfileManager> profile_manager_;
   Profile* profile_;
 
+ private:
   DISALLOW_COPY_AND_ASSIGN(PrivetNotificationsNotificationTest);
 };
 
@@ -293,14 +292,20 @@ TEST_F(PrivetNotificationsNotificationTest, AddToCloudPrint) {
   // The notification is added asynchronously.
   base::RunLoop().RunUntilIdle();
 
-  ASSERT_EQ(1U, ui_manager()->GetNotificationCount());
-  const auto& notification = ui_manager()->GetNotificationAt(0);
-  notification.ButtonClick(0 /* add */);
+  auto notifications = display_service_->GetDisplayedNotificationsForType(
+      NotificationHandler::Type::TRANSIENT);
+  ASSERT_EQ(1U, notifications.size());
+  display_service_->SimulateClick(NotificationHandler::Type::TRANSIENT,
+                                  notifications[0].id(), 0 /* add */,
+                                  base::nullopt);
 
   EXPECT_EQ("chrome://devices/", service.open_tab_url().spec());
   EXPECT_EQ(1U, service.open_tab_count());
   EXPECT_EQ(0U, service.disable_notifications_count());
-  EXPECT_EQ(0U, ui_manager()->GetNotificationCount());
+  EXPECT_EQ(0U, display_service_
+                    ->GetDisplayedNotificationsForType(
+                        NotificationHandler::Type::TRANSIENT)
+                    .size());
 }
 
 TEST_F(PrivetNotificationsNotificationTest, DontShowAgain) {
@@ -309,14 +314,20 @@ TEST_F(PrivetNotificationsNotificationTest, DontShowAgain) {
   // The notification is added asynchronously.
   base::RunLoop().RunUntilIdle();
 
-  ASSERT_EQ(1U, ui_manager()->GetNotificationCount());
-  const auto& notification = ui_manager()->GetNotificationAt(0);
-  notification.ButtonClick(1 /* don't show again */);
+  auto notifications = display_service_->GetDisplayedNotificationsForType(
+      NotificationHandler::Type::TRANSIENT);
+  ASSERT_EQ(1U, notifications.size());
+  display_service_->SimulateClick(NotificationHandler::Type::TRANSIENT,
+                                  notifications[0].id(),
+                                  1 /* don't show again */, base::nullopt);
 
   EXPECT_EQ("", service.open_tab_url().spec());
   EXPECT_EQ(0U, service.open_tab_count());
   EXPECT_EQ(1U, service.disable_notifications_count());
-  EXPECT_EQ(0U, ui_manager()->GetNotificationCount());
+  EXPECT_EQ(0U, display_service_
+                    ->GetDisplayedNotificationsForType(
+                        NotificationHandler::Type::TRANSIENT)
+                    .size());
 }
 
 }  // namespace
