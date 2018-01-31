2684dddc03dc7e5177d9a77733aa0de12c236b39
Thiemo Nagel <tnagel@chromium.org>
2018-01-31 17:16:02 +0000

    webview: fold function which is only used once
    No functional change.
    
    Bug: None
    Change-Id: Ie077b642c13f5b4b88f7940250912c6b032285cb
    Reviewed-on: https://chromium-review.googlesource.com/873632
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Thiemo Nagel <tnagel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533304}
    Former-commit-id: 98ede601cb22dbd3b1c957fc18650958cbf53c1f

--- android_webview/common/aw_content_client.h
+++ android_webview/common/aw_content_client.h
@@ -17,8 +17,6 @@ namespace android_webview {
 
 std::string GetProduct();
 std::string GetUserAgent();
-// extra text to be put into the OS section of the user agent text
-std::string GetExtraOSUserAgentInfo();
 
 class AwContentClient : public content::ContentClient {
  public:
