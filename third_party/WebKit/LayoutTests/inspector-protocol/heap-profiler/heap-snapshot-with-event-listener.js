baec0ce616b466927d193297c06300da728e846f
Tommy Steimel <steimel@chromium.org>
2018-02-01 17:54:06 +0000

    Revert "Capture all DOM nodes reachable from V8 in heap snapshot."
    This reverts commit 273d66e02109140e199ae8671dcc8e4e5e676940 [formerly 75edf20e769f39825d6e17fab52e16589350e7f2].
    
    Reason for revert: inspector-protocol/heap-profiler/heap-snapshot-with-iframe.js is failing on Mac bots. 
    
    Example:
    https://ci.chromium.org/buildbot/chromium.webkit/WebKit%20Mac10.12/10410
    
    diff from failure:
    https://isolateserver.appspot.com/browse?namespace=default-gzip&digest=c317416b36978b21d32382d0c64469a05a334287&as=heap-snapshot-with-iframe-diff.txt
    
    Original change's description:
    > Capture all DOM nodes reachable from V8 in heap snapshot.
    > 
    > This patch uses the new EmbedderGraph API to provide the heap snapshot
    > generator with all DOM nodes and references reachable from V8.
    > 
    > Bug: 749490
    > Change-Id: I26750a8b80dab4f88508e504b431d976a598c6c3
    > Reviewed-on: https://chromium-review.googlesource.com/890260
    > Commit-Queue: Ulan Degenbaev <ulan@chromium.org>
    > Reviewed-by: Alexei Filippov <alph@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#533664}
    
    TBR=dcheng@chromium.org,ulan@chromium.org,alph@chromium.org,haraken@chromium.org,mlippautz@chromium.org
    
    Change-Id: I8fc35e1c2638bb8ef77b3b19053a7bcbe74a347a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 749490
    Reviewed-on: https://chromium-review.googlesource.com/896686
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533725}
    Former-commit-id: 0c2648420c80bff20154a6abd2d267a9a53cc6ad

--- third_party/WebKit/LayoutTests/inspector-protocol/heap-profiler/heap-snapshot-with-event-listener.js
+++ third_party/WebKit/LayoutTests/inspector-protocol/heap-profiler/heap-snapshot-with-event-listener.js
@@ -1,6 +1,6 @@
 (async function(testRunner) {
   var {page, session, dp} = await testRunner.startBlank(
-      `Test retaining path for an event listener.`);
+      `Test that all nodes from the detached DOM tree will get into one group in the heap snapshot. Bug 107819.`);
 
   await session.evaluate(`
     function addEventListenerAndRunTest() {
@@ -26,13 +26,24 @@
   if (node)
     testRunner.log('SUCCESS: found ' + node.name());
   else
-    return testRunner.fail('cannot find myEventListener node');
+    return testRunner.fail('cannot find detached DOM trees root');
 
-  var retainers = helper.firstRetainingPath(node).map(node => node.name());
-  // Limit to the retainers until the Window object to keep the test robust
-  // against root node name changes.
-  retainers = retainers.slice(0, retainers.indexOf('Window'));
-  var actual = retainers.join(', ');
-  testRunner.log(`SUCCESS: retaining path = [${actual}]`);
+  var nativeRetainers = 0;
+  for (var iter = node.retainers(); iter.hasNext(); iter.next()) {
+    var retainingEdge = iter.retainer;
+    if (retainingEdge.isInternal() && retainingEdge.name() === 'native') {
+      if (++nativeRetainers === 1) {
+        var retainerName = retainingEdge.node().name();
+        if (retainerName === 'HTMLBodyElement')
+          testRunner.log('SUCCESS: found link from HTMLBodyElement to ' + node.name());
+        else
+          return testRunner.fail('unexpected retainer of ' + node.name() + ': ' + retainerName);
+      } else
+        return testRunner.fail('too many retainers of ' + node.name());
+    } else if (!retainingEdge.isWeak())
+      return testRunner.fail('unexpected retaining edge of ' + node.name() + ' type: ' + retainingEdge.type() + ', name: ' + retainingEdge.name());
+  }
+  if (!nativeRetainers)
+    return testRunner.fail('cannot find HTMLBodyElement among retainers of ' + node.name());
   testRunner.completeTest();
 })
