ec3b66e1f727284d4e99cfff8181d5dfaa5237b4
Friedrich Horschig <fhorschig@chromium.org>
2018-02-01 16:24:04 +0000

    Reland "Remove influence of group name on NTPPopularSites studies"
    This is a reland of 8309e2e7dc4f77f052aca360619634302144fea3 [formerly 1a544c783e00bf6adb51be7590fee71f5c4a4185].
    
    Original change's description:
    > Remove influence of group name on NTPPopularSites studies
    > 
    > Recently, a misnamed study (see deleted test config) caused the feature
    > to fall back to the default popular sites. This isn't a big problem but
    > when it was finally named correctly, the names of the groups disabled
    > all popular sites.
    > 
    > Popular sites should not be so easy to disable. Therefore, remove the
    > group name condition.
    > 
    > A forced cmd line flag should cover all possible cases and make the
    > intention very clear. This is already in place and remains untouched.
    > 
    > Bug: 794329
    > Change-Id: I71458d41de6d0e0d5f0d8fcde1b636b0fe18618c
    > Reviewed-on: https://chromium-review.googlesource.com/827072
    > Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    > Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#528301}
    
    Bug: 794329
    Change-Id: I0c391668095cd66be012f0d1d483ff0970cbb018
    Reviewed-on: https://chromium-review.googlesource.com/888560
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533690}
    Former-commit-id: ffc93ab8afc61f4bfea18b81c8b8cc4290dc238a

--- components/ntp_tiles/switches.cc
+++ components/ntp_tiles/switches.cc
@@ -12,11 +12,5 @@ namespace switches {
 const char kEnableNTPSearchEngineCountryDetection[] =
     "enable-ntp-search-engine-country-detection";
 
-// Enables showing popular sites on the NTP.
-const char kEnableNTPPopularSites[] = "enable-ntp-popular-sites";
-
-// Disables showing popular sites on the NTP.
-const char kDisableNTPPopularSites[] = "disable-ntp-popular-sites";
-
 }  // namespace switches
 }  // namespace ntp_tiles
