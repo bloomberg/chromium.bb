485bdb720fce31a8665a8a4b5bb5401539316a87
Meredith Lane <meredithl@google.com>
2018-02-01 22:16:45 +0000

    Implement string accessors for ComputedAccessibleNode.
    Adds the IDL code and accessor methods into ComputedAccessibleNode for
    the intersection of properties between AccessibleNode and
    AXStringAttribute. These accessors will return null strings to the
    window in a few cases:
       - when the AXNode for a given matching AXID is not in the tree,
       - when the string property has not been set.
    
    Currently, the accessors are rudimentary and require an enum
    translation, although this may change in future in favour of an
    individual hard coded method on the AomContentAXTree.
    
    BUG=792783
    
    Change-Id: Icd9cac2cb8486d549932f400b0aeeae569a1f712
    Reviewed-on: https://chromium-review.googlesource.com/883863
    Commit-Queue: Meredith Lane <meredithl@google.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533834}
    Former-commit-id: b0fc027b8deefa3111b33ab3e1efcdeef5379ed0

--- third_party/WebKit/public/platform/WebComputedAXTree.h
+++ third_party/WebKit/public/platform/WebComputedAXTree.h
@@ -22,20 +22,39 @@ enum WebAOMIntAttribute {
   AOM_ATTR_SET_SIZE,
 };
 
+enum WebAOMStringAttribute {
+  AOM_STRING_ATTRIBUTE_NONE,
+  AOM_ATTR_KEY_SHORTCUTS,
+  AOM_ATTR_NAME,
+  AOM_ATTR_PLACEHOLDER,
+  AOM_ATTR_ROLE_DESCRIPTION,
+  AOM_ATTR_VALUE_TEXT,
+
+};
+
 class WebComputedAXTree {
  public:
   virtual ~WebComputedAXTree() {}
 
+  // Compute the accessibility tree as a snapshot. Returns true if the snapshot
+  // could be unserialized into a tree successfully.
   virtual bool ComputeAccessibilityTree() = 0;
-  virtual WebString GetNameForAXNode(int32_t axID) = 0;
-  virtual WebString GetRoleForAXNode(int32_t axID) = 0;
+
+  // Accessibility Property Accessors.
 
   // Get the specified attribute for a given AXID, returning true if the
-  // node exists in the tree and contains the attribute. Stores the result in
-  // |out_param|.
+  // node exists in the tree and contains the attribute, and stores the result
+  // in |out_param|.
   virtual bool GetIntAttributeForAXNode(int32_t axID,
                                         WebAOMIntAttribute,
                                         int32_t* out_param) = 0;
+  virtual bool GetStringAttributeForAXNode(int32_t,
+                                           blink::WebAOMStringAttribute,
+                                           blink::WebString* out_param) = 0;
+
+  // The role is stored seperately from other attributes in the AXNode, so we
+  // expose a seperate method for retrieving this.
+  virtual bool GetRoleForAXNode(int32_t axID, blink::WebString* out_param) = 0;
 };
 
 }  // namespace blink
