d5f3e36a858bcb7b4c485e4c57bbbb22cd152122
Bill Budge <bbudge@chromium.org>
2018-02-01 19:46:36 +0000

    [gin] Implement v8::Platform::GetPageAllocator.
    - Implements a v8::PageAllocator in terms of base page allocator functions.
      This will cause V8 to use Chromium's page allocator and address space
      randomization.
    
    Bug: chromium:756050
    Change-Id: Id076e599e7d5f09eddc78888245fc6bf879b1236
    Reviewed-on: https://chromium-review.googlesource.com/896959
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Bill Budge <bbudge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533782}
    Former-commit-id: 1cc19640473a99e99ef5d4223a7470765a3616ef

--- gin/public/v8_platform.h
+++ gin/public/v8_platform.h
@@ -8,6 +8,7 @@
 #include "base/compiler_specific.h"
 #include "base/lazy_instance.h"
 #include "base/macros.h"
+#include "gin/features.h"
 #include "gin/gin_export.h"
 #include "v8/include/v8-platform.h"
 
@@ -18,8 +19,12 @@ class GIN_EXPORT V8Platform : public v8::Platform {
  public:
   static V8Platform* Get();
 
-  // v8::Platform implementation.
+// v8::Platform implementation.
+// Some configurations do not use page_allocator.
+#if BUILDFLAG(USE_PARTITION_ALLOC)
+  v8::PageAllocator* GetPageAllocator() override;
   void OnCriticalMemoryPressure() override;
+#endif
   std::shared_ptr<v8::TaskRunner> GetForegroundTaskRunner(
       v8::Isolate*) override;
   std::shared_ptr<v8::TaskRunner> GetBackgroundTaskRunner(
