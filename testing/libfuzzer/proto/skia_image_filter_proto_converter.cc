3c5faf42929974449732b6e309fd1f77df7be28b
Nico Weber <thakis@chromium.org>
2018-02-01 15:05:33 +0000

    Fix -Wimplicit-fallthrough build after https://chromium-review.googlesource.com/c/chromium/src/+/885085
    TBR=metzman
    
    Bug: 177475
    Change-Id: I331bc2d1bac790133cbba8ba61ab02e04243a3a6
    Reviewed-on: https://chromium-review.googlesource.com/897745
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#533661}
    Former-commit-id: 776e607303a6e1a381ef896361e919c445610523

--- testing/libfuzzer/proto/skia_image_filter_proto_converter.cc
+++ testing/libfuzzer/proto/skia_image_filter_proto_converter.cc
@@ -1302,7 +1302,7 @@ void Converter::Visit(const PathRef& path_ref) {
           break;
         case ValidVerb::kConic_Verb:
           num_conics += 1;
-        // fall-through
+          FALLTHROUGH;
         case ValidVerb::kQuad_Verb:
           num_points += 2;
           break;
