19ab569f499f8d1b9ef922fdd65407af80da1345
Yuta Kitamura <yutak@chromium.org>
2018-02-02 08:56:54 +0000

    Revert "[css-typed-om] Refactor CSSStyleImageValue to prepare for gradients."
    This reverts commit c00751d5dce86c2d9bad6a3fc136e32d8ae56e28 [formerly 6d10aad7a6f28294851e37f6ab7f0b0ac5086508].
    
    Reason for revert: Caused MSAN bot failure.
    
    https://ci.chromium.org/buildbot/chromium.memory/Linux%20MSan%20Tests/7639
    
    Original change's description:
    > [css-typed-om] Refactor CSSStyleImageValue to prepare for gradients.
    > 
    > Currently CSSStyleImageValue implements the CSSImageValue class in
    > Typed OM. CSSImageValue can be anything that's an <image> i.e. a <url>,
    > <gradient>, or <cross-fade>.
    > 
    > However, the current implementation of CSSStyleImageValue only really
    > works for <url>s. This patch moves <url> specific logic into
    > CSSURLImageValue. This prepares for the implementation of <gradient> and
    > <cross-fade>, which will be subclasses of CSSStyleImageValue.
    > 
    > We also do some cleanup work (e.g. making things const, moving code in
    > header to cpp).
    > 
    > Bug: 803680
    > Change-Id: I848b3eab5dbbcb9bbe7f0aea0d377497f63bfda3
    > Reviewed-on: https://chromium-review.googlesource.com/882786
    > Commit-Queue: Darren Shen <shend@chromium.org>
    > Reviewed-by: nainar <nainar@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#533949}
    
    TBR=nainar@chromium.org,shend@chromium.org
    
    Change-Id: I9aedca3ac9e3ecb07ed283447febede0628d7aeb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 803680
    Reviewed-on: https://chromium-review.googlesource.com/896671
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534002}
    Former-commit-id: 1f977216838ab364205dcd288b0b3efc0de1b01d

--- third_party/WebKit/Source/core/css/cssom/CSSStyleImageValue.cpp
+++ third_party/WebKit/Source/core/css/cssom/CSSStyleImageValue.cpp
@@ -7,36 +7,49 @@
 namespace blink {
 
 double CSSStyleImageValue::intrinsicWidth(bool& is_null) const {
-  const WTF::Optional<IntSize> size = IntrinsicSize();
-  if (!size) {
-    is_null = true;
+  is_null = Status() != ResourceStatus::kCached;
+  if (is_null)
     return 0;
-  }
-  return size.value().Width();
+  return ImageSize().Width();
 }
 
 double CSSStyleImageValue::intrinsicHeight(bool& is_null) const {
-  const WTF::Optional<IntSize> size = IntrinsicSize();
-  if (!size) {
-    is_null = true;
+  is_null = Status() != ResourceStatus::kCached;
+  if (is_null)
     return 0;
-  }
-  return size.value().Height();
+  return ImageSize().Height();
 }
 
-double CSSStyleImageValue::intrinsicRatio(bool& is_null) const {
-  const WTF::Optional<IntSize> size = IntrinsicSize();
-  if (!size || size.value().Height() == 0) {
+double CSSStyleImageValue::intrinsicRatio(bool& is_null) {
+  is_null = Status() != ResourceStatus::kCached;
+  if (is_null || intrinsicHeight(is_null) == 0) {
     is_null = true;
     return 0;
   }
-  return static_cast<double>(size.value().Width()) / size.value().Height();
+  return intrinsicWidth(is_null) / intrinsicHeight(is_null);
 }
 
 FloatSize CSSStyleImageValue::ElementSize(
     const FloatSize& default_object_size) const {
   bool not_used;
   return FloatSize(intrinsicWidth(not_used), intrinsicHeight(not_used));
+}
+
+bool CSSStyleImageValue::IsAccelerated() const {
+  return GetImage() && GetImage()->IsTextureBacked();
+}
+
+scoped_refptr<Image> CSSStyleImageValue::GetImage() const {
+  if (IsCachePending())
+    return nullptr;
+  // cachedImage can be null if image is StyleInvalidImage
+  ImageResourceContent* cached_image =
+      image_value_->CachedImage()->CachedImage();
+  if (cached_image) {
+    // getImage() returns the nullImage() if the image is not available yet
+    return cached_image->GetImage()->ImageForDefaultFrame();
+  }
+  return nullptr;
 }
 
 }  // namespace blink
--- third_party/WebKit/Source/core/css/cssom/CSSStyleImageValue.h
+++ third_party/WebKit/Source/core/css/cssom/CSSStyleImageValue.h
@@ -7,9 +7,13 @@
 
 #include "base/macros.h"
 #include "core/CoreExport.h"
+#include "core/css/CSSImageValue.h"
 #include "core/css/cssom/CSSResourceValue.h"
+#include "core/css/cssom/CSSStyleValue.h"
 #include "core/html/canvas/CanvasImageSource.h"
-#include "platform/wtf/Optional.h"
+#include "core/imagebitmap/ImageBitmapSource.h"
+#include "core/loader/resource/ImageResourceContent.h"
+#include "core/style/StyleImage.h"
 
 namespace blink {
 
@@ -22,10 +26,9 @@ class CORE_EXPORT CSSStyleImageValue : public CSSResourceValue,
  public:
   virtual ~CSSStyleImageValue() = default;
 
-  // IDL
   double intrinsicWidth(bool& is_null) const;
   double intrinsicHeight(bool& is_null) const;
-  double intrinsicRatio(bool& is_null) const;
+  double intrinsicRatio(bool& is_null);
 
   // CanvasImageSource
   bool IsCSSImageValue() const final { return true; }
@@ -34,13 +37,45 @@ class CORE_EXPORT CSSStyleImageValue : public CSSResourceValue,
     return true;
   }
   FloatSize ElementSize(const FloatSize& default_object_size) const final;
+  scoped_refptr<Image> GetSourceImageForCanvas(SourceImageStatus*,
+                                               AccelerationHint,
+                                               const FloatSize&) final {
+    return GetImage();
+  }
+  bool IsAccelerated() const override;
+
+  virtual void Trace(blink::Visitor* visitor) {
+    visitor->Trace(image_value_);
+    CSSResourceValue::Trace(visitor);
+  }
 
  protected:
-  CSSStyleImageValue() = default;
+  CSSStyleImageValue(const CSSImageValue* image_value)
+      : image_value_(image_value) {}
 
-  virtual WTF::Optional<IntSize> IntrinsicSize() const = 0;
+  virtual IntSize ImageSize() const {
+    DCHECK(!IsCachePending());
+    ImageResourceContent* resource_content =
+        image_value_->CachedImage()->CachedImage();
+    return resource_content
+               ? resource_content->IntrinsicSize(kDoNotRespectImageOrientation)
+               : IntSize(0, 0);
+  }
+
+  virtual bool IsCachePending() const { return image_value_->IsCachePending(); }
+
+  ResourceStatus Status() const override {
+    if (IsCachePending())
+      return ResourceStatus::kNotStarted;
+    return image_value_->CachedImage()->CachedImage()->GetContentStatus();
+  }
+
+  const CSSImageValue* CssImageValue() const { return image_value_.Get(); };
 
  private:
+  scoped_refptr<Image> GetImage() const;
+
+  Member<const CSSImageValue> image_value_;
   DISALLOW_COPY_AND_ASSIGN(CSSStyleImageValue);
 };
 
