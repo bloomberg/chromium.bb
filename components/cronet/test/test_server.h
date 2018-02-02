d88f2886ef9cbe966bf5d2c7214e8f67657371b9
Misha Efimov <mef@chromium.org>
2018-02-02 17:47:12 +0000

    [Cronet] Move cronet::TestServer from cronet/ios/test to cronet/test.
    - Make it available for native tests.
    - Fix lint warnings.
    
    Bug: 786559
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ib8d704d63e7eaebdc92134fc595f3f3c7a1cb79e
    Reviewed-on: https://chromium-review.googlesource.com/899522
    Reviewed-by: Andrei Kapishnikov <kapishnikov@chromium.org>
    Commit-Queue: Misha Efimov <mef@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534087}
    Former-commit-id: 475d980791b916b18507acae2bcdaf21e8f271cd

--- components/cronet/test/test_server.h
+++ components/cronet/test/test_server.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_CRONET_IOS_TEST_TEST_SERVER_H_
-#define COMPONENTS_CRONET_IOS_TEST_TEST_SERVER_H_
+#ifndef COMPONENTS_CRONET_TEST_TEST_SERVER_H_
+#define COMPONENTS_CRONET_TEST_TEST_SERVER_H_
 
 #include <string>
 
@@ -30,11 +30,11 @@ class TestServer {
   static std::string EchoRequestBodyURL();
   // Prepares response and returns URL which respond with |data_size| of bytes
   // in response body.
-  static std::string PrepareBigDataURL(long data_size);
+  static std::string PrepareBigDataURL(size_t data_size);
   // Releases response created by PrepareBigDataURL().
   static void ReleaseBigDataURL();
 };
 
 }  // namespace cronet
 
-#endif  // COMPONENTS_CRONET_IOS_TEST_TEST_SERVER_H_
+#endif  // COMPONENTS_CRONET_TEST_TEST_SERVER_H_
