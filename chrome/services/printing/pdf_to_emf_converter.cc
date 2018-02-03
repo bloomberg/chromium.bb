a5f4e8964f1173ed98fb486ea520ee54179765ef
Daniel Cheng <dcheng@chromium.org>
2018-02-03 05:42:15 +0000

    Actually erase dead InterfacePtrs in pdf_to_emf_converter.cc
    Despite the name, std::remove_if() doesn't actually remove elements from
    a container: it merely shuffles the elements so the ones that fail the
    predicate are at the end and can be easily erased.
    
    Bug: 805881
    Change-Id: I3bb4894af565784eb05c8f870415d38393ec69a4
    Reviewed-on: https://chromium-review.googlesource.com/900136
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534262}
    Former-commit-id: cb5c4ce87f6cdbebdb43687d519aefb5c5f25bd9

--- chrome/services/printing/pdf_to_emf_converter.cc
+++ chrome/services/printing/pdf_to_emf_converter.cc
@@ -7,6 +7,7 @@
 #include <algorithm>
 
 #include "base/lazy_instance.h"
+#include "base/stl_util.h"
 #include "mojo/public/cpp/system/platform_handle.h"
 #include "pdf/pdf.h"
 #include "printing/emf_win.h"
@@ -41,11 +42,10 @@ void OnConvertedClientDisconnected() {
   // We have no direct way of tracking which PdfToEmfConverterClientPtr got
   // disconnected as it is a movable type, short of using a wrapper.
   // Just traverse the list of clients and remove the ones that are not bound.
-  std::remove_if(g_converter_clients.Get().begin(),
-                 g_converter_clients.Get().end(),
-                 [](const mojom::PdfToEmfConverterClientPtr& client) {
-                   return !client.is_bound();
-                 });
+  base::EraseIf(g_converter_clients.Get(),
+                [](const mojom::PdfToEmfConverterClientPtr& client) {
+                  return !client.is_bound();
+                });
 }
 
 void RegisterConverterClient(mojom::PdfToEmfConverterClientPtr client) {
