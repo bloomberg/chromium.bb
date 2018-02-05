8936311b2cd60f929a0fac20308458562b1586c7
Nico Weber <thakis@chromium.org>
2018-02-05 20:36:13 +0000

    Fix -Wimplicit-fallthrough warnings for Chrome OS.
    This CL was uploaded by git cl split.
    
    R=alemate@chromium.org
    
    Bug: 177475
    Change-Id: I4b73b5ce51090fb9e2dd40adcf4117c95ee5a37a
    Reviewed-on: https://chromium-review.googlesource.com/899448
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534487}
    Former-commit-id: 8bcf4e70ea9345cb4750759d381e87dd5a3d3ba1

--- chromeos/login/auth/cryptohome_authenticator.cc
+++ chromeos/login/auth/cryptohome_authenticator.cc
@@ -836,6 +836,7 @@ void CryptohomeAuthenticator::Resolve() {
       break;
     case CREATE_NEW:
       create_if_nonexistent = true;
+      FALLTHROUGH;
     case RECOVER_MOUNT:
       current_state_->ResetCryptohomeStatus();
       StartMount(current_state_->AsWeakPtr(),
@@ -854,10 +855,10 @@ void CryptohomeAuthenticator::Resolve() {
       break;
     case OFFLINE_LOGIN:
       VLOG(2) << "Offline login";
-    // Fall through.
+      FALLTHROUGH;
     case UNLOCK:
       VLOG(2) << "Unlock";
-    // Fall through.
+      FALLTHROUGH;
     case ONLINE_LOGIN:
       VLOG(2) << "Online login";
       task_runner_->PostTask(
