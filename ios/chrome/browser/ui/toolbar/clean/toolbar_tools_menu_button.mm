29b5e2e3e0c714bf9dd68429278a9e8533b950ac
gambard <gambard@chromium.org>
2018-02-05 23:52:40 +0000

    Update the tools menu button animation
    This CL changes the tools menu button such as it is correctly
    positionned and the animation is occurring in the right direction in
    RTL.
    
    Bug: 807285
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I9cfcd86931a22f4b34e6d8a3dfc575afd50f95de
    Reviewed-on: https://chromium-review.googlesource.com/899364
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534538}
    Former-commit-id: 61a58b4b756980a978aea64a0ee6413c644c5301

--- ios/chrome/browser/ui/toolbar/clean/toolbar_tools_menu_button.mm
+++ ios/chrome/browser/ui/toolbar/clean/toolbar_tools_menu_button.mm
@@ -8,6 +8,7 @@
 #import <QuartzCore/CAMediaTimingFunction.h>
 
 #include "base/logging.h"
+#include "ios/chrome/browser/ui/rtl_geometry.h"
 #include "ios/chrome/browser/ui/toolbar/clean/toolbar_button_tints.h"
 #include "ios/chrome/browser/ui/ui_util.h"
 
@@ -24,9 +25,9 @@ const CGFloat kDotOffsetYVertical = 18;
 const CGFloat kVerticalSpaceBetweenDots = 6;
 
 // *** Constants for the adaptive toolbar, 3 horizontal dots. ***
-// Position of the left most dot.
-const CGFloat kDotOffsetXHorizontal = 22;
-const CGFloat kDotOffsetYHorizontal = 18;
+// Position of the leftmost dot.
+const CGFloat kDotOffsetXHorizontal = 18;
+const CGFloat kDotOffsetYHorizontal = 28;
 // Horizontal space between dots.
 const CGFloat kHorizontalSpaceBetweenDots = 6;
 
@@ -266,8 +267,11 @@ const CGFloat kLineWidthAtApogee = 3;
   // Add four animations for each stroke.
   for (int i = 0; i < kNumberOfDots; i++) {
     CAShapeLayer* pathLayer = pathLayers_[i];
-    const int frameStart =
-        (kNumberOfDots - i) * kFramesBetweenAnimationOfEachDot;
+    int dotToAnimate = kNumberOfDots - i;
+    if (UseRTLLayout()) {
+      dotToAnimate = i;
+    }
+    const int frameStart = dotToAnimate * kFramesBetweenAnimationOfEachDot;
 
     // Start of the stroke animation.
     CAAnimation* strokeStartAnimation =
