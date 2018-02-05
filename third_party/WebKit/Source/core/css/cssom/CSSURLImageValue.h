534ebcc07d82a346d64222d481f7bad8d3a555ed
Darren Shen <shend@chromium.org>
2018-02-05 04:52:59 +0000

    Revert "Revert "[css-typed-om] Refactor CSSStyleImageValue to prepare for gradients.""
    This reverts commit c13ecdc785e389c3e0a6a0b2fdd5f0a2aa2fd804 [formerly 1f977216838ab364205dcd288b0b3efc0de1b01d].
    
    Reason for revert: Relanding
    
    Original change's description:
    > Revert "[css-typed-om] Refactor CSSStyleImageValue to prepare for gradients."
    > 
    > This reverts commit 47b5b672170defd3b22db22d86ecd8a3910aa06c [formerly 6d10aad7a6f28294851e37f6ab7f0b0ac5086508].
    > 
    > Reason for revert: Caused MSAN bot failure.
    > 
    > https://ci.chromium.org/buildbot/chromium.memory/Linux%20MSan%20Tests/7639
    > 
    > Original change's description:
    > > [css-typed-om] Refactor CSSStyleImageValue to prepare for gradients.
    > > 
    > > Currently CSSStyleImageValue implements the CSSImageValue class in
    > > Typed OM. CSSImageValue can be anything that's an <image> i.e. a <url>,
    > > <gradient>, or <cross-fade>.
    > > 
    > > However, the current implementation of CSSStyleImageValue only really
    > > works for <url>s. This patch moves <url> specific logic into
    > > CSSURLImageValue. This prepares for the implementation of <gradient> and
    > > <cross-fade>, which will be subclasses of CSSStyleImageValue.
    > > 
    > > We also do some cleanup work (e.g. making things const, moving code in
    > > header to cpp).
    > > 
    > > Bug: 803680
    > > Change-Id: I848b3eab5dbbcb9bbe7f0aea0d377497f63bfda3
    > > Reviewed-on: https://chromium-review.googlesource.com/882786
    > > Commit-Queue: Darren Shen <shend@chromium.org>
    > > Reviewed-by: nainar <nainar@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#533949}
    > 
    > TBR=nainar@chromium.org,shend@chromium.org
    > 
    > Change-Id: I9aedca3ac9e3ecb07ed283447febede0628d7aeb
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 803680
    > Reviewed-on: https://chromium-review.googlesource.com/896671
    > Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    > Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#534002}
    
    TBR=yutak@chromium.org,nainar@chromium.org,shend@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 803680
    Change-Id: Iccabf87cde1e9bd30ab1d08e31fe1b774a59f029
    Reviewed-on: https://chromium-review.googlesource.com/900563
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534328}
    Former-commit-id: d9a71c341af98d201c3c2cafc12f47612444db76

--- third_party/WebKit/Source/core/css/cssom/CSSURLImageValue.h
+++ third_party/WebKit/Source/core/css/cssom/CSSURLImageValue.h
@@ -7,42 +7,46 @@
 
 #include "base/macros.h"
 #include "core/css/cssom/CSSStyleImageValue.h"
-#include "platform/bindings/ScriptState.h"
 
 namespace blink {
+
+class ScriptState;
+class CSSImageValue;
 
 class CORE_EXPORT CSSURLImageValue final : public CSSStyleImageValue {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  static CSSURLImageValue* Create(ScriptState* script_state,
+  static CSSURLImageValue* Create(ScriptState*,
                                   const AtomicString& url,
-                                  ExceptionState& exception_state) {
-    const auto* execution_context = ExecutionContext::From(script_state);
-    DCHECK(execution_context);
-    KURL parsed_url = execution_context->CompleteURL(url);
-    if (!parsed_url.IsValid()) {
-      exception_state.ThrowTypeError("Failed to parse URL from " + url);
-      return nullptr;
-    }
-    // Use absolute URL for CSSImageValue but keep relative URL for
-    // getter and serialization.
-    return new CSSURLImageValue(
-        CSSImageValue::Create(url, parsed_url, Referrer()));
-  }
-  static CSSURLImageValue* Create(const CSSImageValue* image_value) {
-    return new CSSURLImageValue(image_value);
-  }
+                                  ExceptionState&);
 
-  StyleValueType GetType() const override { return kURLImageType; }
+  static CSSURLImageValue* FromCSSValue(const CSSImageValue&);
 
-  const CSSValue* ToCSSValue() const override { return CssImageValue(); }
+  const String& url() const;
 
-  const String& url() const { return CssImageValue()->RelativeUrl(); }
+  // CSSStyleImageValue
+  WTF::Optional<IntSize> IntrinsicSize() const final;
+
+  // CanvasImageSource
+  ResourceStatus Status() const final;
+  scoped_refptr<Image> GetSourceImageForCanvas(SourceImageStatus*,
+                                               AccelerationHint,
+                                               const FloatSize&) final;
+  bool IsAccelerated() const final;
+
+  // CSSStyleValue
+  StyleValueType GetType() const final { return kURLImageType; }
+  const CSSValue* ToCSSValue() const final;
+
+  virtual void Trace(blink::Visitor*);
 
  private:
-  explicit CSSURLImageValue(const CSSImageValue* image_value)
-      : CSSStyleImageValue(image_value) {}
+  explicit CSSURLImageValue(const CSSImageValue& value) : value_(value) {}
+
+  scoped_refptr<Image> GetImage() const;
+
+  Member<const CSSImageValue> value_;
   DISALLOW_COPY_AND_ASSIGN(CSSURLImageValue);
 };
 
