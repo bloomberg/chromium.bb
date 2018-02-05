ce52d701ded55618f773b01af8adf117c8dfa883
Patti <patricialor@chromium.org>
2018-02-05 02:07:38 +0000

    Omnibox/Views: Round omnibox edges.
    Round out the left and right edges of the LocationBarView and KeywordHintView.
    This change does not account for the LocationIconView corners on focus / hover
    backgrounds, which are not clipped by the omnibox's new rounded corners, nor the
    OmniboxResultView, which now also shows a rounded highlight on the currently
    selected result. These will be fixed in a follow up.
    
    Screenshots -
    https://drive.google.com/file/d/1WNUIQ9k2n5HFZpeMnLmpSgiDenoYptH8/view?usp=sharing
    
    Bug: 801583
    Change-Id: Ifc7c22f3549cba5fef255f9b0eb336580ace4de1
    Reviewed-on: https://chromium-review.googlesource.com/878106
    Commit-Queue: Patti <patricialor@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534314}
    Former-commit-id: 3b2c903a2e03e77f15ed2a16bf504dd7e929a10d

--- chrome/browser/ui/views/location_bar/background_with_1_px_border.h
+++ chrome/browser/ui/views/location_bar/background_with_1_px_border.h
@@ -22,15 +22,34 @@ class View;
 // that the border is one pixel regardless of display scaling.
 class BackgroundWith1PxBorder : public views::Background {
  public:
-  // Corner radius of the inside edge of the roundrect border stroke.
-  static constexpr int kCornerRadius = 2;
-
   // The thickness of the location bar's border in DIP.
   static constexpr int kLocationBarBorderThicknessDip = 1;
 
   BackgroundWith1PxBorder(SkColor background, SkColor border);
 
+  // Whether the OmniboxBackgroundBorder is a pill shape.
+  static bool IsRounded();
+
+  // Paints a blue focus ring that draws over the top of the existing border.
+  void PaintFocusRing(gfx::Canvas* canvas,
+                      ui::NativeTheme* theme,
+                      const gfx::Rect& local_bounds);
+
+  // views::Background:
   void Paint(gfx::Canvas* canvas, views::View* view) const override;
+
+ protected:
+  // Returns the amount of border radius to use for the inside edge of the
+  // border stroke for this background.
+  float GetBorderRadius(int height_in_px) const;
+
+  // Paints the background. |inner_border_radius| is the border radius of the
+  // inside of the stroke.
+  static void Paint(gfx::Canvas* canvas,
+                    SkColor background,
+                    SkColor border,
+                    float inner_border_radius,
+                    const gfx::Rect& bounds);
 
  private:
   // Color for the one pixel border.
