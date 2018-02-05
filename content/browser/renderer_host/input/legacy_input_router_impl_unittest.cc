2cb7ca7ff5713dae495c9af8394bd2c1fb354442
Yuta Kitamura <yutak@chromium.org>
2018-02-05 05:43:14 +0000

    Disable LegacyInputRouterImplScaleGestureEventTest.GestureFlingStart on Fuchsia.
    This test is flaky on Fuchsia.
    
    TBR=sfiera@chromium.org
    
    Bug: 807803
    Change-Id: I2426a2689381d5f33b3c7a22bbc7ef3af014bc64
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/899704
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534334}
    Former-commit-id: eac223255d2284a9fb861c210d08b8757dc950a2

--- content/browser/renderer_host/input/legacy_input_router_impl_unittest.cc
+++ content/browser/renderer_host/input/legacy_input_router_impl_unittest.cc
@@ -2301,7 +2301,13 @@ TEST_F(LegacyInputRouterImplScaleGestureEventTest, GestureTwoFingerTap) {
   EXPECT_EQ(40, filter_event->data.two_finger_tap.first_finger_height);
 }
 
-TEST_F(LegacyInputRouterImplScaleGestureEventTest, GestureFlingStart) {
+// Thie test is flaky on Fuchsia. crbug.com/807803
+#if defined(OS_FUCHSIA)
+#define MAYBE_GestureFlingStart DISABLED_GestureFlingStart
+#else
+#define MAYBE_GestureFlingStart GestureFlingStart
+#endif
+TEST_F(LegacyInputRouterImplScaleGestureEventTest, MAYBE_GestureFlingStart) {
   // Simulate a GSB since touchscreen flings must happen inside scroll.
   SimulateGestureEvent(SyntheticWebGestureEventBuilder::BuildScrollBegin(
       10.f, 20.f, blink::kWebGestureDeviceTouchscreen));
