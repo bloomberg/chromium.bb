48b4954bf2da1b09de1dd37fd02e83e834f8cf7e
Klaus Weidner <klausw@chromium.org>
2018-02-06 03:19:47 +0000

    Fix out-of-sync poses for WebVR
    Change 47b83657bce5ae767adf0538e4163a8e67159d0d [formerly 7332d07490f42ab4fa9ee1a958d9b4e07531b025] "[vr] Add controller
    tooltips" changed the OnVSync controller handling to overwrite
    render_info_primary_.head_pose, and if this happened in between
    DrawFrame and DrawIntoAcquiredFrame it would submit with a wrong
    pose, leading to jerky head tracking.
    
    The fix is to always use the saved WebVR pose and ignore the
    render_info_primary_.head_pose member.
    
    BUG=808147
    
    Change-Id: I1c9b3e9c9e7dad6c9dff2837ae00d46a6fecb915
    Reviewed-on: https://chromium-review.googlesource.com/903542
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Klaus Weidner <klausw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534609}
    Former-commit-id: ef56830b31d5b6be1379b956cf4aad9bfa67a1f5

--- chrome/browser/android/vr_shell/vr_shell_gl.cc
+++ chrome/browser/android/vr_shell/vr_shell_gl.cc
@@ -942,6 +942,7 @@ void VrShellGl::DrawFrame(int16_t frame_index, base::TimeTicks current_time) {
   if (ShouldDrawWebVr()) {
     static_assert(!((kPoseRingBufferSize - 1) & kPoseRingBufferSize),
                   "kPoseRingBufferSize must be a power of 2");
+    // Copy into render info for overlay UI. WebVR doesn't use this.
     render_info_primary_.head_pose =
         webvr_head_pose_[frame_index % kPoseRingBufferSize];
     webvr_frame_oustanding_[frame_index % kPoseRingBufferSize] = false;
@@ -1085,6 +1086,11 @@ void VrShellGl::DrawIntoAcquiredFrame(int16_t frame_index,
     acquired_frame_.Unbind();
   }
 
+  // GVR submit needs the saved head pose that was used for JS rendering. Don't
+  // use render_info_primary_.head_pose here, that may have been overwritten by
+  // OnVSync's controller handling.
+  gfx::Transform webvr_head_pose =
+      webvr_head_pose_[frame_index % kPoseRingBufferSize];
   if (ShouldDrawWebVr() && surfaceless_rendering_ && !webvr_use_gpu_fence_) {
     // Continue with submit once a GL fence signals that current drawing
     // operations have completed.
@@ -1095,10 +1101,10 @@ void VrShellGl::DrawIntoAcquiredFrame(int16_t frame_index,
     task_runner_->PostTask(
         FROM_HERE,
         base::BindOnce(webvr_delayed_frame_submit_.callback(), frame_index,
-                       render_info_primary_.head_pose, base::Passed(&fence)));
+                       webvr_head_pose, base::Passed(&fence)));
   } else {
     // Continue with submit immediately.
-    DrawFrameSubmitNow(frame_index, render_info_primary_.head_pose);
+    DrawFrameSubmitNow(frame_index, webvr_head_pose);
   }
 }
 
@@ -1115,7 +1121,7 @@ void VrShellGl::DrawFrameSubmitWhenReady(
       task_runner_->PostTask(
           FROM_HERE,
           base::BindOnce(webvr_delayed_frame_submit_.callback(), frame_index,
-                         render_info_primary_.head_pose, base::Passed(&fence)));
+                         head_pose, base::Passed(&fence)));
       return;
     }
   }
