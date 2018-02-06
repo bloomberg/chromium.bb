245d4e42593a15b747ca33dac880b13c9ea3e886
Victor Costan <pwnall@chromium.org>
2018-02-06 23:56:15 +0000

    sqlite: Prefix SQLite API methods with chrome_.
    In component builds, SQLite's API methods are exported from the
    chromium_sqlite component, which means they are visible to the dynamic
    library loader. This opens up the following possibilities:
    
    1) A system library calls into our SQLite instead of calling into the
       system's SQLite library which it was built against. The patches in
       our SQLite version lead to different behavior from the system's
       SQLite, which can cause subtle failures. This happens if the dynamic
       library loader resolves the system library's symbol imports with our
       SQLite's exported symbols.
    2) A system library loads the system SQLite, and we end up calling into
       it, instead of calling into our version of SQLite. This happens if
       the dynamic library loader resolves our symbol imports with the
       system's SQLite library.
    
    Both possibilities above lead to the possibility that the component
    build will behave differently from the release build, in subtle and
    potentially non-deterministic ways. This is not a purely academic
    concern. https://crbug.com/807487 happened because we use NSS on Linux,
    and NSS invokes SQLite via a complex plugin system. On non-component builds,
    NSS (a system library) loads and uses the system version of SQLite. On
    component builds, NSS ends up using our SQLite.
    
    This CL fixes the problem by adding a chrome_ prefix to all the symbols
    exported from SQLite3. In C++ libraries, namespaces can make prefixing
    easy. Unfortunately, SQLite is a C library, so the prefixing is fairly
    heavy-handed. A high-level overview of the approach follows:
    
    * An extract_sqlite_api Python script reads SQLite's header, extracts
      the names of all exported symbols, and writes a header file consisting
      of renaming preprocessor macros, e.g.
          #define sqlite3_init chrome_sqlite3_init
      David Benjamin <davidben@chromium.org> designed the approach and wrote
      the original version of the script.
    * The script that we use to generate SQLite's amalgamation now also
      invokes the extract_sqlite_api script described above, and saves the
      output to amalgamation/rename_exports.h.
    * The SQLite component exposes an sqlite3.h header that must be used by
      all SQLite3 users in Chromium. This header now #includes
      rename_exports.h (containing the renaming preprocessor macros) before
      #including amalgamation/sqlite3.h.
    * sqlite3.c (the main output of the amalgamation process) does not
      #include "sqlite3.h". However, in order to facilitate autoconf builds,
      it does #include a "config.h", if a certain preprocessor define
      exists. We abuse that define to have sqlite.c always load config.h,
      and have config.h load our rename_exports.h.
    
    This CL also adds a PRESUBMIT.py that runs unit tests for the
    extract_sqlite_api Python script, which ensures that the script will not
    break accidentally. Both the unit tests and the PRESUBIMT script are
    inspired from //tools/vim.
    
    Bug: 807093, 807487
    Change-Id: If3868ba119ffd4ccbb06d1a6fcd4cc2ecd9ef2ae
    Reviewed-on: https://chromium-review.googlesource.com/898549
    Reviewed-by: Chris Mumford <cmumford@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534843}
    Former-commit-id: 3d8ec48766b2075fb6f242d13bbc181e850df1fc

--- third_party/sqlite/amalgamation/config.h
+++ third_party/sqlite/amalgamation/config.h
@@ -1,41 +1,41 @@
-/* On Windows and OSX, SQLite uses preprocessor macros to configure itself.  On
- * Linux, it expects config.h from autoconf.  autoconf generates config.h by
- * compiling a series of probe programs, and Chromium's build system has no
- * "configure" phase to put such generation in.  This file is a workaround for
- * this issue.
- */
-/* TODO(shess): Expand this to OSX and Windows? */
-/* TODO(shess): Consider config_linux.h, config_mac.h, config_win.h? */
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-/* NOTE(shess): This file is included by sqlite3.c, be very careful about adding
- * #include lines.
- */
-/* TODO(shess): Consider using build/build_config.h for OS_ macros. */
-/* TODO(shess): build_config.h uses unistd.h, perhaps for portability reasons,
- * but AFAICT there are no current portability concerns here.  limits.h is
- * another alternative.
- */
+#ifndef THIRD_PARTY_SQLITE_AMALGAMATION_CONFIG_H_
+#define THIRD_PARTY_SQLITE_AMALGAMATION_CONFIG_H_
+
+// This file is included by sqlite3.c fairly early.
+
+// We prefix chrome_ to SQLite's exported symbols, so that we don't clash with
+// other SQLite libraries loaded by the system libraries. This only matters when
+// using the component build, where our SQLite's symbols are visible to the
+// dynamic library loader.
+#include "third_party/sqlite/amalgamation/rename_exports.h"
+
+// Linux-specific configuration fixups.
+#if defined(__linux__)
 
 // features.h, included below, indirectly includes sys/mman.h. The latter header
 // only defines mremap if _GNU_SOURCE is defined. Depending on the order of the
 // files in the amalgamation, removing the define below may result in a build
 // error on Linux.
 #if defined(__GNUC__) && !defined(_GNU_SOURCE)
-# define _GNU_SOURCE
+#define _GNU_SOURCE
 #endif
-
 #include <features.h>
 
-/* SQLite wants to track malloc sizes.  On OSX it uses malloc_size(), on
- * Windows _msize(), elsewhere it handles it manually by enlarging the malloc
- * and injecting a field.  Enable malloc_usable_size() for Linux.
- *
- * malloc_usable_size() is not exported by the Android NDK.  It is not
- * implemented by uclibc.
- */
-#if defined(__linux__) && !defined(__UCLIBC__)
+// SQLite wants to track malloc sizes. On OSX it uses malloc_size(), on Windows
+// _msize(), elsewhere it handles it manually by enlarging the malloc and
+// injecting a field. Enable malloc_usable_size() for Linux.
+//
+// malloc_usable_size() is not exported by the Android NDK. It is not
+// implemented by uclibc.
+#if !defined(__UCLIBC__) && !defined(__ANDROID__)
 #define HAVE_MALLOC_H 1
 #define HAVE_MALLOC_USABLE_SIZE 1
 #endif
 
-/* TODO(shess): Eat other config options from gn and gyp? */
+#endif  // defined(__linux__)
+
+#endif  // THIRD_PARTY_SQLITE_AMALGAMATION_CONFIG_H_
