843d0c717f0f608a9f6736dbf73ef11faca3671b
John Chen <johnchen@chromium.org>
2018-02-06 00:34:05 +0000

    [ChromeDriver] Add Pixel 2 to mobile emulation list
    Also modified embed_mobile_devices_in_cpp.py, so users can use both old
    device names such as 'iPhone 6', and new names such as 'iPhone 6/7/8'.
    
    Bug: chromedriver:2231
    Change-Id: I476527d223bd6d3e440bef3823edba65b214b1fd
    Reviewed-on: https://chromium-review.googlesource.com/879467
    Reviewed-by: Jonathon Kereliuk <kereliuk@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534553}
    Former-commit-id: 167a7f5e03f8b9bd297d2663ec35affa0edd5076

--- chrome/test/chromedriver/chrome/mobile_device_list.h
+++ chrome/test/chromedriver/chrome/mobile_device_list.h
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// This file was generated at (2018-01-03 09:47:36.639726) by running:
+// This file was generated at (2018-01-22 12:56:00.187529) by running:
 //     ./chrome/test/chromedriver/embed_mobile_devices_in_cpp.py --directory
 //     chrome/test/chromedriver/chrome/
 //     third_party/WebKit/Source/devtools/front_end/emulated_devices/module.json
