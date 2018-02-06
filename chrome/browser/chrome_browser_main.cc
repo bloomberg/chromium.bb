f7870bed8abc7da9fcfba009114b4269914b42c9
David 'Digit' Turner <digit@google.com>
2018-02-06 12:22:56 +0000

    Deprecate Downloadable Strings experiment.
    This experiment is no longer useful, and it has been decided
    to not implement the feature with the Chromium component
    updater. Instead, Chrome on Android will rely on split APKs
    to reduce the size of the main application APK.
    
    BUG=766746
    R=sorin@chromium.org,mpearson@chromium.org,waffles@chromium.org
    
    Change-Id: I13d934e8f82a239688c9c1d02be3f3e299b8587b
    Reviewed-on: https://chromium-review.googlesource.com/902104
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: David Turner <digit@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534677}
    Former-commit-id: 9bc1bab1635797b13d1033ce1ed9ce9d66eb53bc

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -60,7 +60,6 @@
 #include "chrome/browser/chrome_browser_field_trials.h"
 #include "chrome/browser/chrome_browser_main_extra_parts.h"
 #include "chrome/browser/component_updater/crl_set_component_installer.h"
-#include "chrome/browser/component_updater/downloadable_strings_component_installer.h"
 #include "chrome/browser/component_updater/file_type_policies_component_installer.h"
 #include "chrome/browser/component_updater/mei_preload_component_installer.h"
 #include "chrome/browser/component_updater/optimization_hints_component_installer.h"
@@ -560,10 +559,6 @@ void RegisterComponentsForUpdate(PrefService* profile_prefs) {
 
 #if BUILDFLAG(ENABLE_CAPTIVE_PORTAL_DETECTION)
     RegisterSSLErrorAssistantComponent(cus, path);
-#endif
-
-#if BUILDFLAG(ENABLE_DOWNLOADABLE_STRINGS)
-    RegisterDownloadableStringsComponent(cus);
 #endif
   }
 
