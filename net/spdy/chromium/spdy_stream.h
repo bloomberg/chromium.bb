b065aa9112e1744268afa64ea68e9d02bb6d342f
Bence Béky <bnc@chromium.org>
2018-02-06 23:04:51 +0000

    Use GetPromisedUrlFromHeaderBlock for pushed headers.
    Use GetPromisedUrlFromHeaderBlock() instead of GetUrlFromHeaderBlock()
    for pushed HTTP/2 headers.
    
    Remove dangerous GetDefaultUrlWithPath() and inline pushed URLs instead
    in SpdyNetworkTransactionTests.
    
    Pass URL from SpdySession to SpdyStream so that GetUrlFromHeaderBlock()
    does not need to be called once more.
    
    Change-Id: Ibc51015d20d0f5a3cf1431ad668feff1662a178a
    Reviewed-on: https://chromium-review.googlesource.com/896808
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534823}
    Former-commit-id: 3e01b53639df2aa09bdce7da7e111440f0487b2e

--- net/spdy/chromium/spdy_stream.h
+++ net/spdy/chromium/spdy_stream.h
@@ -248,7 +248,7 @@ class NET_EXPORT_PRIVATE SpdyStream {
 
   // Called by the SpdySession when a frame carrying request headers opening a
   // push stream is received. Stream transits to STATE_RESERVED_REMOTE state.
-  void OnPushPromiseHeadersReceived(SpdyHeaderBlock headers);
+  void OnPushPromiseHeadersReceived(SpdyHeaderBlock headers, GURL url);
 
   // Called by the SpdySession when response data has been received
   // for this stream.  This callback may be called multiple times as
