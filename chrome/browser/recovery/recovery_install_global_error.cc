a2d1dde13ab55bfc309903fd0c71c5e151f89e51
Peter Boström <pbos@chromium.org>
2018-02-06 17:41:33 +0000

    Add branded titlecase for global-error bubble.
    These strings were missed as they were not in generated_resources.grd.
    
    Bug: chromium:807886
    Change-Id: I3c0c812f751487fffa6dc167cff21e0af269bf73
    Reviewed-on: https://chromium-review.googlesource.com/902187
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534707}
    Former-commit-id: 40381082a700e5615c75746e186f2bb9f66ed641

--- chrome/browser/recovery/recovery_install_global_error.cc
+++ chrome/browser/recovery/recovery_install_global_error.cc
@@ -15,6 +15,7 @@
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/grit/chromium_strings.h"
+#include "chrome/grit/generated_resources.h"
 #include "components/prefs/pref_service.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/paint_vector_icon.h"
