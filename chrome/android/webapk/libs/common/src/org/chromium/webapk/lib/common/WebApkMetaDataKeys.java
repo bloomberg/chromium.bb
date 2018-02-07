924af53c7af6dc62cac866ee7a0c3111668398ee
Xi Han <hanxi@google.com>
2018-02-07 18:56:43 +0000

    Open WebAPKs in Custom Tabs.
    It is possible that an unbound WebAPK doesn't have a valid Chrome's
    signature. Currently, they are opened in Chrome tabs. In this CL:
    1. A new <meta-data> tag "disableFullScreenMode" is introduced in the shell
       APK. The WAM server could choose to set this tag be true in order to make
       the created WebAPK launched in Custom Tabs.
    2. Update HostBrowserLauncher in the shell APK to check the metadata tag and
       launch such kind of WebAPKs in Custom Tabs.
    
    Bug: 806946
    Change-Id: Ifafbea6b2fb6764f3d5db146feaa6f0396246368
    Reviewed-on: https://chromium-review.googlesource.com/893884
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535082}
    Former-commit-id: 42cebb5e5639370608312e20b3810f84f467b465

--- chrome/android/webapk/libs/common/src/org/chromium/webapk/lib/common/WebApkMetaDataKeys.java
+++ chrome/android/webapk/libs/common/src/org/chromium/webapk/lib/common/WebApkMetaDataKeys.java
@@ -23,6 +23,8 @@ public final class WebApkMetaDataKeys {
     public static final String THEME_COLOR = "org.chromium.webapk.shell_apk.themeColor";
     public static final String BACKGROUND_COLOR = "org.chromium.webapk.shell_apk.backgroundColor";
     public static final String ICON_ID = "org.chromium.webapk.shell_apk.iconId";
+    public static final String DISABLE_FULL_SCREEN_MODE =
+            "org.chromium.webapk.shell_apk.disableFullScreenMode";
 
     public static final String ICON_URLS_AND_ICON_MURMUR2_HASHES =
             "org.chromium.webapk.shell_apk.iconUrlsAndIconMurmur2Hashes";
