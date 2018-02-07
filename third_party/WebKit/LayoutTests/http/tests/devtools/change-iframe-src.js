f0b6802c9fa9a2c728ed5cfae2058ce78d145f6b
Joel Einbinder <einbinder@chromium.org>
2018-02-07 23:02:41 +0000

    DevTools: Fix broken test change-iframe-src.js
    Bug: 777222
    Change-Id: I593734454dac4251a01284b177ee82fbef78f93e
    Reviewed-on: https://chromium-review.googlesource.com/896576
    Commit-Queue: Joel Einbinder <einbinder@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535178}
    Former-commit-id: f1d88fd2143300f670dd1ca72f31c08716e45ecc

--- third_party/WebKit/LayoutTests/http/tests/devtools/change-iframe-src.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/change-iframe-src.js
@@ -7,27 +7,18 @@
       `Tests that Elements panel allows to change src attribute on iframes inside inspected page. See bug 41350.\n`);
   await TestRunner.loadModule('elements_test_runner');
   await TestRunner.loadModule('console_test_runner');
+
+  var messagePromise = new Promise(x => ConsoleTestRunner.addConsoleSniffer(x));
   await TestRunner.loadHTML(`
-      <iframe src="resources/iframe-from-different-domain-data.html" id="receiver" onload="onIFrameLoad()"></iframe>
+      <iframe src="resources/iframe-from-different-domain-data.html" id="receiver"></iframe>
     `);
-  await TestRunner.evaluateInPagePromise(`
-      var onIFrameLoadCalled = false;
-      function onIFrameLoad()
-      {
-          if (onIFrameLoadCalled)
-              return;
-          onIFrameLoadCalled = true;    }
-  `);
+  var message = await messagePromise;
+  TestRunner.addResult(message.messageText);
 
-  ElementsTestRunner.nodeWithId('receiver', step1);
-
-  function step1(node) {
-    node.setAttribute('src', 'src="http://localhost:8000/devtools/resources/iframe-from-different-domain-data.html"');
-    ConsoleTestRunner.addConsoleSniffer(step2);
-  }
-
-  function step2() {
-    ConsoleTestRunner.dumpConsoleMessages();
-    TestRunner.completeTest();
-  }
+  var node = await new Promise(x => ElementsTestRunner.nodeWithId('receiver', x));
+  var messagePromise = new Promise(x => ConsoleTestRunner.addConsoleSniffer(x));
+  node.setAttribute('src', 'src="http://localhost:8000/devtools/resources/iframe-from-different-domain-data.html"');
+  var message = await messagePromise;
+  TestRunner.addResult(message.messageText);
+  TestRunner.completeTest();
 })();
