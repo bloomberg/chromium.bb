ce983934e5582f9f0eaf2491423b11d943d72629
Alexei Filippov <alph@chromium.org>
2018-02-07 09:56:57 +0000

    Sampling Heap Profiler: Move profiler to WebKit/common/heap_profiler
    BUG=803276
    
    Change-Id: I3e8391fa2fe8a78b29e8ee14323ae0d01671e911
    Reviewed-on: https://chromium-review.googlesource.com/900683
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#534969}
    Former-commit-id: 9fbb754e04fc4484b2198fd43632b0a5a5f0ea2d

--- third_party/WebKit/common/sampling_heap_profiler/sampling_heap_profiler.h
+++ third_party/WebKit/common/sampling_heap_profiler/sampling_heap_profiler.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef SamplingNativeHeapProfiler_h
-#define SamplingNativeHeapProfiler_h
+#ifndef THIRD_PARTY_WEBKIT_COMMON_SAMPLING_HEAP_PROFILER_SAMPLING_HEAP_PROFILER_H
+#define THIRD_PARTY_WEBKIT_COMMON_SAMPLING_HEAP_PROFILER_SAMPLING_HEAP_PROFILER_H
 
 #include <unordered_map>
 #include <vector>
@@ -11,17 +11,23 @@
 #include "base/macros.h"
 #include "base/synchronization/lock.h"
 #include "base/threading/thread_local.h"
-#include "platform/PlatformExport.h"
-#include "public/platform/SamplingHeapProfiler.h"
+#include "third_party/WebKit/common/common_export.h"
 
 namespace base {
 template <typename T>
 struct DefaultSingletonTraits;
-}
+}  // namespace base
 
 namespace blink {
 
-class PLATFORM_EXPORT SamplingNativeHeapProfiler : public SamplingHeapProfiler {
+// The class implements sampling of native memory heap.
+// It hooks on three basic allocators:
+//   - base::allocator
+//   - base::PartitionAlloc
+//   - Blink GC
+// When started it records allocation samples based on the sampling_interval
+// parameter. The recorded samples can be retrieve using GetSamples method.
+class BLINK_COMMON_EXPORT SamplingHeapProfiler {
  public:
   class Sample {
    public:
@@ -30,27 +36,27 @@ class PLATFORM_EXPORT SamplingNativeHeapProfiler : public SamplingHeapProfiler {
     std::vector<void*> stack;
 
    private:
-    friend class SamplingNativeHeapProfiler;
+    friend class SamplingHeapProfiler;
 
     Sample(size_t, size_t count, uint32_t ordinal);
 
     uint32_t ordinal;
   };
 
-  uint32_t Start() override;
-  void Stop() override;
-  void SetSamplingInterval(size_t sampling_interval) override;
+  uint32_t Start();
+  void Stop();
+  void SetSamplingInterval(size_t sampling_interval);
   void SuppressRandomnessForTest();
 
   std::vector<Sample> GetSamples(uint32_t profile_id);
 
-  static inline void MaybeRecordAlloc(void* address, size_t);
+  static inline void MaybeRecordAlloc(void* address, size_t, uint32_t);
   static inline void MaybeRecordFree(void* address);
 
-  static SamplingNativeHeapProfiler* GetInstance();
+  static SamplingHeapProfiler* GetInstance();
 
  private:
-  SamplingNativeHeapProfiler();
+  SamplingHeapProfiler();
 
   static void InstallAllocatorHooksOnce();
   static bool InstallAllocatorHooks();
@@ -59,7 +65,7 @@ class PLATFORM_EXPORT SamplingNativeHeapProfiler : public SamplingHeapProfiler {
   void RecordAlloc(size_t total_allocated,
                    size_t allocation_size,
                    void* address,
-                   unsigned skip_frames);
+                   uint32_t skip_frames);
   void RecordFree(void* address);
   void RecordStackTrace(Sample*, unsigned skip_frames);
 
@@ -67,11 +73,11 @@ class PLATFORM_EXPORT SamplingNativeHeapProfiler : public SamplingHeapProfiler {
   base::Lock mutex_;
   std::unordered_map<void*, Sample> samples_;
 
-  friend struct base::DefaultSingletonTraits<SamplingNativeHeapProfiler>;
+  friend struct base::DefaultSingletonTraits<SamplingHeapProfiler>;
 
-  DISALLOW_COPY_AND_ASSIGN(SamplingNativeHeapProfiler);
+  DISALLOW_COPY_AND_ASSIGN(SamplingHeapProfiler);
 };
 
 }  // namespace blink
 
-#endif  // SamplingNativeHeapProfiler_h
+#endif  // THIRD_PARTY_WEBKIT_COMMON_SAMPLING_HEAP_PROFILER_SAMPLING_HEAP_PROFILER_H
