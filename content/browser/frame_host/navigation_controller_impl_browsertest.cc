03d6c76ed7d5fb01dd75839dff73b62fcbd4742b
Andrey Kosyakov <caseq@chromium.org>
2018-02-07 22:39:38 +0000

    Disable NavigationControllerBrowserTest.RaceCrossOriginNavigationAndSameDocumentHistoryNavigation
    It's a flaky timeout on most platforms.
    
    TBR: clamy
    NOTRY: true
    Bug: 809488
    Change-Id: Idfa6896f55622c5b79b7ffc81fe008241991daa3
    Reviewed-on: https://chromium-review.googlesource.com/907646
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535167}
    Former-commit-id: e3c894eb0b22d2a85e7e46f6f24cb80961c8245d

--- content/browser/frame_host/navigation_controller_impl_browsertest.cc
+++ content/browser/frame_host/navigation_controller_impl_browsertest.cc
@@ -6692,17 +6692,10 @@ class HistoryNavigationBeforeCommitInjector
 // the cross-origin navigation and updates the URL, but not the origin of the
 // document. This results in mismatch between the two and causes the renderer
 // process to be killed. See https://crbug.com/630103.
-// Currently disabled on Android due to flakyness. See https://crbug.com/809488.
-#if defined(OS_ANDROID)
-#define MAYBE_RaceCrossOriginNavigationAndSameDocumentHistoryNavigation \
-  DISABLED_RaceCrossOriginNavigationAndSameDocumentHistoryNavigation
-#else
-#define MAYBE_RaceCrossOriginNavigationAndSameDocumentHistoryNavigation \
-  RaceCrossOriginNavigationAndSameDocumentHistoryNavigation
-#endif
+// Currently disabled due to flaky timeouts. See https://crbug.com/809488.
 IN_PROC_BROWSER_TEST_F(
     NavigationControllerBrowserTest,
-    MAYBE_RaceCrossOriginNavigationAndSameDocumentHistoryNavigation) {
+    DISABLED_RaceCrossOriginNavigationAndSameDocumentHistoryNavigation) {
   WebContentsImpl* web_contents =
       static_cast<WebContentsImpl*>(shell()->web_contents());
   FrameTreeNode* root = web_contents->GetFrameTree()->root();
