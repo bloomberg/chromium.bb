3b84fd1fffe09f1f39c4f25c5750071a7a9dc011
Evan Stade <estade@chromium.org>
2018-02-07 17:50:46 +0000

    Clean up some constants in ui/message_center/views
    Use more named color constants, like SK_ColorBLACK and
    gfx::kGoogleBlue700.
    
    Bug: none
    Change-Id: Id720a06c4c3ee08ac74250b70d8c847a59a45253
    Reviewed-on: https://chromium-review.googlesource.com/898442
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535050}
    Former-commit-id: 36871aea37760661a62111859033c75a5241e838

--- ui/gfx/color_palette.h
+++ ui/gfx/color_palette.h
@@ -13,24 +13,25 @@ namespace gfx {
 // as a visual flag for misbehaving code.
 constexpr SkColor kPlaceholderColor = SK_ColorRED;
 
-const SkColor kChromeIconGrey = SkColorSetRGB(0x5A, 0x5A, 0x5A);
+constexpr SkColor kChromeIconGrey = SkColorSetRGB(0x5A, 0x5A, 0x5A);
 
 // The number refers to the shade of darkness. Each color in the MD
 // palette ranges from 100-900.
-const SkColor kGoogleBlue300 = SkColorSetRGB(0x7B, 0xAA, 0xF7);
-const SkColor kGoogleBlue500 = SkColorSetRGB(0x42, 0x85, 0xF4);
-const SkColor kGoogleBlue700 = SkColorSetRGB(0x33, 0x67, 0xD6);
-const SkColor kGoogleBlue900 = SkColorSetRGB(0x1C, 0x3A, 0xA9);
-const SkColor kGoogleRed300 = SkColorSetRGB(0xE6, 0x7C, 0x73);
-const SkColor kGoogleRed700 = SkColorSetRGB(0xC5, 0x39, 0x29);
-const SkColor kGoogleGreen300 = SkColorSetRGB(0x57, 0xBB, 0x8A);
-const SkColor kGoogleGreen700 = SkColorSetRGB(0x0B, 0x80, 0x43);
-const SkColor kGoogleYellow300 = SkColorSetRGB(0xF7, 0xCB, 0x4D);
-const SkColor kGoogleYellow700 = SkColorSetRGB(0xF0, 0x93, 0x00);
+constexpr SkColor kGoogleBlue300 = SkColorSetRGB(0x7B, 0xAA, 0xF7);
+constexpr SkColor kGoogleBlue500 = SkColorSetRGB(0x42, 0x85, 0xF4);
+constexpr SkColor kGoogleBlue700 = SkColorSetRGB(0x33, 0x67, 0xD6);
+constexpr SkColor kGoogleBlue900 = SkColorSetRGB(0x1C, 0x3A, 0xA9);
+constexpr SkColor kGoogleRed300 = SkColorSetRGB(0xE6, 0x7C, 0x73);
+constexpr SkColor kGoogleRed700 = SkColorSetRGB(0xC5, 0x39, 0x29);
+constexpr SkColor kGoogleGreen300 = SkColorSetRGB(0x57, 0xBB, 0x8A);
+constexpr SkColor kGoogleGreen700 = SkColorSetRGB(0x0B, 0x80, 0x43);
+constexpr SkColor kGoogleYellow300 = SkColorSetRGB(0xF7, 0xCB, 0x4D);
+constexpr SkColor kGoogleYellow700 = SkColorSetRGB(0xF0, 0x93, 0x00);
+constexpr SkColor kGoogleYellow900 = SkColorSetRGB(0xEA, 0x61, 0x00);
 
 // An alpha value for designating a control's disabled state. In specs this is
 // sometimes listed as 0.38a.
-const SkAlpha kDisabledControlAlpha = 0x61;
+constexpr SkAlpha kDisabledControlAlpha = 0x61;
 
 }  // namespace gfx
 
