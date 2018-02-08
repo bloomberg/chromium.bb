bb6e213f101cb1eb8660755e7287dbb302625427
Stephen McGruer <smcgruer@chromium.org>
2018-02-08 00:42:45 +0000

    Fix enum name used for IsValidEnum for sequence<Enum> in unions
    Previously this value was always taken from 'type_name', which for a
    sequence is "EnumSequence" rather than the correct "Enum".
    
    Bug: 809190
    Change-Id: I27f2b6e10fa441d9bc766342c99cceb0173604f6
    Reviewed-on: https://chromium-review.googlesource.com/906674
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535220}
    Former-commit-id: ecdb1462f5e9e86a0115e145abdbd6ffa442304e

--- third_party/WebKit/Source/bindings/scripts/v8_union.py
+++ third_party/WebKit/Source/bindings/scripts/v8_union.py
@@ -151,6 +151,7 @@ def member_context(member, info_provider):
         'cpp_value_to_v8_value': member.cpp_value_to_v8_value(
             cpp_value='impl.GetAs%s()' % member.name, isolate='isolate',
             creation_context='creationContext'),
+        'enum_type': member.enum_type,
         'enum_values': member.enum_values,
         'is_array_buffer_or_view_type': member.is_array_buffer_or_view,
         'is_array_buffer_view_or_typed_array': member.is_array_buffer_view_or_typed_array,
