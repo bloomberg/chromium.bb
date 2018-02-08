355fbe7f5f24a26f4770e53238ec3917981dc7ce
Mugdha Lakhani <nator@chromium.org>
2018-02-08 19:15:02 +0000

    [GCM Driver] Use OnceCallback and OnceClosure for
    GCM key store instead of Callback and Closure, respectively.
    This is because we only are using these callbacks once.
    This also lays the foundation for replacing KeyPair with ECPrivateKey.
    
    Bug: 603319
    Change-Id: I01632b0d3be7cf92cdced81a3b35d99134d37ad7
    Reviewed-on: https://chromium-review.googlesource.com/907348
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535468}
    Former-commit-id: c0f6cdb1b64d1789fa3ad88af79824e3ddd5c157

--- chrome/browser/push_messaging/push_messaging_service_impl.cc
+++ chrome/browser/push_messaging/push_messaging_service_impl.cc
@@ -1071,9 +1071,11 @@ void PushMessagingServiceImpl::GetEncryptionInfoForAppId(
     gcm::GCMEncryptionProvider::EncryptionInfoCallback callback) {
   if (PushMessagingAppIdentifier::UseInstanceID(app_id)) {
     GetInstanceIDDriver()->GetInstanceID(app_id)->GetEncryptionInfo(
-        NormalizeSenderInfo(sender_id), callback);
+        NormalizeSenderInfo(sender_id),
+        base::AdaptCallbackForRepeating(std::move(callback)));
   } else {
-    GetGCMDriver()->GetEncryptionInfo(app_id, callback);
+    GetGCMDriver()->GetEncryptionInfo(
+        app_id, base::AdaptCallbackForRepeating(std::move(callback)));
   }
 }
 
