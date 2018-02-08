2b8f89e45cea5b2b06bc5cc8a3c03e156ee42285
Kristian H. Kristensen <hoegsberg@chromium.org>
2018-02-07 16:10:06 -0800

    i915: Always set the modifier when allocating BOs
    With CL:907756 we now pass modifiers around when export to and
    importing from the rest of Chrome and we need to give the right answer
    when ozone queries the modifier of a bo allocated with use flags.
    
    Bug: 809670, 809666
    Change-Id: Iab5ea62d5e0da9ddd90013f559f802270de1e420
    Reviewed-on: https://chromium-review.googlesource.com/907848
    Commit-Ready: Kristian H. Kristensen <hoegsberg@chromium.org>
    Tested-by: Kristian H. Kristensen <hoegsberg@chromium.org>
    Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>

--- i915.c
+++ i915.c
@@ -298,6 +298,8 @@ static int i915_bo_create_for_modifier(struct bo *bo, uint32_t width, uint32_t h
 		break;
 	}
 
+	bo->format_modifiers[0] = modifier;
+
 	stride = drv_stride_from_format(format, width, 0);
 
 	ret = i915_align_dimensions(bo, bo->tiling, &stride, &height);
@@ -397,8 +399,6 @@ static int i915_bo_create_with_modifiers(struct bo *bo, uint32_t width, uint32_t
 	uint64_t modifier;
 
 	modifier = drv_pick_modifier(modifiers, count, modifier_order, ARRAY_SIZE(modifier_order));
-
-	bo->format_modifiers[0] = modifier;
 
 	return i915_bo_create_for_modifier(bo, width, height, format, modifier);
 }
