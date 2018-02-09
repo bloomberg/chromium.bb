41f19d5b37d582480b0b0ca9c411fd18ebe8b5e8
Bence Béky <bnc@chromium.org>
2018-02-09 04:17:53 +0000

    Remove some unused classes, constants, and methods.
    This CL lands internal change 183657074.
    
    BUG=488484
    
    Change-Id: I9f2da0358f9fdc9a87465838bcc861f85985c0ff
    Reviewed-on: https://chromium-review.googlesource.com/889849
    Commit-Queue: Dianna Hu <diannahu@chromium.org>
    Reviewed-by: Dianna Hu <diannahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535642}
    Former-commit-id: 0b2c97e9c53286065053adc8d07d71f9713e8d3f

--- net/spdy/core/spdy_deframer_visitor_test.cc
+++ net/spdy/core/spdy_deframer_visitor_test.cc
@@ -58,20 +58,6 @@ class SpdyDeframerVisitorTest : public ::testing::Test {
             decoder_.spdy_framer_error() == Http2DecoderAdapter::SPDY_NO_ERROR);
   }
 
-  SpdySerializedFrame SerializeFrame(const SpdyFrameIR& frame) {
-    return encoder_.SerializeFrame(frame);
-  }
-
-  SpdyString SerializeFrames(
-      const std::vector<std::unique_ptr<SpdyFrameIR>>& frames) {
-    SpdyString result;
-    for (const auto& frame_ptr : frames) {
-      auto sf = SerializeFrame(*frame_ptr);
-      result.append(sf.data(), sf.size());
-    }
-    return result;
-  }
-
   //       bool
 
   SpdyFramer encoder_;
