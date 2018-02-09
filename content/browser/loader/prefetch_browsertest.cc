b77baf05b3067988eb275d1e59287e00aa98f507
Kinuko Yasuda <kinuko@chromium.org>
2018-02-09 10:17:40 +0000

    Add more prefetch browser tests
    - Double-prefetch should result in single network hit
    - Prefetching No-Cache should populate the cache for a certain period
    - Prefetching No-Store does not populate the cache
    - Prefetch a resource with link rel=preload would also trigger the preload
    
    Would like to have these to make sure that new code we're adding won't
    break the detailed assumptions. (We could also have some of these as
    layout tests maybe later)
    
    Bug: 803776
    Change-Id: I5be6b9b9661148ad6759c9fbd15c340cf19233a6
    Reviewed-on: https://chromium-review.googlesource.com/910548
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535680}
    Former-commit-id: 5e06da6ad2f2e8e6984fb5ed57c236ecc66b8a6e

--- content/browser/loader/prefetch_browsertest.cc
+++ content/browser/loader/prefetch_browsertest.cc
@@ -4,8 +4,11 @@
 
 #include <map>
 #include <string>
+#include <vector>
 
 #include "base/run_loop.h"
+#include "base/strings/string_number_conversions.h"
+#include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_feature_list.h"
 #include "content/public/test/browser_test_utils.h"
@@ -21,6 +24,19 @@ namespace content {
 class PrefetchBrowserTest : public ContentBrowserTest,
                             public testing::WithParamInterface<bool> {
  public:
+  struct ResponseEntry {
+    ResponseEntry() = default;
+    explicit ResponseEntry(
+        const std::string& content,
+        const std::string& content_type = "text/html",
+        const std::vector<std::pair<std::string, std::string>>& headers = {})
+        : content(content), content_type(content_type), headers(headers) {}
+    ~ResponseEntry() = default;
+    std::string content;
+    std::string content_type;
+    std::vector<std::pair<std::string, std::string>> headers;
+  };
+
   PrefetchBrowserTest() = default;
   ~PrefetchBrowserTest() = default;
 
@@ -30,8 +46,8 @@ class PrefetchBrowserTest : public ContentBrowserTest,
     ContentBrowserTest::SetUp();
   }
 
-  void RegisterResponse(const std::string& url, const std::string& content) {
-    response_map_[url] = content;
+  void RegisterResponse(const std::string& url, const ResponseEntry& entry) {
+    response_map_[url] = entry;
   }
 
   std::unique_ptr<net::test_server::HttpResponse> ServeResponses(
@@ -40,7 +56,10 @@ class PrefetchBrowserTest : public ContentBrowserTest,
     if (found != response_map_.end()) {
       auto response = std::make_unique<net::test_server::BasicHttpResponse>();
       response->set_code(net::HTTP_OK);
-      response->set_content(found->second);
+      response->set_content(found->second.content);
+      response->set_content_type(found->second.content_type);
+      for (const auto& header : found->second.headers)
+        response->AddCustomHeader(header.first, header.second);
       return std::move(response);
     }
     return nullptr;
@@ -52,24 +71,28 @@ class PrefetchBrowserTest : public ContentBrowserTest,
                              const net::test_server::HttpRequest& request) {
     if (request.relative_url == relative_url) {
       (*visit_count)++;
-      std::move(closure).Run();
+      if (closure)
+        std::move(closure).Run();
     }
   }
 
  private:
   base::test::ScopedFeatureList feature_list_;
-  std::map<std::string, std::string> response_map_;
+  std::map<std::string, ResponseEntry> response_map_;
 
   DISALLOW_COPY_AND_ASSIGN(PrefetchBrowserTest);
 };
 
 IN_PROC_BROWSER_TEST_P(PrefetchBrowserTest, Simple) {
   int target_fetch_count = 0;
-  const std::string prefetch_url = "/prefetch.html";
-  const std::string target_url = "/target.html";
-  RegisterResponse(prefetch_url,
-                   "<body><link rel='prefetch' href='/target.html'></body>");
-  RegisterResponse(target_url, "<head><title>Prefetch Target</title></head>");
+  const char* prefetch_url = "/prefetch.html";
+  const char* target_url = "/target.html";
+  RegisterResponse(
+      prefetch_url,
+      ResponseEntry(base::StringPrintf(
+          "<body><link rel='prefetch' href='%s'></body>", target_url)));
+  RegisterResponse(
+      target_url, ResponseEntry("<head><title>Prefetch Target</title></head>"));
 
   base::RunLoop prefetch_waiter;
   embedded_test_server()->RegisterRequestMonitor(base::BindRepeating(
@@ -94,6 +117,150 @@ IN_PROC_BROWSER_TEST_P(PrefetchBrowserTest, Simple) {
   NavigateToURL(shell(), embedded_test_server()->GetURL(target_url));
   EXPECT_EQ(title, title_watcher.WaitAndGetTitle());
   EXPECT_EQ(1, target_fetch_count);
+}
+
+IN_PROC_BROWSER_TEST_P(PrefetchBrowserTest, DoublePrefetch) {
+  int target_fetch_count = 0;
+  const char* prefetch_url = "/prefetch.html";
+  const char* target_url = "/target.html";
+  RegisterResponse(prefetch_url, ResponseEntry(base::StringPrintf(
+                                     "<body><link rel='prefetch' href='%s'>"
+                                     "<link rel='prefetch' href='%s'></body>",
+                                     target_url, target_url)));
+  RegisterResponse(
+      target_url, ResponseEntry("<head><title>Prefetch Target</title></head>"));
+
+  base::RunLoop prefetch_waiter;
+  embedded_test_server()->RegisterRequestMonitor(base::BindRepeating(
+      &PrefetchBrowserTest::WatchURLAndRunClosure, base::Unretained(this),
+      target_url, &target_fetch_count, prefetch_waiter.QuitClosure()));
+  embedded_test_server()->RegisterRequestHandler(base::BindRepeating(
+      &PrefetchBrowserTest::ServeResponses, base::Unretained(this)));
+  ASSERT_TRUE(embedded_test_server()->Start());
+  EXPECT_EQ(0, target_fetch_count);
+
+  // Loading a page that prefetches the target URL would increment the
+  // |target_fetch_count|, but it should hit only once.
+  NavigateToURL(shell(), embedded_test_server()->GetURL(prefetch_url));
+  prefetch_waiter.Run();
+  EXPECT_EQ(1, target_fetch_count);
+
+  // Subsequent navigation to the target URL wouldn't hit the network for
+  // the target URL (therefore not increment |target_fetch_count|.
+  // The target content should still be read correctly.
+  base::string16 title = base::ASCIIToUTF16("Prefetch Target");
+  TitleWatcher title_watcher(shell()->web_contents(), title);
+  NavigateToURL(shell(), embedded_test_server()->GetURL(target_url));
+  EXPECT_EQ(title, title_watcher.WaitAndGetTitle());
+  EXPECT_EQ(1, target_fetch_count);
+}
+
+IN_PROC_BROWSER_TEST_P(PrefetchBrowserTest, NoCacheAndNoStore) {
+  int nocache_fetch_count = 0;
+  int nostore_fetch_count = 0;
+  const char* prefetch_url = "/prefetch.html";
+  const char* nocache_url = "/target1.html";
+  const char* nostore_url = "/target2.html";
+  RegisterResponse(prefetch_url, ResponseEntry(base::StringPrintf(
+                                     "<body>"
+                                     "<link rel='prefetch' href='%s'>"
+                                     "<link rel='prefetch' href='%s'></body>",
+                                     nocache_url, nostore_url)));
+  RegisterResponse(nocache_url,
+                   ResponseEntry("<head><title>NoCache Target</title></head>",
+                                 "text/html", {{"cache-control", "no-cache"}}));
+  RegisterResponse(nostore_url,
+                   ResponseEntry("<head><title>NoStore Target</title></head>",
+                                 "text/html", {{"cache-control", "no-store"}}));
+
+  base::RunLoop nocache_waiter;
+  base::RunLoop nostore_waiter;
+  embedded_test_server()->RegisterRequestMonitor(base::BindRepeating(
+      &PrefetchBrowserTest::WatchURLAndRunClosure, base::Unretained(this),
+      nocache_url, &nocache_fetch_count, nocache_waiter.QuitClosure()));
+  embedded_test_server()->RegisterRequestMonitor(base::BindRepeating(
+      &PrefetchBrowserTest::WatchURLAndRunClosure, base::Unretained(this),
+      nostore_url, &nostore_fetch_count, nostore_waiter.QuitClosure()));
+  embedded_test_server()->RegisterRequestHandler(base::BindRepeating(
+      &PrefetchBrowserTest::ServeResponses, base::Unretained(this)));
+  ASSERT_TRUE(embedded_test_server()->Start());
+
+  // Loading a page that prefetches the target URL would increment the
+  // fetch count for the both targets.
+  NavigateToURL(shell(), embedded_test_server()->GetURL(prefetch_url));
+  nocache_waiter.Run();
+  nostore_waiter.Run();
+  EXPECT_EQ(1, nocache_fetch_count);
+  EXPECT_EQ(1, nostore_fetch_count);
+
+  {
+    // Subsequent navigation to the no-cache URL wouldn't hit the network,
+    // because no-cache resource is kept available up to kPrefetchReuseMins.
+    base::string16 title = base::ASCIIToUTF16("NoCache Target");
+    TitleWatcher title_watcher(shell()->web_contents(), title);
+    NavigateToURL(shell(), embedded_test_server()->GetURL(nocache_url));
+    EXPECT_EQ(title, title_watcher.WaitAndGetTitle());
+    EXPECT_EQ(1, nocache_fetch_count);
+  }
+  {
+    // Subsequent navigation to the no-store URL hit the network again,
+    // because no-store resource is not cached even for prefetch.
+    base::string16 title = base::ASCIIToUTF16("NoStore Target");
+    TitleWatcher title_watcher(shell()->web_contents(), title);
+    NavigateToURL(shell(), embedded_test_server()->GetURL(nostore_url));
+    EXPECT_EQ(title, title_watcher.WaitAndGetTitle());
+    EXPECT_EQ(2, nostore_fetch_count);
+  }
+}
+
+IN_PROC_BROWSER_TEST_P(PrefetchBrowserTest, WithPreload) {
+  int target_fetch_count = 0;
+  int preload_fetch_count = 0;
+  const char* prefetch_url = "/prefetch.html";
+  const char* target_url = "/target.html";
+  const char* preload_url = "/preload.js";
+  RegisterResponse(
+      prefetch_url,
+      ResponseEntry(base::StringPrintf(
+          "<body><link rel='prefetch' href='%s'></body>", target_url)));
+  RegisterResponse(
+      target_url,
+      ResponseEntry(
+          "<head><title>Prefetch Target</title></head>", "text/html",
+          {{"link", "<./preload.js>;rel=\"preload\";as=\"script\""}}));
+  RegisterResponse(preload_url,
+                   ResponseEntry("function foo() {}", "text/javascript"));
+
+  base::RunLoop preload_waiter;
+  embedded_test_server()->RegisterRequestMonitor(base::BindRepeating(
+      &PrefetchBrowserTest::WatchURLAndRunClosure, base::Unretained(this),
+      target_url, &target_fetch_count, base::RepeatingClosure()));
+  embedded_test_server()->RegisterRequestMonitor(base::BindRepeating(
+      &PrefetchBrowserTest::WatchURLAndRunClosure, base::Unretained(this),
+      preload_url, &preload_fetch_count, preload_waiter.QuitClosure()));
+  embedded_test_server()->RegisterRequestHandler(base::BindRepeating(
+      &PrefetchBrowserTest::ServeResponses, base::Unretained(this)));
+  ASSERT_TRUE(embedded_test_server()->Start());
+
+  // Loading a page that prefetches the target URL would increment both
+  // |target_fetch_count| and |preload_fetch_count|.
+  NavigateToURL(shell(), embedded_test_server()->GetURL(prefetch_url));
+  preload_waiter.Run();
+  EXPECT_EQ(1, target_fetch_count);
+  EXPECT_EQ(1, preload_fetch_count);
+
+  int preload_url_entries = 0;
+  while (preload_url_entries == 0) {
+    const bool result = ExecuteScriptAndExtractInt(
+        shell()->web_contents(),
+        base::StringPrintf(
+            "window.domAutomationController.send(performance.getEntriesByName('"
+            "%s').length)",
+            embedded_test_server()->GetURL(preload_url).spec().c_str()),
+        &preload_url_entries);
+    ASSERT_TRUE(result);
+  }
+  EXPECT_GE(preload_url_entries, 1);
 }
 
 INSTANTIATE_TEST_CASE_P(PrefetchBrowserTest,
