18eebeafed7895dd667bbcdd454a1bf7713f9ba7
Zhenyao Mo <zmo@chromium.org>
2018-02-09 20:23:48 +0000

    Remove dead code related with ContentGpuClient and GL strings.
    Now we collect GPUInfo on GPU startup, we always initialize GL bindings,
    create a GL contetx, and collect the GL strings. No need for specific
    mechanism on ChromeCAST.
    
    BUG=744658,808119
    TEST=bots
    R=halliwell@chromium.org,piman@chromium.org
    
    Change-Id: I23183baa03349e6fa3ce9d6598cbed426bd160a8
    Reviewed-on: https://chromium-review.googlesource.com/907860
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535807}
    Former-commit-id: 5788a1b9e936ed523f8390eaa62a06a7b073bd7e

--- chromecast/app/cast_main_delegate.h
+++ chromecast/app/cast_main_delegate.h
@@ -23,7 +23,6 @@ class CastResourceDelegate;
 namespace shell {
 
 class CastContentBrowserClient;
-class CastContentGpuClient;
 class CastContentRendererClient;
 class CastContentUtilityClient;
 
@@ -42,7 +41,6 @@ class CastMainDelegate : public content::ContentMainDelegate {
   void ZygoteForked() override;
 #endif  // defined(OS_LINUX)
   content::ContentBrowserClient* CreateContentBrowserClient() override;
-  content::ContentGpuClient* CreateContentGpuClient() override;
   content::ContentRendererClient* CreateContentRendererClient() override;
   content::ContentUtilityClient* CreateContentUtilityClient() override;
 
@@ -50,7 +48,6 @@ class CastMainDelegate : public content::ContentMainDelegate {
   void InitializeResourceBundle();
 
   std::unique_ptr<CastContentBrowserClient> browser_client_;
-  std::unique_ptr<CastContentGpuClient> gpu_client_;
   std::unique_ptr<CastContentRendererClient> renderer_client_;
   std::unique_ptr<CastContentUtilityClient> utility_client_;
   std::unique_ptr<CastResourceDelegate> resource_delegate_;
