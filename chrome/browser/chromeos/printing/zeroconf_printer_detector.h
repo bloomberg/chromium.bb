0e7c0513ba24a84db5e3e00d0ec2f8b9d77da6bc
Justin Carlson <justincarlson@chromium.org>
2018-02-09 16:25:35 +0000

    Update ZeroconfPrinterDetector.
    Add flushing semantics consistent with (some) other device lister
    users.  This involved relatively intrusive changes, because we have to
    track *which* service a given entry came from, and, to be correct, we
    have to keep overridden entries around in case the overriding entry
    disappears.
    
    Also add substantial unit tests, which are sort of hairy and
    complicated.  Sorry.  For the hairiness.  Not the tests.  Mostly.
    
    Bug: 761536
    Change-Id: I694119edc85e8e4486b1b53e04dce643e95baaf5
    Reviewed-on: https://chromium-review.googlesource.com/905474
    Commit-Queue: Justin Carlson <justincarlson@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535729}
    Former-commit-id: 8c6fd10001315b2192c4d536ea2e51d578b5e3b6

--- chrome/browser/chromeos/printing/zeroconf_printer_detector.h
+++ chrome/browser/chromeos/printing/zeroconf_printer_detector.h
@@ -4,21 +4,39 @@
 #ifndef CHROME_BROWSER_CHROMEOS_PRINTING_ZEROCONF_PRINTER_DETECTOR_H_
 #define CHROME_BROWSER_CHROMEOS_PRINTING_ZEROCONF_PRINTER_DETECTOR_H_
 
+#include <map>
 #include <memory>
+#include <string>
 
 #include "chrome/browser/chromeos/printing/printer_detector.h"
-
-class Profile;
+#include "chrome/browser/local_discovery/service_discovery_device_lister.h"
 
 namespace chromeos {
 
-// Use mDNS and DNS-SD to detect nearby networked printers.  This
-// is sometimes called zeroconf, or Bonjour.
-class ZeroconfPrinterDetector : public PrinterDetector {
+// Use mDNS and DNS-SD to detect nearby networked printers.  This is sometimes
+// called zeroconf, or Bonjour.  Or Rendezvous.
+class ZeroconfPrinterDetector
+    : public PrinterDetector,
+      public local_discovery::ServiceDiscoveryDeviceLister::Delegate {
  public:
+  // Service types used by the detector.
+  static const char* kIppServiceName;
+  static const char* kIppsServiceName;
+  static const char* kIppEverywhereServiceName;
+  static const char* kIppsEverywhereServiceName;
+
   ~ZeroconfPrinterDetector() override = default;
 
-  static std::unique_ptr<ZeroconfPrinterDetector> Create(Profile* profile);
+  static std::unique_ptr<ZeroconfPrinterDetector> Create();
+
+  // Create an instance that uses the passed device listers instead of creating
+  // its own.  |device_listers| is a map from service type to lister, and should
+  // supply a lister for each of the service names used by the detector.
+  // Ownership is taken of the map storage.
+  static std::unique_ptr<ZeroconfPrinterDetector> CreateForTesting(
+      std::map<std::string,
+               std::unique_ptr<local_discovery::ServiceDiscoveryDeviceLister>>*
+          device_listers);
 };
 
 }  // namespace chromeos
