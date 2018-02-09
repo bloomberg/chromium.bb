a14ab4d6db6d5635104ef54d42e7c45d83402a7b
Pavol Marko <pmarko@chromium.org>
2018-02-09 14:58:44 +0000

    cryptohome: Use MigrateToDircrypto instead of MigrateToDircryptoEx
    After CL:900299, MigrateToDircryptoEx and MigrateToDircrypto are
    synonymous, so switch back to MigrateToDircrypto.
    
    BUG=758837
    
    Change-Id: Id3eae32963c06491ad74464e6641a6acb4c1b008
    Reviewed-on: https://chromium-review.googlesource.com/901243
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535710}
    Former-commit-id: 8fb734513eb3d69a9a153057a044ec134f7eab24

--- chromeos/dbus/cryptohome_client.cc
+++ chromeos/dbus/cryptohome_client.cc
@@ -890,10 +890,8 @@ class CryptohomeClientImpl : public CryptohomeClient {
   void MigrateToDircrypto(const cryptohome::Identification& cryptohome_id,
                           const cryptohome::MigrateToDircryptoRequest& request,
                           VoidDBusMethodCallback callback) override {
-    // TODO(bug758837,pmarko): Switch back to MigrateToDircrypto when its
-    // signature matches MigrateToDircryptoEx.
     dbus::MethodCall method_call(cryptohome::kCryptohomeInterface,
-                                 cryptohome::kCryptohomeMigrateToDircryptoEx);
+                                 cryptohome::kCryptohomeMigrateToDircrypto);
 
     cryptohome::AccountIdentifier id_proto;
     FillIdentificationProtobuf(cryptohome_id, &id_proto);
