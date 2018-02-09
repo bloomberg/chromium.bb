3f2413462e4b79295bd16287db448f4866a62487
John Abd-El-Malek <jam@chromium.org>
2018-02-09 19:22:29 +0000

    Add a helper for testing code that take in a URLLoaderFactory.
    This allows priming a TestURLLoaderFactory with responses based on URLs, which are then served when the factory is called.
    
    Bug: 773295
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Iaed636efc8d099764604269d7bebf487faa109f0
    Reviewed-on: https://chromium-review.googlesource.com/911510
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535779}
    Former-commit-id: 7730c51cad6702fdc508ae5c798c85c011e2b799

---
+++ services/network/test/test_url_loader_factory.h
@@ -0,0 +1,81 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_NETWORK_TEST_TEST_URL_LOADER_FACTORY_H_
+#define SERVICES_NETWORK_TEST_TEST_URL_LOADER_FACTORY_H_
+
+#include <vector>
+
+#include "base/macros.h"
+#include "services/network/public/interfaces/url_loader_factory.mojom.h"
+
+namespace network {
+
+// A helper class to ease testing code that uses URLLoader interface. A test
+// would pass this factory instead of the production factory to code, and
+// would prime it with response data for arbitrary URLs.
+class TestURLLoaderFactory : public mojom::URLLoaderFactory {
+ public:
+  TestURLLoaderFactory();
+  ~TestURLLoaderFactory() override;
+
+  using Redirects =
+      std::vector<std::pair<net::RedirectInfo, ResourceResponseHead>>;
+
+  // Adds a response to be served. Once served it will be removed, so if you
+  // expect n requests to the same URL call this method n times.
+  // This can be called before or after a request is made. If it's called after,
+  // then pending requests will be "woken up".
+  void AddResponse(const GURL& url,
+                   const ResourceResponseHead& head,
+                   const std::string& content,
+                   const URLLoaderCompletionStatus& status,
+                   const Redirects& redirects = Redirects());
+
+  // Simpler version of above for the common success case.
+  void AddResponse(const std::string& url, const std::string& content);
+
+  // mojom::URLLoaderFactory implementation.
+  void CreateLoaderAndStart(mojom::URLLoaderRequest request,
+                            int32_t routing_id,
+                            int32_t request_id,
+                            uint32_t options,
+                            const ResourceRequest& url_request,
+                            mojom::URLLoaderClientPtr client,
+                            const net::MutableNetworkTrafficAnnotationTag&
+                                traffic_annotation) override;
+  void Clone(mojom::URLLoaderFactoryRequest request) override;
+
+ private:
+  bool CreateLoaderAndStartInternal(const GURL& url,
+                                    mojom::URLLoaderClient* client);
+
+  struct Response {
+    Response();
+    ~Response();
+    Response(const Response&);
+    GURL url;
+    Redirects redirects;
+    ResourceResponseHead head;
+    std::string content;
+    URLLoaderCompletionStatus status;
+  };
+  std::vector<Response> responses_;
+
+  struct Pending {
+    Pending();
+    ~Pending();
+    Pending(Pending&& other);
+    Pending& operator=(Pending&& other);
+    GURL url;
+    mojom::URLLoaderClientPtr client;
+  };
+  std::vector<Pending> pending_;
+
+  DISALLOW_COPY_AND_ASSIGN(TestURLLoaderFactory);
+};
+
+}  // namespace network
+
+#endif  // SERVICES_NETWORK_TEST_TEST_URL_LOADER_FACTORY_H_
---
+++ services/network/test/test_url_loader_factory_unittest.cc
@@ -0,0 +1,99 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/network/test/test_url_loader_factory.h"
+#include "base/logging.h"
+#include "base/test/scoped_task_environment.h"
+#include "mojo/common/data_pipe_utils.h"
+#include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
+#include "services/network/test/test_url_loader_client.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace network {
+
+class TestURLLoaderFactoryTest : public testing::Test {
+ public:
+  TestURLLoaderFactoryTest() {}
+  ~TestURLLoaderFactoryTest() override {}
+
+  void SetUp() override {}
+
+  void StartRequest(const std::string& url) { StartRequest(url, &client_); }
+
+  void StartRequest(const std::string& url, TestURLLoaderClient* client) {
+    ResourceRequest request;
+    request.url = GURL(url);
+    factory_.CreateLoaderAndStart(
+        mojo::MakeRequest(&loader_), 0, 0, 0, request,
+        client->CreateInterfacePtr(),
+        net::MutableNetworkTrafficAnnotationTag(TRAFFIC_ANNOTATION_FOR_TESTS));
+  }
+
+  std::string GetData(TestURLLoaderClient* client) {
+    std::string response;
+    EXPECT_TRUE(client->response_body().is_valid());
+    EXPECT_TRUE(mojo::common::BlockingCopyToString(
+        client->response_body_release(), &response));
+    return response;
+  }
+
+  TestURLLoaderFactory* factory() { return &factory_; }
+  TestURLLoaderClient* client() { return &client_; }
+
+ private:
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
+  TestURLLoaderFactory factory_;
+  mojom::URLLoaderPtr loader_;
+  TestURLLoaderClient client_;
+};
+
+TEST_F(TestURLLoaderFactoryTest, Simple) {
+  std::string url = "http://foo";
+  std::string data = "bar";
+
+  factory()->AddResponse(url, data);
+  StartRequest(url);
+  client()->RunUntilComplete();
+  EXPECT_EQ(GetData(client()), data);
+}
+
+TEST_F(TestURLLoaderFactoryTest, MultipleSameURL) {
+  std::string url = "http://foo";
+  std::string data = "bar";
+
+  factory()->AddResponse(url, data);
+  factory()->AddResponse(url, data);
+
+  StartRequest(url);
+  client()->RunUntilComplete();
+  EXPECT_EQ(GetData(client()), data);
+
+  mojom::URLLoaderPtr loader2;
+  TestURLLoaderClient client2;
+  StartRequest(url, &client2);
+  client2.RunUntilComplete();
+  EXPECT_EQ(GetData(&client2), data);
+}
+
+TEST_F(TestURLLoaderFactoryTest, MultipleDifferentURL) {
+  std::string url1 = "http://foo";
+  std::string data1 = "bar";
+  factory()->AddResponse(url1, data1);
+
+  StartRequest(url1);
+  client()->RunUntilComplete();
+  EXPECT_EQ(GetData(client()), data1);
+
+  std::string url2 = "http://foo2";
+  std::string data2 = "bar2";
+  factory()->AddResponse(url2, data2);
+
+  mojom::URLLoaderPtr loader2;
+  TestURLLoaderClient client2;
+  StartRequest(url2, &client2);
+  client2.RunUntilComplete();
+  EXPECT_EQ(GetData(&client2), data2);
+}
+
+}  // namespace network
