6712d7d441bacd2ecfae9faa1fed7eeeced20c11
Bill Orr <billorr@chromium.org>
2018-02-09 04:11:43 +0000

    Fix black screen when presenting on some WebVR pages on Windows
    The previous fix was incomplete - it fixed the passthrough command buffer,
    but doesn't work with the validating command buffer.  This has been
    tested with both command buffers.
    
    The issue is that the validating command buffer's implementation of
    CopySubTextureCHROMIUM allows conversions RGB->RGBA, but not RGBA->RGB,
    while the passthrough is ultimately ignoring the fact that the destination
    was requesting to be RGB.
    
    BUG=804628
    
    Change-Id: Ia3c900c2d4a73af832c592964e67f33f93dbbd2d
    Reviewed-on: https://chromium-review.googlesource.com/910450
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Bill Orr <billorr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535631}
    Former-commit-id: 134756fec9112709f082c0895fe8921142797f82

--- third_party/WebKit/Source/platform/graphics/GpuMemoryBufferImageCopy.cpp
+++ third_party/WebKit/Source/platform/graphics/GpuMemoryBufferImageCopy.cpp
@@ -31,7 +31,7 @@ bool GpuMemoryBufferImageCopy::EnsureMemoryBuffer(int width, int height) {
       return false;
 
     gpu_memory_buffer_ = gpu_memory_buffer_manager->CreateGpuMemoryBuffer(
-        gfx::Size(width, height), gfx::BufferFormat::RGBX_8888,
+        gfx::Size(width, height), gfx::BufferFormat::RGBA_8888,
         gfx::BufferUsage::SCANOUT, gpu::kNullSurfaceHandle);
     if (!gpu_memory_buffer_)
       return false;
@@ -55,7 +55,7 @@ gfx::GpuMemoryBuffer* GpuMemoryBufferImageCopy::CopyImage(Image* image) {
 
   // Bind the write framebuffer to our memory buffer.
   GLuint image_id = gl_->CreateImageCHROMIUM(
-      gpu_memory_buffer_->AsClientBuffer(), width, height, GL_RGB);
+      gpu_memory_buffer_->AsClientBuffer(), width, height, GL_RGBA);
   if (!image_id)
     return nullptr;
   GLuint dest_texture_id;
