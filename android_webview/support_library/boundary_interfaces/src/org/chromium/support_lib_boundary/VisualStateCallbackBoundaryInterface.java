ba6bf7a51ce6fb5c19ed9993870f8e6ec22b9b67
Gustav Sennton <gsennton@google.com>
2018-02-09 15:10:33 +0000

    Rename boundary interfaces, and pass WebView to WVFP.createWebView.
    Suffix all boundary interfaces with 'BoundaryInterfaces'.
    
    Pass WebView instead of an Object representing a WebViewProvider to the
    method for creating support library WebViewProviders - the chromium-side
    is compiled against the system sdk so we can fetch the corresponding
    WebViewProvider from the given WebView from there.
    
    Bug: 788177
    Change-Id: I51d71ecab79624902031e5fdab247f7d903ae335
    Reviewed-on: https://chromium-review.googlesource.com/911088
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Gustav Sennton <gsennton@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535714}
    Former-commit-id: 4235bf67f2a85c2e044b22b4cf37e2243c1912ac

--- android_webview/support_library/boundary_interfaces/src/org/chromium/support_lib_boundary/VisualStateCallbackBoundaryInterface.java
+++ android_webview/support_library/boundary_interfaces/src/org/chromium/support_lib_boundary/VisualStateCallbackBoundaryInterface.java
@@ -1,4 +1,4 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
+// Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,4 +6,4 @@ package org.chromium.support_lib_boundary;
 
 /**
  */
-public interface VisualStateCallbackInterface { void onComplete(long requestId); }
+public interface VisualStateCallbackBoundaryInterface { void onComplete(long requestId); }
