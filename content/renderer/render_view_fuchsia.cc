da11449512109e3177c3642181261aec4c120f8a
Sergey Ulanov <sergeyu@google.com>
2018-02-10 03:09:46 +0000

    [Fuchsia] Implement RenderViewImpl::UpdateFontRenderingFromRendererPrefs()
    Bug: 778467
    Change-Id: I4d5c662915b34237f706abdccd110748f1dbabcc
    Reviewed-on: https://chromium-review.googlesource.com/912669
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535947}
    Former-commit-id: 2462e59f962af98014a02c1cf2b9e8804cd36c10

--- content/renderer/render_view_fuchsia.cc
+++ content/renderer/render_view_fuchsia.cc
@@ -4,11 +4,49 @@
 
 #include "content/renderer/render_view_impl.h"
 
+#include "third_party/WebKit/public/platform/WebFontRenderStyle.h"
+
 namespace content {
 
+namespace {
+
+SkPaint::Hinting RendererPreferencesToSkiaHinting(
+    const RendererPreferences& prefs) {
+  switch (prefs.hinting) {
+    case gfx::FontRenderParams::HINTING_NONE:
+      return SkPaint::kNo_Hinting;
+    case gfx::FontRenderParams::HINTING_SLIGHT:
+      return SkPaint::kSlight_Hinting;
+    case gfx::FontRenderParams::HINTING_MEDIUM:
+      return SkPaint::kNormal_Hinting;
+    case gfx::FontRenderParams::HINTING_FULL:
+      return SkPaint::kFull_Hinting;
+    default:
+      NOTREACHED();
+      return SkPaint::kNormal_Hinting;
+  }
+}
+
+}  // namespace
+
 void RenderViewImpl::UpdateFontRenderingFromRendererPrefs() {
-  // TODO(fuchsia): Implement this. See crbug.com/750946
-  NOTIMPLEMENTED();
+  const RendererPreferences& prefs = renderer_preferences_;
+  blink::WebFontRenderStyle::SetHinting(
+      RendererPreferencesToSkiaHinting(prefs));
+  blink::WebFontRenderStyle::SetAutoHint(prefs.use_autohinter);
+  blink::WebFontRenderStyle::SetUseBitmaps(prefs.use_bitmaps);
+  SkFontLCDConfig::SetSubpixelOrder(
+      gfx::FontRenderParams::SubpixelRenderingToSkiaLCDOrder(
+          prefs.subpixel_rendering));
+  SkFontLCDConfig::SetSubpixelOrientation(
+      gfx::FontRenderParams::SubpixelRenderingToSkiaLCDOrientation(
+          prefs.subpixel_rendering));
+  blink::WebFontRenderStyle::SetAntiAlias(prefs.should_antialias_text);
+  blink::WebFontRenderStyle::SetSubpixelRendering(
+      prefs.subpixel_rendering !=
+      gfx::FontRenderParams::SUBPIXEL_RENDERING_NONE);
+  blink::WebFontRenderStyle::SetSubpixelPositioning(
+      prefs.use_subpixel_positioning);
 }
 
 }  // namespace content
