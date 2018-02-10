f7b8acf504c87886fc0449a4e9b65d6cfe4023d8
Victor Miura <vmiura@chromium.org>
2018-02-10 02:56:46 +0000

    Fix and re-enable GpuPreferencesUtilTest.EncodeDecode test.
    BUG=799458
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ib2898c97168b164c718dcc3c0303950c4c271659
    Reviewed-on: https://chromium-review.googlesource.com/852639
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Victor Miura <vmiura@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535944}
    Former-commit-id: 015cbc255a73cefb26c2b9fbe3102f4ac26eece1

--- gpu/ipc/common/gpu_preferences_util_unittest.cc
+++ gpu/ipc/common/gpu_preferences_util_unittest.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <algorithm>
 #include <cstring>
 
 #include "build/build_config.h"
@@ -13,46 +14,79 @@ namespace gpu {
 
 namespace {
 
-class ScopedGpuPreferences {
- public:
-  ScopedGpuPreferences() {
-    // To make sure paddings are zeroed so we can use memcmp() in the tests.
-    memset(buffer_, 0, sizeof(buffer_));
-    prefs_ = new (buffer_) GpuPreferences();
-  }
-
-  ~ScopedGpuPreferences() { prefs_->~GpuPreferences(); }
-
-  GpuPreferences& Ref() { return *prefs_; }
-
- private:
-  GpuPreferences* prefs_;
-  alignas(GpuPreferences) char buffer_[sizeof(GpuPreferences)];
-};
+void CheckGpuPreferencesEqual(GpuPreferences left, GpuPreferences right) {
+  EXPECT_EQ(left.single_process, right.single_process);
+  EXPECT_EQ(left.in_process_gpu, right.in_process_gpu);
+  EXPECT_EQ(left.disable_accelerated_video_decode,
+            right.disable_accelerated_video_decode);
+  EXPECT_EQ(left.disable_accelerated_video_encode,
+            right.disable_accelerated_video_encode);
+  EXPECT_EQ(left.gpu_startup_dialog, right.gpu_startup_dialog);
+  EXPECT_EQ(left.disable_gpu_watchdog, right.disable_gpu_watchdog);
+  EXPECT_EQ(left.gpu_sandbox_start_early, right.gpu_sandbox_start_early);
+  EXPECT_EQ(left.enable_accelerated_vpx_decode,
+            right.enable_accelerated_vpx_decode);
+  EXPECT_EQ(left.enable_low_latency_dxva, right.enable_low_latency_dxva);
+  EXPECT_EQ(left.enable_zero_copy_dxgi_video,
+            right.enable_zero_copy_dxgi_video);
+  EXPECT_EQ(left.enable_nv12_dxgi_video, right.enable_nv12_dxgi_video);
+  EXPECT_EQ(left.enable_media_foundation_vea_on_windows7,
+            right.enable_media_foundation_vea_on_windows7);
+  EXPECT_EQ(left.disable_software_rasterizer,
+            right.disable_software_rasterizer);
+  EXPECT_EQ(left.log_gpu_control_list_decisions,
+            right.log_gpu_control_list_decisions);
+  EXPECT_EQ(left.compile_shader_always_succeeds,
+            right.compile_shader_always_succeeds);
+  EXPECT_EQ(left.disable_gl_error_limit, right.disable_gl_error_limit);
+  EXPECT_EQ(left.disable_glsl_translator, right.disable_glsl_translator);
+  EXPECT_EQ(left.disable_shader_name_hashing,
+            right.disable_shader_name_hashing);
+  EXPECT_EQ(left.enable_gpu_command_logging, right.enable_gpu_command_logging);
+  EXPECT_EQ(left.enable_gpu_debugging, right.enable_gpu_debugging);
+  EXPECT_EQ(left.enable_gpu_service_logging_gpu,
+            right.enable_gpu_service_logging_gpu);
+  EXPECT_EQ(left.enable_gpu_driver_debug_logging,
+            right.enable_gpu_driver_debug_logging);
+  EXPECT_EQ(left.disable_gpu_program_cache, right.disable_gpu_program_cache);
+  EXPECT_EQ(left.enforce_gl_minimums, right.enforce_gl_minimums);
+  EXPECT_EQ(left.force_gpu_mem_available, right.force_gpu_mem_available);
+  EXPECT_EQ(left.gpu_program_cache_size, right.gpu_program_cache_size);
+  EXPECT_EQ(left.disable_gpu_shader_disk_cache,
+            right.disable_gpu_shader_disk_cache);
+  EXPECT_EQ(left.enable_threaded_texture_mailboxes,
+            right.enable_threaded_texture_mailboxes);
+  EXPECT_EQ(left.gl_shader_interm_output, right.gl_shader_interm_output);
+  EXPECT_EQ(left.emulate_shader_precision, right.emulate_shader_precision);
+  EXPECT_EQ(left.enable_raster_decoder, right.enable_raster_decoder);
+  EXPECT_EQ(left.enable_gpu_service_logging, right.enable_gpu_service_logging);
+  EXPECT_EQ(left.enable_gpu_service_tracing, right.enable_gpu_service_tracing);
+  EXPECT_EQ(left.use_passthrough_cmd_decoder,
+            right.use_passthrough_cmd_decoder);
+  EXPECT_EQ(left.disable_biplanar_gpu_memory_buffers_for_video_frames,
+            right.disable_biplanar_gpu_memory_buffers_for_video_frames);
+  EXPECT_EQ(left.texture_target_exception_list,
+            right.texture_target_exception_list);
+  EXPECT_EQ(left.disable_gpu_driver_bug_workarounds,
+            right.disable_gpu_driver_bug_workarounds);
+  EXPECT_EQ(left.ignore_gpu_blacklist, right.ignore_gpu_blacklist);
+}
 
 }  // namespace
 
-// TODO(https://crbug.com/799458): Fix this test.
-#if defined(OS_WIN)
-#define MAYBE_EncodeDecode DISABLED_EncodeDecode
-#else
-#define MAYBE_EncodeDecode EncodeDecode
-#endif
-TEST(GpuPreferencesUtilTest, MAYBE_EncodeDecode) {
+TEST(GpuPreferencesUtilTest, EncodeDecode) {
   {  // Testing default values.
-    ScopedGpuPreferences scoped_input_prefs, scoped_decoded_prefs;
-    GpuPreferences& input_prefs = scoped_input_prefs.Ref();
-    GpuPreferences& decoded_prefs = scoped_decoded_prefs.Ref();
+    GpuPreferences input_prefs;
+    GpuPreferences decoded_prefs;
     std::string encoded = GpuPreferencesToSwitchValue(input_prefs);
     bool flag = SwitchValueToGpuPreferences(encoded, &decoded_prefs);
     EXPECT_TRUE(flag);
-    EXPECT_EQ(0, memcmp(&input_prefs, &decoded_prefs, sizeof(input_prefs)));
+    CheckGpuPreferencesEqual(input_prefs, decoded_prefs);
   }
 
   {  // Change all fields to non default values.
-    ScopedGpuPreferences scoped_input_prefs, scoped_decoded_prefs;
-    GpuPreferences& input_prefs = scoped_input_prefs.Ref();
-    GpuPreferences& decoded_prefs = scoped_decoded_prefs.Ref();
+    GpuPreferences input_prefs;
+    GpuPreferences decoded_prefs;
 
     GpuPreferences default_prefs;
     mojom::GpuPreferences prefs_mojom;
@@ -101,6 +135,7 @@ TEST(GpuPreferencesUtilTest, MAYBE_EncodeDecode) {
     GPU_PREFERENCES_FIELD(enable_threaded_texture_mailboxes, true)
     GPU_PREFERENCES_FIELD(gl_shader_interm_output, true)
     GPU_PREFERENCES_FIELD(emulate_shader_precision, true)
+    GPU_PREFERENCES_FIELD(enable_raster_decoder, true)
     GPU_PREFERENCES_FIELD(enable_gpu_service_logging, true)
     GPU_PREFERENCES_FIELD(enable_gpu_service_tracing, true)
     GPU_PREFERENCES_FIELD(use_passthrough_cmd_decoder, true)
@@ -109,11 +144,16 @@ TEST(GpuPreferencesUtilTest, MAYBE_EncodeDecode) {
     GPU_PREFERENCES_FIELD(disable_gpu_driver_bug_workarounds, true)
     GPU_PREFERENCES_FIELD(ignore_gpu_blacklist, true)
 
+    input_prefs.texture_target_exception_list.emplace_back(
+        gfx::BufferUsage::SCANOUT, gfx::BufferFormat::RGBA_8888);
+    input_prefs.texture_target_exception_list.emplace_back(
+        gfx::BufferUsage::GPU_READ, gfx::BufferFormat::BGRA_8888);
+
     // Make sure every field is encoded/decoded.
     std::string encoded = GpuPreferencesToSwitchValue(input_prefs);
     bool flag = SwitchValueToGpuPreferences(encoded, &decoded_prefs);
     EXPECT_TRUE(flag);
-    EXPECT_EQ(0, memcmp(&input_prefs, &decoded_prefs, sizeof(input_prefs)));
+    CheckGpuPreferencesEqual(input_prefs, decoded_prefs);
   }
 }
 
