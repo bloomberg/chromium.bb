a1c375751b46e4f1efa334598bc5095a41485e27
John Budorick <jbudorick@chromium.org>
2018-02-10 00:53:10 +0000

    Switch //build/print_python_deps.py to vpython. (RELAND)
    Bug: 800662
    Change-Id: I3907c09c3b8898c0e487348735a8bb582ff9e468
    Reviewed-on: https://chromium-review.googlesource.com/881466
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#535905}
    Former-commit-id: 47ca3fe45074c35b30670cfa4e1f4117a4c676c5

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -2079,13 +2079,14 @@ class PydepsChecker(object):
   def DetermineIfStale(self, pydeps_path):
     """Runs print_python_deps.py to see if the files is stale."""
     import difflib
+    import os
+
     old_pydeps_data = self._LoadFile(pydeps_path).splitlines()
     cmd = old_pydeps_data[1][1:].strip()
-    env = {
-      'PYTHONDONTWRITEBYTECODE': '1'
-    }
+    env = dict(os.environ)
+    env['PYTHONDONTWRITEBYTECODE'] = '1'
     new_pydeps_data = self._input_api.subprocess.check_output(
-        cmd  + ' --output ""', shell=True, env=env)
+        cmd + ' --output ""', shell=True, env=env)
     old_contents = old_pydeps_data[2:]
     new_contents = new_pydeps_data.splitlines()[2:]
     if old_pydeps_data[2:] != new_pydeps_data.splitlines()[2:]:
