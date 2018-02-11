d97cf0b91fa8d0f9cadf0fbd69fa8e40d16b5871
Kush Sinha <sinhak@chromium.org>
2018-02-11 17:39:05 +0000

    Enable Mirror on Chrome OS if set by preference
    Enable Mirror account reconciliation on Chrome OS if the preference
    |kAccountConsistencyMirrorRequired| is set. This is a Chrome OS specific
    preference and was being used to stop sign in from secondary Google accounts
    and sign out from the primary account. Enabling Mirror here will have the
    effect of users' cookies getting re-minted if they clear their cookies from
    the Settings page.
    
    Bug: 805519
    Test: out/ChromeOS/unit_tests --gtest_filter="*AccountConsistencyModeManagerTest*"
    Change-Id: Ie8fe9c4be9868d1301f6063dc84764a1c9dea028
    Reviewed-on: https://chromium-review.googlesource.com/906430
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536005}
    Former-commit-id: c03de0efe26376dedc7b14557dba1d3f5e3f7dc0

--- chrome/browser/signin/account_consistency_mode_manager_unittest.cc
+++ chrome/browser/signin/account_consistency_mode_manager_unittest.cc
@@ -17,6 +17,7 @@
 #include "components/signin/core/browser/profile_management_switches.h"
 #include "components/signin/core/browser/scoped_account_consistency.h"
 #include "components/signin/core/browser/signin_features.h"
+#include "components/signin/core/browser/signin_pref_names.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "content/public/common/content_features.h"
 #include "content/public/test/test_browser_thread_bundle.h"
@@ -143,18 +144,27 @@ TEST(AccountConsistencyModeManagerTest, MirrorDisabledForNonUnicorn) {
             AccountConsistencyModeManager::GetMethodForProfile(&profile));
 }
 
-TEST(AccountConsistencyModeManagerTest, MirrorEnabledForUnicorn) {
+TEST(AccountConsistencyModeManagerTest, MirrorEnabledByPreference) {
   // Creation of this object sets the current thread's id as UI thread.
   content::TestBrowserThreadBundle test_thread_bundle;
 
-  TestingProfile profile;
-  profile.SetSupervisedUserId(supervised_users::kChildAccountSUID);
+  TestingProfile::Builder profile_builder;
+  {
+    std::unique_ptr<sync_preferences::TestingPrefServiceSyncable> pref_service =
+        std::make_unique<sync_preferences::TestingPrefServiceSyncable>();
+    RegisterUserProfilePrefs(pref_service->registry());
+    profile_builder.SetPrefService(std::move(pref_service));
+  }
+  std::unique_ptr<TestingProfile> profile = profile_builder.Build();
+  profile->GetPrefs()->SetBoolean(prefs::kAccountConsistencyMirrorRequired,
+                                  true);
+
   EXPECT_TRUE(
-      AccountConsistencyModeManager::IsMirrorEnabledForProfile(&profile));
+      AccountConsistencyModeManager::IsMirrorEnabledForProfile(profile.get()));
   EXPECT_EQ(signin::AccountConsistencyMethod::kMirror,
-            AccountConsistencyModeManager::GetMethodForProfile(&profile));
+            AccountConsistencyModeManager::GetMethodForProfile(profile.get()));
 }
-#endif
+#endif  // defined(OS_CHROMEOS)
 
 #if BUILDFLAG(ENABLE_MIRROR)
 TEST(AccountConsistencyModeManagerTest, MirrorEnabled) {
@@ -175,4 +185,4 @@ TEST(AccountConsistencyModeManagerTest, MirrorEnabled) {
   EXPECT_EQ(signin::AccountConsistencyMethod::kMirror,
             AccountConsistencyModeManager::GetMethodForProfile(&profile));
 }
-#endif
+#endif  // BUILDFLAG(ENABLE_MIRROR)
