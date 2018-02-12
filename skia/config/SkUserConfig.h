403a4ed2cd0d861fb542cb17b09f0630bed2236a
Marc Treib <treib@chromium.org>
2018-02-12 09:06:16 +0000

    Revert "opt Chromium back into HQ scaling clamps"
    This reverts commit 3bb748985003b0064a01fb216866ef80c1834731 [formerly 58525850de9c493fd4f59e008d9e9ed44534fe02].
    
    Reason for revert: Broke fast/webgl/texImage-imageBitmap-from-image-resize.html on Mac 10.11:
    https://ci.chromium.org/buildbot/chromium.mac/Mac10.11%20Tests/23254
    
    Original change's description:
    > opt Chromium back into HQ scaling clamps
    > 
    > Change-Id: I2693be780057c1ba715fce3ae3c398c536f147ea
    > Reviewed-on: https://chromium-review.googlesource.com/911734
    > Reviewed-by: Florin Malita <fmalita@chromium.org>
    > Commit-Queue: Mike Klein <mtklein@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#536009}
    
    TBR=fmalita@chromium.org,brianosman@google.com,mtklein@chromium.org
    
    Change-Id: I01d68484de5892eeab4c1ec42e389ab8c0fdbde7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/912471
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536041}
    Former-commit-id: 4defcb39ba903cf2a4e96f008c4dfed8e1a69777

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -216,10 +216,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_DASH_CULL_PATH
 #endif
 
-#ifndef SK_LEGACY_HIGH_QUALITY_SCALING_CLAMP
-#define SK_LEGACY_HIGH_QUALITY_SCALING_CLAMP
-#endif
-
 // Max. verb count for paths rendered by the edge-AA tessellating path renderer.
 #define GR_AA_TESSELLATOR_MAX_VERB_COUNT 10
 
