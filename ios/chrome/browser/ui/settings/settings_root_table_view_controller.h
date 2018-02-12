bc0cc3830c55d59090f8d13a264ed2773ea3124d
Rohit Rao <rohitrao@google.com>
2018-02-12 16:59:20 +0000

    [ios] Creates a base class for Settings table views.
    Integrating with the MDC AppBar requires a bunch of boilerplate setup
    and scrollview delegate code.  Move that code to a separate base class,
    so that the tableview cell catalog class becomes simpler and easier to
    read.
    
    BUG=811075
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I8a14d2f8aacc60480c50ee1a361c3413b82dd20a
    Reviewed-on: https://chromium-review.googlesource.com/913669
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536109}
    Former-commit-id: 78f1c2cf7a53bd2b0d8d51924b3c92b7f53786dd

---
+++ ios/chrome/browser/ui/settings/settings_root_table_view_controller.h
@@ -0,0 +1,52 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_SETTINGS_SETTINGS_ROOT_TABLE_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_UI_SETTINGS_SETTINGS_ROOT_TABLE_VIEW_CONTROLLER_H_
+
+#import <UIKit/UIKit.h>
+
+#import "ios/chrome/browser/ui/material_components/app_bar_presenting.h"
+
+// SettingsRootTableViewController is a base class for integrating UITableViews
+// into the Settings UI.  It handles the configuration and display of the MDC
+// AppBar.
+@interface SettingsRootTableViewController
+    : UITableViewController<AppBarPresenting>
+
+- (instancetype)initWithStyle:(UITableViewStyle)style NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithNibName:(NSString*)name
+                         bundle:(NSBundle*)bundle NS_UNAVAILABLE;
+- (instancetype)initWithCoder:(NSCoder*)decoder NS_UNAVAILABLE;
+
+#pragma mark UIScrollViewDelegate
+
+// Updates the MDCFlexibleHeader with changes to the table view scroll
+// state. Must be called by subclasses if they override this method in order to
+// maintain this functionality.
+- (void)scrollViewDidScroll:(UIScrollView*)scrollView NS_REQUIRES_SUPER;
+
+// Updates the MDCFlexibleHeader with changes to the table view scroll
+// state. Must be called by subclasses if they override this method in order to
+// maintain this functionality.
+- (void)scrollViewDidEndDragging:(UIScrollView*)scrollView
+                  willDecelerate:(BOOL)decelerate NS_REQUIRES_SUPER;
+
+// Updates the MDCFlexibleHeader with changes to the table view scroll
+// state. Must be called by subclasses if they override this method in order to
+// maintain this functionality.
+- (void)scrollViewDidEndDecelerating:(UIScrollView*)scrollView
+    NS_REQUIRES_SUPER;
+
+// Updates the MDCFlexibleHeader with changes to the table view scroll
+// state. Must be called by subclasses if they override this method in order to
+// maintain this functionality.
+- (void)scrollViewWillEndDragging:(UIScrollView*)scrollView
+                     withVelocity:(CGPoint)velocity
+              targetContentOffset:(inout CGPoint*)targetContentOffset
+    NS_REQUIRES_SUPER;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_SETTINGS_SETTINGS_ROOT_TABLE_VIEW_CONTROLLER_H_
--- ios/chrome/browser/ui/settings/table_cell_catalog_view_controller.mm
+++ ios/chrome/browser/ui/settings/table_cell_catalog_view_controller.mm
@@ -4,31 +4,14 @@
 
 #import "ios/chrome/browser/ui/settings/table_cell_catalog_view_controller.h"
 
-#import "base/mac/foundation_util.h"
-#import "ios/chrome/browser/ui/material_components/utils.h"
-#import "ios/chrome/browser/ui/settings/settings_navigation_controller.h"
-#import "ios/third_party/material_components_ios/src/components/AppBar/src/MaterialAppBar.h"
-
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
-@interface TableCellCatalogViewController ()
-
-// The AppBar for this view controller.
-@property(nonatomic, readwrite, strong) MDCAppBar* appBar;
-
-@end
-
 @implementation TableCellCatalogViewController
-@synthesize appBar = _appBar;
 
 - (instancetype)init {
-  self = [super initWithStyle:UITableViewStyleGrouped];
-  if (self) {
-    // Configure the AppBar.
-    _appBar = [[MDCAppBar alloc] init];
-    [self addChildViewController:_appBar.headerViewController];
+  if ((self = [super initWithStyle:UITableViewStyleGrouped])) {
   }
   return self;
 }
@@ -36,35 +19,6 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   self.title = @"Table Cell Catalog";
-
-  // Configure the app bar.  This cannot be in a shared base class because
-  // UITableViewControllers normally do not use MDC styling, but this one does
-  // in order to match the other Settings screens.
-  ConfigureAppBarWithCardStyle(self.appBar);
-  self.appBar.headerViewController.headerView.trackingScrollView =
-      self.tableView;
-  // Add the AppBar's views after all other views have been registered.
-  [self.appBar addSubviewsToParent];
-  self.tableView.delegate = self.appBar.headerViewController;
-}
-
-- (void)viewWillAppear:(BOOL)animated {
-  [super viewWillAppear:animated];
-
-  // Set up the "Done" button in the upper right of the nav bar.
-  SettingsNavigationController* navigationController =
-      base::mac::ObjCCast<SettingsNavigationController>(
-          self.navigationController);
-  UIBarButtonItem* doneButton = [navigationController doneButton];
-  self.navigationItem.rightBarButtonItem = doneButton;
-}
-
-- (UIViewController*)childViewControllerForStatusBarHidden {
-  return self.appBar.headerViewController;
-}
-
-- (UIViewController*)childViewControllerForStatusBarStyle {
-  return self.appBar.headerViewController;
 }
 
 @end
