189baaa49f159860c44dc6172bdf6b1a4413592b
Lei Zhang <thestig@chromium.org>
2018-02-12 21:30:03 +0000

    Add jumbo support to pdf/.
    Just need to rename a variabled named "interface" which is also used in
    Windows headers. This is a reland of r531347 without r531314. r531314
    had issues and caused both CLs to be reverted.
    
    Change-Id: I9bb8e2ad86e9b9da8edf6b80448db2e398f72be1
    Reviewed-on: https://chromium-review.googlesource.com/912004
    Reviewed-by: dsinclair <dsinclair@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536196}
    Former-commit-id: 76085096ccd5cb4197e3d1ebe0d7b9a2b9e06cfc

--- pdf/out_of_process_instance.cc
+++ pdf/out_of_process_instance.cc
@@ -330,12 +330,12 @@ pp::Var ModalDialog(const pp::Instance* instance,
                     const std::string& type,
                     const std::string& message,
                     const std::string& default_answer) {
-  const PPB_Instance_Private* interface =
+  const PPB_Instance_Private* ppb_interface =
       reinterpret_cast<const PPB_Instance_Private*>(
           pp::Module::Get()->GetBrowserInterface(
               PPB_INSTANCE_PRIVATE_INTERFACE));
-  pp::VarPrivate window(pp::PASS_REF,
-                        interface->GetWindowObject(instance->pp_instance()));
+  pp::VarPrivate window(
+      pp::PASS_REF, ppb_interface->GetWindowObject(instance->pp_instance()));
   if (default_answer.empty())
     return window.Call(type, message);
   return window.Call(type, message, default_answer);
