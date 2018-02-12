2ca1024ec1b2bfb66146b169185ab1de535a0d1c
Adam Klein <adamk@chromium.org>
2018-02-12 22:46:23 +0000

    Rebaseline layout tests affected by v8's --harmony-function-tostring flag
    It is now on-by-default (and aimed for launch in M66).
    
    Bug: 753073
    Change-Id: Iba7cc4eeeb6aae142498de7d1afc70175cf3838e
    Reviewed-on: https://chromium-review.googlesource.com/914728
    Commit-Queue: Adam Klein <adamk@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536221}
    Former-commit-id: 7c340949cf8f166c74fc234502983671381fba3c

--- third_party/WebKit/LayoutTests/fast/js/script-tests/function-names.js
+++ third_party/WebKit/LayoutTests/fast/js/script-tests/function-names.js
@@ -4,14 +4,14 @@ description(
 
 document.documentElement.setAttribute("onclick", " return 2; ");
 
-shouldBe("new Function(' return 1; ').toString().replace(/[ \\n]+/g, ' ')", "'function anonymous() { return 1; }'");
+shouldBe("new Function(' return 1; ').toString().replace(/[ \\n]+/g, ' ')", "'function anonymous( ) { return 1; }'");
 shouldBe("document.documentElement.onclick.toString().replace(/[ \\n]+/g, ' ')", "'function onclick(event) { return 2; }'");
 
 shouldBe("''.constructor", "String");
 
 function checkConstructorName(name)
 {
-    shouldBe(name + ".toString()", "'function " + name + "() {\\n    [native code]\\n}'");
+    shouldBe(name + ".toString()", "'function " + name + "() { [native code] }'");
 }
 
 checkConstructorName("Boolean");
