ce70f04755b590839443d619938afbad84ad3c2d
Mark Cogan <marq@google.com>
2018-02-13 16:10:38 +0000

    [ios] Create some initial files for new tab grid.
    This CL creates stub files for the new tab grid's coordinator and
    view controller.
    
    Bug: 804496
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I68cc168e7c7d1d3329e4cbfb3eb03a47c9d62c9a
    Reviewed-on: https://chromium-review.googlesource.com/914609
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536366}
    Former-commit-id: cee58ae0922097a9f85623f27c4bec26a71ba9c6

---
+++ ios/chrome/browser/ui/tab_grid/tab_grid_coordinator.h
@@ -0,0 +1,15 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_TAB_GRID_TAB_GRID_COORDINATOR_H_
+#define IOS_CHROME_BROWSER_UI_TAB_GRID_TAB_GRID_COORDINATOR_H_
+
+#import <Foundation/Foundation.h>
+
+#import "ios/chrome/browser/ui/coordinators/chrome_coordinator.h"
+
+@interface TabGridCoordinator : ChromeCoordinator
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_TAB_GRID_TAB_GRID_COORDINATOR_H_
