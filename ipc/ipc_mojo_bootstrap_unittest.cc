d964c99f3ca67ff592188af298d02ae457f653af
Eve Martin-Jones <evem@chromium.org>
2018-02-13 22:57:25 +0000

    Add dependency on //mojo/public/interfaces/bindings:bindings to //ipc:mojom.
    Now that the internal mojo bindings is a component target
    (see cr/893655) ipc.mojom can safely import
    //mojo/public/interfaces/bindings/native_struct.mojom and depend
    on //mojo/public/interfaces/bindings:bindings.
    
    The duplicated SerializedHandle definition in //ipc/ipc.mojom
    can now be reverted.
    
    Change-Id: I78288b86bd08ac2af2a75cf83f5b033dde4e4aa4
    Reviewed-on: https://chromium-review.googlesource.com/913070
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Eve Martin-Jones <evem@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536531}
    Former-commit-id: 475e7e6382cdf4f2bbad6212ee960139af2bfeeb

--- ipc/ipc_mojo_bootstrap_unittest.cc
+++ ipc/ipc_mojo_bootstrap_unittest.cc
@@ -74,7 +74,7 @@ class PeerPidReceiver : public IPC::mojom::Channel {
   }
 
   void Receive(base::span<const uint8_t> data,
-               base::Optional<std::vector<IPC::mojom::SerializedHandlePtr>>
+               base::Optional<std::vector<mojo::native::SerializedHandlePtr>>
                    handles) override {
     ASSERT_NE(MessageExpectation::kNotExpected, message_expectation_);
 
