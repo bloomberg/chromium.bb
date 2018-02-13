2f376de620e5763542ad343897e434af56a2aa03
Linkun Chen <lkchen@chromium.org>
2018-02-13 20:05:15 +0000

    [Chromecast] Use absolute directory to load fonts
    local font.
    
    Bug: 69627595
    Test: Push clean built OTA to chromecast, force HTML element to use
    Change-Id: Id6269912717598acd5a9ed9226fd2c4590f45be4
    Reviewed-on: https://chromium-review.googlesource.com/915961
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Linkun Chen <lkchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536433}
    Former-commit-id: c8d8b349d332395adf5e73406f0bcccb1efb69d0

--- chromecast/browser/cast_browser_main_parts.cc
+++ chromecast/browser/cast_browser_main_parts.cc
@@ -395,7 +395,7 @@ void CastBrowserMainParts::ToolkitInitialized() {
   // the background (resources have not yet been granted to cast) since it
   // prevents the long delay the user would have seen on first rendering. Note
   // that future calls to FcInit() are safe no-ops per the FontConfig interface.
-  FcChar8 bundle_dir[] = "fonts/";
+  FcChar8 bundle_dir[] = "/chrome/fonts/";
 
   FcInit();
 
