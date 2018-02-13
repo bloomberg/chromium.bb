4a82bc166fefe3cd1d8753c7c9812fb54621941a
mallikarjun82 <vm.arjun@samsung.com>
2018-02-13 17:28:24 +0000

    Migrate PeerConnection Tracker IPC to Mojo-part 1
    This CL migrates IPC messages from renderer to browser with basic data
    types. This is first in upcoming CLs to migrate from IPC to Mojo.
    
    Bug: 792801
    Change-Id: I79e892b4961df597c17e9ffa612be25e0d8356f9
    Reviewed-on: https://chromium-review.googlesource.com/805675
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536397}
    Former-commit-id: 21dda42e8b1e9040215b9e020af9b90fc655cf89

--- content/common/media/peer_connection_tracker_messages.h
+++ content/common/media/peer_connection_tracker_messages.h
@@ -24,26 +24,9 @@ IPC_STRUCT_END()
 // Messages sent from PeerConnectionTracker to PeerConnectionTrackerHost.
 IPC_MESSAGE_CONTROL1(PeerConnectionTrackerHost_AddPeerConnection,
                      PeerConnectionInfo /* info */)
-IPC_MESSAGE_CONTROL1(PeerConnectionTrackerHost_RemovePeerConnection,
-                     int /* lid */)
-IPC_MESSAGE_CONTROL3(PeerConnectionTrackerHost_UpdatePeerConnection,
-                     int /* lid */,
-                     std::string /* type */,
-                     std::string /* value */)
 IPC_MESSAGE_CONTROL2(PeerConnectionTrackerHost_AddStats,
                      int /* lid */,
                      base::ListValue /* value */)
-IPC_MESSAGE_CONTROL5(PeerConnectionTrackerHost_GetUserMedia,
-                     std::string /*origin*/,
-                     bool /*audio*/,
-                     bool /*video*/,
-                     // The constraints strings are for dispaly only and should
-                     // not be parsed by the browser for security reasons.
-                     std::string /* audio_constraints */,
-                     std::string /* video_constraints */)
-IPC_MESSAGE_CONTROL2(PeerConnectionTrackerHost_WebRtcEventLogWrite,
-                     int /* lid */,
-                     std::string /* output */)
 
 // Messages sent to PeerConnectionTracker.
 IPC_MESSAGE_CONTROL0(PeerConnectionTracker_GetAllStats)
