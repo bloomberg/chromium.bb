d86aa96079edfaa14258b153ed2964d5be7b93cb
Allen Vicencio <allenvic@chromium.org>
2018-02-13 21:26:06 +0000

    Implement CreateDirectory in SmbFileSystem
    Bug: chromium:757625
    Change-Id: I781ee0ab1cfff9669797dbe796a2b3a84856560a
    Reviewed-on: https://chromium-review.googlesource.com/915241
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Allen Vicencio <allenvic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536485}
    Former-commit-id: 792c536c5e60313a5886c854ba2af060d28f95b9

--- chrome/browser/chromeos/smb_client/smb_file_system.h
+++ chrome/browser/chromeos/smb_client/smb_file_system.h
@@ -218,6 +218,10 @@ class SmbFileSystem : public file_system_provider::ProvidedFileSystemInterface {
       const storage::AsyncFileUtil::StatusCallback& callback,
       smbprovider::ErrorType error) const;
 
+  void HandleRequestCreateDirectoryCallback(
+      const storage::AsyncFileUtil::StatusCallback& callback,
+      smbprovider::ErrorType error) const;
+
   base::File::Error RunUnmountCallback(
       const ProviderId& provider_id,
       const std::string& file_system_id,
