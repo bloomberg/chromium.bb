92446c528c45dc2161771c622411a5079e19ea5d
Jingning Han <jingning@google.com>
2018-02-13 14:56:22 -0800

    [NORMATIVE] Reduce spatial search region from 4 to 3 cols
    Reduce the ref mv search over spatial neighbors from 4 to 3
    columns.
    
    BUG=aomedia:1382
    
    Change-Id: I44eb96e2ff4243d720a5f4f68be504995ebd69b6

--- av1/common/mvref_common.h
+++ av1/common/mvref_common.h
@@ -20,7 +20,11 @@ extern "C" {
 
 #define MVREF_NEIGHBOURS 9
 #define MVREF_ROWS 3
+#if CONFIG_OPT_REF_MV
+#define MVREF_COLS 3
+#else
 #define MVREF_COLS 4
+#endif
 
 // Set the upper limit of the motion vector component magnitude.
 // This would make a motion vector fit in 26 bits. Plus 3 bits for the
