d3aa6656aa67d5e8d52869a30cc7e89fe7445846
Kyle Horimoto <khorimoto@google.com>
2018-02-14 20:18:48 +0000

    [CrOS MultiDevice] Create multidevice::CryptAuthEnrollerFactoryImpl.
    This class generates CryptAuthEnroller objects via the IdentityManager
    service. This will be used to fetch synced CryptAuth data in the
    DeviceSync API.
    
    Bug: 752273
    Change-Id: If0fa5d29a3c04e4dc2a71a7a28c56b08957981e6
    Reviewed-on: https://chromium-review.googlesource.com/907788
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Tim Song <tengs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536795}
    Former-commit-id: 036d953497aab767f9ced06a75066f6630726aa8

---
+++ components/multidevice/service/cryptauth_enroller_factory_impl.cc
@@ -0,0 +1,34 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/multidevice/service/cryptauth_enroller_factory_impl.h"
+
+#include <memory>
+
+#include "components/cryptauth/cryptauth_enroller_impl.h"
+#include "components/multidevice/service/cryptauth_client_factory_impl.h"
+
+namespace multidevice {
+
+CryptAuthEnrollerFactoryImpl::CryptAuthEnrollerFactoryImpl(
+    identity::IdentityManager* identity_manager,
+    cryptauth::SecureMessageDelegate::Factory* secure_message_delegate_factory,
+    scoped_refptr<net::URLRequestContextGetter> url_request_context,
+    const cryptauth::DeviceClassifier& device_classifier)
+    : identity_manager_(identity_manager),
+      secure_message_delegate_factory_(secure_message_delegate_factory),
+      url_request_context_(url_request_context),
+      device_classifier_(device_classifier) {}
+
+CryptAuthEnrollerFactoryImpl::~CryptAuthEnrollerFactoryImpl() {}
+
+std::unique_ptr<cryptauth::CryptAuthEnroller>
+CryptAuthEnrollerFactoryImpl::CreateInstance() {
+  return std::make_unique<cryptauth::CryptAuthEnrollerImpl>(
+      std::make_unique<CryptAuthClientFactoryImpl>(
+          identity_manager_, url_request_context_, device_classifier_),
+      secure_message_delegate_factory_->CreateSecureMessageDelegate());
+}
+
+}  // namespace multidevice
