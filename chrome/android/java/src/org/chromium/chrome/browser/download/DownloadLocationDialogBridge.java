e2db239ccb9c005c9854faca8ebcfb18dae212d7
Joy Ming <jming@chromium.org>
2018-02-14 18:52:13 +0000

    [Downloads location] Switch to using ModalDialogManager.
    This CL switches the implementation of the DownloadLocationDialog from
    AlertDialog to ModalDialogManager.
    
    Bug: 792775
    Change-Id: Ida60fb9cd55f9b2e07a918bfb90e79468552de6d
    Reviewed-on: https://chromium-review.googlesource.com/915105
    Commit-Queue: Joy Ming <jming@chromium.org>
    Reviewed-by: Becky Zhou <huayinz@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536763}
    Former-commit-id: f3581827364d7f31f217b05c0c9696826992aee7

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadLocationDialogBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadLocationDialogBridge.java
@@ -4,9 +4,11 @@
 
 package org.chromium.chrome.browser.download;
 
-import android.app.Activity;
-
 import org.chromium.base.annotations.CalledByNative;
+import org.chromium.chrome.browser.ChromeActivity;
+import org.chromium.chrome.browser.modaldialog.ModalDialogManager;
+import org.chromium.chrome.browser.modaldialog.ModalDialogView;
+import org.chromium.chrome.browser.preferences.PrefServiceBridge;
 import org.chromium.content_public.browser.WebContents;
 
 import java.io.File;
@@ -16,13 +18,13 @@ import javax.annotation.Nullable;
 /**
  * Helper class to handle communication between download location dialog and native.
  */
-public class DownloadLocationDialogBridge
-        implements DownloadLocationDialog.DownloadLocationDialogListener {
+public class DownloadLocationDialogBridge implements ModalDialogView.Controller {
     // TODO(jming): Remove this when switching to a dropdown instead of going to preferences.
     private static DownloadLocationDialogBridge sInstance;
 
     private long mNativeDownloadLocationDialogBridge;
     private DownloadLocationDialog mLocationDialog;
+    private ModalDialogManager mModalDialogManager;
 
     @Nullable
     public static DownloadLocationDialogBridge getInstance() {
@@ -52,36 +54,87 @@ public class DownloadLocationDialogBridge
     @CalledByNative
     private void destroy() {
         mNativeDownloadLocationDialogBridge = 0;
-        if (mLocationDialog != null) mLocationDialog.dismiss();
+        if (mModalDialogManager != null) mModalDialogManager.dismissDialog(mLocationDialog);
         sInstance = null;
     }
 
     @CalledByNative
     public void showDialog(WebContents webContents, String suggestedPath) {
-        Activity windowAndroidActivity = webContents.getTopLevelNativeWindow().getActivity().get();
-        if (windowAndroidActivity == null) return;
+        // TODO(jming): Remove WebContents requirement.
+        ChromeActivity activity =
+                (ChromeActivity) webContents.getTopLevelNativeWindow().getActivity().get();
+        // If the activity has gone away, just clean up the native pointer.
+        if (activity == null) {
+            onCancel();
+            return;
+        }
+
+        mModalDialogManager = activity.getModalDialogManager();
 
         if (mLocationDialog != null) return;
-        mLocationDialog =
-                new DownloadLocationDialog(windowAndroidActivity, this, new File(suggestedPath));
-        // TODO(jming): Use ModalDialogManager.
-        mLocationDialog.show();
+        mLocationDialog = DownloadLocationDialog.create(this, activity, new File(suggestedPath));
+
+        mModalDialogManager.showDialog(mLocationDialog, ModalDialogManager.APP_MODAL);
     }
 
     @Override
-    public void onComplete(File returnedPath) {
-        if (mNativeDownloadLocationDialogBridge == 0) return;
+    public void onClick(@ModalDialogView.ButtonType int buttonType) {
+        switch (buttonType) {
+            case ModalDialogView.BUTTON_POSITIVE:
+                handleResponses(mLocationDialog.getFileName(), mLocationDialog.getFileLocation(),
+                        mLocationDialog.getDontShowAgain());
+                mModalDialogManager.dismissDialog(mLocationDialog);
+                break;
+            case ModalDialogView.BUTTON_NEGATIVE:
+            // Intentional fall-through.
+            default:
+                cancel();
+                mModalDialogManager.dismissDialog(mLocationDialog);
+        }
 
-        nativeOnComplete(mNativeDownloadLocationDialogBridge, returnedPath.getAbsolutePath());
         mLocationDialog = null;
     }
 
     @Override
-    public void onCanceled() {
-        if (mNativeDownloadLocationDialogBridge == 0) return;
-
-        nativeOnCanceled(mNativeDownloadLocationDialogBridge);
+    public void onCancel() {
+        cancel();
         mLocationDialog = null;
+    }
+
+    /**
+     * Pass along information from location dialog to native.
+     *
+     * @param fileName      Name the user gave the file.
+     * @param fileLocation  Location the user wants the file saved to.
+     * @param dontShowAgain Whether the user wants the "Save download to..." dialog shown again.
+     */
+    private void handleResponses(String fileName, File fileLocation, boolean dontShowAgain) {
+        // If there's no file location, treat as a cancellation.
+        if (fileLocation == null) {
+            cancel();
+            return;
+        }
+
+        // Update native with new path.
+        if (mNativeDownloadLocationDialogBridge != 0) {
+            File filePath = new File(fileLocation, fileName);
+            nativeOnComplete(mNativeDownloadLocationDialogBridge, filePath.getAbsolutePath());
+        }
+
+        // Update preference to show prompt based on whether checkbox is checked.
+        if (dontShowAgain) {
+            PrefServiceBridge.getInstance().setPromptForDownloadAndroid(
+                    DownloadPromptStatus.DONT_SHOW);
+        } else {
+            PrefServiceBridge.getInstance().setPromptForDownloadAndroid(
+                    DownloadPromptStatus.SHOW_PREFERENCE);
+        }
+    }
+
+    private void cancel() {
+        if (mNativeDownloadLocationDialogBridge != 0) {
+            nativeOnCanceled(mNativeDownloadLocationDialogBridge);
+        }
     }
 
     public native void nativeOnComplete(
