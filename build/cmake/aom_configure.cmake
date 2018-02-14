a20f7bf17c7311eb65dc823066437f36918ca363
Luc Trudeau <luc@trud.ca>
2018-02-14 15:15:47 -0500

    [cmake] Add support for "aarch64" CMAKE_SYSTEM_PROCESSOR
    When the CMAKE_SYSTEM_PROCESSOR is aarch64, AOM_TARGET_CPU
    is set to arm64.
    
    BUG=aomedia:1385
    
    Change-Id: I35676a3ad8a90dc814fe8acb2a9b56fbf60b3700

--- build/cmake/aom_configure.cmake
+++ build/cmake/aom_configure.cmake
@@ -68,6 +68,8 @@ if (NOT AOM_TARGET_CPU)
   elseif ("${CMAKE_SYSTEM_PROCESSOR}" MATCHES "^arm" OR
           "${CMAKE_SYSTEM_PROCESSOR}" MATCHES "^mips")
     set(AOM_TARGET_CPU "${CMAKE_SYSTEM_PROCESSOR}")
+  elseif ("${CMAKE_SYSTEM_PROCESSOR}" MATCHES "aarch64")
+    set(AOM_TARGET_CPU "arm64")
   endif ()
 endif ()
 
@@ -136,8 +138,9 @@ elseif ("${AOM_TARGET_CPU}" MATCHES "arm")
     set(AS_EXECUTABLE as)
     set(AOM_AS_FLAGS -arch ${AOM_TARGET_CPU} -isysroot ${CMAKE_OSX_SYSROOT})
   elseif ("${AOM_TARGET_SYSTEM}" STREQUAL "Linux")
-    # arm linux assembler settings controlled by
-    # build/cmake/toolchains/arm*-linux*.cmake
+    if (NOT AS_EXECUTABLE)
+      set(AS_EXECUTABLE as)
+    endif ()
   endif ()
   if (NOT AS_EXECUTABLE)
     message(FATAL_ERROR
