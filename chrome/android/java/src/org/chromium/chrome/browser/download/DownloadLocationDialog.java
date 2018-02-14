a5bcc3d18cb65a1b23f5562e33d3f4e6ffc99bd2
Joy Ming <jming@chromium.org>
2018-02-14 18:52:13 +0000

    [Downloads location] Switch to using ModalDialogManager.
    This CL switches the implementation of the DownloadLocationDialog from
    AlertDialog to ModalDialogManager.
    
    Bug: 792775
    Change-Id: Ida60fb9cd55f9b2e07a918bfb90e79468552de6d
    Reviewed-on: https://chromium-review.googlesource.com/915105
    Commit-Queue: Joy Ming <jming@chromium.org>
    Reviewed-by: Becky Zhou <huayinz@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536763}
    Former-commit-id: f3581827364d7f31f217b05c0c9696826992aee7

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadLocationDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadLocationDialog.java
@@ -5,15 +5,14 @@
 package org.chromium.chrome.browser.download;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
 import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.CheckBox;
 
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.modaldialog.ModalDialogView;
 import org.chromium.chrome.browser.preferences.PrefServiceBridge;
 import org.chromium.chrome.browser.preferences.PreferencesLauncher;
 import org.chromium.chrome.browser.preferences.download.DownloadDirectoryList;
@@ -22,13 +21,12 @@ import org.chromium.chrome.browser.widget.AlertDialogEditText;
 
 import java.io.File;
 
+import javax.annotation.Nullable;
+
 /**
  * Dialog that is displayed to ask user where they want to download the file.
  */
-public class DownloadLocationDialog
-        extends AlertDialog implements DialogInterface.OnClickListener, View.OnFocusChangeListener,
-                                       DialogInterface.OnCancelListener {
-    private DownloadLocationDialogListener mListener;
+public class DownloadLocationDialog extends ModalDialogView implements View.OnFocusChangeListener {
     private DownloadDirectoryList mDownloadDirectoryUtil;
 
     private AlertDialogEditText mFileName;
@@ -36,63 +34,48 @@ public class DownloadLocationDialog
     private CheckBox mDontShowAgain;
 
     /**
-     * Interface for a listener to the events related to the DownloadLocationDialog.
+     * Create a {@link DownloadLocationDialog} with the given properties.
+     *
+     * @param controller    Controller that listens to the events from the dialog.
+     * @param context       Context from which the dialog emerged.
+     * @param suggestedPath The path that was automatically generated, used as a starting point.
+     * @return              A {@link DownloadLocationDialog} with the given properties.
      */
-    public interface DownloadLocationDialogListener {
-        /**
-         * Notify listeners that the dialog has been completed.
-         * @param returnedPath from the dialog.
-         */
-        void onComplete(File returnedPath);
+    public static DownloadLocationDialog create(
+            Controller controller, Context context, File suggestedPath) {
+        Params params = new Params();
+        params.title = context.getString(R.string.download_location_dialog_title);
+        params.positiveButtonTextId = R.string.duplicate_download_infobar_download_button;
+        params.negativeButtonTextId = R.string.cancel;
+        params.customView =
+                LayoutInflater.from(context).inflate(R.layout.download_location_dialog, null);
 
-        /**
-         * Notify listeners that the dialog has been canceled.
-         */
-        void onCanceled();
+        return new DownloadLocationDialog(controller, context, suggestedPath, params);
     }
 
-    /**
-     * Create a DownloadLocationDialog that is displayed before a download begins.
-     *
-     * @param context of the dialog.
-     * @param listener to the updates from the dialog.
-     * @param suggestedPath of the download location.
-     */
-    DownloadLocationDialog(
-            Context context, DownloadLocationDialogListener listener, File suggestedPath) {
-        super(context, 0);
+    private DownloadLocationDialog(
+            Controller controller, Context context, File suggestedPath, Params params) {
+        super(controller, params);
 
-        mListener = listener;
         mDownloadDirectoryUtil = new DownloadDirectoryList(context);
 
-        setButton(BUTTON_POSITIVE,
-                context.getText(R.string.duplicate_download_infobar_download_button), this);
-        setButton(BUTTON_NEGATIVE, context.getText(R.string.cancel), this);
-        setIcon(0);
-        setTitle(context.getString(R.string.download_location_dialog_title));
-
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View view = inflater.inflate(R.layout.download_location_dialog, null);
-
-        mFileName = (AlertDialogEditText) view.findViewById(R.id.file_name);
+        mFileName = (AlertDialogEditText) params.customView.findViewById(R.id.file_name);
         mFileName.setText(suggestedPath.getName());
 
-        mFileLocation = (AlertDialogEditText) view.findViewById(R.id.file_location);
+        mFileLocation = (AlertDialogEditText) params.customView.findViewById(R.id.file_location);
         // NOTE: This makes the EditText correctly styled but not editable.
         mFileLocation.setInputType(InputType.TYPE_NULL);
         mFileLocation.setOnFocusChangeListener(this);
         setFileLocation(suggestedPath.getParentFile());
 
         // Automatically check "don't show again" the first time the user is seeing the dialog.
-        mDontShowAgain = (CheckBox) view.findViewById(R.id.show_again_checkbox);
+        mDontShowAgain = (CheckBox) params.customView.findViewById(R.id.show_again_checkbox);
         boolean isInitial = PrefServiceBridge.getInstance().getPromptForDownloadAndroid()
                 == DownloadPromptStatus.SHOW_INITIAL;
         mDontShowAgain.setChecked(isInitial);
-
-        setView(view);
-        setCanceledOnTouchOutside(false);
-        setOnCancelListener(this);
     }
+
+    // Helper methods available to DownloadLocationDialogBridge.
 
     /**
      * Update the string in the file location text view.
@@ -104,45 +87,29 @@ public class DownloadLocationDialog
         mFileLocation.setText(mDownloadDirectoryUtil.getNameForFile(location));
     }
 
-    // DialogInterface.OnClickListener implementation.
-
-    @Override
-    public void onClick(DialogInterface dialogInterface, int which) {
-        if (which == BUTTON_POSITIVE) {
-            // Get new file path information.
-            String fileName = mFileName.getText().toString();
-            File fileLocation =
-                    mDownloadDirectoryUtil.getFileForName(mFileLocation.getText().toString());
-            if (fileLocation != null) {
-                mListener.onComplete(new File(fileLocation, fileName));
-            } else {
-                // If file location returned null, treat as cancellation.
-                mListener.onCanceled();
-                dismiss();
-                return;
-            }
-
-            // Update preference to show prompt based on whether checkbox is checked.
-            if (mDontShowAgain.isChecked()) {
-                PrefServiceBridge.getInstance().setPromptForDownloadAndroid(
-                        DownloadPromptStatus.DONT_SHOW);
-            } else {
-                PrefServiceBridge.getInstance().setPromptForDownloadAndroid(
-                        DownloadPromptStatus.SHOW_PREFERENCE);
-            }
-        } else {
-            mListener.onCanceled();
-        }
-
-        dismiss();
+    /**
+     * @return  The text that the user inputted as the name of the file.
+     */
+    @Nullable
+    String getFileName() {
+        if (mFileName == null) return null;
+        return mFileName.getText().toString();
     }
 
-    // Dialog.OnCancelListener implementation.
+    /**
+     * @return  The file path based on what the user selected as the location of the file.
+     */
+    @Nullable
+    File getFileLocation() {
+        if (mFileLocation == null) return null;
+        return mDownloadDirectoryUtil.getFileForName(mFileLocation.getText().toString());
+    }
 
-    @Override
-    public void onCancel(DialogInterface dialogInterface) {
-        mListener.onCanceled();
-        dismiss();
+    /**
+     * @return  Whether the "don't show again" checkbox is checked.
+     */
+    boolean getDontShowAgain() {
+        return mDontShowAgain != null && mDontShowAgain.isChecked();
     }
 
     // View.OnFocusChange implementation.
