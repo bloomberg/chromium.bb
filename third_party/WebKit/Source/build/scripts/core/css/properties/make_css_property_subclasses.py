76e5740ecd17b21f92a11aa8f3ca3457c80a9f25
Takuto Ikuta <tikuta@google.com>
2018-02-14 06:37:57 +0000

    Cache template in template_expander
    Reading a few template file many times make make_core_generated_css_longhand_property_classes step slow.
    This patch improves generating step time of make_core_generated_css_longhand_property_classes from 32.477s to 2.190s by caching template.
    
    Bug: 804695
    Change-Id: I39f7a117b29a502292f00af51485757c73af3166
    Reviewed-on: https://chromium-review.googlesource.com/918322
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@google.com>
    Cr-Commit-Position: refs/heads/master@{#536665}
    Former-commit-id: d53e00287f64a25663d44f057f314d95bdefa67b

--- third_party/WebKit/Source/build/scripts/core/css/properties/make_css_property_subclasses.py
+++ third_party/WebKit/Source/build/scripts/core/css/properties/make_css_property_subclasses.py
@@ -25,6 +25,8 @@ class CSSPropertiesWriter(CSSPropertyBaseWriter):
             ('CSSPropertiesWriter requires 3 input json5 files, ' +
              'got {}.'.format(len(json5_file_paths)))
 
+        self.template_cache = {}
+
         # Map of property method name -> (return_type, parameters)
         self._property_methods = {}
         property_methods = json5_generator.Json5File.load_from_files(
@@ -69,7 +71,8 @@ class CSSPropertiesWriter(CSSPropertyBaseWriter):
 
     def generate_property_h_builder(self, property_classname, property_):
         @template_expander.use_jinja(
-            'core/css/properties/templates/CSSPropertySubclass.h.tmpl')
+            'core/css/properties/templates/CSSPropertySubclass.h.tmpl',
+            template_cache=self.template_cache)
         def generate_property_h():
             return {
                 'input_files': self._input_files,
@@ -81,7 +84,8 @@ class CSSPropertiesWriter(CSSPropertyBaseWriter):
 
     def generate_property_cpp_builder(self, property_classname, property_):
         @template_expander.use_jinja(
-            'core/css/properties/templates/CSSPropertySubclass.cpp.tmpl')
+            'core/css/properties/templates/CSSPropertySubclass.cpp.tmpl',
+            template_cache=self.template_cache)
         def generate_property_cpp():
             return {
                 'input_files': self._input_files,
