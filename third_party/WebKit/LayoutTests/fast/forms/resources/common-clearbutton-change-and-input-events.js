1c83d8dda3e8ad5228185ff5d986ca6a1144fe1d
Takayoshi Kochi <kochi@chromium.org>
2018-02-15 10:45:59 +0000

    Reland "Remove dead code after multiple shadow removal [5/5]"
    This is a reland of ebdde71504b60d07d3bd7536591c241b1731e853 [formerly 0b95262849c3c386d4216f5fc08816970911c256].
    
    The original CL was once reverted due to potential cause for
    Mac bot's flakiness: crbug.com/805383
    
    Original change's description:
    > Remove dead code after multiple shadow removal [5/5]
    >
    > Remove window.internals.{youngest/oldest/younger}ShadowRoot
    > Adjust layout tests.
    >
    > Bug: 795221
    > Change-Id: I75024668007771c87ddd9598fbd1728859d7f9e9
    > Reviewed-on: https://chromium-review.googlesource.com/831815
    > Commit-Queue: Takayoshi Kochi <kochi@chromium.org>
    > Reviewed-by: Hayato Ito <hayato@chromium.org>
    > Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#531490}
    
    Bug: 795221
    Change-Id: I854ed889f811f3768ac95653b74ddd3f30ce6b5a
    Reviewed-on: https://chromium-review.googlesource.com/919923
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Takayoshi Kochi <kochi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536984}
    Former-commit-id: c78013e0e5f3c501f016cd0ca3a9b9d257b1faf1

--- third_party/WebKit/LayoutTests/fast/forms/resources/common-clearbutton-change-and-input-events.js
+++ third_party/WebKit/LayoutTests/fast/forms/resources/common-clearbutton-change-and-input-events.js
@@ -26,7 +26,7 @@ function testClearButtonChangeAndInputEvents(inputType, initialValue)
 
     debug('Click the clear button');
     // Move the cursor on to the clear button.
-    var clearButton = getElementByPseudoId(internals.oldestShadowRoot(testInput), "-webkit-clear-button");
+    var clearButton = getElementByPseudoId(internals.shadowRoot(testInput), "-webkit-clear-button");
     eventSender.mouseMoveTo(clearButton.offsetLeft + clearButton.offsetWidth / 2, clearButton.offsetTop + clearButton.offsetHeight / 2);
     eventSender.mouseDown();
     eventSender.mouseUp();
