f612f3f7793f237d13dc1567bab8e1aad404ff97
Robert Liao <robliao@chromium.org>
2018-02-15 01:19:38 +0000

    Revert "Add Child Move and Resize Support to the BubbleAnchorHelper"
    This reverts commit 713dd180357f5c821bb48acb13011dbfb330c563 [formerly b36af4e16af33aa286fd266869f4ed2b7d2e132a].
    
    Reason for revert: There seem to be some rounding issues during continuous repositioning of the parent window.
    
    Original change's description:
    > Add Child Move and Resize Support to the BubbleAnchorHelper
    > 
    > The ExtensionPopup resizes its view dynamically after it can be
    > anchored and shown.
    > 
    > This change adds child resize support to BubbleAnchorHelper to
    > accommodate that scenario. Move is brought along for free.
    > 
    > BUG=728174
    > 
    > Change-Id: I7ca12b92da339263ca5558254d2bbb0595a57b4c
    > Reviewed-on: https://chromium-review.googlesource.com/915010
    > Commit-Queue: Robert Liao <robliao@chromium.org>
    > Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#536753}
    
    TBR=ellyjones@chromium.org,robliao@chromium.org
    
    Change-Id: Ic8c3c6766f10a779b467605f808c883869def07e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 728174
    Reviewed-on: https://chromium-review.googlesource.com/920604
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536915}
    Former-commit-id: 151790add2db27316a3fffed72028eb4729047d8

--- chrome/browser/ui/cocoa/bubble_anchor_helper_views.mm
+++ chrome/browser/ui/cocoa/bubble_anchor_helper_views.mm
@@ -6,7 +6,6 @@
 
 #import <Cocoa/Cocoa.h>
 
-#include "base/bind.h"
 #import "base/mac/scoped_nsobject.h"
 #import "chrome/browser/ui/cocoa/browser_window_controller.h"
 #import "chrome/browser/ui/cocoa/location_bar/location_bar_decoration.h"
@@ -32,18 +31,8 @@ class BubbleAnchorHelper final : public views::WidgetObserver {
                      AnchorType type);
 
  private:
-  // Observe |name| on the bubble parent window with a block to call
-  // RepositionFromAnchor().
-  void ObserveParentWindow(NSString* name);
-
-  // Observe |name| on the bubble window with a block to call
-  // RecalculateAnchor();
-  void ObserveBubbleWindow(NSString* name);
-
-  // Use |callback| to observe |name| on |window|.
-  void ObserveWindow(NSWindow* window,
-                     NSString* name,
-                     base::RepeatingClosure callback);
+  // Observe |name| on the bubble parent window with a block to call ReAnchor().
+  void Observe(NSString* name);
 
   // Whether offset from the left of the parent window is fixed.
   bool IsMinXFixed() const {
@@ -52,10 +41,7 @@ class BubbleAnchorHelper final : public views::WidgetObserver {
 
   // Re-positions |bubble_| so that the offset to the parent window at
   // construction time is preserved.
-  void RepositionFromAnchor();
-
-  // Recalculates the offsets for |bubble_|.
-  void RecalculateAnchor();
+  void ReAnchor();
 
   // WidgetObserver:
   void OnWidgetDestroying(views::Widget* widget) override;
@@ -115,49 +101,40 @@ BubbleAnchorHelper::BubbleAnchorHelper(views::BubbleDialogDelegateView* bubble,
   if (type == IGNORE_PARENT)
     return;
 
-  RecalculateAnchor();
+  NSRect parent_frame = [[bubble->parent_window() window] frame];
+  NSRect bubble_frame = [bubble->GetWidget()->GetNativeWindow() frame];
 
-  ObserveParentWindow(NSWindowDidEnterFullScreenNotification);
-  ObserveParentWindow(NSWindowDidExitFullScreenNotification);
-  ObserveParentWindow(NSWindowDidResizeNotification);
+  // Note: when anchored on the right, this doesn't support changes to the
+  // bubble size, just the parent size.
+  horizontal_offset_ =
+      (IsMinXFixed() ? NSMinX(parent_frame) : NSMaxX(parent_frame)) -
+      NSMinX(bubble_frame);
+  vertical_offset_ = NSMaxY(parent_frame) - NSMinY(bubble_frame);
+
+  Observe(NSWindowDidEnterFullScreenNotification);
+  Observe(NSWindowDidExitFullScreenNotification);
+  Observe(NSWindowDidResizeNotification);
 
   // Also monitor move. Note that for user-initiated window moves this is not
   // necessary: the bubble's child window status keeps the position pinned to
   // the parent during the move (which is handy since AppKit doesn't send out
   // notifications until the move completes). Programmatic -[NSWindow
   // setFrame:..] calls, however, do not update child window positions for us.
-  ObserveParentWindow(NSWindowDidMoveNotification);
-
-  ObserveBubbleWindow(NSWindowDidResizeNotification);
-  ObserveBubbleWindow(NSWindowDidMoveNotification);
+  Observe(NSWindowDidMoveNotification);
 }
 
-void BubbleAnchorHelper::ObserveParentWindow(NSString* name) {
-  ObserveWindow([bubble_->parent_window() window], name,
-                base::BindRepeating(&BubbleAnchorHelper::RepositionFromAnchor,
-                                    base::Unretained(this)));
-}
-
-void BubbleAnchorHelper::ObserveBubbleWindow(NSString* name) {
-  ObserveWindow(bubble_->GetWidget()->GetNativeWindow(), name,
-                base::BindRepeating(&BubbleAnchorHelper::RecalculateAnchor,
-                                    base::Unretained(this)));
-}
-
-void BubbleAnchorHelper::ObserveWindow(NSWindow* window,
-                                       NSString* name,
-                                       base::RepeatingClosure callback) {
+void BubbleAnchorHelper::Observe(NSString* name) {
   NSNotificationCenter* center = [NSNotificationCenter defaultCenter];
   id token = [center addObserverForName:name
-                                 object:window
+                                 object:[bubble_->parent_window() window]
                                   queue:nil
                              usingBlock:^(NSNotification* notification) {
-                               callback.Run();
+                               ReAnchor();
                              }];
   [observer_tokens_ addObject:token];
 }
 
-void BubbleAnchorHelper::RepositionFromAnchor() {
+void BubbleAnchorHelper::ReAnchor() {
   NSRect bubble_frame = [bubble_->GetWidget()->GetNativeWindow() frame];
   NSRect parent_frame = [[bubble_->parent_window() window] frame];
   if (IsMinXFixed())
@@ -168,15 +145,6 @@ void BubbleAnchorHelper::RepositionFromAnchor() {
   [bubble_->GetWidget()->GetNativeWindow() setFrame:bubble_frame
                                             display:YES
                                             animate:NO];
-}
-
-void BubbleAnchorHelper::RecalculateAnchor() {
-  NSRect parent_frame = [[bubble_->parent_window() window] frame];
-  NSRect bubble_frame = [bubble_->GetWidget()->GetNativeWindow() frame];
-  horizontal_offset_ =
-      (IsMinXFixed() ? NSMinX(parent_frame) : NSMaxX(parent_frame)) -
-      NSMinX(bubble_frame);
-  vertical_offset_ = NSMaxY(parent_frame) - NSMinY(bubble_frame);
 }
 
 void BubbleAnchorHelper::OnWidgetDestroying(views::Widget* widget) {
