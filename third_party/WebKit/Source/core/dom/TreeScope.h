e020e9ae693a39df88c71a88ef13350dffb86e62
Takayoshi Kochi <kochi@chromium.org>
2018-02-15 05:59:03 +0000

    Reland "Remove dead code after multiple shadow removal [4/5]"
    This is a reland of c0fcdb12e7b33ef66f12d4d8d72c9526a9a74a9c [formerly 391d909bf8ff508c895164adae6aa5ab7cf4c454].
    
    This was once reverted due to potential cause for Mac bot's
    flakiness: crbug.com/805383
    
    Original change's description:
    > Remove dead code after multiple shadow removal [4/5]
    >
    > Remove ShadowRoot::{Younger,Older}ShadowRoot
    >
    > Bug: 795221
    > Change-Id: Ib4754de2184d08db9bb5fc35379f27dea5ef2997
    > Reviewed-on: https://chromium-review.googlesource.com/831810
    > Commit-Queue: Takayoshi Kochi <kochi@chromium.org>
    > Reviewed-by: Hayato Ito <hayato@chromium.org>
    > Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#531478}
    
    TBR=hayato@chromium.org, futhark@chromium.org
    
    Bug: 795221
    Change-Id: I61a8c2d7b39398fc676515f67a7c0d0a9536da22
    Reviewed-on: https://chromium-review.googlesource.com/920941
    Reviewed-by: Takayoshi Kochi <kochi@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Takayoshi Kochi <kochi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#536951}
    Former-commit-id: a67f7bc4dbf87ba2270c98516871a25fcc303558

--- third_party/WebKit/Source/core/dom/TreeScope.h
+++ third_party/WebKit/Source/core/dom/TreeScope.h
@@ -54,7 +54,6 @@ class CORE_EXPORT TreeScope : public GarbageCollectedMixin {
  public:
   TreeScope* ParentTreeScope() const { return parent_tree_scope_; }
 
-  TreeScope* OlderShadowRootOrParentTreeScope() const;
   bool IsInclusiveOlderSiblingShadowRootOrAncestorTreeScopeOf(
       const TreeScope&) const;
 
