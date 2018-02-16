9ad7a4751f283204a9891f004584ddebea1d62a4
Bernhard Bauer <bauerb@chromium.org>
2018-02-16 10:30:28 +0000

    Only allow multiple action buttons in Custom Tabs for trusted intents.
    Also, ignore any actions buttons beyond the first two (instead of
    silently moving them to the secondary toolbar) if they are explicitly
    meant to go to the primary toolbar.
    
    Bug: 810854
    Change-Id: I97d4730c884209848096e79e5f2bc414e051a8b1
    Reviewed-on: https://chromium-review.googlesource.com/919087
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537277}
    Former-commit-id: debce06e67728c92cbebb2eb7b0072f4c040b6aa

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomButtonParams.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomButtonParams.java
@@ -154,9 +154,11 @@ public class CustomButtonParams {
     /**
      * Parses a list of {@link CustomButtonParams} from the intent sent by clients.
      * @param intent The intent sent by the client.
+     * @param isTrustedIntent whether the intent is trusted.
      * @return A list of parsed {@link CustomButtonParams}. Return an empty list if input is invalid
      */
-    public static List<CustomButtonParams> fromIntent(Context context, Intent intent) {
+    public static List<CustomButtonParams> fromIntent(
+            Context context, Intent intent, boolean isTrustedIntent) {
         List<CustomButtonParams> paramsList = new ArrayList<>(1);
         if (intent == null) return paramsList;
 
