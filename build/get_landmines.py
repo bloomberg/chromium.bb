ba6360d8faef522ec0587f552cc3cd5d9b229787
Michael Achenbach <machenbach@chromium.org>
2018-02-16 22:40:01 +0000

    [build] Simplify landmines and strip off gyp-dependent options
    This prepares removing the option to make landmines target-platform
    dependent, as this depends on gyp environment variables.
    
    Instead we'll be able to only depend on host_os.
    
    This will cause clobber on all Android builds, as Android-specific
    landmines will no longer be supported.
    
    In follow-ups, we'll remove use of landmine_utils.platform() in other
    client projects and then remove the definition here.
    
    Bug: 756691
    Change-Id: I08c1f4ecd05f5f4331bf1ab9eedda22419af2942
    Reviewed-on: https://chromium-review.googlesource.com/924114
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Michael Achenbach <machenbach@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537436}
    Former-commit-id: d76f98f52683e21606b35d15b156e28a72cb85f4

--- build/get_landmines.py
+++ build/get_landmines.py
@@ -13,10 +13,7 @@ import sys
 import landmine_utils
 
 
-distributor = landmine_utils.distributor
-gyp_defines = landmine_utils.gyp_defines
-gyp_msvs_version = landmine_utils.gyp_msvs_version
-platform = landmine_utils.platform
+host_os = landmine_utils.host_os
 
 
 def print_landmines():
@@ -39,29 +36,21 @@ def print_landmines():
   # across the change that added the landmine, adding to the cost. There are
   # usually less troublesome alternatives.
 
-  if distributor() == 'goma' and platform() == 'win32':
-    print 'Need to clobber winja goma due to backend cwd cache fix.'
-  if platform() == 'android':
-    print 'Clobber: to handle new way of suppressing findbugs failures.'
-    print 'Clobber to fix gyp not rename package name (crbug.com/457038)'
-    print 'Clobber to recalculate reversed dependency (crbug.com/639042)'
-    print 'Clobber to recalculate reversed dependency (crbug.com/639042)'
-  if platform() == 'win':
+  if host_os() == 'win':
     print 'Compile on cc_unittests fails due to symbols removed in r185063.'
-  if platform() == 'linux':
+  if host_os() == 'linux':
     print 'Builders switching from make to ninja will clobber on this.'
-  if platform() == 'mac':
+  if host_os() == 'mac':
     print 'Switching from bundle to unbundled dylib (issue 14743002).'
-  if platform() in ('win', 'mac'):
+  if host_os() in ('win', 'mac'):
     print ('Improper dependency for create_nmf.py broke in r240802, '
            'fixed in r240860.')
-  if platform() == 'win':
+  if host_os() == 'win':
     print 'Switch to VS2015 Update 3, 14393 SDK'
   print 'Need to clobber everything due to an IDL change in r154579 (blink)'
   print 'Need to clobber everything due to gen file moves in r175513 (Blink)'
-  if (platform() != 'ios'):
-    print 'Clobber to get rid of obselete test plugin after r248358'
-    print 'Clobber to rebuild GN files for V8'
+  print 'Clobber to get rid of obselete test plugin after r248358'
+  print 'Clobber to rebuild GN files for V8'
   print 'Clobber to get rid of stale generated mojom.h files'
   print 'Need to clobber everything due to build_nexe change in nacl r13424'
   print '[chromium-dev] PSA: clobber build needed for IDR_INSPECTOR_* compil...'
@@ -71,19 +60,15 @@ def print_landmines():
   print 'Another clobber for missing NaCl gyp deps (crbug.com/427427).'
   print 'Clobber to fix GN not picking up increased ID range (crbug.com/444902)'
   print 'Remove NaCl toolchains from the output dir (crbug.com/456902)'
-  if platform() == 'ios':
-    print 'Clobber iOS to workaround Xcode deps bug (crbug.com/485435)'
-  if platform() == 'win':
+  if host_os() == 'win':
     print 'Clobber to delete stale generated files (crbug.com/510086)'
-  if platform() == 'android' and gyp_defines().get('target_arch') == 'arm64':
-    print 'Clobber to support new location/infra for chrome_sync_shell_apk'
-  if platform() == 'mac':
+  if host_os() == 'mac':
     print 'Clobber to get rid of evil libsqlite3.dylib (crbug.com/526208)'
-  if platform() == 'mac':
+  if host_os() == 'mac':
     print 'Clobber to remove libsystem.dylib. See crbug.com/620075'
-  if platform() == 'mac':
+  if host_os() == 'mac':
     print 'Clobber to get past mojo gen build error (crbug.com/679607)'
-  if platform() == 'win':
+  if host_os() == 'win':
     print 'Clobber Windows to fix strange PCH-not-rebuilt errors.'
   print 'CLobber all to fix GN breakage (crbug.com/736215)'
 
