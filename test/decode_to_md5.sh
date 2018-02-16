2db556bafa9b65900395f1dd53cf26c7eaebd08a
Debargha Mukherjee <debargha@google.com>
2018-02-16 11:48:57 -0800

    Turn on adopted convolve precision rounding config
    By default turn:
    CONFIG_LOWPRECISION_BLEND=2
    CONFIG_HIGHPRECISION_INTBUF=1
    
    This is the adopted configuration from the codec WG.
    
    Change-Id: Ic8a324a4bf6316753bea07a17160ab93afacca3b

--- test/decode_to_md5.sh
+++ test/decode_to_md5.sh
@@ -57,7 +57,7 @@ decode_to_md5() {
 
 decode_to_md5_av1() {
   # expected MD5 sum for the last frame.
-  local expected_md5="85ccfd374240849e6849f917cf742c9e"
+  local expected_md5="155facd9ade570f14502bbfcaaeeca06"
   local file="${AV1_IVF_FILE}"
 
   # TODO(urvang): Check in the encoded file (like libvpx does) to avoid
