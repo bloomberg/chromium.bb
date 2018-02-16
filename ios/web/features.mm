4225e42556f47825dc7b278ef7dce407341f2d35
Mohammad Refaat <mrefaat@chromium.org>
2018-02-16 02:29:01 +0000

    Enable WKHTTPCookieStore usage by default for iOS 11+
    Bug: 759229, 779106
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ia79cc0ba778e1b24de5cfddab2068f165b77d64e
    Reviewed-on: https://chromium-review.googlesource.com/922924
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537109}
    Former-commit-id: 107d07bc68a516d5dccc620c98a7bcb5408b7dd2

--- ios/web/features.mm
+++ ios/web/features.mm
@@ -20,7 +20,7 @@ const base::Feature kNewFileDownload{"NewFileDownload",
                                      base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kWKHTTPSystemCookieStore{"WKHTTPSystemCookieStore",
-                                             base::FEATURE_DISABLED_BY_DEFAULT};
+                                             base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kPreloadWebViewWhenNavigatingFromNativeContentOnIOS11_3{
     "PreloadWebViewWhenNavigatingFromNativeContentOnIOS11_3",
