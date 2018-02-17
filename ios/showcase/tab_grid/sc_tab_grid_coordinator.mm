83a5fa3ba44d834e5e94162ba3896b7b98c977d3
edchin <edchin@chromium.org>
2018-02-17 00:45:23 +0000

    [ios] Add tab grid to view controller and showcase
    Bug: 804496, 804497
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I05b6627c4eb9ab6ae33e337631b7f1e70466730c
    Reviewed-on: https://chromium-review.googlesource.com/924650
    Commit-Queue: edchin <edchin@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537485}
    Former-commit-id: 7fb547a7af5b4553eda59b218a5bcf2acfdb9eaa

--- ios/showcase/tab_grid/sc_tab_grid_coordinator.mm
+++ ios/showcase/tab_grid/sc_tab_grid_coordinator.mm
@@ -4,13 +4,14 @@
 
 #import "ios/showcase/tab_grid/sc_tab_grid_coordinator.h"
 
+#import "ios/chrome/browser/ui/tab_grid/grid_item.h"
 #import "ios/chrome/browser/ui/tab_grid/tab_grid_view_controller.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
-@interface SCTabGridCoordinator ()
+@interface SCTabGridCoordinator ()<UINavigationControllerDelegate>
 @property(nonatomic, strong) TabGridViewController* viewController;
 @end
 
@@ -20,9 +21,29 @@
 
 - (void)start {
   self.viewController = [[TabGridViewController alloc] init];
-  self.viewController.title = @"Tab Grid";
-  [self.baseViewController setHidesBarsOnSwipe:YES];
+  self.viewController.title = @"Full TabGrid UI";
+  self.baseViewController.delegate = self;
+  self.baseViewController.hidesBarsOnSwipe = YES;
   [self.baseViewController pushViewController:self.viewController animated:YES];
+}
+
+#pragma mark - UINavigationControllerDelegate
+
+// This delegate method is used as a way to have a completion handler after
+// pushing onto a navigation controller.
+- (void)navigationController:(UINavigationController*)navigationController
+       didShowViewController:(UIViewController*)viewController
+                    animated:(BOOL)animated {
+  if (viewController != self.viewController)
+    return;
+
+  NSMutableArray* items = [[NSMutableArray alloc] init];
+  for (int i = 0; i < 10; i++) {
+    GridItem* item = [[GridItem alloc] init];
+    item.title = @"The New York Times - Breaking News";
+    [items addObject:item];
+  }
+  [self.viewController.regularTabsConsumer populateItems:items selectedIndex:0];
 }
 
 @end
