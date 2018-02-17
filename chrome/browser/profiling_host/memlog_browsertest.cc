45098e2fc1bdaf0b2d7980d015031f431bb01294
Dave Schuyler <dschuyler@chromium.org>
2018-02-17 02:25:27 +0000

    Disable flaking endToEnd test on Windows.
    TBR=erikchen@chromium.org
    
    Bug: 813117
    Change-Id: I08b627b284d88cc6b50b96cfb75f9399fb8a67f7
    Reviewed-on: https://chromium-review.googlesource.com/924758
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Commit-Queue: Dave Schuyler <dschuyler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537508}
    Former-commit-id: f344088f3f614cc77de502e3afcdb469523ac5e5

--- chrome/browser/profiling_host/memlog_browsertest.cc
+++ chrome/browser/profiling_host/memlog_browsertest.cc
@@ -77,7 +77,13 @@ class MemlogBrowserTest : public InProcessBrowserTest,
 
 // Ensure invocations via TracingController can generate a valid JSON file with
 // expected data.
-IN_PROC_BROWSER_TEST_P(MemlogBrowserTest, EndToEnd) {
+// Crashes on Win only.  http://crbug.com/813117
+#if defined(OS_WIN)
+#define MAYBE_EndToEnd DISABLED_EndToEnd
+#else
+#define MAYBE_EndToEnd EndToEnd
+#endif
+IN_PROC_BROWSER_TEST_P(MemlogBrowserTest, MAYBE_EndToEnd) {
   LOG(INFO) << "Memlog mode: " << static_cast<int>(GetParam().mode);
   LOG(INFO) << "Memlog stack mode: " << static_cast<int>(GetParam().stack_mode);
   LOG(INFO) << "Started via command line flag: "
