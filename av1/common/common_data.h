f0e12c271141d0e4b5786d0666e00df78a45ceb4
Yaowu Xu <yaowu@google.com>
2018-02-17 21:30:44 -0800

    Remove an unused array
    Change-Id: I1b74c2efcef553f7d9a5a4a701ff29fa905a61f5

--- av1/common/common_data.h
+++ av1/common/common_data.h
@@ -901,7 +901,6 @@ static const int intra_mode_context[INTRA_MODES] = {
 static const int quant_dist_weight[4][2] = {
   { 2, 3 }, { 2, 5 }, { 2, 7 }, { 1, MAX_FRAME_DISTANCE }
 };
-static const double quant_dist_category[4] = { 1.5, 2.5, 3.5, 255 };
 static const int quant_dist_lookup_table[2][4][2] = {
   { { 9, 7 }, { 11, 5 }, { 12, 4 }, { 13, 3 } },
   { { 7, 9 }, { 5, 11 }, { 4, 12 }, { 3, 13 } },
