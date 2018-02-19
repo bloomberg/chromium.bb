9c0289c9d9603311a18680c986083606507ebf8c
Bernhard Bauer <bauerb@chromium.org>
2018-02-19 16:56:10 +0000

    Update SHA256 fingerprint for OriginVerifierTest in official builds.
    Bug: 813594
    Change-Id: I33c8d72cb8a856f363799049743c63584593a362
    Reviewed-on: https://chromium-review.googlesource.com/925272
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537667}
    Former-commit-id: f8112735d19ffa1fe45143815ed43ba9bf0c1199

--- chrome/android/javatests/src/org/chromium/chrome/browser/browserservices/OriginVerifierTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/browserservices/OriginVerifierTest.java
@@ -15,6 +15,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.chrome.browser.ChromeVersionInfo;
 import org.chromium.chrome.browser.browserservices.OriginVerifier.OriginVerificationListener;
 
 import java.util.concurrent.Callable;
@@ -30,9 +31,15 @@ public class OriginVerifierTest {
     private static final String STRING_ARRAY = "AA:BB:CC:10:20:30:01:02";
 
     private static final String PACKAGE_NAME = "org.chromium.chrome";
-    private static final String SHA_256_FINGERPRINT =
+    private static final String SHA_256_FINGERPRINT_PUBLIC =
             "32:A2:FC:74:D7:31:10:58:59:E5:A8:5D:F1:6D:95:F1:02:D8:5B"
             + ":22:09:9B:80:64:C5:D8:91:5C:61:DA:D1:E0";
+    private static final String SHA_256_FINGERPRINT_OFFICIAL =
+            "19:75:B2:F1:71:77:BC:89:A5:DF:F3:1F:9E:64:A6:CA:E2:81:A5"
+            + ":3D:C1:D1:D5:9B:1D:14:7F:E1:C8:2A:FA:00";
+    private static final String SHA_256_FINGERPRINT = ChromeVersionInfo.isOfficialBuild()
+            ? SHA_256_FINGERPRINT_OFFICIAL
+            : SHA_256_FINGERPRINT_PUBLIC;
     private static final Uri TEST_HTTPS_ORIGIN_1 = Uri.parse("https://www.example.com");
     private static final Uri TEST_HTTPS_ORIGIN_2 = Uri.parse("https://www.android.com");
     private static final Uri TEST_HTTP_ORIGIN = Uri.parse("http://www.android.com");
@@ -66,9 +73,9 @@ public class OriginVerifierTest {
     @Test
     @SmallTest
     public void testSHA256CertificateChecks() {
-        Assert.assertEquals(OriginVerifier.byteArrayToHexString(BYTE_ARRAY), STRING_ARRAY);
-        Assert.assertEquals(OriginVerifier.getCertificateSHA256FingerprintForPackage(PACKAGE_NAME),
-                SHA_256_FINGERPRINT);
+        Assert.assertEquals(STRING_ARRAY, OriginVerifier.byteArrayToHexString(BYTE_ARRAY));
+        Assert.assertEquals(SHA_256_FINGERPRINT,
+                OriginVerifier.getCertificateSHA256FingerprintForPackage(PACKAGE_NAME));
     }
 
     @Test
