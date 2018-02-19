wjdz99/aom

Add cdf_update_mode experiment
Allow the CDF update to operate in different modes, e.g. update vs no
update.

The update mode is transmitted in the uncompressed frame header of
every keyframe and intra-only frame.

This patch only adds bitstream signaling and API support. The
implementation of the update modes will be in later patches.

Change-Id: Ic9fcd60e8a75f9c01f414253823d78cf9b3113dd

av1/av1_cx_iface.c
@@ -113,6 +113,9 @@ struct av1_extracfg {
   int film_grain_test_vector;
 #endif
   unsigned int motion_vector_unit_test;
+#if CONFIG_CDF_UPDATE_RATE
+  unsigned int cdf_update_rate;
+#endif  // CONFIG_CDF_UPDATE_RATE
 };
 
 static struct av1_extracfg default_extra_cfg = {
@@ -195,6 +198,9 @@ static struct av1_extracfg default_extra_cfg = {
   0,
 #endif
   0,  // motion_vector_unit_test
+#if CONFIG_CDF_UPDATE_RATE
+  1,    // CDF update rate
+#endif  // CONFIG_CDF_UPDATE_RATE
 };
 
 struct aom_codec_alg_priv {
@@ -306,6 +312,9 @@ static aom_codec_err_t validate_config(aom_codec_alg_priv_t *ctx,
   RANGE_CHECK(cfg, rc_superres_qthresh, 1, 63);
   RANGE_CHECK(cfg, rc_superres_kf_qthresh, 1, 63);
 #endif  // CONFIG_HORZONLY_FRAME_SUPERRES
+#if CONFIG_CDF_UPDATE_RATE
+  RANGE_CHECK_HI(extra_cfg, cdf_update_rate, 3);
+#endif  // CONFIG_CDF_UPDATE_RATE
 
   // AV1 does not support a lower bound on the keyframe interval in
   // automatic keyframe placement mode.
@@ -690,6 +699,10 @@ static aom_codec_err_t set_encoder_config(
 
   oxcf->tuning = extra_cfg->tuning;
   oxcf->content = extra_cfg->content;
+
+#if CONFIG_CDF_UPDATE_RATE
+  oxcf->cdf_update_rate = (uint8_t)extra_cfg->cdf_update_rate;
+#endif  // CONFIG_CDF_UPDATE_RATE
 
 #if CONFIG_EXT_PARTITION
   oxcf->superblock_size = extra_cfg->superblock_size;
@@ -1682,6 +1695,15 @@ static aom_codec_err_t ctrl_set_tune_content(aom_codec_alg_priv_t *ctx,
   return update_extra_cfg(ctx, &extra_cfg);
 }
 
+#if CONFIG_CDF_UPDATE_RATE
+static aom_codec_err_t ctrl_set_cdf_update_rate(aom_codec_alg_priv_t *ctx,
+                                                va_list args) {
+  struct av1_extracfg extra_cfg = ctx->extra_cfg;
+  extra_cfg.cdf_update_rate = CAST(AV1E_SET_CDF_UPDATE_RATE, args);
+  return update_extra_cfg(ctx, &extra_cfg);
+}
+#endif  // CONFIG_CDF_UPDATE_RATE
+
 #if CONFIG_CICP
 static aom_codec_err_t ctrl_set_color_primaries(aom_codec_alg_priv_t *ctx,
                                                 va_list args) {
@@ -1831,6 +1853,9 @@ static aom_codec_ctrl_fn_map_t encoder_ctrl_maps[] = {
 #endif
   { AV1E_SET_FRAME_PERIODIC_BOOST, ctrl_set_frame_periodic_boost },
   { AV1E_SET_TUNE_CONTENT, ctrl_set_tune_content },
+#if CONFIG_CDF_UPDATE_RATE
+  { AV1E_SET_CDF_UPDATE_RATE, ctrl_set_cdf_update_rate },
+#endif  // CONFIG_CDF_UPDATE_RATE
 #if CONFIG_CICP
   { AV1E_SET_COLOR_PRIMARIES, ctrl_set_color_primaries },
   { AV1E_SET_TRANSFER_CHARACTERISTICS, ctrl_set_transfer_characteristics },

build/cmake/aom_config_defaults.cmake
@@ -92,6 +92,7 @@ set(CONFIG_AOM_QM 1 CACHE NUMBER "AV1 experiment flag.")
 set(CONFIG_AOM_QM_EXT 0 CACHE NUMBER "AV1 experiment flag.")
 set(CONFIG_BGSPRITE 0 CACHE NUMBER "AV1 experiment flag.")
 set(CONFIG_CDF_STORAGE_REDUCTION 0 CACHE NUMBER "AV1 experiment flag.")
+set(CONFIG_CDF_UPDATE_RATE 0 CACHE NUMBER "AV1 experiment flag.")
 set(CONFIG_CFL 1 CACHE NUMBER "AV1 experiment flag.")
 set(CONFIG_CICP 0 CACHE NUMBER "AV1 experiment flag.")
 set(CONFIG_COLORSPACE_HEADERS 0 CACHE NUMBER "AV1 experiment flag.")

bloomberg/chromium.bb

av1/av1_cx_iface.c
@@ -113,6 +113,9 @@ struct av1_extracfg {
   int film_grain_test_vector;
 #endif
   unsigned int motion_vector_unit_test;
+#if CONFIG_CDF_UPDATE_MODE
+  unsigned int cdf_update_mode;
+#endif  // CONFIG_CDF_UPDATE_MODE
 };
 
 static struct av1_extracfg default_extra_cfg = {
@@ -195,6 +198,9 @@ static struct av1_extracfg default_extra_cfg = {
   0,
 #endif
   0,  // motion_vector_unit_test
+#if CONFIG_CDF_UPDATE_MODE
+  1,    // CDF update mode
+#endif  // CONFIG_CDF_UPDATE_MODE
 };
 
 struct aom_codec_alg_priv {
@@ -306,6 +312,9 @@ static aom_codec_err_t validate_config(aom_codec_alg_priv_t *ctx,
   RANGE_CHECK(cfg, rc_superres_qthresh, 1, 63);
   RANGE_CHECK(cfg, rc_superres_kf_qthresh, 1, 63);
 #endif  // CONFIG_HORZONLY_FRAME_SUPERRES
+#if CONFIG_CDF_UPDATE_MODE
+  RANGE_CHECK_HI(extra_cfg, cdf_update_mode, 3);
+#endif  // CONFIG_CDF_UPDATE_MODE
 
   // AV1 does not support a lower bound on the keyframe interval in
   // automatic keyframe placement mode.
@@ -690,6 +699,10 @@ static aom_codec_err_t set_encoder_config(
 
   oxcf->tuning = extra_cfg->tuning;
   oxcf->content = extra_cfg->content;
+
+#if CONFIG_CDF_UPDATE_MODE
+  oxcf->cdf_update_mode = (uint8_t)extra_cfg->cdf_update_mode;
+#endif  // CONFIG_CDF_UPDATE_MODE
 
 #if CONFIG_EXT_PARTITION
   oxcf->superblock_size = extra_cfg->superblock_size;
@@ -1682,6 +1695,15 @@ static aom_codec_err_t ctrl_set_tune_content(aom_codec_alg_priv_t *ctx,
   return update_extra_cfg(ctx, &extra_cfg);
 }
 
+#if CONFIG_CDF_UPDATE_MODE
+static aom_codec_err_t ctrl_set_cdf_update_mode(aom_codec_alg_priv_t *ctx,
+                                                va_list args) {
+  struct av1_extracfg extra_cfg = ctx->extra_cfg;
+  extra_cfg.cdf_update_mode = CAST(AV1E_SET_CDF_UPDATE_MODE, args);
+  return update_extra_cfg(ctx, &extra_cfg);
+}
+#endif  // CONFIG_CDF_UPDATE_MODE
+
 #if CONFIG_CICP
 static aom_codec_err_t ctrl_set_color_primaries(aom_codec_alg_priv_t *ctx,
                                                 va_list args) {
@@ -1831,6 +1853,9 @@ static aom_codec_ctrl_fn_map_t encoder_ctrl_maps[] = {
 #endif
   { AV1E_SET_FRAME_PERIODIC_BOOST, ctrl_set_frame_periodic_boost },
   { AV1E_SET_TUNE_CONTENT, ctrl_set_tune_content },
+#if CONFIG_CDF_UPDATE_MODE
+  { AV1E_SET_CDF_UPDATE_MODE, ctrl_set_cdf_update_mode },
+#endif  // CONFIG_CDF_UPDATE_MODE
 #if CONFIG_CICP
   { AV1E_SET_COLOR_PRIMARIES, ctrl_set_color_primaries },
   { AV1E_SET_TRANSFER_CHARACTERISTICS, ctrl_set_transfer_characteristics },

build/cmake/aom_config_defaults.cmake
@@ -92,6 +92,7 @@ set(CONFIG_AOM_QM 1 CACHE NUMBER "AV1 experiment flag.")
 set(CONFIG_AOM_QM_EXT 0 CACHE NUMBER "AV1 experiment flag.")
 set(CONFIG_BGSPRITE 0 CACHE NUMBER "AV1 experiment flag.")
 set(CONFIG_CDF_STORAGE_REDUCTION 0 CACHE NUMBER "AV1 experiment flag.")
+set(CONFIG_CDF_UPDATE_MODE 0 CACHE NUMBER "AV1 experiment flag.")
 set(CONFIG_CFL 1 CACHE NUMBER "AV1 experiment flag.")
 set(CONFIG_CICP 0 CACHE NUMBER "AV1 experiment flag.")
 set(CONFIG_COLORSPACE_HEADERS 0 CACHE NUMBER "AV1 experiment flag.")