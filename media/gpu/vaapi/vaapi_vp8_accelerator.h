384c7041648a63bb69ad1e0311113297ce176442
Miguel Casas <mcasas@chromium.org>
2018-02-20 17:00:54 +0000

    vaapi deco cleanup: extract H264, VP8 and VP9 accelerators
    vaapi_video_decode_accelerator.* have three inner classes for H264, VP8
    and VP9 decoding. To reduce the size of the file(s), this CL extracts
    those into their own files. No new code is introduced in those new files,
    but VideoDecodeAccelerator needs to make 3 methods public in order for
    those not-anymore-internal classes to work (idea for next CL would be to
    make those into an interface that VaapiVDA implements).
    
    Test: Compile and run in simplechrome on soraka. Ran h264, vp8, vp9
    videos of crosvideo.appspot.com, verified GpuVideoDecoder engaged.
    
    Bug: 717265
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I054b39a330f3876031fd30d6e3e36878322d9036
    Reviewed-on: https://chromium-review.googlesource.com/922811
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537800}
    Former-commit-id: 9dd0a7c17a09ac1df6b5c4246e7a8d1c8aef4760

---
+++ media/gpu/vaapi/vaapi_vp8_accelerator.h
@@ -0,0 +1,49 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_GPU_VAAPI_VAAPI_VP8_ACCELERATOR_H_
+#define MEDIA_GPU_VAAPI_VAAPI_VP8_ACCELERATOR_H_
+
+#include "base/sequence_checker.h"
+#include "media/filters/vp8_parser.h"
+#include "media/gpu/media_gpu_export.h"
+#include "media/gpu/vp8_decoder.h"
+
+namespace media {
+
+class VP8Picture;
+class VaapiDecodeSurface;
+class VaapiVideoDecodeAccelerator;
+class VaapiWrapper;
+
+class MEDIA_GPU_EXPORT VaapiVP8Accelerator : public VP8Decoder::VP8Accelerator {
+ public:
+  VaapiVP8Accelerator(VaapiVideoDecodeAccelerator* vaapi_dec,
+                      VaapiWrapper* vaapi_wrapper);
+  ~VaapiVP8Accelerator() override;
+
+  // VP8Decoder::VP8Accelerator implementation.
+  scoped_refptr<VP8Picture> CreateVP8Picture() override;
+  bool SubmitDecode(const scoped_refptr<VP8Picture>& pic,
+                    const Vp8FrameHeader* frame_hdr,
+                    const scoped_refptr<VP8Picture>& last_frame,
+                    const scoped_refptr<VP8Picture>& golden_frame,
+                    const scoped_refptr<VP8Picture>& alt_frame) override;
+  bool OutputPicture(const scoped_refptr<VP8Picture>& pic) override;
+
+ private:
+  scoped_refptr<VaapiDecodeSurface> VP8PictureToVaapiDecodeSurface(
+      const scoped_refptr<VP8Picture>& pic);
+
+  VaapiWrapper* vaapi_wrapper_;
+  VaapiVideoDecodeAccelerator* vaapi_dec_;
+
+  SEQUENCE_CHECKER(sequence_checker_);
+
+  DISALLOW_COPY_AND_ASSIGN(VaapiVP8Accelerator);
+};
+
+}  // namespace media
+
+#endif  // MEDIA_GPU_VAAPI_VAAPI_VP8_ACCELERATOR_H_
