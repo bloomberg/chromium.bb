7ee966594503b17b5f109c74109e7bb86b6e070f
Bailey Berro <baileyberro@chromium.org>
2018-02-20 23:14:16 +0000

    Implement MoveEntry on SmbProviderClient
    This change implements MoveEntry on SmbProviderClient
    and FakeSmbProviderClient.
    
    BUG=chromium:757625
    TEST=compiles
    
    Change-Id: Icf38fa220dbb993a86f12e8c5464b93b18f06c3d
    Reviewed-on: https://chromium-review.googlesource.com/923081
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#537924}
    Former-commit-id: 879b7350de24ebcb29e757379a9ee67d9c7e8887

--- chromeos/dbus/fake_smb_provider_client.cc
+++ chromeos/dbus/fake_smb_provider_client.cc
@@ -115,4 +115,12 @@ void FakeSmbProviderClient::CreateDirectory(
       FROM_HERE, base::BindOnce(std::move(callback), smbprovider::ERROR_OK));
 }
 
+void FakeSmbProviderClient::MoveEntry(int32_t mount_id,
+                                      const base::FilePath& source_path,
+                                      const base::FilePath& target_path,
+                                      StatusCallback callback) {
+  base::ThreadTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE, base::BindOnce(std::move(callback), smbprovider::ERROR_OK));
+}
+
 }  // namespace chromeos
--- chromeos/dbus/smb_provider_client.cc
+++ chromeos/dbus/smb_provider_client.cc
@@ -186,6 +186,17 @@ class SmbProviderClientImpl : public SmbProviderClient {
     CallDefaultMethod(smbprovider::kCreateDirectoryMethod, options, &callback);
   }
 
+  void MoveEntry(int32_t mount_id,
+                 const base::FilePath& source_path,
+                 const base::FilePath& target_path,
+                 StatusCallback callback) override {
+    smbprovider::MoveEntryOptionsProto options;
+    options.set_mount_id(mount_id);
+    options.set_source_path(source_path.value());
+    options.set_target_path(target_path.value());
+    CallDefaultMethod(smbprovider::kMoveEntryMethod, options, &callback);
+  }
+
  protected:
   // DBusClient override.
   void Init(dbus::Bus* bus) override {
