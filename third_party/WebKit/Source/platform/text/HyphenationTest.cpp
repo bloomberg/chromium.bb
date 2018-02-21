e8f36889b5c94fd8cba633917896d64f291adb88
Koji Ishii <kojii@chromium.org>
2018-02-21 13:57:06 +0000

    Add tests for the hyphenation engine itself
    We had tests to test the logic in Blink, but did not test the
    hyphenation engines itself.
    
    This patch adds tests to check some real words from samples
    in crbug.com/813414:
      en-us:
        "hyphenation"
      de-1996:
        "konsonantien"
        "Bächlein"
    
    Bug: 813414
    Change-Id: I646cb74a31282771709a3272716b4d9b7ee82589
    Reviewed-on: https://chromium-review.googlesource.com/927421
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538089}
    Former-commit-id: 7921afe5aaa58509b9ed9f38e35e7e19efc29181

--- third_party/WebKit/Source/platform/text/HyphenationTest.cpp
+++ third_party/WebKit/Source/platform/text/HyphenationTest.cpp
@@ -30,40 +30,56 @@ class NoHyphenation : public Hyphenation {
   }
 };
 
-TEST(HyphenationTest, Get) {
+class HyphenationTest : public ::testing::Test {
+ protected:
+  void TearDown() override { FontGlobalContext::ClearForTesting(); }
+
+#if defined(USE_MINIKIN_HYPHENATION) || defined(OS_MACOSX)
+  // Get a |Hyphenation| instnace for the specified locale for testing.
+  scoped_refptr<Hyphenation> GetHyphenation(const AtomicString& locale) {
+#if defined(USE_MINIKIN_HYPHENATION)
+    // Because the mojo service to open hyphenation dictionaries is not
+    // accessible from the unit test, open the dictionary file directly for
+    // testing.
+    std::string filename =
+        std::string("hyph-") + locale.Ascii().data() + ".hyb";
+#if defined(OS_ANDROID)
+    base::FilePath path("/system/usr/hyphen-data");
+#else
+#error "This configuration is not supported."
+#endif
+    path = path.AppendASCII(filename);
+    base::File file(path, base::File::FLAG_OPEN | base::File::FLAG_READ);
+    if (file.IsValid())
+      return HyphenationMinikin::FromFileForTesting(std::move(file));
+#else
+    if (const LayoutLocale* layout_locale = LayoutLocale::Get(locale))
+      return layout_locale->GetHyphenation();
+#endif
+    return nullptr;
+  }
+#endif
+};
+
+TEST_F(HyphenationTest, Get) {
   scoped_refptr<Hyphenation> hyphenation = base::AdoptRef(new NoHyphenation);
   LayoutLocale::SetHyphenationForTesting("en-US", hyphenation);
   EXPECT_EQ(hyphenation.get(), LayoutLocale::Get("en-US")->GetHyphenation());
 
   LayoutLocale::SetHyphenationForTesting("en-UK", nullptr);
   EXPECT_EQ(nullptr, LayoutLocale::Get("en-UK")->GetHyphenation());
-
-  FontGlobalContext::ClearForTesting();
 }
 
 #if defined(USE_MINIKIN_HYPHENATION) || defined(OS_MACOSX)
-TEST(HyphenationTest, HyphenLocations) {
-#if defined(USE_MINIKIN_HYPHENATION)
-  // Because the mojo service to open hyphenation dictionaries is not accessible
-  // from the unit test, open the dictionary file directly for testing.
-  base::FilePath path("/system/usr/hyphen-data/hyph-en-us.hyb");
-  base::File file(path, base::File::FLAG_OPEN | base::File::FLAG_READ);
-  if (!file.IsValid()) {
-    // Ignore this test on platforms without hyphenation dictionaries.
-    return;
-  }
-  scoped_refptr<Hyphenation> hyphenation =
-      HyphenationMinikin::FromFileForTesting(std::move(file));
-#else
-  const LayoutLocale* locale = LayoutLocale::Get("en-us");
-  ASSERT_TRUE(locale);
-  Hyphenation* hyphenation = locale->GetHyphenation();
-#endif
+TEST_F(HyphenationTest, HyphenLocations) {
+  scoped_refptr<Hyphenation> hyphenation = GetHyphenation("en-us");
   ASSERT_TRUE(hyphenation) << "Cannot find the hyphenation engine";
 
   // Get all hyphenation points by |HyphenLocations|.
   const String word("hyphenation");
   Vector<size_t, 8> locations = hyphenation->HyphenLocations(word);
+  EXPECT_GT(locations.size(), 0u);
+
   for (unsigned i = 1; i < locations.size(); i++) {
     ASSERT_GT(locations[i - 1], locations[i])
         << "hyphenLocations must return locations in the descending order";
@@ -89,6 +105,40 @@ TEST(HyphenationTest, HyphenLocations) {
   }
   locations.Reverse();
   EXPECT_THAT(actual, ElementsAreArray(locations));
+}
+
+TEST_F(HyphenationTest, English) {
+  scoped_refptr<Hyphenation> hyphenation = GetHyphenation("en-us");
+#if defined(OS_ANDROID)
+  // Hyphenation is available only for Android M MR1 or later.
+  if (!hyphenation)
+    return;
+#endif
+  ASSERT_TRUE(hyphenation) << "Cannot find the hyphenation for en-us";
+
+  Vector<size_t, 8> locations = hyphenation->HyphenLocations("hyphenation");
+  EXPECT_THAT(locations, testing::AnyOf(ElementsAreArray({6, 2}),
+                                        ElementsAreArray({7, 6, 2})));
+}
+
+TEST_F(HyphenationTest, German) {
+  scoped_refptr<Hyphenation> hyphenation = GetHyphenation("de-1996");
+#if defined(OS_ANDROID)
+  // Hyphenation is available only for Android M MR1 or later.
+  if (!hyphenation)
+    return;
+#endif
+  ASSERT_TRUE(hyphenation) << "Cannot find the hyphenation for de-1996";
+
+  Vector<size_t, 8> locations = hyphenation->HyphenLocations("konsonantien");
+  EXPECT_THAT(locations, ElementsAreArray({8, 5, 3}));
+
+  // Test words with non-ASCII (> U+0080) characters.
+  locations = hyphenation->HyphenLocations(
+      "B"
+      "\xE4"  // LATIN SMALL LETTER A WITH DIAERESIS
+      "chlein");
+  EXPECT_THAT(locations, ElementsAreArray({4}));
 }
 #endif
 
