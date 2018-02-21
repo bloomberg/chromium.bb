b14e6e982727f33ba5f49a60456138970b3e1f93
tzik <tzik@chromium.org>
2018-02-21 11:34:21 +0000

    Add a rewriter to remove unneeded base::Passed on base::OnceCallback
    This CL adds a clang based rewriter as a sub-rewriter of base_bind_rewriter
    that removes unneeded base::Passed usage on base::BindOnce parameter.
    Example:
      // Before:
      base::BindOnce(&Foo, base::Passed(&ptr));
      base::BindOnce(&Foo, base::Passed(std::move(ptr)));
      base::BindOnce(&Foo, base::Passed(std::make_unique<int>()));
    
      // After:
      base::BindOnce(&Foo, std::move(ptr));
      base::BindOnce(&Foo, std::move(ptr));
      base::BindOnce(&Foo, std::make_unique<int>());
    
    Bug: 812523
    Change-Id: I81c391531f02d608cffbbbe2f12a1cff6a1201de
    Reviewed-on: https://chromium-review.googlesource.com/923620
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538064}
    Former-commit-id: 6d2135b081286959d9ea3963eafe58fa92094581

--- tools/clang/base_bind_rewriters/BaseBindRewriters.cpp
+++ tools/clang/base_bind_rewriters/BaseBindRewriters.cpp
@@ -26,6 +26,96 @@ using Replacements = std::vector<clang::tooling::Replacement>;
 
 namespace {
 
+class Rewriter {
+ public:
+  virtual ~Rewriter() {}
+};
+
+// Removes unneeded base::Passed() on a parameter of base::BindOnce().
+// Example:
+//   // Before
+//   base::BindOnce(&Foo, base::Passed(&bar));
+//   base::BindOnce(&Foo, base::Passed(std::move(baz)));
+//   base::BindOnce(&Foo, base::Passed(qux));
+//
+//   // After
+//   base::BindOnce(&Foo, std::move(bar));
+//   base::BindOnce(&Foo, std::move(baz));
+//   base::BindOnce(&Foo, std::move(*qux));
+class PassedToMoveRewriter : public MatchFinder::MatchCallback,
+                             public Rewriter {
+ public:
+  explicit PassedToMoveRewriter(Replacements* replacements)
+      : replacements_(replacements) {}
+  ~PassedToMoveRewriter() override {}
+
+  StatementMatcher GetMatcher() {
+    auto is_passed = namedDecl(hasName("::base::Passed"));
+    auto is_bind_once_call = callee(namedDecl(hasName("::base::BindOnce")));
+
+    // Matches base::Passed() call on a base::BindOnce() argument.
+    return callExpr(is_bind_once_call,
+                    hasAnyArgument(ignoringImplicit(
+                        callExpr(callee(is_passed)).bind("target"))));
+  }
+
+  void run(const MatchFinder::MatchResult& result) override {
+    auto* target = result.Nodes.getNodeAs<clang::CallExpr>("target");
+    auto* callee = target->getCallee()->IgnoreImpCasts();
+
+    auto* callee_decl = clang::dyn_cast<clang::DeclRefExpr>(callee)->getDecl();
+    auto* passed_decl = clang::dyn_cast<clang::FunctionDecl>(callee_decl);
+    auto* param_type = passed_decl->getParamDecl(0)->getType().getTypePtr();
+
+    if (param_type->isRValueReferenceType()) {
+      // base::Passed(xxx) -> xxx.
+      // The parameter type is already an rvalue reference.
+      // Example:
+      //   std::unique_ptr<int> foo();
+      //   std::unique_ptr<int> bar;
+      //   base::Passed(foo());
+      //   base::Passed(std::move(bar));
+      // In these cases, we can just remove base::Passed.
+      auto left = clang::CharSourceRange::getTokenRange(
+          result.SourceManager->getSpellingLoc(target->getLocStart()),
+          result.SourceManager->getSpellingLoc(target->getArg(0)->getExprLoc())
+              .getLocWithOffset(-1));
+      auto r_paren = clang::CharSourceRange::getTokenRange(
+          result.SourceManager->getSpellingLoc(target->getRParenLoc()),
+          result.SourceManager->getSpellingLoc(target->getRParenLoc()));
+      replacements_->emplace_back(*result.SourceManager, left, " ");
+      replacements_->emplace_back(*result.SourceManager, r_paren, " ");
+      return;
+    }
+
+    if (!param_type->isPointerType())
+      return;
+
+    auto* passed_arg = target->getArg(0)->IgnoreImpCasts();
+    if (auto* unary = clang::dyn_cast<clang::UnaryOperator>(passed_arg)) {
+      if (unary->getOpcode() == clang::UO_AddrOf) {
+        // base::Passed(&xxx) -> std::move(xxx).
+        auto left = clang::CharSourceRange::getTokenRange(
+            result.SourceManager->getSpellingLoc(target->getLocStart()),
+            result.SourceManager->getSpellingLoc(
+                target->getArg(0)->getExprLoc()));
+        replacements_->emplace_back(*result.SourceManager, left, "std::move(");
+        return;
+      }
+    }
+
+    // base::Passed(xxx) -> std::move(*xxx)
+    auto left = clang::CharSourceRange::getTokenRange(
+        result.SourceManager->getSpellingLoc(target->getLocStart()),
+        result.SourceManager->getSpellingLoc(target->getArg(0)->getExprLoc())
+            .getLocWithOffset(-1));
+    replacements_->emplace_back(*result.SourceManager, left, "std::move(*");
+  }
+
+ private:
+  Replacements* replacements_;
+};
+
 // Replace base::Bind() to base::BindOnce() where resulting base::Callback is
 // implicitly converted into base::OnceCallback.
 // Example:
@@ -36,10 +126,11 @@ namespace {
 //   // After
 //   base::PostTask(FROM_HERE, base::BindOnce(&Foo));
 //   base::OnceCallback<void()> cb = base::BindOnce(&Foo);
-class BindOnceRewriter : public MatchFinder::MatchCallback {
+class BindOnceRewriter : public MatchFinder::MatchCallback, public Rewriter {
  public:
   explicit BindOnceRewriter(Replacements* replacements)
       : replacements_(replacements) {}
+  ~BindOnceRewriter() override {}
 
   StatementMatcher GetMatcher() {
     auto is_once_callback = hasType(hasCanonicalType(hasDeclaration(
@@ -75,23 +166,42 @@ class BindOnceRewriter : public MatchFinder::MatchCallback {
 };
 
 llvm::cl::extrahelp common_help(CommonOptionsParser::HelpMessage);
+llvm::cl::OptionCategory rewriter_category("Rewriter Options");
+
+llvm::cl::opt<std::string> rewriter_option(
+    "rewriter",
+    llvm::cl::desc(R"(One of the name of rewriter to apply.
+Available rewriters are:
+    remove_unneeded_passed
+    bind_to_bind_once
+The default is remove_unneeded_passed.
+)"),
+    llvm::cl::init("remove_unneeded_passed"),
+    llvm::cl::cat(rewriter_category));
 
 }  // namespace.
 
 int main(int argc, const char* argv[]) {
   llvm::InitializeNativeTarget();
   llvm::InitializeNativeTargetAsmParser();
-  llvm::cl::OptionCategory category(
-      "Remove raw pointer on the receiver of Bind() target");
-  CommonOptionsParser options(argc, argv, category);
+  CommonOptionsParser options(argc, argv, rewriter_category);
   clang::tooling::ClangTool tool(options.getCompilations(),
                                  options.getSourcePathList());
 
   MatchFinder match_finder;
   std::vector<clang::tooling::Replacement> replacements;
 
-  BindOnceRewriter bind_once_rewriter(&replacements);
-  match_finder.addMatcher(bind_once_rewriter.GetMatcher(), &bind_once_rewriter);
+  std::unique_ptr<Rewriter> rewriter;
+  if (rewriter_option == "remove_unneeded_passed") {
+    auto passed_to_move =
+        llvm::make_unique<PassedToMoveRewriter>(&replacements);
+    match_finder.addMatcher(passed_to_move->GetMatcher(), passed_to_move.get());
+    rewriter = std::move(passed_to_move);
+  } else if (rewriter_option == "bind_toBind_once") {
+    auto bind_once = llvm::make_unique<BindOnceRewriter>(&replacements);
+    match_finder.addMatcher(bind_once->GetMatcher(), bind_once.get());
+    rewriter = std::move(bind_once);
+  }
 
   std::unique_ptr<clang::tooling::FrontendActionFactory> factory =
       clang::tooling::newFrontendActionFactory(&match_finder);
