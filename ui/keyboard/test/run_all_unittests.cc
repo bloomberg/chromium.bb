089dc04a9ba26c6cc7e9216bb5f4f10185d845bf
Yuichiro Hanada <yhanada@chromium.org>
2018-02-22 04:11:37 +0000

    Fix crashes in keyboard_unittests.
    - Creatig aura::Env in TestSuite::Initialize() instead of SetUp in
      each test case.
    - Using AuraTestBase instead of using AuraTestHelper directly in
      KeyboardControllerTest.
    
    Bug: 813453
    Test: Run unit tests 100 times.
    Change-Id: I3bebc9a8fc7a132c36f4fb182bc2739848e30007
    Reviewed-on: https://chromium-review.googlesource.com/928446
    Reviewed-by: Blake O'Hare <blakeo@chromium.org>
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538345}
    Former-commit-id: d963ed9d96274fe198ede650a55b8951a184b57b

--- ui/keyboard/test/run_all_unittests.cc
+++ ui/keyboard/test/run_all_unittests.cc
@@ -10,6 +10,7 @@
 #include "base/test/test_suite.h"
 #include "mojo/edk/embedder/embedder.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/aura/env.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_paths.h"
 #include "ui/gl/test/gl_surface_test_support.h"
@@ -24,6 +25,7 @@ class KeyboardTestSuite : public base::TestSuite {
   void Initialize() override {
     base::TestSuite::Initialize();
     gl::GLSurfaceTestSupport::InitializeOneOff();
+    env_ = aura::Env::CreateInstance();
     ui::RegisterPathProvider();
 
     base::FilePath ui_test_pak_path;
@@ -32,11 +34,14 @@ class KeyboardTestSuite : public base::TestSuite {
   }
 
   void Shutdown() override {
+    env_.reset();
     ui::ResourceBundle::CleanupSharedInstance();
     base::TestSuite::Shutdown();
   }
 
  private:
+  std::unique_ptr<aura::Env> env_;
+
   DISALLOW_COPY_AND_ASSIGN(KeyboardTestSuite);
 };
 
