15a87f1f2664a21ad6ed9948efd1a9d90bed236e
Ella Ge <eirage@chromium.org>
2018-02-22 00:41:14 +0000

    Apply same adjustment distance limit to GE and TE
    From UMA histogram Event.Touch.TouchAdjustment.AdjustDistance metric,
    In Windows, 90% of gesture adjust distance(DiagonalLength) are under
    16 pixel, 95% are 22 pixel, 99% are under 32 pixel. (Peak is around
    15~16 pixel)
    In Android, There are an upper bound of GestureEvent radius 16pixel.
    The histogram data are: 90% under 7, 95% under 9, and 99% under 15.
    
    Generally, laptop's pixel density is lower than mobile device, (e.g.
    SurfaceBook2 is 3200*2000 267 PPI, GooglePixel is 1920*1080 441 PPI)
    Using MaxAdjustmentRadius = 16 should be fine.
    
    In this CL, apply a upper bound = 16 to GE and TE adjustment padding
    under UnifiedTouchAdjustment flag to experiment the adjustment
    distance.
    
    Bug: 625198
    Change-Id: I6079c6aa99f19e1b89e89cc2c8e8bd44449cfcbf
    Reviewed-on: https://chromium-review.googlesource.com/911899
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538282}
    Former-commit-id: 57b9fb55cab5e7687799483b469253d00e98f611

--- third_party/WebKit/Source/core/page/TouchAdjustment.h
+++ third_party/WebKit/Source/core/page/TouchAdjustment.h
@@ -24,6 +24,7 @@
 #include "platform/geometry/FloatPoint.h"
 #include "platform/geometry/IntPoint.h"
 #include "platform/geometry/IntRect.h"
+#include "platform/geometry/LayoutSize.h"
 #include "platform/heap/Handle.h"
 #include "platform/wtf/Vector.h"
 
@@ -41,7 +42,8 @@ bool FindBestContextMenuCandidate(Node*& target_node,
                                   const IntPoint& touch_hotspot,
                                   const IntRect& touch_area,
                                   const HeapVector<Member<Node>>&);
-// FIXME: Implement the similar functions for other gestures here as well.
+
+LayoutSize GetHitTestRectForAdjustment(const IntSize& touch_area);
 
 struct TouchAdjustmentResult {
   uint32_t unique_event_id;
