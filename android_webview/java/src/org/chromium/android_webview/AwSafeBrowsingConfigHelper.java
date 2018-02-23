69729115b0629771d664bd7625737f1b276bf984
Nate Fischer <ntfschr@chromium.org>
2018-02-23 03:17:10 +0000

    AW: UMA metric for Safe Browsing manifest opt-in
    No change to logic, this just adds UMA metrics.
    
    This adds the "SafeBrowsing.WebView.AppOptIn" metric, which tracks
    application opt-in preference for Safe Browsing. Most applications will
    be in the "no opt-in prefernce" category, but it may be useful to see if
    there's a change in "opt-out" or "opt-in" numbers, especially as we
    begin to enable Safe Browsing by default.
    
    This won't give us per-application metrics, but this should still
    indicate (1) if a popular app(s) explicitly opts out of Safe Browsing,
    and (2) a rough idea of how often users are protected by WebView Safe
    Browsing (although this doesn't account for user opt-in preference).
    
    Bug: 713724
    Test: check that metric is logged under chrome://histograms/SafeBrowsing
    Change-Id: I2fa01a7938eabd61aeacbb31f09e7729115254d3
    Reviewed-on: https://chromium-review.googlesource.com/933290
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538684}
    Former-commit-id: 4fd98c9e833e4af00a899ea68ae30e466477c6cf

--- android_webview/java/src/org/chromium/android_webview/AwSafeBrowsingConfigHelper.java
+++ android_webview/java/src/org/chromium/android_webview/AwSafeBrowsingConfigHelper.java
@@ -7,12 +7,14 @@ package org.chromium.android_webview;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
 
 import org.chromium.base.Callback;
 import org.chromium.base.CommandLine;
 import org.chromium.base.Log;
 import org.chromium.base.annotations.JNINamespace;
+import org.chromium.base.metrics.RecordHistogram;
 
 /**
  * Helper class for getting the configuration settings related to safebrowsing in WebView.
@@ -26,9 +28,32 @@ public class AwSafeBrowsingConfigHelper {
 
     private static Boolean sSafeBrowsingUserOptIn;
 
+    // Used to record the UMA histogram SafeBrowsing.WebView.AppOptIn. Since these values are
+    // persisted to logs, they should never be renumbered nor reused.
+    @IntDef({AppOptIn.NO_PREFERENCE, AppOptIn.OPT_IN, AppOptIn.OPT_OUT})
+    @interface AppOptIn {
+        int NO_PREFERENCE = 0;
+        int OPT_IN = 1;
+        int OPT_OUT = 2;
+
+        int COUNT = 3;
+    }
+
+    private static void recordAppOptIn(@AppOptIn int value) {
+        RecordHistogram.recordEnumeratedHistogram(
+                "SafeBrowsing.WebView.AppOptIn", value, AppOptIn.COUNT);
+    }
+
     @SuppressWarnings("unchecked")
     public static void maybeEnableSafeBrowsingFromManifest(final Context appContext) {
         Boolean appOptIn = getAppOptInPreference(appContext);
+        if (appOptIn == null) {
+            recordAppOptIn(AppOptIn.NO_PREFERENCE);
+        } else if (appOptIn) {
+            recordAppOptIn(AppOptIn.OPT_IN);
+        } else {
+            recordAppOptIn(AppOptIn.OPT_OUT);
+        }
 
         // If the app specifies something, fallback to the app's preference, otherwise check for the
         // existence of the CLI switch.
