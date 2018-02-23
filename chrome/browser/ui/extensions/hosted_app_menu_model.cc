3449cb6ca8fbbb95f9362cbffab8f8f93d506132
Alan Cutter <alancutter@chromium.org>
2018-02-23 07:55:20 +0000

    Show origin and security icon inside App Info menu item
    This CL adds the origin string and security level icon indicator inside
    the hosted app's three dot menu.
    
    Screenshots:
    Lock icon: https://bugs.chromium.org/p/chromium/issues/attachment?aid=325757&signed_aid=y0yCNag6ofiB9TGamFqxVw==&inline=1
    Info icon: https://bugs.chromium.org/p/chromium/issues/attachment?aid=325758&signed_aid=BpISC2MNMuvLyPJfJhaIkw==&inline=1
    Warning icon: https://bugs.chromium.org/p/chromium/issues/attachment?aid=325763&signed_aid=e5D-egGM3PjGsn9ASERWkg==&inline=1
    
    Bug: 814109
    Change-Id: I98425b340a12d90dcf05b11a484229fedf7475a9
    Reviewed-on: https://chromium-review.googlesource.com/928322
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538725}
    Former-commit-id: c6210fbb60b61360409eb9f9808f39c0f2d8629e

--- chrome/browser/ui/extensions/hosted_app_menu_model.cc
+++ chrome/browser/ui/extensions/hosted_app_menu_model.cc
@@ -5,6 +5,7 @@
 #include "chrome/browser/ui/extensions/hosted_app_menu_model.h"
 
 #include "base/metrics/histogram_macros.h"
+#include "base/strings/utf_string_conversions.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/media/router/media_router_feature.h"
 #include "chrome/browser/profiles/profile.h"
@@ -12,6 +13,7 @@
 #include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/strings/grit/components_strings.h"
+#include "url/gurl.h"
 
 HostedAppMenuModel::HostedAppMenuModel(ui::AcceleratorProvider* provider,
                                        Browser* browser)
@@ -22,6 +24,16 @@ HostedAppMenuModel::~HostedAppMenuModel() {}
 void HostedAppMenuModel::Build() {
   AddItemWithStringId(IDC_HOSTED_APP_MENU_APP_INFO,
                       IDS_APP_CONTEXT_MENU_SHOW_INFO);
+  int app_info_index = GetItemCount() - 1;
+  SetMinorText(
+      app_info_index,
+      base::UTF8ToUTF16(url::Origin::Create(browser()
+                                                ->tab_strip_model()
+                                                ->GetActiveWebContents()
+                                                ->GetVisibleURL())
+                            .host()));
+  SetMinorIcon(app_info_index, browser()->toolbar_model()->GetVectorIcon());
+
   AddSeparator(ui::NORMAL_SEPARATOR);
   AddItemWithStringId(IDC_COPY_URL, IDS_COPY_URL);
   AddItemWithStringId(IDC_OPEN_IN_CHROME, IDS_OPEN_IN_CHROME);
