d5d499499e6b83b9010d76c55a2a081ddc6b61ed
Christopher Grant <cjgrant@chromium.org>
2018-02-23 20:48:47 +0000

    VR: Render the keyboard through a UI renderer
    This ensures that it interacts properly with other renderers. If we
    render the keyboard directly, it won't flush previously-queued work, and
    elements may draw out of order.
    
    BUG=815078
    R=ymalik
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Id691fea5a5a0bbbbe5f8e6d56b0ed2c33b0a8b59
    Reviewed-on: https://chromium-review.googlesource.com/934981
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Yash Malik <ymalik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538890}
    Former-commit-id: 754202450a581d633850874e23bf20b820c0fe9e

--- chrome/browser/vr/elements/keyboard.h
+++ chrome/browser/vr/elements/keyboard.h
@@ -8,6 +8,7 @@
 #include "base/macros.h"
 #include "chrome/browser/vr/elements/ui_element.h"
 #include "chrome/browser/vr/keyboard_delegate.h"
+#include "chrome/browser/vr/renderers/base_renderer.h"
 
 namespace vr {
 
@@ -30,6 +31,16 @@ class Keyboard : public UiElement {
   void OnMove(const gfx::PointF& position) override;
   void OnButtonDown(const gfx::PointF& position) override;
   void OnButtonUp(const gfx::PointF& position) override;
+
+  class Renderer : public BaseRenderer {
+   public:
+    Renderer();
+    ~Renderer() override;
+    void Draw(const CameraModel& camera_model, KeyboardDelegate* delegate);
+
+   private:
+    DISALLOW_COPY_AND_ASSIGN(Renderer);
+  };
 
  private:
   bool OnBeginFrame(const base::TimeTicks& time,
