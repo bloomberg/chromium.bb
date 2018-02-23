f58df08fdaff970e67515e9b81cdf270cfc9ef43
Peter Kasting <pkasting@chromium.org>
2018-02-23 07:27:10 +0000

    Fix unique_ptr<>() PRESUBMIT.
    Three directories had a PRESUBMIT designed to catch "std::unique_ptr<T>()" and
    require nullptr instead.  In two directories, the match was overbroad, catching
    things like "std::vector<std::unique_ptr<T>>()".  In ui/, ricea attempted a fix
    (in https://codereview.chromium.org/2311783002 ) that made the regex too
    conservative, failing to catch std::unique_ptr<Foo<T>>().
    
    Instead, fail to match std::unique_ptr<T>() if it's immediately preceded by '<'.
    
    This moves the check to the global PRESUBMIT to avoid duplication.
    
    BUG=none
    TEST=none
    TBR=sky
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Ia8888408683b2de3bbe60e0e43409d72475ea169
    Reviewed-on: https://chromium-review.googlesource.com/933547
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#538723}
    Former-commit-id: 4844e46e3da42c8cf833664dca51eed4301e503b

--- cc/PRESUBMIT.py
+++ cc/PRESUBMIT.py
@@ -144,34 +144,6 @@ def CheckDoubleAngles(input_api, output_api, white_list=CC_SOURCE_FILES,
     return [output_api.PresubmitError('Use >> instead of > >:', items=errors)]
   return []
 
-def CheckUniquePtr(input_api, output_api,
-                   white_list=CC_SOURCE_FILES, black_list=None):
-  black_list = tuple(black_list or input_api.DEFAULT_BLACK_LIST)
-  source_file_filter = lambda x: input_api.FilterSourceFile(x,
-                                                            white_list,
-                                                            black_list)
-  errors = []
-  for f in input_api.AffectedSourceFiles(source_file_filter):
-    for line_number, line in f.ChangedContents():
-      # Disallow:
-      # return std::unique_ptr<T>(foo);
-      # bar = std::unique_ptr<T>(foo);
-      # But allow:
-      # return std::unique_ptr<T[]>(foo);
-      # bar = std::unique_ptr<T[]>(foo);
-      if re.search(r'(=|\breturn)\s*std::unique_ptr<.*?(?<!])>\([^)]+\)', line):
-        errors.append(output_api.PresubmitError(
-          ('%s:%d uses explicit std::unique_ptr constructor. ' +
-           'Use std::make_unique<T>() instead.') %
-          (f.LocalPath(), line_number)))
-      # Disallow:
-      # std::unique_ptr<T>()
-      if re.search(r'\bstd::unique_ptr<.*?>\(\)', line):
-        errors.append(output_api.PresubmitError(
-          '%s:%d uses std::unique_ptr<T>(). Use nullptr instead.' %
-          (f.LocalPath(), line_number)))
-  return errors
-
 def FindUnquotedQuote(contents, pos):
   match = re.search(r"(?<!\\)(?P<quote>\")", contents[pos:])
   return -1 if not match else match.start("quote") + pos
@@ -330,7 +302,6 @@ def CheckChangeOnUpload(input_api, output_api):
   results += CheckChangeLintsClean(input_api, output_api)
   results += CheckTodos(input_api, output_api)
   results += CheckDoubleAngles(input_api, output_api)
-  results += CheckUniquePtr(input_api, output_api)
   results += CheckNamespace(input_api, output_api)
   results += CheckForUseOfWrongClock(input_api, output_api)
   results += FindUselessIfdefs(input_api, output_api)
