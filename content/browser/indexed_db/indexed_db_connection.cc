a0c556985fa5851a1dcb9298a0f0d0a11c480a07
Daniel Bratell <bratell@opera.com>
2018-02-26 17:39:08 +0000

    Fix "shadow" warning/error in jumbo builds
    There are local variables named next_id so having a global
    variable with that name triggers -Wshadow warning. This
    patch renames the global next_id -> g_next_indexed_db_connection_id.
    
    Change-Id: I31925418e69999ea789fef856058ab66125f60fa
    Reviewed-on: https://chromium-review.googlesource.com/937206
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539184}
    Former-commit-id: ff1d9bfdf7d879021b9e2b606113d8addfbfd315

--- content/browser/indexed_db/indexed_db_connection.cc
+++ content/browser/indexed_db/indexed_db_connection.cc
@@ -17,7 +17,7 @@ namespace content {
 
 namespace {
 
-static int32_t next_id;
+static int32_t g_next_indexed_db_connection_id;
 
 }  // namespace
 
@@ -25,7 +25,7 @@ IndexedDBConnection::IndexedDBConnection(
     int child_process_id,
     scoped_refptr<IndexedDBDatabase> database,
     scoped_refptr<IndexedDBDatabaseCallbacks> callbacks)
-    : id_(next_id++),
+    : id_(g_next_indexed_db_connection_id++),
       child_process_id_(child_process_id),
       database_(database),
       callbacks_(callbacks),
