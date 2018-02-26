ed5655c8980821ef9e799e715cdd236280fca1f3
Rune Lillesveen <futhark@chromium.org>
2018-02-26 09:23:43 +0000

    Use StyleForLayoutObject for option to update stacking context properly.
    We used OriginalStyleForLayoutObject which doesn't update animations or
    stacking context. Hopefully a better fix for 813348.
    
    Bug: 813348, 813439, 813836
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I93584261a935c4813f618a9291d384a5b1203ec7
    Reviewed-on: https://chromium-review.googlesource.com/924705
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539093}
    Former-commit-id: 4b96f800bd6d6c017ac14ab19e7b341c1a6efcc6

--- third_party/WebKit/Source/core/layout/LayoutBoxModelObjectTest.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBoxModelObjectTest.cpp
@@ -1088,4 +1088,27 @@ TEST_F(LayoutBoxModelObjectTest, BackfaceVisibilityChange) {
   EXPECT_FALSE(target_layer->NeedsRepaint());
 }
 
+TEST_F(LayoutBoxModelObjectTest, UpdateStackingContextForOption) {
+  SetBodyInnerHTML(R"HTML(
+    <style>
+      @keyframes op {
+        0% { opacity: 0 }
+        100% { opacity: 1 }
+      }
+      option {
+        animation: op 0.001s;
+      }
+    </style>
+    <select multiple size=1>
+      <option id=opt>PASS</option>
+    </select>
+  )HTML");
+
+  auto* option_element = GetDocument().getElementById("opt");
+  auto* option_layout = option_element->GetLayoutObject();
+  ASSERT_TRUE(option_layout);
+  EXPECT_TRUE(option_layout->StyleRef().IsStackingContext());
+  EXPECT_TRUE(option_layout->StyleRef().HasCurrentOpacityAnimation());
+}
+
 }  // namespace blink
