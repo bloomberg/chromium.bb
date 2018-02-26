400df40dfa8ec9095b0614a0be45dbea3f2a5895
Pekka Paalanen <pekka.paalanen@collabora.co.uk>
2018-02-26 11:44:25 +0200

    tests: fix scanner private-code test reference data
    Commit e501230d1dc8a5015616e104ec0e08886a0b88df "scanner: Fix broken
    private-code generation" changed the scanner output without updating the
    reference output for scanner tests.
    
    Update the reference data. This fixes 'make check'.
    
    Signed-off-by: Pekka Paalanen <pekka.paalanen@collabora.co.uk>
    Reviewed-by: Derek Foreman <derekf@osg.samsung.com>

--- tests/data/small-private-code.c
+++ tests/data/small-private-code.c
@@ -33,7 +33,7 @@
 # define __has_attribute(x) 0  /* Compatibility with non-clang compilers. */
 #endif
 
-#if (__has_attribute(visibility) || defined(__GNUC__) && __GNUC__ >= 4
+#if (__has_attribute(visibility) || defined(__GNUC__) && __GNUC__ >= 4)
 #define WL_PRIVATE __attribute__ ((visibility("hidden")))
 #else
 #define WL_PRIVATE
