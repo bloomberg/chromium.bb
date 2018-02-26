1793dc288f11e51b8cafc1e341a403cfadcb49af
Katie D <katie@chromium.org>
2018-02-26 19:52:13 +0000

    Refactors STS node processing code into node_utils.js.
    select_to_speak.js was getting too messy so this moves
    automation node related logic into a separate class.
    
    Bug: 803160
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I6867864f5ccefc14f4a5b98a31af5220d16f8259
    Reviewed-on: https://chromium-review.googlesource.com/937802
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539242}
    Former-commit-id: 941fec3de6c9e736ed2b342ffa5c7951774b9a83

--- chrome/browser/resources/chromeos/select_to_speak/select_to_speak.js
+++ chrome/browser/resources/chromeos/select_to_speak/select_to_speak.js
@@ -25,166 +25,6 @@ const DRIVE_APP_REGEXP =
     /^https:\/\/docs\.(?:sandbox\.)?google\.com\/(?:(?:presentation)|(?:document)|(?:spreadsheets)|(?:drawings)){1}\//;
 
 /**
- * Node state. Nodes can be on-screen like normal, or they may
- * be invisible if they are in a tab that is not in the foreground
- * or similar, or they may be invalid if they were removed from their
- * root, i.e. if they were in a window that was closed.
- * @enum {number}
- */
-const NodeState = {
-  NODE_STATE_INVALID: 0,
-  NODE_STATE_INVISIBLE: 1,
-  NODE_STATE_NORMAL: 2,
-};
-
-/**
- * Gets the first window containing this node.
- */
-function getNearestContainingWindow(node) {
-  // Go upwards to root nodes' parents until we find the first window.
-  if (node.root.role == RoleType.ROOT_WEB_AREA) {
-    var nextRootParent = node;
-    while (nextRootParent != null && nextRootParent.role != RoleType.WINDOW &&
-           nextRootParent.root != null &&
-           nextRootParent.root.role == RoleType.ROOT_WEB_AREA) {
-      nextRootParent = nextRootParent.root.parent;
-    }
-    return nextRootParent;
-  }
-  // If the parent isn't a root web area, just walk up the tree to find the
-  // nearest window.
-  var parent = node;
-  while (parent != null && parent.role != chrome.automation.RoleType.WINDOW) {
-    parent = parent.parent;
-  }
-  return parent;
-}
-
-/**
- * Gets the current visiblity state for a given node.
- *
- * @param {AutomationNode} node The starting node.
- * @return {NodeState} the current node state.
- */
-function getNodeState(node) {
-  if (node === undefined || node.root === null || node.root === undefined) {
-    // The node has been removed from the tree, perhaps because the
-    // window was closed.
-    return NodeState.NODE_STATE_INVALID;
-  }
-  // This might not be populated correctly on children nodes even if their
-  // parents or roots are now invisible.
-  // TODO: Update the C++ bindings to set 'invisible' automatically based
-  // on parents, rather than going through parents in JS below.
-  if (node.state.invisible) {
-    return NodeState.NODE_STATE_INVISIBLE;
-  }
-  // Walk up the tree to make sure the window it is in is not invisible.
-  var window = getNearestContainingWindow(node);
-  if (window != null && window.state[chrome.automation.StateType.INVISIBLE]) {
-    return NodeState.NODE_STATE_INVISIBLE;
-  }
-  // TODO: Also need a check for whether the window is minimized,
-  // which would also return NodeState.NODE_STATE_INVISIBLE.
-  return NodeState.NODE_STATE_NORMAL;
-}
-
-/**
- * Returns true if a node should be ignored by Select-to-Speak.
- * @param {AutomationNode} node The node to test
- * @param {boolean} includeOffscreen Whether to include offscreen nodes.
- * @return {boolean} whether this node should be ignored.
- */
-function shouldIgnoreNode(node, includeOffscreen) {
-  return (
-      !node.name || isWhitespace(node.name) || !node.location ||
-      node.state.invisible || (node.state.offscreen && !includeOffscreen));
-}
-
-/**
- * Finds all nodes within the subtree rooted at |node| that overlap
- * a given rectangle.
- * @param {AutomationNode} node The starting node.
- * @param {{left: number, top: number, width: number, height: number}} rect
- *     The bounding box to search.
- * @param {Array<AutomationNode>} nodes The matching node array to be
- *     populated.
- * @return {boolean} True if any matches are found.
- */
-function findAllMatching(node, rect, nodes) {
-  var found = false;
-  for (var c = node.firstChild; c; c = c.nextSibling) {
-    if (findAllMatching(c, rect, nodes))
-      found = true;
-  }
-
-  if (found)
-    return true;
-
-  // Closure needs node.location check here to allow the next few
-  // lines to compile.
-  if (shouldIgnoreNode(node, /* don't include offscreen */ false) ||
-      node.location === undefined)
-    return false;
-
-  if (overlaps(node.location, rect)) {
-    if (!node.children || node.children.length == 0 ||
-        node.children[0].role != RoleType.INLINE_TEXT_BOX) {
-      // Only add a node if it has no inlineTextBox children. If
-      // it has text children, they will be more precisely bounded
-      // and specific, so no need to add the parent node.
-      nodes.push(node);
-      return true;
-    }
-  }
-
-  return false;
-}
-
-/**
- * Class representing a position on the accessibility, made of a
- * selected node and the offset of that selection.
- * @typedef {{node: (!AutomationNode),
- *            offset: (number)}}
- */
-var Position;
-
-/**
- * Finds the deep equivalent node where a selection starts given a node
- * object and selection offset. This is meant to be used in conjunction with
- * the anchorObject/anchorOffset and focusObject/focusOffset of the
- * automation API.
- * @param {AutomationNode} parent The parent node of the selection,
- * similar to chrome.automation.focusObject.
- * @param {number} offset The integer offset of the selection. This is
- * similar to chrome.automation.focusOffset.
- * @return {!Position} The node matching the selected offset.
- */
-function getDeepEquivalentForSelection(parent, offset) {
-  if (parent.children.length == 0)
-    return {node: parent, offset: offset};
-  // Create a stack of children nodes to search through.
-  let nodesToCheck = parent.children.slice().reverse();
-  let index = 0;
-  var node;
-  // Delve down into the children recursively to find the
-  // one at this offset.
-  while (nodesToCheck.length > 0) {
-    node = nodesToCheck.pop();
-    if (node.children.length > 0) {
-      nodesToCheck = nodesToCheck.concat(node.children.slice().reverse());
-    } else {
-      index += node.name ? node.name.length : 0;
-      if (index > offset) {
-        return {node: node, offset: offset - index + node.name.length};
-      }
-    }
-  }
-  // We are off the end of the last node.
-  return {node: node, offset: node.name ? node.name.length : 0};
-}
-
-/**
  * Determines if a node is in one of the known Google Drive apps that needs
  * special case treatment for speaking selected text. Not all Google Drive pages
  * are included, because some are not known to have a problem with selection:
@@ -535,7 +375,8 @@ SelectToSpeak.prototype = {
   },
 
   /**
-   * Queues up selected text for reading.
+   * Queues up selected text for reading by finding the Position objects
+   * representing the selection.
    */
   requestSpeakSelectedText_: function(focusedNode) {
     // If nothing is selected, return early.
@@ -552,6 +393,16 @@ SelectToSpeak.prototype = {
       this.onNullSelection_();
       return;
     }
+    // First calculate the equivalant position for this selection.
+    // Sometimes the automation selection returns a offset into a root
+    // node rather than a child node, which may be a bug. This allows us to
+    // work around that bug until it is fixed or redefined.
+    // Note that this calculation is imperfect: it uses node name length
+    // to index into child nodes. However, not all node names are
+    // user-visible text, so this does not always work. Instead, we must
+    // fix the Blink bug where focus offset is not specific enough to
+    // say which node is selected and at what charOffset. See
+    // https://crbug.com/803160 for more.
     let anchorPosition =
         getDeepEquivalentForSelection(anchorObject, anchorOffset);
     let focusPosition = getDeepEquivalentForSelection(focusObject, focusOffset);
@@ -589,7 +440,17 @@ SelectToSpeak.prototype = {
       lastPosition.offset =
           lastPosition.node.name ? lastPosition.node.name.length : 0;
     }
+    this.readNodesInSelection_(firstPosition, lastPosition, focusedNode);
+  },
 
+  /**
+   * Reads nodes between the first and last position selected by the user.
+   * @param {Position} firstPosition The first position at which to start
+   *     reading.
+   * @param {Position} lastPosition The last position at which to stop reading.
+   * @param {AutomationNode} focusedNode The node with user focus.
+   */
+  readNodesInSelection_: function(firstPosition, lastPosition, focusedNode) {
     let nodes = [];
     let selectedNode = firstPosition.node;
     if (selectedNode.name && firstPosition.offset < selectedNode.name.length &&
@@ -653,6 +514,11 @@ SelectToSpeak.prototype = {
     this.recordStartEvent_(START_SPEECH_METHOD_KEYSTROKE);
   },
 
+  /**
+   * Gets ready to cancel future scrolling to offscreen nodes as soon as
+   * a user-initiated scroll is done.
+   * @param {AutomationNode=} root The root node to listen for events on.
+   */
   initializeScrollingToOffscreenNodes_: function(root) {
     this.scrollToSpokenNode_ = true;
     let listener = (event) => {
