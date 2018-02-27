6e272711311b2c427f94710c971d23d228a53bb3
Wenzhao Zang <wzang@chromium.org>
2018-02-27 19:34:51 +0000

    cros: Disable back button on network error screen when it's not clickable
    Bug: 781194
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I4c0946028e8fdb6e86825052af14cef498119cdb
    Reviewed-on: https://chromium-review.googlesource.com/933706
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539522}
    Former-commit-id: b04e162cfd5a703f8cee8586bae5941343b7a8f0

--- chrome/browser/resources/chromeos/login/screen_error_message.js
+++ chrome/browser/resources/chromeos/login/screen_error_message.js
@@ -253,7 +253,7 @@ login.createScreen('ErrorMessageScreen', 'error-message', function() {
     },
 
     /**
-     * Event handler that is invoked just before the screen in shown.
+     * Event handler that is invoked just before the screen is shown.
      * @param {Object} data Screen init payload.
      */
     onBeforeShow: function(data) {
@@ -261,6 +261,7 @@ login.createScreen('ErrorMessageScreen', 'error-message', function() {
       cr.ui.DropDown.show('offline-networks-list', false);
       $('login-header-bar').signinUIState = SIGNIN_UI_STATE.ERROR;
       this.cancelable = $('pod-row').pods.length;
+      $('error-message-back-button').disabled = !this.cancelable;
     },
 
     /**
