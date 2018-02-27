7fb9da44b081f573d98c8d748d37549cac0478b3
Dominic Mazzoni <dmazzoni@chromium.org>
2018-02-27 19:56:42 +0000

    Re-enable DumpAccessibilityTreeTest.AccessibilityInputTypes
    Bug: 98976,622387
    Change-Id: I61560e6c55966a2df05050d301391a385d114554
    Tbr: aboxhall@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/938382
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539530}
    Former-commit-id: 1b211867511dc0b96bfaeaa6d9ef43253cd1d8bc

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -1409,10 +1409,7 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityInputTime) {
   RunHtmlTest(FILE_PATH_LITERAL("input-time.html"));
 }
 
-// crbug.com/98976 will cause new elements to be added to the Blink a11y tree
-// Re-baseline after the Blink change goes in
-IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
-                       DISABLED_AccessibilityInputTypes) {
+IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityInputTypes) {
   RunHtmlTest(FILE_PATH_LITERAL("input-types.html"));
 }
 
