601ed64dc962b95ba6dfeb3b17f8f96aebab58e8
Mostyn Bramley-Moore <mostynb@vewd.com>
2018-02-27 13:08:58 +0000

    [jumbo] followup after 'tracing: Singleton -> NoDestructor and fix TSan test violation'
    There are multiple g_instance_for_testing globals after
    https://chromium-review.googlesource.com/c/chromium/src/+/935841 - let's
    rename one of them to avoid jumbo build breakage.
    
    TBR=primiano@chromium.org
    
    Bug: 815234,775547
    Change-Id: I184578b694e59dad8b06643db199f32820fd5887
    Reviewed-on: https://chromium-review.googlesource.com/938821
    Commit-Queue: Mostyn Bramley-Moore <mostynb@vewd.com>
    Reviewed-by: Mostyn Bramley-Moore <mostynb@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#539424}
    Former-commit-id: 4b4bfc5848baa03343cba8c470df845968e42559

--- base/trace_event/trace_log.cc
+++ base/trace_event/trace_log.cc
@@ -74,7 +74,7 @@ const size_t kEchoToConsoleTraceEventBufferChunks = 256;
 const size_t kTraceEventBufferSizeInBytes = 100 * 1024;
 const int kThreadFlushTimeoutMs = 3000;
 
-TraceLog* g_instance_for_testing = nullptr;
+TraceLog* g_instance_for_trace_log_testing = nullptr;
 
 #define MAX_TRACE_EVENT_FILTERS 32
 
@@ -335,11 +335,11 @@ TraceLog* TraceLog::GetInstance() {
 
 // static
 void TraceLog::ResetForTesting() {
-  if (!g_instance_for_testing)
+  if (!g_instance_for_trace_log_testing)
     return;
   CategoryRegistry::ResetForTesting();
-  g_instance_for_testing->~TraceLog();
-  new (g_instance_for_testing) TraceLog;
+  g_instance_for_trace_log_testing->~TraceLog();
+  new (g_instance_for_trace_log_testing) TraceLog;
 }
 
 TraceLog::TraceLog()
@@ -376,7 +376,7 @@ TraceLog::TraceLog()
 
   MemoryDumpManager::GetInstance()->RegisterDumpProvider(this, "TraceLog",
                                                          nullptr);
-  g_instance_for_testing = this;
+  g_instance_for_trace_log_testing = this;
 }
 
 TraceLog::~TraceLog() = default;
