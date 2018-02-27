aa02e03a431875fecc2a03e0faca73d4177bb2fe
Mark Pilgrim <pilgrim@chromium.org>
2018-02-27 23:28:14 +0000

    Migrate JSONResponseFetcher to SimpleURLLoader
    BUG=773295
    
    Change-Id: I306b7b9b289d48d1df9306100814c567727fdaee
    Reviewed-on: https://chromium-review.googlesource.com/939743
    Commit-Queue: Mark Pilgrim <pilgrim@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539579}
    Former-commit-id: 0828c7e5fa226b20a1590dc95d5c3142f1ab2108

--- chrome/browser/ui/app_list/search/common/json_response_fetcher.cc
+++ chrome/browser/ui/app_list/search/common/json_response_fetcher.cc
@@ -9,11 +9,17 @@
 #include "base/bind.h"
 #include "base/memory/ptr_util.h"
 #include "base/values.h"
+#include "content/public/browser/browser_context.h"
+#include "content/public/browser/browser_thread.h"
+#include "content/public/browser/storage_partition.h"
 #include "content/public/common/service_manager_connection.h"
 #include "net/base/load_flags.h"
+#include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_request_status.h"
 #include "services/data_decoder/public/cpp/safe_json_parser.h"
+#include "services/network/public/cpp/resource_request.h"
+#include "services/network/public/cpp/simple_url_loader.h"
 #include "url/gurl.h"
 
 namespace app_list {
@@ -22,9 +28,9 @@ const char kBadResponse[] = "Bad Web Service search response";
 
 JSONResponseFetcher::JSONResponseFetcher(
     const Callback& callback,
-    net::URLRequestContextGetter* context_getter)
+    content::BrowserContext* browser_context)
     : callback_(callback),
-      context_getter_(context_getter),
+      browser_context_(browser_context),
       weak_factory_(this) {
   DCHECK(!callback_.is_null());
 }
@@ -34,15 +40,40 @@ JSONResponseFetcher::~JSONResponseFetcher() {}
 void JSONResponseFetcher::Start(const GURL& query_url) {
   Stop();
 
-  fetcher_ = net::URLFetcher::Create(query_url, net::URLFetcher::GET, this);
-  fetcher_->SetRequestContext(context_getter_);
-  fetcher_->SetLoadFlags(net::LOAD_DO_NOT_SAVE_COOKIES |
-                         net::LOAD_DISABLE_CACHE);
-  fetcher_->Start();
+  net::NetworkTrafficAnnotationTag traffic_annotation =
+      net::DefineNetworkTrafficAnnotation("json_response_fetcher", R"(
+          semantics {
+            sender: "JSON Response Fetcher"
+            description:
+              "Chrome OS downloads data for a web store result."
+            trigger:
+              "When a user initiates a web store search and views results. "
+            data:
+              "JSON data comprising search results. "
+              "No user information is sent."
+            destination: GOOGLE_OWNED_SERVICE
+          }
+          policy {
+            cookies_allowed: NO
+          })");
+  auto resource_request = std::make_unique<network::ResourceRequest>();
+  resource_request->url = query_url;
+  resource_request->load_flags =
+      net::LOAD_DO_NOT_SAVE_COOKIES | net::LOAD_DISABLE_CACHE;
+  simple_loader_ = network::SimpleURLLoader::Create(std::move(resource_request),
+                                                    traffic_annotation);
+  network::mojom::URLLoaderFactory* loader_factory =
+      content::BrowserContext::GetDefaultStoragePartition(browser_context_)
+          ->GetURLLoaderFactoryForBrowserProcess()
+          .get();
+  simple_loader_->DownloadToStringOfUnboundedSizeUntilCrashAndDie(
+      loader_factory,
+      base::BindOnce(&JSONResponseFetcher::OnSimpleLoaderComplete,
+                     base::Unretained(this)));
 }
 
 void JSONResponseFetcher::Stop() {
-  fetcher_.reset();
+  simple_loader_.reset();
   weak_factory_.InvalidateWeakPtrs();
 }
 
@@ -61,25 +92,17 @@ void JSONResponseFetcher::OnJsonParseError(const std::string& error) {
   callback_.Run(std::unique_ptr<base::DictionaryValue>());
 }
 
-void JSONResponseFetcher::OnURLFetchComplete(
-    const net::URLFetcher* source) {
-  CHECK_EQ(fetcher_.get(), source);
-
-  std::unique_ptr<net::URLFetcher> fetcher(std::move(fetcher_));
-
-  if (!fetcher->GetStatus().is_success() ||
-      fetcher->GetResponseCode() != 200) {
+void JSONResponseFetcher::OnSimpleLoaderComplete(
+    std::unique_ptr<std::string> response_body) {
+  if (!response_body) {
     OnJsonParseError(kBadResponse);
     return;
   }
 
-  std::string json_data;
-  fetcher->GetResponseAsString(&json_data);
-
   // The parser will call us back via one of the callbacks.
   data_decoder::SafeJsonParser::Parse(
       content::ServiceManagerConnection::GetForProcess()->GetConnector(),
-      json_data,
+      *response_body,
       base::Bind(&JSONResponseFetcher::OnJsonParseSuccess,
                  weak_factory_.GetWeakPtr()),
       base::Bind(&JSONResponseFetcher::OnJsonParseError,
