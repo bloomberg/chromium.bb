ae15e82cb4dbcbf65289c8447c39905b81839e6d
Mina Almasry <almasrymina@chromium.org>
2018-02-28 00:06:49 +0000

    [Chromecast] Rename MediaPipelineBackendAudio
    To MediaPipelineBackendForMixer. This component will no longer be audio
    devices specific, but will be used by all devices that use stream_mixer.
    
    Rename it accordingly.
    
    BUG= internal 73746352
    Depends-On: eureka-internal/150487
    TEST=Build
    
    Change-Id: I2eaeccff32433c966b40e0ee159411899240527b
    Reviewed-on: https://chromium-review.googlesource.com/938967
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Commit-Queue: Mina Almasry <almasrymina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539591}
    Former-commit-id: a36e6c018407d5c14b6d612f4e49f3c80127a13c

--- chromecast/media/cma/backend/android/media_pipeline_backend_android.h
+++ chromecast/media/cma/backend/android/media_pipeline_backend_android.h
@@ -24,7 +24,7 @@ namespace media {
 class AudioDecoderAndroid;
 class VideoDecoderNull;
 
-// TODO(ckuiper): This class is very similar to MediaPipelineBackendAudio
+// TODO(ckuiper): This class is very similar to MediaPipelineBackendForMixer
 // (alsa/media_pipeline_backend_alsa.h) and should be consolidated into one
 // shared class/file.
 class MediaPipelineBackendAndroid : public MediaPipelineBackend {
