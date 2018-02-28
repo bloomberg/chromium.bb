5fea69246267c9bfa236721e37d625bfb1155a2d
Kinuko Yasuda <kinuko@chromium.org>
2018-02-28 06:30:18 +0000

    Simply PrefetchURLLoader{Service,Factory} with dispatch_context
    Use the binding_set's dispatch_context to attach frame_tree_node_id info
    to the binding itself, so that we don't need to create separate objects
    for each frame's factory.
    
    R=horo@chromium.org
    
    Bug: 803776
    Change-Id: I3a564ae2c75a82a82c4cbb720788f52137552f92
    Reviewed-on: https://chromium-review.googlesource.com/937521
    Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539734}
    Former-commit-id: babe4321511709c9f4abe32046dc8e7101272428

--- content/browser/loader/prefetch_url_loader_service.cc
+++ content/browser/loader/prefetch_url_loader_service.cc
@@ -17,78 +17,8 @@
 #include "mojo/public/cpp/bindings/strong_binding.h"
 #include "net/url_request/url_request_context_getter.h"
 #include "services/network/public/cpp/features.h"
-#include "services/network/public/mojom/url_loader_factory.mojom.h"
 
 namespace content {
-
-namespace {
-
-// Per-frame URLLoaderFactory. Used only when Network Service is enabled.
-class PrefetchURLLoaderFactory : public network::mojom::URLLoaderFactory,
-                                 public blink::mojom::PrefetchURLLoaderService {
- public:
-  PrefetchURLLoaderFactory(
-      content::PrefetchURLLoaderService* service,
-      scoped_refptr<URLLoaderFactoryGetter> loader_factory_getter,
-      int frame_tree_node_id)
-      : service_(std::move(service)),
-        loader_factory_getter_(std::move(loader_factory_getter)),
-        frame_tree_node_id_(frame_tree_node_id) {}
-  ~PrefetchURLLoaderFactory() override = default;
-
-  // blink::mojom::PrefetchURLLoaderService:
-  void GetFactory(network::mojom::URLLoaderFactoryRequest request) override {
-    Clone(std::move(request));
-  }
-
- private:
-  // network::mojom::URLLoaderFactory:
-  void CreateLoaderAndStart(network::mojom::URLLoaderRequest request,
-                            int32_t routing_id,
-                            int32_t request_id,
-                            uint32_t options,
-                            const network::ResourceRequest& resource_request,
-                            network::mojom::URLLoaderClientPtr client,
-                            const net::MutableNetworkTrafficAnnotationTag&
-                                traffic_annotation) override {
-    DCHECK_CURRENTLY_ON(BrowserThread::IO);
-    DCHECK(base::FeatureList::IsEnabled(network::features::kNetworkService));
-    if (!network_loader_factory_ ||
-        network_loader_factory_.encountered_error()) {
-      loader_factory_getter_->GetNetworkFactory()->Clone(
-          mojo::MakeRequest(&network_loader_factory_));
-    }
-    service_->CreateLoaderAndStart(
-        std::move(request), routing_id, request_id, options, resource_request,
-        std::move(client), traffic_annotation,
-        // NOTE: This should be fine in most cases, where the loader
-        // factory may become invalid if Network Service process is killed
-        // and restarted. The load can just fail in the case here, but if
-        // we want to be extra sure this should create a SharedURLLoaderFactory
-        // that internally holds a ref to the URLLoaderFactoryGetter so that
-        // it can re-obtain the factory if necessary.
-        base::MakeRefCounted<WeakWrapperSharedURLLoaderFactory>(
-            network_loader_factory_.get()),
-        frame_tree_node_id_);
-  }
-
-  void Clone(network::mojom::URLLoaderFactoryRequest request) override {
-    DCHECK_CURRENTLY_ON(BrowserThread::IO);
-    bindings_.AddBinding(this, std::move(request));
-  }
-
-  scoped_refptr<content::PrefetchURLLoaderService> service_;
-
-  network::mojom::URLLoaderFactoryPtr network_loader_factory_;
-  scoped_refptr<URLLoaderFactoryGetter> loader_factory_getter_;
-  int frame_tree_node_id_;
-
-  mojo::BindingSet<network::mojom::URLLoaderFactory> bindings_;
-
-  DISALLOW_COPY_AND_ASSIGN(PrefetchURLLoaderFactory);
-};
-
-}  // namespace
 
 PrefetchURLLoaderService::PrefetchURLLoaderService(
     scoped_refptr<URLLoaderFactoryGetter> factory_getter)
@@ -114,10 +44,7 @@ void PrefetchURLLoaderService::ConnectToService(
                        frame_tree_node_id, std::move(request)));
     return;
   }
-  service_bindings_.AddBinding(
-      std::make_unique<PrefetchURLLoaderFactory>(this, loader_factory_getter_,
-                                                 frame_tree_node_id),
-      std::move(request));
+  service_bindings_.AddBinding(this, std::move(request), frame_tree_node_id);
 }
 
 void PrefetchURLLoaderService::CreateLoaderAndStart(
@@ -153,6 +80,49 @@ void PrefetchURLLoaderService::CreateLoaderAndStart(
 }
 
 PrefetchURLLoaderService::~PrefetchURLLoaderService() = default;
+
+void PrefetchURLLoaderService::GetFactory(
+    network::mojom::URLLoaderFactoryRequest request) {
+  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  loader_factory_bindings_.AddBinding(this, std::move(request),
+                                      service_bindings_.dispatch_context());
+}
+
+void PrefetchURLLoaderService::CreateLoaderAndStart(
+    network::mojom::URLLoaderRequest request,
+    int32_t routing_id,
+    int32_t request_id,
+    uint32_t options,
+    const network::ResourceRequest& resource_request,
+    network::mojom::URLLoaderClientPtr client,
+    const net::MutableNetworkTrafficAnnotationTag& traffic_annotation) {
+  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  DCHECK(base::FeatureList::IsEnabled(network::features::kNetworkService));
+  if (!network_loader_factory_ || network_loader_factory_.encountered_error()) {
+    loader_factory_getter_->GetNetworkFactory()->Clone(
+        mojo::MakeRequest(&network_loader_factory_));
+  }
+  int frame_tree_node_id = loader_factory_bindings_.dispatch_context();
+  CreateLoaderAndStart(
+      std::move(request), routing_id, request_id, options, resource_request,
+      std::move(client), traffic_annotation,
+      // NOTE: This should be fine in most cases, where the loader
+      // factory may become invalid if Network Service process is killed
+      // and restarted. The load can just fail in the case here, but if
+      // we want to be extra sure this should create a SharedURLLoaderFactory
+      // that internally holds a ref to the URLLoaderFactoryGetter so that
+      // it can re-obtain the factory if necessary.
+      base::MakeRefCounted<WeakWrapperSharedURLLoaderFactory>(
+          network_loader_factory_.get()),
+      frame_tree_node_id);
+}
+
+void PrefetchURLLoaderService::Clone(
+    network::mojom::URLLoaderFactoryRequest request) {
+  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  loader_factory_bindings_.AddBinding(
+      this, std::move(request), loader_factory_bindings_.dispatch_context());
+}
 
 std::vector<std::unique_ptr<content::URLLoaderThrottle>>
 PrefetchURLLoaderService::CreateURLLoaderThrottles(
