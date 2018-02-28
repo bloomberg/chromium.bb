8d7650248f08ecd8cd9c31e691340b6d8b7685eb
tzik <tzik@chromium.org>
2018-02-28 09:43:55 +0000

    Add a static_assert to ban non-ref-counted raw-pointer receiver on base::Bind
    base::Bind used to fail if a raw pointer to a non-ref-counted object is
    passed as a receiver. Though that is an expected failure, its error
    was hard to read. This CL adds a static_assert for the failing case
    for better error message.
    
    Change-Id: I7057fbfaa0c24530f3b9e28b4362f4b8637cd1be
    Reviewed-on: https://chromium-review.googlesource.com/923661
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539772}
    Former-commit-id: 4625ac6130a090c971a3e93f021241db635b0c25

--- base/bind_internal.h
+++ base/bind_internal.h
@@ -745,13 +745,19 @@ struct MakeBindStateTypeImpl<true, Functor> {
 
 template <typename Functor, typename Receiver, typename... BoundArgs>
 struct MakeBindStateTypeImpl<true, Functor, Receiver, BoundArgs...> {
-  static_assert(!std::is_array<std::remove_reference_t<Receiver>>::value,
-                "First bound argument to a method cannot be an array.");
-  static_assert(!HasRefCountedTypeAsRawPtr<std::decay_t<BoundArgs>...>::value,
-                "A parameter is a refcounted type and needs scoped_refptr.");
-
  private:
   using DecayedReceiver = std::decay_t<Receiver>;
+
+  static_assert(!std::is_array<std::remove_reference_t<Receiver>>::value,
+                "First bound argument to a method cannot be an array.");
+  static_assert(
+      !std::is_pointer<DecayedReceiver>::value ||
+          IsRefCountedType<std::remove_pointer_t<DecayedReceiver>>::value,
+      "Receivers may not be raw pointers. If using a raw pointer here is safe"
+      " and has no lifetime concerns, use base::Unretained() and document why"
+      " it's safe.");
+  static_assert(!HasRefCountedTypeAsRawPtr<std::decay_t<BoundArgs>...>::value,
+                "A parameter is a refcounted type and needs scoped_refptr.");
 
  public:
   using Type = BindState<
