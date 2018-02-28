d57b36dbcc08e71e1d88158c0c1deefe5284b1a5
Pritam <pritam.nikam@samsung.com>
2018-02-28 17:18:07 +0000

    Converts Android-specific cookie accesses to Mojo.
    Converts all instances of access to net::CookieStore over to
    network::mojom::CookieManager.
    
    Addressed the uses of cookie store in the following android-specific files:
      - chrome/browser/android/cookies
      - chrome/browser/android/profiles
    
    Bug: 792672
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Icdbacc17b7bee4cf191994fda3639865cbf31b01
    Reviewed-on: https://chromium-review.googlesource.com/903307
    Commit-Queue: Pritam Nikam <pritam.nikam@samsung.com>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Randy Smith <rdsmith@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539857}
    Former-commit-id: ff860c7fb791d30e1532c061471732088e556cc0

--- chrome/browser/android/cookies/cookies_fetcher.cc
+++ chrome/browser/android/cookies/cookies_fetcher.cc
@@ -2,67 +2,54 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/android/cookies/cookies_fetcher.h"
+
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/time/time.h"
-#include "chrome/browser/android/cookies/cookies_fetcher.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/storage_partition.h"
 #include "content/public/common/content_switches.h"
 #include "jni/CookiesFetcher_jni.h"
-#include "net/cookies/cookie_store.h"
 #include "net/url_request/url_request_context.h"
+#include "services/network/public/mojom/cookie_manager.mojom.h"
 
 using base::android::JavaParamRef;
 using base::android::ScopedJavaLocalRef;
 
-CookiesFetcher::CookiesFetcher(JNIEnv* env, jobject obj, Profile* profile) {
+namespace {
+
+// Returns the cookie service at the client end of the mojo pipe.
+network::mojom::CookieManager* GetCookieServiceClient() {
+  return content::BrowserContext::GetDefaultStoragePartition(
+             ProfileManager::GetPrimaryUserProfile()->GetOffTheRecordProfile())
+      ->GetCookieManagerForBrowserProcess();
 }
 
-CookiesFetcher::~CookiesFetcher() {
-}
+}  // namespace
+
+CookiesFetcher::CookiesFetcher() = default;
+
+CookiesFetcher::~CookiesFetcher() = default;
 
 void CookiesFetcher::PersistCookies(JNIEnv* env,
                                     const JavaParamRef<jobject>& obj) {
-  Profile* profile = ProfileManager::GetPrimaryUserProfile();
-  if (!profile->HasOffTheRecordProfile()) {
+  if (!ProfileManager::GetPrimaryUserProfile()->HasOffTheRecordProfile()) {
     // There is no work to be done. We might consider calling
     // the Java callback if needed.
     return;
   }
-  profile = profile->GetOffTheRecordProfile();
-
-  scoped_refptr<net::URLRequestContextGetter> getter(
-      profile->GetRequestContext());
 
   // Take a strong global reference to the Java counter part and hold
   // on to it. This ensures the object would not be GC'd so we can call
   // the Java callback.
   jobject_.Reset(env, obj);
 
-  // The rest must be done from the IO thread.
-  content::BrowserThread::PostTask(
-      content::BrowserThread::IO, FROM_HERE,
-      base::Bind(&CookiesFetcher::PersistCookiesInternal,
-                 base::Unretained(this), base::RetainedRef(getter)));
-}
-
-void CookiesFetcher::PersistCookiesInternal(
-    net::URLRequestContextGetter* getter) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
-
-  net::CookieStore* store = getter->GetURLRequestContext()->cookie_store();
-
-  // Nullable sometimes according to docs. There is no work need to be done
-  // but we can consider calling the Java callback with empty output.
-  if (!store) {
-    delete this;
-    return;
-  }
-
-  store->GetAllCookiesAsync(
+  GetCookieServiceClient()->GetAllCookies(
       base::Bind(&CookiesFetcher::OnCookiesFetchFinished, base::Owned(this)));
 }
 
@@ -92,28 +79,6 @@ void CookiesFetcher::OnCookiesFetchFinished(const net::CookieList& cookies) {
   Java_CookiesFetcher_onCookieFetchFinished(env, jobject_, joa);
 }
 
-static void RestoreToCookieJarInternal(
-    net::URLRequestContextGetter* getter,
-    std::unique_ptr<net::CanonicalCookie> cookie) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
-  DCHECK(cookie->IsCanonical());
-
-  net::CookieStore* store = getter->GetURLRequestContext()->cookie_store();
-
-  // Nullable sometimes according to docs.
-  if (!store) {
-    return;
-  }
-
-  base::Callback<void(bool success)> cb;
-
-  // Assume HTTPS - since the cookies are being restored from another store,
-  // they have already gone through the strict secure check.
-  store->SetCanonicalCookieAsync(std::move(cookie), true /*secure_source*/,
-                                 true /*modify_http_only*/,
-                                 net::CookieStore::SetCookiesCallback());
-}
-
 static void JNI_CookiesFetcher_RestoreCookies(
     JNIEnv* env,
     const JavaParamRef<jclass>& jcaller,
@@ -128,14 +93,9 @@ static void JNI_CookiesFetcher_RestoreCookies(
     jboolean httponly,
     jint same_site,
     jint priority) {
-  Profile* profile = ProfileManager::GetPrimaryUserProfile();
-  if (!profile->HasOffTheRecordProfile()) {
+  if (!ProfileManager::GetPrimaryUserProfile()->HasOffTheRecordProfile()) {
     return;  // Don't create it. There is nothing to do.
   }
-  profile = profile->GetOffTheRecordProfile();
-
-  scoped_refptr<net::URLRequestContextGetter> getter(
-      profile->GetRequestContext());
 
   std::unique_ptr<net::CanonicalCookie> cookie(
       std::make_unique<net::CanonicalCookie>(
@@ -149,15 +109,15 @@ static void JNI_CookiesFetcher_RestoreCookies(
           static_cast<net::CookieSameSite>(same_site),
           static_cast<net::CookiePriority>(priority)));
 
-  // The rest must be done from the IO thread.
-  content::BrowserThread::PostTask(
-      content::BrowserThread::IO, FROM_HERE,
-      base::BindOnce(&RestoreToCookieJarInternal, base::RetainedRef(getter),
-                     std::move(cookie)));
+  // Assume HTTPS - since the cookies are being restored from another store,
+  // they have already gone through the strict secure check.
+  GetCookieServiceClient()->SetCanonicalCookie(
+      *cookie, true /* secure_source */, true /* modify_http_only */,
+      network::mojom::CookieManager::SetCanonicalCookieCallback());
 }
 
 // JNI functions
 static jlong JNI_CookiesFetcher_Init(JNIEnv* env,
                                      const JavaParamRef<jobject>& obj) {
-  return reinterpret_cast<intptr_t>(new CookiesFetcher(env, obj, 0));
+  return reinterpret_cast<intptr_t>(new CookiesFetcher());
 }
