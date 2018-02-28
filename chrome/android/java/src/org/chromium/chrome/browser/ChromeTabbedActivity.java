b5215185fcdc80da5ca9923692c924d5d41a69a8
Becky Zhou <huayinz@chromium.org>
2018-02-28 18:50:29 +0000

    [NTP OptOut] Add metrics to record click actions and expanded state
    Bug: 810433
    Change-Id: I59aa22cefec747e16993e7f747a02087b80f6aac
    Reviewed-on: https://chromium-review.googlesource.com/935684
    Commit-Queue: Becky Zhou <huayinz@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#539906}
    Former-commit-id: 3d2df3a20e14a3f76a807ec392fe0b7c89f0960f

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -105,6 +105,7 @@ import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.signin.SigninPromoUtil;
 import org.chromium.chrome.browser.snackbar.undo.UndoBarController;
 import org.chromium.chrome.browser.suggestions.SuggestionsEventReporterBridge;
+import org.chromium.chrome.browser.suggestions.SuggestionsMetrics;
 import org.chromium.chrome.browser.survey.ChromeHomeSurveyController;
 import org.chromium.chrome.browser.tab.BrowserControlsVisibilityDelegate;
 import org.chromium.chrome.browser.tab.Tab;
@@ -672,6 +673,7 @@ public class ChromeTabbedActivity
             SuggestionsEventReporterBridge.onActivityWarmResumed();
         } else {
             SuggestionsEventReporterBridge.onColdStart();
+            SuggestionsMetrics.recordArticlesListVisible();
         }
 
         maybeStartMonitoringForScreenshots();
