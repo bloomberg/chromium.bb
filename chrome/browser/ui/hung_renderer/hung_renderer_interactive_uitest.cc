03472ee60851c483875c43972e2fd3c7b06ac419
Boris Yusupov <boriay@yandex-team.ru>
2018-02-28 08:55:03 +0000

    Fix test when browser window is not active while test running
    It expects platform_util::IsWindowActive() before subcribe to renderer events
    and it isn't necessary so for example at local execution or simulatesly running
    several tests. Thus it should set window active and it is interactive ui test.
    
    Change-Id: Ia2bfd487fc1fe33aee1c35e0bbee342b72ef6fe2
    Reviewed-on: https://chromium-review.googlesource.com/936541
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: Boris Yusupov <boriay@yandex-team.ru>
    Cr-Commit-Position: refs/heads/master@{#539761}
    Former-commit-id: e7d579a80a306a09fd0735967304e495069efc04

--- chrome/browser/ui/hung_renderer/hung_renderer_interactive_uitest.cc
+++ chrome/browser/ui/hung_renderer/hung_renderer_interactive_uitest.cc
@@ -9,16 +9,17 @@
 #include "chrome/browser/ui/tab_dialogs.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
+#include "chrome/test/base/interactive_test_utils.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/test/browser_test_utils.h"
 #include "net/dns/mock_host_resolver.h"
 #include "ui/base/ui_features.h"
 
-class HungRendererNavigationBrowserTest : public InProcessBrowserTest {
+class HungRendererNavigationTest : public InProcessBrowserTest {
  public:
-  HungRendererNavigationBrowserTest() {}
-  ~HungRendererNavigationBrowserTest() override {}
+  HungRendererNavigationTest() {}
+  ~HungRendererNavigationTest() override {}
 
  protected:
   void SetUpOnMainThread() override {
@@ -26,14 +27,16 @@ class HungRendererNavigationBrowserTest : public InProcessBrowserTest {
   }
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(HungRendererNavigationBrowserTest);
+  DISALLOW_COPY_AND_ASSIGN(HungRendererNavigationTest);
 };
 
 // Verify that a cross-process navigation will dismiss the hung renderer
 // dialog so that we do not kill the new (responsive) process.
-IN_PROC_BROWSER_TEST_F(HungRendererNavigationBrowserTest,
+IN_PROC_BROWSER_TEST_F(HungRendererNavigationTest,
                        HungRendererWithCrossProcessNavigation) {
   EXPECT_TRUE(embedded_test_server()->Start());
+
+  ASSERT_TRUE(ui_test_utils::BringBrowserWindowToFront(browser()));
 
   ui_test_utils::NavigateToURL(
       browser(), embedded_test_server()->GetURL("a.com", "/title1.html"));
