b06a12d0ed254d2ba1de033690786f69985ee114
Muyuan Li <muyuanli@chromium.org>
2018-03-01 19:31:50 +0000

    add CrosDisplayConnection to assistant
    Bug: 807015
    Test: None
    Change-Id: I6b9ae265f6105dc7160f041b7e752785180889f3
    Reviewed-on: https://chromium-review.googlesource.com/941389
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Muyuan Li <muyuanli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540251}
    Former-commit-id: 5b2883ca60a48281bc230394cf877ed3ec27f0b0

--- chromeos/services/assistant/assistant_manager_service_impl.h
+++ chromeos/services/assistant/assistant_manager_service_impl.h
@@ -10,6 +10,7 @@
 
 // TODO(xiaohuic): replace with "base/macros.h" once we remove
 // libassistant/contrib dependency.
+#include "chromeos/assistant/internal/cros_display_connection.h"
 #include "chromeos/services/assistant/assistant_manager_service.h"
 #include "chromeos/services/assistant/platform_api_impl.h"
 #include "libassistant/contrib/core/macros.h"
@@ -33,6 +34,7 @@ class AssistantManagerServiceImpl : public AssistantManagerService {
   void SetAccessToken(const std::string& access_token) override;
 
  private:
+  CrosDisplayConnection display_connection_;
   PlatformApiImpl platform_api_;
   std::unique_ptr<assistant_client::AssistantManager> assistant_manager_;
   assistant_client::AssistantManagerInternal* const assistant_manager_internal_;
