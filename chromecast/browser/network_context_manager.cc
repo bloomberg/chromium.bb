6f242cc66c548c1e97b33412ab4084f013b04a36
Stephen Lanham <slan@google.com>
2018-03-01 22:13:59 +0000

    [Chromecast] Introduce chromecast::NetworkContextManager.
    This is a helper class that will be useful during Chromium's migration
    to the "network" service (see crbug.com/773295). Given a
    net::URLRequestContextGetter, this class creates a NetworkContext for
    the NetworkService instance hosted in content, and uses it to create
    URLLoaderFactories that can be created on any thread.
    
    Currently, to ease the transition, this class is designed to be
    instantiated anywhere a net::URCG might otherwise be used, wrapping the
    net::URCG instance. Currently, it lives in chromecast/browser/ because
    of the content dependencies in its implementation.
    
    When the "network" service is ready, this class can be removed.
    
    BUG=773295
    BUG= b/73881919
    
    Change-Id: I30601b837e3c960cab49b4e4024f9c69cceca05e
    Reviewed-on: https://chromium-review.googlesource.com/943594
    Commit-Queue: Stephen Lanham <slan@chromium.org>
    Reviewed-by: Sergey Volk <servolk@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540302}
    Former-commit-id: 8ca98dc3735850f82905392fc2eb5e220bfe38a0

---
+++ chromecast/browser/network_context_manager.cc
@@ -0,0 +1,78 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromecast/browser/network_context_manager.h"
+
+#include <string>
+
+#include "content/public/browser/browser_thread.h"
+#include "content/public/browser/network_service_instance.h"
+#include "net/url_request/url_request_context_getter.h"
+#include "services/network/network_context.h"
+#include "services/network/network_service.h"
+
+namespace chromecast {
+
+NetworkContextManager::NetworkContextManager(
+    net::URLRequestContextGetter* url_request_context_getter)
+    : NetworkContextManager(url_request_context_getter, nullptr) {}
+
+NetworkContextManager::NetworkContextManager(
+    net::URLRequestContextGetter* url_request_context_getter,
+    std::unique_ptr<network::NetworkService> network_service)
+    : network_service_for_test_(std::move(network_service)),
+      url_request_context_getter_(url_request_context_getter),
+      weak_factory_(this) {
+  DCHECK(url_request_context_getter_);
+  // The NetworkContext must be initialized on the browser's IO thread. Posting
+  // this task from the constructor ensures that |network_context_| will
+  // be initialized for subsequent calls to BindRequestOnIOThread().
+  content::BrowserThread::PostTask(
+      content::BrowserThread::IO, FROM_HERE,
+      base::Bind(&NetworkContextManager::InitializeOnIoThread,
+                 weak_factory_.GetWeakPtr()));
+}
+
+NetworkContextManager::~NetworkContextManager() {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
+}
+
+void NetworkContextManager::InitializeOnIoThread() {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
+
+  network::NetworkService* network_service =
+      network_service_for_test_ ? network_service_for_test_.get()
+                                : content::GetNetworkServiceImpl();
+  network_context_ = std::make_unique<network::NetworkContext>(
+      network_service, mojo::MakeRequest(&network_context_ptr_),
+      url_request_context_getter_);
+}
+
+void NetworkContextManager::BindRequestOnIOThread(
+    network::mojom::URLLoaderFactoryRequest request) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
+  network_context_->CreateURLLoaderFactory(std::move(request),
+                                           0 /* process_id */);
+}
+
+network::mojom::URLLoaderFactoryPtr
+NetworkContextManager::GetURLLoaderFactory() {
+  network::mojom::URLLoaderFactoryPtr loader_factory;
+  content::BrowserThread::PostTask(
+      content::BrowserThread::IO, FROM_HERE,
+      base::BindOnce(&NetworkContextManager::BindRequestOnIOThread,
+                     weak_factory_.GetWeakPtr(),
+                     mojo::MakeRequest(&loader_factory)));
+  return loader_factory;
+}
+
+//  static
+NetworkContextManager* NetworkContextManager::CreateForTest(
+    net::URLRequestContextGetter* url_request_context_getter,
+    std::unique_ptr<network::NetworkService> network_service) {
+  return new NetworkContextManager(url_request_context_getter,
+                                   std::move(network_service));
+}
+
+}  // namespace chromecast
