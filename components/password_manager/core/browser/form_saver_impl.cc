bb9eaf859ba04355718b6877676a3387733ffbaa
Vasilii Sukhanov <vasilii@chromium.org>
2018-03-01 17:38:49 +0000

    Don't save username/password when blacklisting a site.
    It happens when the form is prefilled when detected by the password manager.
    
    Bug: 817754
    Change-Id: Ia6c8ecd2257950aa34b3fedf796a9696c71cf4fb
    Reviewed-on: https://chromium-review.googlesource.com/943503
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540202}
    Former-commit-id: a3c2603f8a3af07b29b115c22c6d16f4bcb603a6

--- components/password_manager/core/browser/form_saver_impl.cc
+++ components/password_manager/core/browser/form_saver_impl.cc
@@ -29,6 +29,8 @@ FormSaverImpl::~FormSaverImpl() = default;
 void FormSaverImpl::PermanentlyBlacklist(PasswordForm* observed) {
   observed->preferred = false;
   observed->blacklisted_by_user = true;
+  observed->username_value.clear();
+  observed->password_value.clear();
   observed->other_possible_usernames.clear();
   observed->date_created = base::Time::Now();
 
