23e006a90b51589a7515a66777477398ee20c576
Stéphane Marchesin <marcheu@chromium.org>
2018-02-28 16:37:46 -0800

    msm: Align buffer dimensions for llvmpipe
    While we are using llvmpipe, we should align the buffer dimensions
    to a tile size.
    
    Change-Id: Ia0da10851886a2c25f8a4bff779fe890ec86baef
    Reviewed-on: https://chromium-review.googlesource.com/942346
    Commit-Ready: Douglas Anderson <dianders@chromium.org>
    Tested-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>

--- msm.c
+++ msm.c
@@ -10,6 +10,9 @@
 #include "helpers.h"
 #include "util.h"
 
+#define MESA_LLVMPIPE_TILE_ORDER 6
+#define MESA_LLVMPIPE_TILE_SIZE (1 << MESA_LLVMPIPE_TILE_ORDER)
+
 static const uint32_t render_target_formats[] = { DRM_FORMAT_ARGB8888, DRM_FORMAT_XRGB8888 };
 
 static int msm_init(struct driver *drv)
@@ -20,10 +23,23 @@ static int msm_init(struct driver *drv)
 	return drv_modify_linear_combinations(drv);
 }
 
+static int msm_bo_create(struct bo *bo, uint32_t width, uint32_t height, uint32_t format,
+			  uint64_t flags)
+{
+	width = ALIGN(width, MESA_LLVMPIPE_TILE_SIZE);
+	height = ALIGN(height, MESA_LLVMPIPE_TILE_SIZE);
+
+	/* HAL_PIXEL_FORMAT_YV12 requires that the buffer's height not be aligned. */
+	if (bo->format == DRM_FORMAT_YVU420_ANDROID)
+		height = bo->height;
+
+	return drv_dumb_bo_create(bo, width, height, format, flags);
+}
+
 const struct backend backend_msm = {
 	.name = "msm",
 	.init = msm_init,
-	.bo_create = drv_dumb_bo_create,
+	.bo_create = msm_bo_create,
 	.bo_destroy = drv_dumb_bo_destroy,
 	.bo_import = drv_prime_bo_import,
 	.bo_map = drv_dumb_bo_map,
