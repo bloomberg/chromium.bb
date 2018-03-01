49059285e1b58a12038f5f257a356277cfba7e4c
bsheedy <bsheedy@chromium.org>
2018-03-01 18:22:40 +0000

    Add VR browsing WPR benchmark
    Adds a new VR browsing Telemetry benchmark using WPR archives. It
    re-uses the URL list and WPR archive from the memory.top_10_mobile
    benchmark since they're reasonable sites that users of the VR browser
    might visit.
    
    Bug: 808597
    Change-Id: I37a2a9ba14cdf2887125564d1cf1f653940fed6d
    Reviewed-on: https://chromium-review.googlesource.com/942086
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540216}
    Former-commit-id: 37c64e8dc133ca2b7a3e09d3273458b8cd629be4

--- tools/perf/contrib/vr_benchmarks/vr_browsing_mode_pages.py
+++ tools/perf/contrib/vr_benchmarks/vr_browsing_mode_pages.py
@@ -2,9 +2,39 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import re
+
+from telemetry import page
 from telemetry import story
 from devil.android.sdk import intent  # pylint: disable=import-error
+from contrib.vr_benchmarks import shared_android_vr_page_state as vr_state
 from contrib.vr_benchmarks.vr_sample_page import VrSamplePage
+from page_sets import top_10_mobile
+
+
+def _VrBrowsingInteraction(current_page, action_runner):
+  def isNfcAppReady(android_app):
+    del android_app
+    # TODO(tiborg): Find a way to tell if the NFC app ran successfully.
+    return True
+
+  # Enter VR by simulating an NFC tag scan.
+  current_page.platform.LaunchAndroidApplication(
+      start_intent=intent.Intent(
+          component=
+          'org.chromium.chrome.browser.vr_shell.nfc_apk/.SimNfcActivity'),
+      is_app_ready_predicate=isNfcAppReady,
+      app_has_webviews=False).Close()
+
+  # Wait until Chrome is settled in VR.
+  # TODO(tiborg): Implement signal indicating that Chrome went into VR
+  # Browsing Mode. Wait times are flaky.
+  action_runner.Wait(2)
+
+  # MeasureMemory() waits for 10 seconds before measuring memory, which is
+  # long enough for us to collect our other data, so no additional sleeps
+  # necessary.
+  action_runner.MeasureMemory(True)
 
 
 class Simple2dStillPage(VrSamplePage):
@@ -15,34 +45,60 @@ class Simple2dStillPage(VrSamplePage):
          sample_page=sample_page, page_set=page_set)
 
   def RunPageInteractions(self, action_runner):
+    _VrBrowsingInteraction(self, action_runner)
 
-    def isNfcAppReady(android_app):
-      del android_app
-      # TODO(tiborg): Find a way to tell if the NFC app ran successfully.
-      return True
 
-    # Enter VR by simulating an NFC tag scan.
-    self.platform.LaunchAndroidApplication(
-        start_intent=intent.Intent(
-            component=
-            'org.chromium.chrome.browser.vr_shell.nfc_apk/.SimNfcActivity'),
-        is_app_ready_predicate=isNfcAppReady,
-        app_has_webviews=False).Close()
+class VrBrowsingModeWprPage(page.Page):
+  """Class for running a VR browsing story on a WPR page."""
 
-    # Wait until Chrome is settled in VR.
-    # TODO(tiborg): Implement signal indicating that Chrome went into VR
-    # Browsing Mode. Wait times are flaky.
-    action_runner.Wait(2)
+  def __init__(self, page_set, url, name, extra_browser_args=None):
+    """
+    Args:
+      page_set: The StorySet the VrBrowsingModeWprPage is being added to
+      url: The URL to navigate to for the story
+      name: The name of the story
+      extra_browser_args: Extra browser args that are simply forwarded to
+          page.Page
+    """
+    super(VrBrowsingModeWprPage, self).__init__(
+        url=url,
+        page_set=page_set,
+        name=name,
+        extra_browser_args=extra_browser_args,
+        shared_page_state_class=vr_state.SharedAndroidVrPageState)
+    self._shared_page_state = None
 
-    # MeasureMemory() waits for 10 seconds before measuring memory, which is
-    # long enough for us to collect our other data, so no additional sleeps
-    # necessary.
-    action_runner.MeasureMemory(True)
+  def RunPageInteractions(self, action_runner):
+    _VrBrowsingInteraction(self, action_runner)
+
+  def Run(self, shared_state):
+    self._shared_page_state = shared_state
+    super(VrBrowsingModeWprPage, self).Run(shared_state)
+
+  @property
+  def platform(self):
+    return self._shared_page_state.platform
 
 
 class VrBrowsingModePageSet(story.StorySet):
-  """Pageset for VR Browsing Mode tests."""
+  """Pageset for VR Browsing Mode tests on sample pages."""
 
   def __init__(self):
     super(VrBrowsingModePageSet, self).__init__()
     self.AddStory(Simple2dStillPage(self))
+
+
+class VrBrowsingModeWprPageSet(story.StorySet):
+  """Pageset for VR browsing mode on WPR recordings of live sites.
+
+  Re-uses the URL list and WPR archive from the memory.top_10_mobile benchmark.
+  """
+
+  def __init__(self):
+    super(VrBrowsingModeWprPageSet, self).__init__(
+        archive_data_file='../../page_sets/data/memory_top_10_mobile.json',
+        cloud_storage_bucket=story.PARTNER_BUCKET)
+
+    for url in top_10_mobile.URL_LIST:
+      name = re.sub(r'\W+', '_', url)
+      self.AddStory(VrBrowsingModeWprPage(self, url, name))
