9046db5f4b4a9cd3948a1d6f9099fbf7194a2db4
Patrick Monette <pmonette@chromium.org>
2018-03-01 21:41:23 +0000

    Reland "Add the LaunchUninstallFlow() function"
    This is a reland of 6cdf26fa7f826994918d7b81b6ea759c151a864c [formerly 5e06564abe4c3b49a41b35f9379df9f6106c4cc1]
    
    Original change's description:
    > Add the LaunchUninstallFlow() function
    >
    > This function uses automation to automatically write the name of a
    > program into the search box of the Apps & Features page.
    >
    > Bug: 717696
    > Change-Id: Ifc6a1be295a3d06ac33f394dcf3fc4688bd4a907
    > Reviewed-on: https://chromium-review.googlesource.com/907590
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Reviewed-by: Greg Thompson <grt@chromium.org>
    > Commit-Queue: Patrick Monette <pmonette@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#540209}
    
    TBR: grt@chromium.org, dpapad@chromium.org
    Bug: 717696
    Change-Id: I9048a36c52113311496abe5d2d5d1ac3fe3c79fc
    Reviewed-on: https://chromium-review.googlesource.com/944202
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540296}
    Former-commit-id: 10fd49ae901ae6f0e2b3ee735729839995164b60

--- chrome/browser/ui/webui/settings/incompatible_applications_handler_win.cc
+++ chrome/browser/ui/webui/settings/incompatible_applications_handler_win.cc
@@ -8,8 +8,10 @@
 
 #include "base/bind.h"
 #include "base/logging.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
 #include "chrome/browser/conflicts/problematic_programs_updater_win.h"
+#include "chrome/browser/conflicts/uninstall_application_win.h"
 #include "chrome/grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 
@@ -63,8 +65,9 @@ void IncompatibleApplicationsHandler::HandleStartProgramUninstallation(
     const base::ListValue* args) {
   CHECK_EQ(1u, args->GetList().size());
 
-  // TODO(pmonette): Open the Apps & Settings page with the program name
-  // highlighted.
+  // Open the Apps & Settings page with the program name highlighted.
+  uninstall_application::LaunchUninstallFlow(
+      base::UTF8ToUTF16(args->GetList()[0].GetString()));
 }
 
 void IncompatibleApplicationsHandler::HandleGetSubtitlePluralString(
