7ae3da75ac04237a9b3ef2a63172f06ac85f910f
Tarun Bansal <tbansal@chromium.org>
2018-03-01 03:45:47 +0000

    Set the default RSSI multiplier to a value less than 1.0
    Setting the default RSSI multiplier to a value less than 1.0
    allows network quality estimator (NQE) to query signal strength
    and index cached network quality by signal strength.
    
    Bug: 817102
    Change-Id: Ia507f381660ead29cfff8263415618f14e44eeb4
    Reviewed-on: https://chromium-review.googlesource.com/940274
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540027}
    Former-commit-id: 1221a49b85e0b6b9c99699f60076045bdacd8dea

--- net/nqe/network_quality_estimator_params.cc
+++ net/nqe/network_quality_estimator_params.cc
@@ -421,7 +421,15 @@ NetworkQualityEstimatorParams::NetworkQualityEstimatorParams(
           GetDoubleValueForVariationParamWithDefaultValue(
               params_,
               "rssi_weight_per_signal_strength_level",
-              1.0)),
+              // Set the default value to something less than 1.0. This enables
+              // caching of the network quality indexed by signal strength.
+              // A value of 1.0 indicates that the signal strength is not used
+              // when assigning weight to the observations. A value less than
+              // 1.0, but close to 1.0 allows caching of the network quality
+              // indexed by signal strength, while not significantly changing
+              // the weight of the observations based on signal strength.
+              // TODO(tbansal): https://crbug.com/513681: Update this value.
+              0.999)),
       forced_effective_connection_type_(
           GetInitForcedEffectiveConnectionType(params_)),
       forced_effective_connection_type_on_cellular_only_(
@@ -516,6 +524,9 @@ NetworkQualityEstimatorParams::NetworkQualityEstimatorParams(
          hanging_request_http_rtt_upper_bound_transport_rtt_multiplier_ == -1 ||
          upper_bound_http_rtt_transport_rtt_multiplier_ <
              hanging_request_http_rtt_upper_bound_transport_rtt_multiplier_);
+
+  DCHECK_GE(1.0, weight_multiplier_per_signal_strength_level_);
+  DCHECK_LE(0.0, weight_multiplier_per_signal_strength_level_);
 
   const auto algorithm_it = params_.find("effective_connection_type_algorithm");
   effective_connection_type_algorithm_ =
