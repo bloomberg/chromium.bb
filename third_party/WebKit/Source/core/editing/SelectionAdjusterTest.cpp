cee0779d8318e27148879f990dc4e35342b2bb2c
Yoshifumi Inoue <yosin@chromium.org>
2018-03-01 12:17:45 +0000

    Make SelectionAdjuster to handle collapsed selection correctly
    This patch makes |SelectionAdjuster| not to call |SetAsBackwardSelection()|
    of |SelectionTemplate::Builder| with logically collapsed range because
    |SetAsBackwardSelection()| expects |range.end > range.start|.
    
    Note: |EphemeralRange| constructor checks |start <= end|
    
    Note: logically collapsed range means |Position::CompareTo(other)| for start
    and end positions returns 0, e.g. "abc"@0 and before "abc".
    
    
    This patch is a preparation of the patch[1].
    
    
    [1] http://crrev.com/c/939142 Utilize selection_test() to
    editing/selection/select-across-readonly-input-[1-5].html
    
    Change-Id: I6e39b69aa9b0975255d262f9346506d22f800dad
    Reviewed-on: https://chromium-review.googlesource.com/938871
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540131}
    Former-commit-id: b348c20a3e9a54b876e2200ef0abc5fbafe17b05

---
+++ third_party/WebKit/Source/core/editing/SelectionAdjusterTest.cpp
@@ -0,0 +1,41 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/editing/SelectionAdjuster.h"
+
+#include "core/editing/SelectionTemplate.h"
+#include "core/editing/testing/EditingTestBase.h"
+#include "core/html/forms/TextControlElement.h"
+
+namespace blink {
+
+class SelectionAdjusterTest : public EditingTestBase {};
+
+TEST_F(SelectionAdjusterTest, AdjustShadowToCollpasedInDOMTree) {
+  const SelectionInDOMTree& selection = SetSelectionTextToBody(
+      "<span><template data-mode=\"open\">a|bc</template></span>^");
+  const SelectionInDOMTree& result =
+      SelectionAdjuster::AdjustSelectionToAvoidCrossingShadowBoundaries(
+          selection);
+  EXPECT_EQ("<span></span>|", GetSelectionTextFromBody(result));
+}
+
+TEST_F(SelectionAdjusterTest, AdjustShadowToCollpasedInFlatTree) {
+  SetBodyContent("<input value=abc>");
+  const TextControlElement& input =
+      ToTextControlElement(*GetDocument().QuerySelector("input"));
+  const SelectionInFlatTree& selection =
+      SelectionInFlatTree::Builder()
+          .Collapse(PositionInFlatTree::AfterNode(input))
+          .Extend(
+              PositionInFlatTree(*input.InnerEditorElement()->firstChild(), 1))
+          .Build();
+  const SelectionInFlatTree& result =
+      SelectionAdjuster::AdjustSelectionToAvoidCrossingShadowBoundaries(
+          selection);
+  EXPECT_EQ("<input value=\"abc\"><div>abc</div></input>|",
+            GetSelectionTextInFlatTreeFromBody(result));
+}
+
+}  // namespace blink
