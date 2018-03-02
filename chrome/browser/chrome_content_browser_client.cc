67334919e598ff9cadd06341a3382949d29109b1
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2018-03-02 03:19:46 +0000

    desktop-pwas: Specify that throttle and tests are for experimental feature
    Renames BookmarkAppNavigationThrottle to
    BookmarkAppExperimentalNavigationThrottle.
    
    Renames BookmarkAppNavigationThrottle*BrowserTests to
    BookmarkAppNavigationThrottleExperimental*BrowserTests since the
    majority of these tests are only valid when Link Capturing is
    enabled.
    
    First patch in a series of patches to disable link capturing by
    default.
    
    [1] Specify that throttle and tests are for experimental feature
        (this patch)
    [2] Extract tests that should run when link capturing is off
        (https://crrev.com/c/939883)
    [3] Handle out-of-scope navigations and stop link capturing by
        default (https://crrev.com/c/942597)
    
    Bug: 814102
    Change-Id: I9ec3923e3ee66f93617feb2f68f210fe58bc553b
    Reviewed-on: https://chromium-review.googlesource.com/940824
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Matt Giuca <mgiuca@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540433}
    Former-commit-id: ba5d9afb197d8e0ec8f843f1e3d79935b8372b48

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -369,7 +369,7 @@
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 #include "chrome/browser/accessibility/animation_policy_prefs.h"
 #include "chrome/browser/apps/platform_app_navigation_redirector.h"
-#include "chrome/browser/extensions/bookmark_app_navigation_throttle.h"
+#include "chrome/browser/extensions/bookmark_app_experimental_navigation_throttle.h"
 #include "chrome/browser/extensions/chrome_content_browser_client_extensions_part.h"
 #include "chrome/browser/extensions/chrome_extension_web_contents_observer.h"
 #include "chrome/browser/speech/extension_api/tts_engine_extension_api.h"
@@ -3420,11 +3420,11 @@ ChromeContentBrowserClient::CreateThrottlesForNavigation(
   }
 
   if (base::FeatureList::IsEnabled(features::kDesktopPWAWindowing)) {
-    auto bookmark_app_throttle =
-        extensions::BookmarkAppNavigationThrottle::MaybeCreateThrottleFor(
-            handle);
-    if (bookmark_app_throttle)
-      throttles.push_back(std::move(bookmark_app_throttle));
+    auto bookmark_app_experimental_throttle =
+        extensions::BookmarkAppExperimentalNavigationThrottle::
+            MaybeCreateThrottleFor(handle);
+    if (bookmark_app_experimental_throttle)
+      throttles.push_back(std::move(bookmark_app_experimental_throttle));
   }
 #endif
 
