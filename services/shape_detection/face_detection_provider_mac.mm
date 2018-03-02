e8649bab2e5d003059fea8d05165f9379b0b88fb
junweifu <junwei.fu@intel.com>
2018-03-02 06:42:43 +0000

    ShapeDetection: Forward declaring the necessary symbols of Vision APIs
    Forward declaring the necessary symbols in sdk_forward_declarations.h [1],
    then use the @available(macOS 10.13, *) in the face detection provider to choose
    different implementation.
    Load dynamically Vision.framework with dlopen function[2].
    Start the minimum implementation of mojom::FaceDetection based on Vision Framework.
    
    Link Mac 10.13 build bots [3] and Face Detection demo[4] here.
    Split original large CL[5] up in smaller subpatches including this CL.
    
    [1] https://cs.chromium.org/chromium/src/base/mac/sdk_forward_declarations.h?sq=package:chromium&dr=CSs&l=5
    [2] https://developer.apple.com/library/content/documentation/DeveloperTools/Conceptual/DynamicLibraries/100-Articles/DynamicLibraryUsageGuidelines.html
    [3] https://ci.chromium.org/buildbot/chromium.fyi/Chromium%20Mac%2010.13/
    [4] https://codepen.io/miguelao/pen/PmJWro
    [5] https://chromium-review.googlesource.com/c/chromium/src/+/867651/10
    
    BUG=799319
    
    TBR=thakis@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    Change-Id: I18d06c7cadd9088d2c637258c8bd36299cb3c250
    Reviewed-on: https://chromium-review.googlesource.com/900707
    Commit-Queue: Junwei Fu <junwei.fu@intel.com>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540465}
    Former-commit-id: b108a8db67a4eb1d508868b6af8573b54b7ac9e4

--- services/shape_detection/face_detection_provider_mac.mm
+++ services/shape_detection/face_detection_provider_mac.mm
@@ -9,6 +9,7 @@
 
 #include "mojo/public/cpp/bindings/strong_binding.h"
 #include "services/shape_detection/face_detection_impl_mac.h"
+#include "services/shape_detection/face_detection_impl_mac_vision.h"
 
 namespace shape_detection {
 
@@ -26,6 +27,17 @@ void FaceDetectionProviderMac::Create(
 void FaceDetectionProviderMac::CreateFaceDetection(
     mojom::FaceDetectionRequest request,
     mojom::FaceDetectorOptionsPtr options) {
+  // Vision Framework needs at least MAC OS X 10.13.
+  if (@available(macOS 10.13, *)) {
+    // Vision is more accurate than Core Image Framework, but it also needs more
+    // processing time.
+    if (!options->fast_mode) {
+      mojo::MakeStrongBinding(std::make_unique<FaceDetectionImplMacVision>(),
+                              std::move(request));
+      return;
+    }
+  }
+
   mojo::MakeStrongBinding(
       std::make_unique<FaceDetectionImplMac>(std::move(options)),
       std::move(request));
