bcadae9f0cc1e584c09340c2b3fc4c7fe348cd0b
Chris Harrelson <chrishtr@chromium.org>
2018-03-03 07:01:38 +0000

    [RLS] Re-land: skip compositing steps on frame scroll.
    This is mostly a re-land of https://chromium-review.googlesource.com/c/chromium/src/+/923089,
    now that a squashing bug was fixed in
    https://chromium.googlesource.com/chromium/src/+/55d9344e208725c72a72f4a6b7891a46863fac5d [formerly 8e4234fdc272ddda5ec9c156a2397c64d7a69f60].
    
    It differs from a straight-reland in that it also does not mark the PaintLayer with
    SetNeedsGraphicsLayerUpdate.
    
    Bug: 770343
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: If4a29f30b42ff45eb6a456bced4ab2e837f81d3e
    Reviewed-on: https://chromium-review.googlesource.com/947579
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540746}
    Former-commit-id: 9732521599b291a0703830c0d9d94c381916b7d8

--- third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.cpp
@@ -2091,10 +2091,17 @@ void PaintLayerScrollableArea::UpdateCompositingLayersAfterScroll() {
 
   if (UsesCompositedScrolling()) {
     DCHECK(Layer()->HasCompositedLayerMapping());
-    Layer()->GetCompositedLayerMapping()->SetNeedsGraphicsLayerUpdate(
-        kGraphicsLayerUpdateSubtree);
-    compositor->SetNeedsCompositingUpdate(
-        kCompositingUpdateAfterGeometryChange);
+    ScrollingCoordinator* scrolling_coordinator = GetScrollingCoordinator();
+    bool handled_scroll =
+        Layer()->IsRootLayer() && scrolling_coordinator &&
+        scrolling_coordinator->ScrollableAreaScrollLayerDidChange(this);
+
+    if (!handled_scroll) {
+      Layer()->GetCompositedLayerMapping()->SetNeedsGraphicsLayerUpdate(
+          kGraphicsLayerUpdateSubtree);
+      compositor->SetNeedsCompositingUpdate(
+          kCompositingUpdateAfterGeometryChange);
+    }
 
     // If we have fixed elements and we scroll the root layer in RLS we might
     // change compositing since the fixed elements might now overlap a
