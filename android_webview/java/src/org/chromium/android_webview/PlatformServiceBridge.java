0027d2bc913971513d4e4ad04997ddc890a46231
James Wallace-Lee <jamwalla@chromium.org>
2018-03-03 14:42:36 +0000

    Make PlatformServiceBridge instantiate PlatformServiceBridgeImpl
    PlatformServiceBridge, instead of using reflection, should load
    an instance of PlatformServiceBridgeImpl that is determined by GN.
    A downstream cl, which should land first, provides an alternate
    implementation of PlatformServiceBridgeImpl.
    
    Bug: 682070
    Change-Id: I642603ccb4e4dd4a602803772acd343bfe6cf0dd
    Reviewed-on: https://chromium-review.googlesource.com/897994
    Commit-Queue: James Wallace-Lee <jamwalla@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Paul Miller <paulmiller@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540752}
    Former-commit-id: 088e6832cc34214563bc64c9a33a034c0de3b58d

--- android_webview/java/src/org/chromium/android_webview/PlatformServiceBridge.java
+++ android_webview/java/src/org/chromium/android_webview/PlatformServiceBridge.java
@@ -8,17 +8,13 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 
 import org.chromium.base.Callback;
-import org.chromium.base.Log;
-import org.chromium.base.StrictModeContext;
 import org.chromium.base.ThreadUtils;
-
-import java.lang.reflect.InvocationTargetException;
 
 /**
  * This class manages platform-specific services. (i.e. Google Services) The platform
  * should extend this class and use this base class to fetch their specialized version.
  */
-public class PlatformServiceBridge {
+public abstract class PlatformServiceBridge {
     private static final String TAG = "PlatformServiceBrid-";
     private static final String PLATFORM_SERVICE_BRIDGE =
             "com.android.webview.chromium.PlatformServiceBridgeGoogle";
@@ -31,27 +27,12 @@ public class PlatformServiceBridge {
     @SuppressWarnings("unused")
     public static PlatformServiceBridge getInstance() {
         synchronized (sInstanceLock) {
-            if (sInstance != null) return sInstance;
-
-            // Try to get a specialized service bridge. Starting with Android O, failed reflection
-            // may cause file reads. The reflection will go away soon: https://crbug.com/682070
-            try (StrictModeContext unused = StrictModeContext.allowDiskReads()) {
-                Class<?> cls = Class.forName(PLATFORM_SERVICE_BRIDGE);
-                sInstance = (PlatformServiceBridge) cls.getDeclaredConstructor().newInstance();
-                return sInstance;
-            } catch (ClassNotFoundException e) {
-                // This is not an error; it just means this device doesn't have specialized
-                // services.
-            } catch (IllegalAccessException | IllegalArgumentException | InstantiationException
-                    | NoSuchMethodException e) {
-                Log.e(TAG, "Failed to get " + PLATFORM_SERVICE_BRIDGE + ": " + e);
-            } catch (InvocationTargetException e) {
-                Log.e(TAG, "Failed invocation to get " + PLATFORM_SERVICE_BRIDGE + ": ",
-                        e.getCause());
+            if (sInstance == null) {
+                // Load an instance of PlatformServiceBridgeImpl. Because this can change
+                // depending on the GN configuration, this may not be the PlatformServiceBridgeImpl
+                // defined upstream.
+                sInstance = new PlatformServiceBridgeImpl();
             }
-
-            // Otherwise, get the generic service bridge.
-            sInstance = new PlatformServiceBridge();
             return sInstance;
         }
     }
