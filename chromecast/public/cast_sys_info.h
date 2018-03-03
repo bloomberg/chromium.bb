66afc0a4f351a6c447fce9e8a8f4d7735f974692
Bailey Forrest <bcf@chromium.org>
2018-03-03 01:00:42 +0000

    [Chromecast] Support an arbitrary number of factory locales
    BUG=b/74038945
    TEST=Manual
    
    Change-Id: Ia61729fa620e90ae6147ca426e433b78dd48c32e
    Reviewed-on: https://chromium-review.googlesource.com/947032
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Bailey Forrest <bcf@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540703}
    Former-commit-id: 985b44c05c339d032ec82652334f37c90d11677f

--- chromecast/public/cast_sys_info.h
+++ chromecast/public/cast_sys_info.h
@@ -8,6 +8,8 @@
 #include <string>
 #include <vector>
 
+#include "chromecast_export.h"  // NOLINT(build/include)
+
 namespace chromecast {
 
 // Pure abstract interface for system information which is accessed by other
@@ -50,6 +52,10 @@ class CastSysInfo {
   // Returns default country and locale baked from the factory.
   virtual std::string GetFactoryCountry() = 0;
   virtual std::string GetFactoryLocale(std::string* second_locale) = 0;
+
+  // TODO(bcf): Make unstatic in next API update.
+  static CHROMECAST_EXPORT std::vector<std::string> GetFactoryLocaleList()
+      __attribute__((__weak__));
 
   // Returns the name of the wifi interface used to connect to the internet.
   virtual std::string GetWifiInterface() = 0;
