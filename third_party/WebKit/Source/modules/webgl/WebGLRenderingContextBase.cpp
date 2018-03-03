63e013e64eef96fedad33aab343dba97df95660a
xlai <xlai@chromium.org>
2018-03-03 02:16:17 +0000

    Clean up duplicate function WebGLRenderingContextBase::MakeImagesnapshot()
    A follow-up CL to do more cleaning on code duplication in WebGL rendering context,
    after https://chromium-review.googlesource.com/943761.
    
    Bug: 794706
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I8c9de02c113a36b4c6b95bb1dd3af25079b4c4d3
    Reviewed-on: https://chromium-review.googlesource.com/946899
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Olivia Lai <xlai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540721}
    Former-commit-id: da8c644e5580077d7b2b0640532b363cf658b159

--- third_party/WebKit/Source/modules/webgl/WebGLRenderingContextBase.cpp
+++ third_party/WebKit/Source/modules/webgl/WebGLRenderingContextBase.cpp
@@ -751,13 +751,9 @@ WebGLRenderingContextBase::GetStaticBitmapImage(
   if (!GetDrawingBuffer())
     return nullptr;
 
-  if (CreationAttributes().preserve_drawing_buffer) {
-    int width = GetDrawingBuffer()->Size().Width();
-    int height = GetDrawingBuffer()->Size().Height();
-    SkImageInfo image_info = SkImageInfo::Make(
-        width, height, kRGBA_8888_SkColorType, ColorParams().GetSkAlphaType());
-    return MakeImageSnapshot(image_info);
-  }
+  if (CreationAttributes().preserve_drawing_buffer)
+    return GetImage();
+
   return GetDrawingBuffer()->TransferToStaticBitmapImage(out_release_callback);
 }
 
@@ -782,34 +778,6 @@ scoped_refptr<StaticBitmapImage> WebGLRenderingContextBase::GetImage(
     return nullptr;
   }
   return resource_provider->Snapshot();
-}
-
-scoped_refptr<StaticBitmapImage> WebGLRenderingContextBase::MakeImageSnapshot(
-    SkImageInfo& image_info) const {
-  GetDrawingBuffer()->ResolveAndBindForReadAndDraw();
-  base::WeakPtr<WebGraphicsContext3DProviderWrapper> shared_context_wrapper =
-      SharedGpuContext::ContextProviderWrapper();
-  if (!shared_context_wrapper)
-    return nullptr;
-  gpu::gles2::GLES2Interface* gl =
-      shared_context_wrapper->ContextProvider()->ContextGL();
-
-  SkSurfaceProps disable_lcd_props(0, kUnknown_SkPixelGeometry);
-  sk_sp<SkSurface> surface = SkSurface::MakeRenderTarget(
-      shared_context_wrapper->ContextProvider()->GetGrContext(),
-      SkBudgeted::kYes, image_info, 0,
-      image_info.alphaType() == kOpaque_SkAlphaType ? nullptr
-                                                    : &disable_lcd_props);
-  const GrGLTextureInfo* texture_info = skia::GrBackendObjectToGrGLTextureInfo(
-      surface->getTextureHandle(SkSurface::kDiscardWrite_TextureHandleAccess));
-  GLuint texture_id = texture_info->fID;
-  GLenum texture_target = texture_info->fTarget;
-
-  GetDrawingBuffer()->CopyToPlatformTexture(
-      gl, texture_target, texture_id, true, false, IntPoint(0, 0),
-      IntRect(IntPoint(0, 0), GetDrawingBuffer()->Size()), kBackBuffer);
-  return AcceleratedStaticBitmapImage::CreateFromSkImage(
-      surface->makeImageSnapshot(), std::move(shared_context_wrapper));
 }
 
 ScriptPromise WebGLRenderingContextBase::setCompatibleXRDevice(
