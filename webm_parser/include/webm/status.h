af81f26025b7435fa9a14ad07c58b44cf9280430
Michael Bradshaw <mjbshaw@google.com>
2018-03-02 16:14:03 -0800

    Fix MSVC warning about 2147483648 not fitting in an int
    This also cleans up some of the documentation. I'm not sure what happened to that last line.
    
    BUG=webm:1501
    
    Change-Id: Ie0ff0607ebb60d033e7ad40bdc7943c0a6bd6783

--- webm_parser/include/webm/status.h
+++ webm_parser/include/webm/status.h
@@ -95,9 +95,10 @@ struct Status {
     // The following codes are internal-only and should not be used by users.
     // Additionally, these codes should never be returned to the user; doing so
     // is considered a bug.
-    // Parsing should switch from reading to skipping elements (internal-only
-    // use; .
-    kSwitchToSkip = -2147483648,
+    /**
+     \internal Parsing should switch from reading to skipping elements.
+     */
+    kSwitchToSkip = INT32_MIN,
   };
 
   /**
