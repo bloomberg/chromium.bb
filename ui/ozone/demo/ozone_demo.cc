7cbdbe4e70a8e31cbc707fb234882a5cf1dde2dd
Kristian H. Kristensen <hoegsberg@chromium.org>
2018-03-05 18:58:59 +0000

    ozone_demo: Add a --help option
    This demo is growing too many options and they're scattered all
    over the source. Add a --help to print them all as a quick
    reminder.
    
    Change-Id: Ieb0907165e06fbbf71e9556aa98256b705ee2a3e
    Reviewed-on: https://chromium-review.googlesource.com/943935
    Commit-Queue: Kristian H. Kristensen <hoegsberg@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540886}
    Former-commit-id: 30a4f9372825d57379d05284c773c9a9155d9ebd

--- ui/ozone/demo/ozone_demo.cc
+++ ui/ozone/demo/ozone_demo.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <iostream>
 #include <utility>
 
 #include "base/at_exit.h"
@@ -41,6 +42,7 @@ const int kTestWindowHeight = 600;
 
 const char kDisableGpu[] = "disable-gpu";
 const char kDisableSurfaceless[] = "disable-surfaceless";
+const char kHelp[] = "help";
 
 const char kWindowSize[] = "window-size";
 
@@ -340,6 +342,25 @@ int main(int argc, char** argv) {
   // Initialize logging so we can enable VLOG messages.
   logging::LoggingSettings settings;
   logging::InitLogging(settings);
+
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(kHelp)) {
+    std::cout <<
+        "Usage:\n\n"
+        "  --enable-drm-atomic         Use the atomic KMS API\n"
+        "  --disable-gpu               Force software rendering\n"
+        "  --disable-surfaceless       Don't use surfaceless EGL\n"
+        "  --window-size=WIDTHxHEIGHT  Specify window size\n"
+        "  --use-ddl                   Use SkDeferredDisplayList\n"
+        "  --partial-primary-plane     "
+        "Use smaller than fullscreen primary plane\n"
+        "  --enable-overlay            Use an overlay plane\n"
+        "  --disable-primary-plane     Don't use the primary plane\n";
+
+    // TODO(hoegsberg): We should add a little more help text about how these
+    // options interact and depend on each other.
+
+    exit(EXIT_SUCCESS);
+  }
 
   // Initialize tracing.
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(
