bloomberg/chromium.bb

commit 0f41fe195ca7429b7c753066cd836b7d98b3edf5

Fix warnings such as -Wunused-parameter, etc
-Wunused-parameter
-Wunused-but-set-variable
-Wunused-function
-Wunused-result
-Wunused-variable
-Wold-style-declaration
-Wmissing-braces
-Wswitch

This is related to #322 but doesn't actually fix any warnings that are
mentioned in there. It does however fix some warnings that are listed
in
https://www.freelists.org/post/liblouis-liblouisxml/liblouis-340-has-been-released,1

tools/lou_checkyaml.c
@@ -294,7 +294,7 @@ parse_number(const char *number, char *name, int file_line) {
 }
 
 int *
-read_inPos(yaml_parser_t *parser, int wrdlen, int translen) {
+read_inPos(yaml_parser_t *parser, int translen) {
 	int *pos = malloc(sizeof(int) * translen);
 	int i = 0;
 	yaml_event_t event;
@@ -501,7 +501,7 @@ read_options(yaml_parser_t *parser, int wordLen, int translationLen, int *xfail,
 			*typeform = read_typeforms(parser, wordLen);
 		} else if (!strcmp(option_name, "inputPos")) {
 			yaml_event_delete(&event);
-			*inPos = read_inPos(parser, wordLen, translationLen);
+			*inPos = read_inPos(parser, translationLen);
 		} else if (!strcmp(option_name, "outputPos")) {
 			yaml_event_delete(&event);
 			*outPos = read_outPos(parser, wordLen, translationLen);
@@ -733,7 +733,8 @@ main(int argc, char *argv[]) {
 	// FIXME: problem with this is that
 	// LOUIS_TABLEPATH=$(top_srcdir)/tables,... does not work anymore because
 	// $(top_srcdir) == .. (not an absolute path)
-	chdir(dir_name);
+	if (chdir(dir_name))
+		error(EXIT_FAILURE, EIO, "Cannot change directory to %s", dir_name);
 
 	// register custom table resolver
 	lou_registerTableResolver(&customTableResolver);

liblouis/compileTranslationTable.c
@@ -1413,8 +1413,7 @@ deallocateRuleNames(RuleName **ruleNames) {
 }
 
 static int
-compileSwapDots(FileInfo *nested, CharsString *source, CharsString *dest,
-		TranslationTableHeader *table) {
+compileSwapDots(FileInfo *nested, CharsString *source, CharsString *dest) {
 	int k = 0;
 	int kk = 0;
 	CharsString dotsSource;
@@ -1451,12 +1450,12 @@ compileSwap(FileInfo *nested, TranslationTableOpcode opcode, int *lastToken,
 	if (opcode == CTO_SwapCc || opcode == CTO_SwapCd) {
 		if (!parseChars(nested, &ruleChars, &matches)) return 0;
 	} else {
-		if (!compileSwapDots(nested, &matches, &ruleChars, *table)) return 0;
+		if (!compileSwapDots(nested, &matches, &ruleChars)) return 0;
 	}
 	if (opcode == CTO_SwapCc) {
 		if (!parseChars(nested, &ruleDots, &replacements)) return 0;
 	} else {
-		if (!compileSwapDots(nested, &replacements, &ruleDots, *table)) return 0;
+		if (!compileSwapDots(nested, &replacements, &ruleDots)) return 0;
 	}
 	if (!addRule(nested, opcode, &ruleChars, &ruleDots, 0, 0, newRuleOffset, newRule,
 				noback, nofor, table))
@@ -4239,25 +4238,6 @@ compileString(const char *inString, CharacterClass **characterClasses,
 	nested.linelen = k;
 	return compileRule(&nested, characterClasses, characterClassAttribute, opcodeLengths,
 			newRuleOffset, newRule, ruleNames, table);
-}
-
-static int
-makeDoubleRule(TranslationTableOpcode opcode, TranslationTableOffset *singleRule,
-		TranslationTableOffset *doubleRule, TranslationTableOffset *newRuleOffset,
-		TranslationTableRule **newRule, int noback, int nofor,
-		TranslationTableHeader **table) {
-	CharsString dots;
-	TranslationTableRule *rule;
-	if (!*singleRule || *doubleRule) return 1;
-	rule = (TranslationTableRule *)&(*table)->ruleArea[*singleRule];
-	memcpy(dots.chars, &rule->charsdots[0], rule->dotslen * CHARSIZE);
-	memcpy(&dots.chars[rule->dotslen], &rule->charsdots[0], rule->dotslen * CHARSIZE);
-	dots.length = 2 * rule->dotslen;
-	if (!addRule(NULL, opcode, NULL, &dots, 0, 0, newRuleOffset, newRule, noback, nofor,
-				table))
-		return 0;
-	*doubleRule = *newRuleOffset;
-	return 1;
 }
 
 static int

liblouis/lou_translateString.c
@@ -413,9 +413,8 @@ swapReplace(int start, int end, const TranslationTableHeader *table, InString in
 
 static int
 replaceGrouping(const TranslationTableHeader *table, InString input, OutString *output,
-		int *posMapping, int transOpcode, int passCharDots,
-		const widechar *passInstructions, int passIC, int startReplace,
-		TranslationTableRule *groupingRule, widechar groupingOp) {
+		int transOpcode, int passCharDots, const widechar *passInstructions, int passIC,
+		int startReplace, TranslationTableRule *groupingRule, widechar groupingOp) {
 	widechar startCharDots = groupingRule->charsdots[2 * passCharDots];
 	widechar endCharDots = groupingRule->charsdots[2 * passCharDots + 1];
 	widechar *curin = (widechar *)input.chars;
@@ -457,9 +456,8 @@ replaceGrouping(const TranslationTableHeader *table, InString input, OutString *
 }
 
 static int
-removeGrouping(InString *input, OutString *output, int *posMapping, int passCharDots,
-		int startReplace, int endReplace, TranslationTableRule *groupingRule,
-		widechar groupingOp) {
+removeGrouping(InString *input, OutString *output, int passCharDots, int startReplace,
+		TranslationTableRule *groupingRule, widechar groupingOp) {
 	widechar startCharDots = groupingRule->charsdots[2 * passCharDots];
 	widechar endCharDots = groupingRule->charsdots[2 * passCharDots + 1];
 	widechar *curin = (widechar *)input->chars;
@@ -849,16 +847,16 @@ passDoAction(const TranslationTableHeader *table, int *pos, int mode, InString *
 			break;
 		case pass_groupreplace:
 			if (!groupingRule ||
-					!replaceGrouping(table, *input, output, posMapping, transOpcode,
-							passCharDots, passInstructions, *passIC, startReplace,
-							groupingRule, groupingOp))
+					!replaceGrouping(table, *input, output, transOpcode, passCharDots,
+							passInstructions, *passIC, startReplace, groupingRule,
+							groupingOp))
 				return 0;
 			*passIC += 3;
 			break;
 		case pass_omit:
 			if (groupingRule)
-				removeGrouping(input, output, posMapping, passCharDots, startReplace,
-						*endReplace, groupingRule, groupingOp);
+				removeGrouping(input, output, passCharDots, startReplace, groupingRule,
+						groupingOp);
 			(*passIC)++;
 			break;
 		case pass_copy: {
@@ -906,7 +904,6 @@ translatePass(const TranslationTableHeader *table, int mode, int currentPass,
 		int *cursorStatus, int compbrlStart, int compbrlEnd) {
 	int pos;
 	int transOpcode;
-	int prevTransOpcode;
 	const TranslationTableRule *transRule;
 	int transCharslen;
 	int passCharDots;
@@ -918,7 +915,6 @@ translatePass(const TranslationTableHeader *table, int mode, int currentPass,
 	int endMatch;
 	TranslationTableRule *groupingRule;
 	widechar groupingOp;
-	prevTransOpcode = CTO_None;
 	pos = output->length = 0;
 	*posIncremented = 1;
 	_lou_resetPassVariables();
@@ -1504,21 +1500,6 @@ validMatch(const TranslationTableHeader *table, int pos, InString input,
 		prevAttr = inputChar->attributes;
 	}
 	return 1;
-}
-
-static int
-doCompEmph(const TranslationTableHeader *table, int *pos, int mode, InString input,
-		OutString *output, int *posMapping, formtype *typebuf,
-		unsigned int *emphasisBuffer, unsigned int *transNoteBuffer,
-		const TranslationTableRule **transRule, int *cursorPosition, int *cursorStatus,
-		int compbrlStart, int compbrlEnd) {
-	int endEmph;
-	for (endEmph = *pos; (typebuf[endEmph] & computer_braille) && endEmph <= input.length;
-			endEmph++)
-		;
-	return doCompTrans(*pos, endEmph, table, pos, mode, input, output, posMapping,
-			emphasisBuffer, transNoteBuffer, transRule, cursorPosition, cursorStatus,
-			compbrlStart, compbrlEnd);
 }
 
 static int
@@ -2855,8 +2836,7 @@ resolveEmphasisResets(unsigned int *buffer, const unsigned int bit_begin,
 static void
 markEmphases(const TranslationTableHeader *table, InString input, formtype *typebuf,
 		unsigned int *wordBuffer, unsigned int *emphasisBuffer,
-		unsigned int *transNoteBuffer, int haveEmphasis, int *cursorPosition,
-		int *cursorStatus, int compbrlStart, int compbrlEnd) {
+		unsigned int *transNoteBuffer, int haveEmphasis) {
 	/* Relies on the order of typeforms emph_1..emph_10. */
 	int caps_start = -1, last_caps = -1, caps_cnt = 0;
 	int emph_start[10] = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
@@ -3381,7 +3361,7 @@ translateString(const TranslationTableHeader *table, int mode, int currentPass,
 				typebuf[k] |= CAPSEMPH;
 
 	markEmphases(table, *input, typebuf, wordBuffer, emphasisBuffer, transNoteBuffer,
-			haveEmphasis, cursorPosition, cursorStatus, compbrlStart, compbrlEnd);
+			haveEmphasis);
 
 	while (pos < input->length) { /* the main translation loop */
 		if ((pos >= compbrlStart) && (pos < compbrlEnd)) {

liblouis/maketable.c
@@ -299,6 +299,8 @@ find_matching_rules(widechar *text, int text_len, widechar *braille, int braille
 			case CTO_NoCross:
 				memset(data, '0', rule->charslen - 1);
 				debug("%s", data);
+			default:
+				break;
 			}
 			free(data_save);
 			return 1;
@@ -381,7 +383,7 @@ findRelevantRules(widechar *text, widechar **rules_str) {
 	TranslationTableCharacter *character;
 	TranslationTableRule *rule;
 	TranslationTableRule **rules;
-	int hash_len, k, l, m, n;
+	int hash_len, k, m, n;
 	for (text_len = 0; text[text_len]; text_len++)
 		;
 	for (rules_len = 0; rules_str[rules_len]; rules_len++)

liblouis/pattern.c
@@ -88,7 +88,7 @@ findCharOrDots(widechar c, int m) {
 }
 
 static int
-checkAttr(const widechar c, const TranslationTableCharacterAttributes a, int m) {
+checkAttr(const widechar c, const TranslationTableCharacterAttributes a) {
 	static widechar prevc = 0;
 	static TranslationTableCharacterAttributes preva = 0;
 	if (c != prevc) {
@@ -1322,7 +1322,7 @@ pattern_check_attrs(const widechar input_char, const widechar *expr_data) {
 	int attrs;
 
 	attrs = ((expr_data[0] << 16) | expr_data[1]) & ~(CTC_EndOfInput | CTC_EmpMatch);
-	if (!checkAttr(input_char, attrs, 0)) return 0;
+	if (!checkAttr(input_char, attrs)) return 0;
 	return 1;
 }
 

liblouis/utils.c
@@ -202,11 +202,11 @@ _lou_showDots(widechar const *dots, int length) {
 /**
  * Mapping between character attribute and textual representation
  */
-const static intCharTupple attributeMapping[] = {
+static const intCharTupple attributeMapping[] = {
 	{ CTC_Space, 's' }, { CTC_Letter, 'l' }, { CTC_Digit, 'd' }, { CTC_Punctuation, 'p' },
 	{ CTC_UpperCase, 'U' }, { CTC_LowerCase, 'u' }, { CTC_Math, 'm' }, { CTC_Sign, 'S' },
 	{ CTC_LitDigit, 'D' }, { CTC_Class1, 'w' }, { CTC_Class2, 'x' }, { CTC_Class3, 'y' },
-	{ CTC_Class4, 'z' }, 0,
+	{ CTC_Class4, 'z' }, { 0, 0 },
 };
 
 /**

tools/lou_tableinfo.c
@@ -85,7 +85,6 @@ main(int argc, char **argv) {
 		fprintf(stderr, "Try `%s --help' for more information.\n", program_name);
 		exit(EXIT_FAILURE);
 	} else if (optind == argc - 1) {
-		const char *table = argv[optind];
 		fprintf(stderr, "Not supported yet\n");
 		exit(EXIT_FAILURE);
 	} else if (optind == argc - 2) {

commit d187f92b1a986ceb6aa0fb367d6e0db08dda2eeb

Fix bug in lou_checkyaml when running test in current directory
Bug was introduced in 0f41fe195ca7429b7c753066cd836b7d98b3edf5

see https://github.com/liblouis/liblouis/pull/488#issuecomment-369421321

tools/lou_checkyaml.c
@@ -808,8 +808,9 @@ main(int argc, char *argv[]) {
 	// FIXME: problem with this is that
 	// LOUIS_TABLEPATH=$(top_srcdir)/tables,... does not work anymore because
 	// $(top_srcdir) == .. (not an absolute path)
-	if (chdir(dir_name))
-		error(EXIT_FAILURE, EIO, "Cannot change directory to %s", dir_name);
+	if (i > 0)
+		if (chdir(dir_name))
+			error(EXIT_FAILURE, EIO, "Cannot change directory to %s", dir_name);
 
 	// register custom table resolver
 	lou_registerTableResolver(&customTableResolver);