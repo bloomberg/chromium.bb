fcdc6bc0dad54a0549ec39241b9c57561e525e8d
sczs <sczs@chromium.org>
2018-03-06 00:00:26 +0000

    [ios] Creates TableViewHeaderFooterItem and TableViewTextHeaderFooterItem.
    - Creates TableViewHeaderFooterItem and TableViewTextHeaderFooterItem.
    - Adds viewForHeaderInSection and viewForFooterInSection in ChromeTableVC.
    
    
    Table Catalog Screenshot:
    https://drive.google.com/open?id=1xKjc3vGVxtkfcz39OdSZY_ENFDO1OnHv
    
    Bug: 805136
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I5f3923db7909e6fbf732b0bc8d069d2cfbc6603a
    Reviewed-on: https://chromium-review.googlesource.com/942605
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540993}
    Former-commit-id: 5c22cf8bb7bf18392f1b002df9e5dbf6bf0e5995

---
+++ ios/chrome/browser/ui/table_view/cells/table_view_text_header_footer_item.mm
@@ -0,0 +1,87 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/table_view/cells/table_view_text_header_footer_item.h"
+
+#include "base/mac/foundation_util.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+namespace {
+// The inner insets of the View content.
+const CGFloat kMargin = 16;
+}
+
+@implementation TableViewTextHeaderFooterItem
+@synthesize text = _text;
+
+- (instancetype)initWithType:(NSInteger)type {
+  self = [super initWithType:type];
+  if (self) {
+    self.cellClass = [TableViewTextHeaderFooterView class];
+  }
+  return self;
+}
+
+- (void)configureHeaderFooterView:(UITableViewHeaderFooterView*)headerFooter {
+  [super configureHeaderFooterView:headerFooter];
+  TableViewTextHeaderFooterView* header =
+      base::mac::ObjCCastStrict<TableViewTextHeaderFooterView>(headerFooter);
+  header.textLabel.text = self.text;
+}
+
+@end
+
+#pragma mark - TableViewTextHeaderFooter
+
+@implementation TableViewTextHeaderFooterView
+@synthesize textLabel = _textLabel;
+
+- (instancetype)initWithReuseIdentifier:(NSString*)reuseIdentifier {
+  self = [super initWithReuseIdentifier:reuseIdentifier];
+  if (self) {
+    // Text Label, set font sizes using dynamic type.
+    _textLabel = [[UILabel alloc] init];
+    _textLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    _textLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+
+    // Container View.
+    UIView* containerView = [[UIView alloc] init];
+    containerView.translatesAutoresizingMaskIntoConstraints = NO;
+
+    // Add subviews to View Hierarchy.
+    [containerView addSubview:_textLabel];
+    [self.contentView addSubview:containerView];
+
+    // Set and activate constraints.
+    [NSLayoutConstraint activateConstraints:@[
+      // Container Constraints.
+      [containerView.leadingAnchor
+          constraintEqualToAnchor:self.contentView.leadingAnchor
+                         constant:kMargin],
+      [containerView.trailingAnchor
+          constraintEqualToAnchor:self.contentView.trailingAnchor
+                         constant:-kMargin],
+      [containerView.topAnchor
+          constraintGreaterThanOrEqualToAnchor:self.contentView.topAnchor
+                                      constant:kMargin],
+      [containerView.bottomAnchor
+          constraintLessThanOrEqualToAnchor:self.contentView.bottomAnchor
+                                   constant:-kMargin],
+      [containerView.centerYAnchor
+          constraintEqualToAnchor:self.contentView.centerYAnchor],
+      // Title Label Constraints.
+      [_textLabel.leadingAnchor
+          constraintEqualToAnchor:containerView.leadingAnchor],
+      [_textLabel.topAnchor constraintEqualToAnchor:containerView.topAnchor],
+      [_textLabel.bottomAnchor
+          constraintEqualToAnchor:containerView.bottomAnchor],
+    ]];
+  }
+  return self;
+}
+
+@end
--- ios/chrome/browser/ui/table_view/table_view_model.h
+++ ios/chrome/browser/ui/table_view/table_view_model.h
@@ -8,11 +8,12 @@
 #import <UIKit/UIKit.h>
 
 #import "ios/chrome/browser/ui/list_model/list_model.h"
+#import "ios/chrome/browser/ui/table_view/cells/table_view_header_footer_item.h"
 #import "ios/chrome/browser/ui/table_view/cells/table_view_item.h"
 
 // TableViewModel acts as a model class for table view controllers.
 @interface TableViewModel<__covariant ObjectType : TableViewItem*>
-    : ListModel<ObjectType, TableViewItem*>
+    : ListModel<ObjectType, TableViewHeaderFooterItem*>
 
 @end
 
