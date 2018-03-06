c9e3baf2a217923caefe03bf3bf27f740a63139d
Illia Martyniuk <illiam@google.com>
2018-03-06 18:19:28 +0000

    Refactoring paint_filter_struct_traits
    Created a source file and moved definitions there.
    
    Bug: 646651
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I8364220fce05e32a86118b053384097a7ba8855c
    Reviewed-on: https://chromium-review.googlesource.com/950240
    Reviewed-by: Yi Xu <yiyix@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Illia Martyniuk <illiam@google.com>
    Cr-Commit-Position: refs/heads/master@{#541149}
    Former-commit-id: a7e797cc80c00fa9686ddee815675ac23016185d

--- services/viz/public/cpp/compositing/paint_filter_struct_traits.h
+++ services/viz/public/cpp/compositing/paint_filter_struct_traits.h
@@ -7,62 +7,23 @@
 
 #include <vector>
 
-#include "cc/paint/paint_filter.h"
 #include "cc/paint/paint_op_reader.h"
 #include "cc/paint/paint_op_writer.h"
 #include "services/viz/public/interfaces/compositing/paint_filter.mojom-shared.h"
+
+namespace cc {
+class PaintFilter;
+}
 
 namespace mojo {
 
 template <>
 struct StructTraits<viz::mojom::PaintFilterDataView, sk_sp<cc::PaintFilter>> {
   static base::Optional<std::vector<uint8_t>> data(
-      const sk_sp<cc::PaintFilter>& filter) {
-    std::vector<uint8_t> memory;
-    memory.resize(cc::PaintOpWriter::HeaderBytes() +
-                  cc::PaintFilter::GetFilterSize(filter.get()));
-    cc::PaintOpWriter writer(memory.data(), memory.size(), nullptr, nullptr,
-                             true /* enable_security_constraints */);
-    writer.Write(filter.get());
-
-    if (writer.size() == 0)
-      return base::nullopt;
-
-    memory.resize(writer.size());
-    return memory;
-  }
+      const sk_sp<cc::PaintFilter>& filter);
 
   static bool Read(viz::mojom::PaintFilterDataView data,
-                   sk_sp<cc::PaintFilter>* out) {
-    base::Optional<std::vector<uint8_t>> buffer;
-    if (!data.ReadData(&buffer))
-      return false;
-
-    if (!buffer) {
-      // We may fail to serialize the filter if it doesn't fit in kBufferSize
-      // above, use an empty filter instead of rejecting the message.
-      *out = nullptr;
-      return true;
-    }
-
-    cc::PaintOpReader reader(buffer->data(), buffer->size(), nullptr,
-                             true /* enable_security_constraints */);
-    sk_sp<cc::PaintFilter> filter;
-    reader.Read(&filter);
-    if (!reader.valid()) {
-      *out = nullptr;
-      return false;
-    }
-
-    // We must have consumed all bytes writen when reading this filter.
-    if (reader.remaining_bytes() != 0u) {
-      *out = nullptr;
-      return false;
-    }
-
-    *out = std::move(filter);
-    return true;
-  }
+                   sk_sp<cc::PaintFilter>* out);
 };
 
 }  // namespace mojo
