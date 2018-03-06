9974d50ec37235f49c8b0b9c1e2e2ad2bb0febc2
Penny MacNeil <pennymac@chromium.org>
2018-03-06 01:32:20 +0000

    [Windows Sandbox] Disable PreferSys32 mitigation for now.
    Breaking debug component build, gpu process.  There is a component DLL called
    display.dll that is clashing with the same name in system32.
    
    Temporary disable for the moment.
    
    (Note: also removing the other IMAGE_LOAD mitigations from the browser
    process (post-startup) for now.  Only set in child processes.)
    
    BUG=818388
    TEST=sbox_integration_tests.exe, ProcessMitigationsTest.*
    
    Change-Id: I1198e2222493a792df675d4b7675220d0da3459f
    Reviewed-on: https://chromium-review.googlesource.com/950128
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Penny MacNeil <pennymac@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541021}
    Former-commit-id: fc897c242fac290704e0f3e9297ce97012e849f0

--- content/app/sandbox_helper_win.cc
+++ content/app/sandbox_helper_win.cc
@@ -16,12 +16,8 @@ void InitializeSandboxInfo(sandbox::SandboxInterfaceInfo* info) {
   } else {
     // Ensure the proper mitigations are enforced for the browser process.
     sandbox::ApplyProcessMitigationsToCurrentProcess(
-        sandbox::MITIGATION_DEP |
-        sandbox::MITIGATION_DEP_NO_ATL_THUNK |
-        sandbox::MITIGATION_HARDEN_TOKEN_IL_POLICY |
-        sandbox::MITIGATION_IMAGE_LOAD_NO_REMOTE |
-        sandbox::MITIGATION_IMAGE_LOAD_NO_LOW_LABEL |
-        sandbox::MITIGATION_IMAGE_LOAD_PREFER_SYS32);
+        sandbox::MITIGATION_DEP | sandbox::MITIGATION_DEP_NO_ATL_THUNK |
+        sandbox::MITIGATION_HARDEN_TOKEN_IL_POLICY);
     // Note: these mitigations are "post-startup".  Some mitigations that need
     // to be enabled sooner (e.g. MITIGATION_EXTENSION_POINT_DISABLE) are done
     // so in Chrome_ELF.
