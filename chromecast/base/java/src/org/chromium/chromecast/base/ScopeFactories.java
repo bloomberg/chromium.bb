a26c313b9b079bdfb506fbd28b0660cf55131829
Simeon Anfinrud <sanfin@chromium.org>
2018-03-06 17:14:28 +0000

    [Chromecast] Replace ReactiveUtils.
    The problem that watchBoth() tries to solve is better solved by
    using a BiFunction->ScopeFactory adapter.
    
    Old approach:
    
      ReactiveUtils.watchBoth(
              observableA.and(observableB),
              (A a, B b) -> ...);
    
    New approach:
    
      observableA.and(observableB).watch(
              ScopeFactories.both((A a, B b) -> ...));
    
    This also includes overloads for the onEnter() and onExit()
    ScopeFactories methods.
    
    Bug: None
    Test: cast_base_junit_tests
    Merge-With: eureka-internal/151014
    Change-Id: I9630f2137f31eb5b826a6843988ad7756a850fdb
    Reviewed-on: https://chromium-review.googlesource.com/937808
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Simeon Anfinrud <sanfin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541129}
    Former-commit-id: 01250292cc95ca32f6008bda6c79755ac2c52466

--- chromecast/base/java/src/org/chromium/chromecast/base/ScopeFactories.java
+++ chromecast/base/java/src/org/chromium/chromecast/base/ScopeFactories.java
@@ -34,6 +34,30 @@ public final class ScopeFactories {
     }
 
     /**
+     * Shorthand for making a ScopeFactory that only has side effects on activation, and has a Both
+     * object for activation data.
+     *
+     * For example, one can refactor the following:
+     *
+     *     observableA.and(observableB).watch((Both<A, B> data) -> {
+     *         A a = data.first;
+     *         B b = data.second;
+     *         ... // side effects on activation
+     *         return () -> {}; // no side effects on deactivation.
+     *     });
+     *
+     * ... into this:
+     *
+     *    observableA.and(observableB).watch(ScopeFactories.onEnter((A a, B b) -> ...));
+     *
+     * @param <A> The first argument of the consumer (and the first item in the Both).
+     * @param <B> The second argument of the consumer (and the second item in the Both).
+     */
+    public static <A, B> ScopeFactory<Both<A, B>> onEnter(BiConsumer<A, B> consumer) {
+        return onEnter(Both.adapt(consumer));
+    }
+
+    /**
      * Shorthand for making a ScopeFactory that only has side effects on deactivation.
      *
      * @param <T> The type of the activation data.
@@ -50,5 +74,54 @@ public final class ScopeFactories {
      */
     public static <T> ScopeFactory<T> onExit(Runnable runnable) {
         return onExit((T value) -> runnable.run());
+    }
+
+    /**
+     * Shorthand for making a ScopeFactory that only has side effects on deactivation, and has a
+     * Both object for activation data.
+     *
+     * For example, one can refactor the following:
+     *
+     *     observableA.and(observableB).watch((Both<A, B> data) -> {
+     *         A a = data.first;
+     *         B b = data.second;
+     *         return () -> {
+     *             // side effects on deactivation.
+     *         };
+     *     });
+     *
+     * ... into this:
+     *
+     *    observableA.and(observableB).watch(ScopeFactories.onExit((A a, B b) -> ...));
+     *
+     * @param <A> The first argument of the consumer (and the first item in the Both).
+     * @param <B> The second argument of the consumer (and the second item in the Both).
+     */
+    public static <A, B> ScopeFactory<Both<A, B>> onExit(BiConsumer<A, B> consumer) {
+        return onExit(Both.adapt(consumer));
+    }
+
+    /**
+     * Adapts a ScopeFactory-like function that takes two arguments into a true ScopeFactory that
+     * takes a Both object.
+     *
+     * @param <A> The type of the first argument (and the first item in the Both).
+     * @param <B> The type of the second argument (and the second item in the Both).
+     *
+     * For example, one can refactor the following:
+     *
+     *     observableA.and(observableB).watch((Both<A, B> data) -> {
+     *         A a = data.first;
+     *         B b = data.second;
+     *         ...
+     *     });
+     *
+     * ... into this:
+     *
+     *     observableA.and(observableB).watch(ScopeFactories.both((A a, B b) -> ...));
+     */
+    public static <A, B> ScopeFactory<Both<A, B>> both(
+            BiFunction<? super A, ? super B, AutoCloseable> function) {
+        return (Both<A, B> data) -> function.apply(data.first, data.second);
     }
 }
--- chromecast/base/java/test/org/chromium/chromecast/base/ScopeFactoriesTest.java
+++ chromecast/base/java/test/org/chromium/chromecast/base/ScopeFactoriesTest.java
@@ -134,4 +134,67 @@ public class ScopeFactoriesTest {
                 contains("enter Derived", "enter and ignore data", "exit and ignore data",
                         "exit Derived"));
     }
+
+    @Test
+    public void testWatchBothWithStrings() {
+        Controller<String> controllerA = new Controller<>();
+        Controller<String> controllerB = new Controller<>();
+        List<String> result = new ArrayList<>();
+        controllerA.and(controllerB).watch(ScopeFactories.both((String a, String b) -> {
+            result.add("enter: " + a + ", " + b);
+            return () -> result.add("exit: " + a + ", " + b);
+        }));
+        controllerA.set("A");
+        controllerB.set("B");
+        controllerA.set("AA");
+        controllerB.set("BB");
+        assertThat(result,
+                contains("enter: A, B", "exit: A, B", "enter: AA, B", "exit: AA, B",
+                        "enter: AA, BB"));
+    }
+
+    @Test
+    public void testWatchBothWithSuperclassAsScopeFactoryParameters() {
+        Controller<Derived> controllerA = new Controller<>();
+        Controller<Derived> controllerB = new Controller<>();
+        List<String> result = new ArrayList<>();
+        // Compile error if generics are wrong.
+        controllerA.and(controllerB).watch(ScopeFactories.both((Base a, Base b) -> {
+            result.add("enter: " + a + ", " + b);
+            return () -> result.add("exit: " + a + ", " + b);
+        }));
+        controllerA.set(new Derived());
+        controllerB.set(new Derived());
+        assertThat(result, contains("enter: Derived, Derived"));
+    }
+
+    @Test
+    public void testWatchBothOnEnter() {
+        Controller<String> controllerA = new Controller<>();
+        Controller<String> controllerB = new Controller<>();
+        List<String> result = new ArrayList<>();
+        controllerA.and(controllerB).watch(ScopeFactories.onEnter((String a, String b) -> {
+            result.add("enter: " + a + ", " + b);
+        }));
+        controllerA.set("A");
+        controllerB.set("B");
+        controllerA.set("AA");
+        controllerB.set("BB");
+        assertThat(result, contains("enter: A, B", "enter: AA, B", "enter: AA, BB"));
+    }
+
+    @Test
+    public void testWatchBothOnExit() {
+        Controller<String> controllerA = new Controller<>();
+        Controller<String> controllerB = new Controller<>();
+        List<String> result = new ArrayList<>();
+        controllerA.and(controllerB).watch(ScopeFactories.onExit((String a, String b) -> {
+            result.add("exit: " + a + ", " + b);
+        }));
+        controllerA.set("A");
+        controllerB.set("B");
+        controllerA.set("AA");
+        controllerB.set("BB");
+        assertThat(result, contains("exit: A, B", "exit: AA, B"));
+    }
 }
