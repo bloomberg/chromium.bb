5ff81110daa4210348684b8d7881aaa60c2380cc
Donn Denman <donnd@google.com>
2018-03-07 20:47:33 +0000

    [TTS] reland mojo-ify ShowUnhandleTapUIIfNeeded.
    Replaces the whole call chain for ShowUnhandledTapUIIfNeeded
    between Blink and Chrome/WebView with Mojo messaging.
    
    Removes the Blink WebTappedInfo class in favor of a new UnhandledTapInfo
    that sends a Mojo construct of the same name to the Browser which
    has a service installed via a RenderHostFrame observer.
    
    All functionality should remain unchanged.
    
    NEW CODE:
    The CSTabHelper now creates an UnhandledTapWebContentsObserver that
    connects the Mojo service via UnhandledTapNotifierImpl.  That Impl
    calls back through Java to the CSTabHelper to the CSManager to notify
    that a Tap has occurred.  The CSTabHelper ignores these notifications
    when CS is disabled.
    
    The mojo message is only sent to the browser if the tap is unhandled
    and the other required conditions are met.
    
    UPDATED TESTS:
    Test notification is now done through mojo messaging, so we now only
    know if the tap was unhandled, not whether the page changed or other
    details about why the tap was unhandled.
    
    Updated the test page by adding nodes that do not trigger, and removed
    the mousemove handler since it's unrelated to tap handling.
    
    BUG=754862
    
    TBR=dcheng@chromium, yfriedman@chromium.org, kinuko@chromium.org
    
    Original CL:
    
    Reviewed-on: https://chromium-review.googlesource.com/841544
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Change-Id: I824837419cd4116b26b32170009e7a7c3308d109
    Reviewed-on: https://chromium-review.googlesource.com/952679
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541569}
    Former-commit-id: ba4d1c49e07674cd231602aefff33b37064c4219

--- third_party/WebKit/public/web/WebViewClient.h
+++ third_party/WebKit/public/web/WebViewClient.h
@@ -226,7 +226,6 @@ class WebViewClient : protected WebWidgetClient {
   WebLayerTreeView* InitializeLayerTreeView() override { return nullptr; }
   WebScreenInfo GetScreenInfo() override { return WebScreenInfo(); }
   void SetTouchAction(WebTouchAction touch_action) override {}
-  void ShowUnhandledTapUIIfNeeded(const WebTappedInfo& tapped_info) override {}
   void Show(WebNavigationPolicy) override {}
   virtual WebWidgetClient* WidgetClient() { return this; }
 
