31578b8bee450cefd9c60d464f50389ba0979a41
Yuly Novikov <ynovikov@chromium.org>
2018-03-07 19:08:24 +0000

    Mark depthStreamToRGBAUint8Texture Flaky on HighSierra AMD
    BUG=819661
    TBR=kbr@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: If9d0357866a390ad88cf049ebb76f80588dfe0b2
    Reviewed-on: https://chromium-review.googlesource.com/953073
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541521}
    Former-commit-id: 3cfbdd621e5ea02f752bd27d4a40c616d916d6ba

--- content/test/gpu/gpu_tests/depth_capture_expectations.py
+++ content/test/gpu/gpu_tests/depth_capture_expectations.py
@@ -17,5 +17,7 @@ class DepthCaptureExpectations(GpuTestExpectations):
                ['linux', ('nvidia', 0x104a)], bug=737410)
     self.Flaky('DepthCapture_depthStreamToRGBAUint8Texture',
                ['linux', ('nvidia', 0x104a)], bug=737410)
+    self.Flaky('DepthCapture_depthStreamToRGBAUint8Texture',
+               ['highsierra', ('amd', 0x6821)], bug=819661)
     self.Fail('DepthCapture_depthStreamToR32FloatTexture',
               ['android', ('qualcomm', 'Adreno (TM) 330')], bug=765913)
