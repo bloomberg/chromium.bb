ec5ca7d55495d081a623a0e537972ed51a72697d
Pavel Feldman <pfeldman@chromium.org>
2018-03-07 19:30:47 +0000

    DevTools: unflake http/tests/devtools/oopif/oopif-elements-navigate-in.js
    Bug: 818076
    Change-Id: Iab6b751bc8e436294e3f32de3f6cd73bf9366525
    Reviewed-on: https://chromium-review.googlesource.com/952272
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541529}
    Former-commit-id: 2f43d0a5b554eb1eb41c28f1a5d5c57c955710a8

--- third_party/WebKit/LayoutTests/http/tests/devtools/oopif/oopif-elements-navigate-in.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/oopif/oopif-elements-navigate-in.js
@@ -17,16 +17,24 @@
     targetAdded: async function(target) {
       if (!target.name().startsWith('inner'))
         return;
-      let mainTarget = SDK.targetManager.mainTarget();
-      await ElementsTestRunner.expandAndDump();
+      target.pageAgent().setLifecycleEventsEnabled(true);
+      target.model(SDK.ResourceTreeModel).addEventListener(SDK.ResourceTreeModel.Events.LifecycleEvent, async (event) => {
+        if (event.data.name !== 'load')
+          return;
 
-      await mainTarget.model(SDK.ResourceTreeModel)._agent.setLifecycleEventsEnabled(true);
-      TestRunner.evaluateInPagePromise(`document.getElementById('page-iframe').src = 'http://127.0.0.1:8000/devtools/oopif/resources/inner-iframe.html';`);
-      mainTarget.model(SDK.ResourceTreeModel).addEventListener(SDK.ResourceTreeModel.Events.LifecycleEvent, async (event) => {
-        if (event.data.name === 'load') {
-          await ElementsTestRunner.expandAndDump();
-          TestRunner.completeTest();
-        }
+        // OOPIF loaded, proceed with the test.
+        await ElementsTestRunner.expandAndDump();
+
+        // Navigate iframe to in-process
+        let mainTarget = SDK.targetManager.mainTarget();
+        await mainTarget.model(SDK.ResourceTreeModel)._agent.setLifecycleEventsEnabled(true);
+        TestRunner.evaluateInPagePromise(`document.getElementById('page-iframe').src = 'http://127.0.0.1:8000/devtools/oopif/resources/inner-iframe.html';`);
+        mainTarget.model(SDK.ResourceTreeModel).addEventListener(SDK.ResourceTreeModel.Events.LifecycleEvent, async (event) => {
+          if (event.data.name === 'load') {
+            await ElementsTestRunner.expandAndDump();
+            TestRunner.completeTest();
+          }
+        });
       });
     },
 
