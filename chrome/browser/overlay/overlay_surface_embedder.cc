e0f47c1c95ac2ef498e66227da2c95f0728c9576
Mounir Lamouri <mlamouri@chromium.org>
2018-03-07 18:13:13 +0000

    Picture-in-Picture: build overlay/ and picture-in-picture/ on Android.
    This will simplify the plumbing required for Android as it will be
    shared with the one on desktop.
    
    Bug: 726619
    Change-Id: I19cf0b6befac15d540e869c195e04d9d71ea1f28
    Reviewed-on: https://chromium-review.googlesource.com/946255
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: apacible <apacible@chromium.org>
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541502}
    Former-commit-id: 41a73f163ee48a2b1968403eb7a9c90076851c18

--- chrome/browser/overlay/overlay_surface_embedder.cc
+++ chrome/browser/overlay/overlay_surface_embedder.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/overlay/overlay_surface_embedder.h"
+#include "chrome/browser/overlay/overlay_surface_embedder.h"
 
 #include "ui/compositor/layer.h"
 
