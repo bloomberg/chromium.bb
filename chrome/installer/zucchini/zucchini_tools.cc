67853fc2488a7e722b628def64630e1aff175885
ckitagawa <ckitagawa@google.com>
2018-03-07 19:25:11 +0000

    [Zucchini-read]: Remove std::set usage to reduce peak memory.
    binary tree. std::vector followed by std::sort and std::unique is a
    cheaper operation that saves a lot of RAM. Zucchini-read approximately
    halves in memory consumption (binary dependent) as a result of this
    change.
    
    std: :set is expensive in 64 bit binaries as it uses pointers to store a
    Change-Id: Ib7dd8242f8629ba436943695aa8ed3c3b8803ee4
    Reviewed-on: https://chromium-review.googlesource.com/951753
    Commit-Queue: Calder Kitagawa <ckitagawa@google.com>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541526}
    Former-commit-id: c8718204e98afc6b4696bf8078c75f9966d5b13d

--- chrome/installer/zucchini/zucchini_tools.cc
+++ chrome/installer/zucchini/zucchini_tools.cc
@@ -7,9 +7,9 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include <algorithm>
 #include <memory>
 #include <ostream>
-#include <set>
 #include <string>
 
 #include "base/bind.h"
@@ -31,30 +31,22 @@ status::Code ReadReferences(ConstBufferView image,
     return status::kStatusInvalidOldImage;
   }
 
-  auto get_num_locations = [](const ReferenceGroup& group,
-                              Disassembler* disasm) {
-    size_t count = 0;
-    auto refs = group.GetReader(disasm);
-    for (auto ref = refs->GetNext(); ref.has_value(); ref = refs->GetNext())
-      ++count;
-    return count;
-  };
-
-  auto get_num_targets = [](const ReferenceGroup& group, Disassembler* disasm) {
-    std::set<offset_t> target_set;
-    auto refs = group.GetReader(disasm);
-    for (auto ref = refs->GetNext(); ref.has_value(); ref = refs->GetNext())
-      target_set.insert(ref->target);
-    return target_set.size();
-  };
-
+  std::vector<offset_t> targets;
   for (const auto& group : disasm->MakeReferenceGroups()) {
+    targets.clear();
+    auto refs = group.GetReader(disasm.get());
+    for (auto ref = refs->GetNext(); ref.has_value(); ref = refs->GetNext())
+      targets.push_back(ref->target);
+
+    size_t num_locations = targets.size();
+    std::sort(targets.begin(), targets.end());
+    targets.erase(std::unique(targets.begin(), targets.end()), targets.end());
+    size_t num_targets = targets.size();
+
     out << "Type " << int(group.type_tag().value());
     out << ": Pool=" << static_cast<uint32_t>(group.pool_tag().value());
     out << ", width=" << group.width();
-    size_t num_locations = get_num_locations(group, disasm.get());
     out << ", #locations=" << num_locations;
-    size_t num_targets = get_num_targets(group, disasm.get());
     out << ", #targets=" << num_targets;
     if (num_targets > 0) {
       double ratio = static_cast<double>(num_locations) / num_targets;
