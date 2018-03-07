46045303198ccefb0cb98beabdb221b8fa7b35d0
Mina Almasry <almasrymina@chromium.org>
2018-03-07 19:25:37 +0000

    Reland "[Chromecast] Add support for mixer compatible CMA backend"
    This is a reland of 25b9a95652425579dc6335810dc2d39a95ba8ebd [formerly ca8bb6e77136c7fd3af0a54e465800c032ed3251]
    
    Original change's description:
    > [Chromecast] Add support for mixer compatible CMA backend
    >
    > enable_video_with_mixed_audio gn flag has been added to turn on the new
    > CMA backend.
    >
    > Created libcast_media_1.0_avsync, a target that implements this new CMA
    > backend.
    >
    > AudioDecoderForMixer now owns an instance of a new interface, AvSync.
    > AudioDecoderForMixer will notify AvSync of its current status, and
    > AvSync will call into a new interface, VideoDecoderForMixer.
    > VideoDecoderForMixer will give AvSync enough control over the video
    > clock.
    >
    > BUG=internal 73746352
    >
    > Change-Id: Id7f991ec9606b6f04dbb68f819ad5bf1c3da9cce
    > Reviewed-on: https://chromium-review.googlesource.com/947258
    > Reviewed-by: Sergey Volk <servolk@chromium.org>
    > Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    > Commit-Queue: Mina Almasry <almasrymina@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#541293}
    
    Bug: internal 73746352
    Change-Id: I7bca4c61ec5ef0338116f57d235eb74fa88133b6
    Reviewed-on: https://chromium-review.googlesource.com/952167
    Reviewed-by: Sergey Volk <servolk@chromium.org>
    Commit-Queue: Mina Almasry <almasrymina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541527}
    Former-commit-id: dc22e80e07acd4b77e83ad7b98a3eea45d935b5f

--- chromecast/media/cma/backend/audio_decoder_for_mixer.h
+++ chromecast/media/cma/backend/audio_decoder_for_mixer.h
@@ -28,6 +28,7 @@ class AudioRendererAlgorithm;
 
 namespace chromecast {
 namespace media {
+class AvSync;
 class DecoderBufferBase;
 class MediaPipelineBackendForMixer;
 
@@ -85,6 +86,7 @@ class AudioDecoderForMixer : public MediaPipelineBackend::AudioDecoder,
   bool BypassDecoder() const;
   bool ShouldStartClock() const;
   void UpdateStatistics(Statistics delta);
+  void WritePcmWrapper(const scoped_refptr<DecoderBufferBase>& buffer);
 
   MediaPipelineBackendForMixer* const backend_;
   const scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
@@ -117,6 +119,8 @@ class AudioDecoderForMixer : public MediaPipelineBackend::AudioDecoder,
   float volume_multiplier_;
 
   scoped_refptr<::media::AudioBufferMemoryPool> pool_;
+
+  std::unique_ptr<AvSync> av_sync_;
 
   base::WeakPtrFactory<AudioDecoderForMixer> weak_factory_;
 
