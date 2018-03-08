d4886f3e3c1b15392bc46ca6b9c56b8bfc733f4b
Ramin Halavati <rhalavati@chromium.org>
2018-03-08 08:53:26 +0000

    Add traffic annotation self tests.
    A script is added to check network traffic annotation tests. Common
    parts of this script and the former annotation checker are separated
    as annotation_tools.py.
    
    Bug: 690323
    Change-Id: If712cca4359a98549318fafe80d64b651765d800
    Reviewed-on: https://chromium-review.googlesource.com/951255
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541755}
    Former-commit-id: 2418cebbda28730b811d71aa97894ad13427abdb

---
+++ tools/traffic_annotation/scripts/traffic_annotation_auditor_tests.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Runs tests to ensure annotation tests are working as expected.
+"""
+
+import os
+import argparse
+import sys
+
+from annotation_tools import NetworkTrafficAnnotationTools
+
+# If this test starts failing, please set TEST_IS_ENABLED to "False" and file a
+# bug to get this reenabled, and cc the people listed in
+# //tools/traffic_annotation/OWNERS.
+TEST_IS_ENABLED = True
+
+
+class TrafficAnnotationTestsChecker():
+  def __init__(self, build_path=None):
+    """Initializes a TrafficAnnotationTestsChecker object.
+
+    Args:
+      build_path: str Absolute or relative path to a fully compiled build
+          directory.
+    """
+    self.tools = NetworkTrafficAnnotationTools(build_path)
+
+  def RunAllTests(self):
+    result = self.RunOnAllFiles()
+    #TODO(rhalavati): Add more tests, and create a pipeline for them.
+    return result
+
+  def RunOnAllFiles(self):
+    args = ["--test-only"]
+    _, stderr_text, return_code = self.tools.RunAuditor(args)
+
+    if not return_code:
+      print("RunOnAlFiles Passed.")
+    elif stderr_text:
+      print(stderr_text)
+    return return_code
+
+
+def main():
+  if not TEST_IS_ENABLED:
+    return 0
+
+  parser = argparse.ArgumentParser(
+      description="Traffic Annotation Tests checker.")
+  parser.add_argument(
+      '--build-path',
+      help='Specifies a compiled build directory, e.g. out/Debug. If not '
+           'specified, the script tries to guess it. Will not proceed if not '
+           'found.')
+
+  args = parser.parse_args()
+  checker = TrafficAnnotationTestsChecker(args.build_path)
+  return checker.RunAllTests()
+
+
+if '__main__' == __name__:
+  sys.exit(main())
