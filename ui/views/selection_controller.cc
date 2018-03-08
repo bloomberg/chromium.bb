412da36827f06abc594f14d614b709344df99a74
Tommy C. Li <tommycli@chromium.org>
2018-03-08 17:38:51 +0000

    Views: Make double/triple click work better for moving Labels/Textfields
    We currently track double and triple clicks' last click locations, and
    prevent them from aggregating and "counting" as a double/triple click
    if the user has moved the mouse a delta exceeding the drag threshold.
    
    This is currently problematic for Views that move as a result of a
    click, as we are tracking the location() with respect to the local
    View.
    
    This CL makes the location stored track the root_location(), which is
    likely closer to what the user expects, since we don't care if the
    local View moves with respect to the mouse, but rather if the mouse
    moves in an absolute sense.
    
    This also fixes triple-click for Steady State Elisions in Omnibox UI.
    
    Bug: 797354
    Change-Id: Ie29a5f61fa6ae7292dcdf854f1ace2935a640e31
    Reviewed-on: https://chromium-review.googlesource.com/954014
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541826}
    Former-commit-id: bcca8d4a2b5164aecdfa039c29a065a832ab50af

--- ui/views/selection_controller.cc
+++ ui/views/selection_controller.cc
@@ -164,7 +164,8 @@ void SelectionController::TrackMouseClicks(const ui::MouseEvent& event) {
     base::TimeDelta time_delta = event.time_stamp() - last_click_time_;
     if (!last_click_time_.is_null() &&
         time_delta.InMilliseconds() <= GetDoubleClickInterval() &&
-        !View::ExceededDragThreshold(event.location() - last_click_location_)) {
+        !View::ExceededDragThreshold(event.root_location() -
+                                     last_click_location_)) {
       // Upon clicking after a triple click, the count should go back to
       // double click and alternate between double and triple. This assignment
       // maps 0 to 1, 1 to 2, 2 to 1.
@@ -173,7 +174,7 @@ void SelectionController::TrackMouseClicks(const ui::MouseEvent& event) {
       aggregated_clicks_ = 0;
     }
     last_click_time_ = event.time_stamp();
-    last_click_location_ = event.location();
+    last_click_location_ = event.root_location();
   }
 }
 
