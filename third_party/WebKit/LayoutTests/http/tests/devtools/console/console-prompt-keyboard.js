b065866a6c196aa2e78d7f32acefa3d148500617
Erik Luo <luoe@chromium.org>
2018-03-08 03:40:05 +0000

    DevTools: make console prompt aware of linewrap in handling arrow keys
    The console prompt currently listens for ArrowUp/ArrowDown and loads
    the next/previous command from history, if the cursor is on the
    first/last line.
    
    Since a logical CodeMirror line may wrap onto multiple visual rows,
    this may unintentionally load the next command. This CL adds a check
    whether the cursor is truly on the first/last visual row.
    
    Problem gif: https://imgur.com/a/s7gPL
    
    Bug: 808130
    Change-Id: I04363e25206cece28b32b80dfc548340413112cf
    Reviewed-on: https://chromium-review.googlesource.com/927690
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541714}
    Former-commit-id: 8bb1cb186012b6a8e690419b3d18b1eb7ebc76b3

--- third_party/WebKit/LayoutTests/http/tests/devtools/console/console-prompt-keyboard.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/console/console-prompt-keyboard.js
@@ -8,12 +8,15 @@
   await TestRunner.loadModule('console_test_runner');
   await TestRunner.showPanel('console');
   await ConsoleTestRunner.waitUntilConsoleEditorLoaded();
+  // Make sure that `singleLineCharCount` wraps onto multiple lines.
+  var singleLineCharCount = 100;
+  ConsoleTestRunner.fixConsoleViewportDimensions(300, 200);
 
-  var firstCommand = 'First\nmultiline command';
-  var secondCommand = 'Second\nmultiline command';
+  var firstCommand = 'a'.repeat(singleLineCharCount) + '\n' + 'b'.repeat(singleLineCharCount);
   TestRunner.addResult('Adding first message: ' + firstCommand);
   await ConsoleTestRunner.evaluateInConsolePromise(firstCommand);
 
+  var secondCommand = 'x'.repeat(singleLineCharCount) + '\n' + 'y'.repeat(singleLineCharCount);
   TestRunner.addResult('Setting prompt text: ' + secondCommand);
   var prompt = Console.ConsoleView.instance()._prompt;
   prompt.setText(secondCommand);
@@ -22,40 +25,61 @@
   prompt._editor.setSelection(TextUtils.TextRange.createFromLocation(1, 0));
   dumpSelection();
   sendKeyUpToPrompt();
-  TestRunner.addResult('Prompt text:' + prompt.text());
+  printSelectedCommand();
 
   TestRunner.addResult('\nTest that ArrowUp+shift stays in the same command');
   prompt._editor.setSelection(new TextUtils.TextRange(0, 0, 0, 1));
   dumpSelection();
   sendKeyUpToPrompt(true);
-  TestRunner.addResult('Prompt text:' + prompt.text());
+  printSelectedCommand();
+
+  TestRunner.addResult('\nTest that arrow Up on the first line, second visual row stays in the same command');
+  prompt._editor.setSelection(TextUtils.TextRange.createFromLocation(0, singleLineCharCount));
+  dumpSelection();
+  sendKeyUpToPrompt();
+  printSelectedCommand();
 
   TestRunner.addResult('\nTest that arrow Up from the first line loads previous command');
   prompt._editor.setSelection(TextUtils.TextRange.createFromLocation(0, 0));
   dumpSelection();
   sendKeyUpToPrompt();
-  TestRunner.addResult('Prompt text:' + prompt.text());
+  printSelectedCommand();
 
 
   TestRunner.addResult('\nTest that arrow Down stays in the same command');
   prompt._editor.setSelection(TextUtils.TextRange.createFromLocation(0, 0));
   dumpSelection();
   sendKeyDownToPrompt();
-  TestRunner.addResult('Prompt text:' + prompt.text());
+  printSelectedCommand();
 
   TestRunner.addResult('\nTest that ArrowDown+shift stays in the same command');
   prompt._editor.setSelection(new TextUtils.TextRange(1, 0, 1, 1));
   dumpSelection();
   sendKeyDownToPrompt(true);
-  TestRunner.addResult('Prompt text:' + prompt.text());
+  printSelectedCommand();
 
-  TestRunner.addResult('\nTest that arrow Down from the first line loads next command');
+  TestRunner.addResult('\nTest that arrow Down on the last line, first visual row stays in the same command');
   prompt._editor.setSelection(TextUtils.TextRange.createFromLocation(1, 0));
   dumpSelection();
   sendKeyDownToPrompt();
-  TestRunner.addResult('Prompt text:' + prompt.text());
+  printSelectedCommand();
+
+  TestRunner.addResult('\nTest that arrow Down from the first line loads next command');
+  prompt._editor.setSelection(TextUtils.TextRange.createFromLocation(1, singleLineCharCount));
+  dumpSelection();
+  sendKeyDownToPrompt();
+  printSelectedCommand();
 
   TestRunner.completeTest();
+
+  function printSelectedCommand() {
+    if (prompt.text().startsWith('a'))
+      TestRunner.addResult('Prompt is displaying FIRST message');
+    else if (prompt.text().startsWith('x'))
+      TestRunner.addResult('Prompt is displaying SECOND message');
+    else
+      TestRunner.addResult('TEST FAILURE');
+  }
 
   /**
    * @param {boolean} shiftKey
