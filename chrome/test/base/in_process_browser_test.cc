70b365d6a950a3cf4645ee9b6dca131f0bc4e0a4
Karan Bhatia <karandeepb@chromium.org>
2018-03-09 00:13:57 +0000

    Extensions: Fix memory leak in ExtensionApiFrameIdMap.
    Currently in ExtensionApiFrameIdMap::UpdateTabAndWindowId and
    ExtensionApiFrameIdMap::LookupFrameDataOnUI we don't check whether a
    RenderFrameHost is alive, before storing it's frame data. It's possible that
    such a RenderFrameHost never becomes alive, and hence we don't get any
    OnRenderFrameDeleted notification for it. Since ExtensionApiFrameIdMap is a
    singleton, this causes a memory leak. To fix, ensure that only live
    RenderFrameHosts are tracked.
    
    Also ensure in InProcessBrowserTest::TearDown that ExtensionApiFrameIdMap is not
    leaking memory for any browser test.
    
    BUG=817205
    
    Change-Id: I78bf06cee7e10465f9eb5973ee8e59f525a74d93
    Reviewed-on: https://chromium-review.googlesource.com/951975
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#541950}
    Former-commit-id: 613ba3311438407d2356193b8881759f0125dced

--- chrome/test/base/in_process_browser_test.cc
+++ chrome/test/base/in_process_browser_test.cc
@@ -60,6 +60,7 @@
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_launcher.h"
 #include "content/public/test/test_navigation_observer.h"
+#include "extensions/features/features.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "ui/display/display_switches.h"
 
@@ -89,6 +90,10 @@
 
 #if !defined(OS_CHROMEOS) && defined(OS_LINUX)
 #include "ui/views/test/test_desktop_screen_x11.h"
+#endif
+
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+#include "extensions/browser/extension_api_frame_id_map.h"
 #endif
 
 namespace {
@@ -286,6 +291,14 @@ void InProcessBrowserTest::TearDown() {
   BrowserTestBase::TearDown();
   OSCryptMocker::TearDown();
   ChromeContentBrowserClient::SetDefaultQuotaSettingsForTesting(nullptr);
+
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+  // By now, all the WebContents should be destroyed, Ensure that we are not
+  // leaking memory in ExtensionAPIFrameIdMap. crbug.com/817205.
+  EXPECT_EQ(
+      0u,
+      extensions::ExtensionApiFrameIdMap::Get()->GetFrameDataCountForTesting());
+#endif
 }
 
 void InProcessBrowserTest::CloseBrowserSynchronously(Browser* browser) {
