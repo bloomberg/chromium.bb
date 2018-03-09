f5ba3a36c69a284412b67af19b31747af074c59a
Antoine Labour <piman@chromium.org>
2018-03-09 05:25:10 +0000

    Fix TextureDefinition when texture level 0 is not defined
    Before this patch, TextureDefinition considers itself "defined" if any image is
    defined on the source Texture. However it could be the case that the level 0
    (which is the one we deal with in MailboxManagerSync) might not be defined (but
    others are). Make sure that this is handled correctly, by considering
    TextureDefinition to be "defined" iff level 0 is defined
    
    Bug: 814677
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Id14f00d0c6ac36385d378a42df7b2119adbd0879
    Reviewed-on: https://chromium-review.googlesource.com/956748
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542035}
    Former-commit-id: c6a51e602deb00511d2615ac62553a05e95c0da2

--- gpu/command_buffer/service/mailbox_manager_unittest.cc
+++ gpu/command_buffer/service/mailbox_manager_unittest.cc
@@ -727,6 +727,84 @@ TEST_F(MailboxManagerSyncTest, ProduceTextureNotDefined) {
   DestroyTexture(new_texture);
 }
 
+TEST_F(MailboxManagerSyncTest, ProduceTextureDefinedNotLevel0) {
+  const GLuint kNewTextureId = 1234;
+  InSequence sequence;
+
+  Texture* texture = CreateTexture();
+  const GLsizei levels_needed = TextureManager::ComputeMipMapCount(
+      GL_TEXTURE_2D, kMaxTextureWidth, kMaxTextureHeight, kMaxTextureDepth);
+  SetTarget(texture, GL_TEXTURE_2D, levels_needed);
+  SetLevelInfo(texture, GL_TEXTURE_2D, 1, GL_RGBA, 1, 1, 1, 0, GL_RGBA,
+               GL_UNSIGNED_BYTE, gfx::Rect(1, 1));
+  SetParameter(texture, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  SetParameter(texture, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  Mailbox name = Mailbox::Generate();
+
+  manager_->ProduceTexture(name, texture);
+
+  // Share
+  manager_->PushTextureUpdates(g_sync_token);
+  EXPECT_CALL(*gl_, GenTextures(1, _))
+      .WillOnce(SetArgPointee<1>(kNewTextureId));
+  SetupUpdateTexParamExpectations(kNewTextureId, GL_LINEAR, GL_LINEAR,
+                                  GL_REPEAT, GL_REPEAT);
+  TextureBase* new_texture = manager2_->ConsumeTexture(name);
+  EXPECT_EQ(kNewTextureId, new_texture->service_id());
+
+  // Change something so that the push recreates the TextureDefinition.
+  SetParameter(static_cast<Texture*>(new_texture), GL_TEXTURE_MIN_FILTER,
+               GL_NEAREST);
+
+  // Synchronize manager2 -> manager
+  manager2_->PushTextureUpdates(g_sync_token);
+  SetupUpdateTexParamExpectations(1, GL_NEAREST, GL_LINEAR, GL_REPEAT,
+                                  GL_REPEAT);
+  manager_->PullTextureUpdates(g_sync_token);
+
+  DestroyTexture(texture);
+  DestroyTexture(new_texture);
+}
+
+TEST_F(MailboxManagerSyncTest, ProduceTextureDefined0Size) {
+  const GLuint kNewTextureId = 1234;
+  InSequence sequence;
+
+  Texture* texture = CreateTexture();
+  const GLsizei levels_needed = TextureManager::ComputeMipMapCount(
+      GL_TEXTURE_2D, kMaxTextureWidth, kMaxTextureHeight, kMaxTextureDepth);
+  SetTarget(texture, GL_TEXTURE_2D, levels_needed);
+  SetLevelInfo(texture, GL_TEXTURE_2D, 0, GL_RGBA, 0, 0, 0, 0, GL_RGBA,
+               GL_UNSIGNED_BYTE, gfx::Rect(0, 0));
+  SetParameter(texture, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  SetParameter(texture, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  Mailbox name = Mailbox::Generate();
+
+  manager_->ProduceTexture(name, texture);
+
+  // Share
+  manager_->PushTextureUpdates(g_sync_token);
+  EXPECT_CALL(*gl_, GenTextures(1, _))
+      .WillOnce(SetArgPointee<1>(kNewTextureId));
+  SetupUpdateTexParamExpectations(kNewTextureId, GL_LINEAR, GL_LINEAR,
+                                  GL_REPEAT, GL_REPEAT);
+  TextureBase* new_texture = manager2_->ConsumeTexture(name);
+  EXPECT_EQ(kNewTextureId, new_texture->service_id());
+
+  // Change something so that the push recreates the TextureDefinition.
+  SetParameter(static_cast<Texture*>(new_texture), GL_TEXTURE_MIN_FILTER,
+               GL_NEAREST);
+
+  // Synchronize manager2 -> manager
+  manager2_->PushTextureUpdates(g_sync_token);
+  SetupUpdateTexParamExpectations(1, GL_NEAREST, GL_LINEAR, GL_REPEAT,
+                                  GL_REPEAT);
+  manager_->PullTextureUpdates(g_sync_token);
+
+  DestroyTexture(texture);
+  DestroyTexture(new_texture);
+}
+
 TEST_F(MailboxManagerSyncTest, ProduceTextureNotBound) {
   InSequence sequence;
 
