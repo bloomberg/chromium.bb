dd975ca9678402b00c41c48b88db2b4cf1892555
Egor Pasko <pasko@chromium.org>
2018-03-09 13:37:33 +0000

    startup benchmark: enable category: navigation
    This will make it easy to make a metric like request_start_time. I was
    going to make it based on netlog, but that appears more difficult (many
    events in netlog, etc). Also navigation events are generally useful for
    startup, and not very frequent.
    
    Bug: 760498
    Change-Id: I7b9e9bb7efa0d4f2db729dafbb2b56e8ca1ec0ed
    Reviewed-on: https://chromium-review.googlesource.com/955632
    Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542095}
    Former-commit-id: 03e607bbf36f2f4225ed20bdbe420d9bf17ab827

--- tools/perf/benchmarks/start_with_url.py
+++ tools/perf/benchmarks/start_with_url.py
@@ -91,8 +91,8 @@ class ExperimentalStartWithUrlCold(perf_benchmark.PerfBenchmark):
   def CreateCoreTimelineBasedMeasurementOptions(self):
     startup_category_filter = (
         chrome_trace_category_filter.ChromeTraceCategoryFilter(
-            filter_string=('loading,net,netlog,network,offline_pages,'
-                'startup,toplevel,Java,EarlyJava')))
+            filter_string=('navigation,loading,net,netlog,network,'
+                'offline_pages,startup,toplevel,Java,EarlyJava')))
     options = timeline_based_measurement.Options(
         overhead_level=startup_category_filter)
     options.config.enable_chrome_trace = True
