14051158beed09b47252ec8d68e69e573e9f3fd1
Matt Menke <mmenke@chromium.org>
2018-03-09 19:14:47 +0000

    NetworkService + iOS:  Use a NetworkContext to make URLLoaderFactories.
    This will let use remove the use of URLRequestContextGetters in the
    NetworkService, and also let us adjust URLLoaderFactory lifetime to
    so that it lives as long as it has a live URLLoader, so it can own
    shared objects.
    
    Bug: 803149
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo;master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I7028886f9df671530ee9863f80dc437adc20cdfb
    Reviewed-on: https://chromium-review.googlesource.com/952342
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542188}
    Former-commit-id: 1f2fb3734a11ef572cc24d471deee3b9a1b168e3

--- ios/web/public/browser_state.h
+++ ios/web/public/browser_state.h
@@ -8,6 +8,8 @@
 #include <memory>
 
 #include "base/supports_user_data.h"
+#include "services/network/public/mojom/network_service.mojom.h"
+#include "services/network/public/mojom/url_loader_factory.mojom.h"
 #include "services/service_manager/embedder/embedded_service_info.h"
 
 namespace base {
@@ -98,7 +100,7 @@ class BrowserState : public base::SupportsUserData {
                          const base::FilePath& path);
 
  private:
-  class URLLoaderFactory;
+  class NetworkContextOwner;
   friend class URLDataManagerIOS;
   friend class URLRequestChromeJob;
 
@@ -108,7 +110,12 @@ class BrowserState : public base::SupportsUserData {
   // Not intended for usage outside of //web.
   URLDataManagerIOSBackend* GetURLDataManagerIOSBackendOnIOThread();
 
-  std::unique_ptr<URLLoaderFactory> url_loader_factory_;
+  network::mojom::URLLoaderFactoryPtr url_loader_factory_;
+  network::mojom::NetworkContextPtr network_context_;
+
+  // Owns the network::NetworkContext that backs |url_loader_factory_|. Created
+  // on the UI thread, destroyed on the IO thread.
+  std::unique_ptr<NetworkContextOwner> network_context_owner_;
 
   // The URLDataManagerIOSBackend instance associated with this BrowserState.
   // Created and destroyed on the IO thread, and should be accessed only from
