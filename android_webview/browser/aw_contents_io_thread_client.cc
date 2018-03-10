7ee58da36c36bfe0cefa7ad2d6ca09686c7961e6
Nate Fischer <ntfschr@chromium.org>
2018-03-10 03:03:29 +0000

    AW: fix AwContentsIoThreadClient::FromID map
    This fixes a bug related to the implementation of
    AwContentsIoThreadClient::FromID(frame_tree_node_id). This function is
    implemented using a map to keep track of the AwContentsIoThreadClient
    for a particular FrameTreeNode.
    
    Before this change, we would remove a map entry for a frame_tree_node_id
    whenever we destroy an associated RenderFrameHost. This is incorrect in
    multiprocess mode however, because a FrameTreeNode may have multiple
    associated RenderFrameHosts in its lifetime (such as swapping
    RenderFrameHosts for cross-site navigations). When we swap, we would
    first overwrite the map entry for the new RenderFrameHost, then erase
    the map entry when we destroy the old RenderFrameHost.
    
    The issue manifests most visibly for Safe Browsing, because FromID()
    returns nullptr, and Safe Browsing logic defaults to disabling the
    feature. However, this issue is not specific to Safe Browsing, and this
    would bug would cause a bad state whenever we trigger multiple
    cross-site browser-initiated navigations.
    
    With this change, we keep track of live RenderFrameHosts for a given
    FrameTreeNode, and only remove an entry from the map if we've destroyed
    the last associated RenderFrameHost.
    
    Bug: 818025
    Test: manual - see bug for repro steps
    Change-Id: I2ae81fdb5338084cdf253582206144b4f45704b1
    Reviewed-on: https://chromium-review.googlesource.com/956411
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542334}
    Former-commit-id: 1715b8dc26a9b994de4fcb3487b0bb98f16fba2e

--- android_webview/browser/aw_contents_io_thread_client.cc
+++ android_webview/browser/aw_contents_io_thread_client.cc
@@ -14,6 +14,7 @@
 #include "base/android/jni_array.h"
 #include "base/android/jni_string.h"
 #include "base/android/jni_weak_ref.h"
+#include "base/containers/flat_set.h"
 #include "base/lazy_instance.h"
 #include "base/synchronization/lock.h"
 #include "content/public/browser/browser_thread.h"
@@ -58,10 +59,16 @@ IoThreadClientData::IoThreadClientData() : pending_association(false) {}
 typedef map<pair<int, int>, IoThreadClientData>
     RenderFrameHostToIoThreadClientType;
 
+typedef pair<base::flat_set<RenderFrameHost*>, IoThreadClientData>
+    HostsAndClientDataPair;
+
 // When browser side navigation is enabled, RenderFrameIDs do not have
 // valid render process host and render frame ids for frame navigations.
-// We need to identify these by using Frame Tree Node ids.
-typedef map<int, IoThreadClientData> FrameTreeNodeToIoThreadClientType;
+// We need to identify these by using FrameTreeNodeIds. Furthermore, we need
+// to keep track of which RenderFrameHosts are associated with each
+// FrameTreeNodeId, so we know when the last RenderFrameHost is deleted (and
+// therefore the FrameTreeNodeId should be removed).
+typedef map<int, HostsAndClientDataPair> FrameTreeNodeToIoThreadClientType;
 
 static pair<int, int> GetRenderFrameHostIdPair(RenderFrameHost* rfh) {
   return pair<int, int>(rfh->GetProcess()->GetID(), rfh->GetRoutingID());
@@ -73,14 +80,19 @@ class RfhToIoThreadClientMap {
   static RfhToIoThreadClientMap* GetInstance();
   void Set(pair<int, int> rfh_id, const IoThreadClientData& client);
   bool Get(pair<int, int> rfh_id, IoThreadClientData* client);
-  void Erase(pair<int, int> rfh_id);
 
-  void Set(int frame_tree_node_id, const IoThreadClientData& client);
   bool Get(int frame_tree_node_id, IoThreadClientData* client);
-  void Erase(int frame_tree_node_id);
+
+  // Prefer to call these when RenderFrameHost* is available, because they
+  // update both maps at the same time.
+  void Set(RenderFrameHost* rfh, const IoThreadClientData& client);
+  void Erase(RenderFrameHost* rfh);
 
  private:
   base::Lock map_lock_;
+  // We maintain two maps simultaneously so that we can always get the correct
+  // IoThreadClientData, even when only HostIdPair or FrameTreeNodeId is
+  // available.
   RenderFrameHostToIoThreadClientType rfh_to_io_thread_client_;
   FrameTreeNodeToIoThreadClientType frame_tree_node_to_io_thread_client_;
 };
@@ -116,17 +128,6 @@ bool RfhToIoThreadClientMap::Get(pair<int, int> rfh_id,
   return true;
 }
 
-void RfhToIoThreadClientMap::Erase(pair<int, int> rfh_id) {
-  base::AutoLock lock(map_lock_);
-  rfh_to_io_thread_client_.erase(rfh_id);
-}
-
-void RfhToIoThreadClientMap::Set(int frame_tree_node_id,
-                                 const IoThreadClientData& client) {
-  base::AutoLock lock(map_lock_);
-  frame_tree_node_to_io_thread_client_[frame_tree_node_id] = client;
-}
-
 bool RfhToIoThreadClientMap::Get(int frame_tree_node_id,
                                  IoThreadClientData* client) {
   base::AutoLock lock(map_lock_);
@@ -135,13 +136,47 @@ bool RfhToIoThreadClientMap::Get(int frame_tree_node_id,
   if (iterator == frame_tree_node_to_io_thread_client_.end())
     return false;
 
-  *client = iterator->second;
+  *client = iterator->second.second;
   return true;
 }
 
-void RfhToIoThreadClientMap::Erase(int frame_tree_node_id) {
+void RfhToIoThreadClientMap::Set(RenderFrameHost* rfh,
+                                 const IoThreadClientData& client) {
+  int frame_tree_node_id = rfh->GetFrameTreeNodeId();
+  pair<int, int> rfh_id = GetRenderFrameHostIdPair(rfh);
   base::AutoLock lock(map_lock_);
-  frame_tree_node_to_io_thread_client_.erase(frame_tree_node_id);
+
+  // If this FrameTreeNodeId already has an associated IoThreadClientData, add
+  // this RenderFrameHost to the hosts set (it's harmless to overwrite the
+  // IoThreadClientData). Otherwise, operator[] creates a new map entry and we
+  // add this RenderFrameHost to the hosts set and insert |client| in the pair.
+  HostsAndClientDataPair& current_entry =
+      frame_tree_node_to_io_thread_client_[frame_tree_node_id];
+  current_entry.second = client;
+  current_entry.first.insert(rfh);
+
+  // Always add the entry to the HostIdPair map, since entries are 1:1 with
+  // RenderFrameHosts.
+  rfh_to_io_thread_client_[rfh_id] = client;
+}
+
+void RfhToIoThreadClientMap::Erase(RenderFrameHost* rfh) {
+  int frame_tree_node_id = rfh->GetFrameTreeNodeId();
+  pair<int, int> rfh_id = GetRenderFrameHostIdPair(rfh);
+  base::AutoLock lock(map_lock_);
+  HostsAndClientDataPair& current_entry =
+      frame_tree_node_to_io_thread_client_[frame_tree_node_id];
+  size_t num_erased = current_entry.first.erase(rfh);
+  DCHECK(num_erased == 1);
+  // Only remove this entry from the FrameTreeNodeId map if there are no more
+  // live RenderFrameHosts.
+  if (current_entry.first.empty()) {
+    frame_tree_node_to_io_thread_client_.erase(frame_tree_node_id);
+  }
+
+  // Always safe to remove the entry from the HostIdPair map, since entries are
+  // 1:1 with RenderFrameHosts.
+  rfh_to_io_thread_client_.erase(rfh_id);
 }
 
 // ClientMapEntryUpdater ------------------------------------------------------
@@ -175,15 +210,11 @@ void ClientMapEntryUpdater::RenderFrameCreated(RenderFrameHost* rfh) {
   IoThreadClientData client_data;
   client_data.io_thread_client = jdelegate_;
   client_data.pending_association = false;
-  RfhToIoThreadClientMap::GetInstance()->Set(GetRenderFrameHostIdPair(rfh),
-                                             client_data);
-  RfhToIoThreadClientMap::GetInstance()->Set(rfh->GetFrameTreeNodeId(),
-                                             client_data);
+  RfhToIoThreadClientMap::GetInstance()->Set(rfh, client_data);
 }
 
 void ClientMapEntryUpdater::RenderFrameDeleted(RenderFrameHost* rfh) {
-  RfhToIoThreadClientMap::GetInstance()->Erase(GetRenderFrameHostIdPair(rfh));
-  RfhToIoThreadClientMap::GetInstance()->Erase(rfh->GetFrameTreeNodeId());
+  RfhToIoThreadClientMap::GetInstance()->Erase(rfh);
 }
 
 void ClientMapEntryUpdater::WebContentsDestroyed() {
