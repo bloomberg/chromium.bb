8064bfce70b6b553ff5010bd1f67f62e853010ec
Michael Thiessen <mthiesse@chromium.org>
2018-03-12 20:39:27 +0000

    Revert "Reland "VR: Use isInVrSession API to keep Chrome in VR until user has left VR""
    This reverts commit 7c10f7dff1f75dcf443edb1a2b57ac270eabbc6d [formerly 2702f2e5ea8cf5eeef26a42e23c4063cbc48bbef].
    
    Reason for revert: Making fixing DLAs on P too difficult. Will reland once P issues are fixed.
    
    Original change's description:
    > Reland "VR: Use isInVrSession API to keep Chrome in VR until user has left VR"
    > 
    > This reverts commit 7a15a86c4a1cd39b9a8ce6db905a1a10ecd502e9 [formerly 0e39bd6284ff5240ec730fc9e674f51b2e872021].
    > 
    > Original change's description:
    > > Revert "VR: Use isInVrSession API to keep Chrome in VR until user has left VR"
    > > 
    > > This reverts commit 995007e3c1c8e56d57982e881a76bb0394a62258 [formerly dc55fd14dc6a42a6c6cd0dadcb713c9309f23318].
    > > 
    > > Reason for revert: Suspected to have broken chrome_public_test_vr_apk on main waterfall
    > > https://ci.chromium.org/buildbot/chromium.android/Marshmallow%20Phone%20Tester%20%28rel%29/?limit=200
    > > first failing build contains this cl:
    > > https://ci.chromium.org/buildbot/chromium.android/Marshmallow%20Phone%20Tester%20%28rel%29/3088
    > > 
    > > Original change's description:
    > > > VR: Use isInVrSession API to keep Chrome in VR until user has left VR
    > > > 
    > > > This fixes the Daydream Dashboard issues so that if Chrome is simply
    > > > stopped/started in VR without any intents, Chrome stays in VR.
    > > > 
    > > > Also makes resuming Chrome from DD home way faster if Chrome was
    > > > already in VR.
    > > > 
    > > > Bug: 780887
    > > > Change-Id: Iab7f713bfc5b85972f908ea141e3b495297d7286
    > > > Reviewed-on: https://chromium-review.googlesource.com/944547
    > > > Reviewed-by: Yash Malik <ymalik@chromium.org>
    > > > Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    > > > Cr-Commit-Position: refs/heads/master@{#540403}
    > > 
    > > TBR=mthiesse@chromium.org,ymalik@chromium.org
    > > 
    > > Change-Id: I7ae67f5d23d816796cc7f567792e181900b3012a
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Bug: 780887
    > > Reviewed-on: https://chromium-review.googlesource.com/946709
    > > Reviewed-by: Benjamin Pastene <bpastene@chromium.org>
    > > Commit-Queue: Benjamin Pastene <bpastene@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#540588}
    > 
    > TBR=mthiesse@chromium.org,ymalik@chromium.org
    > 
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    > 
    > Bug: 780887
    > Change-Id: I690474b0dd7b8dff7e7a5b2dbd036b0f97eacb52
    > Reviewed-on: https://chromium-review.googlesource.com/949083
    > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#540950}
    
    TBR=mthiesse@chromium.org,ymalik@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 780887
    Change-Id: If259e45f972b4ccc24e39304ca2e1a683f295059
    Reviewed-on: https://chromium-review.googlesource.com/958020
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542586}
    Former-commit-id: f6287e44e08c19e5885fd2f7e5934a729c3adbdc

--- chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrDaydreamApi.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrDaydreamApi.java
@@ -78,12 +78,6 @@ public interface VrDaydreamApi {
     Intent setupVrIntent(Intent intent);
 
     /**
-     * Whether the user is currently in a VR session. Note that this should be treated as heuristic,
-     * and may at times be wrong.
-     */
-    boolean isInVrSession();
-
-    /**
      * Closes this DaydreamApi instance.
      */
     void close();
