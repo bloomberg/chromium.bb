2cb4b0a79f1e32e2164c864a2779ff2e2a84085e
Christopher Lam <calamity@chromium.org>
2018-03-12 05:59:30 +0000

    Enable experimental app banners by default on ChromeOS.
    This CL enabled experimental app banners on CrOS in preparation of
    enabling DesktopPWAWindowing by default.
    
    Bug: 707014
    Change-Id: Ia1c56612df17212e42130c639280a8c97de217be
    Reviewed-on: https://chromium-review.googlesource.com/956646
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542429}
    Former-commit-id: 88fbaf8dd90b76b2f193fbb3d21cec451505f3b5

--- chrome/browser/banners/app_banner_manager_browsertest.cc
+++ chrome/browser/banners/app_banner_manager_browsertest.cc
@@ -154,6 +154,7 @@ class AppBannerManagerTest : public AppBannerManager {
 class AppBannerManagerBrowserTest : public InProcessBrowserTest {
  public:
   void SetUpOnMainThread() override {
+    feature_list_.InitAndDisableFeature(features::kExperimentalAppBanners);
     AppBannerSettingsHelper::SetTotalEngagementToTrigger(10);
     SiteEngagementScore::SetParamValuesForTesting();
     ASSERT_TRUE(embedded_test_server()->Start());
@@ -313,6 +314,9 @@ class AppBannerManagerBrowserTest : public InProcessBrowserTest {
     EXPECT_EQ(expected_will_show, manager->banner_shown());
     EXPECT_EQ(expected_state, manager->state());
   }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
 };
 
 IN_PROC_BROWSER_TEST_F(AppBannerManagerBrowserTest, WebAppBannerCreated) {
