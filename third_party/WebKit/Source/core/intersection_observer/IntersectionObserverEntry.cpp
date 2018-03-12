382bf8eb16eb359c38f1f6abb2b4b193b5bff321
Stefan Zager <szager@chromium.org>
2018-03-12 19:01:15 +0000

    [IntersectionObserver] Don't snap bounding rects
    These rects should mirror the behavior of getBoundingClientRect,
    which doesn't snap.
    
    BUG=758587,737228
    
    Change-Id: If735df6bd24414dfbbd58dce01dc2a90f71df381
    Reviewed-on: https://chromium-review.googlesource.com/951640
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542548}
    Former-commit-id: f8bf3f03dd8f4b81cb36d21c8af07844de4e9ecb

--- third_party/WebKit/Source/core/intersection_observer/IntersectionObserverEntry.cpp
+++ third_party/WebKit/Source/core/intersection_observer/IntersectionObserverEntry.cpp
@@ -11,17 +11,18 @@ namespace blink {
 IntersectionObserverEntry::IntersectionObserverEntry(
     DOMHighResTimeStamp time,
     double intersection_ratio,
-    const IntRect& bounding_client_rect,
-    const IntRect* root_bounds,
-    const IntRect& intersection_rect,
+    const FloatRect& bounding_client_rect,
+    const FloatRect* root_bounds,
+    const FloatRect& intersection_rect,
     bool is_intersecting,
     Element* target)
     : time_(time),
       intersection_ratio_(intersection_ratio),
-      bounding_client_rect_(DOMRectReadOnly::FromIntRect(bounding_client_rect)),
-      root_bounds_(root_bounds ? DOMRectReadOnly::FromIntRect(*root_bounds)
+      bounding_client_rect_(
+          DOMRectReadOnly::FromFloatRect(bounding_client_rect)),
+      root_bounds_(root_bounds ? DOMRectReadOnly::FromFloatRect(*root_bounds)
                                : nullptr),
-      intersection_rect_(DOMRectReadOnly::FromIntRect(intersection_rect)),
+      intersection_rect_(DOMRectReadOnly::FromFloatRect(intersection_rect)),
       target_(target),
       is_intersecting_(is_intersecting)
 
