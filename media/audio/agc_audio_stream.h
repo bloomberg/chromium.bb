d873be5f826b52467a7b7626bb3b077ed26b931b
Max Morin <maxmorin@chromium.org>
2018-03-12 11:26:52 +0000

    Get rid of lock in AGCAudioStream.
    The lock shouldn't be very contended, but in this case the lock is easy
    to get rid of, so we probably should.
    
    Bug: None
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I53ac4bed5b4d97e5cf67e388f32fe8facaf6b170
    Reviewed-on: https://chromium-review.googlesource.com/955843
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Max Morin <maxmorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542460}
    Former-commit-id: e3f64bf6d9fb2ebe77a57149753616185307e041

--- media/audio/agc_audio_stream.h
+++ media/audio/agc_audio_stream.h
@@ -5,9 +5,10 @@
 #ifndef MEDIA_AUDIO_AGC_AUDIO_STREAM_H_
 #define MEDIA_AUDIO_AGC_AUDIO_STREAM_H_
 
+#include <atomic>
+
 #include "base/logging.h"
 #include "base/macros.h"
-#include "base/synchronization/lock.h"
 #include "base/threading/thread_checker.h"
 #include "base/timer/timer.h"
 #include "media/audio/audio_io.h"
@@ -59,9 +60,8 @@
 // as well to ensure that thread safety is maintained. It will also guarantee
 // that the periodic timer runs on the audio manager thread.
 // |normalized_volume_|, which is updated by QueryAndStoreNewMicrophoneVolume()
-// and read in GetAgcVolume(), is protected by a lock to ensure that it can
-// be accessed from any real-time audio thread that needs it to update the its
-// AGC volume.
+// and read in GetAgcVolume(), is atomic to ensure that it can be accessed from
+// any real-time audio thread that needs it to update the its AGC volume.
 
 namespace media {
 
@@ -128,8 +128,7 @@ class MEDIA_EXPORT AgcAudioStream : public AudioInterface {
   // Called at each capture callback on a real-time capture thread (platform
   // dependent).
   void GetAgcVolume(double* normalized_volume) {
-    base::AutoLock lock(lock_);
-    *normalized_volume = normalized_volume_;
+    *normalized_volume = normalized_volume_.load(std::memory_order_relaxed);
   }
 
   // Gets the current automatic gain control state.
@@ -170,8 +169,7 @@ class MEDIA_EXPORT AgcAudioStream : public AudioInterface {
     if (max_volume_ != 0.0) {
       double normalized_volume =
           static_cast<AudioInterface*>(this)->GetVolume() / max_volume_;
-      base::AutoLock auto_lock(lock_);
-      normalized_volume_ = normalized_volume;
+      normalized_volume_.store(normalized_volume, std::memory_order_relaxed);
     }
   }
 
@@ -190,12 +188,9 @@ class MEDIA_EXPORT AgcAudioStream : public AudioInterface {
   double max_volume_;
 
   // Contains last result of internal call to GetVolume(). We save resources
-  // by not querying the capture volume for each callback. Guarded by |lock_|.
-  // The range is normalized to [0.0, 1.0].
-  double normalized_volume_;
-
-  // Protects |normalized_volume_| .
-  base::Lock lock_;
+  // by not querying the capture volume for each callback. The range is
+  // normalized to [0.0, 1.0].
+  std::atomic<double> normalized_volume_;
 
   DISALLOW_COPY_AND_ASSIGN(AgcAudioStream<AudioInterface>);
 };
