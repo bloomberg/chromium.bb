04fc205d0dc59425f61209d26201540bbf16d332
Kyle Horimoto <khorimoto@google.com>
2018-03-13 03:25:45 +0000

    Refactor ui::WebDialogUI and add new class ui::MojoWebDialogUI.
    This CL breaks out the dialog-specific functionality of ui::WebDialogUI
    into a new class, ui::WebDialogUIBase. ui::WebDialogUI now derives from
    both WebDialogUIBase and content::WebUIController.
    
    The new ui::MojoWebDialogUI class integrates Mojo functionality into
    web dialogs via ui::MojoWebUIController. Classes deriving from
    ui::MojoWebDialogUI can produce dialogs that make Mojo calls in JS.
    
    Change-Id: Ie91ae935aba5d65f7bcde1c13dfd9ecdee28ffab
    Reviewed-on: https://chromium-review.googlesource.com/947048
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542715}
    Former-commit-id: 919b2520c8b87f153953b24555a204b21a0a5723

--- ui/web_dialogs/web_dialog_ui.h
+++ ui/web_dialogs/web_dialog_ui.h
@@ -15,6 +15,7 @@
 #include "content/public/browser/web_ui_controller.h"
 #include "ui/base/ui_base_types.h"
 #include "ui/web_dialogs/web_dialogs_export.h"
+#include "ui/webui/mojo_web_ui_controller.h"
 #include "url/gurl.h"
 
 namespace content {
@@ -24,6 +25,35 @@ class WebContents;
 namespace ui {
 
 class WebDialogDelegate;
+
+class WEB_DIALOGS_EXPORT WebDialogUIBase {
+ public:
+  // Sets the delegate on the WebContents.
+  static void SetDelegate(content::WebContents* web_contents,
+                          WebDialogDelegate* delegate);
+
+  WebDialogUIBase(content::WebUI* web_ui);
+
+  // Close the dialog, passing the specified arguments to the close handler.
+  void CloseDialog(const base::ListValue* args);
+
+ protected:
+  virtual ~WebDialogUIBase();
+
+  // Prepares |render_frame_host| to host a dialog.
+  void HandleRenderFrameCreated(content::RenderFrameHost* render_frame_host);
+
+ private:
+  // Gets the delegate for the WebContent set with SetDelegate.
+  static WebDialogDelegate* GetDelegate(content::WebContents* web_contents);
+
+  // JS message handler.
+  void OnDialogClosed(const base::ListValue* args);
+
+  content::WebUI* web_ui_;
+
+  DISALLOW_COPY_AND_ASSIGN(WebDialogUIBase);
+};
 
 // Displays file URL contents inside a modal web dialog.
 //
@@ -36,42 +66,43 @@ class WebDialogDelegate;
 // the dialog to pass its delegate to the Web UI without having nasty accessors
 // on the WebContents. The correct design using RVH directly would avoid all of
 // this.
-class WEB_DIALOGS_EXPORT WebDialogUI : public content::WebUIController {
+class WEB_DIALOGS_EXPORT WebDialogUI : public WebDialogUIBase,
+                                       public content::WebUIController {
  public:
-  struct WebDialogParams {
-    // The URL for the content that will be loaded in the dialog.
-    GURL url;
-    // Width of the dialog.
-    int width;
-    // Height of the dialog.
-    int height;
-    // The JSON input to pass to the dialog when showing it.
-    std::string json_input;
-  };
-
   // When created, the delegate should already be set as user data on the
   // WebContents.
   explicit WebDialogUI(content::WebUI* web_ui);
   ~WebDialogUI() override;
 
-  // Close the dialog, passing the specified arguments to the close handler.
-  void CloseDialog(const base::ListValue* args);
-
-  // Sets the delegate on the WebContents.
-  static void SetDelegate(content::WebContents* web_contents,
-                          WebDialogDelegate* delegate);
-
  private:
-  // WebUIController
+  // content::WebUIController:
   void RenderFrameCreated(content::RenderFrameHost* render_frame_host) override;
 
-  // Gets the delegate for the WebContent set with SetDelegate.
-  static WebDialogDelegate* GetDelegate(content::WebContents* web_contents);
-
-  // JS message handler.
-  void OnDialogClosed(const base::ListValue* args);
-
   DISALLOW_COPY_AND_ASSIGN(WebDialogUI);
+};
+
+// Displays file URL contents inside a modal web dialog while also enabling
+// Mojo calls to be made from within the dialog.
+template <typename Interface>
+class WEB_DIALOGS_EXPORT MojoWebDialogUI
+    : public WebDialogUIBase,
+      public MojoWebUIController<Interface> {
+ public:
+  // When created, the delegate should already be set as user data on the
+  // WebContents.
+  explicit MojoWebDialogUI(content::WebUI* web_ui)
+      : WebDialogUIBase(web_ui), MojoWebUIController<Interface>(web_ui) {}
+  ~MojoWebDialogUI() override {}
+
+ private:
+  // content::WebUIController:
+  void RenderFrameCreated(
+      content::RenderFrameHost* render_frame_host) override {
+    MojoWebUIControllerBase::RenderFrameCreated(render_frame_host);
+    HandleRenderFrameCreated(render_frame_host);
+  }
+
+  DISALLOW_COPY_AND_ASSIGN(MojoWebDialogUI);
 };
 
 }  // namespace ui
