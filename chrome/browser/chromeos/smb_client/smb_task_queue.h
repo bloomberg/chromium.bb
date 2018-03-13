a50b0886471f8446cf0a8cfb0960c2cd01ce95e9
Bailey Berro <baileyberro@chromium.org>
2018-03-13 21:29:39 +0000

    Add abort functionality to SmbTaskQueue
    This change adds Abort functionality to SmbTaskQueue. The unrun tasks for
    a given operation can be aborted via SmbTaskQueue::AbortOperation().
    
    Test=unittest
    
    Bug: chromium:757625
    Change-Id: I317cc95556e4952a2c7049ad2cdf358d3058147b
    Reviewed-on: https://chromium-review.googlesource.com/956260
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542915}
    Former-commit-id: cbcb5ba6087e274152ada9f49d61fbef3563b225

--- chrome/browser/chromeos/smb_client/smb_task_queue.h
+++ chrome/browser/chromeos/smb_client/smb_task_queue.h
@@ -15,6 +15,7 @@ namespace smb_client {
 // An SmbTask is a call to SmbProviderClient with a bound SmbFileSystem callback
 // that runs when SmbProviderClient receives a D-Bus message response.
 using SmbTask = base::OnceClosure;
+using OperationId = uint32_t;
 
 // SmbTaskQueue handles the queuing of SmbTasks. Tasks are 'pending' while
 // SmbProviderClient awaits a D-Bus Message Response. Tasks are added to
@@ -24,8 +25,8 @@ using SmbTask = base::OnceClosure;
 //
 // Example:
 //
-//  void CreateDirectory(FilePath directory_path, bool recursive,
-//                       StatusCallback callback) {
+//  AbortCallback CreateDirectory(FilePath directory_path, bool recursive,
+//                                StatusCallback callback) {
 //    auto reply = base::BindOnce(&SmbFileSystem::HandleStatusCallback,
 //                                AsWeakPtr(), callback);
 //
@@ -35,7 +36,10 @@ using SmbTask = base::OnceClosure;
 //                                  directory_path,
 //                                  recursive,
 //                                  std::move(reply));
+//    OperationId operation_id = 1;
 //    tq_.AddTask(std::move(task));
+//
+//    return CreateAbortCallbackForOperationId(operation_id);
 //  }
 //
 //  void HandleStatusCallback(StatusCallback callback, ErrorType error) {
@@ -44,10 +48,20 @@ using SmbTask = base::OnceClosure;
 //  }
 class SmbTaskQueue {
  public:
-  // Adds |task| to the task queue. If fewer that max_pending_ tasks are
-  // outstanding, |task| will run immediately. Otherwise, it will be added to
-  // operations_ and run in FIFO order.
-  void AddTask(SmbTask task);
+  // Provides the caller with a new OperationId to associate new tasks with.
+  OperationId GetNextOperationId();
+
+  // Adds the sub-task |task| for the Operation |operation_id| to the task
+  // queue. If fewer that max_pending_ tasks are outstanding, |task| will run
+  // immediately. Otherwise, it will be added to operations_ and run in FIFO
+  // order.
+  void AddTask(SmbTask task, OperationId operation_id);
+
+  // Attempts to abort any outstanding tasks associated with the operation
+  // |operation_id|. Any subtasks that have not been sent over to D-Bus to the
+  // Smb Daemon will be cancelled, and OperationId will be removed from
+  // operation_map_.
+  void AbortOperation(OperationId operation_id);
 
   // Must be called by the owner of this class to indicate that a response to a
   // task was received.
@@ -57,6 +71,8 @@ class SmbTaskQueue {
   ~SmbTaskQueue();
 
  private:
+  using TaskList = base::queue<SmbTask>;
+
   // This runs the next task in operations_ if there is capacity to run an
   // additional task, and a task remaing to run.
   void RunTaskIfNeccessary();
@@ -67,16 +83,30 @@ class SmbTaskQueue {
   // Helper method that runs the next task.
   void RunNextTask();
 
+  // Prunes operations_ by removing OperationIds from the front of the queue if
+  // there are no tasks associated with them.
+  void PruneOperationQueue();
+
+  // Helper method that returns whether operations_ has been pruned.
+  bool IsPruned() const;
+
   // Helper method that returns whether there are tasks in operations_ to run.
+  // operations_ must be pruned (i.e. the top Operation in operations_ must have
+  // atleast 1 task associated with it).
   bool IsTaskToRun() const;
 
   // Helper method that returns whether there are fewer than max_pending tasks
   // outstanding.
   bool IsCapacityToRunTask() const;
 
+  // Helper method that returns whether |operation_id| is valid.
+  bool IsValidOperationId(OperationId operation_id) const;
+
   const size_t max_pending_;
   size_t num_pending_ = 0;
-  base::queue<SmbTask> operations_;
+  OperationId next_operation_id = 0;
+  base::queue<OperationId> operations_;
+  std::map<OperationId, TaskList> operation_map_;
 
   DISALLOW_COPY_AND_ASSIGN(SmbTaskQueue);
 };
