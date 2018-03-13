c4236c251a8f66d5d47edf367319aedb3df55723
David Tseng <dtseng@chromium.org>
2018-03-13 03:10:26 +0000

    Optimize computed bounds in AXTreeSourceArc
    This change takes the O(n^2) computation of a node's bounds down to O(n) by caching previously computed bounds.
    
    It also solves a crash which seemed to result from corruption of the incoming node data. This was observed while using NY Times with webviews containing many nodes and firing many events.
    
    Change-Id: I43c9295864fdcaf3775348eb17f55a4cf66da281
    Reviewed-on: https://chromium-review.googlesource.com/956413
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Yuki Awano <yawano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542708}
    Former-commit-id: d204c4ab1b2edace660ab2a34e913b1e2e9ef50e

--- chrome/browser/chromeos/arc/accessibility/ax_tree_source_arc.h
+++ chrome/browser/chromeos/arc/accessibility/ax_tree_source_arc.h
@@ -119,6 +119,8 @@ class AXTreeSourceArc
   const Delegate* const delegate_;
   std::unique_ptr<FocusStealer> focus_stealer_;
   std::string package_name_;
+  std::map<mojom::AccessibilityNodeInfoData*, gfx::Rect>
+      cached_computed_bounds_;
 
   DISALLOW_COPY_AND_ASSIGN(AXTreeSourceArc);
 };
