eff8dc0d030958b7763432911174a054c3ea6782
Christopher Lam <calamity@chromium.org>
2018-03-13 02:45:35 +0000

    Fix chrome/browser/resources PRESUBMIT.py
    The presubmit had the wrong file used as a test, and didn't trigger on
    unpack_pak.py changes.
    
    Bug: None
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ia7125e6b034261c29a2acdb067896f2ac9c9f216
    Reviewed-on: https://chromium-review.googlesource.com/956650
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542702}
    Former-commit-id: 125cd9f388fd6b6177909323b5618b09c464cc32

--- chrome/browser/resources/PRESUBMIT.py
+++ chrome/browser/resources/PRESUBMIT.py
@@ -8,6 +8,7 @@ See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details about the presubmit API built into depot_tools.
 """
 
+import os
 
 ACTION_XML_PATH = '../../../tools/metrics/actions/actions.xml'
 
@@ -96,7 +97,7 @@ def CheckHtml(input_api, output_api):
 
 def RunOptimizeWebUiTests(input_api, output_api):
   presubmit_path = input_api.PresubmitLocalPath()
-  sources = ['optimize_webui_test.py', 'unpack_pak.py']
+  sources = ['optimize_webui_test.py', 'unpack_pak_test.py']
   tests = [input_api.os_path.join(presubmit_path, s) for s in sources]
   return input_api.canned_checks.RunUnitTests(input_api, output_api, tests)
 
@@ -122,7 +123,10 @@ def _CheckChangeOnUploadOrCommit(input_api, output_api):
   affected = input_api.AffectedFiles()
   if any(f for f in affected if f.LocalPath().endswith('.html')):
     results += CheckHtml(input_api, output_api)
-  if any(f for f in affected if f.LocalPath().endswith('optimize_webui.py')):
+
+  webui_sources = set(['optimize_webui.py', 'unpack_pak.py'])
+  affected_filenames = set([os.path.basename(f.LocalPath()) for f in affected])
+  if webui_sources.intersection(affected_filenames):
     results += RunOptimizeWebUiTests(input_api, output_api)
   results += _CheckWebDevStyle(input_api, output_api)
   results += input_api.canned_checks.CheckPatchFormatted(input_api, output_api,
