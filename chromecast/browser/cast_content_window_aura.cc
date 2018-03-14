7ee8d99bec0d8e6f35659c978478cc07b9092eb4
Yuanyao Zhong <yyzhong@chromium.org>
2018-03-14 00:25:25 +0000

    Adding filtering for handling touch events in RenderWidgetHostViewAura.
    Bug: b/73285025
    Change-Id: I052cf918edb3cf15d3c6576c39adfbd6475ed93e
    Reviewed-on: https://chromium-review.googlesource.com/955956
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Yuanyao Zhong <yyzhong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#542963}
    Former-commit-id: e17503d8ea6ed87e16ca3d7f92c5189dab602cab

--- chromecast/browser/cast_content_window_aura.cc
+++ chromecast/browser/cast_content_window_aura.cc
@@ -14,16 +14,45 @@
 namespace chromecast {
 namespace shell {
 
+class TouchBlocker : public ui::EventHandler, public aura::WindowObserver {
+ public:
+  explicit TouchBlocker(aura::Window* window) : window_(window) {
+    DCHECK(window_);
+    window_->AddObserver(this);
+    window_->AddPreTargetHandler(this);
+  }
+
+  ~TouchBlocker() override {
+    if (window_) {
+      window_->RemoveObserver(this);
+      window_->RemovePreTargetHandler(this);
+    }
+  }
+
+ private:
+  // Overriden from ui::EventHandler.
+  void OnTouchEvent(ui::TouchEvent* touch) override { touch->SetHandled(); }
+
+  // Overriden from aura::WindowObserver.
+  void OnWindowDestroyed(aura::Window* window) override { window_ = nullptr; }
+
+  aura::Window* window_;
+
+  DISALLOW_COPY_AND_ASSIGN(TouchBlocker);
+};
+
 // static
 std::unique_ptr<CastContentWindow> CastContentWindow::Create(
-    CastContentWindow::Delegate* /* delegate */,
-    bool /* is_headless */,
-    bool /* enable_touch_input */) {
-  return base::WrapUnique(new CastContentWindowAura());
+    CastContentWindow::Delegate* delegate,
+    bool is_headless,
+    bool enable_touch_input) {
+  return base::WrapUnique(new CastContentWindowAura(enable_touch_input));
 }
 
-CastContentWindowAura::CastContentWindowAura() = default;
 CastContentWindowAura::~CastContentWindowAura() = default;
+
+CastContentWindowAura::CastContentWindowAura(bool is_touch_enabled)
+    : is_touch_enabled_(is_touch_enabled), touch_blocker_() {}
 
 void CastContentWindowAura::CreateWindowForWebContents(
     content::WebContents* web_contents,
@@ -34,6 +63,11 @@ void CastContentWindowAura::CreateWindowForWebContents(
   gfx::NativeView window = web_contents->GetNativeView();
   window_manager->SetWindowId(window, CastWindowManager::APP);
   window_manager->AddWindow(window);
+
+  if (!is_touch_enabled_) {
+    touch_blocker_ = std::make_unique<TouchBlocker>(window);
+  }
+
   if (is_visible) {
     window->Show();
   } else {
