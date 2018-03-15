bc59a6e598561cc51d037850a09f6ce105678db5
Ulan Degenbaev <ulan@chromium.org>
2018-03-15 16:10:22 +0000

    [bindings] Show detached DOM nodes in DevTools heap snapshot.
    The patch uses Node::isConnected() and Document::IsContextDestroyed()
    predicates to compute whether a Node is detached or not. This information
    is then propagated to all ScriptWrappables.
    
    DevTools shows detached nodes with the "Detached" prefix. For example,
    "Detached HTMLDocument" or "Detached Window".
    
    Bug: 811925
    Change-Id: Ib7987f578cc513b6bdfcfe084c885cbff84a4dba
    Reviewed-on: https://chromium-review.googlesource.com/961401
    Commit-Queue: Ulan Degenbaev <ulan@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543382}
    Former-commit-id: ee7b1d65ba78fffa1239da99ae8a035186f59510

--- third_party/WebKit/Source/bindings/core/v8/V8EmbedderGraphBuilder.h
+++ third_party/WebKit/Source/bindings/core/v8/V8EmbedderGraphBuilder.h
@@ -34,24 +34,60 @@ class V8EmbedderGraphBuilder : public ScriptWrappableVisitor,
              const ScriptWrappable*) const final;
 
  private:
+  // Information about whether a node is attached to the main DOM tree
+  // or not. It is computed as follows:
+  // 1) A Document with IsContextDestroyed() = true is detached.
+  // 2) A Document with IsContextDestroyed() = false is attached.
+  // 3) A Node that is not connected to any Document is detached.
+  // 4) A Node that is connected to a detached Document is detached.
+  // 5) A Node that is connected to an attached Document is attached.
+  // 6) A ScriptWrappable that is reachable from an attached Node is
+  //    attached.
+  // 7) A ScriptWrappable that is reachable from a detached Node is
+  //    detached.
+  // 8) A ScriptWrappable that is not reachable from any Node is
+  //    considered (conservatively) as attached.
+  // The unknown state applies to ScriptWrappables during graph
+  // traversal when we don't have reachability information yet.
+  enum class DomTreeState { kAttached, kDetached, kUnknown };
+
+  DomTreeState DomTreeStateFromWrapper(uint16_t class_id,
+                                       v8::Local<v8::Object> v8_value);
+
   class EmbedderNode : public Graph::Node {
    public:
-    EmbedderNode(const char* name, Graph::Node* wrapper)
-        : name_(name), wrapper_(wrapper) {}
+    EmbedderNode(const char* name,
+                 Graph::Node* wrapper,
+                 DomTreeState dom_tree_state)
+        : name_(name), wrapper_(wrapper), dom_tree_state_(dom_tree_state) {}
 
+    DomTreeState GetDomTreeState() { return dom_tree_state_; }
+    void UpdateDomTreeState(DomTreeState parent_dom_tree_state) {
+      // If the child's state is unknown, then take the parent's state.
+      // If the parent is attached, then the child is also attached.
+      if (dom_tree_state_ == DomTreeState::kUnknown ||
+          parent_dom_tree_state == DomTreeState::kAttached) {
+        dom_tree_state_ = parent_dom_tree_state;
+      }
+    }
     // Graph::Node overrides.
     const char* Name() override { return name_; }
+    const char* NamePrefix() override {
+      return dom_tree_state_ == DomTreeState::kDetached ? "Detached" : nullptr;
+    }
     size_t SizeInBytes() override { return 0; }
     Graph::Node* WrapperNode() override { return wrapper_; }
 
    private:
     const char* name_;
     Graph::Node* wrapper_;
+    DomTreeState dom_tree_state_;
   };
 
   class EmbedderRootNode : public EmbedderNode {
    public:
-    explicit EmbedderRootNode(const char* name) : EmbedderNode(name, nullptr) {}
+    explicit EmbedderRootNode(const char* name)
+        : EmbedderNode(name, nullptr, DomTreeState::kUnknown) {}
     // Graph::Node override.
     bool IsRootNode() { return true; }
   };
@@ -60,7 +96,7 @@ class V8EmbedderGraphBuilder : public ScriptWrappableVisitor,
     STACK_ALLOCATED();
 
    public:
-    ParentScope(V8EmbedderGraphBuilder* visitor, Graph::Node* parent)
+    ParentScope(V8EmbedderGraphBuilder* visitor, EmbedderNode* parent)
         : visitor_(visitor) {
       DCHECK_EQ(visitor->current_parent_, nullptr);
       visitor->current_parent_ = parent;
@@ -72,28 +108,40 @@ class V8EmbedderGraphBuilder : public ScriptWrappableVisitor,
   };
 
   struct WorklistItem {
-    Graph::Node* node;
+    EmbedderNode* node;
     Traceable traceable;
     TraceWrappersCallback trace_wrappers_callback;
   };
 
-  WorklistItem ToWorklistItem(Graph::Node*,
+  WorklistItem ToWorklistItem(EmbedderNode*,
                               const TraceWrapperDescriptor&) const;
 
   Graph::Node* GraphNode(const v8::Local<v8::Value>&) const;
-  Graph::Node* GraphNode(Traceable,
-                         const char* name,
-                         Graph::Node* wrapper) const;
+  EmbedderNode* GraphNode(Traceable,
+                          const char* name,
+                          Graph::Node* wrapper,
+                          DomTreeState) const;
 
   void VisitPendingActivities();
   void VisitTransitiveClosure();
 
+  // Push the item to the default worklist if item.traceable was not
+  // already visited.
+  void PushToWorklist(WorklistItem) const;
+
   v8::Isolate* isolate_;
-  Graph::Node* current_parent_;
+  EmbedderNode* current_parent_;
   mutable Graph* graph_;
   mutable HashSet<Traceable> visited_;
-  mutable HashMap<Traceable, Graph::Node*> graph_node_;
+  mutable HashMap<Traceable, EmbedderNode*> graph_node_;
+  // The default worklist that is used to visit transitive closure.
   mutable Deque<WorklistItem> worklist_;
+  // The worklist that collects detached Nodes during persistent handle
+  // iteration.
+  mutable Deque<WorklistItem> detached_worklist_;
+  // The worklist that collects ScriptWrappables with unknown information
+  // about attached/detached state during persistent handle iteration.
+  mutable Deque<WorklistItem> unknown_worklist_;
 };
 
 }  // namespace blink
