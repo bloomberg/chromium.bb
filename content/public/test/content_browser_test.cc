436db187b34b9ed7e5c84a69bf60d28b45165dd3
Alexander Semashko <ahest@yandex-team.ru>
2018-03-15 09:02:56 +0000

    Ignore window [de]activation in content_browsertests.
    These events can happen when another test starts or finishes and cause
    frame/widget focus state changes, breaking some tests.
    
    Bug: 695054
    Change-Id: I9ab6b3887c00421d4c6f96dd508e912b23706176
    Reviewed-on: https://chromium-review.googlesource.com/926368
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Alexander Semashko <ahest@yandex-team.ru>
    Cr-Commit-Position: refs/heads/master@{#543328}
    Former-commit-id: 901d5088e10ab1149e7e51b94e70949fe20fe67d

--- content/public/test/content_browser_test.cc
+++ content/public/test/content_browser_test.cc
@@ -30,6 +30,10 @@
 #include "ui/base/ime/input_method_initializer.h"
 #endif
 
+#if defined(USE_AURA)
+#include "ui/views/widget/desktop_aura/desktop_native_widget_aura.h"  // nogncheck
+#endif
+
 namespace content {
 
 ContentBrowserTest::ContentBrowserTest() {
@@ -79,6 +83,13 @@ void ContentBrowserTest::SetUp() {
       "Frameworks/Content Shell Helper.app/Contents/MacOS/Content Shell Helper");
   command_line->AppendSwitchPath(switches::kBrowserSubprocessPath,
                                  subprocess_path);
+#endif
+
+#if defined(USE_AURA)
+  // https://crbug.com/695054: Ignore window activation/deactivation to make
+  // the Chrome-internal focus unaffected by OS events caused by running tests
+  // in parallel.
+  views::DesktopNativeWidgetAura::DisableActivationChangeHandlingForTests();
 #endif
 
   // LinuxInputMethodContextFactory has to be initialized.
