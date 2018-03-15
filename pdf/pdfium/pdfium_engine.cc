885f951104da0100d437e4353442218e08c1c52e
Henrique Nakashima <hnakashima@chromium.org>
2018-03-15 21:11:33 +0000

    Copy visible_pages_ when iterating over it.
    On this case, a call inside the loop may cause visible_pages_ to
    change.
    
    Bug: 822091
    Change-Id: I41b0715faa6fe3e39203cd9142cf5ea38e59aefb
    Reviewed-on: https://chromium-review.googlesource.com/964592
    Reviewed-by: dsinclair <dsinclair@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543494}
    Former-commit-id: 94b3728a2836da335a10085d4089c9d8e1c9d225

--- pdf/pdfium/pdfium_engine.cc
+++ pdf/pdfium/pdfium_engine.cc
@@ -3742,7 +3742,10 @@ void PDFiumEngine::DeviceToPage(int page_index,
 }
 
 int PDFiumEngine::GetVisiblePageIndex(FPDF_PAGE page) {
-  for (int page_index : visible_pages_) {
+  // Copy visible_pages_ since it can change as a result of loading the page in
+  // GetPage(). See https://crbug.com/822091.
+  std::vector<int> visible_pages_copy(visible_pages_);
+  for (int page_index : visible_pages_copy) {
     if (pages_[page_index]->GetPage() == page)
       return page_index;
   }
