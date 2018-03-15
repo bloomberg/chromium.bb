c64567ec97d2d1f0d38486a6861d9e8f8b172d5c
Cathy Li <chili@chromium.org>
2018-03-15 18:33:00 +0000

    [Offline pages]: DeletePagesByClientIdAndOrigin API should take origin instead of string
    Bug: 812022
    Change-Id: I8cb1c8e9566bf4fb92f67ef5b8c9e434467e647b
    Reviewed-on: https://chromium-review.googlesource.com/944620
    Commit-Queue: Cathy Li <chili@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543455}
    Former-commit-id: 96ddff8eeb2b8c3176d8ba866e19be65bef2baad

--- chrome/android/junit/src/org/chromium/chrome/browser/offlinepages/OfflinePageOriginUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/offlinepages/OfflinePageOriginUnitTest.java
@@ -5,6 +5,8 @@
 package org.chromium.chrome.browser.offlinepages;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,5 +25,26 @@ public class OfflinePageOriginUnitTest {
         OfflinePageOrigin origin = new OfflinePageOrigin(appName, signatures);
 
         assertEquals("[\"abc.xyz\",[\"deadbeef\",\"00c0ffee\"]]", origin.encodeAsJsonString());
+    }
+
+    @Test
+    public void testEquals() {
+        String appName = "abc.xyz";
+        String[] signature1 = new String[] {"deadbeef", "00c0ffee"};
+        String[] signature2 = new String[] {"deadbeef", "fooba499"};
+        OfflinePageOrigin origin1 = new OfflinePageOrigin(appName, signature1);
+        OfflinePageOrigin origin2 = new OfflinePageOrigin(appName, signature2);
+        OfflinePageOrigin origin3 = new OfflinePageOrigin("", signature1);
+        OfflinePageOrigin origin4 = new OfflinePageOrigin(appName, signature1);
+
+        assertFalse("Equivalent to null", origin1.equals(null));
+        assertTrue("Not equivalent to self", origin1.equals(origin1));
+        assertFalse("Equivalent when signatures not equal", origin1.equals(origin2));
+        assertFalse("Equivalent when name not equal", origin1.equals(origin3));
+        assertTrue("Equally created items not equal", origin1.equals(origin4));
+
+        assertEquals("HashCode not equal to self", origin1.hashCode(), origin1.hashCode());
+        assertEquals(
+                "HashCode not equal when items are equal", origin1.hashCode(), origin4.hashCode());
     }
 }
