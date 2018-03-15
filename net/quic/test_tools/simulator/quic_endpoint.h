90d6df9974320d4be0e5b555f238f5cb2e005eb2
Yixin Wang <wangyix@chromium.org>
2018-03-15 20:26:36 +0000

    Reland "Add OnForwardProgressConfirmed() for QuicConnectionVisitorInterface and call it whenever an ACK increases the largest_acked value."
    Original CL: https://chromium-review.googlesource.com/c/chromium/src/+/961385
    Revert CL: https://chromium-review.googlesource.com/c/chromium/src/+/963621
    
    
    Original description:
    
    The purpose of this is to have Chromium's QuicChromiumClientSession pass the MSG_CONFIRM flag to the next UDP sendto() call when OnForwardProgressConfirmed() is called for Linux platforms. MSG_CONFIRM is used to signal the Linux kernel of "forward progress" on that path to prevent the kernel from doing unnecessary probing.
    More info: https://bugs.chromium.org/p/chromium/issues/detail?id=488402
    
    Merge internal change 188887862
    
    Bug: 488402
    Change-Id: I257cc8b302c6331714a5b351f026bd25ee5e5664
    Reviewed-on: https://chromium-review.googlesource.com/963681
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Yixin Wang <wangyix@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543480}
    Former-commit-id: f14f94de10ce40b770fbfbd26e77a681793e6c4e

--- net/quic/test_tools/simulator/quic_endpoint.h
+++ net/quic/test_tools/simulator/quic_endpoint.h
@@ -98,6 +98,7 @@ class QuicEndpoint : public Endpoint,
   void OnAckNeedsRetransmittableFrame() override {}
   void SendPing() override {}
   bool AllowSelfAddressChange() const override;
+  void OnForwardProgressConfirmed() override {}
   // End QuicConnectionVisitorInterface implementation.
 
   // Begin SessionNotifierInterface methods:
