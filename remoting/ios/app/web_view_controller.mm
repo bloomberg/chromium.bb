1e93c89cd12653ffe8618d718193f0de5ab86410
Yuwei Huang <yuweih@chromium.org>
2018-03-15 00:02:02 +0000

    [CRD iOS] Allow WebViewController to open webpage with target="_blank"
    Previously tapping a link on the help page has no effect because the
    links have target="_blank" attribute and we haven't properly set up the
    WebViewController to handle that.
    
    This CL makes the app open a target="_blank" link using the default
    browser, aka Safari.
    
    Bug: 821955
    Change-Id: I1557a37d9b1a341186ee1b59fec1de77f075da1a
    Reviewed-on: https://chromium-review.googlesource.com/963690
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543245}
    Former-commit-id: 7b69eacc0d62de928264252c29daa3883fb386ca

--- remoting/ios/app/web_view_controller.mm
+++ remoting/ios/app/web_view_controller.mm
@@ -13,7 +13,7 @@
 #import "remoting/ios/app/remoting_theme.h"
 #import "remoting/ios/app/view_utils.h"
 
-@interface WebViewController () {
+@interface WebViewController ()<WKUIDelegate> {
   NSString* _urlString;
 }
 @end
@@ -37,6 +37,7 @@
   NSURLRequest* request =
       [NSURLRequest requestWithURL:[NSURL URLWithString:_urlString]];
   [webView loadRequest:request];
+  webView.UIDelegate = self;
   self.view = webView;
 }
 
@@ -58,6 +59,21 @@
     remoting::SetAccessibilityInfoFromImage(
         self.navigationItem.leftBarButtonItem);
   }
+}
+
+#pragma mark - WKUIDelegate
+
+- (WKWebView*)webView:(WKWebView*)webView
+    createWebViewWithConfiguration:(WKWebViewConfiguration*)configuration
+               forNavigationAction:(WKNavigationAction*)navigationAction
+                    windowFeatures:(WKWindowFeatures*)windowFeatures {
+  // This is called when the web view needs to open a webpage in new window,
+  // i.e. target="_blank".
+  [UIApplication.sharedApplication openURL:navigationAction.request.URL
+                                   options:@{}
+                         completionHandler:nil];
+
+  return nil;
 }
 
 #pragma mark - Private
