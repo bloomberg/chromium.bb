8d3361e5508fa5d4c1a3166e797ea58ac3a4410e
Daniel Bratell <bratell@opera.com>
2018-03-15 16:30:17 +0000

    Solving jumbo build offline_pages clash with base::DoNothing()
    Too many SavePageLaterCallback functions caused problems in
    jumbo builds. Since two of the three were just empty functions
    this patch replaces them with base::DoNothing()
    
    Change-Id: Ie9c939ee10674cdab77aab0a9ade65d70293fb0c
    Reviewed-on: https://chromium-review.googlesource.com/964441
    Reviewed-by: Cathy Li <chili@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#543397}
    Former-commit-id: 3ebe17ecae8c66673a7f501d5b135c84dc6e55ab

--- chrome/browser/offline_pages/android/downloads/offline_page_download_bridge.cc
+++ chrome/browser/offline_pages/android/downloads/offline_page_download_bridge.cc
@@ -8,6 +8,7 @@
 
 #include "base/android/jni_string.h"
 #include "base/bind.h"
+#include "base/bind_helpers.h"
 #include "base/guid.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
@@ -107,10 +108,6 @@ content::WebContents* GetWebContentsFromJavaTab(
   return tab->web_contents();
 }
 
-void SavePageLaterCallback(AddRequestResult result) {
-  // do nothing.
-}
-
 void SavePageIfNotNavigatedAway(const GURL& url,
                                 const GURL& original_url,
                                 const ScopedJavaGlobalRef<jobject>& j_tab_ref,
@@ -147,8 +144,8 @@ void SavePageIfNotNavigatedAway(const GURL& url,
           RequestCoordinator::RequestAvailability::DISABLED_FOR_OFFLINER;
       params.original_url = original_url;
       params.request_origin = origin;
-      request_id = request_coordinator->SavePageLater(
-          params, base::Bind(&SavePageLaterCallback));
+      request_id =
+          request_coordinator->SavePageLater(params, base::DoNothing());
     } else {
       DVLOG(1) << "SavePageIfNotNavigatedAway has no valid coordinator.";
     }
