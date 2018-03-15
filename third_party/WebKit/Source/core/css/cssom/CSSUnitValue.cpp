1428f95ceca7a0759477c50429ba91386ec799b5
Darren Shen <shend@chromium.org>
2018-03-15 04:54:06 +0000

    [css-typed-om] Add support for flex properties.
    This patch adds support for flex properties. Failing test because Blink
    doesn't support 'content' for flex-basis yet.
    
    Bug: 820299
    Change-Id: Ie2df3fcaecda16d52ded1f90aab9f1df82e9e283
    Reviewed-on: https://chromium-review.googlesource.com/963865
    Reviewed-by: nainar <nainar@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543305}
    Former-commit-id: 5bd654a757956bcc0e916734340c8001701d6e37

--- third_party/WebKit/Source/core/css/cssom/CSSUnitValue.cpp
+++ third_party/WebKit/Source/core/css/cssom/CSSUnitValue.cpp
@@ -44,6 +44,8 @@ bool IsValueOutOfRangeForProperty(CSSPropertyID property_id, double value) {
 
   // For non-length properties and special cases.
   switch (property_id) {
+    case CSSPropertyFlexGrow:
+    case CSSPropertyFlexShrink:
     case CSSPropertyFontSize:
       return value < 0;
     case CSSPropertyFontWeight:
