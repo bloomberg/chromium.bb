8336229c9b1bf41f98a653f095295a18b19146da
Rakina Zata Amni <rakina@chromium.org>
2018-03-16 04:12:04 +0000

    Refactor Find-in-page methods to WebLocalFrameImplFindInPage.cpp
    We are refactoring find-in-page related methods out of
    WebLocalFrameImpl.cpp into WebLocalFrameImplFindInPage.cpp
    
    WebLocalFrameImplFindInPage.cpp implements WebLocalFrameImpl.h,
    but only contains the find-in-page related functions for
    easier maintanability.
    
    Refactor plan doc: https://docs.google.com/document/d/1JQWkQbTpYuxNHseAPbOq4rwtS7eFUISymstxDolQ504/edit?usp=sharing
    
    Related:
    Find-in-page S13n/changing legacy IPC to Mojo:
    Design doc: https://goo.gl/JenyGm
    
    Find-in-page Web API to allow suppressing showing Find Bar:
    https://github.com/rakina/find-in-page-api
    
    
    Bug: 819919
    Change-Id: If8e25262245d3117e67b2a93c1bd1b527d457541
    Reviewed-on: https://chromium-review.googlesource.com/956648
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543617}
    Former-commit-id: 0570e6b00d7c938329b8cfd0b0c7c65b80ebcb5d

--- third_party/WebKit/Source/core/frame/WebLocalFrameImpl.cpp
+++ third_party/WebKit/Source/core/frame/WebLocalFrameImpl.cpp
@@ -227,7 +227,6 @@
 #include "public/web/WebConsoleMessage.h"
 #include "public/web/WebDOMEvent.h"
 #include "public/web/WebDocument.h"
-#include "public/web/WebFindOptions.h"
 #include "public/web/WebFormElement.h"
 #include "public/web/WebFrameClient.h"
 #include "public/web/WebFrameOwnerProperties.h"
@@ -2281,109 +2280,6 @@ void WebLocalFrameImpl::DidCallIsSearchProviderInstalled() {
   UseCounter::Count(GetFrame(), WebFeature::kExternalIsSearchProviderInstalled);
 }
 
-void WebLocalFrameImpl::RequestFind(int identifier,
-                                    const WebString& search_text,
-                                    const WebFindOptions& options) {
-  // Send "no results" if this frame has no visible content.
-  if (!HasVisibleContent() && !options.force) {
-    Client()->ReportFindInPageMatchCount(identifier, 0 /* count */,
-                                         true /* finalUpdate */);
-    return;
-  }
-
-  WebRange current_selection = SelectionRange();
-  bool result = false;
-  bool active_now = false;
-
-  // Search for an active match only if this frame is focused or if this is a
-  // find next request.
-  if (IsFocused() || options.find_next) {
-    result = Find(identifier, search_text, options, false /* wrapWithinFrame */,
-                  &active_now);
-  }
-
-  if (result && !options.find_next) {
-    // Indicate that at least one match has been found. 1 here means
-    // possibly more matches could be coming.
-    Client()->ReportFindInPageMatchCount(identifier, 1 /* count */,
-                                         false /* finalUpdate */);
-  }
-
-  // There are three cases in which scoping is needed:
-  //
-  // (1) This is an initial find request (|options.findNext| is false). This
-  // will be the first scoping effort for this find session.
-  //
-  // (2) Something has been selected since the last search. This means that we
-  // cannot just increment the current match ordinal; we need to re-generate
-  // it.
-  //
-  // (3) TextFinder::Find() found what should be the next match (|result| is
-  // true), but was unable to activate it (|activeNow| is false). This means
-  // that the text containing this match was dynamically added since the last
-  // scope of the frame. The frame needs to be re-scoped so that any matches
-  // in the new text can be highlighted and included in the reported number of
-  // matches.
-  //
-  // If none of these cases are true, then we just report the current match
-  // count without scoping.
-  if (/* (1) */ options.find_next && /* (2) */ current_selection.IsNull() &&
-      /* (3) */ !(result && !active_now)) {
-    // Force report of the actual count.
-    IncreaseMatchCount(0, identifier);
-    return;
-  }
-
-  // Start a new scoping request. If the scoping function determines that it
-  // needs to scope, it will defer until later.
-  EnsureTextFinder().StartScopingStringMatches(identifier, search_text,
-                                               options);
-}
-
-bool WebLocalFrameImpl::Find(int identifier,
-                             const WebString& search_text,
-                             const WebFindOptions& options,
-                             bool wrap_within_frame,
-                             bool* active_now) {
-  if (!GetFrame())
-    return false;
-
-  // Unlikely, but just in case we try to find-in-page on a detached frame.
-  DCHECK(GetFrame()->GetPage());
-
-  // Up-to-date, clean tree is required for finding text in page, since it
-  // relies on TextIterator to look over the text.
-  GetFrame()->GetDocument()->UpdateStyleAndLayoutIgnorePendingStylesheets();
-
-  return EnsureTextFinder().Find(identifier, search_text, options,
-                                 wrap_within_frame, active_now);
-}
-
-void WebLocalFrameImpl::StopFinding(StopFindAction action) {
-  bool clear_selection = action == kStopFindActionClearSelection;
-  if (clear_selection)
-    ExecuteCommand(WebString::FromUTF8("Unselect"));
-
-  if (text_finder_) {
-    if (!clear_selection)
-      text_finder_->SetFindEndstateFocusAndSelection();
-    text_finder_->StopFindingAndClearSelection();
-  }
-
-  if (action == kStopFindActionActivateSelection && IsFocused()) {
-    WebDocument doc = GetDocument();
-    if (!doc.IsNull()) {
-      WebElement element = doc.FocusedElement();
-      if (!element.IsNull())
-        element.SimulateClick();
-    }
-  }
-}
-
-void WebLocalFrameImpl::IncreaseMatchCount(int count, int identifier) {
-  EnsureTextFinder().IncreaseMatchCount(identifier, count);
-}
-
 void WebLocalFrameImpl::DispatchMessageEventWithOriginCheck(
     const WebSecurityOrigin& intended_target_origin,
     const WebDOMEvent& event,
@@ -2405,43 +2301,6 @@ void WebLocalFrameImpl::DispatchMessageEventWithOriginCheck(
       SourceLocation::Create(String(), 0, 0, nullptr));
 }
 
-int WebLocalFrameImpl::FindMatchMarkersVersion() const {
-  if (text_finder_)
-    return text_finder_->FindMatchMarkersVersion();
-  return 0;
-}
-
-int WebLocalFrameImpl::SelectNearestFindMatch(const WebFloatPoint& point,
-                                              WebRect* selection_rect) {
-  return EnsureTextFinder().SelectNearestFindMatch(point, selection_rect);
-}
-
-float WebLocalFrameImpl::DistanceToNearestFindMatch(
-    const WebFloatPoint& point) {
-  float nearest_distance;
-  EnsureTextFinder().NearestFindMatch(point, &nearest_distance);
-  return nearest_distance;
-}
-
-WebFloatRect WebLocalFrameImpl::ActiveFindMatchRect() {
-  if (text_finder_)
-    return text_finder_->ActiveFindMatchRect();
-  return WebFloatRect();
-}
-
-void WebLocalFrameImpl::FindMatchRects(WebVector<WebFloatRect>& output_rects) {
-  EnsureTextFinder().FindMatchRects(output_rects);
-}
-
-void WebLocalFrameImpl::SetTickmarks(const WebVector<WebRect>& tickmarks) {
-  if (GetFrameView()) {
-    Vector<IntRect> tickmarks_converted(tickmarks.size());
-    for (size_t i = 0; i < tickmarks.size(); ++i)
-      tickmarks_converted[i] = tickmarks[i];
-    GetFrameView()->SetTickmarks(tickmarks_converted);
-  }
-}
-
 WebNode WebLocalFrameImpl::ContextMenuNode() const {
   return ContextMenuNodeInner();
 }
@@ -2461,17 +2320,6 @@ void WebLocalFrameImpl::WillDetachParent() {
 
     text_finder_->CancelPendingScopingEffort();
   }
-}
-
-TextFinder* WebLocalFrameImpl::GetTextFinder() const {
-  return text_finder_;
-}
-
-TextFinder& WebLocalFrameImpl::EnsureTextFinder() {
-  if (!text_finder_)
-    text_finder_ = TextFinder::Create(*this);
-
-  return *text_finder_;
 }
 
 void WebLocalFrameImpl::SetFrameWidget(WebFrameWidgetBase* frame_widget) {
