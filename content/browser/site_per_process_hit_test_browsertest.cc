0407e4e81f5d94a35c4117fcad1587b2045e3b72
Sahel Sharifymoghaddam <sahel@chromium.org>
2018-03-16 18:30:51 +0000

    Revert "Disable wheel scroll latching in failing content_browsertests."
    This reverts commit 1158f19aa89ebdf613a832d1dbe401319924bdc5 [formerly f2bf009a8ad55dd79d87c14a7cad6473ece890bc].
    
    Reason for revert: There has been a few bug fixes related to oopif and
    latching, I will enable the latching flag to see if the failing tests
    are fixed or not. If not I will disable the tests again addressing crbug.com/822764 bug as well.
    
    Original change's description:
    > Disable wheel scroll latching in failing content_browsertests.
    > 
    > I cannot reproduce the test failures on my local official windows build,
    > in this cl I force wheel scroll latching to be disabled in the tests to
    > see if it changes the test results on Win official or not.
    > 
    > Bug: 800822
    > Change-Id: Ife07a29617ca5e0142e75f926967685cb61e2a08
    > Reviewed-on: https://chromium-review.googlesource.com/895842
    > Reviewed-by: James MacLean <wjmaclean@chromium.org>
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Commit-Queue: Sahel Sharifymoghaddam <sahel@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#533347}
    
    TBR=nasko@chromium.org,wjmaclean@chromium.org,sahel@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 800822, 822764
    Change-Id: I563672dfbcb01298324a2b884cbed0e69c220339
    Reviewed-on: https://chromium-review.googlesource.com/966687
    Reviewed-by: Sahel Sharifymoghaddam <sahel@chromium.org>
    Commit-Queue: Sahel Sharifymoghaddam <sahel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543765}
    Former-commit-id: 431c6dbf0a42d0c31c7dccd6553c6c496f1042a0

--- content/browser/site_per_process_hit_test_browsertest.cc
+++ content/browser/site_per_process_hit_test_browsertest.cc
@@ -10,7 +10,6 @@
 #include "base/feature_list.h"
 #include "base/json/json_reader.h"
 #include "base/test/bind_test_util.h"
-#include "base/test/scoped_feature_list.h"
 #include "base/test/test_timeouts.h"
 #include "build/build_config.h"
 #include "components/viz/common/features.h"
@@ -1878,17 +1877,12 @@ class SitePerProcessMouseWheelHitTestBrowserTest
     EXPECT_EQ("\"scroll: 3\"", reply);
   }
 
- protected:
-  base::test::ScopedFeatureList feature_list_;
-
  private:
   RenderWidgetHostViewAura* rwhv_root_;
 };
 
 IN_PROC_BROWSER_TEST_P(SitePerProcessMouseWheelHitTestBrowserTest,
                        SubframeWheelEventsOnMainThread) {
-  feature_list_.InitWithFeatures({}, {features::kTouchpadAndWheelScrollLatching,
-                                      features::kAsyncWheelEvents});
   GURL main_url(embedded_test_server()->GetURL(
       "/frame_tree/page_with_positioned_nested_frames.html"));
   EXPECT_TRUE(NavigateToURL(shell(), main_url));
@@ -1904,8 +1898,6 @@ IN_PROC_BROWSER_TEST_P(SitePerProcessMouseWheelHitTestBrowserTest,
   // surface information required for event hit testing is ready.
   RenderWidgetHostViewBase* child_rwhv = static_cast<RenderWidgetHostViewBase*>(
       root->child_at(0)->current_frame_host()->GetView());
-
-  EXPECT_FALSE(child_rwhv->wheel_scroll_latching_enabled());
   WaitForChildFrameSurfaceReady(root->child_at(0)->current_frame_host());
 
   content::RenderFrameHostImpl* child = root->child_at(0)->current_frame_host();
@@ -1921,8 +1913,6 @@ IN_PROC_BROWSER_TEST_P(SitePerProcessMouseWheelHitTestBrowserTest,
 // the same page loaded in the mainframe.
 IN_PROC_BROWSER_TEST_P(SitePerProcessMouseWheelHitTestBrowserTest,
                        MainframeWheelEventsOnMainThread) {
-  feature_list_.InitWithFeatures({}, {features::kTouchpadAndWheelScrollLatching,
-                                      features::kAsyncWheelEvents});
   GURL main_url(
       embedded_test_server()->GetURL("/page_with_scrollable_div.html"));
   EXPECT_TRUE(NavigateToURL(shell(), main_url));
@@ -1930,9 +1920,6 @@ IN_PROC_BROWSER_TEST_P(SitePerProcessMouseWheelHitTestBrowserTest,
   FrameTreeNode* root = web_contents()->GetFrameTree()->root();
   content::RenderFrameHostImpl* rfhi = root->current_frame_host();
   SetupWheelAndScrollHandlers(rfhi);
-
-  EXPECT_FALSE(
-      rfhi->GetRenderWidgetHost()->GetView()->wheel_scroll_latching_enabled());
 
   gfx::Point pos(10, 10);
 
