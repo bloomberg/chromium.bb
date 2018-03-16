bf84b7afe4a4893bb2a8801737f80ee4dde9fd0a
Mark Cogan <marq@google.com>
2018-03-16 17:36:32 +0000

    [iOS] Update tab counts in grid via delegate
    Bug: 804552
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I183368403e1977c28dd0696532feff849fcbf0df
    Reviewed-on: https://chromium-review.googlesource.com/966625
    Commit-Queue: edchin <edchin@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543751}
    Former-commit-id: d89dd9673b7ffaacdc1b346a1a424350d9eee3fa

--- ios/chrome/browser/ui/tab_grid/grid_view_controller.h
+++ ios/chrome/browser/ui/tab_grid/grid_view_controller.h
@@ -16,20 +16,18 @@
 
 // Protocol used to relay relevant user interactions from a grid UI.
 @protocol GridViewControllerDelegate
-// Tells the receiver that the item at |index| was selected in
+// Tells the delegate that the item at |index| was selected in
 // |gridViewController|.
 - (void)gridViewController:(GridViewController*)gridViewController
       didSelectItemAtIndex:(NSUInteger)index;
-// Tells the receiver that the item at |index| was closed in
+// Tells the delegate that the item at |index| was closed in
 // |gridViewController|.
 - (void)gridViewController:(GridViewController*)gridViewController
        didCloseItemAtIndex:(NSUInteger)index;
-// Tells the receiver that the last item was closed in |gridViewController|.
-- (void)lastItemWasClosedInGridViewController:
-    (GridViewController*)gridViewController;
-// Tells the receiver that the first item was added in |gridViewController|.
-- (void)firstItemWasAddedInGridViewController:
-    (GridViewController*)gridViewController;
+// Tells the delegate that the the number of items in |gridViewController|
+// changed to |count|.
+- (void)gridViewController:(GridViewController*)gridViewController
+        didChangeItemCount:(NSUInteger)count;
 @end
 
 // A view controller that contains a grid of items.
@@ -40,8 +38,6 @@
 @property(nonatomic, strong) UIView* emptyStateView;
 // Returns YES if the grid has no items.
 @property(nonatomic, readonly, getter=isGridEmpty) BOOL gridEmpty;
-// The number of items in the grid. Not all of the items may be visible.
-@property(nonatomic, readonly) NSUInteger itemCount;
 // The visual look of the grid.
 @property(nonatomic, assign) GridTheme theme;
 // Delegate is informed of user interactions in the grid UI.
