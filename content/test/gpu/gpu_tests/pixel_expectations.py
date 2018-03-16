e8cf3bf91a6882db63765302474269d7fc1ebc85
Geoff Lang <geofflang@chromium.org>
2018-03-16 13:24:51 +0000

    Unsupress video tests after rebaseline.
    TBR=zmo@chromium.org
    
    BUG=749748
    
    Cq-Include-Trybots: luci.chromium.try:linux_optional_gpu_tests_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I0d19ab8893767bf6752c3c67813e4d0354c5e5fe
    Reviewed-on: https://chromium-review.googlesource.com/963070
    Commit-Queue: Geoff Lang <geofflang@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543681}
    Former-commit-id: 84c9e1939751868a4fc0f535cc5dabb2af55841f

--- content/test/gpu/gpu_tests/pixel_expectations.py
+++ content/test/gpu/gpu_tests/pixel_expectations.py
@@ -105,10 +105,6 @@ class PixelExpectations(GpuTestExpectations):
     self.Fail('Pixel_WebGL_PremultipliedAlpha_False',
               ['android', 'nvidia'], bug=791733)
 
-    # Temporary supression for rebaseline
-    self.Fail('Pixel_DirectComposition_Video_MP4', ['win'], bug=749748)
-    self.Fail('Pixel_DirectComposition_Video_VP9', ['win'], bug=749748)
-
     # TODO(zmo): temporarily suppress these two tests until new
     # reference images with new names are generated.
     self.Fail('Pixel_Canvas2DRedBox_NoGpuProcess',
