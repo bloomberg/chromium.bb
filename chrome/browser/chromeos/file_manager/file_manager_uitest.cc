2b55cc3bf94d89af2350277126b8c8eedd10de19
Joel Hockey <joelhockey@chromium.org>
2018-03-16 04:18:08 +0000

    CrOS FileManager UI Test
    Runs UI tests for CrOS FileManager using test/main.html.
    
    Added //ui/file_manager/file_manager/test:create_test_main
    as dep for chrome/browser/chromeos:test_support to ensure
    that test/main.html is generated for chromeos browser_tests.
    
    Bug: 813477
    Tbr: fukino@chromium.org
    Change-Id: Icccc53c724e5af79f4fd4412e3e1f03e6515b3e9
    Reviewed-on: https://chromium-review.googlesource.com/965542
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Sasha Morrissey <sashab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#543620}
    Former-commit-id: c6338bb14b48d30838391e0b2519f709e366e2df

---
+++ chrome/browser/chromeos/file_manager/file_manager_uitest.cc
@@ -0,0 +1,54 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/path_service.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "content/public/common/content_switches.h"
+#include "content/public/test/browser_test_utils.h"
+#include "net/base/filename_util.h"
+
+namespace file_manager {
+
+// Test fixture class for FileManager UI.
+class FileManagerUITest : public InProcessBrowserTest {
+ public:
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    // --disable-web-security required to load resources from
+    // files and from chrome://resources/... urls.
+    command_line->AppendSwitch(switches::kDisableWebSecurity);
+  }
+
+  void RunTest() {
+    base::FilePath root_path;
+    ASSERT_TRUE(PathService::Get(base::DIR_SOURCE_ROOT, &root_path));
+
+    // Load test/main.html.
+    const GURL url = net::FilePathToFileURL(root_path.Append(
+        FILE_PATH_LITERAL("ui/file_manager/file_manager/test/main.html")));
+    content::WebContents* const web_contents =
+        browser()->tab_strip_model()->GetActiveWebContents();
+    ASSERT_TRUE(web_contents);
+    ui_test_utils::NavigateToURL(browser(), url);
+
+    // Load and run specified test file.
+    content::DOMMessageQueue message_queue;
+    ExecuteScriptAsync(web_contents, "runTests()");
+
+    // Wait for JS to call domAutomationController.send("SUCCESS").
+    std::string message;
+    do {
+      EXPECT_TRUE(message_queue.WaitForMessage(&message));
+    } while (message == "\"PENDING\"");
+
+    EXPECT_TRUE(message == "\"SUCCESS\"");
+  }
+};
+
+IN_PROC_BROWSER_TEST_F(FileManagerUITest, UI) {
+  RunTest();
+}
+
+}  // namespace file_manager
