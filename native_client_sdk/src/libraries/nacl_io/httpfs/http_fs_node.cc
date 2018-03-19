c97ff47db8c0c1f90feaea62874b00b7145fd9a4
Ben Smith <binji@chromium.org>
2018-03-19 19:37:32 +0000

    Remove fallthrough in http_fs_node.cc
    Fallthrough in a switch statement now omits a warning in modern
    compilers. Unfortunately, the NaCl compilers are too old to support
    the required annotation. We could enable this conditionally with a
    macro, but it's simpler to just remove the fallthrough and replace
    it with a goto.
    
    Change-Id: I0c112883d170155424c8c40879d3024e2d144c8e
    Reviewed-on: https://chromium-review.googlesource.com/965563
    Reviewed-by: Sam Clegg <sbc@chromium.org>
    Commit-Queue: Ben Smith <binji@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544124}
    Former-commit-id: 12c9fc600f39c4e44f40c9268a33c01dc68c7af1

--- native_client_sdk/src/libraries/nacl_io/httpfs/http_fs_node.cc
+++ native_client_sdk/src/libraries/nacl_io/httpfs/http_fs_node.cc
@@ -62,9 +62,14 @@ StringMap_t ParseHeaders(const char* headers, int32_t headers_length) {
         // Found a non-whitespace, mark this as the start of the value.
         start = &headers[i];
         state = FINDING_VALUE;
-        // Fallthrough to start processing value without incrementing i.
-        [[clang::fallthrough]];
 
+        // NOTE: Avoid fallthrough as it produces a warning on newer compilers,
+        // but can't easily be silenced by the older NaCl compilers.
+        //
+        // Fallthrough to start processing value without incrementing i.
+        goto finding_value;
+
+      finding_value:
       case FINDING_VALUE:
         if (headers[i] == '\n') {
           // Found value.
