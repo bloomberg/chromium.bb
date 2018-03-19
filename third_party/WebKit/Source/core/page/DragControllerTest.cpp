cb162ba0bff82c49b023cc9670c4eaf1afc24989
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2018-03-19 23:20:31 +0000

    DragControllerTest: Rename |ChromeClient()| to avoid naming conflicts
    GCC is stricter than clang when it comes to class members' names and how
    they can change the meaning of a previously existing symbol with the same
    name:
    
        ../../third_party/WebKit/Source/core/page/DragControllerTest.cpp:57:72: error: declaration of ‘blink::DragMockChromeClient& blink::DragControllerTest::ChromeClient() const’ [-fpermissive]
           DragMockChromeClient& ChromeClient() const { return *chrome_client_; }
                                                                                ^
        In file included from ../../third_party/WebKit/Source/core/loader/EmptyClients.h:39:0,
                         from ../../third_party/WebKit/Source/core/layout/LayoutTestHelper.h:17,
                         from ../../third_party/WebKit/Source/core/page/DragControllerTest.cpp:14:
        ../../third_party/WebKit/Source/core/page/ChromeClient.h:94:19: error: changes meaning of ‘ChromeClient’ from ‘class blink::ChromeClient’ [-fpermissive]
         class CORE_EXPORT ChromeClient : public PlatformChromeClient {
                           ^~~~~~~~~~~~
    
    https://gcc.gnu.org/bugzilla/show_bug.cgi?id=84709 contains a longer
    explanation, but essentially having a |ChromeClient()| method can change the
    meaning of references to |ChromeClient| in the code depending on where it
    is declared, which contradicts the C++ standard.
    
    Fix it by taking advantage of covariant return types: instead of having a
    GetChromeClient() method that returns a |ChromeClient| and a ChromeClient()
    method that returns a |DragMockChromeClient|, make GetChromeClient() return
    a |DragMockChromeClient| and do away with ChromeClient() entirely.
    
    Change-Id: I97834bfe22acb0745e5307d13e377f596587e199
    Reviewed-on: https://chromium-review.googlesource.com/968496
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544204}
    Former-commit-id: 5427e0195a70a4707778a1d7ed0b0782afab3b7f

--- third_party/WebKit/Source/core/page/DragControllerTest.cpp
+++ third_party/WebKit/Source/core/page/DragControllerTest.cpp
@@ -53,8 +53,9 @@ class DragControllerTest
 
         chrome_client_(new DragMockChromeClient) {}
   LocalFrame& GetFrame() const { return *GetDocument().GetFrame(); }
-  ChromeClient& GetChromeClient() const override { return *chrome_client_; }
-  DragMockChromeClient& ChromeClient() const { return *chrome_client_; }
+  DragMockChromeClient& GetChromeClient() const override {
+    return *chrome_client_;
+  }
 
   void UpdateAllLifecyclePhases() {
     GetDocument().View()->UpdateAllLifecyclePhases();
@@ -404,12 +405,14 @@ TEST_P(DragControllerTest, DragImageOffsetWithPageScaleFactor) {
 
   IntSize expected_image_size = IntSize(50, 40);
   expected_image_size.Scale(page_scale_factor);
-  EXPECT_EQ(expected_image_size, IntSize(ChromeClient().last_drag_image_size));
+  EXPECT_EQ(expected_image_size,
+            IntSize(GetChromeClient().last_drag_image_size));
   // The drag image has a margin of 2px which should offset the selection
   // image by 2px from the dragged location of (5, 10).
   IntPoint expected_offset = IntPoint(5, 10 - 2);
   expected_offset.Scale(page_scale_factor, page_scale_factor);
-  EXPECT_EQ(expected_offset, IntPoint(ChromeClient().last_drag_image_offset));
+  EXPECT_EQ(expected_offset,
+            IntPoint(GetChromeClient().last_drag_image_offset));
 }
 
 TEST_P(DragControllerTest, DragLinkWithPageScaleFactor) {
@@ -446,7 +449,7 @@ TEST_P(DragControllerTest, DragLinkWithPageScaleFactor) {
   GetFrame().GetPage()->GetDragController().StartDrag(
       &GetFrame(), drag_state, mouse_event, IntPoint(5, 10));
 
-  IntSize link_image_size = IntSize(ChromeClient().last_drag_image_size);
+  IntSize link_image_size = IntSize(GetChromeClient().last_drag_image_size);
   // The drag link image should be a textual representation of the drag url in a
   // system font (see: DragImageForLink in DragController.cpp) and should not be
   // an empty image.
@@ -459,8 +462,10 @@ TEST_P(DragControllerTest, DragLinkWithPageScaleFactor) {
   // The offset is mapped using integers which can introduce rounding errors
   // (see TODO in DragController::DoSystemDrag) so we accept values near our
   // expectation until more precise offset mapping is available.
-  EXPECT_NEAR(expected_offset.X(), ChromeClient().last_drag_image_offset.x, 1);
-  EXPECT_NEAR(expected_offset.Y(), ChromeClient().last_drag_image_offset.y, 1);
+  EXPECT_NEAR(expected_offset.X(), GetChromeClient().last_drag_image_offset.x,
+              1);
+  EXPECT_NEAR(expected_offset.Y(), GetChromeClient().last_drag_image_offset.y,
+              1);
 }
 
 }  // namespace blink
