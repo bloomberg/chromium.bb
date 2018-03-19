32c6600a38c984e790462ab92774a243811fcdb2
Asanka Herath <asanka@chromium.org>
2018-03-19 11:50:42 +0000

    Remove use of SEE_MASK_FLAG_NO_UI from Chrome Windows installer.
    This flag was originally added to ui::base::win to suppress a specific
    error message when attempting to open a file via the shell using the
    "open" verb. The flag has additional side-effects and shouldn't be used
    when invoking ShellExecute().
    
    R=grt@chromium.org
    
    Bug: 819809
    Change-Id: I7db2344982dd206c85a73928e906c21e06a47a9e
    Reviewed-on: https://chromium-review.googlesource.com/966964
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544012}
    Former-commit-id: bd0fde2518644eea1cc53a01e3e3cce1c70e7157

--- chrome/installer/util/google_chrome_distribution.cc
+++ chrome/installer/util/google_chrome_distribution.cc
@@ -56,7 +56,7 @@ base::string16 GetUninstallSurveyUrl() {
 bool NavigateToUrlWithEdge(const base::string16& url) {
   base::string16 protocol_url = L"microsoft-edge:" + url;
   SHELLEXECUTEINFO info = { sizeof(info) };
-  info.fMask = SEE_MASK_NOASYNC | SEE_MASK_FLAG_NO_UI;
+  info.fMask = SEE_MASK_NOASYNC;
   info.lpVerb = L"open";
   info.lpFile = protocol_url.c_str();
   info.nShow = SW_SHOWNORMAL;
