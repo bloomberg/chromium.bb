1793399011a6095aa6511402bf07c2b1ac27091c
Dominick Ng <dominickn@chromium.org>
2018-03-20 00:04:56 +0000

    Remove the WebApk.Install.InstallSource metric.
    This Android-specific metric was replaced by the cross-platform
    Webapp.Install.InstallEvent in M65. It was intended that these metrics exist
    alongside each other, but WebApk.Install.InstallSource was inadvertently
    removed from being recorded in https://crrev.com/c/802336, meaning that data
    is no longer collected as of M65.
    
    BUG=790788
    
    Change-Id: I0fc4724cc19fd4103e192f1e5caf6a9347831250
    Reviewed-on: https://chromium-review.googlesource.com/967906
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544216}
    Former-commit-id: 10125460b69e7b3dee07fe121e572e9e8dc9317e

--- chrome/browser/android/webapk/webapk_metrics.cc
+++ chrome/browser/android/webapk/webapk_metrics.cc
@@ -7,13 +7,11 @@
 #include "base/metrics/histogram_macros.h"
 #include "base/time/time.h"
 #include "chrome/browser/android/webapk/chrome_webapk_host.h"
-#include "chrome/browser/installable/installable_metrics.h"
 
 namespace webapk {
 
 const char kInstallDurationHistogram[] = "WebApk.Install.InstallDuration";
 const char kInstallEventHistogram[] = "WebApk.Install.InstallEvent";
-const char kInstallSourceHistogram[] = "WebApk.Install.InstallSource";
 
 void TrackRequestTokenDuration(base::TimeDelta delta) {
   UMA_HISTOGRAM_TIMES("WebApk.Install.RequestTokenDuration", delta);
@@ -25,37 +23,6 @@ void TrackInstallDuration(base::TimeDelta delta) {
 
 void TrackInstallEvent(InstallEvent event) {
   UMA_HISTOGRAM_ENUMERATION(kInstallEventHistogram, event, INSTALL_EVENT_MAX);
-}
-
-void TrackInstallSource(WebappInstallSource event) {
-  webapk::InstallSource source;
-  switch (event) {
-    case WebappInstallSource::AUTOMATIC_PROMPT_BROWSER_TAB:
-    case WebappInstallSource::AUTOMATIC_PROMPT_CUSTOM_TAB:
-    case WebappInstallSource::API_BROWSER_TAB:
-    case WebappInstallSource::API_CUSTOM_TAB:
-    case WebappInstallSource::DEVTOOLS:
-
-    // Fall through for the ambient badge as this metric will be removed by the
-    // time the badge ships.
-    case WebappInstallSource::AMBIENT_BADGE_BROWSER_TAB:
-    case WebappInstallSource::AMBIENT_BADGE_CUSTOM_TAB:
-      source = InstallSource::INSTALL_SOURCE_BANNER;
-      break;
-    case WebappInstallSource::MENU_BROWSER_TAB:
-    case WebappInstallSource::MENU_CUSTOM_TAB:
-      source = InstallSource::INSTALL_SOURCE_MENU;
-      break;
-    case WebappInstallSource::MANAGEMENT_API:
-    // MANAGEMENT_API is not reported. Fallthrough to NOTREACHED().
-    case WebappInstallSource::COUNT:
-      NOTREACHED();
-      return;
-  }
-
-  UMA_HISTOGRAM_ENUMERATION(kInstallSourceHistogram, source,
-                            INSTALL_SOURCE_MAX);
-  InstallableMetrics::TrackInstallEvent(event);
 }
 
 }  // namespace webapk
