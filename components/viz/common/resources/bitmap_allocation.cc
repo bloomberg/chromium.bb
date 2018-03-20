1d640eaad0013b53923e093dd76395ae082eb155
erikchen <erikchen@chromium.org>
2018-03-20 20:20:44 +0000

    Add back debugging logic to CollectMemoryUsageAndDie.
    Most of the logic was removed in
    https://chromium-review.googlesource.com/c/chromium/src/+/969089/. This CL adds
    back the logic and expands it to include non-Windows platforms.
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I36438ed123353c578c4cd4dd27ef14da68c24d03
    Reviewed-on: https://chromium-review.googlesource.com/971622
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544504}
    Former-commit-id: ff3e4e28d1428990266669920579f3cea0e007e9

--- components/viz/common/resources/bitmap_allocation.cc
+++ components/viz/common/resources/bitmap_allocation.cc
@@ -6,6 +6,7 @@
 
 #include <stdint.h>
 
+#include "base/debug/alias.h"
 #include "base/memory/shared_memory.h"
 #include "base/process/memory.h"
 #include "build/build_config.h"
@@ -16,6 +17,27 @@
 
 namespace viz {
 
+namespace {
+// Collect extra information for debugging bitmap creation failures.
+void CollectMemoryUsageAndDie(const gfx::Size& size,
+                              ResourceFormat format,
+                              size_t alloc_size) {
+#if defined(OS_WIN)
+  DWORD last_error = GetLastError();
+  base::debug::Alias(&last_error);
+#endif
+
+  int width = size.width();
+  int height = size.height();
+
+  base::debug::Alias(&width);
+  base::debug::Alias(&height);
+  base::debug::Alias(&format);
+
+  base::TerminateBecauseOutOfMemory(alloc_size);
+}
+}  // namespace
+
 namespace bitmap_allocation {
 
 std::unique_ptr<base::SharedMemory> AllocateMappedBitmap(
@@ -25,26 +47,26 @@ std::unique_ptr<base::SharedMemory> AllocateMappedBitmap(
   size_t bytes = 0;
   if (!ResourceSizes::MaybeSizeInBytes(size, format, &bytes)) {
     DLOG(ERROR) << "AllocateMappedBitmap with size that overflows";
-    base::TerminateBecauseOutOfMemory(std::numeric_limits<int>::max());
+    CollectMemoryUsageAndDie(size, format, std::numeric_limits<int>::max());
   }
 
   auto mojo_buf = mojo::SharedBufferHandle::Create(bytes);
   if (!mojo_buf->is_valid()) {
     DLOG(ERROR) << "Browser failed to allocate shared memory";
-    base::TerminateBecauseOutOfMemory(bytes);
+    CollectMemoryUsageAndDie(size, format, bytes);
   }
 
   base::SharedMemoryHandle shared_buf;
   if (mojo::UnwrapSharedMemoryHandle(std::move(mojo_buf), &shared_buf, nullptr,
                                      nullptr) != MOJO_RESULT_OK) {
     DLOG(ERROR) << "Browser failed to allocate shared memory";
-    base::TerminateBecauseOutOfMemory(bytes);
+    CollectMemoryUsageAndDie(size, format, bytes);
   }
 
   auto memory = std::make_unique<base::SharedMemory>(shared_buf, false);
   if (!memory->Map(bytes)) {
     DLOG(ERROR) << "Browser failed to map shared memory";
-    base::TerminateBecauseOutOfMemory(bytes);
+    CollectMemoryUsageAndDie(size, format, bytes);
   }
 
   return memory;
@@ -59,7 +81,7 @@ mojo::ScopedSharedBufferHandle DuplicateAndCloseMappedBitmap(
       base::SharedMemory::DuplicateHandle(memory->handle());
   if (!base::SharedMemory::IsHandleValid(dupe_handle)) {
     DLOG(ERROR) << "Failed to duplicate shared memory handle for bitmap.";
-    base::TerminateBecauseOutOfMemory(memory->requested_size());
+    CollectMemoryUsageAndDie(size, format, memory->requested_size());
   }
 
   return mojo::WrapSharedMemoryHandle(
