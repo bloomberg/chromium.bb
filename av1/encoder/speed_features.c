75b6ebb5b7b9d86c5c32757a0d44e467f09e72f2
Jingning Han <jingning@google.com>
2018-03-20 14:45:11 -0700

    Use enums for optimize_speed_feature setup
    Replace the hard coded 0 with NO_TRELLIS_OPT.
    
    Change-Id: I56a34fd4d920148104fa00da089b37b682064b37

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -547,7 +547,7 @@ void av1_set_speed_features_framesize_independent(AV1_COMP *cpi) {
   cpi->optimize_speed_feature =
       oxcf->pass != 1 ? sf->optimize_coefficients : NO_TRELLIS_OPT;
   // FIXME: trellis not very efficient for quantisation matrices
-  if (cm->using_qmatrix) cpi->optimize_speed_feature = 0;
+  if (cm->using_qmatrix) cpi->optimize_speed_feature = NO_TRELLIS_OPT;
 
   x->min_partition_size = sf->default_min_partition_size;
   x->max_partition_size = sf->default_max_partition_size;
