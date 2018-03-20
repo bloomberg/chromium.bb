7e5b4f3059b7ac2072fc4295da3278a1ac57ae90
Boris Sazonov <bsazonov@chromium.org>
2018-03-20 15:49:07 +0000

    [Unity][Android] Add account picker to sign-in screen
    This CL adds account picker to the streamlined sign-in screen. It will
    be wired up by subsequent CLs. RelativeLayout is used to avoid using
    nested LinearLayouts and minimize the depth of the layout. Strings in
    grd file are currently marked as not translateable as they haven't been
    confirmed by the UI team yet.
    This CL also adds initialization for ConsentTextTracker in
    SigninFragmentBase.
    
    Bug: 814728
    Change-Id: I60ae3841998d5dd9c13d35583915e7fe443d3b31
    Reviewed-on: https://chromium-review.googlesource.com/966902
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544384}
    Former-commit-id: a7f353b61b913700e8a8e5537847fbaeaeb189ab

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninFragmentBase.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninFragmentBase.java
@@ -4,10 +4,13 @@
 
 package org.chromium.chrome.browser.signin;
 
+import android.annotation.SuppressLint;
 import android.app.Fragment;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
+import android.support.v7.content.res.AppCompatResources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -72,6 +75,8 @@ public abstract class SigninFragmentBase extends Fragment {
 
         Bundle arguments = getSigninArguments();
         initAccessPoint(arguments.getInt(ARGUMENT_ACCESS_POINT, -1));
+
+        mConsentTextTracker = new ConsentTextTracker(getResources());
     }
 
     private void initAccessPoint(@SigninAccessPoint int accessPoint) {
@@ -95,6 +100,7 @@ public abstract class SigninFragmentBase extends Fragment {
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mView = (SigninView) inflater.inflate(R.layout.signin_view, container, false);
+
         mView.getAcceptButton().setVisibility(View.GONE);
         mView.getMoreButton().setVisibility(View.VISIBLE);
         mView.getMoreButton().setOnClickListener(view -> {
@@ -110,14 +116,27 @@ public abstract class SigninFragmentBase extends Fragment {
         });
 
         updateConsentText();
+        updateProfileData();
 
         return mView;
     }
 
     private void updateConsentText() {
+        mConsentTextTracker.setText(mView.getTitleView(), R.string.signin_title);
         mConsentTextTracker.setText(mView.getRefuseButton(), mCancelButtonTextId);
         mConsentTextTracker.setText(mView.getAcceptButton(), R.string.signin_accept_button);
         mConsentTextTracker.setText(mView.getMoreButton(), R.string.more);
+    }
+
+    // TODO(https://crbug.com/814728): Get real profile data and remove SuppressLint.
+    @SuppressLint("SetTextI18n")
+    private void updateProfileData() {
+        Drawable avatar =
+                AppCompatResources.getDrawable(getActivity(), R.drawable.logo_avatar_anonymous);
+        mView.getAccountImageView().setImageDrawable(avatar);
+        mConsentTextTracker.setTextNonRecordable(mView.getAccountNameView(), "John Doe");
+        mConsentTextTracker.setTextNonRecordable(
+                mView.getAccountEmailView(), "john.doe@example.com");
     }
 
     private void showAcceptButton() {
