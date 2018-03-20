2ccbda7969b7a1a4f3744ea4520f20e3a14d4b9c
Peter Kasting <pkasting@chromium.org>
2018-03-20 17:17:38 +0000

    Fall back from PRS_THEME_FRAME_OVERLAY_INACTIVE correctly.
    Instead of falling back to PRS_THEME_FRAME_OVERLAY as intended, we were falling
    back to PRS_THEME_FRAME.
    
    BUG=none
    TEST=none
    
    Change-Id: Id8cb10d7bacbf6cd0487d27c291ec13e0f5c3be5
    Reviewed-on: https://chromium-review.googlesource.com/969861
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544418}
    Former-commit-id: 8e23c7996d890fdc77fca236e6b1dc2289a94a65

--- chrome/browser/themes/browser_theme_pack.cc
+++ chrome/browser/themes/browser_theme_pack.cc
@@ -1217,17 +1217,17 @@ void BrowserThemePack::CreateFrameImages(ImageCache* images) const {
   for (size_t i = 0; i < arraysize(kFrameTintMap); ++i) {
     int prs_id = kFrameTintMap[i].key;
     if (!images->count(prs_id)) {
-      // Fall back from inactive incognito to active incognito.
-      if (prs_id == PRS_THEME_FRAME_INCOGNITO_INACTIVE)
+      if (prs_id == PRS_THEME_FRAME_INCOGNITO_INACTIVE) {
+        // Fall back from inactive incognito to active incognito.
         prs_id = PRS_THEME_FRAME_INCOGNITO;
-
-      // From there, fall back to the normal frame.
-      if (!images->count(prs_id))
-        prs_id = PRS_THEME_FRAME;
-
-      // Fall back from inactive overlay to overlay, but stop there.
-      if (prs_id == PRS_THEME_FRAME_OVERLAY_INACTIVE)
+        if (!images->count(prs_id)) {
+          // From there, fall back to the normal frame.
+          prs_id = PRS_THEME_FRAME;
+        }
+      } else if (prs_id == PRS_THEME_FRAME_OVERLAY_INACTIVE) {
+        // Fall back from inactive overlay to overlay, but stop there.
         prs_id = PRS_THEME_FRAME_OVERLAY;
+      }
     }
 
     // Note that if the original ID and all the fallbacks are absent, the caller
