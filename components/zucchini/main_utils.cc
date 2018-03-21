6a4aab10f91e82023b1f72034d6db29bf227fdcd
Samuel Huang <huangs@chromium.org>
2018-03-21 18:54:03 +0000

    [Zucchini] Add "-keep" flag; remove redundnat CRC32 check.
    By defaulti, Zucchini deletes the output (patch or patched "new") file
    upon failure. The "-keep" debug flag overrides this behavior, so the
    bad output file can be examined. This CL adds the flag for Zucchini-gen
    and Zucchini-apply.
    
    This CL also removes a duplicated call to Ensemble::CheckOldFile(),
    which checks the CRC32 of the "old" file. This removal prevents
    redundant work and slightly speed up Zucchini-apply (by ~5%).
    
    Bug: 729154
    Change-Id: I9ba8e9b00b7783661de06b39fae010761bedc6e6
    Reviewed-on: https://chromium-review.googlesource.com/973746
    Reviewed-by: agrieve <agrieve@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544786}
    Former-commit-id: 21879c3adbfbbf676e5ee28b9f0a19e60d104d80

--- components/zucchini/main_utils.cc
+++ components/zucchini/main_utils.cc
@@ -63,8 +63,10 @@ struct Command {
 /******** List of Zucchini commands ********/
 
 constexpr Command kCommands[] = {
-    {"gen", "-gen <old_file> <new_file> <patch_file> [-raw]", 3, &MainGen},
-    {"apply", "-apply <old_file> <patch_file> <new_file>", 3, &MainApply},
+    {"gen", "-gen <old_file> <new_file> <patch_file> [-raw] [-keep]", 3,
+     &MainGen},
+    {"apply", "-apply <old_file> <patch_file> <new_file> [-keep]", 3,
+     &MainApply},
     {"read", "-read <exe> [-dump]", 1, &MainRead},
     {"detect", "-detect <archive_file> [-dd=format#]", 1, &MainDetect},
     {"match", "-match <old_file> <new_file>", 2, &MainMatch},
