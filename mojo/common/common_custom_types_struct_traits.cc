22198b7ac4c8525215b11d57f02ab1d6db05ea9a
Oksana Zhuravlova <oksamyt@chromium.org>
2018-03-21 21:13:29 +0000

    Move logfont_win.mojom and text_direction.mojom from common to base
    This change moves logfont_win.mojom from //mojo/common to
    //mojo/public/mojom/base and logfont_win.typemap to
    //mojo/public/cpp/base, as well as text_direction.mojom and
    corresponding files. Unit test for TextDirection was updated and moved
    to mojo_unittests.
    
    Bug: 799482
    Change-Id: Ib3d8b09d7aa3b7e12d3f66b38dbcea294a28c035
    Reviewed-on: https://chromium-review.googlesource.com/971263
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544843}
    Former-commit-id: ce80ad3c0925333527fa32ac22046f0510ac0106

--- mojo/common/common_custom_types_struct_traits.cc
+++ mojo/common/common_custom_types_struct_traits.cc
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 #include "mojo/common/common_custom_types_struct_traits.h"
-
 #include "mojo/public/cpp/system/platform_handle.h"
 
 namespace mojo {
@@ -22,40 +21,6 @@ bool StructTraits<
 
   *out = base::UnguessableToken::Deserialize(high, low);
   return true;
-}
-
-// static
-common::mojom::TextDirection
-EnumTraits<common::mojom::TextDirection, base::i18n::TextDirection>::ToMojom(
-    base::i18n::TextDirection text_direction) {
-  switch (text_direction) {
-    case base::i18n::UNKNOWN_DIRECTION:
-      return common::mojom::TextDirection::UNKNOWN_DIRECTION;
-    case base::i18n::RIGHT_TO_LEFT:
-      return common::mojom::TextDirection::RIGHT_TO_LEFT;
-    case base::i18n::LEFT_TO_RIGHT:
-      return common::mojom::TextDirection::LEFT_TO_RIGHT;
-  }
-  NOTREACHED();
-  return common::mojom::TextDirection::UNKNOWN_DIRECTION;
-}
-
-// static
-bool EnumTraits<common::mojom::TextDirection, base::i18n::TextDirection>::
-    FromMojom(common::mojom::TextDirection input,
-              base::i18n::TextDirection* out) {
-  switch (input) {
-    case common::mojom::TextDirection::UNKNOWN_DIRECTION:
-      *out = base::i18n::UNKNOWN_DIRECTION;
-      return true;
-    case common::mojom::TextDirection::RIGHT_TO_LEFT:
-      *out = base::i18n::RIGHT_TO_LEFT;
-      return true;
-    case common::mojom::TextDirection::LEFT_TO_RIGHT:
-      *out = base::i18n::LEFT_TO_RIGHT;
-      return true;
-  }
-  return false;
 }
 
 // static
