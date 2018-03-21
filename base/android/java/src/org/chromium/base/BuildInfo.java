bloomberg/chromium.bb

commit d03119cd641b9bdfd2e69157ea998b49991ed953

Android: Refactor BuildInfo to correct the "package" field of WebView minidumps
This adds the concept of "hostPackage" vs "package", which for webview
will contain different values.

TBR=agrieve  # trivial rename in devtools_http_handler.cc
Bug: 620323

Change-Id: I4a3c3376d3793eb3ddb90afe3359e320d6255edc
Reviewed-on: https://chromium-review.googlesource.com/939715
Commit-Queue: agrieve <agrieve@chromium.org>
Reviewed-by: Maria Khomenko <mariakhomenko@chromium.org>
Reviewed-by: Tobias Sargeant <tobiasjs@chromium.org>
Cr-Commit-Position: refs/heads/master@{#541114}
Former-commit-id: be35cd67008fdd35bd56f83baab8abce07543332

base/android/java/src/org/chromium/base/BuildInfo.java
@@ -4,6 +4,7 @@
 
 package org.chromium.base;
 
+import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -18,109 +19,113 @@ import org.chromium.base.annotations.CalledByNative;
  * primarily of use for accessing package information from native code.
  */
 public class BuildInfo {
-    /**
-     * Array index to access field in {@link BuildInfo#getAll()}.
-     */
-    public static final int BRAND_INDEX = 0;
-    public static final int DEVICE_INDEX = 1;
-    public static final int ANDROID_BUILD_ID_INDEX = 2;
-    public static final int MODEL_INDEX = 4;
-    public static final int ANDROID_BUILD_FP_INDEX = 11;
-    public static final int GMS_CORE_VERSION_INDEX = 12;
-    public static final int INSTALLER_PACKAGE_NAME_INDEX = 13;
-    public static final int ABI_NAME_INDEX = 14;
-
     private static final String TAG = "BuildInfo";
     private static final int MAX_FINGERPRINT_LENGTH = 128;
 
-    /**
-     * BuildInfo is a static utility class and therefore shouldn't be instantiated.
-     */
-    private BuildInfo() {}
+    private static PackageInfo sBrowserPackageInfo;
+    private static boolean sInitialized;
 
-    @SuppressWarnings("deprecation")
+    /** The application name (e.g. "Chrome"). For WebView, this is name of the embedding app. */
+    public final String hostPackageLabel;
+    /** By default: same as versionCode. For WebView: versionCode of the embedding app. */
+    public final int hostVersionCode;
+    /** The packageName of Chrome/WebView. Use application context for host app packageName. */
+    public final String packageName;
+    /** The versionCode of the apk. */
+    public final int versionCode;
+    /** The versionName of Chrome/WebView. Use application context for host app versionName. */
+    public final String versionName;
+    /** Result of PackageManager.getInstallerPackageName(). Never null, but may be "". */
+    public final String installerPackageName;
+    /** The versionCode of Play Services (for crash reporting). */
+    public final String gmsVersionCode;
+    /** Formatted ABI string (for crash reporting). */
+    public final String abiString;
+    /** Truncated version of Build.FINGERPRINT (for crash reporting). */
+    public final String androidBuildFingerprint;
+    /** A string that is different each time the apk changes. */
+    public final String extractedFileSuffix;
+
+    private static class Holder { private static BuildInfo sInstance = new BuildInfo(); }
+
     @CalledByNative
-    public static String[] getAll() {
+    private static String[] getAll() {
+        BuildInfo buildInfo = getInstance();
+        String hostPackageName = ContextUtils.getApplicationContext().getPackageName();
+        return new String[] {
+                Build.BRAND, Build.DEVICE, Build.ID, Build.MANUFACTURER, Build.MODEL,
+                String.valueOf(Build.VERSION.SDK_INT), Build.TYPE, hostPackageName,
+                String.valueOf(buildInfo.hostVersionCode), buildInfo.hostPackageLabel,
+                buildInfo.packageName, String.valueOf(buildInfo.versionCode), buildInfo.versionName,
+                buildInfo.androidBuildFingerprint, buildInfo.gmsVersionCode,
+                buildInfo.installerPackageName, buildInfo.abiString, BuildConfig.FIREBASE_APP_ID,
+                buildInfo.extractedFileSuffix,
+        };
+    }
+
+    /**
+     * @param packageInfo Package for Chrome/WebView (as opposed to host app).
+     */
+    public static void setBrowserPackageInfo(PackageInfo packageInfo) {
+        assert !sInitialized;
+        sBrowserPackageInfo = packageInfo;
+    }
+
+    public static BuildInfo getInstance() {
+        return Holder.sInstance;
+    }
+
+    private BuildInfo() {
+        sInitialized = true;
         try {
-            String packageName = ContextUtils.getApplicationContext().getPackageName();
-            PackageManager pm = ContextUtils.getApplicationContext().getPackageManager();
-            PackageInfo pi = pm.getPackageInfo(packageName, 0);
-            String versionCode = pi.versionCode <= 0 ? "" : Integer.toString(pi.versionCode);
-            String versionName = pi.versionName == null ? "" : pi.versionName;
-
-            CharSequence label = pm.getApplicationLabel(pi.applicationInfo);
-            String packageLabel = label == null ? "" : label.toString();
-
-            String installerPackageName = pm.getInstallerPackageName(packageName);
-            if (installerPackageName == null) {
-                installerPackageName = "";
+            Context appContext = ContextUtils.getApplicationContext();
+            String hostPackageName = appContext.getPackageName();
+            PackageManager pm = appContext.getPackageManager();
+            PackageInfo pi = pm.getPackageInfo(hostPackageName, 0);
+            hostVersionCode = pi.versionCode;
+            if (sBrowserPackageInfo != null) {
+                packageName = sBrowserPackageInfo.packageName;
+                versionCode = sBrowserPackageInfo.versionCode;
+                versionName = sBrowserPackageInfo.versionName;
+                sBrowserPackageInfo = null;
+            } else {
+                packageName = hostPackageName;
+                versionCode = hostVersionCode;
+                versionName = pi.versionName;
             }
 
-            String abiString = null;
+            CharSequence label = pm.getApplicationLabel(pi.applicationInfo);
+            hostPackageLabel = label == null ? "" : label.toString();
+
+            String value = pm.getInstallerPackageName(packageName);
+            installerPackageName = value == null ? "" : value;
+
+            PackageInfo gmsPackageInfo = null;
+            try {
+                gmsPackageInfo = pm.getPackageInfo("com.google.android.gms", 0);
+            } catch (NameNotFoundException e) {
+                Log.d(TAG, "GMS package is not found.", e);
+            }
+            gmsVersionCode = gmsPackageInfo != null ? String.valueOf(gmsPackageInfo.versionCode)
+                                                    : "gms versionCode not available.";
+
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 abiString = TextUtils.join(", ", Build.SUPPORTED_ABIS);
             } else {
-                abiString = "ABI1: " + Build.CPU_ABI + ", ABI2: " + Build.CPU_ABI2;
+                abiString = String.format("ABI1: %s, ABI2: %s", Build.CPU_ABI, Build.CPU_ABI2);
             }
 
             // Use lastUpdateTime when developing locally, since versionCode does not normally
             // change in this case.
-            long version = pi.versionCode > 10 ? pi.versionCode : pi.lastUpdateTime;
-            String extractedFileSuffix = String.format("@%s", Long.toHexString(version));
+            long version = versionCode > 10 ? versionCode : pi.lastUpdateTime;
+            extractedFileSuffix = String.format("@%x", version);
 
-            // Do not alter this list without updating callers of it.
-            return new String[] {
-                    Build.BRAND, Build.DEVICE, Build.ID, Build.MANUFACTURER, Build.MODEL,
-                    String.valueOf(Build.VERSION.SDK_INT), Build.TYPE, packageLabel, packageName,
-                    versionCode, versionName, getAndroidBuildFingerprint(), getGMSVersionCode(pm),
-                    installerPackageName, abiString, BuildConfig.FIREBASE_APP_ID,
-                    extractedFileSuffix,
-            };
+            // The value is truncated, as this is used for crash and UMA reporting.
+            androidBuildFingerprint = Build.FINGERPRINT.substring(
+                    0, Math.min(Build.FINGERPRINT.length(), MAX_FINGERPRINT_LENGTH));
         } catch (NameNotFoundException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    /**
-     * @return The build fingerprint for the current Android install.  The value is truncated to a
-     * 128 characters as this is used for crash and UMA reporting, which should avoid huge
-     * strings.
-     */
-    private static String getAndroidBuildFingerprint() {
-        return Build.FINGERPRINT.substring(
-                0, Math.min(Build.FINGERPRINT.length(), MAX_FINGERPRINT_LENGTH));
-    }
-
-    private static String getGMSVersionCode(PackageManager packageManager) {
-        String msg = "gms versionCode not available.";
-        try {
-            PackageInfo packageInfo = packageManager.getPackageInfo("com.google.android.gms", 0);
-            msg = Integer.toString(packageInfo.versionCode);
-        } catch (NameNotFoundException e) {
-            Log.d(TAG, "GMS package is not found.", e);
-        }
-        return msg;
-    }
-
-    public static String getPackageVersionCode() {
-        return getAll()[9];
-    }
-
-    public static String getPackageVersionName() {
-        return getAll()[10];
-    }
-
-    /** Returns a string that is different each time the apk changes. */
-    public static String getExtractedFileSuffix() {
-        return getAll()[16];
-    }
-
-    public static String getPackageLabel() {
-        return getAll()[7];
-    }
-
-    public static String getPackageName() {
-        return ContextUtils.getApplicationContext().getPackageName();
     }
 
     /**

android_webview/browser/net/aw_network_delegate.cc
@@ -53,7 +53,7 @@ int AwNetworkDelegate::OnBeforeStartTransaction(
   DCHECK(headers);
   headers->SetHeaderIfMissing(
       "X-Requested-With",
-      base::android::BuildInfo::GetInstance()->package_name());
+      base::android::BuildInfo::GetInstance()->host_package_name());
   return net::OK;
 }
 

android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
@@ -36,6 +36,7 @@ import org.chromium.android_webview.AwBrowserProcess;
 import org.chromium.android_webview.ResourcesContextWrapperFactory;
 import org.chromium.android_webview.WebViewChromiumRunQueue;
 import org.chromium.android_webview.command_line.CommandLineUtil;
+import org.chromium.base.BuildInfo;
 import org.chromium.base.CommandLine;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.PackageUtils;
@@ -202,10 +203,11 @@ public class WebViewChromiumFactoryProvider implements WebViewFactoryProvider {
         }
 
         ThreadUtils.setWillOverrideUiThread();
+        final PackageInfo packageInfo = WebViewFactory.getLoadedPackageInfo();
+        BuildInfo.setBrowserPackageInfo(packageInfo);
+
         // Load chromium library.
         AwBrowserProcess.loadLibrary(mWebViewDelegate.getDataDirectorySuffix());
-
-        final PackageInfo packageInfo = WebViewFactory.getLoadedPackageInfo();
 
         StrictMode.ThreadPolicy oldPolicy = StrictMode.allowThreadDiskWrites();
         try {

android_webview/lib/aw_main_delegate.cc
@@ -241,11 +241,11 @@ void AwMainDelegate::PreSandboxStartup() {
 
   static ::crash_reporter::CrashKeyString<64> app_name_key(
       crash_keys::kAppPackageName);
-  app_name_key.Set(android_build_info->package_name());
+  app_name_key.Set(android_build_info->host_package_name());
 
   static ::crash_reporter::CrashKeyString<64> app_version_key(
       crash_keys::kAppPackageVersionCode);
-  app_version_key.Set(android_build_info->package_version_code());
+  app_version_key.Set(android_build_info->host_version_code());
 
   static ::crash_reporter::CrashKeyString<8> sdk_int_key(
       crash_keys::kAndroidSdkInt);

base/android/build_info.cc
@@ -61,16 +61,18 @@ BuildInfo::BuildInfo(const std::vector<std::string>& params)
       model_(StrDupParam(params, 4)),
       sdk_int_(SdkIntParam(params, 5)),
       build_type_(StrDupParam(params, 6)),
-      package_label_(StrDupParam(params, 7)),
-      package_name_(StrDupParam(params, 8)),
-      package_version_code_(StrDupParam(params, 9)),
-      package_version_name_(StrDupParam(params, 10)),
-      android_build_fp_(StrDupParam(params, 11)),
-      gms_version_code_(StrDupParam(params, 12)),
-      installer_package_name_(StrDupParam(params, 13)),
-      abi_name_(StrDupParam(params, 14)),
-      firebase_app_id_(StrDupParam(params, 15)),
-      extracted_file_suffix_(params[16]),
+      host_package_name_(StrDupParam(params, 7)),
+      host_version_code_(StrDupParam(params, 8)),
+      host_package_label_(StrDupParam(params, 9)),
+      package_name_(StrDupParam(params, 10)),
+      package_version_code_(StrDupParam(params, 11)),
+      package_version_name_(StrDupParam(params, 12)),
+      android_build_fp_(StrDupParam(params, 13)),
+      gms_version_code_(StrDupParam(params, 14)),
+      installer_package_name_(StrDupParam(params, 15)),
+      abi_name_(StrDupParam(params, 16)),
+      firebase_app_id_(StrDupParam(params, 17)),
+      extracted_file_suffix_(params[18]),
       java_exception_info_(NULL) {}
 
 // static

base/android/build_info.h
@@ -82,16 +82,18 @@ class BASE_EXPORT BuildInfo {
     return gms_version_code_;
   }
 
+  const char* host_package_name() const { return host_package_name_; }
+
+  const char* host_version_code() const { return host_version_code_; }
+
+  const char* host_package_label() const { return host_package_label_; }
+
   const char* package_version_code() const {
     return package_version_code_;
   }
 
   const char* package_version_name() const {
     return package_version_name_;
-  }
-
-  const char* package_label() const {
-    return package_label_;
   }
 
   const char* package_name() const {
@@ -139,7 +141,9 @@ class BASE_EXPORT BuildInfo {
   const char* const model_;
   const int sdk_int_;
   const char* const build_type_;
-  const char* const package_label_;
+  const char* const host_package_name_;
+  const char* const host_version_code_;
+  const char* const host_package_label_;
   const char* const package_name_;
   const char* const package_version_code_;
   const char* const package_version_name_;

base/android/java/src/org/chromium/base/ResourceExtractor.java
@@ -64,7 +64,7 @@ public class ResourceExtractor {
 
             // Use a suffix for extracted files in order to guarantee that the version of the file
             // on disk matches up with the version of the APK.
-            String extractSuffix = BuildInfo.getExtractedFileSuffix();
+            String extractSuffix = BuildInfo.getInstance().extractedFileSuffix;
             String[] existingFileNames = outputDir.list();
             boolean allFilesExist = existingFileNames != null;
             if (allFilesExist) {

chrome/android/java/src/org/chromium/chrome/browser/DefaultBrowserInfo.java
@@ -92,8 +92,8 @@ public final class DefaultBrowserInfo {
                         Context context = ContextUtils.getApplicationContext();
                         ArrayList<String> menuTitles = new ArrayList<String>(2);
                         // Store the package label of current application.
-                        menuTitles.add(
-                                getTitleFromPackageLabel(context, BuildInfo.getPackageLabel()));
+                        menuTitles.add(getTitleFromPackageLabel(
+                                context, BuildInfo.getInstance().hostPackageLabel));
 
                         PackageManager pm = context.getPackageManager();
                         ResolveInfo info = getResolveInfoForViewIntent(pm);

commit 1a9601fe4c2842ec6a3d9af5b548e8df4510f243

Android: Guard against null versionName in BuildInfo
This regressed in d03119cd641b9bdfd2e69157ea998b49991ed953 [formerly be35cd67008fdd35bd56f83baab8abce07543332]

Bug: 824141
Change-Id: I7975bc8dffbfafeec7f0a50bad4d75dfeefff0ee
Reviewed-on: https://chromium-review.googlesource.com/973383
Commit-Queue: agrieve <agrieve@chromium.org>
Reviewed-by: Paul Jensen <pauljensen@chromium.org>
Cr-Commit-Position: refs/heads/master@{#544711}
Former-commit-id: 3cb9bc5965437ce539163205e8d8ab763fde1f5c

base/android/java/src/org/chromium/base/BuildInfo.java
@@ -63,6 +63,10 @@ public class BuildInfo {
         };
     }
 
+    private static String nullToEmpty(CharSequence seq) {
+        return seq == null ? "" : seq.toString();
+    }
+
     /**
      * @param packageInfo Package for Chrome/WebView (as opposed to host app).
      */
@@ -86,19 +90,16 @@ public class BuildInfo {
             if (sBrowserPackageInfo != null) {
                 packageName = sBrowserPackageInfo.packageName;
                 versionCode = sBrowserPackageInfo.versionCode;
-                versionName = sBrowserPackageInfo.versionName;
+                versionName = nullToEmpty(sBrowserPackageInfo.versionName);
                 sBrowserPackageInfo = null;
             } else {
                 packageName = hostPackageName;
                 versionCode = hostVersionCode;
-                versionName = pi.versionName;
+                versionName = nullToEmpty(pi.versionName);
             }
 
-            CharSequence label = pm.getApplicationLabel(pi.applicationInfo);
-            hostPackageLabel = label == null ? "" : label.toString();
-
-            String value = pm.getInstallerPackageName(packageName);
-            installerPackageName = value == null ? "" : value;
+            hostPackageLabel = nullToEmpty(pm.getApplicationLabel(pi.applicationInfo));
+            installerPackageName = nullToEmpty(pm.getInstallerPackageName(packageName));
 
             PackageInfo gmsPackageInfo = null;
             try {