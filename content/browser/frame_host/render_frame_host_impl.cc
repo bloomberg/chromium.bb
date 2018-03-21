c1988a67c408beaf785fe3f35bb1bd89d6dded2f
Helen Li <xunjieli@chromium.org>
2018-03-21 21:26:19 +0000

    Modify handle.is_valid() check in render_frame_host_impl.cc
    handle->is_valid() will try to de-reference the handle. This CL changes
    ->is_valid() to .is_valid() so that the validity check is performed correctly.
    
    Bug: 824144
    Change-Id: Ie6f7a5cfa2a7568cdbaceb30e0667d990d1326e3
    Reviewed-on: https://chromium-review.googlesource.com/973914
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Helen Li <xunjieli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544851}
    Former-commit-id: 9463d54cee1fdbb52c285775b5d72cc8e58aad86

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -4402,7 +4402,7 @@ void RenderFrameHostImpl::GetInterface(
   if (!registry_ ||
       !registry_->TryBindInterface(interface_name, &interface_pipe)) {
     delegate_->OnInterfaceRequest(this, interface_name, &interface_pipe);
-    if (interface_pipe->is_valid() &&
+    if (interface_pipe.is_valid() &&
         !TryBindFrameInterface(interface_name, &interface_pipe, this)) {
       GetContentClient()->browser()->BindInterfaceRequestFromFrame(
           this, interface_name, std::move(interface_pipe));
