45e34ea88a368a8f8bb5e0930963352c9a616807
Lei Zhang <thestig@chromium.org>
2018-03-22 16:22:14 +0000

    Remove a shadow variable in printing::PageRange.
    Change-Id: I269b9b0249500cee9ea4c123951d89c347f791e3
    Reviewed-on: https://chromium-review.googlesource.com/974319
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545106}
    Former-commit-id: 3de3db8be2f35aa1fa194764fa4e14e055b32d9c

--- printing/page_range.cc
+++ printing/page_range.cc
@@ -8,20 +8,16 @@
 
 #include <set>
 
-namespace {
-const std::size_t kMaxNumberOfPages = 100000;
-}
-
 namespace printing {
 
-/* static */
+// static
 std::vector<int> PageRange::GetPages(const PageRanges& ranges) {
   // TODO(vitalybuka): crbug.com/95548 Remove this method as part fix.
   std::set<int> pages;
-  for (unsigned i = 0; i < ranges.size(); ++i) {
-    const PageRange& range = ranges[i];
+  for (const PageRange& range : ranges) {
     // Ranges are inclusive.
     for (int i = range.from; i <= range.to; ++i) {
+      static constexpr size_t kMaxNumberOfPages = 100000;
       pages.insert(i);
       if (pages.size() >= kMaxNumberOfPages)
         return std::vector<int>(pages.begin(), pages.end());
