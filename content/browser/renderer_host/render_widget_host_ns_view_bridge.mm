5724803f0edfb5f9b5268f3439e9c9d903069800
Moe Ahmadi <mahmadi@chromium.org>
2018-03-22 19:21:09 +0000

    Revert "Remove redundant code from RWHVCocoa, move some code to RWHVNSViewBridge"
    This reverts commit 46eb792f08ff0436d35c04084b96048765a77b67 [formerly 99341850cb8c2b7edf0b9802d0411be77f6111d4].
    
    Reason for revert: Causing compilation failure
    https://ci.chromium.org/buildbot/chromium/Mac/39586
    
    Original change's description:
    > Remove redundant code from RWHVCocoa, move some code to RWHVNSViewBridge
    > 
    > Remove suppressNextEscapeKeyUp_ because its functionality is made
    > redundant by keyDownCodes_. The escape-key-specific version was
    > introduced in crrev.com/165615 while the generic version was introduced
    > in crrev.com/304728.
    > 
    > While in the -[NSView keyEvent:] method, replace repeated method calls
    > with local variables in preparation for moving most of the funciton
    > across an IPC boundary, and fold shouldAutohideCursorForEvent into
    > the function (since it's called nowhere else).
    > 
    > Move SetBackgroundColor and Show/Hide/IsHidden functionality into
    > the RWHVNSViewBridge.
    > 
    > Bug: 821651
    > Change-Id: I31382a46b330fedbd3c4a6593ffafdb4a0b83be1
    > Reviewed-on: https://chromium-review.googlesource.com/974887
    > Commit-Queue: ccameron <ccameron@chromium.org>
    > Reviewed-by: Trent Apted <tapted@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#545160}
    
    TBR=tapted@chromium.org,ccameron@chromium.org
    
    Change-Id: Idff422824e86d1413026526c61334ac6ead4c3c5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 821651
    Reviewed-on: https://chromium-review.googlesource.com/976441
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545186}
    Former-commit-id: 0fb2b596b05350d741d727c42b02eacf089f892b

--- content/browser/renderer_host/render_widget_host_ns_view_bridge.mm
+++ content/browser/renderer_host/render_widget_host_ns_view_bridge.mm
@@ -4,13 +4,8 @@
 
 #import "content/browser/renderer_host/render_widget_host_ns_view_bridge.h"
 
-#import <Cocoa/Cocoa.h>
-
-#import "base/mac/scoped_nsobject.h"
 #import "content/browser/renderer_host/render_widget_host_view_cocoa.h"
 #include "content/browser/renderer_host/render_widget_host_view_mac.h"
-#import "skia/ext/skia_utils_mac.h"
-#import "ui/base/cocoa/animation_utils.h"
 
 namespace content {
 
@@ -23,55 +18,25 @@ class RenderWidgetHostViewNSViewBridgeLocal
     : public RenderWidgetHostNSViewBridge {
  public:
   explicit RenderWidgetHostViewNSViewBridgeLocal(
-      std::unique_ptr<RenderWidgetHostNSViewClient> client);
-  ~RenderWidgetHostViewNSViewBridgeLocal() override;
-  RenderWidgetHostViewCocoa* GetRenderWidgetHostViewCocoa() override;
+      std::unique_ptr<RenderWidgetHostNSViewClient> client) {
+    // Since we autorelease |cocoa_view|, our caller must put |GetNativeView()|
+    // into the view hierarchy right after calling us.
+    cocoa_view_ = [[[RenderWidgetHostViewCocoa alloc]
+        initWithClient:std::move(client)] autorelease];
+  }
+  ~RenderWidgetHostViewNSViewBridgeLocal() override {}
 
-  void SetBackgroundColor(SkColor color) override;
-  void SetVisible(bool visible) override;
+  RenderWidgetHostViewCocoa* GetRenderWidgetHostViewCocoa() override {
+    return cocoa_view_;
+  }
 
  private:
   // Weak, this is owned by |cocoa_view_|'s |client_|, and |cocoa_view_| owns
   // its |client_|.
   RenderWidgetHostViewCocoa* cocoa_view_ = nil;
 
-  // The background CoreAnimation layer which is hosted by |cocoa_view_|.
-  base::scoped_nsobject<CALayer> background_layer_;
-
   DISALLOW_COPY_AND_ASSIGN(RenderWidgetHostViewNSViewBridgeLocal);
 };
-
-RenderWidgetHostViewNSViewBridgeLocal::RenderWidgetHostViewNSViewBridgeLocal(
-    std::unique_ptr<RenderWidgetHostNSViewClient> client) {
-  // Since we autorelease |cocoa_view|, our caller must put |GetNativeView()|
-  // into the view hierarchy right after calling us.
-  cocoa_view_ = [[[RenderWidgetHostViewCocoa alloc]
-      initWithClient:std::move(client)] autorelease];
-
-  background_layer_.reset([[CALayer alloc] init]);
-  [cocoa_view_ setLayer:background_layer_];
-  [cocoa_view_ setWantsLayer:YES];
-}
-
-RenderWidgetHostViewNSViewBridgeLocal::
-    ~RenderWidgetHostViewNSViewBridgeLocal() {}
-
-RenderWidgetHostViewCocoa*
-RenderWidgetHostViewNSViewBridgeLocal::GetRenderWidgetHostViewCocoa() {
-  return cocoa_view_;
-}
-
-void RenderWidgetHostViewNSViewBridgeLocal::SetBackgroundColor(SkColor color) {
-  ScopedCAActionDisabler disabler;
-  base::ScopedCFTypeRef<CGColorRef> cg_color(
-      skia::CGColorCreateFromSkColor(color));
-  [background_layer_ setBackgroundColor:cg_color];
-}
-
-void RenderWidgetHostViewNSViewBridgeLocal::SetVisible(bool visible) {
-  ScopedCAActionDisabler disabler;
-  [cocoa_view_ setHidden:!visible];
-}
 
 }  // namespace
 
