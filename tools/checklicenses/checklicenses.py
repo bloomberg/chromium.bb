69c7f2feaedbe943085479ad7d9aa297bcf06d6f
Kent Tamura <tkent@chromium.org>
2018-03-22 07:05:07 +0000

    checklicenses.py: Add "UNKNOWN" entry for third_party/blink
    as well as third_party/WebKit because we'll move
    third_party/WebKit/{Source,common,public} to third_party/blink soon.
    
    Bug: 622551
    Change-Id: Ib916a70651318dadc333416d8ca49927bceba1cb
    Reviewed-on: https://chromium-review.googlesource.com/952946
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544995}
    Former-commit-id: 6eaf64fc5dfff9b820ed79cfb664487aff9cf7b1

--- tools/checklicenses/checklicenses.py
+++ tools/checklicenses/checklicenses.py
@@ -168,6 +168,8 @@ PATH_SPECIFIC_WHITELISTED_LICENSES = {
         'UNKNOWN',
     ],
 
+    # TODO(tkent): Remove this entry after the move is completed.
+    # crbug.com/622551
     'third_party/WebKit': [
         'UNKNOWN',
     ],
@@ -179,6 +181,10 @@ PATH_SPECIFIC_WHITELISTED_LICENSES = {
 
     # http://code.google.com/p/angleproject/issues/detail?id=217
     'third_party/angle': [
+        'UNKNOWN',
+    ],
+
+    'third_party/blink': [
         'UNKNOWN',
     ],
 
