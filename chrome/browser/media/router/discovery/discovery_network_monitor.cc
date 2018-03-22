d3bbf25681df4dc036d2bdd7b88d5ae23d251653
btolsch <btolsch@chromium.org>
2018-03-22 20:58:24 +0000

    Avoid shutdown hang in DiscoveryNetworkMonitor
    It has been observed that getifaddrs may hang during Chrome shutdown
    (most likely only Chrome OS shutdown).  This change adds the
    CONTINUE_ON_SHUTDOWN trait to the network query tasks posted by
    DiscoveryNetworkMonitor so shutdown won't hang.
    
    Bug: 814822
    Change-Id: If2273a1612ead7e94142f9684c9a6a04b272cf10
    Reviewed-on: https://chromium-review.googlesource.com/976045
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Commit-Queue: Brandon Tolsch <btolsch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545244}
    Former-commit-id: 454e04a8da1a8325166a6aa7dbad6f96e8965d5f

--- chrome/browser/media/router/discovery/discovery_network_monitor.cc
+++ chrome/browser/media/router/discovery/discovery_network_monitor.cc
@@ -14,6 +14,7 @@
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/task_runner_util.h"
+#include "base/task_scheduler/task_traits.h"
 #include "base/time/default_tick_clock.h"
 #include "chrome/browser/media/router/discovery/discovery_network_list.h"
 #include "chrome/browser/media/router/discovery/discovery_network_monitor_metric_observer.h"
@@ -94,7 +95,9 @@ DiscoveryNetworkMonitor::DiscoveryNetworkMonitor(NetworkInfoFunction strategy)
     : network_id_(kNetworkIdDisconnected),
       observers_(new base::ObserverListThreadSafe<Observer>(
           base::ObserverListPolicy::EXISTING_ONLY)),
-      task_runner_(base::CreateSequencedTaskRunnerWithTraits(base::MayBlock())),
+      task_runner_(base::CreateSequencedTaskRunnerWithTraits(
+          {base::MayBlock(),
+           base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN})),
       network_info_function_(strategy),
       metric_observer_(std::make_unique<DiscoveryNetworkMonitorMetricObserver>(
           base::DefaultTickClock::GetInstance(),
@@ -139,6 +142,9 @@ std::string DiscoveryNetworkMonitor::UpdateNetworkInfo() {
   auto network_info_list = network_info_function_();
   auto network_id = ComputeNetworkId(network_info_list);
 
+  // Although we are called with CONTINUE_ON_SHUTDOWN, none of these fields will
+  // disappear out from under us since |g_discovery_monitor| is declared with
+  // LeakyLazyInstanceTraits, and is therefore never deleted.
   network_id_.swap(network_id);
 
   if (network_id_ != network_id) {
