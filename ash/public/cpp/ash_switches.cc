0cc8bc84877f3424bfb711f4457c0746c2fb929c
MinChen <minch@chromium.org>
2018-03-22 17:32:01 +0000

    ash: Position power button menu next to power button.
    doc: go/add-power-button-location
    
    changes in this cl:
    - Add a switch kAshPowerButtonPosition with value to store the related
      power button position info that read at ChromeOS side.
    
    - Parse the value of the switch in PowerButtonController.
    
    - Get the menu bounds origins in different screen orientations according
      to the position info in PowerButtonMenuScreenView. This will help to
      set the bounds of the menu in each screen orientation.
    
    - Get the menu animation direction and transform distance according to the
      position info in PowerButtonMenuView. We need to minus the animation
      transform when setting the bounds of the menu before showing the menu.
    
    - Add PowerButtonControllerWithPositionTest*.
    
    
    Bug: 818331
    Change-Id: I0a1191072b85db59ed2d1d20e5ce25266a357e95
    Reviewed-on: https://chromium-review.googlesource.com/963644
    Commit-Queue: min c <minch@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Qiang Xu <warx@google.com>
    Cr-Commit-Position: refs/heads/master@{#545130}
    Former-commit-id: 03251dfbfdecd64835944357c5dd19eaab3147c7

--- ash/public/cpp/ash_switches.cc
+++ ash/public/cpp/ash_switches.cc
@@ -74,6 +74,17 @@ const char kAshEnableScaleSettingsTray[] = "ash-enable-scale-settings-tray";
 // Enables the stylus tools next to the status area.
 const char kAshForceEnableStylusTools[] = "force-enable-stylus-tools";
 
+// Power button position includes the power button's physical display side and
+// the percentage for power button center position to the display's
+// width/height in landscape_primary screen orientation. The value is a JSON
+// object containing a "position" property with the value "left", "right",
+// "top", or "bottom". For "left" and "right", a "y" property specifies the
+// button's center position as a fraction of the display's height (in [0.0,
+// 1.0]) relative to the top of the display. For "top" and "bottom", an "x"
+// property gives the position as a fraction of the display's width relative to
+// the left side of the display.
+const char kAshPowerButtonPosition[] = "ash-power-button-position";
+
 // Enables required things for the selected UI mode, regardless of whether the
 // Chromebook is currently in the selected UI mode.
 const char kAshUiMode[] = "force-tablet-mode";
--- ash/system/power/power_button_menu_screen_view.h
+++ ash/system/power/power_button_menu_screen_view.h
@@ -5,7 +5,11 @@
 #ifndef ASH_SYSTEM_POWER_POWER_BUTTON_MENU_SCREEN_VIEW_H_
 #define ASH_SYSTEM_POWER_POWER_BUTTON_MENU_SCREEN_VIEW_H_
 
+#include <unordered_map>
+
 #include "ash/ash_export.h"
+#include "ash/display/screen_orientation_controller_chromeos.h"
+#include "ash/system/power/power_button_controller.h"
 #include "base/macros.h"
 #include "ui/display/display_observer.h"
 #include "ui/views/view.h"
@@ -21,7 +25,9 @@ class ASH_EXPORT PowerButtonMenuScreenView : public views::View,
  public:
   // |show_animation_done| is a callback for when the animation that shows the
   // power menu has finished.
-  explicit PowerButtonMenuScreenView(
+  PowerButtonMenuScreenView(
+      PowerButtonController::PowerButtonPosition power_button_position,
+      double power_button_offset,
       base::RepeatingClosure show_animation_done);
   ~PowerButtonMenuScreenView() override;
 
@@ -47,10 +53,31 @@ class ASH_EXPORT PowerButtonMenuScreenView : public views::View,
   void OnDisplayMetricsChanged(const display::Display& display,
                                uint32_t changed_metrics) override;
 
+  // Lays out the view without animation transform.
+  void LayoutWithoutTransform();
+
+  // Initializes |menu_bounds_origins_| according to power button position info.
+  void InitializeMenuBoundsOrigins();
+
+  // Gets the bounds of power button menu.
+  gfx::Rect GetMenuBounds();
+
   // Created by PowerButtonMenuScreenView. Owned by views hierarchy.
   PowerButtonMenuView* power_button_menu_view_ = nullptr;
   PowerButtonMenuBackgroundView* power_button_screen_background_shield_ =
       nullptr;
+
+  // The physical display side of power button in landscape primary.
+  PowerButtonController::PowerButtonPosition power_button_position_;
+
+  // The center of the power button's offset from the top of the screen (for
+  // left/right) or left side of the screen (for top/bottom) in
+  // landscape_primary. Values are in [0.0, 1.0] and express a fraction of the
+  // display's height or width, respectively.
+  double power_button_offset_percentage_ = 0.f;
+
+  // The origin of the menu bounds in different screen orientations.
+  std::unordered_map<OrientationLockType, gfx::Point> menu_bounds_origins_;
 
   DISALLOW_COPY_AND_ASSIGN(PowerButtonMenuScreenView);
 };
