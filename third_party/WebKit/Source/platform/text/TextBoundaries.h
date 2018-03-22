75acbe92ed2d8c6a4189d6ac52c4b9083fd14d2b
Yoshifumi Inoue <yosin@chromium.org>
2018-03-22 10:03:34 +0000

    Introduce FindWord{Back,For}ward()
    This patch introduces |FindWordBackward()| and |FindWordForward()| for a
    preparation of the patch[1].
    
    [1] http://crrev.com/c/737981 Simplify word granularity handling
    
    Bug: 778507
    Change-Id: I405683cd07a0dc3a5f8cc26c307e1d980dcb2961
    Reviewed-on: https://chromium-review.googlesource.com/970191
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545030}
    Former-commit-id: 47080ffd267b8db3d5cd9633f91c6d99919cd4ba

--- third_party/WebKit/Source/platform/text/TextBoundaries.h
+++ third_party/WebKit/Source/platform/text/TextBoundaries.h
@@ -26,6 +26,8 @@
 #ifndef TextBoundaries_h
 #define TextBoundaries_h
 
+#include <utility>
+
 #include "platform/PlatformExport.h"
 #include "platform/wtf/text/Unicode.h"
 
@@ -42,6 +44,12 @@ PLATFORM_EXPORT int StartOfLastWordBoundaryContext(const UChar* characters,
 
 // |UChar*| should be a string in logical order instead of visual order, since
 // |FindWordBoundary()| uses ICU, which works on logical order strings
+PLATFORM_EXPORT std::pair<int, int> FindWordBackward(const UChar*,
+                                                     int len,
+                                                     int position);
+PLATFORM_EXPORT std::pair<int, int> FindWordForward(const UChar*,
+                                                    int len,
+                                                    int position);
 PLATFORM_EXPORT int FindWordStartBoundary(const UChar*, int len, int position);
 PLATFORM_EXPORT int FindWordEndBoundary(const UChar*, int len, int position);
 PLATFORM_EXPORT int FindNextWordBackward(const UChar*, int len, int position);
