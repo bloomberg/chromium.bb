bc430f684dd190aa8c3636c85aead7679dee7274
Moe Ahmadi <mahmadi@chromium.org>
2018-03-22 17:55:24 +0000

    Revert "ash: Position power button menu next to power button."
    This reverts commit 0cc8bc84877f3424bfb711f4457c0746c2fb929c [formerly 03251dfbfdecd64835944357c5dd19eaab3147c7].
    
    Reason for revert: Compile error caused Chrome build tree closure
    https://ci.chromium.org/buildbot/chromium.chromiumos/chromeos-daisy-rel/8564
    
    Original change's description:
    > ash: Position power button menu next to power button.
    > 
    > doc: go/add-power-button-location
    > 
    > changes in this cl:
    > - Add a switch kAshPowerButtonPosition with value to store the related
    >   power button position info that read at ChromeOS side.
    > 
    > - Parse the value of the switch in PowerButtonController.
    > 
    > - Get the menu bounds origins in different screen orientations according
    >   to the position info in PowerButtonMenuScreenView. This will help to
    >   set the bounds of the menu in each screen orientation.
    > 
    > - Get the menu animation direction and transform distance according to the
    >   position info in PowerButtonMenuView. We need to minus the animation
    >   transform when setting the bounds of the menu before showing the menu.
    > 
    > - Add PowerButtonControllerWithPositionTest*.
    > 
    > 
    > Bug: 818331
    > Change-Id: I0a1191072b85db59ed2d1d20e5ce25266a357e95
    > Reviewed-on: https://chromium-review.googlesource.com/963644
    > Commit-Queue: min c <minch@chromium.org>
    > Reviewed-by: Dan Erat <derat@chromium.org>
    > Reviewed-by: Qiang Xu <warx@google.com>
    > Cr-Commit-Position: refs/heads/master@{#545130}
    
    TBR=derat@chromium.org,minch@chromium.org,warx@google.com
    
    Change-Id: I9480d8cbf20c6ee0dd3970a88e6221badbc29906
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 818331
    Reviewed-on: https://chromium-review.googlesource.com/976097
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545146}
    Former-commit-id: 569b79f2b4adc3383cd96c642836bf068f472ff4

--- ash/system/power/power_button_menu_screen_view.cc
+++ ash/system/power/power_button_menu_screen_view.cc
@@ -4,11 +4,9 @@
 
 #include "ash/system/power/power_button_menu_screen_view.h"
 
-#include <utility>
-
 #include "ash/shell.h"
+#include "ash/system/power/power_button_controller.h"
 #include "ash/system/power/power_button_menu_view.h"
-#include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/layer_animation_observer.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
@@ -26,19 +24,7 @@ constexpr SkColor kShieldColor = SkColorSetARGBMacro(0xFF, 0x00, 0x00, 0x00);
 // Opacity of the power button menu fullscreen background shield.
 constexpr float kPowerButtonMenuOpacity = 0.6f;
 
-// TODO(minch): Get the internal display size instead if needed.
-// Gets the landscape size of the primary display. For landscape orientation,
-// the width is always larger than height.
-gfx::Size GetPrimaryDisplayLandscapeSize() {
-  gfx::Rect bounds = display::Screen::GetScreen()->GetPrimaryDisplay().bounds();
-  return gfx::Size(std::max(bounds.width(), bounds.height()),
-                   std::min(bounds.width(), bounds.height()));
-}
-
 }  // namespace
-
-using PowerButtonPosition = PowerButtonController::PowerButtonPosition;
-using TransformDirection = PowerButtonMenuView::TransformDirection;
 
 class PowerButtonMenuScreenView::PowerButtonMenuBackgroundView
     : public views::View,
@@ -86,21 +72,15 @@ class PowerButtonMenuScreenView::PowerButtonMenuBackgroundView
 };
 
 PowerButtonMenuScreenView::PowerButtonMenuScreenView(
-    PowerButtonPosition power_button_position,
-    double power_button_offset_percentage,
-    base::RepeatingClosure show_animation_done)
-    : power_button_position_(power_button_position),
-      power_button_offset_percentage_(power_button_offset_percentage) {
+    base::RepeatingClosure show_animation_done) {
   power_button_screen_background_shield_ =
       new PowerButtonMenuBackgroundView(show_animation_done);
   AddChildView(power_button_screen_background_shield_);
-  power_button_menu_view_ = new PowerButtonMenuView(power_button_position_);
+
+  power_button_menu_view_ = new PowerButtonMenuView();
   AddChildView(power_button_menu_view_);
 
   display::Screen::GetScreen()->AddObserver(this);
-
-  if (power_button_position_ != PowerButtonPosition::NONE)
-    InitializeMenuBoundsOrigins();
 }
 
 PowerButtonMenuScreenView::~PowerButtonMenuScreenView() {
@@ -113,16 +93,13 @@ void PowerButtonMenuScreenView::ScheduleShowHideAnimation(bool show) {
 }
 
 void PowerButtonMenuScreenView::Layout() {
-  power_button_screen_background_shield_->SetBoundsRect(GetContentsBounds());
+  const gfx::Rect contents_bounds = GetContentsBounds();
+  power_button_screen_background_shield_->SetBoundsRect(contents_bounds);
 
-  gfx::Rect menu_bounds = GetMenuBounds();
-  PowerButtonMenuView::TransformDisplacement transform_displacement =
-      power_button_menu_view_->GetTransformDisplacement();
-  if (transform_displacement.direction == TransformDirection::X)
-    menu_bounds.set_x(menu_bounds.x() - transform_displacement.distance);
-  else if (transform_displacement.direction == TransformDirection::Y)
-    menu_bounds.set_y(menu_bounds.y() - transform_displacement.distance);
-
+  // TODO(minch): Get the menu bounds according to the power button position.
+  gfx::Rect menu_bounds = contents_bounds;
+  menu_bounds.ClampToCenteredSize(power_button_menu_view_->GetPreferredSize());
+  menu_bounds.set_y(menu_bounds.y() - PowerButtonMenuView::kMenuViewTopPadding);
   power_button_menu_view_->SetBoundsRect(menu_bounds);
 }
 
@@ -145,135 +122,6 @@ void PowerButtonMenuScreenView::OnDisplayMetricsChanged(
     uint32_t changed_metrics) {
   GetWidget()->SetBounds(
       display::Screen::GetScreen()->GetPrimaryDisplay().bounds());
-
-  LayoutWithoutTransform();
-}
-
-void PowerButtonMenuScreenView::LayoutWithoutTransform() {
-  power_button_screen_background_shield_->SetBoundsRect(GetContentsBounds());
-  power_button_menu_view_->layer()->SetTransform(gfx::Transform());
-  power_button_menu_view_->SetBoundsRect(GetMenuBounds());
-}
-
-void PowerButtonMenuScreenView::InitializeMenuBoundsOrigins() {
-  // Power button position offset in pixels from the top when the button is at
-  // the left/right of the screen after rotation.
-  int left_power_button_y = 0, right_power_button_y = 0;
-
-  // Power button position offset in pixels from the left when the button is at
-  // the top/bottom of the screen after rotation.
-  int top_power_button_x = 0, bottom_power_button_x = 0;
-
-  // The screen orientation when the power button is at the
-  // left/right/top/bottom of the screen after rotation.
-  OrientationLockType left_screen_orientation, right_screen_orientation,
-      top_screen_orientation, bottom_screen_orientation;
-  const gfx::Size landscape_size = GetPrimaryDisplayLandscapeSize();
-  int display_width = landscape_size.width();
-  int display_height = landscape_size.height();
-
-  if (power_button_position_ == PowerButtonPosition::TOP ||
-      power_button_position_ == PowerButtonPosition::BOTTOM) {
-    std::swap(display_width, display_height);
-  }
-
-  int power_button_offset = display_height * power_button_offset_percentage_;
-  switch (power_button_position_) {
-    case PowerButtonPosition::LEFT:
-    case PowerButtonPosition::BOTTOM:
-      left_power_button_y = bottom_power_button_x = power_button_offset;
-      right_power_button_y = top_power_button_x =
-          display_height - power_button_offset;
-      break;
-    case PowerButtonPosition::RIGHT:
-    case PowerButtonPosition::TOP:
-      left_power_button_y = bottom_power_button_x =
-          display_height - power_button_offset;
-      right_power_button_y = top_power_button_x = power_button_offset;
-      break;
-    default:
-      NOTREACHED();
-      return;
-  }
-
-  switch (power_button_position_) {
-    case PowerButtonPosition::LEFT:
-      left_screen_orientation = OrientationLockType::kLandscapePrimary;
-      right_screen_orientation = OrientationLockType::kLandscapeSecondary;
-      top_screen_orientation = OrientationLockType::kPortraitPrimary;
-      bottom_screen_orientation = OrientationLockType::kPortraitSecondary;
-      break;
-    case PowerButtonPosition::RIGHT:
-      left_screen_orientation = OrientationLockType::kLandscapeSecondary;
-      right_screen_orientation = OrientationLockType::kLandscapePrimary;
-      top_screen_orientation = OrientationLockType::kPortraitSecondary;
-      bottom_screen_orientation = OrientationLockType::kPortraitPrimary;
-      break;
-    case PowerButtonPosition::TOP:
-      left_screen_orientation = OrientationLockType::kPortraitSecondary;
-      right_screen_orientation = OrientationLockType::kPortraitPrimary;
-      top_screen_orientation = OrientationLockType::kLandscapePrimary;
-      bottom_screen_orientation = OrientationLockType::kLandscapeSecondary;
-      break;
-    case PowerButtonPosition::BOTTOM:
-      left_screen_orientation = OrientationLockType::kPortraitPrimary;
-      right_screen_orientation = OrientationLockType::kPortraitSecondary;
-      top_screen_orientation = OrientationLockType::kLandscapeSecondary;
-      bottom_screen_orientation = OrientationLockType::kLandscapePrimary;
-      break;
-    default:
-      NOTREACHED();
-      return;
-  }
-
-  const gfx::Size menu_size = power_button_menu_view_->GetPreferredSize();
-  // Power button position offset from the left when the button is at the left
-  // is always zero.
-  menu_bounds_origins_.insert(std::make_pair(
-      left_screen_orientation,
-      gfx::Point(PowerButtonMenuView::kMenuViewTransformDistanceDp,
-                 left_power_button_y - menu_size.height() / 2)));
-
-  menu_bounds_origins_.insert(std::make_pair(
-      right_screen_orientation,
-      gfx::Point(display_width -
-                     PowerButtonMenuView::kMenuViewTransformDistanceDp -
-                     menu_size.width(),
-                 right_power_button_y - menu_size.height() / 2)));
-
-  // Power button position offset from the top when the button is at the top
-  // is always zero.
-  menu_bounds_origins_.insert(std::make_pair(
-      top_screen_orientation,
-      gfx::Point(top_power_button_x - menu_size.width() / 2,
-                 PowerButtonMenuView::kMenuViewTransformDistanceDp)));
-
-  menu_bounds_origins_.insert(std::make_pair(
-      bottom_screen_orientation,
-      gfx::Point(bottom_power_button_x - menu_size.width() / 2,
-                 display_width -
-                     PowerButtonMenuView::kMenuViewTransformDistanceDp -
-                     menu_size.height())));
-}
-
-gfx::Rect PowerButtonMenuScreenView::GetMenuBounds() {
-  gfx::Rect menu_bounds;
-
-  if (power_button_position_ == PowerButtonPosition::NONE ||
-      !Shell::Get()
-           ->tablet_mode_controller()
-           ->IsTabletModeWindowManagerEnabled()) {
-    menu_bounds = GetContentsBounds();
-    menu_bounds.ClampToCenteredSize(
-        power_button_menu_view_->GetPreferredSize());
-  } else {
-    menu_bounds.set_origin(
-        menu_bounds_origins_[Shell::Get()
-                                 ->screen_orientation_controller()
-                                 ->GetCurrentOrientation()]);
-    menu_bounds.set_size(power_button_menu_view_->GetPreferredSize());
-  }
-  return menu_bounds;
 }
 
 }  // namespace ash
