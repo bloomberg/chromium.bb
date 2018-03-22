564fc3c3b88ae5eeca5b288e741517179a394199
Yafei Duan <romax@chromium.org>
2018-03-22 06:58:23 +0000

    [Offline Pages] Improving ArchiveManager and storage stats.
    In this CL:
    - Removed unused methods from ArchiveManager.
    - Reporting more storage stats from ArchiveManager, for multiple archive
      directories.
    - Fixed multiple tests related.
    
    There will be a followup change based on this one for reporting storage
    related UMAs from OfflinePageModelTaskified.
    
    Bug: NONE
    Change-Id: I8f9d2837b4c47a1f88fd380c86f63d0f5340c6e0
    Reviewed-on: https://chromium-review.googlesource.com/967561
    Commit-Queue: Yafei Duan <romax@chromium.org>
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#544993}
    Former-commit-id: 5c2da0422338ed06ad5ec192e530d3f25554abc2

--- components/offline_pages/core/archive_manager.h
+++ components/offline_pages/core/archive_manager.h
@@ -5,9 +5,6 @@
 #ifndef COMPONENTS_OFFLINE_PAGES_CORE_ARCHIVE_MANAGER_H_
 #define COMPONENTS_OFFLINE_PAGES_CORE_ARCHIVE_MANAGER_H_
 
-#include <set>
-#include <vector>
-
 #include "base/callback_forward.h"
 #include "base/files/file_path.h"
 #include "base/memory/ref_counted.h"
@@ -22,23 +19,30 @@ namespace offline_pages {
 // different archive directories based on their lifetime types (persistent or
 // temporary).
 // All tasks are performed using |task_runner_|.
-// TODO(romax): When P2P sharing comes and pages saved in internal storage get
-// moved out to external storage, make sure nothing breaks (and it's necessary
-// to check if both internal/external are on the volume, in case any logic
-// breaks.)
 class ArchiveManager {
  public:
-  // Used by metrics collection and clearing storage of temporary pages. The
-  // |free_disk_space| will be the free disk space of the volume that contains
-  // temporary archives directory. Another field may be added if the free disk
-  // space of persistent archives is needed in future.
+  // Used by metrics collection and clearing storage of temporary pages.
+  // - |internal_free_disk_space| is the free space of the volume which
+  //   contains temporary and private archive directories.
+  // - |external_free_disk_space| is the free space of the volume which contains
+  //   public archives directory, in most cases it should be Download directory
+  //   in /sdcard/.
+  // - |{temporary/private}_archives_size| is the size of the directory. Since
+  //   these are inside app directory and are fully controlled, it's unnecessary
+  //   to calculate the size by iterating and counting.
+  // - |public_archives_size| will enumerate all mhtml files in the public
+  //   directory and records the total file size. This will include mhtml files
+  //   shared into the public directory through other approaches.
   struct StorageStats {
     int64_t total_archives_size() const {
-      return temporary_archives_size + private_archives_size;
+      return temporary_archives_size + private_archives_size +
+             public_archives_size;
     }
-    int64_t free_disk_space;
+    int64_t internal_free_disk_space;
+    int64_t external_free_disk_space;
     int64_t temporary_archives_size;
     int64_t private_archives_size;
+    int64_t public_archives_size;
   };
 
   ArchiveManager(const base::FilePath& temporary_archives_dir,
@@ -48,35 +52,12 @@ class ArchiveManager {
   virtual ~ArchiveManager();
 
   // Creates archives directory if one does not exist yet;
-  virtual void EnsureArchivesDirCreated(const base::Closure& callback);
+  virtual void EnsureArchivesDirCreated(base::OnceCallback<void()> callback);
 
-  // Checks whether an archive with specified |archive_path| exists.
-  virtual void ExistsArchive(const base::FilePath& archive_path,
-                             const base::Callback<void(bool)>& callback);
-
-  // Deletes an archive with specified |archive_path|.
-  // It is considered successful to attempt to delete a file that does not
-  // exist.
-  virtual void DeleteArchive(const base::FilePath& archive_path,
-                             const base::Callback<void(bool)>& callback);
-
-  // Deletes multiple archives that are specified in |archive_paths|.
-  // It is considered successful to attempt to delete a file that does not
-  // exist.
-  virtual void DeleteMultipleArchives(
-      const std::vector<base::FilePath>& archive_paths,
-      const base::Callback<void(bool)>& callback);
-
-  // Lists all archive files in both temporary and persistent archive
-  // directories.
-  virtual void GetAllArchives(
-      const base::Callback<void(const std::set<base::FilePath>&)>& callback)
-      const;
-
-  // Gets stats about archive storage, i.e. sizes of temporary and persistent
-  // archive files and free disk space.
+  // Gets stats about archive storage, i.e. sizes of all archive directories
+  // and free disk spaces.
   virtual void GetStorageStats(
-      const base::Callback<void(const StorageStats& storage_sizes)>& callback)
+      base::OnceCallback<void(const StorageStats& storage_sizes)> callback)
       const;
 
   // Gets the archive directories.
@@ -85,6 +66,7 @@ class ArchiveManager {
   const base::FilePath& GetPublicArchivesDir() const;
 
  protected:
+  // Used for testing.
   ArchiveManager();
 
  private:
