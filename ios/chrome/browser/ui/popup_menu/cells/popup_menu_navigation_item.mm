141c1be04456a27f65c6d4d872225b08588c677b
Gauthier Ambard <gambard@chromium.org>
2018-03-23 13:49:20 +0000

    Change the sizing of popup menus
    If scroll views are presented in the popup menu, the size of the view
    needs to be set directly, as it cannot rely on its internal element to
    size its outside view.
    This CL adds a method to get the ideal size for the elements in the
    table view, allowing to have a table view with a minimal width.
    
    Bug: 821856
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I4d0c56fbc9897e604526fd1ae5ccc29c801506a4
    Reviewed-on: https://chromium-review.googlesource.com/973444
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545420}
    Former-commit-id: 8e636ba3f5ee24fee06066e8ab82503dd309d2a2

--- ios/chrome/browser/ui/popup_menu/cells/popup_menu_navigation_item.mm
+++ ios/chrome/browser/ui/popup_menu/cells/popup_menu_navigation_item.mm
@@ -4,6 +4,8 @@
 
 #import "ios/chrome/browser/ui/popup_menu/cells/popup_menu_navigation_item.h"
 
+#include "base/logging.h"
+
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
@@ -18,6 +20,14 @@
     self.cellClass = [PopupMenuNavigationCell class];
   }
   return self;
+}
+
+#pragma mark - PopupMenuItem
+
+- (CGSize)cellSizeForWidth:(CGFloat)width {
+  // TODO(crbug.com/804779): implement this.
+  NOTREACHED();
+  return CGSizeZero;
 }
 
 @end
