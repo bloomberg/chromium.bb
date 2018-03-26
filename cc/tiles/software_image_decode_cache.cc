5c41098895b4383c58b92b7e8d1ca52468254712
Khushal <khushalsagar@chromium.org>
2018-03-26 04:10:20 +0000

    cc: Add some diagnostic CHECKs for sw image cache crashes.
    Change a few DCHECKs to CHECKs to validate if some crashes are
    resulting from using unlocked images, or incorrectly unlocking them
    earlier.
    
    R=enne@chromium.org, ericrk@chromium.org
    
    Bug: 802976
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ibe0b9ef6ac52e6fafc9fd2670e20a9eefac17234
    Reviewed-on: https://chromium-review.googlesource.com/979160
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545724}
    Former-commit-id: fb48059ccdfe83de78d14b889cf734668ded071b

--- cc/tiles/software_image_decode_cache.cc
+++ cc/tiles/software_image_decode_cache.cc
@@ -303,7 +303,8 @@ void SoftwareImageDecodeCache::UnrefImage(const CacheKey& key) {
   auto decoded_image_it = decoded_images_.Peek(key);
   DCHECK(decoded_image_it != decoded_images_.end());
   auto* entry = decoded_image_it->second.get();
-  DCHECK_GT(entry->ref_count, 0);
+  // TODO(khushalsagar): Temp CHECK to diagnose crbug.com/802976.
+  CHECK_GT(entry->ref_count, 0);
   if (--entry->ref_count == 0) {
     if (entry->is_budgeted)
       RemoveBudgetForImage(key, entry);
