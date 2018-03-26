33146a20418aff04ca926680f32f793ccfb72c0b
Jacob Kopczynski <jkop@google.com>
2018-03-26 10:35:22 -0700

    Wall-clock time and dashboard are no longer new.
    BUG=None
    TEST=None
    
    Change-Id: I7d0c713c3e007528f9c94ed59243136a198849be
    Reviewed-on: https://chromium-review.googlesource.com/981109
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Jacob Kopczynski <jkop@chromium.org>
    Reviewed-by: Aviv Keshet <akeshet@chromium.org>

--- scripts/summarize_build_stats.py
+++ scripts/summarize_build_stats.py
@@ -622,13 +622,13 @@ The pre-CQ <b>rejected {bad_cl_precq_rejected} changes this week</b>, which woul
 
 <h2>CL handling times</h2>
 <p>
-(New!) See also <a href="http://shortn/_cN4FAtq4sP">CQ Weekly Summary Dashboard</a>.<br>
+See also <a href="http://shortn/_cN4FAtq4sP">CQ Weekly Summary Dashboard</a>.<br>
 The CL handling time was <b>{cl_handling_time_50:.2f} hours</b> 50%ile <b>{cl_handling_time_90:.2f} hours</b> 90%ile <a href="http://shortn/_DfXfGnzqow">history</a>.<br>
 Time spent in the CQ was <b>{cq_time_50:.2f} hours</b> 50%ile <b>{cq_time_90:.2f} hours</b> 90%ile <a href="http://shortn/_2yYTGswEKh">history</a>.<br>
 Time spent waiting was <b>{wait_time_50:.2f} hours</b> 50%ile <b>{wait_time_90:.2f} hours</b> 90%ile <a href="http://shortn/_e9vKXdSOjH">history</a>.<br>
 CQ run time was <b>{cq_run_time_50:.2f} hours</b> 50%ile <b>{cq_run_time_90:.2f} hours</b> 90%ile <a href="http://shortn/_STokpvKTaS">history</a>.<br>
 Pre-cq times available on <a href="http://shortn/_ZP8ivoYPUZ">monarch history</a>.<br>
-(New!) Wall-clock times times available on <a href="http://shortn/_qXixBiSDUA">monarch history</a>.<br>
+Wall-clock times times available on <a href="http://shortn/_qXixBiSDUA">monarch history</a>.<br>
 </p>
 
 <h2>Slowest Passing Slaves</h2>
