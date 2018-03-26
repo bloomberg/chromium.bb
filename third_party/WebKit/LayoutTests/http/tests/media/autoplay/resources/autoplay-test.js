f398e335a0559209aeb07ef14cff9500e95830f0
Becca Hughes <beccahughes@chromium.org>
2018-03-26 15:53:20 +0000

    Autoplay: Harden and switch layout tests back on
    Harden feature policy layout tests and switch them back on.
    
    These tests are still disabled with site isolation until
    HasBeenActivated is replicated.
    
    BUG=794631,790549
    
    Change-Id: I8fd2be1f9222c71288d5e39e1639c4d990df995b
    Reviewed-on: https://chromium-review.googlesource.com/966069
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545804}
    Former-commit-id: e3b44520f3132325defac71697871d328e28b890

--- third_party/WebKit/LayoutTests/http/tests/media/autoplay/resources/autoplay-test.js
+++ third_party/WebKit/LayoutTests/http/tests/media/autoplay/resources/autoplay-test.js
@@ -3,9 +3,10 @@
  */
 
 // Tracks the results and how many active tests we have running.
-let activeTests;
 let testExpectations = {};
 let callback;
+let resultCallback = (data) => { top.postMessage(data, '*') };
+let completedTestResults = [];
 
 function tearDown(result) {
   // Reset the flag state.
@@ -31,11 +32,20 @@ function receivedResult(data) {
     return;
   }
 
-  activeTests--;
-  assert_equals(testExpectations[data.url], data.message);
+  completedTestResults.push(data);
+  processTestResults();
+}
 
-  if (!activeTests)
-    callback();
+function processTestResults() {
+  // Check if we have completed all the tests.
+  if (Object.keys(testExpectations).length != completedTestResults.length)
+    return;
+
+  completedTestResults.forEach((data) => {
+    assert_equals(testExpectations[data.url], data.message);
+  });
+
+  callback();
 }
 
 function runVideoTest() {
@@ -44,15 +54,15 @@ function runVideoTest() {
   video.play().then(tearDown, tearDown);
 }
 
-function simulateViewportClick() {
+function simulateViewportClick(callback) {
   chrome.gpuBenchmarking.pointerActionSequence([
       {"source": "mouse",
        "actions": [
        { "name": "pointerDown", "x": 0, "y": 0 },
-       { "name": "pointerUp" } ]}]);
+       { "name": "pointerUp" } ]}], callback);
 }
 
-function simulateFrameClick() {
+function simulateFrameClick(callback) {
   const frame = document.getElementsByTagName('iframe')[0];
   const rect = frame.getBoundingClientRect();
 
@@ -64,32 +74,42 @@ function simulateFrameClick() {
          "x": rect.left + (rect.width / 2),
          "y": rect.top + (rect.height / 2)
        },
-       { "name": "pointerUp" } ]}]);
+       { "name": "pointerUp" } ]}], callback);
 }
 
-function runTest(expectations) {
+function simulateNoGesture(callback) {
+  callback();
+}
+
+function runTest(pointerSequence, expectations) {
+  // Setup the global variables.
+  expectations.forEach((expectation) => {
+    testExpectations[expectation[0]] = expectation[1];
+  });
+
+  // Run the test.
   async_test((t) => {
-    testExpectations = expectations;
-    activeTests = Object.keys(expectations).length;
     callback = t.step_func_done();
+
+    // Fire the pointer sequence and then run the video test.
+    pointerSequence(t.step_func(() => {
+      runVideoTest();
+
+      // Navigate the iframe now we have the gesture.
+      document.getElementsByTagName('iframe')[0].src =
+          expectations[1][0];
+    }));
+
+    resultCallback = t.step_func(receivedResult);
   });
 }
 
-window.addEventListener('load', () => {
-  if (!window.testRunner)
-    return;
+// Setup the flags before the test is run.
+window.internals.settings.setAutoplayPolicy('document-user-activation-required');
 
-  // Setup the flags before the test is run.
-  window.internals.settings.setAutoplayPolicy('document-user-activation-required');
+// Setup the event listener to forward messages.
+window.addEventListener('message', (e) => { resultCallback(e.data); });
 
-  // Setup the event listener to forward messages.
-  window.addEventListener('message', (e) => {
-    if (callback) {
-      receivedResult(e.data);
-    } else {
-      top.postMessage(e.data, '*');
-    }
-  });
-
+// If we are on an iframe then run the video test automatically.
+if (window.self !== window.top)
   runVideoTest();
-}, { once: true });
