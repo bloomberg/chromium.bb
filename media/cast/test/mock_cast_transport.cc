90d9da468d55430531f2e6b8108213e9c77bad44
Xiangjun Zhang <xjz@chromium.org>
2018-03-27 21:41:15 +0000

    Mirroring service: Add rtp stream implementation.
    Add RtpStream for the Mirroring service, which receives audio/video
    data from the capturer and sends the data to media::cast::CastSender.
    
    Bug: 734672
    Change-Id: I8bf5813681463c1110458bf055211c3a233dc939
    Reviewed-on: https://chromium-review.googlesource.com/967548
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Commit-Queue: Xiangjun Zhang <xjz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546271}
    Former-commit-id: d3b390cf40fa9295ebc37eb5d8f2d3e2f598264e

--- media/cast/test/mock_cast_transport.cc
+++ media/cast/test/mock_cast_transport.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "media/cast/net/mock_cast_transport.h"
+#include "media/cast/test/mock_cast_transport.h"
 
 namespace media {
 namespace cast {
