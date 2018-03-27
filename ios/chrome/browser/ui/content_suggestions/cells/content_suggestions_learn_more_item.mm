32abfa7018595ab04c6153d65ecaf0ae9947b504
Justin Cohen <justincohen@google.com>
2018-03-27 13:51:44 +0000

    [ios] More tweaks to content suggestions icons, font and blur.
    Bug: 819223
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I41edb5310f0529f9d770f5b415cd6f5179a89d1a
    Reviewed-on: https://chromium-review.googlesource.com/980672
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546098}
    Former-commit-id: 9adb38cd6cc59e9612dbd864dd49cdaf785e695f

--- ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_learn_more_item.mm
+++ ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_learn_more_item.mm
@@ -112,8 +112,11 @@ const int kLinkColorRGB = 0x5595FE;
 + (void)configureLabel:(UILabel*)label withText:(NSString*)text {
   label.numberOfLines = 0;
   label.textColor = [[MDCPalette greyPalette] tint700];
-  label.font = [MDCTypography italicFontFromFont:[MDCTypography captionFont]];
-
+  if (IsUIRefreshPhase1Enabled()) {
+    label.font = [UIFont preferredFontForTextStyle:UIFontTextStyleFootnote];
+  } else {
+    label.font = [MDCTypography italicFontFromFont:[MDCTypography captionFont]];
+  }
   NSRange linkRange;
   NSString* strippedText = ParseStringWithLink(text, &linkRange);
   DCHECK_NE(NSNotFound, static_cast<NSInteger>(linkRange.location));
