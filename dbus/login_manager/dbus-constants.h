3290a8e598f2bd2aecac0fbb25fc410b04d6b3c4
Toni Barzic <tbarzic@chromium.org>
2018-03-26 18:17:04 -0700

    Add EmitAshInitialized method to dbus constants
    BUG=chromium:818057
    TEST=None
    
    Change-Id: I848d5c4cd995bc318d7d0fc6ecdfe26c9602e8c3
    Reviewed-on: https://chromium-review.googlesource.com/982792
    Commit-Ready: Toni Barzic <tbarzic@chromium.org>
    Tested-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>

--- dbus/login_manager/dbus-constants.h
+++ dbus/login_manager/dbus-constants.h
@@ -11,6 +11,7 @@ const char kSessionManagerServicePath[] = "/org/chromium/SessionManager";
 const char kSessionManagerServiceName[] = "org.chromium.SessionManager";
 // Methods
 const char kSessionManagerEmitLoginPromptVisible[] = "EmitLoginPromptVisible";
+const char kSessionManagerEmitAshInitialized[] = "EmitAshInitialized";
 const char kSessionManagerEnableChromeTesting[] = "EnableChromeTesting";
 const char kSessionManagerSaveLoginPassword[] = "SaveLoginPassword";
 const char kSessionManagerStartSession[] = "StartSession";
