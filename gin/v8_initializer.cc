60d5ee25f6ff9e3b4d577745be7a74c68c753d46
Ross McIlroy <rmcilroy@chromium.org>
2018-03-27 17:06:02 +0000

    [Gin] Add feature to control optimization in V8.
    Change-Id: I7ff3a987d132d4ca8e0f350e851020b53d4fb009
    Reviewed-on: https://chromium-review.googlesource.com/980947
    Commit-Queue: Ross McIlroy <rmcilroy@chromium.org>
    Reviewed-by: Mythri Alle <mythria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546143}
    Former-commit-id: 45e9500ab86e13dce04a032e4a73cda72fa103d1

--- gin/v8_initializer.cc
+++ gin/v8_initializer.cc
@@ -25,6 +25,7 @@
 #include "base/threading/platform_thread.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
+#include "gin/gin_features.h"
 
 #if defined(V8_USE_EXTERNAL_STARTUP_DATA)
 #if defined(OS_ANDROID)
@@ -241,6 +242,14 @@ void V8Initializer::Initialize(IsolateHolder::ScriptMode mode,
     return;
 
   v8::V8::InitializePlatform(V8Platform::Get());
+
+  if (base::FeatureList::IsEnabled(features::kV8OptimizeJavascript)) {
+    static const char optimize[] = "--opt";
+    v8::V8::SetFlagsFromString(optimize, sizeof(optimize) - 1);
+  } else {
+    static const char no_optimize[] = "--no-opt";
+    v8::V8::SetFlagsFromString(no_optimize, sizeof(no_optimize) - 1);
+  }
 
   if (IsolateHolder::kStrictMode == mode) {
     static const char use_strict[] = "--use_strict";
