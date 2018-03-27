ae393b54b128817aa90a73a55cdb9b72106653bd
Matt Mueller <mattm@chromium.org>
2018-03-27 21:36:22 +0000

    Delete unused CertVerifyResult::operator== method.
    Change-Id: I53465dd47dbfbe7db46024f217e9372764cc049b
    Reviewed-on: https://chromium-review.googlesource.com/982381
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546269}
    Former-commit-id: 24f513c158eb235e4bb2364e437c75b10e1d8e4c

--- net/cert/cert_verify_result.h
+++ net/cert/cert_verify_result.h
@@ -26,8 +26,6 @@ class NET_EXPORT CertVerifyResult {
 
   void Reset();
 
-  bool operator==(const CertVerifyResult& other) const;
-
   // The certificate chain that was constructed during verification.
   //
   // Note: Although |verified_cert| will match the originally supplied
