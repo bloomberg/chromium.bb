cd2fa1bc3cd3b608663acef1c7d53cf4ce97b7ef
Scott Violet <sky@chromium.org>
2018-03-27 03:08:56 +0000

    Rename buildflag_headers for consistency
    This updates ios.
    
    Targets go from foo_features to foo_buildflags
    And similarly the header now has buildflags in the name.
    
    BUG=814485
    TEST=covered by build
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I7a59eb0e91b3813855e79352a6146f4d1410c85a
    Reviewed-on: https://chromium-review.googlesource.com/981322
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#545932}
    Former-commit-id: d9ec3a6d0f8e60ca6390ae6f0d5c0114f273f4fc

--- ios/chrome/browser/ui/qr_scanner/camera_controller.mm
+++ ios/chrome/browser/ui/qr_scanner/camera_controller.mm
@@ -7,7 +7,7 @@
 #include "base/logging.h"
 #include "base/mac/foundation_util.h"
 #include "base/strings/stringprintf.h"
-#include "ios/chrome/common/ios_app_bundle_id_prefix.h"
+#include "ios/chrome/common/ios_app_bundle_id_prefix_buildflags.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
