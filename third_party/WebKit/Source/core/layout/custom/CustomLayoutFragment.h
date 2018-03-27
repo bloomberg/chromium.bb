7b8e0412d65538b88c1cadc8366d300c8bf6a531
Ian Kilpatrick <ikilpatrick@chromium.org>
2018-03-27 04:55:48 +0000

    [css-layout-api] Allow developers to position fragments.
    This implements the LayoutFragment.inlineOffset and
    LayoutFragment.blockOffset attributes.
    
    The tests added simply re-create the reference by setting these two
    attributes in different text directions and writing modes.
    
    Change-Id: I1865403ca12e3b174738ee93320eae5ba16ac292
    Bug: 726125
    Reviewed-on: https://chromium-review.googlesource.com/971832
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546018}
    Former-commit-id: 5aba54d93514567262df30d0dbba0cee712f6da5

--- third_party/WebKit/Source/core/layout/custom/CustomLayoutFragment.h
+++ third_party/WebKit/Source/core/layout/custom/CustomLayoutFragment.h
@@ -37,6 +37,12 @@ class CustomLayoutFragment : public ScriptWrappable {
   double inlineSize() const { return inline_size_; }
   double blockSize() const { return block_size_; }
 
+  double inlineOffset() const { return inline_offset_; }
+  double blockOffset() const { return block_offset_; }
+
+  void setInlineOffset(double inline_offset) { inline_offset_ = inline_offset; }
+  void setBlockOffset(double block_offset) { block_offset_ = block_offset; }
+
   LayoutBox* GetLayoutBox() const;
   bool IsValid() const;
 
@@ -59,6 +65,10 @@ class CustomLayoutFragment : public ScriptWrappable {
   // The inline and block size on this object should never change.
   const double inline_size_;
   const double block_size_;
+
+  // The offset is relative to our parent, and in the parent's writing mode.
+  double inline_offset_ = 0;
+  double block_offset_ = 0;
 };
 
 }  // namespace blink
