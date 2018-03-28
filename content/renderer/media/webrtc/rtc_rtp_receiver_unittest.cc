dfa753fbdf4dcd4b09e8c09f6ae3d11975668f83
Henrik Boström <hbos@chromium.org>
2018-03-28 19:27:12 +0000

    Refactor content::RTCRtpReceiver and add unittests.
    RTCRtpReceiver is a thin content layer wrapper for a webrtc layer
    receiver. It is passed around with std::unique_ptr<> ownership (it is an
    implementation of WebRTCRtpReceiver which is not reference counted).
    It is possible for multiple RTCRtpReceivers to wrap around the same
    webrtc layer receiver.
    
    This ownership is limiting:
    - Because wrappers are independent of one another even if they represent
      the same webrtc layer receiver, it is not possible to add any content
      layer states without worrying about different wrappers becoming
      out-of-sync.
    - Because each wrapper uses std::unique_ptr<> it is not possible to add
      asynchronous methods to it without worrying that the RTCRtpReceiver is
      destroyed before the async op is finished.
    
    This CL solves these problems by moving all of RTCRtpReceiver's
    functionality into an internal ref-counted class RTCRtpReceiverInternal,
    alling multiple wrappers to reference the same internal state and
    allowing async operations to keep RTCRtpReceiverInternal alive with
    references. (Same design as content::RTCRtpSender[Internal].)
    
    This CL also adds rtc_rtp_receiver_unittest.cc.
    
    The motivation for this CL is wanting to add async method getStats() to
    RTCRtpReceiver and to test it in content_unittests. This will be done in
    a follow-up CL.
    
    TBR=guidou@chromium.org
    
    Bug: 680172
    Change-Id: Ic74b08b5bc6f1f2a148be36f1f0ebef60ec80757
    Reviewed-on: https://chromium-review.googlesource.com/975567
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546563}
    Former-commit-id: 3628cfd2c1805650f0ba5ebd805cdd6315ab263f

---
+++ content/renderer/media/webrtc/rtc_rtp_receiver_unittest.cc
@@ -0,0 +1,117 @@
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/renderer/media/webrtc/rtc_rtp_receiver.h"
+
+#include <memory>
+
+#include "base/logging.h"
+#include "base/memory/ref_counted.h"
+#include "base/message_loop/message_loop.h"
+#include "base/run_loop.h"
+#include "base/single_thread_task_runner.h"
+#include "base/synchronization/waitable_event.h"
+#include "content/child/child_process.h"
+#include "content/renderer/media/webrtc/mock_peer_connection_dependency_factory.h"
+#include "content/renderer/media/webrtc/test/webrtc_stats_report_obtainer.h"
+#include "content/renderer/media/webrtc/webrtc_media_stream_adapter_map.h"
+#include "content/renderer/media/webrtc/webrtc_media_stream_track_adapter_map.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/WebKit/public/platform/WebRTCStats.h"
+#include "third_party/WebKit/public/platform/WebString.h"
+#include "third_party/WebKit/public/platform/scheduler/test/renderer_scheduler_test_support.h"
+#include "third_party/WebKit/public/web/WebHeap.h"
+#include "third_party/webrtc/api/stats/rtcstats_objects.h"
+#include "third_party/webrtc/api/stats/rtcstatsreport.h"
+#include "third_party/webrtc/api/test/mock_rtpreceiver.h"
+
+namespace content {
+
+class RTCRtpReceiverTest : public ::testing::Test {
+ public:
+  void SetUp() override {
+    dependency_factory_.reset(new MockPeerConnectionDependencyFactory());
+    main_thread_ = blink::scheduler::GetSingleThreadTaskRunnerForTesting();
+    stream_map_ = new WebRtcMediaStreamAdapterMap(
+        dependency_factory_.get(), main_thread_,
+        new WebRtcMediaStreamTrackAdapterMap(dependency_factory_.get(),
+                                             main_thread_));
+  }
+
+  void TearDown() override { blink::WebHeap::CollectAllGarbageForTesting(); }
+
+  std::unique_ptr<RTCRtpReceiver> CreateReceiver(
+      scoped_refptr<webrtc::MediaStreamTrackInterface> webrtc_track) {
+    std::unique_ptr<WebRtcMediaStreamTrackAdapterMap::AdapterRef> track_adapter;
+    base::RunLoop run_loop;
+    dependency_factory_->GetWebRtcSignalingThread()->PostTask(
+        FROM_HERE,
+        base::BindOnce(&RTCRtpReceiverTest::CreateReceiverOnSignalingThread,
+                       base::Unretained(this), std::move(webrtc_track),
+                       base::Unretained(&track_adapter),
+                       base::Unretained(&run_loop)));
+    run_loop.Run();
+    DCHECK(mock_webrtc_receiver_);
+    DCHECK(track_adapter);
+    return std::make_unique<RTCRtpReceiver>(
+        mock_webrtc_receiver_.get(), std::move(track_adapter),
+        std::vector<
+            std::unique_ptr<WebRtcMediaStreamAdapterMap::AdapterRef>>());
+  }
+
+ protected:
+  void CreateReceiverOnSignalingThread(
+      scoped_refptr<webrtc::MediaStreamTrackInterface> webrtc_track,
+      std::unique_ptr<WebRtcMediaStreamTrackAdapterMap::AdapterRef>*
+          track_adapter,
+      base::RunLoop* run_loop) {
+    mock_webrtc_receiver_ =
+        new rtc::RefCountedObject<webrtc::MockRtpReceiver>();
+    *track_adapter =
+        stream_map_->track_adapter_map()->GetOrCreateRemoteTrackAdapter(
+            webrtc_track);
+    run_loop->Quit();
+  }
+
+  // Message loop and child processes is needed for task queues and threading to
+  // work, as is necessary to create tracks and adapters.
+  base::MessageLoop message_loop_;
+  ChildProcess child_process_;
+
+  std::unique_ptr<MockPeerConnectionDependencyFactory> dependency_factory_;
+  scoped_refptr<base::SingleThreadTaskRunner> main_thread_;
+  scoped_refptr<WebRtcMediaStreamAdapterMap> stream_map_;
+  rtc::scoped_refptr<webrtc::MockRtpReceiver> mock_webrtc_receiver_;
+  std::unique_ptr<RTCRtpReceiver> receiver_;
+};
+
+TEST_F(RTCRtpReceiverTest, CreateReceiver) {
+  scoped_refptr<MockWebRtcAudioTrack> webrtc_track =
+      MockWebRtcAudioTrack::Create("webrtc_track");
+  receiver_ = CreateReceiver(webrtc_track);
+  EXPECT_FALSE(receiver_->Track().IsNull());
+  EXPECT_EQ(receiver_->Track().Id().Utf8(), webrtc_track->id());
+  EXPECT_EQ(&receiver_->webrtc_track(), webrtc_track);
+}
+
+TEST_F(RTCRtpReceiverTest, ShallowCopy) {
+  scoped_refptr<MockWebRtcAudioTrack> webrtc_track =
+      MockWebRtcAudioTrack::Create("webrtc_track");
+  receiver_ = CreateReceiver(webrtc_track);
+  auto copy = receiver_->ShallowCopy();
+  EXPECT_EQ(&receiver_->webrtc_track(), webrtc_track);
+  auto* webrtc_receiver = receiver_->webrtc_receiver();
+  auto web_track_unique_id = receiver_->Track().UniqueId();
+  // Copy is identical to original.
+  EXPECT_EQ(copy->webrtc_receiver(), webrtc_receiver);
+  EXPECT_EQ(&copy->webrtc_track(), webrtc_track);
+  EXPECT_EQ(copy->Track().UniqueId(), web_track_unique_id);
+  // Copy keeps the internal state alive.
+  receiver_.reset();
+  EXPECT_EQ(copy->webrtc_receiver(), webrtc_receiver);
+  EXPECT_EQ(&copy->webrtc_track(), webrtc_track);
+  EXPECT_EQ(copy->Track().UniqueId(), web_track_unique_id);
+}
+
+}  // namespace content
