821328a26ac9634fa3fce6a8473f934053b3e36f
Alex Newcomer <newcomer@chromium.org>
2018-03-28 16:57:14 +0000

    Revert "app_list: merge AppListViewDelegate into AppListClientImpl in Chrome."
    This reverts commit f2c4d4c9b48d26870d1711db01af1af093b86cad [formerly db8976b8b9dcd6a99057d49742cf6c42395d9058].
    
    Reason for revert: "Invisible AppList Contents" crbug.com/826751
    
    Original change's description:
    > app_list: merge AppListViewDelegate into AppListClientImpl in Chrome.
    > 
    > This commit does a few things:
    > 
    > - merges the Chrome AppListViewDelegate into AppListClientImpl;
    > - cleans up unused references;
    > - updates methods to proper accesses.
    > 
    > Ownership of major classes:
    > 
    > - Each user has its own AppListSyncableService, and it holds its own
    >   AppListModelUpdater, ModelBuilders, etc.
    > - There's only one AppListClientImpl in ChromeBrowserMainExtraPartsAsh
    >   shared by all users, and it holds Search classes, a mojo pointer to
    >   the AppListControllerImpl in Ash, etc.
    > - There's only one singleton AppListServiceImpl shared by all users, and
    >   it holds AppListControllerDelegateImpl.
    > 
    > Their lifecycles:
    > 
    > - Among these classes, AppListClientImpl is the first to be initiated, in
    >   PreProfileInit. It binds to AppListController in Ash, and then tells
    >   AppListServiceImpl two pointers (AppListController*, AppListClientImpl*).
    > - When AppListServiceImpl receives the two pointers, it should have been
    >   initiated as a singleton.
    > - The AppListClientImpl is associated with the current active user profile
    >   when it's accessed from AppListServiceImpl.
    > 
    > Bug: 733662
    > Change-Id: Iac6de011185bf88c4b3c575871b69bd7a67c340b
    > Reviewed-on: https://chromium-review.googlesource.com/974660
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Jiaquan He <hejq@google.com>
    > Cr-Commit-Position: refs/heads/master@{#546247}
    
    TBR=xiyuan@chromium.org,hejq@google.com
    
    Change-Id: Ic96c585e3bd123bf255768d8a47fb64e812bf946
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 733662
    Reviewed-on: https://chromium-review.googlesource.com/984473
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546526}
    Former-commit-id: e204645cb7365399a0b598777ad42855b4beb206

--- chrome/browser/ui/app_list/crostini/crostini_installer_view_browsertest.cc
+++ chrome/browser/ui/app_list/crostini/crostini_installer_view_browsertest.cc
@@ -6,8 +6,8 @@
 
 #include "base/run_loop.h"
 #include "base/test/scoped_feature_list.h"
-#include "chrome/browser/ui/app_list/app_list_client_impl.h"
 #include "chrome/browser/ui/app_list/app_list_service_impl.h"
+#include "chrome/browser/ui/app_list/app_list_view_delegate.h"
 #include "chrome/browser/ui/app_list/crostini/crostini_app_model_builder.h"
 #include "chrome/browser/ui/app_list/crostini/crostini_util.h"
 #include "chrome/browser/ui/app_list/test/chrome_app_list_test_support.h"
@@ -29,10 +29,10 @@ class CrostiniInstallerViewBrowserTest : public DialogBrowserTest {
   // DialogBrowserTest:
   void ShowUi(const std::string& name) override {
     AppListServiceImpl* service = test::GetAppListServiceImpl();
-    AppListClientImpl* client = service->GetAppListClient();
+    AppListViewDelegate* view_delegate = service->GetViewDelegate();
     const std::string kCrostiniTerminalId =
         crx_file::id_util::GenerateId(kCrostiniTerminalAppName);
-    client->ActivateItem(kCrostiniTerminalId, 0);
+    view_delegate->ActivateItem(kCrostiniTerminalId, 0);
   }
 
   void SetUp() override {
