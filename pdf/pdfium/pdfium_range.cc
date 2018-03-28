b5fa3eae44e4058bc01cc057bb97f6f769d94561
Lei Zhang <thestig@chromium.org>
2018-03-28 22:06:58 +0000

    Add AdjustForBackwardsRange() helper to PDFiumRange.
    Fix some nits as well.
    
    Change-Id: I72aaee1ab9fe6a4a9441d8d245fbb8892e7da986
    Reviewed-on: https://chromium-review.googlesource.com/984853
    Reviewed-by: dsinclair <dsinclair@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546614}
    Former-commit-id: 281b1935975df01b3362b4f8e477c3b884eb68db

--- pdf/pdfium/pdfium_range.cc
+++ pdf/pdfium/pdfium_range.cc
@@ -10,11 +10,21 @@
 
 namespace chrome_pdf {
 
+namespace {
+
+void AdjustForBackwardsRange(int* index, int* count) {
+  int& char_index = *index;
+  int& char_count = *count;
+  if (char_count < 0) {
+    char_count *= -1;
+    char_index -= char_count - 1;
+  }
+}
+
+}  // namespace
+
 PDFiumRange::PDFiumRange(PDFiumPage* page, int char_index, int char_count)
-    : page_(page),
-      char_index_(char_index),
-      char_count_(char_count),
-      cached_screen_rects_zoom_(0) {}
+    : page_(page), char_index_(char_index), char_count_(char_count) {}
 
 PDFiumRange::PDFiumRange(const PDFiumRange& that) = default;
 
@@ -42,14 +52,12 @@ const std::vector<pp::Rect>& PDFiumRange::GetScreenRects(
 
   int char_index = char_index_;
   int char_count = char_count_;
-  if (char_count < 0) {
-    char_count *= -1;
-    char_index -= char_count - 1;
-  }
-  DCHECK_GE(char_index, 0) << "start: " << char_index_
+
+  AdjustForBackwardsRange(&char_index, &char_count);
+  DCHECK_GE(char_index, 0) << " start: " << char_index_
                            << " count: " << char_count_;
   DCHECK_LT(char_index, FPDFText_CountChars(page_->GetTextPage()))
-      << "start: " << char_index_ << " count: " << char_count_;
+      << " start: " << char_index_ << " count: " << char_count_;
 
   int count = FPDFText_CountRects(page_->GetTextPage(), char_index, char_count);
   for (int i = 0; i < count; ++i) {
@@ -72,11 +80,8 @@ base::string16 PDFiumRange::GetText() const {
   int index = char_index_;
   int count = char_count_;
   base::string16 rv;
-  if (count < 0) {
-    count *= -1;
-    index -= count - 1;
-  }
 
+  AdjustForBackwardsRange(&index, &count);
   if (count > 0) {
     PDFiumAPIStringBufferAdapter<base::string16> api_string_adapter(&rv, count,
                                                                     false);
