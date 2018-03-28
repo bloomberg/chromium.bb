6bb488ce917409264cd248a9315caf88435b0ccd
Dan Harrington <harringtond@chromium.org>
2018-03-28 18:38:46 +0000

    Add MockImageDecoder
    Already has one use in ntp_tiles/. Will be used in offline_pages/.
    
    Bug: 794828
    Change-Id: I0d8676d8c962e2348bb5d40472e0696a534eaa3e
    Reviewed-on: https://chromium-review.googlesource.com/984272
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546554}
    Former-commit-id: c71cbd534a49f2f0a15817b05a7a1b35ca8e2066

--- components/ntp_tiles/icon_cacher_impl_unittest.cc
+++ components/ntp_tiles/icon_cacher_impl_unittest.cc
@@ -25,6 +25,7 @@
 #include "components/history/core/browser/history_service.h"
 #include "components/image_fetcher/core/image_decoder.h"
 #include "components/image_fetcher/core/image_fetcher.h"
+#include "components/image_fetcher/core/mock_image_decoder.h"
 #include "components/image_fetcher/core/mock_image_fetcher.h"
 #include "components/image_fetcher/core/request_metadata.h"
 #include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
@@ -48,14 +49,7 @@ using ::testing::ReturnArg;
 
 namespace ntp_tiles {
 namespace {
-
-class MockImageDecoder : public image_fetcher::ImageDecoder {
- public:
-  MOCK_METHOD3(DecodeImage,
-               void(const std::string& image_data,
-                    const gfx::Size& desired_image_frame_size,
-                    const image_fetcher::ImageDecodedCallback& callback));
-};
+using MockImageDecoder = image_fetcher::MockImageDecoder;
 
 // This class provides methods to inject an image resource where a real resource
 // would be necessary otherwise. All other methods have return values that allow
