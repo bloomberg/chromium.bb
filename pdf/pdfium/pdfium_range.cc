0326d912beefc6f09c5aaabb0a139f7cd17a980f
Lei Zhang <thestig@chromium.org>
2018-03-28 22:53:34 +0000

    Handle 0 char count in PDFiumRange.
    Return early since 0 char count means empty rect / text. This avoids
    unnecessary FPDFText_CountChars() / FPDFText_GetText() calls, and it
    avoids DCHECK failure when FPDFText_CountChars() returns 0.
    
    Also add some DCHECKs to make sure the char count is valid when set.
    
    BUG=823374
    
    Change-Id: Ibe3e6abd902d4a4b05333f8667d6edbe9359ff25
    Reviewed-on: https://chromium-review.googlesource.com/984893
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: dsinclair <dsinclair@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546640}
    Former-commit-id: 97712e0473907fae23b6fd639d69ab8c987cbcae

--- pdf/pdfium/pdfium_range.cc
+++ pdf/pdfium/pdfium_range.cc
@@ -24,7 +24,12 @@ void AdjustForBackwardsRange(int* index, int* count) {
 }  // namespace
 
 PDFiumRange::PDFiumRange(PDFiumPage* page, int char_index, int char_count)
-    : page_(page), char_index_(char_index), char_count_(char_count) {}
+    : page_(page), char_index_(char_index), char_count_(char_count) {
+#if DCHECK_IS_ON()
+  AdjustForBackwardsRange(&char_index, &char_count);
+  DCHECK_LE(char_count, FPDFText_CountChars(page_->GetTextPage()));
+#endif
+}
 
 PDFiumRange::PDFiumRange(const PDFiumRange& that) = default;
 
@@ -32,6 +37,11 @@ PDFiumRange::~PDFiumRange() = default;
 
 void PDFiumRange::SetCharCount(int char_count) {
   char_count_ = char_count;
+#if DCHECK_IS_ON()
+  int dummy_index = 0;
+  AdjustForBackwardsRange(&dummy_index, &char_count);
+  DCHECK_LE(char_count, FPDFText_CountChars(page_->GetTextPage()));
+#endif
 
   cached_screen_rects_offset_ = pp::Point();
   cached_screen_rects_zoom_ = 0;
@@ -52,6 +62,8 @@ const std::vector<pp::Rect>& PDFiumRange::GetScreenRects(
 
   int char_index = char_index_;
   int char_count = char_count_;
+  if (char_count == 0)
+    return cached_screen_rects_;
 
   AdjustForBackwardsRange(&char_index, &char_count);
   DCHECK_GE(char_index, 0) << " start: " << char_index_
@@ -80,6 +92,8 @@ base::string16 PDFiumRange::GetText() const {
   int index = char_index_;
   int count = char_count_;
   base::string16 rv;
+  if (count == 0)
+    return rv;
 
   AdjustForBackwardsRange(&index, &count);
   if (count > 0) {
