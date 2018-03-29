32e447f54dd7f7a8c1941dfc4bed736be449a9e6
Eric Noyau <noyau@google.com>
2018-03-29 13:59:06 +0000

    Removing iOS9 code remnants (11)
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I3b59dbf9eb39917960837ae2d561ef12fda251d8
    Reviewed-on: https://chromium-review.googlesource.com/986382
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Eric Noyau <noyau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546812}
    Former-commit-id: db3305b414745e9eb1391ae4fb4106d99ceb853f

--- ios/chrome/browser/ui/qr_scanner/camera_controller.mm
+++ ios/chrome/browser/ui/qr_scanner/camera_controller.mm
@@ -182,35 +182,27 @@
   dispatch_async(_sessionQueue, ^{
     // Get the back camera.
     NSArray* videoCaptureDevices = nil;
-    if (@available(iOS 10, *)) {
-      // Although Apple documentation claims that
-      // AVCaptureDeviceDiscoverySession etc. is available on iOS 10+, they are
-      // not really available on an app whose deployment target is iOS 10.0
-      // (iOS 10.1+ are okay) and Chrome will fail at dynamic link time and
-      // instantly crash.  NSClassFromString() checks if Objective-C run-time
-      // has the classes before using them.
-      Class discoverSessionClass =
-          NSClassFromString(@"AVCaptureDeviceDiscoverySession");
-      if (discoverSessionClass) {
-        // Hardcoded value of AVCaptureDeviceTypeBuiltInWideAngleCamera here.
-        // When this @available(iOS 10, *) is deprecated, the unit test
-        // CameraControllerTest.TestAVCaptureDeviceValue can be removed.
-        // See https://crbug.com/826011
-        NSString* cameraType = @"AVCaptureDeviceTypeBuiltInWideAngleCamera";
-        AVCaptureDeviceDiscoverySession* discoverySession =
-            [discoverSessionClass
-                discoverySessionWithDeviceTypes:@[ cameraType ]
-                                      mediaType:AVMediaTypeVideo
-                                       position:AVCaptureDevicePositionBack];
-        videoCaptureDevices = [discoverySession devices];
-      }
+
+    // Although Apple documentation claims that
+    // AVCaptureDeviceDiscoverySession etc. is available on iOS 10+, they are
+    // not really available on an app whose deployment target is iOS 10.0
+    // (iOS 10.1+ are okay) and Chrome will fail at dynamic link time and
+    // instantly crash.  NSClassFromString() checks if Objective-C run-time
+    // has the classes before using them.
+    Class discoverSessionClass =
+        NSClassFromString(@"AVCaptureDeviceDiscoverySession");
+    if (discoverSessionClass) {
+      // Hardcoded value of AVCaptureDeviceTypeBuiltInWideAngleCamera here.
+      // When this @available(iOS 10, *) is deprecated, the unit test
+      // CameraControllerTest.TestAVCaptureDeviceValue can be removed.
+      // See https://crbug.com/826011
+      NSString* cameraType = @"AVCaptureDeviceTypeBuiltInWideAngleCamera";
+      AVCaptureDeviceDiscoverySession* discoverySession = [discoverSessionClass
+          discoverySessionWithDeviceTypes:@[ cameraType ]
+                                mediaType:AVMediaTypeVideo
+                                 position:AVCaptureDevicePositionBack];
+      videoCaptureDevices = [discoverySession devices];
     }
-#if !defined(__IPHONE_10_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0
-    else {
-      videoCaptureDevices =
-          [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
-    }
-#endif
     if ([videoCaptureDevices count] == 0) {
       [self setCameraState:qr_scanner::CAMERA_UNAVAILABLE];
       return;
