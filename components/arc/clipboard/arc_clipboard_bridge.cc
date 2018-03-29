6807fbebf90ee6f6420e1a8d2f9b6d548a14b7da
Satoshi Niwa <niwa@google.com>
2018-03-29 07:07:58 +0000

    Update mojo API version of GetClipContent in clipboard.mojom Client (ClipboardInstance) of this version can handle a ClipData larger than 800K.
    Android-side CL : ag/3763924
    Doc : go/arc-image-cp
    
    Bug: b:74877813
    Test: cheets_ClipboardTest
    Change-Id: I74dbd9e2e39cf5051b6f68cadef14ec5d769e897
    Reviewed-on: https://chromium-review.googlesource.com/974669
    Reviewed-by: Mattias Nissler <mnissler@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: Satoshi Niwa <niwa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546754}
    Former-commit-id: 8b9210eedc763b7e3d49bb0adf8b7ee9671cf3d3

--- components/arc/clipboard/arc_clipboard_bridge.cc
+++ components/arc/clipboard/arc_clipboard_bridge.cc
@@ -84,6 +84,31 @@ mojom::ClipRepresentationPtr CreatePlainText(const ui::Clipboard* clipboard) {
                                         mojom::ClipValue::NewText(text));
 }
 
+mojom::ClipDataPtr GetClipData(const ui::Clipboard* clipboard) {
+  DCHECK(clipboard);
+
+  std::vector<base::string16> mime_types;
+  bool contains_files;
+  clipboard->ReadAvailableTypes(ui::CLIPBOARD_TYPE_COPY_PASTE, &mime_types,
+                                &contains_files);
+
+  mojom::ClipDataPtr clip_data(mojom::ClipData::New());
+
+  // Populate ClipData with ClipRepresentation objects.
+  for (const auto& mime_type16 : mime_types) {
+    const std::string mime_type(base::UTF16ToUTF8(mime_type16));
+    if (mime_type == ui::Clipboard::kMimeTypeHTML) {
+      clip_data->representations.push_back(CreateHTML(clipboard));
+    } else if (mime_type == ui::Clipboard::kMimeTypeText) {
+      clip_data->representations.push_back(CreatePlainText(clipboard));
+    } else {
+      // TODO(ricardoq): Add other supported mime_types here.
+      DLOG(WARNING) << "Unsupported mime type: " << mime_type;
+    }
+  }
+  return clip_data;
+}
+
 void ProcessHTML(const mojom::ClipRepresentation* repr,
                  ui::ScopedClipboardWriter* writer) {
   DCHECK(repr);
@@ -205,35 +230,24 @@ void ArcClipboardBridge::GetClipContent(GetClipContentCallback callback) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
 
   ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
-  std::vector<base::string16> mime_types;
-  bool contains_files;
-  clipboard->ReadAvailableTypes(ui::CLIPBOARD_TYPE_COPY_PASTE, &mime_types,
-                                &contains_files);
+  mojom::ClipDataPtr clip_data = GetClipData(clipboard);
+  std::move(callback).Run(std::move(clip_data));
+}
 
-  mojom::ClipDataPtr clip_data(mojom::ClipData::New());
+void ArcClipboardBridge::GetClipContentDeprecated(
+    GetClipContentCallback callback) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
 
-  // Populate ClipData with ClipRepresentation objects.
-  for (const auto& mime_type16 : mime_types) {
-    const std::string mime_type(base::UTF16ToUTF8(mime_type16));
-    if (mime_type == ui::Clipboard::kMimeTypeHTML) {
-      clip_data->representations.push_back(CreateHTML(clipboard));
-    } else if (mime_type == ui::Clipboard::kMimeTypeText) {
-      clip_data->representations.push_back(CreatePlainText(clipboard));
-    } else {
-      // TODO(ricardoq): Add other supported mime_types here.
-      DLOG(WARNING) << "Unsupported mime type: " << mime_type;
-    }
-  }
+  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
+  mojom::ClipDataPtr clip_data = GetClipData(clipboard);
 
+  // Old version of ClipboardInstance can't handle a ClipData larger than 800K.
   if (!DoesClipFitIntoInstance(clip_data)) {
     clip_data->representations.clear();
     clip_data->representations.push_back(mojom::ClipRepresentation::New(
         kMimeTypeTextError,
         mojom::ClipValue::NewText(kErrorSizeTooBigForBinder)));
   }
-
-  // Invoke the |callback|, even if |clip_data| is empty, since Instance is
-  // waiting for a response.
   std::move(callback).Run(std::move(clip_data));
 }
 
