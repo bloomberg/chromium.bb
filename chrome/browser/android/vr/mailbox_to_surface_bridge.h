624d30b6b97294fc09916512e2b773bc1d7766d8
Klaus Weidner <klausw@chromium.org>
2018-03-29 22:43:25 +0000

    Add mailbox_to_surface_bridge GpuMemoryBuffer APIs
    This supports using GpuMemoryBuffer to share images between a native
    GL context and a command buffer context in the GPU process.
    
    BUG=761436
    
    Change-Id: Iae0668e055fa1ed181bc8ff640cc290c15bb4906
    Reviewed-on: https://chromium-review.googlesource.com/986663
    Commit-Queue: Klaus Weidner <klausw@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546995}
    Former-commit-id: 3db61e79e472202aff089b27408870d498d235ca

--- chrome/browser/android/vr/mailbox_to_surface_bridge.h
+++ chrome/browser/android/vr/mailbox_to_surface_bridge.h
@@ -6,15 +6,26 @@
 #define CHROME_BROWSER_ANDROID_VR_MAILBOX_TO_SURFACE_BRIDGE_H_
 
 #include "base/callback.h"
+#include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "gpu/command_buffer/common/sync_token.h"
+#include "ui/gfx/buffer_format_util.h"
+#include "ui/gfx/gpu_fence.h"
 
 namespace gl {
 class ScopedJavaSurface;
 class SurfaceTexture;
 }  // namespace gl
 
+namespace gfx {
+struct GpuMemoryBufferHandle;
+}
+
 namespace gpu {
+class ContextSupport;
+struct Mailbox;
 struct MailboxHolder;
+struct SyncToken;
 namespace gles2 {
 class GLES2Interface;
 }
@@ -33,7 +44,7 @@ class MailboxToSurfaceBridge {
 
   // Returns true if the GPU process connection is established and ready to use.
   // Equivalent to waiting for on_initialized to be called.
-  bool IsReady();
+  bool IsConnected();
 
   // Checks if a workaround from "gpu/config/gpu_driver_bug_workaround_type.h"
   // is active. Requires initialization to be complete.
@@ -48,6 +59,40 @@ class MailboxToSurfaceBridge {
   // won't get a new frame on the SurfaceTexture.
   bool CopyMailboxToSurfaceAndSwap(const gpu::MailboxHolder& mailbox);
 
+  void GenSyncToken(gpu::SyncToken* out_sync_token);
+
+  // Copies a GpuFence from the local context to the GPU process,
+  // and issues a server wait for it.
+  void WaitForClientGpuFence(gfx::GpuFence*);
+
+  // Creates a GpuFence in the GPU process after the supplied sync_token
+  // completes, and copies it for use in the local context. This is
+  // asynchronous, the callback receives the GpuFence once it's available.
+  void CreateGpuFence(
+      const gpu::SyncToken& sync_token,
+      base::OnceCallback<void(std::unique_ptr<gfx::GpuFence>)> callback);
+
+  void GenerateMailbox(gpu::Mailbox& out_mailbox);
+
+  // Creates a texture and binds it to the mailbox. Returns its
+  // texture ID in the command buffer context. (Don't use that
+  // in the local GL context, it's not valid there.)
+  uint32_t CreateMailboxTexture(const gpu::Mailbox& mailbox);
+
+  // Unbinds the texture from the mailbox and destroys it.
+  void DestroyMailboxTexture(const gpu::Mailbox& mailbox, uint32_t texture_id);
+
+  // Creates a GLImage from the handle's GpuMemoryBuffer and binds it to
+  // the supplied texture_id in the GPU process. Returns the image ID in the
+  // command buffer context.
+  uint32_t BindSharedBufferImage(const gfx::GpuMemoryBufferHandle&,
+                                 const gfx::Size& size,
+                                 gfx::BufferFormat format,
+                                 gfx::BufferUsage usage,
+                                 uint32_t texture_id);
+
+  void UnbindSharedBuffer(uint32_t image_id, uint32_t texture_id);
+
  private:
   void OnContextAvailable(std::unique_ptr<gl::ScopedJavaSurface> surface,
                           scoped_refptr<viz::ContextProvider>);
@@ -57,6 +102,7 @@ class MailboxToSurfaceBridge {
 
   scoped_refptr<viz::ContextProvider> context_provider_;
   gpu::gles2::GLES2Interface* gl_ = nullptr;
+  gpu::ContextSupport* context_support_ = nullptr;
   int surface_handle_ = 0;
   base::OnceClosure on_initialized_;
 
@@ -68,6 +114,8 @@ class MailboxToSurfaceBridge {
 
   // Must be last.
   base::WeakPtrFactory<MailboxToSurfaceBridge> weak_ptr_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(MailboxToSurfaceBridge);
 };
 
 }  // namespace vr
