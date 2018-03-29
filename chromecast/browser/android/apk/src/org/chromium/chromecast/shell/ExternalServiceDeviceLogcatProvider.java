4580a460d973db339395574eb5f796e8e0d99984
Simeon Anfinrud <sanfin@chromium.org>
2018-03-29 05:17:40 +0000

    [Chromecast] Add AsyncTaskRunner.
    This provides a way to more concisely do asynchronous work using
    lambda syntax and also register asynchronous tasks as scopes
    watching Observables. For example:
    
        AsyncTaskRunner runner = new AsyncTaskRunner();
        state.watch(value -> runner.doAsync(
                () -> { /* do work asynchronously */ },
                result -> { /* handle response on UI thread */ }));
    
    The above code will run the asynchronous task whenever `state`
    activates, and cancel the asynchronous task automatically if
    `state` deactivates. No more null-checks and canceling existing
    tasks when a creating a new task!
    
    Refactored ExternalServiceDeviceLogcatProvider as an example.
    
    Bug: None
    Test: cast_shell_junit_tests
    Change-Id: Iaa277ff75cd398fb1e50fd1c25e2d6afdb4c5b31
    Reviewed-on: https://chromium-review.googlesource.com/980671
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Simeon Anfinrud <sanfin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546731}
    Former-commit-id: 9784c178df1b4da762ae6bdf313b4fca4e652018

--- chromecast/browser/android/apk/src/org/chromium/chromecast/shell/ExternalServiceDeviceLogcatProvider.java
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/ExternalServiceDeviceLogcatProvider.java
@@ -8,7 +8,6 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.os.AsyncTask;
 import android.os.IBinder;
 import android.os.RemoteException;
 
@@ -27,7 +26,7 @@ import java.io.IOException;
  * Javascript console messages.
  */
 class ExternalServiceDeviceLogcatProvider extends ElidedLogcatProvider {
-    private static final String TAG = "AssistantDeviceLogcatProvider";
+    private static final String TAG = "ExternalLogProvider";
 
     @Override
     protected void getRawLogcat(RawLogcatCallback callback) {
@@ -45,41 +44,31 @@ class ExternalServiceDeviceLogcatProvider extends ElidedLogcatProvider {
                 IDeviceLogsProvider provider = IDeviceLogsProvider.Stub.asInterface(service);
 
                 ServiceConnection conn = this;
-                new AsyncTask<Void, Void, CircularBuffer<String>>() {
-                    @Override
-                    public CircularBuffer<String> doInBackground(Void... params) {
-                        CircularBuffer<String> rawLogcat =
-                                new CircularBuffer<>(BuildConfig.LOGCAT_SIZE);
-                        try {
-                            // getLogs() currently gives us the filename of the location of the logs
-                            String logsFileName = provider.getLogs();
-                            Log.i(TAG, "Log Location: " + logsFileName);
+                new AsyncTaskRunner().doAsync(() -> {
+                    CircularBuffer<String> rawLogcat =
+                            new CircularBuffer<>(BuildConfig.LOGCAT_SIZE);
+                    try {
+                        // getLogs() currently gives us the filename of the location of the logs
+                        String logsFileName = provider.getLogs();
+                        Log.i(TAG, "Log Location: " + logsFileName);
 
-                            try (BufferedReader bReader =
-                                            new BufferedReader(new FileReader(logsFileName))) {
-                                String logLn;
-                                while ((logLn = bReader.readLine()) != null) {
-                                    rawLogcat.add(logLn);
-                                }
-                            } catch (IOException e) {
-                                Log.e(TAG, "Can't read logs", e);
+                        try (BufferedReader bReader =
+                                        new BufferedReader(new FileReader(logsFileName))) {
+                            String logLn;
+                            while ((logLn = bReader.readLine()) != null) {
+                                rawLogcat.add(logLn);
                             }
-                        } catch (RemoteException e) {
-                            Log.e(TAG, "Can't get logs", e);
-                        } finally {
-                            ContextUtils.getApplicationContext().unbindService(conn);
-                            return rawLogcat;
+                        } catch (IOException e) {
+                            Log.e(TAG, "Can't read logs", e);
                         }
+                    } catch (RemoteException e) {
+                        Log.e(TAG, "Can't get logs", e);
+                    } finally {
+                        ContextUtils.getApplicationContext().unbindService(conn);
+                        return rawLogcat;
                     }
-
-                    @Override
-                    public void onPostExecute(CircularBuffer<String> rawLogcat) {
-                        callback.onLogsDone(rawLogcat);
-                    }
-                }
-                        .execute();
+                }, callback::onLogsDone);
             }
-
             @Override
             public void onServiceDisconnected(ComponentName name) {
                 Log.i(TAG, "onServiceConnected");
