a3600ea2e32133fdc2997294790293f98ad2b849
Tim Zheng <timzheng@google.com>
2018-03-29 00:54:37 +0000

    Enable virtual machines by version and channel.
    BUG=chromium:800120
    TEST=manual test.
    
    Change-Id: I1d05d5b8d75f8a4380820bcab2d09f0869adfce4
    Reviewed-on: https://chromium-review.googlesource.com/985143
    Commit-Queue: Tim Zheng <timzheng@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546680}
    Former-commit-id: 9c29a858ddd6cb8341f88f29ceda15c993cebb0b

--- chrome/browser/chromeos/virtual_machines/virtual_machines_util.cc
+++ chrome/browser/chromeos/virtual_machines/virtual_machines_util.cc
@@ -4,8 +4,11 @@
 
 #include "chrome/browser/chromeos/virtual_machines/virtual_machines_util.h"
 
+#include "base/version.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
+#include "chrome/common/channel_info.h"
 #include "chromeos/settings/cros_settings_names.h"
+#include "components/version_info/version_info.h"
 
 namespace virtual_machines {
 
@@ -16,6 +19,22 @@ bool AreVirtualMachinesAllowedByPolicy() {
     return virtual_machines_allowed;
   }
   // If device policy is not set, allow virtual machines.
+  return true;
+}
+
+// Disabled for beta/stable channel for M67 and all versions older than M67.
+bool AreVirtualMachinesAllowedByVersionAndChannel() {
+  base::Version current_version(version_info::GetVersionNumber());
+  if (!current_version.IsValid())
+    return false;
+  if (current_version.CompareToWildcardString("67.*") < 0)
+    return false;
+  if (current_version.CompareToWildcardString("67.*") == 0) {
+    version_info::Channel channel = chrome::GetChannel();
+    return channel != version_info::Channel::STABLE &&
+           channel != version_info::Channel::BETA;
+  }
+
   return true;
 }
 
