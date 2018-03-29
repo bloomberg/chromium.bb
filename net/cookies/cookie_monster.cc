4a027d64e539ca22a8d26032b90a333942ec5104
Ken Rockot <rockot@chromium.org>
2018-03-29 01:43:32 +0000

    Browser-side instrumentation of cookie fetching
    Adds a histogram and possible dump-without-crashing to observe
    behavior of pending cookie fetch requests waiting to be fulfilled
    in the browser.
    
    Bug: 824024
    Change-Id: I034792ece9dd6f72ef308c3bea16fc439712c7fd
    Reviewed-on: https://chromium-review.googlesource.com/985021
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#546695}
    Former-commit-id: c204861f71b634a028fdd29a35c7d6104ea4dda4

--- net/cookies/cookie_monster.cc
+++ net/cookies/cookie_monster.cc
@@ -49,17 +49,20 @@
 
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/metrics/field_trial.h"
 #include "base/metrics/histogram.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/single_thread_task_runner.h"
 #include "base/stl_util.h"
 #include "base/strings/string_piece.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "base/timer/elapsed_timer.h"
 #include "base/trace_event/process_memory_dump.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 #include "net/cookies/canonical_cookie.h"
@@ -121,6 +124,48 @@ void MaybeRunCookieCallback(base::OnceCallback<void(T)> callback,
                             const T& result) {
   if (callback)
     std::move(callback).Run(result);
+}
+
+// See |InstrumentGetCookieListClosure()| below.
+class GetCookieListRequestTracker {
+ public:
+  GetCookieListRequestTracker() : weak_ptr_factory_(this) {
+    // If this callback isn't destroyed within 60 seconds, we'll issue a crash
+    // dump.
+    base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+        FROM_HERE,
+        base::BindOnce(&GetCookieListRequestTracker::CrashToReportTimeout,
+                       weak_ptr_factory_.GetWeakPtr()),
+        base::TimeDelta::FromSeconds(60));
+  }
+
+  ~GetCookieListRequestTracker() {
+    UMA_HISTOGRAM_CUSTOM_TIMES("Cookie.GetCookieListCompletionTime",
+                               request_timer_.Elapsed(),
+                               base::TimeDelta::FromMilliseconds(10),
+                               base::TimeDelta::FromSeconds(60), 50);
+  }
+
+ private:
+  void CrashToReportTimeout() { base::debug::DumpWithoutCrashing(); }
+
+  base::ElapsedTimer request_timer_;
+  base::WeakPtrFactory<GetCookieListRequestTracker> weak_ptr_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(GetCookieListRequestTracker);
+};
+
+// Wraps a OnceClosure -- specifically one used by
+// |GetCookieListWithOptionsAsync()| -- with additional bound state to track the
+// duration between when its creation and destruction time. If the callback
+// still exists after 60 seconds, we'll also log a crash dump.
+//
+// See https://crbug.com/824024 for context.
+base::OnceClosure InstrumentGetCookieListClosure(base::OnceClosure closure) {
+  return base::BindOnce(
+      [](std::unique_ptr<GetCookieListRequestTracker> tracker,
+         base::OnceClosure closure) { std::move(closure).Run(); },
+      std::make_unique<GetCookieListRequestTracker>(), std::move(closure));
 }
 
 }  // namespace
@@ -447,12 +492,12 @@ void CookieMonster::GetCookieListWithOptionsAsync(
     const CookieOptions& options,
     GetCookieListCallback callback) {
   DoCookieCallbackForURL(
-      base::BindOnce(
+      InstrumentGetCookieListClosure(base::BindOnce(
           // base::Unretained is safe as DoCookieCallbackForURL stores
           // the callback on |*this|, so the callback will not outlive
           // the object.
           &CookieMonster::GetCookieListWithOptions, base::Unretained(this), url,
-          options, std::move(callback)),
+          options, std::move(callback))),
       url);
 }
 
