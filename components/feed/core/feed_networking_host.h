a2d399151baec243352e4fcae6dcd9a49ae45f40
Patrick Noland <pnoland@google.com>
2018-03-30 21:46:34 +0000

    Implement Feed Networking Host
    Bug:808135,808131
    
    Change-Id: Icd344a1e7f247c88d5fbf419da3d9434f59f1811
    Reviewed-on: https://chromium-review.googlesource.com/959157
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547279}
    Former-commit-id: c3ad02e3d20d1d617a755d3d608dcf2acc8328a9

---
+++ components/feed/core/feed_networking_host.h
@@ -0,0 +1,78 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_FEED_CORE_FEED_NETWORKING_HOST_H_
+#define COMPONENTS_FEED_CORE_FEED_NETWORKING_HOST_H_
+
+#include <stdint.h>
+
+#include <vector>
+
+#include "base/callback.h"
+#include "base/containers/flat_set.h"
+#include "base/containers/unique_ptr_adapters.h"
+#include "base/macros.h"
+#include "url/gurl.h"
+
+namespace identity {
+class IdentityManager;
+}
+
+namespace network {
+class SharedURLLoaderFactoryInfo;
+}
+
+namespace feed {
+
+class NetworkFetch;
+
+// Implementation of the Feed Networking Host API. The networking host handles
+// the process of sending http requests to remote endpoints, including the
+// fetching of access tokens for the primary user.
+class FeedNetworkingHost {
+ public:
+  // status_code is a union of net::Error (if the request failed) and the http
+  // status code(if the request succeeded in reaching the server).
+  using ResponseCallback =
+      base::OnceCallback<void(int32_t status_code,
+                              std::vector<uint8_t> response_bytes)>;
+
+  FeedNetworkingHost(identity::IdentityManager* identity_manager,
+                     const std::string& api_key);
+
+  ~FeedNetworkingHost();
+
+  // Cancels all pending requests immediately. This could be used, for example,
+  // if there are pending requests for a user who just signed out.
+  void CancelRequests();
+
+  // Start a request to |url| of type |request_type| with body |request_body|.
+  // |callback| will be called when the response is received or if there is
+  // an error, including non-protocol errors. The contents of |request_body|
+  // will be gzipped. Uses |loader_factory| to construct the url loader that
+  // performs the request.
+  void Send(
+      std::unique_ptr<network::SharedURLLoaderFactoryInfo> loader_factory_info,
+      const GURL& url,
+      const std::string& request_type,
+      std::vector<uint8_t> request_body,
+      ResponseCallback callback);
+
+ private:
+  void NetworkFetchFinished(NetworkFetch* fetch,
+                            ResponseCallback callback,
+                            int32_t http_code,
+                            std::vector<uint8_t> response_body);
+
+  base::flat_set<std::unique_ptr<NetworkFetch>, base::UniquePtrComparator>
+      pending_requests_;
+  identity::IdentityManager* identity_manager_;
+  const std::string api_key_;
+
+  DISALLOW_COPY_AND_ASSIGN(FeedNetworkingHost);
+};
+
+}  // namespace feed
+
+#endif  // COMPONENTS_FEED_CORE_FEED_NETWORKING_HOST_H_
