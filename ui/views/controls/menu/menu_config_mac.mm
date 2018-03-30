a1e9692e3f5aa2c37d3a90c09d489f78c48f4898
Elly Fong-Jones <ellyjones@chromium.org>
2018-03-30 18:16:21 +0000

    macviews: fix menu transparency, remove some 10.9 code
    This change:
    1) Reduces the transparency of Views menus on Mac - the transparency is far
       too great as is. The newer value is close to what Mac native menus use.
    2) Removes a couple of 10.9-only conditionals in the menu code
    
    Bug: 756892
    Change-Id: Icf6c777e3ac99961bbf9f980f3e245d12475c375
    Reviewed-on: https://chromium-review.googlesource.com/987476
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547210}
    Former-commit-id: 179f56a77bd6e89bb98ad1c764c9181adc469416

--- ui/views/controls/menu/menu_config_mac.mm
+++ ui/views/controls/menu/menu_config_mac.mm
@@ -10,14 +10,6 @@
 
 namespace views {
 
-namespace {
-
-// The height of the menu separator in pixels.
-const int kMenuSeparatorHeight = 2;
-const int kMenuSeparatorHeightMavericks = 1;
-
-}  // namespace
-
 void MenuConfig::Init() {
   font_list = gfx::FontList(gfx::Font([NSFont menuFontOfSize:0.0]));
   menu_vertical_border_size = 4;
@@ -31,8 +23,7 @@ void MenuConfig::Init() {
   separator_height = 13;
   separator_upper_height = 7;
   separator_lower_height = 6;
-  separator_thickness = base::mac::IsOS10_9() ? kMenuSeparatorHeightMavericks
-                                              : kMenuSeparatorHeight;
+  separator_thickness = 2;
   align_arrow_and_shortcut = true;
   icons_in_label = true;
   check_selected_combobox_item = true;
