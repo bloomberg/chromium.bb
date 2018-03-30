ea5ab0819e0f73050cbe42823e25600546f97896
Xiaoqian Dai <xdai@chromium.org>
2018-03-30 20:49:41 +0000

    Revert "Cros splitscreen: Disable splitscreen on M66 stable channel."
    This reverts commit 27a90c149aab039f94c2ff4c37c709a3aa8941ae [formerly a87712b1a0634e8851c9060b9ccc67ee7db0ddc0].
    
    Reason for revert: A misspelled string broke the build. Will reland it later.
    
    Original change's description:
    > Cros splitscreen: Disable splitscreen on M66 stable channel.
    > 
    > Per PM's request, disable splitscreen on M66 stable channel.
    > This CL will be merged back to M66 and then reverted later on tot.
    > 
    > Bug: 827322
    > Change-Id: If1d4af68dc64a9f37e93cd7e48056714fda2206a
    > Reviewed-on: https://chromium-review.googlesource.com/985092
    > Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#547252}
    
    TBR=xiyuan@chromium.org,xdai@chromium.org
    
    Change-Id: I54934161f37be5bfe09d4f91b24a0c6518b51ea3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 827322
    Reviewed-on: https://chromium-review.googlesource.com/988672
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547260}
    Former-commit-id: f1a2e94f6bfc0506020a308fc1f5458278bbb0f3

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -2602,6 +2602,10 @@ const char kDisableTabletAutohideTitlebarsDescription[] =
     "Disable tablet mode autohide titlebars functionality. The user will be "
     "able to see the titlebar in tablet mode.";
 
+const char kDisableTabletSplitViewName[] = "Disable split view in Tablet mode";
+const char kDisableTabletSplitViewDescription[] =
+    "Disable split view for Chrome OS tablet mode.";
+
 const char kEnablePerUserTimezoneName[] = "Per-user time zone preferences.";
 const char kEnablePerUserTimezoneDescription[] =
     "Chrome OS system timezone preference is stored and handled for each user "
@@ -2656,10 +2660,6 @@ const char kEnableFullscreenHandwritingVirtualKeyboardName[] =
 const char kEnableFullscreenHandwritingVirtualKeyboardDescription[] =
     "If enabled, the handwriting virtual keyboard will allow user to write "
     "anywhere on the screen";
-
-const char kEnableTabletSplitViewName[] = "Split view in Tablet mode";
-const char kEnableTabletSplitViewDescription[] =
-    "Enable split view for Chrome OS tablet mode.";
 
 const char kEnableUnifiedMultiDeviceSettingsName[] =
     "Enable unified MultiDevice settings";
