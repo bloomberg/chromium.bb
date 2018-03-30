ac7574da05ccea06caa45fd069dda0b7d00d6e2d
Yuke Liao <liaoyuke@chromium.org>
2018-03-30 16:39:02 +0000

    Revert "Enabled FeedbackKit V2 without SSOService object."
    This reverts commit 85b26ffcb2a2286b2f558cf44c9d371bfb599485 [formerly af7f86c11cca3ebef9f91a9b3b5cd51f5843a77b].
    
    Reason for revert:
    
    This CL is breaking UserFeedbackTestCase in ios internal.
    
    Original change's description:
    > Enabled FeedbackKit V2 without SSOService object.
    > 
    > This replaces http://crrev/c/986713 (which I botched during rebasing).
    > 
    > Bug: 814587
    > Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    > Change-Id: I214ea18fd70c218ad44718b29b6e9c8ed35ee1bc
    > Reviewed-on: https://chromium-review.googlesource.com/987539
    > Commit-Queue: Peter Lee <pkl@chromium.org>
    > Commit-Queue: Sergio Collazos <sczs@chromium.org>
    > Reviewed-by: Sergio Collazos <sczs@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#547116}
    
    TBR=rohitrao@chromium.org,pkl@chromium.org,sczs@chromium.org
    
    Change-Id: I6be9c92223ac0e3dac85e79cfbe6986b3e85ade5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 814587
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Reviewed-on: https://chromium-review.googlesource.com/988172
    Reviewed-by: Yuke Liao <liaoyuke@chromium.org>
    Commit-Queue: Yuke Liao <liaoyuke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547184}
    Former-commit-id: 4c450ef0390085aa39b1a85ce394950daee5e7b8

--- ios/chrome/browser/ui/user_feedback_features.cc
+++ ios/chrome/browser/ui/user_feedback_features.cc
@@ -5,7 +5,7 @@
 #include "ios/chrome/browser/ui/user_feedback_features.h"
 
 const base::Feature kFeedbackKitV2{"FeedbackKitV2",
-                                   base::FEATURE_ENABLED_BY_DEFAULT};
+                                   base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kFeedbackKitV2WithSSOService{
     "FeedbackKitV2WithSSOService", base::FEATURE_DISABLED_BY_DEFAULT};
