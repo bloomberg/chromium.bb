d0b8f8983cdab86df1735495b97423d73611a9df
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-03-30 06:36:25 +0000

    Revert "Blockfile cache: fix long-standing sparse + evict reentrancy problem"
    This reverts commit 9e8800018adf8e120b27470208cbd643910e326f [formerly df5b1e1f88e013bc96107cc52c4a4f33a8238444].
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 547103 as the
    culprit for failures in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2RmNWIxZTFmODhlMDEzYmM5NjEwN2NjNTJjNGE0ZjMzYTgyMzg0NDQM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.memory/Linux%20MSan%20Tests/8841
    
    Sample Failed Step: net_unittests
    
    Original change's description:
    > Blockfile cache: fix long-standing sparse + evict reentrancy problem
    > 
    > Thanks to nedwilliamson@ (on gmail) for an alternative perspective
    > plus a reduction to make fixing this much easier.
    > 
    > Bug: 826626, 518908, 537063, 802886
    > Change-Id: Ibfa01416f9a8e7f7b361e4f93b4b6b134728b85f
    > Reviewed-on: https://chromium-review.googlesource.com/985052
    > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#547103}
    
    Change-Id: I79f8547110bb29265e17dd38f751c948de1910c4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 826626, 518908, 537063, 802886
    Reviewed-on: https://chromium-review.googlesource.com/987692
    Cr-Commit-Position: refs/heads/master@{#547120}
    Former-commit-id: 87b3e251dc1941e7e14a17f150c6cc1f39ccf748

--- net/disk_cache/blockfile/backend_impl.cc
+++ net/disk_cache/blockfile/backend_impl.cc
@@ -168,7 +168,6 @@ BackendImpl::BackendImpl(
       new_eviction_(false),
       first_timer_(true),
       user_load_(false),
-      consider_evicting_at_op_end_(false),
       net_log_(net_log),
       done_(base::WaitableEvent::ResetPolicy::MANUAL,
             base::WaitableEvent::InitialState::NOT_SIGNALED),
@@ -196,7 +195,6 @@ BackendImpl::BackendImpl(
       new_eviction_(false),
       first_timer_(true),
       user_load_(false),
-      consider_evicting_at_op_end_(false),
       net_log_(net_log),
       done_(base::WaitableEvent::ResetPolicy::MANUAL,
             base::WaitableEvent::InitialState::NOT_SIGNALED),
@@ -915,16 +913,9 @@ void BackendImpl::OnEntryDestroyBegin(Addr address) {
 
 void BackendImpl::OnEntryDestroyEnd() {
   DecreaseNumRefs();
-  consider_evicting_at_op_end_ = true;
-}
-
-void BackendImpl::OnSyncBackendOpComplete() {
-  if (consider_evicting_at_op_end_) {
-    if (data_->header.num_bytes > max_size_ && !read_only_ &&
-        (up_ticks_ > kTrimDelay || user_flags_ & kNoRandom))
-      eviction_.TrimCache(false);
-    consider_evicting_at_op_end_ = false;
-  }
+  if (data_->header.num_bytes > max_size_ && !read_only_ &&
+      (up_ticks_ > kTrimDelay || user_flags_ & kNoRandom))
+    eviction_.TrimCache(false);
 }
 
 EntryImpl* BackendImpl::GetOpenEntry(CacheRankingsBlock* rankings) const {
