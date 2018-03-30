b9970c8ec7a64287686277297a58ee0479b36d93
Gauthier Ambard <gambard@chromium.org>
2018-03-30 10:39:48 +0000

    Add shadow asset to the popup menu
    This CL adds the shadow asset, replacing the CALayer shadow currently
    used. It also removes the blur effect as it is not needed anymore.
    
    Bug: 821765
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I55802e6bbebc7f2da429ad78c8834f604d956e00
    Reviewed-on: https://chromium-review.googlesource.com/986432
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547138}
    Former-commit-id: 0e42f108019798f7d87f99b3fbe35f14ecba1304

--- ios/chrome/browser/ui/popup_menu/popup_menu_view_controller.mm
+++ ios/chrome/browser/ui/popup_menu/popup_menu_view_controller.mm
@@ -5,6 +5,7 @@
 #import "ios/chrome/browser/ui/popup_menu/popup_menu_view_controller.h"
 
 #import "ios/chrome/browser/ui/commands/browser_commands.h"
+#import "ios/chrome/browser/ui/image_util/image_util.h"
 #import "ios/chrome/browser/ui/util/constraints_ui_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -12,11 +13,9 @@
 #endif
 
 namespace {
+const CGFloat kImageMargin = 196;
 const CGFloat kCornerRadius = 13;
-const CGFloat kShadowRadius = 10;
-const CGFloat kShadowOpacity = 0.3;
-const CGFloat kBlurBackgroundGreyScale = 0.98;
-const CGFloat kBlurBackgroundAlpha = 0.95;
+const CGFloat kBackgroundGreyScale = 0.98;
 }  // namespace
 
 @interface PopupMenuViewController ()<UIGestureRecognizerDelegate>
@@ -58,30 +57,21 @@ const CGFloat kBlurBackgroundAlpha = 0.95;
 - (void)setUpContentContainer {
   _contentContainer = [[UIView alloc] init];
 
-  if (UIAccessibilityIsReduceTransparencyEnabled()) {
-    _contentContainer.backgroundColor =
-        [UIColor colorWithWhite:kBlurBackgroundGreyScale alpha:1];
-  } else {
-    _contentContainer.backgroundColor = [UIColor clearColor];
-    UIBlurEffect* blurEffect =
-        [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
-    UIVisualEffectView* blur =
-        [[UIVisualEffectView alloc] initWithEffect:blurEffect];
-    blur.contentView.backgroundColor =
-        [UIColor colorWithWhite:kBlurBackgroundGreyScale
-                          alpha:kBlurBackgroundAlpha];
-    [_contentContainer addSubview:blur];
-    blur.translatesAutoresizingMaskIntoConstraints = NO;
-    blur.layer.cornerRadius = kCornerRadius;
-    blur.layer.masksToBounds = YES;
-    AddSameConstraints(blur, _contentContainer);
-  }
+  _contentContainer.backgroundColor =
+      [UIColor colorWithWhite:kBackgroundGreyScale alpha:1];
 
+  UIImageView* shadow = [[UIImageView alloc]
+      initWithImage:StretchableImageNamed(@"popup_menu_shadow")];
+  [_contentContainer addSubview:shadow];
+  shadow.translatesAutoresizingMaskIntoConstraints = NO;
+  AddSameConstraintsToSidesWithInsets(
+      _contentContainer, shadow,
+      LayoutSides::kTop | LayoutSides::kBottom | LayoutSides::kLeading |
+          LayoutSides::kTrailing,
+      ChromeDirectionalEdgeInsetsMake(kImageMargin, kImageMargin, kImageMargin,
+                                      kImageMargin));
   _contentContainer.layer.cornerRadius = kCornerRadius;
-  _contentContainer.layer.shadowRadius = kShadowRadius;
-  _contentContainer.layer.shadowOpacity = kShadowOpacity;
   _contentContainer.translatesAutoresizingMaskIntoConstraints = NO;
-  // TODO(crbug.com/821765): Add update the shadow.
   [self.view addSubview:_contentContainer];
 }
 
