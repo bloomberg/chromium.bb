ac16ce13db682ed43077a8046e24455fc2e00953
Sammie Quon <sammiequon@google.com>
2018-03-31 01:39:26 +0000

    splitview: Workaround for artifacts on split view indicators.
    Some artifacts show up on intel devices on the splitview indicators
    highlights, which are rounded_rect_views. Use addCircle and addRect
    as a temporary workaround.
    
    Test: manual
    Bug: 824564
    Change-Id: Ic5bee0b6eaf412ea5fa569afefcde66843af4e4c
    Reviewed-on: https://chromium-review.googlesource.com/986974
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547354}
    Former-commit-id: a9ce932269f8cf73d718047bedba984b770d23a6

--- ash/wm/overview/rounded_rect_view.cc
+++ ash/wm/overview/rounded_rect_view.cc
@@ -17,12 +17,24 @@ RoundedRectView::~RoundedRectView() = default;
 void RoundedRectView::OnPaint(gfx::Canvas* canvas) {
   views::View::OnPaint(canvas);
 
-  SkScalar radius = SkIntToScalar(corner_radius_);
-  const SkScalar kRadius[8] = {radius, radius, radius, radius,
-                               radius, radius, radius, radius};
+  // Draw a rounded rect. Use 3 rectangles and 4 circles to create a rounded
+  // rect with the least overlap between shapes. SkPath::addRoundRect is not
+  // used as some artifacts show up on intel devices for
+  // SplitViewDragIndicators.
+  // TODO(crbug.com/824564): Once the bug is resolved we should use
+  // SkPath::addRoundRect.
+  const int r = corner_radius_;
+  const int w = size().width();
+  const int h = size().height();
+
   SkPath path;
-  gfx::Rect bounds(size());
-  path.addRoundRect(gfx::RectToSkRect(bounds), kRadius);
+  path.addCircle(r, r, r);          // top-left
+  path.addCircle(r, h - r, r);      // bottom-left
+  path.addCircle(w - r, h - r, r);  // bottom-right
+  path.addCircle(w - r, r, r);      // top-right
+  path.addRect(gfx::RectToSkRect(gfx::Rect(r, 0, w - 2 * r, r)));      // top
+  path.addRect(gfx::RectToSkRect(gfx::Rect(0, r, w, h - 2 * r)));      // middle
+  path.addRect(gfx::RectToSkRect(gfx::Rect(r, h - r, w - 2 * r, r)));  // bottom
 
   canvas->ClipPath(path, true);
   canvas->DrawColor(background_color_);
