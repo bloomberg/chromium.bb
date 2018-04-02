6366c27b69c9454c601c1c55466d79acb4b5996c
Eric Holk <eholk@chromium.org>
2018-04-02 21:59:44 +0000

    Make failing to enable WebAssembly trap handler non-fatal
    V8's side of this function fails safely, leaving trap handlers disabled and
    relying on bounds checks. There's no reason to make this a fatal failure
    here. This also lets us remove the special cases around sanitizers, operating
    systems, and architectures, because V8 knows what configurations are supported
    for trap handlers.
    
    Change-Id: I461af9590f336e912f94e5cf169f50ddd985b839
    Reviewed-on: https://chromium-review.googlesource.com/988257
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Eric Holk <eholk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547534}
    Former-commit-id: 2c28bc9ee72d9f216edbd970eca8ff32abbec3a7

--- content/renderer/render_process_impl.cc
+++ content/renderer/render_process_impl.cc
@@ -154,10 +154,7 @@ RenderProcessImpl::RenderProcessImpl(
                      "--harmony-array-prototype-values");
   SetV8FlagIfNotFeature(features::kArrayPrototypeValues,
                         "--no-harmony-array-prototype-values");
-// Memory sanitizer compiles V8 to generate ARM code and run it in a simulator,
-// meaning we cannot support trap handlers there.
-#if defined(OS_LINUX) && defined(ARCH_CPU_X86_64) && !defined(OS_ANDROID) && \
-    !defined(MEMORY_SANITIZER)
+#if defined(OS_LINUX) && defined(ARCH_CPU_X86_64) && !defined(OS_ANDROID)
   if (base::FeatureList::IsEnabled(features::kWebAssemblyTrapHandler)) {
     bool use_v8_signal_handler = false;
     base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
@@ -173,7 +170,8 @@ RenderProcessImpl::RenderProcessImpl(
       // (https://crbug.com/798150).
       use_v8_signal_handler = true;
     }
-    CHECK(v8::V8::EnableWebAssemblyTrapHandler(use_v8_signal_handler));
+    // TODO(eholk): report UMA stat for how often this succeeds
+    v8::V8::EnableWebAssemblyTrapHandler(use_v8_signal_handler);
   }
 #endif
 
