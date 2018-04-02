3b33787859485e3241ae3d22f9766fbfac50ad59
Miguel Casas-Sanchez <mcasas@chromium.org>
2018-04-02 18:01:54 +0000

    Face detection mac: extend unittests
    This CL extends FaceDetectionImplMacTest to also use
    FaceDetectionImplMacVision when appropriate.
    
    services_unittests --gtest_filter=*Face* --use-gpu-in-tests
    
    Bug: 799319
    Cq-Include-Trybots: luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    Change-Id: I06c04d9773cac223f5f3612daa6a534a744fa9d8
    Reviewed-on: https://chromium-review.googlesource.com/968671
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547469}
    Former-commit-id: 756acd697b0cb2ac998ad3aa7799f28a6d4d3113

--- services/shape_detection/face_detection_impl_mac_unittest.mm
+++ services/shape_detection/face_detection_impl_mac_unittest.mm
@@ -4,13 +4,20 @@
 
 #include "services/shape_detection/face_detection_impl_mac.h"
 
+#include <memory>
+#include <utility>
+
 #include "base/base64.h"
+#include "base/bind.h"
 #include "base/command_line.h"
 #include "base/files/file_util.h"
+#include "base/logging.h"
+#include "base/mac/mac_util.h"
 #include "base/mac/scoped_nsobject.h"
 #include "base/message_loop/message_loop.h"
 #include "base/path_service.h"
 #include "base/run_loop.h"
+#include "services/shape_detection/face_detection_impl_mac_vision.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkBitmap.h"
@@ -28,6 +35,25 @@ ACTION_P(RunClosure, closure) {
   closure.Run();
 }
 
+std::unique_ptr<mojom::FaceDetection> CreateFaceDetectorImplMac(
+    shape_detection::mojom::FaceDetectorOptionsPtr options) {
+  return std::make_unique<FaceDetectionImplMac>(
+      mojom::FaceDetectorOptions::New());
+}
+
+std::unique_ptr<mojom::FaceDetection> CreateFaceDetectorImplMacVision(
+    shape_detection::mojom::FaceDetectorOptionsPtr options) {
+  if (@available(macOS 10.13, *)) {
+    return std::make_unique<FaceDetectionImplMacVision>();
+  } else {
+    return nullptr;
+  }
+}
+
+using FaceDetectorFactory =
+    base::Callback<std::unique_ptr<mojom::FaceDetection>(
+        shape_detection::mojom::FaceDetectorOptionsPtr)>;
+
 }  // anonymous namespace
 
 struct TestParams {
@@ -35,38 +61,55 @@ struct TestParams {
   int image_width;
   int image_height;
   const char* image_path;
-  uint32_t num_faces;
+  size_t num_faces;
+  size_t num_landmarks;
+  FaceDetectorFactory factory;
 } kTestParams[] = {
-    {false, 120, 120, "services/test/data/mona_lisa.jpg", 1},
-    {true, 120, 120, "services/test/data/mona_lisa.jpg", 1},
-    {false, 240, 240, "services/test/data/the_beatles.jpg", 3},
-    {true, 240, 240, "services/test/data/the_beatles.jpg", 3},
+    {false, 120, 120, "services/test/data/mona_lisa.jpg", 1, 3,
+     base::Bind(&CreateFaceDetectorImplMac)},
+    {true, 120, 120, "services/test/data/mona_lisa.jpg", 1, 3,
+     base::Bind(&CreateFaceDetectorImplMac)},
+    {false, 120, 120, "services/test/data/mona_lisa.jpg", 1, 0,
+     base::Bind(&CreateFaceDetectorImplMacVision)},
+    {false, 240, 240, "services/test/data/the_beatles.jpg", 3, 3,
+     base::Bind(&CreateFaceDetectorImplMac)},
+    {true, 240, 240, "services/test/data/the_beatles.jpg", 3, 3,
+     base::Bind(&CreateFaceDetectorImplMac)},
+    {false, 240, 240, "services/test/data/the_beatles.jpg", 4, 0,
+     base::Bind(&CreateFaceDetectorImplMacVision)},
 };
 
 class FaceDetectionImplMacTest : public TestWithParam<struct TestParams> {
  public:
   ~FaceDetectionImplMacTest() override {}
 
-  void DetectCallback(uint32_t num_faces,
+  void DetectCallback(size_t num_faces,
+                      size_t num_landmarks,
                       std::vector<mojom::FaceDetectionResultPtr> results) {
-    ASSERT_EQ(num_faces, results.size());
+    EXPECT_EQ(num_faces, results.size());
     for (const auto& face : results) {
-      ASSERT_EQ(3u, face->landmarks.size());
-      EXPECT_EQ(mojom::LandmarkType::EYE, face->landmarks[0]->type);
-      EXPECT_EQ(mojom::LandmarkType::EYE, face->landmarks[1]->type);
-      EXPECT_EQ(mojom::LandmarkType::MOUTH, face->landmarks[2]->type);
+      EXPECT_EQ(num_landmarks, face->landmarks.size());
+      if (face->landmarks.size() == 3u) {
+        EXPECT_EQ(mojom::LandmarkType::EYE, face->landmarks[0]->type);
+        EXPECT_EQ(mojom::LandmarkType::EYE, face->landmarks[1]->type);
+        EXPECT_EQ(mojom::LandmarkType::MOUTH, face->landmarks[2]->type);
+      }
     }
     Detection();
   }
   MOCK_METHOD0(Detection, void(void));
 
-  std::unique_ptr<FaceDetectionImplMac> impl_;
+  std::unique_ptr<mojom::FaceDetection> impl_;
   const base::MessageLoop message_loop_;
 };
 
-TEST_F(FaceDetectionImplMacTest, CreateAndDestroy) {
-  impl_ = std::make_unique<FaceDetectionImplMac>(
-      shape_detection::mojom::FaceDetectorOptions::New());
+TEST_P(FaceDetectionImplMacTest, CreateAndDestroy) {
+  impl_ = GetParam().factory.Run(mojom::FaceDetectorOptions::New());
+  if (!impl_ && base::mac::IsAtMostOS10_12()) {
+    LOG(WARNING) << "FaceDetectionImplMacVision is not available before Mac "
+                    "OSX 10.13. Skipping test.";
+    return;
+  }
 }
 
 TEST_P(FaceDetectionImplMacTest, ScanOneFace) {
@@ -78,7 +121,12 @@ TEST_P(FaceDetectionImplMacTest, ScanOneFace) {
 
   auto options = shape_detection::mojom::FaceDetectorOptions::New();
   options->fast_mode = GetParam().fast_mode;
-  impl_ = std::make_unique<FaceDetectionImplMac>(std::move(options));
+  impl_ = GetParam().factory.Run(std::move(options));
+  if (!impl_ && base::mac::IsAtMostOS10_12()) {
+    LOG(WARNING) << "FaceDetectionImplMacVision is not available before Mac "
+                    "OSX 10.13. Skipping test.";
+    return;
+  }
 
   // Load image data from test directory.
   base::FilePath image_path;
@@ -105,7 +153,8 @@ TEST_P(FaceDetectionImplMacTest, ScanOneFace) {
   EXPECT_CALL(*this, Detection()).WillOnce(RunClosure(quit_closure));
   impl_->Detect(*image,
                 base::BindOnce(&FaceDetectionImplMacTest::DetectCallback,
-                               base::Unretained(this), GetParam().num_faces));
+                               base::Unretained(this), GetParam().num_faces,
+                               GetParam().num_landmarks));
 
   run_loop.Run();
 }
