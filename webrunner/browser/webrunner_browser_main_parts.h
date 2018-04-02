1108f7cc12c71c70a1f59746178f3dfc561a26de
Sergey Ulanov <sergeyu@chromium.org>
2018-04-02 19:07:51 +0000

    [Fuchsia] Add display::Screen implementation for WebRunner
    Aura requires that display::Screen::GetScreen() returns a valid object.
    1. Added WebRunnerContentBrowserClient,
    2. Added WebRunnerBrowserMainParts,
    3. Added display::Screen, created by WebRunnerBrowserMainParts.
    
    Bug: 822474
    Change-Id: Id22a6a369b89ed0f911513adb17f33ed8b503f26
    Reviewed-on: https://chromium-review.googlesource.com/988841
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547481}
    Former-commit-id: c9af172618b13922923f39fc1d614624398a3564

---
+++ webrunner/browser/webrunner_browser_main_parts.h
@@ -0,0 +1,33 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WEBRUNNER_BROWSER_WEBRUNNER_BROWSER_MAIN_PARTS_H_
+#define WEBRUNNER_BROWSER_WEBRUNNER_BROWSER_MAIN_PARTS_H_
+
+#include <memory>
+
+#include "base/macros.h"
+#include "content/public/browser/browser_main_parts.h"
+
+namespace webrunner {
+
+class WebRunnerScreen;
+
+class WebRunnerBrowserMainParts : public content::BrowserMainParts {
+ public:
+  WebRunnerBrowserMainParts();
+  ~WebRunnerBrowserMainParts() override;
+
+  // content::BrowserMainParts overrides.
+  void PreMainMessageLoopRun() override;
+
+ private:
+  std::unique_ptr<WebRunnerScreen> screen_;
+
+  DISALLOW_COPY_AND_ASSIGN(WebRunnerBrowserMainParts);
+};
+
+}  // namespace webrunner
+
+#endif  // WEBRUNNER_BROWSER_WEBRUNNER_BROWSER_MAIN_PARTS_H_
