d15035bb23ad8e055b84a1cc80f95c03c476b18b
Rune Lillesveen <futhark@chromium.org>
2018-04-03 07:53:22 +0000

    [Squad] Clone ComputedStyle adjusting autosizing in test.
    Getting rid of another mutable style access.
    
    Bug: 813068
    Change-Id: Ied084dcce6c9477ce43763ba42dd4256e87efa25
    Reviewed-on: https://chromium-review.googlesource.com/983953
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547642}
    Former-commit-id: f4b06ef324b3c86ffbccdb7bd28ff67fc968cdba

--- third_party/WebKit/Source/core/editing/commands/ReplaceSelectionCommandTest.cpp
+++ third_party/WebKit/Source/core/editing/commands/ReplaceSelectionCommandTest.cpp
@@ -115,9 +115,11 @@ bool SetTextAutosizingMultiplier(Document* document, float multiplier) {
   for (LayoutObject* layout_object = document->GetLayoutView(); layout_object;
        layout_object = layout_object->NextInPreOrder()) {
     if (layout_object->Style()) {
-      layout_object->MutableStyleRef().SetTextAutosizingMultiplier(multiplier);
-
-      EXPECT_EQ(multiplier, layout_object->Style()->TextAutosizingMultiplier());
+      scoped_refptr<ComputedStyle> modified_style =
+          ComputedStyle::Clone(layout_object->StyleRef());
+      modified_style->SetTextAutosizingMultiplier(multiplier);
+      EXPECT_EQ(multiplier, modified_style->TextAutosizingMultiplier());
+      layout_object->SetStyleInternal(std::move(modified_style));
       multiplier_set = true;
     }
   }
