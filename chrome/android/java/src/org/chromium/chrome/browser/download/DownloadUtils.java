bloomberg/chromium.bb

commit 5bc91eaade73af4629013bf8e1594fce47410919

Pass a context when deciding isTablet().
On Android, each activity is associated with a Display. While
the normal case is that there is only one display, it can happen
on Laptops with external monitors, or when casting, that
two Chrome activities are associated with different displays.

I'm hopeful that this will fix / reduce the number of
Resources$NotFoundException crashes that are caused by
phone / tablet mismatches.

This also changes applyOverrideConfiguration to always be called,
as we don't support changing the value of mIsTablet in
onConfigurationUpdated().

Bug: 608790, 669974

Change-Id: I12bf7cf99e87cabdb626dcef4c20107824f0fcca
Reviewed-on: https://chromium-review.googlesource.com/884232
Commit-Queue: agrieve <agrieve@chromium.org>
Reviewed-by: Bo <boliu@chromium.org>
Reviewed-by: Ted Choc <tedchoc@chromium.org>
Reviewed-by: Theresa <twellington@chromium.org>
Cr-Commit-Position: refs/heads/master@{#546523}
Former-commit-id: 1796079a6b853c24e76e016a8fc3a52799f9d92e

chrome/android/java/src/org/chromium/chrome/browser/download/DownloadUtils.java
@@ -143,7 +143,7 @@ public class DownloadUtils {
 
         Context appContext = ContextUtils.getApplicationContext();
 
-        if (DeviceFormFactor.isTablet()) {
+        if (DeviceFormFactor.isNonMultiDisplayContextOnTablet(activity)) {
             // Download Home shows up as a tab on tablets.
             LoadUrlParams params = new LoadUrlParams(UrlConstants.DOWNLOADS_URL);
             if (tab == null || !tab.isInitialized()) {

commit ee45aef898c347dbc87d9bebb49ca87b3adc775d

Android: Fix crash when showing downloads with no active activity
Recent regression from 5bc91eaade73af4629013bf8e1594fce47410919 [formerly 1796079a6b853c24e76e016a8fc3a52799f9d92e]

Bug: 827844
Change-Id: Ia29d752c0144d60e5e0fe74d6170671412375f0d
Reviewed-on: https://chromium-review.googlesource.com/991409
Reviewed-by: Joy Ming <jming@chromium.org>
Commit-Queue: agrieve <agrieve@chromium.org>
Cr-Commit-Position: refs/heads/master@{#547775}
Former-commit-id: 73b2804de38b3110b278297ee1478c4f2f055070

chrome/android/java/src/org/chromium/chrome/browser/download/DownloadUtils.java
@@ -137,14 +137,19 @@ public class DownloadUtils {
             @Nullable Activity activity, @Nullable Tab tab, boolean showPrefetchedContent) {
         // Figure out what tab was last being viewed by the user.
         if (activity == null) activity = ApplicationStatus.getLastTrackedFocusedActivity();
+        Context appContext = ContextUtils.getApplicationContext();
+        boolean isTablet;
 
         if (tab == null && activity instanceof ChromeTabbedActivity) {
-            tab = ((ChromeTabbedActivity) activity).getActivityTab();
+            ChromeTabbedActivity chromeActivity = ((ChromeTabbedActivity) activity);
+            tab = chromeActivity.getActivityTab();
+            isTablet = chromeActivity.isTablet();
+        } else {
+            Context displayContext = activity != null ? activity : appContext;
+            isTablet = DeviceFormFactor.isNonMultiDisplayContextOnTablet(displayContext);
         }
 
-        Context appContext = ContextUtils.getApplicationContext();
-
-        if (DeviceFormFactor.isNonMultiDisplayContextOnTablet(activity)) {
+        if (isTablet) {
             // Download Home shows up as a tab on tablets.
             LoadUrlParams params = new LoadUrlParams(UrlConstants.DOWNLOADS_URL);
             if (tab == null || !tab.isInitialized()) {