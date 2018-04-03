899b1e8423f51587534b416388343142329a8c70
Andrey Kosyakov <caseq@chromium.org>
2018-04-03 23:02:51 +0000

    DevTools: get frame name from frame tree, not owner element
    Bug: 823579
    Change-Id: I05cc69fc17b4195a1404583ca6635b9fd0888eb7
    Reviewed-on: https://chromium-review.googlesource.com/993972
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547858}
    Former-commit-id: cae175987c1f842d1f232f9f23d8615611ba4747

--- third_party/WebKit/Source/core/inspector/InspectorPageAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorPageAgent.cpp
@@ -937,15 +937,13 @@ std::unique_ptr<protocol::Page::Frame> InspectorPageAgent::BuildObjectForFrame(
           .setSecurityOrigin(SecurityOrigin::Create(url)->ToRawString())
           .build();
   Frame* parent_frame = frame->Tree().Parent();
-  if (parent_frame)
+  if (parent_frame) {
     frame_object->setParentId(IdentifiersFactory::FrameId(parent_frame));
-  if (frame->DeprecatedLocalOwner()) {
-    AtomicString name = frame->DeprecatedLocalOwner()->GetNameAttribute();
-    if (name.IsEmpty())
+    AtomicString name = frame->Tree().GetName();
+    if (name.IsEmpty() && frame->DeprecatedLocalOwner())
       name = frame->DeprecatedLocalOwner()->getAttribute(HTMLNames::idAttr);
     frame_object->setName(name);
   }
-
   if (loader && !loader->UnreachableURL().IsEmpty())
     frame_object->setUnreachableUrl(loader->UnreachableURL().GetString());
   return frame_object;
