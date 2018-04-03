0fc5fef6728a90192e8060ab9997ef4926797caf
Ken Rockot <rockot@chromium.org>
2018-04-03 16:22:26 +0000

    Mojo: More shared buffer API prep work
    Cherry-picks a couple of things:
    
      - Adds support for wrapping and unwrapping new base shm regions
      - Ports one use (PDF compositor test code) to use the above
      - Updates base::SharedMemory wrapping to force read-only regions
        when wrapping a handle as read-only
    
    Bug: 826213
    Change-Id: I11d923b23688454ffdbf26ba5efa93ba1605b855
    Reviewed-on: https://chromium-review.googlesource.com/992360
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Jay Civelli <jcivelli@chromium.org>
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547728}
    Former-commit-id: 39bd21e42f727b991022337f9168cb13e2207d5e

--- mojo/public/cpp/system/platform_handle.cc
+++ mojo/public/cpp/system/platform_handle.cc
@@ -4,6 +4,9 @@
 
 #include "mojo/public/cpp/system/platform_handle.h"
 
+#include "base/memory/platform_shared_memory_region.h"
+#include "build/build_config.h"
+
 #if defined(OS_MACOSX) && !defined(OS_IOS)
 #include <mach/mach.h>
 #include "base/mac/mach_logging.h"
@@ -27,6 +30,103 @@ base::PlatformFile PlatformFileFromPlatformHandleValue(uint64_t value) {
 #else
   return static_cast<base::PlatformFile>(value);
 #endif
+}
+
+ScopedSharedBufferHandle WrapPlatformSharedMemoryRegion(
+    base::subtle::PlatformSharedMemoryRegion region) {
+  if (!region.IsValid())
+    return ScopedSharedBufferHandle();
+
+  // TODO(https://crbug.com/826213): Support writable regions too, then simplify
+  // mojom base shared memory traits using this code. This will require the C
+  // API to be extended first.
+  DCHECK_NE(base::subtle::PlatformSharedMemoryRegion::Mode::kWritable,
+            region.GetMode());
+
+  base::subtle::PlatformSharedMemoryRegion::ScopedPlatformHandle handle =
+      region.PassPlatformHandle();
+  MojoPlatformHandle platform_handle;
+  platform_handle.struct_size = sizeof(platform_handle);
+#if defined(OS_WIN)
+  platform_handle.type = MOJO_PLATFORM_HANDLE_TYPE_WINDOWS_HANDLE;
+  platform_handle.value = reinterpret_cast<uint64_t>(handle.Take());
+#elif defined(OS_FUCHSIA)
+  platform_handle.type = MOJO_PLATFORM_HANDLE_TYPE_FUCHSIA_HANDLE;
+  platform_handle.value = static_cast<uint64_t>(handle.release());
+#elif defined(OS_MACOSX) && !defined(OS_IOS)
+  platform_handle.type = MOJO_PLATFORM_HANDLE_TYPE_MACH_PORT;
+  platform_handle.value = static_cast<uint64_t>(handle.release());
+#elif defined(OS_ANDROID)
+  platform_handle.type = MOJO_PLATFORM_HANDLE_TYPE_FILE_DESCRIPTOR;
+  platform_handle.value = static_cast<uint64_t>(handle.release());
+#else
+  platform_handle.type = MOJO_PLATFORM_HANDLE_TYPE_FILE_DESCRIPTOR;
+  platform_handle.value = static_cast<uint64_t>(handle.fd.release());
+#endif
+  const auto& guid = region.GetGUID();
+  MojoSharedBufferGuid mojo_guid = {guid.GetHighForSerialization(),
+                                    guid.GetLowForSerialization()};
+  MojoHandle mojo_handle;
+  MojoResult result = MojoWrapPlatformSharedBufferHandle(
+      &platform_handle, region.GetSize(), &mojo_guid,
+      region.GetMode() ==
+              base::subtle::PlatformSharedMemoryRegion::Mode::kReadOnly
+          ? MOJO_PLATFORM_SHARED_BUFFER_HANDLE_FLAG_HANDLE_IS_READ_ONLY
+          : MOJO_PLATFORM_SHARED_BUFFER_HANDLE_FLAG_NONE,
+      &mojo_handle);
+  if (result != MOJO_RESULT_OK)
+    return ScopedSharedBufferHandle();
+  return ScopedSharedBufferHandle(SharedBufferHandle(mojo_handle));
+}
+
+base::subtle::PlatformSharedMemoryRegion UnwrapPlatformSharedMemoryRegion(
+    ScopedSharedBufferHandle mojo_handle) {
+  if (!mojo_handle.is_valid())
+    return base::subtle::PlatformSharedMemoryRegion();
+
+  MojoPlatformHandle platform_handle;
+  platform_handle.struct_size = sizeof(platform_handle);
+  size_t size;
+  MojoSharedBufferGuid mojo_guid;
+  MojoPlatformSharedBufferHandleFlags flags;
+  MojoResult result = MojoUnwrapPlatformSharedBufferHandle(
+      mojo_handle.release().value(), &platform_handle, &size, &mojo_guid,
+      &flags);
+  if (result != MOJO_RESULT_OK)
+    return base::subtle::PlatformSharedMemoryRegion();
+
+  base::subtle::PlatformSharedMemoryRegion::ScopedPlatformHandle region_handle;
+#if defined(OS_WIN)
+  if (platform_handle.type != MOJO_PLATFORM_HANDLE_TYPE_WINDOWS_HANDLE)
+    return base::subtle::PlatformSharedMemoryRegion();
+  region_handle.Set(reinterpret_cast<HANDLE>(platform_handle.value));
+#elif defined(OS_FUCHSIA)
+  if (platform_handle.type != MOJO_PLATFORM_HANDLE_TYPE_FUCHSIA_HANDLE)
+    return base::subtle::PlatformSharedMemoryRegion();
+  region_handle.reset(static_cast<zx_handle_t>(platform_handle.value));
+#elif defined(OS_MACOSX) && !defined(OS_IOS)
+  if (platform_handle.type != MOJO_PLATFORM_HANDLE_TYPE_MACH_PORT)
+    return base::subtle::PlatformSharedMemoryRegion();
+  region_handle.reset(static_cast<mach_port_t>(platform_handle.value));
+#elif defined(OS_ANDROID)
+  if (platform_handle.type != MOJO_PLATFORM_HANDLE_TYPE_FILE_DESCRIPTOR)
+    return base::subtle::PlatformSharedMemoryRegion();
+  region_handle.reset(static_cast<int>(platform_handle.value));
+#else
+  if (platform_handle.type != MOJO_PLATFORM_HANDLE_TYPE_FILE_DESCRIPTOR)
+    return base::subtle::PlatformSharedMemoryRegion();
+  region_handle.fd.reset(static_cast<int>(platform_handle.value));
+#endif
+
+  // TODO(https://crbug.com/826213): Support unwrapping writable regions. See
+  // comment in |WrapPlatformSharedMemoryRegion()| above.
+  base::subtle::PlatformSharedMemoryRegion::Mode mode =
+      flags & MOJO_PLATFORM_SHARED_BUFFER_HANDLE_FLAG_HANDLE_IS_READ_ONLY
+          ? base::subtle::PlatformSharedMemoryRegion::Mode::kReadOnly
+          : base::subtle::PlatformSharedMemoryRegion::Mode::kUnsafe;
+  return base::subtle::PlatformSharedMemoryRegion::Take(
+      std::move(region_handle), mode, size,
+      base::UnguessableToken::Deserialize(mojo_guid.high, mojo_guid.low));
 }
 
 }  // namespace
@@ -81,8 +181,21 @@ ScopedSharedBufferHandle WrapSharedMemoryHandle(
 
   MojoPlatformSharedBufferHandleFlags flags =
       MOJO_PLATFORM_SHARED_BUFFER_HANDLE_FLAG_NONE;
-  if (protection == UnwrappedSharedMemoryHandleProtection::kReadOnly)
+  if (protection == UnwrappedSharedMemoryHandleProtection::kReadOnly) {
     flags |= MOJO_PLATFORM_SHARED_BUFFER_HANDLE_FLAG_HANDLE_IS_READ_ONLY;
+
+#if defined(OS_ANDROID)
+    // Many callers assume that base::SharedMemory::GetReadOnlyHandle() gives
+    // them a handle which is actually read-only. This assumption is invalid on
+    // Android. As a precursor to migrating all base::SharedMemory usage --
+    // including Mojo internals -- to the new base shared memory API, we ensure
+    // that regions are set to read-only if any of their handles are wrapped
+    // read-only. This relies on existing usages not attempting to map the
+    // region writable any time after this call.
+    if (!memory_handle.IsRegionReadOnly())
+      memory_handle.SetRegionReadOnly();
+#endif
+  }
 
   MojoSharedBufferGuid guid;
   guid.high = memory_handle.GetGUID().GetHighForSerialization();
@@ -146,6 +259,48 @@ MojoResult UnwrapSharedMemoryHandle(
 #endif
 
   return MOJO_RESULT_OK;
+}
+
+ScopedSharedBufferHandle WrapReadOnlySharedMemoryRegion(
+    base::ReadOnlySharedMemoryRegion region) {
+  return WrapPlatformSharedMemoryRegion(
+      base::ReadOnlySharedMemoryRegion::TakeHandleForSerialization(
+          std::move(region)));
+}
+
+ScopedSharedBufferHandle WrapUnsafeSharedMemoryRegion(
+    base::UnsafeSharedMemoryRegion region) {
+  return WrapPlatformSharedMemoryRegion(
+      base::UnsafeSharedMemoryRegion::TakeHandleForSerialization(
+          std::move(region)));
+}
+
+ScopedSharedBufferHandle WrapWritableSharedMemoryRegion(
+    base::WritableSharedMemoryRegion region) {
+  // TODO(https://crbug.com/826213): Support wrapping writable regions. See
+  // comment in |WrapPlatformSharedMemoryRegion()| above.
+  NOTIMPLEMENTED();
+  return ScopedSharedBufferHandle();
+}
+
+base::ReadOnlySharedMemoryRegion UnwrapReadOnlySharedMemoryRegion(
+    ScopedSharedBufferHandle handle) {
+  return base::ReadOnlySharedMemoryRegion::Deserialize(
+      UnwrapPlatformSharedMemoryRegion(std::move(handle)));
+}
+
+base::UnsafeSharedMemoryRegion UnwrapUnsafeSharedMemoryRegion(
+    ScopedSharedBufferHandle handle) {
+  return base::UnsafeSharedMemoryRegion::Deserialize(
+      UnwrapPlatformSharedMemoryRegion(std::move(handle)));
+}
+
+base::WritableSharedMemoryRegion UnwrapWritableSharedMemoryRegion(
+    ScopedSharedBufferHandle handle) {
+  // TODO(https://crbug.com/826213): Support unwrapping writable regions. See
+  // comment in |WrapPlatformSharedMemoryRegion()| above.
+  NOTIMPLEMENTED();
+  return base::WritableSharedMemoryRegion();
 }
 
 #if defined(OS_MACOSX) && !defined(OS_IOS)
