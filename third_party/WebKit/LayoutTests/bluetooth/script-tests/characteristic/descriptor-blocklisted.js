b33efebb0653a0b6d13388a5d3727691c127ad98
Ovidio Henriquez <odejesush@chromium.org>
2018-04-03 01:15:59 +0000

    bluetooth: web: test: Update blocklist UUID tests
    This change updates the blocklisted UUID tests to use the Web
    Bluetooth Test API. This will allow the test to be migrated into wpt/.
    
    BUG=509038
    
    Change-Id: I72acd10e5aabc4c03f955dc4c7b2a8620930cc2d
    Reviewed-on: https://chromium-review.googlesource.com/894738
    Commit-Queue: Ovidio Henriquez <odejesush@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547588}
    Former-commit-id: c7535dc32337aadba84688689a5c1920ca6973be

--- third_party/WebKit/LayoutTests/bluetooth/script-tests/characteristic/descriptor-blocklisted.js
+++ third_party/WebKit/LayoutTests/bluetooth/script-tests/characteristic/descriptor-blocklisted.js
@@ -1,20 +1,19 @@
 'use strict';
-bluetooth_test(() => {
-  let promise;
-  return setBluetoothFakeAdapter('DisconnectingHealthThermometerAdapter')
-    .then(() => requestDeviceWithTrustedClick({
-      filters: [{services: ['health_thermometer']}]}))
-    .then(device => device.gatt.connect())
-    .then(gattServer => gattServer.getPrimaryService('health_thermometer'))
-    .then(service => service.getCharacteristic('measurement_interval'))
-    .then(characteristic => {
-      return assert_promise_rejects_with_message(
+const test_desc = 'Make sure that FUNCTION_NAME can not access blocklisted ' +
+    'descriptors.';
+const expected = new DOMException(
+    'getDescriptor(s) called with blocklisted UUID. https://goo.gl/4NeimX',
+    'SecurityError');
+
+// 1. Connect to the device and retrieve the GATT characteristic.
+bluetooth_test(() => getBlocklistExcludeReadsCharacteristic()
+    // 2. Attempt to call FUNCTION_NAME with a blocklisted UUID.
+    .then(({characteristic}) => assert_promise_rejects_with_message(
         // Using UUIDs instead of names to avoid making a name<>uuid mapping.
         characteristic.CALLS([
           getDescriptor('bad2ddcf-60db-45cd-bef9-fd72b153cf7c')|
-          getDescriptors('bad2ddcf-60db-45cd-bef9-fd72b153cf7c')]),
-          new DOMException('getDescriptor(s) called with blocklisted UUID. ' +
-                         'https://goo.gl/4NeimX',
-              'SecurityError'));
-    })
-}, 'Making sure FUNCTION_NAME can not access blocklisted descriptors.');
+          getDescriptors('bad2ddcf-60db-45cd-bef9-fd72b153cf7c')
+        ]),
+        expected,
+        'FUNCTION_NAME should reject.')),
+    test_desc);
