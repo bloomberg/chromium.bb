6390837397ccafd0bae59409467dff166502a69f
Marc Treib <treib@chromium.org>
2018-04-04 15:28:21 +0000

    FakeProfileOAuth2TokenService: fix auto_post_fetch_response vs manual response
    FakeProfileOAuth2TokenService has two modes of operation:
    auto_post_fetch_response mode, which means it'll automatically post
    successful responses to all access token requests to the message loop,
    or regular (manual) mode, where the user calls IssueToken*/IssueError*
    etc. This CL adds DCHECKs to make sure the two modes are not mixed.
    It also fixes some ProfileSyncService tests which did just that.
    
    Bug: 825190
    Change-Id: Ibed800c7defa09fb8efbf8cb8a56608731aaf70e
    Reviewed-on: https://chromium-review.googlesource.com/995434
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548067}
    Former-commit-id: d0138da3e9025bd01618cb10b642f9961618573e

--- components/signin/core/browser/fake_profile_oauth2_token_service.cc
+++ components/signin/core/browser/fake_profile_oauth2_token_service.cc
@@ -35,6 +35,7 @@ void FakeProfileOAuth2TokenService::IssueAllTokensForAccount(
     const std::string& account_id,
     const std::string& access_token,
     const base::Time& expiration) {
+  DCHECK(!auto_post_fetch_response_on_message_loop_);
   CompleteRequests(account_id, true, ScopeSet(),
                    GoogleServiceAuthError::AuthErrorNone(), access_token,
                    expiration);
@@ -43,6 +44,7 @@ void FakeProfileOAuth2TokenService::IssueAllTokensForAccount(
 void FakeProfileOAuth2TokenService::IssueErrorForAllPendingRequestsForAccount(
     const std::string& account_id,
     const GoogleServiceAuthError& error) {
+  DCHECK(!auto_post_fetch_response_on_message_loop_);
   CompleteRequests(account_id, true, ScopeSet(), error, std::string(),
                    base::Time());
 }
@@ -51,6 +53,7 @@ void FakeProfileOAuth2TokenService::IssueTokenForScope(
     const ScopeSet& scope,
     const std::string& access_token,
     const base::Time& expiration) {
+  DCHECK(!auto_post_fetch_response_on_message_loop_);
   CompleteRequests("", false, scope, GoogleServiceAuthError::AuthErrorNone(),
                    access_token, expiration);
 }
@@ -58,17 +61,20 @@ void FakeProfileOAuth2TokenService::IssueTokenForScope(
 void FakeProfileOAuth2TokenService::IssueErrorForScope(
     const ScopeSet& scope,
     const GoogleServiceAuthError& error) {
+  DCHECK(!auto_post_fetch_response_on_message_loop_);
   CompleteRequests("", false, scope, error, std::string(), base::Time());
 }
 
 void FakeProfileOAuth2TokenService::IssueErrorForAllPendingRequests(
     const GoogleServiceAuthError& error) {
+  DCHECK(!auto_post_fetch_response_on_message_loop_);
   CompleteRequests("", true, ScopeSet(), error, std::string(), base::Time());
 }
 
 void FakeProfileOAuth2TokenService::IssueTokenForAllPendingRequests(
     const std::string& access_token,
     const base::Time& expiration) {
+  DCHECK(!auto_post_fetch_response_on_message_loop_);
   CompleteRequests("", true, ScopeSet(),
                    GoogleServiceAuthError::AuthErrorNone(), access_token,
                    expiration);
@@ -125,9 +131,11 @@ void FakeProfileOAuth2TokenService::FetchOAuth2Token(
   if (auto_post_fetch_response_on_message_loop_) {
     base::ThreadTaskRunnerHandle::Get()->PostTask(
         FROM_HERE,
-        base::Bind(&FakeProfileOAuth2TokenService::IssueAllTokensForAccount,
-                   weak_ptr_factory_.GetWeakPtr(), account_id, "access_token",
-                   base::Time::Max()));
+        base::BindOnce(&FakeProfileOAuth2TokenService::CompleteRequests,
+                       weak_ptr_factory_.GetWeakPtr(), account_id,
+                       /*all_scoped=*/true, ScopeSet(),
+                       GoogleServiceAuthError::AuthErrorNone(), "access_token",
+                       base::Time::Max()));
   }
 }
 
