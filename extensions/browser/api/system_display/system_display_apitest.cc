68eb92bb84b0597496718d6b98b8c1f027a7eb2c
Felix Ekblom <felixe@chromium.org>
2018-04-04 19:05:32 +0000

    Actually check whether extension is in kiosk mode
    Add the missing check for actually being in kiosk mode, but keep the
    autotest bail to simplify testing of the feature.
    
    BUG=chromium:826568
    TEST=Ran autotest, verified that extension can't access edid.
    
    Change-Id: I8f93e29d8d977b0977b3eb597ce2aae263b4e9b9
    Reviewed-on: https://chromium-review.googlesource.com/982619
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Felix Ekblom <felixe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548147}
    Former-commit-id: 52d8147416a1006b8fd50bc13bf6781bdf38fb7e

--- extensions/browser/api/system_display/system_display_apitest.cc
+++ extensions/browser/api/system_display/system_display_apitest.cc
@@ -247,7 +247,7 @@ IN_PROC_BROWSER_TEST_F(SystemDisplayApiTest, SetDisplay) {
   set_info_function->set_has_callback(true);
 
   EXPECT_EQ(
-      SystemDisplayFunction::kCrosOnlyError,
+      SystemDisplayCrOSRestrictedFunction::kCrosOnlyError,
       api_test_utils::RunFunctionAndReturnError(
           set_info_function.get(), "[\"display_id\", {}]", browser_context()));
 
@@ -297,7 +297,7 @@ IN_PROC_BROWSER_TEST_F(SystemDisplayApiTest, SetDisplayNotKioskEnabled) {
   set_info_function->set_has_callback(true);
 
   EXPECT_EQ(
-      SystemDisplayFunction::kKioskOnlyError,
+      SystemDisplayCrOSRestrictedFunction::kKioskOnlyError,
       api_test_utils::RunFunctionAndReturnError(
           set_info_function.get(), "[\"display_id\", {}]", browser_context()));
 
