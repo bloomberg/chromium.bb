1877eace0c36d9ca573fbf8b42d93471fb8a0951
Tetsui Ohkubo <tetsui@chromium.org>
2018-04-04 02:01:39 +0000

    Generate CapsLock notification outside tray
    Previously, CapsLock notification is generated from TrayCapsLock, which
    is going to be removed when UnifiedSystemTray is launched.
    
    This CL separates the notification generation from TrayCapsLock to
    CapsLockNotificationController.
    
    TEST=CapsLockNotificationControllerTest
    BUG=824631
    
    Change-Id: I56bf6df710f4f06c60f19c98d0b2c46a98ce1486
    Reviewed-on: https://chromium-review.googlesource.com/991623
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547944}
    Former-commit-id: 5e249953d115b580f4040c9d401d102793d07a01

--- ash/system/tray_caps_lock_unittest.cc
+++ ash/system/tray_caps_lock_unittest.cc
@@ -4,9 +4,6 @@
 
 #include "ash/system/tray_caps_lock.h"
 
-#include "ash/accessibility/accessibility_controller.h"
-#include "ash/accessibility/test_accessibility_controller_client.h"
-#include "ash/shell.h"
 #include "ash/system/tray/system_tray.h"
 #include "ash/system/tray/system_tray_test_api.h"
 #include "ash/system/web_notification/web_notification_tray.h"
@@ -48,27 +45,6 @@ TEST_F(TrayCapsLockTest, Visibility) {
   // Simulate turning off caps lock.
   caps_lock->OnCapsLockChanged(false);
   EXPECT_FALSE(caps_lock->tray_view()->visible());
-}
-
-// Tests that a11y alert is sent on toggling caps lock.
-TEST_F(TrayCapsLockTest, A11yAlert) {
-  SystemTray* tray = GetPrimarySystemTray();
-  TrayCapsLock* caps_lock = SystemTrayTestApi(tray).tray_caps_lock();
-
-  TestAccessibilityControllerClient client;
-  AccessibilityController* controller =
-      Shell::Get()->accessibility_controller();
-  controller->SetClient(client.CreateInterfacePtrAndBind());
-
-  // Simulate turning on caps lock.
-  caps_lock->OnCapsLockChanged(true);
-  controller->FlushMojoForTest();
-  EXPECT_EQ(mojom::AccessibilityAlert::CAPS_ON, client.last_a11y_alert());
-
-  // Simulate turning off caps lock.
-  caps_lock->OnCapsLockChanged(false);
-  controller->FlushMojoForTest();
-  EXPECT_EQ(mojom::AccessibilityAlert::CAPS_OFF, client.last_a11y_alert());
 }
 
 }  // namespace ash
