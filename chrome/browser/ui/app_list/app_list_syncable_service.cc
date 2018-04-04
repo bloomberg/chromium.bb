02c9ca9b19d3b66d35e6df023645760b9021fbce
Jiaquan He <hejq@google.com>
2018-04-04 02:11:51 +0000

    Reland "app_list: merge AppListViewDelegate into AppListClientImpl in Chrome."
    This is a reland of bedf4b050e2786d8e2b7f4cbd7ddf70c76f18740 [formerly db8976b8b9dcd6a99057d49742cf6c42395d9058]
    
    Original change's description:
    > app_list: merge AppListViewDelegate into AppListClientImpl in Chrome.
    >
    > This commit does a few things:
    >
    > - merges the Chrome AppListViewDelegate into AppListClientImpl;
    > - cleans up unused references;
    > - updates methods to proper accesses.
    >
    > Ownership of major classes:
    >
    > - Each user has its own AppListSyncableService, and it holds its own
    >   AppListModelUpdater, ModelBuilders, etc.
    > - There's only one AppListClientImpl in ChromeBrowserMainExtraPartsAsh
    >   shared by all users, and it holds Search classes, a mojo pointer to
    >   the AppListControllerImpl in Ash, etc.
    > - There's only one singleton AppListServiceImpl shared by all users,
    and
    >   it holds AppListControllerDelegateImpl.
    >
    > Their lifecycles:
    >
    > - Among these classes, AppListClientImpl is the first to be initiated,
    in
    >   PreProfileInit. It binds to AppListController in Ash, and then tells
    >   AppListServiceImpl two pointers (AppListController*,
    AppListClientImpl*).
    > - When AppListServiceImpl receives the two pointers, it should have
    been
    >   initiated as a singleton.
    > - The AppListClientImpl is associated with the current active user
    profile
    >   when it's accessed from AppListServiceImpl.
    >
    > Bug: 733662
    > Change-Id: Iac6de011185bf88c4b3c575871b69bd7a67c340b
    > Reviewed-on: https://chromium-review.googlesource.com/974660
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Jiaquan He <hejq@google.com>
    > Cr-Commit-Position: refs/heads/master@{#546247}
    
    Bug: 733662
    Change-Id: Ia779e0ec795a9457dc7214f38b528c2dab688b90
    Reviewed-on: https://chromium-review.googlesource.com/991232
    Commit-Queue: Jiaquan He <hejq@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#547956}
    Former-commit-id: de013df9f9a9aae41e99ffa34b437f9ef9fe3a1e

--- chrome/browser/ui/app_list/app_list_syncable_service.cc
+++ chrome/browser/ui/app_list/app_list_syncable_service.cc
@@ -445,7 +445,6 @@ void AppListSyncableService::SetOemFolderName(const std::string& name) {
 }
 
 AppListModelUpdater* AppListSyncableService::GetModelUpdater() {
-  DCHECK(IsInitialized());
   return model_updater_.get();
 }
 
