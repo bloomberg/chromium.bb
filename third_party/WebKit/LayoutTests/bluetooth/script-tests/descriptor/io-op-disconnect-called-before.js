e0f68ff9b3d8b70b44ea9275ed7409e89b8aa185
Conley Owens <cco3@chromium.org>
2018-04-04 17:45:06 +0000

    bluetooth: web: test: Update descriptor disconnect tests
    This updates 6 generated descriptor/*/gen-io-op-disconnect-*
    tests to use getUserDescriptionDescriptor(), etc.
    
    BUG=509038
    
    Change-Id: I997897747c7da2374ac2a0497f9a23e32fa170d2
    Reviewed-on: https://chromium-review.googlesource.com/985393
    Commit-Queue: Conley Owens <cco3@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548121}
    Former-commit-id: 02efcd537d98667f3d5e19802993410857fd5877

--- third_party/WebKit/LayoutTests/bluetooth/script-tests/descriptor/io-op-disconnect-called-before.js
+++ third_party/WebKit/LayoutTests/bluetooth/script-tests/descriptor/io-op-disconnect-called-before.js
@@ -1,25 +1,15 @@
 'use strict';
-bluetooth_test(() => {
-  let val = new Uint8Array([1]);
-  return setBluetoothFakeAdapter('DisconnectingHealthThermometerAdapter')
-      .then(
-          () => requestDeviceWithTrustedClick(
-              {filters: [{services: ['health_thermometer']}]}))
-      .then(device => device.gatt.connect())
-      .then(gattServer => {
-        return gattServer.getPrimaryService('health_thermometer')
-            .then(service => service.getCharacteristic('measurement_interval'))
-            .then(
-                characteristic =>
-                    characteristic.getDescriptor(user_description.name))
-            .then(descriptor => {
-              gattServer.disconnect();
-              return assert_promise_rejects_with_message(
-                  descriptor.CALLS([readValue()|writeValue(val)]),
-                  new DOMException(
-                    'GATT Server is disconnected. Cannot perform GATT operations. ' +
-                      '(Re)connect first with `device.gatt.connect`.',
-                      'NetworkError'));
-            });
-      });
-}, 'disconnect() called before FUNCTION_NAME. Reject with NetworkError.');
+const test_desc = 'disconnect() called before FUNCTION_NAME. Reject with ' +
+    'NetworkError.';
+const value = new Uint8Array([1]);
+const expected = new DOMException(
+    'GATT Server is disconnected. Cannot perform GATT operations. ' +
+    '(Re)connect first with `device.gatt.connect`.',
+    'NetworkError');
+
+bluetooth_test(() => getUserDescriptionDescriptor()
+    .then(({device, descriptor}) => {
+      device.gatt.disconnect();
+      return assert_promise_rejects_with_message(
+          descriptor.CALLS([readValue()|writeValue(value)]), expected);
+    }), test_desc);
