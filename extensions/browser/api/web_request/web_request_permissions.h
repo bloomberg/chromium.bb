2f3f8a7c8120748ed22cdb2c5246d809ebbe31d4
Ken Rockot <rockot@chromium.org>
2018-04-05 16:08:20 +0000

    Revert "Re-enable WebRequestApiTest.WebRequestClientsGoogleComProtection"
    This reverts commit 9c2c0e6bcebe39885137e1bc4da8c638ba2158d6 [formerly 7c2240a79be9457c38c9132196bcec3b6c66c2e4].
    
    Reason for revert: https://crbug.com/829218
    
    Original change's description:
    > Re-enable WebRequestApiTest.WebRequestClientsGoogleComProtection
    > 
    > Re-enables this test with the Network Service enabled.
    > 
    > This test did some things which the Network Service does not support and
    > does not need to support, or which otherwise made testing with the
    > Network Service more complex than necessary. The following changes have
    > been made:
    > 
    >   * We no longer attempt to load a network resource from a WebUI
    >     renderer. This will not be supported with Network Service and is no
    >     longer desirable to have working in production anyway. There should
    >     no longer be any WebUI pages making network requests.
    > 
    >   * Use http instead of https for the clients1.google.com requests. This
    >     allows such requests to be handled by the EmbeddedTestServer which
    >     already exists in BrowserTestBase, which is itself not configured to
    >     handle HTTPS requests. Since these requests are filtered by
    >     hostname, the scheme doesn't matter.
    > 
    >   * Switch the test to use a SimpleURLLoader instead of a
    >     FakeURLFetcher. This allows the test code to exercise the same
    >     behavior with or without the Network Service enabled.
    > 
    > Bug: 721414
    > Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    > Change-Id: Iad1f2deaec2e218b9cb168acbe71c90591dc6acf
    > Reviewed-on: https://chromium-review.googlesource.com/972425
    > Commit-Queue: Ken Rockot <rockot@chromium.org>
    > Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#548137}
    
    TBR=jam@chromium.org,rockot@chromium.org,karandeepb@chromium.org
    
    Change-Id: I054a7e11a4945e379cd45405546a90ee4d1e9577
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 721414
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Reviewed-on: https://chromium-review.googlesource.com/998076
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548440}
    Former-commit-id: c317c0ee196eb1e5269388f648592be2b445a5af

--- extensions/browser/api/web_request/web_request_permissions.h
+++ extensions/browser/api/web_request/web_request_permissions.h
@@ -22,8 +22,7 @@ struct WebRequestInfo;
 
 // Exposed for unit testing.
 bool IsSensitiveURL(const GURL& url,
-                    bool is_request_from_browser,
-                    bool is_request_from_webui_renderer);
+                    bool is_request_from_browser_or_webui_renderer);
 
 // This class is used to test whether extensions may modify web requests.
 class WebRequestPermissions {
