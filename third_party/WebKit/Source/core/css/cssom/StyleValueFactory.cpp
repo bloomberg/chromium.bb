9d809a7b0a03b51029aa2b1d3a358393d6cafd33
Darren Shen <shend@chromium.org>
2018-04-05 02:45:33 +0000

    [css-typed-om] Add support for grid properties.
    This patch adds support for some grid properties.
    
    We add a new <flex> CSS value object, which meant rebaselining all the tests.
    
    We're failing some tests because the 'grid', 'grid-template' and
    'grid-gap' shorthands don't serialize to anything [1], so Typed OM
    can't create an unsupported style value from it. Not sure if this is
    a bug...
    
    [1] https://cs.chromium.org/chromium/src/third_party/WebKit/Source/core/css/StylePropertySerializer.cpp?type=cs&l=406
    
    Bug: 820299
    Change-Id: I8ff871fffed2977ca20a623bd283d7cbdc247870
    Reviewed-on: https://chromium-review.googlesource.com/984756
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548296}
    Former-commit-id: a8f7af3cb00fbabd40d5a407b5528b2d5658436b

--- third_party/WebKit/Source/core/css/cssom/StyleValueFactory.cpp
+++ third_party/WebKit/Source/core/css/cssom/StyleValueFactory.cpp
@@ -68,6 +68,13 @@ CSSStyleValue* CreateStyleValueWithPropertyInternal(CSSPropertyID property_id,
         return CSSKeywordValue::Create("currentcolor");
       }
       return CSSUnsupportedStyleValue::Create(property_id, value);
+    case CSSPropertyGridAutoFlow: {
+      const auto& value_list = ToCSSValueList(value);
+      // Only single keywords are supported in level 1.
+      if (value_list.length() == 1U)
+        return CreateStyleValue(value_list.Item(0));
+      return nullptr;
+    }
     case CSSPropertyTransform:
       return CSSTransformValue::FromCSSValue(value);
     case CSSPropertyOffsetAnchor:
