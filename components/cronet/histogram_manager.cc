03d23232946433c524bc8865d86fce01fb67aa88
Misha Efimov <mef@google.com>
2018-04-06 17:40:33 +0000

    [Cronet] Fix HistogramManager.HistogramBucketFields crashes on Win7 Tests
    Bug: 812268
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I15a0d9f4e3a0c16d9a63e82ebb10943e082e1835
    Reviewed-on: https://chromium-review.googlesource.com/999331
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548845}
    Former-commit-id: 406630a0bd675a39ca5abdc9f768f77c1b945231

--- components/cronet/histogram_manager.cc
+++ components/cronet/histogram_manager.cc
@@ -49,7 +49,7 @@ bool HistogramManager::GetDeltas(std::vector<uint8_t>* data) {
         &histogram_snapshot_manager_);
     int32_t data_size = uma_proto_.ByteSize();
     data->resize(data_size);
-    if (uma_proto_.SerializeToArray(&(*data)[0], data_size))
+    if (data_size == 0 || uma_proto_.SerializeToArray(data->data(), data_size))
       return true;
   }
   data->clear();
