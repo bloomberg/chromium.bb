afb7c99698345aaf25bcb13dfb10440f06f9d44b
Daniel Bratell <bratell@opera.com>
2018-04-06 09:58:31 +0000

    [jumbo] Too many kSha256Hash
    Earlier this year, all kSha2Hash got longer names but two
    kSha256Hash must have remained, and now they ended up in
    the same translation unit and jumbo on Windows stopped
    compiling. This renaming follows the pattern of earlier renames.
    
    TBR=thestig@chromium.org
    
    Change-Id: I95b7d6b7e5f43f25bee8e98aa3eeeb4de9d7e55f
    Reviewed-on: https://chromium-review.googlesource.com/999415
    Reviewed-by: Daniel Bratell <bratell@opera.com>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#548730}
    Former-commit-id: a992c363ca57594b41f5aff6fb94b377945329fa

--- chrome/browser/component_updater/sw_reporter_installer_win.cc
+++ chrome/browser/component_updater/sw_reporter_installer_win.cc
@@ -68,10 +68,10 @@ enum SRTCompleted {
 // CRX hash. The extension id is: gkmgaooipdjhmangpemjhigmamcehddo. The hash was
 // generated in Python with something like this:
 // hashlib.sha256().update(open("<file>.crx").read()[16:16+294]).digest().
-const uint8_t kSha256Hash[] = {0x6a, 0xc6, 0x0e, 0xe8, 0xf3, 0x97, 0xc0, 0xd6,
-                               0xf4, 0xc9, 0x78, 0x6c, 0x0c, 0x24, 0x73, 0x3e,
-                               0x05, 0xa5, 0x62, 0x4b, 0x2e, 0xc7, 0xb7, 0x1c,
-                               0x5f, 0xea, 0xf0, 0x88, 0xf6, 0x97, 0x9b, 0xc7};
+const uint8_t kSwReporterSha2Hash[] = {
+    0x6a, 0xc6, 0x0e, 0xe8, 0xf3, 0x97, 0xc0, 0xd6, 0xf4, 0xc9, 0x78,
+    0x6c, 0x0c, 0x24, 0x73, 0x3e, 0x05, 0xa5, 0x62, 0x4b, 0x2e, 0xc7,
+    0xb7, 0x1c, 0x5f, 0xea, 0xf0, 0x88, 0xf6, 0x97, 0x9b, 0xc7};
 
 const base::FilePath::CharType kSwReporterExeName[] =
     FILE_PATH_LITERAL("software_reporter_tool.exe");
@@ -392,7 +392,8 @@ base::FilePath SwReporterInstallerPolicy::GetRelativeInstallDir() const {
 
 void SwReporterInstallerPolicy::GetHash(std::vector<uint8_t>* hash) const {
   DCHECK(hash);
-  hash->assign(kSha256Hash, kSha256Hash + sizeof(kSha256Hash));
+  hash->assign(kSwReporterSha2Hash,
+               kSwReporterSha2Hash + sizeof(kSwReporterSha2Hash));
 }
 
 std::string SwReporterInstallerPolicy::GetName() const {
