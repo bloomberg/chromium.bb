93b381a6c19373c80dff0cd027fe544450e5e482
Tim Zheng <timzheng@google.com>
2018-04-06 02:22:28 +0000

    Fix an error in a comment.
    BUG=chromium:800120
    TEST=comment only change.
    
    Change-Id: Ie3a5d99186419c6a11d3efac4e7b50ec459cf772
    Reviewed-on: https://chromium-review.googlesource.com/999118
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Tim Zheng <timzheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#548642}
    Former-commit-id: 852d7040bfa75c7046902eeca97ded8e0670f121

--- chromeos/dbus/services/chrome_features_service_provider.h
+++ chromeos/dbus/services/chrome_features_service_provider.h
@@ -27,7 +27,7 @@ namespace chromeos {
 // % dbus-send --system --type=method_call --print-reply
 //     --dest=org.chromium.ChromeFeaturesService
 //     /org/chromium/ChromeFeaturesService
-//     org.chromium.ChromeFeaturesService.IsCrostiniEnabled
+//     org.chromium.ChromeFeaturesServiceInterface.IsCrostiniEnabled
 //
 // % (returns true if Crostini is enabled, otherwise returns false)
 class CHROMEOS_EXPORT ChromeFeaturesServiceProvider
