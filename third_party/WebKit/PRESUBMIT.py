efada4800457947c6916542bfe280ec4021a90f9
Kent Tamura <tkent@chromium.org>
2018-04-07 16:30:24 +0000

    Copy and adjust PRESUBMIT*.py in //third_party/WebKit and //third_party/blink
    NOTRY=true
    NOTREECHECKS=true
    TBR=qyearsley@chromium.org
    
    Bug: 768828
    Change-Id: Iae8c7ed5cd81b04cd51f2abb69a5feecbd71fa57
    Reviewed-on: https://chromium-review.googlesource.com/999433
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549063}
    Former-commit-id: c3e4431f5febf4c56a0b64654399659484773655

--- third_party/WebKit/PRESUBMIT.py
+++ third_party/WebKit/PRESUBMIT.py
@@ -30,58 +30,6 @@ _EXCLUDED_PATHS = (
 )
 
 
-def _CheckForWrongMojomIncludes(input_api, output_api):
-    # In blink the code should either use -blink.h or -shared.h mojom
-    # headers, except in public where only -shared.h headers should be
-    # used to avoid exporting Blink types outside Blink.
-    def source_file_filter(path):
-        return input_api.FilterSourceFile(path,
-                                          black_list=[r'third_party/WebKit/common/', r'third_party/WebKit/public/common'])
-
-    # The list of files that we specifically want to allow including
-    # -blink variant files (e.g. because it has #if INSIDE_BLINK).
-    allow_blink_files = [r'third_party/WebKit/public/platform/modules/serviceworker/WebServiceWorkerRequest.h']
-
-    pattern = input_api.re.compile(r'#include\s+.+\.mojom(.*)\.h[>"]')
-    public_folder = input_api.os_path.normpath('third_party/WebKit/public/')
-    non_blink_mojom_errors = []
-    public_blink_mojom_errors = []
-    for f in input_api.AffectedFiles(file_filter=source_file_filter):
-        for line_num, line in f.ChangedContents():
-            error_list = None
-            # This is not super precise as we don't check endif, but allow
-            # including Blink variant mojom files if the file has
-            # '#if INSIDE_BLINK'.
-            match = pattern.match(line)
-            if match:
-                if match.group(1) != '-shared':
-                    if f.LocalPath().startswith(public_folder) and \
-                            not f.LocalPath() in allow_blink_files:
-                        error_list = public_blink_mojom_errors
-                    elif match.group(1) != '-blink':
-                        # Neither -shared.h, nor -blink.h.
-                        error_list = non_blink_mojom_errors
-
-            if error_list is not None:
-                error_list.append('    %s:%d %s' % (
-                    f.LocalPath(), line_num, line))
-
-    results = []
-    if non_blink_mojom_errors:
-        results.append(output_api.PresubmitError(
-            'Files that include non-Blink variant mojoms found. '
-            'You must include .mojom-blink.h or .mojom-shared.h instead:',
-            non_blink_mojom_errors))
-
-    if public_blink_mojom_errors:
-        results.append(output_api.PresubmitError(
-            'Public blink headers using Blink variant mojoms found. '
-            'You must include .mojom-shared.h instead:',
-            public_blink_mojom_errors))
-
-    return results
-
-
 def _CommonChecks(input_api, output_api):
     """Checks common to both upload and commit."""
     # We should figure out what license checks we actually want to use.
@@ -91,7 +39,6 @@ def _CommonChecks(input_api, output_api):
     results.extend(input_api.canned_checks.PanProjectChecks(
         input_api, output_api, excluded_paths=_EXCLUDED_PATHS,
         maxlen=800, license_header=license_header))
-    results.extend(_CheckForWrongMojomIncludes(input_api, output_api))
     return results
 
 
@@ -127,47 +74,10 @@ def _CheckStyle(input_api, output_api):
     return results
 
 
-def _CheckForPrintfDebugging(input_api, output_api):
-    """Generally speaking, we'd prefer not to land patches that printf
-    debug output.
-    """
-    printf_re = input_api.re.compile(r'^\s*(printf\(|fprintf\(stderr,)')
-    errors = input_api.canned_checks._FindNewViolationsOfRule(
-        lambda _, x: not printf_re.search(x),
-        input_api, None)
-    errors = ['  * %s' % violation for violation in errors]
-    if errors:
-        return [output_api.PresubmitPromptOrNotify(
-            'printf debugging is best debugging! That said, it might '
-            'be a good idea to drop the following occurences from '
-            'your patch before uploading:\n%s' % '\n'.join(errors))]
-    return []
-
-
-def _CheckForForbiddenChromiumCode(input_api, output_api):
-    """Checks that Blink uses Chromium classes and namespaces only in permitted code."""
-    # TODO(dcheng): This is pretty similar to _FindNewViolationsOfRule. Unfortunately, that can;'t
-    # be used directly because it doesn't give the callable enough information (namely the path to
-    # the file), so we duplicate the logic here...
-    results = []
-    for f in input_api.AffectedFiles():
-        path = f.LocalPath()
-        errors = audit_non_blink_usage.check(path, [(i + 1, l) for i, l in enumerate(f.NewContents())])
-        if errors:
-            errors = audit_non_blink_usage.check(path, f.ChangedContents())
-            if errors:
-                for line_number, disallowed_identifier in errors:
-                    results.append(output_api.PresubmitError(
-                        '%s:%d uses disallowed identifier %s' % (path, line_number, disallowed_identifier)))
-    return results
-
-
 def CheckChangeOnUpload(input_api, output_api):
     results = []
     results.extend(_CommonChecks(input_api, output_api))
     results.extend(_CheckStyle(input_api, output_api))
-    results.extend(_CheckForPrintfDebugging(input_api, output_api))
-    results.extend(_CheckForForbiddenChromiumCode(input_api, output_api))
     return results
 
 
@@ -185,11 +95,6 @@ def CheckChangeOnCommit(input_api, output_api):
 def _ArePaintOrCompositingDirectoriesModified(change):  # pylint: disable=C0103
     """Checks whether CL has changes to paint or compositing directories."""
     paint_or_compositing_paths = [
-        os.path.join('third_party', 'WebKit', 'Source', 'platform', 'graphics'),
-        os.path.join('third_party', 'WebKit', 'Source', 'core', 'layout',
-                     'compositing'),
-        os.path.join('third_party', 'WebKit', 'Source', 'core', 'svg'),
-        os.path.join('third_party', 'WebKit', 'Source', 'core', 'paint'),
         os.path.join('third_party', 'WebKit', 'LayoutTests', 'FlagExpectations',
                      'enable-slimming-paint-v2'),
         os.path.join('third_party', 'WebKit', 'LayoutTests', 'flag-specific',
@@ -209,10 +114,6 @@ def _ArePaintOrCompositingDirectoriesModified(change):  # pylint: disable=C0103
 def _AreLayoutNGDirectoriesModified(change):  # pylint: disable=C0103
     """Checks whether CL has changes to a layout ng directory."""
     layout_ng_paths = [
-        os.path.join('third_party', 'WebKit', 'Source', 'core', 'layout',
-                     'ng'),
-        os.path.join('third_party', 'WebKit', 'Source', 'core', 'paint',
-                     'ng'),
         os.path.join('third_party', 'WebKit', 'LayoutTests', 'FlagExpectations',
                      'enable-blink-features=LayoutNG'),
         os.path.join('third_party', 'WebKit', 'LayoutTests', 'flag-specific',
--- third_party/WebKit/PRESUBMIT_test.py
+++ third_party/WebKit/PRESUBMIT_test.py
@@ -8,15 +8,17 @@
 # pylint: disable=F0401
 import PRESUBMIT
 
-import mock
 import os.path
 import subprocess
 import sys
 import unittest
 
 sys.path.append(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'pymock'))
+sys.path.append(
     os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..'))
 
+import mock
 from PRESUBMIT_test_mocks import MockInputApi
 from PRESUBMIT_test_mocks import MockOutputApi
 from PRESUBMIT_test_mocks import MockAffectedFile
@@ -55,9 +57,9 @@ class PresubmitTest(unittest.TestCase):
         capture = Capture()
         # pylint: disable=E1101
         subprocess.Popen.assert_called_with(capture, stderr=-1)
-        self.assertEqual(5, len(capture.value))
+        self.assertEqual(6, len(capture.value))
         self.assertEqual('../../FileWebkit.h', capture.value[3])
-        self.assertEqual('../../LayoutTests/TestExpectations', capture.value[4])
+        self.assertEqual('../../LayoutTests/TestExpectations', capture.value[5])
 
     @mock.patch('subprocess.Popen')
     def testCheckChangeOnUploadWithEmptyAffectedFileList(self, _):
@@ -78,144 +80,6 @@ class PresubmitTest(unittest.TestCase):
         PRESUBMIT._CheckStyle(mock_input_api, MockOutputApi())
         # pylint: disable=E1101
         subprocess.Popen.assert_not_called()
-
-    def testCheckPublicHeaderWithBlinkMojo(self):
-        """This verifies that _CheckForWrongMojomIncludes detects -blink mojo
-        headers in public files.
-        """
-
-        mock_input_api = MockInputApi()
-        potentially_bad_content = '#include "public/platform/modules/cache_storage.mojom-blink.h"'
-        mock_input_api.files = [
-            MockAffectedFile('third_party/WebKit/public/AHeader.h',
-                             [potentially_bad_content], None)
-        ]
-        # Access to a protected member _CheckForWrongMojomIncludes
-        # pylint: disable=W0212
-        errors = PRESUBMIT._CheckForWrongMojomIncludes(mock_input_api,
-                                                       MockOutputApi())
-        self.assertEquals(
-            'Public blink headers using Blink variant mojoms found. ' +
-            'You must include .mojom-shared.h instead:',
-            errors[0].message)
-
-    def testCheckInternalHeaderWithBlinkMojo(self):
-        """This verifies that _CheckForWrongMojomIncludes accepts -blink mojo
-        headers in blink internal files.
-        """
-
-        mock_input_api = MockInputApi()
-        potentially_bad_content = '#include "public/platform/modules/cache_storage.mojom-blink.h"'
-        mock_input_api.files = [
-            MockAffectedFile('third_party/WebKit/Source/public/AHeader.h',
-                             [potentially_bad_content], None)
-        ]
-        # Access to a protected member _CheckForWrongMojomIncludes
-        # pylint: disable=W0212
-        errors = PRESUBMIT._CheckForWrongMojomIncludes(mock_input_api,
-                                                       MockOutputApi())
-        self.assertEquals([], errors)
-
-class CxxDependencyTest(unittest.TestCase):
-    allow_list = [
-        'gfx::ColorSpace',
-        'gfx::CubicBezier',
-        'gfx::ICCProfile',
-        'gfx::ScrollOffset',
-        'scoped_refptr<base::SingleThreadTaskRunner>',
-    ]
-    disallow_list = [
-        'GURL',
-        'base::Callback<void()>',
-        'content::RenderFrame',
-        'gfx::Point',
-        'gfx::Rect',
-        'net::IPEndPoint',
-        'ui::Clipboard',
-    ]
-    disallow_message = [
-    ]
-
-    def runCheck(self, filename, file_contents):
-        mock_input_api = MockInputApi()
-        mock_input_api.files = [
-            MockAffectedFile(filename, file_contents),
-        ]
-        # Access to a protected member
-        # pylint: disable=W0212
-        return PRESUBMIT._CheckForForbiddenChromiumCode(mock_input_api, MockOutputApi())
-
-    # References in comments should never be checked.
-    def testCheckCommentsIgnored(self):
-        filename = 'third_party/WebKit/Source/core/frame/frame.cc'
-        for item in self.allow_list:
-            errors = self.runCheck(filename, ['// %s' % item])
-            self.assertEqual([], errors)
-
-        for item in self.disallow_list:
-            errors = self.runCheck(filename, ['// %s' % item])
-            self.assertEqual([], errors)
-
-    # References in Test files should never be checked.
-    def testCheckTestsIgnored(self):
-        filename = 'third_party/WebKit/Source/core/frame/FrameTest.cc'
-        for item in self.allow_list:
-            errors = self.runCheck(filename, ['// %s' % item])
-            self.assertEqual([], errors)
-
-        for item in self.disallow_list:
-            errors = self.runCheck(filename, ['// %s' % item])
-            self.assertEqual([], errors)
-
-    # core, modules, public, et cetera should all have dependency enforcement.
-    def testCheckCoreEnforcement(self):
-        filename = 'third_party/WebKit/Source/core/frame/frame.cc'
-        for item in self.allow_list:
-            errors = self.runCheck(filename, ['%s' % item])
-            self.assertEqual([], errors)
-
-        for item in self.disallow_list:
-            errors = self.runCheck(filename, ['%s' % item])
-            self.assertEquals(1, len(errors))
-            self.assertRegexpMatches(
-                errors[0].message,
-                r'^[^:]+:\d+ uses disallowed identifier .+$')
-
-    def testCheckModulesEnforcement(self):
-        filename = 'third_party/WebKit/Source/modules/modules_initializer.cc'
-        for item in self.allow_list:
-            errors = self.runCheck(filename, ['%s' % item])
-            self.assertEqual([], errors)
-
-        for item in self.disallow_list:
-            errors = self.runCheck(filename, ['%s' % item])
-            self.assertEquals(1, len(errors))
-            self.assertRegexpMatches(
-                errors[0].message,
-                r'^[^:]+:\d+ uses disallowed identifier .+$')
-
-    def testCheckPublicEnforcement(self):
-        filename = 'third_party/WebKit/Source/public/platform/WebThread.h'
-        for item in self.allow_list:
-            errors = self.runCheck(filename, ['%s' % item])
-            self.assertEqual([], errors)
-
-        for item in self.disallow_list:
-            errors = self.runCheck(filename, ['%s' % item])
-            self.assertEquals(1, len(errors))
-            self.assertRegexpMatches(
-                errors[0].message,
-                r'^[^:]+:\d+ uses disallowed identifier .+$')
-
-    # platform and controller should be opted out of enforcement, but aren't
-    # currently checked because the PRESUBMIT test mocks are missing too
-    # much functionality...
-
-    # External module checks should not affect CSS files.
-    def testCheckCSSIgnored(self):
-        filename = 'third_party/WebKit/Source/devtools/front_end/timeline/someFile.css'
-        errors = self.runCheck(filename, ['.toolbar::after { color: pink; }\n'])
-        self.assertEqual([], errors)
 
 if __name__ == '__main__':
     unittest.main()
