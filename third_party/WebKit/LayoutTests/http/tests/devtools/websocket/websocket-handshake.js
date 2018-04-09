4cbb928869398b18fe2ede799e05a81e46a33784
Matt Menke <mmenke@chromium.org>
2018-04-09 17:39:00 +0000

    Make websocket-handshake.js not depend on the Accept-Encoding header.
    Also enable the test when the network service is enabled.
    
    Content Shell sends a different content-encoding with the network
    service enabled and disabled, due to Brotli only being enabled on one
    path.  While I plan to enable Brotli in both cases, it also seems like
    a good idea to remove this dependency.
    
    Bug: 825687
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I9c768099dfd6a876f7650088118de4582ec1ca46
    Reviewed-on: https://chromium-review.googlesource.com/998756
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549214}
    Former-commit-id: 1503cea47cd162eb6741162b900f710ab290878e

--- third_party/WebKit/LayoutTests/http/tests/devtools/websocket/websocket-handshake.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/websocket/websocket-handshake.js
@@ -15,9 +15,9 @@
     var headersToOutput = [];
     for (var i = 0; i < headers.length; ++i) {
       if (headers[i].name === 'Sec-WebSocket-Key' || headers[i].name == 'Sec-WebSocket-Accept' ||
-          headers[i].name == 'User-Agent') {
-        // We hide the header value of these headers because
-        // they can be flaky or platform dependent.
+          headers[i].name == 'User-Agent' || headers[i].name == 'Accept-Encoding') {
+        // We hide the header value of these headers because they can be flaky,
+        // platform dependent, or irrelevant to this test.
         headersToOutput.push({name: headers[i].name, value: '***'});
       } else {
         headersToOutput.push(headers[i]);
