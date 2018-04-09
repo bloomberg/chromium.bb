8bdec835126190e2ada8fd207159835929f7b55a
Rayan Kanso <rayankans@chromium.org>
2018-04-09 18:10:11 +0000

    [Background Fetch] Add BackgroundFetchSettledEvent idl files and basic implementation.
    Specs are defined in https://wicg.github.io/background-fetch/#backgroundfetchsettledevent.
    
    This is the first step in replacing the BackgroundFetched* API.
    
    Bug: 822765
    Change-Id: Ib725b0c389edc32563f45bd26d72621b7031ad31
    Reviewed-on: https://chromium-review.googlesource.com/975549
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549226}
    Former-commit-id: 22db776a23d41dfaba27400e386dc0143b2d2582

---
+++ third_party/blink/renderer/modules/background_fetch/background_fetch_settled_event.cc
@@ -0,0 +1,26 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/background_fetch/background_fetch_settled_event.h"
+
+namespace blink {
+
+BackgroundFetchSettledEvent::BackgroundFetchSettledEvent(
+    const AtomicString& type,
+    const BackgroundFetchSettledEventInit& initializer)
+    : BackgroundFetchEvent(type, initializer, nullptr /* observer */),
+      fetches_(*initializer.fetches()) {}
+
+BackgroundFetchSettledEvent::~BackgroundFetchSettledEvent() = default;
+
+BackgroundFetchSettledFetches* BackgroundFetchSettledEvent::fetches() const {
+  return fetches_;
+}
+
+void BackgroundFetchSettledEvent::Trace(blink::Visitor* visitor) {
+  visitor->Trace(fetches_);
+  BackgroundFetchEvent::Trace(visitor);
+}
+
+}  // namespace blink
---
+++ third_party/blink/renderer/modules/background_fetch/background_fetch_settled_fetches.h
@@ -0,0 +1,43 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_BACKGROUND_FETCH_BACKGROUND_FETCH_SETTLED_FETCHES_H_
+#define THIRD_PARTY_BLINK_RENDERER_MODULES_BACKGROUND_FETCH_BACKGROUND_FETCH_SETTLED_FETCHES_H_
+
+#include "third_party/blink/public/platform/modules/background_fetch/web_background_fetch_settled_fetch.h"
+#include "third_party/blink/public/platform/web_vector.h"
+#include "third_party/blink/renderer/bindings/core/v8/request_or_usv_string.h"
+#include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
+#include "third_party/blink/renderer/modules/background_fetch/background_fetch_settled_fetch.h"
+#include "third_party/blink/renderer/modules/modules_export.h"
+#include "third_party/blink/renderer/platform/bindings/script_state.h"
+#include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
+#include "third_party/blink/renderer/platform/heap/handle.h"
+
+namespace blink {
+
+class MODULES_EXPORT BackgroundFetchSettledFetches final
+    : public ScriptWrappable {
+  DEFINE_WRAPPERTYPEINFO();
+
+ public:
+  BackgroundFetchSettledFetches(
+      ScriptState*,
+      const WebVector<WebBackgroundFetchSettledFetch>& fetches);
+
+  ~BackgroundFetchSettledFetches() override = default;
+
+  // Web Exposed functions defined in the IDL file.
+  ScriptPromise match(ScriptState*, const RequestOrUSVString&);
+  ScriptPromise values(ScriptState*);
+
+  void Trace(blink::Visitor*);
+
+ private:
+  HeapVector<Member<BackgroundFetchSettledFetch>> fetches_;
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_MODULES_BACKGROUND_FETCH_BACKGROUND_FETCH_SETTLED_FETCHES_H_
