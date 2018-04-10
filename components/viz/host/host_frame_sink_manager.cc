99bc8ebfd47d5485b82951f4db88942c14d24269
Ria Jiang <riajiang@chromium.org>
2018-04-10 00:20:22 +0000

    Don't crash viz host when shared memory buffer is not valid, crash viz instead.
    When viz host receives invalid shared memory buffers, use the callback
    received from content/browser to shut down gpu process.
    
    Bug: 746470
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: Ic5fc690266aa4acb76d66c006706e9166771c900
    Reviewed-on: https://chromium-review.googlesource.com/983002
    Commit-Queue: Ria Jiang <riajiang@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549340}
    Former-commit-id: 5e5ba789e10b0319d1812467d8ef834f69fd847b

--- components/viz/host/host_frame_sink_manager.cc
+++ components/viz/host/host_frame_sink_manager.cc
@@ -54,6 +54,11 @@ void HostFrameSinkManager::SetConnectionLostCallback(
   connection_lost_callback_ = std::move(callback);
 }
 
+void HostFrameSinkManager::SetBadMessageReceivedFromGpuCallback(
+    base::RepeatingClosure callback) {
+  bad_message_received_from_gpu_callback_ = std::move(callback);
+}
+
 void HostFrameSinkManager::RegisterFrameSinkId(const FrameSinkId& frame_sink_id,
                                                HostFrameSinkClient* client) {
   DCHECK(frame_sink_id.is_valid());
@@ -138,7 +143,8 @@ void HostFrameSinkManager::CreateRootCompositorFrameSink(
   data.has_created_compositor_frame_sink = true;
 
   frame_sink_manager_->CreateRootCompositorFrameSink(std::move(params));
-  display_hit_test_query_[frame_sink_id] = std::make_unique<HitTestQuery>();
+  display_hit_test_query_[frame_sink_id] =
+      std::make_unique<HitTestQuery>(bad_message_received_from_gpu_callback_);
 }
 
 void HostFrameSinkManager::CreateCompositorFrameSink(
