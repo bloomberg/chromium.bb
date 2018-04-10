43aa45a111baa8b78836479a27477246a9e02a4b
Matthew Jones <mdjones@chromium.org>
2018-04-10 00:14:55 +0000

    Remove unused bottom sheet and Chrome Home feature engagement constants
    This patch also removes some obsolete testing config.
    
    Change-Id: I81541b27d9f54ea0984e4b864c0e5ae1c6e42b75
    Reviewed-on: https://chromium-review.googlesource.com/1003044
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549336}
    Former-commit-id: 4768de8de81a519333a7b02ccd89c005211e100c

--- components/feature_engagement/public/android/java/src/org/chromium/components/feature_engagement/EventConstants.java
+++ components/feature_engagement/public/android/java/src/org/chromium/components/feature_engagement/EventConstants.java
@@ -29,38 +29,10 @@ public final class EventConstants {
     public static final String DOWNLOAD_HOME_OPENED = "download_home_opened";
 
     /**
-     * The bottom sheet was expanded by the user.
-     */
-    public static final String BOTTOM_SHEET_EXPANDED = "bottom_sheet_expanded";
-
-    /**
-     * The bottom sheet was expanded by the user swiping on the toolbar.
-     */
-    public static final String BOTTOM_SHEET_EXPANDED_FROM_SWIPE =
-            "bottom_sheet_expanded_from_swipe";
-
-    /**
-     * The bottom sheet was expanded by the user tapping the expand button.
-     */
-    public static final String BOTTOM_SHEET_EXPANDED_FROM_BUTTON =
-            "bottom_sheet_expanded_from_button";
-
-    /**
-     * The bottom sheet was expanded by the user focusing the omnibox.
-     */
-    public static final String BOTTOM_SHEET_EXPANDED_FROM_OMNIBOX_FOCUS =
-            "bottom_sheet_expanded_from_omnibox_focus";
-
-    /**
      * The user triggered pull to refresh. Used to help determine when to show the Chrome Home
      * in-product help.
      */
     public static final String PULL_TO_REFRESH = "pull_to_refresh";
-
-    /**
-     * The user clicked the Chrome Home menu header.
-     */
-    public static final String CHROME_HOME_MENU_HEADER_CLICKED = "chrome_home_menu_header_clicked";
 
     /**
      * The contextual suggestions bottom sheet was explicitly dismissed via a tap on its close
