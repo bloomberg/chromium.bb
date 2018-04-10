wjdz99/aom

[NORMATIVE] Decoder buffer model implementation
BUG=aomedia:1716

Change-Id: I07c2c0f6dedae861ebf64a7148cec181ea2cfd19

av1/av1_cx_iface.c
@@ -61,7 +61,8 @@ struct av1_extracfg {
 #endif
   unsigned int num_tg;
   unsigned int mtu_size;
-  aom_timing_info_t timing_info;
+
+  aom_timing_info_type_t timing_info_type;
   unsigned int frame_parallel_decoding_mode;
   int use_dual_filter;
   AQ_MODE aq_mode;
@@ -371,7 +372,13 @@ static aom_codec_err_t validate_config(aom_codec_alg_priv_t *ctx,
   RANGE_CHECK(extra_cfg, tuning, AOM_TUNE_PSNR, AOM_TUNE_SSIM);
 #endif
 
-  RANGE_CHECK(extra_cfg, timing_info, AOM_TIMING_UNSPECIFIED, AOM_TIMING_EQUAL);
+#if CONFIG_BUFFER_MODEL
+  RANGE_CHECK(extra_cfg, timing_info_type, AOM_TIMING_UNSPECIFIED,
+              AOM_TIMING_DEC_MODEL);
+#else
+  RANGE_CHECK(extra_cfg, timing_info_type, AOM_TIMING_UNSPECIFIED,
+              AOM_TIMING_EQUAL);
+#endif
 
   RANGE_CHECK(extra_cfg, film_grain_test_vector, 0, 16);
 
@@ -456,15 +463,54 @@ static aom_codec_err_t set_encoder_config(
   oxcf->input_bit_depth = cfg->g_input_bit_depth;
   // guess a frame rate if out of whack, use 30
   oxcf->init_framerate = (double)cfg->g_timebase.den / cfg->g_timebase.num;
-  if (extra_cfg->timing_info == AOM_TIMING_EQUAL) {
+#if CONFIG_BUFFER_MODEL
+  if (extra_cfg->timing_info_type == AOM_TIMING_EQUAL ||
+      extra_cfg->timing_info_type == AOM_TIMING_VARIABLE ||
+      extra_cfg->timing_info_type == AOM_TIMING_DEC_MODEL) {
+#else
+  if (extra_cfg->timing_info_type == AOM_TIMING_EQUAL) {
+#endif
     oxcf->timing_info_present = 1;
+#if CONFIG_BUFFER_MODEL
+    oxcf->timing_info.num_units_in_display_tick = cfg->g_timebase.num;
+    oxcf->timing_info.time_scale = cfg->g_timebase.den;
+    oxcf->timing_info.num_ticks_per_picture = 1;
+#else
     oxcf->num_units_in_tick = cfg->g_timebase.num;
     oxcf->time_scale = cfg->g_timebase.den;
     oxcf->equal_picture_interval = 1;
     oxcf->num_ticks_per_picture = 1;
+#endif
   } else {
     oxcf->timing_info_present = 0;
+#if CONFIG_BUFFER_MODEL
+    oxcf->operating_points_decoder_model_cnt = 0;
+#endif
   }
+#if CONFIG_BUFFER_MODEL
+  if (extra_cfg->timing_info_type == AOM_TIMING_EQUAL) {
+    oxcf->timing_info.equal_picture_interval = 1;
+    oxcf->decoder_model_info_present = 0;
+    oxcf->operating_points_decoder_model_cnt = 0;
+  } else if (extra_cfg->timing_info_type == AOM_TIMING_DEC_MODEL) {
+    //    if( extra_cfg->arnr_strength > 0 )
+    //    {
+    //      printf("Only --arnr-strength=0 can currently be used with
+    //      --timing-info=model."); return AOM_CODEC_INVALID_PARAM;
+    //    }
+    //    if( extra_cfg->enable_superres)
+    //    {
+    //      printf("Only --superres-mode=0 can currently be used with
+    //      --timing-info=model."); return AOM_CODEC_INVALID_PARAM;
+    //    }
+    oxcf->buffer_model.num_units_in_decoding_tick = cfg->g_timebase.num;
+    oxcf->timing_info.equal_picture_interval = 0;
+    oxcf->decoder_model_info_present = 1;
+    oxcf->buffer_removal_delay_present = 1;
+    oxcf->operating_points_decoder_model_cnt = 1;
+    oxcf->op_frame_timing[0].buffer_removal_delay_flag = 1;
+  }
+#endif
   if (oxcf->init_framerate > 180) {
     oxcf->init_framerate = 30;
     oxcf->timing_info_present = 0;
@@ -912,10 +958,10 @@ static aom_codec_err_t ctrl_set_mtu(aom_codec_alg_priv_t *ctx, va_list args) {
   extra_cfg.mtu_size = CAST(AV1E_SET_MTU, args);
   return update_extra_cfg(ctx, &extra_cfg);
 }
-static aom_codec_err_t ctrl_set_timing_info(aom_codec_alg_priv_t *ctx,
-                                            va_list args) {
+static aom_codec_err_t ctrl_set_timing_info_type(aom_codec_alg_priv_t *ctx,
+                                                 va_list args) {
   struct av1_extracfg extra_cfg = ctx->extra_cfg;
-  extra_cfg.timing_info = CAST(AV1E_SET_TIMING_INFO, args);
+  extra_cfg.timing_info_type = CAST(AV1E_SET_TIMING_INFO_TYPE, args);
   return update_extra_cfg(ctx, &extra_cfg);
 }
 
@@ -1123,6 +1169,7 @@ static aom_codec_err_t encoder_destroy(aom_codec_alg_priv_t *ctx) {
 // Turn on to test if supplemental superframe data breaks decoding
 #define TEST_SUPPLEMENTAL_SUPERFRAME_DATA 0
 
+#if !CONFIG_BUFFER_MODEL
 // av1 uses 10,000,000 ticks/second as time stamp
 #define TICKS_PER_SEC 10000000LL
 
@@ -1136,7 +1183,7 @@ static int64_t ticks_to_timebase_units(const aom_rational_t *timebase,
   const int64_t round = TICKS_PER_SEC * timebase->num / 2 - 1;
   return (n * timebase->den + round) / timebase->num / TICKS_PER_SEC;
 }
-
+#endif
 static aom_codec_frame_flags_t get_frame_pkt_flags(const AV1_COMP *cpi,
                                                    unsigned int lib_flags) {
   aom_codec_frame_flags_t flags = lib_flags << 16;
@@ -1258,15 +1305,22 @@ static aom_codec_err_t encoder_encode(aom_codec_alg_priv_t *ctx,
     // invisible frames get packed with the next visible frame
     while (cx_data_sz - index_size >= ctx->cx_data_sz / 2 &&
            !is_frame_visible &&
+#if CONFIG_BUFFER_MODEL
            -1 != av1_get_compressed_data(cpi, &lib_flags, &frame_size, cx_data,
                                          &dst_time_stamp, &dst_end_time_stamp,
-                                         !img)) {
+                                         !img, timebase)) {
+#else
+           -1 != av1_get_compressed_data(cpi, &lib_flags, &frame_size, cx_data,
+                                         &dst_time_stamp, &dst_end_time_stamp,
+                                         !imgCONFI)) {
+#endif
       if (cpi->common.seq_params.frame_id_numbers_present_flag) {
         if (cpi->common.invalid_delta_frame_id_minus1) {
           ctx->base.err_detail = "Invalid delta_frame_id_minus1";
           return AOM_CODEC_ERROR;
         }
       }
+
       if (frame_size) {
         if (ctx->pending_cx_data == 0) ctx->pending_cx_data = cx_data;
 
@@ -1646,7 +1700,7 @@ static aom_codec_ctrl_fn_map_t encoder_ctrl_maps[] = {
 #endif
   { AV1E_SET_NUM_TG, ctrl_set_num_tg },
   { AV1E_SET_MTU, ctrl_set_mtu },
-  { AV1E_SET_TIMING_INFO, ctrl_set_timing_info },
+  { AV1E_SET_TIMING_INFO_TYPE, ctrl_set_timing_info_type },
   { AV1E_SET_FRAME_PARALLEL_DECODING, ctrl_set_frame_parallel_decoding_mode },
   { AV1E_SET_ERROR_RESILIENT_MODE, ctrl_set_error_resilient_mode },
   { AV1E_SET_S_FRAME_MODE, ctrl_set_s_frame_mode },

av1/encoder/bitstream.c
@@ -2498,20 +2498,89 @@ static void write_bitdepth_colorspace_sampling(
 
 static void write_timing_info_header(AV1_COMMON *const cm,
                                      struct aom_write_bit_buffer *wb) {
+#if !CONFIG_BUFFER_MODEL
   aom_wb_write_bit(wb, cm->timing_info_present);  // timing info present flag
 
   if (cm->timing_info_present) {
-    aom_wb_write_unsigned_literal(wb, cm->num_units_in_tick,
+#endif
+#if CONFIG_BUFFER_MODEL
+    aom_wb_write_unsigned_literal(wb, cm->timing_info.num_units_in_display_tick,
                                   32);  // Number of units in tick
-    aom_wb_write_unsigned_literal(wb, cm->time_scale, 32);  // Time scale
-    aom_wb_write_bit(wb,
-                     cm->equal_picture_interval);  // Equal picture interval bit
-    if (cm->equal_picture_interval) {
-      aom_wb_write_uvlc(wb,
-                        cm->num_ticks_per_picture - 1);  // ticks per picture
+    aom_wb_write_unsigned_literal(wb, cm->timing_info.time_scale,
+                                  32);  // Time scale
+    aom_wb_write_bit(
+        wb,
+        cm->timing_info.equal_picture_interval);  // Equal picture interval bit
+    if (cm->timing_info.equal_picture_interval) {
+      aom_wb_write_uvlc(
+          wb,
+          cm->timing_info.num_ticks_per_picture - 1);  // ticks per picture
+#else
+  aom_wb_write_unsigned_literal(wb, cm->num_units_in_tick,
+                                32);  // Number of units in tick
+  aom_wb_write_unsigned_literal(wb, cm->time_scale, 32);  // Time scale
+  aom_wb_write_bit(wb,
+                   cm->equal_picture_interval);  // Equal picture interval bit
+  if (cm->equal_picture_interval) {
+    aom_wb_write_uvlc(wb,
+                      cm->num_ticks_per_picture - 1);  // ticks per picture
+#endif
     }
+#if !CONFIG_BUFFER_MODEL
   }
+#endif
 }
+
+#if CONFIG_BUFFER_MODEL
+static void write_decoder_model_info(AV1_COMMON *const cm,
+                                     struct aom_write_bit_buffer *wb) {
+  aom_wb_write_literal(wb, cm->buffer_model.bitrate_scale, 4);
+  aom_wb_write_literal(wb, cm->buffer_model.buffer_size_scale, 4);
+  aom_wb_write_literal(
+      wb, cm->buffer_model.encoder_decoder_buffer_delay_length - 1, 5);
+  aom_wb_write_unsigned_literal(wb, cm->buffer_model.num_units_in_decoding_tick,
+                                32);  // Number of units in decoding tick
+  aom_wb_write_literal(wb, cm->buffer_model.buffer_removal_delay_length - 1, 5);
+  aom_wb_write_literal(wb, cm->buffer_model.frame_presentation_delay_length - 1,
+                       5);
+}
+
+static void write_dec_model_op_parameters(AV1_COMMON *const cm,
+                                          struct aom_write_bit_buffer *wb,
+                                          int op_num) {
+  if (op_num > MAX_NUM_OP_POINTS)
+    aom_internal_error(
+        &cm->error, AOM_CODEC_UNSUP_BITSTREAM,
+        "Encoder does not support %d decoder model operating points", op_num);
+
+  //  aom_wb_write_bit(wb, cm->op_params[op_num].has_parameters);
+  //  if (!cm->op_params[op_num].has_parameters) return;
+
+  aom_wb_write_uvlc(wb, cm->op_params[op_num].bitrate - 1);
+
+  aom_wb_write_uvlc(wb, cm->op_params[op_num].buffer_size - 1);
+
+  aom_wb_write_bit(wb, cm->op_params[op_num].cbr_flag);
+
+  aom_wb_write_literal(wb, cm->op_params[op_num].decoder_buffer_delay,
+                       cm->buffer_model.encoder_decoder_buffer_delay_length);
+
+  aom_wb_write_literal(wb, cm->op_params[op_num].encoder_buffer_delay,
+                       cm->buffer_model.encoder_decoder_buffer_delay_length);
+
+  aom_wb_write_bit(wb, cm->op_params[op_num].low_delay_mode_flag);
+
+  cm->op_frame_timing[op_num].buffer_removal_delay =
+      0;  // reset the decoded frame counter
+}
+
+static void write_tu_pts_info(AV1_COMMON *const cm,
+                              struct aom_write_bit_buffer *wb) {
+  aom_wb_write_unsigned_literal(
+      wb, (uint32_t)cm->tu_presentation_delay,
+      cm->buffer_model.frame_presentation_delay_length);
+}
+#endif
 
 static void write_film_grain_params(AV1_COMP *cpi,
                                     struct aom_write_bit_buffer *wb) {
@@ -2833,6 +2902,12 @@ static void write_uncompressed_header_obu(AV1_COMP *cpi,
     aom_wb_write_bit(wb, 1);  // show_existing_frame
     aom_wb_write_literal(wb, cpi->existing_fb_idx_to_show, 3);
 
+#if CONFIG_BUFFER_MODEL
+    if (cm->timing_info_present &&
+        cm->timing_info.equal_picture_interval == 0) {
+      write_tu_pts_info(cm, wb);
+    }
+#endif
     if (cm->seq_params.frame_id_numbers_present_flag) {
       int frame_id_len = cm->seq_params.frame_id_length;
       int display_frame_id = cm->ref_frame_id[cpi->existing_fb_idx_to_show];
@@ -2857,7 +2932,14 @@ static void write_uncompressed_header_obu(AV1_COMP *cpi,
   if (cm->intra_only) cm->frame_type = INTRA_ONLY_FRAME;
 
   aom_wb_write_bit(wb, cm->show_frame);
+#if CONFIG_BUFFER_MODEL
+  if (cm->show_frame) {
+    if (cm->timing_info_present && cm->timing_info.equal_picture_interval == 0)
+      write_tu_pts_info(cm, wb);
+  } else {
+#else
   if (!cm->show_frame) {
+#endif
     aom_wb_write_bit(wb, cm->showable_frame);
   }
   if (frame_is_sframe(cm)) {
@@ -2912,6 +2994,28 @@ static void write_uncompressed_header_obu(AV1_COMP *cpi,
     aom_wb_write_literal(wb, cm->primary_ref_frame, PRIMARY_REF_BITS);
   }
 
+#if CONFIG_BUFFER_MODEL
+  if (cm->decoder_model_info_present) {
+    aom_wb_write_bit(wb, cm->buffer_removal_delay_present);
+    if (cm->buffer_removal_delay_present) {
+      for (int op_num = 0; op_num < cm->operating_points_decoder_model_cnt;
+           op_num++) {
+        if (((cm->op_params[op_num].decoder_model_operating_point_idc >>
+              cm->temporal_layer_id) &
+                 0x1 &&
+             (cm->op_params[op_num].decoder_model_operating_point_idc >>
+              (cm->enhancement_layer_id + 8)) &
+                 0x1) ||
+            cm->op_params[op_num].decoder_model_operating_point_idc == 0) {
+          aom_wb_write_literal(
+              wb, (uint32_t)cm->op_frame_timing[op_num].buffer_removal_delay,
+              cm->buffer_model.buffer_removal_delay_length);
+          cm->op_frame_timing[op_num].buffer_removal_delay++;
+        }
+      }
+    }
+  }
+#endif
   if (cm->frame_type == KEY_FRAME) {
     write_frame_size(cm, frame_size_override_flag, wb);
     assert(av1_superres_unscaled(cm) ||
@@ -3337,16 +3441,47 @@ static uint32_t write_sequence_header_obu(AV1_COMP *cpi, uint8_t *const dst,
   for (i = 0; i < operating_points_minus1_cnt + 1; i++) {
     aom_wb_write_literal(&wb, 0, 12);  // operating_point_idc[i]
     aom_wb_write_literal(&wb, 0, 4);   // level[i]
-    aom_wb_write_literal(&wb, 0, 1);   // decoder_rate_model_present_flag[i]
+#if !CONFIG_BUFFER_MODEL
+    aom_wb_write_literal(&wb, 0, 1);  // decoder_rate_model_present_flag[i]
+#endif
   }
 
   write_sequence_header(cpi, &wb);
 
   // color_config
   write_bitdepth_colorspace_sampling(cm, &wb);
+#if CONFIG_BUFFER_MODEL
+  aom_wb_write_bit(&wb, cm->timing_info_present);  // timing info present flag
 
-  // timing_info
-  write_timing_info_header(cm, &wb);
+  if (cm->timing_info_present) {
+#endif
+    // timing_info
+    write_timing_info_header(cm, &wb);
+#if CONFIG_BUFFER_MODEL
+    aom_wb_write_bit(&wb, cm->decoder_model_info_present);
+    if (cm->decoder_model_info_present) write_decoder_model_info(cm, &wb);
+  }
+#endif
+
+#if CONFIG_BUFFER_MODEL
+  aom_wb_write_literal(&wb, cm->operating_points_decoder_model_cnt, 5);
+  for (int op_num = 0; op_num < cm->operating_points_decoder_model_cnt;
+       ++op_num) {
+    aom_wb_write_literal(
+        &wb, cm->op_params[op_num].decoder_model_operating_point_idc, 12);
+    aom_wb_write_bit(&wb,
+                     cm->op_params[op_num].display_model_param_present_flag);
+    if (cm->op_params[op_num].display_model_param_present_flag)
+      aom_wb_write_literal(&wb, cm->op_params[op_num].initial_display_delay - 1,
+                           4);
+    if (cm->decoder_model_info_present) {
+      aom_wb_write_bit(&wb,
+                       cm->op_params[op_num].decoder_model_param_present_flag);
+      if (cm->op_params[op_num].decoder_model_param_present_flag)
+        write_dec_model_op_parameters(cm, &wb, op_num);
+    }
+  }
+#endif
 
   aom_wb_write_bit(&wb, cm->film_grain_params_present);
 

bloomberg/chromium.bb

av1/av1_cx_iface.c
@@ -61,7 +61,8 @@ struct av1_extracfg {
 #endif
   unsigned int num_tg;
   unsigned int mtu_size;
-  aom_timing_info_t timing_info;
+
+  aom_timing_info_type_t timing_info_type;
   unsigned int frame_parallel_decoding_mode;
   int use_dual_filter;
   AQ_MODE aq_mode;
@@ -371,7 +372,13 @@ static aom_codec_err_t validate_config(aom_codec_alg_priv_t *ctx,
   RANGE_CHECK(extra_cfg, tuning, AOM_TUNE_PSNR, AOM_TUNE_SSIM);
 #endif
 
-  RANGE_CHECK(extra_cfg, timing_info, AOM_TIMING_UNSPECIFIED, AOM_TIMING_EQUAL);
+#if CONFIG_BUFFER_MODEL
+  RANGE_CHECK(extra_cfg, timing_info_type, AOM_TIMING_UNSPECIFIED,
+              AOM_TIMING_DEC_MODEL);
+#else
+  RANGE_CHECK(extra_cfg, timing_info_type, AOM_TIMING_UNSPECIFIED,
+              AOM_TIMING_EQUAL);
+#endif
 
   RANGE_CHECK(extra_cfg, film_grain_test_vector, 0, 16);
 
@@ -456,15 +463,52 @@ static aom_codec_err_t set_encoder_config(
   oxcf->input_bit_depth = cfg->g_input_bit_depth;
   // guess a frame rate if out of whack, use 30
   oxcf->init_framerate = (double)cfg->g_timebase.den / cfg->g_timebase.num;
-  if (extra_cfg->timing_info == AOM_TIMING_EQUAL) {
+#if CONFIG_BUFFER_MODEL
+  if (extra_cfg->timing_info_type == AOM_TIMING_EQUAL ||
+      extra_cfg->timing_info_type == AOM_TIMING_DEC_MODEL) {
+#else
+  if (extra_cfg->timing_info_type == AOM_TIMING_EQUAL) {
+#endif
     oxcf->timing_info_present = 1;
+#if CONFIG_BUFFER_MODEL
+    oxcf->timing_info.num_units_in_display_tick = cfg->g_timebase.num;
+    oxcf->timing_info.time_scale = cfg->g_timebase.den;
+    oxcf->timing_info.num_ticks_per_picture = 1;
+#else
     oxcf->num_units_in_tick = cfg->g_timebase.num;
     oxcf->time_scale = cfg->g_timebase.den;
     oxcf->equal_picture_interval = 1;
     oxcf->num_ticks_per_picture = 1;
+#endif
   } else {
     oxcf->timing_info_present = 0;
+#if CONFIG_BUFFER_MODEL
+    oxcf->operating_points_decoder_model_cnt = 0;
+#endif
   }
+#if CONFIG_BUFFER_MODEL
+  if (extra_cfg->timing_info_type == AOM_TIMING_EQUAL) {
+    oxcf->timing_info.equal_picture_interval = 1;
+    oxcf->decoder_model_info_present_flag = 0;
+    oxcf->operating_points_decoder_model_cnt = 0;
+  } else if (extra_cfg->timing_info_type == AOM_TIMING_DEC_MODEL) {
+    //    if( extra_cfg->arnr_strength > 0 )
+    //    {
+    //      printf("Only --arnr-strength=0 can currently be used with
+    //      --timing-info=model."); return AOM_CODEC_INVALID_PARAM;
+    //    }
+    //    if( extra_cfg->enable_superres)
+    //    {
+    //      printf("Only --superres-mode=0 can currently be used with
+    //      --timing-info=model."); return AOM_CODEC_INVALID_PARAM;
+    //    }
+    oxcf->buffer_model.num_units_in_decoding_tick = cfg->g_timebase.num;
+    oxcf->timing_info.equal_picture_interval = 0;
+    oxcf->decoder_model_info_present_flag = 1;
+    oxcf->buffer_removal_delay_present = 1;
+    oxcf->operating_points_decoder_model_cnt = 1;
+  }
+#endif
   if (oxcf->init_framerate > 180) {
     oxcf->init_framerate = 30;
     oxcf->timing_info_present = 0;
@@ -925,10 +969,10 @@ static aom_codec_err_t ctrl_set_mtu(aom_codec_alg_priv_t *ctx, va_list args) {
   extra_cfg.mtu_size = CAST(AV1E_SET_MTU, args);
   return update_extra_cfg(ctx, &extra_cfg);
 }
-static aom_codec_err_t ctrl_set_timing_info(aom_codec_alg_priv_t *ctx,
-                                            va_list args) {
+static aom_codec_err_t ctrl_set_timing_info_type(aom_codec_alg_priv_t *ctx,
+                                                 va_list args) {
   struct av1_extracfg extra_cfg = ctx->extra_cfg;
-  extra_cfg.timing_info = CAST(AV1E_SET_TIMING_INFO, args);
+  extra_cfg.timing_info_type = CAST(AV1E_SET_TIMING_INFO_TYPE, args);
   return update_extra_cfg(ctx, &extra_cfg);
 }
 
@@ -1136,6 +1180,7 @@ static aom_codec_err_t encoder_destroy(aom_codec_alg_priv_t *ctx) {
 // Turn on to test if supplemental superframe data breaks decoding
 #define TEST_SUPPLEMENTAL_SUPERFRAME_DATA 0
 
+#if !CONFIG_BUFFER_MODEL
 // av1 uses 10,000,000 ticks/second as time stamp
 #define TICKS_PER_SEC 10000000LL
 
@@ -1149,7 +1194,7 @@ static int64_t ticks_to_timebase_units(const aom_rational_t *timebase,
   const int64_t round = TICKS_PER_SEC * timebase->num / 2 - 1;
   return (n * timebase->den + round) / timebase->num / TICKS_PER_SEC;
 }
-
+#endif
 static aom_codec_frame_flags_t get_frame_pkt_flags(const AV1_COMP *cpi,
                                                    unsigned int lib_flags) {
   aom_codec_frame_flags_t flags = lib_flags << 16;
@@ -1271,9 +1316,15 @@ static aom_codec_err_t encoder_encode(aom_codec_alg_priv_t *ctx,
     // invisible frames get packed with the next visible frame
     while (cx_data_sz - index_size >= ctx->cx_data_sz / 2 &&
            !is_frame_visible &&
+#if CONFIG_BUFFER_MODEL
+           -1 != av1_get_compressed_data(cpi, &lib_flags, &frame_size, cx_data,
+                                         &dst_time_stamp, &dst_end_time_stamp,
+                                         !img, timebase)) {
+#else
            -1 != av1_get_compressed_data(cpi, &lib_flags, &frame_size, cx_data,
                                          &dst_time_stamp, &dst_end_time_stamp,
                                          !img)) {
+#endif
       if (cpi->common.seq_params.frame_id_numbers_present_flag) {
         if (cpi->common.invalid_delta_frame_id_minus1) {
           ctx->base.err_detail = "Invalid delta_frame_id_minus1";
@@ -1660,7 +1711,7 @@ static aom_codec_ctrl_fn_map_t encoder_ctrl_maps[] = {
 #endif
   { AV1E_SET_NUM_TG, ctrl_set_num_tg },
   { AV1E_SET_MTU, ctrl_set_mtu },
-  { AV1E_SET_TIMING_INFO, ctrl_set_timing_info },
+  { AV1E_SET_TIMING_INFO_TYPE, ctrl_set_timing_info_type },
   { AV1E_SET_FRAME_PARALLEL_DECODING, ctrl_set_frame_parallel_decoding_mode },
   { AV1E_SET_ERROR_RESILIENT_MODE, ctrl_set_error_resilient_mode },
   { AV1E_SET_S_FRAME_MODE, ctrl_set_s_frame_mode },

av1/encoder/bitstream.c
@@ -2498,20 +2498,89 @@ static void write_bitdepth_colorspace_sampling(
 
 static void write_timing_info_header(AV1_COMMON *const cm,
                                      struct aom_write_bit_buffer *wb) {
+#if !CONFIG_BUFFER_MODEL
   aom_wb_write_bit(wb, cm->timing_info_present);  // timing info present flag
 
   if (cm->timing_info_present) {
-    aom_wb_write_unsigned_literal(wb, cm->num_units_in_tick,
+#endif
+#if CONFIG_BUFFER_MODEL
+    aom_wb_write_unsigned_literal(wb, cm->timing_info.num_units_in_display_tick,
                                   32);  // Number of units in tick
-    aom_wb_write_unsigned_literal(wb, cm->time_scale, 32);  // Time scale
-    aom_wb_write_bit(wb,
-                     cm->equal_picture_interval);  // Equal picture interval bit
-    if (cm->equal_picture_interval) {
-      aom_wb_write_uvlc(wb,
-                        cm->num_ticks_per_picture - 1);  // ticks per picture
+    aom_wb_write_unsigned_literal(wb, cm->timing_info.time_scale,
+                                  32);  // Time scale
+    aom_wb_write_bit(
+        wb,
+        cm->timing_info.equal_picture_interval);  // Equal picture interval bit
+    if (cm->timing_info.equal_picture_interval) {
+      aom_wb_write_uvlc(
+          wb,
+          cm->timing_info.num_ticks_per_picture - 1);  // ticks per picture
+#else
+  aom_wb_write_unsigned_literal(wb, cm->num_units_in_tick,
+                                32);  // Number of units in tick
+  aom_wb_write_unsigned_literal(wb, cm->time_scale, 32);  // Time scale
+  aom_wb_write_bit(wb,
+                   cm->equal_picture_interval);  // Equal picture interval bit
+  if (cm->equal_picture_interval) {
+    aom_wb_write_uvlc(wb,
+                      cm->num_ticks_per_picture - 1);  // ticks per picture
+#endif
     }
+#if !CONFIG_BUFFER_MODEL
   }
+#endif
 }
+
+#if CONFIG_BUFFER_MODEL
+static void write_decoder_model_info(AV1_COMMON *const cm,
+                                     struct aom_write_bit_buffer *wb) {
+  aom_wb_write_literal(wb, cm->buffer_model.bitrate_scale, 4);
+  aom_wb_write_literal(wb, cm->buffer_model.buffer_size_scale, 4);
+  aom_wb_write_literal(
+      wb, cm->buffer_model.encoder_decoder_buffer_delay_length - 1, 5);
+  aom_wb_write_unsigned_literal(wb, cm->buffer_model.num_units_in_decoding_tick,
+                                32);  // Number of units in decoding tick
+  aom_wb_write_literal(wb, cm->buffer_model.buffer_removal_delay_length - 1, 5);
+  aom_wb_write_literal(wb, cm->buffer_model.frame_presentation_delay_length - 1,
+                       5);
+}
+
+static void write_dec_model_op_parameters(AV1_COMMON *const cm,
+                                          struct aom_write_bit_buffer *wb,
+                                          int op_num) {
+  if (op_num > MAX_NUM_OPERATING_POINTS)
+    aom_internal_error(
+        &cm->error, AOM_CODEC_UNSUP_BITSTREAM,
+        "Encoder does not support %d decoder model operating points", op_num);
+
+  //  aom_wb_write_bit(wb, cm->op_params[op_num].has_parameters);
+  //  if (!cm->op_params[op_num].has_parameters) return;
+
+  aom_wb_write_uvlc(wb, cm->op_params[op_num].bitrate - 1);
+
+  aom_wb_write_uvlc(wb, cm->op_params[op_num].buffer_size - 1);
+
+  aom_wb_write_bit(wb, cm->op_params[op_num].cbr_flag);
+
+  aom_wb_write_literal(wb, cm->op_params[op_num].decoder_buffer_delay,
+                       cm->buffer_model.encoder_decoder_buffer_delay_length);
+
+  aom_wb_write_literal(wb, cm->op_params[op_num].encoder_buffer_delay,
+                       cm->buffer_model.encoder_decoder_buffer_delay_length);
+
+  aom_wb_write_bit(wb, cm->op_params[op_num].low_delay_mode_flag);
+
+  cm->op_frame_timing[op_num].buffer_removal_delay =
+      0;  // reset the decoded frame counter
+}
+
+static void write_tu_pts_info(AV1_COMMON *const cm,
+                              struct aom_write_bit_buffer *wb) {
+  aom_wb_write_unsigned_literal(
+      wb, (uint32_t)cm->tu_presentation_delay,
+      cm->buffer_model.frame_presentation_delay_length);
+}
+#endif
 
 static void write_film_grain_params(AV1_COMP *cpi,
                                     struct aom_write_bit_buffer *wb) {
@@ -2846,6 +2915,12 @@ static void write_uncompressed_header_obu(AV1_COMP *cpi,
       aom_wb_write_bit(wb, 1);  // show_existing_frame
       aom_wb_write_literal(wb, cpi->existing_fb_idx_to_show, 3);
 
+#if CONFIG_BUFFER_MODEL
+      if (cm->decoder_model_info_present_flag &&
+          cm->timing_info.equal_picture_interval == 0) {
+        write_tu_pts_info(cm, wb);
+      }
+#endif
       if (cm->seq_params.frame_id_numbers_present_flag) {
         int frame_id_len = cm->seq_params.frame_id_length;
         int display_frame_id = cm->ref_frame_id[cpi->existing_fb_idx_to_show];
@@ -2870,7 +2945,15 @@ static void write_uncompressed_header_obu(AV1_COMP *cpi,
     if (cm->intra_only) cm->frame_type = INTRA_ONLY_FRAME;
 
     aom_wb_write_bit(wb, cm->show_frame);
+#if CONFIG_BUFFER_MODEL
+    if (cm->show_frame) {
+      if (cm->decoder_model_info_present_flag &&
+          cm->timing_info.equal_picture_interval == 0)
+        write_tu_pts_info(cm, wb);
+    } else {
+#else
     if (!cm->show_frame) {
+#endif
       aom_wb_write_bit(wb, cm->showable_frame);
     }
     if (frame_is_sframe(cm)) {
@@ -2932,6 +3015,28 @@ static void write_uncompressed_header_obu(AV1_COMP *cpi,
     }
   }
 
+#if CONFIG_BUFFER_MODEL
+  if (cm->decoder_model_info_present_flag) {
+    aom_wb_write_bit(wb, cm->buffer_removal_delay_present);
+    if (cm->buffer_removal_delay_present) {
+      for (int op_num = 0; op_num < cm->operating_points_decoder_model_cnt;
+           op_num++) {
+        if (((cm->op_params[op_num].decoder_model_operating_point_idc >>
+              cm->temporal_layer_id) &
+                 0x1 &&
+             (cm->op_params[op_num].decoder_model_operating_point_idc >>
+              (cm->enhancement_layer_id + 8)) &
+                 0x1) ||
+            cm->op_params[op_num].decoder_model_operating_point_idc == 0) {
+          aom_wb_write_literal(
+              wb, (uint32_t)cm->op_frame_timing[op_num].buffer_removal_delay,
+              cm->buffer_model.buffer_removal_delay_length);
+          cm->op_frame_timing[op_num].buffer_removal_delay++;
+        }
+      }
+    }
+  }
+#endif
   if (cm->frame_type == KEY_FRAME) {
     write_frame_size(cm, frame_size_override_flag, wb);
     assert(av1_superres_unscaled(cm) ||
@@ -3360,19 +3465,54 @@ static uint32_t write_sequence_header_obu(AV1_COMP *cpi, uint8_t *const dst,
     for (i = 0; i < operating_points_minus1_cnt + 1; i++) {
       aom_wb_write_literal(&wb, 0, 12);  // operating_point_idc[i]
       aom_wb_write_literal(&wb, 0, 4);   // level[i]
-      aom_wb_write_literal(&wb, 0, 1);   // decoder_rate_model_present_flag[i]
+#if !CONFIG_BUFFER_MODEL
+      aom_wb_write_literal(&wb, 0, 1);  // decoder_rate_model_present_flag[i]
+#endif
     }
   }
-
   write_sequence_header(cpi, &wb);
 
   // color_config
   write_bitdepth_colorspace_sampling(cm, &wb);
 
+#if !CONFIG_BUFFER_MODEL
   // timing_info
   if (!cm->seq_params.reduced_still_picture_hdr)
     write_timing_info_header(cm, &wb);
+#else
+  if (!cm->seq_params.reduced_still_picture_hdr)
+    aom_wb_write_bit(&wb, cm->timing_info_present);  // timing info present flag
 
+  if (cm->timing_info_present) {
+    // timing_info
+    write_timing_info_header(cm, &wb);
+    aom_wb_write_bit(&wb, cm->decoder_model_info_present_flag);
+    if (cm->decoder_model_info_present_flag) write_decoder_model_info(cm, &wb);
+  }
+
+  if (cm->operating_points_decoder_model_cnt > 0) {
+    aom_wb_write_bit(&wb, 1);
+    aom_wb_write_literal(&wb, cm->operating_points_decoder_model_cnt - 1, 5);
+  } else {
+    aom_wb_write_bit(&wb, 0);
+  }
+  for (int op_num = 0; op_num < cm->operating_points_decoder_model_cnt;
+       ++op_num) {
+    aom_wb_write_literal(
+        &wb, cm->op_params[op_num].decoder_model_operating_point_idc, 12);
+    aom_wb_write_bit(&wb,
+                     cm->op_params[op_num].display_model_param_present_flag);
+    if (cm->op_params[op_num].display_model_param_present_flag)
+      aom_wb_write_literal(&wb, cm->op_params[op_num].initial_display_delay - 1,
+                           4);
+    if (cm->decoder_model_info_present_flag) {
+      aom_wb_write_bit(&wb,
+                       cm->op_params[op_num].decoder_model_param_present_flag);
+      if (cm->op_params[op_num].decoder_model_param_present_flag)
+        write_dec_model_op_parameters(cm, &wb, op_num);
+    }
+  }
+#endif
   aom_wb_write_bit(&wb, cm->film_grain_params_present);
 
   add_trailing_bits(&wb);