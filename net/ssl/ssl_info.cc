d848ae1f3a6e1debed4b8297c2420092a1833bbb
David Benjamin <davidben@chromium.org>
2018-04-10 17:14:07 +0000

    Make SSLInfo::Reset less error-prone.
    Take advantage of inline member initializers, which are harder to
    forget, and operator=, which the compiler generates for us.
    
    Bug: none
    Change-Id: I6658cab836444ea74ae237fe3c065edebc400615
    Reviewed-on: https://chromium-review.googlesource.com/1003553
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549567}
    Former-commit-id: b00da14fdda9e49de9976a6c35c806bc83e40304

--- net/ssl/ssl_info.cc
+++ net/ssl/ssl_info.cc
@@ -4,52 +4,20 @@
 
 #include "net/ssl/ssl_info.h"
 
-#include "base/pickle.h"
-#include "base/stl_util.h"
-#include "net/cert/cert_status_flags.h"
-#include "net/cert/ct_policy_status.h"
-#include "net/cert/signed_certificate_timestamp.h"
 #include "net/cert/x509_certificate.h"
-#include "net/ssl/ssl_connection_status_flags.h"
-#include "third_party/boringssl/src/include/openssl/ssl.h"
 
 namespace net {
 
-SSLInfo::SSLInfo() {
-  Reset();
-}
+SSLInfo::SSLInfo() = default;
 
-SSLInfo::SSLInfo(const SSLInfo& info) {
-  *this = info;
-}
+SSLInfo::SSLInfo(const SSLInfo& info) = default;
 
 SSLInfo::~SSLInfo() = default;
 
 SSLInfo& SSLInfo::operator=(const SSLInfo& info) = default;
 
 void SSLInfo::Reset() {
-  cert = NULL;
-  unverified_cert = NULL;
-  cert_status = 0;
-  security_bits = -1;
-  key_exchange_group = 0;
-  connection_status = 0;
-  is_issued_by_known_root = false;
-  pkp_bypassed = false;
-  client_cert_sent = false;
-  channel_id_sent = false;
-  token_binding_negotiated = false;
-  token_binding_key_param = TB_PARAM_ECDSAP256;
-  dummy_pq_padding_received = false;
-  handshake_type = HANDSHAKE_UNKNOWN;
-  base::STLClearObject(&public_key_hashes);
-  base::STLClearObject(&pinning_failure_log);
-  base::STLClearObject(&signed_certificate_timestamps);
-  ct_policy_compliance =
-      ct::CTPolicyCompliance::CT_POLICY_COMPLIANCE_DETAILS_NOT_AVAILABLE;
-  ct_policy_compliance_required = false;
-  ocsp_result = OCSPVerifyResult();
-  is_fatal_cert_error = false;
+  *this = SSLInfo();
 }
 
 void SSLInfo::SetCertError(int error) {
