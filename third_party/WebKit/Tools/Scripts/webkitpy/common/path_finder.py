8264548af7a87cdda99d41c4ee926868ae43645f
Kent Tamura <tkent@chromium.org>
2018-04-11 01:23:21 +0000

    webkitpy: Remove unnecessary code for third_party/WebKit/Source
    Bug: 768828
    Change-Id: I1a0b5f3542c53db34111a5411beb57b7fbcb189c
    Reviewed-on: https://chromium-review.googlesource.com/1004877
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549713}
    Former-commit-id: 8cc7765241a07b47db97bbddae2fa74148001d16

--- third_party/WebKit/Tools/Scripts/webkitpy/common/path_finder.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/path_finder.py
@@ -67,17 +67,8 @@ def get_scripts_dir():
         os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 
-@memoized
 def get_source_dir():
-    post_move_path = os.path.join(get_chromium_src_dir(), 'third_party', 'blink', 'renderer')
-    if os.path.exists(post_move_path):
-        return post_move_path
-    return os.path.join(get_blink_dir(), 'Source')
-
-
-# TODO(tkent): Remove this function after the source move. crbug.com/622551
-def is_source_in_blink():
-    return '/blink/renderer' in get_source_dir().replace(os.path.sep, '/')
+    return os.path.join(get_chromium_src_dir(), 'third_party', 'blink', 'renderer')
 
 
 def get_typ_dir():
@@ -132,13 +123,9 @@ class PathFinder(object):
     def path_from_chromium_base(self, *comps):
         return self._filesystem.join(self.chromium_base(), *comps)
 
-    @memoized
     def _blink_source_dir(self):
-        post_move_path = self._filesystem.join(
+        return self._filesystem.join(
             self.chromium_base(), 'third_party', 'blink', 'renderer')
-        if self._filesystem.exists(post_move_path):
-            return post_move_path
-        return self._filesystem.join(self._webkit_base(), 'Source')
 
     def path_from_blink_source(self, *comps):
         return self._filesystem.join(self._blink_source_dir(), *comps)
