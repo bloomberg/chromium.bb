e710b573458ef54cba2e5f209a15fea64e8b542e
Kenneth Russell <kbr@chromium.org>
2018-04-11 00:32:53 +0000

    Mark Pixel_OffscreenCanvasTransferToImageBitmap flaky on Windows.
    Already marked flaky on Linux and Android.
    
    NOTRY=true
    TBR=xlai@chromium.org, trchen@chromium.org, ynovikov@chromium.org
    
    Bug: 807742
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I883f6f0e21f20aebdff32071de29d4f02c477c72
    Reviewed-on: https://chromium-review.googlesource.com/1006357
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549696}
    Former-commit-id: 88ce09b95a8289cf5ca0c27b5265ea5ad9f8ea96

--- content/test/gpu/gpu_tests/pixel_expectations.py
+++ content/test/gpu/gpu_tests/pixel_expectations.py
@@ -82,7 +82,7 @@ class PixelExpectations(GpuTestExpectations):
     self.Flaky('Pixel_OffscreenCanvasTransferAfterStyleResize',
               ['mac', 'linux', 'win', 'android'], bug=735171)
     self.Flaky('Pixel_OffscreenCanvasTransferToImageBitmap',
-              ['linux', 'android'], bug=807742)
+              ['linux', 'win', 'android'], bug=807742)
 
     self.Flaky('Pixel_OffscreenCanvasWebGLSoftwareCompositingWorker',
         ['mac', ('nvidia', 0xfe9), 'debug'], bug=751328)
