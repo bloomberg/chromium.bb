60b19d9b40d1ddcac75f6c9d69b063432490557d
Yuly Novikov <ynovikov@chromium.org>
2018-04-11 05:39:51 +0000

    Update WebGL2 CTS expectations
    conformance2/transform_feedback/too-small-buffers.html
    is also flaky on non-passthrough Linux NVIDIA
    
    BUG=818383
    TBR=kbr@chromium.org
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I73df9ae3b32d253251820b5495e8c77fa01af8f9
    Reviewed-on: https://chromium-review.googlesource.com/1006363
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549777}
    Former-commit-id: 6bb2d8a58c8f79e93ad5e9da1abc7cf4ac45efc9

--- content/test/gpu/gpu_tests/webgl2_conformance_expectations.py
+++ content/test/gpu/gpu_tests/webgl2_conformance_expectations.py
@@ -135,7 +135,7 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
     self.Fail('conformance2/transform_feedback/too-small-buffers.html',
         ['win', 'nvidia'], bug=818383)
     self.Fail('conformance2/transform_feedback/too-small-buffers.html',
-        ['linux', 'nvidia', 'passthrough'], bug=818383)
+        ['linux', 'nvidia'], bug=818383)
     self.Fail('conformance2/transform_feedback/too-small-buffers.html',
         ['mac', 'amd'], bug=818383)
 
