cb10fb19720aca1913e90686817a16bd322bfbfd
Ian Clelland <iclelland@chromium.org>
2018-04-11 02:23:07 +0000

    Make DOMTokenList.supports() case-insensitive
    According to the DOM spec (https://dom.spec.whatwg.org/#dom-domtokenlist-supports)
    the supports() method should compare the ASCII-lowercase transform of its input to
    the list of tokens supported by the DOMTokenList.
    
    Bug: 831166
    Change-Id: I40903a307ffb949bbbbc99e02b56565cb81d00ed
    Reviewed-on: https://chromium-review.googlesource.com/1005412
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549733}
    Former-commit-id: 5d969dd6c09fe37e89de502e16983274fa39567c

--- third_party/blink/renderer/core/dom/dom_token_list.cc
+++ third_party/blink/renderer/core/dom/dom_token_list.cc
@@ -228,7 +228,7 @@ bool DOMTokenList::replace(const AtomicString& token,
 
 bool DOMTokenList::supports(const AtomicString& token,
                             ExceptionState& exception_state) {
-  return ValidateTokenValue(token, exception_state);
+  return ValidateTokenValue(token.LowerASCII(), exception_state);
 }
 
 // https://dom.spec.whatwg.org/#dom-domtokenlist-add
