f11e450bf00f353208a2a998089d94f5d3ed22ee
Timothy Loh <timloh@chromium.org>
2018-04-11 04:09:33 +0000

    Update the app launcher when the Crostini App Registry receives updates
    This patch adds an observer interface to the CrostiniAppRegistry so that
    the CrostiniAppModelBuilder can be updated when the app registry is
    updated.
    
    We also fix the CrostiniAppRegistry to not delete registrations from
    unrelated containers when receiving an update, to allow support for
    multiple VMs or containers.
    
    Bug: 821662
    Change-Id: I2ee679c94a7d833ad83a28da3120ad5eb3d67737
    Reviewed-on: https://chromium-review.googlesource.com/1001077
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549753}
    Former-commit-id: d4853f2bcda70fb641b4c46c6ebed522bb070652

--- chrome/browser/ui/app_list/crostini/crostini_app_model_builder.cc
+++ chrome/browser/ui/app_list/crostini/crostini_app_model_builder.cc
@@ -5,7 +5,6 @@
 #include "chrome/browser/ui/app_list/crostini/crostini_app_model_builder.h"
 
 #include "ash/resources/grit/ash_resources.h"
-#include "chrome/browser/chromeos/crostini/crostini_registry_service.h"
 #include "chrome/browser/chromeos/crostini/crostini_registry_service_factory.h"
 #include "chrome/browser/ui/app_list/app_list_controller_delegate.h"
 #include "chrome/browser/ui/app_list/crostini/crostini_app_item.h"
@@ -21,7 +20,10 @@ CrostiniAppModelBuilder::CrostiniAppModelBuilder(
     AppListControllerDelegate* controller)
     : AppListModelBuilder(controller, CrostiniAppItem::kItemType) {}
 
-CrostiniAppModelBuilder::~CrostiniAppModelBuilder() = default;
+CrostiniAppModelBuilder::~CrostiniAppModelBuilder() {
+  // We don't need to remove ourself from the registry's observer list as these
+  // are both KeyedServices (this class lives on AppListSyncableService).
+}
 
 void CrostiniAppModelBuilder::BuildModel() {
   static const std::string kCrostiniTerminalId =
@@ -35,18 +37,42 @@ void CrostiniAppModelBuilder::BuildModel() {
   chromeos::CrostiniRegistryService* registry_service =
       chromeos::CrostiniRegistryServiceFactory::GetForProfile(profile());
   for (const std::string& app_id : registry_service->GetRegisteredAppIds()) {
-    std::unique_ptr<chromeos::CrostiniRegistryService::Registration>
-        registration = registry_service->GetRegistration(app_id);
-    DCHECK(registration);
-    if (registration->no_display)
-      continue;
-    const std::string& localized_name =
-        chromeos::CrostiniRegistryService::Registration::Localize(
-            registration->name);
-    // TODO(timloh): Use a real icon
-    InsertApp(std::make_unique<CrostiniAppItem>(
-        profile(), GetSyncItem(app_id), app_id, localized_name,
-        ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
-            IDR_LOGO_CROSTINI_TERMINAL)));
+    InsertCrostiniAppItem(registry_service, app_id);
   }
+
+  registry_service->AddObserver(this);
+}
+
+void CrostiniAppModelBuilder::InsertCrostiniAppItem(
+    const chromeos::CrostiniRegistryService* registry_service,
+    const std::string& app_id) {
+  std::unique_ptr<chromeos::CrostiniRegistryService::Registration>
+      registration = registry_service->GetRegistration(app_id);
+  DCHECK(registration);
+  if (registration->no_display)
+    return;
+  const std::string& localized_name =
+      chromeos::CrostiniRegistryService::Registration::Localize(
+          registration->name);
+  // TODO(timloh): Use a real icon
+  InsertApp(std::make_unique<CrostiniAppItem>(
+      profile(), GetSyncItem(app_id), app_id, localized_name,
+      ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
+          IDR_LOGO_CROSTINI_TERMINAL)));
+}
+
+void CrostiniAppModelBuilder::OnRegistryUpdated(
+    chromeos::CrostiniRegistryService* registry_service,
+    const std::vector<std::string>& updated_apps,
+    const std::vector<std::string>& removed_apps,
+    const std::vector<std::string>& inserted_apps) {
+  const bool unsynced_change = false;
+  for (const std::string& app_id : removed_apps)
+    RemoveApp(app_id, unsynced_change);
+  for (const std::string& app_id : updated_apps) {
+    RemoveApp(app_id, unsynced_change);
+    InsertCrostiniAppItem(registry_service, app_id);
+  }
+  for (const std::string& app_id : inserted_apps)
+    InsertCrostiniAppItem(registry_service, app_id);
 }
