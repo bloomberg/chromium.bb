83efa40d1015166a77aa7f723aa459093a651a1f
Sunny Sachanandani <sunnyps@chromium.org>
2018-04-12 01:17:17 +0000

    Put device removed reason in the correct scope
    We want |reason_hr| to be in scope so that the debugger can show it.
    Otherwise it's still on the stack at [rbp-10h], and you have to view the
    value in raw memory.
    
    R=piman
    CC=zmo
    BUG=794735
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I4ecf1ce80885a64fd34a3aae235bab7a84ea9972
    Reviewed-on: https://chromium-review.googlesource.com/1008629
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#549991}
    Former-commit-id: 948c9c02ab6188e9e00f1ea4a31d1e544add7413

--- ui/gl/gl_image_dxgi.cc
+++ ui/gl/gl_image_dxgi.cc
@@ -239,10 +239,10 @@ bool CopyingGLImageDXGI::Initialize() {
       &desc, nullptr, decoder_copy_texture_.GetAddressOf());
   // TODO(sunnyps): Remove after fixing https://crbug.com/794735
   base::debug::Alias(&hr);
-  if (hr == DXGI_ERROR_DEVICE_REMOVED) {
-    HRESULT reason_hr = d3d11_device_->GetDeviceRemovedReason();
-    base::debug::Alias(&reason_hr);
-  }
+  HRESULT reason_hr = S_OK;
+  base::debug::Alias(&reason_hr);
+  if (hr == DXGI_ERROR_DEVICE_REMOVED)
+    reason_hr = d3d11_device_->GetDeviceRemovedReason();
   CHECK(SUCCEEDED(hr));
   EGLDisplay egl_display = gl::GLSurfaceEGL::GetHardwareDisplay();
 
