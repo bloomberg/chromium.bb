577ab3eb512c2c59b77336b33893177aa4f2ee36
Matt Giuca <mgiuca@chromium.org>
2018-04-12 22:07:54 +0000

    Added null checks for properties on ToolbarView that can be null.
    BrowserView's toolbar() will have null app_menu_button() and
    browser_actions() in hosted app windows, which could result in null
    dereferences in certain corner cases. This adds null checks where it's
    obviously safe to do so.
    
    A better fix would be to use toolbar_button_provider() to access the
    hosted app window's real app menu button and actions container, but that
    requires more careful consideration.
    
    Bug: 826596, 811982
    Change-Id: Iea0a0ea3acffe5ed1016e61bd8ae2fe36971c79d
    Reviewed-on: https://chromium-review.googlesource.com/1010002
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: Matt Giuca <mgiuca@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550381}
    Former-commit-id: 8c852c8db3273cf728bdeb36b5ecd152aea8271c

--- chrome/browser/feature_engagement/incognito_window/incognito_window_tracker.cc
+++ chrome/browser/feature_engagement/incognito_window/incognito_window_tracker.cc
@@ -23,13 +23,13 @@ constexpr int kDefaultIncognitoWindowPromoShowTimeInHours = 2;
 constexpr char kIncognitoWindowObservedSessionTimeKey[] =
     "incognito_window_in_product_help_observed_session_time_key";
 
+// Note: May return null.
 BrowserAppMenuButton* GetAppMenuButton() {
   auto* browser = BrowserView::GetBrowserViewForBrowser(
       BrowserList::GetInstance()->GetLastActive());
   DCHECK(browser);
   DCHECK(browser->IsActive());
   DCHECK(browser->toolbar());
-  DCHECK(browser->toolbar()->app_menu_button());
   return browser->toolbar()->app_menu_button();
 }
 
@@ -53,7 +53,6 @@ void IncognitoWindowTracker::OnIncognitoWindowOpened() {
 
 void IncognitoWindowTracker::OnBrowsingDataCleared() {
   auto* app_menu_button = GetAppMenuButton();
-  // TODO(bdea): Remove early return once https://crbug.com/811982 is fixed.
   if (!app_menu_button)
     return;
 
@@ -87,7 +86,9 @@ void IncognitoWindowTracker::OnWidgetDestroying(views::Widget* widget) {
 
   if (incognito_promo_observer_.IsObserving(widget)) {
     incognito_promo_observer_.Remove(widget);
-    GetAppMenuButton()->SetIsProminent(false);
+    BrowserAppMenuButton* app_menu_button = GetAppMenuButton();
+    if (app_menu_button)
+      app_menu_button->SetIsProminent(false);
   }
 }
 
