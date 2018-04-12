8b58b13c93e811b27595c6c8770222996d86f73d
Daniel Park <danielpark@chromium.org>
2018-04-12 19:59:54 +0000

    [Home Button] Don't cache home page settings if OEM enabled
    Bug: 832212
    Change-Id: If9f76cf12ac58c8367c16275796acd2e38847051
    Reviewed-on: https://chromium-review.googlesource.com/1010837
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Daniel Park <danielpark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550326}
    Former-commit-id: e950badfe6154dbb0a6fede954c5b211ce17c79d

--- chrome/android/java/src/org/chromium/chrome/browser/util/FeatureUtilities.java
+++ chrome/android/java/src/org/chromium/chrome/browser/util/FeatureUtilities.java
@@ -25,6 +25,7 @@ import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.firstrun.FirstRunUtils;
 import org.chromium.chrome.browser.locale.LocaleManager;
+import org.chromium.chrome.browser.partnercustomizations.PartnerBrowserCustomizations;
 import org.chromium.chrome.browser.preferences.ChromePreferenceManager;
 import org.chromium.chrome.browser.tabmodel.DocumentModeAssassin;
 import org.chromium.components.signin.AccountManagerFacade;
@@ -188,6 +189,7 @@ public class FeatureUtilities {
      * be made available immediately.
      */
     public static void cacheHomePageButtonForceEnabled() {
+        if (PartnerBrowserCustomizations.isHomepageProviderAvailableAndEnabled()) return;
         ChromePreferenceManager.getInstance().setHomePageButtonForceEnabled(
                 ChromeFeatureList.isEnabled(ChromeFeatureList.HOME_PAGE_BUTTON_FORCE_ENABLED));
     }
