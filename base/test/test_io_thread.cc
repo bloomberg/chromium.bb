afd2265e9a62841256359b730da745c3b035c90f
tzik <tzik@chromium.org>
2018-04-12 10:31:52 +0000

    Migrate base::TestIOThread to OnceCallback
    base::TestIOThread::PostTask is a left-over of the OnceCallback support
    of TaskRunner::PostTask, that is needed by its user to migrate to
    OnceCallback.
    
    Bug: 714018
    Change-Id: Iceb18fb84bde555e4fa217c32e463e01adf034a1
    Reviewed-on: https://chromium-review.googlesource.com/997313
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550135}
    Former-commit-id: fbf68c4d029c34fa518ec08310f1c7d704fad6ba

--- base/test/test_io_thread.cc
+++ base/test/test_io_thread.cc
@@ -38,9 +38,8 @@ void TestIOThread::Stop() {
   io_thread_started_ = false;
 }
 
-void TestIOThread::PostTask(const Location& from_here,
-                            const base::Closure& task) {
-  task_runner()->PostTask(from_here, task);
+void TestIOThread::PostTask(const Location& from_here, base::OnceClosure task) {
+  task_runner()->PostTask(from_here, std::move(task));
 }
 
 }  // namespace base
