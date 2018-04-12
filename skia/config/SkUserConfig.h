260b496f4e2402d6ab94152afd5934d93fba672c
Yuqian Li <liyuqian@google.com>
2018-04-12 15:08:12 +0000

    Add SK_SUPPORT_LEGACY_PATH_DAA_BIT
    Bug: chromium:821353
    
    Change-Id: I69a86f539e5e7d3b038fd085abfbba0826eb04bc
    Reviewed-on: https://chromium-review.googlesource.com/1008043
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Yuqian Li <liyuqian@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550201}
    Former-commit-id: 997bf7ed26db3d5dd46d68bfd188bd6c8c68d24a

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -225,6 +225,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define GR_AA_TESSELLATOR_MAX_VERB_COUNT 10
 
 
+#ifndef SK_SUPPORT_LEGACY_PATH_DAA_BIT
+#define SK_SUPPORT_LEGACY_PATH_DAA_BIT
+#endif
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
