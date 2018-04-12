f910556971ae8a5ccb9fb7341d1aa5f3aeff4996
Jonas Olsson <jonasolsson@chromium.org>
2018-04-12 10:44:46 +0000

    Measure stream creation times in Mojo audio IPC
    Media.Audio.Render.[Input|Output]DeviceStreamCreationTime measures the
    time from stream creation starting until the StreamCreated callback
    gets triggered.
    
    Bug: chromium:672469
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I32f50b8cf19afea7b97ae415fce17308085567bf
    Reviewed-on: https://chromium-review.googlesource.com/1000855
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Jonas Olsson <jonasolsson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550138}
    Former-commit-id: 488accfc5949582c5280e4930ac91a1382b25ae0

--- content/renderer/media/mojo_audio_input_ipc.h
+++ content/renderer/media/mojo_audio_input_ipc.h
@@ -11,6 +11,7 @@
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
+#include "base/time/time.h"
 #include "content/common/content_export.h"
 #include "content/common/media/renderer_audio_input_stream_factory.mojom.h"
 #include "media/audio/audio_input_ipc.h"
@@ -67,6 +68,8 @@ class CONTENT_EXPORT MojoAudioInputIPC
   mojo::Binding<AudioInputStreamClient> stream_client_binding_;
   mojo::Binding<RendererAudioInputStreamFactoryClient> factory_client_binding_;
   media::AudioInputIPCDelegate* delegate_ = nullptr;
+
+  base::TimeTicks stream_creation_start_time_;
 
   base::WeakPtrFactory<MojoAudioInputIPC> weak_factory_;
 
