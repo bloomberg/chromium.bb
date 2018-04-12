4215aa37d0fe67f65447dddf25e35ab57ff0f817
Noel Gordon <noel@chromium.org>
2018-04-12 11:17:48 +0000

    [ChromeOS] Disable DriveSpecific/FileManagerBrowserTest/Test.*
    This test group (5 tests in all) flakes more often than desired on chromium
    trys (the CQ). Disable them while the flake cause is being investigated.
    
    Note: these tests are only built and run on ChromeOS. TODO(noel) once issue
    829310 is resolved, this change can be reverted.
    
    Bug: 829310
    Change-Id: If6e30690d9076debfd988a1fa0d7c93575099a14
    Reviewed-on: https://chromium-review.googlesource.com/1006746
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Henrik Grunell <grunell@chromium.org>
    Reviewed-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550143}
    Former-commit-id: d65b43d69f13e93abcb8a8d73a56abf6fc2d2cd6

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -248,13 +248,9 @@ WRAPPED_INSTANTIATE_TEST_CASE_P(
                       "rectory")));
 
 // Fails on official build. http://crbug.com/429294
-#if defined(DISABLE_SLOW_FILESAPP_TESTS) || defined(OFFICIAL_BUILD)
-#define MAYBE_DriveSpecific DISABLED_DriveSpecific
-#else
-#define MAYBE_DriveSpecific DriveSpecific
-#endif
+// Disabled: too often flakey on chromium trys. http://crbug.com/829310
 WRAPPED_INSTANTIATE_TEST_CASE_P(
-    MAYBE_DriveSpecific,
+    DISABLED_DriveSpecific,
     FileManagerBrowserTest,
     ::testing::Values(
         TestParameter(NOT_IN_GUEST_MODE, "openSidebarOffline"),
