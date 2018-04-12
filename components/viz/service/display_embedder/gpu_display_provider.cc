ea755fca66690e782f65db228777e1e0d22a015f
Kristian H. Kristensen <hoegsberg@chromium.org>
2018-04-12 17:15:28 +0000

    viz: Use GL_BGRA_EXT internalformat for GpuDisplayProvider
    One last spot where we need to adjust the internalformat to match the
    new default buffer format on Ozone.
    
    Bug: 829943
    Test: Boot to login, verify screen not black
    Change-Id: Id45eac8cbd029dfbaea03d46effe5efb1fc7dc55
    Reviewed-on: https://chromium-review.googlesource.com/1000406
    Commit-Queue: Kristian H. Kristensen <hoegsberg@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550247}
    Former-commit-id: 25be9395356503624b4b7888647155666fd58cbd

--- components/viz/service/display_embedder/gpu_display_provider.cc
+++ components/viz/service/display_embedder/gpu_display_provider.cc
@@ -130,7 +130,7 @@ std::unique_ptr<Display> GpuDisplayProvider::CreateDisplay(
       output_surface = std::make_unique<GLOutputSurfaceOzone>(
           std::move(context_provider), surface_handle,
           synthetic_begin_frame_source.get(), gpu_memory_buffer_manager_.get(),
-          GL_TEXTURE_2D, GL_RGB);
+          GL_TEXTURE_2D, GL_BGRA_EXT);
 #elif defined(OS_MACOSX)
       output_surface = std::make_unique<GLOutputSurfaceMac>(
           std::move(context_provider), surface_handle,
