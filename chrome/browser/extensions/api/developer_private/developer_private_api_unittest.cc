6779880edbf1c91e467e5b571202e53bae343f93
Istiaque Ahmed <lazyboy@chromium.org>
2018-04-13 04:49:46 +0000

    Reland "Extensions: Rename DictionaryBuilder::SetBooean -> Set. Same for ListBuilder."
    This is a reland of 27bc727a65f68fe8693fbe189b79f06a1a8a17f2 [formerly fef93a84a5b56945d02cada97f4ec886ae34c0e2]
    
    There is no change in the reland CL, the previous attempt collided with
    another CL that was reverted at 7999d9c2a5920ea63cedd6673f26e234c3663247 [formerly 348808c076714a5422e9f6be8aca7e206d4c7464]
    
    
    Original change's description:
    > Extensions: Rename DictionaryBuilder::SetBooean -> Set. Same for ListBuilder.
    >
    > The overload resolution failure isn't any issue anymore after
    > https://chromium.googlesource.com/chromium/src/+/e0aa07231f84984f2bd3e6e7a455442429786394 [formerly 723fb47254fc5f49bda577df447303dc3554beb9],
    > so bool overload can be provided for DictionaryBuilder::Set and ListBuilder::Append.
    >
    > Bug: None
    > Test: Internal only change.
    > Change-Id: Id55283aeaa8868cf533f206b006c3c909a835df1
    > Reviewed-on: https://chromium-review.googlesource.com/1009489
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    > Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#550475}
    
    TBR=avi@chromium.org,lazyboy@chromium.org,rdevlin.cronin@chromium.org
    
    Bug: None
    Change-Id: I2896a13c9421b1c08076bb5f756b7630f050c404
    Reviewed-on: https://chromium-review.googlesource.com/1011711
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550522}
    Former-commit-id: 6f874680b9e1dcaeb15ccef939fdf26ea108380b

--- chrome/browser/extensions/api/developer_private/developer_private_api_unittest.cc
+++ chrome/browser/extensions/api/developer_private/developer_private_api_unittest.cc
@@ -299,9 +299,7 @@ void DeveloperPrivateApiUnitTest::UpdateProfileConfigurationDevMode(
       new api::DeveloperPrivateUpdateProfileConfigurationFunction());
   std::unique_ptr<base::ListValue> args =
       ListBuilder()
-          .Append(DictionaryBuilder()
-                      .SetBoolean("inDeveloperMode", dev_mode)
-                      .Build())
+          .Append(DictionaryBuilder().Set("inDeveloperMode", dev_mode).Build())
           .Build();
   EXPECT_TRUE(RunFunction(function, *args)) << function->GetError();
 }
