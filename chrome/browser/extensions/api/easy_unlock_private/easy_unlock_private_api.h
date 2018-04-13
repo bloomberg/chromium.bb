9ab9bcdb489ae88e8e84e7fb5df0f61c91ea54d4
James Hawkins <jhawkins@chromium.org>
2018-04-13 23:21:38 +0000

    extensions/EasyUnlockPrivateApi: Remove unused methods.
    R=hansberry@chromium.org, tbarzic@chromium.org
    
    Bug: none
    Test: none
    Change-Id: I0a02c8bc0cd6a5a38f88ead70d6876f9014045ce
    Reviewed-on: https://chromium-review.googlesource.com/1012449
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550804}
    Former-commit-id: 92ac2271938502cf29d149c127fce574a503d5d7

--- chrome/browser/extensions/api/easy_unlock_private/easy_unlock_private_api.h
+++ chrome/browser/extensions/api/easy_unlock_private/easy_unlock_private_api.h
@@ -27,8 +27,6 @@ class BrowserContext;
 
 namespace cryptauth {
 class Connection;
-class ExternalDeviceInfo;
-class SecureMessageDelegate;
 }
 
 namespace proximity_auth {
@@ -38,7 +36,6 @@ class BluetoothLowEnergyConnectionFinder;
 namespace extensions {
 
 class EasyUnlockPrivateConnectionManager;
-class EasyUnlockPrivateCryptoDelegate;
 
 class EasyUnlockPrivateAPI : public BrowserContextKeyedAPI {
  public:
@@ -49,8 +46,6 @@ class EasyUnlockPrivateAPI : public BrowserContextKeyedAPI {
 
   explicit EasyUnlockPrivateAPI(content::BrowserContext* context);
   ~EasyUnlockPrivateAPI() override;
-
-  EasyUnlockPrivateCryptoDelegate* GetCryptoDelegate();
 
   EasyUnlockPrivateConnectionManager* get_connection_manager() {
     return connection_manager_.get();
@@ -64,8 +59,6 @@ class EasyUnlockPrivateAPI : public BrowserContextKeyedAPI {
 
   // KeyedService implementation.
   void Shutdown() override;
-
-  std::unique_ptr<EasyUnlockPrivateCryptoDelegate> crypto_delegate_;
 
   std::unique_ptr<EasyUnlockPrivateConnectionManager> connection_manager_;
 
@@ -87,198 +80,6 @@ class EasyUnlockPrivateGetStringsFunction : public UIThreadExtensionFunction {
                              EASYUNLOCKPRIVATE_GETSTRINGS)
 
   DISALLOW_COPY_AND_ASSIGN(EasyUnlockPrivateGetStringsFunction);
-};
-
-class EasyUnlockPrivatePerformECDHKeyAgreementFunction
-    : public UIThreadExtensionFunction {
- public:
-  EasyUnlockPrivatePerformECDHKeyAgreementFunction();
-
- protected:
-  ~EasyUnlockPrivatePerformECDHKeyAgreementFunction() override;
-
-  // ExtensionFunction:
-  ResponseAction Run() override;
-
- private:
-  void OnData(const std::string& secret_key);
-
-  DECLARE_EXTENSION_FUNCTION("easyUnlockPrivate.performECDHKeyAgreement",
-                             EASYUNLOCKPRIVATE_PERFORMECDHKEYAGREEMENT)
-
-  DISALLOW_COPY_AND_ASSIGN(EasyUnlockPrivatePerformECDHKeyAgreementFunction);
-};
-
-class EasyUnlockPrivateGenerateEcP256KeyPairFunction
-    : public UIThreadExtensionFunction {
- public:
-  EasyUnlockPrivateGenerateEcP256KeyPairFunction();
-
- protected:
-  ~EasyUnlockPrivateGenerateEcP256KeyPairFunction() override;
-
-  // ExtensionFunction:
-  ResponseAction Run() override;
-
- private:
-  void OnData(const std::string& public_key,
-              const std::string& private_key);
-
-  DECLARE_EXTENSION_FUNCTION("easyUnlockPrivate.generateEcP256KeyPair",
-                             EASYUNLOCKPRIVATE_GENERATEECP256KEYPAIR)
-
-  DISALLOW_COPY_AND_ASSIGN(EasyUnlockPrivateGenerateEcP256KeyPairFunction);
-};
-
-class EasyUnlockPrivateCreateSecureMessageFunction
-    : public UIThreadExtensionFunction {
- public:
-  EasyUnlockPrivateCreateSecureMessageFunction();
-
- protected:
-  ~EasyUnlockPrivateCreateSecureMessageFunction() override;
-
-  // ExtensionFunction:
-  ResponseAction Run() override;
-
- private:
-  void OnData(const std::string& message);
-
-  DECLARE_EXTENSION_FUNCTION("easyUnlockPrivate.createSecureMessage",
-                             EASYUNLOCKPRIVATE_CREATESECUREMESSAGE)
-
-  DISALLOW_COPY_AND_ASSIGN(EasyUnlockPrivateCreateSecureMessageFunction);
-};
-
-class EasyUnlockPrivateUnwrapSecureMessageFunction
-    : public UIThreadExtensionFunction {
- public:
-  EasyUnlockPrivateUnwrapSecureMessageFunction();
-
- protected:
-  ~EasyUnlockPrivateUnwrapSecureMessageFunction() override;
-
-  // ExtensionFunction:
-  ResponseAction Run() override;
-
- private:
-  void OnData(const std::string& data);
-
-  DECLARE_EXTENSION_FUNCTION("easyUnlockPrivate.unwrapSecureMessage",
-                             EASYUNLOCKPRIVATE_UNWRAPSECUREMESSAGE)
-
-  DISALLOW_COPY_AND_ASSIGN(EasyUnlockPrivateUnwrapSecureMessageFunction);
-};
-
-class EasyUnlockPrivateGetPermitAccessFunction
-    : public UIThreadExtensionFunction {
- public:
-  DECLARE_EXTENSION_FUNCTION("easyUnlockPrivate.getPermitAccess",
-                             EASYUNLOCKPRIVATE_GETPERMITACCESS)
-  EasyUnlockPrivateGetPermitAccessFunction();
-
- protected:
-  ~EasyUnlockPrivateGetPermitAccessFunction() override;
-
-  // Writes the user's public and private key in base64 form to the
-  // |user_public_key| and |user_private_key| fields. Exposed for testing.
-  virtual void GetKeyPairForExperiment(std::string* user_public_key,
-                                       std::string* user_private_key);
-
- private:
-  // ExtensionFunction:
-  ResponseAction Run() override;
-
-  DISALLOW_COPY_AND_ASSIGN(EasyUnlockPrivateGetPermitAccessFunction);
-};
-
-class EasyUnlockPrivateClearPermitAccessFunction
-    : public UIThreadExtensionFunction {
- public:
-  DECLARE_EXTENSION_FUNCTION("easyUnlockPrivate.clearPermitAccess",
-                             EASYUNLOCKPRIVATE_CLEARPERMITACCESS)
-  EasyUnlockPrivateClearPermitAccessFunction();
-
- private:
-  ~EasyUnlockPrivateClearPermitAccessFunction() override;
-
-  // ExtensionFunction:
-  ResponseAction Run() override;
-
-  DISALLOW_COPY_AND_ASSIGN(EasyUnlockPrivateClearPermitAccessFunction);
-};
-
-class EasyUnlockPrivateSetRemoteDevicesFunction
-    : public UIThreadExtensionFunction {
- public:
-  DECLARE_EXTENSION_FUNCTION("easyUnlockPrivate.setRemoteDevices",
-                             EASYUNLOCKPRIVATE_SETREMOTEDEVICES)
-  EasyUnlockPrivateSetRemoteDevicesFunction();
-
- private:
-  ~EasyUnlockPrivateSetRemoteDevicesFunction() override;
-
-  // ExtensionFunction:
-  ResponseAction Run() override;
-
-  DISALLOW_COPY_AND_ASSIGN(EasyUnlockPrivateSetRemoteDevicesFunction);
-};
-
-class EasyUnlockPrivateGetRemoteDevicesFunction
-    : public UIThreadExtensionFunction {
- public:
-  DECLARE_EXTENSION_FUNCTION("easyUnlockPrivate.getRemoteDevices",
-                             EASYUNLOCKPRIVATE_GETREMOTEDEVICES)
-  EasyUnlockPrivateGetRemoteDevicesFunction();
-
- protected:
-  ~EasyUnlockPrivateGetRemoteDevicesFunction() override;
-
-  // Returns the user's private key used for the native experiment.
-  // Exposed for testing.
-  virtual std::string GetUserPrivateKey();
-
-  // Returns the user's unlock keys used for the native experiment.
-  // Exposed for testing.
-  virtual std::vector<cryptauth::ExternalDeviceInfo> GetUnlockKeys();
-
- private:
-  // ExtensionFunction:
-  ResponseAction Run() override;
-
-  // Callback when the PSK of a device is derived.
-  void OnPSKDerivedForDevice(const cryptauth::ExternalDeviceInfo& device,
-                             const std::string& persistent_symmetric_key);
-
-  // The permit id of the user. Used for the native experiment.
-  std::string permit_id_;
-
-  // The expected number of devices to return. Used for the native experiment.
-  size_t expected_devices_count_;
-
-  // Working list of the devices to return. Used for the native experiment.
-  std::unique_ptr<base::ListValue> remote_devices_;
-
-  // Used to derive devices' PSK. Used for the native experiment.
-  std::unique_ptr<cryptauth::SecureMessageDelegate>
-      secure_message_delegate_;
-
-  DISALLOW_COPY_AND_ASSIGN(EasyUnlockPrivateGetRemoteDevicesFunction);
-};
-
-class EasyUnlockPrivateGetUserInfoFunction : public UIThreadExtensionFunction {
- public:
-  DECLARE_EXTENSION_FUNCTION("easyUnlockPrivate.getUserInfo",
-                             EASYUNLOCKPRIVATE_GETUSERINFO)
-  EasyUnlockPrivateGetUserInfoFunction();
-
- private:
-  ~EasyUnlockPrivateGetUserInfoFunction() override;
-
-  // ExtensionFunction:
-  ResponseAction Run() override;
-
-  DISALLOW_COPY_AND_ASSIGN(EasyUnlockPrivateGetUserInfoFunction);
 };
 
 class EasyUnlockPrivateShowErrorBubbleFunction
