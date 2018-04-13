84faec45b39f3a06f09a8573d00968017ffd3a61
Alexei Filippov <alph@chromium.org>
2018-04-13 18:56:13 +0000

    DevTools: Make performance monitor report a single process memory again.
    BUG=832364
    
    Change-Id: I4baf520742f740bc296eb12ce76d9e82796f9f9a
    Reviewed-on: https://chromium-review.googlesource.com/1011586
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550704}
    Former-commit-id: c96d64efeec6c741091a188895d24d84bea90268

--- third_party/WebKit/LayoutTests/http/tests/devtools/oopif/oopif-performance-monitor.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/oopif/oopif-performance-monitor.js
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 (async function() {
-  TestRunner.addResult(`Tests list of performance metrics and that memory is not double counted.\n`);
+  TestRunner.addResult(`Tests stability of performance metrics list.\n`);
 
   const model = SDK.targetManager.mainTarget().model(SDK.PerformanceMetricsModel);
   await model.enable();
@@ -11,22 +11,6 @@
 
   TestRunner.addResult('\nMetrics reported:');
   TestRunner.addResults(metrics.keysArray().sort());
-
-  await TestRunner.navigatePromise('resources/page.html');
-  TestRunner.addResult('\nTargets after navigate');
-  TestRunner.addResults(SDK.targetManager.targets().map(t => t.name()).sort());
-
-  let lastTotal, total;
-  do {
-    metrics = (await model.requestMetrics()).metrics;
-    lastTotal = total;
-    total = 0;
-    for (const m of SDK.targetManager.models(SDK.RuntimeModel))
-      total += (await m.heapUsage()).totalSize;
-  } while (total !== lastTotal);
-
-  TestRunner.addResult('\nmetrics size is twice smaller than simple sum: ' +
-      (metrics.get('JSHeapTotalSize') * 2 === total));
 
   TestRunner.completeTest();
 })();
