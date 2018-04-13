09d7ec003cb1874a4e310655aae6e90ef5d19e6a
Bailey Forrest <bcf@chromium.org>
2018-04-13 01:12:12 +0000

    [chromecast] BLE: Add regex name matcher to ScanFilter
    BUG=internal b/77925032
    TEST=cast_bluetooth_unittests
    
    Change-Id: Ie6e0eff3d3656b4e12905dcf93df114271383c2b
    Reviewed-on: https://chromium-review.googlesource.com/1009167
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Stephen Lanham <slan@chromium.org>
    Commit-Queue: Bailey Forrest <bcf@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550455}
    Former-commit-id: 2ec2aaf9572617169c2740903694b9d03b42a6e3

--- chromecast/device/bluetooth/le/scan_filter_test.cc
+++ chromecast/device/bluetooth/le/scan_filter_test.cc
@@ -9,51 +9,113 @@
 namespace chromecast {
 namespace bluetooth {
 
-TEST(ScanFilterTest, Matches) {
-  static const char kName[] = "foo";
-  static const bluetooth_v2_shlib::Uuid kUuid = {
-      {0x12, 0x3e, 0x45, 0x67, 0xe8, 0x9b, 0x12, 0xd3, 0xa4, 0x56, 0x42, 0x66,
-       0x55, 0x44, 0x00, 0x00}};
+namespace {
 
-  ScanFilter name;
-  name.name = kName;
+const char kName[] = "foo";
+const bluetooth_v2_shlib::Uuid kUuid = {{0x12, 0x3e, 0x45, 0x67, 0xe8, 0x9b,
+                                         0x12, 0xd3, 0xa4, 0x56, 0x42, 0x66,
+                                         0x55, 0x44, 0x00, 0x00}};
 
-  ScanFilter uuid;
-  uuid.service_uuid = kUuid;
+}  // namespace
 
-  ScanFilter name_and_uuid;
-  name_and_uuid.name = kName;
-  name_and_uuid.service_uuid = kUuid;
-
+TEST(ScanFilterTest, Name) {
+  ScanFilter filter;
   LeScanResult result;
+  EXPECT_TRUE(filter.Matches(result));
 
-  const ScanFilter empty;
-  EXPECT_TRUE(empty.Matches(result));
-  EXPECT_FALSE(name.Matches(result));
-  EXPECT_FALSE(name_and_uuid.Matches(result));
+  filter.name = kName;
+  EXPECT_FALSE(filter.Matches(result));
 
   result.type_to_data[LeScanResult::kGapShortName].emplace_back(
       reinterpret_cast<const uint8_t*>(kName),
       reinterpret_cast<const uint8_t*>(kName) + strlen(kName));
-  EXPECT_TRUE(name.Matches(result));
-  EXPECT_FALSE(name_and_uuid.Matches(result));
+  EXPECT_TRUE(filter.Matches(result));
 
   ++result.type_to_data[LeScanResult::kGapShortName][0][0];
-  EXPECT_FALSE(name.Matches(result));
-  EXPECT_FALSE(uuid.Matches(result));
-  EXPECT_FALSE(name_and_uuid.Matches(result));
+  EXPECT_FALSE(filter.Matches(result));
+}
+
+TEST(ScanFilterTest, Uuid) {
+  ScanFilter filter;
+  filter.service_uuid = kUuid;
+
+  LeScanResult result;
+  EXPECT_FALSE(filter.Matches(result));
 
   result.type_to_data[LeScanResult::kGapIncomplete128BitServiceUuids]
       .emplace_back(kUuid.rbegin(), kUuid.rend());
-  EXPECT_TRUE(uuid.Matches(result));
-  EXPECT_FALSE(name_and_uuid.Matches(result));
-
-  --result.type_to_data[LeScanResult::kGapShortName][0][0];
-  EXPECT_TRUE(name_and_uuid.Matches(result));
+  EXPECT_TRUE(filter.Matches(result));
 
   ++result.type_to_data[LeScanResult::kGapIncomplete128BitServiceUuids][0][0];
-  EXPECT_FALSE(uuid.Matches(result));
-  EXPECT_FALSE(name_and_uuid.Matches(result));
+  EXPECT_FALSE(filter.Matches(result));
+}
+
+TEST(ScanFilterTest, NameAndUuid) {
+  ScanFilter filter;
+  filter.name = kName;
+  filter.service_uuid = kUuid;
+
+  LeScanResult result;
+  EXPECT_FALSE(filter.Matches(result));
+
+  result.type_to_data[LeScanResult::kGapShortName].emplace_back(
+      reinterpret_cast<const uint8_t*>(kName),
+      reinterpret_cast<const uint8_t*>(kName) + strlen(kName));
+  EXPECT_FALSE(filter.Matches(result));
+
+  result.type_to_data[LeScanResult::kGapIncomplete128BitServiceUuids]
+      .emplace_back(kUuid.rbegin(), kUuid.rend());
+  EXPECT_TRUE(filter.Matches(result));
+
+  // Unmatching name shouldn't work.
+  --result.type_to_data[LeScanResult::kGapShortName][0][0];
+  EXPECT_FALSE(filter.Matches(result));
+
+  // Unmatching uuid shouldn't work.
+  ++result.type_to_data[LeScanResult::kGapIncomplete128BitServiceUuids][0][0];
+  ++result.type_to_data[LeScanResult::kGapShortName][0][0];
+  EXPECT_FALSE(filter.Matches(result));
+
+  --result.type_to_data[LeScanResult::kGapIncomplete128BitServiceUuids][0][0];
+  EXPECT_TRUE(filter.Matches(result));
+}
+
+TEST(ScanFilterTest, RegexName) {
+  const char kHello[] = "hello";
+
+  // Just test some basic regular experssions, we don't want this to be testing
+  // RE2.
+  ScanFilter filter;
+  filter.regex_name = "ell";
+
+  LeScanResult result;
+  EXPECT_FALSE(filter.Matches(result));
+
+  result.type_to_data[LeScanResult::kGapShortName].emplace_back(
+      reinterpret_cast<const uint8_t*>(kHello),
+      reinterpret_cast<const uint8_t*>(kHello) + strlen(kHello));
+  EXPECT_TRUE(filter.Matches(result));
+
+  filter.regex_name = "g";
+  EXPECT_FALSE(filter.Matches(result));
+
+  filter.regex_name = "h.*o";
+  EXPECT_TRUE(filter.Matches(result));
+}
+
+TEST(ScanFilterTest, RegexNameIgnoredIfNameSet) {
+  ScanFilter filter;
+  filter.regex_name = ".";  // Match any string.
+  filter.name = "bar";
+
+  LeScanResult result;
+  result.type_to_data[LeScanResult::kGapShortName].emplace_back(
+      reinterpret_cast<const uint8_t*>(kName),
+      reinterpret_cast<const uint8_t*>(kName) + strlen(kName));
+  EXPECT_FALSE(filter.Matches(result));
+
+  filter.name = kName;
+  EXPECT_TRUE(filter.Matches(result));
 }
 
 }  // namespace bluetooth
