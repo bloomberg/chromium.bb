aeb87252876d443946174df6718e97e6ed3ab269
Rouslan Solomakhin <rouslan@chromium.org>
2018-04-13 01:29:27 +0000

    [Payment Request] Configurable skip-UI user gesture requirement.
    This patch adds the ability for implementers of PaymentInstrument
    interface to determine whether skipping the payment sheet into this
    instrument should require a user gesture. By default, the user gesture
    is required for the skip UI flow.
    
    Bug: 828427
    Change-Id: Id6878c7d0d9bef71da7734d2efec3fd6002fe282
    Reviewed-on: https://chromium-review.googlesource.com/1010975
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550461}
    Former-commit-id: d5ccecda0fa773e0d71a36a2c88af2f7af1632d8

--- chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentRequestImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentRequestImpl.java
@@ -365,6 +365,9 @@ public class PaymentRequestImpl
      */
     private boolean mShouldSkipShowingPaymentRequestUi;
 
+    /** Whether PaymentRequest.show() was invoked with a user gesture. */
+    private boolean mIsUserGestureShow;
+
     /** The helper to create and fill the response to send to the merchant. */
     private PaymentResponseHelper mPaymentResponseHelper;
 
@@ -659,8 +662,8 @@ public class PaymentRequestImpl
         mObservedTabModelSelector.addObserver(mSelectorObserver);
         mObservedTabModel.addObserver(mTabModelObserver);
 
+        mIsUserGestureShow = isUserGesture;
         buildUI(chromeActivity);
-        if (!isUserGesture) mShouldSkipShowingPaymentRequestUi = false;
         if (!mShouldSkipShowingPaymentRequestUi) mUI.show();
 
         triggerPaymentAppUiSkipIfApplicable();
@@ -673,11 +676,16 @@ public class PaymentRequestImpl
                 && mIsCurrentPaymentRequestShowing) {
             assert !mPaymentMethodsSection.isEmpty();
 
-            // Do not skip to payment app if it is not the only one or it's not pre-selected. Note
-            // that ServiceWorkerPaymentApp can not be pre-selected if its name and/or icon is
+            PaymentInstrument selectedInstrument =
+                    (PaymentInstrument) mPaymentMethodsSection.getSelectedItem();
+
+            // Do not skip to payment app if it is not the only one, it's not pre-selected, or if
+            // skip-UI requires a user gesture in show(), which was not present.
+            // Note that ServiceWorkerPaymentApp cannot be pre-selected if its name and/or icon is
             // missing.
-            if (mPaymentMethodsSection.getSize() > 1
-                    || mPaymentMethodsSection.getSelectedItem() == null) {
+            if (mPaymentMethodsSection.getSize() > 1 || selectedInstrument == null
+                    || (selectedInstrument.isUserGestureRequiredToSkipUi()
+                               && !mIsUserGestureShow)) {
                 mUI.show();
             } else {
                 mDidRecordShowEvent = true;
@@ -685,7 +693,7 @@ public class PaymentRequestImpl
                 mJourneyLogger.setEventOccurred(Event.SKIPPED_SHOW);
 
                 onPayClicked(null /* selectedShippingAddress */, null /* selectedShippingOption */,
-                        mPaymentMethodsSection.getItem(0));
+                        selectedInstrument);
             }
         }
     }
