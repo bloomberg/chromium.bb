1b557839e6060cd0e8552ec2ad297e5138b70fca
Eugene But <eugenebut@google.com>
2018-04-13 20:33:16 +0000

    Add BrowserContainerViewController class to manage BrowserContainerView.
    This view controller will be used as a base view controller for Download
    Manager presentation. Download Manager must be presented under toolbar
    view, so using BVC as a base view controller does not work.
    
    This CL replaces BrowserContainerView with BrowserContainerViewController
    which should not cause any functional changes.
    
    Bug: 818264
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I93d66906c671a8647589db8d9717ab2dcd26cd51
    Reviewed-on: https://chromium-review.googlesource.com/981684
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550747}
    Former-commit-id: 4cebbb3329f5e29b780a4b9072d67d67646615a8

--- ios/chrome/browser/ui/browser_container_view_controller.mm
+++ ios/chrome/browser/ui/browser_container_view_controller.mm
@@ -1,25 +1,34 @@
-// Copyright 2015 The Chromium Authors. All rights reserved.
+// Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/browser_container_view.h"
-
-#import "ios/chrome/browser/ui/util/named_guide.h"
+#import "ios/chrome/browser/ui/browser_container_view_controller.h"
 
 #include "base/logging.h"
+#import "ios/chrome/browser/ui/util/named_guide.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
-@implementation BrowserContainerView {
+@interface BrowserContainerViewController () {
   // Weak reference to content view, so old _contentView can be removed from
   // superview when new one is added.
   __weak UIView* _contentView;
 }
+@end
+
+@implementation BrowserContainerViewController
 
 - (void)dealloc {
-  DCHECK(![_contentView superview] || [_contentView superview] == self);
+  DCHECK(![_contentView superview] || [_contentView superview] == self.view);
+}
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  self.view.autoresizingMask =
+      UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
 }
 
 - (void)displayContentView:(UIView*)contentView {
@@ -33,18 +42,18 @@
     // |contentView|, regardless of the NamedGuide constraints.
     // TODO(crbug.com/826093): Remove NamedGuide check and simply early return.
     NamedGuide* voiceSearchGuide =
-        [NamedGuide guideWithName:kVoiceSearchButtonGuide view:self];
+        [NamedGuide guideWithName:kVoiceSearchButtonGuide view:self.view];
     UIView* voiceSearchButton = voiceSearchGuide.constrainedView;
     if ([voiceSearchButton isDescendantOfView:_contentView])
       return;
   }
 
-  DCHECK(![_contentView superview] || [_contentView superview] == self);
+  DCHECK(![_contentView superview] || [_contentView superview] == self.view);
   [_contentView removeFromSuperview];
   _contentView = contentView;
 
   if (contentView)
-    [self addSubview:contentView];
+    [self.view addSubview:contentView];
 }
 
 @end
