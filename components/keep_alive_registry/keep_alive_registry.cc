5f91bb563de11f57e0929661b3376332a70ba6ac
Wez <wez@chromium.org>
2018-04-16 18:03:09 +0000

    Promote KeepAliveRegistry logging and DCHECKs to CHECKs.
    - Enable CHECK against registration of new keep-alives during shutdown.
    - Promote some DLOG() checks to DCHECKs to make issues more visible.
    
    Bug: 720078
    Change-Id: I11e24a8de0ec5ab9e0a71f776d4b7149bb847ad2
    Reviewed-on: https://chromium-review.googlesource.com/1013394
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551033}
    Former-commit-id: 2f88261bfd678f85d68744a9744970286cb69cf4

--- components/keep_alive_registry/keep_alive_registry.cc
+++ components/keep_alive_registry/keep_alive_registry.cc
@@ -81,9 +81,7 @@ bool KeepAliveRegistry::WouldRestartWithout(
 }
 
 void KeepAliveRegistry::SetIsShuttingDown(bool value) {
-#if DCHECK_IS_ON()
   is_shutting_down_ = value;
-#endif
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -93,15 +91,14 @@ KeepAliveRegistry::KeepAliveRegistry()
     : registered_count_(0), restart_allowed_count_(0) {}
 
 KeepAliveRegistry::~KeepAliveRegistry() {
-  DLOG_IF(ERROR, registered_count_ > 0 || registered_keep_alives_.size() > 0)
-      << "KeepAliveRegistry not empty at destruction time. State:" << *this;
+  DCHECK_LE(registered_count_, 0) << "KeepAliveRegistry state:" << *this;
+  DCHECK_EQ(registered_keep_alives_.size(), 0u)
+      << "KeepAliveRegistry state:" << *this;
 }
 
 void KeepAliveRegistry::Register(KeepAliveOrigin origin,
                                  KeepAliveRestartOption restart) {
-#if DCHECK_IS_ON()
-  DCHECK(!is_shutting_down_);
-#endif
+  CHECK(!is_shutting_down_);
 
   bool old_keeping_alive = IsKeepingAlive();
   bool old_restart_allowed = IsRestartAllowed();
