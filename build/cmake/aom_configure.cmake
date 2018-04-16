96ecf6f270944b1d57a614dd758937350da83ac3
Johann <johannkoenig@google.com>
2018-04-16 14:17:57 -0700

    cmake: only test nasm if it is found
    Avoid a cryptic error message about unsupported nasm version.
    
    Change-Id: I1788be24de9f410da4b8f72d5031fc46c15e067f

--- build/cmake/aom_configure.cmake
+++ build/cmake/aom_configure.cmake
@@ -127,13 +127,15 @@ if ("${AOM_TARGET_CPU}" STREQUAL "x86" OR "${AOM_TARGET_CPU}" STREQUAL "x86_64")
   if (NOT AS_EXECUTABLE OR ENABLE_NASM)
     unset(AS_EXECUTABLE CACHE)
     find_program(AS_EXECUTABLE nasm $ENV{NASM_PATH})
-    test_nasm()
+    if (AS_EXECUTABLE)
+      test_nasm()
+    endif()
   endif ()
 
   if (NOT AS_EXECUTABLE)
-    message(FATAL_ERROR "Unable to find assembler. To build without "
-            "optimizations, add -DAOM_TARGET_CPU=generic to your cmake command "
-            "line.")
+    message(FATAL_ERROR "Unable to find assembler. Install 'yasm' or 'nasm.' "
+            "To build without optimizations, add -DAOM_TARGET_CPU=generic to "
+            "your cmake command line.")
   endif ()
   get_asm_obj_format("objformat")
   set(AOM_AS_FLAGS -f ${objformat} ${AOM_AS_FLAGS})
