e2b895eff8d049816e5b32c04c76f8477aedf912
Charlie Harrison <csharrison@chromium.org>
2018-04-16 21:37:57 +0000

    [subresource_filter] Move test-only JNI calls out of binary
    Before, the JNI shim called into native code that was compiled into
    Chrome proper.
    
    Now, due to r517541 there is a better way to do this by including the
    JNI code in libchromefortest.
    
    Bug: None
    Change-Id: I9f34f5105366b927d6dd6676d97a9da6d2540385
    Reviewed-on: https://chromium-review.googlesource.com/997877
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551131}
    Former-commit-id: 39b4e33b85eda61eca33e85a14f46b6955e0716c

--- chrome/android/javatests/src/org/chromium/chrome/browser/subresource_filter/SubresourceFilterTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/subresource_filter/SubresourceFilterTest.java
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser;
+package org.chromium.chrome.browser.subresource_filter;
 
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.MediumTest;
@@ -17,9 +17,11 @@ import org.junit.runner.RunWith;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.chrome.browser.ChromeActivity;
+import org.chromium.chrome.browser.ChromeSwitches;
+import org.chromium.chrome.browser.MockSafeBrowsingApiHandler;
 import org.chromium.chrome.browser.infobar.AdsBlockedInfoBar;
 import org.chromium.chrome.browser.infobar.InfoBar;
-import org.chromium.chrome.browser.subresource_filter.TestSubresourceFilterPublisher;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.EmptyTabModelObserver;
 import org.chromium.chrome.browser.tabmodel.TabModel;
@@ -58,7 +60,7 @@ public final class SubresourceFilterTest {
             "{\"matches\":[{\"threat_type\":\"13\",\"sf_bas\":\"warn\"}]}";
 
     private void createAndPublishRulesetDisallowingSuffix(String suffix) {
-        TestSubresourceFilterPublisher publisher = new TestSubresourceFilterPublisher();
+        TestRulesetPublisher publisher = new TestRulesetPublisher();
         ThreadUtils.runOnUiThreadBlocking(
                 (Runnable) ()
                         -> publisher.createAndPublishRulesetDisallowingSuffixForTesting(suffix));
