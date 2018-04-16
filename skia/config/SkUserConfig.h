fcf2f1933721dbd37db4565e9b978cfdc1457a16
Yuqian Li <liyuqian@google.com>
2018-04-16 16:24:33 +0000

    Add SK_SUPPORT_LEGACY_THREADED_DAA_BUGS
    Change-Id: I41d086b4f3754d08c1cb8952a9f0da35cd170a43
    Reviewed-on: https://chromium-review.googlesource.com/1013609
    Reviewed-by: Leon Scroggins <scroggo@chromium.org>
    Commit-Queue: Yuqian Li <liyuqian@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#550997}
    Former-commit-id: 5069f8e5d080c7b39454fdf6d63786589f2c35f6

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -229,6 +229,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_PATH_DAA_BIT
 #endif
 
+#ifndef SK_SUPPORT_LEGACY_THREADED_DAA_BUGS
+#define SK_SUPPORT_LEGACY_THREADED_DAA_BUGS
+#endif
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
