78c54ab83a25028cf7c7390671a2cbfcb24dcc46
Jandro Alderman <aalderman@chromium.org>
2018-04-16 19:23:28 +0000

    Ensure timer is initialized before IsRunning().
    It is currently possible to trigger a segfault using
    GetContentRenderingTimeoutFrom() if other's timeout is nullptr.
    
    R=samans@chromium.org
    
    Change-Id: I455ef5e45357efb048199549d3d8f7313c37bc33
    Reviewed-on: https://chromium-review.googlesource.com/1010702
    Reviewed-by: Saman Sami <samans@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Jandro Alderman <aalderman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551071}
    Former-commit-id: 73523fab863659f381620fbd87fd7cc816b8c842

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -1921,7 +1921,8 @@ bool RenderWidgetHostImpl::IsKeyboardLocked() const {
 
 void RenderWidgetHostImpl::GetContentRenderingTimeoutFrom(
     RenderWidgetHostImpl* other) {
-  if (other->new_content_rendering_timeout_->IsRunning()) {
+  if (other->new_content_rendering_timeout_ &&
+      other->new_content_rendering_timeout_->IsRunning()) {
     new_content_rendering_timeout_->Start(
         other->new_content_rendering_timeout_->GetCurrentDelay());
   }
