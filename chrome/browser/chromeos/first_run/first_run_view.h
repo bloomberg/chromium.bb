39b3c99aaab9bb490975640ab52fee6cfdec9a5c
James Cook <jamescook@chromium.org>
2018-04-16 22:26:46 +0000

    cros: Move first-run Widget from ash to chrome
    This is another step toward out-of-process ash (go/mustash).
    
    * Create the Widget in FirstRunController in chrome
    * Move escape key handling into chrome
    * Stop using OverlayEventFilter in ash. The old code wasn't actually
      relying on it for blocking event propagation, just for escape key
      handling and detecting screen lock and shutdown. The old code was
      also watching for LoginStatus changes, but that dates back to the
      use of OverlayEventFilter for partial screenshots (no longer used)
      and isn't relevant to first-run.
    * Add mojo interfaces to tell ash to start/stop tutorial
    
    Bug: 756057
    Test: ash_unittests, browser_tests
    Change-Id: Icfd88e3541f1393b6a2309120c74bbe6a69b967f
    Reviewed-on: https://chromium-review.googlesource.com/1000395
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551147}
    Former-commit-id: 126093fb4c54277c77bc37f23be5a758cc1fb1df

--- chrome/browser/chromeos/first_run/first_run_view.h
+++ chrome/browser/chromeos/first_run/first_run_view.h
@@ -5,7 +5,6 @@
 #ifndef CHROME_BROWSER_CHROMEOS_FIRST_RUN_FIRST_RUN_VIEW_H_
 #define CHROME_BROWSER_CHROMEOS_FIRST_RUN_FIRST_RUN_VIEW_H_
 
-#include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "content/public/browser/web_contents_delegate.h"
 #include "ui/views/view.h"
@@ -21,13 +20,14 @@ class WebView;
 namespace chromeos {
 
 class FirstRunActor;
+class FirstRunController;
 
 // WebUI view used for first run tutorial.
 class FirstRunView : public views::View,
                      public content::WebContentsDelegate {
  public:
   FirstRunView();
-  void Init(content::BrowserContext* context);
+  void Init(content::BrowserContext* context, FirstRunController* controller);
   FirstRunActor* GetActor();
 
   // Overriden from views::View.
@@ -39,10 +39,14 @@ class FirstRunView : public views::View,
  private:
   // Overriden from content::WebContentsDelegate.
   bool HandleContextMenu(const content::ContextMenuParams& params) override;
+  content::KeyboardEventProcessingResult PreHandleKeyboardEvent(
+      content::WebContents* source,
+      const content::NativeWebKeyboardEvent& event) override;
   bool PreHandleGestureEvent(content::WebContents* source,
                              const blink::WebGestureEvent& event) override;
 
-  views::WebView* web_view_;
+  views::WebView* web_view_ = nullptr;
+  FirstRunController* controller_ = nullptr;
 
   DISALLOW_COPY_AND_ASSIGN(FirstRunView);
 };
