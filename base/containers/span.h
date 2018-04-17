df92d1a02e69f46dffb882bf518cbed70551a90e
jdoerrie <jdoerrie@chromium.org>
2018-04-17 13:57:48 +0000

    [base] Add missing methods to base::span
    This change implements constructors for base::span that allow the
    construction from a pair of pointers, std::arrays and containers
    supporting base::data and base::size, e.g. std::initializer_list.
    In addition, this change adds operator().
    
    Bug: 788913
    Change-Id: Ibc280eef1c7e47a5a27e92503dda3614ef5513a4
    Reviewed-on: https://chromium-review.googlesource.com/981139
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551313}
    Former-commit-id: 82d9545a59298536b27e95096148b1bb12f80fc4

--- base/containers/span.h
+++ base/containers/span.h
@@ -14,6 +14,7 @@
 #include <utility>
 
 #include "base/logging.h"
+#include "base/stl_util.h"
 
 namespace base {
 
@@ -40,45 +41,36 @@ struct IsStdArrayImpl<std::array<T, N>> : std::true_type {};
 template <typename T>
 using IsStdArray = IsStdArrayImpl<std::decay_t<T>>;
 
+template <typename T>
+using IsCArray = std::is_array<std::remove_reference_t<T>>;
+
 template <typename From, typename To>
-using IsLegalSpanConversion = std::is_convertible<From*, To*>;
+using IsLegalSpanConversion = std::is_convertible<From (*)[], To (*)[]>;
 
 template <typename Container, typename T>
 using ContainerHasConvertibleData = IsLegalSpanConversion<
-    std::remove_pointer_t<decltype(std::declval<Container>().data())>,
+    std::remove_pointer_t<decltype(base::data(std::declval<Container>()))>,
     T>;
+
 template <typename Container>
 using ContainerHasIntegralSize =
-    std::is_integral<decltype(std::declval<Container>().size())>;
+    std::is_integral<decltype(base::size(std::declval<Container>()))>;
 
 template <typename From, typename To>
 using EnableIfLegalSpanConversion =
     std::enable_if_t<IsLegalSpanConversion<From, To>::value>;
 
-// SFINAE check if Container can be converted to a span<T>. Note that the
-// implementation details of this check differ slightly from the requirements in
-// the working group proposal: in particular, the proposal also requires that
-// the container conversion constructor participate in overload resolution only
-// if two additional conditions are true:
-//
-//   1. Container implements operator[].
-//   2. Container::value_type matches remove_const_t<element_type>.
-//
-// The requirements are relaxed slightly here: in particular, not requiring (2)
-// means that an immutable span can be easily constructed from a mutable
-// container.
+// SFINAE check if Array can be converted to a span<T>.
+template <typename Array, typename T>
+using EnableIfSpanCompatibleArray =
+    std::enable_if_t<ContainerHasConvertibleData<Array, T>::value>;
+
+// SFINAE check if Container can be converted to a span<T>.
 template <typename Container, typename T>
 using EnableIfSpanCompatibleContainer =
     std::enable_if_t<!internal::IsSpan<Container>::value &&
                      !internal::IsStdArray<Container>::value &&
-                     ContainerHasConvertibleData<Container, T>::value &&
-                     ContainerHasIntegralSize<Container>::value>;
-
-template <typename Container, typename T>
-using EnableIfConstSpanCompatibleContainer =
-    std::enable_if_t<std::is_const<T>::value &&
-                     !internal::IsSpan<Container>::value &&
-                     !internal::IsStdArray<Container>::value &&
+                     !internal::IsCArray<Container>::value &&
                      ContainerHasConvertibleData<Container, T>::value &&
                      ContainerHasIntegralSize<Container>::value>;
 
@@ -133,7 +125,7 @@ using EnableIfConstSpanCompatibleContainer =
 // -------------------------------
 //
 // Const and pointers can get confusing. Here are vectors of pointers and their
-// corresponding spans (you can always make the span "more const" too):
+// corresponding spans:
 //
 //   const std::vector<int*>        =>  base::span<int* const>
 //   std::vector<const int*>        =>  base::span<const int*>
@@ -143,7 +135,7 @@ using EnableIfConstSpanCompatibleContainer =
 // -------------------------------------------
 //
 // https://wg21.link/P0122 is the latest working group proposal, Chromium
-// currently implements R6. The biggest difference is span does not support a
+// currently implements R7. The biggest difference is span does not support a
 // static extent template parameter. Other differences are documented in
 // subsections below.
 //
@@ -151,7 +143,7 @@ using EnableIfConstSpanCompatibleContainer =
 // - no dynamic_extent constant
 //
 // Differences from [span.objectrep]:
-// - as_bytes() and as_writeable_bytes() return spans of uint8_t instead of
+// - as_bytes() and as_writable_bytes() return spans of uint8_t instead of
 //   std::byte
 //
 // Differences in constants and types:
@@ -159,10 +151,6 @@ using EnableIfConstSpanCompatibleContainer =
 // - no index_type type alias
 // - no different_type type alias
 // - no extent constant
-//
-// Differences from [span.cons]:
-// - no constructor from a pointer range
-// - no constructor from std::array
 //
 // Differences from [span.sub]:
 // - no templated first()
@@ -174,7 +162,6 @@ using EnableIfConstSpanCompatibleContainer =
 // - using size_t instead of ptrdiff_t to represent size()
 //
 // Differences from [span.elem]:
-// - no operator ()()
 // - using size_t instead of ptrdiff_t for indexing
 
 // [span], class template span
@@ -192,20 +179,39 @@ class span {
   // [span.cons], span constructors, copy, assignment, and destructor
   constexpr span() noexcept : data_(nullptr), size_(0) {}
   constexpr span(T* data, size_t size) noexcept : data_(data), size_(size) {}
-  // TODO(dcheng): Implement construction from a |begin| and |end| pointer.
-  template <size_t N>
+  // Artificially templatized to break ambiguity for span(ptr, 0).
+  template <typename = void>
+  constexpr span(T* begin, T* end)
+      : data_(begin), size_(std::distance(begin, end)) {
+    CHECK_LE(begin, end);
+  }
+  template <size_t N,
+            typename = internal::EnableIfSpanCompatibleArray<T (&)[N], T>>
   constexpr span(T (&array)[N]) noexcept : span(array, N) {}
-  // TODO(dcheng): Implement construction from std::array.
+
+  template <
+      size_t N,
+      typename =
+          internal::EnableIfSpanCompatibleArray<std::array<value_type, N>&, T>>
+  constexpr span(std::array<value_type, N>& array) noexcept
+      : span(array.data(), array.size()) {}
+  template <size_t N,
+            typename = internal::EnableIfSpanCompatibleArray<
+                const std::array<value_type, N>&,
+                T>>
+  constexpr span(const std::array<value_type, N>& array) noexcept
+      : span(array.data(), array.size()) {}
   // Conversion from a container that provides |T* data()| and |integral_type
   // size()|.
   template <typename Container,
-            typename = internal::EnableIfSpanCompatibleContainer<Container, T>>
+            typename = internal::EnableIfSpanCompatibleContainer<Container&, T>>
   constexpr span(Container& container)
-      : span(container.data(), container.size()) {}
+      : span(base::data(container), container.size()) {}
   template <
       typename Container,
-      typename = internal::EnableIfConstSpanCompatibleContainer<Container, T>>
-  span(const Container& container) : span(container.data(), container.size()) {}
+      typename = internal::EnableIfSpanCompatibleContainer<const Container&, T>>
+  span(const Container& container)
+      : span(base::data(container), container.size()) {}
   constexpr span(const span& other) noexcept = default;
   // Conversions from spans of compatible types: this allows a span<T> to be
   // seamlessly used as a span<const T>, but not the other way around.
@@ -239,6 +245,10 @@ class span {
 
   // [span.elem], span element access
   constexpr T& operator[](size_t index) const noexcept {
+    CHECK(index < size_);
+    return data_[index];
+  }
+  constexpr T& operator()(size_t index) const noexcept {
     CHECK(index < size_);
     return data_[index];
   }
@@ -320,22 +330,37 @@ constexpr span<T> make_span(T* data, size_t size) noexcept {
   return span<T>(data, size);
 }
 
+template <typename T>
+constexpr span<T> make_span(T* begin, T* end) {
+  return span<T>(begin, end);
+}
+
 template <typename T, size_t N>
 constexpr span<T> make_span(T (&array)[N]) noexcept {
   return span<T>(array);
 }
 
+template <typename T, size_t N>
+constexpr span<T> make_span(std::array<T, N>& array) noexcept {
+  return span<T>(array);
+}
+
+template <typename T, size_t N>
+constexpr span<const T> make_span(const std::array<T, N>& array) noexcept {
+  return span<const T>(array);
+}
+
 template <typename Container,
           typename T = typename Container::value_type,
-          typename = internal::EnableIfSpanCompatibleContainer<Container, T>>
+          typename = internal::EnableIfSpanCompatibleContainer<Container&, T>>
 constexpr span<T> make_span(Container& container) {
   return span<T>(container);
 }
 
 template <
     typename Container,
-    typename T = std::add_const_t<typename Container::value_type>,
-    typename = internal::EnableIfConstSpanCompatibleContainer<Container, T>>
+    typename T = const typename Container::value_type,
+    typename = internal::EnableIfSpanCompatibleContainer<const Container&, T>>
 constexpr span<T> make_span(const Container& container) {
   return span<T>(container);
 }
