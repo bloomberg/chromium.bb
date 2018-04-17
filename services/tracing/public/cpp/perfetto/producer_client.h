e4acda1f6e5c134bd73b54dadba342bf5d4e7c30
Oystein Eftevaag <oysteine@google.com>
2018-04-17 23:49:12 +0000

    Reland "Add a Mojo-based transportation layer for Perfetto"
    This is a reland of c5b3acb7cc7ff8cb62eb3425ba09190a2a7e96bc [formerly 4df8a2dd8f3b875773af061c1cbdcc519d19b283]
    
    Original change's description:
    > Add a Mojo-based transportation layer for Perfetto
    >
    > This adds a base layer that individual logging protocols (like tracing)
    > can use to send their data to Perfetto, using Mojo messaging and
    > shared memory.
    >
    > First CL of several, after this comes the Chrome tracing layer: https://chromium-review.googlesource.com/c/chromium/src/+/981270
    >
    > A preview of the full integration can be seen here: https://chromium-review.googlesource.com/c/chromium/src/+/852808
    >
    > The design doc for the integration: https://docs.google.com/document/d/1a8lvN3zYWEyAsBjtuRRsy0fWOWdUdbw6bGnKLBscGdw/edit
    >
    > R=primiano@chromium.org,skyostil@chromium.org
    >
    > Change-Id: Ie1db5745ebffdcf1e2d74e2f6b12395f447c8552
    > Reviewed-on: https://chromium-review.googlesource.com/980578
    > Reviewed-by: oysteine <oysteine@google.com>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Primiano Tucci <primiano@chromium.org>
    > Commit-Queue: oysteine <oysteine@google.com>
    > Cr-Commit-Position: refs/heads/master@{#551019}
    
    Change-Id: Ic47afdb6c2e09924a5f540fa56552ceb6e29e6d7
    
    TBR=dcheng@chromium.org,primiano@chromium.org,oysteine@google.com,skyostil@chromium.org
    
    Change-Id: Ic47afdb6c2e09924a5f540fa56552ceb6e29e6d7
    Reviewed-on: https://chromium-review.googlesource.com/1015189
    Reviewed-by: oysteine <oysteine@chromium.org>
    Commit-Queue: oysteine <oysteine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551526}
    Former-commit-id: 62436735e211820d7e63fe9c755be609b8a390d5

---
+++ services/tracing/public/cpp/perfetto/producer_client.h
@@ -0,0 +1,122 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_TRACING_PUBLIC_CPP_PERFETTO_PRODUCER_CLIENT_H_
+#define SERVICES_TRACING_PUBLIC_CPP_PERFETTO_PRODUCER_CLIENT_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "base/atomicops.h"
+#include "base/component_export.h"
+#include "base/macros.h"
+#include "base/sequence_checker.h"
+#include "mojo/public/cpp/bindings/binding.h"
+#include "services/tracing/public/cpp/perfetto/task_runner.h"
+#include "services/tracing/public/mojom/perfetto_service.mojom.h"
+#include "third_party/perfetto/include/perfetto/tracing/core/service.h"
+
+namespace perfetto {
+class SharedMemoryArbiter;
+}  // namespace perfetto
+
+namespace tracing {
+
+class MojoSharedMemory;
+
+// This class is the per-process client side of the Perfetto
+// producer, and is responsible for creating specific kinds
+// of DataSources (like ChromeTracing) on demand, and provide
+// them with TraceWriters and a configuration to start logging.
+
+// Implementations of new DataSources should:
+// * Implement ProducerClient::DataSourceBase.
+// * Add a new data source name in perfetto_service.mojom.
+// * Register the data source with Perfetto in ProducerHost::OnConnect.
+// * Construct the new implementation when requested to
+//   in ProducerClient::CreateDataSourceInstance.
+class COMPONENT_EXPORT(TRACING_CPP) ProducerClient
+    : public mojom::ProducerClient,
+      public perfetto::Service::ProducerEndpoint {
+ public:
+  // Base class for different DataSource implementations,
+  // i.e. something which is able to provide data in the
+  // form of protos of a given type to Perfetto (like trace events).
+  class DataSourceBase {
+   public:
+    virtual ~DataSourceBase() = default;
+  };
+
+  ProducerClient();
+  ~ProducerClient() override;
+
+  static void DeleteSoon(std::unique_ptr<ProducerClient>);
+
+  // Connect to the service-side ProducerHost which will
+  // let Perfetto know we're ready to start logging
+  // our data.
+  mojom::ProducerClientPtr CreateAndBindProducerClient();
+  mojom::ProducerHostRequest CreateProducerHostRequest();
+
+  bool has_connected_producer_host_for_testing() const {
+    return !!producer_host_;
+  }
+
+  // mojom::ProducerClient implementation.
+  // Called through Mojo by the ProducerHost on the service-side to control
+  // tracing and toggle specific DataSources.
+  void OnTracingStart(mojo::ScopedSharedBufferHandle shared_memory) override;
+  void CreateDataSourceInstance(
+      uint64_t id,
+      mojom::DataSourceConfigPtr data_source_config) override;
+
+  void TearDownDataSourceInstance(uint64_t id) override;
+  void Flush(uint64_t flush_request_id,
+             const std::vector<uint64_t>& data_source_ids) override;
+
+  // perfetto::Service::ProducerEndpoint implementation.
+  // Used by the TraceWriters
+  // to signal Perfetto that shared memory chunks are ready
+  // for consumption.
+  void CommitData(const perfetto::CommitDataRequest& commit,
+                  CommitDataCallback callback) override;
+  // Used by the DataSource implementations to create TraceWriters
+  // for writing their protobufs
+  std::unique_ptr<perfetto::TraceWriter> CreateTraceWriter(
+      perfetto::BufferID target_buffer) override;
+  perfetto::SharedMemory* shared_memory() const override;
+
+  // These ProducerEndpoint functions are only used on the service
+  // side and should not be called on the clients.
+  void RegisterDataSource(const perfetto::DataSourceDescriptor&) override;
+  void UnregisterDataSource(const std::string& name) override;
+  size_t shared_buffer_page_size_kb() const override;
+  void NotifyFlushComplete(perfetto::FlushRequestID) override;
+
+ protected:
+  base::SequencedTaskRunner* GetTaskRunner();
+
+  // TODO(oysteine): Allow multiple enabled data source instances.
+  std::unique_ptr<DataSourceBase> enabled_data_source_instance_;
+
+ private:
+  void BindOnSequence(mojom::ProducerClientRequest request);
+
+  // Keep the TaskRunner first in the member list so it outlives
+  // everything else and no dependent classes will try to use
+  // an invalid taskrunner.
+  PerfettoTaskRunner perfetto_task_runner_;
+  std::unique_ptr<mojo::Binding<mojom::ProducerClient>> binding_;
+  std::unique_ptr<perfetto::SharedMemoryArbiter> shared_memory_arbiter_;
+  mojom::ProducerHostPtr producer_host_;
+  std::unique_ptr<MojoSharedMemory> shared_memory_;
+  SEQUENCE_CHECKER(sequence_checker_);
+
+  DISALLOW_COPY_AND_ASSIGN(ProducerClient);
+};
+
+}  // namespace tracing
+
+#endif  // SERVICES_TRACING_PUBLIC_CPP_PERFETTO_PRODUCER_CLIENT_H_
