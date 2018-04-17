6d1ab151352f445c755abdf2831499279c2cf6ad
Bruce Dawson <brucedawson@chromium.org>
2018-04-17 22:15:19 +0000

    Fix comment to accurately reflection function names
    SingleThread, not SingleTreaded (add one letter, remove two). Spelling
    matters to allow searching for the referenced functions.
    
    Change-Id: I46909856cd3540fe246d9d8a9db70aaee2ebe71f
    Reviewed-on: https://chromium-review.googlesource.com/1012489
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551500}
    Former-commit-id: 26391829bb60fbe501ead5bbf6fa522ee69dd97a

--- base/threading/thread.h
+++ base/threading/thread.h
@@ -29,7 +29,7 @@ class MessagePump;
 class RunLoop;
 
 // IMPORTANT: Instead of creating a base::Thread, consider using
-// base::Create(Sequenced|SingleTreaded)TaskRunnerWithTraits().
+// base::Create(Sequenced|SingleThread)TaskRunnerWithTraits().
 //
 // A simple thread abstraction that establishes a MessageLoop on a new thread.
 // The consumer uses the MessageLoop of the thread to cause code to execute on
