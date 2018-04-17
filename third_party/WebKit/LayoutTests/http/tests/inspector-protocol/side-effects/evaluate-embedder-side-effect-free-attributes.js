ae2f504bbdb476b8f82f28c20ca108c36e228ced
Erik Luo <luoe@chromium.org>
2018-04-17 20:17:38 +0000

    Mark side-effect-free methods/attributes with simple return types
    Adds [Affects=Nothing] to common IDL methods/attributes with return
    types: short, long, double, int, boolean, DOMString, USVString.
    
    Followups will whitelist ones that use DOMWrapper and update style
    or layout.
    List of some common attributes: https://goo.gl/qGd2i7
    
    Bug: 829571
    Change-Id: I4c335b7eeac449bd7454df7f829f393469d84387
    Reviewed-on: https://chromium-review.googlesource.com/1013126
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551456}
    Former-commit-id: f71d954c746115229ace55f1a571daa39fbd7096

--- third_party/WebKit/LayoutTests/http/tests/inspector-protocol/side-effects/evaluate-embedder-side-effect-free-attributes.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector-protocol/side-effects/evaluate-embedder-side-effect-free-attributes.js
@@ -4,9 +4,45 @@
 
   await session.evaluate(`
     var div = document.createElement('div');
+    div.id = 'foo';
+    div.className = 'bar baz';
+    var textNode = document.createTextNode('footext');
+    div.appendChild(textNode);
   `);
 
-  await checkHasNoSideEffect('div.isConnected');
+  // Sanity check: test that setters are not allowed on whitelisted accessors.
+  await checkHasSideEffect(`document.title = "foo"`);
+
+  // Document
+  await checkHasNoSideEffect(`document.domain`);
+  await checkHasNoSideEffect(`document.referrer`);
+  await checkHasNoSideEffect(`document.cookie`);
+  await checkHasNoSideEffect(`document.title`);
+
+  // Element
+  await checkHasNoSideEffect(`div.tagName`);
+  await checkHasNoSideEffect(`div.id`);
+  await checkHasNoSideEffect(`div.className`);
+
+  // Node
+  var testNodes = ['div', 'document', 'textNode'];
+  for (var node of testNodes) {
+    await checkHasNoSideEffect(`${node}.nodeType`);
+    await checkHasNoSideEffect(`${node}.nodeName`);
+    await checkHasNoSideEffect(`${node}.nodeValue`);
+    await checkHasNoSideEffect(`${node}.textContent`);
+    await checkHasNoSideEffect(`${node}.isConnected`);
+  }
+
+  // ParentNode
+  await checkHasNoSideEffect(`document.childElementCount`);
+  await checkHasNoSideEffect(`div.childElementCount`);
+
+  // Window
+  await checkHasNoSideEffect(`devicePixelRatio`);
+  await checkHasNoSideEffect(`screenX`);
+  await checkHasNoSideEffect(`screenY`);
+
   testRunner.completeTest();
 
 
@@ -19,7 +55,6 @@
   }
 
   async function checkExpression(expression, expectSideEffect) {
-    testRunner.log(`Checking: \`${expression}\` for ${expectSideEffect ? '' : 'no'} side effect`);
     var response = await dp.Runtime.evaluate({expression, throwOnSideEffect: true});
     var hasSideEffect = false;
     var exceptionDetails = response.result.exceptionDetails;
@@ -27,7 +62,7 @@
         exceptionDetails.exception.description.startsWith('EvalError: Possible side-effect in debug-evaluate'))
       hasSideEffect = true;
     if (hasSideEffect !== expectSideEffect) {
-      testRunner.log(`FAIL: hasSideEffect = ${hasSideEffect}, expectSideEffect = ${expectSideEffect}`);
+      testRunner.log(`FAIL: "${expression}" hasSideEffect = ${hasSideEffect}, expectSideEffect = ${expectSideEffect}`);
       testRunner.completeTest();
       return;
     }
--- third_party/WebKit/LayoutTests/http/tests/inspector-protocol/side-effects/evaluate-embedder-side-effect-free-methods.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector-protocol/side-effects/evaluate-embedder-side-effect-free-methods.js
@@ -4,9 +4,34 @@
 
   await session.evaluate(`
     var global_performance = window.performance;
+    var div = document.createElement('div');
+    div.setAttribute('attr1', 'attr1-value');
+    div.setAttribute('attr2', 'attr2-value');
+    var textNode = document.createTextNode('footext');
+    var divNoAttrs = document.createElement('div');
   `);
 
-  await checkHasNoSideEffect('global_performance.now()');
+  // Element
+  await checkHasNoSideEffect(`div.getAttributeNames()`);
+  await checkHasNoSideEffect(`divNoAttrs.getAttributeNames()`);
+  await checkHasNoSideEffect(`div.getAttribute()`);
+  await checkHasNoSideEffect(`div.getAttribute('attr1')`);
+  await checkHasNoSideEffect(`div.getAttribute({})`);
+  await checkHasNoSideEffect(`divNoAttrs.getAttribute('attr1')`);
+  await checkHasNoSideEffect(`div.hasAttribute()`);
+  await checkHasNoSideEffect(`div.hasAttribute('attr1')`);
+  await checkHasNoSideEffect(`div.hasAttribute({})`);
+  await checkHasNoSideEffect(`divNoAttrs.hasAttribute('attr1')`);
+
+  // Node
+  await checkHasNoSideEffect(`div.contains(textNode)`);
+  await checkHasNoSideEffect(`div.contains()`);
+  await checkHasNoSideEffect(`div.contains({})`);
+  await checkHasNoSideEffect(`textNode.contains(textNode)`);
+
+  // Window
+  await checkHasNoSideEffect(`global_performance.now()`);
+
   testRunner.completeTest();
 
 
@@ -19,7 +44,6 @@
   }
 
   async function checkExpression(expression, expectSideEffect) {
-    testRunner.log(`Checking: \`${expression}\` for ${expectSideEffect ? '' : 'no'} side effect`);
     var response = await dp.Runtime.evaluate({expression, throwOnSideEffect: true});
     var hasSideEffect = false;
     var exceptionDetails = response.result.exceptionDetails;
@@ -27,7 +51,7 @@
         exceptionDetails.exception.description.startsWith('EvalError: Possible side-effect in debug-evaluate'))
       hasSideEffect = true;
     if (hasSideEffect !== expectSideEffect) {
-      testRunner.log(`FAIL: hasSideEffect = ${hasSideEffect}, expectSideEffect = ${expectSideEffect}`);
+      testRunner.log(`FAIL: ${expression} hasSideEffect = ${hasSideEffect}, expectSideEffect = ${expectSideEffect}`);
       testRunner.completeTest();
       return;
     }
