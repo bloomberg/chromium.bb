015252ecddd398964a7ccf8241c1aedcdcd89214
Sergey Poromov <poromov@chromium.org>
2018-04-18 18:44:48 +0000

    Remove unused CloudPolicyService::ManagedBy()
    This function was extracting domain from username in PolicyData
    which could be not up-to-date after domain rename.
    It's better to get rid of as much |username| usages as possible.
    
    BUG=780792
    TEST=Trybots
    
    Change-Id: I6aba952cb63d4450863ac7f3ee30c18a3a9830d1
    Reviewed-on: https://chromium-review.googlesource.com/1017122
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551761}
    Former-commit-id: 12b295bbff146a175c49c8d75f11b886343d8b98

--- components/policy/core/common/cloud/cloud_policy_service.cc
+++ components/policy/core/common/cloud/cloud_policy_service.cc
@@ -43,17 +43,6 @@ CloudPolicyService::~CloudPolicyService() {
   store_->RemoveObserver(this);
 }
 
-std::string CloudPolicyService::ManagedBy() const {
-  const em::PolicyData* policy = store_->policy();
-  if (policy) {
-    std::string username = policy->username();
-    std::size_t pos = username.find('@');
-    if (pos != std::string::npos)
-      return username.substr(pos + 1);
-  }
-  return std::string();
-}
-
 void CloudPolicyService::RefreshPolicy(const RefreshPolicyCallback& callback) {
   // If the client is not registered or is unregistering, bail out.
   if (!client_->is_registered() || unregister_state_ != UNREGISTER_NONE) {
