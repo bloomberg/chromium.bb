1663959bd6573bc61edaf5553abed5c77b0bf766
Noel Gordon <noel@chromium.org>
2018-04-18 04:42:32 +0000

    Add comments and Closure annotations from the JS fix for issue 831074
    Issue 831074 was resolved with a C++ fix, but there was a JS fix patch
    prior to that that also fixed the problem. It was abandoned [1] but it
    contained useful new comments + Closure annotations. Let's use them.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/1013823/18
    
    Bug: 833834
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ic33a6f90ba99eedebc631dae09cb8bc45040fac9
    Reviewed-on: https://chromium-review.googlesource.com/1014822
    Reviewed-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551581}
    Former-commit-id: 2c9c29527ada10af4745ab26dd000ebb6d5e055e

--- ui/file_manager/audio_player/js/background.js
+++ ui/file_manager/audio_player/js/background.js
@@ -7,16 +7,22 @@
  * TODO(yoshiki): Consider providing an exact size icon, instead of relying
  * on downsampling by ash.
  *
- * @type {string}
+ * @type {!string}
  * @const
  */
 var AUDIO_PLAYER_ICON = 'icons/audio-player-64.png';
 
+/**
+ * HTML source of the audio player.
+ * @type {!string}
+ * @const
+ */
 var AUDIO_PLAYER_APP_URL = 'audio_player.html';
 
 /**
  * Configuration of the audio player.
- * @type {Object}
+ * @type {!Object}
+ * @const
  */
 var audioPlayerCreateOptions = {
   id: 'audio-player',
@@ -40,7 +46,7 @@ function AudioPlayerBackground() {
 AudioPlayerBackground.prototype.__proto__ = BackgroundBase.prototype;
 
 /**
- * Called when an app is restarted.
+ * Called when an audio player app is restarted.
  */
 AudioPlayerBackground.prototype.onRestarted_ = function() {
   audioPlayer.reopen(function() {
@@ -51,22 +57,21 @@ AudioPlayerBackground.prototype.onRestarted_ = function() {
   });
 };
 
-
 /**
  * Backgound object. This is necessary for AppWindowWrapper.
- * @type {BackgroundBase}
+ * @type {!AudioPlayerBackground}
  */
 var background = new AudioPlayerBackground();
 
 /**
- * Wrapper of audio player window.
- * @type {SingletonAppWindowWrapper}
+ * Audio player app window wrapper.
+ * @type {!SingletonAppWindowWrapper}
  */
 var audioPlayer = new SingletonAppWindowWrapper(AUDIO_PLAYER_APP_URL,
                                                 audioPlayerCreateOptions);
 
 /**
- * Opens player window.
+ * Opens the audio player window.
  * @param {!Array<string>} urls List of audios to play and index to start
  *     playing.
  * @return {!Promise} Promise to be fulfilled on success, or rejected on error.
