faf3911ca812c270eff3863ef06b0589b64a1fd6
Qiang Xu <warx@google.com>
2018-04-18 19:26:38 +0000

    cros: remove MagnificationManagerImpl
    Changes:
    This is a preparation CL. MagnificationManager could just be a concrete
    class.
    
    Bug: 817157
    Test: compiles
    Change-Id: If2d1e94e2679d220df142d115eaf36c93d456b6b
    Reviewed-on: https://chromium-review.googlesource.com/1016321
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Qiang Xu <warx@google.com>
    Cr-Commit-Position: refs/heads/master@{#551779}
    Former-commit-id: bd0c79c3a69e07d6cb2dabef2a3d3bc4fea3a7da

--- chrome/browser/chromeos/accessibility/magnification_manager.cc
+++ chrome/browser/chromeos/accessibility/magnification_manager.cc
@@ -7,25 +7,19 @@
 #include <limits>
 #include <memory>
 
-#include "ash/accessibility/accessibility_controller.h"
 #include "ash/magnifier/magnification_controller.h"
-#include "ash/magnifier/partial_magnification_controller.h"
-#include "ash/public/cpp/accessibility_types.h"
 #include "ash/public/cpp/ash_pref_names.h"
 #include "ash/shell.h"
-#include "base/macros.h"
-#include "base/memory/singleton.h"
 #include "chrome/browser/chrome_notification_types.h"
+#include "chrome/browser/chromeos/accessibility/accessibility_manager.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
-#include "components/prefs/pref_member.h"
+#include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_service.h"
 #include "components/user_manager/user_manager.h"
 #include "content/public/browser/focused_node_details.h"
 #include "content/public/browser/notification_details.h"
-#include "content/public/browser/notification_observer.h"
-#include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_source.h"
 
@@ -33,246 +27,219 @@ namespace chromeos {
 
 namespace {
 MagnificationManager* g_magnification_manager = nullptr;
-}
-
-class MagnificationManagerImpl
-    : public MagnificationManager,
-      public content::NotificationObserver,
-      public user_manager::UserManager::UserSessionStateObserver {
- public:
-  MagnificationManagerImpl()
-      : profile_(NULL),
-        enabled_(false),
-        keep_focus_centered_(false),
-        scale_(0.0),
-        observing_focus_change_in_page_(false) {
-    registrar_.Add(this, chrome::NOTIFICATION_LOGIN_OR_LOCK_WEBUI_VISIBLE,
-                   content::NotificationService::AllSources());
-    registrar_.Add(this, chrome::NOTIFICATION_SESSION_STARTED,
-                   content::NotificationService::AllSources());
-    registrar_.Add(this, chrome::NOTIFICATION_PROFILE_DESTROYED,
-                   content::NotificationService::AllSources());
-  }
-
-  ~MagnificationManagerImpl() override {
-    CHECK(this == g_magnification_manager);
-  }
-
-  // MagnificationManager implimentation:
-  bool IsMagnifierEnabled() const override { return enabled_; }
-
-  void SetMagnifierEnabled(bool enabled) override {
-    if (!profile_)
-      return;
-
-    PrefService* prefs = profile_->GetPrefs();
-    prefs->SetBoolean(ash::prefs::kAccessibilityScreenMagnifierEnabled,
-                      enabled);
-    prefs->CommitPendingWrite();
-  }
-
-  void SaveScreenMagnifierScale(double scale) override {
-    if (!profile_)
-      return;
-
-    profile_->GetPrefs()->SetDouble(
-        ash::prefs::kAccessibilityScreenMagnifierScale, scale);
-  }
-
-  double GetSavedScreenMagnifierScale() const override {
-    if (!profile_)
-      return std::numeric_limits<double>::min();
-
-    return profile_->GetPrefs()->GetDouble(
-        ash::prefs::kAccessibilityScreenMagnifierScale);
-  }
-
-  void SetProfileForTest(Profile* profile) override { SetProfile(profile); }
-
-  // user_manager::UserManager::UserSessionStateObserver overrides:
-  void ActiveUserChanged(const user_manager::User* active_user) override {
-    if (active_user && active_user->is_profile_created())
-      SetProfile(ProfileManager::GetActiveUserProfile());
-  }
-
- private:
-  void SetProfile(Profile* profile) {
-    pref_change_registrar_.reset();
-
-    if (profile) {
-      // TODO(yoshiki): Move following code to PrefHandler.
-      pref_change_registrar_.reset(new PrefChangeRegistrar);
-      pref_change_registrar_->Init(profile->GetPrefs());
-      pref_change_registrar_->Add(
-          ash::prefs::kAccessibilityScreenMagnifierEnabled,
-          base::BindRepeating(
-              &MagnificationManagerImpl::UpdateMagnifierFromPrefs,
-              base::Unretained(this)));
-      pref_change_registrar_->Add(
-          ash::prefs::kAccessibilityScreenMagnifierCenterFocus,
-          base::BindRepeating(
-              &MagnificationManagerImpl::UpdateMagnifierFromPrefs,
-              base::Unretained(this)));
-      pref_change_registrar_->Add(
-          ash::prefs::kAccessibilityScreenMagnifierScale,
-          base::BindRepeating(
-              &MagnificationManagerImpl::UpdateMagnifierFromPrefs,
-              base::Unretained(this)));
-    }
-
-    profile_ = profile;
-    UpdateMagnifierFromPrefs();
-  }
-
-  virtual void SetMagnifierEnabledInternal(bool enabled) {
-    // This method may be invoked even when the other magnifier settings (e.g.
-    // type or scale) are changed, so we need to call magnification controller
-    // even if |enabled| is unchanged. Only if |enabled| is false and the
-    // magnifier is already disabled, we are sure that we don't need to reflect
-    // the new settings right now because the magnifier keeps disabled.
-    if (!enabled && !enabled_)
-      return;
-
-    enabled_ = enabled;
-
-    ash::Shell::Get()->magnification_controller()->SetEnabled(enabled_);
-    MonitorFocusInPageChange();
-  }
-
-  virtual void SetMagnifierKeepFocusCenteredInternal(bool keep_focus_centered) {
-    if (keep_focus_centered_ == keep_focus_centered)
-      return;
-
-    keep_focus_centered_ = keep_focus_centered;
-
-    ash::Shell::Get()->magnification_controller()->SetKeepFocusCentered(
-        keep_focus_centered_);
-  }
-
-  virtual void SetMagnifierScaleInternal(double scale) {
-    if (scale_ == scale)
-      return;
-
-    scale_ = scale;
-
-    ash::Shell::Get()->magnification_controller()->SetScale(
-        scale_, false /* animate */);
-  }
-
-  void UpdateMagnifierFromPrefs() {
-    if (!profile_)
-      return;
-
-    PrefService* prefs = profile_->GetPrefs();
-    const bool enabled =
-        prefs->GetBoolean(ash::prefs::kAccessibilityScreenMagnifierEnabled);
-    const bool keep_focus_centered =
-        prefs->GetBoolean(ash::prefs::kAccessibilityScreenMagnifierCenterFocus);
-    const double scale =
-        prefs->GetDouble(ash::prefs::kAccessibilityScreenMagnifierScale);
-
-    if (!enabled) {
-      SetMagnifierEnabledInternal(enabled);
-      SetMagnifierKeepFocusCenteredInternal(keep_focus_centered);
-      SetMagnifierScaleInternal(scale);
-    } else {
-      SetMagnifierScaleInternal(scale);
-      SetMagnifierKeepFocusCenteredInternal(keep_focus_centered);
-      SetMagnifierEnabledInternal(enabled);
-    }
-
-    AccessibilityStatusEventDetails details(
-        ACCESSIBILITY_TOGGLE_SCREEN_MAGNIFIER, enabled_);
-
-    if (!AccessibilityManager::Get())
-      return;
-    AccessibilityManager::Get()->NotifyAccessibilityStatusChanged(details);
-    if (ash::Shell::Get())
-      ash::Shell::Get()->UpdateCursorCompositingEnabled();
-  }
-
-  void MonitorFocusInPageChange() {
-    if (enabled_ && !observing_focus_change_in_page_) {
-      registrar_.Add(this, content::NOTIFICATION_FOCUS_CHANGED_IN_PAGE,
-                     content::NotificationService::AllSources());
-      observing_focus_change_in_page_ = true;
-    } else if (!enabled_ && observing_focus_change_in_page_) {
-      registrar_.Remove(this, content::NOTIFICATION_FOCUS_CHANGED_IN_PAGE,
-                        content::NotificationService::AllSources());
-      observing_focus_change_in_page_ = false;
-    }
-  }
-
-  // content::NotificationObserver implementation:
-  void Observe(int type,
-               const content::NotificationSource& source,
-               const content::NotificationDetails& details) override {
-    switch (type) {
-      case chrome::NOTIFICATION_LOGIN_OR_LOCK_WEBUI_VISIBLE: {
-        // Update |profile_| when entering the login screen.
-        Profile* profile = ProfileManager::GetActiveUserProfile();
-        if (ProfileHelper::IsSigninProfile(profile))
-          SetProfile(profile);
-        break;
-      }
-      case chrome::NOTIFICATION_SESSION_STARTED:
-        // Update |profile_| when entering a session.
-        SetProfile(ProfileManager::GetActiveUserProfile());
-
-        // Add a session state observer to be able to monitor session changes.
-        if (!session_state_observer_.get())
-          session_state_observer_.reset(
-              new user_manager::ScopedUserSessionStateObserver(this));
-        break;
-      case chrome::NOTIFICATION_PROFILE_DESTROYED: {
-        // Update |profile_| when exiting a session or shutting down.
-        Profile* profile = content::Source<Profile>(source).ptr();
-        if (profile_ == profile)
-          SetProfile(NULL);
-        break;
-      }
-      case content::NOTIFICATION_FOCUS_CHANGED_IN_PAGE: {
-        content::FocusedNodeDetails* node_details =
-            content::Details<content::FocusedNodeDetails>(details).ptr();
-        ash::Shell::Get()->magnification_controller()->HandleFocusedNodeChanged(
-            node_details->is_editable_node,
-            node_details->node_bounds_in_screen);
-        break;
-      }
-    }
-  }
-
-  Profile* profile_;
-
-  bool enabled_;
-  bool keep_focus_centered_;
-  double scale_;
-  bool observing_focus_change_in_page_;
-
-  content::NotificationRegistrar registrar_;
-  std::unique_ptr<PrefChangeRegistrar> pref_change_registrar_;
-  std::unique_ptr<user_manager::ScopedUserSessionStateObserver>
-      session_state_observer_;
-
-  DISALLOW_COPY_AND_ASSIGN(MagnificationManagerImpl);
-};
+}  // namespace
 
 // static
 void MagnificationManager::Initialize() {
-  CHECK(g_magnification_manager == NULL);
-  g_magnification_manager = new MagnificationManagerImpl();
+  CHECK(g_magnification_manager == nullptr);
+  g_magnification_manager = new MagnificationManager();
 }
 
 // static
 void MagnificationManager::Shutdown() {
   CHECK(g_magnification_manager);
   delete g_magnification_manager;
-  g_magnification_manager = NULL;
+  g_magnification_manager = nullptr;
 }
 
 // static
 MagnificationManager* MagnificationManager::Get() {
   return g_magnification_manager;
+}
+
+bool MagnificationManager::IsMagnifierEnabled() const {
+  return enabled_;
+}
+
+void MagnificationManager::SetMagnifierEnabled(bool enabled) {
+  if (!profile_)
+    return;
+
+  PrefService* prefs = profile_->GetPrefs();
+  prefs->SetBoolean(ash::prefs::kAccessibilityScreenMagnifierEnabled, enabled);
+  prefs->CommitPendingWrite();
+}
+
+void MagnificationManager::SaveScreenMagnifierScale(double scale) {
+  if (!profile_)
+    return;
+
+  profile_->GetPrefs()->SetDouble(
+      ash::prefs::kAccessibilityScreenMagnifierScale, scale);
+}
+
+double MagnificationManager::GetSavedScreenMagnifierScale() const {
+  if (!profile_)
+    return std::numeric_limits<double>::min();
+
+  return profile_->GetPrefs()->GetDouble(
+      ash::prefs::kAccessibilityScreenMagnifierScale);
+}
+
+void MagnificationManager::SetProfileForTest(Profile* profile) {
+  SetProfile(profile);
+}
+
+MagnificationManager::MagnificationManager() {
+  registrar_.Add(this, chrome::NOTIFICATION_LOGIN_OR_LOCK_WEBUI_VISIBLE,
+                 content::NotificationService::AllSources());
+  registrar_.Add(this, chrome::NOTIFICATION_SESSION_STARTED,
+                 content::NotificationService::AllSources());
+  registrar_.Add(this, chrome::NOTIFICATION_PROFILE_DESTROYED,
+                 content::NotificationService::AllSources());
+}
+
+MagnificationManager::~MagnificationManager() {
+  CHECK(this == g_magnification_manager);
+}
+
+void MagnificationManager::Observe(
+    int type,
+    const content::NotificationSource& source,
+    const content::NotificationDetails& details) {
+  switch (type) {
+    case chrome::NOTIFICATION_LOGIN_OR_LOCK_WEBUI_VISIBLE: {
+      // Update |profile_| when entering the login screen.
+      Profile* profile = ProfileManager::GetActiveUserProfile();
+      if (ProfileHelper::IsSigninProfile(profile))
+        SetProfile(profile);
+      break;
+    }
+    case chrome::NOTIFICATION_SESSION_STARTED:
+      // Update |profile_| when entering a session.
+      SetProfile(ProfileManager::GetActiveUserProfile());
+
+      // Add a session state observer to be able to monitor session changes.
+      if (!session_state_observer_.get())
+        session_state_observer_.reset(
+            new user_manager::ScopedUserSessionStateObserver(this));
+      break;
+    case chrome::NOTIFICATION_PROFILE_DESTROYED: {
+      // Update |profile_| when exiting a session or shutting down.
+      Profile* profile = content::Source<Profile>(source).ptr();
+      if (profile_ == profile)
+        SetProfile(NULL);
+      break;
+    }
+    case content::NOTIFICATION_FOCUS_CHANGED_IN_PAGE: {
+      content::FocusedNodeDetails* node_details =
+          content::Details<content::FocusedNodeDetails>(details).ptr();
+      ash::Shell::Get()->magnification_controller()->HandleFocusedNodeChanged(
+          node_details->is_editable_node, node_details->node_bounds_in_screen);
+      break;
+    }
+  }
+}
+
+void MagnificationManager::ActiveUserChanged(
+    const user_manager::User* active_user) {
+  if (active_user && active_user->is_profile_created())
+    SetProfile(ProfileManager::GetActiveUserProfile());
+}
+
+void MagnificationManager::SetProfile(Profile* profile) {
+  pref_change_registrar_.reset();
+
+  if (profile) {
+    // TODO(yoshiki): Move following code to PrefHandler.
+    pref_change_registrar_.reset(new PrefChangeRegistrar);
+    pref_change_registrar_->Init(profile->GetPrefs());
+    pref_change_registrar_->Add(
+        ash::prefs::kAccessibilityScreenMagnifierEnabled,
+        base::BindRepeating(&MagnificationManager::UpdateMagnifierFromPrefs,
+                            base::Unretained(this)));
+    pref_change_registrar_->Add(
+        ash::prefs::kAccessibilityScreenMagnifierCenterFocus,
+        base::BindRepeating(&MagnificationManager::UpdateMagnifierFromPrefs,
+                            base::Unretained(this)));
+    pref_change_registrar_->Add(
+        ash::prefs::kAccessibilityScreenMagnifierScale,
+        base::BindRepeating(&MagnificationManager::UpdateMagnifierFromPrefs,
+                            base::Unretained(this)));
+  }
+
+  profile_ = profile;
+  UpdateMagnifierFromPrefs();
+}
+
+void MagnificationManager::SetMagnifierEnabledInternal(bool enabled) {
+  // This method may be invoked even when the other magnifier settings (e.g.
+  // type or scale) are changed, so we need to call magnification controller
+  // even if |enabled| is unchanged. Only if |enabled| is false and the
+  // magnifier is already disabled, we are sure that we don't need to reflect
+  // the new settings right now because the magnifier keeps disabled.
+  if (!enabled && !enabled_)
+    return;
+
+  enabled_ = enabled;
+
+  ash::Shell::Get()->magnification_controller()->SetEnabled(enabled_);
+  MonitorFocusInPageChange();
+}
+
+void MagnificationManager::SetMagnifierKeepFocusCenteredInternal(
+    bool keep_focus_centered) {
+  if (keep_focus_centered_ == keep_focus_centered)
+    return;
+
+  keep_focus_centered_ = keep_focus_centered;
+
+  ash::Shell::Get()->magnification_controller()->SetKeepFocusCentered(
+      keep_focus_centered_);
+}
+
+void MagnificationManager::SetMagnifierScaleInternal(double scale) {
+  if (scale_ == scale)
+    return;
+
+  scale_ = scale;
+
+  ash::Shell::Get()->magnification_controller()->SetScale(scale_,
+                                                          false /* animate */);
+}
+
+void MagnificationManager::UpdateMagnifierFromPrefs() {
+  if (!profile_)
+    return;
+
+  PrefService* prefs = profile_->GetPrefs();
+  const bool enabled =
+      prefs->GetBoolean(ash::prefs::kAccessibilityScreenMagnifierEnabled);
+  const bool keep_focus_centered =
+      prefs->GetBoolean(ash::prefs::kAccessibilityScreenMagnifierCenterFocus);
+  const double scale =
+      prefs->GetDouble(ash::prefs::kAccessibilityScreenMagnifierScale);
+
+  if (!enabled) {
+    SetMagnifierEnabledInternal(enabled);
+    SetMagnifierKeepFocusCenteredInternal(keep_focus_centered);
+    SetMagnifierScaleInternal(scale);
+  } else {
+    SetMagnifierScaleInternal(scale);
+    SetMagnifierKeepFocusCenteredInternal(keep_focus_centered);
+    SetMagnifierEnabledInternal(enabled);
+  }
+
+  AccessibilityStatusEventDetails details(ACCESSIBILITY_TOGGLE_SCREEN_MAGNIFIER,
+                                          enabled_);
+
+  if (!AccessibilityManager::Get())
+    return;
+  AccessibilityManager::Get()->NotifyAccessibilityStatusChanged(details);
+  if (ash::Shell::Get())
+    ash::Shell::Get()->UpdateCursorCompositingEnabled();
+}
+
+void MagnificationManager::MonitorFocusInPageChange() {
+  if (enabled_ && !observing_focus_change_in_page_) {
+    registrar_.Add(this, content::NOTIFICATION_FOCUS_CHANGED_IN_PAGE,
+                   content::NotificationService::AllSources());
+    observing_focus_change_in_page_ = true;
+  } else if (!enabled_ && observing_focus_change_in_page_) {
+    registrar_.Remove(this, content::NOTIFICATION_FOCUS_CHANGED_IN_PAGE,
+                      content::NotificationService::AllSources());
+    observing_focus_change_in_page_ = false;
+  }
 }
 
 }  // namespace chromeos
