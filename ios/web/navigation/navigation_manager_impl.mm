1e01b7c9bea3b5d91e29a1d3a2d725807bd662eb
Danyao Wang <danyao@google.com>
2018-04-18 19:30:16 +0000

    Reland "[Nav Experiment] Propagate virtual URL in ReloadWithUserAgentType."
    Fixed broken unit test reported in crbug.com/834307
    
    This is a reland of 57902635ab6d33a1074210321a96961ec9e12e96 [formerly f1291fb3d1932fb80484eb81ec2769bc71d31c18]
    
    Original change's description:
    > [Nav Experiment] Propagate virtual URL in ReloadWithUserAgentType.
    >
    > This hides the internal URL used by WKBasedNavigationManager to force
    > create an entry in WKBackForwardList when reloading with a different
    > user agent type (see crrev.com/c/919175).
    >
    > Also fixed a bug in NavigationManagerTest which wasn't testing the
    > redirect case when GetParam() == TEST_WK_BASED_NAVIGATION_MANAGER.
    >
    > Bug: 833958
    > Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    > Change-Id: I095178302bfc4d209c2d058aab736777cc8b2de4
    > Reviewed-on: https://chromium-review.googlesource.com/1016002
    > Reviewed-by: Eugene But <eugenebut@chromium.org>
    > Commit-Queue: Danyao Wang <danyao@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#551548}
    
    Bug: 833958,834307
    Change-Id: I9856f340e408d92fbce934f8ae2bbddb119b066d
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Reviewed-on: https://chromium-review.googlesource.com/1017005
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551781}
    Former-commit-id: 6f991a72a11c64549f11ae37183d12fb9a7e2745

--- ios/web/navigation/navigation_manager_impl.mm
+++ ios/web/navigation/navigation_manager_impl.mm
@@ -348,19 +348,21 @@ void NavigationManagerImpl::ReloadWithUserAgentType(
     return;
 
   // |reloadURL| will be empty if a page was open by DOM.
-  GURL reloadURL(last_non_redirect_item->GetOriginalRequestURL());
-  if (reloadURL.is_empty()) {
-    reloadURL = last_non_redirect_item->GetVirtualURL();
+  GURL reload_url(last_non_redirect_item->GetOriginalRequestURL());
+  if (reload_url.is_empty()) {
+    reload_url = last_non_redirect_item->GetVirtualURL();
   }
 
   // Reload using a client-side redirect URL to create a new entry in
   // WKBackForwardList for the new user agent type. This hack is not needed for
   // LegacyNavigationManagerImpl which manages its own history entries.
   if (web::GetWebClient()->IsSlimNavigationManagerEnabled()) {
-    reloadURL = wk_navigation_util::CreateRedirectUrl(reloadURL);
+    reload_url = wk_navigation_util::CreateRedirectUrl(reload_url);
   }
 
-  WebLoadParams params(reloadURL);
+  WebLoadParams params(reload_url);
+  if (last_non_redirect_item->GetVirtualURL() != reload_url)
+    params.virtual_url = last_non_redirect_item->GetVirtualURL();
   params.referrer = last_non_redirect_item->GetReferrer();
   params.transition_type = ui::PAGE_TRANSITION_RELOAD;
 
