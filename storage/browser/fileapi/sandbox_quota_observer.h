a3bb3946239b62d1d7d3d11ab411e3ff267ae462
Francois Doray <fdoray@chromium.org>
2018-04-18 04:01:15 +0000

    Use base::Timer in storage::SandboxQuotaObserver.
    Previously, storage::SandboxQuotaObserver used a custom timer
    implementation (TimedTaskHelper). Crash involving this custom timer
    implementation have been reported (e.g. http://crash/e2c6ff1ef4863460).
    Therefore, we are moving all uses of TimedTaskHelper to the well
    maintained base::Timer.
    
    Note: The TaskRunner passed to the TimedTaskHelper in
    storage::SandboxQuotaObserver was always the one on which the
    storage::SandboxQuotaObserver was used (as proved by the
    DCHECK(update_notify_runner_->RunsTasksInCurrentSequence()) at the top
    of the method from which the timer is started), so there is no need to
    an explicit call to base::Timer::SetTaskRunner in this CL.
    
    Bug: 760326
    Change-Id: I3e6c8955338437e99b7f6437ed2faf17b00038f8
    Reviewed-on: https://chromium-review.googlesource.com/1012588
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551573}
    Former-commit-id: a45b2666d846a8c39b759db198cf74e0de30ae91

--- storage/browser/fileapi/sandbox_quota_observer.h
+++ storage/browser/fileapi/sandbox_quota_observer.h
@@ -8,13 +8,13 @@
 #include <stdint.h>
 
 #include <map>
-#include <memory>
 
 #include "base/compiler_specific.h"
 #include "base/files/file_path.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
+#include "base/timer/timer.h"
 #include "storage/browser/fileapi/file_observers.h"
 #include "storage/browser/fileapi/file_system_url.h"
 
@@ -30,7 +30,6 @@ namespace storage {
 
 class FileSystemUsageCache;
 class FileSystemURL;
-class TimedTaskHelper;
 class ObfuscatedFileUtil;
 
 class SandboxQuotaObserver
@@ -74,7 +73,7 @@ class SandboxQuotaObserver
   FileSystemUsageCache* file_system_usage_cache_;
 
   PendingUpdateNotificationMap pending_update_notification_;
-  std::unique_ptr<TimedTaskHelper> delayed_cache_update_helper_;
+  base::OneShotTimer delayed_cache_update_helper_;
 
   DISALLOW_COPY_AND_ASSIGN(SandboxQuotaObserver);
 };
