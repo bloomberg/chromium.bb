2bfd5ebd653a955eccad9f457ea58974867b9684
Peter Boström <pbos@chromium.org>
2018-04-18 18:50:24 +0000

    Add profile icons to the toolbar avatar button
    This adds profile icons to the toolbar avatar button that is available
    under Material refresh. These icons are a visible badging of the current
    user.
    
    It also enlarges the avatar icon to 20 dips matching the avatar menu. 16
    dips was very cramped here for several built-in avatar icons as well as
    profile pictures.
    
    Bug: chromium:822070
    Change-Id: I195fc161639cf31479bc20440b37a2eb1c3039e3
    Reviewed-on: https://chromium-review.googlesource.com/1015795
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551762}
    Former-commit-id: e52da01aac484eb087545585ae36a807c946c76d

--- chrome/browser/ui/views/profiles/avatar_toolbar_button.h
+++ chrome/browser/ui/views/profiles/avatar_toolbar_button.h
@@ -5,13 +5,36 @@
 #ifndef CHROME_BROWSER_UI_VIEWS_PROFILES_AVATAR_TOOLBAR_BUTTON_H_
 #define CHROME_BROWSER_UI_VIEWS_PROFILES_AVATAR_TOOLBAR_BUTTON_H_
 
+#include "base/macros.h"
+#include "base/scoped_observer.h"
+#include "chrome/browser/profiles/profile_attributes_storage.h"
+#include "chrome/browser/ui/avatar_button_error_controller.h"
+#include "chrome/browser/ui/avatar_button_error_controller_delegate.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_button.h"
 
-class AvatarToolbarButton : public ToolbarButton {
+class AvatarToolbarButton : public ToolbarButton,
+                            public AvatarButtonErrorControllerDelegate,
+                            public ProfileAttributesStorage::Observer {
  public:
   AvatarToolbarButton(Profile* profile, views::ButtonListener* listener);
+  ~AvatarToolbarButton() override;
 
   void UpdateIcon();
+
+ private:
+  // AvatarButtonErrorControllerDelegate:
+  void OnAvatarErrorChanged() override;
+
+  // ProfileAttributesStorage::Observer:
+  void OnProfileAvatarChanged(const base::FilePath& profile_path) override;
+
+  Profile* const profile_;
+
+  AvatarButtonErrorController error_controller_;
+  ScopedObserver<ProfileAttributesStorage, AvatarToolbarButton>
+      profile_observer_;
+
+  DISALLOW_COPY_AND_ASSIGN(AvatarToolbarButton);
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_PROFILES_AVATAR_TOOLBAR_BUTTON_H_
