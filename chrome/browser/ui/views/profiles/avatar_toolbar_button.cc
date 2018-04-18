62cadb774aafc2ebde36fd772126352ef7c9cb15
Peter Boström <pbos@chromium.org>
2018-04-18 18:50:24 +0000

    Add profile icons to the toolbar avatar button
    This adds profile icons to the toolbar avatar button that is available
    under Material refresh. These icons are a visible badging of the current
    user.
    
    It also enlarges the avatar icon to 20 dips matching the avatar menu. 16
    dips was very cramped here for several built-in avatar icons as well as
    profile pictures.
    
    Bug: chromium:822070
    Change-Id: I195fc161639cf31479bc20440b37a2eb1c3039e3
    Reviewed-on: https://chromium-review.googlesource.com/1015795
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551762}
    Former-commit-id: e52da01aac484eb087545585ae36a807c946c76d

--- chrome/browser/ui/views/profiles/avatar_toolbar_button.cc
+++ chrome/browser/ui/views/profiles/avatar_toolbar_button.cc
@@ -6,7 +6,11 @@
 
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/app/vector_icons/vector_icons.h"
+#include "chrome/browser/browser_process.h"
+#include "chrome/browser/profiles/avatar_menu.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_avatar_icon_util.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/themes/theme_properties.h"
 #include "chrome/browser/ui/view_ids.h"
 #include "chrome/grit/generated_resources.h"
@@ -14,11 +18,20 @@
 #include "ui/base/material_design/material_design_controller.h"
 #include "ui/base/models/menu_model.h"
 #include "ui/base/theme_provider.h"
+#include "ui/gfx/image/image.h"
 #include "ui/gfx/paint_vector_icon.h"
 
 AvatarToolbarButton::AvatarToolbarButton(Profile* profile,
                                          views::ButtonListener* listener)
-    : ToolbarButton(profile, listener, nullptr) {
+    : ToolbarButton(profile, listener, nullptr),
+      profile_(profile),
+      error_controller_(this, profile_),
+      profile_observer_(this) {
+  profile_observer_.Add(
+      &g_browser_process->profile_manager()->GetProfileAttributesStorage());
+
+  SetImageAlignment(HorizontalAlignment::ALIGN_CENTER,
+                    VerticalAlignment::ALIGN_MIDDLE);
   set_triggerable_event_flags(ui::EF_LEFT_MOUSE_BUTTON |
                               ui::EF_MIDDLE_MOUSE_BUTTON);
   set_tag(IDC_SHOW_AVATAR_MENU);
@@ -36,17 +49,36 @@ AvatarToolbarButton::AvatarToolbarButton(Profile* profile,
   Init();
 }
 
+AvatarToolbarButton::~AvatarToolbarButton() = default;
+
 void AvatarToolbarButton::UpdateIcon() {
   const bool is_touch =
       ui::MaterialDesignController::IsTouchOptimizedUiEnabled();
   // TODO(pbos): Move these constants to LayoutProvider or LayoutConstants.
-  const int icon_size = is_touch ? 24 : 16;
+  const int icon_size = is_touch ? 24 : 20;
 
+  gfx::Image avatar_icon;
   // TODO(pbos): Account for incognito by either changing the icon and
   // effectively disabling the menu or by not showing it at all in incognito.
-  SetImage(
-      views::Button::STATE_NORMAL,
-      gfx::CreateVectorIcon(kUserAccountAvatarIcon, icon_size,
-                            GetThemeProvider()->GetColor(
-                                ThemeProperties::COLOR_TOOLBAR_BUTTON_ICON)));
+  if (AvatarMenu::GetImageForMenuButton(profile_->GetPath(), &avatar_icon) ==
+      AvatarMenu::ImageLoadStatus::LOADED) {
+    avatar_icon = profiles::GetSizedAvatarIcon(
+        avatar_icon, true, icon_size, icon_size, profiles::SHAPE_CIRCLE);
+    SetImage(views::Button::STATE_NORMAL, avatar_icon.ToImageSkia());
+  } else {
+    SetImage(
+        views::Button::STATE_NORMAL,
+        gfx::CreateVectorIcon(kUserAccountAvatarIcon, icon_size,
+                              GetThemeProvider()->GetColor(
+                                  ThemeProperties::COLOR_TOOLBAR_BUTTON_ICON)));
+  }
+}
+
+void AvatarToolbarButton::OnAvatarErrorChanged() {
+  UpdateIcon();
+}
+
+void AvatarToolbarButton::OnProfileAvatarChanged(
+    const base::FilePath& profile_path) {
+  UpdateIcon();
 }
