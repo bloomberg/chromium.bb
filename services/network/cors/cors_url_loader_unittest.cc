34b3dc143d4c2b9d83fca03f01233aa2a54a9ece
Takashi Toyoshima <toyoshim@chromium.org>
2018-04-18 09:52:30 +0000

    OOR-CORS: strip username and password in CORSURLLoader
    In the legacy Blink CORS, these information are stripped in
    DocumentThreadableLoader in MakeCrossOriginAccessRequest().
    
    Current OOR-CORS implementation only has DCHECKS for them,
    but hasn't stripped.
    
    Bug: 803766
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: If1a4657804215a8ce9a302acb658f9407348a79a
    Reviewed-on: https://chromium-review.googlesource.com/999672
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551621}
    Former-commit-id: 58b5d61eb090fbe8aa6dfea4172b43df2e3845b0

--- services/network/cors/cors_url_loader_unittest.cc
+++ services/network/cors/cors_url_loader_unittest.cc
@@ -53,23 +53,28 @@ class TestURLLoaderFactory : public mojom::URLLoaderFactory {
 
   bool IsCreateLoaderAndStartCalled() { return !!client_ptr_; }
 
+  const GURL& GetRequestedURL() const { return requested_url_; }
+
  private:
   // mojom::URLLoaderFactory implementation.
   void CreateLoaderAndStart(mojom::URLLoaderRequest request,
                             int32_t routing_id,
                             int32_t request_id,
                             uint32_t options,
-                            const ResourceRequest& url_request,
+                            const ResourceRequest& resource_request,
                             mojom::URLLoaderClientPtr client,
                             const net::MutableNetworkTrafficAnnotationTag&
                                 traffic_annotation) override {
     DCHECK(client);
+    requested_url_ = resource_request.url;
     client_ptr_ = std::move(client);
   }
 
   void Clone(mojom::URLLoaderFactoryRequest request) override { NOTREACHED(); }
 
   mojom::URLLoaderClientPtr client_ptr_;
+
+  GURL requested_url_;
 
   base::WeakPtrFactory<TestURLLoaderFactory> weak_factory_;
 
@@ -122,6 +127,11 @@ class CORSURLLoaderTest : public testing::Test {
   void NotifyLoaderClientOnComplete(int error_code) {
     DCHECK(test_url_loader_factory_);
     test_url_loader_factory_->NotifyClientOnComplete(error_code);
+  }
+
+  const GURL& GetRequestedURL() {
+    DCHECK(test_url_loader_factory_);
+    return test_url_loader_factory_->GetRequestedURL();
   }
 
   const TestURLLoaderClient& client() const { return test_cors_loader_client_; }
@@ -258,6 +268,26 @@ TEST_F(CORSURLLoaderTest,
   ASSERT_TRUE(client().completion_status().cors_error_status);
   EXPECT_EQ(mojom::CORSError::kAllowOriginMismatch,
             client().completion_status().cors_error_status->cors_error);
+}
+
+TEST_F(CORSURLLoaderTest, StripUsernameAndPassword) {
+  const GURL origin("http://example.com");
+  const GURL url("http://foo:bar@other.com/foo.png");
+  std::string stripped_url = "http://other.com/foo.png";
+  CreateLoaderAndStart(origin, url, mojom::FetchRequestMode::kCORS);
+
+  NotifyLoaderClientOnReceiveResponse(
+      "Access-Control-Allow-Origin: http://example.com");
+  NotifyLoaderClientOnComplete(net::OK);
+
+  RunUntilComplete();
+
+  EXPECT_TRUE(IsNetworkLoaderStarted());
+  EXPECT_FALSE(client().has_received_redirect());
+  EXPECT_TRUE(client().has_received_response());
+  EXPECT_TRUE(client().has_received_completion());
+  EXPECT_EQ(net::OK, client().completion_status().error_code);
+  EXPECT_EQ(stripped_url, GetRequestedURL().spec());
 }
 
 }  // namespace
