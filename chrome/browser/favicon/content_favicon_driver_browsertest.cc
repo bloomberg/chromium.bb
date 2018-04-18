e5aa74c514d5dce9a4994a52a27600034dbef42f
Mikel Astiz <mastiz@chromium.org>
2018-04-18 22:46:30 +0000

    Fix sending partial list of favicons on updates
    FrameHostMsg_UpdateFaviconURL() contains a list of favicons that the
    browser process expects to be complete (ContentFaviconDriver).
    
    On favicon changes via Javascript, handled via
    RenderFrameImpl::DidChangeIcon(), a partial list (filtered by icon
    type) was sent. This is tricky to handle and not worth optimizing for,
    so this patch proposes to instead always send the full list of favicons.
    
    Bug: 833476
    Change-Id: Ie62f2ddcddfa740b470aecb767ae70991ca09eb7
    Reviewed-on: https://chromium-review.googlesource.com/1013464
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551866}
    Former-commit-id: 37e6023df7523f1339780f70fbb633382f686317

--- chrome/browser/favicon/content_favicon_driver_browsertest.cc
+++ chrome/browser/favicon/content_favicon_driver_browsertest.cc
@@ -314,6 +314,25 @@ IN_PROC_BROWSER_TEST_F(ContentFaviconDriverTest,
       GetFaviconForPageURL(url, favicon_base::IconType::kFavicon).bitmap_data);
 }
 
+// Test that favicon changes via Javascript affecting a specific type (touch
+// icons) do not influence the rest of the types (favicons).
+IN_PROC_BROWSER_TEST_F(ContentFaviconDriverTest, ChangeTouchIconViaJavascript) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+  GURL url = embedded_test_server()->GetURL(
+      "/favicon/page_change_touch_icon_via_js.html");
+
+  PendingTaskWaiter waiter(web_contents());
+  waiter.AlsoRequireTitle(base::ASCIIToUTF16("OK"));
+  ui_test_utils::NavigateToURLWithDisposition(
+      browser(), url, WindowOpenDisposition::CURRENT_TAB,
+      ui_test_utils::BROWSER_TEST_NONE);
+  waiter.Wait();
+
+  EXPECT_NE(
+      nullptr,
+      GetFaviconForPageURL(url, favicon_base::IconType::kFavicon).bitmap_data);
+}
+
 // Test that favicon mappings are removed if the page initially lists a touch
 // icon and later uses Javascript to remove it.
 #if defined(OS_ANDROID)
