355bc3bb4b93e9f0669da9adea192b24e0ef2d1c
Xiaohan Wang <xhwang@chromium.org>
2018-04-19 17:38:50 +0000

    media: Refactor supported CDM version check
    - Convert runtime DCHECK to compile time static_assert.
    - Add helper function to simplify the check logic.
    - Use CDM interface version as CdmWrapperImpl template argument to
      simplify some code and check logic.
    
    Bug: 799169,733821
    Test: No functionality change.
    Change-Id: I2a97be9a507db853eb91b739bf26511492665eee
    Reviewed-on: https://chromium-review.googlesource.com/1018144
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552070}
    Former-commit-id: ff1f496d021b78cd49df2e26e1bcc2c47f2a4044

--- media/cdm/supported_cdm_versions.cc
+++ media/cdm/supported_cdm_versions.cc
@@ -4,42 +4,12 @@
 
 #include "media/cdm/supported_cdm_versions.h"
 
-#include "media/cdm/api/content_decryption_module.h"
-
 namespace media {
 
 bool IsSupportedCdmModuleVersion(int version) {
   switch (version) {
     // Latest.
     case CDM_MODULE_VERSION:
-      return true;
-    default:
-      return false;
-  }
-}
-
-bool IsSupportedCdmInterfaceVersion(int version) {
-  static_assert(cdm::ContentDecryptionModule::kVersion ==
-                    cdm::ContentDecryptionModule_9::kVersion,
-                "update the code below");
-  switch (version) {
-    // Supported versions in decreasing order.
-    case cdm::ContentDecryptionModule_10::kVersion:
-    case cdm::ContentDecryptionModule_9::kVersion:
-      return true;
-    default:
-      return false;
-  }
-}
-
-bool IsSupportedCdmHostVersion(int version) {
-  static_assert(cdm::ContentDecryptionModule::Host::kVersion ==
-                    cdm::ContentDecryptionModule_9::Host::kVersion,
-                "update the code below");
-  switch (version) {
-    // Supported versions in decreasing order.
-    case cdm::Host_10::kVersion:
-    case cdm::Host_9::kVersion:
       return true;
     default:
       return false;
