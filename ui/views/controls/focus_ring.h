3a1551ac6815f27cdecb5a496e510ec85c3f669a
Patti <patricialor@chromium.org>
2018-04-19 05:21:49 +0000

    FocusRing/Views: Use only one Install() signature accepting a SkColor.
    Consolidate the two existing FocusRing::Install() functions into one for
    simplicity and update callers. FocusRing users may still apply overrides to the
    FocusRing color, but must observe for changes to the NativeTheme if that is the
    source of their chosen color.
    
    Bug: 831926
    Change-Id: Ica2a94ffef155f29f1f3d08cccec0c87055f4c83
    Reviewed-on: https://chromium-review.googlesource.com/1013671
    Commit-Queue: Patti <patricialor@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#551948}
    Former-commit-id: a7a562592505582e21f66c6a802eff61b15ca9cb

--- ui/views/controls/focus_ring.h
+++ ui/views/controls/focus_ring.h
@@ -17,25 +17,19 @@ namespace views {
 // FocusRing is a View that is designed to act as an indicator of focus for its
 // parent. It is a stand-alone view that paints to a layer which extends beyond
 // the bounds of its parent view.
-class VIEWS_EXPORT FocusRing : public View, public ViewObserver {
+class VIEWS_EXPORT FocusRing : public View {
  public:
   static const char kViewClassName[];
 
   // Create a FocusRing and adds it to |parent|, or updates the one that already
-  // exists with the given |color| and |corner_radius|.
-  // TODO(crbug.com/831926): Prefer using the below Install() method - this one
-  // should eventually be removed.
+  // exists with the given |color| and |corner_radius|. If not provided, the
+  // focus ring color defaults to ui::NativeTheme::kColorId_FocusedBorderColor.
+  // TODO(crbug.com/831926): Investigate switching to a ui::NativeTheme::ColorId
+  // instead of a raw SkColor.
   static FocusRing* Install(
       View* parent,
-      SkColor color,
+      SkColor override_color = kInvalidColor,
       float corner_radius = FocusableBorder::kCornerRadiusDp);
-
-  // Similar to FocusRing::Install(View, SkColor, float), but
-  // |override_color_id| will be used in place of the default coloration
-  // when provided.
-  static FocusRing* Install(View* parent,
-                            ui::NativeTheme::ColorId override_color_id =
-                                ui::NativeTheme::kColorId_NumColors);
 
   // Removes the FocusRing from |parent|.
   static void Uninstall(View* parent);
@@ -48,18 +42,24 @@ class VIEWS_EXPORT FocusRing : public View, public ViewObserver {
   void Layout() override;
   void OnPaint(gfx::Canvas* canvas) override;
 
-  // ViewObserver:
-  void OnViewNativeThemeChanged(View* observed_view) override;
-
  protected:
-  FocusRing(SkColor color, float corner_radius);
+  FocusRing(View* parent, SkColor color, float corner_radius);
   ~FocusRing() override;
 
  private:
+  SkColor GetColor() const;
+
+  static constexpr SkColor kInvalidColor = SK_ColorTRANSPARENT;
+
+  // The View this focus ring is installed on.
+  View* view_;
+
+  // The focus ring color.
   SkColor color_;
+
+  // The corner radius of the focus ring. This will be slightly enlarged by half
+  // the thickness of the ring to make sure it hugs the border of |view_|.
   float corner_radius_;
-  base::Optional<ui::NativeTheme::ColorId> override_color_id_;
-  ScopedObserver<View, FocusRing> view_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(FocusRing);
 };
