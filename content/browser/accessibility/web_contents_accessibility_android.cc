3fd454ee30e5be7e3d57fc1de94c2239087c7dc2
Dominic Mazzoni <dmazzoni@chromium.org>
2018-04-19 21:41:44 +0000

    Revert "Android Accessibility: Set flag to enable jump to last element on ACTION_PREVIOUS_HTML_ELEMENT"
    This reverts commit 3f1f053061afe1f0abe615bad07c66e023e5ce2d [formerly 90d351407da304eb29e4b94ab937a527fe7f2347].
    
    Reason for revert: infinite loop (crbug.com/834866)
    
    Original change's description:
    > Android Accessibility: Set flag to enable jump to last element on ACTION_PREVIOUS_HTML_ELEMENT
    >
    > This CL is a followup on the following CL:
    > https://chromium-review.googlesource.com/736043
    >
    > The above CL returns the deepest child of web view on ACTION_PREVIOUS_HTML_ELEMENT if
    > can_wrap_to_last_element flag in OneShotAccessibilityTreeSearch is set to true.
    > This CL sets that flag for Android. Assuming a web view is in between native elements, the
    > following order will be followed for a11y focus:
    > Native elements -> web view container -> inside web view container -> native elements.
    > So web view container will always be focused only in the above order.
    >
    > Bug: 661793
    > Change-Id: I62dbcb143ed5a152172dc7cde9355f4b7eed79e1
    > Reviewed-on: https://chromium-review.googlesource.com/884302
    > Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    > Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#536742}
    
    TBR=dmazzoni@chromium.org,ibobra@google.com
    NOTRY=true
    
    Bug: 661793, 834866
    Change-Id: I3dc34c17908aa06c33a904415d10fc0d5a56fc2c
    Reviewed-on: https://chromium-review.googlesource.com/1019960
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552169}
    Former-commit-id: c89b77ae1e33f331ee6766e89c5c41530675aa36

--- content/browser/accessibility/web_contents_accessibility_android.cc
+++ content/browser/accessibility/web_contents_accessibility_android.cc
@@ -950,7 +950,9 @@ jint WebContentsAccessibilityAndroid::FindElementType(
                                : OneShotAccessibilityTreeSearch::BACKWARDS);
   tree_search.SetResultLimit(1);
   tree_search.SetImmediateDescendantsOnly(false);
-  tree_search.SetCanWrapToLastElement(true);
+  // SetCanWrapToLastElement needs to be set as true after talkback pushes its
+  // corresponding change for b/29103330.
+  tree_search.SetCanWrapToLastElement(false);
   tree_search.SetVisibleOnly(false);
   tree_search.AddPredicate(predicate);
 
