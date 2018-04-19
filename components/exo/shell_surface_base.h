4b495500f84ecf15e84c32310b828b395bc16cd5
David Reveman <reveman@chromium.org>
2018-04-19 21:59:11 +0000

    exo: Don't assume that application and startup IDs are non-null.
    Use base::Optional for startup and application ID in order to
    handle null values properly. Setting one of these to null will
    now clear the window property.
    
    Bug: 834971
    Test: exo_unittests --gtest_filter=ShellSurfaceTest.Set*Id
    Change-Id: I395fb02200f310b027060eb28e0132d8976532ab
    Reviewed-on: https://chromium-review.googlesource.com/1020042
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Commit-Queue: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552178}
    Former-commit-id: 8bc1641aca75f25f995b55910f2b4e94562cb24f

--- components/exo/shell_surface_base.h
+++ components/exo/shell_surface_base.h
@@ -117,15 +117,17 @@ class ShellSurfaceBase : public SurfaceTreeHost,
 
   // Sets the application ID for the window. The application ID identifies the
   // general class of applications to which the window belongs.
-  static void SetApplicationId(aura::Window* window, const std::string& id);
+  static void SetApplicationId(aura::Window* window,
+                               const base::Optional<std::string>& id);
   static const std::string* GetApplicationId(const aura::Window* window);
 
   // Set the application ID for the surface.
-  void SetApplicationId(const std::string& application_id);
+  void SetApplicationId(const char* application_id);
 
   // Sets the startup ID for the window. The startup ID identifies the
   // application using startup notification protocol.
-  static void SetStartupId(aura::Window* window, const std::string& id);
+  static void SetStartupId(aura::Window* window,
+                           const base::Optional<std::string>& id);
   static const std::string* GetStartupId(aura::Window* window);
 
   // Set the startup ID for the surface.
@@ -358,8 +360,8 @@ class ShellSurfaceBase : public SurfaceTreeHost,
   SkColor active_frame_color_ = SK_ColorBLACK;
   SkColor inactive_frame_color_ = SK_ColorBLACK;
   bool pending_show_widget_ = false;
-  std::string application_id_;
-  std::string startup_id_;
+  base::Optional<std::string> application_id_;
+  base::Optional<std::string> startup_id_;
   base::RepeatingClosure close_callback_;
   base::OnceClosure surface_destroyed_callback_;
   ScopedConfigure* scoped_configure_ = nullptr;
--- components/exo/shell_surface_unittest.cc
+++ components/exo/shell_surface_unittest.cc
@@ -265,6 +265,9 @@ TEST_F(ShellSurfaceTest, SetApplicationId) {
   EXPECT_EQ("pre-widget-id", *ShellSurface::GetApplicationId(window));
   shell_surface->SetApplicationId("test");
   EXPECT_EQ("test", *ShellSurface::GetApplicationId(window));
+
+  shell_surface->SetApplicationId(nullptr);
+  EXPECT_EQ(nullptr, ShellSurface::GetApplicationId(window));
 }
 
 TEST_F(ShellSurfaceTest, SetStartupId) {
@@ -283,6 +286,9 @@ TEST_F(ShellSurfaceTest, SetStartupId) {
   EXPECT_EQ("pre-widget-id", *ShellSurface::GetStartupId(window));
   shell_surface->SetStartupId("test");
   EXPECT_EQ("test", *ShellSurface::GetStartupId(window));
+
+  shell_surface->SetStartupId(nullptr);
+  EXPECT_EQ(nullptr, ShellSurface::GetStartupId(window));
 }
 
 TEST_F(ShellSurfaceTest, Move) {
