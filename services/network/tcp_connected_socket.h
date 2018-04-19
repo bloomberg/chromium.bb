68a94391212ef1ebd44783e85850b45070f8b022
Helen Li <xunjieli@chromium.org>
2018-04-19 21:29:18 +0000

    Move writing/reading logic out of tcp_connected_socket.cc into a separate class
    This CL moves writing/reading logic out of tcp_connected_socket.cc into a
    separate class, socket_data_pump.h/cc. This separate class will be re-used by
    tls socket implementation.
    
    Bug: 721401
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I5d5b723d52c10a653f06b0f6e8eed9fb5b895899
    Reviewed-on: https://chromium-review.googlesource.com/1005535
    Commit-Queue: Helen Li <xunjieli@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552163}
    Former-commit-id: 17c77ccb593beabff75d207213d1b31b7e369f56

--- services/network/tcp_connected_socket.h
+++ services/network/tcp_connected_socket.h
@@ -8,7 +8,6 @@
 #include <memory>
 
 #include "base/component_export.h"
-#include "base/containers/span.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "mojo/public/cpp/system/data_pipe.h"
@@ -23,11 +22,13 @@
 #include "services/network/public/cpp/net_adapters.h"
 #include "services/network/public/mojom/network_service.mojom.h"
 #include "services/network/public/mojom/tcp_socket.mojom.h"
+#include "services/network/socket_data_pump.h"
 
 namespace net {
 class ClientSocketFactory;
 class NetLog;
 class StreamSocket;
+class ClientSocketFactory;
 }  // namespace net
 
 namespace network {
@@ -59,23 +60,6 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) TCPConnectedSocket
   // Invoked when net::TCPClientSocket::Connect() completes.
   void OnConnectCompleted(int net_result);
 
-  // Helper to start watching |send_stream_| and |receive_stream_|.
-  void StartWatching();
-
-  // "Receiving" in this context means reading from |socket_| and writing to
-  // the Mojo |receive_stream_|.
-  void ReceiveMore();
-  void OnReceiveStreamWritable(MojoResult result);
-  void OnNetworkReadCompleted(int result);
-  void ShutdownReceive();
-
-  // "Writing" is reading from the Mojo |send_stream_| and writing to the
-  // |socket_|.
-  void SendMore();
-  void OnSendStreamReadable(MojoResult result);
-  void OnNetworkWriteCompleted(int result);
-  void ShutdownSend();
-
   mojom::TCPConnectedSocketObserverPtr observer_;
 
   net::NetLog* net_log_;
@@ -85,19 +69,7 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) TCPConnectedSocket
 
   mojom::NetworkContext::CreateTCPConnectedSocketCallback connect_callback_;
 
-  // The *stream handles will be null while there is an in-progress transation
-  // between net and mojo. During this time, the handle will be owned by the
-  // *PendingBuffer.
-
-  // For reading from the Mojo pipe and writing to the network.
-  mojo::ScopedDataPipeConsumerHandle send_stream_;
-  scoped_refptr<MojoToNetPendingBuffer> pending_send_;
-  mojo::SimpleWatcher readable_handle_watcher_;
-
-  // For reading from the network and writing to Mojo pipe.
-  mojo::ScopedDataPipeProducerHandle receive_stream_;
-  scoped_refptr<NetToMojoPendingBuffer> pending_receive_;
-  mojo::SimpleWatcher writable_handle_watcher_;
+  std::unique_ptr<SocketDataPump> socket_data_pump_;
 
   net::NetworkTrafficAnnotationTag traffic_annotation_;
 
