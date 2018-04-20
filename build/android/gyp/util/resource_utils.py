9b6db24f73ad068e62d7bfa6cfd5c0ab8281133a
Andrew Grieve <agrieve@chromium.org>
2018-04-20 10:26:17 +0000

    generate_gradle.py: Fix crash due to import paths
    This broke yesterday in a round-about way by
    940a33d8c298201bb7afc92a6bff3644d006d29b [formerly da8025abca3ac835c8f59f4622372d4dec02fcae]
    
    We really need to implement a sane package system...
    
    TBR=jbudorick  # Minimize disruption
    NOTRY=true  # One flake
    
    Change-Id: I6e432cce422210906d363219511941d51efd73c4
    Reviewed-on: https://chromium-review.googlesource.com/1021182
    Reviewed-by: agrieve <agrieve@chromium.org>
    Commit-Queue: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552298}
    Former-commit-id: 8f8e7c766c1f8c481ea65ea74b4b5a96ad5aef6c

--- build/android/gyp/util/resource_utils.py
+++ build/android/gyp/util/resource_utils.py
@@ -14,8 +14,12 @@ from xml.etree import ElementTree
 
 import util.build_utils as build_utils
 
+# Why normpath():
+# When this module is imported with a ".." in the sys.path that points to it,
+# then __file__ will have a ".." entry in it and dirname() will treat it as a
+# path component.
 _SOURCE_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(
-    os.path.dirname(__file__)))))
+    os.path.dirname(os.path.normpath(__file__))))))
 # Import jinja2 from third_party/jinja2
 sys.path.insert(1, os.path.join(_SOURCE_ROOT, 'third_party'))
 from jinja2 import Template # pylint: disable=F0401
