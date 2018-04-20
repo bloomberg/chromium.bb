d10d335af1def037ab06b074b9c5473fc2d5fb8c
crystallambert@chromium.org <crystallambert@chromium.org>
2018-04-20 22:52:26 +0000

    [Extension Docs] Get Started Tutorial Code Issue
    Get started tutorial code snippet was causing an error.
    This update corrects this issue on tutorial page and completed file.
    
    Bug: 834614
    Change-Id: Ia3d61ef1510383b3b278ac957deeab047142fcd9
    Reviewed-on: https://chromium-review.googlesource.com/1022950
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Crystal Lambert <crystallambert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552505}
    Former-commit-id: 59445b2be75f9c8e0dc72f2f7196e7c492998d35

--- chrome/common/extensions/docs/examples/tutorials/get_started_complete/popup.js
+++ chrome/common/extensions/docs/examples/tutorials/get_started_complete/popup.js
@@ -13,7 +13,9 @@ chrome.storage.sync.get('color', function(data) {
 
 changeColor.onclick = function(element) {
   let color = element.target.value;
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
         tabs[0].id,
         {code: 'document.body.style.backgroundColor = "' + color + '";'});
+  });
 };
