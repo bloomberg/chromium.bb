f34e16d633496cfde8fd3b2f3a32d5c7708a1f20
Matthew Jones <mdjones@chromium.org>
2018-04-20 16:06:04 +0000

    Allow swipe-to-dismiss to be disabled in the bottom sheet
    This patch adds an interface method to BottomSheetContent that allows
    implementors to enable or disable swipe-to-dismiss. If disabled,
    flinging the sheet down will return the sheet to its peeking state.
    
    Bug: 834348
    Change-Id: Ib64ef5a956697f57da9bf584e9592adf0e33dcfd
    Reviewed-on: https://chromium-review.googlesource.com/1020373
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552347}
    Former-commit-id: 47aae2e24ffcb7b83f2f366b98c1f6dac79c6e12

--- chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
@@ -292,6 +292,11 @@ public class BottomSheet extends FrameLayout
          */
         @ContentPriority
         int getPriority();
+
+        /**
+         * @return Whether swiping the sheet down hard enough will cause the sheet to be dismissed.
+         */
+        boolean swipeToDismissEnabled();
     }
 
     /**
@@ -709,14 +714,14 @@ public class BottomSheet extends FrameLayout
 
     @Override
     public float getMinOffsetPx() {
-        return (flingToHideEnabled() ? getHiddenRatio() : getPeekRatio()) * mContainerHeight;
+        return (swipeToDismissEnabled() ? getHiddenRatio() : getPeekRatio()) * mContainerHeight;
     }
 
     /**
      * @return Whether flinging down hard enough will close the sheet.
      */
-    private boolean flingToHideEnabled() {
-        return true;
+    private boolean swipeToDismissEnabled() {
+        return mSheetContent != null ? mSheetContent.swipeToDismissEnabled() : true;
     }
 
     /**
@@ -724,7 +729,7 @@ public class BottomSheet extends FrameLayout
      *         close the sheet or peek it.
      */
     private @SheetState int getMinSwipableSheetState() {
-        return flingToHideEnabled() ? SHEET_STATE_HIDDEN : SHEET_STATE_PEEK;
+        return swipeToDismissEnabled() ? SHEET_STATE_HIDDEN : SHEET_STATE_PEEK;
     }
 
     @Override
