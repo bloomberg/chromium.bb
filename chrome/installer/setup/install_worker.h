925f6df4aad6222581a08d90502662cc71772fa8
Xi Cheng <chengx@chromium.org>
2018-04-20 18:49:34 +0000

    Dedup notification_helper OS registration code -- part 1
    This makes notification_helper_process_unittest.cc reuse the code in
    install_worker.*
    
    Bug: 734095
    Change-Id: I058971502e3681244e034de413acb891758045ba
    Reviewed-on: https://chromium-review.googlesource.com/1014328
    Commit-Queue: Xi Cheng <chengx@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552411}
    Former-commit-id: 8f9b8270a64dc81be573382d16f345d76cf53ee8

--- chrome/installer/setup/install_worker.h
+++ chrome/installer/setup/install_worker.h
@@ -85,6 +85,14 @@ void AddInstallWorkItems(const InstallationState& original_state,
                          const base::Version& new_version,
                          WorkItemList* install_list);
 
+// Adds work items to |list| to register a COM server with the OS, which is used
+// to handle the toast notification activation.
+void AddNativeNotificationInstallWorkItems(
+    HKEY root,
+    const base::FilePath& notification_helper,
+    const base::string16& toast_activator_reg_path,
+    WorkItemList* list);
+
 void AddSetMsiMarkerWorkItem(const InstallerState& installer_state,
                              BrowserDistribution* dist,
                              bool set,
