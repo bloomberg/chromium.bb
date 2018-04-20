78497d533a12f4360e0ded9a7f5cc6e802cd164d
Kurt Horimoto <kkhorimoto@chromium.org>
2018-04-20 18:13:40 +0000

    [iOS] Fix BrowserContainerViewController's |-displayContentView|.
    This function should be a no-op if called with the currently-displayed
    content view.
    
    Bug: 826093
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I5283f1723d4f2240c017e4561644d134d989869d
    Reviewed-on: https://chromium-review.googlesource.com/1015979
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552391}
    Former-commit-id: d565f8638922e78e289ad7090b5bf6d7c7869360

--- ios/chrome/browser/ui/browser_container_view_controller.mm
+++ ios/chrome/browser/ui/browser_container_view_controller.mm
@@ -32,21 +32,8 @@
 }
 
 - (void)displayContentView:(UIView*)contentView {
-  if (_contentView == contentView) {
-    // When voice search is launched from the NTP, the voice search button
-    // NamedGuide is constrained to a subview of |_contentView|.  If this
-    // occurs, removing |_contentView| from the hierarchy below will deactivate
-    // the constraints.  The check against the voice search button guide is a
-    // temporary workaround in order to minimize unintended side effects from
-    // this change; this function should be a no-op when |_contentView| ==
-    // |contentView|, regardless of the NamedGuide constraints.
-    // TODO(crbug.com/826093): Remove NamedGuide check and simply early return.
-    NamedGuide* voiceSearchGuide =
-        [NamedGuide guideWithName:kVoiceSearchButtonGuide view:self.view];
-    UIView* voiceSearchButton = voiceSearchGuide.constrainedView;
-    if ([voiceSearchButton isDescendantOfView:_contentView])
-      return;
-  }
+  if (_contentView == contentView)
+    return;
 
   DCHECK(![_contentView superview] || [_contentView superview] == self.view);
   [_contentView removeFromSuperview];
