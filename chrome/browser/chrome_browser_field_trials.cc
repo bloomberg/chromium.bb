51e4dfcdd4cff68c005d26c25acb1b067fe33c6b
Brian White <bcwhite@chromium.org>
2018-04-20 17:14:34 +0000

    No file-backed memory on Linux 4.4.0.
    There is a disproportional number of SIGBUS errors (92%) with Linux kernel 4.4.0.
    Stop using file-backed memory for persistent histograms on these systems.
    Using local memory will still provide proper persistence for Renderer processes.
    
    A typical OS version string would be something like "4.9.0-6-amd64".
    
    Bug: 753741
    Change-Id: I8dfa1d0a558c4b255fba14b3981f96203ac60684
    Reviewed-on: https://chromium-review.googlesource.com/1020100
    Commit-Queue: Brian White <bcwhite@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552364}
    Former-commit-id: 83d587981e7240c69ce6aaccfd8f42d48026051c

--- chrome/browser/chrome_browser_field_trials.cc
+++ chrome/browser/chrome_browser_field_trials.cc
@@ -17,6 +17,7 @@
 #include "base/metrics/persistent_histogram_allocator.h"
 #include "base/path_service.h"
 #include "base/strings/string_util.h"
+#include "base/sys_info.h"
 #include "base/task_scheduler/post_task.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
@@ -113,7 +114,22 @@ void InstantiatePersistentHistograms() {
   std::string storage = variations::GetVariationParamValueByFeature(
       base::kPersistentHistogramsFeature, "storage");
 
-  if (storage.empty() || storage == "MappedFile") {
+  static const char kMappedFile[] = "MappedFile";
+  static const char kLocalMemory[] = "LocalMemory";
+
+#if defined(OS_LINUX) && !defined(OS_ANDROID) && !defined(OS_CHROMEOS)
+  // Linux kernel 4.4.0.* shows a huge number of SIGBUS crashes with persistent
+  // histograms enabled using a mapped file.  Change this to use local memory.
+  // https://bugs.chromium.org/p/chromium/issues/detail?id=753741
+  if (storage.empty() || storage == kMappedFile) {
+    int major, minor, bugfix;
+    base::SysInfo::OperatingSystemVersionNumbers(&major, &minor, &bugfix);
+    if (major == 4 && minor == 4 && bugfix == 0)
+      storage = kLocalMemory;
+  }
+#endif
+
+  if (storage.empty() || storage == kMappedFile) {
     if (!base::PathExists(upload_dir)) {
       // Handle failure to create the directory.
       result = NO_UPLOAD_DIR;
@@ -143,7 +159,7 @@ void InstantiatePersistentHistograms() {
                            &base::GlobalHistogramAllocator::CreateSpareFile),
                        std::move(spare_file), kAllocSize),
         base::TimeDelta::FromSeconds(kSpareFileCreateDelaySeconds));
-  } else if (storage == "LocalMemory") {
+  } else if (storage == kLocalMemory) {
     // Use local memory for storage even though it will not persist across
     // an unclean shutdown. This sets the result but the actual creation is
     // done below.
