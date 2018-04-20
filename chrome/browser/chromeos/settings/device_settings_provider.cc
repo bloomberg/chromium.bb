bc4be81e5e191db279a1b4312bed83fbd1111cf2
Tim Zheng <timzheng@google.com>
2018-04-20 17:53:09 +0000

    Flip policy VirtualMachinesAllowed default value.
    This change flip the default value of device policy for managed devices
    only. When the device policy VirtualMachinesAllowed is not set for
    managed devices, the default is now False.
    
    Please note that this change doesn't modify the policy value for
    unmanaged devices. It's always True.
    
    BUG=chromium:800120
    TEST=Manual test with YAPS tool.
    
    Change-Id: Iddcfc4ad818db5be076b05c23d0f51f960fef059
    Reviewed-on: https://chromium-review.googlesource.com/1017308
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: Tim Zheng <timzheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552381}
    Former-commit-id: f468cd062d5e0a170ee9d6cfc0c346fdd993b104

--- chrome/browser/chromeos/settings/device_settings_provider.cc
+++ chrome/browser/chromeos/settings/device_settings_provider.cc
@@ -647,13 +647,17 @@ void DecodeGenericPolicies(
     }
   }
 
-  if (policy.has_virtual_machines_allowed()) {
-    const em::VirtualMachinesAllowedProto& container(
-        policy.virtual_machines_allowed());
-    if (container.has_virtual_machines_allowed()) {
-      new_values_cache->SetValue(
-          kVirtualMachinesAllowed,
-          std::make_unique<base::Value>(container.virtual_machines_allowed()));
+  if (policy.virtual_machines_allowed().has_virtual_machines_allowed()) {
+    new_values_cache->SetBoolean(
+        kVirtualMachinesAllowed,
+        policy.virtual_machines_allowed().virtual_machines_allowed());
+  } else {
+    // If the policy is missing, default to false on enterprise-enrolled
+    // devices.
+    policy::BrowserPolicyConnectorChromeOS* connector =
+        g_browser_process->platform_part()->browser_policy_connector_chromeos();
+    if (connector->IsEnterpriseManaged()) {
+      new_values_cache->SetBoolean(kVirtualMachinesAllowed, false);
     }
   }
 }
