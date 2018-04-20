3363d3fe77be31ca8dcce53e53ad8410827ca54e
Reid Kleckner <rnk@google.com>
2018-04-20 08:15:09 +0000

    Fix target_arch kward in asan_symbolize.py
    This bug was introduced in:
    https://chromium-review.googlesource.com/c/chromium/src/+/1004635
    
    I haven't filed a bug for it.
    
    R=digit@chromium.org,pasko@chromium.org
    
    Bug: None
    Change-Id: I9348f68876d11764ddc8bc2b00b8b8b337c9be1a
    Reviewed-on: https://chromium-review.googlesource.com/1020180
    Commit-Queue: agrieve <agrieve@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552281}
    Former-commit-id: 662aee164245d182a59d6d3257c0435146399e8d

--- build/android/asan_symbolize.py
+++ build/android/asan_symbolize.py
@@ -91,7 +91,7 @@ def _PrintSymbolized(asan_input, arch):
     info_dict = symbol.SymbolInformationForSet(libname,
                                                lib_relative_addrs,
                                                True,
-                                               target_arch=arch)
+                                               cpu_arch=arch)
     if info_dict:
       all_symbols[library] = info_dict
 
