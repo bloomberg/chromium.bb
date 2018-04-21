e754e762bb7301ff38587016ce26f707b5853e7e
Chris Nardi <cnardi@chromium.org>
2018-04-21 05:09:29 +0000

    Remove GetAlignmentShorthandValue() and BorderSpacingValue()
    These functions were functionally equivalent to Get2Values(), but were
    only used for their respective shorthands. Remove both in favor of
    Get2Values().
    
    Change-Id: Icae9c96ee97bb1d45cf6bd7d7bc00879597f79d0
    Reviewed-on: https://chromium-review.googlesource.com/1023127
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Commit-Queue: Chris Nardi <cnardi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552576}
    Former-commit-id: bfd74098faba28a120b25459cc2f2553e7ebeb98

--- third_party/blink/renderer/core/css/style_property_serializer.h
+++ third_party/blink/renderer/core/css/style_property_serializer.h
@@ -44,13 +44,11 @@ class StylePropertySerializer {
 
  private:
   String GetCommonValue(const StylePropertyShorthand&) const;
-  String GetAlignmentShorthandValue(const StylePropertyShorthand&) const;
   String BorderPropertyValue() const;
   String BorderImagePropertyValue() const;
   String GetLayeredShorthandValue(const StylePropertyShorthand&) const;
   String Get2Values(const StylePropertyShorthand&) const;
   String Get4Values(const StylePropertyShorthand&) const;
-  String BorderSpacingValue(const StylePropertyShorthand&) const;
   String PageBreakPropertyValue(const StylePropertyShorthand&) const;
   String GetShorthandValue(const StylePropertyShorthand&,
                            String separator = " ") const;
