1e5a9e7dec4313b5b001816212fc6c61c85ac46b
Noel Gordon <noel@chromium.org>
2018-04-22 09:59:28 +0000

    [ChromeOS] Re-enable AudioPlayer FileManagerBrowserTests
    Cause of the flakinessin the FileManagerBrowserTest suite was resolved
    on issue 831074 issue 804413 issue 829310 issue 618198.
    
    Re-enable the AudioPlayer tests in this browser test suite on all bots
    RELEASE/DEBUG/MSAN/ASAN on ChromeOS.
    
    OpenAudioOnDrive, OpenAudioOnDownloads were disabled on MSAN (in issue
    508949). Re-enabling here to see if MSAN is still a problem.
    
    Mash doesn't support these tests (see mash browser tests patch set #1)
    so disable these AudioPlayer FileManagerBrowserTests in mash.
    
    Tbr: yamaguchi@chromium.org
    Tbr: maxmorin@chromium.org
    Tbr: kbr@chromium.org
    Bug: 835626,508949,787806,830493
    Change-Id: I966bc9123b15fec453fe85fec09059af091c916f
    Reviewed-on: https://chromium-review.googlesource.com/1023072
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552603}
    Former-commit-id: b44ed3c60b2d1558f6149dfc962e73aff3ee0afc

--- chrome/browser/chromeos/file_manager/audio_player_browsertest.cc
+++ chrome/browser/chromeos/file_manager/audio_player_browsertest.cc
@@ -6,19 +6,20 @@
 
 namespace file_manager {
 
-template <GuestMode M>
+template <GuestMode MODE>
 class AudioPlayerBrowserTestBase : public FileManagerBrowserTestBase {
  public:
-  GuestMode GetGuestModeParam() const override { return M; }
+  GuestMode GetGuestModeParam() const override { return MODE; }
+
   const char* GetTestCaseNameParam() const override {
     return test_case_name_.c_str();
   }
 
- protected:
   const char* GetTestManifestName() const override {
     return "audio_player_test_manifest.json";
   }
 
+ protected:
   void set_test_case_name(const std::string& name) { test_case_name_ = name; }
 
  private:
@@ -26,72 +27,34 @@ class AudioPlayerBrowserTestBase : public FileManagerBrowserTestBase {
 };
 
 typedef AudioPlayerBrowserTestBase<NOT_IN_GUEST_MODE> AudioPlayerBrowserTest;
-typedef AudioPlayerBrowserTestBase<IN_GUEST_MODE>
-    AudioPlayerBrowserTestInGuestMode;
+typedef AudioPlayerBrowserTestBase<IN_GUEST_MODE> AudioPlayerBrowserTestGuest;
 
-// http://crbug.com/508949
-#if defined(MEMORY_SANITIZER)
-#define MAYBE_OpenAudioOnDownloads DISABLED_OpenAudioOnDownloads
-#else
-// TODO(yamaguchi): Enable after removing root cause of the test flakiness.
-// http://crbug.com/804413.
-#define MAYBE_OpenAudioOnDownloads DISABLED_OpenAudioOnDownloads
-#endif
-IN_PROC_BROWSER_TEST_F(AudioPlayerBrowserTest, MAYBE_OpenAudioOnDownloads) {
+IN_PROC_BROWSER_TEST_F(AudioPlayerBrowserTest, OpenAudioOnDownloads) {
   set_test_case_name("openAudioOnDownloads");
   StartTest();
 }
 
-IN_PROC_BROWSER_TEST_F(AudioPlayerBrowserTestInGuestMode,
-                       OpenAudioOnDownloads) {
+IN_PROC_BROWSER_TEST_F(AudioPlayerBrowserTestGuest, OpenAudioOnDownloads) {
   set_test_case_name("openAudioOnDownloads");
   StartTest();
 }
 
-// http://crbug.com/508949
-#if defined(MEMORY_SANITIZER)
-#define MAYBE_OpenAudioOnDrive DISABLED_OpenAudioOnDrive
-#else
-// TODO(yamaguchi): Enable after removing root cause of the test flakiness.
-// http://crbug.com/804413.
-#define MAYBE_OpenAudioOnDrive DISABLED_OpenAudioOnDrive
-#endif
-IN_PROC_BROWSER_TEST_F(AudioPlayerBrowserTest, MAYBE_OpenAudioOnDrive) {
+IN_PROC_BROWSER_TEST_F(AudioPlayerBrowserTest, OpenAudioOnDrive) {
   set_test_case_name("openAudioOnDrive");
   StartTest();
 }
 
-#if defined(MEMORY_SANITIZER)
-#define MAYBE_TogglePlayState DISABLED_TogglePlayState
-#else
-// TODO(yamaguchi): Enable after removing root cause of the test flakiness.
-// http://crbug.com/804413.
-#define MAYBE_TogglePlayState DISABLED_TogglePlayState
-#endif
-IN_PROC_BROWSER_TEST_F(AudioPlayerBrowserTest, MAYBE_TogglePlayState) {
+IN_PROC_BROWSER_TEST_F(AudioPlayerBrowserTest, TogglePlayState) {
   set_test_case_name("togglePlayState");
   StartTest();
 }
 
-#if defined(MEMORY_SANITIZER)
-#define MAYBE_ChangeVolumeLevel DISABLED_ChangeVolumeLevel
-#else
-// TODO(yamaguchi): Enable after removing root cause of the test flakiness.
-// http://crbug.com/804413.
-#define MAYBE_ChangeVolumeLevel DISABLED_ChangeVolumeLevel
-#endif
-IN_PROC_BROWSER_TEST_F(AudioPlayerBrowserTest, MAYBE_ChangeVolumeLevel) {
+IN_PROC_BROWSER_TEST_F(AudioPlayerBrowserTest, ChangeVolumeLevel) {
   set_test_case_name("changeVolumeLevel");
   StartTest();
 }
 
-#if defined(MEMORY_SANITIZER)
-#define MAYBE_ChangeTracks DISABLED_ChangeTracks
-#else
-// Also disable because of flakyness. http://crbug.com/618198
-#define MAYBE_ChangeTracks DISABLED_ChangeTracks
-#endif
-IN_PROC_BROWSER_TEST_F(AudioPlayerBrowserTest, MAYBE_ChangeTracks) {
+IN_PROC_BROWSER_TEST_F(AudioPlayerBrowserTest, ChangeTracks) {
   set_test_case_name("changeTracks");
   StartTest();
 }
