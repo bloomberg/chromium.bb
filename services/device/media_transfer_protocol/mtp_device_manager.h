f6443ba844903d379e606d1ef781d65349765ee5
Donna Wu <donna.wu@intel.com>
2018-04-23 08:28:32 +0000

    Add remaining mojo methods to device::mojom::MtpManager.
    This CL makes device::mojom::MtpManager interface complete with
    remaining methods. Clients will be migrated to use MTP mojo
    interfaces in the subsequent CL.
    
    BUG=769630
    
    Change-Id: Ic0c443873f7645c4d221fb8892a322c2d2162bfe
    Reviewed-on: https://chromium-review.googlesource.com/1002572
    Commit-Queue: Donna Wu <donna.wu@intel.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552652}
    Former-commit-id: 5d01e7d6448d8bc11f270facabf274b25227f7ed

--- services/device/media_transfer_protocol/mtp_device_manager.h
+++ services/device/media_transfer_protocol/mtp_device_manager.h
@@ -32,6 +32,44 @@ class MtpDeviceManager : public mojom::MtpManager {
   void EnumerateStoragesAndSetClient(
       mojom::MtpManagerClientAssociatedPtrInfo client,
       EnumerateStoragesAndSetClientCallback callback) override;
+  void GetStorageInfo(const std::string& storage_name,
+                      GetStorageInfoCallback callback) override;
+  void GetStorageInfoFromDevice(
+      const std::string& storage_name,
+      GetStorageInfoFromDeviceCallback callback) override;
+  void OpenStorage(const std::string& storage_name,
+                   const std::string& mode,
+                   OpenStorageCallback callback) override;
+  void CloseStorage(const std::string& storage_handle,
+                    CloseStorageCallback callback) override;
+  void CreateDirectory(const std::string& storage_handle,
+                       uint32_t parent_id,
+                       const std::string& directory_name,
+                       CreateDirectoryCallback callback) override;
+  void ReadDirectory(const std::string& storage_handle,
+                     uint32_t file_id,
+                     uint64_t max_size,
+                     ReadDirectoryCallback callback) override;
+  void ReadFileChunk(const std::string& storage_handle,
+                     uint32_t file_id,
+                     uint32_t offset,
+                     uint32_t count,
+                     ReadFileChunkCallback callback) override;
+  void GetFileInfo(const std::string& storage_handle,
+                   uint32_t file_id,
+                   GetFileInfoCallback callback) override;
+  void RenameObject(const std::string& storage_handle,
+                    uint32_t object_id,
+                    const std::string& new_name,
+                    RenameObjectCallback callback) override;
+  void CopyFileFromLocal(const std::string& storage_handle,
+                         int64_t source_file_descriptor,
+                         uint32_t parent_id,
+                         const std::string& file_name,
+                         CopyFileFromLocalCallback callback) override;
+  void DeleteObject(const std::string& storage_handle,
+                    uint32_t object_id,
+                    DeleteObjectCallback callback) override;
 
  private:
   std::unique_ptr<MediaTransferProtocolManager::Observer> observer_;
