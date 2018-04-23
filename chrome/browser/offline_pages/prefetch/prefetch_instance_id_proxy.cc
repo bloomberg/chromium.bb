bff17386a2b6d088aa21281c1d0aa6eaf9e325eb
Tatiana Gornak <melandory@chromium.org>
2018-04-23 09:25:59 +0000

    Componetize InstanceIdProfileService to be used in Invalidation implemetation.
    Bug: 801985
    Change-Id: I1783db374cf4bcdeda6e5bb6a8a85ce9a8fbc03e
    
    TBR=carlosk@chromium.org, rdevlin.cronin@chromium.org,jkrcal@chromium.org
    
    Change-Id: I1783db374cf4bcdeda6e5bb6a8a85ce9a8fbc03e
    Reviewed-on: https://chromium-review.googlesource.com/1021692
    Commit-Queue: Tatiana Gornak <melandory@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552657}
    Former-commit-id: f4fdea55b315ed5a83991c3debecd9738747b92d

--- chrome/browser/offline_pages/prefetch/prefetch_instance_id_proxy.cc
+++ chrome/browser/offline_pages/prefetch/prefetch_instance_id_proxy.cc
@@ -9,10 +9,10 @@
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/threading/thread_task_runner_handle.h"
-#include "chrome/browser/gcm/instance_id/instance_id_profile_service.h"
 #include "chrome/browser/gcm/instance_id/instance_id_profile_service_factory.h"
 #include "components/gcm_driver/instance_id/instance_id.h"
 #include "components/gcm_driver/instance_id/instance_id_driver.h"
+#include "components/gcm_driver/instance_id/instance_id_profile_service.h"
 #include "components/offline_pages/core/offline_page_feature.h"
 
 using instance_id::InstanceID;
