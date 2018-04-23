928e8df78dec14ebb361362b2448686ecd622bfb
Xi Cheng <chengx@chromium.org>
2018-04-23 18:34:25 +0000

    Move notification_helper/ to chrome/notification_helper/
    ... plus all the necessary code migration. A follow-up CL will be used
    to update relevant comments.
    
    Bug: 734095
    Change-Id: I8516c750518e10a366bdc1496e75a3c2f4f0898b
    Reviewed-on: https://chromium-review.googlesource.com/1022957
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Xi Cheng <chengx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552762}
    Former-commit-id: 850001f395fe619b3add0fdb80a1cfbd7d32df01

--- chrome/notification_helper/notification_helper_util.cc
+++ chrome/notification_helper/notification_helper_util.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "notification_helper/notification_helper_util.h"
+#include "chrome/notification_helper/notification_helper_util.h"
 
 #include "base/base_paths.h"
 #include "base/files/file_util.h"
