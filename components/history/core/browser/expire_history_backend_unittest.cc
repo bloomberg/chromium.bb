c7d09b7b542059399330a6b359bacdade6a67571
Gabriel Charette <gab@chromium.org>
2018-04-23 22:47:40 +0000

    Migrate MessageLoop::current() to MessageLoopCurrent in /components/history
    These changes were scripted as part of the effort to restrict exposure of the
    raw MessageLoop*.
    
    Note that many static methods exposed on MessageLoopCurrent are also deprecated
    so it is very well possible that this is migrating to an already deprecated
    method.
    
    The goal of this pass is to reduce usage of MessageLoop::current() (and
    ultimately make it impossible to obtain the MessageLoop* statically).
    
    As such I will not edit this CL unless the script did something logically wrong.
    I defer to owners to fix highlighted usage of already deprecated APIs.
    
    Possible script screw ups / things to look out for in this review:
     - Storing MessageLoopCurrent in a MessageLoop* variable or comparing it against
       one (I will go over that in a separate pass).
    
    Includes should have been stripped if that was the last usage of message_loop.h
    in that file.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    R=sky@chromium.org
    
    Bug: 825327
    Change-Id: If9302ab0bd2df5e0bb6926ff2c0a784e45442bb0
    Reviewed-on: https://chromium-review.googlesource.com/1024809
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552870}
    Former-commit-id: 0137b7736ed05df21a201d20eb14d6e93460e83f

--- components/history/core/browser/expire_history_backend_unittest.cc
+++ components/history/core/browser/expire_history_backend_unittest.cc
@@ -16,7 +16,7 @@
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/macros.h"
-#include "base/message_loop/message_loop.h"
+#include "base/message_loop/message_loop_current.h"
 #include "base/run_loop.h"
 #include "base/scoped_observer.h"
 #include "base/strings/string16.h"
@@ -191,7 +191,7 @@ class ExpireHistoryTest : public testing::Test, public HistoryBackendNotifier {
     top_sites_->ShutdownOnUIThread();
     top_sites_ = nullptr;
 
-    if (base::MessageLoop::current())
+    if (base::MessageLoopCurrent::Get())
       base::RunLoop().RunUntilIdle();
 
     pref_service_.reset();
