48b608cff2331acd8f5466b301689f3e70235f9f
Gabriel Charette <gab@chromium.org>
2018-04-24 15:59:10 +0000

    Migrate MessageLoop::current() to MessageLoopCurrent in /chrome/browser/ssl
    These changes were scripted as part of the effort to restrict exposure of the
    raw MessageLoop*.
    
    Note that many static methods exposed on MessageLoopCurrent are also deprecated
    so it is very well possible that this is migrating to an already deprecated
    method.
    
    The goal of this pass is to reduce usage of MessageLoop::current() (and
    ultimately make it impossible to obtain the MessageLoop* statically).
    
    As such I will not edit this CL unless the script did something logically wrong.
    I defer to owners to fix highlighted usage of already deprecated APIs.
    
    Possible script screw ups / things to look out for in this review:
     - Storing MessageLoopCurrent in a MessageLoop* variable or comparing it against
       one (I will go over that in a separate pass).
    
    Includes should have been stripped if that was the last usage of message_loop.h
    in that file.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    R=rsleevi@chromium.org
    
    Bug: 825327
    Change-Id: I145bea58989dc37d67a2d37ce4b14581363ac647
    Reviewed-on: https://chromium-review.googlesource.com/1024474
    Commit-Queue: Ryan Sleevi <rsleevi@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553142}
    Former-commit-id: 989f91f7f3a497679d059ac1af795886e001565b

--- chrome/browser/ssl/ssl_browsertest.cc
+++ chrome/browser/ssl/ssl_browsertest.cc
@@ -13,7 +13,7 @@
 #include "base/command_line.h"
 #include "base/location.h"
 #include "base/macros.h"
-#include "base/message_loop/message_loop.h"
+#include "base/message_loop/message_loop_current.h"
 #include "base/metrics/field_trial.h"
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
@@ -918,7 +918,7 @@ class SSLUITestBase : public InProcessBrowserTest {
 
   void UpdateChromePolicy(const policy::PolicyMap& policies) {
     policy_provider_.UpdateChromePolicy(policies);
-    ASSERT_TRUE(base::MessageLoop::current());
+    ASSERT_TRUE(base::MessageLoopCurrent::Get());
 
     base::RunLoop loop;
     loop.RunUntilIdle();
