38265f613cb82777905cacbade5f68c93d3cf338
Daniel Cheng <dcheng@chromium.org>
2018-04-24 23:59:25 +0000

    Add parens around url/origin argument to DEBUG_ALIAS macros.
    This allows more complex expressions to be used without confusing the
    compiler.
    
    TBR=mkwst@chromium.org
    
    Change-Id: I0b6d3a9526ac932bf1a8e3f7e9b3a76e3da34927
    Reviewed-on: https://chromium-review.googlesource.com/1026805
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553378}
    Former-commit-id: afbf6358993732eaa023254bb45ff7bb71a2c230

--- url/gurl.h
+++ url/gurl.h
@@ -485,6 +485,6 @@ URL_EXPORT bool operator!=(const base::StringPiece& spec, const GURL& x);
 // variable named |<var_name>|.  This helps ensure that the value of |url| gets
 // preserved in crash dumps.
 #define DEBUG_ALIAS_FOR_GURL(var_name, url) \
-  DEBUG_ALIAS_FOR_CSTR(var_name, url.possibly_invalid_spec().c_str(), 128)
+  DEBUG_ALIAS_FOR_CSTR(var_name, (url).possibly_invalid_spec().c_str(), 128)
 
 #endif  // URL_GURL_H_
