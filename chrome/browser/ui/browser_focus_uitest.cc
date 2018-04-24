1c7006339dbf5c20d929d669a054f45cdb5ec24c
Xida Chen <xidachen@chromium.org>
2018-04-24 15:01:16 +0000

    Disable BrowserFocusTest.FocusOnNavigate on Win
    TBR=clamy@chromium.org
    NOTRY=true
    
    Bug: 665296
    Change-Id: I538e83666a06e8e385c79f339f9089f4ee293242
    Reviewed-on: https://chromium-review.googlesource.com/1025964
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553107}
    Former-commit-id: 1ca3014d6b21fbe83025eabfb9f0880f061db67c

--- chrome/browser/ui/browser_focus_uitest.cc
+++ chrome/browser/ui/browser_focus_uitest.cc
@@ -684,8 +684,8 @@ IN_PROC_BROWSER_TEST_F(BrowserFocusTest, NavigateFromOmniboxIntoNewTab) {
   EXPECT_FALSE(IsViewFocused(VIEW_ID_OMNIBOX));
 }
 
-// Flaky on Mac and ChromeOS (http://crbug.com/665296).
-#if defined(OS_MACOSX) || defined(OS_CHROMEOS)
+// Flaky on Mac, ChromeOS and Win (http://crbug.com/665296).
+#if defined(OS_MACOSX) || defined(OS_CHROMEOS) || defined(OS_WIN)
 #define MAYBE_FocusOnNavigate DISABLED_FocusOnNavigate
 #else
 #define MAYBE_FocusOnNavigate FocusOnNavigate
