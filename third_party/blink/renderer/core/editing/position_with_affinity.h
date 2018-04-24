541ad517e231b3845dbb571b6177acfb56748197
Xiaocheng Hu <xiaochengh@chromium.org>
2018-04-24 02:16:22 +0000

    Fix copyright header of position_with_affinity.h
    This patch removes a leftover "/*" from the copyright header of the file.
    
    Notry: True
    Change-Id: I2d44da15a07e9003756647dfc631a8e813259380
    Reviewed-on: https://chromium-review.googlesource.com/1025180
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#552959}
    Former-commit-id: ebbd443e2255751cc3179f995f362a90a3d32ece

--- third_party/blink/renderer/core/editing/position_with_affinity.h
+++ third_party/blink/renderer/core/editing/position_with_affinity.h
@@ -1,6 +1,6 @@
 // Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file./*
+// found in the LICENSE file.
 
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_EDITING_POSITION_WITH_AFFINITY_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_EDITING_POSITION_WITH_AFFINITY_H_
