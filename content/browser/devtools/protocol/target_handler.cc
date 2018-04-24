7dcdba17519748a001870332f72ee29fe451c803
Andrey Lushnikov <lushnikov@chromium.org>
2018-04-24 20:55:32 +0000

    DevTools(Protocol): expose BrowserContextId in TargetInfo
    This patch exposes BrowserContextId as a part of TargetInfo.
    This helps to attribute pages to the browser contexts they
    belong to.
    
    BUG=836272
    R=dgozman
    
    Change-Id: I93d54756c98124a077bd979c207c0c7cd7e38971
    Reviewed-on: https://chromium-review.googlesource.com/1026391
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553268}
    Former-commit-id: 092e601673b89bf0db1b6ea56dfb33b6e52a4fa6

--- content/browser/devtools/protocol/target_handler.cc
+++ content/browser/devtools/protocol/target_handler.cc
@@ -10,6 +10,7 @@
 #include "content/browser/devtools/devtools_manager.h"
 #include "content/browser/devtools/devtools_session.h"
 #include "content/browser/frame_host/navigation_handle_impl.h"
+#include "content/public/browser/browser_context.h"
 #include "content/public/browser/devtools_agent_host_client.h"
 #include "content/public/browser/navigation_throttle.h"
 
@@ -32,6 +33,8 @@ std::unique_ptr<Target::TargetInfo> CreateInfo(DevToolsAgentHost* host) {
           .Build();
   if (!host->GetOpenerId().empty())
     target_info->SetOpenerId(host->GetOpenerId());
+  if (host->GetBrowserContext())
+    target_info->SetBrowserContextId(host->GetBrowserContext()->UniqueId());
   return target_info;
 }
 
