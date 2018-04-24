14718e01d4078223b0e90d84180e2ede4ffc50ab
Stephen McGruer <smcgruer@chromium.org>
2018-04-24 19:15:48 +0000

    Fix DCHECK when setting transform-box style to 'border-box'.
    This is an invalid value for the property, but had been incorrectly left
    in the CSS parser fast-path code as allowed.
    
    Bug: 816534
    Change-Id: Ic787508d21b3f385a37e5c4d50989c69d8dd48ac
    Reviewed-on: https://chromium-review.googlesource.com/1024775
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553224}
    Former-commit-id: 010c0cc318b4d718edcb7c7bb4d4af2b81ebac8b

--- third_party/blink/renderer/core/css/parser/css_parser_fast_paths.cc
+++ third_party/blink/renderer/core/css/parser/css_parser_fast_paths.cc
@@ -854,8 +854,7 @@ bool CSSParserFastPaths::IsValidKeywordPropertyAndValue(
       return value_id == CSSValueDisc || value_id == CSSValueCircle ||
              value_id == CSSValueSquare || value_id == CSSValueNone;
     case CSSPropertyTransformBox:
-      return value_id == CSSValueBorderBox || value_id == CSSValueFillBox ||
-             value_id == CSSValueViewBox;
+      return value_id == CSSValueFillBox || value_id == CSSValueViewBox;
     case CSSPropertyTransformStyle:
       return value_id == CSSValueFlat || value_id == CSSValuePreserve3d;
     case CSSPropertyWebkitUserDrag:
