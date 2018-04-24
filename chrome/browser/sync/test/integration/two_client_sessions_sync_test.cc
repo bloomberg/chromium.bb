8c30938559778a895ed7ffa4ef363f4246f9570b
Xida Chen <xidachen@chromium.org>
2018-04-24 17:19:39 +0000

    Revert "Disable DeleteActiveSession on Win"
    This reverts commit 059df35f5bbaf8ad92b663ad9ea9c642843c1f9d [formerly 0b61ff47226c5f98ea22902c75914b27235789ea].
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > Disable DeleteActiveSession on Win
    > 
    > TBR=thestig@chromium.org
    > NOTRY=true
    > 
    > Bug: 836289
    > Change-Id: I669edb4d3ec8af8e144e5fac812ed0b1422e5e55
    > Reviewed-on: https://chromium-review.googlesource.com/1026155
    > Commit-Queue: Xida Chen <xidachen@chromium.org>
    > Reviewed-by: Xida Chen <xidachen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#553165}
    
    TBR=thestig@chromium.org,xidachen@chromium.org
    
    Change-Id: I6b1f989a749c7cbda554d5d8b4ccee735ded64e7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 836289
    Reviewed-on: https://chromium-review.googlesource.com/1026132
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553185}
    Former-commit-id: d1cba3793d8e9008fa903316203517e0f64297f3

--- chrome/browser/sync/test/integration/two_client_sessions_sync_test.cc
+++ chrome/browser/sync/test/integration/two_client_sessions_sync_test.cc
@@ -192,13 +192,7 @@ IN_PROC_BROWSER_TEST_P(TwoClientSessionsSyncTest, DeleteIdleSession) {
   EXPECT_FALSE(GetSessionData(1, &sessions1));
 }
 
-// Timeout on win (https://crbug.com/836289).
-#if deinfed(OS_WIN)
-#define MAYBE_DeleteActiveSession DISABLED_DeleteActiveSession
-#else
-#define MAYBE_DeleteActiveSession DeleteActiveSession
-#endif
-IN_PROC_BROWSER_TEST_P(TwoClientSessionsSyncTest, MAYBE_DeleteActiveSession) {
+IN_PROC_BROWSER_TEST_P(TwoClientSessionsSyncTest, DeleteActiveSession) {
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
 
   ASSERT_TRUE(CheckInitialState(0));
