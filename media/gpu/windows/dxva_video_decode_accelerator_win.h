a703a1bcdaafcf4a39521cabea06bf6fb58b181a
liberato@chromium.org <liberato@chromium.org>
2018-04-24 19:45:48 +0000

    Add UMA for DXVA PictureBufferMechanism.
    To help us figure out how to prioritize the various picture buffer
    types in the new D3D11VideoDecoder, this UMA records how often
    each is used in the DXVA VDA.
    
    Bug: 835972
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I39b41000b54b932ce8cc592893bef1826f458308
    Reviewed-on: https://chromium-review.googlesource.com/1024927
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553236}
    Former-commit-id: ddaa30b6f4e207985fbbb03a9c8fd738285475d1

--- media/gpu/windows/dxva_video_decode_accelerator_win.h
+++ media/gpu/windows/dxva_video_decode_accelerator_win.h
@@ -128,20 +128,27 @@ class MEDIA_GPU_EXPORT DXVAVideoDecodeAccelerator
   typedef void* EGLSurface;
   typedef std::list<Microsoft::WRL::ComPtr<IMFSample>> PendingInputs;
 
+  // These are used for histograms, so don't change their numeric value (except
+  // for kMaxValue as described below).
   enum class PictureBufferMechanism {
     // Copy to either a BGRA8 or FP16 texture using the video processor.
-    COPY_TO_RGB,
+    COPY_TO_RGB = 0,
 
     // Copy to another NV12 texture that can be used in ANGLE.
-    COPY_TO_NV12,
+    COPY_TO_NV12 = 1,
 
     // Bind the resulting GLImage to the NV12 texture. If the texture's used
     // in a an overlay than use it directly, otherwise copy it to another NV12
     // texture when necessary.
-    DELAYED_COPY_TO_NV12,
+    DELAYED_COPY_TO_NV12 = 2,
 
     // Bind the NV12 decoder texture directly to the texture used in ANGLE.
-    BIND
+    BIND = 3,
+
+    // For UMA.  Must be the last entry.  It should be initialized to the
+    // numerically largest value above; if you add more entries, then please
+    // update this to be the last one.
+    kMaxValue = BIND
   };
 
   // Creates and initializes an instance of the D3D device and the
