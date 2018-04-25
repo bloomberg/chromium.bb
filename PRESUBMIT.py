81fad8d54e32b15d8d365b67d4def9589f4c304c
Gabriel Charette <gab@chromium.org>
2018-04-25 20:52:51 +0000

    [PRESUBMIT] Discourage but don't ban ScopedMockTimeMessageLoopTaskRunner
    A valid use case in a browser test was recently presented to me. And
    while I ultimately plan to support this in ScopedTaskEnvironment as
    well, it isn't for now.
    
    R=dpranke@chromium.org
    
    Bug: None
    Change-Id: Id4197c8a8cd567c5daaf4b1b39fe8f34fb8b3549
    Reviewed-on: https://chromium-review.googlesource.com/1027809
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553736}
    Former-commit-id: 87cc1af1e72bdb67590be2fbc82dcc334e2a40fd

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -431,9 +431,13 @@ _BANNED_CPP_FUNCTIONS = (
     (
       'base::ScopedMockTimeMessageLoopTaskRunner',
       (
-        'ScopedMockTimeMessageLoopTaskRunner is deprecated.',
+        'ScopedMockTimeMessageLoopTaskRunner is deprecated. Prefer',
+        'ScopedTaskEnvironment::MainThreadType::MOCK_TIME. There are still a',
+        'few cases that may require a ScopedMockTimeMessageLoopTaskRunner',
+        '(i.e. mocking the main MessageLoopForUI in browser_tests), but check',
+        'with gab@ first if you think you need it)',
       ),
-      True,
+      False,
       (),
     ),
     (
