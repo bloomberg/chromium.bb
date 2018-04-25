8345e379ae79267b0efc3a04799056a9f48a89eb
Gabriel Charette <gab@chromium.org>
2018-04-25 17:59:03 +0000

    Migrate MessageLoop::current() to MessageLoopCurrent in /components/spellcheck
    These changes were scripted as part of the effort to restrict exposure of the
    raw MessageLoop*.
    
    Note that many static methods exposed on MessageLoopCurrent are also deprecated
    so it is very well possible that this is migrating to an already deprecated
    method.
    
    The goal of this pass is to reduce usage of MessageLoop::current() (and
    ultimately make it impossible to obtain the MessageLoop* statically).
    
    As such I will not edit this CL unless the script did something logically wrong.
    I defer to owners to fix highlighted usage of already deprecated APIs.
    
    Possible script screw ups / things to look out for in this review:
     - Storing MessageLoopCurrent in a MessageLoop* variable or comparing it against
       one (I will go over that in a separate pass).
    
    Includes should have been stripped if that was the last usage of message_loop.h
    in that file.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    TBR=groby@chromium.org
    
    Bug: 825327
    Change-Id: I7b103879e33e4c71534ca16b1270949ceb70ce9b
    Reviewed-on: https://chromium-review.googlesource.com/1024657
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553647}
    Former-commit-id: 21ff80483b000614faf98d72f4bf47dd89f3337b

--- components/spellcheck/renderer/spellcheck_provider_test.cc
+++ components/spellcheck/renderer/spellcheck_provider_test.cc
@@ -7,6 +7,7 @@
 #include <memory>
 
 #include "base/message_loop/message_loop.h"
+#include "base/message_loop/message_loop_current.h"
 #include "base/run_loop.h"
 #include "components/spellcheck/common/spellcheck.mojom.h"
 #include "components/spellcheck/common/spellcheck_result.h"
@@ -49,7 +50,7 @@ TestingSpellCheckProvider::~TestingSpellCheckProvider() {
 void TestingSpellCheckProvider::RequestTextChecking(
     const base::string16& text,
     blink::WebTextCheckingCompletion* completion) {
-  if (!loop_ && !base::MessageLoop::current())
+  if (!loop_ && !base::MessageLoopCurrent::Get())
     loop_ = std::make_unique<base::MessageLoop>();
   if (!binding_.is_bound()) {
     spellcheck::mojom::SpellCheckHostPtr host_proxy;
