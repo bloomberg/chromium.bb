e3bd9789c9d109e4169ce1866cb0f4a24be36bb7
Tarun Bansal <tbansal@chromium.org>
2018-04-26 22:18:32 +0000

    Cap the cellular signal strength level to be between 0 and 4 (both inclusive).
    Bug: 835701
    Change-Id: I93f2aeed4bd615e5380f6d877479df0a46367f3d
    Reviewed-on: https://chromium-review.googlesource.com/1031172
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554167}
    Former-commit-id: 7ceb20aa2d291781ab6b0c9a2b0c3a2f44cf0581

--- net/android/cellular_signal_strength.h
+++ net/android/cellular_signal_strength.h
@@ -19,7 +19,9 @@ namespace cellular_signal_strength {
 
 // Returns the signal strength level (between 0 and 4, both inclusive) of the
 // currently registered cellular connection. If the value is unavailable, an
-// empty value is returned.
+// empty value is returned. If the signal strength value returned by platform
+// API is less than 0, this method returns 0. If the platform API returns a
+// value larger than 4, then this method returns 4.
 NET_EXPORT_PRIVATE base::Optional<int32_t> GetSignalStrengthLevel();
 
 }  // namespace cellular_signal_strength
