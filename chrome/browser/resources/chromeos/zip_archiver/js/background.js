c98ab88e1fc72655d3f7237e8bcbbb65f6995255
Tatsuhisa Yamaguchi <yamaguchi@google.com>
2018-04-26 08:14:11 +0000

    Zip Archiver: Suppress in incognito context of regular session.
    Currently Zip Archiver run in the normal and incognito contexts in
    parallel when opening a single ZIP file in the Files app.
    The instance in the incognito context also received and processed events
    in onGetMetadataRequested, but it always fails because it doesn't run
    the normal initialization procedure.
    (i.e. loading volume and filling unpacker.app.volumeLoadedPromises)
    
    It once had worked harmlessly because error was ignored, but now
    the error in the incognito context triggers unmounting of the file.
    It can cancel out the volume mounted by the other instance in the
    normal context right before it.
    
    Bug: 833603
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I712a974ca4fe287d795256fc22a62d5c85d66401
    Reviewed-on: https://chromium-review.googlesource.com/1018624
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Reviewed-by: Yuki Awano <yawano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553950}
    Former-commit-id: 430b4497a96dfc6e706f9387a559ed0be1d35967

--- chrome/browser/resources/chromeos/zip_archiver/js/background.js
+++ chrome/browser/resources/chromeos/zip_archiver/js/background.js
@@ -4,33 +4,48 @@
 
 'use strict';
 
-// Event called on opening a file with the extension or mime type
-// declared in the manifest file.
-chrome.app.runtime.onLaunched.addListener(unpacker.app.onLaunched);
+function setupZipArchiver() {
+  // Event called on opening a file with the extension or mime type
+  // declared in the manifest file.
+  chrome.app.runtime.onLaunched.addListener(unpacker.app.onLaunched);
 
-// Save the state before suspending the event page, so we can resume it
-// once new events arrive.
-chrome.runtime.onSuspend.addListener(unpacker.app.onSuspend);
+  // Save the state before suspending the event page, so we can resume it
+  // once new events arrive.
+  chrome.runtime.onSuspend.addListener(unpacker.app.onSuspend);
 
-chrome.fileSystemProvider.onUnmountRequested.addListener(
-    unpacker.app.onUnmountRequested);
-chrome.fileSystemProvider.onGetMetadataRequested.addListener(
-    unpacker.app.onGetMetadataRequested);
-chrome.fileSystemProvider.onReadDirectoryRequested.addListener(
-    unpacker.app.onReadDirectoryRequested);
-chrome.fileSystemProvider.onOpenFileRequested.addListener(
-    unpacker.app.onOpenFileRequested);
-chrome.fileSystemProvider.onCloseFileRequested.addListener(
-    unpacker.app.onCloseFileRequested);
-chrome.fileSystemProvider.onReadFileRequested.addListener(
-    unpacker.app.onReadFileRequested);
+  chrome.fileSystemProvider.onUnmountRequested.addListener(
+      unpacker.app.onUnmountRequested);
+  chrome.fileSystemProvider.onGetMetadataRequested.addListener(
+      unpacker.app.onGetMetadataRequested);
+  chrome.fileSystemProvider.onReadDirectoryRequested.addListener(
+      unpacker.app.onReadDirectoryRequested);
+  chrome.fileSystemProvider.onOpenFileRequested.addListener(
+      unpacker.app.onOpenFileRequested);
+  chrome.fileSystemProvider.onCloseFileRequested.addListener(
+      unpacker.app.onCloseFileRequested);
+  chrome.fileSystemProvider.onReadFileRequested.addListener(
+      unpacker.app.onReadFileRequested);
 
-// Load the PNaCl module.
-unpacker.app.loadNaclModule('module.nmf', 'application/x-pnacl');
+  // Load the PNaCl module.
+  unpacker.app.loadNaclModule('module.nmf', 'application/x-pnacl');
 
-// Load translations
-unpacker.app.loadStringData();
+  // Load translations
+  unpacker.app.loadStringData();
 
-// Clean all temporary files inside the work directory, just in case the
-// extension aborted previously without removing ones.
-unpacker.app.cleanWorkDirectory();
+  // Clean all temporary files inside the work directory, just in case the
+  // extension aborted previously without removing ones.
+  unpacker.app.cleanWorkDirectory();
+}
+
+// Avoid handling events duplicatedly if this is in incognito context in a
+// regular session. https://crbug.com/833603
+chrome.fileManagerPrivate.getProfiles((profiles) => {
+  if ((profiles[0] && profiles[0].profileId == '$guest') ||
+      !chrome.extension.inIncognitoContext) {
+    setupZipArchiver();
+  } else {
+    console.info(
+        'The extension was silenced ' +
+        'because this is in the incognito context of a regular session.');
+  }
+});
