3a9e72d7f0be694999399c148434bc5f0ad9ec81
Yuwei Huang <yuweih@chromium.org>
2018-04-26 00:19:30 +0000

    [CRD iOS] Feed viewport transformation matrix when renderer context is recreated
    The app no longer renders the first frame after we recreate the renderer
    context when the app resumes. User has to move the cursor or viewport
    to get the frame rendered. That's because we didn't set the transformation
    matrix after the renderer context is recreated.
    
    This CL makes GlRenderer cache the viewport matrix and set it on the
    canvas when the canvas is recreated.
    
    Bug: 836975
    Change-Id: I3d4bb2575aa59f9148181104f320cf7a72770ad5
    Reviewed-on: https://chromium-review.googlesource.com/1028848
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553841}
    Former-commit-id: ab25983531e94546c8d4e658c67c2cbaa4640900

--- remoting/client/display/gl_renderer.cc
+++ remoting/client/display/gl_renderer.cc
@@ -52,9 +52,8 @@ void GlRenderer::RequestCanvasSize() {
 void GlRenderer::OnPixelTransformationChanged(
     const std::array<float, 9>& matrix) {
   DCHECK(thread_checker_.CalledOnValidThread());
+  transformation_matrix_ = matrix;
   if (!canvas_) {
-    VLOG(1) << "Trying to set transformation matrix when the canvas is not "
-            << "ready.";
     return;
   }
   canvas_->SetTransformationMatrix(matrix);
@@ -108,6 +107,9 @@ void GlRenderer::OnSurfaceCreated(std::unique_ptr<Canvas> canvas) {
   canvas_ = std::move(canvas);
   if (view_width_ > 0 && view_height_ > 0) {
     canvas_->SetViewSize(view_width_, view_height_);
+  }
+  if (transformation_matrix_) {
+    canvas_->SetTransformationMatrix(*transformation_matrix_);
   }
   for (auto& drawable : drawables_) {
     drawable->SetCanvas(canvas_->GetWeakPtr());
