86c3eacf79fbc9bfd2ad7a83c6b654b50786cf6b
Max Morin <maxmorin@chromium.org>
2018-04-26 11:20:28 +0000

    Revert "Migrate Arc unit tests to IdentityManager"
    This reverts commit adb9b7952fa0e3982296b3bd0981c0ed0d97cd0c [formerly 1e3550d572933d5dd57a47ce32d7a56f53f01887].
    
    Reason for revert: Failing tests, see crbug.com/837201
    
    Original change's description:
    > Migrate Arc unit tests to IdentityManager
    > 
    > identity::IdentityManager is the new API that replaces SigninManager[Base].
    > 
    > This includes ArcTermsOfServiceDefaultNegotiatorTest and ArcSupportHostTest.
    > 
    > Bug: 825190, 731023
    > Change-Id: I316c90c6d278e52d723468d63bc21cd288b2642b
    > Reviewed-on: https://chromium-review.googlesource.com/1025898
    > Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    > Commit-Queue: Marc Treib <treib@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#553949}
    
    TBR=yusukes@chromium.org,treib@chromium.org
    
    Change-Id: Iba9649631ec8633c0b89b867a60e89d3d63a01ad
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 825190, 731023, 837201
    Reviewed-on: https://chromium-review.googlesource.com/1030150
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Commit-Queue: Max Morin <maxmorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553979}
    Former-commit-id: 78ad1e6721949ebe27212347bed02ab4a08fcc7a

--- chrome/browser/chromeos/arc/arc_support_host_unittest.cc
+++ chrome/browser/chromeos/arc/arc_support_host_unittest.cc
@@ -10,16 +10,14 @@
 #include "chrome/browser/chromeos/login/users/fake_chrome_user_manager.h"
 #include "chrome/browser/consent_auditor/consent_auditor_factory.h"
 #include "chrome/browser/consent_auditor/consent_auditor_test_utils.h"
-#include "chrome/browser/signin/identity_manager_factory.h"
-#include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
+#include "chrome/browser/signin/fake_signin_manager_builder.h"
 #include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/consent_auditor/fake_consent_auditor.h"
+#include "components/signin/core/browser/fake_signin_manager.h"
 #include "components/user_manager/scoped_user_manager.h"
 #include "content/public/test/test_browser_thread_bundle.h"
-#include "services/identity/public/cpp/identity_manager.h"
-#include "services/identity/public/cpp/identity_test_utils.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -74,10 +72,7 @@ class ArcSupportHostTest : public BrowserWithTestWindowTest {
     BrowserWithTestWindowTest::SetUp();
     user_manager_enabler_ = std::make_unique<user_manager::ScopedUserManager>(
         std::make_unique<chromeos::FakeChromeUserManager>());
-    identity::MakePrimaryAccountAvailable(
-        SigninManagerFactory::GetForProfile(profile()),
-        ProfileOAuth2TokenServiceFactory::GetForProfile(profile()),
-        IdentityManagerFactory::GetForProfile(profile()), "test@account.com");
+    signin_manager()->SignIn("testing_account_id");
 
     support_host_ = std::make_unique<ArcSupportHost>(profile());
     fake_arc_support_ = std::make_unique<FakeArcSupport>(support_host_.get());
@@ -128,9 +123,15 @@ class ArcSupportHostTest : public BrowserWithTestWindowTest {
         ConsentAuditorFactory::GetForProfile(profile()));
   }
 
+  FakeSigninManagerBase* signin_manager() {
+    return static_cast<FakeSigninManagerBase*>(
+        SigninManagerFactory::GetForProfile(profile()));
+  }
+
   // BrowserWithTestWindowTest:
   TestingProfile::TestingFactories GetTestingFactories() override {
-    return {{ConsentAuditorFactory::GetInstance(), BuildFakeConsentAuditor}};
+    return {{SigninManagerFactory::GetInstance(), BuildFakeSigninManagerBase},
+            {ConsentAuditorFactory::GetInstance(), BuildFakeConsentAuditor}};
   }
 
  private:
