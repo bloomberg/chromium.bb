1daa3f1e8d0feb7257b4520cd180ae1acb6b47c5
Ahmed Fakhry <afakhry@google.com>
2018-04-26 20:43:26 +0000

    Tablet mode page mobile-like behavior exclusions
    This CL excludes the mobile-like page bahvior Webkit
    prefs from being set in tablet mode for:
    - Hosted apps in browsers (V1 apps and PWAs)
    - Internal pages such as chrome://settings
    - New Tab Pages.
    
    BUG=822455, 835939
    TEST=Added new test.
    
    Change-Id: I07592d511e78092e0f66959463aa456f7177efb0
    Reviewed-on: https://chromium-review.googlesource.com/1030899
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554137}
    Former-commit-id: 549fb56aa764e43c5a6adcaaacb95dea6a242a46

--- chrome/browser/chromeos/chrome_content_browser_client_chromeos_part.cc
+++ chrome/browser/chromeos/chrome_content_browser_client_chromeos_part.cc
@@ -4,14 +4,36 @@
 
 #include "chrome/browser/chromeos/chrome_content_browser_client_chromeos_part.h"
 
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/search/search.h"
 #include "chrome/browser/ui/ash/tablet_mode_client.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "content/public/browser/navigation_controller.h"
+#include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/web_preferences.h"
+
+namespace {
+
+// Returns true if |contents| is of an internal pages (such as
+// chrome://settings, chrome://extensions, ... etc) or the New Tab Page.
+bool ShouldExcludePage(content::WebContents* contents) {
+  DCHECK(contents);
+
+  content::NavigationEntry* entry = contents->GetController().GetVisibleEntry();
+  const GURL& url = entry->GetURL();
+  Profile* profile = Profile::FromBrowserContext(contents->GetBrowserContext());
+  if (profile && search::IsNTPURL(url, profile))
+    return true;
+
+  return url.SchemeIs("chrome");
+}
+
+}  // namespace
 
 ChromeContentBrowserClientChromeOsPart ::
     ChromeContentBrowserClientChromeOsPart() = default;
@@ -22,16 +44,24 @@ ChromeContentBrowserClientChromeOsPart ::
 void ChromeContentBrowserClientChromeOsPart::OverrideWebkitPrefs(
     content::RenderViewHost* rvh,
     content::WebPreferences* web_prefs) {
-  content::WebContents* contents =
-      content::WebContents::FromRenderViewHost(rvh);
+  // Enable some mobile-like behaviors when in tablet mode on Chrome OS.
   if (!TabletModeClient::Get() ||
-      !TabletModeClient::Get()->tablet_mode_enabled() ||
-      !chrome::FindBrowserWithWebContents(contents)) {
+      !TabletModeClient::Get()->tablet_mode_enabled()) {
     return;
   }
 
-  // Enable some mobile-like behaviors when in tablet mode on Chrome OS. Do
-  // this only for webcontents displayed in browsers.
+  // Do this only for webcontents displayed in browsers and are not of hosted
+  // apps.
+  content::WebContents* contents =
+      content::WebContents::FromRenderViewHost(rvh);
+  auto* browser = chrome::FindBrowserWithWebContents(contents);
+  if (!browser || browser->is_app())
+    return;
+
+  // Also exclude internal pages and NTPs.
+  if (ShouldExcludePage(contents))
+    return;
+
   web_prefs->viewport_enabled = true;
   web_prefs->viewport_meta_enabled = true;
   web_prefs->shrinks_viewport_contents_to_fit = true;
