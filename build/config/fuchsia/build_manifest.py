f1bdef665c58a84b928f0ef178e63729f0b5a7bb
Kevin Marshall <kmarshall@chromium.org>
2018-04-26 20:38:53 +0000

    Fuchsia: remove "shell" capability and add dynamic libraries to packages.
    Removing the "shell" capability allows packaged apps to load dynamic
    libraries from the package. In addition, it prevents the app from using any
    system-provided libraries, in favor of the package supplying all of
    its data and dynamic library dependencies.
    
    This CL includes the system libraries from the SDK's "dist" directory
    in packages built using the package() GN template.
    
    This reverts commit 6b9ce40f9c3206f804bd3e0dbdcc11579e3c355b [formerly 2841fb2f7be2c0b91953a86fa0460c4ac94aac24].
    
    Bug: 823927,836341, 837203
    Change-Id: Iea57853f6c3c4ebc36de4f758f168fffbd4d79ba
    Reviewed-on: https://chromium-review.googlesource.com/1027087
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554129}
    Former-commit-id: 98ffc41ad2518b62ec32d2c62f8da62ea1571357

--- build/config/fuchsia/build_manifest.py
+++ build/config/fuchsia/build_manifest.py
@@ -18,6 +18,7 @@ Arguments:
 
 import json
 import os
+import re
 import sys
 import tempfile
 
@@ -55,10 +56,8 @@ def MakePackagePath(file_path, roots):
     if file_path.startswith(next_root):
       relative_path = file_path[len(next_root):]
 
-      # TODO(fuchsia): The requirements for finding/loading .so are in flux, so
-      # this ought to be reconsidered at some point.
-      # See https://crbug.com/732897.
-      if file_path.endswith('.so'):
+      # Move all dynamic libraries (ending in .so or .so.<number>) to lib/.
+      if re.search('.*\.so(\.\d+)?$', file_path):
         relative_path = 'lib/' + os.path.basename(relative_path)
 
       return relative_path
@@ -70,8 +69,10 @@ def _GetStrippedPath(bin_path):
   """Finds the stripped version of the binary |bin_path| in the build
   output directory."""
 
+  # Skip the resolution step for binaries that don't have stripped counterparts,
+  # like system libraries or other libraries built outside the Chromium build.
   if not '.unstripped' in bin_path:
-    raise Exception('File "%s" is not in an .unstripped directory.' % bin_path)
+    return bin_path
 
   return os.path.normpath(os.path.join(bin_path,
                                        os.path.pardir,
@@ -100,20 +101,21 @@ def BuildManifest(root_dir, out_dir, app_name, app_filename,
       next_path = next_path.strip()
       if os.path.isdir(next_path):
         for root, _, files in os.walk(next_path):
-          for next_file in files:
-            if next_file.startswith('.'):
+          for current_file in files:
+            if current_file.startswith('.'):
               continue
-            expanded_files.add(os.path.abspath(os.path.join(root, next_file)))
+            expanded_files.add(os.path.abspath(
+                os.path.join(root, current_file)))
       else:
         expanded_files.add(os.path.abspath(next_path))
 
     # Format and write out the manifest contents.
     app_found = False
-    for next_file in expanded_files:
-      if _IsBinary(next_file):
-        next_file = _GetStrippedPath(next_file)
+    for current_file in expanded_files:
+      if _IsBinary(current_file):
+        current_file = _GetStrippedPath(current_file)
 
-      in_package_path = MakePackagePath(os.path.join(out_dir, next_file),
+      in_package_path = MakePackagePath(os.path.join(out_dir, current_file),
                                         [root_dir, out_dir])
       if in_package_path == app_filename:
         in_package_path = 'bin/app'
@@ -123,7 +125,14 @@ def BuildManifest(root_dir, out_dir, app_name, app_filename,
       # environments with differing parent directory structures,
       # e.g. builder bots and swarming clients.
       output.write('%s=%s\n' % (in_package_path,
-                                os.path.relpath(next_file, out_dir)))
+                                os.path.relpath(current_file, out_dir)))
+
+      # Use libc.so's dynamic linker by aliasing libc.so to ld.so.1.
+      # Fuchsia always looks for the linker implementation in ld.so.1.
+      if os.path.basename(in_package_path) == 'libc.so':
+        output.write('%s=%s\n' % (os.path.dirname(in_package_path) + '/ld.so.1',
+                                  os.path.relpath(current_file, out_dir)))
+
     if not app_found:
       raise Exception('Could not locate executable inside runtime_deps.')
 
