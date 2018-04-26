1af43c46cfb888629e3dff949485afd5361ccfd9
Antonio Gomes <tonikitoo@igalia.com>
2018-04-26 14:57:51 +0000

    Reland "Migrate ProfileDownloader from URLFetcher to URLLoader"
    This is a reland of 526485fd09ec18d2c4ef3f93339f89b987430763 [formerly 9f9200be58484a62d515953aaf13bc729c377ed3],
    https://crrev.com/c/1024992.
    
    Original CL was bitten by the "doing real work within a DCHECK"
    sina.
    We fixed it by %s/DCHECK/EXPECT_TRUE/g in UserImageManagerTest::HandleRequest.
    
    TBR=xiyuan@chromium.org
    
    Original change's description:
    > Migrate ProfileDownloader from URLFetcher to URLLoader
    >
    > BUG=773295
    >
    > Change-Id: Ia199631a83a21b3210baa7741f6591875075a3b5
    > Reviewed-on: https://chromium-review.googlesource.com/1024992
    > Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#553719}
    
    Bug: 773295
    Change-Id: I8ba52ee9a5b434b4954ddbfea2703159859bbe97
    Reviewed-on: https://chromium-review.googlesource.com/1030290
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#554027}
    Former-commit-id: 452e2c3db502e6970fd23cc285044cd0d740406e

--- chrome/browser/profiles/profile_downloader.h
+++ chrome/browser/profiles/profile_downloader.h
@@ -15,19 +15,15 @@
 #include "components/signin/core/browser/account_info.h"
 #include "components/signin/core/browser/account_tracker_service.h"
 #include "google_apis/gaia/oauth2_token_service.h"
-#include "net/url_request/url_fetcher_delegate.h"
+#include "services/network/public/cpp/simple_url_loader.h"
+#include "services/network/public/mojom/url_loader_factory.mojom.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 
 class ProfileDownloaderDelegate;
 
-namespace net {
-class URLFetcher;
-}  // namespace net
-
 // Downloads user profile information. The profile picture is decoded in a
 // sandboxed process.
-class ProfileDownloader : public net::URLFetcherDelegate,
-                          public ImageDecoder::ImageRequest,
+class ProfileDownloader : public ImageDecoder::ImageRequest,
                           public OAuth2TokenService::Observer,
                           public OAuth2TokenService::Consumer,
                           public AccountTrackerService::Observer {
@@ -87,8 +83,7 @@ class ProfileDownloader : public net::URLFetcherDelegate,
 
   void FetchImageData();
 
-  // Overriden from net::URLFetcherDelegate:
-  void OnURLFetchComplete(const net::URLFetcher* source) override;
+  void OnURLLoaderComplete(std::unique_ptr<std::string> response_body);
 
   // Overriden from ImageDecoder::ImageRequest:
   void OnImageDecoded(const SkBitmap& decoded_image) override;
@@ -121,7 +116,7 @@ class ProfileDownloader : public net::URLFetcherDelegate,
   ProfileDownloaderDelegate* delegate_;
   std::string account_id_;
   std::string auth_token_;
-  std::unique_ptr<net::URLFetcher> profile_image_fetcher_;
+  std::unique_ptr<network::SimpleURLLoader> simple_loader_;
   std::unique_ptr<OAuth2TokenService::Request> oauth2_access_token_request_;
   AccountInfo account_info_;
   SkBitmap profile_picture_;
