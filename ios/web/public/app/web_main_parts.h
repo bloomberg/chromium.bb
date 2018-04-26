3901d25afa6bea193a33d728e40c0c431888c787
Gabriel Charette <gab@chromium.org>
2018-04-26 00:37:56 +0000

    Migrate MessageLoop::current() to MessageLoopCurrent in /ios/web
    These changes were scripted as part of the effort to restrict exposure of the
    raw MessageLoop*.
    
    Note that many static methods exposed on MessageLoopCurrent are also deprecated
    so it is very well possible that this is migrating to an already deprecated
    method.
    
    The goal of this pass is to reduce usage of MessageLoop::current() (and
    ultimately make it impossible to obtain the MessageLoop* statically).
    
    As such I will not edit this CL unless the script did something logically wrong.
    I defer to owners to fix highlighted usage of already deprecated APIs.
    
    Possible script screw ups / things to look out for in this review:
     - Storing MessageLoopCurrent in a MessageLoop* variable or comparing it against
       one (I will go over that in a separate pass).
    
    Includes should have been stripped if that was the last usage of message_loop.h
    in that file.
    
    Please CQ if LGTY
    
    This CL was uploaded by git cl split.
    
    R=eugenebut@chromium.org
    
    Bug: 825327
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ic89cc4297947534db6418c8c1b65f18a9528c71f
    Reviewed-on: https://chromium-review.googlesource.com/1024872
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#553851}
    Former-commit-id: cb6928930fc1fb8e29aebc6dd24af31367719bfb

--- ios/web/public/app/web_main_parts.h
+++ ios/web/public/app/web_main_parts.h
@@ -27,7 +27,7 @@ namespace web {
 //    should be done immediately before the start of the main message loop
 //    should go in |PreMainMessageLoopStart()|.
 //  - RunMainMessageLoopParts:  things to be done before and after invoking the
-//    main message loop run method (e.g. MessageLoopForUI::current()->Run()).
+//    main message loop run method (e.g. MessageLoopCurrentForUI::Get()->Run()).
 //
 // How to add stuff (to existing parts):
 //  - Figure out when your new code should be executed. What must happen
--- ios/web/public/global_state/ios_global_state.mm
+++ ios/web/public/global_state/ios_global_state.mm
@@ -8,6 +8,7 @@
 #include "base/command_line.h"
 #include "base/memory/ptr_util.h"
 #include "base/message_loop/message_loop.h"
+#include "base/message_loop/message_loop_current.h"
 #include "base/task_scheduler/initialization_util.h"
 #include "net/base/network_change_notifier.h"
 
@@ -58,10 +59,10 @@ void BuildMessageLoop() {
   dispatch_once(&once_token, ^{
     // Create a MessageLoop if one does not already exist for the current
     // thread.
-    if (!base::MessageLoop::current()) {
+    if (!base::MessageLoopCurrent::Get()) {
       g_message_loop = new base::MessageLoopForUI();
     }
-    base::MessageLoopForUI::current()->Attach();
+    base::MessageLoopCurrentForUI::Get()->Attach();
   });
 }
 
