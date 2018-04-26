74272cd5560a27b37c5fa41fdaa534a7a30ee703
Donn Denman <donnd@chromium.org>
2018-04-26 23:36:22 +0000

    Disable testEnterExitFullscreenWithControlsButton
    Disable this test on L only.  Seems to work fine on all other
    versions of Android.
    
    TBR=yfriedman@chromium.org
    
    BUG=837423
    
    Change-Id: Ib0ca3d638425a0459ea9e06486884fdeed2fbaca
    Reviewed-on: https://chromium-review.googlesource.com/1031558
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554185}
    Former-commit-id: 52330fb18db20386459948a141ec9af7e2d97ccd

--- content/public/android/javatests/src/org/chromium/content/browser/VideoFullscreenOrientationLockTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/VideoFullscreenOrientationLockTest.java
@@ -6,6 +6,7 @@ package org.chromium.content.browser;
 
 import android.content.pm.ActivityInfo;
 import android.graphics.Rect;
+import android.os.Build;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.MediumTest;
 
@@ -16,6 +17,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.content.browser.test.ContentJUnit4ClassRunner;
@@ -134,6 +136,8 @@ public class VideoFullscreenOrientationLockTest {
 
     @Test
     @MediumTest
+    @DisableIf.Build(message = "crbug.com/837423", sdk_is_greater_than = Build.VERSION_CODES.KITKAT,
+            sdk_is_less_than = Build.VERSION_CODES.M)
     @Feature({"VideoFullscreenOrientationLock"})
     @Restriction({UiRestriction.RESTRICTION_TYPE_PHONE})
     public void testEnterExitFullscreenWithControlsButton() throws Exception {
