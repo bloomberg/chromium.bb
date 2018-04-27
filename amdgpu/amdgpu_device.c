bc9c789073c82c645210c80b6e63117444dce252
Michel Dänzer <michel.daenzer@amd.com>
2018-04-27 16:42:26 +0200

    amdgpu: Deinitialize vamgr_high{,_32}
    Fixes memory leaks.
    
    Reviewed-by: Andrey Grodzovsky <andrey.grodzovsky@amd.com>

--- amdgpu/amdgpu_device.c
+++ amdgpu/amdgpu_device.c
@@ -128,6 +128,8 @@ static void amdgpu_device_free_internal(amdgpu_device_handle dev)
 {
 	amdgpu_vamgr_deinit(&dev->vamgr_32);
 	amdgpu_vamgr_deinit(&dev->vamgr);
+	amdgpu_vamgr_deinit(&dev->vamgr_high_32);
+	amdgpu_vamgr_deinit(&dev->vamgr_high);
 	util_hash_table_destroy(dev->bo_flink_names);
 	util_hash_table_destroy(dev->bo_handles);
 	pthread_mutex_destroy(&dev->bo_table_mutex);
