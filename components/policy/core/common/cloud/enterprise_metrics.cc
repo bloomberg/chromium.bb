4950e50b86488e4c67a20eb9d53b4bbb7b66c0f5
Maksim Ivanov <emaxx@chromium.org>
2018-04-27 21:45:36 +0000

    Add UMAs on session abort due to fatal policy error
    Gather statistics on the number of cases when the fatal Chrome OS user
    policy initialization happens, which leads to termination of the user
    session.
    
    BUG=chromium:830654
    TEST=none
    
    Change-Id: Iae8e2455e36e3daae5663b3ff2266199bd6283dd
    Reviewed-on: https://chromium-review.googlesource.com/1017124
    Reviewed-by: Lutz Justen <ljusten@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554519}
    Former-commit-id: 1cd2c16c0a1693ffa5fb7c918f7c8fd58bb9b7c0

--- components/policy/core/common/cloud/enterprise_metrics.cc
+++ components/policy/core/common/cloud/enterprise_metrics.cc
@@ -10,6 +10,8 @@ const char kMetricToken[] = "Enterprise.DMToken";
 const char kMetricPolicy[] = "Enterprise.Policy";
 const char kMetricUserPolicyRefresh[] = "Enterprise.PolicyRefresh";
 const char kMetricUserPolicyInvalidations[] = "Enterprise.PolicyInvalidations";
+const char kMetricUserPolicyChromeOSSessionAbort[] =
+    "Enterprise.UserPolicyChromeOS.SessionAbort";
 const char kMetricDevicePolicyRefresh[] = "Enterprise.DevicePolicyRefresh";
 const char kMetricDevicePolicyInvalidations[] =
     "Enterprise.DevicePolicyInvalidations";
