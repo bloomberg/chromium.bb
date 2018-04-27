846b99d62ecb9aa65d03319bd0c7d7cd0a35153a
Eric Orth <ericorth@chromium.org>
2018-04-27 23:52:38 +0000

    Create a noop test implementation of NetworkContext.
    Useful to override to create specialized mocks or fakes that only care
    about a portion of the overall interface.  Especially useful for mocks
    or fakes owned outside the network service code as the mocks can create
    an interface dependency on only the overridden methods.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Id03bdcaab6d991fb1a79ddcaf4eaf0612fe64d52
    Reviewed-on: https://chromium-review.googlesource.com/1026418
    Commit-Queue: Eric Orth <ericorth@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554566}
    Former-commit-id: 83ba87a0a6306b2e5d5645a3da16b6f86fcd1a3d

--- components/mirroring/service/fake_network_service.h
+++ components/mirroring/service/fake_network_service.h
@@ -8,8 +8,8 @@
 #include "base/callback.h"
 #include "media/cast/net/cast_transport_config.h"
 #include "mojo/public/cpp/bindings/binding.h"
-#include "services/network/public/mojom/network_service.mojom.h"
 #include "services/network/public/mojom/udp_socket.mojom.h"
+#include "services/network/test/test_network_context.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -61,7 +61,7 @@ class MockUdpSocket final : public network::mojom::UDPSocket {
   DISALLOW_COPY_AND_ASSIGN(MockUdpSocket);
 };
 
-class MockNetworkContext final : public network::mojom::NetworkContext {
+class MockNetworkContext final : public network::TestNetworkContext {
  public:
   explicit MockNetworkContext(network::mojom::NetworkContextRequest request);
   ~MockNetworkContext() override;
@@ -69,57 +69,8 @@ class MockNetworkContext final : public network::mojom::NetworkContext {
   MOCK_METHOD0(OnUDPSocketCreated, void());
 
   // network::mojom::NetworkContext implementation:
-  void CreateURLLoaderFactory(network::mojom::URLLoaderFactoryRequest request,
-                              uint32_t process_id) override {}
-  void GetCookieManager(network::mojom::CookieManagerRequest request) override {
-  }
-  void GetRestrictedCookieManager(
-      network::mojom::RestrictedCookieManagerRequest request,
-      int32_t render_process_id,
-      int32_t render_frame_id) override {}
-  void ClearNetworkingHistorySince(
-      base::Time time,
-      base::OnceClosure completion_callback) override {}
-  void ClearHttpCache(base::Time start_time,
-                      base::Time end_time,
-                      network::mojom::ClearDataFilterPtr filter,
-                      ClearHttpCacheCallback callback) override {}
-  void ClearChannelIds(base::Time start_time,
-                       base::Time end_time,
-                       network::mojom::ClearDataFilterPtr filter,
-                       ClearChannelIdsCallback callback) override {}
-  void SetNetworkConditions(
-      const std::string& profile_id,
-      network::mojom::NetworkConditionsPtr conditions) override {}
-  void SetAcceptLanguage(const std::string& new_accept_language) override {}
-  void SetCTPolicy(
-      const std::vector<std::string>& required_hosts,
-      const std::vector<std::string>& excluded_hosts,
-      const std::vector<std::string>& excluded_spkis,
-      const std::vector<std::string>& excluded_legacy_spkis) override {}
-  void AddHSTSForTesting(const std::string& host,
-                         base::Time expiry,
-                         bool include_subdomains,
-                         AddHSTSForTestingCallback callback) override {}
   void CreateUDPSocket(network::mojom::UDPSocketRequest request,
                        network::mojom::UDPSocketReceiverPtr receiver) override;
-  void CreateTCPServerSocket(
-      const net::IPEndPoint& local_addr,
-      uint32_t backlog,
-      const net::MutableNetworkTrafficAnnotationTag& traffic_annotation,
-      network::mojom::TCPServerSocketRequest request,
-      CreateTCPServerSocketCallback callback) override {}
-  void CreateTCPConnectedSocket(
-      const base::Optional<net::IPEndPoint>& local_addr,
-      const net::AddressList& remote_addr_list,
-      const net::MutableNetworkTrafficAnnotationTag& traffic_annotation,
-      network::mojom::TCPConnectedSocketRequest request,
-      network::mojom::TCPConnectedSocketObserverPtr observer,
-      CreateTCPConnectedSocketCallback callback) override {}
-  void CreateWebSocket(network::mojom::WebSocketRequest request,
-                       int process_id,
-                       int render_frame_id,
-                       const url::Origin& origin) override {}
 
   MockUdpSocket* udp_socket() const { return udp_socket_.get(); }
 
