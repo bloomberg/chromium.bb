1cf126bb568161056c20699c4ae3a3c1ba6f5cb1
John Budorick <jbudorick@chromium.org>
2018-04-27 20:11:59 +0000

    Revert "Add ruleset_converter tool to subresource_filter"
    This reverts commit 1848f35ebc1e3f9ec0dd22879bac4d224136afbd [formerly ce1305123e10f97adb155c8930684b57ab28d4ad].
    
    Reason for revert: broke compilation: https://ci.chromium.org/buildbot/chromium/Win/66613
    
    Original change's description:
    > Add ruleset_converter tool to subresource_filter
    > 
    > This CL also
    > - Aggregates all subresource_filter tools into a single target
    > - changes subresource_filter tools to use the same (but equiv)
    >   condition as some other tools
    > - Adds that target to "all"
    > 
    > Bug: 833419
    > Change-Id: I732ac0860f90e036281bdf899f8b5e6e92f3f3e5
    > Reviewed-on: https://chromium-review.googlesource.com/1031211
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    > Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#554455}
    
    TBR=sky@chromium.org,jkarlin@chromium.org,csharrison@chromium.org
    
    Change-Id: I03e004c156e40d695c866ba07a00c480eedcd225
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 833419
    Reviewed-on: https://chromium-review.googlesource.com/1033399
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554471}
    Former-commit-id: fc07a87c5c5081e79a1b52d0fd344d0284d18783

--- components/subresource_filter/tools/ruleset_converter/rule_stream.cc
+++ components/subresource_filter/tools/ruleset_converter/rule_stream.cc
@@ -298,7 +298,6 @@ std::unique_ptr<RuleInputStream> RuleInputStream::Create(
     std::unique_ptr<std::istream> input,
     RulesetFormat format) {
   CHECK(input);
-  CHECK(!input->bad());
   std::unique_ptr<RuleInputStream> result;
   switch (format) {
     case RulesetFormat::kFilterList:
@@ -322,7 +321,6 @@ std::unique_ptr<RuleOutputStream> RuleOutputStream::Create(
     std::unique_ptr<std::ostream> output,
     RulesetFormat format) {
   CHECK(output);
-  CHECK(!output->bad());
   std::unique_ptr<RuleOutputStream> result;
   switch (format) {
     case RulesetFormat::kFilterList:
