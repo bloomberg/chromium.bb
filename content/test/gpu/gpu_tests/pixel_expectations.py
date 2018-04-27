f2f24e56bafc2cf3a2ba011f8b0e64af4f22b26b
Fredrik Hubinette <hubbe@google.com>
2018-04-27 09:52:50 +0000

    re-activate highsierra bypass
    ... for Pixel_Video_VP9
    
    Bug: 754986
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I5fd7742ca773d8a5abbb8ad8326f559af246f19c
    Reviewed-on: https://chromium-review.googlesource.com/1024497
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Fredrik Hubinette <hubbe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554347}
    Former-commit-id: 4e7d0b02070eff7055388cb1f061061134edf8b9

--- content/test/gpu/gpu_tests/pixel_expectations.py
+++ content/test/gpu/gpu_tests/pixel_expectations.py
@@ -83,9 +83,8 @@ class PixelExpectations(GpuTestExpectations):
     # Failing on Mac Intel HighSierra
     self.Fail('Pixel_Video_MP4',
         ['highsierra', ('intel', 0xa2e)], bug=774809)
-    # TODO(hubbe): put this back after rebaseline below
-    # self.Fail('Pixel_Video_VP9',
-    #     ['highsierra', ('intel', 0xa2e)], bug=774809)
+    self.Fail('Pixel_Video_VP9',
+        ['highsierra', ('intel', 0xa2e)], bug=774809)
     self.Fail('Pixel_WebGLGreenTriangle_NonChromiumImage_NoAA_NoAlpha',
         ['highsierra', ('intel', 0xa2e)], bug=774809)
 
