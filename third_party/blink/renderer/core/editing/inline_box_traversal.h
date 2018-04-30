e80fce669ac792a13aee4f00105f09bbd91d37a6
Xiaocheng Hu <xiaochengh@chromium.org>
2018-04-30 22:14:19 +0000

    Add/Revise documentation in inline_box_traversal.h
    Notry: True
    Tbr: yosin@chromium.org
    Change-Id: Ic11c0a45bf1d17579ba1afe401ce31ec8c659f97
    Reviewed-on: https://chromium-review.googlesource.com/1036448
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554897}
    Former-commit-id: c37adf34129c515b3ee5d325d350645078fbf7e2

--- third_party/blink/renderer/core/editing/inline_box_traversal.h
+++ third_party/blink/renderer/core/editing/inline_box_traversal.h
@@ -19,27 +19,32 @@ class InlineBoxTraversal final {
   // TODO(yosin): We should take |bidi_level| from |InlineBox::BidiLevel()|,
   // once all call sites satisfy it.
 
-  // Returns |InlineBox| which is less than or equal to |bidi_level| of
-  // left/right of specified |InlineBox|.
-  static InlineBox* FindLeftBidiRun(const InlineBox&, unsigned bidi_level);
-  static InlineBox* FindRightBidiRun(const InlineBox&, unsigned bidi_level);
+  // Traverses left/right from |box|, and returns the first box with bidi level
+  // less than or equal to |bidi_level| (excluding |box| itself). Returns
+  // |nullptr| when such a box doesn't exist.
+  static InlineBox* FindLeftBidiRun(const InlineBox& box, unsigned bidi_level);
+  static InlineBox* FindRightBidiRun(const InlineBox& box, unsigned bidi_level);
 
-  // Find left boundary variations
+  // Traverses left/right from |box|, and returns the last non-linebreak box
+  // with bidi level greater than |bidi_level| (including |box| itself).
   static InlineBox* FindLeftBoundaryOfBidiRunIgnoringLineBreak(
-      const InlineBox&,
+      const InlineBox& box,
       unsigned bidi_level);
-  static InlineBox* FindLeftBoundaryOfEntireBidiRun(const InlineBox&,
+  static InlineBox* FindRightBoundaryOfBidiRunIgnoringLineBreak(
+      const InlineBox& box,
+      unsigned bidi_level);
+
+  // Traverses left/right from |box|, and returns the last box with bidi level
+  // greater than or equal to |bidi_level| (including |box| itself).
+  static InlineBox* FindLeftBoundaryOfEntireBidiRun(const InlineBox& box,
                                                     unsigned bidi_level);
+  static InlineBox* FindRightBoundaryOfEntireBidiRun(const InlineBox& box,
+                                                     unsigned bidi_level);
+
+  // Variants of the above two where line break boxes are ignored.
   static InlineBox* FindLeftBoundaryOfEntireBidiRunIgnoringLineBreak(
       const InlineBox&,
       unsigned bidi_level);
-
-  // Find right boundary variations
-  static InlineBox* FindRightBoundaryOfBidiRunIgnoringLineBreak(
-      const InlineBox&,
-      unsigned bidi_level);
-  static InlineBox* FindRightBoundaryOfEntireBidiRun(const InlineBox&,
-                                                     unsigned bidi_level);
   static InlineBox* FindRightBoundaryOfEntireBidiRunIgnoringLineBreak(
       const InlineBox&,
       unsigned bidi_level);
