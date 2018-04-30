26ecd05eb971f20a335267d31041dba9a940536e
Joe Downing <joedow@chromium.org>
2018-04-30 18:42:33 +0000

    [KeyboardLock] Enabling browser-level keyboard lock for X11
    This CL adds a minimal KeyboardHook impl for X11 which enables
    browser-level keyboard lock functionality.
    
    I split up the original X11 KeyboardHook impl into two pieces as
    the browser level keyboard lock functionality is well understood
    and can be tested now whereas the system level keyboard lock for
    X11 has a few questions that need to be resolved before checking
    it in.
    
    Therefore I want to get the the browser-level lock functionality
    working so it can be tested in Canary while I sort out the
    system shortcut aspect of the impl.
    
    BUG=680809
    
    Change-Id: I47ea23f50d6c61364343574ab4cdc73bd17f0c3f
    Reviewed-on: https://chromium-review.googlesource.com/1034272
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#554822}
    Former-commit-id: 0a445800d9af4815b80f5dd71e169b348804a154

---
+++ ui/events/x/keyboard_hook_x11.cc
@@ -0,0 +1,56 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/callback.h"
+#include "base/containers/flat_set.h"
+#include "base/macros.h"
+#include "base/optional.h"
+#include "ui/events/keyboard_hook_base.h"
+
+namespace ui {
+
+namespace {
+
+// A default implementation for the X11 platform.
+class KeyboardHookX11 : public KeyboardHookBase {
+ public:
+  KeyboardHookX11(base::Optional<base::flat_set<int>> native_key_codes,
+                  KeyEventCallback callback);
+  ~KeyboardHookX11() override;
+
+  bool Register();
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(KeyboardHookX11);
+};
+
+KeyboardHookX11::KeyboardHookX11(base::Optional<base::flat_set<int>> key_codes,
+                                 KeyEventCallback callback)
+    : KeyboardHookBase(std::move(key_codes), std::move(callback)) {}
+
+KeyboardHookX11::~KeyboardHookX11() = default;
+
+bool KeyboardHookX11::Register() {
+  // TODO(680809): Implement system-level keyboard lock feature for X11.
+  // Return true to enable browser-level keyboard lock for the X11 platform.
+  return true;
+}
+
+}  // namespace
+
+// static
+std::unique_ptr<KeyboardHook> KeyboardHook::Create(
+    base::Optional<base::flat_set<int>> native_key_codes,
+    KeyboardHook::KeyEventCallback callback) {
+  std::unique_ptr<KeyboardHookX11> keyboard_hook =
+      std::make_unique<KeyboardHookX11>(std::move(native_key_codes),
+                                        std::move(callback));
+
+  if (!keyboard_hook->Register())
+    return nullptr;
+
+  return keyboard_hook;
+}
+
+}  // namespace ui
