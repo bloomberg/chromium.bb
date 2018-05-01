6ec889743025290c03f56dcacf99a750b24acb0b
Hirokazu Honda <hiroh@chromium.org>
2018-05-01 20:43:58 +0000

    media/gpu/test VDA: Implement EncodedDataHelper to handle input (encoded) data
    GLRenderingVDAClient has several functions to handle input data (i.e. encoded
    data), e.g., GetBytesForFirstFragment() and GetBytesNextNALU().
    They are not core role of GLRenderingVdaClient.
    This introduces EncodedDataHelper as a VDA unittest helper class. It handles
    encoded data instead of GLRenderingVDAClient.
    
    BUG=chromium:834170
    TEST=VDA unittest on kevin and caroline
    TEST=VDA unittest on other platforms in CQ
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I5116b03172b6a7a80b0473d75d4aeb23868db20d
    Reviewed-on: https://chromium-review.googlesource.com/1016822
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Pawel Osciak <posciak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555176}
    Former-commit-id: 6d9f8a44b4bce6edb29911783ccd2fa5e415d8af

--- media/gpu/test/video_decode_accelerator_unittest_helpers.cc
+++ media/gpu/test/video_decode_accelerator_unittest_helpers.cc
@@ -8,8 +8,10 @@
 #include "base/files/file_util.h"
 #include "base/strings/string_split.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "media/base/video_decoder_config.h"
 #include "media/gpu/format_utils.h"
 #include "media/gpu/test/rendering_helper.h"
+#include "media/video/h264_parser.h"
 
 #if defined(OS_CHROMEOS)
 #include "ui/gfx/native_pixmap.h"
@@ -17,6 +19,8 @@
 #include "ui/ozone/public/ozone_platform.h"
 #include "ui/ozone/public/surface_factory_ozone.h"
 #endif
+
+#define VLOGF(level) VLOG(level) << __func__ << "(): "
 
 namespace media {
 namespace test {
@@ -120,6 +124,115 @@ gfx::GpuMemoryBufferHandle TextureRef::ExportGpuMemoryBufferHandle() const {
   }
 #endif
   return handle;
+}
+
+EncodedDataHelper::EncodedDataHelper(const std::string& data,
+                                     VideoCodecProfile profile)
+    : data_(data), profile_(profile) {}
+
+EncodedDataHelper::~EncodedDataHelper() {
+  base::STLClearObject(&data_);
+}
+
+bool EncodedDataHelper::IsNALHeader(const std::string& data, size_t pos) {
+  return data[pos] == 0 && data[pos + 1] == 0 && data[pos + 2] == 0 &&
+         data[pos + 3] == 1;
+}
+
+std::string EncodedDataHelper::GetBytesForNextData() {
+  switch (VideoCodecProfileToVideoCodec(profile_)) {
+    case kCodecH264:
+      return GetBytesForNextFragment();
+    case kCodecVP8:
+    case kCodecVP9:
+      return GetBytesForNextFrame();
+    default:
+      NOTREACHED();
+      return std::string();
+  }
+}
+
+std::string EncodedDataHelper::GetBytesForNextFragment() {
+  if (next_pos_to_decode_ == 0) {
+    size_t skipped_fragments_count = 0;
+    if (!LookForSPS(&skipped_fragments_count)) {
+      next_pos_to_decode_ = 0;
+      return std::string();
+    }
+    num_skipped_fragments_ += skipped_fragments_count;
+  }
+
+  size_t start_pos = next_pos_to_decode_;
+  size_t next_nalu_pos = GetBytesForNextNALU(start_pos);
+
+  // Update next_pos_to_decode_.
+  next_pos_to_decode_ = next_nalu_pos;
+  return data_.substr(start_pos, next_nalu_pos - start_pos);
+}
+
+size_t EncodedDataHelper::GetBytesForNextNALU(size_t start_pos) {
+  size_t pos = start_pos;
+  if (pos + 4 > data_.size())
+    return pos;
+  LOG_ASSERT(IsNALHeader(data_, pos));
+  pos += 4;
+  while (pos + 4 <= data_.size() && !IsNALHeader(data_, pos)) {
+    ++pos;
+  }
+  if (pos + 3 >= data_.size())
+    pos = data_.size();
+  return pos;
+}
+
+bool EncodedDataHelper::LookForSPS(size_t* skipped_fragments_count) {
+  *skipped_fragments_count = 0;
+  while (next_pos_to_decode_ + 4 < data_.size()) {
+    if ((data_[next_pos_to_decode_ + 4] & 0x1f) == 0x7) {
+      return true;
+    }
+    *skipped_fragments_count += 1;
+    next_pos_to_decode_ = GetBytesForNextNALU(next_pos_to_decode_);
+  }
+  return false;
+}
+
+std::string EncodedDataHelper::GetBytesForNextFrame() {
+  // Helpful description: http://wiki.multimedia.cx/index.php?title=IVF
+  size_t pos = next_pos_to_decode_;
+  std::string bytes;
+  if (pos == 0)
+    pos = 32;  // Skip IVF header.
+
+  uint32_t frame_size = *reinterpret_cast<uint32_t*>(&data_[pos]);
+  pos += 12;  // Skip frame header.
+  bytes.append(data_.substr(pos, frame_size));
+
+  // Update next_pos_to_decode_.
+  next_pos_to_decode_ = pos + frame_size;
+  return bytes;
+}
+
+// static
+bool EncodedDataHelper::HasConfigInfo(const uint8_t* data,
+                                      size_t size,
+                                      VideoCodecProfile profile) {
+  if (profile >= H264PROFILE_MIN && profile <= H264PROFILE_MAX) {
+    H264Parser parser;
+    parser.SetStream(data, size);
+    H264NALU nalu;
+    H264Parser::Result result = parser.AdvanceToNextNALU(&nalu);
+    if (result != H264Parser::kOk) {
+      // Let the VDA figure out there's something wrong with the stream.
+      return false;
+    }
+
+    return nalu.nal_unit_type == H264NALU::kSPS;
+  } else if (profile >= VP8PROFILE_MIN && profile <= VP9PROFILE_MAX) {
+    return (size > 0 && !(data[0] & 0x01));
+  }
+  // Shouldn't happen at this point.
+  LOG(FATAL) << "Invalid profile: " << GetProfileName(profile);
+  return false;
 }
 
 // Read in golden MD5s for the thumbnailed rendering of this video
