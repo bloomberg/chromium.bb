b57cbe2be9fdfb7b785a25a3a9a9c83808672f80
Philip Rogers <pdr@chromium.org>
2018-05-01 17:02:33 +0000

    [BlinkGenPropertyTrees] Run paint unit tests with BlinkGenPropertyTrees
    This patch starts running the paint unit tests with the new
    BlinkGenPropertyTrees configuration.
    
    A small codechange was required to pass FrameThrottlingTest's
    LifecycleUpdateAfterUnthrottledCompositingUpdate test. Just like
    https://chromium-review.googlesource.com/c/chromium/src/+/900262,
    we need to skip the collection of throttled GraphicsLayers because
    the property tree state will be undefined.
    
    Bug: 836902
    Change-Id: I038d55bb05e9560f7e1bd0594cb3386a8bae016a
    Reviewed-on: https://chromium-review.googlesource.com/1037189
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555080}
    Former-commit-id: dcf07e326c023622947b3e6ec6ccc1bb7d256cbd

--- third_party/blink/renderer/platform/testing/paint_test_configurations.h
+++ third_party/blink/renderer/platform/testing/paint_test_configurations.h
@@ -13,20 +13,23 @@ namespace blink {
 enum {
   kRootLayerScrolling = 1 << 0,
   kSlimmingPaintV175 = 1 << 1,
-  kSlimmingPaintV2 = 1 << 2,
-  kUnderInvalidationChecking = 1 << 3,
+  kBlinkGenPropertyTrees = 1 << 2,
+  kSlimmingPaintV2 = 1 << 3,
+  kUnderInvalidationChecking = 1 << 4,
 };
 
 class PaintTestConfigurations
     : public testing::WithParamInterface<unsigned>,
       private ScopedRootLayerScrollingForTest,
       private ScopedSlimmingPaintV175ForTest,
+      private ScopedBlinkGenPropertyTreesForTest,
       private ScopedSlimmingPaintV2ForTest,
       private ScopedPaintUnderInvalidationCheckingForTest {
  public:
   PaintTestConfigurations()
       : ScopedRootLayerScrollingForTest(GetParam() & kRootLayerScrolling),
         ScopedSlimmingPaintV175ForTest(GetParam() & kSlimmingPaintV175),
+        ScopedBlinkGenPropertyTreesForTest(GetParam() & kBlinkGenPropertyTrees),
         ScopedSlimmingPaintV2ForTest(GetParam() & kSlimmingPaintV2),
         ScopedPaintUnderInvalidationCheckingForTest(
             GetParam() & kUnderInvalidationChecking) {}
@@ -35,6 +38,7 @@ class PaintTestConfigurations
 static constexpr unsigned kAllSlimmingPaintTestConfigurations[] = {
     0,
     kSlimmingPaintV175,
+    kBlinkGenPropertyTrees | kSlimmingPaintV175 | kRootLayerScrolling,
     kSlimmingPaintV2,
     kRootLayerScrolling,
     kSlimmingPaintV175 | kRootLayerScrolling,
@@ -42,8 +46,11 @@ static constexpr unsigned kAllSlimmingPaintTestConfigurations[] = {
 };
 
 static constexpr unsigned kSlimmingPaintNonV1TestConfigurations[] = {
-    kSlimmingPaintV175, kSlimmingPaintV175 | kRootLayerScrolling,
-    kSlimmingPaintV2, kSlimmingPaintV2 | kRootLayerScrolling,
+    kSlimmingPaintV175,
+    kSlimmingPaintV175 | kRootLayerScrolling,
+    kSlimmingPaintV2,
+    kSlimmingPaintV2 | kRootLayerScrolling,
+    kBlinkGenPropertyTrees | kSlimmingPaintV175 | kRootLayerScrolling,
 };
 
 static constexpr unsigned kSlimmingPaintV2TestConfigurations[] = {
@@ -51,7 +58,7 @@ static constexpr unsigned kSlimmingPaintV2TestConfigurations[] = {
 };
 
 static constexpr unsigned kSlimmingPaintVersions[] = {
-    0, kSlimmingPaintV175, kSlimmingPaintV2,
+    0, kSlimmingPaintV175, kBlinkGenPropertyTrees, kSlimmingPaintV2,
 };
 
 }  // namespace blink
