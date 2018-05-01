43fb9705e5d24b2ce095515fc7f2d2a3557428e1
Karan Bhatia <karandeepb@chromium.org>
2018-05-01 06:34:35 +0000

    DNR tests: Use ExtensionTestMessageListener to detect background page load.
    This CL changes declarative net request browsertests to use
    ExtensionTestMessageListener to detect background page load instead of relying
    on the notification system, which is deprecated. Also, add a
    extension_id_for_message() method to ExtensionTestMessageListener. This is
    useful to detect the extension id, the message was received from.
    
    BUG=696822
    
    Change-Id: Ie1fc74d547d4ee417c5f6b2d5a7082adbc9d164a
    Reviewed-on: https://chromium-review.googlesource.com/1034155
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555005}
    Former-commit-id: 6e6741a13720e1298a2d921540c265424155ffb3

--- chrome/browser/extensions/api/declarative_net_request/declarative_net_request_browsertest.cc
+++ chrome/browser/extensions/api/declarative_net_request/declarative_net_request_browsertest.cc
@@ -31,7 +31,6 @@
 #include "components/proxy_config/proxy_config_pref_names.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_entry.h"
-#include "content/public/browser/notification_source.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_utils.h"
@@ -44,13 +43,12 @@
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/browser/extension_util.h"
-#include "extensions/browser/notification_types.h"
-#include "extensions/browser/runtime_data.h"
 #include "extensions/common/api/declarative_net_request/constants.h"
 #include "extensions/common/api/declarative_net_request/test_utils.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/extension_id.h"
 #include "extensions/common/url_pattern.h"
+#include "extensions/test/extension_test_message_listener.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/spawned_test_server/spawned_test_server.h"
 #include "net/test/test_data_directory.h"
@@ -131,6 +129,9 @@ class DeclarativeNetRequestBrowserTest
     host_resolver()->AddRule("*", "127.0.0.1");
 
     ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
+    background_page_ready_listener_ =
+        std::make_unique<ExtensionTestMessageListener>("ready",
+                                                       false /*will_reply*/);
   }
 
  protected:
@@ -184,6 +185,7 @@ class DeclarativeNetRequestBrowserTest
                             kJSONRulesFilename, rules, hosts,
                             has_background_script_);
 
+    background_page_ready_listener_->Reset();
     const Extension* extension = nullptr;
     switch (GetParam()) {
       case ExtensionLoadType::PACKED:
@@ -200,6 +202,10 @@ class DeclarativeNetRequestBrowserTest
 
     // Ensure the ruleset is also loaded on the IO thread.
     content::RunAllTasksUntilIdle();
+
+    // Wait for the background page to load if needed.
+    if (has_background_script_)
+      WaitForBackgroundScriptToLoad(extension->id());
 
     // Ensure no load errors were reported.
     EXPECT_TRUE(LoadErrorReporter::GetInstance()->GetErrors()->empty());
@@ -220,9 +226,16 @@ class DeclarativeNetRequestBrowserTest
                            {URLPattern::kAllUrlsPattern});
   }
 
+  void WaitForBackgroundScriptToLoad(const ExtensionId& extension_id) {
+    ASSERT_TRUE(background_page_ready_listener_->WaitUntilSatisfied());
+    ASSERT_EQ(extension_id,
+              background_page_ready_listener_->extension_id_for_message());
+  }
+
  private:
   base::ScopedTempDir temp_dir_;
   bool has_background_script_ = false;
+  std::unique_ptr<ExtensionTestMessageListener> background_page_ready_listener_;
 
   DISALLOW_COPY_AND_ASSIGN(DeclarativeNetRequestBrowserTest);
 };
@@ -1137,15 +1150,6 @@ IN_PROC_BROWSER_TEST_P(DeclarativeNetRequestBrowserTest_Packed,
   ASSERT_TRUE(dnr_extension);
   EXPECT_EQ("Test extension", dnr_extension->name());
 
-  // Ensure the background page is ready before dispatching the script to it.
-  if (!ExtensionSystem::Get(profile())->runtime_data()->IsBackgroundPageReady(
-          dnr_extension)) {
-    content::WindowedNotificationObserver(
-        NOTIFICATION_EXTENSION_BACKGROUND_PAGE_READY,
-        content::Source<Extension>(dnr_extension))
-        .Wait();
-  }
-
   // Whitelist "https://www.google.com/".
   const char* script1 = R"(
     chrome.declarativeNetRequest.addWhitelistedPages(
@@ -1191,13 +1195,7 @@ IN_PROC_BROWSER_TEST_P(DeclarativeNetRequestBrowserTest_Packed,
   ASSERT_TRUE(dnr_extension);
 
   // Ensure the background page is ready before dispatching the script to it.
-  if (!ExtensionSystem::Get(profile())->runtime_data()->IsBackgroundPageReady(
-          dnr_extension)) {
-    content::WindowedNotificationObserver(
-        NOTIFICATION_EXTENSION_BACKGROUND_PAGE_READY,
-        content::Source<Extension>(dnr_extension))
-        .Wait();
-  }
+  WaitForBackgroundScriptToLoad(dnr_extension->id());
 
   const char* script1 = R"(
     chrome.declarativeNetRequest.getWhitelistedPages(function(patterns) {
