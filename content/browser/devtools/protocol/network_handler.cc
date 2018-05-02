1471d6e12c77e1cca6196cae819311b3309c1ffd
Andrey Kosyakov <caseq@chromium.org>
2018-05-02 21:53:04 +0000

    Remove dead code from devtools/protocol/network_handler
    TBR=dgozman
    
    Change-Id: I9a8b4067f5adb9f70738d647cd636e02aed1186a
    Reviewed-on: https://chromium-review.googlesource.com/1040652
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555564}
    Former-commit-id: 34bad3c829c0b3faf9fea6b5edd5f7b3b7a44506

--- content/browser/devtools/protocol/network_handler.cc
+++ content/browser/devtools/protocol/network_handler.cc
@@ -1608,54 +1608,6 @@ void NetworkHandler::LoadingComplete(
       status.encoded_data_length);
 }
 
-void NetworkHandler::NavigationFailed(NavigationRequest* navigation_request) {
-  if (!enabled_)
-    return;
-
-  static int next_id = 0;
-  std::string request_id = base::IntToString(base::GetCurrentProcId()) + "." +
-                           base::IntToString(++next_id);
-  std::string error_string =
-      net::ErrorToString(navigation_request->net_error());
-  bool cancelled = navigation_request->net_error() == net::Error::ERR_ABORTED;
-
-  std::unique_ptr<DictionaryValue> headers_dict(DictionaryValue::create());
-  net::HttpRequestHeaders headers;
-  headers.AddHeadersFromString(navigation_request->begin_params()->headers);
-  for (net::HttpRequestHeaders::Iterator it(headers); it.GetNext();)
-    headers_dict->setString(it.name(), it.value());
-  frontend_->RequestWillBeSent(
-      request_id, "" /* loader_id */,
-      StripFragment(navigation_request->common_params().url),
-      Network::Request::Create()
-          .SetUrl(StripFragment(navigation_request->common_params().url))
-          .SetMethod(navigation_request->common_params().method)
-          .SetHeaders(Object::fromValue(headers_dict.get(), nullptr))
-          // Note: the priority value is copied from
-          // ResourceDispatcherHostImpl::BeginNavigationRequest but there isn't
-          // a good way of sharing this.
-          .SetInitialPriority(resourcePriority(net::HIGHEST))
-          .SetReferrerPolicy(referrerPolicy(
-              navigation_request->common_params().referrer.policy))
-          .Build(),
-      base::TimeTicks::Now().ToInternalValue() /
-          static_cast<double>(base::Time::kMicrosecondsPerSecond),
-      base::Time::Now().ToDoubleT(),
-      Network::Initiator::Create()
-          .SetType(Network::Initiator::TypeEnum::Parser)
-          .Build(),
-      std::unique_ptr<Network::Response>(),
-      std::string(Page::ResourceTypeEnum::Document),
-      Maybe<std::string>() /* frame_id */,
-      navigation_request->common_params().has_user_gesture);
-
-  frontend_->LoadingFailed(
-      request_id,
-      base::TimeTicks::Now().ToInternalValue() /
-          static_cast<double>(base::Time::kMicrosecondsPerSecond),
-      Page::ResourceTypeEnum::Document, error_string, cancelled);
-}
-
 DispatchResponse NetworkHandler::SetRequestInterception(
     std::unique_ptr<protocol::Array<protocol::Network::RequestPattern>>
         patterns) {
