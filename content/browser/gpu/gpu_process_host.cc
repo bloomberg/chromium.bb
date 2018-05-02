c60cc881787143d8fe4c412df717abc4d733610d
Scott Violet <sky@chromium.org>
2018-05-02 16:26:06 +0000

    Makes GpuProcessHost::BindInterface deal with DiscardableSharedMemoryManager
    This way code can call content::BindInterfaceInGpuProcess() and have
    it correctly wire up DiscardableSharedMemoryManager.
    
    BUG=837689
    TEST=none
    
    Change-Id: I93b1b5fb07bb0b1f0f38ebed03ca8d47c15d1b0d
    Reviewed-on: https://chromium-review.googlesource.com/1033961
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555420}
    Former-commit-id: 90f1b760f80df99495dfdda48e067ba9490e4fdb

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -582,6 +582,16 @@ void GpuProcessHost::CallOnIO(
 void GpuProcessHost::BindInterface(
     const std::string& interface_name,
     mojo::ScopedMessagePipeHandle interface_pipe) {
+  if (interface_name ==
+      discardable_memory::mojom::DiscardableSharedMemoryManager::Name_) {
+    BrowserThread::PostTask(
+        BrowserThread::UI, FROM_HERE,
+        base::BindOnce(
+            &BindDiscardableMemoryRequestOnUI,
+            discardable_memory::mojom::DiscardableSharedMemoryManagerRequest(
+                std::move(interface_pipe))));
+    return;
+  }
   process_->child_connection()->BindInterface(interface_name,
                                               std::move(interface_pipe));
 }
