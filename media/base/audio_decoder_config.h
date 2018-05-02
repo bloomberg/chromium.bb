74dcf251b89f2e860f03872eda31d29826397836
chcunningham <chcunningham@chromium.org>
2018-05-02 00:43:52 +0000

    (RELAND) media: Disable opus decode phase inversion for mono output.
    Encoders may use "phase inversion" to improve stereo compression. But
    this creates artifacts if the decoded stereo is later down-mixed to
    mono. This CL sets a flag on the decoder to disable the phase inversion
    decode step whenever the HW is detected as mono.
    
    Bug:806219
    TBR:dalecurtis@chromium.org
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Icff0387c7fc0fc0e0e31657176721cee5abf6fd9
    Reviewed-on: https://chromium-review.googlesource.com/1038617
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555257}
    Former-commit-id: 26bcc610d2d00379598554c1128319c05edc6767

--- media/base/audio_decoder_config.h
+++ media/base/audio_decoder_config.h
@@ -102,16 +102,31 @@ class MEDIA_EXPORT AudioDecoderConfig {
     should_discard_decoder_delay_ = false;
   }
 
+  // Optionally set by renderer to provide hardware layout when playback
+  // starts. Intentionally not part of IsValid(). Layout is not updated for
+  // device changes - use with care!
+  void set_target_output_channel_layout(ChannelLayout output_layout) {
+    target_output_channel_layout_ = output_layout;
+  }
+  ChannelLayout target_output_channel_layout() const {
+    return target_output_channel_layout_;
+  }
+
  private:
-  AudioCodec codec_;
-  SampleFormat sample_format_;
-  int bytes_per_channel_;
-  ChannelLayout channel_layout_;
-  int channels_;
-  int samples_per_second_;
-  int bytes_per_frame_;
+  AudioCodec codec_ = kUnknownAudioCodec;
+  SampleFormat sample_format_ = kUnknownSampleFormat;
+  int bytes_per_channel_ = 0;
+  int samples_per_second_ = 0;
+  int bytes_per_frame_ = 0;
   std::vector<uint8_t> extra_data_;
   EncryptionScheme encryption_scheme_;
+
+  // Layout and count of the *stream* being decoded.
+  ChannelLayout channel_layout_ = CHANNEL_LAYOUT_UNSUPPORTED;
+  int channels_ = 0;
+
+  // Layout of the output hardware. Optionally set. See setter comments.
+  ChannelLayout target_output_channel_layout_ = CHANNEL_LAYOUT_NONE;
 
   // |seek_preroll_| is the duration of the data that the decoder must decode
   // before the decoded data is valid.
@@ -120,11 +135,11 @@ class MEDIA_EXPORT AudioDecoderConfig {
   // |codec_delay_| is the number of frames the decoder should discard before
   // returning decoded data.  This value can include both decoder delay as well
   // as padding added during encoding.
-  int codec_delay_;
+  int codec_delay_ = 0;
 
   // Indicates if a decoder should implicitly discard decoder delay without it
   // being explicitly marked in discard padding.
-  bool should_discard_decoder_delay_;
+  bool should_discard_decoder_delay_ = true;
 
   // Not using DISALLOW_COPY_AND_ASSIGN here intentionally to allow the compiler
   // generated copy constructor and assignment operator. Since the extra data is
