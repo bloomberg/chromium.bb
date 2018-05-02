5fac7ba0b39b2886f3c358faa44595bcb2cc9b72
Kevin Marshall <kmarshall@chromium.org>
2018-05-02 21:13:10 +0000

    Reland "Fuchsia: Explicitly check that all file dependencies exist."
    This is a reland of 87b21b043553819a62bded99c1f57f0b22983565 [formerly 7891d057259d6df8483de1f03a39a6c202cca241]
    
    The underlying issue in crbug.com/838703 was fixed and
    "lint_test_expectations.py" Works On My Box(TM).
    
    TBR=scottmg@chromium.org
    
    Original change's description:
    > Fuchsia: Explicitly check that all file dependencies exist.
    >
    > Wrap file path references with "EnsureFileExists()" which acts as a
    > simple identity function, except for invalid paths for which it
    > raises IOErrors.
    >
    > Bug: 837200
    > Change-Id: Ie718cd5517415b375ed3bf5d5112d59b32074c04
    > Reviewed-on: https://chromium-review.googlesource.com/1037778
    > Reviewed-by: Scott Graham <scottmg@chromium.org>
    > Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#555137}
    
    Bug: 837200
    Change-Id: I21713adc9e8d9fcaab06957a25255de3ee6e596e
    Reviewed-on: https://chromium-review.googlesource.com/1038963
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555534}
    Former-commit-id: fe0226186fa4a13732cdbf2aa175cc318b882748

--- build/fuchsia/common.py
+++ build/fuchsia/common.py
@@ -8,3 +8,11 @@ DIR_SOURCE_ROOT = os.path.abspath(
     os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
 SDK_ROOT = os.path.join(DIR_SOURCE_ROOT, 'third_party', 'fuchsia-sdk', 'sdk')
 
+def EnsurePathExists(path):
+  """Checks that the file |path| exists on the filesystem and returns the path
+  if it does, raising an exception otherwise."""
+
+  if not os.path.exists(path):
+    raise IOError('Missing file: ' + path)
+
+  return path
