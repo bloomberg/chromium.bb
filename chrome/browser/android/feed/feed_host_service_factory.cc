f70bf893d71a1733ffa0e3767d4383ff790fad5e
Gang Wu <gangwu@chromium.org>
2018-05-03 18:55:30 +0000

    [Feed] FeedHostServiceFactory creates image loader
    FeedHostServiceFactory will create FeedImageManager when creates
    FeedHostService.
    
    Bug: 807359
    Change-Id: Ibe4718c382972d16de6081ce8b84309e5308557f
    Reviewed-on: https://chromium-review.googlesource.com/1041074
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555833}
    Former-commit-id: f4db7c47e437c2cb24488e61a8f718cdfb1bebe9

--- chrome/browser/android/feed/feed_host_service_factory.cc
+++ chrome/browser/android/feed/feed_host_service_factory.cc
@@ -8,17 +8,25 @@
 #include <string>
 #include <utility>
 
+#include "base/files/file_path.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/search/suggestions/image_decoder_impl.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/common/channel_info.h"
 #include "components/feed/core/feed_host_service.h"
+#include "components/image_fetcher/core/image_fetcher_impl.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/version_info/version_info.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/storage_partition.h"
 #include "google_apis/google_api_keys.h"
+#include "net/url_request/url_request_context_getter.h"
 
 namespace feed {
+
+namespace {
+const char kFeedFolder[] = "feed";
+}  // namespace
 
 // static
 FeedHostService* FeedHostServiceFactory::GetForBrowserContext(
@@ -42,6 +50,7 @@ FeedHostServiceFactory::~FeedHostServiceFactory() = default;
 KeyedService* FeedHostServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
   Profile* profile = Profile::FromBrowserContext(context);
+
   content::StoragePartition* storage_partition =
       content::BrowserContext::GetDefaultStoragePartition(context);
 
@@ -58,7 +67,20 @@ KeyedService* FeedHostServiceFactory::BuildServiceInstanceFor(
       identity_manager, api_key,
       storage_partition->GetURLLoaderFactoryForBrowserProcess());
 
-  return new FeedHostService(std::move(networking_host));
+  scoped_refptr<net::URLRequestContextGetter> request_context =
+      profile->GetRequestContext();
+
+  auto image_fetcher = std::make_unique<image_fetcher::ImageFetcherImpl>(
+      std::make_unique<suggestions::ImageDecoderImpl>(), request_context.get());
+
+  base::FilePath feed_dir(profile->GetPath().Append(kFeedFolder));
+  auto image_database = std::make_unique<FeedImageDatabase>(feed_dir);
+
+  auto image_manager = std::make_unique<FeedImageManager>(
+      std::move(image_fetcher), std::move(image_database));
+
+  return new FeedHostService(std::move(image_manager),
+                             std::move(networking_host));
 }
 
 content::BrowserContext* FeedHostServiceFactory::GetBrowserContextToUse(
