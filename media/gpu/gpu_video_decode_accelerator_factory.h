27a25c59d260674f78fd8a3712f1b1736926c00b
Dan Sanders <sandersd@chromium.org>
2018-05-04 20:04:24 +0000

    [media] Log to MediaLog from VTVDA.
    This moves logging of bitstream errors that were previously in the GPU
    log to the media log.
    
    To support async destruction correctly, VdaVideoDecoder now implements
    the MediaLog interface and handles thread hopping.
    
    Also includes some additional cleanups in VTVDA: improved log messages,
    increase the number of requested picture buffers to match recent changes
    in DXVAVD, and returns no supported profiles if VideoToolbox fails to
    initialize.
    
    Bug: 522298
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I7f13deb9e92fdeeeac60479e17081e72c5f22066
    Reviewed-on: https://chromium-review.googlesource.com/1038609
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556164}
    Former-commit-id: 39e7de240b63c0103078624d09130c4a990920d2

--- media/gpu/gpu_video_decode_accelerator_factory.h
+++ media/gpu/gpu_video_decode_accelerator_factory.h
@@ -32,6 +32,8 @@ class ContextGroup;
 
 namespace media {
 
+class MediaLog;
+
 class MEDIA_GPU_EXPORT GpuVideoDecodeAcceleratorFactory {
  public:
   ~GpuVideoDecodeAcceleratorFactory();
@@ -80,7 +82,8 @@ class MEDIA_GPU_EXPORT GpuVideoDecodeAcceleratorFactory {
       VideoDecodeAccelerator::Client* client,
       const VideoDecodeAccelerator::Config& config,
       const gpu::GpuDriverBugWorkarounds& workarounds,
-      const gpu::GpuPreferences& gpu_preferences);
+      const gpu::GpuPreferences& gpu_preferences,
+      MediaLog* media_log = nullptr);
 
  private:
   GpuVideoDecodeAcceleratorFactory(
@@ -93,33 +96,40 @@ class MEDIA_GPU_EXPORT GpuVideoDecodeAcceleratorFactory {
 #if defined(OS_WIN)
   std::unique_ptr<VideoDecodeAccelerator> CreateD3D11VDA(
       const gpu::GpuDriverBugWorkarounds& workarounds,
-      const gpu::GpuPreferences& gpu_preferences) const;
+      const gpu::GpuPreferences& gpu_preferences,
+      MediaLog* media_log) const;
   std::unique_ptr<VideoDecodeAccelerator> CreateDXVAVDA(
       const gpu::GpuDriverBugWorkarounds& workarounds,
-      const gpu::GpuPreferences& gpu_preferences) const;
+      const gpu::GpuPreferences& gpu_preferences,
+      MediaLog* media_log) const;
 #endif
 #if BUILDFLAG(USE_V4L2_CODEC)
   std::unique_ptr<VideoDecodeAccelerator> CreateV4L2VDA(
       const gpu::GpuDriverBugWorkarounds& workarounds,
-      const gpu::GpuPreferences& gpu_preferences) const;
+      const gpu::GpuPreferences& gpu_preferences,
+      MediaLog* media_log) const;
   std::unique_ptr<VideoDecodeAccelerator> CreateV4L2SVDA(
       const gpu::GpuDriverBugWorkarounds& workarounds,
-      const gpu::GpuPreferences& gpu_preferences) const;
+      const gpu::GpuPreferences& gpu_preferences,
+      MediaLog* media_log) const;
 #endif
 #if BUILDFLAG(USE_VAAPI)
   std::unique_ptr<VideoDecodeAccelerator> CreateVaapiVDA(
       const gpu::GpuDriverBugWorkarounds& workarounds,
-      const gpu::GpuPreferences& gpu_preferences) const;
+      const gpu::GpuPreferences& gpu_preferences,
+      MediaLog* media_log) const;
 #endif
 #if defined(OS_MACOSX)
   std::unique_ptr<VideoDecodeAccelerator> CreateVTVDA(
       const gpu::GpuDriverBugWorkarounds& workarounds,
-      const gpu::GpuPreferences& gpu_preferences) const;
+      const gpu::GpuPreferences& gpu_preferences,
+      MediaLog* media_log) const;
 #endif
 #if defined(OS_ANDROID)
   std::unique_ptr<VideoDecodeAccelerator> CreateAndroidVDA(
       const gpu::GpuDriverBugWorkarounds& workarounds,
-      const gpu::GpuPreferences& gpu_preferences) const;
+      const gpu::GpuPreferences& gpu_preferences,
+      MediaLog* media_log) const;
 #endif
 
   const GetGLContextCallback get_gl_context_cb_;
