44d1c83d1d31bf92c102b7a7fad0874d19435591
Johannes Henkel <johannes@chromium.org>
2018-05-04 00:01:24 +0000

    Rename to network_param_ipc_traits.{cc,h}.
    This was suggested by dcheng in
    https://chromium-review.googlesource.com/c/chromium/src/+/1038808.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I4e9de0aaf4d126de446b9134c7d1e6062d397385
    Reviewed-on: https://chromium-review.googlesource.com/1041189
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Johannes Henkel <johannes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555914}
    Former-commit-id: 31404166b59d2f71a2edb6fee313996b85ed19c3

--- services/network/public/cpp/url_loader_completion_status.h
+++ services/network/public/cpp/url_loader_completion_status.h
@@ -18,7 +18,7 @@
 namespace network {
 
 // NOTE: When adding/removing fields to this struct, don't forget to
-// update services/network/public/cpp/network_param_ipc_traits.h.
+// update services/network/public/cpp/network_ipc_param_traits.h.
 
 struct COMPONENT_EXPORT(NETWORK_CPP_BASE) URLLoaderCompletionStatus {
   URLLoaderCompletionStatus();
