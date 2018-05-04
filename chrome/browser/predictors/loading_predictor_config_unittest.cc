fb1acbc19551be79e630168de1e0dd20e4d5c681
Alexandr Ilin <alexilin@chromium.org>
2018-05-04 18:39:45 +0000

    Revert "predictors: Enable the new preconnect predictor by default"
    This reverts commit bc703efd307f336b42a0324f009c35e5ad2e7ee2 [formerly 5f1039f1025b5294133baab17a578d45c4431063].
    
    Reason for revert: multiple tests on Chrome OS became flaky because of disk I/O errors.
    
    Original change's description:
    > predictors: Enable the new preconnect predictor by default
    > 
    > The old predictor is automatically disabled when the new one is enabled.
    > 
    > Change-Id: Ia5c1a79dbeb0e096720fc3d50da3dc707b3761a6
    > Reviewed-on: https://chromium-review.googlesource.com/1042388
    > Reviewed-by: Egor Pasko <pasko@chromium.org>
    > Commit-Queue: Alexandr Ilin <alexilin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#555766}
    
    TBR=pasko@chromium.org,alexilin@chromium.org
    Bug: 839886
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: I8c286f29cee249990c57486851842e4d9dd900ac
    Reviewed-on: https://chromium-review.googlesource.com/1044326
    Reviewed-by: Alexandr Ilin <alexilin@chromium.org>
    Commit-Queue: Alexandr Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556130}
    Former-commit-id: cc89d6bebc0eb8d95e8aa72b4d9852020d4257da

--- chrome/browser/predictors/loading_predictor_config_unittest.cc
+++ chrome/browser/predictors/loading_predictor_config_unittest.cc
@@ -66,14 +66,12 @@ class LoadingPredictorConfigTest : public testing::Test {
 LoadingPredictorConfigTest::LoadingPredictorConfigTest()
     : profile_(new TestingProfile()) {}
 
-TEST_F(LoadingPredictorConfigTest, IsEnabledByDefault) {
+TEST_F(LoadingPredictorConfigTest, IsDisabledByDefault) {
   LoadingPredictorConfig config;
-  EXPECT_TRUE(MaybeEnableSpeculativePreconnect(&config));
+  EXPECT_FALSE(MaybeEnableSpeculativePreconnect(&config));
 
-  EXPECT_TRUE(config.IsLearningEnabled());
-  EXPECT_TRUE(config.should_disable_other_preconnects);
-  EXPECT_FALSE(IsNetPredictorEnabled());
-  EXPECT_TRUE(config.IsPreconnectEnabledForSomeOrigin(profile_.get()));
+  EXPECT_FALSE(config.IsLearningEnabled());
+  EXPECT_FALSE(config.IsPreconnectEnabledForSomeOrigin(profile_.get()));
 }
 
 TEST_F(LoadingPredictorConfigTest, EnablePreconnectLearning) {
