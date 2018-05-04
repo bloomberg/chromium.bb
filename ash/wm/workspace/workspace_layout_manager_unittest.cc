90818e9e05c8c6093b1f8749b8b76999d29758f7
Min Chen <minch@google.com>
2018-05-04 18:44:38 +0000

    Disable WorkspaceLayoutManagerBackdropTest.OpenAppListInOverviewMode in asan.
    Bug: 838756, 838822, 837092
    Change-Id: I9f7fcb79a8305632ca264324f69aee1ae7d2ac71
    Reviewed-on: https://chromium-review.googlesource.com/1042868
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556132}
    Former-commit-id: 6d730ee7e25d0c76ea769914442de439f32c3611

--- ash/wm/workspace/workspace_layout_manager_unittest.cc
+++ ash/wm/workspace/workspace_layout_manager_unittest.cc
@@ -1547,8 +1547,13 @@ TEST_F(WorkspaceLayoutManagerBackdropTest,
   GetAppListTestHelper()->Dismiss();
 }
 
-// TODO(minch): This test has a high flake rate. https://crbug.com/838756
-TEST_F(WorkspaceLayoutManagerBackdropTest, DISABLED_OpenAppListInOverviewMode) {
+// TODO(crbug.com/803286): The npot texture check failed on asan tests bot.
+#if defined(ADDRESS_SANITIZER)
+#define MAYBE_OpenAppListInOverviewMode DISABLED_OpenAppListInOverviewMode
+#else
+#define MAYBE_OpenAppListInOverviewMode OpenAppListInOverviewMode
+#endif
+TEST_F(WorkspaceLayoutManagerBackdropTest, MAYBE_OpenAppListInOverviewMode) {
   WorkspaceController* wc = ShellTestApi(Shell::Get()).workspace_controller();
   WorkspaceControllerTestApi test_helper(wc);
 
