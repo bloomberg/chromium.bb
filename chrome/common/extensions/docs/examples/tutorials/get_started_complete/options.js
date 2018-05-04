4df98fe213eff3305f4c9c9502cea10dc9ae92d6
crystallambert@chromium.org <crystallambert@chromium.org>
2018-05-04 16:15:35 +0000

    [Extension Documentation]Get Started Options Bug
    Updated options page on get started tutorial
    and complete sample extension.
    
    Bug: 839545
    Change-Id: I13cf017d1bf23e5a9eafe32cf8efe0eda214990f
    Reviewed-on: https://chromium-review.googlesource.com/1044274
    Commit-Queue: Crystal Lambert <crystallambert@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556078}
    Former-commit-id: 79f487c6c81a05693b1a4a9e4d2899e44db7d4a2

--- chrome/common/extensions/docs/examples/tutorials/get_started_complete/options.js
+++ chrome/common/extensions/docs/examples/tutorials/get_started_complete/options.js
@@ -4,6 +4,7 @@
 
 'use strict';
 
+let page = document.getElementById('buttonDiv');
 const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
 function constructOptions(kButtonColors) {
   for (let item of kButtonColors) {
