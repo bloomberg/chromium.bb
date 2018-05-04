98dd5d4c9febf671ddb34674f38a612a24ed0ee3
Scott Violet <sky@chromium.org>
2018-05-04 00:02:31 +0000

    Makes ws2/WindowService a real service
    This way it can be responsible for various interfaces that need to be
    registered. This also makes it easier to use in consuming code. I
    deleted from the README with the expectation that I'll update it later
    on once things are further along.
    
    BUG=837689
    TEST=none
    
    Change-Id: I07f8f2902d02a6f2a8183bf141855494661a33d3
    Reviewed-on: https://chromium-review.googlesource.com/1042879
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#555916}
    Former-commit-id: 890faecec1d6143b39689417b4e1376341290797

--- services/ui/ws2/window_service.cc
+++ services/ui/ws2/window_service.cc
@@ -4,14 +4,20 @@
 
 #include "services/ui/ws2/window_service.h"
 
+#include "base/bind.h"
+#include "base/single_thread_task_runner.h"
+#include "services/ui/ws2/gpu_support.h"
 #include "services/ui/ws2/window_data.h"
 #include "services/ui/ws2/window_service_client.h"
+#include "services/ui/ws2/window_service_delegate.h"
+#include "services/ui/ws2/window_tree_factory.h"
 
 namespace ui {
 namespace ws2 {
 
-WindowService::WindowService(WindowServiceDelegate* delegate)
-    : delegate_(delegate) {}
+WindowService::WindowService(WindowServiceDelegate* delegate,
+                             std::unique_ptr<GpuSupport> gpu_support)
+    : delegate_(delegate), gpu_support_(std::move(gpu_support)) {}
 
 WindowService::~WindowService() {}
 
@@ -34,6 +40,60 @@ std::unique_ptr<WindowServiceClient> WindowService::CreateWindowServiceClient(
   CHECK_NE(0u, next_client_id_);
   return std::make_unique<WindowServiceClient>(
       this, client_id, window_tree_client, intercepts_events);
+}
+
+void WindowService::OnStart() {
+  window_tree_factory_ = std::make_unique<WindowTreeFactory>(this);
+
+  registry_.AddInterface(base::BindRepeating(
+      &WindowService::BindClipboardRequest, base::Unretained(this)));
+  registry_.AddInterface(base::BindRepeating(
+      &WindowService::BindDisplayManagerRequest, base::Unretained(this)));
+  registry_.AddInterface(base::BindRepeating(
+      &WindowService::BindImeDriverRequest, base::Unretained(this)));
+  registry_.AddInterface(base::BindRepeating(
+      &WindowService::BindWindowTreeFactoryRequest, base::Unretained(this)));
+
+  // |gpu_support_| may be null in tests.
+  if (gpu_support_) {
+    registry_.AddInterface(
+        base::BindRepeating(
+            &GpuSupport::BindDiscardableSharedMemoryManagerOnGpuTaskRunner,
+            base::Unretained(gpu_support_.get())),
+        gpu_support_->GetGpuTaskRunner());
+    registry_.AddInterface(
+        base::BindRepeating(&GpuSupport::BindGpuRequestOnGpuTaskRunner,
+                            base::Unretained(gpu_support_.get())),
+        gpu_support_->GetGpuTaskRunner());
+  }
+}
+
+void WindowService::OnBindInterface(
+    const service_manager::BindSourceInfo& remote_info,
+    const std::string& interface_name,
+    mojo::ScopedMessagePipeHandle handle) {
+  registry_.BindInterface(interface_name, std::move(handle));
+}
+
+void WindowService::BindClipboardRequest(mojom::ClipboardRequest request) {
+  // TODO: https://crbug.com/839591.
+  NOTIMPLEMENTED();
+}
+
+void WindowService::BindDisplayManagerRequest(
+    mojom::DisplayManagerRequest request) {
+  // TODO: https://crbug.com/839592.
+  NOTIMPLEMENTED();
+}
+
+void WindowService::BindImeDriverRequest(mojom::IMEDriverRequest request) {
+  // TODO: https://crbug.com/837710.
+  NOTIMPLEMENTED();
+}
+
+void WindowService::BindWindowTreeFactoryRequest(
+    ui::mojom::WindowTreeFactoryRequest request) {
+  window_tree_factory_->AddBinding(std::move(request));
 }
 
 }  // namespace ws2
