ce6a560cd75d8041de32c14d21d41b328ee6ff23
Guido Urdaneta <guidou@chromium.org>
2018-05-07 11:00:42 +0000

    Disable flaky HeadlessRenderBrowserTests on Linux
    TBR=eseckler@chromium.org
    
    Bug: 839747
    Change-Id: Ic98a3b585163f663c983403d34d958460658c884
    Reviewed-on: https://chromium-review.googlesource.com/1046597
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556410}
    Former-commit-id: fb054ef275af619e1790098622908e2d0d992ff9

--- headless/test/headless_render_browsertest.cc
+++ headless/test/headless_render_browsertest.cc
@@ -510,7 +510,13 @@ class ServerRedirectToFailure : public HeadlessRenderTest {
                     "http://www.example.com/FAIL"));
   }
 };
-HEADLESS_RENDER_BROWSERTEST(ServerRedirectToFailure);
+// Flaky on Linux. https://crbug.com/839747
+#if defined(OS_LINUX)
+#define MAYBE_HEADLESS_RENDER_BROWSERTEST DISABLED_HEADLESS_RENDER_BROWSERTEST
+#else
+#define MAYBE_HEADLESS_RENDER_BROWSERTEST HEADLESS_RENDER_BROWSERTEST
+#endif
+MAYBE_HEADLESS_RENDER_BROWSERTEST(ServerRedirectToFailure);
 
 class ServerRedirectRelativeChain : public HeadlessRenderTest {
  private:
@@ -1001,7 +1007,8 @@ class RedirectInvalidUrl : public HeadlessRenderTest {
                 ElementsAre("http://www.example.com/"));
   }
 };
-HEADLESS_RENDER_BROWSERTEST(RedirectInvalidUrl);
+// Flaky on Linux. https://crbug.com/839747
+MAYBE_HEADLESS_RENDER_BROWSERTEST(RedirectInvalidUrl);
 
 class RedirectKeepsFragment : public HeadlessRenderTest {
  private:
@@ -1164,11 +1171,6 @@ class CookieSetFromJs_NoCookies : public CookieSetFromJs {
 };
 
 // Flaky on Linux. https://crbug.com/839747
-#if defined(OS_LINUX)
-#define MAYBE_HEADLESS_RENDER_BROWSERTEST DISABLED_HEADLESS_RENDER_BROWSERTEST
-#else
-#define MAYBE_HEADLESS_RENDER_BROWSERTEST HEADLESS_RENDER_BROWSERTEST
-#endif
 MAYBE_HEADLESS_RENDER_BROWSERTEST(CookieSetFromJs_NoCookies);
 
 class CookieUpdatedFromJs : public HeadlessRenderTest {
