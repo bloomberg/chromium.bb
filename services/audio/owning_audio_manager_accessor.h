fd98245cb9d5f55fb8c647ba7c9816ce0dd07d36
Max Morin <maxmorin@chromium.org>
2018-05-07 08:32:44 +0000

    Use FakeAudioLogFactory when out-of-process.
    This facilitates further development until logging stuff is ready.
    
    No-Try since last PS only added a comment.
    
    No-Try: true
    Bug: 812557
    Change-Id: I2121ea9d485c7db999b8d89d9fa0039ef9ab8136
    Reviewed-on: https://chromium-review.googlesource.com/1046206
    Commit-Queue: Max Morin <maxmorin@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556392}
    Former-commit-id: 7c90637ba27fd4d5300abe83c85ba21ad355f240

--- services/audio/owning_audio_manager_accessor.h
+++ services/audio/owning_audio_manager_accessor.h
@@ -5,9 +5,12 @@
 #ifndef SERVICES_AUDIO_OWNING_AUDIO_MANAGER_ACCESSOR_H_
 #define SERVICES_AUDIO_OWNING_AUDIO_MANAGER_ACCESSOR_H_
 
+#include <memory>
+
 #include "base/macros.h"
 #include "base/threading/thread_checker.h"
 #include "build/build_config.h"
+#include "media/audio/fake_audio_log_factory.h"
 #include "services/audio/service.h"
 
 #if defined(OS_WIN)
@@ -46,6 +49,11 @@ class OwningAudioManagerAccessor : public Service::AudioManagerAccessor {
 #endif
   AudioManagerFactoryCallback audio_manager_factory_cb_;
   std::unique_ptr<media::AudioManager> audio_manager_;
+
+  // TODO(http://crbug/812557): Use a real AudioLogFactory (needed for output
+  // streams).
+  media::FakeAudioLogFactory log_factory_;
+
   THREAD_CHECKER(thread_checker_);
   DISALLOW_COPY_AND_ASSIGN(OwningAudioManagerAccessor);
 };
