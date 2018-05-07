058e6fcbdce9079f7fa1292c0b9287af8d8020ba
Ryan Daum <rdaum@chromium.org>
2018-05-07 14:20:47 +0000

    Fixes to propagation of touch events in system gesture handling.
      - Switches from detecting fling gestures to detecting touch events,
        making it possible to prevent the initial ET_TOUCH_PRESSED event
        from propagating (b/78461207)
      - Uses new handler interface, which includes HandleSideSwipeContinue,
        to allow gradual exposure of the bottom settings bar (b/78462154) and
        to allow using X delta moved instead of the gesture end event for back
        gesture detection in the future (b/78397215)
    
    Bug: b/78462154
    Bug: b/78461207
    Change-Id: Ic64e13825eaaa5cf8420e94df767a057563a8281
    Tested: manual on device and unit test
    Reviewed-on: https://chromium-review.googlesource.com/1030614
    Commit-Queue: Ryan Daum <rdaum@chromium.org>
    Reviewed-by: Alex Sakhartchouk <alexst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556433}
    Former-commit-id: d1c002911030ccb12940ad01444ecd4a83f69511

--- chromecast/graphics/cast_system_gesture_event_handler.cc
+++ chromecast/graphics/cast_system_gesture_event_handler.cc
@@ -66,65 +66,79 @@ CastSideSwipeOrigin CastSystemGestureEventHandler::GetDragPosition(
   return CastSideSwipeOrigin::NONE;
 }
 
-void CastSystemGestureEventHandler::OnEvent(ui::Event* event) {
-  if (current_swipe_ != CastSideSwipeOrigin::NONE &&
-      (event->IsTouchEvent() || event->IsGestureEvent())) {
-    // If we're in the process of handling a swipe, prevent the underlying touch
-    // or gesture event from being propagated out to other users.
-    event->StopPropagation();
-  }
-
-  // From here-on in, we're only interested in gestures.
-  if (!event->IsGestureEvent()) {
+void CastSystemGestureEventHandler::OnTouchEvent(ui::TouchEvent* event) {
+  if (swipe_gesture_handlers_.empty()) {
     return;
   }
-  ui::GestureEvent* gesture_event = event->AsGestureEvent();
-  gfx::Point gesture_location(gesture_event->location());
-  aura::Window* target = static_cast<aura::Window*>(event->target());
 
+  gfx::Point touch_location(event->location());
+  aura::Window* target = static_cast<aura::Window*>(event->target());
   // Convert the event's point to the point on the physical screen.
   // For cast on root window this is likely going to be identical, but put it
   // through the ScreenPositionClient just to be sure.
-  ::wm::ConvertPointToScreen(target, &gesture_location);
+  ::wm::ConvertPointToScreen(target, &touch_location);
   gfx::Rect screen_bounds = display::Screen::GetScreen()
-                                ->GetDisplayNearestPoint(gesture_location)
+                                ->GetDisplayNearestPoint(touch_location)
                                 .bounds();
   CastSideSwipeOrigin side_swipe_origin =
-      GetDragPosition(gesture_location, screen_bounds);
+      GetDragPosition(touch_location, screen_bounds);
 
-  // Detect the beginning of a system gesture swipe.
-  if (side_swipe_origin != CastSideSwipeOrigin::NONE &&
-      (event->type() == ui::ET_SCROLL_FLING_START ||
-       event->type() == ui::ET_GESTURE_BEGIN)) {
+  // A located event has occurred inside the margin. It might be the start of
+  // our gesture, or a touch that we need to squash.
+  if (current_swipe_ == CastSideSwipeOrigin::NONE &&
+      side_swipe_origin != CastSideSwipeOrigin::NONE) {
+    // Check to see if we have any potential consumers of events on this side.
+    // If not, we can continue on without consuming it.
+    bool have_swipe_consumer = false;
     for (auto* side_swipe_handler : swipe_gesture_handlers_) {
-      // Let the subscriber know about the swipe. If it is actually consumed by
-      // them, it will be marked as handled.
-      side_swipe_handler->OnSideSwipeBegin(side_swipe_origin, gesture_event);
-
-      // If handled, remember the origin and then stop the further propagation
-      // of the event.
-      if (event->handled()) {
-        // Record the swipe origin to properly fire OnSideSwipeEnd when the
-        // swipe gesture finishes.
-        current_swipe_ = side_swipe_origin;
-        event->StopPropagation();
-
+      if (side_swipe_handler->CanHandleSwipe(side_swipe_origin)) {
+        have_swipe_consumer = true;
         break;
       }
     }
+    if (!have_swipe_consumer) {
+      return;
+    }
+
+    // All touch or gesture events inside the margins have to be consumed, or we
+    // risk a state issue later when the touch ends (b/78461207)
+    event->StopPropagation();
+
+    // Detect the beginning of a system gesture swipe.
+    if (event->type() == ui::ET_TOUCH_PRESSED) {
+      current_swipe_ = side_swipe_origin;
+      for (auto* side_swipe_handler : swipe_gesture_handlers_) {
+        // Let the subscriber know about the gesture begin.
+        side_swipe_handler->HandleSideSwipeBegin(side_swipe_origin,
+                                                 touch_location);
+      }
+    }
+
     return;
   }
 
-  // Detect the end of a system gesture swipe.
-  if (current_swipe_ != CastSideSwipeOrigin::NONE &&
-      event->type() == ui::ET_GESTURE_END) {
+  if (current_swipe_ == CastSideSwipeOrigin::NONE) {
+    return;
+  }
+
+  // A swipe is in progress, or has completed, so stop propagation of underlying
+  // gesture/touch events.
+  event->StopPropagation();
+
+  // The system gesture has ended.
+  if (event->type() == ui::ET_TOUCH_RELEASED) {
     for (auto* side_swipe_handler : swipe_gesture_handlers_) {
-      side_swipe_handler->OnSideSwipeEnd(current_swipe_, gesture_event);
+      side_swipe_handler->HandleSideSwipeEnd(current_swipe_, touch_location);
     }
     current_swipe_ = CastSideSwipeOrigin::NONE;
 
-    // Prevent the gesture from being used for other gesture uses.
-    target->CleanupGestureState();
+    return;
+  }
+
+  // The system gesture is ongoing...
+  for (auto* side_swipe_handler : swipe_gesture_handlers_) {
+    // Let the subscriber know about the gesture begin.
+    side_swipe_handler->HandleSideSwipeContinue(current_swipe_, touch_location);
   }
 }
 
