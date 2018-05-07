afe19743803758e4be1f6ea14492b897b10464b6
Yunqing Wang <yunqingwang@google.com>
2018-05-07 16:51:11 -0700

    Fix the segmentation fault in the encoder thread test
    The multi-thread loop filter didn't work correctly in the encoder,
    and was disabled in the encoder. This should fix the issue for
    now, and the multi-thread loop filter will need to be fixed to
    work in the encoder.
    
    BUG=aomedia:1856
    
    Change-Id: I0471e26a058ada9590ab5ea7956c22066278500a

--- av1/encoder/picklpf.c
+++ av1/encoder/picklpf.c
@@ -67,13 +67,16 @@ static int64_t try_filter_frame(const YV12_BUFFER_CONFIG *sd,
     case 2: cm->lf.filter_level_v = filter_level[0]; break;
   }
 
-  if (cpi->num_workers > 1)
-    av1_loop_filter_frame_mt(cm->frame_to_show, cm, &cpi->td.mb.e_mbd, plane,
-                             plane + 1, partial_frame, cpi->workers,
-                             cpi->num_workers, &cpi->lf_row_sync);
-  else
-    av1_loop_filter_frame(cm->frame_to_show, cm, &cpi->td.mb.e_mbd, plane,
-                          plane + 1, partial_frame);
+  // TODO(Deepa, Yunqing): The multi-thread loopfilter doesn't work in the
+  // encoder.
+  //  if (cpi->num_workers > 1)
+  //    av1_loop_filter_frame_mt(cm->frame_to_show, cm, &cpi->td.mb.e_mbd,
+  //    plane,
+  //                             plane + 1, partial_frame, cpi->workers,
+  //                             cpi->num_workers, &cpi->lf_row_sync);
+  //  else
+  av1_loop_filter_frame(cm->frame_to_show, cm, &cpi->td.mb.e_mbd, plane,
+                        plane + 1, partial_frame);
 
   int highbd = 0;
   highbd = cm->use_highbitdepth;
