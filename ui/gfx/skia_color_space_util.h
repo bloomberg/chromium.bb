c4420adaf41710b135c070743e8c2450a7be7f6f
Brian Osman <brianosman@google.com>
2018-05-08 17:47:49 +0000

    Switch to using skcms for ICC profile parsing
    Change-Id: I8bafe64117d7247b7806e1d64d6e7e793078877c
    Reviewed-on: https://chromium-review.googlesource.com/1028452
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Reviewed-by: Mike Klein <mtklein@chromium.org>
    Commit-Queue: Mike Klein <mtklein@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556861}
    Former-commit-id: e6ae8f4c3bf66e5bf6641711062c15ea61df9cd2

--- ui/gfx/skia_color_space_util.h
+++ ui/gfx/skia_color_space_util.h
@@ -31,21 +31,6 @@ SkTransferFnsApproximatelyCancel(const SkColorSpaceTransferFn& a,
 bool COLOR_SPACE_EXPORT
 SkTransferFnIsApproximatelyIdentity(const SkColorSpaceTransferFn& fn);
 
-// Approximates the |n| points in |x| and |t| by the transfer function |fn|.
-// Returns true if the approximation converged.
-bool COLOR_SPACE_EXPORT SkApproximateTransferFn(const float* x,
-                                                const float* t,
-                                                size_t n,
-                                                SkColorSpaceTransferFn* fn);
-
-// Approximates |sk_icc| by the transfer function |fn|. Returns in |max_error|
-// the maximum pointwise of all color channels' transfer functions with |fn|.
-// Returns false if no approximation was possible, or no approximation
-// converged.
-bool COLOR_SPACE_EXPORT SkApproximateTransferFn(sk_sp<SkICC> sk_icc,
-                                                float* max_error,
-                                                SkColorSpaceTransferFn* fn);
-
 bool COLOR_SPACE_EXPORT SkMatrixIsApproximatelyIdentity(const SkMatrix44& m);
 
 }  // namespace gfx
