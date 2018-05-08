b2d3ac329f2006ad8bab74e10f86358fadf5e8fc
Xu Xing <xing.xu@intel.com>
2018-05-08 02:14:22 +0000

    viz: Remove TestWebGraphicsContext3D in DisplayTest and ShaderTest-Step 1a
    BUG=837155
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: I0ee28059ab44ace3fd066d500a57cbde252863d7
    Reviewed-on: https://chromium-review.googlesource.com/1046371
    Commit-Queue: Xing Xu <xing.xu@intel.com>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556651}
    Former-commit-id: 4162f59ed9e38c27e971a9ba3a182e905206a818

--- components/viz/service/display/shader_unittest.cc
+++ components/viz/service/display/shader_unittest.cc
@@ -4,8 +4,8 @@
 
 #include "components/viz/service/display/shader.h"
 
+#include "components/viz/test/test_context_provider.h"
 #include "components/viz/test/test_gles2_interface.h"
-#include "components/viz/test/test_web_graphics_context_3d.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/size.h"
@@ -13,12 +13,11 @@
 namespace viz {
 
 TEST(ShaderTest, HighpThresholds) {
-  // The test context always uses a mediump precision of 10 bits which
+  // The test gl always uses a mediump precision of 10 bits which
   // corresponds to a native highp threshold of 2^10 = 1024
-  std::unique_ptr<TestWebGraphicsContext3D> test_context =
-      TestWebGraphicsContext3D::Create();
-  TestGLES2Interface test_gl;
-  test_gl.set_test_context(test_context.get());
+  scoped_refptr<TestContextProvider> provider = TestContextProvider::Create();
+  provider->BindToCurrentThread();
+  gpu::gles2::GLES2Interface* test_gl = provider->ContextGL();
 
   int threshold_cache = 0;
   int threshold_min;
@@ -29,30 +28,30 @@ TEST(ShaderTest, HighpThresholds) {
 
   threshold_min = 0;
   EXPECT_EQ(TEX_COORD_PRECISION_MEDIUM,
-            TexCoordPrecisionRequired(&test_gl, &threshold_cache, threshold_min,
+            TexCoordPrecisionRequired(test_gl, &threshold_cache, threshold_min,
                                       closePoint));
   EXPECT_EQ(TEX_COORD_PRECISION_MEDIUM,
-            TexCoordPrecisionRequired(&test_gl, &threshold_cache, threshold_min,
+            TexCoordPrecisionRequired(test_gl, &threshold_cache, threshold_min,
                                       smallSize));
   EXPECT_EQ(TEX_COORD_PRECISION_HIGH,
-            TexCoordPrecisionRequired(&test_gl, &threshold_cache, threshold_min,
+            TexCoordPrecisionRequired(test_gl, &threshold_cache, threshold_min,
                                       farPoint));
   EXPECT_EQ(TEX_COORD_PRECISION_HIGH,
-            TexCoordPrecisionRequired(&test_gl, &threshold_cache, threshold_min,
+            TexCoordPrecisionRequired(test_gl, &threshold_cache, threshold_min,
                                       bigSize));
 
   threshold_min = 3000;
   EXPECT_EQ(TEX_COORD_PRECISION_MEDIUM,
-            TexCoordPrecisionRequired(&test_gl, &threshold_cache, threshold_min,
+            TexCoordPrecisionRequired(test_gl, &threshold_cache, threshold_min,
                                       closePoint));
   EXPECT_EQ(TEX_COORD_PRECISION_MEDIUM,
-            TexCoordPrecisionRequired(&test_gl, &threshold_cache, threshold_min,
+            TexCoordPrecisionRequired(test_gl, &threshold_cache, threshold_min,
                                       smallSize));
   EXPECT_EQ(TEX_COORD_PRECISION_MEDIUM,
-            TexCoordPrecisionRequired(&test_gl, &threshold_cache, threshold_min,
+            TexCoordPrecisionRequired(test_gl, &threshold_cache, threshold_min,
                                       farPoint));
   EXPECT_EQ(TEX_COORD_PRECISION_MEDIUM,
-            TexCoordPrecisionRequired(&test_gl, &threshold_cache, threshold_min,
+            TexCoordPrecisionRequired(test_gl, &threshold_cache, threshold_min,
                                       bigSize));
 }
 
