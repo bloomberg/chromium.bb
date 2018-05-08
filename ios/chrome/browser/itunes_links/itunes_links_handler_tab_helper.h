ef41393b62eb508241c0f75e4b55454979ea7493
mrefaat <mrefaat@chromium.org>
2018-05-08 17:24:00 +0000

    Block iTunes Urls requests for Storekit handling
    This should fix the problem with sending 2 requests to iTunes servers
    with cases of StoreKit handling.
    
    Bug: 838359
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I14ef3b3053391187d34885be939d598827e7bb6d
    Reviewed-on: https://chromium-review.googlesource.com/1043648
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556848}
    Former-commit-id: 805ee5101d3ea63ec75e53b350bae7381670ad97

--- ios/chrome/browser/itunes_links/itunes_links_handler_tab_helper.h
+++ ios/chrome/browser/itunes_links/itunes_links_handler_tab_helper.h
@@ -6,12 +6,8 @@
 #define IOS_CHROME_BROWSER_ITUNES_LINKS_ITUNES_LINKS_HANDLER_TAB_HELPER_H_
 
 #include "base/macros.h"
-#include "ios/web/public/web_state/web_state_observer.h"
+#import "ios/web/public/web_state/web_state_policy_decider.h"
 #include "ios/web/public/web_state/web_state_user_data.h"
-
-namespace web {
-class WebStatePolicyDecider;
-}
 
 // Enum for the IOS.StoreKit.ITunesURLsHandlingResult UMA histogram to report
 // the results of the StoreKit handling.
@@ -31,28 +27,26 @@ enum class ITunesUrlsStoreKitHandlingResult {
   kCount
 };
 
-// TabHelper which handles navigation to iTunes links.
-// If a navigation to web page for a product in iTunes App Store happens while
-// in non off the record browsing mode, this helper will use StoreKitTabHelper
-// to present the information of that product. The goal of this class is to
-// workaround a bug where appstore website serves the wrong content for
-// itunes.apple.com pages, see http://crbug.com/623016.
+// A Tab helper for iTunes Apps URLs handling.
+// If a navigation to web page for a supported product in iTunes App Store
+// happens while in non off the record browsing mode, this helper will use
+// StoreKitTabHelper to present the information of that product. The goal of
+// this class is to workaround a bug where appstore website serves the wrong
+// content for itunes.apple.com pages, see http://crbug.com/623016.
 class ITunesLinksHandlerTabHelper
-    : public web::WebStateObserver,
+    : public web::WebStatePolicyDecider,
       public web::WebStateUserData<ITunesLinksHandlerTabHelper> {
  public:
   ~ITunesLinksHandlerTabHelper() override;
   explicit ITunesLinksHandlerTabHelper(web::WebState* web_state);
+  // web::WebStatePolicyDecider implementation
+  bool ShouldAllowRequest(NSURLRequest* request,
+                          ui::PageTransition transition,
+                          bool from_main_frame) override;
 
  private:
-  // web::WebStateObserver implementation
-  void DidFinishNavigation(web::WebState* web_state,
-                           web::NavigationContext* navigation_context) override;
-  void WebStateDestroyed(web::WebState* web_state) override;
-
-  // PolicyDecider instance that will be initialized with the
-  // ITunesLinkHandlerTabHelper object, and destroyed with it.
-  std::unique_ptr<web::WebStatePolicyDecider> policy_decider_;
+  // Opens the StoreKit for the given iTunes app |url|.
+  void HandleITunesUrl(const GURL& url);
 
   DISALLOW_COPY_AND_ASSIGN(ITunesLinksHandlerTabHelper);
 };
