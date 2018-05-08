70b82ceac7abaa5058fe0472eeb775492cdf80a8
Yuri Wiitala <miu@chromium.org>
2018-05-08 07:39:23 +0000

    The SnooperNode: Audio loopback for a single stream.
    An audio::GroupMember::Snooper that records the audio from a GroupMember
    on one thread, and re-renders it to the desired output format on another
    thread. Since the data flow rates are known to be driven by different
    clocks (audio hardware clock versus system clock), the SnooperNode also
    uses its resampler to compensate for skew and re-synchronize the audio
    going into and out of it.
    
    Bug: 824019
    Change-Id: I87d410724fd00f9372232bfffdfbb89ada0b3de8
    Reviewed-on: https://chromium-review.googlesource.com/1041657
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: Xiangjun Zhang <xjz@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556719}
    Former-commit-id: f1b4c43b85e7cec2a61da121b9244deffec38d03

--- media/base/channel_mixer.cc
+++ media/base/channel_mixer.cc
@@ -42,12 +42,20 @@ void ChannelMixer::Initialize(
 ChannelMixer::~ChannelMixer() = default;
 
 void ChannelMixer::Transform(const AudioBus* input, AudioBus* output) {
+  CHECK_EQ(input->frames(), output->frames());
+  TransformPartial(input, input->frames(), output);
+}
+
+void ChannelMixer::TransformPartial(const AudioBus* input,
+                                    int frame_count,
+                                    AudioBus* output) {
   CHECK_EQ(matrix_.size(), static_cast<size_t>(output->channels()));
   CHECK_EQ(matrix_[0].size(), static_cast<size_t>(input->channels()));
-  CHECK_EQ(input->frames(), output->frames());
+  CHECK_LE(frame_count, input->frames());
+  CHECK_LE(frame_count, output->frames());
 
   // Zero initialize |output| so we're accumulating from zero.
-  output->Zero();
+  output->ZeroFrames(frame_count);
 
   // If we're just remapping we can simply copy the correct input to output.
   if (remapping_) {
@@ -57,7 +65,7 @@ void ChannelMixer::Transform(const AudioBus* input, AudioBus* output) {
         if (scale > 0) {
           DCHECK_EQ(scale, 1.0f);
           memcpy(output->channel(output_ch), input->channel(input_ch),
-                 sizeof(*output->channel(output_ch)) * output->frames());
+                 sizeof(*output->channel(output_ch)) * frame_count);
           break;
         }
       }
@@ -71,7 +79,7 @@ void ChannelMixer::Transform(const AudioBus* input, AudioBus* output) {
       // Scale should always be positive.  Don't bother scaling by zero.
       DCHECK_GE(scale, 0);
       if (scale > 0) {
-        vector_math::FMAC(input->channel(input_ch), scale, output->frames(),
+        vector_math::FMAC(input->channel(input_ch), scale, frame_count,
                           output->channel(output_ch));
       }
     }
