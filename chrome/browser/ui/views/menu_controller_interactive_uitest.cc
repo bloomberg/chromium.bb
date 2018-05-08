3072996a75164661547d2b03f454f6e69f4eda0a
Elly Fong-Jones <ellyjones@chromium.org>
2018-05-08 21:33:00 +0000

    macviews: disable menu mnemonics
    These aren't used in Mac apps. They aren't *shown* in MacViews, but they were
    still used, which meant that trying to prefix search could instead activate
    an item by mnemonic.
    
    Test: Manual
    1) Open the app menu
    2) Press "N"
    Before this CL, "New Window" is activated; after it, "New Tab" is
    prefix-selected.
    
    Bug: 836964
    Change-Id: Id672a1e17589e3ebd09e6bff473341947bd2df2a
    Reviewed-on: https://chromium-review.googlesource.com/1050305
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#556966}
    Former-commit-id: cbc70bf500172632ab61df8122c0b15a91e6edef

--- chrome/browser/ui/views/menu_controller_interactive_uitest.cc
+++ chrome/browser/ui/views/menu_controller_interactive_uitest.cc
@@ -4,6 +4,7 @@
 
 #include "base/macros.h"
 #include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
 #include "chrome/browser/ui/views/menu_test_base.h"
 #include "ui/views/controls/menu/menu_item_view.h"
 #include "ui/views/controls/menu/submenu_view.h"
@@ -55,7 +56,14 @@ class MenuControllerMnemonicTest : public MenuTestBase {
 typedef MenuControllerMnemonicTest<ui::VKEY_DIVIDE,1>
     MenuControllerMnemonicTestMnemonicMatch;
 
-VIEW_TEST(MenuControllerMnemonicTestMnemonicMatch, MnemonicMatch);
+#if defined(OS_MACOSX)
+// Mnemonics aren't used on macOS.
+#define MAYBE_MnemonicMatch DISABLED_MnemonicMatch
+#else
+#define MAYBE_MnemonicMatch MnemonicMatch
+#endif
+
+VIEW_TEST(MenuControllerMnemonicTestMnemonicMatch, MAYBE_MnemonicMatch);
 
 // Pressing a key which matches the first letter of the menu item's title
 // should execute the command for that menu item.
