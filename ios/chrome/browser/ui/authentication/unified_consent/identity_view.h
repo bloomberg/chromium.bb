27957e3063d9b61d699178cf28a68446227a4bc0
Jérôme Lebel <jlebel@chromium.org>
2018-05-09 16:50:31 +0000

    [iOS] Creating IdentityView
    This patch extracts code to display an avatar with an email and an
    optional name from IdentityPickerView into IdentityView.
    
    This code can be share between IdentityPickerView and the identity
    chooser dialog in this mock:
    https://gallery.googleplex.com/projects/MCHbtQVoQ2HCZQNtxfU4ytJv/files/MCHtA7U1iMGr62Mmhjs3q0iDhQHgIuNdWV0
    
    Bug: 827072
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ib7df934aa678c35b42b8a37c71dfac6fd52ca5c4
    Reviewed-on: https://chromium-review.googlesource.com/1044373
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557219}
    Former-commit-id: 37d6dae3aad51a0c49e37a517398eed6e2e4f420

---
+++ ios/chrome/browser/ui/authentication/unified_consent/identity_view.h
@@ -0,0 +1,39 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_AUTHENTICATION_UNIFIED_CONSENT_IDENTITY_VIEW_H_
+#define IOS_CHROME_BROWSER_UI_AUTHENTICATION_UNIFIED_CONSENT_IDENTITY_VIEW_H_
+
+#import <UIKit/UIKit.h>
+
+// View with the avatar on the leading side, the name and the email address of
+// an identity.
+// The email address is required. The name and the avatar are optional.
+// If the name is empty (or nil), then the email address is vertically centered.
+// The avatar is displayed as a round image.
+// +--------------------------------+
+// |  +------+                      |
+// |  |      |  Full Name           |
+// |  |Avatar|  Email address       |
+// |  +------+                      |
+// +--------------------------------+
+@interface IdentityView : UIView
+
+// Initialises IdentityView.
+- (instancetype)initWithFrame:(CGRect)frame NS_DESIGNATED_INITIALIZER;
+
+// See -[IdentityView initWithFrame:].
+- (instancetype)init NS_UNAVAILABLE;
+- (instancetype)initWithCoder:(NSCoder*)aDecoder NS_UNAVAILABLE;
+
+// Sets the avatar shown. The image is resized (40px) and shaped as a round
+// image.
+- (void)setAvatar:(UIImage*)avatar;
+
+// Sets the name and the email address. |name| can be nil.
+- (void)setName:(NSString*)name email:(NSString*)email;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_AUTHENTICATION_UNIFIED_CONSENT_IDENTITY_VIEW_H_
