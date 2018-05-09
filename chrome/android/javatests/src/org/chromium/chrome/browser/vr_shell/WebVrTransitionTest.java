15a19644b64f2fd96e9f729ec788bfb973bc3811
vitaliii <vitaliii@chromium.org>
2018-05-09 15:23:10 +0000

    Disable flaky testWindowRafStopsFiringWhilePresenting_WebXr.
    Disable org.chromium.chrome.browser.vr_shell.WebVrTransitionTest
    testWindowRafStopsFiringWhilePresenting_WebXr because flaky.
    
    TBR=bsheedy@chromium.org
    
    Bug: 840883
    Change-Id: I2300a6be8d79d1d6f47d33d5fd06edc9fee5ca41
    Reviewed-on: https://chromium-review.googlesource.com/1051788
    Reviewed-by: vitaliii <vitaliii@chromium.org>
    Commit-Queue: vitaliii <vitaliii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557189}
    Former-commit-id: e079f1560454dfe7b2a19d10527d963583c48acb

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/WebVrTransitionTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/WebVrTransitionTest.java
@@ -423,11 +423,11 @@ public class WebVrTransitionTest {
      */
     @Test
     @MediumTest
-    @CommandLineFlags.Remove({"enable-webvr"})
-    @CommandLineFlags.Add({"enable-features=WebXR"})
+    @DisabledTest(message = "crbug.com/840883")
+    //@CommandLineFlags.Remove({"enable-webvr"})
+    //@CommandLineFlags.Add({"enable-features=WebXR"})
     @VrActivityRestriction({VrActivityRestriction.SupportedActivity.ALL})
-    public void testWindowRafStopsFiringWhilePresenting_WebXr()
-            throws InterruptedException {
+    public void testWindowRafStopsFiringWhilePresenting_WebXr() throws InterruptedException {
         windowRafStopsFiringWhilePresentingImpl(
                 XrTestFramework.getFileUrlForHtmlTestFile(
                         "webxr_test_window_raf_stops_firing_during_exclusive_session"),
