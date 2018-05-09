1eedd2c74fa1c08a583476b21d5cfff37ef739f7
Justin Cohen <justincohen@google.com>
2018-05-09 01:38:51 +0000

    [ios] Fix testLongPDFInitialState for ui refresh.
    Followup to tab strip refresh.
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Iac880a7a687e413badef031451a24c61d855175d
    Reviewed-on: https://chromium-review.googlesource.com/1050472
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557054}
    Former-commit-id: a67e4c54dd11361ebd77194b5df1f14d88390c91

--- ios/chrome/browser/ui/fullscreen/fullscreen_egtest.mm
+++ ios/chrome/browser/ui/fullscreen/fullscreen_egtest.mm
@@ -93,7 +93,7 @@ void AssertURLIs(const GURL& expectedURL) {
   CGFloat yOffset = 0;
   if (IsUIRefreshPhase1Enabled()) {
     if (IsIPadIdiom()) {
-      yOffset = -89.0;
+      yOffset = -104.0;
     } else {
       yOffset = -48.0;
     }
