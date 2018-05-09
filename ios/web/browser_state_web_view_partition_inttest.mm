48ddaa3dfa5a6332dd029cb61ced589990e69501
Eugene But <eugenebut@chromium.org>
2018-05-09 18:52:14 +0000

    Modernize BrowserStateWebViewPartitionTest.
    The following changes mde to the test:
     - everything is placed into web namespace
     - do not assert inside helper fulctions (assert inside tests instead)
     - use EmbeddedTestServer instead of deprecated web::HttpServer
     - use WaitUntilConditionOrTimeout for waiting
     - s/TestNavigationDelegate/FakeNavigationDelegate
    
    Bug: None
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I4369091a71f16e9e4ab2e6e5c24723d77f2c833e
    Reviewed-on: https://chromium-review.googlesource.com/1052169
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557261}
    Former-commit-id: 051fa275ed2ef8dc59511ded0673f463eef848b3

--- ios/web/browser_state_web_view_partition_inttest.mm
+++ ios/web/browser_state_web_view_partition_inttest.mm
@@ -7,16 +7,15 @@
 #include <memory>
 #include <string>
 
+#include "base/compiler_specific.h"
 #include "base/mac/foundation_util.h"
-#import "base/test/ios/wait_util.h"
-#include "base/test/test_timeouts.h"
+#import "ios/testing/wait_util.h"
 #include "ios/web/public/browser_state.h"
-#import "ios/web/public/test/http_server/http_server.h"
-#import "ios/web/public/test/http_server/string_response_provider.h"
 #import "ios/web/public/test/js_test_util.h"
 #import "ios/web/public/web_view_creation_util.h"
 #import "ios/web/test/web_int_test.h"
 #import "net/base/mac/url_conversions.h"
+#include "net/test/embedded_test_server/default_handlers.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #import "testing/gtest_mac.h"
 
@@ -24,14 +23,17 @@
 #error "This file requires ARC support."
 #endif
 
+using testing::kWaitForPageLoadTimeout;
+using testing::WaitUntilConditionOrTimeout;
+
 // A WKNavigationDelegate that is used to check if a WKWebView has finished
 // a navigation. Used for testing purposes.
-@interface TestNavigationDelegate : NSObject <WKNavigationDelegate>
+@interface FakeNavigationDelegate : NSObject<WKNavigationDelegate>
 // YES if a navigation has finished.
 @property (nonatomic, assign) BOOL didFinishNavigation;
 @end
 
-@implementation TestNavigationDelegate
+@implementation FakeNavigationDelegate
 
 @synthesize didFinishNavigation = _didFinishNavigation;
 
@@ -42,31 +44,21 @@
 
 @end
 
+namespace web {
+
 // A test fixture for testing that browsing data is partitioned between
 // web views created with a non-OTR BrowserState and web views created with an
 // OTR BrowserState.
-class BrowserStateWebViewPartitionTest : public web::WebIntTest {
+class BrowserStateWebViewPartitionTest : public WebIntTest {
  protected:
+  BrowserStateWebViewPartitionTest() = default;
   void SetUp() override {
-    web::WebIntTest::SetUp();
+    WebIntTest::SetUp();
 
     otr_browser_state_.SetOffTheRecord(true);
 
-    web::test::HttpServer& server = web::test::HttpServer::GetSharedInstance();
-    ASSERT_TRUE(server.IsRunning());
-
-    auto provider =
-        std::make_unique<web::StringResponseProvider>("Hello World");
-    provider_ = provider.get();  // Keep a weak copy to allow unregistration.
-    server.AddResponseProvider(std::move(provider));
-  }
-
-  void TearDown() override {
-    web::test::HttpServer& server = web::test::HttpServer::GetSharedInstance();
-    server.RemoveResponseProvider(provider_);
-    provider_ = nullptr;
-
-    web::WebIntTest::TearDown();
+    RegisterDefaultHandlers(&server_);
+    ASSERT_TRUE(server_.Start());
   }
 
   // Sets a persistent cookie with key, value on |web_view|.
@@ -103,49 +95,42 @@ class BrowserStateWebViewPartitionTest : public web::WebIntTest {
 
   // Loads a test web page (that contains a small string) in |web_view| and
   // waits until the web view has finished the navigation.
-  void LoadTestWebPage(WKWebView* web_view) {
-    DCHECK(web_view);
-
-    TestNavigationDelegate* navigation_delegate =
-        [[TestNavigationDelegate alloc] init];
+  bool LoadTestWebPage(WKWebView* web_view) WARN_UNUSED_RESULT {
+    FakeNavigationDelegate* navigation_delegate =
+        [[FakeNavigationDelegate alloc] init];
 
     id old_navigation_delegate = web_view.navigationDelegate;
     web_view.navigationDelegate = navigation_delegate;
 
-    web::test::HttpServer& server = web::test::HttpServer::GetSharedInstance();
-    ASSERT_TRUE(server.IsRunning());
-
-    NSURL* url = net::NSURLWithGURL(server.MakeUrl("http://whatever/"));
+    NSURL* url = net::NSURLWithGURL(server_.GetURL("/echo"));
     [web_view loadRequest:[NSURLRequest requestWithURL:url]];
-    base::test::ios::WaitUntilCondition(
-        ^bool {
-          return [navigation_delegate didFinishNavigation];
-        },
-        false, TestTimeouts::action_max_timeout());
+
+    bool result = WaitUntilConditionOrTimeout(kWaitForPageLoadTimeout, ^bool {
+      return navigation_delegate.didFinishNavigation;
+    });
 
     web_view.navigationDelegate = old_navigation_delegate;
+
+    return result;
   }
 
-  // Returns the BrowserState that is used for testing.
-  web::BrowserState* GetOtrBrowserState() { return &otr_browser_state_; }
+ protected:
+  net::EmbeddedTestServer server_;
+  TestBrowserState otr_browser_state_;
 
- private:
-  // The ResponseProvider used to load a simple web page.
-  web::ResponseProvider* provider_;
-  // The OTR browser state used in tests.
-  web::TestBrowserState otr_browser_state_;
+  DISALLOW_COPY_AND_ASSIGN(BrowserStateWebViewPartitionTest);
 };
 
 // Tests that cookies are partitioned between web views created with a
 // non-OTR BrowserState and an OTR BrowserState.
 TEST_F(BrowserStateWebViewPartitionTest, Cookies) {
-  WKWebView* web_view_1 = web::BuildWKWebView(CGRectZero, GetBrowserState());
-  LoadTestWebPage(web_view_1);
+  WKWebView* web_view_1 = BuildWKWebView(CGRectZero, GetBrowserState());
+  ASSERT_TRUE(LoadTestWebPage(web_view_1));
   SetCookie(@"someCookieName1", @"someCookieValue1", web_view_1);
   ASSERT_NSEQ(@"someCookieName1=someCookieValue1", GetCookies(web_view_1));
 
-  WKWebView* web_view_2 = web::BuildWKWebView(CGRectZero, GetOtrBrowserState());
-  LoadTestWebPage(web_view_2);
+  WKWebView* web_view_2 = BuildWKWebView(CGRectZero, &otr_browser_state_);
+  ASSERT_TRUE(LoadTestWebPage(web_view_2));
 
   // Test that the cookie has not leaked over to |web_view_2|.
   ASSERT_NSEQ(@"", GetCookies(web_view_2));
@@ -161,13 +146,13 @@ TEST_F(BrowserStateWebViewPartitionTest, Cookies) {
 // Tests that localStorage is partitioned between web views created with a
 // non-OTR BrowserState and an OTR BrowserState.
 TEST_F(BrowserStateWebViewPartitionTest, LocalStorage) {
-  WKWebView* web_view_1 = web::BuildWKWebView(CGRectZero, GetBrowserState());
-  LoadTestWebPage(web_view_1);
+  WKWebView* web_view_1 = BuildWKWebView(CGRectZero, GetBrowserState());
+  ASSERT_TRUE(LoadTestWebPage(web_view_1));
   SetLocalStorageItem(@"someKey1", @"someValue1", web_view_1);
   EXPECT_NSEQ(@"someValue1", GetLocalStorageItem(@"someKey1", web_view_1));
 
-  WKWebView* web_view_2 = web::BuildWKWebView(CGRectZero, GetOtrBrowserState());
-  LoadTestWebPage(web_view_2);
+  WKWebView* web_view_2 = BuildWKWebView(CGRectZero, &otr_browser_state_);
+  ASSERT_TRUE(LoadTestWebPage(web_view_2));
 
   // Test that LocalStorage has not leaked over to |web_view_2|.
   EXPECT_NSEQ([NSNull null], GetLocalStorageItem(@"someKey1", web_view_2));
@@ -182,3 +167,5 @@ TEST_F(BrowserStateWebViewPartitionTest, LocalStorage) {
   // Test that LocalStorage has not leaked over to |web_view_1|.
   EXPECT_NSEQ([NSNull null], GetLocalStorageItem(@"someKey2", web_view_1));
 }
+
+}  // namespace web
