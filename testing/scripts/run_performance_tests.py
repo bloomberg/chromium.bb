b1d07ed4de4cd434c20f767a14b970fa792241b8
Ethan Kuefner <eakuefner@chromium.org>
2018-05-09 16:16:37 +0000

    Revert "[Perf] Enable system_health.* for histogram uploads"
    This reverts commit f846c186525dc0b30705c24e58c66b9ed06d3fde [formerly f3f894934642a8189d08b532c18d49645c3ad6b3].
    
    Reason for revert: Causing failures
    
    Original change's description:
    > [Perf] Enable system_health.* for histogram uploads
    > 
    > This CL switches all system_health benchmarks to the histogram pipeline.
    > 
    > Bug: 744736
    > Change-Id: Icb97397ec417d27220e489651cc51591757d8c9e
    > Reviewed-on: https://chromium-review.googlesource.com/1048626
    > Reviewed-by: Emily Hanley <eyaich@chromium.org>
    > Commit-Queue: Ethan Kuefner <eakuefner@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#556842}
    
    TBR=eakuefner@chromium.org,eyaich@chromium.org
    
    Change-Id: I53f1788cc1f488b207a0718e8fbe14e2899af5b7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 744736
    Reviewed-on: https://chromium-review.googlesource.com/1052407
    Reviewed-by: Ethan Kuefner <eakuefner@chromium.org>
    Commit-Queue: Ethan Kuefner <eakuefner@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557211}
    Former-commit-id: 983e4035400618b2fc9335160b82a71d29bce2b2

--- testing/scripts/run_performance_tests.py
+++ testing/scripts/run_performance_tests.py
@@ -72,12 +72,7 @@ BENCHMARKS_TO_OUTPUT_HISTOGRAMS = [
     'blink_perf.parser',
     'blink_perf.shadow_dom',
     'blink_perf.svg',
-    'memory.top_10_mobile',
-    'system_health.common_desktop',
-    'system_health.common_mobile',
-    'system_health.memory_desktop',
-    'system_health.memory_mobile',
-    'system_health.webview_startup',
+    'memory.top_10_mobile'
 ]
 
 # We currently have two different sharding schemes for android
