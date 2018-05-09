wjdz99/aom

Add CoeffHalfDequant test
Force the coeff to be half the value of the dequant.  This exposes a
mismatch found in av1_quantize_fp_sse2().

BUG=aomedia:1668

Change-Id: I0323ce91799b53b2ec17ba0b8c7c737e1c709f5f

test/quantize_func_test.cc
@@ -275,6 +275,13 @@ TEST_P(QuantizeTest, MultipleQ) {
   }
 }
 
+// Force the coeff to be half the value of the dequant.  This exposes a
+// mismatch found in av1_quantize_fp_sse2().
+TEST_P(QuantizeTest, CoeffHalfDequant) {
+  FillCoeff(16);
+  QuantizeRun(false, 25, 1);
+}
+
 TEST_P(QuantizeTest, DISABLED_Speed) {
   tran_low_t *coeff_ptr = coeff_;
   const intptr_t n_coeffs = coeff_num();

bloomberg/chromium.bb

test/quantize_func_test.cc
@@ -275,6 +275,13 @@ TEST_P(QuantizeTest, MultipleQ) {
   }
 }
 
+// Force the coeff to be half the value of the dequant.  This exposes a
+// mismatch found in av1_quantize_fp_sse2().
+TEST_P(QuantizeTest, DISABLED_CoeffHalfDequant) {
+  FillCoeff(16);
+  QuantizeRun(false, 25, 1);
+}
+
 TEST_P(QuantizeTest, DISABLED_Speed) {
   tran_low_t *coeff_ptr = coeff_;
   const intptr_t n_coeffs = coeff_num();