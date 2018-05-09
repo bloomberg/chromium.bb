015297b9cc6580db35c253d7f6bdc47292af1525
Nick Harper <nharper@chromium.org>
2018-05-09 20:35:03 +0000

    Revert "Revert "Disable Channel ID on all platforms except Android""
    This reverts commit e2ff7a1a7e45514282e215453a304be720e7adb0 [formerly 3c79bfc0c46a9a5042351f2cae8d42e4e530ff3e].
    
    In addition to the original CL bd3d1ea, this includes a fix to the crash
    when relaunching after changing about:flags.
    
    Bug: 841006
    Change-Id: I04c82074bb83da7ba5854b783862ec689d7a36e0
    Reviewed-on: https://chromium-review.googlesource.com/1050976
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Nick Harper <nharper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557307}
    Former-commit-id: 7ebf529cfab66f217371f992eddfb5865e7884f4

--- content/browser/browser_context.cc
+++ content/browser/browser_context.cc
@@ -143,8 +143,11 @@ void SaveSessionStateOnIOThread(
     AppCacheServiceImpl* appcache_service) {
   net::URLRequestContext* context = context_getter->GetURLRequestContext();
   context->cookie_store()->SetForceKeepSessionState();
-  context->channel_id_service()->GetChannelIDStore()->
-      SetForceKeepSessionState();
+  if (context->channel_id_service()) {
+    context->channel_id_service()
+        ->GetChannelIDStore()
+        ->SetForceKeepSessionState();
+  }
   appcache_service->set_force_keep_session_state();
 }
 
