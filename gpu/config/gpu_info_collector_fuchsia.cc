c09f84347f0baad7667e714ac313695e1696b427
Sergey Ulanov <sergeyu@chromium.org>
2018-05-10 22:15:06 +0000

    [Fuchsia] Implement gpu::CollectContextGraphicsInfo()
    CollectContextGraphicsInfo() now calls CollectGraphicsInfoGL(), same as
    on Linux and Mac. Layout tests were failing to start due to
    CollectContextGraphicsInfo() returning false.
    
    Bug: 778467
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: If089e57b167fb74f95da4462cbe2b1756a76c2be
    Reviewed-on: https://chromium-review.googlesource.com/1054265
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557682}
    Former-commit-id: dd758856f402c7851de6c7e1d238ec35787ea365

--- gpu/config/gpu_info_collector_fuchsia.cc
+++ gpu/config/gpu_info_collector_fuchsia.cc
@@ -4,12 +4,16 @@
 
 #include "gpu/config/gpu_info_collector.h"
 
+#include "base/trace_event/trace_event.h"
+
 namespace gpu {
 
 bool CollectContextGraphicsInfo(GPUInfo* gpu_info) {
-  // TODO(crbug.com/707031): Implement this.
-  NOTIMPLEMENTED();
-  return false;
+  DCHECK(gpu_info);
+
+  TRACE_EVENT0("gpu", "gpu_info_collector::CollectGraphicsInfo");
+
+  return CollectGraphicsInfoGL(gpu_info);
 }
 
 bool CollectBasicGraphicsInfo(GPUInfo* gpu_info) {
