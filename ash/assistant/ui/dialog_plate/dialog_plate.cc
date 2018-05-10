36024b240fa2a96139e76d0714d124fd0888dbe3
David Black <dmblack@google.com>
2018-05-10 22:47:30 +0000

    Moves DialogPlate to ash/assistant/ui/dialog_plate/
    There is a follow up CL to add an ActionView class which is a child of
    DialogPlate that I want to keep grouped in the same directory.
    
    Doing the DialogPlate move here in a separate CL because adding the
    ActionView in the same CL introduced enough changes where git didn't
    track the DIFF but instead treated the changes as a file delete and
    new file add.
    
    Follow up CL is:
    https://chromium-review.googlesource.com/c/chromium/src/+/1053160
    
    Screenshot in bug.
    
    Bug: b:79473532
    Change-Id: Ifcd7a0db2c67c2c7b4f61244b5107ff3bc4fb42b
    Reviewed-on: https://chromium-review.googlesource.com/1054149
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557710}
    Former-commit-id: a03c60824c318e21d55afdaababb1ac15861d292

--- ash/assistant/ui/dialog_plate/dialog_plate.cc
+++ ash/assistant/ui/dialog_plate/dialog_plate.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ash/assistant/ui/dialog_plate.h"
+#include "ash/assistant/ui/dialog_plate/dialog_plate.h"
 
 #include <memory>
 
