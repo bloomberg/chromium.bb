a2876ed16d8cd50f4655a6cf8b57d7673b8048c2
Tetsui Ohkubo <tetsui@chromium.org>
2018-05-11 09:53:16 +0000

    Show supervised state in UnifiedSystemTray.
    In UnifiedSystemTray, information that was shown in TraySupervisedUser
    default view will be shown at the right bottom of the system tray.
    The style is same as one for enterprise managed devices.
    
    TEST=UnifiedSystemInfoViewNoSessionTest
    BUG=none
    
    Change-Id: I7cb1512a41894a2005a4d4a76a2a667c231ea0ad
    Reviewed-on: https://chromium-review.googlesource.com/1051927
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557829}
    Former-commit-id: 1abedab6532f96a9fd3227910e219865bed334e9

--- ash/system/unified/unified_system_info_view_unittest.cc
+++ ash/system/unified/unified_system_info_view_unittest.cc
@@ -4,6 +4,8 @@
 
 #include "ash/system/unified/unified_system_info_view.h"
 
+#include "ash/session/session_controller.h"
+#include "ash/session/test_session_controller_client.h"
 #include "ash/shell.h"
 #include "ash/system/model/enterprise_domain_model.h"
 #include "ash/system/model/system_tray_model.h"
@@ -61,6 +63,33 @@ TEST_F(UnifiedSystemInfoViewTest, EnterpriseManagedVisibleForActiveDirectory) {
 
   // EnterpriseManagedView should be shown.
   EXPECT_TRUE(info_view()->enterprise_managed_->visible());
+}
+
+using UnifiedSystemInfoViewNoSessionTest = NoSessionAshTestBase;
+
+TEST_F(UnifiedSystemInfoViewNoSessionTest, SupervisedVisible) {
+  std::unique_ptr<UnifiedSystemInfoView> info_view_;
+
+  SessionController* session = Shell::Get()->session_controller();
+  ASSERT_FALSE(session->IsActiveUserSessionStarted());
+
+  // Before login the supervised user view is invisible.
+  info_view_ = std::make_unique<UnifiedSystemInfoView>();
+  EXPECT_FALSE(info_view_->supervised_->visible());
+  info_view_.reset();
+
+  // Simulate a supervised user logging in.
+  TestSessionControllerClient* client = GetSessionControllerClient();
+  client->Reset();
+  client->AddUserSession("child@test.com", user_manager::USER_TYPE_SUPERVISED);
+  client->SetSessionState(session_manager::SessionState::ACTIVE);
+  mojom::UserSessionPtr user_session = session->GetUserSession(0)->Clone();
+  user_session->custodian_email = "parent@test.com";
+  session->UpdateUserSession(std::move(user_session));
+
+  // Now the supervised user view is visible.
+  info_view_ = std::make_unique<UnifiedSystemInfoView>();
+  ASSERT_TRUE(info_view_->supervised_->visible());
 }
 
 }  // namespace ash
