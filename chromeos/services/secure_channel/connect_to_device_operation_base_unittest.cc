76ad3305aad50d883e962067cca5554be7748c1d
Kyle Horimoto <khorimoto@google.com>
2018-05-11 03:39:42 +0000

    [CrOS MultiDevice] Create ConnectToDeviceOperation.
    This class performs the low-level work to bootstrap a connection to a
    remote device; each specific connection type (e.g., BLE listener) must
    implement its own derived type.
    
    Bug: 824568, 752273
    Change-Id: I74d5244ac3ad79aac6fa89c6e1f39513d72431de
    Reviewed-on: https://chromium-review.googlesource.com/1053392
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557782}
    Former-commit-id: c1203f334b8d129de3ec05a7a74d4d232a3afd04

---
+++ chromeos/services/secure_channel/connect_to_device_operation_base_unittest.cc
@@ -0,0 +1,181 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/services/secure_channel/connect_to_device_operation_base.h"
+
+#include <memory>
+
+#include "base/memory/ptr_util.h"
+#include "base/memory/ref_counted.h"
+#include "base/test/scoped_task_environment.h"
+#include "base/test/test_simple_task_runner.h"
+#include "components/cryptauth/fake_connection.h"
+#include "components/cryptauth/fake_secure_channel.h"
+#include "components/cryptauth/remote_device_test_util.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace chromeos {
+
+namespace secure_channel {
+
+namespace {
+
+const char kTestFailureReason[] = "testFailureReason";
+
+// Since ConnectToDeviceOperationBase is templatized, a concrete implementation
+// is needed for its test.
+class TestConnectToDeviceOperation
+    : public ConnectToDeviceOperationBase<std::string> {
+ public:
+  static std::unique_ptr<TestConnectToDeviceOperation> Create(
+      ConnectToDeviceOperation<std::string>::ConnectionSuccessCallback
+          success_callback,
+      ConnectToDeviceOperation<std::string>::ConnectionFailedCallback
+          failure_callback,
+      const cryptauth::RemoteDevice& device_to_connect_to,
+      base::OnceClosure destructor_callback) {
+    auto test_task_runner = base::MakeRefCounted<base::TestSimpleTaskRunner>();
+    auto operation = base::WrapUnique(new TestConnectToDeviceOperation(
+        std::move(success_callback), std::move(failure_callback),
+        device_to_connect_to, std::move(destructor_callback),
+        test_task_runner));
+    test_task_runner->RunUntilIdle();
+    return operation;
+  }
+
+  ~TestConnectToDeviceOperation() override = default;
+
+  bool has_attempted_connection() { return has_attempted_connection_; }
+  bool has_canceled_connection() { return has_canceled_connection_; }
+
+  // ConnectToDeviceOperationBase<std::string>:
+  void AttemptConnectionToDevice(
+      const cryptauth::RemoteDevice& device_to_connect_to) override {
+    EXPECT_EQ(remote_device_, device_to_connect_to);
+    has_attempted_connection_ = true;
+  }
+
+  void CancelConnectionAttemptToDevice(
+      const cryptauth::RemoteDevice& device_to_cancel_connection_to) override {
+    EXPECT_EQ(remote_device_, device_to_cancel_connection_to);
+    has_canceled_connection_ = true;
+  }
+
+  // Make On{Successful|Failed}ConnectionAttempt() public for testing.
+  using ConnectToDeviceOperation<std::string>::OnSuccessfulConnectionAttempt;
+  using ConnectToDeviceOperation<std::string>::OnFailedConnectionAttempt;
+
+ private:
+  TestConnectToDeviceOperation(
+      ConnectToDeviceOperation<std::string>::ConnectionSuccessCallback
+          success_callback,
+      ConnectToDeviceOperation<std::string>::ConnectionFailedCallback
+          failure_callback,
+      const cryptauth::RemoteDevice& device_to_connect_to,
+      base::OnceClosure destructor_callback,
+      scoped_refptr<base::TestSimpleTaskRunner> test_task_runner)
+      : ConnectToDeviceOperationBase<std::string>(
+            std::move(success_callback),
+            std::move(failure_callback),
+            device_to_connect_to,
+            std::move(destructor_callback),
+            test_task_runner),
+        remote_device_(device_to_connect_to) {}
+
+  const cryptauth::RemoteDevice remote_device_;
+  bool has_attempted_connection_ = false;
+  bool has_canceled_connection_ = false;
+};
+
+}  // namespace
+
+class SecureChannelConnectToDeviceOperationBaseTest : public testing::Test {
+ protected:
+  SecureChannelConnectToDeviceOperationBaseTest()
+      : test_device_(cryptauth::CreateRemoteDeviceForTest()) {}
+
+  ~SecureChannelConnectToDeviceOperationBaseTest() override = default;
+
+  void SetUp() override {
+    test_operation_ = TestConnectToDeviceOperation::Create(
+        base::BindOnce(&SecureChannelConnectToDeviceOperationBaseTest::
+                           OnSuccessfulConnectionAttempt,
+                       base::Unretained(this)),
+        base::BindOnce(&SecureChannelConnectToDeviceOperationBaseTest::
+                           OnFailedConnectionAttempt,
+                       base::Unretained(this)),
+        test_device_,
+        base::BindOnce(
+            &SecureChannelConnectToDeviceOperationBaseTest::OnOperationDeleted,
+            base::Unretained(this)));
+    EXPECT_TRUE(test_operation_->has_attempted_connection());
+  }
+
+  void TearDown() override {
+    EXPECT_FALSE(destructor_callback_called_);
+    test_operation_.reset();
+    EXPECT_TRUE(destructor_callback_called_);
+  }
+
+  TestConnectToDeviceOperation* test_operation() {
+    return test_operation_.get();
+  }
+
+  const cryptauth::SecureChannel* last_secure_channel() const {
+    return last_secure_channel_.get();
+  }
+
+  const std::string& last_failure_detail() const {
+    return last_failure_detail_;
+  }
+
+  const cryptauth::RemoteDevice& test_device() const { return test_device_; }
+
+ private:
+  void OnSuccessfulConnectionAttempt(
+      std::unique_ptr<cryptauth::SecureChannel> secure_channel) {
+    last_secure_channel_ = std::move(secure_channel);
+  }
+
+  void OnFailedConnectionAttempt(std::string failure_detail) {
+    last_failure_detail_ = failure_detail;
+  }
+
+  void OnOperationDeleted() { destructor_callback_called_ = true; }
+
+  const base::test::ScopedTaskEnvironment scoped_task_environment_;
+  const cryptauth::RemoteDevice test_device_;
+
+  std::unique_ptr<cryptauth::SecureChannel> last_secure_channel_;
+  std::string last_failure_detail_;
+  bool destructor_callback_called_ = false;
+
+  std::unique_ptr<TestConnectToDeviceOperation> test_operation_;
+
+  DISALLOW_COPY_AND_ASSIGN(SecureChannelConnectToDeviceOperationBaseTest);
+};
+
+TEST_F(SecureChannelConnectToDeviceOperationBaseTest, Success) {
+  auto fake_secure_channel = std::make_unique<cryptauth::FakeSecureChannel>(
+      std::make_unique<cryptauth::FakeConnection>(test_device()),
+      nullptr /* cryptauth_service */);
+  auto* fake_secure_channel_raw = fake_secure_channel.get();
+  test_operation()->OnSuccessfulConnectionAttempt(
+      std::move(fake_secure_channel));
+  EXPECT_EQ(fake_secure_channel_raw, last_secure_channel());
+}
+
+TEST_F(SecureChannelConnectToDeviceOperationBaseTest, Failure) {
+  test_operation()->OnFailedConnectionAttempt(kTestFailureReason);
+  EXPECT_EQ(kTestFailureReason, last_failure_detail());
+}
+
+TEST_F(SecureChannelConnectToDeviceOperationBaseTest, Cancelation) {
+  test_operation()->Cancel();
+  EXPECT_TRUE(test_operation()->has_canceled_connection());
+}
+
+}  // namespace secure_channel
+
+}  // namespace chromeos
