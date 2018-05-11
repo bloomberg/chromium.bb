32611f130c15fe993cf44b66773284ea002c0e6f
Donna Wu <donna.wu@intel.com>
2018-05-11 09:47:45 +0000

    Implement MTPManagerClientChromeOS to replace existing MTP observer.
    Implementation of MTPManagerClientChromeOS is mostly copied from
    media_transfer_protocol_device_observer_chromeos.h/cc files which
    will be replaced in subsequent CLs.
    
    This is a preparation CL for the conversion of MTP mojo interfaces'
    usage in the client side without any functional changes for the
    notifications to upper level clients have been blocked.
    
    BUG=769630
    
    Change-Id: I28076cc4a4e30783426c6ccb5af1805ef913d7ae
    Reviewed-on: https://chromium-review.googlesource.com/1046376
    Commit-Queue: Donna Wu <donna.wu@intel.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#557827}
    Former-commit-id: 6547fc81718ae2da3750725863841b8005a9fb5e

--- components/storage_monitor/mtp_manager_client_chromeos.h
+++ components/storage_monitor/mtp_manager_client_chromeos.h
@@ -5,22 +5,35 @@
 #ifndef COMPONENTS_STORAGE_MONITOR_MTP_MANAGER_CLIENT_CHROMEOS_H_
 #define COMPONENTS_STORAGE_MONITOR_MTP_MANAGER_CLIENT_CHROMEOS_H_
 
+#include <map>
 #include <string>
 #include <vector>
 
 #include "base/memory/weak_ptr.h"
+#include "components/storage_monitor/storage_monitor.h"
 #include "mojo/public/cpp/bindings/associated_binding.h"
 #include "services/device/public/mojom/mtp_manager.mojom.h"
 
+namespace base {
+class FilePath;
+}
+
 namespace storage_monitor {
 
-// This class will replace 'MediaTransferProtocolDeviceObserverChromeOS'.
 // This client listens for MTP storage attachment and detachment events
 // from MtpManager and forwards them to StorageMonitor.
 class MtpManagerClientChromeOS : public device::mojom::MtpManagerClient {
  public:
   explicit MtpManagerClientChromeOS(device::mojom::MtpManager* mtp_manager);
   ~MtpManagerClientChromeOS() override;
+
+  // Finds the storage that contains |path| and populates |storage_info|.
+  // Returns false if unable to find the storage.
+  bool GetStorageInfoForPath(const base::FilePath& path,
+                             StorageInfo* storage_info) const;
+
+  void EjectDevice(const std::string& device_id,
+                   base::Callback<void(StorageMonitor::EjectStatus)> callback);
 
  protected:
   // device::mojom::MtpManagerClient implementation.
@@ -29,8 +42,20 @@ class MtpManagerClientChromeOS : public device::mojom::MtpManagerClient {
   void StorageDetached(const std::string& storage_name) override;
 
  private:
+  // Mapping of storage location and MTP storage info object.
+  using StorageLocationToInfoMap = std::map<std::string, StorageInfo>;
+
+  // Enumerate existing MTP storage devices.
   void OnReceivedStorages(
       std::vector<device::mojom::MtpStorageInfoPtr> storage_info_list);
+
+  // Find the |storage_map_| key for the record with this |device_id|. Returns
+  // true on success, false on failure.
+  bool GetLocationForDeviceId(const std::string& device_id,
+                              std::string* location) const;
+
+  // Map of all attached MTP devices.
+  StorageLocationToInfoMap storage_map_;
 
   // Pointer to the MTP manager. Not owned. Client must ensure the MTP
   // manager outlives this object.
