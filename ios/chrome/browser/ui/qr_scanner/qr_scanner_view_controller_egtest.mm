a40c62a252f35d07640ae2acc928c15ed887a49b
Olivier Robin <olivierrobin@chromium.org>
2018-05-14 11:25:54 +0000

    Always unfocus omnibox when displaying QRScanner.
    On iOS10, when dismissing the keyboard, there is a short time when the
    scroll view containing the suggestion is offset.
    This cause the omnibox to not be unfocused automatically because of the
    test in
    https://cs.chromium.org/chromium/src/ios/chrome/browser/ui/content_suggestions/content_suggestions_header_view_controller.mm?l=516
    Fix the issue by always unfocus the omnibox when displaying QR scanner.
    
    Bug: 842072
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ie94884a18335753e919c0cd974c26e4a53abac8b
    Reviewed-on: https://chromium-review.googlesource.com/1055394
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558259}
    Former-commit-id: e53c9ee5b3fce6d95aa91b33ff1af4b7fb1a939d

--- ios/chrome/browser/ui/qr_scanner/qr_scanner_view_controller_egtest.mm
+++ ios/chrome/browser/ui/qr_scanner/qr_scanner_view_controller_egtest.mm
@@ -31,7 +31,6 @@
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
-#include "ios/testing/earl_grey/disabled_test_macros.h"
 #import "ios/web/public/test/http_server/http_server.h"
 #include "ios/web/public/test/http_server/http_server_util.h"
 #import "third_party/ocmock/OCMock/OCMock.h"
@@ -585,12 +584,6 @@ void TapKeyboardReturnKeyInOmniboxWithText(std::string text) {
 // switched off and the correct button indicating that the torch is off is shown
 // when the scanner is opened again.
 - (void)testTorchButtonIsResetWhenQRScannerIsReopened {
-#if TARGET_IPHONE_SIMULATOR
-  if (!IsIPadIdiom() && !base::ios::IsRunningOnIOS11OrLater()) {
-    // TODO(crbug.com/842072): Re-enable those tests.
-    EARL_GREY_TEST_DISABLED(@"Disabled on iPhone iOS 10 simulator.");
-  }
-#endif  // TARGET_IPHONE_SIMULATOR
   id cameraControllerMock =
       [self getCameraControllerMockWithAuthorizationStatus:
                 AVAuthorizationStatusAuthorized];
@@ -671,12 +664,6 @@ void TapKeyboardReturnKeyInOmniboxWithText(std::string text) {
 // Tests that a UIAlertController is presented by the QRScannerViewController if
 // the camera state changes after the QRScannerViewController is presented.
 - (void)testDialogIsDisplayedIfCameraStateChanges {
-#if TARGET_IPHONE_SIMULATOR
-  if (!IsIPadIdiom() && !base::ios::IsRunningOnIOS11OrLater()) {
-    // TODO(crbug.com/842072): Re-enable those tests.
-    EARL_GREY_TEST_DISABLED(@"Disabled on iPhone iOS 10 simulator.");
-  }
-#endif  // TARGET_IPHONE_SIMULATOR
   id cameraControllerMock =
       [self getCameraControllerMockWithAuthorizationStatus:
                 AVAuthorizationStatusAuthorized];
@@ -740,12 +727,6 @@ void TapKeyboardReturnKeyInOmniboxWithText(std::string text) {
 
 // Tests that an error dialog is dismissed if the camera becomes available.
 - (void)testDialogDismissedIfCameraBecomesAvailable {
-#if TARGET_IPHONE_SIMULATOR
-  if (!IsIPadIdiom() && !base::ios::IsRunningOnIOS11OrLater()) {
-    // TODO(crbug.com/842072): Re-enable those tests.
-    EARL_GREY_TEST_DISABLED(@"Disabled on iPhone iOS 10 simulator.");
-  }
-#endif  // TARGET_IPHONE_SIMULATOR
   id cameraControllerMock =
       [self getCameraControllerMockWithAuthorizationStatus:
                 AVAuthorizationStatusAuthorized];
