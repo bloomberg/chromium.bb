19881f546a39bfc354ba06d27ab511daa9817210
Noel Gordon <noel@chromium.org>
2018-05-14 00:03:19 +0000

    Re-enable OpenAudioFiles/FileManagerBrowserTest
    Enabled in RELEASE, now re-enable this test in DEBUG/MSAN/ASAN.
    
    Bug: 836254
    Change-Id: Ie6ca28f923dcf70be407c8782985195874f47758
    Reviewed-on: https://chromium-review.googlesource.com/1056748
    Reviewed-by: Stuart Langley <slangley@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558168}
    Former-commit-id: 738768f5f049c256a32e138551bead9f559a7536

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -113,14 +113,8 @@ WRAPPED_INSTANTIATE_TEST_CASE_P(
                       TestParameter(NOT_IN_GUEST_MODE, "videoOpenDownloads"),
                       TestParameter(NOT_IN_GUEST_MODE, "videoOpenDrive")));
 
-// Fails on official build. http://crbug.com/429294
-#if defined(DISABLE_SLOW_FILESAPP_TESTS) || defined(OFFICIAL_BUILD)
-#define MAYBE_OpenAudioFiles DISABLED_OpenAudioFiles
-#else
-#define MAYBE_OpenAudioFiles OpenAudioFiles
-#endif
 WRAPPED_INSTANTIATE_TEST_CASE_P(
-    MAYBE_OpenAudioFiles,
+    OpenAudioFiles,
     FileManagerBrowserTest,
     ::testing::Values(
         TestParameter(IN_GUEST_MODE, "audioOpenDownloads"),
