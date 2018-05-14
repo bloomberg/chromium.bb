7e3952b62938c5bfef5b6f62bdd1a9c6fbf58835
Ken Rockot <rockot@chromium.org>
2018-05-14 18:46:22 +0000

    Revert "Mojo EDK: Improve internal process handle ownership"
    This reverts commit 7abec5546834a70313b915210c55d1fed17309a0 [formerly 27c9967014302ecc77c888a8628773f53a830e5c].
    
    Reason for revert: https://crbug.com/841565
    
    Original change's description:
    > Mojo EDK: Improve internal process handle ownership
    > 
    > Mojo passes around base::ProcessHandle values for various reasons. On
    > most systems this is fine, but at least on Windows, a ProcessHandle
    > refers to an owned reference to a system process object, and if not
    > careful it's possible for a base::ProcessHandle value to inadvertently
    > change meaning over time.
    > 
    > This CL introduces the concept of a move-only ScopedProcessHandle
    > within Mojo, which on most platforms is just a base::PlatformHandle.
    > On Windows, this represents an owned base::ProcessHandle which closes
    > on destruction and clones correctly using DuplicateHandle rather than
    > merely copying the raw handle value.
    > 
    > ScopedProcessHandle is used in a few places where process handle
    > ownership semantics were previously weaker than necessary, or were
    > correct but implemented ad hoc.
    > 
    > This also updates ScopedPlatformHandle (and supporting code like
    > Channel::RewriteHandles) such that the |owning_process| field (if not
    > the current process) is always an owned process handle. This ensures
    > that when such handles are closed in unsent messages, they can be
    > safely closed in the target process (from within the source process)
    > without any risk of raciness against target process termination.
    > 
    > Bug: 837612
    > Change-Id: I943bb5f70ede56351d52b2ecea7d76fcfdee46ce
    > Reviewed-on: https://chromium-review.googlesource.com/1036459
    > Reviewed-by: Jay Civelli <jcivelli@chromium.org>
    > Commit-Queue: Ken Rockot <rockot@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#555117}
    
    TBR=jcivelli@chromium.org,rockot@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 837612
    Change-Id: Ief6e1d6d6f2f96dc7420e06d8438cc06cbf17490
    Reviewed-on: https://chromium-review.googlesource.com/1057699
    Reviewed-by: Jay Civelli <jcivelli@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558388}
    Former-commit-id: 2f8cd83e52bbab72c8c1ff79308fd7f6b9647859

--- mojo/edk/system/node_channel.cc
+++ mojo/edk/system/node_channel.cc
@@ -232,24 +232,41 @@ void NodeChannel::NotifyBadMessage(const std::string& error) {
     process_error_callback_.Run("Received bad user message: " + error);
 }
 
-void NodeChannel::SetRemoteProcessHandle(ScopedProcessHandle process_handle) {
+void NodeChannel::SetRemoteProcessHandle(base::ProcessHandle process_handle) {
   DCHECK(io_task_runner_->RunsTasksInCurrentSequence());
   base::AutoLock lock(remote_process_handle_lock_);
-  DCHECK(!remote_process_handle_.is_valid());
-  CHECK_NE(remote_process_handle_.get(), base::GetCurrentProcessHandle());
-  remote_process_handle_ = std::move(process_handle);
+  DCHECK_EQ(base::kNullProcessHandle, remote_process_handle_);
+  CHECK_NE(remote_process_handle_, base::GetCurrentProcessHandle());
+  remote_process_handle_ = process_handle;
+#if defined(OS_WIN)
+  DCHECK(!scoped_remote_process_handle_.is_valid());
+  scoped_remote_process_handle_.reset(PlatformHandle(process_handle));
+#endif
 }
 
 bool NodeChannel::HasRemoteProcessHandle() {
   base::AutoLock lock(remote_process_handle_lock_);
-  return remote_process_handle_.is_valid();
+  return remote_process_handle_ != base::kNullProcessHandle;
 }
 
-ScopedProcessHandle NodeChannel::CloneRemoteProcessHandle() {
+base::ProcessHandle NodeChannel::CopyRemoteProcessHandle() {
   base::AutoLock lock(remote_process_handle_lock_);
-  if (!remote_process_handle_.is_valid())
-    return ScopedProcessHandle();
-  return remote_process_handle_.Clone();
+#if defined(OS_WIN)
+  if (remote_process_handle_ != base::kNullProcessHandle) {
+    // Privileged nodes use this to pass their invitees' process handles to the
+    // broker on launch.
+    HANDLE handle = remote_process_handle_;
+    BOOL result =
+        DuplicateHandle(base::GetCurrentProcessHandle(), remote_process_handle_,
+                        base::GetCurrentProcessHandle(), &handle, 0, FALSE,
+                        DUPLICATE_SAME_ACCESS);
+    DPCHECK(result);
+    return handle;
+  }
+  return base::kNullProcessHandle;
+#else
+  return remote_process_handle_;
+#endif
 }
 
 void NodeChannel::SetRemoteNodeName(const ports::NodeName& name) {
@@ -290,12 +307,11 @@ void NodeChannel::AcceptPeer(const ports::NodeName& sender_name,
 }
 
 void NodeChannel::AddBrokerClient(const ports::NodeName& client_name,
-                                  ScopedProcessHandle process_handle) {
+                                  base::ProcessHandle process_handle) {
   AddBrokerClientData* data;
   std::vector<ScopedPlatformHandle> handles;
 #if defined(OS_WIN)
-  handles.emplace_back(
-      ScopedPlatformHandle(PlatformHandle(process_handle.release())));
+  handles.emplace_back(ScopedPlatformHandle(PlatformHandle(process_handle)));
 #endif
   Channel::MessagePtr message =
       CreateMessage(MessageType::ADD_BROKER_CLIENT, sizeof(AddBrokerClientData),
@@ -303,7 +319,7 @@ void NodeChannel::AddBrokerClient(const ports::NodeName& client_name,
   message->SetHandles(std::move(handles));
   data->client_name = client_name;
 #if !defined(OS_WIN)
-  data->process_handle = process_handle.get();
+  data->process_handle = process_handle;
   data->padding = 0;
 #endif
   WriteChannelMessage(std::move(message));
@@ -485,13 +501,14 @@ void NodeChannel::OnChannelMessage(const void* payload,
   // from a privileged descendant.
   {
     base::AutoLock lock(remote_process_handle_lock_);
-    if (!handles.empty() && remote_process_handle_.is_valid()) {
+    if (!handles.empty() &&
+        remote_process_handle_ != base::kNullProcessHandle) {
       // Note that we explicitly mark the handles as being owned by the sending
       // process before rewriting them, in order to accommodate RewriteHandles'
       // internal sanity checks.
       for (auto& handle : handles)
-        handle.get().owning_process = remote_process_handle_.get();
-      if (!Channel::Message::RewriteHandles(remote_process_handle_.get(),
+        handle.get().owning_process = remote_process_handle_;
+      if (!Channel::Message::RewriteHandles(remote_process_handle_,
                                             base::GetCurrentProcessHandle(),
                                             &handles)) {
         DLOG(ERROR) << "Received one or more invalid handles.";
@@ -652,10 +669,7 @@ void NodeChannel::OnChannelMessage(const void* payload,
       base::ProcessHandle from_process;
       {
         base::AutoLock lock(remote_process_handle_lock_);
-        // NOTE: It's safe to retain a weak reference to this process handle
-        // through the extent of this call because |this| is kept alive and
-        // |remote_process_handle_| is never reset once set.
-        from_process = remote_process_handle_.get();
+        from_process = remote_process_handle_;
       }
       const RelayEventMessageData* data;
       if (GetMessagePayload(payload, payload_size, &data)) {
@@ -786,7 +800,7 @@ void NodeChannel::ProcessPendingMessagesWithMachPorts() {
   base::ProcessHandle remote_process_handle;
   {
     base::AutoLock lock(remote_process_handle_lock_);
-    remote_process_handle = remote_process_handle_.get();
+    remote_process_handle = remote_process_handle_;
   }
   PendingMessageQueue pending_writes;
   PendingRelayMessageQueue pending_relays;
@@ -832,14 +846,17 @@ void NodeChannel::WriteChannelMessage(Channel::MessagePtr message) {
   // here (they'll be unpacked and duplicated by the broker).
 
   if (message->has_handles()) {
-    base::AutoLock lock(remote_process_handle_lock_);
+    base::ProcessHandle remote_process_handle;
+    {
+      base::AutoLock lock(remote_process_handle_lock_);
+      remote_process_handle = remote_process_handle_;
+    }
 
     // Rewrite outgoing handles if we have a handle to the destination process.
-    if (remote_process_handle_.is_valid()) {
+    if (remote_process_handle != base::kNullProcessHandle) {
       std::vector<ScopedPlatformHandle> handles = message->TakeHandles();
       if (!Channel::Message::RewriteHandles(base::GetCurrentProcessHandle(),
-                                            remote_process_handle_.get(),
-                                            &handles)) {
+                                            remote_process_handle, &handles)) {
         DLOG(ERROR) << "Failed to duplicate one or more outgoing handles.";
       }
       message->SetHandles(std::move(handles));
@@ -851,11 +868,16 @@ void NodeChannel::WriteChannelMessage(Channel::MessagePtr message) {
   if (message->has_mach_ports()) {
     MachPortRelay* relay = delegate_->GetMachPortRelay();
     if (relay) {
-      base::AutoLock lock(remote_process_handle_lock_);
-      DCHECK(remote_process_handle_.is_valid());
+      base::ProcessHandle remote_process_handle;
+      {
+        base::AutoLock lock(remote_process_handle_lock_);
+        // Expect that the receiving node is a known process.
+        DCHECK(remote_process_handle_ != base::kNullProcessHandle);
+        remote_process_handle = remote_process_handle_;
+      }
       {
         base::AutoLock lock(pending_mach_messages_lock_);
-        if (relay->port_provider()->TaskForPid(remote_process_handle_.get()) ==
+        if (relay->port_provider()->TaskForPid(remote_process_handle) ==
             MACH_PORT_NULL) {
           // It is also possible for TaskForPid() to return MACH_PORT_NULL when
           // the process has started, then died. In that case, the queued
@@ -866,7 +888,7 @@ void NodeChannel::WriteChannelMessage(Channel::MessagePtr message) {
         }
       }
 
-      relay->SendPortsToProcess(message.get(), remote_process_handle_.get());
+      relay->SendPortsToProcess(message.get(), remote_process_handle);
     }
   }
 #endif
