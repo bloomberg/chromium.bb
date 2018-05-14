94cc39c19b663add12984541a70f0536c236ba35
Christian Dullweber <dullweber@chromium.org>
2018-05-14 09:04:56 +0000

    Migrate HistoryServiceObserver::OnUrlsDeleted
    Change HistoryServiceObservers to use the new OnUrlsDeleted method for
    predictors and favicon_cache.
    
    Bug: 839798
    Change-Id: Ibcf4d48eabe3649282c2246be73e8a8d263dc98d
    Reviewed-on: https://chromium-review.googlesource.com/1051888
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Alexandr Ilin <alexilin@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558233}
    Former-commit-id: b2eb8c94961ed9b5f6e9d58501785e72dd6a3a15

--- components/sync_sessions/favicon_cache.h
+++ components/sync_sessions/favicon_cache.h
@@ -201,10 +201,7 @@ class FaviconCache : public syncer::SyncableService,
 
   // history::HistoryServiceObserver:
   void OnURLsDeleted(history::HistoryService* history_service,
-                     bool all_history,
-                     bool expired,
-                     const history::URLRows& deleted_rows,
-                     const std::set<GURL>& favicon_urls) override;
+                     const history::DeletionInfo& deletion_info) override;
 
   favicon::FaviconService* favicon_service_;
 
