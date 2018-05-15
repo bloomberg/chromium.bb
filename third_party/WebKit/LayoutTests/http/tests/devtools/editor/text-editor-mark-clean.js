519b79b8dd609c930c6cdf8fdf32e16c130779b1
Joel Einbinder <einbinder@chromium.org>
2018-05-15 17:41:54 +0000

    DevTools: Auto pretty print in sources panel experiment
    Change-Id: Ib16be938e7df8c90f02da1211f350d172c34ebff
    Reviewed-on: https://chromium-review.googlesource.com/1047949
    Commit-Queue: Joel Einbinder <einbinder@chromium.org>
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558766}
    Former-commit-id: 6cc4917ff46b654205b4aa1700c049c14d2ebabd

--- third_party/WebKit/LayoutTests/http/tests/devtools/editor/text-editor-mark-clean.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/editor/text-editor-mark-clean.js
@@ -9,55 +9,56 @@
 
   var textEditor = SourcesTestRunner.createTestEditor();
   textEditor.setText('1\n2\n3\n4');
+  let changeGeneration = 0;
 
   TestRunner.runTestSuite([
     function testMarkiningInitialStateAsClean(next) {
-      TestRunner.addResult('Initial state: clean=' + textEditor.isClean());
-      textEditor.markClean();
-      TestRunner.addResult('After marking clean: clean=' + textEditor.isClean());
+      TestRunner.addResult('Initial state: clean=' + textEditor.isClean(changeGeneration));
+      changeGeneration = textEditor.markClean();
+      TestRunner.addResult('After marking clean: clean=' + textEditor.isClean(changeGeneration));
       textEditor.editRange(TextUtils.TextRange.createFromLocation(0, 0), 'newText');
-      TestRunner.addResult('EDIT; clean=' + textEditor.isClean());
+      TestRunner.addResult('EDIT; clean=' + textEditor.isClean(changeGeneration));
       textEditor.undo();
-      TestRunner.addResult('UNDO; clean=' + textEditor.isClean());
+      TestRunner.addResult('UNDO; clean=' + textEditor.isClean(changeGeneration));
       textEditor.redo();
-      TestRunner.addResult('REDO; clean=' + textEditor.isClean());
+      TestRunner.addResult('REDO; clean=' + textEditor.isClean(changeGeneration));
       textEditor.undo();
-      TestRunner.addResult('UNDO; clean=' + textEditor.isClean());
+      TestRunner.addResult('UNDO; clean=' + textEditor.isClean(changeGeneration));
       textEditor.editRange(TextUtils.TextRange.createFromLocation(1, 0), 'newText2');
-      TestRunner.addResult('EDIT; clean=' + textEditor.isClean());
+      TestRunner.addResult('EDIT; clean=' + textEditor.isClean(changeGeneration));
       textEditor.undo();
-      TestRunner.addResult('UNDO; clean=' + textEditor.isClean());
+      TestRunner.addResult('UNDO; clean=' + textEditor.isClean(changeGeneration));
       next();
     },
 
     function testMiddleStateAsClean(next) {
-      TestRunner.addResult('Initial state: clean=' + textEditor.isClean());
+      TestRunner.addResult('Initial state: clean=' + textEditor.isClean(changeGeneration));
       for (var i = 0; i < 3; ++i) {
         textEditor.editRange(TextUtils.TextRange.createFromLocation(i, 0), 'newText' + i);
-        TestRunner.addResult('EDIT; clean=' + textEditor.isClean());
+        TestRunner.addResult('EDIT; clean=' + textEditor.isClean(changeGeneration));
       }
-      textEditor.markClean();
-      TestRunner.addResult('After marking clean: clean=' + textEditor.isClean());
+      changeGeneration = textEditor.markClean();
+      TestRunner.addResult('After marking clean: clean=' + textEditor.isClean(changeGeneration));
       textEditor.editRange(TextUtils.TextRange.createFromLocation(3, 0), 'newText' + 3);
-      TestRunner.addResult('EDIT; clean=' + textEditor.isClean());
+      TestRunner.addResult('EDIT; clean=' + textEditor.isClean(changeGeneration));
       for (var i = 0; i < 4; ++i) {
         textEditor.undo();
-        TestRunner.addResult('UNDO; clean=' + textEditor.isClean());
+        TestRunner.addResult('UNDO; clean=' + textEditor.isClean(changeGeneration));
       }
       for (var i = 0; i < 4; ++i) {
         textEditor.redo();
-        TestRunner.addResult('REDO; clean=' + textEditor.isClean());
+        TestRunner.addResult('REDO; clean=' + textEditor.isClean(changeGeneration));
       }
       for (var i = 0; i < 2; ++i) {
         textEditor.undo();
-        TestRunner.addResult('UNDO; clean=' + textEditor.isClean());
+        TestRunner.addResult('UNDO; clean=' + textEditor.isClean(changeGeneration));
       }
       textEditor.editRange(TextUtils.TextRange.createFromLocation(1, 0), 'foo');
-      TestRunner.addResult('EDIT; clean=' + textEditor.isClean());
+      TestRunner.addResult('EDIT; clean=' + textEditor.isClean(changeGeneration));
       textEditor.undo();
-      TestRunner.addResult('UNDO; clean=' + textEditor.isClean());
+      TestRunner.addResult('UNDO; clean=' + textEditor.isClean(changeGeneration));
       textEditor.undo();
-      TestRunner.addResult('UNDO; clean=' + textEditor.isClean());
+      TestRunner.addResult('UNDO; clean=' + textEditor.isClean(changeGeneration));
       next();
     },
   ]);
