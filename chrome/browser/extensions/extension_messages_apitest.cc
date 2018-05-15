25b79d212263edde56bb68bb9ca202b99057cb60
Nick Harper <nharper@chromium.org>
2018-05-15 02:40:07 +0000

    Check that Channel ID is in use before providing Channel IDs to extensions
    This is a re-land of crrev.com/c/1055676, with additional changes to
    extension_messages_apitest.cc so that tests pass when both Token Binding
    and Channel ID are disabled. (Token Binding is enabled in testing
    configurations, but both are disabled in official builds.)
    
    Change-Id: If519b6bbbf1aea111f411c2faf1f2f2678a328e8
    Reviewed-on: https://chromium-review.googlesource.com/1058428
    Commit-Queue: Nick Harper <nharper@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558592}
    Former-commit-id: fcf7b165a47df059bf6891a3949b7f8250d7d4e5

--- chrome/browser/extensions/extension_messages_apitest.cc
+++ chrome/browser/extensions/extension_messages_apitest.cc
@@ -37,6 +37,7 @@
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/crx_file/id_util.h"
+#include "components/network_session_configurator/common/network_features.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/notification_service.h"
@@ -86,6 +87,10 @@ void CreateDomainBoundCertOnIOThread(
   DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
   net::ChannelIDService* channel_id_service =
       request_context_getter->GetURLRequestContext()->channel_id_service();
+  if (!channel_id_service) {
+    waitable_event->Signal();
+    return;
+  }
   int status = channel_id_service->GetOrCreateChannelID(
       url.host(), channel_id_key,
       base::BindRepeating(&GotDomainBoundCert, waitable_event), request);
@@ -111,7 +116,7 @@ std::string CreateTlsChannelId(
   waitable_event.Wait();
   // Create the expected value.
   std::vector<uint8_t> spki_vector;
-  if (!channel_id_key->ExportPublicKey(&spki_vector))
+  if (!channel_id_key || !channel_id_key->ExportPublicKey(&spki_vector))
     return std::string();
   base::StringPiece spki(reinterpret_cast<char*>(spki_vector.data()),
                          spki_vector.size());
@@ -1140,6 +1145,12 @@ IN_PROC_BROWSER_TEST_P(ExternallyConnectableMessagingTest,
                        WebConnectableWithNonEmptyTlsChannelId) {
   std::string expected_tls_channel_id_value =
       CreateTlsChannelId(profile()->GetRequestContext(), chromium_org_url());
+  bool expect_empty_id = false;
+  if (!base::FeatureList::IsEnabled(::features::kChannelID) &&
+      !base::FeatureList::IsEnabled(::features::kTokenBinding)) {
+    expected_tls_channel_id_value = "";
+    expect_empty_id = true;
+  }
 
   scoped_refptr<const Extension> chromium_connectable =
       LoadChromiumConnectableExtensionWithTlsChannelId();
@@ -1157,7 +1168,7 @@ IN_PROC_BROWSER_TEST_P(ExternallyConnectableMessagingTest,
   // If the page does ask to send the TLS channel ID, it's sent and non-empty.
   std::string tls_channel_id_from_port_connect =
       GetTlsChannelIdFromPortConnect(chromium_connectable.get(), true);
-  EXPECT_NE(0u, tls_channel_id_from_port_connect.size());
+  EXPECT_EQ(tls_channel_id_from_port_connect.empty(), expect_empty_id);
 
   // The same value is received by both connect and sendMessage.
   std::string tls_channel_id_from_send_message =
@@ -1189,6 +1200,54 @@ IN_PROC_BROWSER_TEST_P(ExternallyConnectableMessagingTest,
       GetTlsChannelIdFromSendMessage(chromium_connectable.get(), true);
   EXPECT_EQ(tls_channel_id, tls_channel_id2);
 }
+
+class ExternallyConnectableMessagingTestNoChannelID
+    : public ExternallyConnectableMessagingTest {
+ public:
+  ExternallyConnectableMessagingTestNoChannelID() {}
+  ~ExternallyConnectableMessagingTestNoChannelID() override {}
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    std::vector<base::Feature> enabled_features;
+    std::vector<base::Feature> disabled_features = {::features::kChannelID,
+                                                    ::features::kTokenBinding};
+    scoped_feature_list_.InitWithFeatures(enabled_features, disabled_features);
+    ExternallyConnectableMessagingTest::SetUpCommandLine(command_line);
+  }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+
+  DISALLOW_COPY_AND_ASSIGN(ExternallyConnectableMessagingTestNoChannelID);
+};
+
+IN_PROC_BROWSER_TEST_P(ExternallyConnectableMessagingTestNoChannelID,
+                       TlsChannelIdEmptyWhenDisabled) {
+  std::string expected_tls_channel_id_value =
+      CreateTlsChannelId(profile()->GetRequestContext(), chromium_org_url());
+
+  scoped_refptr<const Extension> chromium_connectable =
+      LoadChromiumConnectableExtensionWithTlsChannelId();
+  ASSERT_TRUE(chromium_connectable.get());
+
+  ui_test_utils::NavigateToURL(browser(), chromium_org_url());
+
+  // Check that both connect and sendMessage don't report a Channel ID.
+  std::string tls_channel_id_from_port_connect =
+      GetTlsChannelIdFromPortConnect(chromium_connectable.get(), true);
+  EXPECT_EQ(0u, tls_channel_id_from_port_connect.size());
+
+  std::string tls_channel_id_from_send_message =
+      GetTlsChannelIdFromSendMessage(chromium_connectable.get(), true);
+  EXPECT_EQ(0u, tls_channel_id_from_send_message.size());
+}
+
+INSTANTIATE_TEST_CASE_P(NativeBindingsNoChannelID,
+                        ExternallyConnectableMessagingTestNoChannelID,
+                        ::testing::Values(NATIVE_BINDINGS));
+INSTANTIATE_TEST_CASE_P(JavaScriptBindingsNoChannelID,
+                        ExternallyConnectableMessagingTestNoChannelID,
+                        ::testing::Values(JAVASCRIPT_BINDINGS));
 
 // Tests a web connectable extension that receives TLS channel id, but
 // immediately closes its background page upon receipt of a message.
@@ -1411,6 +1470,10 @@ IN_PROC_BROWSER_TEST_P(MessagingApiTest,
   std::string tls_channel_id = CreateTlsChannelId(
       app_storage_partition->GetURLRequestContext(), background_url);
   ASSERT_FALSE(tls_channel_id.empty());
+  if (!base::FeatureList::IsEnabled(::features::kChannelID) &&
+      !base::FeatureList::IsEnabled(::features::kTokenBinding)) {
+    tls_channel_id = "undefined";
+  }
 
   // Load up an extension that the app can message, which we will use to
   // verify the TLS channel id sent to the message listener.
