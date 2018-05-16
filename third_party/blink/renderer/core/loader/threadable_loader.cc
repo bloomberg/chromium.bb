b8fb1a9411f2638bd5fbd1b564acda682c308c97
Tsuyoshi Horo <horo@chromium.org>
2018-05-16 15:26:48 +0000

    Reland "Revive WorkerThreadableLoader for sync XHR again"
    This reverts commit af58b5391d740dfccd2d1837e7eec172d5de0802 [formerly 4b00991ee27817b60b2a142261bd1d89ca253615].
    
    Reason for revert: It is easy to fix the test failure.
    
    Original change's description:
    > Revert "Revive WorkerThreadableLoader for sync XHR again"
    > 
    > This reverts commit 2630e84f188bdfb26d7778b5700bc4852f141aa1 [formerly 14b1861dc42443535a48b97c09126f1f59cd4088].
    > 
    > Reason for revert: This CL introduced a layout test that seems to be consistently failing on Win10 starting in https://ci.chromium.org/buildbot/chromium.webkit/WebKit%20Win10/35033 in http/tests/xmlhttprequest/workers/sync-cors-after-redirect-on-worker.html:
    > 
    > FAIL Synchronous CORS XHR after redirect on worker assert_equals: expected "PASS: Cross-domain access allowed.\n" but got "PASS: Cross-domain access allowed.\r\n"
    > 
    > Original change's description:
    > > Revive WorkerThreadableLoader for sync XHR again
    > > 
    > > The cl "60933547a87ecf2891b06434881be1539a27b3fa [formerly 5e1b52dd7e828fb2e4bd69f599c0c30eec3e873c]" which made ThreadableLoader
    > > use DocumentThreadableLoader for sync loading from worker thread had broken
    > > existing site. https://crbug.com/840348
    > > 
    > > This is because DocumentThreadableLoader::LoadResourceSynchronously() doesn't
    > > support CORS after redirect.
    > > 
    > > It is difficult to make DocumentThreadableLoader::LoadResourceSynchronously()
    > > support it. So This CL revives WorkerThreadableLoader for sync XHR.
    > > 
    > > Bug: 840348, 706331
    > > Change-Id: Ib41fc442ddca508011431609cad1b6b6f9bda537
    > > Reviewed-on: https://chromium-review.googlesource.com/1060793
    > > Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    > > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#559023}
    > 
    > TBR=horo@chromium.org,kinuko@chromium.org
    > 
    > Change-Id: I1605a0b591d59737fa09951e6f8d5f3d907d52b4
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 840348, 706331
    > Reviewed-on: https://chromium-review.googlesource.com/1061894
    > Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    > Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#559101}
    
    TBR=horo@chromium.org,kinuko@chromium.org,lukasza@chromium.org
    
    Change-Id: I6d078b4124defec2f9d7514b95eed00ff6f685f5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 840348, 706331
    Reviewed-on: https://chromium-review.googlesource.com/1061435
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559107}
    Former-commit-id: b1e172c8d0e85286f9025f291c3fc3e719bc30ff

--- third_party/blink/renderer/core/loader/threadable_loader.cc
+++ third_party/blink/renderer/core/loader/threadable_loader.cc
@@ -33,6 +33,7 @@
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/loader/document_threadable_loader.h"
 #include "third_party/blink/renderer/core/loader/threadable_loading_context.h"
+#include "third_party/blink/renderer/core/loader/worker_threadable_loader.h"
 #include "third_party/blink/renderer/core/workers/worker_global_scope.h"
 
 namespace blink {
@@ -56,6 +57,12 @@ void ThreadableLoader::LoadResourceSynchronously(
     ThreadableLoaderClient& client,
     const ThreadableLoaderOptions& options,
     const ResourceLoaderOptions& resource_loader_options) {
+  if (context.IsWorkerGlobalScope()) {
+    WorkerThreadableLoader::LoadResourceSynchronously(
+        ToWorkerGlobalScope(context), request, client, options,
+        resource_loader_options);
+    return;
+  }
   DocumentThreadableLoader::LoadResourceSynchronously(
       *ThreadableLoadingContext::Create(context), request, client, options,
       resource_loader_options);
