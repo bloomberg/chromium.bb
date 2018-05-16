e6a3ecdf4d14269a54c2681449a19a3d4f72df3d
David Black <dmblack@google.com>
2018-05-16 01:37:53 +0000

    Added suggestion chip icon support.
    This CL:
    - Adds support for optional suggestion chip icon.
    - Modifies suggestion chip style.
    - Uses placeholder for assistant chip icon.
    
    Assistant chip icon URL to be handled in follow up CL.
    
    See bug for before/after.
    
    Bug: b:79599200
    Change-Id: I622644441dd77d923b3bf8208ad89d339ef00138
    Reviewed-on: https://chromium-review.googlesource.com/1058153
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#558915}
    Former-commit-id: 6c15c9903f71f0671423d5dbc2a8ace6e7debd97

--- ui/app_list/views/suggestion_chip_view.cc
+++ ui/app_list/views/suggestion_chip_view.cc
@@ -5,6 +5,8 @@
 #include "ui/app_list/views/suggestion_chip_view.h"
 
 #include "ui/gfx/canvas.h"
+#include "ui/views/background.h"
+#include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/box_layout.h"
 
@@ -12,42 +14,92 @@ namespace app_list {
 
 namespace {
 
-// Appearance.
-constexpr SkColor kBackgroundColor = SkColorSetA(SK_ColorBLACK, 0x1F);
-constexpr int kCornerRadiusDip = 12;
-constexpr int kPaddingHorizontalDip = 8;
-constexpr int kPaddingVerticalDip = 4;
+// Colors.
+constexpr SkColor kBackgroundColor = SK_ColorWHITE;
+constexpr SkColor kStrokeColor = SkColorSetRGB(0xDA, 0xDC, 0xE0);  // G Grey 300
+constexpr SkColor kTextColor = SkColorSetRGB(0x3C, 0x40, 0x43);    // G Grey 800
 
-// Typography.
-constexpr SkColor kTextColor = SkColorSetA(SK_ColorBLACK, 0xDE);
+// Dimensions.
+constexpr int kIconMarginDip = 8;
+constexpr int kIconSizeDip = 20;
+constexpr int kPaddingDip = 16;
+constexpr int kPreferredHeightDip = 32;
+constexpr int kStrokeWidthDip = 1;
 
 }  // namespace
 
-SuggestionChipView::SuggestionChipView(const base::string16& text,
+// Params ----------------------------------------------------------------------
+
+SuggestionChipView::Params::Params() = default;
+
+SuggestionChipView::Params::~Params() = default;
+
+// SuggestionChipView ----------------------------------------------------------
+
+SuggestionChipView::SuggestionChipView(const Params& params,
                                        SuggestionChipListener* listener)
-    : text_view_(new views::Label(text)), listener_(listener) {
-  InitLayout();
+    : icon_view_(new views::ImageView()),
+      text_view_(new views::Label()),
+      listener_(listener) {
+  InitLayout(params);
 }
 
-void SuggestionChipView::InitLayout() {
-  SetLayoutManager(std::make_unique<views::BoxLayout>(
-      views::BoxLayout::Orientation::kHorizontal,
-      gfx::Insets(kPaddingVerticalDip, kPaddingHorizontalDip)));
+SuggestionChipView::~SuggestionChipView() = default;
 
-  // TODO(dmblack): Add optional icon.
+gfx::Size SuggestionChipView::CalculatePreferredSize() const {
+  return gfx::Size(views::View::CalculatePreferredSize().width(),
+                   kPreferredHeightDip);
+}
 
-  // Text view.
+void SuggestionChipView::InitLayout(const Params& params) {
+  // Layout padding differs depending on icon visibility.
+  const int padding_left_dip = params.icon ? kIconMarginDip : kPaddingDip;
+
+  views::BoxLayout* layout_manager =
+      SetLayoutManager(std::make_unique<views::BoxLayout>(
+          views::BoxLayout::Orientation::kHorizontal,
+          gfx::Insets(0, padding_left_dip, 0, kPaddingDip), kIconMarginDip));
+
+  layout_manager->set_cross_axis_alignment(
+      views::BoxLayout::CrossAxisAlignment::CROSS_AXIS_ALIGNMENT_CENTER);
+
+  // Icon.
+  icon_view_->SetImageSize(gfx::Size(kIconSizeDip, kIconSizeDip));
+  icon_view_->SetPreferredSize(gfx::Size(kIconSizeDip, kIconSizeDip));
+
+  if (params.icon)
+    icon_view_->SetImage(params.icon.value());
+  else
+    icon_view_->SetVisible(false);
+
+  AddChildView(icon_view_);
+
+  // Text.
   text_view_->SetAutoColorReadabilityEnabled(false);
   text_view_->SetEnabledColor(kTextColor);
   text_view_->SetFontList(text_view_->font_list().DeriveWithSizeDelta(2));
+  text_view_->SetText(params.text);
   AddChildView(text_view_);
 }
 
 void SuggestionChipView::OnPaintBackground(gfx::Canvas* canvas) {
   cc::PaintFlags flags;
   flags.setAntiAlias(true);
+
+  gfx::Rect bounds = GetContentsBounds();
+
+  // Background.
   flags.setColor(kBackgroundColor);
-  canvas->DrawRoundRect(GetContentsBounds(), kCornerRadiusDip, flags);
+  canvas->DrawRoundRect(bounds, height() / 2, flags);
+
+  // Stroke should be drawn within our contents bounds.
+  bounds.Inset(gfx::Insets(kStrokeWidthDip));
+
+  // Stroke.
+  flags.setColor(kStrokeColor);
+  flags.setStrokeWidth(kStrokeWidthDip);
+  flags.setStyle(cc::PaintFlags::Style::kStroke_Style);
+  canvas->DrawRoundRect(bounds, height() / 2, flags);
 }
 
 void SuggestionChipView::OnGestureEvent(ui::GestureEvent* event) {
