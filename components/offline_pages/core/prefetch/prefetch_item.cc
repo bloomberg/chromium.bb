4c4cb770d562a0413bd58a749b375d0fb4af8ca0
Dan Harrington <harringtond@chromium.org>
2018-05-16 21:20:42 +0000

    Add PrefetchItemFlow unittest
    This helps test the integration of tasks that make up the prefetch
    pipeline. I found that this coverage was lacking when making a change
    to a task.
    
    This CL also improves output of PrefetchItem and prefetch enums for tests.
    
    Change-Id: I719d815da3a6473c120f077f723c3c67a30e29cc
    Reviewed-on: https://chromium-review.googlesource.com/1058169
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559294}
    Former-commit-id: 61fc3569c5ff779419f749a682651d89dd956c71

--- components/offline_pages/core/prefetch/prefetch_item.cc
+++ components/offline_pages/core/prefetch/prefetch_item.cc
@@ -14,67 +14,8 @@ namespace offline_pages {
 
 PrefetchItem::PrefetchItem() = default;
 
-PrefetchItem::PrefetchItem(const PrefetchItem& other) = default;
-
 PrefetchItem::PrefetchItem(PrefetchItem&& other) = default;
 
 PrefetchItem::~PrefetchItem() = default;
-
-PrefetchItem& PrefetchItem::operator=(const PrefetchItem& other) = default;
-
-PrefetchItem& PrefetchItem::operator=(PrefetchItem&& other) = default;
-
-bool PrefetchItem::operator==(const PrefetchItem& other) const {
-  return offline_id == other.offline_id && guid == other.guid &&
-         client_id == other.client_id && state == other.state &&
-         url == other.url && final_archived_url == other.final_archived_url &&
-         generate_bundle_attempts == other.generate_bundle_attempts &&
-         get_operation_attempts == other.get_operation_attempts &&
-         download_initiation_attempts == other.download_initiation_attempts &&
-         operation_name == other.operation_name &&
-         archive_body_name == other.archive_body_name &&
-         archive_body_length == other.archive_body_length &&
-         creation_time == other.creation_time &&
-         freshness_time == other.freshness_time &&
-         error_code == other.error_code && title == other.title &&
-         file_path == other.file_path && file_size == other.file_size;
-}
-
-bool PrefetchItem::operator!=(const PrefetchItem& other) const {
-  return !(*this == other);
-}
-
-bool PrefetchItem::operator<(const PrefetchItem& other) const {
-  return offline_id < other.offline_id;
-}
-
-std::string PrefetchItem::ToString() const {
-  std::string s("PrefetchItem(");
-  s.append(base::Int64ToString(offline_id)).append(", ");
-  s.append(guid).append(", ");
-  s.append(client_id.ToString()).append(", ");
-  s.append(base::IntToString(static_cast<int>(state))).append(", ");
-  s.append(url.possibly_invalid_spec()).append(", ");
-  s.append(final_archived_url.possibly_invalid_spec()).append(", ");
-  s.append(base::IntToString(generate_bundle_attempts)).append(", ");
-  s.append(base::IntToString(get_operation_attempts)).append(", ");
-  s.append(base::IntToString(download_initiation_attempts)).append(", ");
-  s.append(operation_name).append(", ");
-  s.append(archive_body_name).append(", ");
-  s.append(base::IntToString(archive_body_length)).append(", ");
-  s.append(base::Int64ToString(store_utils::ToDatabaseTime(creation_time)))
-      .append(", ");
-  s.append(base::Int64ToString(store_utils::ToDatabaseTime(freshness_time)))
-      .append(", ");
-  s.append(base::IntToString(static_cast<int>(error_code))).append(", ");
-  s.append(base::UTF16ToUTF8(title)).append(", ");
-  s.append(file_path.AsUTF8Unsafe()).append(", ");
-  s.append(base::IntToString(static_cast<int>(file_size))).append(")");
-  return s;
-}
-
-std::ostream& operator<<(std::ostream& out, const PrefetchItem& pi) {
-  return out << pi.ToString();
-}
 
 }  // namespace offline_pages
