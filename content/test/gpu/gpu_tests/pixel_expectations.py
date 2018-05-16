89316ebf449504da4e14ec5b4038a2a1c5afa0ae
Yuly Novikov <ynovikov@chromium.org>
2018-05-16 21:08:47 +0000

    Update pixel test expectations
    Pixel_OffscreenCanvas2DResizeOnWorker flaky also on Linux
    Correct bug number for Pixel_WorkerRAF_OOPD expectation
    
    TBR=kbr@chromium.org
    
    Bug: 833902, 840394
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ie2d90eedc8b16baec2890f09dd8ef96c40293ada
    Reviewed-on: https://chromium-review.googlesource.com/1062194
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559285}
    Former-commit-id: 2c42a3e76aa04dcd2653a12b44a039647223a039

--- content/test/gpu/gpu_tests/pixel_expectations.py
+++ content/test/gpu/gpu_tests/pixel_expectations.py
@@ -95,8 +95,9 @@ class PixelExpectations(GpuTestExpectations):
               ['linux', 'mac', 'win'], bug=744658)
 
     # TODO(fserb): temporarily suppress this test.
-    self.Flaky('Pixel_OffscreenCanvas2DResizeOnWorker', ['mac'], bug=840394)
-    self.Flaky('Pixel_WorkerRAF_OOPD', ['android', 'nvidia'], bug=840394)
+    self.Flaky('Pixel_OffscreenCanvas2DResizeOnWorker',
+        ['linux', 'mac'], bug=840394)
+    self.Fail('Pixel_WorkerRAF_OOPD', ['android', 'nvidia'], bug=833902)
 
     # TODO(kbr): temporary suppression for new test.
     self.Flaky('Pixel_WebGLSadCanvas', ['mac'], bug=575305)
