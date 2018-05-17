c0404a72010768a4511c16a8aa43a080f1839a39
stkhapugin@chromium.org <stkhapugin@chromium.org>
2018-05-17 18:00:38 +0000

    Creates a OmniboxViewController.
    Creates a view controller to manage the omnibox and its container.
    
    Bug: 818637
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I35d1e86a0283efe5d983307b9a0fa3c342d5d06e
    Reviewed-on: https://chromium-review.googlesource.com/1059954
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559601}
    Former-commit-id: 84f931390d4b8297e2279871406aa2bdee5f6130

---
+++ ios/chrome/browser/ui/omnibox/omnibox_view_controller.mm
@@ -0,0 +1,58 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/omnibox/omnibox_view_controller.h"
+
+#import "ios/chrome/browser/ui/location_bar/location_bar_edit_view.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@interface OmniboxViewController ()
+
+// Override of UIViewController's view with a different type.
+@property(nonatomic, strong) LocationBarEditView* view;
+
+@property(nonatomic, strong) UIFont* textFieldFont;
+@property(nonatomic, strong) UIColor* textFieldTintColor;
+@property(nonatomic, strong) UIColor* textFieldTextColor;
+
+@end
+
+@implementation OmniboxViewController
+@synthesize textFieldFont = _textFieldFont;
+@synthesize textFieldTintColor = _textFieldTintColor;
+@synthesize textFieldTextColor = _textFieldTextColor;
+@dynamic view;
+
+- (instancetype)initWithFont:(UIFont*)font
+                   textColor:(UIColor*)textColor
+                   tintColor:(UIColor*)tintColor {
+  self = [super init];
+  if (self) {
+    _textFieldFont = font;
+    _textFieldTextColor = textColor;
+    _textFieldTintColor = tintColor;
+  }
+  return self;
+}
+
+#pragma mark - UIViewController
+
+- (void)loadView {
+  self.view =
+      [[LocationBarEditView alloc] initWithFrame:CGRectZero
+                                            font:self.textFieldFont
+                                       textColor:self.textFieldTextColor
+                                       tintColor:self.textFieldTintColor];
+}
+
+#pragma mark - public methods
+
+- (OmniboxTextFieldIOS*)textField {
+  return self.view.textField;
+}
+
+@end
