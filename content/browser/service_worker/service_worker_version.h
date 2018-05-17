181c1390304fbf6cb0879c0b35e6cc5e666de52a
Matt Falkenhagen <falken@chromium.org>
2018-05-17 07:53:44 +0000

    service worker: Add temporary instrumentation to debug startup failures.
    Somehow service workers are timing out and I don't see what could have
    changed. We are timing out in "Allocate Process" before a Start mesage
    is sent to the renderer, which suggests some thread on the brower
    process is hung somewhere. Try to get more info by generating a crash
    report which will also let us see if any unusual command line flags
    or features are enabled.
    
    Bug: 843456
    Change-Id: Ia4f8d8506e02ef0f0166530172ef033c300a1494
    Reviewed-on: https://chromium-review.googlesource.com/1063571
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559447}
    Former-commit-id: ed47a6c1ab57e35f5ee878c745953c63ec08d8b5

--- content/browser/service_worker/service_worker_version.h
+++ content/browser/service_worker/service_worker_version.h
@@ -864,6 +864,8 @@ class CONTENT_EXPORT ServiceWorkerVersion
 
   base::WeakPtrFactory<ServiceWorkerVersion> weak_factory_;
 
+  static bool s_dumped_for_timeout_;
+
   DISALLOW_COPY_AND_ASSIGN(ServiceWorkerVersion);
 };
 
