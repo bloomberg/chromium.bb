68e204175605642ca2528c3076cf654f01f52be2
Kent Tamura <tkent@chromium.org>
2018-05-17 09:12:35 +0000

    Replace --run-layout-test with --run-web-tests in LayoutTests/.
    Bug: 843412
    Change-Id: Ib6d25a9eaca5a7a6153bb1be2ddf48e3bb4538a9
    Reviewed-on: https://chromium-review.googlesource.com/1063430
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559465}
    Former-commit-id: bbb277a83ca482af83e0c6e760eda60d9a015cc9

--- third_party/WebKit/LayoutTests/http/tests/text-autosizing/resources/autosizingTest.js
+++ third_party/WebKit/LayoutTests/http/tests/text-autosizing/resources/autosizingTest.js
@@ -14,7 +14,7 @@ function initAutosizingTest() {
         setWindowSizeOverride(320, 480);
     } else if (window.console && console.warn) {
         console.warn("This test depends on Text Autosizing being enabled. Run with content shell "
-                + "and --run-layout-test or manually enable Text Autosizing and either use a "
+                + "and --run-web-tests or manually enable Text Autosizing and either use a "
                 + "mobile device with 320px device-width (like Nexus S or iPhone), or define "
                 + "DEBUG_TEXT_AUTOSIZING_ON_DESKTOP.");
     }
