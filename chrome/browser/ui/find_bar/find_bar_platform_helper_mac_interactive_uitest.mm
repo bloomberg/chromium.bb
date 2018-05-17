52cb2c5e5154b66acf34edaf7d27883a15fd1f53
Robert Liao <robliao@chromium.org>
2018-05-17 18:10:39 +0000

    Add Equivalent FindInPageControllerTest.GlobalPasteBoardClearMatches Using Interactive Text Input
    Views Textfields do not fire change updates when using SetText(),
    breaking any global pasteboard and Find Bar state updates.
    
    This workaround simulates text input, which does fire the events.
    
    BUG=843878,834754
    
    Change-Id: I482854bb221bfbdc5bd430c6a631e5de29ecd725
    Reviewed-on: https://chromium-review.googlesource.com/1063490
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559609}
    Former-commit-id: d308168d695f0e44a48cbd432460ca6c93913daf

--- chrome/browser/ui/find_bar/find_bar_platform_helper_mac_interactive_uitest.mm
+++ chrome/browser/ui/find_bar/find_bar_platform_helper_mac_interactive_uitest.mm
@@ -10,14 +10,24 @@
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
+#include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/find_bar/find_bar.h"
 #include "chrome/browser/ui/find_bar/find_bar_controller.h"
 #include "chrome/browser/ui/view_ids.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/interactive_test_utils.h"
+#include "chrome/test/base/ui_test_utils.h"
 #include "chrome/test/views/scoped_macviews_browser_mode.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #import "ui/base/cocoa/find_pasteboard.h"
+#include "url/gurl.h"
+
+const char kSimple[] = "simple.html";
+
+GURL GetURL(const std::string& filename) {
+  return ui_test_utils::GetTestUrl(base::FilePath().AppendASCII("find_in_page"),
+                                   base::FilePath().AppendASCII(filename));
+}
 
 class FindBarPlatformHelperMacInteractiveUITest
     : public InProcessBrowserTest,
@@ -77,6 +87,61 @@ IN_PROC_BROWSER_TEST_P(FindBarPlatformHelperMacInteractiveUITest,
   ASSERT_EQ(base::ASCIIToUTF16("asdf"), find_bar_string);
   EXPECT_EQ(find_bar_string, base::SysNSStringToUTF16(
                                  [[FindPasteboard sharedInstance] findText]));
+}
+
+// Equivalent to browser_tests
+// FindInPageControllerTest.GlobalPasteBoardClearMatches.
+// TODO(http://crbug.com/843878): Remove when referenced bug is fixed.
+IN_PROC_BROWSER_TEST_P(FindBarPlatformHelperMacInteractiveUITest,
+                       GlobalPasteBoardClearMatches) {
+  FindBarController* find_bar_controller = browser()->GetFindBarController();
+  ASSERT_NE(nullptr, find_bar_controller);
+
+  GURL url = GetURL(kSimple);
+  ui_test_utils::NavigateToURL(browser(), url);
+
+  base::string16 empty_string(base::ASCIIToUTF16(""));
+  find_bar_controller->SetText(empty_string);
+
+  chrome::Find(browser());
+  EXPECT_TRUE(
+      ui_test_utils::IsViewFocused(browser(), VIEW_ID_FIND_IN_PAGE_TEXT_FIELD));
+
+  ASSERT_TRUE(ui_test_utils::SendKeyPressSync(browser(), ui::VKEY_P, false,
+                                              false, false, false));
+  ASSERT_TRUE(ui_test_utils::SendKeyPressSync(browser(), ui::VKEY_A, false,
+                                              false, false, false));
+  ASSERT_TRUE(ui_test_utils::SendKeyPressSync(browser(), ui::VKEY_G, false,
+                                              false, false, false));
+  ASSERT_TRUE(ui_test_utils::SendKeyPressSync(browser(), ui::VKEY_E, false,
+                                              false, false, false));
+
+  EXPECT_EQ(base::ASCIIToUTF16("1/1"), find_bar_controller->find_bar()
+                                           ->GetFindBarTesting()
+                                           ->GetMatchCountText());
+
+  chrome::AddTabAt(browser(), GURL(), -1, true);
+  ui_test_utils::NavigateToURL(browser(), url);
+  chrome::Find(browser());
+
+  ASSERT_TRUE(ui_test_utils::SendKeyPressSync(browser(), ui::VKEY_T, false,
+                                              false, false, false));
+  ASSERT_TRUE(ui_test_utils::SendKeyPressSync(browser(), ui::VKEY_E, false,
+                                              false, false, false));
+  ASSERT_TRUE(ui_test_utils::SendKeyPressSync(browser(), ui::VKEY_X, false,
+                                              false, false, false));
+  ASSERT_TRUE(ui_test_utils::SendKeyPressSync(browser(), ui::VKEY_T, false,
+                                              false, false, false));
+
+  EXPECT_EQ(base::ASCIIToUTF16("1/1"), find_bar_controller->find_bar()
+                                           ->GetFindBarTesting()
+                                           ->GetMatchCountText());
+
+  // Go back to the first tab and verify that the match text is cleared.
+  browser()->tab_strip_model()->ActivateTabAt(0, false);
+  EXPECT_EQ(base::ASCIIToUTF16(""), find_bar_controller->find_bar()
+                                        ->GetFindBarTesting()
+                                        ->GetMatchCountText());
 }
 
 INSTANTIATE_TEST_CASE_P(
