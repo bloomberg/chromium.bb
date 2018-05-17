c8912526c2ce94e2dc7fbcab0618038aab030e18
Thomas Tangl <tangltom@chromium.org>
2018-05-17 17:17:36 +0000

    [Dice] Make profile card clickable when in auth error
    When sync is paused, i.e. the profile is in an auth
    error state, the profile card in the user menu
    is made clickable and directs to /syncSetup when
    clicked.
    
    Screenshot:
    https://drive.google.com/file/d/1UjnJta-iZZ6onG4SbSzlhfHt-3Ybre2G/view?usp=sharing
    
    Bug: 823783
    Change-Id: Idcd2554c9ebe6df8ed747f05b249d197e7fb614c
    Reviewed-on: https://chromium-review.googlesource.com/1063754
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Thomas Tangl <tangltom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559583}
    Former-commit-id: 84b4fc0cddf4bd41c57d07d6b35ecd0770ac2bab

--- chrome/browser/ui/views/profiles/profile_chooser_view.cc
+++ chrome/browser/ui/views/profiles/profile_chooser_view.cc
@@ -920,11 +920,16 @@ views::View* ProfileChooserView::CreateDiceSyncErrorView(
     sync_ui_util::AvatarSyncErrorType error,
     int button_string_id) {
   // Creates a view containing an error hover button displaying the current
-  // profile (non-selectable) and a blue button to resolve the error.
+  // profile (only selectable when sync is paused) and a blue button to resolve
+  // the error.
   views::View* view = new views::View();
+  const int current_profile_vertical_margin =
+      ChromeLayoutProvider::Get()->GetDistanceMetric(
+          views::DISTANCE_CONTROL_VERTICAL_TEXT_PADDING);
   view->SetLayoutManager(std::make_unique<views::BoxLayout>(
-      views::BoxLayout::kVertical, gfx::Insets(kMenuEdgeMargin),
-      kMenuEdgeMargin));
+      views::BoxLayout::kVertical,
+      gfx::Insets(current_profile_vertical_margin, 0, kMenuEdgeMargin, 0),
+      current_profile_vertical_margin));
 
   const bool show_sync_paused_ui = error == sync_ui_util::AUTH_ERROR;
   // Add profile hover button.
@@ -939,18 +944,23 @@ views::View* ProfileChooserView::CreateDiceSyncErrorView(
                                     : IDS_SYNC_ERROR_USER_MENU_TITLE),
       avatar_item.username);
 
-  if (!show_sync_paused_ui)
+  if (!show_sync_paused_ui) {
     current_profile->SetStyle(HoverButton::STYLE_ERROR);
-  current_profile->SetEnabled(false);
-  // Remove the default |HoverButton| border from |current_profile| so the
-  // insets of |BoxLayout| are used for aligment instead.
-  current_profile->SetBorder(nullptr);
+    current_profile->SetEnabled(false);
+  }
   view->AddChildView(current_profile);
+  current_profile_card_ = current_profile;
   // Add blue button.
   sync_error_button_ = views::MdTextButton::CreateSecondaryUiBlueButton(
       this, l10n_util::GetStringUTF16(button_string_id));
   sync_error_button_->set_id(error);
-  view->AddChildView(sync_error_button_);
+  // Add horizontal margin to blue button.
+  views::View* padded_view = new views::View();
+  padded_view->SetLayoutManager(std::make_unique<views::FillLayout>());
+  padded_view->SetBorder(
+      views::CreateEmptyBorder(0, kMenuEdgeMargin, 0, kMenuEdgeMargin));
+  padded_view->AddChildView(sync_error_button_);
+  view->AddChildView(padded_view);
   return view;
 }
 
