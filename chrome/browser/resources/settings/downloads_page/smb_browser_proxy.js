b1be6a383415234955ef1345a129565d02e9974f
Bailey Berro <baileyberro@chromium.org>
2018-05-17 02:04:05 +0000

    Display more informative Smb Mount error
    This change adds more descriptive mount errors in order to display more
    detailed reasons for a failed mount to the user.
    
    Bug: chromium:757625
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I11d46ca74d40d77276bce6839899ac723b3b77c6
    Reviewed-on: https://chromium-review.googlesource.com/1060428
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559395}
    Former-commit-id: 2c8078956c2941a2e49cf79de2a3006669c773af

--- chrome/browser/resources/settings/downloads_page/smb_browser_proxy.js
+++ chrome/browser/resources/settings/downloads_page/smb_browser_proxy.js
@@ -7,6 +7,20 @@
  * interact with the browser. Used only on Chrome OS.
  */
 
+/**
+ *  @enum {number}
+ *  These values must be kept in sync with the SmbMountResult enum in
+ *  chrome/browser/chromeos/smb_client/smb_service.h.
+ */
+const SmbMountResult = {
+  SUCCESS: 0,
+  UNKNOWN_FAILURE: 1,
+  AUTHENTICATION_FAILED: 2,
+  NOT_FOUND: 3,
+  UNSUPPORTED_DEVICE: 4,
+  MOUNT_EXISTS: 5,
+};
+
 cr.define('settings', function() {
   /** @interface */
   class SmbBrowserProxy {
