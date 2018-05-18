ae5fecfd469da3f873eb989235188c50477aa5fb
Matt Falkenhagen <falken@chromium.org>
2018-05-18 23:07:38 +0000

    Revert "service worker: Add temporary instrumentation to debug startup failures."
    This reverts commit 5b26adaec02e749d22cd64ee84c4000744c22af8 [formerly ed47a6c1ab57e35f5ee878c745953c63ec08d8b5].
    
    Reason for revert: We have enough reports from Canary now.
    
    Original change's description:
    > service worker: Add temporary instrumentation to debug startup failures.
    > 
    > Somehow service workers are timing out and I don't see what could have
    > changed. We are timing out in "Allocate Process" before a Start mesage
    > is sent to the renderer, which suggests some thread on the brower
    > process is hung somewhere. Try to get more info by generating a crash
    > report which will also let us see if any unusual command line flags
    > or features are enabled.
    > 
    > Bug: 843456
    > Change-Id: Ia4f8d8506e02ef0f0166530172ef033c300a1494
    > Reviewed-on: https://chromium-review.googlesource.com/1063571
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#559447}
    
    TBR=falken@chromium.org,kinuko@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 843456
    Change-Id: Ia6d740dff15a26e19c0533e83e11f1cd4237c368
    Reviewed-on: https://chromium-review.googlesource.com/1065691
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560090}
    Former-commit-id: 5c9a3ecea8791ddd24b4fd5a36f8b630f0eb763b

--- content/browser/service_worker/service_worker_version.cc
+++ content/browser/service_worker/service_worker_version.cc
@@ -11,7 +11,6 @@
 #include <string>
 
 #include "base/command_line.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/guid.h"
 #include "base/location.h"
 #include "base/memory/ref_counted.h"
@@ -1795,15 +1794,6 @@ void ServiceWorkerVersion::RecordStartWorkerResult(
   UMA_HISTOGRAM_ENUMERATION("ServiceWorker.StartWorker.TimeoutPhase",
                             phase,
                             EmbeddedWorkerInstance::STARTING_PHASE_MAX_VALUE);
-
-  if (IsInstalled(prestart_status) && !s_dumped_for_timeout_) {
-    // TODO(crbug.com/843456): Remove this instrumentation when the linked bug
-    // is fixed.
-    s_dumped_for_timeout_ = true;
-    DEBUG_ALIAS_FOR_GURL(script_url, script_url_);
-    base::debug::Alias(&phase);
-    base::debug::DumpWithoutCrashing();
-  }
 }
 
 bool ServiceWorkerVersion::MaybeTimeoutRequest(
@@ -2026,8 +2016,5 @@ bool ServiceWorkerVersion::IsStartWorkerAllowed() const {
 
   return true;
 }
-
-// static
-bool ServiceWorkerVersion::s_dumped_for_timeout_ = false;
 
 }  // namespace content
