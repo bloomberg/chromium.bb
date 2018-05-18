97f49248af4e67cf77cae78869c5b55fcdae5599
Aga Wronska <agawronska@chromium.org>
2018-05-18 16:45:03 +0000

    Update shelf visibility when display configuration changes.
    Shelf is supposed to be hidden on secondary diaplay on lock and login
    screens. To reporduce the bug external display needs to be set as primary
    screen and then attached while on lock screen. In that case external
    monitor is first added as secondary and later set as primary. Shelf
    visibility was not updated for the new secondary disply after the primary
    display was changed.
    
    Similar bug was also present for login screen. To fix it removing the early
    return from ShelfLayoutManager::UpdateVisibilityState was needed. The issue
    that required early return is already fixed.
    
    Bug: 824504
    Change-Id: I2e3b8e9d6427e3496e72651ba4be1bab538fe25c
    Reviewed-on: https://chromium-review.googlesource.com/1052488
    Commit-Queue: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559922}
    Former-commit-id: 8af5e106aa826f15b44303af358312d3a7e35c96

--- ash/shelf/shelf_unittest.cc
+++ ash/shelf/shelf_unittest.cc
@@ -5,6 +5,8 @@
 #include <utility>
 
 #include "ash/public/cpp/shelf_model.h"
+#include "ash/root_window_controller.h"
+#include "ash/session/test_session_controller_client.h"
 #include "ash/shelf/shelf.h"
 #include "ash/shelf/shelf_button.h"
 #include "ash/shelf/shelf_controller.h"
@@ -14,6 +16,7 @@
 #include "ash/shell.h"
 #include "ash/test/ash_test_base.h"
 #include "base/strings/string_number_conversions.h"
+#include "components/session_manager/session_manager_types.h"
 #include "mojo/public/cpp/bindings/associated_binding.h"
 
 namespace ash {
@@ -36,6 +39,13 @@ class ShelfTest : public AshTestBase {
   ShelfModel* shelf_model() { return shelf_model_; }
 
   ShelfViewTestAPI* test_api() { return test_.get(); }
+
+ protected:
+  Shelf* GetSecondaryShelf() {
+    return Shell::GetRootWindowControllerWithDisplayId(
+               GetSecondaryDisplay().id())
+        ->shelf();
+  }
 
  private:
   ShelfModel* shelf_model_ = nullptr;
@@ -111,6 +121,25 @@ TEST_F(ShelfTest, ShowOverflowBubble) {
   // Waits for all transitions to finish and there should be no crash.
   test_api()->RunMessageLoopUntilAnimationsDone();
   EXPECT_FALSE(shelf_widget->IsShowingOverflowBubble());
+}
+
+// Tests if shelf is hidden on secondary display after the primary display is
+// changed.
+TEST_F(ShelfTest, ShelfHiddenOnScreenOnSecondaryDisplay) {
+  for (const auto& state : {session_manager::SessionState::LOCKED,
+                            session_manager::SessionState::LOGIN_PRIMARY}) {
+    SCOPED_TRACE(base::StringPrintf("Testing state: %d", state));
+    GetSessionControllerClient()->SetSessionState(state);
+    UpdateDisplay("800x600,800x600");
+
+    EXPECT_EQ(SHELF_VISIBLE, GetPrimaryShelf()->GetVisibilityState());
+    EXPECT_EQ(SHELF_HIDDEN, GetSecondaryShelf()->GetVisibilityState());
+
+    SwapPrimaryDisplay();
+
+    EXPECT_EQ(SHELF_VISIBLE, GetPrimaryShelf()->GetVisibilityState());
+    EXPECT_EQ(SHELF_HIDDEN, GetSecondaryShelf()->GetVisibilityState());
+  }
 }
 
 }  // namespace
