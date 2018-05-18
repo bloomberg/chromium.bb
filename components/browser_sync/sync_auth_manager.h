d262337b45618cbcdb2d2d14605746d552454f7d
Marc Treib <treib@chromium.org>
2018-05-18 15:58:45 +0000

    ProfileSyncService: Move access token handling into SyncAuthManager
    Followup to https://crrev.com/c/1057508
    
    Bug: 842697
    Change-Id: Iec5b08efccf62427b4e1069ea829c7fadad02deb
    Reviewed-on: https://chromium-review.googlesource.com/1064062
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#559908}
    Former-commit-id: 938887e6d723ecc8ad4c6699630176c0ef258cc0

--- components/browser_sync/sync_auth_manager.h
+++ components/browser_sync/sync_auth_manager.h
@@ -5,12 +5,28 @@
 #ifndef COMPONENTS_BROWSER_SYNC_SYNC_AUTH_MANAGER_H_
 #define COMPONENTS_BROWSER_SYNC_SYNC_AUTH_MANAGER_H_
 
+#include <memory>
 #include <string>
 
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
+#include "base/timer/timer.h"
 #include "components/signin/core/browser/account_info.h"
+// TODO(treib): Needed for SyncTokenStatus. Move that to its own file instead.
+#include "components/sync/driver/sync_service.h"
+#include "google_apis/gaia/google_service_auth_error.h"
 #include "google_apis/gaia/oauth2_token_service.h"
+#include "net/base/backoff_entry.h"
 #include "services/identity/public/cpp/identity_manager.h"
+
+namespace identity {
+class PrimaryAccountAccessTokenFetcher;
+}
+
+namespace syncer {
+struct SyncCredentials;
+class SyncPrefs;
+}  // namespace syncer
 
 namespace browser_sync {
 
@@ -21,12 +37,13 @@ class ProfileSyncService;
 class SyncAuthManager : public identity::IdentityManager::Observer,
                         public OAuth2TokenService::Observer {
  public:
-  // |sync_service| must not be null and must outlive this object.
+  // |sync_service| and |sync_prefs| must not be null and must outlive this.
   // |identity_manager| and |token_service| may be null (this is the case if
   // local Sync is enabled), but if non-null, must outlive this object.
   // TODO(crbug.com/842697): Don't pass the ProfileSyncService in here. Instead,
   // pass a callback ("AccountStateChanged(new_state)").
   SyncAuthManager(ProfileSyncService* sync_service,
+                  syncer::SyncPrefs* sync_prefs,
                   identity::IdentityManager* identity_manager,
                   OAuth2TokenService* token_service);
   ~SyncAuthManager() override;
@@ -41,6 +58,8 @@ class SyncAuthManager : public identity::IdentityManager::Observer,
   AccountInfo GetAuthenticatedAccountInfo() const;
 
   // Returns whether a refresh token is available for the primary account.
+  // TODO(crbug.com/842697, crbug.com/825190): ProfileSyncService shouldn't have
+  // to care about the refresh token state.
   bool RefreshTokenIsAvailable() const;
 
   const GoogleServiceAuthError& GetLastAuthError() const {
@@ -48,10 +67,26 @@ class SyncAuthManager : public identity::IdentityManager::Observer,
   }
   bool IsAuthInProgress() const { return is_auth_in_progress_; }
 
-  // TODO(crbug.com/842697): These shouldn't be necessary anymore once all
-  // auth-related things have been moved here from ProfileSyncService.
-  void UpdateAuthErrorState(const GoogleServiceAuthError& error);
-  void ClearAuthError();
+  // Returns the credentials to be passed to the SyncEngine.
+  syncer::SyncCredentials GetCredentials() const;
+
+  const std::string& access_token() const { return access_token_; }
+
+  // Returns the state of the access token and token request, for display in
+  // internals UI.
+  syncer::SyncService::SyncTokenStatus GetSyncTokenStatus() const;
+
+  // Called by ProfileSyncService when the status of the connection to the Sync
+  // server changed. Updates auth error state accordingly.
+  void ConnectionStatusChanged(syncer::ConnectionStatus status);
+
+  // TODO(crbug.com/842697, crbug.com/825190): Make this private once
+  // ProfileSyncService doesn't care about the refresh token state anymore.
+  void RequestAccessToken();
+
+  // Clears all auth-related state (error, cached access token etc). Called
+  // when Sync is turned off.
+  void Clear();
 
   // identity::IdentityManager::Observer implementation.
   void OnPrimaryAccountSet(const AccountInfo& primary_account_info) override;
@@ -63,8 +98,18 @@ class SyncAuthManager : public identity::IdentityManager::Observer,
   void OnRefreshTokenRevoked(const std::string& account_id) override;
   void OnRefreshTokensLoaded() override;
 
+  // Test-only method for inspecting internal state.
+  bool IsRetryingAccessTokenFetchForTest() const;
+
  private:
+  void UpdateAuthErrorState(const GoogleServiceAuthError& error);
+  void ClearAuthError();
+
+  void AccessTokenFetched(const GoogleServiceAuthError& error,
+                          const std::string& access_token);
+
   ProfileSyncService* const sync_service_;
+  syncer::SyncPrefs* const sync_prefs_;
   identity::IdentityManager* const identity_manager_;
   OAuth2TokenService* const token_service_;
 
@@ -77,6 +122,23 @@ class SyncAuthManager : public identity::IdentityManager::Observer,
   // Set to true if a signin has completed but we're still waiting for the
   // engine to refresh its credentials.
   bool is_auth_in_progress_;
+
+  std::string access_token_;
+
+  // Pending request for an access token. Non-null iff there is a request
+  // ongoing.
+  std::unique_ptr<identity::PrimaryAccountAccessTokenFetcher>
+      ongoing_access_token_fetch_;
+
+  // If RequestAccessToken fails with transient error then retry requesting
+  // access token with exponential backoff.
+  base::OneShotTimer request_access_token_retry_timer_;
+  net::BackoffEntry request_access_token_backoff_;
+
+  // Info about the state of our access token, for display in the internals UI.
+  syncer::SyncService::SyncTokenStatus token_status_;
+
+  base::WeakPtrFactory<SyncAuthManager> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(SyncAuthManager);
 };
