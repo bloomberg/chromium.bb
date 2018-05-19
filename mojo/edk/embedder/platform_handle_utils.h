81562e42ff919d501f65ae85d7bdb29ab83cd583
Ken Rockot <rockot@chromium.org>
2018-05-19 21:41:41 +0000

    Mojo: Invitation API
    This is a public C API replacement for the EDK invitation API, i.e.
    Incoming/OutgoingBrokerClientInvitation et al. Here the new C calls
    are introduced and covered by unit tests.
    
    See the associated bug and the bugs it's blocking for more context
    and follow-up work.
    
    Bug: 844231
    Change-Id: Iffa2e5b08eefbe3476430eb420efe2f52f4ad195
    Reviewed-on: https://chromium-review.googlesource.com/1066807
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Jay Civelli <jcivelli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560164}
    Former-commit-id: a523cf48c629d7327cde4a5f36098f47ad50a696

--- mojo/edk/embedder/platform_handle_utils.h
+++ mojo/edk/embedder/platform_handle_utils.h
@@ -9,6 +9,8 @@
 #include "mojo/edk/embedder/platform_handle.h"
 #include "mojo/edk/embedder/scoped_platform_handle.h"
 #include "mojo/edk/system/system_impl_export.h"
+#include "mojo/public/c/system/platform_handle.h"
+#include "mojo/public/c/system/types.h"
 
 namespace mojo {
 namespace edk {
@@ -22,6 +24,14 @@ MOJO_SYSTEM_IMPL_EXPORT inline void CloseAllPlatformHandles(
        it != platform_handles->end(); ++it)
     it->CloseIfNecessary();
 }
+
+MOJO_SYSTEM_IMPL_EXPORT MojoResult MojoPlatformHandleToScopedPlatformHandle(
+    const MojoPlatformHandle* platform_handle,
+    ScopedPlatformHandle* out_handle);
+
+MOJO_SYSTEM_IMPL_EXPORT MojoResult
+ScopedPlatformHandleToMojoPlatformHandle(ScopedPlatformHandle handle,
+                                         MojoPlatformHandle* platform_handle);
 
 // Duplicates the given |PlatformHandle| (which must be valid). (Returns an
 // invalid |ScopedPlatformHandle| on failure.)
--- mojo/public/c/system/core.h
+++ mojo/public/c/system/core.h
@@ -12,6 +12,7 @@
 #include "mojo/public/c/system/buffer.h"
 #include "mojo/public/c/system/data_pipe.h"
 #include "mojo/public/c/system/functions.h"
+#include "mojo/public/c/system/invitation.h"
 #include "mojo/public/c/system/macros.h"
 #include "mojo/public/c/system/message_pipe.h"
 #include "mojo/public/c/system/platform_handle.h"
