a94cc7390c88dff3b5e58fc3fe276b50e392e2cc
Pavel Feldman <pfeldman@chromium.org>
2018-05-20 04:15:37 +0000

    Headless: complete the virtual time test migration to the new protocol harness.
    Change-Id: I9bb1a1aadc3f50f8b6e9bed38ae7eb0834e3c66a
    TBR: dgozman
    Reviewed-on: https://chromium-review.googlesource.com/1066992
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560192}
    Former-commit-id: a368c7081bbbd22f2da48114238e7df7575552e0

--- headless/test/headless_protocol_browsertest.cc
+++ headless/test/headless_protocol_browsertest.cc
@@ -62,8 +62,8 @@ class HeadlessProtocolBrowserTest
     CHECK(base::PathService::Get(base::DIR_SOURCE_ROOT, &src_dir));
     static const base::FilePath kTestsDirectory(
         FILE_PATH_LITERAL("headless/test/data/protocol"));
-    base::FilePath test_path = src_dir.Append(kTestsDirectory)
-        .AppendASCII(script_name_);
+    base::FilePath test_path =
+        src_dir.Append(kTestsDirectory).AppendASCII(script_name_);
     std::string script;
     if (!base::ReadFileToString(test_path, &script)) {
       ADD_FAILURE() << "Unable to read test at " << test_path;
@@ -71,10 +71,9 @@ class HeadlessProtocolBrowserTest
       return;
     }
     GURL test_url = embedded_test_server()->GetURL("/protocol/" + script_name_);
-    GURL page_url = embedded_test_server() ->GetURL(
-        "harness.test",
-        "/protocol/inspector-protocol-test.html?test=" +
-            test_url.spec() + "&script=" + EncodeQuery(script));
+    GURL page_url = embedded_test_server()->GetURL(
+        "harness.test", "/protocol/inspector-protocol-test.html?test=" +
+                            test_url.spec() + "&script=" + EncodeQuery(script));
     devtools_client_->GetPage()->Navigate(page_url.spec());
   }
 
@@ -199,11 +198,21 @@ class HeadlessProtocolBrowserTest
 // Headless-specific tests
 HEADLESS_PROTOCOL_TEST(VirtualTimeAdvance, "emulation/virtual-time-advance.js");
 HEADLESS_PROTOCOL_TEST(VirtualTimeBasics, "emulation/virtual-time-basics.js");
+HEADLESS_PROTOCOL_TEST(VirtualTimeCrossProcessNavigation,
+                       "emulation/virtual-time-cross-process-navigation.js");
+HEADLESS_PROTOCOL_TEST(VirtualTimeDetachFrame,
+                       "emulation/virtual-time-detach-frame.js");
 HEADLESS_PROTOCOL_TEST(VirtualTimeErrorLoop,
                        "emulation/virtual-time-error-loop.js");
+HEADLESS_PROTOCOL_TEST(VirtualTimeHistoryNavigation,
+                       "emulation/virtual-time-history-navigation.js");
 HEADLESS_PROTOCOL_TEST(VirtualTimeNoBlock404, "emulation/virtual-time-404.js");
 HEADLESS_PROTOCOL_TEST(VirtualTimeLocalStorage,
                        "emulation/virtual-time-local-storage.js");
+HEADLESS_PROTOCOL_TEST(VirtualTimePendingScript,
+                       "emulation/virtual-time-pending-script.js");
+HEADLESS_PROTOCOL_TEST(VirtualTimeRedirect,
+                       "emulation/virtual-time-redirect.js");
 HEADLESS_PROTOCOL_TEST(VirtualTimeSessionStorage,
                        "emulation/virtual-time-session-storage.js");
 HEADLESS_PROTOCOL_TEST(VirtualTimeStarvation,
@@ -218,10 +227,10 @@ HEADLESS_PROTOCOL_TEST(VirtualTimeVideo, "emulation/virtual-time-video.js");
 #define MAYBE_VirtualTimeTimerOrder VirtualTimeTimerOrder
 #define MAYBE_VirtualTimeTimerSuspend VirtualTimeTimerSuspend
 #endif
-HEADLESS_PROTOCOL_TEST(MAYBE_VirtualTimeTimerSuspend,
-                       "emulation/virtual-time-timer-suspended.js");
 HEADLESS_PROTOCOL_TEST(MAYBE_VirtualTimeTimerOrder,
                        "emulation/virtual-time-timer-order.js");
+HEADLESS_PROTOCOL_TEST(MAYBE_VirtualTimeTimerSuspend,
+                       "emulation/virtual-time-timer-suspended.js");
 #undef MAYBE_VirtualTimeTimerOrder
 #undef MAYBE_VirtualTimeTimerSuspend
 
