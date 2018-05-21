e783171cc1279776b3146405eee9b25e763209af
Tom Finegan <tomfinegan@google.com>
2018-05-21 13:05:33 -0700

    Remove decode_with_drops and decode_to_md5 from excludes.
    decode_with_drops.sh and decode_to_md5.sh do not belong in the exclude
    list.
    
    Change-Id: I75927c0dbdbbba32c6c6377bf67c3a0c4f1e14ad

--- test/examples.sh
+++ test/examples.sh
@@ -15,8 +15,7 @@
 example_tests=$(ls -r $(dirname $0)/*.sh)
 
 # List of script names to exclude.
-exclude_list="best_encode decode_to_md5 examples run_encodes tools_common \
-              decode_with_drops"
+exclude_list="best_encode examples run_encodes tools_common"
 
 # Filter out the scripts in $exclude_list.
 for word in ${exclude_list}; do
