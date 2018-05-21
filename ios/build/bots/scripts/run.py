9a85ee46ecdced43e0385629554facbb012ab9cc
Sergey Berezin <sergeyberezin@google.com>
2018-05-21 19:49:07 +0000

    [ios test_runner] Delete support for --xcode-version.
    This removes the support for system-wide Xcode.
    
    R=huangml@chromium.org
    
    Bug: 835036
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I925587983d618593771522a28ccd4bdf0cd16d78
    Reviewed-on: https://chromium-review.googlesource.com/1064829
    Reviewed-by: Menglu Huang <huangml@chromium.org>
    Commit-Queue: Sergey Berezin <sergeyberezin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560337}
    Former-commit-id: 7f9b4bf88647dbea6e3174192a64bc54f7e2a92b

--- ios/build/bots/scripts/run.py
+++ ios/build/bots/scripts/run.py
@@ -42,7 +42,6 @@ def main():
         args.iossim,
         args.platform,
         args.version,
-        args.xcode_version,
         args.xcode_build_version,
         args.out_dir,
         env_vars=args.env_var,
@@ -57,7 +56,6 @@ def main():
     else:
       tr = test_runner.DeviceTestRunner(
         args.app,
-        args.xcode_version,
         args.xcode_build_version,
         args.out_dir,
         env_vars=args.env_var,
@@ -171,13 +169,8 @@ def parse_args():
     '-b',
     '--xcode-build-version',
     help='Xcode build version to install.',
+    required=True,
     metavar='build_id',
-  )
-  parser.add_argument(
-    '-x',
-    '--xcode-version',
-    help='Version of Xcode to use. DEPRECATED by --xcode-build-version.',
-    metavar='ver',
   )
   parser.add_argument(
     '--xcode-path',
@@ -207,10 +200,6 @@ def parse_args():
     if not (args.iossim and args.platform and args.version):
       parser.error(
         'must specify all or none of -i/--iossim, -p/--platform, -v/--version')
-
-  if not (bool(args.xcode_version) ^ bool(args.xcode_build_version)):
-    parser.error(
-      'must specify exactly one of --xcode-build-version or --xcode-version')
 
   args_json = json.loads(args.args_json)
   args.env_var = args.env_var or []
