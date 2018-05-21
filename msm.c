1805a9b9e300f0a9e632dc48c2f82ac764d83fdc
Alexandre Courbot <acourbot@chromium.org>
2018-05-21 19:05:10 +0900

    msm: allow allocation of NV12 dumb buffers
    Cheza's codec validation requires the ability to import external buffers
    into the codec driver. However the only possible provider of such
    buffers so far is the display. Make it possible to allocate dumb NV12
    buffers by adding the format to the list of allowed formats, and
    adjusting the requested buffers' height to include room for the UV plane
    and extra padding the venus driver requires.
    
    BUG=b:80589901
    TEST=Checked that VDA unittest's TestDecodeTimeMedian passed in import
    mode on Cheza.
    
    Change-Id: I6993f654a3bf5e64cdf8a1c665c60bb4c7d9cc9a
    Reviewed-on: https://chromium-review.googlesource.com/1118078
    Commit-Ready: Alexandre Courbot <acourbot@chromium.org>
    Tested-by: Alexandre Courbot <acourbot@chromium.org>
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>

--- msm.c
+++ msm.c
@@ -16,10 +16,15 @@
 static const uint32_t render_target_formats[] = { DRM_FORMAT_ARGB8888, DRM_FORMAT_BGR888,
 						  DRM_FORMAT_XRGB8888 };
 
+static const uint32_t supported_formats[] = { DRM_FORMAT_NV12 };
+
 static int msm_init(struct driver *drv)
 {
 	drv_add_combinations(drv, render_target_formats, ARRAY_SIZE(render_target_formats),
 			     &LINEAR_METADATA, BO_USE_RENDER_MASK);
+
+	drv_add_combinations(drv, supported_formats, ARRAY_SIZE(supported_formats),
+			     &LINEAR_METADATA, BO_USE_TEXTURE_MASK | BO_USE_HW_VIDEO_DECODER);
 
 	return drv_modify_linear_combinations(drv);
 }
@@ -33,6 +38,11 @@ static int msm_bo_create(struct bo *bo, uint32_t width, uint32_t height, uint32_
 	/* HAL_PIXEL_FORMAT_YV12 requires that the buffer's height not be aligned. */
 	if (bo->format == DRM_FORMAT_YVU420_ANDROID)
 		height = bo->height;
+
+	/* Adjust the height of NV12 buffers to include room for the UV plane */
+	/* The extra 12KB at the end are a requirement of the Venus codec driver */
+	if (bo->format == DRM_FORMAT_NV12)
+		height += DIV_ROUND_UP(height, 2) + DIV_ROUND_UP(0x3000, width);
 
 	return drv_dumb_bo_create(bo, width, height, format, flags);
 }
