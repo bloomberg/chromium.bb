e8b7038fcd155aaafa6d89e933fa02361fade8b1
Dmitry Gozman <dgozman@chromium.org>
2018-05-21 20:00:03 +0000

    Move LineOrientationLayoutRectOutsets to line_orientation_utils
    This addition to LayoutRectOutsets is only used for inline layout,
    but brings an unnecessary dependency geometry -> text.
    Moving utility closer to the usage makes reasoning about deps easier.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Ib29fe76da950b1e1316a0d9573fca0b2c93c1c84
    Reviewed-on: https://chromium-review.googlesource.com/1062664
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560340}
    Former-commit-id: e71613a2b7d066b983c2904c72eefb0df3f6deec

--- third_party/blink/renderer/platform/geometry/layout_rect_outsets.h
+++ third_party/blink/renderer/platform/geometry/layout_rect_outsets.h
@@ -36,7 +36,6 @@
 #include "third_party/blink/renderer/platform/geometry/layout_size.h"
 #include "third_party/blink/renderer/platform/layout_unit.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
-#include "third_party/blink/renderer/platform/text/writing_mode.h"
 #include "third_party/blink/renderer/platform/wtf/allocator.h"
 
 namespace blink {
@@ -92,17 +91,6 @@ class PLATFORM_EXPORT LayoutRectOutsets {
   void Unite(const LayoutRectOutsets&);
 
   void FlipHorizontally() { std::swap(left_, right_); }
-
-  // Produces a new LayoutRectOutsets in line orientation
-  // (https://www.w3.org/TR/css-writing-modes-3/#line-orientation), whose
-  // - |top| is the logical 'over',
-  // - |right| is the logical 'line right',
-  // - |bottom| is the logical 'under',
-  // - |left| is the logical 'line left'.
-  LayoutRectOutsets LineOrientationOutsets(WritingMode) const;
-
-  // The same as |logicalOutsets|, but also adjusting for flipped lines.
-  LayoutRectOutsets LineOrientationOutsetsWithFlippedLines(WritingMode) const;
 
   bool operator==(const LayoutRectOutsets other) const {
     return Top() == other.Top() && Right() == other.Right() &&
