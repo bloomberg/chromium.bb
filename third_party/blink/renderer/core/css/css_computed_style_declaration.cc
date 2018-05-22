42538433098bffb7561fa145288c938d7fff6ed1
Anders Hartvoll Ruud <andruud@chromium.org>
2018-05-22 12:24:47 +0000

    Fix crash when setting aliases on computed style.
    The incoming CSSPropertyID may be an unresolved property, therefore
    CSSUnresolvedProperty::Get must be used rather than CSSProperty::Get.
    
    This bug exists in Chrome stable as well, but it was pretty hard to
    discover (by e.g. ClusterFuzz) because aliases were not enumerated until
    recently.
    
    R=futhark@chromium.org
    
    Bug: 844816
    Change-Id: I97c81764d2027f86004d3b02316cac44412ef0ea
    Reviewed-on: https://chromium-review.googlesource.com/1065993
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560540}
    Former-commit-id: b53a6c3a60b195e6a43f56c6de3abc68dec05c7a

--- third_party/blink/renderer/core/css/css_computed_style_declaration.cc
+++ third_party/blink/renderer/core/css/css_computed_style_declaration.cc
@@ -520,11 +520,10 @@ void CSSComputedStyleDeclaration::SetPropertyInternal(
     bool,
     SecureContextMode,
     ExceptionState& exception_state) {
-  // TODO(leviw): This code is currently unreachable, but shouldn't be.
   exception_state.ThrowDOMException(
       kNoModificationAllowedError,
       "These styles are computed, and therefore the '" +
-          CSSProperty::Get(id).GetPropertyNameString() +
+          CSSUnresolvedProperty::Get(id).GetPropertyNameString() +
           "' property is read-only.");
 }
 
