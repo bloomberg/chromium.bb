3dd644afde0530cc8e0b2ffe68f514f45c53e9da
Xing Liu <xingliu@chromium.org>
2018-05-22 19:16:44 +0000

    Tolerate file descriptor close error on Android.
    Chrome will crash when the user removes the SD card on certain Android
    devices while downloading file to SD card, since closing file
    descriptor will throw error.
    
    Instead, on Android, we should tolerate this error as same as other
    linux kernel based OS.
    
    Bug: 844148,792775
    Change-Id: I293fdcb4fd5277394efab6bca72089feb287cee0
    Reviewed-on: https://chromium-review.googlesource.com/1066185
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560721}
    Former-commit-id: f43149f8bf93d333ab678d7d2342fc88c7b6e405

--- base/files/scoped_file.cc
+++ base/files/scoped_file.cc
@@ -30,7 +30,8 @@ void ScopedFDCloseTraits::Free(int fd) {
   // a single open directory would bypass the entire security model.
   int ret = IGNORE_EINTR(close(fd));
 
-#if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_FUCHSIA)
+#if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_FUCHSIA) || \
+    defined(OS_ANDROID)
   // NB: Some file descriptors can return errors from close() e.g. network
   // filesystems such as NFS and Linux input devices. On Linux, macOS, and
   // Fuchsia's POSIX layer, errors from close other than EBADF do not indicate
