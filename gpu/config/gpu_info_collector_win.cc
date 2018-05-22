74e074752b6c5ac88f0caeddcb7ed45e4bcbf136
Corentin Wallez <cwallez@chromium.org>
2018-05-22 16:41:28 +0000

    Revert "gpu/config: use angle_gpu_info_util on Windows"
    This reverts commit d8678d29e56eb50352a71547ffdb498bb6bdc9b3 [formerly ff09f2699303e890b3e8d94e8b3ebfdf9d35b5ff].
    
    Reason for revert: might be introducing flaky freezes during GPU info collection.
    
    TBR=zmo@chromium.org
    
    Original change's description:
    > gpu/config: use angle_gpu_info_util on Windows
    >
    > BUG=angleproject:1874
    >
    > Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    > Change-Id: Icc5d832f7500b76bc8b7a4dfe9a99d2c61ac0b05
    > Reviewed-on: https://chromium-review.googlesource.com/655801
    > Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    > Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#557339}
    
    TBR=zmo@chromium.org,cwallez@chromium.org,georgesak@chromium.org
    
    Bug: angleproject:1874
    Change-Id: I7cfe9f0f8f262672467cd6f6b20691fced77b9bb
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Reviewed-on: https://chromium-review.googlesource.com/1067643
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560642}
    Former-commit-id: 0a46c60dee1bbc584c6e2de569a7d5cdbd60d94a

--- gpu/config/gpu_info_collector_win.cc
+++ gpu/config/gpu_info_collector_win.cc
@@ -4,21 +4,60 @@
 
 #include "gpu/config/gpu_info_collector.h"
 
+// This has to be included before windows.h.
+#include "third_party/re2/src/re2/re2.h"
+
+#include <windows.h>
+
+#include <cfgmgr32.h>
+#include <d3d11.h>
 #include <d3d12.h>
+#include <d3d9.h>
+#include <dxgi.h>
+#include <setupapi.h>
+#include <stddef.h>
+#include <stdint.h>
 
 #include "base/file_version_info_win.h"
+#include "base/files/file_enumerator.h"
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
+#include "base/logging.h"
+#include "base/metrics/field_trial.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/scoped_native_library.h"
+#include "base/strings/string16.h"
+#include "base/strings/string_number_conversions.h"
+#include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
+#include "base/strings/utf_string_conversions.h"
+#include "base/threading/thread.h"
 #include "base/trace_event/trace_event.h"
-#include "third_party/angle/src/gpu_info_util/SystemInfo.h"
-#include "third_party/re2/src/re2/re2.h"
+#include "base/win/scoped_com_initializer.h"
 #include "third_party/vulkan/include/vulkan/vulkan.h"
 
 namespace gpu {
 
 namespace {
+
+void DeviceIDToVendorAndDevice(const std::wstring& id,
+                               uint32_t* vendor_id,
+                               uint32_t* device_id) {
+  *vendor_id = 0;
+  *device_id = 0;
+  if (id.length() < 21)
+    return;
+  base::string16 vendor_id_string = id.substr(8, 4);
+  base::string16 device_id_string = id.substr(17, 4);
+  int vendor = 0;
+  int device = 0;
+  base::HexStringToInt(base::UTF16ToASCII(vendor_id_string), &vendor);
+  base::HexStringToInt(base::UTF16ToASCII(device_id_string), &device);
+  *vendor_id = vendor;
+  *device_id = device;
+}
 
 // These values are persisted to logs. Entries should not be renumbered and
 // numeric values should never be reused.
@@ -70,6 +109,168 @@ inline VulkanVersion ConvertToHistogramVulkanVersion(uint32_t vulkan_version) {
 }
 
 }  // namespace anonymous
+
+#if defined(GOOGLE_CHROME_BUILD) && defined(OFFICIAL_BUILD)
+// This function has a real implementation for official builds that can
+// be found in src/third_party/amd.
+void GetAMDVideocardInfo(GPUInfo* gpu_info);
+#else
+void GetAMDVideocardInfo(GPUInfo* gpu_info) {
+  DCHECK(gpu_info);
+  return;
+}
+#endif
+
+bool CollectDriverInfoD3D(const std::wstring& device_id, GPUInfo* gpu_info) {
+  TRACE_EVENT0("gpu", "CollectDriverInfoD3D");
+
+  // Display adapter class GUID from
+  // https://msdn.microsoft.com/en-us/library/windows/hardware/ff553426%28v=vs.85%29.aspx
+  GUID display_class = {0x4d36e968,
+                        0xe325,
+                        0x11ce,
+                        {0xbf, 0xc1, 0x08, 0x00, 0x2b, 0xe1, 0x03, 0x18}};
+
+  // create device info for the display device
+  HDEVINFO device_info =
+      ::SetupDiGetClassDevs(&display_class, NULL, NULL, DIGCF_PRESENT);
+  if (device_info == INVALID_HANDLE_VALUE) {
+    LOG(ERROR) << "Creating device info failed";
+    return false;
+  }
+
+  struct GPUDriver {
+    GPUInfo::GPUDevice device;
+    std::string driver_vendor;
+    std::string driver_version;
+    std::string driver_date;
+  };
+
+  std::vector<GPUDriver> drivers;
+
+  size_t primary_device = std::numeric_limits<size_t>::max();
+  bool found_amd = false;
+  bool found_intel = false;
+
+  DWORD index = 0;
+  SP_DEVINFO_DATA device_info_data;
+  device_info_data.cbSize = sizeof(device_info_data);
+  while (SetupDiEnumDeviceInfo(device_info, index++, &device_info_data)) {
+    WCHAR value[255];
+    if (SetupDiGetDeviceRegistryPropertyW(
+            device_info, &device_info_data, SPDRP_DRIVER, NULL,
+            reinterpret_cast<PBYTE>(value), sizeof(value), NULL)) {
+      HKEY key;
+      std::wstring driver_key = L"System\\CurrentControlSet\\Control\\Class\\";
+      driver_key += value;
+      LONG result = RegOpenKeyExW(HKEY_LOCAL_MACHINE, driver_key.c_str(), 0,
+                                  KEY_QUERY_VALUE, &key);
+      if (result == ERROR_SUCCESS) {
+        DWORD dwcb_data = sizeof(value);
+        std::string driver_version;
+        result = RegQueryValueExW(key, L"DriverVersion", NULL, NULL,
+                                  reinterpret_cast<LPBYTE>(value), &dwcb_data);
+        if (result == ERROR_SUCCESS)
+          driver_version = base::UTF16ToASCII(std::wstring(value));
+
+        std::string driver_date;
+        dwcb_data = sizeof(value);
+        result = RegQueryValueExW(key, L"DriverDate", NULL, NULL,
+                                  reinterpret_cast<LPBYTE>(value), &dwcb_data);
+        if (result == ERROR_SUCCESS)
+          driver_date = base::UTF16ToASCII(std::wstring(value));
+
+        std::string driver_vendor;
+        dwcb_data = sizeof(value);
+        result = RegQueryValueExW(key, L"ProviderName", NULL, NULL,
+                                  reinterpret_cast<LPBYTE>(value), &dwcb_data);
+        if (result == ERROR_SUCCESS)
+          driver_vendor = base::UTF16ToASCII(std::wstring(value));
+
+        wchar_t new_device_id[MAX_DEVICE_ID_LEN];
+        CONFIGRET status = CM_Get_Device_ID(
+            device_info_data.DevInst, new_device_id, MAX_DEVICE_ID_LEN, 0);
+
+        if (status == CR_SUCCESS) {
+          GPUDriver driver;
+
+          driver.driver_vendor = driver_vendor;
+          driver.driver_version = driver_version;
+          driver.driver_date = driver_date;
+          std::wstring id = new_device_id;
+
+          if (id.compare(0, device_id.size(), device_id) == 0)
+            primary_device = drivers.size();
+
+          uint32_t vendor_id = 0, device_id = 0;
+          DeviceIDToVendorAndDevice(id, &vendor_id, &device_id);
+          driver.device.vendor_id = vendor_id;
+          driver.device.device_id = device_id;
+          drivers.push_back(driver);
+
+          if (vendor_id == 0x8086)
+            found_intel = true;
+          if (vendor_id == 0x1002)
+            found_amd = true;
+        }
+
+        RegCloseKey(key);
+      }
+    }
+  }
+  SetupDiDestroyDeviceInfoList(device_info);
+  bool found = false;
+  if (found_amd && found_intel) {
+    // Potential AMD Switchable system found.
+    for (const auto& driver : drivers) {
+      if (driver.device.vendor_id == 0x8086) {
+        gpu_info->gpu = driver.device;
+      }
+
+      if (driver.device.vendor_id == 0x1002) {
+        gpu_info->driver_vendor = driver.driver_vendor;
+        gpu_info->driver_version = driver.driver_version;
+        gpu_info->driver_date = driver.driver_date;
+      }
+    }
+    GetAMDVideocardInfo(gpu_info);
+
+    if (!gpu_info->amd_switchable) {
+      bool amd_is_primary = false;
+      for (size_t i = 0; i < drivers.size(); ++i) {
+        const GPUDriver& driver = drivers[i];
+        if (driver.device.vendor_id == 0x1002) {
+          if (i == primary_device)
+            amd_is_primary = true;
+          gpu_info->gpu = driver.device;
+        } else {
+          gpu_info->secondary_gpus.push_back(driver.device);
+        }
+      }
+      // Some machines aren't properly detected as AMD switchable, but count
+      // them anyway. This may erroneously count machines where there are
+      // independent AMD and Intel cards and the AMD isn't hooked up to
+      // anything, but that should be rare.
+      gpu_info->amd_switchable = !amd_is_primary;
+    }
+    found = true;
+  } else {
+    for (size_t i = 0; i < drivers.size(); ++i) {
+      const GPUDriver& driver = drivers[i];
+      if (i == primary_device) {
+        found = true;
+        gpu_info->gpu = driver.device;
+        gpu_info->driver_vendor = driver.driver_vendor;
+        gpu_info->driver_version = driver.driver_version;
+        gpu_info->driver_date = driver.driver_date;
+      } else {
+        gpu_info->secondary_gpus.push_back(driver.device);
+      }
+    }
+  }
+
+  return found;
+}
 
 // DirectX 12 are included with Windows 10 and Server 2016.
 void GetGpuSupportedD3D12Version(GPUInfo* gpu_info) {
@@ -384,83 +585,46 @@ bool CollectContextGraphicsInfo(GPUInfo* gpu_info) {
   return true;
 }
 
-#if defined(GOOGLE_CHROME_BUILD) && defined(OFFICIAL_BUILD)
-// This function has a real implementation for official builds that can
-// be found in src/third_party/amd.
-bool GetAMDSwitchableInfo(bool* is_switchable,
-                          uint32_t* active_vendor_id,
-                          uint32_t* active_device_id);
-#else
-bool GetAMDSwitchableInfo(bool* is_switchable,
-                          uint32_t* active_vendor_id,
-                          uint32_t* active_device_id) {
-  return false;
-}
-#endif
-
-namespace {
-
-void CorrectAMDSwitchableInfo(angle::SystemInfo* system_info) {
-  bool is_switchable = false;
-  uint32_t active_vendor_id = 0;
-  uint32_t active_device_id = 0;
-  if (!GetAMDSwitchableInfo(&is_switchable, &active_vendor_id,
-                            &active_device_id)) {
-    return;
-  }
-
-  system_info->isAMDSwitchable = is_switchable;
-
-  bool found_active = false;
-  for (size_t i = 0; i < system_info->gpus.size(); ++i) {
-    if (system_info->gpus[i].vendorId == active_vendor_id &&
-        system_info->gpus[i].deviceId == active_device_id) {
-      found_active = true;
-      system_info->activeGPUIndex = static_cast<int>(i);
-      break;
-    }
-  }
-}
-
-}  // anonymous namespace
-
 bool CollectBasicGraphicsInfo(GPUInfo* gpu_info) {
-  TRACE_EVENT0("gpu", "CollectBasicGraphicsInfo");
+  TRACE_EVENT0("gpu", "CollectPreliminaryGraphicsInfo");
 
   DCHECK(gpu_info);
 
-  angle::SystemInfo system_info;
-  bool collection_successful = angle::GetSystemInfo(&system_info);
+  // nvd3d9wrap.dll is loaded into all processes when Optimus is enabled.
+  HMODULE nvd3d9wrap = GetModuleHandleW(L"nvd3d9wrap.dll");
+  gpu_info->optimus = nvd3d9wrap != nullptr;
 
-  // We can get more precise information about AMD switchable systems in
-  // official builds by querying the driver directly.
-  if (system_info.isAMDSwitchable) {
-    CorrectAMDSwitchableInfo(&system_info);
+  // Taken from http://www.nvidia.com/object/device_ids.html
+  DISPLAY_DEVICE dd;
+  dd.cb = sizeof(DISPLAY_DEVICE);
+  std::wstring id;
+  for (int i = 0; EnumDisplayDevices(NULL, i, &dd, 0); ++i) {
+    if (dd.StateFlags & DISPLAY_DEVICE_PRIMARY_DEVICE) {
+      id = dd.DeviceID;
+      break;
+    }
   }
 
-  // Always fill in gpu_info so that information, even partial is displayed in
-  // about:gpu
-  FillGPUInfoFromSystemInfo(gpu_info, &system_info);
-
-  if (collection_successful) {
-    return true;
+  if (id.length() <= 20) {
+    // EnumDisplayDevices returns an empty id when called inside a remote
+    // session (unless that session happens to be attached to the console). In
+    // that case, we do not want to fail, as we should be able to grab the
+    // device/vendor ids from the D3D context, below. Therefore, only fail if
+    // the device string is not one of either the RDP mirror driver "RDPUDD
+    // Chained DD" or the citrix display driver.
+    if (wcscmp(dd.DeviceString, L"RDPUDD Chained DD") != 0 &&
+        wcscmp(dd.DeviceString, L"Citrix Systems Inc. Display Driver") != 0) {
+      // Set vendor_id/device_id for blacklisting purpose.
+      gpu_info->gpu.vendor_id = 0xffff;
+      gpu_info->gpu.device_id = 0xfffe;
+      return false;
+    }
   }
 
-  // angle::GetSystemInfo fails in remote sessions because EnumDisplayDevices
-  // returns an empty id (unless that session happens to be attached to the
-  // console). We don't want to fail in that case as we should still have a
-  // device/vendor id. Only fail if the device string doesn't match the RDP
-  // mirror driver or the citrix display driver.
-  if (system_info.primaryDisplayDeviceId == "RDPUDD Chained DD" ||
-      system_info.primaryDisplayDeviceId ==
-          "Citrix Systems Inc. Display Driver") {
-    return true;
-  }
-
-  // On failure set fake vendor_id/device_id for blacklisting purpose.
-  gpu_info->gpu.vendor_id = 0xffff;
-  gpu_info->gpu.device_id = 0xfffe;
-  return false;
+  DeviceIDToVendorAndDevice(id, &gpu_info->gpu.vendor_id,
+                            &gpu_info->gpu.device_id);
+  // TODO(zmo): we only need to call CollectDriverInfoD3D() if we use ANGLE.
+  return CollectDriverInfoD3D(id, gpu_info);
 }
 
 }  // namespace gpu
