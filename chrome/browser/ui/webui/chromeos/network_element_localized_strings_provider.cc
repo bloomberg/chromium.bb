cb16e22e1b6f21a2e6298aadf1894e21d3bb2017
Steven Bennetts <stevenjb@chromium.org>
2018-05-22 16:57:11 +0000

    Network config: Support OpenVPN with no user cert
    Bug: 839195
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I20514f612345a16629e5a1370277885b6b2e3ad3
    Reviewed-on: https://chromium-review.googlesource.com/1066610
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Ben Chan <benchan@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560657}
    Former-commit-id: a02de1ab8e0c82fc05daaf68af1c1532998a48f6

--- chrome/browser/ui/webui/chromeos/network_element_localized_strings_provider.cc
+++ chrome/browser/ui/webui/chromeos/network_element_localized_strings_provider.cc
@@ -9,6 +9,7 @@
 #include "chrome/browser/chromeos/net/shill_error.h"
 #include "chrome/grit/generated_resources.h"
 #include "chromeos/login/login_state.h"
+#include "chromeos/network/network_connection_handler.h"
 #include "components/login/localized_values_builder.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
@@ -262,6 +263,7 @@ void AddConfigLocalizedStrings(content::WebUIDataSource* html_source) {
   } localized_strings[] = {
       {"networkCAUseDefault", IDS_SETTINGS_INTERNET_NETWORK_CA_USE_DEFAULT},
       {"networkCADoNotCheck", IDS_SETTINGS_INTERNET_NETWORK_CA_DO_NOT_CHECK},
+      {"networkNoUserCert", IDS_SETTINGS_INTERNET_NETWORK_NO_USER_CERT},
       {"networkCertificateName",
        IDS_SETTINGS_INTERNET_NETWORK_CERTIFICATE_NAME},
       {"networkCertificateNameHardwareBacked",
@@ -297,6 +299,8 @@ void AddErrorLocalizedStrings(content::WebUIDataSource* html_source) {
        IDS_NETWORK_ERROR_CANNOT_CHANGE_SHARED_CONFIG},
       {"Error.PolicyControlled", IDS_NETWORK_ERROR_POLICY_CONTROLLED},
       {"networkErrorNoUserCertificate", IDS_NETWORK_ERROR_NO_USER_CERT},
+      {NetworkConnectionHandler::kErrorPassphraseRequired,
+       IDS_NETWORK_ERROR_PASSPHRASE_REQUIRED},
       {"networkErrorUnknown", IDS_NETWORK_ERROR_UNKNOWN},
       // TODO(stevenjb): Move this id to settings_strings.grdp:
       {"networkErrorNotHardwareBacked",
