4d01e15077e6bbb8633419eb973aba77e877dce1
Chris Hamilton <chrisha@chromium.org>
2018-05-22 17:33:50 +0000

    Create TabManage lifecycle state change UKMs.
    This will allow us to evaluate the performance of lifecycles,
    determine how often and to which sites they occur, and allow analysis
    of the reasons for which they fail most often
    
    BUG=753486
    
    Change-Id: I64884a3314f2170c5e14e5c13dd5a95e5d258a3e
    Reviewed-on: https://chromium-review.googlesource.com/1062491
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560679}
    Former-commit-id: e0042c0f1b632e43b9f5343822bbdf1294fc6d1c

--- chrome/browser/resource_coordinator/lifecycle_state.h
+++ chrome/browser/resource_coordinator/lifecycle_state.h
@@ -7,17 +7,32 @@
 
 namespace resource_coordinator {
 
+// Note that these states are emitted via UKMs, so the integer values should
+// remain consistent.
 enum class LifecycleState {
-  // The LifecycleUnit is alive and active.
-  ACTIVE,
-  // The LifecycleUnit is pending a freeze.
-  PENDING_FREEZE,
+  // The LifecycleUnit is alive and active with no CPU throttling.
+  ACTIVE = 0,
+  // The LifecycleUnit is currently being CPU throttled.
+  THROTTLED = 1,
+  // The LifecycleUnit is currently transitioning to a FROZEN state.
+  PENDING_FREEZE = 2,
   // The LifecycleUnit is frozen.
-  FROZEN,
-  // The LifecycleUnit is pending a discard.
-  PENDING_DISCARD,
+  FROZEN = 3,
+  // The LifecycleUnit is currently transitioning to a DISCARDED state.
+  PENDING_DISCARD = 4,
   // The LifecycleUnit is discarded, and is consuming no system resources.
-  DISCARDED,
+  DISCARDED = 5,
+};
+
+// An enumeration of reasons why a lifecycle state change was applied. These are
+// also emitted via UKM so need to remain stable.
+enum class LifecycleStateChangeReason {
+  // Policy in the browser decided to initiate the state change.
+  BROWSER_INITIATED = 0,
+  // Policy in the renderer decided to initiate the state change.
+  RENDERER_INITIATED = 1,
+  // A system wide memory pressure condition initiated the state change.
+  SYSTEM_MEMORY_PRESSURE = 2,
 };
 
 }  // namespace resource_coordinator
