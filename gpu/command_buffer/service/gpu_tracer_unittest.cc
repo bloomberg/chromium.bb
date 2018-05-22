cea23d64cc6a2b8c8459628375cf09aafb4ba836
Zhenyao Mo <zmo@chromium.org>
2018-05-22 02:55:36 +0000

    [Reland] Redo GL/ES driver version detection.
    The motivation is we fail to process ARM Mali drivers.
    
    I scanned through gpuinfo.org and tested against almost all GL_VERSION patterns.
    
    BUG=839609,839618
    TEST=gl_unittests,gpu_unittests
    TBR=piman@chromium.org,kbr@chromium.org
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I74ab7cca87df5a83f1d310b1939d949e0e36818f
    Reviewed-on: https://chromium-review.googlesource.com/1068249
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560468}
    Former-commit-id: 1283f136595336721c75e6db0af10855c9d994bc

--- gpu/command_buffer/service/gpu_tracer_unittest.cc
+++ gpu/command_buffer/service/gpu_tracer_unittest.cc
@@ -84,7 +84,7 @@ class BaseGpuTest : public GpuServiceTest {
       gl_version = "2.1";
       extensions = "GL_EXT_timer_query";
     } else if (GetTimerType() == gl::GPUTiming::kTimerTypeDisjoint) {
-      gl_version = "opengl es 3.0";
+      gl_version = "OpenGL ES 3.0";
       extensions = "GL_EXT_disjoint_timer_query";
     } else if (GetTimerType() == gl::GPUTiming::kTimerTypeARB) {
       // TODO(sievers): The tracer should not depend on ARB_occlusion_query.
