f7941c01fe0a073f84df59e753e3f228ad64bc89
Kenneth Russell <kbr@chromium.org>
2018-05-22 21:57:16 +0000

    Run WebGL 2.0 conformance tests against Viz changes.
    This will increase the likelihood that intermittent failures caused by
    changes in this directory will be caught on the commit queue.
    
    Bug: 843290
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ie4d230c0290706c956d88f483b82b98f3a62f4bf
    Reviewed-on: https://chromium-review.googlesource.com/1069598
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560799}
    Former-commit-id: 3cb37960cf3a2e83edaf6f460e2316278eefb3c3

--- components/viz/PRESUBMIT.py
+++ components/viz/PRESUBMIT.py
@@ -19,9 +19,10 @@ def PostUploadHook(cl, change, output_api):
   """git cl upload will call this hook after the issue is created/modified.
 
   This hook modifies the CL description in order to run extra GPU
-  tests (in particular, on Android) in addition to the regular CQ try
-  bots. These tests don't yet run by default on
-  android_n5x_swarming_rel, but viz changes need to run them.
+  tests (in particular, the WebGL 2.0 conformance tests) in addition
+  to the regular CQ try bots. This test suite is too large to run
+  against all Chromium commits, but should be run against changes
+  likely to affect these tests.
 
   When adding/removing tests here, ensure that both gpu/PRESUBMIT.py and
   ui/gl/PRESUBMIT.py are updated.
@@ -29,6 +30,9 @@ def PostUploadHook(cl, change, output_api):
   return output_api.EnsureCQIncludeTrybotsAreAdded(
     cl,
     [
+      'luci.chromium.try:linux_optional_gpu_tests_rel',
+      'luci.chromium.try:mac_optional_gpu_tests_rel',
+      'luci.chromium.try:win_optional_gpu_tests_rel',
       'luci.chromium.try:android_optional_gpu_tests_rel',
     ],
     'Automatically added optional GPU tests to run on CQ.')
