1b347c4e324146195097aa508bc95f0916473695
Benoit Lize <lizeb@chromium.org>
2018-05-22 14:09:02 +0000

    tools/android: Add a script to show swap for a given process.
    Also converts code_pages_pss.py to the new parser.
    
    Example output:
    $ parse_smaps.py --pid 24452
    Total Swap Size (kB) = 4216
    /vendor/lib/egl/libGLES_mali.so r--p: 684 kB (Total Size: 816 kB)
    /dev/ashmem/dalvik-large object space allocation (deleted) rw-p: 384 kB (Total Size: 388 kB)
    [anon:libc_malloc] rw-p: 304 kB (Total Size: 512 kB)
    /dev/ashmem/dalvik-zygote space (deleted) rw-p: 276 kB (Total Size: 664 kB)
    [anon:libc_malloc] rw-p: 184 kB (Total Size: 512 kB)
    [anon:libc_malloc] rw-p: 96 kB (Total Size: 512 kB)
    [...]
    
    Bug: 845459
    Change-Id: I2123650699ba6cc8aed535741255b4096df5e3fe
    Reviewed-on: https://chromium-review.googlesource.com/1068872
    Reviewed-by: Matthew Cary <mattcary@chromium.org>
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560558}
    Former-commit-id: bf5c9291d5b7eb3808fa8c2be8c0796f48d001b0

--- tools/android/native_lib_memory/code_pages_pss.py
+++ tools/android/native_lib_memory/code_pages_pss.py
@@ -23,76 +23,22 @@ import os
 import re
 import sys
 
+import parse_smaps
+
 _SRC_PATH = os.path.join(
     os.path.dirname(__file__), os.pardir, os.pardir, os.pardir)
 sys.path.append(os.path.join(_SRC_PATH, 'third_party', 'catapult', 'devil'))
 from devil.android import device_utils
 
 
-def _GetPssFromProcSmapsLinesInKb(lines, chrome_package, verbose):
-  """Same as |_GetPssInKb()|, starting from the content of /proc/[pid]/smaps.
-
-  Args:
-    lines: ([str]) Content of /proc/[pid]/smaps.
-    chrome_package: (str) Chrome's package name, e.g. 'com.android.chrome'.
-    verbose: (bool) Verbose output.
-  """
-  SMAPS_ENTRY_START_RE = '^[0-9a-f]{1,16}-[0-9a-f]{1,16} '
-  assert re.search(SMAPS_ENTRY_START_RE,
-                   '7fffc6fbe000-7fffc6fc0000 r-xp '
-                   '00000000 00:00 0                          [vdso]')
-  # Various kernels have various amount of detail for each /proc/[pid]/smaps
-  # entry. Detect the start of a new entry to find the length on this kernel.
-  assert re.search(SMAPS_ENTRY_START_RE, lines[0])
-  length = 0
-  for (index, line) in enumerate(lines[1:]):
-    if re.search(SMAPS_ENTRY_START_RE, line):
-      length = index + 1
-      break
-  assert length
-  pss_offset = 0
-  for (index, line) in enumerate(lines[:length]):
-    if line.startswith('Pss:'):
-      pss_offset = index
-      break
-  assert pss_offset
-
+def _GetPssInKb(mappings, chrome_package, verbose):
   pss = 0
-  for (index, line) in enumerate(lines):
-    # Look for read-only, executable private mappings coming from Chrome's
-    # APK. The APK is in the file path.
-    # Example line:
-    # 9d241000-9fb82000 r-xp 009a0000 fd:00 5201 [...]
-    #     /data/app/com.android.chrome-G1amO4b7kPsNBi51zH8QWQ==/base.apk
-    if not re.search(SMAPS_ENTRY_START_RE, line):
-      continue
-    if not ('r-xp' in line and chrome_package in line):
-      continue
-    if verbose:
-        print '\n'.join(lines[index:index + length])
-    pss_line = lines[index + pss_offset]
-    assert pss_line.startswith('Pss:')
-    assert pss_line.endswith(' kB')
-    pss += int(pss_line[len('Pss:'):-2])
+  for mapping in mappings:
+    if chrome_package in mapping.pathname and mapping.permissions == 'r-xp':
+      pss += mapping.fields['Pss']
+      if verbose:
+        print mapping.ToString()
   return pss
-
-
-def _GetPssInKb(device, pid, chrome_package, verbose):
-  """Returns the PSS taken by chrome's code pages for a pid, in kB.
-
-  Args:
-    device: (device_utils.DeviceUtils) Device to get the data from.
-    pid: (int) PID of the process to inspect.
-    chrome_package: (str) Chrome's package name, e.g. 'com.android.chrome'.
-    verbose: (bool) Verbose output. Prints relevant /proc/[pid]/smaps entries
-                    if set.
-
-  Returns:
-    (int) PSS in kB from Chrome's code pages in this process.
-  """
-  command = ['cat', '/proc/%d/smaps' % pid]
-  lines = device.RunShellCommand(command, check_return=True)
-  return _GetPssFromProcSmapsLinesInKb(lines, chrome_package, verbose)
 
 
 def _CreateArgumentParser():
@@ -107,7 +53,6 @@ def _CreateArgumentParser():
 
 
 def main():
-  logging.basicConfig(level=logging.INFO)
   parser = _CreateArgumentParser()
   args = parser.parse_args()
   devices = device_utils.DeviceUtils.HealthyDevices()
@@ -117,11 +62,12 @@ def main():
   device = devices[0]
   device.EnableRoot()
   processes = device.ListProcesses(args.app_package)
+  logging.basicConfig(level=logging.INFO)
   logging.info('Processes:\n\t' + '\n\t'.join(p.name for p in processes))
   total_pss_kb = 0
   for process in processes:
-    total_pss_kb += _GetPssInKb(device, process.pid, args.chrome_package,
-                                args.verbose)
+    mappings = parse_smaps.ParseProcSmaps(device, process.pid)
+    total_pss_kb += _GetPssInKb(mappings, args.chrome_package, args.verbose)
   print 'Total PSS from code pages = %dkB' % total_pss_kb
 
 
