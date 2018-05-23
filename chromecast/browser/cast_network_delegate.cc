eab0cd1052b573dd881116fc3697b5a41a277577
Junbo Ke <juke@chromium.org>
2018-05-23 23:44:47 +0000

    [Chromecast] Add a NetworkDelegate.
    Bug: internal b/72129709
    Test: mannual test, cast_shell_unittests
    Change-Id: I54a0e8971e7869cd6c222d9abf089161b84ac467
    Reviewed-on: https://chromium-review.googlesource.com/1053293
    Commit-Queue: Junbo Ke <juke@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561312}
    Former-commit-id: 3e2804aff30c244f6ce511909033e473cb457f7a

--- chromecast/browser/cast_network_delegate.cc
+++ chromecast/browser/cast_network_delegate.cc
@@ -4,18 +4,45 @@
 
 #include "chromecast/browser/cast_network_delegate.h"
 
+#include <utility>
+
 #include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "chromecast/base/chromecast_switches.h"
+#include "chromecast/browser/cast_navigation_ui_data.h"
+#include "chromecast/browser/cast_network_request_interceptor.h"
+#include "content/public/browser/resource_request_info.h"
+#include "content/public/common/child_process_host.h"
+#include "net/base/net_errors.h"
 
 namespace chromecast {
 namespace shell {
 
-CastNetworkDelegate::CastNetworkDelegate() {
+std::unique_ptr<CastNetworkDelegate> CastNetworkDelegate::Create() {
+  return std::make_unique<CastNetworkDelegate>(
+      CastNetworkRequestInterceptor::Create());
+}
+
+CastNetworkDelegate::CastNetworkDelegate(
+    std::unique_ptr<CastNetworkRequestInterceptor> network_request_interceptor)
+    : network_request_interceptor_(std::move(network_request_interceptor)) {
+  DCHECK(network_request_interceptor_);
   DETACH_FROM_THREAD(thread_checker_);
 }
 
 CastNetworkDelegate::~CastNetworkDelegate() {
+}
+
+void CastNetworkDelegate::Initialize() {
+  network_request_interceptor_->Initialize();
+}
+
+bool CastNetworkDelegate::IsWhitelisted(const GURL& gurl,
+                                        const std::string& session_id,
+                                        int render_process_id,
+                                        bool for_device_auth) const {
+  return network_request_interceptor_->IsWhiteListed(
+      gurl, session_id, render_process_id, for_device_auth);
 }
 
 bool CastNetworkDelegate::OnCanAccessFile(
@@ -30,6 +57,51 @@ bool CastNetworkDelegate::OnCanAccessFile(
   LOG(WARNING) << "Could not access file " << original_path.value()
                << ". All file accesses are forbidden.";
   return false;
+}
+
+int CastNetworkDelegate::OnBeforeURLRequest(
+    net::URLRequest* request,
+    const net::CompletionCallback& callback,
+    GURL* new_url) {
+  if (!network_request_interceptor_->IsInitialized())
+    return net::OK;
+
+  // Get session id
+  std::string session_id;
+  const content::ResourceRequestInfo* request_info =
+      content::ResourceRequestInfo::ForRequest(request);
+  CastNavigationUIData* nav_data =
+      request_info ? static_cast<CastNavigationUIData*>(
+                         request_info->GetNavigationUIData())
+                   : nullptr;
+  if (nav_data) {
+    session_id = nav_data->session_id();
+  }
+
+  // Get render process PID
+  int render_process_id;
+  int render_frame_id;
+  if (!content::ResourceRequestInfo::GetRenderFrameForRequest(
+          request, &render_process_id, &render_frame_id)) {
+    render_process_id = content::ChildProcessHost::kInvalidUniqueID;
+  }
+  return network_request_interceptor_->OnBeforeURLRequest(
+      request, session_id, render_process_id, callback, new_url);
+}
+
+int CastNetworkDelegate::OnBeforeStartTransaction(
+    net::URLRequest* request,
+    const net::CompletionCallback& callback,
+    net::HttpRequestHeaders* headers) {
+  if (!network_request_interceptor_->IsInitialized())
+    return net::OK;
+  return network_request_interceptor_->OnBeforeStartTransaction(
+      request, callback, headers);
+}
+
+void CastNetworkDelegate::OnURLRequestDestroyed(net::URLRequest* request) {
+  if (network_request_interceptor_->IsInitialized())
+    network_request_interceptor_->OnURLRequestDestroyed(request);
 }
 
 }  // namespace shell
