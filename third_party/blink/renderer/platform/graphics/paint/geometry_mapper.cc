9c709289fb52e7fd0fa91c4f91a0da61202f8068
Xianzhu Wang <wangxianzhu@chromium.org>
2018-05-23 06:26:56 +0000

    [PE] ClipPath should clear tight flag of clip rect
    ClipPath in a ClipPaintPropertyNode may clip off pixels within the clip
    bounds, so the result clip rect should clear the tight flag.
    
    During raster invalidation, we won't compare non-tight clip rects for
    change of clip but will check the changed flag of the clip nodes to
    ensure correct raster invalidation.
    
    Bug: 845269
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I41e48e3eb15232d6cf88bd8986df5c3d8075d7db
    Reviewed-on: https://chromium-review.googlesource.com/1069671
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Tien-Ren Chen <trchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560968}
    Former-commit-id: f222ad1371257fc742880a2fb512619e036e8b7c

--- third_party/blink/renderer/platform/graphics/paint/geometry_mapper.cc
+++ third_party/blink/renderer/platform/graphics/paint/geometry_mapper.cc
@@ -287,12 +287,15 @@ FloatClipRect GeometryMapper::LocalToAncestorClipRect(
   return result;
 }
 
-static const FloatRoundedRect& GetClipRect(
-    const ClipPaintPropertyNode* clip_node,
-    OverlayScrollbarClipBehavior clip_behavior) {
-  return UNLIKELY(clip_behavior == kExcludeOverlayScrollbarSizeForHitTesting)
-             ? clip_node->ClipRectExcludingOverlayScrollbars()
-             : clip_node->ClipRect();
+static FloatClipRect GetClipRect(const ClipPaintPropertyNode* clip_node,
+                                 OverlayScrollbarClipBehavior clip_behavior) {
+  FloatClipRect clip_rect(
+      UNLIKELY(clip_behavior == kExcludeOverlayScrollbarSizeForHitTesting)
+          ? clip_node->ClipRectExcludingOverlayScrollbars()
+          : clip_node->ClipRect());
+  if (clip_node->ClipPath())
+    clip_rect.ClearIsTight();
+  return clip_rect;
 }
 
 FloatClipRect GeometryMapper::LocalToAncestorClipRectInternal(
@@ -310,7 +313,7 @@ FloatClipRect GeometryMapper::LocalToAncestorClipRectInternal(
   if (descendant->Parent() == ancestor_clip &&
       descendant->LocalTransformSpace() == ancestor_transform) {
     success = true;
-    return FloatClipRect(GetClipRect(descendant, clip_behavior));
+    return GetClipRect(descendant, clip_behavior);
   }
 
   FloatClipRect clip;
