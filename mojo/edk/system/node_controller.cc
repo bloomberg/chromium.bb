0f4b8900a11f7cf842a5742f4aa6f18e40a5bd4c
Luis Hector Chavez <lhchavez@google.com>
2018-05-23 00:39:19 +0000

    [mojo]: Avoid a crash when NodeController pending invitations diverge
    This change avoids a crash when NodeController::pending_broker_clients_
    and NodeController::pending_invitations_ diverge. This might happen with
    complex enough node topologies, where there is a node that proxies
    invitations to a set of other nodes.
    
    BUG=845709
    
    Change-Id: Ia678f464fafb69628600ec00dac19da3b6868fe0
    Reviewed-on: https://chromium-review.googlesource.com/1069728
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Luis Hector Chavez <lhchavez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#560857}
    Former-commit-id: 30a0b449c8f7036c300d808db96b391220b7698f

--- mojo/edk/system/node_controller.cc
+++ mojo/edk/system/node_controller.cc
@@ -929,9 +929,13 @@ void NodeController::OnAcceptBrokerClient(
   while (!pending_broker_clients.empty()) {
     const ports::NodeName& invitee_name = pending_broker_clients.front();
     auto it = pending_invitations_.find(invitee_name);
-    DCHECK(it != pending_invitations_.end());
-    broker->AddBrokerClient(invitee_name,
-                            it->second->CloneRemoteProcessHandle());
+    // If for any reason we don't have a pending invitation for the invitee,
+    // there's nothing left to do: we've already swapped the relevant state into
+    // the stack.
+    if (it != pending_invitations_.end()) {
+      broker->AddBrokerClient(invitee_name,
+                              it->second->CloneRemoteProcessHandle());
+    }
     pending_broker_clients.pop();
   }
 
