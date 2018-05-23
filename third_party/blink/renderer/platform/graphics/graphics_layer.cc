9d13de5834f52b731b2157f8e5b96d1c7a518a2e
danakj <danakj@chromium.org>
2018-05-23 17:20:18 +0000

    Revert "Stop preventing image layers from being marked as opaque."
    This reverts commit 5012b0a663cede5b7b4dcf6bfa1d3614ac29d2a2 [formerly 43090f2c8c2cde1683fc1d97354b63616ea1c671].
    
    Reason for revert: This makes the system more correct, the regression in memory is additional shaders to draw the opaque images, and we gain gpu (over)drawing performance from it.
    
    Original change's description:
    > Stop preventing image layers from being marked as opaque.
    > 
    > Before https://chromium-review.googlesource.com/c/chromium/src/+/1038565
    > these layers would change their contents-opaque state when the
    > GraphicsLayer did, even though the image layer has its contents-opaque
    > set when setting the image.
    > 
    > Disallowing that to change created a memory regression, as it must be
    > that some images are not opaque, but the GraphicsLayer becomes opaque
    > later and the image layer should be marked as such too.
    > 
    > R=​pdr@chromium.org
    > 
    > Bug: 838693, 841252
    > Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    > Change-Id: I97c007b46d0bafcb9ecf19fad77327053e978aa2
    > Reviewed-on: https://chromium-review.googlesource.com/1069194
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Commit-Queue: danakj <danakj@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#560824}
    
    TBR=danakj@chromium.org,pdr@chromium.org
    
    Change-Id: I59f9468a0fdb50234b26b3b6d63b22dfc5e2d7a6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 838693, 841252
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Reviewed-on: https://chromium-review.googlesource.com/1070003
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561137}
    Former-commit-id: 381752d85c690caca4de32ba2723726165c81ebf

--- third_party/blink/renderer/platform/graphics/graphics_layer.cc
+++ third_party/blink/renderer/platform/graphics/graphics_layer.cc
@@ -1248,7 +1248,7 @@ void GraphicsLayer::SetContentsToImage(
   }
 
   SetContentsTo(image_layer_.get(),
-                /*prevent_contents_opaque_changes=*/false);
+                /*prevent_contents_opaque_changes=*/true);
 }
 
 cc::Layer* GraphicsLayer::CcLayer() const {
