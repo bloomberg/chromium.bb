d85e13535cb8b66c818872e895b358d4019a192d
Dave Schuyler <dschuyler@chromium.org>
2018-05-23 23:14:55 +0000

    Revert "[base] escape Polymer binding characters in escape HTML"
    This reverts commit 74b820225dde1d427c50794d048ca75bb5741935 [formerly 801b2e7c907cef13941db6d268b9b8519d52d9d8].
    
    Reason for revert: Doesn't have the intended effect.
    
    Original change's description:
    > [base] escape Polymer binding characters in escape HTML
    > 
    > This cl escapes Google Polymer HTML binding characters when escaping
    > HTML (and the reverse when un-escaping).
    > 
    > Other changes were due to presubmit or linter errors/warnings.
    > 
    > Bug: 843736
    > Change-Id: I080bb610f80ab689f792c3d27797a9cd55b531f6
    > Reviewed-on: https://chromium-review.googlesource.com/1066732
    > Commit-Queue: Dave Schuyler <dschuyler@chromium.org>
    > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#561274}
    
    TBR=mmenke@chromium.org,dschuyler@chromium.org
    
    Change-Id: Idaddadd1223286caad17a412ba9f593bfaf57984
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 843736
    Reviewed-on: https://chromium-review.googlesource.com/1070891
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Commit-Queue: Dave Schuyler <dschuyler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561301}
    Former-commit-id: 48e970d6811691808ef92a8883a8f3275f348438

--- net/base/escape_unittest.cc
+++ net/base/escape_unittest.cc
@@ -430,11 +430,9 @@ TEST(EscapeTest, UnescapeBinaryURLComponent) {
 
 TEST(EscapeTest, EscapeForHTML) {
   const EscapeForHTMLCase tests[] = {
-      {"hello", "hello"},
-      {"<hello>", "&lt;hello&gt;"},
-      {"don\'t mess with me", "don&#39;t mess with me"},
-      {"[[alert('')]]", "&#91;&#91;alert(&#39;&#39;)&#93;&#93;"},
-      {"{{foo}}", "&#123;&#123;foo&#125;&#125;"},
+    { "hello", "hello" },
+    { "<hello>", "&lt;hello&gt;" },
+    { "don\'t mess with me", "don&#39;t mess with me" },
   };
   for (const auto& test : tests) {
     std::string result = EscapeForHTML(std::string(test.input));
@@ -444,18 +442,17 @@ TEST(EscapeTest, EscapeForHTML) {
 
 TEST(EscapeTest, UnescapeForHTML) {
   const EscapeForHTMLCase tests[] = {
-      {"", ""},
-      {"&lt;hello&gt;", "<hello>"},
-      {"don&#39;t mess with me", "don\'t mess with me"},
-      {"&lt;&gt;&amp;&quot;&#39;", "<>&\"'"},
-      {"& lt; &amp ; &; '", "& lt; &amp ; &; '"},
-      {"&amp;", "&"},
-      {"&quot;", "\""},
-      {"&#39;", "'"},
-      {"&lt;", "<"},
-      {"&gt;", ">"},
-      {"&amp; &", "& &"},
-      {"&#91;&#93;&#123;&#125;", "[]{}"},
+    { "", "" },
+    { "&lt;hello&gt;", "<hello>" },
+    { "don&#39;t mess with me", "don\'t mess with me" },
+    { "&lt;&gt;&amp;&quot;&#39;", "<>&\"'" },
+    { "& lt; &amp ; &; '", "& lt; &amp ; &; '" },
+    { "&amp;", "&" },
+    { "&quot;", "\"" },
+    { "&#39;", "'" },
+    { "&lt;", "<" },
+    { "&gt;", ">" },
+    { "&amp; &", "& &" },
   };
   for (const auto& test : tests) {
     base::string16 result = UnescapeForHTML(base::ASCIIToUTF16(test.input));
