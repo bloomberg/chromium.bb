13e36bcab7c7759cecf92857653e6d9e29b3917e
Marina Ciocea <marinaciocea@chromium.org>
2018-05-23 22:59:16 +0000

    Add observed audio service lifetime UMA metrics.
    Add lifetime metrics for audio service, logged from browser process in AudioServiceListener.
    List of added metrics:
    - ObservedStartupTime: how long it took audio service to start.
    - ObservedStartStatus: success, failure, or service started before listener.
    - ObservedServiceProcessTerminationStatus: disconnect (normal termination), kill (e.g through
      SIGKILL or taskmanager kill) or crash.
    - ObservedDowntime: time between service started or listener init and service stopped.
    - ObservedUptime: time between service started and service stopped (normal stop, kill or crash).
    
    Audio service design doc: https://docs.google.com/document/d/1s_Fd1WRDdpb5n6C2MSJjeC3fis6hULZwfKMeDd4K5tI/edit?usp=sharing
    
    Bug: 845407
    Change-Id: I1d55314a40d00e64942d63eba1b8de6b392beceb
    Reviewed-on: https://chromium-review.googlesource.com/1068709
    Commit-Queue: Marina Ciocea <marinaciocea@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Max Morin <maxmorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561296}
    Former-commit-id: a24a3874335d2b14a32f20d2b8911a788efd2a17

--- content/browser/renderer_host/media/audio_service_listener_unittest.cc
+++ content/browser/renderer_host/media/audio_service_listener_unittest.cc
@@ -4,6 +4,14 @@
 
 #include "content/browser/renderer_host/media/audio_service_listener.h"
 
+#include <utility>
+
+#include "base/process/kill.h"
+#include "base/test/histogram_tester.h"
+#include "base/test/simple_test_tick_clock.h"
+#include "content/public/browser/child_process_data.h"
+#include "content/public/browser/child_process_termination_info.h"
+#include "content/public/common/process_type.h"
 #include "services/audio/public/mojom/constants.mojom.h"
 #include "services/service_manager/public/cpp/identity.h"
 #include "services/service_manager/public/mojom/service_manager.mojom.h"
@@ -25,6 +33,123 @@ RunningServiceInfoPtr MakeTestServiceInfo(
 }
 
 }  // namespace
+
+struct AudioServiceListenerMetricsTest : public testing::Test {
+  AudioServiceListenerMetricsTest() {
+    test_clock.SetNowTicks(base::TimeTicks::Now());
+  }
+
+  base::SimpleTestTickClock test_clock;
+  base::HistogramTester histogram_tester;
+};
+
+TEST_F(AudioServiceListenerMetricsTest,
+       ServiceCreatedStartedStopped_LogsStartupTime_LogsUptime) {
+  AudioServiceListener::Metrics metrics(&test_clock);
+  metrics.ServiceCreated();
+  test_clock.Advance(base::TimeDelta::FromMilliseconds(42));
+  metrics.ServiceStarted();
+  histogram_tester.ExpectTimeBucketCount(
+      "Media.AudioService.ObservedStartupTime",
+      base::TimeDelta::FromMilliseconds(42), 1);
+  test_clock.Advance(base::TimeDelta::FromDays(2));
+  metrics.ServiceStopped();
+  histogram_tester.ExpectTimeBucketCount("Media.AudioService.ObservedUptime",
+                                         base::TimeDelta::FromDays(2), 1);
+
+  test_clock.Advance(base::TimeDelta::FromHours(5));
+  metrics.ServiceCreated();
+  metrics.ServiceStarted();
+  histogram_tester.ExpectTimeBucketCount("Media.AudioService.ObservedDowntime",
+                                         base::TimeDelta::FromHours(5), 1);
+}
+
+TEST_F(AudioServiceListenerMetricsTest,
+       CreateMetricsStartService_LogsDowntime) {
+  AudioServiceListener::Metrics metrics(&test_clock);
+  test_clock.Advance(base::TimeDelta::FromHours(12));
+  metrics.ServiceStarted();
+  histogram_tester.ExpectTimeBucketCount("Media.AudioService.ObservedDowntime",
+                                         base::TimeDelta::FromHours(12), 1);
+}
+
+TEST_F(AudioServiceListenerMetricsTest,
+       ServiceAlreadyRunningStopService_LogsUptime) {
+  AudioServiceListener::Metrics metrics(&test_clock);
+  metrics.ServiceAlreadyRunning();
+  test_clock.Advance(base::TimeDelta::FromMinutes(42));
+  metrics.ServiceStopped();
+  histogram_tester.ExpectTimeBucketCount("Media.AudioService.ObservedUptime",
+                                         base::TimeDelta::FromMinutes(42), 1);
+}
+
+TEST_F(AudioServiceListenerMetricsTest,
+       ServiceAlreadyRunningCreateService_LogsStartupTime) {
+  AudioServiceListener::Metrics metrics(&test_clock);
+  metrics.ServiceAlreadyRunning();
+  test_clock.Advance(base::TimeDelta::FromMilliseconds(2));
+  metrics.ServiceCreated();
+  test_clock.Advance(base::TimeDelta::FromMilliseconds(20));
+  metrics.ServiceStarted();
+  histogram_tester.ExpectTimeBucketCount(
+      "Media.AudioService.ObservedStartupTime",
+      base::TimeDelta::FromMilliseconds(20), 1);
+}
+
+// Check that if service was already started and ServiceStarted() is called,
+// ObservedStartupTime and ObservedDowntime are not logged and start time is
+// reset.
+TEST_F(AudioServiceListenerMetricsTest,
+       ServiceAlreadyRunningStartService_ResetStartTime) {
+  AudioServiceListener::Metrics metrics(&test_clock);
+  metrics.ServiceAlreadyRunning();
+  test_clock.Advance(base::TimeDelta::FromMilliseconds(20));
+  metrics.ServiceStarted();
+  histogram_tester.ExpectTotalCount("Media.AudioService.ObservedStartupTime",
+                                    0);
+  histogram_tester.ExpectTotalCount("Media.AudioService.ObservedDowntime", 0);
+  test_clock.Advance(base::TimeDelta::FromMilliseconds(200));
+  metrics.ServiceStopped();
+  histogram_tester.ExpectTimeBucketCount("Media.AudioService.ObservedUptime",
+                                         base::TimeDelta::FromMilliseconds(200),
+                                         1);
+}
+
+TEST(AudioServiceListenerTest, StartService_LogStartStatus) {
+  base::HistogramTester histogram_tester;
+  AudioServiceListener audio_service_listener(nullptr);
+  service_manager::Identity audio_service_identity(audio::mojom::kServiceName);
+  constexpr base::ProcessId pid(42);
+
+  std::vector<RunningServiceInfoPtr> instances;
+  instances.push_back(MakeTestServiceInfo(audio_service_identity, pid));
+  audio_service_listener.OnInit(std::move(instances));
+  histogram_tester.ExpectBucketCount(
+      "Media.AudioService.ObservedStartStatus",
+      static_cast<int>(
+          AudioServiceListener::Metrics::ServiceStartStatus::kAlreadyStarted),
+      1);
+  audio_service_listener.OnServiceStopped(audio_service_identity);
+
+  audio_service_listener.OnServiceCreated(
+      MakeTestServiceInfo(audio_service_identity, pid));
+  audio_service_listener.OnServiceStarted(audio_service_identity, pid);
+  histogram_tester.ExpectBucketCount(
+      "Media.AudioService.ObservedStartStatus",
+      static_cast<int>(
+          AudioServiceListener::Metrics::ServiceStartStatus::kSuccess),
+      1);
+  audio_service_listener.OnServiceStopped(audio_service_identity);
+
+  audio_service_listener.OnServiceCreated(
+      MakeTestServiceInfo(audio_service_identity, pid));
+  audio_service_listener.OnServiceFailedToStart(audio_service_identity);
+  histogram_tester.ExpectBucketCount(
+      "Media.AudioService.ObservedStartStatus",
+      static_cast<int>(
+          AudioServiceListener::Metrics::ServiceStartStatus::kFailure),
+      1);
+}
 
 TEST(AudioServiceListenerTest, OnInitWithoutAudioService_ProcessIdNull) {
   AudioServiceListener audio_service_listener(nullptr);
@@ -50,6 +175,8 @@ TEST(AudioServiceListenerTest, OnAudioServiceCreated_ProcessIdNotNull) {
   AudioServiceListener audio_service_listener(nullptr);
   service_manager::Identity audio_service_identity(audio::mojom::kServiceName);
   constexpr base::ProcessId pid(42);
+  audio_service_listener.OnServiceCreated(
+      MakeTestServiceInfo(audio_service_identity, pid));
   audio_service_listener.OnServiceStarted(audio_service_identity, pid);
   EXPECT_EQ(pid, audio_service_listener.GetProcessId());
 }
@@ -58,6 +185,8 @@ TEST(AudioServiceListenerTest, OnAudioServiceStopped_ProcessIdNull) {
   AudioServiceListener audio_service_listener(nullptr);
   service_manager::Identity audio_service_identity(audio::mojom::kServiceName);
   constexpr base::ProcessId pid(42);
+  audio_service_listener.OnServiceCreated(
+      MakeTestServiceInfo(audio_service_identity, pid));
   audio_service_listener.OnServiceStarted(audio_service_identity, pid);
   EXPECT_EQ(pid, audio_service_listener.GetProcessId());
   audio_service_listener.OnServiceStopped(audio_service_identity);
