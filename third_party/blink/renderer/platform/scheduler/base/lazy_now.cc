a5d97f7c32503d244fb96324e9ae7d714d3bf3e6
Greg Kraynov <kraynov@chromium.org>
2018-05-24 11:22:52 +0000

    Reintroduce Optional in LazyNow.
    Change-Id: I1aa07b72b008f76a94edc7c6372aff3bcea7062c
    Reviewed-on: https://chromium-review.googlesource.com/1070976
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Greg Kraynov <kraynov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561461}
    Former-commit-id: 02e83f93a6dff164e62ff3e5e20035d1b273caed

--- third_party/blink/renderer/platform/scheduler/base/lazy_now.cc
+++ third_party/blink/renderer/platform/scheduler/base/lazy_now.cc
@@ -9,9 +9,7 @@
 namespace base {
 namespace sequence_manager {
 
-LazyNow::LazyNow(TimeTicks now) : tick_clock_(nullptr), now_(now) {
-  DCHECK(!now.is_null());
-}
+LazyNow::LazyNow(TimeTicks now) : tick_clock_(nullptr), now_(now) {}
 
 LazyNow::LazyNow(const TickClock* tick_clock)
     : tick_clock_(tick_clock), now_() {
@@ -21,17 +19,17 @@ LazyNow::LazyNow(const TickClock* tick_clock)
 LazyNow::LazyNow(LazyNow&& move_from)
     : tick_clock_(move_from.tick_clock_), now_(move_from.now_) {
   move_from.tick_clock_ = nullptr;
-  move_from.now_ = TimeTicks();
+  move_from.now_ = nullopt;
 }
 
 TimeTicks LazyNow::Now() {
-  // TickClock might return null values only in tests, we're okay with
-  // extra calls which might occur in that case.
-  if (now_.is_null()) {
+  // It looks tempting to avoid using Optional and to rely on is_null() instead,
+  // but in some test environments clock intentionally starts from zero.
+  if (!now_) {
     DCHECK(tick_clock_);  // It can fire only on use after std::move.
     now_ = tick_clock_->NowTicks();
   }
-  return now_;
+  return *now_;
 }
 
 }  // namespace sequence_manager
