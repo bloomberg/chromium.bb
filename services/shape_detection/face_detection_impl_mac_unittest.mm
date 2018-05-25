e2370fe9e3138c5dbd78641320f51a2a067812b5
junweifu <junwei.fu@intel.com>
2018-05-25 01:14:55 +0000

    Add services unittests for landmarks points on Mac
    The landmarks is one point with the implementation of Core Image Framework,
    but an array of points along landmarks are more accurate with Vision Framework.
    
    Bug=835078
    
    Cq-Include-Trybots: luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win10_chromium_x64_rel_ng
    Change-Id: I8b09068bc95309fc66d3a8bd6726ac951815eb4f
    Reviewed-on: https://chromium-review.googlesource.com/1070766
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Commit-Queue: Junwei Fu <junwei.fu@intel.com>
    Cr-Commit-Position: refs/heads/master@{#561719}
    Former-commit-id: 2327f4cadd3ef6aa4e3b5ed29f84e81dcc368d28

--- services/shape_detection/face_detection_impl_mac_unittest.mm
+++ services/shape_detection/face_detection_impl_mac_unittest.mm
@@ -63,19 +63,20 @@ struct TestParams {
   const char* image_path;
   size_t num_faces;
   size_t num_landmarks;
+  size_t num_mouth_points;
   FaceDetectorFactory factory;
 } kTestParams[] = {
-    {false, 120, 120, "services/test/data/mona_lisa.jpg", 1, 3,
+    {false, 120, 120, "services/test/data/mona_lisa.jpg", 1, 3, 1,
      base::Bind(&CreateFaceDetectorImplMac)},
-    {true, 120, 120, "services/test/data/mona_lisa.jpg", 1, 3,
+    {true, 120, 120, "services/test/data/mona_lisa.jpg", 1, 3, 1,
      base::Bind(&CreateFaceDetectorImplMac)},
-    {false, 120, 120, "services/test/data/mona_lisa.jpg", 1, 4,
+    {false, 120, 120, "services/test/data/mona_lisa.jpg", 1, 4, 10,
      base::Bind(&CreateFaceDetectorImplMacVision)},
-    {false, 240, 240, "services/test/data/the_beatles.jpg", 3, 3,
+    {false, 240, 240, "services/test/data/the_beatles.jpg", 3, 3, 1,
      base::Bind(&CreateFaceDetectorImplMac)},
-    {true, 240, 240, "services/test/data/the_beatles.jpg", 3, 3,
+    {true, 240, 240, "services/test/data/the_beatles.jpg", 3, 3, 1,
      base::Bind(&CreateFaceDetectorImplMac)},
-    {false, 240, 240, "services/test/data/the_beatles.jpg", 4, 4,
+    {false, 240, 240, "services/test/data/the_beatles.jpg", 4, 4, 10,
      base::Bind(&CreateFaceDetectorImplMacVision)},
 };
 
@@ -85,15 +86,15 @@ class FaceDetectionImplMacTest : public TestWithParam<struct TestParams> {
 
   void DetectCallback(size_t num_faces,
                       size_t num_landmarks,
+                      size_t num_mouth_points,
                       std::vector<mojom::FaceDetectionResultPtr> results) {
     EXPECT_EQ(num_faces, results.size());
     for (const auto& face : results) {
       EXPECT_EQ(num_landmarks, face->landmarks.size());
-      if (face->landmarks.size() == 3u) {
-        EXPECT_EQ(mojom::LandmarkType::EYE, face->landmarks[0]->type);
-        EXPECT_EQ(mojom::LandmarkType::EYE, face->landmarks[1]->type);
-        EXPECT_EQ(mojom::LandmarkType::MOUTH, face->landmarks[2]->type);
-      }
+      EXPECT_EQ(mojom::LandmarkType::EYE, face->landmarks[0]->type);
+      EXPECT_EQ(mojom::LandmarkType::EYE, face->landmarks[1]->type);
+      EXPECT_EQ(mojom::LandmarkType::MOUTH, face->landmarks[2]->type);
+      EXPECT_EQ(num_mouth_points, face->landmarks[2]->locations.size());
     }
     Detection();
   }
@@ -151,10 +152,11 @@ TEST_P(FaceDetectionImplMacTest, ScanOneFace) {
   base::Closure quit_closure = run_loop.QuitClosure();
   // Send the image to Detect() and expect the response in callback.
   EXPECT_CALL(*this, Detection()).WillOnce(RunClosure(quit_closure));
-  impl_->Detect(*image,
-                base::BindOnce(&FaceDetectionImplMacTest::DetectCallback,
-                               base::Unretained(this), GetParam().num_faces,
-                               GetParam().num_landmarks));
+  impl_->Detect(
+      *image,
+      base::BindOnce(&FaceDetectionImplMacTest::DetectCallback,
+                     base::Unretained(this), GetParam().num_faces,
+                     GetParam().num_landmarks, GetParam().num_mouth_points));
 
   run_loop.Run();
 }
