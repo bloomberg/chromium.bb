971ccdd49245e74c54ee046e9bd202f66d658b14
Elly Fong-Jones <ellyjones@chromium.org>
2018-05-25 14:46:32 +0000

    macviews: mark PermissionBubbleInteractiveUITest as Cocoa-only
    TBR=sky@chromium.org
    
    Bug: 845389
    Change-Id: I694cf9a8c2f0dc8f82d163cc6f96b9752742a295
    Reviewed-on: https://chromium-review.googlesource.com/1073051
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#561875}
    Former-commit-id: cabbdc2e3c0cfece36888a45b4a1cc4cd9c88eaa

--- chrome/browser/ui/cocoa/permission_bubble/permission_bubble_cocoa_interactive_uitest.mm
+++ chrome/browser/ui/cocoa/permission_bubble/permission_bubble_cocoa_interactive_uitest.mm
@@ -20,6 +20,7 @@
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/interactive_test_utils.h"
+#include "chrome/test/views/scoped_macviews_browser_mode.h"
 #include "ui/base/test/ui_controls.h"
 #import "ui/base/test/windowed_nsnotification_observer.h"
 #include "ui/base/ui_base_features.h"
@@ -131,6 +132,8 @@ class PermissionBubbleInteractiveUITest
   base::test::ScopedFeatureList scoped_feature_list_;
 
  private:
+  test::ScopedMacViewsBrowserMode cocoa_browser_mode_{false};
+
   DISALLOW_COPY_AND_ASSIGN(PermissionBubbleInteractiveUITest);
 };
 
