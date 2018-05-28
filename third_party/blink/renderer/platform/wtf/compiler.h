4ca9b5f88e2bb5caea3841c288996df44971150c
Jeremy Roman <jbroman@chromium.org>
2018-05-28 06:29:08 +0000

    wtf: Remove GCC version detection logic.
    It is unused.
    
    Change-Id: I5ab0b86c78f145727dbf69cce7fe13683bbe969f
    Reviewed-on: https://chromium-review.googlesource.com/1074731
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562193}
    Former-commit-id: b2e751988fe4b20f39d91d171847d8a97d127c22

--- third_party/blink/renderer/platform/wtf/compiler.h
+++ third_party/blink/renderer/platform/wtf/compiler.h
@@ -29,17 +29,6 @@
 #include "base/compiler_specific.h"
 #include "build/build_config.h"
 
-#if defined(__GNUC__)
-#define GCC_VERSION \
-  (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
-#define GCC_VERSION_AT_LEAST(major, minor, patch) \
-  (GCC_VERSION >= (major * 10000 + minor * 100 + patch))
-#else
-/* Define this for !GCC compilers, just so we can write things like
- * GCC_VERSION_AT_LEAST(4, 1, 0). */
-#define GCC_VERSION_AT_LEAST(major, minor, patch) 0
-#endif
-
 /* ==== Compiler features ==== */
 
 /* NEVER_INLINE */
