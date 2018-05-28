75579f7d5a28c76d8d478589c008e84ae4be427f
Daniel Bratell <bratell@opera.com>
2018-05-28 13:13:01 +0000

    Use the same libjpeg in all of blink/platform
    The normal libjpeg renames some symbols with macros so if its
    headers are included together with libjpeg-turbo's headers
    in the same translation unit, there will be an inconsistent
    renaming of libjpeg symbols. This happened in some extreme
    jumbo configuration and resulted in confising linker errors.
    
    This patch changes an include so that jpeglib.h becomes included
    the same way everywhere.
    
    Change-Id: I7f122d0919d58371bb40dc0097a766b857b9815e
    Reviewed-on: https://chromium-review.googlesource.com/1073423
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#562243}
    Former-commit-id: c6b0194f7a4d9f494b2d51f46d2c332e2e5f4050

--- third_party/blink/renderer/platform/image-encoders/image_encoder.h
+++ third_party/blink/renderer/platform/image-encoders/image_encoder.h
@@ -7,8 +7,6 @@
 
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
-#include "third_party/libjpeg/jpeglib.h"          // for JPEG_MAX_DIMENSION
-#include "third_party/libwebp/src/webp/encode.h"  // for WEBP_MAX_DIMENSION
 #include "third_party/skia/include/core/SkStream.h"
 #include "third_party/skia/include/encode/SkJpegEncoder.h"
 #include "third_party/skia/include/encode/SkPngEncoder.h"
