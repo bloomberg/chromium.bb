570a73c8d582c6b5769b26134e8ac9e3cce1db31
Bence Béky <bnc@chromium.org>
2018-05-29 15:23:59 +0000

    Fix typo in comments.
    Comment-only change.
    
    Change-Id: I784462e75db527ddba04c70be21717ae4f3db9f4
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/1076408
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562432}
    Former-commit-id: a9838e05fd01cdada076134daf4d5e2eb35ead3e

--- base/containers/stack.h
+++ base/containers/stack.h
@@ -12,7 +12,7 @@
 namespace base {
 
 // Provides a definition of base::stack that's like std::stack but uses a
-// base::circular_queue instead of std::deque. Since std::stack is just a
+// base::circular_deque instead of std::deque. Since std::stack is just a
 // wrapper for an underlying type, we can just provide a typedef for it that
 // defaults to the base circular_deque.
 template <class T, class Container = circular_deque<T>>
