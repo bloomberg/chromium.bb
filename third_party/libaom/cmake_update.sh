f66ae0dce3fa341128570f9c602bc01e81691177
David Michael Barr <david.barr@samsung.com>
2018-05-29 19:53:52 +0000

    libaom: move aom_config to config sub dir
    The include structure has changed in libaom.
    
    Bug: 783519
    Change-Id: Ibea981e41e25ef5aa18c3c840b6ebfae9ceee526
    Reviewed-on: https://chromium-review.googlesource.com/1074797
    Reviewed-by: Johann Koenig <johannkoenig@google.com>
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Cr-Commit-Position: refs/heads/master@{#562559}
    Former-commit-id: 3a04bb03fdcf7860f8e4787e8db1c7799eaa69be

--- third_party/libaom/cmake_update.sh
+++ third_party/libaom/cmake_update.sh
@@ -35,6 +35,18 @@ function clean {
   rm -rf "${TMP}"
 }
 
+# Create empty temp and config directories.
+# $1 - Header file directory.
+function reset_dirs {
+  cd ..
+  rm -rf "${TMP}"
+  mkdir "${TMP}"
+  cd "${TMP}"
+
+  rm -fr "${CFG}/${1}"
+  mkdir -p "${CFG}/${1}/config"
+}
+
 if [ $# -ne 0 ]; then
   echo "Unknown option(s): ${@}"
   exit 1
@@ -47,7 +59,7 @@ fi
 # Generate libaom.config file for rtcd.pl.
 # $1 - platform/arch
 function print_config {
-  combined_config="$(cat ${CFG}/${1}/aom_config.h | grep -E ' +[01] *$')"
+  combined_config="$(cat ${CFG}/${1}/config/aom_config.h | grep -E ' +[01] *$')"
   combined_config="$(echo "$combined_config" | grep -v DO1STROUNDING)"
   combined_config="$(echo "$combined_config" | sed 's/[ \t]//g')"
   combined_config="$(echo "$combined_config" | sed 's/.*define//')"
@@ -70,27 +82,27 @@ function gen_rtcd_header {
     --sym=av1_rtcd ${3} \
     --config=libaom.config \
     "${SRC}/av1/common/av1_rtcd_defs.pl" \
-    > "${CFG}/${1}/av1_rtcd.h"
+    > "${CFG}/${1}/config/av1_rtcd.h"
 
-  clang-format -i "${CFG}/${1}/av1_rtcd.h"
+  clang-format -i "${CFG}/${1}/config/av1_rtcd.h"
 
   ${SRC}/build/make/rtcd.pl \
     --arch=${2} \
     --sym=aom_scale_rtcd ${3} \
     --config=libaom.config \
     "${SRC}/aom_scale/aom_scale_rtcd.pl" \
-    > "${CFG}/${1}/aom_scale_rtcd.h"
+    > "${CFG}/${1}/config/aom_scale_rtcd.h"
 
-  clang-format -i "${CFG}/${1}/aom_scale_rtcd.h"
+  clang-format -i "${CFG}/${1}/config/aom_scale_rtcd.h"
 
   ${SRC}/build/make/rtcd.pl \
     --arch=${2} \
     --sym=aom_dsp_rtcd ${3} \
     --config=libaom.config \
     "${SRC}/aom_dsp/aom_dsp_rtcd_defs.pl" \
-    > "${CFG}/${1}/aom_dsp_rtcd.h"
+    > "${CFG}/${1}/config/aom_dsp_rtcd.h"
 
-  clang-format -i "${CFG}/${1}/aom_dsp_rtcd.h"
+  clang-format -i "${CFG}/${1}/config/aom_dsp_rtcd.h"
 }
 
 # Generate Config files.
@@ -112,14 +124,16 @@ function gen_config_files {
     *mips*) ;;
     nacl) ;;
     *x64*|*ia32*)
-      egrep "#define [A-Z0-9_]+ [01]" aom_config.h | \
-        awk '{print "%define " $2 " " $3}' > aom_config.asm
+      egrep "#define [A-Z0-9_]+ [01]" config/aom_config.h | \
+        awk '{print "%define " $2 " " $3}' > config/aom_config.asm
       ;;
     *)
-      egrep "#define [A-Z0-9_]+ [01]" aom_config.h | \
+      egrep "#define [A-Z0-9_]+ [01]" config/aom_config.h | \
         awk '{print $2 " EQU " $3}' | \
-        perl "${SRC}/build/make/${ASM_CONV}" > aom_config.asm
+        perl "${SRC}/build/make/${ASM_CONV}" > config/aom_config.asm
   esac
+
+  cp config/aom_config.{h,c,asm} "${CFG}/${1}/config/"
 }
 
 function update_readme {
@@ -153,81 +167,51 @@ all_platforms+=" -DCONFIG_LOWBITDEPTH=1"
 toolchain="-DCMAKE_TOOLCHAIN_FILE=${SRC}/build/cmake/toolchains"
 
 echo "Generate linux/ia32 config files."
+reset_dirs linux/ia32
 gen_config_files linux/ia32 "${toolchain}/x86-linux.cmake ${all_platforms}"
 # libaom_srcs.gni and aom_version.h are shared.
 cp libaom_srcs.gni "${BASE}"
-cp aom_version.h "${CFG}"
-rm -f "${CFG}/linux/ia32"/*
-cp aom_config.h aom_config.c aom_config.asm "${CFG}/linux/ia32/"
+rm -f "${CFG}/aom_version.h"
+cp config/aom_version.h "${CFG}/config"
 gen_rtcd_header linux/ia32 x86 #--disable-avx2
 
-cd ..
-rm -rf "${TMP}"
-mkdir "${TMP}"
-cd "${TMP}"
-
 echo "Generate linux/x64 config files."
+reset_dirs linux/x64
 gen_config_files linux/x64 "${all_platforms}"
-rm -f "${CFG}/linux/x64"/*
-cp aom_config.h aom_config.c aom_config.asm "${CFG}/linux/x64/"
 gen_rtcd_header linux/x64 x86_64 #--disable-avx2
 
 # Windows looks like linux but with some minor tweaks. Cmake doesn't generate VS
 # project files on linux otherwise we would not resort to these hacks.
 
 echo "Generate win/x64 config files"
-rm -f "${CFG}/win/x64"/*
-cp "${CFG}/linux/x64"/* "${CFG}/win/x64/"
+reset_dirs win/x64
+cp "${CFG}/linux/x64/config"/* "${CFG}/win/x64/config/"
 sed -i.bak \
   -e 's/\(#define[[:space:]]INLINE[[:space:]]*\)inline/#define INLINE __inline/' \
   -e 's/\(#define[[:space:]]HAVE_PTHREAD_H[[:space:]]*\)1/#define HAVE_PTHREAD_H 0/' \
   -e 's/\(#define[[:space:]]HAVE_UNISTD_H[[:space:]]*\)1/#define HAVE_UNISTD_H 0/' \
   -e 's/\(#define[[:space:]]CONFIG_GCC[[:space:]]*\)1/#define CONFIG_GCC 0/' \
   -e 's/\(#define[[:space:]]CONFIG_MSVS[[:space:]]*\)0/#define CONFIG_MSVS 1/' \
-  "${CFG}/win/x64/aom_config.h"
-rm "${CFG}/win/x64/aom_config.h.bak"
-egrep "#define [A-Z0-9_]+ [01]" "${CFG}/win/x64/aom_config.h" \
-  | awk '{print "%define " $2 " " $3}' > "${CFG}/win/x64/aom_config.asm"
-
-cd ..
-rm -rf "${TMP}"
-mkdir "${TMP}"
-cd "${TMP}"
+  "${CFG}/win/x64/config/aom_config.h"
+rm "${CFG}/win/x64/config/aom_config.h.bak"
+egrep "#define [A-Z0-9_]+ [01]" "${CFG}/win/x64/config/aom_config.h" \
+  | awk '{print "%define " $2 " " $3}' > "${CFG}/win/x64/config/aom_config.asm"
 
 echo "Generate linux/arm config files."
+reset_dirs linux/arm
 gen_config_files linux/arm \
   "${toolchain}/armv7-linux-gcc.cmake -DENABLE_NEON=0 -DENABLE_NEON_ASM=0 ${all_platforms}"
-rm -f "${CFG}/linux/arm"/*
-# mkdir required only for initial commit
-mkdir -p "${CFG}/linux/arm"
-cp aom_config.h aom_config.c aom_config.asm "${CFG}/linux/arm/"
 gen_rtcd_header linux/arm armv7 --disable-neon
 
-cd ..
-rm -rf "${TMP}"
-mkdir "${TMP}"
-cd "${TMP}"
-
 echo "Generate linux/arm-neon config files."
+reset_dirs linux/arm-neon
 gen_config_files linux/arm-neon "${toolchain}/armv7-linux-gcc.cmake ${all_platforms}"
-rm -f "${CFG}/linux/arm-neon"/*
-# mkdir required only for initial commit
-mkdir -p "${CFG}/linux/arm-neon"
-cp aom_config.h aom_config.c aom_config.asm "${CFG}/linux/arm-neon/"
 gen_rtcd_header linux/arm-neon armv7
 
-cd ..
-rm -rf "${TMP}"
-mkdir "${TMP}"
-cd "${TMP}"
-
 echo "Generate linux/arm-neon-cpu-detect config files."
+reset_dirs linux/arm-neon-cpu-detect
 gen_config_files linux/arm-neon-cpu-detect \
   "${toolchain}/armv7-linux-gcc.cmake -DCONFIG_RUNTIME_CPU_DETECT=1 ${all_platforms}"
-rm -f "${CFG}/linux/arm-neon-cpu-detect"/*
-# mkdir required only for initial commit
-mkdir -p "${CFG}/linux/arm-neon-cpu-detect"
-cp aom_config.h aom_config.c aom_config.asm "${CFG}/linux/arm-neon-cpu-detect/"
 gen_rtcd_header linux/arm-neon-cpu-detect armv7
 
 clean
