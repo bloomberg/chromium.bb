c00c52973efbfb836d6899127925f7ec8208094d
Kyle Horimoto <khorimoto@google.com>
2018-05-30 00:28:04 +0000

    [CrOS MultiDevice] Add ActiveConnectionManager.
    This class manages zero or more active connections to remote devices.
    Each connection can be shared among one or more clients so that the
    underlying resources for the connection to not need to be duplicated.
    
    Bug: 824568, 752273
    Change-Id: If938b7ae7bc0e8a1029e72acc96ff3cfa5e9ea0c
    Reviewed-on: https://chromium-review.googlesource.com/1072801
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562670}
    Former-commit-id: 454a004b60bbfa4475daf2fa34e1786afce73f2a

---
+++ chromeos/services/secure_channel/fake_active_connection_manager.h
@@ -0,0 +1,88 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_SERVICES_SECURE_CHANNEL_FAKE_ACTIVE_CONNECTION_MANAGER_H_
+#define CHROMEOS_SERVICES_SECURE_CHANNEL_FAKE_ACTIVE_CONNECTION_MANAGER_H_
+
+#include <memory>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <vector>
+
+#include "base/macros.h"
+#include "chromeos/services/secure_channel/active_connection_manager.h"
+#include "chromeos/services/secure_channel/client_connection_parameters.h"
+#include "chromeos/services/secure_channel/connection_details.h"
+#include "chromeos/services/secure_channel/public/mojom/secure_channel.mojom.h"
+
+namespace chromeos {
+
+namespace secure_channel {
+
+class AuthenticatedChannel;
+
+// Test ActiveConnectionManager implementation.
+class FakeActiveConnectionManager : public ActiveConnectionManager {
+ public:
+  FakeActiveConnectionManager(ActiveConnectionManager::Delegate* delegate);
+  ~FakeActiveConnectionManager() override;
+
+  using DetailsToMetadataMap =
+      std::unordered_map<ConnectionDetails,
+                         std::tuple<ConnectionState,
+                                    std::unique_ptr<AuthenticatedChannel>,
+                                    std::vector<ClientConnectionParameters>>,
+                         ConnectionDetailsHash>;
+
+  DetailsToMetadataMap& connection_details_to_channel_map() {
+    return connection_details_to_channel_map_;
+  }
+
+  void SetDisconnecting(const ConnectionDetails& connection_details);
+  void SetDisconnected(const ConnectionDetails& connection_details);
+
+ private:
+  // ActiveConnectionManager:
+  ConnectionState GetConnectionState(
+      const ConnectionDetails& connection_details) const override;
+  void PerformAddActiveConnection(
+      std::unique_ptr<AuthenticatedChannel> authenticated_channel,
+      std::vector<ClientConnectionParameters> initial_clients,
+      const ConnectionDetails& connection_details) override;
+  void PerformAddClientToChannel(
+      ClientConnectionParameters client_connection_parameters,
+      const ConnectionDetails& connection_details) override;
+
+  DetailsToMetadataMap connection_details_to_channel_map_;
+
+  DISALLOW_COPY_AND_ASSIGN(FakeActiveConnectionManager);
+};
+
+// Test ActiveConnectionManager::Delegate implementation.
+class FakeActiveConnectionManagerDelegate
+    : public ActiveConnectionManager::Delegate {
+ public:
+  FakeActiveConnectionManagerDelegate();
+  ~FakeActiveConnectionManagerDelegate() override;
+
+  const std::unordered_map<ConnectionDetails, size_t, ConnectionDetailsHash>&
+  connection_details_to_num_disconnections_map() {
+    return connection_details_to_num_disconnections_map_;
+  }
+
+ private:
+  void OnDisconnected(const ConnectionDetails& connection_details) override;
+
+  std::unordered_map<ConnectionDetails, size_t, ConnectionDetailsHash>
+      connection_details_to_num_disconnections_map_;
+
+  DISALLOW_COPY_AND_ASSIGN(FakeActiveConnectionManagerDelegate);
+};
+
+}  // namespace secure_channel
+
+}  // namespace chromeos
+
+#endif  // CHROMEOS_SERVICES_SECURE_CHANNEL_FAKE_ACTIVE_CONNECTION_MANAGER_H_
