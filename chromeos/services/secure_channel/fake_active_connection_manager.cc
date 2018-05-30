bf1e439e584fc1377dbc91b0e593fcae70b93c8c
Kyle Horimoto <khorimoto@google.com>
2018-05-30 00:28:04 +0000

    [CrOS MultiDevice] Add ActiveConnectionManager.
    This class manages zero or more active connections to remote devices.
    Each connection can be shared among one or more clients so that the
    underlying resources for the connection to not need to be duplicated.
    
    Bug: 824568, 752273
    Change-Id: If938b7ae7bc0e8a1029e72acc96ff3cfa5e9ea0c
    Reviewed-on: https://chromium-review.googlesource.com/1072801
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562670}
    Former-commit-id: 454a004b60bbfa4475daf2fa34e1786afce73f2a

---
+++ chromeos/services/secure_channel/fake_active_connection_manager.cc
@@ -0,0 +1,97 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/services/secure_channel/fake_active_connection_manager.h"
+
+#include "base/logging.h"
+#include "base/stl_util.h"
+#include "chromeos/services/secure_channel/authenticated_channel.h"
+
+namespace chromeos {
+
+namespace secure_channel {
+
+FakeActiveConnectionManager::FakeActiveConnectionManager(
+    ActiveConnectionManager::Delegate* delegate)
+    : ActiveConnectionManager(delegate) {}
+
+FakeActiveConnectionManager::~FakeActiveConnectionManager() = default;
+
+void FakeActiveConnectionManager::SetDisconnecting(
+    const ConnectionDetails& connection_details) {
+  DCHECK(base::ContainsKey(connection_details_to_channel_map_,
+                           connection_details));
+
+  ConnectionState& state =
+      std::get<0>(connection_details_to_channel_map_[connection_details]);
+  DCHECK_EQ(ConnectionState::kActiveConnectionExists, state);
+
+  state = ConnectionState::kDisconnectingConnectionExists;
+}
+
+void FakeActiveConnectionManager::SetDisconnected(
+    const ConnectionDetails& connection_details) {
+  DCHECK(base::ContainsKey(connection_details_to_channel_map_,
+                           connection_details));
+  DCHECK_NE(
+      ConnectionState::kNoConnectionExists,
+      std::get<0>(connection_details_to_channel_map_[connection_details]));
+
+  size_t num_erased =
+      connection_details_to_channel_map_.erase(connection_details);
+  DCHECK_EQ(1u, num_erased);
+
+  OnChannelDisconnected(connection_details);
+}
+
+ActiveConnectionManager::ConnectionState
+FakeActiveConnectionManager::GetConnectionState(
+    const ConnectionDetails& connection_details) const {
+  if (!base::ContainsKey(connection_details_to_channel_map_,
+                         connection_details)) {
+    return ConnectionState::kNoConnectionExists;
+  }
+
+  return std::get<0>(connection_details_to_channel_map_.at(connection_details));
+}
+
+void FakeActiveConnectionManager::PerformAddActiveConnection(
+    std::unique_ptr<AuthenticatedChannel> authenticated_channel,
+    std::vector<ClientConnectionParameters> initial_clients,
+    const ConnectionDetails& connection_details) {
+  DCHECK(!base::ContainsKey(connection_details_to_channel_map_,
+                            connection_details));
+  connection_details_to_channel_map_[connection_details] = std::make_tuple(
+      ConnectionState::kActiveConnectionExists,
+      std::move(authenticated_channel), std::move(initial_clients));
+}
+
+void FakeActiveConnectionManager::PerformAddClientToChannel(
+    ClientConnectionParameters client_connection_parameters,
+    const ConnectionDetails& connection_details) {
+  DCHECK(base::ContainsKey(connection_details_to_channel_map_,
+                           connection_details));
+  std::get<2>(connection_details_to_channel_map_[connection_details])
+      .push_back(std::move(client_connection_parameters));
+}
+
+FakeActiveConnectionManagerDelegate::FakeActiveConnectionManagerDelegate() =
+    default;
+
+FakeActiveConnectionManagerDelegate::~FakeActiveConnectionManagerDelegate() =
+    default;
+
+void FakeActiveConnectionManagerDelegate::OnDisconnected(
+    const ConnectionDetails& connection_details) {
+  if (!base::ContainsKey(connection_details_to_num_disconnections_map_,
+                         connection_details)) {
+    connection_details_to_num_disconnections_map_[connection_details] = 0u;
+  }
+
+  ++connection_details_to_num_disconnections_map_[connection_details];
+}
+
+}  // namespace secure_channel
+
+}  // namespace chromeos
