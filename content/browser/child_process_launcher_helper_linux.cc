198722fd99137fd27c5479f80720b1c1cd4006c6
Adrienne Walker <enne@chromium.org>
2018-05-30 17:20:45 +0000

    Revert "oop raster: Plumb linux font service to the gpu process."
    This reverts commit 3f56d2fee44cd4fc8f4e0464b5951072194623f8 [formerly c44ffb1d59079c072e6f188c0775ed665e854c42].
    
    This is no longer needed as fonts have been implemented as a
    separate path.
    
    Bug: 847684
    Change-Id: I47ac41ab73917670750e41e8ca7ea11995fda728
    Reviewed-on: https://chromium-review.googlesource.com/1077661
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#562885}
    Former-commit-id: 1af0567156a195d5b778f724470c32dbe0750cc5

--- content/browser/child_process_launcher_helper_linux.cc
+++ content/browser/child_process_launcher_helper_linux.cc
@@ -15,7 +15,6 @@
 #include "content/public/common/content_switches.h"
 #include "content/public/common/result_codes.h"
 #include "content/public/common/sandboxed_process_launcher_delegate.h"
-#include "gpu/config/gpu_switches.h"
 #include "services/service_manager/sandbox/linux/sandbox_linux.h"
 #include "services/service_manager/zygote/common/common_sandbox_support_linux.h"
 #include "services/service_manager/zygote/common/zygote_handle.h"
@@ -50,10 +49,7 @@ bool ChildProcessLauncherHelper::BeforeLaunchOnLauncherThread(
   options->fds_to_remap = files_to_register.GetMappingWithIDAdjustment(
       base::GlobalDescriptors::kBaseDescriptor);
 
-  if (GetProcessType() == switches::kRendererProcess ||
-      (GetProcessType() == switches::kGpuProcess &&
-       base::CommandLine::ForCurrentProcess()->HasSwitch(
-           switches::kEnableOOPRasterization))) {
+  if (GetProcessType() == switches::kRendererProcess) {
     const int sandbox_fd = SandboxHostLinux::GetInstance()->GetChildSocket();
     options->fds_to_remap.push_back(
         std::make_pair(sandbox_fd, service_manager::GetSandboxFD()));
