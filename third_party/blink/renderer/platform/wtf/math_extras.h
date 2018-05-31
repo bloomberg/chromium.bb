da46246974351e6ac90327ad8e7d403adbbc224a
Jeremy Roman <jbroman@chromium.org>
2018-05-31 10:11:08 +0000

    wtf: Delete WTF::FastLog2.
    It is unused.
    
    Change-Id: Ie40a3735d0d6c26fbe5a6387bcb0324b80a69ece
    Reviewed-on: https://chromium-review.googlesource.com/1074733
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563186}
    Former-commit-id: 4b87d01f4888e7a63b4ebf036b9619506d4d0bef

--- third_party/blink/renderer/platform/wtf/math_extras.h
+++ third_party/blink/renderer/platform/wtf/math_extras.h
@@ -373,25 +373,4 @@ inline void doubleToInteger(double d, unsigned long long& value) {
   }
 }
 
-namespace WTF {
-
-inline unsigned FastLog2(unsigned i) {
-  unsigned log2 = 0;
-  if (i & (i - 1))
-    log2 += 1;
-  if (i >> 16)
-    log2 += 16, i >>= 16;
-  if (i >> 8)
-    log2 += 8, i >>= 8;
-  if (i >> 4)
-    log2 += 4, i >>= 4;
-  if (i >> 2)
-    log2 += 2, i >>= 2;
-  if (i >> 1)
-    log2 += 1;
-  return log2;
-}
-
-}  // namespace WTF
-
 #endif  // #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_WTF_MATH_EXTRAS_H_
