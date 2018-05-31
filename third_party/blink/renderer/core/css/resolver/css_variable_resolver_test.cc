c281050d5dd5699581590f113534467c3e5b19fd
Becca Hughes <beccahughes@chromium.org>
2018-05-31 22:13:01 +0000

    [CSS Env Vars] Parse env()
    Parse environment variables using the env() syntax. This does
    not include resolving them which will be in a subsequent CL.
    
    BUG=847651
    
    Change-Id: I80f502afe53146da6cc58b450b20c6b1c323d354
    Reviewed-on: https://chromium-review.googlesource.com/1077589
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563396}
    Former-commit-id: b5aa6bf1f11d6e94548b10f12a245933c78a0cfb

---
+++ third_party/blink/renderer/core/css/resolver/css_variable_resolver_test.cc
@@ -0,0 +1,116 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/html/html_element.h"
+#include "third_party/blink/renderer/core/style/computed_style.h"
+#include "third_party/blink/renderer/core/testing/page_test_base.h"
+
+namespace blink {
+
+namespace {
+
+// blue
+static const Color kFallbackTestColor = Color(0, 0, 255);
+
+// black
+static const Color kMainBgTestColor = Color(0, 0, 0);
+
+// red
+static const Color kTestColor = Color(255, 0, 0);
+
+}  // namespace
+
+class CSSVariableResolverTest : public PageTestBase {
+ public:
+  void SetUp() override {
+    PageTestBase::SetUp();
+
+    RuntimeEnabledFeatures::SetCSSEnvironmentVariablesEnabled(true);
+  }
+
+  void SetTestHTML(const String& value) {
+    GetDocument().body()->SetInnerHTMLFromString(
+        "<style>"
+        "  #target {"
+        "    --main-bg-color: black;"
+        "    --test: red;"
+        "    background-color: " +
+        value +
+        "  }"
+        "</style>"
+        "<div>"
+        "  <div id=target></div>"
+        "</div>");
+  }
+};
+
+TEST_F(CSSVariableResolverTest, ParseEnvVariable_Missing_NestedVar) {
+  SetTestHTML("env(--missing, var(--main-bg-color))");
+
+  // Check that the element has the background color provided by the
+  // nested variable.
+  Element* target = GetDocument().getElementById("target");
+  EXPECT_EQ(kMainBgTestColor,
+            target->EnsureComputedStyle()->VisitedDependentColor(
+                GetCSSPropertyBackgroundColor()));
+}
+
+TEST_F(CSSVariableResolverTest, ParseEnvVariable_Missing_NestedVar_Fallback) {
+  SetTestHTML("env(--missing, var(--missing, blue))");
+
+  // Check that the element has the fallback background color.
+  Element* target = GetDocument().getElementById("target");
+  EXPECT_EQ(kFallbackTestColor,
+            target->EnsureComputedStyle()->VisitedDependentColor(
+                GetCSSPropertyBackgroundColor()));
+}
+
+TEST_F(CSSVariableResolverTest, ParseEnvVariable_Missing_WithFallback) {
+  SetTestHTML("env(--missing, blue)");
+
+  // Check that the element has the fallback background color.
+  Element* target = GetDocument().getElementById("target");
+  EXPECT_EQ(kFallbackTestColor,
+            target->EnsureComputedStyle()->VisitedDependentColor(
+                GetCSSPropertyBackgroundColor()));
+}
+
+TEST_F(CSSVariableResolverTest, ParseEnvVariable_Valid) {
+  SetTestHTML("env(--test)");
+
+  // Check that the element has the background color provided by the variable.
+  Element* target = GetDocument().getElementById("target");
+  EXPECT_EQ(kTestColor, target->EnsureComputedStyle()->VisitedDependentColor(
+                            GetCSSPropertyBackgroundColor()));
+}
+
+TEST_F(CSSVariableResolverTest, ParseEnvVariable_Valid_WithFallback) {
+  SetTestHTML("env(--test, blue)");
+
+  // Check that the element has the background color provided by the variable.
+  Element* target = GetDocument().getElementById("target");
+  EXPECT_EQ(kTestColor, target->EnsureComputedStyle()->VisitedDependentColor(
+                            GetCSSPropertyBackgroundColor()));
+}
+
+TEST_F(CSSVariableResolverTest, ParseEnvVariable_WhenNested) {
+  SetTestHTML("var(--main-bg-color, var(--missing))");
+
+  // Check that the element has the background color provided by var().
+  Element* target = GetDocument().getElementById("target");
+  EXPECT_EQ(kMainBgTestColor,
+            target->EnsureComputedStyle()->VisitedDependentColor(
+                GetCSSPropertyBackgroundColor()));
+}
+
+TEST_F(CSSVariableResolverTest, ParseEnvVariable_WhenNested_WillFallback) {
+  SetTestHTML("var(--missing, env(--test))");
+
+  // Check that the element has the background color provided by the variable.
+  Element* target = GetDocument().getElementById("target");
+  EXPECT_EQ(kTestColor, target->EnsureComputedStyle()->VisitedDependentColor(
+                            GetCSSPropertyBackgroundColor()));
+}
+
+}  // namespace blink
