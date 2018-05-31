c257005cc7382fc424b078a582887019293eedd9
Jamie Madill <jmadill@chromium.org>
2018-05-31 02:30:56 +0000

    Upgrade WebGL Mac failure to fail.
    The flaky expectation for shaderoperator_common_functions was
    insufficient and the test was still flakily failing. See issue for
    more details.
    
    Bug: 820225
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ie30e47e8ffaa44ebade9723742c541f3571a3aee
    Tbr: kbr@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/1079793
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Jamie Madill <jmadill@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563123}
    Former-commit-id: 6b9dc426fe3b26dba05886f450e51db6786ab855

--- content/test/gpu/gpu_tests/webgl2_conformance_expectations.py
+++ content/test/gpu/gpu_tests/webgl2_conformance_expectations.py
@@ -722,7 +722,7 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
         ['mac', 'no_passthrough', 'intel'], bug=679692)
     self.Fail('deqp/functional/gles3/fbomultisample*',
         ['mac', 'intel'], bug=641209)
-    self.Flaky('deqp/functional/gles3/shaderoperator/common_functions.html',
+    self.Fail('deqp/functional/gles3/shaderoperator/common_functions.html',
         ['mac', 'intel'], bug=820225)
     self.Fail('deqp/functional/gles3/texturefiltering/2d_combinations_01.html',
         ['mac', 'intel'], bug=606074)
