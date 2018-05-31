57834465fc68e488415740ecd03aa9a544a4376b
Eugene But <eugenebut@google.com>
2018-05-31 14:35:23 +0000

    Mark NavigationAndLoadCallbacksTest.FailedLoad flaky only on device.
    This test was not flaky on the simulator.
    
    Bug: 845879
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I391d4f094551a38c22baf4b88755ef840fa30fea
    Reviewed-on: https://chromium-review.googlesource.com/1079319
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563223}
    Former-commit-id: 3a92725be1ac33dd765b350a089549ae97a08056

--- ios/web/web_state/navigation_and_load_callbacks_inttest.mm
+++ ios/web/web_state/navigation_and_load_callbacks_inttest.mm
@@ -1262,7 +1262,12 @@ TEST_F(NavigationAndLoadCallbacksTest, DownloadNavigation) {
 }
 
 // Tests failed load after the navigation is sucessfully finished.
-// TODO(crbug.com/845879): test is flaky (probably since crrev.com/1065632).
+// TODO(crbug.com/845879): test is flaky (probably since crrev.com/1056203).
+#if TARGET_IPHONE_SIMULATOR
+#define MAYBE_FailedLoad FailedLoad
+#else
+#define MAYBE_FailedLoad FLAKY_FailedLoad
+#endif
 TEST_F(NavigationAndLoadCallbacksTest, FLAKY_FailedLoad) {
   GURL url = test_server_->GetURL("/exabyte_response");
 
