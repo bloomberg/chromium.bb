63d4ce127fc81ec1738b6e4715fa50b784256240
Christian Egli <christian.egli@sbs.ch>
2018-05-31 14:17:51 +0200

    Make clang-format happy

--- liblouis/compileTranslationTable.c
+++ liblouis/compileTranslationTable.c
@@ -4483,8 +4483,7 @@ char **EXPORT_CALL
 _lou_resolveTable(const char *tableList, const char *base) {
 	char **tableFiles = (*tableResolver)(tableList, base);
 	char **result = copyStringArray(tableFiles);
-	if (tableResolver == &_lou_defaultTableResolver)
-		free_tablefiles(tableFiles);
+	if (tableResolver == &_lou_defaultTableResolver) free_tablefiles(tableFiles);
 	return result;
 }
 
