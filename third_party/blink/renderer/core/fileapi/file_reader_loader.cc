381ebdb7dfc7c630cca9941bc5bcd08cd57251c2
Daniel Murphy <dmurph@chromium.org>
2018-06-01 03:18:16 +0000

    FileReaderLoader: fixed setting net_error_
    TBR: mek@chromium.org
    Bug: 846046
    Change-Id: I212d5f8c5633b821b4d43cdf47cc575a2bd907bc
    Reviewed-on: https://chromium-review.googlesource.com/1081671
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563516}
    Former-commit-id: 400fbde7f97580933b8cd7df38ffd4230552492a

--- third_party/blink/renderer/core/fileapi/file_reader_loader.cc
+++ third_party/blink/renderer/core/fileapi/file_reader_loader.cc
@@ -314,6 +314,7 @@ void FileReaderLoader::OnComplete(int32_t status, uint64_t data_length) {
                                   file_reader_loader_read_errors_histogram,
                                   ("Storage.Blob.FileReaderLoader.ReadError"));
   if (status != net::OK) {
+    net_error_ = status;
     file_reader_loader_read_errors_histogram.Sample(std::max(0, -net_error_));
     Failed(status == net::ERR_FILE_NOT_FOUND ? FileError::kNotFoundErr
                                              : FileError::kNotReadableErr,
