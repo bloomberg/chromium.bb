382e942b488d24cda1c97fb881137ef31fcea5dd
Amy Qiu <amyqiu@google.com>
2018-06-01 21:23:50 +0000

    Rename pages in pathological_mobile_sites
    Rename pages to be consistent with naming convention in rendering page
    sets
    
    Bug: 847857
    Change-Id: I45aebbd5a4f3cae9be7a6858c483f3ef4cec3df2
    Reviewed-on: https://chromium-review.googlesource.com/1082989
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Amy Qiu <amyqiu@google.com>
    Cr-Commit-Position: refs/heads/master@{#563801}
    Former-commit-id: a74b47ec199c9e7a05ca34386e51af3f42608a7b

--- tools/perf/page_sets/pathological_mobile_sites.py
+++ tools/perf/page_sets/pathological_mobile_sites.py
@@ -8,11 +8,11 @@ from telemetry import story
 
 class PathologicalMobileSitesPage(page_module.Page):
 
-  def __init__(self, url, page_set):
+  def __init__(self, name, url, page_set):
     super(PathologicalMobileSitesPage, self).__init__(
         url=url, page_set=page_set,
         shared_page_state_class=shared_page_state.SharedMobilePageState,
-        name=url)
+        name=name)
 
   def RunPageInteractions(self, action_runner):
     with action_runner.CreateGestureInteraction('ScrollAction'):
@@ -28,18 +28,21 @@ class PathologicalMobileSitesPageSet(story.StorySet):
         archive_data_file='data/pathological_mobile_sites.json',
         cloud_storage_bucket=story.PARTNER_BUCKET)
 
-    sites = ['http://edition.cnn.com',
-             'http://m.espn.go.com/nhl/rankings',
-             'http://recode.net',
-             'http://sports.yahoo.com/',
-             'http://www.latimes.com',
-             ('http://www.pbs.org/newshour/bb/'
+    sites = [('cnn_pathological', 'http://edition.cnn.com'),
+             ('espn_pathological', 'http://m.espn.go.com/nhl/rankings'),
+             ('recode_pathological', 'http://recode.net'),
+             ('yahoo_sports_pathological', 'http://sports.yahoo.com/'),
+             ('latimes_pathological', 'http://www.latimes.com'),
+             ('pbs_pathological', 'http://www.pbs.org/newshour/bb/'
               'much-really-cost-live-city-like-seattle/#the-rundown'),
-             ('http://www.theguardian.com/politics/2015/mar/09/'
+             ('guardian_pathological', 'http://www.theguardian.com/politics/2015/mar/09/'
               'ed-balls-tory-spending-plans-nhs-charging'),
-             'http://www.zdnet.com',
-             'http://www.wowwiki.com/World_of_Warcraft:_Mists_of_Pandaria',
-             'https://www.linkedin.com/in/linustorvalds']
+             ('zdnet_pathological', 'http://www.zdnet.com'),
+             ('wow_wiki_pathological', 'http://www.wowwiki.com/'
+              'World_of_Warcraft:_Mists_of_Pandaria'),
+             ('linkedin_pathological', 'https://www.linkedin.com/in/linustorvalds')]
 
-    for site in sites:
-      self.AddStory(PathologicalMobileSitesPage(site, self))
+    for name, site in sites:
+      self.AddStory(PathologicalMobileSitesPage(name=name,
+                                                url=site,
+                                                page_set=self))
