7f20f41365e2d8cdba2293a9d1d0a492f1759551
David Trainor <dtrainor@chromium.org>
2018-06-01 07:12:50 +0000

    Add a GridLayoutManager to download home's RecyclerView
    - Incorporate a new GridLayoutManager into download home's RecyclerView.
    This allows supporting custom item widths based on the type of View
    representing the item as per spec.
    - Add a super simple ItemDecoration to the View as well to add some
    padding and make it easier to see the current rough Views.
    
    BUG=842345
    
    Change-Id: I7fd23fcef3ce34426fad6b0023be05d696d8c0fd
    Reviewed-on: https://chromium-review.googlesource.com/1079966
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563557}
    Former-commit-id: 9e0169368d66a438e7f45482971614eb0ef2f8fa

--- chrome/android/java/src/org/chromium/chrome/browser/download/home/list/DateOrderedListView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/home/list/DateOrderedListView.java
@@ -5,10 +5,16 @@
 package org.chromium.chrome.browser.download.home.list;
 
 import android.content.Context;
-import android.support.v7.widget.LinearLayoutManager;
+import android.graphics.Rect;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.GridLayoutManager.SpanSizeLookup;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ItemDecoration;
+import android.support.v7.widget.RecyclerView.Recycler;
+import android.support.v7.widget.RecyclerView.State;
 import android.view.View;
 
+import org.chromium.chrome.R;
 import org.chromium.chrome.browser.modelutil.RecyclerViewModelChangeProcessor;
 
 /**
@@ -17,11 +23,20 @@ import org.chromium.chrome.browser.modelutil.RecyclerViewModelChangeProcessor;
  */
 class DateOrderedListView {
     private final DecoratedListItemModel mModel;
+
+    private final int mImageWidthPx;
+    private final int mImagePaddingPx;
+
     private final RecyclerView mView;
 
     /** Creates an instance of a {@link DateOrderedListView} representing {@code model}. */
     public DateOrderedListView(Context context, DecoratedListItemModel model) {
         mModel = model;
+
+        mImageWidthPx =
+                context.getResources().getDimensionPixelSize(R.dimen.download_manager_image_width);
+        mImagePaddingPx = context.getResources().getDimensionPixelOffset(
+                R.dimen.download_manager_image_padding);
 
         DateOrderedListViewBinder viewBinder = new DateOrderedListViewBinder();
         DateOrderedListViewAdapter adapter = new DateOrderedListViewAdapter(mModel, viewBinder);
@@ -33,13 +48,62 @@ class DateOrderedListView {
         mView = new RecyclerView(context);
         mView.setHasFixedSize(true);
         mView.getItemAnimator().setChangeDuration(0);
-        mView.setLayoutManager(
-                new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false));
+        mView.getItemAnimator().setMoveDuration(0);
+        mView.setLayoutManager(new GridLayoutManagerImpl(context));
+        mView.addItemDecoration(new ItemDecorationImpl());
+
+        // Do the final hook up to the underlying data adapter.
         mView.setAdapter(adapter);
     }
 
     /** @return The Android {@link View} representing this widget. */
     public View getView() {
         return mView;
+    }
+
+    private class GridLayoutManagerImpl extends GridLayoutManager {
+        /** Creates an instance of a {@link GridLayoutManagerImpl}. */
+        public GridLayoutManagerImpl(Context context) {
+            super(context, 1 /* spanCount */, VERTICAL, false /* reverseLayout */);
+            setSpanSizeLookup(new SpanSizeLookupImpl());
+        }
+
+        // GridLayoutManager implementation.
+        @Override
+        public void onLayoutChildren(Recycler recycler, State state) {
+            assert getOrientation() == VERTICAL;
+
+            int availableWidth = getWidth() - mImagePaddingPx;
+            int columnWidth = mImageWidthPx - mImagePaddingPx;
+            setSpanCount(Math.max(1, availableWidth / columnWidth));
+
+            super.onLayoutChildren(recycler, state);
+        }
+
+        private class SpanSizeLookupImpl extends SpanSizeLookup {
+            // SpanSizeLookup implementation.
+            @Override
+            public int getSpanSize(int position) {
+                return ListUtils.getSpanSize(mModel.getItemAt(position), getSpanCount());
+            }
+        }
+    }
+
+    private class ItemDecorationImpl extends ItemDecoration {
+        // ItemDecoration implementation.
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, State state) {
+            int position = parent.getChildAdapterPosition(view);
+            if (position < 0 || position >= mModel.getItemCount()) return;
+
+            switch (ListUtils.getViewTypeForItem(mModel.getItemAt(position))) {
+                case ListUtils.IMAGE:
+                    outRect.left = mImagePaddingPx;
+                    outRect.right = mImagePaddingPx;
+                    outRect.top = mImagePaddingPx;
+                    outRect.bottom = mImagePaddingPx;
+                    break;
+            }
+        }
     }
 }
--- chrome/android/java/src/org/chromium/chrome/browser/download/home/list/DateOrderedListViewBinder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/home/list/DateOrderedListViewBinder.java
@@ -4,45 +4,31 @@
 
 package org.chromium.chrome.browser.download.home.list;
 
-import android.support.annotation.IntDef;
 import android.view.ViewGroup;
 
+import org.chromium.chrome.browser.download.home.list.ListUtils.ViewType;
 import org.chromium.chrome.browser.modelutil.RecyclerViewAdapter.ViewBinder;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 
 /**
  * A {@link ViewBinder} responsible for connecting a {@link DateOrderedListModel} with a underlying
  * {@link ViewHolder}.
  */
 class DateOrderedListViewBinder implements ViewBinder<DecoratedListItemModel, ListItemViewHolder> {
-    /** The potential types of list items that could be displayed. */
-    @Retention(RetentionPolicy.SOURCE)
-    @IntDef({DATE, IN_PROGRESS, GENERIC, VIDEO, IMAGE, CUSTOM_VIEW})
-    public @interface ViewType {}
-    public static final int DATE = 0;
-    public static final int IN_PROGRESS = 1;
-    public static final int GENERIC = 2;
-    public static final int VIDEO = 3;
-    public static final int IMAGE = 4;
-    public static final int CUSTOM_VIEW = 5;
-
     // ViewBinder implementation.
     @Override
     public ListItemViewHolder onCreateViewHolder(ViewGroup parent, @ViewType int viewType) {
         switch (viewType) {
-            case DATE:
+            case ListUtils.DATE:
                 return new ListItemViewHolder.DateViewHolder(parent);
-            case IN_PROGRESS:
+            case ListUtils.IN_PROGRESS:
                 return new ListItemViewHolder.InProgressViewHolder(parent);
-            case GENERIC:
+            case ListUtils.GENERIC:
                 return new ListItemViewHolder.GenericViewHolder(parent);
-            case VIDEO:
+            case ListUtils.VIDEO:
                 return new ListItemViewHolder.VideoViewHolder(parent);
-            case IMAGE:
+            case ListUtils.IMAGE:
                 return new ListItemViewHolder.ImageViewHolder(parent);
-            case CUSTOM_VIEW:
+            case ListUtils.CUSTOM_VIEW:
                 return new ListItemViewHolder.CustomViewHolder(parent);
         }
 
