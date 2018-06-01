6e63270c7739efa1e398ec4ba90702294a536a17
Aleks Totic <atotic@chromium.org>
2018-06-01 00:14:17 +0000

    [LayoutNG] Fix outline invalidation
    The problem:
    In NG, outlines are painted by block flow container.
    In Legacy, outlines are painted by LayoutInline.
    
    If outline of an inline child changes, block flow does not get
    invalidated, and the outline is not repainted.
    
    I've discussed what the right fix is with pdr. We've agreed that
    invalidating the block flow is the sanest way forward.
    
    This patch invalidates Container() DisplayItem
    when inline child is invalidated.
    
    This fixes one test:
    fast/history/visited-link-hover-outline-color.html
    
    Bug: 835484
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Ia7c44b2a2aa341bc5b8ebfd19aec9aea25c8d42c
    Reviewed-on: https://chromium-review.googlesource.com/1077621
    Commit-Queue: Aleks Totic <atotic@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563452}
    Former-commit-id: dd6cd16e6007348873b40e969a0fc880bbd96530

--- third_party/blink/renderer/core/layout/layout_inline.cc
+++ third_party/blink/renderer/core/layout/layout_inline.cc
@@ -1635,6 +1635,10 @@ void LayoutInline::InvalidateDisplayItemClients(
   if (RuntimeEnabledFeatures::LayoutNGEnabled()) {
     auto fragments = NGPaintFragment::InlineFragmentsFor(this);
     if (fragments.IsInLayoutNGInlineFormattingContext()) {
+      if (Container()->IsLayoutNGMixin() && StyleRef().HasOutline()) {
+        Container()->InvalidateDisplayItemClients(
+            PaintInvalidationReason::kOutline);
+      }
       for (NGPaintFragment* fragment : fragments) {
         paint_invalidator.InvalidateDisplayItemClient(*fragment,
                                                       invalidation_reason);
