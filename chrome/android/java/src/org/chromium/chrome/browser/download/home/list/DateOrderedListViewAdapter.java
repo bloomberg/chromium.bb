dcbbb78d5065e3cff0f394148a38d804e99274e8
David Trainor <dtrainor@chromium.org>
2018-06-01 07:12:50 +0000

    Add a GridLayoutManager to download home's RecyclerView
    - Incorporate a new GridLayoutManager into download home's RecyclerView.
    This allows supporting custom item widths based on the type of View
    representing the item as per spec.
    - Add a super simple ItemDecoration to the View as well to add some
    padding and make it easier to see the current rough Views.
    
    BUG=842345
    
    Change-Id: I7fd23fcef3ce34426fad6b0023be05d696d8c0fd
    Reviewed-on: https://chromium-review.googlesource.com/1079966
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563557}
    Former-commit-id: 9e0169368d66a438e7f45482971614eb0ef2f8fa

--- chrome/android/java/src/org/chromium/chrome/browser/download/home/list/DateOrderedListViewAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/home/list/DateOrderedListViewAdapter.java
@@ -6,13 +6,8 @@ package org.chromium.chrome.browser.download.home.list;
 
 import android.support.v7.widget.RecyclerView;
 
-import org.chromium.chrome.browser.download.home.list.DateOrderedListViewBinder.ViewType;
-import org.chromium.chrome.browser.download.home.list.ListItem.DateListItem;
-import org.chromium.chrome.browser.download.home.list.ListItem.OfflineItemListItem;
-import org.chromium.chrome.browser.download.home.list.ListItem.ViewListItem;
+import org.chromium.chrome.browser.download.home.list.ListUtils.ViewType;
 import org.chromium.chrome.browser.modelutil.RecyclerViewAdapter;
-import org.chromium.components.offline_items_collection.OfflineItemFilter;
-import org.chromium.components.offline_items_collection.OfflineItemState;
 
 /**
  * A helper {@link RecyclerView.Adapter} implementation meant to glue a {@link ListItemModel}
@@ -36,35 +31,6 @@ class DateOrderedListViewAdapter
     @Override
     public @ViewType int getItemViewType(int position) {
         ListItem item = mModel.getItemAt(position);
-
-        if (item instanceof ViewListItem) return DateOrderedListViewBinder.CUSTOM_VIEW;
-        if (item instanceof DateListItem) {
-            if (item instanceof OfflineItemListItem) {
-                OfflineItemListItem offlineItem = (OfflineItemListItem) item;
-
-                if (offlineItem.item.state == OfflineItemState.IN_PROGRESS) {
-                    return DateOrderedListViewBinder.IN_PROGRESS;
-                }
-
-                switch (offlineItem.item.filter) {
-                    case OfflineItemFilter.FILTER_VIDEO:
-                        return DateOrderedListViewBinder.VIDEO;
-                    case OfflineItemFilter.FILTER_IMAGE:
-                        return DateOrderedListViewBinder.IMAGE;
-                    case OfflineItemFilter.FILTER_ALL:
-                    case OfflineItemFilter.FILTER_PAGE:
-                    case OfflineItemFilter.FILTER_AUDIO:
-                    case OfflineItemFilter.FILTER_OTHER:
-                    case OfflineItemFilter.FILTER_DOCUMENT:
-                    default:
-                        return DateOrderedListViewBinder.GENERIC;
-                }
-            } else {
-                return DateOrderedListViewBinder.DATE;
-            }
-        }
-
-        assert false;
-        return DateOrderedListViewBinder.GENERIC;
+        return ListUtils.getViewTypeForItem(item);
     }
 }
