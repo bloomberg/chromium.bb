c5a349e7e47958aae591a03859e7b0e9d6d24bba
Bernhard Bauer <bauerb@chromium.org>
2018-06-01 19:11:15 +0000

    Update PrefsTest.TestIndex to reflect added prefs
    Also, ensure that the test will have to be updated if any additional prefs are
    added in the future, by counting the umber of tested prefs.
    
    Change-Id: I54d9ba0b6c72b6e6879f17c4b8143108e85663e0
    Reviewed-on: https://chromium-review.googlesource.com/1082443
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Becky Zhou <huayinz@chromium.org>
    Commit-Queue: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563748}
    Former-commit-id: 002bde2fa87264b477981dc26c90345972cfd4b8

--- chrome/browser/android/preferences/prefs_unittest.cc
+++ chrome/browser/android/preferences/prefs_unittest.cc
@@ -11,27 +11,42 @@
 
 namespace {
 
-const char* GetPrefName(Pref pref) {
-  return PrefServiceBridge::GetPrefNameExposedToJava(pref);
-}
+class PrefsTest : public ::testing::Test {
+ protected:
+  const char* GetPrefName(Pref pref) {
+    pref_count_++;
+    return PrefServiceBridge::GetPrefNameExposedToJava(pref);
+  }
 
-TEST(PrefsTest, TestSize) {
-  // If this test fails, you most likely forgot to update either Pref enum or
-  // |kPrefExposedToJava|.
+  int pref_count_;
+};
+
+TEST_F(PrefsTest, TestIndex) {
+  pref_count_ = 0;
+
+  // If one of these checks fails, most likely the Pref enum and
+  // |kPrefExposedToJava| are out of sync.
   EXPECT_EQ(Pref::PREF_NUM_PREFS, arraysize(kPrefsExposedToJava));
-}
 
-TEST(PrefsTest, TestIndex) {
   EXPECT_EQ(prefs::kAllowDeletingBrowserHistory,
             GetPrefName(ALLOW_DELETING_BROWSER_HISTORY));
+  EXPECT_EQ(contextual_suggestions::prefs::kContextualSuggestionsEnabled,
+            GetPrefName(CONTEXTUAL_SUGGESTIONS_ENABLED));
   EXPECT_EQ(prefs::kIncognitoModeAvailability,
             GetPrefName(INCOGNITO_MODE_AVAILABILITY));
   EXPECT_EQ(ntp_snippets::prefs::kEnableSnippets,
             GetPrefName(NTP_ARTICLES_SECTION_ENABLED));
   EXPECT_EQ(ntp_snippets::prefs::kArticlesListVisible,
             GetPrefName(NTP_ARTICLES_LIST_VISIBLE));
+  EXPECT_EQ(prefs::kPromptForDownloadAndroid,
+            GetPrefName(PROMPT_FOR_DOWNLOAD_ANDROID));
   EXPECT_EQ(dom_distiller::prefs::kReaderForAccessibility,
             GetPrefName(READER_FOR_ACCESSIBILITY_ENABLED));
+  EXPECT_EQ(prefs::kShowMissingSdCardErrorAndroid,
+            GetPrefName(SHOW_MISSING_SD_CARD_ERROR_ANDROID));
+
+  // If this check fails, a pref is missing a test case above.
+  EXPECT_EQ(Pref::PREF_NUM_PREFS, pref_count_);
 }
 
 }  // namespace
