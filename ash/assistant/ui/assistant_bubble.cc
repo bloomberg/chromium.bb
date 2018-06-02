ed22b581af6e7d6449c98e1c1592c6a59c58b646
David Black <dmblack@google.com>
2018-06-02 02:11:58 +0000

    Pulls AssistantBubbleView out of AssistantBubble.
    AssistantBubbleView was formerly called AssistantContainerView inside
    AssistantBubble anonymous namespace.
    
    AssistantMainView is now a child of AssistantBubbleView.
    
    Follow up CLs will add AssistantMiniView and AssistantSettingsView
    children. Only one child will be visible at a time.
    
    Bug: b:80551552
    Change-Id: I8fcdca44aefce764ad1d82b2122af5b1dd48425d
    Reviewed-on: https://chromium-review.googlesource.com/1082754
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#563916}
    Former-commit-id: d14cb0ba1b22c49abed76c7efb4db0601b93a68d

--- ash/assistant/ui/assistant_bubble.cc
+++ ash/assistant/ui/assistant_bubble.cc
@@ -4,109 +4,10 @@
 
 #include "ash/assistant/ui/assistant_bubble.h"
 
-#include <memory>
-
 #include "ash/assistant/assistant_controller.h"
-#include "ash/assistant/ui/assistant_main_view.h"
-#include "base/strings/utf_string_conversions.h"
-#include "ui/display/display.h"
-#include "ui/display/screen.h"
-#include "ui/views/bubble/bubble_dialog_delegate.h"
-#include "ui/views/bubble/bubble_frame_view.h"
-#include "ui/views/layout/fill_layout.h"
-#include "ui/views/view.h"
-#include "ui/wm/core/shadow_types.h"
+#include "ash/assistant/ui/assistant_bubble_view.h"
 
 namespace ash {
-
-namespace {
-
-// Appearance.
-constexpr SkColor kBackgroundColor = SK_ColorWHITE;
-constexpr int kCornerRadiusDip = 20;
-constexpr int kMarginDip = 8;
-
-// AssistantContainerView ------------------------------------------------------
-
-class AssistantContainerView : public views::BubbleDialogDelegateView {
- public:
-  explicit AssistantContainerView(AssistantController* assistant_controller)
-      : assistant_controller_(assistant_controller) {
-    set_accept_events(true);
-    SetAnchor();
-    set_arrow(views::BubbleBorder::Arrow::BOTTOM_CENTER);
-    set_close_on_deactivate(false);
-    set_color(kBackgroundColor);
-    set_margins(gfx::Insets());
-    set_shadow(views::BubbleBorder::Shadow::NO_ASSETS);
-    set_title_margins(gfx::Insets());
-
-    views::BubbleDialogDelegateView::CreateBubble(this);
-
-    // These attributes can only be set after bubble creation:
-    GetBubbleFrameView()->bubble_border()->SetCornerRadius(kCornerRadiusDip);
-    SetAlignment(
-        views::BubbleBorder::BubbleAlignment::ALIGN_EDGE_TO_ANCHOR_EDGE);
-    SetArrowPaintType(views::BubbleBorder::PAINT_NONE);
-  }
-
-  ~AssistantContainerView() override = default;
-
-  // views::BubbleDialogDelegateView:
-  void OnBeforeBubbleWidgetInit(views::Widget::InitParams* params,
-                                views::Widget* widget) const override {
-    params->corner_radius = kCornerRadiusDip;
-    params->keep_on_top = true;
-    params->shadow_type = views::Widget::InitParams::SHADOW_TYPE_DROP;
-    params->shadow_elevation = wm::kShadowElevationActiveWindow;
-  }
-
-  void Init() override { InitLayout(); }
-
-  void ChildPreferredSizeChanged(views::View* child) override {
-    SizeToContents();
-  }
-
-  int GetDialogButtons() const override { return ui::DIALOG_BUTTON_NONE; }
-
-  void RequestFocus() override {
-    if (assistant_main_view_)
-      assistant_main_view_->RequestFocus();
-  }
-
- private:
-  void InitLayout() {
-    SetLayoutManager(std::make_unique<views::FillLayout>());
-
-    assistant_main_view_ = new AssistantMainView(assistant_controller_);
-    AddChildView(assistant_main_view_);
-  }
-
-  void SetAnchor() {
-    // TODO(dmblack): Handle multiple displays, dynamic shelf repositioning, and
-    // any other corner cases.
-    // Anchors to bottom center of primary display's work area.
-    display::Display primary_display =
-        display::Screen::GetScreen()->GetPrimaryDisplay();
-
-    gfx::Rect work_area = primary_display.work_area();
-    gfx::Rect anchor = gfx::Rect(work_area.x(), work_area.bottom() - kMarginDip,
-                                 work_area.width(), 0);
-
-    SetAnchorRect(anchor);
-  }
-
-  AssistantController* const assistant_controller_;  // Owned by Shell.
-
-  // Owned by view hierarchy.
-  AssistantMainView* assistant_main_view_ = nullptr;
-
-  DISALLOW_COPY_AND_ASSIGN(AssistantContainerView);
-};
-
-}  // namespace
-
-// AssistantBubble -------------------------------------------------------------
 
 AssistantBubble::AssistantBubble(AssistantController* assistant_controller)
     : assistant_controller_(assistant_controller) {
@@ -116,14 +17,14 @@ AssistantBubble::AssistantBubble(AssistantController* assistant_controller)
 AssistantBubble::~AssistantBubble() {
   assistant_controller_->RemoveInteractionModelObserver(this);
 
-  if (container_view_)
-    container_view_->GetWidget()->RemoveObserver(this);
+  if (bubble_view_)
+    bubble_view_->GetWidget()->RemoveObserver(this);
 }
 
 void AssistantBubble::OnWidgetActivationChanged(views::Widget* widget,
                                                 bool active) {
   if (active)
-    container_view_->RequestFocus();
+    bubble_view_->RequestFocus();
 }
 
 void AssistantBubble::OnWidgetDestroying(views::Widget* widget) {
@@ -133,8 +34,8 @@ void AssistantBubble::OnWidgetDestroying(views::Widget* widget) {
   // widget visibility state and the underlying interaction model state.
   assistant_controller_->StopInteraction();
 
-  container_view_->GetWidget()->RemoveObserver(this);
-  container_view_ = nullptr;
+  bubble_view_->GetWidget()->RemoveObserver(this);
+  bubble_view_ = nullptr;
 }
 
 void AssistantBubble::OnInteractionStateChanged(
@@ -150,20 +51,20 @@ void AssistantBubble::OnInteractionStateChanged(
 }
 
 bool AssistantBubble::IsVisible() const {
-  return container_view_ && container_view_->GetWidget()->IsVisible();
+  return bubble_view_ && bubble_view_->GetWidget()->IsVisible();
 }
 
 void AssistantBubble::Show() {
-  if (!container_view_) {
-    container_view_ = new AssistantContainerView(assistant_controller_);
-    container_view_->GetWidget()->AddObserver(this);
+  if (!bubble_view_) {
+    bubble_view_ = new AssistantBubbleView(assistant_controller_);
+    bubble_view_->GetWidget()->AddObserver(this);
   }
-  container_view_->GetWidget()->Show();
+  bubble_view_->GetWidget()->Show();
 }
 
 void AssistantBubble::Dismiss() {
-  if (container_view_)
-    container_view_->GetWidget()->Hide();
+  if (bubble_view_)
+    bubble_view_->GetWidget()->Hide();
 }
 
 }  // namespace ash
