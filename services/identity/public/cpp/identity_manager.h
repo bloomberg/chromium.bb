ba89d59b8e6ce3c1b30fa9538594c61b7b1f7219
Marc Treib <treib@chromium.org>
2018-06-04 14:56:39 +0000

    Cleanup ProfileSyncServiceStartupTests
    I started out trying to fix and re-enable some long-disabled tests here,
    but then decided that this needs a generic cleanup pass first.
    
    Bug: none
    Change-Id: If1f605252e4326cd5d0b7a449bf4c6f2ccc9dc49
    Reviewed-on: https://chromium-review.googlesource.com/1079069
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564091}
    Former-commit-id: 845a3da0daa518fed2f198367e97102044c45619

--- services/identity/public/cpp/identity_manager.h
+++ services/identity/public/cpp/identity_manager.h
@@ -23,7 +23,7 @@ class ArcTermsOfServiceDefaultNegotiatorTest;
 
 // Necessary to declare this class as a friend.
 namespace browser_sync {
-class ProfileSyncServiceStartupCrosTest;
+class ProfileSyncServiceStartupTest;
 }
 
 // Necessary to declare these classes as friends.
@@ -160,7 +160,7 @@ class IdentityManager : public SigninManagerBase::Observer,
   friend arc::ArcTermsOfServiceDefaultNegotiatorTest;
   friend chromeos::ChromeSessionManager;
   friend chromeos::UserSessionManager;
-  friend browser_sync::ProfileSyncServiceStartupCrosTest;
+  friend browser_sync::ProfileSyncServiceStartupTest;
 
   // Sets the primary account info synchronously with both the IdentityManager
   // and its backing SigninManager/ProfileOAuth2TokenService instances.
