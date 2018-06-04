7413b93c2da39d68e7c4f6f3b5ee67449aa52845
Kevin McNee <mcnee@chromium.org>
2018-06-04 17:01:39 +0000

    Have vertical-scroll-touch-block-manual.tentative.html run with a compositor
    As part of issue 787924, we will no longer handle gesture pinch events
    on the main thread. We now run this test with --enable-threaded-compositing
    
    Bug: 787924
    Change-Id: I800da132cb986e86789b3792e193640e40e108c0
    Reviewed-on: https://chromium-review.googlesource.com/1081345
    Reviewed-by: Ehsan Karamad <ekaramad@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564118}
    Former-commit-id: b19d962194d1a7317658cd237b829f083ca07b5c

--- third_party/WebKit/LayoutTests/external/wpt_automation/feature-policy/experimental-features/vertical-scroll.js
+++ third_party/WebKit/LayoutTests/external/wpt_automation/feature-policy/experimental-features/vertical-scroll.js
@@ -4,6 +4,16 @@ function platformAPIExists() {
   return window.chrome && window.chrome.gpuBenchmarking;
 }
 
+function waitForCompositorCommit() {
+  return new Promise((resolve) => {
+    // For now, we just rAF twice. It would be nice to have a proper mechanism
+    // for this.
+    window.requestAnimationFrame(() => {
+      window.requestAnimationFrame(resolve);
+    });
+  });
+}
+
 function touchScroll(direction, start_x, start_y) {
   if (!platformAPIExists())
     return Promise.reject();
@@ -16,19 +26,21 @@ function touchScroll(direction, start_x, start_y) {
   if (delta_x === delta_y)
     return Promise.reject("Invalid touch direction.");
 
-  return new Promise((resolve) => {
-    chrome.gpuBenchmarking.pointerActionSequence( [
-        {source: "touch",
-         actions: [
-            { name: "pointerDown", x: start_x, y: start_y},
-            { name: "pointerMove",
-              x: (start_x + delta_x),
-              y: (start_y + delta_y)
-            },
-            { name: "pause", duration: 0.1 },
-            { name: "pointerUp" }
-        ]}], resolve);
-  });
+  return waitForCompositorCommit().then(() => {
+    return new Promise((resolve) => {
+      chrome.gpuBenchmarking.pointerActionSequence( [
+          {source: "touch",
+           actions: [
+              { name: "pointerDown", x: start_x, y: start_y},
+              { name: "pointerMove",
+                x: (start_x + delta_x),
+                y: (start_y + delta_y)
+              },
+              { name: "pause", duration: 0.1 },
+              { name: "pointerUp" }
+          ]}], resolve);
+    });
+  }).then(waitForCompositorCommit);
 }
 
 function pinchZoom(direction, start_x_1, start_y_1, start_x_2, start_y_2) {
@@ -40,32 +52,34 @@ function pinchZoom(direction, start_x_1, start_y_1, start_x_2, start_y_2) {
   let move_offset = 10;
   let offset_upper_bound = 80;
 
-  return new Promise((resolve) => {
-    var pointerActions = [{'source': 'touch'}, {'source': 'touch'}];
-    var pointerAction1 = pointerActions[0];
-    var pointerAction2 = pointerActions[1];
-    pointerAction1.actions = [];
-    pointerAction2.actions = [];
-    pointerAction1.actions.push(
-        {name: 'pointerDown', x: start_x_1, y: start_y_1});
-    pointerAction2.actions.push(
-        {name: 'pointerDown', x: start_x_2, y: start_y_2});
-    for (var offset = move_offset; offset < offset_upper_bound; offset += move_offset) {
-      pointerAction1.actions.push({
-          name: 'pointerMove',
-          x: (start_x_1 - offset),
-          y: start_y_1,
-        });
-      pointerAction2.actions.push({
-          name: 'pointerMove',
-          x: start_x_2 + offset,
-          y: start_y_2,
-        });
-    }
-    pointerAction1.actions.push({name: 'pointerUp'});
-    pointerAction2.actions.push({name: 'pointerUp'});
-    chrome.gpuBenchmarking.pointerActionSequence(pointerActions, resolve);
-  });
+  return waitForCompositorCommit().then(() => {
+    return new Promise((resolve) => {
+      var pointerActions = [{'source': 'touch'}, {'source': 'touch'}];
+      var pointerAction1 = pointerActions[0];
+      var pointerAction2 = pointerActions[1];
+      pointerAction1.actions = [];
+      pointerAction2.actions = [];
+      pointerAction1.actions.push(
+          {name: 'pointerDown', x: start_x_1, y: start_y_1});
+      pointerAction2.actions.push(
+          {name: 'pointerDown', x: start_x_2, y: start_y_2});
+      for (var offset = move_offset; offset < offset_upper_bound; offset += move_offset) {
+        pointerAction1.actions.push({
+            name: 'pointerMove',
+            x: (start_x_1 - offset),
+            y: start_y_1,
+          });
+        pointerAction2.actions.push({
+            name: 'pointerMove',
+            x: start_x_2 + offset,
+            y: start_y_2,
+          });
+      }
+      pointerAction1.actions.push({name: 'pointerUp'});
+      pointerAction2.actions.push({name: 'pointerUp'});
+      chrome.gpuBenchmarking.pointerActionSequence(pointerActions, resolve);
+    });
+  }).then(waitForCompositorCommit);
 }
 
 window.touch_scroll_api_ready = true;
