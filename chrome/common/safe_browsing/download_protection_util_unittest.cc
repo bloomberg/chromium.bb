b3b4904d1b04dfd53a361ae1b390fe5f0c52bcd4
Varun Khaneja <vakh@chromium.org>
2018-06-04 19:56:22 +0000

    unrar: Add the results of parsing .rar file to ClientDownloadRequest.
    All this is behind the flag: InspectDownloadedRarFiles
    
    TODOs:
    - results.archived_binary is currently empty.
    
    The backend proto has been updated but not in production yet:
    http://cl/198807217
    
    R=jialiul
    
    Bug: 750327
    Change-Id: I9456a53cfcf6c250b868f411b148cd8d21523632
    Reviewed-on: https://chromium-review.googlesource.com/1081476
    Commit-Queue: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564197}
    Former-commit-id: 78df4a17c557cb2784eaa098a67450759a23e9dc

--- chrome/common/safe_browsing/download_protection_util_unittest.cc
+++ chrome/common/safe_browsing/download_protection_util_unittest.cc
@@ -17,7 +17,7 @@ TEST(DownloadProtectionUtilTest, KnownValues) {
             GetDownloadType(base::FilePath(FILE_PATH_LITERAL("foo.crx"))));
   EXPECT_EQ(ClientDownloadRequest::ZIPPED_EXECUTABLE,
             GetDownloadType(base::FilePath(FILE_PATH_LITERAL("foo.zip"))));
-  EXPECT_EQ(ClientDownloadRequest::ARCHIVE,
+  EXPECT_EQ(ClientDownloadRequest::RAR_COMPRESSED_EXECUTABLE,
             GetDownloadType(base::FilePath(FILE_PATH_LITERAL("foo.rar"))));
   EXPECT_EQ(ClientDownloadRequest::MAC_EXECUTABLE,
             GetDownloadType(base::FilePath(FILE_PATH_LITERAL("foo.pkg"))));
