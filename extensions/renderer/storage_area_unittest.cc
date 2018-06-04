2a7fb0d97fc89f154453a900b457ad5a487b597e
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-06-04 20:48:47 +0000

    [Extensions Bindings] Provide a better error for StorageArea invocations
    Provide a better error when a developer invokes a storage area method
    incorrectly. Add a unittest for the same.
    
    Bug: 848904
    Change-Id: If3c27d29d35bebb710318333937deecf954c5cbc
    Reviewed-on: https://chromium-review.googlesource.com/1085486
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564224}
    Former-commit-id: b1c38f6465d5dd12d2e848dde1da790692de9e43

--- extensions/renderer/storage_area_unittest.cc
+++ extensions/renderer/storage_area_unittest.cc
@@ -7,6 +7,7 @@
 #include "extensions/common/extension_builder.h"
 #include "extensions/renderer/bindings/api_binding_test_util.h"
 #include "extensions/renderer/bindings/api_binding_util.h"
+#include "extensions/renderer/bindings/api_invocation_errors.h"
 #include "extensions/renderer/native_extension_bindings_system.h"
 #include "extensions/renderer/native_extension_bindings_system_test_base.h"
 #include "extensions/renderer/script_context.h"
@@ -76,6 +77,38 @@ TEST_F(StorageAreaTest, TestUseAfterInvalidation) {
   EXPECT_FALSE(binding::IsContextValid(context));
   RunFunctionAndExpectError(run_storage_get, context, arraysize(args), args,
                             "Uncaught Error: Extension context invalidated.");
+}
+
+TEST_F(StorageAreaTest, InvalidInvocationError) {
+  scoped_refptr<Extension> extension =
+      ExtensionBuilder("foo").AddPermission("storage").Build();
+  RegisterExtension(extension);
+
+  v8::HandleScope handle_scope(isolate());
+  v8::Local<v8::Context> context = MainContext();
+
+  ScriptContext* script_context = CreateScriptContext(
+      context, extension.get(), Feature::BLESSED_EXTENSION_CONTEXT);
+  script_context->set_url(extension->url());
+
+  bindings_system()->UpdateBindingsForContext(script_context);
+
+  v8::Local<v8::Value> storage =
+      V8ValueFromScriptSource(context, "chrome.storage.local");
+  ASSERT_TRUE(storage->IsObject());
+
+  constexpr char kRunStorageGet[] =
+      "(function(storage) { storage.get(1, function() {}); })";
+  v8::Local<v8::Function> run_storage_get =
+      FunctionFromString(context, kRunStorageGet);
+  v8::Local<v8::Value> args[] = {storage};
+  RunFunctionAndExpectError(
+      run_storage_get, context, base::size(args), args,
+      "Uncaught TypeError: " +
+          api_errors::InvocationError(
+              "storage.get",
+              "optional [string|array|object] keys, function callback",
+              "No matching signature."));
 }
 
 }  // namespace extensions
