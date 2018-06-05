7c5adff076cd348526d51d5b60f67b418b507751
Maria Khomenko <mariakhomenko@chromium.org>
2018-06-05 18:31:56 +0000

    Remove obsolete histogram recording call.
    The function recording the metric has not been used by any code for a
    long time now.
    
    Bug: 849443
    Change-Id: I9b53a7801a99df7a1b92d32c614d1c79c13c47fa
    Reviewed-on: https://chromium-review.googlesource.com/1086214
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Maria Khomenko <mariakhomenko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564589}
    Former-commit-id: 4f72b1c20517c96291c1e9509e7dc01bbbc88cc7

--- chrome/android/java/src/org/chromium/chrome/browser/instantapps/InstantAppsHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/instantapps/InstantAppsHandler.java
@@ -92,10 +92,6 @@ public class InstantAppsHandler {
     private static final TimesHistogramSample sFallbackIntentTimes = new TimesHistogramSample(
             "Android.InstantApps.FallbackDuration", TimeUnit.MILLISECONDS);
 
-    /** A histogram to record how long the GMS Core API call took. */
-    private static final TimesHistogramSample sInstantAppsApiCallTimes = new TimesHistogramSample(
-            "Android.InstantApps.ApiCallDuration2", TimeUnit.MILLISECONDS);
-
     // Only two possible call sources for fallback intents, set boundary at n+1.
     private static final int SOURCE_BOUNDARY = 3;
 
@@ -149,14 +145,6 @@ public class InstantAppsHandler {
      */
     private void recordHandleIntentDuration(long startTime) {
         sHandleIntentDuration.record(SystemClock.elapsedRealtime() - startTime);
-    }
-
-    /**
-     * Record the amount of time spent in the instant apps API call.
-     * @param startTime The time at which we started doing computations.
-     */
-    protected void recordInstantAppsApiCallTime(long startTime) {
-        sInstantAppsApiCallTimes.record(SystemClock.elapsedRealtime() - startTime);
     }
 
     /**
