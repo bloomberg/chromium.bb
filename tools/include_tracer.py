5fb345741f90f0bc85e03f6a58b5a511a165be1f
Kent Tamura <tkent@chromium.org>
2018-06-05 05:59:46 +0000

    inlucde_tracer.py: Add -I flag to specify additional header directories
    Change-Id: I1cca17830ed415f6f1f2d93d48a8c776a4cbfd37
    Reviewed-on: https://chromium-review.googlesource.com/1086560
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564372}
    Former-commit-id: a28b0bba5e4c762bb6dfcff97ec07862e5898111

--- tools/include_tracer.py
+++ tools/include_tracer.py
@@ -9,9 +9,10 @@
 Since it ignores defines, it gives just a rough estimation of file size.
 
 Usage:
-  tools/include_tracer.py chrome/browser/ui/browser.h
+  tools/include_tracer.py -Iout/Default/gen chrome/browser/ui/browser.h
 """
 
+import argparse
 import os
 import re
 import sys
@@ -42,7 +43,7 @@ INCLUDE_PATHS = [
 ]
 
 
-def Walk(seen, filename, parent, indent):
+def Walk(include_dirs, seen, filename, parent, indent):
   """Returns the size of |filename| plus the size of all files included by
   |filename| and prints the include tree of |filename| to stdout. Every file
   is visited at most once.
@@ -71,7 +72,7 @@ def Walk(seen, filename, parent, indent):
 
   # Find file in all include paths.
   resolved_filename = filename
-  for root in INCLUDE_PATHS + [os.path.dirname(parent)]:
+  for root in INCLUDE_PATHS + [os.path.dirname(parent)] + include_dirs:
     if os.path.exists(os.path.join(root, filename)):
       resolved_filename = os.path.join(root, filename)
       break
@@ -89,15 +90,23 @@ def Walk(seen, filename, parent, indent):
       include = match.group(1)
       if include.startswith('"'):
         include = include[1:-1]
-      total_bytes += Walk(seen, include, resolved_filename, indent + 2)
+      total_bytes += Walk(
+        include_dirs, seen, include, resolved_filename, indent + 2)
     elif line.startswith('import '):
       total_bytes += Walk(
-          seen, line.split('"')[1], resolved_filename, indent + 2)
+        include_dirs, seen, line.split('"')[1], resolved_filename, indent + 2)
   return total_bytes + len("".join(lines))
 
 
 def main():
-  bytes = Walk(set(), sys.argv[1], '', 0)
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-I', action='append', dest='include_dirs')
+  parser.add_argument('source_file')
+  options = parser.parse_args(sys.argv[1:])
+  if not options.include_dirs:
+    options.include_dirs = []
+
+  bytes = Walk(options.include_dirs, set(), options.source_file, '', 0)
   print
   print float(bytes) / (1 << 20), "megabytes of chrome source"
 
