5233caf16d244e184a03505dc9ba69c8a57246f0
Ryan Sturm <ryansturm@chromium.org>
2018-06-05 00:14:35 +0000

    Reduce race condition in loading previews while loading DRP config
    The cached config takes a few thread hops to apply and in the case of
    opening the browser from an intent, may not be loaded by the time the
    navigation occurs. There is a quick optimization to check if the config
    would apply to the navigation URL that will fail if this race is lost.
    
    Removing this optimization should not change the result of the preview
    in the case that lite page should not be applied.
    
    Bug: 849375
    Change-Id: I5fa1c08710f2e60da5e218a02d2ba3849348ed78
    Reviewed-on: https://chromium-review.googlesource.com/1086108
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564307}
    Former-commit-id: ed57667746a408b79b8be8b2e2d5f16289997a54

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_io_data.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_io_data.cc
@@ -320,8 +320,7 @@ bool DataReductionProxyIOData::ShouldAcceptServerPreview(
     previews::PreviewsDecider* previews_decider) {
   DCHECK(previews_decider);
   DCHECK((request.load_flags() & net::LOAD_MAIN_FRAME_DEPRECATED) != 0);
-  if (!config_ || (config_->IsBypassedByDataReductionProxyLocalRules(
-                      request, configurator_->GetProxyConfig()))) {
+  if (!config_) {
     return false;
   }
   return config_->ShouldAcceptServerPreview(request, *previews_decider);
