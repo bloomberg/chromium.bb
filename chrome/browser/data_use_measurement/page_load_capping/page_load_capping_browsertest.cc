433c954bd42e2c6fe969469c5d3a3aa7d62e09d1
Ryan Sturm <ryansturm@chromium.org>
2018-06-05 20:32:59 +0000

    Making the page capping browser test robust
    This test suite now handles requests instead of looking at connections
    and tracks the count of requests based on a specific URL pattern. The
    first test will pass when no image requests have been loaded, so if for
    some reason 20 round trips to the renderer do not gaurantee the request
    is seen on the net/ stack, the test will still pass. The second test
    creates an image request while the page is paused, and upon unpausing
    the page, waits for the image request to be seen in the embedded test
    server. The third test loads the image in the same pattern as the second
    without ever pausing the page.
    
    Bug: 848675
    Change-Id: I5a72cf1765b583bdb83ade2046366c12d6d70613
    Reviewed-on: https://chromium-review.googlesource.com/1086125
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564645}
    Former-commit-id: d47abb700fd7714183c33d1fd35e5c1b56809042

--- chrome/browser/data_use_measurement/page_load_capping/page_load_capping_browsertest.cc
+++ chrome/browser/data_use_measurement/page_load_capping/page_load_capping_browsertest.cc
@@ -2,7 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <memory>
+
+#include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/string_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -11,7 +15,10 @@
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/common/content_features.h"
 #include "content/public/test/browser_test_utils.h"
+#include "net/http/http_status_code.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
+#include "net/test/embedded_test_server/http_request.h"
+#include "net/test/embedded_test_server/http_response.h"
 #include "net/test/embedded_test_server/simple_connection_listener.h"
 #include "services/network/public/cpp/features.h"
 #include "third_party/blink/public/mojom/loader/pause_subresource_loading_handle.mojom.h"
@@ -19,150 +26,172 @@
 namespace {
 const base::FilePath::CharType kDocRoot[] =
     FILE_PATH_LITERAL("chrome/test/data/data_use_measurement");
+const char kImagePrefix[] = "/image";
 }  // namespace
 
 class PageLoadCappingBrowserTest : public InProcessBrowserTest {
- protected:
-  PageLoadCappingBrowserTest() {
+ public:
+  PageLoadCappingBrowserTest()
+      : https_test_server_(std::make_unique<net::EmbeddedTestServer>(
+            net::EmbeddedTestServer::TYPE_HTTPS)) {
     scoped_feature_list_.InitWithFeatures(
         {network::features::kRendererSideResourceScheduler,
          features::kResourceLoadScheduler},
         {});
   }
 
+  std::unique_ptr<net::test_server::HttpResponse> HandleRequest(
+      const net::test_server::HttpRequest& request) {
+    // Check if this matches the image requests from the test suite.
+    if (!StartsWith(request.relative_url, kImagePrefix,
+                    base::CompareCase::SENSITIVE)) {
+      return nullptr;
+    }
+    // This request should match "/image.*" for this test suite.
+    images_attempted_++;
+
+    // Return a 404. This is expected in the test, but embedded test server will
+    // create warnings when serving its own 404 responses.
+    std::unique_ptr<net::test_server::BasicHttpResponse> not_found_response =
+        std::make_unique<net::test_server::BasicHttpResponse>();
+    not_found_response->set_code(net::HTTP_NOT_FOUND);
+    if (waiting_) {
+      run_loop_->QuitWhenIdle();
+      waiting_ = false;
+    }
+    return not_found_response;
+  }
+
+  void WaitForRequest() {
+    EXPECT_FALSE(waiting_);
+    waiting_ = true;
+    run_loop_ = std::make_unique<base::RunLoop>();
+    run_loop_->Run();
+  }
+
+  size_t images_attempted() const { return images_attempted_; }
+
+ protected:
+  std::unique_ptr<net::EmbeddedTestServer> https_test_server_;
+
  private:
+  size_t images_attempted_ = 0u;
+  bool waiting_ = false;
+
+  std::unique_ptr<base::RunLoop> run_loop_;
+
   base::test::ScopedFeatureList scoped_feature_list_;
 };
 
-// TODO(crbug.com/848675) Disabled to due flaky timeouts on pretty much all
-// platforms.
-IN_PROC_BROWSER_TEST_F(PageLoadCappingBrowserTest,
-                       DISABLED_PageLoadCappingBlocksLoads) {
-  net::EmbeddedTestServer https_test_server(
-      net::EmbeddedTestServer::TYPE_HTTPS);
-  // The main resource and the favicon should be fetched. Additionally, images
-  // will not be fetched after script is injected.
-  net::test_server::SimpleConnectionListener listener(
-      2, net::test_server::SimpleConnectionListener::
-             FAIL_ON_ADDITIONAL_CONNECTIONS);
+IN_PROC_BROWSER_TEST_F(PageLoadCappingBrowserTest, PageLoadCappingBlocksLoads) {
+  // Tests that subresource loading can be blocked from the browser process.
+  https_test_server_->RegisterRequestHandler(base::BindRepeating(
+      &PageLoadCappingBrowserTest::HandleRequest, base::Unretained(this)));
+  https_test_server_->ServeFilesFromSourceDirectory(base::FilePath(kDocRoot));
 
-  https_test_server.SetConnectionListener(&listener);
-
-  https_test_server.ServeFilesFromSourceDirectory(base::FilePath(kDocRoot));
-
-  ASSERT_TRUE(https_test_server.Start());
+  ASSERT_TRUE(https_test_server_->Start());
 
   // Load a mostly empty page.
-  ui_test_utils::NavigateToURL(browser(),
-                               https_test_server.GetURL("/page_capping.html"));
+  ui_test_utils::NavigateToURL(
+      browser(), https_test_server_->GetURL("/page_capping.html"));
 
   content::WebContents* contents =
       browser()->tab_strip_model()->GetActiveWebContents();
 
-  // Block new resource loads.
+  // Block new subresource loads.
   std::vector<blink::mojom::PauseSubresourceLoadingHandlePtr> handles =
       contents->PauseSubresourceLoading();
 
   // Adds images to the page. They should not be allowed to load.
-  // Running this 5 times makes 5 round trips to the renderer, making it very
-  // likely the earlier requests would have started by the time all of the calls
-  // have been made.
-  for (size_t i = 0; i < 5; ++i) {
+  // Running this 20 times makes 20 round trips to the renderer, making it very
+  // likely the earliest request would have made it to the network by the time
+  // all of the calls have been made.
+  for (size_t i = 0; i < 20; ++i) {
     std::string create_image_script =
         std::string(
             "var image = document.createElement('img'); "
-            "document.body.appendChild(image); image.src = 'image")
+            "document.body.appendChild(image); image.src = '")
+            .append(kImagePrefix)
             .append(base::IntToString(i))
             .append(".png';");
     EXPECT_TRUE(content::ExecuteScript(contents, create_image_script));
   }
-  listener.WaitForConnections();
+
+  // No images should be loaded as subresource loading was paused.
+  EXPECT_EQ(0u, images_attempted());
+  https_test_server_.reset();
 }
 
 IN_PROC_BROWSER_TEST_F(PageLoadCappingBrowserTest,
                        PageLoadCappingBlocksLoadsAndResume) {
-  net::EmbeddedTestServer https_test_server(
-      net::EmbeddedTestServer::TYPE_HTTPS);
-  // The main resource and the favicon should be fetched. Additionally, 5 images
-  // will not be fetched after script is injected, but will once the page is
-  // resumed.
-  net::test_server::SimpleConnectionListener listener(
-      7, net::test_server::SimpleConnectionListener::
-             FAIL_ON_ADDITIONAL_CONNECTIONS);
+  // Tests that after triggerring subresource pausing, resuming allows deferred
+  // requests to be initiated.
+  https_test_server_->RegisterRequestHandler(base::BindRepeating(
+      &PageLoadCappingBrowserTest::HandleRequest, base::Unretained(this)));
 
-  https_test_server.SetConnectionListener(&listener);
+  https_test_server_->ServeFilesFromSourceDirectory(base::FilePath(kDocRoot));
 
-  https_test_server.ServeFilesFromSourceDirectory(base::FilePath(kDocRoot));
-
-  ASSERT_TRUE(https_test_server.Start());
+  ASSERT_TRUE(https_test_server_->Start());
 
   // Load a mostly empty page.
-  ui_test_utils::NavigateToURL(browser(),
-                               https_test_server.GetURL("/page_capping.html"));
+  ui_test_utils::NavigateToURL(
+      browser(), https_test_server_->GetURL("/page_capping.html"));
 
   content::WebContents* contents =
       browser()->tab_strip_model()->GetActiveWebContents();
 
-  // Block new resource loads.
+  // Block new subresource loads.
   std::vector<blink::mojom::PauseSubresourceLoadingHandlePtr> handles =
       contents->PauseSubresourceLoading();
 
-  // Adds images to the page. They should not be allowed to load.
-  // Running this 5 times makes 5 round trips to the renderer, making it very
-  // likely the earlier requests would have started by the time all of the calls
-  // have been made.
-  for (size_t i = 0; i < 5; ++i) {
-    std::string create_image_script =
-        std::string(
-            "var image = document.createElement('img'); "
-            "document.body.appendChild(image); image.src = 'image")
-            .append(base::IntToString(i))
-            .append(".png';");
-    EXPECT_TRUE(content::ExecuteScript(contents, create_image_script));
-  }
+  // Adds an image to the page. It should not be allowed to load at first.
+  // PageLoadCappingBlocksLoads tests that it is not loaded more robustly
+  std::string create_image_script =
+      std::string(
+          "var image = document.createElement('img'); "
+          "document.body.appendChild(image); image.src = '")
+          .append(kImagePrefix)
+          .append(".png';");
+  ASSERT_TRUE(content::ExecuteScript(contents, create_image_script));
 
+  // Previous image should be allowed to load now.
   handles.clear();
 
-  // Wait for the request to start.
-  listener.WaitForConnections();
+  // An image should be fetched because subresource loading was paused then
+  // resumed.
+  if (images_attempted() < 1u)
+    WaitForRequest();
+  EXPECT_EQ(1u, images_attempted());
+  https_test_server_.reset();
 }
 
 IN_PROC_BROWSER_TEST_F(PageLoadCappingBrowserTest, PageLoadCappingAllowLoads) {
-  net::EmbeddedTestServer https_test_server(
-      net::EmbeddedTestServer::TYPE_HTTPS);
-  // The main resource and the favicon should be fetched. Additionally, an image
-  // will be fetched after script is injected.
-  net::test_server::SimpleConnectionListener listener(
-      7, net::test_server::SimpleConnectionListener::
-             FAIL_ON_ADDITIONAL_CONNECTIONS);
-
-  https_test_server.SetConnectionListener(&listener);
-
-  https_test_server.ServeFilesFromSourceDirectory(base::FilePath(kDocRoot));
-
-  ASSERT_TRUE(https_test_server.Start());
+  // Tests that the image request loads normally when the page has not been
+  // paused.
+  https_test_server_->ServeFilesFromSourceDirectory(base::FilePath(kDocRoot));
+  https_test_server_->RegisterRequestHandler(base::BindRepeating(
+      &PageLoadCappingBrowserTest::HandleRequest, base::Unretained(this)));
+  ASSERT_TRUE(https_test_server_->Start());
 
   // Load a mostly empty page.
-  ui_test_utils::NavigateToURL(browser(),
-                               https_test_server.GetURL("/page_capping.html"));
+  ui_test_utils::NavigateToURL(
+      browser(), https_test_server_->GetURL("/page_capping.html"));
 
   content::WebContents* contents =
       browser()->tab_strip_model()->GetActiveWebContents();
 
-  // Adds images to the page. They should be allowed to load.
-  // Running this 5 times makes 5 round trips to the renderer, making it very
-  // likely the earlier requests would have started by the time all of the calls
-  // have been made.
-  for (size_t i = 0; i < 5; ++i) {
-    std::string create_image_script =
-        std::string(
-            "var image = document.createElement('img'); "
-            "document.body.appendChild(image); image.src = 'image")
-            .append(base::IntToString(i))
-            .append(".png';");
-    EXPECT_TRUE(content::ExecuteScript(contents, create_image_script));
-  }
+  // Adds an image to the page. It should be allowed to load.
+  std::string create_image_script =
+      std::string(
+          "var image = document.createElement('img'); "
+          "document.body.appendChild(image); image.src = '")
+          .append(kImagePrefix)
+          .append(".png';");
+  ASSERT_TRUE(content::ExecuteScript(contents, create_image_script));
 
-  // Wait for the request to start.
-  listener.WaitForConnections();
+  // An image should be fetched because subresource loading was never paused.
+  if (images_attempted() < 1u)
+    WaitForRequest();
+  EXPECT_EQ(1u, images_attempted());
+  https_test_server_.reset();
 }
