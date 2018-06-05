4a22778e1a510f8f148f9830b2dc2e1700d1814f
Dominic Battre <battre@chromium.org>
2018-06-05 08:24:36 +0000

    Disable several PasswordManagerBrowserTestWithConditionalPopupViews tests
    Disable several tests due to flakiness. Details of the failures are captured in
    the linked bug.
    
    TBR=vasilii@chromium.org
    
    Bug: 849582
    Change-Id: I6dcc9196310bf351366675531731325704f6d730
    Reviewed-on: https://chromium-review.googlesource.com/1086792
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564405}
    Former-commit-id: ca7b48c79b3edcfbbf93f109cbd81ae886cc487f

--- chrome/browser/password_manager/password_manager_interactive_uitest.cc
+++ chrome/browser/password_manager/password_manager_interactive_uitest.cc
@@ -277,36 +277,40 @@ IN_PROC_BROWSER_TEST_P(PasswordManagerBrowserTestWithConditionalPopupViews,
                                  std::string(), "password", submit);
 }
 
+// Disabled for flakiness crbug.com/849582.
 // Tests that if a site embeds the login and signup forms into one <form>, the
 // login form still gets autofilled.
 IN_PROC_BROWSER_TEST_P(PasswordManagerBrowserTestWithConditionalPopupViews,
-                       AutofillLoginSignupForm) {
+                       DISABLED_AutofillLoginSignupForm) {
   std::string submit = "document.getElementById('submit').click();";
   VerifyPasswordIsSavedAndFilled("/password/login_signup_form.html", "username",
                                  "password", submit);
 }
 
+// Disabled for flakiness crbug.com/849582.
 // Tests that password suggestions still work if the fields have the
 // "autocomplete" attribute set to off.
 IN_PROC_BROWSER_TEST_P(PasswordManagerBrowserTestWithConditionalPopupViews,
-                       AutofillPasswordFormWithAutocompleteOff) {
+                       DISABLED_AutofillPasswordFormWithAutocompleteOff) {
   std::string submit = "document.getElementById('submit').click();";
   VerifyPasswordIsSavedAndFilled(
       "/password/password_autocomplete_off_test.html", "username", "password",
       submit);
 }
 
+// Disabled for flakiness crbug.com/849582.
 IN_PROC_BROWSER_TEST_P(PasswordManagerBrowserTestWithConditionalPopupViews,
-                       AutofillPasswordNoFormElement) {
+                       DISABLED_AutofillPasswordNoFormElement) {
   VerifyPasswordIsSavedAndFilled("/password/no_form_element.html",
                                  "username_field", "password_field",
                                  "send_xhr();");
 }
 
+// Disabled for flakiness crbug.com/849582.
 // Check that we can fill in cases where <base href> is set and the action of
 // the form is not set. Regression test for https://crbug.com/360230.
 IN_PROC_BROWSER_TEST_P(PasswordManagerBrowserTestWithConditionalPopupViews,
-                       AutofillBaseTagWithNoActionTest) {
+                       DISABLED_AutofillBaseTagWithNoActionTest) {
   std::string submit = "document.getElementById('submit_button').click();";
   VerifyPasswordIsSavedAndFilled("/password/password_xhr_submit.html",
                                  "username_field", "password_field", submit);
