5920a93ec56c1368d538d8e41e46de152f6efa6d
Sky Malice <skym@google.com>
2018-06-05 18:18:02 +0000

    [Feed] Initial setup and display of feed.
    Bug: 819305, 803317
    Change-Id: I11c4eae7313a45faa64c6a771fb89a296eb2181b
    Reviewed-on: https://chromium-review.googlesource.com/1050746
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564581}
    Former-commit-id: a3daaa494c24047f29f32f7309d2ed94d54bfa2e

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedNewTabPage.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedNewTabPage.java
@@ -4,8 +4,23 @@
 
 package org.chromium.chrome.browser.feed;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.widget.FrameLayout;
+
+import com.google.android.libraries.feed.api.common.ThreadUtils;
+import com.google.android.libraries.feed.api.scope.FeedProcessScope;
+import com.google.android.libraries.feed.api.scope.FeedStreamScope;
+import com.google.android.libraries.feed.api.stream.Stream;
+import com.google.android.libraries.feed.common.time.SystemClockImpl;
+import com.google.android.libraries.feed.host.config.Configuration;
+import com.google.android.libraries.feed.host.config.Configuration.ConfigKey;
+import com.google.android.libraries.feed.host.config.DebugBehavior;
+import com.google.android.libraries.feed.host.stream.CardConfiguration;
+import com.google.android.libraries.feed.host.stream.StreamConfiguration;
+import com.google.android.libraries.feed.hostimpl.logging.LoggingApiImpl;
+import com.google.android.libraries.feed.hostimpl.scheduler.TimeoutScheduler;
 
 import org.chromium.chrome.browser.BasicNativePage;
 import org.chromium.chrome.browser.ChromeActivity;
@@ -16,17 +31,55 @@ import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.suggestions.SuggestionsNavigationDelegateImpl;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 
+import java.util.concurrent.Executors;
+
 /**
  * Provides a new tab page that displays an interest feed rendered list of content suggestions.
  */
 public class FeedNewTabPage extends BasicNativePage {
-    private final FeedActionHandler mActionHandler;
+    private static FeedProcessScope sFeedProcessScope;
 
-    private View mRootView;
+    private FrameLayout mRootView;
     private String mTitle;
+    private FeedImageLoader mImageLoader;
+
+    private static class StubStreamConfiguration implements StreamConfiguration {
+        @Override
+        public int getPaddingStart() {
+            return 0;
+        }
+        @Override
+        public int getPaddingEnd() {
+            return 0;
+        }
+        @Override
+        public int getPaddingTop() {
+            return 0;
+        }
+        @Override
+        public int getPaddingBottom() {
+            return 0;
+        }
+    }
+
+    private static class StubCardConfiguration implements CardConfiguration {
+        @Override
+        public int getDefaultCornerRadius() {
+            return 0;
+        }
+        @Override
+        public Drawable getCardBackground() {
+            return null;
+        }
+        @Override
+        public float getCardBottomPadding() {
+            return 0;
+        }
+    }
 
     /**
      * Constructs a new FeedNewTabPage.
+     *
      * @param activity The containing {@link ChromeActivity}.
      * @param nativePageHost The host for this native page.
      * @param tabModelSelector The {@link TabModelSelector} for the containing activity.
@@ -35,13 +88,47 @@ public class FeedNewTabPage extends BasicNativePage {
             TabModelSelector tabModelSelector) {
         super(activity, nativePageHost);
 
-        // Initialize Action Handler
         Profile profile = nativePageHost.getActiveTab().getProfile();
+        Configuration configHostApi =
+                new Configuration.Builder()
+                        .put(ConfigKey.FEED_SERVER_HOST, "https://www.google.com")
+                        .put(ConfigKey.FEED_SERVER_PATH_AND_PARAMS,
+                                "/httpservice/noretry/NowStreamService/FeedQuery")
+                        .put(ConfigKey.SESSION_LIFETIME_MS, 300000L)
+                        .build();
+
+        if (sFeedProcessScope == null) {
+            sFeedProcessScope =
+                    new FeedProcessScope
+                            .Builder(configHostApi, Executors.newSingleThreadExecutor(),
+                                    new LoggingApiImpl(), new FeedNetworkBridge(profile),
+                                    new TimeoutScheduler(new ThreadUtils(), new SystemClockImpl(),
+                                            configHostApi),
+                                    DebugBehavior.VERBOSE)
+                            .build();
+        }
+
+        mImageLoader = new FeedImageLoader(profile, activity);
         SuggestionsNavigationDelegateImpl navigationDelegate =
                 new SuggestionsNavigationDelegateImpl(
                         activity, profile, nativePageHost, tabModelSelector);
-        mActionHandler = new FeedActionHandler(navigationDelegate);
-        // TODO(huayinz): Pass the action handler into Stream.
+        FeedStreamScope streamScope = sFeedProcessScope
+                                              .createFeedStreamScopeBuilder(activity, mImageLoader,
+                                                      new FeedActionHandler(navigationDelegate),
+                                                      new StubStreamConfiguration(),
+                                                      new StubCardConfiguration(), configHostApi)
+                                              .build();
+
+        Stream stream = streamScope.getStream();
+        stream.onCreate(null);
+        stream.getView().setBackgroundColor(Color.WHITE);
+        mRootView.addView(stream.getView());
+
+        // TODO(skym): This is a work around for outstanding Feed bug. Should be
+        // removed on next DEPS roll.
+        stream.triggerRefresh();
+
+        // TODO(https://crbug.com/803317): Call appropriate lifecycle methods.
     }
 
     @Override
@@ -49,9 +136,13 @@ public class FeedNewTabPage extends BasicNativePage {
         mRootView = new FrameLayout(activity);
         mRootView.setLayoutParams(new FrameLayout.LayoutParams(
                 FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
-        // TODO(twellington): Add Stream to root view.
-
         mTitle = activity.getResources().getString(org.chromium.chrome.R.string.button_new_tab);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        mImageLoader.destroy();
     }
 
     @Override
