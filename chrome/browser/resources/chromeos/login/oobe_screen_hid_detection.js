04170b7bd377af5a3a9749fceced88195cfa4e51
Steven Bennetts <stevenjb@chromium.org>
2018-06-05 17:49:26 +0000

    OOBE: Eliminate non MD update and hid screens
    Bug: 647411
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I4a0519bd64bec46d131ea52cafe00634a8576065
    Reviewed-on: https://chromium-review.googlesource.com/1079962
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564566}
    Former-commit-id: 93244d98f79d6f2f7056f2715f73f91c93b5387d

--- chrome/browser/resources/chromeos/login/oobe_screen_hid_detection.js
+++ chrome/browser/resources/chromeos/login/oobe_screen_hid_detection.js
@@ -31,12 +31,6 @@ login.createScreen('HIDDetectionScreen', 'hid-detection', function() {
       UPDATE: 'update',
     },
 
-    // Possible ids of device blocks.
-    BLOCK: {
-      MOUSE: 'hid-mouse-block',
-      KEYBOARD: 'hid-keyboard-block',
-    },
-
     /**
      * Button to move to usual OOBE flow after detection.
      * @private
@@ -52,23 +46,19 @@ login.createScreen('HIDDetectionScreen', 'hid-detection', function() {
       this.context.addObserver(CONTEXT_KEY_MOUSE_STATE, function(stateId) {
         if (stateId === undefined)
           return;
-        self.setDeviceBlockState_('hid-mouse-block', stateId);
         $('oobe-hid-detection-md').setMouseState(stateId);
       });
       this.context.addObserver(CONTEXT_KEY_KEYBOARD_STATE, function(stateId) {
         self.updatePincodeKeysState_();
         if (stateId === undefined)
           return;
-        self.setDeviceBlockState_('hid-keyboard-block', stateId);
         $('oobe-hid-detection-md').setKeyboardState(stateId);
         if (stateId == self.CONNECTION.PAIRED) {
-          var label = self.context.get(CONTEXT_KEY_KEYBOARD_LABEL, '');
-          $('hid-keyboard-label-paired').textContent = label;
-          $('oobe-hid-detection-md').keyboardPairedLabel = label;
+          $('oobe-hid-detection-md').keyboardPairedLabel =
+              self.context.get(CONTEXT_KEY_KEYBOARD_LABEL, '');
         } else if (stateId == self.CONNECTION.PAIRING) {
-          var label = self.context.get(CONTEXT_KEY_KEYBOARD_LABEL, '');
-          $('hid-keyboard-label-pairing').textContent = label;
-          $('oobe-hid-detection-md').keyboardPairingLabel = label;
+          $('oobe-hid-detection-md').keyboardPairingLabel =
+              self.context.get(CONTEXT_KEY_KEYBOARD_LABEL, '');
         }
       });
       this.context.addObserver(
@@ -82,52 +72,15 @@ login.createScreen('HIDDetectionScreen', 'hid-detection', function() {
           this.updatePincodeKeysState_.bind(this));
       this.context.addObserver(
           CONTEXT_KEY_CONTINUE_BUTTON_ENABLED, function(enabled) {
-            $('hid-continue-button').disabled = !enabled;
             $('oobe-hid-detection-md').continueButtonDisabled = !enabled;
           });
-    },
-
-    /**
-     * Buttons in oobe wizard's button strip.
-     * @type {array} Array of Buttons.
-     */
-    get buttons() {
-      var buttons = [];
-      var continueButton = this.ownerDocument.createElement('button');
-      continueButton.id = 'hid-continue-button';
-      continueButton.textContent =
-          loadTimeData.getString('hidDetectionContinue');
-      continueButton.addEventListener('click', function(e) {
-        chrome.send('HIDDetectionOnContinue');
-        e.stopPropagation();
-      });
-      buttons.push(continueButton);
-
-      return buttons;
     },
 
     /**
      * Returns a control which should receive an initial focus.
      */
     get defaultControl() {
-      return $('hid-continue-button');
-    },
-
-    /**
-     * Sets a device-block css class to reflect device state of searching, usb,
-     * connected, pairing or paired (for BT devices).
-     * @param {blockId} id one of keys of this.BLOCK dict.
-     * @param {state} one of keys of this.CONNECTION dict.
-     * @private
-     */
-    setDeviceBlockState_: function(blockId, state) {
-      if (state == 'update')
-        return;
-      var deviceBlock = $(blockId);
-      for (var key in this.CONNECTION) {
-        var stateCase = this.CONNECTION[key];
-        deviceBlock.classList.toggle(stateCase, stateCase == state);
-      }
+      return $('oobe-hid-detection-md');
     },
 
     /**
@@ -137,7 +90,6 @@ login.createScreen('HIDDetectionScreen', 'hid-detection', function() {
     setPointingDeviceState: function(state) {
       if (state === undefined)
         return;
-      this.setDeviceBlockState_(this.BLOCK.MOUSE, state);
       $('oobe-hid-detection-md').setMouseState(state);
     },
 
@@ -145,14 +97,11 @@ login.createScreen('HIDDetectionScreen', 'hid-detection', function() {
      * Updates state for pincode key elements based on context state.
      */
     updatePincodeKeysState_: function() {
-      var pincodeKeys = $('hid-keyboard-pincode');
       var pincode = this.context.get(CONTEXT_KEY_KEYBOARD_PINCODE, '');
       var state = this.context.get(CONTEXT_KEY_KEYBOARD_STATE, '');
       var label = this.context.get(CONTEXT_KEY_KEYBOARD_LABEL, '');
-
       var entered =
           this.context.get(CONTEXT_KEY_KEYBOARD_ENTERED_PART_PINCODE, 0);
-
       // Whether the functionality of getting num of entered keys is available.
       var expected =
           this.context.get(CONTEXT_KEY_KEYBOARD_ENTERED_PART_EXPECTED, false);
@@ -160,31 +109,6 @@ login.createScreen('HIDDetectionScreen', 'hid-detection', function() {
       $('oobe-hid-detection-md').setKeyboardState(state);
       $('oobe-hid-detection-md')
           .setPincodeState(pincode, entered, expected, label);
-
-      if (!pincode || state !== this.CONNECTION.PAIRING) {
-        pincodeKeys.hidden = true;
-        return;
-      }
-
-      if (pincodeKeys.hidden) {
-        pincodeKeys.hidden = false;
-        announceAccessibleMessage(
-            this.context.get(CONTEXT_KEY_KEYBOARD_LABEL, '') + ' ' + pincode +
-            ' ' + loadTimeData.getString('hidDetectionBTEnterKey'));
-      }
-
-      if (pincode.length != PINCODE_LENGTH)
-        console.error('Wrong pincode length');
-
-      // Pincode keys plus Enter key.
-      for (var i = 0; i < (PINCODE_LENGTH + 1); i++) {
-        var pincodeSymbol = $('hid-keyboard-pincode-sym-' + (i + 1));
-        pincodeSymbol.classList.toggle('key-typed', i < entered && expected);
-        pincodeSymbol.classList.toggle('key-untyped', i > entered && expected);
-        pincodeSymbol.classList.toggle('key-next', i == entered && expected);
-        if (i < PINCODE_LENGTH)
-          pincodeSymbol.textContent = pincode[i] ? pincode[i] : '';
-      }
     },
 
     /**
@@ -193,9 +117,6 @@ login.createScreen('HIDDetectionScreen', 'hid-detection', function() {
      */
     onBeforeShow: function(data) {
       this.setMDMode_();
-      this.setDeviceBlockState_('hid-mouse-block', this.CONNECTION.SEARCHING);
-      this.setDeviceBlockState_(
-          'hid-keyboard-block', this.CONNECTION.SEARCHING);
       $('oobe-hid-detection-md').setMouseState(this.CONNECTION.SEARCHING);
       $('oobe-hid-detection-md').setKeyboardState(this.CONNECTION.SEARCHING);
     },
@@ -206,8 +127,6 @@ login.createScreen('HIDDetectionScreen', 'hid-detection', function() {
      */
     setMDMode_: function() {
       $('oobe-hid-detection-md').hidden = false;
-      // TODO(stevenjb): Remove oobe-hid-detection. https://crbug.com/647411.
-      $('oobe-hid-detection').hidden = true;
     },
   };
 });
