940a846293f4c9c198f01416c286708af11ad32f
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-06-05 18:04:16 +0000

    [Cleanup] Update histogram_tester.h includes in //extensions
    Update all histogram_tester.h includes in //extensions to include
    //base/test/metrics/histogram_tester.h
    
    TBR=lazyboy@chromium.org
    
    Bug: 846421
    Change-Id: I5abdeff3f8bf9a34d9782f292ddf1e38ffd2434c
    Reviewed-on: https://chromium-review.googlesource.com/1087294
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564576}
    Former-commit-id: f603053c17def597f455c1a471e62abd1626dc45

--- extensions/browser/api/web_request/web_request_time_tracker_unittest.cc
+++ extensions/browser/api/web_request/web_request_time_tracker_unittest.cc
@@ -7,7 +7,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#include "base/test/histogram_tester.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
