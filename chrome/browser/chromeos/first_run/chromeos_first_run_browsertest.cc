3414730255a424db31c207a910587691f297e27f
Tetsui Ohkubo <tetsui@chromium.org>
2018-06-06 00:08:20 +0000

    Fix Mash compatible SystemTray browser tests.
    This CL fixes browser tests when UnifiedSystemTray is enabled.
    These tests already works on Mash, so they are easier to port to
    UnifiedSystemTray by introducing UnifiedSystemTrayTestApi
    https://crrev.com/c/1086674 .
    Remaining SystemTray browser tests do not work with Mash, so
    we first have to make them work with Mash (in upcoming CLs).
    
    TEST=browser_tests --enable-features=SystemTrayUnified
    BUG=847104
    
    Change-Id: Ic6ce4ce547913240b68a32959dc86242276807e9
    Reviewed-on: https://chromium-review.googlesource.com/1086683
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564718}
    Former-commit-id: 1764a0dc7928ab6fb83201bf457ac72a976282c6

--- chrome/browser/chromeos/first_run/chromeos_first_run_browsertest.cc
+++ chrome/browser/chromeos/first_run/chromeos_first_run_browsertest.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "ash/public/cpp/ash_features.h"
 #include "ash/public/interfaces/constants.mojom.h"
 #include "ash/public/interfaces/system_tray_test_api.mojom.h"
 #include "chrome/browser/chromeos/first_run/first_run.h"
@@ -188,10 +189,12 @@ IN_PROC_BROWSER_TEST_F(FirstRunUIBrowserTest, FirstRunFlow) {
   FlushForTesting();
   EXPECT_TRUE(IsTrayBubbleOpen());
 
-  AdvanceStep();
-  WaitForStep(first_run::kHelpStep);
-  FlushForTesting();
-  EXPECT_TRUE(IsTrayBubbleOpen());
+  if (!ash::features::IsSystemTrayUnifiedEnabled()) {
+    AdvanceStep();
+    WaitForStep(first_run::kHelpStep);
+    FlushForTesting();
+    EXPECT_TRUE(IsTrayBubbleOpen());
+  }
 
   AdvanceStep();
   WaitForFinalization();
