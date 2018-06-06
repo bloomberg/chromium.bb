be3608b0ec323d151a09d47a8e6ca5430d2c9429
Don Garrett <dgarrett@google.com>
2018-06-05 17:10:09 -0700

    gen_luci_scheduler_config: Add new script.
    This script can generate a new LUCI Scheduler configuration file based
    on the chromeos_config. Nothing publishes this scheduler configuration
    file, but it can be used manually.
    
    BUG=chromium:848547
    TEST=run_tests + manually comparing the output with live config.
    
    Change-Id: Ib5dae431a06fd1d5b0e8f06e150ee29307224f96
    Reviewed-on: https://chromium-review.googlesource.com/1087766
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Don Garrett <dgarrett@chromium.org>
    Reviewed-by: Don Garrett <dgarrett@chromium.org>

---
+++ scripts/gen_luci_scheduler.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Generate LUCI Scheduler config file.
+
+This generates the LUCI Scheduler configuration file for ChromeOS builds based
+on the chromeos_config contents.
+
+To update the production config:
+  cd $REPO/manifest-internal
+  git checkout infra/config
+  $REPO/chromite/scripts/gen_luci_scheduler > luci-scheduler.cfg
+  git commit -a
+  git cl upload        (yes, really) (Say Yes to removing commit-msg hook)
+  repo abandon infra/config
+
+Limitations:
+  This script can only handle builds on the "master" branch.
+"""
+
+from __future__ import print_function
+
+import sys
+
+from chromite.lib import commandline
+from chromite.lib import config_lib
+
+
+_CONFIG_HEADER = """# Defines buckets on luci-scheduler.appspot.com.
+#
+# For schema of this file and documentation see ProjectConfig message in
+# https://github.com/luci/luci-go/blob/master/scheduler/appengine/messages/config.proto
+
+# Generated with chromite/scripts/gen_luci_scheduler
+
+acl_sets {
+  name: "default"
+  acls {
+    role: READER
+    granted_to: "group:googlers"
+  }
+  acls {
+    role: OWNER
+    granted_to: "group:project-chromeos-admins"
+  }
+}
+"""
+
+
+def genSchedulerJob(build_config):
+  """Generate the luci scheduler job for a given build config.
+
+  Args:
+    build_config: config_lib.BuildConfig.
+
+  Returns:
+    Multiline string to include in the luci scheduler configuration.
+  """
+  # TODO: Move --buildbot arg into recipe, and remove from here.
+  template = """
+job {
+  id: "%(name)s"
+  acl_sets: "default"
+  schedule: "%(schedule)s"
+  buildbucket: {
+    server: "cr-buildbucket.appspot.com"
+    bucket: "luci.chromeos.general"
+    builder: "%(builder)s"
+    tags: "cbb_branch:%(branch)s"
+    tags: "cbb_display_label:%(display_label)s"
+    tags: "cbb_config:%(name)s"
+    properties: "cbb_branch:%(branch)s"
+    properties: "cbb_display_label:%(display_label)s"
+    properties: "cbb_config:%(name)s"
+    properties: "cbb_extra_args:[\\"--buildbot\\"]"
+  }
+}
+"""
+  return template % {
+      'name': build_config.name,
+      'builder': build_config.luci_builder,
+      'branch': 'master',
+      'display_label': build_config.display_label,
+      'schedule': build_config.schedule,
+  }
+
+
+def genSchedulerTrigger(trigger_name, repo, refs, builds):
+  """Generate the luci scheduler job for a given build config.
+
+  Args:
+    trigger_name: Name of the trigger as a string.
+    repo: Gitiles URL git git repository.
+    refs: Iterable of git refs to check. May use regular expressions.
+    builds: Iterable of build config names to trigger.
+
+  Returns:
+    Multiline string to include in the luci scheduler configuration.
+  """
+  template = """
+trigger {
+  id: "%(trigger_name)s"
+  acl_sets: "default"
+  schedule: "with 5m interval"
+  gitiles: {
+    repo: "%(repo)s"
+%(refs)s
+  }
+%(triggers)s
+}
+"""
+
+  return template % {
+      'trigger_name': trigger_name,
+      'repo': repo,
+      'refs': '\n'.join('    refs: "%s"' % r for r in refs),
+      'triggers': '\n'.join('  triggers: "%s"' % b for b in builds),
+  }
+
+
+def genLuciSchedulerConfig(site_config):
+  """Generate a luciSchedulerConfig as a string.
+
+  Args:
+    site_config: A config_lib.SiteConfig instance.
+
+  Returns:
+    The complete scheduler configuration contents as a string.
+  """
+  # Trigger collection is used to collect together trigger information, so
+  # we can reuse the same trigger for multiple builds as needed.
+  # It maps gitiles_key to a set of build_names.
+  # A gitiles_key = (gitiles_url, tuple(ref_list))
+  trigger_collection = {}
+
+  jobs = []
+  for name in sorted(site_config):
+    config = site_config[name]
+
+    # Populate jobs.
+    if config.schedule:
+      jobs.append(genSchedulerJob(config))
+
+    # Populate trigger_collection.
+    if config.triggered_gitiles:
+      for gitiles_url, ref_list in config.triggered_gitiles:
+        gitiles_key = (gitiles_url, tuple(ref_list))
+        trigger_collection.setdefault(gitiles_key, set())
+        trigger_collection[gitiles_key].add(config.name)
+
+  # Populate triggers.
+  triggers = []
+  trigger_counter = 0
+  for gitiles_key in sorted(trigger_collection):
+    builds = sorted(trigger_collection[gitiles_key])
+
+    trigger_name = 'trigger_%s' % trigger_counter
+    gitiles_url, refs = gitiles_key
+    triggers.append(genSchedulerTrigger(
+        trigger_name, gitiles_url, refs, builds))
+    trigger_counter += 1
+
+  return ''.join([_CONFIG_HEADER] + triggers + jobs)
+
+
+def GetParser():
+  """Creates the argparse parser."""
+  parser = commandline.ArgumentParser(description=__doc__)
+
+  parser.add_argument('-o', '--file_out', type='path',
+                      help='Write output to specified file.')
+
+  return parser
+
+
+def main(argv):
+  parser = GetParser()
+  options = parser.parse_args(argv)
+  options.Freeze()
+
+  site_config = config_lib.GetConfig()
+
+  with (open(options.file_out, 'w') if options.file_out else sys.stdout) as fh:
+    fh.write(genLuciSchedulerConfig(site_config))
---
+++ scripts/gen_luci_scheduler_unittest.py
@@ -0,0 +1,272 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Test gen_luci_scheduler."""
+from __future__ import print_function
+
+from chromite.lib import cros_test_lib
+from chromite.lib import config_lib
+from chromite.lib import config_lib_unittest
+from chromite.scripts import gen_luci_scheduler
+
+# It's reasonable for unittests to look at internals.
+# pylint: disable=protected-access
+
+
+class GenLuciSchedulerTest(cros_test_lib.MockTestCase):
+  """Tests for cbuildbot_launch script."""
+  def testSanityAgainstProd(self):
+    """Test we can generate a luci scheduler config with live data."""
+    # If it runs without crashing, we pass.
+    gen_luci_scheduler.genLuciSchedulerConfig(config_lib.GetConfig())
+
+  def testGenSchedulerJob(self):
+    """Test the job creation helper."""
+    build_config = config_lib_unittest.MockBuildConfig().apply(
+        schedule='funky schedule'
+    )
+
+    expected = '''
+job {
+  id: "amd64-generic-paladin"
+  acl_sets: "default"
+  schedule: "funky schedule"
+  buildbucket: {
+    server: "cr-buildbucket.appspot.com"
+    bucket: "luci.chromeos.general"
+    builder: "Prod"
+    tags: "cbb_branch:master"
+    tags: "cbb_display_label:MockLabel"
+    tags: "cbb_config:amd64-generic-paladin"
+    properties: "cbb_branch:master"
+    properties: "cbb_display_label:MockLabel"
+    properties: "cbb_config:amd64-generic-paladin"
+    properties: "cbb_extra_args:[\\"--buildbot\\"]"
+  }
+}
+'''
+
+    result = gen_luci_scheduler.genSchedulerJob(build_config)
+    self.assertEqual(result, expected)
+
+  def testGenSchedulerTriggerSimple(self):
+    """Test the trigger creation helper."""
+    trigger_name = 'simple'
+    repo = 'url://repo'
+    refs = ['refs/path']
+    builds = ['test_build']
+
+    expected = '''
+trigger {
+  id: "simple"
+  acl_sets: "default"
+  schedule: "with 5m interval"
+  gitiles: {
+    repo: "url://repo"
+    refs: "refs/path"
+  }
+  triggers: "test_build"
+}
+'''
+
+    result = gen_luci_scheduler.genSchedulerTrigger(
+        trigger_name, repo, refs, builds)
+
+    self.assertEqual(result, expected)
+
+  def testGenSchedulerTriggerComplex(self):
+    """Test the trigger creation helper."""
+    trigger_name = 'complex'
+    repo = 'url://repo'
+    refs = ['refs/path', 'refs/other_path']
+    builds = ['test_build_a', 'test_build_b']
+
+    expected = '''
+trigger {
+  id: "complex"
+  acl_sets: "default"
+  schedule: "with 5m interval"
+  gitiles: {
+    repo: "url://repo"
+    refs: "refs/path"
+    refs: "refs/other_path"
+  }
+  triggers: "test_build_a"
+  triggers: "test_build_b"
+}
+'''
+
+    result = gen_luci_scheduler.genSchedulerTrigger(
+        trigger_name, repo, refs, builds)
+
+    self.assertEqual(result, expected)
+
+  def testGenLuciSchedulerConfig(self):
+    """Test a full LUCI Scheduler config file."""
+    site_config = config_lib.SiteConfig()
+
+    site_config.Add(
+        'not_scheduled',
+        luci_builder='ProdBuilder',
+        display_label='MockLabel',
+    )
+
+    site_config.Add(
+        'build_prod',
+        luci_builder='ProdBuilder',
+        display_label='MockLabel',
+        schedule='run once in a while',
+    )
+
+    site_config.Add(
+        'build_tester',
+        luci_builder='TestBuilder',
+        display_label='TestLabel',
+        schedule='run daily',
+    )
+
+    site_config.Add(
+        'build_triggered_a',
+        luci_builder='ProdBuilder',
+        display_label='MockLabel',
+        schedule='triggered',
+        triggered_gitiles=[[
+            'gitiles_url_a',
+            ['ref_a', 'ref_b'],
+        ], [
+            'gitiles_url_b',
+            ['ref_c'],
+        ]],
+    )
+
+    site_config.Add(
+        'build_triggered_b',
+        luci_builder='ProdBuilder',
+        display_label='MockLabel',
+        schedule='triggered',
+        triggered_gitiles=[[
+            'gitiles_url_b',
+            ['ref_c'],
+        ]],
+    )
+
+    expected = '''# Defines buckets on luci-scheduler.appspot.com.
+#
+# For schema of this file and documentation see ProjectConfig message in
+# https://github.com/luci/luci-go/blob/master/scheduler/appengine/messages/config.proto
+
+# Generated with chromite/scripts/gen_luci_scheduler
+
+acl_sets {
+  name: "default"
+  acls {
+    role: READER
+    granted_to: "group:googlers"
+  }
+  acls {
+    role: OWNER
+    granted_to: "group:project-chromeos-admins"
+  }
+}
+
+trigger {
+  id: "trigger_0"
+  acl_sets: "default"
+  schedule: "with 5m interval"
+  gitiles: {
+    repo: "gitiles_url_a"
+    refs: "ref_a"
+    refs: "ref_b"
+  }
+  triggers: "build_triggered_a"
+}
+
+trigger {
+  id: "trigger_1"
+  acl_sets: "default"
+  schedule: "with 5m interval"
+  gitiles: {
+    repo: "gitiles_url_b"
+    refs: "ref_c"
+  }
+  triggers: "build_triggered_a"
+  triggers: "build_triggered_b"
+}
+
+job {
+  id: "build_prod"
+  acl_sets: "default"
+  schedule: "run once in a while"
+  buildbucket: {
+    server: "cr-buildbucket.appspot.com"
+    bucket: "luci.chromeos.general"
+    builder: "ProdBuilder"
+    tags: "cbb_branch:master"
+    tags: "cbb_display_label:MockLabel"
+    tags: "cbb_config:build_prod"
+    properties: "cbb_branch:master"
+    properties: "cbb_display_label:MockLabel"
+    properties: "cbb_config:build_prod"
+    properties: "cbb_extra_args:[\\"--buildbot\\"]"
+  }
+}
+
+job {
+  id: "build_tester"
+  acl_sets: "default"
+  schedule: "run daily"
+  buildbucket: {
+    server: "cr-buildbucket.appspot.com"
+    bucket: "luci.chromeos.general"
+    builder: "TestBuilder"
+    tags: "cbb_branch:master"
+    tags: "cbb_display_label:TestLabel"
+    tags: "cbb_config:build_tester"
+    properties: "cbb_branch:master"
+    properties: "cbb_display_label:TestLabel"
+    properties: "cbb_config:build_tester"
+    properties: "cbb_extra_args:[\\"--buildbot\\"]"
+  }
+}
+
+job {
+  id: "build_triggered_a"
+  acl_sets: "default"
+  schedule: "triggered"
+  buildbucket: {
+    server: "cr-buildbucket.appspot.com"
+    bucket: "luci.chromeos.general"
+    builder: "ProdBuilder"
+    tags: "cbb_branch:master"
+    tags: "cbb_display_label:MockLabel"
+    tags: "cbb_config:build_triggered_a"
+    properties: "cbb_branch:master"
+    properties: "cbb_display_label:MockLabel"
+    properties: "cbb_config:build_triggered_a"
+    properties: "cbb_extra_args:[\\"--buildbot\\"]"
+  }
+}
+
+job {
+  id: "build_triggered_b"
+  acl_sets: "default"
+  schedule: "triggered"
+  buildbucket: {
+    server: "cr-buildbucket.appspot.com"
+    bucket: "luci.chromeos.general"
+    builder: "ProdBuilder"
+    tags: "cbb_branch:master"
+    tags: "cbb_display_label:MockLabel"
+    tags: "cbb_config:build_triggered_b"
+    properties: "cbb_branch:master"
+    properties: "cbb_display_label:MockLabel"
+    properties: "cbb_config:build_triggered_b"
+    properties: "cbb_extra_args:[\\"--buildbot\\"]"
+  }
+}
+'''
+    result = gen_luci_scheduler.genLuciSchedulerConfig(site_config)
+
+    self.assertEqual(result, expected)
