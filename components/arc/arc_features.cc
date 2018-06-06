2bcb9c5069abec64474f88d13c5ab9ea4f3568f8
B. Menegola <menegola@chromium.org>
2018-06-06 20:51:26 +0000

    Enable ArcAvailableForChildAccount by default.
    Leave the flag in place so we can revert the state in case we have any blockers.
    
    R=escordeiro@chromium.org, yusukes@chromium.org
    
    Bug: 828983
    Change-Id: I9886bde062fc076acc34fe14cf42203784ef18f5
    Reviewed-on: https://chromium-review.googlesource.com/1089150
    Commit-Queue: B. Menegola <menegola@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565029}
    Former-commit-id: 59bab866dc95e223150b384b847d86ada5159653

--- components/arc/arc_features.cc
+++ components/arc/arc_features.cc
@@ -8,7 +8,7 @@ namespace arc {
 
 // Controls whether ARC is available for CHILD accounts.
 const base::Feature kAvailableForChildAccountFeature{
-    "ArcAvailableForChildAccount", base::FEATURE_DISABLED_BY_DEFAULT};
+    "ArcAvailableForChildAccount", base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Controls ACTION_BOOT_COMPLETED broadcast for third party applications on ARC.
 // When disabled, third party apps will not receive this broadcast.
