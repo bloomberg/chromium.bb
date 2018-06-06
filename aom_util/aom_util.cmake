638bbda8611693a8e7acc242c55767e545ab2759
Tom Finegan <tomfinegan@google.com>
2018-06-06 15:18:42 -0700

    cmake: Use list(APPEND) instead of set() in aom_util.cmake
    Change-Id: I6531675f2195de7108702f7c2ad4ae04d131ad71

--- aom_util/aom_util.cmake
+++ aom_util/aom_util.cmake
@@ -13,9 +13,11 @@ if(AOM_AOM_UTIL_AOM_UTIL_CMAKE_)
 endif() # AOM_AOM_UTIL_AOM_UTIL_CMAKE_
 set(AOM_AOM_UTIL_AOM_UTIL_CMAKE_ 1)
 
-set(AOM_UTIL_SOURCES "${AOM_ROOT}/aom_util/aom_thread.c"
-    "${AOM_ROOT}/aom_util/aom_thread.h" "${AOM_ROOT}/aom_util/endian_inl.h"
-    "${AOM_ROOT}/aom_util/debug_util.c" "${AOM_ROOT}/aom_util/debug_util.h")
+list(APPEND AOM_UTIL_SOURCES "${AOM_ROOT}/aom_util/aom_thread.c"
+            "${AOM_ROOT}/aom_util/aom_thread.h"
+            "${AOM_ROOT}/aom_util/endian_inl.h"
+            "${AOM_ROOT}/aom_util/debug_util.c"
+            "${AOM_ROOT}/aom_util/debug_util.h")
 
 # Creates the aom_util build target and makes libaom depend on it. The libaom
 # target must exist before this function is called.
