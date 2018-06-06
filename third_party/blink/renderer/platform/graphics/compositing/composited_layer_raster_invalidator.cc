a82ccd8129ebd952d6362afe2c11ced0784ccd27
Xianzhu Wang <wangxianzhu@chromium.org>
2018-06-06 02:56:31 +0000

    [PE] Check relative diff for scale components when checking transform changes
    The case is extended from crbug.com/849382. Some elements may display
    by scaling down from huge geometries with a tiny scale. When the scale
    changes (e.g. from 1e-6 from 2e-6), the rasterized result will
    significantly change which should not be ignored.
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I31928d95f8d864de83a75af02fd3cfe95ef8e8fa
    Reviewed-on: https://chromium-review.googlesource.com/1087812
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#564776}
    Former-commit-id: cb6ae972afd9c7aed9229c8e8c101ccb506fbaa3

--- third_party/blink/renderer/platform/graphics/compositing/composited_layer_raster_invalidator.cc
+++ third_party/blink/renderer/platform/graphics/compositing/composited_layer_raster_invalidator.cc
@@ -48,7 +48,13 @@ size_t CompositedLayerRasterInvalidator::MatchNewChunkToOldChunk(
 static bool ApproximatelyEqual(const SkMatrix& a, const SkMatrix& b) {
   static constexpr float kTolerance = 1e-5f;
   for (int i = 0; i < 9; i++) {
-    if (std::abs(a[i] - b[i]) > kTolerance)
+    auto difference = std::abs(a[i] - b[i]);
+    // Check for absolute difference.
+    if (difference > kTolerance)
+      return false;
+    // For scale components, also check for relative difference.
+    if ((i == 0 || i == 4 || i == 9) &&
+        difference > (std::abs(a[i]) + std::abs(b[i])) * kTolerance)
       return false;
   }
   return true;
