8e9793afc09b385eab0faf332d5f363393cfd17b
Mustaq Ahmed <mustaq@google.com>
2018-06-06 19:37:11 +0000

    Fix RFH::OnUpdateUserActivationState for deleted RFH case.
    Bug: 780556
    Change-Id: Ic1feadf01adc9ea60b9246c57e503b29e801e305
    Reviewed-on: https://chromium-review.googlesource.com/1089117
    Commit-Queue: Mustaq Ahmed <mustaq@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565003}
    Former-commit-id: a6f9732fa453c84db446c9b4966ba24eb271da59

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -2847,6 +2847,8 @@ void RenderFrameHostImpl::NotifyUserActivation() {
 
 void RenderFrameHostImpl::OnUpdateUserActivationState(
     blink::UserActivationUpdateType update_type) {
+  if (!is_active())
+    return;
   frame_tree_node_->UpdateUserActivationState(update_type);
 }
 
