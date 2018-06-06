2665aef886bfe0c7f43585a067317d829e410d49
Bruce Dawson <brucedawson@chromium.org>
2018-06-06 23:52:33 +0000

    Update toolchain package to fix dbghelp.dll bug
    This change updates the VS 2017 package to fix a bad version of
    dbghelp.dll in the Debuggers package, caused by a now-obsolete
    workaround in the packaging script, removed in crrev.com/c/1086990.
    
    The package still uses VS 2017 Update 7.1 and the 10.0.17134.12 SDK.
    
    Packaging was done on a Windows Server 2016 VM, cleanly created for this
    purpose.
    
    Compiler was packaged up by downloading VS 2017 Update 7.1, from
    https://www.visualstudio.com/vs/, and then passing these parameters to
    the installer:
    
        --add Microsoft.VisualStudio.Workload.NativeDesktop
        --add Microsoft.VisualStudio.Component.VC.ATLMFC --includeRecommended
        --passive
    
    Then Add or Remove Programs was used to modify the 10.0.17134.0 SDK to add
    the Debuggers package.
    
    Then the packaging script was run like this:
    
      python depot_tools\win_toolchain\package_from_installed.py 2017 -w 10.0.17134.0
    
    The results were compared to make sure that there were no unintended changes. One
    quirk is that the new package is missing the arm/arm64 directories in Debuggers,
    which is correct, whereas the previous package was *not* missing them, for some
    reason. Other than that there are no unintended changes.
    
    Bug: 773476, 846313
    Change-Id: I43db2ea95999fb7b2aeb02ba078e70298b62ffad
    Reviewed-on: https://chromium-review.googlesource.com/1086992
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565106}
    Former-commit-id: ac88874511d02aaf42ebbefaf078d8c8f8c2af8a

--- build/vs_toolchain.py
+++ build/vs_toolchain.py
@@ -347,8 +347,9 @@ def _GetDesiredVsToolchainHashes():
   to build with."""
   env_version = GetVisualStudioVersion()
   if env_version == '2017':
-    # VS 2017 Update 7.1 (15.7.1) with 10.0.17134.12 SDK.
-    toolchain_hash = '5454e45bf3764c03d3fc1024b3bf5bc41e3ab62c'
+    # VS 2017 Update 7.1 (15.7.1) with 10.0.17134.12 SDK, rebuilt with
+    # dbghelp.dll fix.
+    toolchain_hash = '3bc0ec615cf20ee342f3bc29bc991b5ad66d8d2c'
     # Third parties that do not have access to the canonical toolchain can map
     # canonical toolchain version to their own toolchain versions.
     toolchain_hash_mapping_key = 'GYP_MSVS_HASH_%s' % toolchain_hash
