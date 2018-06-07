5524f160ae7a190ca0a00b2c15411c5cea3fa3fa
Mike Frysinger <vapier@chromium.org>
2018-06-07 15:12:43 -0400

    lint: add missing utf-8 encoding
    A few files slipped in while we didn't have lint coverage.
    
    BUG=None
    TEST=precq passes
    
    Change-Id: Iba7bf2467c722fdd2ee51f8e6f2c76c960cd7b16
    Reviewed-on: https://chromium-review.googlesource.com/1091411
    Commit-Ready: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Don Garrett <dgarrett@chromium.org>

--- cbuildbot/stages/handle_changes_stages.py
+++ cbuildbot/stages/handle_changes_stages.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright 2017 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
--- cli/cros/tests/lint_input/func_common_import.py
+++ cli/cros/tests/lint_input/func_common_import.py
@@ -1,2 +1,3 @@
+# -*- coding: utf-8 -*-
 __revision__ = None
 import lint_autotest_integration_test_example_module
