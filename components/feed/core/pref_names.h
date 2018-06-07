a685722295af33764825a100c17ced8c948ead41
Sky Malice <skym@google.com>
2018-06-07 16:55:49 +0000

    [Feed] Add empty pref_names files.
    Bug: 831648
    Change-Id: If86282e15d436bfa1e625f38941d59e47e3e5294
    Reviewed-on: https://chromium-review.googlesource.com/1089887
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565298}
    Former-commit-id: d0fd836c02a19a99c50f4aa75afb4ee7ced2a3c4

---
+++ components/feed/core/pref_names.h
@@ -0,0 +1,14 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_FEED_CORE_PREF_NAMES_H_
+#define COMPONENTS_FEED_CORE_PREF_NAMES_H_
+
+namespace feed {
+
+namespace prefs {}  // namespace prefs
+
+}  // namespace feed
+
+#endif  // COMPONENTS_FEED_CORE_PREF_NAMES_H_
