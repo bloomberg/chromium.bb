b4ac68120d77e278138132759f5124e3389bb2b0
Patti <patricialor@chromium.org>
2018-06-07 07:08:32 +0000

    MD Settings: Fix trashing site usage via Site Details.
    Site Details was calling a function that didn't exist when the user clicks
    the trash can button to delete site hard disk usage. Fix.
    
    Also add a confirmation dialog, update strings, and move the position of the
    reset settings button when #enable-site-settings is turned on.
    
    Manual test - with #enable-site-settings turned on, navigate to a site using
    hard disk usage (typically https://www.google.com works). Open the Page Info
    bubble, click "Site settings". When Site Details opens, it should say 'Usage' in
    one of the headings. Click the clear data button next to the amount of usage
    displayed, confirm the dialog, then verify it now shows a string that says 'No
    usage data'.
    
    Bug: 752859, 835712
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I1f89a60bcc3c63bebee82eae288460f51aabca0a
    Reviewed-on: https://chromium-review.googlesource.com/616503
    Commit-Queue: Patti <patricialor@chromium.org>
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565205}
    Former-commit-id: d4c83652a33127a0d4ec9b723e344b854f8f752a

--- chrome/test/data/webui/settings/site_details_tests.js
+++ chrome/test/data/webui/settings/site_details_tests.js
@@ -197,6 +197,50 @@ suite('SiteDetails', function() {
     assertTrue(testElement.$$('#usage').innerText.indexOf('1 KB') != -1);
   });
 
+  test('storage gets trashed properly', function() {
+    const origin = 'https://foo.com:443';
+    browserProxy.setPrefs(prefs);
+    loadTimeData.overrideValues({enableSiteSettings: true});
+    testElement = createSiteDetails(origin);
+
+    // Remove the current website-usage-private-api element.
+    const parent = testElement.$.usageApi.parentNode;
+    assertTrue(parent != undefined);
+    testElement.$.usageApi.remove();
+
+    // Replace it with a mock version.
+    let usageCleared = false;
+    Polymer({
+      is: 'mock-website-usage-private-api',
+
+      fetchUsageTotal: function(host) {
+        testElement.storedData_ = '1 KB';
+      },
+
+      clearUsage: function(origin, task) {
+        usageCleared = true;
+      },
+    });
+    let api = document.createElement('mock-website-usage-private-api');
+    testElement.$.usageApi = api;
+    Polymer.dom(parent).appendChild(api);
+    Polymer.dom.flush();
+
+    // Call onOriginChanged_() manually to simulate a new navigation.
+    testElement.onOriginChanged_(testElement.site);
+    return browserProxy.whenCalled('getOriginPermissions').then(() => {
+      // Ensure the mock's methods were called and check usage was cleared on
+      // clicking the trash button.
+      assertEquals('1 KB', testElement.storedData_);
+      assertTrue(testElement.$$('#noStorage').hidden);
+      assertFalse(testElement.$$('#storage').hidden);
+
+      MockInteractions.tap(
+          testElement.$$('#confirmClearStorage .action-button'));
+      assertTrue(usageCleared);
+    });
+  });
+
   test('correct pref settings are shown', function() {
     browserProxy.setPrefs(prefs);
     // Make sure all the possible content settings are shown for this test.
@@ -260,16 +304,17 @@ suite('SiteDetails', function() {
   test('show confirmation dialog on reset settings', function() {
     browserProxy.setPrefs(prefs);
     testElement = createSiteDetails('https://foo.com:443');
+    Polymer.dom.flush();
 
     // Check both cancelling and accepting the dialog closes it.
     ['cancel-button', 'action-button'].forEach(buttonType => {
-      MockInteractions.tap(testElement.$.clearAndReset);
-      assertTrue(testElement.$.confirmDeleteDialog.open);
+      MockInteractions.tap(testElement.$$('#resetSettingsButton'));
+      assertTrue(testElement.$.confirmResetSettings.open);
       const actionButtonList =
-          testElement.$.confirmDeleteDialog.getElementsByClassName(buttonType);
+          testElement.$.confirmResetSettings.getElementsByClassName(buttonType);
       assertEquals(1, actionButtonList.length);
       MockInteractions.tap(actionButtonList[0]);
-      assertFalse(testElement.$.confirmDeleteDialog.open);
+      assertFalse(testElement.$.confirmResetSettings.open);
     });
 
     // Accepting the dialog will make a call to setOriginPermissions.
@@ -277,6 +322,44 @@ suite('SiteDetails', function() {
       assertEquals(testElement.origin, args[0]);
       assertDeepEquals(testElement.getCategoryList_(), args[1]);
       assertEquals(settings.ContentSetting.DEFAULT, args[2]);
+    });
+  });
+
+  test('show confirmation dialog on clear storage', function() {
+    browserProxy.setPrefs(prefs);
+    loadTimeData.overrideValues({enableSiteSettings: true});
+    testElement = createSiteDetails('https://foo.com:443');
+
+    // Give |testElement.storedData_| a non-empty value to make the clear
+    // storage button appear. Also replace the website-usage-private-api element
+    // to prevent a call going back to the C++ upon confirming the dialog.
+    const parent = testElement.$.usageApi.parentNode;
+    assertTrue(parent != undefined);
+    testElement.$.usageApi.remove();
+    Polymer({
+      // Use a different mock name here to avoid an error when all tests are run
+      // together as there is no way to unregister a Polymer custom element.
+      is: 'mock1-website-usage-private-api',
+      fetchUsageTotal: function() {
+        testElement.storedData_ = '1 KB';
+      },
+      clearUsage: function(origin) {},
+    });
+    let api = document.createElement('mock1-website-usage-private-api');
+    testElement.$.usageApi = api;
+    Polymer.dom(parent).appendChild(api);
+    Polymer.dom.flush();
+
+    // Check both cancelling and accepting the dialog closes it.
+    ['cancel-button', 'action-button'].forEach(buttonType => {
+      MockInteractions.tap(testElement.$$('#usage paper-button'));
+      assertTrue(testElement.$.confirmClearStorage.open);
+      const actionButtonList =
+          testElement.$.confirmClearStorage.getElementsByClassName(buttonType);
+      assertEquals(1, actionButtonList.length);
+      testElement.storedData_ = '';
+      MockInteractions.tap(actionButtonList[0]);
+      assertFalse(testElement.$.confirmClearStorage.open);
     });
   });
 
