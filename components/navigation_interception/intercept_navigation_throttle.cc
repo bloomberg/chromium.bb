1acc6968e05dd52fc6b326d1d7a39f3a0ef50b7a
Charlie Harrison <csharrison@chromium.org>
2018-06-08 22:46:08 +0000

    Make the intercept navigation throttle async
    Design doc: https://bit.ly/2JgkZyc
    
    This CL changes the intercept navigation throttle to avoid blocking
    network requests from starting. Instead, policy decisions are computed
    asynchronously, and the load is canceled+ignored at the next
    NavigationThrottle callback.
    
    Note that in cases where the request should be ignored, this wastes
    some bytes. However, this is a pretty rare case. It looks like ~99% of
    the time we won't cancel the navigation (from
    Android.TabNavigationInterceptResult)
    
    Bug: 793053
    Change-Id: I9bf4eb29cf7ff92d31432eedb41cd446f738a492
    Reviewed-on: https://chromium-review.googlesource.com/815354
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565764}
    Former-commit-id: ffaf1e2ba66b521c4afe3af865a3f044ae1bff00

--- components/navigation_interception/intercept_navigation_throttle.cc
+++ components/navigation_interception/intercept_navigation_throttle.cc
@@ -4,31 +4,38 @@
 
 #include "components/navigation_interception/intercept_navigation_throttle.h"
 
+#include "base/bind.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "base/timer/elapsed_timer.h"
-#include "components/navigation_interception/navigation_params.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_handle.h"
-
-using content::BrowserThread;
+#include "url/gurl.h"
 
 namespace navigation_interception {
+
+const base::Feature InterceptNavigationThrottle::kAsyncCheck{
+    "AsyncNavigationIntercept", base::FEATURE_DISABLED_BY_DEFAULT};
 
 InterceptNavigationThrottle::InterceptNavigationThrottle(
     content::NavigationHandle* navigation_handle,
     CheckCallback should_ignore_callback)
     : content::NavigationThrottle(navigation_handle),
-      should_ignore_callback_(should_ignore_callback) {}
+      should_ignore_callback_(should_ignore_callback),
+      ui_task_runner_(base::ThreadTaskRunnerHandle::Get()),
+      weak_factory_(this) {}
 
-InterceptNavigationThrottle::~InterceptNavigationThrottle() {}
+InterceptNavigationThrottle::~InterceptNavigationThrottle() {
+  UMA_HISTOGRAM_BOOLEAN("Navigation.Intercept.Ignored", should_ignore_);
+}
 
 content::NavigationThrottle::ThrottleCheckResult
 InterceptNavigationThrottle::WillStartRequest() {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  DCHECK(!should_ignore_);
   base::ElapsedTimer timer;
 
-  auto result = CheckIfShouldIgnoreNavigation(false);
+  auto result = CheckIfShouldIgnoreNavigation(false /* is_redirect */);
   UMA_HISTOGRAM_COUNTS_10M("Navigation.Intercept.WillStart",
                            timer.Elapsed().InMicroseconds());
   return result;
@@ -36,8 +43,19 @@ InterceptNavigationThrottle::WillStartRequest() {
 
 content::NavigationThrottle::ThrottleCheckResult
 InterceptNavigationThrottle::WillRedirectRequest() {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  return CheckIfShouldIgnoreNavigation(true);
+  if (should_ignore_)
+    return content::NavigationThrottle::CANCEL_AND_IGNORE;
+  return CheckIfShouldIgnoreNavigation(true /* is_redirect */);
+}
+
+content::NavigationThrottle::ThrottleCheckResult
+InterceptNavigationThrottle::WillFailRequest() {
+  return WillFinish();
+}
+
+content::NavigationThrottle::ThrottleCheckResult
+InterceptNavigationThrottle::WillProcessResponse() {
+  return WillFinish();
 }
 
 const char* InterceptNavigationThrottle::GetNameForLogging() {
@@ -45,19 +63,69 @@ const char* InterceptNavigationThrottle::GetNameForLogging() {
 }
 
 content::NavigationThrottle::ThrottleCheckResult
+InterceptNavigationThrottle::WillFinish() {
+  DCHECK(!deferring_);
+  if (should_ignore_)
+    return content::NavigationThrottle::CANCEL_AND_IGNORE;
+
+  if (pending_checks_ > 0) {
+    deferring_ = true;
+    return content::NavigationThrottle::DEFER;
+  }
+
+  return content::NavigationThrottle::PROCEED;
+}
+
+content::NavigationThrottle::ThrottleCheckResult
 InterceptNavigationThrottle::CheckIfShouldIgnoreNavigation(bool is_redirect) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  NavigationParams navigation_params(
+  pending_checks_++;
+  if (ShouldCheckAsynchronously()) {
+    ui_task_runner_->PostTask(
+        FROM_HERE, base::BindOnce(&InterceptNavigationThrottle::RunCheck,
+                                  weak_factory_.GetWeakPtr(),
+                                  GetNavigationParams(is_redirect)));
+    return content::NavigationThrottle::PROCEED;
+  }
+  RunCheck(GetNavigationParams(is_redirect));
+  return should_ignore_ ? content::NavigationThrottle::CANCEL_AND_IGNORE
+                        : content::NavigationThrottle::PROCEED;
+}
+
+void InterceptNavigationThrottle::RunCheck(const NavigationParams& params) {
+  should_ignore_ |= should_ignore_callback_.Run(
+      navigation_handle()->GetWebContents(), params);
+  DCHECK_GT(pending_checks_, 0);
+  pending_checks_--;
+  if (!deferring_ || pending_checks_ > 0)
+    return;
+
+  if (should_ignore_) {
+    CancelDeferredNavigation(content::NavigationThrottle::CANCEL_AND_IGNORE);
+  } else {
+    Resume();
+  }
+}
+
+bool InterceptNavigationThrottle::ShouldCheckAsynchronously() const {
+  // Do not apply the async optimization for:
+  // - POST navigations, to ensure we aren't violating idempotency.
+  // - Subframe navigations, which aren't observed on Android, and should be
+  //   fast on other platforms.
+  // - non-http/s URLs, which are more likely to be intercepted.
+  return navigation_handle()->IsInMainFrame() &&
+         !navigation_handle()->IsPost() &&
+         navigation_handle()->GetURL().SchemeIsHTTPOrHTTPS() &&
+         base::FeatureList::IsEnabled(kAsyncCheck);
+}
+
+NavigationParams InterceptNavigationThrottle::GetNavigationParams(
+    bool is_redirect) const {
+  return NavigationParams(
       navigation_handle()->GetURL(), navigation_handle()->GetReferrer(),
       navigation_handle()->HasUserGesture(), navigation_handle()->IsPost(),
       navigation_handle()->GetPageTransition(), is_redirect,
       navigation_handle()->IsExternalProtocol(), true,
       navigation_handle()->GetBaseURLForDataURL());
-  bool should_ignore_navigation = should_ignore_callback_.Run(
-      navigation_handle()->GetWebContents(), navigation_params);
-  return should_ignore_navigation
-             ? content::NavigationThrottle::CANCEL_AND_IGNORE
-             : content::NavigationThrottle::PROCEED;
 }
 
 }  // namespace navigation_interception
--- components/navigation_interception/intercept_navigation_throttle.h
+++ components/navigation_interception/intercept_navigation_throttle.h
@@ -5,11 +5,13 @@
 #ifndef COMPONENTS_NAVIGATION_INTERCEPTION_INTERCEPT_NAVIGATION_THROTTLE_H_
 #define COMPONENTS_NAVIGATION_INTERCEPTION_INTERCEPT_NAVIGATION_THROTTLE_H_
 
-#include <string>
-
 #include "base/callback.h"
+#include "base/feature_list.h"
 #include "base/macros.h"
+#include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
+#include "base/single_thread_task_runner.h"
+#include "components/navigation_interception/navigation_params.h"
 #include "content/public/browser/navigation_throttle.h"
 
 namespace content {
@@ -25,9 +27,12 @@ class NavigationParams;
 // level navigations. This is a UI thread class.
 class InterceptNavigationThrottle : public content::NavigationThrottle {
  public:
-  typedef base::Callback<bool(content::WebContents* /* source */,
-                              const NavigationParams& /* navigation_params */)>
+  typedef base::RepeatingCallback<bool(
+      content::WebContents* /* source */,
+      const NavigationParams& /* navigation_params */)>
       CheckCallback;
+
+  static const base::Feature kAsyncCheck;
 
   InterceptNavigationThrottle(content::NavigationHandle* navigation_handle,
                               CheckCallback should_ignore_callback);
@@ -36,12 +41,42 @@ class InterceptNavigationThrottle : public content::NavigationThrottle {
   // content::NavigationThrottle implementation:
   ThrottleCheckResult WillStartRequest() override;
   ThrottleCheckResult WillRedirectRequest() override;
+  ThrottleCheckResult WillFailRequest() override;
+  ThrottleCheckResult WillProcessResponse() override;
   const char* GetNameForLogging() override;
 
  private:
-  ThrottleCheckResult CheckIfShouldIgnoreNavigation(bool is_redirect);
+  // To be called on either WillFailRequest or WillProcessResponse.
+  ThrottleCheckResult WillFinish();
 
+  ThrottleCheckResult CheckIfShouldIgnoreNavigation(bool is_redirect);
+  void RunCheck(const NavigationParams& params);
+
+  bool ShouldCheckAsynchronously() const;
+
+  // Constructs NavigationParams for this navigation.
+  NavigationParams GetNavigationParams(bool is_redirect) const;
+
+  // This callback should be called at the start of navigation and every
+  // redirect, until |should_ignore_| is true.
   CheckCallback should_ignore_callback_;
+
+  // Note that the CheckCallback currently has thread affinity on the Java side.
+  scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner_;
+
+  // How many outbound pending checks are running. Normally this will be either
+  // 0 or 1, but making this a bool makes too many assumptions about the nature
+  // of Chrome's task queues (e.g. we could be scheduled after the task which
+  // redirects the navigation).
+  int pending_checks_ = 0;
+
+  // Whether the navigation should be ignored. Updated at every redirect.
+  bool should_ignore_ = false;
+
+  // Whether the navigation is currently deferred.
+  bool deferring_ = false;
+
+  base::WeakPtrFactory<InterceptNavigationThrottle> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(InterceptNavigationThrottle);
 };
