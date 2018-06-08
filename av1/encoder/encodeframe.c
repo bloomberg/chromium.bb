wjdz99/aom

Simplify nested ifs in encode_rd_sb_row.
The logic is equivalent.

Change-Id: I8d32140b2b625cd161717d0b5729024a334a89de

av1/encoder/encodeframe.c
@@ -3542,16 +3542,15 @@ static void encode_rd_sb_row(AV1_COMP *cpi, ThreadData *td,
   av1_zero_left_context(xd);
 
   // Reset delta for every tile
-  if (cm->delta_q_present_flag)
-    if (mi_row == tile_info->mi_row_start) xd->prev_qindex = cm->base_qindex;
-  if (cm->delta_lf_present_flag) {
-    if (mi_row == tile_info->mi_row_start) {
+  if (mi_row == tile_info->mi_row_start) {
+    if (cm->delta_q_present_flag) xd->prev_qindex = cm->base_qindex;
+    if (cm->delta_lf_present_flag) {
       const int frame_lf_count =
           av1_num_planes(cm) > 1 ? FRAME_LF_COUNT : FRAME_LF_COUNT - 2;
       for (int lf_id = 0; lf_id < frame_lf_count; ++lf_id)
         xd->prev_delta_lf[lf_id] = 0;
+      xd->prev_delta_lf_from_base = 0;
     }
-    if (mi_row == tile_info->mi_row_start) xd->prev_delta_lf_from_base = 0;
   }
 
   // Code each SB in the row

bloomberg/chromium.bb

av1/encoder/encodeframe.c
@@ -3733,16 +3733,15 @@ static void encode_rd_sb_row(AV1_COMP *cpi, ThreadData *td,
   av1_zero_left_context(xd);
 
   // Reset delta for every tile
-  if (cm->delta_q_present_flag)
-    if (mi_row == tile_info->mi_row_start) xd->current_qindex = cm->base_qindex;
-  if (cm->delta_lf_present_flag) {
-    if (mi_row == tile_info->mi_row_start) {
+  if (mi_row == tile_info->mi_row_start) {
+    if (cm->delta_q_present_flag) xd->current_qindex = cm->base_qindex;
+    if (cm->delta_lf_present_flag) {
       const int frame_lf_count =
           av1_num_planes(cm) > 1 ? FRAME_LF_COUNT : FRAME_LF_COUNT - 2;
       for (int lf_id = 0; lf_id < frame_lf_count; ++lf_id)
         xd->delta_lf[lf_id] = 0;
+      xd->delta_lf_from_base = 0;
     }
-    if (mi_row == tile_info->mi_row_start) xd->delta_lf_from_base = 0;
   }
 
   // Code each SB in the row