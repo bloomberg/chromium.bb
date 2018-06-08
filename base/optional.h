e5449af891ab430f51b2f310bb8a1884b2c91484
jdoerrie <jdoerrie@chromium.org>
2018-06-08 19:52:09 +0000

    [base] Remove CHECKs in base::Optional operator* and ->
    This change partially reverts r550197 which introduced CHECKs to
    base::Optional's operator*, operator-> and value(). This is done to
    reduce binary size and to be more standard's compliant, as std::optional
    also doesn't perform checks for operator* and operator->. Lastly, the
    CHECKs in value() are kept, as here CHECKing is desired, and also
    matches std::optional's behaviour.
    
    Bug: 832678
    Change-Id: I467c7d7623c2880ee761b8a58a74738c09e0ba2a
    Reviewed-on: https://chromium-review.googlesource.com/1093314
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565720}
    Former-commit-id: a000101f58501dac2d090ba09f61cb292f7c9f6b

--- base/optional.h
+++ base/optional.h
@@ -575,32 +575,32 @@ class OPTIONAL_DECLSPEC_EMPTY_BASES Optional
   }
 
   constexpr const T* operator->() const {
-    CHECK(storage_.is_populated_);
+    DCHECK(storage_.is_populated_);
     return &storage_.value_;
   }
 
   constexpr T* operator->() {
-    CHECK(storage_.is_populated_);
+    DCHECK(storage_.is_populated_);
     return &storage_.value_;
   }
 
   constexpr const T& operator*() const & {
-    CHECK(storage_.is_populated_);
+    DCHECK(storage_.is_populated_);
     return storage_.value_;
   }
 
   constexpr T& operator*() & {
-    CHECK(storage_.is_populated_);
+    DCHECK(storage_.is_populated_);
     return storage_.value_;
   }
 
   constexpr const T&& operator*() const && {
-    CHECK(storage_.is_populated_);
+    DCHECK(storage_.is_populated_);
     return std::move(storage_.value_);
   }
 
   constexpr T&& operator*() && {
-    CHECK(storage_.is_populated_);
+    DCHECK(storage_.is_populated_);
     return std::move(storage_.value_);
   }
 
