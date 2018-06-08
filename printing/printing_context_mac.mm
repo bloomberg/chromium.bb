228e9faccb6f2233f3241bd0ba1112fab9cdfdaa
Sidney San Martín <sdy@chromium.org>
2018-06-08 18:08:19 +0000

    Wrap showing the system print dialog in a CATransaction completion block to fix a crash.
    Bug: 849538
    Change-Id: Ib04c020dbc50aae398255654f5404fd8904a5326
    Reviewed-on: https://chromium-review.googlesource.com/1092232
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Sidney San Martín <sdy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#565684}
    Former-commit-id: a1bec15b552ea793cc090cf67ac7515273f05573

--- printing/printing_context_mac.mm
+++ printing/printing_context_mac.mm
@@ -5,6 +5,7 @@
 #include "printing/printing_context_mac.h"
 
 #import <AppKit/AppKit.h>
+#import <QuartzCore/QuartzCore.h>
 
 #import <iomanip>
 #import <numeric>
@@ -121,15 +122,23 @@ void PrintingContextMac::AskUserForSettings(int max_pages,
 
   // TODO(stuartmorgan): We really want a tab sheet here, not a modal window.
   // Will require restructuring the PrintingContext API to use a callback.
-  NSInteger selection = [panel runModalWithPrintInfo:printInfo];
-  if (selection == NSOKButton) {
-    print_info_.reset([[panel printInfo] retain]);
-    settings_.set_ranges(GetPageRangesFromPrintInfo());
-    InitPrintSettingsFromPrintInfo();
-    std::move(callback).Run(OK);
-  } else {
-    std::move(callback).Run(CANCEL);
-  }
+
+  // This function may be called in the middle of a CATransaction, where
+  // running a modal panel is forbidden. That situation isn't ideal, but from
+  // this code's POV the right answer is to defer running the panel until after
+  // the current transaction. See https://crbug.com/849538.
+  __block auto block_callback = std::move(callback);
+  [CATransaction setCompletionBlock:^{
+    NSInteger selection = [panel runModalWithPrintInfo:printInfo];
+    if (selection == NSOKButton) {
+      print_info_.reset([[panel printInfo] retain]);
+      settings_.set_ranges(GetPageRangesFromPrintInfo());
+      InitPrintSettingsFromPrintInfo();
+      std::move(block_callback).Run(OK);
+    } else {
+      std::move(block_callback).Run(CANCEL);
+    }
+  }];
 }
 
 gfx::Size PrintingContextMac::GetPdfPaperSizeDeviceUnits() {
