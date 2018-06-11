5f669fd657ba92a23428c324369f617cb421283b
Dave Tapuska <dtapuska@chromium.org>
2018-06-11 22:21:55 +0000

    Avoid truncation error by using the appropriate value.
    The ctor for v8::Integer takes 32 bit values so avoid a truncation warning
    by using the correct variable.
    
    BUG=588506
    
    Change-Id: I8a521da01be5a18d866ae6ff67c701ac0a5612cd
    Reviewed-on: https://chromium-review.googlesource.com/1093307
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566172}
    Former-commit-id: bcde1f6ac692c7cf5c58c80041fed1c27cac5a15

--- third_party/blink/renderer/platform/bindings/to_v8.h
+++ third_party/blink/renderer/platform/bindings/to_v8.h
@@ -81,7 +81,7 @@ inline v8::Local<v8::Value> ToV8SignedIntegerInternal<8>(int64_t value,
                                                          v8::Isolate* isolate) {
   int32_t value_in32_bit = static_cast<int32_t>(value);
   if (value_in32_bit == value)
-    return v8::Integer::New(isolate, value);
+    return v8::Integer::New(isolate, value_in32_bit);
   // V8 doesn't have a 64-bit integer implementation.
   return v8::Number::New(isolate, value);
 }
@@ -103,7 +103,7 @@ inline v8::Local<v8::Value> ToV8UnsignedIntegerInternal<8>(
     v8::Isolate* isolate) {
   uint32_t value_in32_bit = static_cast<uint32_t>(value);
   if (value_in32_bit == value)
-    return v8::Integer::NewFromUnsigned(isolate, value);
+    return v8::Integer::NewFromUnsigned(isolate, value_in32_bit);
   // V8 doesn't have a 64-bit integer implementation.
   return v8::Number::New(isolate, value);
 }
