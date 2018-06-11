7fb2f1a53eabdb2a2631a433716ba608730e2773
Lann Martin <lannm@chromium.org>
2018-06-11 14:42:10 -0600

    Make FindOverlays output have deterministic ordering
    This will make it easier to debug certain kinds of failures (see BUG).
    
    BUG=chromium:851059
    TEST=portage_util_unittests
    
    Change-Id: Ic23f3cd6b4bbaebede2e3118dd2760fb42c45e76
    Reviewed-on: https://chromium-review.googlesource.com/1096119
    Commit-Ready: Lann Martin <lannm@chromium.org>
    Tested-by: Lann Martin <lannm@chromium.org>
    Reviewed-by: Jason Clinton <jclinton@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- lib/portage_util.py
+++ lib/portage_util.py
@@ -117,7 +117,7 @@ def _ListOverlays(board=None, buildroot=constants.SOURCE_ROOT):
   overlays = {}
   for path in paths:
     path = os.path.join(buildroot, path, '*')
-    for overlay in glob.glob(path):
+    for overlay in sorted(glob.glob(path)):
       name = GetOverlayName(overlay)
       if name is None:
         continue
@@ -140,7 +140,7 @@ def _ListOverlays(board=None, buildroot=constants.SOURCE_ROOT):
 
   # Easy enough -- dump them all.
   if board is None:
-    return [x['path'] for x in overlays.values()]
+    return sorted([x['path'] for x in overlays.values()])
 
   # Build up the list of repos we need.
   ret = []
@@ -176,7 +176,7 @@ def _ListOverlays(board=None, buildroot=constants.SOURCE_ROOT):
   _AddRepo('chromeos', optional=True)
   path = os.path.join(buildroot, 'src', 'private-overlays',
                       'chromeos-*-overlay')
-  ret += glob.glob(path)
+  ret += sorted(glob.glob(path))
 
   # Locate the board repo by name.
   # Load the public & private versions if available.
