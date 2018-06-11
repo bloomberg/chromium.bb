d3786343df3bf69e45d3697b20811533301b351a
Edward Jung <edwardjung@chromium.org>
2018-06-11 18:21:46 +0000

    Add new mode for the offline dino easter egg
    Bug: 846473
    Change-Id: Iaf7e5d1153b7a78be5b2c2396d8baa9d34d5a306
    Reviewed-on: https://chromium-review.googlesource.com/1085908
    Commit-Queue: Edward Jung <edwardjung@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566064}
    Former-commit-id: 693b9fcac0ff52f6653c0a27815d39fd077856a4

--- components/neterror/resources/offline.js
+++ components/neterror/resources/offline.js
@@ -49,6 +49,7 @@ function Runner(outerContainerId, opt_config) {
   this.inverted = false;
   this.invertTimer = 0;
   this.resizeTimerId_ = null;
+  this.bdayFlashTimer = null;
 
   this.playCount = 0;
 
@@ -104,6 +105,9 @@ Runner.config = {
   ACCELERATION: 0.001,
   BG_CLOUD_SPEED: 0.2,
   BOTTOM_PAD: 10,
+  BOTTOM_PAD_BDAY: 26,
+  BDAY_FLASH_DURATION: 1000,
+  BDAY_Y_POS_ADJUST: 16,
   CLEAR_TIME: 3000,
   CLOUD_FREQUENCY: 0.5,
   GAMEOVER_CLEAR_TIME: 750,
@@ -160,6 +164,7 @@ Runner.classes = {
  */
 Runner.spriteDefinition = {
   LDPI: {
+    BALLOON: {x: 417, y: 29},
     CACTUS_LARGE: {x: 332, y: 2},
     CACTUS_SMALL: {x: 228, y: 2},
     CLOUD: {x: 86, y: 2},
@@ -169,9 +174,12 @@ Runner.spriteDefinition = {
     RESTART: {x: 2, y: 2},
     TEXT_SPRITE: {x: 655, y: 2},
     TREX: {x: 848, y: 2},
+    TREX_BDAY: {x: 0, y: 0},
+    SNACK: {x: 384, y: 22},
     STAR: {x: 645, y: 2}
   },
   HDPI: {
+    BALLOON: {x: 834, y: 58},
     CACTUS_LARGE: {x: 652, y: 2},
     CACTUS_SMALL: {x: 446, y: 2},
     CLOUD: {x: 166, y: 2},
@@ -181,6 +189,8 @@ Runner.spriteDefinition = {
     RESTART: {x: 2, y: 2},
     TEXT_SPRITE: {x: 1294, y: 2},
     TREX: {x: 1678, y: 2},
+    TREX_BDAY: {x: 0, y: 0},
+    SNACK: {x: 768, y: 44},
     STAR: {x: 1276, y: 2}
   }
 };
@@ -287,9 +297,13 @@ Runner.prototype = {
   loadImages: function() {
     if (IS_HIDPI) {
       Runner.imageSprite = document.getElementById('offline-resources-2x');
+      Runner.bdayImageSprite =
+          document.getElementById('offline-resources-bday-2x');
       this.spriteDef = Runner.spriteDefinition.HDPI;
     } else {
       Runner.imageSprite = document.getElementById('offline-resources-1x');
+      Runner.bdayImageSprite =
+          document.getElementById('offline-resources-bday-1x');
       this.spriteDef = Runner.spriteDefinition.LDPI;
     }
 
@@ -525,6 +539,20 @@ Runner.prototype = {
 
     var now = getTimeStamp();
     var deltaTime = now - (this.time || now);
+
+    // Flashing.
+    if (this.bdayFlashTimer) {
+      if (this.bdayFlashTimer <= 0) {
+        this.bdayFlashTimer = null;
+        this.tRex.setFlashing(false);
+        this.tRex.enableBdayMode(this.spriteDef.TREX_BDAY);
+      } else {
+        this.bdayFlashTimer -= deltaTime;
+        this.tRex.update(deltaTime);
+        deltaTime = 0;
+      }
+    }
+
     this.time = now;
 
     if (this.playing) {
@@ -554,6 +582,15 @@ Runner.prototype = {
       // Check for collisions.
       var collision = hasObstacles &&
           checkForCollision(this.horizon.obstacles[0], this.tRex);
+
+      // Ate snack.
+      if (Runner.isBdayModeEnabled() && collision &&
+          this.horizon.obstacles[0].typeConfig.type == 'SNACK') {
+        this.horizon.enableBdayMode();
+        this.tRex.setFlashing(true);
+        collision = false;
+        this.bdayFlashTimer = this.config.BDAY_FLASH_DURATION;
+      }
 
       if (!collision) {
         this.distanceRan += this.currentSpeed * deltaTime / this.msPerFrame;
@@ -957,6 +994,15 @@ Runner.updateCanvasScaling = function(canvas, opt_width, opt_height) {
 
 
 /**
+ * Whether the bday mode is enabled.
+ * @return {boolean}
+ */
+Runner.isBdayModeEnabled = function() {
+  return loadTimeData && loadTimeData.valueExists('bdayMode');
+}
+
+
+/**
  * Get random number.
  * @param {number} min
  * @param {number} max
@@ -1142,7 +1188,7 @@ function checkForCollision(obstacle, tRex, opt_canvasCtx) {
       tRex.xPos + 1,
       tRex.yPos + 1,
       tRex.config.WIDTH - 2,
-      tRex.config.HEIGHT - 2);
+      (tRex.bdayModeActive ? tRex.config.HEIGHT_BDAY : tRex.config.HEIGHT) - 2);
 
   var obstacleBox = new CollisionBox(
       obstacle.xPos + 1,
@@ -1170,6 +1216,10 @@ function checkForCollision(obstacle, tRex, opt_canvasCtx) {
         var adjObstacleBox =
             createAdjustedCollisionBox(collisionBoxes[i], obstacleBox);
         var crashed = boxCompare(adjTrexBox, adjObstacleBox);
+
+        if (tRex.bdayModeActive) {
+          adjTrexBox.y += Runner.config.BDAY_Y_POS_ADJUST;
+        }
 
         // Draw boxes for debug.
         if (opt_canvasCtx) {
@@ -1287,6 +1337,8 @@ function Obstacle(canvasCtx, type, spriteImgPos, dimensions,
   this.collisionBoxes = [];
   this.gap = 0;
   this.speedOffset = 0;
+  this.imageSprite = this.typeConfig.type == 'SNACK' ? Runner.bdayImageSprite :
+        Runner.imageSprite;
 
   // For animated obstacles.
   this.currentFrame = 0;
@@ -1378,7 +1430,7 @@ Obstacle.prototype = {
       sourceX += sourceWidth * this.currentFrame;
     }
 
-    this.canvasCtx.drawImage(Runner.imageSprite,
+    this.canvasCtx.drawImage(this.imageSprite,
       sourceX, this.spritePos.y,
       sourceWidth * this.size, sourceHeight,
       this.xPos, this.yPos,
@@ -1508,6 +1560,18 @@ Obstacle.types = [
     numFrames: 2,
     frameRate: 1000/6,
     speedOffset: .8
+  },
+  {
+    type: 'SNACK',
+    width: 33,
+    height: 42,
+    yPos: 85,
+    multipleSpeed: 999,
+    minGap: 999,
+    minSpeed: 0,
+    collisionBoxes: [
+      new CollisionBox(0, 0, 40, 40)
+    ]
   }
 ];
 
@@ -1522,6 +1586,7 @@ Obstacle.types = [
 function Trex(canvas, spritePos) {
   this.canvas = canvas;
   this.canvasCtx = canvas.getContext('2d');
+  this.imageSprite = Runner.imageSprite;
   this.spritePos = spritePos;
   this.xPos = 0;
   this.yPos = 0;
@@ -1545,6 +1610,8 @@ function Trex(canvas, spritePos) {
   this.speedDrop = false;
   this.jumpCount = 0;
   this.jumpspotX = 0;
+  this.bdayModeActive = false;
+  this.flashing = false;
 
   this.init();
 };
@@ -1556,8 +1623,11 @@ function Trex(canvas, spritePos) {
  */
 Trex.config = {
   DROP_VELOCITY: -5,
+  FLASH_OFF: 175,
+  FLASH_ON: 100,
   GRAVITY: 0.6,
   HEIGHT: 47,
+  HEIGHT_BDAY: 63,
   HEIGHT_DUCK: 25,
   INIITAL_JUMP_VELOCITY: -10,
   INTRO_DURATION: 1500,
@@ -1631,7 +1701,7 @@ Trex.animFrames = {
     msPerFrame: 1000 / 60
   },
   DUCKING: {
-    frames: [262, 321],
+    frames: [264, 323],
     msPerFrame: 1000 / 8
   }
 };
@@ -1650,6 +1720,25 @@ Trex.prototype = {
 
     this.draw(0, 0);
     this.update(0, Trex.status.WAITING);
+  },
+
+  /**
+   * @param {Object} spritePos New positioning within image sprite.
+   */
+  enableBdayMode: function(spritePos) {
+    this.bdayModeActive = true;
+    this.spritePos = spritePos;
+    this.imageSprite = Runner.bdayImageSprite;
+    this.groundYPos = Runner.defaultDimensions.HEIGHT - this.config.HEIGHT -
+        Runner.config.BOTTOM_PAD_BDAY;
+    this.yPos -= Runner.config.BDAY_Y_POS_ADJUST;
+  },
+
+  /**
+   * @param {boolean} status Whether dino is flashing.
+   */
+  setFlashing: function(status) {
+    this.flashing = status;
   },
 
   /**
@@ -1695,7 +1784,7 @@ Trex.prototype = {
     }
 
     // Update the frame position.
-    if (this.timer >= this.msPerFrame) {
+    if (!this.flashing && this.timer >= this.msPerFrame) {
       this.currentFrame = this.currentFrame ==
           this.currentAnimFrames.length - 1 ? 0 : this.currentFrame + 1;
       this.timer = 0;
@@ -1718,7 +1807,10 @@ Trex.prototype = {
     var sourceY = y;
     var sourceWidth = this.ducking && this.status != Trex.status.CRASHED ?
         this.config.WIDTH_DUCK : this.config.WIDTH;
-    var sourceHeight = this.config.HEIGHT;
+    var sourceHeight = this.bdayModeActive ? this.config.HEIGHT_BDAY :
+        this.config.HEIGHT;
+    var outputHeight = sourceHeight;
+
 
     if (IS_HIDPI) {
       sourceX *= 2;
@@ -1731,23 +1823,33 @@ Trex.prototype = {
     sourceX += this.spritePos.x;
     sourceY += this.spritePos.y;
 
+    // Flashing.
+    if (this.flashing) {
+      if (this.timer < this.config.FLASH_ON) {
+        this.canvasCtx.globalAlpha = 0.5;
+      } else if (this.timer > this.config.FLASH_OFF) {
+        this.timer = 0;
+      }
+    }
+
     // Ducking.
     if (this.ducking && this.status != Trex.status.CRASHED) {
-      this.canvasCtx.drawImage(Runner.imageSprite, sourceX, sourceY,
+      this.canvasCtx.drawImage(this.imageSprite, sourceX, sourceY,
           sourceWidth, sourceHeight,
           this.xPos, this.yPos,
-          this.config.WIDTH_DUCK, this.config.HEIGHT);
+          this.config.WIDTH_DUCK, outputHeight);
     } else {
       // Crashed whilst ducking. Trex is standing up so needs adjustment.
       if (this.ducking && this.status == Trex.status.CRASHED) {
         this.xPos++;
       }
       // Standing / running
-      this.canvasCtx.drawImage(Runner.imageSprite, sourceX, sourceY,
+      this.canvasCtx.drawImage(this.imageSprite, sourceX, sourceY,
           sourceWidth, sourceHeight,
           this.xPos, this.yPos,
-          this.config.WIDTH, this.config.HEIGHT);
+          this.config.WIDTH, outputHeight);
     }
+    this.canvasCtx.globalAlpha = 1;
   },
 
   /**
@@ -1835,8 +1937,6 @@ Trex.prototype = {
       this.reset();
       this.jumpCount++;
     }
-
-    this.update(deltaTime);
   },
 
   /**
@@ -2144,9 +2244,10 @@ DistanceMeter.prototype = {
  * Similar to an obstacle object but without collision boxes.
  * @param {HTMLCanvasElement} canvas Canvas element.
  * @param {Object} spritePos Position of image in sprite.
+ * @param {boolean} isBalloon Switch Cloud to balloon.
  * @param {number} containerWidth
  */
-function Cloud(canvas, spritePos, containerWidth) {
+function Cloud(canvas, spritePos, containerWidth, isBalloon) {
   this.canvas = canvas;
   this.canvasCtx = this.canvas.getContext('2d');
   this.spritePos = spritePos;
@@ -2156,6 +2257,8 @@ function Cloud(canvas, spritePos, containerWidth) {
   this.remove = false;
   this.cloudGap = getRandomNum(Cloud.config.MIN_CLOUD_GAP,
       Cloud.config.MAX_CLOUD_GAP);
+  this.isBalloon = isBalloon;
+  this.imageSprite = isBalloon ? Runner.bdayImageSprite : Runner.imageSprite;
 
   this.init();
 };
@@ -2167,11 +2270,13 @@ function Cloud(canvas, spritePos, containerWidth) {
  */
 Cloud.config = {
   HEIGHT: 14,
+  HEIGHT_BALLOON: 34,
   MAX_CLOUD_GAP: 400,
   MAX_SKY_LEVEL: 30,
   MIN_CLOUD_GAP: 100,
   MIN_SKY_LEVEL: 71,
-  WIDTH: 46
+  WIDTH: 46,
+  WIDTH_BALLOON: 16
 };
 
 
@@ -2190,19 +2295,22 @@ Cloud.prototype = {
    */
   draw: function() {
     this.canvasCtx.save();
-    var sourceWidth = Cloud.config.WIDTH;
-    var sourceHeight = Cloud.config.HEIGHT;
-
+    var sourceWidth = this.isBalloon ? Cloud.config.WIDTH_BALLOON :
+        Cloud.config.WIDTH;
+    var sourceHeight = this.isBalloon ? Cloud.config.HEIGHT_BALLOON :
+        Cloud.config.HEIGHT;
+    var outputWidth = sourceWidth;
+    var outputHeight = sourceHeight;
     if (IS_HIDPI) {
       sourceWidth = sourceWidth * 2;
       sourceHeight = sourceHeight * 2;
     }
 
-    this.canvasCtx.drawImage(Runner.imageSprite, this.spritePos.x,
+    this.canvasCtx.drawImage(this.imageSprite, this.spritePos.x,
         this.spritePos.y,
         sourceWidth, sourceHeight,
         this.xPos, this.yPos,
-        Cloud.config.WIDTH, Cloud.config.HEIGHT);
+        outputWidth, outputHeight);
 
     this.canvasCtx.restore();
   },
@@ -2540,6 +2648,7 @@ function Horizon(canvas, spritePos, dimensions, gapCoefficient) {
   this.cloudFrequency = this.config.CLOUD_FREQUENCY;
   this.spritePos = spritePos;
   this.nightMode = null;
+  this.bdayModeActive = false;
 
   // Cloud
   this.clouds = [];
@@ -2573,6 +2682,14 @@ Horizon.prototype = {
     this.horizonLine = new HorizonLine(this.canvas, this.spritePos.HORIZON);
     this.nightMode = new NightMode(this.canvas, this.spritePos.MOON,
         this.dimensions.WIDTH);
+  },
+
+  /**
+   * Enable additional items.
+   */
+  enableBdayMode: function() {
+    this.bdayModeActive = true;
+    this.removeFirstObstacle();
   },
 
   /**
@@ -2671,7 +2788,10 @@ Horizon.prototype = {
    * @param {number} currentSpeed
    */
   addNewObstacle: function(currentSpeed) {
-    var obstacleTypeIndex = getRandomNum(0, Obstacle.types.length - 1);
+    var obstacleCount = Runner.isBdayModeEnabled() &&
+        !this.bdayModeActive ? Obstacle.types.length - 1 :
+        Obstacle.types.length - 2;
+    var obstacleTypeIndex = getRandomNum(0, obstacleCount);
     var obstacleType = Obstacle.types[obstacleTypeIndex];
 
     // Check for multiples of the same type of obstacle.
@@ -2733,8 +2853,10 @@ Horizon.prototype = {
    * Add a new cloud to the horizon.
    */
   addCloud: function() {
-    this.clouds.push(new Cloud(this.canvas, this.spritePos.CLOUD,
-        this.dimensions.WIDTH));
+    var cloudType = this.bdayModeActive && getRandomNum(0, 1) > 0 ?
+        this.spritePos.BALLOON : this.spritePos.CLOUD;
+    this.clouds.push(new Cloud(this.canvas, cloudType,
+        this.dimensions.WIDTH, cloudType == this.spritePos.BALLOON));
   }
 };
 })();
