83a96d4c2623fc6951acf5b8b802178a1c527c9c
Charlie Ma <charliema@chromium.org>
2018-06-11 23:51:32 +0000

    Fix recent tabs missing bug
    Recent tabs are cleared upon app exit since we are using InMemoryTabRestoreService in Chrome
    for Android, changed it to PersistentTabRestoreService like other platform, meanwhile reduce max tab entries
    to 5, on par with the RecentTabsManager.RECENTLY_CLOSED_MAX_TAB_COUNT.
    
    Bug: 169083
    Change-Id: I77d77174ba3e459f2df3791325664b4ee8850c5b
    Reviewed-on: https://chromium-review.googlesource.com/1089353
    Commit-Queue: Charlie Ma <charliema@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566208}
    Former-commit-id: 883e0286497f47292e3cd33d5525c0ceceed3da3

--- chrome/browser/sessions/tab_restore_service_factory.cc
+++ chrome/browser/sessions/tab_restore_service_factory.cc
@@ -6,16 +6,10 @@
 
 #include <utility>
 
-#include "build/build_config.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sessions/chrome_tab_restore_service_client.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
-
-#if defined(OS_ANDROID)
-#include "components/sessions/core/in_memory_tab_restore_service.h"
-#else
 #include "components/sessions/core/persistent_tab_restore_service.h"
-#endif
 
 // static
 sessions::TabRestoreService* TabRestoreServiceFactory::GetForProfile(
@@ -61,9 +55,5 @@ KeyedService* TabRestoreServiceFactory::BuildServiceInstanceFor(
   std::unique_ptr<sessions::TabRestoreServiceClient> client(
       new ChromeTabRestoreServiceClient(profile));
 
-#if defined(OS_ANDROID)
-  return new sessions::InMemoryTabRestoreService(std::move(client), nullptr);
-#else
   return new sessions::PersistentTabRestoreService(std::move(client), nullptr);
-#endif
 }
