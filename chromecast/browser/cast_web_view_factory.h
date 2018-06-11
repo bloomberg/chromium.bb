e4e85522968c724c068a5ccf4b732c0620f47d23
Albert Chaulk <achaulk@google.com>
2018-06-11 21:46:25 +0000

    [Chromecast] Partial implementation of chrome.tabs and chrome.windows
    This is focused on providing an implementation of most of the APIs, but not
    handling any events in the APIs, which will be in a followup if needed.
    
    All webviews are treated as tabs in one window, with arbitrarily assigned
    IDs by CastWebViewManager.
    
    Bug: b/78577097
    Test: local build
    Change-Id: Iacb457e00e38620c2087e5e0e26d5f01023f1328
    Reviewed-on: https://chromium-review.googlesource.com/1047794
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Albert Chaulk <achaulk@google.com>
    Cr-Commit-Position: refs/heads/master@{#566158}
    Former-commit-id: 7f637c7c0191606f79cb13bf10389f3520878531

--- chromecast/browser/cast_web_view_factory.h
+++ chromecast/browser/cast_web_view_factory.h
@@ -6,7 +6,9 @@
 #define CHROMECAST_BROWSER_CAST_WEB_VIEW_FACTORY_H_
 
 #include <memory>
+#include <vector>
 
+#include "base/callback_forward.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "chromecast/browser/cast_web_view.h"
@@ -25,10 +27,15 @@ namespace chromecast {
 
 class CastWebContentsManager;
 
-class CastWebViewFactory {
+struct ActiveWebview {
+  CastWebView* web_view;
+  int id;
+};
+
+class CastWebViewFactory : public CastWebView::Observer {
  public:
   explicit CastWebViewFactory(content::BrowserContext* browser_context);
-  virtual ~CastWebViewFactory();
+  ~CastWebViewFactory() override;
 
   virtual std::unique_ptr<CastWebView> CreateWebView(
       const CastWebView::CreateParams& params,
@@ -37,10 +44,20 @@ class CastWebViewFactory {
       const extensions::Extension* extension,
       const GURL& initial_url);
 
+  const std::vector<ActiveWebview>& active_webviews() const {
+    return active_webviews_;
+  }
   content::BrowserContext* browser_context() const { return browser_context_; }
 
- private:
+ protected:
+  // CastWebView::Observer implementation:
+  void OnPageDestroyed(CastWebView* web_view) override;
+
   content::BrowserContext* const browser_context_;
+  base::RepeatingCallback<void(CastWebView*, int)> register_callback_;
+
+  std::vector<ActiveWebview> active_webviews_;
+  int next_id_ = 1;
 
   DISALLOW_COPY_AND_ASSIGN(CastWebViewFactory);
 };
