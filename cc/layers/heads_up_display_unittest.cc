d2014231466c415ce8e26d6f9da0d1ff281f8b7f
danakj <danakj@chromium.org>
2018-06-12 20:57:28 +0000

    cc: Remove virtual from Layer::DrawsContent().
    It was virtual just for tests, but tests can simply call SetIsDrawble()
    to control the value of DrawsContent on a Layer.
    
    R=weiliangc@chromium.org
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I195ee17cbcc047d2baa288259f0ebcda0aed13a1
    Reviewed-on: https://chromium-review.googlesource.com/1097544
    Reviewed-by: weiliangc <weiliangc@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566565}
    Former-commit-id: 762a25542878ed74c5b59c47a1c1b67b77eb8510

--- cc/layers/heads_up_display_unittest.cc
+++ cc/layers/heads_up_display_unittest.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include "cc/layers/heads_up_display_layer.h"
+
 #include "cc/layers/layer.h"
 #include "cc/test/layer_tree_test.h"
 #include "cc/trees/layer_tree_host.h"
@@ -18,26 +19,11 @@ class HeadsUpDisplayTest : public LayerTreeTest {
   }
 };
 
-class DrawsContentLayer : public Layer {
- public:
-  static scoped_refptr<DrawsContentLayer> Create() {
-    return base::WrapRefCounted(new DrawsContentLayer());
-  }
-  bool DrawsContent() const override { return true; }
-
- private:
-  DrawsContentLayer() = default;
-  ~DrawsContentLayer() override = default;
-};
-
 class HudWithRootLayerChange : public HeadsUpDisplayTest {
  public:
-  HudWithRootLayerChange()
-      : root_layer1_(DrawsContentLayer::Create()),
-        root_layer2_(DrawsContentLayer::Create()),
-        num_commits_(0) {}
-
   void BeginTest() override {
+    root_layer1_ = Layer::Create();
+    root_layer2_ = Layer::Create();
     root_layer1_->SetBounds(gfx::Size(30, 30));
     root_layer2_->SetBounds(gfx::Size(30, 30));
 
@@ -76,7 +62,7 @@ class HudWithRootLayerChange : public HeadsUpDisplayTest {
         break;
       case 6:
         EXPECT_EQ(root_layer2_.get(), layer_tree_host()->hud_layer()->parent());
-        // Change directly back to the last root layer/
+        // Change directly back to the last root layer.
         layer_tree_host()->SetRootLayer(root_layer1_);
         break;
       case 7:
@@ -89,12 +75,12 @@ class HudWithRootLayerChange : public HeadsUpDisplayTest {
   void AfterTest() override {}
 
  private:
-  scoped_refptr<DrawsContentLayer> root_layer1_;
-  scoped_refptr<DrawsContentLayer> root_layer2_;
-  int num_commits_;
+  scoped_refptr<Layer> root_layer1_;
+  scoped_refptr<Layer> root_layer2_;
+  int num_commits_ = 0;
 };
 
-MULTI_THREAD_TEST_F(HudWithRootLayerChange);
+SINGLE_AND_MULTI_THREAD_TEST_F(HudWithRootLayerChange);
 
 }  // namespace
 }  // namespace cc
