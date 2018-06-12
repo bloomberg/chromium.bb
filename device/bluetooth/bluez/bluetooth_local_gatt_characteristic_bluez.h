0eca1e906b112af8b765b1ada65d13e49c03df78
Qiyu Hu <qiyuh@google.com>
2018-06-12 22:10:54 +0000

    arc: bluetooth: Enforce permissions on GATT server
    Convert properties and permissions from android to bluez.
    Support permissions in gatt characteristics.
    A relevant patch is crrev.com/c/1053126
    
    BUG=b:78593133
    CQ-DEPEND=CL:1079668
    TEST=Pass CtsVerifier test on gatt server permissions
    
    Change-Id: Ia4a5f63bebd8a52d24511ddbc93a72870ae44b16
    Reviewed-on: https://chromium-review.googlesource.com/1037196
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Reviewed-by: Miao-chen Chou <mcchou@chromium.org>
    Commit-Queue: Qiyu Hu <qiyuh@google.com>
    Cr-Commit-Position: refs/heads/master@{#566598}
    Former-commit-id: 1c4601f1b7d719a8a5574756156b692419d26743

--- device/bluetooth/bluez/bluetooth_local_gatt_characteristic_bluez.h
+++ device/bluetooth/bluez/bluetooth_local_gatt_characteristic_bluez.h
@@ -30,6 +30,7 @@ class BluetoothLocalGattCharacteristicBlueZ
   BluetoothLocalGattCharacteristicBlueZ(
       const device::BluetoothUUID& uuid,
       Properties properties,
+      Permissions permissions,
       BluetoothLocalGattServiceBlueZ* service);
   ~BluetoothLocalGattCharacteristicBlueZ() override;
 
@@ -61,6 +62,9 @@ class BluetoothLocalGattCharacteristicBlueZ
 
   // Properties of this characteristic.
   Properties properties_;
+
+  // Permissions of this characteristic.
+  Permissions permissions_;
 
   // Service that contains this characteristic.
   BluetoothLocalGattServiceBlueZ* service_;
