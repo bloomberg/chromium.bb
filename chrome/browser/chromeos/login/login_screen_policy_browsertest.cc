1395a3546c3f7d88ca07b14bf8b487f8a648fbcf
John Budorick <jbudorick@chromium.org>
2018-06-12 23:47:38 +0000

    Revert "Enable guest/offline signin links in views login network error screen."
    This reverts commit 5efeb7dc9423e6013ccf24747853de6788ac1693 [formerly b048769fa41f8f2c0c04f5632d307852318e808b].
    
    Reason for revert: chromium.memory cros msan
    
    Original change's description:
    > Enable guest/offline signin links in views login network error screen.
    > 
    > This CL also disables the LoginScreenLocalePolicyTest.LoginLocaleEnforcedByPolicy
    > browser test. It seems to be related to a pre-existing bug: crbug/844572
    > 
    > Bug: 839593
    > Change-Id: I022bf1603ba54386c3686aebd3868d6dee0cd993
    > Reviewed-on: https://chromium-review.googlesource.com/1093881
    > Commit-Queue: Quan Nguyen <qnnguyen@chromium.org>
    > Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#566197}
    
    TBR=jdufault@chromium.org,qnnguyen@chromium.org
    
    Change-Id: I03db3d2a5389bced6efbda91219c020ba4f19c68
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 839593
    Reviewed-on: https://chromium-review.googlesource.com/1098275
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566631}
    Former-commit-id: 83ce891c704906a68b9ed3ca02a86b4fd2d8a9e7

--- chrome/browser/chromeos/login/login_screen_policy_browsertest.cc
+++ chrome/browser/chromeos/login/login_screen_policy_browsertest.cc
@@ -143,12 +143,12 @@ class LoginScreenLocalePolicyTest : public LoginScreenPolicyTest {
 };
 
 IN_PROC_BROWSER_TEST_F(LoginScreenLocalePolicyTest,
-                       DISABLED_PRE_LoginLocaleEnforcedByPolicy) {
+                       PRE_LoginLocaleEnforcedByPolicy) {
   chromeos::StartupUtils::MarkOobeCompleted();
 }
 
 IN_PROC_BROWSER_TEST_F(LoginScreenLocalePolicyTest,
-                       DISABLED_LoginLocaleEnforcedByPolicy) {
+                       LoginLocaleEnforcedByPolicy) {
   // Verifies that the default locale can be overridden with policy.
   EXPECT_EQ("fr", g_browser_process->GetApplicationLocale());
   base::string16 french_title =
