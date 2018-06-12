6bc95b6bb973326433e2d8f28aeabd9535cd97a6
Xing Liu <xingliu@chromium.org>
2018-06-12 03:18:45 +0000

    Download location: Update layout for storage summary in download home.
    This CL updates the layout for storage summary string in download home
    that the current SpannableString is not flexible enough.
    
    Now we use a dedicated TextView and the "Download" title will be in
    the center of a relative layout. Also move storage summary code to a
    single class.
    
    Bug: 849973
    Change-Id: Ic05c16d85f2f04e0bedb93ec3e6c48812ecc2cfb
    Reviewed-on: https://chromium-review.googlesource.com/1093385
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566292}
    Former-commit-id: dc54c7827dbb6e65a5f9b5c78183a6f951ad226b

--- chrome/android/java/src/org/chromium/chrome/browser/download/ui/FilterAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ui/FilterAdapter.java
@@ -4,17 +4,8 @@
 
 package org.chromium.chrome.browser.download.ui;
 
-import android.graphics.Typeface;
-import android.os.AsyncTask;
-import android.os.Environment;
 import android.support.annotation.LayoutRes;
 import android.support.graphics.drawable.VectorDrawableCompat;
-import android.support.v7.content.res.AppCompatResources;
-import android.text.SpannableStringBuilder;
-import android.text.Spanned;
-import android.text.style.ForegroundColorSpan;
-import android.text.style.RelativeSizeSpan;
-import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,37 +15,13 @@ import android.widget.TextView;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.ChromeFeatureList;
-import org.chromium.chrome.browser.download.DirectoryOption;
-import org.chromium.chrome.browser.download.DownloadUtils;
 import org.chromium.chrome.browser.download.ui.DownloadManagerUi.DownloadUiObserver;
 import org.chromium.chrome.browser.util.FeatureUtilities;
-
-import java.io.File;
 
 /** An adapter that allows selecting an item from a dropdown spinner. */
 class FilterAdapter
         extends BaseAdapter implements AdapterView.OnItemSelectedListener, DownloadUiObserver {
     private DownloadManagerUi mManagerUi;
-    private DirectoryOption mDirectoryOption;
-    private DefaultDirectoryTask mDirectorySpaceTask;
-
-    /**
-     * Asynchronous task to query the default download directory option on primary storage.
-     * Pass one String parameter as the name of the directory option.
-     */
-    private static class DefaultDirectoryTask extends AsyncTask<String, Void, DirectoryOption> {
-        @Override
-        protected DirectoryOption doInBackground(String... params) {
-            assert params.length == 1;
-            File defaultDownloadDir =
-                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-            DirectoryOption directoryOption = new DirectoryOption(params[0],
-                    defaultDownloadDir.getAbsolutePath(), defaultDownloadDir.getUsableSpace(),
-                    defaultDownloadDir.getTotalSpace(), DirectoryOption.DEFAULT_OPTION);
-            return directoryOption;
-        }
-    }
 
     @Override
     public int getCount() {
@@ -80,8 +47,6 @@ class FilterAdapter
         VectorDrawableCompat iconDrawable =
                 VectorDrawableCompat.create(mManagerUi.getActivity().getResources(), iconId,
                         mManagerUi.getActivity().getTheme());
-        iconDrawable.setTintList(AppCompatResources.getColorStateList(
-                mManagerUi.getActivity(), R.color.dark_mode_tint));
         labelView.setCompoundDrawablesWithIntrinsicBounds(iconDrawable, null, null, null);
 
         return labelView;
@@ -92,38 +57,10 @@ class FilterAdapter
         TextView labelView =
                 getTextViewFromResource(convertView, R.layout.download_manager_spinner);
 
-        SpannableStringBuilder titleBuilder = new SpannableStringBuilder();
         CharSequence title = mManagerUi.getActivity().getResources().getText(position == 0
                         ? R.string.menu_downloads
                         : DownloadFilter.getStringIdForFilter(position));
-        titleBuilder.append(title);
-
-        // Set the storage summary for download location change feature.
-        if (ChromeFeatureList.isEnabled(ChromeFeatureList.DOWNLOADS_LOCATION_CHANGE)
-                && mDirectoryOption != null) {
-            titleBuilder.append("\n");
-            String storageSummary =
-                    mManagerUi.getActivity().getString(R.string.download_manager_ui_space_using,
-                            DownloadUtils.getStringForBytes(
-                                    mManagerUi.getActivity(), mDirectoryOption.availableSpace),
-                            DownloadUtils.getStringForBytes(
-                                    mManagerUi.getActivity(), mDirectoryOption.totalSpace));
-            titleBuilder.append(storageSummary);
-            @SuppressWarnings("deprecation")
-            int color = mManagerUi.getActivity().getResources().getColor(R.color.black_alpha_38);
-
-            int start = title.length() + 1;
-            int end = title.length() + storageSummary.length() + 1;
-
-            titleBuilder.setSpan(
-                    new ForegroundColorSpan(color), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            titleBuilder.setSpan(
-                    new RelativeSizeSpan(0.65f), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            titleBuilder.setSpan(
-                    new StyleSpan(Typeface.NORMAL), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        }
-
-        labelView.setText(titleBuilder);
+        labelView.setText(title);
 
         if (!FeatureUtilities.isChromeModernDesignEnabled()) {
             ApiCompatibilityUtils.setTextAppearance(labelView, R.style.BlackHeadline2);
@@ -150,18 +87,6 @@ class FilterAdapter
 
     public void initialize(DownloadManagerUi manager) {
         mManagerUi = manager;
-
-        // Show storage summary.
-        if (ChromeFeatureList.isEnabled(ChromeFeatureList.DOWNLOADS_LOCATION_CHANGE)) {
-            mDirectorySpaceTask = new DefaultDirectoryTask() {
-                @Override
-                protected void onPostExecute(DirectoryOption directoryOption) {
-                    mDirectoryOption = directoryOption;
-                    notifyDataSetChanged();
-                }
-            };
-            mDirectorySpaceTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "");
-        }
     }
 
     @Override
