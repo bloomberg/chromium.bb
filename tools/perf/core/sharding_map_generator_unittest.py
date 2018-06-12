bb3881980af46938e3cea415b36c0baa713a5680
Ashley Enstad <ashleymarie@chromium.org>
2018-06-12 21:57:45 +0000

    Fix and reenable unittest
    Bug: 713357
    Change-Id: I88006945277d7081ca71b33dac183e7619d29b82
    Reviewed-on: https://chromium-review.googlesource.com/1097233
    Reviewed-by: Emily Hanley <eyaich@chromium.org>
    Commit-Queue: Ashley Enstad <ashleymarie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566591}
    Former-commit-id: c8782f90b2b07290427d5baa368a06a3b9aae8ee

--- tools/perf/core/sharding_map_generator_unittest.py
+++ tools/perf/core/sharding_map_generator_unittest.py
@@ -3,20 +3,18 @@
 # found in the LICENSE file.
 
 from collections import OrderedDict
+import copy
 from core import sharding_map_generator
 import json
 import os
 import tempfile
 import unittest
 
-from telemetry import decorators
-
 
 class TestShardingMapGenerator(unittest.TestCase):
 
   def _init_sample_timing_data(self, times):
     timing_data = OrderedDict()
-    timing_list = []
     all_stories = {}
     for i in range(len(times)):
       all_stories['benchmark_' + str(i)] = []
@@ -25,30 +23,22 @@ class TestShardingMapGenerator(unittest.TestCase):
         all_stories['benchmark_' + str(i)].append('story_' + str(j))
         timing_data['benchmark_' + str(i) + '/' + 'story_' + str(j)] = (
             story_times[j])
-        timing_list.append({"run_time": story_times[j],
-          "run_name": 'benchmark_' + str(i) + '/' + 'story_' + str(j)})
-    return timing_data, all_stories, timing_list
+    return timing_data, all_stories
 
-  # TODO(eyaich@, ashleymarie@): fix & reenable this test
-  @decorators.Disabled('all')
   def testGenerateAndTestShardingMap(self):
-    timing_data, all_stories, timing_list = self._init_sample_timing_data(
+    timing_data, all_stories = self._init_sample_timing_data(
         [[60, 56, 57], [66, 54, 80, 4], [2, 8, 7, 37, 2]])
-
+    timing_data_for_testing = copy.deepcopy(timing_data)
     sharding_map = sharding_map_generator.generate_sharding_map(
         timing_data, all_stories, 3, None)
     fd_map, map_path = tempfile.mkstemp(suffix='.json')
-    fd_test_data, test_path = tempfile.mkstemp(suffix='.json')
     try:
       with os.fdopen(fd_map, 'w') as f:
         json.dump(sharding_map, f)
-      with os.fdopen(fd_test_data, 'w') as f:
-        json.dump(timing_list, f)
       results = sharding_map_generator.test_sharding_map(map_path,
-          test_path, all_stories)
-      self.assertEqual(results['0'], 173)
-      self.assertEqual(results['1'], 120)
-      self.assertEqual(results['2'], 140)
+          timing_data_for_testing, all_stories)
+      self.assertEqual(results['0']['full_time'], 173)
+      self.assertEqual(results['1']['full_time'], 120)
+      self.assertEqual(results['2']['full_time'], 140)
     finally:
       os.remove(map_path)
-      os.remove(test_path)
