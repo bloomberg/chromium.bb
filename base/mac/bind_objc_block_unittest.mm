69a2e972d3bc2f4511c561a99064df31ba8966d5
Sylvain Defresne <sdefresne@chromium.org>
2018-06-12 10:03:13 +0000

    Remove deprecated functions base::BindBlock(Arc)?
    All the code has been converted to use base::BindOnce
    or base::BindRepeating, so remove the old deprecated
    functions.
    
    Bug: 701275
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I42b9e3ee5446c6d7fab43572d7fbb29f78b798e2
    Reviewed-on: https://chromium-review.googlesource.com/1095259
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566369}
    Former-commit-id: bb291e0c35c7e29cb2e09813ddf703d4e85e0fea

--- base/mac/bind_objc_block_unittest.mm
+++ base/mac/bind_objc_block_unittest.mm
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "base/mac/bind_objc_block.h"
-
 #include <string>
 
 #include "base/bind.h"
@@ -24,9 +22,9 @@ TEST(BindObjcBlockTest, TestScopedClosureRunnerExitScope) {
   int run_count = 0;
   int* ptr = &run_count;
   {
-    base::ScopedClosureRunner runner(base::BindBlock(^{
-        (*ptr)++;
-    }));
+    base::ScopedClosureRunner runner(base::BindOnce(base::RetainBlock(^{
+      (*ptr)++;
+    })));
     EXPECT_EQ(0, run_count);
   }
   EXPECT_EQ(1, run_count);
@@ -37,9 +35,9 @@ TEST(BindObjcBlockTest, TestScopedClosureRunnerRelease) {
   int* ptr = &run_count;
   base::OnceClosure c;
   {
-    base::ScopedClosureRunner runner(base::BindBlock(^{
-        (*ptr)++;
-    }));
+    base::ScopedClosureRunner runner(base::BindOnce(base::RetainBlock(^{
+      (*ptr)++;
+    })));
     c = runner.Release();
     EXPECT_EQ(0, run_count);
   }
@@ -50,39 +48,42 @@ TEST(BindObjcBlockTest, TestScopedClosureRunnerRelease) {
 
 TEST(BindObjcBlockTest, TestReturnValue) {
   const int kReturnValue = 42;
-  base::Callback<int(void)> c = base::BindBlock(^{return kReturnValue;});
-  EXPECT_EQ(kReturnValue, c.Run());
+  base::OnceCallback<int(void)> c = base::BindOnce(base::RetainBlock(^{
+    return kReturnValue;
+  }));
+  EXPECT_EQ(kReturnValue, std::move(c).Run());
 }
 
 TEST(BindObjcBlockTest, TestArgument) {
   const int kArgument = 42;
-  base::Callback<int(int)> c = base::BindBlock(^(int a){return a + 1;});
-  EXPECT_EQ(kArgument + 1, c.Run(kArgument));
+  base::OnceCallback<int(int)> c = base::BindOnce(base::RetainBlock(^(int a) {
+    return a + 1;
+  }));
+  EXPECT_EQ(kArgument + 1, std::move(c).Run(kArgument));
 }
 
 TEST(BindObjcBlockTest, TestTwoArguments) {
   std::string result;
   std::string* ptr = &result;
-  base::Callback<void(const std::string&, const std::string&)> c =
-      base::BindBlock(^(const std::string& a, const std::string& b) {
-          *ptr = a + b;
-      });
-  c.Run("forty", "two");
+  base::OnceCallback<void(const std::string&, const std::string&)> c =
+      base::BindOnce(
+          base::RetainBlock(^(const std::string& a, const std::string& b) {
+            *ptr = a + b;
+          }));
+  std::move(c).Run("forty", "two");
   EXPECT_EQ(result, "fortytwo");
 }
 
 TEST(BindObjcBlockTest, TestThreeArguments) {
   std::string result;
   std::string* ptr = &result;
-  base::Callback<void(const std::string&,
-                      const std::string&,
-                      const std::string&)> c =
-      base::BindBlock(^(const std::string& a,
-                        const std::string& b,
-                        const std::string& c) {
-          *ptr = a + b + c;
-      });
-  c.Run("six", "times", "nine");
+  base::OnceCallback<void(const std::string&, const std::string&,
+                          const std::string&)>
+      c = base::BindOnce(base::RetainBlock(
+          ^(const std::string& a, const std::string& b, const std::string& c) {
+            *ptr = a + b + c;
+          }));
+  std::move(c).Run("six", "times", "nine");
   EXPECT_EQ(result, "sixtimesnine");
 }
 
@@ -91,14 +92,15 @@ TEST(BindObjcBlockTest, TestSixArguments) {
   std::string* ptr = &result1;
   int result2;
   int* ptr2 = &result2;
-  base::Callback<void(int, int, const std::string&, const std::string&,
-                      int, const std::string&)> c =
-      base::BindBlock(^(int a, int b, const std::string& c,
-                        const std::string& d, int e, const std::string& f) {
-          *ptr = c + d + f;
-          *ptr2 = a + b + e;
-      });
-  c.Run(1, 2, "infinite", "improbability", 3, "drive");
+  base::OnceCallback<void(int, int, const std::string&, const std::string&, int,
+                          const std::string&)>
+      c = base::BindOnce(base::RetainBlock(^(int a, int b, const std::string& c,
+                                             const std::string& d, int e,
+                                             const std::string& f) {
+        *ptr = c + d + f;
+        *ptr2 = a + b + e;
+      }));
+  std::move(c).Run(1, 2, "infinite", "improbability", 3, "drive");
   EXPECT_EQ(result1, "infiniteimprobabilitydrive");
   EXPECT_EQ(result2, 6);
 }
@@ -142,9 +144,9 @@ TEST(BindObjcBlockTest, TestBlockReleased) {
     NSObject* nsobject = [[[NSObject alloc] init] autorelease];
     weak_nsobject.reset(nsobject);
 
-    auto callback = base::BindBlock(^{
+    auto callback = base::BindOnce(base::RetainBlock(^{
       [nsobject description];
-    });
+    }));
   }
   EXPECT_NSEQ(nil, weak_nsobject);
 }
