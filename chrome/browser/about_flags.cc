e95fde0a5b5f74372abf066f3f625c4924f516fa
Adam Rice <ricea@chromium.org>
2018-06-13 05:34:51 +0000

    Temporarily remove enable-websocket-auth-connection-reuse from about:flags
    The M68 branch has a non-working implementation of the
    enable-websocket-auth-connection-reuse flag. In order to avoid the
    confusion of having a flag that works in some versions and not in
    others, temporarily remove the flag from about:flags.
    
    Once the flag has been disabled in the M68 branch it will be re-enabled
    for M69 by reverting this CL.
    
    BUG=423609
    
    Change-Id: Id17ac18e13effcfd5bd946dc1fe0f1c70df96539
    Reviewed-on: https://chromium-review.googlesource.com/1090700
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566730}
    Former-commit-id: 7e3874d48e23ea0ef6f1464763cfb08b48ce86d4

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -3877,11 +3877,6 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_VALUE_TYPE(ash::features::kTapVisualizerApp)},
 #endif
 
-    {"enable-websocket-auth-connection-reuse",
-     flag_descriptions::kWebSocketHandshakeReuseConnectionName,
-     flag_descriptions::kWebSocketHandshakeReuseConnectionDescription, kOsAll,
-     FEATURE_VALUE_TYPE(net::WebSocketBasicHandshakeStream::
-                            kWebSocketHandshakeReuseConnection)},
     {"unsafely-treat-insecure-origin-as-secure",
      flag_descriptions::kTreatInsecureOriginAsSecureName,
      flag_descriptions::kTreatInsecureOriginAsSecureDescription, kOsAll,
