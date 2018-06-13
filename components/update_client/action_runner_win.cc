d8ed06f0dceec951f8d25697b32e5ff369f230fc
Sorin Jianu <sorin@chromium.org>
2018-06-13 23:57:57 +0000

    Adds an appguid argument for the recovery component action run.
    The code now provides as a run action command line argument the Chrome
    app guid registered with Omaha, as returned by the configurator, like this:
    
    --appguid={8A69D345-D564-463c-AFF1-A69D9E530F96}
    
    Bug: 687231
    Change-Id: I22125e434ffbd4e98de9c3373dcb54ac7f98799a
    Reviewed-on: https://chromium-review.googlesource.com/1100088
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567051}
    Former-commit-id: 709aed6767d199e9125a948cc41b412ba938e35c

--- components/update_client/action_runner_win.cc
+++ components/update_client/action_runner_win.cc
@@ -56,6 +56,10 @@ base::CommandLine ActionRunner::MakeCommandLine(
   command_line.AppendSwitchASCII(
       "browser-version", component_.config()->GetBrowserVersion().GetString());
   command_line.AppendSwitchASCII("sessionid", component_.session_id());
+  const auto app_guid = component_.config()->GetAppGuid();
+  if (!app_guid.empty())
+    command_line.AppendSwitchASCII("appguid", app_guid);
+  VLOG(1) << "run action: " << command_line.GetCommandLineString();
   return command_line;
 }
 
