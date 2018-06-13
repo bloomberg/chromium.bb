1011f08b4a53824c8deb80cb5bfa5e767edbcabb
jdoerrie <jdoerrie@chromium.org>
2018-06-13 23:39:46 +0000

    [bluetooth] Unify SimulateLowEnergyDevice for BlueZ and Win
    This change aims to reduce code duplication in the implementations of
    BluetoothTest::SimulateLowEnergyDevice for BlueZ and Windows. In order
    to do so it adds a utility struct and method to BluetoothTestBase, that
    implementations of SimulateLowEnergyDevice make use of.
    
    Bug: 821766
    Change-Id: I5283a1eb55e50d68dce2c9810994cf6c4ed80212
    Reviewed-on: https://chromium-review.googlesource.com/1096675
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567047}
    Former-commit-id: ce567f0154deea317a6ebb16ae75f794e0b15aca

--- device/bluetooth/test/bluetooth_test_bluez.cc
+++ device/bluetooth/test/bluetooth_test_bluez.cc
@@ -115,52 +115,20 @@ void BluetoothTestBlueZ::InitWithFakeAdapter() {
 
 BluetoothDevice* BluetoothTestBlueZ::SimulateLowEnergyDevice(
     int device_ordinal) {
-  if (device_ordinal > 7 || device_ordinal < 1)
-    return nullptr;
+  LowEnergyDeviceData data = GetLowEnergyDeviceData(device_ordinal);
 
-  base::Optional<std::string> device_name = std::string(kTestDeviceName);
-  std::string device_address = kTestDeviceAddress1;
   std::vector<std::string> service_uuids;
-  BluetoothTransport device_type = BLUETOOTH_TRANSPORT_LE;
+  for (const auto& uuid : data.advertised_uuids)
+    service_uuids.push_back(uuid.canonical_value());
+
   std::map<std::string, std::vector<uint8_t>> service_data;
-  std::map<uint16_t, std::vector<uint8_t>> manufacturer_data;
+  for (const auto& service : data.service_data)
+    service_data.emplace(service.first.canonical_value(), service.second);
 
-  switch (device_ordinal) {
-    case 1:
-      service_uuids.push_back(kTestUUIDGenericAccess);
-      service_uuids.push_back(kTestUUIDGenericAttribute);
-      service_data[kTestUUIDHeartRate] = {0x01};
-      manufacturer_data[kTestManufacturerId] = {1, 2, 3, 4};
-      break;
-    case 2:
-      service_uuids.push_back(kTestUUIDImmediateAlert);
-      service_uuids.push_back(kTestUUIDLinkLoss);
-      service_data[kTestUUIDHeartRate] = {};
-      service_data[kTestUUIDImmediateAlert] = {0x00, 0x02};
-      manufacturer_data[kTestManufacturerId] = {};
-      break;
-    case 3:
-      device_name = std::string(kTestDeviceNameEmpty);
-      break;
-    case 4:
-      device_name = std::string(kTestDeviceNameEmpty);
-      device_address = kTestDeviceAddress2;
-      break;
-    case 5:
-      device_name = base::nullopt;
-      break;
-    case 6:
-      device_address = kTestDeviceAddress2;
-      device_type = BLUETOOTH_TRANSPORT_DUAL;
-      break;
-    case 7:
-      device_name.emplace(kTestDeviceNameU2f);
-      service_uuids.push_back(kTestUUIDU2f);
-      service_data[kTestUUIDU2fControlPointLength] = {0x00, 0x14};
-      break;
-  }
+  std::map<uint16_t, std::vector<uint8_t>> manufacturer_data(
+      data.manufacturer_data.begin(), data.manufacturer_data.end());
 
-  BluetoothDevice* device = adapter_->GetDevice(device_address);
+  BluetoothDevice* device = adapter_->GetDevice(data.address);
   if (device) {
     fake_bluetooth_device_client_->UpdateServiceAndManufacturerData(
         GetDevicePath(device), service_uuids, service_data, manufacturer_data);
@@ -169,11 +137,11 @@ BluetoothDevice* BluetoothTestBlueZ::SimulateLowEnergyDevice(
 
   fake_bluetooth_device_client_->CreateTestDevice(
       dbus::ObjectPath(bluez::FakeBluetoothAdapterClient::kAdapterPath),
-      /* name */ device_name,
-      /* alias */ device_name.value_or("") + "(alias)", device_address,
-      service_uuids, device_type, service_data, manufacturer_data);
+      /* name */ data.name,
+      /* alias */ data.name.value_or("") + "(alias)", data.address,
+      service_uuids, data.transport, service_data, manufacturer_data);
 
-  return adapter_->GetDevice(device_address);
+  return adapter_->GetDevice(data.address);
 }
 
 BluetoothDevice* BluetoothTestBlueZ::SimulateClassicDevice() {
