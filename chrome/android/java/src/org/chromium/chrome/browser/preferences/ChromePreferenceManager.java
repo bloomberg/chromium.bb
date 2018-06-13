07f6305fa543d0814f7e352b4878a5bcddec555e
Peter E Conn <peconn@chromium.org>
2018-06-13 10:10:45 +0000

    🤝 Notify user that a Trusted Web Activity is running in Chrome.
    Notify the user via Toast that an opened Trusted Web Activity is part
    of Chrome and therefore will have access to their cookies, autofill
    data, etc.
    
    This is shown at most once per week for a single app.
    
    Bug: 813100
    Change-Id: I7f921637ffa042ff4d0f076ad4ca007708f06879
    Reviewed-on: https://chromium-review.googlesource.com/1097399
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566784}
    Former-commit-id: f1f3e06f07823f78ea19fcab5cb17b8dc862b441

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/ChromePreferenceManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/ChromePreferenceManager.java
@@ -11,6 +11,7 @@ import org.chromium.base.ContextUtils;
 import org.chromium.chrome.browser.crash.MinidumpUploadService.ProcessType;
 
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Set;
@@ -78,6 +79,8 @@ public class ChromePreferenceManager {
 
     private static final String VERIFIED_DIGITAL_ASSET_LINKS =
             "verified_digital_asset_links";
+    private static final String TRUSTED_WEB_ACTIVITY_LAST_DISCLOSURE_TIME =
+            "trusted_web_activity_last_disclosure_time:";
 
     private static class LazyHolder {
         static final ChromePreferenceManager INSTANCE = new ChromePreferenceManager();
@@ -521,6 +524,43 @@ public class ChromePreferenceManager {
      */
     public void setVerifiedDigitalAssetLinks(Set<String> links) {
         mSharedPreferences.edit().putStringSet(VERIFIED_DIGITAL_ASSET_LINKS, links).apply();
+    }
+
+    /**
+     * Private convenience method to create a Preferences Key to hold the last time the given
+     * package displayed a "Running in Chrome" disclosure while opening a Trusted Web Activity.
+     */
+    private static String getTrustedWebActivityDisclosureTimeKey(String packageName) {
+        return TRUSTED_WEB_ACTIVITY_LAST_DISCLOSURE_TIME + packageName;
+    }
+
+    /**
+     * Gets the last time a disclosure was shown while opening a Trusted Web Activity for the given
+     * package. Returns a Date object representing the Unix epoch if no data was found.
+     */
+    public Date getTrustedWebActivityLastDisclosureTime(String packageName) {
+        return new Date(readLong(getTrustedWebActivityDisclosureTimeKey(packageName), 0));
+    }
+
+    /**
+     * Sets the last time a disclosure was shown while opening a Trusted Web Activity for the given
+     * package.
+     */
+    public void setTrustedWebActivityLastDisclosureTime(String packageName, Date time) {
+        writeLong(getTrustedWebActivityDisclosureTimeKey(packageName), time.getTime());
+    }
+
+    /**
+     * Wipes all recordings of the last disclosure times for packages opening TWAs.
+     */
+    public void clearAllTrustedWebActivityLastDisclosureTimes() {
+        SharedPreferences.Editor ed = mSharedPreferences.edit();
+        for (String key : mSharedPreferences.getAll().keySet()) {
+            if (!key.startsWith(TRUSTED_WEB_ACTIVITY_LAST_DISCLOSURE_TIME)) continue;
+
+            ed.remove(key);
+        }
+        ed.apply();
     }
 
     /**
