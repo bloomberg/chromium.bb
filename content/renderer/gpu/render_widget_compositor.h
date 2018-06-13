d2aee947dba5f5dd6bf6649e6294c3a72ae7765a
Sadrul Habib Chowdhury <sadrul@chromium.org>
2018-06-13 19:16:01 +0000

    Reland "paint timing: Use presentation-time instead of swap-time."
    Reland of crrev.com/566724. Original commit message:
    
    The presentation-time from gpu gives a more correct view of what the
    user experiences, rather than the swap-time in the renderer process.
    So switch to using the presentation-time to measure the various
    paint-time metrics.
    
    The various paint-time metrics in telemetry tests (loading benchmark)
    will go up after this change, but it is expected, and it is not a
    regression. Results on various platforms with this change:
    
    Win 7: https://pinpoint-dot-chromeperf.appspot.com/job/14d09714240000
    Win 10: https://pinpoint-dot-chromeperf.appspot.com/job/149921e4240000
    Mac low-end: https://pinpoint-dot-chromeperf.appspot.com/job/12eeeba5240000
    Android one: https://pinpoint-dot-chromeperf.appspot.com/job/16ab13e9240000
    Android N5x: https://pinpoint-dot-chromeperf.appspot.com/job/12d45da5240000
    
    BUG=811961
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I7742fa669ab583dbfda8be2259c553385b7fef10
    Reviewed-on: https://chromium-review.googlesource.com/1099187
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#566942}
    Former-commit-id: 992979a662b28e7b9a62419f87b1839557611efc

--- content/renderer/gpu/render_widget_compositor.h
+++ content/renderer/gpu/render_widget_compositor.h
@@ -8,6 +8,7 @@
 #include <stdint.h>
 
 #include "base/callback.h"
+#include "base/containers/circular_deque.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "base/values.h"
@@ -208,10 +209,10 @@ class CONTENT_EXPORT RenderWidgetCompositor
   void DidCommitAndDrawFrame() override;
   void DidReceiveCompositorFrameAck() override;
   void DidCompletePageScaleAnimation() override;
+  bool IsForSubframe() override;
   void DidPresentCompositorFrame(
       uint32_t frame_token,
-      const gfx::PresentationFeedback& feedback) override {}
-  bool IsForSubframe() override;
+      const gfx::PresentationFeedback& feedback) override;
 
   // cc::LayerTreeHostSingleThreadClient implementation.
   void RequestScheduleAnimation() override;
@@ -228,6 +229,10 @@ class CONTENT_EXPORT RenderWidgetCompositor
   void CreateRenderFrameObserver(
       mojom::RenderFrameMetadataObserverRequest request,
       mojom::RenderFrameMetadataObserverClientPtrInfo client_info);
+
+  void AddPresentationCallback(
+      uint32_t frame_token,
+      base::OnceCallback<void(base::TimeTicks)> callback);
 
   cc::LayerTreeHost* layer_tree_host() { return layer_tree_host_.get(); }
 
@@ -259,6 +264,10 @@ class CONTENT_EXPORT RenderWidgetCompositor
   base::OnceClosure layout_and_paint_async_callback_;
 
   viz::FrameSinkId frame_sink_id_;
+  base::circular_deque<
+      std::pair<uint32_t,
+                std::vector<base::OnceCallback<void(base::TimeTicks)>>>>
+      presentation_callbacks_;
 
   base::WeakPtrFactory<RenderWidgetCompositor> weak_factory_;
 
