5d99b64d81d9fe2ec6af6552e78d9e47e16a5e47
Ryan Hansberry <hansberry@chromium.org>
2018-06-14 18:29:57 +0000

    [CrOS Multidevice] Migrate ProximityAuthWebUIHandler to conditionally use DeviceSyncClient.
    ProximityAuthWebUiHandler now uses either ProximityAuthClient or DeviceSyncClient
    to get remote device info or perform Cryptauth calls, depending on whether the
    chromeos::features::kMultiDeviceApi is enabled. Once the migration to DeviceSyncClient
    has been completed across all of Smart Lock, code that references ProximityAuthClient will
    be removed.
    
    This is one of many future CLs which are migrating Smart Lock to use
    DeviceSync Mojo API instead of directly using the CryptAuth API.
    
    I have manually verified the correctness of this change by verifying the
    output on chrome://proximity-auth.
    
    Bug: 824568, 752273, 848956
    Change-Id: Ia6bf1666f631a79c2873df3745232b8a8152bc8e
    Reviewed-on: https://chromium-review.googlesource.com/1099050
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567354}
    Former-commit-id: e30241b2090ac9d36fadf0c31ce53eede27e4303

--- chromeos/components/proximity_auth/webui/proximity_auth_ui.cc
+++ chromeos/components/proximity_auth/webui/proximity_auth_ui.cc
@@ -9,6 +9,7 @@
 #include "chromeos/components/proximity_auth/webui/proximity_auth_webui_handler.h"
 #include "chromeos/components/proximity_auth/webui/url_constants.h"
 #include "chromeos/grit/chromeos_resources.h"
+#include "chromeos/services/device_sync/public/cpp/device_sync_client.h"
 #include "chromeos/services/multidevice_setup/public/mojom/constants.mojom.h"
 #include "components/grit/components_resources.h"
 #include "content/public/browser/browser_context.h"
@@ -19,8 +20,10 @@
 
 namespace proximity_auth {
 
-ProximityAuthUI::ProximityAuthUI(content::WebUI* web_ui,
-                                 ProximityAuthClient* delegate)
+ProximityAuthUI::ProximityAuthUI(
+    content::WebUI* web_ui,
+    ProximityAuthClient* proximity_auth_client,
+    chromeos::device_sync::DeviceSyncClient* device_sync_client)
     : ui::MojoWebUIController(web_ui, true /* enable_chrome_send */) {
   content::WebUIDataSource* source =
       content::WebUIDataSource::Create(kChromeUIProximityAuthHost);
@@ -49,8 +52,8 @@ ProximityAuthUI::ProximityAuthUI(content::WebUI* web_ui,
   content::BrowserContext* browser_context =
       web_ui->GetWebContents()->GetBrowserContext();
   content::WebUIDataSource::Add(browser_context, source);
-  web_ui->AddMessageHandler(
-      std::make_unique<ProximityAuthWebUIHandler>(delegate));
+  web_ui->AddMessageHandler(std::make_unique<ProximityAuthWebUIHandler>(
+      proximity_auth_client, device_sync_client));
   AddHandlerToRegistry(base::BindRepeating(
       &ProximityAuthUI::BindMultiDeviceSetup, base::Unretained(this)));
 }
