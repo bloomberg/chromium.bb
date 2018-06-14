834accf1ddf0d0f306d3b86ac72b3520ac483bd3
btolsch <btolsch@chromium.org>
2018-06-13 23:10:28 -0700

    Add comparison CHECK()s and debug logging
    Change-Id: I5fb8027277e3f92147a56381432d564d0258b85d
    Reviewed-on: https://chromium-review.googlesource.com/1100551
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>

--- platform/api/logging.h
+++ platform/api/logging.h
@@ -88,16 +88,44 @@ class Voidify {
 #define LOG_IF(level, condition) LAZY_STREAM(LOG_##level, (condition))
 #define VLOG_IF(level, condition) LAZY_STREAM(VLOG(level), (condition))
 
+// TODO(btolsch): Add tests for (D)CHECK and possibly logging.
 #define CHECK(condition) \
   LAZY_STREAM(LOG_FATAL, !(condition)) << "CHECK(" << #condition << ") failed: "
+
+#define CHECK_EQ(a, b) CHECK((a) == (b)) << a << " vs. " << b << ": "
+#define CHECK_NE(a, b) CHECK((a) != (b)) << a << " vs. " << b << ": "
+#define CHECK_LT(a, b) CHECK((a) < (b)) << a << " vs. " << b << ": "
+#define CHECK_LE(a, b) CHECK((a) <= (b)) << a << " vs. " << b << ": "
+#define CHECK_GT(a, b) CHECK((a) > (b)) << a << " vs. " << b << ": "
+#define CHECK_GE(a, b) CHECK((a) >= (b)) << a << " vs. " << b << ": "
 
 #if defined(_DEBUG) || defined(DCHECK_ALWAYS_ON)
 #define DCHECK_IS_ON() 1
 #define DCHECK(condition) CHECK(condition)
+#define DCHECK_EQ(a, b) CHECK_EQ(a, b)
+#define DCHECK_NE(a, b) CHECK_NE(a, b)
+#define DCHECK_LT(a, b) CHECK_LT(a, b)
+#define DCHECK_LE(a, b) CHECK_LE(a, b)
+#define DCHECK_GT(a, b) CHECK_GT(a, b)
+#define DCHECK_GE(a, b) CHECK_GE(a, b)
 #else
 #define DCHECK_IS_ON() 0
 #define DCHECK(condition) EAT_STREAM
+#define DCHECK_EQ(a, b) EAT_STREAM
+#define DCHECK_NE(a, b) EAT_STREAM
+#define DCHECK_LT(a, b) EAT_STREAM
+#define DCHECK_LE(a, b) EAT_STREAM
+#define DCHECK_GT(a, b) EAT_STREAM
+#define DCHECK_GE(a, b) EAT_STREAM
 #endif
+
+#define DLOG_INFO LOG_IF(INFO, DCHECK_IS_ON())
+#define DLOG_WARN LOG_IF(WARN, DCHECK_IS_ON())
+#define DLOG_ERROR LOG_IF(ERROR, DCHECK_IS_ON())
+#define DLOG_FATAL LOG_IF(FATAL, DCHECK_IS_ON())
+#define DLOG_IF(level, condition) LOG_IF(level, DCHECK_IS_ON() && (condition))
+#define DVLOG(l) VLOG_IF(l, DCHECK_IS_ON())
+#define DVLOG_IF(l, condition) VLOG_IF(l, DCHECK_IS_ON() && (condition))
 
 }  // namespace platform
 }  // namespace openscreen
