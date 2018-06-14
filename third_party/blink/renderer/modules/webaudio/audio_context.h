48d8bca6f25d0b22cda085d1cb308f6830136196
Hongchan Choi <hongchan@chromium.org>
2018-06-14 01:40:37 +0000

    Move Autoplay-related code from BaseAudioContext to AudioContext
    This CL splits the Autoplay-related code from BAC to AC. It simply
    changes the code location without changing logic.
    
    Note that the code around resume() promise resolvers has not been
    modified in this CL. The code is not directly related to the auto
    play and it needs a bit of rewriting.
    
    Bug: 851608
    Test: AudioContextAutoplayTest
    Change-Id: I2e0b2b10fc56e86e4724635aba0fe452c73c54da
    Reviewed-on: https://chromium-review.googlesource.com/1097988
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567082}
    Former-commit-id: 4b48f2def6c7d240fc4242a4ed8e3cb4fe8b3986

--- third_party/blink/renderer/modules/webaudio/audio_context.h
+++ third_party/blink/renderer/modules/webaudio/audio_context.h
@@ -7,6 +7,7 @@
 
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
+#include "third_party/blink/renderer/core/html/media/autoplay_policy.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_context_options.h"
 #include "third_party/blink/renderer/modules/webaudio/base_audio_context.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
@@ -44,17 +45,59 @@ class MODULES_EXPORT AudioContext : public BaseAudioContext {
   void getOutputTimestamp(ScriptState*, AudioTimestamp&);
   double baseLatency() const;
 
+  // For metrics purpose, records when start() is called on a
+  // AudioScheduledSourceHandler or a AudioBufferSourceHandler without a user
+  // gesture while the AudioContext requires a user gesture.
+  void MaybeRecordStartAttempt() final;
+
  protected:
   AudioContext(Document&, const WebAudioLatencyHint&);
+  void Uninitialize() final;
 
   void DidClose() final;
 
  private:
+  friend class AudioContextAutoplayTest;
+
+  // Do not change the order of this enum, it is used for metrics.
+  enum AutoplayStatus {
+    // The AudioContext failed to activate because of user gesture requirements.
+    kAutoplayStatusFailed = 0,
+    // Same as AutoplayStatusFailed but start() on a node was called with a user
+    // gesture.
+    kAutoplayStatusFailedWithStart = 1,
+    // The AudioContext had user gesture requirements and was able to activate
+    // with a user gesture.
+    kAutoplayStatusSucceeded = 2,
+
+    // Keep at the end.
+    kAutoplayStatusCount
+  };
+
+  // Returns the AutoplayPolicy currently applying to this instance.
+  AutoplayPolicy::Type GetAutoplayPolicy() const;
+
+  // Returns whether the autoplay requirements are fulfilled.
+  bool AreAutoplayRequirementsFulfilled() const;
+
+  // If any, unlock user gesture requirements if a user gesture is being
+  // processed.
+  void MaybeUnlockUserGesture();
+
+  // Returns whether the AudioContext is allowed to start rendering.
+  bool IsAllowedToStart() const;
+
+  // Record the current autoplay status and clear it.
+  void RecordAutoplayStatus();
+
   void StopRendering();
 
   unsigned context_id_;
   Member<ScriptPromiseResolver> close_resolver_;
 
+  // Whether a user gesture is required to start this AudioContext.
+  bool user_gesture_required_;
+  base::Optional<AutoplayStatus> autoplay_status_;
 };
 
 }  // namespace blink
