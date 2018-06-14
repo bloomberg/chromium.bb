24c7ac7c69f95c1ed037ce176075bc47b26a806a
Sergiy Byelozyorov <sergiyb@chromium.org>
2018-06-14 08:07:11 +0000

    Use correct syntax to define 1-tuple
    TBR=dpranke@chromium.org
    
    Bug: 831171
    Change-Id: Ida71a6072a3b25910bdda8fa4f15901349c7c8a0
    Reviewed-on: https://chromium-review.googlesource.com/1100461
    Reviewed-by: Sergiy Byelozyorov <sergiyb@chromium.org>
    Commit-Queue: Sergiy Byelozyorov <sergiyb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567187}
    Former-commit-id: f78077a9a68bc4b4681cb7b3357eb5eba2301a17

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -660,7 +660,7 @@ _KNOWN_ROBOTS = set(
               'src-internal', 'webrtc')
   ) | set('%s@appspot.gserviceaccount.com' % s for s in ('findit-for-me',)
   ) | set('%s@chops-service-accounts.iam.gserviceaccount.com' % s
-    for s in ('v8-ci-autoroll-builder'))
+          for s in ('v8-ci-autoroll-builder',))
 
 
 def _CheckNoProductionCodeUsingTestOnlyFunctions(input_api, output_api):
