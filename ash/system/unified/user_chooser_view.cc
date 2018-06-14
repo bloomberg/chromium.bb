173cb48cb4bf8f7b26eb0c9efd680392ed32775f
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2018-06-14 05:16:05 +0000

    Add separator and close button to multi user switcher.
    Bug: 845805
    Change-Id: I2c17eab602db7b45d6c140a4da5a1a1f5248926b
    Reviewed-on: https://chromium-review.googlesource.com/1082188
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567151}
    Former-commit-id: c80df7c4d5f048c28e6c4c99ee2c8c9ad98acaf9

--- ash/system/unified/user_chooser_view.cc
+++ ash/system/unified/user_chooser_view.cc
@@ -11,14 +11,18 @@
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/system/tray/tray_constants.h"
 #include "ash/system/tray/tray_popup_utils.h"
+#include "ash/system/tray/tri_view.h"
 #include "ash/system/unified/top_shortcuts_view.h"
 #include "ash/system/unified/unified_system_tray_controller.h"
 #include "ash/system/user/rounded_image_view.h"
 #include "base/strings/utf_string_conversions.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/paint_vector_icon.h"
+#include "ui/views/border.h"
+#include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/button/label_button.h"
 #include "ui/views/layout/box_layout.h"
+#include "ui/views/layout/fill_layout.h"
 #include "ui/views/widget/widget.h"
 
 namespace ash {
@@ -55,10 +59,37 @@ base::string16 GetUserItemAccessibleString(int user_index) {
 
 namespace {
 
+class CloseButton : public views::ImageButton, public views::ButtonListener {
+ public:
+  explicit CloseButton(UnifiedSystemTrayController* controller);
+
+  // views::ButtonListener:
+  void ButtonPressed(views::Button* sender, const ui::Event& event) override;
+
+ private:
+  UnifiedSystemTrayController* const controller_;
+
+  DISALLOW_COPY_AND_ASSIGN(CloseButton);
+};
+
+CloseButton::CloseButton(UnifiedSystemTrayController* controller)
+    : ImageButton(this), controller_(controller) {
+  // TODO(crbug.com/845805): Replace the close button icon.
+  gfx::ImageSkia icon =
+      gfx::CreateVectorIcon(kOverviewWindowCloseIcon, kMenuIconColor);
+  SetImage(views::Button::STATE_NORMAL, icon);
+}
+
+void CloseButton::ButtonPressed(views::Button* sender, const ui::Event& event) {
+  controller_->TransitionToMainView(true /* restore_focus */);
+}
+
 // A button item of a switchable user.
 class UserItemButton : public views::Button, public views::ButtonListener {
  public:
-  UserItemButton(int user_index, UnifiedSystemTrayController* controller);
+  UserItemButton(int user_index,
+                 UnifiedSystemTrayController* controller,
+                 bool has_close_button);
   ~UserItemButton() override = default;
 
   // views::ButtonListener:
@@ -71,19 +102,42 @@ class UserItemButton : public views::Button, public views::ButtonListener {
   DISALLOW_COPY_AND_ASSIGN(UserItemButton);
 };
 
-UserItemButton::UserItemButton(int user_index,
-                               UnifiedSystemTrayController* controller)
-    : Button(this), user_index_(user_index), controller_(controller) {
-  SetLayoutManager(std::make_unique<views::BoxLayout>(
-      views::BoxLayout::kHorizontal, gfx::Insets(kUnifiedTopShortcutSpacing),
-      kUnifiedTopShortcutSpacing));
-  AddChildView(CreateUserAvatarView(user_index));
+std::unique_ptr<views::LayoutManager> CreateLayoutManagerForIconRow() {
+  auto layout =
+      std::make_unique<views::BoxLayout>(views::BoxLayout::kHorizontal);
+  layout->set_main_axis_alignment(views::BoxLayout::MAIN_AXIS_ALIGNMENT_CENTER);
+  layout->set_cross_axis_alignment(
+      views::BoxLayout::CROSS_AXIS_ALIGNMENT_CENTER);
+  return std::move(layout);
+}
 
-  views::View* vertical_labels = new views::View;
-  auto* layout = vertical_labels->SetLayoutManager(
-      std::make_unique<views::BoxLayout>(views::BoxLayout::kVertical));
+std::unique_ptr<views::LayoutManager> CreateLayoutManagerForBodyRow() {
+  auto layout = std::make_unique<views::BoxLayout>(views::BoxLayout::kVertical);
+  layout->set_main_axis_alignment(views::BoxLayout::MAIN_AXIS_ALIGNMENT_CENTER);
   layout->set_cross_axis_alignment(
       views::BoxLayout::CROSS_AXIS_ALIGNMENT_START);
+  return std::move(layout);
+}
+
+UserItemButton::UserItemButton(int user_index,
+                               UnifiedSystemTrayController* controller,
+                               bool has_close_button)
+    : Button(this), user_index_(user_index), controller_(controller) {
+  TriView* tri_view = new TriView(TriView::Orientation::HORIZONTAL, 0);
+
+  tri_view->SetMinSize(TriView::Container::START,
+                       gfx::Size(kUnifiedUserChooserAvatorIconColumnWidth,
+                                 kUnifiedUserChooserRowHeight));
+  tri_view->SetContainerLayout(TriView::Container::START,
+                               CreateLayoutManagerForIconRow());
+  tri_view->AddView(TriView::Container::START,
+                    CreateUserAvatarView(user_index));
+
+  tri_view->SetMinSize(TriView::Container::CENTER,
+                       gfx::Size(0, kUnifiedUserChooserRowHeight));
+  tri_view->SetFlexForContainer(TriView::Container::CENTER, 1);
+  tri_view->SetContainerLayout(TriView::Container::CENTER,
+                               CreateLayoutManagerForBodyRow());
 
   const mojom::UserSession* const user_session =
       Shell::Get()->session_controller()->GetUserSession(user_index);
@@ -93,16 +147,26 @@ UserItemButton::UserItemButton(int user_index,
   name->SetEnabledColor(kUnifiedMenuTextColor);
   name->SetAutoColorReadabilityEnabled(false);
   name->SetSubpixelRenderingEnabled(false);
-  vertical_labels->AddChildView(name);
+  tri_view->AddView(TriView::Container::CENTER, name);
 
   auto* email = new views::Label(
       base::UTF8ToUTF16(user_session->user_info->display_email));
   email->SetEnabledColor(kUnifiedMenuSecondaryTextColor);
   email->SetAutoColorReadabilityEnabled(false);
   email->SetSubpixelRenderingEnabled(false);
-  vertical_labels->AddChildView(email);
+  tri_view->AddView(TriView::Container::CENTER, email);
 
-  AddChildView(vertical_labels);
+  if (has_close_button) {
+    tri_view->SetMinSize(TriView::Container::END,
+                         gfx::Size(kUnifiedUserChooserCloseIconColumnWidth,
+                                   kUnifiedUserChooserRowHeight));
+    tri_view->SetContainerLayout(TriView::Container::END,
+                                 CreateLayoutManagerForIconRow());
+    tri_view->AddView(TriView::Container::END, new CloseButton(controller_));
+  }
+
+  SetLayoutManager(std::make_unique<views::FillLayout>());
+  AddChildView(tri_view);
 
   SetTooltipText(GetUserItemAccessibleString(user_index));
   SetFocusPainter(TrayPopupUtils::CreateFocusPainter());
@@ -158,6 +222,27 @@ void AddUserButton::ButtonPressed(views::Button* sender,
   controller_->HandleAddUserAction();
 }
 
+class Separator : public views::View {
+ public:
+  explicit Separator(bool between_user) {
+    SetLayoutManager(std::make_unique<views::FillLayout>());
+    SetBorder(views::CreateEmptyBorder(
+        between_user
+            ? gfx::Insets(0, kUnifiedUserChooserSeparatorSideMargin)
+            : gfx::Insets(kUnifiedUserChooserLargeSeparatorVerticalSpacing,
+                          0)));
+    views::View* child = new views::View();
+    // make sure that the view is displayed by setting non-zero size
+    child->SetPreferredSize(gfx::Size(1, 1));
+    AddChildView(child);
+    child->SetBorder(views::CreateSolidSidedBorder(
+        0, 0, kUnifiedNotificationSeparatorThickness, 0,
+        kUnifiedMenuSeparatorColor));
+  }
+
+  DISALLOW_COPY_AND_ASSIGN(Separator);
+};
+
 }  // namespace
 
 UserChooserView::UserChooserView(UnifiedSystemTrayController* controller) {
@@ -165,8 +250,10 @@ UserChooserView::UserChooserView(UnifiedSystemTrayController* controller) {
       std::make_unique<views::BoxLayout>(views::BoxLayout::kVertical));
   const int num_users =
       Shell::Get()->session_controller()->NumberOfLoggedInUsers();
-  for (int i = 0; i < num_users; ++i)
-    AddChildView(new UserItemButton(i, controller));
+  for (int i = 0; i < num_users; ++i) {
+    AddChildView(new UserItemButton(i, controller, i == 0));
+    AddChildView(new Separator(i < num_users - 1));
+  }
   if (Shell::Get()->session_controller()->GetAddUserPolicy() ==
       AddUserSessionPolicy::ALLOWED) {
     AddChildView(new AddUserButton(controller));
