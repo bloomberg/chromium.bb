012cbe249df6092f924f601ed84fe65b10c5ec91
Zoe Liu <zoeliu@google.com>
2018-06-14 16:56:18 -0700

    Fix int32_t overflow in av1_round_shift_array_c
    BUG=aomedia:1960
    
    Change-Id: Ie9e39bbcd21b7cae9f0f9c5b6777d892bb78a473

--- av1/common/av1_txfm.c
+++ av1/common/av1_txfm.c
@@ -78,7 +78,7 @@ void av1_round_shift_array_c(int32_t *arr, int size, int bit) {
       }
     } else {
       for (i = 0; i < size; i++) {
-        arr[i] = arr[i] * (1 << (-bit));
+        arr[i] = (int32_t)((int64_t)arr[i] * (1ULL << (-bit)));
       }
     }
   }
