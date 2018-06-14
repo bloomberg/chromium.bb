880b2f59d74a661f9435b15c9e0eeaaffe1eef5d
François Doray <fdoray@chromium.org>
2018-06-14 21:05:19 +0000

    Revert "Make base::ScopedZxHandle a zx::handle."
    This reverts commit 1f398cda5ec4dbeb77508c1f0a01c969ff25cf22 [formerly e17dbaf1b7b5da00d05fd5d2bf199f9a711ff514].
    
    Reason for revert: Compile failure https://crbug.com/852958
    
    Original change's description:
    > Make base::ScopedZxHandle a zx::handle.
    > 
    > - Add the ScopedZxHandle APIs to the base zx::handle.
    > - Add missing includes.
    > 
    > This allows ScopedZxHandle to be removed incrementally from call-sites.
    > 
    > Bug: 852541
    > Change-Id: Idc452a450ce2bbe1266e9e0ee266115e2ea38f97
    > Reviewed-on: https://chromium-review.googlesource.com/1100132
    > Commit-Queue: Wez <wez@chromium.org>
    > Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@chromium.org>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#567382}
    
    TBR=wez@chromium.org,gab@chromium.org,rockot@chromium.org,kmarshall@chromium.org
    
    Change-Id: I44b3be92d12a29a972ca3c59d3356f97b21292f2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 852541
    Reviewed-on: https://chromium-review.googlesource.com/1101777
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567416}
    Former-commit-id: 2e0857a0db62e3e9a317f7139c5482cd205853d7

--- base/memory/shared_memory_fuchsia.cc
+++ base/memory/shared_memory_fuchsia.cc
@@ -11,8 +11,8 @@
 #include <zircon/syscalls.h>
 
 #include "base/bits.h"
-#include "base/fuchsia/fuchsia_logging.h"
 #include "base/fuchsia/scoped_zx_handle.h"
+#include "base/logging.h"
 #include "base/memory/shared_memory_tracker.h"
 #include "base/process/process_metrics.h"
 
@@ -66,7 +66,8 @@ bool SharedMemory::Create(const SharedMemoryCreateOptions& options) {
     ScopedZxHandle old_vmo(std::move(vmo));
     status = zx_handle_replace(old_vmo.get(), kNoExecFlags, vmo.receive());
     if (status != ZX_OK) {
-      ZX_DLOG(ERROR, status) << "zx_handle_replace()";
+      DLOG(ERROR) << "zx_handle_replace() failed: "
+                  << zx_status_get_string(status);
       return false;
     }
     ignore_result(old_vmo.release());
@@ -94,7 +95,7 @@ bool SharedMemory::MapAt(off_t offset, size_t bytes) {
   zx_status_t status = zx_vmar_map(zx_vmar_root_self(), 0, shm_.GetHandle(),
                                    offset, bytes, flags, &addr);
   if (status != ZX_OK) {
-    ZX_DLOG(ERROR, status) << "zx_vmar_map failed";
+    DLOG(ERROR) << "zx_vmar_map failed, status=" << status;
     return false;
   }
   memory_ = reinterpret_cast<void*>(addr);
@@ -114,7 +115,7 @@ bool SharedMemory::Unmap() {
   uintptr_t addr = reinterpret_cast<uintptr_t>(memory_);
   zx_status_t status = zx_vmar_unmap(zx_vmar_root_self(), addr, mapped_size_);
   if (status != ZX_OK) {
-    ZX_DLOG(ERROR, status) << "zx_vmar_unmap";
+    DLOG(ERROR) << "zx_vmar_unmap failed, status=" << status;
     return false;
   }
 
