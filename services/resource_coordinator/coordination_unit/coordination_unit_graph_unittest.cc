d462e4bfea2436fffcc3e1cd6ffa79b769660890
Sigurdur Asgeirsson <siggi@chromium.org>
2018-06-14 13:17:19 +0000

    RC: Make the graph resilient to PID reuse.
    Change-Id: I9695d4c554ce45c339282dcd2c70adc1e83da557
    Reviewed-on: https://chromium-review.googlesource.com/1099257
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567241}
    Former-commit-id: 5a766d54088a6c6860bd5a55929a60abe9520bb9

--- services/resource_coordinator/coordination_unit/coordination_unit_graph_unittest.cc
+++ services/resource_coordinator/coordination_unit/coordination_unit_graph_unittest.cc
@@ -8,15 +8,22 @@
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace resource_coordinator {
+namespace {
+
+ProcessCoordinationUnitImpl* CreateProcessCU(CoordinationUnitGraph* graph) {
+  return graph->CreateProcessCoordinationUnit(
+      CoordinationUnitID(CoordinationUnitType::kProcess,
+                         CoordinationUnitID::RANDOM_ID),
+      nullptr);
+}
+
+}  // namespace
 
 TEST(CoordinationUnitGraphTest, DestructionWhileCUSOutstanding) {
   std::unique_ptr<CoordinationUnitGraph> graph(new CoordinationUnitGraph());
 
   for (size_t i = 0; i < 10; ++i) {
-    auto* process = graph->CreateProcessCoordinationUnit(
-        CoordinationUnitID(CoordinationUnitType::kProcess,
-                           CoordinationUnitID::RANDOM_ID),
-        nullptr);
+    ProcessCoordinationUnitImpl* process = CreateProcessCU(graph.get());
     EXPECT_NE(nullptr, process);
 
     process->SetPID(i + 100);
@@ -47,11 +54,7 @@ TEST(CoordinationUnitGraphTest, FindOrCreateSystemCoordinationUnit) {
 TEST(CoordinationUnitGraphTest, GetProcessCoordinationUnitByPid) {
   CoordinationUnitGraph graph;
 
-  ProcessCoordinationUnitImpl* process = graph.CreateProcessCoordinationUnit(
-      CoordinationUnitID(CoordinationUnitType::kProcess,
-                         CoordinationUnitID::RANDOM_ID),
-      nullptr);
-
+  ProcessCoordinationUnitImpl* process = CreateProcessCU(&graph);
   EXPECT_EQ(base::kNullProcessId, process->process_id());
 
   static constexpr base::ProcessId kPid = 10;
@@ -63,6 +66,30 @@ TEST(CoordinationUnitGraphTest, GetProcessCoordinationUnitByPid) {
   process->Destruct();
 
   EXPECT_EQ(nullptr, graph.GetProcessCoordinationUnitByPid(12));
+}
+
+TEST(CoordinationUnitGraphTest, PIDReuse) {
+  // This test emulates what happens on Windows under aggressive PID reuse,
+  // where a process termination notification can be delayed until after the
+  // PID has been reused for a new process.
+  CoordinationUnitGraph graph;
+
+  static constexpr base::ProcessId kPid = 10;
+
+  ProcessCoordinationUnitImpl* process1 = CreateProcessCU(&graph);
+  ProcessCoordinationUnitImpl* process2 = CreateProcessCU(&graph);
+
+  process1->SetPID(kPid);
+  EXPECT_EQ(process1, graph.GetProcessCoordinationUnitByPid(kPid));
+
+  // The second registration for the same PID should override the first one.
+  process2->SetPID(kPid);
+  EXPECT_EQ(process2, graph.GetProcessCoordinationUnitByPid(kPid));
+
+  // The destruction of the first process CU shouldn't clear the PID
+  // registration.
+  process1->Destruct();
+  EXPECT_EQ(process2, graph.GetProcessCoordinationUnitByPid(kPid));
 }
 
 }  // namespace resource_coordinator
