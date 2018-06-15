bfc1732af22b4d0887fd1efafff1200c00d17350
Pete Williamson <petewil@chromium.org>
2018-06-15 23:16:09 +0000

    Add error logging and histogram for when file move fails.
    Looking at UMA for the P2P file sharing feature, the file move fails
    more often than it should.  Add some UMA to find out why.
    
    Bug: 758690
    
    Change-Id: Ifbf549ef2bb17d40c4190451d8089d3c42019f74
    Reviewed-on: https://chromium-review.googlesource.com/1101402
    Commit-Queue: Peter Williamson <petewil@chromium.org>
    Reviewed-by: Dmitry Titov <dimich@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567823}
    Former-commit-id: 5fb26806019bad289f0b799fbae4fb704ded7ccf

--- components/offline_pages/core/offline_page_archiver.cc
+++ components/offline_pages/core/offline_page_archiver.cc
@@ -4,10 +4,12 @@
 
 #include "components/offline_pages/core/offline_page_archiver.h"
 
+#include <errno.h>
 #include <utility>
 
 #include "base/bind.h"
 #include "base/files/file_util.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task_runner_util.h"
 #include "components/offline_pages/core/model/offline_page_model_taskified.h"
@@ -37,6 +39,10 @@ PublishArchiveResult MoveAndRegisterArchive(
   bool moved = base::Move(offline_page.file_path, new_file_path);
   if (!moved) {
     archive_result.move_result = SavePageResult::FILE_MOVE_FAILED;
+    DVLOG(0) << "OfflinePage publishing file move failure errno is " << errno
+             << __func__;
+    base::UmaHistogramSparse("OfflinePages.PublishArchive.MoveFileError",
+                             errno);
     return archive_result;
   }
 
