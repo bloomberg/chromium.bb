b6b32ab88da24181ef7eb3028e890a2607479863
John Z Wu <jzw@chromium.org>
2018-06-15 20:55:08 +0000

    Fix WebViewKvoTests by using WaitForWebViewContainingTextOrTimeout.
    Navigation may not occur right away after tapping on links, causing
    WaitForWebViewLoadCompletionOrTimeout to return immediately.
    
    Bug: 851472
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I5bf173f696dceeebc0118105a74fd0b19ebb27c9
    Reviewed-on: https://chromium-review.googlesource.com/1103008
    Commit-Queue: John Wu <jzw@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567782}
    Former-commit-id: b76fc5cc56b64e75d0ebd7a1c825653fb5a7a1aa

--- ios/web_view/test/web_view_kvo_inttest.mm
+++ ios/web_view/test/web_view_kvo_inttest.mm
@@ -5,7 +5,6 @@
 #import <ChromeWebView/ChromeWebView.h>
 #import <Foundation/Foundation.h>
 
-#include "base/ios/ios_util.h"
 #include "base/strings/stringprintf.h"
 #import "base/strings/sys_string_conversions.h"
 #import "ios/testing/wait_util.h"
@@ -28,10 +27,6 @@ typedef ios_web_view::WebViewIntTest WebViewKvoTest;
 
 // Tests that CWVWebView correctly reports |canGoBack| and |canGoForward| state.
 TEST_F(WebViewKvoTest, CanGoBackForward) {
-  // TODO(crbug.com/851472): WebViewKvoTest tests failing on iOS12.
-  if (base::ios::IsRunningOnIOS12OrLater())
-    return;
-
   Observer* back_observer = [[Observer alloc] init];
   [back_observer setObservedObject:web_view_ keyPath:@"canGoBack"];
 
@@ -42,7 +37,7 @@ TEST_F(WebViewKvoTest, CanGoBackForward) {
   ASSERT_FALSE(forward_observer.lastValue);
 
   // Define pages in reverse order so the links can reference the "next" page.
-  GURL page_3_url = GetUrlForPageWithTitle("Page 3");
+  GURL page_3_url = GetUrlForPageWithTitleAndBody("Page 3", "Body 3");
 
   std::string page_2_html =
       "<a id='link_2' href='" + page_3_url.spec() + "'>Link 2</a>";
@@ -59,47 +54,48 @@ TEST_F(WebViewKvoTest, CanGoBackForward) {
 
   // Navigate to page 2.
   EXPECT_TRUE(test::TapWebViewElementWithId(web_view_, @"link_1"));
-  ASSERT_TRUE(test::WaitForWebViewLoadCompletionOrTimeout(web_view_));
+  ASSERT_TRUE(
+      test::WaitForWebViewContainingTextOrTimeout(web_view_, @"Link 2"));
   EXPECT_TRUE([back_observer.lastValue boolValue]);
   EXPECT_FALSE([forward_observer.lastValue boolValue]);
 
   // Navigate back to page 1.
   [web_view_ goBack];
-  ASSERT_TRUE(test::WaitForWebViewLoadCompletionOrTimeout(web_view_));
+  ASSERT_TRUE(
+      test::WaitForWebViewContainingTextOrTimeout(web_view_, @"Link 1"));
   EXPECT_FALSE([back_observer.lastValue boolValue]);
   EXPECT_TRUE([forward_observer.lastValue boolValue]);
 
   // Navigate forward to page 2.
   [web_view_ goForward];
-  ASSERT_TRUE(test::WaitForWebViewLoadCompletionOrTimeout(web_view_));
+  ASSERT_TRUE(
+      test::WaitForWebViewContainingTextOrTimeout(web_view_, @"Link 2"));
   EXPECT_TRUE([back_observer.lastValue boolValue]);
   EXPECT_FALSE([forward_observer.lastValue boolValue]);
 
   // Navigate to page 3.
   EXPECT_TRUE(test::TapWebViewElementWithId(web_view_, @"link_2"));
-  ASSERT_TRUE(test::WaitForWebViewLoadCompletionOrTimeout(web_view_));
+  ASSERT_TRUE(
+      test::WaitForWebViewContainingTextOrTimeout(web_view_, @"Body 3"));
   EXPECT_TRUE([back_observer.lastValue boolValue]);
   EXPECT_FALSE([forward_observer.lastValue boolValue]);
 
   // Navigate back to page 2.
   [web_view_ goBack];
-  ASSERT_TRUE(test::WaitForWebViewLoadCompletionOrTimeout(web_view_));
+  ASSERT_TRUE(
+      test::WaitForWebViewContainingTextOrTimeout(web_view_, @"Link 2"));
   EXPECT_TRUE([back_observer.lastValue boolValue]);
   EXPECT_TRUE([forward_observer.lastValue boolValue]);
 }
 
 // Tests that CWVWebView correctly reports current |title|.
 TEST_F(WebViewKvoTest, Title) {
-  // TODO(crbug.com/851472): WebViewKvoTest tests failing on iOS12.
-  if (base::ios::IsRunningOnIOS12OrLater())
-    return;
-
   Observer* observer = [[Observer alloc] init];
   [observer setObservedObject:web_view_ keyPath:@"title"];
 
   NSString* page_2_title = @"Page 2";
-  GURL page_2_url =
-      GetUrlForPageWithTitle(base::SysNSStringToUTF8(page_2_title));
+  GURL page_2_url = GetUrlForPageWithTitleAndBody(
+      base::SysNSStringToUTF8(page_2_title), "Body 2");
 
   NSString* page_1_title = @"Page 1";
   std::string page_1_html = base::StringPrintf(
@@ -112,25 +108,23 @@ TEST_F(WebViewKvoTest, Title) {
 
   // Navigate to page 2.
   EXPECT_TRUE(test::TapWebViewElementWithId(web_view_, @"link_1"));
-  ASSERT_TRUE(test::WaitForWebViewLoadCompletionOrTimeout(web_view_));
+  ASSERT_TRUE(
+      test::WaitForWebViewContainingTextOrTimeout(web_view_, @"Body 2"));
   EXPECT_NSEQ(page_2_title, observer.lastValue);
 
   // Navigate back to page 1.
   [web_view_ goBack];
-  ASSERT_TRUE(test::WaitForWebViewLoadCompletionOrTimeout(web_view_));
+  ASSERT_TRUE(
+      test::WaitForWebViewContainingTextOrTimeout(web_view_, @"Link 1"));
   EXPECT_NSEQ(page_1_title, observer.lastValue);
 }
 
 // Tests that CWVWebView correctly reports |isLoading| value.
 TEST_F(WebViewKvoTest, Loading) {
-  // TODO(crbug.com/851472): WebViewKvoTest tests failing on iOS12.
-  if (base::ios::IsRunningOnIOS12OrLater())
-    return;
-
   Observer* observer = [[Observer alloc] init];
   [observer setObservedObject:web_view_ keyPath:@"loading"];
 
-  GURL page_2_url = GetUrlForPageWithTitle("Page 2");
+  GURL page_2_url = GetUrlForPageWithTitleAndBody("Page 2", "Body 2");
 
   std::string page_1_html = base::StringPrintf(
       "<a id='link_1' href='%s'>Link 1</a>", page_2_url.spec().c_str());
@@ -142,23 +136,21 @@ TEST_F(WebViewKvoTest, Loading) {
 
   // Navigate to page 2.
   EXPECT_TRUE(test::TapWebViewElementWithId(web_view_, @"link_1"));
-  ASSERT_TRUE(test::WaitForWebViewLoadCompletionOrTimeout(web_view_));
+  ASSERT_TRUE(
+      test::WaitForWebViewContainingTextOrTimeout(web_view_, @"Body 2"));
   EXPECT_TRUE([observer.previousValue boolValue]);
   EXPECT_FALSE([observer.lastValue boolValue]);
 
   // Navigate back to page 1.
   [web_view_ goBack];
-  ASSERT_TRUE(test::WaitForWebViewLoadCompletionOrTimeout(web_view_));
+  ASSERT_TRUE(
+      test::WaitForWebViewContainingTextOrTimeout(web_view_, @"Link 1"));
   EXPECT_TRUE([observer.previousValue boolValue]);
   EXPECT_FALSE([observer.lastValue boolValue]);
 }
 
 // Tests that CWVWebView correctly reports |visibleURL| and |lastCommittedURL|.
 TEST_F(WebViewKvoTest, URLs) {
-  // TODO(crbug.com/851472): WebViewKvoTest tests failing on iOS12.
-  if (base::ios::IsRunningOnIOS12OrLater())
-    return;
-
   Observer* last_committed_url_observer = [[Observer alloc] init];
   [last_committed_url_observer setObservedObject:web_view_
                                          keyPath:@"lastCommittedURL"];
@@ -166,7 +158,7 @@ TEST_F(WebViewKvoTest, URLs) {
   Observer* visible_url_observer = [[Observer alloc] init];
   [visible_url_observer setObservedObject:web_view_ keyPath:@"visibleURL"];
 
-  GURL page_2 = GetUrlForPageWithTitle("Page 2");
+  GURL page_2 = GetUrlForPageWithTitleAndBody("Page 2", "Body 2");
   NSURL* page_2_url = net::NSURLWithGURL(page_2);
 
   std::string page_1_html = base::StringPrintf(
@@ -179,19 +171,22 @@ TEST_F(WebViewKvoTest, URLs) {
   // |visibleURL| will update immediately
   EXPECT_NSEQ(page_1_url, visible_url_observer.lastValue);
 
-  ASSERT_TRUE(test::WaitForWebViewLoadCompletionOrTimeout(web_view_));
+  ASSERT_TRUE(
+      test::WaitForWebViewContainingTextOrTimeout(web_view_, @"Link 1"));
   EXPECT_NSEQ(page_1_url, last_committed_url_observer.lastValue);
   EXPECT_NSEQ(page_1_url, visible_url_observer.lastValue);
 
   // Navigate to page 2.
   EXPECT_TRUE(test::TapWebViewElementWithId(web_view_, @"link_1"));
-  ASSERT_TRUE(test::WaitForWebViewLoadCompletionOrTimeout(web_view_));
+  ASSERT_TRUE(
+      test::WaitForWebViewContainingTextOrTimeout(web_view_, @"Body 2"));
   EXPECT_NSEQ(page_2_url, last_committed_url_observer.lastValue);
   EXPECT_NSEQ(page_2_url, visible_url_observer.lastValue);
 
   // Navigate back to page 1.
   [web_view_ goBack];
-  ASSERT_TRUE(test::WaitForWebViewLoadCompletionOrTimeout(web_view_));
+  ASSERT_TRUE(
+      test::WaitForWebViewContainingTextOrTimeout(web_view_, @"Link 1"));
   EXPECT_NSEQ(page_1_url, last_committed_url_observer.lastValue);
   EXPECT_NSEQ(page_1_url, visible_url_observer.lastValue);
 }
