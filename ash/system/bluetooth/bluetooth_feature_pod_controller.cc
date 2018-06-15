04743a349891ed287663d4900c8106d22444e5a9
Tetsui Ohkubo <tetsui@chromium.org>
2018-06-15 17:00:57 +0000

    Unified: Improve Bluetooth button labels.
    This CL replaces UI strings of Bluetooth feature pod button in
    UnifiedSystemTray. It also shows the connected device name, and if there
    are multiple connected devices, it shows the number.
    
    TEST=manual
    BUG=813499
    
    Change-Id: Ic4d840586f2ff490a9c72b19c6679bb3c5c674a6
    Reviewed-on: https://chromium-review.googlesource.com/1102225
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567696}
    Former-commit-id: 3252db345d2ffe8a60ea63da5b2ae6cbfadfb887

--- ash/system/bluetooth/bluetooth_feature_pod_controller.cc
+++ ash/system/bluetooth/bluetooth_feature_pod_controller.cc
@@ -11,6 +11,7 @@
 #include "ash/system/tray/system_tray_notifier.h"
 #include "ash/system/unified/feature_pod_button.h"
 #include "ash/system/unified/unified_system_tray_controller.h"
+#include "base/i18n/number_formatting.h"
 #include "ui/base/l10n/l10n_util.h"
 
 namespace ash {
@@ -57,9 +58,36 @@ void BluetoothFeaturePodController::UpdateButton() {
   bool is_enabled =
       Shell::Get()->tray_bluetooth_helper()->GetBluetoothEnabled();
   button_->SetToggled(is_enabled);
-  button_->SetLabel(l10n_util::GetStringUTF16(
-      is_enabled ? IDS_ASH_STATUS_TRAY_BLUETOOTH_ENABLED
-                 : IDS_ASH_STATUS_TRAY_BLUETOOTH_DISABLED));
+
+  if (!is_enabled) {
+    button_->SetLabel(l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_BLUETOOTH));
+    button_->SetSubLabel(l10n_util::GetStringUTF16(
+        IDS_ASH_STATUS_TRAY_BLUETOOTH_DISABLED_SHORT));
+    return;
+  }
+
+  BluetoothDeviceList connected_devices;
+  for (const auto& device :
+       Shell::Get()->tray_bluetooth_helper()->GetAvailableBluetoothDevices()) {
+    if (device.connected)
+      connected_devices.push_back(device);
+    if (connected_devices.size() > 1)
+      break;
+  }
+
+  if (connected_devices.size() > 1) {
+    button_->SetLabel(l10n_util::GetStringUTF16(
+        IDS_ASH_STATUS_TRAY_BLUETOOTH_MULTIPLE_DEVICES_CONNECTED_LABEL));
+    button_->SetSubLabel(base::FormatNumber(connected_devices.size()));
+  } else if (connected_devices.size() == 1) {
+    button_->SetLabel(connected_devices.back().display_name);
+    button_->SetSubLabel(l10n_util::GetStringUTF16(
+        IDS_ASH_STATUS_TRAY_BLUETOOTH_DEVICE_CONNECTED_LABEL));
+  } else {
+    button_->SetLabel(l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_BLUETOOTH));
+    button_->SetSubLabel(
+        l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_BLUETOOTH_ENABLED_SHORT));
+  }
 }
 
 void BluetoothFeaturePodController::OnBluetoothRefresh() {
