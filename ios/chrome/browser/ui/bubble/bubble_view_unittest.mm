9f83a1a7bcdeb4f5d675e54a24ec041ac97e491b
Rohit Rao <rohitrao@chromium.org>
2018-06-15 21:05:46 +0000

    [ios] Fixes BubbleViewTest with the UIRefresh flag disabled.
    BUG=853330
    
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ia30d658545ce7d3eb3397f1d1e282cbb7d581dc7
    Reviewed-on: https://chromium-review.googlesource.com/1103117
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567789}
    Former-commit-id: 36ec54e24c89454587c466d697df3885daf3d9d2

--- ios/chrome/browser/ui/bubble/bubble_view_unittest.mm
+++ ios/chrome/browser/ui/bubble/bubble_view_unittest.mm
@@ -6,6 +6,7 @@
 
 #include "base/ios/ios_util.h"
 #include "base/logging.h"
+#include "ios/chrome/browser/ui/ui_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/gtest_mac.h"
 #include "testing/platform_test.h"
@@ -47,8 +48,13 @@ TEST_F(BubbleViewTest, BubbleSizeShortText) {
   CGSize bubbleSize = [bubble sizeThatFits:maxSize_];
   // Since the label is shorter than the minimum line width, expect the bubble
   // to be the minimum width and accommodate one line of text.
-  EXPECT_NEAR(58.0f, bubbleSize.width, 1.0f);
-  EXPECT_NEAR(65.0f, bubbleSize.height, 1.0f);
+  if (IsUIRefreshPhase1Enabled()) {
+    EXPECT_NEAR(58.0f, bubbleSize.width, 1.0f);
+    EXPECT_NEAR(65.0f, bubbleSize.height, 1.0f);
+  } else {
+    EXPECT_NEAR(52.0f, bubbleSize.width, 1.0f);
+    EXPECT_NEAR(53.5f, bubbleSize.height, 1.0f);
+  }
 }
 
 // Test |sizeThatFits| given text that should wrap onto multiple lines.
@@ -59,12 +65,19 @@ TEST_F(BubbleViewTest, BubbleSizeMultipleLineText) {
   CGSize bubbleSize = [bubble sizeThatFits:maxSize_];
 
   // The bubble should fit the label, which contains two lines of text.
-  if (base::ios::IsRunningOnIOS11OrLater()) {
+  if (!IsUIRefreshPhase1Enabled()) {
+    EXPECT_NEAR(339.0f, bubbleSize.width, 1.0f);
+  } else if (base::ios::IsRunningOnIOS11OrLater()) {
     EXPECT_NEAR(329.0f, bubbleSize.width, 1.0f);
   } else {
     EXPECT_NEAR(402.0f, bubbleSize.width, 1.0f);
   }
-  EXPECT_NEAR(83.0f, bubbleSize.height, 1.0f);
+
+  if (IsUIRefreshPhase1Enabled()) {
+    EXPECT_NEAR(83.0f, bubbleSize.height, 1.0f);
+  } else {
+    EXPECT_NEAR(72.5f, bubbleSize.height, 1.0f);
+  }
 }
 
 // Test that the accessibility label matches the display text.
