c477c62594dd2d53bd00991e044bf9e7f4aa48fb
Ryan Hansberry <hansberry@chromium.org>
2018-06-15 04:29:17 +0000

    [CrOS Multidevice] ChromeProximityAuthClient: conditionally use DeviceSyncClient.
    ChromeProximityAuthClient now uses either CryptAuthService or DeviceSyncClient
    to get remote device info or perform Cryptauth calls, depending on whether the
    chromeos::features::kMultiDeviceApi is enabled. Once the migration to DeviceSyncClient
    has been completed across all of Smart Lock, code that references CryptAuthService will
    be removed.
    
    This is one of many future CLs which are migrating Smart Lock to use
    DeviceSync Mojo API instead of directly using the CryptAuth API.
    
    TBR=jhawkins@chromium.org
    
    Bug: 824568, 752273, 848956
    Change-Id: If78959aa2a0ef6dc9433bca29649ab2d837114a1
    Reviewed-on: https://chromium-review.googlesource.com/1102161
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567546}
    Former-commit-id: d687ea9e30cde1863a51645a974841010a95c9a1

--- chrome/browser/chromeos/login/easy_unlock/chrome_proximity_auth_client.cc
+++ chrome/browser/chromeos/login/easy_unlock/chrome_proximity_auth_client.cc
@@ -11,6 +11,7 @@
 #include "base/version.h"
 #include "build/build_config.h"
 #include "chrome/browser/chromeos/cryptauth/chrome_cryptauth_service_factory.h"
+#include "chrome/browser/chromeos/device_sync/device_sync_client_factory.h"
 #include "chrome/browser/chromeos/login/easy_unlock/easy_unlock_service.h"
 #include "chrome/browser/chromeos/login/easy_unlock/easy_unlock_service_regular.h"
 #include "chrome/browser/chromeos/login/easy_unlock/easy_unlock_service_signin_chromeos.h"
@@ -18,7 +19,9 @@
 #include "chrome/browser/profiles/profile_window.h"
 #include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
 #include "chrome/browser/signin/signin_manager_factory.h"
+#include "chromeos/chromeos_features.h"
 #include "chromeos/components/proximity_auth/logging/logging.h"
+#include "chromeos/services/device_sync/public/cpp/device_sync_client.h"
 #include "components/cryptauth/cryptauth_client_impl.h"
 #include "components/cryptauth/cryptauth_device_manager.h"
 #include "components/cryptauth/cryptauth_enrollment_manager.h"
@@ -72,7 +75,7 @@ void ChromeProximityAuthClient::GetChallengeForUserAndDevice(
   EasyUnlockService* easy_unlock_service = EasyUnlockService::Get(profile_);
   if (easy_unlock_service->GetType() == EasyUnlockService::TYPE_REGULAR) {
     PA_LOG(ERROR) << "Unable to get challenge when user is logged in.";
-    callback.Run(std::string());
+    callback.Run(std::string() /* challenge */);
     return;
   }
 
@@ -92,37 +95,53 @@ ChromeProximityAuthClient::GetPrefManager() {
 
 std::unique_ptr<cryptauth::CryptAuthClientFactory>
 ChromeProximityAuthClient::CreateCryptAuthClientFactory() {
+  DCHECK(!base::FeatureList::IsEnabled(chromeos::features::kMultiDeviceApi));
   return GetCryptAuthService()->CreateCryptAuthClientFactory();
 }
 
 cryptauth::DeviceClassifier ChromeProximityAuthClient::GetDeviceClassifier() {
+  DCHECK(!base::FeatureList::IsEnabled(chromeos::features::kMultiDeviceApi));
   return GetCryptAuthService()->GetDeviceClassifier();
 }
 
 std::string ChromeProximityAuthClient::GetAccountId() {
+  DCHECK(!base::FeatureList::IsEnabled(chromeos::features::kMultiDeviceApi));
   return GetCryptAuthService()->GetAccountId();
 }
 
 cryptauth::CryptAuthEnrollmentManager*
 ChromeProximityAuthClient::GetCryptAuthEnrollmentManager() {
+  DCHECK(!base::FeatureList::IsEnabled(chromeos::features::kMultiDeviceApi));
   return GetCryptAuthService()->GetCryptAuthEnrollmentManager();
 }
 
 cryptauth::CryptAuthDeviceManager*
 ChromeProximityAuthClient::GetCryptAuthDeviceManager() {
+  DCHECK(!base::FeatureList::IsEnabled(chromeos::features::kMultiDeviceApi));
   return GetCryptAuthService()->GetCryptAuthDeviceManager();
 }
 
+std::string ChromeProximityAuthClient::GetLocalDevicePublicKey() {
+  if (base::FeatureList::IsEnabled(chromeos::features::kMultiDeviceApi)) {
+    return GetDeviceSyncClient()->GetLocalDeviceMetadata()->public_key();
+  } else {
+    cryptauth::LocalDeviceDataProvider provider(GetCryptAuthService());
+    std::string local_public_key;
+    provider.GetLocalDeviceData(&local_public_key, nullptr);
+    return local_public_key;
+  }
+}
+
 cryptauth::CryptAuthService* ChromeProximityAuthClient::GetCryptAuthService() {
+  DCHECK(!base::FeatureList::IsEnabled(chromeos::features::kMultiDeviceApi));
   return ChromeCryptAuthServiceFactory::GetInstance()->GetForBrowserContext(
       profile_);
 }
 
-std::string ChromeProximityAuthClient::GetLocalDevicePublicKey() {
-  cryptauth::LocalDeviceDataProvider provider(GetCryptAuthService());
-  std::string local_public_key;
-  provider.GetLocalDeviceData(&local_public_key, nullptr);
-  return local_public_key;
+device_sync::DeviceSyncClient*
+ChromeProximityAuthClient::GetDeviceSyncClient() {
+  DCHECK(base::FeatureList::IsEnabled(chromeos::features::kMultiDeviceApi));
+  return device_sync::DeviceSyncClientFactory::GetForProfile(profile_);
 }
 
 }  // namespace chromeos
