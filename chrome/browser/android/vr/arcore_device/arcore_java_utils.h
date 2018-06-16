6467cde2d4fff442c875f59b7fa7010631a0bce0
Iker Jamardo <ijamardo@chromium.org>
2018-06-16 15:49:12 +0000

    Reland "Check if ARCore is installed or needs update."
    This reverts commit 95f5ee7515f1d82511dbf65f37eb41971cc3837f [formerly 91a0deb09438f0e980fb5cdf2a71faf48643cd87].
    
    Reason for revert: Added conditional checks for strings.
    
    Original change's description:
    > Revert "Check if ARCore is installed or needs update."
    >
    > This reverts commit 3c929fe4238cae8cc6dd03d8cacf8c882a26fe7c [formerly 703de90c00513e3800a9524fd9338424fdd85c2a].
    >
    > Reason for revert: lint errors (http://crbug.com/852189)
    >
    > Original change's description:
    > > Check if ARCore is installed or needs update.
    > >
    > > When requesting a session and after the camera permission is confirmed
    > > to have been granted, check if ARCore is installed and if the version
    > > used in Chromium is compatible with it. If ARCore is not installed
    > > or an update is required, a prompt will be shown.
    > >
    > > Bug: 838954
    > > Change-Id: I35ae39041e7958743bba9cc1c71f5af817bf4240
    > > Reviewed-on: https://chromium-review.googlesource.com/1074494
    > > Reviewed-by: Theresa <twellington@chromium.org>
    > > Reviewed-by: agrieve <agrieve@chromium.org>
    > > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > > Reviewed-by: Evan Stade <estade@chromium.org>
    > > Reviewed-by: Klaus Weidner <klausw@chromium.org>
    > > Commit-Queue: Iker Jamardo <ijamardo@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#566589}
    >
    > TBR=ddorwin@chromium.org,vollick@chromium.org,pkasting@chromium.org,yfriedman@chromium.org,bshe@chromium.org,mthiesse@chromium.org,estade@chromium.org,twellington@chromium.org,agrieve@chromium.org,klausw@chromium.org,billorr@chromium.org,lincolnfrog@chromium.org,ijamardo@chromium.org
    >
    > Change-Id: I1f88a1de68d4929a842a4c7bf95aa90a7f48bcba
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 838954
    > Bug: 852189
    > Reviewed-on: https://chromium-review.googlesource.com/1098335
    > Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    > Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#566635}
    
    
    Bug: 838954, 852189
    Change-Id: I33fd3d06d749e3f522e3eb4403462112f67295f3
    Reviewed-on: https://chromium-review.googlesource.com/1100487
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Commit-Queue: Iker Jamardo <ijamardo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#567890}
    Former-commit-id: 7f8b47da922df5359de161f1cb5e926e63c339bb

--- chrome/browser/android/vr/arcore_device/arcore_java_utils.h
+++ chrome/browser/android/vr/arcore_device/arcore_java_utils.h
@@ -7,14 +7,32 @@
 
 #include <jni.h>
 #include "base/android/scoped_java_ref.h"
+#include "base/memory/weak_ptr.h"
+
+namespace device {
+class ARCoreDevice;
+}
 
 namespace vr {
 
 class ArCoreJavaUtils {
  public:
   static base::android::ScopedJavaLocalRef<jobject> GetApplicationContext();
-
   static bool EnsureLoaded();
+  explicit ArCoreJavaUtils(device::ARCoreDevice* arcore_device);
+  ~ArCoreJavaUtils();
+  bool ShouldRequestInstallSupportedArCore();
+  void RequestInstallSupportedArCore(
+      base::android::ScopedJavaLocalRef<jobject> j_tab_android);
+
+  // Method called from the Java side
+  void OnRequestInstallSupportedArCoreCanceled(
+      JNIEnv* env,
+      const base::android::JavaParamRef<jobject>& obj);
+
+ private:
+  device::ARCoreDevice* arcore_device_;
+  base::android::ScopedJavaGlobalRef<jobject> j_arcore_java_utils_;
 };
 
 }  // namespace vr
