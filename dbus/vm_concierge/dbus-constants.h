f4c6660897af41ccede6f9bed6e9473e30b77906
Jeffrey Kardatzke <jkardatzke@google.com>
2018-06-18 10:40:13 -0700

    Removed unused protobufs/constants for concierge
    This removes dead code from breaking half of concierge into cicerone.
    All code being removed is no longer referenced in Chrome or Chrome OS.
    
    BUG=chromium:842845
    TEST=Builds
    CQ-DEPEND=CL:1104872
    
    Change-Id: I008f87a81e809417d5137ce8ab1236b261674b95
    Reviewed-on: https://chromium-review.googlesource.com/1104875
    Commit-Ready: Jeffrey Kardatzke <jkardatzke@google.com>
    Tested-by: Jeffrey Kardatzke <jkardatzke@google.com>
    Reviewed-by: Dan Erat <derat@chromium.org>

--- dbus/vm_concierge/dbus-constants.h
+++ dbus/vm_concierge/dbus-constants.h
@@ -20,12 +20,8 @@ const char kCreateDiskImageMethod[] = "CreateDiskImage";
 const char kDestroyDiskImageMethod[] = "DestroyDiskImage";
 const char kListVmDisksMethod[] = "ListVmDisks";
 const char kStartContainerMethod[] = "StartContainer";
-const char kLaunchContainerApplicationMethod[] = "LaunchContainerApplication";
-const char kGetContainerAppIconMethod[] = "GetContainerAppIcon";
 const char kGetContainerSshKeysMethod[] = "GetContainerSshKeys";
-const char kLaunchVshdMethod[] = "LaunchVshd";
 
-const char kContainerStartedSignal[] = "ContainerStarted";
 const char kContainerStartupFailedSignal[] = "ContainerStartupFailed";
 
 }  // namespace concierge
