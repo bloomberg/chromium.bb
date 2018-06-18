c0f0e0edaa3e69da6490b15b6983fd713bdd986c
Sky Malice <skym@google.com>
2018-06-18 23:17:35 +0000

    [Feed] Notify Feed when Android foregrounds.
    To facilitate notifying feed of foregrounds, added FeedEventReporter
    to act as the entry point. This class has a dummy clone because it is
    called outside of Feed code.
    
    Bug: 831648
    Change-Id: Id25205e33b03d20cd63c6b97920d2cb90ac3d4c2
    Reviewed-on: https://chromium-review.googlesource.com/1095774
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568225}
    Former-commit-id: 6c164d6904d99210a5c6332ee25f6938fcf72f69

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedProcessScopeFactory.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedProcessScopeFactory.java
@@ -15,31 +15,48 @@ import org.chromium.chrome.browser.profiles.Profile;
 import java.util.concurrent.Executors;
 
 /**
- * Holds singleton {@link FeedProcessScope}.
+ * Holds singleton {@link FeedProcessScope} and some of the scope's host implementations.
  */
 public class FeedProcessScopeFactory {
     private static FeedProcessScope sFeedProcessScope;
+    private static FeedSchedulerBridge sFeedSchedulerBridge;
 
     /**
      * @return The shared {@link FeedProcessScope} instance.
      */
     public static FeedProcessScope getFeedProcessScope() {
         if (sFeedProcessScope == null) {
-            Profile profile = Profile.getLastUsedProfile().getOriginalProfile();
-            Configuration configHostApi =
-                    new Configuration.Builder()
-                            .put(ConfigKey.FEED_SERVER_HOST, "https://www.google.com")
-                            .put(ConfigKey.FEED_SERVER_PATH_AND_PARAMS,
-                                    "/httpservice/noretry/NowStreamService/FeedQuery")
-                            .put(ConfigKey.SESSION_LIFETIME_MS, 300000L)
-                            .build();
-            sFeedProcessScope =
-                    new FeedProcessScope
-                            .Builder(configHostApi, Executors.newSingleThreadExecutor(),
-                                    new LoggingApiImpl(), new FeedNetworkBridge(profile),
-                                    new FeedSchedulerBridge(profile), DebugBehavior.SILENT)
-                            .build();
+            initialize();
         }
         return sFeedProcessScope;
+    }
+
+    /*
+     * @return The {@link FeedSchedulerBridge} that was given to the {@link FeedProcessScope}.
+     */
+    public static FeedSchedulerBridge getFeedSchedulerBridge() {
+        if (sFeedSchedulerBridge == null) {
+            initialize();
+        }
+        return sFeedSchedulerBridge;
+    }
+
+    private static void initialize() {
+        assert sFeedSchedulerBridge == null && sFeedProcessScope == null;
+        Profile profile = Profile.getLastUsedProfile().getOriginalProfile();
+        Configuration configHostApi =
+                new Configuration.Builder()
+                        .put(ConfigKey.FEED_SERVER_HOST, "https://www.google.com")
+                        .put(ConfigKey.FEED_SERVER_PATH_AND_PARAMS,
+                                "/httpservice/noretry/NowStreamService/FeedQuery")
+                        .put(ConfigKey.SESSION_LIFETIME_MS, 300000L)
+                        .build();
+        sFeedSchedulerBridge = new FeedSchedulerBridge(profile);
+        sFeedProcessScope = new FeedProcessScope
+                                    .Builder(configHostApi, Executors.newSingleThreadExecutor(),
+                                            new LoggingApiImpl(), new FeedNetworkBridge(profile),
+                                            sFeedSchedulerBridge, DebugBehavior.SILENT)
+                                    .build();
+        sFeedSchedulerBridge.setRequestManager(sFeedProcessScope.getRequestManager());
     }
 }
