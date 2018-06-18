3c8f475a6fc3a28833196701bcd00c27e0fcaf52
Paul Miller <paulmiller@google.com>
2018-06-18 22:35:28 +0000

    WebView instrumentation test shell: Use try with StrictMode
    BUG=628650
    
    Change-Id: Ia78bfe6ad2d12362cb85bcc4580d907fe1ed6b47
    Reviewed-on: https://chromium-review.googlesource.com/1105181
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Paul Miller <paulmiller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568202}
    Former-commit-id: 19dcc411896aea71d39693997821612f85ec702e

--- android_webview/test/shell/src/org/chromium/android_webview/test/AwTestRunnerActivity.java
+++ android_webview/test/shell/src/org/chromium/android_webview/test/AwTestRunnerActivity.java
@@ -7,7 +7,6 @@ package org.chromium.android_webview.test;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
@@ -15,6 +14,7 @@ import android.widget.LinearLayout;
 import org.chromium.android_webview.AwBrowserProcess;
 import org.chromium.android_webview.shell.AwShellResourceProvider;
 import org.chromium.base.ContextUtils;
+import org.chromium.base.StrictModeContext;
 
 /**
  * This is a lightweight activity for tests that only require WebView functionality.
@@ -31,9 +31,9 @@ public class AwTestRunnerActivity extends Activity {
 
         AwShellResourceProvider.registerResources(this);
         ContextUtils.initApplicationContext(getApplicationContext());
-        StrictMode.ThreadPolicy oldPolicy = StrictMode.allowThreadDiskReads();
-        AwBrowserProcess.loadLibrary(null);
-        StrictMode.setThreadPolicy(oldPolicy);
+        try (StrictModeContext ctx = StrictModeContext.allowDiskReads()) {
+            AwBrowserProcess.loadLibrary(null);
+        }
 
         mLinearLayout = new LinearLayout(this);
         mLinearLayout.setOrientation(LinearLayout.VERTICAL);
