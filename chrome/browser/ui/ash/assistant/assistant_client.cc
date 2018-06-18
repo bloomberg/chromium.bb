42720c183f46783824115b2eb87000235973b95f
Yue Li <updowndota@chromium.org>
2018-06-18 16:51:16 +0000

    Add entrypoint for Assistant OptIn flow
    Bug: b/78191741
    Change-Id: I0bd7969e411adbace1f76eea8ba0d0d964312afe
    Reviewed-on: https://chromium-review.googlesource.com/1099729
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568039}
    Former-commit-id: bf89f72a96c084c8e7cdf638cea24d1aeb0c8ee3

--- chrome/browser/ui/ash/assistant/assistant_client.cc
+++ chrome/browser/ui/ash/assistant/assistant_client.cc
@@ -9,6 +9,7 @@
 #include "ash/public/interfaces/voice_interaction_controller.mojom.h"
 #include "chrome/browser/chromeos/arc/voice_interaction/voice_interaction_controller_client.h"
 #include "chrome/browser/ui/ash/assistant/assistant_image_downloader.h"
+#include "chrome/browser/ui/ash/assistant/assistant_setup.h"
 #include "chrome/browser/ui/ash/assistant/web_contents_manager.h"
 #include "chromeos/services/assistant/public/mojom/constants.mojom.h"
 #include "services/service_manager/public/cpp/connector.h"
@@ -59,8 +60,8 @@ void AssistantClient::MaybeInit(service_manager::Connector* connector) {
 
   assistant_image_downloader_ =
       std::make_unique<AssistantImageDownloader>(connector);
-
   web_contents_manager_ = std::make_unique<WebContentsManager>(connector);
+  assistant_setup_ = std::make_unique<AssistantSetup>(connector);
 }
 
 void AssistantClient::OnAssistantStatusChanged(bool running) {
