3dad4bddef12457cf113072506debd0c14ff6a75
Oksana Zhuravlova <oksamyt@chromium.org>
2018-06-18 20:50:54 +0000

    Create a shared component for gamepad_mojom_traits.{h,cc}
    This change moves gamepad_mojom_traits to device/gamepad/public/cpp and
    creates a shared component for them to be specified as a dependency in
    gamepad.typemap. This will enable GamepadSharedMemoryReader to depend on
    the traits after the move to blink.
    
    Bug: 612330
    Change-Id: I688a981313ec90a8c5bf909b0df4bb3e1f711388
    Reviewed-on: https://chromium-review.googlesource.com/1103612
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568154}
    Former-commit-id: 19de023d406fad43277cb4007154d8ac700baf93

--- device/gamepad/public/cpp/gamepad_mojom_traits.cc
+++ device/gamepad/public/cpp/gamepad_mojom_traits.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "device/gamepad/public/mojom/gamepad_mojom_traits.h"
+#include "device/gamepad/public/cpp/gamepad_mojom_traits.h"
 
 #include "base/containers/span.h"
 
