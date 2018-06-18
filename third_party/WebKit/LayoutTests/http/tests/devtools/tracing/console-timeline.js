a15664a1658cda70e0f32063a6d825ed0f83c9c6
Dominic Farolino <domfarolino@gmail.com>
2018-06-18 14:23:20 +0000

    Prepare to remove deprecated console methods
    This CL removes layout tests for deprecated console methods as well as
    removes expectation files for failing console WPTs that assert the
    non-existence of these methods. These need removed so that the v8
    auto-roll with the removed console methods don't fail the chromium
    layout tests. Also this CL removes the test expectations for console
    WPTs that assert the non-existence of these deprecated methods, but
    marks the tests as skipped for now, until the v8 changes are in
    chromium.
    
    R=kozyatinskiy@chromium.org
    
    Bug: 706804
    Change-Id: If11e0c6a26d6ccc29474fb0148900d034cd40b86
    Reviewed-on: https://chromium-review.googlesource.com/1103937
    Reviewed-by: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Commit-Queue: Dominic Farolino <domfarolino@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#567994}
    Former-commit-id: fd8a54959dd463f6fcb91c39e33edbfb26a4bd3d

--- third_party/WebKit/LayoutTests/http/tests/devtools/tracing/console-timeline.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/tracing/console-timeline.js
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 (async function() {
-  TestRunner.addResult(`Tests console.timeline and timelineEnd commands.\n`);
+  TestRunner.addResult(`Tests console.time and timeEnd methods.\n`);
   await TestRunner.loadModule('console_test_runner');
   await TestRunner.loadModule('performance_test_runner');
   await TestRunner.showPanel('timeline');
@@ -11,70 +11,70 @@
       function startStopTimeline()
       {
           console.timeStamp("timestamp 0");
-          console.timeline("one");
+          console.time("one");
           console.timeStamp("timestamp 1");
-          console.timelineEnd("one");
+          console.timeEnd("one");
           console.timeStamp("timestamp 2");
       }
 
       function startStopMultiple()
       {
           console.timeStamp("timestamp 0");
-          console.timeline("one");
+          console.time("one");
           console.timeStamp("timestamp 1");
-          console.timeline("one");
-          console.timeline("two");
-          console.timeline("two");
-          console.timelineEnd("two");
+          console.time("one");
+          console.time("two");
+          console.time("two");
+          console.timeEnd("two");
           console.timeStamp("timestamp 2");
-          console.timelineEnd("one");
+          console.timeEnd("one");
           console.timeStamp("timestamp 3");
-          console.timelineEnd("two");
+          console.timeEnd("two");
           console.timeStamp("timestamp 4");
-          console.timelineEnd("one");
+          console.timeEnd("one");
           console.timeStamp("timestamp 5");
       }
 
       function startMultiple()
       {
           console.timeStamp("timestamp 0");
-          console.timeline("one");
+          console.time("one");
           console.timeStamp("timestamp 1");
-          console.timeline("two");
+          console.time("two");
           console.timeStamp("timestamp 2");
       }
 
       function stopTwo()
       {
           console.timeStamp("timestamp 3");
-          console.timelineEnd("two");
+          console.timeEnd("two");
           console.timeStamp("timestamp 4");
       }
 
       function stopOne()
       {
           console.timeStamp("timestamp 5");
-          console.timelineEnd("one");
+          console.timeEnd("one");
           console.timeStamp("timestamp 6 - FAIL");
       }
 
       function stopUnknown()
       {
           console.timeStamp("timestamp 0");
-          console.timeline("one");
+          console.time("one");
           console.timeStamp("timestamp 1");
-          console.timelineEnd("two");
+          console.timeEnd("two");
           console.timeStamp("timestamp 2");
-          console.timelineEnd("one");
+          console.timeEnd("one");
           console.timeStamp("timestamp 3");
       }
 
       function startTimeline()
       {
           console.timeStamp("timestamp 0");
-          console.timeline("one");
+          console.time("one");
           console.timeStamp("timestamp 1");
-          console.timeline("two");
+          console.time("two");
           console.timeStamp("timestamp 2");
       }
   `);
