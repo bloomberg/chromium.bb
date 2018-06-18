f75a57390ad2439f24d546e5e7538519b193a2cf
Sonny Sasaka <sonnysasaka@chromium.org>
2018-06-18 20:23:12 +0000

    chrome.bluetoothSocket: Fix regression in send()
    In https://crrev.com/c/997098, params_ was changed to a local variable,
    but it needs to last longer than that since net::WrappedIOBuffer may use
    the data after the local variable goes out of scope.
    
    This CL changed it back to be an instance variable.
    
    Bug: 851799
    Change-Id: I392f8acaef4c6473d6ea4fbee7209445aa09112e
    Reviewed-on: https://chromium-review.googlesource.com/1103676
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Sonny Sasaka <sonnysasaka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568137}
    Former-commit-id: c5c6320f80159dc41dffc3cfbf0298925c7dcf1b

--- extensions/browser/api/bluetooth_socket/bluetooth_socket_api.cc
+++ extensions/browser/api/bluetooth_socket/bluetooth_socket_api.cc
@@ -545,13 +545,13 @@ BluetoothSocketSendFunction::~BluetoothSocketSendFunction() {}
 ExtensionFunction::ResponseAction BluetoothSocketSendFunction::Run() {
   DCHECK_CURRENTLY_ON(work_thread_id());
 
-  auto params = bluetooth_socket::Send::Params::Create(*args_);
-  EXTENSION_FUNCTION_VALIDATE(params.get());
+  params_ = bluetooth_socket::Send::Params::Create(*args_);
+  EXTENSION_FUNCTION_VALIDATE(params_.get());
 
-  io_buffer_size_ = params->data.size();
-  io_buffer_ = new net::WrappedIOBuffer(params->data.data());
+  io_buffer_size_ = params_->data.size();
+  io_buffer_ = new net::WrappedIOBuffer(params_->data.data());
 
-  BluetoothApiSocket* socket = GetSocket(params->socket_id);
+  BluetoothApiSocket* socket = GetSocket(params_->socket_id);
   if (!socket)
     return RespondNow(Error(kSocketNotFoundError));
 
