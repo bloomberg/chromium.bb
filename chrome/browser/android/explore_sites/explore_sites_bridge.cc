71c11abe9e915013fe30b8f33073cdb6e57758ce
Yafei Duan <romax@chromium.org>
2018-06-19 19:42:13 +0000

    [ExploreSites] Adds a fetcher for the NTP JSON schema.
    This doesn't decode the JSON, just grabs it from the appropriate
    endpoint.
    
    The CL was originally uploaded by dewittj@. This CL is fixing the test
    failure and re-uploading to land since dewittj@ is out of office.
    
    Bug: 852075
    Change-Id: Ib8adc7cad661665c884a2d058c27c99f40ac0a58
    Reviewed-on: https://chromium-review.googlesource.com/1105018
    Commit-Queue: Yafei Duan <romax@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568571}
    Former-commit-id: 66f0831c883bad092128e4a6e7c574e07e0fc5b7

--- chrome/browser/android/explore_sites/explore_sites_bridge.cc
+++ chrome/browser/android/explore_sites/explore_sites_bridge.cc
@@ -7,37 +7,58 @@
 #include "base/android/callback_android.h"
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
+#include "base/android/scoped_java_ref.h"
 #include "base/bind.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_android.h"
 #include "jni/ExploreSitesBridge_jni.h"
+#include "jni/ExploreSitesCategoryTile_jni.h"
 #include "services/network/public/cpp/simple_url_loader.h"
 
 namespace explore_sites {
 
-static jlong JNI_ExploreSitesBridge_Init(
-    JNIEnv* env,
-    const base::android::JavaParamRef<jobject>& obj,
-    const base::android::JavaParamRef<jobject>& j_profile) {
-  ExploreSitesBridge* explore_sites_bridge = new ExploreSitesBridge(j_profile);
-  return reinterpret_cast<intptr_t>(explore_sites_bridge);
+using base::android::JavaParamRef;
+using base::android::JavaRef;
+using base::android::ScopedJavaLocalRef;
+using base::android::ScopedJavaGlobalRef;
+
+namespace {
+
+void GotNTPCategoriesFromJson(
+    const ScopedJavaGlobalRef<jobject>& j_callback_ref,
+    const ScopedJavaGlobalRef<jobject>& j_result_ref,
+    std::unique_ptr<NTPJsonFetcher> fetcher,
+    std::unique_ptr<NTPCatalog> catalog) {
+  JNIEnv* env = base::android::AttachCurrentThread();
+  if (catalog) {
+    for (NTPCatalog::Category category : catalog->categories) {
+      Java_ExploreSitesCategoryTile_createInList(
+          env, j_result_ref,
+          base::android::ConvertUTF8ToJavaString(env, category.id),
+          base::android::ConvertUTF8ToJavaString(env, category.icon_url.spec()),
+          base::android::ConvertUTF8ToJavaString(env, category.title));
+    }
+  }
+
+  base::android::RunCallbackAndroid(j_callback_ref, j_result_ref);
 }
 
-ExploreSitesBridge::ExploreSitesBridge(
-    const base::android::JavaRef<jobject>& j_profile)
-    : weak_ptr_factory_(this) {}
+}  // namespace
 
-void ExploreSitesBridge::Destroy(
+// static
+void JNI_ExploreSitesBridge_GetNtpCategories(
     JNIEnv* env,
-    const base::android::JavaParamRef<jobject>& obj) {
-  delete this;
+    const JavaParamRef<jclass>& j_caller,
+    const JavaParamRef<jobject>& j_profile,
+    const JavaParamRef<jobject>& j_result_obj,
+    const JavaParamRef<jobject>& j_callback_obj) {
+  NTPJsonFetcher* ntp_fetcher =
+      new NTPJsonFetcher(ProfileAndroid::FromProfileAndroid(j_profile));
+
+  ntp_fetcher->Start(base::BindOnce(
+      &GotNTPCategoriesFromJson, ScopedJavaGlobalRef<jobject>(j_callback_obj),
+      ScopedJavaGlobalRef<jobject>(j_result_obj),
+      base::WrapUnique(ntp_fetcher)));
 }
-
-void ExploreSitesBridge::GetNtpCategories(
-    JNIEnv* env,
-    const base::android::JavaParamRef<jobject>& obj,
-    const base::android::JavaParamRef<jstring>& url,
-    const base::android::JavaParamRef<jobject>& j_result_obj,
-    const base::android::JavaParamRef<jobject>& j_callback_obj) {}
-
-ExploreSitesBridge::~ExploreSitesBridge() {}
 
 }  // namespace explore_sites
