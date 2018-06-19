ab8379a8a2a67c2174e64364828424a496306717
Mike Klein <mtklein@chromium.org>
2018-06-19 00:19:16 +0000

    remove Skia flags that do nothing
    Change-Id: I70485ca47cb75157bc8945314561a3b6ff6ec104
    Reviewed-on: https://chromium-review.googlesource.com/1104801
    Commit-Queue: Mike Klein <mtklein@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568250}
    Former-commit-id: 925c5140874a047dbbf544bcfdbb85da4477822d

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -61,50 +61,10 @@
     #define SK_DEBUG
 #endif
 
-/*  If, in debugging mode, Skia needs to stop (presumably to invoke a debugger)
-    it will call SK_CRASH(). If this is not defined it, it is defined in
-    SkPostConfig.h to write to an illegal address
- */
-//#define SK_CRASH() *(int *)(uintptr_t)0 = 0
-
-
-/*  preconfig will have attempted to determine the endianness of the system,
-    but you can change these mutually exclusive flags here.
- */
-//#define SK_CPU_BENDIAN
-//#define SK_CPU_LENDIAN
-
 /*  Define this to provide font subsetter for font subsetting when generating
     PDF documents.
  */
 #define SK_PDF_USE_SFNTLY
-
-/*  To write debug messages to a console, skia will call SkDebugf(...) following
-    printf conventions (e.g. const char* format, ...). If you want to redirect
-    this to something other than printf, define yours here
- */
-//#define SkDebugf(...)  MyFunction(__VA_ARGS__)
-
-
-/*  If SK_DEBUG is defined, then you can optionally define SK_SUPPORT_UNITTEST
-    which will run additional self-tests at startup. These can take a long time,
-    so this flag is optional.
- */
-#ifdef SK_DEBUG
-#define SK_SUPPORT_UNITTEST
-#endif
-
-/* If your system embeds skia and has complex event logging, define this
-   symbol to name a file that maps the following macros to your system's
-   equivalents:
-       SK_TRACE_EVENT0(event)
-       SK_TRACE_EVENT1(event, name1, value1)
-       SK_TRACE_EVENT2(event, name1, value1, name2, value2)
-   src/utils/SkDebugTrace.h has a trivial implementation that writes to
-   the debug output stream. If SK_USER_TRACE_INCLUDE_FILE is not defined,
-   SkTrace.h will define the above three macros to do nothing.
-*/
-#undef SK_USER_TRACE_INCLUDE_FILE
 
 // ===== Begin Chrome-specific definitions =====
 
@@ -116,8 +76,6 @@
 
 #define SK_MSCALAR_IS_FLOAT
 #undef SK_MSCALAR_IS_DOUBLE
-
-#define GR_MAX_OFFSCREEN_AA_DIM     512
 
 // Log the file and line number for assertions.
 #define SkDebugf(...) SkDebugf_FileLine(__FILE__, __LINE__, false, __VA_ARGS__)
@@ -220,10 +178,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_SVG_ARC_TO
 #endif
 
-#ifndef SK_SUPPORT_LEGACY_YUV_COLORSPACE
-#define SK_SUPPORT_LEGACY_YUV_COLORSPACE
-#endif
-
 // Max. verb count for paths rendered by the edge-AA tessellating path renderer.
 #define GR_AA_TESSELLATOR_MAX_VERB_COUNT 100
 
@@ -236,9 +190,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #ifndef SK_SUPPORT_LEGACY_THREADED_DAA_BUGS
 #define SK_SUPPORT_LEGACY_THREADED_DAA_BUGS
 #endif
-
-#define SK_DONT_OPTIMIZE_SRC_PROFILES_FOR_SPEED
-#define SK_DONT_OPTIMIZE_DST_PROFILES_FOR_SPEED
 
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
@@ -255,9 +206,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 
 #define SK_ATTR_DEPRECATED          SK_NOTHING_ARG1
 #define GR_GL_CUSTOM_SETUP_HEADER   "GrGLConfig_chrome.h"
-
-// mtklein's fiddling with Src / SrcOver.  Will rebaseline these only once when done.
-#define SK_SUPPORT_LEGACY_X86_BLITS
 
 // ===== End Chrome-specific definitions =====
 
