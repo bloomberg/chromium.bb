1ba8ba7a7c2a959a0b34e8728d21182ec07ccc19
Bailey Berro <baileyberro@chromium.org>
2018-06-19 00:54:09 +0000

    Add IsKerberos field to FileSystemId
    This change adds an additional encoded field to the FileSystemId string
    for Smb Mounts when mounting with ChromAD Kerberos. This encodes
    whether the share was initially mounted using ChromAD Kerberos and
    allows us to remount ChromAD Kerberos users' shares
    automatically.
    
    Bug: chromium:757625
    Test: unittests
    Change-Id: Ic22e9bb6f26e5b526984388785b1ae9b1d8ca1fa
    Reviewed-on: https://chromium-review.googlesource.com/1093954
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568268}
    Former-commit-id: bfcc445d6f5eff3faa5de0cc239f72dd11f5eee7

--- chrome/browser/chromeos/smb_client/smb_file_system_id.cc
+++ chrome/browser/chromeos/smb_client/smb_file_system_id.cc
@@ -9,46 +9,66 @@
 #include "base/files/file_path.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/string_split.h"
 
 namespace chromeos {
 namespace smb_client {
 namespace {
 
 constexpr char kDelimiter[] = "@@";
-constexpr size_t kDelimiterLen = arraysize(kDelimiter) - 1;
+constexpr char kKerberosSymbol[] = "kerberos_chromad";
 
-size_t GetDelimLocation(const std::string& file_system_id) {
-  const size_t delim_location = file_system_id.find(kDelimiter);
-  DCHECK(delim_location != std::string::npos);
+std::vector<std::string> GetComponents(const std::string& file_system_id) {
+  const std::vector<std::string> components =
+      SplitString(file_system_id, kDelimiter, base::TRIM_WHITESPACE,
+                  base::SPLIT_WANT_NONEMPTY);
 
-  return delim_location;
+  DCHECK_GE(components.size(), 2u);
+  DCHECK_LE(components.size(), 3u);
+
+  return components;
 }
 
 }  // namespace.
 
 std::string CreateFileSystemId(int32_t mount_id,
                                const base::FilePath& share_path) {
-  return base::StrCat(
+  return CreateFileSystemId(mount_id, share_path, false /* is_kerberos */);
+}
+
+std::string CreateFileSystemId(int32_t mount_id,
+                               const base::FilePath& share_path,
+                               bool is_kerberos_chromad) {
+  const std::string file_system_id = base::StrCat(
       {base::NumberToString(mount_id), kDelimiter, share_path.value()});
+  if (is_kerberos_chromad) {
+    return base::StrCat({file_system_id, kDelimiter, kKerberosSymbol});
+  }
+  return file_system_id;
 }
 
 int32_t GetMountIdFromFileSystemId(const std::string& file_system_id) {
-  const size_t delim_location = GetDelimLocation(file_system_id);
-
-  const std::string mount_id_string = file_system_id.substr(0, delim_location);
+  const std::vector<std::string> components = GetComponents(file_system_id);
+  DCHECK_GE(components.size(), 1u);
 
   int32_t mount_id;
-  bool result = base::StringToInt(mount_id_string, &mount_id);
+  const bool result = base::StringToInt(components[0], &mount_id);
   DCHECK(result);
 
   return mount_id;
 }
 
 base::FilePath GetSharePathFromFileSystemId(const std::string& file_system_id) {
-  const size_t delim_location = GetDelimLocation(file_system_id);
-  const size_t share_path_start = delim_location + kDelimiterLen;
+  const std::vector<std::string> components = GetComponents(file_system_id);
+  DCHECK_GE(components.size(), 1u);
 
-  return base::FilePath(file_system_id.substr(share_path_start));
+  return base::FilePath(components[1]);
+}
+
+bool IsKerberosChromadFileSystemId(const std::string& file_system_id) {
+  const std::vector<std::string> components = GetComponents(file_system_id);
+
+  return components.size() >= 3 && components[2] == kKerberosSymbol;
 }
 
 }  // namespace smb_client
