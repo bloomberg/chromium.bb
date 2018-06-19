ab9a422190ae340f782f43aa1ce260d61062e291
James Cook <jamescook@chromium.org>
2018-06-19 02:18:47 +0000

    chromeos: Move ash keyboard code and add OWNERS
    Move the code to //ash/keyboard. Add yhanada to OWNERS.
    
    This should make it easier for the people working on keyboard support to
    get approvals from nearby time zones.
    
    Bug: none
    Change-Id: Idbb3023913c7e34daad9b42718dd90ad7ed8e319
    Reviewed-on: https://chromium-review.googlesource.com/1105396
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568304}
    Former-commit-id: 8d91252fa79d6b1b219dfc848b09c89ab68bbe48

--- ash/keyboard/virtual_keyboard_container_layout_manager.cc
+++ ash/keyboard/virtual_keyboard_container_layout_manager.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ash/virtual_keyboard_container_layout_manager.h"
+#include "ash/keyboard/virtual_keyboard_container_layout_manager.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ui/aura/window.h"
 
