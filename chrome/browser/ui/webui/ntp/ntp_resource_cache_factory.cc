97e475026ad11811baede1c1dc116988ece70363
Sylvain Defresne <sdefresne@chromium.org>
2018-06-19 10:19:09 +0000

    Convert NTPResourceCache to use IdentityManager
    IdentityManager is the recommended API to access the
    Google account used by the user and SigninManager is
    deprecated.
    
    Convert NTPResourceCache to the new API.
    
    Bug: 798408
    Change-Id: Ia94e15dd8b50f546a9d6b02957dfefe9d40105e0
    Reviewed-on: https://chromium-review.googlesource.com/1097332
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568391}
    Former-commit-id: c9746c4c7a0c2c1a3970b1ad37d6a434db38a395

--- chrome/browser/ui/webui/ntp/ntp_resource_cache_factory.cc
+++ chrome/browser/ui/webui/ntp/ntp_resource_cache_factory.cc
@@ -6,7 +6,7 @@
 
 #include "chrome/browser/profiles/incognito_helpers.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/themes/theme_service_factory.h"
 #include "chrome/browser/ui/webui/ntp/ntp_resource_cache.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
@@ -26,7 +26,7 @@ NTPResourceCacheFactory::NTPResourceCacheFactory()
     : BrowserContextKeyedServiceFactory(
         "NTPResourceCache",
         BrowserContextDependencyManager::GetInstance()) {
-  DependsOn(SigninManagerFactory::GetInstance());
+  DependsOn(IdentityManagerFactory::GetInstance());
   DependsOn(ThemeServiceFactory::GetInstance());
 }
 
