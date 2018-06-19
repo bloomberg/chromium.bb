97d967cd41f08daf5c4852fc8f24b4786cee94ac
Sylvain Defresne <sdefresne@chromium.org>
2018-06-19 23:44:31 +0000

    Unregister SyncObserver on shutdown
    ProfileSyncService DCHECK in Shutdown that all observer have
    been unregistered, so refactor DesktopPromotionSyncService
    to unregister its observer in Shutdown.
    
    The refactor consist in removal of the helper observer class
    DesktopPromotionSyncObserver that is folded in the service
    as it makes it easier to track whether the observer is still
    registered (the alternative is to introduce a delegate class
    that is implemented by the service and invoked by the observer
    when it needs to unregister). As the class had no behaviour
    at this point, merging it with the observer is simpler.
    
    Also fix the fact that desktop_metrics_logger_initiated_ was
    not initialized in DesktopPromotionSyncObserver constructor,
    so the check in OnStateChange was undefined behaviour (most
    likely the result would have been that the variable was read
    to be non-false).
    
    Mark some local variable as const and avoid reading prefs that
    are not used.
    
    Bug: 854118
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I8d5d2bcb09da0430d3a08d79058bdecd047f3f28
    Reviewed-on: https://chromium-review.googlesource.com/1105978
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568647}
    Former-commit-id: ddd2927c777cbc385efa52154ba7c4ba5b0d1b91

--- ios/chrome/browser/desktop_promotion/desktop_promotion_sync_service_factory.cc
+++ ios/chrome/browser/desktop_promotion/desktop_promotion_sync_service_factory.cc
@@ -6,6 +6,7 @@
 
 #include <memory>
 
+#include "components/browser_sync/profile_sync_service.h"
 #include "components/keyed_service/ios/browser_state_dependency_manager.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/desktop_promotion/desktop_promotion_sync_service.h"
