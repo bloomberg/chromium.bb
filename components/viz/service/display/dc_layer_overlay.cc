08eb05c759cac3b45ae6c29ddf1dd949cfd029dc
Maggie Chen <magchen@chromium.org>
2018-06-20 00:20:02 +0000

    Allow multiple overlays/underlays for protected video.
    The current DCLayerOverlayProcessor only allows one overlay in the root
    render pass. Also no underlay is allowed. In order to support protected
    video, overlays and underlays (swapchains) are supported if
    PROTECTED_VIDEO and REQUIRE_OVERLAY are set.
    
    BUG=843814
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Iab1aa4adeb27f9ebed2fcc30727f17e73cea1d6f
    Reviewed-on: https://chromium-review.googlesource.com/1093986
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#568659}
    Former-commit-id: 6bb8878b029fa96ff2a5fa0f6857a3763dfc2044

--- components/viz/service/display/dc_layer_overlay.cc
+++ components/viz/service/display/dc_layer_overlay.cc
@@ -32,6 +32,9 @@ DCLayerOverlayProcessor::DCLayerResult FromYUVQuad(
   dc_layer_overlay->color_space = quad->video_color_space;
   dc_layer_overlay->require_overlay = quad->require_overlay;
   dc_layer_overlay->is_protected_video = quad->is_protected_video;
+  if (dc_layer_overlay->is_protected_video)
+    DCHECK(dc_layer_overlay->require_overlay);
+
   return DCLayerOverlayProcessor::DC_LAYER_SUCCESS;
 }
 
@@ -305,7 +308,8 @@ void DCLayerOverlayProcessor::ProcessRenderPass(
     // our damage calculations will be wrong otherwise.
     // TODO(magchen): Collect all overlay candidates, and filter the list at the
     // end to find the best candidates (largest size?).
-    if (is_root && !processed_overlay_in_frame_ &&
+    if (is_root &&
+        (!processed_overlay_in_frame_ || dc_layer.is_protected_video) &&
         ProcessForOverlay(display_rect, quad_list, quad_rectangle,
                           occlusion_bounding_box, &it, damage_rect)) {
       // ProcessForOverlay makes the iterator point to the next value on
