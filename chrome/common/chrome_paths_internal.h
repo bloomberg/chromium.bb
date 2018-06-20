f91c6699937a5ea8a5babc8777195fe5778b49ea
Denis Yaroshevskiy <dyaroshev@yandex-team.ru>
2018-06-20 09:30:53 +0000

    Removing unused function.
    This function is never called. If there is ever a need to do this,
    chome_paths has a generalized mechanism for overriding file paths.
    
    Change-Id: I0822b22aed510492f88ecc0b4595649aee749583
    Reviewed-on: https://chromium-review.googlesource.com/1102466
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Denis Yaroshevskiy <dyaroshev@yandex-team.ru>
    Cr-Commit-Position: refs/heads/master@{#568773}
    Former-commit-id: 90ed4e500d4b2f37d966e4b02b67d47041f06db8

--- chrome/common/chrome_paths_internal.h
+++ chrome/common/chrome_paths_internal.h
@@ -69,13 +69,6 @@ bool GetUserVideosDirectory(base::FilePath* result);
 // in the .app at Contents/Versions/w.x.y.z.
 base::FilePath GetVersionedDirectory();
 
-// This overrides the directory returned by |GetVersionedDirectory()|, to be
-// used when |GetVersionedDirectory()| can't automatically determine the proper
-// location. This is the case when the browser didn't load itself but by, e.g.,
-// the app mode loader. This should be called before |ChromeMain()|. This takes
-// ownership of the object |path| and the caller must not delete it.
-void SetOverrideVersionedDirectory(const base::FilePath* path);
-
 // Most of the application is further contained within the framework.  The
 // framework bundle is located within the versioned directory at a specific
 // path.  The only components in the versioned directory not included in the
