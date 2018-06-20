d0e705579f6ecd603ab72c1105f5b76048918faa
Hui Su <huisu@google.com>
2018-06-20 11:41:16 -0700

    Remove unnecessary clipping in dr predictors
    These clipping operations in intra directional predictors are not
    necessary because the values prior to them are always in range.
    
    This does not change bitstream.
    
    Change-Id: Idf65deeaec29066971569113c3dfabf6a608d666

--- av1/common/reconintra.c
+++ av1/common/reconintra.c
@@ -543,8 +543,7 @@ void av1_dr_prediction_z1_c(uint8_t *dst, ptrdiff_t stride, int bw, int bh,
     for (c = 0; c < bw; ++c, base += base_inc) {
       if (base < max_base_x) {
         val = above[base] * (32 - shift) + above[base + 1] * shift;
-        val = ROUND_POWER_OF_TWO(val, 5);
-        dst[c] = clip_pixel(val);
+        dst[c] = ROUND_POWER_OF_TWO(val, 5);
       } else {
         dst[c] = above[max_base_x];
       }
@@ -582,7 +581,7 @@ void av1_dr_prediction_z2_c(uint8_t *dst, ptrdiff_t stride, int bw, int bh,
         val = left[base2] * (32 - shift2) + left[base2 + 1] * shift2;
         val = ROUND_POWER_OF_TWO(val, 5);
       }
-      dst[c] = clip_pixel(val);
+      dst[c] = val;
     }
   }
 }
@@ -610,8 +609,7 @@ void av1_dr_prediction_z3_c(uint8_t *dst, ptrdiff_t stride, int bw, int bh,
     for (r = 0; r < bh; ++r, base += base_inc) {
       if (base < max_base_y) {
         val = left[base] * (32 - shift) + left[base + 1] * shift;
-        val = ROUND_POWER_OF_TWO(val, 5);
-        dst[r * stride + c] = clip_pixel(val);
+        dst[r * stride + c] = val = ROUND_POWER_OF_TWO(val, 5);
       } else {
         for (; r < bh; ++r) dst[r * stride + c] = left[max_base_y];
         break;
@@ -654,6 +652,7 @@ void av1_highbd_dr_prediction_z1_c(uint16_t *dst, ptrdiff_t stride, int bw,
 
   (void)left;
   (void)dy;
+  (void)bd;
   assert(dy == 1);
   assert(dx > 0);
 
@@ -676,8 +675,7 @@ void av1_highbd_dr_prediction_z1_c(uint16_t *dst, ptrdiff_t stride, int bw,
     for (c = 0; c < bw; ++c, base += base_inc) {
       if (base < max_base_x) {
         val = above[base] * (32 - shift) + above[base + 1] * shift;
-        val = ROUND_POWER_OF_TWO(val, 5);
-        dst[c] = clip_pixel_highbd(val, bd);
+        dst[c] = ROUND_POWER_OF_TWO(val, 5);
       } else {
         dst[c] = above[max_base_x];
       }
@@ -692,6 +690,7 @@ void av1_highbd_dr_prediction_z2_c(uint16_t *dst, ptrdiff_t stride, int bw,
                                    int upsample_left, int dx, int dy, int bd) {
   int r, c, x, y, shift, val, base;
 
+  (void)bd;
   assert(dx > 0);
   assert(dy > 0);
 
@@ -715,7 +714,7 @@ void av1_highbd_dr_prediction_z2_c(uint16_t *dst, ptrdiff_t stride, int bw,
         val = left[base] * (32 - shift) + left[base + 1] * shift;
         val = ROUND_POWER_OF_TWO(val, 5);
       }
-      dst[c] = clip_pixel_highbd(val, bd);
+      dst[c] = val;
     }
     dst += stride;
   }
@@ -730,6 +729,7 @@ void av1_highbd_dr_prediction_z3_c(uint16_t *dst, ptrdiff_t stride, int bw,
 
   (void)above;
   (void)dx;
+  (void)bd;
   assert(dx == 1);
   assert(dy > 0);
 
@@ -744,8 +744,7 @@ void av1_highbd_dr_prediction_z3_c(uint16_t *dst, ptrdiff_t stride, int bw,
     for (r = 0; r < bh; ++r, base += base_inc) {
       if (base < max_base_y) {
         val = left[base] * (32 - shift) + left[base + 1] * shift;
-        val = ROUND_POWER_OF_TWO(val, 5);
-        dst[r * stride + c] = clip_pixel_highbd(val, bd);
+        dst[r * stride + c] = ROUND_POWER_OF_TWO(val, 5);
       } else {
         for (; r < bh; ++r) dst[r * stride + c] = left[max_base_y];
         break;
