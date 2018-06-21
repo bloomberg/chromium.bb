9f3bbc871f3e71324c7b6e3515be6abb042ecbaa
Qiyu Hu <qiyuh@google.com>
2018-06-21 23:25:06 +0000

    bluetooth: Change ReadDevicePath() into ReadOptions()
    This is to make the API more general. The current API cannot parse
    any options other than DevicePath. In the case where we want to parse
    multiple options, the current API just throw them away and returns us a
    device path.
    
    BUG: b:78593133
    TEST: Things are still going well, at least on Android CTS Verifier.
    Change-Id: I088492e280a023998d0295137bddcd6857088606
    Reviewed-on: https://chromium-review.googlesource.com/1103659
    Reviewed-by: Miao-chen Chou <mcchou@chromium.org>
    Commit-Queue: Qiyu Hu <qiyuh@google.com>
    Cr-Commit-Position: refs/heads/master@{#569447}
    Former-commit-id: 76217ee6a6c3931e8fce2cfa17d01f33069bb0a4

--- device/bluetooth/dbus/bluetooth_gatt_attribute_helpers.h
+++ device/bluetooth/dbus/bluetooth_gatt_attribute_helpers.h
@@ -5,6 +5,8 @@
 #ifndef DEVICE_BLUETOOTH_DBUS_BLUETOOTH_GATT_ATTRIBUTES_HELPER_H_
 #define DEVICE_BLUETOOTH_DBUS_BLUETOOTH_GATT_ATTRIBUTES_HELPER_H_
 
+#include <map>
+
 #include "dbus/object_path.h"
 
 namespace dbus {
@@ -14,7 +16,8 @@ class MessageReader;
 namespace bluez {
 
 // Helper methods used from various GATT attribute providers and clients.
-dbus::ObjectPath ReadDevicePath(dbus::MessageReader* reader);
+bool ReadOptions(dbus::MessageReader* reader,
+                 std::map<std::string, dbus::MessageReader>* options);
 
 }  // namespace bluez
 
