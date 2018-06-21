f2377106fa431af62213887f0080607711456713
Cathy Li <chili@chromium.org>
2018-06-21 00:49:28 +0000

    [Explore sites]: Make explore category tiles pretty
    Bug: 852075
    Change-Id: Ieb6329d57005bdba63cdf7ed1f991b5da61bb362
    Reviewed-on: https://chromium-review.googlesource.com/1103138
    Commit-Queue: Cathy Li <chili@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569109}
    Former-commit-id: 39718f1f0040c56af72fece3e925b1c19611221d

--- chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesSection.java
+++ chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesSection.java
@@ -12,25 +12,22 @@ import android.widget.LinearLayout;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.suggestions.SuggestionsNavigationDelegate;
-import org.chromium.chrome.browser.util.ViewUtils;
 import org.chromium.ui.mojom.WindowOpenDisposition;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Describes a portion of UI responsible for rendering a group of categories.
  * It abstracts general tasks related to initializing and fetching data for the UI.
  */
 public class ExploreSitesSection {
-    private static final int ICON_CORNER_RADIUS = 5;
+    private static final int MAX_TILES = 3;
 
     private Profile mProfile;
     private SuggestionsNavigationDelegate mNavigationDelegate;
     private View mExploreSection;
     private LinearLayout mCategorySection;
 
-    private AtomicInteger mCategoryIconFetchesInFlight;
     public ExploreSitesSection(
             View view, Profile profile, SuggestionsNavigationDelegate navigationDelegate) {
         mProfile = profile;
@@ -42,19 +39,37 @@ public class ExploreSitesSection {
     private void initialize() {
         mCategorySection = mExploreSection.findViewById(R.id.explore_sites_tiles);
         ExploreSitesBridge.getNtpCategories(mProfile, this::initializeTiles);
+
+        View moreCategoriesButton = mExploreSection.findViewById(R.id.explore_sites_more_button);
+        moreCategoriesButton.setOnClickListener(
+                (View v)
+                        -> mNavigationDelegate.navigateToSuggestionUrl(
+                                WindowOpenDisposition.CURRENT_TAB,
+                                ExploreSitesBridge.nativeGetCatalogUrl()));
     }
 
     private void initializeTiles(List<ExploreSitesCategoryTile> tileList) {
-        if (tileList.isEmpty()) {
-            return;
-        }
-        mCategoryIconFetchesInFlight = new AtomicInteger(tileList.size());
+        if (tileList == null) return;
+
+        int parentWidth = mExploreSection.getWidth();
+        int tileWidth = (parentWidth
+                                - (mExploreSection.getResources().getDimensionPixelSize(
+                                           R.dimen.explore_sites_padding)
+                                          * 2))
+                / MAX_TILES;
+        int tileHeight = tileWidth / 3 * 2;
+
+        int tileCount = 0;
         for (final ExploreSitesCategoryTile tile : tileList) {
+            // Ensures only 3 tiles are shown.
+            tileCount++;
+            if (tileCount > MAX_TILES) break;
+
             final ExploreSitesCategoryTileView tileView =
                     (ExploreSitesCategoryTileView) LayoutInflater.from(mExploreSection.getContext())
                             .inflate(R.layout.explore_sites_category_tile_view, mCategorySection,
                                     false);
-            tileView.initialize(tile);
+            tileView.initialize(tile, tileWidth, tileHeight);
             mCategorySection.addView(tileView);
             tileView.setOnClickListener(
                     (View v)
@@ -66,7 +81,6 @@ public class ExploreSitesSection {
     }
 
     private void onIconRetrieved(ExploreSitesCategoryTileView tileView, Bitmap icon) {
-        tileView.updateIcon(ViewUtils.createRoundedBitmapDrawable(icon, ICON_CORNER_RADIUS));
-        mCategoryIconFetchesInFlight.decrementAndGet();
+        tileView.updateIcon(icon);
     }
 }
