281ba6b8911ce3fd2f3f4e34e13c78d63193a0df
Stephen Martinis <martiniss@chromium.org>
2018-06-22 02:24:10 +0000

    Monochrome: Ignore isolated script test argument when retrying
    This argument is passed when the test is being retried. Parse and ignore
    it so the retry without patch logic works correctly.
    
    Also fixes the json test result output for the test, so that if the test
    fails, it's marked as unexpected.
    
    Also moves the wrapper script into //testing/scripts, which is where
    wrapper scripts are supposed to live.
    
    Bug: 533481
    Change-Id: I4409be32a84c885d677ae90791a51c847d0c7e1f
    Reviewed-on: https://chromium-review.googlesource.com/1107054
    Commit-Queue: Stephen Martinis <martiniss@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569510}
    Former-commit-id: e58ad2f781177df53a4a0ea8a81847bf9e71429a

--- testing/scripts/monochrome_apk_checker_wrapper.py
+++ testing/scripts/monochrome_apk_checker_wrapper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 #
 # Copyright 2018 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
@@ -13,6 +13,10 @@ import sys
 import subprocess
 import time
 
+
+import common
+
+
 def main():
   parser = argparse.ArgumentParser(prog='monochrome_apk_checker_wrapper')
 
@@ -21,9 +25,17 @@ def main():
                       help='The path to the monochrome_apk_checker.py script')
   parser.add_argument('--isolated-script-test-output',
                       required=True)
-  # ignored, but required to satisfy the isolated_script interface.
+  # Only run one test, we check that it's the test we expect.
+  parser.add_argument('--isolated-script-test-filter')
+  # Ignored, but required to satisfy the isolated_script interface.
+  # We aren't a perf test, so don't have any perf output.
   parser.add_argument('--isolated-script-test-perf-output')
   args, extra = parser.parse_known_args(sys.argv[1:])
+
+  if args.isolated_script_test_filter and (
+      'monochrome_apk_checker' not in args.isolated_script_test_filter):
+      parser.error('isolated-script-test-filter has invalid test: %s' % (
+          args.isolated_script_test_filter))
 
   cmd = [args.script] + extra
 
@@ -33,6 +45,13 @@ def main():
 
   # Schema is at //docs/testing/json_test_results_format.md
   with open(args.isolated_script_test_output, 'w') as fp:
+    test = {
+      'expected': 'PASS',
+      'actual': 'PASS' if success else 'FAIL',
+    }
+    if not success:
+      test['unexpected'] = True
+
     json.dump({
       'version': 3,
       'interrupted': False,
@@ -43,15 +62,24 @@ def main():
         'FAIL': int(not success),
       },
       'tests': {
-        'monochrome_apk_checker': {
-          'expected': 'PASS',
-          'actual': 'PASS' if success else 'FAIL',
-        }
+        'monochrome_apk_checker': test,
       }
     }, fp)
 
   return ret
 
+# This is not really a "script test" so does not need to manually add
+# any additional compile targets.
+def main_compile_targets(args):
+  json.dump([], args.output)
 
 if __name__ == '__main__':
+  # Conform minimally to the protocol defined by ScriptTest.
+  if 'compile_targets' in sys.argv:
+    funcs = {
+      'run': None,
+      'compile_targets': main_compile_targets,
+    }
+    sys.exit(common.run_script(sys.argv[1:], funcs))
+
   sys.exit(main())
