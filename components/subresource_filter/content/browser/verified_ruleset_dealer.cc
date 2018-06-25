31a1ad4838a36086a0094ce6904829f06fb40f65
John Delaney <johnidel@chromium.org>
2018-06-25 23:17:00 +0000

    Add trace events to RulesetService
    Add trace of prefs format and content version at RulesetService
    creation. Add trace of invalid Indexed Ruleset file read.
    
    Bug: 721512
    Change-Id: I6809a2cccb8903889b74e64a8c08788ff3c8422a
    Reviewed-on: https://chromium-review.googlesource.com/1108242
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570227}
    Former-commit-id: 47d5537ba10dd8b11c1402ac3e451940b5035724

--- components/subresource_filter/content/browser/verified_ruleset_dealer.cc
+++ components/subresource_filter/content/browser/verified_ruleset_dealer.cc
@@ -30,6 +30,9 @@ base::File VerifiedRulesetDealer::OpenAndSetRulesetFile(
   // there are handles to it still open.
   base::File file(file_path, base::File::FLAG_OPEN | base::File::FLAG_READ |
                                  base::File::FLAG_SHARE_DELETE);
+  TRACE_EVENT1(TRACE_DISABLED_BY_DEFAULT("loading"),
+               "VerifiedRulesetDealer::OpenAndSetRulesetFile", "file_valid",
+               file.IsValid());
   if (file.IsValid())
     SetRulesetFile(file.Duplicate());
   return file;
