7365727988584f79546cdfef66832d76e5045908
Yue Li <updowndota@chromium.org>
2018-06-25 18:53:53 +0000

    Migrate methods from AssitantOptInUI into Handler
    - migrate many methods into the handler
    - update loading/error screen specs
    
    Bug: b/78190629
    Test: Manual
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I7a590f547cc85309f982c58421f0984721a4f04b
    Reviewed-on: https://chromium-review.googlesource.com/1112716
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570113}
    Former-commit-id: 814448e99db78bc68ee5de3237c902eba08f7fb3

--- chrome/browser/ui/webui/chromeos/assistant_optin/assistant_optin_handler.h
+++ chrome/browser/ui/webui/chromeos/assistant_optin/assistant_optin_handler.h
@@ -9,11 +9,15 @@
 #include <string>
 
 #include "base/macros.h"
+#include "chrome/browser/chromeos/arc/voice_interaction/voice_interaction_controller_client.h"
 #include "chrome/browser/ui/webui/chromeos/login/base_webui_handler.h"
+#include "chromeos/services/assistant/public/mojom/settings.mojom.h"
 
 namespace chromeos {
 
-class AssistantOptInHandler : public BaseWebUIHandler {
+class AssistantOptInHandler
+    : public BaseWebUIHandler,
+      public arc::VoiceInteractionControllerClient::Observer {
  public:
   explicit AssistantOptInHandler(JSCallsContainer* js_calls_container);
   ~AssistantOptInHandler() override;
@@ -24,14 +28,45 @@ class AssistantOptInHandler : public BaseWebUIHandler {
   void RegisterMessages() override;
   void Initialize() override;
 
+  // Send messages to the page.
+  void ShowNextScreen();
+
+  // Handle user opt-in result.
+  void OnActivityControlOptInResult(bool opted_in);
+  void OnEmailOptInResult(bool opted_in);
+
+ private:
+  // arc::VoiceInteractionControllerClient::Observer overrides
+  void OnStateChanged(ash::mojom::VoiceInteractionState state) override;
+
+  // Connect to assistant settings manager.
+  void BindAssistantSettingsManager();
+
+  // Send GetSettings request for the opt-in UI.
+  void SendGetSettingsRequest();
+
   // Send message and consent data to the page.
   void ReloadContent(const base::DictionaryValue& dict);
   void AddSettingZippy(const std::string& type, const base::ListValue& data);
-  void ShowNextScreen();
 
- private:
+  // Handle response from the settings manager.
+  void OnGetSettingsResponse(const std::string& settings);
+  void OnUpdateSettingsResponse(const std::string& settings);
+
   // Handler for JS WebUI message.
   void HandleInitialized();
+
+  // Consent token used to complete the opt-in.
+  std::string consent_token_;
+
+  // Whether activity control is needed for user.
+  bool activity_control_needed_ = true;
+
+  // Whether email optin is needed for user.
+  bool email_optin_needed_ = false;
+
+  assistant::mojom::AssistantSettingsManagerPtr settings_manager_;
+  base::WeakPtrFactory<AssistantOptInHandler> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(AssistantOptInHandler);
 };
