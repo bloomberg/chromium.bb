1e28e24b300041be836091e45db8a92470a7ea35
Michael Spang <spang@chromium.org>
2018-06-25 23:42:37 +0000

    ozone: demo: Add support for overlay surfaces
    This adds a new renderer to ozone demo that presents to an
    OverlaySurface.
    
    Synchronization by submitting a gfx::GpuFence with the planes is not
    finished yet; instead we just wait after every queue submission.
    
    Bug: 851997
    Test: ozone_demo --enable-vulkan on vulkan-enabled eve
    
    Change-Id: Icb4a6d2fdb8f74b68c801edc66c340ff2bd64df1
    Reviewed-on: https://chromium-review.googlesource.com/1104867
    Commit-Queue: Michael Spang <spang@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570239}
    Former-commit-id: 7c52fbaac1c57ec1f596fa2d02f11f9413da12d2

--- ui/ozone/demo/simple_renderer_factory.cc
+++ ui/ozone/demo/simple_renderer_factory.cc
@@ -12,10 +12,13 @@
 #include "ui/ozone/demo/gl_renderer.h"
 #include "ui/ozone/demo/software_renderer.h"
 #include "ui/ozone/demo/surfaceless_gl_renderer.h"
+#include "ui/ozone/public/overlay_surface.h"
 #include "ui/ozone/public/ozone_platform.h"
+#include "ui/ozone/public/surface_factory_ozone.h"
 
 #if BUILDFLAG(ENABLE_VULKAN)
 #include "gpu/vulkan/init/vulkan_factory.h"
+#include "ui/ozone/demo/vulkan_overlay_renderer.h"
 #include "ui/ozone/demo/vulkan_renderer.h"
 #endif
 
@@ -88,9 +91,20 @@ std::unique_ptr<Renderer> SimpleRendererFactory::CreateRenderer(
       return std::make_unique<GlRenderer>(widget, surface, size);
     }
 #if BUILDFLAG(ENABLE_VULKAN)
-    case VULKAN:
+    case VULKAN: {
+      SurfaceFactoryOzone* surface_factory_ozone =
+          OzonePlatform::GetInstance()->GetSurfaceFactoryOzone();
+
+      std::unique_ptr<OverlaySurface> overlay_surface =
+          surface_factory_ozone->CreateOverlaySurface(widget);
+      if (overlay_surface) {
+        return std::make_unique<VulkanOverlayRenderer>(
+            std::move(overlay_surface), surface_factory_ozone,
+            vulkan_implementation_.get(), widget, size);
+      }
       return std::make_unique<VulkanRenderer>(vulkan_implementation_.get(),
                                               widget, size);
+    }
 #endif
     case SOFTWARE:
       return std::make_unique<SoftwareRenderer>(widget, size);
---
+++ ui/ozone/demo/vulkan_overlay_renderer.h
@@ -0,0 +1,113 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_OZONE_DEMO_VULKAN_OVERLAY_RENDERER_H_
+#define UI_OZONE_DEMO_VULKAN_OVERLAY_RENDERER_H_
+
+#include <vulkan/vulkan.h>
+
+#include "base/macros.h"
+#include "base/memory/ref_counted.h"
+#include "base/memory/weak_ptr.h"
+#include "gpu/vulkan/vulkan_function_pointers.h"
+#include "ui/gfx/buffer_types.h"
+#include "ui/gfx/presentation_feedback.h"
+#include "ui/gfx/swap_result.h"
+#include "ui/ozone/demo/renderer_base.h"
+
+namespace gfx {
+class NativePixmap;
+}  // namespace gfx
+
+namespace gpu {
+class VulkanDeviceQueue;
+class VulkanImplementation;
+class VulkanCommandBuffer;
+class VulkanCommandPool;
+}  // namespace gpu
+
+namespace ui {
+class OverlaySurface;
+class SurfaceFactoryOzone;
+
+class VulkanOverlayRenderer : public RendererBase {
+ public:
+  VulkanOverlayRenderer(std::unique_ptr<OverlaySurface> overlay_surface,
+                        SurfaceFactoryOzone* surface_factory_ozone,
+                        gpu::VulkanImplementation* vulkan_instance,
+                        gfx::AcceleratedWidget widget,
+                        const gfx::Size& size);
+  ~VulkanOverlayRenderer() override;
+
+  // Renderer:
+  bool Initialize() override;
+
+ private:
+  void RecreateBuffers();
+  void RenderFrame();
+  void PostRenderFrameTask();
+  void OnFrameSubmitted(uint64_t frame_sequence, gfx::SwapResult swap_result);
+  void OnFramePresented(uint64_t frame_sequence,
+                        const gfx::PresentationFeedback& feedback);
+  void OnFrameReleased(uint64_t frame_sequence);
+
+  class Buffer {
+   public:
+    Buffer(const gfx::Size& size,
+           scoped_refptr<gfx::NativePixmap> native_pixmap,
+           VkDeviceMemory vk_device_memory,
+           VkImage vk_image,
+           VkImageView vk_image_view,
+           VkFramebuffer vk_framebuffer);
+    ~Buffer();
+
+    static std::unique_ptr<Buffer> Create(
+        SurfaceFactoryOzone* surface_factory_ozone,
+        const gpu::VulkanFunctionPointers* vulkan_function_pointers,
+        VkDevice vk_device,
+        VkRenderPass vk_render_pass,
+        gfx::AcceleratedWidget widget,
+        const gfx::Size& size);
+
+    const gfx::Size& size() const { return size_; }
+    const scoped_refptr<gfx::NativePixmap>& native_pixmap() const {
+      return native_pixmap_;
+    }
+    VkDeviceMemory vk_device_memory() const { return vk_device_memory_; }
+    VkImage vk_image() const { return vk_image_; }
+    VkImageView vk_image_view() const { return vk_image_view_; }
+    VkFramebuffer vk_framebuffer() const { return vk_framebuffer_; }
+
+   private:
+    const scoped_refptr<gfx::NativePixmap> native_pixmap_;
+    const gfx::Size size_;
+    const VkDeviceMemory vk_device_memory_;
+    const VkImage vk_image_;
+    const VkImageView vk_image_view_;
+    const VkFramebuffer vk_framebuffer_;
+  };
+
+  uint64_t frame_sequence_ = 0;
+  int next_buffer_ = 0;
+  size_t in_use_buffers_ = 0;
+  std::unique_ptr<Buffer> buffers_[2];
+
+  SurfaceFactoryOzone* const surface_factory_ozone_;
+  gpu::VulkanImplementation* const vulkan_implementation_;
+  const gpu::VulkanFunctionPointers* const vulkan_function_pointers_;
+  std::unique_ptr<gpu::VulkanDeviceQueue> device_queue_;
+  std::unique_ptr<gpu::VulkanCommandPool> command_pool_;
+  std::unique_ptr<gpu::VulkanCommandBuffer> command_buffer_;
+  std::unique_ptr<OverlaySurface> overlay_surface_;
+
+  VkRenderPass render_pass_;
+
+  base::WeakPtrFactory<VulkanOverlayRenderer> weak_ptr_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(VulkanOverlayRenderer);
+};
+
+}  // namespace ui
+
+#endif  // UI_OZONE_DEMO_VULKAN_OVERLAY_RENDERER_H_
