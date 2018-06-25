f39b7aa853f3c8b954fddb87863419a60e657a04
vitaliii <vitaliii@chromium.org>
2018-06-25 09:52:36 +0000

    [Sync] Wire ConsentSyncBridge behind a feature on iOS.
    Connect ConsentSyncBridge to ConsentAuditor behind a feature
    on iOS. ConsentAuditor does not use the bridge at all yet.
    
    The bridge is wired to the auditor on all other platforms in
    https://chromium-review.googlesource.com/c/chromium/src/+/1084475.
    
    This CL is a part of a series to split user consents from user events
    into a separate datatype.
    
    Bug: 840357
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ie47fb3241cfdd64f7962d9d8b62f28578a76c0a1
    Reviewed-on: https://chromium-review.googlesource.com/1087269
    Reviewed-by: Markus Heintz <markusheintz@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: vitaliii <vitaliii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569995}
    Former-commit-id: 0a4de8ea67d1df1dbd6bd0f51c3d8876be4b5841

--- ios/chrome/browser/consent_auditor/consent_auditor_factory.cc
+++ ios/chrome/browser/consent_auditor/consent_auditor_factory.cc
@@ -4,15 +4,25 @@
 
 #include "ios/chrome/browser/consent_auditor/consent_auditor_factory.h"
 
+#include "base/bind_helpers.h"
+#include "base/feature_list.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/singleton.h"
+#include "components/browser_sync/profile_sync_service.h"
 #include "components/consent_auditor/consent_auditor.h"
+#include "components/consent_auditor/consent_sync_bridge.h"
+#include "components/consent_auditor/consent_sync_bridge_impl.h"
 #include "components/keyed_service/ios/browser_state_dependency_manager.h"
 #include "components/pref_registry/pref_registry_syncable.h"
+#include "components/sync/base/report_unrecoverable_error.h"
+#include "components/sync/driver/sync_driver_switches.h"
+#include "components/sync/model_impl/client_tag_based_model_type_processor.h"
 #include "components/version_info/version_info.h"
 #include "ios/chrome/browser/application_context.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/sync/ios_user_event_service_factory.h"
+#include "ios/chrome/browser/sync/profile_sync_service_factory.h"
+#include "ios/chrome/common/channel_info.h"
 #include "ios/web/public/browser_state.h"
 
 // static
@@ -39,6 +49,10 @@ ConsentAuditorFactory::ConsentAuditorFactory()
     : BrowserStateKeyedServiceFactory(
           "ConsentAuditor",
           BrowserStateDependencyManager::GetInstance()) {
+  // TODO(crbug.com/850428): This is missing
+  // DependsOn(ProfileSyncServiceFactory::GetInstance()), which we can't
+  // simply add because ProfileSyncServiceFactory itself depends on this
+  // factory.
   DependsOn(IOSUserEventServiceFactory::GetInstance());
 }
 
@@ -48,12 +62,36 @@ std::unique_ptr<KeyedService> ConsentAuditorFactory::BuildServiceInstanceFor(
     web::BrowserState* browser_state) const {
   ios::ChromeBrowserState* ios_browser_state =
       ios::ChromeBrowserState::FromBrowserState(browser_state);
+  // TODO(crbug.com/851438): Don't create user event service at all if it is not
+  // needed.
   syncer::UserEventService* const user_event_service =
       IOSUserEventServiceFactory::GetForBrowserState(ios_browser_state);
-  // TODO(crbug.com/840357): Wire ConsentSyncBridge behind a feature.
+
+  std::unique_ptr<syncer::ConsentSyncBridge> bridge;
+  if (base::FeatureList::IsEnabled(switches::kSyncUserConsentSeparateType)) {
+    syncer::OnceModelTypeStoreFactory store_factory =
+        browser_sync::ProfileSyncService::GetModelTypeStoreFactory(
+            browser_state->GetStatePath());
+    auto change_processor =
+        std::make_unique<syncer::ClientTagBasedModelTypeProcessor>(
+            syncer::USER_CONSENTS,
+            base::BindRepeating(&syncer::ReportUnrecoverableError,
+                                ::GetChannel()));
+    syncer::SyncService* sync_service =
+        ProfileSyncServiceFactory::GetForBrowserState(
+            ios::ChromeBrowserState::FromBrowserState(browser_state));
+    bridge = std::make_unique<syncer::ConsentSyncBridgeImpl>(
+        std::move(store_factory), std::move(change_processor),
+        /*authenticated_account_id_callback=*/
+        base::BindRepeating(
+            [](syncer::SyncService* sync_service) {
+              return sync_service->GetAuthenticatedAccountInfo().account_id;
+            },
+            sync_service));
+  }
+
   return std::make_unique<consent_auditor::ConsentAuditor>(
-      ios_browser_state->GetPrefs(), /*consent_sync_bridge=*/nullptr,
-      user_event_service,
+      ios_browser_state->GetPrefs(), std::move(bridge), user_event_service,
       // The browser version and locale do not change runtime, so we can pass
       // them directly.
       version_info::GetVersionNumber(),
