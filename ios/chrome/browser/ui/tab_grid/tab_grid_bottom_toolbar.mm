a46fbe292cb90f0913d50b9ae8bb82a02981749b
edchin <edchin@chromium.org>
2018-06-25 23:17:04 +0000

    [ios] Extend done button in tab grid
    The done button tap target should be expanded so that the user can tap
    the same spot when entering and exiting the tab grid.
    
    This CL also fixes the ellipsis when changing the "Close All" button text to "Undo".
    
    Bug: 825787, 849193
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I352e9654419c670f6dea431003a9c189ed639156
    Reviewed-on: https://chromium-review.googlesource.com/1114149
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570228}
    Former-commit-id: 7a6346e3f1912afb92d8a35a7977eaaa9386e770

--- ios/chrome/browser/ui/tab_grid/tab_grid_bottom_toolbar.mm
+++ ios/chrome/browser/ui/tab_grid/tab_grid_bottom_toolbar.mm
@@ -4,6 +4,7 @@
 
 #import "ios/chrome/browser/ui/tab_grid/tab_grid_bottom_toolbar.h"
 
+#include "base/i18n/rtl.h"
 #import "ios/chrome/browser/ui/tab_grid/tab_grid_constants.h"
 #import "ios/chrome/browser/ui/tab_grid/tab_grid_new_tab_button.h"
 #import "ios/chrome/browser/ui/uikit_ui_util.h"
@@ -46,8 +47,27 @@
   UIButton* leadingButton = [UIButton buttonWithType:UIButtonTypeSystem];
   leadingButton.translatesAutoresizingMaskIntoConstraints = NO;
   leadingButton.tintColor = UIColorFromRGB(kTabGridToolbarTextButtonColor);
+  leadingButton.titleLabel.lineBreakMode = NSLineBreakByClipping;
   UIButton* trailingButton = [UIButton buttonWithType:UIButtonTypeSystem];
   trailingButton.translatesAutoresizingMaskIntoConstraints = NO;
+
+  if (@available(iOS 11, *)) {
+    leadingButton.contentHorizontalAlignment =
+        UIControlContentHorizontalAlignmentLeading;
+    trailingButton.contentHorizontalAlignment =
+        UIControlContentHorizontalAlignmentTrailing;
+  } else if (base::i18n::IsRTL()) {
+    leadingButton.contentHorizontalAlignment =
+        UIControlContentHorizontalAlignmentRight;
+    trailingButton.contentHorizontalAlignment =
+        UIControlContentHorizontalAlignmentLeft;
+  } else {
+    leadingButton.contentHorizontalAlignment =
+        UIControlContentHorizontalAlignmentLeft;
+    trailingButton.contentHorizontalAlignment =
+        UIControlContentHorizontalAlignmentRight;
+  }
+
   trailingButton.tintColor = UIColorFromRGB(kTabGridToolbarTextButtonColor);
   TabGridNewTabButton* centerButton = [TabGridNewTabButton
       buttonWithSizeClass:TabGridNewTabButtonSizeClassSmall];
@@ -76,6 +96,8 @@
                        constant:kTabGridBottomToolbarHeight / 2.0f],
     [trailingButton.heightAnchor
         constraintEqualToConstant:kTabGridBottomToolbarHeight],
+    [trailingButton.leadingAnchor
+        constraintEqualToAnchor:centerButton.trailingAnchor],
     [trailingButton.trailingAnchor
         constraintEqualToAnchor:self.layoutMarginsGuide.trailingAnchor],
     [trailingButton.topAnchor constraintEqualToAnchor:toolbar.topAnchor],
