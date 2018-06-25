5aa755659f8bf9f2e53f3e52cfa332f54b4adf9d
Oscar Johansson <oscarj@opera.com>
2018-06-25 07:15:58 +0000

    Rename kNameField to be unique (browser/ui)
    When building using Jumbo unnamed namespaces gets merged
    and variables with the same name conflict. This happens
    for the variables kNameField in webui/certficates_handler.cc
    and webui/inspect_ui.cc.
    
    This commit solves the issue by renaming the variables to
    something more file specific. It also renames other variables
    in the same namespace, so the naming is locally consistent.
    
    Bug: 773275
    Change-Id: I0b900803f4f84c058c43e1b5b3f395c8e14774f9
    Reviewed-on: https://chromium-review.googlesource.com/1107929
    Commit-Queue: Oscar Johansson <oscarj@opera.com>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#569975}
    Former-commit-id: 6b1a7bd534af9e4a162e459132e6bcc0e621052a

--- chrome/browser/ui/webui/inspect_ui.cc
+++ chrome/browser/ui/webui/inspect_ui.cc
@@ -38,41 +38,42 @@ using content::WebUIMessageHandler;
 
 namespace {
 
-const char kInitUICommand[]  = "init-ui";
-const char kInspectCommand[]  = "inspect";
-const char kInspectFallbackCommand[] = "inspect-fallback";
-const char kInspectAdditionalCommand[] = "inspect-additional";
-const char kActivateCommand[]  = "activate";
-const char kCloseCommand[] = "close";
-const char kReloadCommand[]  = "reload";
-const char kOpenCommand[]  = "open";
-const char kInspectBrowser[] = "inspect-browser";
-const char kLocalHost[] = "localhost";
+const char kInspectUiInitUICommand[] = "init-ui";
+const char kInspectUiInspectCommand[] = "inspect";
+const char kInspectUiInspectFallbackCommand[] = "inspect-fallback";
+const char kInspectUiInspectAdditionalCommand[] = "inspect-additional";
+const char kInspectUiActivateCommand[] = "activate";
+const char kInspectUiCloseCommand[] = "close";
+const char kInspectUiReloadCommand[] = "reload";
+const char kInspectUiOpenCommand[] = "open";
+const char kInspectUiInspectBrowser[] = "inspect-browser";
+const char kInspectUiLocalHost[] = "localhost";
 
-const char kDiscoverUsbDevicesEnabledCommand[] =
+const char kInspectUiDiscoverUsbDevicesEnabledCommand[] =
     "set-discover-usb-devices-enabled";
-const char kPortForwardingEnabledCommand[] =
+const char kInspectUiPortForwardingEnabledCommand[] =
     "set-port-forwarding-enabled";
-const char kPortForwardingConfigCommand[] = "set-port-forwarding-config";
-const char kDiscoverTCPTargetsEnabledCommand[] =
+const char kInspectUiPortForwardingConfigCommand[] =
+    "set-port-forwarding-config";
+const char kInspectUiDiscoverTCPTargetsEnabledCommand[] =
     "set-discover-tcp-targets-enabled";
-const char kTCPDiscoveryConfigCommand[] = "set-tcp-discovery-config";
-const char kOpenNodeFrontendCommand[] = "open-node-frontend";
+const char kInspectUiTCPDiscoveryConfigCommand[] = "set-tcp-discovery-config";
+const char kInspectUiOpenNodeFrontendCommand[] = "open-node-frontend";
 
-const char kPortForwardingDefaultPort[] = "8080";
-const char kPortForwardingDefaultLocation[] = "localhost:8080";
+const char kInspectUiPortForwardingDefaultPort[] = "8080";
+const char kInspectUiPortForwardingDefaultLocation[] = "localhost:8080";
 
-const char kNameField[] = "name";
-const char kUrlField[] = "url";
-const char kIsAdditionalField[] = "isAdditional";
+const char kInspectUiNameField[] = "name";
+const char kInspectUiUrlField[] = "url";
+const char kInspectUiIsAdditionalField[] = "isAdditional";
 
 void GetUiDevToolsTargets(base::ListValue& targets) {
   for (const auto& client_pair :
        ui_devtools::UiDevToolsServer::GetClientNamesAndUrls()) {
     auto target_data = std::make_unique<base::DictionaryValue>();
-    target_data->SetString(kNameField, client_pair.first);
-    target_data->SetString(kUrlField, client_pair.second);
-    target_data->SetBoolean(kIsAdditionalField, true);
+    target_data->SetString(kInspectUiNameField, client_pair.first);
+    target_data->SetString(kInspectUiUrlField, client_pair.second);
+    target_data->SetBoolean(kInspectUiIsAdditionalField, true);
     targets.Append(std::move(target_data));
   }
 }
@@ -111,69 +112,69 @@ class InspectMessageHandler : public WebUIMessageHandler {
 
 void InspectMessageHandler::RegisterMessages() {
   web_ui()->RegisterMessageCallback(
-      kInitUICommand,
+      kInspectUiInitUICommand,
       base::BindRepeating(&InspectMessageHandler::HandleInitUICommand,
                           base::Unretained(this)));
   web_ui()->RegisterMessageCallback(
-      kInspectCommand,
+      kInspectUiInspectCommand,
       base::BindRepeating(&InspectMessageHandler::HandleInspectCommand,
                           base::Unretained(this)));
   web_ui()->RegisterMessageCallback(
-      kInspectFallbackCommand,
+      kInspectUiInspectFallbackCommand,
       base::BindRepeating(&InspectMessageHandler::HandleInspectFallbackCommand,
                           base::Unretained(this)));
   web_ui()->RegisterMessageCallback(
-      kInspectAdditionalCommand,
+      kInspectUiInspectAdditionalCommand,
       base::BindRepeating(
           &InspectMessageHandler::HandleInspectAdditionalCommand,
           base::Unretained(this)));
   web_ui()->RegisterMessageCallback(
-      kActivateCommand,
+      kInspectUiActivateCommand,
       base::BindRepeating(&InspectMessageHandler::HandleActivateCommand,
                           base::Unretained(this)));
   web_ui()->RegisterMessageCallback(
-      kCloseCommand,
+      kInspectUiCloseCommand,
       base::BindRepeating(&InspectMessageHandler::HandleCloseCommand,
                           base::Unretained(this)));
   web_ui()->RegisterMessageCallback(
-      kDiscoverUsbDevicesEnabledCommand,
+      kInspectUiDiscoverUsbDevicesEnabledCommand,
       base::BindRepeating(&InspectMessageHandler::HandleBooleanPrefChanged,
                           base::Unretained(this),
                           &prefs::kDevToolsDiscoverUsbDevicesEnabled[0]));
   web_ui()->RegisterMessageCallback(
-      kPortForwardingEnabledCommand,
+      kInspectUiPortForwardingEnabledCommand,
       base::BindRepeating(&InspectMessageHandler::HandleBooleanPrefChanged,
                           base::Unretained(this),
                           &prefs::kDevToolsPortForwardingEnabled[0]));
   web_ui()->RegisterMessageCallback(
-      kPortForwardingConfigCommand,
+      kInspectUiPortForwardingConfigCommand,
       base::BindRepeating(
           &InspectMessageHandler::HandlePortForwardingConfigCommand,
           base::Unretained(this)));
   web_ui()->RegisterMessageCallback(
-      kDiscoverTCPTargetsEnabledCommand,
+      kInspectUiDiscoverTCPTargetsEnabledCommand,
       base::BindRepeating(&InspectMessageHandler::HandleBooleanPrefChanged,
                           base::Unretained(this),
                           &prefs::kDevToolsDiscoverTCPTargetsEnabled[0]));
   web_ui()->RegisterMessageCallback(
-      kTCPDiscoveryConfigCommand,
+      kInspectUiTCPDiscoveryConfigCommand,
       base::BindRepeating(
           &InspectMessageHandler::HandleTCPDiscoveryConfigCommand,
           base::Unretained(this)));
   web_ui()->RegisterMessageCallback(
-      kOpenNodeFrontendCommand,
+      kInspectUiOpenNodeFrontendCommand,
       base::BindRepeating(&InspectMessageHandler::HandleOpenNodeFrontendCommand,
                           base::Unretained(this)));
   web_ui()->RegisterMessageCallback(
-      kReloadCommand,
+      kInspectUiReloadCommand,
       base::BindRepeating(&InspectMessageHandler::HandleReloadCommand,
                           base::Unretained(this)));
   web_ui()->RegisterMessageCallback(
-      kOpenCommand,
+      kInspectUiOpenCommand,
       base::BindRepeating(&InspectMessageHandler::HandleOpenCommand,
                           base::Unretained(this)));
   web_ui()->RegisterMessageCallback(
-      kInspectBrowser,
+      kInspectUiInspectBrowser,
       base::BindRepeating(&InspectMessageHandler::HandleInspectBrowserCommand,
                           base::Unretained(this)));
 }
@@ -437,7 +438,7 @@ void InspectUI::InspectBrowserWithCustomFrontend(
     const GURL& frontend_url) {
   if (!frontend_url.SchemeIs(content::kChromeUIScheme) &&
       !frontend_url.SchemeIs(content::kChromeDevToolsScheme) &&
-      frontend_url.host() != kLocalHost) {
+      frontend_url.host() != kInspectUiLocalHost) {
     return;
   }
 
@@ -608,8 +609,8 @@ void InspectUI::SetPortForwardingDefaults() {
     return;
 
   base::DictionaryValue default_config;
-  default_config.SetString(
-      kPortForwardingDefaultPort, kPortForwardingDefaultLocation);
+  default_config.SetString(kInspectUiPortForwardingDefaultPort,
+                           kInspectUiPortForwardingDefaultLocation);
   prefs->Set(prefs::kDevToolsPortForwardingConfig, default_config);
 }
 
