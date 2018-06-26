c1306f104dbf9c09374a804aed58cee9c040609d
Yiming Zhou <uwyiming@google.com>
2018-06-26 17:38:09 +0000

    Update the Action Recorder Extension with a new design.
    The Action Recorder Extension is a Chrome Extension that records user actions inside Chrome and serialize the recording into a JSON test recipe file. An automation framework can then execute the test recipe file as a test. One such automation framework is the Autofill Desktop Automation Framework.
    
    Action Recorder Extension Eng Design Doc: https://docs.google.com/document/d/1JUigAf309sFkeiM7cNLnVlKJhxKKAr1kmYpR-FmaaWQ/edit#
    
    The new design makes a number of improvements over the previous version of the Extension. The old version has a number of issues, among these are:
    
    1. The extension violates Chrome Extension development best practice by using persistent background scripts. Persistent background scripts are bad for Chrome performance.
    2. The extension is unnecessarily intrusive. The extension records user actions on every tab regardless whether the user targets the tab for recording, and regardless of whether the user is actively recording a recipe. In other words, the extension records user action even when it shouldn't be recording.
    3. The old version has a number of bugs when generating unique XPath for HTML elements. The extension may generate an XPath that returns 0 elements, or an XPath that returns multiple elements.
    4. The extension hides errors and fails silently.
    
    In addition to addressing these problems, the new extension also adds the following features:
    1. The new extension records Chrome Autofill actions.
    2. The new extension prompts the user to download the recipe as a file when recording ends.
    3. The new extension displays a UI listing all the actions the extension has recorded in the recipe.
    
    Bug: 855284
    Change-Id: I35c8f0077082162881a35afdeb305fff4f8e7f87
    Reviewed-on: https://chromium-review.googlesource.com/1099734
    Commit-Queue: Yiming Zhou <uwyiming@google.com>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/master@{#570450}
    Former-commit-id: eaa5cfb30bcfc8c2454dea286e08a4045d2027c7

---
+++ components/test/data/autofill/automated_integration/action_recorder_extension/content/panel.js
@@ -0,0 +1,135 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const nameField = document.getElementById('recipeName');
+  const startingURLField = document.getElementById('startingURL');
+  const saveBtn = document.getElementById('saveBtn');
+  const stopBtn = document.getElementById('stopBtn');
+  const cancelBtn = document.getElementById('cancelBtn');
+  const actionsListDiv = document.getElementById('actions');
+
+  function createActionElement(action) {
+    const actionDiv = document.createElement('div');
+    actionDiv.classList.add('action');
+
+    // The action row.
+    const flexContainerDiv = document.createElement('div');
+    flexContainerDiv.classList.add('h-flex-container');
+    flexContainerDiv.classList.add('tooltip-trigger');
+    const selectorLabel = document.createElement('h4');
+    const actionLabel = document.createElement('h4');
+    const iconBoxDiv = document.createElement('div');
+    iconBoxDiv.classList.add('iconBox');
+    iconBoxDiv.textContent = 'X';
+    iconBoxDiv.addEventListener('click', (event) => {
+      sendRuntimeMessageToBackgroundScript({
+          type: RecorderUiMsgEnum.REMOVE_ACTION,
+          action_index: action.action_index })
+      .then((response) => {
+        if (response) {
+          actionDiv.remove();
+        }
+      })
+      .catch((error) => {
+        console.error('Unable to remove the recorded action!', error);
+      });
+    });
+    flexContainerDiv.appendChild(selectorLabel);
+    flexContainerDiv.appendChild(actionLabel);
+    flexContainerDiv.appendChild(iconBoxDiv);
+
+    // The action detail row.
+    const detailsDiv = document.createElement('div');
+    detailsDiv.classList.add('tooltip-body');
+    const selectorDetailLabel = document.createElement('label');
+    const actionDetailLabel = document.createElement('label');
+    detailsDiv.appendChild(selectorDetailLabel);
+    detailsDiv.appendChild(actionDetailLabel);
+
+    selectorLabel.textContent = action.selector;
+    selectorDetailLabel.textContent = action.selector;
+
+    switch (action.type) {
+      case 'click':
+        actionLabel.textContent = 'left-click';
+        actionDetailLabel.textContent = `left click element`;
+        break;
+      case 'hover':
+        actionLabel.textContent = 'hover';
+        actionDetailLabel.textContent = `hover over element`;
+        break;
+      case 'autofill':
+        actionLabel.textContent = 'trigger autofill';
+        actionDetailLabel.textContent = `trigger autofill`;
+        break;
+      case 'select':
+        actionLabel.textContent = 'select dropdown option';
+        actionDetailLabel.textContent = `select option ${action.index}`;
+        break;
+      case 'type':
+        actionLabel.textContent = 'type';
+        actionDetailLabel.textContent = `type '${action.value}'`;
+        break;
+      case 'validateField':
+        actionLabel.textContent = 'validate';
+        actionDetailLabel.textContent = `check that field
+            (${action.expectedAutofillType}) has the value
+            '${action.expectedValue}'`;
+        break;
+      default:
+        actionLabel.textContent = 'unknown';
+        actionDetailLabel.textContent = `unknown action: ${action.type}`;
+    }
+
+    flexContainerDiv.appendChild(detailsDiv);
+    actionDiv.appendChild(flexContainerDiv);
+    return actionDiv;
+  }
+
+  function addAction(action) {
+    actionsListDiv.appendChild(createActionElement(action));
+    // Scroll to the action that the panel just appended.
+    actionsListDiv.scrollTop = actionsListDiv.scrollHeight;
+  }
+
+  saveBtn.addEventListener('click', (event) => {
+    sendRuntimeMessageToBackgroundScript({ type: RecorderMsgEnum.SAVE });
+  });
+  stopBtn.addEventListener('click', (event) => {
+    sendRuntimeMessageToBackgroundScript({ type: RecorderMsgEnum.STOP });
+  });
+  cancelBtn.addEventListener('click', (event) => {
+    sendRuntimeMessageToBackgroundScript({ type: RecorderMsgEnum.CANCEL });
+  });
+
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (!request) return;
+    switch (request.type) {
+      case RecorderUiMsgEnum.ADD_ACTION:
+        addAction(request.action);
+        sendResponse(true);
+        break;
+      default:
+    }
+    return false;
+  });
+
+  sendRuntimeMessageToBackgroundScript(
+      { type: RecorderUiMsgEnum.GET_RECIPE })
+  .then((recipe) => {
+    while(actionsListDiv.hasChildNodes()) {
+      actionsListDiv.removeChild(actionsListDiv.lastChild);
+    }
+
+    nameField.value = recipe.name;
+    startingURLField.value = recipe.startingURL;
+
+    for (let index = 0; index < recipe.actions.length; index++) {
+      const action = recipe.actions[index];
+      actionsListDiv.appendChild(createActionElement(action));
+    }
+  });
+});
