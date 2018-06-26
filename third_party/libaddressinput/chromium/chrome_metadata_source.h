dbdd28f9169cc323acc35c8cb0e9649ad69d261b
Mark Pilgrim <pilgrim@chromium.org>
2018-06-26 18:11:40 +0000

    Migrate ChromeMetadataSource to SimpleURLLoader
    TBR=sky@chromium.org
    
    Bug: 844990
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I9f2caa470570053c539da83f9cac1d93f11a0b98
    Reviewed-on: https://chromium-review.googlesource.com/1101562
    Commit-Queue: Mark Pilgrim <pilgrim@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570464}
    Former-commit-id: bd08a0f6dccbda3e549e806b027327a6d2e5cefc

--- third_party/libaddressinput/chromium/chrome_metadata_source.h
+++ third_party/libaddressinput/chromium/chrome_metadata_source.h
@@ -5,59 +5,61 @@
 #ifndef THIRD_PARTY_LIBADDRESSINPUT_CHROMIUM_CHROME_METADATA_SOURCE_H_
 #define THIRD_PARTY_LIBADDRESSINPUT_CHROMIUM_CHROME_METADATA_SOURCE_H_
 
-#include <map>
+#include <list>
 #include <memory>
 #include <string>
 
 #include "base/macros.h"
-#include "net/url_request/url_fetcher_delegate.h"
+#include "base/memory/scoped_refptr.h"
 #include "third_party/libaddressinput/src/cpp/include/libaddressinput/source.h"
 
-namespace net {
-class URLFetcher;
-class URLRequestContextGetter;
+namespace network {
+class SharedURLLoaderFactory;
+class SimpleURLLoader;
 }
 
 namespace autofill {
 
 // A class for downloading rules to let libaddressinput validate international
 // addresses.
-class ChromeMetadataSource : public ::i18n::addressinput::Source,
-                             public net::URLFetcherDelegate {
+class ChromeMetadataSource : public ::i18n::addressinput::Source {
  public:
-  ChromeMetadataSource(const std::string& validation_data_url,
-                       net::URLRequestContextGetter* getter);
+  ChromeMetadataSource(
+      const std::string& validation_data_url,
+      scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory);
   virtual ~ChromeMetadataSource();
 
   // ::i18n::addressinput::Source:
   virtual void Get(const std::string& key,
                    const Callback& downloaded) const override;
 
-  // net::URLFetcherDelegate:
-  virtual void OnURLFetchComplete(const net::URLFetcher* source) override;
-
  private:
   struct Request {
     Request(const std::string& key,
-            std::unique_ptr<net::URLFetcher> fetcher,
+            std::unique_ptr<network::SimpleURLLoader> loader,
             const Callback& callback);
 
     std::string key;
     // The data that's received.
     std::string data;
     // The object that manages retrieving the data.
-    std::unique_ptr<net::URLFetcher> fetcher;
+    std::unique_ptr<network::SimpleURLLoader> loader;
     const Callback& callback;
   };
+
+  using RequestList = std::list<std::unique_ptr<Request>>;
 
   // Non-const method actually implementing Get().
   void Download(const std::string& key, const Callback& downloaded);
 
-  const std::string validation_data_url_;
-  net::URLRequestContextGetter* const getter_;  // weak
+  void OnSimpleLoaderComplete(RequestList::iterator it,
+                              std::unique_ptr<std::string> response_body);
 
-  // Maps from active URL fetcher to request metadata.
-  std::map<const net::URLFetcher*, std::unique_ptr<Request>> requests_;
+  const std::string validation_data_url_;
+  scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
+
+  // Holds all pending requests and their URL loaders.
+  RequestList requests_;
 
   DISALLOW_COPY_AND_ASSIGN(ChromeMetadataSource);
 };
