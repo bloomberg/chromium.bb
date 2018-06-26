fed6c59582eebc5cfb2d87b9d19de1f091a79b27
Dmitry Gozman <dgozman@chromium.org>
2018-06-26 16:41:49 +0000

    Revert "Reland "Settings: Content settings can now be reset for a group of origins in All Sites.""
    This reverts commit 077597b964e74ac5e144d290782a2377bb765a03 [formerly 112407912fd7aab9fb93add99131322a088559e4].
    
    Reason for revert: CrSettingsSiteDetailsTest.All consistently fails on linux-chromeos-dbg:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/linux-chromeos-dbg
    
    Original change's description:
    > Reland "Settings: Content settings can now be reset for a group of origins in All Sites."
    > 
    > This is a reland of 8f4bdaddad998deb36650ca574f361b9f6fe5f86 [formerly e2cac39f3c0aca0166794ea935e2244293aa9d4e]
    > 
    > Original change's description:
    > > Settings: Content settings can now be reset for a group of origins in All Sites.
    > > 
    > > Add a three-dot / overflow menu to groups of origins in All Sites. This overflow
    > > menu allows the entire list of origins to have all their content settings reset
    > > in one go. This will be preceded with a confirmation dialog.
    > > 
    > > Manual test - With #enable-site-settings turned on, change a content setting via
    > > Page Info > Site settings on both https://permission.site and
    > > http://permission.site. Navigate to chrome://settings/content/all and see that
    > > both sites are listed under 'permission.site'. Verify there is an overflow menu
    > > on this entry and that inside the overflow menu, there is a 'Reset permissions'
    > > option. Clicking this should bring up a reset permission dialog and confirming
    > > the dialog should remove this entry from All Sites.
    > > 
    > > Bug: 835712, 717468
    > > Cq-Include-Trybots: luci.chromium.try:closure_compilation
    > > Change-Id: I777f759c26bf0fe7f8da086fa887969897cf9833
    > > Reviewed-on: https://chromium-review.googlesource.com/1098577
    > > Commit-Queue: Patti <patricialor@chromium.org>
    > > Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#568742}
    > 
    > Bug: 835712, 717468
    > Change-Id: I7825326b8fa133d54971180bfd37494b19c5e86c
    > Cq-Include-Trybots: luci.chromium.try:closure_compilation
    > Reviewed-on: https://chromium-review.googlesource.com/1113037
    > Commit-Queue: Patti <patricialor@chromium.org>
    > Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#570309}
    
    TBR=dschuyler@chromium.org,patricialor@chromium.org
    
    Change-Id: Ia270a380442ebdd75014034a1ee980341a9b0f1f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 835712, 717468
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Reviewed-on: https://chromium-review.googlesource.com/1115179
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570427}
    Former-commit-id: 0cf209e50a20f7e7993decdc5f2947c755c26b31

--- chrome/browser/resources/settings/site_settings/site_details.js
+++ chrome/browser/resources/settings/site_settings/site_details.js
@@ -51,6 +51,47 @@ Polymer({
       },
     },
 
+    /** @private */
+    enableSafeBrowsingSubresourceFilter_: {
+      type: Boolean,
+      value: function() {
+        return loadTimeData.getBoolean('enableSafeBrowsingSubresourceFilter');
+      },
+    },
+
+    /** @private */
+    enableSoundContentSetting_: {
+      type: Boolean,
+      value: function() {
+        return loadTimeData.getBoolean('enableSoundContentSetting');
+      },
+    },
+
+    /** @private */
+    enableClipboardContentSetting_: {
+      type: Boolean,
+      value: function() {
+        return loadTimeData.getBoolean('enableClipboardContentSetting');
+      },
+    },
+
+    /** @private */
+    enableSensorsContentSetting_: {
+      type: Boolean,
+      readOnly: true,
+      value: function() {
+        return loadTimeData.getBoolean('enableSensorsContentSetting');
+      },
+    },
+
+    /** @private */
+    enablePaymentHandlerContentSetting_: {
+      type: Boolean,
+      value: function() {
+        return loadTimeData.getBoolean('enablePaymentHandlerContentSetting');
+      },
+    },
+
     /**
      * The type of storage for the origin.
      * @private
@@ -103,7 +144,7 @@ Polymer({
         if (this.enableSiteSettings_)
           this.$.usageApi.fetchUsageTotal(this.toUrl(this.origin).hostname);
 
-        this.updatePermissions_(this.getCategoryList());
+        this.updatePermissions_(this.getCategoryList_());
       }
     });
   },
@@ -121,7 +162,7 @@ Polymer({
         origin === undefined || origin == '') {
       return;
     }
-    if (!this.getCategoryList().includes(category))
+    if (!this.getCategoryList_().includes(category))
       return;
 
     // Site details currently doesn't support embedded origins, so ignore it and
@@ -198,20 +239,20 @@ Polymer({
    * Resets all permissions for the current origin.
    * @private
    */
-  onResetSettings_: function(e) {
+  onResetSettings_: function() {
     this.browserProxy.setOriginPermissions(
-        this.origin, this.getCategoryList(), settings.ContentSetting.DEFAULT);
-    if (this.getCategoryList().includes(settings.ContentSettingsTypes.PLUGINS))
+        this.origin, this.getCategoryList_(), settings.ContentSetting.DEFAULT);
+    if (this.getCategoryList_().includes(settings.ContentSettingsTypes.PLUGINS))
       this.browserProxy.clearFlashPref(this.origin);
 
-    this.onCloseDialog_(e);
+    this.$.confirmResetSettings.close();
   },
 
   /**
    * Clears all data stored, except cookies, for the current origin.
    * @private
    */
-  onClearStorage_: function(e) {
+  onClearStorage_: function() {
     // Since usage is only shown when "Site Settings" is enabled, don't clear it
     // when it's not shown.
     if (this.enableSiteSettings_ && this.storedData_ != '') {
@@ -219,7 +260,7 @@ Polymer({
           this.toUrl(this.origin).href, this.storageType_);
     }
 
-    this.onCloseDialog_(e);
+    this.$.confirmClearStorage.close();
   },
 
   /**
@@ -231,6 +272,20 @@ Polymer({
   onUsageDeleted_: function(event) {
     if (event.detail.origin == this.toUrl(this.origin).href)
       this.storedData_ = '';
+  },
+
+  /**
+   * Returns list of categories for each permission displayed in <site-details>.
+   * @return {!Array<!settings.ContentSettingsTypes>}
+   * @private
+   */
+  getCategoryList_: function() {
+    const categoryList = [];
+    this.root.querySelectorAll('site-details-permission').forEach((element) => {
+      if (!element.hidden)
+        categoryList.push(element.category);
+    });
+    return categoryList;
   },
 
   /**
