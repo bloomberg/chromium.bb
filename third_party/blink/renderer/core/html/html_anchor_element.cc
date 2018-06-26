4b6926c42d4e6bbf2d502f2ffb40c0a20ed6e82c
Jochen Eisinger <jochen@chromium.org>
2018-06-26 07:20:59 +0000

    Ignore suggested filename for URLs we can't download
    BUG=827083
    R=dtrainor@chromium.org
    
    Change-Id: Iffaa3adff0bb82cdeabf9af70a5952e6a50c7079
    Reviewed-on: https://chromium-review.googlesource.com/1113447
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570342}
    Former-commit-id: 280c895af0a1d870df902bf06e2fc5c44036421a

--- third_party/blink/renderer/core/html/html_anchor_element.cc
+++ third_party/blink/renderer/core/html/html_anchor_element.cc
@@ -369,11 +369,10 @@ void HTMLAnchorElement::HandleClick(Event* event) {
               : WebFeature::
                     kHTMLAnchorElementDownloadInSandboxWithoutUserGesture);
     }
-    // TODO(jochen): Only set the suggested filename for URLs we can request.
-    request.SetSuggestedFilename(
-        static_cast<String>(FastGetAttribute(downloadAttr)));
     if (GetDocument().GetSecurityOrigin()->CanReadContent(completed_url)) {
       // TODO(jochen): Handle cross origin server redirects.
+      request.SetSuggestedFilename(
+          static_cast<String>(FastGetAttribute(downloadAttr)));
       request.SetRequestContext(WebURLRequest::kRequestContextDownload);
       request.SetRequestorOrigin(SecurityOrigin::Create(GetDocument().Url()));
       frame->Client()->DownloadURL(request);
