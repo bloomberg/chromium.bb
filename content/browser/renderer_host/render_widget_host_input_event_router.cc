0bfb9c77cd6a81dde30e82d157448fa52b5a9e89
W. James MacLean <wjmaclean@chromium.org>
2018-06-27 13:48:46 +0000

    Confirm last_mouse_move_target_ still valid before context menu.
    Since the context menu can be dispatched an arbitrary time after
    the mouse event that sets last_mouse_move_target_, we should verify
    the pointer is still valid by making sure it's still in the owner's
    map.
    
    Bug: 856976
    Change-Id: I928935b74d6fa99ba7769fa170a9c817f98d5a46
    Reviewed-on: https://chromium-review.googlesource.com/1116862
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: James MacLean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#570749}
    Former-commit-id: e557b08415d5a9d24dc0345e5fc0828bf4b36812

--- content/browser/renderer_host/render_widget_host_input_event_router.cc
+++ content/browser/renderer_host/render_widget_host_input_event_router.cc
@@ -1543,7 +1543,11 @@ void RenderWidgetHostInputEventRouter::SetCursor(const WebCursor& cursor) {
 void RenderWidgetHostInputEventRouter::ShowContextMenuAtPoint(
     const gfx::Point& point,
     const ui::MenuSourceType source_type) {
-  DCHECK(last_mouse_move_target_);
+  // It's possible that since |last_mouse_move_target_| was set by the
+  // outbound mouse event that the view may have gone away. Before dispatching
+  // the context menu, confirm the view is still available.
+  if (!IsViewInMap(last_mouse_move_target_))
+    return;
 
   auto* rwhi = static_cast<RenderWidgetHostImpl*>(
       last_mouse_move_target_->GetRenderWidgetHost());
