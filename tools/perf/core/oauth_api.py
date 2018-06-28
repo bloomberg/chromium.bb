a3e40971ac442a62ddba33916a6ea31ae9bbc7f2
Emily Hanley <eyaich@google.com>
2018-06-28 18:50:17 +0000

    Parallelizing perf dashboard uploads
    This change also update the oauth token to generating
    ne per run to one per benchmark
    
    Bug:713357,854162
    Change-Id: If06e71c53fe8083f82307584a6e92104e33b2f65
    
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.perf:obbs_fyi
    
    Change-Id: If06e71c53fe8083f82307584a6e92104e33b2f65
    Reviewed-on: https://chromium-review.googlesource.com/1114690
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#571212}
    Former-commit-id: 681ba107f87635d29521442b150e84dcc1e2a05f

--- tools/perf/core/oauth_api.py
+++ tools/perf/core/oauth_api.py
@@ -13,13 +13,13 @@ import tempfile
 
 
 @contextlib.contextmanager
-def with_access_token(service_account_json):
+def with_access_token(service_account_json, append):
   """Yields an access token for the service account.
 
   Args:
     service_account_json: The path to the service account JSON file.
   """
-  fd, path = tempfile.mkstemp(suffix='.json', prefix='tok')
+  fd, path = tempfile.mkstemp(suffix='.json', prefix=append)
   try:
     args = ['luci-auth', 'token']
     if service_account_json:
