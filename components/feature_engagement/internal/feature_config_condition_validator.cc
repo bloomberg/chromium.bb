34503ada47243fcc4faa1c7d050f4cffb53be502
Hwanseung Lee <hs1217.lee@samsung.com>
2018-06-28 15:21:12 +0000

    Use ContainsValue() instead of std::find() at components -3-
    replace std::find() with base::containsValue()
    at components/certificate_transparency and components/policy
    and components/feature_engagement
    
    Bug: 561800
    Change-Id: I481fcc85d3d0043301991f08be47d1280a2585df
    Reviewed-on: https://chromium-review.googlesource.com/1114406
    Commit-Queue: Hwanseung Lee <hs1217.lee@samsung.com>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571130}
    Former-commit-id: 231c90d68e0381770aa90bd1adb22c9a5c0e205c

--- components/feature_engagement/internal/feature_config_condition_validator.cc
+++ components/feature_engagement/internal/feature_config_condition_validator.cc
@@ -9,6 +9,7 @@
 #include <vector>
 
 #include "base/feature_list.h"
+#include "base/stl_util.h"
 #include "components/feature_engagement/internal/availability_model.h"
 #include "components/feature_engagement/internal/configuration.h"
 #include "components/feature_engagement/internal/display_lock_controller.h"
@@ -79,8 +80,7 @@ void FeatureConfigConditionValidator::NotifyIsShowing(
       DCHECK(config.session_rate_impact.affected_features.has_value());
       for (const std::string& feature_name :
            config.session_rate_impact.affected_features.value()) {
-        DCHECK(std::find(all_feature_names.begin(), all_feature_names.end(),
-                         feature_name) != all_feature_names.end());
+        DCHECK(base::ContainsValue(all_feature_names, feature_name));
         ++times_shown_for_feature_[feature_name];
       }
       break;
