c8a3e31de2ebe43f6592d9499696fe59ab2d2c64
Antonio Gomes <tonikitoo@igalia.com>
2018-06-28 13:10:43 +0000

    Migrate client_update::SendProtocolRequest to SimpleURLLoader
    URLFetcher et al will stop working in the browser process with advent
    of Network Service, and SimpleURLLoader is the replacement API
    for most clients. CL migrates SendProtocolRequest to the new API.
    
    Major remarks:
    
    * URLRequestPostInterceptor is replaced by URLLoaderPostInterceptor although the public APIs
    of the former is kept (are almost the same) in the later. This allows the extensive set of unit
    tests to remain untouched. The replacement is needed because URLRequestPostInterceptor used
    to intercept requests instantiated by URLFetcher, precisely the class we are moving away to use.
    
    * URLLoaderPostInterceptor operates in two modes: it supports working both with TestURLLoaderFactory
    and EmbeddedTestServer. Two constructors are offered that control the 'mode' it operates on.
    This is needed because of the nature of the tests: some tests manipulate the TestConfigurator
    directly, hence the needed SharedURLLoaderFactory instance can be acquired directly from it,
    eg PingManagerTest, UpdateCheckerTest and RequestSenderTest.
    Some other tests, trigger the respective SimpleURLLoader machinery from upper layers, and then
    have no access to TestConfigurator instances. In such cases, URLLoaderPostInterceptor operates
    using EmbeddedTestServer in order to intercept URL loads.
    
    Note that this CL migrates components/update_client/utils.cc|h to SimpleURLLoader, but there
    some other classes on the same directory that still use URLFetcher (eg UrlFetcherDownloader),
    so we simply can not *yet* remove all the references to URLFetcher and URLRequestContextGetter.
    It will be done in a follow up CL.
    
    BUG=773295,844973
    
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I3d8be368818ba77b559907c0217995f8c6a95b04
    Reviewed-on: https://chromium-review.googlesource.com/1097337
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571097}
    Former-commit-id: ccd4ec0882bbc5d235b49188ca6cc3ac43c6c9d5

--- chrome/browser/extensions/updater/extension_update_client_base_browsertest.cc
+++ chrome/browser/extensions/updater/extension_update_client_base_browsertest.cc
@@ -11,7 +11,7 @@
 #include "chrome/browser/chrome_browser_main.h"
 #include "chrome/browser/chrome_browser_main_extra_parts.h"
 #include "chrome/browser/extensions/browsertest_util.h"
-#include "components/update_client/url_request_post_interceptor.h"
+#include "components/update_client/url_loader_post_interceptor.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_thread.h"
 #include "extensions/browser/updater/update_service.h"
@@ -105,16 +105,18 @@ class UpdateClientCompleteEventWaiter
 
 }  // namespace
 
-ExtensionUpdateClientBaseTest::ExtensionUpdateClientBaseTest() {}
+ExtensionUpdateClientBaseTest::ExtensionUpdateClientBaseTest()
+    : https_server_for_update_(net::EmbeddedTestServer::TYPE_HTTPS),
+      https_server_for_ping_(net::EmbeddedTestServer::TYPE_HTTPS) {}
 
 ExtensionUpdateClientBaseTest::~ExtensionUpdateClientBaseTest() {}
 
 std::vector<GURL> ExtensionUpdateClientBaseTest::GetUpdateUrls() const {
-  return {GURL("https://updatehost/service/update")};
+  return {https_server_for_update_.GetURL("/updatehost/service/update")};
 }
 
 std::vector<GURL> ExtensionUpdateClientBaseTest::GetPingUrls() const {
-  return {GURL("https://pinghost/service/ping")};
+  return {https_server_for_ping_.GetURL("/pinghost/service/ping")};
 }
 
 ConfigFactoryCallback
@@ -130,6 +132,9 @@ ExtensionUpdateClientBaseTest::ChromeUpdateClientConfigFactory() const {
 }
 
 void ExtensionUpdateClientBaseTest::SetUp() {
+  ASSERT_TRUE(https_server_for_update_.InitializeAndListen());
+  ASSERT_TRUE(https_server_for_ping_.InitializeAndListen());
+
   scoped_feature_list_.InitAndEnableFeature(
       features::kNewExtensionUpdaterService);
   ChromeUpdateClientConfig::SetChromeUpdateClientConfigFactoryForTesting(
@@ -161,20 +166,19 @@ void ExtensionUpdateClientBaseTest::SetUpNetworkInterceptors() {
   const auto update_urls = GetUpdateUrls();
   ASSERT_TRUE(!update_urls.empty());
   const GURL update_url = update_urls.front();
-  update_interceptor_factory_ =
-      std::make_unique<update_client::URLRequestPostInterceptorFactory>(
-          update_url.scheme(), update_url.host(), io_thread);
-  update_interceptor_ = update_interceptor_factory_->CreateInterceptor(
-      base::FilePath::FromUTF8Unsafe(update_url.path()));
+
+  update_interceptor_ =
+      std::make_unique<update_client::URLLoaderPostInterceptor>(
+          update_urls, &https_server_for_update_);
+  https_server_for_update_.StartAcceptingConnections();
 
   const auto ping_urls = GetPingUrls();
   ASSERT_TRUE(!ping_urls.empty());
   const GURL ping_url = ping_urls.front();
-  ping_interceptor_factory_ =
-      std::make_unique<update_client::URLRequestPostInterceptorFactory>(
-          ping_url.scheme(), ping_url.host(), io_thread);
-  ping_interceptor_ = ping_interceptor_factory_->CreateInterceptor(
-      base::FilePath::FromUTF8Unsafe(ping_url.path()));
+
+  ping_interceptor_ = std::make_unique<update_client::URLLoaderPostInterceptor>(
+      ping_urls, &https_server_for_ping_);
+  https_server_for_ping_.StartAcceptingConnections();
 
   get_interceptor_ = std::make_unique<net::LocalHostTestURLRequestInterceptor>(
       io_thread, base::CreateTaskRunnerWithTraits(
--- components/update_client/utils.h
+++ components/update_client/utils.h
@@ -24,9 +24,12 @@ class FilePath;
 
 namespace net {
 class URLFetcher;
-class URLFetcherDelegate;
-class URLRequestContextGetter;
 }
+
+namespace network {
+class SharedURLLoaderFactory;
+class SimpleURLLoader;
+}  // namespace network
 
 namespace update_client {
 
@@ -38,15 +41,18 @@ struct CrxComponent;
 // in an update check request.
 using InstallerAttribute = std::pair<std::string, std::string>;
 
+using LoadCompleteCallback =
+    base::OnceCallback<void(std::unique_ptr<std::string> response_body)>;
+
 // Sends a protocol request to the the service endpoint specified by |url|.
 // The body of the request is provided by |protocol_request| and it is
 // expected to contain XML data. The caller owns the returned object.
-std::unique_ptr<net::URLFetcher> SendProtocolRequest(
+std::unique_ptr<network::SimpleURLLoader> SendProtocolRequest(
     const GURL& url,
     const std::map<std::string, std::string>& protocol_request_extra_headers,
     const std::string& protocol_request,
-    net::URLFetcherDelegate* url_fetcher_delegate,
-    scoped_refptr<net::URLRequestContextGetter> url_request_context_getter);
+    LoadCompleteCallback callback,
+    scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory);
 
 // Returns true if the url request of |fetcher| was succesful.
 bool FetchSuccess(const net::URLFetcher& fetcher);
