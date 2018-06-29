858ee7ce4a6a60f7c8dc3fbb9c372bccc5a3f536
Yuly Novikov <ynovikov@chromium.org>
2018-06-29 20:52:29 +0000

    Ensure ANGLE-specific arguments are passed to ChildGTestProcess
    Bug: 839893
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I04433ae8f98448817a86f882fa70342c055a4633
    Reviewed-on: https://chromium-review.googlesource.com/1121039
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571622}
    Former-commit-id: caac66bc0d58305e84d0d5e32acff759742ee85f

--- gpu/angle_deqp_tests_main.cc
+++ gpu/angle_deqp_tests_main.cc
@@ -26,8 +26,14 @@ void InitTestHarness(int* argc, char** argv);
 }
 
 int main(int argc, char** argv) {
-  angle::InitTestHarness(&argc, argv);
+  // base::CommandLine::Init must be called before angle::InitTestHarness,
+  // because angle::InitTestHarness deletes ANGLE-specific arguments from argv.
+  // But, on Linux, tests are run in ChildGTestProcess, which inherits its
+  // command line from the one initialized in base::CommandLine::Init.
+  // In this order, ChildGTestProcess inherits all the ANGLE-specific
+  // arguments that it requires.
   base::CommandLine::Init(argc, argv);
+  angle::InitTestHarness(&argc, argv);
   base::TestSuite test_suite(argc, argv);
   int rt = base::LaunchUnitTestsSerially(
       argc,
