40591dfd5fdfb46ed182908ac3d1544fda8c89a8
Ned Nguyen <nednguyen@google.com>
2018-06-29 20:46:39 +0000

    Add validator script that validates correctness of perf json configs
    THis would help prevent issues like https://chromium-review.googlesource.com/c/chromium/src/+/1107213
    
    Bug: 713357
    Change-Id: Id43c20d5c232120232faf9568b5691721ccdbe47
    Reviewed-on: https://chromium-review.googlesource.com/1107302
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Emily Hanley <eyaich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571618}
    Former-commit-id: aba7760cfc44ff949261e50e2c71d3bfdbc0cfb6

--- tools/perf/PRESUBMIT.py
+++ tools/perf/PRESUBMIT.py
@@ -23,7 +23,10 @@ def _CommonChecks(input_api, output_api, block_on_failure=False):
 
   results.extend(_CheckExpectations(input_api, output_api))
   results.extend(_CheckJson(input_api, output_api))
-  results.extend(_CheckPerfData(input_api, output_api, block_on_failure))
+  results.extend(
+      _CheckPerfDataCurrentness(input_api, output_api, block_on_failure))
+  results.extend(
+      _CheckPerfJsonConfigs(input_api, output_api, block_on_failure))
   results.extend(_CheckWprShaFiles(input_api, output_api))
   results.extend(input_api.RunTests(input_api.canned_checks.GetPylint(
       input_api, output_api, extra_paths_list=_GetPathsToPrepend(input_api),
@@ -75,7 +78,7 @@ def _CheckExpectations(input_api, output_api):
   return results
 
 
-def _CheckPerfData(input_api, output_api, block_on_failure):
+def _CheckPerfDataCurrentness(input_api, output_api, block_on_failure):
   results = []
   perf_dir = input_api.PresubmitLocalPath()
   vpython = 'vpython.bat' if input_api.is_windows else 'vpython'
@@ -86,10 +89,27 @@ def _CheckPerfData(input_api, output_api, block_on_failure):
   if return_code:
     if block_on_failure:
       results.append(output_api.PresubmitError(
-          'Validating perf data failed', long_text=out))
+          'Validating perf data currentness failed', long_text=out))
     else:
       results.append(output_api.PresubmitPromptWarning(
-          'Validating perf data failed', long_text=out))
+          'Validating perf data currentness failed', long_text=out))
+  return results
+
+
+def _CheckPerfJsonConfigs(input_api, output_api, block_on_failure):
+  results = []
+  perf_dir = input_api.PresubmitLocalPath()
+  vpython = 'vpython.bat' if input_api.is_windows else 'vpython'
+  out, return_code = _RunArgs([
+      vpython,
+      input_api.os_path.join(perf_dir, 'validate_perf_json_config')], input_api)
+  if return_code:
+    if block_on_failure:
+      results.append(output_api.PresubmitError(
+          'Validating perf data correctness failed', long_text=out))
+    else:
+      results.append(output_api.PresubmitPromptWarning(
+          'Validating perf data correctness failed', long_text=out))
   return results
 
 
