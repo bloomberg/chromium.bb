4371e29df04922bca268b25f1d232c3c2639fc99
Tetsui Ohkubo <tetsui@chromium.org>
2018-06-29 05:46:44 +0000

    Add IME mode icon to UnifiedSystemTray.
    This CL restores IME mode icon to UnifiedSystemTray. The icon was
    removed as a part of UnifiedSystemTray & Shelf spec, but it turned out
    the icon is important especially for CJK users with laptop mode.
    We still hide the icon for tablet mode.
    
    TEST=manual
    BUG=854948
    
    Change-Id: I55bdd88627f91c9e285242b725c4a55451d0c60a
    Reviewed-on: https://chromium-review.googlesource.com/1116412
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#571395}
    Former-commit-id: c3b34354fd7572702bf619b1e4bf9b46d9974b6f

---
+++ ash/system/unified/ime_mode_view.h
@@ -0,0 +1,39 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SYSTEM_UNIFIED_IME_MODE_VIEW_H_
+#define ASH_SYSTEM_UNIFIED_IME_MODE_VIEW_H_
+
+#include "ash/system/ime/ime_observer.h"
+#include "ash/system/tray/tray_item_view.h"
+#include "ash/wm/tablet_mode/tablet_mode_observer.h"
+#include "base/macros.h"
+
+namespace ash {
+
+// An IME mode icon view in UnifiedSystemTray button.
+class ImeModeView : public TrayItemView,
+                    public IMEObserver,
+                    public TabletModeObserver {
+ public:
+  ImeModeView();
+  ~ImeModeView() override;
+
+  // IMEObserver:
+  void OnIMERefresh() override;
+  void OnIMEMenuActivationChanged(bool is_active) override;
+
+  // TabletModeObserver:
+  void OnTabletModeStarted() override;
+  void OnTabletModeEnded() override;
+
+ private:
+  void Update();
+
+  DISALLOW_COPY_AND_ASSIGN(ImeModeView);
+};
+
+}  // namespace ash
+
+#endif  // ASH_SYSTEM_UNIFIED_IME_MODE_VIEW_H_
