d73b40c814646342de4c0b4ca534f347569c4cb5
Matt Swartwout <mwswartwout@google.com>
2018-07-02 17:56:23 +0000

    Enable MockGattClientManager to have > 1 observer
    Bug: None
    Test: Unit tests that add/remove more than 1 observer now pass.
    Change-Id: I097eaa3bea90501e9325b969963e2e02caf62878
    Reviewed-on: https://chromium-review.googlesource.com/1119627
    Reviewed-by: Stephen Lanham <slan@chromium.org>
    Commit-Queue: Matt Swartwout <mwswartwout@google.com>
    Cr-Commit-Position: refs/heads/master@{#571940}
    Former-commit-id: 318d6767bb5bc3e160d229373f5fe37f82940ab1

--- chromecast/device/bluetooth/le/mock_gatt_client_manager.h
+++ chromecast/device/bluetooth/le/mock_gatt_client_manager.h
@@ -5,6 +5,7 @@
 #ifndef CHROMECAST_DEVICE_BLUETOOTH_LE_MOCK_GATT_CLIENT_MANAGER_H_
 #define CHROMECAST_DEVICE_BLUETOOTH_LE_MOCK_GATT_CLIENT_MANAGER_H_
 
+#include "base/containers/flat_set.h"
 #include "chromecast/device/bluetooth/le/gatt_client_manager.h"
 #include "chromecast/device/bluetooth/le/mock_remote_device.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -18,12 +19,12 @@ class MockGattClientManager : public GattClientManager {
   ~MockGattClientManager();
 
   void AddObserver(Observer* o) override {
-    DCHECK(o && !observer_);
-    observer_ = o;
+    DCHECK(o && !observers_.count(o));
+    observers_.insert(o);
   }
   void RemoveObserver(Observer* o) override {
-    DCHECK(o && o == observer_);
-    observer_ = nullptr;
+    DCHECK(o && observers_.count(o));
+    observers_.erase(o);
   }
 
   MOCK_METHOD1(
@@ -42,7 +43,7 @@ class MockGattClientManager : public GattClientManager {
   MOCK_METHOD1(NotifyConnect, void(const bluetooth_v2_shlib::Addr& addr));
   MOCK_METHOD0(task_runner, scoped_refptr<base::SingleThreadTaskRunner>());
 
-  Observer* observer_ = nullptr;
+  base::flat_set<Observer*> observers_;
 };
 
 }  // namespace bluetooth
