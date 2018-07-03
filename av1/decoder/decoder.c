b390df92048c40fc3956e3863f825ff2b8d4adf5
Hui Su <huisu@google.com>
2018-07-03 10:59:07 -0700

    Release frame buffer after failed frame decoding
    If a frame decoding fails, its allocated frame buffer should be
    released.
    
    BUG=oss-fuzz:9127
    
    Change-Id: I2265ab5e470ecb90adcb9dd1176200abe2b7b343

--- av1/decoder/decoder.c
+++ av1/decoder/decoder.c
@@ -474,7 +474,10 @@ int av1_receive_compressed_data(AV1Decoder *pbi, size_t size,
   int frame_decoded =
       aom_decode_frame_from_obus(pbi, source, source + size, psource);
 
-  if (cm->error.error_code != AOM_CODEC_OK) return 1;
+  if (cm->error.error_code != AOM_CODEC_OK) {
+    cm->cur_frame->ref_count = 0;
+    return 1;
+  }
 
 #if TXCOEFF_TIMER
   cm->cum_txcoeff_timer += cm->txcoeff_timer;
