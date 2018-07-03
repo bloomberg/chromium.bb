6f61d268fb279d4742b46a3871cc40ff7ee4b120
Timothy Gu <timothygu@chromium.org>
2018-07-03 19:20:27 +0000

    More granular UseCounter for document.open()
    Bug: 859691
    Change-Id: I9f1fbb3dac9b794dcf1f069447096c8559e03d5b
    Reviewed-on: https://chromium-review.googlesource.com/1123202
    Commit-Queue: Timothy Gu <timothygu@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572316}
    Former-commit-id: 136369cac7fc33a14bea3c6650af8ac3bbaf6813

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -3205,6 +3205,10 @@ Document* Document::open(LocalDOMWindow* entered_window,
                          const AtomicString& type,
                          const AtomicString& replace,
                          ExceptionState& exception_state) {
+  if (replace == "replace") {
+    UseCounter::Count(entered_window->document(),
+                      WebFeature::kDocumentOpenTwoArgsWithReplace);
+  }
   open(entered_window->document(), exception_state);
   return this;
 }
