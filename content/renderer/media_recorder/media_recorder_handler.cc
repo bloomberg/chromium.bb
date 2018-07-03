a7ea58b9f05316799fdee10efba766bebb16e7f0
Miguel Casas <mcasas@chromium.org>
2018-07-03 14:45:43 +0000

    MediaRecorder: Retrieve actual recording mime type if not user-specified
    This CL introduces a new  MediaRecorderHandler::ActualMimeType() method
    that produces the mimeType used for recording by MRHandler. This method
    is then called from Blink's MediaRecorder when the user has specified
    no mime type for recording, so the output Blobs can have the correct
    type.
    
    Bug: 859199
    Change-Id: I941f46a2ef495f5ac68995f79319fe6f07836a89
    Reviewed-on: https://chromium-review.googlesource.com/1121141
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572207}
    Former-commit-id: 59f0d053cfa8bfe6f20d7c1d20503d139e0182e2

--- content/renderer/media_recorder/media_recorder_handler.cc
+++ content/renderer/media_recorder/media_recorder_handler.cc
@@ -123,8 +123,8 @@ MediaRecorderHandler::MediaRecorderHandler(
     scoped_refptr<base::SingleThreadTaskRunner> task_runner)
     : video_bits_per_second_(0),
       audio_bits_per_second_(0),
-      video_codec_id_(VideoTrackRecorder::CodecId::VP8),
-      audio_codec_id_(AudioTrackRecorder::CodecId::OPUS),
+      video_codec_id_(VideoTrackRecorder::CodecId::LAST),
+      audio_codec_id_(AudioTrackRecorder::CodecId::LAST),
       recording_(false),
       client_(nullptr),
       task_runner_(std::move(task_runner)),
@@ -397,6 +397,72 @@ void MediaRecorderHandler::EncodingInfo(
            << " and" << (info->smooth ? " smooth" : " NOT smooth");
 
   scoped_callbacks.PassCallbacks()->OnSuccess(std::move(info));
+}
+
+blink::WebString MediaRecorderHandler::ActualMimeType() {
+  DCHECK(main_render_thread_checker_.CalledOnValidThread());
+  DCHECK(client_) << __func__ << " should be called after Initialize()";
+
+  const bool has_video_tracks = !media_stream_.VideoTracks().empty();
+  const bool has_audio_tracks = !media_stream_.AudioTracks().empty();
+  if (!has_video_tracks && !has_audio_tracks)
+    return blink::WebString();
+
+  std::string mime_type;
+  if (!has_video_tracks && has_audio_tracks) {
+    mime_type.append("audio/webm;codecs=");
+  } else {
+    switch (video_codec_id_) {
+      case VideoTrackRecorder::CodecId::VP8:
+      case VideoTrackRecorder::CodecId::VP9:
+        mime_type.append("video/webm;codecs=");
+        break;
+#if BUILDFLAG(RTC_USE_H264)
+      case VideoTrackRecorder::CodecId::H264:
+        mime_type.append("video/x-matroska;codecs=");
+        break;
+#endif
+      case VideoTrackRecorder::CodecId::LAST:
+        // Do nothing.
+        break;
+    }
+  }
+  if (has_video_tracks) {
+    switch (video_codec_id_) {
+      case VideoTrackRecorder::CodecId::VP8:
+        mime_type.append("vp8");
+        break;
+      case VideoTrackRecorder::CodecId::VP9:
+        mime_type.append("vp9");
+        break;
+#if BUILDFLAG(RTC_USE_H264)
+      case VideoTrackRecorder::CodecId::H264:
+        mime_type.append("avc1");
+        break;
+#endif
+      case VideoTrackRecorder::CodecId::LAST:
+        DCHECK_NE(audio_codec_id_, AudioTrackRecorder::CodecId::LAST);
+    }
+  }
+  if (has_video_tracks && has_audio_tracks) {
+    if (video_codec_id_ != VideoTrackRecorder::CodecId::LAST &&
+        audio_codec_id_ != AudioTrackRecorder::CodecId::LAST) {
+      mime_type.append(",");
+    }
+  }
+  if (has_audio_tracks) {
+    switch (audio_codec_id_) {
+      case AudioTrackRecorder::CodecId::OPUS:
+        mime_type.append("opus");
+        break;
+      case AudioTrackRecorder::CodecId::PCM:
+        mime_type.append("pcm");
+        break;
+      case AudioTrackRecorder::CodecId::LAST:
+        DCHECK_NE(video_codec_id_, VideoTrackRecorder::CodecId::LAST);
+    }
+  }
+  return blink::WebString::FromUTF8(mime_type);
 }
 
 void MediaRecorderHandler::OnEncodedVideo(
