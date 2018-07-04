7d890a6c64169129da205e0a5457832e8575a73b
Sergey Ulanov <sergeyu@chromium.org>
2018-07-04 01:17:07 +0000

    [Fuchsia] Update ScopedServiceBinding to use BindingSet.
    Previously ScopedServiceBinding was using fidl::Binding instead of
    BindingSet, which didn't allow multiple bindings. Also added some new
    unittests for ServiceDirectory to validate cases that were not handled
    previously. That required an update in ComponentContext class: it now
    can handle the case when the target directory is gone.
    
    Change-Id: I7975275bebfba716d97c3f41d13046172385aff7
    Reviewed-on: https://chromium-review.googlesource.com/1125308
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572426}
    Former-commit-id: b49869e69fda522dd8e5a1fd80d8e4aa782c61bc

--- base/fuchsia/service_directory_unittest.cc
+++ base/fuchsia/service_directory_unittest.cc
@@ -25,101 +25,116 @@ class TestInterfaceImpl : public test_fidl::TestInterface {
   }
 };
 
-// Verifies that a service connected by ServiceDirectory can be imported from
-// another ServiceDirectory.
-TEST(ServiceDirectoryTest, Connect) {
+class ServiceDirectoryTest : public testing::Test {
+ public:
+  ServiceDirectoryTest() {
+    zx::channel service_directory_channel;
+    EXPECT_EQ(zx::channel::create(0, &service_directory_channel,
+                                  &service_directory_client_channel_),
+              ZX_OK);
+
+    // Mount service dir and publish the service.
+    service_directory_ = std::make_unique<ServiceDirectory>(
+        std::move(service_directory_channel));
+    service_binding_ =
+        std::make_unique<ScopedServiceBinding<test_fidl::TestInterface>>(
+            service_directory_.get(), &test_service_);
+
+    ConnectClientContextToDirectory("public");
+  }
+
+  void ConnectClientContextToDirectory(const char* path) {
+    // Open directory |path| from the service directory.
+    zx::channel public_directory_channel;
+    zx::channel public_directory_client_channel;
+    EXPECT_EQ(zx::channel::create(0, &public_directory_channel,
+                                  &public_directory_client_channel),
+              ZX_OK);
+    EXPECT_EQ(fdio_open_at(service_directory_client_channel_.get(), path, 0,
+                           public_directory_channel.release()),
+              ZX_OK);
+
+    // Create ComponentContext and connect to the test service.
+    client_context_ = std::make_unique<ComponentContext>(
+        std::move(public_directory_client_channel));
+  }
+
+  void VerifyTestInterface(fidl::InterfacePtr<test_fidl::TestInterface>* stub,
+                           bool expect_error) {
+    // Call the service and wait for response.
+    base::RunLoop run_loop;
+    bool error = false;
+
+    stub->set_error_handler([&run_loop, &error]() {
+      error = true;
+      run_loop.Quit();
+    });
+
+    (*stub)->Add(2, 2, [&run_loop](int32_t result) {
+      EXPECT_EQ(result, 4);
+      run_loop.Quit();
+    });
+
+    run_loop.Run();
+
+    EXPECT_EQ(error, expect_error);
+
+    // Reset error handler because the current one captures |run_loop| and
+    // |error| references which are about to be destroyed.
+    stub->set_error_handler([]() {});
+  }
+
+ protected:
   MessageLoopForIO message_loop_;
+  std::unique_ptr<ServiceDirectory> service_directory_;
+  zx::channel service_directory_client_channel_;
+  TestInterfaceImpl test_service_;
+  std::unique_ptr<ScopedServiceBinding<test_fidl::TestInterface>>
+      service_binding_;
+  std::unique_ptr<ComponentContext> client_context_;
+};
 
-  zx::channel dir_service_channel;
-  zx::channel dir_client_channel;
-  ASSERT_EQ(zx::channel::create(0, &dir_service_channel, &dir_client_channel),
-            ZX_OK);
+// Verifies that ComponentContext can consume a public service in
+// ServiceDirectory.
+TEST_F(ServiceDirectoryTest, Connect) {
+  auto stub = client_context_->ConnectToService<test_fidl::TestInterface>();
+  VerifyTestInterface(&stub, false);
+}
 
-  // Mount service dir and publish the service.
-  ServiceDirectory service_dir(std::move(dir_service_channel));
-  TestInterfaceImpl test_service;
-  ScopedServiceBinding<test_fidl::TestInterface> service_binding(&service_dir,
-                                                                 &test_service);
-
-  // Open public directory from the service directory.
-  zx::channel public_dir_service_channel;
-  zx::channel public_dir_client_channel;
-  ASSERT_EQ(zx::channel::create(0, &public_dir_service_channel,
-                                &public_dir_client_channel),
-            ZX_OK);
-  ASSERT_EQ(fdio_open_at(dir_client_channel.get(), "public", 0,
-                         public_dir_service_channel.release()),
-            ZX_OK);
-
-  // Create ComponentContext and connect to the test service.
-  ComponentContext client_context(std::move(public_dir_client_channel));
-  auto stub = client_context.ConnectToService<test_fidl::TestInterface>();
-
-  // Call the service and wait for response.
-  base::RunLoop run_loop;
-  bool error = false;
-
-  stub.set_error_handler([&run_loop, &error]() {
-    error = true;
-    run_loop.Quit();
-  });
-
-  stub->Add(2, 2, [&run_loop](int32_t result) {
-    EXPECT_EQ(result, 4);
-    run_loop.Quit();
-  });
-
-  run_loop.Run();
-
-  EXPECT_FALSE(error);
+// Verifies that we can connect to the service service more than once.
+TEST_F(ServiceDirectoryTest, ConnectMulti) {
+  auto stub = client_context_->ConnectToService<test_fidl::TestInterface>();
+  auto stub2 = client_context_->ConnectToService<test_fidl::TestInterface>();
+  VerifyTestInterface(&stub, false);
+  VerifyTestInterface(&stub2, false);
 }
 
 // Verify that services are also exported to the legacy flat service namespace.
-TEST(ServiceDirectoryTest, ConnectLegacy) {
-  MessageLoopForIO message_loop_;
+TEST_F(ServiceDirectoryTest, ConnectLegacy) {
+  ConnectClientContextToDirectory(".");
+  auto stub = client_context_->ConnectToService<test_fidl::TestInterface>();
+  VerifyTestInterface(&stub, false);
+}
 
-  zx::channel dir_service_channel;
-  zx::channel dir_client_channel;
-  ASSERT_EQ(zx::channel::create(0, &dir_service_channel, &dir_client_channel),
-            ZX_OK);
+// Verify that ComponentContext can handle the case when the service directory
+// connection is disconnected.
+TEST_F(ServiceDirectoryTest, DirectoryGone) {
+  service_binding_.reset();
+  service_directory_.reset();
 
-  // Mount service dir and publish the service.
-  ServiceDirectory service_dir(std::move(dir_service_channel));
-  TestInterfaceImpl test_service;
-  ScopedServiceBinding<test_fidl::TestInterface> service_binding(&service_dir,
-                                                                 &test_service);
+  fidl::InterfacePtr<test_fidl::TestInterface> stub;
+  zx_status_t status =
+      client_context_->ConnectToService(FidlInterfaceRequest(&stub));
+  EXPECT_EQ(status, ZX_ERR_PEER_CLOSED);
 
-  // Open public directory from the service directory.
-  zx::channel public_dir_service_channel;
-  zx::channel public_dir_client_channel;
-  ASSERT_EQ(zx::channel::create(0, &public_dir_service_channel,
-                                &public_dir_client_channel),
-            ZX_OK);
-  ASSERT_EQ(fdio_open_at(dir_client_channel.get(), ".", 0,
-                         public_dir_service_channel.release()),
-            ZX_OK);
+  VerifyTestInterface(&stub, true);
+}
 
-  // Create ComponentContext and connect to the test service.
-  ComponentContext client_context(std::move(public_dir_client_channel));
-  auto stub = client_context.ConnectToService<test_fidl::TestInterface>();
-
-  // Call the service and wait for response.
-  base::RunLoop run_loop;
-  bool error = false;
-
-  stub.set_error_handler([&run_loop, &error]() {
-    error = true;
-    run_loop.Quit();
-  });
-
-  stub->Add(2, 2, [&run_loop](int32_t result) {
-    EXPECT_EQ(result, 4);
-    run_loop.Quit();
-  });
-
-  run_loop.Run();
-
-  EXPECT_FALSE(error);
+// Verify that the case when the service doesn't exist is handled properly.
+TEST_F(ServiceDirectoryTest, NoService) {
+  service_binding_.reset();
+  auto stub = client_context_->ConnectToService<test_fidl::TestInterface>();
+  VerifyTestInterface(&stub, true);
 }
 
 }  // namespace fuchsia
