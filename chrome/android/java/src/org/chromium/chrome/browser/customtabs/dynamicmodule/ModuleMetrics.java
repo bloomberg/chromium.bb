4c1a522287ce261a63bcbe55fbe43ef3dc9cb414
Michael van Ouwerkerk <mvanouwerkerk@google.com>
2018-07-05 16:29:34 +0000

    Record the result of attempting to load a module.
    Bug: 843161
    Change-Id: I51a056bd41c5897bfcf529fefe0f3beb340eda47
    Reviewed-on: https://chromium-review.googlesource.com/1126920
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572812}
    Former-commit-id: 2642babf83b6e0e7c25c045957260f5ef9873e78

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/dynamicmodule/ModuleMetrics.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/dynamicmodule/ModuleMetrics.java
@@ -5,9 +5,13 @@
 package org.chromium.chrome.browser.customtabs.dynamicmodule;
 
 import android.os.SystemClock;
+import android.support.annotation.IntDef;
 
+import org.chromium.base.Log;
 import org.chromium.base.metrics.RecordHistogram;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -15,6 +19,61 @@ import java.util.concurrent.TimeUnit;
  */
 public final class ModuleMetrics {
     private ModuleMetrics() {}
+
+    private static final String TAG = "ModuleMetrics";
+
+    /**
+     * Possible results when loading a dynamic module. Keep in sync with the
+     * CustomTabs.DynamicModule.LoadResult enum in histograms.xml. Do not remove or change existing
+     * values other than LOAD_RESULT_BOUNDARY.
+     */
+    @IntDef({LOAD_RESULT_SUCCESS_NEW, LOAD_RESULT_SUCCESS_CACHED, LOAD_RESULT_FEATURE_DISABLED,
+            LOAD_RESULT_NOT_GOOGLE_SIGNED, LOAD_RESULT_PACKAGE_NAME_NOT_FOUND_EXCEPTION,
+            LOAD_RESULT_CLASS_NOT_FOUND_EXCEPTION, LOAD_RESULT_INSTANTIATION_EXCEPTION,
+            LOAD_RESULT_INCOMPATIBLE_VERSION})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface LoadResult {}
+
+    /** A new instance of the module was loaded successfully. */
+    public static final int LOAD_RESULT_SUCCESS_NEW = 0;
+
+    /** A cached instance of the module was used. */
+    public static final int LOAD_RESULT_SUCCESS_CACHED = 1;
+
+    /** The module could not be loaded because the feature is disabled. */
+    public static final int LOAD_RESULT_FEATURE_DISABLED = 2;
+
+    /** The module could not be loaded because the package is not Google-signed. */
+    public static final int LOAD_RESULT_NOT_GOOGLE_SIGNED = 3;
+
+    /** The module could not be loaded because the package name could not be found. */
+    public static final int LOAD_RESULT_PACKAGE_NAME_NOT_FOUND_EXCEPTION = 4;
+
+    /** The module could not be loaded because the entry point class could not be found. */
+    public static final int LOAD_RESULT_CLASS_NOT_FOUND_EXCEPTION = 5;
+
+    /** The module could not be loaded because the entry point class could not be instantiated. */
+    public static final int LOAD_RESULT_INSTANTIATION_EXCEPTION = 6;
+
+    /** The module was loaded but the host and module versions are incompatible. */
+    public static final int LOAD_RESULT_INCOMPATIBLE_VERSION = 7;
+
+    /** Upper bound for legal sample values - all sample values have to be strictly lower. */
+    private static final int LOAD_RESULT_BOUNDARY = 8;
+
+    /**
+     * Records the result of attempting to load a dynamic module.
+     * @param result result key, one of {@link LoadResult}'s values.
+     */
+    public static void recordLoadResult(@LoadResult int result) {
+        assert result >= 0;
+        assert result < LOAD_RESULT_BOUNDARY;
+        RecordHistogram.recordEnumeratedHistogram(
+                "CustomTabs.DynamicModule.LoadResult", result, LOAD_RESULT_BOUNDARY);
+        if (result != LOAD_RESULT_SUCCESS_NEW && result != LOAD_RESULT_SUCCESS_CACHED) {
+            Log.w(TAG, "Did not load module, result: %s", result);
+        }
+    }
 
     /**
      * SystemClock.uptimeMillis() is used here as it uses the same system call as all the native
