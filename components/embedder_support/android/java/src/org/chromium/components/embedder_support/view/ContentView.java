4387524bcd3534fafb0ac14be51a74a10a5a3d4e
Jinsuk Kim <jinsukkim@chromium.org>
2018-07-05 06:32:05 +0000

    Android: Merge components/content_view to embedder_support
    Consolidates Android support libraries under components into
    embedder_support.
    
    Change-Id: I7ad89d6b1b515a41c16509f2150fb1e2d0050e34
    Reviewed-on: https://chromium-review.googlesource.com/1115240
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#572720}
    Former-commit-id: d86d7597c36d47a6340a2294255d707645a2e259

--- components/embedder_support/android/java/src/org/chromium/components/embedder_support/view/ContentView.java
+++ components/embedder_support/android/java/src/org/chromium/components/embedder_support/view/ContentView.java
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.components.content_view;
+package org.chromium.components.embedder_support.view;
 
 import android.content.Context;
 import android.content.res.Configuration;
