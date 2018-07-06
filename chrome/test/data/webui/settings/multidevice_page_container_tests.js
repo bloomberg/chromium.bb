33e49daf2799f2fa15285bf08cbc41302d0d815c
Jordy Greenblatt <jordynass@chromium.org>
2018-07-06 21:33:08 +0000

    [CrOS MultiDevice]: JavaScript side for getPageContentData IPC
    In order for the MultiDevice page to render in a useful way, we need a
    few pieces of user data ASAP in its lifecycle. This includes the status
    of a potential or already set "host device" on the account (i.e. a
    phone to connect to the user's Chromebook(s)) and the multidevice
    features it supports.
    
    This CL takes care of the Javascript code necessary for the multidevice-
    page-container element to retrieve this data and pass is to the page
    when it first attaches the page. The actual functionality will require
    changes to the MultiDevice browser proxy handler (in C++), the that
    task is effectively invisible to this one.
    
    Change-Id: I7a0b618f3fa398e9435f368c5985d0eeed12a85a
    Reviewed-on: https://chromium-review.googlesource.com/1123225
    Commit-Queue: Jordy Greenblatt <jordynass@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573076}
    Former-commit-id: 6f0eadd7e19600704d74e63b810b38b80bb5b59d

--- chrome/test/data/webui/settings/multidevice_page_container_tests.js
+++ chrome/test/data/webui/settings/multidevice_page_container_tests.js
@@ -2,10 +2,53 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/**
+ * @implements {settings.MultideviceBrowserProxy}
+ * Note: showMultiDeviceSetupDialog is not used by the
+ * multidevice-page-container element.
+ */
+class TestMultideviceBrowserProxy extends TestBrowserProxy {
+  constructor(initialPageContentData) {
+    super([
+      'showMultiDeviceSetupDialog',
+      'getPageContentData',
+    ]);
+    this.data = initialPageContentData;
+  }
+
+  /** @override */
+  getPageContentData() {
+    this.methodCalled('getPageContentData');
+    return Promise.resolve(this.data);
+  }
+}
+
 suite('Multidevice', function() {
   let multidevicePageContainer = null;
+  let browserProxy = null;
   let ALL_MODES;
   let HOST_SET_MODES;
+
+  /**
+   * @param {!settings.MultiDeviceSettingsMode}
+   * @return {!MultiDevicePageContentData}
+   */
+  function getFakePageContentData(mode) {
+    return {
+      mode: mode,
+      hostDevice: HOST_SET_MODES.includes(mode) ? {name: 'Pixel XL'} : null,
+    };
+  }
+
+  /**
+   * @param {!settings.MultiDeviceSettingsMode}
+   */
+  function changeHostStatus(newMode) {
+    cr.webUIListenerCallback(
+        'settings.updateMultidevicePageContentData',
+        getFakePageContentData(newMode));
+    Polymer.dom.flush();
+  }
 
   suiteSetup(function() {
     ALL_MODES = Object.values(settings.MultiDeviceSettingsMode);
@@ -16,65 +59,90 @@ suite('Multidevice', function() {
     ];
   });
 
-  setup(function() {
+  /** @return {?HTMLElement} */
+  const getMultidevicePage = () =>
+      multidevicePageContainer.$$('settings-multidevice-page');
+
+  // Initializes page container with provided mode and returns a promise that
+  // only resolves when the multidevice-page has changed its pageContentData and
+  // the ensuing callbacks back been flushed.
+  /**
+   * @param {!settings.MultiDeviceSettingsMode}
+   * @return {!Promise}
+   */
+  function setInitialHostStatus(mode) {
+    if (multidevicePageContainer)
+      multidevicePageContainer.remove();
+    browserProxy =
+        new TestMultideviceBrowserProxy(getFakePageContentData(mode));
+    settings.MultiDeviceBrowserProxyImpl.instance_ = browserProxy;
+
     PolymerTest.clearBody();
     multidevicePageContainer =
         document.createElement('settings-multidevice-page-container');
     assertTrue(!!multidevicePageContainer);
 
     document.body.appendChild(multidevicePageContainer);
-    Polymer.dom.flush();
+
+    return browserProxy.whenCalled('getPageContentData')
+        .then(Polymer.dom.flush());
+  }
+
+  test('WebUIListener toggles multidevice page', function() {
+    return setInitialHostStatus(
+               settings.MultiDeviceSettingsMode.NO_ELIGIBLE_HOSTS)
+        .then(() => {
+          // Check that the settings-page is visible iff the mode is not
+          // NO_ELIGIBLE_HOSTS.
+          for (let mode of ALL_MODES) {
+            changeHostStatus(mode);
+            assertEquals(
+                !getMultidevicePage(),
+                mode === settings.MultiDeviceSettingsMode.NO_ELIGIBLE_HOSTS);
+          }
+          // One more loop to ensure we transition in and out of
+          // NO_ELIGIBLE_HOSTS mode.
+          for (let mode of ALL_MODES) {
+            changeHostStatus(mode);
+            assertEquals(
+                !getMultidevicePage(),
+                mode === settings.MultiDeviceSettingsMode.NO_ELIGIBLE_HOSTS);
+          }
+        });
   });
 
-  teardown(function() {
-    multidevicePageContainer.remove();
+  test('multidevice-page is not attached if not host is found', function() {
+    return setInitialHostStatus(
+               settings.MultiDeviceSettingsMode.NO_ELIGIBLE_HOSTS)
+        .then(() => assertTrue(!getMultidevicePage()));
   });
 
-  const getFakePageContentData = function(mode) {
-    return {
-      mode: mode,
-      hostDevice: HOST_SET_MODES.includes(mode) ? {name: 'Pixel XL'} : null,
-    };
-  };
+  test('multidevice-page is attached if a potential host is found', function() {
+    return setInitialHostStatus(settings.MultiDeviceSettingsMode.NO_HOST_SET)
+        .then(() => assertTrue(!!getMultidevicePage()));
+  });
 
-  const setMode = function(mode) {
-    multidevicePageContainer.onStatusChanged_(getFakePageContentData(mode));
-    Polymer.dom.flush();
-  };
-
-  const getMultidevicePage = () =>
-      multidevicePageContainer.$$('settings-multidevice-page');
-
-  test('mode toggles multidevice page', function() {
-    // Check that the settings-page is visible iff the mode is not
-    // NO_ELIGIBLE_HOSTS.
-    for (let mode of ALL_MODES) {
-      setMode(mode);
-      assertEquals(
-          !getMultidevicePage(),
-          mode === settings.MultiDeviceSettingsMode.NO_ELIGIBLE_HOSTS);
-    }
-    // One more loop to ensure we transition in and out of NO_ELIGIBLE_HOSTS
-    // mode.
-    for (let mode of ALL_MODES) {
-      setMode(mode);
-      assertEquals(
-          !getMultidevicePage(),
-          mode === settings.MultiDeviceSettingsMode.NO_ELIGIBLE_HOSTS);
-    }
+  test('multidevice-page is attached if a set host is found', function() {
+    return setInitialHostStatus(
+               settings.MultiDeviceSettingsMode.HOST_SET_VERIFIED)
+        .then(() => assertTrue(!!getMultidevicePage()));
   });
 
   test(
       'pageContentData_ property passes to multidevice page if present',
       function() {
-        for (let mode of ALL_MODES) {
-          setMode(mode);
-          if (mode === settings.MultiDeviceSettingsMode.NO_ELIGIBLE_HOSTS)
-            assertEquals(getMultidevicePage(), null);
-          else
-            assertDeepEquals(
-                multidevicePageContainer.pageContentData_,
-                getMultidevicePage().pageContentData);
-        }
+        return setInitialHostStatus(
+                   settings.MultiDeviceSettingsMode.NO_ELIGIBLE_HOSTS)
+            .then(() => {
+              for (let mode of ALL_MODES) {
+                changeHostStatus(mode);
+                if (mode === settings.MultiDeviceSettingsMode.NO_ELIGIBLE_HOSTS)
+                  assertEquals(getMultidevicePage(), null);
+                else
+                  assertDeepEquals(
+                      multidevicePageContainer.pageContentData_,
+                      getMultidevicePage().pageContentData);
+              }
+            });
       });
 });
