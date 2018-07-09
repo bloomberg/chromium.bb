6eecfe927de9e86b4408fa76d83fcffcae2d904b
Xing Jin <ddvfinite@gmail.com>
2018-07-09 14:54:38 +0800

    Add obmc add avx2 code
    Speed up about 0.8%~1.0%
    
    test sequence: BasketballDrill_832x480_50.y4m
    
    test command line:./aomenc --cpu-used=1 --psnr -D \
     -q --end-usage=vbr --target-bitrate=1200 --limit=20 \
     BasketballDrill_832x480_50.y4m -otest.webm
    
    Change-Id: I4f3afb6ee829ed742eb27988ebbaf081f7d683ac

--- aom_dsp/aom_dsp_rtcd_defs.pl
+++ aom_dsp/aom_dsp_rtcd_defs.pl
@@ -750,7 +750,7 @@ if (aom_config("CONFIG_AV1_ENCODER") eq "yes") {
     ($w, $h) = @$_;
     add_proto qw/unsigned int/, "aom_obmc_sad${w}x${h}", "const uint8_t *pre, int pre_stride, const int32_t *wsrc, const int32_t *mask";
     if (! (($w == 128 && $h == 32) || ($w == 32 && $h == 128))) {
-       specialize "aom_obmc_sad${w}x${h}", qw/sse4_1/;
+       specialize "aom_obmc_sad${w}x${h}", qw/sse4_1 avx2/;
     }
   }
 
@@ -759,7 +759,7 @@ if (aom_config("CONFIG_AV1_ENCODER") eq "yes") {
       ($w, $h) = @$_;
       add_proto qw/unsigned int/, "aom_highbd_obmc_sad${w}x${h}", "const uint8_t *pre, int pre_stride, const int32_t *wsrc, const int32_t *mask";
       if (! (($w == 128 && $h == 32) || ($w == 32 && $h == 128))) {
-        specialize "aom_highbd_obmc_sad${w}x${h}", qw/sse4_1/;
+        specialize "aom_highbd_obmc_sad${w}x${h}", qw/sse4_1 avx2/;
       }
     }
 
