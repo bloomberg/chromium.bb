344f932635eb3ab592b4086e779fc3e7b930a110
Khushal <khushalsagar@chromium.org>
2018-07-09 22:11:46 +0000

    gpu: Add memory tracing for GrContext in GPU process.
    Include memory dump from GrContext used for OOP raster in the GPU
    process in traces.
    
    R=bsalomon@chromium.org, ericrk@chromium.org, sadrul@chromium.org
    TBR=bsalomon@google.com
    
    Bug: 859419
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ie010e1c567626343d5698b02c45776d215640da0
    Reviewed-on: https://chromium-review.googlesource.com/1123894
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573479}
    Former-commit-id: 2c905214c7eb050e574c337751d1d156c8ba1f27

--- gpu/command_buffer/common/skia_utils.cc
+++ gpu/command_buffer/common/skia_utils.cc
@@ -5,10 +5,119 @@
 #include "gpu/command_buffer/common/skia_utils.h"
 
 #include "base/sys_info.h"
+#include "base/trace_event/memory_dump_manager.h"
+#include "base/trace_event/process_memory_dump.h"
 #include "build/build_config.h"
+#include "third_party/skia/include/core/SkTraceMemoryDump.h"
+#include "third_party/skia/include/gpu/GrContext.h"
+#include "ui/gl/trace_util.h"
 
 namespace gpu {
 namespace raster {
+namespace {
+
+// Derives from SkTraceMemoryDump and implements graphics specific memory
+// backing functionality.
+class SkiaGpuTraceMemoryDump : public SkTraceMemoryDump {
+ public:
+  // This should never outlive the provided ProcessMemoryDump, as it should
+  // always be scoped to a single OnMemoryDump funciton call.
+  SkiaGpuTraceMemoryDump(base::trace_event::ProcessMemoryDump* pmd,
+                         base::Optional<uint64_t> share_group_tracing_guid)
+      : pmd_(pmd),
+        share_group_tracing_guid_(share_group_tracing_guid),
+        tracing_process_id_(base::trace_event::MemoryDumpManager::GetInstance()
+                                ->GetTracingProcessId()) {}
+
+  ~SkiaGpuTraceMemoryDump() override = default;
+
+  // Overridden from SkTraceMemoryDump:
+  void dumpNumericValue(const char* dump_name,
+                        const char* value_name,
+                        const char* units,
+                        uint64_t value) override {
+    auto* dump = GetOrCreateAllocatorDump(dump_name);
+    dump->AddScalar(value_name, units, value);
+  }
+
+  void setMemoryBacking(const char* dump_name,
+                        const char* backing_type,
+                        const char* backing_object_id) override {
+    // If we don't have a unique tracing guid, these resources are not
+    // referenced across processes and don't need ownership edges.
+    if (!share_group_tracing_guid_)
+      return;
+
+    // For uniformity, skia provides this value as a string. Convert back to a
+    // uint32_t.
+    uint32_t gl_id =
+        std::strtoul(backing_object_id, nullptr /* str_end */, 10 /* base */);
+
+    // Constants used by SkiaGpuTraceMemoryDump to identify different memory
+    // types.
+    const char* kGLTextureBackingType = "gl_texture";
+    const char* kGLBufferBackingType = "gl_buffer";
+    const char* kGLRenderbufferBackingType = "gl_renderbuffer";
+
+    // Populated in if statements below.
+    base::trace_event::MemoryAllocatorDumpGuid guid;
+
+    if (strcmp(backing_type, kGLTextureBackingType) == 0) {
+      guid = gl::GetGLTextureClientGUIDForTracing(*share_group_tracing_guid_,
+                                                  gl_id);
+    } else if (strcmp(backing_type, kGLBufferBackingType) == 0) {
+      guid = gl::GetGLBufferGUIDForTracing(tracing_process_id_, gl_id);
+    } else if (strcmp(backing_type, kGLRenderbufferBackingType) == 0) {
+      guid = gl::GetGLRenderbufferGUIDForTracing(tracing_process_id_, gl_id);
+    }
+
+    if (!guid.empty()) {
+      pmd_->CreateSharedGlobalAllocatorDump(guid);
+
+      auto* dump = GetOrCreateAllocatorDump(dump_name);
+
+      const int kImportance = 2;
+      pmd_->AddOwnershipEdge(dump->guid(), guid, kImportance);
+    }
+  }
+
+  void setDiscardableMemoryBacking(
+      const char* dump_name,
+      const SkDiscardableMemory& discardable_memory_object) override {
+    // We don't use this class for dumping discardable memory.
+    NOTREACHED();
+  }
+
+  LevelOfDetail getRequestedDetails() const override {
+    // TODO(ssid): Use MemoryDumpArgs to create light dumps when requested
+    // (crbug.com/499731).
+    return kObjectsBreakdowns_LevelOfDetail;
+  }
+
+  bool shouldDumpWrappedObjects() const override {
+    // Chrome already dumps objects it imports into Skia. Avoid duplicate dumps
+    // by asking Skia not to dump them.
+    return false;
+  }
+
+ private:
+  // Helper to create allocator dumps.
+  base::trace_event::MemoryAllocatorDump* GetOrCreateAllocatorDump(
+      const char* dump_name) {
+    auto* dump = pmd_->GetAllocatorDump(dump_name);
+    if (!dump)
+      dump = pmd_->CreateAllocatorDump(dump_name);
+    return dump;
+  }
+
+  base::trace_event::ProcessMemoryDump* pmd_;
+  base::Optional<uint64_t> share_group_tracing_guid_;
+  uint64_t tracing_process_id_;
+
+  DISALLOW_COPY_AND_ASSIGN(SkiaGpuTraceMemoryDump);
+};
+
+}  // namespace
 
 void DetermineGrCacheLimitsFromAvailableMemory(
     size_t* max_resource_cache_bytes,
@@ -48,6 +157,13 @@ void DefaultGrCacheLimitsForTests(size_t* max_resource_cache_bytes,
   constexpr size_t kDefaultGaneshResourceCacheBytes = 96 * 1024 * 1024;
   *max_resource_cache_bytes = kDefaultGaneshResourceCacheBytes;
   *max_glyph_cache_texture_bytes = kDefaultGlyphCacheTextureBytes;
+}
+
+void DumpGrMemoryStatistics(const GrContext* context,
+                            base::trace_event::ProcessMemoryDump* pmd,
+                            base::Optional<uint64_t> tracing_guid) {
+  SkiaGpuTraceMemoryDump trace_memory_dump(pmd, tracing_guid);
+  context->dumpMemoryStatistics(&trace_memory_dump);
 }
 
 }  // namespace raster
--- services/ui/public/cpp/gpu/context_provider_command_buffer.cc
+++ services/ui/public/cpp/gpu/context_provider_command_buffer.cc
@@ -40,104 +40,6 @@
 
 class SkDiscardableMemory;
 
-namespace {
-
-// Derives from SkTraceMemoryDump and implements graphics specific memory
-// backing functionality.
-class SkiaGpuTraceMemoryDump : public SkTraceMemoryDump {
- public:
-  // This should never outlive the provided ProcessMemoryDump, as it should
-  // always be scoped to a single OnMemoryDump funciton call.
-  explicit SkiaGpuTraceMemoryDump(base::trace_event::ProcessMemoryDump* pmd,
-                                  uint64_t share_group_tracing_guid)
-      : pmd_(pmd), share_group_tracing_guid_(share_group_tracing_guid) {}
-
-  // Overridden from SkTraceMemoryDump:
-  void dumpNumericValue(const char* dump_name,
-                        const char* value_name,
-                        const char* units,
-                        uint64_t value) override {
-    auto* dump = GetOrCreateAllocatorDump(dump_name);
-    dump->AddScalar(value_name, units, value);
-  }
-
-  void setMemoryBacking(const char* dump_name,
-                        const char* backing_type,
-                        const char* backing_object_id) override {
-    const uint64_t tracing_process_id =
-        base::trace_event::MemoryDumpManager::GetInstance()
-            ->GetTracingProcessId();
-
-    // For uniformity, skia provides this value as a string. Convert back to a
-    // uint32_t.
-    uint32_t gl_id =
-        std::strtoul(backing_object_id, nullptr /* str_end */, 10 /* base */);
-
-    // Constants used by SkiaGpuTraceMemoryDump to identify different memory
-    // types.
-    const char* kGLTextureBackingType = "gl_texture";
-    const char* kGLBufferBackingType = "gl_buffer";
-    const char* kGLRenderbufferBackingType = "gl_renderbuffer";
-
-    // Populated in if statements below.
-    base::trace_event::MemoryAllocatorDumpGuid guid;
-
-    if (strcmp(backing_type, kGLTextureBackingType) == 0) {
-      guid = gl::GetGLTextureClientGUIDForTracing(share_group_tracing_guid_,
-                                                  gl_id);
-    } else if (strcmp(backing_type, kGLBufferBackingType) == 0) {
-      guid = gl::GetGLBufferGUIDForTracing(tracing_process_id, gl_id);
-    } else if (strcmp(backing_type, kGLRenderbufferBackingType) == 0) {
-      guid = gl::GetGLRenderbufferGUIDForTracing(tracing_process_id, gl_id);
-    }
-
-    if (!guid.empty()) {
-      pmd_->CreateSharedGlobalAllocatorDump(guid);
-
-      auto* dump = GetOrCreateAllocatorDump(dump_name);
-
-      const int kImportance = 2;
-      pmd_->AddOwnershipEdge(dump->guid(), guid, kImportance);
-    }
-  }
-
-  void setDiscardableMemoryBacking(
-      const char* dump_name,
-      const SkDiscardableMemory& discardable_memory_object) override {
-    // We don't use this class for dumping discardable memory.
-    NOTREACHED();
-  }
-
-  LevelOfDetail getRequestedDetails() const override {
-    // TODO(ssid): Use MemoryDumpArgs to create light dumps when requested
-    // (crbug.com/499731).
-    return kObjectsBreakdowns_LevelOfDetail;
-  }
-
-  bool shouldDumpWrappedObjects() const override {
-    // Chrome already dumps objects it imports into Skia. Avoid duplicate dumps
-    // by asking Skia not to dump them.
-    return false;
-  }
-
- private:
-  // Helper to create allocator dumps.
-  base::trace_event::MemoryAllocatorDump* GetOrCreateAllocatorDump(
-      const char* dump_name) {
-    auto* dump = pmd_->GetAllocatorDump(dump_name);
-    if (!dump)
-      dump = pmd_->CreateAllocatorDump(dump_name);
-    return dump;
-  }
-
-  base::trace_event::ProcessMemoryDump* pmd_;
-  uint64_t share_group_tracing_guid_;
-
-  DISALLOW_COPY_AND_ASSIGN(SkiaGpuTraceMemoryDump);
-};
-
-}  // namespace
-
 namespace ui {
 
 ContextProviderCommandBuffer::ContextProviderCommandBuffer(
@@ -538,9 +440,8 @@ bool ContextProviderCommandBuffer::OnMemoryDump(
 
   if (gr_context_) {
     context_thread_checker_.DetachFromThread();
-    SkiaGpuTraceMemoryDump trace_memory_dump(
-        pmd, gles2_impl_->ShareGroupTracingGUID());
-    gr_context_->get()->dumpMemoryStatistics(&trace_memory_dump);
+    gpu::raster::DumpGrMemoryStatistics(gr_context_->get(), pmd,
+                                        gles2_impl_->ShareGroupTracingGUID());
     context_thread_checker_.DetachFromThread();
   }
   return true;
