ade74e00d5206c5bc860932c4716df4618dc13ab
Alexander Semashko <ahest@yandex-team.ru>
2018-07-09 17:05:15 +0000

    Fix/deflake tests in /chrome/browser/profiles.
    This is a prerequisite for switching WindowedNotificationObserver to use
    base::RunLoop instead of deprecated MessageLoopRunner.
    
    This CL is split out from https://crrev.com/c/982612.
    
    This CL was uploaded by git cl split.
    
    R=skuhne@chromium.org
    
    Bug: 668707
    Change-Id: I3bd19577a68a77d450b8c3bfaff5dda39813b66d
    Reviewed-on: https://chromium-review.googlesource.com/1120497
    Commit-Queue: Alexander Semashko <ahest@yandex-team.ru>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573338}
    Former-commit-id: 3eafc7f1009b64dfed21bac94e133e1629e1c8d3

--- chrome/browser/profiles/profile_list_desktop_browsertest.cc
+++ chrome/browser/profiles/profile_list_desktop_browsertest.cc
@@ -21,6 +21,7 @@
 #include "chrome/common/chrome_paths.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/testing_browser_process.h"
+#include "content/public/browser/notification_service.h"
 #include "content/public/test/test_utils.h"
 
 namespace {
@@ -86,6 +87,10 @@ IN_PROC_BROWSER_TEST_F(ProfileListDesktopBrowserTest, MAYBE_SignOut) {
       chrome::NOTIFICATION_BROWSER_CLOSED,
       content::Source<Browser>(browser()));
 
+  content::WindowedNotificationObserver system_profile_created_observer(
+      chrome::NOTIFICATION_PROFILE_CREATED,
+      content::NotificationService::AllSources());
+
   EXPECT_FALSE(entry->IsSigninRequired());
   profiles::LockProfile(current_profile);
   window_close_observer.Wait();  // rely on test time-out for failure indication
@@ -94,6 +99,9 @@ IN_PROC_BROWSER_TEST_F(ProfileListDesktopBrowserTest, MAYBE_SignOut) {
   EXPECT_EQ(0u, browser_list->size());
 
   // Signing out brings up the User Manager which we should close before exit.
+  // But the User Manager is shown only when the system profile is created,
+  // which happens asynchronously.
+  system_profile_created_observer.Wait();
   UserManager::Hide();
 }
 
