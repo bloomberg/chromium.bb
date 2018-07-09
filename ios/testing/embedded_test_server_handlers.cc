53e1249dda3c6a9d6e471653bb73fd917a5c2289
Eugene But <eugenebut@google.com>
2018-07-09 23:04:51 +0000

    Add testing::HandleDownload embedded test server handler.
    This handler allows to control the size of the download response, and
    test larger downloads. The handler was adopted in inttests and egtests.
    
    Bug: 854232
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I6569cc43081d03ffb065e64c5f9d80da833f6541
    Reviewed-on: https://chromium-review.googlesource.com/1110599
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573506}
    Former-commit-id: 33438ab4ce4f3acf1cd2f431a958238b4367955b

--- ios/testing/embedded_test_server_handlers.cc
+++ ios/testing/embedded_test_server_handlers.cc
@@ -6,11 +6,20 @@
 
 #include <string>
 
+#include "base/bind.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "net/base/escape.h"
 #include "net/test/embedded_test_server/http_request.h"
 #include "net/test/embedded_test_server/http_response.h"
 #include "url/gurl.h"
+
+namespace testing {
+
+const char kTestFormPage[] = "ios.testing.HandleForm";
+const char kTestFormFieldValue[] = "test-value";
+const char kTestDownloadMimeType[] = "application/vnd.test";
 
 namespace {
 // Extracts and escapes url spec from the query.
@@ -23,12 +32,46 @@ std::string ExtractUlrSpecFromQuery(
   GURL url(spec);
   return url.is_valid() ? net::EscapeForHTML(url.spec()) : spec;
 }
+
+// A HttpResponse that responds with |length| zeroes and kTestDownloadMimeType
+// MIME Type.
+class DownloadResponse : public net::test_server::BasicHttpResponse {
+ public:
+  DownloadResponse(int length) : length_(length) {}
+
+  void SendResponse(
+      const net::test_server::SendBytesCallback& send,
+      const net::test_server::SendCompleteCallback& done) override {
+    send.Run(base::StringPrintf("HTTP/1.1 200 OK\r\n"
+                                "Content-Type:%s\r\n\r\n"
+                                "Content-Length:%d\r\n\r\n",
+                                kTestDownloadMimeType, length_),
+             base::BindRepeating(&DownloadResponse::Send, send, done, length_));
+  }
+
+ private:
+  // Sends "0" |count| times.
+  static void Send(const net::test_server::SendBytesCallback& send,
+                   const net::test_server::SendCompleteCallback& done,
+                   int count) {
+    if (!count) {
+      done.Run();
+      return;
+    }
+
+    base::ThreadTaskRunnerHandle::Get()->PostTask(
+        FROM_HERE,
+        base::BindRepeating(send, "0",
+                            base::BindRepeating(&DownloadResponse::Send, send,
+                                                done, count - 1)));
+  }
+
+  int length_ = 0;
+
+  DISALLOW_COPY_AND_ASSIGN(DownloadResponse);
+};
+
 }  // namespace
-
-namespace testing {
-
-const char kTestFormPage[] = "ios.testing.HandleForm";
-const char kTestFormFieldValue[] = "test-value";
 
 std::unique_ptr<net::test_server::HttpResponse> HandleIFrame(
     const net::test_server::HttpRequest& request) {
@@ -66,6 +109,16 @@ std::unique_ptr<net::test_server::HttpResponse> HandleForm(
       form_action.c_str(), kTestFormFieldValue, kTestFormPage));
 
   return std::move(response);
+}
+
+std::unique_ptr<net::test_server::HttpResponse> HandleDownload(
+    const net::test_server::HttpRequest& request) {
+  int length = 0;
+  if (!base::StringToInt(request.GetURL().query(), &length)) {
+    length = 1;
+  }
+
+  return std::make_unique<DownloadResponse>(length);
 }
 
 }  // namespace testing
