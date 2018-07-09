99d7733c1023b772e6da8d8a6255d5e6be1f17b8
Daniel Murphy <dmurph@chromium.org>
2018-07-09 21:30:32 +0000

    [DOMStorage] Moving cached namespaces to std::map
    There has been a bunch of memory-corruption-smelling crashes around this
    area. Changing the container to a std::map should give us more
    information about where this is happening, and maybe cause it to crash
    somewhere else.
    
    R=mek@chromium.org
    
    Bug: 845400,821371,857464
    Change-Id: Ife8cb10336a4f0752db89e2bca836ed873f7c711
    Reviewed-on: https://chromium-review.googlesource.com/1129849
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573452}
    Former-commit-id: 080fe17ea4d82ef3148ce38a12c53f19c9a7a8f9

--- content/renderer/dom_storage/local_storage_cached_areas.h
+++ content/renderer/dom_storage/local_storage_cached_areas.h
@@ -83,7 +83,7 @@ class CONTENT_EXPORT LocalStorageCachedAreas {
     DISALLOW_COPY_AND_ASSIGN(DOMStorageNamespace);
   };
 
-  base::flat_map<std::string, DOMStorageNamespace> cached_namespaces_;
+  std::map<std::string, DOMStorageNamespace> cached_namespaces_;
   size_t total_cache_limit_;
 
   // Not owned.
