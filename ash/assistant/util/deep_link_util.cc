707a6bae2eb98d169ea1232df8297c178ceb95f9
David Black <dmblack@google.com>
2018-07-09 19:23:19 +0000

    Adds onboarding/reminders to deep_link_util.
    Reminders is classified a web deep link, so it automatically will
    open in the AssistantWebView (though we are only providing
    placeholder content for the time being).
    
    Onboarding will be wired up in a follow up CL.
    
    This CL:
    - Adds IsAssistantOnboardingDeepLink API.
    - Adds IsAssistantRemindersDeepLink API.
    - Adds ParseDeepLinkParams API. Some deep links, like onboarding, will
      have parameters and so I've added a method to parse parameters from
      a provided deep link and to return them in a provided map.
    - Adds unit tests.
    
    Bug: b:110986528
    Change-Id: Ic17c625bd4b6869a256e1df09b4740fd2b6f1ee0
    Reviewed-on: https://chromium-review.googlesource.com/1121581
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#573401}
    Former-commit-id: 4bcc8496216deccbb0515d6ed0ea4da0d886a09d

--- ash/assistant/util/deep_link_util.cc
+++ ash/assistant/util/deep_link_util.cc
@@ -4,6 +4,8 @@
 
 #include "ash/assistant/util/deep_link_util.h"
 
+#include "base/strings/string_split.h"
+#include "base/strings/string_util.h"
 #include "url/gurl.h"
 
 namespace ash {
@@ -12,10 +14,22 @@ namespace util {
 
 namespace {
 
-constexpr char kAssistantSettingsSpec[] = "googleassistant://settings";
+constexpr char kAssistantOnboardingPrefix[] = "googleassistant://onboarding";
+constexpr char kAssistantRemindersPrefix[] = "googleassistant://reminders";
+constexpr char kAssistantSettingsPrefix[] = "googleassistant://settings";
+
+// TODO(dmblack): Wire up actual Assistant Reminders URL.
+constexpr char kAssistantRemindersWebUrl[] = R"(data:text/html,
+  <html>
+    <body style="padding:32px;">
+      <h3>Assistant Reminders</h3>
+      <p>Coming Soon! :)</p>
+    </body>
+  </html>
+)";
 
 // TODO(dmblack): Wire up actual Assistant Settings URL.
-constexpr char kAssistantSettingsWebUiUrl[] = R"(data:text/html,
+constexpr char kAssistantSettingsWebUrl[] = R"(data:text/html,
   <html>
     <body style="padding:32px;">
       <h3>Assistant Settings</h3>
@@ -27,30 +41,74 @@ constexpr char kAssistantSettingsWebUiUrl[] = R"(data:text/html,
 }  // namespace
 
 bool IsDeepLinkUrl(const GURL& url) {
-  return IsAssistantSettingsDeepLink(url);
+  return IsAssistantOnboardingDeepLink(url) ||
+         IsAssistantRemindersDeepLink(url) || IsAssistantSettingsDeepLink(url);
+}
+
+bool IsAssistantOnboardingDeepLink(const GURL& url) {
+  return base::StartsWith(url.spec(), kAssistantOnboardingPrefix,
+                          base::CompareCase::SENSITIVE);
+}
+
+bool IsAssistantRemindersDeepLink(const GURL& url) {
+  return base::StartsWith(url.spec(), kAssistantRemindersPrefix,
+                          base::CompareCase::SENSITIVE);
 }
 
 GURL CreateAssistantSettingsDeepLink() {
-  return GURL(kAssistantSettingsSpec);
+  return GURL(kAssistantSettingsPrefix);
 }
 
 bool IsAssistantSettingsDeepLink(const GURL& url) {
-  return url.spec() == kAssistantSettingsSpec;
+  return base::StartsWith(url.spec(), kAssistantSettingsPrefix,
+                          base::CompareCase::SENSITIVE);
 }
 
 base::Optional<GURL> GetWebUrl(const GURL& deep_link) {
   if (!IsWebDeepLink(deep_link))
     return base::nullopt;
 
+  if (IsAssistantRemindersDeepLink(deep_link))
+    return GURL(kAssistantRemindersWebUrl);
+
   if (IsAssistantSettingsDeepLink(deep_link))
-    return GURL(kAssistantSettingsWebUiUrl);
+    return GURL(kAssistantSettingsWebUrl);
 
   NOTIMPLEMENTED();
   return base::nullopt;
 }
 
 bool IsWebDeepLink(const GURL& url) {
-  return IsAssistantSettingsDeepLink(url);
+  return IsAssistantRemindersDeepLink(url) || IsAssistantSettingsDeepLink(url);
+}
+
+bool ParseDeepLinkParams(const GURL& deep_link,
+                         std::map<std::string, std::string>& params) {
+  if (!IsDeepLinkUrl(deep_link))
+    return false;
+
+  // If the deep link does not have a query then there are no key-value pairs to
+  // be parsed. We still return true in this case because omission of parameters
+  // in a deep link does not cause a parse attempt to fail.
+  if (!deep_link.has_query())
+    return true;
+
+  // Key-value pairs are '&' delimited and the keys/values are '=' delimited.
+  // Example: "googleassistant://onboarding?k1=v1&k2=v2".
+  base::StringPairs pairs;
+  if (!base::SplitStringIntoKeyValuePairs(deep_link.query(), '=', '&',
+                                          &pairs)) {
+    return false;
+  }
+
+  // Insert the parsed values into the caller provided map of |params|.
+  for (std::pair<std::string, std::string>& pair : pairs) {
+    if (params.count(pair.first) > 0) {
+      params[pair.first] = pair.second;
+    }
+  }
+
+  return true;
 }
 
 }  // namespace util
--- ash/assistant/util/deep_link_util.h
+++ ash/assistant/util/deep_link_util.h
@@ -5,6 +5,10 @@
 #ifndef ASH_ASSISTANT_UTIL_DEEP_LINK_UTIL_H_
 #define ASH_ASSISTANT_UTIL_DEEP_LINK_UTIL_H_
 
+#include <map>
+#include <string>
+
+#include "ash/ash_export.h"
 #include "base/optional.h"
 
 class GURL;
@@ -14,22 +18,39 @@ namespace assistant {
 namespace util {
 
 // Returns true if the specified |url| is a deep link, false otherwise.
-bool IsDeepLinkUrl(const GURL& url);
+ASH_EXPORT bool IsDeepLinkUrl(const GURL& url);
+
+// Returns true if the specified |url| is an Assistant onboarding deep link,
+// false otherwise.
+ASH_EXPORT bool IsAssistantOnboardingDeepLink(const GURL& url);
+
+// Returns true if the specified |url| is an Assistant reminders deep link,
+// false otherwise.
+ASH_EXPORT bool IsAssistantRemindersDeepLink(const GURL& url);
 
 // Returns a deep link to top level Assistant Settings.
-GURL CreateAssistantSettingsDeepLink();
+ASH_EXPORT GURL CreateAssistantSettingsDeepLink();
 
 // Returns true if the specified |url| is an Assistant Settings deep link, false
 // otherwise.
-bool IsAssistantSettingsDeepLink(const GURL& url);
+ASH_EXPORT bool IsAssistantSettingsDeepLink(const GURL& url);
 
 // Returns the web URL for the specified |deep_link|. A return value will only
 // be present if |deep_link| is a web deep link as identified by the
 // IsWebDeepLink(GURL) API.
-base::Optional<GURL> GetWebUrl(const GURL& deep_link);
+ASH_EXPORT base::Optional<GURL> GetWebUrl(const GURL& deep_link);
 
 // Returns true if the specified |url| is a web deep link, false otherwise.
-bool IsWebDeepLink(const GURL& url);
+ASH_EXPORT bool IsWebDeepLink(const GURL& url);
+
+// Parses the specified |deep_link| for the values corresponding to the keys in
+// |params|. If a value for a key in |params| is found, it is inserted into the
+// map. If not, the existing value in the map is untouched, so it is recommended
+// to initiate the map with default values prior to utilizing this API. The
+// return value is true if the deep link was successfully parse, false
+// otherwise.
+ASH_EXPORT bool ParseDeepLinkParams(const GURL& deep_link,
+                                    std::map<std::string, std::string>& params);
 
 }  // namespace util
 }  // namespace assistant
