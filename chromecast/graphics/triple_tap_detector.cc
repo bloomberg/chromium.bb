781487908576d2147886a282dd94d5995aff2657
Ryan Daum <rdaum@chromium.org>
2018-07-09 21:32:29 +0000

    [chromecast] Facility for triple-tap detection in cast
      - Adds a new event rewriter which (when enabled) intercepts press
        and release events and detects triple-tap type events.
    
    Bug: internal b/110532350
    Test: unit test and manual
    Change-Id: I29eacc2e2402f304b534c94799d0f67f1cbc01f6
    Reviewed-on: https://chromium-review.googlesource.com/1124865
    Commit-Queue: Ryan Daum <rdaum@chromium.org>
    Reviewed-by: Alex Sakhartchouk <alexst@chromium.org>
    Reviewed-by: Kevin Schoedel <kpschoedel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573453}
    Former-commit-id: 17ee9c1a629b22750e0b4e6c93a8fa2bc9beb657

---
+++ chromecast/graphics/triple_tap_detector.cc
@@ -0,0 +1,132 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromecast/graphics/triple_tap_detector.h"
+
+#include "base/logging.h"
+#include "ui/aura/window.h"
+#include "ui/aura/window_event_dispatcher.h"
+#include "ui/aura/window_tree_host.h"
+#include "ui/events/event.h"
+#include "ui/events/event_processor.h"
+#include "ui/events/event_utils.h"
+
+namespace chromecast {
+
+TripleTapDetector::TripleTapDetector(aura::Window* root_window,
+                                     TripleTapDetectorDelegate* delegate)
+    : root_window_(root_window),
+      delegate_(delegate),
+      enabled_(false),
+      triple_tap_state_(TripleTapState::NONE),
+      tap_count_(0) {
+  root_window->GetHost()->GetEventSource()->AddEventRewriter(this);
+}
+
+TripleTapDetector::~TripleTapDetector() {}
+
+ui::EventRewriteStatus TripleTapDetector::RewriteEvent(
+    const ui::Event& event,
+    std::unique_ptr<ui::Event>* rewritten_event) {
+  if (!enabled_ || !delegate_ || !event.IsTouchEvent()) {
+    return ui::EVENT_REWRITE_CONTINUE;
+  }
+
+  const ui::TouchEvent& touch_event = static_cast<const ui::TouchEvent&>(event);
+  return HandleTripleTapState(touch_event, triple_tap_state_);
+}
+
+ui::EventRewriteStatus TripleTapDetector::NextDispatchEvent(
+    const ui::Event& last_event,
+    std::unique_ptr<ui::Event>* new_event) {
+  // Only called if the rewriter has returned EVENT_REWRITE_DISPATCH_ANOTHER,
+  // and we don't do that.
+  NOTREACHED();
+  return ui::EVENT_REWRITE_CONTINUE;
+}
+
+ui::EventRewriteStatus TripleTapDetector::HandleTripleTapState(
+    const ui::TouchEvent& event,
+    TripleTapState state) {
+  if (event.type() == ui::ET_TOUCH_PRESSED) {
+    // If a press happened again before the minimum inter-tap interval, cancel
+    // the triple tap.
+    if (state == TripleTapState::INTERVAL_WAIT &&
+        (event.time_stamp() - stashed_events_.back().time_stamp()) <
+            gesture_detector_config_.double_tap_min_time) {
+      TripleTapReset();
+      return ui::EVENT_REWRITE_DISCARD;
+    }
+
+    // Otherwise transition into a touched state.
+    triple_tap_state_ = TripleTapState::TOUCH;
+    last_tap_location_ = event.location();
+
+    // If this is pressed too long, it should be treated as a long-press, and
+    // not part of a triple-tap, so set a timer to detect that.
+    triple_tap_timer_.Start(FROM_HERE,
+                            gesture_detector_config_.longpress_timeout, this,
+                            &TripleTapDetector::OnLongPressIntervalTimerFired);
+
+    // Copy the event and then kill the original touch pressed event, we'll
+    // produce a new one when the timer expires if it turns out this wasn't a
+    // triple-tap.
+    stashed_events_.push_back(event);
+
+    return ui::EVENT_REWRITE_DISCARD;
+  }
+
+  // Finger was released while we were waiting for one, count it as a tap.
+  if (event.type() == ui::ET_TOUCH_RELEASED && state == TripleTapState::TOUCH) {
+    triple_tap_state_ = TripleTapState::INTERVAL_WAIT;
+    triple_tap_timer_.Start(FROM_HERE,
+                            gesture_detector_config_.double_tap_timeout, this,
+                            &TripleTapDetector::OnTripleTapIntervalTimerFired);
+    stashed_events_.push_back(event);
+    tap_count_++;
+    if (tap_count_ == 3) {
+      stashed_events_.clear();
+      TripleTapReset();
+      delegate_->OnTripleTap(event.location());
+    }
+    return ui::EVENT_REWRITE_DISCARD;
+  }
+
+  if (state != TripleTapState::NONE) {
+    stashed_events_.push_back(event);
+    return ui::EVENT_REWRITE_DISCARD;
+  }
+
+  return ui::EVENT_REWRITE_CONTINUE;
+}
+
+void TripleTapDetector::OnTripleTapIntervalTimerFired() {
+  TripleTapReset();
+}
+
+void TripleTapDetector::OnLongPressIntervalTimerFired() {
+  TripleTapReset();
+}
+
+void TripleTapDetector::TripleTapReset() {
+  triple_tap_state_ = TripleTapState::NONE;
+  tap_count_ = 0;
+  triple_tap_timer_.Stop();
+  for (auto& event : stashed_events_) {
+    DispatchEvent(&event);
+  }
+  stashed_events_.clear();
+}
+
+void TripleTapDetector::DispatchEvent(ui::TouchEvent* event) {
+  // Turn off triple-tap before re-dispatching to avoid infinite recursion into
+  // this detector.
+  base::AutoReset<bool> toggle_enable(&enabled_, false);
+  DCHECK(!root_window_->GetHost()
+              ->dispatcher()
+              ->OnEventFromSource(event)
+              .dispatcher_destroyed);
+}
+
+}  // namespace chromecast
