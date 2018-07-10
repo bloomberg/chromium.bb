9c0b96fca4215b924d39305d594ae2b66011848f
Francois Doray <fdoray@chromium.org>
2018-07-10 17:00:22 +0000

    RC: Add a clock that advances when Chrome is in use.
    This clock will be use to age tabs only when Chrome is in use. That
    will prevent users from finding all their tabs discarded when they
    come back to Chrome after a long period of inactivity.
    
    Bug: 775644
    Change-Id: Id65a90f0258e2b9095bdc2b35b7f3ed16cc75c38
    Reviewed-on: https://chromium-review.googlesource.com/1129825
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573777}
    Former-commit-id: a49186f23b99bd728507dd4f3d974aa28f206087

---
+++ chrome/browser/resource_coordinator/usage_clock.cc
@@ -0,0 +1,55 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/resource_coordinator/usage_clock.h"
+
+#include "chrome/browser/resource_coordinator/time.h"
+
+namespace resource_coordinator {
+
+UsageClock::UsageClock() {
+#if defined(OS_CHROMEOS)
+  // DesktopSessionDurationTracker isn't implemented on ChromeOS. Consider that
+  // Chrome is always in an active session.
+  current_usage_session_start_time_ = NowTicks();
+#else
+  metrics::DesktopSessionDurationTracker::Get()->AddObserver(this);
+
+  if (metrics::DesktopSessionDurationTracker::Get()->in_session())
+    current_usage_session_start_time_ = NowTicks();
+#endif  // defined(OS_CHROMEOS)
+}
+
+UsageClock::~UsageClock() {
+#if !defined(OS_CHROMEOS)
+  metrics::DesktopSessionDurationTracker::Get()->RemoveObserver(this);
+#endif
+}
+
+base::TimeDelta UsageClock::GetTotalUsageTime() const {
+  base::TimeDelta elapsed_time_in_session = usage_time_in_completed_sessions_;
+  if (!current_usage_session_start_time_.is_null())
+    elapsed_time_in_session += NowTicks() - current_usage_session_start_time_;
+  return elapsed_time_in_session;
+}
+
+#if !defined(OS_CHROMEOS)
+void UsageClock::OnSessionStarted(base::TimeTicks session_start) {
+  // Ignore |session_start| because it doesn't come from the resource
+  // coordinator clock.
+  DCHECK(current_usage_session_start_time_.is_null());
+  current_usage_session_start_time_ = NowTicks();
+}
+
+void UsageClock::OnSessionEnded(base::TimeDelta session_length) {
+  // Ignore |session_length| because it wasn't measured using the resource
+  // coordinator clock.
+  DCHECK(!current_usage_session_start_time_.is_null());
+  usage_time_in_completed_sessions_ +=
+      NowTicks() - current_usage_session_start_time_;
+  current_usage_session_start_time_ = base::TimeTicks();
+}
+#endif  // !defined(OS_CHROMEOS)
+
+}  // namespace resource_coordinator
---
+++ chrome/browser/resource_coordinator/usage_clock.h
@@ -0,0 +1,57 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_RESOURCE_COORDINATOR_USAGE_CLOCK_H_
+#define CHROME_BROWSER_RESOURCE_COORDINATOR_USAGE_CLOCK_H_
+
+#include "base/macros.h"
+#include "base/time/time.h"
+#include "build/build_config.h"
+
+#if !defined(OS_CHROMEOS)
+#include "chrome/browser/metrics/desktop_session_duration/desktop_session_duration_tracker.h"
+#endif
+
+namespace resource_coordinator {
+
+// A clock that advances when Chrome is in use.
+//
+// See metrics::DesktopSessionDurationTracker for how Chrome usage is tracked.
+class UsageClock
+#if !defined(OS_CHROMEOS)
+    : public metrics::DesktopSessionDurationTracker::Observer
+#endif
+{
+ public:
+  UsageClock();
+#if defined(OS_CHROMEOS)
+  ~UsageClock();
+#else
+  ~UsageClock() override;
+#endif
+
+  // Returns the amount of Chrome usage time since this was instantiated.
+  base::TimeDelta GetTotalUsageTime() const;
+
+ private:
+#if !defined(OS_CHROMEOS)
+  void OnSessionStarted(base::TimeTicks session_start) override;
+  void OnSessionEnded(base::TimeDelta session_length) override;
+#endif
+
+  // The total time elapsed in completed usage sessions. The duration of the
+  // current usage session, if any, must be added to this to get the total usage
+  // time of Chrome.
+  base::TimeDelta usage_time_in_completed_sessions_;
+
+  // The time at which the current session started, or a null TimeTicks if not
+  // currently in a session.
+  base::TimeTicks current_usage_session_start_time_;
+
+  DISALLOW_COPY_AND_ASSIGN(UsageClock);
+};
+
+}  // namespace resource_coordinator
+
+#endif  // CHROME_BROWSER_RESOURCE_COORDINATOR_USAGE_CLOCK_H_
