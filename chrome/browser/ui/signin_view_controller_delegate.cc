d03e2357d3f47cdfdacafadbc78b13575f60b313
Charlie Reis <creis@chromium.org>
2018-07-10 18:27:02 +0000

    Add null check to signin view code to handle new error page isolation.
    This is a speculative crash fix, since there are no repro steps for
    a test.
    
    BUG=860409
    
    Change-Id: I10b5668635fd18f996245f8eb24b553cdf315eb0
    Reviewed-on: https://chromium-review.googlesource.com/1128386
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573818}
    Former-commit-id: 7d1f64092e8f3e50e303bfe735f07686c16327e9

--- chrome/browser/ui/signin_view_controller_delegate.cc
+++ chrome/browser/ui/signin_view_controller_delegate.cc
@@ -81,12 +81,18 @@ void SigninViewControllerDelegate::ResetSigninViewControllerDelegate() {
 void SigninViewControllerDelegate::LoadingStateChanged(
     content::WebContents* source,
     bool to_different_document) {
-  if (CanGoBack(source))
+  // The WebUI object can be missing for an error page, per
+  // https://crbug.com/860409.
+  if (!source->GetWebUI())
+    return;
+
+  if (CanGoBack(source)) {
     source->GetWebUI()->CallJavascriptFunctionUnsafe(
         "inline.login.showBackButton");
-  else
+  } else {
     source->GetWebUI()->CallJavascriptFunctionUnsafe(
         "inline.login.showCloseButton");
+  }
 }
 
 void SigninViewControllerDelegate::HandleKeyboardEvent(
