f5f24604f61e12e8b8e612bec99b343a13b80875
Lei Zhang <thestig@chromium.org>
2018-07-10 01:15:50 +0000

    Fix checks for OS_LINUX && !OS_ANDROID.
    When OS_ANDROID is set, OS_LINUX is not set.
    
    Change-Id: I6dd69bd621a99d63ca73dcfae15b365a87ff94ee
    Reviewed-on: https://chromium-review.googlesource.com/1130554
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573580}
    Former-commit-id: 2d3ead605a428130fc7ee7714d45a9f43cf7c41f

--- content/renderer/render_process_impl.cc
+++ content/renderer/render_process_impl.cc
@@ -165,7 +165,7 @@ RenderProcessImpl::RenderProcessImpl(
 
   SetV8FlagIfNotFeature(features::kWebAssemblyTrapHandler,
                         "--no-wasm-trap-handler");
-#if defined(OS_LINUX) && defined(ARCH_CPU_X86_64) && !defined(OS_ANDROID)
+#if defined(OS_LINUX) && defined(ARCH_CPU_X86_64)
   if (base::FeatureList::IsEnabled(features::kWebAssemblyTrapHandler)) {
     bool use_v8_signal_handler = false;
     base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
