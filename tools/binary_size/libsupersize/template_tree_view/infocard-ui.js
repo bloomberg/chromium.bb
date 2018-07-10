61a7bc741182cb87d707b1289db29513f40c238e
Tiger Oakes <tigero@google.com>
2018-07-10 20:07:32 +0000

    Adds icon and type description to supersize container infocard
    Updates to the infocard to improve style, readability and performance.
    An icon has been added to the center of the pie chart on container
    cards, similar to the icon shown on symbol cards. Text to describe
    the container type has also been added ("File", "Directory", etc) in
    the same style as the symbol type text.
    
    The infocard code has been rewritten to be class based, and the two
    types of cards inherit from the same base class. This improves
    readability of the code.
    
    The previous icon shown on the card is now cached. This means that if
    the next node displayed is of the same type (such as moving from a
    directory to another directory), then the corresponding icon will be
    reused instead of being cloned again. This improves performance by
    avoiding unnessecary garbage collection from the identical icon clones.
    
    Bug: 847599
    Change-Id: Iabe9b5eca2173338aed3d99025d2cbd917aa7d0f
    Reviewed-on: https://chromium-review.googlesource.com/1132123
    Reviewed-by: agrieve <agrieve@chromium.org>
    Reviewed-by: Eric Stevenson <estevenson@chromium.org>
    Commit-Queue: Tiger Oakes <tigero@google.com>
    Cr-Commit-Position: refs/heads/master@{#573874}
    Former-commit-id: 5a2f606212e07c4952209417dfb1f94f740a8edd

--- tools/binary_size/libsupersize/template_tree_view/infocard-ui.js
+++ tools/binary_size/libsupersize/template_tree_view/infocard-ui.js
@@ -12,246 +12,300 @@
  */
 
 {
-  // Infocard elements
-  const _containerInfocard = document.getElementById('infocard-container');
-  const _symbolInfocard = document.getElementById('infocard-symbol');
-
-  const _tableBody = _containerInfocard.querySelector('tbody');
-
-  /** @type {CanvasRenderingContext2D} Pie chart canvas from infocard */
-  const _ctx = _containerInfocard
-    .querySelector('.type-pie-info')
-    .getContext('2d');
-
-  /**
-   * @type {{[type:string]: HTMLTableRowElement}} Rows in the container infocard
-   * that represent a particular symbol type.
-   */
-  const _INFO_ROWS = {
-    b: _containerInfocard.querySelector('.bss-info'),
-    d: _containerInfocard.querySelector('.data-info'),
-    r: _containerInfocard.querySelector('.rodata-info'),
-    t: _containerInfocard.querySelector('.text-info'),
-    v: _containerInfocard.querySelector('.vtable-info'),
-    '*': _containerInfocard.querySelector('.gen-info'),
-    x: _containerInfocard.querySelector('.dexnon-info'),
-    m: _containerInfocard.querySelector('.dex-info'),
-    p: _containerInfocard.querySelector('.pak-info'),
-    P: _containerInfocard.querySelector('.paknon-info'),
-    o: _containerInfocard.querySelector('.other-info'),
-  };
-
   const _CANVAS_RADIUS = 40;
 
-  /** Update the DPI of the canvas for zoomed in and high density screens. */
-  function _updateCanvasDpi() {
-    _ctx.canvas.height = _CANVAS_RADIUS * 2 * devicePixelRatio;
-    _ctx.canvas.width = _CANVAS_RADIUS * 2 * devicePixelRatio;
-    _ctx.scale(devicePixelRatio, devicePixelRatio);
-  }
-  _updateCanvasDpi();
-  window.addEventListener('resize', _updateCanvasDpi);
+  class Infocard {
+    constructor(id) {
+      this._infocard = document.getElementById(id);
+      /** @type {HTMLHeadingElement} */
+      this._sizeInfo = this._infocard.querySelector('.size-info');
+      /** @type {HTMLParagraphElement} */
+      this._pathInfo = this._infocard.querySelector('.path-info');
+      /** @type {HTMLDivElement} */
+      this._iconInfo = this._infocard.querySelector('.icon-info');
+      /** @type {HTMLParagraphElement} */
+      this._typeInfo = this._infocard.querySelector('.type-info');
 
-  /**
-   * Draw a slice of a pie chart.
-   * @param {CanvasRenderingContext2D} ctx
-   * @param {number} angleStart Starting angle, in radians.
-   * @param {number} percentage Percentage of circle to draw.
-   * @param {string} color Color of the pie slice.
-   * @returns {number} Ending angle, in radians.
-   */
-  function _drawSlice(ctx, angleStart, percentage, color) {
-    const arcLength = percentage * 2 * Math.PI;
-    const angleEnd = angleStart + arcLength;
-    if (arcLength === 0) return angleEnd;
+      /**
+       * Last symbol type displayed.
+       * Tracked to avoid re-cloning the same icon.
+       */
+      this._lastType = '';
+    }
 
-    // Update DOM
-    ctx.fillStyle = color;
-    // Move cursor to center, where line will start
-    ctx.beginPath();
-    ctx.moveTo(40, 40);
-    // Move cursor to start of arc then draw arc
-    ctx.arc(40, 40, _CANVAS_RADIUS, angleStart, angleEnd);
-    // Move cursor back to center
-    ctx.closePath();
-    ctx.fill();
+    /**
+     * Updates the size header, which normally displayed the byte size of the
+     * node followed by an abbreviated version.
+     *
+     * Example: "1,234 bytes (1.23 KiB)"
+     * @param {TreeNode} node
+     * @param {GetSize} getSizeLabel
+     */
+    _updateSize(node, getSizeLabel) {
+      const {title, element} = getSizeLabel(
+        node.size,
+        state.get('byteunit', {default: 'MiB', valid: _BYTE_UNITS_SET})
+      );
+      const sizeFragment = dom.createFragment([
+        document.createTextNode(`${title} (`),
+        element,
+        document.createTextNode(')'),
+      ]);
 
-    return angleEnd;
-  }
-
-  /**
-   * Update a row in the breakdown table with the given values.
-   * @param {HTMLTableRowElement} row
-   * @param {number} size Total size of the symbols of a given type in a
-   * container.
-   * @param {number} percentage How much the size represents in relation to the
-   * total size of the symbols in the container.
-   */
-  function _updateBreakdownRow(row, size, percentage) {
-    if (size === 0) {
-      if (row.parentElement != null) {
-        _tableBody.removeChild(row);
+      // Update DOM
+      if (node.size < 0) {
+        this._sizeInfo.classList.add('negative');
+      } else {
+        this._sizeInfo.classList.remove('negative');
       }
-      return;
+
+      dom.replace(this._sizeInfo, sizeFragment);
     }
 
-    const sizeColumn = row.querySelector('.size');
-    const percentColumn = row.querySelector('.percent');
-
-    const sizeString = size.toLocaleString(undefined, {
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
-      useGrouping: true,
-    });
-    const percentString = percentage.toLocaleString(undefined, {
-      style: 'percent',
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
-    });
-
-    // Update DOM
-    sizeColumn.textContent = sizeString;
-    percentColumn.textContent = percentString;
-    _tableBody.appendChild(row);
-  }
-
-  /**
-   * @param {TreeNode} symbol
-   * @param {GetSize} getSizeLabel
-   * @returns {[DocumentFragment, DocumentFragment]}
-   */
-  function _buildHeaderFragments(symbol, getSizeLabel) {
-    // Create size info fragment, such as "1,234 bytes (1.23 KiB)"
-    const {title, element} = getSizeLabel(
-      symbol.size,
-      state.get('byteunit', {default: 'MiB', valid: _BYTE_UNITS_SET})
-    );
-    const sizeInfoFragment = dom.createFragment([
-      document.createTextNode(`${title} (`),
-      element,
-      document.createTextNode(')'),
-    ]);
-
-    // Create path fragment where the shortName is highlighted.
-    const path = symbol.idPath.slice(0, symbol.shortNameIndex);
-    const boldShortName = dom.textElement(
-      'span',
-      shortName(symbol),
-      'symbol-name-info'
-    );
-    const pathInfoFragment = dom.createFragment([
-      document.createTextNode(path),
-      boldShortName,
-    ]);
-
-    return [sizeInfoFragment, pathInfoFragment];
-  }
-
-  function _insertHeaderFragments(
-    infocard,
-    symbol,
-    [sizeInfoFragment, pathInfoFragment]
-  ) {
-    const sizeInfo = infocard.querySelector('.size-info');
-    // Update DOM
-    infocard.removeAttribute('hidden');
-    if (symbol.size < 0) {
-      sizeInfo.classList.add('negative');
-    } else {
-      sizeInfo.classList.remove('negative');
-    }
-
-    dom.replace(sizeInfo, sizeInfoFragment);
-    dom.replace(infocard.querySelector('.path-info'), pathInfoFragment);
-  }
-
-  /**
-   * Update DOM for the container infocard
-   * @param {TreeNode} container
-   * @param {Array<[string, number]>} sizeEntries
-   * @param {{[type: string]: HTMLTableRowElement}} extraRows
-   * @param {[DocumentFragment, DocumentFragment]} headerFragments
-   */
-  function _updateContainerInfocard(
-    container,
-    sizeEntries,
-    extraRows,
-    headerFragments
-  ) {
-    let angleStart = 0;
-    for (const [type, size] of sizeEntries) {
-      delete extraRows[type];
-      const {color} = getIconStyle(type);
-      const percentage = size / container.size;
+    /**
+     * Updates the path text, which shows the idPath of the node but highlights
+     * the symbol name portion using bold text.
+     * @param {TreeNode} node
+     */
+    _updatePath(node) {
+      const path = node.idPath.slice(0, node.shortNameIndex);
+      const boldShortName = dom.textElement(
+        'span',
+        shortName(node),
+        'symbol-name-info'
+      );
+      const pathFragment = dom.createFragment([
+        document.createTextNode(path),
+        boldShortName,
+      ]);
 
       // Update DOM
-      angleStart = _drawSlice(_ctx, angleStart, percentage, color);
-      _updateBreakdownRow(_INFO_ROWS[type], size, percentage);
+      dom.replace(this._pathInfo, pathFragment);
     }
 
-    // Hide unused types
-    for (const row of Object.values(extraRows)) {
+    /**
+     * Updates the icon and type text. The type label is pulled from the
+     * title of the icon supplied.
+     * @param {SVGSVGElement} icon Icon to display
+     */
+    _setTypeContent(icon) {
+      const typeDescription = icon.querySelector('title').textContent;
+      icon.setAttribute('fill', '#fff');
+
+      this._typeInfo.textContent = typeDescription;
+      this._iconInfo.removeChild(this._iconInfo.lastElementChild);
+      this._iconInfo.appendChild(icon);
+    }
+
+    /**
+     * Toggle wheter or not the card is visible.
+     * @param {boolean} isHidden
+     */
+    setHidden(isHidden) {
+      if (isHidden) {
+        this._infocard.setAttribute('hidden', '');
+      } else {
+        this._infocard.removeAttribute('hidden');
+      }
+    }
+
+    /**
+     * Updates the DOM for the info card.
+     * @param {TreeNode} node
+     * @param {GetSize} getSizeLabel
+     */
+    _updateInfocard(node, getSizeLabel) {
+      const type = node.type[0];
+
       // Update DOM
-      _updateBreakdownRow(row, 0, 0);
+      this._updateSize(node, getSizeLabel);
+      this._updatePath(node);
+      if (type !== this._lastType) {
+        // No need to create a new icon if it is identical.
+        const icon = getIconTemplate(type);
+        this._setTypeContent(icon);
+        this._lastType = type;
+      }
     }
 
-    // Update DOM
-    _insertHeaderFragments(_containerInfocard, container, headerFragments);
-    _symbolInfocard.setAttribute('hidden', '');
+    /**
+     * Updates the card on the next animation frame.
+     * @param {TreeNode} node
+     * @param {GetSize} getSizeLabel
+     */
+    updateInfocard(node, getSizeLabel) {
+      cancelAnimationFrame(Infocard._pendingFrame);
+      Infocard._pendingFrame = requestAnimationFrame(() =>
+        this._updateInfocard(node, getSizeLabel)
+      );
+    }
   }
 
-  /**
-   * Update DOM for the symbol infocard
-   * @param {TreeNode} symbol
-   * @param {SVGSVGElement} icon
-   * @param {HTMLDivElement} iconInfo
-   * @param {[DocumentFragment, DocumentFragment]} headerFragments
-   */
-  function _updateSymbolInfocard(symbol, icon, iconInfo, headerFragments) {
-    const typeInfo = icon.querySelector('title').textContent;
-    // Update DOM
-    iconInfo.style.backgroundColor = icon.getAttribute('fill');
-    icon.setAttribute('fill', '#fff');
-    dom.replace(iconInfo, icon);
-
-    _symbolInfocard.querySelector('.type-info').textContent = typeInfo;
-    _insertHeaderFragments(_symbolInfocard, symbol, headerFragments);
-    _containerInfocard.setAttribute('hidden', '');
+  class SymbolInfocard extends Infocard {
+    /**
+     * @param {SVGSVGElement} icon Icon to display
+     */
+    _setTypeContent(icon) {
+      const color = icon.getAttribute('fill');
+      super._setTypeContent(icon);
+      this._iconInfo.style.backgroundColor = color;
+    }
   }
 
-  let _pendingFrame;
+  class ContainerInfocard extends Infocard {
+    constructor(id) {
+      super(id);
+      this._tableBody = this._infocard.querySelector('tbody');
+      this._ctx = this._infocard.querySelector('canvas').getContext('2d');
 
-  /**
-   * Displays an infocard for the given symbol on the next frame.
-   * @param {TreeNode} symbol
-   * @param {GetSize} getSizeLabel
-   */
-  function displayInfocard(symbol, getSizeLabel) {
-    cancelAnimationFrame(_pendingFrame);
-    const headerFragments = _buildHeaderFragments(symbol, getSizeLabel);
+      /**
+       * @type {{[type:string]: HTMLTableRowElement}} Rows in the container
+       * infocard that represent a particular symbol type.
+       */
+      this._infoRows = {
+        b: this._tableBody.querySelector('.bss-info'),
+        d: this._tableBody.querySelector('.data-info'),
+        r: this._tableBody.querySelector('.rodata-info'),
+        t: this._tableBody.querySelector('.text-info'),
+        v: this._tableBody.querySelector('.vtable-info'),
+        '*': this._tableBody.querySelector('.gen-info'),
+        x: this._tableBody.querySelector('.dexnon-info'),
+        m: this._tableBody.querySelector('.dex-info'),
+        p: this._tableBody.querySelector('.pak-info'),
+        P: this._tableBody.querySelector('.paknon-info'),
+        o: this._tableBody.querySelector('.other-info'),
+      };
 
-    if (_CONTAINER_TYPE_SET.has(symbol.type[0])) {
-      const extraRows = {..._INFO_ROWS};
-      const sizeEntries = Object.entries(symbol.childSizes).sort(
+      /**
+       * Update the DPI of the canvas for zoomed in and high density screens.
+       */
+      const _updateCanvasDpi = () => {
+        this._ctx.canvas.height = _CANVAS_RADIUS * 2 * devicePixelRatio;
+        this._ctx.canvas.width = _CANVAS_RADIUS * 2 * devicePixelRatio;
+        this._ctx.scale(devicePixelRatio, devicePixelRatio);
+      };
+
+      _updateCanvasDpi();
+      window.addEventListener('resize', _updateCanvasDpi);
+    }
+
+    /**
+     * @param {SVGSVGElement} icon Icon to display
+     */
+    _setTypeContent(icon) {
+      super._setTypeContent(icon);
+      icon.classList.add('canvas-overlay');
+    }
+
+    /**
+     * Draw a slice of a pie chart.
+     * @param {number} angleStart Starting angle, in radians.
+     * @param {number} percentage Percentage of circle to draw.
+     * @param {string} color Color of the pie slice.
+     * @returns {number} Ending angle, in radians.
+     */
+    _drawSlice(angleStart, percentage, color) {
+      const arcLength = percentage * 2 * Math.PI;
+      const angleEnd = angleStart + arcLength;
+      if (arcLength === 0) return angleEnd;
+
+      // Update DOM
+      this._ctx.fillStyle = color;
+      // Move cursor to center, where line will start
+      this._ctx.beginPath();
+      this._ctx.moveTo(40, 40);
+      // Move cursor to start of arc then draw arc
+      this._ctx.arc(40, 40, _CANVAS_RADIUS, angleStart, angleEnd);
+      // Move cursor back to center
+      this._ctx.closePath();
+      this._ctx.fill();
+
+      return angleEnd;
+    }
+
+    /**
+     * Update a row in the breakdown table with the given values.
+     * @param {HTMLTableRowElement} row
+     * @param {number} size Total size of the symbols of a given type in a
+     * container.
+     * @param {number} percentage How much the size represents in relation to
+     * the total size of the symbols in the container.
+     */
+    _updateBreakdownRow(row, size, percentage) {
+      if (size === 0) {
+        if (row.parentElement != null) {
+          this._tableBody.removeChild(row);
+        }
+        return;
+      }
+
+      const sizeColumn = row.querySelector('.size');
+      const percentColumn = row.querySelector('.percent');
+
+      const sizeString = size.toLocaleString(undefined, {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+        useGrouping: true,
+      });
+      const percentString = percentage.toLocaleString(undefined, {
+        style: 'percent',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      });
+
+      // Update DOM
+      sizeColumn.textContent = sizeString;
+      percentColumn.textContent = percentString;
+      this._tableBody.appendChild(row);
+    }
+
+    /**
+     * Update DOM for the container infocard
+     * @param {TreeNode} containerNode
+     * @param {GetSize} getSizeLabel
+     */
+    _updateInfocard(containerNode, getSizeLabel) {
+      const extraRows = {...this._infoRows};
+      const sizeEntries = Object.entries(containerNode.childSizes).sort(
         (a, b) => b[1] - a[1]
       );
 
-      _pendingFrame = requestAnimationFrame(() =>
-        _updateContainerInfocard(
-          symbol,
-          sizeEntries,
-          extraRows,
-          headerFragments
-        )
-      );
-    } else {
-      const icon = getIconTemplate(symbol.type[0]);
-      /** @type {HTMLDivElement} */
-      const iconInfo = _symbolInfocard.querySelector('.icon-info');
+      // Update DOM
+      super._updateInfocard(containerNode, getSizeLabel);
+      let angleStart = 0;
+      for (const [type, size] of sizeEntries) {
+        delete extraRows[type];
+        const {color} = getIconStyle(type);
+        const percentage = size / containerNode.size;
 
-      _pendingFrame = requestAnimationFrame(() =>
-        _updateSymbolInfocard(symbol, icon, iconInfo, headerFragments)
-      );
+        angleStart = this._drawSlice(angleStart, percentage, color);
+        this._updateBreakdownRow(this._infoRows[type], size, percentage);
+      }
+
+      // Hide unused types
+      for (const row of Object.values(extraRows)) {
+        this._updateBreakdownRow(row, 0, 0);
+      }
+    }
+  }
+
+  const _containerInfo = new ContainerInfocard('infocard-container');
+  const _symbolInfo = new SymbolInfocard('infocard-symbol');
+
+  /**
+   * Displays an infocard for the given symbol on the next frame.
+   * @param {TreeNode} node
+   * @param {GetSize} getSizeLabel
+   */
+  function displayInfocard(node, getSizeLabel) {
+    if (_CONTAINER_TYPE_SET.has(node.type[0])) {
+      _containerInfo.updateInfocard(node, getSizeLabel);
+      _containerInfo.setHidden(false);
+      _symbolInfo.setHidden(true);
+    } else {
+      _symbolInfo.updateInfocard(node, getSizeLabel);
+      _symbolInfo.setHidden(false);
+      _containerInfo.setHidden(true);
     }
   }
 
