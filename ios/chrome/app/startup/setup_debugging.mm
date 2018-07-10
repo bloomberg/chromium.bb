ac5414013965433a7df7f4813f78352621335be7
Rohit Rao <rohitrao@chromium.org>
2018-07-10 14:39:06 +0000

    [ios] Adds more cells to the Material Cell Catalog.
    Adds cells that are used in Settings but weren't previously included in
    the catalog.
    
    BUG=754032,805213
    
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I72bfffe0b338073ba52f9283a9cbc1ec62d69816
    Reviewed-on: https://chromium-review.googlesource.com/1129040
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573721}
    Former-commit-id: 6283fa786a92c6d7a25a5f02dd3f62c852f83d77

--- ios/chrome/app/startup/setup_debugging.mm
+++ ios/chrome/app/startup/setup_debugging.mm
@@ -35,6 +35,8 @@ void swizzleUIImageImageNamed() {
   // TODO(crbug.com/721338): Add missing image.
   [whiteList addObject:@"voice_icon_keyboard_accessory"];
   [whiteList addObject:@"voice_icon"];
+  // TODO(crbug.com/754032): Add missing image.
+  [whiteList addObject:@"ios_default_avatar"];
 
   // The original implementation of [UIImage imageNamed:].
   // Called by the new implementation.
