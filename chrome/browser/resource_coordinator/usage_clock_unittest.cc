7511725e0e260069a96b26e077b83b2aef4bceb5
Francois Doray <fdoray@chromium.org>
2018-07-10 17:00:22 +0000

    RC: Add a clock that advances when Chrome is in use.
    This clock will be use to age tabs only when Chrome is in use. That
    will prevent users from finding all their tabs discarded when they
    come back to Chrome after a long period of inactivity.
    
    Bug: 775644
    Change-Id: Id65a90f0258e2b9095bdc2b35b7f3ed16cc75c38
    Reviewed-on: https://chromium-review.googlesource.com/1129825
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573777}
    Former-commit-id: a49186f23b99bd728507dd4f3d974aa28f206087

---
+++ chrome/browser/resource_coordinator/usage_clock_unittest.cc
@@ -0,0 +1,87 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/resource_coordinator/usage_clock.h"
+
+#include "base/test/scoped_task_environment.h"
+#include "base/test/simple_test_tick_clock.h"
+#include "build/build_config.h"
+#include "chrome/browser/resource_coordinator/time.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+#if !defined(OS_CHROMEOS)
+#include "chrome/browser/metrics/desktop_session_duration/desktop_session_duration_tracker.h"
+#endif
+
+namespace resource_coordinator {
+
+TEST(ResourceCoordinatorUsageClock, UsageClock) {
+  // Required to use DesktopSessionDurationTracker.
+  base::test::ScopedTaskEnvironment scoped_task_environment;
+
+  {
+    base::SimpleTestTickClock clock;
+    clock.Advance(base::TimeDelta::FromMinutes(42));
+    ScopedSetTickClockForTesting scoped_set_tick_clock_for_testing(&clock);
+
+#if defined(OS_CHROMEOS)
+    // On ChromeOS, UsageClock behaves like a normal clock.
+    UsageClock session_clock;
+    EXPECT_EQ(session_clock.GetTotalUsageTime(), base::TimeDelta());
+
+    clock.Advance(base::TimeDelta::FromMinutes(1));
+    EXPECT_EQ(session_clock.GetTotalUsageTime(),
+              base::TimeDelta::FromMinutes(1));
+    clock.Advance(base::TimeDelta::FromMinutes(1));
+    EXPECT_EQ(session_clock.GetTotalUsageTime(),
+              base::TimeDelta::FromMinutes(2));
+#else
+    // On non-ChromeOS, UsageClock advances when
+    // DesktopSessionDurationTracker::in_session() is true.
+    metrics::DesktopSessionDurationTracker::Initialize();
+    auto* tracker = metrics::DesktopSessionDurationTracker::Get();
+    tracker->SetInactivityTimeoutForTesting(0);
+    tracker->OnVisibilityChanged(true, base::TimeDelta());
+    tracker->OnUserEvent();
+    EXPECT_TRUE(tracker->in_session());
+
+    UsageClock session_clock;
+    EXPECT_EQ(session_clock.GetTotalUsageTime(), base::TimeDelta());
+
+    // Verify that time advances when Chrome is in use.
+    clock.Advance(base::TimeDelta::FromMinutes(1));
+    EXPECT_EQ(session_clock.GetTotalUsageTime(),
+              base::TimeDelta::FromMinutes(1));
+    clock.Advance(base::TimeDelta::FromMinutes(1));
+    EXPECT_EQ(session_clock.GetTotalUsageTime(),
+              base::TimeDelta::FromMinutes(2));
+
+    // Verify that time is updated when Chrome stops being used.
+    clock.Advance(base::TimeDelta::FromMinutes(1));
+    tracker->OnVisibilityChanged(false, base::TimeDelta());
+    EXPECT_FALSE(tracker->in_session());
+    EXPECT_EQ(session_clock.GetTotalUsageTime(),
+              base::TimeDelta::FromMinutes(3));
+
+    // Verify that time stays still when Chrome is not in use.
+    clock.Advance(base::TimeDelta::FromMinutes(1));
+    EXPECT_EQ(session_clock.GetTotalUsageTime(),
+              base::TimeDelta::FromMinutes(3));
+
+    // Verify that time advances again when Chrome is in use.
+    tracker->OnVisibilityChanged(true, base::TimeDelta());
+    EXPECT_TRUE(tracker->in_session());
+    clock.Advance(base::TimeDelta::FromMinutes(1));
+    EXPECT_EQ(session_clock.GetTotalUsageTime(),
+              base::TimeDelta::FromMinutes(4));
+#endif  // defined(OS_CHROMEOS)
+  }
+
+#if !defined(OS_CHROMEOS)
+  // Must be after UsageClock destruction.
+  metrics::DesktopSessionDurationTracker::CleanupForTesting();
+#endif
+}
+
+}  // namespace resource_coordinator
