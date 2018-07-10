8cd53489dc05bbd158505a80c0478300847cd66e
Mike Dougherty <michaeldo@chromium.org>
2018-07-10 06:57:53 +0000

    Add WebFramesManager class.
    The WebFrameManager class maintains a list of (and provides access to) WebFrame objects representing the currently rendered frames of the associated WebState.
    
    Bug: 851636
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I62473ded9af401bf3b556e0d03776f9b67780d87
    Reviewed-on: https://chromium-review.googlesource.com/1119661
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573639}
    Former-commit-id: 03f9320605ad419e8ddfcfad0c6436b34e527382

---
+++ ios/web/web_state/web_frames_manager_impl.h
@@ -0,0 +1,43 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_WEB_WEB_STATE_WEB_FRAMES_MANAGER_IMPL_H_
+#define IOS_WEB_WEB_STATE_WEB_FRAMES_MANAGER_IMPL_H_
+
+#include "ios/web/public/web_state/web_frames_manager.h"
+#include "ios/web/public/web_state/web_state_user_data.h"
+
+namespace web {
+
+class WebFrame;
+
+class WebFramesManagerImpl
+    : public WebFramesManager,
+      public web::WebStateUserData<WebFramesManagerImpl> {
+ public:
+  ~WebFramesManagerImpl() override;
+  explicit WebFramesManagerImpl(web::WebState* web_state);
+
+  // Adds |frame| to the list of web frames associated with WebState.
+  void AddFrame(WebFrame& frame);
+  // Removes the web frame with |frame_id|, if one exists, from the list of
+  // associated web frames.
+  void RemoveFrame(const std::string& frame_id);
+
+  // WebFramesManager overrides
+  const std::vector<WebFrame*>& GetAllWebFrames() override;
+  WebFrame* GetMainWebFrame() override;
+
+ private:
+  friend class web::WebStateUserData<WebFramesManagerImpl>;
+
+  // List of all web frames associated with WebState.
+  std::vector<WebFrame*> web_frames_;
+  // Reference to the current main web frame.
+  WebFrame* main_web_frame_ = nullptr;
+};
+
+}  // namespace web
+
+#endif  // IOS_WEB_WEB_STATE_WEB_FRAMES_MANAGER_H_
