e2df34da429aa1fe898aabb5e48059353c57c369
Alexey Kozyatinskiy <kozyatinskiy@chromium.org>
2018-07-10 06:36:08 +0000

    [DevTools] fixed breakpoints for Node.js
    We should mark target as Node.js target as soon as possible. It is
    important for target observers, e.g. for Breakpoint in BreakpointManager.
    
    R=lushnikov@chromium.org
    
    Bug: none
    Change-Id: Ia62abd0fad83760fc3a812530e3bc0953513348b
    Reviewed-on: https://chromium-review.googlesource.com/1130936
    Commit-Queue: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#573633}
    Former-commit-id: 9e05025b8eb95ba331fad2686b0ac9f3408d69f3

--- third_party/WebKit/LayoutTests/http/tests/devtools/persistence/persistence-sync-content-nodejs.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/persistence/persistence-sync-content-nodejs.js
@@ -9,7 +9,7 @@
 
   var testMapping = BindingsTestRunner.initializeTestMapping();
   // Pretend we are running under V8 front-end.
-  SDK.targetManager.mainTarget().markAsNodeJS();
+  SDK.targetManager.mainTarget().markAsNodeJSForTest();
 
   var content = ['', '', 'var express = require("express");', '//TODO'].join('\n');
 
