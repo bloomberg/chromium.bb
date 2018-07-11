b5f0b0ab0ee6870e9490326071ed175627724958
Charlie Andrews <charliea@chromium.org>
2018-07-11 01:08:36 +0000

    Remove power.idle_platform benchmark
    This benchmark is no longer necessary now that we are no longer using
    ground-truth power in our integration testing.
    
    Bug: 859514
    Cq-Include-Trybots: master.tryserver.chromium.perf:obbs_fyi
    Change-Id: I9d9fd8d5a629d903e38a9ad5d5e03a2fda58c650
    Reviewed-on: https://chromium-review.googlesource.com/1130008
    Commit-Queue: Charlie Andrews <charliea@chromium.org>
    Reviewed-by: Emily Hanley <eyaich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574023}
    Former-commit-id: 5e72208df845b9c44f5ec066ea03e41cbe8a7024

--- tools/perf/benchmarks/power.py
+++ tools/perf/benchmarks/power.py
@@ -12,19 +12,6 @@ from telemetry.timeline import chrome_trace_category_filter
 from telemetry.web_perf import timeline_based_measurement
 
 
-class _PowerBenchmark(perf_benchmark.PerfBenchmark):
-  """A benchmark that indirectly measures power through CPU time."""
-
-  def CreateCoreTimelineBasedMeasurementOptions(self):
-    category_filter = chrome_trace_category_filter.ChromeTraceCategoryFilter(
-        filter_string='toplevel')
-    options = timeline_based_measurement.Options(category_filter)
-    options.config.enable_chrome_trace = True
-    options.config.enable_cpu_trace = True
-    options.SetTimelineBasedMetrics(['cpuTimeMetric'])
-    return options
-
-
 @benchmark.Info(emails=['perezju@chromium.org'])
 class PowerTypical10Mobile(perf_benchmark.PerfBenchmark):
   """Android typical 10 mobile power test."""
@@ -40,42 +27,21 @@ class PowerTypical10Mobile(perf_benchmark.PerfBenchmark):
     return 'power.typical_10_mobile'
 
 
-# TODO(charliea): Delete this benchmark now that we're no longer measuring
-# ground-truth power.
 @benchmark.Info(emails=['charliea@chromium.org'])
-class IdlePlatformBenchmark(perf_benchmark.PerfBenchmark):
-  """Idle platform benchmark.
-
-  This benchmark just starts up tracing agents and lets the platform sit idle.
-  Our power benchmarks are prone to noise caused by other things running on the
-  system. This benchmark is intended to help find the sources of noise.
-  """
-  def CreateCoreTimelineBasedMeasurementOptions(self):
-    options = timeline_based_measurement.Options(
-        chrome_trace_category_filter.ChromeTraceCategoryFilter())
-    options.config.enable_cpu_trace = True
-    # Atrace tracing agent autodetects if its android and only runs if it is.
-    options.config.enable_atrace_trace = True
-    options.config.enable_chrome_trace = False
-    options.SetTimelineBasedMetrics([
-        'tracingMetric'
-    ])
-    return options
-
-  def CreateStorySet(self, options):
-    return page_sets.IdleStorySet()
-
-  @classmethod
-  def Name(cls):
-    return 'power.idle_platform'
-
-
-@benchmark.Info(emails=['charliea@chromium.org'])
-class PowerDesktop(_PowerBenchmark):
+class PowerDesktop(perf_benchmark.PerfBenchmark):
   SUPPORTED_PLATFORMS = [story.expectations.ALL_DESKTOP]
 
   def CreateStorySet(self, options):
     return page_sets.DesktopPowerStorySet()
+
+  def CreateCoreTimelineBasedMeasurementOptions(self):
+    category_filter = chrome_trace_category_filter.ChromeTraceCategoryFilter(
+        filter_string='toplevel')
+    options = timeline_based_measurement.Options(category_filter)
+    options.config.enable_chrome_trace = True
+    options.config.enable_cpu_trace = True
+    options.SetTimelineBasedMetrics(['cpuTimeMetric'])
+    return options
 
   @classmethod
   def Name(cls):
