994e1e2ee29d4ce9bcf7fcc80a287c56dd34ebd5
Rintaro Kuroiwa <rkuroiwa@chromium.org>
2018-07-11 00:44:20 +0000

    Add DecryptConfig::Clone()
    - Add a clone method on DecryptConfig.
    - This is required for later patches for decoders and CodecPictures to
      copy and own DecryptConfigs.
    
    Bug: 820192
    Change-Id: I5ae0826e86781d876af09509a9168e8a8e5be8a6
    Reviewed-on: https://chromium-review.googlesource.com/1123767
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: Rintaro Kuroiwa <rkuroiwa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574002}
    Former-commit-id: 1a60f2b8295dbc71fc6ce6ea5d6882c6ad736a88

--- media/base/decrypt_config.h
+++ media/base/decrypt_config.h
@@ -72,6 +72,8 @@ class MEDIA_EXPORT DecryptConfig {
     return encryption_pattern_;
   };
 
+  std::unique_ptr<DecryptConfig> Clone();
+
   // Returns whether this config has EncryptionPattern set or not.
   bool HasPattern() const;
 
@@ -82,6 +84,8 @@ class MEDIA_EXPORT DecryptConfig {
   std::ostream& Print(std::ostream& os) const;
 
  private:
+  DecryptConfig(const DecryptConfig& other);
+
   const EncryptionMode encryption_mode_;
   const std::string key_id_;
 
@@ -95,7 +99,7 @@ class MEDIA_EXPORT DecryptConfig {
   // Only specified if |encryption_mode_| requires a pattern.
   base::Optional<EncryptionPattern> encryption_pattern_;
 
-  DISALLOW_COPY_AND_ASSIGN(DecryptConfig);
+  DISALLOW_ASSIGN(DecryptConfig);
 };
 
 }  // namespace media
