6c579721da787a8a5cb62dbb8797d10e03836cd0
jonross <jonross@chromium.org>
2018-07-11 21:09:02 +0000

    Update Chrome callsites that use MainThreadFrameObserver
    MainThreadFrameObserver does not guarantee that a compositor frame was actually
    submitted. It however does synchronize the Renderer's main thread, which allows
    for syncing Javascript execution in tests.
    
    This updates two tests in chrome/browser to use RenderFrameSubmissionObserver
    for synchronizing on compositor frame submission. As well as InputEventAckWaiter
    for synchronizing on input event processing.
    
    TEST=WebViewTest.InterstitialPageFocusedWidget,
    WebViewBrowserPluginSpecificInteractiveTest.EnsureFocusSynced
    
    Bug: 862683
    Change-Id: Iae40b92dedf656898b9e77c6529ef118d89e0a6e
    Reviewed-on: https://chromium-review.googlesource.com/1133960
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574335}
    Former-commit-id: cb82e2e48313e755713f169fa151199fec459a02

--- chrome/browser/apps/guest_view/web_view_interactive_browsertest.cc
+++ chrome/browser/apps/guest_view/web_view_interactive_browsertest.cc
@@ -1165,13 +1165,11 @@ IN_PROC_BROWSER_TEST_F(WebViewBrowserPluginSpecificInteractiveTest,
   content::WebContents* embedder_web_contents = GetFirstAppWindowWebContents();
   content::WebContents* guest_web_contents =
       GetGuestViewManager()->WaitForSingleGuestCreated();
-
-  content::MainThreadFrameObserver embedder_observer(
-      embedder_web_contents->GetMainFrame()->GetView()->GetRenderWidgetHost());
-  content::MainThreadFrameObserver guest_observer(
-      guest_web_contents->GetMainFrame()->GetView()->GetRenderWidgetHost());
-  embedder_observer.Wait();
-  guest_observer.Wait();
+  content::RenderFrameSubmissionObserver embedder_observer(
+      embedder_web_contents);
+  content::RenderFrameSubmissionObserver guest_observer(guest_web_contents);
+  embedder_observer.WaitForMetadataChange();
+  guest_observer.WaitForMetadataChange();
 
   ExtensionTestMessageListener listener{"WebViewTest.WEBVIEW_LOADED", false};
   EXPECT_TRUE(ui_test_utils::ShowAndFocusNativeWindow(GetPlatformAppWindow()));
