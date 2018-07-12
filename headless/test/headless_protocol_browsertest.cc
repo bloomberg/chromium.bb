8036bd1d450b9ee244a151c660367b58f731944a
Peter Kvitek <kvitekp@chromium.org>
2018-07-12 20:37:18 +0000

    Implemented headless compositor protocol tests for basic rAF, GIF and CSS animations. (III)
    Browser flags that are required for BeginFrameControl are now set only
    for compositor specific tests thus preventing virtual time flakyness.
    
    Reinstate VirtualTimeErrorLoop test since this change fixes its
    flakiness.
    
    Change-Id: I0b01a81df1b8be63da316b27f7404f929f1e0fd1
    Reviewed-on: https://chromium-review.googlesource.com/1133711
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574708}
    Former-commit-id: dcd804265b610b002db7023e6c180bd12e1e29bb

--- headless/test/headless_protocol_browsertest.cc
+++ headless/test/headless_protocol_browsertest.cc
@@ -10,8 +10,13 @@
 #include "base/files/file_util.h"
 #include "base/json/json_reader.h"
 #include "base/path_service.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/threading/thread_restrictions.h"
 #include "build/build_config.h"
+#include "cc/base/switches.h"
+#include "components/viz/common/features.h"
+#include "components/viz/common/switches.h"
+#include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test.h"
 #include "headless/public/devtools/domains/runtime.h"
 #include "headless/public/headless_browser.h"
@@ -212,15 +217,12 @@ HEADLESS_PROTOCOL_TEST(VirtualTimeSessionStorage,
 HEADLESS_PROTOCOL_TEST(VirtualTimeStarvation,
                        "emulation/virtual-time-starvation.js");
 HEADLESS_PROTOCOL_TEST(VirtualTimeVideo, "emulation/virtual-time-video.js");
+HEADLESS_PROTOCOL_TEST(VirtualTimeErrorLoop,
+                       "emulation/virtual-time-error-loop.js");
 
 // Flaky Test crbug.com/859382
 HEADLESS_PROTOCOL_TEST(DISABLED_VirtualTimeHistoryNavigation,
                        "emulation/virtual-time-history-navigation.js");
-
-// Flaky Test crbug.com/860123. See also failing on MacOS10.13 (dbg)
-// crbug.com/859382
-HEADLESS_PROTOCOL_TEST(DISABLED_VirtualTimeErrorLoop,
-                       "emulation/virtual-time-error-loop.js");
 
 // http://crbug.com/633321
 #if defined(OS_ANDROID)
@@ -236,5 +238,79 @@ HEADLESS_PROTOCOL_TEST(MAYBE_VirtualTimeTimerSuspend,
                        "emulation/virtual-time-timer-suspended.js");
 #undef MAYBE_VirtualTimeTimerOrder
 #undef MAYBE_VirtualTimeTimerSuspend
+
+class HeadlessProtocolCompositorBrowserTest
+    : public HeadlessProtocolBrowserTest {
+ public:
+  HeadlessProtocolCompositorBrowserTest() = default;
+
+ private:
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    HeadlessProtocolBrowserTest::SetUpCommandLine(command_line);
+    // The following switches are recommended for BeginFrameControl required by
+    // compositor tests, see https://goo.gl/3zHXhB for details
+    static const char* const compositor_switches[] = {
+        // We control BeginFrames ourselves and need all compositing stages to
+        // run.
+        switches::kRunAllCompositorStagesBeforeDraw,
+        switches::kDisableNewContentRenderingTimeout,
+
+        // Animtion-only BeginFrames are only supported when updates from the
+        // impl-thread are disabled, see go/headless-rendering.
+        cc::switches::kDisableThreadedAnimation,
+        cc::switches::kDisableCheckerImaging,
+        switches::kDisableThreadedScrolling,
+
+        // Ensure that image animations don't resync their animation timestamps
+        // when looping back around.
+        switches::kDisableImageAnimationResync,
+    };
+
+    for (auto* compositor_switch : compositor_switches) {
+      command_line->AppendSwitch(compositor_switch);
+    }
+
+    // In surface synchronization, child surface IDs are allocated by
+    // parents and new CompositorFrames only activate once all their child
+    // surfaces exist. In --run-all-compositor-stages-before-draw mode, this
+    // means that child surface initialization and resize fully propagates
+    // within a single BeginFrame.
+    scoped_feature_list_.InitAndEnableFeature(
+        features::kEnableSurfaceSynchronization);
+  }
+
+ protected:
+  base::test::ScopedFeatureList scoped_feature_list_;
+};
+
+#define HEADLESS_PROTOCOL_COMPOSITOR_TEST(TEST_NAME, SCRIPT_NAME)            \
+  IN_PROC_BROWSER_TEST_F(HeadlessProtocolCompositorBrowserTest, TEST_NAME) { \
+    test_folder_ = "/protocol/";                                             \
+    script_name_ = SCRIPT_NAME;                                              \
+    RunTest();                                                               \
+  }
+
+// BeginFrameControl is not supported on MacOS yet, see: https://cs.chromium.org
+// chromium/src/headless/lib/browser/protocol/target_handler.cc?
+// rcl=5811aa08e60ba5ac7622f029163213cfbdb682f7&l=32
+#if defined(OS_MACOSX)
+#define MAYBE_CompositorBasicRaf DISABLED_CompositorBasicRaf
+#define MAYBE_CompositorImageAnimation DISABLED_CompositorImageAnimation
+#define MAYBE_CompositorCssAnimation DISABLED_CompositorCssAnimation
+#else
+#define MAYBE_CompositorBasicRaf CompositorBasicRaf
+#define MAYBE_CompositorImageAnimation CompositorImageAnimation
+#define MAYBE_CompositorCssAnimation CompositorCssAnimation
+#endif
+HEADLESS_PROTOCOL_COMPOSITOR_TEST(MAYBE_CompositorBasicRaf,
+                                  "emulation/compositor-basic-raf.js");
+HEADLESS_PROTOCOL_COMPOSITOR_TEST(
+    MAYBE_CompositorImageAnimation,
+    "emulation/compositor-image-animation-test.js");
+HEADLESS_PROTOCOL_COMPOSITOR_TEST(MAYBE_CompositorCssAnimation,
+                                  "emulation/compositor-css-animation-test.js");
+#undef MAYBE_CompositorBasicRaf
+#undef MAYBE_CompositorImageAnimation
+#undef MAYBE_CompositorCssAnimation
 
 }  // namespace headless
