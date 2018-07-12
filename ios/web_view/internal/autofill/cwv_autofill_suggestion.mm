e724feeb68f0335b1ea599ae1ea1916e5f33260e
Scott Wu <scottwu@chromium.org>
2018-07-12 01:38:12 +0000

    Add icon getter for CWVAutofillSuggestion.
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ic1c360a24dfdb35581a13cf0532173d66c9f953c
    Reviewed-on: https://chromium-review.googlesource.com/1131050
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Reviewed-by: John Wu <jzw@chromium.org>
    Commit-Queue: Scott Wu <scottwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574450}
    Former-commit-id: de45c8fcb6f97db5346bc6a52dc4edba0b7d1f3e

--- ios/web_view/internal/autofill/cwv_autofill_suggestion.mm
+++ ios/web_view/internal/autofill/cwv_autofill_suggestion.mm
@@ -4,8 +4,11 @@
 
 #import "ios/web_view/internal/autofill/cwv_autofill_suggestion_internal.h"
 
+#include "base/strings/sys_string_conversions.h"
+#include "components/autofill/core/browser/credit_card.h"
 #include "components/autofill/core/browser/popup_item_ids.h"
 #import "components/autofill/ios/browser/form_suggestion.h"
+#include "ui/base/resource/resource_bundle.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -40,6 +43,17 @@
 
 - (NSString*)displayDescription {
   return [_formSuggestion.displayDescription copy];
+}
+
+- (UIImage* __nullable)icon {
+  if ([_formSuggestion.icon length] == 0) {
+    return nil;
+  }
+  int resourceID = autofill::CreditCard::IconResourceId(
+      base::SysNSStringToUTF8(_formSuggestion.icon));
+  return ui::ResourceBundle::GetSharedInstance()
+      .GetNativeImageNamed(resourceID)
+      .ToUIImage();
 }
 
 #pragma mark - NSObject
