e7f998283f97dd17e0a4764f9bf4728880b79345
Nico Weber <thakis@chromium.org>
2018-07-12 00:36:28 +0000

    Make blink's {CORE,PLATFORM,MODULES}_TEMPLATE_CLASS_EXPORT macros no-ops.
    On Windows, the macro already had no effect.
    
    Export on template classes doesn't make a lot of sense: The template itself
    doesn't exist, only its instantiations do. The export is inherited by the
    instantiations, so if the template had an instantiation in
    core/platform/modules then that instantiation would be visible outside the
    respective .so, but without an explicit instantation declaration, clients
    outside those sos still get their own instantation and that doesn't really do
    anything.
    
    (The bindings generator also uses this macro on explicit template
    specializations which _are_ instantiations, so the macro does have
    a useful effect there -- but it looks like the effect there isn't needed
    since everything builds fine with this patch applied.)
    
    While here, also remove a warning suppression pragma for a warning that's
    been disabled globally since #492411.
    
    Bug: 859989
    Change-Id: Ib22993d06f3ad62c79e2b53b010614ae6577d227
    Reviewed-on: https://chromium-review.googlesource.com/1133329
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574431}
    Former-commit-id: 59aea43550238d1866c865e032633e101e7f1fa4

--- third_party/blink/renderer/platform/platform_export.h
+++ third_party/blink/renderer/platform/platform_export.h
@@ -33,9 +33,6 @@
 // - PLATFORM_EXPORT
 //   Exports non-template symbols.
 //
-// - PLATFORM_TEMPLATE_CLASS_EXPORT
-//   Exports an entire definition of class template.
-//
 // - PLATFORM_EXTERN_TEMPLATE_EXPORT
 //   Applicable to template declarations (except for definitions). The
 //   corresponding definition must come along with PLATFORM_TEMPLATE_EXPORT.
@@ -85,43 +82,29 @@
 #endif  // !defined(COMPONENT_BUILD)
 
 //
-// PLATFORM_TEMPLATE_CLASS_EXPORT
 // PLATFORM_EXTERN_TEMPLATE_EXPORT
 // PLATFORM_TEMPLATE_EXPORT
 //
 #if BLINK_PLATFORM_IMPLEMENTATION
 
 #if defined(COMPILER_MSVC)
-#define PLATFORM_TEMPLATE_CLASS_EXPORT
 #define PLATFORM_EXTERN_TEMPLATE_EXPORT PLATFORM_EXPORT
 #define PLATFORM_TEMPLATE_EXPORT PLATFORM_EXPORT
 #endif
 
 #if defined(COMPILER_GCC)
-#define PLATFORM_TEMPLATE_CLASS_EXPORT PLATFORM_EXPORT
 #define PLATFORM_EXTERN_TEMPLATE_EXPORT PLATFORM_EXPORT
 #define PLATFORM_TEMPLATE_EXPORT
 #endif
 
 #else  // BLINK_PLATFORM_IMPLEMENTATION
 
-#define PLATFORM_TEMPLATE_CLASS_EXPORT
 #define PLATFORM_EXTERN_TEMPLATE_EXPORT PLATFORM_EXPORT
 #define PLATFORM_TEMPLATE_EXPORT
 
 #endif  // BLINK_PLATFORM_IMPLEMENTATION
 
-#if defined(COMPILER_MSVC)
-// MSVC Compiler warning C4275:
-// non dll-interface class 'Bar' used as base for dll-interface class 'Foo'.
-// Note that this is intended to be used only when no access to the base class'
-// static data is done through derived classes or inline methods. For more info,
-// see http://msdn.microsoft.com/en-us/library/3tdb471s(VS.80).aspx
-//
-// This pragma will allow exporting a class that inherits from a non-exported
-// base class, anywhere in the Blink platform component. This is only
-// a problem when using the MSVC compiler on Windows.
-#pragma warning(suppress : 4275)
-#endif
+// TODO(thakis): Remove all references to this, https://crbug.com/859989
+#define PLATFORM_TEMPLATE_CLASS_EXPORT
 
 #endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_PLATFORM_EXPORT_H_
