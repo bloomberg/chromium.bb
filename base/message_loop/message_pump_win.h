e03f16ab2d85338830e6e04ba01a6d62da9b4e60
Lei Zhang <thestig@chromium.org>
2018-07-12 05:29:49 +0000

    Remove debugging code in base::MessagePumpWin.
    BUG=596190
    
    Change-Id: Ie9b6ee2cc1db8399247be767cabba1b39df50544
    Reviewed-on: https://chromium-review.googlesource.com/1128274
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574491}
    Former-commit-id: 95f61506c064bf502b2f2f81293866e40843ab2b

--- base/message_loop/message_pump_win.h
+++ base/message_loop/message_pump_win.h
@@ -38,11 +38,6 @@ class BASE_EXPORT MessagePumpWin : public MessagePump {
 
     // Used to count how many Run() invocations are on the stack.
     int run_depth;
-
-    // Used to help diagnose hangs.
-    // TODO(stanisc): crbug.com/596190: Remove these once the bug is fixed.
-    int schedule_work_error_count;
-    Time last_schedule_work_error_time;
   };
 
   // State used with |work_state_| variable.
