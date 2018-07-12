9ea1c6c85594cfaf5e84e47e1ab81dec2bf515b8
Wan-Teh Chang <wtc@google.com>
2018-07-12 10:11:33 -0700

    Delete an obsolete TODO comment.
    The action described in the TODO comment ("remove CONFIG_LOWBITDEPTH")
    was done in commit bf9d6d4b3a7e2496aaf226784861882bcba72322:
    
      https://aomedia-review.googlesource.com/c/aom/+/62961
    
    but the TODO comment was left behind inadvertently.
    
    Change-Id: I708fcd0eadfd7d36ff72c0de2b30661a967bdcd8

--- test/test_vector_test.cc
+++ test/test_vector_test.cc
@@ -144,9 +144,6 @@ TEST_P(TestVectorTest, MD5Match) {
   ASSERT_NO_FATAL_FAILURE(RunLoop(video.get(), cfg));
 }
 
-// TODO(yaowu): Current md5 check works only when CONFIG_LOWBITDEPTH is enabled,
-// remove CONFIG_LOWBITDEPTH when md5 check is reworked to be compatible with
-// CONFIG_LOWBITDEPTH = 0
 #if CONFIG_AV1_DECODER
 AV1_INSTANTIATE_TEST_CASE(
     TestVectorTest,
