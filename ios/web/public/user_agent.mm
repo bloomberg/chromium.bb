62e82d6f50cb060fe44e5fa909ab6158b47576b0
Thiemo Nagel <tnagel@chromium.org>
2018-07-13 14:24:23 +0000

    Freeze iOS kernel version in user-agent
    Freeze the iOS kernel version in the user-agent string to be "15E148"
    for iOS 11.3 and later for consistency with Safari and to reduce the
    fingerprinting surface.
    
    Likewise, reduce OS version granularity from three numbers to two
    numbers for iOS 11.3 and later.
    
    BUG=860229
    
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I0165ba4ae9dfd51308e26ebecba1a5dfba3df398
    Reviewed-on: https://chromium-review.googlesource.com/1126105
    Commit-Queue: Thiemo Nagel <tnagel@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#574910}
    Former-commit-id: ca28ae7f2f9fedc710ef4de09af32c17c57786ef

--- ios/web/public/user_agent.mm
+++ ios/web/public/user_agent.mm
@@ -70,6 +70,21 @@ const UAVersions& GetUAVersionsForCurrentOS() {
   return version_map[arraysize(version_map) - 1].ua_versions;
 }
 
+std::string BuildKernelVersion() {
+  // Freeze the kernel version for iOS 11.3 and later (as Safari does).
+  if (@available(iOS 11.3, *))
+    return "15E148";
+
+  int mib[2] = {CTL_KERN, KERN_OSVERSION};
+  unsigned int namelen = sizeof(mib) / sizeof(mib[0]);
+  size_t bufferSize = 0;
+  sysctl(mib, namelen, nullptr, &bufferSize, nullptr, 0);
+  char kernel_version[bufferSize];
+  int result = sysctl(mib, namelen, kernel_version, &bufferSize, nullptr, 0);
+  DCHECK(result == 0);
+  return kernel_version;
+}
+
 }  // namespace
 
 namespace web {
@@ -102,6 +117,11 @@ std::string BuildOSCpuInfo() {
   base::SysInfo::OperatingSystemVersionNumbers(&os_major_version,
                                                &os_minor_version,
                                                &os_bugfix_version);
+
+  // Drop bugfix version for iOS 11.3 and later (as Safari does).
+  if (@available(iOS 11.3, *))
+    os_bugfix_version = 0;
+
   std::string os_version;
   if (os_bugfix_version == 0) {
     base::StringAppendF(&os_version,
@@ -136,15 +156,6 @@ std::string BuildOSCpuInfo() {
 }
 
 std::string BuildUserAgentFromProduct(const std::string& product) {
-  // Retrieve the kernel build number.
-  int mib[2] = {CTL_KERN, KERN_OSVERSION};
-  unsigned int namelen = sizeof(mib) / sizeof(mib[0]);
-  size_t bufferSize = 0;
-  sysctl(mib, namelen, nullptr, &bufferSize, nullptr, 0);
-  char kernel_version[bufferSize];
-  int result = sysctl(mib, namelen, kernel_version, &bufferSize, nullptr, 0);
-  DCHECK(result == 0);
-
   UAVersions ua_versions = GetUAVersionsForCurrentOS();
 
   std::string user_agent;
@@ -152,9 +163,8 @@ std::string BuildUserAgentFromProduct(const std::string& product) {
                       "Mozilla/5.0 (%s) AppleWebKit/%s"
                       " (KHTML, like Gecko) %s Mobile/%s Safari/%s",
                       BuildOSCpuInfo().c_str(),
-                      ua_versions.webkit_version_string,
-                      product.c_str(),
-                      kernel_version,
+                      ua_versions.webkit_version_string, product.c_str(),
+                      BuildKernelVersion().c_str(),
                       ua_versions.safari_version_string);
 
   return user_agent;
