86f4dbcd0c6a994232ab729d59d94189c9f4c6d8
Tommy Steimel <steimel@chromium.org>
2018-07-13 23:39:32 +0000

    Revert "[Media Intent Handler] Add UMA metrics for media type"
    This reverts commit e5d2f78812a6719304f05a0c2604955ab683349d [formerly 76ee32bd378f02564effa4ddc7b956cb0b134123].
    
    Reason for revert: Histogram recording crashes Chrome if it isn't already open because native isn't initialized.
    
    Original change's description:
    > [Media Intent Handler] Add UMA metrics for media type
    > 
    > This CL adds a MediaLauncherActivity.MediaType histogram to track the
    > type of media the user opens with the MediaLauncherActivity
    > (audio/image/video).
    > 
    > Bug: 861954
    > Change-Id: I28cc0a379ad2bc70d34f2d2f5751c3eb2ebbda45
    > Reviewed-on: https://chromium-review.googlesource.com/1135522
    > Reviewed-by: Steven Holte <holte@chromium.org>
    > Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    > Commit-Queue: Tommy Steimel <steimel@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#575071}
    
    TBR=mlamouri@chromium.org,holte@chromium.org,steimel@chromium.org
    
    Change-Id: Ic090a8660d6139fc6043a8990b0c6966a9cb311e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 861954
    Reviewed-on: https://chromium-review.googlesource.com/1137392
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575116}
    Former-commit-id: 29f92361cbf018cc8af847ac3cc4d52224d5a2d5

--- chrome/android/java/src/org/chromium/chrome/browser/media/MediaLauncherActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/MediaLauncherActivity.java
@@ -8,31 +8,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.IntDef;
 
-import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.browser.util.IntentUtils;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 
 /**
  * The MediaLauncherActivity handles media-viewing Intents from other apps. It takes the given
  * content:// URI from the Intent and properly routes it to a media-viewing CustomTabActivity.
  */
 public class MediaLauncherActivity extends Activity {
-    // UMA histogram values for media types the user can open.
-    // Keep in sync with MediaLauncherActivityMediaType enum in enums.xml.
-    @IntDef({MediaType.AUDIO, MediaType.IMAGE, MediaType.VIDEO})
-    @Retention(RetentionPolicy.SOURCE)
-    @interface MediaType {
-        int AUDIO = 0;
-        int IMAGE = 1;
-        int VIDEO = 2;
-        int UNKNOWN = 3;
-        int NUM_ENTRIES = 4;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,12 +23,8 @@ public class MediaLauncherActivity extends Activity {
         Intent input = IntentUtils.sanitizeIntent(getIntent());
         Uri contentUri = input.getData();
         String mimeType = getContentResolver().getType(contentUri);
-        int mediaType = MediaViewerUtils.getMediaTypeFromMIMEType(mimeType);
 
-        RecordHistogram.recordEnumeratedHistogram(
-                "MediaLauncherActivity.MediaType", mediaType, MediaType.NUM_ENTRIES);
-
-        if (mediaType == MediaType.UNKNOWN) {
+        if (!MediaViewerUtils.isMediaMIMEType(mimeType)) {
             // With our intent-filter, we should only receive implicit intents with media MIME
             // types. If we receive a non-media MIME type, it is likely a malicious explicit intent,
             // so we should not proceed.
