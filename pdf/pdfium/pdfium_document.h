478df390efa44c15718e9ebec37255ff79e3a3d1
Artem Strygin <art-snake@yandex-team.ru>
2018-07-16 12:23:09 +0000

    Hide internal implementation of PDFiumDocument.
    Change-Id: I7e46d0b3e03708c425c35dcd3939e7b67cbacbd1
    Reviewed-on: https://chromium-review.googlesource.com/1136536
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Art Snake <art-snake@yandex-team.ru>
    Cr-Commit-Position: refs/heads/master@{#575233}
    Former-commit-id: 8a7145651ffa132b520d54f0da617f1a1f46beac

--- pdf/pdfium/pdfium_document.h
+++ pdf/pdfium/pdfium_document.h
@@ -19,13 +19,13 @@ class PDFiumDocument {
   explicit PDFiumDocument(DocumentLoader* doc_loader);
   ~PDFiumDocument();
 
-  FPDF_FILEACCESS& file_access() { return file_access_; }
-  FX_FILEAVAIL& file_availability() { return file_availability_; }
-  FX_DOWNLOADHINTS& download_hints() { return download_hints_; }
+  FPDF_FILEACCESS& file_access() { return *file_access_; }
+  FX_FILEAVAIL& file_availability() { return *file_availability_; }
+  FX_DOWNLOADHINTS& download_hints() { return *download_hints_; }
 
   FPDF_AVAIL fpdf_availability() const { return fpdf_availability_.get(); }
-  FPDF_DOCUMENT doc() const { return doc_.get(); }
-  FPDF_FORMHANDLE form() const { return form_.get(); }
+  FPDF_DOCUMENT doc() const { return doc_handle_.get(); }
+  FPDF_FORMHANDLE form() const { return form_handle_.get(); }
 
   int form_status() const { return form_status_; }
 
@@ -38,47 +38,27 @@ class PDFiumDocument {
   void InitializeForm(FPDF_FORMFILLINFO* form_info);
 
  private:
-  struct FileAvail : public FX_FILEAVAIL {
-    DocumentLoader* doc_loader;
-  };
-
-  struct DownloadHints : public FX_DOWNLOADHINTS {
-    DocumentLoader* doc_loader;
-  };
-
-  // PDFium interface to get block of data.
-  static int GetBlock(void* param,
-                      unsigned long position,
-                      unsigned char* buffer,
-                      unsigned long size);
-
-  // PDFium interface to check is block of data is available.
-  static FPDF_BOOL IsDataAvail(FX_FILEAVAIL* param, size_t offset, size_t size);
-
-  // PDFium interface to request download of the block of data.
-  static void AddSegment(FX_DOWNLOADHINTS* param, size_t offset, size_t size);
-
   DocumentLoader* const doc_loader_;
 
   // Interface structure to provide access to document stream.
-  FPDF_FILEACCESS file_access_;
+  std::unique_ptr<FPDF_FILEACCESS> file_access_;
 
   // Interface structure to check data availability in the document stream.
-  FileAvail file_availability_;
+  std::unique_ptr<FX_FILEAVAIL> file_availability_;
 
   // Interface structure to request data chunks from the document stream.
-  DownloadHints download_hints_;
+  std::unique_ptr<FX_DOWNLOADHINTS> download_hints_;
 
   // Pointer to the document availability interface.
   ScopedFPDFAvail fpdf_availability_;
 
   // The PDFium wrapper object for the document. Must come after
   // |fpdf_availability_| to prevent outliving it.
-  ScopedFPDFDocument doc_;
+  ScopedFPDFDocument doc_handle_;
 
   // The PDFium wrapper for form data.  Used even if there are no form controls
-  // on the page. Must come after |doc_| to prevent outliving it.
-  ScopedFPDFFormHandle form_;
+  // on the page. Must come after |doc_handle_| to prevent outliving it.
+  ScopedFPDFFormHandle form_handle_;
 
   // Current form availability status.
   int form_status_ = PDF_FORM_NOTAVAIL;
