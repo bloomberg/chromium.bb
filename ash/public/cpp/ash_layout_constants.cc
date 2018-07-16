921a424d99b574f6f7e3c17731d95a65ea9f354e
Lei Zhang <thestig@chromium.org>
2018-07-16 22:48:41 +0000

    Change ChromeOS restored frame top to 8 DIP.
    Change it to 8 from 7. Do this programmatically rather than with
    hard-coded constants.
    
    BUG=851101
    
    Change-Id: I638dfcfc641c92af860f4b0d4e630914bc6426f9
    Reviewed-on: https://chromium-review.googlesource.com/1137236
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575457}
    Former-commit-id: 2c224b0605ec4cb3c2fc1ec06e2bb2e07c489d9b

--- ash/public/cpp/ash_layout_constants.cc
+++ ash/public/cpp/ash_layout_constants.cc
@@ -11,28 +11,19 @@ namespace ash {
 
 gfx::Size GetAshLayoutSize(AshLayoutSize size) {
   constexpr int kButtonWidth = 32;
-  const int mode = ui::MaterialDesignController::GetMode();
-  switch (size) {
-    case AshLayoutSize::kBrowserCaptionMaximized: {
-      // These constants should be kept in sync with those for TAB_HEIGHT in
-      // chrome/browser/ui/layout_constants.cc.
-      // TODO: Ideally these values should be obtained from a common location.
-      constexpr int kBrowserMaximizedCaptionButtonHeight[] = {29, 33, 41, 34,
-                                                              41};
-      return gfx::Size(kButtonWidth,
-                       kBrowserMaximizedCaptionButtonHeight[mode]);
-    }
-    case AshLayoutSize::kBrowserCaptionRestored: {
-      constexpr int kBrowserRestoredCaptionButtonHeight[] = {36, 40, 48, 41,
-                                                             48};
-      return gfx::Size(kButtonWidth, kBrowserRestoredCaptionButtonHeight[mode]);
-    }
-    case AshLayoutSize::kNonBrowserCaption:
-      return gfx::Size(kButtonWidth, 32);
-  }
 
-  NOTREACHED();
-  return gfx::Size();
+  if (size == AshLayoutSize::kNonBrowserCaption)
+    return gfx::Size(kButtonWidth, 32);
+
+  // |kBrowserMaximizedCaptionButtonHeight| should be kept in sync with those
+  // for TAB_HEIGHT in // chrome/browser/ui/layout_constants.cc.
+  // TODO: Ideally these values should be obtained from a common location.
+  constexpr int kBrowserMaximizedCaptionButtonHeight[] = {29, 33, 41, 34, 41};
+  const int mode = ui::MaterialDesignController::GetMode();
+  int height = kBrowserMaximizedCaptionButtonHeight[mode];
+  if (size == AshLayoutSize::kBrowserCaptionRestored)
+    height += 8;  // Restored window titlebars are 8 DIP taller than maximized.
+  return gfx::Size(kButtonWidth, height);
 }
 
 }  // namespace ash
