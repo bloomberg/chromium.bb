03cf276909675d16bdd26070d3749321e7bf1775
Tom Anderson <thomasanderson@chromium.org>
2018-07-16 19:25:20 +0000

    Roll fontconfig to d1f48f11
    Commits:
    https://chromium.googlesource.com/external/fontconfig/+log/6cc99d6a82ad67d2f5eac887b28bca13c0dfddde..d1f48f11d5dffa1d954a1b0abe44ce9e4bfc3709
    
    BUG=857511
    TBR=dnicoara
    
    Change-Id: Ib3b499aa63d87c2886261ddcfbe692f24f7be59a
    Reviewed-on: https://chromium-review.googlesource.com/1138543
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575380}
    Former-commit-id: 3ff47e60c5ce645973e57fc8dbe88b43d63edaf4

--- third_party/fontconfig/include/src/fcstdint.h
+++ third_party/fontconfig/include/src/fcstdint.h
@@ -2,7 +2,7 @@
 #define _FONTCONFIG_SRC_FCSTDINT_H 1
 #ifndef _GENERATED_STDINT_H
 #define _GENERATED_STDINT_H "fontconfig 2.13.0"
-/* generated using gnu compiler gcc (Debian 7.3.0-21) 7.3.0 */
+/* generated using gnu compiler gcc (Debian 7.3.0-24) 7.3.0 */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
 #endif
