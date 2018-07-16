466d064862a0ebf853cc3994f8acd22e0888ce5f
Maja Kabus <kabusm@google.com>
2018-07-16 12:19:42 +0000

    HTMLBaseElement::href changed to accept TrustedTypes
    Changed implementation of setHref(), older version is abandoned.
    Added href() getter that accepts URLString as an argument.
    
    Bug: 739170
    Change-Id: I7703c1a33f037a912f1a105d8c79ef8fe5b4a289
    Reviewed-on: https://chromium-review.googlesource.com/1136544
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Maja Kabus <kabusm@google.com>
    Cr-Commit-Position: refs/heads/master@{#575232}
    Former-commit-id: c289eae7a6054babad42b4cf6860da9de1468c12

--- third_party/blink/renderer/core/html/html_base_element.h
+++ third_party/blink/renderer/core/html/html_base_element.h
@@ -27,6 +27,9 @@
 
 namespace blink {
 
+class ExceptionState;
+class USVStringOrTrustedURL;
+
 class HTMLBaseElement final : public HTMLElement {
   DEFINE_WRAPPERTYPEINFO();
 
@@ -34,7 +37,8 @@ class HTMLBaseElement final : public HTMLElement {
   DECLARE_NODE_FACTORY(HTMLBaseElement);
 
   KURL href() const;
-  void setHref(const AtomicString&);
+  void href(USVStringOrTrustedURL&) const;
+  void setHref(const USVStringOrTrustedURL&, ExceptionState&);
 
  private:
   explicit HTMLBaseElement(Document&);
