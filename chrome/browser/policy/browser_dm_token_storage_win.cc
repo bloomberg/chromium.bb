3fbc66afb0646dab85181a7d1d1ec322338cc84d
Owen Min <zmin@chromium.org>
2018-07-16 15:45:36 +0000

    Revert "Refactor BrowserDMTokenStorage for easier platform implementations."
    This reverts commit e8bd7ff736515c3478636b714ac602cf33d46b45 [formerly 1c479eb8e6677209ab599e3102e32d2d9dc99726].
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > Refactor BrowserDMTokenStorage for easier platform implementations.
    > 
    > This CL refactors BrowserDMTokenStorage & BrowserDMTokenStorageWin to move code that is common to all platforms.
    > 
    > After this CL, adding a new platform requires implementing the following methods:
    > 
    > -  virtual std::string InitClientId();
    > -  virtual std::string InitEnrollmentToken();
    > -  virtual std::string InitDMToken();
    > -  virtual void SaveDMToken(const std::string& token);
    > 
    > Note that this CL keeps the existing Windows implementation, it only refactors the code (no functional change).
    > 
    > BUG=812641
    > 
    > Change-Id: If362c4368160e7bd038a8a4b1d93e151c0a60e1b
    > Reviewed-on: https://chromium-review.googlesource.com/1133315
    > Commit-Queue: Georges Khalil <georgesak@chromium.org>
    > Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    > Reviewed-by: Roger Tawa <rogerta@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#575276}
    
    TBR=rogerta@chromium.org,pastarmovj@chromium.org,georgesak@chromium.org,zmin@chromium.org
    
    Change-Id: Idbf78da964051aaac710b780fabccf248c8d583e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 812641
    Reviewed-on: https://chromium-review.googlesource.com/1138513
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575281}
    Former-commit-id: e0ca7658cdbb708cd2751cc1e62ffae6ac04d313

--- chrome/browser/policy/browser_dm_token_storage_win.cc
+++ chrome/browser/policy/browser_dm_token_storage_win.cc
@@ -40,7 +40,81 @@
 #endif  // defined(GOOGLE_CHROME_BUILD)
 
 namespace policy {
+
 namespace {
+
+std::string GetClientId() {
+  // For the client id, use the Windows machine GUID.
+  // TODO(crbug.com/821977): Need a backup plan if machine GUID doesn't exist.
+  base::win::RegKey key;
+  LSTATUS status = key.Open(HKEY_LOCAL_MACHINE,
+                            L"SOFTWARE\\Microsoft\\Cryptography", KEY_READ);
+  if (status != ERROR_SUCCESS)
+    return std::string();
+
+  base::string16 value;
+  status = key.ReadValue(L"MachineGuid", &value);
+  if (status != ERROR_SUCCESS)
+    return std::string();
+
+  std::string client_id;
+  if (!base::WideToUTF8(value.c_str(), value.length(), &client_id))
+    return std::string();
+  return client_id;
+}
+
+std::string GetDMToken() {
+  base::win::RegKey key;
+  base::string16 dm_token_key_path;
+  base::string16 dm_token_value_name;
+  InstallUtil::GetMachineLevelUserCloudPolicyDMTokenRegistryPath(
+      &dm_token_key_path, &dm_token_value_name);
+  LONG result = key.Open(HKEY_LOCAL_MACHINE, dm_token_key_path.c_str(),
+                         KEY_QUERY_VALUE | KEY_WOW64_64KEY);
+  if (result != ERROR_SUCCESS)
+    return std::string();
+
+  // At the time of writing (January 2018), the DM token is about 200 bytes
+  // long. The initial size of the buffer should be enough to cover most
+  // realistic future size-increase scenarios, although we still make an effort
+  // to support somewhat larger token sizes just to be safe.
+  constexpr size_t kInitialDMTokenSize = 512;
+
+  DWORD size = kInitialDMTokenSize;
+  std::vector<char> raw_value(size);
+  DWORD dtype = REG_NONE;
+  result = key.ReadValue(dm_token_value_name.c_str(), raw_value.data(), &size,
+                         &dtype);
+  if (result == ERROR_MORE_DATA && size <= installer::kMaxDMTokenLength) {
+    raw_value.resize(size);
+    result = key.ReadValue(dm_token_value_name.c_str(), raw_value.data(), &size,
+                           &dtype);
+  }
+
+  if (result != ERROR_SUCCESS || dtype != REG_BINARY || size == 0) {
+    DVLOG(1) << "Failed to get DMToken from Registry.";
+    return std::string();
+  }
+  DCHECK_LE(size, installer::kMaxDMTokenLength);
+  std::string dm_token;
+  dm_token.assign(raw_value.data(), size);
+  return dm_token;
+}
+
+#if defined(GOOGLE_CHROME_BUILD)
+// Explicitly allow DMTokenStorage impersonate the client since some COM code
+// elsewhere in the browser process may have previously used
+// CoInitializeSecurity to set the impersonation level to something other than
+// the default. Ignore errors since an attempt to use Google Update may succeed
+// regardless.
+void ConfigureProxyBlanket(IUnknown* interface_pointer) {
+  ::CoSetProxyBlanket(
+      interface_pointer, RPC_C_AUTHN_DEFAULT, RPC_C_AUTHZ_DEFAULT,
+      COLE_DEFAULT_PRINCIPAL, RPC_C_AUTHN_LEVEL_PKT_PRIVACY,
+      RPC_C_IMP_LEVEL_IMPERSONATE, nullptr, EOAC_DYNAMIC_CLOAKING);
+}
+#endif  // defined(GOOGLE_CHROME_BUILD)
+
 bool StoreDMTokenInRegistry(const std::string& token) {
 #if defined(GOOGLE_CHROME_BUILD)
   if (token.empty())
@@ -110,92 +184,11 @@ bool StoreDMTokenInRegistry(const std::string& token) {
   return false;
 #endif  // defined(GOOGLE_CHROME_BUILD)
 }
+
 }  // namespace
 
-std::string BrowserDMTokenStorageWin::InitClientId() {
-  // For the client id, use the Windows machine GUID.
-  // TODO(crbug.com/821977): Need a backup plan if machine GUID doesn't exist.
-  base::win::RegKey key;
-  LSTATUS status = key.Open(HKEY_LOCAL_MACHINE,
-                            L"SOFTWARE\\Microsoft\\Cryptography", KEY_READ);
-  if (status != ERROR_SUCCESS)
-    return std::string();
-
-  base::string16 value;
-  status = key.ReadValue(L"MachineGuid", &value);
-  if (status != ERROR_SUCCESS)
-    return std::string();
-
-  std::string client_id;
-  if (!base::WideToUTF8(value.c_str(), value.length(), &client_id))
-    return std::string();
-  return client_id;
-}
-
-std::string BrowserDMTokenStorageWin::InitEnrollmentToken() {
-  return base::WideToUTF8(
-      InstallUtil::GetMachineLevelUserCloudPolicyEnrollmentToken());
-}
-
-std::string BrowserDMTokenStorageWin::InitDMToken() {
-  base::win::RegKey key;
-  base::string16 dm_token_key_path;
-  base::string16 dm_token_value_name;
-  InstallUtil::GetMachineLevelUserCloudPolicyDMTokenRegistryPath(
-      &dm_token_key_path, &dm_token_value_name);
-  LONG result = key.Open(HKEY_LOCAL_MACHINE, dm_token_key_path.c_str(),
-                         KEY_QUERY_VALUE | KEY_WOW64_64KEY);
-  if (result != ERROR_SUCCESS)
-    return std::string();
-
-  // At the time of writing (January 2018), the DM token is about 200 bytes
-  // long. The initial size of the buffer should be enough to cover most
-  // realistic future size-increase scenarios, although we still make an effort
-  // to support somewhat larger token sizes just to be safe.
-  constexpr size_t kInitialDMTokenSize = 512;
-
-  DWORD size = kInitialDMTokenSize;
-  std::vector<char> raw_value(size);
-  DWORD dtype = REG_NONE;
-  result = key.ReadValue(dm_token_value_name.c_str(), raw_value.data(), &size,
-                         &dtype);
-  if (result == ERROR_MORE_DATA && size <= installer::kMaxDMTokenLength) {
-    raw_value.resize(size);
-    result = key.ReadValue(dm_token_value_name.c_str(), raw_value.data(), &size,
-                           &dtype);
-  }
-
-  if (result != ERROR_SUCCESS || dtype != REG_BINARY || size == 0) {
-    DVLOG(1) << "Failed to get DMToken from Registry.";
-    return std::string();
-  }
-  DCHECK_LE(size, installer::kMaxDMTokenLength);
-  std::string dm_token;
-  dm_token.assign(raw_value.data(), size);
-  return dm_token;
-}
-
-#if defined(GOOGLE_CHROME_BUILD)
-// Explicitly allow DMTokenStorage impersonate the client since some COM code
-// elsewhere in the browser process may have previously used
-// CoInitializeSecurity to set the impersonation level to something other than
-// the default. Ignore errors since an attempt to use Google Update may succeed
-// regardless.
-void ConfigureProxyBlanket(IUnknown* interface_pointer) {
-  ::CoSetProxyBlanket(
-      interface_pointer, RPC_C_AUTHN_DEFAULT, RPC_C_AUTHZ_DEFAULT,
-      COLE_DEFAULT_PRINCIPAL, RPC_C_AUTHN_LEVEL_PKT_PRIVACY,
-      RPC_C_IMP_LEVEL_IMPERSONATE, nullptr, EOAC_DYNAMIC_CLOAKING);
-}
-#endif  // defined(GOOGLE_CHROME_BUILD)
-
-void BrowserDMTokenStorageWin::SaveDMToken(const std::string& token) {
-  base::PostTaskAndReplyWithResult(
-      com_sta_task_runner_.get(), FROM_HERE,
-      base::BindOnce(&StoreDMTokenInRegistry, token),
-      base::BindOnce(&BrowserDMTokenStorage::OnDMTokenStored,
-                     weak_factory_.GetWeakPtr()));
-}
+// static
+BrowserDMTokenStorage* BrowserDMTokenStorage::storage_for_testing_ = nullptr;
 
 // static
 BrowserDMTokenStorage* BrowserDMTokenStorage::Get() {
@@ -209,8 +202,85 @@ BrowserDMTokenStorage* BrowserDMTokenStorage::Get() {
 BrowserDMTokenStorageWin::BrowserDMTokenStorageWin()
     : com_sta_task_runner_(
           base::CreateCOMSTATaskRunnerWithTraits({base::MayBlock()})),
-      weak_factory_(this) {}
+      is_initialized_(false),
+      weak_factory_(this) {
+  DETACH_FROM_SEQUENCE(sequence_checker_);
 
-BrowserDMTokenStorageWin::~BrowserDMTokenStorageWin() {}
+  // We don't call InitIfNeeded() here so that the global instance can be
+  // created early during startup if needed. The tokens and client ID are read
+  // from the system as part of the first retrieve or store operation.
+}
+
+BrowserDMTokenStorageWin::~BrowserDMTokenStorageWin() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+}
+
+std::string BrowserDMTokenStorageWin::RetrieveClientId() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  InitIfNeeded();
+  return client_id_;
+}
+
+std::string BrowserDMTokenStorageWin::RetrieveEnrollmentToken() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  InitIfNeeded();
+  return enrollment_token_;
+}
+
+void BrowserDMTokenStorageWin::StoreDMToken(const std::string& dm_token,
+                                            StoreCallback callback) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK(!store_callback_);
+
+  InitIfNeeded();
+
+  dm_token_ = dm_token;
+
+  store_callback_ = std::move(callback);
+
+  base::PostTaskAndReplyWithResult(
+      com_sta_task_runner_.get(), FROM_HERE,
+      base::BindOnce(&StoreDMTokenInRegistry, dm_token),
+      base::BindOnce(&BrowserDMTokenStorageWin::OnDMTokenStored,
+                     weak_factory_.GetWeakPtr()));
+}
+
+std::string BrowserDMTokenStorageWin::RetrieveDMToken() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK(!store_callback_);
+
+  InitIfNeeded();
+  return dm_token_;
+}
+
+void BrowserDMTokenStorageWin::InitIfNeeded() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  if (is_initialized_)
+    return;
+
+  is_initialized_ = true;
+  client_id_ = GetClientId();
+  DVLOG(1) << "Client ID = " << client_id_;
+  if (client_id_.empty())
+    return;
+
+  enrollment_token_ = base::WideToUTF8(
+      InstallUtil::GetMachineLevelUserCloudPolicyEnrollmentToken());
+  DVLOG(1) << "Enrollment token = " << enrollment_token_;
+
+  dm_token_ = GetDMToken();
+  DVLOG(1) << "DM Token = " << dm_token_;
+}
+
+void BrowserDMTokenStorageWin::OnDMTokenStored(bool success) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK(store_callback_);
+
+  if (!store_callback_.is_null())
+    std::move(store_callback_).Run(success);
+}
 
 }  // namespace policy
