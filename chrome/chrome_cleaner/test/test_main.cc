622c5723756593958b4bdbb96b5521b7ce4b024c
Olivier Li <olivierli@chromium.org>
2018-07-17 21:07:41 +0000

    Add the pup_data directory
    Bug: 830892
    Change-Id: Id148e1503564e86f220a6a775aa9055294e465b1
    Reviewed-on: https://chromium-review.googlesource.com/1140198
    Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    Commit-Queue: Oliver Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575771}
    Former-commit-id: bb31578254ac036fe8445c9828d837aa8693c79e

--- chrome/chrome_cleaner/test/test_main.cc
+++ chrome/chrome_cleaner/test/test_main.cc
@@ -6,9 +6,17 @@
 #include "base/bind_helpers.h"
 #include "base/test/launcher/unit_test_launcher.h"
 #include "base/test/test_suite.h"
+#include "chrome/chrome_cleaner/os/initializer.h"
+#include "chrome/chrome_cleaner/pup_data/pup_data.h"
+#include "chrome/chrome_cleaner/test/test_uws_catalog.h"
 
 int main(int argc, char** argv) {
   base::TestSuite test_suite(argc, argv);
+
+  chrome_cleaner::PUPData::InitializePUPData(
+      {&chrome_cleaner::TestUwSCatalog::GetInstance()});
+  chrome_cleaner::InitializeOSUtils();
+
   return base::LaunchUnitTests(
       argc, argv,
       base::BindOnce(&base::TestSuite::Run, base::Unretained(&test_suite)));
