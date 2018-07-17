3624dfa97aa10ef22973f527c73b0644b5ee2f5b
Ahmed Fakhry <afakhry@google.com>
2018-07-17 18:36:57 +0000

    Adjust page mobile-like behavior in tablet mode
    This prevents using the mobile-viewport and the
    Android-style scrollbars for pages in tablet mode.
    This gives a better UX on tablets and prevents
    unexpected page layout changes when switching to
    tablet mode.
    
    BUG=831473
    TEST=Manually by visiting old.reddit.com/r/chromeos
         and switching to tablet mode. Page layout should
         not change to the mobile one, and ChromeOS
         scrollbars should remain in use.
         Also modified modified browser_tests.
    
    Change-Id: Id858c03d37515f48b0f3515f099853999e3aafdd
    Reviewed-on: https://chromium-review.googlesource.com/1139075
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575732}
    Former-commit-id: b9ef14fc54809521f4011afcbd1844e77e0ee2f3

--- chrome/browser/chromeos/chrome_content_browser_client_chromeos_part.cc
+++ chrome/browser/chromeos/chrome_content_browser_client_chromeos_part.cc
@@ -72,10 +72,7 @@ void ChromeContentBrowserClientChromeOsPart::OverrideWebkitPrefs(
 
   web_prefs->double_tap_to_zoom_enabled = true;
   web_prefs->text_autosizing_enabled = true;
-  web_prefs->viewport_enabled = true;
-  web_prefs->viewport_meta_enabled = true;
   web_prefs->shrinks_viewport_contents_to_fit = true;
-  web_prefs->viewport_style = content::ViewportStyle::MOBILE;
   web_prefs->main_frame_resizes_are_orientation_changes = true;
   web_prefs->default_minimum_page_scale_factor = 0.25f;
   web_prefs->default_maximum_page_scale_factor = 5.0;
