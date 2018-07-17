1a9887746f53df670c7076e422f6cb4d94fb09cf
Benoit L <lizeb@chromium.org>
2018-07-17 02:49:59 +0000

    Revert "blink/wtf: Really move MovableStrings when DCHECK_IS_ON."
    This reverts commit c784976ad8a0bf5308f839e982915c09c2bbedef [formerly d1fef2b1ad38fe1ef8c3914fb5dadc39bae80b9b].
    
    Reason for revert: Suspected culprit for crbug.com/863786
    
    Original change's description:
    > blink/wtf: Really move MovableStrings when DCHECK_IS_ON.
    >
    > When a MovableString is parked, actually park the content, and poison
    > the previous allocation. This is intended to find cached raw pointer
    > accesses, which are not allowed beyond the UI thread task per the
    > contract of the class.
    > Enabled only when DCHECK_IS_ON to prevent potential crashes.
    >
    > Changes with and without DCHECK()s:
    > - Cache some String properties in MovableStringImpl
    > - Add a parked_strings_ to MovableStringTable for accounting, and
    >   update tests accordingly.
    >
    > Changes with DCHECK()s:
    > - Add a parked_string_ member to MovableStringImpl
    > - Copy the string to the parked version in Park(), and poison the
    >   previous allocation.
    > - Restore the string in Unpark().
    >
    > Bug: 837659
    > Change-Id: Ib7631d5e12471826c9e71a424e30d26cf8f1f042
    > Reviewed-on: https://chromium-review.googlesource.com/1133169
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Commit-Queue: Benoit L <lizeb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#574529}
    
    TBR=haraken@chromium.org,lizeb@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 837659, 863786
    Change-Id: I380210859a1eed42d869c1548d0d93b0e8606e58
    Reviewed-on: https://chromium-review.googlesource.com/1138873
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575530}
    Former-commit-id: 38b255587ab8598eee49da1b7581646682f6d5f5

--- third_party/blink/renderer/platform/wtf/text/movable_string.cc
+++ third_party/blink/renderer/platform/wtf/text/movable_string.cc
@@ -4,16 +4,13 @@
 
 #include "third_party/blink/renderer/platform/wtf/text/movable_string.h"
 
-#include <string.h>
-
 #include "base/metrics/histogram_macros.h"
 
 namespace WTF {
 
 namespace {
 
-template <typename T>
-bool IsLargeEnough(const T* impl) {
+bool IsLargeEnough(const StringImpl* impl) {
   // Don't attempt to park strings smaller than this size.
   static constexpr unsigned int kSizeThreshold = 10000;
   return impl && impl->length() > kSizeThreshold;
@@ -28,28 +25,18 @@ void RecordParkingAction(MovableStringImpl::ParkingAction action) {
 MovableStringImpl::MovableStringImpl() = default;
 
 MovableStringImpl::MovableStringImpl(scoped_refptr<StringImpl>&& impl)
-    : is_parked_(false),
-      string_(std::move(impl))
-#if DCHECK_IS_ON()
-      ,
-      parked_string_()
-#endif
-{
-  is_null_ = string_.IsNull();
-  is_8bit_ = is_null_ ? false : string_.Is8Bit();
-  length_ = is_null_ ? 0 : string_.length();
-}
+    : string_(std::move(impl)), is_parked_(false) {}
 
 MovableStringImpl::~MovableStringImpl() {
-  MovableStringTable::Instance().Remove(this, string_.Impl());
+  MovableStringTable::Instance().Remove(string_.Impl());
 }
 
 bool MovableStringImpl::Is8Bit() const {
-  return is_8bit_;
+  return string_.Is8Bit();
 }
 
 bool MovableStringImpl::IsNull() const {
-  return is_null_;
+  return string_.IsNull();
 }
 
 const String& MovableStringImpl::ToString() {
@@ -58,24 +45,13 @@ const String& MovableStringImpl::ToString() {
 }
 
 unsigned MovableStringImpl::CharactersSizeInBytes() const {
-  return length() * (Is8Bit() ? sizeof(LChar) : sizeof(UChar));
+  return string_.CharactersSizeInBytes();
 }
 
 bool MovableStringImpl::Park() {
   // Cannot park strings with several references.
-  if (!is_parked_ && string_.Impl()->HasOneRef()) {
+  if (string_.Impl()->HasOneRef()) {
     RecordParkingAction(ParkingAction::kParkedInBackground);
-#if DCHECK_IS_ON()
-    // Since the copy is done before freeing the original data, the two strings
-    // are guaranteed to not have the same address.
-    parked_string_ = string_.IsolatedCopy();
-    // Poison the previous allocation.
-    // |string_| is kept as is to make sure the memory isn't reused.
-    const void* data = Is8Bit()
-                           ? static_cast<const void*>(string_.Characters8())
-                           : static_cast<const void*>(string_.Characters16());
-    memset(const_cast<void*>(data), 0xcc, string_.CharactersSizeInBytes());
-#endif
     is_parked_ = true;
   }
   return is_parked_;
@@ -89,20 +65,15 @@ void MovableStringImpl::Unpark() {
   RecordParkingAction(backgrounded ? ParkingAction::kUnparkedInBackground
                                    : ParkingAction::kUnparkedInForeground);
   is_parked_ = false;
-#if DCHECK_IS_ON()
-  // string_'s data has the same address as parked_string_.
-  // This enforces MovableStringImpl.ToString().Characthers{8,16}() returning
-  // a different value after Park() / Unpark().
-  string_ = parked_string_;
-  parked_string_ = String();
-#endif
-  MovableStringTable::Instance().OnUnpark(this, string_.Impl());
 }
 
 MovableString::MovableString(scoped_refptr<StringImpl>&& impl) {
-  impl_ = MovableStringTable::Instance().Add(std::move(impl));
-  if (!impl_)
+  // Don't move small strings.
+  if (IsLargeEnough(impl.get())) {
+    impl_ = MovableStringTable::Instance().Add(std::move(impl));
+  } else {
     impl_ = base::MakeRefCounted<MovableStringImpl>(std::move(impl));
+  }
 }
 
 MovableString::~MovableString() = default;
@@ -127,45 +98,24 @@ MovableStringTable::~MovableStringTable() = default;
 
 scoped_refptr<MovableStringImpl> MovableStringTable::Add(
     scoped_refptr<StringImpl>&& string) {
-  StringImpl* impl = string.get();
-  if (!IsLargeEnough(impl))
-    return nullptr;
-
-  auto it = unparked_strings_.find(impl);
-  if (it != unparked_strings_.end()) {
+  StringImpl* raw_ptr = string.get();
+  auto it = table_.find(raw_ptr);
+  if (it != table_.end()) {
     return it->second;
   }
   auto new_movable_string =
       base::MakeRefCounted<MovableStringImpl>(std::move(string));
-  unparked_strings_.emplace(impl, new_movable_string.get());
+  table_.emplace(raw_ptr, new_movable_string.get());
   return new_movable_string;
 }
 
-void MovableStringTable::OnUnpark(MovableStringImpl* movable_impl,
-                                  StringImpl* impl) {
-  if (!IsLargeEnough(movable_impl))
+void MovableStringTable::Remove(StringImpl* string) {
+  if (!IsLargeEnough(string))
     return;
 
-  auto it = parked_strings_.find(movable_impl);
-  DCHECK(it != parked_strings_.end());
-  parked_strings_.erase(it);
-  unparked_strings_.emplace(impl, movable_impl);
-}
-
-void MovableStringTable::Remove(MovableStringImpl* movable_impl,
-                                StringImpl* string) {
-  if (!IsLargeEnough(movable_impl))
-    return;
-
-  if (movable_impl->is_parked()) {
-    auto it = parked_strings_.find(movable_impl);
-    DCHECK(it != parked_strings_.end());
-    parked_strings_.erase(it);
-  } else {
-    auto it = unparked_strings_.find(string);
-    DCHECK(it != unparked_strings_.end());
-    unparked_strings_.erase(it);
-  }
+  auto it = table_.find(string);
+  DCHECK(it != table_.end());
+  table_.erase(it);
 }
 
 void MovableStringTable::SetRendererBackgrounded(bool backgrounded) {
@@ -181,20 +131,9 @@ void MovableStringTable::MaybeParkAll() {
     return;
 
   size_t total_size = 0, count = 0;
-  for (auto it = unparked_strings_.begin(); it != unparked_strings_.end();) {
-    MovableStringImpl* str = it->second;
-    bool parked = str->Park();
-    if (parked) {
-      parked_strings_.insert(str);
-      it = unparked_strings_.erase(it);
-    } else {
-      ++it;
-      total_size += str->CharactersSizeInBytes();
-      count += 1;
-    }
-  }
-  for (const auto* str : parked_strings_) {
-    DCHECK(str);
+  for (auto& kv : table_) {
+    MovableStringImpl* str = kv.second;
+    str->Park();
     total_size += str->CharactersSizeInBytes();
     count += 1;
   }
@@ -202,11 +141,7 @@ void MovableStringTable::MaybeParkAll() {
   size_t total_size_kb = total_size / 1000;
   UMA_HISTOGRAM_COUNTS_100000("Memory.MovableStringsTotalSizeKb",
                               total_size_kb);
-  UMA_HISTOGRAM_COUNTS_1000("Memory.MovableStringsCount", count);
-}
-
-size_t MovableStringTable::Size() const {
-  return parked_strings_.size() + unparked_strings_.size();
+  UMA_HISTOGRAM_COUNTS_1000("Memory.MovableStringsCount", table_.size());
 }
 
 }  // namespace WTF
