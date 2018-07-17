3d637b7a40a4e830bc355150aad1f1dbb2ce52fb
Rohit Rao <rohitrao@chromium.org>
2018-07-17 15:09:09 +0000

    [ios] Sets a solid toolbar color when Reduce Transparency is enabled.
    We do not add a blur effect when Reduce Transparency is enabled,
    which was causing the incognito toolbar to display with a white
    background.  Instead, fall back to the pre-refresh incognito
    toolbar color.
    
    BUG=862860
    
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I55e4305527c19c90faa00672fc2b80321eca7b4c
    Reviewed-on: https://chromium-review.googlesource.com/1138344
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575642}
    Former-commit-id: 2165d2d0389a15c25df83d00ba45ac14058596ec

--- ios/chrome/browser/ui/toolbar/buttons/toolbar_configuration.mm
+++ ios/chrome/browser/ui/toolbar/buttons/toolbar_configuration.mm
@@ -39,8 +39,15 @@
 }
 
 - (UIColor*)blurBackgroundColor {
-  if (UIAccessibilityIsReduceTransparencyEnabled())
-    return [UIColor colorWithWhite:kBlurBackgroundGrayscaleComponent alpha:1];
+  if (UIAccessibilityIsReduceTransparencyEnabled()) {
+    switch (self.style) {
+      case NORMAL:
+        return
+            [UIColor colorWithWhite:kBlurBackgroundGrayscaleComponent alpha:1];
+      case INCOGNITO:
+        return UIColorFromRGB(kIncognitoToolbarBackgroundColor);
+    }
+  }
   return [UIColor colorWithWhite:kBlurBackgroundGrayscaleComponent
                            alpha:kBlurBackgroundAlpha];
 }
