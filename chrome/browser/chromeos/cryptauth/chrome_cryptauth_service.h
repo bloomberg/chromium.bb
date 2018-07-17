d25250c7d6eaa9cd5eb88f379d26f07a0c998a35
Jochen Eisinger <jochen@chromium.org>
2018-07-17 20:53:26 +0000

    Fully migrate ChromeCryptAuthService to IdentityManager
    After the latest round of API changes, it's a drop-in replacement.
    
    BUG=831073
    R=blundell@chromium.org,tengs@chromium.org
    
    Change-Id: I086340744a2df1763c29da879ca2f87465028fac
    Reviewed-on: https://chromium-review.googlesource.com/1126256
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575764}
    Former-commit-id: 6896f144b18afb1b1cb12957748bbdd9edb8a6d2

--- chrome/browser/chromeos/cryptauth/chrome_cryptauth_service.h
+++ chrome/browser/chromeos/cryptauth/chrome_cryptauth_service.h
@@ -14,8 +14,7 @@
 #include "components/cryptauth/proto/cryptauth_api.pb.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/prefs/pref_change_registrar.h"
-#include "components/signin/core/browser/signin_manager_base.h"
-#include "google_apis/gaia/oauth2_token_service.h"
+#include "services/identity/public/cpp/identity_manager.h"
 
 class Profile;
 
@@ -30,8 +29,7 @@ class ChromeCryptAuthService
     : public KeyedService,
       public cryptauth::CryptAuthService,
       public cryptauth::CryptAuthEnrollmentManager::Observer,
-      public OAuth2TokenService::Observer,
-      public SigninManagerBase::Observer {
+      public identity::IdentityManager::Observer {
  public:
   static std::unique_ptr<ChromeCryptAuthService> Create(Profile* profile);
   ~ChromeCryptAuthService() override;
@@ -60,17 +58,15 @@ class ChromeCryptAuthService
       std::unique_ptr<cryptauth::CryptAuthDeviceManager> device_manager,
       std::unique_ptr<cryptauth::CryptAuthEnrollmentManager> enrollment_manager,
       Profile* profile,
-      OAuth2TokenService* token_service,
-      SigninManagerBase* signin_manager);
+      identity::IdentityManager* identity_manager);
 
  private:
-  // OAuth2TokenService::Observer:
-  void OnRefreshTokenAvailable(const std::string& account_id) override;
+  // identity::IdentityManager::Observer:
+  void OnPrimaryAccountSet(const AccountInfo& primary_account_info) override;
+  void OnRefreshTokenUpdatedForAccount(const AccountInfo& account_info,
+                                       bool is_valid) override;
 
-  // SigninManagerBase::Observer:
-  void GoogleSigninSucceeded(const std::string& account_id,
-                             const std::string& username) override;
-
+  void OnAuthenticationStateChanged();
   void PerformEnrollmentAndDeviceSyncIfPossible();
   bool IsEnrollmentAllowedByPolicy();
   void OnPrefsChanged();
@@ -80,8 +76,7 @@ class ChromeCryptAuthService
   std::unique_ptr<cryptauth::CryptAuthEnrollmentManager> enrollment_manager_;
   std::unique_ptr<cryptauth::CryptAuthDeviceManager> device_manager_;
   Profile* profile_;
-  OAuth2TokenService* token_service_;
-  SigninManagerBase* signin_manager_;
+  identity::IdentityManager* identity_manager_;
   PrefChangeRegistrar registrar_;
 
   base::WeakPtrFactory<ChromeCryptAuthService> weak_ptr_factory_;
