54a9fbde60e197fc517826d1675ffeaa5ca4b056
Ramin Halavati <rhalavati@chromium.org>
2018-07-17 08:12:38 +0000

    Enable ephemeral Flash permissions by default.
    Switch EnableEphemeralFlashPermission switch's default value to true.
    
    Bug: 850062
    Change-Id: I42cc3adb918f9c0e95d5ad17f08577d6149b3999
    Reviewed-on: https://chromium-review.googlesource.com/1138238
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575577}
    Former-commit-id: cb2802bdf77878126f93612ca1ae463d34072aed

--- components/content_settings/core/common/features.cc
+++ components/content_settings/core/common/features.cc
@@ -12,7 +12,7 @@ namespace features {
 
 // Makes Flash plugin permissions persistent only through the current session.
 const base::Feature kEnableEphemeralFlashPermission{
-    "EnableEphemeralFlashPermission", base::FEATURE_DISABLED_BY_DEFAULT};
+    "EnableEphemeralFlashPermission", base::FEATURE_ENABLED_BY_DEFAULT};
 
 }  // namespace features
 }  // namespace content_settings
