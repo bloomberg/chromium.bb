295c90dac819f64888a846b0f9940e443c80731c
Owen Min <zmin@chromium.org>
2018-07-17 17:45:11 +0000

    Disable two flaky tests on ChromeOS
    Disable ShelfLayoutManagerTest.FlingUpOnShelfForFullscreenAppList
    Disable WindowSelectorTest.RoundedEdgeMaskVisibility
    
    Tbr: xiyuan, xdai
    Bug: 864467, 864465
    Change-Id: I77a0dbcddad5176575f0dc5991beb9eef8090167
    Reviewed-on: https://chromium-review.googlesource.com/1140683
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575707}
    Former-commit-id: fed7475d501d0622e3604313eb93867405cba55f

--- ash/shelf/shelf_layout_manager_unittest.cc
+++ ash/shelf/shelf_layout_manager_unittest.cc
@@ -1416,9 +1416,10 @@ TEST_F(ShelfLayoutManagerTest, GestureDrag) {
   }
 }
 
+// crbug.com/864467
 // If swiping up on shelf ends with fling event, the app list state should
 // depends on the fling velocity.
-TEST_F(ShelfLayoutManagerTest, FlingUpOnShelfForFullscreenAppList) {
+TEST_F(ShelfLayoutManagerTest, DISABLED_FlingUpOnShelfForFullscreenAppList) {
   Shelf* shelf = GetPrimaryShelf();
   EXPECT_EQ(SHELF_ALIGNMENT_BOTTOM, shelf->alignment());
   EXPECT_EQ(SHELF_AUTO_HIDE_BEHAVIOR_NEVER, shelf->auto_hide_behavior());
