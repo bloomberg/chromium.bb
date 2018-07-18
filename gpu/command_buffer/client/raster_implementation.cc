1a175c8b39ce3b817bea1e74ff29a1ae5b478932
Jonathan Backer <backer@chromium.org>
2018-07-18 15:53:47 +0000

    Reject long trace and category names
    This fixes a fuzzer OOM. 256 characters ought to be enough for anyone.
    
    Bug: 863775
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I0103a2ab95aa6d87375dbc055d6a0f517c726799
    Reviewed-on: https://chromium-review.googlesource.com/1140774
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Jonathan Backer <backer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576075}
    Former-commit-id: ff2ac0f9f7d01330dd10193103963b3f7de6df88

--- gpu/command_buffer/client/raster_implementation.cc
+++ gpu/command_buffer/client/raster_implementation.cc
@@ -1238,6 +1238,9 @@ void RasterImplementation::TraceBeginCHROMIUM(const char* category_name,
   GPU_CLIENT_SINGLE_THREAD_CHECK();
   GPU_CLIENT_LOG("[" << GetLogPrefix() << "] glTraceBeginCHROMIUM("
                      << category_name << ", " << trace_name << ")");
+  static constexpr size_t kMaxStrLen = 256;
+  DCHECK_LE(strlen(category_name), kMaxStrLen);
+  DCHECK_LE(strlen(trace_name), kMaxStrLen);
   SetBucketAsCString(kResultBucketId, category_name);
   SetBucketAsCString(kResultBucketId + 1, trace_name);
   helper_->TraceBeginCHROMIUM(kResultBucketId, kResultBucketId + 1);
