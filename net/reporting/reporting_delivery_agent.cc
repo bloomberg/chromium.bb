e909d2105b6616d53ace0775488e4ff21472753b
Douglas Creager <dcreager@chromium.org>
2018-07-18 01:25:40 +0000

    Reporting: Payload field should be `body`, not `report`
    Per https://w3c.github.io/reporting/#try-delivery, the field in a
    JavaScript report object that contains the body is called `body`, not
    `report`.
    
    Bug: 704259
    Change-Id: Iba3186a0104c2192ced5e4233d55e042e05908f3
    Reviewed-on: https://chromium-review.googlesource.com/1140193
    Reviewed-by: Misha Efimov <mef@chromium.org>
    Commit-Queue: Douglas Creager <dcreager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575894}
    Former-commit-id: 048081e45f7508eeaeea40a756ee083b08dd7f8f

--- net/reporting/reporting_delivery_agent.cc
+++ net/reporting/reporting_delivery_agent.cc
@@ -41,7 +41,7 @@ void SerializeReports(const std::vector<const ReportingReport*>& reports,
     report_value->SetInteger("age", (now - report->queued).InMilliseconds());
     report_value->SetString("type", report->type);
     report_value->SetString("url", report->url.spec());
-    report_value->SetKey("report", report->body->Clone());
+    report_value->SetKey("body", report->body->Clone());
 
     reports_value.Append(std::move(report_value));
   }
