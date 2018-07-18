5202a844ac74c1780697429ed20e9d3a7686172d
Jérôme Lebel <jlebel@chromium.org>
2018-07-18 09:53:32 +0000

    [iOS] Adding UnifiedConsentServiceFactory
    Adding UnifiedConsentServiceFactory and UnifiedConsentServiceClientImpl
    for iOS.
    Those classes will be used by the Sync and Google services settings later.
    
    Bug: 827072
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I6b6e43527bba4a26fb6e3eae486ba62a900db81e
    Reviewed-on: https://chromium-review.googlesource.com/1138084
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575991}
    Former-commit-id: 11ab1959bc61922ac8ac6cef5e1cc2331413c0d3

---
+++ ios/chrome/browser/unified_consent/unified_consent_service_factory.cc
@@ -0,0 +1,65 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/unified_consent/unified_consent_service_factory.h"
+
+#include "components/browser_sync/profile_sync_service.h"
+#include "components/keyed_service/ios/browser_state_dependency_manager.h"
+#include "components/sync/driver/sync_service.h"
+#include "components/unified_consent/unified_consent_service.h"
+#include "ios/chrome/browser/browser_state/chrome_browser_state.h"
+#include "ios/chrome/browser/signin/identity_manager_factory.h"
+#include "ios/chrome/browser/sync/profile_sync_service_factory.h"
+#include "ios/chrome/browser/unified_consent/unified_consent_service_client_impl.h"
+
+UnifiedConsentServiceFactory::UnifiedConsentServiceFactory()
+    : BrowserStateKeyedServiceFactory(
+          "UnifiedConsentService",
+          BrowserStateDependencyManager::GetInstance()) {
+  DependsOn(IdentityManagerFactory::GetInstance());
+  DependsOn(ProfileSyncServiceFactory::GetInstance());
+}
+
+UnifiedConsentServiceFactory::~UnifiedConsentServiceFactory() = default;
+
+// static
+unified_consent::UnifiedConsentService*
+UnifiedConsentServiceFactory::GetForBrowserState(
+    ios::ChromeBrowserState* browser_state) {
+  return static_cast<unified_consent::UnifiedConsentService*>(
+      GetInstance()->GetServiceForBrowserState(browser_state, true));
+}
+
+// static
+unified_consent::UnifiedConsentService*
+UnifiedConsentServiceFactory::GetForBrowserStateIfExists(
+    ios::ChromeBrowserState* browser_state) {
+  return static_cast<unified_consent::UnifiedConsentService*>(
+      GetInstance()->GetServiceForBrowserState(browser_state, false));
+}
+
+// static
+UnifiedConsentServiceFactory* UnifiedConsentServiceFactory::GetInstance() {
+  return base::Singleton<UnifiedConsentServiceFactory>::get();
+}
+
+std::unique_ptr<KeyedService>
+UnifiedConsentServiceFactory::BuildServiceInstanceFor(
+    web::BrowserState* context) const {
+  if (signin::GetUnifiedConsentFeatureState() ==
+      signin::UnifiedConsentFeatureState::kDisabled)
+    return nullptr;
+
+  ios::ChromeBrowserState* browser_state =
+      ios::ChromeBrowserState::FromBrowserState(context);
+  PrefService* pref_service = browser_state->GetPrefs();
+  std::unique_ptr<unified_consent::UnifiedConsentServiceClient> service_client =
+      std::make_unique<UnifiedConsentServiceClientImpl>(pref_service);
+  identity::IdentityManager* identity_manager =
+      IdentityManagerFactory::GetForBrowserState(browser_state);
+  syncer::SyncService* sync_service =
+      ProfileSyncServiceFactory::GetForBrowserState(browser_state);
+  return std::make_unique<unified_consent::UnifiedConsentService>(
+      std::move(service_client), pref_service, identity_manager, sync_service);
+}
