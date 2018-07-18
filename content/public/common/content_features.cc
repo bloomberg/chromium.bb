d8878e535c67638d3b46963169fb0762840b62b2
Robert Sesek <rsesek@chromium.org>
2018-07-18 16:30:48 +0000

    Enable the MacV2Sandbox feature by default.
    Changes in sandbox behavior can have couple-pixel adjustments to controls
    drawn with Cocoa, like scrollbars. This moves some affected layout test
    baselines from mac10.12 to mac10.10 as a result. The baseline search path
    for macOS 10.10 is 10.10->10.11->10.12->mac->generic. Enabling the V2
    sandbox means that mac10.11 and 10.12 now behave like "mac", and now
    only 10.10 has a differing result for these tests.
    
    Bug: 749839, 689306
    Change-Id: I9133a74c39fe995282daf950efe6d8b0fbcb2b4f
    Reviewed-on: https://chromium-review.googlesource.com/1132398
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576088}
    Former-commit-id: 6528ab86187022c24d36630831babcaf625c3f92

--- content/public/common/content_features.cc
+++ content/public/common/content_features.cc
@@ -646,7 +646,7 @@ const base::Feature kIOSurfaceCapturer{"IOSurfaceCapturer",
 // The V2 sandbox on MacOS removes the unsandboed warmup phase and sandboxes the
 // entire life of the process.
 const base::Feature kMacV2Sandbox{"MacV2Sandbox",
-                                  base::FEATURE_DISABLED_BY_DEFAULT};
+                                  base::FEATURE_ENABLED_BY_DEFAULT};
 #endif  // defined(OS_MACOSX)
 
 #if defined(OS_ANDROID) || defined(OS_CHROMEOS)
