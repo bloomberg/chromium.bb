7cde71788930326b79e091e59e1b0676a853357c
Fabio Tirelo <ftirelo@chromium.org>
2018-07-18 18:20:27 +0000

    [AF] Notify row on mouse events on child views
    The current implementation considers that a hover on the label or the
    icon corresponds to a mouse exit event for the row, leading to the
    current row being deselected. This CL fixes that by notifying the row
    view on mouse enter/exit on child events as well.
    
    Bug: 861996
    Change-Id: If187048b2893afb7c779f91010b7b225d0da49d7
    Reviewed-on: https://chromium-review.googlesource.com/1142035
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576142}
    Former-commit-id: 36fa5b4c741e453bb3bec89dde72f6a4beb1504c

--- chrome/browser/ui/views/autofill/autofill_popup_view_native_views.cc
+++ chrome/browser/ui/views/autofill/autofill_popup_view_native_views.cc
@@ -525,7 +525,9 @@ bool AutofillPopupRowView::OnMousePressed(const ui::MouseEvent& event) {
 AutofillPopupRowView::AutofillPopupRowView(
     AutofillPopupViewNativeViews* popup_view,
     int line_number)
-    : popup_view_(popup_view), line_number_(line_number) {}
+    : popup_view_(popup_view), line_number_(line_number) {
+  set_notify_enter_exit_on_child(true);
+}
 
 void AutofillPopupRowView::Init() {
   CreateContent();
