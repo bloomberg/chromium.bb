e76c84bfc53e74104bbae05fad13e6e18d1670c2
Daniel Rubery <drubery@chromium.org>
2018-07-18 02:16:39 +0000

    Add tabs to chrome://safe-browsing page
    Just a cosmetic change, because I was tired of scrolling through many
    sections.
    
    Render: https://screenshot.googleplex.com/mgoTZ4qfD0n
    
    Change-Id: Idaf6b98eb9835f3f9cf440f800985da154fefb10
    Reviewed-on: https://chromium-review.googlesource.com/1140528
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575909}
    Former-commit-id: 256f47b4cd4dfbdf03a9e9bd4fef88fac32f1517

--- components/safe_browsing/web_ui/resources/safe_browsing.js
+++ components/safe_browsing/web_ui/resources/safe_browsing.js
@@ -2,6 +2,8 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file. */
 
+cr.ui.decorate('tabbox', cr.ui.TabBox);
+
 cr.define('safe_browsing', function() {
   'use strict';
   /**
