2eae22fe66156eac262ef1be0f0d80af1d70f33a
Scott Haseley <shaseley@google.com>
2018-07-18 02:33:49 +0000

    [scheduler] Adding FrameTaskQueueController
    FrameTaskQueueController creates and manages the task queues used by
    FrameSchedulerImpl. It is responsible for tracking the mappings from
    QueueTraits to MainThreadTaskQueues.
    
    This CL adds FrameTaskQueueController and basic task queue management.
    Refactoring FrameSchedulerImpl to use this new class will be added in
    a follow-up CL.
    
    Design Doc:
    https://docs.google.com/document/d/1Jm4xLPqv013VfZswgmnoQ7-ghJK92Uf3hIT7WcNcFck/edit?usp=sharing
    
    Bug: 859963
    Change-Id: Ie9fefaaad83c30f06a13c427e38d54089fc362a4
    Reviewed-on: https://chromium-review.googlesource.com/1132691
    Commit-Queue: Scott Haseley <shaseley@google.com>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575912}
    Former-commit-id: f87335e795c6018344578acb40c1da2538d3223b

---
+++ third_party/blink/renderer/platform/scheduler/main_thread/frame_task_queue_controller.h
@@ -0,0 +1,128 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_SCHEDULER_MAIN_THREAD_FRAME_TASK_QUEUE_CONTROLLER_H_
+#define THIRD_PARTY_BLINK_RENDERER_PLATFORM_SCHEDULER_MAIN_THREAD_FRAME_TASK_QUEUE_CONTROLLER_H_
+
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include "base/callback.h"
+#include "base/macros.h"
+#include "base/memory/scoped_refptr.h"
+#include "third_party/blink/renderer/platform/platform_export.h"
+#include "third_party/blink/renderer/platform/scheduler/main_thread/main_thread_task_queue.h"
+#include "third_party/blink/renderer/platform/wtf/hash_map.h"
+
+namespace base {
+namespace sequence_manager {
+class TaskQueue;
+}  // namespace sequence_manager
+}  // namespace base
+
+namespace blink {
+namespace scheduler {
+
+class FrameSchedulerImpl;
+class FrameTaskQueueControllerTest;
+class MainThreadSchedulerImpl;
+
+// FrameTaskQueueController creates and manages and FrameSchedulerImpl's task
+// queues. It is in charge of maintaining mappings between TaskType and
+// QueueTraits and from QueueTraits to MainThreadTaskQueue, for accessing task
+// queues and their related voters, and for creating new task queues.
+class PLATFORM_EXPORT FrameTaskQueueController {
+ public:
+  using TaskQueueAndEnabledVoterPair =
+      std::pair<MainThreadTaskQueue*,
+                base::sequence_manager::TaskQueue::QueueEnabledVoter*>;
+
+  // Delegate receives callbacks when task queues are created to perform any
+  // additional task queue setup or tasks.
+  class Delegate {
+   public:
+    Delegate() = default;
+    virtual ~Delegate() = default;
+
+    virtual void OnTaskQueueCreated(
+        MainThreadTaskQueue*,
+        base::sequence_manager::TaskQueue::QueueEnabledVoter*) = 0;
+
+   private:
+    DISALLOW_COPY_AND_ASSIGN(Delegate);
+  };
+
+  FrameTaskQueueController(MainThreadSchedulerImpl*,
+                           FrameSchedulerImpl*,
+                           Delegate*);
+  ~FrameTaskQueueController();
+
+  // Get the list of all task queue and voter pairs.
+  const std::vector<TaskQueueAndEnabledVoterPair>& GetAllTaskQueuesAndVoters()
+      const;
+
+  // Gets the associated QueueEnabledVoter for the given task queue, or nullptr
+  // if one doesn't exist.
+  base::sequence_manager::TaskQueue::QueueEnabledVoter* GetQueueEnabledVoter(
+      const scoped_refptr<MainThreadTaskQueue>&);
+
+ private:
+  friend class FrameTaskQueueControllerTest;
+
+  scoped_refptr<MainThreadTaskQueue> NewLoadingTaskQueue();
+  scoped_refptr<MainThreadTaskQueue> NewLoadingControlTaskQueue();
+  scoped_refptr<MainThreadTaskQueue> NewNonLoadingTaskQueue(
+      MainThreadTaskQueue::QueueTraits);
+
+  void TaskQueueCreated(const scoped_refptr<MainThreadTaskQueue>&);
+
+  // Returns the unique non-loading task queue for the given QueueTraits, or
+  // nullptr if one has not yet been created.
+  scoped_refptr<MainThreadTaskQueue> GetNonLoadingTaskQueue(
+      MainThreadTaskQueue::QueueTraits) const;
+
+  // Map a set of QueueTraits to a QueueType.
+  // TODO(shaseley): Consider creating a new queue type kFrameNonLoading and use
+  // it instead of this for new queue types.
+  static MainThreadTaskQueue::QueueType QueueTypeFromQueueTraits(
+      MainThreadTaskQueue::QueueTraits);
+
+  MainThreadSchedulerImpl* const main_thread_scheduler_impl_;
+  FrameSchedulerImpl* const frame_scheduler_impl_;
+  Delegate* const delegate_;
+
+  // Keep track of loading queues separately. We keep these separate because
+  // loading and loading control task queues share the same queue traits, and
+  // those queue traits can be the same as non-loading queues. This prevents us
+  // from being able to the find right task queue by queue traits alone.
+  scoped_refptr<MainThreadTaskQueue> loading_task_queue_;
+  scoped_refptr<MainThreadTaskQueue> loading_control_task_queue_;
+
+  using NonLoadingTaskQueueMap =
+      WTF::HashMap<MainThreadTaskQueue::QueueTraitsKeyType,
+                   scoped_refptr<MainThreadTaskQueue>>;
+
+  // Map of all non-loading TaskQueues, indexed by QueueTraits.
+  NonLoadingTaskQueueMap non_loading_task_queues_;
+
+  using TaskQueueEnabledVoterMap = WTF::HashMap<
+      scoped_refptr<MainThreadTaskQueue>,
+      std::unique_ptr<base::sequence_manager::TaskQueue::QueueEnabledVoter>>;
+
+  // QueueEnabledVoters for the task queues we've created. Note: Some task
+  // queues do not have an associated voter.
+  TaskQueueEnabledVoterMap task_queue_enabled_voters_;
+
+  // The list of all task queue and voter pairs for all QueueTypeInternal queue
+  // types.
+  std::vector<TaskQueueAndEnabledVoterPair> all_task_queues_and_voters_;
+
+  DISALLOW_COPY_AND_ASSIGN(FrameTaskQueueController);
+};
+
+}  // namespace scheduler
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_SCHEDULER_MAIN_THREAD_FRAME_TASK_QUEUE_CONTROLLER_H_
