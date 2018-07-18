1ca3350f8f43fea25022828744cadb1aed1cf237
Dominick Ng <dominickn@chromium.org>
2018-07-18 07:24:14 +0000

    Create a skeleton App Service and App Registry.
    This CL creates the App Service as a in-process per-profile service, and
    attaches the App Registry to it.
    
    App Service is currently located in chrome/browser as many of its direct
    dependencies (which are not yet connected to the service) live there.
    Over time, we will refactor the dependencies such that:
    
    1. data which can be made independent of chrome/browser is moved within
       the App Service
    2. components which rely on that data access it asynchronously via the
       App Service
    
    This will allow us to move the App Service to chrome/services in the
    future.
    
    BUG=826982
    
    Change-Id: Ia7608fbb8c8b235d2b3e69ffa447963b32f2d5f0
    Reviewed-on: https://chromium-review.googlesource.com/1127513
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575968}
    Former-commit-id: 2d1e7d102ae50dbe5f2e36e76c3c1c519478891c

---
+++ chrome/browser/apps/foundation/app_service/app_service.cc
@@ -0,0 +1,64 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <utility>
+
+#include "base/bind.h"
+#include "base/memory/ref_counted.h"
+#include "chrome/browser/apps/foundation/app_service/app_registry/app_registry.h"
+#include "chrome/browser/apps/foundation/app_service/app_service.h"
+#include "components/prefs/pref_registry_simple.h"
+#include "components/prefs/pref_service.h"
+#include "services/preferences/public/cpp/pref_service_factory.h"
+#include "services/service_manager/public/cpp/service_context.h"
+
+namespace apps {
+
+AppService::AppService() = default;
+
+AppService::~AppService() = default;
+
+void AppService::OnStart() {
+  binder_registry_.AddInterface<apps::mojom::AppRegistry>(base::BindRepeating(
+      &AppService::BindAppRegistryRequest, base::Unretained(this)));
+
+  auto pref_registry = base::MakeRefCounted<PrefRegistrySimple>();
+  AppRegistry::RegisterPrefs(pref_registry.get());
+
+  prefs::ConnectToPrefService(
+      context()->connector(), std::move(pref_registry),
+      base::BindRepeating(&AppService::OnPrefServiceConnected,
+                          base::Unretained(this)));
+}
+
+void AppService::OnBindInterface(const service_manager::BindSourceInfo& source,
+                                 const std::string& interface_name,
+                                 mojo::ScopedMessagePipeHandle interface_pipe) {
+  binder_registry_.BindInterface(interface_name, std::move(interface_pipe));
+}
+
+void AppService::BindAppRegistryRequest(
+    apps::mojom::AppRegistryRequest request) {
+  if (!IsInitializationComplete()) {
+    pending_app_registry_requests_.push_back(std::move(request));
+    return;
+  }
+
+  app_registry_->BindRequest(std::move(request));
+}
+
+void AppService::OnPrefServiceConnected(
+    std::unique_ptr<::PrefService> pref_service) {
+  // Connecting to the pref service is required for this class to function.
+  DCHECK(pref_service);
+  app_registry_ = std::make_unique<AppRegistry>(std::move(pref_service));
+
+  DCHECK(IsInitializationComplete());
+  for (auto& request : pending_app_registry_requests_)
+    BindAppRegistryRequest(std::move(request));
+
+  pending_app_registry_requests_.clear();
+}
+
+}  // namespace apps
