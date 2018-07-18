6d97d563ca9aeabad0aa1319695bbd9b73def25f
sczs <sczs@chromium.org>
2018-07-18 19:52:25 +0000

    [ios] Adds custom accessibility actions to HistoryEntryItem
    - Migrates the code from LegacyHistoryCollectionVC and LegacyHistoryEntryItem into
    HistoryTableVC and HistoryEntryItem to add support for custom accessibility actions.
    
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I73d9a35a31822e98fb09d5828f7eee37211d06d5
    Reviewed-on: https://chromium-review.googlesource.com/1125328
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576174}
    Former-commit-id: 28ae7e13cf5b8ac2e51ccaed9c342934ff3a39e6

--- ios/chrome/browser/ui/history/history_entry_item_delegate.h
+++ ios/chrome/browser/ui/history/history_entry_item_delegate.h
@@ -5,24 +5,29 @@
 #ifndef IOS_CHROME_BROWSER_UI_HISTORY_HISTORY_ENTRY_ITEM_DELEGATE_H_
 #define IOS_CHROME_BROWSER_UI_HISTORY_HISTORY_ENTRY_ITEM_DELEGATE_H_
 
-@class LegacyHistoryEntryItem;
+@class ListItem;
+@protocol HistoryEntryItemInterface;
 
 // Delegate for HistoryEntryItem. Handles actions invoked as custom
 // accessibility actions.
 @protocol HistoryEntryItemDelegate
 // Called when custom accessibility action to delete the entry is invoked.
-- (void)historyEntryItemDidRequestDelete:(LegacyHistoryEntryItem*)item;
+- (void)historyEntryItemDidRequestDelete:
+    (ListItem<HistoryEntryItemInterface>*)item;
 // Called when custom accessibility action to open the entry in a new tab is
 // invoked.
-- (void)historyEntryItemDidRequestOpenInNewTab:(LegacyHistoryEntryItem*)item;
+- (void)historyEntryItemDidRequestOpenInNewTab:
+    (ListItem<HistoryEntryItemInterface>*)item;
 // Called when custom accessibility action to open the entry in a new incognito
 // tab is invoked.
 - (void)historyEntryItemDidRequestOpenInNewIncognitoTab:
-    (LegacyHistoryEntryItem*)item;
+    (ListItem<HistoryEntryItemInterface>*)item;
 // Called when custom accessibility action to copy the entry's URL is invoked.
-- (void)historyEntryItemDidRequestCopy:(LegacyHistoryEntryItem*)item;
+- (void)historyEntryItemDidRequestCopy:
+    (ListItem<HistoryEntryItemInterface>*)item;
 // Called when the view associated with the HistoryEntryItem should be updated.
-- (void)historyEntryItemShouldUpdateView:(LegacyHistoryEntryItem*)item;
+- (void)historyEntryItemShouldUpdateView:
+    (ListItem<HistoryEntryItemInterface>*)item;
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_HISTORY_HISTORY_ENTRY_ITEM_DELEGATE_H_
