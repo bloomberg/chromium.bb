a7155ce3e6caeb44d699a14dc8838ec59f58b2b9
Scott Chen <scottchen@chromium.org>
2018-07-18 04:06:10 +0000

    WebUI: update non-DICE and win10 welcome pages
    This CL changes the non-DICE welcome page to a custom element in order
    to reuse paper-button-style.
    
    This CL also updates the win10 welcome page's paper-button to use
    paper-button-style.
    
    Bug: 860274
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I8678e89fb99fda64c0e5f1f54b53f2edeb14e3a9
    Reviewed-on: https://chromium-review.googlesource.com/1138984
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Scott Chen <scottchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575934}
    Former-commit-id: d6e554116a126faf75deb318dddb77feedadc9e3

--- chrome/browser/resources/welcome/welcome.js
+++ chrome/browser/resources/welcome/welcome.js
@@ -2,36 +2,29 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-cr.define('welcome', function() {
-  'use strict';
+Polymer({
+  is: 'welcome-app',
 
-  function onAccept(e) {
+  /** @private */
+  onAccept_: function() {
     chrome.send('handleActivateSignIn');
-  }
+  },
 
-  function onDecline(e) {
+  /** @private */
+  onDecline_: function() {
     chrome.send('handleUserDecline');
-    e.preventDefault();
-  }
+  },
 
-  function initialize() {
-    $('accept-button').addEventListener('click', onAccept);
-    $('decline-button').addEventListener('click', onDecline);
-
-    var logo = document.querySelector('.logo-icon');
-    logo.onclick = function(e) {
-      logo.animate(
-          {
-            transform: ['none', 'rotate(-10turn)'],
-          },
-          /** @type {!KeyframeEffectOptions} */ ({
-            duration: 500,
-            easing: 'cubic-bezier(1, 0, 0, 1)',
-          }));
-    };
-  }
-
-  return {initialize: initialize};
+  /** @private */
+  onLogoTap_: function() {
+    this.$$('.logo-icon')
+        .animate(
+            {
+              transform: ['none', 'rotate(-10turn)'],
+            },
+            /** @type {!KeyframeEffectOptions} */ ({
+              duration: 500,
+              easing: 'cubic-bezier(1, 0, 0, 1)',
+            }));
+  },
 });
-
-document.addEventListener('DOMContentLoaded', welcome.initialize);
