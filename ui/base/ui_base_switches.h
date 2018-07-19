2347fd3acae261cca5e6f7bfada82919445ad266
Allen Bauer <kylixrd@chromium.org>
2018-07-19 00:39:20 +0000

    Added --animation-duration-scale to allow the setting of a duration scale factor for all animations using and derived from LinearAnimation.
    This allows for easier testing and tuning of the many animations throughout the code. By
    setting a scale factor, animations can be sped up or slowed down. This allows for them
    to be better observed to ensure the intermediate states for transitions, movements, and
    fades look good.
    
    Bug: None
    Change-Id: Ia2350e71bf6f3bac3fae26351418a95d2452f415
    Reviewed-on: https://chromium-review.googlesource.com/1138820
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576280}
    Former-commit-id: 466073135b549b44057e5372ba57c9f6f86f5589

--- ui/base/ui_base_switches.h
+++ ui/base/ui_base_switches.h
@@ -20,6 +20,7 @@ UI_BASE_EXPORT extern const char kDisableRemoteCoreAnimation[];
 UI_BASE_EXPORT extern const char kShowMacOverlayBorders[];
 #endif
 
+UI_BASE_EXPORT extern const char kAnimationDurationScale[];
 UI_BASE_EXPORT extern const char kDisableCompositedAntialiasing[];
 UI_BASE_EXPORT extern const char kDisableDwmComposition[];
 UI_BASE_EXPORT extern const char kDisableTouchAdjustment[];
