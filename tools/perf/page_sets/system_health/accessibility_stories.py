08e7ac886793de71d6e5920e182ad3de7a9da076
Annie Sullivan <sullivan@chromium.org>
2018-07-19 17:25:39 +0000

    Remove browse_accessibility:tools:gmail_compose from system_health benchmark as it has never passed consistently.
    Bug: 769809
    Change-Id: Ib5015379a59ea2a1d16618e94e2ff125a8341780
    Reviewed-on: https://chromium-review.googlesource.com/1143595
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Annie Sullivan <sullivan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576539}
    Former-commit-id: e4023189272a6fc29b336d15d70fe9049f6bcd1e

--- tools/perf/page_sets/system_health/accessibility_stories.py
+++ tools/perf/page_sets/system_health/accessibility_stories.py
@@ -2,8 +2,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from page_sets.login_helpers import google_login
-
 from page_sets.system_health import platforms
 from page_sets.system_health import story_tags
 from page_sets.system_health import system_health_story
@@ -50,48 +48,3 @@ class AccessibilityAmazonStory(_AccessibilityStory):
   NAME = 'load_accessibility:shopping:amazon'
   URL = 'https://www.amazon.com/gp/offer-listing/B01IENFJ14'
   TAGS = [story_tags.ACCESSIBILITY]
-
-
-class AccessibilityGmailComposeStory(_AccessibilityStory):
-  """Tests typing a lot of text into a Gmail compose window."""
-  NAME = 'browse_accessibility:tools:gmail_compose'
-  URL = 'https://mail.google.com/mail/#inbox?compose=new'
-  TAGS = [story_tags.ACCESSIBILITY, story_tags.KEYBOARD_INPUT]
-
-  def RunNavigateSteps(self, action_runner):
-    google_login.LoginGoogleAccount(action_runner, 'googletest')
-
-    # Navigating to https://mail.google.com immediately leads to an infinite
-    # redirection loop due to a bug in WPR (see
-    # https://github.com/chromium/web-page-replay/issues/70). We therefore first
-    # navigate to a sub-URL to set up the session and hit the resulting
-    # redirection loop. Afterwards, we can safely navigate to
-    # https://mail.google.com.
-    action_runner.tab.WaitForDocumentReadyStateToBeComplete()
-    action_runner.Navigate(
-        'https://mail.google.com/mail/mu/mp/872/trigger_redirection_loop')
-    action_runner.tab.WaitForDocumentReadyStateToBeComplete()
-
-    super(AccessibilityGmailComposeStory, self).RunNavigateSteps(
-        action_runner)
-
-    action_runner.WaitForJavaScriptCondition(
-        'document.getElementById("loading").style.display === "none"')
-
-    # Tab from the To field to the message body.
-    action_runner.WaitForElement(selector='#\\:gr')
-    action_runner.PressKey('Tab')
-    action_runner.PressKey('Tab')
-
-    # EnterText doesn't handle newlines for some reason.
-    long_text = LONG_TEXT.replace('\n', ' ')
-
-    # Enter some text
-    action_runner.EnterText(long_text, character_delay_ms=1)
-
-    # Move up a couple of lines and then enter it again, this causes
-    # a huge amount of wrapping and re-layout
-    action_runner.PressKey('Home')
-    action_runner.PressKey('ArrowUp')
-    action_runner.PressKey('ArrowUp')
-    action_runner.EnterText(long_text, character_delay_ms=1)
