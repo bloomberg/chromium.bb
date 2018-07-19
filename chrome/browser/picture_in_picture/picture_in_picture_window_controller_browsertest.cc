5eca63828243b0db94a0d0316de27797c00b2492
Christian Dullweber <dullweber@chromium.org>
2018-07-19 12:26:07 +0000

    Revert "[Picture-in-Picture] Update window playback when video src changes."
    This reverts commit af54b8b8eaf7d173d0a3234f75ce1f1bb862dd08 [formerly 93488940ae5770af18ca5405395a206b426756a6].
    
    Reason for revert: PictureInPictureWindowControllerBrowserTest.UpdateVideoSrcKeepsPictureInPictureWindowOpened is very flaky: https://crbug.com/865410
    
    Original change's description:
    > [Picture-in-Picture] Update window playback when video src changes.
    > 
    > This CL make sure the Picture-in-Picture window playback state is
    > reflected when video src changes. It is a follow-up to
    > https://chromium-review.googlesource.com/1089331
    > 
    > Bug: 726619
    > Change-Id: Ic756c03aa023d3e4e229c7495686ea8f322503e3
    > Reviewed-on: https://chromium-review.googlesource.com/1133168
    > Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    > Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#576428}
    
    TBR=dalecurtis@chromium.org,wolenetz@chromium.org,beaufort.francois@gmail.com,mlamouri@chromium.org,jochen@chromium.org
    
    Change-Id: I2b35b9ef8c119915b1c07f9f12321439c3242ab2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 726619, 865410
    Reviewed-on: https://chromium-review.googlesource.com/1143324
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576454}
    Former-commit-id: 94b0138d2ec5f88119917d183ac5039302d88460

--- chrome/browser/picture_in_picture/picture_in_picture_window_controller_browsertest.cc
+++ chrome/browser/picture_in_picture/picture_in_picture_window_controller_browsertest.cc
@@ -474,49 +474,24 @@ IN_PROC_BROWSER_TEST_F(PictureInPictureWindowControllerBrowserTest,
 // keep Picture-in-Picture window opened.
 IN_PROC_BROWSER_TEST_F(PictureInPictureWindowControllerBrowserTest,
                        ResetVideoSrcKeepsPictureInPictureWindowOpened) {
-  LoadTabAndEnterPictureInPicture(browser());
-
-  EXPECT_TRUE(window_controller()->GetWindowForTesting()->IsVisible());
-  EXPECT_TRUE(
-      window_controller()->GetWindowForTesting()->GetVideoLayer()->visible());
-
-  content::WebContents* active_web_contents =
-      browser()->tab_strip_model()->GetActiveWebContents();
-  EXPECT_TRUE(content::ExecuteScript(active_web_contents, "video.src = null;"));
-
-  bool in_picture_in_picture = false;
-  EXPECT_TRUE(ExecuteScriptAndExtractBool(
-      active_web_contents, "isInPictureInPicture();", &in_picture_in_picture));
-  EXPECT_TRUE(in_picture_in_picture);
-
-  EXPECT_TRUE(window_controller()->GetWindowForTesting()->IsVisible());
-  EXPECT_FALSE(
-      window_controller()->GetWindowForTesting()->GetVideoLayer()->visible());
-}
-
-#if defined(OS_LINUX)
-#define MAYBE_UpdateVideoSrcKeepsPictureInPictureWindowOpened \
-    DISABLED_UpdateVideoSrcKeepsPictureInPictureWindowOpened
-#else
-#define MAYBE_UpdateVideoSrcKeepsPictureInPictureWindowOpened \
-    UpdateVideoSrcKeepsPictureInPictureWindowOpened
-#endif
-
-// Tests that updating video src when video is in Picture-in-Picture session
-// keep Picture-in-Picture window opened.
-IN_PROC_BROWSER_TEST_F(PictureInPictureWindowControllerBrowserTest,
-                       MAYBE_UpdateVideoSrcKeepsPictureInPictureWindowOpened) {
-  LoadTabAndEnterPictureInPicture(browser());
-
-  EXPECT_TRUE(window_controller()->GetWindowForTesting()->IsVisible());
-  EXPECT_TRUE(
-      window_controller()->GetWindowForTesting()->GetVideoLayer()->visible());
+  GURL test_page_url = ui_test_utils::GetTestUrl(
+      base::FilePath(base::FilePath::kCurrentDirectory),
+      base::FilePath(
+          FILE_PATH_LITERAL("media/picture-in-picture/window-size.html")));
+  ui_test_utils::NavigateToURL(browser(), test_page_url);
 
   content::WebContents* active_web_contents =
       browser()->tab_strip_model()->GetActiveWebContents();
+  ASSERT_TRUE(active_web_contents);
+
+  SetUpWindowController(active_web_contents);
+  ASSERT_TRUE(window_controller());
+
+  EXPECT_TRUE(content::ExecuteScript(active_web_contents, "video.play();"));
+
   bool result = false;
   ASSERT_TRUE(content::ExecuteScriptAndExtractBool(
-      active_web_contents, "changeVideoSrc();", &result));
+      active_web_contents, "enterPictureInPicture();", &result));
   EXPECT_TRUE(result);
 
   bool in_picture_in_picture = false;
@@ -525,8 +500,15 @@ IN_PROC_BROWSER_TEST_F(PictureInPictureWindowControllerBrowserTest,
   EXPECT_TRUE(in_picture_in_picture);
 
   EXPECT_TRUE(window_controller()->GetWindowForTesting()->IsVisible());
-  EXPECT_TRUE(
-      window_controller()->GetWindowForTesting()->GetVideoLayer()->visible());
+
+  EXPECT_TRUE(content::ExecuteScript(active_web_contents, "video.src = null;"));
+
+  in_picture_in_picture = false;
+  EXPECT_TRUE(ExecuteScriptAndExtractBool(
+      active_web_contents, "isInPictureInPicture();", &in_picture_in_picture));
+  EXPECT_TRUE(in_picture_in_picture);
+
+  EXPECT_TRUE(window_controller()->GetWindowForTesting()->IsVisible());
 }
 
 // Tests that we can enter Picture-in-Picture when a video is not preloaded,
