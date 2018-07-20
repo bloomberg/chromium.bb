02640fcaab6f3ca52ce641841ba1417544ea4d25
Aidan Wolter <awolter@chromium.org>
2018-07-20 15:26:11 +0000

    Fix CastAudioManagerAlsaTest
    A CL was landed upstream that broke this unittest, which was not caught
    until it was tested downstream.
    
    BUG=None
    TEST=Build cast_media_unittests
    
    Change-Id: Ieccb420af2d5945422b1b218a7fb8380d2e46d99
    Reviewed-on: https://chromium-review.googlesource.com/1144782
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Commit-Queue: Aidan Wolter <awolter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576870}
    Former-commit-id: c9cf9319882295a2cdf6a68537077e6d6b282f80

--- chromecast/media/audio/cast_audio_manager_alsa_unittest.cc
+++ chromecast/media/audio/cast_audio_manager_alsa_unittest.cc
@@ -7,6 +7,7 @@
 #include <memory>
 #include <utility>
 
+#include "base/bind.h"
 #include "base/test/test_message_loop.h"
 #include "chromecast/media/cma/test/mock_cma_backend_factory.h"
 #include "media/audio/fake_audio_log_factory.h"
@@ -33,16 +34,20 @@ class CastAudioManagerAlsaTest : public testing::Test {
   CastAudioManagerAlsaTest() : media_thread_("CastMediaThread") {
     CHECK(media_thread_.Start());
 
+    backend_factory_ = std::make_unique<MockCmaBackendFactory>();
     audio_manager_ = std::make_unique<CastAudioManagerAlsa>(
         std::make_unique<::media::TestAudioThread>(), &audio_log_factory_,
-        std::make_unique<MockCmaBackendFactory>(), media_thread_.task_runner(),
-        false);
+        base::BindRepeating(&CastAudioManagerAlsaTest::GetCmaBackendFactory,
+                            base::Unretained(this)),
+        media_thread_.task_runner(), false);
   }
 
   ~CastAudioManagerAlsaTest() override { audio_manager_->Shutdown(); }
+  CmaBackendFactory* GetCmaBackendFactory() { return backend_factory_.get(); }
 
  protected:
   base::TestMessageLoop message_loop_;
+  std::unique_ptr<MockCmaBackendFactory> backend_factory_;
   base::Thread media_thread_;
   ::media::FakeAudioLogFactory audio_log_factory_;
   std::unique_ptr<CastAudioManagerAlsa> audio_manager_;
