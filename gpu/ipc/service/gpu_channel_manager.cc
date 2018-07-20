3b9920aeb9681e415edc325ffa6b061d0732fcff
Khushal <khushalsagar@chromium.org>
2018-07-20 07:39:24 +0000

    Reland part-3 gpu: Add disk caching for skia generated shaders in OOP-R.
    This just re-enables the cache use that was disabled in the previous
    patch.
    
    TBR=piman@chromium.org
    
    Bug: 865138
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ib61a148ddefea7e7491bea8f59ea363af990d811
    Reviewed-on: https://chromium-review.googlesource.com/1144788
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#576806}
    Former-commit-id: 7386a688ddb2a1fb27ce548e4f10024fbb716882

--- gpu/ipc/service/gpu_channel_manager.cc
+++ gpu/ipc/service/gpu_channel_manager.cc
@@ -84,16 +84,14 @@ GpuChannelManager::GpuChannelManager(
   DCHECK(io_task_runner);
   DCHECK(scheduler);
 
-#if 0
   const bool enable_raster_transport =
       gpu_feature_info_.status_values[GPU_FEATURE_TYPE_OOP_RASTERIZATION] ==
       gpu::kGpuFeatureStatusEnabled;
-  bool disable_disk_cache =
+  const bool disable_disk_cache =
       gpu_preferences_.disable_gpu_shader_disk_cache ||
       gpu_driver_bug_workarounds_.disable_program_disk_cache;
   if (enable_raster_transport && !disable_disk_cache)
     gr_shader_cache_.emplace(gpu_preferences.gpu_program_cache_size, this);
-#endif
 }
 
 GpuChannelManager::~GpuChannelManager() {
