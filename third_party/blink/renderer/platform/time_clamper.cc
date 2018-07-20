6ab83ad8c3bb23bef0f6be72951320c016663a3f
Kenneth Russell <kbr@chromium.org>
2018-07-20 22:03:35 +0000

    Handle negative times in TimeClamper::ClampTimeResolution.
    Patch from dansnyder@.
    
    Negative times are being seen on certain platforms (the Android P
    preview and some Cast devices, in particular). Handle them gracefully
    rather than asserting.
    
    Bug: 856858
    Tbr: jochen@chromium.org
    Change-Id: Ib05cb8534bec9e6a95df2f989a9d65fedfeece0b
    Reviewed-on: https://chromium-review.googlesource.com/1142238
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577005}
    Former-commit-id: 0ebb714c18a2ec435ac6ed9532435330dcbef44d

--- third_party/blink/renderer/platform/time_clamper.cc
+++ third_party/blink/renderer/platform/time_clamper.cc
@@ -15,13 +15,19 @@ namespace blink {
 TimeClamper::TimeClamper() : secret_(base::RandUint64()) {}
 
 double TimeClamper::ClampTimeResolution(double time_seconds) const {
-  DCHECK_GE(time_seconds, 0);
+  bool was_negative = false;
+  if (time_seconds < 0) {
+    was_negative = true;
+    time_seconds = -time_seconds;
+  }
   double interval = floor(time_seconds / kResolutionSeconds);
   double clamped_time = interval * kResolutionSeconds;
   double tick_threshold = ThresholdFor(clamped_time);
 
   if (time_seconds >= tick_threshold)
-    return (interval + 1) * kResolutionSeconds;
+    clamped_time = (interval + 1) * kResolutionSeconds;
+  if (was_negative)
+    clamped_time = -clamped_time;
   return clamped_time;
 }
 
