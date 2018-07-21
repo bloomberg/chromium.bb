00c047cbdd18928fa51ebf74d737853bc53241bc
Lei Zhang <thestig@chromium.org>
2018-07-21 00:39:17 +0000

    Validate data in PrintPreviewDataStore.
    It should be storing PDF data and not MSKP. Adjust tests to pass.
    
    BUG=865793
    
    Change-Id: Ib28e9c0a1579dd05d9d500bd7473103828c30079
    Reviewed-on: https://chromium-review.googlesource.com/1144781
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577047}
    Former-commit-id: 7996d9622f4bd1a72bddc58b24fdab366b1976e4

--- chrome/browser/ui/webui/print_preview/print_preview_ui_unittest.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_ui_unittest.cc
@@ -31,7 +31,7 @@ namespace {
 
 scoped_refptr<base::RefCountedBytes> CreateTestData() {
   const unsigned char blob1[] =
-      "12346102356120394751634516591348710478123649165419234519234512349134";
+      "%PDF-1.4123461023561203947516345165913487104781236491654192345192345";
   std::vector<unsigned char> preview_data(blob1, blob1 + sizeof(blob1));
   return base::MakeRefCounted<base::RefCountedBytes>(preview_data);
 }
@@ -104,11 +104,6 @@ TEST_F(PrintPreviewUIUnitTest, PrintPreviewData) {
       &data);
   EXPECT_EQ(dummy_data->size(), data->size());
   EXPECT_EQ(dummy_data.get(), data.get());
-
-  // This should not cause any memory leaks.
-  dummy_data = base::MakeRefCounted<base::RefCountedBytes>();
-  preview_ui->SetPrintPreviewDataForIndex(printing::FIRST_PAGE_INDEX,
-                                          dummy_data.get());
 
   // Clear the preview data.
   preview_ui->ClearAllPreviewData();
