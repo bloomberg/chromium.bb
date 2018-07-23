cccf36b3e697a455586bba81ad766e1525f7891a
Wojciech Dzierżanowski <wdzierzanowski@opera.com>
2018-07-23 08:39:16 +0000

    Reset timestamp validator when config change is complete
    When DecoderStream<DemuxerStream::AUDIO> encounters a config change it needs to
    do two things, among others: flush the decoder and reset the audio timestamp
    validator. It's important that it does the latter only after processing any
    decoder output produced during the flushing.
    
    This CL moves the responsibility of resetting the validator from
    DecoderStream to DecoderStreamTraits<DemuxerStream::AUDIO>. It also adds
    a test where the decoder's behavior of releasing internally buffered
    samples upon flushing is mocked.
    
    Bug: 865926
    Test: New test AudioBufferStreamTest.FlushOnConfigChange doesn't fail assertions. No regressions in media_unittests.
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I8cbb01df3792ca9de5db8ab637285d1e64188b39
    Reviewed-on: https://chromium-review.googlesource.com/1145188
    Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Wojciech Dzierżanowski <wdzierzanowski@opera.com>
    Cr-Commit-Position: refs/heads/master@{#577123}
    Former-commit-id: e318f812b99e7e795be931df6404d35957635aa9

--- media/filters/decoder_stream_traits.cc
+++ media/filters/decoder_stream_traits.cc
@@ -66,6 +66,11 @@ void DecoderStreamTraits<DemuxerStream::AUDIO>::InitializeDecoder(
     const DecoderType::WaitingForDecryptionKeyCB&
         waiting_for_decryption_key_cb) {
   DCHECK(config.IsValidConfig());
+
+  if (config_.IsValidConfig() && !config_.Matches(config))
+    OnConfigChanged(config);
+  config_ = config;
+
   stats_.audio_decoder_name = decoder->GetDisplayName();
   decoder->Initialize(config, cdm_context, init_cb, output_cb,
                       waiting_for_decryption_key_cb);
