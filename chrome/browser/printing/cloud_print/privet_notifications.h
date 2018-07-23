791845e001119f921a9ffcf049202046c09b78eb
Lei Zhang <thestig@chromium.org>
2018-07-23 19:28:59 +0000

    Always stop PrivetTrafficDetector.
    Currently, there are some code paths in PrivetNotificationService where
    PrivetTrafficDetector is not stopped. This causes PrivetTrafficDetector
    to forget to unregister itself from NetworkConnectionTracker.
    
    BUG=866199
    
    Change-Id: I75721603889a6886b012d439bd6e8f2266083ee1
    Reviewed-on: https://chromium-review.googlesource.com/1145888
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577220}
    Former-commit-id: 957f866abf50ff69a919bc2db001e326e142cf38

--- chrome/browser/printing/cloud_print/privet_notifications.h
+++ chrome/browser/printing/cloud_print/privet_notifications.h
@@ -7,6 +7,7 @@
 
 #include <map>
 #include <memory>
+#include <set>
 #include <string>
 
 #include "chrome/browser/printing/cloud_print/privet_device_lister.h"
@@ -36,7 +37,7 @@ struct DeviceDescription;
 
 #if BUILDFLAG(ENABLE_MDNS)
 class PrivetTrafficDetector;
-#endif  // ENABLE_MDNS
+#endif
 
 // Contains logic related to notifications not tied actually displaying them.
 class PrivetNotificationsListener  {
@@ -140,7 +141,7 @@ class PrivetNotificationService
 
 #if BUILDFLAG(ENABLE_MDNS)
   scoped_refptr<PrivetTrafficDetector> traffic_detector_;
-#endif  // ENABLE_MDNS
+#endif
 };
 
 class PrivetNotificationDelegate : public message_center::NotificationDelegate {
