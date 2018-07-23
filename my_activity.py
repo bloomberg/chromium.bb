356c288a106fc5724223fd807d8afd7ea4b448c2
jdoerrie <jdoerrie@chromium.org>
2018-07-23 10:02:02 +0000

    [my_activity] Fix Filtering of Gerrit Reviews
    This change fixes the filtering of Gerrit Reviews. Prior to this change
    authored Changes were duplicated in the list of Reviews.
    
    Bug: NONE
    Change-Id: If0dceb3dc24ed12f039a9549ec22b9b78c3d80dd
    Reviewed-on: https://chromium-review.googlesource.com/1145381
    Reviewed-by: Sergiy Byelozyorov <sergiyb@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>

--- my_activity.py
+++ my_activity.py
@@ -688,7 +688,7 @@ class MyActivity(object):
           gerrit_instances)
       rietveld_reviews = itertools.chain.from_iterable(rietveld_reviews.get())
       gerrit_reviews = itertools.chain.from_iterable(gerrit_reviews.get())
-      gerrit_reviews = [r for r in gerrit_reviews if r['owner'] != self.user]
+      gerrit_reviews = [r for r in gerrit_reviews if not self.match(r['owner'])]
       self.reviews = list(rietveld_reviews) + list(gerrit_reviews)
 
   def print_reviews(self):
