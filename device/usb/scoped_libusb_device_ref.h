18ec31342ece8d5a345f796e52b91d53966d5cd8
Reilly Grant <reillyg@chromium.org>
2018-07-23 23:40:59 +0000

    Attach UsbContext to libusb_device(_handle) pointers
    This change modifies ScopedLibusbDeviceRef and adds a new class
    ScopedLibusbDeviceHandle. This now explicitly own a reference to the
    UsbContext object (which reference counts a libusb_context) in addition
    to the libusb_device or libusb_device_handle they are wrapping.
    
    This resolves potential use-after-frees possible when posting tasks
    with a ScopedLibusbDeviceRef since the UsbService the task is being
    posted to could be destroyed before the task is executed. The
    libusb_device would then be released after its libusb_context has been
    destroyed.
    
    This is based on https://crrev.com/c/1131949 after I realized there
    where additional issues that needed to be addressed.
    
    Bug: 838947
    Change-Id: Idee02828bf615bd477033e585fffe03cf4d20595
    Reviewed-on: https://chromium-review.googlesource.com/1145910
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577329}
    Former-commit-id: daa348fab9cae0919ebe9ded50782c6f7aa38be5

--- device/usb/scoped_libusb_device_ref.h
+++ device/usb/scoped_libusb_device_ref.h
@@ -5,19 +5,40 @@
 #ifndef DEVICE_USB_SCOPED_LIBUSB_DEVICE_REF_H_
 #define DEVICE_USB_SCOPED_LIBUSB_DEVICE_REF_H_
 
-#include "base/scoped_generic.h"
-#include "third_party/libusb/src/libusb/libusb.h"
+#include "base/macros.h"
+#include "base/memory/scoped_refptr.h"
+
+struct libusb_device;
 
 namespace device {
 
-struct LibusbDeviceRefTraits {
-  static libusb_device* InvalidValue() { return nullptr; }
+class UsbContext;
 
-  static void Free(libusb_device* device) { libusb_unref_device(device); }
+// This class owns a reference to a libusb_device as well as a reference to
+// the libusb_context. The libusb_context must outlive any libusb_device
+// instances created from it.
+class ScopedLibusbDeviceRef {
+ public:
+  ScopedLibusbDeviceRef(libusb_device* device,
+                        scoped_refptr<UsbContext> context);
+  ScopedLibusbDeviceRef(ScopedLibusbDeviceRef&& other);
+  ~ScopedLibusbDeviceRef();
+
+  libusb_device* get() const { return device_; }
+
+  scoped_refptr<UsbContext> GetContext() const { return context_; }
+
+  void Reset();
+  bool IsValid() const;
+
+ private:
+  libusb_device* device_;
+  scoped_refptr<UsbContext> context_;
+
+  DISALLOW_COPY_AND_ASSIGN(ScopedLibusbDeviceRef);
 };
 
-using ScopedLibusbDeviceRef =
-    base::ScopedGeneric<libusb_device*, LibusbDeviceRefTraits>;
+bool operator==(const ScopedLibusbDeviceRef& ref, libusb_device* device);
 
 }  // namespace device
 
