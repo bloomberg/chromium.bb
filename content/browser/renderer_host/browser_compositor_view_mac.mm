f24b8c14363ff9db31e6c1388235b6fa854ee068
Christopher Cameron <ccameron@chromium.org>
2018-07-23 07:20:25 +0000

    Fix typo causing warped mouse input
    This function was computing the right transform, and then applying it
    to a temporary variable instead of the input.
    
    TBR=fsamuel
    
    Bug: 860344
    Change-Id: I917b1ea2610b6edaaa43eb2d7abc6febbb44f0fc
    Reviewed-on: https://chromium-review.googlesource.com/1146480
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577118}
    Former-commit-id: ffd2547f1bd23b0b5788e7a07fa65e5d2a15cdce

--- content/browser/renderer_host/browser_compositor_view_mac.mm
+++ content/browser/renderer_host/browser_compositor_view_mac.mm
@@ -476,8 +476,7 @@ void BrowserCompositorMac::TransformPointToRootSurface(gfx::PointF* point) {
   gfx::Transform transform_to_root;
   if (parent_ui_layer_)
     parent_ui_layer_->GetTargetTransformRelativeTo(nullptr, &transform_to_root);
-  gfx::Point origin_in_root;
-  transform_to_root.TransformPoint(&origin_in_root);
+  transform_to_root.TransformPoint(point);
 }
 
 void BrowserCompositorMac::LayerDestroyed(ui::Layer* layer) {
