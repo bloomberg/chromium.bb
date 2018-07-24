1996bc377daa8852b26a5e9385a7a22833a89b7f
Bailey Berro <baileyberro@chromium.org>
2018-07-24 23:01:03 +0000

    Introduce cr-searchable-drop-down polymer component.
    This change takes the drop-down-search-box from the "Add Cups Printer"
    dialog and makes it a cr-element so that it can be used elsewhere.
    
    - Updates cups_add_printers_dialog to use cr-searchable-drop-down
    
    Bug: 866146
    Test: browser_tests
    Change-Id: Ib0d953c3dbb1f51dad94a73451eb924d12d1b39e
    Reviewed-on: https://chromium-review.googlesource.com/1145774
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577722}
    Former-commit-id: 6072bcf5e577ca68eae15767dc43f9e65decb383

--- chrome/browser/resources/settings/printing_page/cups_add_printer_dialog_util.js
+++ chrome/browser/resources/settings/printing_page/cups_add_printer_dialog_util.js
@@ -29,62 +29,6 @@ Polymer({
   },
 });
 
-/** 'drop-down-search-box' implements a search box with suggestions dropdown. */
-Polymer({
-  is: 'drop-down-search-box',
-
-  properties: {
-    /** @type {!Array<string>} */
-    items: {
-      type: Array,
-    },
-
-    /** @type {string} */
-    value: {
-      type: String,
-      notify: true,
-    },
-
-    /** @private {string} */
-    searchTerm_: String,
-
-    label: String,
-  },
-
-  /**
-   * @param {!Event} event
-   * @private
-   */
-  onClick_: function(event) {
-    this.$$('iron-dropdown').open();
-  },
-
-  /** @private */
-  onInputValueChanged_: function() {
-    this.searchTerm_ = this.$.search.value;
-  },
-
-  /**
-   * @param {{model:Object}} event
-   * @private
-   */
-  onSelect_: function(event) {
-    this.$$('iron-dropdown').close();
-
-    this.value = event.model.item;
-    this.searchTerm_ = '';
-  },
-
-  /** @private */
-  filterItems_: function(searchTerm) {
-    if (!searchTerm)
-      return null;
-    return function(item) {
-      return item.toLowerCase().includes(searchTerm.toLowerCase());
-    };
-  },
-});
-
 /** 'add-printer-dialog' is the template of the Add Printer dialog. */
 Polymer({
   is: 'add-printer-dialog',
