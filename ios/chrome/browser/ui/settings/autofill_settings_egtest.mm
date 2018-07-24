006dc6b4e17d6ad8b78991b5cfff14d4315c95b5
Moe Ahmadi <mahmadi@chromium.org>
2018-07-24 16:01:24 +0000

    Revert "[ios] Disable failing autofill tests."
    This reverts commit c06ce057b502979f45a03b15e60153b515438833 [formerly 5e8f9218c0f90c57df0c6ba8cbfe6d719ea79e58].
    
    Reason for revert:
    These tests failed due to a change to the Autofill predictions server in which it started sending UNKNOWN_TYPE for unknown fields (which signals Autofill not to fill and save form data) rather than NO_SERVER_DATA ( which signals Autofill to fall back to the local heuristics). Since these tests rely on saving a profile in order to populate the settings page and that the test form wasn't known to the predictions server, they started to fail. crbug.com/866922 is filed to eliminate that dependency.
    
    Original change's description:
    > [ios] Disable failing autofill tests.
    > 
    > TBR: sczs@chromium.org
    > Bug: 866730
    > Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    > Change-Id: Ibc3828d336d5f6247f2a656d747e35847f86fd70
    > Reviewed-on: https://chromium-review.googlesource.com/1147701
    > Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    > Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    > Commit-Queue: Justin Cohen <justincohen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#577384}
    
    TBR=rohitrao@chromium.org,justincohen@chromium.org,kkhorimoto@chromium.org,sczs@chromium.org
    
    Change-Id: Iba5ecab647a979cebf6d4b32894c665b356037b5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 866730
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Reviewed-on: https://chromium-review.googlesource.com/1148502
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577563}
    Former-commit-id: d1523a7e02bc6c56ee243fe48257377ad03c898a

--- ios/chrome/browser/ui/settings/autofill_settings_egtest.mm
+++ ios/chrome/browser/ui/settings/autofill_settings_egtest.mm
@@ -127,9 +127,6 @@ NSString* GetTextFieldForID(int categoryId) {
 
 // Test that submitting a form ensures saving the data as an autofill profile.
 - (void)testAutofillProfileSaving {
-  // TODO(crbug.com/866730): Re-enable when fixed
-  EARL_GREY_TEST_DISABLED(@"Test broken.");
-
   [self loadAndSubmitTheForm];
   [self openEditAddress:@"George Washington, 1600 Pennsylvania Ave NW"];
 
@@ -150,9 +147,6 @@ NSString* GetTextFieldForID(int categoryId) {
 // Test that editing country names is followed by validating the value and
 // replacing it with a canonical one.
 - (void)testAutofillProfileEditing {
-  // TODO(crbug.com/866730): Re-enable when fixed
-  EARL_GREY_TEST_DISABLED(@"Test broken.");
-
   [self loadAndSubmitTheForm];
   [self openEditAddress:@"George Washington, 1600 Pennsylvania Ave NW"];
 
@@ -187,9 +181,6 @@ NSString* GetTextFieldForID(int categoryId) {
 
 // Test that the page for viewing autofill profile details is accessible.
 - (void)testAccessibilityOnAutofillProfileViewPage {
-  // TODO(crbug.com/866730): Re-enable when fixed
-  EARL_GREY_TEST_DISABLED(@"Test broken.");
-
   [self loadAndSubmitTheForm];
   [self openEditAddress:@"George Washington, 1600 Pennsylvania Ave NW"];
   chrome_test_util::VerifyAccessibilityForCurrentScreen();
@@ -199,9 +190,6 @@ NSString* GetTextFieldForID(int categoryId) {
 
 // Test that the page for editing autofill profile details is accessible.
 - (void)testAccessibilityOnAutofillProfileEditPage {
-  // TODO(crbug.com/866730): Re-enable when fixed
-  EARL_GREY_TEST_DISABLED(@"Test broken.");
-
   [self loadAndSubmitTheForm];
   [self openEditAddress:@"George Washington, 1600 Pennsylvania Ave NW"];
   // Switch on edit mode.
@@ -216,9 +204,6 @@ NSString* GetTextFieldForID(int categoryId) {
 // Checks that if the autofill profiles and credit cards list view is in edit
 // mode, the "autofill" and "wallet" switch items are disabled.
 - (void)testListViewEditMode {
-  // TODO(crbug.com/866730): Re-enable when fixed
-  EARL_GREY_TEST_DISABLED(@"Test broken.");
-
   [self loadAndSubmitTheForm];
 
   [ChromeEarlGreyUI openSettingsMenu];
