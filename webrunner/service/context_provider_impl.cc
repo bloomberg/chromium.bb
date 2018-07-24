90640d58ccb1f21fa8124bfcc74392ab7b02d370
Sergey Ulanov <sergeyu@chromium.org>
2018-07-24 20:27:47 +0000

    [Fuchsia] Disable GPU in webrunner
    Now ContextProvider will run Context process with --disable-gpu.
    GPU process doesn't work correctly on Fuchsia yet.
    
    Bug: 850743, 867052
    Change-Id: Ifde22a36beb69f187be07b7fd075a0f8d807bcdf
    Reviewed-on: https://chromium-review.googlesource.com/1147550
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577663}
    Former-commit-id: 54f61462b4e9f09f37ec4760181464b74a3a31af

--- webrunner/service/context_provider_impl.cc
+++ webrunner/service/context_provider_impl.cc
@@ -17,6 +17,7 @@
 #include "base/fuchsia/fuchsia_logging.h"
 #include "base/logging.h"
 #include "base/process/launch.h"
+#include "content/public/common/content_switches.h"
 #include "webrunner/service/common.h"
 
 namespace webrunner {
@@ -25,6 +26,8 @@ namespace {
 // Relaunches the current executable as a Context process.
 base::Process LaunchContextProcess(const base::LaunchOptions& launch_options) {
   base::CommandLine launch_command = *base::CommandLine::ForCurrentProcess();
+  // TODO(crbug.com/867052): Remove this flag when GPU process works on Fuchsia.
+  launch_command.AppendSwitch(switches::kDisableGpu);
   return base::LaunchProcess(launch_command, launch_options);
 }
 
