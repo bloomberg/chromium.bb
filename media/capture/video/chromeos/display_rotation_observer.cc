2f156a47336e9dea3cdf355f172c3cf592b3efc5
tzik <tzik@chromium.org>
2018-07-24 18:46:48 +0000

    Avoid binding ref counted objects in ScreenObserverDelegate constructor
    This CL adds a static constructor to ScreenObserverDelegate to avoid
    a racy ref count operation.
    
    As ScreenObserverDelegate is a ref counted class, base::Bind implicitly
    increments the reference count, and releases it on the callback instance
    destruction. If PostTask in the SOD ctor fails, or the posted task ran
    soon before the ctor has completed, the ScreenObserverDelegate instance
    may be destroyed immediately, and `new ScreenObserverDelegate` may
    returns a stale pointer.
    
    Bug: 866456
    Change-Id: I7c64915529786b2f0738731c323aaf2f293dbc20
    Reviewed-on: https://chromium-review.googlesource.com/1147896
    Reviewed-by: Christian Fremerey <chfremer@chromium.org>
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577624}
    Former-commit-id: f994a90a78bce4d3b4a2109b713ae3be0110395c

--- media/capture/video/chromeos/display_rotation_observer.cc
+++ media/capture/video/chromeos/display_rotation_observer.cc
@@ -11,17 +11,25 @@
 
 namespace media {
 
+// static
+scoped_refptr<ScreenObserverDelegate> ScreenObserverDelegate::Create(
+    DisplayRotationObserver* observer,
+    scoped_refptr<base::SingleThreadTaskRunner> display_task_runner) {
+  auto delegate = base::WrapRefCounted(
+      new ScreenObserverDelegate(observer, display_task_runner));
+  display_task_runner->PostTask(
+      FROM_HERE,
+      base::BindOnce(&ScreenObserverDelegate::AddObserverOnDisplayThread,
+                     delegate));
+  return delegate;
+}
+
 ScreenObserverDelegate::ScreenObserverDelegate(
     DisplayRotationObserver* observer,
     scoped_refptr<base::SingleThreadTaskRunner> display_task_runner)
     : observer_(observer),
       display_task_runner_(std::move(display_task_runner)),
-      delegate_task_runner_(base::ThreadTaskRunnerHandle::Get()) {
-  display_task_runner_->PostTask(
-      FROM_HERE,
-      base::BindOnce(&ScreenObserverDelegate::AddObserverOnDisplayThread,
-                     this));
-}
+      delegate_task_runner_(base::ThreadTaskRunnerHandle::Get()) {}
 
 void ScreenObserverDelegate::RemoveObserver() {
   DCHECK(delegate_task_runner_->BelongsToCurrentThread());
