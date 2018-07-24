bf99017fe0a7cdedededd4d7a2d3b18c96044f0f
Haruka Matsumura <harukamt@google.com>
2018-07-24 09:45:24 +0000

    Oilpan: Change slots registration in HeapCompaction
    This CL changes the system of slots registration in HeapCompaction.
    As preparing for HeapCompaction with IncrementalMarking, we change a timing of the registration on HashMap that related to slots, from during tracing to during atomic pause.
    So, we add HashSet that registeres traced slots.
    
    Bug: 864425
    Change-Id: Ia63c36abbf160dfea115ebd6c6953c499d895770
    Reviewed-on: https://chromium-review.googlesource.com/1145144
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Haruka Matsumura <harukamt@google.com>
    Cr-Commit-Position: refs/heads/master@{#577469}
    Former-commit-id: 49874bf7b6420872744e1e8e7a336ae245993d18

--- third_party/blink/renderer/platform/heap/heap_compact.cc
+++ third_party/blink/renderer/platform/heap/heap_compact.cc
@@ -13,7 +13,6 @@
 #include "third_party/blink/renderer/platform/histogram.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/wtf/hash_map.h"
-#include "third_party/blink/renderer/platform/wtf/hash_set.h"
 #include "third_party/blink/renderer/platform/wtf/time.h"
 
 namespace blink {
@@ -357,7 +356,7 @@ void HeapCompact::RegisterMovingObjectReference(MovableReference* slot) {
   if (!do_compact_)
     return;
 
-  Fixups().Add(slot);
+  traced_slots_.insert(slot);
 }
 
 void HeapCompact::RegisterMovingObjectCallback(MovableReference reference,
@@ -422,6 +421,13 @@ void HeapCompact::Relocate(Address from, Address to) {
 void HeapCompact::StartThreadCompaction() {
   if (!do_compact_)
     return;
+
+  DCHECK(fixups_);
+  // The mapping between the slots and the backing stores are created
+  for (auto** slot : traced_slots_) {
+    fixups_->Add(slot);
+  }
+  traced_slots_.clear();
 }
 
 void HeapCompact::FinishThreadCompaction() {
