7f3e6b26b95543cfb424c44b722183ab7975d6a3
sczs <sczs@chromium.org>
2018-07-25 18:13:06 +0000

    [ios] Reloads Bookmark tableView after loading model.
    Bug: 866860
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I321613158bac990345f01e46e34153eb84091411
    Reviewed-on: https://chromium-review.googlesource.com/1148588
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577975}
    Former-commit-id: f254c80cc54f0612a895db475b47dc4011e56af5

--- ios/chrome/browser/ui/bookmarks/bookmark_home_view_controller.mm
+++ ios/chrome/browser/ui/bookmarks/bookmark_home_view_controller.mm
@@ -876,6 +876,7 @@ const CGFloat kShadowRadius = 12.0f;
           self.spinnerView = nil;
         }];
     [strongSelf loadBookmarkViews];
+    [strongSelf.sharedState.tableView reloadData];
   }];
 }
 
