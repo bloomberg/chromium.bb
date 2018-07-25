a847e631b78aa78468884a6be7557145df1d990d
Tien-Ren Chen <trchen@chromium.org>
2018-07-25 00:23:28 +0000

    [Blink] Unify replaced element clipping
    Prior to this CL each replaced element type implemented its own clipping.
    This CL refactors it so that a clip node will be applied in the common
    path, ReplacedPainter::Paint(), so the specialized
    XyzPainter::PaintReplaced() won't need to apply their own clips.
    
    Note that ImagePainter still mutates its internal painting for changed
    clip rect. This behavior should be changed in a follow-up CL, and
    changing only the content box of a replaced element should only result
    in incremental invalidation.
    
    BUG=730284
    
    Cq-Include-Trybots: luci.chromium.try:linux-blink-gen-property-trees;luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ia5c1412b1bd3182a22849d0b3196f3e72d52248a
    Reviewed-on: https://chromium-review.googlesource.com/1147617
    Commit-Queue: Tien-Ren Chen <trchen@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577741}
    Former-commit-id: 05d439bae59df4f21124c2bf2e7f96070fd0e796

--- third_party/blink/renderer/core/paint/embedded_content_painter.cc
+++ third_party/blink/renderer/core/paint/embedded_content_painter.cc
@@ -20,104 +20,12 @@
 
 namespace blink {
 
-bool EmbeddedContentPainter::IsSelected() const {
-  SelectionState s = layout_embedded_content_.GetSelectionState();
-  if (s == SelectionState::kNone)
-    return false;
-
-  return true;
-}
-
-void EmbeddedContentPainter::Paint(const PaintInfo& paint_info) {
-  // TODO(crbug.com/797779): For now embedded contents don't know whether
-  // they are painted in a fragmented context and may do something bad in a
-  // fragmented context, e.g. creating subsequences. Skip cache to avoid that.
-  // This will be unnecessary when the contents are fragment aware.
-  base::Optional<DisplayItemCacheSkipper> cache_skipper;
-  DCHECK(layout_embedded_content_.HasLayer());
-  if (layout_embedded_content_.Layer()->EnclosingPaginationLayer())
-    cache_skipper.emplace(paint_info.context);
-
-  AdjustPaintOffsetScope adjustment(layout_embedded_content_, paint_info);
-  const auto& local_paint_info = adjustment.GetPaintInfo();
-  auto paint_offset = adjustment.PaintOffset();
-  if (!ReplacedPainter(layout_embedded_content_)
-           .ShouldPaint(local_paint_info, paint_offset))
-    return;
-
-  if (layout_embedded_content_.HasBoxDecorationBackground() &&
-      (local_paint_info.phase == PaintPhase::kForeground ||
-       local_paint_info.phase == PaintPhase::kSelection)) {
-    BoxPainter(layout_embedded_content_)
-        .PaintBoxDecorationBackground(local_paint_info, paint_offset);
-  }
-
-  if (local_paint_info.phase == PaintPhase::kMask) {
-    BoxPainter(layout_embedded_content_)
-        .PaintMask(local_paint_info, paint_offset);
-    return;
-  }
-
-  if (ShouldPaintSelfOutline(local_paint_info.phase)) {
-    ObjectPainter(layout_embedded_content_)
-        .PaintOutline(local_paint_info, paint_offset);
-  }
-
-  if (local_paint_info.phase != PaintPhase::kForeground)
-    return;
-
-  if (layout_embedded_content_.GetEmbeddedContentView()) {
-    LayoutRect border_rect(paint_offset, layout_embedded_content_.Size());
-    if (border_rect.IsEmpty())
-      return;
-
-    base::Optional<ScopedPaintChunkProperties> scoped_paint_chunk_properties;
-    const auto* fragment =
-        local_paint_info.FragmentToPaint(layout_embedded_content_);
-    if (!fragment)
-      return;
-    const auto* properties = fragment->PaintProperties();
-
-    if (properties && properties->OverflowClip()) {
-      scoped_paint_chunk_properties.emplace(
-          local_paint_info.context.GetPaintController(),
-          properties->OverflowClip(), layout_embedded_content_,
-          DisplayItem::PaintPhaseToDrawingType(local_paint_info.phase));
-    }
-
-    layout_embedded_content_.PaintContents(local_paint_info, paint_offset);
-  }
-
-  // Paint a partially transparent wash over selected EmbeddedContentViews.
-  if (IsSelected() && !local_paint_info.IsPrinting() &&
-      !DrawingRecorder::UseCachedDrawingIfPossible(local_paint_info.context,
-                                                   layout_embedded_content_,
-                                                   local_paint_info.phase)) {
-    LayoutRect rect = layout_embedded_content_.LocalSelectionRect();
-    rect.MoveBy(paint_offset);
-    IntRect selection_rect = PixelSnappedIntRect(rect);
-    DrawingRecorder recorder(local_paint_info.context, layout_embedded_content_,
-                             local_paint_info.phase);
-    Color selection_bg = SelectionPaintingUtils::SelectionBackgroundColor(
-        layout_embedded_content_.GetDocument(),
-        layout_embedded_content_.StyleRef(),
-        layout_embedded_content_.GetNode());
-    local_paint_info.context.FillRect(selection_rect, selection_bg);
-  }
-
-  if (layout_embedded_content_.CanResize()) {
-    ScrollableAreaPainter(
-        *layout_embedded_content_.Layer()->GetScrollableArea())
-        .PaintResizer(local_paint_info.context, RoundedIntPoint(paint_offset),
-                      local_paint_info.GetCullRect());
-  }
-}
-
-void EmbeddedContentPainter::PaintContents(const PaintInfo& paint_info,
+void EmbeddedContentPainter::PaintReplaced(const PaintInfo& paint_info,
                                            const LayoutPoint& paint_offset) {
   EmbeddedContentView* embedded_content_view =
       layout_embedded_content_.GetEmbeddedContentView();
-  CHECK(embedded_content_view);
+  if (!embedded_content_view)
+    return;
 
   IntPoint paint_location(RoundedIntPoint(
       paint_offset +
