8fb1036ad10d909f066258e271e7f9df21f89cd1
Parastoo Geranmayeh <parastoog@google.com>
2018-07-25 19:54:38 +0000

    [Autofill] Regex for credit card number heuristics
    For a label to represent a credit card number having the word number
    is not enough. We need to make sure the label doesn't contain the
    words that denote other fields such as document or phone.
    
    Test added.
    
    Bug: 865838
    Change-Id: I06f606b8a324336509dcccbcce98262d43cd00f0
    Reviewed-on: https://chromium-review.googlesource.com/1148960
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578025}
    Former-commit-id: 6fe8d29cebf89cb8ed5c11ade2a7ba4ff5f0e7e3

--- components/autofill/core/common/autofill_regex_constants.cc
+++ components/autofill/core/common/autofill_regex_constants.cc
@@ -160,14 +160,14 @@ const char kNameOnCardRe[] =
 const char kNameOnCardContextualRe[] = "name";
 const char kCardNumberRe[] =
     "(add)?(?:card|cc|acct).?(?:number|#|no|num|field)"
-    "|(?<!telefon|haus)nummer"  // de-DE
-    "|credito|numero|número"    // es
-    "|numéro"                   // fr-FR
-    "|カード番号"               // ja-JP
-    "|Номер.*карты"             // ru
-    "|信用卡号|信用卡号码"      // zh-CN
-    "|信用卡卡號"               // zh-TW
-    "|카드";                    // ko-KR
+    "|(?<!telefon|haus)nummer"                             // de-DE
+    "|カード番号"                                          // ja-JP
+    "|Номер.*карты"                                        // ru
+    "|信用卡号|信用卡号码"                                 // zh-CN
+    "|信用卡卡號"                                          // zh-TW
+    "|카드"                                                // ko-KR
+    "|(numero|número|numéro)(?!.*(document|fono|phone))";  // es/pt/fr
+
 const char kCardCvcRe[] =
     "verification|card.?identification|security.?code|card.?code"
     "|security.?value"
