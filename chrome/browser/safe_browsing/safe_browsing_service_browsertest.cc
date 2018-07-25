4677e139e122d7c341bbea420413c29a0a9963e3
Daniel Rubery <drubery@chromium.org>
2018-07-25 17:27:36 +0000

    Enable SafeBrowsingServiceMetadataTest.MalwareImg with MSAN
    This test was disabled with MSAN due to the race condition in
    OnReceivedThreatDOMDetails. Repeated local runs with MSAN now succeed,
    so I think it's safe to re-enable.
    
    Bug: 860445
    Change-Id: I4d01568c79562ac1125c4410dbe2c52c17056c2d
    Reviewed-on: https://chromium-review.googlesource.com/1149260
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577959}
    Former-commit-id: a30c58ea7443b6895c21c3ade5e9fd3ad0f11d91

--- chrome/browser/safe_browsing/safe_browsing_service_browsertest.cc
+++ chrome/browser/safe_browsing/safe_browsing_service_browsertest.cc
@@ -1043,14 +1043,7 @@ IN_PROC_BROWSER_TEST_P(SafeBrowsingServiceMetadataTest, MalwareIFrame) {
   EXPECT_TRUE(hit_report().is_subresource);
 }
 
-// TODO(https://crbug.com/860445) - Reenable this once potential race condition
-// is addressed, which is triggered in certain conditions under MSAN, see bug.
-#if defined(MEMORY_SANITIZER)
-#define MAYBE_MalwareImg DISABLED_MalwareImg
-#else
-#define MAYBE_MalwareImg MalwareImg
-#endif
-IN_PROC_BROWSER_TEST_P(SafeBrowsingServiceMetadataTest, MAYBE_MalwareImg) {
+IN_PROC_BROWSER_TEST_P(SafeBrowsingServiceMetadataTest, MalwareImg) {
   GURL main_url = embedded_test_server()->GetURL(kMalwarePage);
   GURL img_url = embedded_test_server()->GetURL(kMalwareImg);
 
