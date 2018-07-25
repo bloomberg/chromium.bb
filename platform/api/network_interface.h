50905ba30cbb083645deb57de09e57003d12d19d
btolsch <btolsch@chromium.org>
2018-07-25 14:12:32 -0700

    Add network interface platform API
    This change adds a platform API (with a linux implementation) which
    provides information about all active network interfaces and any
    assigned IP (v4 and v6) addresses.
    
    This change also adds a ScopedPipe class for helping with C-style file
    descriptors.
    
    Change-Id: I8b89825a7b5f45dc5b9e6e497442272dc5fe05fc
    Reviewed-on: https://chromium-review.googlesource.com/1150720
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>

---
+++ platform/api/network_interface.h
@@ -0,0 +1,71 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PLATFORM_API_NETWORK_INTERFACE_H_
+#define PLATFORM_API_NETWORK_INTERFACE_H_
+
+#include <vector>
+
+#include "base/ip_address.h"
+
+namespace openscreen {
+namespace platform {
+
+struct InterfaceInfo {
+  enum class Type {
+    kEthernet = 0,
+    kWifi,
+    kOther,
+  };
+
+  // Interface index, typically as specified by the operating system,
+  // identifying this interface on the host machine.
+  int32_t index = 0;
+
+  // MAC address of the interface.  All 0s if unavailable.
+  uint8_t hardware_address[6] = {};
+
+  // Interface name (e.g. eth0) if available.
+  std::string name;
+
+  // Hardware type of the interface.
+  Type type = Type::kOther;
+};
+
+struct IPv4Subnet {
+  IPv4Address address;
+
+  // Prefix length of |address|, which is another way of specifying a subnet
+  // mask.  For example, 192.168.0.10/24 is a common representation of the
+  // address 192.168.0.10 with a 24-bit prefix, and therefore a subnet mask of
+  // 255.255.255.0.
+  uint8_t prefix_length = 0;
+};
+
+struct IPv6Subnet {
+  IPv6Address address;
+
+  // Prefix length of |address|.  This works the same was as
+  // IPv4PrefixedAddress.prefix_length, but can be larger.
+  uint8_t prefix_length = 0;
+};
+
+struct InterfaceAddresses {
+  InterfaceInfo info = {};
+
+  // All IPv4 addresses associated with the interface identified by |info|.
+  std::vector<IPv4Subnet> ipv4_addresses;
+
+  // All IPv6 addresses associated with the interface identified by |info|.
+  std::vector<IPv6Subnet> ipv6_addresses;
+};
+
+// Returns at most one InterfaceAddresses per interface, so there will be no
+// duplicate InterfaceInfo entries.
+std::vector<InterfaceAddresses> GetInterfaceAddresses();
+
+}  // namespace platform
+}  // namespace openscreen
+
+#endif  // PLATFORM_API_NETWORK_INTERFACE_H_
