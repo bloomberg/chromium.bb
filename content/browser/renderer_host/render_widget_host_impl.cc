78761744b971c15f1b7cddbda9039f6681a9db37
danakj <danakj@chromium.org>
2018-07-25 14:43:45 +0000

    When CompositorFrameSink replaced, consider SharedBitmapIds destroyed
    Any SharedBitmapIds registered with a CompositorFrameSink should be
    considered as destroyed by the renderer when the CompositorFrameSink
    is lost, as the renderer will treat them as so and will not report when
    they are deleted, and will re-register anything it wishes to reuse.
    
    This makes RenderWidgetHostImpl::RequestCompositorFrameSink() report
    all |owned_bitmaps_| as being deleted to the ServerSharedBitmapManager
    so that the same bitmaps are not re-registered, and bitmaps that are
    not reused are not leaked.
    
    R=piman@chromium.org
    
    Bug: 862584
    Change-Id: I39fcbe49559f9d32eab3a52310b136f65a5f92ab
    Reviewed-on: https://chromium-review.googlesource.com/1148812
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#577883}
    Former-commit-id: e341c8a9f4665dcc97df8fc1d7e49462da1b6f08

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -2830,14 +2830,22 @@ void RenderWidgetHostImpl::RequestCompositorFrameSink(
       return;
   }
 
+  // Consider any bitmaps registered with the old CompositorFrameSink as gone,
+  // they will be re-registered on the newly requested CompositorFrameSink if
+  // they are meant to be used still. https://crbug.com/862584.
+  for (const auto& id : owned_bitmaps_)
+    shared_bitmap_manager_->ChildDeletedSharedBitmap(id);
+  owned_bitmaps_.clear();
+
   if (compositor_frame_sink_binding_.is_bound())
     compositor_frame_sink_binding_.Close();
   compositor_frame_sink_binding_.Bind(
       std::move(compositor_frame_sink_request),
       BrowserMainLoop::GetInstance()->GetResizeTaskRunner());
-  if (view_)
+  if (view_) {
     view_->DidCreateNewRendererCompositorFrameSink(
         compositor_frame_sink_client.get());
+  }
   renderer_compositor_frame_sink_ = std::move(compositor_frame_sink_client);
 }
 
