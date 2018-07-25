d103d77a148e490390e47d2f3c18e0c24235757a
Daniel Bratell <bratell@opera.com>
2018-07-25 04:45:09 +0000

    Switch ui/gl/gl_enums.cc from khronos GL to ui/gl
    gl_enums used Khronos to get a definition of GL_NONE but
    that broke jumbo build experiments because you cannot have
    khronos headers and ui/gl headers in the same translation
    unit.
    
    This patch switches gl_enums.cc to use ui/gl/gl_bindings.h like
    the rest of the code.
    
    Bug: 864986
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Id239ca4aa55278ac012c066d6a6f8e243b3ec319
    Reviewed-on: https://chromium-review.googlesource.com/1148442
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#577799}
    Former-commit-id: 3e7ad811e5213421556c84a0101f761f0e303cfb

--- ui/gl/gl_enums.cc
+++ ui/gl/gl_enums.cc
@@ -2,10 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <sstream>
-#include <GLES2/gl2.h>
-
 #include "ui/gl/gl_enums.h"
+
+#include <sstream>
+
+#include "ui/gl/gl_bindings.h"
 
 namespace gl {
 
