79cd9d6b71bfc2d9f609590b0dad41640d472368
James Hawkins <jhawkins@chromium.org>
2018-07-26 14:14:35 +0000

    ui/base/ime: Fix the cros chrome-sdk build.
    This change removes unused X11 includes, which fixes the
    ui/base:ui_base_unittests target on the cros chrome-sdk shell build.
    
    R=yhanada@chromium.org
    
    Bug: 867013
    Test: none
    Change-Id: I42f1d1e72f9cbe35ca585764db19353e8721bfbc
    Reviewed-on: https://chromium-review.googlesource.com/1150507
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578290}
    Former-commit-id: 5d9776b560ad9a51f9b244d125096b755bf82fe8

--- ui/base/ime/input_method_chromeos_unittest.cc
+++ ui/base/ime/input_method_chromeos_unittest.cc
@@ -27,9 +27,7 @@
 #include "ui/events/event_utils.h"
 #include "ui/events/keycodes/dom/dom_code.h"
 #include "ui/events/keycodes/dom/keycode_converter.h"
-#include "ui/events/test/events_test_utils_x11.h"
 #include "ui/gfx/geometry/rect.h"
-#include "ui/gfx/x/x11.h"
 
 using base::UTF8ToUTF16;
 using base::UTF16ToUTF8;
