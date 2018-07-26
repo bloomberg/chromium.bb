8a07baa900cd6c98e1b74d624be340fd5a5e654d
David Black <dmblack@google.com>
2018-07-26 03:39:59 +0000

    Modify handling of Assistant query views.
    This is prep work to support animations in which the pending query will
    animate from the bottom of its parent to the top of its parent on
    commit.
    
    Previously, we used two distinct views to represent a pending query and
    a committed query, respectively.
    
    Now, the same view will be used for a query thoughout its lifecycle.
    This will allow us to animate it when committed.
    
    This CL does NOT animate the view, just prepares for animating in a
    follow up CL.
    
    Bug: b:110228421
    Change-Id: I119db0d4be48960bc13db04caeee4529d240e41d
    Reviewed-on: https://chromium-review.googlesource.com/1149002
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578185}
    Former-commit-id: ff0f61b5b8aaffaa00372aed638a0f6061be5a3d

--- ash/assistant/ui/main_stage/assistant_main_stage.h
+++ ash/assistant/ui/main_stage/assistant_main_stage.h
@@ -5,6 +5,7 @@
 #ifndef ASH_ASSISTANT_UI_MAIN_STAGE_ASSISTANT_MAIN_STAGE_H_
 #define ASH_ASSISTANT_UI_MAIN_STAGE_ASSISTANT_MAIN_STAGE_H_
 
+#include "ash/assistant/model/assistant_interaction_model_observer.h"
 #include "base/macros.h"
 #include "ui/views/view.h"
 #include "ui/views/view_observer.h"
@@ -19,7 +20,9 @@ class UiElementContainerView;
 // AssistantMainStage is the child of AssistantMainView responsible for
 // displaying the Assistant interaction to the user. This includes visual
 // affordances for the query, response, as well as suggestions.
-class AssistantMainStage : public views::View, public views::ViewObserver {
+class AssistantMainStage : public views::View,
+                           public views::ViewObserver,
+                           public AssistantInteractionModelObserver {
  public:
   explicit AssistantMainStage(AssistantController* assistant_controller);
   ~AssistantMainStage() override;
@@ -29,20 +32,35 @@ class AssistantMainStage : public views::View, public views::ViewObserver {
   void ChildVisibilityChanged(views::View* child) override;
 
   // views::ViewObserver:
+  void OnViewBoundsChanged(views::View* view) override;
   void OnViewPreferredSizeChanged(views::View* view) override;
   void OnViewVisibilityChanged(views::View* view) override;
+  void OnViewIsDeleting(views::View* view) override;
+
+  // AssistantInteractionModelObserver:
+  void OnCommittedQueryChanged(const AssistantQuery& query) override;
+  void OnCommittedQueryCleared() override;
+  void OnPendingQueryChanged(const AssistantQuery& query) override;
+  void OnPendingQueryCleared() override;
 
  private:
   void InitLayout(AssistantController* assistant_controller);
   void InitContentLayoutContainer(AssistantController* assistant_controller);
   void InitQueryLayoutContainer(AssistantController* assistant_controller);
 
-  AssistantQueryView* committed_query_view_;       // Owned by view hierarchy.
+  void UpdateCommittedQueryViewSpacer();
+  void UpdateSuggestionContainer();
+
+  AssistantController* const assistant_controller_;  // Owned by Shell.
+
   views::View* committed_query_view_spacer_;       // Owned by view hierarchy.
-  AssistantQueryView* pending_query_view_;         // Owned by view hierarchy.
+  views::View* query_layout_container_;            // Owned by view hierarchy.
   SuggestionContainerView* suggestion_container_;  // Owned by view hierarchy.
-  views::View* suggestion_container_spacer_;       // Owned by view hierarchy.
   UiElementContainerView* ui_element_container_;   // Owned by view hierarchy.
+
+  // Owned by view hierarchy.
+  AssistantQueryView* committed_query_view_ = nullptr;
+  AssistantQueryView* pending_query_view_ = nullptr;
 
   DISALLOW_COPY_AND_ASSIGN(AssistantMainStage);
 };
--- ash/assistant/ui/main_stage/assistant_query_view.h
+++ ash/assistant/ui/main_stage/assistant_query_view.h
@@ -5,28 +5,19 @@
 #ifndef ASH_ASSISTANT_UI_MAIN_STAGE_ASSISTANT_QUERY_VIEW_H_
 #define ASH_ASSISTANT_UI_MAIN_STAGE_ASSISTANT_QUERY_VIEW_H_
 
-#include "ash/assistant/model/assistant_interaction_model_observer.h"
 #include "base/macros.h"
 #include "ui/views/controls/styled_label.h"
 #include "ui/views/view.h"
 
 namespace ash {
 
-class AssistantController;
+class AssistantQuery;
 
 // AssistantQueryView is the visual representation of an AssistantQuery. It is a
 // child view of UiElementContainerView.
-class AssistantQueryView : public views::View,
-                           public AssistantInteractionModelObserver {
+class AssistantQueryView : public views::View {
  public:
-  // Dictates whether AssistantQueryView observes a committed or pending query.
-  enum ObservedQueryState {
-    kCommitted,
-    kPending,
-  };
-
-  AssistantQueryView(AssistantController* assistant_controller,
-                     ObservedQueryState observed_query_state);
+  AssistantQueryView();
   ~AssistantQueryView() override;
 
   // views::View:
@@ -35,25 +26,14 @@ class AssistantQueryView : public views::View,
   void ChildPreferredSizeChanged(views::View* child) override;
   void OnBoundsChanged(const gfx::Rect& prev_bounds) override;
 
-  // AssistantInteractionModelObserver:
-  void OnCommittedQueryChanged(const AssistantQuery& committed_query) override;
-  void OnCommittedQueryCleared() override;
-  void OnPendingQueryChanged(const AssistantQuery& pending_query) override;
-  void OnPendingQueryCleared() override;
+  void SetQuery(const AssistantQuery& query);
 
  private:
   void InitLayout();
   void SetText(const std::string& high_confidence_text,
                const std::string& low_confidence_text = std::string());
-  views::StyledLabel::RangeStyleInfo CreateStyleInfo(SkColor color) const;
 
-  void OnQueryChanged(const AssistantQuery& query);
-  void OnQueryCleared();
-
-  AssistantController* const assistant_controller_;  // Owned by Shell.
   views::StyledLabel* label_;                        // Owned by view hierarchy.
-
-  const ObservedQueryState observed_query_state_;
 
   DISALLOW_COPY_AND_ASSIGN(AssistantQueryView);
 };
