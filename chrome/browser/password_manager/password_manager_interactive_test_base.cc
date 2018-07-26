d028faec4ea9c1b57f8549a1e3756c5e85a46214
Alexey Khoroshilov <sense@yandex-team.ru>
2018-07-26 11:22:56 +0000

    Revert "Improve an HTML input fill logic to fix test flakiness."
    This reverts commit 29eade0ba64f4b792908ee99c351df992796fc1b [formerly b9a893cd50f940feedb3ebe8bbd29f0da95e1653].
    
    Reason for revert: Flaky on MacOS.
    
    Original change's description:
    > Improve an HTML input fill logic to fix test flakiness.
    > 
    > Bug: 849582
    > Change-Id: Idb681d282fd6754033779fb36c879cb72b0abf88
    > Reviewed-on: https://chromium-review.googlesource.com/1142765
    > Commit-Queue: Alexey Khoroshilov <sense@yandex-team.ru>
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#578206}
    
    TBR=sky@chromium.org,vasilii@chromium.org,sense@yandex-team.ru
    
    Change-Id: I8d1e71e16dbae6d6293a6f6c2ed63102de8713aa
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 849582
    Reviewed-on: https://chromium-review.googlesource.com/1151150
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578261}
    Former-commit-id: 89b69b4f8865bead99045fc90d70db07521b8f18

--- chrome/browser/password_manager/password_manager_interactive_test_base.cc
+++ chrome/browser/password_manager/password_manager_interactive_test_base.cc
@@ -4,39 +4,9 @@
 
 #include "chrome/browser/password_manager/password_manager_interactive_test_base.h"
 
-#include "base/json/json_reader.h"
-#include "base/strings/strcat.h"
 #include "content/public/test/browser_test_utils.h"
 #include "ui/events/keycodes/dom_us_layout_data.h"
 #include "ui/events/keycodes/keyboard_code_conversion.h"
-
-namespace {
-
-// Returns element's bounds in frame coordinates.
-bool GetElementBoundsById(content::WebContents* web_contents,
-                          base::StringPiece element_id,
-                          gfx::Rect* bounds) {
-  DCHECK(bounds);
-  const std::string get_rect_js = base::StrCat(
-      {"var json_rect = JSON.stringify(document.getElementById('", element_id,
-       "')."
-       "getBoundingClientRect().toJSON());"
-       "domAutomationController.send(json_rect);"});
-  std::string json_rect;
-  EXPECT_TRUE(content::ExecuteScriptAndExtractString(web_contents, get_rect_js,
-                                                     &json_rect));
-  std::unique_ptr<base::Value> value = base::JSONReader::Read(json_rect);
-  if (!value)
-    return false;
-  auto get_floored_int = [&value](base::StringPiece member) {
-    return static_cast<int>(std::floor(value->FindKey(member)->GetDouble()));
-  };
-  bounds->SetRect(get_floored_int("x"), get_floored_int("y"),
-                  get_floored_int("width"), get_floored_int("height"));
-  return true;
-}
-
-}  // namespace
 
 PasswordManagerInteractiveTestBase::PasswordManagerInteractiveTestBase() =
     default;
@@ -46,13 +16,11 @@ PasswordManagerInteractiveTestBase::~PasswordManagerInteractiveTestBase() =
 
 void PasswordManagerInteractiveTestBase::FillElementWithValue(
     const std::string& element_id,
-    const std::string& value,
-    const base::Optional<std::string>& expected_value) {
-  gfx::Rect element_bounds;
-  ASSERT_TRUE(GetElementBoundsById(WebContents(), element_id, &element_bounds));
-  content::SimulateMouseClickAt(WebContents(), 0,
-                                blink::WebMouseEvent::Button::kLeft,
-                                element_bounds.CenterPoint());
+    const std::string& value) {
+  ASSERT_TRUE(content::ExecuteScript(
+      RenderFrameHost(),
+      base::StringPrintf("document.getElementById('%s').focus();",
+                         element_id.c_str())));
   for (base::char16 character : value) {
     ui::DomKey dom_key = ui::DomKey::FromCharacter(character);
     const ui::PrintableCodeEntry* code_entry = std::find_if(
@@ -68,7 +36,6 @@ void PasswordManagerInteractiveTestBase::FillElementWithValue(
                               ui::DomCodeToUsLayoutKeyboardCode(dom_code),
                               false, shift, false, false);
   }
-  WaitForElementValue(element_id, expected_value.value_or(value));
 }
 
 void PasswordManagerInteractiveTestBase::VerifyPasswordIsSavedAndFilled(
