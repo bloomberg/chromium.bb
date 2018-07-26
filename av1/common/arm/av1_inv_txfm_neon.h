wjdz99/aom

Add ARM Neon optimization of txfm 8x8
2 % gain in decoder time.

Change-Id: I17c3c92533b9809b308c7dc621d607570346c0a2

av1/common/arm/av1_inv_txfm_neon.h
@@ -23,6 +23,8 @@
 typedef void (*transform_1d_neon)(const int32_t *input, int32_t *output,
                                   const int8_t cos_bit,
                                   const int8_t *stage_ptr);
+typedef void (*transform_8x8_neon)(int16x8_t *input, int16x8_t *output,
+                                   int8_t cos_bit, int bit);
 
 DECLARE_ALIGNED(16, static const int16_t, av1_eob_to_eobxy_8x8_default[8]) = {
   0x0707, 0x0707, 0x0707, 0x0707, 0x0707, 0x0707, 0x0707, 0x0707,

bloomberg/chromium.bb

av1/common/arm/av1_inv_txfm_neon.h
@@ -23,6 +23,8 @@
 typedef void (*transform_1d_neon)(const int32_t *input, int32_t *output,
                                   const int8_t cos_bit,
                                   const int8_t *stage_ptr);
+typedef void (*transform_neon)(int16x8_t *input, int16x8_t *output,
+                               int8_t cos_bit, int bit);
 
 DECLARE_ALIGNED(16, static const int16_t, av1_eob_to_eobxy_8x8_default[8]) = {
   0x0707, 0x0707, 0x0707, 0x0707, 0x0707, 0x0707, 0x0707, 0x0707,