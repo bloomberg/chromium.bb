c39c6576110e8df233bd6c02299444718d3e972c
Henrik Boström <hbos@chromium.org>
2018-07-26 09:57:18 +0000

    Revert "Synchronize QuaRTC code up to the Wed Jul 25 20:25:29 EDT 2018"
    This reverts commit 167f1cef5e6bbb2b865e6df85b83cac40ef798c6 [formerly 812abd15e33ab7dd8179bca0cf783d03d361cb9a].
    
    Reason for revert: Speculative revert for making several QuartcSessionTests flaky (net_unittests). This is recent to the first flake. The flakes are frequent, but which test fail or on which device of the bot is random.
    
    First flake:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/ios-simulator/35890
    Other flakes: 35893, 35894, 35895, 35896, 35897, 35898, 35899, 35900
    
    Includes failures in:
    QuartcSessionTest.StreamConnection
    QuartcSessionTest.CancelQuartcStream
    QuartcSessionTest.PreSharedKeyHandshake
    QuartcSessionTest.WriterGivesPacketNumberToTransport
    QuartcSessionTest.CloseConnection
    
    Original change's description:
    > Synchronize QuaRTC code up to the Wed Jul 25 20:25:29 EDT 2018
    > 
    > R=​rch@chromium.org
    > 
    > Change-Id: I93d8c963e2d40a409aab5985625659e39f3ffb87
    > Reviewed-on: https://chromium-review.googlesource.com/1150883
    > Reviewed-by: Ryan Hamilton <rch@chromium.org>
    > Commit-Queue: Victor Vasiliev <vasilvv@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#578172}
    
    TBR=rch@chromium.org,vasilvv@chromium.org
    
    Change-Id: I0b7e0ae028d8371c57e66bc9876d4aa05c10b471
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/1151148
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578252}
    Former-commit-id: 8cc4e50cd13a899c4b5b5425e5fa4823557afdf8

--- net/third_party/quic/quartc/quartc_factory.cc
+++ net/third_party/quic/quartc/quartc_factory.cc
@@ -11,17 +11,106 @@
 
 namespace quic {
 
+namespace {
+
+// Implements the QuicAlarm with QuartcTaskRunnerInterface for the Quartc
+//  users other than Chromium. For example, WebRTC will create QuartcAlarm with
+// a QuartcTaskRunner implemented by WebRTC.
+class QuartcAlarm : public QuicAlarm, public QuartcTaskRunnerInterface::Task {
+ public:
+  QuartcAlarm(const QuicClock* clock,
+              QuartcTaskRunnerInterface* task_runner,
+              QuicArenaScopedPtr<QuicAlarm::Delegate> delegate)
+      : QuicAlarm(std::move(delegate)),
+        clock_(clock),
+        task_runner_(task_runner) {}
+
+  ~QuartcAlarm() override {
+    // Cancel the scheduled task before getting deleted.
+    CancelImpl();
+  }
+
+  // QuicAlarm overrides.
+  void SetImpl() override {
+    DCHECK(deadline().IsInitialized());
+    // Cancel it if already set.
+    CancelImpl();
+
+    int64_t delay_ms = (deadline() - (clock_->Now())).ToMilliseconds();
+    if (delay_ms < 0) {
+      delay_ms = 0;
+    }
+
+    DCHECK(task_runner_);
+    DCHECK(!scheduled_task_);
+    scheduled_task_ = task_runner_->Schedule(this, delay_ms);
+  }
+
+  void CancelImpl() override {
+    if (scheduled_task_) {
+      scheduled_task_->Cancel();
+      scheduled_task_.reset();
+    }
+  }
+
+  // QuartcTaskRunner::Task overrides.
+  void Run() override {
+    // The alarm may have been cancelled.
+    if (!deadline().IsInitialized()) {
+      return;
+    }
+
+    // The alarm may have been re-set to a later time.
+    if (clock_->Now() < deadline()) {
+      SetImpl();
+      return;
+    }
+
+    Fire();
+  }
+
+ private:
+  // Not owned by QuartcAlarm. Owned by the QuartcFactory.
+  const QuicClock* clock_;
+  // Not owned by QuartcAlarm. Owned by the QuartcFactory.
+  QuartcTaskRunnerInterface* task_runner_;
+  // Owned by QuartcAlarm.
+  std::unique_ptr<QuartcTaskRunnerInterface::ScheduledTask> scheduled_task_;
+};
+
+// Adapts QuartcClockInterface (provided by the user) to QuicClock
+// (expected by QUIC).
+class QuartcClock : public QuicClock {
+ public:
+  explicit QuartcClock(QuartcClockInterface* clock) : clock_(clock) {}
+  QuicTime ApproximateNow() const override { return Now(); }
+  QuicTime Now() const override {
+    return QuicTime::Zero() +
+           QuicTime::Delta::FromMicroseconds(clock_->NowMicroseconds());
+  }
+  QuicWallTime WallNow() const override {
+    return QuicWallTime::FromUNIXMicroseconds(clock_->NowMicroseconds());
+  }
+
+ private:
+  QuartcClockInterface* clock_;
+};
+
+}  // namespace
+
 QuartcFactory::QuartcFactory(const QuartcFactoryConfig& factory_config)
-    : alarm_factory_(factory_config.alarm_factory),
-      clock_(factory_config.clock) {}
+    : task_runner_(factory_config.task_runner),
+      clock_(new QuartcClock(factory_config.clock)) {}
 
 QuartcFactory::~QuartcFactory() {}
 
-std::unique_ptr<QuartcSession> QuartcFactory::CreateQuartcSession(
+std::unique_ptr<QuartcSessionInterface> QuartcFactory::CreateQuartcSession(
     const QuartcSessionConfig& quartc_session_config) {
   DCHECK(quartc_session_config.packet_transport);
 
-  Perspective perspective = quartc_session_config.perspective;
+  Perspective perspective = quartc_session_config.is_server
+                                ? Perspective::IS_SERVER
+                                : Perspective::IS_CLIENT;
 
   // QuartcSession will eventually own both |writer| and |quic_connection|.
   auto writer =
@@ -46,50 +135,53 @@ std::unique_ptr<QuartcSession> QuartcFactory::CreateQuartcSession(
   // Enable time-based loss detection.
   copt.push_back(kTIME);
 
-  copt.push_back(kTBBR);
+  if (quartc_session_config.congestion_control ==
+      QuartcCongestionControl::kBBR) {
+    copt.push_back(kTBBR);
 
-  // Note: These settings have no effect for Exoblaze builds since
-  // SetQuicReloadableFlag() gets stubbed out.
-  SetQuicReloadableFlag(quic_bbr_less_probe_rtt, true);
-  SetQuicReloadableFlag(quic_unified_iw_options, true);
-  for (const auto option : quartc_session_config.bbr_options) {
-    switch (option) {
-      case (QuartcBbrOptions::kSlowerStartup):
-        copt.push_back(kBBRS);
-        break;
-      case (QuartcBbrOptions::kFullyDrainQueue):
-        copt.push_back(kBBR3);
-        break;
-      case (QuartcBbrOptions::kReduceProbeRtt):
-        copt.push_back(kBBR6);
-        break;
-      case (QuartcBbrOptions::kSkipProbeRtt):
-        copt.push_back(kBBR7);
-        break;
-      case (QuartcBbrOptions::kSkipProbeRttAggressively):
-        copt.push_back(kBBR8);
-        break;
-      case (QuartcBbrOptions::kFillUpLinkDuringProbing):
-        quic_connection->set_fill_up_link_during_probing(true);
-        break;
-      case (QuartcBbrOptions::kInitialWindow3):
-        copt.push_back(kIW03);
-        break;
-      case (QuartcBbrOptions::kInitialWindow10):
-        copt.push_back(kIW10);
-        break;
-      case (QuartcBbrOptions::kInitialWindow20):
-        copt.push_back(kIW20);
-        break;
-      case (QuartcBbrOptions::kInitialWindow50):
-        copt.push_back(kIW50);
-        break;
-      case (QuartcBbrOptions::kStartup1RTT):
-        copt.push_back(k1RTT);
-        break;
-      case (QuartcBbrOptions::kStartup2RTT):
-        copt.push_back(k2RTT);
-        break;
+    // Note: These settings have no effect for Exoblaze builds since
+    // SetQuicReloadableFlag() gets stubbed out.
+    SetQuicReloadableFlag(quic_bbr_less_probe_rtt, true);
+    SetQuicReloadableFlag(quic_unified_iw_options, true);
+    for (const auto option : quartc_session_config.bbr_options) {
+      switch (option) {
+        case (QuartcBbrOptions::kSlowerStartup):
+          copt.push_back(kBBRS);
+          break;
+        case (QuartcBbrOptions::kFullyDrainQueue):
+          copt.push_back(kBBR3);
+          break;
+        case (QuartcBbrOptions::kReduceProbeRtt):
+          copt.push_back(kBBR6);
+          break;
+        case (QuartcBbrOptions::kSkipProbeRtt):
+          copt.push_back(kBBR7);
+          break;
+        case (QuartcBbrOptions::kSkipProbeRttAggressively):
+          copt.push_back(kBBR8);
+          break;
+        case (QuartcBbrOptions::kFillUpLinkDuringProbing):
+          quic_connection->set_fill_up_link_during_probing(true);
+          break;
+        case (QuartcBbrOptions::kInitialWindow3):
+          copt.push_back(kIW03);
+          break;
+        case (QuartcBbrOptions::kInitialWindow10):
+          copt.push_back(kIW10);
+          break;
+        case (QuartcBbrOptions::kInitialWindow20):
+          copt.push_back(kIW20);
+          break;
+        case (QuartcBbrOptions::kInitialWindow50):
+          copt.push_back(kIW50);
+          break;
+        case (QuartcBbrOptions::kStartup1RTT):
+          copt.push_back(k1RTT);
+          break;
+        case (QuartcBbrOptions::kStartup2RTT):
+          copt.push_back(k2RTT);
+          break;
+      }
     }
   }
   QuicConfig quic_config;
@@ -110,15 +202,15 @@ std::unique_ptr<QuartcSession> QuartcFactory::CreateQuartcSession(
       kStreamReceiveWindowLimit);
   quic_config.SetConnectionOptionsToSend(copt);
   quic_config.SetClientConnectionOptions(copt);
-  if (quartc_session_config.max_time_before_crypto_handshake >
-      QuicTime::Delta::Zero()) {
+  if (quartc_session_config.max_time_before_crypto_handshake_secs > 0) {
     quic_config.set_max_time_before_crypto_handshake(
-        quartc_session_config.max_time_before_crypto_handshake);
+        QuicTime::Delta::FromSeconds(
+            quartc_session_config.max_time_before_crypto_handshake_secs));
   }
-  if (quartc_session_config.max_idle_time_before_crypto_handshake >
-      QuicTime::Delta::Zero()) {
+  if (quartc_session_config.max_idle_time_before_crypto_handshake_secs > 0) {
     quic_config.set_max_idle_time_before_crypto_handshake(
-        quartc_session_config.max_idle_time_before_crypto_handshake);
+        QuicTime::Delta::FromSeconds(
+            quartc_session_config.max_idle_time_before_crypto_handshake_secs));
   }
   if (quartc_session_config.idle_network_timeout > QuicTime::Delta::Zero()) {
     quic_config.SetIdleNetworkTimeout(
@@ -132,7 +224,7 @@ std::unique_ptr<QuartcSession> QuartcFactory::CreateQuartcSession(
   return QuicMakeUnique<QuartcSession>(
       std::move(quic_connection), quic_config,
       quartc_session_config.unique_remote_server_id, perspective,
-      this /*QuicConnectionHelperInterface*/, clock_, std::move(writer));
+      this /*QuicConnectionHelperInterface*/, clock_.get(), std::move(writer));
 }
 
 std::unique_ptr<QuicConnection> QuartcFactory::CreateQuicConnection(
@@ -144,12 +236,28 @@ std::unique_ptr<QuicConnection> QuartcFactory::CreateQuicConnection(
   QuicSocketAddress dummy_address(QuicIpAddress::Any4(), 0 /*Port*/);
   return QuicMakeUnique<QuicConnection>(
       dummy_id, dummy_address, this, /*QuicConnectionHelperInterface*/
-      alarm_factory_ /*QuicAlarmFactory*/, packet_writer, /*owns_writer=*/false,
+      this /*QuicAlarmFactory*/, packet_writer, /*owns_writer=*/false,
       perspective, CurrentSupportedVersions());
 }
 
+QuicAlarm* QuartcFactory::CreateAlarm(QuicAlarm::Delegate* delegate) {
+  return new QuartcAlarm(GetClock(), task_runner_,
+                         QuicArenaScopedPtr<QuicAlarm::Delegate>(delegate));
+}
+
+QuicArenaScopedPtr<QuicAlarm> QuartcFactory::CreateAlarm(
+    QuicArenaScopedPtr<QuicAlarm::Delegate> delegate,
+    QuicConnectionArena* arena) {
+  if (arena != nullptr) {
+    return arena->New<QuartcAlarm>(GetClock(), task_runner_,
+                                   std::move(delegate));
+  }
+  return QuicArenaScopedPtr<QuicAlarm>(
+      new QuartcAlarm(GetClock(), task_runner_, std::move(delegate)));
+}
+
 const QuicClock* QuartcFactory::GetClock() const {
-  return clock_;
+  return clock_.get();
 }
 
 QuicRandom* QuartcFactory::GetRandomGenerator() {
@@ -160,9 +268,10 @@ QuicBufferAllocator* QuartcFactory::GetStreamSendBufferAllocator() {
   return &buffer_allocator_;
 }
 
-std::unique_ptr<QuartcFactory> CreateQuartcFactory(
+std::unique_ptr<QuartcFactoryInterface> CreateQuartcFactory(
     const QuartcFactoryConfig& factory_config) {
-  return std::unique_ptr<QuartcFactory>(new QuartcFactory(factory_config));
+  return std::unique_ptr<QuartcFactoryInterface>(
+      new QuartcFactory(factory_config));
 }
 
 }  // namespace quic
