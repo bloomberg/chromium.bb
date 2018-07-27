808a86a90b673b4504241b56b8cc7c793c6816e1
Aidan Wolter <awolter@chromium.org>
2018-07-27 21:15:25 +0000

    Reland "Fetch and apply Multiroom info in CAOS"
    Relanding go/chromium-cl/1145892 with the fix.
    
    This reverts commit dfb41092bb59b69b915402bd843c414e2208397e [formerly 3f6d28576d01bf54009edee4159c29e9607bab73].
    
    Bug: b/111694183
    Test: Numerous runs of cast_media_unittest
    Change-Id: I46dd922bd99e8a957d90446cfcebd56f405fdc0f
    Reviewed-on: https://chromium-review.googlesource.com/1153504
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Aidan Wolter <awolter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578798}
    Former-commit-id: 63d7be6796d471677e36af70a4e6dee7186622c0

--- chromecast/media/audio/cast_audio_manager_unittest.cc
+++ chromecast/media/audio/cast_audio_manager_unittest.cc
@@ -9,11 +9,15 @@
 #include <utility>
 
 #include "base/bind.h"
-#include "base/test/test_message_loop.h"
+#include "base/test/scoped_task_environment.h"
+#include "chromecast/common/mojom/multiroom.mojom.h"
+#include "chromecast/media/audio/cast_audio_output_stream.h"
 #include "chromecast/media/cma/test/mock_cma_backend.h"
 #include "chromecast/media/cma/test/mock_cma_backend_factory.h"
+#include "chromecast/media/cma/test/mock_multiroom_manager.h"
 #include "media/audio/fake_audio_log_factory.h"
 #include "media/audio/test_audio_thread.h"
+#include "services/service_manager/public/cpp/connector.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -21,6 +25,15 @@ using testing::_;
 using testing::Invoke;
 using testing::Return;
 using testing::StrictMock;
+
+namespace {
+constexpr char kChromecastServiceName[] = "chromecast";
+
+std::unique_ptr<service_manager::Connector> CreateConnector() {
+  service_manager::mojom::ConnectorRequest request;
+  return service_manager::Connector::Create(&request);
+}
+}  // namespace
 
 namespace chromecast {
 namespace media {
@@ -34,26 +47,56 @@ const ::media::AudioParameters kDefaultAudioParams(
 
 class CastAudioManagerTest : public testing::Test {
  public:
-  CastAudioManagerTest() : media_thread_("CastMediaThread") {
+  CastAudioManagerTest()
+      : media_thread_("CastMediaThread"), connector_(CreateConnector()) {
     CHECK(media_thread_.Start());
+
+    // Set the test connector to override interface bindings.
+    service_manager::Connector::TestApi connector_test_api(connector_.get());
+    connector_test_api.OverrideBinderForTesting(
+        service_manager::Identity(kChromecastServiceName),
+        mojom::MultiroomManager::Name_,
+        base::BindRepeating(&CastAudioManagerTest::BindMultiroomManager,
+                            base::Unretained(this)));
 
     backend_factory_ = std::make_unique<MockCmaBackendFactory>();
     audio_manager_ = std::make_unique<CastAudioManager>(
         std::make_unique<::media::TestAudioThread>(), &audio_log_factory_,
         base::BindRepeating(&CastAudioManagerTest::GetCmaBackendFactory,
                             base::Unretained(this)),
+        scoped_task_environment_.GetMainThreadTaskRunner(),
         media_thread_.task_runner(), false);
+    audio_manager_->SetBrowserConnectorForTesting(connector_.get());
   }
 
   ~CastAudioManagerTest() override { audio_manager_->Shutdown(); }
   CmaBackendFactory* GetCmaBackendFactory() { return backend_factory_.get(); }
 
+  // Binds |multiroom_manager_| to the interface requested through the test
+  // connector.
+  void BindMultiroomManager(mojo::ScopedMessagePipeHandle handle) {
+    multiroom_manager_.Bind(std::move(handle));
+  }
+
  protected:
-  base::TestMessageLoop message_loop_;
+  bool OpenStream(::media::AudioOutputStream* stream) {
+    bool success = stream->Open();
+    // TODO(awolter) Determine a better way to ensure that all the tasks are
+    // flushed.
+    media_thread_.FlushForTesting();
+    scoped_task_environment_.RunUntilIdle();
+    media_thread_.FlushForTesting();
+    media_thread_.FlushForTesting();
+    return success;
+  }
+
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   base::Thread media_thread_;
   ::media::FakeAudioLogFactory audio_log_factory_;
   std::unique_ptr<CastAudioManager> audio_manager_;
   std::unique_ptr<MockCmaBackendFactory> backend_factory_;
+  std::unique_ptr<service_manager::Connector> connector_;
+  MockMultiroomManager multiroom_manager_;
 };
 
 TEST_F(CastAudioManagerTest, MakeAudioOutputStreamProxy) {
@@ -74,7 +117,7 @@ TEST_F(CastAudioManagerTest, MakeAudioOutputStreamProxy) {
   ::media::AudioOutputStream* stream =
       audio_manager_->MakeAudioOutputStreamProxy(kDefaultAudioParams,
                                                  std::string());
-  EXPECT_TRUE(stream->Open());
+  EXPECT_TRUE(OpenStream(stream));
   stream->Close();
 }
 
