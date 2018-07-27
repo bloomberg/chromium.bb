f0f29c69388f1eed240f37ffcca997736790d006
Oksana Zhuravlova <oksamyt@chromium.org>
2018-07-27 01:40:24 +0000

    Remove unused device::MotionData and device::OrientationData
    Bug: 861923
    Change-Id: If70fbe3e56d9e6e11841bf694fb45041fdcc7a38
    Reviewed-on: https://chromium-review.googlesource.com/1150624
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578510}
    Former-commit-id: f1b87dada38c5555d81a55ceade4baad4600bb38

--- content/shell/test_runner/test_runner.cc
+++ content/shell/test_runner/test_runner.cc
@@ -38,8 +38,6 @@
 #include "gin/handle.h"
 #include "gin/object_template_builder.h"
 #include "gin/wrappable.h"
-#include "services/device/public/cpp/generic_sensor/motion_data.h"
-#include "services/device/public/cpp/generic_sensor/orientation_data.h"
 #include "third_party/blink/public/platform/modules/service_worker/web_service_worker_registration.h"
 #include "third_party/blink/public/platform/web_data.h"
 #include "third_party/blink/public/platform/web_point.h"
