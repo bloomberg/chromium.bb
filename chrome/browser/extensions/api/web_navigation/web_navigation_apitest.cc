1c816bc4ca9c396522fc084ea7d07b13477833cc
Nasko Oskov <nasko@chromium.org>
2018-07-27 22:36:33 +0000

    Eanble WebNavigationApiTest.Failures test.
    This test was likely flaky due to a race condition in navigation code,
    which has now been fixed. Let's try to enable it and see if the fix has
    helped deflake this test.
    
    Bug: 477840, 746407
    Change-Id: Ib1d449dd34c2d6044a60da38f03ac548179d475c
    Reviewed-on: https://chromium-review.googlesource.com/1153221
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578836}
    Former-commit-id: 461c65103432d20f1ba32bc6f2176dc5c8134ac2

--- chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
+++ chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
@@ -306,14 +306,7 @@ IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, SimpleLoad) {
   ASSERT_TRUE(RunExtensionTest("webnavigation/simpleLoad")) << message_;
 }
 
-// Flaky on Windows, Mac and Linux. See http://crbug.com/477480 (Windows) and
-// https://crbug.com/746407 (Mac, Linux).
-#if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_LINUX)
-#define MAYBE_Failures DISABLED_Failures
-#else
-#define MAYBE_Failures Failures
-#endif
-IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, MAYBE_Failures) {
+IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, Failures) {
   ASSERT_TRUE(RunExtensionTest("webnavigation/failures")) << message_;
 }
 
