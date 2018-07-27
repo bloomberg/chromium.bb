de36cb88b78d147c6cfb15dd5bdc86537b8ab29b
Saman Sami <samans@chromium.org>
2018-07-27 14:08:13 +0000

    Fix missing OOPIFs after GPU crash
    Always submit a CompositorFrame on OnFirstSurfaceActivation even if
    the fallback doesn't change.
    
    Bug: 827242
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I560b5251a188eae2a83db384367e2201ffcce83b
    Reviewed-on: https://chromium-review.googlesource.com/1152209
    Commit-Queue: Saman Sami <samans@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578624}
    Former-commit-id: 757662261f21dc1f44f9258355bcec3271bc5a17

--- cc/layers/surface_layer.cc
+++ cc/layers/surface_layer.cc
@@ -68,8 +68,13 @@ void SurfaceLayer::SetPrimarySurfaceId(const viz::SurfaceId& surface_id,
 }
 
 void SurfaceLayer::SetFallbackSurfaceId(const viz::SurfaceId& surface_id) {
-  if (surface_range_.start() == surface_id)
+  if (surface_range_.start() == surface_id) {
+    // TODO(samans): This was added to fix https://crbug.com/827242. Remove this
+    // once fallback SurfaceIds aren't tied to SurfaceReferences, and
+    // viz::Display can handle missing fallbacks. https://crbug.com/857575
+    layer_tree_host()->SetNeedsCommitWithForcedRedraw();
     return;
+  }
 
   TRACE_EVENT_WITH_FLOW2(
       TRACE_DISABLED_BY_DEFAULT("viz.surface_id_flow"),
