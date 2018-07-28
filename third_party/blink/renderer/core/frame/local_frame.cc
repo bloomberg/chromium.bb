686c878cfde89cade0f937d41eef82b85fcabd31
Ken Buchanan <kenrb@chromium.org>
2018-07-28 16:12:37 +0000

    Force OOPIF intersection observations after viewport update
    Currently when an OOPIF receives an updated viewport intersection rect
    from its parent, it schedules a lifecycle update. However if the OOPIF
    is now throttled it will not calculate new intersection observations
    based on the new viewport rect, and as a consequence any contained
    OOPIFs will not be throttled.
    
    This forces intersection observations whenever an OOPIF has its
    viewport intersection changed.
    
    Bug: 868558
    Change-Id: I18c955b8cc40ec2ebe31d1f0fb35aa5a26f9f447
    Reviewed-on: https://chromium-review.googlesource.com/1153838
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#578922}
    Former-commit-id: eb3c53e64b63a74417d11b566c24081e5f386eb9

--- third_party/blink/renderer/core/frame/local_frame.cc
+++ third_party/blink/renderer/core/frame/local_frame.cc
@@ -1357,8 +1357,10 @@ void LocalFrame::SetViewportIntersectionFromParent(
     const IntRect& viewport_intersection) {
   if (remote_viewport_intersection_ != viewport_intersection) {
     remote_viewport_intersection_ = viewport_intersection;
-    if (View())
+    if (View()) {
+      View()->SetNeedsIntersectionObservation(LocalFrameView::kRequired);
       View()->ScheduleAnimation();
+    }
   }
 }
 
