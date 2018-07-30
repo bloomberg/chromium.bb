2d958f1104dec2b59fb368d64c53cfd67e6c0491
Peter Kasting <pkasting@chromium.org>
2018-07-30 21:27:46 +0000

    Bump theme pack version.
    This was supposed to have been in
    https://chromium-review.googlesource.com/c/chromium/src/+/1152522 .
    
    Bug: 866672
    Change-Id: I4b2076e13b1dc0ae2db0eec51775fb80790b90d7
    TBR: estade
    Reviewed-on: https://chromium-review.googlesource.com/1155486
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579176}
    Former-commit-id: f4837db49d1301b0d769a0b53c9942c6ab001502

--- chrome/browser/themes/browser_theme_pack.cc
+++ chrome/browser/themes/browser_theme_pack.cc
@@ -51,7 +51,7 @@ constexpr int kTallestTabHeight = 41;
 // theme packs that aren't int-equal to this. Increment this number if you
 // change default theme assets or if you need themes to recreate their generated
 // images (which are cached).
-const int kThemePackVersion = 51;
+const int kThemePackVersion = 52;
 
 // IDs that are in the DataPack won't clash with the positive integer
 // uint16_t. kHeaderID should always have the maximum value because we want the
