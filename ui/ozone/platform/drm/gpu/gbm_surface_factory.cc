aa0297c4a8d078fa0019cbc914153d943756d357
Michael Spang <spang@chromium.org>
2018-07-30 18:53:22 +0000

    ozone: drm: Fix vulkan Chrome OS build
    This broke as of 0ec16793d508 [formerly b29640a793cd] ("[ozone/wayland/drm] Implement gbm bo
    wrapper in common.").
    
    Bug: 866564
    Test: compile
    Change-Id: Id85dbb719e39b61f11cb756bbb7212d8bfdf88b3
    Reviewed-on: https://chromium-review.googlesource.com/1151457
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Commit-Queue: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579104}
    Former-commit-id: e8038f34518f62304c663ca558c0860bf2a0542b

--- ui/ozone/platform/drm/gpu/gbm_surface_factory.cc
+++ ui/ozone/platform/drm/gpu/gbm_surface_factory.cc
@@ -178,10 +178,10 @@ scoped_refptr<gfx::NativePixmap> GbmSurfaceFactory::CreateNativePixmapForVulkan(
     return nullptr;
   }
 
-  DCHECK(buffer->AreFdsValid());
-  DCHECK_EQ(buffer->GetFdCount(), 1U);
+  DCHECK(buffer->gbm_bo()->AreFdsValid());
+  DCHECK_EQ(buffer->gbm_bo()->fd_count(), 1U);
 
-  base::ScopedFD vk_image_fd(dup(buffer->GetFd(0)));
+  base::ScopedFD vk_image_fd(dup(buffer->gbm_bo()->GetFd(0)));
   DCHECK(vk_image_fd.is_valid());
 
   VkDmaBufImageCreateInfo dma_buf_image_create_info = {
@@ -190,7 +190,7 @@ scoped_refptr<gfx::NativePixmap> GbmSurfaceFactory::CreateNativePixmapForVulkan(
       .fd = vk_image_fd.release(),
       .format = VK_FORMAT_B8G8R8A8_SRGB,
       .extent = (VkExtent3D){size.width(), size.height(), 1},
-      .strideInBytes = buffer->GetStride(0),
+      .strideInBytes = buffer->gbm_bo()->GetStride(0),
   };
 
   VkResult result =
