0dedefb8bc4db5cfdbe307e650d484ba340e05c8
Andres Medina <medinaandres@chromium.org>
2018-07-31 18:53:17 +0000

    [chromecast] Added virtual-channel to product data
    Added virtual-channel field to be able to filter crash reports by this it.
    
    Bug: internal b/64942730
    Change-Id: I5375d574f6095a689e89d0c543df20da2b281b33
    Reviewed-on: https://chromium-review.googlesource.com/1026150
    Commit-Queue: Andres Medina <medinaandres@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579501}
    Former-commit-id: 554b977f0d0cec4a08c562e55d31e6a1ac2c4c47

--- chromecast/crash/linux/minidump_uploader_unittest.cc
+++ chromecast/crash/linux/minidump_uploader_unittest.cc
@@ -31,6 +31,8 @@ namespace {
 const char kLockfileName[] = "lockfile";
 const char kMetadataName[] = "metadata";
 const char kMinidumpSubdir[] = "minidumps";
+const char kVirtualChannel[] = "virtual-channel";
+const char kVirtualChannelName[] = "a-virtual-chanel";
 
 typedef std::vector<std::unique_ptr<DumpInfo>> DumpList;
 
@@ -40,6 +42,7 @@ std::unique_ptr<PrefService> CreateFakePrefService(bool opt_in) {
   retval->registry()->RegisterBooleanPref(prefs::kOptInStats, opt_in);
   retval->registry()->RegisterStringPref(::metrics::prefs::kMetricsClientID,
                                          "");
+  retval->registry()->RegisterStringPref(kVirtualChannel, kVirtualChannelName);
   return std::move(retval);
 }
 
