58b4c67506262feec0f4b16baf82ae10c1f0621d
Johannes Henkel <johannes@chromium.org>
2018-07-31 23:59:08 +0000

    Port InspectorLogAgent to use InspectorSessionState fields.
    The enabled bit is represented directly, the settings
    are actually thresholds, so they're a map from names to
    doubles. It would be possible to make each threshold a field,
    but I figure that's a bit less convenient to update when
    more thresdholds get added later.
    
    Change-Id: I15324b6b7bac0e72ad501965ca9477a265db38b3
    Reviewed-on: https://chromium-review.googlesource.com/1157277
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Johannes Henkel <johannes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579627}
    Former-commit-id: a7aee6489166dc209c1d07c3b1610f31d9884331

--- third_party/blink/renderer/core/inspector/inspector_log_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_log_agent.cc
@@ -14,13 +14,7 @@
 #include "third_party/blink/renderer/platform/bindings/script_forbidden_scope.h"
 
 namespace blink {
-
 using protocol::Response;
-
-namespace LogAgentState {
-static const char kLogEnabled[] = "logEnabled";
-static const char kLogViolations[] = "logViolations";
-}  // namespace LogAgentState
 
 namespace {
 
@@ -80,10 +74,11 @@ InspectorLogAgent::InspectorLogAgent(
     ConsoleMessageStorage* storage,
     PerformanceMonitor* performance_monitor,
     v8_inspector::V8InspectorSession* v8_session)
-    : enabled_(false),
-      storage_(storage),
+    : storage_(storage),
       performance_monitor_(performance_monitor),
-      v8_session_(v8_session) {}
+      v8_session_(v8_session),
+      enabled_(&agent_state_, /*default_value=*/false),
+      violation_thresholds_(&agent_state_, -1.0) {}
 
 InspectorLogAgent::~InspectorLogAgent() = default;
 
@@ -95,19 +90,23 @@ void InspectorLogAgent::Trace(blink::Visitor* visitor) {
 }
 
 void InspectorLogAgent::Restore() {
-  if (!state_->booleanProperty(LogAgentState::kLogEnabled, false))
+  if (!enabled_.Get())
     return;
-  enable();
-  protocol::Value* config = state_->get(LogAgentState::kLogViolations);
-  if (config) {
-    protocol::ErrorSupport errors;
-    startViolationsReport(
-        protocol::Array<ViolationSetting>::fromValue(config, &errors));
+  InnerEnable();
+  if (violation_thresholds_.IsEmpty())
+    return;
+  auto settings = protocol::Array<ViolationSetting>::create();
+  for (const WTF::String& key : violation_thresholds_.Keys()) {
+    settings->addItem(ViolationSetting::create()
+                          .setName(key)
+                          .setThreshold(violation_thresholds_.Get(key))
+                          .build());
   }
+  startViolationsReport(std::move(settings));
 }
 
 void InspectorLogAgent::ConsoleMessageAdded(ConsoleMessage* message) {
-  DCHECK(enabled_);
+  DCHECK(enabled_.Get());
 
   std::unique_ptr<protocol::Log::LogEntry> entry =
       protocol::Log::LogEntry::create()
@@ -163,13 +162,8 @@ void InspectorLogAgent::ConsoleMessageAdded(ConsoleMessage* message) {
   GetFrontend()->flush();
 }
 
-Response InspectorLogAgent::enable() {
-  if (enabled_)
-    return Response::OK();
+void InspectorLogAgent::InnerEnable() {
   instrumenting_agents_->addInspectorLogAgent(this);
-  state_->setBoolean(LogAgentState::kLogEnabled, true);
-  enabled_ = true;
-
   if (storage_->ExpiredCount()) {
     std::unique_ptr<protocol::Log::LogEntry> expired =
         protocol::Log::LogEntry::create()
@@ -184,15 +178,21 @@ Response InspectorLogAgent::enable() {
   }
   for (size_t i = 0; i < storage_->size(); ++i)
     ConsoleMessageAdded(storage_->at(i));
+}
+
+Response InspectorLogAgent::enable() {
+  if (enabled_.Get())
+    return Response::OK();
+  enabled_.Set(true);
+  InnerEnable();
   return Response::OK();
 }
 
 Response InspectorLogAgent::disable() {
-  if (!enabled_)
+  if (!enabled_.Get())
     return Response::OK();
-  state_->setBoolean(LogAgentState::kLogEnabled, false);
+  enabled_.Clear();
   stopViolationsReport();
-  enabled_ = false;
   instrumenting_agents_->removeInspectorLogAgent(this);
   return Response::OK();
 }
@@ -222,27 +222,27 @@ static PerformanceMonitor::Violation ParseViolation(const String& name) {
 
 Response InspectorLogAgent::startViolationsReport(
     std::unique_ptr<protocol::Array<ViolationSetting>> settings) {
-  if (!enabled_)
+  if (!enabled_.Get())
     return Response::Error("Log is not enabled");
-  state_->setValue(LogAgentState::kLogViolations, settings->toValue());
   if (!performance_monitor_)
     return Response::Error("Violations are not supported for this target");
   performance_monitor_->UnsubscribeAll(this);
+  violation_thresholds_.ClearAll();
   for (size_t i = 0; i < settings->length(); ++i) {
-    PerformanceMonitor::Violation violation =
-        ParseViolation(settings->get(i)->getName());
+    const WTF::String& name = settings->get(i)->getName();
+    double threshold = settings->get(i)->getThreshold();
+    PerformanceMonitor::Violation violation = ParseViolation(name);
     if (violation == PerformanceMonitor::kAfterLast)
       continue;
     performance_monitor_->Subscribe(
-        violation,
-        base::TimeDelta::FromMillisecondsD(settings->get(i)->getThreshold()),
-        this);
+        violation, base::TimeDelta::FromMillisecondsD(threshold), this);
+    violation_thresholds_.Set(name, threshold);
   }
   return Response::OK();
 }
 
 Response InspectorLogAgent::stopViolationsReport() {
-  state_->remove(LogAgentState::kLogViolations);
+  violation_thresholds_.ClearAll();
   if (!performance_monitor_)
     return Response::Error("Violations are not supported for this target");
   performance_monitor_->UnsubscribeAll(this);
