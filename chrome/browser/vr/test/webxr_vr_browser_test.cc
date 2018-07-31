2c707b91ea971f4d5f4cc5d8da40337c8f12f53b
bsheedy <bsheedy@chromium.org>
2018-07-31 02:12:18 +0000

    VR Browser Test Refactor Part 1: Match Java Implementation
    Refactors the VR browser tests to match the newly refactored Java
    instrumentation tests. Main changes include:
    
    * Changing the *TestBase hierarchy to match the *TestFramework Java
      hierarchy.
    * Renaming functions that refer to "presentation" to instead refer to
      "session"
    * Add versions of functions that automatically use the return value of
      GetFirstTabWebContents instead of taking a WebContents reference.
    
    Follow-up CL(s) will do additional cleanup and update the documentation.
    
    Bug: 868143
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:linux_vr;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ifa75c68eafddb9afe1d5f5dee955bd8689c55db7
    Reviewed-on: https://chromium-review.googlesource.com/1155718
    Reviewed-by: Marc-Antoine Ruel <maruel@chromium.org>
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579282}
    Former-commit-id: 03aebcbf8620fb93b4bab5d9aaa8edbc894809c2

---
+++ chrome/browser/vr/test/webxr_vr_browser_test.cc
@@ -0,0 +1,43 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <cstring>
+
+#include "chrome/browser/vr/test/webxr_vr_browser_test.h"
+#include "content/public/browser/web_contents.h"
+#include "content/public/test/browser_test_utils.h"
+
+namespace vr {
+
+void WebXrVrBrowserTestBase::EnterSessionWithUserGesture(
+    content::WebContents* web_contents) {
+  // ExecuteScript runs with a user gesture, so we can just directly call
+  // requestSession instead of having to do the hacky workaround the
+  // instrumentation tests use of actually sending a click event to the canvas.
+  EXPECT_TRUE(content::ExecuteScript(web_contents, "onRequestSession()"));
+}
+
+void WebXrVrBrowserTestBase::EnterSessionWithUserGestureOrFail(
+    content::WebContents* web_contents) {
+  EnterSessionWithUserGesture(web_contents);
+  EXPECT_TRUE(PollJavaScriptBoolean(
+      "sessionInfos[sessionTypes.IMMERSIVE].currentSession != null",
+      kPollTimeoutLong, web_contents));
+}
+
+void WebXrVrBrowserTestBase::EndSession(content::WebContents* web_contents) {
+  EXPECT_TRUE(content::ExecuteScript(
+      web_contents,
+      "sessionInfos[sessionTypes.IMMERSIVE].currentSession.end()"));
+}
+
+void WebXrVrBrowserTestBase::EndSessionOrFail(
+    content::WebContents* web_contents) {
+  EndSession(web_contents);
+  EXPECT_TRUE(PollJavaScriptBoolean(
+      "sessionInfos[sessionTypes.IMMERSIVE].currentSession == null",
+      kPollTimeoutLong, web_contents));
+}
+
+}  // namespace vr
--- chrome/browser/vr/test/xr_browser_test.cc
+++ chrome/browser/vr/test/xr_browser_test.cc
@@ -4,43 +4,272 @@
 
 #include <cstring>
 
+#include "base/bind.h"
+#include "base/callback.h"
+#include "base/command_line.h"
+#include "base/environment.h"
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
+#include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_feature_list.h"
+#include "base/threading/platform_thread.h"
+#include "base/time/time.h"
+#include "build/build_config.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/vr/test/xr_browser_test.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test_utils.h"
+#include "url/gurl.h"
 
 namespace vr {
 
-bool XrBrowserTestBase::XrDeviceFound(content::WebContents* web_contents) {
-  return RunJavaScriptAndExtractBoolOrFail("xrDevice != null", web_contents);
+constexpr base::TimeDelta XrBrowserTestBase::kPollCheckIntervalShort;
+constexpr base::TimeDelta XrBrowserTestBase::kPollCheckIntervalLong;
+constexpr base::TimeDelta XrBrowserTestBase::kPollTimeoutShort;
+constexpr base::TimeDelta XrBrowserTestBase::kPollTimeoutLong;
+constexpr char XrBrowserTestBase::kVrOverrideEnvVar[];
+constexpr char XrBrowserTestBase::kVrOverrideVal[];
+constexpr char XrBrowserTestBase::kVrConfigPathEnvVar[];
+constexpr char XrBrowserTestBase::kVrConfigPathVal[];
+constexpr char XrBrowserTestBase::kVrLogPathEnvVar[];
+constexpr char XrBrowserTestBase::kVrLogPathVal[];
+
+XrBrowserTestBase::XrBrowserTestBase() : env_(base::Environment::Create()) {}
+
+XrBrowserTestBase::~XrBrowserTestBase() = default;
+
+// We need an std::string that is an absolute file path, which requires
+// platform-specific logic since Windows uses std::wstring instead of
+// std::string for FilePaths, but SetVar only accepts std::string
+#ifdef OS_WIN
+#define MAKE_ABSOLUTE(x) \
+  base::WideToUTF8(      \
+      base::MakeAbsoluteFilePath(base::FilePath(base::UTF8ToWide(x))).value())
+#else
+#define MAKE_ABSOLUTE(x) base::MakeAbsoluteFilePath(base::FilePath(x)).value()
+#endif
+
+void XrBrowserTestBase::SetUp() {
+  // Set the environment variable to use the mock OpenVR client.
+  EXPECT_TRUE(env_->SetVar(kVrOverrideEnvVar, MAKE_ABSOLUTE(kVrOverrideVal)));
+  EXPECT_TRUE(
+      env_->SetVar(kVrConfigPathEnvVar, MAKE_ABSOLUTE(kVrConfigPathVal)));
+  EXPECT_TRUE(env_->SetVar(kVrLogPathEnvVar, MAKE_ABSOLUTE(kVrLogPathVal)));
+
+  // Set any command line flags that subclasses have set, e.g. enabling WebVR
+  // and OpenVR support
+  for (const auto& switch_string : append_switches_) {
+    base::CommandLine::ForCurrentProcess()->AppendSwitch(switch_string);
+  }
+  scoped_feature_list_.InitWithFeatures(enable_features_, {});
+
+  InProcessBrowserTest::SetUp();
 }
 
-void XrBrowserTestBase::EnterPresentation(content::WebContents* web_contents) {
-  // ExecuteScript runs with a user gesture, so we can just directly call
-  // requestSession instead of having to do the hacky workaround the
-  // instrumentation tests use of actually sending a click event to the canvas.
-  EXPECT_TRUE(content::ExecuteScript(web_contents, "onRequestSession()"));
+GURL XrBrowserTestBase::GetHtmlTestFile(const std::string& test_name) {
+  return ui_test_utils::GetTestUrl(
+      base::FilePath(FILE_PATH_LITERAL("xr/e2e_test_files/html")),
+#ifdef OS_WIN
+      base::FilePath(base::UTF8ToWide(test_name + ".html")
+#else
+      base::FilePath(test_name + ".html")
+#endif
+                         ));
 }
 
-void XrBrowserTestBase::EnterPresentationOrFail(
-    content::WebContents* web_contents) {
-  EnterPresentation(web_contents);
+content::WebContents* XrBrowserTestBase::GetFirstTabWebContents() {
+  return browser()->tab_strip_model()->GetWebContentsAt(0);
+}
+
+void XrBrowserTestBase::LoadUrlAndAwaitInitialization(const GURL& url) {
+  ui_test_utils::NavigateToURL(browser(), url);
   EXPECT_TRUE(PollJavaScriptBoolean(
-      "sessionInfos[sessionTypes.IMMERSIVE].currentSession != null",
-      kPollTimeoutLong, web_contents));
+      "isInitializationComplete()", kPollTimeoutShort,
+      browser()->tab_strip_model()->GetActiveWebContents()))
+      << "Timed out waiting for JavaScript test initialization.";
 }
 
-void XrBrowserTestBase::ExitPresentation(content::WebContents* web_contents) {
-  EXPECT_TRUE(content::ExecuteScript(
+XrBrowserTestBase::TestStatus XrBrowserTestBase::CheckTestStatus(
+    content::WebContents* web_contents) {
+  std::string result_string =
+      RunJavaScriptAndExtractStringOrFail("resultString", web_contents);
+  bool test_passed =
+      RunJavaScriptAndExtractBoolOrFail("testPassed", web_contents);
+  if (test_passed) {
+    return XrBrowserTestBase::TestStatus::STATUS_PASSED;
+  } else if (!test_passed && result_string == "") {
+    return XrBrowserTestBase::TestStatus::STATUS_RUNNING;
+  }
+  // !test_passed && result_string != ""
+  return XrBrowserTestBase::TestStatus::STATUS_FAILED;
+}
+
+void XrBrowserTestBase::EndTest(content::WebContents* web_contents) {
+  switch (CheckTestStatus(web_contents)) {
+    case XrBrowserTestBase::TestStatus::STATUS_PASSED:
+      break;
+    case XrBrowserTestBase::TestStatus::STATUS_FAILED:
+      FAIL() << "JavaScript testharness failed with result: "
+             << RunJavaScriptAndExtractStringOrFail("resultString",
+                                                    web_contents);
+      break;
+    case XrBrowserTestBase::TestStatus::STATUS_RUNNING:
+      FAIL() << "Attempted to end test in C++ without finishing in JavaScript";
+      break;
+    default:
+      FAIL() << "Received unknown test status.";
+  }
+}
+
+void XrBrowserTestBase::AssertNoJavaScriptErrors(
+    content::WebContents* web_contents) {
+  if (CheckTestStatus(web_contents) ==
+      XrBrowserTestBase::TestStatus::STATUS_FAILED) {
+    FAIL() << "JavaScript test harness failed with result: "
+           << RunJavaScriptAndExtractStringOrFail("resultString", web_contents);
+  }
+}
+
+bool XrBrowserTestBase::PollJavaScriptBoolean(
+    const std::string& bool_expression,
+    const base::TimeDelta& timeout,
+    content::WebContents* web_contents) {
+  // Lambda used because otherwise BindRepeating gets confused about which
+  // version of RunJavaScriptAndExtractBoolOrFail to use.
+  return BlockOnConditionUnsafe(
+      base::BindRepeating(
+          [](XrBrowserTestBase* base, std::string expression,
+             content::WebContents* contents) {
+            return base->RunJavaScriptAndExtractBoolOrFail(expression,
+                                                           contents);
+          },
+          this, bool_expression, web_contents),
+      timeout);
+}
+
+void XrBrowserTestBase::WaitOnJavaScriptStep(
+    content::WebContents* web_contents) {
+  // Make sure we aren't trying to wait on a JavaScript test step without the
+  // code to do so.
+  bool code_available = RunJavaScriptAndExtractBoolOrFail(
+      "typeof javascriptDone !== 'undefined'", web_contents);
+  EXPECT_TRUE(code_available) << "Attempted to wait on a JavaScript test step "
+                              << "without the code to do so. You either forgot "
+                              << "to import webvr_e2e.js or "
+                              << "are incorrectly using a C++ function.";
+
+  // Actually wait for the step to finish
+  bool success =
+      PollJavaScriptBoolean("javascriptDone", kPollTimeoutLong, web_contents);
+
+  // Check what state we're in to make sure javascriptDone wasn't called
+  // because the test failed.
+  XrBrowserTestBase::TestStatus test_status = CheckTestStatus(web_contents);
+  if (!success || test_status == XrBrowserTestBase::TestStatus::STATUS_FAILED) {
+    // Failure states: Either polling failed or polling succeeded, but because
+    // the test failed.
+    std::string reason;
+    if (!success) {
+      reason = "Polling JavaScript boolean javascriptDone timed out.";
+    } else {
+      reason =
+          "Polling JavaScript boolean javascriptDone succeeded, but test "
+          "failed.";
+    }
+
+    std::string result_string =
+        RunJavaScriptAndExtractStringOrFail("resultString", web_contents);
+    if (result_string == "") {
+      reason += " Did not obtain specific reason from testharness.";
+    } else {
+      reason += " Testharness reported failure: " + result_string;
+    }
+    FAIL() << reason;
+  }
+
+  // Reset the synchronization boolean
+  EXPECT_TRUE(content::ExecuteScript(web_contents, "javascriptDone = false"));
+}
+
+void XrBrowserTestBase::ExecuteStepAndWait(const std::string& step_function,
+                                           content::WebContents* web_contents) {
+  EXPECT_TRUE(content::ExecuteScript(web_contents, step_function));
+  WaitOnJavaScriptStep(web_contents);
+}
+
+bool XrBrowserTestBase::BlockOnConditionUnsafe(
+    base::RepeatingCallback<bool()> condition,
+    const base::TimeDelta& timeout,
+    const base::TimeDelta& period) {
+  base::Time start = base::Time::Now();
+  bool successful = false;
+  while (base::Time::Now() - start < timeout) {
+    successful = condition.Run();
+    if (successful) {
+      break;
+    }
+    base::PlatformThread::Sleep(period);
+  }
+  return successful;
+}
+
+bool XrBrowserTestBase::RunJavaScriptAndExtractBoolOrFail(
+    const std::string& js_expression,
+    content::WebContents* web_contents) {
+  bool result;
+  DLOG(ERROR) << "Run javascript: " << js_expression;
+  EXPECT_TRUE(content::ExecuteScriptAndExtractBool(
       web_contents,
-      "sessionInfos[sessionTypes.IMMERSIVE].currentSession.end()"));
+      "window.domAutomationController.send(" + js_expression + ")", &result));
+  return result;
 }
 
-void XrBrowserTestBase::ExitPresentationOrFail(
+std::string XrBrowserTestBase::RunJavaScriptAndExtractStringOrFail(
+    const std::string& js_expression,
     content::WebContents* web_contents) {
-  ExitPresentation(web_contents);
-  EXPECT_TRUE(PollJavaScriptBoolean(
-      "sessionInfos[sessionTypes.IMMERSIVE].currentSession == null",
-      kPollTimeoutLong, web_contents));
+  std::string result;
+  EXPECT_TRUE(content::ExecuteScriptAndExtractString(
+      web_contents,
+      "window.domAutomationController.send(" + js_expression + ")", &result));
+  return result;
+}
+
+void XrBrowserTestBase::EndTest() {
+  EndTest(GetFirstTabWebContents());
+}
+
+void XrBrowserTestBase::AssertNoJavaScriptErrors() {
+  AssertNoJavaScriptErrors(GetFirstTabWebContents());
+}
+
+bool XrBrowserTestBase::PollJavaScriptBoolean(
+    const std::string& bool_expression,
+    const base::TimeDelta& timeout) {
+  return PollJavaScriptBoolean(bool_expression, timeout,
+                               GetFirstTabWebContents());
+}
+
+void XrBrowserTestBase::WaitOnJavaScriptStep() {
+  WaitOnJavaScriptStep(GetFirstTabWebContents());
+}
+
+void XrBrowserTestBase::ExecuteStepAndWait(const std::string& step_function) {
+  ExecuteStepAndWait(step_function, GetFirstTabWebContents());
+}
+
+bool XrBrowserTestBase::RunJavaScriptAndExtractBoolOrFail(
+    const std::string& js_expression) {
+  return RunJavaScriptAndExtractBoolOrFail(js_expression,
+                                           GetFirstTabWebContents());
+}
+
+std::string XrBrowserTestBase::RunJavaScriptAndExtractStringOrFail(
+    const std::string& js_expression) {
+  return RunJavaScriptAndExtractStringOrFail(js_expression,
+                                             GetFirstTabWebContents());
 }
 
 }  // namespace vr
