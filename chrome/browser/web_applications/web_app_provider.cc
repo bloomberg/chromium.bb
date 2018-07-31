9302fe02ed7efe29cce97746c0a57299294d5464
Roger Tawa <rogerta@chromium.org>
2018-07-31 00:24:36 +0000

    Revert "Allow (auto-installed) external web apps"
    This reverts commit c9282df883a4ffe7f92d32e62ec5cdb63637b439 [formerly b43fa6db6b8f6bc2ce0a368f8d6d2765a4078fb1].
    
    Reason for revert:
    Caused three bugs open for flaky tests:
    https://bugs.chromium.org/p/chromium/issues/detail?id=869043
    https://bugs.chromium.org/p/chromium/issues/detail?id=868769
    https://bugs.chromium.org/p/chromium/issues/detail?id=868839
    
    The speculation is that new calls to ScanDirForExternalWebApps() are not being done on the correct thread.
    
    Original change's description:
    > Allow (auto-installed) external web apps
    > 
    > These are the Web App analogs of external extensions, described at
    > https://developer.chrome.com/apps/external_extensions
    > 
    > On start up, *.json files (often but not necessarily named
    > external_extensions.json) are scanned in a number of directories. Prior
    > to this CL, such .json files can install regular extensions (.crx files)
    > or, on Chrome OS, Android apps. New in this CL is being able to install
    > Web Apps (sometimes known as Progressive Web Apps or PWAs).
    > 
    > For example, the chrome::DIR_USER_EXTERNAL_EXTENSIONS path (defined in
    > chrome/common/chrome_paths.h) can correspond to file system directory
    > like "$HOME/.config/chromium/test-user/.config/chromium/External
    > Extensions". Placing a foo_bar.json file containing:
    > 
    > {
    >   "web_app_manifest_url": "https://www.chromestatus.com/static/manifest.json",
    >   "web_app_start_url": "https://www.chromestatus.com/features"
    > }
    > 
    > in that directory will install the Chrome Platform Status web app.
    > 
    > Bug: 855281
    > Change-Id: I716fead81d407076a0dda32f5c0b3e8869351c83
    > Reviewed-on: https://chromium-review.googlesource.com/1127214
    > Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    > Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#578944}
    
    TBR=ortuno@chromium.org,loyso@chromium.org,dominickn@chromium.org,nigeltao@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 855281
    Change-Id: I976928b151910bcc39ea372a6f80d9d833fc8253
    Reviewed-on: https://chromium-review.googlesource.com/1155496
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579247}
    Former-commit-id: e3086692ff021da63831405cd127d14c9b0488ce

--- chrome/browser/web_applications/web_app_provider.cc
+++ chrome/browser/web_applications/web_app_provider.cc
@@ -4,14 +4,8 @@
 
 #include "chrome/browser/web_applications/web_app_provider.h"
 
-#include "base/bind.h"
-#include "base/path_service.h"
-#include "base/task_scheduler/post_task.h"
-#include "build/build_config.h"
-#include "chrome/browser/web_applications/bookmark_apps/external_web_apps.h"
 #include "chrome/browser/web_applications/bookmark_apps/policy/web_app_policy_manager.h"
 #include "chrome/browser/web_applications/web_app_provider_factory.h"
-#include "chrome/common/chrome_paths.h"
 
 namespace web_app {
 
@@ -23,70 +17,9 @@ WebAppProvider* WebAppProvider::Get(Profile* profile) {
 WebAppProvider::WebAppProvider(PrefService* pref_service)
     : web_app_policy_manager_(
           std::make_unique<WebAppPolicyManager>(pref_service)) {
-#if defined(OS_CHROMEOS)
-  // As of mid 2018, only Chrome OS has default web apps or external web apps.
-  // In the future, we might open external web apps to other flavors.
-  ScanForExternalWebApps();
-#endif  // defined(OS_CHROMEOS)
+  // TODO(nigeltao): install default web apps as per http://crbug.com/855281
 }
 
 WebAppProvider::~WebAppProvider() = default;
-
-void WebAppProvider::ScanForExternalWebApps() {
-#if !defined(OS_CHROMEOS)
-  // chrome::DIR_STANDALONE_EXTERNAL_EXTENSIONS is only defined for OS_LINUX.
-  // OS_CHROMEOS is a variant of OS_LINUX.
-#else
-  // For manual testing, it can be useful to s/STANDALONE/USER/, as writing to
-  // "$HOME/.config/chromium/test-user/.config/chromium/External Extensions"
-  // does not require root ACLs, unlike "/usr/share/chromium/extensions".
-  //
-  // TODO(nigeltao): do we want to append a sub-directory name, analogous to
-  // the "arc" in "/usr/share/chromium/extensions/arc" as per
-  // chrome/browser/ui/app_list/arc/arc_default_app_list.cc? Or should we not
-  // sort "system apps" into directories based on their platform (e.g. ARC,
-  // PWA, etc.), and instead examine the JSON contents (e.g. an "activity"
-  // key means ARC, "web_app_start_url" key means PWA, etc.)?
-  base::FilePath dir;
-  if (!base::PathService::Get(chrome::DIR_STANDALONE_EXTERNAL_EXTENSIONS,
-                              &dir)) {
-    LOG(ERROR) << "ScanForExternalWebApps: base::PathService::Get failed";
-    return;
-  }
-
-  auto callback =
-      base::BindOnce(&WebAppProvider::ScanForExternalWebAppsCallback,
-                     weak_ptr_factory_.GetWeakPtr());
-
-  base::PostTaskWithTraits(FROM_HERE,
-                           {base::MayBlock(), base::TaskPriority::BEST_EFFORT},
-                           base::BindOnce(&web_app::ScanDirForExternalWebApps,
-                                          dir, std::move(callback)));
-#endif  // !defined(OS_CHROMEOS)
-}
-
-void WebAppProvider::ScanForExternalWebAppsCallback(
-    std::vector<web_app::PendingAppManager::AppInfo> app_infos) {
-  // TODO(nigeltao/ortuno): we shouldn't need a *policy* manager to get to a
-  // PendingAppManager. As ortuno@ says, "We should refactor PendingAppManager
-  // to no longer be owned by WebAppPolicyManager. I made it that way since at
-  // the time, WebAppPolicyManager was its only client."
-  //
-  // TODO(nigeltao/ortuno): drop the const_cast. Either
-  // WebAppPolicyManager::pending_app_manager should lose the const or we
-  // should acquire a (non-const) PendingAppManager by other means.
-  auto& pending_app_manager = const_cast<web_app::PendingAppManager&>(
-      web_app_policy_manager_->pending_app_manager());
-
-  // TODO(nigeltao/ortuno): confirm that the PendingAppManager callee is
-  // responsible for filtering out already-installed apps.
-  //
-  // TODO(nigeltao/ortuno): does the PendingAppManager care which thread we're
-  // on (e.g. "the UI thread", "the File thread") when we call into it? Do we
-  // need to bounce to another thread from here? Note that, in the long term,
-  // we might not be in the browser process, so there might not be the concept
-  // of "the UI thread".
-  pending_app_manager.ProcessAppOperations(std::move(app_infos));
-}
 
 }  // namespace web_app
