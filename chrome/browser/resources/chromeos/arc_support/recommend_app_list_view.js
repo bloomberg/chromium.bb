2ee265b0637e95e69bd61c2b8ce684d3e7337dba
Yue Cen <rsgingerrs@chromium.org>
2018-07-31 01:18:41 +0000

    Recommend app screen: Add ripple effect to the chip and scrolling shadow effect.
    Bug: 835029
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I677cbf3a3e34a84afabb839a2e8adc61fb27c1bb
    Reviewed-on: https://chromium-review.googlesource.com/1139177
    Commit-Queue: Yue Cen <rsgingerrs@chromium.org>
    Reviewed-by: Wenzhao (Colin) Zang <wzang@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579274}
    Former-commit-id: fb2aa0a73d8dce125307a1bb292b065f6d031333

--- chrome/browser/resources/chromeos/arc_support/recommend_app_list_view.js
+++ chrome/browser/resources/chromeos/arc_support/recommend_app_list_view.js
@@ -11,7 +11,6 @@ function generateContents(appIcon, appTitle, appPackageName) {
   item.classList.add('item');
   item.classList.add('checked');
   item.setAttribute('data-packagename', appPackageName);
-  item.onclick = toggleCheckStatus;
 
   var imagePicker = doc.createElement('div');
   imagePicker.classList.add('image-picker');
@@ -19,6 +18,9 @@ function generateContents(appIcon, appTitle, appPackageName) {
 
   var chip = doc.createElement('div');
   chip.classList.add('chip');
+  chip.addEventListener('mousedown', addRippleCircle);
+  chip.addEventListener('mouseup', toggleCheckStatus);
+  chip.addEventListener('animationend', removeRippleCircle);
   item.appendChild(chip);
 
   var img = doc.createElement('img');
@@ -35,10 +37,32 @@ function generateContents(appIcon, appTitle, appPackageName) {
   recommendAppsContainer.appendChild(item);
 }
 
+// Add a layer on top of the chip to create the ripple effect.
+function addRippleCircle(e) {
+  let chip = e.currentTarget;
+  let item = chip.parentNode;
+  let offsetX = e.pageX - item.offsetLeft;
+  let offsetY = e.pageY - item.offsetTop;
+  chip.style.setProperty('--x', offsetX);
+  chip.style.setProperty('--y', offsetY);
+  chip.innerHTML += '<div class="ripple"></div>';
+}
+
+// After the animation ends, remove the ripple layer.
+function removeRippleCircle(e) {
+  let chip = e.currentTarget;
+  let rippleLayers = chip.querySelectorAll('.ripple');
+  for (let rippleLayer of rippleLayers) {
+    if (rippleLayer.className === 'ripple') {
+      rippleLayer.remove();
+    }
+  }
+}
+
 // Toggle the check status of an app. If an app is selected, add the "checked"
 // class so that the checkmark is visible. Otherwise, remove the checked class.
 function toggleCheckStatus(e) {
-  var item = e.currentTarget;
+  var item = e.currentTarget.parentNode;
   item.classList.toggle('checked');
 }
 
@@ -50,3 +74,19 @@ function getSelectedPackages() {
   }
   return selectedPackages;
 }
+
+// Add the scrolling shadow effect.
+(function() {
+const shadowThreshold = 5;
+var doc = document;
+doc.getElementById('recommend-apps-container').onscroll = function() {
+  doc.getElementById('scroll-top')
+      .classList[this.scrollTop > shadowThreshold ? 'add' : 'remove']('shadow');
+  doc.getElementById('scroll-bottom')
+      .classList
+          [this.scrollHeight - this.clientHeight - this.scrollTop <
+                   shadowThreshold ?
+               'remove' :
+               'add']('shadow');
+};
+})();
