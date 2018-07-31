c2b9fb61cd10ec3fc2147765e438b9ee1bd6de81
Hwanseung Lee <hs1217.lee@samsung.com>
2018-07-31 05:41:17 +0000

    Remove constructed_tree_scopes from CSSStyleSheet
    Previously the field constructed_tree_scopes is added
    to CSSStyleSheet to track addition of constructed CSSStyleSheets
    in tree scopes when adding/removing them from
    TreeScope.moreStyleSheets.
    
    moreStyleSheets already was removed at blink[1].
    so constructed_tree_scopes is not needed anymore.
    
    [1]https://chromium-review.googlesource.com/1125741
    
    Bug: 869244
    Change-Id: Icc0b68393fd100d08ee826d6dbe0859468ee1e23
    Reviewed-on: https://chromium-review.googlesource.com/1155286
    Commit-Queue: Hwanseung Lee <hs1217.lee@samsung.com>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579321}
    Former-commit-id: 3b10abf0f91e61eb30e80d0b78c1dea2c0c6b50c

--- third_party/blink/renderer/core/css/css_style_sheet.cc
+++ third_party/blink/renderer/core/css/css_style_sheet.cc
@@ -214,14 +214,6 @@ void CSSStyleSheet::DidMutateRules() {
         ownerNode()->GetTreeScope());
     if (StyleResolver* resolver = owner->GetStyleEngine().Resolver())
       resolver->InvalidateMatchedPropertiesCache();
-  } else if (!constructed_tree_scopes_.IsEmpty()) {
-    for (auto tree_scope : constructed_tree_scopes_) {
-      tree_scope->GetDocument().GetStyleEngine().SetNeedsActiveStyleUpdate(
-          *tree_scope);
-      if (StyleResolver* resolver =
-              tree_scope->GetDocument().GetStyleEngine().Resolver())
-        resolver->InvalidateMatchedPropertiesCache();
-    }
   }
 }
 
@@ -576,7 +568,6 @@ void CSSStyleSheet::Trace(blink::Visitor* visitor) {
   visitor->Trace(contents_);
   visitor->Trace(owner_node_);
   visitor->Trace(owner_rule_);
-  visitor->Trace(constructed_tree_scopes_);
   visitor->Trace(media_cssom_wrapper_);
   visitor->Trace(child_rule_cssom_wrappers_);
   visitor->Trace(rule_list_cssom_wrapper_);
