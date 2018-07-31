52a921a2e51cf4f166307d849625e8e6a9555644
Eyor Alemayehu <eyor@google.com>
2018-07-31 15:56:58 +0000

    Applied Libassistant Platform API change
    In Libassistant, the ResourceProvider platform interface got
    removed. The alternative way of getting resources is via the
    FileProvider. Updated the implementation of the interfaces to
    comply with API change.
    
    Bug: b/111924222
    Change-Id: I8e29a73f2db0c1bffee39357dbc7e6e4853e84f2
    Reviewed-on: https://chromium-review.googlesource.com/1153563
    Commit-Queue: Eyor Alemayehu <eyor@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579419}
    Former-commit-id: f1392886276c01e33df348a5235b7a2ffde9ba45

--- chromeos/services/assistant/platform/file_provider_impl.cc
+++ chromeos/services/assistant/platform/file_provider_impl.cc
@@ -7,6 +7,8 @@
 #include "base/files/file_util.h"
 #include "base/logging.h"
 #include "base/path_service.h"
+#include "chromeos/grit/chromeos_resources.h"
+#include "ui/base/resource/resource_bundle.h"
 
 namespace chromeos {
 namespace assistant {
@@ -80,6 +82,37 @@ bool FileProviderImpl::WriteSecureFile(const std::string& path,
 
 void FileProviderImpl::CleanAssistantData() {
   base::DeleteFile(root_path_, true);
+}
+
+bool FileProviderImpl::GetResource(uint16_t resource_id, std::string* out) {
+  int chrome_resource_id = -1;
+  switch (resource_id) {
+    case assistant_client::resource_ids::kGeneralError:
+      chrome_resource_id = IDR_ASSISTANT_SPEECH_RECOGNITION_ERROR;
+      break;
+    case assistant_client::resource_ids::kWifiNeedsSetupError:
+    case assistant_client::resource_ids::kWifiNotConnectedError:
+    case assistant_client::resource_ids::kWifiCannotConnectError:
+    case assistant_client::resource_ids::kNetworkConnectingError:
+    // These above do not apply to ChromeOS, but let it fall through to get a
+    // generic error.
+    case assistant_client::resource_ids::kNetworkCannotReachServerError:
+      chrome_resource_id = IDR_ASSISTANT_NO_INTERNET_ERROR;
+      break;
+    case assistant_client::resource_ids::kDefaultHotwordResourceId:
+      chrome_resource_id = IDR_ASSISTANT_HOTWORD_MODEL;
+      break;
+    default:
+      break;
+  }
+
+  if (chrome_resource_id < 0)
+    return false;
+
+  auto data = ui::ResourceBundle::GetSharedInstance().GetRawDataResource(
+      chrome_resource_id);
+  out->assign(data.data(), data.length());
+  return true;
 }
 
 }  // namespace assistant
