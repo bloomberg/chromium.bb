d64aa0ea94bdba0263a0d673665edfd5dee0c894
Olivier Li <olivierli@chromium.org>
2018-07-31 19:50:44 +0000

    Add chrome/chrome_cleaner to the _CheckSpamLogging whitelist.
    The chrome_cleaner logs are separate from Chrome's.
    
    Bug: 830892
    Change-Id: Idb89dfc5f477451b8135e5cbe9cf02a256473aca
    Reviewed-on: https://chromium-review.googlesource.com/1157077
    Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Oliver Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579537}
    Former-commit-id: ec4400b28a913290681684212c9dfccb5ea103ed

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -1500,6 +1500,7 @@ def _CheckSpamLogging(input_api, output_api):
                  r"^chrome[\\\/]browser[\\\/]ui[\\\/]startup[\\\/]"
                      r"startup_browser_creator\.cc$",
                  r"^chrome[\\\/]installer[\\\/]setup[\\\/].*",
+                 r"^chrome[\\\/]chrome_cleaner[\\\/].*",
                  r"chrome[\\\/]browser[\\\/]diagnostics[\\\/]" +
                      r"diagnostics_writer\.cc$",
                  r"^chrome_elf[\\\/]dll_hash[\\\/]dll_hash_main\.cc$",
