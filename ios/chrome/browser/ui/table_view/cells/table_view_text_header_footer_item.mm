641839999c881907aac695c0fa688a563006259a
sczs <sczs@chromium.org>
2018-07-31 18:05:08 +0000

    [ios] Improves ReadingList header insert/delete animation
    - Changes ReadingListTableVC section rowAnimation to
    UITableViewRowAnimationMiddle and UITableViewRowAnimationFade.
    
    - Constraints were breaking while animating, this because of a
    known bug that was fixed in other headers (crbug.com/854117).
    In order to fix this all the padding constraints priority was
    lowered.
    
    Video:
    https://drive.google.com/open?id=1jf1n56P4apUCHjC6ZqZlLe0mCEORsIzG
    
    Bug: 868228
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I5580fe7377f16070fc43b1bc283754d4b00d60ef
    Reviewed-on: https://chromium-review.googlesource.com/1155923
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579479}
    Former-commit-id: eb5e1734c257ce47c84cdda7ed8c5b9ba126f622

--- ios/chrome/browser/ui/table_view/cells/table_view_text_header_footer_item.mm
+++ ios/chrome/browser/ui/table_view/cells/table_view_text_header_footer_item.mm
@@ -76,26 +76,41 @@
     [containerView addSubview:verticalStack];
     [self.contentView addSubview:containerView];
 
-    // If performBatchUpdates is used with this header there might be a need to
-    // change the padding constraints priority. See https://crbug.com/854117 for
-    // more information.
+    // Lower the padding constraints priority. UITableView might try to set
+    // the header view height/width to 0 breaking the constraints. See
+    // https://crbug.com/854117 for more information.
+    NSLayoutConstraint* heightConstraint =
+        [self.contentView.heightAnchor constraintGreaterThanOrEqualToConstant:
+                                           kTableViewHeaderFooterViewHeight];
+    // Set this constraint to UILayoutPriorityDefaultHigh + 1 in order to guard
+    // against some elements that might UILayoutPriorityDefaultHigh to expand
+    // beyond the margins.
+    heightConstraint.priority = UILayoutPriorityDefaultHigh + 1;
+    NSLayoutConstraint* topAnchorConstraint = [containerView.topAnchor
+        constraintGreaterThanOrEqualToAnchor:self.contentView.topAnchor
+                                    constant:kTableViewVerticalSpacing];
+    topAnchorConstraint.priority = UILayoutPriorityDefaultHigh;
+    NSLayoutConstraint* bottomAnchorConstraint = [containerView.bottomAnchor
+        constraintLessThanOrEqualToAnchor:self.contentView.bottomAnchor
+                                 constant:-kTableViewVerticalSpacing];
+    bottomAnchorConstraint.priority = UILayoutPriorityDefaultHigh;
+    NSLayoutConstraint* leadingAnchorConstraint = [containerView.leadingAnchor
+        constraintEqualToAnchor:self.contentView.leadingAnchor
+                       constant:kTableViewHorizontalSpacing];
+    leadingAnchorConstraint.priority = UILayoutPriorityDefaultHigh;
+    NSLayoutConstraint* trailingAnchorConstraint = [containerView.trailingAnchor
+        constraintEqualToAnchor:self.contentView.trailingAnchor
+                       constant:-kTableViewHorizontalSpacing];
+    trailingAnchorConstraint.priority = UILayoutPriorityDefaultHigh;
+
     // Set and activate constraints.
     [NSLayoutConstraint activateConstraints:@[
       // Container Constraints.
-      [self.contentView.heightAnchor constraintGreaterThanOrEqualToConstant:
-                                         kTableViewHeaderFooterViewHeight],
-      [containerView.leadingAnchor
-          constraintEqualToAnchor:self.contentView.leadingAnchor
-                         constant:kTableViewHorizontalSpacing],
-      [containerView.trailingAnchor
-          constraintEqualToAnchor:self.contentView.trailingAnchor
-                         constant:-kTableViewHorizontalSpacing],
-      [containerView.topAnchor
-          constraintGreaterThanOrEqualToAnchor:self.contentView.topAnchor
-                                      constant:kTableViewVerticalSpacing],
-      [containerView.bottomAnchor
-          constraintLessThanOrEqualToAnchor:self.contentView.bottomAnchor
-                                   constant:-kTableViewVerticalSpacing],
+      heightConstraint,
+      topAnchorConstraint,
+      bottomAnchorConstraint,
+      leadingAnchorConstraint,
+      trailingAnchorConstraint,
       [containerView.centerYAnchor
           constraintEqualToAnchor:self.contentView.centerYAnchor],
       // Vertical StackView Constraints.
