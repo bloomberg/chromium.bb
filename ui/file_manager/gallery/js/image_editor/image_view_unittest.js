b6a74784d6a4199178b18e6e528b07d510a7a4c3
Trent Apted <tapted@chromium.org>
2018-07-31 02:47:11 +0000

    Gallery: closure-compile image_view_unittest.js
    Bug: 867700, 860355
    Change-Id: Ifcc4ada00bc459b8cd59a5c2b234af0ef3554c61
    Reviewed-on: https://chromium-review.googlesource.com/1150015
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579292}
    Former-commit-id: 9025984c2c16156a590918081ea1628285044e27

--- ui/file_manager/gallery/js/image_editor/image_view_unittest.js
+++ ui/file_manager/gallery/js/image_editor/image_view_unittest.js
@@ -2,53 +2,77 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/**
+ * Helper to construct testing GalleryItem objects.
+ *
+ * @param {!FileEntry} entry
+ * @param {EntryLocation} locationInfo
+ * @param {Object} metadataItem
+ * @param {ThumbnailMetadataItem=} opt_thumbnailMetadataItem
+ * @param {boolean=} opt_original Whether the entry is original or edited.
+ * @constructor
+ * @extends GalleryItem
+ */
+function MockGalleryItem(
+    entry, locationInfo, metadataItem, opt_thumbnailMetadataItem,
+    opt_original) {
+  let entryLocation = locationInfo || /** @type {!EntryLocation} */ ({});
+  GalleryItem.call(
+      this, entry, entryLocation, /** @type {MetadataItem} */ (metadataItem),
+      opt_thumbnailMetadataItem || null, opt_original || false);
+}
+
+MockGalleryItem.prototype = {
+  __proto__: GalleryItem.prototype
+};
+
 function testImageView() {
   var mockFileSystem = new MockFileSystem('volumeId');
-  var mockEntry = new MockEntry(mockFileSystem, '/test.jpg');
+  var mockEntry = new MockFileEntry(mockFileSystem, '/test.jpg');
 
   // Item has full size cache.
-  var itemWithFullCache = new GalleryItem(mockEntry, null, {}, null, false);
-  itemWithFullCache.contentImage = document.createElement('canvas');
+  var itemWithFullCache = new MockGalleryItem(mockEntry, null, {});
+  itemWithFullCache.contentImage =
+      assertInstanceof(document.createElement('canvas'), HTMLCanvasElement);
   assertEquals(
       ImageView.LoadTarget.CACHED_MAIN_IMAGE,
       ImageView.getLoadTarget(itemWithFullCache, new ImageView.Effect.None()));
 
   // Item with content thumbnail.
-  var itemWithContentThumbnail = new GalleryItem(
-      mockEntry, null, {thumbnail: {url: 'url'}}, null, false);
+  var itemWithContentThumbnail =
+      new MockGalleryItem(mockEntry, null, {thumbnail: {url: 'url'}});
   assertEquals(
       ImageView.LoadTarget.THUMBNAIL,
       ImageView.getLoadTarget(
           itemWithContentThumbnail, new ImageView.Effect.None()));
 
   // Item with external thumbnail.
-  var itemWithExternalThumbnail = new GalleryItem(
-      mockEntry, null, {external: {thumbnailUrl: 'url'}}, null, false);
+  var itemWithExternalThumbnail =
+      new MockGalleryItem(mockEntry, null, {external: {thumbnailUrl: 'url'}});
   assertEquals(
       ImageView.LoadTarget.THUMBNAIL,
       ImageView.getLoadTarget(
           itemWithExternalThumbnail, new ImageView.Effect.None()));
 
-  // Item with external thumbnail but present localy.
-  var itemWithExternalThumbnailPresent = new GalleryItem(
-      mockEntry, null, {external: {thumbnailUrl: 'url', present: true}}, null,
-      false);
+  // Item with external thumbnail but present locally.
+  var itemWithExternalThumbnailPresent = new MockGalleryItem(
+      mockEntry, null, {external: {thumbnailUrl: 'url', present: true}});
   assertEquals(
       ImageView.LoadTarget.MAIN_IMAGE,
       ImageView.getLoadTarget(
           itemWithExternalThumbnailPresent, new ImageView.Effect.None()));
 
   // Item with external thumbnail shown by slide effect.
-  var itemWithExternalThumbnailSlide = new GalleryItem(
-      mockEntry, null, {external: {thumbnailUrl: 'url'}}, null, false);
+  var itemWithExternalThumbnailSlide =
+      new MockGalleryItem(mockEntry, null, {external: {thumbnailUrl: 'url'}});
   assertEquals(
       ImageView.LoadTarget.THUMBNAIL,
       ImageView.getLoadTarget(
           itemWithExternalThumbnailSlide, new ImageView.Effect.Slide(1)));
 
   // Item with external thumbnail shown by zoom to screen effect.
-  var itemWithExternalThumbnailZoomToScreen = new GalleryItem(
-      mockEntry, null, {external: {thumbnailUrl: 'url'}}, null, false);
+  var itemWithExternalThumbnailZoomToScreen =
+      new MockGalleryItem(mockEntry, null, {external: {thumbnailUrl: 'url'}});
   assertEquals(
       ImageView.LoadTarget.THUMBNAIL,
       ImageView.getLoadTarget(
@@ -56,17 +80,16 @@ function testImageView() {
           new ImageView.Effect.ZoomToScreen(new ImageRect(0, 0, 100, 100))));
 
   // Item with external thumbnail shown by zoom effect.
-  var itemWithExternalThumbnailZoom = new GalleryItem(
-      mockEntry, null, {external: {thumbnailUrl: 'url'}}, null, false);
+  var itemWithExternalThumbnailZoom =
+      new MockGalleryItem(mockEntry, null, {external: {thumbnailUrl: 'url'}});
   assertEquals(
       ImageView.LoadTarget.MAIN_IMAGE,
       ImageView.getLoadTarget(
           itemWithExternalThumbnailZoom,
-          new ImageView.Effect.Zoom(0, 0, null)));
+          new ImageView.Effect.Zoom(0, 0, new ImageRect(0, 0, 1, 1))));
 
   // Item without cache/thumbnail.
-  var itemWithoutCacheOrThumbnail = new GalleryItem(
-      mockEntry, null, {}, null, false);
+  var itemWithoutCacheOrThumbnail = new MockGalleryItem(mockEntry, null, {});
   assertEquals(
       ImageView.LoadTarget.MAIN_IMAGE,
       ImageView.getLoadTarget(
@@ -74,27 +97,31 @@ function testImageView() {
 }
 
 function testLoadVideo(callback) {
-  var container = document.createElement('div');
+  var container = assertInstanceof(document.createElement('div'), HTMLElement);
   // We re-use the image-container for video, it starts with this class.
   container.classList.add('image-container');
 
   var viewport = new Viewport(window);
 
-  // Mock volume manager.
-  var volumeManager = {
-    getVolumeInfo: function(entry) {
-      return {volumeType: VolumeManagerCommon.VolumeType.DOWNLOADS};
-    }
-  };
+  /**
+   * Mock volume manager.
+   * @constructor
+   * @extends MetadataProvider
+   */
+  function MockDownloadVolumeManager() {
+    this.__proto__.getVolumeInfo =
+        function(entry) {
+          return {volumeType: VolumeManagerCommon.VolumeType.DOWNLOADS};
+        };
+  }
 
-  var metadataModel = new MetadataModel(volumeManager);
+  var metadataModel = new MetadataModel(new MockDownloadVolumeManager());
   var imageView = new ImageView(container, viewport, metadataModel);
 
   var downloads = new MockFileSystem('file:///downloads');
   var getGalleryItem = function(path) {
-    return new GalleryItem(
-        new MockEntry(downloads, path), {isReadOnly: false}, {size: 100}, {},
-        true /* original */);
+    return new MockGalleryItem(
+        new MockFileEntry(downloads, path), null, {size: 100});
   };
   var item = getGalleryItem('/test.webm');
   var effect = new ImageView.Effect.None();
