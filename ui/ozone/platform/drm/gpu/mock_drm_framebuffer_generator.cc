9b55b539b86a283ea782dcb6038977d6a1e2920e
Michael Spang <spang@chromium.org>
2018-07-31 21:54:26 +0000

    ozone: drm: Inline MockDrmFramebuffer into MockDrmFramebufferGenerator
    Prepare for removing mock framebuffers by moving the class into
    MockDrmFramebufferGenerator. We don't need a mock framebuffer type.
    
    Bug: 869206
    
    Change-Id: I8a909d385f455fde0776e3cf5ad8d24791753e23
    Reviewed-on: https://chromium-review.googlesource.com/1155913
    Commit-Queue: Michael Spang <spang@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579586}
    Former-commit-id: e03a11646747ae389fb6a20146c0259b9c1704f9

--- ui/ozone/platform/drm/gpu/mock_drm_framebuffer_generator.cc
+++ ui/ozone/platform/drm/gpu/mock_drm_framebuffer_generator.cc
@@ -4,10 +4,53 @@
 
 #include "ui/ozone/platform/drm/gpu/mock_drm_framebuffer_generator.h"
 
+#include <drm_fourcc.h>
+
 #include "ui/ozone/platform/drm/common/drm_util.h"
-#include "ui/ozone/platform/drm/gpu/mock_drm_framebuffer.h"
+#include "ui/ozone/platform/drm/gpu/drm_device.h"
 
 namespace ui {
+
+namespace {
+
+uint32_t g_current_framebuffer_id = 1;
+
+class MockDrmFramebuffer : public DrmFramebuffer {
+ public:
+  MockDrmFramebuffer(const gfx::Size& size,
+                     uint32_t format,
+                     uint64_t modifier,
+                     const scoped_refptr<DrmDevice>& drm)
+      : size_(size),
+        format_(format),
+        modifier_(modifier),
+        id_(g_current_framebuffer_id++),
+        opaque_id_(g_current_framebuffer_id++),
+        drm_(drm) {}
+
+  // DrmFramebuffer:
+  uint32_t GetFramebufferId() const override { return id_; }
+  uint32_t GetOpaqueFramebufferId() const override { return opaque_id_; }
+  gfx::Size GetSize() const override { return size_; }
+  uint32_t GetFramebufferPixelFormat() const override { return format_; }
+  uint32_t GetOpaqueFramebufferPixelFormat() const override { return format_; }
+  uint64_t GetFormatModifier() const override { return modifier_; }
+  const DrmDevice* GetDrmDevice() const override { return drm_.get(); }
+
+ private:
+  ~MockDrmFramebuffer() override {}
+
+  gfx::Size size_;
+  uint32_t format_;
+  uint64_t modifier_;
+  uint32_t id_;
+  uint32_t opaque_id_;
+  scoped_refptr<DrmDevice> drm_;
+
+  DISALLOW_COPY_AND_ASSIGN(MockDrmFramebuffer);
+};
+
+}  // namespace
 
 MockDrmFramebufferGenerator::MockDrmFramebufferGenerator() {}
 
@@ -31,7 +74,7 @@ scoped_refptr<DrmFramebuffer> MockDrmFramebufferGenerator::CreateWithModifier(
   if (allocation_failure_)
     return nullptr;
 
-  scoped_refptr<MockDrmFramebuffer> buffer(
+  scoped_refptr<DrmFramebuffer> buffer(
       new MockDrmFramebuffer(size, format, modifier, drm));
 
   return buffer;
