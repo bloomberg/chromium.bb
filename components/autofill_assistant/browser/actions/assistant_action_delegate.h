baa4cea2b353c9d0aa4af583d186df0bfd327529
gogerald <gogerald@google.com>
2018-08-01 18:48:20 +0000

    [Autofill Assistant] Introduce assistant web controller
    Bug: 806868
    Change-Id: I2877c4cfa3241dc6a783311b69f46c27c3b347c5
    Reviewed-on: https://chromium-review.googlesource.com/1158855
    Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579874}
    Former-commit-id: 898777f8f390c155a6c54bd01f64a2fa79a5cb45

--- components/autofill_assistant/browser/actions/assistant_action_delegate.h
+++ components/autofill_assistant/browser/actions/assistant_action_delegate.h
@@ -5,6 +5,11 @@
 #ifndef COMPONENTS_AUTOFILL_ASSISTANT_BROWSER_ACTIONS_ASSISTANT_ACTION_DELEGATE_H_
 #define COMPONENTS_AUTOFILL_ASSISTANT_BROWSER_ACTIONS_ASSISTANT_ACTION_DELEGATE_H_
 
+#include <string>
+#include <vector>
+
+#include "base/callback_forward.h"
+
 namespace autofill_assistant {
 // Assistant action delegate called when processing assistant actions.
 class AssistantActionDelegate {
@@ -13,6 +18,10 @@ class AssistantActionDelegate {
 
   // Show status message on the assistant bottom bar.
   virtual void ShowStatusMessage(const std::string& message) = 0;
+
+  // Click the element given by |selectors| on the web page.
+  virtual void ClickElement(const std::vector<std::string>& selectors,
+                            base::OnceCallback<void(bool)> callback) = 0;
 
  protected:
   AssistantActionDelegate() = default;
---
+++ components/autofill_assistant/browser/assistant_web_controller.h
@@ -0,0 +1,33 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_AUTOFILL_ASSISTANT_BROWSER_ASSISTANT_WEB_CONTROLLER_H_
+#define COMPONENTS_AUTOFILL_ASSISTANT_BROWSER_ASSISTANT_WEB_CONTROLLER_H_
+
+#include <string>
+#include <vector>
+
+#include "base/callback_forward.h"
+#include "base/macros.h"
+
+namespace autofill_assistant {
+// Controller to interact with the web pages.
+class AssistantWebController {
+ public:
+  AssistantWebController();
+  ~AssistantWebController();
+
+  // Perform a moust left button click on the element given by |selectors| and
+  // return the result through callback.
+  // CSS selectors in |selectors| are ordered from top frame to the frame
+  // contains the element and the element.
+  void ClickElement(const std::vector<std::string>& selectors,
+                    base::OnceCallback<void(bool)> callback);
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(AssistantWebController);
+};
+
+}  // namespace autofill_assistant.
+#endif  // COMPONENTS_AUTOFILL_ASSISTANT_BROWSER_ASSISTANT_WEB_CONTROLLER_H_
