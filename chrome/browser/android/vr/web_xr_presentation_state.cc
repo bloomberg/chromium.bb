ee77beb64042023cdf1340bfb1de074f576a4c93
Aldo Culquicondor <acondor@chromium.org>
2018-08-01 13:59:51 +0000

    VR: Extract the WebXR presentation state in a separate file
    Bug: 767282
    Change-Id: Ie0a3951f454f8ee1d8df36d770b758d06d9a6826
    Reviewed-on: https://chromium-review.googlesource.com/1157269
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579791}
    Former-commit-id: 43e1c81e5b2d1ae57c3e3fedcd061a82948c8e75

---
+++ chrome/browser/android/vr/web_xr_presentation_state.cc
@@ -0,0 +1,141 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/android/vr/web_xr_presentation_state.h"
+
+#include "base/callback_helpers.h"
+#include "base/trace_event/trace_event.h"
+#include "gpu/command_buffer/common/mailbox_holder.h"
+#include "gpu/ipc/common/gpu_memory_buffer_impl_android_hardware_buffer.h"
+#include "ui/gl/gl_fence.h"
+#include "ui/gl/gl_image_egl.h"
+
+namespace vr {
+
+WebXrSharedBuffer::WebXrSharedBuffer() = default;
+WebXrSharedBuffer::~WebXrSharedBuffer() = default;
+
+WebXrFrame::WebXrFrame() = default;
+
+WebXrFrame::~WebXrFrame() = default;
+
+bool WebXrFrame::IsValid() {
+  return index >= 0;
+}
+
+void WebXrFrame::Recycle() {
+  DCHECK(!state_locked);
+  index = -1;
+  deferred_start_processing.Reset();
+  recycle_once_unlocked = false;
+  gvr_handoff_fence.reset();
+}
+
+WebXrPresentationState::WebXrPresentationState() {
+  for (auto& frame : frames_storage_) {
+    // Create frames in "idle" state.
+    frame = std::make_unique<WebXrFrame>();
+    idle_frames_.push(frame.get());
+  }
+}
+
+WebXrPresentationState::~WebXrPresentationState() {}
+
+WebXrFrame* WebXrPresentationState::GetAnimatingFrame() {
+  DCHECK(HaveAnimatingFrame());
+  DCHECK(animating_frame_->IsValid());
+  return animating_frame_;
+}
+
+WebXrFrame* WebXrPresentationState::GetProcessingFrame() {
+  DCHECK(HaveProcessingFrame());
+  DCHECK(processing_frame_->IsValid());
+  return processing_frame_;
+}
+
+WebXrFrame* WebXrPresentationState::GetRenderingFrame() {
+  DCHECK(HaveRenderingFrame());
+  DCHECK(rendering_frame_->IsValid());
+  return rendering_frame_;
+}
+
+WebXrPresentationState::FrameIndexType
+WebXrPresentationState::StartFrameAnimating() {
+  DCHECK(!HaveAnimatingFrame());
+  DCHECK(!idle_frames_.empty());
+  animating_frame_ = idle_frames_.front();
+  idle_frames_.pop();
+  animating_frame_->index = next_frame_index_++;
+  return animating_frame_->index;
+}
+
+void WebXrPresentationState::TransitionFrameAnimatingToProcessing() {
+  DCHECK(HaveAnimatingFrame());
+  DCHECK(animating_frame_->IsValid());
+  DCHECK(!animating_frame_->state_locked);
+  DCHECK(!HaveProcessingFrame());
+  processing_frame_ = animating_frame_;
+  animating_frame_ = nullptr;
+}
+
+void WebXrPresentationState::RecycleUnusedAnimatingFrame() {
+  DCHECK(HaveAnimatingFrame());
+  animating_frame_->Recycle();
+  idle_frames_.push(animating_frame_);
+  animating_frame_ = nullptr;
+}
+
+void WebXrPresentationState::TransitionFrameProcessingToRendering() {
+  DCHECK(HaveProcessingFrame());
+  DCHECK(processing_frame_->IsValid());
+  DCHECK(!processing_frame_->state_locked);
+  DCHECK(!HaveRenderingFrame());
+  rendering_frame_ = processing_frame_;
+  processing_frame_ = nullptr;
+}
+
+void WebXrPresentationState::EndFrameRendering() {
+  DCHECK(HaveRenderingFrame());
+  DCHECK(rendering_frame_->IsValid());
+  rendering_frame_->Recycle();
+  idle_frames_.push(rendering_frame_);
+  rendering_frame_ = nullptr;
+}
+
+bool WebXrPresentationState::RecycleProcessingFrameIfPossible() {
+  DCHECK(HaveProcessingFrame());
+  bool can_cancel = !processing_frame_->state_locked;
+  if (can_cancel) {
+    processing_frame_->Recycle();
+    idle_frames_.push(processing_frame_);
+    processing_frame_ = nullptr;
+  } else {
+    processing_frame_->recycle_once_unlocked = true;
+  }
+  return can_cancel;
+}
+
+void WebXrPresentationState::EndPresentation() {
+  TRACE_EVENT0("gpu", __FUNCTION__);
+
+  if (end_presentation_callback) {
+    base::ResetAndReturn(&end_presentation_callback).Run();
+  }
+
+  if (HaveRenderingFrame()) {
+    rendering_frame_->Recycle();
+    idle_frames_.push(rendering_frame_);
+    rendering_frame_ = nullptr;
+  }
+  if (HaveProcessingFrame()) {
+    RecycleProcessingFrameIfPossible();
+  }
+  if (HaveAnimatingFrame()) {
+    RecycleUnusedAnimatingFrame();
+  }
+
+  last_ui_allows_sending_vsync = false;
+}
+
+}  // namespace vr
