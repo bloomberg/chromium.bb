1854820b12bd4dafc7e2cb1081edb6b39bdf7071
Raymond Toy <rtoy@chromium.org>
2018-08-01 01:35:16 +0000

    Remove unused methods
    To support deprecating the AudioParam value setter in favor of
    setValueAtTime, some methods were added.  The deprecation has happened
    and these are no longer needed.
    
    Bug: 764396
    Test: 
    Change-Id: I6e296e17582a078cacc01f91fb56af812079db38
    Reviewed-on: https://chromium-review.googlesource.com/1157213
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579660}
    Former-commit-id: a751300d83b471921c752bbc2fe4f117d019eadb

--- third_party/blink/renderer/modules/webaudio/audio_param_timeline.h
+++ third_party/blink/renderer/modules/webaudio/audio_param_timeline.h
@@ -445,22 +445,6 @@ class AudioParamTimeline {
                            size_t end_frame,
                            unsigned write_index);
 
-  // TODO(crbug.com/764396): Remove these two methods when the bug is fixed.
-
-  // |EventAtFrame| finds the current event that would run at the specified
-  // |frame|. The first return value is true if a setValueAtTime call would
-  // overlap some ongoing event.  The second return value is the index of the
-  // current event. The second value must be ignored if the first value is
-  // false.
-  std::tuple<bool, size_t> EventAtFrame(size_t frame, float sample_rate) const;
-
-  // Prints a console warning that a call to the AudioParam value setter
-  // overlaps the event at |event_index|.  |param_name| is the name of the
-  // AudioParam where the where this is happening.
-  void WarnSetterOverlapsEvent(String param_name,
-                               size_t event_index,
-                               BaseAudioContext&) const;
-
   // When cancelling events, remove the items from |events_| starting
   // at the given index.  Update |new_events_| too.
   void RemoveCancelledEvents(size_t first_event_to_remove);
