2dfe8af38423c416f39b5a97f9fd10faadaeaaa2
Alex Moshchuk <alexmos@chromium.org>
2018-08-01 20:40:15 +0000

    Remove temporary DumpWithoutCrashing to InitRenderFrameProxy()
    We have collected the data we need and found the likely cause of
    crashes.
    
    Bug: 794625
    Change-Id: I1e730b27e17b916bec6bbbe20b7734f2b81481db
    Reviewed-on: https://chromium-review.googlesource.com/1158721
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579925}
    Former-commit-id: c79afd77ff3e596039645afc16dc806309f8c61c

--- content/browser/frame_host/render_frame_proxy_host.cc
+++ content/browser/frame_host/render_frame_proxy_host.cc
@@ -8,8 +8,6 @@
 #include <vector>
 
 #include "base/callback.h"
-#include "base/debug/alias.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/lazy_instance.h"
 #include "content/browser/bad_message.h"
 #include "content/browser/blob_storage/chrome_blob_storage_context.h"
@@ -202,32 +200,6 @@ bool RenderFrameProxyHost::InitRenderFrameProxy() {
   if (frame_tree_node_->opener()) {
     opener_routing_id = frame_tree_node_->render_manager()->GetOpenerRoutingID(
         site_instance_.get());
-  }
-
-  // Temporary debugging code for https://crbug.com/794625 to see if we're ever
-  // sending a message to create a RenderFrameProxy when one already exists.
-  // TODO(alexmos): Remove after the investigation.
-  if (GetProcess()->GetProcess().IsValid() &&
-      GetProcess()->GetProcess().Pid() ==
-          pid_of_last_create_message_for_debugging_) {
-    SiteInstanceImpl* site_instance =
-        static_cast<SiteInstanceImpl*>(site_instance_.get());
-    GURL site_url(site_instance->GetSiteURL());
-    DEBUG_ALIAS_FOR_GURL(site_url_copy, site_url);
-    GURL current_rfh_site_url(frame_tree_node_->render_manager()
-                                  ->current_frame_host()
-                                  ->GetSiteInstance()
-                                  ->GetSiteURL());
-    DEBUG_ALIAS_FOR_GURL(current_rfh_site_url_copy, current_rfh_site_url);
-
-    int routing_id_copy = routing_id_;
-    base::debug::Alias(&routing_id_copy);
-    int parent_routing_id_copy = parent_routing_id;
-    base::debug::Alias(&parent_routing_id_copy);
-    int active_frame_count = site_instance->active_frame_count();
-    base::debug::Alias(&active_frame_count);
-
-    base::debug::DumpWithoutCrashing();
   }
 
   int view_routing_id = frame_tree_node_->frame_tree()
