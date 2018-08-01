79b1101282fece3ac4ef0ffaf6a105e719895bc2
Alexis Hetu <sugoi@google.com>
2018-08-01 21:30:54 +0000

    Reland "Reland "Switching all ui/gl dependent tests to SwiftShader""
    This is a reland of b4a3a0604dd2d9d67d0c7dd490ed8db559154fd7 [formerly 047ba2f8a02d6323b1a175f23f6b7f3495e46613]
    
    TBR=dcastagna@chromium.org,piman@chromium.org,ericrk@chromium.org,danakj@chromium.org
    
    Original change's description:
    > Reland "Switching all ui/gl dependent tests to SwiftShader"
    >
    > This is a reland of 818c5eabc657b706cf9dc9791251667e00440381 [formerly 9f1eb0bad804bf4ebfe8202fe91413450f9cf062]
    >
    > The only change from the original cl is in cc/test/layer_tree_test.cc,
    > so TBRing reviewers who approved other parts of this cl.
    > TBR=dcastagna@chromium.org,piman@chromium.org,ericrk@chromium.org
    >
    > Original change's description:
    > > Switching all ui/gl dependent tests to SwiftShader
    > >
    > > This cl's switches cc_unittests and viz_unittests from OSMesa
    > > to SwiftShader and fixes other issues related to that change.
    > >
    > > Bug chromium:848035
    > >
    > > Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    > > Change-Id: Ib1b58e02d4df4a687ef7be57284c06097bdeca94
    > > Reviewed-on: https://chromium-review.googlesource.com/1070490
    > > Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    > > Reviewed-by: danakj <danakj@chromium.org>
    > > Reviewed-by: Antoine Labour <piman@chromium.org>
    > > Reviewed-by: Eric Karl <ericrk@chromium.org>
    > > Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#572442}
    >
    > Change-Id: I85f723ed8a8f7d981a9652cd7979c54d0f1248ae
    > Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    > Reviewed-on: https://chromium-review.googlesource.com/1125899
    > Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    > Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    > Reviewed-by: danakj <danakj@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#572623}
    
    Change-Id: I8e0a113f356ea997ee9dff45e3cd0d465f349006
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Reviewed-on: https://chromium-review.googlesource.com/1127419
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579950}
    Former-commit-id: b07b69524bd7b47c023c6f5f8930fd70272a337e

--- ui/gl/gl_implementation.cc
+++ ui/gl/gl_implementation.cc
@@ -20,6 +20,7 @@
 #include "base/strings/string_util.h"
 #include "build/build_config.h"
 #include "ui/gl/gl_bindings.h"
+#include "ui/gl/gl_features.h"
 #include "ui/gl/gl_gl_api_implementation.h"
 #include "ui/gl/gl_version_info.h"
 
@@ -121,9 +122,9 @@ GLImplementation GetNamedGLImplementation(const std::string& name) {
 }
 
 GLImplementation GetSoftwareGLImplementation() {
-#if (defined(OS_WIN) ||                                                     \
-     (defined(OS_LINUX) && !defined(OS_CHROMEOS) && !defined(USE_OZONE)) || \
-     (defined(OS_MACOSX) && defined(USE_EGL)))
+#if (BUILDFLAG(ENABLE_SWIFTSHADER) &&         \
+     (defined(OS_WIN) || defined(OS_LINUX) || \
+      (defined(OS_MACOSX) && defined(USE_EGL))))
   return kGLImplementationSwiftShaderGL;
 #else
   return kGLImplementationOSMesaGL;
