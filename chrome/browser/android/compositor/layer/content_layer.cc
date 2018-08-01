57372e2dceeee4acccda194846b7e0e7653fbe3b
Yusuf Ozuysal <yusufo@chromium.org>
2018-08-01 17:58:40 +0000

    Revert "Fix animation jankiness after selecting the current NTP in Android tab switcher"
    This reverts commit d34ec61c4d1d67ffd37ae10f087070b65ba5dc90 [formerly 734d2825be76c8b605a03cb55e5739905cc2622f].
    
    Reason for revert: The CL this CL was fixing was already revertedand looks like this is causing other regressions.
    
    Original change's description:
    > Fix animation jankiness after selecting the current NTP in Android tab switcher
    >
    > If you open an NTP on Android, open the tab switcher, and then focus the NTP,
    > there is often some jankiness at the end of the animation where the page very
    > quickly appears to jump back-and-forth by a few pixels.
    >
    > It turns out that this happens when we switch from TabListSceneLayer (what we
    > use to display the tab switcher) to StaticTabSceneLayer (what we use to show
    > the tab during normal browsing): for the NTP, we never use the live layer in
    > the tab switcher, so we have to use the static layer. When we switch to
    > StaticTabSceneLayer, it (sometimes?) still uses the static layer very briefly,
    > but displays it at not quite the right size, which causes the jumpiness before
    > we switch back to the live layer.
    >
    > The fix is to make ContentLayer clip the static layer to the value of
    > ComputedSize() in the case where should_clip == false.
    >
    > Bug: 748648
    > Change-Id: I2999ee1be91681d692e381a9aff230567ace20fc
    > Reviewed-on: https://chromium-review.googlesource.com/1114349
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Commit-Queue: Ryan Landay <rlanday@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#570974}
    
    TBR=dtrainor@chromium.org,rlanday@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 748648,  867868, 865395
    Change-Id: I9f092f7906a3b2d96480e3a797292d96dda22074
    Reviewed-on: https://chromium-review.googlesource.com/1151568
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579857}
    Former-commit-id: 4a1c70a7e3d1952a5864022cb944f3219551e26a

--- chrome/browser/android/compositor/layer/content_layer.cc
+++ chrome/browser/android/compositor/layer/content_layer.cc
@@ -91,17 +91,10 @@ void ContentLayer::SetProperties(int id,
   }
   if (static_layer.get()) {
     static_layer->layer()->SetIsDrawable(true);
-
-    if (should_clip) {
+    if (should_clip)
       static_layer->Clip(clip);
-    } else {
-      // Clipping to the computed size of the layer fixes an issue where the tab
-      // contents briefly jump back-and-forth when transitioning from a
-      // TabListSceneLayer to a StaticTabSceneLayer.
-      const gfx::Size& size = ComputeSize(id);
-      static_layer->Clip(gfx::Rect(0, 0, size.width(), size.height()));
-    }
-
+    else
+      static_layer->ClearClip();
     SetOpacityOnLeaf(static_layer->layer(), static_opacity);
 
     cc::FilterOperations static_filter_operations;
