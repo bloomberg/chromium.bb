d84f7bc4e6dc02a5dc01535008d560d812942a13
Yuqian Li <liyuqian@google.com>
2018-08-01 14:10:30 +0000

    Add SK_SUPPORT_LEGACY_AAA_CHOICE
    For https://skia-review.googlesource.com/c/skia/+/144412
    
    Change-Id: I9c1f983bb7020a3335bf58739cdaf343a7f983db
    Reviewed-on: https://chromium-review.googlesource.com/1157516
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#579795}
    Former-commit-id: b1694bf720f8397abb4b9c362e0497652a3b3e44

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -193,6 +193,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_VULKAN_INTERFACE
 #endif
 
+#ifndef SK_SUPPORT_LEGACY_AAA_CHOICE
+#define SK_SUPPORT_LEGACY_AAA_CHOICE
+#endif
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
