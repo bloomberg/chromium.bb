5053a10675a76fe5cdb80530bd5cdccc55739f8a
Chris Ching <chingcodes@google.com>
2018-08-01 12:18:08 -0600

    signing: Add BiosSigner
    Class responsible for signing BIOS with futility
    
    BUG=chromium:813829
    TEST=./firmware_unittest
    
    Change-Id: I75f74874f493bdf3cc3f6c9a7cbf1718b6f127ab
    Reviewed-on: https://chromium-review.googlesource.com/1155817
    Commit-Ready: Chris Ching <chingcodes@chromium.org>
    Tested-by: Chris Ching <chingcodes@chromium.org>
    Reviewed-by: Lann Martin <lannm@chromium.org>

--- signing/lib/firmware.py
+++ signing/lib/firmware.py
@@ -10,6 +10,61 @@ from __future__ import print_function
 from chromite.signing.lib.signer import FutilitySigner
 
 
+class BiosSigner(FutilitySigner):
+  """Sign bios.bin file using futility."""
+
+  _required_keys_private = ('firmware',)
+  _required_keys_public = ('kernel',)
+  _required_keyblocks = ('firmware',)
+
+  def __init__(self, bios_version=1, sig_dir=None, sig_id=None):
+    """Init BiosSigner
+
+    Args:
+      bios_version: Version of the bios being signed
+      sig_dir: Signature directory (aka loem dir)
+      sig_id: Signature ID (aka loem id)
+    """
+    self.version = bios_version
+    self.sig_dir = sig_dir
+    self.sig_id = sig_id
+
+  def GetFutilityArgs(self, keyset, input_name, output_name):
+    fw_key = keyset.GetKey('firmware')
+    fw_keyblock = keyset.GetKeyblock('firmware')
+
+
+    kernel_key = keyset.keys['kernel']
+
+    args = ['sign',
+            '--type', 'bios',
+            '--signprivate', fw_key.private,
+            '--keyblock', fw_keyblock.filename,
+            '--kernelkey', kernel_key.public,
+            '--version', str(self.version)]
+
+    # Add developer key arguments
+    dev_fw_key = keyset.GetKey('dev_firmware')
+    dev_fw_keyblock = keyset.GetKeyblock('dev_firmware')
+
+    if dev_fw_key is not None and dev_fw_keyblock is not None:
+      args += ['--devsign', dev_fw_key.private,
+               '--devkeyblock', dev_fw_keyblock.filename]
+    else:
+      # Fallback to fw_key if device key not found (legacy, still needed?)
+      args += ['--devsign', fw_key.private,
+               '--devkeyblock', fw_keyblock.filename]
+
+    # Add loem related arguments
+    if self.sig_dir is not None and self.sig_id is not None:
+      args += ['--loemdir', self.sig_dir,
+               '--loemid', self.sig_id]
+
+    # Add final input/output arguments
+    args += [input_name, output_name]
+    return args
+
+
 class ECSigner(FutilitySigner):
   """Sign EC bin file."""
 
--- signing/lib/firmware_unittest.py
+++ signing/lib/firmware_unittest.py
@@ -7,9 +7,75 @@
 
 from __future__ import print_function
 
+import os
+
 from chromite.lib import cros_test_lib
 from chromite.signing.lib import firmware
+from chromite.signing.lib import keys
+from chromite.signing.lib import keys_unittest
 from chromite.signing.lib import signer_unittest
+
+
+class TestBiosSigner(cros_test_lib.RunCommandTestCase,
+                     cros_test_lib.TempDirTestCase):
+  """Test BiosSigner."""
+
+  def testGetCmdArgs(self):
+    bs = firmware.BiosSigner()
+    ks = signer_unittest.KeysetFromSigner(bs, self.tempdir)
+
+    self.assertListEqual(bs.GetFutilityArgs(ks, 'foo', 'bar'),
+                         ['sign',
+                          '--type', 'bios',
+                          '--signprivate', ks.keys['firmware'].private,
+                          '--keyblock', ks.keyblocks['firmware'].filename,
+                          '--kernelkey', ks.keys['kernel'].public,
+                          '--version', str(bs.version),
+                          '--devsign', ks.keys['firmware'].private,
+                          '--devkeyblock', ks.keyblocks['firmware'].filename,
+                          'foo', 'bar'])
+
+  def testGetCmdArgsWithDevKeys(self):
+    bs = firmware.BiosSigner()
+    ks = signer_unittest.KeysetFromSigner(bs, self.tempdir)
+
+    # Add 'dev_firmware' keys and keyblock
+    ks.AddKey(keys.KeyPair('dev_firmware', keydir=self.tempdir))
+    keys_unittest.CreateDummyPrivateKey(ks.GetKey('dev_firmware'))
+
+    ks.AddKeyblock(ks.keys['dev_firmware'].CreateKeyblock())
+    keys_unittest.CreateDummyKeyblock(ks.GetKeyblock('dev_firmware'))
+
+    self.assertListEqual(bs.GetFutilityArgs(ks, 'foo', 'bar'),
+                         ['sign',
+                          '--type', 'bios',
+                          '--signprivate', ks.keys['firmware'].private,
+                          '--keyblock', ks.keyblocks['firmware'].filename,
+                          '--kernelkey', ks.keys['kernel'].public,
+                          '--version', str(bs.version),
+                          '--devsign', ks.keys['dev_firmware'].private,
+                          '--devkeyblock',
+                          ks.keyblocks['dev_firmware'].filename,
+                          'foo', 'bar'])
+
+  def testGetCmdArgsWithSig(self):
+    loem_dir = os.path.join(self.tempdir, 'loem')
+    loem_id = 'loem1'
+    bs = firmware.BiosSigner(sig_dir=loem_dir, sig_id=loem_id)
+    ks = signer_unittest.KeysetFromSigner(bs, self.tempdir)
+
+    self.assertListEqual(bs.GetFutilityArgs(ks, 'foo', 'bar'),
+                         ['sign',
+                          '--type', 'bios',
+                          '--signprivate', ks.keys['firmware'].private,
+                          '--keyblock', ks.keyblocks['firmware'].filename,
+                          '--kernelkey', ks.keys['kernel'].public,
+                          '--version', str(bs.version),
+                          '--devsign', ks.keys['firmware'].private,
+                          '--devkeyblock', ks.keyblocks['firmware'].filename,
+                          '--loemdir', loem_dir,
+                          '--loemid', loem_id,
+                          'foo', 'bar'])
 
 
 class TestECSigner(cros_test_lib.RunCommandTestCase,
