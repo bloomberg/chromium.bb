e6e3a1e107d187e027eff780b437cf7bbd4f77e8
Adithya Srinivasan <adithyas@chromium.org>
2018-08-02 22:16:29 +0000

    Mojofy FileSystem
    - Mojofies and removes all messages in file_system_messages.h.
    - Replaces FileAPIMessageFilter with FileSystemDispatcherHost.
    - Adds mojom types for FileSystemInfo and FileSystemType and adds
      type converters to convert these types to storage::FileSystemInfo
      and storage FileSystemInfo respectively. The storage types will be
      replaced with the mojom types in follow up CLs.
    
    Change-Id: I82aaf9dcb04149880e68238540766f21771d2b21
    Bug: 787281
    Reviewed-on: https://chromium-review.googlesource.com/1125316
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580346}
    Former-commit-id: 104d9bf8175d3ec3feb862cee68808c57ce575af

--- content/public/test/test_fileapi_operation_waiter.cc
+++ content/public/test/test_fileapi_operation_waiter.cc
@@ -5,109 +5,36 @@
 #include "content/public/test/test_fileapi_operation_waiter.h"
 
 #include "base/bind_helpers.h"
-#include "base/lazy_instance.h"
-#include "base/observer_list.h"
-#include "content/public/browser/browser_thread.h"
-#include "storage/browser/fileapi/file_system_context.h"
-#include "storage/browser/fileapi/sandbox_file_system_backend_delegate.h"
 
 namespace content {
 
-using storage::FileSystemContext;
-using storage::FileSystemURL;
-using storage::FileUpdateObserver;
+TestFileapiOperationWaiter::TestFileapiOperationWaiter() = default;
 
-namespace {
+TestFileapiOperationWaiter::~TestFileapiOperationWaiter() = default;
 
-// Because of how fileapi internally creates copies of its observer lists,
-// removing an observer is not a supported operation. So to support temporary,
-// test-style observers, we create one long-lived global observer instance that
-// dispatches to a list of short-lived observers.
-//
-// This object operates on the UI thread, though it registers itself as an
-// observer on the IO thread.
-class FileUpdateObserverMultiplexer : public FileUpdateObserver {
- public:
-  FileUpdateObserverMultiplexer() {}
-
-  void AddObserver(FileSystemContext* context, FileUpdateObserver* observer) {
-    DCHECK_CURRENTLY_ON(BrowserThread::UI);
-
-    // On first initialization, install ourself as an observer. We never
-    // uninstall, because we expect to leak.
-    if (!context_) {
-      // Currently we only listen to kFileSystemTypeTemporary; it should be fine
-      // to add other filesystem types as needed.
-      context_ = context;
-      base::Closure task = base::Bind(
-          &storage::SandboxFileSystemBackendDelegate::AddFileUpdateObserver,
-          base::Unretained(context_->sandbox_delegate()),
-          storage::kFileSystemTypeTemporary, base::Unretained(this),
-          base::RetainedRef(
-              BrowserThread::GetTaskRunnerForThread(BrowserThread::UI)));
-      BrowserThread::PostTask(BrowserThread::IO, FROM_HERE, std::move(task));
-    }
-
-    CHECK_EQ(context, context_) << "Multiprofile is not implemented";
-
-    observers_.AddObserver(observer);
-  }
-
-  void RemoveObserver(FileUpdateObserver* observer) {
-    DCHECK_CURRENTLY_ON(BrowserThread::UI);
-    observers_.RemoveObserver(observer);
-  }
-
-  // FileUpdateObserver overrides:
-  void OnStartUpdate(const FileSystemURL& url) override {
-    DCHECK_CURRENTLY_ON(BrowserThread::UI);
-    for (auto& observer : observers_)
-      observer.OnStartUpdate(url);
-  }
-  void OnUpdate(const FileSystemURL& url, int64_t delta) override {
-    DCHECK_CURRENTLY_ON(BrowserThread::UI);
-    for (auto& observer : observers_)
-      observer.OnUpdate(url, delta);
-  }
-  void OnEndUpdate(const FileSystemURL& url) override {
-    DCHECK_CURRENTLY_ON(BrowserThread::UI);
-    for (auto& observer : observers_)
-      observer.OnEndUpdate(url);
-  }
-
- private:
-  FileSystemContext* context_ = nullptr;
-  base::ObserverList<FileUpdateObserver> observers_;
-  DISALLOW_COPY_AND_ASSIGN(FileUpdateObserverMultiplexer);
-};
-
-static base::LazyInstance<FileUpdateObserverMultiplexer>::Leaky g_multiplexer =
-    LAZY_INSTANCE_INITIALIZER;
-
-}  // namespace
-
-TestFileapiOperationWaiter::TestFileapiOperationWaiter(
-    FileSystemContext* context) {
-  g_multiplexer.Get().AddObserver(context, this);
-}
-
-TestFileapiOperationWaiter::~TestFileapiOperationWaiter() {
-  g_multiplexer.Get().RemoveObserver(this);
-}
-
-void TestFileapiOperationWaiter::WaitForEndUpdate() {
+void TestFileapiOperationWaiter::WaitForOperationToFinish() {
   run_loop_.Run();
 }
 
-void TestFileapiOperationWaiter::OnStartUpdate(const FileSystemURL& url) {
-  did_start_update_ = true;
+void TestFileapiOperationWaiter::DidCreate(base::File::Error error_code) {
+  run_loop_.Quit();
 }
 
-void TestFileapiOperationWaiter::OnUpdate(const FileSystemURL& url,
-                                          int64_t delta) {}
+void TestFileapiOperationWaiter::ResultsRetrieved(
+    std::vector<filesystem::mojom::DirectoryEntryPtr> entries,
+    bool has_more) {
+  // blink::mojom::FileSystemManager::ReadDirectory isn't being called by tests
+  // right now, so this callback shouldn't be called.
+  NOTREACHED();
+}
 
-void TestFileapiOperationWaiter::OnEndUpdate(const FileSystemURL& url) {
+void TestFileapiOperationWaiter::ErrorOccurred(base::File::Error error_code) {
   run_loop_.Quit();
+}
+
+void TestFileapiOperationWaiter::DidWrite(int64_t byte_count, bool complete) {
+  if (complete)
+    run_loop_.Quit();
 }
 
 }  // namespace content
