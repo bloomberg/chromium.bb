47edf8633b353db0af2d39e57600d0b6dea0d426
Ned Nguyen <nednguyen@google.com>
2018-08-02 15:19:43 +0000

    Revert "Add logging and chrome_utils directories"
    This reverts commit 416b48f89da688fba5b7a076f362389347afea39 [formerly e63be647a452a23a177b5c4ac33d2603abaea132].
    
    Reason for revert: breaking generate_build_files on chromium.perf/Win Builder Perf
    
    Original change's description:
    > Add logging and chrome_utils directories
    > 
    > Bug: 830892
    > Change-Id: I8c121dff724dea526a7cc7ce198da51cc1b8d045
    > Reviewed-on: https://chromium-review.googlesource.com/1158719
    > Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    > Commit-Queue: Oliver Li <olivierli@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#580163}
    
    TBR=pkasting@chromium.org,joenotcharles@chromium.org,dullweber@chromium.org,olivierli@chromium.org
    
    Change-Id: I295b58fdf9300e681b985c93b6f68b0ec0702da0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 830892
    Reviewed-on: https://chromium-review.googlesource.com/1160881
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#580181}
    Former-commit-id: ec57bf7827e47a8db197b6a57f53d42f88b0fa2d

--- chrome/chrome_cleaner/http/mock_http_agent_factory.h
+++ chrome/chrome_cleaner/http/mock_http_agent_factory.h
@@ -126,7 +126,7 @@ class MockHttpAgentFactory : public HttpAgentFactory {
   explicit MockHttpAgentFactory(MockHttpAgentConfig* config);
 
   // HttpAgentFactory:
-  std::unique_ptr<chrome_cleaner::HttpAgent> CreateHttpAgent() const override;
+  std::unique_ptr<chrome_cleaner::HttpAgent> CreateHttpAgent() override;
 
  private:
   MockHttpAgentConfig* config_{nullptr};
