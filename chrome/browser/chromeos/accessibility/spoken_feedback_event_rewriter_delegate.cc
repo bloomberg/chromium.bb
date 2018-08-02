65b588ac8b66081a507afb5bd8ae5dceb614d599
David Tseng <dtseng@chromium.org>
2018-08-02 17:24:03 +0000

    Remove logic to repost key events when ChromeVox is off
    The capture state lives in ash, where ChromeVox sets the state (extension -> browser -> ash).
    
    This state gets delivered (ash -> browser) to the SpokenFeedbackEventRewriterDelegate.
    
    At that point, we should always allow the event to continue onto ChromeVox, and only re-inject if the key is captured.
    
    The previous logic was put into place when this code always sent the delegate all keys.
    
    Bug: 868573
    Change-Id: I6bde8b021d093c52d4450682c4d037a7ef4938a0
    Reviewed-on: https://chromium-review.googlesource.com/1159176
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580226}
    Former-commit-id: 0de4429aaead184a4468dee14e1d63c1af606053

--- chrome/browser/chromeos/accessibility/spoken_feedback_event_rewriter_delegate.cc
+++ chrome/browser/chromeos/accessibility/spoken_feedback_event_rewriter_delegate.cc
@@ -33,11 +33,6 @@ SpokenFeedbackEventRewriterDelegate::~SpokenFeedbackEventRewriterDelegate() {}
 void SpokenFeedbackEventRewriterDelegate::DispatchKeyEventToChromeVox(
     std::unique_ptr<ui::Event> event,
     bool capture) {
-  if (!ShouldDispatchKeyEventToChromeVox(event.get())) {
-    OnUnhandledSpokenFeedbackEvent(std::move(event));
-    return;
-  }
-
   extensions::ExtensionHost* host = chromeos::GetAccessibilityExtensionHost(
       extension_misc::kChromeVoxExtensionId);
   // Listen for any unhandled keyboard events from ChromeVox's background page
