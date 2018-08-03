e38f95cd801696ce8e1a3c9daee0880fce5c4cdd
Xixuan Wu <xixuan@chromium.org>
2018-08-03 09:02:10 -0700

    cbuildbot: Let blocking suite run first.
    Provision suite is blocking suite. We want them to run and finish before
    other suites, including unibuild.
    
    It's not a problem in current system since if the DUTs are in
    provisioning, scheduler will wait for them to finish then schedule.
    However, when we move to skylab, swarming will return 'NO_RESOURCE'
    immediately if no DUTs available:
    e.g.
        https://chrome-swarming.appspot.com/task?id=3f179019f0108810&refresh=10
    
    So if a suite want to be run in parallel, the correct way to do it is to
    change its 'blocking' to False.
    
    BUG=chromium:870046
    TEST=None
    
    Change-Id: Iaf0d573de356f2d6829217a4567ac9e30bdb8844
    Reviewed-on: https://chromium-review.googlesource.com/1162301
    Commit-Ready: Xixuan Wu <xixuan@chromium.org>
    Tested-by: Xixuan Wu <xixuan@chromium.org>
    Reviewed-by: Don Garrett <dgarrett@chromium.org>

--- cbuildbot/builders/simple_builders.py
+++ cbuildbot/builders/simple_builders.py
@@ -104,10 +104,8 @@ class SimpleBuilder(generic_builders.Builder):
     # For non-uni builds, we don't pass a model (just board)
     models = [config_lib.ModelTestConfig(None, board)]
 
-    unibuild = False
     if builder_run.config.models:
       models = builder_run.config.models
-      unibuild = True
 
     parallel_stages = []
     for suite_config in builder_run.config.hw_tests:
@@ -121,7 +119,11 @@ class SimpleBuilder(generic_builders.Builder):
 
       # Please see docstring for blocking in the HWTestConfig for more
       # information on this behavior.
-      if suite_config.blocking and not unibuild:
+      # Expected behavior:
+      #     1) Blocking suites are kicked off first, e.g. provision suite.
+      #     2) If it's unibuild, the blocking suites of all models are kicked
+      #        off in parallel first.
+      if suite_config.blocking:
         self._RunParallelStages(parallel_stages)
         parallel_stages = []
 
