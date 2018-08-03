24fd3d32312853d8e65641cb496d33f45f892085
Jennifer Apacible <japacible@gmail.com>
2018-08-03 05:31:48 +0000

    [Picture in Picture] Only show controls when mouse is moving on Windows.
    A previous change made controls always appear when the mouse is moving
    on all OS except Windows. This change gates handling the ET_MOUSE_MOVED
    event to Windows OS.
    
    Bug: 870120
    Change-Id: I556c354643a920c34b973066369e26f38130705f
    Reviewed-on: https://chromium-review.googlesource.com/1161300
    Commit-Queue: apacible <apacible@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580457}
    Former-commit-id: 6698555a6f8132847bd6198fa4d39a507999d6cd

--- chrome/browser/ui/views/overlay/overlay_window_views.cc
+++ chrome/browser/ui/views/overlay/overlay_window_views.cc
@@ -485,8 +485,10 @@ void OverlayWindowViews::OnMouseEvent(ui::MouseEvent* event) {
     // Only show the media controls when the mouse is hovering over the window.
     // This is checking for both ENTERED and MOVED because ENTERED is not fired
     // after a resize on Windows.
-    case ui::ET_MOUSE_ENTERED:
+#if defined (OS_WIN)
     case ui::ET_MOUSE_MOVED:
+#endif  // OS_WIN
+    case ui::ET_MOUSE_ENTERED:
       UpdateControlsVisibility(true);
       break;
 
