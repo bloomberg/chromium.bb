551371acff28635124a7cce996e48b6e288ad83d
Robert Sesek <rsesek@chromium.org>
2018-08-03 22:18:24 +0000

    [Mac] Bring up the audio service sandbox.
    This requires --enable-features=AudioServiceAudioStreams,AudioServiceOutOfProcess,AudioServiceSandbox
    
    Bug: 850878
    Change-Id: I9c759b2537483612f9b3106bdf2ec193b343f198
    Reviewed-on: https://chromium-review.googlesource.com/1152080
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580672}
    Former-commit-id: dc7c07308016766a798dac600d8a9cb2bb44a43d

--- services/service_manager/sandbox/mac/sandbox_mac.mm
+++ services/service_manager/sandbox/mac/sandbox_mac.mm
@@ -38,6 +38,7 @@
 #include "base/strings/utf_string_conversions.h"
 #include "base/sys_info.h"
 #include "sandbox/mac/sandbox_compiler.h"
+#include "services/service_manager/sandbox/mac/audio.sb.h"
 #include "services/service_manager/sandbox/mac/cdm.sb.h"
 #include "services/service_manager/sandbox/mac/common.sb.h"
 #include "services/service_manager/sandbox/mac/gpu.sb.h"
@@ -62,7 +63,6 @@ struct SandboxTypeToResourceIDMapping {
 // Mapping from sandbox process types to resource IDs containing the sandbox
 // profile for all process types known to service_manager.
 // TODO(tsepez): Implement profile for SANDBOX_TYPE_NETWORK.
-// TODO(https://crbug.com/850878): Implement profile for SANDBOX_TYPE_AUDIO.
 SandboxTypeToResourceIDMapping kDefaultSandboxTypeToResourceIDMapping[] = {
     {SANDBOX_TYPE_NO_SANDBOX, nullptr},
     {SANDBOX_TYPE_RENDERER, kSeatbeltPolicyString_renderer},
@@ -74,7 +74,7 @@ SandboxTypeToResourceIDMapping kDefaultSandboxTypeToResourceIDMapping[] = {
     {SANDBOX_TYPE_NACL_LOADER, kSeatbeltPolicyString_nacl_loader},
     {SANDBOX_TYPE_PDF_COMPOSITOR, kSeatbeltPolicyString_ppapi},
     {SANDBOX_TYPE_PROFILING, kSeatbeltPolicyString_utility},
-    {SANDBOX_TYPE_AUDIO, nullptr},
+    {SANDBOX_TYPE_AUDIO, kSeatbeltPolicyString_audio},
 };
 
 static_assert(arraysize(kDefaultSandboxTypeToResourceIDMapping) ==
