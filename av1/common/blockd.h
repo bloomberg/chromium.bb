3a53922126c750f0cb4c3fd891db1f2cf2460a1e
Wan-Teh Chang <wtc@google.com>
2018-08-03 12:33:28 -0700

    Document get_partition_subsize and ss_size_lookup.
    Add comments for the get_partition_subsize and get_plane_block_size
    functions and the related lookup tables.
    
    Add test/blockd_test.cc for the unit tests for av1/common/blockd.{h,c}.
    
    BUG=aomedia:2044
    
    Change-Id: I75a771369c2352afe2e511429235708a6072ed53

--- av1/common/blockd.h
+++ av1/common/blockd.h
@@ -619,6 +619,11 @@ static INLINE int get_sqr_bsize_idx(BLOCK_SIZE bsize) {
   }
 }
 
+// For a square block size 'bsize', returns the size of the sub-blocks used by
+// the given partition type. If the partition produces sub-blocks of different
+// sizes, then the function returns the largest sub-block size.
+// Implements the Partition_Subsize lookup table in the spec (Section 9.3.
+// Conversion tables).
 // Note: the input block size should be square.
 // Otherwise it's considered invalid.
 static INLINE BLOCK_SIZE get_partition_subsize(BLOCK_SIZE bsize,
@@ -777,6 +782,8 @@ static INLINE TX_TYPE get_default_tx_type(PLANE_TYPE plane_type,
   return intra_mode_to_tx_type(mbmi, plane_type);
 }
 
+// Implements the get_plane_residual_size() function in the spec (Section
+// 5.11.38. Get plane residual size function).
 static INLINE BLOCK_SIZE get_plane_block_size(BLOCK_SIZE bsize,
                                               int subsampling_x,
                                               int subsampling_y) {
