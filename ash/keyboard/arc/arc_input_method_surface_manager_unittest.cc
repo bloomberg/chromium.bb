142aa49340fa02cbf19f74ea8095998501fa56d4
Yuichiro Hanada <yhanada@chromium.org>
2018-08-04 09:38:37 +0000

    Add ArcInputMethodSurfaceManager.
    Bug: 845079
    Test: ash_unittests
    Change-Id: I022910de2c6fa820909ae1a415b9ce5cdd3859dd
    Reviewed-on: https://chromium-review.googlesource.com/1156339
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580755}
    Former-commit-id: 78b25253fa183ca52a167a43dc46dd88971b3daf

---
+++ ash/keyboard/arc/arc_input_method_surface_manager_unittest.cc
@@ -0,0 +1,49 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/keyboard/arc/arc_input_method_surface_manager.h"
+#include "ash/test/ash_test_base.h"
+#include "base/macros.h"
+#include "components/exo/input_method_surface.h"
+#include "components/exo/surface.h"
+#include "components/exo/wm_helper.h"
+
+namespace ash {
+
+class ArcInputMethodSurfaceManagerTest : public AshTestBase {
+ public:
+  ArcInputMethodSurfaceManagerTest() = default;
+  ~ArcInputMethodSurfaceManagerTest() override = default;
+
+  void SetUp() override {
+    AshTestBase::SetUp();
+    wm_helper_ = std::make_unique<exo::WMHelper>();
+    exo::WMHelper::SetInstance(wm_helper_.get());
+  }
+
+  void TearDown() override {
+    exo::WMHelper::SetInstance(nullptr);
+    wm_helper_.reset();
+    AshTestBase::TearDown();
+  }
+
+ private:
+  std::unique_ptr<exo::WMHelper> wm_helper_;
+
+  DISALLOW_COPY_AND_ASSIGN(ArcInputMethodSurfaceManagerTest);
+};
+
+TEST_F(ArcInputMethodSurfaceManagerTest, AddRemoveSurface) {
+  ArcInputMethodSurfaceManager manager;
+  EXPECT_EQ(nullptr, manager.GetSurface());
+  auto surface = std::make_unique<exo::Surface>();
+  auto input_method_surface =
+      std::make_unique<exo::InputMethodSurface>(nullptr, surface.get(), 1.0);
+  manager.AddSurface(input_method_surface.get());
+  EXPECT_EQ(input_method_surface.get(), manager.GetSurface());
+  manager.RemoveSurface(input_method_surface.get());
+  EXPECT_EQ(nullptr, manager.GetSurface());
+}
+
+}  // namespace ash
