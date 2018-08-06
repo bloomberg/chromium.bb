8ea1cbc6f355cc531afd7ac786feaed1cc003428
Mihai Sardarescu <msarda@chromium.org>
2018-08-06 09:41:25 +0000

    [unified-consent] Personalized URL-keyed data collection should be on when history sync is enabled
    It is impossible for the sync service to ensure that USER_EVENTS model type
    disabled when HISTORY_DELETE_DIRECTIVESis disabled. Howver, "Activity and
    interactions" must be disabled when HISTORY_DELETE_DIRECTIVES is disabled.
    
    This CL changes the personalized version of UrlKeyedDataCollectionConsentHelper
    to be enabled only when sync is active for both HISTORY_DELETE_DIRECTIVES and
    USER_EVENTS data types.
    
    Bug: 865537
    Change-Id: Iaad834761ad4d7ddf56feec73f1f567b240c2b7f
    Reviewed-on: https://chromium-review.googlesource.com/1162235
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#580832}
    Former-commit-id: f6af1216790ed1f9a48c0e0a84d51d86689941b2

--- components/unified_consent/url_keyed_data_collection_consent_helper_unittest.cc
+++ components/unified_consent/url_keyed_data_collection_consent_helper_unittest.cc
@@ -21,9 +21,10 @@ class TestSyncService : public syncer::FakeSyncService {
   void set_sync_initialized(bool sync_initialized) {
     sync_initialized_ = sync_initialized;
   }
-  void set_sync_active_data_type(syncer::ModelType type) {
-    sync_active_data_type_ = type;
+  void AddActiveDataType(syncer::ModelType type) {
+    sync_active_data_types_.Put(type);
   }
+  void ClearActiveDataTypes() { sync_active_data_types_.Clear(); }
   void FireOnStateChangeOnAllObservers() {
     for (auto& observer : observers_)
       observer.OnStateChanged(this);
@@ -34,7 +35,8 @@ class TestSyncService : public syncer::FakeSyncService {
   State GetState() const override { return State::ACTIVE; }
   syncer::ModelTypeSet GetPreferredDataTypes() const override {
     return syncer::ModelTypeSet(syncer::ModelType::HISTORY_DELETE_DIRECTIVES,
-                                syncer::ModelType::USER_EVENTS);
+                                syncer::ModelType::USER_EVENTS,
+                                syncer::ModelType::EXTENSIONS);
   }
   bool IsFirstSetupComplete() const override { return true; }
 
@@ -53,10 +55,7 @@ class TestSyncService : public syncer::FakeSyncService {
   }
 
   syncer::ModelTypeSet GetActiveDataTypes() const override {
-    if (sync_active_data_type_ != syncer::ModelType::UNSPECIFIED) {
-      return syncer::ModelTypeSet(sync_active_data_type_);
-    }
-    return syncer::ModelTypeSet();
+    return sync_active_data_types_;
   }
 
   void AddObserver(syncer::SyncServiceObserver* observer) override {
@@ -68,7 +67,7 @@ class TestSyncService : public syncer::FakeSyncService {
 
  private:
   bool sync_initialized_ = false;
-  syncer::ModelType sync_active_data_type_ = syncer::ModelType::UNSPECIFIED;
+  syncer::ModelTypeSet sync_active_data_types_;
   base::ObserverList<syncer::SyncServiceObserver> observers_;
 };
 
@@ -128,11 +127,10 @@ TEST_F(UrlKeyedDataCollectionConsentHelperTest,
   EXPECT_TRUE(state_changed_notifications.empty());
 
   sync_service_.set_sync_initialized(true);
-  sync_service_.set_sync_active_data_type(
-      syncer::ModelType::HISTORY_DELETE_DIRECTIVES);
+  sync_service_.AddActiveDataType(syncer::ModelType::HISTORY_DELETE_DIRECTIVES);
   sync_service_.FireOnStateChangeOnAllObservers();
   EXPECT_TRUE(helper->IsEnabled());
-  ASSERT_EQ(1U, state_changed_notifications.size());
+  EXPECT_EQ(1U, state_changed_notifications.size());
   helper->RemoveObserver(this);
 }
 
@@ -154,12 +152,30 @@ TEST_F(UrlKeyedDataCollectionConsentHelperTest,
   helper->AddObserver(this);
   EXPECT_FALSE(helper->IsEnabled());
   EXPECT_TRUE(state_changed_notifications.empty());
-
   sync_service_.set_sync_initialized(true);
-  sync_service_.set_sync_active_data_type(syncer::ModelType::USER_EVENTS);
+
+  // Peronalized data collection is disabled when only USER_EVENTS are enabled.
+  sync_service_.AddActiveDataType(syncer::ModelType::USER_EVENTS);
+  sync_service_.FireOnStateChangeOnAllObservers();
+  EXPECT_FALSE(helper->IsEnabled());
+  EXPECT_TRUE(state_changed_notifications.empty());
+
+  // Peronalized data collection is disabled when only HISTORY_DELETE_DIRECTIVES
+  // are enabled.
+  sync_service_.ClearActiveDataTypes();
+  sync_service_.AddActiveDataType(syncer::ModelType::HISTORY_DELETE_DIRECTIVES);
+  sync_service_.FireOnStateChangeOnAllObservers();
+  EXPECT_FALSE(helper->IsEnabled());
+  EXPECT_TRUE(state_changed_notifications.empty());
+
+  // Personalized data collection is enabled iff USER_EVENTS and
+  // HISTORY_DELETE_DIRECTIVES are enabled.
+  sync_service_.ClearActiveDataTypes();
+  sync_service_.AddActiveDataType(syncer::ModelType::HISTORY_DELETE_DIRECTIVES);
+  sync_service_.AddActiveDataType(syncer::ModelType::USER_EVENTS);
   sync_service_.FireOnStateChangeOnAllObservers();
   EXPECT_TRUE(helper->IsEnabled());
-  ASSERT_EQ(1U, state_changed_notifications.size());
+  EXPECT_EQ(1U, state_changed_notifications.size());
   helper->RemoveObserver(this);
 }
 
@@ -173,11 +189,10 @@ TEST_F(UrlKeyedDataCollectionConsentHelperTest,
   EXPECT_TRUE(state_changed_notifications.empty());
 
   sync_service_.set_sync_initialized(true);
-  sync_service_.set_sync_active_data_type(
-      syncer::ModelType::HISTORY_DELETE_DIRECTIVES);
+  sync_service_.AddActiveDataType(syncer::ModelType::HISTORY_DELETE_DIRECTIVES);
   sync_service_.FireOnStateChangeOnAllObservers();
   EXPECT_TRUE(helper->IsEnabled());
-  ASSERT_EQ(1U, state_changed_notifications.size());
+  EXPECT_EQ(1U, state_changed_notifications.size());
   helper->RemoveObserver(this);
 }
 
