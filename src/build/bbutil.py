6e9610e4b853ec4e3faec84a25d61db0b7de6308
Imran Haider <ihaider3@bloomberg.net>
2018-08-06 16:41:38 -0400

    Build requirements for content_shell
    This commit contains the following changes:
    1. Fix the 'enable_extensions' GN flag so that it can be turned off without
       breaking the build.
    2. Includes scripts to generate commonly used ninja files
    3. Includes scripts to generate devkits
    4. Removes dependency on internet connectivity when building:
      a. Removes dependency on Google storage servers
      b. Removes dependency on gclient.  As a result, the following components
         needed to be disabled:
         1. NativeClient
         2. WebVR
      c. Includes a custom implementation of the runhooks script that does not
         require internet connectivity.
      d. Disables the download of the following components:
        1.  NativeClient
        2.  Traffic Annotation tools
        3.  Clang formatter
        4.  LUCI services and tools
        5.  Syzygy transformation toolchain
        6.  Apache
        7.  WebAssembly Fuzzer
        8.  NodeJS
        9.  WebUI Node modules
        10. Dependencies of Telemetry
        11. Test data for Zucchini
        12. VPython
      e. These tools and files are included in the source tree so they needn't be
         downloaded at build time:
        1. GN
        2. Clang + LLVM
        3. Clang orderfiles - These files are essentially precomputed link lines
           for clang that helps to improve startup performance in
           official (buildmaster) builds.
        4. Resource compiler
    5. Modification to support Incredibuild
    
    Rebased from commit dcc167aff293 of Chromium 67

---
+++ src/build/bbutil.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2013 Bloomberg L.P. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import subprocess
+
+
+def shellExec(cmd):
+  p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  output = p.communicate()
+  return p.returncode, output[0], output[1]
+
+
+def shellExecNoPipe(cmd):
+  p = subprocess.Popen(cmd, shell=True)
+  p.communicate()
+  return p.returncode
+
+
+def getStrippedShellOutput(cmd):
+  p = shellExec(cmd)
+  if p[0] != 0:
+    raise Exception("Failed to run command '" + cmd + "': " + p[2])
+  return p[1].strip()
+
+
+def getSplitShellOutput(cmd):
+  p = shellExec(cmd)
+  if p[0] != 0:
+    raise Exception("Failed to run command '" + cmd + "': " + p[2])
+  return splitLines(p[1].strip())
+
+
+def getHEADSha():
+  return getStrippedShellOutput("git rev-parse HEAD")
+
+
