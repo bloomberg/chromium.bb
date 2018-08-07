333617bc86fef3ec7b4612d35eba8a36f531a86a
Vadim Shtayura <vadimsh@chromium.org>
2018-08-07 20:46:38 +0000

    [cipd] Bump versions of all CIPD packages + add mips(64)le platform support.
    New packages are built using SHA256 as instance ID (instead of SHA1).
    
    R=iannucci@chromium.org
    BUG=821194, 867819, 813782
    
    Change-Id: I61cf71386975725f7f63097eeb62f094ff50e396
    Reviewed-on: https://chromium-review.googlesource.com/1165598
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Vadim Shtayura <vadimsh@chromium.org>

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -18,7 +18,8 @@ CIPD_CLIENT_ENSURE_FILE_TEMPLATE = r'''
 $VerifiedPlatform linux-amd64 mac-amd64 windows-amd64 windows-386
 # Best effort support.
 $VerifiedPlatform linux-386 linux-ppc64 linux-ppc64le linux-s390x
-$VerifiedPlatform linux-arm64 linux-armv6l linux-mips64
+$VerifiedPlatform linux-arm64 linux-armv6l
+$VerifiedPlatform linux-mips64 linux-mips64le linux-mipsle
 
 %s %s
 '''
