b114e5c723a95a8ba4c60acc94a291083fca4e8b
erikchen <erikchen@chromium.org>
2018-08-07 19:29:43 +0000

    Implement native event observer for linux.
    This CL uses ui::PlatformEventSource observer API to hook into pre and post
    native event dispatch on Linux.
    
    Timestamps for X11 events are in X11 server time. The implementation makes a
    round-trip to the X11 server to obtain a delta, and then uses that delta with
    base::TimeTicks::Now() to convert to a base::TimeTicks.
    
    The overhead from the rount-trip seems reasonable. 99th percentile X11 RTT is
    3ms. 99.9th percentile is 16ms. 99.99th percentile is 40ms. These include the
    time for syscalls to base::TimeTicks::Now().
    
    To further reduce overhead, this CL changes the UMA metric to only be computed
    with 1/1000 frequency, to avoid two calls to base::TimeTicks::Now().
    
    Note: The implementation of native event observer will be turned on via Finch
    experiment to measure overhead.
    
    Change-Id: I786537f093e862a7a19630c9150606a2eb8267d9
    Bug: 859155
    Reviewed-on: https://chromium-review.googlesource.com/1162658
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581304}
    Former-commit-id: 17a922fba5b6ef0e399b3c26b6b21ac49e125502

--- content/browser/scheduler/responsiveness/native_event_observer.cc
+++ content/browser/scheduler/responsiveness/native_event_observer.cc
@@ -4,6 +4,18 @@
 
 #include "content/browser/scheduler/responsiveness/native_event_observer.h"
 
+#include "ui/events/platform/platform_event_source.h"
+
+#if defined(OS_LINUX)
+#if defined(USE_X11)
+#include "ui/events/platform/x11/x11_event_source.h"  // nogncheck
+#elif defined(USE_OZONE)
+#include "ui/events/event.h"
+#endif
+
+#include "ui/events/platform_event.h"
+#endif
+
 namespace content {
 namespace responsiveness {
 
@@ -20,11 +32,54 @@ NativeEventObserver::~NativeEventObserver() {
 }
 
 #if !defined(OS_MACOSX)
-// TODO(erikchen): Implement this for non-macOS platforms.
+#if defined(OS_LINUX)
+void NativeEventObserver::RegisterObserver() {
+  ui::PlatformEventSource::GetInstance()->AddPlatformEventObserver(this);
+}
+void NativeEventObserver::DeregisterObserver() {
+  ui::PlatformEventSource::GetInstance()->RemovePlatformEventObserver(this);
+}
+
+void NativeEventObserver::WillProcessEvent(const ui::PlatformEvent& event) {
+  will_run_event_callback_.Run(&event);
+}
+
+void NativeEventObserver::DidProcessEvent(const ui::PlatformEvent& event) {
+#if defined(USE_OZONE)
+  did_run_event_callback_.Run(&event, event->time_stamp());
+#elif defined(USE_X11)
+  // X11 uses a uint32_t on the wire protocol. Xlib casts this to an unsigned
+  // long by prepending with 0s. We cast back to a uint32_t so that subtraction
+  // works properly when the timestamp overflows back to 0.
+  uint32_t event_server_time_ms =
+      static_cast<uint32_t>(ui::X11EventSource::GetInstance()->GetTimestamp());
+  uint32_t current_server_time_ms = static_cast<uint32_t>(
+      ui::X11EventSource::GetInstance()->GetCurrentServerTime());
+
+  int32_t delta_ms = current_server_time_ms - event_server_time_ms;
+
+  // If for some reason server time is not monotonically increasing, ignore it.
+  if (delta_ms < 0)
+    delta_ms = 0;
+
+  // Ignore pathologically long deltas. Server is probably having issues.
+  base::TimeDelta delta = base::TimeDelta::FromMilliseconds(delta_ms);
+  base::TimeDelta long_duration = base::TimeDelta::FromSeconds(30);
+  if (delta > long_duration)
+    delta = base::TimeDelta();
+
+  did_run_event_callback_.Run(&event, base::TimeTicks::Now() - delta);
+#else
+#error
+#endif
+}
+#else   // defined(OS_LINUX)
+// TODO(erikchen): Implement this for non-macOS, non-Linux platforms.
 // https://crbug.com/859155.
 void NativeEventObserver::RegisterObserver() {}
 void NativeEventObserver::DeregisterObserver() {}
-#endif
+#endif  // defined(OS_LINUX)
+#endif  // !defined(OS_MACOSX)
 
 }  // namespace responsiveness
 }  // namespace content
