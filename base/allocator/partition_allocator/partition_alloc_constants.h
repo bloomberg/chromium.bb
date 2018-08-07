6a3ff605cbeb8b8cda0c8dbb6bc6e0f6352c0a30
Chris Palmer <palmer@chromium.org>
2018-08-07 00:35:25 +0000

    Ensure that `PartitionAllocGenericFlags` callers are only using valid flags.
    Bug: 680657
    Change-Id: I2d879f5b28c392a083fbd867a3eb05568d19dc67
    Reviewed-on: https://chromium-review.googlesource.com/1164252
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Chris Palmer <palmer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581049}
    Former-commit-id: 96acdb1abb37f8e49b35fa2bdcfda7a93cee4ab6

--- base/allocator/partition_allocator/partition_alloc_constants.h
+++ base/allocator/partition_allocator/partition_alloc_constants.h
@@ -154,6 +154,8 @@ static const size_t kReasonableSizeOfUnusedPages = 1024 * 1024 * 1024;  // 1GB
 // Flags for PartitionAllocGenericFlags.
 enum PartitionAllocFlags {
   PartitionAllocReturnNull = 1 << 0,
+
+  PartitionAllocLastFlag = PartitionAllocReturnNull
 };
 
 }  // namespace base
