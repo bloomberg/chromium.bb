bbb5851d507d2e1c9194da16a0ee4d1759e08267
Nicholas Verne <nverne@chromium.org>
2018-08-07 07:42:51 +0000

    Support Tremplin container setup in CiceroneClient
    Crostini linux containers are going to use a new Cicerone based API for
    setup. This will allow us to surface more install progress for users. A future
    CL will wire this in to CrostiniManager and CrostiniRestarter.
    
    Bug: 860094
    Change-Id: Iea91285af1bd42e5bb8ecfd5f62f66091b7e6eae
    Reviewed-on: https://chromium-review.googlesource.com/1163350
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581161}
    Former-commit-id: bf30fc974f29b9ca7bc77d51b39b2f72094abb7e

--- chromeos/dbus/cicerone_client.cc
+++ chromeos/dbus/cicerone_client.cc
@@ -40,6 +40,18 @@ class CiceroneClientImpl : public CiceroneClient {
     return is_install_linux_package_progress_signal_connected_;
   }
 
+  bool IsLxdContainerCreatedSignalConnected() override {
+    return is_lxd_container_created_signal_connected_;
+  }
+
+  bool IsLxdContainerDownloadingSignalConnected() override {
+    return is_lxd_container_downloading_signal_connected_;
+  }
+
+  bool IsTremplinStartedSignalConnected() override {
+    return is_tremplin_started_signal_connected_;
+  }
+
   void LaunchContainerApplication(
       const vm_tools::cicerone::LaunchContainerApplicationRequest& request,
       DBusMethodCallback<vm_tools::cicerone::LaunchContainerApplicationResponse>
@@ -108,6 +120,92 @@ class CiceroneClientImpl : public CiceroneClient {
                        weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
   }
 
+  void CreateLxdContainer(
+      const vm_tools::cicerone::CreateLxdContainerRequest& request,
+      DBusMethodCallback<vm_tools::cicerone::CreateLxdContainerResponse>
+          callback) override {
+    dbus::MethodCall method_call(vm_tools::cicerone::kVmCiceroneInterface,
+                                 vm_tools::cicerone::kCreateLxdContainerMethod);
+    dbus::MessageWriter writer(&method_call);
+
+    if (!writer.AppendProtoAsArrayOfBytes(request)) {
+      LOG(ERROR) << "Failed to encode CreateLxdContainerRequest protobuf";
+      std::move(callback).Run(base::nullopt);
+      return;
+    }
+
+    cicerone_proxy_->CallMethod(
+        &method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
+        base::BindOnce(&CiceroneClientImpl::OnDBusProtoResponse<
+                           vm_tools::cicerone::CreateLxdContainerResponse>,
+                       weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
+  }
+
+  void StartLxdContainer(
+      const vm_tools::cicerone::StartLxdContainerRequest& request,
+      DBusMethodCallback<vm_tools::cicerone::StartLxdContainerResponse>
+          callback) override {
+    dbus::MethodCall method_call(vm_tools::cicerone::kVmCiceroneInterface,
+                                 vm_tools::cicerone::kStartLxdContainerMethod);
+    dbus::MessageWriter writer(&method_call);
+
+    if (!writer.AppendProtoAsArrayOfBytes(request)) {
+      LOG(ERROR) << "Failed to encode StartLxdContainerRequest protobuf";
+      std::move(callback).Run(base::nullopt);
+      return;
+    }
+
+    cicerone_proxy_->CallMethod(
+        &method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
+        base::BindOnce(&CiceroneClientImpl::OnDBusProtoResponse<
+                           vm_tools::cicerone::StartLxdContainerResponse>,
+                       weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
+  }
+
+  void GetLxdContainerUsername(
+      const vm_tools::cicerone::GetLxdContainerUsernameRequest& request,
+      DBusMethodCallback<vm_tools::cicerone::GetLxdContainerUsernameResponse>
+          callback) override {
+    dbus::MethodCall method_call(
+        vm_tools::cicerone::kVmCiceroneInterface,
+        vm_tools::cicerone::kGetLxdContainerUsernameMethod);
+    dbus::MessageWriter writer(&method_call);
+
+    if (!writer.AppendProtoAsArrayOfBytes(request)) {
+      LOG(ERROR) << "Failed to encode GetLxdContainerUsernameRequest protobuf";
+      std::move(callback).Run(base::nullopt);
+      return;
+    }
+
+    cicerone_proxy_->CallMethod(
+        &method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
+        base::BindOnce(&CiceroneClientImpl::OnDBusProtoResponse<
+                           vm_tools::cicerone::GetLxdContainerUsernameResponse>,
+                       weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
+  }
+
+  void SetUpLxdContainerUser(
+      const vm_tools::cicerone::SetUpLxdContainerUserRequest& request,
+      DBusMethodCallback<vm_tools::cicerone::SetUpLxdContainerUserResponse>
+          callback) override {
+    dbus::MethodCall method_call(
+        vm_tools::cicerone::kVmCiceroneInterface,
+        vm_tools::cicerone::kSetUpLxdContainerUserMethod);
+    dbus::MessageWriter writer(&method_call);
+
+    if (!writer.AppendProtoAsArrayOfBytes(request)) {
+      LOG(ERROR) << "Failed to encode SetUpLxdContainerUserRequest protobuf";
+      std::move(callback).Run(base::nullopt);
+      return;
+    }
+
+    cicerone_proxy_->CallMethod(
+        &method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
+        base::BindOnce(&CiceroneClientImpl::OnDBusProtoResponse<
+                           vm_tools::cicerone::SetUpLxdContainerUserResponse>,
+                       weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
+  }
+
   void WaitForServiceToBeAvailable(
       dbus::ObjectProxy::WaitForServiceToBeAvailableCallback callback)
       override {
@@ -143,6 +241,28 @@ class CiceroneClientImpl : public CiceroneClient {
         base::BindRepeating(
             &CiceroneClientImpl::OnInstallLinuxPackageProgressSignal,
             weak_ptr_factory_.GetWeakPtr()),
+        base::BindOnce(&CiceroneClientImpl::OnSignalConnected,
+                       weak_ptr_factory_.GetWeakPtr()));
+    cicerone_proxy_->ConnectToSignal(
+        vm_tools::cicerone::kVmCiceroneInterface,
+        vm_tools::cicerone::kLxdContainerCreatedSignal,
+        base::BindRepeating(&CiceroneClientImpl::OnLxdContainerCreatedSignal,
+                            weak_ptr_factory_.GetWeakPtr()),
+        base::BindOnce(&CiceroneClientImpl::OnSignalConnected,
+                       weak_ptr_factory_.GetWeakPtr()));
+    cicerone_proxy_->ConnectToSignal(
+        vm_tools::cicerone::kVmCiceroneInterface,
+        vm_tools::cicerone::kLxdContainerDownloadingSignal,
+        base::BindRepeating(
+            &CiceroneClientImpl::OnLxdContainerDownloadingSignal,
+            weak_ptr_factory_.GetWeakPtr()),
+        base::BindOnce(&CiceroneClientImpl::OnSignalConnected,
+                       weak_ptr_factory_.GetWeakPtr()));
+    cicerone_proxy_->ConnectToSignal(
+        vm_tools::cicerone::kVmCiceroneInterface,
+        vm_tools::cicerone::kTremplinStartedSignal,
+        base::BindRepeating(&CiceroneClientImpl::OnTremplinStartedSignal,
+                            weak_ptr_factory_.GetWeakPtr()),
         base::BindOnce(&CiceroneClientImpl::OnSignalConnected,
                        weak_ptr_factory_.GetWeakPtr()));
   }
@@ -201,6 +321,42 @@ class CiceroneClientImpl : public CiceroneClient {
     }
   }
 
+  void OnLxdContainerCreatedSignal(dbus::Signal* signal) {
+    vm_tools::cicerone::LxdContainerCreatedSignal proto;
+    dbus::MessageReader reader(signal);
+    if (!reader.PopArrayOfBytesAsProto(&proto)) {
+      LOG(ERROR) << "Failed to parse proto from DBus Signal";
+      return;
+    }
+    for (auto& observer : observer_list_) {
+      observer.OnLxdContainerCreated(proto);
+    }
+  }
+
+  void OnLxdContainerDownloadingSignal(dbus::Signal* signal) {
+    vm_tools::cicerone::LxdContainerDownloadingSignal proto;
+    dbus::MessageReader reader(signal);
+    if (!reader.PopArrayOfBytesAsProto(&proto)) {
+      LOG(ERROR) << "Failed to parse proto from DBus Signal";
+      return;
+    }
+    for (auto& observer : observer_list_) {
+      observer.OnLxdContainerDownloading(proto);
+    }
+  }
+
+  void OnTremplinStartedSignal(dbus::Signal* signal) {
+    vm_tools::cicerone::TremplinStartedSignal proto;
+    dbus::MessageReader reader(signal);
+    if (!reader.PopArrayOfBytesAsProto(&proto)) {
+      LOG(ERROR) << "Failed to parse proto from DBus Signal";
+      return;
+    }
+    for (auto& observer : observer_list_) {
+      observer.OnTremplinStarted(proto);
+    }
+  }
+
   void OnSignalConnected(const std::string& interface_name,
                          const std::string& signal_name,
                          bool is_connected) {
@@ -216,6 +372,13 @@ class CiceroneClientImpl : public CiceroneClient {
     } else if (signal_name ==
                vm_tools::cicerone::kInstallLinuxPackageProgressSignal) {
       is_install_linux_package_progress_signal_connected_ = is_connected;
+    } else if (signal_name == vm_tools::cicerone::kLxdContainerCreatedSignal) {
+      is_lxd_container_created_signal_connected_ = is_connected;
+    } else if (signal_name ==
+               vm_tools::cicerone::kLxdContainerDownloadingSignal) {
+      is_lxd_container_downloading_signal_connected_ = is_connected;
+    } else if (signal_name == vm_tools::cicerone::kTremplinStartedSignal) {
+      is_tremplin_started_signal_connected_ = is_connected;
     } else {
       NOTREACHED();
     }
@@ -228,6 +391,9 @@ class CiceroneClientImpl : public CiceroneClient {
   bool is_container_started_signal_connected_ = false;
   bool is_container_shutdown_signal_connected_ = false;
   bool is_install_linux_package_progress_signal_connected_ = false;
+  bool is_lxd_container_created_signal_connected_ = false;
+  bool is_lxd_container_downloading_signal_connected_ = false;
+  bool is_tremplin_started_signal_connected_ = false;
 
   // Note: This should remain the last member so it'll be destroyed and
   // invalidate its weak pointers before any other members are destroyed.
--- chromeos/dbus/fake_cicerone_client.h
+++ chromeos/dbus/fake_cicerone_client.h
@@ -32,6 +32,18 @@ class CHROMEOS_EXPORT FakeCiceroneClient : public CiceroneClient {
   // This should be true prior to calling InstallLinuxPackage.
   bool IsInstallLinuxPackageProgressSignalConnected() override;
 
+  // This should be true prior to calling CreateLxdContainer or
+  // StartLxdContainer.
+  bool IsLxdContainerCreatedSignalConnected() override;
+
+  // This should be true prior to calling CreateLxdContainer or
+  // StartLxdContainer.
+  bool IsLxdContainerDownloadingSignalConnected() override;
+
+  // This should be true prior to calling CreateLxdContainer or
+  // StartLxdContainer.
+  bool IsTremplinStartedSignalConnected() override;
+
   // Fake version of the method that launches an application inside a running
   // Container. |callback| is called after the method call finishes.
   void LaunchContainerApplication(
@@ -54,6 +66,34 @@ class CHROMEOS_EXPORT FakeCiceroneClient : public CiceroneClient {
       DBusMethodCallback<vm_tools::cicerone::InstallLinuxPackageResponse>
           callback) override;
 
+  // Fake version of the method that creates a new Container.
+  // |callback| is called to indicate creation status.
+  void CreateLxdContainer(
+      const vm_tools::cicerone::CreateLxdContainerRequest& request,
+      DBusMethodCallback<vm_tools::cicerone::CreateLxdContainerResponse>
+          callback) override;
+
+  // Fake version of the method that starts a new Container.
+  // |callback| is called when the method completes.
+  void StartLxdContainer(
+      const vm_tools::cicerone::StartLxdContainerRequest& request,
+      DBusMethodCallback<vm_tools::cicerone::StartLxdContainerResponse>
+          callback) override;
+
+  // Fake version of the method that gets the container username.
+  // |callback| is called when the method completes.
+  void GetLxdContainerUsername(
+      const vm_tools::cicerone::GetLxdContainerUsernameRequest& request,
+      DBusMethodCallback<vm_tools::cicerone::GetLxdContainerUsernameResponse>
+          callback) override;
+
+  // Fake version of the method that sets the container user.
+  // |callback| is called when the method completes.
+  void SetUpLxdContainerUser(
+      const vm_tools::cicerone::SetUpLxdContainerUserRequest& request,
+      DBusMethodCallback<vm_tools::cicerone::SetUpLxdContainerUserResponse>
+          callback) override;
+
   // Fake version of the method that waits for the Cicerone service to be
   // availble.  |callback| is called after the method call finishes.
   void WaitForServiceToBeAvailable(
@@ -71,7 +111,20 @@ class CHROMEOS_EXPORT FakeCiceroneClient : public CiceroneClient {
 
   // Set InstallLinuxPackageProgressSignalConnected state
   void set_install_linux_package_progress_signal_connected(bool connected) {
-    is_install_linux_package_progress_signal_connected = connected;
+    is_install_linux_package_progress_signal_connected_ = connected;
+  }
+
+  // Set LxdContainerCreatedSignalConnected state
+  void set_lxd_container_created_signal_connected(bool connected) {
+    is_lxd_container_created_signal_connected_ = connected;
+  }
+  // Set LxdContainerDownloadingSignalConnected state
+  void set_lxd_container_downloading_signal_connected(bool connected) {
+    is_lxd_container_downloading_signal_connected_ = connected;
+  }
+  // Set TremplinStartedSignalConnected state
+  void set_tremplin_started_signal_connected(bool connected) {
+    is_tremplin_started_signal_connected_ = connected;
   }
 
   void set_launch_container_application_response(
@@ -93,19 +146,53 @@ class CHROMEOS_EXPORT FakeCiceroneClient : public CiceroneClient {
     install_linux_package_response_ = install_linux_package_response;
   }
 
+  void set_create_lxd_container_response(
+      const vm_tools::cicerone::CreateLxdContainerResponse&
+          create_lxd_container_response) {
+    create_lxd_container_response_ = create_lxd_container_response;
+  }
+
+  void set_start_lxd_container_response(
+      const vm_tools::cicerone::StartLxdContainerResponse&
+          start_lxd_container_response) {
+    start_lxd_container_response_ = start_lxd_container_response;
+  }
+
+  void set_get_lxd_container_username_response(
+      const vm_tools::cicerone::GetLxdContainerUsernameResponse&
+          get_lxd_container_username_response) {
+    get_lxd_container_username_response_ = get_lxd_container_username_response;
+  }
+
+  void set_setup_lxd_container_user_response(
+      const vm_tools::cicerone::SetUpLxdContainerUserResponse&
+          setup_lxd_container_user_response) {
+    setup_lxd_container_user_response_ = setup_lxd_container_user_response;
+  }
+
  protected:
   void Init(dbus::Bus* bus) override {}
 
  private:
   bool is_container_started_signal_connected_ = true;
   bool is_container_shutdown_signal_connected_ = true;
-  bool is_install_linux_package_progress_signal_connected = true;
+  bool is_install_linux_package_progress_signal_connected_ = true;
+  bool is_lxd_container_created_signal_connected_ = true;
+  bool is_lxd_container_downloading_signal_connected_ = true;
+  bool is_tremplin_started_signal_connected_ = true;
 
   vm_tools::cicerone::LaunchContainerApplicationResponse
       launch_container_application_response_;
   vm_tools::cicerone::ContainerAppIconResponse container_app_icon_response_;
   vm_tools::cicerone::InstallLinuxPackageResponse
       install_linux_package_response_;
+  vm_tools::cicerone::CreateLxdContainerResponse create_lxd_container_response_;
+  vm_tools::cicerone::StartLxdContainerResponse start_lxd_container_response_;
+  vm_tools::cicerone::GetLxdContainerUsernameResponse
+      get_lxd_container_username_response_;
+  vm_tools::cicerone::SetUpLxdContainerUserResponse
+      setup_lxd_container_user_response_;
+
   base::ObserverList<Observer> observer_list_;
 
   DISALLOW_COPY_AND_ASSIGN(FakeCiceroneClient);
