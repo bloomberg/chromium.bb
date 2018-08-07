e216fb1945954a31c31e2f7c13aca68d8ff37adc
Becca Hughes <beccahughes@chromium.org>
2018-08-07 22:56:59 +0000

    [Autoplay] Policy should follow kBlockAutoplayEnabled pref
    The autoplay policy should follow the kBlockAutoplayEnabled pref
    if the kAutoplaySoundSettings feature is enabled. If the pref is
    true we will use document-user-activation otherwise we will
    use no-user-gesture-required.
    
    The only exception is if the user has overridden the autoplay
    policy in which case we will use the autoplay policy command
    line switch.
    
    BUG=865548
    
    Change-Id: I79ba3bbf5542ba17af9ab96baef80cd89d5b5326
    Reviewed-on: https://chromium-review.googlesource.com/1157199
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581382}
    Former-commit-id: 7d2bbb8f69651b430dd20f570c13d66c05801c05

---
+++ chrome/browser/media/unified_autoplay_config.cc
@@ -0,0 +1,30 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/media/unified_autoplay_config.h"
+
+#include "chrome/browser/content_settings/host_content_settings_map_factory.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/common/pref_names.h"
+#include "components/content_settings/core/browser/host_content_settings_map.h"
+#include "components/pref_registry/pref_registry_syncable.h"
+#include "components/prefs/pref_service.h"
+
+// static
+void UnifiedAutoplayConfig::RegisterProfilePrefs(
+    user_prefs::PrefRegistrySyncable* registry) {
+  registry->RegisterBooleanPref(prefs::kBlockAutoplayEnabled, true);
+}
+
+// static
+bool UnifiedAutoplayConfig::ShouldBlockAutoplay(Profile* profile) {
+  HostContentSettingsMap* settings_map =
+      HostContentSettingsMapFactory::GetForProfile(profile);
+  if (settings_map->GetDefaultContentSetting(CONTENT_SETTINGS_TYPE_SOUND,
+                                             nullptr) == CONTENT_SETTING_BLOCK) {
+    return true;
+  }
+
+  return profile->GetPrefs()->GetBoolean(prefs::kBlockAutoplayEnabled);
+}
