3d22eef51241aae1eb106a2c60bc2d4d46049871
Rouslan Solomakhin <rouslan@chromium.org>
2018-08-07 16:30:52 +0000

    Enable skip UI by default on all platforms.
    Bug: 775647
    Change-Id: I78a9cf071fb843808effdd63c4380a99b59046a4
    Reviewed-on: https://chromium-review.googlesource.com/1165310
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581249}
    Former-commit-id: 24e796268134b1fdc8bd28b8999f60725f2a8acb

--- components/payments/core/features.cc
+++ components/payments/core/features.cc
@@ -24,13 +24,8 @@ const base::Feature kWebPaymentsMethodSectionOrderV2{
 const base::Feature kWebPaymentsModifiers{"WebPaymentsModifiers",
                                           base::FEATURE_ENABLED_BY_DEFAULT};
 
-#if defined(OS_ANDROID)
 const base::Feature kWebPaymentsSingleAppUiSkip{
     "WebPaymentsSingleAppUiSkip", base::FEATURE_ENABLED_BY_DEFAULT};
-#else
-const base::Feature kWebPaymentsSingleAppUiSkip{
-    "WebPaymentsSingleAppUiSkip", base::FEATURE_DISABLED_BY_DEFAULT};
-#endif
 
 const base::Feature kWebPaymentsJustInTimePaymentApp{
     "WebPaymentsJustInTimePaymentApp", base::FEATURE_DISABLED_BY_DEFAULT};
