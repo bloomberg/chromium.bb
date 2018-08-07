970a755a023691748edb586dbe99e0515d4b47a2
Fredrik Hubinette <hubbe@chromium.org>
2018-08-07 22:10:29 +0000

    Revert "Handle legacy camera app in consolidated icon"
    This reverts commit 4bda2511415313885e4124252c538b96f7a51fdc [formerly 5e61a5b773e5da7724683b7823690ba2f96e9238].
    
    Reason for revert: Seems to be calling LSAN problems on linux:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29
    
    BUG:  871986
    
    Original change's description:
    > Handle legacy camera app in consolidated icon
    > 
    > This CL does 2 things:
    > 1. Hide legacy camera app icon
    > 2. Launch legacy camera app (com.android.camera2) when
    > com.google.android.GoogleCameraArc is not available.
    > 
    > Sometimes when devices are upgraded from version with Camera2 installed,
    > GCA still has com.android.camera2 as the package name. That's why this
    > CL is needed.
    > 
    > Bug: b:112121210
    > Test: Tested on nautilus that after this CL, the legacy camera app icon
    > 
    > Change-Id: I16e4d29d4cb538bd2ad6607456bcf4667be1f188
    > Reviewed-on: https://chromium-review.googlesource.com/1127899
    > Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    > Commit-Queue: Sheng-hao Tsao <shenghao@google.com>
    > Cr-Commit-Position: refs/heads/master@{#581047}
    
    TBR=stevenjb@chromium.org,shenghao@google.com
    
    Change-Id: Ibd24eb5f9821e23a3986fbabc45db3d3ed14c81b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: b:112121210
    Reviewed-on: https://chromium-review.googlesource.com/1166004
    Reviewed-by: Fredrik Hubinette <hubbe@chromium.org>
    Commit-Queue: Fredrik Hubinette <hubbe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581359}
    Former-commit-id: f024069809d20c96ddd628f6a96095b87b714e0b

--- chrome/browser/ui/app_list/arc/arc_app_utils.cc
+++ chrome/browser/ui/app_list/arc/arc_app_utils.cc
@@ -85,11 +85,10 @@ constexpr char kLaunchFlags[] = "launchFlags";
 constexpr char kAndroidClockAppId[] = "ddmmnabaeomoacfpfjgghfpocfolhjlg";
 constexpr char kAndroidFilesAppId[] = "gmiohhmfhgfclpeacmdfancbipocempm";
 constexpr char kAndroidCameraAppId[] = "goamfaniemdfcajgcmmflhchgkmbngka";
-constexpr char kAndroidLegacyCameraAppId[] = "obfofkigjfamlldmipdegnjlcpincibc";
 
 constexpr char const* kAppIdsHiddenInLauncher[] = {
-    kAndroidClockAppId, kSettingsAppId, kAndroidFilesAppId, kAndroidCameraAppId,
-    kAndroidLegacyCameraAppId};
+    kAndroidClockAppId, kSettingsAppId, kAndroidFilesAppId,
+    kAndroidCameraAppId};
 
 // Returns true if |event_flags| came from a mouse or touch event.
 bool IsMouseOrTouchEventFromFlags(int event_flags) {
