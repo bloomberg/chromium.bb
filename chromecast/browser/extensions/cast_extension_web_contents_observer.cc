57ab8ed4da0035a32b0c66233470b4c23ec8f070
Francois Doray <fdoray@chromium.org>
2018-08-07 14:11:46 +0000

    Remove DEFINE_WEB_CONTENTS_USER_DATA_KEY in //chromecast.
    It is no longer necessary to invoke the DEFINE_WEB_CONTENTS_USER_DATA_KEY
    macro to define the global variable from which the key of a
    WebContentsUserData is derived. The key is now derived from a static
    local variable in WebContentsUserData::UserDataKey().
    See https://chromium-review.googlesource.com/c/chromium/src/+/1093015
    
    This CL was uploaded by git cl split.
    
    R=slan@chromium.org
    
    Bug: 589840
    Change-Id: I9742e8415f6b34bad58acba7ccdc50e4d5ddf12e
    Reviewed-on: https://chromium-review.googlesource.com/1150282
    Reviewed-by: Stephen Lanham <slan@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581215}
    Former-commit-id: 129ba66909d2e9c5ead426f1029ce8ef94d31ee7

--- chromecast/browser/extensions/cast_extension_web_contents_observer.cc
+++ chromecast/browser/extensions/cast_extension_web_contents_observer.cc
@@ -4,8 +4,6 @@
 
 #include "chromecast/browser/extensions/cast_extension_web_contents_observer.h"
 
-DEFINE_WEB_CONTENTS_USER_DATA_KEY(extensions::CastExtensionWebContentsObserver);
-
 namespace extensions {
 
 CastExtensionWebContentsObserver::CastExtensionWebContentsObserver(
