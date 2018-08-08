427e0065827c60f03bbdddd1c6dc9817b67b6820
Alexander Alekseev <alemate@chromium.org>
2018-08-08 02:15:28 +0000

    Chrome OS Sync Consent: Integrate with Consent Auditor.
    This Cl integrates ChromeOS sync consent screen with Consent auditor
    and adds an interactive ui test for the Sybnc consent screen.
    
    Bug: 842222
    Change-Id: I66e731ff0470add7d68f7faa6f5f541fe04138c5
    Reviewed-on: https://chromium-review.googlesource.com/1165106
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581443}
    Former-commit-id: 320c43bd08bc977ccd0bf9a4f9d52481d88d164b

--- chrome/browser/resources/chromeos/login/sync_consent.js
+++ chrome/browser/resources/chromeos/login/sync_consent.js
@@ -79,13 +79,56 @@ Polymer({
    * This is 'on-tap' event handler for 'AcceptAndContinue' button.
    * @private
    */
-  onSettingsSaveAndContinue_: function() {
+  onSettingsSaveAndContinue_: function(e) {
     if (this.$.reviewSettingsBox.checked) {
-      chrome.send('login.SyncConsentScreen.userActed', ['continue-and-review']);
+      chrome.send('login.SyncConsentScreen.continueAndReview', [
+        this.getConsentDescription_(), this.getConsentConfirmation_(e.path)
+      ]);
     } else {
-      chrome.send(
-          'login.SyncConsentScreen.userActed', ['continue-with-defaults']);
+      chrome.send('login.SyncConsentScreen.continueWithDefaults', [
+        this.getConsentDescription_(), this.getConsentConfirmation_(e.path)
+      ]);
     }
+  },
+
+  /**
+   * @param {!Array<!HTMLElement>} path Path of the click event. Must contain
+   *     a consent confirmation element.
+   * @return {string} The text of the consent confirmation element.
+   * @private
+   */
+  getConsentConfirmation_: function(path) {
+    for (let element of path) {
+      if (!element.hasAttribute)
+        continue;
+
+      if (element.hasAttribute('consent-confirmation'))
+        return element.innerHTML.trim();
+
+      // Search down in case of click on a button with description below.
+      let labels = element.querySelectorAll('[consent-confirmation]');
+      if (labels && labels.length > 0) {
+        assert(labels.length == 1);
+
+        let result = '';
+        for (let label of labels) {
+          result += label.innerHTML.trim();
+        }
+        return result;
+      }
+    }
+    assertNotReached('No consent confirmation element found.');
+    return '';
+  },
+
+  /** @return {!Array<string>} Text of the consent description elements. */
+  getConsentDescription_: function() {
+    let consentDescription =
+        Array.from(this.shadowRoot.querySelectorAll('[consent-description]'))
+            .filter(element => element.clientWidth * element.clientHeight > 0)
+            .map(element => element.innerHTML.trim());
+    assert(consentDescription);
+    return consentDescription;
   },
 
   /******************************************************
