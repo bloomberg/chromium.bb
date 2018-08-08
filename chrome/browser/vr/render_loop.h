d124a90d55db6e26d68ddab7a9280720fa5ae102
Aldo Culquicondor <acondor@chromium.org>
2018-08-08 19:35:14 +0000

    VR: Make controller_delegate private to RenderLoop
    Also ProcessControllerInput is now private to RenderLoop, to reduce the
    risk of updating the controller more than once per frame.
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:linux_vr;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Iae8b797027d7be147f2139276acd3b6e1264a4fe
    Reviewed-on: https://chromium-review.googlesource.com/1165834
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581662}
    Former-commit-id: 880ba3d357766f3a3435be3f24603b765e6a359c

--- chrome/browser/vr/render_loop.h
+++ chrome/browser/vr/render_loop.h
@@ -8,8 +8,10 @@
 #include <memory>
 
 #include "base/macros.h"
+#include "chrome/browser/vr/controller_delegate.h"
 #include "chrome/browser/vr/sliding_average.h"
 #include "chrome/browser/vr/vr_export.h"
+#include "device/vr/public/mojom/vr_service.mojom.h"
 
 namespace base {
 class TimeDelta;
@@ -19,10 +21,10 @@ class TimeTicks;
 namespace vr {
 
 enum class VrUiTestActivityResult;
-class ControllerDelegate;
 class GraphicsDelegate;
 class RenderLoopBrowserInterface;
 class UiInterface;
+struct ControllerTestInput;
 struct RenderInfo;
 struct UiTestActivityExpectation;
 struct UiTestState;
@@ -41,18 +43,26 @@ class VR_EXPORT RenderLoop {
                       size_t sliding_time_size);
   virtual ~RenderLoop();
 
+  virtual void OnPause();
+  virtual void OnResume();
+
+  void PerformControllerActionForTesting(ControllerTestInput controller_input);
   void SetUiExpectingActivityForTesting(
       UiTestActivityExpectation ui_expectation);
 
  protected:
+  void set_controller_delegate(std::unique_ptr<ControllerDelegate> delegate) {
+    controller_delegate_ = std::move(delegate);
+  }
+
   // Position, hide and/or show UI elements, process input and update textures.
   // Returns true if the scene changed.
   void UpdateUi(const RenderInfo& render_info,
                 base::TimeTicks currrent_time,
                 FrameType frame_type);
-  base::TimeDelta ProcessControllerInput(const RenderInfo& render_info,
-                                         base::TimeTicks current_time,
-                                         FrameType frame_type);
+  device::mojom::XRInputSourceStatePtr ProcessControllerInputForWebXr(
+      const RenderInfo& render_info,
+      base::TimeTicks current_time);
   void ForceExitVr();
 
   const SlidingTimeDeltaAverage& ui_controller_update_time() const {
@@ -63,15 +73,22 @@ class VR_EXPORT RenderLoop {
   }
 
   std::unique_ptr<UiInterface> ui_;
-  std::unique_ptr<ControllerDelegate> controller_delegate_;
   std::unique_ptr<GraphicsDelegate> graphics_delegate_;
 
  private:
+  base::TimeDelta ProcessControllerInput(const RenderInfo& render_info,
+                                         base::TimeTicks current_time);
+
   void ReportUiStatusForTesting(const base::TimeTicks& current_time,
                                 bool ui_updated);
   void ReportUiActivityResultForTesting(VrUiTestActivityResult result);
 
   RenderLoopBrowserInterface* browser_;
+
+  std::unique_ptr<ControllerDelegate> controller_delegate_;
+  std::unique_ptr<ControllerDelegate> controller_delegate_for_testing_;
+  bool using_controller_delegate_for_testing_ = false;
+
   std::unique_ptr<UiTestState> ui_test_state_;
   SlidingTimeDeltaAverage ui_processing_time_;
   SlidingTimeDeltaAverage ui_controller_update_time_;
