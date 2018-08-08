532bfb2bdf540299d57aefebdfdef7bc6c6d7941
Wei Li <weili@chromium.org>
2018-08-08 19:52:25 +0000

    Composite nested oopifs for printing
    When there are nested oopifs, an out-of-process subframe may have
    nested out-of-process subframe of its own. In such cases, we
    should link custom data field of PaintRecord to its place-holder
    SkPicture as well. This CL adds such processing in subframe
    printing.
    
    BUG=853942
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ib81e376839be8bd28258fd92ba22e29da566548d
    Reviewed-on: https://chromium-review.googlesource.com/1163829
    Commit-Queue: Wei Li <weili@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581669}
    Former-commit-id: 6b0cfc97ccc2d85e7112cf1b46534c0abdb77fa3

--- cc/paint/paint_record.h
+++ cc/paint/paint_record.h
@@ -21,12 +21,16 @@ using PaintRecord = PaintOpBuffer;
 CC_PAINT_EXPORT sk_sp<SkPicture> ToSkPicture(
     sk_sp<PaintRecord> record,
     const SkRect& bounds,
-    ImageProvider* image_provider = nullptr);
+    ImageProvider* image_provider = nullptr,
+    PlaybackParams::CustomDataRasterCallback callback =
+        PlaybackParams::CustomDataRasterCallback());
 
 CC_PAINT_EXPORT sk_sp<const SkPicture> ToSkPicture(
     sk_sp<const PaintRecord> record,
     const SkRect& bounds,
-    ImageProvider* image_provider = nullptr);
+    ImageProvider* image_provider = nullptr,
+    PlaybackParams::CustomDataRasterCallback callback =
+        PlaybackParams::CustomDataRasterCallback());
 
 }  // namespace cc
 
