9818cdbb2a9cb7c38b3e0b9e7d05436f17745fa2
Mike Klein <mtklein@chromium.org>
2018-08-09 23:11:22 +0000

    add guard for skia-review.googlesource.com/146522
    Should be a good number of teensy layout test diffs.
    
    Change-Id: I3dc74252fab7ec8ee4aa38d95ba1f60ed3e96b01
    Reviewed-on: https://chromium-review.googlesource.com/1169313
    Commit-Queue: Mike Klein <mtklein@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581950}
    Former-commit-id: 593fa77c299b8d25be132c8c57d527da802e9466

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -197,6 +197,8 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_AAA_CHOICE
 #endif
 
+#define SK_LEGACY_CONVERT_PIXELS_IMPL
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
