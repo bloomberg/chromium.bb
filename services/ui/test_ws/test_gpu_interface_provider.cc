bf037e90d492b54c45da0c530d44cb86c23633eb
Xiyuan Xia <xiyuan@chromium.org>
2018-08-09 23:04:36 +0000

    chromeos: Make test_ws bring up more services
    This is needed to make content_browsertests happy under mash.
    
    Bug: 866942
    Change-Id: I21885e4328706f83bb57805abe4cff581551120a
    Reviewed-on: https://chromium-review.googlesource.com/1168143
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581947}
    Former-commit-id: 7d38fcb5c241daecd9ee091136183ff050e21fba

--- services/ui/test_ws/test_gpu_interface_provider.cc
+++ services/ui/test_ws/test_gpu_interface_provider.cc
@@ -5,42 +5,60 @@
 #include "services/ui/test_ws/test_gpu_interface_provider.h"
 
 #include "base/bind.h"
-#include "components/discardable_memory/public/interfaces/discardable_shared_memory_manager.mojom.h"
+#include "base/logging.h"
+#include "components/discardable_memory/service/discardable_shared_memory_manager.h"
+#include "services/service_manager/public/cpp/bind_source_info.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
-#include "services/ui/public/interfaces/gpu.mojom.h"
-
-namespace {
-
-// A placeholder to prevent test crashes on unbound requests.
-void BindGpuRequest(ui::mojom::GpuRequest request) {
-  NOTIMPLEMENTED_LOG_ONCE();
-}
-
-// A placeholder to prevent test crashes on unbound requests.
-void BindDiscardableSharedMemoryManagerRequest(
-    discardable_memory::mojom::DiscardableSharedMemoryManagerRequest request) {
-  NOTIMPLEMENTED_LOG_ONCE();
-}
-
-}  // namespace
+#include "services/ui/gpu_host/gpu_host.h"
 
 namespace ui {
 namespace test {
 
-TestGpuInterfaceProvider::TestGpuInterfaceProvider() = default;
+TestGpuInterfaceProvider::TestGpuInterfaceProvider(
+    ui::gpu_host::GpuHost* gpu_host,
+    discardable_memory::DiscardableSharedMemoryManager*
+        discardable_shared_memory_manager)
+    : gpu_host_(gpu_host),
+      discardable_shared_memory_manager_(discardable_shared_memory_manager) {}
+
 TestGpuInterfaceProvider::~TestGpuInterfaceProvider() = default;
 
 void TestGpuInterfaceProvider::RegisterGpuInterfaces(
     service_manager::BinderRegistry* registry) {
-  registry->AddInterface(
-      base::BindRepeating(&BindDiscardableSharedMemoryManagerRequest));
-  registry->AddInterface(base::BindRepeating(BindGpuRequest));
+  registry->AddInterface(base::BindRepeating(
+      &TestGpuInterfaceProvider::BindDiscardableSharedMemoryManagerRequest,
+      base::Unretained(this)));
+  registry->AddInterface(base::BindRepeating(
+      &TestGpuInterfaceProvider::BindGpuRequest, base::Unretained(this)));
 }
 
 #if defined(USE_OZONE)
 void TestGpuInterfaceProvider::RegisterOzoneGpuInterfaces(
     service_manager::BinderRegistry* registry) {}
 #endif
+
+void TestGpuInterfaceProvider::BindDiscardableSharedMemoryManagerRequest(
+    discardable_memory::mojom::DiscardableSharedMemoryManagerRequest request) {
+  // |discardable_shared_memory_manager_| could be null. See test_ws.cc for
+  // details.
+  if (!discardable_shared_memory_manager_) {
+    NOTIMPLEMENTED_LOG_ONCE();
+    return;
+  }
+
+  discardable_shared_memory_manager_->Bind(std::move(request),
+                                           service_manager::BindSourceInfo());
+}
+
+void TestGpuInterfaceProvider::BindGpuRequest(ui::mojom::GpuRequest request) {
+  // |gpu_host_| could be null. See test_ws.cc for details.
+  if (!gpu_host_) {
+    NOTIMPLEMENTED_LOG_ONCE();
+    return;
+  }
+
+  gpu_host_->Add(std::move(request));
+}
 
 }  // namespace test
 }  // namespace ui
