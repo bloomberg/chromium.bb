70f68ebbe1e3b923b946ccdc6ccbaef9d2003de2
Trent Apted <tapted@chromium.org>
2018-08-09 05:08:31 +0000

    FileManager: Generate unit test html files from closure dependencies.
    JS files don't name their `#includes`, but for closure compilation we
    must effectively list each .js file's #includes in build dependencies.
    We can use this to generate the list of .js files, in the correct
    order, that we use for js unittests. Manually creating the
    foo_unittest.html files is fiddly, error-prone, and is not robust to
    files being moved around. However, it does require the unittest js
    file to be properly closure-compiled.
    
    Start by obsoleting the 3 foo_unittest.html files in
    gallery/image_editor, which are the only ones currently closure-
    compiled.
    
    Note that the dependency files are created _regardless_ of the
    `closure_compilation = true` gn flag. i.e. this does not introduce a
    dependency on the closure _compiler_: dependency information comes from
    the GN build files alone. The closure_compilation flag only affects the
    js_type_check(..) targets, but the approach here uses js_library(..)
    targets.
    
    Bug: 867700
    Cq-Include-Trybots: luci.chromium.try:closure_compilation
    Change-Id: I91b3a726b999a9f7db721fefc23b3f6512fd3c0c
    Reviewed-on: https://chromium-review.googlesource.com/1161720
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581795}
    Former-commit-id: efca1917701b0c85a0d7c3316eadaf179dbf1cc7

--- chrome/browser/chromeos/file_manager/file_manager_jstest_base.cc
+++ chrome/browser/chromeos/file_manager/file_manager_jstest_base.cc
@@ -4,7 +4,6 @@
 
 #include "chrome/browser/chromeos/file_manager/file_manager_jstest_base.h"
 
-#include <vector>
 #include "base/path_service.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -19,16 +18,31 @@ FileManagerJsTestBase::FileManagerJsTestBase(const base::FilePath& base_path)
 void FileManagerJsTestBase::RunTest(const base::FilePath& file) {
   base::FilePath root_path;
   ASSERT_TRUE(base::PathService::Get(base::DIR_SOURCE_ROOT, &root_path));
+  base::FilePath full_path = root_path.Append(base_path_).Append(file);
+  ASSERT_TRUE(base::PathExists(full_path)) << full_path.value();
+  RunTestImpl(net::FilePathToFileURL(full_path));
+}
 
-  const GURL url = net::FilePathToFileURL(
-      root_path.Append(base_path_)
-      .Append(file));
+void FileManagerJsTestBase::RunGeneratedTest(const std::string& file) {
+  base::FilePath path;
+  ASSERT_TRUE(base::PathService::Get(base::DIR_EXE, &path));
+  path = path.AppendASCII("gen");
+
+  // Serve the generated html file from out/gen. It references files from
+  // DIR_SOURCE_ROOT, so serve from there as well. An alternative would be to
+  // copy the js files as a build step and serve file:// URLs, but the embedded
+  // test server gives better output for troubleshooting errors.
+  embedded_test_server()->ServeFilesFromDirectory(path.Append(base_path_));
+  embedded_test_server()->ServeFilesFromSourceDirectory(base::FilePath());
+
+  ASSERT_TRUE(embedded_test_server()->Start());
+  RunTestImpl(embedded_test_server()->GetURL(file));
+}
+
+void FileManagerJsTestBase::RunTestImpl(const GURL& url) {
   ui_test_utils::NavigateToURL(browser(), url);
-
   content::WebContents* const web_contents =
       browser()->tab_strip_model()->GetActiveWebContents();
   ASSERT_TRUE(web_contents);
-
-  const std::vector<int> empty_libraries;
-  EXPECT_TRUE(ExecuteWebUIResourceTest(web_contents, empty_libraries));
+  EXPECT_TRUE(ExecuteWebUIResourceTest(web_contents, {}));
 }
