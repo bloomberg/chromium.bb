544b1525d9ea5fdcb922a994cc87da33d357fbc2
Reilly Grant <reillyg@chromium.org>
2018-08-10 01:08:39 +0000

    Reland "Attach UsbContext to libusb_device(_handle) pointers"
    This is a reland of 18ec31342ece8d5a345f796e52b91d53966d5cd8 [formerly daa348fab9cae0919ebe9ded50782c6f7aa38be5]
    
    Original change's description:
    > Attach UsbContext to libusb_device(_handle) pointers
    > 
    > This change modifies ScopedLibusbDeviceRef and adds a new class
    > ScopedLibusbDeviceHandle. This now explicitly own a reference to the
    > UsbContext object (which reference counts a libusb_context) in addition
    > to the libusb_device or libusb_device_handle they are wrapping.
    > 
    > This resolves potential use-after-frees possible when posting tasks
    > with a ScopedLibusbDeviceRef since the UsbService the task is being
    > posted to could be destroyed before the task is executed. The
    > libusb_device would then be released after its libusb_context has been
    > destroyed.
    > 
    > This is based on https://crrev.com/c/1131949 after I realized there
    > where additional issues that needed to be addressed.
    > 
    > Bug: 838947
    > Change-Id: Idee02828bf615bd477033e585fffe03cf4d20595
    > Reviewed-on: https://chromium-review.googlesource.com/1145910
    > Reviewed-by: Ken Rockot <rockot@chromium.org>
    > Commit-Queue: Reilly Grant <reillyg@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#577329}
    
    Bug: 838947
    Change-Id: Ibb916b6dea86f05f0b6d2fdfa2d3b71f8c1e8c66
    Reviewed-on: https://chromium-review.googlesource.com/1158770
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581997}
    Former-commit-id: ca944e535b93f8f1c7b5b77ca95368b127eaa17c

--- device/usb/usb_service_impl.cc
+++ device/usb/usb_service_impl.cc
@@ -131,7 +131,7 @@ void GetDeviceListOnBlockingThread(
   std::vector<ScopedLibusbDeviceRef> scoped_devices;
   scoped_devices.reserve(device_count);
   for (ssize_t i = 0; i < device_count; ++i)
-    scoped_devices.emplace_back(platform_devices[i]);
+    scoped_devices.emplace_back(platform_devices[i], usb_context);
 
   // Free the list but don't unref the devices because ownership has been
   // been transfered to the elements of |scoped_devices|.
@@ -231,6 +231,7 @@ UsbServiceImpl::UsbServiceImpl()
       device_observer_(this),
 #endif
       weak_factory_(this) {
+  weak_self_ = weak_factory_.GetWeakPtr();
   base::PostTaskWithTraits(
       FROM_HERE, kBlockingTaskTraits,
       base::Bind(&InitializeUsbContextOnBlockingThread, task_runner(),
@@ -361,7 +362,7 @@ void UsbServiceImpl::OnDeviceList(
       // Mark the existing device object visited and remove it from the list so
       // it will not be ignored.
       it->second->set_visited(true);
-      device.reset();
+      device.Reset();
     }
   }
 
@@ -382,7 +383,7 @@ void UsbServiceImpl::OnDeviceList(
   // that have been removed don't remain in |ignored_devices_| indefinitely.
   ignored_devices_.clear();
   for (auto& device : *devices) {
-    if (device.is_valid())
+    if (device.IsValid())
       ignored_devices_.push_back(std::move(device));
   }
 
@@ -441,8 +442,8 @@ void UsbServiceImpl::EnumerateDevice(ScopedLibusbDeviceRef platform_device,
 
   devices_being_enumerated_.insert(platform_device.get());
 
-  auto device = base::MakeRefCounted<UsbDeviceImpl>(
-      context_, std::move(platform_device), descriptor);
+  auto device = base::MakeRefCounted<UsbDeviceImpl>(std::move(platform_device),
+                                                    descriptor);
   base::OnceClosure add_device =
       base::BindOnce(&UsbServiceImpl::AddDevice, weak_factory_.GetWeakPtr(),
                      refresh_complete, device);
@@ -458,7 +459,8 @@ void UsbServiceImpl::EnumerateDevice(ScopedLibusbDeviceRef platform_device,
     libusb_ref_device(device->platform_device());
     base::OnceClosure enumeration_failed = base::BindOnce(
         &UsbServiceImpl::EnumerationFailed, weak_factory_.GetWeakPtr(),
-        ScopedLibusbDeviceRef(device->platform_device()), refresh_complete);
+        ScopedLibusbDeviceRef(device->platform_device(), context_),
+        refresh_complete);
 
     device->Open(base::BindOnce(
         &OnDeviceOpenedReadDescriptors, descriptor.iManufacturer,
@@ -518,18 +520,18 @@ int LIBUSB_CALL UsbServiceImpl::HotplugCallback(libusb_context* context,
   // libusb does not transfer ownership of |device_raw| to this function so a
   // reference must be taken here.
   libusb_ref_device(device_raw);
-  ScopedLibusbDeviceRef device(device_raw);
+  ScopedLibusbDeviceRef device(device_raw, self->context_);
 
   switch (event) {
     case LIBUSB_HOTPLUG_EVENT_DEVICE_ARRIVED:
       self->task_runner()->PostTask(
           FROM_HERE, base::BindOnce(&UsbServiceImpl::OnPlatformDeviceAdded,
-                                    base::Unretained(self), std::move(device)));
+                                    self->weak_self_, std::move(device)));
       break;
     case LIBUSB_HOTPLUG_EVENT_DEVICE_LEFT:
       self->task_runner()->PostTask(
           FROM_HERE, base::BindOnce(&UsbServiceImpl::OnPlatformDeviceRemoved,
-                                    base::Unretained(self), std::move(device)));
+                                    self->weak_self_, std::move(device)));
       break;
     default:
       NOTREACHED();
