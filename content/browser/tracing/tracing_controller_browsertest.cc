97832874ea4e8c122812473e8f645c0e3606b277
vitaliii <vitaliii@chromium.org>
2018-08-10 08:50:34 +0000

    Disable some TracingControllerTest on Linux, because flaky.
    TracingControllerTest.DoubleStopTracing
    TracingControllerTest.EnableAndStopTracingWithEmptyFile
    
    TBR=oysteine@chromium.org
    
    Bug: 873073
    Change-Id: I1455984cf656abb30f700cdf30c5a9f770971b16
    Reviewed-on: https://chromium-review.googlesource.com/1170683
    Reviewed-by: vitaliii <vitaliii@chromium.org>
    Commit-Queue: vitaliii <vitaliii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582094}
    Former-commit-id: f616c54c91dbf061d4c13b54155d1884320e111e

--- content/browser/tracing/tracing_controller_browsertest.cc
+++ content/browser/tracing/tracing_controller_browsertest.cc
@@ -488,7 +488,8 @@ IN_PROC_BROWSER_TEST_F(TracingControllerTest,
 
 // TODO(crbug.com/871770): Disabled for failing on ASAN.
 // TODO(crbug.com/872631): Disabled on Android as flaky.
-#if defined(ADDRESS_SANITIZER) || defined(OS_ANDROID)
+// TODO(crbug.com/873073): Disabled on Linux as flaky.
+#if defined(ADDRESS_SANITIZER) || defined(OS_ANDROID) || defined(OS_LINUX)
 #define MAYBE_EnableAndStopTracingWithEmptyFile \
   DISABLED_EnableAndStopTracingWithEmptyFile
 #else
@@ -517,7 +518,8 @@ IN_PROC_BROWSER_TEST_F(TracingControllerTest,
 
 // TODO(crbug.com/871770): Disabled for failing on ASAN.
 // TODO(crbug.com/872631): Disabled on Android as flaky.
-#if defined(ADDRESS_SANITIZER) || defined(OS_ANDROID)
+// TODO(crbug.com/873073): Disabled on Linux as flaky.
+#if defined(ADDRESS_SANITIZER) || defined(OS_ANDROID) || defined(OS_LINUX)
 #define MAYBE_DoubleStopTracing DISABLED_DoubleStopTracing
 #else
 #define MAYBE_DoubleStopTracing DoubleStopTracing
