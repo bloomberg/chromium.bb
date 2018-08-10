ea501422081905b828c70e737e0ff56a9aae5e9b
Oystein Eftevaag <oysteine@google.com>
2018-08-10 02:13:49 +0000

    Enable Perfetto usage for the loading perf tests.
    This enables it for a limited subset of tests to catch any
    regressions before enabling it for all perf tests, and will only
    have an effect on Mac/Windows/Android platforms currently.
    
    Bug: 839084
    Change-Id: Ifcfcaefbbb3be177f9a5941031e48ead60f1fc77
    Reviewed-on: https://chromium-review.googlesource.com/1169768
    Commit-Queue: oysteine <oysteine@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#582017}
    Former-commit-id: 49a68554cf5658d0cc3741a89db2fca1380a6fcc

--- tools/perf/benchmarks/loading.py
+++ tools/perf/benchmarks/loading.py
@@ -18,6 +18,11 @@ class _LoadingBase(perf_benchmark.PerfBenchmark):
 
   options = {'pageset_repeat': 2}
 
+  def SetExtraBrowserOptions(self, options):
+    options.AppendExtraBrowserArgs([
+        '--enable-features=TracingPerfettoBackend',
+    ])
+
   def CreateCoreTimelineBasedMeasurementOptions(self):
     tbm_options = timeline_based_measurement.Options()
     loading_metrics_category.AugmentOptionsForLoadingMetrics(tbm_options)
