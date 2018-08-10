2cc50400070a932871d6b5195622616ef704c708
Joe Mason <joenotcharles@chromium.org>
2018-08-10 21:34:05 +0000

    Make ExecutionMode pretty-printer available for CHECK outside tests
    R=proberge
    
    Bug: 872748
    Change-Id: I42ae9acf6bd189ef45543ddfa15521262c56b95f
    Reviewed-on: https://chromium-review.googlesource.com/1171549
    Reviewed-by: proberge <proberge@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582343}
    Former-commit-id: da2cbfa6d74f34482c227e4715bc0b03c3298f01

--- components/chrome_cleaner/public/constants/constants.cc
+++ components/chrome_cleaner/public/constants/constants.cc
@@ -45,4 +45,46 @@ const wchar_t kStartTimeValueName[] = L"StartTime";
 const wchar_t kUploadResultsValueName[] = L"UploadResults";
 const wchar_t kVersionValueName[] = L"Version";
 
+std::ostream& operator<<(std::ostream& stream, ExecutionMode mode) {
+  switch (mode) {
+    case ExecutionMode::kNone:
+      stream << "ExecutionModeNone";
+      break;
+    case ExecutionMode::kScanning:
+      stream << "ExecutionModeScanning";
+      break;
+    case ExecutionMode::kCleanup:
+      stream << "ExecutionModeCleanup";
+      break;
+    case ExecutionMode::kNumValues:
+      stream << "ExecutionModeNumValues";
+      break;
+  }
+  return stream;
+}
+
+std::ostream& operator<<(std::ostream& stream, ChromePromptValue value) {
+  switch (value) {
+    case ChromePromptValue::kUnspecified:
+      stream << "ChromePromptUnspecified";
+      break;
+    case ChromePromptValue::kPrompted:
+      stream << "ChromePromptPrompted";
+      break;
+    case ChromePromptValue::kUserInitiated:
+      stream << "ChromePromptUserInitiated";
+      break;
+    case ChromePromptValue::kLegacyNotPrompted:
+      stream << "ChromePromptLegacyNotPrompted";
+      break;
+    case ChromePromptValue::kLegacyUnknown:
+      stream << "ChromePromptLegacyUnknown";
+      break;
+    case ChromePromptValue::kLegacyShownFromMenu:
+      stream << "ChromePromptLegacyShownFromMenu";
+      break;
+  }
+  return stream;
+}
+
 }  // namespace chrome_cleaner
