562de5380624d0398135a71d286b67a0e93d9209
Peter Kasting <pkasting@chromium.org>
2018-08-10 22:43:55 +0000

    Disable failing test (FLAKY_ has no meaning)
    Bug: 871812
    Change-Id: I0e4ee3e6539a6e4fa6c48495782a75a74a71dbc3
    TBR: mgiuca
    Reviewed-on: https://chromium-review.googlesource.com/1171681
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582366}
    Former-commit-id: 1b6a32c22de15bea00ed58fa55fdeafdb91767f6

--- third_party/blink/renderer/core/css/font_face_cache_test.cc
+++ third_party/blink/renderer/core/css/font_face_cache_test.cc
@@ -223,7 +223,7 @@ FontSelectionRange ExpectedRangeForChoice(
 }
 
 // Flaky; https://crbug.com/871812
-TEST_F(FontFaceCacheTest, FLAKY_MatchCombinations) {
+TEST_F(FontFaceCacheTest, DISABLED_MatchCombinations) {
   CSSValue* widths[] = {CSSIdentifierValue::Create(CSSValueCondensed),
                         CSSIdentifierValue::Create(CSSValueExpanded)};
   CSSValue* slopes[] = {CSSIdentifierValue::Create(CSSValueNormal),
