1a67f2a899feba6a3c3470d7513b7b1ed2042198
Mohsen Izadi <mohsen@chromium.org>
2018-08-10 03:07:26 +0000

    Make callback in BrowserGpuClientDelegate non-member function
    BrowserGpuClientDelegate might have been destroyed when
    OnEstablishGpuChannel callback is run. Although this callback does not
    use |this| and just runs the other callback passed to it, it is
    technically an undefined behavior. Making it a non-member function,
    helps us avoid this undefined behavior.
    
    BUG=872140,872219,872244
    
    Change-Id: Id7641c6c3ff2dcb153e9ed9b245013e878cc947a
    Reviewed-on: https://chromium-review.googlesource.com/1169964
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582031}
    Former-commit-id: 1a49e225c111a22448c13c1527904ef7caa8da4d

--- content/browser/gpu/browser_gpu_client_delegate.cc
+++ content/browser/gpu/browser_gpu_client_delegate.cc
@@ -4,11 +4,42 @@
 
 #include "content/browser/gpu/browser_gpu_client_delegate.h"
 
+#include "content/browser/gpu/gpu_process_host.h"
 #include "gpu/config/gpu_feature_info.h"
 #include "gpu/config/gpu_info.h"
 #include "mojo/public/cpp/system/message_pipe.h"
 
 namespace content {
+namespace {
+
+void OnEstablishGpuChannel(
+    GpuClientDelegate::EstablishGpuChannelCallback callback,
+    mojo::ScopedMessagePipeHandle channel_handle,
+    const gpu::GPUInfo& gpu_info,
+    const gpu::GpuFeatureInfo& gpu_feature_info,
+    GpuProcessHost::EstablishChannelStatus status) {
+  if (!callback)
+    return;
+
+  GpuClientDelegate::EstablishGpuChannelStatus delegate_status;
+  switch (status) {
+    case GpuProcessHost::EstablishChannelStatus::GPU_ACCESS_DENIED:
+      delegate_status =
+          GpuClientDelegate::EstablishGpuChannelStatus::kGpuAccessDenied;
+      break;
+    case GpuProcessHost::EstablishChannelStatus::GPU_HOST_INVALID:
+      delegate_status =
+          GpuClientDelegate::EstablishGpuChannelStatus::kGpuHostInvalid;
+      break;
+    case GpuProcessHost::EstablishChannelStatus::SUCCESS:
+      delegate_status = GpuClientDelegate::EstablishGpuChannelStatus::kSuccess;
+      break;
+  }
+  std::move(callback).Run(std::move(channel_handle), gpu_info, gpu_feature_info,
+                          delegate_status);
+}
+
+}  // namespace
 
 BrowserGpuClientDelegate::BrowserGpuClientDelegate() = default;
 
@@ -35,35 +66,7 @@ void BrowserGpuClientDelegate::EstablishGpuChannel(
   const bool is_gpu_host = false;
   host->EstablishGpuChannel(
       client_id, client_tracing_id, is_gpu_host,
-      base::BindOnce(&BrowserGpuClientDelegate::OnEstablishGpuChannel,
-                     base::Unretained(this), std::move(callback)));
-}
-
-void BrowserGpuClientDelegate::OnEstablishGpuChannel(
-    GpuClientDelegate::EstablishGpuChannelCallback callback,
-    mojo::ScopedMessagePipeHandle channel_handle,
-    const gpu::GPUInfo& gpu_info,
-    const gpu::GpuFeatureInfo& gpu_feature_info,
-    GpuProcessHost::EstablishChannelStatus status) {
-  if (!callback)
-    return;
-
-  GpuClientDelegate::EstablishGpuChannelStatus delegate_status;
-  switch (status) {
-    case GpuProcessHost::EstablishChannelStatus::GPU_ACCESS_DENIED:
-      delegate_status =
-          GpuClientDelegate::EstablishGpuChannelStatus::kGpuAccessDenied;
-      break;
-    case GpuProcessHost::EstablishChannelStatus::GPU_HOST_INVALID:
-      delegate_status =
-          GpuClientDelegate::EstablishGpuChannelStatus::kGpuHostInvalid;
-      break;
-    case GpuProcessHost::EstablishChannelStatus::SUCCESS:
-      delegate_status = GpuClientDelegate::EstablishGpuChannelStatus::kSuccess;
-      break;
-  }
-  std::move(callback).Run(std::move(channel_handle), gpu_info, gpu_feature_info,
-                          delegate_status);
+      base::BindOnce(&OnEstablishGpuChannel, std::move(callback)));
 }
 
 }  // namespace content
