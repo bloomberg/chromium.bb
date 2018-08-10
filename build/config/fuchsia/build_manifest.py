09a2dc8358ccfc35bd03192ad4b7476ff189a772
Wez <wez@chromium.org>
2018-08-10 01:16:48 +0000

    Temporarily bundle all SOs from the SDK "dist" directory into packages.
    We previously attempted to include only the SOs listed in the primary
    executable's ELF headers as NEEDED. This causes SOs needed by other
    executables in the package to be missed, as well as any libraries used
    by the executable, but loaded via dlopen() rather than at load-time.
    
    Bug: 861931
    Change-Id: If72d8007ca7d18674dd28eb310a0a5aa76519516
    Reviewed-on: https://chromium-review.googlesource.com/1170179
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#581998}
    Former-commit-id: 30029f7b62764903416a8648bec3a1c6ff509347

--- build/config/fuchsia/build_manifest.py
+++ build/config/fuchsia/build_manifest.py
@@ -179,10 +179,13 @@ def BuildManifest(root_dir, out_dir, app_name, app_filename,
     # Compute the set of dynamic libraries used by the application or its
     # transitive dependencies (dist libs and components), and merge the result
     # with |expanded_files| so that they are included in the manifest.
-    expanded_files = expanded_files.union(
-        ComputeTransitiveLibDeps(
-            app_filename,
-            {os.path.basename(f): f for f in expanded_files.union(dist_libs)}))
+
+    # TODO(https://crbug.com/861931): Temporarily just include all |dist_libs|.
+    #expanded_files = expanded_files.union(
+    #    ComputeTransitiveLibDeps(
+    #        app_filename,
+    #        {os.path.basename(f): f for f in expanded_files.union(dist_libs)}))
+    expanded_files = expanded_files.union(dist_libs)
 
     # Format and write out the manifest contents.
     app_found = False
