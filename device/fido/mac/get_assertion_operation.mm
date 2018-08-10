1b21f0ff3cea240b102bd26a7aca0fd7d3bbe3ae
Martin Kreichgauer <martinkr@google.com>
2018-08-10 22:46:01 +0000

    fido/mac: look up credential IDs before proceeding with GetAssertion requests
    This changes the GetAssertion operation for the Touch ID authenticator
    to locate credential IDs in the keychain at the beginning of the
    request. If no matching credential exists, the user is not prompted for
    a fingerprint. A method to locate a credential by ID is extracted from
    the existing code and moved into the Keychain class.
    
    This is done in anticipation of adding the WebAuthn UI. With the new UI,
    GetAssertion requests will only be sent to the Touch ID authenticator,
    if we know a matching credential ID to exist in the keychain.
    
    Bug: 871370
    
    Change-Id: Ie5e4916df2a68bc0e9d7552322d6b993069ce95b
    Reviewed-on: https://chromium-review.googlesource.com/1163927
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582369}
    Former-commit-id: a4ac527a4777da75ba6c44ed4a7e1837ed92693d

--- device/fido/mac/get_assertion_operation.mm
+++ device/fido/mac/get_assertion_operation.mm
@@ -47,20 +47,8 @@ void GetAssertionOperation::Run() {
     return;
   }
 
-  // Prompt the user for consent.
-  // TODO(martinkr): Localize reason strings.
-  PromptTouchId("sign in to " + RpId());
-}
-
-void GetAssertionOperation::PromptTouchIdDone(bool success) {
-  if (!success) {
-    std::move(callback())
-        .Run(CtapDeviceResponseCode::kCtap2ErrOperationDenied, base::nullopt);
-    return;
-  }
-
   // Collect the credential ids from allowList. If allowList is absent, we will
-  // just pick the first available credential for the RP below.
+  // pick the first available credential for the RP.
   std::set<std::vector<uint8_t>> allowed_credential_ids;
   if (request().allow_list()) {
     for (const PublicKeyCredentialDescriptor& desc : *request().allow_list()) {
@@ -72,64 +60,37 @@ void GetAssertionOperation::PromptTouchIdDone(bool success) {
   }
 
   // Fetch credentials for RP from the request and current user profile.
-  ScopedCFTypeRef<CFArrayRef> keychain_items;
-  base::ScopedCFTypeRef<CFMutableDictionaryRef> query = DefaultKeychainQuery();
-  CFDictionarySetValue(query, kSecUseAuthenticationContext,
-                       authentication_context());
-  CFDictionarySetValue(query, kSecReturnRef, @YES);
-  CFDictionarySetValue(query, kSecReturnAttributes, @YES);
-  CFDictionarySetValue(query, kSecMatchLimit, kSecMatchLimitAll);
+  credential_ = FindCredentialInKeychain(
+      keychain_access_group(), metadata_secret(), RpId(),
+      allowed_credential_ids, authentication_context());
+  if (!credential_) {
+    // For now, don't show a Touch ID prompt if no credential exists.
+    // TODO(martinkr): Prompt for the fingerprint anyway, once dispatch to this
+    // authenticator is moved behind user interaction with the authenticator
+    // selection UI.
+    std::move(callback())
+        .Run(CtapDeviceResponseCode::kCtap2ErrNoCredentials, base::nullopt);
+    return;
+  }
 
-  OSStatus status = Keychain::GetInstance().ItemCopyMatching(
-      query, reinterpret_cast<CFTypeRef*>(keychain_items.InitializeInto()));
-  if (status == errSecItemNotFound) {
-    DVLOG(1) << "no credentials found for RP";
+  // Prompt the user for consent.
+  // TODO(martinkr): Localize reason strings.
+  PromptTouchId("sign in to " + RpId());
+}
+
+void GetAssertionOperation::PromptTouchIdDone(bool success) {
+  DCHECK(credential_);
+
+  if (!success) {
     std::move(callback())
-        .Run(CtapDeviceResponseCode::kCtap2ErrNoCredentials, base::nullopt);
-    return;
-  }
-  if (status != errSecSuccess) {
-    OSSTATUS_DLOG(ERROR, status) << "SecItemCopyMatching failed";
-    std::move(callback())
-        .Run(CtapDeviceResponseCode::kCtap2ErrOther, base::nullopt);
-    return;
-  }
-  SecKeyRef private_key = nil;  // Owned by |keychain_items|.
-  std::vector<uint8_t> credential_id;
-  for (CFIndex i = 0; i < CFArrayGetCount(keychain_items); ++i) {
-    CFDictionaryRef attributes = base::mac::CFCast<CFDictionaryRef>(
-        CFArrayGetValueAtIndex(keychain_items, i));
-    CFDataRef application_label = base::mac::GetValueFromDictionary<CFDataRef>(
-        attributes, kSecAttrApplicationLabel);
-    SecKeyRef key =
-        base::mac::GetValueFromDictionary<SecKeyRef>(attributes, kSecValueRef);
-    if (!application_label || !key) {
-      // Corrupted keychain?
-      DLOG(ERROR) << "could not find application label or key ref: "
-                  << attributes;
-      continue;
-    }
-    std::vector<uint8_t> cid(CFDataGetBytePtr(application_label),
-                             CFDataGetBytePtr(application_label) +
-                                 CFDataGetLength(application_label));
-    if (allowed_credential_ids.empty() ||
-        allowed_credential_ids.find(cid) != allowed_credential_ids.end()) {
-      private_key = key;
-      credential_id = std::move(cid);
-      break;
-    }
-  }
-  if (!private_key) {
-    DVLOG(1) << "no allowed credential found";
-    std::move(callback())
-        .Run(CtapDeviceResponseCode::kCtap2ErrNoCredentials, base::nullopt);
+        .Run(CtapDeviceResponseCode::kCtap2ErrOperationDenied, base::nullopt);
     return;
   }
 
   // Decrypt the user entity from the credential ID.
   base::Optional<CredentialMetadata::UserEntity> credential_user =
       CredentialMetadata::UnsealCredentialId(metadata_secret(), RpId(),
-                                             credential_id);
+                                             credential_->credential_id);
   if (!credential_user) {
     // The keychain query already filtered for the RP ID encoded under this
     // operation's metadata secret, so the credential id really should have
@@ -141,25 +102,27 @@ void GetAssertionOperation::PromptTouchIdDone(bool success) {
   }
 
   base::ScopedCFTypeRef<SecKeyRef> public_key(
-      Keychain::GetInstance().KeyCopyPublicKey(private_key));
+      Keychain::GetInstance().KeyCopyPublicKey(credential_->private_key));
   if (!public_key) {
     DLOG(ERROR) << "failed to get public key for credential id "
-                << base::HexEncode(credential_id.data(), credential_id.size());
+                << base::HexEncode(credential_->credential_id.data(),
+                                   credential_->credential_id.size());
     std::move(callback())
         .Run(CtapDeviceResponseCode::kCtap2ErrOther, base::nullopt);
     return;
   }
 
   base::Optional<AuthenticatorData> authenticator_data = MakeAuthenticatorData(
-      RpId(), credential_id, SecKeyRefToECPublicKey(public_key));
+      RpId(), credential_->credential_id, SecKeyRefToECPublicKey(public_key));
   if (!authenticator_data) {
     DLOG(ERROR) << "MakeAuthenticatorData failed";
     std::move(callback())
         .Run(CtapDeviceResponseCode::kCtap2ErrOther, base::nullopt);
     return;
   }
-  base::Optional<std::vector<uint8_t>> signature = GenerateSignature(
-      *authenticator_data, request().client_data_hash(), private_key);
+  base::Optional<std::vector<uint8_t>> signature =
+      GenerateSignature(*authenticator_data, request().client_data_hash(),
+                        credential_->private_key);
   if (!signature) {
     DLOG(ERROR) << "GenerateSignature failed";
     std::move(callback())
@@ -169,7 +132,7 @@ void GetAssertionOperation::PromptTouchIdDone(bool success) {
   auto response = AuthenticatorGetAssertionResponse(
       std::move(*authenticator_data), std::move(*signature));
   response.SetCredential(PublicKeyCredentialDescriptor(
-      CredentialType::kPublicKey, std::move(credential_id)));
+      CredentialType::kPublicKey, std::move(credential_->credential_id)));
   response.SetUserEntity(credential_user->ToPublicKeyCredentialUserEntity());
 
   std::move(callback())
--- device/fido/mac/keychain.mm
+++ device/fido/mac/keychain.mm
@@ -4,6 +4,13 @@
 
 #include "device/fido/mac/keychain.h"
 
+#import <Foundation/Foundation.h>
+
+#include "base/mac/foundation_util.h"
+#include "base/mac/mac_logging.h"
+#include "base/strings/sys_string_conversions.h"
+#include "device/fido/mac/credential_metadata.h"
+
 namespace device {
 namespace fido {
 namespace mac {
@@ -60,6 +67,85 @@ OSStatus Keychain::ItemCopyMatching(CFDictionaryRef query, CFTypeRef* result) {
 
 OSStatus Keychain::ItemDelete(CFDictionaryRef query) {
   return SecItemDelete(query);
+}
+
+Credential::Credential(base::ScopedCFTypeRef<SecKeyRef> private_key_,
+                       std::vector<uint8_t> credential_id_)
+    : private_key(std::move(private_key_)),
+      credential_id(std::move(credential_id_)) {}
+Credential::~Credential() = default;
+Credential::Credential(Credential&& other) = default;
+Credential& Credential::operator=(Credential&& other) = default;
+
+base::Optional<Credential> FindCredentialInKeychain(
+    const std::string& keychain_access_group,
+    const std::string& metadata_secret,
+    const std::string& rp_id,
+    const std::set<std::vector<uint8_t>>& credential_id_filter,
+    LAContext* authentication_context) {
+  base::Optional<std::string> encoded_rp_id =
+      CredentialMetadata::EncodeRpId(metadata_secret, rp_id);
+  if (!encoded_rp_id)
+    return base::nullopt;
+
+  base::ScopedCFTypeRef<CFMutableDictionaryRef> query(
+      CFDictionaryCreateMutable(kCFAllocatorDefault, 0, nullptr, nullptr));
+  CFDictionarySetValue(query, kSecClass, kSecClassKey);
+  CFDictionarySetValue(query, kSecAttrAccessGroup,
+                       base::SysUTF8ToNSString(keychain_access_group));
+  CFDictionarySetValue(query, kSecAttrLabel,
+                       base::SysUTF8ToNSString(*encoded_rp_id));
+  if (authentication_context) {
+    CFDictionarySetValue(query, kSecUseAuthenticationContext,
+                         authentication_context);
+  }
+  CFDictionarySetValue(query, kSecReturnRef, @YES);
+  CFDictionarySetValue(query, kSecReturnAttributes, @YES);
+  CFDictionarySetValue(query, kSecMatchLimit, kSecMatchLimitAll);
+
+  base::ScopedCFTypeRef<CFArrayRef> keychain_items;
+  OSStatus status = Keychain::GetInstance().ItemCopyMatching(
+      query, reinterpret_cast<CFTypeRef*>(keychain_items.InitializeInto()));
+  if (status == errSecItemNotFound) {
+    // No credentials for the RP.
+    return base::nullopt;
+  }
+  if (status != errSecSuccess) {
+    OSSTATUS_DLOG(ERROR, status) << "SecItemCopyMatching failed";
+    return base::nullopt;
+  }
+
+  // Credentials for the RP exist. Find a match.
+  std::vector<uint8_t> credential_id;
+  base::ScopedCFTypeRef<SecKeyRef> private_key(nil);
+  for (CFIndex i = 0; i < CFArrayGetCount(keychain_items); ++i) {
+    CFDictionaryRef attributes = base::mac::CFCast<CFDictionaryRef>(
+        CFArrayGetValueAtIndex(keychain_items, i));
+    CFDataRef application_label = base::mac::GetValueFromDictionary<CFDataRef>(
+        attributes, kSecAttrApplicationLabel);
+    SecKeyRef key =
+        base::mac::GetValueFromDictionary<SecKeyRef>(attributes, kSecValueRef);
+    if (!application_label || !key) {
+      // Corrupted keychain?
+      DLOG(ERROR) << "could not find application label or key ref: "
+                  << attributes;
+      continue;
+    }
+    std::vector<uint8_t> cid(CFDataGetBytePtr(application_label),
+                             CFDataGetBytePtr(application_label) +
+                                 CFDataGetLength(application_label));
+    if (credential_id_filter.empty() ||
+        credential_id_filter.find(cid) != credential_id_filter.end()) {
+      private_key.reset(key, base::scoped_policy::RETAIN);
+      credential_id = std::move(cid);
+      break;
+    }
+  }
+  if (private_key == nil) {
+    DVLOG(1) << "no allowed credential found";
+    return base::nullopt;
+  }
+  return Credential(std::move(private_key), std::move(credential_id));
 }
 
 }  // namespace mac
