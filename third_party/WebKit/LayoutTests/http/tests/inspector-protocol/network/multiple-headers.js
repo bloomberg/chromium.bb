5180ebabe973b14145e86cceb026a511d56c07f6
Andrey Kosyakov <caseq@chromium.org>
2018-08-10 02:02:39 +0000

    Fix http/tests/inspector-protocol/network/multiple-headers.js to use same-origin request
    We accidentally used a CORS fetch() to a resource that dit not allow CORS access in the test,
    expecting Network.loadingFinished to happen after the resource is loaded. However, with
    per-message dispatch tasks (https://chromium-review.googlesource.com/c/chromium/src/+/1145692),
    the request is cancelled before we receive loadingFinished, so we now get loadingFailed
    with net::ERR_ABORTED instead.
    
    Bug: 872081
    Change-Id: I9cfd6c8ebc49f1e835916fb87f4d6cea0e4262b1
    Reviewed-on: https://chromium-review.googlesource.com/1168185
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582014}
    Former-commit-id: 6edf709c841f879bdfee2f1b2d9aab913c08b992

--- third_party/WebKit/LayoutTests/http/tests/inspector-protocol/network/multiple-headers.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector-protocol/network/multiple-headers.js
@@ -3,11 +3,11 @@
       '../resources/test-page.html',
       `Tests that multiple HTTP headers with same name are correctly folded into one LF-separated line.`);
 
-  const url = 'http://localhost:8000/inspector-protocol/network/resources/multiple-headers.php';
+  const url = 'http://127.0.0.1:8000/inspector-protocol/network/resources/multiple-headers.php';
   await dp.Network.enable();
   await dp.Page.enable();
 
-  session.evaluate(`fetch("${url}?fetch=1")`);
+  session.evaluate(`fetch("${url}?fetch=1").then(r => r.text())`);
   const fetchResponse = (await dp.Network.onceResponseReceived()).params.response;
   testRunner.log(`Pragma header of fetch of ${fetchResponse.url}: ${fetchResponse.headers['Access-Control-Pragma']}`);
   await dp.Network.onceLoadingFinished();
