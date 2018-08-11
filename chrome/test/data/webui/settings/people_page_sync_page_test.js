2ece740555c02bf353a9bb84907cbed858c56b8d
David Roger <droger@chromium.org>
2018-08-11 06:43:35 +0000

    [Settings] UserEvents depends on history and passphrase
    This is a reland of:
    https://chromium-review.googlesource.com/1170202
    
    which was reverted because of missing svg icon.
    
    The user events UI toggle should be grayed out and turned off
    when there is a passphrase or when history sync is disabled.
    Note that this is purely a UI change: the internal sync datatype
    is not really turned off in that case, and we assume that the Sync
    engines will correctly stop syncing user events in these cases,
    even if the preference is still ON.
    
    Bug: 865522, 865537
    Change-Id: I745f048b3ca301231f57c04b82d0d9d82a6f3b90
    Reviewed-on: https://chromium-review.googlesource.com/1170825
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582433}
    Former-commit-id: 31322898bc81e2e07e3d9f89aa051a60c5f93215

--- chrome/test/data/webui/settings/people_page_sync_page_test.js
+++ chrome/test/data/webui/settings/people_page_sync_page_test.js
@@ -54,7 +54,24 @@ cr.define('settings_people_page_sync_page', function() {
         typedUrlsEnforced: false,
         typedUrlsRegistered: true,
         typedUrlsSynced: true,
+        userEventsEnforced: false,
+        userEventsRegistered: true,
+        userEventsSynced: true,
       };
+    }
+
+    function openDatatypeConfigurationWithUnifiedConsent(prefs) {
+      syncPage.unifiedConsentEnabled = true;
+      cr.webUIListenerCallback('sync-prefs-changed', prefs);
+
+      Polymer.dom.flush();
+
+      const syncAllDataTypesControl = syncPage.$.syncAllDataTypesControl;
+      assertFalse(syncAllDataTypesControl.disabled);
+      assertTrue(syncAllDataTypesControl.checked);
+
+      // Uncheck the Sync All control.
+      syncAllDataTypesControl.click();
     }
 
     setup(function() {
@@ -455,6 +472,66 @@ cr.define('settings_people_page_sync_page', function() {
 
       assertTrue(syncPage.$.encryptionDescription.hidden);
       assertTrue(syncPage.$.encryptionRadioGroupContainer.hidden);
+    });
+
+    test('UserEvents_UnifiedConsent_Encrypted', function() {
+      const prefs = getSyncAllPrefs();
+      prefs.encryptAllData = true;
+      openDatatypeConfigurationWithUnifiedConsent(prefs);
+
+      assertTrue(prefs.userEventsSynced);
+      // History.
+      historyToggle = syncPage.$$('#historyToggle');
+      assertFalse(historyToggle.disabled);
+      assertTrue(historyToggle.checked);
+      // User events.
+      userEventsToggle = syncPage.$$('#userEventsToggle');
+      assertTrue(userEventsToggle.disabled);
+      assertFalse(userEventsToggle.checked);
+      resetSyncMessageBox = syncPage.$$('#reset-sync-message-box-user-events');
+      assertFalse(resetSyncMessageBox.hidden);
+    });
+
+    test('UserEvents_UnifiedConsent_NotEncrypted', function() {
+      const prefs = getSyncAllPrefs();
+      openDatatypeConfigurationWithUnifiedConsent(prefs);
+
+      assertTrue(prefs.userEventsSynced);
+      // Check history toggle.
+      historyToggle = syncPage.$$('#historyToggle');
+      assertFalse(historyToggle.disabled);
+      assertTrue(historyToggle.checked);
+      // Check user events toggle.
+      userEventsToggle = syncPage.$$('#userEventsToggle');
+      assertFalse(userEventsToggle.disabled);
+      assertTrue(userEventsToggle.checked);
+      resetSyncMessageBox = syncPage.$$('#reset-sync-message-box-user-events');
+      assertTrue(resetSyncMessageBox.hidden);
+
+      // Toggling history also toggles user events.
+      // Turn history off.
+      historyToggle.click();
+      cr.webUIListenerCallback('sync-prefs-changed', prefs);
+      assertFalse(historyToggle.checked);
+      assertTrue(userEventsToggle.disabled);
+      assertFalse(userEventsToggle.checked);
+      assertTrue(resetSyncMessageBox.hidden);
+      assertTrue(prefs.userEventsSynced);
+      // Turn history on.
+      historyToggle.click();
+      cr.webUIListenerCallback('sync-prefs-changed', prefs);
+      assertTrue(historyToggle.checked);
+      assertFalse(userEventsToggle.disabled);
+      assertTrue(userEventsToggle.checked);
+      assertTrue(prefs.userEventsSynced);
+
+      // Toggling user events also toggles the sync preference.
+      userEventsToggle.click();
+      cr.webUIListenerCallback('sync-prefs-changed', prefs);
+      assertFalse(userEventsToggle.disabled);
+      assertFalse(userEventsToggle.checked);
+      assertFalse(prefs.userEventsSynced);
+      assertTrue(resetSyncMessageBox.hidden);
     });
 
     test(
