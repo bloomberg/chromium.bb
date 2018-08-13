79c9b9fafb9eb6fce5d73b528d7e791590bbb4c4
David Black <dmblack@google.com>
2018-08-13 17:16:49 +0000

    Animate Assistant container growth/shrink.
    Motion spec still being delivered by UX but this should be a step in
    the right direction and removes a lot of our existing jank.
    
    Note: We still have yet to crossfade between mini/main/web states.
    
    Also revised some AssistantMiniView metrics per the spec.
    
    See bug for demo.
    
    Bug: b:112487675
    Change-Id: I9cecb3afdd7d5e85e04d8ef95a25e5dfdd263c91
    Reviewed-on: https://chromium-review.googlesource.com/1171952
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582624}
    Former-commit-id: 5743dbdde4240c5ba3edd0cf8ac46a634c570cc1

--- ash/assistant/ui/assistant_container_view.h
+++ ash/assistant/ui/assistant_container_view.h
@@ -7,7 +7,12 @@
 
 #include "ash/assistant/model/assistant_ui_model_observer.h"
 #include "base/macros.h"
+#include "ui/gfx/animation/animation_delegate.h"
 #include "ui/views/bubble/bubble_dialog_delegate.h"
+
+namespace gfx {
+class SlideAnimation;
+}  // namespace gfx
 
 namespace ash {
 
@@ -17,7 +22,8 @@ class AssistantMiniView;
 class AssistantWebView;
 
 class AssistantContainerView : public views::BubbleDialogDelegateView,
-                               public AssistantUiModelObserver {
+                               public AssistantUiModelObserver,
+                               public gfx::AnimationDelegate {
  public:
   explicit AssistantContainerView(AssistantController* assistant_controller);
   ~AssistantContainerView() override;
@@ -35,6 +41,9 @@ class AssistantContainerView : public views::BubbleDialogDelegateView,
   // AssistantUiModelObserver:
   void OnUiModeChanged(AssistantUiMode ui_mode) override;
 
+  // gfx::AnimationDelegate:
+  void AnimationProgressed(const gfx::Animation* animation) override;
+
  private:
   void SetAnchor();
 
@@ -43,6 +52,10 @@ class AssistantContainerView : public views::BubbleDialogDelegateView,
   AssistantMainView* assistant_main_view_;  // Owned by view hierarchy.
   AssistantMiniView* assistant_mini_view_;  // Owned by view hierarchy.
   AssistantWebView* assistant_web_view_;    // Owned by view hierarchy.
+
+  std::unique_ptr<gfx::SlideAnimation> resize_animation_;
+  gfx::SizeF resize_start_;
+  gfx::SizeF resize_end_;
 
   DISALLOW_COPY_AND_ASSIGN(AssistantContainerView);
 };
