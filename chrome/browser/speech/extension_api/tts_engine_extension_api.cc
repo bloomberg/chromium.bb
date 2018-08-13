f75ceaa3d01fbc79fa6f8c28fa5eacc49fb280c3
Katie D <katie@chromium.org>
2018-08-13 22:38:16 +0000

    Revert "Add logging for whether extension voices specify gender."
    This reverts commit 74d45b8114fdaf443286efb7a2ac1f0b92cd89d1 [formerly e5fb6b85b8cec339347bb9786c8165a8945e3f7f], without
    removing the histograms from histograms.xml.
    
    These histograms were initially added to get a sense of how frequently
    gender was used by TTS and TTS Engine extensions, and are obsolete.
    
    TBR=dtseng@chromium.org,rockot@chromium.org
    
    Bug: 855203,871506,871511
    Change-Id: Ic2625a7277d0bf277dd2f85e3460c417a64cea9c
    Reviewed-on: https://chromium-review.googlesource.com/1173313
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582721}
    Former-commit-id: 9b2fb2c362d2b95727f20efdbc626738f77f56d7

--- chrome/browser/speech/extension_api/tts_engine_extension_api.cc
+++ chrome/browser/speech/extension_api/tts_engine_extension_api.cc
@@ -9,7 +9,6 @@
 #include <utility>
 
 #include "base/json/json_writer.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
 #include "build/build_config.h"
@@ -297,8 +296,6 @@ ExtensionTtsEngineUpdateVoicesFunction::Run() {
     }
     if (voice_data->HasKey(constants::kGenderKey))
       voice_data->GetString(constants::kGenderKey, &voice.gender);
-    UMA_HISTOGRAM_BOOLEAN("TextToSpeechEngine.UpdateVoice.HasGender",
-                          !voice.gender.empty());
     if (voice_data->HasKey(constants::kRemoteKey))
       voice_data->GetBoolean(constants::kRemoteKey, &voice.remote);
     if (voice_data->HasKey(constants::kExtensionIdKey)) {
