213ca3cceffa5de41afd23796f7d50813b2afd64
Fergal Daly <fergal@chromium.org>
2018-08-13 12:16:44 +0000

    Control the HTML parser yield interval via field trial.
    The parser currently yields every 500ms unless it encounters a script tag. We will experiment
    with different values and observe the impact.
    
    https://goo.gl/zwQWyW
    
    Change-Id: I1dcec74fb41e438393e505323707c00d76b49d15
    Reviewed-on: https://chromium-review.googlesource.com/1148172
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582554}
    Former-commit-id: b727ed00d8038c1097a92711def5a75f1c6d9a21

--- third_party/blink/renderer/core/html/parser/html_parser_scheduler.h
+++ third_party/blink/renderer/core/html/parser/html_parser_scheduler.h
@@ -105,6 +105,7 @@ class HTMLParserScheduler final
 
   TaskHandle cancellable_continue_parse_task_handle_;
   bool is_paused_with_active_timer_;
+  const double parser_time_limit_;
 
   DISALLOW_COPY_AND_ASSIGN(HTMLParserScheduler);
 };
