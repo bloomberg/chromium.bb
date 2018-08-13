6f5c3e3d515c4818b670fc9f11052412562f187c
Rouslan Solomakhin <rouslan@chromium.org>
2018-08-13 18:19:22 +0000

    [Desktop Payment Handler] Popup disposition link.
    Before this patch, a popup disposition link in payment handler would
    always be blocked. This disposition is being used by legitimate payment
    handlers for opening USPS ZIP code lookup, for example.
    
    This patch allows opening popup disposition links in payment handler, as
    long as it was triggered by a user gesture.
    
    After this patch, both new foreground tab and popup disposition links
    can be opened with user gesture in payment handler.
    
    Bug: 863541
    Change-Id: Ie1bf41cbd2fab4cd390eca08ea96e7af361efbd8
    Reviewed-on: https://chromium-review.googlesource.com/1172792
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582642}
    Former-commit-id: 7237b3679c1a6c71c723d83cce21948e838c9772

--- chrome/browser/ui/views/payments/payment_handler_web_flow_view_controller.cc
+++ chrome/browser/ui/views/payments/payment_handler_web_flow_view_controller.cc
@@ -265,11 +265,12 @@ void PaymentHandlerWebFlowViewController::AddNewContents(
     const gfx::Rect& initial_rect,
     bool user_gesture,
     bool* was_blocked) {
-  // Open new foreground tab triggered by user activation in payment handler
-  // window in browser.
+  // Open new foreground tab or popup triggered by user activation in payment
+  // handler window in browser.
   Browser* browser = chrome::FindLastActiveWithProfile(profile_);
   if (browser && user_gesture &&
-      disposition == WindowOpenDisposition::NEW_FOREGROUND_TAB) {
+      (disposition == WindowOpenDisposition::NEW_FOREGROUND_TAB ||
+       disposition == WindowOpenDisposition::NEW_POPUP)) {
     chrome::AddWebContents(browser, source, std::move(new_contents),
                            disposition, initial_rect);
   }
