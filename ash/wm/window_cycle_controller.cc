49ddd4b3d01a05b0c21a8fe4e67fcce70641bf53
James Cook <jamescook@chromium.org>
2018-08-13 03:37:25 +0000

    Collapse ash::WindowCycleEventFilter with WindowCycleEventFilterClassic
    There's no separate "classic" mode in ash anymore. Collapse the
    superclass with its only subclass.
    
    Just cleanup, no functional changes.
    
    Bug: 873266
    Test: ash_unittests
    Change-Id: I70cdf56a8cb534cc5f5f1385ef5d702d3694b5a5
    Reviewed-on: https://chromium-review.googlesource.com/1171821
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582499}
    Former-commit-id: 0021ce28165f8adb1f6f1f915ac818fd75e1b7ad

--- ash/wm/window_cycle_controller.cc
+++ ash/wm/window_cycle_controller.cc
@@ -12,7 +12,7 @@
 #include "ash/shell.h"
 #include "ash/wm/mru_window_tracker.h"
 #include "ash/wm/screen_pinning_controller.h"
-#include "ash/wm/window_cycle_event_filter_classic.h"
+#include "ash/wm/window_cycle_event_filter.h"
 #include "ash/wm/window_cycle_list.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics.h"
@@ -61,7 +61,7 @@ void WindowCycleController::StartCycling() {
   active_window_before_window_cycle_ = GetActiveWindow(window_list);
 
   window_cycle_list_.reset(new WindowCycleList(window_list));
-  event_filter_ = std::make_unique<WindowCycleEventFilterClassic>();
+  event_filter_ = std::make_unique<WindowCycleEventFilter>();
   cycle_start_time_ = base::Time::Now();
   base::RecordAction(base::UserMetricsAction("WindowCycleController_Cycle"));
   UMA_HISTOGRAM_COUNTS_100("Ash.WindowCycleController.Items",
--- ash/wm/window_cycle_event_filter.h
+++ ash/wm/window_cycle_event_filter.h
@@ -6,16 +6,46 @@
 #define ASH_WM_WINDOW_CYCLE_EVENT_FILTER_H_
 
 #include "ash/ash_export.h"
+#include "base/macros.h"
+#include "base/timer/timer.h"
+#include "ui/events/event_handler.h"
 
 namespace ash {
 
 // Created by WindowCycleController when cycling through windows. Eats all key
 // events and stops cycling when the necessary key sequence is encountered.
-class ASH_EXPORT WindowCycleEventFilter {
+class ASH_EXPORT WindowCycleEventFilter : public ui::EventHandler {
  public:
-  virtual ~WindowCycleEventFilter() {}
+  WindowCycleEventFilter();
+  ~WindowCycleEventFilter() override;
+
+  // Overridden from ui::EventHandler:
+  void OnKeyEvent(ui::KeyEvent* event) override;
+  void OnMouseEvent(ui::MouseEvent* event) override;
+
+ private:
+  class AltReleaseHandler : public ui::EventHandler {
+   public:
+    AltReleaseHandler();
+    ~AltReleaseHandler() override;
+
+    void OnKeyEvent(ui::KeyEvent* event) override;
+
+   private:
+    DISALLOW_COPY_AND_ASSIGN(AltReleaseHandler);
+  };
+
+  // When the user holds Alt+Tab, this timer is used to send repeated
+  // cycle commands to WindowCycleController. Note this is not accomplished
+  // by marking the Alt+Tab accelerator as "repeatable" in the accelerator
+  // table because we wish to control the repeat interval.
+  base::RepeatingTimer repeat_timer_;
+
+  AltReleaseHandler alt_release_handler_;
+
+  DISALLOW_COPY_AND_ASSIGN(WindowCycleEventFilter);
 };
 
-}  // namepsace ash
+}  // namespace ash
 
 #endif  // ASH_WM_WINDOW_CYCLE_EVENT_FILTER_H_
