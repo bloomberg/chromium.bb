08e37b3b159e9d660cd0ed37ad745af9abe05241
Robert Liao <robliao@chromium.org>
2018-08-13 18:46:00 +0000

    Rename bubble_dialog_delegate.* to bubble_dialog_delegate_view.*
    This is part 1 of 3 for aligning BubbleDialogDelegateView with its
    filename.
    
    BUG=873422
    
    Change-Id: Ie57ac06eeda6afde915d3de6a1b60770a22e0da7
    Reviewed-on: https://chromium-review.googlesource.com/1171907
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582651}
    Former-commit-id: 6657440b025d75ff5fd5c810273b0986c263a07c

--- ui/views/bubble/bubble_dialog_delegate_view.cc
+++ ui/views/bubble/bubble_dialog_delegate_view.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ui/views/bubble/bubble_dialog_delegate.h"
+#include "ui/views/bubble/bubble_dialog_delegate_view.h"
 
 #include "base/metrics/histogram_macros.h"
 #include "build/build_config.h"
