2fd477503193dbd9da3b9b3dc4ec928c0767d812
Steven Holte <holte@google.com>
2018-08-14 01:24:43 +0000

    Update rappor xml tests and add to test target.
    Bug: 772404
    Change-Id: I7cb5b15f10aec3db6f28be16d7035bcc7b4d8525
    Reviewed-on: https://chromium-review.googlesource.com/1173360
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582783}
    Former-commit-id: 3198224d8205daf80a856d42418c936350ae0729

--- tools/metrics/rappor/pretty_print_test.py
+++ tools/metrics/rappor/pretty_print_test.py
@@ -3,120 +3,352 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from __future__ import unicode_literals
+
+import copy
+import json
 import unittest
 
-import pretty_print
+import model
 
-
-PRETTY_XML = """
-<!-- Comment1 -->
-
+PUBLIC_XML_SNIPPET = """
 <rappor-configuration>
-<!-- Comment2 -->
+
+<noise-levels>
+<noise-level name="NO_NOISE">
+  <summary>
+    NO_NOISE description.
+  </summary>
+  <noise-values fake-prob="0.0" fake-one-prob="0.0" one-coin-prob="1.0"
+      zero-coin-prob="0.0"/>
+</noise-level>
+
+<noise-level name="NORMAL_NOISE">
+  <summary>
+    NORMAL_NOISE description.
+  </summary>
+  <noise-values fake-prob="0.5" fake-one-prob="0.5" one-coin-prob="0.75"
+      zero-coin-prob="0.25"/>
+</noise-level>
+</noise-levels>
 
 <rappor-parameter-types>
-<!-- Comment3 -->
-
-<rappor-parameters name="TEST_RAPPOR_TYPE">
+<rappor-parameters name="ETLD_PLUS_ONE">
   <summary>
-    Fake type for tests.
+    ETLD+1 parameters.
   </summary>
-  <parameters num-cohorts="128" bytes="1" hash-functions="2" fake-prob="0.5"
-      fake-one-prob="0.5" one-coin-prob="0.75" zero-coin-prob="0.25"
-      reporting-level="COARSE"/>
+  <parameters num-cohorts="128"
+              bytes="16"
+              hash-functions="2"
+              reporting-level="FINE"
+              noise-level="NORMAL_NOISE"/>
+</rappor-parameters>
+<rappor-parameters name="SAFEBROWSING_RAPPOR_TYPE">
+  <summary>
+    SAFEBROWSING parameters.
+  </summary>
+  <parameters num-cohorts="128"
+              bytes="1"
+              hash-functions="2"
+              reporting-level="COARSE"
+              noise-level="NORMAL_NOISE"/>
+</rappor-parameters>
+</rappor-parameter-types>
+
+<rappor-metrics>
+
+<rappor-metric name="MultiD.TestMetric" type="SAFEBROWSING_RAPPOR_TYPE">
+  <owner>holte@chromium.org</owner>
+  <summary>
+    Metric Summary.
+  </summary>
+  <string-field name="domain">
+    <summary>
+      Domain Summary.
+    </summary>
+  </string-field>
+  <flags-field name="flags">
+    <flag>Bit 0</flag>
+    <flag label="MyBit" bit="1">What it means</flag>
+    <summary>
+      Flags Summary.
+    </summary>
+  </flags-field>
+</rappor-metric>
+
+<rappor-metric name="Search.DefaultSearchProvider" type="ETLD_PLUS_ONE">
+  <owner>
+    holte@chromium.org
+  </owner>
+  <summary>
+
+    BLAH BLAH  BLAH
+  </summary>
+</rappor-metric>
+</rappor-metrics>
+</rappor-configuration>
+"""
+
+PARSED_XML = {
+    model.models.COMMENT_KEY: [],
+    'noiseLevels': {
+        model.models.COMMENT_KEY: [],
+        'levels': [
+            {
+                model.models.COMMENT_KEY: [],
+                'name': 'NO_NOISE',
+                'summary': 'NO_NOISE description.',
+                'values': {
+                    model.models.COMMENT_KEY: [],
+                    'fake-prob': 0.0,
+                    'fake-one-prob': 0.0,
+                    'one-coin-prob': 1.0,
+                    'zero-coin-prob': 0.0,
+                },
+            },
+            {
+                model.models.COMMENT_KEY: [],
+                'name': 'NORMAL_NOISE',
+                'summary': 'NORMAL_NOISE description.',
+                'values': {
+                    model.models.COMMENT_KEY: [],
+                    'fake-prob': 0.5,
+                    'fake-one-prob': 0.5,
+                    'one-coin-prob': 0.75,
+                    'zero-coin-prob': 0.25,
+                },
+            },
+        ]
+    },
+    'parameterTypes': {
+        model.models.COMMENT_KEY: [],
+        'types': [
+            {
+                model.models.COMMENT_KEY: [],
+                'name': 'ETLD_PLUS_ONE',
+                'summary': 'ETLD+1 parameters.',
+                'parameters': {
+                    model.models.COMMENT_KEY: [],
+                    'num-cohorts': 128,
+                    'bytes': 16,
+                    'hash-functions': 2,
+                    'reporting-level': 'FINE',
+                    'noise-level': 'NORMAL_NOISE',
+                },
+            },
+            {
+                model.models.COMMENT_KEY: [],
+                'name': 'SAFEBROWSING_RAPPOR_TYPE',
+                'summary': 'SAFEBROWSING parameters.',
+                'parameters': {
+                    model.models.COMMENT_KEY: [],
+                    'num-cohorts': 128,
+                    'bytes': 1,
+                    'hash-functions': 2,
+                    'reporting-level': 'COARSE',
+                    'noise-level': 'NORMAL_NOISE',
+                },
+            },
+        ]
+    },
+    'metrics': {
+        model.models.COMMENT_KEY: [],
+        'metrics': [{
+            model.models.COMMENT_KEY: [],
+            'name':
+                'MultiD.TestMetric',
+            'flags': [{
+                model.models.COMMENT_KEY: [],
+                'name':
+                    'flags',
+                'summary':
+                    'Flags Summary.',
+                'flags': [
+                    {
+                        model.models.COMMENT_KEY: [],
+                        'summary': 'Bit 0'
+                    },
+                    {
+                        model.models.COMMENT_KEY: [],
+                        'summary': 'What it means',
+                        'label': 'MyBit',
+                        'bit': 1,
+                    },
+                ],
+            }],
+            'owners': ['holte@chromium.org'],
+            'strings': [{
+                model.models.COMMENT_KEY: [],
+                'name': 'domain',
+                'summary': 'Domain Summary.',
+            }],
+            'summary':
+                'Metric Summary.',
+            'type':
+                'SAFEBROWSING_RAPPOR_TYPE',
+            'uint64': [],
+        }, {
+            model.models.COMMENT_KEY: [],
+            'name': 'Search.DefaultSearchProvider',
+            'flags': [],
+            'owners': ['holte@chromium.org'],
+            'strings': [],
+            'summary': 'BLAH BLAH  BLAH',
+            'type': 'ETLD_PLUS_ONE',
+            'uint64': [],
+        }]
+    }
+}
+
+PRETTY_PRINTED_XML = """<rappor-configuration>
+<noise-levels>
+<noise-level name="NO_NOISE">
+  <summary>
+    NO_NOISE description.
+  </summary>
+  <noise-values fake-prob="0.0" fake-one-prob="0.0" one-coin-prob="1.0"
+      zero-coin-prob="0.0"/>
+</noise-level>
+
+<noise-level name="NORMAL_NOISE">
+  <summary>
+    NORMAL_NOISE description.
+  </summary>
+  <noise-values fake-prob="0.5" fake-one-prob="0.5" one-coin-prob="0.75"
+      zero-coin-prob="0.25"/>
+</noise-level>
+
+</noise-levels>
+
+<rappor-parameter-types>
+<rappor-parameters name="ETLD_PLUS_ONE">
+  <summary>
+    ETLD+1 parameters.
+  </summary>
+  <parameters num-cohorts="128" bytes="16" hash-functions="2"
+      reporting-level="FINE" noise-level="NORMAL_NOISE"/>
+</rappor-parameters>
+
+<rappor-parameters name="SAFEBROWSING_RAPPOR_TYPE">
+  <summary>
+    SAFEBROWSING parameters.
+  </summary>
+  <parameters num-cohorts="128" bytes="1" hash-functions="2"
+      reporting-level="COARSE" noise-level="NORMAL_NOISE"/>
 </rappor-parameters>
 
 </rappor-parameter-types>
 
 <rappor-metrics>
-<!-- Comment4 -->
-
-<rappor-metric name="Test.Rappor.Metric" type="TEST_RAPPOR_TYPE">
-  <owner>user1@chromium.org</owner>
-  <owner>user2@chromium.org</owner>
+<rappor-metric name="MultiD.TestMetric" type="SAFEBROWSING_RAPPOR_TYPE">
+  <owner>holte@chromium.org</owner>
   <summary>
-    A fake metric summary.
+    Metric Summary.
   </summary>
-</rappor-metric>
-
-<rappor-metric name="Test.Rappor.Metric2" type="TEST_RAPPOR_TYPE">
-  <owner>user1@chromium.org</owner>
-  <owner>user2@chromium.org</owner>
-  <summary>
-    A fake metric summary.
-  </summary>
-  <string-field name="Url">
+  <string-field name="domain">
     <summary>
-      The url of the event.
+      Domain Summary.
     </summary>
   </string-field>
-  <flags-field name="Flags">
-    <flag>Flag bit #1</flag>
-    <flag>Flag bit #2</flag>
+  <flags-field name="flags">
+    <flag bit="0" label="Bit 0"/>
+    <flag bit="1" label="MyBit">What it means</flag>
+    <summary>
+      Flags Summary.
+    </summary>
   </flags-field>
+</rappor-metric>
+
+<rappor-metric name="Search.DefaultSearchProvider" type="ETLD_PLUS_ONE">
+  <owner>holte@chromium.org</owner>
+  <summary>
+    BLAH BLAH  BLAH
+  </summary>
 </rappor-metric>
 
 </rappor-metrics>
 
 </rappor-configuration>
-""".strip()
-
-BASIC_METRIC = {
-  'comments': [],
-  'name': 'Test.Rappor.Metric',
-  'type': 'TEST_RAPPOR_TYPE',
-  'owners': ['user1@chromium.org', 'user2@chromium.org'],
-  'summary': 'A fake metric summary.',
-  'flags': [],
-  'strings': [],
-}
-
-MULTI_FIELD_METRIC = {
-  'comments': [],
-  'name': 'Test.Rappor.Metric2',
-  'type': 'TEST_RAPPOR_TYPE',
-  'owners': ['user1@chromium.org', 'user2@chromium.org'],
-  'summary': 'A fake metric summary.',
-  'strings': [{
-    'comments': [],
-    'name': 'Url',
-    'summary': 'The url of the event.',
-  }],
-  'flags': [{
-    'comments': [],
-    'name': 'Flags',
-    'flags': [
-      'Flag bit #1',
-      'Flag bit #2',
-    ]
-  }]
-}
+"""
 
 
-class ActionXmlTest(unittest.TestCase):
+class RapporModelTest(unittest.TestCase):
 
-  def testIsPretty(self):
-    result = pretty_print.UpdateXML(PRETTY_XML)
-    self.assertMultiLineEqual(PRETTY_XML, result.strip())
+  def testParse(self):
+    parsed = model.RAPPOR_XML_TYPE.Parse(PUBLIC_XML_SNIPPET)
+    self.assertEqual(PARSED_XML, parsed)
 
-  def testParsing(self):
-    comments, config = pretty_print.RAPPOR_XML_TYPE.Parse(PRETTY_XML)
-    self.assertEqual(BASIC_METRIC, config['metrics']['metrics'][0])
-    self.assertEqual(MULTI_FIELD_METRIC, config['metrics']['metrics'][1])
-    self.assertEqual(set(['TEST_RAPPOR_TYPE']),
-                     pretty_print.GetTypeNames(config))
+  def testUpdate(self):
+    updated = model.UpdateXML(PUBLIC_XML_SNIPPET)
+    # Compare list of lines for nicer diff on errors.
+    self.assertEqual(PRETTY_PRINTED_XML.split('\n'), updated.split('\n'))
+    reprinted = model.UpdateXML(updated)
+    self.assertEqual(PRETTY_PRINTED_XML.split('\n'), reprinted.split('\n'))
 
-  def testMissingOwners(self):
-    self.assertFalse(pretty_print.GetMissingOwnerErrors([BASIC_METRIC]))
-    no_owners = BASIC_METRIC.copy()
-    no_owners['owners'] = []
-    self.assertTrue(pretty_print.GetMissingOwnerErrors([no_owners]))
+  def testIsValidNoise(self):
+    valid_noise_level = {
+        'name': 'A',
+        'summary': 'B',
+        'values': {
+            'fake-prob': 0.5,
+            'fake-one-prob': 0.5,
+            'one-coin-prob': 0.75,
+            'zero-coin-prob': 0.25,
+        },
+    }
+    self.assertTrue(model._IsValidNoiseLevel(valid_noise_level))
+    invalid_noise_level = copy.copy(valid_noise_level)
+    del invalid_noise_level['name']
+    self.assertFalse(model._IsValidNoiseLevel(invalid_noise_level))
+    invalid_noise_level = copy.copy(valid_noise_level)
+    del invalid_noise_level['summary']
+    self.assertFalse(model._IsValidNoiseLevel(invalid_noise_level))
+    invalid_noise_level = copy.copy(valid_noise_level)
+    del invalid_noise_level['values']
+    self.assertFalse(model._IsValidNoiseLevel(invalid_noise_level))
+    invalid_noise_level = copy.deepcopy(valid_noise_level)
+    del invalid_noise_level['values']['fake-prob']
+    self.assertFalse(model._IsValidNoiseLevel(invalid_noise_level))
 
-  def testInvalidTypes(self):
-    self.assertFalse(pretty_print.GetInvalidTypeErrors(
-        set(['TEST_RAPPOR_TYPE']), [BASIC_METRIC]))
-    self.assertTrue(pretty_print.GetInvalidTypeErrors(
-        set(['OTHER_TYPE']), [BASIC_METRIC]))
+  def testIsValidRapporType(self):
+    noise_level_names = {'NORMAL_NOISE'}
+    valid_type = {
+        'name': 'ETLD_PLUS_ONE',
+        'summary': 'ETLD+1 parameters.',
+        'parameters': {
+            'num-cohorts': 128,
+            'bytes': 16,
+            'hash-functions': 2,
+            'reporting-level': 'FINE',
+            'noise-level': 'NORMAL_NOISE',
+        },
+    }
+    self.assertTrue(model._IsValidRapporType(valid_type, noise_level_names))
+
+  def testIsValidRapporMetric(self):
+    type_names = {'ETLD_PLUS_ONE'}
+    valid_metric = {
+        'name': 'Search.DefaultSearchProvider',
+        'flags': [],
+        'owners': ['holte@chromium.org'],
+        'strings': [],
+        'summary': 'BLAH BLAH  BLAH',
+        'type': 'ETLD_PLUS_ONE',
+        'uint64': [],
+    }
+    self.assertTrue(model._IsValidMetric(valid_metric, type_names))
+    invalid_metric = copy.copy(valid_metric)
+    del invalid_metric['name']
+    self.assertFalse(model._IsValidMetric(invalid_metric, type_names))
+    invalid_metric = copy.copy(valid_metric)
+    del invalid_metric['summary']
+    self.assertFalse(model._IsValidMetric(invalid_metric, type_names))
+    invalid_metric = copy.copy(valid_metric)
+    invalid_metric['type'] = 'FOO'
+    self.assertFalse(model._IsValidMetric(invalid_metric, type_names))
+    invalid_metric = copy.copy(valid_metric)
+    invalid_metric['owners'] = []
+    self.assertFalse(model._IsValidMetric(invalid_metric, type_names))
 
 
 if __name__ == '__main__':
