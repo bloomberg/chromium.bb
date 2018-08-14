e04de3712beb5bfeb4467c9468194da6e8b02122
Makoto Shimazu <shimazu@chromium.org>
2018-08-14 04:20:21 +0000

    Revert "Reenable ResourceLoadingHintsBrowserTest on Linux"
    This reverts commit db57bbf74e3991e0c34a4cfcdc7b5788e5f7af5f [formerly 762a6a7d62fadf2dacd5d125c984f8ee961ab707].
    
    Reason for revert: It looks still flaky.
    https://test-results.appspot.com/dashboards/flakiness_dashboard.html#testType=viz_browser_tests&tests=ResourceLoadingHintsBrowserTest.ResourceLoadingHintsHttp%0AResourceLoadingHintsBrowserTest
    
    Original change's description:
    > Reenable ResourceLoadingHintsBrowserTest on Linux
    > 
    > The plan is to keep a close eye on bots, and disable the tests back
    > if we see flakiness.
    > 
    > This CL will help us understand which of the tests are failing,
    > and also get a stack trace (which is now lost).
    > 
    > Bug: 866212
    > Change-Id: I94ff8ffb7278eff1ad3533d7872c1781e1e38b67
    > Reviewed-on: https://chromium-review.googlesource.com/1173443
    > Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    > Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#582730}
    
    TBR=tbansal@chromium.org,ryansturm@chromium.org
    
    Change-Id: Ic7974e852ddb12daadb956d38b54a94bc2f8096d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 866212
    Reviewed-on: https://chromium-review.googlesource.com/1173971
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582829}
    Former-commit-id: 8399dbaefcc6808ff83b8731fd9a954297a18fb9

--- chrome/browser/previews/resource_loading_hints/resource_loading_hints_browsertest.cc
+++ chrome/browser/previews/resource_loading_hints/resource_loading_hints_browsertest.cc
@@ -283,8 +283,8 @@ class ResourceLoadingHintsBrowserTest
 
 // Previews InfoBar (which these tests triggers) does not work on Mac.
 // See crbug.com/782322 for details. Also occasional flakes on win7/linux
-// (https://crbug.com/789542).
-#if !defined(OS_MACOSX) && !defined(OS_WIN)
+// (crbug.com/789542, crbug.com/866212).
+#if !defined(OS_MACOSX) && !defined(OS_WIN) && !defined(OS_LINUX)
 #define MAYBE_ResourceLoadingHintsHttpsWhitelisted \
   ResourceLoadingHintsHttpsWhitelisted
 #define MAYBE_ResourceLoadingHintsHttpsWhitelistedRedirectToHttps \
