a6df02a722c5aaad8127e267d4122c3b57de6d44
Oriol Brufau <obrufau@igalia.com>
2018-08-14 12:56:02 +0000

    [css-logical] Interpolate logical properties like physical ones in CSS transitions
    Spec: https://drafts.csswg.org/css-logical/#box
    
    BUG=873101
    
    TEST=external/wpt/css/css-logical/animation-004.html
    
    Change-Id: I104a8ecc746f4516e567b16a75c5d32a31d4dab8
    Reviewed-on: https://chromium-review.googlesource.com/1170769
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Oriol Brufau <obrufau@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#582895}
    Former-commit-id: decbfa5fb71791af107753bbea02d6e32371b109

--- third_party/blink/renderer/core/animation/css/css_animations.h
+++ third_party/blink/renderer/core/animation/css/css_animations.h
@@ -185,7 +185,8 @@ class CSSAnimations final {
   static void CalculateTransitionUpdateForStandardProperty(
       TransitionUpdateState&,
       const CSSTransitionData::TransitionProperty&,
-      size_t transition_index);
+      size_t transition_index,
+      const ComputedStyle&);
 
   static void CalculateTransitionUpdateForProperty(TransitionUpdateState&,
                                                    const PropertyHandle&,
