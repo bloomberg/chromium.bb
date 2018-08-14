b78c62339dcafc6fe385554467f563f939dfea6a
Uladzimir Miniailau <miniailau@google.com>
2018-08-14 09:58:30 +0000

    Fuzz parsing LookupAffiliationResponse
    This CL isolates code for parsing LookupAffiliationResponse into lookup_affiliation_response_parser.*, and adds a fuzzer for it.
    
    Bug: 834707
    Change-Id: If269a3603a0b84891491955ec0a99ad8506e0fc7
    Reviewed-on: https://chromium-review.googlesource.com/1131185
    Commit-Queue: Uladzimir Miniailau <miniailau@google.com>
    Reviewed-by: Jonathan Metzman <metzman@chromium.org>
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#582878}
    Former-commit-id: a9495a91185ea35f5acfb4cfd82b47e248531675

--- components/password_manager/core/browser/android_affiliation/affiliation_fetcher.cc
+++ components/password_manager/core/browser/android_affiliation/affiliation_fetcher.cc
@@ -15,6 +15,7 @@
 #include "base/metrics/histogram_macros.h"
 #include "components/password_manager/core/browser/android_affiliation/affiliation_api.pb.h"
 #include "components/password_manager/core/browser/android_affiliation/affiliation_utils.h"
+#include "components/password_manager/core/browser/android_affiliation/lookup_affiliation_response_parser.h"
 #include "components/password_manager/core/browser/android_affiliation/test_affiliation_fetcher_factory.h"
 #include "google_apis/google_api_keys.h"
 #include "net/base/load_flags.h"
@@ -168,55 +169,8 @@ bool AffiliationFetcher::ParseResponse(
   if (!response.ParseFromString(serialized_response))
     return false;
 
-  result->reserve(requested_facet_uris_.size());
-
-  std::map<FacetURI, size_t> facet_uri_to_class_index;
-  for (int i = 0; i < response.affiliation_size(); ++i) {
-    const affiliation_pb::Affiliation& equivalence_class(
-        response.affiliation(i));
-
-    AffiliatedFacets affiliated_facets;
-    for (int j = 0; j < equivalence_class.facet_size(); ++j) {
-      const affiliation_pb::Facet& facet(equivalence_class.facet(j));
-      const std::string& uri_spec(facet.id());
-      FacetURI uri = FacetURI::FromPotentiallyInvalidSpec(uri_spec);
-      // Ignore potential future kinds of facet URIs (e.g. for new platforms).
-      if (!uri.is_valid())
-        continue;
-      affiliated_facets.push_back(
-          {uri, FacetBrandingInfo{facet.branding_info().name(),
-                                  GURL(facet.branding_info().icon_url())}});
-    }
-
-    // Be lenient and ignore empty (after filtering) equivalence classes.
-    if (affiliated_facets.empty())
-      continue;
-
-    // Ignore equivalence classes that are duplicates of earlier ones. However,
-    // fail in the case of a partial overlap, which violates the invariant that
-    // affiliations must form an equivalence relation.
-    for (const Facet& facet : affiliated_facets) {
-      if (!facet_uri_to_class_index.count(facet.uri))
-        facet_uri_to_class_index[facet.uri] = result->size();
-      if (facet_uri_to_class_index[facet.uri] !=
-          facet_uri_to_class_index[affiliated_facets[0].uri]) {
-        return false;
-      }
-    }
-
-    // Filter out duplicate equivalence classes in the response.
-    if (facet_uri_to_class_index[affiliated_facets[0].uri] == result->size())
-      result->push_back(affiliated_facets);
-  }
-
-  // Synthesize an equivalence class (of size one) for each facet that did not
-  // appear in the server response due to not being affiliated with any others.
-  for (const FacetURI& uri : requested_facet_uris_) {
-    if (!facet_uri_to_class_index.count(uri))
-      result->push_back({{uri}});
-  }
-
-  return true;
+  return ParseLookupAffiliationResponse(requested_facet_uris_, response,
+                                        result);
 }
 
 void AffiliationFetcher::OnSimpleLoaderComplete(
