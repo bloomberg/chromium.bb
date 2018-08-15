ab0cd102be44ca41520ded5aa94887336de9e3da
Etienne Bergeron <etienneb@chromium.org>
2018-08-15 23:24:11 +0000

    Add safe browsing category to navigation scenarios
    Safe browsing is a low bandwidth category and it worth collecting
    the information since it may affect system wide performance.
    
    Under that caterory, the main important trace events are to detect
    whether or not the chrome_cleaner tool is scanning or cleaning. We
    suspect that the cleanup tool is having system wide impact and may
    explain janks observed in slow-reports.
    
    R=oysteine@chromium.org
    
    Change-Id: I604fdb89fd2bbbb0b13751c281cc0d936f426848
    Reviewed-on: https://chromium-review.googlesource.com/1176264
    Reviewed-by: oysteine <oysteine@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583440}
    Former-commit-id: 941614d3b680ad63cf3fb859bd8c6fb1a3df1627

--- content/browser/tracing/background_tracing_manager_impl.cc
+++ content/browser/tracing/background_tracing_manager_impl.cc
@@ -654,7 +654,7 @@ BackgroundTracingManagerImpl::GetCategoryFilterStringForCategoryPreset(
       return "blink.console,v8";
     case BackgroundTracingConfigImpl::CategoryPreset::BENCHMARK_NAVIGATION:
       return "benchmark,toplevel,ipc,base,browser,navigation,omnibox,"
-             "disabled-by-default-system_stats";
+             "safe_browsing,disabled-by-default-system_stats";
     case BackgroundTracingConfigImpl::CategoryPreset::BLINK_STYLE:
       return "blink_style";
     case BackgroundTracingConfigImpl::CategoryPreset::CATEGORY_PRESET_UNSET:
