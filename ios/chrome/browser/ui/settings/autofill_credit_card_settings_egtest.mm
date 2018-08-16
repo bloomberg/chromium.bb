4957084dea9d01356bd7f23d49e236cedb9029e1
Moe Ahmadi <mahmadi@chromium.org>
2018-08-16 17:47:36 +0000

    [AF][IOS] Split Autofill settings page into two separate pages
    Splits Autofill settings page into two separate pages, one for addresses
    and the other for credit cards.
    
    Bug: 860526
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I8f02f93fbd54fc17c2b97e5a97d9d5a08f729197
    Reviewed-on: https://chromium-review.googlesource.com/1173314
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583716}
    Former-commit-id: 0609fce66284c741b2a63d4a9b548555af2ad8d3

---
+++ ios/chrome/browser/ui/settings/autofill_credit_card_settings_egtest.mm
@@ -0,0 +1,224 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <XCTest/XCTest.h>
+
+#import "base/test/ios/wait_util.h"
+#include "components/autofill/core/browser/autofill_test_utils.h"
+#include "components/autofill/core/browser/credit_card.h"
+#include "components/autofill/core/browser/personal_data_manager.h"
+#include "components/strings/grit/components_strings.h"
+#include "ios/chrome/browser/autofill/personal_data_manager_factory.h"
+#include "ios/chrome/browser/ui/tools_menu/public/tools_menu_constants.h"
+#include "ios/chrome/grit/ios_strings.h"
+#import "ios/chrome/test/app/chrome_test_util.h"
+#import "ios/chrome/test/app/web_view_interaction_test_util.h"
+#include "ios/chrome/test/earl_grey/accessibility_util.h"
+#import "ios/chrome/test/earl_grey/chrome_actions.h"
+#import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
+#import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
+#import "ios/chrome/test/earl_grey/chrome_matchers.h"
+#import "ios/chrome/test/earl_grey/chrome_test_case.h"
+#include "ui/base/l10n/l10n_util.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+using chrome_test_util::ButtonWithAccessibilityLabel;
+using chrome_test_util::ButtonWithAccessibilityLabelId;
+using chrome_test_util::NavigationBarDoneButton;
+using chrome_test_util::SettingsDoneButton;
+using chrome_test_util::SettingsMenuBackButton;
+
+namespace {
+
+// Expectation of how the saved Autofill credit card looks like, a map from cell
+// name IDs to expected contents.
+struct DisplayStringIDToExpectedResult {
+  int display_string_id;
+  NSString* expected_result;
+};
+
+const DisplayStringIDToExpectedResult kExpectedFields[] = {
+    {IDS_IOS_AUTOFILL_CARDHOLDER, @"Test User"},
+    {IDS_IOS_AUTOFILL_CARD_NUMBER, @"4111111111111111"},
+    {IDS_IOS_AUTOFILL_EXP_MONTH, @"11"},
+    {IDS_IOS_AUTOFILL_EXP_YEAR, @"2022"}};
+
+NSString* const kCreditCardLabel =
+    @"Test User, Visa  ‪• • • • 1111‬";
+
+}  // namespace
+
+// Various tests for the Autofill credit cards section of the settings.
+@interface AutofillCreditCardSettingsTestCase : ChromeTestCase
+@end
+
+@implementation AutofillCreditCardSettingsTestCase {
+  // The PersonalDataManager instance for the current browser state.
+  autofill::PersonalDataManager* _personalDataManager;
+}
+
+- (void)setUp {
+  [super setUp];
+
+  _personalDataManager =
+      autofill::PersonalDataManagerFactory::GetForBrowserState(
+          chrome_test_util::GetOriginalBrowserState());
+  _personalDataManager->SetSyncingForTest(true);
+}
+
+- (void)tearDown {
+  // Clear existing credit cards.
+  for (const auto* creditCard : _personalDataManager->GetCreditCards()) {
+    _personalDataManager->RemoveByGUID(creditCard->guid());
+  }
+
+  [super tearDown];
+}
+
+- (autofill::CreditCard)addCreditCard {
+  autofill::CreditCard creditCard = autofill::test::GetCreditCard();
+  size_t creditCardCount = _personalDataManager->GetCreditCards().size();
+  _personalDataManager->AddCreditCard(creditCard);
+  GREYAssert(base::test::ios::WaitUntilConditionOrTimeout(
+                 base::test::ios::kWaitForActionTimeout,
+                 ^bool() {
+                   return creditCardCount <
+                          _personalDataManager->GetCreditCards().size();
+                 }),
+             @"Failed to add credit card.");
+  return creditCard;
+}
+
+// Helper to open the settings page for Autofill credit cards.
+- (void)openCreditCardsSettings {
+  [ChromeEarlGreyUI openSettingsMenu];
+  [[EarlGrey selectElementWithMatcher:ButtonWithAccessibilityLabel(
+                                          l10n_util::GetNSString(
+                                              IDS_AUTOFILL_PAYMENT_METHODS))]
+      performAction:grey_tap()];
+}
+
+// Helper to open the settings page for the Autofill credit card with |label|.
+- (void)openEditCreditCard:(NSString*)label {
+  [self openCreditCardsSettings];
+
+  [[EarlGrey selectElementWithMatcher:grey_accessibilityLabel(label)]
+      performAction:grey_tap()];
+}
+
+// Close the settings.
+- (void)exitSettingsMenu {
+  [[EarlGrey selectElementWithMatcher:SettingsMenuBackButton()]
+      performAction:grey_tap()];
+  [[EarlGrey selectElementWithMatcher:SettingsDoneButton()]
+      performAction:grey_tap()];
+  // Wait for UI components to finish loading.
+  [[GREYUIThreadExecutor sharedInstance] drainUntilIdle];
+}
+
+// Test that the page for viewing Autofill credit card details is as expected.
+- (void)testCreditCardViewPage {
+  autofill::CreditCard creditCard = [self addCreditCard];
+  [self openEditCreditCard:kCreditCardLabel];
+
+  // Check that all fields and values match the expectations.
+  for (const DisplayStringIDToExpectedResult& expectation : kExpectedFields) {
+    [[EarlGrey selectElementWithMatcher:
+                   grey_accessibilityLabel([NSString
+                       stringWithFormat:@"%@, %@",
+                                        l10n_util::GetNSString(
+                                            expectation.display_string_id),
+                                        expectation.expected_result])]
+        assertWithMatcher:grey_notNil()];
+  }
+
+  // Go back to the list view page.
+  [[EarlGrey selectElementWithMatcher:SettingsMenuBackButton()]
+      performAction:grey_tap()];
+
+  [self exitSettingsMenu];
+}
+
+// Test that the page for viewing Autofill credit card details is accessible.
+- (void)testAccessibilityOnCreditCardViewPage {
+  autofill::CreditCard creditCard = [self addCreditCard];
+  [self openEditCreditCard:kCreditCardLabel];
+
+  chrome_test_util::VerifyAccessibilityForCurrentScreen();
+
+  // Go back to the list view page.
+  [[EarlGrey selectElementWithMatcher:SettingsMenuBackButton()]
+      performAction:grey_tap()];
+
+  [self exitSettingsMenu];
+}
+
+// Test that the page for editing Autofill credit card details is accessible.
+- (void)testAccessibilityOnCreditCardEditPage {
+  autofill::CreditCard creditCard = [self addCreditCard];
+  [self openEditCreditCard:kCreditCardLabel];
+
+  // Switch on edit mode.
+  [[EarlGrey selectElementWithMatcher:ButtonWithAccessibilityLabelId(
+                                          IDS_IOS_NAVIGATION_BAR_EDIT_BUTTON)]
+      performAction:grey_tap()];
+  chrome_test_util::VerifyAccessibilityForCurrentScreen();
+
+  // Go back to the list view page.
+  [[EarlGrey selectElementWithMatcher:SettingsMenuBackButton()]
+      performAction:grey_tap()];
+
+  [self exitSettingsMenu];
+}
+
+// Checks that the Autofill credit cards list view is in edit mode and the
+// Autofill credit cards switch is disabled.
+- (void)testListViewEditMode {
+  autofill::CreditCard creditCard = [self addCreditCard];
+  [self openCreditCardsSettings];
+
+  // Switch on edit mode.
+  [[EarlGrey selectElementWithMatcher:ButtonWithAccessibilityLabelId(
+                                          IDS_IOS_NAVIGATION_BAR_EDIT_BUTTON)]
+      performAction:grey_tap()];
+
+  // Check the Autofill credit card switch is disabled.
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::SettingsSwitchCell(
+                                          @"cardItem_switch", YES, NO)]
+      assertWithMatcher:grey_notNil()];
+
+  [self exitSettingsMenu];
+}
+
+// Checks that the Autofill credit card switch can be toggled on/off and the
+// list of Autofill credit cards is not affected by it.
+- (void)testToggleCreditCardSwitch {
+  autofill::CreditCard creditCard = [self addCreditCard];
+  [self openCreditCardsSettings];
+
+  // Toggle the Autofill credit cards switch off.
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::SettingsSwitchCell(
+                                          @"cardItem_switch", YES, YES)]
+      performAction:chrome_test_util::TurnSettingsSwitchOn(NO)];
+
+  // Expect Autofill credit cards to remain visible.
+  [[EarlGrey selectElementWithMatcher:grey_accessibilityLabel(kCreditCardLabel)]
+      assertWithMatcher:grey_notNil()];
+
+  // Toggle the Autofill credit cards switch back on.
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::SettingsSwitchCell(
+                                          @"cardItem_switch", NO, YES)]
+      performAction:chrome_test_util::TurnSettingsSwitchOn(YES)];
+
+  // Expect Autofill credit cards to remain visible.
+  [[EarlGrey selectElementWithMatcher:grey_accessibilityLabel(kCreditCardLabel)]
+      assertWithMatcher:grey_notNil()];
+
+  [self exitSettingsMenu];
+}
+
+@end
--- ios/chrome/browser/ui/settings/settings_collection_view_controller.mm
+++ ios/chrome/browser/ui/settings/settings_collection_view_controller.mm
@@ -44,7 +44,8 @@
 #import "ios/chrome/browser/ui/commands/settings_main_page_commands.h"
 #import "ios/chrome/browser/ui/settings/about_chrome_collection_view_controller.h"
 #import "ios/chrome/browser/ui/settings/accounts_collection_view_controller.h"
-#import "ios/chrome/browser/ui/settings/autofill_collection_view_controller.h"
+#import "ios/chrome/browser/ui/settings/autofill_credit_card_collection_view_controller.h"
+#import "ios/chrome/browser/ui/settings/autofill_profile_collection_view_controller.h"
 #import "ios/chrome/browser/ui/settings/bandwidth_management_collection_view_controller.h"
 #import "ios/chrome/browser/ui/settings/cells/account_signin_item.h"
 #import "ios/chrome/browser/ui/settings/cells/settings_detail_item.h"
@@ -96,7 +97,9 @@ const CGFloat kAccountProfilePhotoDimension = 40.0f;
 NSString* const kSyncAndGoogleServicesImageName = @"sync_and_google_services";
 NSString* const kSettingsSearchEngineImageName = @"settings_search_engine";
 NSString* const kSettingsPasswordsImageName = @"settings_passwords";
-NSString* const kSettingsAutofillFormsImageName = @"settings_autofill_forms";
+NSString* const kSettingsAutofillCreditCardImageName =
+    @"settings_payment_methods";
+NSString* const kSettingsAutofillProfileImageName = @"settings_addresses";
 NSString* const kSettingsVoiceSearchImageName = @"settings_voice_search";
 NSString* const kSettingsPrivacyImageName = @"settings_privacy";
 NSString* const kSettingsContentSettingsImageName =
@@ -124,7 +127,8 @@ typedef NS_ENUM(NSInteger, ItemType) {
   ItemTypeHeader,
   ItemTypeSearchEngine,
   ItemTypeSavedPasswords,
-  ItemTypeAutofill,
+  ItemTypeAutofillCreditCard,
+  ItemTypeAutofillProfile,
   ItemTypeVoiceSearch,
   ItemTypePrivacy,
   ItemTypeContentSettings,
@@ -240,7 +244,8 @@ void SigninObserverBridge::GoogleSignedOut(const std::string& account_id,
   SettingsDetailItem* _voiceSearchDetailItem;
   SettingsDetailItem* _defaultSearchEngineItem;
   SettingsDetailItem* _savePasswordsDetailItem;
-  SettingsDetailItem* _autoFillDetailItem;
+  SettingsDetailItem* _autoFillProfileDetailItem;
+  SettingsDetailItem* _autoFillCreditCardDetailItem;
 
   // YES if the user used at least once the sign-in promo view buttons.
   BOOL _signinStarted;
@@ -424,7 +429,9 @@ void SigninObserverBridge::GoogleSignedOut(const std::string& account_id,
       toSectionWithIdentifier:SectionIdentifierBasics];
   [model addItem:[self savePasswordsDetailItem]
       toSectionWithIdentifier:SectionIdentifierBasics];
-  [model addItem:[self autoFillDetailItem]
+  [model addItem:[self AutoFillCreditCardDetailItem]
+      toSectionWithIdentifier:SectionIdentifierBasics];
+  [model addItem:[self autoFillProfileDetailItem]
       toSectionWithIdentifier:SectionIdentifierBasics];
 
   // Advanced Section
@@ -562,19 +569,34 @@ void SigninObserverBridge::GoogleSignedOut(const std::string& account_id,
   return _savePasswordsDetailItem;
 }
 
-- (CollectionViewItem*)autoFillDetailItem {
-  BOOL autofillEnabled =
-      autofill::prefs::IsAutofillEnabled(_browserState->GetPrefs());
-  NSString* autofillDetail = autofillEnabled
-                                 ? l10n_util::GetNSString(IDS_IOS_SETTING_ON)
-                                 : l10n_util::GetNSString(IDS_IOS_SETTING_OFF);
-  _autoFillDetailItem =
-      [self detailItemWithType:ItemTypeAutofill
-                          text:l10n_util::GetNSString(IDS_IOS_AUTOFILL)
-                    detailText:autofillDetail
-                 iconImageName:kSettingsAutofillFormsImageName];
+- (CollectionViewItem*)AutoFillCreditCardDetailItem {
+  BOOL autofillCreditCardEnabled =
+      autofill::prefs::IsCreditCardAutofillEnabled(_browserState->GetPrefs());
+  NSString* detailText = autofillCreditCardEnabled
+                             ? l10n_util::GetNSString(IDS_IOS_SETTING_ON)
+                             : l10n_util::GetNSString(IDS_IOS_SETTING_OFF);
+  _autoFillCreditCardDetailItem = [self
+      detailItemWithType:ItemTypeAutofillCreditCard
+                    text:l10n_util::GetNSString(IDS_AUTOFILL_PAYMENT_METHODS)
+              detailText:detailText
+           iconImageName:kSettingsAutofillCreditCardImageName];
 
-  return _autoFillDetailItem;
+  return _autoFillCreditCardDetailItem;
+}
+
+- (CollectionViewItem*)autoFillProfileDetailItem {
+  BOOL autofillProfileEnabled =
+      autofill::prefs::IsProfileAutofillEnabled(_browserState->GetPrefs());
+  NSString* detailText = autofillProfileEnabled
+                             ? l10n_util::GetNSString(IDS_IOS_SETTING_ON)
+                             : l10n_util::GetNSString(IDS_IOS_SETTING_OFF);
+  _autoFillProfileDetailItem =
+      [self detailItemWithType:ItemTypeAutofillProfile
+                          text:l10n_util::GetNSString(IDS_AUTOFILL_ADDRESSES)
+                    detailText:detailText
+                 iconImageName:kSettingsAutofillProfileImageName];
+
+  return _autoFillProfileDetailItem;
 }
 
 - (CollectionViewItem*)voiceSearchDetailItem {
@@ -856,8 +878,12 @@ void SigninObserverBridge::GoogleSignedOut(const std::string& account_id,
       controller = [[SavePasswordsCollectionViewController alloc]
           initWithBrowserState:_browserState];
       break;
-    case ItemTypeAutofill:
-      controller = [[AutofillCollectionViewController alloc]
+    case ItemTypeAutofillCreditCard:
+      controller = [[AutofillCreditCardCollectionViewController alloc]
+          initWithBrowserState:_browserState];
+      break;
+    case ItemTypeAutofillProfile:
+      controller = [[AutofillProfileCollectionViewController alloc]
           initWithBrowserState:_browserState];
       break;
     case ItemTypeVoiceSearch:
@@ -1260,15 +1286,24 @@ void SigninObserverBridge::GoogleSignedOut(const std::string& account_id,
     [self reconfigureCellsForItems:@[ _savePasswordsDetailItem ]];
   }
 
-  if (preferenceName == autofill::prefs::kAutofillCreditCardEnabled ||
-      preferenceName == autofill::prefs::kAutofillProfileEnabled) {
-    BOOL autofillEnabled =
-        autofill::prefs::IsAutofillEnabled(_browserState->GetPrefs());
-    NSString* autofillDetail =
-        autofillEnabled ? l10n_util::GetNSString(IDS_IOS_SETTING_ON)
-                        : l10n_util::GetNSString(IDS_IOS_SETTING_OFF);
-    _autoFillDetailItem.detailText = autofillDetail;
-    [self reconfigureCellsForItems:@[ _autoFillDetailItem ]];
+  if (preferenceName == autofill::prefs::kAutofillProfileEnabled) {
+    BOOL autofillProfileEnabled =
+        autofill::prefs::IsProfileAutofillEnabled(_browserState->GetPrefs());
+    NSString* detailText = autofillProfileEnabled
+                               ? l10n_util::GetNSString(IDS_IOS_SETTING_ON)
+                               : l10n_util::GetNSString(IDS_IOS_SETTING_OFF);
+    _autoFillProfileDetailItem.detailText = detailText;
+    [self reconfigureCellsForItems:@[ _autoFillProfileDetailItem ]];
+  }
+
+  if (preferenceName == autofill::prefs::kAutofillCreditCardEnabled) {
+    BOOL autofillCreditCardEnabled =
+        autofill::prefs::IsCreditCardAutofillEnabled(_browserState->GetPrefs());
+    NSString* detailText = autofillCreditCardEnabled
+                               ? l10n_util::GetNSString(IDS_IOS_SETTING_ON)
+                               : l10n_util::GetNSString(IDS_IOS_SETTING_OFF);
+    _autoFillCreditCardDetailItem.detailText = detailText;
+    [self reconfigureCellsForItems:@[ _autoFillCreditCardDetailItem ]];
   }
 }
 
