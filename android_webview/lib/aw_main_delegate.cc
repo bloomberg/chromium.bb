5ec4c25d62efaba8f9658bc04e8946ea56703fbd
kylechar <kylechar@chromium.org>
2018-08-16 21:47:27 +0000

    webview: Disable VizDisplayCompositor feature.
    The VizDisplayCompositor feature enables OOP-d which changes the display
    compositor pipeline. WebView doesn't support OOP-D and any shared code
    that checks the features status risks breaking if it's enabled.
    
    Bug: 732555
    Change-Id: Ibc84d53a0ca4691ba7fadd10ed362244fb2b0e7a
    Reviewed-on: https://chromium-review.googlesource.com/1177630
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583828}
    Former-commit-id: c466fd2389ddd82bb72310e291d4c87aea3cce9a

--- android_webview/lib/aw_main_delegate.cc
+++ android_webview/lib/aw_main_delegate.cc
@@ -36,6 +36,7 @@
 #include "components/safe_browsing/android/safe_browsing_api_handler_bridge.h"
 #include "components/services/heap_profiling/public/cpp/allocator_shim.h"
 #include "components/spellcheck/spellcheck_buildflags.h"
+#include "components/viz/common/features.h"
 #include "content/public/browser/android/browser_media_player_manager_register.h"
 #include "content/public/browser/browser_main_runner.h"
 #include "content/public/browser/browser_thread.h"
@@ -160,6 +161,10 @@ bool AwMainDelegate::BasicStartupComplete(int* exit_code) {
 #endif  // ENABLE_SPELLCHECK
 
   CommandLineHelper::AddDisabledFeature(*cl, features::kWebPayments.name);
+
+  // WebView isn't compatible with OOP-D.
+  CommandLineHelper::AddDisabledFeature(*cl,
+                                        features::kVizDisplayCompositor.name);
 
   // WebView does not support AndroidOverlay yet for video overlays.
   CommandLineHelper::AddDisabledFeature(*cl, media::kUseAndroidOverlay.name);
