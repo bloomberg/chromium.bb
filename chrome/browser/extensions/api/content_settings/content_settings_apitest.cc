bfba90a077eb6b5de808e92695f6719ef7b86a66
Raymes Khoury <raymes@chromium.org>
2018-08-16 06:40:15 +0000

    Measure the number of embedded content settings set by extensions
    This adds a metric which measures the number of times an extension sets
    a content setting that has an embedded exception. This means that the
    primary and secondary pattern are different.
    
    We plan to deprecate this type of usage for permissions. If usage of
    this feature is low for other types of settings we may also consider
    deprecating those.
    
    Bug: 802945,874322
    Change-Id: I5076603eb462cda4495538022c4c3a93c8894519
    Reviewed-on: https://chromium-review.googlesource.com/1174082
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Raymes Khoury <raymes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583566}
    Former-commit-id: 06d6aef7859cffc2f2331ca055acd41d4be1ad81

--- chrome/browser/extensions/api/content_settings/content_settings_apitest.cc
+++ chrome/browser/extensions/api/content_settings/content_settings_apitest.cc
@@ -8,6 +8,7 @@
 #include "base/location.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
 #include "chrome/browser/chrome_notification_types.h"
@@ -20,6 +21,7 @@
 #include "chrome/common/chrome_switches.h"
 #include "components/content_settings/core/browser/cookie_settings.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
+#include "components/content_settings/core/common/content_settings.h"
 #include "components/keep_alive_registry/keep_alive_types.h"
 #include "components/keep_alive_registry/scoped_keep_alive.h"
 #include "components/prefs/pref_service.h"
@@ -357,6 +359,35 @@ IN_PROC_BROWSER_TEST_F(ExtensionContentSettingsApiTest,
   EXPECT_FALSE(RunExtensionSubtest("content_settings/incognitoisolation",
                                    "test.html?allow"))
       << message_;
+}
+
+IN_PROC_BROWSER_TEST_F(ExtensionContentSettingsApiTest,
+                       EmbeddedSettingsMetric) {
+  base::HistogramTester histogram_tester;
+  const char kExtensionPath[] = "content_settings/embeddedsettingsmetric";
+  EXPECT_TRUE(RunExtensionSubtest(kExtensionPath, "test.html")) << message_;
+
+  size_t num_values = 0;
+  int javascript_type = ContentSettingTypeToHistogramValue(
+      CONTENT_SETTINGS_TYPE_IMAGES, &num_values);
+  int geolocation_type = ContentSettingTypeToHistogramValue(
+      CONTENT_SETTINGS_TYPE_GEOLOCATION, &num_values);
+  int cookies_type = ContentSettingTypeToHistogramValue(
+      CONTENT_SETTINGS_TYPE_COOKIES, &num_values);
+
+  histogram_tester.ExpectBucketCount(
+      "ContentSettings.ExtensionEmbeddedSettingSet", javascript_type, 1);
+  histogram_tester.ExpectBucketCount(
+      "ContentSettings.ExtensionEmbeddedSettingSet", geolocation_type, 1);
+  histogram_tester.ExpectTotalCount(
+      "ContentSettings.ExtensionEmbeddedSettingSet", 2);
+
+  histogram_tester.ExpectBucketCount(
+      "ContentSettings.ExtensionNonEmbeddedSettingSet", javascript_type, 1);
+  histogram_tester.ExpectBucketCount(
+      "ContentSettings.ExtensionNonEmbeddedSettingSet", cookies_type, 1);
+  histogram_tester.ExpectTotalCount(
+      "ContentSettings.ExtensionNonEmbeddedSettingSet", 2);
 }
 
 }  // namespace extensions
