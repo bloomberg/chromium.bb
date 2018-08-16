491bea446a443140354e194c19820b0836c737fe
Maksim Sisov <msisov@igalia.com>
2018-08-16 12:40:47 +0000

    [ozone/wayland] Ensure all the mojo calls happen on gpu main thread.
    This cl fixes threading model: the proxy object is created on
    a gpu main thread, whereas mojo ptr for a WaylandConnection is
    set on a IO child thread. ScheduleSwap is also called on GpuMainThread,
    but other methods can be called from IO threads.
    
    To make a consitency, always use GpuMainThread's task runner.
    This is also needed for a frame callback, which will be passed during
    ScheduleSwap calls in a follow up cl.
    
    Bug: 859012, 820047
    Change-Id: I6833f3722cb4531da375387a7d443c1835f3c870
    Reviewed-on: https://chromium-review.googlesource.com/1170827
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583618}
    Former-commit-id: 0b8963fa7962904f890818d8205ca11981afe2bf

--- ui/ozone/platform/wayland/gpu/wayland_connection_proxy.cc
+++ ui/ozone/platform/wayland/gpu/wayland_connection_proxy.cc
@@ -13,17 +13,16 @@ namespace ui {
 
 WaylandConnectionProxy::WaylandConnectionProxy(WaylandConnection* connection)
     : connection_(connection),
-      ui_runner_(base::ThreadTaskRunnerHandle::Get()) {}
+      gpu_thread_runner_(connection_ ? nullptr
+                                     : base::ThreadTaskRunnerHandle::Get()) {}
 
 WaylandConnectionProxy::~WaylandConnectionProxy() = default;
 
 void WaylandConnectionProxy::SetWaylandConnection(
     ozone::mojom::WaylandConnectionPtr wc_ptr) {
-  // Store current thread's task runner to be able to make mojo calls on the
-  // right sequence.
-  ui_runner_ = base::ThreadTaskRunnerHandle::Get();
-  wc_ptr.Bind(wc_ptr.PassInterface());
-  wc_ptr_ = std::move(wc_ptr);
+  // This is an IO child thread. To satisfy our needs, we pass interface here
+  // and bind it again on a gpu main thread, where buffer swaps happen.
+  wc_ptr_info_ = wc_ptr.PassInterface();
 }
 
 void WaylandConnectionProxy::CreateZwpLinuxDmabuf(
@@ -35,7 +34,36 @@ void WaylandConnectionProxy::CreateZwpLinuxDmabuf(
     uint32_t current_format,
     uint32_t planes_count,
     uint32_t buffer_id) {
-  DCHECK(ui_runner_->BelongsToCurrentThread());
+  DCHECK(gpu_thread_runner_);
+  // Do a mojo call on the GpuMainThread instead of the io child thread to
+  // ensure proper functionality.
+  gpu_thread_runner_->PostTask(
+      FROM_HERE,
+      base::BindOnce(&WaylandConnectionProxy::CreateZwpLinuxDmabufInternal,
+                     base::Unretained(this), std::move(file), std::move(size),
+                     std::move(strides), std::move(offsets),
+                     std::move(modifiers), current_format, planes_count,
+                     buffer_id));
+}
+
+void WaylandConnectionProxy::CreateZwpLinuxDmabufInternal(
+    base::File file,
+    gfx::Size size,
+    const std::vector<uint32_t>& strides,
+    const std::vector<uint32_t>& offsets,
+    const std::vector<uint64_t>& modifiers,
+    uint32_t current_format,
+    uint32_t planes_count,
+    uint32_t buffer_id) {
+  // The interface pointer is passed on an IO child thread, which is different
+  // from the thread, which is used to call these methods. Thus, rebind the
+  // interface on a first call to ensure mojo calls will always happen on a
+  // sequence we want.
+  if (!bound_) {
+    wc_ptr_.Bind(std::move(wc_ptr_info_));
+    bound_ = true;
+  }
+  DCHECK(gpu_thread_runner_->BelongsToCurrentThread());
   DCHECK(wc_ptr_);
   wc_ptr_->CreateZwpLinuxDmabuf(std::move(file), size.width(), size.height(),
                                 strides, offsets, current_format, modifiers,
@@ -43,34 +71,24 @@ void WaylandConnectionProxy::CreateZwpLinuxDmabuf(
 }
 
 void WaylandConnectionProxy::DestroyZwpLinuxDmabuf(uint32_t buffer_id) {
-  // Mojo calls must be done on the right sequence.
-  DCHECK(ui_runner_);
-  ui_runner_->PostTask(
+  DCHECK(gpu_thread_runner_);
+  // Do a mojo call on the GpuMainThread instead of the io child thread to
+  // ensure proper functionality.
+  gpu_thread_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(&WaylandConnectionProxy::DestroyZwpLinuxDmabufInternal,
                      base::Unretained(this), buffer_id));
 }
 
 void WaylandConnectionProxy::DestroyZwpLinuxDmabufInternal(uint32_t buffer_id) {
-  DCHECK(ui_runner_->BelongsToCurrentThread());
+  DCHECK(gpu_thread_runner_->BelongsToCurrentThread());
   DCHECK(wc_ptr_);
   wc_ptr_->DestroyZwpLinuxDmabuf(buffer_id);
 }
 
 void WaylandConnectionProxy::ScheduleBufferSwap(gfx::AcceleratedWidget widget,
                                                 uint32_t buffer_id) {
-  // Mojo calls must be done on the right sequence.
-  DCHECK(ui_runner_);
-  ui_runner_->PostTask(
-      FROM_HERE,
-      base::BindOnce(&WaylandConnectionProxy::ScheduleBufferSwapInternal,
-                     base::Unretained(this), widget, buffer_id));
-}
-
-void WaylandConnectionProxy::ScheduleBufferSwapInternal(
-    gfx::AcceleratedWidget widget,
-    uint32_t buffer_id) {
-  DCHECK(ui_runner_->BelongsToCurrentThread());
+  DCHECK(gpu_thread_runner_->BelongsToCurrentThread());
   DCHECK(wc_ptr_);
   wc_ptr_->ScheduleBufferSwap(widget, buffer_id);
 }
