1c5ef1020ba886fdabd8bc3084e54ed1f0d14df1
kylechar <kylechar@chromium.org>
2018-08-16 21:46:23 +0000

    Use VizCompositorThreadRunner in VizMainImpl.
    Add missing code VizCompositorThreadRunner to support GPU compositing
    and GPU process force shutdown. Remove code in VizMainImpl and just use
    VizCompositorThreadRunner there.
    
    Bug: 849639
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: I34ae6adec13d8be11e563f2c32de36504b6711b4
    Reviewed-on: https://chromium-review.googlesource.com/1165842
    Commit-Queue: kylechar <kylechar@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583827}
    Former-commit-id: 6626eab7fc7ea41a3f3076fdd3436c09b184bbf5

--- components/viz/service/main/viz_compositor_thread_runner.h
+++ components/viz/service/main/viz_compositor_thread_runner.h
@@ -9,37 +9,79 @@
 
 #include "base/macros.h"
 #include "base/memory/scoped_refptr.h"
+#include "build/build_config.h"
 #include "components/viz/service/viz_service_export.h"
 #include "services/viz/privileged/interfaces/viz_main.mojom.h"
+
+#if defined(OS_ANDROID)
+#include "base/android/java_handler_thread.h"
+#endif
 
 namespace base {
 class SingleThreadTaskRunner;
 class Thread;
 }  // namespace base
 
+namespace gpu {
+class CommandBufferTaskExecutor;
+class GpuChannelManager;
+class ImageFactory;
+}  // namespace gpu
+
 namespace viz {
 class DisplayProvider;
 class FrameSinkManagerImpl;
+class GpuServiceImpl;
 class ServerSharedBitmapManager;
+
+#if defined(OS_ANDROID)
+using VizCompositorThreadType = base::android::JavaHandlerThread;
+#else
+using VizCompositorThreadType = base::Thread;
+#endif
 
 // Starts and runs the VizCompositorThread. The thread will be started when this
 // object is constructed. Objects on the thread will be initialized after
 // calling CreateFrameSinkManager(). Destructor will teardown objects on thread
 // and then stop the thread.
-// TODO(kylechar): Convert VizMainImpl to use VizCompositorThreadRunner.
 class VIZ_SERVICE_EXPORT VizCompositorThreadRunner {
  public:
   VizCompositorThreadRunner();
   // Performs teardown on thread and then stops thread.
   ~VizCompositorThreadRunner();
 
-  // Create FrameSinkManager from |params|. This can be called from the thread
-  // that owns |this| to initialize state on VizCompositorThreadRunner.
+  // Returns the TaskRunner for VizCompositorThread.
+  base::SingleThreadTaskRunner* task_runner() { return task_runner_.get(); }
+
+  // Creates FrameSinkManager from |params|. The version with |gpu_service| and
+  // |task_executor| supports both GPU and software compositing, while the
+  // version without supports only software compositing. Should be called from
+  // the thread that owns |this| to initialize state on VizCompositorThread.
   void CreateFrameSinkManager(mojom::FrameSinkManagerParamsPtr params);
+  void CreateFrameSinkManager(
+      mojom::FrameSinkManagerParamsPtr params,
+      scoped_refptr<gpu::CommandBufferTaskExecutor> task_executor,
+      GpuServiceImpl* gpu_service);
+
+  // Performs cleanup on VizCompositorThread needed before forcing thread to
+  // shut down. Ensures VizCompositorThread teardown during the destructor
+  // doesn't block on PostTasks back to the GPU thread. After cleanup has
+  // finished |cleanup_finished_callback| will be run. Should be called from the
+  // thread that owns |this|.
+  //
+  // This is intended to be used when the GPU thread wants to force restart. The
+  // cleanup is normally handled by the browser process before GPU process
+  // shutdown, except if the GPU thread is forcing restart.
+  void CleanupForShutdown(base::OnceClosure cleanup_finished_callback);
 
  private:
   void CreateFrameSinkManagerOnCompositorThread(
-      mojom::FrameSinkManagerParamsPtr params);
+      mojom::FrameSinkManagerParamsPtr params,
+      scoped_refptr<gpu::CommandBufferTaskExecutor> task_executor,
+      GpuServiceImpl* gpu_service,
+      gpu::ImageFactory* image_factory,
+      gpu::GpuChannelManager* gpu_channel_manager);
+  void CleanupForShutdownOnCompositorThread();
   void TearDownOnCompositorThread();
 
   // Start variables to be accessed only on |task_runner_|.
@@ -48,7 +90,7 @@ class VIZ_SERVICE_EXPORT VizCompositorThreadRunner {
   std::unique_ptr<FrameSinkManagerImpl> frame_sink_manager_;
   // End variables to be accessed only on |task_runner_|.
 
-  std::unique_ptr<base::Thread> thread_;
+  std::unique_ptr<VizCompositorThreadType> thread_;
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
 
   DISALLOW_COPY_AND_ASSIGN(VizCompositorThreadRunner);
