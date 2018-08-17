c12fdaed7a4f4c1fc1c4a79c1ae1cd8d2e67ee7d
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2018-08-17 03:01:18 +0000

    Stop rolling python/plugin_pb2.py to dom_distiller/dist
    The file python/plugin_pb2.py generated by newer version of
    protobuf cannot be consumed by older versions on some bots.
    
    Bug: 874509, 764774
    Change-Id: I3045ba021263b204fbf6f0ab6af9a5da2bd96121
    Reviewed-on: https://chromium-review.googlesource.com/1176823
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583949}
    Former-commit-id: 6811ae6cd8a2b123cac4ab92a06c40b591c5d837

--- third_party/dom_distiller_js/update_domdistiller_js.sh
+++ third_party/dom_distiller_js/update_domdistiller_js.sh
@@ -60,6 +60,11 @@
   rm -rf $tmpdir/dom-distiller-dist/*
   pushd dom-distiller-dist
   cp -r $tmpdir/dom-distiller/out/package/* .
+
+  # Stop rolling python/plugin_pb2.py for protobuf backward compatibility.
+  # See https://crbug.com/874509
+  git checkout -- python/plugin_pb2.py
+
   git add .
   if [[ $(git status --short | wc -l) -ne 0 ]]; then
     git commit -a -m "Package for ${new_gitsha}"
