3ce24f6302a8417edd1d026ef551ee9828e19145
Antonio Gomes <tonikitoo@igalia.com>
2018-08-17 14:11:09 +0000

    Move sync_client.cc away from base::Bind
    BUG=714018
    
    Change-Id: I59944400286435c71ed605ba019cd7f2680e5532
    Reviewed-on: https://chromium-review.googlesource.com/1174656
    Reviewed-by: vitaliii <vitaliii@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#584052}
    Former-commit-id: ee1fcd4bcbcbf2c7c89f760ef12b670aca5d8b9c

--- components/sync/engine/net/network_time_update_callback.h
+++ components/sync/engine/net/network_time_update_callback.h
@@ -19,7 +19,7 @@ namespace syncer {
 // const base::Time& network_time - the new network time.
 // const base::TimeDelta& resolution - how precise the reading is.
 // const base::TimeDelta& latency - the http request's latency.
-using NetworkTimeUpdateCallback = base::Callback<
+using NetworkTimeUpdateCallback = base::RepeatingCallback<
     void(const base::Time&, const base::TimeDelta&, const base::TimeDelta&)>;
 
 }  // namespace syncer
