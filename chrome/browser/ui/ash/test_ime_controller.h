bfbdbd361f6fb60a163b968d51a98029f7c3c230
Shu Chen <shuchen@google.com>
2018-08-17 01:51:59 +0000

    Moves TestImeController from ash/ime/ to c/b/ui/ash/.
    So that the input_method_manager_impl_unittest won't depend on ash/.
    
    Bug: 756059
    Change-Id: Ibba9f87a05d322489a1778658c5bc4adffd908e4
    Reviewed-on: https://chromium-review.googlesource.com/1177217
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Shu Chen <shuchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583936}
    Former-commit-id: 3c379ddb0084016c3188cd385c8bd316253b2fdd

--- chrome/browser/ui/ash/test_ime_controller.h
+++ chrome/browser/ui/ash/test_ime_controller.h
@@ -2,32 +2,29 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_IME_TEST_IME_CONTROLLER_H_
-#define ASH_IME_TEST_IME_CONTROLLER_H_
+#ifndef CHROME_BROWSER_UI_ASH_TEST_IME_CONTROLLER_H_
+#define CHROME_BROWSER_UI_ASH_TEST_IME_CONTROLLER_H_
 
 #include <memory>
 #include <string>
 #include <utility>
 
-#include "ash/ash_export.h"
 #include "ash/public/interfaces/ime_controller.mojom.h"
 #include "mojo/public/cpp/bindings/binding.h"
 
-namespace ash {
-
-class ASH_EXPORT TestImeController : mojom::ImeController {
+class TestImeController : ash::mojom::ImeController {
  public:
   TestImeController();
   ~TestImeController() override;
 
   // Returns a mojo interface pointer bound to this object.
-  mojom::ImeControllerPtr CreateInterfacePtr();
+  ash::mojom::ImeControllerPtr CreateInterfacePtr();
 
-  // mojom::ImeController:
-  void SetClient(mojom::ImeControllerClientPtr client) override;
+  // ash::mojom::ImeController:
+  void SetClient(ash::mojom::ImeControllerClientPtr client) override;
   void RefreshIme(const std::string& current_ime_id,
-                  std::vector<mojom::ImeInfoPtr> available_imes,
-                  std::vector<mojom::ImeMenuItemPtr> menu_items) override;
+                  std::vector<ash::mojom::ImeInfoPtr> available_imes,
+                  std::vector<ash::mojom::ImeMenuItemPtr> menu_items) override;
   void SetImesManagedByPolicy(bool managed) override;
   void ShowImeMenuOnShelf(bool show) override;
   void UpdateCapsLockState(bool enabled) override;
@@ -41,8 +38,8 @@ class ASH_EXPORT TestImeController : mojom::ImeController {
 
   // The most recent values received via mojo.
   std::string current_ime_id_;
-  std::vector<mojom::ImeInfoPtr> available_imes_;
-  std::vector<mojom::ImeMenuItemPtr> menu_items_;
+  std::vector<ash::mojom::ImeInfoPtr> available_imes_;
+  std::vector<ash::mojom::ImeMenuItemPtr> menu_items_;
   bool managed_by_policy_ = false;
   bool show_ime_menu_on_shelf_ = false;
   bool show_mode_indicator_ = false;
@@ -54,11 +51,9 @@ class ASH_EXPORT TestImeController : mojom::ImeController {
   bool is_voice_enabled_ = false;
 
  private:
-  mojo::Binding<mojom::ImeController> binding_;
+  mojo::Binding<ash::mojom::ImeController> binding_;
 
   DISALLOW_COPY_AND_ASSIGN(TestImeController);
 };
 
-}  // namespace ash
-
-#endif  // ASH_IME_TEST_IME_CONTROLLER_H_
+#endif  // CHROME_BROWSER_UI_ASH_TEST_IME_CONTROLLER_H_
