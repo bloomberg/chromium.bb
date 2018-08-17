d13ac5f9ec0f84657b7e3ca5d37eb2363b6de9c0
rbpotter <rbpotter@chromium.org>
2018-08-17 04:10:05 +0000

    WebUI Polymer2: Filter out material history item test
    The test is failing (flaky?) on Mac 10.12.
    
    Bug: 874976
    Change-Id: I913c6d39d7650e8298cd83eb2236f0f0d027f5d1
    Reviewed-on: https://chromium-review.googlesource.com/1178639
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#583956}
    Former-commit-id: 10c0c8c01c8eea683478dabc3e3308edc3f698c4

--- chrome/test/data/webui/md_history/md_history_browsertest.js
+++ chrome/test/data/webui/md_history/md_history_browsertest.js
@@ -82,12 +82,9 @@ MaterialHistoryItemTest.prototype = {
   ]),
 };
 
-// https://crbug.com/874976, fails on Mac.
-GEN('#if !defined(OS_MACOSX)');
 TEST_F('MaterialHistoryItemTest', 'All', function() {
   mocha.run();
 });
-GEN('#endif');
 
 function MaterialHistoryListTest() {}
 
