a3699f9041c0d0b6600e46d700ca6fdecabf8f40
Antonio Gomes <tonikitoo@igalia.com>
2018-08-17 14:11:09 +0000

    Move sync_client.cc away from base::Bind
    BUG=714018
    
    Change-Id: I59944400286435c71ed605ba019cd7f2680e5532
    Reviewed-on: https://chromium-review.googlesource.com/1174656
    Reviewed-by: vitaliii <vitaliii@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#584052}
    Former-commit-id: ee1fcd4bcbcbf2c7c89f760ef12b670aca5d8b9c

--- components/sync/tools/sync_client.cc
+++ components/sync/tools/sync_client.cc
@@ -395,7 +395,7 @@ int SyncClientMain(int argc, char* argv[]) {
   // HttpPostProviderFactory is removed.
   CancelationSignal factory_cancelation_signal;
   std::unique_ptr<HttpPostProviderFactory> post_factory(new HttpBridgeFactory(
-      context_getter.get(), base::Bind(&StubNetworkTimeUpdateCallback),
+      context_getter.get(), base::BindRepeating(&StubNetworkTimeUpdateCallback),
       &factory_cancelation_signal));
   post_factory->Init(kUserAgent, BindToTrackerCallback());
   // Used only when committing bookmarks, so it's okay to leave this as null.
@@ -427,7 +427,7 @@ int SyncClientMain(int argc, char* argv[]) {
   args.encryptor = &null_encryptor;
   args.unrecoverable_error_handler = WeakHandle<UnrecoverableErrorHandler>();
   args.report_unrecoverable_error_function =
-      base::Bind(LogUnrecoverableErrorContext);
+      base::BindRepeating(LogUnrecoverableErrorContext);
   args.cancelation_signal = &scm_cancelation_signal;
   sync_manager->Init(&args);
   // TODO(akalin): Avoid passing in model parameters multiple times by
