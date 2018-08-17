67c813f9800e140c8fee12ee30eb18099514d132
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-08-17 19:41:04 +0000

    Revert "Disable flaky RestoreHistogramTest testWritingHistogramAtStartup."
    This reverts commit f9b862c5877af1787aa58c2ed3428dfb2a07a0b2 [formerly 434f3432590888b898ce85fdb99eabfbb2d59f72].
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 584133 as the
    culprit for failures in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzQzNGYzNDMyNTkwODg4Yjg5OGNlODVmZGI5OWVhYmZiYjJkNTlmNzIM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium/android-rel/738
    
    Sample Failed Step: compile
    
    Original change's description:
    > Disable flaky RestoreHistogramTest testWritingHistogramAtStartup.
    > 
    > TBR=tedchoc@chromium.org
    > NOTRY=true (disabling a test)
    > 
    > Bug: 875372
    > Change-Id: Ia51964a62207de7300b159e80a3e4816af8f579b
    > Reviewed-on: https://chromium-review.googlesource.com/1180165
    > Reviewed-by: apacible <apacible@chromium.org>
    > Commit-Queue: apacible <apacible@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#584133}
    
    Change-Id: Ib147d1b5da422966072e141e1612b571aaf29587
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 875372
    Reviewed-on: https://chromium-review.googlesource.com/1180263
    Cr-Commit-Position: refs/heads/master@{#584151}
    Former-commit-id: ba042db109e0fc457f3a421bd6f0bb62b4ddee07

--- chrome/android/javatests/src/org/chromium/chrome/browser/RestoreHistogramTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/RestoreHistogramTest.java
@@ -97,7 +97,6 @@ public class RestoreHistogramTest {
      * @throws ProcessInitException
      */
     @Test
-    @DisabledTest(message = "Test is flaky on. crbug.com/875372")
     @SmallTest
     public void testWritingHistogramAtStartup() throws InterruptedException, ProcessInitException {
         LibraryLoader.getInstance().ensureInitialized(LibraryProcessType.PROCESS_BROWSER);
