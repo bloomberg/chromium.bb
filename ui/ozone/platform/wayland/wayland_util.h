16f43071cc802d1039180151b57649c4716bf293
Julie Jeongeun Kim <jkim@igalia.com>
2018-08-19 02:46:10 +0000

    [ozone/wayland] Add wayland_util.cc/h to create utiliy APIs
    It introduces wayland_util.cc/h and moves the code to create shared
    memory buffer and draw bitmap on it from WaylandCursor, as it could
    be used generally rather than specific to cursor area.
    On upcoming patch for drag and drop on Wayland, it's also required
    to pass the drag icon surface to Wayland.
    
    It doesn't bring behavioral changes.
    
    Bug: 578890, 875164
    Change-Id: I0cd4603c2abacaa899a9fcf19d97186d6f493146
    Reviewed-on: https://chromium-review.googlesource.com/1179487
    Commit-Queue: Julie Jeongeun Kim <jkim@igalia.com>
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#584328}
    Former-commit-id: 2f1d779496e6d7caefd1780f4946aced1e22401c

---
+++ ui/ozone/platform/wayland/wayland_util.h
@@ -0,0 +1,34 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_OZONE_PLATFORM_WAYLAND_WAYLAND_UTIL_H_
+#define UI_OZONE_PLATFORM_WAYLAND_WAYLAND_UTIL_H_
+
+#include <wayland-client.h>
+
+#include "base/macros.h"
+#include "ui/ozone/platform/wayland/wayland_object.h"
+
+class SkBitmap;
+
+namespace base {
+class SharedMemory;
+}
+
+namespace gfx {
+class Size;
+}
+
+namespace wl {
+
+wl_buffer* CreateSHMBuffer(const gfx::Size& size,
+                           base::SharedMemory* shared_memory,
+                           wl_shm* shm);
+void DrawBitmapToSHMB(const gfx::Size& size,
+                      const base::SharedMemory& shared_memory,
+                      const SkBitmap& bitmap);
+
+}  // namespace wl
+
+#endif  // UI_OZONE_PLATFORM_WAYLAND_WAYLAND_UTIL_H_
