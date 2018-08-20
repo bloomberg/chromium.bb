c0d17faafb4668403a5cf79a2f773fb75514bc2d
Dominick Ng <dominickn@chromium.org>
2018-08-20 16:15:56 +0000

    Move callback dancing to external_web_apps.cc
    It becomes more complicated, but web_app_provider.cc becomes simpler.
    
    Change-Id: I976ac72d40c1e153ba8cbb15f42931885a453ee1
    Reviewed-on: https://chromium-review.googlesource.com/1170145
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584465}
    Former-commit-id: 4b176fda138de1f32d6de5f61d2d34ee9b8a4356

--- chrome/browser/web_applications/web_app_provider.cc
+++ chrome/browser/web_applications/web_app_provider.cc
@@ -7,16 +7,11 @@
 #include <utility>
 
 #include "base/bind.h"
-#include "base/path_service.h"
-#include "base/task/post_task.h"
-#include "build/build_config.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/web_applications/bookmark_apps/external_web_apps.h"
 #include "chrome/browser/web_applications/bookmark_apps/policy/web_app_policy_manager.h"
 #include "chrome/browser/web_applications/extensions/pending_bookmark_app_manager.h"
 #include "chrome/browser/web_applications/web_app_provider_factory.h"
-#include "chrome/common/chrome_paths.h"
-#include "content/public/browser/browser_thread.h"
 
 namespace web_app {
 
@@ -31,34 +26,9 @@ WebAppProvider::WebAppProvider(Profile* profile)
       web_app_policy_manager_(
           std::make_unique<WebAppPolicyManager>(profile->GetPrefs(),
                                                 pending_app_manager_.get())) {
-#if defined(OS_CHROMEOS)
-  // As of mid 2018, only Chrome OS has default web apps or external web apps.
-  // In the future, we might open external web apps to other flavors.
-
-  // Do a three-part callback dance, across different TaskRunners.
-  //
-  // 1. Start here, on the UI thread. We don't do any work straight away, in
-  // the constructor, because browser startup is already a busy time, and this
-  // scan is low priority. Also, some tests, for better or worse, assume that
-  // there are no pending tasks on the UI TaskRunner queue after all the
-  // KeyedService instances are spun up. Instead, we schedule
-  // ScanForExternalWebApps to happen as an after-startup task, explicitly on
-  // the UI thread.
-  //
-  // 2. In ScanForExternalWebApps, schedule web_app::ScanDirForExternalWebApps
-  // to happen on a background thread, so that we don't block the UI thread.
-  // When that's done, base::PostTaskWithTraitsAndReplyWithResult will bounce
-  // us back to the originating thread (the UI thread).
-  //
-  // 3. In ScanForExternalWebAppsCallback, forward the vector of AppInfo's on
-  // to the pending_app_manager_, which can only be called on the UI thread.
-  content::BrowserThread::PostAfterStartupTask(
-      FROM_HERE,
-      content::BrowserThread::GetTaskRunnerForThread(
-          content::BrowserThread::UI),
-      base::BindOnce(&WebAppProvider::ScanForExternalWebApps,
+  web_app::ScanForExternalWebApps(
+      base::BindOnce(&WebAppProvider::OnScanForExternalWebApps,
                      weak_ptr_factory_.GetWeakPtr()));
-#endif  // defined(OS_CHROMEOS)
 }
 
 WebAppProvider::~WebAppProvider() = default;
@@ -69,43 +39,9 @@ void WebAppProvider::RegisterProfilePrefs(
   WebAppPolicyManager::RegisterProfilePrefs(registry);
 }
 
-void WebAppProvider::ScanForExternalWebApps() {
-#if !defined(OS_CHROMEOS)
-// chrome::DIR_STANDALONE_EXTERNAL_EXTENSIONS is only defined for OS_LINUX.
-// OS_CHROMEOS is a variant of OS_LINUX.
-#else
-  // For manual testing, it can be useful to s/STANDALONE/USER/, as writing to
-  // "$HOME/.config/chromium/test-user/.config/chromium/External Extensions"
-  // does not require root ACLs, unlike "/usr/share/chromium/extensions".
-  //
-  // TODO(nigeltao): do we want to append a sub-directory name, analogous to
-  // the "arc" in "/usr/share/chromium/extensions/arc" as per
-  // chrome/browser/ui/app_list/arc/arc_default_app_list.cc? Or should we not
-  // sort "system apps" into directories based on their platform (e.g. ARC,
-  // PWA, etc.), and instead examine the JSON contents (e.g. an "activity"
-  // key means ARC, "web_app_start_url" key means PWA, etc.)?
-  base::FilePath dir;
-  if (!base::PathService::Get(chrome::DIR_STANDALONE_EXTERNAL_EXTENSIONS,
-                              &dir)) {
-    LOG(ERROR) << "ScanForExternalWebApps: base::PathService::Get failed";
-    return;
-  }
-
-  auto task = base::BindOnce(&web_app::ScanDirForExternalWebApps, dir);
-  auto reply = base::BindOnce(&WebAppProvider::ScanForExternalWebAppsCallback,
-                              weak_ptr_factory_.GetWeakPtr());
-
-  base::PostTaskWithTraitsAndReplyWithResult(
-      FROM_HERE, {base::MayBlock(), base::TaskPriority::BEST_EFFORT},
-      std::move(task), std::move(reply));
-#endif  // !defined(OS_CHROMEOS)
-}
-
-void WebAppProvider::ScanForExternalWebAppsCallback(
+void WebAppProvider::OnScanForExternalWebApps(
     std::vector<web_app::PendingAppManager::AppInfo> app_infos) {
-#if defined(OS_CHROMEOS)
   pending_app_manager_->InstallApps(std::move(app_infos), base::DoNothing());
-#endif
 }
 
 }  // namespace web_app
