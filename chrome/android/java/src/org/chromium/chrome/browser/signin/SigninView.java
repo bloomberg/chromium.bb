24672ec4344bc7af9000e6e8349c74b6fbe252cb
Boris Sazonov <bsazonov@chromium.org>
2018-08-20 10:48:49 +0000

    [Unity][Android] Extract AnimationLooper
    This CL moves the code from SigninView startAnimations/stopAnimations
    methods into separate AnimationLooper class that encapsulates animation
    looping logic. This class is then used in ConsentBumpMoreOptionsFragment
    to run the header animation.
    
    Bug: 869426
    Change-Id: Ie305e4535adf27662d7d6724afc419b7bcfd8084
    Reviewed-on: https://chromium-review.googlesource.com/1172688
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584402}
    Former-commit-id: 884b3a0d3204db69836f9dff42b4ce2797b02700

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninView.java
@@ -4,15 +4,10 @@
 
 package org.chromium.chrome.browser.signin;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.ColorStateList;
-import android.graphics.drawable.Animatable;
-import android.graphics.drawable.Animatable2;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.support.annotation.Nullable;
-import android.support.graphics.drawable.Animatable2Compat;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.content.res.AppCompatResources;
 import android.util.AttributeSet;
@@ -22,8 +17,8 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import org.chromium.base.ThreadUtils;
 import org.chromium.chrome.R;
+import org.chromium.ui.drawable.AnimationLooper;
 import org.chromium.ui.widget.ButtonCompat;
 
 /** View that wraps signin screen and caches references to UI elements. */
@@ -44,8 +39,7 @@ public class SigninView extends LinearLayout {
     private Button mRefuseButton;
     private Button mMoreButton;
     private View mAcceptButtonEndPadding;
-
-    private Runnable mStopAnimationsRunnable;
+    private AnimationLooper mAnimationLooper;
 
     public SigninView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -73,14 +67,12 @@ public class SigninView extends LinearLayout {
         mRefuseButton = (Button) findViewById(R.id.negative_button);
         mMoreButton = (Button) findViewById(R.id.more_button);
         mAcceptButtonEndPadding = findViewById(R.id.positive_button_end_padding);
+
+        mAnimationLooper = AnimationLooper.create(mHeaderImage.getDrawable());
     }
 
     SigninScrollView getScrollView() {
         return mScrollView;
-    }
-
-    ImageView getHeaderImage() {
-        return mHeaderImage;
     }
 
     TextView getTitleView() {
@@ -140,59 +132,11 @@ public class SigninView extends LinearLayout {
     }
 
     void startAnimations() {
-        Drawable headerImage = getHeaderImage().getDrawable();
-        if (headerImage instanceof Animatable2Compat) {
-            Animatable2Compat animatable = (Animatable2Compat) headerImage;
-            Animatable2Compat.AnimationCallback animationCallback =
-                    new Animatable2Compat.AnimationCallback() {
-                        @Override
-                        public void onAnimationEnd(Drawable drawable) {
-                            restartAnimations(animatable);
-                        }
-                    };
-            mStopAnimationsRunnable = () -> {
-                animatable.unregisterAnimationCallback(animationCallback);
-                animatable.stop();
-            };
-            animatable.registerAnimationCallback(animationCallback);
-            animatable.start();
-            return;
-        }
-
-        // Animatable2 was added in API level 23 (Android M).
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return;
-        if (headerImage instanceof Animatable2) {
-            Animatable2 animatable = (Animatable2) headerImage;
-            Animatable2.AnimationCallback animationCallback = new Animatable2.AnimationCallback() {
-                @Override
-                @TargetApi(Build.VERSION_CODES.M)
-                public void onAnimationEnd(Drawable drawable) {
-                    restartAnimations(animatable);
-                }
-            };
-            mStopAnimationsRunnable = () -> {
-                animatable.unregisterAnimationCallback(animationCallback);
-                animatable.stop();
-            };
-            animatable.registerAnimationCallback(animationCallback);
-            animatable.start();
-        }
+        mAnimationLooper.start();
     }
 
     void stopAnimations() {
-        if (mStopAnimationsRunnable == null) return;
-        mStopAnimationsRunnable.run();
-        mStopAnimationsRunnable = null;
-    }
-
-    private void restartAnimations(Animatable animatable) {
-        // In some cases (Animatable2Compat from Support Library, etc.), onAnimationEnd is invoked
-        // before the animation is marked as ended, so calls to start() here may be ignored.
-        // This issue is worked around by deferring the call to start().
-        ThreadUtils.postOnUiThread(() -> {
-            if (mStopAnimationsRunnable == null) return;
-            animatable.start();
-        });
+        mAnimationLooper.stop();
     }
 
     static Drawable getExpandArrowDrawable(Context context) {
