8f898a0661ca7990a28a705fb4eed1b31066f8a5
Boris Sazonov <bsazonov@chromium.org>
2018-08-20 10:48:49 +0000

    [Unity][Android] Extract AnimationLooper
    This CL moves the code from SigninView startAnimations/stopAnimations
    methods into separate AnimationLooper class that encapsulates animation
    looping logic. This class is then used in ConsentBumpMoreOptionsFragment
    to run the header animation.
    
    Bug: 869426
    Change-Id: Ie305e4535adf27662d7d6724afc419b7bcfd8084
    Reviewed-on: https://chromium-review.googlesource.com/1172688
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584402}
    Former-commit-id: 884b3a0d3204db69836f9dff42b4ce2797b02700

--- chrome/android/java/src/org/chromium/chrome/browser/signin/ConsentBumpMoreOptionsFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/ConsentBumpMoreOptionsFragment.java
@@ -11,10 +11,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.widget.RadioButtonWithDescription;
+import org.chromium.ui.drawable.AnimationLooper;
 
 /**
  * This fragment implements the advanced consent bump screen. This screen lets users to enable or
@@ -23,12 +25,17 @@ import org.chromium.chrome.browser.widget.RadioButtonWithDescription;
 public class ConsentBumpMoreOptionsFragment extends Fragment {
     private static final String TAG = "ConsentBumpMoreOptionsFragment";
 
+    private AnimationLooper mAnimationLooper;
+
     public ConsentBumpMoreOptionsFragment() {}
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.consent_bump_more_options_view, container, false);
+
+        ImageView headerImage = view.findViewById(R.id.consent_bump_header_image);
+        mAnimationLooper = AnimationLooper.create(headerImage.getDrawable());
 
         RadioButtonWithDescription noChanges = view.findViewById(R.id.consent_bump_no_changes);
         noChanges.setDescriptionText(getText(R.string.consent_bump_no_changes_description));
@@ -42,5 +49,17 @@ public class ConsentBumpMoreOptionsFragment extends Fragment {
             fragmentManager.popBackStack();
         });
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mAnimationLooper.start();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mAnimationLooper.stop();
     }
 }
