73080baa3bd55c6e0db993e0f7fb2bedbba5c96a
Maciej Pawlowski <mpawlowski@opera.com>
2018-08-20 18:13:53 +0000

    Don't re-initialize mojo::core in gaia unittests
    mojo::core::Init() should be called once per process, not once per test.
    run_all_unittests.cc happens to already define a test launcher that
    calls mojo::core::Init(), so if we use that instead of the base
    launcher, we can get rid of re-initialization in test fixtures.
    
    Bug: 869281
    Change-Id: Ieb96cbe580f5f1750d7f61693c288bb578cb63b0
    Reviewed-on: https://chromium-review.googlesource.com/1155595
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584501}
    Former-commit-id: a53663e9e5fa4e8e012a89050d818a29589e9119

--- google_apis/run_all_unittests.cc
+++ google_apis/run_all_unittests.cc
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/message_loop/message_loop.h"
 #include "base/test/launcher/unit_test_launcher.h"
 #include "base/test/test_suite.h"
 #include "mojo/core/embedder/embedder.h"
