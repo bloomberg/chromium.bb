64b3fa25d9d15aea4bb46a6f62ec7fd2aab5866b
Jeremy Klein <jlklein@google.com>
2018-08-20 20:43:10 +0000

    Use staging android messages URL until changes go to prod.
    R=azeemarshad@chromium.org
    
    Bug: 850823
    Change-Id: If8779b18e39f498c75798b092816adb79b1a7bfb
    Reviewed-on: https://chromium-review.googlesource.com/1180487
    Commit-Queue: Jeremy Klein <jlklein@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584555}
    Former-commit-id: a8d26f1dc3872d5e267f53f9364acb1b39322914

--- chrome/browser/chromeos/android_sms/android_sms_urls.cc
+++ chrome/browser/chromeos/android_sms/android_sms_urls.cc
@@ -3,6 +3,9 @@
 // found in the LICENSE file.
 
 #include "chrome/browser/chromeos/android_sms/android_sms_urls.h"
+
+#include <string>
+
 #include "base/command_line.h"
 #include "base/optional.h"
 #include "chrome/browser/chromeos/android_sms/android_sms_switches.h"
@@ -14,19 +17,36 @@ namespace android_sms {
 
 namespace {
 
-const char kDefaultAndroidMessagesUrl[] = "https://messages.android.com";
+// NOTE: Using internal staging server until changes roll out to prod.
+const char kDefaultAndroidMessagesUrl[] =
+    "https://android-messages-web.corp.google.com";
 
-}  // namespace
+// NOTE: Using experiment mods until changes roll out to prod.
+const char kExperimentUrlParams[] =
+    "/?e=DittoServiceWorker,DittoPwa,DittoIndexedDb";
 
-GURL GetAndroidMessagesURL() {
+GURL GetURLInternal(bool with_experiments) {
   const base::CommandLine* command_line =
       base::CommandLine::ForCurrentProcess();
   GURL android_messages_url(command_line->GetSwitchValueASCII(
       switches::kAlternateAndroidMessagesUrl));
   if (android_messages_url.is_empty()) {
-    android_messages_url = GURL(kDefaultAndroidMessagesUrl);
+    std::string url_string = std::string(kDefaultAndroidMessagesUrl);
+    if (with_experiments)
+      url_string += std::string(kExperimentUrlParams);
+    android_messages_url = GURL(url_string);
   }
   return android_messages_url;
+}
+
+}  // namespace
+
+GURL GetAndroidMessagesURL() {
+  return GetURLInternal(false /* with_experiments */);
+}
+
+GURL GetAndroidMessagesURLWithExperiments() {
+  return GetURLInternal(true /* with_experiments */);
 }
 
 }  // namespace android_sms
