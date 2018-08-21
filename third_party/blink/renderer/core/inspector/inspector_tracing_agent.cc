2d6dbb5d3706222e6b2f7d6f5cbc9792a06c5bb4
Thomas Anderson <thomasanderson@chromium.org>
2018-08-21 17:33:20 +0000

    Revert "DevTools: Do not start tracing in renderer process explicitly."
    This reverts commit a506b8c8c4ad098281e9f3ef3b5b82297793b699 [formerly c51260229bc32b4f82926536ad474de0f1a9c3d0].
    
    Reason for revert: Suspected cause of linux-rel breakage:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/linux-rel/2129
    
    Original change's description:
    > DevTools: Do not start tracing in renderer process explicitly.
    > 
    > The tracing is now initiated by the browser process. There's no need
    > to start it in renderer anymore. TracingStartedInPage event is not
    > going to be produced anymore. It has been replaced with TracingStartedInBrowser.
    > 
    > Change-Id: I9e7759f7f792421d3f32fd21e82066e13a4901dc
    > Reviewed-on: https://chromium-review.googlesource.com/1182287
    > Commit-Queue: Alexei Filippov <alph@chromium.org>
    > Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#584797}
    
    TBR=dgozman@chromium.org,alph@chromium.org
    
    Change-Id: I16a9138a8ab10ac74e032d02f4d4cbea1b173a30
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/1183802
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584819}
    Former-commit-id: d80f55ce8353053334c7dae6b39c26c2cac46d63

---
+++ third_party/blink/renderer/core/inspector/inspector_tracing_agent.cc
@@ -0,0 +1,92 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/inspector/inspector_tracing_agent.h"
+
+#include "third_party/blink/renderer/core/frame/local_frame.h"
+#include "third_party/blink/renderer/core/inspector/identifiers_factory.h"
+#include "third_party/blink/renderer/core/inspector/inspected_frames.h"
+#include "third_party/blink/renderer/core/inspector/inspector_trace_events.h"
+#include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
+
+namespace blink {
+
+using protocol::Maybe;
+using protocol::Response;
+
+namespace {
+const char kDevtoolsMetadataEventCategory[] =
+    TRACE_DISABLED_BY_DEFAULT("devtools.timeline");
+}
+
+InspectorTracingAgent::InspectorTracingAgent(InspectedFrames* inspected_frames)
+    : session_id_(&agent_state_, /*default_value=*/ WTF::String()),
+      inspected_frames_(inspected_frames) {}
+
+InspectorTracingAgent::~InspectorTracingAgent() {}
+
+void InspectorTracingAgent::Trace(blink::Visitor* visitor) {
+  visitor->Trace(inspected_frames_);
+  InspectorBaseAgent::Trace(visitor);
+}
+
+void InspectorTracingAgent::Restore() {
+  if (IsStarted())
+    EmitMetadataEvents();
+}
+
+void InspectorTracingAgent::start(Maybe<String> categories,
+                                  Maybe<String> options,
+                                  Maybe<double> buffer_usage_reporting_interval,
+                                  Maybe<String> transfer_mode,
+                                  Maybe<String> transfer_compression,
+                                  Maybe<protocol::Tracing::TraceConfig> config,
+                                  std::unique_ptr<StartCallback> callback) {
+  DCHECK(!IsStarted());
+  if (config.isJust()) {
+    callback->sendFailure(Response::Error(
+        "Using trace config on renderer targets is not supported yet."));
+    return;
+  }
+
+  session_id_.Set(IdentifiersFactory::CreateIdentifier());
+
+  // Tracing is already started by DevTools TracingHandler::Start for the
+  // renderer target in the browser process. It will eventually start tracing
+  // in the renderer process via IPC. But we still need a redundant enable here
+  // for EmitMetadataEvents, at which point we are not sure if tracing
+  // is already started in the renderer process.
+  TraceEvent::EnableTracing(categories.fromMaybe(String()));
+
+  EmitMetadataEvents();
+  callback->sendSuccess();
+}
+
+void InspectorTracingAgent::end(std::unique_ptr<EndCallback> callback) {
+  TraceEvent::DisableTracing();
+  InnerDisable();
+  callback->sendSuccess();
+}
+
+bool InspectorTracingAgent::IsStarted() const {
+  return !session_id_.Get().IsEmpty();
+}
+
+void InspectorTracingAgent::EmitMetadataEvents() {
+  TRACE_EVENT_INSTANT1(kDevtoolsMetadataEventCategory, "TracingStartedInPage",
+                       TRACE_EVENT_SCOPE_THREAD, "data",
+                       InspectorTracingStartedInFrame::Data(
+                           session_id_.Get(), inspected_frames_->Root()));
+}
+
+Response InspectorTracingAgent::disable() {
+  InnerDisable();
+  return Response::OK();
+}
+
+void InspectorTracingAgent::InnerDisable() {
+  session_id_.Clear();
+}
+
+}  // namespace blink
