f8dbc8de128827f8b6061ec9df9ad1b603439fb7
Tibor Goldschwendt <tiborg@chromium.org>
2018-08-21 14:31:23 +0000

    [vr] Remove DCHECK whether registered in VrAssetsComponentInstaller
    If we enter VR without a Daydream headset paired the assets component
    won't be registered. This is intended. However, we will always trigger
    an on-demand update no matter if the component is registered or not.
    Thus, remove DCHECK for testing whether component is registered when
    on-demand updating the component. The component updater will just return
    if we try to on-demand update an unregistered component.
    
    Change-Id: If512f9c8aa7170e34864c40333ba6a727da01a58
    Reviewed-on: https://chromium-review.googlesource.com/1182362
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584734}
    Former-commit-id: 0852e1b047bf092346928e0dbf845b17177a6533

--- chrome/browser/component_updater/vr_assets_component_installer.cc
+++ chrome/browser/component_updater/vr_assets_component_installer.cc
@@ -97,8 +97,6 @@ void VrAssetsComponentInstallerPolicy::UpdateComponent(
   ondemand_update_pending_ = false;
   const std::string crx_id = crx_file::id_util::GenerateIdFromHash(
       kVrAssetsPublicKeySHA256, sizeof(kVrAssetsPublicKeySHA256));
-  // Make sure the component is registered.
-  DCHECK(base::ContainsValue(cus->GetComponentIDs(), crx_id));
   cus->GetOnDemandUpdater().OnDemandUpdate(
       crx_id, OnDemandUpdater::Priority::FOREGROUND,
       base::BindOnce([](update_client::Error error) { return; }));
