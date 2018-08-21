5f62f84970f90de500efbe3dfb8556b7bb1b6d4d
DongJun Kim <djmix.kim@samsung.com>
2018-08-21 13:39:47 +0000

    Use ContainsValue() instead of std::find() in components/autofill #2
    autofill : Use ContainsValue() instead of std::find() in components/autofill
    This simplifies many conditions around the code.
    
    Bug: 561800
    Change-Id: I36579da9e0e62fbf834a39419c9bc253546fec24
    Reviewed-on: https://chromium-review.googlesource.com/1183268
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584727}
    Former-commit-id: da17c63f8478e48c9c4bce6d8acbf9490b05ddd1

--- components/autofill/content/renderer/html_based_username_detector.cc
+++ components/autofill/content/renderer/html_based_username_detector.cc
@@ -10,6 +10,7 @@
 #include "base/containers/flat_set.h"
 #include "base/i18n/case_conversion.h"
 #include "base/macros.h"
+#include "base/stl_util.h"
 #include "base/strings/string_split.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/autofill/content/renderer/form_autofill_util.h"
@@ -240,12 +241,9 @@ void FindWordsFromCategoryInForm(
     }
   }
 
-  if (fields_found > 0 && fields_found <= 2) {
-    if (std::find(username_predictions->begin(), username_predictions->end(),
-                  chosen_field) == username_predictions->end()) {
+  if (fields_found > 0 && fields_found <= 2)
+    if (!base::ContainsValue(*username_predictions, chosen_field))
       username_predictions->push_back(chosen_field);
-    }
-  }
 }
 
 // Find username elements if there is no cached result for the given form and
