6bc95da56aa858d17caf292d77dfdecbe59f5923
Dmitry Gozman <dgozman@chromium.org>
2018-08-21 00:05:37 +0000

    Move TimeClamper to core/timing
    TimeClamper is only used by Performance, so there is no need
    to put it as low as platform/.
    
    Change-Id: I531be082528f2e569bbff08a5ace42076d9bb845
    Reviewed-on: https://chromium-review.googlesource.com/1157174
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584583}
    Former-commit-id: 2c38ce9585da7b0d07aff3b03faf8085548f1c6e

--- third_party/blink/renderer/core/timing/time_clamper.cc
+++ third_party/blink/renderer/core/timing/time_clamper.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "third_party/blink/renderer/platform/time_clamper.h"
+#include "third_party/blink/renderer/core/timing/time_clamper.h"
 
 #include "base/bit_cast.h"
 #include "base/rand_util.h"
