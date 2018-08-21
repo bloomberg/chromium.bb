94682e58b5364bc24b0b1b78d3552f157f79f6a9
David Black <dmblack@google.com>
2018-08-21 22:56:05 +0000

    Finish conversation starters.
    This involved:
    - Limiting conversation starter count to 3.
    - Shuffling and selecting conversation starters to reach 3.
    - Refreshing conversation starter cache for new UI sessions.
    - Center aligning conversation starters (but keeping suggestion chips
      for interaction responses start aligned).
    
    Bug: b:111694337
    Change-Id: I1b5b94fabcd0a37ce3a3400a670db4224fc6a667
    Reviewed-on: https://chromium-review.googlesource.com/1182863
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584916}
    Former-commit-id: 02974f882755d857cef268302274e6c7095dce3a

--- ash/assistant/assistant_cache_controller.h
+++ ash/assistant/assistant_cache_controller.h
@@ -5,7 +5,9 @@
 #ifndef ASH_ASSISTANT_ASSISTANT_CACHE_CONTROLLER_H_
 #define ASH_ASSISTANT_ASSISTANT_CACHE_CONTROLLER_H_
 
+#include "ash/assistant/assistant_controller_observer.h"
 #include "ash/assistant/model/assistant_cache_model.h"
+#include "ash/assistant/model/assistant_ui_model_observer.h"
 #include "ash/public/interfaces/voice_interaction_controller.mojom.h"
 #include "base/macros.h"
 #include "mojo/public/cpp/bindings/binding.h"
@@ -13,10 +15,13 @@
 namespace ash {
 
 class AssistantCacheModelObserver;
+class AssistantController;
 
-class AssistantCacheController : public mojom::VoiceInteractionObserver {
+class AssistantCacheController : public AssistantControllerObserver,
+                                 public AssistantUiModelObserver,
+                                 public mojom::VoiceInteractionObserver {
  public:
-  AssistantCacheController();
+  explicit AssistantCacheController(AssistantController* assistant_controller);
   ~AssistantCacheController() override;
 
   // Returns a reference to the underlying model.
@@ -25,6 +30,13 @@ class AssistantCacheController : public mojom::VoiceInteractionObserver {
   // Adds/removes the specified cache model |observer|.
   void AddModelObserver(AssistantCacheModelObserver* observer);
   void RemoveModelObserver(AssistantCacheModelObserver* observer);
+
+  // AssistantControllerObserver:
+  void OnAssistantControllerConstructed() override;
+  void OnAssistantControllerDestroying() override;
+
+  // AssistantUiModelObserver:
+  void OnUiVisibilityChanged(bool visible, AssistantSource source) override;
 
  private:
   // mojom::VoiceInteractionObserver:
@@ -38,6 +50,8 @@ class AssistantCacheController : public mojom::VoiceInteractionObserver {
       mojom::AssistantAllowedState state) override {}
 
   void UpdateConversationStarters();
+
+  AssistantController* const assistant_controller_;  // Owned by Shell.
 
   mojo::Binding<mojom::VoiceInteractionObserver> voice_interaction_binding_;
 
