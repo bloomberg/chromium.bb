0038a9e730b0cbfdd261d2422ddf3c6cdbe53f25
Tommy Martino <tmartino@chromium.org>
2018-08-21 14:52:57 +0000

    [Autofill Views] Enforcing minimum space btwn text
    Currently in the Autofill dropdown, spacers which are subject to
    flex sizing can end up smaller than the desired minimum size, which
    means the primary and secondary text run together. This CL adds a small
    class for the spacers which enforces a minimum size, and tells the
    layout manager to respect the minimum size.
    
    Screenshots (Googlers only):
    https://docs.google.com/presentation/d/1IeyKg22nSbt8aDkvCGrHx1SBg7UBQIqymWDj2sLJ9JQ/edit?usp=sharing
    
    Change-Id: I3b62af0f8355edf2353485986c7e529f8a0a8017
    Bug: 875014
    Reviewed-on: https://chromium-review.googlesource.com/1179981
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584739}
    Former-commit-id: e6c8f25ff400a0aa43dd0dc7ed2ea6569fe65f7f

--- chrome/browser/ui/views/autofill/autofill_popup_view_native_views.cc
+++ chrome/browser/ui/views/autofill/autofill_popup_view_native_views.cc
@@ -448,7 +448,9 @@ void AutofillPopupItemView::AddSpacerWithSize(int spacer_width,
   auto* spacer = new views::View;
   spacer->SetPreferredSize(gfx::Size(spacer_width, 1));
   AddChildView(spacer);
-  layout->SetFlexForView(spacer, /*flex=*/resize ? 1 : 0);
+  layout->SetFlexForView(spacer,
+                         /*flex=*/resize ? 1 : 0,
+                         /*use_min_size=*/true);
 }
 
 /************** AutofillPopupSuggestionView **************/
