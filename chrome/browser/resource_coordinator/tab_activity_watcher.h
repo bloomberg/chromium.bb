e9c22d8208755a76ba1d12d6167989927e7c2abb
sangwoo.ko <sangwoo108@chromium.org>
2018-08-22 06:22:23 +0000

    Use TabStripModelObserver's new API in chrome/browser/resource_coordinator
    Replace old API with new API. This CL is a refactor
    ans has no intended behavior change.
    
    Bug: 842194
    Change-Id: Iac5ac3946c8590d5983c93d14a725d7de670311a
    Reviewed-on: https://chromium-review.googlesource.com/1179120
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Commit-Queue: Sang Woo Ko <sangwoo108@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#584967}
    Former-commit-id: d2c3c8fb842ac9a74e8592684ae8b1d4d0b17c20

--- chrome/browser/resource_coordinator/tab_activity_watcher.h
+++ chrome/browser/resource_coordinator/tab_activity_watcher.h
@@ -54,17 +54,10 @@ class TabActivityWatcher : public BrowserListObserver,
   void OnBrowserSetLastActive(Browser* browser) override;
 
   // TabStripModelObserver:
-  void TabInsertedAt(TabStripModel* tab_strip_model,
-                     content::WebContents* contents,
-                     int index,
-                     bool foreground) override;
-  void TabDetachedAt(content::WebContents* contents,
-                     int index,
-                     bool was_active) override;
-  void TabReplacedAt(TabStripModel* tab_strip_model,
-                     content::WebContents* old_contents,
-                     content::WebContents* new_contents,
-                     int index) override;
+  void OnTabStripModelChanged(
+      TabStripModel* tab_strip_model,
+      const TabStripModelChange& change,
+      const TabStripSelectionChange& selection) override;
   void TabPinnedStateChanged(TabStripModel* tab_strip_model,
                              content::WebContents* contents,
                              int index) override;
