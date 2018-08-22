513662f97b5785f770bf83f8b9174086f25d4943
Tom Anderson <thomasanderson@chromium.org>
2018-08-22 22:11:52 +0000

    Add libgtk-4-dev to sysroots
    R=thestig
    BUG=876586
    
    Change-Id: I97c9609bd49f034027f3eb18ae7f57fef14c2267
    Reviewed-on: https://chromium-review.googlesource.com/1096401
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585275}
    Former-commit-id: 9e44ffb4da52bc4ffa7c039742898ba20524875a

--- build/linux/sysroot_scripts/sysroot-creator-sid.sh
+++ build/linux/sysroot_scripts/sysroot-creator-sid.sh
@@ -8,7 +8,14 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DISTRO=debian
 DIST=sid
 
-APT_SOURCES_LIST="http://ftp.us.debian.org/debian/ sid main"
+# Keep the "experimental" repo before the "sid" repo.  There are some packages
+# that are currently only available in experimental like libgtk-4-0, but if it
+# were to be placed first, experimental (unreleased) versions of other packages
+# like libc6 would take precedence over the sid (released) versions.  While this
+# may be useful for certain kinds of development, the standard sysroots should
+# continue to be shipped only with released packages.
+APT_SOURCES_LIST="http://ftp.us.debian.org/debian/ experimental main
+http://ftp.us.debian.org/debian/ sid main"
 
 # gpg keyring file generated using:
 #   export KEYS="518E17E1 46925553 2B90D010 C857C906 F66AEC98 8AE22BA9 1A7B6500"
@@ -94,12 +101,14 @@ DEBIAN_PACKAGES="\
   libexpat1-dev
   libffi-dev
   libffi6
+  libffi7
   libflac-dev
   libflac8
   libfontconfig1
   libfontconfig1-dev
   libfreetype6
   libfreetype6-dev
+  libfribidi-dev
   libfribidi0
   libgbm-dev
   libgbm1
@@ -113,6 +122,7 @@ DEBIAN_PACKAGES="\
   libgl1-mesa-dev
   libgl1-mesa-glx
   libglapi-mesa
+  libgles1
   libgles2
   libglib2.0-0
   libglib2.0-dev
@@ -130,12 +140,16 @@ DEBIAN_PACKAGES="\
   libgomp1
   libgpg-error-dev
   libgpg-error0
+  libgraphene-1.0-0
+  libgraphene-1.0-dev
   libgraphite2-3
   libgraphite2-dev
   libgssapi-krb5-2
   libgssrpc4
   libgtk-3-0
   libgtk-3-dev
+  libgtk-4-0
+  libgtk-4-dev
   libgtk2.0-0
   libharfbuzz-dev
   libharfbuzz-gobject0
@@ -143,7 +157,9 @@ DEBIAN_PACKAGES="\
   libharfbuzz0b
   libhogweed4
   libice6
+  libicu-le-hb0
   libicu57
+  libicu60
   libidl-2-0
   libidn11
   libidn2-0
@@ -205,6 +221,7 @@ DEBIAN_PACKAGES="\
   libpixman-1-dev
   libpng-dev
   libpng16-16
+  libpsl5
   libpthread-stubs0-dev
   libpulse-dev
   libpulse-mainloop-glib0
@@ -246,9 +263,11 @@ DEBIAN_PACKAGES="\
   libvorbisenc2
   libvpx-dev
   libvpx5
+  libvulkan1
   libwayland-client0
   libwayland-cursor0
   libwayland-dev
+  libwayland-egl1
   libwayland-egl1-mesa
   libwayland-server0
   libwebp-dev
