dc237950af44d00dc80277c0ef74126a4667ef50
Steven Bennetts <stevenjb@chromium.org>
2018-08-22 17:45:33 +0000

    Add ChromeKeyboardWebContents
    This separates the WebContentsObserver and WebContentsDelegate code
    from ChromeKeyboardUI.
    
    Note: ChromeKeyboardWebContents has a dependency on ui::keyboard that
    will need to be replaced with a mojo API call for multi process mash,
    but the separation should at least help with the single process mash
    split, and calls out / documents where the multi process mash change
    needs to happen.
    
    Bug: 843332
    Change-Id: I09a8ea3ac0536737d395fda372b98919221c1a3a
    Reviewed-on: https://chromium-review.googlesource.com/1182679
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585146}
    Former-commit-id: 41b1fa513dddfb04b2c18503e1f97a76f1560114

--- chrome/browser/ui/ash/chrome_keyboard_ui.cc
+++ chrome/browser/ui/ash/chrome_keyboard_ui.cc
@@ -14,27 +14,16 @@
 #include "base/macros.h"
 #include "base/no_destructor.h"
 #include "base/values.h"
-#include "chrome/browser/extensions/chrome_extension_web_contents_observer.h"
-#include "chrome/browser/media/webrtc/media_capture_devices_dispatcher.h"
-#include "content/public/browser/host_zoom_map.h"
-#include "content/public/browser/render_process_host.h"
-#include "content/public/browser/render_view_host.h"
+#include "chrome/browser/ui/ash/chrome_keyboard_web_contents.h"
 #include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/render_widget_host_iterator.h"
 #include "content/public/browser/render_widget_host_view.h"
-#include "content/public/browser/site_instance.h"
 #include "content/public/browser/web_contents.h"
-#include "content/public/browser/web_contents_delegate.h"
-#include "content/public/browser/web_contents_observer.h"
-#include "content/public/browser/web_ui.h"
 #include "content/public/common/bindings_policy.h"
 #include "extensions/browser/api/virtual_keyboard_private/virtual_keyboard_delegate.h"
 #include "extensions/browser/api/virtual_keyboard_private/virtual_keyboard_private_api.h"
 #include "extensions/browser/event_router.h"
-#include "extensions/browser/extension_registry.h"
-#include "extensions/browser/view_type_utils.h"
 #include "extensions/common/api/virtual_keyboard_private.h"
-#include "extensions/common/constants.h"
 #include "extensions/common/extension_messages.h"
 #include "ipc/ipc_message_macros.h"
 #include "ui/accessibility/ax_enums.mojom.h"
@@ -52,8 +41,6 @@
 #include "ui/keyboard/keyboard_controller.h"
 #include "ui/keyboard/keyboard_controller_observer.h"
 #include "ui/keyboard/keyboard_resource_util.h"
-#include "ui/keyboard/keyboard_switches.h"
-#include "ui/keyboard/keyboard_util.h"
 #include "ui/wm/core/shadow_types.h"
 
 namespace virtual_keyboard_private = extensions::api::virtual_keyboard_private;
@@ -66,102 +53,6 @@ GURL& GetOverrideVirtualKeyboardUrl() {
   static base::NoDestructor<GURL> url;
   return *url;
 }
-
-// The WebContentsDelegate for the chrome keyboard.
-// The delegate deletes itself when the keyboard is destroyed.
-class ChromeKeyboardContentsDelegate : public content::WebContentsDelegate,
-                                       public content::WebContentsObserver {
- public:
-  ChromeKeyboardContentsDelegate() = default;
-  ~ChromeKeyboardContentsDelegate() override = default;
-
- private:
-  // content::WebContentsDelegate:
-  content::WebContents* OpenURLFromTab(
-      content::WebContents* source,
-      const content::OpenURLParams& params) override {
-    source->GetController().LoadURL(params.url, params.referrer,
-                                    params.transition, params.extra_headers);
-    Observe(source);
-    return source;
-  }
-
-  bool CanDragEnter(content::WebContents* source,
-                    const content::DropData& data,
-                    blink::WebDragOperationsMask operations_allowed) override {
-    return false;
-  }
-
-  bool ShouldCreateWebContents(
-      content::WebContents* web_contents,
-      content::RenderFrameHost* opener,
-      content::SiteInstance* source_site_instance,
-      int32_t route_id,
-      int32_t main_frame_route_id,
-      int32_t main_frame_widget_route_id,
-      content::mojom::WindowContainerType window_container_type,
-      const GURL& opener_url,
-      const std::string& frame_name,
-      const GURL& target_url,
-      const std::string& partition_id,
-      content::SessionStorageNamespace* session_storage_namespace) override {
-    return false;
-  }
-
-  void SetContentsBounds(content::WebContents* source,
-                         const gfx::Rect& bounds) override {
-    aura::Window* keyboard_window = source->GetNativeView();
-    // keyboard window must have been added to keyboard container window at this
-    // point. Otherwise, wrong keyboard bounds is used and may cause problem as
-    // described in crbug.com/367788.
-    DCHECK(keyboard_window->parent());
-    // keyboard window bounds may not set to |pos| after this call. If keyboard
-    // is in FULL_WIDTH mode, only the height of keyboard window will be
-    // changed.
-    keyboard_window->SetBounds(bounds);
-  }
-
-  // content::WebContentsDelegate:
-  void RequestMediaAccessPermission(
-      content::WebContents* web_contents,
-      const content::MediaStreamRequest& request,
-      content::MediaResponseCallback callback) override {
-    const extensions::Extension* extension = nullptr;
-    GURL origin(request.security_origin);
-    if (origin.SchemeIs(extensions::kExtensionScheme)) {
-      const extensions::ExtensionRegistry* registry =
-          extensions::ExtensionRegistry::Get(web_contents->GetBrowserContext());
-      extension = registry->enabled_extensions().GetByID(origin.host());
-      DCHECK(extension);
-    }
-    MediaCaptureDevicesDispatcher::GetInstance()->ProcessMediaAccessRequest(
-        web_contents, request, std::move(callback), extension);
-  }
-
-  // content::WebContentsDelegate:
-  bool PreHandleGestureEvent(content::WebContents* source,
-                             const blink::WebGestureEvent& event) override {
-    switch (event.GetType()) {
-      // Scroll events are not suppressed because the menu to select IME should
-      // be scrollable.
-      case blink::WebInputEvent::kGestureScrollBegin:
-      case blink::WebInputEvent::kGestureScrollEnd:
-      case blink::WebInputEvent::kGestureScrollUpdate:
-      case blink::WebInputEvent::kGestureFlingStart:
-      case blink::WebInputEvent::kGestureFlingCancel:
-        return false;
-      default:
-        // Stop gesture events from being passed to renderer to suppress the
-        // context menu. crbug.com/685140
-        return true;
-    }
-  }
-
-  // content::WebContentsObserver:
-  void WebContentsDestroyed() override { delete this; }
-
-  DISALLOW_COPY_AND_ASSIGN(ChromeKeyboardContentsDelegate);
-};
 
 class AshKeyboardControllerObserver
     : public keyboard::KeyboardControllerObserver {
@@ -302,21 +193,20 @@ void ChromeKeyboardUI::UpdateInsetsForWindow(aura::Window* window) {
 
 aura::Window* ChromeKeyboardUI::GetKeyboardWindow() {
   if (keyboard_contents_)
-    return keyboard_contents_->GetNativeView();
+    return keyboard_contents_->web_contents()->GetNativeView();
 
   GURL keyboard_url = GetVirtualKeyboardUrl();
-  content::WebContents::CreateParams web_contents_params(
-      browser_context_,
-      content::SiteInstance::CreateForURL(browser_context_, keyboard_url));
-  keyboard_contents_ = content::WebContents::Create(web_contents_params);
-  keyboard_contents_->SetDelegate(new ChromeKeyboardContentsDelegate());
-  SetupWebContents(keyboard_contents_.get());
-  LoadContents(keyboard_url);
-  aura::Window* keyboard_window = keyboard_contents_->GetNativeView();
+  keyboard_contents_ = std::make_unique<ChromeKeyboardWebContents>(
+      browser_context_, keyboard_url);
+
+  aura::Window* keyboard_window =
+      keyboard_contents_->web_contents()->GetNativeView();
+
   keyboard_window->AddObserver(this);
   keyboard_window->set_owned_by_parent(false);
+
   content::RenderWidgetHostView* view =
-      keyboard_contents_->GetMainFrame()->GetView();
+      keyboard_contents_->web_contents()->GetMainFrame()->GetView();
 
   // Only use transparent background when fullscreen handwriting or the new UI
   // is enabled. The old UI sometimes reloads itself, which will cause the
@@ -364,19 +254,7 @@ void ChromeKeyboardUI::SetController(keyboard::KeyboardController* controller) {
 
 void ChromeKeyboardUI::ReloadKeyboardIfNeeded() {
   DCHECK(keyboard_contents_);
-  GURL keyboard_url = GetVirtualKeyboardUrl();
-  if (keyboard_contents_->GetURL() != keyboard_url) {
-    if (keyboard_contents_->GetURL().GetOrigin() != keyboard_url.GetOrigin()) {
-      // Sets keyboard window rectangle to 0 and close current page before
-      // navigate to a keyboard in a different extension. This keeps the UX the
-      // same as Android. Note we need to explicitly close current page as it
-      // might try to resize keyboard window in javascript on a resize event.
-      TRACE_EVENT0("vk", "ReloadKeyboardIfNeeded");
-      GetKeyboardWindow()->SetBounds(gfx::Rect());
-      keyboard_contents_->ClosePage();
-    }
-    LoadContents(keyboard_url);
-  }
+  keyboard_contents_->SetKeyboardUrl(GetVirtualKeyboardUrl());
 }
 
 void ChromeKeyboardUI::InitInsets(const gfx::Rect& new_bounds) {
@@ -440,16 +318,6 @@ void ChromeKeyboardUI::OnWindowDestroyed(aura::Window* window) {
 void ChromeKeyboardUI::OnWindowParentChanged(aura::Window* window,
                                              aura::Window* parent) {
   SetShadowAroundKeyboard();
-}
-
-void ChromeKeyboardUI::LoadContents(const GURL& url) {
-  if (keyboard_contents_) {
-    TRACE_EVENT0("vk", "LoadContents");
-    content::OpenURLParams params(url, content::Referrer(),
-                                  WindowOpenDisposition::SINGLETON_TAB,
-                                  ui::PAGE_TRANSITION_AUTO_TOPLEVEL, false);
-    keyboard_contents_->OpenURL(params);
-  }
 }
 
 GURL ChromeKeyboardUI::GetVirtualKeyboardUrl() {
@@ -529,27 +397,4 @@ void ChromeKeyboardUI::SetShadowAroundKeyboard() {
   shadow_->layer()->SetVisible(
       keyboard_controller()->GetActiveContainerType() ==
       keyboard::ContainerType::FULL_WIDTH);
-}
-
-void ChromeKeyboardUI::SetupWebContents(content::WebContents* contents) {
-  extensions::SetViewType(contents, extensions::VIEW_TYPE_COMPONENT);
-  extensions::ChromeExtensionWebContentsObserver::CreateForWebContents(
-      contents);
-  Observe(contents);
-}
-
-void ChromeKeyboardUI::RenderViewCreated(
-    content::RenderViewHost* render_view_host) {
-  content::HostZoomMap* zoom_map =
-      content::HostZoomMap::GetDefaultForBrowserContext(browser_context_);
-  DCHECK(zoom_map);
-  int render_process_id = render_view_host->GetProcess()->GetID();
-  int render_view_id = render_view_host->GetRoutingID();
-  zoom_map->SetTemporaryZoomLevel(render_process_id, render_view_id, 0);
-}
-
-void ChromeKeyboardUI::DidFinishLoad(
-    content::RenderFrameHost* render_frame_host,
-    const GURL& validated_url) {
-  keyboard_controller()->NotifyKeyboardWindowLoaded();
 }
