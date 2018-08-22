24ed0259238014f47d484f5aa7bfd6689faf113b
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2018-08-22 14:21:48 +0000

    [iOS][MF] Update keyboard accessory view
    Adds the new icon for addresses and for keyboard.
    
    Bug: 845472
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I775b06b39a8f797c16669511ab8aa057bdd14133
    Reviewed-on: https://chromium-review.googlesource.com/1183223
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585009}
    Former-commit-id: 0b6f9c31effad8383009582bc60a7b9c196fdc79

--- ios/chrome/browser/ui/autofill/manual_fill/manual_fill_accessory_view_controller.h
+++ ios/chrome/browser/ui/autofill/manual_fill/manual_fill_accessory_view_controller.h
@@ -2,19 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_KEYBOARD_ACCESSORY_VIEW_H_
-#define IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_KEYBOARD_ACCESSORY_VIEW_H_
+#ifndef IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_MANUAL_FILL_ACCESSORY_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_MANUAL_FILL_ACCESSORY_VIEW_CONTROLLER_H_
 
 #import <UIKit/UIKit.h>
 
-// Protocol to handle user interactions in a ManualFillKeyboardAccessoryView.
-@protocol ManualFillKeyboardAccessoryViewDelegate
+// Protocol to handle user interactions in a ManualFillAccessoryViewController.
+@protocol ManualFillAccessoryViewControllerDelegate
 
 // Invoked after the user touches the `accounts` button.
 - (void)accountButtonPressed;
 
 // Invoked after the user touches the `credit cards` button.
 - (void)cardButtonPressed;
+
+// Invoked after the user touches the `keyboard` button.
+- (void)keyboardButtonPressed;
 
 // Invoked after the user touches the `passwords` button.
 - (void)passwordButtonPressed;
@@ -23,28 +26,26 @@
 
 // This view contains the icons to activate "Manual Fill". It is meant to be
 // shown above the keyboard on iPhone and above the manual fill view.
-@interface ManualFillKeyboardAccessoryView : UIView
+@interface ManualFillAccessoryViewController : UIViewController
 
 // Instances an object with the desired delegate.
 //
 // @param delegate The delegate for this object.
 // @return A fresh object with the passed delegate.
 - (instancetype)initWithDelegate:
-    (id<ManualFillKeyboardAccessoryViewDelegate>)delegate
+    (id<ManualFillAccessoryViewControllerDelegate>)delegate
     NS_DESIGNATED_INITIALIZER;
 
 // Unavailable. Use `initWithDelegate:`.
+- (instancetype)initWithNibName:(NSString*)nibNameOrNil
+                         bundle:(NSBundle*)nibBundleOrNil NS_UNAVAILABLE;
+- (instancetype)initWithCoder:(NSCoder*)aDecoder NS_UNAVAILABLE;
 - (instancetype)init NS_UNAVAILABLE;
 
-// Unavailable. Use `initWithDelegate:`.
-- (instancetype)initWithCoder:(NSCoder*)aDecoder NS_UNAVAILABLE;
-
-// Unavailable. Use `initWithDelegate:`.
-- (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
-
-// Unavailable. Use `initWithDelegate:`.
-- (instancetype)initWithCoder:(NSCoder*)aDecoder NS_UNAVAILABLE;
+// Resets to the original state, with the keyboard icon hidden and no icon
+// selected.
+- (void)reset;
 
 @end
 
-#endif  // IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_KEYBOARD_ACCESSORY_VIEW_H_
+#endif  // IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_MANUAL_FILL_ACCESSORY_VIEW_CONTROLLER_H_
