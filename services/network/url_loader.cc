d436a0fbce5fc6035fa9e529b23c0255aade8df0
Clark DuVall <cduvall@chromium.org>
2018-08-22 16:31:02 +0000

    Fix PageLoadMetricsBrowserTest.ReceivedCompleteResources with network service
    We need to notify download progress even if report_raw_headers is not
    set.
    
    Bug: 875298
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I1074a50ca60cdbbb00cd265d83487abe55160732
    Reviewed-on: https://chromium-review.googlesource.com/1184038
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585052}
    Former-commit-id: 40d057eba6707d3838a09bd6e3be861dd65d85ad

--- services/network/url_loader.cc
+++ services/network/url_loader.cc
@@ -778,9 +778,9 @@ void URLLoader::DidRead(int num_bytes, bool completed_synchronously) {
   if (num_bytes > 0) {
     pending_write_buffer_offset_ += num_bytes;
 
-    // Only notify client of download progress in case DevTools are attached
-    // and we're done sniffing and started sending response.
-    if (report_raw_headers_ && !consumer_handle_.is_valid()) {
+    // Only notify client of download progress if we're done sniffing and
+    // started sending response.
+    if (!consumer_handle_.is_valid()) {
       int64_t total_encoded_bytes = url_request_->GetTotalReceivedBytes();
       int64_t delta = total_encoded_bytes - reported_total_encoded_bytes_;
       DCHECK_LE(0, delta);
