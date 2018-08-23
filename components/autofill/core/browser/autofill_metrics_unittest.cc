37f7711446704704d13108d791d1b147ebf35247
Hayato Ito <hayato@chromium.org>
2018-08-23 05:53:13 +0000

    Disable flaky AutofillMetricsTest.AutofillSuggestionShownTest on Win
    TBR=dvadym@chromium.org
    
    BUG: 876954
    Change-Id: Id8c8994dbb3c17abdaec0e3136ce825ea9d9c20b
    Reviewed-on: https://chromium-review.googlesource.com/1186205
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585407}
    Former-commit-id: b08a5594fb85db5c27c3fd484abcee2e3eebec09

--- components/autofill/core/browser/autofill_metrics_unittest.cc
+++ components/autofill/core/browser/autofill_metrics_unittest.cc
@@ -7402,7 +7402,13 @@ TEST_F(AutofillMetricsTest, RecordCardUploadDecisionMetric_NoUkmService) {
 }
 
 // Test the ukm recorded when Suggestion is shown.
-TEST_F(AutofillMetricsTest, AutofillSuggestionShownTest) {
+// Flaky on Win.  http://crbug.com/876954
+#if defined(OS_WIN)
+#define MAYBE_AutofillSuggestionShownTest DISABLED_AutofillSuggestionShownTest
+#else
+#define MAYBE_AutofillSuggestionShownTest AutofillSuggestionShownTest
+#endif
+TEST_F(AutofillMetricsTest, MAYBE_AutofillSuggestionShownTest) {
   RecreateCreditCards(true /* include_local_credit_card */,
                       false /* include_masked_server_credit_card */,
                       false /* include_full_server_credit_card */);
