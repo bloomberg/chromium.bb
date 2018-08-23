bb7d8811801028fef87e89e5d2dc6f3c59e9424b
Gabriel Marin <gmx@chromium.org>
2018-08-23 18:35:26 +0000

    tcmalloc: restrict maximum size of memory allocations
    Original CL: https://chromiumcodereview.appspot.com/11857007
    
      TCMalloc: restrict maximum size of memory allocations
    
      For security purposes, we restrict the maximum size of memory
      allocations under what can be indexed by an int.
    
      BUG=169327
      NOTRY=true
    
    BUG=724399,b:70905156
    
    Change-Id: I530659b9d0f519db1bc70f8d8f5b15537dc1f1b8
    Reviewed-on: https://chromium-review.googlesource.com/1184336
    Commit-Queue: Gabriel Marin <gmx@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585558}
    Former-commit-id: 93604967c25f5e54d5e7d8e94e9f8edf752a7f37

--- third_party/tcmalloc/chromium/src/tcmalloc.cc
+++ third_party/tcmalloc/chromium/src/tcmalloc.cc
@@ -135,6 +135,9 @@
 
 #include "maybe_emergency_malloc.h"
 
+// Remove to enable huge allocations (> 2GB), the default for gperftools.
+#define TCMALLOC_DISABLE_HUGE_ALLOCATIONS
+
 #if (defined(_WIN32) && !defined(__CYGWIN__) && !defined(__CYGWIN32__)) && !defined(WIN32_OVERRIDE_ALLOCATORS)
 # define WIN32_DO_PATCHING 1
 #endif
@@ -343,6 +346,18 @@ size_t InvalidGetAllocatedSize(const void* ptr) {
       "Attempt to get the size of an invalid pointer", ptr);
   return 0;
 }
+
+#if defined(TCMALLOC_DISABLE_HUGE_ALLOCATIONS)
+// For security reasons, we want to limit the size of allocations.
+// See crbug.com/169327.
+inline bool IsAllocSizePermitted(size_t alloc_size) {
+  // Never allow an allocation larger than what can be indexed via an int.
+  // Remove kPageSize to account for various rounding, padding and to have a
+  // small margin.
+  return alloc_size <= ((std::numeric_limits<int>::max)() - kPageSize);
+}
+#endif
+
 }  // unnamed namespace
 
 // Extract interesting stats
@@ -1347,6 +1362,13 @@ static void *nop_oom_handler(size_t size) {
 }
 
 ATTRIBUTE_ALWAYS_INLINE inline void* do_malloc(size_t size) {
+#if defined(TCMALLOC_DISABLE_HUGE_ALLOCATIONS)
+  if (!IsAllocSizePermitted(size)) {
+    errno = ENOMEM;
+    return NULL;
+  }
+#endif
+
   if (PREDICT_FALSE(ThreadCache::IsUseEmergencyMalloc())) {
     return tcmalloc::EmergencyMalloc(size);
   }
