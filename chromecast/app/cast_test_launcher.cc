5c9f92a4ff7ec98cf349b22f8bad404bc85677a9
Wez <wez@chromium.org>
2018-08-23 23:21:39 +0000

    Check that TaskScheduler is not leaked between tests, or test-cases.
    - Move some APIs off of base::TestSuite, that we unnecessarily public.
    - Rename the various internal TestEventListeners to express purpose.
    - Add CheckForLeakedGlobals check, and DisableCheckForLeakedGlobals API.
      The latter is called by the content::ContentTestSuiteBase, since the
      various browser- and ui-tests running under that suite base run more
      like the browser itself, and so are expected to let global singletons
      leak.
    
    Bug: 875486
    Change-Id: If749dfe8530abc4a5c13a840bbf8258d48c377c5
    Reviewed-on: https://chromium-review.googlesource.com/1182612
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585652}
    Former-commit-id: a2a4e75d2752687d91e1272450bdcc6e48b33739

--- chromecast/app/cast_test_launcher.cc
+++ chromecast/app/cast_test_launcher.cc
@@ -21,7 +21,10 @@ class CastTestLauncherDelegate : public content::TestLauncherDelegate {
   ~CastTestLauncherDelegate() override {}
 
   int RunTestSuite(int argc, char** argv) override {
-    return base::TestSuite(argc, argv).Run();
+    base::TestSuite test_suite(argc, argv);
+    // Browser tests are expected not to tear-down various globals.
+    test_suite.DisableCheckForLeakedGlobals();
+    return test_suite.Run();
   }
 
   bool AdjustChildProcessCommandLine(
