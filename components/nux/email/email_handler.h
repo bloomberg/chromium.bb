feae71794b83d22568c4a3ccd5863a15fd25322c
Hector Carmona <hcarmona@chromium.org>
2018-08-23 17:37:25 +0000

    NUX Email: Create initial files for Email NUX.
    These files were copied from Google Apps NUX and modified slightly.
    
    Bug: 832933
    Change-Id: I8fd79b2e00d2224e20c6159f478e54defaad0d73
    Reviewed-on: https://chromium-review.googlesource.com/1185919
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Commit-Queue: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585534}
    Former-commit-id: 8a6cc7eedf584dd15b16b8e41d736ec1269a84cc

---
+++ components/nux/email/email_handler.h
@@ -0,0 +1,71 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_NUX_EMAIL_EMAIL_HANDLER_H_
+#define COMPONENTS_NUX_EMAIL_EMAIL_HANDLER_H_
+
+#include "base/macros.h"
+#include "base/values.h"
+#include "content/public/browser/web_ui_message_handler.h"
+
+class PrefService;
+
+namespace bookmarks {
+class BookmarkModel;
+}  // namespace bookmarks
+
+namespace content {
+class WebUIDataSource;
+}  // namespace content
+
+namespace favicon {
+class FaviconService;
+}  // namespace favicon
+
+namespace nux {
+
+extern const char* kEmailInteractionHistogram;
+
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class EmailInteraction {
+  kPromptShown = 0,
+  kNoThanks = 1,
+  kGetStarted = 2,
+  kCount,
+};
+
+class EmailHandler : public content::WebUIMessageHandler {
+ public:
+  EmailHandler(PrefService* prefs,
+               favicon::FaviconService* favicon_service,
+               bookmarks::BookmarkModel* bookmark_model);
+  ~EmailHandler() override;
+
+  // WebUIMessageHandler:
+  void RegisterMessages() override;
+
+  // Callbacks for JS APIs.
+  void HandleRejectEmails(const base::ListValue* args);
+  void HandleAddEmails(const base::ListValue* args);
+
+  // Adds webui sources.
+  static void AddSources(content::WebUIDataSource* html_source);
+
+ private:
+  // Weak reference.
+  PrefService* prefs_;
+
+  // Weak reference.
+  favicon::FaviconService* favicon_service_;
+
+  // Weak reference.
+  bookmarks::BookmarkModel* bookmark_model_;
+
+  DISALLOW_COPY_AND_ASSIGN(EmailHandler);
+};
+
+}  // namespace nux
+
+#endif  // COMPONENTS_NUX_GOOGLE_APPS_GOOGLE_APPS_HANDLER_H_
