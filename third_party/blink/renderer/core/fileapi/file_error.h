3cb5cb925993c21ef8e74642447a619b680e88cf
Marijn Kruisselbrink <mek@chromium.org>
2018-08-23 02:06:39 +0000

    [FileSystem] Use more descriptive error messages/codes in new APIs.
    This adds a method to blink::FileError that more or less does the same
    as blink::FileError::CreateDOMException(content::FileErrorToWebFileError(code)),
    just without the extra conversion through WebFileError and
    blink::FileError::ErrorCode in between.
    
    Bug: 872460, 872465
    Change-Id: I6b7f94d9a3da2fa730cf78f5a74d5315d34c95c0
    Reviewed-on: https://chromium-review.googlesource.com/1185791
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585367}
    Former-commit-id: 9a6a0cab0631500581bab3d33a69746212fb1301

--- third_party/blink/renderer/core/fileapi/file_error.h
+++ third_party/blink/renderer/core/fileapi/file_error.h
@@ -31,6 +31,7 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_FILEAPI_FILE_ERROR_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_FILEAPI_FILE_ERROR_H_
 
+#include "base/files/file.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
@@ -73,6 +74,7 @@ CORE_EXPORT void ThrowDOMException(ExceptionState&,
                                    ErrorCode,
                                    String message = String());
 CORE_EXPORT DOMException* CreateDOMException(ErrorCode);
+CORE_EXPORT DOMException* CreateDOMException(base::File::Error);
 
 }  // namespace FileError
 
