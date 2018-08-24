ec7e6abaddf41e5238378169d4b5abe656908c29
Hugo Holgersson <hugoh@vewd.com>
2018-08-24 15:50:52 +0000

    Snav: Assert focus at keyup, not at setTimout(0, ...)
    "... the default action MUST be to shift the document focus".
    https://www.w3.org/TR/uievents/#event-type-keydown
    
    As focus movement happens during keydown, once we got the
    succeeding keyup-event, it's safe to assert activeElement.
    
    Why not do the asserts at 'focusin' instead?
    After 'keyup', we're safe to send another trigger-key.
    (Most spatnav tests verify several focus movements).
    
    Bug: 803086
    Change-Id: I6927d61a9d323f5bd3fe8a6022aef2f6eca50521
    Reviewed-on: https://chromium-review.googlesource.com/1188476
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Hugo Holgersson <hugoh@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#585855}
    Former-commit-id: 64440a46c370db1c91083e21dfb9176ee60944df

--- third_party/WebKit/LayoutTests/fast/spatial-navigation/resources/snav-testharness.js
+++ third_party/WebKit/LayoutTests/fast/spatial-navigation/resources/snav-testharness.js
@@ -18,13 +18,6 @@
     }
   }
 
-  function moveFocusAsync(direction) {
-    return new Promise(function(resolve) {
-      triggerMove(direction);
-      setTimeout(resolve, 0);
-    });
-  }
-
   function stepAndAssertMoves(expectedMoves, focusMoves) {
     if (expectedMoves.length == 0)
       return;
@@ -32,19 +25,24 @@
     let move = expectedMoves.shift();
     let direction = move[0];
     let expectedId = move[1];
+    // TODO: Use WebDriver's API instead of eventSender.
+    //       Hopefully something like:
+    //         test_driver.move_focus(direction).then(...)
     async_test(t => {
-      // TODO: Use WebDriver's API instead of eventSender.
-      //       Hopefully something like:
-      //         test_driver.move_focus(direction).then(...)
-      moveFocusAsync(direction).then(() => {
-        t.step(() => {
-          let expectedElement = document.getElementById(expectedId);
-          assert_equals(document.activeElement, expectedElement);
-          // Kick off another async test before closing this one.
-          stepAndAssertMoves(expectedMoves, focusMoves + 1);
-          t.done();
-        });
+      let checkFocus = t.step_func_done(function() {
+        let expectedElement = document.getElementById(expectedId);
+        assert_equals(document.activeElement, expectedElement);
+
+        // Kick off another async test before closing this one.
+        stepAndAssertMoves(expectedMoves, focusMoves + 1);
       });
+
+      // "... the default action MUST be to shift the document focus".
+      //   https://www.w3.org/TR/uievents/#event-type-keydown
+      // Any focus movement must have happened during keydown, so once we got
+      // the succeeding keyup-event, it's safe to assert activeElement.
+      document.addEventListener('keyup', checkFocus, {once: true});
+      triggerMove(direction);
     }, focusMoves + "th move: " + direction + '-key moves focus to ' +
        'the element with id: ' + expectedId + '.');
   }
