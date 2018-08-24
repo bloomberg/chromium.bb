a8fb48639c38db1d22ed502efb5abe3d375091aa
Kevin Marshall <kmarshall@chromium.org>
2018-08-24 00:11:34 +0000

    [fuchsia] Fix threading for FilteredServiceDirectory connect call.
    Previously, ChildProcessLauncherHelper would bind a ServiceDirectory
    connection on the launcher thread. The binding would attach to the
    asynchronous dispatcher of the launcher task runner's thread,
    rather than the client thread. This generated some test flakiness
    during test teardown.
    
    This CL moves the ServiceDirectory binding call so that it runs
    in the client thread instead.
    
    Bug: 876894
    Change-Id: I53e05d79a24ab8347e3df697f65f84601f618f40
    Reviewed-on: https://chromium-review.googlesource.com/1187125
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#585667}
    Former-commit-id: 98f6ea73cededae100a81f046c4b1f57146547b7

--- content/common/sandbox_policy_fuchsia.h
+++ content/common/sandbox_policy_fuchsia.h
@@ -5,6 +5,8 @@
 #ifndef CONTENT_COMMON_SANDBOX_POLICY_FUCHSIA_H_
 #define CONTENT_COMMON_SANDBOX_POLICY_FUCHSIA_H_
 
+#include <lib/zx/channel.h>
+
 #include "base/memory/ref_counted.h"
 #include "services/service_manager/sandbox/sandbox_type.h"
 
@@ -40,6 +42,7 @@ class SandboxPolicyFuchsia {
 
   // Services directory used for the /svc namespace of the child process.
   std::unique_ptr<base::fuchsia::FilteredServiceDirectory> service_directory_;
+  zx::channel service_directory_client_channel_;
   scoped_refptr<base::SequencedTaskRunner> service_directory_task_runner_;
 };
 
