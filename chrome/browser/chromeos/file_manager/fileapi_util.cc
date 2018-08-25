63529d23bcd1a0ed8fec6a360a817307b326d222
Wez <wez@chromium.org>
2018-08-25 00:19:02 +0000

    Reland "Simplify implementation of FileSystemOperationRunner."
    This reverts commit 955a0f094bba8e17a2ac0b9f1fc031bf18370291 [formerly acf1c986e3c9e31fae898d2413fbe054c26b8cf9], which
    speculatively reverted the change to establish whether it might be
    responsible for a spike in BlobReader crashes. Re-landing now, since the
    revert had no impact on crash rate of that signature in Canary (see
    https://crbug.com/864351).
    
    - Use a base::AutoReset instance to manage a single integer tracking
      whether the completion callback for an operation is invoked before
      we have actually returned to the caller.
    - Remove the now-unused OperationHandle and BeginOperationScoper.
    - Replace SupportsWeakPtr with an internal WeakPtrFactory.
    - Replace potentially unsafe use of GetWeakPtr() with a |weak_ptr_|
      member.
    - Temporarily disables the recently-added FileSystemURLLoaderFactoryTest
      tests, which have incorrect threading, causing WeakPtr checks to
      fire.
    
    TBR=wez@chromium.org,rockot@chromium.org,alemate@chromium.org,noel@chromium.org,mek@chromium.org,fukino@chromium.org,cmumford@chromium.org,govind@chromium.org,tzik@chromium.org,abdulsyed@chromium.org
    
    Bug: 846985, 860547, 864351
    Change-Id: Ia843811a2e85be9347f410ec9f97dc3dc0f1713a
    Reviewed-on: https://chromium-review.googlesource.com/1189048
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586074}
    Former-commit-id: 3639ab9fd75a78e660dd716d265f1f9e69a9716f

--- chrome/browser/chromeos/file_manager/fileapi_util.cc
+++ chrome/browser/chromeos/file_manager/fileapi_util.cc
@@ -406,6 +406,25 @@ class ConvertSelectedFileInfoListToFileChooserFileInfoListImpl {
       ConvertSelectedFileInfoListToFileChooserFileInfoListImpl);
 };
 
+void CheckIfDirectoryExistsOnIoThread(
+    scoped_refptr<storage::FileSystemContext> file_system_context,
+    const storage::FileSystemURL& internal_url,
+    storage::FileSystemOperationRunner::StatusCallback callback) {
+  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  file_system_context->operation_runner()->DirectoryExists(internal_url,
+                                                           std::move(callback));
+}
+
+void GetMetadataForPathOnIoThread(
+    scoped_refptr<storage::FileSystemContext> file_system_context,
+    const storage::FileSystemURL& internal_url,
+    int fields,
+    storage::FileSystemOperationRunner::GetMetadataCallback callback) {
+  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  file_system_context->operation_runner()->GetMetadata(internal_url, fields,
+                                                       callback);
+}
+
 }  // namespace
 
 EntryDefinition::EntryDefinition() = default;
@@ -548,9 +567,7 @@ void CheckIfDirectoryExists(
 
   BrowserThread::PostTask(
       BrowserThread::IO, FROM_HERE,
-      base::BindOnce(base::IgnoreResult(
-                         &storage::FileSystemOperationRunner::DirectoryExists),
-                     file_system_context->operation_runner()->AsWeakPtr(),
+      base::BindOnce(&CheckIfDirectoryExistsOnIoThread, file_system_context,
                      internal_url, google_apis::CreateRelayCallback(callback)));
 }
 
@@ -569,10 +586,9 @@ void GetMetadataForPath(
 
   BrowserThread::PostTask(
       BrowserThread::IO, FROM_HERE,
-      base::BindOnce(
-          base::IgnoreResult(&storage::FileSystemOperationRunner::GetMetadata),
-          file_system_context->operation_runner()->AsWeakPtr(), internal_url,
-          fields, google_apis::CreateRelayCallback(callback)));
+      base::BindOnce(&GetMetadataForPathOnIoThread, file_system_context,
+                     internal_url, fields,
+                     google_apis::CreateRelayCallback(callback)));
 }
 
 storage::FileSystemURL CreateIsolatedURLFromVirtualPath(
