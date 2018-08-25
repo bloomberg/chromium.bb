586501a8911c09fe4e5396713fa95714ed15d345
Wez <wez@chromium.org>
2018-08-25 21:24:12 +0000

    Fix flaky AXPlatformUniqueIdTest.TestIdsNotReused test.
    - Fix AXPlatformUniqueIdTest.TestIdsNotReused, which made invalid
      assumptions about the specific identifier values that would be
      assigned.
    - Remove unused AXPlatformUniqueIdTest base-class.
    - Renamed both AXPlatformUniqueIdTests to better reflect their purpose.
    
    TBR: aleventhal
    Bug: 877777, 797992
    Change-Id: I0f86092e4e785843522913fa98b0222e04dbd67d
    Reviewed-on: https://chromium-review.googlesource.com/1188698
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586146}
    Former-commit-id: eae7c5e562b04cc03a24bf135214045ee8524107

--- ui/accessibility/platform/ax_unique_id_unittest.cc
+++ ui/accessibility/platform/ax_unique_id_unittest.cc
@@ -4,21 +4,13 @@
 
 #include "ui/accessibility/platform/ax_unique_id.h"
 
+#include <memory>
+
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace ui {
 
-class AXPlatformUniqueIdTest : public testing::Test {
- public:
-  AXPlatformUniqueIdTest() {}
-  ~AXPlatformUniqueIdTest() override {}
-
-  void SetUp() override {}
-
-  void TearDown() override {}
-};
-
-TEST_F(AXPlatformUniqueIdTest, TestIdsUnique) {
+TEST(AXPlatformUniqueIdTest, IdsAreUnique) {
   AXUniqueId id1, id2;
   EXPECT_FALSE(id1 == id2);
   EXPECT_GT(id2.Get(), id1.Get());
@@ -35,33 +27,26 @@ class AXTestSmallBankUniqueId : public AXUniqueId {
 
 AXTestSmallBankUniqueId::AXTestSmallBankUniqueId() : AXUniqueId(kMaxId) {}
 
-TEST_F(AXPlatformUniqueIdTest, TestIdsNotReused) {
+TEST(AXPlatformUniqueIdTest, UnassignedIdsAreReused) {
   // Create a bank of ids that uses up all available ids.
   // Then remove an id and replace with a new one. Since it's the only
   // slot available, the id will end up having the same value, rather than
   // starting over at 1.
-  AXTestSmallBankUniqueId* ids[kMaxId];
+  std::unique_ptr<AXTestSmallBankUniqueId> ids[kMaxId];
 
   for (int i = 0; i < kMaxId; i++) {
-    ids[i] = new AXTestSmallBankUniqueId();
+    ids[i] = std::make_unique<AXTestSmallBankUniqueId>();
   }
 
   static int kIdToReplace = 10;
-
-  // IDs are 1-based.
-  EXPECT_EQ(ids[kIdToReplace]->Get(), kIdToReplace + 1);
+  int32_t expected_id = ids[kIdToReplace]->Get();
 
   // Delete one of the ids and replace with a new one.
-  delete ids[kIdToReplace];
-  ids[kIdToReplace] = new AXTestSmallBankUniqueId();
+  ids[kIdToReplace] = nullptr;
+  ids[kIdToReplace] = std::make_unique<AXTestSmallBankUniqueId>();
 
-  // IDs are 1-based.
-  EXPECT_EQ(ids[kIdToReplace]->Get(), kIdToReplace + 1);
-
-  // Clean up.
-  for (int i = 0; i < kMaxId; i++) {
-    delete ids[i];
-  }
+  // Expect that the original Id gets reused.
+  EXPECT_EQ(ids[kIdToReplace]->Get(), expected_id);
 }
 
 }  // namespace ui
