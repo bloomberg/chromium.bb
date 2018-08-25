9adf02e5f0ff040e3e4563f44a437ffbf286bc8a
Xiaohui Chen <xiaohuic@google.com>
2018-08-25 00:06:34 +0000

    Make assistant main view buttons focusable
    Create a utility method to unify the way we create image buttons. This
    turns on default ink drop click and focus styles and make them focusable.
    
    Test: manually build and test
    Bug: b:112925587
    Change-Id: I70c72e033fae2dbad39034d716483c77ea578161
    Reviewed-on: https://chromium-review.googlesource.com/1186137
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Muyuan Li <muyuanli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586068}
    Former-commit-id: 6c37d9bc2f0adb377b0cd46fc2c2c97246ba2c00

--- ash/assistant/ui/caption_bar.cc
+++ ash/assistant/ui/caption_bar.cc
@@ -7,10 +7,12 @@
 #include <memory>
 
 #include "ash/assistant/ui/assistant_ui_constants.h"
+#include "ash/assistant/util/views_util.h"
 #include "ash/public/cpp/vector_icons/vector_icons.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/paint_vector_icon.h"
+#include "ui/strings/grit/ui_strings.h"
 #include "ui/views/background.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/layout/box_layout.h"
@@ -27,27 +29,13 @@ constexpr int kVectorIconSizeDip = 12;
 
 // CaptionButton ---------------------------------------------------------------
 
-class CaptionButton : public views::ImageButton {
- public:
-  explicit CaptionButton(const gfx::VectorIcon& icon,
-                         views::ButtonListener* listener)
-      : views::ImageButton(listener) {
-    SetImage(
-        views::Button::ButtonState::STATE_NORMAL,
-        gfx::CreateVectorIcon(icon, kVectorIconSizeDip, gfx::kGoogleGrey700));
-    SetImageAlignment(ALIGN_CENTER, ALIGN_MIDDLE);
-  }
-
-  ~CaptionButton() override = default;
-
-  // views::View:
-  gfx::Size CalculatePreferredSize() const override {
-    return gfx::Size(kCaptionButtonSizeDip, kCaptionButtonSizeDip);
-  }
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(CaptionButton);
-};
+views::ImageButton* CreateCaptionButton(const gfx::VectorIcon& icon,
+                                        int accessible_name_id,
+                                        views::ButtonListener* listener) {
+  return assistant::util::CreateImageButton(
+      listener, icon, kCaptionButtonSizeDip, kVectorIconSizeDip,
+      accessible_name_id, gfx::kGoogleGrey700);
+}
 
 }  // namespace
 
@@ -77,7 +65,8 @@ void CaptionBar::InitLayout() {
       views::BoxLayout::CrossAxisAlignment::CROSS_AXIS_ALIGNMENT_CENTER);
 
   // Back.
-  CaptionButton* back_button = new CaptionButton(kWindowControlBackIcon, this);
+  auto* back_button =
+      CreateCaptionButton(kWindowControlBackIcon, IDS_APP_LIST_BACK, this);
   back_button->set_id(static_cast<int>(CaptionButtonId::kBack));
   AddChildView(back_button);
 
@@ -88,14 +77,14 @@ void CaptionBar::InitLayout() {
   layout_manager->SetFlexForView(spacer, 1);
 
   // Minimize.
-  CaptionButton* minimize_button =
-      new CaptionButton(kWindowControlMinimizeIcon, this);
+  auto* minimize_button = CreateCaptionButton(kWindowControlMinimizeIcon,
+                                              IDS_APP_ACCNAME_MINIMIZE, this);
   minimize_button->set_id(static_cast<int>(CaptionButtonId::kMinimize));
   AddChildView(minimize_button);
 
   // Close.
-  CaptionButton* close_button =
-      new CaptionButton(kWindowControlCloseIcon, this);
+  auto* close_button =
+      CreateCaptionButton(kWindowControlCloseIcon, IDS_APP_ACCNAME_CLOSE, this);
   close_button->set_id(static_cast<int>(CaptionButtonId::kClose));
   AddChildView(close_button);
 }
