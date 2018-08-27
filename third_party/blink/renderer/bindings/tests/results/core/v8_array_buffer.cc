3aa36cd5bc25746ddedfc27d4d7e5f761839c181
Stephan Herhut <herhut@chromium.org>
2018-08-27 11:37:04 +0000

    Start using the v8 deleter to free array buffers.
    Changes the v8 binding to propagate the custom v8 deleter for
    array buffers into blinks internal DataHandle object. Also, always
    use deleters, regardless of reservation mode.
    
    As a follow-up, reservation mode and corresponding data can now be
    removed.
    
    Bug: v8:8073
    Change-Id: I6db795d5a4f19ee0cea727a54d9085ff2ba5b5dc
    Reviewed-on: https://chromium-review.googlesource.com/1186333
    Commit-Queue: Stephan Herhut <herhut@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586232}
    Former-commit-id: 3493357087e41d3fcbcf8abf98e2cbd08b2d574f

--- third_party/blink/renderer/bindings/tests/results/core/v8_array_buffer.cc
+++ third_party/blink/renderer/bindings/tests/results/core/v8_array_buffer.cc
@@ -87,16 +87,13 @@ TestArrayBuffer* V8ArrayBuffer::ToImpl(v8::Local<v8::Object> object) {
     default:
       NOTREACHED();
   };
-  // TODO(v8:8073): Use specific deallocator per mode once v8 provides information.
   WTF::ArrayBufferContents::DataHandle data(v8Contents.AllocationBase(),
                                             v8Contents.AllocationLength(),
                                             v8Contents.Data(),
                                             v8Contents.ByteLength(),
                                             kind,
-                                            [](void* buffer, size_t length, void* alloc_data) {
-                                              WTF::ArrayBufferContents::FreeMemory(buffer);
-                                            },
-                                            nullptr);
+                                            v8Contents.Deleter(),
+                                            v8Contents.DeleterData());
   WTF::ArrayBufferContents contents(std::move(data), WTF::ArrayBufferContents::kNotShared);
   TestArrayBuffer* buffer = TestArrayBuffer::Create(contents);
   v8::Local<v8::Object> associatedWrapper = buffer->AssociateWithWrapper(v8::Isolate::GetCurrent(), buffer->GetWrapperTypeInfo(), object);
