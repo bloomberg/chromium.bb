1dfa4f8247ffe946bcdf7324f09490746ebca27e
Clark DuVall <cduvall@chromium.org>
2018-08-27 22:39:59 +0000

    Fix DeclarativeApiTest.PersistRules flaking on ToTLinux
    This test was flaking on ToTLinux:
    https://ci.chromium.org/buildbot/chromium.clang/ToTLinux/3448
    
    This reinitializes the URLLoaderFactory early so we don't have to wait
    for the round trip to the network service when resetting
    URLLoaderFactories for web request listeners. This is similar to the
    change made in http://crrev.com/c/1147072.
    
    Bug: 347159
    Change-Id: I2c6fbd43b587a9ea097b6a8078d7711c1c7ef695
    Reviewed-on: https://chromium-review.googlesource.com/1191134
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586457}
    Former-commit-id: 7f92da4c273371aa447667e1bba0d93ff4810f41

--- content/browser/storage_partition_impl.cc
+++ content/browser/storage_partition_impl.cc
@@ -1205,6 +1205,7 @@ void StoragePartitionImpl::Flush() {
 void StoragePartitionImpl::ResetURLLoaderFactories() {
   GetNetworkContext()->ResetURLLoaderFactories();
   url_loader_factory_for_browser_process_.reset();
+  url_loader_factory_getter_->Initialize(this);
 }
 
 void StoragePartitionImpl::ClearBluetoothAllowedDevicesMapForTesting() {
