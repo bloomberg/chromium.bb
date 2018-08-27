0c6fab839765ebc4570786cebdd2bc92727024a6
nednguyen <nednguyen@google.com>
2018-08-27 20:58:42 +0000

    Fix generate_perf_sharding to use a deterministic list of ordered benchmarks
    Previously, generate_perf_sharding use an undordered map of benchmarks to be
    shards. This CL fixes it by using a list datastructure and sort the benchmarks
    using the order of their names.
    
    In addition to that fix, this CL also refactor the binding between generate_perf_sharding
    and core.sharding_map_generator by moving all the commandline related logic
    from sharding_map_generator to generate_perf_sharding.
    
    Cq-Include-Trybots: master.tryserver.chromium.perf:obbs_fyi
    Change-Id: Id81eb4ef9a18bb716dca5754cdee876a4c5bf0c2
    
    TBR=perezju@chromium.org, eyaich@chromium.org
    
    Change-Id: Id81eb4ef9a18bb716dca5754cdee876a4c5bf0c2
    Reviewed-on: https://chromium-review.googlesource.com/1191403
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#586412}
    Former-commit-id: df8d7a0c1dff7258a5cd2180f42e6da7088319ae

--- tools/perf/core/sharding_map_generator_unittest.py
+++ tools/perf/core/sharding_map_generator_unittest.py
@@ -2,74 +2,68 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import collections
 import copy
 import json
 import os
-import tempfile
 import unittest
 
 from core import sharding_map_generator
 
-from py_utils import tempfile_ext
-
 
 class TestShardingMapGenerator(unittest.TestCase):
 
-  def _init_sample_timing_data(self, times):
-    timing_data = collections.OrderedDict()
-    all_stories = {}
+  def _generate_test_data(self, times):
+    timing_data = []
+    benchmarks_data = []
     for i, _ in enumerate(times):
-      all_stories['benchmark_' + str(i)] = []
+      b = {
+        'name': 'benchmark_' + str(i),
+        'stories': [],
+        'repeat': 1,
+      }
+      benchmarks_data.append(b)
       story_times = times[i]
       for j, _ in enumerate(story_times):
-        all_stories['benchmark_' + str(i)].append('story_' + str(j))
-        timing_data['benchmark_' + str(i) + '/' + 'story_' + str(j)] = (
-            story_times[j])
-    return timing_data, all_stories
+        benchmark_name = 'benchmark_' + str(i)
+        story_name = 'story_' + str(j)
+        b['stories'].append(story_name)
+        timing_data.append({
+           'name': benchmark_name + '/' + story_name,
+           'duration': story_times[j]
+        })
+    return benchmarks_data, timing_data
 
   def testGenerateAndTestShardingMap(self):
-    timing_data, all_stories = self._init_sample_timing_data(
+    benchmarks_data, timing_data, = self._generate_test_data(
         [[60, 56, 57], [66, 54, 80, 4], [2, 8, 7, 37, 2]])
     timing_data_for_testing = copy.deepcopy(timing_data)
     sharding_map = sharding_map_generator.generate_sharding_map(
-        timing_data, all_stories, 3, None)
-    fd_map, map_path = tempfile.mkstemp(suffix='.json')
-    try:
-      with os.fdopen(fd_map, 'w') as f:
-        json.dump(sharding_map, f)
-      results = sharding_map_generator.test_sharding_map(map_path,
-          timing_data_for_testing, all_stories)
-      self.assertEqual(results['0']['full_time'], 116)
-      self.assertEqual(results['1']['full_time'], 177)
-      self.assertEqual(results['2']['full_time'], 140)
-    finally:
-      os.remove(map_path)
+        benchmarks_data, timing_data, 3, None)
+    results = sharding_map_generator.test_sharding_map(sharding_map,
+        benchmarks_data, timing_data_for_testing)
+    self.assertEqual(results['0']['full_time'], 116)
+    self.assertEqual(results['1']['full_time'], 177)
+    self.assertEqual(results['2']['full_time'], 140)
 
   def testGeneratePerfSharding(self):
-    with tempfile_ext.NamedTemporaryDirectory() as temp_dir:
-      path_output = os.path.join(temp_dir, 'path_output')
-      path_results = os.path.join(temp_dir, 'path_results')
-      test_data_dir = os.path.join(os.path.dirname(__file__), 'test_data')
-      args = [
-          '--output-file', path_output,
-          '--timing-data', os.path.join(test_data_dir, 'test_timing_data.json'),
-          '--num-shards', '5',
-          '--test-data',
-          os.path.join(test_data_dir, 'test_timing_data_1_build.json'),
-          '--test-data-output', path_results
-      ]
+    test_data_dir = os.path.join(os.path.dirname(__file__), 'test_data')
+    with open(os.path.join(test_data_dir, 'benchmarks_to_shard.json')) as f:
+      benchmarks_to_shard = json.load(f)
 
-      with open(os.path.join(test_data_dir, 'benchmarks_data.json')) as f:
-        benchmark_data = json.load(f)
+    with open(os.path.join(test_data_dir, 'test_timing_data.json')) as f:
+      timing_data = json.load(f)
 
-      parser = sharding_map_generator.get_parser()
-      options = parser.parse_args(args)
-      sharding_map_generator.main(options, benchmark_data)
+    with open(
+        os.path.join(test_data_dir, 'test_timing_data_1_build.json')) as f:
+      timing_data_single_build = json.load(f)
 
-      with open(path_results, 'r') as test_results:
-        results = json.load(test_results)
-        shard_total_timing = []
-        for shard in results:
-          shard_total_timing.append(results[shard]['full_time'])
-        self.assertTrue(max(shard_total_timing) - min(shard_total_timing) < 400)
+    sharding_map = sharding_map_generator.generate_sharding_map(
+        benchmarks_to_shard, timing_data, num_shards=5, debug=False)
+
+    results = sharding_map_generator.test_sharding_map(
+        sharding_map, benchmarks_to_shard, timing_data_single_build)
+
+    shards_timing = []
+    for shard in results:
+      shards_timing.append(results[shard]['full_time'])
+    self.assertTrue(max(shards_timing) - min(shards_timing) < 300)
