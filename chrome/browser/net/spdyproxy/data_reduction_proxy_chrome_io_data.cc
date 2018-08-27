babb011fabd710f391486b15db2023ab124c4d7e
Robert Ogden <robertogden@chromium.org>
2018-08-27 20:18:34 +0000

    Try again to fix Previews Tab Helper crash
    Got one new crash over the weekend and the if (!web_contents()) check
    that I added was the line that crashed. Seems to me like |this| is a
    nullptr.
    
    Bug: 876835
    Change-Id: I8ee5f1ebf64a864a9d6fb4dc6e3018a6ef8eca83
    Reviewed-on: https://chromium-review.googlesource.com/1190844
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586389}
    Former-commit-id: 9944c0c120541f6bd84b97a9a296e15e532a97b1

--- chrome/browser/net/spdyproxy/data_reduction_proxy_chrome_io_data.cc
+++ chrome/browser/net/spdyproxy/data_reduction_proxy_chrome_io_data.cc
@@ -65,6 +65,9 @@ void OnLoFiResponseReceivedOnUI(content::WebContents* web_contents) {
   PreviewsUITabHelper* ui_tab_helper =
       PreviewsUITabHelper::FromWebContents(web_contents);
 
+  if (!ui_tab_helper)
+    return;
+
   uint64_t page_id = 0;
   if (ui_tab_helper && ui_tab_helper->previews_user_data()) {
     page_id = ui_tab_helper->previews_user_data()->page_id();
