e891dc751a2143f1f44577ba053000bcc70bf668
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2018-08-27 06:14:56 +0000

    Add a container view of MessageView to be used for swipe control.
    This change adds a new container view between MessageListView and
    MessageView. The new view is intended to host buttons views for swipe
    control.
    This change also returns slided notifications when another notification
    swipe is started, so as to ensure only one swipe control can be opened.
    
    Bug: 840497
    Change-Id: Ica92933a8d3f5d92caa43a5c926252681873ede1
    Reviewed-on: https://chromium-review.googlesource.com/1184724
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586198}
    Former-commit-id: 16ab50cc3829e7278a8e48ac293e2124f90f08ac

--- ui/message_center/views/message_view.h
+++ ui/message_center/views/message_view.h
@@ -42,6 +42,11 @@ class MESSAGE_CENTER_EXPORT MessageView : public views::InkDropHostView,
  public:
   static const char kViewClassName[];
 
+  class SlideObserver {
+   public:
+    virtual void OnSlideChanged(const std::string& notification_id) = 0;
+  };
+
   enum class Mode {
     // Normal mode.
     NORMAL = 0,
@@ -117,6 +122,8 @@ class MESSAGE_CENTER_EXPORT MessageView : public views::InkDropHostView,
   void OnSlideChanged() override;
   void OnSlideOut() override;
 
+  void AddSlideObserver(SlideObserver* observer);
+
   Mode GetMode() const;
 
   // Gets the current horizontal scroll offset of the view by slide gesture.
@@ -166,6 +173,7 @@ class MESSAGE_CENTER_EXPORT MessageView : public views::InkDropHostView,
   std::unique_ptr<views::Painter> focus_painter_;
 
   SlideOutController slide_out_controller_;
+  std::vector<SlideObserver*> slide_observers_;
 
   // True if |this| is embedded in another view. Equivalent to |!top_level| in
   // MessageViewFactory parlance.
