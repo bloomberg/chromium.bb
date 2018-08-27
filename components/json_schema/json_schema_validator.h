c5e9df0f69accb200377c1d02752d90a86756647
Alexander Hendrich <hendrich@chromium.org>
2018-08-27 16:25:25 +0000

    Remove value validation from json_schema_validator.h/.cc
    This CL is part of a larger clean-up operation.
    The schema validation from json_schema_validator.h/cc is only used in
    schema.h/cc and will be merged there in a follow-up CL.
    The value validation in json_schema_validator.h/cc is unused since
    we use the value validation from schema.h/cc instead.
    
    Bug: 873641
    Change-Id: Ibe45ac4b4c5911dfb852c6f24fbd7023ebe10ce6
    Reviewed-on: https://chromium-review.googlesource.com/1181137
    Reviewed-by: Lutz Justen <ljusten@chromium.org>
    Commit-Queue: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586280}
    Former-commit-id: 18593a3a29a6a2cb5ce172c61f288e54cd2691a5

--- components/json_schema/json_schema_validator.h
+++ components/json_schema/json_schema_validator.h
@@ -14,8 +14,6 @@
 
 namespace base {
 class DictionaryValue;
-class ListValue;
-class Value;
 }
 
 //==============================================================================
@@ -55,56 +53,11 @@ class Value;
 //==============================================================================
 class JSONSchemaValidator {
  public:
-  // Details about a validation error.
-  struct Error {
-    Error();
-
-    explicit Error(const std::string& message);
-
-    Error(const std::string& path, const std::string& message);
-
-    // The path to the location of the error in the JSON structure.
-    std::string path;
-
-    // An english message describing the error.
-    std::string message;
-  };
-
   enum Options {
     // Ignore unknown attributes. If this option is not set then unknown
     // attributes will make the schema validation fail.
     OPTIONS_IGNORE_UNKNOWN_ATTRIBUTES = 1 << 0,
   };
-
-  // Error messages.
-  static const char kUnknownTypeReference[];
-  static const char kInvalidChoice[];
-  static const char kInvalidEnum[];
-  static const char kObjectPropertyIsRequired[];
-  static const char kUnexpectedProperty[];
-  static const char kArrayMinItems[];
-  static const char kArrayMaxItems[];
-  static const char kArrayItemRequired[];
-  static const char kStringMinLength[];
-  static const char kStringMaxLength[];
-  static const char kStringPattern[];
-  static const char kNumberMinimum[];
-  static const char kNumberMaximum[];
-  static const char kInvalidType[];
-  static const char kInvalidTypeIntegerNumber[];
-  static const char kInvalidRegex[];
-
-  // Classifies a Value as one of the JSON schema primitive types.
-  static std::string GetJSONSchemaType(const base::Value* value);
-
-  // Utility methods to format error messages. The first method can have one
-  // wildcard represented by '*', which is replaced with s1. The second method
-  // can have two, which are replaced by s1 and s2.
-  static std::string FormatErrorMessage(const std::string& format,
-                                        const std::string& s1);
-  static std::string FormatErrorMessage(const std::string& format,
-                                        const std::string& s1,
-                                        const std::string& s2);
 
   // Verifies if |schema| is a valid JSON v3 schema. When this validation passes
   // then |schema| is valid JSON that can be parsed into a DictionaryValue,
@@ -123,127 +76,6 @@ class JSONSchemaValidator {
   // Options above.
   static std::unique_ptr<base::DictionaryValue>
   IsValidSchema(const std::string& schema, int options, std::string* error);
-
-  // Creates a validator for the specified schema.
-  //
-  // NOTE: This constructor assumes that |schema| is well formed and valid.
-  // Errors will result in CHECK at runtime; this constructor should not be used
-  // with untrusted schemas.
-  explicit JSONSchemaValidator(base::DictionaryValue* schema);
-
-  // Creates a validator for the specified schema and user-defined types. Each
-  // type must be a valid JSONSchema type description with an additional "id"
-  // field. Schema objects in |schema| can refer to these types with the "$ref"
-  // property.
-  //
-  // NOTE: This constructor assumes that |schema| and |types| are well-formed
-  // and valid. Errors will result in CHECK at runtime; this constructor should
-  // not be used with untrusted schemas.
-  JSONSchemaValidator(base::DictionaryValue* schema, base::ListValue* types);
-
-  ~JSONSchemaValidator();
-
-  // Whether the validator allows additional items for objects and lists, beyond
-  // those defined by their schema, by default.
-  //
-  // This setting defaults to false: all items in an instance list or object
-  // must be defined by the corresponding schema.
-  //
-  // This setting can be overridden on individual object and list schemas by
-  // setting the "additionalProperties" field.
-  bool default_allow_additional_properties() const {
-    return default_allow_additional_properties_;
-  }
-
-  void set_default_allow_additional_properties(bool val) {
-    default_allow_additional_properties_ = val;
-  }
-
-  // Returns any errors from the last call to to Validate().
-  const std::vector<Error>& errors() const {
-    return errors_;
-  }
-
-  // Validates a JSON value. Returns true if the instance is valid, false
-  // otherwise. If false is returned any errors are available from the errors()
-  // getter.
-  bool Validate(const base::Value* instance);
-
- private:
-  typedef std::map<std::string, const base::DictionaryValue*> TypeMap;
-
-  // Each of the below methods handle a subset of the validation process. The
-  // path paramater is the path to |instance| from the root of the instance tree
-  // and is used in error messages.
-
-  // Validates any instance node against any schema node. This is called for
-  // every node in the instance tree, and it just decides which of the more
-  // detailed methods to call.
-  void Validate(const base::Value* instance,
-                const base::DictionaryValue* schema,
-                const std::string& path);
-
-  // Validates a node against a list of possible schemas. If any one of the
-  // schemas match, the node is valid.
-  void ValidateChoices(const base::Value* instance,
-                       const base::ListValue* choices,
-                       const std::string& path);
-
-  // Validates a node against a list of exact primitive values, eg 42, "foobar".
-  void ValidateEnum(const base::Value* instance,
-                    const base::ListValue* choices,
-                    const std::string& path);
-
-  // Validates a JSON object against an object schema node.
-  void ValidateObject(const base::DictionaryValue* instance,
-                      const base::DictionaryValue* schema,
-                      const std::string& path);
-
-  // Validates a JSON array against an array schema node.
-  void ValidateArray(const base::ListValue* instance,
-                     const base::DictionaryValue* schema,
-                     const std::string& path);
-
-  // Validates a JSON array against an array schema node configured to be a
-  // tuple. In a tuple, there is one schema node for each item expected in the
-  // array.
-  void ValidateTuple(const base::ListValue* instance,
-                     const base::DictionaryValue* schema,
-                     const std::string& path);
-
-  // Validate a JSON string against a string schema node.
-  void ValidateString(const base::Value* instance,
-                      const base::DictionaryValue* schema,
-                      const std::string& path);
-
-  // Validate a JSON number against a number schema node.
-  void ValidateNumber(const base::Value* instance,
-                      const base::DictionaryValue* schema,
-                      const std::string& path);
-
-  // Validates that the JSON node |instance| has |expected_type|.
-  bool ValidateType(const base::Value* instance,
-                    const std::string& expected_type,
-                    const std::string& path);
-
-  // Returns true if |schema| will allow additional items of any type.
-  bool SchemaAllowsAnyAdditionalItems(
-      const base::DictionaryValue* schema,
-      const base::DictionaryValue** addition_items_schema);
-
-  // The root schema node.
-  base::DictionaryValue* schema_root_;
-
-  // Map of user-defined name to type.
-  TypeMap types_;
-
-  // Whether we allow additional properties on objects by default. This can be
-  // overridden by the allow_additional_properties flag on an Object schema.
-  bool default_allow_additional_properties_;
-
-  // Errors accumulated since the last call to Validate().
-  std::vector<Error> errors_;
-
 
   DISALLOW_COPY_AND_ASSIGN(JSONSchemaValidator);
 };
