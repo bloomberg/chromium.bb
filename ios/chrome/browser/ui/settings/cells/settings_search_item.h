e6a8944f07a349c7e74d943ddb6cb1c0bd29c6e5
David Jean <djean@google.com>
2018-08-28 13:41:17 +0000

    [ios] Added a search cell to settings catalog
    On the way to adding saved passwords search in settings.
    
    Bug: 826094
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-full-configs;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I5fcbd2dcbe7ea0378b963d1948c782499f691dae
    Reviewed-on: https://chromium-review.googlesource.com/1186413
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586677}
    Former-commit-id: 6699fd7e7d593041b0120bfe7fdc43f389cc9767

---
+++ ios/chrome/browser/ui/settings/cells/settings_search_item.h
@@ -0,0 +1,43 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_SETTINGS_CELLS_SETTINGS_SEARCH_ITEM_H_
+#define IOS_CHROME_BROWSER_UI_SETTINGS_CELLS_SETTINGS_SEARCH_ITEM_H_
+
+#import <UIKit/UIKit.h>
+
+#import "ios/chrome/browser/ui/collection_view/cells/collection_view_item.h"
+#import "ios/third_party/material_components_ios/src/components/CollectionCells/src/MaterialCollectionCells.h"
+
+// Delegate for SettingsSearchItem. Used to pass on search interactions.
+@protocol SettingsSearchItemDelegate<NSObject>
+
+// Called when the search term changes.
+- (void)didRequestSearchForTerm:(NSString*)searchTerm;
+
+@end
+
+// View for displaying a search.
+@interface SettingsSearchItem : CollectionViewItem
+
+// Delegate for forwarding interactions with the search item.
+@property(nonatomic, weak) id<SettingsSearchItemDelegate> delegate;
+
+// The placeholder for the search input field.
+@property(nonatomic, copy) NSString* placeholder;
+
+@end
+
+// Cell representation for SettingsSearchItem.
+@interface SettingsSearchCell : MDCCollectionViewCell
+
+// Text field for the search view.
+@property(nonatomic, strong) UITextField* textField;
+
+// Delegate for forwarding interactions with the search item.
+@property(nonatomic, weak) id<SettingsSearchItemDelegate> delegate;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_SETTINGS_CELLS_SETTINGS_SEARCH_ITEM_H_
