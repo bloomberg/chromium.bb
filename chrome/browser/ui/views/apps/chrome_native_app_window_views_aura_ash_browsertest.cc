1106bbfb6a83255eacbd885c38133105882c0218
Trent Apted <tapted@chromium.org>
2018-08-28 02:36:39 +0000

    Revert "Get Chrome App frame immersive mode closer to working in Mash"
    This reverts commit d2b851e41e915ba398861b5f20545b5282bbab11 [formerly 58a77e6664aaaca0bfd9c2f01106f2474ea02092].
    
    Reason for revert: Suspected for asan failures in exo_unittests starting
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/28766
    
    errors like 
    
    ==346==ERROR: AddressSanitizer: heap-use-after-free on address 0x616000049d48 at pc 0x000007341794 bp 0x7ffc028ccfb0 sp 0x7ffc028ccfa8
    READ of size 8 at 0x616000049d48 thread T0
        #0 0x7341793 in begin buildtools/third_party/libc++/trunk/include/vector:1471:30
        #1 0x7341793 in base::ObserverList<views::ViewObserver, false, true, base::internal::UncheckedObserverAdapter>::RemoveObserver(views::ViewObserver const*) base/observer_list.h:272
        #2 0x30ed227 in EnableWindowObservers ash/public/cpp/immersive/immersive_fullscreen_controller.cc:356:21
        #3 0x30ed227 in ash::ImmersiveFullscreenController::Init(ash::ImmersiveFullscreenControllerDelegate*, views::Widget*, views::View*) ash/public/cpp/immersive/immersive_fullscreen_controller.cc:96
        #4 0x7e8968 in exo::ClientControlledShellSurface::UpdateFrame() components/exo/client_controlled_shell_surface.cc:960:23
    
    
    Original change's description:
    > Get Chrome App frame immersive mode closer to working in Mash
    > 
    > Removes the ImmersiveFullscreenController from
    > ChromeNativeAppWindowViewsAuraAsh and relies on the default one provided
    > by Ash. Also:
    > 
    > 1. Remove the WindowStateDelegate from ChromeNativeAppWindowViewsAuraAsh
    >    - Listen for fullscreen changes in order to keep system fullscreen
    >      state and AppWindow fullscreen state in sync.
    >    - RestoreAlwaysOnTop should just work without the special delegate
    >      implementation, as the taskbar handling in AppWindow is Windows-
    >      specific. Tested by
    >      WorkspaceLayoutManagerSoloTest.FullscreenSuspendsAlwaysOnTop
    >    - Fold non-delegate functionality into main class
    > 
    > 2. Remove the default Ash WindowStateDelegate (i.e. that created in
    >    non_client_frame_view_ash.cc) which only overrode ToggleFullscreen.
    >    The default implementation, ::wm::SetWindowFullscreen, works as
    >    intended.
    > 
    > 3. Add window properties to allow the client to enter/exit immersive mode,
    >    get the position of the reveal view, and disable some ways of automatically
    >    triggering immersive.
    > 
    > TODO: propagate window properties across processes so that they work in OopAsh
    > TODO: In OopAsh, listen for window state changes into/out of fullscreen. (This
    >       will replace the functionality that the frame gets from
    >       WindowStateObserver::OnPostWindowStateTypeChange in classic Ash).
    > 
    > Change-Id: I202942a146d96bee92e5997a922df1da361cb1a3
    > Reviewed-on: https://chromium-review.googlesource.com/1176237
    > Commit-Queue: Evan Stade <estade@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#586459}
    
    TBR=sky@chromium.org,estade@chromium.org
    
    Change-Id: I05c1d5daaae64a78492ea5eff17edb9c7b48c16c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/1192366
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586552}
    Former-commit-id: a6b39a98a69432d411926d9a68126dc9236ae63f

--- chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash_browsertest.cc
+++ chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash_browsertest.cc
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 #include "ash/public/cpp/immersive/immersive_fullscreen_controller.h"
-#include "ash/public/cpp/window_properties.h"
 #include "chrome/browser/apps/platform_apps/app_browsertest_util.h"
 #include "chrome/browser/ui/ash/tablet_mode_client.h"
 #include "chrome/browser/ui/ash/tablet_mode_client_test_util.h"
@@ -11,7 +10,6 @@
 #include "chromeos/login/login_state.h"
 #include "chromeos/login/scoped_test_public_session_login_state.h"
 #include "extensions/browser/app_window/app_window.h"
-#include "ui/aura/window.h"
 #include "ui/base/ui_base_types.h"
 #include "ui/wm/core/window_util.h"
 
@@ -21,21 +19,6 @@ class ChromeNativeAppWindowViewsAuraAshBrowserTest
   ChromeNativeAppWindowViewsAuraAshBrowserTest() = default;
   ~ChromeNativeAppWindowViewsAuraAshBrowserTest() override = default;
 
- protected:
-  void InitWindow() { app_window_ = CreateTestAppWindow("{}"); }
-
-  bool IsImmersiveActive() {
-    return window()->widget()->GetNativeWindow()->GetProperty(
-        ash::kImmersiveIsActive);
-  }
-
-  ChromeNativeAppWindowViewsAuraAsh* window() {
-    return static_cast<ChromeNativeAppWindowViewsAuraAsh*>(
-        GetNativeAppWindowForAppWindow(app_window_));
-  }
-
-  extensions::AppWindow* app_window_ = nullptr;
-
  private:
   DISALLOW_COPY_AND_ASSIGN(ChromeNativeAppWindowViewsAuraAshBrowserTest);
 };
@@ -43,102 +26,111 @@ class ChromeNativeAppWindowViewsAuraAshBrowserTest
 // Verify that immersive mode is enabled or disabled as expected.
 IN_PROC_BROWSER_TEST_F(ChromeNativeAppWindowViewsAuraAshBrowserTest,
                        ImmersiveWorkFlow) {
-  InitWindow();
-  ASSERT_TRUE(window());
-  EXPECT_FALSE(IsImmersiveActive());
+  extensions::AppWindow* app_window = CreateTestAppWindow("{}");
+  auto* window = static_cast<ChromeNativeAppWindowViewsAuraAsh*>(
+      GetNativeAppWindowForAppWindow(app_window));
+  ASSERT_TRUE(window != nullptr);
+  ASSERT_TRUE(window->immersive_fullscreen_controller_.get() != nullptr);
+  EXPECT_FALSE(window->immersive_fullscreen_controller_->IsEnabled());
   constexpr int kFrameHeight = 32;
 
   views::ClientView* client_view =
-      window()->widget()->non_client_view()->client_view();
+      window->widget()->non_client_view()->client_view();
   EXPECT_EQ(kFrameHeight, client_view->bounds().y());
 
   // Verify that when fullscreen is toggled on, immersive mode is enabled and
   // that when fullscreen is toggled off, immersive mode is disabled.
-  app_window_->OSFullscreen();
-  EXPECT_TRUE(IsImmersiveActive());
+  app_window->OSFullscreen();
+  EXPECT_TRUE(window->immersive_fullscreen_controller_->IsEnabled());
   EXPECT_EQ(0, client_view->bounds().y());
 
-  app_window_->Restore();
-  EXPECT_FALSE(IsImmersiveActive());
+  app_window->Restore();
+  EXPECT_FALSE(window->immersive_fullscreen_controller_->IsEnabled());
   EXPECT_EQ(kFrameHeight, client_view->bounds().y());
 
   // Verify that since the auto hide title bars in tablet mode feature turned
   // on, immersive mode is enabled once tablet mode is entered, and disabled
   // once tablet mode is exited.
   ASSERT_NO_FATAL_FAILURE(test::SetAndWaitForTabletMode(true));
-  EXPECT_TRUE(IsImmersiveActive());
+  EXPECT_TRUE(window->immersive_fullscreen_controller_->IsEnabled());
   EXPECT_EQ(0, client_view->bounds().y());
 
   ASSERT_NO_FATAL_FAILURE(test::SetAndWaitForTabletMode(false));
-  EXPECT_FALSE(IsImmersiveActive());
+  EXPECT_FALSE(window->immersive_fullscreen_controller_->IsEnabled());
   EXPECT_EQ(kFrameHeight, client_view->bounds().y());
 
   // Verify that the window was fullscreened before entering tablet mode, it
   // will remain fullscreened after exiting tablet mode.
-  app_window_->OSFullscreen();
-  EXPECT_TRUE(IsImmersiveActive());
+  app_window->OSFullscreen();
+  EXPECT_TRUE(window->immersive_fullscreen_controller_->IsEnabled());
   ASSERT_NO_FATAL_FAILURE(test::SetAndWaitForTabletMode(true));
-  EXPECT_TRUE(IsImmersiveActive());
+  EXPECT_TRUE(window->immersive_fullscreen_controller_->IsEnabled());
   ASSERT_NO_FATAL_FAILURE(test::SetAndWaitForTabletMode(false));
-  EXPECT_TRUE(IsImmersiveActive());
-  app_window_->Restore();
+  EXPECT_TRUE(window->immersive_fullscreen_controller_->IsEnabled());
+  app_window->Restore();
 
   // Verify that minimized windows do not have immersive mode enabled.
-  app_window_->Minimize();
-  EXPECT_FALSE(IsImmersiveActive());
+  app_window->Minimize();
+  EXPECT_FALSE(window->immersive_fullscreen_controller_->IsEnabled());
   ASSERT_NO_FATAL_FAILURE(test::SetAndWaitForTabletMode(true));
-  EXPECT_FALSE(IsImmersiveActive());
-  window()->Show();
-  EXPECT_TRUE(IsImmersiveActive());
-  app_window_->Minimize();
-  EXPECT_FALSE(IsImmersiveActive());
+  EXPECT_FALSE(window->immersive_fullscreen_controller_->IsEnabled());
+  window->Show();
+  EXPECT_TRUE(window->immersive_fullscreen_controller_->IsEnabled());
+  app_window->Minimize();
+  EXPECT_FALSE(window->immersive_fullscreen_controller_->IsEnabled());
   ASSERT_NO_FATAL_FAILURE(test::SetAndWaitForTabletMode(false));
-  EXPECT_FALSE(IsImmersiveActive());
+  EXPECT_FALSE(window->immersive_fullscreen_controller_->IsEnabled());
 
   // Verify that activation change should not change the immersive
   // state.
-  window()->Show();
-  app_window_->OSFullscreen();
-  EXPECT_TRUE(IsImmersiveActive());
-  ::wm::DeactivateWindow(window()->GetNativeWindow());
-  EXPECT_TRUE(IsImmersiveActive());
-  ::wm::ActivateWindow(window()->GetNativeWindow());
-  EXPECT_TRUE(IsImmersiveActive());
+  window->Show();
+  app_window->OSFullscreen();
+  EXPECT_TRUE(window->immersive_fullscreen_controller_->IsEnabled());
+  ::wm::DeactivateWindow(window->GetNativeWindow());
+  EXPECT_TRUE(window->immersive_fullscreen_controller_->IsEnabled());
+  ::wm::ActivateWindow(window->GetNativeWindow());
+  EXPECT_TRUE(window->immersive_fullscreen_controller_->IsEnabled());
 
-  CloseAppWindow(app_window_);
+  CloseAppWindow(app_window);
 }
 
 // Verifies that apps in immersive fullscreen will have a restore state of
 // maximized.
 IN_PROC_BROWSER_TEST_F(ChromeNativeAppWindowViewsAuraAshBrowserTest,
                        ImmersiveModeFullscreenRestoreType) {
-  InitWindow();
-  ASSERT_TRUE(window());
+  extensions::AppWindow* app_window = CreateTestAppWindow("{}");
+  auto* window = static_cast<ChromeNativeAppWindowViewsAuraAsh*>(
+      GetNativeAppWindowForAppWindow(app_window));
+  ASSERT_TRUE(window != nullptr);
+  ASSERT_TRUE(window->immersive_fullscreen_controller_.get() != nullptr);
 
-  app_window_->OSFullscreen();
-  EXPECT_EQ(ui::SHOW_STATE_MAXIMIZED, window()->GetRestoredState());
+  app_window->OSFullscreen();
+  EXPECT_EQ(ui::SHOW_STATE_MAXIMIZED, window->GetRestoredState());
   ASSERT_NO_FATAL_FAILURE(test::SetAndWaitForTabletMode(true));
-  EXPECT_TRUE(window()->IsFullscreen());
-  EXPECT_EQ(ui::SHOW_STATE_MAXIMIZED, window()->GetRestoredState());
+  EXPECT_TRUE(window->IsFullscreen());
+  EXPECT_EQ(ui::SHOW_STATE_MAXIMIZED, window->GetRestoredState());
   ASSERT_NO_FATAL_FAILURE(test::SetAndWaitForTabletMode(false));
-  EXPECT_EQ(ui::SHOW_STATE_MAXIMIZED, window()->GetRestoredState());
+  EXPECT_EQ(ui::SHOW_STATE_MAXIMIZED, window->GetRestoredState());
 
-  CloseAppWindow(app_window_);
+  CloseAppWindow(app_window);
 }
 
 // Verify that immersive mode stays disabled when entering tablet mode in
 // forced fullscreen mode (e.g. when running in a kiosk session).
 IN_PROC_BROWSER_TEST_F(ChromeNativeAppWindowViewsAuraAshBrowserTest,
                        NoImmersiveModeWhenForcedFullscreen) {
-  InitWindow();
-  ASSERT_TRUE(window());
+  extensions::AppWindow* app_window = CreateTestAppWindow("{}");
+  auto* window = static_cast<ChromeNativeAppWindowViewsAuraAsh*>(
+      GetNativeAppWindowForAppWindow(app_window));
+  ASSERT_TRUE(window != nullptr);
+  ASSERT_TRUE(window->immersive_fullscreen_controller_.get() != nullptr);
 
-  app_window_->ForcedFullscreen();
+  app_window->ForcedFullscreen();
 
   ASSERT_NO_FATAL_FAILURE(test::SetAndWaitForTabletMode(true));
-  EXPECT_FALSE(IsImmersiveActive());
+  EXPECT_FALSE(window->immersive_fullscreen_controller_->IsEnabled());
   ASSERT_NO_FATAL_FAILURE(test::SetAndWaitForTabletMode(false));
-  EXPECT_FALSE(IsImmersiveActive());
+  EXPECT_FALSE(window->immersive_fullscreen_controller_->IsEnabled());
 }
 
 // Make sure a normal window is not in immersive mode, and uses
@@ -147,29 +139,35 @@ IN_PROC_BROWSER_TEST_F(ChromeNativeAppWindowViewsAuraAshBrowserTest,
                        PublicSessionImmersiveMode) {
   chromeos::ScopedTestPublicSessionLoginState login_state;
 
-  InitWindow();
-  ASSERT_TRUE(window());
-  EXPECT_FALSE(IsImmersiveActive());
+  extensions::AppWindow* app_window = CreateTestAppWindow("{}");
+  auto* window = static_cast<ChromeNativeAppWindowViewsAuraAsh*>(
+      GetNativeAppWindowForAppWindow(app_window));
+  ASSERT_TRUE(window != nullptr);
+  ASSERT_TRUE(window->immersive_fullscreen_controller_.get() != nullptr);
+  EXPECT_FALSE(window->immersive_fullscreen_controller_->IsEnabled());
 
-  app_window_->SetFullscreen(extensions::AppWindow::FULLSCREEN_TYPE_HTML_API,
-                             true);
+  app_window->SetFullscreen(extensions::AppWindow::FULLSCREEN_TYPE_HTML_API,
+                            true);
 
-  EXPECT_TRUE(IsImmersiveActive());
+  EXPECT_TRUE(window->immersive_fullscreen_controller_->IsEnabled());
 }
 
 // Verifies that apps in immersive fullscreen will correctly exit
 // immersive mode when Restore is called.
 IN_PROC_BROWSER_TEST_F(ChromeNativeAppWindowViewsAuraAshBrowserTest,
                        RestoreImmersiveMode) {
-  InitWindow();
-  ASSERT_TRUE(window());
+  extensions::AppWindow* app_window = CreateTestAppWindow("{}");
+  auto* window = static_cast<ChromeNativeAppWindowViewsAuraAsh*>(
+      GetNativeAppWindowForAppWindow(app_window));
+  ASSERT_TRUE(window != nullptr);
+  ASSERT_TRUE(window->immersive_fullscreen_controller_.get());
 
-  app_window_->OSFullscreen();
-  EXPECT_EQ(ui::SHOW_STATE_MAXIMIZED, window()->GetRestoredState());
-  EXPECT_TRUE(window()->IsFullscreen());
+  app_window->OSFullscreen();
+  EXPECT_EQ(ui::SHOW_STATE_MAXIMIZED, window->GetRestoredState());
+  EXPECT_TRUE(window->IsFullscreen());
 
-  window()->Restore();
-  EXPECT_FALSE(IsImmersiveActive());
+  window->Restore();
+  ASSERT_FALSE(window->immersive_fullscreen_controller_->IsEnabled());
 
-  CloseAppWindow(app_window_);
+  CloseAppWindow(app_window);
 }
