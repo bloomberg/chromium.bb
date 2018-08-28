8f9be58bf9d12996a3ca608253ced673960a2d50
François Beaufort <beaufort.francois@gmail.com>
2018-08-28 13:44:52 +0000

    Fix ASan WidgetTest.WidgetDestroyedItselfDoesNotCrash crash.
    This makes sure WidgetTest.WidgetDestroyedItselfDoesNotCrash test does NOT
    crash with use-after-free ASan error in ChromeOS and Mac. Test was added in
    https://chromium-review.googlesource.com/c/chromium/src/+/1145261/ and only
    covered Windows and Linux.
    
    Bug: 878314
    
    Change-Id: Ifde7f402955bd34bfd0618e4f67c52feeb333624
    Reviewed-on: https://chromium-review.googlesource.com/1193283
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586678}
    Former-commit-id: e34ae84a36ab96282f2c99a801d185606d6faf67

--- ui/views/widget/native_widget_mac.mm
+++ ui/views/widget/native_widget_mac.mm
@@ -114,8 +114,12 @@ void NativeWidgetMac::WindowDestroying() {
 void NativeWidgetMac::WindowDestroyed() {
   DCHECK(bridge());
   bridge_host_.reset();
+  // |OnNativeWidgetDestroyed| may delete |this| if the object does not own
+  // itself.
+  bool should_delete_this =
+      (ownership_ == Widget::InitParams::NATIVE_WIDGET_OWNS_WIDGET);
   delegate_->OnNativeWidgetDestroyed();
-  if (ownership_ == Widget::InitParams::NATIVE_WIDGET_OWNS_WIDGET)
+  if (should_delete_this)
     delete this;
 }
 
