24b52fd4e77f3c9ef7bc088dbed2d378472200f8
Orsi Batiz <orsibatiz@google.com>
2018-08-28 09:28:47 +0000

    TrustedTypes: Added TrustedTypes.getPolicyNames & minor changes
    Changed window.trustedTypes to window.TrustedTypes.
    Added TrustedTypes.getPolicyNames.
    Throw exception for TrustedTypes.createPolicy with non-unique policy name.
    Adapted tests to match.
    Most existing applications are synchronous, createPolicy was made synchronous.
    Changed tests from async_test to test, because of change mentioned above.
    
    Bug: 739170
    Change-Id: Ie4e2702994a8674e65376b7bf4b42d4c41e9cec4
    Reviewed-on: https://chromium-review.googlesource.com/1181362
    Commit-Queue: Orsolya Bernadett Batiz <orsibatiz@google.com>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586630}
    Former-commit-id: 136008de5d2e0b2e1913bcbcbd641b2bd4158bee

--- third_party/blink/renderer/core/trustedtypes/trusted_type_policy_factory.cc
+++ third_party/blink/renderer/core/trustedtypes/trusted_type_policy_factory.cc
@@ -4,28 +4,44 @@
 
 #include "third_party/blink/renderer/core/trustedtypes/trusted_type_policy_factory.h"
 
-#include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/core/trustedtypes/trusted_type_policy.h"
-#include "third_party/blink/renderer/platform/bindings/to_v8.h"
+#include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 
-ScriptPromise TrustedTypePolicyFactory::createPolicy(
-    ScriptState* script_state,
+TrustedTypePolicy* TrustedTypePolicyFactory::createPolicy(
     const String& policy_name,
-    const TrustedTypePolicyOptions& policy_options) {
+    const TrustedTypePolicyOptions& policy_options,
+    ExceptionState& exception_state) {
+  for (const TrustedTypePolicy* p : policies_) {
+    if (p->name() == policy_name) {
+      exception_state.ThrowTypeError("Policy with name " + policy_name +
+                                     " already exists.");
+      return nullptr;
+    }
+  }
   TrustedTypePolicy* policy =
       TrustedTypePolicy::Create(policy_name, policy_options);
-  return ScriptPromise::Cast(script_state, ToV8(policy, script_state));
+  policies_.push_back(policy);
+  return policy;
 }
 
 TrustedTypePolicyFactory::TrustedTypePolicyFactory(LocalFrame* frame)
     : DOMWindowClient(frame) {}
 
+Vector<String> TrustedTypePolicyFactory::getPolicyNames() const {
+  Vector<String> policyNames;
+  for (const TrustedTypePolicy* p : policies_) {
+    policyNames.push_back(p->name());
+  }
+  return policyNames;
+}
+
 void TrustedTypePolicyFactory::Trace(blink::Visitor* visitor) {
   ScriptWrappable::Trace(visitor);
   DOMWindowClient::Trace(visitor);
+  visitor->Trace(policies_);
 }
 
 }  // namespace blink
