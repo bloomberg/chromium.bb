48aa8b29d0e6b0c1fcb94f379edc4914ef3b0c5e
Ken Buchanan <kenrb@chromium.org>
2018-08-28 23:05:52 +0000

    Start rendering timer after first navigation
    Currently the new content rendering timer in the browser process,
    which clears an old page's contents 4 seconds after a navigation if the
    new page doesn't draw in that time, is not set on the first navigation
    for a top-level frame.
    
    This is problematic because content can exist before the first
    navigation, for instance if it was created by a javascript: URL.
    
    This CL removes the code that skips the timer activation on the first
    navigation.
    
    Bug: 844881
    Change-Id: I19b3ad1ff62c69ded3a5f7b1c0afde191aaf4584
    Reviewed-on: https://chromium-review.googlesource.com/1188589
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586913}
    Former-commit-id: 7da6c3419fd172405bcece1ae4ec6ec8316cd345

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -1196,19 +1196,8 @@ void RenderWidgetHostImpl::DidNavigate(uint32_t next_source_id) {
     // |visual_properties_ack_pending_| and make sure the next resize will be
     // acked if the last resize before navigation was supposed to be acked.
     visual_properties_ack_pending_ = false;
-    viz::LocalSurfaceId old_surface_id = view_->GetLocalSurfaceId();
     if (view_)
       view_->DidNavigate();
-    viz::LocalSurfaceId new_surface_id = view_->GetLocalSurfaceId();
-    // If |view_| didn't allocate a new surface id, then don't start
-    // |new_content_rendering_timeout_|. Two reasons:
-    //  1. It's not needed (because this was the first navigation event)
-    //  2. If we don't change the surface id, then we will not get the call to
-    //     OnFirstSurfaceActivation, and not stop the timer (even if we get new
-    //     frames).
-    // https://crbug.com/853651, https://crbug.com/535375
-    if (old_surface_id == new_surface_id)
-      return;
   } else {
     // It is possible for a compositor frame to arrive before the browser is
     // notified about the page being committed, in which case no timer is
