4b518cab21e6e0959d251fdefb0c008571903686
John Abd-El-Malek <jam@chromium.org>
2018-08-28 22:58:59 +0000

    Add some logging in crash dumps to track down bad referrer with network service.
    Also make the AppCache code update the referrer policy. This matches the other flows (SharedWorkerScriptLoaderfor Shared Worker and URLLoaderRequestController for navigation). I don't think this is the cause, but seems like it should be consistent.
    
    Bug: 878417
    
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I45c07d66b1dd06c6e4937de864c00b7ec647d939
    Reviewed-on: https://chromium-review.googlesource.com/1194326
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586907}
    Former-commit-id: 589b0c3b15a46f102cf273e8e1877a0ad0008600

--- content/browser/appcache/appcache_url_loader_request.cc
+++ content/browser/appcache/appcache_url_loader_request.cc
@@ -84,6 +84,7 @@ void AppCacheURLLoaderRequest::UpdateWithRedirectInfo(
   request_.url = redirect_info.new_url;
   request_.method = redirect_info.new_method;
   request_.referrer = GURL(redirect_info.new_referrer);
+  request_.referrer_policy = redirect_info.new_referrer_policy;
   request_.site_for_cookies = redirect_info.new_site_for_cookies;
 }
 
