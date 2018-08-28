78e462abbbf9a375a889531eb123b402a2b89939
Eric Roman <eroman@chromium.org>
2018-08-28 22:53:41 +0000

    Enable ProxySettingsApiTest.ProxyEventsInvalidProxy for ChromeOS.
    Change-Id: I9351cd28ed8b329c12c278c4d38770c5e81a90d1
    Reviewed-on: https://chromium-review.googlesource.com/1192325
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Eric Roman <eroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586904}
    Former-commit-id: 3987278a6786cc0570cc46641a9020752d5b50db

--- chrome/browser/extensions/api/proxy/proxy_apitest.cc
+++ chrome/browser/extensions/api/proxy/proxy_apitest.cc
@@ -396,20 +396,10 @@ IN_PROC_BROWSER_TEST_F(ProxySettingsApiTest,
                    pref_service);
 }
 
-// This test sets proxy to an inavalid host "does.not.exist" and then fetches
-// a page from localhost, expecting an error since host is invalid.
-// On ChromeOS, localhost is by default bypassed, so the page from localhost
-// will be fetched successfully, resulting in no error.  Hence this test
-// shouldn't run on ChromeOS.
-#if defined(OS_CHROMEOS)
-#define MAYBE_ProxyEventsInvalidProxy DISABLED_ProxyEventsInvalidProxy
-#else
-#define MAYBE_ProxyEventsInvalidProxy ProxyEventsInvalidProxy
-#endif  // defined(OS_CHROMEOS)
-
-// Tests error events: invalid proxy
-IN_PROC_BROWSER_TEST_F(ProxySettingsApiTest, MAYBE_ProxyEventsInvalidProxy) {
-  ASSERT_TRUE(StartEmbeddedTestServer());
+// This test sets the HTTP proxy to an unreachable host "does.not.exist" and
+// then attempts to fetch "example.test", expecting the listeners of
+// chrome.proxy.onProxyError to fire with ERR_PROXY_CONNECTION_FAILED.
+IN_PROC_BROWSER_TEST_F(ProxySettingsApiTest, ProxyEventsInvalidProxy) {
   ASSERT_TRUE(
       RunExtensionSubtest("proxy/events", "invalid_proxy.html")) << message_;
 }
--- chrome/test/data/extensions/api_test/proxy/events/invalid_proxy.js
+++ chrome/test/data/extensions/api_test/proxy/events/invalid_proxy.js
@@ -6,11 +6,10 @@
 // browser_tests.exe --gtest_filter=ProxySettingsApiTest.ProxyEventsInvalidProxy
 
 var expected_error = {
-    error: "net::ERR_PROXY_CONNECTION_FAILED",
-    details: "",
+    error: 'net::ERR_PROXY_CONNECTION_FAILED',
+    details: '',
     fatal: true
 };
-var empty_json_url = "";
 
 function test() {
   // Install error handler and get the test server config.
@@ -18,30 +17,26 @@ function test() {
     chrome.test.assertEq(expected_error, error);
     chrome.test.notifyPass();
   });
-  chrome.test.getConfig(readConfigAndSetProxy);
-}
 
-function readConfigAndSetProxy(test_config) {
-  // Construct the URL used for XHRs and set the proxy settings.
-  empty_json_url = "http://127.0.0.1:" +
-      test_config.testServer.port +
-      "/extensions/api_test/proxy/events/empty.json";
-
-  // Set an invalid proxy and fire of a XHR. This should trigger proxy errors.
+  // Set an invalid proxy and fire off an XHR. This should trigger proxy errors.
   // There may be any number of proxy errors, as systems like safe browsing
   // might start network traffic as well.
   var rules = {
-    singleProxy: { host: "does.not.exist" }
+    singleProxy: { host: 'does.not.exist' }
   };
-  var config = { rules: rules, mode: "fixed_servers" };
+  var config = { rules: rules, mode: 'fixed_servers' };
   chrome.proxy.settings.set({'value': config}, sendFailingXHR);
 }
 
 function sendFailingXHR() {
+  // The URL for the XHR doesn't matter, since it will be sent through an HTTP
+  // proxy server (and the proxy server is unreachable).
+  var url = 'http://example.test/';
+
   var req = new XMLHttpRequest();
-  req.open("GET", empty_json_url, true);
+  req.open('GET', url, true);
   req.onload = function () {
-    chrome.test.notifyFail("proxy settings should not work");
+    chrome.test.notifyFail('proxy settings should not work');
   }
   req.onerror = testDone;
   req.send(null);
