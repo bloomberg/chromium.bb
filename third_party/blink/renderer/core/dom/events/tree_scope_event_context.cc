98018dcb20d82043c11b7895b5c5f6a7ea5029e2
Kent Tamura <tkent@chromium.org>
2018-08-28 04:05:40 +0000

    DOM: Reduce the size of window_event_context.h
    This CL reduces the pre-processed size of window_event_context.h by
    3.63MB.
    
    Bug: 242216
    Change-Id: I7a93ad794d7adf10b5f67291e41bf550703440e5
    Reviewed-on: https://chromium-review.googlesource.com/1189308
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586575}
    Former-commit-id: 47213728bc1e40d9cd30579122f901f340fe8445

--- third_party/blink/renderer/core/dom/events/tree_scope_event_context.cc
+++ third_party/blink/renderer/core/dom/events/tree_scope_event_context.cc
@@ -31,6 +31,7 @@
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/dom/static_node_list.h"
 #include "third_party/blink/renderer/core/events/touch_event_context.h"
+#include "third_party/blink/renderer/core/frame/local_dom_window.h"
 
 namespace blink {
 
