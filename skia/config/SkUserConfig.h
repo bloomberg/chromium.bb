e901a0e2c6037620b89b6488ac3a8403c2354b2c
Mike Klein <mtklein@chromium.org>
2018-08-29 04:44:20 +0000

    rebaseline SK_LEGACY_SRGB_GAMUT
    Should be a bunch of subtle little diffs.
    
    Change-Id: I8c79b7179741008fbd91faafc2a8f5e23875a5a4
    Reviewed-on: https://chromium-review.googlesource.com/1194162
    Commit-Queue: Mike Klein <mtklein@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587013}
    Former-commit-id: 3946a9d8ca6c4d33323b77fc58811b43a02ae712

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -198,8 +198,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_AAA_CHOICE
 #endif
 
-#define SK_LEGACY_SRGB_GAMUT
-
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
