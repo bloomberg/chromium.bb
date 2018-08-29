f3f4aba86c0b05e7668bf86a8063b85efed2eb3d
Becky Zhou <huayinz@chromium.org>
2018-08-29 17:17:35 +0000

    [Feed] Stub out SuggestionsSource for Feed NTP
    We don't need to create SnippetsBridge and its native counterpart for
    the Feed NTP.
    
    Bug: 870929
    Change-Id: If0551db89f5d87468dd1cff8f7af495b017328ce
    Reviewed-on: https://chromium-review.googlesource.com/1195047
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Becky Zhou <huayinz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587168}
    Former-commit-id: 54985200108415f3fff4d1efa9dbd04fda58ff9a

--- chrome/android/java/src/org/chromium/chrome/browser/contextual_suggestions/ContextualSuggestionsSource.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextual_suggestions/ContextualSuggestionsSource.java
@@ -8,18 +8,15 @@ import android.graphics.Bitmap;
 
 import org.chromium.base.Callback;
 import org.chromium.chrome.browser.contextual_suggestions.ContextualSuggestionsBridge.ContextualSuggestionsResult;
-import org.chromium.chrome.browser.ntp.cards.SuggestionsCategoryInfo;
+import org.chromium.chrome.browser.ntp.snippets.EmptySuggestionsSource;
 import org.chromium.chrome.browser.ntp.snippets.SnippetArticle;
-import org.chromium.chrome.browser.ntp.snippets.SuggestionsSource;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.content_public.browser.WebContents;
-
-import java.util.List;
 
 /**
  * Provides content for contextual suggestions.
  */
-class ContextualSuggestionsSource implements SuggestionsSource {
+class ContextualSuggestionsSource extends EmptySuggestionsSource {
     private ContextualSuggestionsBridge mBridge;
 
     /**
@@ -86,58 +83,8 @@ class ContextualSuggestionsSource implements SuggestionsSource {
         mBridge.clearState();
     }
 
-    // The following methods are not applicable to contextual suggestions.
+    // Some methods from SuggestionsSource are not applicable to contextual suggestions.
     // TODO(twellington): The NTP classes used to display suggestion cards rely
     // on the SuggestionsSource implementation. Refactor to limit reliance to the
     // subset of methods actually used to render cards.
-
-    @Override
-    public void fetchRemoteSuggestions() {}
-
-    @Override
-    public boolean areRemoteSuggestionsEnabled() {
-        return false;
-    }
-
-    @Override
-    public int[] getCategories() {
-        return null;
-    }
-
-    @Override
-    public int getCategoryStatus(int category) {
-        return 0;
-    }
-
-    @Override
-    public SuggestionsCategoryInfo getCategoryInfo(int category) {
-        return null;
-    }
-
-    @Override
-    public List<SnippetArticle> getSuggestionsForCategory(int category) {
-        return null;
-    }
-
-    @Override
-    public void fetchSuggestions(int category, String[] displayedSuggestionIds,
-            Callback<List<SnippetArticle>> successCallback, Runnable failureRunnable) {}
-
-    @Override
-    public void fetchContextualSuggestions(String url, Callback<List<SnippetArticle>> callback) {}
-
-    @Override
-    public void dismissSuggestion(SnippetArticle suggestion) {}
-
-    @Override
-    public void dismissCategory(int category) {}
-
-    @Override
-    public void restoreDismissedCategories() {}
-
-    @Override
-    public void addObserver(Observer observer) {}
-
-    @Override
-    public void removeObserver(Observer observer) {}
 }
