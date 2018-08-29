511c9240da2b591439e4e70bc43dd7f7aa96f499
btolsch <btolsch@chromium.org>
2018-08-29 16:12:49 -0700

    Add include guard check to PRESUBMIT.sh
    This change also skips presubmit checks for code under third_party/.
    
    Change-Id: Ieaa406c034613eafeb094bf3b7f950c175a9bd56
    Reviewed-on: https://chromium-review.googlesource.com/1195594
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>

--- PRESUBMIT.sh
+++ PRESUBMIT.sh
@@ -3,15 +3,31 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Check that all C++ and header files conform to clang-format style.
+# Check that:
+#  - all C++ and header files conform to clang-format style.
+#  - all header files have appropriate include guards.
 fail=0
 for f in $(git diff --name-only @{u}); do
-  if echo $f | sed -n '/\.\(cc\|h\)$/q1;q0'; then
+  if echo $f | sed -n '/^third_party/q0;/\.\(cc\|h\)$/q1;q0'; then
     continue;
   fi
+  # clang-format check.
   if ! cmp -s <(clang-format -style=file "$f") "$f"; then
     echo "Needs format: $f"
     fail=1
+  fi
+  # Include guard check.
+  if echo $f | sed -n '/\.h$/q0;q1'; then
+    guard_name=$(echo $f | sed ':a;s/\//_/;ta;s/\.h$/_h_/')
+    guard_name=${guard_name^^}
+    ifndef_count=$(grep -q -E "^#ifndef $guard_name\$" $f | wc -l)
+    define_count=$(grep -q -E "^#define $guard_name\$" $f | wc -l)
+    endif_count=$(grep -q -E "^#endif  // $guard_name\$" $f | wc -l)
+    if [ $ifndef_count -ne 1 -o $define_count -ne 1 -o \
+         $endif_count -ne 1 ]; then
+      echo "Include guard missing/incorrect: $f"
+      fail=1
+    fi
   fi
 done
 
