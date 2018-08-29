0bbcf27fc0729c1fbe21ab6ce5abf3b2b3cf4f06
Yuta Kitamura <yutak@chromium.org>
2018-08-29 11:04:15 +0000

    Revert "Re-land "Get Chrome App frame immersive mode closer to working in Mash""
    This reverts commit 957a2291c690a511cb45f6f39e10ea10f6032446 [formerly 73315d63925a6b7409c90e413c3c97302674620a].
    
    Reason for revert: Caused ASan bot failure (heap-use-after-free)
    
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/28777
    
    Original change's description:
    > Re-land "Get Chrome App frame immersive mode closer to working in Mash"
    > 
    > This relands commit 5738c7c4d7f9a312a331c59f967a3376736c45bd [formerly 58a77e6664aaaca0bfd9c2f01106f2474ea02092].
    > This reverts commit 8530b53a37ac8233fbc035101483f0bd032df87b [formerly a6b39a98a69432d411926d9a68126dc9236ae63f].
    > 
    > The fix is in ImmersiveFullscreenController, which listens for destruction
    > of |top_container_|.
    > 
    > Original change's description:
    > > Get Chrome App frame immersive mode closer to working in Mash
    > >
    > > Removes the ImmersiveFullscreenController from
    > > ChromeNativeAppWindowViewsAuraAsh and relies on the default one provided
    > > by Ash. Also:
    > >
    > > 1. Remove the WindowStateDelegate from ChromeNativeAppWindowViewsAuraAsh
    > >    - Listen for fullscreen changes in order to keep system fullscreen
    > >      state and AppWindow fullscreen state in sync.
    > >    - RestoreAlwaysOnTop should just work without the special delegate
    > >      implementation, as the taskbar handling in AppWindow is Windows-
    > >      specific. Tested by
    > >      WorkspaceLayoutManagerSoloTest.FullscreenSuspendsAlwaysOnTop
    > >    - Fold non-delegate functionality into main class
    > >
    > > 2. Remove the default Ash WindowStateDelegate (i.e. that created in
    > >    non_client_frame_view_ash.cc) which only overrode ToggleFullscreen.
    > >    The default implementation, ::wm::SetWindowFullscreen, works as
    > >    intended.
    > >
    > > 3. Add window properties to allow the client to enter/exit immersive mode,
    > >    get the position of the reveal view, and disable some ways of automatically
    > >    triggering immersive.
    > >
    > > TODO: propagate window properties across processes so that they work in OopAsh
    > > TODO: In OopAsh, listen for window state changes into/out of fullscreen. (This
    > >       will replace the functionality that the frame gets from
    > >       WindowStateObserver::OnPostWindowStateTypeChange in classic Ash).
    > 
    > Change-Id: If6f4994ed2dbb2f0c0bd92a784f3f1ec6023ab56
    > Reviewed-on: https://chromium-review.googlesource.com/1194874
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Commit-Queue: Evan Stade <estade@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#586934}
    
    TBR=sky@chromium.org,estade@chromium.org
    
    Change-Id: I01a50f00939a034368bc3b0ef507094f7fc22ae9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/1195291
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587072}
    Former-commit-id: 4fffb4ba82c15b83ed78ad750b1c498675add827

--- ash/public/cpp/immersive/immersive_fullscreen_controller.cc
+++ ash/public/cpp/immersive/immersive_fullscreen_controller.cc
@@ -11,7 +11,6 @@
 #include "ash/public/cpp/immersive/immersive_fullscreen_controller_delegate.h"
 #include "ash/public/cpp/immersive/immersive_gesture_handler.h"
 #include "ash/public/cpp/immersive/immersive_handler_factory.h"
-#include "ash/public/cpp/window_properties.h"
 #include "base/metrics/histogram_macros.h"
 #include "ui/aura/window.h"
 #include "ui/display/display.h"
@@ -72,7 +71,7 @@ ImmersiveFullscreenController::ImmersiveFullscreenController()
     : delegate_(NULL),
       top_container_(NULL),
       widget_(NULL),
-      event_observers_enabled_(false),
+      observers_enabled_(false),
       enabled_(false),
       reveal_state_(CLOSED),
       revealed_lock_count_(0),
@@ -83,7 +82,6 @@ ImmersiveFullscreenController::ImmersiveFullscreenController()
       weak_ptr_factory_(this) {}
 
 ImmersiveFullscreenController::~ImmersiveFullscreenController() {
-  EnableEventObservers(false);
   EnableWindowObservers(false);
 }
 
@@ -91,16 +89,10 @@ void ImmersiveFullscreenController::Init(
     ImmersiveFullscreenControllerDelegate* delegate,
     views::Widget* widget,
     views::View* top_container) {
-  // This function may be called more than once (e.g. by
-  // ClientControlledShellSurface).
-  EnableWindowObservers(false);
-
   delegate_ = delegate;
   top_container_ = top_container;
   widget_ = widget;
   ImmersiveContext::Get()->InstallResizeHandleWindowTargeter(this);
-
-  EnableWindowObservers(true);
 }
 
 void ImmersiveFullscreenController::SetEnabled(WindowType window_type,
@@ -109,7 +101,7 @@ void ImmersiveFullscreenController::SetEnabled(WindowType window_type,
     return;
   enabled_ = enabled;
 
-  EnableEventObservers(enabled_);
+  EnableWindowObservers(enabled_);
 
   ImmersiveContext::Get()->OnEnteringOrExitingImmersive(this, enabled);
 
@@ -266,43 +258,14 @@ void ImmersiveFullscreenController::OnPointerEventObserved(
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-// aura::WindowObserver overrides:
+// views::WidgetObserver overrides:
 
-void ImmersiveFullscreenController::OnWindowPropertyChanged(
-    aura::Window* window,
-    const void* key,
-    intptr_t old) {
-  if (key == kImmersiveIsActive) {
-    SetEnabled(ImmersiveFullscreenController::WINDOW_TYPE_OTHER,
-               window->GetProperty(kImmersiveIsActive));
-  }
-}
-
-void ImmersiveFullscreenController::OnWindowDestroying(aura::Window* window) {
-  EnableEventObservers(false);
+void ImmersiveFullscreenController::OnWidgetDestroying(views::Widget* widget) {
   EnableWindowObservers(false);
 
   // Set |enabled_| to false such that any calls to MaybeStartReveal() and
   // MaybeEndReveal() have no effect.
   enabled_ = false;
-  widget_ = nullptr;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// views::Observer overrides:
-
-void ImmersiveFullscreenController::OnViewBoundsChanged(
-    views::View* observed_view) {
-  DCHECK_EQ(top_container_, observed_view);
-  widget()->GetNativeWindow()->SetProperty(
-      kImmersiveTopContainerBoundsInScreen,
-      new gfx::Rect(top_container_->GetBoundsInScreen()));
-}
-
-void ImmersiveFullscreenController::OnViewIsDeleting(
-    views::View* observed_view) {
-  DCHECK_EQ(observed_view, top_container_);
-  top_container_ = nullptr;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -343,43 +306,24 @@ void ImmersiveFullscreenController::UnlockRevealedState() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-// public:
-
-// static
-void ImmersiveFullscreenController::EnableForWidget(views::Widget* widget,
-                                                    bool enabled) {
-  widget->GetNativeWindow()->SetProperty(kImmersiveIsActive, enabled);
-}
-
-////////////////////////////////////////////////////////////////////////////////
 // private:
 
 void ImmersiveFullscreenController::EnableWindowObservers(bool enable) {
-  if (enable) {
-    top_container_->AddObserver(this);
-    widget_->GetNativeWindow()->AddObserver(this);
-  } else {
-    if (top_container_)
-      top_container_->RemoveObserver(this);
-    if (widget_)
-      widget_->GetNativeWindow()->RemoveObserver(this);
-  }
-}
-
-void ImmersiveFullscreenController::EnableEventObservers(bool enable) {
-  if (event_observers_enabled_ == enable)
+  if (observers_enabled_ == enable)
     return;
-  event_observers_enabled_ = enable;
+  observers_enabled_ = enable;
 
   if (enable) {
     immersive_focus_watcher_ =
         ImmersiveHandlerFactory::Get()->CreateFocusWatcher(this);
     immersive_gesture_handler_ =
         ImmersiveHandlerFactory::Get()->CreateGestureHandler(this);
+    widget_->AddObserver(this);
     ImmersiveContext::Get()->AddPointerWatcher(
         this, views::PointerWatcherEventTypes::MOVES);
   } else {
     ImmersiveContext::Get()->RemovePointerWatcher(this);
+    widget_->RemoveObserver(this);
     immersive_gesture_handler_.reset();
     immersive_focus_watcher_.reset();
 
