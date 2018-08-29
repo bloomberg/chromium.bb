32e67d67ca433040b8e8a911cf00f1bcd870e420
Martin Kreichgauer <martinkr@google.com>
2018-08-29 13:38:54 +0000

    fido/mac: require user interaction before returning any error on GetAssertion
    The TouchIdAuthenticator previously skipped the Touch ID prompt if no
    matching credential existed for a GetAssertion request. This information
    is now passed up into the UI at authenticator instantiation time, so the
    authenticator does not need to handle it. Instead, it should implement
    safe, spec-compliant behavior and *always* prompt the user before
    returning an error.
    
    Bug: 678128
    Change-Id: I6aff58807a9c0b9b7b2f2f154af60f5103be5ac9
    Reviewed-on: https://chromium-review.googlesource.com/1194953
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#587094}
    Former-commit-id: ad9b63e7496393cf05c145b3636a7c1258363926

--- device/fido/mac/get_assertion_operation.mm
+++ device/fido/mac/get_assertion_operation.mm
@@ -51,6 +51,18 @@ void GetAssertionOperation::Run() {
     return;
   }
 
+  // Display the macOS Touch ID prompt.
+  PromptTouchId(l10n_util::GetStringFUTF16(IDS_WEBAUTHN_TOUCH_ID_PROMPT_REASON,
+                                           base::UTF8ToUTF16(RpId())));
+}
+
+void GetAssertionOperation::PromptTouchIdDone(bool success) {
+  if (!success) {
+    std::move(callback())
+        .Run(CtapDeviceResponseCode::kCtap2ErrOperationDenied, base::nullopt);
+    return;
+  }
+
   // Collect the credential ids from allowList. If allowList is absent, we will
   // pick the first available credential for the RP.
   std::set<std::vector<uint8_t>> allowed_credential_ids;
@@ -69,10 +81,10 @@ void GetAssertionOperation::Run() {
   }
 
   // Fetch credentials for RP from the request and current user profile.
-  credential_ = FindCredentialInKeychain(
+  base::Optional<Credential> credential = FindCredentialInKeychain(
       keychain_access_group(), metadata_secret(), RpId(),
       allowed_credential_ids, authentication_context());
-  if (!credential_) {
+  if (!credential) {
     // For now, don't show a Touch ID prompt if no credential exists.
     // TODO(martinkr): Prompt for the fingerprint anyway, once dispatch to this
     // authenticator is moved behind user interaction with the authenticator
@@ -82,24 +94,10 @@ void GetAssertionOperation::Run() {
     return;
   }
 
-  // Display the macOS Touch ID prompt.
-  PromptTouchId(l10n_util::GetStringFUTF16(IDS_WEBAUTHN_TOUCH_ID_PROMPT_REASON,
-                                           base::UTF8ToUTF16(RpId())));
-}
-
-void GetAssertionOperation::PromptTouchIdDone(bool success) {
-  DCHECK(credential_);
-
-  if (!success) {
-    std::move(callback())
-        .Run(CtapDeviceResponseCode::kCtap2ErrOperationDenied, base::nullopt);
-    return;
-  }
-
   // Decrypt the user entity from the credential ID.
   base::Optional<CredentialMetadata::UserEntity> credential_user =
       CredentialMetadata::UnsealCredentialId(metadata_secret(), RpId(),
-                                             credential_->credential_id);
+                                             credential->credential_id);
   if (!credential_user) {
     // The keychain query already filtered for the RP ID encoded under this
     // operation's metadata secret, so the credential id really should have
@@ -111,18 +109,18 @@ void GetAssertionOperation::PromptTouchIdDone(bool success) {
   }
 
   base::ScopedCFTypeRef<SecKeyRef> public_key(
-      Keychain::GetInstance().KeyCopyPublicKey(credential_->private_key));
+      Keychain::GetInstance().KeyCopyPublicKey(credential->private_key));
   if (!public_key) {
     DLOG(ERROR) << "failed to get public key for credential id "
-                << base::HexEncode(credential_->credential_id.data(),
-                                   credential_->credential_id.size());
+                << base::HexEncode(credential->credential_id.data(),
+                                   credential->credential_id.size());
     std::move(callback())
         .Run(CtapDeviceResponseCode::kCtap2ErrOther, base::nullopt);
     return;
   }
 
   base::Optional<AuthenticatorData> authenticator_data = MakeAuthenticatorData(
-      RpId(), credential_->credential_id, SecKeyRefToECPublicKey(public_key));
+      RpId(), credential->credential_id, SecKeyRefToECPublicKey(public_key));
   if (!authenticator_data) {
     DLOG(ERROR) << "MakeAuthenticatorData failed";
     std::move(callback())
@@ -131,7 +129,7 @@ void GetAssertionOperation::PromptTouchIdDone(bool success) {
   }
   base::Optional<std::vector<uint8_t>> signature =
       GenerateSignature(*authenticator_data, request().client_data_hash(),
-                        credential_->private_key);
+                        credential->private_key);
   if (!signature) {
     DLOG(ERROR) << "GenerateSignature failed";
     std::move(callback())
@@ -141,7 +139,7 @@ void GetAssertionOperation::PromptTouchIdDone(bool success) {
   auto response = AuthenticatorGetAssertionResponse(
       std::move(*authenticator_data), std::move(*signature));
   response.SetCredential(PublicKeyCredentialDescriptor(
-      CredentialType::kPublicKey, std::move(credential_->credential_id)));
+      CredentialType::kPublicKey, std::move(credential->credential_id)));
   response.SetUserEntity(credential_user->ToPublicKeyCredentialUserEntity());
 
   std::move(callback())
