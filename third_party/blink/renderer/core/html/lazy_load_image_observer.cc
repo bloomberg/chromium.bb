22382f87091ac2f9ab61ae57cab03ae1f8baa4b5
rajendrant <rajendrant@chromium.org>
2018-08-29 08:08:39 +0000

    LazyLoad: Make lazy image loading distance threshold configurable
    This CL makes the distance-from-viewport threshold that controls how
    close the user has to scroll to a deferred image configurable via
    --blink-settings and field trial params. Different distance threshold
    values can be specified for different effective connection types and
    data saver settings, so that e.g. a larger distance threshold can be
    specified for slower networks to give images more time to load in, or
    e.g. a shorter distance threshold could be specified for data saver
    users to save some more data.
    
    Bug: 846170
    Change-Id: I7996dc4e90970adfde6f83826095a2e7f78eda9e
    Reviewed-on: https://chromium-review.googlesource.com/1186263
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587039}
    Former-commit-id: d8026346bbf3912253dde2af73266153481c6be4

--- third_party/blink/renderer/core/html/lazy_load_image_observer.cc
+++ third_party/blink/renderer/core/html/lazy_load_image_observer.cc
@@ -5,10 +5,13 @@
 #include "third_party/blink/renderer/core/html/lazy_load_image_observer.h"
 
 #include "build/build_config.h"
+#include "third_party/blink/public/platform/web_effective_connection_type.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/node_computed_style.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
+#include "third_party/blink/renderer/core/frame/local_frame_client.h"
+#include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/html/html_image_element.h"
 #include "third_party/blink/renderer/core/html_element_type_helpers.h"
 #include "third_party/blink/renderer/core/inspector/console_message.h"
@@ -17,6 +20,34 @@
 #include "third_party/blink/renderer/core/style/computed_style.h"
 
 namespace blink {
+
+namespace {
+
+int GetLazyImageLoadingViewportDistanceThresholdPx(const Document& document) {
+  const Settings* settings = document.GetSettings();
+  if (!settings)
+    return 0;
+
+  DCHECK(document.GetFrame() && document.GetFrame()->Client());
+  switch (document.GetFrame()->Client()->GetEffectiveConnectionType()) {
+    case WebEffectiveConnectionType::kTypeUnknown:
+      return settings->GetLazyImageLoadingDistanceThresholdPxUnknown();
+    case WebEffectiveConnectionType::kTypeOffline:
+      return settings->GetLazyImageLoadingDistanceThresholdPxOffline();
+    case WebEffectiveConnectionType::kTypeSlow2G:
+      return settings->GetLazyImageLoadingDistanceThresholdPxSlow2G();
+    case WebEffectiveConnectionType::kType2G:
+      return settings->GetLazyImageLoadingDistanceThresholdPx2G();
+    case WebEffectiveConnectionType::kType3G:
+      return settings->GetLazyImageLoadingDistanceThresholdPx3G();
+    case WebEffectiveConnectionType::kType4G:
+      return settings->GetLazyImageLoadingDistanceThresholdPx4G();
+  }
+  NOTREACHED();
+  return 0;
+}
+
+}  // namespace
 
 void LazyLoadImageObserver::StartMonitoring(Element* element) {
   if (LocalFrame* frame = element->GetDocument().GetFrame()) {
@@ -43,7 +74,8 @@ LazyLoadImageObserver::LazyLoadImageObserver(Document& document) {
       "Images loaded lazily and replaced with placeholders. Load events are "
       "deferred. See https://crbug.com/846170"));
   lazy_load_intersection_observer_ = IntersectionObserver::Create(
-      {Length(kLazyLoadRootMarginPx, kFixed)},
+      {Length(GetLazyImageLoadingViewportDistanceThresholdPx(document),
+              kFixed)},
       {std::numeric_limits<float>::min()}, &document,
       WTF::BindRepeating(&LazyLoadImageObserver::LoadIfNearViewport,
                          WrapWeakPersistent(this)));
