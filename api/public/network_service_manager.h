c58d33a8f0cfd62f9b327065dfb2dcc7c3e23132
mark a. foltz <mfoltz@chromium.org>
2018-08-29 14:20:53 -0700

    [OSP] Fix compilation errors.
    - Fix typo in #include guard.
    - GMock does not add the 'override' keyword when mocking pure virtual methods (ref:
     https://github.com/google/googletest/issues/533).  This looks unlikely to be fixed
    anytime soon, so suppress this warning.
    
    Bug: openscreen:15
    
    Change-Id: I54ae3c87b4a4aed8907fac9d3a63f2fd78900327
    Reviewed-on: https://chromium-review.googlesource.com/1195590
    Reviewed-by: Brandon Tolsch <btolsch@chromium.org>

--- api/public/network_service_manager.h
+++ api/public/network_service_manager.h
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef API_PUBLIC_NETWORK_SERVICE_MANAGERH_
+#ifndef API_PUBLIC_NETWORK_SERVICE_MANAGER_H_
 #define API_PUBLIC_NETWORK_SERVICE_MANAGER_H_
 
 #include <memory>
