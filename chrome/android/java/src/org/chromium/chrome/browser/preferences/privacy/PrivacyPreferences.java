311ff46e4963f1d682a4f42be64f10ff6e486746
Boris Sazonov <bsazonov@chromium.org>
2018-08-29 00:17:09 +0000

    [Unity][Android] Add explicit divider to PrivacyPreferences
    This CL adds an explicit divider before sync_and_services_link
    (a description text in PrivacyPreferences that links to 'Sync and Google
    services').
    
    Bug: 814728
    Change-Id: Id110a7d6e66c21aeb3043f2823dcfa9727564d20
    Reviewed-on: https://chromium-review.googlesource.com/1193851
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#586953}
    Former-commit-id: 8136c9c2b2be7d4f0e016240bc6748ea9683ebdc

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferences.java
@@ -49,6 +49,8 @@ public class PrivacyPreferences extends PreferenceFragment
     private static final String PREF_DO_NOT_TRACK = "do_not_track";
     private static final String PREF_USAGE_AND_CRASH_REPORTING = "usage_and_crash_reports";
     private static final String PREF_CLEAR_BROWSING_DATA = "clear_browsing_data";
+    private static final String PREF_SYNC_AND_SERVICES_LINK_DIVIDER =
+            "sync_and_services_link_divider";
     private static final String PREF_SYNC_AND_SERVICES_LINK = "sync_and_services_link";
 
     private ManagedPreferenceDelegate mManagedPreferenceDelegate;
@@ -94,6 +96,7 @@ public class PrivacyPreferences extends PreferenceFragment
             updateSummaries();
             return;
         }
+        preferenceScreen.removePreference(findPreference(PREF_SYNC_AND_SERVICES_LINK_DIVIDER));
         preferenceScreen.removePreference(findPreference(PREF_SYNC_AND_SERVICES_LINK));
 
         ChromeBaseCheckBoxPreference networkPredictionPref =
