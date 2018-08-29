382b1b94159356f2487048b2e11bc6d9b9c2c280
Jialiu Lin <jialiul@chromium.org>
2018-08-29 16:39:16 +0000

    Refresh Advanced Protection Status via OAuth2 token refresh
    If user is under advanced protection, refresh OAuth2 id token every 24 hours to
    determine if they are still in this program.
    If user's status is unknown when initiating AdvancedProtectionStatusManager,
    refresh OAuth2 token right away (a.k.a in a minute).
    
    (If user is known to NOT in advanced protection, no need to
    refresh. This is because the enrollment of advance protection will
    trigger reauth in every active sessions.)
    
    Change-Id: Iec2eaffc603d9a909e0e5d1d6a5ea6b959f9960a
    Bug: 866620
    Reviewed-on: https://chromium-review.googlesource.com/1178979
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Commit-Queue: Jialiu Lin <jialiul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587153}
    Former-commit-id: 412124f80dca00ace1b03d5cf7b99c41474c05ea

--- chrome/browser/safe_browsing/advanced_protection_status_manager.cc
+++ chrome/browser/safe_browsing/advanced_protection_status_manager.cc
@@ -8,12 +8,19 @@
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/safe_browsing/advanced_protection_status_manager_factory.h"
 #include "chrome/browser/signin/account_tracker_service_factory.h"
+#include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
 #include "chrome/browser/signin/signin_manager_factory.h"
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing/common/safe_browsing_prefs.h"
 #include "components/safe_browsing/features.h"
+#include "components/signin/core/browser/profile_oauth2_token_service.h"
 #include "components/signin/core/browser/signin_manager.h"
 #include "content/public/browser/browser_context.h"
+#include "content/public/browser/browser_thread.h"
+#include "google_apis/gaia/gaia_constants.h"
+#include "google_apis/gaia/oauth2_id_token_decoder.h"
+
+using content::BrowserThread;
 
 namespace safe_browsing {
 
@@ -21,37 +28,84 @@ namespace {
 
 const base::TimeDelta kRefreshAdvancedProtectionDelay =
     base::TimeDelta::FromDays(1);
-
-// If |info| matches the primary account of |profile|.
-bool IsPrimaryAccount(Profile* profile, const AccountInfo& info) {
-  SigninManagerBase* signin_manager =
-      SigninManagerFactory::GetForProfileIfExists(profile);
-
-  return signin_manager && !info.account_id.empty() &&
-         info.account_id == signin_manager->GetAuthenticatedAccountId();
-}
-
+const base::TimeDelta kRetryDelay = base::TimeDelta::FromMinutes(5);
+const base::TimeDelta kMinimumRefreshDelay = base::TimeDelta::FromMinutes(1);
 }  // namespace
 
+////////////////////////////////////////////////////////////////////////////////
+// AdvancedProtectionTokenConsumer
+////////////////////////////////////////////////////////////////////////////////
+AdvancedProtectionStatusManager::AdvancedProtectionTokenConsumer::
+    AdvancedProtectionTokenConsumer(const std::string& account_id,
+                                    AdvancedProtectionStatusManager* manager)
+    : OAuth2TokenService::Consumer(account_id), manager_(manager) {}
+
+AdvancedProtectionStatusManager::AdvancedProtectionTokenConsumer::
+    ~AdvancedProtectionTokenConsumer() {}
+
+void AdvancedProtectionStatusManager::AdvancedProtectionTokenConsumer::
+    OnGetTokenSuccess(
+        const OAuth2TokenService::Request* request,
+        const OAuth2AccessTokenConsumer::TokenResponse& token_response) {
+  manager_->OnGetIDToken(request->GetAccountId(), token_response.id_token);
+  manager_->OnTokenRefreshDone(
+      request, GoogleServiceAuthError(GoogleServiceAuthError::NONE));
+}
+
+void AdvancedProtectionStatusManager::AdvancedProtectionTokenConsumer::
+    OnGetTokenFailure(const OAuth2TokenService::Request* request,
+                      const GoogleServiceAuthError& error) {
+  manager_->OnTokenRefreshDone(request, error);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// AdvancedProtectionStatusManager
+////////////////////////////////////////////////////////////////////////////////
 AdvancedProtectionStatusManager::AdvancedProtectionStatusManager(
     Profile* profile)
-    : profile_(profile), is_under_advanced_protection_(false) {
+    : profile_(profile),
+      signin_manager_(nullptr),
+      account_tracker_service_(nullptr),
+      is_under_advanced_protection_(false),
+      minimum_delay_(kMinimumRefreshDelay) {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  if (profile_->IsOffTheRecord())
+    return;
+
+  Initialize();
+  MaybeRefreshOnStartUp();
+}
+
+void AdvancedProtectionStatusManager::Initialize() {
+  signin_manager_ = SigninManagerFactory::GetForProfile(profile_);
+  account_tracker_service_ =
+      AccountTrackerServiceFactory::GetForProfile(profile_);
+  SubscribeToSigninEvents();
+}
+
+void AdvancedProtectionStatusManager::MaybeRefreshOnStartUp() {
   // Retrieves advanced protection service status from primary account's info.
-  if (!profile_->IsOffTheRecord()) {
-    AccountInfo info = SigninManagerFactory::GetForProfileIfExists(profile)
-                           ->GetAuthenticatedAccountInfo();
+  AccountInfo info = signin_manager_->GetAuthenticatedAccountInfo();
+  if (info.account_id.empty())
+    return;
 
-    // Initializes advanced protection status if user's signed-in.
-    if (!info.account_id.empty()) {
-      is_under_advanced_protection_ = info.is_under_advanced_protection;
-      primary_account_gaia_ = info.gaia;
-    }
+  is_under_advanced_protection_ = info.is_under_advanced_protection;
+  primary_account_id_ = info.account_id;
 
+  if (profile_->GetPrefs()->HasPrefPath(
+          prefs::kAdvancedProtectionLastRefreshInUs)) {
     last_refreshed_ = base::Time::FromDeltaSinceWindowsEpoch(
         base::TimeDelta::FromMicroseconds(profile_->GetPrefs()->GetInt64(
             prefs::kAdvancedProtectionLastRefreshInUs)));
+    if (is_under_advanced_protection_)
+      ScheduleNextRefresh();
+  } else {
+    // User's advanced protection status is unknown, refresh in
+    // |minimum_delay_|.
+    timer_.Start(
+        FROM_HERE, minimum_delay_, this,
+        &AdvancedProtectionStatusManager::RefreshAdvancedProtectionStatus);
   }
-  SubscribeToSigninEvents();
 }
 
 // static
@@ -76,18 +130,21 @@ void AdvancedProtectionStatusManager::UnsubscribeFromSigninEvents() {
   SigninManagerFactory::GetForProfile(profile_)->RemoveObserver(this);
 }
 
+bool AdvancedProtectionStatusManager::IsRefreshScheduled() {
+  return timer_.IsRunning();
+}
+
 void AdvancedProtectionStatusManager::OnAccountUpdated(
     const AccountInfo& info) {
   // Ignore update if |profile_| is in incognito mode, or the updated account
   // is not the primary account.
-  if (profile_->IsOffTheRecord() || !IsPrimaryAccount(profile_, info))
+  if (profile_->IsOffTheRecord() || !IsPrimaryAccount(info))
     return;
 
-  if (!is_under_advanced_protection_ && info.is_under_advanced_protection) {
+  if (info.is_under_advanced_protection) {
     // User just enrolled into advanced protection.
     OnAdvancedProtectionEnabled();
-  } else if (is_under_advanced_protection_ &&
-             !info.is_under_advanced_protection) {
+  } else {
     // User's no longer in advanced protection.
     OnAdvancedProtectionDisabled();
   }
@@ -99,68 +156,75 @@ void AdvancedProtectionStatusManager::OnAccountRemoved(
     return;
 
   // If user signed out primary account, cancel refresh.
-  if (!primary_account_gaia_.empty() && primary_account_gaia_ == info.gaia) {
-    primary_account_gaia_.clear();
+  if (!primary_account_id_.empty() && primary_account_id_ == info.account_id) {
+    primary_account_id_.clear();
+    is_under_advanced_protection_ = false;
     OnAdvancedProtectionDisabled();
   }
 }
 
 void AdvancedProtectionStatusManager::GoogleSigninSucceeded(
     const AccountInfo& account_info) {
-  primary_account_gaia_ = account_info.gaia;
-  if (!is_under_advanced_protection_ &&
-      account_info.is_under_advanced_protection) {
+  primary_account_id_ = account_info.account_id;
+  if (account_info.is_under_advanced_protection)
     OnAdvancedProtectionEnabled();
-  }
 }
 
 void AdvancedProtectionStatusManager::GoogleSignedOut(
     const AccountInfo& account_info) {
-  if (primary_account_gaia_ == account_info.gaia)
-    primary_account_gaia_.clear();
+  if (primary_account_id_ == account_info.account_id)
+    primary_account_id_.clear();
   OnAdvancedProtectionDisabled();
 }
 
 void AdvancedProtectionStatusManager::OnAdvancedProtectionEnabled() {
-  UpdateLastRefreshTime();
   is_under_advanced_protection_ = true;
+  UpdateLastRefreshTime();
   ScheduleNextRefresh();
 }
 
 void AdvancedProtectionStatusManager::OnAdvancedProtectionDisabled() {
-  if (!is_under_advanced_protection_)
-    return;
-
   is_under_advanced_protection_ = false;
+  UpdateLastRefreshTime();
   CancelFutureRefresh();
 }
 
 void AdvancedProtectionStatusManager::RefreshAdvancedProtectionStatus() {
-  // To Be Implemented.
-}
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
-void AdvancedProtectionStatusManager::
-    RefreshAdvancedProtectionStatusAndScheduleNext() {
-  DCHECK(is_under_advanced_protection_);
-  RefreshAdvancedProtectionStatus();
-  UpdateLastRefreshTime();
-  if (is_under_advanced_protection_)
-    ScheduleNextRefresh();
+  auto* token_service =
+      ProfileOAuth2TokenServiceFactory::GetForProfile(profile_);
+
+  if (!token_service || !IsUserSignedIn() || primary_account_id_.empty())
+    return;
+  // Refresh OAuth access token.
+  OAuth2TokenService::ScopeSet scopes;
+  scopes.insert(GaiaConstants::kOAuth1LoginScope);
+
+  // If there's already a request going on, do nothing.
+  if (access_token_request_)
+    return;
+
+  token_consumer_.reset(
+      new AdvancedProtectionTokenConsumer(primary_account_id_, this));
+  access_token_request_ = token_service->StartRequest(
+      primary_account_id_, scopes, token_consumer_.get());
 }
 
 void AdvancedProtectionStatusManager::ScheduleNextRefresh() {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
   CancelFutureRefresh();
+  base::Time now = base::Time::Now();
   const base::TimeDelta time_since_last_refresh =
-      base::Time::Now() - last_refreshed_;
-  if (time_since_last_refresh > kRefreshAdvancedProtectionDelay) {
-    RefreshAdvancedProtectionStatus();
-  } else {
-    timer_.Start(FROM_HERE,
-                 kRefreshAdvancedProtectionDelay - time_since_last_refresh,
-                 this,
-                 &AdvancedProtectionStatusManager::
-                     RefreshAdvancedProtectionStatusAndScheduleNext);
-  }
+      now > last_refreshed_ ? now - last_refreshed_ : base::TimeDelta::Max();
+  base::TimeDelta delay =
+      time_since_last_refresh > kRefreshAdvancedProtectionDelay
+          ? minimum_delay_
+          : std::max(minimum_delay_,
+                     kRefreshAdvancedProtectionDelay - time_since_last_refresh);
+  timer_.Start(
+      FROM_HERE, delay, this,
+      &AdvancedProtectionStatusManager::RefreshAdvancedProtectionStatus);
 }
 void AdvancedProtectionStatusManager::CancelFutureRefresh() {
   if (timer_.IsRunning())
@@ -184,6 +248,64 @@ bool AdvancedProtectionStatusManager::IsUnderAdvancedProtection(
   return AdvancedProtectionStatusManagerFactory::GetInstance()
       ->GetForBrowserContext(static_cast<content::BrowserContext*>(profile))
       ->is_under_advanced_protection();
+}
+
+bool AdvancedProtectionStatusManager::IsUserSignedIn() {
+  return signin_manager_ &&
+         !signin_manager_->GetAuthenticatedAccountInfo().account_id.empty();
+}
+
+bool AdvancedProtectionStatusManager::IsPrimaryAccount(
+    const AccountInfo& account_info) {
+  return IsUserSignedIn() && account_info.account_id ==
+                                 signin_manager_->GetAuthenticatedAccountId();
+}
+
+void AdvancedProtectionStatusManager::OnGetIDToken(
+    const std::string& account_id,
+    const std::string& id_token) {
+  // Skips if the ID token is not for the primary account. Or user is no longer
+  // signed in.
+  if (account_id != primary_account_id_ || !IsUserSignedIn())
+    return;
+
+  gaia::TokenServiceFlags service_flags = gaia::ParseServiceFlags(id_token);
+  // |OnAccountUpdated()| will only be triggered if the advanced protection
+  // status changed. Therefore, we need to call |UpdateLastRefreshTime()| here
+  // to force update and persist last refresh time.
+  account_tracker_service_->SetIsAdvancedProtectionAccount(
+      primary_account_id_, service_flags.is_under_advanced_protection);
+  UpdateLastRefreshTime();
+}
+
+void AdvancedProtectionStatusManager::OnTokenRefreshDone(
+    const OAuth2TokenService::Request* request,
+    const GoogleServiceAuthError& error) {
+  DCHECK(request == access_token_request_.get());
+
+  // TODO(jialiul): LOG success or error code via UMA.
+  access_token_request_.reset();
+
+  // If failure is transient, we'll retry in 5 minutes.
+  if (error.IsTransientError()) {
+    timer_.Start(
+        FROM_HERE, kRetryDelay, this,
+        &AdvancedProtectionStatusManager::RefreshAdvancedProtectionStatus);
+  }
+}
+
+AdvancedProtectionStatusManager::AdvancedProtectionStatusManager(
+    Profile* profile,
+    const base::TimeDelta& min_delay)
+    : profile_(profile),
+      signin_manager_(nullptr),
+      account_tracker_service_(nullptr),
+      is_under_advanced_protection_(false),
+      minimum_delay_(min_delay) {
+  if (profile_->IsOffTheRecord())
+    return;
+  Initialize();
+  MaybeRefreshOnStartUp();
 }
 
 }  // namespace safe_browsing
