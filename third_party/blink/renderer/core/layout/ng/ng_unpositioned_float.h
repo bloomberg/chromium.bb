ac6fe2e9bb422815bf32a1fdae8a2854305b6d94
Ian Kilpatrick <ikilpatrick@chromium.org>
2018-08-30 13:06:41 +0000

    [LayoutNG] Slim down the NGUnpositionedFloat struct.
    This removes all the fields which can be passed in via. PositionFloat(s)
    There should be no functionality change.
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: I1b05c0f58bb2aa1ba17374f6600deda56912408c
    Bug: 635619
    Reviewed-on: https://chromium-review.googlesource.com/1196428
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587539}
    Former-commit-id: e73d6a2950a29a72edad1e17d5e834f41f27a9f7

--- third_party/blink/renderer/core/layout/ng/ng_unpositioned_float.h
+++ third_party/blink/renderer/core/layout/ng/ng_unpositioned_float.h
@@ -7,7 +7,6 @@
 
 #include "base/memory/scoped_refptr.h"
 #include "third_party/blink/renderer/core/layout/ng/geometry/ng_box_strut.h"
-#include "third_party/blink/renderer/core/layout/ng/geometry/ng_logical_size.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_block_break_token.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_block_node.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_layout_result.h"
@@ -20,38 +19,16 @@ struct CORE_EXPORT NGUnpositionedFloat final {
   DISALLOW_NEW_EXCEPT_PLACEMENT_NEW();
 
  public:
-  NGUnpositionedFloat(const NGLogicalSize& available_size,
-                      const NGLogicalSize& percentage_size,
-                      LayoutUnit origin_bfc_line_offset,
-                      LayoutUnit bfc_line_offset,
-                      NGBlockNode node,
-                      NGBlockBreakToken* token);
+  NGUnpositionedFloat(NGBlockNode node, NGBlockBreakToken* token);
   ~NGUnpositionedFloat();
 
   NGBlockNode node;
   scoped_refptr<NGBlockBreakToken> token;
 
-  // Available size of the constraint space that will be used by
-  // NGLayoutOpportunityIterator to position this floating object.
-  NGLogicalSize available_size;
-  NGLogicalSize percentage_size;
-
-  // This is the BFC inline-offset for where we begin searching for layout
-  // opportunities for this float.
-  LayoutUnit origin_bfc_line_offset;
-
-  // This is the BFC inline-offset for the float's parent. This is used for
-  // calculating the offset between the float and its parent.
-  LayoutUnit bfc_line_offset;
-
-  // The margins are relative to the writing mode of the block formatting
-  // context. They are stored for convinence and could be recomputed with other
-  // data on this object.
-  NGBoxStrut margins;
-
-  // The layout result for this unpositioned float. This is only present if
-  // it's in a different writing mode than the BFC.
+  // layout_result and margins are used as a cache when measuring the
+  // inline_size of a float in an inline context.
   scoped_refptr<NGLayoutResult> layout_result;
+  NGBoxStrut margins;
 
   bool IsLeft() const;
   bool IsRight() const;
