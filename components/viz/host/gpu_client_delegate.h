5b2a24689d62df989054caf66338dd5c0af03d94
Mohsen Izadi <mohsen@chromium.org>
2018-08-30 23:49:23 +0000

    Establish channel directly from viz::GpuClient
    Now that viz::mojom::GpuHost implementation has moved out of content,
    viz::GpuClient can directly call on that to establish a gpu channel and
    we can remove the functionality from GpuClientDelegate.
    
    BUG=709332
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: Ied5a8ead810973068190e3ce485609f68f5826d5
    Reviewed-on: https://chromium-review.googlesource.com/1188883
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587852}
    Former-commit-id: 1c63949de600ac796e2d7969719f7862a91e7fb8

--- components/viz/host/gpu_client_delegate.h
+++ components/viz/host/gpu_client_delegate.h
@@ -8,50 +8,21 @@
 #include "base/callback_forward.h"
 #include "mojo/public/cpp/system/message_pipe.h"
 
-namespace gpu {
-struct GPUInfo;
-struct GpuFeatureInfo;
-}  // namespace gpu
-
 namespace viz {
-namespace mojom {
-class GpuService;
-}
 
+class GpuHostImpl;
 class HostGpuMemoryBufferManager;
 
-// Delegate interface that GpuClientImpl uses to get the current GpuService
-// instance and establish GPU channel for a client. These functions are
-// guaranteed to be called on the thread corresponding to GpuClientImpl's task
-// runner.
+// Delegate interface that GpuClient uses to get the current GpuHost and/or
+// GpuMemoryBufferManager instances. These functions are guaranteed to be called
+// on the thread corresponding to GpuClient's task runner.
 class GpuClientDelegate {
  public:
-  enum class EstablishGpuChannelStatus {
-    kGpuAccessDenied,  // GPU access was not allowed.
-    kGpuHostInvalid,   // Request failed because the gpu host became invalid
-                       // while processing the request (e.g. the gpu process may
-                       // have been killed). The caller should normally make
-                       // another request to establish a new channel.
-    kSuccess,
-  };
-
-  using EstablishGpuChannelCallback =
-      base::OnceCallback<void(mojo::ScopedMessagePipeHandle channel_handle,
-                              const gpu::GPUInfo&,
-                              const gpu::GpuFeatureInfo&,
-                              EstablishGpuChannelStatus status)>;
-
   virtual ~GpuClientDelegate() {}
 
-  // Returns the current instance of GPU service. If GPU service is not running,
+  // Returns the current instance of GpuHostImpl. If GPU service is not running,
   // tries to launch it. If the launch is unsuccessful, returns nullptr.
-  virtual mojom::GpuService* EnsureGpuService() = 0;
-
-  // Sends a request to establish a GPU channel for a client to the GPU service.
-  // If GPU service is not running, tries to launch it and send the request.
-  virtual void EstablishGpuChannel(int client_id,
-                                   uint64_t client_tracing_id,
-                                   EstablishGpuChannelCallback callback) = 0;
+  virtual GpuHostImpl* EnsureGpuHost() = 0;
 
   // Returns the current HostGpuMemoryBufferManager instance.
   virtual HostGpuMemoryBufferManager* GetGpuMemoryBufferManager() = 0;
