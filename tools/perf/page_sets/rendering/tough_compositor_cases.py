b71c915ea2165d389f490de1f40685fc696e59de
Victor Miura <vmiura@chromium.org>
2018-08-30 20:07:33 +0000

    Add layer count based tests to tough_compositor_cases.
    Tests enabled on Mobile:
      - css_opacity_plus_n_layers_0
      - css_opacity_plus_n_layers_75
      - css_opacity_plus_n_layers_99
      - js_opacity_plus_n_layers_0
      - js_opacity_plus_n_layers_75
      - js_opacity_plus_n_layers_99
      - js_paint_plus_n_layers_0
      - js_paint_plus_n_layers_75
      - js_paint_plus_n_layers_99
      - infinite_scroll_element_n_layers_0
      - infinite_scroll_element_n_layers_75
      - infinite_scroll_element_n_layers_99
      - infinite_scroll_root_n_layers_0
      - infinite_scroll_root_n_layers_75
      - infinite_scroll_root_n_layers_99
    
    Tests enabled on Desktop:
      - css_opacity_plus_n_layers_99
      - js_opacity_plus_n_layers_99
      - js_paint_plus_n_layers_99
      - infinite_scroll_element_n_layers_99
      - infinite_scroll_root_n_layers_99
    
    BUG=878951
    
    Change-Id: Iac2cb8d55424ef47e7f33de375540dee7cbc43c3
    Reviewed-on: https://chromium-review.googlesource.com/1192690
    Commit-Queue: Victor Miura <vmiura@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#587719}
    Former-commit-id: 5397fe05b09cd14be4e193cacf92fc68a17868c8

--- tools/perf/page_sets/rendering/tough_compositor_cases.py
+++ tools/perf/page_sets/rendering/tough_compositor_cases.py
@@ -5,7 +5,7 @@ from telemetry.page import shared_page_state
 
 from page_sets.rendering import rendering_story
 from page_sets.rendering import story_tags
-
+from page_sets.system_health import platforms
 
 class ToughCompositorPage(rendering_story.RenderingStory):
   ABSTRACT_STORY = True
@@ -51,12 +51,14 @@ class JSScrollTextPage(ToughCompositorScrollPage):
 
 
 # Why: Scroll by a large number of CC layers """
+# TODO(crbug.com/878951): Remove after 'n_layer' tests below are fully landed.
 class CCScroll200LayerPage(ToughCompositorScrollPage):
   BASE_NAME = 'cc_scroll_200_layer_grid'
   URL = 'http://jsbin.com/yakagevo/1/quiet?CC_SCROLL_200_LAYER_GRID'
 
 
 # Why: Scroll by a large number of JS layers """
+# TODO(crbug.com/878951): Remove after 'n_layer' tests below are fully landed.
 class JSScroll200LayerPage(ToughCompositorScrollPage):
   BASE_NAME = 'js_scroll_200_layer_grid'
   URL = 'http://jsbin.com/jevibahi/4/quiet?JS_SCROLL_200_LAYER_GRID'
@@ -93,3 +95,218 @@ class JSFullScreenPage(ToughCompositorWaitPage):
 class NewTilingsPage(ToughCompositorWaitPage):
   BASE_NAME = 'new_tilings'
   URL = 'http://jsbin.com/covoqi/1/quiet?NEW_TILINGS'
+
+
+# Why: CSS property update baseline """
+class CSSOpacityPlusNLayers0(ToughCompositorWaitPage):
+  BASE_NAME = 'css_opacity_plus_n_layers_0'
+  SUPPORTED_PLATFORMS = platforms.MOBILE_ONLY
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'css_opacity_plus_n_layers.html?layer_count=1&visible_layers=1')
+
+
+# Why: CSS property change on 1 layer with 50%-ile layer count """
+class CSSOpacityPlusNLayers50(ToughCompositorWaitPage):
+  BASE_NAME = 'css_opacity_plus_n_layers_50'
+  SUPPORTED_PLATFORMS = platforms.NO_PLATFORMS
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'css_opacity_plus_n_layers.html?layer_count=31&visible_layers=10')
+
+
+# Why: CSS property change on 1 layer with 75%-ile layer count """
+class CSSOpacityPlusNLayers75(ToughCompositorWaitPage):
+  BASE_NAME = 'css_opacity_plus_n_layers_75'
+  SUPPORTED_PLATFORMS = platforms.MOBILE_ONLY
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'css_opacity_plus_n_layers.html?layer_count=53&visible_layers=16')
+
+
+# Why: CSS property change on 1 layer with 95%-ile layer count """
+class CSSOpacityPlusNLayers95(ToughCompositorWaitPage):
+  BASE_NAME = 'css_opacity_plus_n_layers_95'
+  SUPPORTED_PLATFORMS = platforms.NO_PLATFORMS
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'css_opacity_plus_n_layers.html?layer_count=144&visible_layers=29')
+
+# Why: CSS property change on 1 layer with 99%-ile layer count """
+class CSSOpacityPlusNLayers99(ToughCompositorWaitPage):
+  BASE_NAME = 'css_opacity_plus_n_layers_99'
+  SUPPORTED_PLATFORMS = platforms.ALL_PLATFORMS
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'css_opacity_plus_n_layers.html?layer_count=306&visible_layers=46')
+
+
+# Why: JS driven CSS property change on 1 layer baseline """
+class JSOpacityPlusNLayers0(ToughCompositorWaitPage):
+  BASE_NAME = 'js_opacity_plus_n_layers_0'
+  SUPPORTED_PLATFORMS = platforms.MOBILE_ONLY
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'js_opacity_plus_n_layers.html?layer_count=1&visible_layers=1')
+
+
+# Why: JS driven property change on 1 layer with 50%-ile layer count """
+class JSOpacityPlusNLayers50(ToughCompositorWaitPage):
+  BASE_NAME = 'js_opacity_plus_n_layers_50'
+  SUPPORTED_PLATFORMS = platforms.NO_PLATFORMS
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'js_opacity_plus_n_layers.html?layer_count=31&visible_layers=10')
+
+
+# Why: JS driven property change on 1 layer with 75%-ile layer count """
+class JSOpacityPlusNLayers75(ToughCompositorWaitPage):
+  BASE_NAME = 'js_opacity_plus_n_layers_75'
+  SUPPORTED_PLATFORMS = platforms.MOBILE_ONLY
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'js_opacity_plus_n_layers.html?layer_count=53&visible_layers=16')
+
+
+# Why: JS driven property change on 1 layer with 95%-ile layer count """
+class JSOpacityPlusNLayers95(ToughCompositorWaitPage):
+  BASE_NAME = 'js_opacity_plus_n_layers_95'
+  SUPPORTED_PLATFORMS = platforms.NO_PLATFORMS
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'js_opacity_plus_n_layers.html?layer_count=144&visible_layers=29')
+
+
+# Why: JS driven property update with 99%-ile layer count """
+class JSOpacityPlusNLayers99(ToughCompositorWaitPage):
+  BASE_NAME = 'js_opacity_plus_n_layers_99'
+  SUPPORTED_PLATFORMS = platforms.ALL_PLATFORMS
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'js_opacity_plus_n_layers.html?layer_count=306&visible_layers=46')
+
+
+# Why: Painting 1 layer baseline """
+class JSPaintPlusNLayers0(ToughCompositorWaitPage):
+  BASE_NAME = 'js_paint_plus_n_layers_0'
+  SUPPORTED_PLATFORMS = platforms.MOBILE_ONLY
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'js_paint_plus_n_layers.html?layer_count=1&visible_layers=1')
+
+
+# Why: Painting 1 layer with 50%-ile layer count"""
+class JSPaintPlusNLayers50(ToughCompositorWaitPage):
+  BASE_NAME = 'js_paint_plus_n_layers_50'
+  SUPPORTED_PLATFORMS = platforms.NO_PLATFORMS
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'js_paint_plus_n_layers.html?layer_count=31&visible_layers=10')
+
+
+# Why: Painting 1 layer with 75%-ile layer count"""
+class JSPaintPlusNLayers75(ToughCompositorWaitPage):
+  BASE_NAME = 'js_paint_plus_n_layers_75'
+  SUPPORTED_PLATFORMS = platforms.MOBILE_ONLY
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'js_paint_plus_n_layers.html?layer_count=53&visible_layers=16')
+
+
+# Why: Painting 1 layer with 95%-ile layer count"""
+class JSPaintPlusNLayers95(ToughCompositorWaitPage):
+  BASE_NAME = 'js_paint_plus_n_layers_95'
+  SUPPORTED_PLATFORMS = platforms.NO_PLATFORMS
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'js_paint_plus_n_layers.html?layer_count=144&visible_layers=29')
+
+
+# Why: Painting 1 layer with 99%-ile layer count"""
+class JSPaintPlusNLayers99(ToughCompositorWaitPage):
+  BASE_NAME = 'js_paint_plus_n_layers_99'
+  SUPPORTED_PLATFORMS = platforms.ALL_PLATFORMS
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'js_paint_plus_n_layers.html?layer_count=306&visible_layers=46')
+
+
+class InfiniteScrollElementNLayersPage(ToughCompositorPage):
+  ABSTRACT_STORY = True
+  def RunPageInteractions(self, action_runner):
+    with action_runner.CreateGestureInteraction('ScrollAction'):
+      action_runner.ScrollElement(
+          selector='#container', speed_in_pixels_per_second=1000)
+
+
+# Why: Infinite root scroller with 1 layer baseline"""
+class InfiniteScrollElementNLayers0(InfiniteScrollElementNLayersPage):
+  BASE_NAME = 'infinite_scroll_element_n_layers_0'
+  SUPPORTED_PLATFORMS = platforms.MOBILE_ONLY
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'infinite_scroll_element_n_layers.html?layer_count=1')
+
+
+# Why: Infinite root scroller with 50%-ile layer count"""
+class InfiniteScrollElementNLayers50(InfiniteScrollElementNLayersPage):
+  BASE_NAME = 'infinite_scroll_element_n_layers_50'
+  SUPPORTED_PLATFORMS = platforms.NO_PLATFORMS
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'infinite_scroll_element_n_layers.html?layer_count=31')
+
+
+# Why: Infinite root scroller with 75%-ile layer count"""
+class InfiniteScrollElementNLayers75(InfiniteScrollElementNLayersPage):
+  BASE_NAME = 'infinite_scroll_element_n_layers_75'
+  SUPPORTED_PLATFORMS = platforms.MOBILE_ONLY
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'infinite_scroll_element_n_layers.html?layer_count=53')
+
+
+# Why: Infinite root scroller with 95%-ile layer count"""
+class InfiniteScrollElementNLayers95(InfiniteScrollElementNLayersPage):
+  BASE_NAME = 'infinite_scroll_element_n_layers_95'
+  SUPPORTED_PLATFORMS = platforms.NO_PLATFORMS
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'infinite_scroll_element_n_layers.html?layer_count=144')
+
+
+# Why: Infinite root scroller with 99%-ile layer count"""
+class InfiniteScrollElementNLayers99(InfiniteScrollElementNLayersPage):
+  BASE_NAME = 'infinite_scroll_element_n_layers_99'
+  SUPPORTED_PLATFORMS = platforms.ALL_PLATFORMS
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'infinite_scroll_element_n_layers.html?layer_count=306')
+
+
+class InfiniteScrollRootNLayersPage(ToughCompositorPage):
+  ABSTRACT_STORY = True
+  def RunPageInteractions(self, action_runner):
+    with action_runner.CreateGestureInteraction('ScrollAction'):
+      action_runner.ScrollPage(direction='down',
+                               speed_in_pixels_per_second=1000)
+
+
+# Why: Infinite non-root scroller with 1 layer baseline """
+class InfiniteScrollRootNLayers0(InfiniteScrollRootNLayersPage):
+  BASE_NAME = 'infinite_scroll_root_n_layers_0'
+  SUPPORTED_PLATFORMS = platforms.MOBILE_ONLY
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'infinite_scroll_root_n_layers.html?layer_count=1')
+
+
+# Why: Infinite non-root scroller with 50%-ile layer count"""
+class InfiniteScrollRootNLayers50(InfiniteScrollRootNLayersPage):
+  BASE_NAME = 'infinite_scroll_root_n_layers_50'
+  SUPPORTED_PLATFORMS = platforms.NO_PLATFORMS
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'infinite_scroll_root_n_layers.html?layer_count=31')
+
+
+# Why: Infinite non-root scroller with 75%-ile layer count"""
+class InfiniteScrollRootNLayers75(InfiniteScrollRootNLayersPage):
+  BASE_NAME = 'infinite_scroll_root_n_layers_75'
+  SUPPORTED_PLATFORMS = platforms.MOBILE_ONLY
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'infinite_scroll_root_n_layers.html?layer_count=53')
+
+
+# Why: Infinite non-root scroller with 95%-ile layer count"""
+class InfiniteScrollRootNLayers95(InfiniteScrollRootNLayersPage):
+  BASE_NAME = 'infinite_scroll_root_n_layers_95'
+  SUPPORTED_PLATFORMS = platforms.NO_PLATFORMS
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'infinite_scroll_root_n_layers.html?layer_count=144')
+
+
+# Why: Infinite non-root scroller with 99%-ile layer count"""
+class InfiniteScrollRootNLayers99(InfiniteScrollRootNLayersPage):
+  BASE_NAME = 'infinite_scroll_root_n_layers_99'
+  SUPPORTED_PLATFORMS = platforms.ALL_PLATFORMS
+  URL = ('file://../../../../chrome/test/data/perf/tough_compositor_cases/'
+         'infinite_scroll_root_n_layers.html?layer_count=306')
