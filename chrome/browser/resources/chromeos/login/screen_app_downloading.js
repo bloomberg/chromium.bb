d4bfb883848ea6bd1d167931aa80bddd60e5b125
Yue Cen <rsgingerrs@chromium.org>
2018-08-30 20:31:42 +0000

    Fast app reinstall: UI tweaks.
    Specs based on b/112928364 and b/112934458.
    
    Bug: 835029
    Change-Id: I58deccbbbd83a385407d02e208db3f112bed1fc6
    Reviewed-on: https://chromium-review.googlesource.com/1188594
    Commit-Queue: Yue Cen <rsgingerrs@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587742}
    Former-commit-id: 3d00d1e7a32edad2728b209419f6b129df97fb1f

--- chrome/browser/resources/chromeos/login/screen_app_downloading.js
+++ chrome/browser/resources/chromeos/login/screen_app_downloading.js
@@ -8,11 +8,22 @@
 
 login.createScreen('AppDownloadingScreen', 'app-downloading', function() {
   return {
+    EXTERNAL_API: ['updateNumberOfSelectedApps'],
+
     /**
      * Returns the control which should receive initial focus.
      */
     get defaultControl() {
       return $('app-downloading-screen');
+    },
+
+    /**
+     * Set the number of apps that are selected by the user. It is used in the
+     * screen title.
+     * @param numOfApps Number of apps selected by the user.
+     */
+    updateNumberOfSelectedApps: function(numOfApps) {
+      $('app-downloading-screen').numOfApps = numOfApps;
     },
   };
 });
