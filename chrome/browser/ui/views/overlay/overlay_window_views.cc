e7fbf21b4732348c1de383200da4aad54cbe61d2
Jennifer Apacible <apacible@chromium.org>
2018-08-30 21:43:43 +0000

    Fix styling.
    Change-Id: I29fd4a91e943ecbda810c1ae4085a5476896e3f8
    Reviewed-on: https://chromium-review.googlesource.com/1197288
    Reviewed-by: CJ DiMeglio <lethalantidote@chromium.org>
    Commit-Queue: apacible <apacible@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587789}
    Former-commit-id: 34268d7ae1f0562f145613be09664e63bcfa905b

--- chrome/browser/ui/views/overlay/overlay_window_views.cc
+++ chrome/browser/ui/views/overlay/overlay_window_views.cc
@@ -635,10 +635,10 @@ void OverlayWindowViews::OnKeyEvent(ui::KeyEvent* event) {
 
 void OverlayWindowViews::OnMouseEvent(ui::MouseEvent* event) {
   switch (event->type()) {
-    // Only show the media controls when the mouse is hovering over the window.
-    // This is checking for both ENTERED and MOVED because ENTERED is not fired
-    // after a resize on Windows.
-#if defined (OS_WIN)
+// Only show the media controls when the mouse is hovering over the window.
+// This is checking for both ENTERED and MOVED because ENTERED is not fired
+// after a resize on Windows.
+#if defined(OS_WIN)
     case ui::ET_MOUSE_MOVED:
 #endif  // OS_WIN
     case ui::ET_MOUSE_ENTERED:
