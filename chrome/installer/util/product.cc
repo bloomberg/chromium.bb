a47f3227cbbce0feafb110b2e9affdd06cb903c0
Greg Thompson <grt@chromium.org>
2018-08-30 16:04:06 +0000

    Replace Product::SetMsiMarker with DeleteRegistryValue.
    The latter is simpler, and allows for some code removal.
    
    BUG=none
    
    Change-Id: Ifaf2fd855e07c2c56cbc13920f17acd29055b9ed
    Reviewed-on: https://chromium-review.googlesource.com/1196233
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587604}
    Former-commit-id: ee17c1de9c7a5ad4e8f64d8975b4f4a9ed6293f4

--- chrome/installer/util/product.cc
+++ chrome/installer/util/product.cc
@@ -9,14 +9,10 @@
 #include "base/command_line.h"
 #include "base/logging.h"
 #include "base/process/launch.h"
-#include "base/win/registry.h"
 #include "chrome/installer/util/browser_distribution.h"
 #include "chrome/installer/util/chrome_browser_operations.h"
-#include "chrome/installer/util/google_update_constants.h"
 #include "chrome/installer/util/install_util.h"
 #include "chrome/installer/util/product_operations.h"
-
-using base::win::RegKey;
 
 namespace installer {
 
@@ -75,25 +71,6 @@ bool Product::LaunchChromeAndWait(const base::FilePath& application_path,
   }
 
   return success;
-}
-
-bool Product::SetMsiMarker(bool system_install, bool set) const {
-  HKEY reg_root = system_install ? HKEY_LOCAL_MACHINE : HKEY_CURRENT_USER;
-  RegKey client_state_key;
-  LONG result = client_state_key.Open(reg_root,
-                                      distribution_->GetStateKey().c_str(),
-                                      KEY_SET_VALUE | KEY_WOW64_32KEY);
-  if (result == ERROR_SUCCESS) {
-    result = client_state_key.WriteValue(google_update::kRegMSIField,
-                                         set ? 1 : 0);
-  }
-  if (result != ERROR_SUCCESS && result != ERROR_FILE_NOT_FOUND) {
-    LOG(ERROR)
-        << "Failed to Open or Write MSI value to client state key. error: "
-        << result;
-    return false;
-  }
-  return true;
 }
 
 void Product::AddKeyFiles(std::vector<base::FilePath>* key_files) const {
