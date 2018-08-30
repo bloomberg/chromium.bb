7f6cfca1bace881bf76268cb84aa756258e3ef25
Tommy Steimel <steimel@chromium.org>
2018-08-30 21:18:18 +0000

    Revert "[Media Controls] Reland Prevent ComputedStyleRef crash"
    This reverts commit 4357f77497c17ca5de0cb3d1bf5eccee25e26a5c [formerly 936898b90312b023c56c5e56b20183488cde89dc].
    
    Reason for revert: The null checks are not solving the crash. Must be a deeper issue where the style object is garbage
    
    Original change's description:
    > [Media Controls] Reland Prevent ComputedStyleRef crash
    > 
    > This CL re-adds the null checks for internal_button_ and
    > GetComputedStyle on the overlay play button. These were added to
    > prevent a crash, then removed when the crash was not fixed and replaced
    > with a different null check. However, it looks like we actually need
    > these too, so we're adding them back.
    > 
    > Bug: 870490
    > Change-Id: I1c6ad4b3f1dc36c7a2149a06c4f42f8ca00fb589
    > Reviewed-on: https://chromium-review.googlesource.com/1187710
    > Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    > Commit-Queue: Tommy Steimel <steimel@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#585719}
    
    TBR=beccahughes@chromium.org,steimel@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 870490
    Change-Id: I93d49a3a2071ce5fcca99b68f8cbaf2c3720f889
    Reviewed-on: https://chromium-review.googlesource.com/1197346
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587768}
    Former-commit-id: d67cfae6584af393889390ddd0fe5a3948bb704b

--- third_party/blink/renderer/modules/media_controls/elements/media_control_overlay_play_button_element_test.cc
+++ third_party/blink/renderer/modules/media_controls/elements/media_control_overlay_play_button_element_test.cc
@@ -7,7 +7,6 @@
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/core/css/css_property_value_set.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
-#include "third_party/blink/renderer/core/dom/node_computed_style.h"
 #include "third_party/blink/renderer/core/event_type_names.h"
 #include "third_party/blink/renderer/core/events/mouse_event.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
@@ -19,28 +18,11 @@
 
 namespace blink {
 
-class MediaControlOverlayPlayButtonElementTest
-    : public PageTestBase,
-      private ScopedModernMediaControlsForTest {
+class MediaControlOverlayPlayButtonElementTest : public PageTestBase {
  public:
-  MediaControlOverlayPlayButtonElementTest()
-      : ScopedModernMediaControlsForTest(true) {}
-
   void SetUp() final {
-    // Create page with video element with controls.
+    // Create page and instance of AnimatedArrow to run tests on.
     PageTestBase::SetUp();
-    HTMLVideoElement* media_element = HTMLVideoElement::Create(GetDocument());
-    media_element->SetBooleanAttribute(HTMLNames::controlsAttr, true);
-    GetDocument().body()->AppendChild(media_element);
-
-    // Create instance of MediaControlOverlayPlayButtonElement for tests.
-    MediaControlsImpl* media_controls =
-        static_cast<MediaControlsImpl*>(media_element->GetMediaControls());
-    ASSERT_NE(nullptr, media_controls);
-    overlay_play_button_ =
-        new MediaControlOverlayPlayButtonElement(*media_controls);
-
-    // Create instance of AnimatedArrow to run tests on.
     arrow_element_ = new MediaControlOverlayPlayButtonElement::AnimatedArrow(
         "test", GetDocument());
     GetDocument().body()->AppendChild(arrow_element_);
@@ -64,16 +46,6 @@ class MediaControlOverlayPlayButtonElementTest
   void SimulateAnimationIteration() {
     Event* event = Event::Create(EventTypeNames::animationiteration);
     GetElementById("arrow-3")->DispatchEvent(*event);
-  }
-
-  void RemoveInternalButton() {
-    overlay_play_button_->internal_button_ = nullptr;
-  }
-
-  void EnsureNoComputedStyle() {
-    // Due to the nature of the test setup, we already get a null computedstyle
-    // for free :). Just make sure it's actually not there.
-    ASSERT_EQ(nullptr, overlay_play_button_->GetComputedStyle());
   }
 
   Document& CreateTestDocumentWithBody() {
@@ -148,19 +120,8 @@ TEST_F(MediaControlOverlayPlayButtonElementTest, ShowIncrementsCounter) {
 }
 
 TEST_F(MediaControlOverlayPlayButtonElementTest,
-       KeepEventInNodeWithoutInternalButtonDoesntCrash) {
-  RemoveInternalButton();
-  SimulateKeepEventInNode();
-}
-
-TEST_F(MediaControlOverlayPlayButtonElementTest,
-       KeepEventInNodeWithoutComputedStyleDoesntCrash) {
-  EnsureNoComputedStyle();
-  SimulateKeepEventInNode();
-}
-
-TEST_F(MediaControlOverlayPlayButtonElementTest,
        KeepEventInNodeWithoutLayoutViewDoesntCrash) {
+  ScopedModernMediaControlsForTest enable_modern_media_controls(true);
   Document& document_without_layout_view = CreateTestDocumentWithBody();
   CreateTestOverlayPlayButton(document_without_layout_view);
   SimulateKeepEventInNode();
