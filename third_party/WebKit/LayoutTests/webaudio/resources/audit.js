1cc6b3ac2c4f13ccc3f37c8fe883354032796cf1
Hongchan Choi <hongchan@chromium.org>
2018-08-30 18:07:16 +0000

    Fix the error catching logic of should().throw() in audit.js
    Context: https://github.com/web-platform-tests/wpt/pull/12606
    
    Change the logic of should.throw() so we can handle 3 cases -
    
    should(someExpression).throw();
    should(someExpression).throw(TypeError);
    should(someExpression).throw(DOMException, 'NotSupportedError');
    
    The generic error (except for DOMException) can be passed without
    the second argument, but this change will enforce the second arg
    when the expected error is a DOMException type.
    
    This touches many test files, so the work will be done in several
    steps:
    
    1. Change audit.js, audionodeoptions.js on both locations.
      (wpt, non-wpt)
    2. Update affected test files with the script.
    3. Update the rest of test files which can't be updated
      programmatically.
    
    Bug: 865614
    Test: All layout tests pass.
    Change-Id: I16acacb26c194a0ff950aca05e931195bf55167f
    Reviewed-on: https://chromium-review.googlesource.com/1184146
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587660}
    Former-commit-id: 02a673df348e86553b50467a7dde1afce427b713

--- third_party/WebKit/LayoutTests/webaudio/resources/audit.js
+++ third_party/WebKit/LayoutTests/webaudio/resources/audit.js
@@ -267,18 +267,24 @@ window.Audit = (function() {
 
     /**
      * Check if |actual| operation wrapped in a function throws an exception
-     * with a expected error type correctly. |expected| is optional.
+     * with a expected error type correctly. |expected| is optional. If it is an
+     * instance of DOMException, then the description can be provided to be more
+     * strict about the exception type. |expected| also can be other generic
+     * error types.
      *
      * @example
      *   should(() => { let a = b; }, 'A bad code').throw();
-     *   should(() => { let c = d; }, 'Assigning d to c.')
-     *       .throw('ReferenceError');
-     *   should(() => { let e = f; }, 'Assigning e to f.')
-     *       .throw('ReferenceError', { omitErrorMessage: true });
+     *   should(() => { new SomeConstructor(); }, 'A bad construction')
+     *       .throw(DOMException, 'NotSupportedError');
+     *   should(() => { let c = d; }, 'Assigning d to c')
+     *       .throw(ReferenceError);
+     *   should(() => { let e = f; }, 'Assigning e to f')
+     *       .throw(ReferenceError, { omitErrorMessage: true });
      *
      * @result
      *   "PASS   A bad code threw an exception of ReferenceError: b is not
      *       defined."
+     *   "PASS   A bad construction threw NotSupportedError."
      *   "PASS   Assigning d to c threw ReferenceError: d is not defined."
      *   "PASS   Assigning e to f threw ReferenceError: [error message
      *       omitted]."
@@ -303,10 +309,16 @@ window.Audit = (function() {
           // The expected error type was not given.
           didThrowCorrectly = true;
           passDetail = '${actual} threw ' + error.name + errorMessage + '.';
-        } else if (error.name === this._expected) {
-          // The expected error type match the actual one.
+        } else if (this._expected === DOMException &&
+                   (this._expectedDescription === undefined ||
+                    this._expectedDescription === error.name)) {
+          // Handles DOMException with the associated name.
           didThrowCorrectly = true;
           passDetail = '${actual} threw ${expected}' + errorMessage + '.';
+        } else if (this._expected == error.constructor) {
+          // Handler other error types.
+          didThrowCorrectly = true;
+          passDetail = '${actual} threw ' + error.name + errorMessage + '.';
         } else {
           didThrowCorrectly = false;
           failDetail =
