1186b64007e69662df68fbd28005fd6449055e44
Yi Su <mrsuyi@chromium.org>
2018-08-30 07:47:21 +0000

    Use ImageFetchTabHelper in ImageCopier
    Now that ImageFetchTabHelper supports both fetching image data by JavaScript
    and by downloading via image_fetcher::IOSImageDataFetcherWrapper, we can use
    that to improve ImageCopier.
    
    Bug: 163201
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I8f3c1ad411f74f23b31948f851a1226e9bbe30a6
    Reviewed-on: https://chromium-review.googlesource.com/1188581
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587474}
    Former-commit-id: 4fb7704f74b7f897e119db71c4ede81878c428b3

--- ios/chrome/browser/ui/image_util/image_copier.h
+++ ios/chrome/browser/ui/image_util/image_copier.h
@@ -7,35 +7,25 @@
 
 #import <UIKit/UIKit.h>
 
-#include "components/image_fetcher/core/request_metadata.h"
+class GURL;
 
-typedef int64_t ImageCopierSessionID;
+namespace web {
+struct Referrer;
+class WebState;
+}
 
 // Object copying images to the system's pasteboard.
-// TODO(crbug.com/163201): Current implementation won't terminate the
-// downloading process when user clicks "Cancel", because
-// IOSImageDataFetcherWrapper doesn't provide such API. And a progress bar is
-// preferred for this, but no API for that either. Future steps would be using
-// NSURLSessionTask to provide functionality of reporting progress.
 @interface ImageCopier : NSObject
 
 // Init the ImageCopier with a |baseViewController| used to display alerts.
 - (instancetype)initWithBaseViewController:
     (UIViewController*)baseViewController;
 
-// When user taps "Copy Image", downloader should call |beginSession| and get
-// an ID for the session. That ID will also be recorded by ImageCopier, and may
-// get erased if user cancels the copy, or overridden if user starts another
-// copy. When download is finished, downloader should call
-// |endSession:withImageData:| with that ID, and ImageCopier will compare this
-// ID with its internal ID to decide whether to paste or discard the image data.
-
-// Begin the download session, and return an ID for this session.
-- (ImageCopierSessionID)beginSession;
-
-// End the download session. The image data won't be copied if the user has
-// canceled copying.
-- (void)endSession:(ImageCopierSessionID)sessionID withImageData:(NSData*)data;
+// Copies the image at |url|. |web_state| is used for fetching image data by
+// JavaScript. |referrer| is used for download.
+- (void)copyImageAtURL:(const GURL&)url
+              referrer:(const web::Referrer&)referrer
+              webState:(web::WebState*)webState;
 
 @end
 
