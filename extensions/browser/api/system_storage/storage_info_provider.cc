b02a77b105422fb2303f24037d195b2674f9ebc1
Etienne Pierre-doray <etiennep@chromium.org>
2018-08-30 18:58:59 +0000

    Remove unnecessary AssertBlockingAllowed() in /extensions/browser/api/system_storage.
    Original CL intention was:
    This CL uses ScopedBlockingCall to mark blocking calls in /extensions/browser/api/system_storage.
    
    This CL was created by replacing calls to AssertBlockingAllowed()
    with instantiations of ScopedBlockingCall(MAY_BLOCK).
    I kindly ask the reviewer to make sure of the following:
      - ScopedBlockingCall is instantiated in a scope with minimal CPU usage.
        If this is not the case, ScopedBlockingCall should be instantiated
        closer to the blocking call. See scoped_blocking_call.h for more
        info. Please let me know when/where the blocking call happens if this needs
        to be changed.
      - Parameter |blocking_type| matches expectation (MAY_BLOCK/WILL_BLOCK). See
        BlockingType for more info. While I assumed MAY_BLOCK by default, that might
        not be the best fit if we know that this callsite is guaranteed to block.
      - The ScopedBlockingCall's scope covers the entirety of the blocking operation
        previously asserted against by the AssertBlockingAllowed().
    
    This CL was uploaded by git cl split.
    
    R=ningxin.hu@intel.com
    
    Bug: 874080
    Change-Id: Ia8cf19506aa212d07e8dad54a07940f6e878799c
    Reviewed-on: https://chromium-review.googlesource.com/1191195
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587683}
    Former-commit-id: 146f618a15dd0111074f181d791ee1363a88569c

--- extensions/browser/api/system_storage/storage_info_provider.cc
+++ extensions/browser/api/system_storage/storage_info_provider.cc
@@ -67,7 +67,6 @@ void StorageInfoProvider::InitializeProvider(
 }
 
 bool StorageInfoProvider::QueryInfo() {
-  base::AssertBlockingAllowed();
   // No info to query since we get all available storage devices' info in
   // |PrepareQueryOnUIThread()|.
   return true;
