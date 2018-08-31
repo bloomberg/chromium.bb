99edd923578672b86fbc6771a598b175f8cd9caa
Marc Treib <treib@chromium.org>
2018-08-31 09:47:24 +0000

    SyncAuthManager: Add unit tests for non-primary-account support
    https://chromium-review.googlesource.com/c/chromium/src/+/1162174 added
    support for non-primary accounts (behind a feature flag), but didn't have
    any tests yet.
    
    Bug: 871221
    Change-Id: I5de0b688e1aff9d8eef141d341b9e711511f31e4
    Reviewed-on: https://chromium-review.googlesource.com/1195488
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587976}
    Former-commit-id: 4fc972dc6609d154a3778e3c33bee6972bf10887

--- components/browser_sync/sync_auth_manager_unittest.cc
+++ components/browser_sync/sync_auth_manager_unittest.cc
@@ -7,9 +7,11 @@
 #include "base/bind_helpers.h"
 #include "base/run_loop.h"
 #include "base/test/mock_callback.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/scoped_task_environment.h"
 #include "build/build_config.h"
 #include "components/sync/base/sync_prefs.h"
+#include "components/sync/driver/sync_driver_switches.h"
 #include "components/sync/engine/connection_status.h"
 #include "components/sync/engine/sync_credentials.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
@@ -29,7 +31,8 @@ class SyncAuthManagerTest : public testing::Test {
   using CredentialsChangedCallback =
       SyncAuthManager::CredentialsChangedCallback;
 
-  SyncAuthManagerTest() {
+  SyncAuthManagerTest()
+      : identity_env_(/*use_fake_url_loader_for_gaia_cookie_manager=*/true) {
     syncer::SyncPrefs::RegisterProfilePrefs(pref_service_.registry());
     sync_prefs_ = std::make_unique<syncer::SyncPrefs>(&pref_service_);
   }
@@ -521,6 +524,178 @@ TEST_F(SyncAuthManagerTest, ClearsCredentialsOnInvalidRefreshToken) {
   // No new access token should have been requested. Since the request goes
   // through posted tasks, we have to spin the message loop.
   base::RunLoop().RunUntilIdle();
+}
+
+TEST_F(SyncAuthManagerTest, IgnoresCookieJarIfFeatureDisabled) {
+  base::test::ScopedFeatureList features;
+  features.InitAndDisableFeature(switches::kSyncSupportSecondaryAccount);
+
+  auto auth_manager = CreateAuthManager();
+  auth_manager->RegisterForAuthNotifications();
+
+  ASSERT_TRUE(
+      auth_manager->GetActiveAccountInfo().account_info.account_id.empty());
+
+  // Make a non-primary account available with both a refresh token and cookie.
+  AccountInfo account_info =
+      identity_env()->MakeAccountAvailable("test@email.com");
+  identity_env()->SetCookieAccounts({{account_info.email, account_info.gaia}});
+
+  // Since secondary account support is disabled, this should have no effect.
+  EXPECT_TRUE(
+      auth_manager->GetActiveAccountInfo().account_info.account_id.empty());
+}
+
+TEST_F(SyncAuthManagerTest, UsesCookieJarIfFeatureEnabled) {
+  base::test::ScopedFeatureList features;
+  features.InitWithFeatures(
+      /*enabled_features=*/{switches::kSyncStandaloneTransport,
+                            switches::kSyncSupportSecondaryAccount},
+      /*disabled_features=*/{});
+
+  auto auth_manager = CreateAuthManager();
+  auth_manager->RegisterForAuthNotifications();
+
+  ASSERT_TRUE(
+      auth_manager->GetActiveAccountInfo().account_info.account_id.empty());
+
+  // Make a non-primary account available with both a refresh token and cookie.
+  AccountInfo account_info =
+      identity_env()->MakeAccountAvailable("test@email.com");
+  identity_env()->SetCookieAccounts({{account_info.email, account_info.gaia}});
+
+  // Since secondary account support is enabled, SyncAuthManager should have
+  // picked up this account
+  EXPECT_EQ(auth_manager->GetActiveAccountInfo().account_info.account_id,
+            account_info.account_id);
+}
+
+TEST_F(SyncAuthManagerTest, DropsAccountWhenCookieGoesAway) {
+  base::test::ScopedFeatureList features;
+  features.InitWithFeatures(
+      /*enabled_features=*/{switches::kSyncStandaloneTransport,
+                            switches::kSyncSupportSecondaryAccount},
+      /*disabled_features=*/{});
+
+  auto auth_manager = CreateAuthManager();
+  auth_manager->RegisterForAuthNotifications();
+
+  // Make a non-primary account available with both a refresh token and cookie.
+  AccountInfo account_info =
+      identity_env()->MakeAccountAvailable("test@email.com");
+  identity_env()->SetCookieAccounts({{account_info.email, account_info.gaia}});
+  ASSERT_EQ(auth_manager->GetActiveAccountInfo().account_info.account_id,
+            account_info.account_id);
+
+  // If the cookie goes away, we're not using the account anymore, even though
+  // we still have a refresh token.
+  identity_env()->SetCookieAccounts({});
+  EXPECT_TRUE(
+      auth_manager->GetActiveAccountInfo().account_info.account_id.empty());
+
+  // Once the cookie comes back, we can use the account again.
+  identity_env()->SetCookieAccounts({{account_info.email, account_info.gaia}});
+  EXPECT_EQ(auth_manager->GetActiveAccountInfo().account_info.account_id,
+            account_info.account_id);
+}
+
+TEST_F(SyncAuthManagerTest, DropsAccountWhenRefreshTokenGoesAway) {
+  base::test::ScopedFeatureList features;
+  features.InitWithFeatures(
+      /*enabled_features=*/{switches::kSyncStandaloneTransport,
+                            switches::kSyncSupportSecondaryAccount},
+      /*disabled_features=*/{});
+
+  auto auth_manager = CreateAuthManager();
+  auth_manager->RegisterForAuthNotifications();
+
+  // Make a non-primary account available with both a refresh token and cookie.
+  AccountInfo account_info =
+      identity_env()->MakeAccountAvailable("test@email.com");
+  identity_env()->SetCookieAccounts({{account_info.email, account_info.gaia}});
+  ASSERT_EQ(auth_manager->GetActiveAccountInfo().account_info.account_id,
+            account_info.account_id);
+
+  // If the refresh token goes away, we're not using the account anymore, even
+  // though the cookie is still there.
+  identity_env()->RemoveRefreshTokenForAccount(account_info.account_id);
+  EXPECT_TRUE(
+      auth_manager->GetActiveAccountInfo().account_info.account_id.empty());
+
+  // Once the refresh token comes back, we can use the account again.
+  identity_env()->SetRefreshTokenForAccount(account_info.account_id);
+  EXPECT_EQ(auth_manager->GetActiveAccountInfo().account_info.account_id,
+            account_info.account_id);
+}
+
+TEST_F(SyncAuthManagerTest, PrefersPrimaryAccountOverCookie) {
+  base::test::ScopedFeatureList features;
+  features.InitWithFeatures(
+      /*enabled_features=*/{switches::kSyncStandaloneTransport,
+                            switches::kSyncSupportSecondaryAccount},
+      /*disabled_features=*/{});
+
+  auto auth_manager = CreateAuthManager();
+  auth_manager->RegisterForAuthNotifications();
+
+  ASSERT_TRUE(
+      auth_manager->GetActiveAccountInfo().account_info.account_id.empty());
+
+  // Make a non-primary account available with both a refresh token and cookie.
+  AccountInfo secondary_account_info =
+      identity_env()->MakeAccountAvailable("test@email.com");
+  identity_env()->SetCookieAccounts(
+      {{secondary_account_info.email, secondary_account_info.gaia}});
+  ASSERT_EQ(auth_manager->GetActiveAccountInfo().account_info.account_id,
+            secondary_account_info.account_id);
+
+  // Once a primary account becomes available, that one is preferred over the
+  // one from the cookie.
+  AccountInfo primary_account_info =
+      identity_env()->MakePrimaryAccountAvailable("primary@email.com");
+  EXPECT_EQ(auth_manager->GetActiveAccountInfo().account_info.account_id,
+            primary_account_info.account_id);
+}
+
+TEST_F(SyncAuthManagerTest, OnlyUsesFirstCookieAccount) {
+  base::test::ScopedFeatureList features;
+  features.InitWithFeatures(
+      /*enabled_features=*/{switches::kSyncStandaloneTransport,
+                            switches::kSyncSupportSecondaryAccount},
+      /*disabled_features=*/{});
+
+  auto auth_manager = CreateAuthManager();
+  auth_manager->RegisterForAuthNotifications();
+
+  ASSERT_TRUE(
+      auth_manager->GetActiveAccountInfo().account_info.account_id.empty());
+
+  // Make two non-primary accounts available with both refresh token and cookie.
+  AccountInfo account_info1 =
+      identity_env()->MakeAccountAvailable("test1@email.com");
+  AccountInfo account_info2 =
+      identity_env()->MakeAccountAvailable("test2@email.com");
+  identity_env()->SetCookieAccounts(
+      {{account_info1.email, account_info1.gaia},
+       {account_info2.email, account_info2.gaia}});
+
+  // SyncAuthManager should have picked up the first account.
+  EXPECT_EQ(auth_manager->GetActiveAccountInfo().account_info.account_id,
+            account_info1.account_id);
+
+  // If the order of the accounts in the cookie changes, then SyncAuthManager
+  // should move to the other (now-first) account.
+  identity_env()->SetCookieAccounts(
+      {{account_info2.email, account_info2.gaia},
+       {account_info1.email, account_info1.gaia}});
+  EXPECT_EQ(auth_manager->GetActiveAccountInfo().account_info.account_id,
+            account_info2.account_id);
+
+  // If the refresh token for this account goes away, there should be no active
+  // account anymore - we should *not* fall back to the second cookie account.
+  identity_env()->RemoveRefreshTokenForAccount(account_info2.account_id);
+  EXPECT_TRUE(
+      auth_manager->GetActiveAccountInfo().account_info.account_id.empty());
 }
 
 }  // namespace
