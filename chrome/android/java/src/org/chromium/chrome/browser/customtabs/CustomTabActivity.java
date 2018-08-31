945287c8f466eb7a0a2e4fc6016e60f37c4398ae
arthursonzogni <arthursonzogni@chromium.org>
2018-08-31 12:13:00 +0000

    Remove SeparateTaskcustomTabActivity for Android version > L.
    Make SeparateTaskcustomTabActivity to be used only for Android version
    pre L. The part used for Android version >= L is merged into
    CustomTabActivity.
    
    This is a prerequisite for skipping the ChromeLauncherActivity and
    directly launch the CustomTabActivity.
    
    Bug: 681559
    Change-Id: Ifc33e25486bf70cb4fb25c15e6c33e16d6683157
    Reviewed-on: https://chromium-review.googlesource.com/1188565
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587995}
    Former-commit-id: aa76bac1230fc0b510d0299a5b51b6c2bdc86682

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -15,6 +15,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.os.SystemClock;
@@ -46,6 +47,7 @@ import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ActivityTabTaskDescriptionHelper;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.ChromeSwitches;
@@ -194,6 +196,19 @@ public class CustomTabActivity extends ChromeActivity {
     private boolean mModuleOnStartPending;
     private boolean mModuleOnResumePending;
     private boolean mHasSetOverlayView;
+
+    private ActivityTabTaskDescriptionHelper mTaskDescriptionHelper;
+
+    /**
+     * Return true when the activity has been launched in a separate task. The default behavior is
+     * to reuse the same task and put the activity on top of the previous one (i.e hiding it). A
+     * separate task creates a new entry in the Android recent screen.
+     **/
+    private boolean useSeparateTask() {
+        final int separateTaskFlags =
+                Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_NEW_DOCUMENT;
+        return (getIntent().getFlags() & separateTaskFlags) != 0;
+    }
 
     private TabModelObserver mCloseActivityWhenEmptyTabModelObserver = new EmptyTabModelObserver() {
         @Override
@@ -668,6 +683,11 @@ public class CustomTabActivity extends ChromeActivity {
             mTrustedWebActivityUi.initialShowSnackbarIfNeeded();
         }
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && useSeparateTask()) {
+            mTaskDescriptionHelper = new ActivityTabTaskDescriptionHelper(this,
+                    ApiCompatibilityUtils.getColor(getResources(), R.color.default_primary_color));
+        }
+
         super.finishNativeInitialization();
     }
 
@@ -910,6 +930,8 @@ public class CustomTabActivity extends ChromeActivity {
         if (mIncognitoTabHost != null) {
             IncognitoTabHostRegistry.getInstance().unregister(mIncognitoTabHost);
         }
+
+        if (mTaskDescriptionHelper != null) mTaskDescriptionHelper.destroy();
     }
 
     @Override
@@ -1079,8 +1101,11 @@ public class CustomTabActivity extends ChromeActivity {
      * recents.
      */
     protected void handleFinishAndClose() {
-        // When on top of another app, finish is all that is required.
-        finish();
+        if (useSeparateTask()) {
+            ApiCompatibilityUtils.finishAndRemoveTask(this);
+        } else {
+            finish();
+        }
     }
 
     @Override
