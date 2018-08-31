0ff8fed15479163a4530bc5c6c025c766f5180a8
Wenzhao Zang <wzang@chromium.org>
2018-08-31 01:19:38 +0000

    cros: Disable new wallpaper picker for M69
    The plan is to ship all tablet features together in M70. This CL will
    be merged to M69 and reverted in M70.
    
    Bug: 800945
    Change-Id: I8ca2adb4b8b0c1715d7229f48bb521d0dd3ed35b
    Reviewed-on: https://chromium-review.googlesource.com/1198523
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#587898}
    Former-commit-id: b3b25596c17db65441cfbc343ba95ee5d5b37588

--- ash/public/cpp/ash_features.cc
+++ ash/public/cpp/ash_features.cc
@@ -36,7 +36,7 @@ const base::Feature kLockScreenHideSensitiveNotificationsSupport{
     base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kNewWallpaperPicker{"NewWallpaperPicker",
-                                        base::FEATURE_ENABLED_BY_DEFAULT};
+                                        base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kNightLight{"NightLight", base::FEATURE_ENABLED_BY_DEFAULT};
 
