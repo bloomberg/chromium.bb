65ded62dbaaacbdb41f53a02d55a7b6c3ce639d3
Yue Li <updowndota@chromium.org>
2018-08-31 16:25:03 +0000

    Migrate Assistant opt-in flow into CrOS OOBE
    Bug: b/78190629
    Test: Manual Test
    Change-Id: I3cb433b5c688309453489df85d43657e2a19339b
    Reviewed-on: https://chromium-review.googlesource.com/1195662
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588053}
    Former-commit-id: 90766e309d4081be2b09c1838b0f407bd75c7ff2

--- chrome/browser/resources/chromeos/login/login_non_lock_shared.js
+++ chrome/browser/resources/chromeos/login/login_non_lock_shared.js
@@ -15,6 +15,7 @@
 // <include src="oobe_screen_user_image.js">
 // <include src="oobe_screen_voice_interaction_value_prop.js">
 // <include src="oobe_screen_wait_for_container_ready.js">
+// <include src="oobe_screen_assistant_optin_flow.js">
 // <include src="oobe_select.js">
 
 // <include src="screen_app_launch_splash.js">
