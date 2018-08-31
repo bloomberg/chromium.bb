dcb7e31ff81c212ca5a9e1db19f4da922d9c64a2
Xiangjun Zhang <xjz@chromium.org>
2018-08-31 21:07:38 +0000

    Mirroring Service: Supports OffscreenTab mirroring.
    This CL adds the implementation to open a new OffscreenTab to support
    OffscreenTab mirroring through the mirroring service.
    
    Bug: 734672
    Change-Id: I560e39d74b0e1a147ffee2dfccf0b38ae8ed7fae
    Reviewed-on: https://chromium-review.googlesource.com/1195820
    Commit-Queue: Xiangjun Zhang <xjz@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588166}
    Former-commit-id: 4f6da91d97a023e8fb6c21ba5ed3385a7c6c9b26

--- chrome/browser/media/cast_mirroring_service_host.h
+++ chrome/browser/media/cast_mirroring_service_host.h
@@ -6,13 +6,22 @@
 #define CHROME_BROWSER_MEDIA_CAST_MIRRORING_SERVICE_HOST_H_
 
 #include "base/macros.h"
+#include "build/build_config.h"
 #include "components/mirroring/mojom/mirroring_service_host.mojom.h"
 #include "components/mirroring/mojom/resource_provider.mojom.h"
 #include "content/public/browser/desktop_media_id.h"
 #include "content/public/browser/web_contents_observer.h"
+#include "extensions/buildflags/buildflags.h"
+
+// TODO(https://crbug.com/879012): Remove the build flag. OffscreenTab should
+// not only be defined when extension is enabled.
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+#include "chrome/browser/media/offscreen_tab.h"
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 namespace content {
 class AudioLoopbackStreamCreator;
+class BrowserContext;
 class WebContents;
 }  // namespace content
 
@@ -24,6 +33,9 @@ namespace mirroring {
 // TODO(xjz): Adds the implementation to connect to Mirroring Service.
 class CastMirroringServiceHost final : public mojom::MirroringServiceHost,
                                        public mojom::ResourceProvider,
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+                                       public OffscreenTab::Owner,
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
                                        public content::WebContentsObserver {
  public:
   static void GetForTab(content::WebContents* target_contents,
@@ -32,6 +44,11 @@ class CastMirroringServiceHost final : public mojom::MirroringServiceHost,
   static void GetForDesktop(content::WebContents* initiator_contents,
                             const std::string& desktop_stream_id,
                             mojom::MirroringServiceHostRequest request);
+
+  static void GetForOffscreenTab(content::BrowserContext* context,
+                                 const GURL& presentation_url,
+                                 const std::string& presentation_id,
+                                 mojom::MirroringServiceHostRequest request);
 
   // |source_media_id| indicates the mirroring source.
   explicit CastMirroringServiceHost(content::DesktopMediaID source_media_id);
@@ -62,10 +79,27 @@ class CastMirroringServiceHost final : public mojom::MirroringServiceHost,
   // content::WebContentsObserver implementation.
   void WebContentsDestroyed() override;
 
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+  // OffscreenTab::Owner implementation.
+  void RequestMediaAccessPermission(
+      const content::MediaStreamRequest& request,
+      content::MediaResponseCallback callback) override;
+  void DestroyTab(OffscreenTab* tab) override;
+
+  // Creates and starts a new OffscreenTab.
+  void OpenOffscreenTab(content::BrowserContext* context,
+                        const GURL& presentation_url,
+                        const std::string& presentation_id);
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
+
   // Describes the media source for this mirroring session.
-  const content::DesktopMediaID source_media_id_;
+  content::DesktopMediaID source_media_id_;
 
   std::unique_ptr<content::AudioLoopbackStreamCreator> audio_stream_creator_;
+
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+  std::unique_ptr<OffscreenTab> offscreen_tab_;
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
   DISALLOW_COPY_AND_ASSIGN(CastMirroringServiceHost);
 };
