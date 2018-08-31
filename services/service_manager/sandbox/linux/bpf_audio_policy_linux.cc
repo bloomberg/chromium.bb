ff9132d156f6c21f80d3008d8bc5952a4c65b6d6
Marina Ciocea <marinaciocea@chromium.org>
2018-08-31 18:55:36 +0000

    Sandbox audio service on Linux.
    Run audio service process in sandboxed mode on Linux. Sandbox is enabled behind finch experiment flag:
    --enable-features=AudioServiceStreams,AudioServiceOutOfProcess,AudioServiceSandbox
    
    TBR=mpdenton@chromium.org, palmer@chromium.org, tsepez@chromium.com
    
    
    Bug: 850878
    Change-Id: Iba6b84adc8c4a62ac4c20d5b36aa4f21b36a1574
    Reviewed-on: https://chromium-review.googlesource.com/1160844
    Commit-Queue: Marina Ciocea <marinaciocea@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588100}
    Former-commit-id: 2ea45c062c0d59ed81ec5f185a23ffb39c34ff03

---
+++ services/service_manager/sandbox/linux/bpf_audio_policy_linux.cc
@@ -0,0 +1,102 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/service_manager/sandbox/linux/bpf_audio_policy_linux.h"
+
+#include <sys/socket.h>
+
+#include "sandbox/linux/bpf_dsl/bpf_dsl.h"
+#include "sandbox/linux/seccomp-bpf-helpers/syscall_parameters_restrictions.h"
+#include "sandbox/linux/seccomp-bpf-helpers/syscall_sets.h"
+#include "sandbox/linux/syscall_broker/broker_process.h"
+#include "sandbox/linux/system_headers/linux_futex.h"
+#include "sandbox/linux/system_headers/linux_syscalls.h"
+#include "services/service_manager/sandbox/linux/sandbox_linux.h"
+
+using sandbox::bpf_dsl::Allow;
+using sandbox::bpf_dsl::Arg;
+using sandbox::bpf_dsl::Error;
+using sandbox::bpf_dsl::If;
+using sandbox::bpf_dsl::ResultExpr;
+using sandbox::bpf_dsl::Trap;
+using sandbox::syscall_broker::BrokerProcess;
+
+namespace service_manager {
+
+AudioProcessPolicy::AudioProcessPolicy() = default;
+
+AudioProcessPolicy::~AudioProcessPolicy() = default;
+
+ResultExpr AudioProcessPolicy::EvaluateSyscall(int system_call_number) const {
+  switch (system_call_number) {
+#if defined(__NR_connect)
+    case __NR_connect:
+#endif
+#if defined(__NR_ftruncate)
+    case __NR_ftruncate:
+#endif
+#if defined(__NR_getdents)
+    case __NR_getdents:
+#endif
+#if defined(__NR_getpeername)
+    case __NR_getpeername:
+#endif
+#if defined(__NR_getsockopt)
+    case __NR_getsockopt:
+#endif
+#if defined(__NR_getsockname)
+    case __NR_getsockname:
+#endif
+#if defined(__NR_ioctl)
+    case __NR_ioctl:
+#endif
+#if defined(__NR_memfd_create)
+    case __NR_memfd_create:
+#endif
+#if defined(__NR_sched_setscheduler)
+    case __NR_sched_setscheduler:
+#endif
+#if defined(__NR_setsockopt)
+    case __NR_setsockopt:
+#endif
+#if defined(__NR_uname)
+    case __NR_uname:
+#endif
+      return Allow();
+#if defined(__NR_futex)
+    case __NR_futex: {
+      const Arg<int> op(1);
+#if defined(USE_PULSEAUDIO)
+      return Switch(op & ~FUTEX_PRIVATE_FLAG)
+          .SANDBOX_BPF_DSL_CASES(
+              (FUTEX_CMP_REQUEUE, FUTEX_LOCK_PI, FUTEX_UNLOCK_PI, FUTEX_WAIT,
+               FUTEX_WAIT_BITSET, FUTEX_WAKE),
+              Allow())
+          .Default(Error(EPERM));
+#else
+      return sandbox::RestrictFutex();
+#endif
+    }
+#endif
+#if defined(__NR_socket)
+    case __NR_socket: {
+      const Arg<int> domain(0);
+      return If(domain == AF_UNIX, Allow()).Else(Error(EPERM));
+    }
+#endif
+    default:
+      if (sandbox::SyscallSets::IsSystemVSemaphores(system_call_number) ||
+          sandbox::SyscallSets::IsSystemVSharedMemory(system_call_number)) {
+        return Allow();
+      }
+
+      auto* broker_process = SandboxLinux::GetInstance()->broker_process();
+      if (broker_process->IsSyscallAllowed(system_call_number))
+        return Trap(BrokerProcess::SIGSYS_Handler, broker_process);
+
+      return BPFBasePolicy::EvaluateSyscall(system_call_number);
+  }
+}
+
+}  // namespace service_manager
