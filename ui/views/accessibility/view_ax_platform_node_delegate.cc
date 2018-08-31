b9823e28ed750c89fb55fde84d910ac6a2bcf02b
James Wallace-Lee <jamwalla@chromium.org>
2018-08-31 20:15:12 +0000

    a11y: hide the rest of the browser when a tab-modal dialog is present
    When a tab-modal dialog is shown, the page underneath it is inert. But
    NVDA reads the page underneath the dialog when users press Insert+B to
    read the whole dialog. To fix this, we hide the rest of the browser
    from the accessibility tree when a tab-modal dialog is visible by
    modifying AXPlatformNodeDelegate::PopulateChildWidgetVector to return
    only the dialog.
    
    Bug: 775680
    Change-Id: I2a5ec2eeef5ffe50ebd8819836c89ae73ddfdd36
    Reviewed-on: https://chromium-review.googlesource.com/1189054
    Commit-Queue: James Wallace-Lee <jamwalla@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588142}
    Former-commit-id: 9ce4948885ea3718cb2e8d150da06deb9911deef

--- ui/views/accessibility/view_ax_platform_node_delegate.cc
+++ ui/views/accessibility/view_ax_platform_node_delegate.cc
@@ -15,6 +15,7 @@
 #include "ui/views/controls/native/native_view_host.h"
 #include "ui/views/view.h"
 #include "ui/views/widget/widget.h"
+#include "ui/views/widget/widget_delegate.h"
 
 namespace views {
 
@@ -227,7 +228,12 @@ int ViewAXPlatformNodeDelegate::GetChildCount() {
   int child_count = view()->child_count();
 
   std::vector<Widget*> child_widgets;
-  PopulateChildWidgetVector(&child_widgets);
+  bool is_tab_modal_showing;
+  PopulateChildWidgetVector(&child_widgets, &is_tab_modal_showing);
+  if (is_tab_modal_showing) {
+    DCHECK_EQ(child_widgets.size(), 1ULL);
+    return 1;
+  }
   child_count += child_widgets.size();
 
   return child_count;
@@ -239,7 +245,16 @@ gfx::NativeViewAccessible ViewAXPlatformNodeDelegate::ChildAtIndex(int index) {
 
   // If this is a root view, our widget might have child widgets. Include
   std::vector<Widget*> child_widgets;
-  PopulateChildWidgetVector(&child_widgets);
+  bool is_tab_modal_showing;
+  PopulateChildWidgetVector(&child_widgets, &is_tab_modal_showing);
+
+  // If a visible tab modal dialog is present, ignore |index| and return the
+  // dialog.
+  if (is_tab_modal_showing) {
+    DCHECK_EQ(child_widgets.size(), 1ULL);
+    return child_widgets[0]->GetRootView()->GetNativeViewAccessible();
+  }
+
   int child_widget_count = static_cast<int>(child_widgets.size());
 
   if (index < view()->child_count()) {
@@ -290,7 +305,7 @@ gfx::NativeViewAccessible ViewAXPlatformNodeDelegate::HitTestSync(int x,
 
   // Search child widgets first, since they're on top in the z-order.
   std::vector<Widget*> child_widgets;
-  PopulateChildWidgetVector(&child_widgets);
+  PopulateChildWidgetVector(&child_widgets, nullptr);
   for (Widget* child_widget : child_widgets) {
     View* child_root_view = child_widget->GetRootView();
     gfx::Point point(x, y);
@@ -358,13 +373,20 @@ const ui::AXUniqueId& ViewAXPlatformNodeDelegate::GetUniqueId() const {
 }
 
 void ViewAXPlatformNodeDelegate::PopulateChildWidgetVector(
-    std::vector<Widget*>* result_child_widgets) {
+    std::vector<Widget*>* result_child_widgets,
+    bool* is_tab_modal_showing) {
   // Only attach child widgets to the root view.
   Widget* widget = view()->GetWidget();
   // Note that during window close, a Widget may exist in a state where it has
   // no NativeView, but hasn't yet torn down its view hierarchy.
-  if (!widget || !widget->GetNativeView() || widget->GetRootView() != view())
+  if (!widget || !widget->GetNativeView() || widget->GetRootView() != view()) {
+    *is_tab_modal_showing = false;
     return;
+  }
+
+  const views::FocusManager* focus_manager = view()->GetFocusManager();
+  const views::View* focused_view =
+      focus_manager ? focus_manager->GetFocusedView() : nullptr;
 
   std::set<Widget*> child_widgets;
   Widget::GetAllOwnedWidgets(widget->GetNativeView(), &child_widgets);
@@ -378,8 +400,21 @@ void ViewAXPlatformNodeDelegate::PopulateChildWidgetVector(
     if (widget->GetNativeWindowProperty(kWidgetNativeViewHostKey))
       continue;
 
+    // Focused tab-modal dialogs should take the place of the web page they
+    // cover in the accessibility tree.
+    if (child_widget->widget_delegate()->GetModalType() ==
+            ui::MODAL_TYPE_CHILD &&
+        focused_view &&
+        child_widget->GetContentsView()->Contains(focused_view)) {
+      result_child_widgets->clear();
+      result_child_widgets->push_back(child_widget);
+      *is_tab_modal_showing = true;
+      return;
+    }
+
     result_child_widgets->push_back(child_widget);
   }
+  *is_tab_modal_showing = false;
 }
 
 }  // namespace views
