2d912c53a41bfdc9b9258e5dfb399382761d2ec5
Wang Qing <wangqing-hf@loongson.cn>
2018-09-03 02:41:08 +0000

    Fix build error for blink.
    This CLs fixed the error of constexpr function call to non-constexpr function.
    
    Bug: 878202
    Change-Id: I6ad217a687e62a9a384980d852743a56479de3a9
    Reviewed-on: https://chromium-review.googlesource.com/1192467
    Commit-Queue: 汪 清 <wangqing-hf@loongson.cn>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588316}
    Former-commit-id: 87902b3202f81d689dd314c17006ffc907fe12a1

--- third_party/blink/renderer/core/animation/animation_time_delta.h
+++ third_party/blink/renderer/core/animation/animation_time_delta.h
@@ -90,26 +90,16 @@ AnimationTimeDelta operator*(T a, AnimationTimeDelta td) {
 }
 
 // Comparison operators on AnimationTimeDelta.
-constexpr bool CORE_EXPORT operator==(const AnimationTimeDelta& lhs,
-                                      const AnimationTimeDelta& rhs) {
-  return lhs.InSecondsF() == rhs.InSecondsF();
-}
-constexpr bool CORE_EXPORT operator!=(const AnimationTimeDelta& lhs,
-                                      const AnimationTimeDelta& rhs) {
-  return lhs.InSecondsF() != rhs.InSecondsF();
-}
-constexpr bool CORE_EXPORT operator>(const AnimationTimeDelta& lhs,
-                                     const AnimationTimeDelta& rhs) {
-  return lhs.InSecondsF() > rhs.InSecondsF();
-}
-constexpr bool CORE_EXPORT operator>=(const AnimationTimeDelta& lhs,
-                                      const AnimationTimeDelta& rhs) {
-  return lhs.InSecondsF() >= rhs.InSecondsF();
-}
-constexpr bool CORE_EXPORT operator<=(const AnimationTimeDelta& lhs,
-                                      const AnimationTimeDelta& rhs) {
-  return lhs.InSecondsF() <= rhs.InSecondsF();
-}
+bool CORE_EXPORT operator==(const AnimationTimeDelta& lhs,
+                                      const AnimationTimeDelta& rhs);
+bool CORE_EXPORT operator!=(const AnimationTimeDelta& lhs,
+                                      const AnimationTimeDelta& rhs);
+bool CORE_EXPORT operator>(const AnimationTimeDelta& lhs,
+                                     const AnimationTimeDelta& rhs);
+bool CORE_EXPORT operator>=(const AnimationTimeDelta& lhs,
+                                      const AnimationTimeDelta& rhs);
+bool CORE_EXPORT operator<=(const AnimationTimeDelta& lhs,
+                                      const AnimationTimeDelta& rhs);
 
 // Defined to allow DCHECK_EQ/etc to work with the class.
 CORE_EXPORT std::ostream& operator<<(std::ostream& os, AnimationTimeDelta time);
