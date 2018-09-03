5e4089659bb49b3095fa541fa6387b4c40d7396e
Christian Egli <christian.egli@sbs.ch>
2018-09-03 11:52:24 +0200

    Fix a buffer overflow
    Fixes #635
    
    Thanks to HongxuChen for reporting it

--- liblouis/lou_translateString.c
+++ liblouis/lou_translateString.c
@@ -354,7 +354,9 @@ matchCurrentInput(
 		const InString *input, int pos, const widechar *passInstructions, int passIC) {
 	int k;
 	int kk = pos;
-	for (k = passIC + 2; k < passIC + 2 + passInstructions[passIC + 1]; k++)
+	for (k = passIC + 2;
+			((k < passIC + 2 + passInstructions[passIC + 1]) && (kk < input->length));
+			k++)
 		if (input->chars[kk] == ENDSEGMENT || passInstructions[k] != input->chars[kk++])
 			return 0;
 	return 1;
