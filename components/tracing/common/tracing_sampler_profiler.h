276a130130f8b655b966311b77ffac59d49c763b
Etienne Bergeron <etienneb@chromium.org>
2018-09-04 17:03:52 +0000

    Add a sampling profiler to chrome tracing.
    This CL is plumbing the base::StackProfiling to allow collecting
    stack sample at a low rate on the browser process.
    An event is triggered to the trace with the collected stackframe.
    
    The goal of this code is to get low-rate stack samples in the field
    for the slow-report to help the investigation of stalled tasks.
    
    
    This is a variant of:
      https://chromium-review.googlesource.com/c/chromium/src/+/1188502
    
    R=ssid@chromium.org, oysteine@chromium.org
    
    Change-Id: I150c17df51914f4535d4e7d6ec086a2e13c17cff
    Reviewed-on: https://chromium-review.googlesource.com/1191745
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588554}
    Former-commit-id: 0ea3570a2882d86f47bf2cebf3e85a9fa16cd7a5

---
+++ components/tracing/common/tracing_sampler_profiler.h
@@ -0,0 +1,43 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_TRACING_COMMON_TRACING_SAMPLER_PROFILER_H_
+#define COMPONENTS_TRACING_COMMON_TRACING_SAMPLER_PROFILER_H_
+
+#include "base/macros.h"
+#include "base/sequence_checker.h"
+#include "base/threading/platform_thread.h"
+#include "base/trace_event/trace_log.h"
+#include "components/tracing/tracing_export.h"
+
+namespace base {
+class StackSamplingProfiler;
+}
+
+namespace tracing {
+
+// This class is a bridge between the base stack sampling profiler and chrome
+// tracing. It's listening to TraceLog enabled/disabled events and it's starting
+// a stack profiler on the current thread if needed.
+class TRACING_EXPORT TracingSamplerProfiler
+    : public base::trace_event::TraceLog::EnabledStateObserver {
+ public:
+  TracingSamplerProfiler();
+  ~TracingSamplerProfiler() override;
+
+  // trace_event::TraceLog::EnabledStateObserver implementation:
+  void OnTraceLogEnabled() override;
+  void OnTraceLogDisabled() override;
+
+ private:
+  base::PlatformThreadId sampled_thread_id_;
+  std::unique_ptr<base::StackSamplingProfiler> profiler_;
+  SEQUENCE_CHECKER(sequence_checker_);
+
+  DISALLOW_COPY_AND_ASSIGN(TracingSamplerProfiler);
+};
+
+}  // namespace tracing
+
+#endif  // COMPONENTS_TRACING_COMMON_TRACING_SAMPLER_PROFILER_H_
