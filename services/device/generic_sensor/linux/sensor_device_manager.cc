acb472ab3a47cd5dbda2e1e664d5a42912250e68
Jun Cai <juncai@chromium.org>
2018-09-04 22:42:16 +0000

    Device Sensor: Remove placeholder enumerator values in the sensor::SensorType
    According to the mojo style guide:
    https://chromium.googlesource.com/chromium/src/+/master/docs/security/mojo.md#do-not-define-placeholder-enumerator-values
    It is recommended not to define placeholder enumerator values. Currently,
    sensor::SensorType defines two such values: FIRST and LAST. This CL removes
    these two placeholder values.
    
    Bug: 870781
    Cq-Include-Trybots: luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I8b275bc5680c8c0d97fe2e7208301d6f33236cbe
    Reviewed-on: https://chromium-review.googlesource.com/1197423
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588678}
    Former-commit-id: a4b605bff8e031e952baea11163c8cf1ad883460

--- services/device/generic_sensor/linux/sensor_device_manager.cc
+++ services/device/generic_sensor/linux/sensor_device_manager.cc
@@ -81,8 +81,8 @@ void SensorDeviceManager::OnDeviceAdded(udev_device* dev) {
   if (device_node.empty())
     return;
 
-  const uint32_t first = static_cast<uint32_t>(mojom::SensorType::FIRST);
-  const uint32_t last = static_cast<uint32_t>(mojom::SensorType::LAST);
+  const uint32_t first = static_cast<uint32_t>(mojom::SensorType::kMinValue);
+  const uint32_t last = static_cast<uint32_t>(mojom::SensorType::kMaxValue);
   for (uint32_t i = first; i <= last; ++i) {
     SensorPathsLinux data;
     mojom::SensorType type = static_cast<mojom::SensorType>(i);
