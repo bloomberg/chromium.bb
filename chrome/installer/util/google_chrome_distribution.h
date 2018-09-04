1cbe42b507a5b352edf3e99f30c19b634488f83d
Samuel Huang <huangs@chromium.org>
2018-09-04 19:33:11 +0000

    Revert "Move DoPostUninstallOperations from BrowserDistribution into setup."
    This reverts commit f161241397f33dbc15ce9f6c0a75cd907ad757ee [formerly ea06ab2f566abeef6a195863601f8a25194671e4].
    
    Reason for revert: http://crbug.com/880484
    
    Original change's description:
    > Move DoPostUninstallOperations from BrowserDistribution into setup.
    > 
    > Showing the uninstall survey is a brand-specific behavior. This CL
    > introduces a new setup/brand_behaviors.h file that declares various
    > operations that vary by brand. DoPostUninstallOperations and
    > GetDistributionData (its helper function) both reside in brand-specific
    > implementation files that are selected at build time.
    > 
    > This also allowed moving the uninstall metrics code from installer/util
    > into installer/setup where it really belongs.
    > 
    > I have also taken the liberty to gently modernize some of the moved
    > code.
    > 
    > BUG=879568
    > 
    > Change-Id: I25d9c205d46ffb14454193fcc28ae5803502bf29
    > Reviewed-on: https://chromium-review.googlesource.com/1200862
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Reviewed-by: Brian White <bcwhite@chromium.org>
    > Reviewed-by: Samuel Huang <huangs@chromium.org>
    > Commit-Queue: Greg Thompson <grt@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#588537}
    
    TBR=huangs@chromium.org,bcwhite@chromium.org,rsesek@chromium.org,grt@chromium.org
    
    Change-Id: I7f5efa7c536e8b2c8c18445ff0d3f6b869ffa7b9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 879568
    Reviewed-on: https://chromium-review.googlesource.com/1204725
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588613}
    Former-commit-id: 898c03378ce34174d58f4b1ef1490cc65c008664

--- chrome/installer/util/google_chrome_distribution.h
+++ chrome/installer/util/google_chrome_distribution.h
@@ -12,7 +12,26 @@
 
 class GoogleChromeDistribution : public BrowserDistribution {
  public:
+  // Opens the Google Chrome uninstall survey window.
+  // version refers to the version of Chrome being uninstalled.
+  // local_data_path is the path of the file containing json metrics that
+  //   will be parsed. If this file indicates that the user has opted in to
+  //   providing anonymous usage data, then some additional statistics will
+  //   be added to the survey url.
+  // distribution_data contains Google Update related data that will be
+  //   concatenated to the survey url if the file in local_data_path indicates
+  //   the user has opted in to providing anonymous usage data.
+  void DoPostUninstallOperations(
+      const base::Version& version,
+      const base::FilePath& local_data_path,
+      const base::string16& distribution_data) override;
+
   base::string16 GetPublisherName() override;
+
+  // This method reads data from the Google Update ClientState key for
+  // potential use in the uninstall survey. It must be called before the
+  // key returned by GetVersionKey() is deleted.
+  base::string16 GetDistributionData(HKEY root_key) override;
 
   void UpdateInstallStatus(
       bool system_install,
