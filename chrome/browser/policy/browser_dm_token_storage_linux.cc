cdffa499e7cd4a77f595e914d19400ea1a41ae33
Owen Min <zmin@chromium.org>
2018-09-05 19:13:52 +0000

    InitDMToken should not create DM token directory.
    The DM token directory should be created only when writing token.
    Reading the token should not create any directory or file.
    
    Bug: 880870
    Tbr: pastarmovj
    Change-Id: I6917092a494c6b99508ef6279639ff75906f2175
    Reviewed-on: https://chromium-review.googlesource.com/1207218
    Commit-Queue: Owen Min <zmin@chromium.org>
    Reviewed-by: Georges Khalil <georgesak@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#588962}
    Former-commit-id: 2d0ccc6b329c8e5beb103d46ad913d27a4111830

--- chrome/browser/policy/browser_dm_token_storage_linux.cc
+++ chrome/browser/policy/browser_dm_token_storage_linux.cc
@@ -123,7 +123,7 @@ std::string BrowserDMTokenStorageLinux::InitEnrollmentToken() {
 
 std::string BrowserDMTokenStorageLinux::InitDMToken() {
   base::FilePath token_file_path;
-  if (!GetDmTokenFilePath(&token_file_path, RetrieveClientId(), true))
+  if (!GetDmTokenFilePath(&token_file_path, RetrieveClientId(), false))
     return std::string();
 
   std::string token;
