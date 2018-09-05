5ac897285623a0122094f63d8ad1eb707cb79e75
Brian Osman <brianosman@google.com>
2018-09-05 18:26:37 +0000

    Switch to new (skcms) color space primaries implementation in Skia
    Layout test diffs are low-bit changes due to floating point
    order-of-operations in the new code.
    
    Change-Id: Ie50c9913d2b29f0ddc5c99e6c0b35ad54ff0ef3f
    Reviewed-on: https://chromium-review.googlesource.com/1203932
    Reviewed-by: ccameron <ccameron@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Cr-Commit-Position: refs/heads/master@{#588941}
    Former-commit-id: 9c9d3fe6249780118b9b5c0af9d9a47c1bd99448

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -183,10 +183,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_AAA_CHOICE
 #endif
 
-#ifndef SK_USE_LEGACY_PRIMARIES_TO_XYZ
-#define SK_USE_LEGACY_PRIMARIES_TO_XYZ
-#endif
-
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
