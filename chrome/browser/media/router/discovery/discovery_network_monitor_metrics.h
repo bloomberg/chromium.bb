ac5df82e22cefbe257f98138b642e0bf57f830a0
Robbie McElrath <rmcelrath@chromium.org>
2018-09-05 22:58:26 +0000

    Migrate more of chrome/browser/media/router/discovery off NetworkChangeNotifier
    There was a call to net::NetworkChangeNotifier::GetConnectionType() that
    I missed in the intial NetworkChangeNotifier -> NetworkConnectionTracker
    conversion. This migrates that over.
    
    This also makes some changes to ScopedTaskEnvironment and
    TestBrowserThreadBundle in order to get
    ScopedTaskEnvironment::MainThreadType::MOCK_TIME working with
    TestBrowserThreadBundle.
    
    Bug: 868001
    Change-Id: Ie7d87bdd854ff278a258b26f416c9643b3d4dbcd
    Reviewed-on: https://chromium-review.googlesource.com/1185887
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Derek Cheng <imcheng@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589043}
    Former-commit-id: 57129d7e9fedc149d6b901aeea8e236bc470457b

--- chrome/browser/media/router/discovery/discovery_network_monitor_metrics.h
+++ chrome/browser/media/router/discovery/discovery_network_monitor_metrics.h
@@ -10,36 +10,36 @@
 namespace media_router {
 
 // This enum indicates the state of the host's network connectivity according to
-// both net::NetworkChangeNotifier and DiscoveryNetworkMonitor.  If
+// both network::NetworkConnectionTracker and DiscoveryNetworkMonitor.  If
 // DiscoveryNetworkMonitor reports the network identifier is unknown, we would
-// also like to record what net::NetworkChangeNotifier says about the connection
-// type.
+// also like to record what network::NetworkConnectionTracker says about the
+// connection type.
 //
 // It is tied to the UMA histogram MediaRouter.NetworkMonitor.ConnectionType so
 // new values should only be added at the end, but before kTotalCount.
 enum class DiscoveryNetworkMonitorConnectionType {
-  // net::NetworkChangeNotifier reports the connection type is Wifi and we have
-  // a valid network identifier from DiscoveryNetworkMonitor.
+  // network::NetworkConnectionTracker reports the connection type is Wifi and
+  // we have a valid network identifier from DiscoveryNetworkMonitor.
   kWifi = 0,
-  // net::NetworkChangeNotifier reports the connection type is ethernet and we
-  // have a valid network identifier from DiscoveryNetworkMonitor.
+  // network::NetworkConnectionTracker reports the connection type is ethernet
+  // and we have a valid network identifier from DiscoveryNetworkMonitor.
   kEthernet = 1,
-  // net::NetworkChangeNotifier reports the connection type is Wifi but we don't
-  // have a valid network identifier from DiscoveryNetworkMonitor.
+  // network::NetworkConnectionTracker reports the connection type is Wifi but
+  // we don't have a valid network identifier from DiscoveryNetworkMonitor.
   kUnknownReportedAsWifi = 2,
-  // net::NetworkChangeNotifier reports the connection type is ethernet but we
-  // don't have a valid network identifier from DiscoveryNetworkMonitor.
+  // network::NetworkConnectionTracker reports the connection type is ethernet
+  // but we don't have a valid network identifier from DiscoveryNetworkMonitor.
   kUnknownReportedAsEthernet = 3,
-  // net::NetworkChangeNotifier reports the connection type is something other
-  // than Wifi and ethernet and we don't have a valid network identifier from
-  // DiscoveryNetworkMonitor.
+  // network::NetworkConnectionTracker reports the connection type is something
+  // other than Wifi and ethernet and we don't have a valid network identifier
+  // from DiscoveryNetworkMonitor.
   kUnknownReportedAsOther = 4,
-  // net::NetworkChangeNotifier reports the connection type is unknown and we
-  // don't have a valid network identifier from DiscoveryNetworkMonitor.
+  // network::NetworkConnectionTracker reports the connection type is unknown
+  // and we don't have a valid network identifier from DiscoveryNetworkMonitor.
   kUnknown = 5,
   // DiscoveryNetworkMonitor reports that the host is disconnected from all
-  // networks.  This comes directly from net::NetworkChangeNotifier so there is
-  // no potential inconsistency to record.
+  // networks.  This comes directly from network::NetworkConnectionTracker so
+  // there is no potential inconsistency to record.
   kDisconnected = 6,
 
   // NOTE: Add entries only immediately above this line.
@@ -55,7 +55,7 @@ class DiscoveryNetworkMonitorMetrics {
   virtual void RecordTimeBetweenNetworkChangeEvents(base::TimeDelta delta);
 
   // This records the connection type as reported by both
-  // DiscoveryNetworkMonitor and net::NetworkChangeNotifier.
+  // DiscoveryNetworkMonitor and network::NetworkConnectionTracker.
   virtual void RecordConnectionType(
       DiscoveryNetworkMonitorConnectionType connection_type);
 };
