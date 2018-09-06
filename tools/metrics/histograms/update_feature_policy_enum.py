e59784a69f360b1870d983f54a18c7de325a7d4f
Paul Meyer <paulmeyer@chromium.org>
2018-09-06 20:32:49 +0000

    Add histogram to track use of feature policies in http response header.
    Change-Id: I617662f80f0bcdc723607337d8a2e153f537469b
    Bug: 879692
    TBR: jbroman@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/1200149
    Commit-Queue: Paul Meyer <paulmeyer@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589277}
    Former-commit-id: a3895348d360755b3acd30ce44d51d27067b5f5a

---
+++ tools/metrics/histograms/update_feature_policy_enum.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Updates the FeaturePolicyFeature enum in enums.xml file with
+values read from feature_policy.mojom.
+
+If the file was pretty-printed, the updated version is pretty-printed too.
+"""
+
+import os
+import sys
+
+from update_histogram_enum import UpdateHistogramEnum
+
+if __name__ == '__main__':
+  if len(sys.argv) > 1:
+    print >>sys.stderr, 'No arguments expected!'
+    sys.stderr.write(__doc__)
+    sys.exit(1)
+
+  source_file = 'third_party/blink/public/mojom/feature_policy/' \
+                'feature_policy.mojom'
+  UpdateHistogramEnum(histogram_enum_name='FeaturePolicyFeature',
+                      source_enum_path=source_file,
+                      start_marker='^enum FeaturePolicyFeature {',
+                      end_marker='^};')
