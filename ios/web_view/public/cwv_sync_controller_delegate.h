130d46a8ff245190717eb9335e61e52fd0aa0799
John Z Wu <jzw@chromium.org>
2018-09-06 22:05:21 +0000

    Header files for sync API in ios/web_view.
    Just the @interfaces for now.
    
    Bug: 867548
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I3be40a5d150726d39b5cb6934e2f642093dee32f
    Reviewed-on: https://chromium-review.googlesource.com/1197726
    Commit-Queue: John Wu <jzw@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589326}
    Former-commit-id: e457ee37cab1f74bb6b71cacd33a1248b7afd91f

---
+++ ios/web_view/public/cwv_sync_controller_delegate.h
@@ -0,0 +1,39 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <Foundation/Foundation.h>
+
+#ifndef IOS_WEB_VIEW_PUBLIC_CWV_SYNC_CONTROLLER_DELEGATE_H_
+#define IOS_WEB_VIEW_PUBLIC_CWV_SYNC_CONTROLLER_DELEGATE_H_
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef NS_ENUM(NSInteger, CWVStopSyncReason) {
+  // When sync is stopped explicitly via |stopSyncAndClearIdentity|.
+  CWVStopSyncReasonClient = 0,
+  // When sync was reset from another device.
+  CWVStopSyncReasonServer = 1
+};
+
+@class CWVSyncController;
+
+// Delegate of CWVSyncController.
+@protocol CWVSyncControllerDelegate<NSObject>
+
+@optional
+
+// Called when sync has been started. Check |syncController|'s |needsPassphrase|
+// property to see if |unlockWithPassphrase:| is necessary.
+- (void)syncControllerDidStartSync:(CWVSyncController*)syncController;
+
+// Called after the sync was stopped.
+// |reason| Indicates why sync was stopped.
+- (void)syncController:(CWVSyncController*)syncController
+    didStopSyncWithReason:(CWVStopSyncReason)reason;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#endif  // IOS_WEB_VIEW_PUBLIC_CWV_SYNC_CONTROLLER_DELEGATE_H_
