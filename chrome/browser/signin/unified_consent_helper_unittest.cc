50aa9933a0cf5b8bbce68e5aba1b0a95af1b8102
Mihai Sardarescu <msarda@chromium.org>
2018-09-06 07:27:20 +0000

    [unified-consent] Remove dependency on Desktop Identity Consistency.
    This CL allows users to be included in the Unified Consent experiment,
    regardless of their Desktop Identity Consistency state.
    
    Screenshots:
    https://drive.google.com/file/d/1B-rkxD3kPtdBw5toDYJGE-SWuO7PQULL/view?usp=sharing
    https://drive.google.com/file/d/1W8elDHLNBLJ5rK8QpX1K0xocgoTNpTI6/view?usp=sharing
    https://drive.google.com/file/d/1Abi8z1FPONLsuaErVgwkfFJdkeHT3o2c/view?usp=sharing
    
    
    Bug: 873064
    Change-Id: I27674fe5e6826aba605bbb9b3b4edb3938dc036f
    Reviewed-on: https://chromium-review.googlesource.com/1169814
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#588359}(cherry picked from commit dd19d029fbccd357432646cb762a5e99a16b14c7 [formerly fee394259de2840e75346d26050273793e3e411a])
    Reviewed-on: https://chromium-review.googlesource.com/1209150
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3538@{#82}
    Cr-Branched-From: 57781ff9a8651bc653184f680b0a4d8b92b6971f [formerly 79f7c91a2b2a2932cd447fa6f865cb6662fa8fa6]-refs/heads/master@{#587811}
    Former-commit-id: ec82e30054130578a9895c63930dd9477eada244

--- chrome/browser/signin/unified_consent_helper_unittest.cc
+++ chrome/browser/signin/unified_consent_helper_unittest.cc
@@ -4,46 +4,15 @@
 
 #include "chrome/browser/signin/unified_consent_helper.h"
 
-#include "build/buildflag.h"
-#include "chrome/browser/signin/scoped_account_consistency.h"
 #include "chrome/test/base/testing_profile.h"
-#include "components/signin/core/browser/signin_buildflags.h"
-#include "components/sync/driver/sync_driver_switches.h"
 #include "components/unified_consent/feature.h"
 #include "components/unified_consent/scoped_unified_consent.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-#if BUILDFLAG(ENABLE_DICE_SUPPORT)
-
-// On Dice platforms, unified consent can only be enabled for Dice profiles.
-TEST(UnifiedConsentHelperTest, DiceDisabled) {
-  // Disable Dice.
-  ScopedAccountConsistencyDiceFixAuthErrors dice_fix_auth_errors;
-
-  content::TestBrowserThreadBundle thread_bundle;
-  TestingProfile profile;
-
-  for (unified_consent::UnifiedConsentFeatureState state :
-       {unified_consent::UnifiedConsentFeatureState::kDisabled,
-        unified_consent::UnifiedConsentFeatureState::kEnabledNoBump,
-        unified_consent::UnifiedConsentFeatureState::kEnabledWithBump}) {
-    unified_consent::ScopedUnifiedConsent scoped_state(state);
-    EXPECT_FALSE(IsUnifiedConsentFeatureEnabled(&profile));
-    EXPECT_FALSE(IsUnifiedConsentFeatureWithBumpEnabled(&profile));
-  }
-}
-
-#endif
-
 // Checks that the feature state for the profile is the same as the global
 // feature state.
 TEST(UnifiedConsentHelperTest, FeatureState) {
-#if BUILDFLAG(ENABLE_DICE_SUPPORT)
-  // Enable Dice.
-  ScopedAccountConsistencyDice dice;
-#endif
-
   content::TestBrowserThreadBundle thread_bundle;
   TestingProfile profile;
 
