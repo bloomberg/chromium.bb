cac1a8a4a93746b632ae9eefcf6e32532798140d
Christian Fremerey <chfremer@chromium.org>
2018-09-06 19:27:24 +0000

    Reland [Video Capture Service] Inject GPU dependencies when starting service via extension
    Patchset 1 is the reverted CL as previously reviewed.
    Patchset 2 is the fix.
    
    The reason for the revert was a linker error for component builds.
    The fix is to add a missing CONTENT_EXPORT.
    
    ---
    
    Original CL description:
    
    The video capture service requires GPU dependencies to be injected after startup
    in order to enable accelerated MJPEG decoding. This CL adds this injection call
    where the service is started from the media_perception_private extension.
    
    The injected dependencies use classes that used to live in
    content/browser/renderer_host/media. To be able to reuse them for the
    extension, this CL moves them to a location that is accessible to both
    usage sites, i.e. to content/browser/gpu with a public interface in
    content/public/browser.
    
    TBR=jam@chromium.org
    
    Bug: 877690
    Change-Id: Ibeb29361c762d8366f1433d918e8abead0a39173
    Reviewed-on: https://chromium-review.googlesource.com/1208821
    Reviewed-by: Christian Fremerey <chfremer@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589242}
    Former-commit-id: ac94dafa79689901225dd29a38d882573ba5b456

--- content/browser/gpu/video_capture_dependencies.h
+++ content/browser/gpu/video_capture_dependencies.h
@@ -2,16 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_BROWSER_RENDERER_HOST_MEDIA_VIDEO_CAPTURE_DEPENDENCIES_H_
-#define CONTENT_BROWSER_RENDERER_HOST_MEDIA_VIDEO_CAPTURE_DEPENDENCIES_H_
+#ifndef CONTENT_BROWSER_GPU_VIDEO_CAPTURE_DEPENDENCIES_H_
+#define CONTENT_BROWSER_GPU_VIDEO_CAPTURE_DEPENDENCIES_H_
 
 #include "content/common/content_export.h"
-#include "services/video_capture/public/mojom/device_factory.mojom.h"
 #include "services/viz/privileged/interfaces/gl/gpu_service.mojom.h"
 
 namespace content {
 
-// Browser-provided GPU dependencies for video capture.
+// Browser-process-provided GPU dependencies for video capture.
 class CONTENT_EXPORT VideoCaptureDependencies {
  public:
   static void CreateJpegDecodeAccelerator(
@@ -22,4 +21,4 @@ class CONTENT_EXPORT VideoCaptureDependencies {
 
 }  // namespace content
 
-#endif  // CONTENT_BROWSER_RENDERER_HOST_MEDIA_VIDEO_CAPTURE_DEPENDENCIES_H_
+#endif  // CONTENT_BROWSER_GPU_VIDEO_CAPTURE_DEPENDENCIES_H_
