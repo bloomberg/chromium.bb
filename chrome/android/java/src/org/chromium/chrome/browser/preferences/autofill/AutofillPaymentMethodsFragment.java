59c5fcc519b5797b8d13be0c441d2de7e3832adc
Fabio Tirelo <ftirelo@chromium.org>
2018-09-06 14:17:01 +0000

    [AF] Move "Payment apps" into "Payment methods" on Android Settings
    A follow-up CL will delete the "Autofill and payments" page and move
    "Addresses and more" and "Payment methods" into the main Settings page.
    Moving "Payment apps" into the "Payment methods" as a separate step
    to make CLs easier to review.
    
    Screenshots (Googlers only): https://drive.google.com/open?id=1QMBcQKZ_O2aaxLs5OFf3zfyY7JSgcxgX
    
    Bug: 860526
    Change-Id: I57a107a15a7f8aab94a1125e79e6616957eaa49a
    Reviewed-on: https://chromium-review.googlesource.com/1199684
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589156}
    Former-commit-id: cefd7e5ab34f13fda99797bfb1a054b38e14cce8

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/autofill/AutofillPaymentMethodsFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/autofill/AutofillPaymentMethodsFragment.java
@@ -14,18 +14,24 @@ import android.support.v7.content.res.AppCompatResources;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.autofill.PersonalDataManager;
 import org.chromium.chrome.browser.autofill.PersonalDataManager.CreditCard;
+import org.chromium.chrome.browser.payments.AndroidPaymentAppFactory;
+import org.chromium.chrome.browser.payments.ServiceWorkerPaymentAppBridge;
 import org.chromium.chrome.browser.preferences.ChromeSwitchPreference;
 import org.chromium.chrome.browser.preferences.PreferenceUtils;
 
 /**
- * Autofill credit cards fragment, which allows the user to edit credit cards.
+ * Autofill credit cards fragment, which allows the user to edit credit cards and control
+ * payment apps.
  */
-public class AutofillCreditCardsFragment
+public class AutofillPaymentMethodsFragment
         extends PreferenceFragment implements PersonalDataManager.PersonalDataManagerObserver {
     private static final String PREF_AUTOFILL_ENABLE_CREDIT_CARDS_TOGGLE_LABEL =
             "autofill_enable_credit_cards_toggle_label";
+
+    private static final String PREF_PAYMENT_APPS = "payment_apps";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,10 +47,10 @@ public class AutofillCreditCardsFragment
         // Always rebuild our list of credit cards.  Although we could detect if credit cards are
         // added or deleted, the credit card summary (number) might be different.  To be safe, we
         // update all.
-        rebuildCreditCardList();
+        rebuildPage();
     }
 
-    private void rebuildCreditCardList() {
+    private void rebuildPage() {
         getPreferenceScreen().removeAll();
         getPreferenceScreen().setOrderingAsAdded(true);
 
@@ -65,42 +71,82 @@ public class AutofillCreditCardsFragment
 
         for (CreditCard card : PersonalDataManager.getInstance().getCreditCardsForSettings()) {
             // Add a preference for the credit card.
-            Preference pref = new Preference(getActivity());
-            pref.setTitle(card.getObfuscatedNumber());
-            pref.setSummary(card.getFormattedExpirationDate(getActivity()));
-            pref.setIcon(
+            Preference card_pref = new Preference(getActivity());
+            card_pref.setTitle(card.getObfuscatedNumber());
+            card_pref.setSummary(card.getFormattedExpirationDate(getActivity()));
+            card_pref.setIcon(
                     AppCompatResources.getDrawable(getActivity(), card.getIssuerIconDrawableId()));
 
             if (card.getIsLocal()) {
-                pref.setFragment(AutofillLocalCardEditor.class.getName());
+                card_pref.setFragment(AutofillLocalCardEditor.class.getName());
             } else {
-                pref.setFragment(AutofillServerCardEditor.class.getName());
-                pref.setWidgetLayoutResource(R.layout.autofill_server_data_label);
+                card_pref.setFragment(AutofillServerCardEditor.class.getName());
+                card_pref.setWidgetLayoutResource(R.layout.autofill_server_data_label);
             }
 
-            Bundle args = pref.getExtras();
+            Bundle args = card_pref.getExtras();
             args.putString(AutofillAndPaymentsPreferences.AUTOFILL_GUID, card.getGUID());
-            getPreferenceScreen().addPreference(pref);
+            getPreferenceScreen().addPreference(card_pref);
         }
 
         // Add 'Add credit card' button. Tap of it brings up card editor which allows users type in
         // new credit cards.
-        Preference pref = new Preference(getActivity());
+        Preference add_card_pref = new Preference(getActivity());
         Drawable plusIcon = ApiCompatibilityUtils.getDrawable(getResources(), R.drawable.plus);
         plusIcon.mutate();
         plusIcon.setColorFilter(
                 ApiCompatibilityUtils.getColor(getResources(), R.color.pref_accent_color),
                 PorterDuff.Mode.SRC_IN);
-        pref.setIcon(plusIcon);
-        pref.setTitle(R.string.autofill_create_credit_card);
-        pref.setFragment(AutofillLocalCardEditor.class.getName());
-        pref.setEnabled(PersonalDataManager.isAutofillCreditCardEnabled());
-        getPreferenceScreen().addPreference(pref);
+        add_card_pref.setIcon(plusIcon);
+        add_card_pref.setTitle(R.string.autofill_create_credit_card);
+        add_card_pref.setFragment(AutofillLocalCardEditor.class.getName());
+        add_card_pref.setEnabled(PersonalDataManager.isAutofillCreditCardEnabled());
+        getPreferenceScreen().addPreference(add_card_pref);
+
+        // Add the link to payment apps only after the credit card list is rebuilt.
+        if (ChromeFeatureList.isEnabled(ChromeFeatureList.ANDROID_PAYMENT_APPS)
+                || ChromeFeatureList.isEnabled(ChromeFeatureList.SERVICE_WORKER_PAYMENT_APPS)) {
+            Preference payment_apps_pref = new Preference(getActivity());
+            payment_apps_pref.setTitle(getActivity().getString(R.string.payment_apps_title));
+            payment_apps_pref.setFragment(AndroidPaymentAppsFragment.class.getCanonicalName());
+            payment_apps_pref.setShouldDisableView(true);
+            payment_apps_pref.setKey(PREF_PAYMENT_APPS);
+            getPreferenceScreen().addPreference(payment_apps_pref);
+            refreshPaymentAppsPrefForAndroidPaymentApps(payment_apps_pref);
+        }
+    }
+
+    private void refreshPaymentAppsPrefForAndroidPaymentApps(Preference pref) {
+        if (AndroidPaymentAppFactory.hasAndroidPaymentApps()) {
+            setPaymentAppsPrefStatus(pref, true);
+        } else {
+            refreshPaymentAppsPrefForServiceWorkerPaymentApps(pref);
+        }
+    }
+
+    private void refreshPaymentAppsPrefForServiceWorkerPaymentApps(Preference pref) {
+        ServiceWorkerPaymentAppBridge.hasServiceWorkerPaymentApps(
+                new ServiceWorkerPaymentAppBridge.HasServiceWorkerPaymentAppsCallback() {
+                    @Override
+                    public void onHasServiceWorkerPaymentAppsResponse(boolean hasPaymentApps) {
+                        setPaymentAppsPrefStatus(pref, hasPaymentApps);
+                    }
+                });
+    }
+
+    private void setPaymentAppsPrefStatus(Preference pref, boolean enabled) {
+        if (enabled) {
+            pref.setSummary(null);
+            pref.setEnabled(true);
+        } else {
+            pref.setSummary(getActivity().getString(R.string.payment_no_apps_summary));
+            pref.setEnabled(false);
+        }
     }
 
     @Override
     public void onPersonalDataChanged() {
-        rebuildCreditCardList();
+        rebuildPage();
     }
 
     @Override
