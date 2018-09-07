6dd7eccb9a9676ae564ffbb75600009cff8cb172
Bernie Thompson <bhthompson@google.com>
2018-09-06 17:10:54 -0700

    Add grunt-android-pi-pre-flight-branch builder for R70
    BUG=None
    TEST=None
    
    Change-Id: I4c9cf7ce2c63efe1a0aa7296b1aa14a0c27a5069
    Reviewed-on: https://chromium-review.googlesource.com/1211434
    Commit-Ready: Bernie Thompson <bhthompson@chromium.org>
    Tested-by: Bernie Thompson <bhthompson@chromium.org>
    Reviewed-by: Bernie Thompson <bhthompson@chromium.org>

--- config/chromeos_config.py
+++ config/chromeos_config.py
@@ -4169,6 +4169,18 @@ def SpecialtyBuilders(site_config, boards_dict, ge_build_config):
       prebuilts=False,
   )
 
+  site_config.Add(
+      'grunt-android-pi-pre-flight-branch',
+      site_config.templates.pre_flight_branch,
+      display_label=config_lib.DISPLAY_LABEL_PI_ANDROID_PFQ,
+      boards=['grunt'],
+      sync_chrome=True,
+      android_rev=constants.ANDROID_REV_LATEST,
+      android_package='android-container-pi',
+      android_import_branch=constants.ANDROID_PI_BUILD_BRANCH,
+      prebuilts=False,
+  )
+
   site_config.AddWithoutTemplate(
       'chromeos-infra-go',
       site_config.templates.no_hwtest_builder,
@@ -4335,6 +4347,8 @@ def BranchScheduleConfig():
       ('release-R70-11021.B', 'master-release',
        release_label, '0 5 * * *', None),
       ('release-R70-11021.B', 'reef-android-nyc-pre-flight-branch',
+       release_label, '0 1,5,9,13,17,21 * * *', None),
+      ('release-R70-11021.B', 'grunt-android-pi-pre-flight-branch',
        release_label, '0 1,5,9,13,17,21 * * *', None),
       ('release-R70-11021.B', 'samus-chrome-pre-flight-branch',
        release_label, '0 1,5,9,13,17,21 * * *', None),
