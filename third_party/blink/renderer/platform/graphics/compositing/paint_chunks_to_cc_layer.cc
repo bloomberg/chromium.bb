85d5c2b4f58c5ea175e8cf111b19b53eb65ddd62
Vladimir Levin <vmpstr@chromium.org>
2018-09-07 21:13:49 +0000

    Fix a null-dereference in PaintChunksToCc conversion.
    The patch to call Unalias caused a nullptr dereference, because of
    another bug that previously only triggered a DCHECK. This patch ensures
    to do a "safe unalias" where we first check for null.
    
    This check can be reverted once the underlying issues that trigger the
    DCHECK are fixed (see the two referenced bugs for the nullptr
    dereference and for the DCHECK failure).
    
    R=pdr@chromium.org, trchen@chromium.org
    
    Bug: 881788, 853357
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I412d60581466a40e4543b0ce78b18fb1a739f02b
    Reviewed-on: https://chromium-review.googlesource.com/1213869
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589651}
    Former-commit-id: 4009d1128e1cf626145afe071d90f12b7ec30867

--- third_party/blink/renderer/platform/graphics/compositing/paint_chunks_to_cc_layer.cc
+++ third_party/blink/renderer/platform/graphics/compositing/paint_chunks_to_cc_layer.cc
@@ -21,6 +21,17 @@ namespace blink {
 
 namespace {
 
+// TODO(crbug.com/853357): This is a temporary work-around for a bug. Unalias on
+// a node should always succeed if the node is not null. The code below assumes
+// that the node is not null, so we should be calling Unalias directly. However,
+// because of the referenced bug, which triggers a DCHECK, the node can in fact
+// sometimes be null. This turns the bug symptom from a DCHECK to a
+// null-dereference, which crashes release.
+template <typename NodeType>
+const NodeType* Unalias(const NodeType* node) {
+  return node ? node->Unalias() : nullptr;
+}
+
 class ConversionContext {
  public:
   ConversionContext(const PropertyTreeState& layer_state,
@@ -29,8 +40,8 @@ class ConversionContext {
                     cc::DisplayItemList& cc_list)
       : layer_state_(layer_state),
         layer_offset_(layer_offset),
-        current_transform_(layer_state.Transform()->Unalias()),
-        current_clip_(layer_state.Clip()->Unalias()),
+        current_transform_(Unalias(layer_state.Transform())),
+        current_clip_(Unalias(layer_state.Clip())),
         current_effect_(layer_state.Effect()),
         chunk_to_layer_mapper_(layer_state_,
                                layer_offset_,
@@ -311,7 +322,7 @@ static bool CombineClip(const ClipPaintPropertyNode* clip,
 }
 
 void ConversionContext::SwitchToClip(const ClipPaintPropertyNode* target_clip) {
-  target_clip = target_clip->Unalias();
+  target_clip = Unalias(target_clip);
   if (target_clip == current_clip_)
     return;
 
@@ -328,7 +339,7 @@ void ConversionContext::SwitchToClip(const ClipPaintPropertyNode* target_clip) {
 #endif
     if (!state_stack_.size() || state_stack_.back().type != StateEntry::kClip)
       break;
-    current_clip_ = current_clip_->Parent()->Unalias();
+    current_clip_ = Unalias(current_clip_->Parent());
     StateEntry& previous_state = state_stack_.back();
     if (current_clip_ == lca_clip) {
       // |lca_clip| is an intermediate clip in a series of combined clips.
@@ -346,7 +357,7 @@ void ConversionContext::SwitchToClip(const ClipPaintPropertyNode* target_clip) {
   // At this point the current clip must be an ancestor of the target.
   Vector<const ClipPaintPropertyNode*, 1u> pending_clips;
   for (const ClipPaintPropertyNode* clip = target_clip; clip != current_clip_;
-       clip = clip->Parent()->Unalias()) {
+       clip = Unalias(clip->Parent())) {
     // This should never happen unless the DCHECK in step 1 failed.
     if (!clip)
       break;
@@ -378,9 +389,9 @@ void ConversionContext::SwitchToClip(const ClipPaintPropertyNode* target_clip) {
 void ConversionContext::StartClip(
     const FloatRoundedRect& combined_clip_rect,
     const ClipPaintPropertyNode* lowest_combined_clip_node) {
-  DCHECK_EQ(lowest_combined_clip_node, lowest_combined_clip_node->Unalias());
+  DCHECK_EQ(lowest_combined_clip_node, Unalias(lowest_combined_clip_node));
   auto* local_transform =
-      lowest_combined_clip_node->LocalTransformSpace()->Unalias();
+      Unalias(lowest_combined_clip_node->LocalTransformSpace());
   if (local_transform != current_transform_)
     EndTransform();
   cc_list_.StartPaint();
@@ -612,7 +623,7 @@ void ConversionContext::PopState() {
 
 void ConversionContext::SwitchToTransform(
     const TransformPaintPropertyNode* target_transform) {
-  target_transform = target_transform->Unalias();
+  target_transform = Unalias(target_transform);
   if (target_transform == current_transform_)
     return;
 
