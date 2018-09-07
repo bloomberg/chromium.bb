38487ad3bff4e4a6874e14404a7b54e2e529a4f3
Pin-chih Lin <johnylin@chromium.org>
2018-09-07 03:43:28 +0000

    V4L2SVDA: Don't continue with an invalid iterator
    BUG=b:112181526
    BUG=881669
    TEST=build
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: Ife2de572cad1a47c0e98a6b9f792cfd962af96f0
    Reviewed-on: https://chromium-review.googlesource.com/1212204
    Commit-Queue: Pin-chih Lin <johnylin@chromium.org>
    Reviewed-by: Pawel Osciak <posciak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589426}
    Former-commit-id: 993541aff428c4fdee4765d4c7e19f4bc499d760

--- media/gpu/v4l2/v4l2_slice_video_decode_accelerator.cc
+++ media/gpu/v4l2/v4l2_slice_video_decode_accelerator.cc
@@ -1017,6 +1017,7 @@ void V4L2SliceVideoDecodeAccelerator::Dequeue() {
     if (it == surfaces_at_device_.end()) {
       VLOGF(1) << "Got invalid surface from device.";
       NOTIFY_ERROR(PLATFORM_FAILURE);
+      return;
     }
 
     it->second->SetDecoded();
