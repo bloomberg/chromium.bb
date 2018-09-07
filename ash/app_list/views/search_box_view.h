ea0ee1d6c12e9181cd19eccb753ac8dfc0accdab
Daniel Zhang <oxyflush@google.com>
2018-09-07 18:05:55 +0000

    cros: Fix autocomplete interaction with arrow keys
    Currently, AppList autocomplete feature handles arrow
    key events regardless of whether or not there is
    autocomplete text present, causing unwanted
    interactions.
    
    Change autocomplete so that it only handles arrow keys
    when autocomplete text is present.
    
    Bug: 881039
    Change-Id: I7041646e87e5cd4e95d6299017fa2259899f8994
    Reviewed-on: https://chromium-review.googlesource.com/1208718
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Daniel Zhang <oxyflush@google.com>
    Cr-Commit-Position: refs/heads/master@{#589582}
    Former-commit-id: 81ac5803ecec7d4b1181a37a1cb83f8c07109d3d

--- ash/app_list/views/search_box_view.h
+++ ash/app_list/views/search_box_view.h
@@ -108,6 +108,10 @@ class APP_LIST_EXPORT SearchBoxView : public search_box::SearchBoxViewBase,
   // Accepts one character in the autocomplete text and fires query.
   void AcceptOneCharInAutocompleteText();
 
+  // Returns true if there is currently an autocomplete suggestion in
+  // search_box().
+  bool HasAutocompleteText();
+
   // Removes all autocomplete text.
   void ClearAutocompleteText();
 
