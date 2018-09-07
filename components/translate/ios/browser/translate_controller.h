4dfc0258a7dc129c383c3fe33fe9394f4e8b1ca1
John Z Wu <jzw@chromium.org>
2018-09-07 01:10:26 +0000

    Fix CSP issues with iOS Translate.
    Content Security Policy does not allow the translate element.js script
    to download and execute additional javascript. To get around this, we
    redirect to the browser to download and execute the two additional
    scripts (main.js and element_main.js).
    
    Bug: 686364
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: Ie92f357b9809a0e4efc0ff111e8593f727b936d4
    Reviewed-on: https://chromium-review.googlesource.com/1131826
    Commit-Queue: John Wu <jzw@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589394}
    Former-commit-id: dfaa4293c114f509a2a7c56cb8ede6ebb1b31f77

--- components/translate/ios/browser/translate_controller.h
+++ components/translate/ios/browser/translate_controller.h
@@ -5,6 +5,7 @@
 #ifndef COMPONENTS_TRANSLATE_IOS_BROWSER_TRANSLATE_CONTROLLER_H_
 #define COMPONENTS_TRANSLATE_IOS_BROWSER_TRANSLATE_CONTROLLER_H_
 
+#include <memory>
 #include <string>
 
 #include "base/gtest_prod_util.h"
@@ -13,17 +14,19 @@
 #include "base/memory/weak_ptr.h"
 #include "components/translate/core/common/translate_errors.h"
 #include "ios/web/public/web_state/web_state_observer.h"
+#include "services/network/public/cpp/simple_url_loader.h"
 
 @class JsTranslateManager;
 class GURL;
 
 namespace base {
 class DictionaryValue;
-}
+}  // namespace base
 
 namespace web {
+class NavigationContext;
 class WebState;
-}
+}  // namespace web
 
 namespace translate {
 
@@ -89,13 +92,24 @@ class TranslateController : public web::WebStateObserver {
   // Return false if the command is invalid.
   bool OnTranslateReady(const base::DictionaryValue& command);
   bool OnTranslateComplete(const base::DictionaryValue& command);
+  bool OnTranslateLoadJavaScript(const base::DictionaryValue& command);
+
+  // Use to fetch additional scripts needed for translate.
+  void FetchScript(const std::string& url);
+  // The callback when the script is fetched or a server error occurred.
+  void OnScriptFetchComplete(std::unique_ptr<std::string> response_body);
 
   // web::WebStateObserver implementation:
   void WebStateDestroyed(web::WebState* web_state) override;
+  void DidStartNavigation(web::WebState* web_state,
+                          web::NavigationContext* navigation_context) override;
 
   // The WebState this instance is observing. Will be null after
   // WebStateDestroyed has been called.
   web::WebState* web_state_ = nullptr;
+
+  // Used to fetch additional scripts needed for translate.
+  std::unique_ptr<network::SimpleURLLoader> script_fetcher_;
 
   Observer* observer_;
   base::scoped_nsobject<JsTranslateManager> js_manager_;
