4dca84b927bd598e01720cbdec568812ef24dcda
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2018-09-10 15:32:04 +0000

    [BrowserSwitcher] Add BrowserSwitcherService{,Factory}
    BrowserSwitcherService is a per-BrowserContext class that manages
    resources that should be shared between tabs in the same BrowserContext:
    for now, the sitelist and browser launcher objects.
    
    The BrowserSwitcherServiceFactory lets us easily create only one
    BrowserSwitcherService per BrowserContext.
    
    Bug: 876803
    Change-Id: I6eb4c235143cfe38a077141b62e9332ed0f30a05
    Reviewed-on: https://chromium-review.googlesource.com/1206698
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589922}
    Former-commit-id: 4861f459e4e929f0956b408ad89745be47dd22ca

---
+++ chrome/browser/browser_switcher/browser_switcher_service.cc
@@ -0,0 +1,34 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/browser_switcher/browser_switcher_service.h"
+
+#include "chrome/browser/browser_switcher/alternative_browser_launcher.h"
+#include "chrome/browser/browser_switcher/browser_switcher_sitelist.h"
+#include "chrome/browser/profiles/profile.h"
+#include "components/prefs/pref_service.h"
+#include "content/public/browser/browser_context.h"
+
+namespace browser_switcher {
+
+BrowserSwitcherService::BrowserSwitcherService(PrefService* prefs)
+    : launcher_(nullptr), sitelist_(nullptr), prefs_(prefs) {
+  DCHECK(prefs_);
+}
+
+BrowserSwitcherService::~BrowserSwitcherService() {}
+
+AlternativeBrowserLauncher* BrowserSwitcherService::launcher() {
+  if (!launcher_)
+    launcher_ = std::make_unique<AlternativeBrowserLauncher>(prefs_);
+  return launcher_.get();
+}
+
+BrowserSwitcherSitelist* BrowserSwitcherService::sitelist() {
+  if (!sitelist_)
+    sitelist_ = std::make_unique<BrowserSwitcherSitelist>(prefs_);
+  return sitelist_.get();
+}
+
+}  // namespace browser_switcher
