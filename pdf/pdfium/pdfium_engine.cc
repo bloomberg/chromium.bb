c75e4a80d8d324170bea2dbf69ce751ba5f65acc
Ryan Harrison <rharrison@chromium.org>
2018-09-10 17:45:26 +0000

    Merge definitions of Zero Width Whitespace in pdf/
    I introduced a second definition of ZWW in the PDF plugin code without
    realizing it in a previoud CL. This CL merges the two definitions
    together.
    
    BUG=chromium:743522
    
    Change-Id: Id5389bffc9aca70458c4aa934eb3163bf6ad503a
    Reviewed-on: https://chromium-review.googlesource.com/1213543
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Ryan Harrison <rharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#589970}
    Former-commit-id: 12f2cd31853e9b5b0322207b336ce089087dd368

--- pdf/pdfium/pdfium_engine.cc
+++ pdf/pdfium/pdfium_engine.cc
@@ -515,7 +515,6 @@ bool FindMultipleClickBoundary(bool is_double_click, base::char16 cur) {
   if (cur < 128)
     return true;
 
-  static constexpr base::char16 kZeroWidthSpace = 0x200B;
   if (cur == kZeroWidthSpace)
     return true;
 
