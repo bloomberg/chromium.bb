49f0466f805d9e87db7814a679ba70678936ebc4
Artur Khachatryan <khachatryan@chromium.org>
2018-09-10 22:28:22 +0000

    [ChromeDriver] Disable failing test on Android platforms
    Test ChromeDriver.testTakeElementScreenshot fails on Android platforms.
    Disabling test for now.
    
    Bug: chromedriver:2579
    Change-Id: I5ceb2cf4ba39cbfbeb37c851724e0b7ccd5a9925
    Reviewed-on: https://chromium-review.googlesource.com/1217601
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Artur Khachatryan <khachatryan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590092}
    Former-commit-id: 7351bcacc910ecbfbecfb130ca97ccb60451ecba

--- chrome/test/chromedriver/test/run_py_tests.py
+++ chrome/test/chromedriver/test/run_py_tests.py
@@ -227,6 +227,8 @@ _ANDROID_NEGATIVE_FILTER['chrome'] = (
         'HeadlessInvalidCertificateTest.*',
         # Tests of the desktop Chrome launch process.
         'LaunchDesktopTest.*',
+        # https://bugs.chromium.org/p/chromedriver/issues/detail?id=2579
+        'ChromeDriverTest.testTakeElementScreenshot',
     ]
 )
 _ANDROID_NEGATIVE_FILTER['chrome_stable'] = (
