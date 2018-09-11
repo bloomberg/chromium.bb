dafb29070a4bb186735c9268b6f1b0cb00c5f0ce
Koji Ishii <kojii@chromium.org>
2018-09-11 03:51:56 +0000

    Fix first-line text-transform workaround when start is not zero
    r526281 added a workaround for when text-transform shorten
    the string, but did so only for when inline_text_box_.Start()
    is zero.
    
    This patch extends the workaround for when inline_text_box_.
    Start() is not zero.
    
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ib4536ae5aaf92352e41ef19e53c2046a4c86806c
    Bug: 880176, 795498
    Reviewed-on: https://chromium-review.googlesource.com/1218202
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590194}
    Former-commit-id: d7659161bf6a183c356ad79a45993cc7e9971cb6

--- third_party/blink/renderer/core/paint/inline_text_box_painter.cc
+++ third_party/blink/renderer/core/paint/inline_text_box_painter.cc
@@ -194,13 +194,15 @@ void InlineTextBoxPainter::Paint(const PaintInfo& paint_info,
     // capitalizing letters can change the length of the backing string.
     // That needs to be taken into account when computing the size of the box
     // or its painting.
-    length = std::min(length, first_line_string.length());
+    length = std::min(length, first_line_string.length() -
+                                  std::min(inline_text_box_.Start(),
+                                           first_line_string.length()));
 
     // TODO(szager): Figure out why this CHECK sometimes fails, it shouldn't.
-    CHECK(inline_text_box_.Start() + length <= first_line_string.length());
+    CHECK_LE(inline_text_box_.Start() + length, first_line_string.length());
   } else {
     // TODO(szager): Figure out why this CHECK sometimes fails, it shouldn't.
-    CHECK(inline_text_box_.Start() + length <= layout_item_string.length());
+    CHECK_LE(inline_text_box_.Start() + length, layout_item_string.length());
   }
   StringView string =
       StringView(inline_text_box_.IsFirstLineStyle() ? first_line_string
