ebf7966fc6ecdcc258f378c324b5d117608eb7d4
Stephane Zermatten <szermatt@chromium.org>
2018-09-11 15:58:50 +0000

    [Autofill Assistant] Make scripts without preconditions runnable.
    With this change, scripts without precondition are not treated as
    invalid, but runnable. Also includes a small test
    ProtocolUtils::ParseScripts.
    
    Bug: 806868
    Change-Id: I6c286b1622bfeb4d635310396cb3b158ffd6933e
    Reviewed-on: https://chromium-review.googlesource.com/1219748
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Commit-Queue: Stephane Zermatten <szermatt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590333}
    Former-commit-id: a31099c66efb85c735e5ddd3313a8d5822c65bca

---
+++ components/autofill_assistant/browser/protocol_utils_unittest.cc
@@ -0,0 +1,45 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill_assistant/browser/protocol_utils.h"
+
+#include "base/macros.h"
+#include "components/autofill_assistant/browser/service.pb.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+namespace autofill_assistant {
+namespace {
+
+using ::testing::SizeIs;
+using ::testing::IsEmpty;
+
+TEST(ProtocolUtilsTest, NoScripts) {
+  std::vector<std::unique_ptr<Script>> scripts;
+  EXPECT_TRUE(ProtocolUtils::ParseScripts("", &scripts));
+  EXPECT_THAT(scripts, IsEmpty());
+}
+
+TEST(ProtocolUtilsTest, SomeInvalidScripts) {
+  SupportsScriptResponseProto proto;
+
+  // 2 Invalid scripts, 1 valid one, with no preconditions.
+  proto.add_scripts()->mutable_presentation()->set_name("missing path");
+  proto.add_scripts()->set_path("missing name");
+  SupportedScriptProto* script = proto.add_scripts();
+  script->set_path("ok");
+  script->mutable_presentation()->set_name("ok name");
+
+  // Only the valid script is returned.
+  std::vector<std::unique_ptr<Script>> scripts;
+  std::string proto_str;
+  proto.SerializeToString(&proto_str);
+  EXPECT_TRUE(ProtocolUtils::ParseScripts(proto_str, &scripts));
+  ASSERT_THAT(scripts, SizeIs(1));
+  EXPECT_EQ("ok", scripts[0]->handle.path);
+  EXPECT_EQ("ok name", scripts[0]->handle.name);
+  EXPECT_NE(nullptr, scripts[0]->precondition);
+}
+
+}  // namespace
+}  // namespace autofill_assistant
