997c72f45ba0fdb322a2a1a53689752d8c20ad0f
Mohsen Izadi <mohsen@chromium.org>
2018-09-11 18:18:50 +0000

    Remove surface invariants violation check in RenderWidgetHostImpl
    RenderWidgetHostImpl::SubmitCompositorFrame() checks for surface
    invariants violation and records Compositing.SurfaceInvariantsViolations
    histogram in case of violation. However, CompositorFrameSinkSupport
    already checks for violations and records appropriate histogram values
    (Compositing.CompositorFrameSinkSupport.SubmitResult).
    
    So, the check and histogram in RenderWidgetHostImpl is not very useful
    and can be removed.
    
    BUG=771354
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: I24a9e8078be17175e588383259237f27ae8c28b2
    Reviewed-on: https://chromium-review.googlesource.com/1217706
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590406}
    Former-commit-id: 51a81d39fc6d3db54b04174afda85b826c50fc99

--- components/viz/common/features.h
+++ components/viz/common/features.h
@@ -13,7 +13,6 @@ namespace features {
 
 VIZ_COMMON_EXPORT extern const base::Feature kEnableDrawOcclusion;
 VIZ_COMMON_EXPORT extern const base::Feature kEnableSurfaceSynchronization;
-VIZ_COMMON_EXPORT extern const base::Feature kEnableInvariantsViolationLogging;
 VIZ_COMMON_EXPORT extern const base::Feature kEnableVizHitTestDrawQuad;
 VIZ_COMMON_EXPORT extern const base::Feature kEnableVizHitTestSurfaceLayer;
 VIZ_COMMON_EXPORT extern const base::Feature kUseSkiaDeferredDisplayList;
@@ -22,7 +21,6 @@ VIZ_COMMON_EXPORT extern const base::Feature kVizDisplayCompositor;
 
 VIZ_COMMON_EXPORT bool IsDrawOcclusionEnabled();
 VIZ_COMMON_EXPORT bool IsSurfaceSynchronizationEnabled();
-VIZ_COMMON_EXPORT bool IsSurfaceInvariantsViolationLoggingEnabled();
 VIZ_COMMON_EXPORT bool IsVizHitTestingDrawQuadEnabled();
 VIZ_COMMON_EXPORT bool IsVizHitTestingEnabled();
 VIZ_COMMON_EXPORT bool IsVizHitTestingSurfaceLayerEnabled();
