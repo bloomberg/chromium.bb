8c214fcde7e95988640a8a0117068e819ca9aee1
Justin DeWitt <dewittj@chromium.org>
2018-09-11 17:51:09 +0000

    [EoS] Support for adding catalog proto to the store.
    This adds an ImportCatalogTask that will take a proto directly from the
    service API and add records to the database.  It has the following
    behavior:
    * If the timestamp matches the "current" version, it does nothing. This
      prevents stomping on the currently viewable catalog.
    * If the timestamp matches the "downloading" version, it overwrites it,
      since that version is not yet viewable by the user.
    * Sets the "downloading" version number to the input version timestamp.
    
    Bug: 867488
    Change-Id: I1c1725a664f348eb5399da2d42d0d8c22c44e1c0
    Reviewed-on: https://chromium-review.googlesource.com/1213707
    Reviewed-by: Cathy Li <chili@chromium.org>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590384}
    Former-commit-id: e02502b490a0ebfed24f775cf4eb54d6f002a566

--- chrome/browser/android/explore_sites/explore_sites_service_impl.h
+++ chrome/browser/android/explore_sites/explore_sites_service_impl.h
@@ -10,18 +10,33 @@
 #include "base/macros.h"
 #include "chrome/browser/android/explore_sites/explore_sites_service.h"
 #include "chrome/browser/android/explore_sites/explore_sites_store.h"
+#include "components/offline_pages/task/task_queue.h"
+
+using offline_pages::TaskQueue;
 
 namespace explore_sites {
+class Catalog;
 
-class ExploreSitesServiceImpl : public ExploreSitesService {
+class ExploreSitesServiceImpl : public ExploreSitesService,
+                                public TaskQueue::Delegate {
  public:
   explicit ExploreSitesServiceImpl(std::unique_ptr<ExploreSitesStore> store);
   ~ExploreSitesServiceImpl() override;
 
+  bool IsExploreSitesEnabled();
+
+ private:
   // KeyedService implementation:
   void Shutdown() override;
 
- private:
+  // TaskQueue::Delegate implementation:
+  void OnTaskQueueIsIdle() override;
+
+  void AddUpdatedCatalog(int64_t catalog_timestamp,
+                         std::unique_ptr<Catalog> catalog_proto);
+
+  // Used to control access to the ExploreSitesStore.
+  TaskQueue task_queue_;
   std::unique_ptr<ExploreSitesStore> explore_sites_store_;
 
   DISALLOW_COPY_AND_ASSIGN(ExploreSitesServiceImpl);
