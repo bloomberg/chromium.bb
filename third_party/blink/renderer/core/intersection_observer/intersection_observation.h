25da6bac8ef6b885f087be86599727e14c4f1976
Stefan Zager <szager@chromium.org>
2018-09-11 01:07:28 +0000

    [IntersectionObserver] Reland: Fix the concept of "tracking document"
    Re-landing after revert:
    
    https://chromium-review.googlesource.com/c/chromium/src/+/1200388
    
    ... but leave the flaky tests disabled.
    
    R=chrishtr@chromium.org
    BUG=879798
    
    Change-Id: Ia588f9d82665a533b4e1bd2f77809692ded0cb41
    Reviewed-on: https://chromium-review.googlesource.com/1215370
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590155}
    Former-commit-id: 61b3108056747ea20d02627ec65a02c550e42c59

--- third_party/blink/renderer/core/intersection_observer/intersection_observation.h
+++ third_party/blink/renderer/core/intersection_observer/intersection_observation.h
@@ -6,6 +6,7 @@
 #define THIRD_PARTY_BLINK_RENDERER_CORE_INTERSECTION_OBSERVER_INTERSECTION_OBSERVATION_H_
 
 #include "third_party/blink/renderer/core/dom/dom_high_res_time_stamp.h"
+#include "third_party/blink/renderer/core/intersection_observer/intersection_observer_entry.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 
 namespace blink {
@@ -25,9 +26,11 @@ class IntersectionObservation final
   IntersectionObserver* Observer() const { return observer_.Get(); }
   Element* Target() const { return target_; }
   unsigned LastThresholdIndex() const { return last_threshold_index_; }
-  void ComputeIntersectionObservations(DOMHighResTimeStamp);
+  // If the parameter is true and the observer doesn't have an explicit root,
+  // then any notifications generated will contain root bounds geometry.
+  void Compute(bool should_report_implicit_root_bounds);
+  void TakeRecords(HeapVector<Member<IntersectionObserverEntry>>&);
   void Disconnect();
-  void UpdateShouldReportRootBoundsAfterDomChange();
 
   void Trace(blink::Visitor*);
 
@@ -37,13 +40,18 @@ class IntersectionObservation final
     last_is_visible_ = last_is_visible ? 1 : 0;
   }
 
+  // If trackVisibility is true, don't compute observations more frequently
+  // than this many milliseconds.
+  static const DOMHighResTimeStamp s_v2_throttle_delay_;
+
   Member<IntersectionObserver> observer_;
   WeakMember<Element> target_;
+  HeapVector<Member<IntersectionObserverEntry>> entries_;
+  DOMHighResTimeStamp last_run_time_;
 
-  unsigned should_report_root_bounds_ : 1;
   unsigned last_is_visible_ : 1;
-  unsigned last_threshold_index_ : 29;
-  static const unsigned kMaxThresholdIndex = (unsigned)0x20000000;
+  unsigned last_threshold_index_ : 31;
+  static const unsigned kMaxThresholdIndex = (unsigned)0x80000000;
 };
 
 }  // namespace blink
