7a9cecd1aa5f2617640b721ed044cf5e8c6b28cc
Matthew Jones <mdjones@chromium.org>
2018-09-11 21:48:29 +0000

    Remove unused independent animation from LayoutTab
    This patch reomves some obsolete code that was used to transition
    theme colors on the tab border in the tab switcher. This feature
    has since been removed.
    
    Bug: 882508
    Change-Id: Ie63fabb08631f0377721f7057cbe65a4cd3af9a2
    Reviewed-on: https://chromium-review.googlesource.com/1217405
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590488}
    Former-commit-id: c120ca1d1c52d1a55618c44b6b978c4377765f57

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/Layout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/Layout.java
@@ -852,16 +852,7 @@ public abstract class Layout implements TabContentManager.ThumbnailChangeListene
             }
         }
 
-        // LayoutTabs may be running their own animations; make sure they are done. This should
-        // not block the completion state of the layout animations in general. Particularly, a tab
-        // could be driving theme changes (and therefore fade animations) that are not critical to
-        // the browser's UI. https://crbug.com/627066
-        boolean layoutTabsFinished = true;
-        for (int i = 0; mLayoutTabs != null && i < mLayoutTabs.length; i++) {
-            layoutTabsFinished &= mLayoutTabs[i].onUpdateAnimation(time);
-        }
-
-        if (!finished || !layoutTabsFinished) requestUpdate();
+        if (!finished) requestUpdate();
 
         return finished;
     }
