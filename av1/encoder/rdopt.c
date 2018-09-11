b5e7825b958a7ab8b5d3d687eeb41224a9eabdd5
Venkat <sanampudi.venkatarao@ittiam.com>
2018-09-11 18:58:07 +0530

    Handle early exit for skip case
    During txfm_search early exit handled appropriately for skip case.
    
    When tested for 20 frames of BasketBallDrill_832x480_50 at 1 mbps,
    observed ~0.29% reduction in encoder time for speed=1 preset.
    
    As per AWCY runs 0.01% BD-rate improvement is seen
    for speed=1 preset.
    
    STATS_CHANGED
    
    Change-Id: If6d678637b9b29a3d70d17573138af2b064d05ba

--- av1/encoder/rdopt.c
+++ av1/encoder/rdopt.c
@@ -8417,6 +8417,12 @@ static int txfm_search(const AV1_COMP *cpi, MACROBLOCK *x, BLOCK_SIZE bsize,
       rd_stats->rate += x->skip_cost[skip_ctx][1];
       mbmi->skip = 0;
       // here mbmi->skip temporarily plays a role as what this_skip2 does
+
+      int64_t tmprd = RDCOST(x->rdmult, rd_stats->rate, rd_stats->dist);
+      if (tmprd > ref_best_rd) {
+        mbmi->ref_frame[1] = ref_frame_1;
+        return 0;
+      }
     } else if (!xd->lossless[mbmi->segment_id] &&
                (RDCOST(x->rdmult,
                        rd_stats_y->rate + rd_stats_uv->rate +
@@ -8445,6 +8451,11 @@ static int txfm_search(const AV1_COMP *cpi, MACROBLOCK *x, BLOCK_SIZE bsize,
     rd_stats_y->rate = 0;
     rd_stats_uv->rate = 0;
     rd_stats->skip = 1;
+    int64_t tmprd = RDCOST(x->rdmult, rd_stats->rate, rd_stats->dist);
+    if (tmprd > ref_best_rd) {
+      mbmi->ref_frame[1] = ref_frame_1;
+      return 0;
+    }
   }
   return 1;
 }
