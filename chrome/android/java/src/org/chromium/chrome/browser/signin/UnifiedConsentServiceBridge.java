87dc52a5fbce2e52712f1f955953bcd93d62f3b7
Boris Sazonov <bsazonov@chromium.org>
2018-09-11 12:25:10 +0000

    [Unity][Android] Wire up the rest of SyncAndServicesPreferences
    1. Wire up 'Make searches and browsing better' to
       kUrlKeyedAnonymizedDataCollectionEnabled preference (with necessary
       JNI bridging).
    2. Enable/disable preference entries in 'Other Google services' based on
       'Use sync and all services' value.
    3. When SyncAndServicesPreferences are first opened, expand sections if
       'Use sync and all services' is off, otherwise collapse them.
    4. Simplify the code in SyncAndServicesPreferences by using
       mPrefServiceBridge.
    
    Bug: 814728
    Change-Id: I7946609987008af4250a648793bf31c02dea887e
    Reviewed-on: https://chromium-review.googlesource.com/1196527
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#588561}(cherry picked from commit 67eb191b3ce2dec023eef01a05bdf80ecf6f3cb5 [formerly 36460d13ce99dc8ed276d31b16a2bc181f31610f])
    Reviewed-on: https://chromium-review.googlesource.com/1219488
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3538@{#266}
    Cr-Branched-From: 57781ff9a8651bc653184f680b0a4d8b92b6971f [formerly 79f7c91a2b2a2932cd447fa6f865cb6662fa8fa6]-refs/heads/master@{#587811}
    Former-commit-id: 268f419da75d00579f957e12180e5696e006a0ed

--- chrome/android/java/src/org/chromium/chrome/browser/signin/UnifiedConsentServiceBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/UnifiedConsentServiceBridge.java
@@ -7,7 +7,8 @@ package org.chromium.chrome.browser.signin;
 import org.chromium.chrome.browser.profiles.Profile;
 
 /**
- * Bridge to UnifiedConsentService.
+ * Bridge to UnifiedConsentService. Should only be used if
+ * {@link org.chromium.chrome.browser.ChromeFeatureList.UNIFIED_CONSENT} feature is enabled.
  */
 public class UnifiedConsentServiceBridge {
     private UnifiedConsentServiceBridge() {}
@@ -29,8 +30,27 @@ public class UnifiedConsentServiceBridge {
         return nativeShouldShowConsentBump(Profile.getLastUsedProfile());
     }
 
-    private static native void nativeSetUnifiedConsentGiven(Profile profile, boolean consentGiven);
-    private static native boolean nativeIsUnifiedConsentGiven(Profile lastUsedProfile);
+    /** Returns whether collection of URL-keyed anonymized data is enabled. */
+    public static boolean isUrlKeyedAnonymizedDataCollectionEnabled() {
+        return nativeIsUrlKeyedAnonymizedDataCollectionEnabled(Profile.getLastUsedProfile());
+    }
 
+    /** Sets whether collection of URL-keyed anonymized data is enabled. */
+    public static void setUrlKeyedAnonymizedDataCollectionEnabled(boolean enabled) {
+        nativeSetUrlKeyedAnonymizedDataCollectionEnabled(Profile.getLastUsedProfile(), enabled);
+    }
+
+    /** Returns whether collection of URL-keyed anonymized data is configured by policy. */
+    public static boolean isUrlKeyedAnonymizedDataCollectionManaged() {
+        return nativeIsUrlKeyedAnonymizedDataCollectionManaged(Profile.getLastUsedProfile());
+    }
+
+    private static native void nativeSetUnifiedConsentGiven(Profile profile, boolean consentGiven);
+    private static native boolean nativeIsUnifiedConsentGiven(Profile profile);
     private static native boolean nativeShouldShowConsentBump(Profile profile);
+
+    private static native boolean nativeIsUrlKeyedAnonymizedDataCollectionEnabled(Profile profile);
+    private static native void nativeSetUrlKeyedAnonymizedDataCollectionEnabled(
+            Profile profile, boolean enabled);
+    private static native boolean nativeIsUrlKeyedAnonymizedDataCollectionManaged(Profile profile);
 }
--- chrome/browser/android/signin/unified_consent_service_bridge.cc
+++ chrome/browser/android/signin/unified_consent_service_bridge.cc
@@ -3,8 +3,11 @@
 // found in the LICENSE file.
 
 #include "base/android/jni_android.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_android.h"
 #include "chrome/browser/unified_consent/unified_consent_service_factory.h"
+#include "components/prefs/pref_service.h"
+#include "components/unified_consent/pref_names.h"
 #include "components/unified_consent/unified_consent_service.h"
 #include "jni/UnifiedConsentServiceBridge_jni.h"
 
@@ -39,4 +42,36 @@ static jboolean JNI_UnifiedConsentServiceBridge_ShouldShowConsentBump(
   auto* unifiedConsentService =
       UnifiedConsentServiceFactory::GetForProfile(profile);
   return unifiedConsentService->ShouldShowConsentBump();
+}
+
+static jboolean
+JNI_UnifiedConsentServiceBridge_IsUrlKeyedAnonymizedDataCollectionEnabled(
+    JNIEnv* env,
+    const base::android::JavaParamRef<jclass>& jcaller,
+    const base::android::JavaParamRef<jobject>& profileAndroid) {
+  Profile* profile = ProfileAndroid::FromProfileAndroid(profileAndroid);
+  return profile->GetPrefs()->GetBoolean(
+      unified_consent::prefs::kUrlKeyedAnonymizedDataCollectionEnabled);
+}
+
+static void
+JNI_UnifiedConsentServiceBridge_SetUrlKeyedAnonymizedDataCollectionEnabled(
+    JNIEnv* env,
+    const base::android::JavaParamRef<jclass>& jcaller,
+    const base::android::JavaParamRef<jobject>& profileAndroid,
+    const jboolean enabled) {
+  Profile* profile = ProfileAndroid::FromProfileAndroid(profileAndroid);
+  profile->GetPrefs()->SetBoolean(
+      unified_consent::prefs::kUrlKeyedAnonymizedDataCollectionEnabled,
+      enabled);
+}
+
+static jboolean
+JNI_UnifiedConsentServiceBridge_IsUrlKeyedAnonymizedDataCollectionManaged(
+    JNIEnv* env,
+    const base::android::JavaParamRef<jclass>& jcaller,
+    const base::android::JavaParamRef<jobject>& profileAndroid) {
+  Profile* profile = ProfileAndroid::FromProfileAndroid(profileAndroid);
+  return profile->GetPrefs()->IsManagedPreference(
+      unified_consent::prefs::kUrlKeyedAnonymizedDataCollectionEnabled);
 }
