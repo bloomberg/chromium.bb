f9af4a3166fcb7d8eb8aa0ccb6fb0a2a4805f462
Olivier Robin <olivierrobin@chromium.org>
2018-09-12 07:37:23 +0000

    Inject autofill JS in iframes.
    Bug: 881364
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I23ba4755e9319b98a9c23a71d9edea2d48e49b0d
    Reviewed-on: https://chromium-review.googlesource.com/1209710
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590615}
    Former-commit-id: 41d23bdde3db93542ecd18b65f644676db377540

--- ios/chrome/browser/web/resources/chrome_bundle_all_frames.js
+++ ios/chrome/browser/web/resources/chrome_bundle_all_frames.js
@@ -6,5 +6,7 @@
 goog.provide('__crWeb.chromeBundleAllFrames');
 
 goog.require('__crWeb.accessibility');
+goog.require('__crWeb.autofill');
+goog.require('__crWeb.fill');
 goog.require('__crWeb.form');
 goog.require('__crWeb.print');
