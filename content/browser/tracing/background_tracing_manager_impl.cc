c2b81e224c567068f590994ab91992f171239019
Etienne Bergeron <etienneb@chromium.org>
2018-09-12 15:18:38 +0000

    Fix disabled background tracing categories
    This CL is fixing:
      1) missing comma at the end of a line (missing system_stats)
      2) adding prefix disabled by default
    
    R=oysteine@chromium.org
    
    Bug: 867931
    Change-Id: I1fe47c3ba01a738b20e323099c4c1a8ce481eedb
    Reviewed-on: https://chromium-review.googlesource.com/1220026
    Reviewed-by: oysteine <oysteine@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590685}
    Former-commit-id: 2882d19e0bc8ac455f281c498a877b66675ff94a

--- content/browser/tracing/background_tracing_manager_impl.cc
+++ content/browser/tracing/background_tracing_manager_impl.cc
@@ -669,7 +669,8 @@ TraceConfig BackgroundTracingManagerImpl::GetConfigForCategoryPreset(
     case BackgroundTracingConfigImpl::CategoryPreset::BENCHMARK_NAVIGATION: {
       auto config = TraceConfig(
           "benchmark,toplevel,ipc,base,browser,navigation,omnibox,"
-          "safe_browsing,task_scheduler,task_scheduler_diagnostics"
+          "safe_browsing,task_scheduler,"
+          "disabled-by-default-task_scheduler_diagnostics,"
           "disabled-by-default-system_stats,disabled-by-default-cpu_profiler",
           record_mode);
       // Filter only browser process events.
