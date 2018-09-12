f310490233827d1a5a05044afb88266ddb2d542e
Ned Nguyen <nednguyen@google.com>
2018-09-12 23:04:54 +0000

    Initilize non zero values for benchmark stories that don't have existing timing data
    Bug: 863768
    Cq-Include-Trybots: master.tryserver.chromium.perf:obbs_fyi
    Change-Id: I9733b1f051ffe9fa8b64d14e15704674da0b363a
    Reviewed-on: https://chromium-review.googlesource.com/1199602
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590848}
    Former-commit-id: 8e62b5bc9999b53377aa4f99c234c6c86e03774f

--- tools/perf/core/sharding_map_generator_unittest.py
+++ tools/perf/core/sharding_map_generator_unittest.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import collections
 import copy
 import json
 import os
@@ -44,6 +45,40 @@ class TestShardingMapGenerator(unittest.TestCase):
     self.assertEqual(results['0']['full_time'], 116)
     self.assertEqual(results['1']['full_time'], 177)
     self.assertEqual(results['2']['full_time'], 140)
+
+  def testGenerateShardingMapsWithoutStoryTimingData(self):
+    # Two tests benchmarks are to be sharded between 3 machines. The first one
+    # has 4 stories, each repeat 2 times. The second one has 4 stories
+    # without repeat. Without any assumption about timing, the best sharding
+    # is to shard the first 2 stories of 'foo_benchmark' on shard 1, the next
+    # two stories of 'foo_benchmark' on shard 2, and 'bar_benchmark' entirely on
+    # shard 3.
+    timing_data = []
+    benchmarks_data = [
+      { 'name': 'foo_benchmark',
+        'stories': ['foo_1', 'foo_2', 'foo_3', 'foo_4'],
+        'repeat': 2
+      },
+      { 'name': 'bar_benchmark',
+        'stories': ['bar_1', 'bar_2', 'bar_3', 'bar_4'],
+        'repeat': 1
+      }
+
+    ]
+    sharding_map = sharding_map_generator.generate_sharding_map(
+        benchmarks_data, timing_data, 3, None)
+
+    self.assertEquals(
+      sharding_map['0']['benchmarks'],
+      collections.OrderedDict([('bar_benchmark', {})]))
+
+    self.assertEquals(
+      sharding_map['1']['benchmarks'],
+      collections.OrderedDict([('foo_benchmark', {'end': 2})]))
+
+    self.assertEquals(
+      sharding_map['2']['benchmarks'],
+      collections.OrderedDict([('foo_benchmark', {'begin': 2})]))
 
   def testGeneratePerfSharding(self):
     test_data_dir = os.path.join(os.path.dirname(__file__), 'test_data')
