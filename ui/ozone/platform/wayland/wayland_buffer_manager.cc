99beadd059041ffcf9a1bea35419930c15b2da5c
Maksim Sisov <msisov@igalia.com>
2018-09-13 13:30:49 +0000

    [ozone/wayland] Start using format modifier
    Let's start providing this meta data as well in order to better
    describe the buffer/image.
    
    What is format modifier?
    From drm_fourcc:
    * Format modifiers describe, typically, a re-ordering or modification
    * of the data in a plane of an FB.  This can be used to express tiled/
    * swizzled formats, or compression, or a combination of the two.
    
    Bug: 820047
    Change-Id: I16f87b6130486439a8069ec1e39ff2c6084c4ccc
    Reviewed-on: https://chromium-review.googlesource.com/1219617
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590989}
    Former-commit-id: 53084c963baf5aea98c7b453a8e4c633e267d22e

--- ui/ozone/platform/wayland/wayland_buffer_manager.cc
+++ ui/ozone/platform/wayland/wayland_buffer_manager.cc
@@ -104,8 +104,8 @@ bool WaylandBufferManager::CreateBuffer(base::File file,
   uint32_t fd = file.TakePlatformFile();
   for (size_t i = 0; i < planes_count; i++) {
     zwp_linux_buffer_params_v1_add(params, fd, i /* plane id */, offsets[i],
-                                   strides[i], 0 /* modifier hi */,
-                                   0 /* modifier lo */);
+                                   strides[i], modifiers[i] >> 32,
+                                   modifiers[i] & UINT32_MAX);
   }
   zwp_linux_buffer_params_v1_add_listener(params, &params_listener, this);
   zwp_linux_buffer_params_v1_create(params, width, height, format, 0);
