3f01d34798684762fee912f90eee5e35ea57d6d2
Zhiqiang Zhang <zqzhang@google.com>
2018-09-13 21:08:57 +0000

    [Android MR] Implement FlingingControllerAdapter
    This CL implements the FlingingControllerAdapter backed by
    RemoteMediaClient in RemoteSessionController. The adapter is used to
    bridge calls between blink/content layer and the SDK.
    
    Bug: 711860
    Change-Id: I0ffda9f67e8f64a42f6e9dae9fabc08e62a6d71e
    Reviewed-on: https://chromium-review.googlesource.com/1220675
    Commit-Queue: Zhiqiang Zhang <zqzhang@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591153}
    Former-commit-id: 7d393b04329d5417a078c463beceef78face1741

--- chrome/android/java/src/org/chromium/chrome/browser/media/router/caf/remoting/FlingingControllerAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/router/caf/remoting/FlingingControllerAdapter.java
@@ -4,35 +4,117 @@
 
 package org.chromium.chrome.browser.media.router.caf.remoting;
 
+import com.google.android.gms.cast.MediaStatus;
 import com.google.android.gms.cast.framework.media.RemoteMediaClient;
 
+import org.chromium.base.Log;
 import org.chromium.chrome.browser.media.router.FlingingController;
 import org.chromium.chrome.browser.media.router.MediaController;
+import org.chromium.chrome.browser.media.router.MediaStatusBridge;
 import org.chromium.chrome.browser.media.router.MediaStatusObserver;
 
 /** Adapter class for bridging {@link RemoteMediaClient} and {@link FlingController}. */
-public class FlingingControllerAdapter implements FlingingController {
-    FlingingControllerAdapter(RemotingSessionController sessionController) {}
+public class FlingingControllerAdapter implements FlingingController, MediaController {
+    private static final String TAG = "FlingCtrlAdptr";
+
+    private final RemotingSessionController mSessionController;
+    private MediaStatusObserver mMediaStatusObserver;
+
+    FlingingControllerAdapter(RemotingSessionController sessionController) {
+        mSessionController = sessionController;
+    }
+
+    ////////////////////////////////////////////
+    // FlingingController implementation begin
+    ////////////////////////////////////////////
 
     @Override
     public MediaController getMediaController() {
-        // Not implemented.
-        return null;
+        return this;
     }
 
     @Override
     public void setMediaStatusObserver(MediaStatusObserver observer) {
-        // Not implemented.
+        assert mMediaStatusObserver == null;
+        mMediaStatusObserver = observer;
     }
 
     @Override
     public void clearMediaStatusObserver() {
-        // Not implemented.
+        assert mMediaStatusObserver != null;
+        mMediaStatusObserver = null;
     }
 
     @Override
     public long getApproximateCurrentTime() {
-        // Not implemented.
-        return 0;
+        return mSessionController.getRemoteMediaClient().getApproximateStreamPosition();
+    }
+
+    ////////////////////////////////////////////
+    // FlingingController implementation end
+    ////////////////////////////////////////////
+
+    ////////////////////////////////////////////
+    // MediaController implementation begin
+    ////////////////////////////////////////////
+
+    @Override
+    public void play() {
+        if (!mSessionController.isConnected()) return;
+        mSessionController.getRemoteMediaClient().play().setResultCallback(
+                this ::onMediaCommandResult);
+    }
+
+    @Override
+    public void pause() {
+        if (!mSessionController.isConnected()) return;
+        mSessionController.getRemoteMediaClient().pause().setResultCallback(
+                this ::onMediaCommandResult);
+    }
+
+    @Override
+    public void setMute(boolean mute) {
+        if (!mSessionController.isConnected()) return;
+        mSessionController.getRemoteMediaClient().setStreamMute(mute).setResultCallback(
+                this ::onMediaCommandResult);
+    }
+
+    @Override
+    public void setVolume(double volume) {
+        if (!mSessionController.isConnected()) return;
+        mSessionController.getRemoteMediaClient().setStreamVolume(volume).setResultCallback(
+                this ::onMediaCommandResult);
+    }
+
+    @Override
+    public void seek(long position) {
+        if (!mSessionController.isConnected()) return;
+        mSessionController.getRemoteMediaClient().seek(position).setResultCallback(
+                this ::onMediaCommandResult);
+    }
+
+    ////////////////////////////////////////////
+    // MediaController implementation end
+    ////////////////////////////////////////////
+
+    public void onStatusUpdated() {
+        if (mMediaStatusObserver == null) return;
+
+        MediaStatus mediaStatus = mSessionController.getRemoteMediaClient().getMediaStatus();
+        if (mediaStatus != null) {
+            mMediaStatusObserver.onMediaStatusUpdate(new MediaStatusBridge(mediaStatus));
+        }
+    }
+
+    private void onMediaCommandResult(RemoteMediaClient.MediaChannelResult result) {
+        // When multiple API calls are made in quick succession, "Results have already been set"
+        // IllegalStateExceptions might be thrown from GMS code. We prefer to catch the exception
+        // and noop it, than to crash. This might lead to some API calls never getting their
+        // onResult() called, so we should not rely on onResult() being called for every API call.
+        // See https://crbug.com/853923.
+        if (!result.getStatus().isSuccess()) {
+            Log.e(TAG, "Error when sending command. Status code: %d",
+                    result.getStatus().getStatusCode());
+        }
     }
 }
