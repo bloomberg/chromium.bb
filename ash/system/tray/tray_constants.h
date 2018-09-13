9c544cb0041652c41edae2e09e3476605c588234
Tetsui Ohkubo <tetsui@chromium.org>
2018-09-13 02:52:35 +0000

    Unified: Fix tray button padding with new shelf.
    The horizontal padding of UnifiedSystemTray was not following UX spec.
    Icon sizes will be also changed in a upcoming CL from 16dip to 20dip.
    
    TEST=manual
    BUG=874165
    
    Change-Id: I655806de0404fec19e1c2254c844f01c84226afe
    Reviewed-on: https://chromium-review.googlesource.com/1218803
    Reviewed-by: Manu Cornet <manucornet@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#590902}
    Former-commit-id: 6d50beb00e968d7b53cac2e6c56aeba028be7a9a

--- ash/system/tray/tray_constants.h
+++ ash/system/tray/tray_constants.h
@@ -169,6 +169,7 @@ constexpr gfx::Insets kUnifiedSliderPadding(0, 16);
 constexpr int kUnifiedMenuVerticalPadding = 8;
 constexpr int kUnifiedNotificationCenterSpacing = 16;
 constexpr int kUnifiedTrayCornerRadius = 20;
+constexpr int kUnifiedTrayContentPadding = 5;
 constexpr int kUnifiedTopShortcutSpacing = 16;
 constexpr int kUnifiedNotificationHiddenLineHeight = 20;
 constexpr gfx::Insets kUnifiedTopShortcutPadding(0, 16);
