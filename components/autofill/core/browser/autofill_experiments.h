75962526c8854cbb34f78da865e35ce0cc76b190
Fabio Tirelo <ftirelo@chromium.org>
2018-09-13 15:36:29 +0000

    [AF] Fix medium font weight and remove unused options
    There are many intricacies involved in getting medium font
    weight, since this is not a common terminology across
    platforms. Reusing TypographyProvider::MediumWeightForUI(),
    which implements a best-effort approach for handling those
    issues.
    
    To avoid adding a dependency from //components/autofill to
    //ui/views, this CL also introduces an enum
    ForcedFontWeight to indicate the font weight that should
    be applied. This way, TypographyProvider's uses will be
    restrained to //c/b/ui/views/autofill.
    
    Also remove flag options for "semi-bold" and "extra-bold",
    which were already failed UX pre-analysis, and won't be
    used anyways - so we don't need to add values to the enum
    that will eventually be removed.
    
    Note: some of the code for the dropdown layout experiment
    is leaking to iOS, which was unintended. Will clean it
    up on a follow-up.
    
    Bug: 882525
    Change-Id: I4b3f371c76c7f8e6419b296a9aa70442a8079dd4
    Reviewed-on: https://chromium-review.googlesource.com/1221688
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591023}
    Former-commit-id: 8a2d5a79ca48081949f81d22c280bbe061ec6407

--- components/autofill/core/browser/autofill_experiments.h
+++ components/autofill/core/browser/autofill_experiments.h
@@ -9,7 +9,6 @@
 
 #include "base/strings/string16.h"
 #include "build/build_config.h"
-#include "ui/gfx/font.h"
 
 class PrefService;
 
@@ -29,14 +28,14 @@ extern const base::Feature kAutofillDropdownLayoutExperiment;
 extern const char kAutofillDropdownLayoutParameterName[];
 extern const char kAutofillDropdownLayoutParameterLeadingIcon[];
 extern const char kAutofillDropdownLayoutParameterTrailingIcon[];
-
-extern const base::Feature kAutofillPrimaryInfoStyleExperiment;
-extern const char kAutofillPrimaryInfoFontWeightParameterName[];
-extern const char kAutofillPrimaryInfoFontWeightParameterMedium[];
-extern const char kAutofillPrimaryInfoFontWeightParameterSemiBold[];
-extern const char kAutofillPrimaryInfoFontWeightParameterBold[];
-extern const char kAutofillPrimaryInfoFontWeightParameterExtraBold[];
 #endif  // !defined(OS_ANDROID)
+
+#if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_WIN)
+extern const base::Feature kAutofillPrimaryInfoStyleExperiment;
+extern const char kAutofillForcedFontWeightParameterName[];
+extern const char kAutofillForcedFontWeightParameterMedium[];
+extern const char kAutofillForcedFontWeightParameterBold[];
+#endif  // defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_WIN)
 
 // Returns true if uploading credit cards to Wallet servers is enabled. This
 // requires the appropriate flags and user settings to be true and the user to
@@ -66,19 +65,26 @@ bool OfferStoreUnmaskedCards();
 // Returns whether the account of the active signed-in user should be used.
 bool ShouldUseActiveSignedInAccount();
 
-#if !defined(OS_ANDROID)
-// Sets |font_weight| as the font weight to be used for primary information on
-// the Autofill dropdown for Addresses and Credit Cards. Returns false if
-// feature kAutofillPrimaryInfoStyleExperiment is disabled or if the
-// corresponding feature param is invalid.
-bool ShouldUseCustomFontWeightForPrimaryInfo(gfx::Font::Weight* font_weight);
+#if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_WIN)
+enum class ForcedFontWeight {
+  kDefault,  // No change to the font weight.
+  kMedium,
+  kBold,
+};
 
+// Returns the font weight to be used for primary information on the Autofill
+// dropdown for Addresses and Credit Cards. Returns kDefault if feature
+// kAutofillPrimaryInfoStyleExperiment is disabled or if the corresponding
+// feature param is invalid.
+ForcedFontWeight GetForcedFontWeight();
+#endif  // defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_WIN)
+
+#if !defined(OS_ANDROID)
 enum class ForcedPopupLayoutState {
   kDefault,       // No popup layout forced by experiment.
   kLeadingIcon,   // Experiment forces leading (left in LTR) icon layout.
   kTrailingIcon,  // Experiment forces trailing (right in LTR) icon layout.
 };
-
 // Returns kDefault if no experimental behavior is enabled for
 // kAutofillDropdownLayoutExperiment; returns kLeftIcon or kRightIcon
 // if the experiment param matches kAutofillDropdownLayoutParameterLeadingIcon
