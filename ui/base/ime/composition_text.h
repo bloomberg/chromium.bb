bc8dd0208c77e7c917385a923c8097a4852b1c62
Jani Hautakangas <jani.hautakangas@lge.com>
2018-09-14 06:04:50 +0000

    Reland "[ozone/wayland] Refactor IME deps for ozone platform"
    This is a reland of 9b92c833dd9929b9b3785d8cd16460c8db656013 [formerly 40a87fcf6141373674ddc53cb27fe7042f49410e]
    
    The patch was reverted due to fail in windows 'compile confirm no-op'.
    Compile failed because of empty linux specific gn component which
    resulted after applying sources_assignment_filters on windows builds.
    Relanding patch now by refactoring ime_linux gn component.
    
    Original change's description:
    > [ozone/wayland] Refactor IME deps for ozone platform
    >
    > Refactor base/ime components and dependencies for ozone
    > plaform integration to avoid cyclic dependencies.
    >
    > - Groups ui/base/ime common types into their own GN component
    >   (ime_types) to break a dependency cycle that ozone ime context
    >   integration would cause on ChromeOS.
    > - Groups some of ui/base/ime/linux interfaces into their own GN
    >   component (ime_linux) to break a dependency cycle that ozone
    >   ime context integration would cause on ChromeOS.
    > - Move chromeos source files under is_chromeos flag to avoid
    >   depending them when chromeos is not used, but ime is needed
    >   (kudos to msisov@igalia.com).
    >
    > Bug: 791920
    > Change-Id: I0b1668564dde22c6e2b05ee1323ebbe4a1343475
    > Reviewed-on: https://chromium-review.googlesource.com/1193871
    > Commit-Queue: Jani Hautakangas <jani.hautakangas@lge.com>
    > Reviewed-by: Shu Chen <shuchen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#590072}
    
    Bug: 791920
    Change-Id: I16de600ac66eb53e4e925bc8b8ab292f89a787dc
    Reviewed-on: https://chromium-review.googlesource.com/1224370
    Reviewed-by: Shu Chen <shuchen@chromium.org>
    Commit-Queue: Jani Hautakangas <jani.hautakangas@lge.com>
    Cr-Commit-Position: refs/heads/master@{#591292}
    Former-commit-id: f220c3449cb22b5ee91d81241c7df1e54b900827

--- ui/base/ime/composition_text.h
+++ ui/base/ime/composition_text.h
@@ -9,13 +9,13 @@
 
 #include "base/strings/string16.h"
 #include "ui/base/ime/ime_text_span.h"
-#include "ui/base/ime/ui_base_ime_export.h"
+#include "ui/base/ime/ui_base_ime_types_export.h"
 #include "ui/gfx/range/range.h"
 
 namespace ui {
 
 // A struct represents the status of an ongoing composition text.
-struct UI_BASE_IME_EXPORT CompositionText {
+struct UI_BASE_IME_TYPES_EXPORT CompositionText {
   CompositionText();
   CompositionText(const CompositionText& other);
   ~CompositionText();
