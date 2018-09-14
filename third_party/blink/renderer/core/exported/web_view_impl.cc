c9a004951ea54e508d0fc94be09d817074497dc9
David Bokan <bokan@chromium.org>
2018-09-14 19:25:26 +0000

    Improve rotation check on viewport resize
    Since rotation used to be supported only on Android, where windows can't
    be resized, we made the assumption that if the width changes it must be
    a rotation.
    
    This changes in ChromeOS where entering tablet mode enables rotation but
    also resizes the window (when entering tablet mode). This caused
    inappropriate rotation anchoring.
    
    The underlying issue has been fixed in other patches linked to this bug,
    the rotation trigger should be improved to check that the width and
    height are swapped.
    
    Bug: 831473
    Change-Id: I0acd39d16319d8cb7819faac4c4ce54b6f7a2e46
    Reviewed-on: https://chromium-review.googlesource.com/1224711
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591430}
    Former-commit-id: 432553b5cb40dde1ae7ac1817dc7e2deecbf8e4e

--- third_party/blink/renderer/core/exported/web_view_impl.cc
+++ third_party/blink/renderer/core/exported/web_view_impl.cc
@@ -1504,7 +1504,8 @@ void WebViewImpl::ResizeWithBrowserControls(
 
   bool is_rotation =
       GetPage()->GetSettings().GetMainFrameResizesAreOrientationChanges() &&
-      size_.width && ContentsSize().Width() && new_size.width != size_.width &&
+      size_.width && ContentsSize().Width() && new_size.width == size_.height &&
+      new_size.height == size_.width &&
       !fullscreen_controller_->IsFullscreenOrTransitioning();
   size_ = new_size;
 
