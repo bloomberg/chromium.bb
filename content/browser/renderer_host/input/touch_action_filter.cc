61456a22249c908ad26820e68fcc61171a81453a
Xida Chen <xidachen@chromium.org>
2018-09-17 14:06:08 +0000

    Temporary fix for a crash at TouchActionFilter::ReportTouchAction
    This is a temporary fix to ensure that there will be no crash at
    TouchActionFilter::ReportTouchAction when the |scrolling_touch_action_|
    has no value.
    
    TBR=nzolghadr@chromium.org
    
    Bug: 879511
    Change-Id: I90aa531db743fb989855b2fd42a9a10bf0578561
    Reviewed-on: https://chromium-review.googlesource.com/1227234
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591662}
    Former-commit-id: c300e9a9dc86db06e3ceb156daef990a17012ef8

--- content/browser/renderer_host/input/touch_action_filter.cc
+++ content/browser/renderer_host/input/touch_action_filter.cc
@@ -273,11 +273,12 @@ void TouchActionFilter::ReportTouchAction() {
   // must add one to the upper bound to be able to visualize the number of
   // times |cc::kTouchActionAuto| is hit.
   // https://crbug.com/879511, remove this temporary fix.
-  if (scrolling_touch_action_.has_value()) {
-    UMA_HISTOGRAM_ENUMERATION("TouchAction.EffectiveTouchAction",
-                              scrolling_touch_action_.value(),
-                              cc::kTouchActionMax + 1);
-  }
+  if (!scrolling_touch_action_.has_value())
+    return;
+
+  UMA_HISTOGRAM_ENUMERATION("TouchAction.EffectiveTouchAction",
+                            scrolling_touch_action_.value(),
+                            cc::kTouchActionMax + 1);
 
   // Report how often the effective touch action computed by blink is or is
   // not equivalent to the whitelisted touch action computed by the
