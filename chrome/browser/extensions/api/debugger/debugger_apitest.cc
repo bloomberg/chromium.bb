babbb144446867d72eac2f1159b5e8031a230c0f
Darren Shen <shend@chromium.org>
2018-09-17 01:53:41 +0000

    Revert "[DevTools] Allow debugger extensions to access error page"
    This reverts commit eac909a5954a6c1deb618e688136abcdb4a69c78 [formerly 5cc9be06edfbda2ccea9453d74584a949bb1d246].
    
    Reason for revert: crbug.com/884567
    
    Original change's description:
    > [DevTools] Allow debugger extensions to access error page
    > 
    > Bug: 882589
    > Change-Id: Ie834bfa3e6a14657924d67d179b547bd850072b3
    > Reviewed-on: https://chromium-review.googlesource.com/1217599
    > Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    > Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    > Reviewed-by: Paul Irish <paulirish@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#591547}
    
    TBR=dgozman@chromium.org,nasko@chromium.org,rdevlin.cronin@chromium.org,paulirish@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 882589
    Change-Id: I4fbe9d85deaf80661d1506960132e2c4e0d9efd1
    Reviewed-on: https://chromium-review.googlesource.com/1226726
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591608}
    Former-commit-id: b176d2cbcc4abf8596cab1ad349bebc9b3252b3c

--- chrome/browser/extensions/api/debugger/debugger_apitest.cc
+++ chrome/browser/extensions/api/debugger/debugger_apitest.cc
@@ -164,6 +164,10 @@ testing::AssertionResult DebuggerApiTest::RunAttachFunctionOnTarget(
   return testing::AssertionSuccess();
 }
 
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Debugger) {
+  ASSERT_TRUE(RunExtensionTest("debugger")) << message_;
+}
+
 IN_PROC_BROWSER_TEST_F(DebuggerApiTest,
                        DebuggerNotAllowedOnOtherExtensionPages) {
   // Load another arbitrary extension with an associated resource (popup.html).
@@ -324,8 +328,13 @@ IN_PROC_BROWSER_TEST_F(DebuggerApiTest, InfoBar) {
   EXPECT_EQ(0u, service1->infobar_count());
 }
 
-class DebuggerExtensionApiTest : public ExtensionApiTest {
+class SitePerProcessExtensionApiTest : public ExtensionApiTest {
  public:
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    ExtensionApiTest::SetUpCommandLine(command_line);
+    content::IsolateAllSitesForTesting(command_line);
+  };
+
   void SetUpOnMainThread() override {
     ExtensionApiTest::SetUpOnMainThread();
     host_resolver()->AddRule("*", "127.0.0.1");
@@ -334,19 +343,7 @@ class DebuggerExtensionApiTest : public ExtensionApiTest {
   }
 };
 
-IN_PROC_BROWSER_TEST_F(DebuggerExtensionApiTest, Debugger) {
-  ASSERT_TRUE(RunExtensionTest("debugger")) << message_;
-}
-
-class SitePerProcessDebuggerExtensionApiTest : public DebuggerExtensionApiTest {
- public:
-  void SetUpCommandLine(base::CommandLine* command_line) override {
-    DebuggerExtensionApiTest::SetUpCommandLine(command_line);
-    content::IsolateAllSitesForTesting(command_line);
-  };
-};
-
-IN_PROC_BROWSER_TEST_F(SitePerProcessDebuggerExtensionApiTest, Debugger) {
+IN_PROC_BROWSER_TEST_F(SitePerProcessExtensionApiTest, Debugger) {
   GURL url(embedded_test_server()->GetURL(
       "a.com", "/extensions/api_test/debugger/oopif.html"));
   GURL iframe_url(embedded_test_server()->GetURL(
