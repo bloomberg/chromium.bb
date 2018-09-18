926a44fdd11a192ec360075391d5373329a53849
James MacLean <wjmaclean@chromium.org>
2018-09-18 11:56:49 +0000

    Revert "Tentatively re-enable SitePerProcessHitTestBrowserTest.CursorUpdateReceivedFromCrossSiteIframe."
    This reverts commit b8e10cedfe2901ab493bf63ee80e83c261e1bf88 [formerly 2affd10c4a40bc6c5d34edfcf3dc0a9cc91eba74].
    
    Reason for revert: The test is continuing to flake, so turning it off again on Win.
    
    Original change's description:
    > Tentatively re-enable SitePerProcessHitTestBrowserTest.CursorUpdateReceivedFromCrossSiteIframe.
    > 
    > This CL tests if
    > https://chromium-review.googlesource.com/c/chromium/src/+/1228293
    > has any effect on the flakiness of this test.
    > 
    > Bug: 882458
    > Change-Id: I0bd0503bd223fd4694f9e106882fcd68efeda85d
    > Reviewed-on: https://chromium-review.googlesource.com/1228513
    > Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    > Commit-Queue: James MacLean <wjmaclean@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#591892}
    
    TBR=kenrb@chromium.org,wjmaclean@chromium.org
    
    Change-Id: I651fb628162ec2bf301782d5cc61ad4af043fde4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 882458
    Reviewed-on: https://chromium-review.googlesource.com/1230433
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Commit-Queue: James MacLean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592002}
    Former-commit-id: a3832746f7b9a34125b87f1b21e43038c6780c10

--- content/browser/site_per_process_hit_test_browsertest.cc
+++ content/browser/site_per_process_hit_test_browsertest.cc
@@ -2938,8 +2938,16 @@ void CursorUpdateReceivedFromCrossSiteIframeHelper(
 
 }  // namespace
 
+#if defined(OS_WIN)
+// https://crbug.com/882458
+#define MAYBE_CursorUpdateReceivedFromCrossSiteIframe \
+  DISABLED_CursorUpdateReceivedFromCrossSiteIframe
+#else
+#define MAYBE_CursorUpdateReceivedFromCrossSiteIframe \
+  CursorUpdateReceivedFromCrossSiteIframe
+#endif
 IN_PROC_BROWSER_TEST_P(SitePerProcessHitTestBrowserTest,
-                       CursorUpdateReceivedFromCrossSiteIframe) {
+                       MAYBE_CursorUpdateReceivedFromCrossSiteIframe) {
   CursorUpdateReceivedFromCrossSiteIframeHelper(shell(),
                                                 embedded_test_server());
 }
