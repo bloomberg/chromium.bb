be6e7f1556699fbd3e703c2349d2921e0b807e9f
Eric Seckler <eseckler@chromium.org>
2018-09-18 11:32:05 +0000

    chromeos: Prepare browsertests for PostTask refactor
    This delays initialization of MockMediaRouters in chromeos tests until
    the browser's task executor is initialized.
    
    This is required for the upcoming move of
    BrowserThread::GetTaskRunnerForThread to
    base::Create*TaskRunnerWithTraits. In the future, TaskRunners can only
    be obtained after thread initialization. Since MockMediaRouter obtains
    a TaskRunner in its constructor, this patch moves its creation.
    
    TBR=mtomasz@chromium.org,dominickn@chromium.org
    
    Bug: 878356
    Change-Id: I951e02617de4b81dabe8ed16b8d760028fa6547f
    Reviewed-on: https://chromium-review.googlesource.com/1230038
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#591999}
    Former-commit-id: 1e3605ec3d109901d1a5cd6633f7a396637af811

--- chrome/browser/apps/platform_apps/app_browsertest_util.cc
+++ chrome/browser/apps/platform_apps/app_browsertest_util.cc
@@ -49,6 +49,8 @@ PlatformAppBrowserTest::PlatformAppBrowserTest() {
   ChromeAppDelegate::DisableExternalOpenForTesting();
 }
 
+PlatformAppBrowserTest::~PlatformAppBrowserTest() {}
+
 void PlatformAppBrowserTest::SetUpCommandLine(base::CommandLine* command_line) {
   // Skips ExtensionApiTest::SetUpCommandLine.
   ExtensionBrowserTest::SetUpCommandLine(command_line);
@@ -58,25 +60,26 @@ void PlatformAppBrowserTest::SetUpCommandLine(base::CommandLine* command_line) {
   ProcessManager::SetEventPageSuspendingTimeForTesting(1000);
 }
 
-void PlatformAppBrowserTest::SetUpInProcessBrowserTestFixture() {
-  ExtensionApiTest::SetUpInProcessBrowserTestFixture();
+void PlatformAppBrowserTest::SetUpOnMainThread() {
+  ExtensionApiTest::SetUpOnMainThread();
 #if defined(OS_CHROMEOS)
   // Mock the Media Router in extension api tests. Several of the
   // PlatformAppBrowserTest suites call RunAllPendingInMessageLoop() when there
   // are mojo messages that will call back into Profile creation through the
   // media router.
-  ON_CALL(media_router_, RegisterMediaSinksObserver(testing::_))
+  media_router_ = std::make_unique<media_router::MockMediaRouter>();
+  ON_CALL(*media_router_, RegisterMediaSinksObserver(testing::_))
       .WillByDefault(testing::Return(true));
 
-  CastConfigClientMediaRouter::SetMediaRouterForTest(&media_router_);
+  CastConfigClientMediaRouter::SetMediaRouterForTest(media_router_.get());
 #endif
 }
 
-void PlatformAppBrowserTest::TearDownInProcessBrowserTestFixture() {
+void PlatformAppBrowserTest::TearDownOnMainThread() {
 #if defined(OS_CHROMEOS)
   CastConfigClientMediaRouter::SetMediaRouterForTest(nullptr);
 #endif
-  ExtensionApiTest::TearDownInProcessBrowserTestFixture();
+  ExtensionApiTest::TearDownOnMainThread();
 }
 
 // static
