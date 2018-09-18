d45c9fc3c02b4c0cd74079b2e6b4e8b343d6a65c
Richard Townsend <Richard.Townsend@arm.com>
2018-09-18 18:17:43 +0000

    [css-tables] Force layout when colgroups are removed or added
    When colgroups are modified via Javascript, Blink uses a stale computed
    logical width, resulting in incorrect sizing of auto columns if layout
    was previously done. This patch forces a fresh layout of the affected
    cells, which corrects this behaviour.
    
    Bug: 870296
    Change-Id: I4a12d5a8db689ba1457c6c9960ae8ea9c1dd40d9
    Reviewed-on: https://chromium-review.googlesource.com/1162224
    Commit-Queue: David Grogan <dgrogan@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592115}
    Former-commit-id: 29028a2dea465add1c1433d8260da89adb6709d2

--- third_party/blink/renderer/core/layout/layout_table.cc
+++ third_party/blink/renderer/core/layout/layout_table.cc
@@ -254,16 +254,21 @@ void LayoutTable::InvalidateCachedColumns() {
   column_layout_objects_.resize(0);
 }
 
-void LayoutTable::AddColumn(const LayoutTableCol*) {
+void LayoutTable::ColumnStructureChanged() {
+  column_structure_changed_ = true;
   InvalidateCachedColumns();
+  // We don't really need to recompute our sections, but we do need to update
+  // our column count, whether we have a column, and possibly the logical width
+  // distribution too.
+  SetNeedsSectionRecalc();
+}
+
+void LayoutTable::AddColumn(const LayoutTableCol*) {
+  ColumnStructureChanged();
 }
 
 void LayoutTable::RemoveColumn(const LayoutTableCol*) {
-  InvalidateCachedColumns();
-  // We don't really need to recompute our sections, but we need to update our
-  // column count and whether we have a column. Currently, we only have one
-  // size-fit-all flag but we may have to consider splitting it.
-  SetNeedsSectionRecalc();
+  ColumnStructureChanged();
 }
 
 bool LayoutTable::IsLogicalWidthAuto() const {
@@ -1250,11 +1255,16 @@ void LayoutTable::RecalcSections() const {
        child = child->NextSibling()) {
     if (child->IsTableSection()) {
       LayoutTableSection* section = ToLayoutTableSection(child);
+      if (column_structure_changed_) {
+        section->MarkAllCellsWidthsDirtyAndOrNeedsLayout(
+            LayoutTable::kMarkDirtyAndNeedsLayout);
+      }
       unsigned section_cols = section->NumEffectiveColumns();
       if (section_cols > max_cols)
         max_cols = section_cols;
     }
   }
+  column_structure_changed_ = false;
 
   effective_columns_.resize(max_cols);
   effective_column_positions_.resize(max_cols + 1);
