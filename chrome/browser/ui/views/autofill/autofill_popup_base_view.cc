d4e5f4f7a5495d94d01d99c3626cca1cf6dd7761
Vasilii Sukhanov <vasilii@chromium.org>
2018-09-18 17:14:55 +0000

    Fix the generation dropdown update.
    The bug happened because we didn't call Layout().
    
    TBR=vasilii@chromium.org
    
    (cherry picked from commit b69257f592dfdc7e6daed7b9de44438e64ee397f [formerly d8eaf1b863688da7ea4187094299667fc7c55845])
    
    Bug: 884648
    Change-Id: Iadd1d5b66de2b60103470eccff8862ad14d185a2
    Reviewed-on: https://chromium-review.googlesource.com/1228195
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#591695}
    Reviewed-on: https://chromium-review.googlesource.com/1231194
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3538@{#500}
    Cr-Branched-From: 57781ff9a8651bc653184f680b0a4d8b92b6971f [formerly 79f7c91a2b2a2932cd447fa6f865cb6662fa8fa6]-refs/heads/master@{#587811}
    Former-commit-id: b0e9e05d39e3d779bfd093aa99214760959d3dbf

--- chrome/browser/ui/views/autofill/autofill_popup_base_view.cc
+++ chrome/browser/ui/views/autofill/autofill_popup_base_view.cc
@@ -203,6 +203,7 @@ void AutofillPopupBaseView::DoUpdateBoundsAndRedrawPopup() {
   popup_bounds.Inset(-GetWidget()->GetRootView()->border()->GetInsets());
   GetWidget()->SetBounds(popup_bounds);
 
+  Layout();
   SetClipPath();
   SchedulePaint();
 }
