f49be5efca1e0a5b1691a42f031a1edbf0648a17
Boris Sazonov <bsazonov@chromium.org>
2018-09-18 07:50:14 +0000

    Revert "throttle download notification updates"
    This reverts commit a7be6211e23a084a3ff3c0d7c67877ca0ec16117 [formerly 2de70f1bcf80bebabc6f04fbbf82ca47cdad57dc].
    
    Reason for revert: broke build
    https://uberchromegw.corp.google.com/i/official.android/builders/official-arm/builds/3661
    
    Original change's description:
    > throttle download notification updates
    > 
    > Android throttles the number of notification updates from an app.
    > As a result If important notifications are throttled, the progress
    > notificaiton will get stuck forever. This could happen when there
    > are lots of ongoing downloads. This CL throttles download notification
    > updates before sending them to Android Notification Manager to solve
    > the problem
    > 
    > BUG=876206
    > 
    > TBR=dtrainor@chromium.org
    > (cherry picked from commit 46f824ef770a25c4a0e0448cb25ce8fb286130c5 [formerly aa71ddb8eebed6f0b623463931d7f2a111263e90])
    > 
    > Change-Id: Ic462a51050a2a04e5182991a9446369808ea3bb7
    > Reviewed-on: https://chromium-review.googlesource.com/1222692
    > Commit-Queue: Min Qin <qinmin@chromium.org>
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Cr-Original-Commit-Position: refs/heads/master@{#591112}
    > Reviewed-on: https://chromium-review.googlesource.com/1228200
    > Reviewed-by: Min Qin <qinmin@chromium.org>
    > Cr-Commit-Position: refs/branch-heads/3538@{#451}
    > Cr-Branched-From: 57781ff9a8651bc653184f680b0a4d8b92b6971f [formerly 79f7c91a2b2a2932cd447fa6f865cb6662fa8fa6]-refs/heads/master@{#587811}
    
    TBR=dtrainor@chromium.org,qinmin@chromium.org
    
    Change-Id: Iac32479c65f47103bf38d8925745c9cae6d5b71e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 876206, 885006
    Reviewed-on: https://chromium-review.googlesource.com/1229936
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3538@{#477}
    Cr-Branched-From: 57781ff9a8651bc653184f680b0a4d8b92b6971f [formerly 79f7c91a2b2a2932cd447fa6f865cb6662fa8fa6]-refs/heads/master@{#587811}
    Former-commit-id: 3f9f14f9632407bb3fbdfacac424a71f8615d5f2

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/MockDownloadNotificationService2.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/MockDownloadNotificationService2.java
@@ -27,15 +27,15 @@ public class MockDownloadNotificationService2 extends DownloadNotificationServic
     private final List<Integer> mNotificationIds = new ArrayList<Integer>();
     private boolean mPaused = false;
     private int mLastNotificationId = DEFAULT_NOTIFICATION_ID;
-    private int mNumberOfNotifications;
 
     List<String> mResumedDownloads = new ArrayList<>();
 
     @Override
     void updateNotification(int id, Notification notification) {
-        mNumberOfNotifications++;
-        mLastNotificationId = id;
-        if (!mNotificationIds.contains(id)) mNotificationIds.add(id);
+        if (!mNotificationIds.contains(id)) {
+            mNotificationIds.add(id);
+            mLastNotificationId = id;
+        }
     }
 
     public boolean isPaused() {
@@ -48,10 +48,6 @@ public class MockDownloadNotificationService2 extends DownloadNotificationServic
 
     public int getLastNotificationId() {
         return mLastNotificationId;
-    }
-
-    public int getNumberOfNotifications() {
-        return mNumberOfNotifications;
     }
 
     @Override
