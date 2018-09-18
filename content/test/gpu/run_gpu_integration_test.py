403c9d79868e97c612b44c02a0b22bb716bb2732
bsheedy <bsheedy@chromium.org>
2018-09-18 23:01:27 +0000

    Fail GPU tests if Mac lockscreen detected
    Adds MacOS-only logic to run_gpu_integration_test.py
    that immediately aborts if the lockscreen is detected.
    The device should be automatically unlocked prior to
    the test starting, so a failure to do so needs to be
    easily identified for investigation.
    
    Bug: 884913
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I684a73ace61fd34484af815c7d55517613de16b3
    Reviewed-on: https://chromium-review.googlesource.com/1229366
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592244}
    Former-commit-id: 3c29cf72f42c3f4d0d0140357c392e46d9726946

--- content/test/gpu/run_gpu_integration_test.py
+++ content/test/gpu/run_gpu_integration_test.py
@@ -25,7 +25,25 @@ def PostprocessJSON(file_name, run_test_args):
     with open(file_name, 'w') as f:
       json.dump(test_result, f, indent=2)
 
+def FailIfScreenLockedOnMac():
+  # Detect if the Mac lockscreen is present, which causes issues when running
+  # tests.
+  if not sys.platform.startswith('darwin'):
+    return
+  import Quartz
+  current_session = Quartz.CGSessionCopyCurrentDictionary()
+  if not current_session:
+    # Using the logging module doesn't seem to be guaranteed to show up in
+    # stdout, so use print instead.
+    print ('WARNING: Unable to obtain CGSessionCoppyCurrentDictionary via '
+           'Quartz - unable to determine whether Mac lockscreen is present or '
+           'not.')
+    return
+  if current_session.get('CGSSessionScreenIsLocked'):
+    raise RuntimeError('Mac lockscreen detected, aborting.')
+
 def main():
+  FailIfScreenLockedOnMac()
   rest_args = sys.argv[1:]
   parser = argparse.ArgumentParser(description='Extra argument parser',
                                    add_help=False)
