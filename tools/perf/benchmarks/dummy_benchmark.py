38a2f692226e68adbc08d4f5bdc330548155607b
Ned Nguyen <nednguyen@google.com>
2018-09-19 17:52:26 +0000

    Add components for dummy telemetry benchmarks
    These benchmarks are handy for integration testing our pipeline & to dogfood new
    big features launch, so keeping them around.
    
    *This is part of the effort to add bug components for all perf benchmarks.
    
    Change-Id: Id8d8dee935ccc8b58186b2c6f3ec9ea6e5711397
    BUG: 886965
    Reviewed-on: https://chromium-review.googlesource.com/1234314
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Simon Hatch <simonhatch@chromium.org>
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592463}
    Former-commit-id: 2082aa2352cae2409ea1f102c52d7daa53daa13a

--- tools/perf/benchmarks/dummy_benchmark.py
+++ tools/perf/benchmarks/dummy_benchmark.py
@@ -40,7 +40,7 @@ class _DummyBenchmark(perf_benchmark.PerfBenchmark):
   page_set = dummy_story_set.DummyStorySet
 
 
-@benchmark.Info(emails=['nednguyen@google.com'])
+@benchmark.Info(emails=['nednguyen@google.com'], component='Speed>Telemetry')
 class DummyBenchmarkOne(_DummyBenchmark):
   """A low noise benchmark with mean=100 & std=1."""
 
@@ -52,7 +52,7 @@ class DummyBenchmarkOne(_DummyBenchmark):
     return 'dummy_benchmark.stable_benchmark_1'
 
 
-@benchmark.Info(emails=['nednguyen@google.com'])
+@benchmark.Info(emails=['nednguyen@google.com'], component='Speed>Telemetry')
 class DummyBenchmarkTwo(_DummyBenchmark):
   """A noisy benchmark with mean=50 & std=20."""
 
