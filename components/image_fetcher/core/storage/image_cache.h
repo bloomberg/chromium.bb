29e23f6a7c8ecfe1be7bed09c5acf81ddc268e71
Brandon Wylie <wylieb@chromium.org>
2018-09-19 23:03:37 +0000

    [IC] Implement image cache to control storage classes
    Bug: 882967
    Change-Id: I114ea70a7f5976d8600f81be22b0096606964dac
    Reviewed-on: https://chromium-review.googlesource.com/1225274
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592592}
    Former-commit-id: 6961a2ee421c01adbac944ccffa218c87fb8bb8f

--- components/image_fetcher/core/storage/image_cache.h
+++ components/image_fetcher/core/storage/image_cache.h
@@ -11,6 +11,14 @@
 #include "base/memory/weak_ptr.h"
 #include "components/image_fetcher/core/storage/image_store_types.h"
 
+class PrefRegistrySimple;
+class PrefService;
+
+namespace base {
+class Clock;
+class SequencedTaskRunner;
+}  // namespace base
+
 namespace image_fetcher {
 
 class ImageDataStore;
@@ -19,23 +27,62 @@ class ImageMetadataStore;
 // Persist image meta/data via the given implementations of ImageDataStore and
 // ImageMetadataStore.
 class ImageCache {
+ public:
+  static void RegisterProfilePrefs(PrefRegistrySimple* registry);
+
   ImageCache(std::unique_ptr<ImageDataStore> data_storage,
-             std::unique_ptr<ImageMetadataStore> metadata_storage);
+             std::unique_ptr<ImageMetadataStore> metadata_storage,
+             PrefService* pref_service,
+             base::Clock* clock,
+             scoped_refptr<base::SequencedTaskRunner> task_runner);
   ~ImageCache();
 
   // Adds or updates the image data for the |url|. If the class hasn't been
   // initialized yet, the call is queued.
-  void SaveImage(const std::string& url, const std::string& image_data);
+  void SaveImage(std::string url, std::string image_data);
 
-  // Loads the image data for the |url| and passes it to |callback|.
-  void LoadImage(const std::string& url, ImageDataCallback callback);
+  // Loads the image data for the |url| and passes it to |callback|. If there's
+  // no image in the cache, then an empty string is returned.
+  void LoadImage(std::string url, ImageDataCallback callback);
 
   // Deletes the image data for the |url|.
-  void DeleteImage(const std::string& url);
+  void DeleteImage(std::string url);
 
  private:
+  friend class ImageCacheTest;
+
+  // Queue or start |request| depending if the cache is initialized.
+  void QueueOrStartRequest(base::OnceClosure request);
+  // Start initializing the stores if it hasn't been started already.
+  void MaybeStartInitialization();
+  // Returns true iff both the stores have been initialized.
+  bool AreAllDependenciesInitialized() const;
+  // Receives callbacks when stores are initialized.
+  void OnDependencyInitialized();
+
+  // Saves the |image_data| for |url|.
+  void SaveImageImpl(const std::string& url, std::string image_data);
+  // Loads the data for |url|, calls the user back before updating metadata.
+  void LoadImageImpl(const std::string& url, ImageDataCallback callback);
+  // Deletes the data for |url|.
+  void DeleteImageImpl(const std::string& url);
+
+  // Runs eviction on the data stores.
+  void RunEviction();
+  // Deletes the given keys from the data store.
+  void OnKeysEvicted(std::vector<std::string> keys);
+
+  bool initialization_attempted_;
+  std::vector<base::OnceClosure> queued_requests_;
+
   std::unique_ptr<ImageDataStore> data_store_;
   std::unique_ptr<ImageMetadataStore> metadata_store_;
+  PrefService* pref_service_;
+
+  // Owned by the service which instantiates this.
+  base::Clock* clock_;
+
+  scoped_refptr<base::SequencedTaskRunner> task_runner_;
 
   base::WeakPtrFactory<ImageCache> weak_ptr_factory_;
 
