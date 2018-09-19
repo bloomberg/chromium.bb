d4c9c8b1a9078cb82d0a35497cf372066f6f3de7
Steven Bennetts <stevenjb@chromium.org>
2018-09-19 21:06:36 +0000

    keyboard_util: Remove flag/switch wrappers
    This CL moves some common flag/switch wrappers into the callers to
    remove unnecessary dependencies on keyboard_util.h. With the upcoming
    Chrome/Ash separation, the wrappers cause more confusion than they
    improve readability.
    
    This CL also moves some keyboard_util.cc code only used in
    KeyboardController to that class.
    
    Bug: 843332
    Change-Id: I042928bebfe4f9ea93ccb4fb7a24f8fc46f555cf
    Reviewed-on: https://chromium-review.googlesource.com/1226191
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Shu Chen <shuchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592545}
    Former-commit-id: cedf185f79d7358f0e588d2b69f4ecd7e088e0ab

--- chrome/browser/extensions/api/virtual_keyboard_private/chrome_virtual_keyboard_delegate.cc
+++ chrome/browser/extensions/api/virtual_keyboard_private/chrome_virtual_keyboard_delegate.cc
@@ -11,6 +11,7 @@
 #include "ash/shell.h"
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
@@ -20,6 +21,7 @@
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/common/url_constants.h"
+#include "chromeos/chromeos_features.h"
 #include "components/user_manager/user_manager.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/common/service_manager_connection.h"
@@ -30,6 +32,7 @@
 #include "services/audio/public/cpp/audio_system_factory.h"
 #include "services/service_manager/public/cpp/connector.h"
 #include "ui/aura/window_tree_host.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/keyboard/keyboard_controller.h"
 #include "ui/keyboard/keyboard_switches.h"
 #include "ui/keyboard/keyboard_util.h"
@@ -269,18 +272,24 @@ void ChromeVirtualKeyboardDelegate::OnHasInputDevices(
   // TODO(blakeo): once the old flag's usages have been removed from the
   // extension and all pushes have settled, remove this overly verbose comment.
   features->AppendString(GenerateFeatureFlag(
-      "floatingkeyboard", keyboard::IsFloatingVirtualKeyboardEnabled()));
-  features->AppendString(
-      GenerateFeatureFlag("gesturetyping", keyboard::IsGestureTypingEnabled()));
+      "floatingkeyboard",
+      base::FeatureList::IsEnabled(features::kEnableFloatingVirtualKeyboard)));
   features->AppendString(GenerateFeatureFlag(
-      "gestureediting", keyboard::IsGestureEditingEnabled()));
+      "gesturetyping", !base::CommandLine::ForCurrentProcess()->HasSwitch(
+                           keyboard::switches::kDisableGestureTyping)));
+  features->AppendString(GenerateFeatureFlag(
+      "gestureediting", !base::CommandLine::ForCurrentProcess()->HasSwitch(
+                            keyboard::switches::kDisableGestureEditing)));
   features->AppendString(GenerateFeatureFlag(
       "fullscreenhandwriting",
-      keyboard::IsFullscreenHandwritingVirtualKeyboardEnabled()));
+      base::FeatureList::IsEnabled(
+          features::kEnableFullscreenHandwritingVirtualKeyboard)));
   features->AppendString(GenerateFeatureFlag(
-      "virtualkeyboardmdui", keyboard::IsVirtualKeyboardMdUiEnabled()));
-  features->AppendString(
-      GenerateFeatureFlag("imeservice", keyboard::IsImeServiceEnabled()));
+      "virtualkeyboardmdui",
+      base::FeatureList::IsEnabled(features::kEnableVirtualKeyboardMdUi)));
+  features->AppendString(GenerateFeatureFlag(
+      "imeservice", base::FeatureList::IsEnabled(
+                        chromeos::features::kImeServiceConnectable)));
 
   auto config = keyboard::KeyboardController::Get()->keyboard_config();
   // TODO(oka): Change this to use config.voice_input.
