0d7866ee8ec05645a13b7e0c283c8e9055ccec0c
Christian Biesinger <cbiesinger@chromium.org>
2018-09-19 19:42:11 +0000

    Remove unnecessary include
    After https://chromium-review.googlesource.com/c/chromium/src/+/1232584,
    layout_block.h no longer needs to include ng_constraint_space.h
    
    Change-Id: I9788d6ba0488431facfd3af1eaf6e8f32927ca91
    Reviewed-on: https://chromium-review.googlesource.com/1234218
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592519}
    Former-commit-id: 00a1f63d10a5fad663af2a67d12780d8ae695dfb

--- third_party/blink/renderer/core/layout/layout_block.h
+++ third_party/blink/renderer/core/layout/layout_block.h
@@ -27,13 +27,13 @@
 #include <memory>
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/layout/layout_box.h"
-#include "third_party/blink/renderer/core/layout/ng/ng_constraint_space.h"
 #include "third_party/blink/renderer/platform/wtf/list_hash_set.h"
 
 namespace blink {
 
 struct PaintInfo;
 class LineLayoutBox;
+class NGConstraintSpace;
 class WordMeasurement;
 
 typedef WTF::ListHashSet<LayoutBox*, 16> TrackedLayoutBoxListHashSet;
