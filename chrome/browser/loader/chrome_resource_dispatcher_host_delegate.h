c1ae7cce4326d86dfed7740627373cdad53c4eea
Asanka Herath <asanka@chromium.org>
2018-09-19 16:17:17 +0000

    Revert "Switch UserScriptListener ResourceThrottle to NavigationThrottle"
    This reverts commit d74e4349cf0f331c1c872d4362918aeb122ea59b [formerly 888677bc9b7351f2dd999d3632d718cf6312140f].
    
    Reason for revert: Tests failing on chromium.memory/Linux CFI. See crbug.com/886576
    
    Original change's description:
    > Switch UserScriptListener ResourceThrottle to NavigationThrottle
    > 
    > ResourceThrottles do not work with network service enabled, a
    > NavigationThrottle will work both with NS enabled and disabled.
    > 
    > Bug: 884006
    > Change-Id: Ifea0e59bd67d70b593b46c2a2a2bd4be0e3f98f0
    > Reviewed-on: https://chromium-review.googlesource.com/1227358
    > Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Commit-Queue: Clark DuVall <cduvall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#592182}
    
    TBR=jam@chromium.org,rdevlin.cronin@chromium.org,cduvall@chromium.org
    
    Change-Id: I18519542954bf23e2e76ea83598382f4f00188e3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 884006
    Reviewed-on: https://chromium-review.googlesource.com/1234214
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Commit-Queue: Asanka Herath <asanka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592428}
    Former-commit-id: 18f93e3423700b1faa224a382c90a6b5a4aaf841

--- chrome/browser/loader/chrome_resource_dispatcher_host_delegate.h
+++ chrome/browser/loader/chrome_resource_dispatcher_host_delegate.h
@@ -24,6 +24,10 @@ namespace content {
 class NavigationData;
 }
 
+namespace extensions {
+class UserScriptListener;
+}
+
 namespace net {
 class URLRequest;
 }
@@ -110,6 +114,7 @@ class ChromeResourceDispatcherHostDelegate
   scoped_refptr<DownloadRequestLimiter> download_request_limiter_;
   scoped_refptr<safe_browsing::SafeBrowsingService> safe_browsing_;
 #if BUILDFLAG(ENABLE_EXTENSIONS)
+  scoped_refptr<extensions::UserScriptListener> user_script_listener_;
   std::map<net::URLRequest*, StreamTargetInfo> stream_target_info_;
 #endif
 
