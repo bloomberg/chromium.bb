f80d36c179a9dcbfc5ac2d450a5b42d758eeb68b
Egor Pasko <pasko@chromium.org>
2018-09-19 15:11:26 +0000

    Revert "Reland "Enable Perfetto by default for all telemetry tests""
    This reverts commit efa3697d5b057948dbab08133535a7a8951ac467 [formerly 0f374eff414ec0683aafbe2aae3fd405a4fcc221].
    
    Reason for revert: http://crbug.com/886918
    
    Original change's description:
    > Reland "Enable Perfetto by default for all telemetry tests"
    > 
    > This is a reland of 89e2c18660c2436e519f060d910176cc94e54448 [formerly 32c8ebbeeaee6dfdf46ae51d4c9c5b67dd465fed]
    > 
    > Relanding after https://chromium-review.googlesource.com/c/chromium/src/+/1225306
    > 
    > Original change's description:
    > > Enable Perfetto by default for all telemetry tests
    > >
    > > This will use Perfetto instead of TraceLog for all
    > > trace-based Telemetry tests running on Mac/Windows/Android.
    > >
    > > R=nednguyen@google.com
    > >
    > > Bug: 839084
    > > Cq-Include-Trybots: master.tryserver.chromium.perf:obbs_fyi
    > > Change-Id: I570060cd7f53ea0310f67370b4abec132c8a1a01
    > > Reviewed-on: https://chromium-review.googlesource.com/1176119
    > > Commit-Queue: oysteine <oysteine@chromium.org>
    > > Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    > > Reviewed-by: Ned Nguyen <nednguyen@google.com>
    > > Cr-Commit-Position: refs/heads/master@{#585368}
    > 
    > TBR=nednguyen@google.com
    > 
    > Change-Id: I31b69483281f9fb34c5083888b8f45f00ca95e6e
    > Reviewed-on: https://chromium-review.googlesource.com/1208955
    > Reviewed-by: oysteine <oysteine@chromium.org>
    > Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    > Commit-Queue: oysteine <oysteine@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#592127}
    
    TBR=sullivan@chromium.org,oysteine@chromium.org,nednguyen@google.com
    
    Change-Id: I7e3984b90f8f53d1dface9459ea07d7383e7c6d1
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/1233717
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592400}
    Former-commit-id: f36de8b1bdc40bf31455b052ef63338072b093da

--- tools/perf/core/perf_benchmark.py
+++ tools/perf/core/perf_benchmark.py
@@ -106,18 +106,12 @@ class PerfBenchmark(benchmark.Benchmark):
     options.AppendExtraBrowserArgs(
         '--disable-gpu-process-for-dx12-vulkan-info-collection')
 
+
     # TODO(crbug.com/881469): remove this once Webview support surface
     # synchronization.
     if 'android-webview' in options.browser_type:
       options.AppendExtraBrowserArgs(
           '--disable-features=SurfaceSynchronization')
-
-    # Switch Chrome to use Perfetto instead of TraceLog as the tracing backend,
-    # needed until the feature gets turned on by default everywhere.
-    if options.browser_type != 'reference':
-      options.AppendExtraBrowserArgs(
-          '--enable-features=TracingPerfettoBackend')
-
     self.SetExtraBrowserOptions(options)
 
   @staticmethod
