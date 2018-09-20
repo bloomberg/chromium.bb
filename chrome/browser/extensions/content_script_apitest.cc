f8e29d611fb5dc6ca88e07b0ad6a48af8697af27
Benjamin Ackerman <ackermanb@chromium.org>
2018-09-20 22:35:43 +0000

    [Extensions] Re-enabling the ContentScriptIsolatedWorlds API test.
    The test passes locally and on the trybots now.
    
    Bug: 163097
    Change-Id: I0d7207a2548a568c33241d30ed1bbd7ea2cb4d6a
    Reviewed-on: https://chromium-review.googlesource.com/1236922
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Benjamin Ackerman <ackermanb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592975}
    Former-commit-id: 48f051228ba947beb38459bd096755117df7c764

--- chrome/browser/extensions/content_script_apitest.cc
+++ chrome/browser/extensions/content_script_apitest.cc
@@ -195,14 +195,7 @@ IN_PROC_BROWSER_TEST_F(ContentScriptApiTest, ContentScriptFragmentNavigation) {
   ASSERT_TRUE(RunExtensionTest(extension_name)) << message_;
 }
 
-// Times out on Linux: http://crbug.com/163097
-#if defined(OS_LINUX)
-#define MAYBE_ContentScriptIsolatedWorlds DISABLED_ContentScriptIsolatedWorlds
-#else
-#define MAYBE_ContentScriptIsolatedWorlds ContentScriptIsolatedWorlds
-#endif
-IN_PROC_BROWSER_TEST_F(ContentScriptApiTest,
-                       MAYBE_ContentScriptIsolatedWorlds) {
+IN_PROC_BROWSER_TEST_F(ContentScriptApiTest, ContentScriptIsolatedWorlds) {
   // This extension runs various bits of script and tests that they all run in
   // the same isolated world.
   ASSERT_TRUE(StartEmbeddedTestServer());
