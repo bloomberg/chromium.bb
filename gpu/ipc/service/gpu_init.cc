27754dbbd9970fc89c00443fb488b04a1f1e5f5e
Etienne Pierre-doray <etiennep@chromium.org>
2018-09-20 17:55:00 +0000

    [TaskScheduler]: Use ScopedBlockingCall to mark blocking tasks.
    This CL uses ScopedBlockingCall to mark blocking calls in /gpu.
    
    This CL was created by replacing calls to AssertBlockingAllowed()
    with instantiations of ScopedBlockingCall(MAY_BLOCK).
    I kindly ask the reviewer to make sure of the following:
      - ScopedBlockingCall is instantiated in a scope with minimal CPU usage.
        If this is not the case, ScopedBlockingCall should be instantiated
        closer to the blocking call. See scoped_blocking_call.h for more
        info. Please let me know when/where the blocking call happens if this needs
        to be changed.
      - Parameter |blocking_type| matches expectation (MAY_BLOCK/WILL_BLOCK). See
        BlockingType for more info. While I assumed MAY_BLOCK by default, that might
        not be the best fit if we know that this callsite is guaranteed to block.
      - The ScopedBlockingCall's scope covers the entirety of the blocking operation
        previously asserted against by the AssertBlockingAllowed().
    
    This CL was uploaded by git cl split.
    
    R=piman@chromium.org
    
    Bug: 874080
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I94991868b2383b0f5b359399ea97597518ebea7f
    Reviewed-on: https://chromium-review.googlesource.com/1191410
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592866}
    Former-commit-id: df63a82ac993e212cf74f34355e04d9c8ede40b2

--- gpu/ipc/service/gpu_init.cc
+++ gpu/ipc/service/gpu_init.cc
@@ -9,7 +9,7 @@
 #include "base/command_line.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/threading/thread_restrictions.h"
+#include "base/threading/scoped_blocking_call.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "gpu/command_buffer/service/gpu_switches.h"
@@ -82,7 +82,7 @@ bool CollectGraphicsInfo(GPUInfo* gpu_info,
 #if defined(OS_LINUX) && !defined(OS_CHROMEOS) && !defined(IS_CHROMECAST)
 bool CanAccessNvidiaDeviceFile() {
   bool res = true;
-  base::AssertBlockingAllowed();
+  base::ScopedBlockingCall scoped_blocking_call(base::BlockingType::WILL_BLOCK);
   if (access("/dev/nvidiactl", R_OK) != 0) {
     DVLOG(1) << "NVIDIA device file /dev/nvidiactl access denied";
     res = false;
