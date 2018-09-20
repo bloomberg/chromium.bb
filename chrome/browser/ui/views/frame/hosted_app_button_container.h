a8a42db590220df9726b400799ee21603bc3bd38
Alan Cutter <alancutter@chromium.org>
2018-09-20 02:22:39 +0000

    Reland: Fix inconsistent hosted app title bar text color readability algorithm
    This is a reland of https://chromium-review.googlesource.com/c/chromium/src/+/1218186
    which was reverted in https://chromium-review.googlesource.com/1230435
    due to the added test failing mac-cocoa-rel.
    This reland removes the test from non-Views mac builds as it's only
    applicable with Views enabled.
    
    This CL updates the title text color for Linux and Windows 7 hosted
    app windows to be more consistent with Windows 10, Chrome OS, Android
    and text colors used by Gmail and Inbox.
    
    Before: https://bugs.chromium.org/p/chromium/issues/attachment?aid=357634&signed_aid=ONM068fpCagoCFdrDrfYuw==&inline=1
    After: https://bugs.chromium.org/p/chromium/issues/attachment?aid=357642&signed_aid=2gnT0P6z9_fNMEZAGLKLnw==&inline=1
    See bug for more screenshots.
    
    Bug: 883177, 885164
    Change-Id: Iaeae13799629d418dfa4c4285032bf1d7990c402
    Reviewed-on: https://chromium-review.googlesource.com/1233074
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592661}
    Former-commit-id: 9c6415198fe67aad0461181ac5b544e0f08fe076

--- chrome/browser/ui/views/frame/hosted_app_button_container.h
+++ chrome/browser/ui/views/frame/hosted_app_button_container.h
@@ -77,6 +77,8 @@ class HostedAppButtonContainer : public views::AccessiblePaneView,
                         int y,
                         int available_height);
 
+  SkColor active_color_for_testing() const { return active_color_; }
+
  private:
   friend class HostedAppNonClientFrameViewAshTest;
   friend class HostedAppGlassBrowserFrameViewTest;
