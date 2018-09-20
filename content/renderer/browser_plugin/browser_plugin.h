be7d4b88260b8b9d42cf51fd6179a68e7dfef6a2
Dave Tapuska <dtapuska@chromium.org>
2018-09-20 13:58:36 +0000

    Use appropriate size type for WebPlugin::DidReceiveData
    Using the appropriate type gets rid of a checked cast and a truncation.
    
    BUG=879657
    
    Change-Id: I30e6aa1f28ebcd6e245a95a8662a212e72986829
    Reviewed-on: https://chromium-review.googlesource.com/1234016
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592783}
    Former-commit-id: 6ae67a15a8e291110b3036612c67090bcb99484c

--- content/renderer/browser_plugin/browser_plugin.h
+++ content/renderer/browser_plugin/browser_plugin.h
@@ -130,7 +130,7 @@ class CONTENT_EXPORT BrowserPlugin : public blink::WebPlugin,
                               const blink::WebFloatPoint& position,
                               const blink::WebFloatPoint& screen) override;
   void DidReceiveResponse(const blink::WebURLResponse& response) override;
-  void DidReceiveData(const char* data, int data_length) override;
+  void DidReceiveData(const char* data, size_t data_length) override;
   void DidFinishLoading() override;
   void DidFailLoading(const blink::WebURLError& error) override;
   bool ExecuteEditCommand(const blink::WebString& name) override;
