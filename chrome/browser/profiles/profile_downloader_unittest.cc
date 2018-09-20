e7e2d602d4631e1425be82680392d081aa2498aa
Colin Blundell <blundell@chromium.org>
2018-09-20 08:42:31 +0000

    Fix ProfileDownloader access token fetches after move to IdentityManager
    https://chromium-review.googlesource.com/c/chromium/src/+/1163620
    changed ProfileDownloader to fetch access tokens via IdentityManager
    and PrimaryAccountAccessTokenFetcher. However, ProfileDownloader is
    sometimes used in contexts other than the primary account:
    - ProfileDownloader::StartForAccount() takes in an account ID.
    - This method is invoked from Java on Android via
      ProfileDownloaderAndroid. The Java side passes an |is_pre_signin|
      bool that indicates that this flow can be used in contexts where the
      primary account is not set in IdentityManager/SigninManager.
    
    This CL changes ProfileDownloader to use an AccessTokenFetcher,
    mirroring its fetching of access tokens via |account_id_| from
    before the migration.
    
    This CL also adds a unittest that fails without this change (it
    verifies that when a refresh token is present for the account for
    which ProfileDownloader is asked to fetch info, an access token
    request is made for *that account*).
    
    Bug: 801969
    Change-Id: Ibe3946448530ea22312baefa5f9079c5f6e221eb
    Reviewed-on: https://chromium-review.googlesource.com/1225805
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#591638}(cherry picked from commit 8a4bcba72666293a8269f77c45198027f239f1fe [formerly 941095ea9c05aeac476989da24eec36008bd97f8])
    Reviewed-on: https://chromium-review.googlesource.com/1235576
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3538@{#539}
    Cr-Branched-From: 57781ff9a8651bc653184f680b0a4d8b92b6971f [formerly 79f7c91a2b2a2932cd447fa6f865cb6662fa8fa6]-refs/heads/master@{#587811}
    Former-commit-id: aed7b40366e09cbc546ee6ec03323ea690fba6b1

--- chrome/browser/profiles/profile_downloader_unittest.cc
+++ chrome/browser/profiles/profile_downloader_unittest.cc
@@ -16,6 +16,7 @@
 #include "chrome/test/base/testing_profile.h"
 #include "components/signin/core/browser/account_tracker_service.h"
 #include "components/signin/core/browser/fake_account_fetcher_service.h"
+#include "components/signin/core/browser/fake_profile_oauth2_token_service.h"
 #include "components/signin/core/browser/test_signin_client.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "net/url_request/test_url_fetcher_factory.h"
@@ -35,7 +36,8 @@ const std::string kTestInvalidPictureURL = "invalid_picture_url";
 } // namespace
 
 class ProfileDownloaderTest : public testing::Test,
-                              public ProfileDownloaderDelegate {
+                              public ProfileDownloaderDelegate,
+                              public OAuth2TokenService::DiagnosticsObserver {
  protected:
   ProfileDownloaderTest()
     : thread_bundle_(content::TestBrowserThreadBundle::IO_MAINLOOP) {}
@@ -44,7 +46,7 @@ class ProfileDownloaderTest : public testing::Test,
   void SetUp() override {
     TestingProfile::Builder builder;
     builder.AddTestingFactory(ProfileOAuth2TokenServiceFactory::GetInstance(),
-                              &BuildAutoIssuingFakeProfileOAuth2TokenService);
+                              &BuildFakeProfileOAuth2TokenService);
     builder.AddTestingFactory(AccountFetcherServiceFactory::GetInstance(),
                               FakeAccountFetcherServiceBuilder::BuildForTests);
     profile_ = builder.Build();
@@ -52,8 +54,14 @@ class ProfileDownloaderTest : public testing::Test,
         AccountTrackerServiceFactory::GetForProfile(profile_.get());
     account_fetcher_service_ = static_cast<FakeAccountFetcherService*>(
         AccountFetcherServiceFactory::GetForProfile(profile_.get()));
+    token_service_ = static_cast<FakeProfileOAuth2TokenService*>(
+        ProfileOAuth2TokenServiceFactory::GetForProfile(profile_.get()));
     profile_downloader_.reset(new ProfileDownloader(this));
+
+    token_service_->AddDiagnosticsObserver(this);
   }
+
+  void TearDown() override { token_service_->RemoveDiagnosticsObserver(this); }
 
   bool NeedsProfilePicture() const override { return true; };
   int GetDesiredImageSideLength() const override { return 128; };
@@ -75,12 +83,46 @@ class ProfileDownloaderTest : public testing::Test,
         kTestLocale, picture_url);
   }
 
+  // OAuth2TokenService::DiagnosticsObserver:
+  void OnAccessTokenRequested(
+      const std::string& account_id,
+      const std::string& consumer_id,
+      const OAuth2TokenService::ScopeSet& scopes) override {
+    // This flow should be invoked only when a test has explicitly set up
+    // preconditions so that ProfileDownloader will request access tokens.
+    DCHECK(!on_access_token_request_callback_.is_null());
+
+    account_id_for_access_token_request_ = account_id;
+
+    std::move(on_access_token_request_callback_).Run();
+  }
+
+  void set_on_access_token_requested_callback(base::OnceClosure callback) {
+    on_access_token_request_callback_ = std::move(callback);
+  }
+
   AccountTrackerService* account_tracker_service_;
   FakeAccountFetcherService* account_fetcher_service_;
+  FakeProfileOAuth2TokenService* token_service_;
   content::TestBrowserThreadBundle thread_bundle_;
   std::unique_ptr<Profile> profile_;
+  base::OnceClosure on_access_token_request_callback_;
+  std::string account_id_for_access_token_request_;
   std::unique_ptr<ProfileDownloader> profile_downloader_;
 };
+
+TEST_F(ProfileDownloaderTest, FetchAccessToken) {
+  std::string account_id =
+      account_tracker_service_->SeedAccountInfo(kTestGaia, kTestEmail);
+  token_service_->UpdateCredentials(account_id, "refresh_token");
+
+  base::RunLoop run_loop;
+  set_on_access_token_requested_callback(run_loop.QuitClosure());
+  profile_downloader_->StartForAccount(account_id);
+  run_loop.Run();
+
+  EXPECT_EQ(account_id, account_id_for_access_token_request_);
+}
 
 TEST_F(ProfileDownloaderTest, AccountInfoReady) {
   std::string account_id =
