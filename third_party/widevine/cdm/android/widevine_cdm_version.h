19e1f4504dad2159dd2119c23dd70e6934c9dda8
Xiaohan Wang <xhwang@chromium.org>
2018-09-20 00:36:50 +0000

    media: Remove unncesessary include in android widevine_cdm_version.h
    These includes are already removed on desktop platforms. Also remove it
    on Android. This is in preparation for replacing WIDEVINE_CDM_AVAILABLE
    with a buildflag.
    
    TBR=jrummell@chromium.org
    
    Bug: 349182
    Test: No functionality change
    Change-Id: I7f873247a3c986549caf4864fcb2efcc82918bc5
    Reviewed-on: https://chromium-review.googlesource.com/1231925
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592630}
    Former-commit-id: f4a3540fc2803a51c803f55ebe0c145027db5499

--- third_party/widevine/cdm/android/widevine_cdm_version.h
+++ third_party/widevine/cdm/android/widevine_cdm_version.h
@@ -5,8 +5,6 @@
 #ifndef WIDEVINE_CDM_VERSION_H_
 #define WIDEVINE_CDM_VERSION_H_
 
-#include "third_party/widevine/cdm/widevine_cdm_common.h"
-
 // Indicates that the Widevine CDM is available.
 #define WIDEVINE_CDM_AVAILABLE
 
