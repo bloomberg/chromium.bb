2bc3c895ce76bcb82423bca72ea846eac49839b5
Gauthier Ambard <gambard@chromium.org>
2018-09-20 08:50:44 +0000

    [iOS] Remove legacy toolbar code
    This CL removes the legacy implementation of the toolbar.
    
    Bug: 800266
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: Ic0bcca519ad1340cd537fda7f015b7021a1ea594
    Reviewed-on: https://chromium-review.googlesource.com/1230718
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#592723}
    Former-commit-id: 62cc38ffd3ab8094884fb1df00e4fa98dccd1841

--- ios/chrome/browser/ui/qr_scanner/qr_scanner_view_controller_egtest.mm
+++ ios/chrome/browser/ui/qr_scanner/qr_scanner_view_controller_egtest.mm
@@ -21,7 +21,6 @@
 #include "ios/chrome/browser/ui/qr_scanner/camera_controller.h"
 #include "ios/chrome/browser/ui/qr_scanner/qr_scanner_view.h"
 #include "ios/chrome/browser/ui/qr_scanner/qr_scanner_view_controller.h"
-#import "ios/chrome/browser/ui/toolbar/clean/toolbar_coordinator.h"
 #import "ios/chrome/browser/ui/toolbar/public/features.h"
 #include "ios/chrome/browser/ui/ui_util.h"
 #include "ios/chrome/grit/ios_chromium_strings.h"
