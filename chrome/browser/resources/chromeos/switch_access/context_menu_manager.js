63bda4ddbaeb13134c6f918f1e5ce9803e6436d2
Zach Helfinstein <zhelfins@chromium.org>
2018-09-21 18:34:32 +0000

    Update the rest of SwitchAccess to ES2015
    Bug: 884297
    Change-Id: Iff20201277983eed3d851279ed6e7042ae17d250
    Reviewed-on: https://chromium-review.googlesource.com/1237554
    Commit-Queue: Zach Helfinstein <zhelfins@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593271}
    Former-commit-id: 48df727c4efef6e4fd1d40302846f507e83969b9

--- chrome/browser/resources/chromeos/switch_access/context_menu_manager.js
+++ chrome/browser/resources/chromeos/switch_access/context_menu_manager.js
@@ -5,49 +5,220 @@
 /**
  * Class to handle interactions with the context menu, including moving through
  * and selecting actions.
- *
- * @constructor
- * @param {!AutomationManager} automationManager
- * @param {!chrome.automation.AutomationNode} desktop
  */
-function ContextMenuManager(automationManager, desktop) {
+
+class ContextMenuManager {
   /**
-   * A list of the ContextMenu actions that are currently enabled.
-   * @private {!Array<ContextMenuManager.Action>}
+   * @param {!AutomationManager} automationManager
+   * @param {!chrome.automation.AutomationNode} desktop
    */
-  this.actions_ = [];
+  constructor(automationManager, desktop) {
+    /**
+     * A list of the ContextMenu actions that are currently enabled.
+     * @private {!Array<ContextMenuManager.Action>}
+     */
+    this.actions_ = [];
+
+    /**
+     * The parent automation manager.
+     * @private {!AutomationManager}
+     */
+    this.automationManager_ = automationManager;
+
+    /**
+     * The root node of the screen.
+     * @private {!chrome.automation.AutomationNode}
+     */
+    this.desktop_ = desktop;
+
+    /**
+     * The root node of the context menu.
+     * @private {chrome.automation.AutomationNode}
+     */
+    this.menuNode_;
+
+    /**
+     * The current node of the context menu.
+     * @private {chrome.automation.AutomationNode}
+     */
+    this.node_;
+
+    /**
+     * Keeps track of when we're in the context menu.
+     * @private {boolean}
+     */
+    this.inContextMenu_ = false;
+
+    this.init_();
+  }
 
   /**
-   * The parent automation manager.
-   * @private {!AutomationManager}
+   * Enter the context menu and highlight the first available action.
+   *
+   * @param {!Array<!ContextMenuManager.Action>} actions
    */
-  this.automationManager_ = automationManager;
+  enter(actions) {
+    this.inContextMenu_ = true;
+    if (actions !== this.actions_) {
+      this.actions_ = actions;
+      MessageHandler.sendMessage(
+          MessageHandler.Destination.PANEL, 'setActions', actions);
+    }
+
+    this.node_ = this.menuNode();
+    this.moveForward();
+  }
 
   /**
-   * The root node of the screen.
-   * @private {!chrome.automation.AutomationNode}
+   * Exits the context menu.
    */
-  this.desktop_ = desktop;
+  exit() {
+    this.clearFocusRing_();
+    this.inContextMenu_ = false;
+    if (this.node_)
+      this.node_ = null;
+  }
 
   /**
-   * The root node of the context menu.
-   * @private {chrome.automation.AutomationNode}
+   * Move to the next available action in the menu. If this is no next action,
+   * select the whole context menu to loop again.
+   * @return {boolean} Whether this function had any effect.
    */
-  this.menuNode_;
+  moveForward() {
+    if (!this.node_ || !this.inContextMenu_)
+      return false;
+
+    this.clearFocusRing_();
+    const treeWalker = new AutomationTreeWalker(
+        this.node_, constants.Dir.FORWARD,
+        SwitchAccessPredicate.restrictions(this.menuNode()));
+    const node = treeWalker.next().node;
+    if (!node)
+      this.node_ = this.menuNode();
+    else
+      this.node_ = node;
+    this.updateFocusRing_();
+    return true;
+  }
 
   /**
-   * The current node of the context menu.
-   * @private {chrome.automation.AutomationNode}
+   * Move to the previous available action in the context menu. If we're at the
+   * beginning of the list, start again at the end.
+   * @return {boolean} Whether this function had any effect.
    */
-  this.node_;
+  moveBackward() {
+    if (!this.node_ || !this.inContextMenu_)
+      return false;
+
+    this.clearFocusRing_();
+    const treeWalker = new AutomationTreeWalker(
+        this.node_, constants.Dir.BACKWARD,
+        SwitchAccessPredicate.restrictions(this.menuNode()));
+    let node = treeWalker.next().node;
+
+    // If node is null, find the last enabled button.
+    let lastChild = this.menuNode().lastChild;
+    while (!node && lastChild) {
+      if (SwitchAccessPredicate.isActionable(lastChild)) {
+        node = lastChild;
+        break;
+      } else {
+        lastChild = lastChild.previousSibling;
+      }
+    }
+
+    this.node_ = node;
+    this.updateFocusRing_();
+    return true;
+  }
 
   /**
-   * Keeps track of when we're in the context menu.
-   * @private {boolean}
+   * Perform the action indicated by the current button (or no action if the
+   * entire menu is selected). Then exit the context menu and return to
+   * traditional navigation.
+   * @return {boolean} Whether this function had any effect.
    */
-  this.inContextMenu_ = false;
+  selectCurrentNode() {
+    if (!this.node_ || !this.inContextMenu_)
+      return false;
 
-  this.init_();
+    this.clearFocusRing_();
+    this.node_.doDefault();
+    this.exit();
+    return true;
+  }
+
+  /**
+   * Get the menu node. If it's not defined, search for it.
+   * @return {!chrome.automation.AutomationNode}
+   */
+  menuNode() {
+    if (this.menuNode_)
+      return this.menuNode_;
+
+    const treeWalker = new AutomationTreeWalker(
+        this.desktop_, constants.Dir.FORWARD,
+        SwitchAccessPredicate.contextMenuDiscoveryRestrictions());
+    const node = treeWalker.next().node;
+    if (node) {
+      this.menuNode_ = node;
+      return this.menuNode_;
+    }
+    console.log('Unable to find the context menu.');
+    return this.desktop_;
+  }
+
+  /**
+   * Clear the focus ring.
+   * @private
+   */
+  clearFocusRing_() {
+    this.updateFocusRing_(true);
+  }
+
+  /**
+   * @private
+   */
+  init_() {
+    // Listen for messages from the menu.
+    window.addEventListener('message', this.onMessageReceived_.bind(this));
+  }
+
+  /**
+   * Receive a message from the context menu, and perform the appropriate
+   * action.
+   * @private
+   */
+  onMessageReceived_(event) {
+    this.exit();
+
+    if (event.data === ContextMenuManager.Action.CLICK)
+      this.automationManager_.selectCurrentNode();
+    else if (event.data === ContextMenuManager.Action.OPTIONS)
+      window.switchAccess.showOptionsPage();
+    else if (
+        event.data === ContextMenuManager.Action.SCROLL_DOWN ||
+        event.data === ContextMenuManager.Action.SCROLL_UP ||
+        event.data === ContextMenuManager.Action.SCROLL_LEFT ||
+        event.data === ContextMenuManager.Action.SCROLL_RIGHT)
+      this.automationManager_.scroll(event.data);
+  }
+
+  /**
+   * Send a message to the context menu to update the focus ring around the
+   * current node.
+   * TODO(zhelfins): Revisit focus rings before launch
+   * @private
+   * @param {boolean=} opt_clear If true, will clear the focus ring.
+   */
+  updateFocusRing_(opt_clear) {
+    if (!this.node_)
+      return;
+    const id = this.node_.htmlAttributes.id;
+    const onOrOff = opt_clear ? 'off' : 'on';
+    MessageHandler.sendMessage(
+        MessageHandler.Destination.PANEL, 'setFocusRing', [id, onOrOff]);
+  }
 }
 
 /**
@@ -71,173 +242,3 @@ ContextMenuManager.Action = {
  * @const
  */
 ContextMenuManager.MenuId = 'switchaccess_contextmenu_actions';
-
-ContextMenuManager.prototype = {
-  /**
-   * @private
-   */
-  init_: function() {
-    // Listen for messages from the menu.
-    window.addEventListener('message', this.onMessageReceived_.bind(this));
-  },
-
-  /**
-   * Enter the context menu and highlight the first available action.
-   *
-   * @param {!Array<!ContextMenuManager.Action>} actions
-   */
-  enter: function(actions) {
-    this.inContextMenu_ = true;
-    if (actions !== this.actions_) {
-      this.actions_ = actions;
-      MessageHandler.sendMessage(
-          MessageHandler.Destination.PANEL, 'setActions', actions);
-    }
-
-    this.node_ = this.menuNode();
-    this.moveForward();
-  },
-
-  /**
-   * Exits the context menu.
-   */
-  exit: function() {
-    this.clearFocusRing_();
-    this.inContextMenu_ = false;
-    if (this.node_)
-      this.node_ = null;
-  },
-
-  /**
-   * Move to the next available action in the menu. If this is no next action,
-   * select the whole context menu to loop again.
-   * @return {boolean} Whether this function had any effect.
-   */
-  moveForward: function() {
-    if (!this.node_ || !this.inContextMenu_)
-      return false;
-
-    this.clearFocusRing_();
-    let treeWalker = new AutomationTreeWalker(
-        this.node_, constants.Dir.FORWARD,
-        SwitchAccessPredicate.restrictions(this.menuNode()));
-    let node = treeWalker.next().node;
-    if (!node)
-      this.node_ = this.menuNode();
-    else
-      this.node_ = node;
-    this.updateFocusRing_();
-    return true;
-  },
-
-  /**
-   * Move to the previous available action in the context menu. If we're at the
-   * beginning of the list, start again at the end.
-   * @return {boolean} Whether this function had any effect.
-   */
-  moveBackward: function() {
-    if (!this.node_ || !this.inContextMenu_)
-      return false;
-
-    this.clearFocusRing_();
-    let treeWalker = new AutomationTreeWalker(
-        this.node_, constants.Dir.BACKWARD,
-        SwitchAccessPredicate.restrictions(this.menuNode()));
-    let node = treeWalker.next().node;
-
-    // If node is null, find the last enabled button.
-    let lastChild = this.menuNode().lastChild;
-    while (!node && lastChild) {
-      if (SwitchAccessPredicate.isActionable(lastChild)) {
-        node = lastChild;
-        break;
-      } else {
-        lastChild = lastChild.previousSibling;
-      }
-    }
-
-    this.node_ = node;
-    this.updateFocusRing_();
-    return true;
-  },
-
-  /**
-   * Perform the action indicated by the current button (or no action if the
-   * entire menu is selected). Then exit the context menu and return to
-   * traditional navigation.
-   * @return {boolean} Whether this function had any effect.
-   */
-  selectCurrentNode: function() {
-    if (!this.node_ || !this.inContextMenu_)
-      return false;
-
-    this.clearFocusRing_();
-    this.node_.doDefault();
-    this.exit();
-    return true;
-  },
-
-  /**
-   * Get the menu node. If it's not defined, search for it.
-   * @return {!chrome.automation.AutomationNode}
-   */
-  menuNode: function() {
-    if (this.menuNode_)
-      return this.menuNode_;
-
-    let treeWalker = new AutomationTreeWalker(
-        this.desktop_, constants.Dir.FORWARD,
-        SwitchAccessPredicate.contextMenuDiscoveryRestrictions());
-    treeWalker.next();
-    if (treeWalker.node) {
-      this.menuNode_ = treeWalker.node;
-      return this.menuNode_;
-    }
-    console.log('Unable to find the context menu.');
-    return this.desktop_;
-  },
-
-  /**
-   * Clear the focus ring.
-   * @private
-   */
-  clearFocusRing_: function() {
-    this.updateFocusRing_(true);
-  },
-
-  /**
-   * Receive a message from the context menu, and perform the appropriate
-   * action.
-   * @private
-   */
-  onMessageReceived_: function(event) {
-    this.exit();
-
-    if (event.data === ContextMenuManager.Action.CLICK)
-      this.automationManager_.selectCurrentNode();
-    else if (event.data === ContextMenuManager.Action.OPTIONS)
-      window.switchAccess.showOptionsPage();
-    else if (
-        event.data === ContextMenuManager.Action.SCROLL_DOWN ||
-        event.data === ContextMenuManager.Action.SCROLL_UP ||
-        event.data === ContextMenuManager.Action.SCROLL_LEFT ||
-        event.data === ContextMenuManager.Action.SCROLL_RIGHT)
-      this.automationManager_.scroll(event.data);
-  },
-
-  /**
-   * Send a message to the context menu to update the focus ring around the
-   * current node.
-   * TODO(zhelfins): Revisit focus rings before launch
-   * @private
-   * @param {boolean=} opt_clear If true, will clear the focus ring.
-   */
-  updateFocusRing_: function(opt_clear) {
-    if (!this.node_)
-      return;
-    let id = this.node_.htmlAttributes.id;
-    let onOrOff = opt_clear ? 'off' : 'on';
-    MessageHandler.sendMessage(
-        MessageHandler.Destination.PANEL, 'setFocusRing', [id, onOrOff]);
-  }
-};
