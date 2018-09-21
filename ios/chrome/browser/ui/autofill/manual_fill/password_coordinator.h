8ce3cfa7c55c0dfa7d13d45bdc2dc319c1795d68
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2018-09-21 15:30:44 +0000

    [iOS][MF] Secure filling
    Observe the last focused field to validate it is secure before filling a password
    
    Bug: 878388
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: Ide4407ea51b315c68c50fa010f256cde93f5eb26
    Reviewed-on: https://chromium-review.googlesource.com/1221217
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593194}
    Former-commit-id: 555dcba9d6aeab452e7a233121ad7f1a31249a71

--- ios/chrome/browser/ui/autofill/manual_fill/password_coordinator.h
+++ ios/chrome/browser/ui/autofill/manual_fill/password_coordinator.h
@@ -7,6 +7,7 @@
 
 #import "ios/chrome/browser/ui/coordinators/chrome_coordinator.h"
 
+@class ManualFillInjectionHandler;
 class WebStateList;
 
 // Creates and manages a view controller to present passwords to the user.
@@ -17,12 +18,13 @@ class WebStateList;
 // The view controller of this coordinator.
 @property(nonatomic, readonly) UIViewController* viewController;
 
-// Creates a coordinator that uses a |viewController| a |browserState| and
-// a |webStateList|.
-- (instancetype)initWithBaseViewController:(UIViewController*)viewController
-                              browserState:
-                                  (ios::ChromeBrowserState*)browserState
-                              webStateList:(WebStateList*)webStateList;
+// Creates a coordinator that uses a |viewController|, |browserState|,
+// |webStateList| and an |injectionHandler|.
+- (instancetype)
+initWithBaseViewController:(UIViewController*)viewController
+              browserState:(ios::ChromeBrowserState*)browserState
+              webStateList:(WebStateList*)webStateList
+          injectionHandler:(ManualFillInjectionHandler*)injectionHandler;
 
 // Unavailable, use -initWithBaseViewController:browserState:webStateList:.
 - (instancetype)initWithBaseViewController:(UIViewController*)viewController
