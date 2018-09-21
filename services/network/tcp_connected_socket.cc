1db0fc29b860c345f1405d72e993eba165ceeb20
Daniel Bratell <bratell@opera.com>
2018-09-21 16:33:01 +0000

    Give two ClampBufferSize functions distinct names
    In some jumbo build configurations, two ClampBufferSize functions
    in services/network ended up in the same translation unit which
    caused compilation errors.
    
    Cq-Include-Trybots: luci.chromium.try:linux_mojo
    Change-Id: I6b7ea9c226b114a10d59fa9891e67f83537c5ec6
    Reviewed-on: https://chromium-review.googlesource.com/1238219
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#593215}
    Former-commit-id: b43f546a934e7415ab8e69940bc08d1e83f919e6

--- services/network/tcp_connected_socket.cc
+++ services/network/tcp_connected_socket.cc
@@ -20,7 +20,7 @@ namespace network {
 
 namespace {
 
-int ClampBufferSize(int requested_buffer_size) {
+int ClampTCPBufferSize(int requested_buffer_size) {
   return base::ClampToRange(requested_buffer_size, 0,
                             TCPConnectedSocket::kMaxBufferSize);
 }
@@ -32,7 +32,7 @@ int ConfigureSocket(
     net::TransportClientSocket* socket,
     const mojom::TCPConnectedSocketOptions& tcp_connected_socket_options) {
   int send_buffer_size =
-      ClampBufferSize(tcp_connected_socket_options.send_buffer_size);
+      ClampTCPBufferSize(tcp_connected_socket_options.send_buffer_size);
   if (send_buffer_size > 0) {
     int result = socket->SetSendBufferSize(send_buffer_size);
     DCHECK_NE(net::ERR_IO_PENDING, result);
@@ -41,7 +41,7 @@ int ConfigureSocket(
   }
 
   int receive_buffer_size =
-      ClampBufferSize(tcp_connected_socket_options.receive_buffer_size);
+      ClampTCPBufferSize(tcp_connected_socket_options.receive_buffer_size);
   if (receive_buffer_size > 0) {
     int result = socket->SetReceiveBufferSize(receive_buffer_size);
     DCHECK_NE(net::ERR_IO_PENDING, result);
@@ -168,7 +168,7 @@ void TCPConnectedSocket::SetSendBufferSize(int send_buffer_size,
     std::move(callback).Run(net::ERR_UNEXPECTED);
     return;
   }
-  int result = socket_->SetSendBufferSize(ClampBufferSize(send_buffer_size));
+  int result = socket_->SetSendBufferSize(ClampTCPBufferSize(send_buffer_size));
   std::move(callback).Run(result);
 }
 
@@ -180,7 +180,8 @@ void TCPConnectedSocket::SetReceiveBufferSize(
     std::move(callback).Run(net::ERR_UNEXPECTED);
     return;
   }
-  int result = socket_->SetReceiveBufferSize(ClampBufferSize(send_buffer_size));
+  int result =
+      socket_->SetReceiveBufferSize(ClampTCPBufferSize(send_buffer_size));
   std::move(callback).Run(result);
 }
 
