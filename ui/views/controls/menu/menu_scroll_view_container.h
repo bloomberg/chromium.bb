642475b6b2a3ed2ebbb66d9e6debf1a9e946da69
Thomas Anderson <thomasanderson@chromium.org>
2018-09-24 19:55:22 +0000

    Revert "Reland "Under refresh, use the same menu options for Windows/Linux as under Mac""
    This reverts commit 6a91074679271ab02ae475f881b546f2d8de54f5 [formerly 1922e955fae5e266603059a97356ff8af058f049].
    
    Reason for revert: Causing bug 887424
    
    Original change's description:
    > Reland "Under refresh, use the same menu options for Windows/Linux as under Mac"
    >
    > This is a reland of d9aebbf76188d9cd5f482b925f6560a768e25e2a [formerly 9c5131ad426872a1c931a6e3a27bbd4c469b7721]
    >
    > Cannot reproduce the flake. Relanding for more information.
    >
    > Original change's description:
    > > Under refresh, use the same menu options for Windows/Linux as under Mac
    > >
    > > Use the BubbleBorder for the menus since the drop-shadow under Windows
    > > doesn't track the shape of the menu. We also have much more control over
    > > the shape and depth of the drop-shadow.
    > >
    > > Bug: 837782
    > > Change-Id: I91b9340251c2cd0b60e3b3f66d450a4791113007
    > > Reviewed-on: https://chromium-review.googlesource.com/1159532
    > > Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    > > Reviewed-by: Evan Stade <estade@chromium.org>
    > > Reviewed-by: Michael Wasserman <msw@chromium.org>
    > > Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#590731}
    >
    > TBR=ellyjones@chromium.org
    > TBR=msw@chromium.org
    > TBR=estade@chromium.org
    >
    > Bug: 837782
    > Change-Id: I80a07fb2d716c9372d18a8d48df3e7e00ff286ab
    > Reviewed-on: https://chromium-review.googlesource.com/1229173
    > Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    > Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#592156}
    
    TBR=ellyjones@chromium.org,msw@chromium.org,estade@chromium.org,kylixrd@chromium.org
    NOTRY=true
    NOPRESUBMIT=true
    
    Bug: 837782
    Change-Id: Ie834df815bd1fb1d0f49d7ddc84adecf75ea351e
    Reviewed-on: https://chromium-review.googlesource.com/1240733
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593651}
    Former-commit-id: 869ed2c00969dc8feefa8525eb7e56b0c9097bb5

--- ui/views/controls/menu/menu_scroll_view_container.h
+++ ui/views/controls/menu/menu_scroll_view_container.h
@@ -9,7 +9,6 @@
 #include "ui/views/bubble/bubble_border.h"
 #include "ui/views/controls/menu/menu_types.h"
 #include "ui/views/view.h"
-#include "ui/views/views_export.h"
 
 namespace views {
 
@@ -18,7 +17,7 @@ class SubmenuView;
 // MenuScrollViewContainer contains the SubmenuView (through a MenuScrollView)
 // and two scroll buttons. The scroll buttons are only visible and enabled if
 // the preferred height of the SubmenuView is bigger than our bounds.
-class VIEWS_EXPORT MenuScrollViewContainer : public View {
+class MenuScrollViewContainer : public View {
  public:
   explicit MenuScrollViewContainer(SubmenuView* content_view);
 
@@ -26,7 +25,7 @@ class VIEWS_EXPORT MenuScrollViewContainer : public View {
   View* scroll_down_button() const { return scroll_down_button_; }
   View* scroll_up_button() const { return scroll_up_button_; }
 
-  // External function to check if the bubble border is used.
+  // External function to check if the bubble border is usd.
   bool HasBubbleBorder();
 
   // Offsets the Arrow from the default location.
