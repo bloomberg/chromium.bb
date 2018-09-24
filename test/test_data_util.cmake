99abcb00a6ab2a40bb6516b4baab041135dbf900
Yunqing Wang <yunqingwang@google.com>
2018-09-24 10:38:31 -0700

    Add vase10x10_tiles.txt for lightfiled example
    Added and uploaded vase10x10_tiles.txt file for lightfiled encoder
    and decoder example.
    
    Change-Id: I8925788bc6ebbaf83eb4a19d5cf36886f5ce324e

--- test/test_data_util.cmake
+++ test/test_data_util.cmake
@@ -29,7 +29,8 @@ list(APPEND AOM_TEST_DATA_FILE_NAMES
             "rush_hour_444.y4m"
             "screendata.y4m"
             "niklas_640_480_30.yuv"
-            "vase10x10.yuv")
+            "vase10x10.yuv"
+            "vase10x10_tiles.txt")
 
 if(ENABLE_DECODE_PERF_TESTS AND CONFIG_AV1_ENCODER)
   list(APPEND AOM_TEST_DATA_FILE_NAMES "niklas_1280_720_30.yuv")
