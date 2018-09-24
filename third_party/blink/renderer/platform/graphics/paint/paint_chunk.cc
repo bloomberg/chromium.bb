30d03a3c4ad813a23ad0adb18eceeeba2eebaac3
Philip Rogers <pdr@chromium.org>
2018-09-24 20:25:05 +0000

    [PaintTouchActionRects] Remove unused HitTestData border_rect member
    Bug: 853824
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I6e52260c7966a59434176c20867a11b5505a1cde
    Reviewed-on: https://chromium-review.googlesource.com/1240740
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593661}
    Former-commit-id: 99774c50cd7c63b45b8c3d6abb750f4b8a33a7b0

--- third_party/blink/renderer/platform/graphics/paint/paint_chunk.cc
+++ third_party/blink/renderer/platform/graphics/paint/paint_chunk.cc
@@ -30,19 +30,19 @@ String PaintChunk::ToString() const {
       properties.ToString().Ascii().data(), bounds.ToString().Ascii().data(),
       known_to_be_opaque);
   if (hit_test_data) {
-    ret_val.append(String::Format(
-        ", border_rect=(%s), touch_action_rects=(%u), "
-        "wheel_event_handler_region=(%s) non_fast_scrollable_region=(%s))",
-        hit_test_data->border_rect.ToString().Ascii().data(),
-        hit_test_data->touch_action_rects.size(),
-        hit_test_data->wheel_event_handler_region.Bounds()
-            .ToString()
-            .Ascii()
-            .data(),
-        hit_test_data->non_fast_scrollable_region.Bounds()
-            .ToString()
-            .Ascii()
-            .data()));
+    ret_val.append(
+        String::Format(", touch_action_rects=(%u), "
+                       "wheel_event_handler_region=(%s) "
+                       "non_fast_scrollable_region=(%s))",
+                       hit_test_data->touch_action_rects.size(),
+                       hit_test_data->wheel_event_handler_region.Bounds()
+                           .ToString()
+                           .Ascii()
+                           .data(),
+                       hit_test_data->non_fast_scrollable_region.Bounds()
+                           .ToString()
+                           .Ascii()
+                           .data()));
   } else {
     ret_val.append(")");
   }
