7fa2cde51c35c15ab3dbc666f4a5e11581eac8c1
Etienne Pierre-doray <etiennep@chromium.org>
2018-09-24 22:10:11 +0000

    [TaskScheduler]: Use AssertLongCPUWorkAllowed to better match intent in chrome/browser/android
    Instead of ScopedBlockingCall or AssertBlockingAllowed(),
    AssertLongCPUWorkAllowed is used in FinalizeLauncherIconInBackground()
    and in CreateLauncherIconFromFaviconInBackground().
    
    Original CL intention was:
    This CL uses ScopedBlockingCall to mark blocking calls in /chrome/browser/android/webapps.
    
    This CL was created by replacing calls to AssertBlockingAllowed()
    with instantiations of ScopedBlockingCall(MAY_BLOCK).
    I kindly ask the reviewer to make sure of the following:
      - ScopedBlockingCall is instantiated in a scope with minimal CPU usage.
        If this is not the case, ScopedBlockingCall should be instantiated
        closer to the blocking call. See scoped_blocking_call.h for more
        info. Please let me know when/where the blocking call happens if this needs
        to be changed.
      - Parameter |blocking_type| matches expectation (MAY_BLOCK/WILL_BLOCK). See
        BlockingType for more info. While I assumed MAY_BLOCK by default, that might
        not be the best fit if we know that this callsite is guaranteed to block.
      - The ScopedBlockingCall's scope covers the entirety of the blocking operation
        previously asserted against by the AssertBlockingAllowed().
    
    This CL was uploaded by git cl split.
    
    R=dominickn@chromium.org
    
    Bug: 874080
    Change-Id: I31ed4164dbd93d424b7a3a69f833519d13c6fbf1
    Reviewed-on: https://chromium-review.googlesource.com/1191185
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593708}
    Former-commit-id: 4f2e41beda87f7234d6e85aadf2c05f34613fca8

--- chrome/browser/android/shortcut_helper.cc
+++ chrome/browser/android/shortcut_helper.cc
@@ -16,7 +16,7 @@
 #include "base/optional.h"
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/threading/scoped_blocking_call.h"
+#include "base/threading/thread_restrictions.h"
 #include "base/time/time.h"
 #include "chrome/browser/android/color_helpers.h"
 #include "chrome/browser/android/webapk/chrome_webapk_host.h"
@@ -257,7 +257,7 @@ SkBitmap ShortcutHelper::FinalizeLauncherIconInBackground(
     const SkBitmap& bitmap,
     const GURL& url,
     bool* is_generated) {
-  base::ScopedBlockingCall scoped_blocking_call(base::BlockingType::MAY_BLOCK);
+  base::AssertLongCPUWorkAllowed();
 
   JNIEnv* env = base::android::AttachCurrentThread();
   ScopedJavaLocalRef<jobject> result;
