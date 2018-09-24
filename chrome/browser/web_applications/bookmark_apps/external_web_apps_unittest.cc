48cc05e9ee754333403dbaaffc25bc8bfaed80f0
Ben Wells <benwells@chromium.org>
2018-09-24 08:07:02 +0000

    Add extra options for installing default web apps.
    This adds two new fields: "create_shortcuts" and "launch_container".
    
    "create_shortcuts" is an optional boolean which controls whether OS
    level shortcuts are created. On Chrome OS this controls whether the app is
    pinned to the shelf.
    
    "launch_container" is a required string which can be "window" or "tab"
    and controls what sort of container the web app is launched in.
    
    The existing parameter was also renamed to remove the redundant
    "web_app" prefix and renamed to "app_url" to reflect that it can be any
    URL within the scope of the app that has a link to the manifest.
    
    The unused manifest URL parameter was also removed.
    
    Bug: 855281
    Change-Id: Iff99e06cbd63334d2745fc587939fc15136586f6
    Reviewed-on: https://chromium-review.googlesource.com/1237757
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: Ben Wells <benwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593487}
    Former-commit-id: a364af4a6afa22e077e1ec73d58be3dec026de24

--- chrome/browser/web_applications/bookmark_apps/external_web_apps_unittest.cc
+++ chrome/browser/web_applications/bookmark_apps/external_web_apps_unittest.cc
@@ -41,17 +41,23 @@ TEST_F(ScanDirForExternalWebAppsTest, GoodJson) {
 
   // The good_json directory contains two good JSON files:
   // chrome_platform_status.json and google_io_2016.json.
+  // google_io_2016.json is missing a "create_shortcuts" field, so the default
+  // value of false should be used.
   EXPECT_EQ(2u, app_infos.size());
-  static const char* urls[] = {
-      "https://www.chromestatus.com/features",
-      "https://events.google.com/io2016/?utm_source=web_app_manifest",
+  static const web_app::PendingAppManager::AppInfo test_app_infos[] = {
+      web_app::PendingAppManager::AppInfo(
+          GURL("https://www.chromestatus.com/features"),
+          web_app::PendingAppManager::LaunchContainer::kTab,
+          web_app::PendingAppManager::InstallSource::kExternalDefault,
+          /* create_shortcuts */ true),
+      web_app::PendingAppManager::AppInfo(
+          GURL("https://events.google.com/io2016/?utm_source=web_app_manifest"),
+          web_app::PendingAppManager::LaunchContainer::kWindow,
+          web_app::PendingAppManager::InstallSource::kExternalDefault,
+          /* create_shortcuts */ false),
   };
-  for (const char* url : urls) {
-    EXPECT_TRUE(base::ContainsValue(
-        app_infos,
-        web_app::PendingAppManager::AppInfo(
-            GURL(url), web_app::PendingAppManager::LaunchContainer::kWindow,
-            web_app::PendingAppManager::InstallSource::kExternalDefault)));
+  for (const auto& app_info : test_app_infos) {
+    EXPECT_TRUE(base::ContainsValue(app_infos, app_info));
   }
 }
 
@@ -84,4 +90,49 @@ TEST_F(ScanDirForExternalWebAppsTest, MixedJson) {
     EXPECT_EQ(app_infos[0].url.spec(),
               std::string("https://polytimer.rocks/?homescreen=1"));
   }
+}
+
+TEST_F(ScanDirForExternalWebAppsTest, MissingAppUrl) {
+  auto app_infos =
+      web_app::ScanDirForExternalWebAppsForTesting(test_dir("missing_app_url"));
+
+  // The missing_app_url directory contains one JSON file which is correct
+  // except for a missing "app_url" field.
+  EXPECT_EQ(0u, app_infos.size());
+}
+
+TEST_F(ScanDirForExternalWebAppsTest, InvalidAppUrl) {
+  auto app_infos =
+      web_app::ScanDirForExternalWebAppsForTesting(test_dir("invalid_app_url"));
+
+  // The invalid_app_url directory contains one JSON file which is correct
+  // except for an invalid "app_url" field.
+  EXPECT_EQ(0u, app_infos.size());
+}
+
+TEST_F(ScanDirForExternalWebAppsTest, InvalidCreateShortcuts) {
+  auto app_infos = web_app::ScanDirForExternalWebAppsForTesting(
+      test_dir("invalid_create_shortcuts"));
+
+  // The invalid_create_shortcuts directory contains one JSON file which is
+  // correct except for an invalid "create_shortctus" field.
+  EXPECT_EQ(0u, app_infos.size());
+}
+
+TEST_F(ScanDirForExternalWebAppsTest, MissingLaunchContainer) {
+  auto app_infos = web_app::ScanDirForExternalWebAppsForTesting(
+      test_dir("missing_launch_container"));
+
+  // The missing_launch_container directory contains one JSON file which is
+  // correct except for a missing "launch_container" field.
+  EXPECT_EQ(0u, app_infos.size());
+}
+
+TEST_F(ScanDirForExternalWebAppsTest, InvalidLaunchContainer) {
+  auto app_infos = web_app::ScanDirForExternalWebAppsForTesting(
+      test_dir("invalid_launch_container"));
+
+  // The invalidg_launch_container directory contains one JSON file which is
+  // correct except for an invalid "launch_container" field.
+  EXPECT_EQ(0u, app_infos.size());
 }
