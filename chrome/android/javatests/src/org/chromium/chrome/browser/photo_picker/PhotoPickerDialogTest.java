ab965b021f83b6eb4824283a75e5aec8b187dae7
Finnur Thorarinsson <finnur@chromium.org>
2018-09-24 15:21:31 +0000

    Photo Picker: Re-enable the tests.
    When they were disabled for flakiness, an open bug was
    linked. This bug has long since been fixed, so I have
    reason to believe that they will run. And if not, this
    will give me data to sort through to figure out why
    they were flaky.
    
    Change-Id: Ib5735788d3d4d32772017d3c3161c9ecc5793c75
    Reviewed-on: https://chromium-review.googlesource.com/1238558
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593542}
    Former-commit-id: bb591d1f3a2df6fd8eaad80abefb0e19367d0c0b

--- chrome/android/javatests/src/org/chromium/chrome/browser/photo_picker/PhotoPickerDialogTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/photo_picker/PhotoPickerDialogTest.java
@@ -20,7 +20,6 @@ import org.junit.runner.RunWith;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.ChromeSwitches;
@@ -198,12 +197,7 @@ public class PhotoPickerDialogTest implements PhotoPickerListener, SelectionObse
         });
     }
 
-    /**
-     * Continues to be flaky on bots which doesn't reproduce on local devices,
-     * continuing to investigate offline.
-     */
     @Test
-    @DisabledTest(message = "crbug.com/761060")
     @LargeTest
     public void testNoSelection() throws Throwable {
         createDialog(false, Arrays.asList("image/*")); // Multi-select = false.
@@ -220,12 +214,7 @@ public class PhotoPickerDialogTest implements PhotoPickerListener, SelectionObse
         dismissDialog();
     }
 
-    /**
-     * Continues to be flaky on bots which doesn't reproduce on local devices,
-     * continuing to investigate offline.
-     */
     @Test
-    @DisabledTest(message = "crbug.com/761060")
     @LargeTest
     public void testSingleSelectionPhoto() throws Throwable {
         createDialog(false, Arrays.asList("image/*")); // Multi-select = false.
@@ -245,12 +234,7 @@ public class PhotoPickerDialogTest implements PhotoPickerListener, SelectionObse
         dismissDialog();
     }
 
-    /**
-     * Continues to be flaky on bots which doesn't reproduce on local devices,
-     * continuing to investigate offline.
-     */
     @Test
-    @DisabledTest(message = "crbug.com/761060")
     @LargeTest
     public void testMultiSelectionPhoto() throws Throwable {
         createDialog(true, Arrays.asList("image/*")); // Multi-select = true.
