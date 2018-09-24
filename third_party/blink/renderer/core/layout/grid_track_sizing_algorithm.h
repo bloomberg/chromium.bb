6cc5f051eb0b6e064b7493f4001ad0376c2a0a15
Javier Fernandez <jfernandez@igalia.com>
2018-09-24 16:03:03 +0000

    [css-grid] Performance optimizations in the Baseline alignment code
    Since we integrated the baseline alignment logic in the grid tracks
    sizing algorithm, its impact on performance has grown considerably.
    
    The analysis of the new logic added and its overhead, due to different
    operations, shows that evaluating the item's participation in the
    baseline alignment context is the most expensive one. It's specially
    demanding the evaluation of the grid item's alignment properties.
    
    Considering that currently we are doing this for every grid item, this
    CL propose to reuse the loop we already have to clear the grid item's
    override size to cache the items with a baseline value in their
    alignment CSS properties.
    
    Thanks to this cache we can determine the item's participation in a
    baseline alignment context in the different phases of the track sizing
    algorithm, with almost no cost (compared to the current logic). It may
    be possible to share the cache with the algorithm run for computing the
    grid's intrinsic size; however, if the intrinsic size logic is run
    before the grid's layout, we'll end up duplicating the cache.
    
    Additionally, this cache is also used in the alignment phase of the
    grid layout logic; this change helps to avoid the various issues we
    have been suffering related to the different evaluations of the item's
    participation in baseline during the different phases of the grid
    layout algorithm.
    
    BUG = 873452
    
    Change-Id: Ida27be11ae0f5c455e6077367a277981ab35cec1
    Reviewed-on: https://chromium-review.googlesource.com/1179897
    Commit-Queue: Javier Fernandez <jfernandez@igalia.com>
    Reviewed-by: Manuel Rego <rego@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#593552}
    Former-commit-id: f0179abffe0534ee3487d73436b01c278777993e

--- third_party/blink/renderer/core/layout/grid_track_sizing_algorithm.h
+++ third_party/blink/renderer/core/layout/grid_track_sizing_algorithm.h
@@ -102,6 +102,10 @@ class GridTrackSizingAlgorithm final {
 
   LayoutUnit BaselineOffsetForChild(const LayoutBox&, GridAxis) const;
 
+  void CacheBaselineAlignedItem(const LayoutBox&, GridAxis);
+  void CopyBaselineItemsCache(const GridTrackSizingAlgorithm&, GridAxis);
+  void ClearBaselineItemsCache();
+
   LayoutSize EstimatedGridAreaBreadthForChild(const LayoutBox& child) const;
 
   Vector<GridTrack>& Tracks(GridTrackSizingDirection);
@@ -161,8 +165,9 @@ class GridTrackSizingAlgorithm final {
                                      GridTrackSizingDirection) const;
 
   void ComputeBaselineAlignmentContext();
-  void UpdateBaselineAlignmentContext(LayoutBox&, GridAxis);
+  void UpdateBaselineAlignmentContext(const LayoutBox&, GridAxis);
   bool CanParticipateInBaselineAlignment(const LayoutBox&, GridAxis) const;
+  bool ParticipateInBaselineAlignment(const LayoutBox&, GridAxis) const;
 
   bool IsIntrinsicSizedGridArea(const LayoutBox&, GridAxis) const;
   void ComputeGridContainerIntrinsicSizes();
@@ -239,6 +244,9 @@ class GridTrackSizingAlgorithm final {
   SizingState sizing_state_;
 
   GridBaselineAlignment baseline_alignment_;
+  typedef HashMap<const LayoutBox*, bool> BaselineItemsCache;
+  BaselineItemsCache column_baseline_items_map_;
+  BaselineItemsCache row_baseline_items_map_;
 
   // This is a RAII class used to ensure that the track sizing algorithm is
   // executed as it is suppossed to be, i.e., first resolve columns and then
