e8ce134aa43edf1861b52d359510533068f35a3e
Yuzu Saijo <yuzus@chromium.org>
2018-09-25 06:12:45 +0000

    Run page navigation gc when oom intervention is triggered on the previous page
    When intervention runs and user navigates away from the page, it is likely that intervention runs
    right after the navigation on the new page. This is a bug because it is not the new page's fault
    that the memory usage is high, but the previous page's.
    
    This CL makes page navigation gc run when intervention is triggered on the previous page, so that
    the memory usage of two pages would not overlap in near-oom situation.
    
    Bug: 887223
    Change-Id: I435331410e516d1c87756d84dbd6017d8fff0335
    Reviewed-on: https://chromium-review.googlesource.com/1235854
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593834}
    Former-commit-id: a3be34098f115e8a6f9a91821de47e5fe8e8163e

--- third_party/blink/renderer/bindings/core/v8/v8_gc_for_context_dispose.cc
+++ third_party/blink/renderer/bindings/core/v8/v8_gc_for_context_dispose.cc
@@ -76,8 +76,9 @@ void V8GCForContextDispose::NotifyContextDisposed(
   // memory use and trigger a V8+Blink GC. However, on Android, if the frame
   // will not be reused, the process will likely to be killed soon so skip this.
   if (is_main_frame && frame_reuse_status == WindowProxy::kFrameWillBeReused &&
-      MemoryCoordinator::IsLowEndDevice() &&
-      MemoryCoordinator::IsCurrentlyLowMemory()) {
+      ((MemoryCoordinator::IsLowEndDevice() &&
+        MemoryCoordinator::IsCurrentlyLowMemory()) ||
+       force_page_navigation_gc_)) {
     size_t pre_gc_memory_usage = GetMemoryUsage();
     V8PerIsolateData::MainThreadIsolate()->MemoryPressureNotification(
         v8::MemoryPressureLevel::kCritical);
@@ -88,6 +89,8 @@ void V8GCForContextDispose::NotifyContextDisposed(
         CustomCountHistogram, reduction_histogram,
         ("BlinkGC.LowMemoryPageNavigationGC.Reduction", 1, 512, 50));
     reduction_histogram.Count(reduction / 1024 / 1024);
+
+    force_page_navigation_gc_ = false;
   }
 #endif
   V8PerIsolateData::MainThreadIsolate()->ContextDisposedNotification(
@@ -118,6 +121,10 @@ void V8GCForContextDispose::PseudoIdleTimerFired(TimerBase*) {
 void V8GCForContextDispose::Reset() {
   did_dispose_context_for_main_frame_ = false;
   last_context_disposal_time_ = -1;
+}
+
+void V8GCForContextDispose::SetForcePageNavigationGC() {
+  force_page_navigation_gc_ = true;
 }
 
 }  // namespace blink
--- third_party/blink/renderer/controller/oom_intervention_impl.cc
+++ third_party/blink/renderer/controller/oom_intervention_impl.cc
@@ -7,6 +7,7 @@
 #include "base/metrics/histogram_macros.h"
 #include "mojo/public/cpp/bindings/strong_binding.h"
 #include "third_party/blink/public/platform/platform.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_gc_for_context_dispose.h"
 #include "third_party/blink/renderer/controller/crash_memory_metrics_reporter_impl.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/page/page.h"
@@ -87,6 +88,9 @@ void OomInterventionImpl::Check(TimerBase*) {
     }
     host_->OnHighMemoryUsage();
     timer_.Stop();
+    // Notify V8GCForContextDispose that page navigation gc is needed when
+    // intervention runs, as it indicates that memory usage is high.
+    V8GCForContextDispose::Instance().SetForcePageNavigationGC();
   }
   CrashMemoryMetricsReporterImpl::Instance().WriteIntoSharedMemory(
       current_memory);
