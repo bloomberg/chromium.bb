62b6823aa654f57a1ecbb43bc4674ef8e60454a0
Fredrik Hubinette <hubbe@google.com>
2018-09-25 20:52:11 +0000

    limit parallel media preloading
    When loading many media files, we normally end up having to wait
    for the first request for ALL media files to load before we can
    process the second request of the first media file. By limiting
    the number of parallel preloads, time-to-first frame should improve
    for all but the last media file.
    
    However, this feature is checked in behind a feature flag, so that
    we can run some expriments on it before turning it on by default.
    
    Bug: 884820
    Change-Id: Ib0c366325d82fb59e6ad896bf985b1e828143a82
    Reviewed-on: https://chromium-review.googlesource.com/1228486
    Commit-Queue: Fredrik Hubinette <hubbe@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594090}
    Former-commit-id: 395ca8adef07ee3e2a8644dbb9c5725373f246b2

--- media/base/media_switches.cc
+++ media/base/media_switches.cc
@@ -349,6 +349,11 @@ const base::Feature kExternalClearKeyForTesting{
 const base::Feature kHardwareSecureDecryption{
     "HardwareSecureDecryption", base::FEATURE_DISABLED_BY_DEFAULT};
 
+// Limits number of media tags loading in parallel to 6. This speeds up
+// preloading of any media that requires multiple requests to preload.
+const base::Feature kLimitParallelMediaPreloading{
+    "LimitParallelMediaPreloading", base::FEATURE_DISABLED_BY_DEFAULT};
+
 // Enables low-delay video rendering in media pipeline on "live" stream.
 const base::Feature kLowDelayVideoRenderingOnLiveStream{
     "low-delay-video-rendering-on-live-stream",
--- media/blink/resource_multibuffer_data_provider.cc
+++ media/blink/resource_multibuffer_data_provider.cc
@@ -80,11 +80,11 @@ void ResourceMultiBufferDataProvider::Start() {
   }
 
   // Prepare the request.
-  WebURLRequest request(url_data_->url());
-  request.SetRequestContext(is_client_audio_element_
-                                ? WebURLRequest::kRequestContextAudio
-                                : WebURLRequest::kRequestContextVideo);
-  request.SetHTTPHeaderField(
+  auto request = std::make_unique<WebURLRequest>(url_data_->url());
+  request->SetRequestContext(is_client_audio_element_
+                                 ? WebURLRequest::kRequestContextAudio
+                                 : WebURLRequest::kRequestContextVideo);
+  request->SetHTTPHeaderField(
       WebString::FromUTF8(net::HttpRequestHeaders::kRange),
       WebString::FromUTF8(
           net::HttpByteRange::RightUnbounded(byte_pos()).GetHeaderValue()));
@@ -94,8 +94,8 @@ void ResourceMultiBufferDataProvider::Start() {
     // This lets the data reduction proxy know that we don't have anything
     // previously cached data for this resource. We can only send it if this is
     // the first request for this resource.
-    request.SetHTTPHeaderField(WebString::FromUTF8("chrome-proxy"),
-                               WebString::FromUTF8("frfr"));
+    request->SetHTTPHeaderField(WebString::FromUTF8("chrome-proxy"),
+                                WebString::FromUTF8("frfr"));
   }
 
   // We would like to send an if-match header with the request to
@@ -105,7 +105,7 @@ void ResourceMultiBufferDataProvider::Start() {
   // along the way. See crbug/504194 and crbug/689989 for more information.
 
   // Disable compression, compression for audio/video doesn't make sense...
-  request.SetHTTPHeaderField(
+  request->SetHTTPHeaderField(
       WebString::FromUTF8(net::HttpRequestHeaders::kAcceptEncoding),
       WebString::FromUTF8("identity;q=1, *;q=0"));
 
@@ -117,15 +117,24 @@ void ResourceMultiBufferDataProvider::Start() {
     options.preflight_policy =
         network::mojom::CORSPreflightPolicy::kPreventPreflight;
 
-    request.SetFetchRequestMode(network::mojom::FetchRequestMode::kCORS);
+    request->SetFetchRequestMode(network::mojom::FetchRequestMode::kCORS);
     if (url_data_->cors_mode() != UrlData::CORS_USE_CREDENTIALS) {
-      request.SetFetchCredentialsMode(
+      request->SetFetchCredentialsMode(
           network::mojom::FetchCredentialsMode::kSameOrigin);
     }
   }
+
   active_loader_ =
       url_data_->url_index()->fetch_context()->CreateUrlLoader(options);
-  active_loader_->LoadAsynchronously(request, this);
+
+  url_data_->WaitToLoad(
+      base::BindOnce(&ResourceMultiBufferDataProvider::StartLoading,
+                     weak_factory_.GetWeakPtr(), std::move(request)));
+}
+
+void ResourceMultiBufferDataProvider::StartLoading(
+    std::unique_ptr<WebURLRequest> request) {
+  active_loader_->LoadAsynchronously(*request, this);
 }
 
 /////////////////////////////////////////////////////////////////////////////
