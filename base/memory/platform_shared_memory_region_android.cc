54bef1811c201408b695d6b404fc86f444135711
Ben Kelly <wanderview@chromium.org>
2018-09-25 18:04:46 +0000

    Align ashmem_create_region() allocations.
    The ashmem_create_region() API documentation indicates the size should
    be specified in "page-aligned bytes".
    
    Bug: 887357
    Change-Id: I874914f57408663a380d39d61fddbaf986e6ff24
    Reviewed-on: https://chromium-review.googlesource.com/1238673
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Alexandr Ilin <alexilin@chromium.org>
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594003}
    Former-commit-id: 5d14fa233a666ecead9d0ef1512a7db36e5244cd

--- base/memory/platform_shared_memory_region_android.cc
+++ base/memory/platform_shared_memory_region_android.cc
@@ -6,8 +6,10 @@
 
 #include <sys/mman.h>
 
+#include "base/bits.h"
 #include "base/memory/shared_memory_tracker.h"
 #include "base/posix/eintr_wrapper.h"
+#include "base/process/process_metrics.h"
 #include "third_party/ashmem/ashmem.h"
 
 namespace base {
@@ -159,7 +161,9 @@ PlatformSharedMemoryRegion PlatformSharedMemoryRegion::Create(Mode mode,
   if (size == 0)
     return {};
 
-  if (size > static_cast<size_t>(std::numeric_limits<int>::max()))
+  // Align size as required by ashmem_create_region() API documentation.
+  size_t rounded_size = bits::Align(size, GetPageSize());
+  if (rounded_size > static_cast<size_t>(std::numeric_limits<int>::max()))
     return {};
 
   CHECK_NE(mode, Mode::kReadOnly) << "Creating a region in read-only mode will "
@@ -168,7 +172,7 @@ PlatformSharedMemoryRegion PlatformSharedMemoryRegion::Create(Mode mode,
   UnguessableToken guid = UnguessableToken::Create();
 
   ScopedFD fd(ashmem_create_region(
-      SharedMemoryTracker::GetDumpNameForTracing(guid).c_str(), size));
+      SharedMemoryTracker::GetDumpNameForTracing(guid).c_str(), rounded_size));
   if (!fd.is_valid()) {
     DPLOG(ERROR) << "ashmem_create_region failed";
     return {};
