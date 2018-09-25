17704f5005980f45299ce59322e579cd57f7d5c7
Daniel Vogelheim <vogelheim@chromium.org>
2018-09-25 00:58:55 +0000

    v8 bindings: Move v8_cross_origin_setter_info to platform/bindings.
    This makes the header accessible to all platform clients.
    Preparation for https://crrev.com/c/1238465
    
    Bug: 739170
    Change-Id: Ia30feccae93704f734f0892a8f654f711e3f7251
    Reviewed-on: https://chromium-review.googlesource.com/1240315
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#593786}
    Former-commit-id: ac216a9d0e8d2afe28ce08125653777b89aa77de

--- third_party/blink/renderer/platform/bindings/v8_cross_origin_setter_info.h
+++ third_party/blink/renderer/platform/bindings/v8_cross_origin_setter_info.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef THIRD_PARTY_BLINK_RENDERER_BINDINGS_CORE_V8_V8_CROSS_ORIGIN_SETTER_INFO_H_
-#define THIRD_PARTY_BLINK_RENDERER_BINDINGS_CORE_V8_V8_CROSS_ORIGIN_SETTER_INFO_H_
+#ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_BINDINGS_V8_CROSS_ORIGIN_SETTER_INFO_H_
+#define THIRD_PARTY_BLINK_RENDERER_PLATFORM_BINDINGS_V8_CROSS_ORIGIN_SETTER_INFO_H_
 
 #include "third_party/blink/renderer/platform/wtf/allocator.h"
 #include "v8/include/v8.h"
@@ -36,4 +36,4 @@ class V8CrossOriginSetterInfo {
 
 }  // namespace blink
 
-#endif  // THIRD_PARTY_BLINK_RENDERER_BINDINGS_CORE_V8_V8_CROSS_ORIGIN_SETTER_INFO_H_
+#endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_BINDINGS_V8_CROSS_ORIGIN_SETTER_INFO_H_
