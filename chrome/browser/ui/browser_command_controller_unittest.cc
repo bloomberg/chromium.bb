bcc3ad123a8e1ffe399fdc3a8df051be5ff3d428
Mihai Sardarescu <msarda@chromium.org>
2018-09-26 09:20:05 +0000

    Revert "Remove IDC_SHOW_SYNC_SETUP"
    This reverts commit 2fde92b1093e30d1f0fb452ac1a2310bc4c48610 [formerly eafee2dc30fa6fdb34e07c664fd858a361cb5424].
    
    Reason for revert: Still needed for http://crbug.com/888776
    
    Original change's description:
    > Remove IDC_SHOW_SYNC_SETUP
    > 
    > IDC_SHOW_SYNC_SETUP and IDC_SHOW_SIGNIN have the same integer value. This
    > CL removes IDC_SHOW_SYNC_SETUP to avoid confusion. It also cleans up
    > the code in browser_command_controller that was setting both IDC_SHOW_SYNC_SETUP
    > and IDC_SHOW_SIGNIN without knowing that they are in fact the same thing.
    > 
    > Bug: 870239
    > Change-Id: I3b1ac005d3230fc581e6163a53ad20a84d124402
    > Reviewed-on: https://chromium-review.googlesource.com/1236263
    > Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#593138}
    
    TBR=sky@chromium.org,msarda@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 870239
    Change-Id: I50cd0042f2630dca0bafe4540c2c32411acf0338
    Reviewed-on: https://chromium-review.googlesource.com/1245381
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594261}
    Former-commit-id: 2ec520568c51bcd167a1ed24baa3a6f45023f02b

--- chrome/browser/ui/browser_command_controller_unittest.cc
+++ chrome/browser/ui/browser_command_controller_unittest.cc
@@ -341,7 +341,6 @@ TEST_F(BrowserCommandControllerFullscreenTest,
 
     //         Command ID        |      tab mode      |      fullscreen     |
     //                           | enabled | reserved | enabled  | reserved |
-    // clang-format off
     { IDC_OPEN_CURRENT_URL,        true,     false,     false,     false    },
     { IDC_FOCUS_TOOLBAR,           true,     false,     false,     false    },
     { IDC_FOCUS_LOCATION,          true,     false,     false,     false    },
@@ -370,8 +369,6 @@ TEST_F(BrowserCommandControllerFullscreenTest,
     { IDC_SELECT_PREVIOUS_TAB,     true,     true,      true,      false    },
     { IDC_EXIT,                    true,     true,      true,      true     },
     { IDC_SHOW_AS_TAB,             false,    false,     false,     false    },
-    { IDC_SHOW_SIGNIN,             true,     false,      true,      false   },
-    // clang-format on
   };
   const content::NativeWebKeyboardEvent key_event(
       blink::WebInputEvent::kTypeFirst, 0,
@@ -473,7 +470,7 @@ TEST_F(BrowserCommandControllerTest, OptionsConsistency) {
   EXPECT_TRUE(chrome::IsCommandEnabled(browser(), IDC_OPTIONS));
 }
 
-TEST_F(BrowserCommandControllerTest, IncognitoModeShowSigninCommand) {
+TEST_F(BrowserCommandControllerTest, IncognitoModeShowSyncSetupCommand) {
   // Set up a profile with an off the record profile.
   std::unique_ptr<TestingProfile> profile1 = TestingProfile::Builder().Build();
   Profile* profile2 = profile1->GetOffTheRecordProfile();
@@ -489,6 +486,6 @@ TEST_F(BrowserCommandControllerTest, IncognitoModeShowSigninCommand) {
   chrome::BrowserCommandController command_controller(browser2.get());
   const CommandUpdater* command_updater = &command_controller;
 
-  // Check that the IDC_SHOW_SIGNIN command is enabled in incognito.
-  EXPECT_TRUE(command_updater->IsCommandEnabled(IDC_SHOW_SIGNIN));
+  // Check that the IDC_SHOW_SYNC_SETUP command is enabled in incognito.
+  EXPECT_TRUE(command_updater->IsCommandEnabled(IDC_SHOW_SYNC_SETUP));
 }
