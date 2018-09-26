88538567f04ac5069c816c9e94ae7e5cf1ebda5a
Mark Cogan <marq@google.com>
2018-09-26 08:54:16 +0000

    [iOS] Give NTP Recent Tabs view its own favicon loader.
    The recent tabs view for the NTP was using the tab switcher favicon
    loading utility. Since that's going to be deleted, this CL copies it
    into the NTP recent tabs view utils file.
    
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I6c57925d4d483a856fd63d3f2aeaa8720dc9962b
    Reviewed-on: https://chromium-review.googlesource.com/1233744
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594256}
    Former-commit-id: 02dc4d6eb9a6291530672fc195374d2fab562b83

--- ios/chrome/browser/ui/ntp/recent_tabs/views/views_utils.h
+++ ios/chrome/browser/ui/ntp/recent_tabs/views/views_utils.h
@@ -7,6 +7,16 @@
 
 #import <UIKit/UIKit.h>
 
+class GURL;
+@class FaviconAttributes;
+@class UIImage;
+
+namespace ios {
+class ChromeBrowserState;
+}  // namespace ios
+
+typedef void (^FaviconGetterCompletionBlock)(FaviconAttributes*);
+
 namespace recent_tabs {
 
 // Returns an autoreleased UILabel.
@@ -20,6 +30,10 @@ UIColor* GetSubtitleColorGray();
 UIColor* GetIconColorBlue();
 UIColor* GetIconColorGray();
 
+// Gets the favicon for |url|, calls |block| when loaded.
+void GetFavicon(GURL const& url,
+                ios::ChromeBrowserState* browserState,
+                FaviconGetterCompletionBlock block);
 }  // namespace recent_tabs
 
 #endif  // IOS_CHROME_BROWSER_UI_NTP_RECENT_TABS_VIEWS_VIEWS_UTILS_H_
