9f9156d13a4b174b7ca1c1e90f5d56aaa589d723
Moe Ahmadi <mahmadi@chromium.org>
2018-09-26 17:36:38 +0000

    Revert "[fuchsia] Generate BUILD.gn from SDK metadata, migrate FIDL targets."
    This reverts commit 14e4c6d71b224d9f7d63f54d04290326c40473d0 [formerly dfd34b91e8de763ae7fc52eb12a7534a3e4b97a9].
    
    Reason for revert:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Fuchsia%20ARM64%20Cast%20Audio/37042
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Fuchsia%20x64%20Cast%20Audio/36915
    
    Original change's description:
    > [fuchsia] Generate BUILD.gn from SDK metadata, migrate FIDL targets.
    > 
    > This script uses the metadata JSON files in the SDK to generate a
    > BUILD.gn in //third_party/fuchsia-sdk/sdk. The build file is
    > regenerated whenever a new SDK is downloaded using "gclient sync".
    > 
    > Only FIDL libraries are migrated for now. Additional metadata types can
    > be migrated in followup CLs.
    > 
    > 
    > Bug: 888753
    > Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    > Change-Id: If87010006cd23e90d970da4cb84e6de35804b7dd
    > Reviewed-on: https://chromium-review.googlesource.com/1241842
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    > Reviewed-by: Scott Graham <scottmg@chromium.org>
    > Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#594360}
    
    TBR=sky@chromium.org,kmarshall@chromium.org,sergeyu@chromium.org,scottmg@chromium.org
    
    Change-Id: If818310da713c18990fbed0f6c006bb90d41100d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 888753
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Reviewed-on: https://chromium-review.googlesource.com/1246296
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594372}
    Former-commit-id: 02086b936ca667766ef89c7703186fd40e35550d

--- tools/fuchsia/local-sdk.py
+++ tools/fuchsia/local-sdk.py
@@ -91,10 +91,6 @@ def main(args):
   shutil.rmtree(tempdir)
   os.chdir(original_dir)
 
-  subprocess.check_call([os.path.join(REPOSITORY_ROOT, 'third_party',
-                                      'fuchsia-sdk',
-                                      'gen_build_defs.py')])
-
   return 0
 
 
