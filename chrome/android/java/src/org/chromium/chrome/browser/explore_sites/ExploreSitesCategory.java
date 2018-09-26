956bd2b18f9904a76f1a79bb46dbf677a97086c3
Justin DeWitt <dewittj@chromium.org>
2018-09-26 19:17:58 +0000

    [EoS] Adds support for default category icons.
    This adds the Material icons we intend to use if
    the categories are not yet available - mostly if
    the user is offline during the First Run experience,
    or if the sqlite DB got corrupted for some reason.
    
    Bug: 888176
    Change-Id: I2dcc31ae2b5afbe55561a389296239ba4c566a1c
    Reviewed-on: https://chromium-review.googlesource.com/1244737
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594421}
    Former-commit-id: 89824093b12d691ca52349e906dd9b27567f8e8c

--- chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesCategory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesCategory.java
@@ -4,7 +4,10 @@
 
 package org.chromium.chrome.browser.explore_sites;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.chrome.browser.UrlConstants;
@@ -16,14 +19,23 @@ import java.util.List;
  * An object representing a category in Explore Sites.
  */
 public class ExploreSitesCategory {
-    private int mCategoryId;
+    // The ID to use when creating the More button, that should not scroll the ESP when clicked.
+    public static final int MORE_BUTTON_ID = -1;
 
+    private int mCategoryId;
     private String mCategoryTitle;
+
     // Populated only in NTP.
-    private Bitmap mIcon;
+    private Drawable mDrawable;
     // Populated only for ESP.
     private List<ExploreSitesSite> mSites;
 
+    /**
+     * Creates an explore sites category data structure.
+     * @param categoryId The integer category ID, corresponding to the enum value from the Catalog
+     *   proto, or -1 if this represents the More button.
+     * @param title The string to display as the caption for this tile.
+     */
     public ExploreSitesCategory(int categoryId, String title) {
         mCategoryId = categoryId;
         mCategoryTitle = title;
@@ -38,12 +50,16 @@ public class ExploreSitesCategory {
         return mCategoryTitle;
     }
 
-    public void setIcon(Bitmap icon) {
-        mIcon = icon;
+    public void setIcon(Context context, Bitmap icon) {
+        mDrawable = new BitmapDrawable(context.getResources(), icon);
     }
 
-    public Bitmap getIcon() {
-        return mIcon;
+    public void setDrawable(Drawable drawable) {
+        mDrawable = drawable;
+    }
+
+    public Drawable getDrawable() {
+        return mDrawable;
     }
 
     public void addSite(ExploreSitesSite site) {
@@ -54,7 +70,13 @@ public class ExploreSitesCategory {
         return mSites;
     }
 
+    /**
+     * Returns the URL for the explore sites page, with the correct scrolling ID in the hash value.
+     */
     public String getUrl() {
+        if (mCategoryId < 0) {
+            return UrlConstants.EXPLORE_URL;
+        }
         return UrlConstants.EXPLORE_URL + "#" + getId();
     }
 
--- chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesSection.java
+++ chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesSection.java
@@ -4,6 +4,9 @@
 
 package org.chromium.chrome.browser.explore_sites;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.graphics.drawable.VectorDrawableCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -16,6 +19,7 @@ import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.ui.base.PageTransition;
 import org.chromium.ui.mojom.WindowOpenDisposition;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -23,7 +27,12 @@ import java.util.List;
  * It abstracts general tasks related to initializing and fetching data for the UI.
  */
 public class ExploreSitesSection {
-    private static final int MAX_TILES = 4;
+    private static final int MAX_CATEGORIES = 3;
+
+    /** These should be kept in sync with //chrome/browser/android/explore_sites/catalog.proto */
+    private static final int SPORT = 3;
+    private static final int SHOPPING = 5;
+    private static final int FOOD = 15;
 
     @TileStyle
     private int mStyle;
@@ -37,7 +46,7 @@ public class ExploreSitesSection {
         mStyle = style;
         mExploreSection = (TileGridLayout) view;
         mExploreSection.setMaxRows(1);
-        mExploreSection.setMaxColumns(MAX_TILES);
+        mExploreSection.setMaxColumns(MAX_CATEGORIES + 1);
         mNavigationDelegate = navigationDelegate;
         initialize();
     }
@@ -46,37 +55,77 @@ public class ExploreSitesSection {
         ExploreSitesBridge.getEspCatalog(mProfile, this ::initializeCategoryTiles);
     }
 
+    private Drawable getVectorDrawable(int resource) {
+        return VectorDrawableCompat.create(
+                getContext().getResources(), resource, getContext().getTheme());
+    }
+
+    private Context getContext() {
+        return mExploreSection.getContext();
+    }
+
+    /**
+     * Creates the predetermined categories for when we don't yet have a catalog from the
+     * ExploreSites API server.
+     */
+    private List<ExploreSitesCategory> createDefaultCategoryTiles() {
+        List<ExploreSitesCategory> categoryList = new ArrayList<>();
+
+        // Sport category.
+        ExploreSitesCategory category = new ExploreSitesCategory(
+                SPORT, getContext().getString(R.string.explore_sites_default_category_sports));
+        category.setDrawable(getVectorDrawable(R.drawable.ic_directions_run_blue_24dp));
+        categoryList.add(category);
+
+        // Shopping category.
+        category = new ExploreSitesCategory(
+                SHOPPING, getContext().getString(R.string.explore_sites_default_category_shopping));
+        category.setDrawable(getVectorDrawable(R.drawable.ic_shopping_basket_blue_24dp));
+        categoryList.add(category);
+
+        // Food category.
+        category = new ExploreSitesCategory(
+                FOOD, getContext().getString(R.string.explore_sites_default_category_cooking));
+        category.setDrawable(getVectorDrawable(R.drawable.ic_restaurant_menu_blue_24dp));
+        categoryList.add(category);
+        return categoryList;
+    }
+
+    private ExploreSitesCategory createMoreTileCategory() {
+        ExploreSitesCategory category = new ExploreSitesCategory(
+                ExploreSitesCategory.MORE_BUTTON_ID, getContext().getString(R.string.more));
+        category.setDrawable(getVectorDrawable(R.drawable.ic_arrow_forward_blue_24dp));
+        return category;
+    }
+
+    private void createTileView(ExploreSitesCategory category) {
+        ExploreSitesCategoryTileView tileView;
+        if (mStyle == TileStyle.MODERN_CONDENSED) {
+            tileView = (ExploreSitesCategoryTileView) LayoutInflater.from(getContext())
+                               .inflate(R.layout.explore_sites_category_tile_view_condensed,
+                                       mExploreSection, false);
+        } else {
+            tileView = (ExploreSitesCategoryTileView) LayoutInflater.from(getContext())
+                               .inflate(R.layout.explore_sites_category_tile_view, mExploreSection,
+                                       false);
+        }
+        tileView.initialize(category);
+        mExploreSection.addView(tileView);
+        tileView.setOnClickListener((View v) -> onClicked(category, v));
+    }
+
     private void initializeCategoryTiles(List<ExploreSitesCategory> categoryList) {
-        if (categoryList == null) return;
-        if (categoryList.size() == 0) {
-            // TODO(dewittj): Remove this once the fetcher works.
-            categoryList.add(new ExploreSitesCategory(1, "this"));
-            categoryList.add(new ExploreSitesCategory(1, "this"));
-            categoryList.add(new ExploreSitesCategory(1, "this"));
-            categoryList.add(new ExploreSitesCategory(1, "More"));
+        if (categoryList == null || categoryList.size() == 0) {
+            categoryList = createDefaultCategoryTiles();
         }
 
         int tileCount = 0;
         for (final ExploreSitesCategory category : categoryList) {
             tileCount++;
-            if (tileCount > MAX_TILES) break;
-            ExploreSitesCategoryTileView tileView;
-            if (mStyle == TileStyle.MODERN_CONDENSED) {
-                tileView = (ExploreSitesCategoryTileView) LayoutInflater
-                                   .from(mExploreSection.getContext())
-                                   .inflate(R.layout.explore_sites_category_tile_view_condensed,
-                                           mExploreSection, false);
-            } else {
-                tileView = (ExploreSitesCategoryTileView) LayoutInflater
-                                   .from(mExploreSection.getContext())
-                                   .inflate(R.layout.explore_sites_category_tile_view,
-                                           mExploreSection, false);
-            }
-
-            tileView.initialize(category);
-            mExploreSection.addView(tileView);
-            tileView.setOnClickListener((View v) -> onClicked(category, v));
+            if (tileCount > MAX_CATEGORIES) break;
+            createTileView(category);
         }
+        createTileView(createMoreTileCategory());
     }
 
     private void onClicked(ExploreSitesCategory category, View v) {
