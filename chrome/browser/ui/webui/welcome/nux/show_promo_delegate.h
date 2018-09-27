4065d87cc4e33fbb0964251e5eed62e92cbe2da3
Scott Chen <scottchen@chromium.org>
2018-09-27 05:27:49 +0000

    NUX: Move NUX backend code to chrome/browser/ui/webui/welcome
    Move NUX backend handlers and string resources to a chrome/ directory. There
    will be a follow-up CL to move the frontend resources to the same directory.
    
    This is to make it possible to reuse chrome/ code for NUX handlers.
    
    Bug: 874122
    Change-Id: Id9a5fdd0a1ea88c4027139b66aaade0b120b4904
    Reviewed-on: https://chromium-review.googlesource.com/1232399
    Commit-Queue: Scott Chen <scottchen@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594599}
    Former-commit-id: ea61398177d8de61681325395011610acfd3ad57

--- chrome/browser/ui/webui/welcome/nux/show_promo_delegate.h
+++ chrome/browser/ui/webui/welcome/nux/show_promo_delegate.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_NUX_SHOW_PROMO_DELEGATE_H_
-#define COMPONENTS_NUX_SHOW_PROMO_DELEGATE_H_
+#ifndef CHROME_BROWSER_UI_WEBUI_WELCOME_NUX_SHOW_PROMO_DELEGATE_H_
+#define CHROME_BROWSER_UI_WEBUI_WELCOME_NUX_SHOW_PROMO_DELEGATE_H_
 
 #include <memory>
 
@@ -23,4 +23,4 @@ class ShowPromoDelegate {
       int string_specifier);
 };
 
-#endif  //  COMPONENTS_NUX_SHOW_PROMO_DELEGATE_H_
+#endif  //  CHROME_BROWSER_UI_WEBUI_WELCOME_NUX_SHOW_PROMO_DELEGATE_H_
