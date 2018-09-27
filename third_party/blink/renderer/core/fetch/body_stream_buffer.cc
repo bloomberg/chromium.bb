65ec99a15487c6b50ca9ea3868a90a074d6277e5
Adam Rice <ricea@chromium.org>
2018-09-27 07:11:13 +0000

    Test new hypotheses in CloseAndLockAndDisturb
    Modify BodyStreamBuffer::CloseAndLockAndDisturb to test new hypotheses
    about what is causing the crashes in this function.
    
    1. Test whether IsContextDestroyed() is ever true by adding a CHECK
       if it is.
    2. Test whether stream_ can be empty if stream_broken_ is false. Tests
       with a busy loop have indicated that this function is sometimes
       called with stream_broken_ set to true. This raises the question if
       failures still happen when it is false. This CL will cause it to
       CHECK if stream_broken_ is false and stream_ is empty.
    
    This CL causes intentional crashes and should not be left in the beta
    version as-is.
    
    BUG=882599
    
    Change-Id: Id71b66c51fcab0e821a411a2a97866c992b3f36d
    Reviewed-on: https://chromium-review.googlesource.com/1245040
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594618}
    Former-commit-id: 345bac76820ffa61db322769567bb0a29753f524

--- third_party/blink/renderer/core/fetch/body_stream_buffer.cc
+++ third_party/blink/renderer/core/fetch/body_stream_buffer.cc
@@ -377,19 +377,20 @@ bool BodyStreamBuffer::IsStreamDisturbedForDCheck() {
 }
 
 void BodyStreamBuffer::CloseAndLockAndDisturb(ExceptionState& exception_state) {
+  CHECK(!ExecutionContext::From(script_state_)->IsContextDestroyed());
+  if (stream_broken_) {
+    exception_state.ThrowDOMException(
+        DOMExceptionCode::kInvalidStateError,
+        "Body stream has suffered a fatal error and cannot be disturbed");
+    return;
+  }
   // Speculative fix for https://crbug.com/882599. Stop the stream from being
   // garbage collected while this function is executing.
   // TODO(ricea): Remove this when a better solution is found or if it doesn't
   // work.
   v8::Local<v8::Value> stream_handle =
       stream_.NewLocal(script_state_->GetIsolate());
-  if (stream_handle.IsEmpty()) {
-    stream_broken_ = true;
-    exception_state.ThrowDOMException(
-        DOMExceptionCode::kInvalidStateError,
-        "Body stream has suffered a fatal error and cannot be disturbed");
-    return;
-  }
+  CHECK(!stream_handle.IsEmpty());
 
   base::Optional<bool> is_readable = IsStreamReadable(exception_state);
   if (exception_state.HadException())
