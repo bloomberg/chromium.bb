3b5ba4cfe4b53d65d05629da0c16be91505d54c2
Christopher Cameron <ccameron@chromium.org>
2018-09-28 05:11:32 +0000

    RemoteRWHVMac: Instantiate content::NSViewBridgeFactoryImpl in app shim
    Add to chrome::mojom::AppShim a CreateContentNSViewBridgeFactory
    method. This will instantiate a content::NSViewBridgeFactoryImpl.
    
    The caller of this method (AppShimHost, in the browser), creates
    a content::NSViewBridgeFactoryHost to manage this interface. This
    may be used (e.g, by content::RenderWidgetHostViewMac::
    MigrateNSViewBridge), to create NSViews in an app shim process.
    
    Add content/browser as a dependency of app_shim.
    
    R=dominickn
    TBR=avi
    
    Bug: 821651
    Change-Id: Ice2b46f2d75a9725f5e8e8ad7d2c05e3d70b18eb
    Reviewed-on: https://chromium-review.googlesource.com/1240154
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594987}
    Former-commit-id: f07f828e58ecb6b956266aae541c3c875793a894

--- ui/views/cocoa/bridge_factory_host.cc
+++ ui/views/cocoa/bridge_factory_host.cc
@@ -4,10 +4,18 @@
 
 #include "ui/views/cocoa/bridge_factory_host.h"
 
+#include "mojo/public/cpp/bindings/interface_request.h"
+
 namespace views {
 
+namespace {
+// Start the ids at something far from zero to help in debugging.
+uint64_t g_next_bridge_factory_host_id_ = 0x1000;
+}  // namespace
+
 BridgeFactoryHost::BridgeFactoryHost(
-    views_bridge_mac::mojom::BridgeFactoryRequest* request) {
+    views_bridge_mac::mojom::BridgeFactoryRequest* request)
+    : host_id_(g_next_bridge_factory_host_id_++) {
   *request = mojo::MakeRequest(&bridge_factory_ptr_);
 }
 
--- ui/views/cocoa/bridge_factory_host.h
+++ ui/views/cocoa/bridge_factory_host.h
@@ -24,11 +24,18 @@ class VIEWS_EXPORT BridgeFactoryHost {
 
   BridgeFactoryHost(views_bridge_mac::mojom::BridgeFactoryRequest* request);
   ~BridgeFactoryHost();
+
+  // Return an id for the host process. This can be used to look up other
+  // factories to create NSViews (e.g in content).
+  uint64_t GetHostId() const { return host_id_; }
+
   views_bridge_mac::mojom::BridgeFactory* GetFactory();
+
   void AddObserver(Observer* observer);
   void RemoveObserver(const Observer* observer);
 
  private:
+  const uint64_t host_id_;
   views_bridge_mac::mojom::BridgeFactoryPtr bridge_factory_ptr_;
   base::ObserverList<Observer> observers_;
 };
