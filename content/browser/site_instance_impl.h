6c4899ca1386cb8b7817f7b0d28f576891303059
Lukasz Anforowicz <lukasza@chromium.org>
2018-09-28 23:57:54 +0000

    Allow origin lock for WebUI pages.
    Returning true for WebUI pages in DoesSiteRequireDedicatedProcess helps
    to keep enforcing a SiteInstance swap during chrome://foo ->
    chrome://bar navigation, even after relaxing
    BrowsingInstance::GetSiteInstanceForURL to consider RPH::IsSuitableHost
    (see https://crrev.com/c/783470 for that fixes process sharing in
    isolated(b(c),d(c)) scenario).
    
    I've manually tested this CL by visiting the following URLs:
    - chrome://welcome/
    - chrome://settings
    - chrome://extensions
    - chrome://history
    - chrome://help and chrome://chrome (both redirect to chrome://settings/help)
    
    Bug: 510588, 847127
    Change-Id: I55073bce00f32cb8bc5c1c91034438ff9a3f8971
    Reviewed-on: https://chromium-review.googlesource.com/1237392
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595259}
    Former-commit-id: 1f5ad409dbf5334523931df37598ea49e9849c87

--- content/browser/site_instance_impl.h
+++ content/browser/site_instance_impl.h
@@ -232,12 +232,12 @@ class CONTENT_EXPORT SiteInstanceImpl final : public SiteInstance,
   // true here also implies that |site_url| requires a dedicated process.
   // However, the converse does not hold: this might still return false for
   // certain special cases where an origin lock can't be applied even when
-  // |site_url| requires a dedicated process (e.g., with
-  // --site-per-process).  Examples of those cases include <webview> guests,
-  // WebUI, single-process mode, or extensions where a process is currently
-  // allowed to be reused for different extensions.  Most of these special
-  // cases should eventually be removed, and this function should become
-  // equivalent to DoesSiteRequireDedicatedProcess().
+  // |site_url| requires a dedicated process (e.g., with --site-per-process).
+  // Examples of those cases include <webview> guests, single-process mode, or
+  // extensions where a process is currently allowed to be reused for different
+  // extensions.  Most of these special cases should eventually be removed, and
+  // this function should become equivalent to
+  // DoesSiteRequireDedicatedProcess().
   static bool ShouldLockToOrigin(BrowserContext* browser_context,
                                  GURL site_url);
 
