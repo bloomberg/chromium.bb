eb1e9d53ee391763ee56fade26cfa373027298a6
Rakina Zata Amni <rakina@chromium.org>
2018-09-28 04:11:51 +0000

    Disable flakyEncryptedMediaTest.Playback_Multiple_VideoAudio_WebM
    TBR: hubbe@chromium.org
    Bug: 890124
    Change-Id: Ic8e40e59883f43fcee5ad263ac6f551ecd2e59c0
    Reviewed-on: https://chromium-review.googlesource.com/1250343
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#594978}
    Former-commit-id: 4e3e0c424b96080c8aad5a085f201171fee9093e

--- chrome/browser/media/encrypted_media_browsertest.cc
+++ chrome/browser/media/encrypted_media_browsertest.cc
@@ -556,16 +556,9 @@ IN_PROC_BROWSER_TEST_P(EncryptedMediaTest, Playback_VideoClearAudio_WebM_Opus) {
   TestSimplePlayback("bear-320x240-opus-av_enc-v.webm", kWebMOpusAudioVp9Video);
 }
 
-// TODO(xhwang): Test is flaky on Mac. https://crbug.com/835585
-#if defined(MACOS)
-#define MAYBE_Playback_Multiple_VideoAudio_WebM \
-  DISABLED_Playback_Multiple_VideoAudio_WebM
-#else
-#define MAYBE_Playback_Multiple_VideoAudio_WebM \
-  Playback_Multiple_VideoAudio_WebM
-#endif
+// TODO(xhwang): Test is flaky. https://crbug.com/890124.
 IN_PROC_BROWSER_TEST_P(EncryptedMediaTest,
-                       MAYBE_Playback_Multiple_VideoAudio_WebM) {
+                       DISABLED_Playback_Multiple_VideoAudio_WebM) {
   if (!IsPlayBackPossible(CurrentKeySystem())) {
     DVLOG(0) << "Skipping test - Playback_Multiple test requires playback.";
     return;
