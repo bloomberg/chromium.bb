92e43b4582091d45dfeef5d71e1dfce9a9711267
Wenzhao Zang <wzang@chromium.org>
2018-09-29 00:37:10 +0000

    cros: Support variations of brand code based on market segment
    The brand code map uses similar pattern with the policy map:
    https://cs.chromium.org/chromium/src/chrome/browser/policy/configuration_policy_handler_list_factory.cc?type=cs&q=configuration_policy_handler_list_factory.cc&g=0&l=129
    
    It's trivial work to add more entries in the future.
    
    Bug: 846033
    Change-Id: Ib343476a2338eb17cdcf0fc24bc7c139dec40d02
    Reviewed-on: https://chromium-review.googlesource.com/1237541
    Commit-Queue: Wenzhao (Colin) Zang <wzang@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595272}
    Former-commit-id: 20153791d53f7be390d659763ac794cad405edda

--- chrome/browser/google/google_brand_chromeos.cc
+++ chrome/browser/google/google_brand_chromeos.cc
@@ -9,8 +9,11 @@
 #include "base/strings/string_util.h"
 #include "base/task/post_task.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
+#include "chrome/browser/google/google_brand_code_map_chromeos.h"
 #include "chrome/common/pref_names.h"
 #include "chromeos/system/statistics_provider.h"
+#include "components/policy/core/common/cloud/cloud_policy_constants.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/browser_thread.h"
 
@@ -61,6 +64,18 @@ std::string GetBrand() {
   if (!g_browser_process->local_state())
     return std::string();
   return g_browser_process->local_state()->GetString(prefs::kRLZBrand);
+}
+
+std::string GetRlzBrand() {
+  policy::BrowserPolicyConnectorChromeOS* connector =
+      g_browser_process->platform_part()->browser_policy_connector_chromeos();
+  base::Optional<policy::MarketSegment> market_segment;
+  if (connector->IsEnterpriseManaged())
+    market_segment = connector->GetEnterpriseMarketSegment();
+  // The rlz brand code may change over time (e.g. when device goes from
+  // unenrolled to enrolled status in OOBE). Prefer not to save it in pref to
+  // avoid using outdated value.
+  return GetRlzBrandCode(GetBrand(), market_segment);
 }
 
 void InitBrand(const base::Closure& callback) {
