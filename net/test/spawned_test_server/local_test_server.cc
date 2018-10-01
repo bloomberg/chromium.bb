68690ed0f5b71e76ae54f45c1903374f437e91a2
Francois Doray <fdoray@chromium.org>
2018-10-01 17:43:02 +0000

    Add logging in net test server code.
    This will help investigate issues such as https://crbug.com/890841
    in the future. It is usually helpful to have a lot of logging in
    test-only code.
    
    TBR=rsleevi@chromium.org
    
    Bug: 890841
    Change-Id: I7ed1e06cf578277ffd4f7976cc70b46209998f38
    Reviewed-on: https://chromium-review.googlesource.com/1254742
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595478}
    Former-commit-id: 3c493e34d32255da0fa821149db3b26d5cb7b6d5

--- net/test/spawned_test_server/local_test_server.cc
+++ net/test/spawned_test_server/local_test_server.cc
@@ -95,14 +95,20 @@ bool LocalTestServer::StartInBackground() {
 
   // Get path to Python server script.
   base::FilePath testserver_path;
-  if (!GetTestServerPath(&testserver_path))
+  if (!GetTestServerPath(&testserver_path)) {
+    LOG(ERROR) << "Could not get test server path.";
     return false;
+  }
 
-  if (!SetPythonPath())
+  if (!SetPythonPath()) {
+    LOG(ERROR) << "Could not set Python path.";
     return false;
+  }
 
-  if (!LaunchPython(testserver_path))
+  if (!LaunchPython(testserver_path)) {
+    LOG(ERROR) << "Could not launch Python with path " << testserver_path;
     return false;
+  }
 
   return true;
 }
