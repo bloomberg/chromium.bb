78082f8a922ef4c36140bc112528b4b867353098
Brian Osman <brianosman@google.com>
2018-10-01 21:59:11 +0000

    Add SK_LEGACY_MAKE_COLOR_SPACE_IMPL
    This will guard https://skia-review.googlesource.com/c/skia/+/158344
    It introduces a few pixel_test diffs due to the test framework's
    processing of images after read-back.
    
    Bug: skia:8382
    Change-Id: Id0ca8c4205a5537665610086db348145855dd8c4
    Reviewed-on: https://chromium-review.googlesource.com/1255206
    Commit-Queue: Brian Osman <brianosman@google.com>
    Commit-Queue: Mike Klein <mtklein@chromium.org>
    Reviewed-by: Mike Klein <mtklein@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595547}
    Former-commit-id: a681ac9ad12edb47b3115d84d6eed9975a86cc59

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -173,6 +173,8 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 
 #define SK_LEGACY_COLORSPACE_XFORM_STEPS_IMPL
 
+#define SK_LEGACY_MAKE_COLOR_SPACE_IMPL
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
