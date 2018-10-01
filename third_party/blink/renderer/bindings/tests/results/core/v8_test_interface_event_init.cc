c054ec6dae64732795d0ff38ace3b958aedd4d2e
Adam Langley <agl@chromium.org>
2018-10-01 01:38:23 +0000

    Switch generated dictionary binding code to Chromium style.
    This change alters the code that is generated for dictionaries to use
    Chromium variable names.
    
    Change-Id: I5db361a1ef41dd134b2a2bf2e9af2cae14d17ad0
    Reviewed-on: https://chromium-review.googlesource.com/1247232
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595367}
    Former-commit-id: df1968e70aaf3f468be89ae60ec4720fa7559a8e

--- third_party/blink/renderer/bindings/tests/results/core/v8_test_interface_event_init.cc
+++ third_party/blink/renderer/bindings/tests/results/core/v8_test_interface_event_init.cc
@@ -44,18 +44,18 @@ void V8TestInterfaceEventInit::ToImpl(v8::Isolate* isolate, v8::Local<v8::Value>
   const v8::Eternal<v8::Name>* keys = eternalV8TestInterfaceEventInitKeys(isolate);
   v8::TryCatch block(isolate);
   v8::Local<v8::Context> context = isolate->GetCurrentContext();
-  v8::Local<v8::Value> stringMemberValue;
-  if (!v8Object->Get(context, keys[0].Get(isolate)).ToLocal(&stringMemberValue)) {
+  v8::Local<v8::Value> string_member_value;
+  if (!v8Object->Get(context, keys[0].Get(isolate)).ToLocal(&string_member_value)) {
     exceptionState.RethrowV8Exception(block.Exception());
     return;
   }
-  if (stringMemberValue.IsEmpty() || stringMemberValue->IsUndefined()) {
+  if (string_member_value.IsEmpty() || string_member_value->IsUndefined()) {
     // Do nothing.
   } else {
-    V8StringResource<> stringMemberCppValue = stringMemberValue;
-    if (!stringMemberCppValue.Prepare(exceptionState))
+    V8StringResource<> string_member_cpp_value = string_member_value;
+    if (!string_member_cpp_value.Prepare(exceptionState))
       return;
-    impl.setStringMember(stringMemberCppValue);
+    impl.setStringMember(string_member_cpp_value);
   }
 }
 
@@ -72,14 +72,14 @@ bool toV8TestInterfaceEventInit(const TestInterfaceEventInit& impl, v8::Local<v8
 
   const v8::Eternal<v8::Name>* keys = eternalV8TestInterfaceEventInitKeys(isolate);
   v8::Local<v8::Context> context = isolate->GetCurrentContext();
-  v8::Local<v8::Value> stringMemberValue;
-  bool stringMemberHasValueOrDefault = false;
+  v8::Local<v8::Value> string_member_value;
+  bool string_member_has_value_or_default = false;
   if (impl.hasStringMember()) {
-    stringMemberValue = V8String(isolate, impl.stringMember());
-    stringMemberHasValueOrDefault = true;
+    string_member_value = V8String(isolate, impl.stringMember());
+    string_member_has_value_or_default = true;
   }
-  if (stringMemberHasValueOrDefault &&
-      !V8CallBoolean(dictionary->CreateDataProperty(context, keys[0].Get(isolate), stringMemberValue))) {
+  if (string_member_has_value_or_default &&
+      !V8CallBoolean(dictionary->CreateDataProperty(context, keys[0].Get(isolate), string_member_value))) {
     return false;
   }
 
