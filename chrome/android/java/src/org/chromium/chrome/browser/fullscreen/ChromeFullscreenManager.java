61b0c9a533f4fe5a63770c661f526a458c9db9b0
Aldo Culquicondor <acondor@chromium.org>
2018-10-02 17:10:46 +0000

    VR: Remove bottom margin from NTP
    Bug: 884803
    Change-Id: I20378649418949c2d00097ff8ca204972c54b2ab
    Reviewed-on: https://chromium-review.googlesource.com/1240169
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595884}
    Former-commit-id: 31783d1f676795dd837807d56ce9d38d6fd9d6aa

--- chrome/android/java/src/org/chromium/chrome/browser/fullscreen/ChromeFullscreenManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/fullscreen/ChromeFullscreenManager.java
@@ -357,7 +357,7 @@ public class ChromeFullscreenManager
 
     @Override
     public int getBottomControlsHeight() {
-        return mBottomControlContainerHeight;
+        return VrModuleProvider.getDelegate().isInVr() ? 0 : mBottomControlContainerHeight;
     }
 
     /**
--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
@@ -280,7 +280,7 @@ public class NewTabPageView extends FrameLayout {
 
         if (FeatureUtilities.isBottomToolbarEnabled()) {
             ((MarginLayoutParams) getLayoutParams()).bottomMargin =
-                    getResources().getDimensionPixelSize(R.dimen.bottom_toolbar_height);
+                    mTab.getFullscreenManager().getBottomControlsHeight();
         }
     }
 
