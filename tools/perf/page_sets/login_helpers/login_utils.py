24afd654bc5a8711c6f152e66fe31432796a547d
Camillo Bruni <cbruni@chromium.org>
2018-10-02 10:41:17 +0000

    [perf] Use EnterText for login helpers
    Several login pages do no longer work by simply settings the value of
    the email and password inputs. Using EnterText single KeyboardEvents
    are dispatched to the currently selected input thus simulating more
    closely real input.
    
    Bug: 878390
    Change-Id: I86ba8edbac681d6739acaaebe74ed4e6b1034096
    Reviewed-on: https://chromium-review.googlesource.com/1254962
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595785}
    Former-commit-id: 05d3beb82456be9b199e9df8b3a93895a44201dc

--- tools/perf/page_sets/login_helpers/login_utils.py
+++ tools/perf/page_sets/login_helpers/login_utils.py
@@ -54,9 +54,11 @@ def InputWithSelector(action_runner, input_text, input_selector):
       possible exceptions.
   """
   action_runner.WaitForElement(selector=input_selector)
+  # Focus the requested element first and then enter text using single
+  # Keyboard events to bypass certain restrictions on websites.
   action_runner.ExecuteJavaScript(
-      'document.querySelector({{ selector }}).value = {{ value }};',
-      selector=input_selector, value=input_text)
+      'document.querySelector({{selector}}).focus()', selector=input_selector)
+  action_runner.EnterText(input_text)
 
 def InputForm(action_runner, input_text, input_id, form_id=None):
   """Sets the text value of an input field in a form on the page.
