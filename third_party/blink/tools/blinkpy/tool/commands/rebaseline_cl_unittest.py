1b98037e0ee0e487e23985a319f4e5fcc276f078
Stephen Martinis <martiniss@chromium.org>
2018-10-02 19:42:02 +0000

    rebaseline_cl.py: Set step_name when retrieving results
    Layout test results are now uploaded into a directory indicating which
    step name they were run as. This is to allow for without patch runs to
    be recorded for analytics. This CL changes rebaseline_cl to pass the
    step name when retrieving resuls.
    
    Bug: 882946
    Change-Id: Iad18ca54cc8804fff76f059ceab49d8c69fadd3e
    Reviewed-on: https://chromium-review.googlesource.com/1244598
    Commit-Queue: Stephen Martinis <martiniss@chromium.org>
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595949}
    Former-commit-id: cc86e6e56f06e78bea4f366c3200388bff7e263a

--- third_party/blink/tools/blinkpy/tool/commands/rebaseline_cl_unittest.py
+++ third_party/blink/tools/blinkpy/tool/commands/rebaseline_cl_unittest.py
@@ -119,6 +119,7 @@ class RebaselineCLTest(BaseTestCase, LoggingTestCase):
             'verbose': False,
             'builders': [],
             'patchset': None,
+            'step_name': 'webkit_layout_tests (with patch)',
         }
         options.update(kwargs)
         return optparse.Values(dict(**options))
@@ -411,7 +412,7 @@ class RebaselineCLTest(BaseTestCase, LoggingTestCase):
             'INFO: Finished try jobs found for all try bots.\n',
             'INFO: Failed to fetch results for "MOCK Try Win".\n',
             ('INFO: Results URL: https://test-results.appspot.com/data/layout_results'
-             '/MOCK_Try_Win/5000/layout-test-results/results.html\n'),
+             '/MOCK_Try_Win/5000/webkit_layout_tests (with patch)/layout-test-results/results.html\n'),
             'INFO: There are some builders with no results:\n',
             'INFO:   MOCK Try Win\n',
             'INFO: Would you like to continue?\n',
@@ -428,7 +429,7 @@ class RebaselineCLTest(BaseTestCase, LoggingTestCase):
             'INFO: Finished try jobs found for all try bots.\n',
             'INFO: Failed to fetch results for "MOCK Try Win".\n',
             ('INFO: Results URL: https://test-results.appspot.com/data/layout_results'
-             '/MOCK_Try_Win/5000/layout-test-results/results.html\n'),
+             '/MOCK_Try_Win/5000/webkit_layout_tests (with patch)/layout-test-results/results.html\n'),
             'INFO: There are some builders with no results:\n',
             'INFO:   MOCK Try Win\n',
             'INFO: For one/flaky-fail.html:\n',
