bfec6411758a5b58282cbe1c41fca26ad7e4958b
Anders Carlsson <andersc@chromium.org>
2018-10-02 13:02:35 +0000

    Revert "WebRTC: Migrate completely to new video codec factories API"
    This reverts commit 7a0ae4623bc279814fff01c3fb54eb6d4acaac9d [formerly f990835b73ff945db3500d487a3c799b1d08180c].
    
    Reason for revert: Breaks H264 profile negotiation in some cases.
    
    Original change's description:
    > WebRTC: Migrate completely to new video codec factories API
    > 
    > This CL is a reland of
    > https://chromium-review.googlesource.com/c/chromium/src/+/788410
    > 
    > This CL makes the Chromium HW video factories implement the new
    > interfaces directly. It also adds SW codecs, simulcast, and SW fallback
    > codecs manually, instead of relying on WebRTC to do this. This will
    > give Chromium more flexibility in the future, and will allow us to
    > simplify the WebRTC code, since it's only Chromium that uses some of
    > these features.
    > 
    > Bug: webrtc:7925
    > Change-Id: Ib66ec75827363dc9e0985c35a6de936ca8f3a523
    > Reviewed-on: https://chromium-review.googlesource.com/1100463
    > Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    > Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    > Commit-Queue: Anders Carlsson <andersc@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#580144}
    
    TBR=haraken@chromium.org,emircan@chromium.org,hiroh@chromium.org,andersc@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: webrtc:7925
    Change-Id: I4b9c7ef3c386d310156611898ecbd552df8c1520
    Reviewed-on: https://chromium-review.googlesource.com/1255636
    Commit-Queue: Anders Carlsson <andersc@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#595809}
    Former-commit-id: 6e62467b1e270b6aa1861427e8feca3ed7fbf3c3

--- content/renderer/media/webrtc/rtc_video_decoder.cc
+++ content/renderer/media/webrtc/rtc_video_decoder.cc
@@ -137,6 +137,12 @@ std::unique_ptr<RTCVideoDecoder> RTCVideoDecoder::Create(
   return decoder;
 }
 
+// static
+void RTCVideoDecoder::Destroy(webrtc::VideoDecoder* decoder,
+                              media::GpuVideoAcceleratorFactories* factories) {
+  factories->GetTaskRunner()->DeleteSoon(FROM_HERE, decoder);
+}
+
 int32_t RTCVideoDecoder::InitDecode(const webrtc::VideoCodec* codecSettings,
                                     int32_t /*numberOfCores*/) {
   DVLOG(2) << "InitDecode";
