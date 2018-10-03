6d2e491d9145b2ac5e11156bd5dfd4595388d1c3
Gang Wu <gangwu@chromium.org>
2018-10-03 22:02:00 +0000

    [Feed] implement search history service to check if the url is visited
    Also change the feed_logging_metrics.h into class for future features,
    Like hook up all the UMA with Feed internal page.
    
    Bug: 867767
    
    Change-Id: I5559207df16d9c431208bd65cf20326a22116202
    Reviewed-on: https://chromium-review.googlesource.com/c/1258250
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596389}
    Former-commit-id: 321723b06b8765e55a1fc07d1193cbf4138987a8

--- components/feed/core/feed_logging_metrics.h
+++ components/feed/core/feed_logging_metrics.h
@@ -5,6 +5,11 @@
 #ifndef COMPONENTS_FEED_CORE_FEED_LOGGING_METRICS_H_
 #define COMPONENTS_FEED_CORE_FEED_LOGGING_METRICS_H_
 
+#include "base/callback.h"
+#include "base/macros.h"
+#include "base/memory/weak_ptr.h"
+#include "url/gurl.h"
+
 namespace base {
 class Time;
 class TimeDelta;
@@ -13,35 +18,59 @@ class TimeDelta;
 enum class WindowOpenDisposition;
 
 namespace feed {
-namespace metrics {
 
-// |suggestions_count| contains how many cards show to users. It does not depend
-// on whether the user actually saw the cards.
-void OnPageShown(const int suggestions_count);
+// FeedLoggingMetrics is a central place to report all the UMA metrics for Feed.
+class FeedLoggingMetrics {
+ public:
+  // Return whether the URL is visited when calling checking URL visited.
+  using CheckURLVisitCallback = base::OnceCallback<void(bool)>;
 
-// Should only be called once per NTP for each suggestion.
-void OnSuggestionShown(int position,
-                       base::Time publish_date,
-                       float score,
-                       base::Time fetch_date);
+  // Calling this callback when need to check whether url is visited, and will
+  // tell CheckURLVisitCallback the result.
+  using HistoryURLCheckCallback =
+      base::RepeatingCallback<void(GURL, CheckURLVisitCallback)>;
 
-void OnSuggestionOpened(int position,
-                        base::Time publish_date,
-                        float score,
-                        WindowOpenDisposition disposition);
+  explicit FeedLoggingMetrics(HistoryURLCheckCallback callback);
+  ~FeedLoggingMetrics();
 
-void OnSuggestionMenuOpened(int position, base::Time publish_date, float score);
+  // |suggestions_count| contains how many cards show to users. It does not
+  // depend on whether the user actually saw the cards.
+  void OnPageShown(const int suggestions_count);
 
-void OnSuggestionDismissed(int position, bool visited);
+  // Should only be called once per NTP for each suggestion.
+  void OnSuggestionShown(int position,
+                         base::Time publish_date,
+                         float score,
+                         base::Time fetch_date);
 
-void OnSuggestionArticleVisited(base::TimeDelta visit_time);
+  void OnSuggestionOpened(int position,
+                          base::Time publish_date,
+                          float score,
+                          WindowOpenDisposition disposition);
 
-// Should only be called once per NTP for each "more" button.
-void OnMoreButtonShown(int position);
+  void OnSuggestionMenuOpened(int position,
+                              base::Time publish_date,
+                              float score);
 
-void OnMoreButtonClicked(int position);
+  void OnSuggestionDismissed(int position, const GURL& url);
 
-}  // namespace metrics
+  void OnSuggestionArticleVisited(base::TimeDelta visit_time);
+
+  // Should only be called once per NTP for each "more" button.
+  void OnMoreButtonShown(int position);
+
+  void OnMoreButtonClicked(int position);
+
+ private:
+  void CheckURLVisitedDone(int position, bool visited);
+
+  const HistoryURLCheckCallback history_url_check_callback_;
+
+  base::WeakPtrFactory<FeedLoggingMetrics> weak_ptr_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(FeedLoggingMetrics);
+};
+
 }  // namespace feed
 
 #endif  // COMPONENTS_FEED_CORE_FEED_LOGGING_METRICS_H_
--- components/feed/core/feed_logging_metrics_unittest.cc
+++ components/feed/core/feed_logging_metrics_unittest.cc
@@ -1,4 +1,4 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
+// Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -11,8 +11,9 @@
 #include "ui/base/mojo/window_open_disposition.mojom.h"
 
 namespace feed {
-namespace metrics {
 namespace {
+
+GURL VISITED_URL("http://visited_url.com");
 
 using testing::ElementsAre;
 using testing::IsEmpty;
@@ -28,21 +29,49 @@ enum FeedActionType {
   DOWNLOAD = 5,
 };
 
-TEST(FeedLoggingMetricsTest, ShouldLogOnSuggestionsShown) {
+void CheckURLVisit(GURL url,
+                   FeedLoggingMetrics::CheckURLVisitCallback callback) {
+  if (url == VISITED_URL) {
+    std::move(callback).Run(true);
+  } else {
+    std::move(callback).Run(false);
+  }
+}
+
+}  // namespace
+
+class FeedLoggingMetricsTest : public testing::Test {
+ public:
+  FeedLoggingMetricsTest() {
+    feed_logging_metrics_ = std::make_unique<FeedLoggingMetrics>(
+        base::BindRepeating(&CheckURLVisit));
+  }
+
+  FeedLoggingMetrics* feed_logging_metrics() {
+    return feed_logging_metrics_.get();
+  }
+
+ private:
+  std::unique_ptr<FeedLoggingMetrics> feed_logging_metrics_;
+
+  DISALLOW_COPY_AND_ASSIGN(FeedLoggingMetricsTest);
+};
+
+TEST_F(FeedLoggingMetricsTest, ShouldLogOnSuggestionsShown) {
   base::HistogramTester histogram_tester;
-  OnSuggestionShown(/*position=*/1, base::Time::Now(),
-                    /*score=*/0.01f,
-                    base::Time::Now() - base::TimeDelta::FromHours(2));
+  feed_logging_metrics()->OnSuggestionShown(
+      /*position=*/1, base::Time::Now(),
+      /*score=*/0.01f, base::Time::Now() - base::TimeDelta::FromHours(2));
   // Test corner cases for score.
-  OnSuggestionShown(/*position=*/2, base::Time::Now(),
-                    /*score=*/0.0f,
-                    base::Time::Now() - base::TimeDelta::FromHours(2));
-  OnSuggestionShown(/*position=*/3, base::Time::Now(),
-                    /*score=*/1.0f,
-                    base::Time::Now() - base::TimeDelta::FromHours(2));
-  OnSuggestionShown(/*position=*/4, base::Time::Now(),
-                    /*score=*/8.0f,
-                    base::Time::Now() - base::TimeDelta::FromHours(2));
+  feed_logging_metrics()->OnSuggestionShown(
+      /*position=*/2, base::Time::Now(),
+      /*score=*/0.0f, base::Time::Now() - base::TimeDelta::FromHours(2));
+  feed_logging_metrics()->OnSuggestionShown(
+      /*position=*/3, base::Time::Now(),
+      /*score=*/1.0f, base::Time::Now() - base::TimeDelta::FromHours(2));
+  feed_logging_metrics()->OnSuggestionShown(
+      /*position=*/4, base::Time::Now(),
+      /*score=*/8.0f, base::Time::Now() - base::TimeDelta::FromHours(2));
 
   EXPECT_THAT(
       histogram_tester.GetAllSamples("NewTabPage.ContentSuggestions.Shown"),
@@ -59,24 +88,28 @@ TEST(FeedLoggingMetricsTest, ShouldLogOnSuggestionsShown) {
                   base::Bucket(/*min=*/11, /*count=*/1)));
 }
 
-TEST(FeedLoggingMetricsTest, ShouldLogOnPageShown) {
+TEST_F(FeedLoggingMetricsTest, ShouldLogOnPageShown) {
   base::HistogramTester histogram_tester;
-  OnPageShown(/*content_count=*/10);
+  feed_logging_metrics()->OnPageShown(/*content_count=*/10);
   EXPECT_THAT(histogram_tester.GetAllSamples(
                   "NewTabPage.ContentSuggestions.CountOnNtpOpenedIfVisible"),
               ElementsAre(base::Bucket(/*min=*/10, /*count=*/1)));
 }
 
-TEST(FeedLoggingMetricsTest, ShouldLogPrefetchedSuggestionsWhenOpened) {
+TEST_F(FeedLoggingMetricsTest, ShouldLogPrefetchedSuggestionsWhenOpened) {
   base::HistogramTester histogram_tester;
-  OnSuggestionOpened(/*position=*/11, base::Time::Now(),
-                     /*score=*/1.0f, WindowOpenDisposition::CURRENT_TAB);
-  OnSuggestionOpened(/*position=*/13, base::Time::Now(),
-                     /*score=*/1.0f, WindowOpenDisposition::CURRENT_TAB);
-  OnSuggestionOpened(/*position=*/15, base::Time::Now(),
-                     /*score=*/1.0f, WindowOpenDisposition::CURRENT_TAB);
-  OnSuggestionOpened(/*position=*/23, base::Time::Now(),
-                     /*score=*/1.0f, WindowOpenDisposition::CURRENT_TAB);
+  feed_logging_metrics()->OnSuggestionOpened(
+      /*position=*/11, base::Time::Now(),
+      /*score=*/1.0f, WindowOpenDisposition::CURRENT_TAB);
+  feed_logging_metrics()->OnSuggestionOpened(
+      /*position=*/13, base::Time::Now(),
+      /*score=*/1.0f, WindowOpenDisposition::CURRENT_TAB);
+  feed_logging_metrics()->OnSuggestionOpened(
+      /*position=*/15, base::Time::Now(),
+      /*score=*/1.0f, WindowOpenDisposition::CURRENT_TAB);
+  feed_logging_metrics()->OnSuggestionOpened(
+      /*position=*/23, base::Time::Now(),
+      /*score=*/1.0f, WindowOpenDisposition::CURRENT_TAB);
 
   EXPECT_THAT(
       histogram_tester.GetAllSamples("NewTabPage.ContentSuggestions.Opened"),
@@ -86,6 +119,21 @@ TEST(FeedLoggingMetricsTest, ShouldLogPrefetchedSuggestionsWhenOpened) {
                   base::Bucket(/*min=*/23, /*count=*/1)));
 }
 
-}  // namespace
-}  // namespace metrics
+TEST_F(FeedLoggingMetricsTest, ShouldLogOnSuggestionDismissedIfVisited) {
+  base::HistogramTester histogram_tester;
+  feed_logging_metrics()->OnSuggestionDismissed(/*position=*/10, VISITED_URL);
+  EXPECT_THAT(histogram_tester.GetAllSamples(
+                  "NewTabPage.ContentSuggestions.DismissedVisited"),
+              ElementsAre(base::Bucket(/*min=*/10, /*count=*/1)));
+}
+
+TEST_F(FeedLoggingMetricsTest, ShouldLogOnSuggestionDismissedIfNotVisited) {
+  base::HistogramTester histogram_tester;
+  feed_logging_metrics()->OnSuggestionDismissed(/*position=*/10,
+                                                GURL("http://non_visited.com"));
+  EXPECT_THAT(histogram_tester.GetAllSamples(
+                  "NewTabPage.ContentSuggestions.DismissedVisited"),
+              IsEmpty());
+}
+
 }  // namespace feed
