c1bcf21963313cf5ac8ae028a06b8d5645e98075
Koji Ishii <kojii@chromium.org>
2018-10-03 18:13:54 +0000

    [LayoutNG] Mark LayoutNGMixin functions as final when possible
    Some override functions in LayoutNGMixin are not to be
    overridden further. This patch marks them as 'final' so that
    calling them from inside of LayoutNGMixin or its subclasses
    can make non-virtual calls.
    
    No behavior changes.
    
    Bug: 635619
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_layout_ng
    Change-Id: I97e65d70cbe34b6757ab129c62e37bdaddcda72e
    Reviewed-on: https://chromium-review.googlesource.com/1258664
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596285}
    Former-commit-id: 5a2a07e4a46d9fe8a9c1c14af2bf30d601d99304

--- third_party/blink/renderer/core/layout/ng/layout_ng_mixin.h
+++ third_party/blink/renderer/core/layout/ng/layout_ng_mixin.h
@@ -35,47 +35,42 @@ class LayoutNGMixin : public Base {
   explicit LayoutNGMixin(Element* element);
   ~LayoutNGMixin() override;
 
-  bool IsLayoutNGObject() const override { return true; }
+  bool IsLayoutNGObject() const final { return true; }
 
-  NGInlineNodeData* TakeNGInlineNodeData() override;
-  NGInlineNodeData* GetNGInlineNodeData() const override;
-  void ResetNGInlineNodeData() override;
-  bool HasNGInlineNodeData() const override {
-    return ng_inline_node_data_.get();
-  }
+  NGInlineNodeData* TakeNGInlineNodeData() final;
+  NGInlineNodeData* GetNGInlineNodeData() const final;
+  void ResetNGInlineNodeData() final;
+  bool HasNGInlineNodeData() const final { return ng_inline_node_data_.get(); }
 
-  LayoutUnit FirstLineBoxBaseline() const override;
-  LayoutUnit InlineBlockBaseline(LineDirectionMode) const override;
+  LayoutUnit FirstLineBoxBaseline() const final;
+  LayoutUnit InlineBlockBaseline(LineDirectionMode) const final;
 
-  void InvalidateDisplayItemClients(PaintInvalidationReason) const override;
+  void InvalidateDisplayItemClients(PaintInvalidationReason) const final;
 
-  void Paint(const PaintInfo&) const override;
+  void Paint(const PaintInfo&) const final;
 
   bool NodeAtPoint(HitTestResult&,
                    const HitTestLocation& location_in_container,
                    const LayoutPoint& accumulated_offset,
-                   HitTestAction) override;
+                   HitTestAction) final;
 
-  PositionWithAffinity PositionForPoint(const LayoutPoint&) const override;
+  PositionWithAffinity PositionForPoint(const LayoutPoint&) const final;
 
   // Returns the last layout result for this block flow with the given
   // constraint space and break token, or null if it is not up-to-date or
   // otherwise unavailable.
-  scoped_refptr<NGLayoutResult> CachedLayoutResult(
-      const NGConstraintSpace&,
-      NGBreakToken*) const override;
+  scoped_refptr<NGLayoutResult> CachedLayoutResult(const NGConstraintSpace&,
+                                                   NGBreakToken*) const final;
 
   void SetCachedLayoutResult(const NGConstraintSpace&,
                              const NGBreakToken*,
-                             const NGLayoutResult&) override;
-  void ClearCachedLayoutResult() override;
+                             const NGLayoutResult&) final;
+  void ClearCachedLayoutResult() final;
 
   // For testing only.
-  scoped_refptr<const NGLayoutResult> CachedLayoutResultForTesting() override;
+  scoped_refptr<const NGLayoutResult> CachedLayoutResultForTesting() final;
 
-  NGPaintFragment* PaintFragment() const override {
-    return paint_fragment_.get();
-  }
+  NGPaintFragment* PaintFragment() const final { return paint_fragment_.get(); }
   void SetPaintFragment(const NGBreakToken*,
                         scoped_refptr<const NGPhysicalFragment>,
                         NGPhysicalOffset) final;
@@ -87,7 +82,7 @@ class LayoutNGMixin : public Base {
  protected:
   bool IsOfType(LayoutObject::LayoutObjectType) const override;
 
-  void AddOverflowFromChildren() override;
+  void AddOverflowFromChildren() final;
 
  private:
   void AddScrollingOverflowFromChildren();
@@ -98,14 +93,14 @@ class LayoutNGMixin : public Base {
  protected:
   void AddOutlineRects(Vector<LayoutRect>&,
                        const LayoutPoint& additional_offset,
-                       NGOutlineType) const override;
+                       NGOutlineType) const final;
 
-  const NGPhysicalBoxFragment* CurrentFragment() const override;
+  const NGPhysicalBoxFragment* CurrentFragment() const final;
 
   const NGBaseline* FragmentBaseline(NGBaselineAlgorithmType) const;
 
   void DirtyLinesFromChangedChild(LayoutObject* child,
-                                  MarkingBehavior marking_behavior) override;
+                                  MarkingBehavior marking_behavior) final;
 
   std::unique_ptr<NGInlineNodeData> ng_inline_node_data_;
 
