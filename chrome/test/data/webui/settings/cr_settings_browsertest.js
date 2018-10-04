09c63c37491aaee52341b7ffc0753e646982987c
Alexey Baskakov <loyso@chromium.org>
2018-10-04 04:07:51 +0000

    CrSettings: Disable CrSettingsLanguagesPageTest.AddLanguagesDialog test.
    The test is flaky on multiple platforms.
    
    Example:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Linux%20Tests/72784
    
    TBR=dpapad@chromium.org
    
    Bug: 891955
    Change-Id: I76567527df8d9241754940f6b520d1c52a543931
    Reviewed-on: https://chromium-review.googlesource.com/c/1260703
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596496}
    Former-commit-id: 2bf771c79c890da4fcef6a79a6eecf97b90e173b

--- chrome/test/data/webui/settings/cr_settings_browsertest.js
+++ chrome/test/data/webui/settings/cr_settings_browsertest.js
@@ -1606,9 +1606,12 @@ CrSettingsLanguagesPageTest.prototype = {
   ]),
 };
 
-TEST_F('CrSettingsLanguagesPageTest', 'AddLanguagesDialog', function() {
-  mocha.grep(assert(languages_page_tests.TestNames.AddLanguagesDialog)).run();
-});
+// Failing on multiple builders. crbug.com/891955.
+TEST_F(
+    'CrSettingsLanguagesPageTest', 'DISABLED_AddLanguagesDialog', function() {
+      mocha.grep(assert(languages_page_tests.TestNames.AddLanguagesDialog))
+          .run();
+    });
 
 TEST_F('CrSettingsLanguagesPageTest', 'LanguageMenu', function() {
   mocha.grep(assert(languages_page_tests.TestNames.LanguageMenu)).run();
