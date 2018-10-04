054bed17ef2f52d956914fa111c8cc580f2159e0
Martin Šrámek <msramek@chromium.org>
2018-10-04 15:33:41 +0000

    Disable WebViewTest.Shim_testFindInMultipleWebViews on Windows
    Due to flakiness.
    
    TBR=ekaramad@chromium.org
    
    Bug: 892085
    Change-Id: I96d8a3b14583c340abba18a725941b8ae0c1cf06
    Reviewed-on: https://chromium-review.googlesource.com/c/1261700
    Commit-Queue: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Ehsan Karamad <ekaramad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596682}
    Former-commit-id: 97ba6ddb3e711eff30910c3b5c63c6c07bfc806e

--- chrome/browser/apps/guest_view/web_view_browsertest.cc
+++ chrome/browser/apps/guest_view/web_view_browsertest.cc
@@ -3293,7 +3293,14 @@ IN_PROC_BROWSER_TEST_F(WebViewTest, MAYBE_Shim_TestFindAPI_findupdate) {
   TestHelper("testFindAPI_findupdate", "web_view/shim", NO_TEST_SERVER);
 }
 
-IN_PROC_BROWSER_TEST_F(WebViewTest, Shim_testFindInMultipleWebViews) {
+// TODO(crbug.com/892085): Disabled on Windows due to flakiness. Re-enable.
+#if defined(OS_WIN)
+#define MAYBE_Shim_testFindInMultipleWebViews \
+  DISABLED_Shim_testFindInMultipleWebViews
+#else
+#define MAYBE_Shim_testFindInMultipleWebViews Shim_testFindInMultipleWebViews
+#endif
+IN_PROC_BROWSER_TEST_F(WebViewTest, MAYBE_Shim_testFindInMultipleWebViews) {
   TestHelper("testFindInMultipleWebViews", "web_view/shim", NO_TEST_SERVER);
 }
 
