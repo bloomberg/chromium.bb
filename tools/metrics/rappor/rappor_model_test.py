f5bbe1611c9048b0061fdc6cc3ab68a2d36b9791
Alexei Svitkine <asvitkine@chromium.org>
2018-10-05 00:10:03 +0000

    Reland "Make histograms/pretty_print_test.py run on the bots."
    This reverts commit d48d861c217a42794f4554402e17264db60e526f [formerly 9cf546abc899f13e372202d3e7b97374015ff9cc].
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > Revert "Make histograms/pretty_print_test.py run on the bots."
    > 
    > This reverts commit eb2266388e12e66a806644fe101bc1a60996ab27 [formerly 5c5ce638e36966b872f26e2df86116388e06fd51].
    > 
    > Reason for revert: broke the build
    > 
    > Original change's description:
    > > Make histograms/pretty_print_test.py run on the bots.
    > > 
    > > This test was added via:
    > > https://chromium-review.googlesource.com/1244553
    > > 
    > > This is a follow-up CL to add it to metrics_python_tests so
    > > that it will run on the bots.
    > > 
    > > Renames Python files to avoid name collisions between
    > > different scripts, so that they can run together as part of
    > > metrics_python_tests. Also fixes up the rappor model test
    > > and adds it to the suite.
    > > 
    > > Bug: 714123
    > > Change-Id: I466584493f7090decdb8b21217347a905b3e6391
    > > Reviewed-on: https://chromium-review.googlesource.com/c/1255596
    > > Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    > > Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#596921}
    > 
    > TBR=dpranke@chromium.org,asvitkine@chromium.org,holte@chromium.org
    > 
    > Change-Id: Iebebe68f9c4c4da11e6e79fc14b9853bea737e5c
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 714123
    > Reviewed-on: https://chromium-review.googlesource.com/c/1263434
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#596931}
    
    TBR=dcheng@chromium.org,dpranke@chromium.org,asvitkine@chromium.org,holte@chromium.org
    
    Change-Id: Ie026170296321cd0cbd4a6fd57506f88388ba295
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 714123
    Reviewed-on: https://chromium-review.googlesource.com/c/1263238
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#596936}
    Former-commit-id: 980759fbb321d960dc4a52b6fb3ffb9f8228a3d3

--- tools/metrics/rappor/rappor_model_test.py
+++ tools/metrics/rappor/rappor_model_test.py
@@ -6,10 +6,9 @@
 from __future__ import unicode_literals
 
 import copy
-import json
 import unittest
 
-import model
+import rappor_model
 
 PUBLIC_XML_SNIPPET = """
 <rappor-configuration>
@@ -90,16 +89,16 @@ PUBLIC_XML_SNIPPET = """
 """
 
 PARSED_XML = {
-    model.models.COMMENT_KEY: [],
+    rappor_model.models.COMMENT_KEY: [],
     'noiseLevels': {
-        model.models.COMMENT_KEY: [],
+        rappor_model.models.COMMENT_KEY: [],
         'levels': [
             {
-                model.models.COMMENT_KEY: [],
+                rappor_model.models.COMMENT_KEY: [],
                 'name': 'NO_NOISE',
                 'summary': 'NO_NOISE description.',
                 'values': {
-                    model.models.COMMENT_KEY: [],
+                    rappor_model.models.COMMENT_KEY: [],
                     'fake-prob': 0.0,
                     'fake-one-prob': 0.0,
                     'one-coin-prob': 1.0,
@@ -107,11 +106,11 @@ PARSED_XML = {
                 },
             },
             {
-                model.models.COMMENT_KEY: [],
+                rappor_model.models.COMMENT_KEY: [],
                 'name': 'NORMAL_NOISE',
                 'summary': 'NORMAL_NOISE description.',
                 'values': {
-                    model.models.COMMENT_KEY: [],
+                    rappor_model.models.COMMENT_KEY: [],
                     'fake-prob': 0.5,
                     'fake-one-prob': 0.5,
                     'one-coin-prob': 0.75,
@@ -121,14 +120,14 @@ PARSED_XML = {
         ]
     },
     'parameterTypes': {
-        model.models.COMMENT_KEY: [],
+        rappor_model.models.COMMENT_KEY: [],
         'types': [
             {
-                model.models.COMMENT_KEY: [],
+                rappor_model.models.COMMENT_KEY: [],
                 'name': 'ETLD_PLUS_ONE',
                 'summary': 'ETLD+1 parameters.',
                 'parameters': {
-                    model.models.COMMENT_KEY: [],
+                    rappor_model.models.COMMENT_KEY: [],
                     'num-cohorts': 128,
                     'bytes': 16,
                     'hash-functions': 2,
@@ -137,11 +136,11 @@ PARSED_XML = {
                 },
             },
             {
-                model.models.COMMENT_KEY: [],
+                rappor_model.models.COMMENT_KEY: [],
                 'name': 'SAFEBROWSING_RAPPOR_TYPE',
                 'summary': 'SAFEBROWSING parameters.',
                 'parameters': {
-                    model.models.COMMENT_KEY: [],
+                    rappor_model.models.COMMENT_KEY: [],
                     'num-cohorts': 128,
                     'bytes': 1,
                     'hash-functions': 2,
@@ -152,24 +151,24 @@ PARSED_XML = {
         ]
     },
     'metrics': {
-        model.models.COMMENT_KEY: [],
+        rappor_model.models.COMMENT_KEY: [],
         'metrics': [{
-            model.models.COMMENT_KEY: [],
+            rappor_model.models.COMMENT_KEY: [],
             'name':
                 'MultiD.TestMetric',
             'flags': [{
-                model.models.COMMENT_KEY: [],
+                rappor_model.models.COMMENT_KEY: [],
                 'name':
                     'flags',
                 'summary':
                     'Flags Summary.',
                 'flags': [
                     {
-                        model.models.COMMENT_KEY: [],
+                        rappor_model.models.COMMENT_KEY: [],
                         'summary': 'Bit 0'
                     },
                     {
-                        model.models.COMMENT_KEY: [],
+                        rappor_model.models.COMMENT_KEY: [],
                         'summary': 'What it means',
                         'label': 'MyBit',
                         'bit': 1,
@@ -178,7 +177,7 @@ PARSED_XML = {
             }],
             'owners': ['holte@chromium.org'],
             'strings': [{
-                model.models.COMMENT_KEY: [],
+                rappor_model.models.COMMENT_KEY: [],
                 'name': 'domain',
                 'summary': 'Domain Summary.',
             }],
@@ -188,12 +187,12 @@ PARSED_XML = {
                 'SAFEBROWSING_RAPPOR_TYPE',
             'uint64': [],
         }, {
-            model.models.COMMENT_KEY: [],
+            rappor_model.models.COMMENT_KEY: [],
             'name': 'Search.DefaultSearchProvider',
             'flags': [],
             'owners': ['holte@chromium.org'],
             'strings': [],
-            'summary': 'BLAH BLAH  BLAH',
+            'summary': 'BLAH BLAH BLAH',
             'type': 'ETLD_PLUS_ONE',
             'uint64': [],
         }]
@@ -262,7 +261,7 @@ PRETTY_PRINTED_XML = """<rappor-configuration>
 <rappor-metric name="Search.DefaultSearchProvider" type="ETLD_PLUS_ONE">
   <owner>holte@chromium.org</owner>
   <summary>
-    BLAH BLAH  BLAH
+    BLAH BLAH BLAH
   </summary>
 </rappor-metric>
 
@@ -275,14 +274,14 @@ PRETTY_PRINTED_XML = """<rappor-configuration>
 class RapporModelTest(unittest.TestCase):
 
   def testParse(self):
-    parsed = model.RAPPOR_XML_TYPE.Parse(PUBLIC_XML_SNIPPET)
+    parsed = rappor_model.RAPPOR_XML_TYPE.Parse(PUBLIC_XML_SNIPPET)
     self.assertEqual(PARSED_XML, parsed)
 
   def testUpdate(self):
-    updated = model.UpdateXML(PUBLIC_XML_SNIPPET)
+    updated = rappor_model.UpdateXML(PUBLIC_XML_SNIPPET)
     # Compare list of lines for nicer diff on errors.
     self.assertEqual(PRETTY_PRINTED_XML.split('\n'), updated.split('\n'))
-    reprinted = model.UpdateXML(updated)
+    reprinted = rappor_model.UpdateXML(updated)
     self.assertEqual(PRETTY_PRINTED_XML.split('\n'), reprinted.split('\n'))
 
   def testIsValidNoise(self):
@@ -296,19 +295,19 @@ class RapporModelTest(unittest.TestCase):
             'zero-coin-prob': 0.25,
         },
     }
-    self.assertTrue(model._IsValidNoiseLevel(valid_noise_level))
+    self.assertTrue(rappor_model._IsValidNoiseLevel(valid_noise_level))
     invalid_noise_level = copy.copy(valid_noise_level)
     del invalid_noise_level['name']
-    self.assertFalse(model._IsValidNoiseLevel(invalid_noise_level))
+    self.assertFalse(rappor_model._IsValidNoiseLevel(invalid_noise_level))
     invalid_noise_level = copy.copy(valid_noise_level)
     del invalid_noise_level['summary']
-    self.assertFalse(model._IsValidNoiseLevel(invalid_noise_level))
+    self.assertFalse(rappor_model._IsValidNoiseLevel(invalid_noise_level))
     invalid_noise_level = copy.copy(valid_noise_level)
     del invalid_noise_level['values']
-    self.assertFalse(model._IsValidNoiseLevel(invalid_noise_level))
+    self.assertFalse(rappor_model._IsValidNoiseLevel(invalid_noise_level))
     invalid_noise_level = copy.deepcopy(valid_noise_level)
     del invalid_noise_level['values']['fake-prob']
-    self.assertFalse(model._IsValidNoiseLevel(invalid_noise_level))
+    self.assertFalse(rappor_model._IsValidNoiseLevel(invalid_noise_level))
 
   def testIsValidRapporType(self):
     noise_level_names = {'NORMAL_NOISE'}
@@ -323,7 +322,8 @@ class RapporModelTest(unittest.TestCase):
             'noise-level': 'NORMAL_NOISE',
         },
     }
-    self.assertTrue(model._IsValidRapporType(valid_type, noise_level_names))
+    self.assertTrue(rappor_model._IsValidRapporType(valid_type,
+                                                    noise_level_names))
 
   def testIsValidRapporMetric(self):
     type_names = {'ETLD_PLUS_ONE'}
@@ -332,23 +332,23 @@ class RapporModelTest(unittest.TestCase):
         'flags': [],
         'owners': ['holte@chromium.org'],
         'strings': [],
-        'summary': 'BLAH BLAH  BLAH',
+        'summary': 'BLAH BLAH BLAH',
         'type': 'ETLD_PLUS_ONE',
         'uint64': [],
     }
-    self.assertTrue(model._IsValidMetric(valid_metric, type_names))
+    self.assertTrue(rappor_model._IsValidMetric(valid_metric, type_names))
     invalid_metric = copy.copy(valid_metric)
     del invalid_metric['name']
-    self.assertFalse(model._IsValidMetric(invalid_metric, type_names))
+    self.assertFalse(rappor_model._IsValidMetric(invalid_metric, type_names))
     invalid_metric = copy.copy(valid_metric)
     del invalid_metric['summary']
-    self.assertFalse(model._IsValidMetric(invalid_metric, type_names))
+    self.assertFalse(rappor_model._IsValidMetric(invalid_metric, type_names))
     invalid_metric = copy.copy(valid_metric)
     invalid_metric['type'] = 'FOO'
-    self.assertFalse(model._IsValidMetric(invalid_metric, type_names))
+    self.assertFalse(rappor_model._IsValidMetric(invalid_metric, type_names))
     invalid_metric = copy.copy(valid_metric)
     invalid_metric['owners'] = []
-    self.assertFalse(model._IsValidMetric(invalid_metric, type_names))
+    self.assertFalse(rappor_model._IsValidMetric(invalid_metric, type_names))
 
 
 if __name__ == '__main__':
