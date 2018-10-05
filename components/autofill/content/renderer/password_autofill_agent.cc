e66b56454711f2443330c43e8006ce133ca45c6a
Vasilii Sukhanov <vasilii@chromium.org>
2018-10-05 13:50:35 +0000

    Reland "Don't process password forms for filesystem: and blob: URLs."
    This is a reland of 6fa148c0caed7caf6d7ae9c919f5b1fff4ca5922 [formerly 5df861f9f24af97309cefeacebc3a203448c7790]
    
    Original change's description:
    > Don't process password forms for filesystem: and blob: URLs.
    >
    > (cherry picked from commit a402a4709eaf76515c400fe37cce3108984fed75 [formerly db03c6620f08150d556ac37b16f37a0cc4e2dd65])
    >
    > Bug: 887914
    > Change-Id: I76658cd1d001653da0f32f2d955646f8cd365d64
    > Reviewed-on: https://chromium-review.googlesource.com/1256777
    > Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    > Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    > Cr-Original-Commit-Position: refs/heads/master@{#595876}
    > Reviewed-on: https://chromium-review.googlesource.com/c/1261522
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Cr-Commit-Position: refs/branch-heads/3538@{#851}
    > Cr-Branched-From: 57781ff9a8651bc653184f680b0a4d8b92b6971f [formerly 79f7c91a2b2a2932cd447fa6f865cb6662fa8fa6]-refs/heads/master@{#587811}
    
    TBR=dvadym@chromium.org
    
    Bug: 887914
    Change-Id: I6972416320eea3d642a65bbdb8a8a685060d3873
    Reviewed-on: https://chromium-review.googlesource.com/c/1264775
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3538@{#874}
    Cr-Branched-From: 57781ff9a8651bc653184f680b0a4d8b92b6971f [formerly 79f7c91a2b2a2932cd447fa6f865cb6662fa8fa6]-refs/heads/master@{#587811}
    Former-commit-id: d3aa79cc94324a1c6d5f5fb070668782232ec1f7

--- components/autofill/content/renderer/password_autofill_agent.cc
+++ components/autofill/content/renderer/password_autofill_agent.cc
@@ -1091,7 +1091,9 @@ bool PasswordAutofillAgent::FrameCanAccessPasswordManager() {
   // about:blank or about:srcdoc frames should not be allowed to use password
   // manager.  See https://crbug.com/756587.
   WebLocalFrame* frame = render_frame()->GetWebFrame();
-  if (frame->GetDocument().Url().ProtocolIs(url::kAboutScheme))
+  blink::WebURL url = frame->GetDocument().Url();
+  if (url.ProtocolIs(url::kAboutScheme) || url.ProtocolIs(url::kBlobScheme) ||
+      url.ProtocolIs(url::kFileSystemScheme))
     return false;
   return frame->GetSecurityOrigin().CanAccessPasswordManager();
 }
