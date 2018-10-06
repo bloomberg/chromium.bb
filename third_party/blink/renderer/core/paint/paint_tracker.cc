d821d577f0aa95ff0d4f72f544b004ad9e2b2692
Liquan(Max) Gu <maxlg@chromium.org>
2018-10-06 01:51:18 +0000

    [FCP++] Implement Largest Image Paint and Last Image Paint
    Largest Image Paint and Last Image Paint are implemented as an effort
    to finda better alternative to First Contentful Paint.
    
    Largest Image Paint measures the time when the largest image is first
    painted after it's fully loaded.
    
    Last Image Paint measures the time when the last image is first painted
    after it's fully loaded.
    
    luci.chromium.try:linux_layout_tests_slimming_paint_v2;
    master.tryserver.blink:linux_trusty_blink_rel
    
    Bug: 869924
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Id35b4c257c434a457cf0f9c7447e2f4326f68a68
    Reviewed-on: https://chromium-review.googlesource.com/c/1191818
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Liquan (Max) Gǔ <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597394}
    Former-commit-id: b8d194606ea18af8a3d3c3a9dec528e29a109997

--- third_party/blink/renderer/core/paint/paint_tracker.cc
+++ third_party/blink/renderer/core/paint/paint_tracker.cc
@@ -5,18 +5,20 @@
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
+#include "third_party/blink/renderer/core/paint/image_paint_timing_detector.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
 #include "third_party/blink/renderer/core/paint/text_paint_timing_detector.h"
 
 namespace blink {
 
 PaintTracker::PaintTracker(LocalFrameView* frame_view)
-    : frame_view_(frame_view) {
-  text_paint_timing_detector_ = new TextPaintTimingDetector(frame_view);
-}
+    : frame_view_(frame_view),
+      text_paint_timing_detector_(new TextPaintTimingDetector(frame_view)),
+      image_paint_timing_detector_(new ImagePaintTimingDetector(frame_view)){};
 
 void PaintTracker::NotifyPrePaintFinished() {
   text_paint_timing_detector_->OnPrePaintFinished();
+  image_paint_timing_detector_->OnPrePaintFinished();
 }
 
 void PaintTracker::NotifyObjectPrePaint(const LayoutObject& object,
@@ -28,6 +30,9 @@ void PaintTracker::NotifyObjectPrePaint(const LayoutObject& object,
   if (object.IsText()) {
     text_paint_timing_detector_->RecordText(object, painting_layer);
   }
+  if (object.IsImage()) {
+    image_paint_timing_detector_->RecordImage(object, painting_layer);
+  }
   // Todo(maxlg): add other detectors here.
 }
 
@@ -35,6 +40,8 @@ void PaintTracker::NotifyNodeRemoved(const LayoutObject& object) {
   if (!object.GetNode())
     return;
   text_paint_timing_detector_->NotifyNodeRemoved(
+      DOMNodeIds::IdForNode(object.GetNode()));
+  image_paint_timing_detector_->NotifyNodeRemoved(
       DOMNodeIds::IdForNode(object.GetNode()));
 }
 
@@ -44,6 +51,7 @@ void PaintTracker::Dispose() {
 
 void PaintTracker::Trace(Visitor* visitor) {
   visitor->Trace(text_paint_timing_detector_);
+  visitor->Trace(image_paint_timing_detector_);
   visitor->Trace(frame_view_);
 }
 }  // namespace blink
--- third_party/blink/renderer/core/paint/text_paint_timing_detector_test.cc
+++ third_party/blink/renderer/core/paint/text_paint_timing_detector_test.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "third_party/blink/renderer/core/paint/text_paint_timing_detector.h"
 #include "third_party/blink/renderer/core/paint/paint_tracker.h"
 #include "third_party/blink/renderer/core/testing/core_unit_test_helper.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
