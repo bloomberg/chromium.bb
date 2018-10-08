e693251e34535e6b55e08c946ae3ef4af9d98978
Mustafa Emre Acer <meacer@chromium.org>
2018-10-08 18:15:14 +0000

    Validate external protocols before launching on Windows
    Bug: 889459
    Change-Id: Id33ca6444bff1e6dd71b6000823cf6fec09746ef
    Reviewed-on: https://chromium-review.googlesource.com/c/1256208
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597611}
    Former-commit-id: d19a75fc26fd0ab1ce79ef3d1c1c9b3cc1fbd098

--- chrome/browser/shell_integration_win.cc
+++ chrome/browser/shell_integration_win.cc
@@ -159,13 +159,22 @@ void MigrateTaskbarPinsCallback() {
   win::MigrateShortcutsInPathInternal(chrome_exe, pins_path);
 }
 
+// Windows treats a given scheme as an Internet scheme only if its registry
+// entry has a "URL Protocol" key. Check this, otherwise we allow ProgIDs to be
+// used as custom protocols which leads to security bugs.
+bool IsValidCustomProtocol(const base::string16& scheme) {
+  if (scheme.empty())
+    return false;
+  base::win::RegKey cmd_key(HKEY_CLASSES_ROOT, scheme.c_str(), KEY_QUERY_VALUE);
+  return cmd_key.Valid() && cmd_key.HasValue(L"URL Protocol");
+}
+
 // Windows 8 introduced a new protocol->executable binding system which cannot
 // be retrieved in the HKCR registry subkey method implemented below. We call
 // AssocQueryString with the new Win8-only flag ASSOCF_IS_PROTOCOL instead.
 base::string16 GetAppForProtocolUsingAssocQuery(const GURL& url) {
-  base::string16 url_scheme = base::ASCIIToUTF16(url.scheme());
-  // Don't attempt to query protocol association on an empty string.
-  if (url_scheme.empty())
+  const base::string16 url_scheme = base::ASCIIToUTF16(url.scheme());
+  if (!IsValidCustomProtocol(url_scheme))
     return base::string16();
 
   // Query AssocQueryString for a human-readable description of the program
@@ -174,12 +183,9 @@ base::string16 GetAppForProtocolUsingAssocQuery(const GURL& url) {
   // an unknown external protocol.
   wchar_t out_buffer[1024];
   DWORD buffer_size = arraysize(out_buffer);
-  HRESULT hr = AssocQueryString(ASSOCF_IS_PROTOCOL,
-                                ASSOCSTR_FRIENDLYAPPNAME,
-                                url_scheme.c_str(),
-                                NULL,
-                                out_buffer,
-                                &buffer_size);
+  HRESULT hr =
+      AssocQueryString(ASSOCF_IS_PROTOCOL, ASSOCSTR_FRIENDLYAPPNAME,
+                       url_scheme.c_str(), NULL, out_buffer, &buffer_size);
   if (FAILED(hr)) {
     DLOG(WARNING) << "AssocQueryString failed!";
     return base::string16();
@@ -188,11 +194,13 @@ base::string16 GetAppForProtocolUsingAssocQuery(const GURL& url) {
 }
 
 base::string16 GetAppForProtocolUsingRegistry(const GURL& url) {
-  base::string16 command_to_launch;
+  const base::string16 url_scheme = base::ASCIIToUTF16(url.scheme());
+  if (!IsValidCustomProtocol(url_scheme))
+    return base::string16();
 
   // First, try and extract the application's display name.
-  base::string16 cmd_key_path = base::ASCIIToUTF16(url.scheme());
-  base::win::RegKey cmd_key_name(HKEY_CLASSES_ROOT, cmd_key_path.c_str(),
+  base::string16 command_to_launch;
+  base::win::RegKey cmd_key_name(HKEY_CLASSES_ROOT, url_scheme.c_str(),
                                  KEY_READ);
   if (cmd_key_name.ReadValue(NULL, &command_to_launch) == ERROR_SUCCESS &&
       !command_to_launch.empty()) {
@@ -201,7 +209,7 @@ base::string16 GetAppForProtocolUsingRegistry(const GURL& url) {
 
   // Otherwise, parse the command line in the registry, and return the basename
   // of the program path if it exists.
-  cmd_key_path = base::ASCIIToUTF16(url.scheme() + "\\shell\\open\\command");
+  const base::string16 cmd_key_path = url_scheme + L"\\shell\\open\\command";
   base::win::RegKey cmd_key_exe(HKEY_CLASSES_ROOT, cmd_key_path.c_str(),
                                 KEY_READ);
   if (cmd_key_exe.ReadValue(NULL, &command_to_launch) == ERROR_SUCCESS) {
@@ -581,10 +589,9 @@ bool IsElevationNeededForSettingDefaultProtocolClient() {
 }
 
 base::string16 GetApplicationNameForProtocol(const GURL& url) {
-  base::string16 application_name;
   // Windows 8 or above has a new protocol association query.
   if (base::win::GetVersion() >= base::win::VERSION_WIN8) {
-    application_name = GetAppForProtocolUsingAssocQuery(url);
+    base::string16 application_name = GetAppForProtocolUsingAssocQuery(url);
     if (!application_name.empty())
       return application_name;
   }
