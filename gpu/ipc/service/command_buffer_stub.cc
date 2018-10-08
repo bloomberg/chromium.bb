46a11be4d2358f2c94c924c11be163372782b3f3
Bo Liu <boliu@chromium.org>
2018-10-08 22:23:25 +0000

    Fix WaitSyncToken trace event
    The event name for BEGIN and END has to match. The viewer uses the name
    and id to match up the BEGIN and END events. Right now from a trace,
    WaitSyncToken looks like it never ends.
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I3d217e2919fc29396fb8af57c13bfe0e1696f858
    Reviewed-on: https://chromium-review.googlesource.com/c/1266375
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#597704}
    Former-commit-id: f52d6249596258ba819c6f6a3f081594711ff61f

--- gpu/ipc/service/command_buffer_stub.cc
+++ gpu/ipc/service/command_buffer_stub.cc
@@ -707,8 +707,8 @@ bool CommandBufferStub::OnWaitSyncToken(const SyncToken& sync_token) {
 
 void CommandBufferStub::OnWaitSyncTokenCompleted(const SyncToken& sync_token) {
   DCHECK(waiting_for_sync_point_);
-  TRACE_EVENT_ASYNC_END1("gpu", "WaitSyncTokenCompleted", this,
-                         "CommandBufferStub", this);
+  TRACE_EVENT_ASYNC_END1("gpu", "WaitSyncToken", this, "CommandBufferStub",
+                         this);
   // Don't call PullTextureUpdates here because we can't MakeCurrent if we're
   // executing commands on another context. The WaitSyncToken command will run
   // again and call PullTextureUpdates once this command buffer gets scheduled.
