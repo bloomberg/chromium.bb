68b694f9f146b846f871cf4895d90f8298a9671f
Orin Jaworski <orinj@chromium.org>
2018-10-09 22:59:43 +0000

    [omnibox] Encapsulate Pedal implementations with registration function
    This is a refactoring CL that moves Pedal implementations into their
    own omnibox_pedal_implementations.cc file.
    
    Bug: 893183
    Change-Id: I6b3802cbeb53919b8b2d421146cb5a33371f58ab
    Reviewed-on: https://chromium-review.googlesource.com/c/1271581
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598119}
    Former-commit-id: caedc10e2263a0f337bc163ee821efbb32f381d4

--- components/omnibox/browser/omnibox_pedal_unittest.cc
+++ components/omnibox/browser/omnibox_pedal_unittest.cc
@@ -7,6 +7,7 @@
 #include "base/message_loop/message_loop.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
+#include "components/omnibox/browser/omnibox_pedal_provider.h"
 #include "components/omnibox/browser/test_omnibox_client.h"
 #include "components/omnibox/browser/test_omnibox_edit_controller.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -28,13 +29,16 @@ class OmniboxPedalTest : public testing::Test {
 };
 
 TEST_F(OmniboxPedalTest, PedalExecutes) {
+  OmniboxPedalProvider provider;
   base::TimeTicks match_selection_timestamp;
   OmniboxPedal::ExecutionContext context(
       *omnibox_client_, *omnibox_edit_controller_, match_selection_timestamp);
   {
-    OmniboxPedalClearBrowsingData pedal;
-    EXPECT_TRUE(pedal.IsTriggerMatch(base::ASCIIToUTF16("clear history")));
-    pedal.Execute(context);
+    const base::string16 trigger = base::ASCIIToUTF16("clear history");
+    const OmniboxPedal* pedal = provider.FindPedalMatch(trigger);
+    EXPECT_NE(pedal, nullptr) << "Pedal not registered or not triggered.";
+    EXPECT_TRUE(pedal->IsTriggerMatch(trigger));
+    pedal->Execute(context);
     const GURL& url = omnibox_edit_controller_->destination_url();
     EXPECT_EQ(url, GURL("chrome://settings/clearBrowserData"));
   }
