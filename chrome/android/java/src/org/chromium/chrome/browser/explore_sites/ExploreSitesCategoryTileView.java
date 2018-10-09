d32f6ff397a78c5c0a45dba793efeec6c2d27211
Justin DeWitt <dewittj@chromium.org>
2018-10-09 23:06:01 +0000

    [EoS] Add fade animations to the NTP
    There is a nontrivial delay before icons pop into being, so fade them in.
    The animations cover 3 cases:
    * No catalog exists, just immediately render the vector default assets
    * Catalog exists locally, fade in the category tiles
    * Catalog exists remotely, render vector assets then replace with
      category tiles using a fade-out fade-in animation.
    
    Bug: 867488
    Change-Id: I958e1e8e3ef88c1dad86869a8e1812e432952989
    Reviewed-on: https://chromium-review.googlesource.com/c/1272087
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598122}
    Former-commit-id: f82582e03eca63741c66de3aa13aab940bf3f9aa

--- chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesCategoryTileView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesCategoryTileView.java
@@ -4,12 +4,16 @@
 
 package org.chromium.chrome.browser.explore_sites;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.ViewGroup.MarginLayoutParams;
 import android.widget.ImageView;
 
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.compositor.layouts.ChromeAnimation;
 import org.chromium.chrome.browser.ntp.TitleUtil;
 import org.chromium.chrome.browser.widget.tile.TileWithTextView;
 
@@ -18,6 +22,7 @@ import org.chromium.chrome.browser.widget.tile.TileWithTextView;
  * within the category.  Alternatively, a MORE button.
  */
 public class ExploreSitesCategoryTileView extends TileWithTextView {
+    private static final int FADE_ANIMATION_TIME_MS = 300;
     private static final int TITLE_LINES = 1;
     private static final boolean SUPPORTED_OFFLINE = false;
 
@@ -43,16 +48,15 @@ public class ExploreSitesCategoryTileView extends TileWithTextView {
 
         // Correct the properties of the icon for categories, it should be the entire size of the
         // icon background now.
-        ImageView tileViewIcon = (ImageView) findViewById(R.id.tile_view_icon);
-        tileViewIcon.setScaleType(ImageView.ScaleType.CENTER);
-        MarginLayoutParams layoutParams = (MarginLayoutParams) tileViewIcon.getLayoutParams();
+        mIconView.setScaleType(ImageView.ScaleType.CENTER);
+        MarginLayoutParams layoutParams = (MarginLayoutParams) mIconView.getLayoutParams();
         int tileViewIconSize =
                 getContext().getResources().getDimensionPixelSize(R.dimen.tile_view_icon_size);
         layoutParams.width = tileViewIconSize;
         layoutParams.height = tileViewIconSize;
         layoutParams.topMargin = getContext().getResources().getDimensionPixelSize(
                 R.dimen.tile_view_icon_background_margin_top_modern);
-        tileViewIcon.setLayoutParams(layoutParams);
+        mIconView.setLayoutParams(layoutParams);
     }
 
     /** Retrieves url associated with this view. */
@@ -67,6 +71,44 @@ public class ExploreSitesCategoryTileView extends TileWithTextView {
     /** Renders icon based on tile data.  */
     public void renderIcon(ExploreSitesCategory category) {
         mCategory = category;
-        setIconDrawable(category.getDrawable());
+        // If the category is a placeholder, just instantly render it, as we can assume there has
+        // been no appreciable delay since the NTP was initialized.
+        if (mCategory.isPlaceholder()) {
+            setIconDrawable(category.getDrawable());
+            return;
+        }
+        fadeThumbnailIn(category.getDrawable());
+    }
+
+    private void fadeThumbnailIn(Drawable thumbnail) {
+        int duration =
+                (int) (FADE_ANIMATION_TIME_MS * ChromeAnimation.Animation.getAnimationMultiplier());
+
+        // If animations are disabled, just show the thumbnail.
+        if (duration == 0) {
+            setIconDrawable(thumbnail);
+            return;
+        }
+
+        // We have some transition time, but no existing icon.  TransitionDrawable requires two or
+        // more drawables to crossfade, so manually fade in here.
+        if (mIconView.getDrawable() == null) {
+            mIconView.setImageDrawable(thumbnail);
+            mIconView.setAlpha(0.0f);
+            mIconView.animate().alpha(1.0f).setDuration(duration).start();
+            return;
+        }
+
+        mIconView.animate()
+                .alpha(0.0f)
+                .setDuration(duration / 2)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        mIconView.setImageDrawable(thumbnail);
+                        mIconView.animate().alpha(1.0f).setDuration(duration / 2).start();
+                    }
+                })
+                .start();
     }
 }
