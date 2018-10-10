22679214adf954a9c743bd4d3b29bebd1269d3c1
Nico Weber <thakis@chromium.org>
2018-10-10 21:50:13 +0000

    Build non-test code under services/ with -Wexit-time-destructors.
    No intended behavior change.
    
    Bug: 101600
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_mojo;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win10_chromium_x64_rel_ng
    Change-Id: I209423e9bf5b16cb2ddf20c5074652a27cf700ae
    Reviewed-on: https://chromium-review.googlesource.com/c/1273812
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598525}
    Former-commit-id: 5fd1898e10f8626fb5a1f3ae161b8f2c115bf39d

--- services/ws/public/cpp/gpu/context_provider_command_buffer.cc
+++ services/ws/public/cpp/gpu/context_provider_command_buffer.cc
@@ -14,6 +14,7 @@
 
 #include "base/callback_helpers.h"
 #include "base/command_line.h"
+#include "base/no_destructor.h"
 #include "base/optional.h"
 #include "base/strings/stringprintf.h"
 #include "base/threading/thread_task_runner_handle.h"
@@ -433,8 +434,9 @@ const gpu::GpuFeatureInfo& ContextProviderCommandBuffer::GetGpuFeatureInfo()
   DCHECK_EQ(bind_result_, gpu::ContextResult::kSuccess);
   CheckValidThreadOrLockAcquired();
   if (!command_buffer_ || !command_buffer_->channel()) {
-    static const gpu::GpuFeatureInfo default_gpu_feature_info;
-    return default_gpu_feature_info;
+    static const base::NoDestructor<gpu::GpuFeatureInfo>
+        default_gpu_feature_info;
+    return *default_gpu_feature_info;
   }
   return command_buffer_->channel()->gpu_feature_info();
 }
