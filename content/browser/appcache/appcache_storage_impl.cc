ce16df0aeb72342ac049814983ad1478f5bab329
Victor Costan <pwnall@chromium.org>
2018-10-10 02:08:53 +0000

    Revert "Set the maximum cache size per an origin in the command line for application cache"
    This reverts commit c0bcc03d19bdc382c3625818289b4cd05aa7b10d [formerly d143b1cc5e86eef3b2d7e8dacd629d24f58a5b6b].
    
    Reason for revert: Broke AppCache integration with Quota Manager. https://crbug.com/892547
    
    Original change's description:
    > Set the maximum cache size per an origin in the command line for application cache
    > 
    > Embedded devices have limited storage capacity generally. But current default
    > hard coded value is 5MB per an origin in application cache storage. But embedders
    > often want to reduce the default hard coded value per an origin as well. So it would
    > be good if we give the power them to set the maximum cache size per an origin for
    > application cache through the command line.
    > 
    > Bug: 824619
    > Change-Id: I078365ec412834c024a92ff5ec4ad5d00452e2a7
    > Reviewed-on: https://chromium-review.googlesource.com/979877
    > Commit-Queue: Gyuyoung Kim <gyuyoung.kim@lge.com>
    > Reviewed-by: Antoine Labour <piman@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#546105}
    
    TBR=piman@chromium.org,gyuyoung.kim@lge.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 824619
    Change-Id: I66fb8f36f7d08409af95a288512e2d751c59a88f
    Reviewed-on: https://chromium-review.googlesource.com/c/1272444
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598178}
    Former-commit-id: 1f05f839927e4ef9792d923d8ed0979785dbc137

--- content/browser/appcache/appcache_storage_impl.cc
+++ content/browser/appcache/appcache_storage_impl.cc
@@ -620,22 +620,14 @@ class AppCacheStorageImpl::StoreGroupAndCacheTask : public StoreOrLoadTask {
   bool would_exceed_quota_;
   int64_t space_available_;
   int64_t new_origin_usage_;
-  int64_t max_appcache_origin_cache_size_;
   std::vector<int64_t> newly_deletable_response_ids_;
 };
 
 AppCacheStorageImpl::StoreGroupAndCacheTask::StoreGroupAndCacheTask(
-    AppCacheStorageImpl* storage,
-    AppCacheGroup* group,
-    AppCache* newest_cache)
-    : StoreOrLoadTask(storage),
-      group_(group),
-      cache_(newest_cache),
-      success_(false),
-      would_exceed_quota_(false),
-      space_available_(-1),
-      new_origin_usage_(-1),
-      max_appcache_origin_cache_size_(kDefaultQuota) {
+    AppCacheStorageImpl* storage, AppCacheGroup* group, AppCache* newest_cache)
+    : StoreOrLoadTask(storage), group_(group), cache_(newest_cache),
+      success_(false), would_exceed_quota_(false),
+      space_available_(-1), new_origin_usage_(-1) {
   group_record_.group_id = group->group_id();
   group_record_.manifest_url = group->manifest_url();
   group_record_.origin = url::Origin::Create(group_record_.manifest_url);
@@ -649,15 +641,6 @@ AppCacheStorageImpl::StoreGroupAndCacheTask::StoreGroupAndCacheTask(
       &intercept_namespace_records_,
       &fallback_namespace_records_,
       &online_whitelist_records_);
-
-  base::CommandLine& command_line = *base::CommandLine::ForCurrentProcess();
-  if (command_line.HasSwitch(switches::kMaxAppCacheOriginCacheSizeMb)) {
-    if (base::StringToInt64(command_line.GetSwitchValueASCII(
-                                switches::kMaxAppCacheOriginCacheSizeMb),
-                            &max_appcache_origin_cache_size_)) {
-      max_appcache_origin_cache_size_ *= kMB;
-    }
-  }
 }
 
 void AppCacheStorageImpl::StoreGroupAndCacheTask::GetQuotaThenSchedule() {
@@ -779,10 +762,9 @@ void AppCacheStorageImpl::StoreGroupAndCacheTask::Run() {
     return;
   }
 
-  // Use a user defined value or a simple hard-coded value when not using quota
-  // management.
+  // Use a simple hard-coded value when not using quota management.
   if (space_available_ == -1) {
-    if (new_origin_usage_ > max_appcache_origin_cache_size_) {
+    if (new_origin_usage_ > kDefaultQuota) {
       would_exceed_quota_ = true;
       success_ = false;
       return;
