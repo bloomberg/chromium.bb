bloomberg/chromium.bb

commit 1d9525fc3fc5e2bef72cd82533a3ea94f7421b6a

[size trybot]: Add extra size regression checks and restructure file.
To avoid running Supersize diff twice, trybot_commit_size_checker.py
now performs all size analysis and outputs results to separate files
that can be read by bot steps.

This CL adds checks for uncompressed pak files and dex method counts,
and also adds support for generating Supersize HTML Reports.

Bug: 702625
Change-Id: I791fe749123c7dbe338ea6dc957a3a387be0d5bf
Reviewed-on: https://chromium-review.googlesource.com/1232018
Commit-Queue: Eric Stevenson <estevenson@chromium.org>
Reviewed-by: agrieve <agrieve@chromium.org>
Cr-Commit-Position: refs/heads/master@{#593928}
Former-commit-id: 6c1587f4b1b45fdb672867ec063b19b33edbc027

tools/binary_size/trybot_commit_size_checker.py
@@ -3,46 +3,192 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Fails if a try job increases binary size unexpectedly."""
+"""Creates several files used by the size trybot to monitor size regressions."""
 
 import argparse
+import collections
+import json
+import logging
+import os
 import sys
 
+sys.path.append(os.path.join(os.path.dirname(__file__), 'libsupersize'))
+import archive
+import diagnose_bloat
+import diff
+import describe
+import html_report
+import models
 
-_MAX_UNNOTICED_INCREASE = 16 * 1024
+_MAX_DEX_METHOD_COUNT_INCREASE = 50
+_MAX_NORMALIZED_INCREASE = 16 * 1024
+_MAX_PAK_INCREASE = 1024
+
+_NORMALIZED_APK_SIZE_DETAILS = (
+    'See https://chromium.googlesource.com/chromium/src/+/master/docs/speed/'
+    'binary_size/metrics.md#Normalized-APK-Size '
+    'for an explanation of Normalized APK Size')
+
+
+class _SizeDelta(collections.namedtuple(
+    'SizeDelta', ['name', 'units', 'expected', 'actual', 'details'])):
+
+  @property
+  def explanation(self):
+    return '{}: expected max: {} {}, got {} {}\n{}'.format(
+        self.name, self.expected, self.units, self.actual, self.units,
+        self.details)
+
+  def _IsAllowable(self):
+    return self.actual <= self.expected
+
+  def __cmp__(self, other):
+    return cmp(self.name, other.name)
+
+
+def _CreateAndWriteMethodCountDelta(symbols, output_path):
+  dex_symbols = symbols.WhereInSection(models.SECTION_DEX_METHOD)
+  dex_added = dex_symbols.WhereDiffStatusIs(models.DIFF_STATUS_ADDED)
+  dex_removed = dex_symbols.WhereDiffStatusIs(models.DIFF_STATUS_REMOVED)
+  dex_added_count, dex_removed_count = len(dex_added), len(dex_removed)
+  dex_net_added = dex_added_count - dex_removed_count
+
+  dex_expl = 'Added: {}\n{}\n\nRemoved: {}\n{}'.format(
+      dex_added_count,
+      '\n'.join(s.name for s in dex_added.SortedByName()),
+      dex_removed_count,
+      '\n'.join(s.name for s in dex_removed.SortedByName()))
+
+  with open(output_path, 'w') as f:
+    f.writelines(dex_expl)
+
+  return _SizeDelta('Dex Methods', 'methods', _MAX_DEX_METHOD_COUNT_INCREASE,
+                    dex_net_added, dex_expl)
+
+
+def _CreateAndWriteResourceSizesDelta(apk_name, before_dir, after_dir,
+                                     output_path):
+  sizes_diff = diagnose_bloat.ResourceSizesDiff(apk_name)
+  sizes_diff.ProduceDiff(before_dir, after_dir)
+
+  with open(output_path, 'w') as f:
+    f.writelines(l + '\n' for l in sizes_diff.Summary())
+
+  return _SizeDelta('Normalized APK Size', 'bytes', _MAX_NORMALIZED_INCREASE,
+                    sizes_diff.summary_stat.value, _NORMALIZED_APK_SIZE_DETAILS)
+
+
+def _CreateAndWriteSupersizeDiff(apk_name, before_dir, after_dir, output_path):
+  before_size_path = os.path.join(before_dir, apk_name + '.size')
+  after_size_path = os.path.join(after_dir, apk_name + '.size')
+  before = archive.LoadAndPostProcessSizeInfo(before_size_path)
+  after = archive.LoadAndPostProcessSizeInfo(after_size_path)
+  size_info_delta = diff.Diff(before, after, sort=True)
+
+  with open(output_path, 'w') as f:
+    f.writelines(l + '\n' for l in describe.GenerateLines(size_info_delta))
+
+  return size_info_delta
+
+
+def _CreateUncompressedPakSizeDeltas(symbols):
+  pak_symbols = symbols.Filter(lambda s:
+      s.size > 0 and
+      bool(s.flags & models.FLAG_UNCOMPRESSED) and
+      s.section_name == models.SECTION_PAK_NONTRANSLATED)
+  return [_SizeDelta('Uncompressed Pak Entry', 'bytes', _MAX_PAK_INCREASE,
+                     pak.after_symbol.size, pak.full_name)
+          for pak in pak_symbols]
 
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--author', help='CL author')
-  parser.add_argument('--resource-sizes-diff',
-                      help='Path to resource sizes diff produced by '
-                           '"diagnose_bloat.py diff sizes".')
+  parser.add_argument('--author', required=True, help='CL author')
+  parser.add_argument('--apk-name', required=True,
+                      help='Name of the apk (ex. Name.apk)')
+  parser.add_argument('--before-dir', required=True,
+                      help='Directory containing the APK from reference build.')
+  parser.add_argument('--after-dir', required=True,
+                      help='Directory containing APK for the new build.')
+  parser.add_argument('--resource-sizes-diff-path', required=True,
+                      help='Output path for the resource_sizes.py diff.')
+  parser.add_argument('--supersize-diff-path', required=True,
+                      help='Output path for the Supersize diff.')
+  parser.add_argument('--dex-method-count-diff-path', required=True,
+                      help='Output path for the dex method count diff.')
+  parser.add_argument('--ndjson-path', required=True,
+                      help='Output path for the Supersize HTML report.')
+  parser.add_argument('--results-path', required=True,
+                      help='Output path for the trybot result .json file.')
+  parser.add_argument('-v', '--verbose', action='store_true')
   args = parser.parse_args()
 
-  # Last line looks like:
-  # MonochromePublic.apk_Specifics normalized apk size=1234
-  with open(args.resource_sizes_diff) as f:
-    last_line = f.readlines()[-1]
-    size_delta = int(last_line.partition('=')[2])
+  if args.verbose:
+    logging.basicConfig(level=logging.INFO)
+
+  logging.info('Creating Supersize diff')
+  delta_size_info = _CreateAndWriteSupersizeDiff(
+      args.apk_name, args.before_dir, args.after_dir, args.supersize_diff_path)
+  changed_symbols = delta_size_info.raw_symbols.WhereDiffStatusIs(
+      models.DIFF_STATUS_UNCHANGED).Inverted()
+
+  # Monitor dex method growth since this correlates closely with APK size and
+  # may affect our dex file structure.
+  logging.info('Checking dex symbols')
+  size_deltas = set()
+  size_deltas.add(
+      _CreateAndWriteMethodCountDelta(
+          changed_symbols, args.dex_method_count_diff_path))
+
+  # Check for uncompressed .pak file entries being added to avoid unnecessary
+  # bloat.
+  logging.info('Checking pak symbols')
+  size_deltas.update(_CreateUncompressedPakSizeDeltas(changed_symbols))
+
+  # Normalized APK Size is the main metric we use to monitor binary size.
+  logging.info('Creating sizes diff')
+  size_deltas.add(
+      _CreateAndWriteResourceSizesDelta(
+          args.apk_name, args.before_dir, args.after_dir,
+          args.resource_sizes_diff_path))
+
+  # .ndjson can be consumed by the html viewer.
+  logging.info('Creating HTML Report')
+  html_report.BuildReportFromSizeInfo(
+      args.ndjson_path, delta_size_info, all_symbols=True)
 
   is_roller = '-autoroll' in args.author
 
   # Useful for bot debugging to have these printed out:
-  print 'Is Roller:', is_roller
-  print 'Increase:', size_delta
+  for delta in sorted(size_deltas):
+    print '{}\n\n'.format(delta.explanation)
 
-  if size_delta > _MAX_UNNOTICED_INCREASE and not is_roller:
-    # Failure message printed to stderr, so flush first.
-    sys.stdout.flush()
-    failure_message = """
+  passing_deltas = set(m for m in size_deltas if m._IsAllowable())
+  failing_deltas = size_deltas - passing_deltas
 
-Binary size increase is non-trivial (where "non-trivial" means the normalized \
-size increased by more than {} bytes).
+  result = 'passed' if is_roller or len(failing_deltas) == 0 else 'failed'
+  message = """
 
-Please look at the symbol diffs from the "Show Resource Sizes Diff" and the \
-"Show Supersize Diff" bot steps. Try and understand the growth and see if it \
-can be mitigated. There is guidance at:
+Binary size checks {}.
+
+*******************************************************************************
+FAILING:
+
+{}
+
+*******************************************************************************
+
+PASSING:
+
+{}
+
+*******************************************************************************
+
+Please look at the symbol diffs from the "Show Resource Sizes Diff",
+"Show Supersize Diff", and "Dex Method Count", and "Supersize HTML Report" bot
+steps. Try and understand the growth and see if it can be mitigated.
+
+There is guidance at:
 
 https://chromium.googlesource.com/chromium/src/+/master/docs/speed/apk_size_regressions.md#Debugging-Apk-Size-Increase
 
@@ -59,11 +205,14 @@ Binary-Size: Increase needed to reduce RAM of a common user flow.
 Binary-Size: Increase needed to reduce runtime of a common user flow.
 Binary-Size: Increase needed to implement a feature, and I've already spent a
     non-trivial amount of time trying to reduce its size.
-""".format(_MAX_UNNOTICED_INCREASE)
-    # Make blank lines not blank prevent them from being stripped.
-    # https://crbug.com/855671
-    failure_message.replace('\n\n', '\n.\n')
-    sys.exit(failure_message)
+""".format(result,
+           '\n\n'.join(d.explanation for d in sorted(failing_deltas)),
+           '\n\n'.join(d.explanation for d in sorted(passing_deltas)))
+  # Make blank lines not blank prevent them from being stripped.
+  # https://crbug.com/855671
+  message.replace('\n\n', '\n.\n')
+  with open(args.results_path, 'w') as f:
+    json.dump({'status_code': len(failing_deltas), 'details': message}, f)
 
 
 if __name__ == '__main__':

tools/binary_size/diagnose_bloat.py
@@ -847,32 +847,6 @@ def _SetRestoreFunc(subrepo):
   atexit.register(_GenRestoreFunc(subrepo))
 
 
-# Used by binary size trybot.
-def _DiffMain(args):
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--before-dir', required=True)
-  parser.add_argument('--after-dir', required=True)
-  parser.add_argument('--apk-name', required=True)
-  parser.add_argument('--diff-type', required=True, choices=['native', 'sizes'])
-  parser.add_argument('--diff-output', required=True)
-  args = parser.parse_args(args)
-
-  is_native_diff = args.diff_type == 'native'
-  if is_native_diff:
-    supersize_path = os.path.join(_BINARY_SIZE_DIR, 'supersize')
-    diff = NativeDiff(args.apk_name + '.size', supersize_path)
-  else:
-    diff = ResourceSizesDiff(args.apk_name)
-
-  diff.ProduceDiff(args.before_dir, args.after_dir)
-  lines = diff.DetailedResults() if is_native_diff else diff.Summary()
-
-  with open(args.diff_output, 'w') as f:
-    f.writelines(l + '\n' for l in lines)
-    stat = diff.summary_stat
-    f.write('\n{}={}\n'.format(*stat[:2]))
-
-
 def main():
   parser = argparse.ArgumentParser(
       description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
@@ -965,8 +939,6 @@ def main():
   if len(sys.argv) == 1:
     parser.print_help()
     return 1
-  if sys.argv[1] == 'diff':
-    return _DiffMain(sys.argv[2:])
 
   args = parser.parse_args()
   log_level = logging.DEBUG if args.verbose else logging.INFO

tools/binary_size/libsupersize/canned_queries.py
@@ -5,6 +5,7 @@
 """Contains a set of Chrome-specific size queries."""
 
 import logging
+
 import models
 
 

tools/binary_size/libsupersize/console.py
@@ -153,17 +153,7 @@ class _Session(object):
     """
     before = before if before is not None else self._size_infos[0]
     after = after if after is not None else self._size_infos[1]
-    ret = diff.Diff(before, after)
-    if sort:
-      syms = ret.symbols  # Triggers clustering.
-      logging.debug('Grouping')
-      # Group path aliases so that functions defined in headers will be sorted
-      # by their actual size rather than shown as many small symbols.
-      syms = syms.GroupedByAliases(same_name_only=True)
-      logging.debug('Sorting')
-      ret.symbols = syms.Sorted()
-    logging.debug('Diff complete')
-    return ret
+    return diff.Diff(before, after, sort=sort)
 
   def _PrintFunc(self, obj=None, verbose=False, summarize=True, recursive=False,
                  use_pager=None, to_file=None):

tools/binary_size/libsupersize/diff.py
@@ -121,7 +121,7 @@ def _DiffSymbolGroups(before, after):
   return models.DeltaSymbolGroup(all_deltas)
 
 
-def Diff(before, after):
+def Diff(before, after, sort=False):
   """Diffs two SizeInfo objects. Returns a DeltaSizeInfo."""
   assert isinstance(before, models.SizeInfo)
   assert isinstance(after, models.SizeInfo)
@@ -132,4 +132,15 @@ def Diff(before, after):
       section_sizes[k] = v
 
   symbol_diff = _DiffSymbolGroups(before.raw_symbols, after.raw_symbols)
-  return models.DeltaSizeInfo(before, after, section_sizes, symbol_diff)
+  ret = models.DeltaSizeInfo(before, after, section_sizes, symbol_diff)
+
+  if sort:
+    syms = ret.symbols  # Triggers clustering.
+    logging.debug('Grouping')
+    # Group path aliases so that functions defined in headers will be sorted
+    # by their actual size rather than shown as many small symbols.
+    syms = syms.GroupedByAliases(same_name_only=True)
+    logging.debug('Sorting')
+    ret.symbols = syms.Sorted()
+  logging.debug('Diff complete')
+  return ret

tools/binary_size/libsupersize/html_report.py
@@ -191,38 +191,28 @@ def _MakeTreeViewList(symbols, include_all_symbols):
   return meta, file_nodes.values()
 
 
-def BuildReport(out_file, size_file, before_size_file=(None, None),
-                all_symbols=False):
+def BuildReportFromSizeInfo(out_path, size_info, all_symbols=False):
   """Builds a .ndjson report for a .size file.
 
   Args:
-    out_file: File object to save JSON report to.
-    size_file: Size file to use as input. Tuple of path and file object.
-    before_size_file: If used, creates a diff report where |size_file| is the
-      newer .size file. Tuple of path and file object.
+    out_path: Path to save JSON report to.
+    size_info: A SizeInfo or DeltaSizeInfo to use for the report.
     all_symbols: If true, all symbols will be included in the report rather
       than truncated.
   """
   logging.info('Reading .size file')
-  diff_mode = any(before_size_file)
-
-  size_info = archive.LoadAndPostProcessSizeInfo(*size_file)
-  if diff_mode:
-    before_size_info = archive.LoadAndPostProcessSizeInfo(*before_size_file)
-    after_size_info = size_info
-
-    size_info = diff.Diff(before_size_info, after_size_info)
-    symbols = size_info.raw_symbols
+  symbols = size_info.raw_symbols
+  is_diff = symbols.IsDelta()
+  if is_diff:
     symbols = symbols.WhereDiffStatusIs(models.DIFF_STATUS_UNCHANGED).Inverted()
-  else:
-    symbols = size_info.raw_symbols
 
   meta, tree_nodes = _MakeTreeViewList(symbols, all_symbols)
+  logging.info('Created %d tree nodes', len(tree_nodes))
   meta.update({
-    'diff_mode': diff_mode,
+    'diff_mode': is_diff,
     'section_sizes': size_info.section_sizes,
   })
-  if diff_mode:
+  if is_diff:
     meta.update({
       'before_metadata': size_info.before.metadata,
       'after_metadata': size_info.after.metadata,
@@ -239,12 +229,13 @@ def BuildReport(out_file, size_file, before_size_file=(None, None),
     'check_circular': False,
   }
 
-  json.dump(meta, out_file, **json_dump_args)
-  out_file.write('\n')
-
-  for tree_node in tree_nodes:
-    json.dump(tree_node, out_file, **json_dump_args)
+  with codecs.open(out_path, 'w', encoding='ascii') as out_file:
+    json.dump(meta, out_file, **json_dump_args)
     out_file.write('\n')
+
+    for tree_node in tree_nodes:
+      json.dump(tree_node, out_file, **json_dump_args)
+      out_file.write('\n')
 
 
 def _MakeDirIfDoesNotExist(rel_path):
@@ -278,13 +269,13 @@ def Run(args, parser):
   if not args.output_report_file.endswith('.ndjson'):
     parser.error('Output must end with ".ndjson"')
 
-  with codecs.open(args.output_report_file, 'w', encoding='ascii') as out_file:
-    BuildReport(
-      out_file,
-      size_file=(args.input_size_file, None),
-      before_size_file=(args.diff_with, None),
-      all_symbols=args.all_symbols
-    )
+  size_info = archive.LoadAndPostProcessSizeInfo(args.input_size_file)
+  if args.diff_with:
+    before_size_info = archive.LoadAndPostProcessSizeInfo(args.diff_with)
+    size_info = diff.Diff(before_size_info, size_info)
+
+  BuildReportFromSizeInfo(
+      args.output_report_file, size_info, all_symbols=args.all_symbols)
 
   msg = [
       'Done!',

commit 05a9e57705382be36a375c229b89f6291cebfeee

[size trybot] Fix check for autoroller CLs.
Broken by 1d9525fc3fc5e2bef72cd82533a3ea94f7421b6a [formerly 6c1587f4b1b45fdb672867ec063b19b33edbc027].

Bug: 893991
Change-Id: I76d02190a3d0085cff019a706497e0e85d7ed473
Reviewed-on: https://chromium-review.googlesource.com/c/1273264
Commit-Queue: Eric Stevenson <estevenson@chromium.org>
Reviewed-by: agrieve <agrieve@chromium.org>
Cr-Commit-Position: refs/heads/master@{#598403}
Former-commit-id: bab0fd82fb41367081cd9e0f591f9baf4672650d

tools/binary_size/trybot_commit_size_checker.py
@@ -182,16 +182,16 @@ def main():
       args.ndjson_path, delta_size_info, all_symbols=True)
 
   is_roller = '-autoroll' in args.author
-
   # Useful for bot debugging to have these printed out:
+  print 'Is Roller:', is_roller
   for delta in sorted(size_deltas):
     print '{}\n\n'.format(delta.explanation)
 
   passing_deltas = set(m for m in size_deltas if m._IsAllowable())
   failing_deltas = size_deltas - passing_deltas
-  status_code = len(failing_deltas)
 
-  result = 'passed' if is_roller or status_code == 0 else 'failed'
+  status_code = 0 if is_roller else len(failing_deltas)
+  result = 'passed' if status_code == 0 else 'failed'
   message = """
 
 Binary size checks {}.