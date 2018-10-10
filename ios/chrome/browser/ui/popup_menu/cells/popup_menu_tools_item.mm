4aafd0fa1b54b99213137cd5e45c95ba5fa78a8e
Gauthier Ambard <gambard@chromium.org>
2018-10-10 15:57:36 +0000

    Reland "[iOS] Adjust icon y-position in the popup menus"
    This is a reland of 5f1657148d1b370a37666720591ada6cb6a6f5a3 [formerly b181c1c7dbae73e90ea8677cbb5ac95bdac8f7a2]
    which got reverted because of a wrong rebase.
    crrev.com/c/1270956 has now landed, so it is safe to reland this.
    
    Original change's description:
    > [iOS] Adjust icon y-position in the popup menus
    >
    > This CL adjust the position of the icons and the badges in the popup
    > menus so they are aligned with the first line of the action item when
    > the font size increases.
    >
    > Bug: 893529
    > Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    > Change-Id: Ief6b938fb1a27e3b71471484b632d83e870a70c7
    > Reviewed-on: https://chromium-review.googlesource.com/c/1270999
    > Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    > Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#598304}
    
    Bug: 893529
    Change-Id: Iaa7a96ea7f678be790adc46fb2382fb9be86f97f
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Reviewed-on: https://chromium-review.googlesource.com/c/1273149
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598345}
    Former-commit-id: dd63f41b2606031e35bb6fb239731f662c562171

--- ios/chrome/browser/ui/popup_menu/cells/popup_menu_tools_item.mm
+++ ios/chrome/browser/ui/popup_menu/cells/popup_menu_tools_item.mm
@@ -27,7 +27,6 @@ const CGFloat kCellHeight = 44;
 const CGFloat kInnerMargin = 11;
 const CGFloat kMargin = 15;
 const CGFloat kTopMargin = 8;
-const CGFloat kTopMarginBadge = 14;
 const CGFloat kMaxHeight = 100;
 NSString* const kToolsMenuTextBadgeAccessibilityIdentifier =
     @"kToolsMenuTextBadgeAccessibilityIdentifier";
@@ -130,6 +129,12 @@ NSString* const kToolsMenuTextBadgeAccessibilityIdentifier =
                                             UILayoutConstraintAxisHorizontal];
     [_titleLabel setContentHuggingPriority:UILayoutPriorityDefaultLow - 1
                                    forAxis:UILayoutConstraintAxisHorizontal];
+    // The compression resistance has to be higher priority than the minimal
+    // height constraint so it can increase the height of the cell to be
+    // displayed on multiple lines.
+    [_titleLabel
+        setContentCompressionResistancePriority:UILayoutPriorityDefaultHigh
+                                        forAxis:UILayoutConstraintAxisVertical];
     _titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
     _titleLabel.adjustsFontForContentSizeCategory = YES;
 
@@ -150,14 +155,26 @@ NSString* const kToolsMenuTextBadgeAccessibilityIdentifier =
     [self.contentView addSubview:_numberBadgeView];
     [self.contentView addSubview:_textBadgeView];
 
+    [NSLayoutConstraint activateConstraints:@[
+      [_titleLabel.centerYAnchor
+          constraintEqualToAnchor:self.contentView.centerYAnchor],
+      // Align the center of image with the center of the capital letter of the
+      // first line of the title.
+      [_imageView.centerYAnchor
+          constraintEqualToAnchor:_titleLabel.firstBaselineAnchor
+                         constant:-[self titleFont].capHeight / 2.0],
+      [_numberBadgeView.centerYAnchor
+          constraintEqualToAnchor:_imageView.centerYAnchor],
+      [_textBadgeView.centerYAnchor
+          constraintEqualToAnchor:_imageView.centerYAnchor],
+      [self.contentView.heightAnchor
+          constraintGreaterThanOrEqualToConstant:kCellHeight],
+    ]];
     ApplyVisualConstraintsWithMetrics(
         @[
           @"H:|-(margin)-[image(imageLength)]-(innerMargin)-[label]",
           @"H:[numberBadge]-(margin)-|", @"H:[textBadge]-(margin)-|",
-          @"V:|-(topMargin)-[image(imageLength)]",
-          @"V:|-(topMarginBadge)-[numberBadge]",
-          @"V:|-(topMarginBadge)-[textBadge]",
-          @"V:|-(topMargin)-[label]-(topMargin)-|"
+          @"V:|-(>=topMargin)-[label]-(>=topMargin)-|"
         ],
         @{
           @"image" : _imageView,
@@ -169,18 +186,21 @@ NSString* const kToolsMenuTextBadgeAccessibilityIdentifier =
           @"margin" : @(kMargin),
           @"innerMargin" : @(kInnerMargin),
           @"topMargin" : @(kTopMargin),
-          @"topMarginBadge" : @(kTopMarginBadge),
           @"imageLength" : @(kImageLength),
         });
 
-    [self.contentView.heightAnchor
-        constraintGreaterThanOrEqualToConstant:kCellHeight]
-        .active = YES;
+    // The height constraint is used to have something as small as possible when
+    // calculating the size of the prototype cell.
+    NSLayoutConstraint* heightConstraint =
+        [self.contentView.heightAnchor constraintEqualToConstant:kCellHeight];
+    heightConstraint.priority = UILayoutPriorityDefaultLow;
+
     NSLayoutConstraint* trailingEdge = [_titleLabel.trailingAnchor
         constraintEqualToAnchor:self.contentView.trailingAnchor
                        constant:-kMargin];
     trailingEdge.priority = UILayoutPriorityDefaultHigh - 2;
-    trailingEdge.active = YES;
+    [NSLayoutConstraint
+        activateConstraints:@[ trailingEdge, heightConstraint ]];
 
     self.isAccessibilityElement = YES;
   }
