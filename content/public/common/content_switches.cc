3b60d272841c403deecc0f92a8dcf829b89c1907
Victor Costan <pwnall@chromium.org>
2018-10-10 02:08:53 +0000

    Revert "Set the maximum cache size per an origin in the command line for application cache"
    This reverts commit 0c9a4fc889912da38e88f815907272176314ce2f [formerly d143b1cc5e86eef3b2d7e8dacd629d24f58a5b6b].
    
    Reason for revert: Broke AppCache integration with Quota Manager. https://crbug.com/892547
    
    Original change's description:
    > Set the maximum cache size per an origin in the command line for application cache
    > 
    > Embedded devices have limited storage capacity generally. But current default
    > hard coded value is 5MB per an origin in application cache storage. But embedders
    > often want to reduce the default hard coded value per an origin as well. So it would
    > be good if we give the power them to set the maximum cache size per an origin for
    > application cache through the command line.
    > 
    > Bug: 824619
    > Change-Id: I078365ec412834c024a92ff5ec4ad5d00452e2a7
    > Reviewed-on: https://chromium-review.googlesource.com/979877
    > Commit-Queue: Gyuyoung Kim <gyuyoung.kim@lge.com>
    > Reviewed-by: Antoine Labour <piman@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#546105}
    
    TBR=piman@chromium.org,gyuyoung.kim@lge.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 824619
    Change-Id: I66fb8f36f7d08409af95a288512e2d751c59a88f
    Reviewed-on: https://chromium-review.googlesource.com/c/1272444
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598178}
    Former-commit-id: 1f05f839927e4ef9792d923d8ed0979785dbc137

--- content/public/common/content_switches.cc
+++ content/public/common/content_switches.cc
@@ -572,12 +572,7 @@ const char kLogFile[] = "log-file";
 const char kMainFrameResizesAreOrientationChanges[] =
     "main-frame-resizes-are-orientation-changes";
 
-// Specifies the maximum cache size per an origin for the ApplicationCache.
-// The default value is 5MB.
-const char kMaxAppCacheOriginCacheSizeMb[] =
-    "max-appcache-origin-cache-size-mb";
-
-// Specifies the maximum disk cache size for the ApplicationCache. The default
+// Specifies the maximum disk cache size for the ApplicationCache. Default
 // value is 250MB.
 const char kMaxAppCacheDiskCacheSizeMb[] = "max-appcache-disk-cache-size-mb";
 
