d4c543ed0a46d7949588af6da99104ef43911022
Samuel Huang <huangs@chromium.org>
2018-10-10 15:48:10 +0000

    [Zucchini] Fix patch apply failure from untranslatable abs32 references.
    Disassembler (Win32 and ELF) uses reloc to find abs32 locations, which
    are stored in (1) |abs32_locations_|. Later on, (1) is filtered to
    generate (2) actual abs32 locations.
    
    A patching failure case was discovered: Equivalence blocks must never
    cut across reference boundaries. However, it turns out blocks generated
    using (2) were subject to checks using (1), which triggers a failure
    since (1) - (2) is nonempty.
    
    One way for (1) != (2) to happen is from CURRENT_MODULE() usage, which
    creates an abs32 reference outside a section. This results in an abs32
    target whose RVA does not map to an offset using section data, and
    gets rejected by filtering logic for (2).
    
    Fix: Apply the filtering logic direcly to (1), so (1) == (2). Details:
    * Add RemoveUntranslatableAbs32() (abs32_utils.h), which uses the
      filtering logic for (2) to preemptively remove problematic RVAs
      from (1), so |abs32_locations_| matches (2). Extensive unit tests
      are added.
    * DisassemblerWin32<Traits>::ParseAndStoreAbs32(): Initialize
      |abs32_locations_| with 3 steps: Naive extraction from relocs,
      RemoveUntranslatableAbs32(), and RemoveOverlappingAbs32Locations().
    * DisassemblerElf<Traits>::GetAbs32FromRelocSections(): Do the same,
      noting that ELF's image base is always 0.
    
    Additional fixes:
    * address_translator.h: kInvalidRva was -1, but it should be -2 to
      better match kInvalidOffset.
    * Abs32RvaExtractorWin32::Abs32RvaExtractorWin32: The lambda
      |find_and_check| binds |addr|, which has been std::move()'ed.
      Better to just bind |this| and use |addr_|.
    
    Bug: 892284
    Change-Id: I628f4668ea231c7e06f35bd924652ca4d74bb848
    Reviewed-on: https://chromium-review.googlesource.com/c/1263877
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598342}
    Former-commit-id: b6d108f1cabab2a9f3fe46a7cdeb92685a2c790e

--- components/zucchini/abs32_utils.h
+++ components/zucchini/abs32_utils.h
@@ -45,7 +45,7 @@ class AbsoluteAddress {
   // the write and returns true. On failure (invalid |offset|), returns false.
   bool Write(offset_t offset, MutableBufferView* image);
 
-  size_t width() const { return WidthOf(bitness_); }
+  uint32_t width() const { return WidthOf(bitness_); }
 
   // Exposing |value_| for testing.
   uint64_t* mutable_value() { return &value_; }
@@ -125,6 +125,13 @@ class Abs32WriterWin32 : public ReferenceWriter {
 
   DISALLOW_COPY_AND_ASSIGN(Abs32WriterWin32);
 };
+
+// Given a list of abs32 |locations|, removes all elements whose targets cannot
+// be translated. Returns the number of elements removed.
+size_t RemoveUntranslatableAbs32(ConstBufferView image,
+                                 AbsoluteAddress&& addr,
+                                 const AddressTranslator& translator,
+                                 std::vector<offset_t>* locations);
 
 // Given a sorted list of abs32 |locations|, removes all elements whose body
 // (with |width| given) overlaps with the body of a previous element.
--- components/zucchini/disassembler_elf.cc
+++ components/zucchini/disassembler_elf.cc
@@ -19,6 +19,8 @@ namespace zucchini {
 
 namespace {
 
+constexpr uint64_t kElfImageBase = 0;
+
 // Determines whether |section| is a reloc section.
 template <class Traits>
 bool IsRelocSection(const typename Traits::Elf_Shdr& section) {
@@ -288,23 +290,33 @@ template <class Traits>
 void DisassemblerElf<Traits>::GetAbs32FromRelocSections() {
   constexpr int kAbs32Width = Traits::kVAWidth;
   DCHECK(abs32_locations_.empty());
-  auto relocs = MakeReadRelocs(0, offset_t(size()));
-  for (auto ref = relocs->GetNext(); ref; ref = relocs->GetNext()) {
-    // Reject null targets and targets outside |image_|. Note that here we
-    // assume abs32 targets are never "fake offsets".
-    if (ref->target > 0 && image_.covers({ref->target, kAbs32Width}))
-      abs32_locations_.push_back(ref->target);
-  }
-  abs32_locations_.shrink_to_fit();
+
+  // Read reloc targets as preliminary abs32 locations.
+  std::unique_ptr<ReferenceReader> relocs = MakeReadRelocs(0, offset_t(size()));
+  for (auto ref = relocs->GetNext(); ref.has_value(); ref = relocs->GetNext())
+    abs32_locations_.push_back(ref->target);
+
   std::sort(abs32_locations_.begin(), abs32_locations_.end());
 
+  // Abs32 references must have targets translatable to offsets. Remove those
+  // that are unable to do so.
+  // TODO(huangs): Investigate whether passing |Traits::kBitness| is correct:
+  // Some architectures using ELF might have 4-byte long abs32 body regardless
+  // of bitness.
+  size_t num_untranslatable =
+      RemoveUntranslatableAbs32(image_, {Traits::kBitness, kElfImageBase},
+                                translator_, &abs32_locations_);
+  LOG_IF(WARNING, num_untranslatable) << "Removed " << num_untranslatable
+                                      << " untranslatable abs32 references.";
+
   // Abs32 reference bodies must not overlap. If found, simply remove them.
-  size_t num_removed =
+  size_t num_overlapping =
       RemoveOverlappingAbs32Locations(kAbs32Width, &abs32_locations_);
-  if (num_removed) {
-    LOG(WARNING) << "Warning: Found and removed " << num_removed
-                 << " abs32 locations with overlapping bodies.";
-  }
+  LOG_IF(WARNING, num_overlapping)
+      << "Removed " << num_overlapping
+      << " abs32 references with overlapping bodies.";
+
+  abs32_locations_.shrink_to_fit();
 }
 
 template <class Traits>
@@ -392,9 +404,12 @@ template <class Traits>
 std::unique_ptr<ReferenceReader> DisassemblerElfIntel<Traits>::MakeReadAbs32(
     offset_t lo,
     offset_t hi) {
-  Abs32RvaExtractorWin32 abs_rva_extractor(this->image_,
-                                           AbsoluteAddress(Traits::kBitness, 0),
-                                           this->abs32_locations_, lo, hi);
+  // TODO(huangs): Don't use Abs32RvaExtractorWin32 here; use new class that
+  // caters to different ELF architectures (e.g., abs32 in AArch64 are 4 bytes
+  // long, not 8 bytes long).
+  Abs32RvaExtractorWin32 abs_rva_extractor(
+      this->image_, AbsoluteAddress(Traits::kBitness, kElfImageBase),
+      this->abs32_locations_, lo, hi);
   return std::make_unique<Abs32ReaderWin32>(std::move(abs_rva_extractor),
                                             this->translator_);
 }
@@ -402,8 +417,10 @@ std::unique_ptr<ReferenceReader> DisassemblerElfIntel<Traits>::MakeReadAbs32(
 template <class Traits>
 std::unique_ptr<ReferenceWriter> DisassemblerElfIntel<Traits>::MakeWriteAbs32(
     MutableBufferView image) {
+  // TODO(huangs): For AArch64, see if |Traits::kBitness| should be used here?
   return std::make_unique<Abs32WriterWin32>(
-      image, AbsoluteAddress(Traits::kBitness, 0), this->translator_);
+      image, AbsoluteAddress(Traits::kBitness, kElfImageBase),
+      this->translator_);
 }
 
 template <class Traits>
