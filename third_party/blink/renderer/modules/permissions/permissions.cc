c255ac308d0195ac4b6266f7621978f7ca20c5fc
Mugdha Lakhani <nator@chromium.org>
2018-10-11 17:15:33 +0000

    [Background Fetch] Integrate with origin trials.
    This CL adds the OriginTrialsEnabled idl attribute to all
    BackgroundFetch idl files, and a LayoutTest to test that when
    the origin trial is enabled, the properties in these interfaces
    are available.
    
    Change-Id: Ia71e28aaf5cbd03b943a5eff904d21e8f536ea84
    Bug: 894448
    Reviewed-on: https://chromium-review.googlesource.com/c/1261405
    Reviewed-by: Jason Chase <chasej@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598811}
    Former-commit-id: b62845059e2872d9ae4210b2dfddeadf73e21acf

--- third_party/blink/renderer/modules/permissions/permissions.cc
+++ third_party/blink/renderer/modules/permissions/permissions.cc
@@ -140,7 +140,8 @@ PermissionDescriptorPtr ParsePermission(ScriptState* script_state,
   if (name == "payment-handler")
     return CreatePermissionDescriptor(PermissionName::PAYMENT_HANDLER);
   if (name == "background-fetch") {
-    if (!RuntimeEnabledFeatures::BackgroundFetchEnabled()) {
+    if (!OriginTrials::BackgroundFetchEnabled(
+            ExecutionContext::From(script_state))) {
       exception_state.ThrowTypeError("Background Fetch is not enabled.");
       return nullptr;
     }
