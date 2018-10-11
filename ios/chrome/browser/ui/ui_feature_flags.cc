1ca2aae2e4c0abf49067b77a409767dac4dd8c96
edchin <edchin@chromium.org>
2018-10-11 01:18:48 +0000

    [ios] ClosingLastIncognitoTab flag disabled by default
    Turn off flag by default so it can be rolled out via experiment.
    
    Bug: 891396
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: Iff654222c1dab4faa0443a5ef271c09be8905870
    Reviewed-on: https://chromium-review.googlesource.com/c/1274840
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598614}
    Former-commit-id: dd8f33f8d296c893869967b1936768079de9a3e7

--- ios/chrome/browser/ui/ui_feature_flags.cc
+++ ios/chrome/browser/ui/ui_feature_flags.cc
@@ -9,7 +9,7 @@ const base::Feature kFirstResponderKeyWindow{"FirstResponderKeyWindow",
 
 // TODO(crbug.com/893314) : Remove this flag.
 const base::Feature kClosingLastIncognitoTab{"ClosingLastIncognitoTab",
-                                             base::FEATURE_ENABLED_BY_DEFAULT};
+                                             base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kCopyImage{"CopyImage", base::FEATURE_ENABLED_BY_DEFAULT};
 
