3ad750e0c1655758588b921ad14ac0edb1bdfe43
Jun Cai <juncai@chromium.org>
2018-10-11 01:25:50 +0000

    Network Service: Migrate chromeos::assistant::NetworkProviderImpl to NetworkConnectionTracker
    This migrates chromeos::assistant::NetworkProviderImpl from NetworkChangeNotifier to
    NetworkConnectionTracker, which works with the network service enabled.
    
    Bug: 887040
    Change-Id: I3099af12316db87a2d641a01eb27a6d9b08acfee
    Reviewed-on: https://chromium-review.googlesource.com/c/1270950
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598620}
    Former-commit-id: a918dc1f082d2269c8c9e2a71f59fcc5a87ad53a

--- chromeos/services/assistant/service.cc
+++ chromeos/services/assistant/service.cc
@@ -52,13 +52,14 @@ constexpr base::TimeDelta kMaxTokenRefreshDelay =
 
 }  // namespace
 
-Service::Service()
+Service::Service(network::NetworkConnectionTracker* network_connection_tracker)
     : platform_binding_(this),
       session_observer_binding_(this),
       token_refresh_timer_(std::make_unique<base::OneShotTimer>()),
       main_thread_task_runner_(base::ThreadTaskRunnerHandle::Get()),
       power_manager_observer_(this),
       voice_interaction_observer_binding_(this),
+      network_connection_tracker_(network_connection_tracker),
       weak_ptr_factory_(this) {
   registry_.AddInterface<mojom::AssistantPlatform>(base::BindRepeating(
       &Service::BindAssistantPlatformConnection, base::Unretained(this)));
@@ -283,7 +284,8 @@ void Service::CreateAssistantManagerService(bool enable_hotword) {
   context()->connector()->BindInterface(device::mojom::kServiceName,
                                         mojo::MakeRequest(&battery_monitor));
   assistant_manager_service_ = std::make_unique<AssistantManagerServiceImpl>(
-      context()->connector(), std::move(battery_monitor), this, enable_hotword);
+      context()->connector(), std::move(battery_monitor), this, enable_hotword,
+      network_connection_tracker_);
 
   // Bind to Assistant controller in ash.
   context()->connector()->BindInterface(ash::mojom::kServiceName,
