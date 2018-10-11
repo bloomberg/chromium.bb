05c444a8e7bb9d68c606f0e71bd850295247be97
Pedro Amaral <amaralp@chromium.org>
2018-10-11 18:16:35 +0000

    Hide HTS toggle when accessibility is enabled
    Hide HTS toggle when accessibility is enabled. Also fixed
    accessibility string for tabs. I had originally followed what
    AccessibilityTabModelWrapper.java did but it turned out there
    was a bug in that class:
    https://bugs.chromium.org/p/chromium/issues/detail?id=886975
    
    Bug: 892638
    Change-Id: Ief4a3c65b46d75ef3ffc9eb3786c0d26eac94c7d
    Reviewed-on: https://chromium-review.googlesource.com/c/1275028
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: Pedro Amaral <amaralp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598846}
    Former-commit-id: f6a8e159b31a0e487eb66b9272fd6b231b3cfc4d

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/IncognitoToggleTabLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/IncognitoToggleTabLayout.java
@@ -53,19 +53,18 @@ public class IncognitoToggleTabLayout extends TabLayout {
         mStandardButtonIcon = new AppCompatImageView(getContext());
         mTabSwitcherDrawable = TabSwitcherDrawable.createTabSwitcherDrawable(getContext(), false);
         mStandardButtonIcon.setImageDrawable(mTabSwitcherDrawable);
+        mStandardButtonIcon.setContentDescription(
+                getResources().getString(R.string.accessibility_tab_switcher_standard_stack));
         mIncognitoButtonIcon = new AppCompatImageView(getContext());
         mIncognitoButtonIcon.setImageResource(R.drawable.incognito_simple);
+        mIncognitoButtonIcon.setContentDescription(getResources().getString(
+                ChromeFeatureList.isEnabled(ChromeFeatureList.INCOGNITO_STRINGS)
+                        ? R.string.accessibility_tab_switcher_private_stack
+                        : R.string.accessibility_tab_switcher_incognito_stack));
 
-        mStandardButton =
-                newTab().setCustomView(mStandardButtonIcon)
-                        .setContentDescription(R.string.accessibility_tab_switcher_standard_stack);
+        mStandardButton = newTab().setCustomView(mStandardButtonIcon);
         addTab(mStandardButton);
-        mIncognitoButton =
-                newTab().setCustomView(mIncognitoButtonIcon)
-                        .setContentDescription(
-                                ChromeFeatureList.isEnabled(ChromeFeatureList.INCOGNITO_STRINGS)
-                                        ? R.string.accessibility_tab_switcher_private_stack
-                                        : R.string.accessibility_tab_switcher_incognito_stack);
+        mIncognitoButton = newTab().setCustomView(mIncognitoButtonIcon);
         addTab(mIncognitoButton);
 
         addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
