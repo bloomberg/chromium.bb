36b53ed0d45354c3c0e96f2e09d7aedbbf7d18fd
Xianzhu Wang <wangxianzhu@chromium.org>
2018-10-11 19:39:02 +0000

    Output more meaningful information on compositing layer paint offset mismatch
    Bug: 890598
    Cq-Include-Trybots: luci.chromium.try:linux_layout_tests_slimming_paint_v2;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I2aee8bf37891a163ce8182ef1c4e620eab5dcf2a
    Reviewed-on: https://chromium-review.googlesource.com/c/1276890
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598891}
    Former-commit-id: 2902364fae7a4ae8a6225a643d2a5a3be89900d8

--- third_party/blink/renderer/core/paint/compositing/compositing_layer_property_updater.cc
+++ third_party/blink/renderer/core/paint/compositing/compositing_layer_property_updater.cc
@@ -42,14 +42,18 @@ void CompositingLayerPropertyUpdater::Update(const LayoutObject& object) {
       fragment_data.PaintOffset() - mapping->SubpixelAccumulation();
   IntPoint snapped_paint_offset = RoundedIntPoint(layout_snapped_paint_offset);
 
+#if DCHECK_IS_ON()
   // A layer without visible contents can be composited due to animation.
   // Since the layer itself has no visible subtree, there is no guarantee
   // that all of its ancestors have a visible subtree. An ancestor with no
   // visible subtree can be non-composited despite we expected it to, this
   // resulted in the paint offset used by CompositedLayerMapping to mismatch.
   bool subpixel_accumulation_may_be_bogus = paint_layer->SubtreeIsInvisible();
-  DCHECK(layout_snapped_paint_offset == snapped_paint_offset ||
-         subpixel_accumulation_may_be_bogus);
+  if (!subpixel_accumulation_may_be_bogus) {
+    DCHECK_EQ(layout_snapped_paint_offset, snapped_paint_offset)
+        << object.DebugName();
+  }
+#endif
 
   base::Optional<PropertyTreeState> container_layer_state;
   auto SetContainerLayerState =
