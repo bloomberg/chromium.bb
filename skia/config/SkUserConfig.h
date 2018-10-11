3b2888495795de46d6acd555cf0126b948866e2e
Mike Klein <mtklein@chromium.org>
2018-10-11 18:21:12 +0000

    rebaseline SK_LEGACY_4F_XFERMODES
    Change-Id: Ib1ed6433c4166f42df1728d3f5e9322806a7128d
    Reviewed-on: https://chromium-review.googlesource.com/c/1273523
    Reviewed-by: Robert Phillips <robertphillips@google.com>
    Commit-Queue: Mike Klein <mtklein@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#598851}
    Former-commit-id: f2f9c4e9aeed7b4dbb277e5d12bd503fb7de8c2d

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -162,8 +162,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_THREADED_DAA_BUGS
 #endif
 
-#define SK_LEGACY_4F_XFERMODES
-
 #ifndef SK_SUPPORT_LEGACY_AAA_CHOICE
 #define SK_SUPPORT_LEGACY_AAA_CHOICE
 #endif
