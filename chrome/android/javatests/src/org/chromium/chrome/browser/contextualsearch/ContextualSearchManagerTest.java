d457364a81508e6a317b379f77efa97c83e37627
Donn Denman <donnd@chromium.org>
2018-10-12 17:09:46 +0000

    [TTS] Write snippet to UKM for offline development
    Writes a snippet-hash and doc-id to UKM iff a command-line flag is
    set that allows this development-only data to be logged. Disabled
    on Beta and Stable channels.
    
    See go/ukm-cs-5, go/ukm-dev-data, go/cs-sanitized for details.
    
    Unfortunately our ContextualSearchDelegate cannot decode a JSON 64bit value
    so we now expect to receive these new data in string format.
    
    Also fix a bug in the existing Ranker code: the native logger cannot log
    64-bit values, so we rename some misleading methods.  Also, we need to break
    these new data up into high/low 32-bit values when we log to Ranker.
    
    BUG=894568, 872902
    
    Change-Id: Id0dd3ac218bafeb23396f010c340e5e5531799f0
    Reviewed-on: https://chromium-review.googlesource.com/c/1264591
    Reviewed-by: Robert Kaplow (sloooow) <rkaplow@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Andrew Moylan <amoylan@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599258}
    Former-commit-id: 0036bbd07ae6edfb9c4ef0a7972b9265258620e7

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
@@ -146,6 +146,8 @@ public class ContextualSearchManagerTest {
         // Integer values should contain @Feature values only.
         Set<Integer> expectedOutcomes =
                 new HashSet<Integer>(ContextualSearchRankerLoggerImpl.OUTCOMES.keySet());
+        expectedOutcomes.remove(ContextualSearchInteractionRecorder.Feature.OUTCOME_DOC_ID);
+        expectedOutcomes.remove(ContextualSearchInteractionRecorder.Feature.OUTCOME_SNIPPET_HASH);
         // We don't log whether the quick action was clicked unless we actually have a quick action.
         expectedOutcomes.remove(
                 ContextualSearchInteractionRecorder.Feature.OUTCOME_WAS_QUICK_ACTION_CLICKED);
@@ -498,7 +500,7 @@ public class ContextualSearchManagerTest {
             mFakeServer.handleSearchTermResolutionResponse(mIsNetworkUnavailable, mResponseCode,
                     mSearchTerm, mDisplayText, mAlternateTerm, mMid, mDoPreventPreload,
                     mStartAdjust, mEndAdjust, mContextLanguage, mThumbnailUrl, mCaption,
-                    mQuickActionUri, mQuickActionCategory);
+                    mQuickActionUri, mQuickActionCategory, 0, 0);
         }
     }
 
@@ -3321,5 +3323,40 @@ public class ContextualSearchManagerTest {
                 return selection != null && selection.equals("Search");
             }
         });
+    }
+
+    private void assertRecordedSensitiveDataToUkm(boolean doAssert)
+            throws InterruptedException, TimeoutException {
+        simulateTapSearch("intelligence");
+        // The panel must be closed for outcomes to be logged.
+        // Close the panel by clicking far away in order to make sure the outcomes get logged by
+        // the hideContextualSearchUi call to writeRankerLoggerOutcomesAndReset.
+        clickWordNode("states-far");
+        waitForPanelToClose();
+        Assert.assertEquals(doAssert,
+                getRankerLogger().getOutcomesLogged().containsKey(
+                        ContextualSearchInteractionRecorder.Feature.OUTCOME_DOC_ID));
+        Assert.assertEquals(doAssert,
+                getRankerLogger().getOutcomesLogged().containsKey(
+                        ContextualSearchInteractionRecorder.Feature.OUTCOME_SNIPPET_HASH));
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"ContextualSearch"})
+    @CommandLineFlags.Add(ContextualSearchRankerLoggerImpl.UKM_DEV_DATA_TTS_ENABLE)
+    @MinAndroidSdkLevel(Build.VERSION_CODES.N)
+    public void testCanRecordSensitiveDataToUkm() throws InterruptedException, TimeoutException {
+        assertRecordedSensitiveDataToUkm(true);
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"ContextualSearch"})
+    @MinAndroidSdkLevel(Build.VERSION_CODES.N)
+    public void testDoesNotRecordSensitiveDataToUkmWithoutCommandLineFlag()
+            throws InterruptedException, TimeoutException {
+        // Same test as above, but without the command-line-flag, does not log.
+        assertRecordedSensitiveDataToUkm(false);
     }
 }
