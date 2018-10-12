da29dd57eee69cdf97290936c6327ca51128fef1
Philip Rogers <pdr@chromium.org>
2018-10-12 17:55:13 +0000

    Fix HUD location and size when displaying the FPS meter
    The author of https://crrev.com/588577 did not realize the HUD layer
    could be used for non-full-viewport rect debug information such as the
    FPS meter. This patch restores the non-rect size logic from
    https://crrev.com/588577 and adds a test of the FPS meter size.
    
    Bug: 894717
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ied6d6c80e396ab59dbc201994db5214ed1db09a0
    Reviewed-on: https://chromium-review.googlesource.com/c/1277658
    Reviewed-by: enne <enne@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599282}
    Former-commit-id: 1e1bdbbd406db941c6a0ff4c99271d7d1a9e89f6

--- cc/trees/layer_tree_host.cc
+++ cc/trees/layer_tree_host.cc
@@ -1412,14 +1412,10 @@ void LayerTreeHost::UpdateHudLayer(bool show_hud_info) {
   if (show_hud_info) {
     if (!hud_layer_.get())
       hud_layer_ = HeadsUpDisplayLayer::Create();
-
-    gfx::Size device_viewport_in_layout_pixels =
-        gfx::Size(device_viewport_size_.width() / device_scale_factor_,
-                  device_viewport_size_.height() / device_scale_factor_);
-    hud_layer_->SetBounds(device_viewport_in_layout_pixels);
-
     if (root_layer_.get() && !hud_layer_->parent())
       root_layer_->AddChild(hud_layer_);
+    hud_layer_->UpdateLocationAndSize(device_viewport_size_,
+                                      device_scale_factor_);
   } else if (hud_layer_.get()) {
     hud_layer_->RemoveFromParent();
     hud_layer_ = nullptr;
