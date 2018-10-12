1b9dbc5246c7af2a56341f27b909267fb131517c
Greg Kerr <kerrnel@chromium.org>
2018-10-12 16:44:31 +0000

    Revert "Reland "macOS V2 Sandbox: Remove the feature flags.""
    This reverts commit bdca2a4e5ca214354a95bc2502193c537af2e4fe [formerly 4173f2019d1dada6fa06afa9b3bf7339f0ebcb01].
    
    Reason for revert: The dependent CL was reverted so this has to be reverted.
    
    https://chromium-review.googlesource.com/c/chromium/src/+/1278162
    
    Original change's description:
    > Reland "macOS V2 Sandbox: Remove the feature flags."
    > 
    > This is a reland of dab64420d827d5e2b18412fd824b43bc3ed2761d [formerly 5c4908e8ad7f53e2ddb2cd5631dc701558704b8c]
    > 
    > Original change's description:
    > > macOS V2 Sandbox: Remove the feature flags.
    > >
    > > The V2 sandbox is now default for all but the network and GPU processes
    > > on macOS. This CL deletes the feature flag. A new experiment will be
    > > introduced for the GPU process. The default case for the sandbox profile
    > > switch is removed, because previous CHECK() failures were confusing to
    > > diagnose. All profiles must now be handled explicitly, so Chrome won't
    > > build if a new profile type isn't handled.
    > >
    > > Bug: 749839
    > > CQ-Depend: 1268671
    > > Change-Id: I0860d3a5148b0085be6a99dc01c4971602ee5582
    > > Reviewed-on: https://chromium-review.googlesource.com/c/1269996
    > > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > > Reviewed-by: Mark Pearson <mpearson@chromium.org>
    > > Reviewed-by: Avi Drissman <avi@chromium.org>
    > > Commit-Queue: Greg Kerr <kerrnel@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#598128}
    > 
    > CQ-DEPEND=CL:1268671
    > 
    > Bug: 749839
    > Change-Id: I91544907b640c572823f3aae3e29c0cfa003da51
    > Reviewed-on: https://chromium-review.googlesource.com/c/1274888
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    > Reviewed-by: Mark Pearson <mpearson@chromium.org>
    > Commit-Queue: Greg Kerr <kerrnel@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#598822}
    
    TBR=avi@chromium.org,mpearson@chromium.org,kerrnel@chromium.org,rsesek@chromium.org,mark@chromium.org
    
    Change-Id: I301c95634bd447d35ef19948cf0488c0bf847e58
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 749839
    Reviewed-on: https://chromium-review.googlesource.com/c/1278861
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Commit-Queue: Greg Kerr <kerrnel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599248}
    Former-commit-id: aea5927ad74b946bba2426d966ec139205aed808

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1803,6 +1803,9 @@ extern const char kMacRTLDescription[];
 extern const char kMacTouchBarName[];
 extern const char kMacTouchBarDescription[];
 
+extern const char kMacV2SandboxName[];
+extern const char kMacV2SandboxDescription[];
+
 extern const char kMacViewsNativeAppWindowsName[];
 extern const char kMacViewsNativeAppWindowsDescription[];
 
