229c9203c15bd67eef9f8a41604ee377faa0e58c
Luciano Pacheco <lucmult@chromium.org>
2018-10-12 00:07:24 +0000

    Files app: Select My files when there are no volumes
    Make Files app select "My files" when there are no available volumes,
    this to allow Files app to behave properly when volumes subsequently
    become available.
    
    Change DirectoryModel.onVolumeInfoListUpdated_ method to check for
    non-null |displayRoot| before trying to change to |displayRoot|. This
    fixes the error "Cannot read property 'getParent' of null" when Drive
    volume becomes available before Downloads volume, which is the default
    volume/root.
    
    Change FakeDriveFs to unbind the two mojo bindings if they're bound, so
    DriveFsTestVolume can re-mount itself.
    
    Test: browser_tests --gtest_filter="*/fileDisplayWithoutVolumesThenMount*"
    Bug: 893161, 884967
    Change-Id: Ic813b25261530495c11c9f641a92f6e07f883702
    Reviewed-on: https://chromium-review.googlesource.com/c/1272418
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599026}
    Former-commit-id: 5cd8c123e452c0b76fbbcaf53e82d86ca7d54930

--- ui/file_manager/file_manager/foreground/js/directory_model.js
+++ ui/file_manager/file_manager/foreground/js/directory_model.js
@@ -1197,7 +1197,8 @@ DirectoryModel.prototype.onVolumeInfoListUpdated_ = function(event) {
   var entry = this.getCurrentDirEntry();
   if (entry && !this.volumeManager_.getVolumeInfo(entry)) {
     this.volumeManager_.getDefaultDisplayRoot(function(displayRoot) {
-      this.changeDirectoryEntry(displayRoot);
+      if (displayRoot)
+        this.changeDirectoryEntry(displayRoot);
     }.bind(this));
   }
 
