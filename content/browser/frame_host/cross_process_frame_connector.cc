16bc101d27bee2fd4d63cc4a512abc7732bf5cfa
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-10-13 03:54:15 +0000

    Revert "Reland "Fling bubbles from OOPIF properly.""
    This reverts commit bfdf949fd84b9fc08228023861027cd30ec41641 [formerly 21ba6121d2ba5821515bcb9f3fdf768204425061].
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 599360 as the
    culprit for flakes in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/flake/flake-culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyQwsSDEZsYWtlQ3VscHJpdCIxY2hyb21pdW0vMjFiYTYxMjFkMmJhNTgyMTUxNWJjYjlmM2ZkZjc2ODIwNDQyNTA2MQw
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.chromiumos/linux-chromeos-rel/14616
    
    Sample Failed Step: viz_content_browsertests
    
    Sample Flaky Test: BrowserSideFlingBrowserTest.GFCGetsBubbledFromOOPIF
    
    Original change's description:
    > Reland "Fling bubbles from OOPIF properly."
    > 
    > This is a reland of ed1d81a2b6cca280adb59ae53528992a1a97dc01 [formerly a12ef847a57f4aa0282ca47b555c04da18b00f9e]
    > 
    > The reland has the following two fixes:
    > 1- In the added tests, we wait for the ack of the GSB from the parent view to make
    > sure that GFS is processed after GSB has been bubbled to avoid the flakiness.
    > 2- Early return in BubbleScrollEvent to avoid crbug.com/828422
    > 
    > Original change's description:
    > > Fling bubbles from OOPIF properly.
    > >
    > > While we are bubbling a scroll sequence GFS is sent to the fling controllers
    > > of both the oopif and the bubbling target. The fling controller of the oopif
    > > calls progressFling and generates a GSU with inertial phase, the GSU gets acked
    > > immediately before getting sent to the renderer since we are in bubbling mode.
    > > Then the oopif's fling controller receives the unconsumed GSU ack and generates
    > > a GSE to complete the scrolling sequence on the oopif. The bubbling target's
    > > fling controller receives and processes the GFS and generates GSUs which are
    > > sent to the target's renderer.
    > >
    > > If the GSE generated by the oopif's fling controller gets bubbled before
    > > the GFS, the bubbling target gets reset and the target does not receive the
    > > GFS. This cl fixes the issue by making sure that the events generated by the
    > > oopif's fling controller are not bubbled to the target. Also in case of
    > > GFS bubbling, the GFC should also get sent to the bubbling target to make
    > > sure that the fling controller in charge of flinging receives the GFC.
    > >
    > >
    > > Bug: 884728, 249063
    > > Test: BrowserSideFlingBrowserTest.[Touchpad|Touchscreen]FlingBubblesFromOOPIF
    > > Change-Id: I064944f125bebcb746f329af4cfb117f3be94ff0
    > > Reviewed-on: https://chromium-review.googlesource.com/c/1274193
    > > Reviewed-by: Kevin McNee <mcnee@chromium.org>
    > > Reviewed-by: Charlie Reis <creis@chromium.org>
    > > Commit-Queue: Charlie Reis <creis@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#599001}
    > 
    > Bug: 884728, 249063
    > Change-Id: I1528d904e84ff85f7828392c2b0178a42869d529
    > Reviewed-on: https://chromium-review.googlesource.com/c/1278963
    > Reviewed-by: Kevin McNee <mcnee@chromium.org>
    > Reviewed-by: Charlie Reis <creis@chromium.org>
    > Commit-Queue: Sahel Sharify <sahel@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#599360}
    
    Change-Id: I5553104cdf30a18fb9399b486d5c403a67dda409
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 884728, 249063, 895059
    Reviewed-on: https://chromium-review.googlesource.com/c/1279943
    Cr-Commit-Position: refs/heads/master@{#599483}
    Former-commit-id: 621e5c1c2e0a840308a15e58272228537fe482ab

--- content/browser/frame_host/cross_process_frame_connector.cc
+++ content/browser/frame_host/cross_process_frame_connector.cc
@@ -279,8 +279,7 @@ void CrossProcessFrameConnector::BubbleScrollEvent(
   DCHECK(event.GetType() == blink::WebInputEvent::kGestureScrollBegin ||
          event.GetType() == blink::WebInputEvent::kGestureScrollUpdate ||
          event.GetType() == blink::WebInputEvent::kGestureScrollEnd ||
-         event.GetType() == blink::WebInputEvent::kGestureFlingStart ||
-         event.GetType() == blink::WebInputEvent::kGestureFlingCancel);
+         event.GetType() == blink::WebInputEvent::kGestureFlingStart);
   auto* parent_view = GetParentRenderWidgetHostView();
 
   if (!parent_view)
@@ -303,9 +302,7 @@ void CrossProcessFrameConnector::BubbleScrollEvent(
   if (event.GetType() == blink::WebInputEvent::kGestureScrollBegin) {
     event_router->BubbleScrollEvent(parent_view, resent_gesture_event, view_);
     is_scroll_bubbling_ = true;
-  } else if (is_scroll_bubbling_ ||
-             event.GetType() == blink::WebInputEvent::kGestureFlingCancel) {
-    // For GFC events the router decides whether to bubble them or not.
+  } else if (is_scroll_bubbling_) {
     event_router->BubbleScrollEvent(parent_view, resent_gesture_event, view_);
   }
   if (event.GetType() == blink::WebInputEvent::kGestureScrollEnd ||
