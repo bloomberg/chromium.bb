5afd0ac103146e5ecf8e5bd7524584c9b891e966
sczs <sczs@chromium.org>
2018-10-15 16:07:59 +0000

    [iOS] Makes InfoBarCoordinator a TabModel observer.
    - Moves all the TabModel observer infoBar related code from BVC to the infoBarCoordinator.
    - Makes infoBarCoordinator conform to the SigninPresenter protocol.
    - Plumbs a dispatcher a tabModel to the infoBarCoordinator.
    
    Bug: 892376
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I002fa94a09566770117f1a30c7c6d656e0fba559
    Reviewed-on: https://chromium-review.googlesource.com/c/1265501
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599648}
    Former-commit-id: 4bbc7bc229d0a80db1c5cf94d10977bca7014e8a

--- ios/chrome/browser/ui/infobars/infobar_coordinator.mm
+++ ios/chrome/browser/ui/infobars/infobar_coordinator.mm
@@ -10,14 +10,25 @@
 #include "ios/chrome/browser/infobars/infobar_container_delegate_ios.h"
 #include "ios/chrome/browser/infobars/infobar_container_ios.h"
 #include "ios/chrome/browser/infobars/infobar_container_view.h"
+#include "ios/chrome/browser/infobars/infobar_manager_impl.h"
+#import "ios/chrome/browser/tabs/tab.h"
+#import "ios/chrome/browser/tabs/tab_model.h"
+#import "ios/chrome/browser/tabs/tab_model_observer.h"
+#import "ios/chrome/browser/ui/authentication/re_signin_infobar_delegate.h"
+#import "ios/chrome/browser/ui/commands/application_commands.h"
 #import "ios/chrome/browser/ui/infobars/infobar_positioner.h"
+#import "ios/chrome/browser/ui/settings/sync_utils/sync_util.h"
+#import "ios/chrome/browser/ui/signin_interaction/public/signin_presenter.h"
+#include "ios/chrome/browser/upgrade/upgrade_center.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
-@interface InfobarCoordinator ()<InfobarContainerStateDelegate> {
-  // Bridge class to deliver container change notifications to BVC.
+@interface InfobarCoordinator ()<InfobarContainerStateDelegate,
+                                 TabModelObserver,
+                                 SigninPresenter> {
+  // Bridge class to deliver container change notifications.
   std::unique_ptr<InfoBarContainerDelegateIOS> _infoBarContainerDelegate;
 
   // A single infobar container handles all infobars in all tabs. It keeps
@@ -25,22 +36,40 @@
   // the current tab).
   std::unique_ptr<InfoBarContainerIOS> _infoBarContainer;
 }
+@property(nonatomic, assign) TabModel* tabModel;
 
 @end
 
 @implementation InfobarCoordinator
 
-- (nullable instancetype)
-initWithBaseViewController:(nullable UIViewController*)viewController
-              browserState:(ios::ChromeBrowserState*)browserState {
+- (instancetype)initWithBaseViewController:(UIViewController*)viewController
+                              browserState:
+                                  (ios::ChromeBrowserState*)browserState
+                                  tabModel:(TabModel*)tabModel {
   self = [super initWithBaseViewController:viewController
                               browserState:browserState];
   if (self) {
+    _tabModel = tabModel;
+    [_tabModel addObserver:self];
     _infoBarContainerDelegate.reset(new InfoBarContainerDelegateIOS(self));
     _infoBarContainer.reset(
         new InfoBarContainerIOS(_infoBarContainerDelegate.get()));
   }
   return self;
+}
+
+- (void)start {
+  infobars::InfoBarManager* infoBarManager = nullptr;
+  if (self.tabModel.currentTab) {
+    DCHECK(self.tabModel.currentTab.webState);
+    infoBarManager =
+        InfoBarManagerImpl::FromWebState(self.tabModel.currentTab.webState);
+  }
+  _infoBarContainer->ChangeInfoBarManager(infoBarManager);
+}
+
+- (void)stop {
+  [self.tabModel removeObserver:self];
 }
 
 #pragma mark - Public Interface
@@ -57,9 +86,6 @@ initWithBaseViewController:(nullable UIViewController*)viewController
   _infoBarContainer->SuspendInfobars();
 }
 
-- (void)setInfobarManager:(infobars::InfoBarManager*)infoBarManager {
-  _infoBarContainer->ChangeInfoBarManager(infoBarManager);
-}
 
 - (void)updateInfobarContainer {
   [self infoBarContainerStateDidChangeAnimated:NO];
@@ -94,6 +120,63 @@ initWithBaseViewController:(nullable UIViewController*)viewController
     frameUpdates();
     completion(YES);
   }
+}
+
+#pragma mark - TabModelObserver methods
+
+- (void)tabModel:(TabModel*)model
+    didChangeActiveTab:(Tab*)newTab
+           previousTab:(Tab*)previousTab
+               atIndex:(NSUInteger)index {
+  DCHECK(newTab.webState);
+  infobars::InfoBarManager* infoBarManager =
+      InfoBarManagerImpl::FromWebState(newTab.webState);
+  _infoBarContainer->ChangeInfoBarManager(infoBarManager);
+}
+
+- (void)tabModel:(TabModel*)model
+    newTabWillOpen:(Tab*)tab
+      inBackground:(BOOL)background {
+  // When adding new tabs, check what kind of reminder infobar should
+  // be added to the new tab. Try to add only one of them.
+  // This check is done when a new tab is added either through the Tools Menu
+  // "New Tab", through a long press on the Tab Switcher button "New Tab", and
+  // through creating a New Tab from the Tab Switcher. This method is called
+  // after a new tab has added and finished initial navigation. If this is added
+  // earlier, the initial navigation may end up clearing the infobar(s) that are
+  // just added.
+  web::WebState* webState = tab.webState;
+  DCHECK(webState);
+
+  infobars::InfoBarManager* infoBarManager =
+      InfoBarManagerImpl::FromWebState(webState);
+  [[UpgradeCenter sharedInstance] addInfoBarToManager:infoBarManager
+                                             forTabId:[tab tabId]];
+
+  if (!ReSignInInfoBarDelegate::Create(self.browserState, tab,
+                                       self /* id<SigninPresenter> */)) {
+    DisplaySyncErrors(self.browserState, tab,
+                      self.syncPresenter /* id<SyncPresenter> */);
+  }
+}
+
+- (void)tabModel:(TabModel*)model
+    didReplaceTab:(Tab*)oldTab
+          withTab:(Tab*)newTab
+          atIndex:(NSUInteger)index {
+  infobars::InfoBarManager* infoBarManager = nullptr;
+  if (newTab) {
+    DCHECK(newTab.webState);
+    infoBarManager = InfoBarManagerImpl::FromWebState(newTab.webState);
+  }
+  _infoBarContainer->ChangeInfoBarManager(infoBarManager);
+}
+
+#pragma mark - SigninPresenter
+
+- (void)showSignin:(ShowSigninCommand*)command {
+  [self.dispatcher showSignin:command
+           baseViewController:self.baseViewController];
 }
 
 @end
