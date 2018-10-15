b5fc48806d10c5121ba481d7ac3d1b952997ea40
Alex Betts <abetts3@bloomberg.net>
2018-10-15 14:07:16 -0400

    Reroute mousewheel events to HWND's in the webview's HWND hierarchy
    WM_MOUSEWHEEL events are delivered to the HWND that currently has focus, even
    if the mouse is over a different HWND. Chromium's 'ui' layer includes
    functions that will reroute mouse wheel events if they are over a different
    HWND that meet certain criteria. This commit adds an opttion to
    views::Widget::InitParams that will expand that criteria to include any HWND
    that is in the same HWND hierarchy as the webview HWND that received the
    event in the first place. The purpose of this is to distribute WM_MOUSEWHEEL
    events to any HWND that is embedded in the webview as a plugin, including
    HWND's owned by different processes, etc.

--- src/ui/views/win/hwnd_message_handler.cc
+++ src/ui/views/win/hwnd_message_handler.cc
@@ -401,6 +401,7 @@ HWNDMessageHandler::HWNDMessageHandler(HWNDMessageHandlerDelegate* delegate)
       pointer_events_for_touch_(features::IsUsingWMPointerForTouch()),
       precision_touchpad_scroll_phase_enabled_(base::FeatureList::IsEnabled(
           features::kPrecisionTouchpadScrollPhase)),
+      reroute_mouse_wheel_to_any_related_window_(false),
       autohide_factory_(this) {}
 
 HWNDMessageHandler::~HWNDMessageHandler() {
@@ -833,6 +834,11 @@ bool HWNDMessageHandler::SetTitle(const base::string16& title) {
 }
 
 void HWNDMessageHandler::SetCursor(HCURSOR cursor) {
+  if (is_cursor_overridden_) {
+    current_cursor_ = cursor;
+    return;
+  }
+
   if (cursor) {
     previous_cursor_ = ::SetCursor(cursor);
     current_cursor_ = cursor;
@@ -983,6 +989,9 @@ LRESULT HWNDMessageHandler::OnWndProc(UINT message,
   if (delegate_) {
     delegate_->PostHandleMSG(message, w_param, l_param);
     if (message == WM_NCDESTROY) {
+      if (!handled_wm_destroy_) {
+        OnDestroy();
+      }
       RestoreEnabledIfNecessary();
       delegate_->HandleDestroyed();
     }
@@ -1607,6 +1616,8 @@ void HWNDMessageHandler::OnDestroy() {
       break;
     }
   }
+
+  handled_wm_destroy_ = true;
 }
 
 void HWNDMessageHandler::OnDisplayChange(UINT bits_per_pixel,
@@ -2309,6 +2320,7 @@ LRESULT HWNDMessageHandler::OnSetCursor(UINT message,
       break;
     case HTTOPLEFT:
     case HTBOTTOMRIGHT:
+    case HTOBJECT:  /* see blpwtk2_webviewimpl.cc: HTBOTTOMRIGHT is 'special' in Windows, so we don't use it */
       cursor = IDC_SIZENWSE;
       break;
     case HTTOPRIGHT:
@@ -2316,6 +2328,7 @@ LRESULT HWNDMessageHandler::OnSetCursor(UINT message,
       cursor = IDC_SIZENESW;
       break;
     case HTCLIENT:
+      is_cursor_overridden_ = false;
       SetCursor(current_cursor_);
       return 1;
     case LOWORD(HTERROR):  // Use HTERROR's LOWORD value for valid comparison.
@@ -2325,6 +2338,7 @@ LRESULT HWNDMessageHandler::OnSetCursor(UINT message,
       // Use the default value, IDC_ARROW.
       break;
   }
+  is_cursor_overridden_ = true;
   ::SetCursor(LoadCursor(NULL, cursor));
   return 1;
 }
@@ -2868,7 +2882,7 @@ LRESULT HWNDMessageHandler::HandleMouseEventInternal(UINT message,
   } else if (event.type() == ui::ET_MOUSEWHEEL) {
     ui::MouseWheelEvent mouse_wheel_event(msg);
     // Reroute the mouse wheel to the window under the pointer if applicable.
-    return (ui::RerouteMouseWheel(hwnd(), w_param, l_param) ||
+    return (ui::RerouteMouseWheel(hwnd(), w_param, l_param, reroute_mouse_wheel_to_any_related_window_) ||
             delegate_->HandleMouseEvent(&mouse_wheel_event))
                ? 0
                : 1;
