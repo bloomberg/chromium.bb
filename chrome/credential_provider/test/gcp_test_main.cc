614c4daaa2a5fae5ec5de5605deffc975a40bc7d
Roger Tawa <rogerta@chromium.org>
2018-10-15 17:18:37 +0000

    Add credential provider unit tests run on the buildbots.
    Fix up the tests so that they run correctly with base::LaunchUnitTests().
    Add the unit test exe to the buildbot configs.
    
    Bug: 789588
    Change-Id: Ib0ccf2bf965071e6afe2638db1fe3e565799c3e4
    Reviewed-on: https://chromium-review.googlesource.com/c/1274456
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599656}
    Former-commit-id: ba8218bfb5dec7140b62acf40e91ab0188ec3608

--- chrome/credential_provider/test/gcp_test_main.cc
+++ chrome/credential_provider/test/gcp_test_main.cc
@@ -1,21 +1,17 @@
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-//
-// Main entry point for all unit tests.
 
-#include "base/at_exit.h"
-#include "base/command_line.h"
+#include "base/bind.h"
+#include "base/test/launcher/unit_test_launcher.h"
+#include "base/test/test_suite.h"
 #include "chrome/credential_provider/gaiacp/gaia_credential_provider_module.h"
-#include "testing/gmock/include/gmock/gmock.h"
-#include "testing/gtest/include/gtest/gtest.h"
 
 credential_provider::CGaiaCredentialProviderModule _AtlModule;
 
 int main(int argc, char** argv) {
-  base::AtExitManager at_exit;
-  base::CommandLine::Init(argc, argv);
-  testing::InitGoogleMock(&argc, argv);
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  base::TestSuite test_suite(argc, argv);
+  return base::LaunchUnitTests(
+      argc, argv,
+      base::BindOnce(&base::TestSuite::Run, base::Unretained(&test_suite)));
 }
