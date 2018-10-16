5a24ca34712f6fe109d006369d439f7db3e90508
John Z Wu <jzw@chromium.org>
2018-10-16 03:14:53 +0000

    Fix formatting for some public header files.
    imports should come after the header guards.
    
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Change-Id: I2644a6978cdaf2019e2ad78f990c6c2e0d455f1a
    Reviewed-on: https://chromium-review.googlesource.com/c/1282387
    Commit-Queue: John Wu <jzw@chromium.org>
    Commit-Queue: Hiroshi Ichikawa <ichikawa@chromium.org>
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599829}
    Former-commit-id: 39446d4ed2beca94ca2a9cdf68d1b869731c01b6

--- ios/web_view/public/cwv_web_view.h
+++ ios/web_view/public/cwv_web_view.h
@@ -1,6 +1,7 @@
 // Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 #ifndef IOS_WEB_VIEW_PUBLIC_CWV_WEB_VIEW_H_
 #define IOS_WEB_VIEW_PUBLIC_CWV_WEB_VIEW_H_
 
