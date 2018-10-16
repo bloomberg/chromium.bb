932a92af398048ca5f007e79cf4fe973b8e4f925
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2018-10-16 04:44:41 +0000

    bluetooth: Implement TrayBluetoothHelperExperimental::GetBluetoothState
    Implements GetBluetoothState() by using the new BluetoothSystem mojo
    interface.
    
    Bug: 882346
    Change-Id: I75f11b9de33c98e2913aeb08a1e5e085532e803d
    Reviewed-on: https://chromium-review.googlesource.com/c/1272957
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599854}
    Former-commit-id: c87b6f34e85ea9955986fbca7869b98b449dddf5

--- ash/system/bluetooth/tray_bluetooth_helper_experimental.cc
+++ ash/system/bluetooth/tray_bluetooth_helper_experimental.cc
@@ -5,14 +5,43 @@
 #include "ash/system/bluetooth/tray_bluetooth_helper_experimental.h"
 
 #include <string>
+#include <utility>
+
+#include "ash/shell.h"
+#include "ash/system/tray/system_tray_notifier.h"
+#include "services/device/public/mojom/constants.mojom.h"
+#include "services/service_manager/public/cpp/connector.h"
+#include "services/service_manager/public/cpp/identity.h"
+
+// base::Unretained():
+//
+// Usage of `base::Unretained(this)` is safe when calling BluetoothSystemPtr
+// methods because BluetoothSystemPtr is owned by `this` and guarantees that no
+// callbacks will be run after its destruction.
 
 namespace ash {
 
-TrayBluetoothHelperExperimental::TrayBluetoothHelperExperimental() = default;
+TrayBluetoothHelperExperimental::TrayBluetoothHelperExperimental(
+    service_manager::Connector* connector)
+    : connector_(connector) {}
 
 TrayBluetoothHelperExperimental::~TrayBluetoothHelperExperimental() = default;
 
-void TrayBluetoothHelperExperimental::Initialize() {}
+void TrayBluetoothHelperExperimental::Initialize() {
+  device::mojom::BluetoothSystemFactoryPtr bluetooth_system_factory;
+  connector_->BindInterface(device::mojom::kServiceName,
+                            &bluetooth_system_factory);
+
+  device::mojom::BluetoothSystemClientPtr client_ptr;
+  bluetooth_system_client_binding_.Bind(mojo::MakeRequest(&client_ptr));
+
+  bluetooth_system_factory->Create(mojo::MakeRequest(&bluetooth_system_ptr_),
+                                   std::move(client_ptr));
+  bluetooth_system_ptr_->GetState(
+      base::BindOnce(&TrayBluetoothHelperExperimental::OnStateChanged,
+                     // See base::Unretained() note at the top.
+                     base::Unretained(this)));
+}
 
 BluetoothDeviceList
 TrayBluetoothHelperExperimental::GetAvailableBluetoothDevices() const {
@@ -35,8 +64,7 @@ void TrayBluetoothHelperExperimental::ConnectToBluetoothDevice(
 
 device::mojom::BluetoothSystem::State
 TrayBluetoothHelperExperimental::GetBluetoothState() {
-  NOTIMPLEMENTED();
-  return device::mojom::BluetoothSystem::State::kUnavailable;
+  return cached_state_;
 }
 
 void TrayBluetoothHelperExperimental::SetBluetoothEnabled(bool enabled) {
@@ -46,6 +74,12 @@ void TrayBluetoothHelperExperimental::SetBluetoothEnabled(bool enabled) {
 bool TrayBluetoothHelperExperimental::HasBluetoothDiscoverySession() {
   NOTIMPLEMENTED();
   return false;
+}
+
+void TrayBluetoothHelperExperimental::OnStateChanged(
+    device::mojom::BluetoothSystem::State state) {
+  cached_state_ = state;
+  Shell::Get()->system_tray_notifier()->NotifyRefreshBluetooth();
 }
 
 }  // namespace ash
