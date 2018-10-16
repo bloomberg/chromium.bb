554a7384823fdf2fd61e0ec39fd532a30bcb0fc8
A Olsen <olsen@chromium.org>
2018-10-16 09:46:32 +0000

    Reland "Move InstallAttributes to chromeos/settings"
    This is a reland of 6c6472741d693870cadb71edc4a9218be24c4b7f [formerly aa6c62dce936e966e2db445045dd04414553642b]
    
    Original change's description:
    > Move InstallAttributes to chromeos/settings
    >
    > Necessary for moving CrosSettings to chromeos/settings, so it
    > can eventually be used by Mustash.
    >
    > The following changes were necessary:
    > 1. Move install_attributes{.h, .cc} and unit_test.cc
    > 2. Update c/b/chromeos/BUILD.gn (source) and chromeos/settings/BUILD.gn
    > (destination)
    > 3. Update chromeos/DEPS to allow a dependency on cloud_policy_constants.h,
    > which is included by install_attributes.h
    >
    > 4. Unfortunately, that same dependency lead to a circular dependency.
    > I was able to avoid that by separating out a "common_constants" rule in
    > components/policy/core/common, so that chromeos/settings only depends on
    > that, and not on all of components/policy. So I had to change
    > components/policy/core/common/BUILD.gn
    > and I removed an unnecessary include from
    > components/policy/core/common/cloud/cloud_policy_constants.cc
    >
    > 5. Update #include directives for install_attributes to point to new
    > location. This is the other 22 files, all with diffs of +1 -1.
    >
    > Bug: 446937
    > Change-Id: Icc8ba19c5fec91b13004b19680ead9c9f8b0a288
    > Reviewed-on: https://chromium-review.googlesource.com/c/1264757
    > Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    > Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    > Commit-Queue: A Olsen <olsen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#599140}
    
    Bug: 446937
    Change-Id: If20ad953b2cb9c3808415c7c112eec8d0e97915b
    Reviewed-on: https://chromium-review.googlesource.com/c/1278750
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: A Olsen <olsen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599933}
    Former-commit-id: b346dc5435f1f61bf2a791e3467433f34b2e66da

--- chromeos/settings/install_attributes_unittest.cc
+++ chromeos/settings/install_attributes_unittest.cc
@@ -2,7 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/chromeos/settings/install_attributes.h"
+#include "chromeos/settings/install_attributes.h"
+
 
 #include <memory>
 
