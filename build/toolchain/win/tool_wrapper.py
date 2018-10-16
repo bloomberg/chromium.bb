a90b797cf8181d70e7d402273d66571294be8864
Tom Tan <Tom.Tan@microsoft.com>
2018-10-16 19:37:14 +0000

    Add Windows ARM64 support to Chromium build scripts.
    This change mostly contains build script change under //chrome/build only.
    It is targeting MSVC arm64 toolset, but enable clang-cl
    could be based on this change.
    
    Bug: 893460
    Change-Id: Ia6f69f067a97fcaeb77021c1019bc594b0859eda
    Reviewed-on: https://chromium-review.googlesource.com/c/1272076
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Steven Valdez <svaldez@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600083}
    Former-commit-id: d5bf20624a99401dcb6c9e6aee9244334008222b

--- build/toolchain/win/tool_wrapper.py
+++ build/toolchain/win/tool_wrapper.py
@@ -168,6 +168,11 @@ class WinTool(object):
   def ExecAsmWrapper(self, arch, *args):
     """Filter logo banner from invocations of asm.exe."""
     env = self._GetEnv(arch)
+    if sys.platform == 'win32':
+        # Windows ARM64 uses clang-cl as assembler which has '/' as path
+        # separator, convert it to '\\' when running on Windows.
+        args = list(args) # *args is a tuple by default, which is read-only
+        args[0] = args[0].replace('/', '\\')
     popen = subprocess.Popen(args, shell=True, env=env,
                              stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     out, _ = popen.communicate()
