72992a080eb27c8bff6ba8587d6b7b8fc5b7d2e2
Patrick Monette <pmonette@chromium.org>
2018-10-16 03:06:32 +0000

    Refactor the SelectFileDialog implementation on Windows
    This is meant to make the code easier to read by making ownership more
    obvious and standardize a little bit the different functions that
    implement the various dialog types.
    
    Also will facilitate a possible transition to the IFileDialog interface
    
    This CL should not change the behavior of the dialogs.
    
    Bug: 73098,884075
    Change-Id: I0d2f5cc02e1d08ab3f54385fb166bc34ae1f5b53
    Reviewed-on: https://chromium-review.googlesource.com/c/1249911
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#599827}
    Former-commit-id: e5752ae048ec71bd95bdee2ab6d17b4373d871f7

--- ui/shell_dialogs/select_file_dialog_win_unittest.cc
+++ ui/shell_dialogs/select_file_dialog_win_unittest.cc
@@ -26,44 +26,6 @@
 #include "ui/shell_dialogs/select_file_policy.h"
 #include "ui/strings/grit/ui_strings.h"
 
-TEST(ShellDialogsWin, AppendExtensionIfNeeded) {
-  struct AppendExtensionTestCase {
-    const wchar_t* filename;
-    const wchar_t* filter_selected;
-    const wchar_t* suggested_ext;
-    const wchar_t* expected_filename;
-  } test_cases[] = {
-    // Known extensions, with or without associated MIME types, should not get
-    // an extension appended.
-    { L"sample.html",    L"*.txt",     L"txt",     L"sample.html" },
-    { L"sample.reg",     L"*.txt",     L"txt",     L"sample.reg" },
-
-    // An unknown extension, or no extension, should get the default extension
-    // appended.
-    { L"sample.unknown", L"*.txt",     L"txt",     L"sample.unknown.txt" },
-    { L"sample",         L"*.txt",     L"txt",     L"sample.txt" },
-    // ...unless the unknown and default extensions match.
-    { L"sample.unknown", L"*.unknown", L"unknown", L"sample.unknown" },
-
-    // The extension alone should be treated like a filename with no extension.
-    { L"txt",            L"*.txt",     L"txt",     L"txt.txt" },
-
-    // Trailing dots should cause us to append an extension.
-    { L"sample.txt.",    L"*.txt",     L"txt",     L"sample.txt.txt" },
-    { L"...",            L"*.txt",     L"txt",     L"...txt" },
-
-    // If the filter is changed to "All files", we allow any filename.
-    { L"sample.unknown", L"*.*",       L"",        L"sample.unknown" },
-  };
-
-  for (size_t i = 0; i < arraysize(test_cases); ++i) {
-    EXPECT_EQ(std::wstring(test_cases[i].expected_filename),
-              ui::AppendExtensionIfNeeded(test_cases[i].filename,
-                                          test_cases[i].filter_selected,
-                                          test_cases[i].suggested_ext));
-  }
-}
-
 namespace {
 
 // The default title for the various dialogs.
@@ -169,9 +131,7 @@ void SendCommand(HWND window, int id) {
 class SelectFileDialogWinTest : public ::testing::Test,
                                 public ui::SelectFileDialog::Listener {
  public:
-  SelectFileDialogWinTest()
-      : scoped_task_environment_(
-            base::test::ScopedTaskEnvironment::MainThreadType::UI) {}
+  SelectFileDialogWinTest() = default;
   ~SelectFileDialogWinTest() override = default;
 
   // ui::SelectFileDialog::Listener:
