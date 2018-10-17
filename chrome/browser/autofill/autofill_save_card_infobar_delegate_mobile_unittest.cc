66b76d940574c8870c2320523d8468580a78e157
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-10-17 22:54:11 +0000

    Revert "[Autofill] Add strikes when credit card save ignored/denied/fails"
    This reverts commit 3198989fbe76fd438bcaf80b488e5df9def33b29 [formerly d82a26bd00c46d54e07521af6fb56a9348edae90].
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 600490 as the
    culprit for flakes in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/flake/flake-culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyQwsSDEZsYWtlQ3VscHJpdCIxY2hyb21pdW0vZDgyYTI2YmQwMGM0NmQ1NGUwNzUyMWFmNmZiNTZhOTM0OGVkYWU5MAw
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.win/Win%207%20Tests%20x64%20%281%29/44415
    
    Sample Failed Step: browser_tests
    
    Sample Flaky Test: SaveCardBubbleViewsFullFormBrowserTest.StrikeDatabase_Local_AddStrikeIfBubbleIgnored
    
    Original change's description:
    > [Autofill] Add strikes when credit card save ignored/denied/fails
    > 
    > (When a card accrues 3 strikes, future offers to save for that card
    > should show the omnibox save icon, but NOT pop up the bubble.
    > On Android, no offer to save is shown at all.)
    > 
    > Bug: 884817
    > Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    > Change-Id: I0d6ad27a4e03d0128970af6e22d2a40a62d0601b
    > Reviewed-on: https://chromium-review.googlesource.com/c/1269524
    > Commit-Queue: Jared Saul <jsaul@google.com>
    > Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    > Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#600490}
    
    Change-Id: I7d0536b919212228db19f87c6cc09358928ffd35
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 884817, 896493
    Cq-Include-Trybots: luci.chromium.try:ios-simulator-cronet;luci.chromium.try:ios-simulator-full-configs
    Reviewed-on: https://chromium-review.googlesource.com/c/1287252
    Cr-Commit-Position: refs/heads/master@{#600583}
    Former-commit-id: df2e4163068a0a1876d95b4ae29c1f79a5e5ce7b

--- chrome/browser/autofill/autofill_save_card_infobar_delegate_mobile_unittest.cc
+++ chrome/browser/autofill/autofill_save_card_infobar_delegate_mobile_unittest.cc
@@ -118,7 +118,6 @@ AutofillSaveCardInfoBarDelegateMobileTest::CreateDelegateWithLegalMessage(
     std::unique_ptr<ConfirmInfoBarDelegate> delegate(
         new AutofillSaveCardInfoBarDelegateMobile(
             is_uploading, credit_card, std::move(legal_message),
-            /*strike_database=*/nullptr,
             /*upload_save_card_callback=*/
             base::BindOnce(&AutofillSaveCardInfoBarDelegateMobileTest::
                                UploadSaveCardCallback,
@@ -131,7 +130,6 @@ AutofillSaveCardInfoBarDelegateMobileTest::CreateDelegateWithLegalMessage(
   std::unique_ptr<ConfirmInfoBarDelegate> delegate(
       new AutofillSaveCardInfoBarDelegateMobile(
           is_uploading, credit_card, std::move(legal_message),
-          /*strike_database=*/nullptr,
           /*upload_save_card_callback=*/
           base::OnceCallback<void(const base::string16&)>(),
           /*local_save_card_callback=*/
