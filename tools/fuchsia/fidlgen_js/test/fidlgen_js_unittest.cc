ab722f662e2d05ad4e7682dd48fcbfd52f6f76b1
Scott Graham <scottmg@chromium.org>
2018-10-17 01:57:27 +0000

    fuchsia: Add support for FIDL/JS responses
    Responses are always returned asynchronously as JS promises (the
    newly added zx.objectWaitOne() helper also returns a promise).
    
    The wait and response are serviced by libasync waits, and so must be
    cancelled when the v8 context is torn down. The (default) dispatcher
    must outlive any v8 contexts with which it is used.
    
    Bug: 883496
    Change-Id: Ib8735c6ec4196d36b53929341d57e843abaeea3b
    Reviewed-on: https://chromium-review.googlesource.com/c/1240204
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600238}
    Former-commit-id: d2f4ed4be95bf6d9fb5445a85baeba40ef51db5e

--- tools/fuchsia/fidlgen_js/test/fidlgen_js_unittest.cc
+++ tools/fuchsia/fidlgen_js/test/fidlgen_js_unittest.cc
@@ -2,11 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <lib/fidl/cpp/binding.h>
+#include <lib/fidl/cpp/internal/pending_response.h>
+#include <lib/fidl/cpp/internal/weak_stub_controller.h>
+
 #include "base/bind.h"
+#include "base/fuchsia/async_dispatcher.h"
 #include "base/stl_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/launcher/unit_test_launcher.h"
 #include "base/test/test_suite.h"
+#include "base/test/test_timeouts.h"
 #include "gin/converter.h"
 #include "gin/modules/console.h"
 #include "gin/object_template_builder.h"
@@ -64,15 +70,66 @@ TEST_F(FidlGenJsTest, BasicJSSetup) {
   EXPECT_EQ("HAI", result);
 }
 
+void LoadAndSource(gin::ShellRunner* runner, const base::FilePath& filename) {
+  std::string contents;
+  ASSERT_TRUE(base::ReadFileToString(filename, &contents));
+
+  runner->Run(contents, filename.MaybeAsASCII());
+}
+
+class BindingsSetupHelper {
+ public:
+  explicit BindingsSetupHelper(v8::Isolate* isolate)
+      : isolate_(isolate),
+        handle_scope_(isolate),
+        delegate_(),
+        runner_(&delegate_, isolate),
+        scope_(&runner_),
+        zx_bindings_(
+            std::make_unique<fidljs::ZxBindings>(isolate, runner_.global())) {
+    // TODO(scottmg): Figure out how to set up v8 import hooking and make
+    // fidl_Xyz into $fidl.Xyz. Manually inject the runtime support js files
+    // for now. https://crbug.com/883496.
+    LoadAndSource(&runner_, base::FilePath(kRuntimeFile));
+    LoadAndSource(&runner_, base::FilePath(kTestBindingFile));
+
+    zx_status_t status = zx::channel::create(0, &server_, &client_);
+    EXPECT_EQ(status, ZX_OK);
+
+    runner_.global()->Set(gin::StringToSymbol(isolate, "testHandle"),
+                          gin::ConvertToV8(isolate, client_.get()));
+  }
+
+  template <class T>
+  T Get(const std::string& name) {
+    T t;
+    EXPECT_TRUE(gin::Converter<T>::FromV8(
+        isolate_, runner_.global()->Get(gin::StringToV8(isolate_, name)), &t));
+    return t;
+  }
+
+  void DestroyBindingsForTesting() { zx_bindings_.reset(); }
+
+  zx::channel& server() { return server_; }
+  zx::channel& client() { return client_; }
+  gin::ShellRunner& runner() { return runner_; }
+
+ private:
+  v8::Isolate* isolate_;
+  v8::HandleScope handle_scope_;
+  FidlGenJsTestShellRunnerDelegate delegate_;
+  gin::ShellRunner runner_;
+  gin::Runner::Scope scope_;
+  std::unique_ptr<fidljs::ZxBindings> zx_bindings_;
+  zx::channel server_;
+  zx::channel client_;
+
+  DISALLOW_COPY_AND_ASSIGN(BindingsSetupHelper);
+};
+
 TEST_F(FidlGenJsTest, CreateChannelPair) {
   v8::Isolate* isolate = instance_->isolate();
-  v8::HandleScope handle_scope(isolate);
-
-  FidlGenJsTestShellRunnerDelegate delegate;
-  gin::ShellRunner runner(&delegate, isolate);
-  gin::Runner::Scope scope(&runner);
-
-  fidljs::InjectZxBindings(isolate, runner.global());
+  BindingsSetupHelper helper(isolate);
 
   std::string source = R"(
     var result = zx.channelCreate();
@@ -85,25 +142,16 @@ TEST_F(FidlGenJsTest, CreateChannelPair) {
     }
   )";
 
-  runner.Run(source, "test.js");
+  helper.runner().Run(source, "test.js");
 
-  zx_status_t status = ZX_ERR_INTERNAL;
-  EXPECT_TRUE(gin::Converter<zx_status_t>::FromV8(
-      isolate, runner.global()->Get(gin::StringToV8(isolate, "result_status")),
-      &status));
+  auto status = helper.Get<zx_status_t>("result_status");
   EXPECT_EQ(status, ZX_OK);
 
-  zx_handle_t handle = ZX_HANDLE_INVALID;
+  auto h1 = helper.Get<zx_handle_t>("result_h1");
+  EXPECT_NE(h1, ZX_HANDLE_INVALID);
 
-  EXPECT_TRUE(gin::Converter<zx_handle_t>::FromV8(
-      isolate, runner.global()->Get(gin::StringToV8(isolate, "result_h1")),
-      &handle));
-  EXPECT_NE(handle, ZX_HANDLE_INVALID);
-
-  EXPECT_TRUE(gin::Converter<zx_handle_t>::FromV8(
-      isolate, runner.global()->Get(gin::StringToV8(isolate, "result_h2")),
-      &handle));
-  EXPECT_NE(handle, ZX_HANDLE_INVALID);
+  auto h2 = helper.Get<zx_handle_t>("result_h2");
+  EXPECT_NE(h2, ZX_HANDLE_INVALID);
 }
 
 class TestolaImpl : public fidljstest::Testola {
@@ -130,6 +178,10 @@ class TestolaImpl : public fidljstest::Testola {
     various_stuff_ = stuff_as_vec;
   }
 
+  void WithResponse(int32_t a, int32_t b, WithResponseCallback sum) override {
+    sum(a + b);
+  }
+
   bool was_do_something_called() const { return was_do_something_called_; }
   int32_t received_int() const { return received_int_; }
   const std::string& received_msg() const { return received_msg_; }
@@ -147,48 +199,6 @@ class TestolaImpl : public fidljstest::Testola {
   std::vector<uint32_t> various_stuff_;
 
   DISALLOW_COPY_AND_ASSIGN(TestolaImpl);
-};
-
-void LoadAndSource(gin::ShellRunner* runner, const base::FilePath& filename) {
-  std::string contents;
-  ASSERT_TRUE(base::ReadFileToString(filename, &contents));
-
-  runner->Run(contents, filename.MaybeAsASCII());
-}
-
-class BindingsSetupHelper {
- public:
-  explicit BindingsSetupHelper(v8::Isolate* isolate)
-      : handle_scope_(isolate), delegate_(), runner_(&delegate_, isolate) {
-    gin::Runner::Scope scope(&runner_);
-
-    fidljs::InjectZxBindings(isolate, runner_.global());
-
-    // TODO(crbug.com/883496): Figure out how to set up v8 import hooking and
-    // make fidl_Xyz into $fidl.Xyz. Manually inject the runtime support js
-    // files for now.
-    LoadAndSource(&runner_, base::FilePath(kRuntimeFile));
-    LoadAndSource(&runner_, base::FilePath(kTestBindingFile));
-
-    zx_status_t status = zx::channel::create(0, &server_, &client_);
-    EXPECT_EQ(status, ZX_OK);
-
-    runner_.global()->Set(gin::StringToSymbol(isolate, "testHandle"),
-                          gin::ConvertToV8(isolate, client_.get()));
-  }
-
-  zx::channel& server() { return server_; }
-  zx::channel& client() { return client_; }
-  gin::ShellRunner& runner() { return runner_; }
-
- private:
-  v8::HandleScope handle_scope_;
-  FidlGenJsTestShellRunnerDelegate delegate_;
-  gin::ShellRunner runner_;
-  zx::channel server_;
-  zx::channel client_;
-
-  DISALLOW_COPY_AND_ASSIGN(BindingsSetupHelper);
 };
 
 TEST_F(FidlGenJsTest, RawReceiveFidlMessage) {
@@ -321,6 +331,83 @@ TEST_F(FidlGenJsTest, RawReceiveFidlMessageWithMultipleArgs) {
   EXPECT_EQ(testola_impl.various_stuff()[0], 999u);
   EXPECT_EQ(testola_impl.various_stuff()[1], 987u);
   EXPECT_EQ(testola_impl.various_stuff()[2], 123456u);
+}
+
+TEST_F(FidlGenJsTest, RawWithResponse) {
+  base::AsyncDispatcher dispatcher;
+
+  v8::Isolate* isolate = instance_->isolate();
+  BindingsSetupHelper helper(isolate);
+
+  TestolaImpl testola_impl;
+  fidl::Binding<fidljstest::Testola> binding(&testola_impl);
+  binding.Bind(std::move(helper.server()), &dispatcher);
+
+  // Send the data from the JS side into the channel.
+  std::string source = R"(
+      var proxy = new TestolaProxy();
+      proxy.$bind(testHandle);
+      this.sum_result = -1;
+      proxy.WithResponse(72, 99)
+           .then(sum => {
+              this.sum_result = sum;
+            })
+           .catch((e) => log('HOT GARBAGE: ' + e));
+    )";
+  helper.runner().Run(source, "test.js");
+
+  // Run the dispatcher until the response or timeout.
+  while (helper.Get<int>("sum_result") == -1) {
+    ASSERT_EQ(dispatcher.DispatchOrWaitUntil(zx_deadline_after(
+                  ZX_MSEC(TestTimeouts::action_timeout().InMilliseconds()))),
+              ZX_OK);
+  }
+
+  // Confirm that the response was received with the correct value.
+  auto sum_result = helper.Get<int>("sum_result");
+  EXPECT_EQ(sum_result, 72 + 99);
+}
+
+TEST_F(FidlGenJsTest, NoResponseBeforeTearDown) {
+  base::AsyncDispatcher dispatcher;
+
+  v8::Isolate* isolate = instance_->isolate();
+
+  BindingsSetupHelper helper(isolate);
+
+  TestolaImpl testola_impl;
+  fidl::Binding<fidljstest::Testola> binding(&testola_impl);
+  binding.Bind(std::move(helper.server()), &dispatcher);
+
+  // Send the data from the JS side into the channel.
+  std::string source = R"(
+      var proxy = new TestolaProxy();
+      proxy.$bind(testHandle);
+      this.resolved = false;
+      this.rejected = false;
+      this.excepted = false;
+      proxy.WithResponse(1, 2)
+           .then(sum => {
+              this.resolved = true;
+            }, () => {
+              this.rejected = true;
+            })
+           .catch((e) => {
+             log('HOT GARBAGE: ' + e);
+             this.excepted = true;
+           })
+    )";
+  helper.runner().Run(source, "test.js");
+
+  // Run the dispatcher, to read and queue the response.
+  EXPECT_EQ(dispatcher.DispatchOrWaitUntil(ZX_TIME_INFINITE), ZX_OK);
+
+  // This causes outstanding waits to be canceled.
+  helper.DestroyBindingsForTesting();
+
+  EXPECT_FALSE(helper.Get<bool>("resolved"));
+  EXPECT_TRUE(helper.Get<bool>("rejected"));
+  EXPECT_FALSE(helper.Get<bool>("excepted"));
 }
 
 int main(int argc, char** argv) {
