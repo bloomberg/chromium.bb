1dacb66073d6ca08db5917bbd77023442f1770f7
A Olsen <olsen@chromium.org>
2018-10-17 09:48:15 +0000

    Remove CrosSettings::Get()->Set from tests
    Get rid of CrosSettings::Get()->Set...() usage in tests.
    This CL changes all such calls to ScopedCrosSettingsTestHelper::Set.
    This call does the same thing as before, but it means there is only
    one call to CrosSettings->Set that will need to be changed in order
    to separate the read and write APIs.
    
    This is part of an ancient bug to make CrosSettings read-only -
    the equivalent API for writing is OwnerSettingsService.
    It will also be useful for moving CrosSettings out of chrome/browser
    without having to move all of OwnerSettingsService.
    
    Another minor clean up:
    There are two different ways to have a test setup CrosSettings -
    one way is to use ScopedCrosSettingsTestHelper, which sets up
    InstallAttributes, DeviceSettings, and CrosSettings.
    The other way to set them all up using individual helper classes,
    including ScopedDeviceSettingsTestHelper. What is not obvious, is that
    the first way sets up a real session manager, but the second way
    sets up a fake session manager. This CL also changes
    existing_user_controller_auto_login_unittest.cc to use the first way
    for consistency, and adds the call "SetFakeSessionManager()" which
    is more explicit, instead of having different helper classes set
    things up in subtly different ways.
    
    
    BUG=433840
    
    Change-Id: I0c84af93d446ce8f82d1aabbf070195abea24dd4
    Reviewed-on: https://chromium-review.googlesource.com/c/1256966
    Commit-Queue: A Olsen <olsen@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600331}
    Former-commit-id: 8fa44a345a7714630035df52f485bcc6b2dff323

--- chrome/browser/chromeos/login/login_browsertest.cc
+++ chrome/browser/chromeos/login/login_browsertest.cc
@@ -25,6 +25,7 @@
 #include "chrome/browser/chromeos/login/ui/login_display_host_webui.h"
 #include "chrome/browser/chromeos/login/wizard_controller.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
+#include "chrome/browser/chromeos/settings/scoped_cros_settings_test_helper.h"
 #include "chrome/browser/chromeos/settings/stub_install_attributes.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profiles_state.h"
@@ -181,6 +182,10 @@ class LoginTest : public LoginManagerTest {
     user_context.SetKey(Key(kPassword));
     SetExpectedCredentials(user_context);
   }
+
+ protected:
+  ScopedCrosSettingsTestHelper settings_helper_{
+      /* create_settings_service= */ false};
 };
 
 // Used to make sure that the system tray is visible and within the screen
@@ -259,7 +264,7 @@ IN_PROC_BROWSER_TEST_F(LoginSigninTest, WebUIVisible) {
 IN_PROC_BROWSER_TEST_F(LoginTest, PRE_GaiaAuthOffline) {
   RegisterUser(AccountId::FromUserEmailGaiaId(kTestUser, kGaiaId));
   StartupUtils::MarkOobeCompleted();
-  CrosSettings::Get()->SetBoolean(kAccountsPrefShowUserNamesOnSignIn, false);
+  settings_helper_.SetBoolean(kAccountsPrefShowUserNamesOnSignIn, false);
 }
 
 // Flaky, see http://crbug/692364.
