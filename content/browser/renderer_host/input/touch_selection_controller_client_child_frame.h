67bd7ffafb05e80c46cd0c4d21fe3792c09c1a8a
W. James MacLean <wjmaclean@chromium.org>
2018-10-17 16:19:01 +0000

    Fix TouchSelectionHandle updates on Android.
    This CL addresses the remaining issues with Android touch selection
    handles. First, it removes the manual scaling done by the child frame
    client for the device scale factor; apparently this is now handled by
    the point-transform functions. Second, it makes sure the child frame's
    handles update during pinch just like the main frame's handles do.
    
    Because a child frame's handles are not locked to a surface like the
    main frame's handles, there is a small amount of jitter/lag when
    pinching or scrolling the main-frame, but not enough to impair
    functionality.
    
    Note this CL also resolves the issues for desktop handles not updating
    after pinch as well.
    
    Bug: 809122
    Change-Id: I120386c3d7a0d87dacc655e83b07e0cfeaf4920a
    Reviewed-on: https://chromium-review.googlesource.com/c/1259945
    Commit-Queue: James MacLean <wjmaclean@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600412}
    Former-commit-id: 53a6e84ad2db5dcf1aa2bb1771698c0b2f29ce81

--- content/browser/renderer_host/input/touch_selection_controller_client_child_frame.h
+++ content/browser/renderer_host/input/touch_selection_controller_client_child_frame.h
@@ -37,6 +37,8 @@ class CONTENT_EXPORT TouchSelectionControllerClientChildFrame
       float device_scale_factor);
 
  private:
+  void TransformSelectionBoundsAndUpdate();
+
   // ui::TouchSelectionControllerClient:
   bool SupportsAnimation() const override;
   void SetNeedsAnimate() override;
@@ -60,11 +62,9 @@ class CONTENT_EXPORT TouchSelectionControllerClientChildFrame
   RenderWidgetHostViewChildFrame* rwhv_;
   TouchSelectionControllerClientManager* manager_;
 
-  // The last selection bounds reported by the view.
+  // The last selection bounds reported by the view, in view coordinates.
   gfx::SelectionBound selection_start_;
   gfx::SelectionBound selection_end_;
-  // Keep track of the view origin as of the last time selection was updated.
-  gfx::PointF view_origin_at_last_update_;
 
   DISALLOW_COPY_AND_ASSIGN(TouchSelectionControllerClientChildFrame);
 };
