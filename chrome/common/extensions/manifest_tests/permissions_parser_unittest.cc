04f3ca6648c2823ff1e79a6462d73e3acfa2f1ac
Kelvin Jiang <kelvinjiang@chromium.org>
2018-10-17 20:05:51 +0000

    [Extensions] Add an install warning for redundant optional host permissions
    Add an install warning for the case when an extension requests optional
    host permissions that are already covered by required host permissions.
    
    This is a follow-up to 1265118.
    
    Bug: 859959
    
    Change-Id: Idf639b89016ac72a4df5406d290baa177e83a0e4
    Reviewed-on: https://chromium-review.googlesource.com/c/1274054
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600530}
    Former-commit-id: 0300fe38f87c3dfd72c642daf5d9e56c3532e752

--- chrome/common/extensions/manifest_tests/permissions_parser_unittest.cc
+++ chrome/common/extensions/manifest_tests/permissions_parser_unittest.cc
@@ -15,7 +15,7 @@ using PermissionsParserTest = ChromeManifestTest;
 
 TEST_F(PermissionsParserTest, RemoveOverlappingAPIPermissions) {
   scoped_refptr<Extension> extension(LoadAndExpectWarning(
-      "permissions_parser_overlapping_permissions.json",
+      "permissions_overlapping_api_permissions.json",
       ErrorUtils::FormatErrorMessage(
           manifest_errors::kPermissionMarkedOptionalAndRequired, "tabs")));
 
@@ -32,6 +32,69 @@ TEST_F(PermissionsParserTest, RemoveOverlappingAPIPermissions) {
   EXPECT_THAT(required_api_names,
               testing::UnorderedElementsAre("tabs", "storage"));
   EXPECT_THAT(optional_api_names, testing::UnorderedElementsAre("geolocation"));
+}
+
+TEST_F(PermissionsParserTest, RemoveOverlappingHostPermissions) {
+  scoped_refptr<Extension> extension(LoadAndExpectWarning(
+      "permissions_overlapping_host_permissions.json",
+      ErrorUtils::FormatErrorMessage(
+          manifest_errors::kPermissionMarkedOptionalAndRequired,
+          "https://google.com/*")));
+
+  const URLPatternSet& required_hosts =
+      PermissionsParser::GetRequiredPermissions(extension.get())
+          .explicit_hosts();
+
+  const URLPatternSet& optional_hosts =
+      PermissionsParser::GetOptionalPermissions(extension.get())
+          .explicit_hosts();
+
+  // Check that required hosts have not changed at all while
+  // "https://google.com/maps" is removed from optional hosts as it is a strict
+  // subset of hosts specified as required.
+  EXPECT_THAT(*required_hosts.ToStringVector(),
+              testing::UnorderedElementsAre("https://example.com/*",
+                                            "https://*.google.com/*"));
+  EXPECT_THAT(*optional_hosts.ToStringVector(),
+              testing::UnorderedElementsAre("*://chromium.org/*"));
+}
+
+TEST_F(PermissionsParserTest, RequiredHostPermissionsAllURLs) {
+  scoped_refptr<Extension> extension(LoadAndExpectWarning(
+      "permissions_all_urls_host_permissions.json",
+      ErrorUtils::FormatErrorMessage(
+          manifest_errors::kPermissionMarkedOptionalAndRequired,
+          "http://*/*")));
+
+  const URLPatternSet& optional_hosts =
+      PermissionsParser::GetOptionalPermissions(extension.get())
+          .explicit_hosts();
+
+  // Since we specified <all_urls> as a required host permission,
+  // there should be no optional host permissions.
+  EXPECT_THAT(*optional_hosts.ToStringVector(), testing::IsEmpty());
+}
+
+TEST_F(PermissionsParserTest, OptionalHostPermissionsAllURLs) {
+  scoped_refptr<Extension> extension(
+      LoadAndExpectSuccess("permissions_optional_all_urls_permissions.json"));
+
+  const URLPatternSet& required_hosts =
+      PermissionsParser::GetRequiredPermissions(extension.get())
+          .explicit_hosts();
+
+  const URLPatternSet& optional_hosts =
+      PermissionsParser::GetOptionalPermissions(extension.get())
+          .explicit_hosts();
+
+  // This time, required permissions are a subset of optional permissions
+  // so we make sure that permissions remain the same
+  // as what's specified in the manifest.
+  EXPECT_THAT(*required_hosts.ToStringVector(),
+              testing::UnorderedElementsAre("https://*.google.com/*"));
+
+  EXPECT_THAT(*optional_hosts.ToStringVector(),
+              testing::UnorderedElementsAre("*://*/*"));
 }
 
 }  // namespace extensions
