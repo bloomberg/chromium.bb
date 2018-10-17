b9b0eac242693c292cd6ca23d09403f7a3bbe1ec
zork@chromium.org <zork@google.com>
2018-10-17 20:58:58 +0000

    Move strings for Safety information into OS_CHROMEOS block
    Bug: 894790
    Change-Id: I67d67ee8849e97195fe8e5bf82f83b8f809145a6
    Reviewed-on: https://chromium-review.googlesource.com/c/1281236
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Commit-Queue: Zachary Kuznia <zork@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#600041}(cherry picked from commit 74ad2ce2bf9b22cffb607ce774960950d341f5cd)
    Reviewed-on: https://chromium-review.googlesource.com/c/1287175
    Reviewed-by: Zachary Kuznia <zork@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3578@{#97}
    Cr-Branched-From: abbd18012032883c3113b3bfc5d59d25b4836c5f [formerly 4226ddf99103e493d7afb23a4c7902ee496108b6]-refs/heads/master@{#599034}
    Former-commit-id: b7c870954ca55b89a63823007adcef623f23f58d

--- chrome/browser/ui/webui/settings/about_handler.cc
+++ chrome/browser/ui/webui/settings/about_handler.cc
@@ -320,6 +320,11 @@ AboutHandler* AboutHandler::Create(content::WebUIDataSource* html_source,
   base::string16 tos = l10n_util::GetStringFUTF16(
       IDS_ABOUT_TERMS_OF_SERVICE, base::UTF8ToUTF16(chrome::kChromeUITermsURL));
   html_source->AddString("aboutProductTos", tos);
+#endif
+
+#if defined(OS_CHROMEOS)
+  html_source->AddBoolean("shouldShowSafetyInfo", ShouldShowSafetyInfo());
+#if defined(GOOGLE_CHROME_BUILD)
   html_source->AddString(
       "aboutProductSafety",
       l10n_util::GetStringUTF16(IDS_ABOUT_SAFETY_INFORMATION));
@@ -327,8 +332,6 @@ AboutHandler* AboutHandler::Create(content::WebUIDataSource* html_source,
                          base::UTF8ToUTF16(chrome::kChromeUISafetyURL));
 #endif
 
-#if defined(OS_CHROMEOS)
-  html_source->AddBoolean("shouldShowSafetyInfo", ShouldShowSafetyInfo());
   base::string16 os_license = l10n_util::GetStringFUTF16(
       IDS_ABOUT_CROS_VERSION_LICENSE,
       base::ASCIIToUTF16(chrome::kChromeUIOSCreditsURL));
