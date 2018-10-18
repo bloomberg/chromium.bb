0fd844d5990b80d54cf14f0770d87b212eec54f9
Becca Hughes <beccahughes@chromium.org>
2018-10-18 18:38:31 +0000

    [Media Controller] Forward play pause key to service
    If the media session feature is enabled then use the
    media session service to control playback when the
    play/pause media key is pressed.
    
    BUG=894255
    
    Change-Id: I5c34b24536456e4fcacabed6f4a8d2aa9f5d0ec5
    Reviewed-on: https://chromium-review.googlesource.com/c/1278147
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600839}
    Former-commit-id: 253f1bbf367b8e11db97bf9743a502b4a3ee1002

---
+++ services/media_session/public/cpp/test/test_media_controller.cc
@@ -0,0 +1,25 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/media_session/public/cpp/test/test_media_controller.h"
+
+namespace media_session {
+namespace test {
+
+TestMediaController::TestMediaController() = default;
+
+TestMediaController::~TestMediaController() = default;
+
+mojom::MediaControllerPtr TestMediaController::CreateMediaControllerPtr() {
+  mojom::MediaControllerPtr ptr;
+  binding_.Bind(mojo::MakeRequest(&ptr));
+  return ptr;
+}
+
+void TestMediaController::ToggleSuspendResume() {
+  ++toggle_suspend_resume_count_;
+}
+
+}  // namespace test
+}  // namespace media_session
