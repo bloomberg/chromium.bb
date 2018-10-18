9ccbc25e4f1a4dac2b083dc949ef70cc69d96f92
Alan Cutter <alancutter@chromium.org>
2018-10-18 02:55:05 +0000

    Use default browser theming for hosted app windows
    PWA windows are intended to appear stand alone from the main
    browser so they should not adopt the main browser's theme colors.
    This CL provides a default theme getter to be used by hosted app
    (and PWA) windows.
    
    This CL also removes the quick fix for this issue done by
    https://chromium-review.googlesource.com/c/chromium/src/+/1275466.
    
    Before: https://bugs.chromium.org/p/chromium/issues/attachment?aid=363145&signed_aid=TMzuMVlMKC2dm6ppdYp9PQ==&inline=1
    After: https://bugs.chromium.org/p/chromium/issues/attachment?aid=363146&signed_aid=Og46P66KeV7iP-mnBHO3cA==&inline=1
    
    Bug: 891560
    Change-Id: I2d94bff9f72bfca59fcf7d6cb940f42e4a25cc70
    Reviewed-on: https://chromium-review.googlesource.com/c/1272896
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600643}
    Former-commit-id: fa6292a67320ac8775549f3a7bf3df9d478ade60

--- chrome/browser/ui/views/frame/browser_frame.cc
+++ chrome/browser/ui/views/frame/browser_frame.cc
@@ -16,6 +16,7 @@
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window_state.h"
+#include "chrome/browser/ui/extensions/hosted_app_browser_controller.h"
 #include "chrome/browser/ui/views/frame/browser_non_client_frame_view.h"
 #include "chrome/browser/ui/views/frame/browser_root_view.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
@@ -169,8 +170,14 @@ bool BrowserFrame::GetAccelerator(int command_id,
 }
 
 const ui::ThemeProvider* BrowserFrame::GetThemeProvider() const {
-  return &ThemeService::GetThemeProviderForProfile(
-      browser_view_->browser()->profile());
+  Browser* browser = browser_view_->browser();
+  Profile* profile = browser->profile();
+  // Hosted apps are meant to appear stand alone from the main browser so they
+  // do not use the normal browser's configured theme.
+  using HostedAppController = extensions::HostedAppBrowserController;
+  return HostedAppController::IsForExperimentalHostedAppBrowser(browser)
+             ? &ThemeService::GetDefaultThemeProviderForProfile(profile)
+             : &ThemeService::GetThemeProviderForProfile(profile);
 }
 
 const ui::NativeTheme* BrowserFrame::GetNativeTheme() const {
