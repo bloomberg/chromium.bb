35254bcf141e1292622ca7b7284782fb730f9eef
Sergei Istomin <sistomin@chromium.org>
2018-10-18 20:33:19 +0000

    Fix failure of ExternalAudioPipelineLoopbackData unittest ran on devices
    StreamMixer can be configured to use audio post processor. The test
    did not set any post processing pipeline for StreamMixer. When the test ran
    on a device test failed because a device additionaly processed audio and test got
    audio data changed. The test succeeded only when it is run on host (testx86).
    The test was fixed to use fake post processor.
    
    Bug: 117792326
    Test: Unittests on device.
    Change-Id: Ife64df2690504bac249829f98ca659b19d763f23
    Reviewed-on: https://chromium-review.googlesource.com/c/1285124
    Commit-Queue: Sergei Istomin <sistomin@chromium.org>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600888}
    Former-commit-id: 493fe2e7304a72be939ecc2be0705d4bb5ea3756

--- chromecast/media/cma/backend/stream_mixer_external_audio_pipeline_unittest.cc
+++ chromecast/media/cma/backend/stream_mixer_external_audio_pipeline_unittest.cc
@@ -13,6 +13,7 @@
 #include "base/threading/thread_task_runner_handle.h"
 #include "chromecast/media/audio/fake_external_audio_pipeline_support.h"
 #include "chromecast/media/cma/backend/mock_mixer_source.h"
+#include "chromecast/media/cma/backend/mock_post_processor_factory.h"
 #include "chromecast/media/cma/backend/stream_mixer.h"
 #include "chromecast/public/media/external_audio_pipeline_shlib.h"
 #include "chromecast/public/media/mixer_output_stream.h"
@@ -161,6 +162,10 @@ TEST_F(ExternalAudioPipelineTest, SetMuteChangeRequest) {
 TEST_F(ExternalAudioPipelineTest, ExternalAudioPipelineLoopbackData) {
   // Set Volume to 1, because we'd like the input to be w/o changes.
   mixer_->SetVolume(AudioContentType::kMedia, 1);
+
+  // Add fake postprocessor to override test configuration running on device.
+  mixer_->ResetPostProcessorsForTest(
+      std::make_unique<MockPostProcessorFactory>(), "{}");
 
   // CastMediaShlib::LoopbackAudioObserver mock observer.
   MockLoopbackAudioObserver mock_loopback_observer;
