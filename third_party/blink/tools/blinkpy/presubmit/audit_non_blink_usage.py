23e6cd9683eedb6b7004194bc881a9f52a56bcba
Zhuoyu Qian <zhuoyu.qian@samsung.com>
2018-10-18 07:10:03 +0000

    Rename Encoding namepsace to encoding
    https://google.github.io/styleguide/cppguide.html#Namespace_Names
    > Namespace names are all lower-case.
    
    This CL has no behavior changes.
    
    Bug: 889726
    Change-Id: Ia60a35c3a34147d84d366fad26e6a75f4859b015
    Reviewed-on: https://chromium-review.googlesource.com/c/1286099
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Zhuoyu Qian <zhuoyu.qian@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#600677}
    Former-commit-id: d11a5d9df9f2e5dc04906268b66ece9b02200a0a

--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -217,6 +217,7 @@ _CONFIG = [
 
             # Nested namespaces under the blink namespace
             'cssvalue::.+',
+            'encoding::.+',
             'event_handling_util::.+',
             'scheduler::.+',
             'touch_action_util::.+',
