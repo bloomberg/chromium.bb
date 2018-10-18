c3d8d49e8badcf8a23a2eb9086468a280c131278
Nikita Podguzov <nikitapodguzov@chromium.org>
2018-10-18 12:13:43 +0000

    Disable editing managed network config fields.
    * Disable network configuration field if the field is enforced by policy.
    * Add policy network indicator if the field is controlled by policy.
    
    Bug: 877424
    Change-Id: Icfdb6d7fb3deb09696b6d2edce27d3c9851043a2
    Reviewed-on: https://chromium-review.googlesource.com/c/1251447
    Commit-Queue: Nikita Podguzov <nikitapodguzov@google.com>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600735}
    Former-commit-id: 43116fdd290fd13d324df39e20d3825f5851ca1a

--- ui/webui/resources/cr_elements/policy/cr_tooltip_icon.js
+++ ui/webui/resources/cr_elements/policy/cr_tooltip_icon.js
@@ -4,10 +4,19 @@
 
 Polymer({
   is: 'cr-tooltip-icon',
+
   properties: {
     iconAriaLabel: String,
+
     iconClass: String,
+
     tooltipText: String,
+
+    /** Position of tooltip popup related to the icon. */
+    tooltipPosition: {
+      type: String,
+      value: 'top',
+    }
   },
 
   /** @return {!Element} */
