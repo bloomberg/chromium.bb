ad51a83216ec21394e07572fabee1bfb5d7f00b6
Don Garrett <dgarrett@google.com>
2018-10-19 15:08:43 -0700

    chromeos_config: full builder to chromiumos-image-archive.
    Move the build artifacts for full builders to the world readable
    chromiumos-image-archive bucket.
    
    BUG=chromium:896323
    TEST=run_tests
    
    Change-Id: I04b46fae8b37d2c7bfb96e1b9e2338aaabfa85d1
    Reviewed-on: https://chromium-review.googlesource.com/1292670
    Commit-Ready: Don Garrett <dgarrett@chromium.org>
    Tested-by: Don Garrett <dgarrett@chromium.org>
    Reviewed-by: Jason Clinton <jclinton@chromium.org>

--- config/chromeos_config.py
+++ config/chromeos_config.py
@@ -2376,6 +2376,7 @@ def FullBuilders(site_config, boards_dict, ge_build_config):
           active_builders,
           active_waterfall=waterfall.WATERFALL_SWARMING,
           manifest_version=True,
+          gs_path='gs://chromiumos-image-archive/full',
       )
   )
 
