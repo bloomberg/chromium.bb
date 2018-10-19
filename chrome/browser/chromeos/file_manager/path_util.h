9b2944cbd2fc5f56f94b1ecc3432b26b3acf635c
Stuart Langley <slangley@google.com>
2018-10-19 02:56:38 +0000

    Add support for drive/Computers to PathUtil.
    This ensures that the correctly localized name is displayed in settings for the
    drive/Computers path.
    
    Bug: 884020
    Change-Id: Iaa68242aba57d0ac2228d6b90a6c627f5757875e
    Reviewed-on: https://chromium-review.googlesource.com/c/1290489
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Stuart Langley <slangley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601035}
    Former-commit-id: 5d44fb8f1bba0671a9ea4b02e8bd41fd2e0b63c8

--- chrome/browser/chromeos/file_manager/path_util.h
+++ chrome/browser/chromeos/file_manager/path_util.h
@@ -90,6 +90,7 @@ void ConvertToContentUrls(
 // * /home/chronos/u-<hash>/Downloads            => Downloads
 // * /special/drive-<hash>/root                  => Google Drive
 // * /special/drive-<hash>/team_drives           => Team Drives
+// * /special/drive-<hash>/Computers             => Computers
 // * /run/arc/sdcard/write/emulated/0            => Play files
 // * /media/fuse/crostini_<hash>_termina_penguin => Linux files
 // * '/' with ' \u203a ' (angled quote sign) for display purposes.
