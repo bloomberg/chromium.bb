fe8465f9d9bdfdaef17592c7a82988ed68de6d47
Thomas Tangl <tangltom@chromium.org>
2018-10-19 00:35:35 +0000

    Turn off signin-allowed toggle after sign-out from dialog
    When a sync user confirms to sign out after trying to turn
    off the signin-allowed toggle, the toggle is now turned off.
    
    Additional change:
     - The code related to the sign-out dialog in settings is
     extracted into its own element and refactored.
    
    Bug: 892745
    Change-Id: I9d638d8c77b1f4318fc592cc88f4be95e5eab7c8
    Reviewed-on: https://chromium-review.googlesource.com/c/1280542
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Thomas Tangl <tangltom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#600997}
    Former-commit-id: bca45527fcd1ef23db1a8e399faeddb62be6b1a3

--- chrome/browser/resources/settings/privacy_page/privacy_page.js
+++ chrome/browser/resources/settings/privacy_page/privacy_page.js
@@ -166,6 +166,9 @@ Polymer({
     /** @private */
     showRestart_: Boolean,
     // </if>
+
+    /** @private */
+    showSignoutDialog_: Boolean,
   },
 
   /** @override */
@@ -369,14 +372,27 @@ Polymer({
   /** @private */
   onSigninAllowedChange_: function() {
     if (this.syncStatus.signedIn && !this.$.signinAllowedToggle.checked) {
-      // Switch the toggle back on and route to people_page's disconnect dialog.
+      // Switch the toggle back on and show the signout dialog.
       this.$.signinAllowedToggle.checked = true;
-      settings.navigateTo(settings.routes.SIGN_OUT);
+      this.showSignoutDialog_ = true;
     } else {
       /** @type {!SettingsToggleButtonElement} */ (this.$.signinAllowedToggle)
           .sendPrefChange();
       this.showRestart_ = true;
     }
+  },
+
+  /** @private */
+  onSignoutDialogClosed_: function() {
+    if (/** @type {!SettingsSignoutDialogElement} */ (
+            this.$$('settings-signout-dialog'))
+            .wasConfirmed()) {
+      this.$.signinAllowedToggle.checked = false;
+      /** @type {!SettingsToggleButtonElement} */ (this.$.signinAllowedToggle)
+          .sendPrefChange();
+      this.showRestart_ = true;
+    }
+    this.showSignoutDialog_ = false;
   },
 
   /**
