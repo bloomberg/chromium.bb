896d3e30db80937ba60a16195f7da2352514d9cd
Vladimir Levin <vmpstr@chromium.org>
2018-10-19 17:51:36 +0000

    [DL] Add suspend/resume functionality to display locking context.
    This patch adds the ability to suspend and resume the context, which
    when suspended prevents future tasks from running.
    
    As of right now, the work isn't prevented from finishing since we don't
    have co-operative work implemented and nothing is preventing the rest
    of the system from processing the changes.
    
    Added tests, a few of which fail due to above reason.
    
    R=chrishtr@chromium.org
    
    Bug: 882633
    Change-Id: I256be53b3cd0071405c9ac783fe78fcd36dacb9c
    Reviewed-on: https://chromium-review.googlesource.com/c/1289432
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601219}
    Former-commit-id: adcc96ea829c58cfcc7ed05c3a79c10f4cfab7f8

--- third_party/blink/renderer/core/display_lock/display_lock_context.cc
+++ third_party/blink/renderer/core/display_lock/display_lock_context.cc
@@ -4,6 +4,7 @@
 
 #include "third_party/blink/renderer/core/display_lock/display_lock_context.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_display_lock_callback.h"
+#include "third_party/blink/renderer/core/display_lock/display_lock_suspended_handle.h"
 #include "third_party/blink/renderer/platform/bindings/microtask.h"
 
 namespace blink {
@@ -32,11 +33,16 @@ void DisplayLockContext::ContextDestroyed(ExecutionContext*) {
 }
 
 bool DisplayLockContext::HasPendingActivity() const {
-  // If we don't have a task scheduled, then we should already be resolved.
+  // If we don't have a task scheduled, then we should be suspended or already
+  // be resolved.
   // TODO(vmpstr): This should also be kept alive if we're doing co-operative
   // work.
-  DCHECK(process_queue_task_scheduled_ || !resolver_);
-  return process_queue_task_scheduled_;
+  DCHECK(suspended_count_ || process_queue_task_scheduled_ || !resolver_);
+  // Note that if we're suspended and we have no resolver, it means that we've
+  // resolved either due to execution context being destroyed, or the suspended
+  // handle was disposed without resuming. In either case, we shouldn't keep the
+  // context alive.
+  return process_queue_task_scheduled_ || (suspended_count_ && resolver_);
 }
 
 void DisplayLockContext::ScheduleTask(V8DisplayLockCallback* callback,
@@ -46,19 +52,16 @@ void DisplayLockContext::ScheduleTask(V8DisplayLockCallback* callback,
     DCHECK(script_state);
     resolver_ = ScriptPromiseResolver::Create(script_state);
   }
-  if (!process_queue_task_scheduled_) {
-    DCHECK(GetExecutionContext());
-    DCHECK(GetExecutionContext()->GetTaskRunner(TaskType::kMiscPlatformAPI));
-    GetExecutionContext()
-        ->GetTaskRunner(TaskType::kMiscPlatformAPI)
-        ->PostTask(FROM_HERE, WTF::Bind(&DisplayLockContext::ProcessQueue,
-                                        WrapWeakPersistent(this)));
-    process_queue_task_scheduled_ = true;
-  }
+  ScheduleTaskIfNeeded();
 }
 
 void DisplayLockContext::schedule(V8DisplayLockCallback* callback) {
   ScheduleTask(callback);
+}
+
+DisplayLockSuspendedHandle* DisplayLockContext::suspend() {
+  ++suspended_count_;
+  return new DisplayLockSuspendedHandle(this);
 }
 
 void DisplayLockContext::ProcessQueue() {
@@ -67,11 +70,14 @@ void DisplayLockContext::ProcessQueue() {
   // the work.
   process_queue_task_scheduled_ = false;
 
-  // We might have cleaned up already due to exeuction context being destroyed.
-  if (callbacks_.IsEmpty()) {
-    DCHECK(!resolver_);
+  // If we've become suspended, then abort. We'll schedule a new task when we
+  // resume.
+  if (suspended_count_)
     return;
-  }
+
+  // We might have cleaned up already due to exeuction context being destroyed.
+  if (!resolver_)
+    return;
 
   // Get a local copy of all the tasks we will run.
   // TODO(vmpstr): This should possibly be subject to a budget instead.
@@ -112,6 +118,39 @@ void DisplayLockContext::RejectAndCleanUp() {
     resolver_ = nullptr;
   }
   callbacks_.clear();
+}
+
+void DisplayLockContext::Resume() {
+  DCHECK_GT(suspended_count_, 0u);
+  --suspended_count_;
+  ScheduleTaskIfNeeded();
+}
+
+void DisplayLockContext::NotifyWillNotResume() {
+  DCHECK_GT(suspended_count_, 0u);
+  // The promise will never reject or resolve since we're now indefinitely
+  // suspended.
+  // TODO(vmpstr): We should probably issue a console warning.
+  resolver_->Detach();
+  resolver_ = nullptr;
+}
+
+void DisplayLockContext::ScheduleTaskIfNeeded() {
+  // We don't need a task in the following cases:
+  // - One is already scheduled
+  // - We're suspended, meaning we will schedule one when we resume.
+  // - We've already resolved this context (e.g. due to execution context being
+  //   destroyed).
+  if (process_queue_task_scheduled_ || suspended_count_ || !resolver_)
+    return;
+
+  DCHECK(GetExecutionContext());
+  DCHECK(GetExecutionContext()->GetTaskRunner(TaskType::kMiscPlatformAPI));
+  GetExecutionContext()
+      ->GetTaskRunner(TaskType::kMiscPlatformAPI)
+      ->PostTask(FROM_HERE, WTF::Bind(&DisplayLockContext::ProcessQueue,
+                                      WrapWeakPersistent(this)));
+  process_queue_task_scheduled_ = true;
 }
 
 }  // namespace blink
