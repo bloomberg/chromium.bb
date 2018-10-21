2496e3f501996fdf3a14b47a9f3ec57e7bbc93db
Mounir Lamouri <mlamouri@chromium.org>
2018-10-21 11:47:54 +0000

    Add UKM audibility timer in AudioContextManagerImpl
    This CL implements the audibility timer for multiple AudioContexts
    in AudioContextManagerImpl. The "started" event triggers the
    tracking and the "stopped" event triggers the calculation of
    elapsed time followed by UKM data record.
    
    Bug: 896944
    Change-Id: Id1f635df4783247aca3656d07c02af907ffd72e4
    Reviewed-on: https://chromium-review.googlesource.com/c/1289695
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601446}
    Former-commit-id: 2d95973e289887c5cf6082544adf72af309e230d

--- content/browser/media/webaudio/audio_context_manager_impl.h
+++ content/browser/media/webaudio/audio_context_manager_impl.h
@@ -17,7 +17,10 @@ class RenderFrameHostImpl;
 
 // Implements the mojo interface between WebAudio and the browser so that
 // WebAudio can report when audible sounds from an AudioContext starts and
-// stops.
+// stops. A manager instance can be associated with multiple AudioContexts.
+//
+// We do not expect to see more than 3~4 AudioContexts per render frame, so
+// handling multiple contexts would not be a significant bottle neck.
 class CONTENT_EXPORT AudioContextManagerImpl final
     : public content::FrameServiceBase<blink::mojom::AudioContextManager> {
  public:
@@ -29,12 +32,27 @@ class CONTENT_EXPORT AudioContextManagerImpl final
   static void Create(RenderFrameHost* render_frame_host,
                      blink::mojom::AudioContextManagerRequest request);
 
-  // Called when AudioContext starts or stops playing audible audio.
+  // Notify observers that audible audio started/stopped playing from an
+  // AudioContext.
   void AudioContextAudiblePlaybackStarted(int32_t audio_context_id) final;
   void AudioContextAudiblePlaybackStopped(int32_t audio_context_id) final;
 
+  void set_clock_for_testing(base::TickClock* clock) { clock_ = clock; }
+
  private:
+  // Send measured audible duration to UKM database.
+  void RecordAudibleTime(base::TimeDelta);
+
   RenderFrameHostImpl* const render_frame_host_impl_;
+
+  // To track pending audible time. Stores ID of AudioContext (int32_t) and
+  // the start time of audible period (base::TimeTicks).
+  base::flat_map<int32_t, base::TimeTicks> pending_audible_durations_;
+
+  // Clock used to calculate time between start and stop event. Can be override
+  // by tests.
+  // It is not owned by the implementation.
+  const base::TickClock* clock_;
 
   DISALLOW_COPY_AND_ASSIGN(AudioContextManagerImpl);
 };
