943fa5560402933ad72e86c9faa4989bded7f372
Mike Klein <mtklein@chromium.org>
2018-10-21 03:50:19 +0000

    rebaseline SK_LEGACY_COMPLEX_XFERMODES
    I've replaced two ref tests with image tests.  The diffs there are both the same root cause, basically moving from one approximation of a rounding divide by 255 to another, and some low bits move.
    
    I could have updated the ref tests with new numbers, but if you look at them, they're basically bitmaps already.  And we actually use different div255 approximations per platform, so I think it's a little more "right" to make these image tests.  (If we were running ARM layout test bots, we'd see slightly different results than these Intel ones.)
    
    Weeeeeellllllll.... that was my plan, but I ran into an issue where Macs would end up with a slightly different expected.txt file, and I don't really know what I'm doing with those, so I've switched it back to a ref test updated with the new colors.  :(
    
    Change-Id: I20cbfa886f71cb509d5e60e0d7cd40af5bca13ea
    Reviewed-on: https://chromium-review.googlesource.com/c/1291810
    Commit-Queue: Mike Klein <mtklein@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601414}
    Former-commit-id: 1275ad421bab07c3964480c6329ad9dd9cb5f669

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -154,8 +154,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 // Max. verb count for paths rendered by the edge-AA tessellating path renderer.
 #define GR_AA_TESSELLATOR_MAX_VERB_COUNT 100
 
-#define SK_LEGACY_COMPLEX_XFERMODES
-
 #ifndef SK_SUPPORT_LEGACY_AAA_CHOICE
 #define SK_SUPPORT_LEGACY_AAA_CHOICE
 #endif
