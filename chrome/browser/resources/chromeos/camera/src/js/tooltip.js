0a8f2720be194a2cbb37c7952e49a3d34f54c4ae
Yuli Huang <yuli@chromium.org>
2018-10-22 21:58:26 +0800

    Simplify Tooltip.
    BUG=None
    TEST=Tested by showing/hiding tooltip.
    
    Change-Id: I0f69fa2bdb7ec89ea0917932c701b5a01574b277
    Reviewed-on: https://chromium-review.googlesource.com/c/1292703
    Reviewed-by: Sheng-hao Tsao <shenghao@google.com>
    Tested-by: yuli <yuli@chromium.org>

--- chrome/browser/resources/chromeos/camera/src/js/tooltip.js
+++ chrome/browser/resources/chromeos/camera/src/js/tooltip.js
@@ -10,120 +10,29 @@
 var camera = camera || {};
 
 /**
- * Creates a tooltip controller for the entire document.
- * @constructor
+ * Namespace for tooltip.
  */
-camera.Tooltip = function() {
-  /**
-   * @type {camera.Tooltip.StyleEffect}
-   * @private
-   */
-  this.effect_ = new camera.Tooltip.StyleEffect((args, callback) => {
-    this.setVisibility_(args.element, args.visibility, callback);
-  });
-
-  // No more properties. Freeze the object.
-  Object.freeze(this);
-};
+camera.tooltip = camera.tooltip || {};
 
 /**
- * Wraps an effect in style implemented as either CSS3 animation or CSS3
- * transition. The passed closure should invoke the effect.
- * Only the last callback, passed to the latest invoke() call will be called
- * on the transition or the animation end.
- * @param {function(*, function())} closure Closure for invoking the effect.
- * TODO(yuli): Remove StyleEffect after simplifying Tooltip.
- * @constructor
+ * Sets up the tooltip.
  */
-camera.Tooltip.StyleEffect = function(closure) {
-  /**
-   * @type {function(*, function()}
-   * @private
-   */
-  this.closure_ = closure;
-
-  /**
-   * Callback to be called for the latest invokation.
-   * @type {?function()}
-   * @private
-   */
-  this.callback_ = null;
-
-  /**
-   * @type {?number{
-   * @private
-   */
-  this.invocationTimer_ = null;
-
-  // End of properties. Seal the object.
-  Object.seal(this);
-};
-
-/**
- * Invokes the animation and calls the callback on completion. Note, that
- * the callback will not be called if there is another invocation called after.
- * @param {*} state State of the effect to be set
- * @param {function()} callback Completion callback.
- * @param {number=} delay Timeout in milliseconds before invoking.
- */
-camera.Tooltip.StyleEffect.prototype.invoke = function(state, callback, delay) {
-  if (this.invocationTimer_) {
-    clearTimeout(this.invocationTimer_);
-    this.invocationTimer_ = null;
-  }
-
-  var invokeClosure = function() {
-    this.callback_ = callback;
-    this.closure_(state, function() {
-      if (!this.callback_)
-        return;
-      var callback = this.callback_;
-      this.callback_ = null;
-
-      // Let the animation neatly finish.
-      setTimeout(callback, 0);
-    }.bind(this));
-  }.bind(this);
-
-  if (delay !== undefined)
-    this.invocationTimer_ = setTimeout(invokeClosure, delay);
-  else
-    invokeClosure();
-};
-
-/**
- * Initializes the tooltip.
- */
-camera.Tooltip.initialize = function() {
+camera.tooltip.setup = function() {
   // Add tooltip handlers to every element with the i18n-label attribute.
-  var instance = new camera.Tooltip();
-  var selectors = document.querySelectorAll('*[i18n-label]');
-  for (var index = 0; index < selectors.length; index++) {
-    var element = selectors[index];
-    var handler = instance.show_.bind(instance, element);
+  document.querySelectorAll('*[i18n-label]').forEach((element) => {
+    var handler = camera.tooltip.show_.bind(undefined, element);
     element.addEventListener('mouseover', handler);
     element.addEventListener('focus', handler);
-  }
+  });
 };
 
 /**
- * Positions the tooltip on the screen and toggles its visibility.
- * @param {HTMLElement} element Element to be the tooltip positioned to.
- * @param {boolean} visible True for visible, false for hidden.
- * @param {function()} callback Completion callback for changing visibility.
+ * Positions the tooltip by the element.
+ * @param {HTMLElement} element Element for tooltip to be positioned to.
  * @private
  */
-camera.Tooltip.prototype.setVisibility_ = function(
-    element, visible, callback) {
-  // Hide the tooltip.
+camera.tooltip.position_ = function(element) {
   var tooltip = document.querySelector('#tooltip');
-  if (!visible) {
-    tooltip.classList.remove('visible');
-    callback();  // No animation, finish immediately.
-    return;
-  }
-
-  // Show the tooltip.
   const [edgeMargin, elementMargin] = [5, 8];
   var rect = element.getBoundingClientRect();
   var tooltipTop = rect.top - tooltip.offsetHeight - elementMargin;
@@ -138,27 +47,17 @@ camera.Tooltip.prototype.setVisibility_ = function(
       Math.max(elementCenter - tooltip.clientWidth / 2, edgeMargin),
       document.body.offsetWidth - tooltip.offsetWidth - edgeMargin);
   tooltip.style.left = Math.round(left) + 'px';
-
-  // Show the tooltip element.
-  tooltip.classList.add('visible');
-  camera.util.waitAnimationCompleted(tooltip, 1000, callback);
 };
 
 /**
  * Shows a tooltip over the element.
- * @param {HTMLElement} element Element to be shown.
+ * @param {HTMLElement} element Element whose tooltip to be shown.
  * @private
  */
-camera.Tooltip.prototype.show_ = function(element) {
-  this.effect_.invoke(false, () => {});
-  document.querySelector('#tooltip').textContent = chrome.i18n.getMessage(
-      element.getAttribute('i18n-label'));
-
+camera.tooltip.show_ = function(element) {
+  var tooltip = document.querySelector('#tooltip');
   var hide = () => {
-    this.effect_.invoke({
-      element: element,
-      visibility: false
-    }, () => {});
+    tooltip.classList.remove('visible');
     element.removeEventListener('mouseout', hide);
     element.removeEventListener('click', hide);
     element.removeEventListener('blur', hide);
@@ -167,9 +66,10 @@ camera.Tooltip.prototype.show_ = function(element) {
   element.addEventListener('click', hide);
   element.addEventListener('blur', hide);
 
-  // Show the tooltip after delay.
-  this.effect_.invoke({
-    element: element,
-    visibility: true
-  }, () => {}, 1500);
+  tooltip.classList.remove('visible');
+  tooltip.offsetWidth; // Force calculation to hide tooltip if shown.
+  tooltip.textContent = chrome.i18n.getMessage(
+      element.getAttribute('i18n-label'));
+  camera.tooltip.position_(element);
+  tooltip.classList.add('visible');
 };
