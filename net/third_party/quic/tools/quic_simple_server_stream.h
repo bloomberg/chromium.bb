0edf8939a995b36e5e74b55c70ec01c03d73d82d
Victor Vasiliev <vasilvv@chromium.org>
2018-10-22 23:42:26 +0000

    Add missing copyright headers for QUIC/HTTP2/SPDY
    I am not sure why those were not caught by the presubmit, but I suspect
    that it's due to code being in net/third_party.
    
    R=rch@chromium.org
    
    Change-Id: I525e777487d7e376aaa40d956305e5d1a8374bf3
    Reviewed-on: https://chromium-review.googlesource.com/c/1294869
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Victor Vasiliev <vasilvv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601782}
    Former-commit-id: cc040ea36c790dfa3d11b9fce9d3886c714196ff

--- net/third_party/quic/tools/quic_simple_server_stream.h
+++ net/third_party/quic/tools/quic_simple_server_stream.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// // Use of this source code is governed by a BSD-style license that can be
-// // found in the LICENSE file.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #ifndef NET_THIRD_PARTY_QUIC_TOOLS_QUIC_SIMPLE_SERVER_STREAM_H_
 #define NET_THIRD_PARTY_QUIC_TOOLS_QUIC_SIMPLE_SERVER_STREAM_H_
