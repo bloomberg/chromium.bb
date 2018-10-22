5a1898590499c4bce05a1c75f021c7391853d061
Victor Costan <pwnall@chromium.org>
2018-10-22 14:24:44 +0000

    Blobs: Qualify base::Bind calls and remove deprecated mojo call.
    Change-Id: Ie63379b3002521a1badac056809edbf8b8d40b71
    Reviewed-on: https://chromium-review.googlesource.com/c/1292956
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601564}
    Former-commit-id: e84f42659dab8e4b38b38a4f33ae359494689fda

--- third_party/blink/renderer/platform/blob/blob_bytes_provider.cc
+++ third_party/blink/renderer/platform/blob/blob_bytes_provider.cc
@@ -32,11 +32,12 @@ class BlobBytesStreamer {
                  mojo::SimpleWatcher::ArmingPolicy::AUTOMATIC,
                  std::move(task_runner)) {
     watcher_.Watch(pipe_.get(), MOJO_HANDLE_SIGNAL_WRITABLE,
+                   MOJO_WATCH_CONDITION_SATISFIED,
                    WTF::BindRepeating(&BlobBytesStreamer::OnWritable,
                                       WTF::Unretained(this)));
   }
 
-  void OnWritable(MojoResult result) {
+  void OnWritable(MojoResult result, const mojo::HandleSignalsState& state) {
     if (result == MOJO_RESULT_CANCELLED ||
         result == MOJO_RESULT_FAILED_PRECONDITION) {
       delete this;
