70190ce48400fefc7f9b1b94de9a1446c88a8360
Matt Menke <mmenke@chromium.org>
2018-10-22 23:26:33 +0000

    HttpStreamParser: Reject headers with nulls in them.
    While the HTTP spec further limits what values are legal, nulls are
    particularly concerning, and it's safest just to reject them. See
    discussion here: https://github.com/whatwg/xhr/issues/165
    
    Chrome will be the first browser to reject nulls in responses, despite
    there being wpt tests for this, so we'll have to keep an eye out for
    breakages.
    
    For reference, 0x00 through 0x1F aren't allowed in header values or
    fields, (https://tools.ietf.org/html/rfc7230#section-3.2 - VCHAR
    excludes those characters).  CRs and LFs are of course needed, and
    0x0C and 0x0B are allowed by other specs for particular
    header parsers, strangely.
    
    This CL does not affect other code that can generate HTTP response
    headers, which still uses the old behavior of just removing nulls.
    ServiceWorkers, extensions, WebPackages, Dial (?), and various tests
    still inherit the old behavior, since they create headers directly
    with a method that can't fail.  It does introduce a new helper method,
    however, that they should eventually be switched to use:
    HttpResponseHeaders::TryToCreate().  We should probably put off
    conversion until this successfully makes it to stable.
    
    Bug: 832086
    Change-Id: Ib75ac03a6a298238cafb41eaa5f046c082fd0bdf
    Reviewed-on: https://chromium-review.googlesource.com/c/1291812
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601776}
    Former-commit-id: 900336ff53907fb0263941689ee734103f0ad352

--- net/http/http_response_headers.cc
+++ net/http/http_response_headers.cc
@@ -10,6 +10,7 @@
 #include "net/http/http_response_headers.h"
 
 #include <algorithm>
+#include <limits>
 #include <memory>
 #include <unordered_map>
 #include <utility>
@@ -116,11 +117,19 @@ bool ShouldUpdateHeader(base::StringPiece name) {
   return true;
 }
 
-void CheckDoesNotHaveEmbededNulls(const std::string& str) {
+bool HasEmbeddedNulls(base::StringPiece str) {
+  for (char c : str) {
+    if (c == '\0')
+      return true;
+  }
+  return false;
+}
+
+void CheckDoesNotHaveEmbeddedNulls(base::StringPiece str) {
   // Care needs to be taken when adding values to the raw headers string to
   // make sure it does not contain embeded NULLs. Any embeded '\0' may be
   // understood as line terminators and change how header lines get tokenized.
-  CHECK(str.find('\0') == std::string::npos);
+  CHECK(!HasEmbeddedNulls(str));
 }
 
 }  // namespace
@@ -168,6 +177,18 @@ HttpResponseHeaders::HttpResponseHeaders(base::PickleIterator* iter)
   std::string raw_input;
   if (iter->ReadString(&raw_input))
     Parse(raw_input);
+}
+
+scoped_refptr<HttpResponseHeaders> HttpResponseHeaders::TryToCreate(
+    base::StringPiece headers) {
+  // Reject strings with nulls.
+  if (HasEmbeddedNulls(headers) ||
+      headers.size() > std::numeric_limits<int>::max()) {
+    return nullptr;
+  }
+
+  return base::MakeRefCounted<HttpResponseHeaders>(
+      HttpUtil::AssembleRawHeaders(headers.data(), headers.size()));
 }
 
 void HttpResponseHeaders::Persist(base::Pickle* pickle,
@@ -357,7 +378,7 @@ void HttpResponseHeaders::RemoveHeaderLine(const std::string& name,
 }
 
 void HttpResponseHeaders::AddHeader(const std::string& header) {
-  CheckDoesNotHaveEmbededNulls(header);
+  CheckDoesNotHaveEmbeddedNulls(header);
   DCHECK_EQ('\0', raw_headers_[raw_headers_.size() - 2]);
   DCHECK_EQ('\0', raw_headers_[raw_headers_.size() - 1]);
   // Don't copy the last null.
@@ -377,7 +398,7 @@ void HttpResponseHeaders::AddCookie(const std::string& cookie_string) {
 }
 
 void HttpResponseHeaders::ReplaceStatusLine(const std::string& new_status) {
-  CheckDoesNotHaveEmbededNulls(new_status);
+  CheckDoesNotHaveEmbeddedNulls(new_status);
   // Copy up to the null byte.  This just copies the status line.
   std::string new_raw_headers(new_status);
   new_raw_headers.push_back('\0');
