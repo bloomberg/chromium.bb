aca7f40bd7272ed2702058a7b01c3132f3fac471
Daniele Castagna <dcastagna@chromium.org>
2018-10-22 20:37:59 +0000

    viz: Remove unnecessary fb != 0 check in bufferqueue
    In BufferQueue::CopyBufferDamage DCHECK that a framebuffer is generated
    instead of ignoring that generation might fail and then avoid deleting it.
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel
    Change-Id: I75dcf48a444a613df0a653d3bc5d420afa416430
    Reviewed-on: https://chromium-review.googlesource.com/c/1293796
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Commit-Queue: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601714}
    Former-commit-id: 5b656a0cf5e6b93eda15b5307cca28644d77950c

--- components/viz/service/display_embedder/buffer_queue.cc
+++ components/viz/service/display_embedder/buffer_queue.cc
@@ -75,6 +75,7 @@ void BufferQueue::CopyBufferDamage(int texture,
 
   GLuint dst_framebuffer = 0;
   gl_->GenFramebuffers(1, &dst_framebuffer);
+  DCHECK(dst_framebuffer);
   gl_->BindFramebuffer(GL_FRAMEBUFFER, dst_framebuffer);
   gl_->BindTexture(texture_target_, texture);
   gl_->FramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
@@ -86,8 +87,7 @@ void BufferQueue::CopyBufferDamage(int texture,
   }
   gl_->BindTexture(texture_target_, 0);
   gl_->Flush();
-  if (dst_framebuffer != 0)
-    gl_->DeleteFramebuffers(1, &dst_framebuffer);
+  gl_->DeleteFramebuffers(1, &dst_framebuffer);
 }
 
 void BufferQueue::UpdateBufferDamage(const gfx::Rect& damage) {
