e469146ff18d19215ccf0a0a0e54ffa2e3b31a5e
Scott Violet <sky@chromium.org>
2018-10-23 23:23:39 +0000

    chromeos: makes InputMethodMus notify observers when caret bounds change
    Which is used in some tests.
    
    BUG=889101
    TEST=covered by tests
    
    Change-Id: I74e191dd1bb7576d382e57b10f119bb4f6d4457b
    Reviewed-on: https://chromium-review.googlesource.com/c/1297404
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602171}
    Former-commit-id: 888842918959fc9f3e4028fd9c43702711fd9298

--- ui/aura/mus/input_method_mus.cc
+++ ui/aura/mus/input_method_mus.cc
@@ -112,6 +112,8 @@ void InputMethodMus::OnCaretBoundsChanged(const ui::TextInputClient* client) {
 
   if (input_method_)
     input_method_->OnCaretBoundsChanged(client->GetCaretBounds());
+
+  NotifyTextInputCaretBoundsChanged(client);
 }
 
 void InputMethodMus::CancelComposition(const ui::TextInputClient* client) {
