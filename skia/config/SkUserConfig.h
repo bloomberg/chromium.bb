6f555d5b32cb439b22c909ab65aa76320644968b
Brian Osman <brianosman@google.com>
2018-10-23 12:26:56 +0000

    Switch to new lazy image readPixels behavior
    Change-Id: I1b1d883dfae7611459547b308f0e42f291c36679
    Reviewed-on: https://chromium-review.googlesource.com/c/1293790
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601910}
    Former-commit-id: 8828ebfb0104db8c4be340b8b032e48ae7b2667a

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -158,8 +158,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_AAA_CHOICE
 #endif
 
-#define SK_USE_LEGACY_LAZY_IMAGE_DECODE
-
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
