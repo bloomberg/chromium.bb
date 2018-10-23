8977303f69ff1ee1b12e947ef087180060985287
Alexandre Frechette <frechette@chromium.org>
2018-10-23 16:02:28 +0000

    [Translate Manual Trigger] Added logic to check for manual translation availability and to (manually) initiate a translation.
    Change-Id: I2fdd9832a72fa1700190d4f7ee93365f29579b32
    Bug: 883456
    Reviewed-on: https://chromium-review.googlesource.com/c/1281086
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Michael Martis <martis@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Alexandre Frechette <frechette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#601970}
    Former-commit-id: 703b0c073a87b387efb0e7f095a6cbf1a2f56adf

--- chrome/browser/translate/android/translate_bridge.cc
+++ chrome/browser/translate/android/translate_bridge.cc
@@ -3,12 +3,37 @@
 // found in the LICENSE file.
 
 #include "base/android/jni_string.h"
+#include "chrome/browser/translate/chrome_translate_client.h"
+#include "components/translate/core/browser/translate_manager.h"
+#include "content/public/browser/web_contents.h"
 #include "jni/TranslateBridge_jni.h"
 
 static void JNI_TranslateBridge_Translate(
     JNIEnv* env,
     const base::android::JavaParamRef<jclass>& jcaller,
-    const base::android::JavaParamRef<jobject>& j_web_contents,
-    const base::android::JavaParamRef<jstring>& j_url) {
-  // TODO(crbug.com/883456): Implement translate behavior.
+    const base::android::JavaParamRef<jobject>& j_web_contents) {
+  content::WebContents* web_contents =
+      content::WebContents::FromJavaWebContents(j_web_contents);
+  ChromeTranslateClient* chrome_translate_client =
+      ChromeTranslateClient::FromWebContents(web_contents);
+  DCHECK(chrome_translate_client);
+  translate::TranslateManager* manager =
+      chrome_translate_client->GetTranslateManager();
+  DCHECK(manager);
+  manager->InitiateManualTranslation();
+}
+
+static jboolean JNI_TranslateBridge_CanManuallyTranslate(
+    JNIEnv* env,
+    const base::android::JavaParamRef<jclass>& jcaller,
+    const base::android::JavaParamRef<jobject>& j_web_contents) {
+  content::WebContents* web_contents =
+      content::WebContents::FromJavaWebContents(j_web_contents);
+  ChromeTranslateClient* chrome_translate_client =
+      ChromeTranslateClient::FromWebContents(web_contents);
+  DCHECK(chrome_translate_client);
+  translate::TranslateManager* manager =
+      chrome_translate_client->GetTranslateManager();
+  DCHECK(manager);
+  return manager->CanManuallyTranslate();
 }
