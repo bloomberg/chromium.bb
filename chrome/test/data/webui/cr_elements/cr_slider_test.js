af5e3ec87e9e801af6f156df5d756dc6d1e6b5f2
Esmael El-Moslimany <aee@chromium.org>
2018-10-23 22:25:48 +0000

    WebUI: cr-slider, keep public |disabled| unchanged
    When |ticks.length == 1| or the public |disabled| property is true, the
    slider is effectively disabled.
    
    Bug: 895831
    Change-Id: Iea13afaec1b784a433fd9f7fa8fe9d7a98a531b9
    Reviewed-on: https://chromium-review.googlesource.com/c/1284090
    Commit-Queue: Esmael El-Moslimany <aee@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602135}
    Former-commit-id: 2d7b90a6780a9e78c85adfd2320a7ceb34d758ec

--- chrome/test/data/webui/cr_elements/cr_slider_test.js
+++ chrome/test/data/webui/cr_elements/cr_slider_test.js
@@ -12,6 +12,13 @@ suite('cr-slider', function() {
     crSlider = document.body.querySelector('cr-slider');
   });
 
+  /** @param {boolean} expected */
+  function checkDisabled(expected) {
+    assertEquals(
+        expected,
+        window.getComputedStyle(crSlider)['pointer-events'] == 'none');
+  }
+
   function pressArrowRight() {
     MockInteractions.pressAndReleaseKeyOn(crSlider, 39, [], 'ArrowRight');
   }
@@ -219,5 +226,61 @@ suite('cr-slider', function() {
     assertEquals('Second', crSlider.getAttribute('aria-valuetext'));
     assertEquals('20', crSlider.getAttribute('aria-valuenow'));
     assertEquals('Second', crSlider.$.label.innerHTML.trim());
+  });
+
+  test('disabled whenever public |disabled| is true', () => {
+    crSlider.disabled = true;
+    crSlider.ticks = [];
+    checkDisabled(true);
+    crSlider.ticks = [1];
+    checkDisabled(true);
+    crSlider.ticks = [1, 2, 3];
+    checkDisabled(true);
+  });
+
+  test('not disabled or snaps when |ticks| is empty', () => {
+    assertFalse(crSlider.disabled);
+    crSlider.ticks = [];
+    checkDisabled(false);
+    assertFalse(crSlider.snaps);
+    assertEquals(0, crSlider.min);
+    assertEquals(100, crSlider.max);
+  });
+
+  test('effectively disabled when only one tick', () => {
+    assertFalse(crSlider.disabled);
+    crSlider.ticks = [1];
+    checkDisabled(true);
+    assertFalse(crSlider.snaps);
+    assertEquals(0, crSlider.min);
+    assertEquals(100, crSlider.max);
+  });
+
+  test('not disabled and |snaps| true when |ticks.length| > 0', () => {
+    assertFalse(crSlider.disabled);
+    crSlider.ticks = [1, 2, 3];
+    checkDisabled(false);
+    assertTrue(crSlider.snaps);
+    assertEquals(0, crSlider.min);
+    assertEquals(2, crSlider.max);
+  });
+
+  test('disabled, max, min and snaps update when ticks is mutated', () => {
+    assertFalse(crSlider.disabled);
+    checkDisabled(false);
+
+    // Single tick is effectively disabled.
+    crSlider.push('ticks', 1);
+    checkDisabled(true);
+    assertFalse(crSlider.snaps);
+    assertEquals(0, crSlider.min);
+    assertEquals(100, crSlider.max);
+
+    // Multiple ticks is enabled.
+    crSlider.push('ticks', 2);
+    checkDisabled(false);
+    assertTrue(crSlider.snaps);
+    assertEquals(0, crSlider.min);
+    assertEquals(1, crSlider.max);
   });
 });
