06e5ecbf287976f5459e6d34272e94be8d459120
Kenneth Russell <kbr@chromium.org>
2018-10-24 20:56:00 +0000

    Implement generate_test_filter_args in GpuIntegrationTestAdapater.
    Functionality was lost during a recent refactor. Tested manually.
    
    Tbr: nednguyen@google.com
    Bug: 898528
    Change-Id: Ia850e2c74e2dc353e8c18fbca8b90d3efef21908
    Reviewed-on: https://chromium-review.googlesource.com/c/1298314
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602446}
    Former-commit-id: 72dd53f4e18900f94c849900a88ed3eb459d9996

--- testing/scripts/run_gpu_integration_test_as_googletest.py
+++ testing/scripts/run_gpu_integration_test_as_googletest.py
@@ -41,6 +41,17 @@ class GpuIntegrationTestAdapater(common.BaseIsolatedScriptArgsAdapter):
   def generate_test_output_args(self, output):
     return ['--write-full-results-to', output]
 
+  def generate_test_filter_args(self, test_filter_str):
+    filter_list = common.extract_filter_list(test_filter_str)
+    # isolated_script_test_filter comes in like:
+    #   gpu_tests.webgl_conformance_integration_test.WebGLConformanceIntegrationTest.WebglExtension_WEBGL_depth_texture  # pylint: disable=line-too-long
+    # but we need to pass it to --test-filter like this:
+    #   WebglExtension_WEBGL_depth_texture
+    filter_list = [f.split('.')[-1] for f in filter_list]
+    # Need to convert this to a valid regex.
+    filter_regex = '(' + '|'.join(filter_list) + ')'
+    return ['--test-filter=%s' % filter_regex]
+
   def generate_sharding_args(self, total_shards, shard_index):
     return ['--total-shards=%d' % total_shards,
             '--shard-index=%d' % shard_index]
