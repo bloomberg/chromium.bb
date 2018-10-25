fd646e26f815b6052fba62bbd47331597fe4c31e
Sadrul Habib Chowdhury <sadrul@chromium.org>
2018-10-25 18:00:15 +0000

    Reland "viz: Throttle begin-frames when gpu is busy."
    This reverts commit 9e98a0c61de5f8bc3be1fe68d185c481af57d3ef [formerly 3629d825012de4e1ce8cad1a2be9b654c7b01b6a].
    
    The fix for the revert was to mark the gpu as not-busy during the
    destruction of DisplayScheduler.
    
    Original change's description
    =============================
    Throttle begin-frames to clients until gpu is done processing the
    earlier swaps. This helps with reducing work when the gpu has a high
    load, by not sending begin-frames to clients.
    
    The clients always keep receiving the compositor-frame-ack messages
    when a surface becomes activated (or replaces an older pending frame),
    even if the gpu is busy doing work from earlier frames. So this change
    introduces the back pressure by way of withholding begin-frames so
    clients have to do less work.
    
    BUG=877906, 898127
    
    Change-Id: I3a25cc96ac3f037fc275b12bc956c9875e6fe58e
    Reviewed-on: https://chromium-review.googlesource.com/c/1299206
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602788}
    Former-commit-id: 88023ceeb64cea0946fdcc3018ea28d9330a9976

--- components/viz/service/display/display_scheduler.cc
+++ components/viz/service/display/display_scheduler.cc
@@ -48,6 +48,9 @@ DisplayScheduler::DisplayScheduler(BeginFrameSource* begin_frame_source,
 }
 
 DisplayScheduler::~DisplayScheduler() {
+  // It is possible for DisplayScheduler to be destroyed while there's an
+  // in-flight swap. So always mark the gpu as not busy during destruction.
+  begin_frame_source_->SetIsGpuBusy(false);
   StopObservingBeginFrames();
 }
 
@@ -504,18 +507,22 @@ void DisplayScheduler::OnBeginFrameDeadline() {
 void DisplayScheduler::DidFinishFrame(bool did_draw) {
   DCHECK(begin_frame_source_);
   begin_frame_source_->DidFinishFrame(this);
-
   BeginFrameAck ack(current_begin_frame_args_, did_draw);
   client_->DidFinishFrame(ack);
 }
 
 void DisplayScheduler::DidSwapBuffers() {
   pending_swaps_++;
+  if (pending_swaps_ == max_pending_swaps_)
+    begin_frame_source_->SetIsGpuBusy(true);
+
   uint32_t swap_id = next_swap_id_++;
   TRACE_EVENT_ASYNC_BEGIN0("viz", "DisplayScheduler:pending_swaps", swap_id);
 }
 
 void DisplayScheduler::DidReceiveSwapBuffersAck() {
+  begin_frame_source_->SetIsGpuBusy(false);
+
   uint32_t swap_id = next_swap_id_ - pending_swaps_;
   pending_swaps_--;
   TRACE_EVENT_ASYNC_END0("viz", "DisplayScheduler:pending_swaps", swap_id);
