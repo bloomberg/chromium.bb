76cb2a8ced233718e29fdbcc7711ddf4bc35c5a7
Antonio Gomes <tonikitoo@igalia.com>
2018-10-25 19:31:46 +0000

    [s13n] Convert UserCloudPolicyTokenForwarder away from OAuth2TokenService::Consumer
    This is a 3/n CL that migrates UserCloudPolicyTokenForwarder to
    IdentityManager. Previous two CLs were [1] and [2].
    
    In a follow up step UserCloudPolicyManagerChromeOSTest will be migrated away
    from using SigninManager and ProfileOAuth2TokenService altogether.
    
    [1] https://crrev.com/c/1296873
    [2] https://crrev.com/c/1298775
    
    BUG=893504
    
    Change-Id: I25d66d1ebc7b4c5dbd20b9e281d0cbf788db80e8
    Reviewed-on: https://chromium-review.googlesource.com/c/1299313
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602827}
    Former-commit-id: c06645d4b069d122224d78a9be7b47fff1cce7b2

--- chrome/browser/chromeos/policy/user_cloud_policy_token_forwarder.cc
+++ chrome/browser/chromeos/policy/user_cloud_policy_token_forwarder.cc
@@ -7,20 +7,16 @@
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/chromeos/policy/user_cloud_policy_manager_chromeos.h"
 #include "components/policy/core/common/cloud/cloud_policy_core.h"
-#include "components/signin/core/browser/profile_oauth2_token_service.h"
 #include "content/public/browser/notification_source.h"
 #include "google_apis/gaia/gaia_constants.h"
+#include "services/identity/public/cpp/access_token_fetcher.h"
 
 namespace policy {
 
 UserCloudPolicyTokenForwarder::UserCloudPolicyTokenForwarder(
     UserCloudPolicyManagerChromeOS* manager,
-    identity::IdentityManager* identity_manager,
-    ProfileOAuth2TokenService* token_service)
-    : OAuth2TokenService::Consumer("policy_token_forwarder"),
-      manager_(manager),
-      identity_manager_(identity_manager),
-      token_service_(token_service) {
+    identity::IdentityManager* identity_manager)
+    : manager_(manager), identity_manager_(identity_manager) {
   // Start by waiting for the CloudPolicyService to be initialized, so that
   // we can check if it already has a DMToken or not.
   if (manager_->core()->service()->IsInitializationComplete()) {
@@ -33,7 +29,7 @@ UserCloudPolicyTokenForwarder::UserCloudPolicyTokenForwarder(
 UserCloudPolicyTokenForwarder::~UserCloudPolicyTokenForwarder() {}
 
 void UserCloudPolicyTokenForwarder::Shutdown() {
-  request_.reset();
+  access_token_fetcher_.reset();
   identity_manager_->RemoveObserver(this);
   manager_->core()->service()->RemoveObserver(this);
 }
@@ -42,31 +38,6 @@ void UserCloudPolicyTokenForwarder::OnRefreshTokenUpdatedForAccount(
     const AccountInfo& account_info,
     bool is_valid) {
   RequestAccessToken();
-}
-
-void UserCloudPolicyTokenForwarder::OnGetTokenSuccess(
-    const OAuth2TokenService::Request* request,
-    const OAuth2AccessTokenConsumer::TokenResponse& token_response) {
-  manager_->OnAccessTokenAvailable(token_response.access_token);
-  // All done here.
-  Shutdown();
-}
-
-void UserCloudPolicyTokenForwarder::OnGetTokenFailure(
-    const OAuth2TokenService::Request* request,
-    const GoogleServiceAuthError& error) {
-  // This should seldom happen: if the user is signing in for the first time
-  // then this was an online signin and network errors are unlikely; if the
-  // user had already signed in before then they should have policy cached, and
-  // RequestAccessToken() wouldn't have been invoked.
-  // Still, something just went wrong (server 500, or something). Currently
-  // we don't recover in this case, and we'll just try to register for policy
-  // again on the next signin.
-  // TODO(joaodasilva, atwilson): consider blocking signin when this happens,
-  // so that the user has to try again before getting into the session. That
-  // would guarantee that a session always has fresh policy, or at least
-  // enforces a cached policy.
-  Shutdown();
 }
 
 void UserCloudPolicyTokenForwarder::OnInitializationCompleted(
@@ -89,8 +60,36 @@ void UserCloudPolicyTokenForwarder::RequestAccessToken() {
   OAuth2TokenService::ScopeSet scopes;
   scopes.insert(GaiaConstants::kDeviceManagementServiceOAuth);
   scopes.insert(GaiaConstants::kOAuthWrapBridgeUserInfoScope);
-  request_ = token_service_->StartRequest(
-      identity_manager_->GetPrimaryAccountId(), scopes, this);
+  access_token_fetcher_ = identity_manager_->CreateAccessTokenFetcherForAccount(
+      identity_manager_->GetPrimaryAccountId(), "policy_token_forwarder",
+      scopes,
+      base::BindOnce(
+          &UserCloudPolicyTokenForwarder::OnAccessTokenFetchCompleted,
+          base::Unretained(this)),
+      identity::AccessTokenFetcher::Mode::kImmediate);
+}
+
+void UserCloudPolicyTokenForwarder::OnAccessTokenFetchCompleted(
+    GoogleServiceAuthError error,
+    identity::AccessTokenInfo token_info) {
+  DCHECK(access_token_fetcher_);
+
+  if (error.state() == GoogleServiceAuthError::NONE) {
+    manager_->OnAccessTokenAvailable(token_info.token);
+  } else {
+    // This should seldom happen: if the user is signing in for the first time
+    // then this was an online signin and network errors are unlikely; if the
+    // user had already signed in before then they should have policy cached,
+    // and RequestAccessToken() wouldn't have been invoked. Still, something
+    // just went wrong (server 500, or something). Currently we don't recover in
+    // this case, and we'll just try to register for policy again on the next
+    // signin.
+    // TODO(joaodasilva, atwilson): consider blocking signin when this happens,
+    // so that the user has to try again before getting into the session. That
+    // would guarantee that a session always has fresh policy, or at least
+    // enforces a cached policy.
+  }
+  Shutdown();
 }
 
 }  // namespace policy
