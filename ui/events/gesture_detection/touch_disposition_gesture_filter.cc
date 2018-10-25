7b3b5865ad334ea016328baaaf2fd1612f78be5e
W. James MacLean <wjmaclean@chromium.org>
2018-10-25 21:46:42 +0000

    Update DCHECK to handle case where event with same ID is already in queue.
    This CL corrects a DCHECK which can now be triggered as a consequence of
    https://chromium-review.googlesource.com/1169528.
    
    Prior to that CL, when a new gesture is synthesized from a TOUCH_TIMEOUT
    such as a GestureShowPress or GestureLongPress, it got a
    unique_touch_event_id of 0. The CL changed this so the event would take
    on the unique_touch_event_id of the event that caused it. This means
    it's possible for two adjacent events in the queue to share the same
    value, thus causing the DCHECK to fire.
    
    Here we modify the DCHECK so that any duplicate events, which will all
    have the TOUCH_TIMEOUT source attribute, don't trigger the DCHECK.
    
    TBR=tdresser@chromium.org
    
    Bug: 896042
    Change-Id: I51c45903a2ca2d0dac8e3939ff8193246f843380
    Reviewed-on: https://chromium-review.googlesource.com/c/1288891
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Commit-Queue: James MacLean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602867}
    Former-commit-id: 0da589ad1be1bf58188bb1191fa5304eab9bdd30

--- ui/events/gesture_detection/touch_disposition_gesture_filter.cc
+++ ui/events/gesture_detection/touch_disposition_gesture_filter.cc
@@ -181,9 +181,9 @@ TouchDispositionGestureFilter::OnGesturePacket(
                     Tail().back().unique_touch_event_id()));
   }
   if (!Head().empty()) {
-    DCHECK_NE(packet.unique_touch_event_id(),
-              Head().front().unique_touch_event_id());
-
+    DCHECK((packet.gesture_source() == GestureEventDataPacket::TOUCH_TIMEOUT) ||
+           packet.unique_touch_event_id() !=
+               Head().front().unique_touch_event_id());
   }
 
   Tail().push(packet);
