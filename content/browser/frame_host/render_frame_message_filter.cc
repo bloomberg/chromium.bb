c2c948ed9abc4fb7d3b7bafb0a653da0f54160b5
Robbie McElrath <rmcelrath@chromium.org>
2018-10-25 21:52:12 +0000

    Make setting cookies synchronous.
    Right now setting cookies is asynchronous, which can lead to race
    conditions where setting a cookie then sending a request doesn't result
    in the request having the cookie set (see the bug).
    
    Bug: 898704
    Change-Id: I1d362b0312942f57b933352b85c60cc1ff855fba
    Reviewed-on: https://chromium-review.googlesource.com/c/1298489
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602878}
    Former-commit-id: a71979244edd931ed1837d0670423e026011749c

--- content/browser/frame_host/render_frame_message_filter.cc
+++ content/browser/frame_host/render_frame_message_filter.cc
@@ -535,7 +535,6 @@ void RenderFrameMessageFilter::SetCookie(int32_t render_frame_id,
                                          const GURL& site_for_cookies,
                                          const std::string& cookie_line,
                                          SetCookieCallback callback) {
-  std::move(callback).Run();
   ChildProcessSecurityPolicyImpl* policy =
       ChildProcessSecurityPolicyImpl::GetInstance();
   if (!policy->CanAccessDataForOrigin(render_process_id_, url)) {
@@ -544,18 +543,22 @@ void RenderFrameMessageFilter::SetCookie(int32_t render_frame_id,
     SYSLOG(WARNING) << "Killing renderer: illegal cookie write. Reason: "
                     << reason;
     bad_message::ReceivedBadMessage(this, reason);
+    std::move(callback).Run();
     return;
   }
 
   net::CookieOptions options;
   std::unique_ptr<net::CanonicalCookie> cookie = net::CanonicalCookie::Create(
       url, cookie_line, base::Time::Now(), options);
-  if (!cookie)
+  if (!cookie) {
+    std::move(callback).Run();
     return;
+  }
 
   if (!GetContentClient()->browser()->AllowSetCookie(
           url, site_for_cookies, *cookie, resource_context_, render_process_id_,
           render_frame_id)) {
+    std::move(callback).Run();
     return;
   }
 
@@ -567,6 +570,7 @@ void RenderFrameMessageFilter::SetCookie(int32_t render_frame_id,
           url, resource_context_);
   if (cookie_store ||
       !base::FeatureList::IsEnabled(network::features::kNetworkService)) {
+    std::move(callback).Run();
     if (!cookie_store)
       cookie_store = request_context_->GetURLRequestContext()->cookie_store();
 
@@ -577,10 +581,14 @@ void RenderFrameMessageFilter::SetCookie(int32_t render_frame_id,
     return;
   }
 
+  net::CookieStore::SetCookiesCallback net_callback =
+      base::BindOnce([](SetCookieCallback callback,
+                        bool success) { std::move(callback).Run(); },
+                     std::move(callback));
   (*GetCookieManager())
       ->SetCanonicalCookie(*cookie, url.SchemeIsCryptographic(),
                            !options.exclude_httponly(),
-                           net::CookieStore::SetCookiesCallback());
+                           std::move(net_callback));
 }
 
 void RenderFrameMessageFilter::GetCookies(int render_frame_id,
