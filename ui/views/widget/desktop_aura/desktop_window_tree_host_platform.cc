86142eaa097176089f8b5db8bfb2d47ccf40020b
Scott Graham <scottmg@chromium.org>
2018-10-25 21:24:25 +0000

    fuchsia: Avoid unnecessary references to WindowEventFilter
    The link error I'm trying to avoid is this:
    
    [122->5/127 ~1] SOLINK ./libviews.so
    FAILED: libviews.so libviews.so.TOC lib.unstripped/libviews.so
    python "../../build/toolchain/gcc_solink_wrapper.py" --readelf="readelf" --nm="nm" --strip=../../third_party/eu-strip/bin/eu-strip --sofile="./lib.unstripped/libviews.so" --tocfile="./libviews.so.TOC" --output="./libviews.so" -- ../../third_party/llvm-build/Release+Asserts/bin/clang++ -shared -Wl,--fatal-warnings -fPIC -Wl,-z,noexecstack -Wl,-z,now -Wl,-z,relro -Wl,-z,defs -Wl,--as-needed -fuse-ld=lld -Wl,-z,max-page-size=4096 -Wl,--icf=all -Wl,--color-diagnostics --target=aarch64-fuchsia -Wl,-z,stack-size=0x800000 -Wl,--no-as-needed -lfdio -Wl,--as-needed -Werror -Wl,--gdb-index -nostdlib++ --sysroot=../../third_party/fuchsia-sdk/sdk/arch/arm64/sysroot -L../../third_party/fuchsia-sdk/sdk/arch/arm64/lib -o "./lib.unstripped/libviews.so" -Wl,-soname="libviews.so" @"./libviews.so.rsp"
    ld.lld: error: undefined symbol: views::WindowEventFilter::WindowEventFilter(views::DesktopWindowTreeHost*)
    >>> referenced by memory:3118 (../../buildtools/third_party/libc++/trunk/include/memory:3118)
    >>>               obj/ui/views/views/desktop_window_tree_host_platform.o:(views::DesktopWindowTreeHostPlatform::OnNativeWidgetCreated(views::Widget::InitParams const&))
    
    ld.lld: error: undefined symbol: views::WindowEventFilter::SetWmMoveResizeHandler(ui::WmMoveResizeHandler*)
    >>> referenced by desktop_window_tree_host_platform.cc:106 (../../ui/views/widget/desktop_aura/desktop_window_tree_host_platform.cc:106)
    >>>               obj/ui/views/views/desktop_window_tree_host_platform.o:(views::DesktopWindowTreeHostPlatform::OnNativeWidgetCreated(views::Widget::InitParams const&))
    clang: error: ld.lld command failed with exit code 1 (use -v to see invocation)
    
    I'm not sure what the correct approach is; DesktopWindowTreeHostPlatform
    depends on WindowEventFilter which uses LinuxUI.
    
    Since there's no available non-client area on Fuchsia currently, not
    doing this seems correct for the time being. Or does Fuchsia really need
    its own DesktopWindowTreeHostFuchsia implementation?
    
    Bug: 808287
    Change-Id: I2af683415ea00147607eb4236540d8d3e8643603
    Reviewed-on: https://chromium-review.googlesource.com/c/1298554
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602860}
    Former-commit-id: 55729963b1e454fc0011ac95f008e0ceb7f7976e

--- ui/views/widget/desktop_aura/desktop_window_tree_host_platform.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_platform.cc
@@ -5,6 +5,7 @@
 #include "ui/views/widget/desktop_aura/desktop_window_tree_host_platform.h"
 
 #include "base/time/time.h"
+#include "build/build_config.h"
 #include "ui/aura/client/drag_drop_client.h"
 #include "ui/aura/client/transient_window_client.h"
 #include "ui/base/hit_test.h"
@@ -16,10 +17,13 @@
 #include "ui/platform_window/platform_window_init_properties.h"
 #include "ui/views/corewm/tooltip_aura.h"
 #include "ui/views/widget/desktop_aura/desktop_native_widget_aura.h"
-#include "ui/views/widget/desktop_aura/window_event_filter.h"
 #include "ui/views/widget/widget_aura_utils.h"
 #include "ui/views/window/native_frame_view.h"
 #include "ui/wm/core/window_util.h"
+
+#if defined(OS_LINUX)
+#include "ui/views/widget/desktop_aura/window_event_filter.h"
+#endif
 
 namespace views {
 
@@ -96,6 +100,7 @@ void DesktopWindowTreeHostPlatform::OnNativeWidgetCreated(
     const Widget::InitParams& params) {
   native_widget_delegate_->OnNativeWidgetCreated(true);
 
+#if defined(OS_LINUX)
   // Setup a non_client_window_event_filter, which handles resize/move, double
   // click and other events.
   DCHECK(!non_client_window_event_filter_);
@@ -108,6 +113,7 @@ void DesktopWindowTreeHostPlatform::OnNativeWidgetCreated(
 
   non_client_window_event_filter_ = std::move(window_event_filter);
   window()->AddPreTargetHandler(non_client_window_event_filter_.get());
+#endif
 }
 
 void DesktopWindowTreeHostPlatform::OnWidgetInitDone() {}
