134d3e94c7aedabb5c800cc2121dc8ed8fb8c1a7
Erik Chen <erikchen@chromium.org>
2018-10-25 22:06:05 +0000

    Refactor Watcher::DidRunEventOnUIThread to not take a creation time.
    The creation time of native events is no longer used because there is known
    noise on Windows, macOS and Linux. This CL is a refactor with no intended
    behavior change.
    
    Change-Id: I9eb860fb27b6642be25dfbbaa9d5615d0b619073
    Bug: 859155
    Reviewed-on: https://chromium-review.googlesource.com/c/1296619
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602891}
    Former-commit-id: 2b4696db161a85f9a9716ae045a76bed5f0dcef5

--- content/browser/scheduler/responsiveness/native_event_observer.cc
+++ content/browser/scheduler/responsiveness/native_event_observer.cc
@@ -8,8 +8,6 @@
 // Windows headers must come first.
 #if defined(OS_WIN)
 #include <windows.h>
-
-#include <timeapi.h>
 #endif
 
 // Proceed with header includes in usual order.
@@ -28,29 +26,6 @@
 
 namespace content {
 namespace responsiveness {
-
-#if defined(OS_WIN) || (defined(OS_LINUX) && defined(USE_X11))
-
-namespace {
-
-// Clamps a TimeDelta to be within [0 seconds, 30 seconds].
-// Relies on the assumption that |delta| is >= 0 seconds.
-base::TimeDelta ClampDeltaFromExternalSource(const base::TimeDelta& delta) {
-  DCHECK_GE(delta, base::TimeDelta());
-
-  // Ignore pathologically long deltas. External source is probably having
-  // issues.
-  constexpr base::TimeDelta pathologically_long_duration =
-      base::TimeDelta::FromSeconds(30);
-  if (delta > pathologically_long_duration)
-    return base::TimeDelta();
-
-  return delta;
-}
-
-}  // namespace
-
-#endif  // defined(OS_WIN) || (defined(OS_LINUX) && defined(USE_X11))
 
 NativeEventObserver::NativeEventObserver(
     WillRunEventCallback will_run_event_callback,
@@ -75,7 +50,7 @@ void NativeEventObserver::DeregisterObserver() {
 void NativeEventObserver::OnWindowEventDispatcherStartedProcessing(
     aura::WindowEventDispatcher* dispatcher,
     const ui::Event& event) {
-  EventInfo info{&event, event.time_stamp()};
+  EventInfo info{&event};
   events_being_processed_.push_back(info);
   will_run_event_callback_.Run(&event);
 }
@@ -83,7 +58,7 @@ void NativeEventObserver::OnWindowEventDispatcherStartedProcessing(
 void NativeEventObserver::OnWindowEventDispatcherFinishedProcessingEvent(
     aura::WindowEventDispatcher* dispatcher) {
   EventInfo& info = events_being_processed_.back();
-  did_run_event_callback_.Run(info.unique_id, info.creation_time);
+  did_run_event_callback_.Run(info.unique_id);
   events_being_processed_.pop_back();
 }
 #endif  // defined(OS_LINUX)
@@ -99,31 +74,7 @@ void NativeEventObserver::WillDispatchMSG(const MSG& msg) {
   will_run_event_callback_.Run(&msg);
 }
 void NativeEventObserver::DidDispatchMSG(const MSG& msg) {
-  // On Windows, MSG.time is in units of milliseconds since system started. It
-  // uses the timer exposed by ::GetTickCount().
-  // https://blogs.msdn.microsoft.com/oldnewthing/20140122-00/?p=2013
-  //
-  // This timer has ~16ms granularity. This is okay for us since we require
-  // ~100ms granularity.
-  // https://randomascii.wordpress.com/2013/05/09/timegettime-versus-gettickcount/
-  //
-  // To convert MSG.time to TimeTicks, we subtract MSG.time from
-  // ::GetTickCount() to create a TimeDelta, and then subtract that from
-  // TimeTicks::Now().
-  //
-  // We cast both values to DWORD [uint32], perform subtraction to get a uint32,
-  // and then shove that into a TimeDelta.
-  //
-  // Note: Both measurements of time can experience rollover. If one of them has
-  // rolled over but the other has not, then the result will be very large.
-  // ClampDeltaFromExternalSource takes care of this, in addition to any other
-  // odd timing issues that may come up [e.g. MSG.time time-traveling to give a
-  // larger value than ::timeGetTime() -- this has not been observed in practice
-  // but since we don't control the code in question, we trust nothing].
-  base::TimeDelta delta = base::TimeDelta::FromMilliseconds(
-      ::GetTickCount() - static_cast<DWORD>(msg.time));
-  base::TimeDelta sanitized = ClampDeltaFromExternalSource(delta);
-  did_run_event_callback_.Run(&msg, base::TimeTicks::Now() - sanitized);
+  did_run_event_callback_.Run(&msg);
 }
 #endif  // defined(OS_WIN)
 
