32431255d00dc661db3981abb8e95a61472c1428
Stepan Khapugin <stkhapugin@chromium.org>
2018-10-25 17:17:09 +0000

    Close the popup when no results are shown on NTP.
    Currently, when using the omnibox on NTP, the popup stays open even
    if it has no results. This CL closes the popup in this case.
    
    Bug: 898143, 896721
    Change-Id: I205fbdb051e4a68619aee36e92cedbe271b4a834
    Reviewed-on: https://chromium-review.googlesource.com/c/1299156
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#602769}
    Former-commit-id: 520e9e316d724bce74396f92173034297fbff687

--- ios/chrome/browser/ui/omnibox/popup/omnibox_popup_mediator.h
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_popup_mediator.h
@@ -13,6 +13,7 @@
 
 @protocol BrowserCommands;
 @class OmniboxPopupPresenter;
+class WebStateList;
 
 namespace image_fetcher {
 class IOSImageDataFetcherWrapper;
@@ -57,6 +58,8 @@ class OmniboxPopupMediatorDelegate {
 // Presenter for the popup, handling the positioning and the presentation
 // animations.
 @property(nonatomic, strong) OmniboxPopupPresenter* presenter;
+// The web state list this mediator is handling.
+@property(nonatomic, assign) WebStateList* webStateList;
 
 @end
 
