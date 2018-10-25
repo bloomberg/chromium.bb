0054c9584ba164e8c9d5b107e0e1bb6cb09c6282
Miguel Casas <mcasas@chromium.org>
2018-10-25 16:39:51 +0000

    Canvas LowLatency: wire lowLatency in getContextAttributes()
    This CL wires |lowLatency| in WebGL's getContextAttributes(),
    and adds LayoutTests entries wherever appropriate.
    
    Also cleans up the {} in canvas_rendering_context.cc (adds or
    removes them where appropriate).
    
    Bug: 895918
    Change-Id: I08872f45cbadbf0772c92a761dee15243bd4f17a
    Reviewed-on: https://chromium-review.googlesource.com/c/1294301
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#601943}(cherry picked from commit bf57289404befc2a0bca1900deb0521ba8155b0a [formerly 9e9987d9eb2925d5454685a7374455d5aebae9a7])
    Reviewed-on: https://chromium-review.googlesource.com/c/1299438
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3578@{#323}
    Cr-Branched-From: abbd18012032883c3113b3bfc5d59d25b4836c5f [formerly 4226ddf99103e493d7afb23a4c7902ee496108b6]-refs/heads/master@{#599034}
    Former-commit-id: 5675143d40fd6524180e6fdd0add02de844f19c0

--- third_party/blink/renderer/core/html/canvas/canvas_rendering_context.cc
+++ third_party/blink/renderer/core/html/canvas/canvas_rendering_context.cc
@@ -54,9 +54,8 @@ CanvasRenderingContext::CanvasRenderingContext(
       color_params_.SetCanvasColorSpace(kP3CanvasColorSpace);
   }
 
-  if (!creation_attributes_.alpha) {
+  if (!creation_attributes_.alpha)
     color_params_.SetOpacityMode(kOpaque);
-  }
 
   if (!OriginTrials::LowLatencyCanvasEnabled(host->GetTopExecutionContext()))
     creation_attributes_.low_latency = false;
@@ -96,9 +95,8 @@ WTF::String CanvasRenderingContext::PixelFormatAsString() const {
 }
 
 void CanvasRenderingContext::Dispose() {
-  if (finalize_frame_scheduled_) {
+  if (finalize_frame_scheduled_)
     Platform::Current()->CurrentThread()->RemoveTaskObserver(this);
-  }
 
   // HTMLCanvasElement and CanvasRenderingContext have a circular reference.
   // When the pair is no longer reachable, their destruction order is non-
@@ -134,9 +132,8 @@ void CanvasRenderingContext::DidProcessTask() {
   finalize_frame_scheduled_ = false;
   // The end of a script task that drew content to the canvas is the point
   // at which the current frame may be considered complete.
-  if (Host()) {
+  if (Host())
     Host()->FinalizeFrame();
-  }
   FinalizeFrame();
 }
 
@@ -172,17 +169,18 @@ bool CanvasRenderingContext::WouldTaintOrigin(
     CanvasImageSource* image_source,
     const SecurityOrigin* destination_security_origin) {
   const KURL& source_url = image_source->SourceURL();
-  bool has_url = (source_url.IsValid() && !source_url.IsAboutBlankURL());
+  const bool has_url = (source_url.IsValid() && !source_url.IsAboutBlankURL());
 
   if (has_url) {
     if (source_url.ProtocolIsData() ||
-        clean_urls_.Contains(source_url.GetString()))
+        clean_urls_.Contains(source_url.GetString())) {
       return false;
+    }
     if (dirty_urls_.Contains(source_url.GetString()))
       return true;
   }
 
-  bool taint_origin =
+  const bool taint_origin =
       image_source->WouldTaintOrigin(destination_security_origin);
   if (has_url) {
     if (taint_origin)
