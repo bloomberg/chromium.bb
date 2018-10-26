55c65ee5e8a15a1cfdee93156fc7cced0b0b6a84
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2018-10-26 06:14:33 +0000

    Avoid flashing of notification counter for bundled notifications.
    This is reland of crrev.com/c/1295633 with some fix.
    
    When ARC++ app builds bundled notifications, multiple individual
    notifications are sent and then replaced with a single bundled
    notification. The indicator icon showed such update as-is, resulting
    in flashing. This change limits the frequency to reflect notification
    count updates for avoiding such effect.
    
    Bug: 892021
    Change-Id: If7dbfdfdc530d3498f271f9b7b15b2d02b6b216d
    Reviewed-on: https://chromium-review.googlesource.com/c/1298829
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603001}
    Former-commit-id: 463378c676495faf0b29a3335cb310c4f5e63f13

--- ash/system/unified/unified_system_tray.cc
+++ ash/system/unified/unified_system_tray.cc
@@ -27,6 +27,8 @@
 #include "ash/system/unified/unified_slider_bubble_controller.h"
 #include "ash/system/unified/unified_system_tray_bubble.h"
 #include "ash/system/unified/unified_system_tray_model.h"
+#include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "chromeos/network/network_handler.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/display/display.h"
@@ -64,6 +66,9 @@ class UnifiedSystemTray::UiDelegate : public MessageCenterUiDelegate {
 
   DISALLOW_COPY_AND_ASSIGN(UiDelegate);
 };
+
+const base::TimeDelta UnifiedSystemTray::kNotificationCountUpdateDelay =
+    base::TimeDelta::FromMilliseconds(100);
 
 UnifiedSystemTray::UiDelegate::UiDelegate(UnifiedSystemTray* owner)
     : owner_(owner) {
@@ -327,6 +332,16 @@ void UnifiedSystemTray::HideBubbleInternal() {
 }
 
 void UnifiedSystemTray::UpdateNotificationInternal() {
+  // Limit update frequency in order to avoid flashing when 2 updates are
+  // incoming in a very short period of time. It happens when ARC++ apps
+  // creating bundled notifications.
+  if (!timer_.IsRunning()) {
+    timer_.Start(FROM_HERE, kNotificationCountUpdateDelay, this,
+                 &UnifiedSystemTray::UpdateNotificationAfterDelay);
+  }
+}
+
+void UnifiedSystemTray::UpdateNotificationAfterDelay() {
   notification_counter_item_->Update();
   quiet_mode_view_->Update();
 }
