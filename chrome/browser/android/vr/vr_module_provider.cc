8dc8576954f952b2f6261539aeaebac6835b495d
Tibor Goldschwendt <tiborg@chromium.org>
2018-10-26 15:21:16 +0000

    Reland "[vr] On-demand install VR dynamic feature module"
    This is a reland of c2d10db89006a84224eb4500b90f82cb222fd15a [formerly 0275d36033c0a24af4c67ec8eb1a71cb84adb347] with a fix
    for x86 builders, which have VR disabled.
    
    Original change's description:
    > [vr] On-demand install VR dynamic feature module
    >
    > With this CL we initate the module install once the user enters VR on a
    > WebXR/VR page. In more detail this CL
    >
    > - Introduces native VrModuleDelegate used to request VR module from
    >   native,
    >
    > - Refactors GvrDevice to request the VR module as part of its
    >   initialization flow,
    >
    > - Adds native side of the VrModuleProvider for Java-native
    >   communication.
    >
    > Bug: 862689
    > Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:linux_vr;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    > Change-Id: I960d2bdd4d2c64a19fa6069e457d44f77ab0e73b
    > Reviewed-on: https://chromium-review.googlesource.com/c/1241458
    > Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Reviewed-by: Bill Orr <billorr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#602922}
    
    TBR=tedchoc@chromium.org,billorr@chromium.org
    
    Bug: 862689
    Change-Id: I7a0f383534b622e0e4acb5acb2a1c28e547d0970
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:linux_vr;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Reviewed-on: https://chromium-review.googlesource.com/c/1301568
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603100}
    Former-commit-id: 0a8ce56213d8bba7a870171ab69bb6b28e84bca4

--- chrome/browser/android/vr/vr_module_provider.cc
+++ chrome/browser/android/vr/vr_module_provider.cc
@@ -2,10 +2,63 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/android/vr/vr_module_provider.h"
+
 #include "chrome/browser/android/vr/register_jni.h"
+#include "chrome/browser/android/vr/vr_module_provider.h"
+#include "device/vr/android/gvr/vr_module_delegate.h"
 #include "jni/VrModuleProvider_jni.h"
 
 namespace vr {
+
+VrModuleProvider::VrModuleProvider() = default;
+
+VrModuleProvider::~VrModuleProvider() {
+  if (!j_vr_module_provider_.obj()) {
+    return;
+  }
+  Java_VrModuleProvider_onNativeDestroy(base::android::AttachCurrentThread(),
+                                        j_vr_module_provider_);
+}
+
+bool VrModuleProvider::ModuleInstalled() {
+  return Java_VrModuleProvider_isModuleInstalled(
+      base::android::AttachCurrentThread());
+}
+
+void VrModuleProvider::InstallModule(
+    base::OnceCallback<void(bool)> on_finished) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  on_finished_callbacks_.push(std::move(on_finished));
+  // Don't request VR module multiple times in parallel.
+  if (on_finished_callbacks_.size() > 1) {
+    return;
+  }
+  DCHECK(!j_vr_module_provider_.obj());
+  j_vr_module_provider_.Reset(Java_VrModuleProvider_create(
+      base::android::AttachCurrentThread(), (jlong) this));
+  Java_VrModuleProvider_installModule(base::android::AttachCurrentThread(),
+                                      j_vr_module_provider_);
+}
+
+void VrModuleProvider::OnInstalledModule(
+    JNIEnv* env,
+    const base::android::JavaParamRef<jobject>& obj,
+    bool success) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK(on_finished_callbacks_.size() > 0);
+  while (!on_finished_callbacks_.empty()) {
+    std::move(on_finished_callbacks_.front()).Run(success);
+    on_finished_callbacks_.pop();
+  }
+  j_vr_module_provider_ = nullptr;
+}
+
+static void JNI_VrModuleProvider_Init(
+    JNIEnv* env,
+    const base::android::JavaParamRef<jclass>& clazz) {
+  device::VrModuleDelegate::Set(std::make_unique<VrModuleProvider>());
+}
 
 static void JNI_VrModuleProvider_RegisterJni(
     JNIEnv* env,
