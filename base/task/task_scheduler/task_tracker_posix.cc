eb1df0b84003c306b2dafcc3e4cafb6efcf79d43
Alexander Timin <altimin@chromium.org>
2018-10-27 15:40:10 +0000

    Reland "[base] Use TaskRunner instead of MessageLoop in FileDescriptorWatcher"
    This is a reland of 89badfa8eb96872c46cef9c97b8ed680ee1759d4 [formerly fcce12fe035224e4574097f3f15398b766276ccd]
    
    TBR=gab@chromium.org
    BUG=891670
    
    Original change's description:
    > [base] Use TaskRunner instead of MessageLoop in FileDescriptorWatcher
    >
    > Do not use MessageLoopForIO as it is used to post tasks anyway.
    >
    > R=gab@chromium.org
    > TBR=gab@chromium.org
    > BUG=891670
    >
    > Change-Id: Ib255c6a5a5dd55c2654eaa284d43d7f78f06b244
    > Reviewed-on: https://chromium-review.googlesource.com/c/1299239
    > Commit-Queue: Alexander Timin <altimin@chromium.org>
    > Reviewed-by: Alexander Timin <altimin@chromium.org>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Reviewed-by: François Doray <fdoray@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#603076}
    
    Bug: 891670
    Change-Id: I80392385f1b63226d819e490132b6c4f8cf5198c
    Reviewed-on: https://chromium-review.googlesource.com/c/1303714
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603338}
    Former-commit-id: 6247f6a91ade61d198c618966cdb2669beab981b

--- base/task/task_scheduler/task_tracker_posix.cc
+++ base/task/task_scheduler/task_tracker_posix.cc
@@ -7,6 +7,7 @@
 #include <utility>
 
 #include "base/files/file_descriptor_watcher_posix.h"
+#include "base/message_loop/message_loop.h"
 
 namespace base {
 namespace internal {
@@ -19,7 +20,7 @@ void TaskTrackerPosix::RunOrSkipTask(Task task,
                                      bool can_run_task) {
   DCHECK(watch_file_descriptor_message_loop_);
   FileDescriptorWatcher file_descriptor_watcher(
-      watch_file_descriptor_message_loop_);
+      watch_file_descriptor_message_loop_->task_runner());
   TaskTracker::RunOrSkipTask(std::move(task), sequence, can_run_task);
 }
 
