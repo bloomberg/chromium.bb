4dc74329b5870a109ff2bcd7639a5f548780df91
Becky Zhou <huayinz@chromium.org>
2018-10-30 00:07:10 +0000

    [Feed] Fix compile error and test crash when compiled with dummy version
    Bug: 897502
    Change-Id: If4e9c315c5742be40764e9a13f0d002acb96a43e
    Reviewed-on: https://chromium-review.googlesource.com/c/1305821
    Commit-Queue: Becky Zhou <huayinz@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603696}
    Former-commit-id: 8dfabd8a1696b44ae1c3608d04b3b2f13de85b4a

--- chrome/android/feed/dummy/java/src/org/chromium/chrome/browser/feed/FeedNewTabPage.java
+++ chrome/android/feed/dummy/java/src/org/chromium/chrome/browser/feed/FeedNewTabPage.java
@@ -4,6 +4,7 @@
 
 package org.chromium.chrome.browser.feed;
 
+import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.native_page.NativePageHost;
 import org.chromium.chrome.browser.ntp.NewTabPage;
@@ -22,5 +23,10 @@ public class FeedNewTabPage extends NewTabPage {
     public FeedNewTabPage(ChromeActivity activity, NativePageHost nativePageHost,
             TabModelSelector tabModelSelector) {
         super(activity, nativePageHost, tabModelSelector);
+    }
+
+    @VisibleForTesting
+    public static boolean isDummy() {
+        return true;
     }
 }
