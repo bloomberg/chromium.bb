7549a3394bf2baf4f419147702d37c90c10dc1ca
Takashi Sakamoto <tasak@google.com>
2018-10-30 12:17:35 +0000

    Remove include memory_coordinator_client_registry.h from gpu/
    Bug: 888904
    Change-Id: Ia32cdea2e62828a02f05d555af2635366d077f82
    Reviewed-on: https://chromium-review.googlesource.com/c/1304177
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/master@{#603866}
    Former-commit-id: bd2a5a0d788cd22ec6aeda84911151b1aef433ad

--- gpu/command_buffer/service/service_transfer_cache.cc
+++ gpu/command_buffer/service/service_transfer_cache.cc
@@ -7,9 +7,9 @@
 #include <inttypes.h>
 
 #include "base/bind.h"
-#include "base/memory/memory_coordinator_client_registry.h"
 #include "base/strings/stringprintf.h"
 #include "base/sys_info.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "base/trace_event/memory_dump_manager.h"
 #include "cc/paint/image_transfer_cache_entry.h"
 #include "third_party/skia/include/gpu/GrBackendSurface.h"
