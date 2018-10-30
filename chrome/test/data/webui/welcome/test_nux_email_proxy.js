55d1b413969a411c7e3ea425b5cfa154212b6d8b
Scott Chen <scottchen@chromium.org>
2018-10-30 03:12:19 +0000

    Nux Onboarding: add email-chooser browswertest
    This CL adds the boilerplate codes for browser-testing the onboarding
    welcome UI, and one simple test to verify it works. There will be
    follow-up CLs to add more browser tests to all onboarding welcome elements.
    
    Bug: 881937
    Change-Id: I1c1d33411ad55eff5fd4ab43bd3cc1b93a257d3e
    Reviewed-on: https://chromium-review.googlesource.com/c/1300673
    Commit-Queue: Scott Chen <scottchen@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603766}
    Former-commit-id: 7767728e96f4c30609bb6e33537e3e8f23d15d66

---
+++ chrome/test/data/webui/welcome/test_nux_email_proxy.js
@@ -0,0 +1,73 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/** @implements {nux.NuxEmailProxy} */
+class TestNuxEmailProxy extends TestBrowserProxy {
+  constructor() {
+    super([
+      'cacheBookmarkIcon',
+      'getEmailList',
+      'getSavedProvider',
+      'recordPageInitialized',
+      'recordClickedOption',
+      'recordClickedDisabledButton',
+      'recordProviderSelected',
+      'recordNoThanks',
+      'recordGetStarted',
+      'recordFinalize',
+    ]);
+
+    /** @private {!Array<!nux.BookmarkListItem>} */
+    this.emailList_ = [];
+  }
+
+  /** @override */
+  recordPageInitialized() {
+    this.methodCalled('recordPageInitialized');
+  }
+
+  /** @override */
+  getEmailList() {
+    this.methodCalled('getEmailList');
+    return Promise.resolve(this.emailList_);
+  }
+
+  /** @override */
+  cacheBookmarkIcon() {
+    this.methodCalled('cacheBookmarkIcon');
+  }
+  /** @override */
+  getSavedProvider() {
+    this.methodCalled('getSavedProvider');
+  }
+  /** @override */
+  recordClickedOption() {
+    this.methodCalled('recordClickedOption');
+  }
+  /** @override */
+  recordClickedDisabledButton() {
+    this.methodCalled('recordClickedDisabledButton');
+  }
+  /** @override */
+  recordProviderSelected() {
+    this.methodCalled('recordProviderSelected');
+  }
+  /** @override */
+  recordNoThanks() {
+    this.methodCalled('recordNoThanks');
+  }
+  /** @override */
+  recordGetStarted() {
+    this.methodCalled('recordGetStarted');
+  }
+  /** @override */
+  recordFinalize() {
+    this.methodCalled('recordFinalize');
+  }
+
+  /** @param {!Array<!nux.BookmarkListItem>} emailList */
+  setEmailList(emailList) {
+    this.emailList_ = emailList;
+  }
+}
