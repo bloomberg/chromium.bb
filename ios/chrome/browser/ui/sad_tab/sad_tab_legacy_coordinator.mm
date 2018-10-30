66c0a129aa279782bf0ba7081ef25b46e9b62dde
Eugene But <eugenebut@google.com>
2018-10-30 18:19:19 +0000

    Move model code from SadTabView to SadTabLegacyCoordinator.
    Notable changes:
     - Removed NavigationManager usage from SadTabView
     - Removed Dispatcher usage from SadTabView
     - Reload the page via dispatcher instead of navigation manager
     - Renamed SadTabView's delegate from actionDelegate to delegate
       (because there is just one delegate)
     - Updated delegate names to match Coding Guidelines for Cocoa
    
    This refactoring will allow to create SadTabViewController to be
    presented from SadTabCoordinator. SadTabCoordinator will be a new
    class owned by BVC, and created with BrowserContainerViewController
    as baseViewController. All new code will be written behind the flag.
    
    The refactoring is needed, because in New Architecture view
    controllers are not suppose to have model code.
    
    Change-Id: If98f6d4af9c87a0274b717b266f94f71956b79a0
    Reviewed-on: https://chromium-review.googlesource.com/c/1306351
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603966}
    Former-commit-id: 56a6b03880405c46c43c7c7fee319420122547bb

--- ios/chrome/browser/ui/sad_tab/sad_tab_legacy_coordinator.mm
+++ ios/chrome/browser/ui/sad_tab/sad_tab_legacy_coordinator.mm
@@ -5,23 +5,36 @@
 #import "ios/chrome/browser/ui/sad_tab/sad_tab_legacy_coordinator.h"
 
 #import "ios/chrome/browser/ui/commands/application_commands.h"
+#import "ios/chrome/browser/ui/commands/browser_commands.h"
+#import "ios/chrome/browser/ui/commands/open_new_tab_command.h"
 #import "ios/chrome/browser/ui/sad_tab/sad_tab_view.h"
 #import "ios/chrome/browser/web/sad_tab_tab_helper.h"
+#include "ios/web/public/browser_state.h"
 #import "ios/web/public/navigation_manager.h"
 #import "ios/web/public/web_state/ui/crw_generic_content_view.h"
-#include "ios/web/public/web_state/web_state.h"
+#import "ios/web/public/web_state/web_state.h"
 
-@interface SadTabLegacyCoordinator ()<SadTabActionDelegate>
+@interface SadTabLegacyCoordinator ()<SadTabViewDelegate>
 @end
 
 @implementation SadTabLegacyCoordinator
 @synthesize baseViewController = _baseViewController;
 @synthesize dispatcher = _dispatcher;
 
-#pragma mark - SadTabActionDelegate
+#pragma mark - SadTabViewDelegate
 
-- (void)showReportAnIssue {
+- (void)sadTabViewShowReportAnIssue:(SadTabView*)sadTabView {
   [self.dispatcher showReportAnIssueFromViewController:self.baseViewController];
+}
+
+- (void)sadTabView:(SadTabView*)sadTabView
+    showSuggestionsPageWithURL:(const GURL&)URL {
+  OpenNewTabCommand* command = [OpenNewTabCommand commandWithURLFromChrome:URL];
+  [self.dispatcher openURLInNewTab:command];
+}
+
+- (void)sadTabViewReload:(SadTabView*)sadTabView {
+  [self.dispatcher reload];
 }
 
 #pragma mark - SadTabTabHelperDelegate
@@ -29,15 +42,14 @@
 - (void)sadTabTabHelper:(SadTabTabHelper*)tabHelper
     presentSadTabForWebState:(web::WebState*)webState
              repeatedFailure:(BOOL)repeatedFailure {
-  // Create a SadTabView so |webstate| presents it.
-  SadTabView* sadTabview = [[SadTabView alloc]
-           initWithMode:repeatedFailure ? SadTabViewMode::FEEDBACK
-                                        : SadTabViewMode::RELOAD
-      navigationManager:webState->GetNavigationManager()];
-  sadTabview.dispatcher = static_cast<id<ApplicationCommands>>(self.dispatcher);
-  sadTabview.actionDelegate = self;
+  SadTabViewMode mode =
+      repeatedFailure ? SadTabViewMode::FEEDBACK : SadTabViewMode::RELOAD;
+  SadTabView* sadTabView = [[SadTabView alloc]
+      initWithMode:mode
+      offTheRecord:webState->GetBrowserState()->IsOffTheRecord()];
+  sadTabView.delegate = self;
   CRWContentView* contentView =
-      [[CRWGenericContentView alloc] initWithView:sadTabview];
+      [[CRWGenericContentView alloc] initWithView:sadTabView];
   webState->ShowTransientContentView(contentView);
 }
 
--- ios/chrome/browser/ui/sad_tab/sad_tab_view.h
+++ ios/chrome/browser/ui/sad_tab/sad_tab_view.h
@@ -11,6 +11,7 @@
 #include "ios/web/public/navigation_manager.h"
 
 @protocol ApplicationCommands;
+@class SadTabView;
 
 // Describes the mode of the Sad Tab, whether it should offer an attempt to
 // reload content, or whether it should offer a way to provide feedback.
@@ -19,21 +20,25 @@ enum class SadTabViewMode {
   FEEDBACK,    // A mode which allows the user to provide feedback
 };
 
-// Protocol for actions from the SadTabView.
-@protocol SadTabActionDelegate
-// Shows reportAnIssue UI.
-- (void)showReportAnIssue;
+@protocol SadTabViewDelegate
+// Instructs the delegate to show Report An Issue UI.
+- (void)sadTabViewShowReportAnIssue:(SadTabView*)sadTabView;
+
+// Instructs the delegate to show Suggestions help page with the given URL.
+- (void)sadTabView:(SadTabView*)sadTabView
+    showSuggestionsPageWithURL:(const GURL&)URL;
+
+// Instructs the delegate to reload this page.
+- (void)sadTabViewReload:(SadTabView*)sadTabView;
+
 @end
 
-// The view used to show "sad tab" content to the user when WKWebView's renderer
+// The view used to show "sad tab" content to the user when WebState's renderer
 // process crashes.
 @interface SadTabView : UIView
 
-// Designated initializer. |navigationManager| allows the view to execute
-// actions such as a reload if necessary.
 - (instancetype)initWithMode:(SadTabViewMode)mode
-           navigationManager:(web::NavigationManager*)navigationManager
-    NS_DESIGNATED_INITIALIZER;
+                offTheRecord:(BOOL)offTheRecord NS_DESIGNATED_INITIALIZER;
 
 - (instancetype)init NS_UNAVAILABLE;
 - (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
@@ -42,11 +47,7 @@ enum class SadTabViewMode {
 // Determines the type of Sad Tab information that will be displayed.
 @property(nonatomic, readonly) SadTabViewMode mode;
 
-// The dispatcher for this view.
-@property(nonatomic, weak) id<ApplicationCommands> dispatcher;
-
-// The delegate for actions from this view.
-@property(nonatomic, weak) id<SadTabActionDelegate> actionDelegate;
+@property(nonatomic, weak) id<SadTabViewDelegate> delegate;
 
 @end
 
