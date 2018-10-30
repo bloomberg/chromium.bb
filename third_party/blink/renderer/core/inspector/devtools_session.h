9ee585ea8db7fae3a10cc9af4732358deb197c72
Dmitry Gozman <dgozman@chromium.org>
2018-10-30 12:55:31 +0000

    [DevTools] Merge InspectorSession and DevToolsAgent::Session
    These are not needed as separate entities anymore, after making
    workers reuse DevToolsAgent::Session. We merge them into a single
    DevToolsSession class.
    
    Bug: 775132
    Change-Id: I213de7e33c5b7ce12d4f58e1b2daa363ad563b12
    Reviewed-on: https://chromium-review.googlesource.com/c/1282207
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603869}
    Former-commit-id: 491ed062f13f34b9d8dfa3951ebee4770317b47f

--- third_party/blink/renderer/core/inspector/devtools_session.h
+++ third_party/blink/renderer/core/inspector/devtools_session.h
@@ -2,10 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_INSPECTOR_INSPECTOR_SESSION_H_
-#define THIRD_PARTY_BLINK_RENDERER_CORE_INSPECTOR_INSPECTOR_SESSION_H_
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_INSPECTOR_DEVTOOLS_SESSION_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_INSPECTOR_DEVTOOLS_SESSION_H_
 
 #include "base/macros.h"
+#include "mojo/public/cpp/bindings/associated_binding.h"
 #include "third_party/blink/public/web/devtools_agent.mojom-blink.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/inspector/inspector_session_state.h"
@@ -18,66 +19,56 @@
 
 namespace blink {
 
+class DevToolsAgent;
+class DocumentLoader;
 class InspectorAgent;
-class InspectedFrames;
-class CoreProbeSink;
 class LocalFrame;
 
-class CORE_EXPORT InspectorSession
-    : public GarbageCollectedFinalized<InspectorSession>,
+class CORE_EXPORT DevToolsSession
+    : public GarbageCollectedFinalized<DevToolsSession>,
+      public mojom::blink::DevToolsSession,
       public protocol::FrontendChannel,
       public v8_inspector::V8Inspector::Channel {
  public:
-  class Client {
-   public:
-    virtual void SendProtocolResponse(
-        int session_id,
-        int call_id,
-        const String& response,
-        mojom::blink::DevToolsSessionStatePtr updates) = 0;
-    virtual void SendProtocolNotification(
-        int session_id,
-        const String& message,
-        mojom::blink::DevToolsSessionStatePtr updates) = 0;
-    virtual ~Client() = default;
-  };
-
-  InspectorSession(
-      Client*,
-      CoreProbeSink*,
-      InspectedFrames*,
-      int session_id,
-      v8_inspector::V8Inspector*,
-      int context_group_id,
+  DevToolsSession(
+      DevToolsAgent*,
+      mojom::blink::DevToolsSessionHostAssociatedPtrInfo host_ptr_info,
+      mojom::blink::DevToolsSessionAssociatedRequest main_request,
+      mojom::blink::DevToolsSessionRequest io_request,
       mojom::blink::DevToolsSessionStatePtr reattach_session_state);
-  ~InspectorSession() override;
-  // TODO(dgozman): remove session id once WokrerInspectorController
-  // does not use it anymore.
-  int SessionId() { return session_id_; }
+  ~DevToolsSession() override;
+
+  void ConnectToV8(v8_inspector::V8Inspector*, int context_group_id);
   v8_inspector::V8InspectorSession* V8Session() { return v8_session_.get(); }
 
   void Append(InspectorAgent*);
-  void Restore();
-  void Dispose();
-  void DidStartProvisionalLoad(LocalFrame*);
-  void DidFailProvisionalLoad(LocalFrame*);
-  void DidCommitLoadForLocalFrame(LocalFrame*);
-  void DispatchProtocolMessage(int call_id,
-                               const String& method,
-                               const String& message);
-  void flushProtocolNotifications() override;
-
+  void Detach();
+  void FlushProtocolNotifications();
   void Trace(blink::Visitor*);
 
-  static bool ShouldInterruptForMethod(const String& method);
+  // Core probes.
+  void DidStartProvisionalLoad(LocalFrame*);
+  void DidFailProvisionalLoad(LocalFrame*);
+  void DidCommitLoad(LocalFrame*, DocumentLoader*);
 
  private:
+  class IOSession;
+
+  // mojom::blink::DevToolsSession implementation.
+  void DispatchProtocolCommand(int call_id,
+                               const String& method,
+                               const String& message) override;
+
   // protocol::FrontendChannel implementation.
   void sendProtocolResponse(
       int call_id,
       std::unique_ptr<protocol::Serializable> message) override;
   void sendProtocolNotification(
       std::unique_ptr<protocol::Serializable> message) override;
+  void fallThrough(int call_id,
+                   const String& method,
+                   const String& message) override;
+  void flushProtocolNotifications() override;
 
   // v8_inspector::V8Inspector::Channel implementation.
   void sendResponse(
@@ -86,17 +77,14 @@ class CORE_EXPORT InspectorSession
   void sendNotification(
       std::unique_ptr<v8_inspector::StringBuffer> message) override;
 
+  bool IsDetached();
   void SendProtocolResponse(int call_id, const String& message);
-  void fallThrough(int call_id,
-                   const String& method,
-                   const String& message) override;
 
-  Client* client_;
+  Member<DevToolsAgent> agent_;
+  mojo::AssociatedBinding<mojom::blink::DevToolsSession> binding_;
+  mojom::blink::DevToolsSessionHostAssociatedPtr host_ptr_;
+  IOSession* io_session_;
   std::unique_ptr<v8_inspector::V8InspectorSession> v8_session_;
-  int session_id_;
-  bool disposed_;
-  Member<CoreProbeSink> instrumenting_agents_;
-  Member<InspectedFrames> inspected_frames_;
   std::unique_ptr<protocol::UberDispatcher> inspector_backend_dispatcher_;
   InspectorSessionState session_state_;
   HeapVector<Member<InspectorAgent>> agents_;
@@ -105,9 +93,9 @@ class CORE_EXPORT InspectorSession
   InspectorAgentState v8_session_state_;
   InspectorAgentState::String v8_session_state_json_;
 
-  DISALLOW_COPY_AND_ASSIGN(InspectorSession);
+  DISALLOW_COPY_AND_ASSIGN(DevToolsSession);
 };
 
 }  // namespace blink
 
-#endif  // !defined(InspectorSession_h)
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_INSPECTOR_DEVTOOLS_SESSION_H_
