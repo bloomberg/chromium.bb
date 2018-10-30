0b7e7a9b76ced99be132bde547b744d60053c1c5
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2018-10-30 04:35:00 +0000

    Unified: Set tooltip for Notifications count or dnd icon.
    This change updates tooltip for the latest design spec.
    The tooltips describe either the number of notifications or being in
    DnD mode, so that these should work well for a11y label as well.
    
    Bug: 888072
    Change-Id: Ifed382eca722c6948cf14a9f834ad0bb314c1629
    Reviewed-on: https://chromium-review.googlesource.com/c/1306935
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#603784}
    Former-commit-id: 90f0b74f75f8f750fc9d74d62cf1a3dfb6041b06

--- ash/system/unified/notification_counter_view.cc
+++ ash/system/unified/notification_counter_view.cc
@@ -92,8 +92,6 @@ class NumberIconImageSource : public gfx::CanvasImageSource {
 NotificationCounterView::NotificationCounterView(Shelf* shelf)
     : TrayItemView(shelf) {
   CreateImageView();
-  image_view()->set_tooltip_text(
-      l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_NOTIFICATIONS_LABEL));
   SetVisible(false);
   Shell::Get()->session_controller()->AddObserver(this);
 }
@@ -116,6 +114,8 @@ void NotificationCounterView::Update() {
         gfx::CanvasImageSource::MakeImageSkia<NumberIconImageSource>(icon_id));
     count_for_display_ = icon_id;
   }
+  image_view()->set_tooltip_text(l10n_util::GetPluralStringFUTF16(
+      IDS_ASH_STATUS_TRAY_NOTIFICATIONS_COUNT_TOOLTIP, notification_count));
   SetVisible(true);
 }
 
@@ -126,6 +126,8 @@ void NotificationCounterView::OnSessionStateChanged(
 
 QuietModeView::QuietModeView(Shelf* shelf) : TrayItemView(shelf) {
   CreateImageView();
+  image_view()->set_tooltip_text(
+      l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_QUIET_MODE_TOOLTIP));
   SetVisible(false);
   Shell::Get()->session_controller()->AddObserver(this);
 }
