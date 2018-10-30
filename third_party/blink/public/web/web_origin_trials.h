caa1e71c419786c66eda9b3ba6bccb2df38548e9
Findit <findit-for-me@appspot.gserviceaccount.com>
2018-10-30 04:06:26 +0000

    Revert "Convert WebOriginTrials namepsace to a static-only class"
    This reverts commit 47da3d444aa559b4b44b89cec38e9e868d643572 [formerly 133164fcf4927dbe47ab2003758c6d38bab8192b].
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 603776 as the
    culprit for failures in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzEzMzE2NGZjZjQ5MjdkYmU0N2FiMjAwMzc1OGM2ZDM4YmFiODE5MmIM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.linux/Linux%20Builder%20%28dbg%29/133426
    
    Sample Failed Step: compile
    
    Original change's description:
    > Convert WebOriginTrials namepsace to a static-only class
    > 
    > https://www.chromium.org/blink/public-c-api#TOC-Naming
    > > All public Blink API types and classes are in the namespace blink and
    > have the prefix Web.
    > Namespace name should be lower-case but we can't make 'namespace
    > WebOriginTrials' lower-case because of this naming rule. So we convert
    > WebOriginTrials to a static-only class.
    > 
    > Bug: 889726
    > Change-Id: Ia2fe9888da297b965d891287f05fb99d45d802f5
    > Reviewed-on: https://chromium-review.googlesource.com/c/1304185
    > Commit-Queue: Zhuoyu Qian <zhuoyu.qian@samsung.com>
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#603776}
    
    Change-Id: I121b4217ed4132d9f739c400950b57b6746e70f4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 889726
    Reviewed-on: https://chromium-review.googlesource.com/c/1307064
    Cr-Commit-Position: refs/heads/master@{#603779}
    Former-commit-id: 01e9218ba361ace1a509f87c1c266c0071f768c4

--- third_party/blink/public/web/web_origin_trials.h
+++ third_party/blink/public/web/web_origin_trials.h
@@ -5,17 +5,20 @@
 #ifndef THIRD_PARTY_BLINK_PUBLIC_WEB_WEB_ORIGIN_TRIALS_H_
 #define THIRD_PARTY_BLINK_PUBLIC_WEB_WEB_ORIGIN_TRIALS_H_
 
+#include "third_party/blink/renderer/core/core_export.h"
+
 namespace blink {
 
 class WebDocument;
 class WebString;
 
-// A class with tests for experimental features which can be enabled by the
+// A namespace with tests for experimental features which can be enabled by the
 // origin trials framework via origin trial tokens.
-class WebOriginTrials {
- public:
-  static bool isTrialEnabled(const WebDocument*, const WebString&);
-};
+namespace WebOriginTrials {
+
+CORE_EXPORT bool isTrialEnabled(const WebDocument*, const WebString&);
+
+}  // namespace WebOriginTrials
 
 }  // namespace blink
 
