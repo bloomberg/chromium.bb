51aee1a971887d7db13acdc42bdee557d6ec91e0
Fernando Serboncini <fserb@chromium.org>
2018-10-31 22:25:41 +0000

    Fix *StaticBitmapImage ThreadChecker and unaccelerated SkImage destroy
    - AcceleratedStaticBitmapImage was misusing ThreadChecker by having its
    own detach logic. Using proper DetachThread is simpler, cleaner and
    correct.
    - UnacceleratedStaticBitmapImage didn't destroy the SkImage in the
    proper thread, leading to GrContext/SkSp problems.
    
    Bug: 890576
    Change-Id: Ic71e7f7322b0b851774628247aa5256664bc0723
    Reviewed-on: https://chromium-review.googlesource.com/c/1307775
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604427}
    Former-commit-id: 78d89fe556cb5dabbc47b4967cdf55e607e29580

--- third_party/blink/renderer/platform/graphics/unaccelerated_static_bitmap_image.h
+++ third_party/blink/renderer/platform/graphics/unaccelerated_static_bitmap_image.h
@@ -6,7 +6,10 @@
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_UNACCELERATED_STATIC_BITMAP_IMAGE_H_
 
 #include "base/memory/weak_ptr.h"
+#include "base/single_thread_task_runner.h"
+#include "base/threading/thread_checker.h"
 #include "third_party/blink/renderer/platform/graphics/static_bitmap_image.h"
+#include "third_party/blink/renderer/platform/scheduler/public/thread.h"
 
 namespace blink {
 
@@ -34,11 +37,17 @@ class PLATFORM_EXPORT UnacceleratedStaticBitmapImage final
 
   PaintImage PaintImageForCurrentFrame() override;
 
+  void Transfer() final;
+
  private:
   UnacceleratedStaticBitmapImage(sk_sp<SkImage>);
   UnacceleratedStaticBitmapImage(PaintImage);
 
   PaintImage paint_image_;
+  THREAD_CHECKER(thread_checker_);
+
+  sk_sp<SkImage> original_skia_image_;
+  scoped_refptr<base::SingleThreadTaskRunner> original_skia_image_task_runner_;
 };
 
 }  // namespace blink
