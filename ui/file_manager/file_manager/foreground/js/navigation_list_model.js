abd6cb363277d36b017c0c0ed167e248bfd18273
Luciano Pacheco <lucmult@chromium.org>
2018-10-31 10:11:11 +0000

    Revert "Change NavigationListModel to support MyFiles as Volume."
    This reverts commit bc6793fd21e4aa284486f135c844f36611a07b64 [formerly 2903236d2af38fd7260f3d9369ffe58e6a09ed0a].
    
    Reason for revert: ChromiumOS MSAN got flaky/failing crbug.com/899664
    
    Original change's description:
    > Change NavigationListModel to support MyFiles as Volume.
    > 
    > Add MYFILES_VOLUME_ENABLED in private strings to be able to have the
    > feature flag in the JS/UI code.
    > 
    > Change VolumeManager to use MyFiles folder for Downloads volume, the
    > method |GetMyFilesFolderForProfile| takes care of returning the right
    > value based on the feature flag MyFilesVolume.
    > 
    > Change NavigationListModel to support MyFiles as Volume when
    > MyFilesVolume feature flag is enabled:
    > 1. MyFiles volume is the Downloads volume (after the feature flag is
    > removed we can rename it accordingly). Setup Downloads volume as
    > VolumeEntry,
    > 2. Create a NavigationModelFakeItem with "My files" label, based on the
    > VolumeEntry (step #1).
    > 3. Skip adding "Downloads" to Myfiles, since now MyFiles is in fact
    > Downloads volume.
    > 4. Continue to add Linux and Play files to Myfiles.
    > 
    > Test: unit_tests --gtest_filter='VolumeManagerTest.GetVolumeListMyFilesVolume' and browser_test --gtest_filter='FileManagerJsTest.DirectoryTreeTest:FileManagerJsTest.NavigationList*'
    > Bug: 873539
    > Change-Id: Ia242d52a1e4d7b4fb3c7ca219d9cfdc4fce72543
    > Reviewed-on: https://chromium-review.googlesource.com/c/1308235
    > Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    > Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#604143}
    
    TBR=joelhockey@chromium.org,lucmult@chromium.org,ioanap@chromium.org
    
    Change-Id: I0c5b1fd5f23b2ec2b56ab13ab93a2d3a1ffd277a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 873539
    Reviewed-on: https://chromium-review.googlesource.com/c/1309569
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604193}
    Former-commit-id: a72b9407282281bb0d94a1e3a4cba57050127471

--- ui/file_manager/file_manager/foreground/js/navigation_list_model.js
+++ ui/file_manager/file_manager/foreground/js/navigation_list_model.js
@@ -187,16 +187,6 @@ function NavigationListModel(
   this.myFilesModel_ = null;
 
   /**
-   * True when MyFiles should be a volume and Downloads just a plain folder
-   * inside it. When false MyFiles is an EntryList, which means UI only type,
-   * which contains Downloads as a child volume.
-   * @private {boolean}
-   */
-  this.myFilesVolumeEnabled_ =
-      loadTimeData.valueExists('MY_FILES_VOLUME_ENABLED') &&
-      loadTimeData.getBoolean('MY_FILES_VOLUME_ENABLED');
-
-  /**
    * All root navigation items in display order.
    * @private {!Array<!NavigationModelItem>}
    */
@@ -497,29 +487,12 @@ NavigationListModel.prototype.orderAndNestItems_ = function() {
 
   let myFilesEntry, myFilesModel;
   if (!this.myFilesModel_) {
-    if (this.myFilesVolumeEnabled_) {
-      // When MyFilesVolume is enabled we use the Downloads volume to be the
-      // MyFiles volume.
-      const myFilesVolumeModel =
-          getSingleVolume(VolumeManagerCommon.VolumeType.DOWNLOADS);
-      if (myFilesVolumeModel) {
-        myFilesEntry = new VolumeEntry(myFilesVolumeModel.volumeInfo);
-        myFilesModel = new NavigationModelFakeItem(
-            str('MY_FILES_ROOT_LABEL'), NavigationModelItemType.ENTRY_LIST,
-            myFilesEntry);
-        this.myFilesModel_ = myFilesModel;
-      }
-    } else {
-      // Here is the initial version for MyFiles, which is only an entry in JS
-      // to be displayed in the DirectoryTree, cotaining Downloads, Linux and
-      // Play files volumes.
-      myFilesEntry = new EntryList(
-          str('MY_FILES_ROOT_LABEL'), VolumeManagerCommon.RootType.MY_FILES);
-      myFilesModel = new NavigationModelFakeItem(
-          myFilesEntry.label, NavigationModelItemType.ENTRY_LIST, myFilesEntry);
-      myFilesModel.section = NavigationSection.MY_FILES;
-      this.myFilesModel_ = myFilesModel;
-    }
+    myFilesEntry = new EntryList(
+        str('MY_FILES_ROOT_LABEL'), VolumeManagerCommon.RootType.MY_FILES);
+    myFilesModel = new NavigationModelFakeItem(
+        myFilesEntry.label, NavigationModelItemType.ENTRY_LIST, myFilesEntry);
+    myFilesModel.section = NavigationSection.MY_FILES;
+    this.myFilesModel_ = myFilesModel;
   } else {
     myFilesEntry = this.myFilesModel_.entry;
     myFilesModel = this.myFilesModel_;
@@ -527,18 +500,15 @@ NavigationListModel.prototype.orderAndNestItems_ = function() {
   this.navigationItems_.push(myFilesModel);
 
   // Add Downloads to My Files.
-  if (!this.myFilesVolumeEnabled_) {
-    const downloadsVolume =
-        getSingleVolume(VolumeManagerCommon.VolumeType.DOWNLOADS);
-    if (downloadsVolume) {
-      // Only add volume if MyFiles doesn't have it yet.
-      if (myFilesEntry.findIndexByVolumeInfo(downloadsVolume.volumeInfo) ===
-          -1) {
-        myFilesEntry.addEntry(new VolumeEntry(downloadsVolume.volumeInfo));
-      }
-    } else {
-      myFilesEntry.removeByVolumeType(VolumeManagerCommon.VolumeType.DOWNLOADS);
+  const downloadsVolume =
+      getSingleVolume(VolumeManagerCommon.VolumeType.DOWNLOADS);
+  if (downloadsVolume) {
+    // Only add volume if MyFiles doesn't have it yet.
+    if (myFilesEntry.findIndexByVolumeInfo(downloadsVolume.volumeInfo) === -1) {
+      myFilesEntry.addEntry(new VolumeEntry(downloadsVolume.volumeInfo));
     }
+  } else {
+    myFilesEntry.removeByVolumeType(VolumeManagerCommon.VolumeType.DOWNLOADS);
   }
 
   // Add Android to My Files.
