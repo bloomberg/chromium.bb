4de427f5491cea7bb976255f7b08c86d2d6b84a6
Jared Saul <jsaul@google.com>
2018-10-31 16:15:55 +0000

    [Autofill] Remove dead AutofillScanCardholderName feature
    This is a redo of
    https://chromium-review.googlesource.com/c/chromium/src/+/1300794.
    That CL was reverted because there were still internal Android
    dependencies that I originally missed, which I resolved earlier today.
    
    Bug: 881343
    Change-Id: I99b87e859c0fc147f1c8343f5170d93cad066f7d
    Reviewed-on: https://chromium-review.googlesource.com/c/1308607
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/master@{#604279}
    Former-commit-id: 6c094837a0c2107d20e6902e86cb8d45f80d124b

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeFeatureList.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeFeatureList.java
@@ -158,7 +158,6 @@ public abstract class ChromeFeatureList {
     public static final String AUTOFILL_ASSISTANT = "AutofillAssistant";
     public static final String AUTOFILL_REFRESH_STYLE_ANDROID = "AutofillRefreshStyleAndroid";
     public static final String AUTOFILL_KEYBOARD_ACCESSORY = "AutofillKeyboardAccessory";
-    public static final String AUTOFILL_SCAN_CARDHOLDER_NAME = "AutofillScanCardholderName";
     public static final String CAF_MEDIA_ROUTER_IMPL = "CafMediaRouterImpl";
     public static final String CAPTIVE_PORTAL_CERTIFICATE_LIST = "CaptivePortalCertificateList";
     public static final String CCT_BACKGROUND_TAB = "CCTBackgroundTab";
