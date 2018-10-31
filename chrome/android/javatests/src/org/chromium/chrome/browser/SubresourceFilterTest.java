05fac210018c086c3df0f3ba5720c105e01feaa4
Ioana Pandele <ioanap@chromium.org>
2018-10-31 14:35:44 +0000

    Disable failing testcases in SubresourceFilterTest
    These have starting flaking heavliy on Android CFI.
    
    TBR=csharrison@chromium.org
    
    Bug: 899903
    Change-Id: Iab67f7421ccf576c5e343f56eae333e0adbcae12
    Reviewed-on: https://chromium-review.googlesource.com/c/1309745
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604245}
    Former-commit-id: f4c1c0e72255229896f44aad54a3a309e2a5aa3a

--- chrome/android/javatests/src/org/chromium/chrome/browser/SubresourceFilterTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/SubresourceFilterTest.java
@@ -17,6 +17,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.infobar.AdsBlockedInfoBar;
 import org.chromium.chrome.browser.infobar.InfoBar;
 import org.chromium.chrome.browser.infobar.InfoBarContainer;
@@ -103,6 +104,7 @@ public final class SubresourceFilterTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/899903")
     public void resourceFilteredClose() throws Exception {
         String url = mTestServer.getURL(PAGE_WITH_JPG);
         MockSafeBrowsingApiHandler.addMockResponse(url, METADATA_FOR_ENFORCEMENT);
@@ -130,6 +132,7 @@ public final class SubresourceFilterTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/899903")
     public void resourceFilteredClickLearnMore() throws Exception {
         String url = mTestServer.getURL(PAGE_WITH_JPG);
         MockSafeBrowsingApiHandler.addMockResponse(url, METADATA_FOR_ENFORCEMENT);
@@ -170,6 +173,7 @@ public final class SubresourceFilterTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/899903")
     public void resourceFilteredReload() throws Exception {
         String url = mTestServer.getURL(PAGE_WITH_JPG);
         MockSafeBrowsingApiHandler.addMockResponse(url, METADATA_FOR_ENFORCEMENT);
@@ -201,6 +205,7 @@ public final class SubresourceFilterTest {
     }
 
     @Test
+    @DisabledTest(message = "crbug.com/899903")
     @MediumTest
     public void resourceNotFilteredWithWarning() throws Exception {
         String url = mTestServer.getURL(PAGE_WITH_JPG);
