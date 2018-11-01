c16e4c4c18e3837081e137896977fa127ca96fb7
Jacob Dufault <jdufault@google.com>
2018-11-01 12:20:01 -0700

    chromeos_config: Mark tricky-chrome-pfq as experimental
    BUG=chromium:901046
    TEST=inspect generated output
    
    Change-Id: I730fdafcf783a40204d58b7b7cb3ae903616091c
    Reviewed-on: https://chromium-review.googlesource.com/1313288
    Commit-Ready: Jacob Dufault <jdufault@chromium.org>
    Tested-by: Jacob Dufault <jdufault@chromium.org>
    Reviewed-by: Don Garrett <dgarrett@chromium.org>
    Reviewed-by: Alex Zamorzaev <zamorzaev@chromium.org>

--- config/chromeos_config.py
+++ config/chromeos_config.py
@@ -2691,7 +2691,6 @@ def ChromePfqBuilders(site_config, boards_dict, ge_build_config):
       'peppy',
       'reef',
       'terra',
-      'tricky',
       'veyron_minnie',
       'veyron_rialto',
   ])
@@ -2701,6 +2700,7 @@ def ChromePfqBuilders(site_config, boards_dict, ge_build_config):
       'hana',
       'nyan_big',
       'scarlet',
+      'tricky',  # crbug.com/901046
   ])
 
   _chrome_pfq_skylab_boards = frozenset([
