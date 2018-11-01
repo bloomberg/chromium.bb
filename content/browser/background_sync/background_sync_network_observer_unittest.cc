1e73a1359a0e6c383404a7912a4cc090e0a1aa9a
Josh Karlin <jkarlin@chromium.org>
2018-11-01 20:14:59 +0000

    [BackgroundSync] BGSync missing its initial connection type request
    After the switch to NetworkConnectionTracker, the GetConnectionType
    call wasn't checking for a synchronous return. In the case of a
    synchronous return OnConnectionChanged needs to be called. This led to
    BackgroundSyncManager not getting the correct connection type on
    startup.
    
    Bug: 900943
    Change-Id: I21ffcacdea9cca4c0780f40561e55b10fb9866b1
    Reviewed-on: https://chromium-review.googlesource.com/c/1312676
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604689}
    Former-commit-id: bf943695d0c24fed3f7a1b90566e66b5a8482c36

--- content/browser/background_sync/background_sync_network_observer_unittest.cc
+++ content/browser/background_sync/background_sync_network_observer_unittest.cc
@@ -12,7 +12,7 @@
 namespace content {
 
 class BackgroundSyncNetworkObserverTest : public testing::Test {
- protected:
+ public:
   BackgroundSyncNetworkObserverTest() : network_changed_count_(0) {
     network_observer_ =
         std::make_unique<BackgroundSyncNetworkObserver>(base::BindRepeating(
@@ -28,6 +28,7 @@ class BackgroundSyncNetworkObserverTest : public testing::Test {
 
   void OnConnectionChanged() { network_changed_count_++; }
 
+ protected:
   TestBrowserThreadBundle browser_thread_bundle_;
 
   std::unique_ptr<BackgroundSyncNetworkObserver> network_observer_;
@@ -102,6 +103,24 @@ TEST_F(BackgroundSyncNetworkObserverTest, ConditionsMetOnline) {
 
   SetNetwork(network::mojom::ConnectionType::CONNECTION_NONE);
   EXPECT_FALSE(network_observer_->NetworkSufficient(NETWORK_STATE_ONLINE));
+}
+
+TEST_F(BackgroundSyncNetworkObserverTest, GetNetworkOnConstruction) {
+  // We need to emulate being disconnected before creating the network observer.
+  network_observer_.reset();
+  SetNetwork(network::mojom::ConnectionType::CONNECTION_NONE);
+
+  auto observer =
+      std::make_unique<BackgroundSyncNetworkObserver>(base::BindRepeating(
+          &BackgroundSyncNetworkObserverTest::OnConnectionChanged,
+          base::Unretained(this)));
+
+  base::RunLoop().RunUntilIdle();
+
+  // The network observer should have learned that the current network is not
+  // the default (UNKNOWN) but is in fact NONE.
+  EXPECT_EQ(1, network_changed_count_);
+  EXPECT_FALSE(observer->NetworkSufficient(NETWORK_STATE_ONLINE));
 }
 
 }  // namespace content
