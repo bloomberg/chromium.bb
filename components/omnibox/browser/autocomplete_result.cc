064e51a03f8cdd202973de911a64a808b149961d
manuk hovanesian <manukh@chromium.org>
2018-11-01 16:21:48 +0000

    Revert "[Omnibox] Use GURL::possibly_invalid_spec instead of GURL::spec when sorting and deduping matches."
    This reverts commit db8d14f2a9a78b7bd3f0974a3e824f0a81f6a013 [formerly 61d1146d76ddba79ed29fa8ad368494f930cca2d].
    
    Reason for revert: per review comments, there may be a larger underlying reason as to why we we're generating invalid GURL's that should be addressed.
    
    Original change's description:
    > [Omnibox] Use GURL::possibly_invalid_spec instead of GURL::spec when sorting and deduping matches.
    > 
    > Certain omnibox inputs, such as `http:www.` produce matches with invalid urls. Invoking GURL::spec on these invalid GURL's caused a DCHECK to evaluate false and crash the browser. The crash only affects debug builds as the DCHECK is inactive for non-debug builds.
    > 
    > Bug: 898554
    > Change-Id: I89a27f902b77bf6b221c321ab396787e9249a4e6
    > Reviewed-on: https://chromium-review.googlesource.com/c/1308260
    > Commit-Queue: manuk hovanesian <manukh@chromium.org>
    > Reviewed-by: Tommy Li <tommycli@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#604012}
    
    TBR=krb@chromium.org,tommycli@chromium.org,manukh@chromium.org
    
    Change-Id: I3e2dddb8dd218bce805dc93d842f83b7366cf1bb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 898554
    Reviewed-on: https://chromium-review.googlesource.com/c/1308603
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604610}
    Former-commit-id: b5b111f70b97a3a60c14d1c45845a1f12a9da0ad

--- components/omnibox/browser/autocomplete_result.cc
+++ components/omnibox/browser/autocomplete_result.cc
@@ -35,9 +35,7 @@ struct MatchGURLHash {
   // The |bool| is whether the match is a calculator suggestion. We want them
   // compare differently against other matches with the same URL.
   size_t operator()(const std::pair<GURL, bool>& p) const {
-    // Certain omnibox inputs such as `http:www.` produce matches with invalid
-    // urls, so we use GURL::possibly_invalid_spec.
-    return std::hash<std::string>()(p.first.possibly_invalid_spec()) + p.second;
+    return std::hash<std::string>()(p.first.spec()) + p.second;
   }
 };
 
