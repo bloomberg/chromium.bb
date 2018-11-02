de2564fd1455639fcd97927b931962c40100efaa
Daniel Cheng <dcheng@chromium.org>
2018-11-02 23:22:05 +0000

    Fix misnamed files for mojo::EnumTrait specializations.
    Change-Id: Iece94f3a3b8d94cf0bf12b07967bfd34d11a5fd0
    Reviewed-on: https://chromium-review.googlesource.com/c/1315691
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605089}
    Former-commit-id: e3bd5c131bb1a353561dbcc2d9299ee69043485d

--- third_party/blink/common/manifest/manifest_mojom_traits.cc
+++ third_party/blink/common/manifest/manifest_mojom_traits.cc
@@ -6,7 +6,7 @@
 
 #include "mojo/public/cpp/base/string16_mojom_traits.h"
 #include "third_party/blink/public/common/manifest/web_display_mode_mojom_traits.h"
-#include "third_party/blink/public/common/screen_orientation/web_screen_orientation_enum_traits.h"
+#include "third_party/blink/public/common/screen_orientation/web_screen_orientation_mojom_traits.h"
 #include "ui/gfx/geometry/mojo/geometry_struct_traits.h"
 #include "url/mojom/url_gurl_mojom_traits.h"
 
