fa904519776be98ae8b086d228ef74408d903e54
Keishi Hattori <keishi@chromium.org>
2018-11-02 10:06:11 +0000

    Oilpan: Backing slots are not registered for some weak HashTables
    Weak HashTable's backing slots went unregistered, when it gains a backing after it has been traced.
    We need to register the backing slot from HashTable::Trace even when there is no backing yet.
    
    Bug: 864425
    Change-Id: I09a0f6031e457242a4374429dde5360777bebc21
    Reviewed-on: https://chromium-review.googlesource.com/c/1312189
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604875}
    Former-commit-id: d7dbf77305b6467e1a2b905f72720398a20faa1e

--- third_party/blink/renderer/platform/heap/visitor.h
+++ third_party/blink/renderer/platform/heap/visitor.h
@@ -134,8 +134,6 @@ class PLATFORM_EXPORT Visitor {
     static_assert(IsGarbageCollectedType<T>::value,
                   "T needs to be a garbage collected object");
 
-    if (!backing_store)
-      return;
     VisitBackingStoreWeakly(reinterpret_cast<void*>(backing_store),
                             reinterpret_cast<void**>(backing_store_slot),
                             TraceTrait<T>::GetTraceDescriptor(
