d99ba1fc22eed075136c4ed8a1cc602a1e12a435
Xiaocheng Hu <xiaochengh@chromium.org>
2018-11-02 03:37:00 +0000

    Expose noncanonical versions of Previous/NextWordPosition
    This patch exposes noncanonical versions of Previous/NextWordPosition to
    reduce usage of VisiblePosition.
    
    Bug: 657237
    Change-Id: I0815b0c0bf4de34427432ca8f6aa769c356c6385
    Reviewed-on: https://chromium-review.googlesource.com/c/1313876
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#604832}
    Former-commit-id: 28c5a3cf590bad18175f6fadfe2efb6499b69889

--- third_party/blink/renderer/core/editing/visible_units.h
+++ third_party/blink/renderer/core/editing/visible_units.h
@@ -133,7 +133,10 @@ EndOfWordPosition(const VisiblePositionInFlatTree&,
                   EWordSide = kNextWordIfOnBoundary);
 CORE_EXPORT VisiblePositionInFlatTree
 EndOfWord(const VisiblePositionInFlatTree&, EWordSide = kNextWordIfOnBoundary);
+// TODO(crbug.com/657237): Change all callers to use non-canonical versions.
+CORE_EXPORT PositionWithAffinity PreviousWordPosition(const Position&);
 CORE_EXPORT VisiblePosition PreviousWordPosition(const VisiblePosition&);
+CORE_EXPORT PositionWithAffinity NextWordPosition(const Position&);
 CORE_EXPORT VisiblePosition NextWordPosition(const VisiblePosition&);
 
 // sentences
