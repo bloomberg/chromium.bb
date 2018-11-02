0c0a6a0a25de31161ddd07f8e446c4e9681ab9fe
Hugo Holgersson <hugoh@vewd.com>
2018-11-02 17:01:10 +0000

    Snav: Convert all snav*iframe*.html tests to snav-testharness.js
    The changes to snav-testharness.js lets us assert navigations
    in and out of iframes:
    
     1) To be able to test focus movements in and out of iframes,
        we need to listen for 'keyup'-events on the document where
        focus is _about_to_go_.
    
        'keyup' targets the currently focused document. When 'keyup'
        comes, 'keydown' has already changed focus to the new document.
    
     2) Previously each movement had its own async_test(). Now we use
        one async_test() with many "test steps": one for each movement.
        This helps us avoid tricky (mis)usage of testharness.js's
        fetch_tests_from_window().
    
    The changes to the HTML files are mostly mechanical except that
    I gave some elements new or renamed ids so they become easier
    to distinguish when debugging.
    
    Once we've fixed Issue 801162, the tests' expectations will change:
    We want spatnav to first _focus_ the iframe (before scrolling it
    or searching inside of it).
    
    Bug: 803086, 891658, 801162
    Change-Id: Ib53bb224398510b5744927a5d892381870fa4690
    Reviewed-on: https://chromium-review.googlesource.com/c/1314469
    Commit-Queue: Hugo Holgersson <hugoh@vewd.com>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#604962}
    Former-commit-id: a417a16cbf123394a09dc3ee985591da1b5301ed

--- third_party/WebKit/LayoutTests/fast/spatial-navigation/resources/snav-testharness.js
+++ third_party/WebKit/LayoutTests/fast/spatial-navigation/resources/snav-testharness.js
@@ -1,6 +1,11 @@
 (function() {
   "use strict";
 
+  let gAsyncTest;
+
+  // TODO: Use WebDriver's API instead of eventSender.
+  //       Hopefully something like:
+  //         test_driver.move_focus(direction).then(...)
   function triggerMove(direction) {
     switch (direction) {
       case 'Up':
@@ -18,33 +23,60 @@
     }
   }
 
-  function stepAndAssertMoves(expectedMoves, focusMoves) {
-    if (expectedMoves.length == 0)
+  function focusedDocument(currentDocument=document) {
+    let subframes = Array.from(currentDocument.defaultView.frames);
+    for (let frame of subframes) {
+      let focused = focusedDocument(frame.document);
+      if (focused)
+        return focused;
+    }
+    if (currentDocument.hasFocus())
+      return currentDocument;
+    return null;
+  }
+
+  // Allows us to query element ids also in iframes' documents.
+  function findElement(searchString) {
+    let searchPath = searchString.split(",");
+    let currentDocument = document;
+    let elem = undefined;
+
+    while (searchPath.length > 0) {
+      let id = searchPath.shift();
+      elem = currentDocument.getElementById(id);
+      assert_not_equals(elem, null, 'Could not find "' + searchString + '",');
+
+      if (elem.tagName == "IFRAME")
+        currentDocument = elem.contentDocument;
+    }
+    return elem;
+  }
+
+  function stepAndAssertMoves(expectedMoves) {
+    if (expectedMoves.length == 0) {
+      gAsyncTest.done();
       return;
+    }
 
     let move = expectedMoves.shift();
     let direction = move[0];
     let expectedId = move[1];
-    // TODO: Use WebDriver's API instead of eventSender.
-    //       Hopefully something like:
-    //         test_driver.move_focus(direction).then(...)
-    async_test(t => {
-      let checkFocus = t.step_func_done(function() {
-        let expectedElement = document.getElementById(expectedId);
-        assert_equals(document.activeElement, expectedElement);
+    let wanted = findElement(expectedId);
+    let receivingDoc = wanted.ownerDocument;
+    let verifyAndAdvance = gAsyncTest.step_func(function() {
+      let focused = focusedDocument().activeElement;
+      assert_equals(focused, wanted);
+      // Kick off another async test step.
+      stepAndAssertMoves(expectedMoves);
+    });
 
-        // Kick off another async test before closing this one.
-        stepAndAssertMoves(expectedMoves, focusMoves + 1);
-      });
-
-      // "... the default action MUST be to shift the document focus".
-      //   https://www.w3.org/TR/uievents/#event-type-keydown
-      // Any focus movement must have happened during keydown, so once we got
-      // the succeeding keyup-event, it's safe to assert activeElement.
-      document.addEventListener('keyup', checkFocus, {once: true});
-      triggerMove(direction);
-    }, focusMoves + "th move: " + direction + '-key moves focus to ' +
-       'the element with id: ' + expectedId + '.');
+    // "... the default action MUST be to shift the document focus".
+    //   https://www.w3.org/TR/uievents/#event-type-keydown
+    // Any focus movement must have happened during keydown, so once we got
+    // the succeeding keyup-event, it's safe to assert activeElement.
+    // The keyup-event targets the, perhaps newly, focused document.
+    receivingDoc.addEventListener('keyup', verifyAndAdvance, {once: true});
+    triggerMove(direction);
   }
 
   // TODO: Port all old spatial navigation layout tests to this method.
@@ -59,7 +91,13 @@
 
     assertFocusMoves: function(expectedMoves) {
       snav.assertSnavEnabledAndTestable();
-      stepAndAssertMoves(expectedMoves, 0);
+      gAsyncTest = async_test("Focus movements:\n" +
+          JSON.stringify(expectedMoves).replace(/],/g, ']\n') + '\n');
+
+      // All iframes must be loaded before trying to navigate to them.
+      window.addEventListener('load', gAsyncTest.step_func(() => {
+        stepAndAssertMoves(expectedMoves);
+      }));
     }
   }
 })();
