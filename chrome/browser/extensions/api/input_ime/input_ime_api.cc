22e480625a0c75ed42d501a9b192cba22b17cae0
Shu Chen <shuchen@google.com>
2018-11-05 09:00:58 +0000

    Fix the regression caused by http://crrev.com/c/1288350.
    Bug: 900124,856135
    Change-Id: Ie11ad406bd1ea383dc2a83cc8661076309154865
    Reviewed-on: https://chromium-review.googlesource.com/c/1317010
    Reviewed-by: Lan Wei <azurewei@chromium.org>
    Commit-Queue: Shu Chen <shuchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605282}
    Former-commit-id: 2f0b419df243400f954e11b649f4862a1e0ff367

--- chrome/browser/extensions/api/input_ime/input_ime_api.cc
+++ chrome/browser/extensions/api/input_ime/input_ime_api.cc
@@ -275,9 +275,18 @@ InputImeEventRouterFactory::~InputImeEventRouterFactory() {
 InputImeEventRouter* InputImeEventRouterFactory::GetRouter(Profile* profile) {
   if (!profile)
     return nullptr;
+  // The |router_map_| is keyed by the original profile.
+  // Refers to the comments in |RemoveProfile| method for the reason.
+  profile = profile->GetOriginalProfile();
   InputImeEventRouter* router = router_map_[profile];
   if (!router) {
-    router = new InputImeEventRouter(profile);
+    // The router must attach to the profile from which the extension can
+    // receive events. If |profile| has an off-the-record profile, attach the
+    // off-the-record profile. e.g. In guest mode, the extension is running with
+    // the incognito profile instead of its original profile.
+    router = new InputImeEventRouter(profile->HasOffTheRecordProfile()
+                                         ? profile->GetOffTheRecordProfile()
+                                         : profile);
     router_map_[profile] = router;
   }
   return router;
@@ -450,7 +459,7 @@ InputImeEventRouter* GetInputImeEventRouter(Profile* profile) {
   if (!profile)
     return nullptr;
   return extensions::InputImeEventRouterFactory::GetInstance()->GetRouter(
-      profile->GetOriginalProfile());
+      profile);
 }
 
 }  // namespace extensions
