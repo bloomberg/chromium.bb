767d07587d9ca14e684925f5107169c584efe215
Andrew Grieve <agrieve@chromium.org>
2018-11-05 15:54:52 +0000

    Android: Switch ContentShell to use implicit JNI registration
    Explicit registration is needed only when using the custom linker.
    Content shell doesn't use the custom linker, so has no need to jump
    through the extra hoops involved with explicit registration.
    
    Change motivated by JNI refactorings, which are made simpler by having
    fewer targets use manual JNI registration.
    
    Bug: 898261
    Change-Id: Ia38529c1df92264781ba64235a7aea749ef0356a
    Reviewed-on: https://chromium-review.googlesource.com/c/1317338
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605344}
    Former-commit-id: ce2ed51a2723d95dc3a0289961f9ae95568d51fa

--- content/shell/android/shell_test_library_loader.cc
+++ content/shell/android/shell_test_library_loader.cc
@@ -3,26 +3,14 @@
 // found in the LICENSE file.
 
 #include "base/android/jni_android.h"
-#include "base/android/jni_utils.h"
 #include "content/public/app/content_jni_onload.h"
 #include "content/public/app/content_main.h"
 #include "content/public/browser/android/compositor.h"
-#include "content/shell/android/content_test_jni_registration.h"
 #include "content/shell/app/shell_main_delegate.h"
 
 // This is called by the VM when the shared library is first loaded.
 JNI_EXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
   base::android::InitVM(vm);
-  JNIEnv* env = base::android::AttachCurrentThread();
-  if (!base::android::IsSelectiveJniRegistrationEnabled(env)) {
-    if (!RegisterNonMainDexNatives(env)) {
-      return -1;
-    }
-  }
-
-  if (!RegisterMainDexNatives(env)) {
-    return -1;
-  }
   if (!content::android::OnJNIOnLoadInit())
     return -1;
   content::Compositor::Initialize();
