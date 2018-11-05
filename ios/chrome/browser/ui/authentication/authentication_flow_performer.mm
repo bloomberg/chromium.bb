9a56f0e3626b51eb89ea83f58b27e60cff0b2dd3
Antonio Gomes <tonikitoo@igalia.com>
2018-11-05 14:41:58 +0000

    [s13n][ios]Convert io/c/b/ui/authentication to IdentityManager
    BUG=889870,889869
    
    Change-Id: I8cfb5456ec39e0846f5d6dc56bca8a1e73749137
    Reviewed-on: https://chromium-review.googlesource.com/c/1316933
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#605324}
    Former-commit-id: 9addf391236ba99c61d0046ddf50110f7be039ea

--- ios/chrome/browser/ui/authentication/authentication_flow_performer.mm
+++ ios/chrome/browser/ui/authentication/authentication_flow_performer.mm
@@ -15,7 +15,6 @@
 #include "base/timer/timer.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/core/browser/account_tracker_service.h"
-#include "components/signin/core/browser/signin_manager.h"
 #include "components/signin/core/browser/signin_pref_names.h"
 #include "components/strings/grit/components_strings.h"
 #include "google_apis/gaia/gaia_auth_util.h"
@@ -25,7 +24,7 @@
 #include "ios/chrome/browser/signin/authentication_service.h"
 #include "ios/chrome/browser/signin/authentication_service_factory.h"
 #import "ios/chrome/browser/signin/constants.h"
-#include "ios/chrome/browser/signin/signin_manager_factory.h"
+#include "ios/chrome/browser/signin/identity_manager_factory.h"
 #include "ios/chrome/browser/sync/sync_setup_service.h"
 #include "ios/chrome/browser/sync/sync_setup_service_factory.h"
 #include "ios/chrome/browser/ui/alert_coordinator/alert_coordinator.h"
@@ -37,6 +36,7 @@
 #include "ios/chrome/grit/ios_strings.h"
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
 #import "ios/public/provider/chrome/browser/signin/chrome_identity.h"
+#include "services/identity/public/cpp/identity_manager.h"
 #include "ui/base/l10n/l10n_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -252,8 +252,8 @@ const int64_t kAuthenticationFlowTimeoutSeconds = 10;
   if (AuthenticationServiceFactory::GetForBrowserState(browserState)
           ->IsAuthenticatedIdentityManaged()) {
     NSString* hostedDomain = base::SysUTF8ToNSString(
-        ios::SigninManagerFactory::GetForBrowserState(browserState)
-            ->GetAuthenticatedAccountInfo()
+        IdentityManagerFactory::GetForBrowserState(browserState)
+            ->GetPrimaryAccountInfo()
             .hosted_domain);
     [self promptSwitchFromManagedEmail:lastSignedInEmail
                       withHostedDomain:hostedDomain
