4f0121be85b7ebdd3454133372cd48518a5f7d32
Fernando Serboncini <fserb@chromium.org>
2018-11-05 17:40:12 +0000

    Fix *StaticBitmapImage ThreadChecker and unaccelerated SkImage destroy
    - AcceleratedStaticBitmapImage was misusing ThreadChecker by having its
    own detach logic. Using proper DetachThread is simpler, cleaner and
    correct.
    - UnacceleratedStaticBitmapImage didn't destroy the SkImage in the
    proper thread, leading to GrContext/SkSp problems.
    
    Bug: 890576
    Change-Id: Ic71e7f7322b0b851774628247aa5256664bc0723
    Reviewed-on: https://chromium-review.googlesource.com/c/1307775
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Fernando Serboncini <fserb@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#604427}(cherry picked from commit 51aee1a971887d7db13acdc42bdee557d6ec91e0 [formerly 78d89fe556cb5dabbc47b4967cdf55e607e29580])
    Reviewed-on: https://chromium-review.googlesource.com/c/1318175
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3578@{#500}
    Cr-Branched-From: abbd18012032883c3113b3bfc5d59d25b4836c5f [formerly 4226ddf99103e493d7afb23a4c7902ee496108b6]-refs/heads/master@{#599034}
    Former-commit-id: 1d98e2c78a40847353dffb23b765e83620e9c51a

--- third_party/blink/renderer/platform/graphics/accelerated_static_bitmap_image.h
+++ third_party/blink/renderer/platform/graphics/accelerated_static_bitmap_image.h
@@ -104,20 +104,17 @@ class PLATFORM_EXPORT AcceleratedStaticBitmapImage final
       IntSize mailbox_size);
 
   void CreateImageFromMailboxIfNeeded();
-  void CheckThread();
   void WaitSyncTokenIfNeeded();
   void RetainOriginalSkImage();
 
   std::unique_ptr<TextureHolder> texture_holder_;
 
-  base::ThreadChecker thread_checker_;
-  bool detach_thread_at_next_check_ = false;
+  THREAD_CHECKER(thread_checker_);
   PaintImage::ContentId paint_image_content_id_;
 
   // For RetainOriginalSkImageForCopyOnWrite()
   sk_sp<SkImage> original_skia_image_;
   scoped_refptr<base::SingleThreadTaskRunner> original_skia_image_task_runner_;
-  PlatformThreadId original_skia_image_thread_id_;
   base::WeakPtr<WebGraphicsContext3DProviderWrapper>
       original_skia_image_context_provider_wrapper_;
 };
