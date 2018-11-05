6d99d46dc71b1fa1b91b900e68be3e418a6ee242
Jérôme Lebel <jlebel@chromium.org>
2018-11-05 18:45:26 +0000

    [iOS] Removing calls to IsUIRefreshPhase1Enabled() for Sign in promo
    Removing calls to IsUIRefreshPhase1Enabled() for Sign in promo.
    
    Bug: 884724
    Change-Id: I8417859a633484a1ff46ac440eacb02558ea661f
    Reviewed-on: https://chromium-review.googlesource.com/c/1317859
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605396}
    Former-commit-id: aa305dca061639239e8a9d97d3d1116f3560f160

--- ios/chrome/browser/ui/authentication/signin_promo_view.h
+++ ios/chrome/browser/ui/authentication/signin_promo_view.h
@@ -9,17 +9,7 @@
 
 #include "components/signin/core/browser/signin_metrics.h"
 
-@class MDCFlatButton;
 @protocol SigninPromoViewDelegate;
-
-typedef NS_ENUM(NSInteger, SigninPromoViewUI) {
-  // The legacy SigninPromoViewUI style.
-  SigninPromoViewUILegacy,
-  // The SigninPromoViewRefreshUI style. In order for this value to take effect
-  // the UIRefreshPhase1 flag also needs to be enabled. Otherwise
-  // SigninPromoViewUILegacy style will still be used.
-  SigninPromoViewUIRefresh,
-};
 
 typedef NS_ENUM(NSInteger, SigninPromoViewMode) {
   // No identity available on the device.
@@ -62,12 +52,8 @@ extern NSString* const kSigninPromoCloseButtonId;
 // |textLabel|.
 @property(nonatomic, readonly) CGFloat horizontalPadding;
 
-// Designated initializer. |signinPromoViewUI| sets the style for the
-// SigninPromoView to legacy or refreshed. This same view is used on Collections
-// and Settings, while the Collections UI will be refreshed soon, we still need
-// to support the legacy UI for settings.
+// Designated initializer.
 - (instancetype)initWithFrame:(CGRect)frame
-                        style:(SigninPromoViewUI)signinPromoViewUI
     NS_DESIGNATED_INITIALIZER;
 - (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
 - (instancetype)initWithCoder:(NSCoder*)aDecoder NS_UNAVAILABLE;
