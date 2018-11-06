4673130a25331bdd337f9c135fee96fd2bf24fd0
Mike Frysinger <vapier@chromium.org>
2018-11-06 07:13:44 +0000

    grit: drop python version checks
    Checking the python version on the fly doesn't add any value, so drop it.
    
    Bug: 747171
    Change-Id: Ieb1a6287c365aedd25c17edba1dd06cc14505e07
    Reviewed-on: https://chromium-review.googlesource.com/c/1319169
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#605620}
    Former-commit-id: b776598fdd60135df9429c85ba53e3b61b605ace

--- tools/grit/grit/grit_runner.py
+++ tools/grit/grit/grit_runner.py
@@ -196,10 +196,7 @@ def Main(args):
   """Parses arguments and does the appropriate thing."""
   util.ChangeStdoutEncoding()
 
-  if sys.version_info < (2, 6):
-    print "GRIT requires Python 2.6 or later."
-    return 2
-  elif not args or (len(args) == 1 and args[0] == 'help'):
+  if not args or (len(args) == 1 and args[0] == 'help'):
     PrintUsage()
     return 0
   elif len(args) == 2 and args[0] == 'help':
