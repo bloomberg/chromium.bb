848da1b066678719c946ee8430e5b8e4e4e7282e
Charlie Harrison <csharrison@chromium.org>
2018-11-07 18:25:36 +0000

    Add one more cursor size to UseCounter / UKM
    Bug: 880863
    Change-Id: I90983adf6d7ebd3ead1652c85b7c65a04b142a99
    Reviewed-on: https://chromium-review.googlesource.com/c/1313510
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606079}
    Former-commit-id: 2c3b223bb6f5bfbdef557c70fb9c44e025bd0378

--- chrome/browser/page_load_metrics/observers/use_counter/ukm_features.cc
+++ chrome/browser/page_load_metrics/observers/use_counter/ukm_features.cc
@@ -61,6 +61,7 @@ bool IsAllowedUkmFeature(blink::mojom::WebFeature feature) {
           WebFeature::kSuppressHistoryEntryWithoutUserGesture,
           WebFeature::kCursorImageGT32x32, WebFeature::kCursorImageLE32x32,
           WebFeature::kHistoryPushState, WebFeature::kHistoryReplaceState,
+          WebFeature::kCursorImageGT64x64,
       }));
   return opt_in_features->count(feature);
 }
