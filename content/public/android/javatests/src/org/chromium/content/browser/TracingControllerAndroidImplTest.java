af71af50ee90ee409c90bb30c56bbcb776a65177
agrieve <agrieve@chromium.org>
2018-11-07 19:18:32 +0000

    Revert "tracing: Address TracingControllerAndroid test failures."
    This reverts commit ec5f995f42ac58d75a2fff969390085d5b680ea3 [formerly 2fc9f6f25249bf221e970854bbdd9ddc6eaed3a9].
    
    Reason for revert: Test is still very flaky
    https://test-results.appspot.com/dashboards/flakiness_dashboard.html#testType=content_shell_test_apk&tests=org.chromium.content.browser.TracingControllerAndroidImplTest%23testBufferUsage
    
    Original change's description:
    > tracing: Address TracingControllerAndroid test failures.
    > 
    > Fixes the flakiness of buffer usage / known categories tests by
    > resolving an accidental race condition. Also restricts disabling
    > the other two tests to <= kitkat devices since those seem to be
    > the only ones that show (inexplicable) flakiness.
    > 
    > TBR=yfriedman@chromium.org
    > 
    > Bug: 899894
    > Change-Id: I0225c1519ea19910492ffb60a242bcc71b2275b4
    > Reviewed-on: https://chromium-review.googlesource.com/c/1307433
    > Reviewed-by: Eric Seckler <eseckler@chromium.org>
    > Commit-Queue: Eric Seckler <eseckler@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#604209}
    
    TBR=yfriedman@chromium.org,skyostil@chromium.org,eseckler@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 899894
    Change-Id: Ie47cf56e05861069e1b0a767138b1802f1a2584f
    Reviewed-on: https://chromium-review.googlesource.com/c/1323894
    Reviewed-by: agrieve <agrieve@chromium.org>
    Commit-Queue: agrieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606115}
    Former-commit-id: 8d3cd5fb007606b2f21d74825b188c8f2576dc23

--- content/public/android/javatests/src/org/chromium/content/browser/TracingControllerAndroidImplTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/TracingControllerAndroidImplTest.java
@@ -19,7 +19,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.Callback;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
-import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.content_public.browser.test.util.CriteriaHelper;
 import org.chromium.content_shell_apk.ContentShellActivity;
@@ -42,7 +42,7 @@ public class TracingControllerAndroidImplTest {
     @Test
     @MediumTest
     @Feature({"GPU"})
-    @DisableIf.Build(sdk_is_less_than = 21, message = "crbug.com/899894")
+    @DisabledTest(message = "crbug.com/899894")
     public void testTraceFileCreation() throws Exception {
         ContentShellActivity activity = mActivityTestRule.launchContentShellWithUrl("about:blank");
         mActivityTestRule.waitForActiveShellToBeDoneLoading();
@@ -76,8 +76,8 @@ public class TracingControllerAndroidImplTest {
     private class TestCallback<T> implements Callback<T> {
         @Override
         public void onResult(T result) {
-            mResult = result;
             mWasCalled.open();
+            mResult = result;
         }
 
         public ConditionVariable mWasCalled = new ConditionVariable();
@@ -87,6 +87,7 @@ public class TracingControllerAndroidImplTest {
     @Test
     @MediumTest
     @Feature({"GPU"})
+    @DisabledTest(message = "crbug.com/899894")
     public void testGetKnownCategories() throws Exception {
         ContentShellActivity activity = mActivityTestRule.launchContentShellWithUrl("about:blank");
         mActivityTestRule.waitForActiveShellToBeDoneLoading();
@@ -107,6 +108,7 @@ public class TracingControllerAndroidImplTest {
     @Test
     @MediumTest
     @Feature({"GPU"})
+    @DisabledTest(message = "crbug.com/899894")
     public void testBufferUsage() throws Exception {
         ContentShellActivity activity = mActivityTestRule.launchContentShellWithUrl("about:blank");
         mActivityTestRule.waitForActiveShellToBeDoneLoading();
@@ -129,7 +131,7 @@ public class TracingControllerAndroidImplTest {
     @Test
     @MediumTest
     @Feature({"GPU"})
-    @DisableIf.Build(sdk_is_less_than = 21, message = "crbug.com/899894")
+    @DisabledTest(message = "crbug.com/899894")
     public void testStopCallbackAndCompression() throws Exception {
         ContentShellActivity activity = mActivityTestRule.launchContentShellWithUrl("about:blank");
         mActivityTestRule.waitForActiveShellToBeDoneLoading();
