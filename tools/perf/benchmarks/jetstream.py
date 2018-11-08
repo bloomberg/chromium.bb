cbea355e93e6a693659337d47a212dc8270c9617
Emily Hanley <eyaich@google.com>
2018-11-08 17:53:16 +0000

    Converting kraken, jetstream and octane to new press harness.
    Bug: 714231
    Change-Id: I441067d621effc11fbd8d497716d6693e7bb5971
    Reviewed-on: https://chromium-review.googlesource.com/c/1320200
    Commit-Queue: Emily Hanley <eyaich@chromium.org>
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#606526}
    Former-commit-id: ba46f11f0f8e7ee0c1a5fa0a1023630c784a3beb

--- tools/perf/benchmarks/jetstream.py
+++ tools/perf/benchmarks/jetstream.py
@@ -17,80 +17,18 @@ smoothness. Some benchmarks demonstrate trade-offs, and aggressive or
 specialized optimization for one benchmark might make another benchmark slower.
 """
 
-import json
-import os
-
-from core import perf_benchmark
-
 from telemetry import benchmark
-from telemetry import page as page_module
-from telemetry.page import legacy_page_test
-from telemetry import story
-from telemetry.util import statistics
-from telemetry.value import list_of_scalar_values
 
+import page_sets
+from benchmarks import press
 
-class _JetstreamMeasurement(legacy_page_test.LegacyPageTest):
-
-  def __init__(self):
-    super(_JetstreamMeasurement, self).__init__()
-
-  def WillNavigateToPage(self, page, tab):
-    page.script_to_evaluate_on_commit = """
-        var __results = [];
-        var __real_log = window.console.log;
-        window.console.log = function() {
-          __results.push(Array.prototype.join.call(arguments, ' '));
-          __real_log.apply(this, arguments);
-        }
-        """
-
-  def ValidateAndMeasurePage(self, page, tab, results):
-    del page  # unused
-    tab.WaitForDocumentReadyStateToBeComplete()
-    tab.EvaluateJavaScript('JetStream.start()')
-    result = tab.WaitForJavaScriptCondition("""
-        (function() {
-          for (var i = 0; i < __results.length; i++) {
-            if (!__results[i].indexOf('Raw results: ')) return __results[i];
-          }
-          return null;
-        })();
-        """, timeout=60*20)
-    result = json.loads(result.partition(': ')[2])
-
-    all_score_lists = []
-    for k, v in result.iteritems():
-      results.AddValue(list_of_scalar_values.ListOfScalarValues(
-          results.current_page, k.replace('.', '_'), 'score', v['result'],
-          important=False))
-      # Collect all test scores to compute geometric mean.
-      for i, score in enumerate(v['result']):
-        if len(all_score_lists) <= i:
-          all_score_lists.append([])
-        all_score_lists[i].append(score)
-    all_scores = []
-    for score_list in all_score_lists:
-      all_scores.append(statistics.GeometricMean(score_list))
-    results.AddSummaryValue(list_of_scalar_values.ListOfScalarValues(
-        None, 'Score', 'score', all_scores))
 
 @benchmark.Info(emails=['hablich@chromium.org'],
                 component='Blink>JavaScript')
-class Jetstream(perf_benchmark.PerfBenchmark):
-  test = _JetstreamMeasurement
-
+class Jetstream(press._PressBenchmark): # pylint: disable=protected-access
   @classmethod
   def Name(cls):
     return 'jetstream'
 
   def CreateStorySet(self, options):
-    ps = story.StorySet(
-        archive_data_file='../page_sets/data/jetstream.json',
-        base_dir=os.path.dirname(os.path.abspath(__file__)),
-        cloud_storage_bucket=story.INTERNAL_BUCKET)
-    ps.AddStory(page_module.Page(
-        'http://browserbench.org/JetStream/', ps, ps.base_dir,
-        make_javascript_deterministic=False,
-        name='http://browserbench.org/JetStream/'))
-    return ps
+    return page_sets.JetstreamStorySet()
