185a944c29717bdbc95f8c4d9123e780714944e0
Steve Kobes <skobes@chromium.org>
2018-11-08 01:36:36 +0000

    Add PageLoadMetricsObserverTestHarness::SimulateRenderDataUpdate.
    This lets tests supply PageRenderData to the MetricsWebContentsObserver
    through the plumbing added in crrev.com/603785.
    
    Also remove an unused declaration and make SimulatePageLoadTimingUpdate
    private in PageLoadMetricsObserverTester.
    
    Bug: 581518
    Change-Id: I89409eff3346855c03450ceeb2030b63068d6d3a
    Reviewed-on: https://chromium-review.googlesource.com/c/1318058
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606275}
    Former-commit-id: e940281b6c35f5e937fcd0d5687d41c6ab81a33d

--- chrome/browser/page_load_metrics/observers/page_load_metrics_observer_tester.cc
+++ chrome/browser/page_load_metrics/observers/page_load_metrics_observer_tester.cc
@@ -68,29 +68,40 @@ PageLoadMetricsObserverTester::~PageLoadMetricsObserverTester() {}
 void PageLoadMetricsObserverTester::SimulateTimingUpdate(
     const mojom::PageLoadTiming& timing) {
   SimulatePageLoadTimingUpdate(timing, mojom::PageLoadMetadata(),
-                               mojom::PageLoadFeatures());
+                               mojom::PageLoadFeatures(),
+                               mojom::PageRenderData());
 }
 
 void PageLoadMetricsObserverTester::SimulateTimingAndMetadataUpdate(
     const mojom::PageLoadTiming& timing,
     const mojom::PageLoadMetadata& metadata) {
-  SimulatePageLoadTimingUpdate(timing, metadata, mojom::PageLoadFeatures());
+  SimulatePageLoadTimingUpdate(timing, metadata, mojom::PageLoadFeatures(),
+                               mojom::PageRenderData());
 }
 
 void PageLoadMetricsObserverTester::SimulateFeaturesUpdate(
     const mojom::PageLoadFeatures& new_features) {
   SimulatePageLoadTimingUpdate(mojom::PageLoadTiming(),
-                               mojom::PageLoadMetadata(), new_features);
+                               mojom::PageLoadMetadata(), new_features,
+                               mojom::PageRenderData());
+}
+
+void PageLoadMetricsObserverTester::SimulateRenderDataUpdate(
+    const mojom::PageRenderData& render_data) {
+  SimulatePageLoadTimingUpdate(mojom::PageLoadTiming(),
+                               mojom::PageLoadMetadata(),
+                               mojom::PageLoadFeatures(), render_data);
 }
 
 void PageLoadMetricsObserverTester::SimulatePageLoadTimingUpdate(
     const mojom::PageLoadTiming& timing,
     const mojom::PageLoadMetadata& metadata,
-    const mojom::PageLoadFeatures& new_features) {
+    const mojom::PageLoadFeatures& new_features,
+    const mojom::PageRenderData& render_data) {
   observer_->OnTimingUpdated(web_contents()->GetMainFrame(), timing.Clone(),
                              metadata.Clone(), new_features.Clone(),
                              std::vector<mojom::ResourceDataUpdatePtr>(),
-                             mojom::PageRenderDataPtr(base::in_place));
+                             render_data.Clone());
   // If sending the timing update caused the PageLoadMetricsUpdateDispatcher to
   // schedule a buffering timer, then fire it now so metrics are dispatched to
   // observers.
