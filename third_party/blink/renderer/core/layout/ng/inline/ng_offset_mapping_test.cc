553d560f7886a2de8213d81bdb642c423a6fd5c4
Koji Ishii <kojii@chromium.org>
2018-11-08 17:49:38 +0000

    Reland "Make NGOffsetMapping available for non-LayoutNG LayoutBlockFlow"
    This is a reland of 78674b3ac33b27696f77e675344d58a8090e3eb9 [formerly b43c6fca4349b7f2ff53b41926f5d7cb42ee4e0f]
    
    Cannot reproduce the failures locally, nor in CQ.
    
    Try to reland the same CL to see if it was a flake.
    
    Original change's description:
    > Make NGOffsetMapping available for non-LayoutNG LayoutBlockFlow
    >
    > This patch allows computing NGOffsetMapping for legacy inline
    > formatting context.
    >
    > For an experimental project "Invisible DOM"[1][2] to support
    > the find-in-page feature, it is needed to compute the text
    > content with whitespace collapsing applied, along with the
    > mapping to the DOM offset. This patch adds an API to compute
    > NGOffsetMapping even when the LayoutBlockFlow is not laid out
    > by LayoutNG for that purpose.
    >
    > Note that the project is still in the early phase. We may
    > revisit the design as it moves forward.
    >
    > [1] https://github.com/rakina/searchable-invisible-dom
    > [2] https://www.chromestatus.com/feature/5105291213406208
    >
    > Bug: 636993, 873057
    > Change-Id: I101b411960813a7b9b5c9c6e2db85d28737af882
    > Reviewed-on: https://chromium-review.googlesource.com/c/1322184
    > Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    > Reviewed-by: Emil A Eklund <eae@chromium.org>
    > Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    > Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    > Commit-Queue: Koji Ishii <kojii@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#606292}
    
    TBR=yoshin@chromium.org, eae@chromium.org
    
    Bug: 636993, 873057
    Change-Id: I241c6e9163add4000fa078c5d70a62b89acea9ac
    Reviewed-on: https://chromium-review.googlesource.com/c/1326343
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606525}
    Former-commit-id: b3a685fda9dc74255e5c44b46472eb89e05f7873

--- third_party/blink/renderer/core/layout/ng/inline/ng_offset_mapping_test.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_offset_mapping_test.cc
@@ -1149,4 +1149,40 @@ TEST_F(NGOffsetMappingTest, TextOverflowEllipsis) {
   TEST_RANGE(mapping.GetRanges(), text, 0u, 1u);
 }
 
+// Test |GetOffsetMapping| which is available both for LayoutNG and for legacy.
+class NGOffsetMappingGetterTest : public RenderingTest,
+                                  public testing::WithParamInterface<bool>,
+                                  private ScopedLayoutNGForTest {
+ public:
+  NGOffsetMappingGetterTest() : ScopedLayoutNGForTest(GetParam()) {}
+};
+
+INSTANTIATE_TEST_CASE_P(NGOffsetMappingTest,
+                        NGOffsetMappingGetterTest,
+                        testing::Bool());
+
+TEST_P(NGOffsetMappingGetterTest, Get) {
+  SetBodyInnerHTML(R"HTML(
+    <div id=container>
+      Whitespaces   in this text   should be   collapsed.
+    </div>
+  )HTML");
+  LayoutBlockFlow* layout_block_flow =
+      ToLayoutBlockFlow(GetLayoutObjectByElementId("container"));
+  DCHECK(layout_block_flow->ChildrenInline());
+
+  // For the purpose of this test, ensure this is laid out by each layout
+  // engine.
+  DCHECK_EQ(layout_block_flow->IsLayoutNGMixin(), GetParam());
+
+  std::unique_ptr<NGOffsetMapping> storage;
+  const NGOffsetMapping* mapping =
+      NGInlineNode::GetOffsetMapping(layout_block_flow, &storage);
+  EXPECT_TRUE(mapping);
+  EXPECT_EQ(!storage, GetParam());  // |storage| is used only for legacy.
+
+  const String& text_content = mapping->GetText();
+  EXPECT_EQ(text_content, "Whitespaces in this text should be collapsed.");
+}
+
 }  // namespace blink
