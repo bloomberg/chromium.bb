ebe934627f8a3d699b868076b933cc941918ca0e
Hugo Holgersson <hugoh@vewd.com>
2018-11-08 15:56:55 +0000

    Snav: Remove the redundant CanBeScrolledIntoView()
    This simplifies spatnav's inner loop, the evaluation of
    DOM nodes when looking for candidates, a tiny bit.
    
    Now that CanBeScrolledIntoView lost its call site
    we can remove it completely to reduce spatnav's
    code footprint.
    
    Bug: 770147
    Test: snav-div-overflow-scroll-hidden.html
    Change-Id: Icbf4cb4d0274d63658dbeab1cdae1ce63557576f
    Reviewed-on: https://chromium-review.googlesource.com/c/1319678
    Commit-Queue: Hugo Holgersson <hugoh@vewd.com>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#606480}
    Former-commit-id: f498307fe76198eee05bf40a3167928e1d6a4b35

--- third_party/blink/renderer/core/page/spatial_navigation.h
+++ third_party/blink/renderer/core/page/spatial_navigation.h
@@ -93,7 +93,6 @@ CORE_EXPORT bool IsScrollableAreaOrDocument(const Node*);
 CORE_EXPORT Node* ScrollableAreaOrDocumentOf(Node*);
 bool CanScrollInDirection(const Node* container, WebFocusType);
 bool CanScrollInDirection(const LocalFrame*, WebFocusType);
-bool CanBeScrolledIntoView(WebFocusType, const FocusCandidate&);
 bool AreElementsOnSameLine(const FocusCandidate& first_candidate,
                            const FocusCandidate& second_candidate);
 void DistanceDataForNode(WebFocusType,
