954accd69fba14dac2aec29262f6aa69a7c386f9
Dan Sanders <sandersd@chromium.org>
2018-11-08 03:50:52 +0000

    [media] Disable MojoVideoDecoder by default on CrOS.
    This is a partial revert of commit dbbe6484d18ff9203bc0e01fefa862aff9bd9481 [formerly 44323658c5cc4fff0a2d4a0bcd1d4aa9a7290c87].
    
    Bug: 902900
    Change-Id: I0279d839a83ff67ac37508aad8de632b237fa65e
    Reviewed-on: https://chromium-review.googlesource.com/c/1325072
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606327}
    Former-commit-id: 8df3add8a206b8ff34bd21a3adb95538d2db9803

--- media/base/media_switches.cc
+++ media/base/media_switches.cc
@@ -260,8 +260,16 @@ const base::Feature kMemoryPressureBasedSourceBufferGC{
     "MemoryPressureBasedSourceBufferGC", base::FEATURE_DISABLED_BY_DEFAULT};
 
 // Enable MojoVideoDecoder, replacing GpuVideoDecoder.
-const base::Feature kMojoVideoDecoder{"MojoVideoDecoder",
-                                      base::FEATURE_ENABLED_BY_DEFAULT};
+const base::Feature kMojoVideoDecoder {
+  "MojoVideoDecoder",
+#if defined(OS_CHROMEOS)
+      // TODO(posciak): Re-enable once the feature is verified on CrOS.
+      // https://crbug.com/902968.
+      base::FEATURE_DISABLED_BY_DEFAULT
+#else
+      base::FEATURE_ENABLED_BY_DEFAULT
+#endif
+};
 
 // Enable The D3D11 Video decoder. Must also enable MojoVideoDecoder for
 // this to have any effect.
