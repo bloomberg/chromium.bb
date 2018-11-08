8631d6eea8bd1a4038963a98594b04be3c087586
Vadym Doroshenko <dvadym@chromium.org>
2018-11-08 13:07:26 +0000

    Fix dynamically changed forms in NewPasswordFormManager.
    Sometimes sites change forms after adding them. For example removing/
    adding fields. This CL makes NewPasswordFormManager process this
    situation, by updating |observed_form|, parsing it again and sending
    new data to the renderer.
    
    This first time this CL was uploaded on
    https://chromium-review.googlesource.com/c/chromium/src/+/1326010 (Patch 1)
    but it was reverted because of failures in
    password_generation_interactive_uitest.cc. The reason of failures is that
    no non-blacklisted confirmation because NewPasswordFormManager waits for
    server response. This CL fixes that by turning off waiting of server
    predictions.
    
    TBR=isherman@chromium.org
    
    Bug: 895411, 831123, 895788, 902805
    Change-Id: I99174a73134c582a3975b1f06d797e61ec117855
    Reviewed-on: https://chromium-review.googlesource.com/c/1326010
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606435}
    Former-commit-id: b874f30df5bdd328cd3cf5d52c1c623b11f45fec

--- chrome/browser/password_manager/password_generation_interactive_uitest.cc
+++ chrome/browser/password_manager/password_generation_interactive_uitest.cc
@@ -19,6 +19,7 @@
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/common/autofill_features.h"
+#include "components/password_manager/core/browser/new_password_form_manager.h"
 #include "components/password_manager/core/browser/password_generation_manager.h"
 #include "components/password_manager/core/browser/password_manager_util.h"
 #include "components/password_manager/core/browser/test_password_store.h"
@@ -110,6 +111,8 @@ class PasswordGenerationInteractiveTest
     client->SetTestObserver(&observer_);
     // The base class should enable password generation.
     ASSERT_NE(password_manager::NOT_SYNCING, client->GetPasswordSyncState());
+    password_manager::NewPasswordFormManager::
+        set_wait_for_server_predictions_for_filling(false);
 
     NavigateToFile("/password/signup_form_new_password.html");
   }
