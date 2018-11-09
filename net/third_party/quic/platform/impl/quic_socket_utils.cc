8d3b081320eb37e49ef2401ffa31677e3d06db98
Bin Wu <wub@chromium.org>
2018-11-09 17:45:12 +0000

    Reland "Export the quic batch writer code to Chromium. Note it's not used anywhere in Chrome, we are just exporting the code so people outside of Google can take a look."
    This reverts commit 81a0c09a595b1892cdf3ce7e64d5735d695781d4 [formerly 683a348730bbea7ccea7158eacc04b928b3d5256].
    
    
    In addition to the auto-revert, I also changed two bzero calls to memset calls. I don't know why, but net_unittest under msan fails with a use-of-uninitialized-value: https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8930601665674446576/+/steps/net_unittests/0/stdout
    
    The msan error goes away after I changed the following bzero(s) into memset(s):
    
    diff --git a/net/third_party/quic/platform/impl/quic_linux_socket_utils.h b/net/third_party/quic/platform/impl/quic_linux_socket_utils.h
    index 94f055f35564..198e266dfdf2 100644
    --- a/net/third_party/quic/platform/impl/quic_linux_socket_utils.h
    +++ b/net/third_party/quic/platform/impl/quic_linux_socket_utils.h
    @@ -102,7 +102,7 @@ class QuicMMsgHdr {
         }
    
         storage_.reset(new char[StorageSize()]);
    -    bzero(&storage_[0], StorageSize());
    +    memset(&storage_[0], 0, StorageSize());
    
         int i = -1;
         for (auto it = first; it != last; ++it) {
    diff --git a/net/third_party/quic/platform/impl/quic_socket_utils.cc b/net/third_party/quic/platform/impl/quic_socket_utils.cc
    index c25f4697dbef..1f252e50f953 100644
    --- a/net/third_party/quic/platform/impl/quic_socket_utils.cc
    +++ b/net/third_party/quic/platform/impl/quic_socket_utils.cc
    @@ -78,7 +78,7 @@ void* QuicMsgHdr::GetNextCmsgDataInternal(int cmsg_level,
    
       if (cmsg_ == nullptr) {
         DCHECK_EQ(nullptr, hdr_.msg_control);
    -    bzero(cbuf_, cbuf_size_);
    +    memset(cbuf_, 0, cbuf_size_);
         hdr_.msg_control = cbuf_;
         cmsg_ = CMSG_FIRSTHDR(&hdr_);
       } else {
    
    
    R=rch@chromium.org
    
    Change-Id: Ibe60068cb87c0f974088b8c1601cc516ee333106
    Reviewed-on: https://chromium-review.googlesource.com/c/1329348
    Commit-Queue: Bin Wu <wub@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606891}
    Former-commit-id: 346d1aac0d7c49126b8a9943434ee88af85481af

--- net/third_party/quic/platform/impl/quic_socket_utils.cc
+++ net/third_party/quic/platform/impl/quic_socket_utils.cc
@@ -5,6 +5,7 @@
 #include "net/third_party/quic/platform/impl/quic_socket_utils.h"
 
 #include <errno.h>
+#include <fcntl.h>
 #include <linux/net_tstamp.h>
 #include <netinet/in.h>
 #include <string.h>
@@ -26,6 +27,73 @@
 using std::string;
 
 namespace quic {
+
+QuicMsgHdr::QuicMsgHdr(const char* buffer,
+                       size_t buf_len,
+                       const QuicSocketAddress& peer_address,
+                       char* cbuf,
+                       size_t cbuf_size)
+    : iov_{const_cast<char*>(buffer), buf_len},
+      cbuf_(cbuf),
+      cbuf_size_(cbuf_size),
+      cmsg_(nullptr) {
+  // Only support unconnected sockets.
+  DCHECK(peer_address.IsInitialized());
+
+  raw_peer_address_ = peer_address.generic_address();
+  hdr_.msg_name = &raw_peer_address_;
+  hdr_.msg_namelen = raw_peer_address_.ss_family == AF_INET
+                         ? sizeof(sockaddr_in)
+                         : sizeof(sockaddr_in6);
+
+  hdr_.msg_iov = &iov_;
+  hdr_.msg_iovlen = 1;
+  hdr_.msg_flags = 0;
+
+  hdr_.msg_control = nullptr;
+  hdr_.msg_controllen = 0;
+}
+
+void QuicMsgHdr::SetIpInNextCmsg(const QuicIpAddress& self_address) {
+  if (!self_address.IsInitialized()) {
+    return;
+  }
+
+  if (self_address.IsIPv4()) {
+    QuicSocketUtils::SetIpInfoInCmsgData(
+        self_address, GetNextCmsgData<in_pktinfo>(IPPROTO_IP, IP_PKTINFO));
+  } else {
+    QuicSocketUtils::SetIpInfoInCmsgData(
+        self_address, GetNextCmsgData<in6_pktinfo>(IPPROTO_IPV6, IPV6_PKTINFO));
+  }
+}
+
+void* QuicMsgHdr::GetNextCmsgDataInternal(int cmsg_level,
+                                          int cmsg_type,
+                                          size_t data_size) {
+  // msg_controllen needs to be increased first, otherwise CMSG_NXTHDR will
+  // return nullptr.
+  hdr_.msg_controllen += CMSG_SPACE(data_size);
+  DCHECK_LE(hdr_.msg_controllen, cbuf_size_);
+
+  if (cmsg_ == nullptr) {
+    DCHECK_EQ(nullptr, hdr_.msg_control);
+    memset(cbuf_, 0, cbuf_size_);
+    hdr_.msg_control = cbuf_;
+    cmsg_ = CMSG_FIRSTHDR(&hdr_);
+  } else {
+    DCHECK_NE(nullptr, hdr_.msg_control);
+    cmsg_ = CMSG_NXTHDR(&hdr_, cmsg_);
+  }
+
+  DCHECK_NE(nullptr, cmsg_) << "Insufficient control buffer space";
+
+  cmsg_->cmsg_len = CMSG_LEN(data_size);
+  cmsg_->cmsg_level = cmsg_level;
+  cmsg_->cmsg_type = cmsg_type;
+
+  return CMSG_DATA(cmsg_);
+}
 
 // static
 void QuicSocketUtils::GetAddressAndTimestampFromMsghdr(
@@ -269,6 +337,38 @@ WriteResult QuicSocketUtils::WritePacket(
                          ? WRITE_STATUS_BLOCKED
                          : WRITE_STATUS_ERROR,
                      errno);
+}
+
+// static
+WriteResult QuicSocketUtils::WritePacket(int fd, const QuicMsgHdr& hdr) {
+  int rc;
+  do {
+    rc = sendmsg(fd, hdr.hdr(), 0);
+  } while (rc < 0 && errno == EINTR);
+  if (rc >= 0) {
+    return WriteResult(WRITE_STATUS_OK, rc);
+  }
+  return WriteResult((errno == EAGAIN || errno == EWOULDBLOCK)
+                         ? WRITE_STATUS_BLOCKED
+                         : WRITE_STATUS_ERROR,
+                     errno);
+}
+
+// static
+void QuicSocketUtils::SetIpInfoInCmsgData(const QuicIpAddress& self_address,
+                                          void* cmsg_data) {
+  DCHECK(self_address.IsInitialized());
+  const QuicString& address_str = self_address.ToPackedString();
+  if (self_address.IsIPv4()) {
+    in_pktinfo* pktinfo = static_cast<in_pktinfo*>(cmsg_data);
+    pktinfo->ipi_ifindex = 0;
+    memcpy(&pktinfo->ipi_spec_dst, address_str.c_str(), address_str.length());
+  } else if (self_address.IsIPv6()) {
+    in6_pktinfo* pktinfo = static_cast<in6_pktinfo*>(cmsg_data);
+    memcpy(&pktinfo->ipi6_addr, address_str.c_str(), address_str.length());
+  } else {
+    QUIC_BUG << "Unrecognized IPAddress";
+  }
 }
 
 // static
