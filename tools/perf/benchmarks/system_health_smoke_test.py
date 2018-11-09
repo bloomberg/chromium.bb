4a3a0a5b63eecb4f1b1d1741f60ff2164e9f9de6
Camillo Bruni <cbruni@chromium.org>
2018-11-09 13:09:13 +0000

    [perf] Add browse:media:youtube:2018 system_health story
    Due to many ads being loaded and certain requests will not work.
        [console:error:js]:       ~30
        [console:error:network]:  ~30
    
    Bug: 878390
    Change-Id: I3e09c9302514b1fb089d77b4671c59fb44afb22f
    Reviewed-on: https://chromium-review.googlesource.com/c/1296172
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606812}
    Former-commit-id: e5ece11759649faaa791f155e7c88cc13c99d497

--- tools/perf/benchmarks/system_health_smoke_test.py
+++ tools/perf/benchmarks/system_health_smoke_test.py
@@ -80,11 +80,12 @@ _DISABLED_TESTS = frozenset({
   # crbug.com/696824
   'system_health.memory_desktop/load:news:qq',
   # crbug.com/893615
+  'system_health.memory_desktop/browse:media:pinterest',
+  'system_health.memory_desktop/browse:media:youtube',
+  'system_health.memory_desktop/browse:search:google_india',
+  'system_health.memory_desktop/load:games:alphabetty',
   'system_health.memory_desktop/load:news:cnn',
   'system_health.memory_desktop/load:tools:stackoverflow',
-  'system_health.memory_desktop/load:games:alphabetty',
-  'system_health.memory_desktop/browse:search:google_india',
-  'system_health.memory_desktop/browse:media:pinterest',
 
   # crbug.com/698006
   'system_health.memory_desktop/load:tools:drive',
