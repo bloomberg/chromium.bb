6b60343a5ef1ebed8aa141a75f5960096f8608b1
Yutaka Hirano <yhirano@chromium.org>
2018-11-09 16:47:20 +0000

    [media] Treat cross-origin redirect as TAINTED only for no-cors requests
    With https://crrev.com/2f62b99f8490780fa7aad7bcd1584b39f514ad28 [formerly a9cbaa7a40e2b2723cfc2f266c42f4980038a949],
    WebMediaPlayer blindly treats a resource experiencing cross-origin
    redirects as TAINTED. In fact, it should be treated as TAINTED only
    when its request mode is "no-cors".
    
    The added tests are provided by hongchan@chromium.org.
    
    TBR=yhirano@chromium.org
    
    (cherry picked from commit 9124560d357c2ea6a32b691b3c68bc23dc2a26f4 [formerly 657a055a66492e996b296d7bb999e3c8551eed23])
    
    Bug: 899745, 901383
    Change-Id: Idb66407552085b053818f3e4a9d8d5ff3ddeaf45
    Reviewed-on: https://chromium-review.googlesource.com/c/1325281
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Fredrik Hubinette <hubbe@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#606681}
    Reviewed-on: https://chromium-review.googlesource.com/c/1329801
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3578@{#613}
    Cr-Branched-From: abbd18012032883c3113b3bfc5d59d25b4836c5f [formerly 4226ddf99103e493d7afb23a4c7902ee496108b6]-refs/heads/master@{#599034}
    Former-commit-id: c3aa434fe1fc3c69fd86b03267c7cae747c9440b

--- media/blink/webmediaplayer_impl.h
+++ media/blink/webmediaplayer_impl.h
@@ -336,8 +336,6 @@ class MEDIA_BLINK_EXPORT WebMediaPlayerImpl
   void OnPipelineResumed();
   void OnDemuxerOpened();
 
-  bool HasSingleSecurityOrigin() const;
-
   // Pipeline::Client overrides.
   void OnError(PipelineStatus status) override;
   void OnEnded() override;
