6980339c32be1feebfdf9fbde99ad68341751368
David Jacobo <djacobo@chromium.org>
2018-11-09 22:24:03 +0000

    Reland: Create TouchSelectionMenuViews to separate Menu
    views::TouchSelectionMenuRunnerViews::Menu should be better by
    separating it on their own class, this CL introduces
    TouchSelectionMenuViews.
    
    Bug: None
    Test: Build.
    Change-Id: Ic9e8e258c41d2686746ff9b7d73fef1826424851
    Reviewed-on: https://chromium-review.googlesource.com/c/1328281
    Reviewed-by: Mohsen Izadi <mohsen@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: David Jacobo <djacobo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606999}
    Former-commit-id: fae876efa6f54e88c364ea26c5a7bc2ca761a9a6

--- ui/views/touchui/touch_selection_menu_runner_views.h
+++ ui/views/touchui/touch_selection_menu_runner_views.h
@@ -9,21 +9,18 @@
 
 #include "base/macros.h"
 #include "ui/touch_selection/touch_selection_menu_runner.h"
-#include "ui/views/bubble/bubble_dialog_delegate_view.h"
 #include "ui/views/controls/button/button.h"
-#include "ui/views/controls/button/label_button.h"
 #include "ui/views/views_export.h"
 
 namespace views {
 class LabelButton;
+class TouchSelectionMenuViews;
 class Widget;
 
 // Views implementation for TouchSelectionMenuRunner.
 class VIEWS_EXPORT TouchSelectionMenuRunnerViews
     : public ui::TouchSelectionMenuRunner {
  public:
-  class Menu;
-
   // Test API to access internal state in tests.
   class VIEWS_EXPORT TestApi {
    public:
@@ -45,7 +42,7 @@ class VIEWS_EXPORT TouchSelectionMenuRunnerViews
 
  protected:
   // Sets the menu as the currently runner menu and shows it.
-  void ShowMenu(Menu* menu,
+  void ShowMenu(TouchSelectionMenuViews* menu,
                 const gfx::Rect& anchor_rect,
                 const gfx::Size& handle_image_size);
 
@@ -60,59 +57,13 @@ class VIEWS_EXPORT TouchSelectionMenuRunnerViews
   bool IsRunning() const override;
 
  private:
+  friend class TouchSelectionMenuViews;
+
   // A pointer to the currently running menu, or |nullptr| if no menu is
   // running. The menu manages its own lifetime and deletes itself when closed.
-  Menu* menu_;
+  TouchSelectionMenuViews* menu_;
 
   DISALLOW_COPY_AND_ASSIGN(TouchSelectionMenuRunnerViews);
-};
-
-// A bubble that contains actions available for the selected text. An object of
-// this type, as a BubbleDialogDelegateView, manages its own lifetime.
-class TouchSelectionMenuRunnerViews::Menu : public BubbleDialogDelegateView,
-                                            public ButtonListener {
- public:
-  Menu(TouchSelectionMenuRunnerViews* owner,
-       ui::TouchSelectionMenuClient* client,
-       aura::Window* context);
-
-  void ShowMenu(const gfx::Rect& anchor_rect,
-                const gfx::Size& handle_image_size);
-
-  // Checks whether there is any command available to show in the menu.
-  static bool IsMenuAvailable(const ui::TouchSelectionMenuClient* client);
-
-  // Closes the menu. This will eventually self-destroy the object.
-  void CloseMenu();
-
- protected:
-  ~Menu() override;
-
-  // Queries the |client_| for what commands to show in the menu and sizes the
-  // menu appropriately.
-  virtual void CreateButtons();
-
-  // Helper method to create a single button.
-  LabelButton* CreateButton(const base::string16& title, int tag);
-
-  // ButtonListener:
-  void ButtonPressed(Button* sender, const ui::Event& event) override;
-
- private:
-  friend class TouchSelectionMenuRunnerViews::TestApi;
-
-  // Helper to disconnect this menu object from its owning menu runner.
-  void DisconnectOwner();
-
-  // BubbleDialogDelegateView:
-  void OnPaint(gfx::Canvas* canvas) override;
-  void WindowClosing() override;
-  int GetDialogButtons() const override;
-
-  TouchSelectionMenuRunnerViews* owner_;
-  ui::TouchSelectionMenuClient* const client_;
-
-  DISALLOW_COPY_AND_ASSIGN(Menu);
 };
 
 }  // namespace views
