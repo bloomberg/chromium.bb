926e68b68c6a05f4b00522e37eca1c5f6fedcc30
Lan Wei <lanwei@chromium.org>
2018-11-09 17:28:50 +0000

    Add action_sequence in testdriver
    After we expose test_driver.Actions API to web users, we add their
    implementation in our testdriver file, and fix the wpt tests of
    Actions API.
    
    Bug: 893480
    Change-Id: Ib02c0223208eeb2cc30c2ca35b98d5fc938baa2c
    Reviewed-on: https://chromium-review.googlesource.com/c/1289119
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#606882}
    Former-commit-id: 687313c56366619151285b4d87847b61e6547cbc

--- third_party/WebKit/LayoutTests/resources/testdriver-vendor.js
+++ third_party/WebKit/LayoutTests/resources/testdriver-vendor.js
@@ -5,6 +5,46 @@
   // that will be added in the future.
   const leftButton = 0;
 
+  function getInViewCenterPoint(rect) {
+      var left = Math.max(0, rect.left);
+      var right = Math.min(window.innerWidth, rect.right);
+      var top = Math.max(0, rect.top);
+      var bottom = Math.min(window.innerHeight, rect.bottom);
+
+      var x = 0.5 * (left + right);
+      var y = 0.5 * (top + bottom);
+
+      return [x, y];
+  }
+
+  function getPointerInteractablePaintTree(element) {
+      if (!window.document.contains(element)) {
+          return [];
+      }
+
+      var rectangles = element.getClientRects();
+
+      if (rectangles.length === 0) {
+          return [];
+      }
+
+      var centerPoint = getInViewCenterPoint(rectangles[0]);
+
+      if ("elementsFromPoint" in document) {
+          return document.elementsFromPoint(centerPoint[0], centerPoint[1]);
+      } else if ("msElementsFromPoint" in document) {
+          var rv = document.msElementsFromPoint(centerPoint[0], centerPoint[1]);
+          return Array.prototype.slice.call(rv ? rv : []);
+      } else {
+          throw new Error("document.elementsFromPoint unsupported");
+      }
+  }
+
+  function inView(element) {
+      var pointerInteractablePaintTree = getPointerInteractablePaintTree(element);
+      return pointerInteractablePaintTree.indexOf(element) !== -1;
+  }
+
   window.test_driver_internal.click = function(element, coords) {
     return new Promise(function(resolve, reject) {
       if (window.chrome && chrome.gpuBenchmarking) {
@@ -29,6 +69,61 @@
       if (window.chrome && chrome.gpuBenchmarking) {
         chrome.gpuBenchmarking.freeze();
         resolve();
+      } else {
+        reject(new Error("GPU benchmarking is not enabled."));
+      }
+    });
+  };
+
+  window.test_driver_internal.action_sequence = function(actions) {
+    if (window.top !== window) {
+        return Promise.reject(new Error("can only send keys in top-level window"));
+    }
+
+    var didScrollIntoView = false;
+    for (let i = 0; i < actions.length; i++) {
+        for (let j = 0; j < actions[i].actions.length; j++) {
+            if ('origin' in actions[i].actions[j]) {
+                if (actions[i].actions[j].origin == "viewport")
+                  continue;
+
+                if (actions[i].actions[j].origin == "pointer")
+                  return Promise.reject(new Error("pointer origin is not supported right now"));
+
+                let element = actions[i].actions[j].origin;
+                if (!window.document.contains(element)) {
+                    return Promise.reject(new Error("element in different document or shadow tree"));
+                }
+
+                if (!inView(element)) {
+                    if (didScrollIntoView)
+                      return Promise.reject(new Error("already scrolled into view, the element is not found"));
+
+                    element.scrollIntoView({behavior: "instant",
+                                            block: "end",
+                                            inline: "nearest"});
+                    didScrollIntoView = true;
+                }
+
+                var pointerInteractablePaintTree = getPointerInteractablePaintTree(element);
+                if (pointerInteractablePaintTree.length === 0 ||
+                    !element.contains(pointerInteractablePaintTree[0])) {
+                    return Promise.reject(new Error("element click intercepted error"));
+                }
+
+                var rect = element.getClientRects()[0];
+                var centerPoint = getInViewCenterPoint(rect);
+                var x = centerPoint[0];
+                var y = centerPoint[1];
+                actions[i].actions[j].x += x;
+                actions[i].actions[j].y += y;
+            }
+        }
+    }
+
+    return new Promise(function(resolve, reject) {
+      if (window.chrome && chrome.gpuBenchmarking) {
+        chrome.gpuBenchmarking.pointerActionSequence(actions, resolve);
       } else {
         reject(new Error("GPU benchmarking is not enabled."));
       }
