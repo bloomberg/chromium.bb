e1e671cb2337336f716d9dd66f670144734ed01e
Carlos IL <carlosil@chromium.org>
2018-11-09 22:42:25 +0000

    Check for null WebContents in LoginHandler::GetWebContentsForLogin
    Bug: 885376
    Change-Id: Ic1b4075a169512103994edabc53b5d1b6daffb10
    Reviewed-on: https://chromium-review.googlesource.com/c/1325190
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607010}
    Former-commit-id: 052044311dd4c7c22ae5af43ad7c3377032b8dfb

--- chrome/browser/ui/login/login_handler.cc
+++ chrome/browser/ui/login/login_handler.cc
@@ -164,8 +164,11 @@ WebContents* LoginHandler::GetWebContentsForLogin() const {
 }
 
 password_manager::PasswordManager* LoginHandler::GetPasswordManagerForLogin() {
+  WebContents* contents = GetWebContentsForLogin();
+  if (!contents)
+    return nullptr;
   password_manager::PasswordManagerClient* client =
-      ChromePasswordManagerClient::FromWebContents(GetWebContentsForLogin());
+      ChromePasswordManagerClient::FromWebContents(contents);
   return client ? client->GetPasswordManager() : nullptr;
 }
 
