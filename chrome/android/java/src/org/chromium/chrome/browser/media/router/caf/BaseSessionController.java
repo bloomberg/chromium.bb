4873ef1f770c6773b9b0bc9ee532ea0b393649fb
Zhiqiang Zhang <zqzhang@google.com>
2018-11-10 05:22:10 +0000

    Unit tests for BaseSessionController
    This CL adds unit tests for BaseSessionController.
    
    Also, when ending session, call SessionManager.endCurrentSession()
    instead of selecting the default route. Also the app ID is set to null.
    
    Bug: 711860
    Change-Id: I002d69c16b72fd68d5ff9076624c74e63f0b0003
    Reviewed-on: https://chromium-review.googlesource.com/c/1317337
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Zhiqiang Zhang <zqzhang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607104}
    Former-commit-id: 76f7fc6fb8acb5198518f4108ff47308293482dd

--- chrome/android/java/src/org/chromium/chrome/browser/media/router/caf/BaseSessionController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/router/caf/BaseSessionController.java
@@ -5,6 +5,7 @@
 package org.chromium.chrome.browser.media.router.caf;
 
 import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
 import android.support.v7.media.MediaRouter;
 
 import com.google.android.gms.cast.CastDevice;
@@ -32,7 +33,8 @@ public class BaseSessionController {
     private final CafBaseMediaRouteProvider mProvider;
     private final MediaRouter.Callback mMediaRouterCallbackForSessionLaunch;
     private CreateRouteRequestInfo mRouteCreationInfo;
-    private final CafNotificationController mNotificationController;
+    @VisibleForTesting
+    CafNotificationController mNotificationController;
     private final RemoteMediaClient.Callback mRemoteMediaClientCallback;
 
     public BaseSessionController(CafBaseMediaRouteProvider provider) {
@@ -81,7 +83,7 @@ public class BaseSessionController {
     }
 
     public RemoteMediaClient getRemoteMediaClient() {
-        return mCastSession.getRemoteMediaClient();
+        return isConnected() ? mCastSession.getRemoteMediaClient() : null;
     }
 
     public CafNotificationController getNotificationController() {
@@ -89,8 +91,8 @@ public class BaseSessionController {
     }
 
     public void endSession() {
-        MediaRouter mediaRouter = mProvider.getAndroidMediaRouter();
-        mediaRouter.selectRoute(mediaRouter.getDefaultRoute());
+        CastUtils.getCastContext().getSessionManager().endCurrentSession(/* stopCasting= */ true);
+        CastUtils.getCastContext().setReceiverApplicationId(null);
     }
 
     public List<String> getCapabilities() {
