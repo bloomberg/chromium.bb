106a60a8c76480a89c7594ed61d73938ebe68ac5
Pavel Feldman <pfeldman@chromium.org>
2018-11-12 22:41:30 +0000

    DevTools: add the missing binary implementation for the browser protocol layer.
    Change-Id: I5aaebd553669d9b6594105edd4ba18f6b4701d20
    Reviewed-on: https://chromium-review.googlesource.com/c/1331037
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607357}
    Former-commit-id: 4f22b80a94792a369577923c4e3805c827495c74

--- chrome/browser/devtools/protocol_string.h
+++ chrome/browser/devtools/protocol_string.h
@@ -7,8 +7,11 @@
 
 #include <memory>
 #include <string>
+#include <vector>
 
+#include "base/logging.h"
 #include "base/macros.h"
+#include "base/memory/ref_counted_memory.h"
 #include "base/strings/string_number_conversions.h"
 
 namespace base {
@@ -76,21 +79,31 @@ class StringUtil {
   static String builderToString(StringBuilder& builder) {
     return builder.toString();
   }
+
   static std::unique_ptr<protocol::Value> parseJSON(const String&);
 };
 
 // A read-only sequence of uninterpreted bytes with reference-counted storage.
-// Though the templates for generating the protocol bindings reference
-// this type, thus far it's not used in the Chrome layer, so we provide no
-// implementation here and rely on the linker optimizing it away. If this
-// changes, look to content/browser/devtools/protocol_string{.h,.cc} for
-// inspiration.
 class Binary {
  public:
-  const uint8_t* data() const;
-  size_t size() const;
+  Binary(const Binary&);
+  Binary();
+  ~Binary();
+
+  const uint8_t* data() const { return bytes_->front(); }
+  size_t size() const { return bytes_->size(); }
+  scoped_refptr<base::RefCountedMemory> bytes() const { return bytes_; }
+
   String toBase64() const;
+
   static Binary fromBase64(const String& base64, bool* success);
+  static Binary fromRefCounted(scoped_refptr<base::RefCountedMemory> memory);
+  static Binary fromVector(std::vector<uint8_t> data);
+  static Binary fromString(std::string data);
+
+ private:
+  explicit Binary(scoped_refptr<base::RefCountedMemory> bytes);
+  scoped_refptr<base::RefCountedMemory> bytes_;
 };
 
 std::unique_ptr<protocol::Value> toProtocolValue(const base::Value* value,
