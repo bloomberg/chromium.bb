5ec02ae5e4490542844e57087930c152508a0441
Shawn Gallea <sagallea@google.com>
2018-11-12 23:20:51 +0000

    EXO: Fullscreen protocol server implementation
    An incomplete implementation of the fullscreen shell protocol,
    separated from server.cc to allow selective compilation for
    different platforms that support EXO. The rest of the protocols within
    server.cc will be refactored in future CLs.
    
    Bug: 896710
    Test: Compiled on ChromeOS, works with fullscreen client
    Change-Id: Idda3b73c97c72e5efd16dfe1f7c79831587c8066
    Reviewed-on: https://chromium-review.googlesource.com/c/1299838
    Commit-Queue: Shawn Gallea <sagallea@google.com>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607369}
    Former-commit-id: 3fbba49dc9e193e72b4c8680776404803b2d7a92

---
+++ components/exo/wayland/server_util.h
@@ -0,0 +1,45 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_EXO_WAYLAND_SERVER_UTIL_H_
+#define COMPONENTS_EXO_WAYLAND_SERVER_UTIL_H_
+
+#include <wayland-server-core.h>
+#include <wayland-server-protocol-core.h>
+#include <memory>
+
+#include "base/memory/ptr_util.h"
+
+namespace exo {
+namespace wayland {
+
+template <class T>
+T* GetUserDataAs(wl_resource* resource) {
+  return static_cast<T*>(wl_resource_get_user_data(resource));
+}
+
+template <class T>
+std::unique_ptr<T> TakeUserDataAs(wl_resource* resource) {
+  std::unique_ptr<T> user_data = base::WrapUnique(GetUserDataAs<T>(resource));
+  wl_resource_set_user_data(resource, nullptr);
+  return user_data;
+}
+
+template <class T>
+void DestroyUserData(wl_resource* resource) {
+  TakeUserDataAs<T>(resource);
+}
+
+template <class T>
+void SetImplementation(wl_resource* resource,
+                       const void* implementation,
+                       std::unique_ptr<T> user_data) {
+  wl_resource_set_implementation(resource, implementation, user_data.release(),
+                                 DestroyUserData<T>);
+}
+
+}  // namespace wayland
+}  // namespace exo
+
+#endif  // COMPONENTS_EXO_WAYLAND_SERVER_UTIL_H_
