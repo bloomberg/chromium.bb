ba3107c66a2d3bed8c25954a8445382b25be98e3
Patrik Höglund <phoglund@chromium.org>
2018-11-12 13:01:55 +0000

    Revert "Add more information to |error| of -didFailFavigationWithSSLError:."
    This reverts commit f979dfa774e4abdcfb539317cedaee307163af77 [formerly af760c9155256a267d8f3a284aa52492ae57b3c9].
    
    Reason for revert: Appears to cause flaky crashes in external/wpt/webusb/idlharness.https.any.worker.html. See https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Mac10.13%20Tests%20%28dbg%29/6367. The crash stack trace mentions SSL. If this CL is innocent, then please just reland.
    
    Original change's description:
    > Add more information to |error| of -didFailFavigationWithSSLError:.
    > 
    > Add error.localizedDescription and error.userInfo[CWVCertStatusKey].
    > 
    > Bug: 898037
    > Change-Id: I3e552330378992574d7c08f96f343aba6a7b4d29
    > Reviewed-on: https://chromium-review.googlesource.com/c/1325578
    > Reviewed-by: Carlos IL <carlosil@chromium.org>
    > Reviewed-by: Eugene But <eugenebut@chromium.org>
    > Commit-Queue: Hiroshi Ichikawa <ichikawa@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#607127}
    
    TBR=eugenebut@chromium.org,ichikawa@chromium.org,carlosil@chromium.org
    
    Change-Id: Ieffa8431079996675b1d53f058e4059fa74b8d09
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 898037, 904345
    Reviewed-on: https://chromium-review.googlesource.com/c/1329972
    Reviewed-by: Patrik Höglund <phoglund@chromium.org>
    Commit-Queue: Patrik Höglund <phoglund@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607205}
    Former-commit-id: 87db0e76078d63cad178c06262438ba5ae9351b4

--- ios/web_view/public/cwv_navigation_delegate.h
+++ ios/web_view/public/cwv_navigation_delegate.h
@@ -7,7 +7,6 @@
 
 #import <Foundation/Foundation.h>
 
-#import "cwv_export.h"
 #import "cwv_navigation_type.h"
 
 @protocol CRIWVTranslateDelegate;
@@ -23,10 +22,6 @@ typedef NS_ENUM(NSInteger, CWVSSLErrorDecision) {
   // Ignore the error and reload the page.
   CWVSSLErrorDecisionOverrideErrorAndReload,
 };
-
-// A key of NSError.userInfo. The corresponding value is CWVCertStatus which
-// indicates the type of the SSL error.
-FOUNDATION_EXPORT CWV_EXPORT NSErrorUserInfoKey CWVCertStatusKey;
 
 // Navigation delegate protocol for CWVWebViews.  Allows embedders to hook
 // page loading and receive events for navigation.
@@ -67,10 +62,6 @@ FOUNDATION_EXPORT CWV_EXPORT NSErrorUserInfoKey CWVCertStatusKey;
 // calling |decisionHandler| with CWVSSLErrorDecisionOverrideErrorAndReload. The
 // method can leave the failure as is by calling |decisionHandler| with
 // CWVSSLErrorDecisionDoNothing.
-//
-// error.localizedDescription contains localized description of the SSL error.
-// error.userInfo[CWVCertStatusKey] contains CWVCertStatus which indicates the
-// type of the SSL error.
 //
 // Note: When |decisionHandler| is called with
 // CWVSSLErrorDecisionOverrideErrorAndReload, it must not be called
