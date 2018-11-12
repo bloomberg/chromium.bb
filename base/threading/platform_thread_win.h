e5fa628c1753c6a6a99f9ed373f7011a0820e997
Francois Doray <fdoray@chromium.org>
2018-11-12 17:01:37 +0000

    Base: Ensure that SetCurrentThreadPriority(BACKGROUND) sets CPU priority on Windows (reland).
    Previously landed as
    https://chromium-review.googlesource.com/c/1318438.
    Reverted because
    PlatformThreadWinTest.SetBackgroundThreadModeFailsInIdlePriorityProcess
    failed on Windows 7. This reland adds more possible return values for
    ::GetThreadPriority() on Windows 7 (see diff between ps1 and ps7).
    
    Previously, SetCurrentThreadPriority(BACKGROUND) did not affect CPU
    priority on Windows in a IDLE process when the
    "WindowsThreadModeBackground" feature was enabled. It only affected I/O
    and memory priorities.
    
    With this CL, a second call to ::SetThreadPriority() is made to
    ensure that CPU priority is affected.
    
    TBR=gab@chromium.org
    
    Bug: 901483
    Change-Id: I21ebc6465b3b8d10a956714d55ef88b28bf89fff
    Reviewed-on: https://chromium-review.googlesource.com/c/1326423
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607269}
    Former-commit-id: 0be8c42f6ac8527d89245df06a4a38f8fc2e4958

--- base/threading/platform_thread_win.h
+++ base/threading/platform_thread_win.h
@@ -11,6 +11,7 @@
 #include "base/feature_list.h"
 
 namespace base {
+
 namespace features {
 
 // Use THREAD_MODE_BACKGROUND_BEGIN instead of THREAD_PRIORITY_LOWEST for
@@ -21,6 +22,20 @@ namespace features {
 BASE_EXPORT extern const Feature kWindowsThreadModeBackground;
 
 }  // namespace features
+
+namespace internal {
+
+// The value returned by ::GetThreadPriority() after background thread mode is
+// enabled on Windows 7. Exposed for unit tests.
+constexpr int kWin7BackgroundThreadModePriority = 4;
+
+// Assert that the memory priority of |thread| is |memory_priority|. No-op on
+// Windows 7 because ::GetThreadInformation() is not available. Exposed for unit
+// tests.
+BASE_EXPORT void AssertMemoryPriority(HANDLE thread, int memory_priority);
+
+}  // namespace internal
+
 }  // namespace base
 
 #endif  // BASE_THREADING_PLATFORM_THREAD_WIN_H_
