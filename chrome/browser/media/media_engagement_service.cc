1cf0c2bddbc2bcfa114d3077eef90b524ec83a02
Becca Hughes <beccahughes@chromium.org>
2018-11-13 21:47:44 +0000

    [Media Engagement] Remove histogram
    Remove the ScoreAtStartup histogram as it was causing
    jank and we were not using it.
    
    BUG=900679
    
    Change-Id: I004743c48425c9bb3fbbed67004c7616ef290cf9
    Reviewed-on: https://chromium-review.googlesource.com/c/1330211
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607751}
    Former-commit-id: a78d20743acc9e2777aa375d4dee2d8e970317bc

--- chrome/browser/media/media_engagement_service.cc
+++ chrome/browser/media/media_engagement_service.cc
@@ -23,9 +23,6 @@
 #include "content/public/browser/web_contents.h"
 #include "media/base/media_switches.h"
 
-const char MediaEngagementService::kHistogramScoreAtStartupName[] =
-    "Media.Engagement.ScoreAtStartup";
-
 const char MediaEngagementService::kHistogramURLsDeletedScoreReductionName[] =
     "Media.Engagement.URLsDeletedScoreReduction";
 
@@ -142,18 +139,9 @@ MediaEngagementService::MediaEngagementService(Profile* profile,
         ->ClearSettingsForOneType(CONTENT_SETTINGS_TYPE_MEDIA_ENGAGEMENT);
     SetSchemaVersion(kSchemaVersion);
   }
-
-  // Record the stored scores to a histogram.
-  task_tracker_.PostTask(
-      base::ThreadTaskRunnerHandle::Get().get(), FROM_HERE,
-      base::BindOnce(&MediaEngagementService::RecordStoredScoresToHistogram,
-                     base::Unretained(this)));
 }
 
-MediaEngagementService::~MediaEngagementService() {
-  // Cancel any tasks that depend on |this|.
-  task_tracker_.TryCancelAll();
-}
+MediaEngagementService::~MediaEngagementService() = default;
 
 int MediaEngagementService::GetSchemaVersion() const {
   return profile_->GetPrefs()->GetInteger(prefs::kMediaEngagementSchemaVersion);
@@ -185,14 +173,6 @@ void MediaEngagementService::Shutdown() {
       profile_, ServiceAccessType::IMPLICIT_ACCESS);
   if (history)
     history->RemoveObserver(this);
-}
-
-void MediaEngagementService::RecordStoredScoresToHistogram() {
-  for (const MediaEngagementScore& score : GetAllStoredScores()) {
-    int percentage = round(score.actual_score() * 100);
-    UMA_HISTOGRAM_PERCENTAGE(
-        MediaEngagementService::kHistogramScoreAtStartupName, percentage);
-  }
 }
 
 void MediaEngagementService::OnURLsDeleted(
