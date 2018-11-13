b0169893280bab93db67facd5cba32611bce3ae9
Sergio Villar Senin <svillar@igalia.com>
2018-11-13 10:59:51 +0000

    Migrate RendererUpdater to IdentityManager
    It's currently observer SigninManager but it could instead observe
    IdentityManager. This will allow RenderUpdater to eventually use the
    identity service.
    
    Bug: 887453
    Change-Id: I588bcdaa1e0c5fe02207ad4fabff552193c49766
    Reviewed-on: https://chromium-review.googlesource.com/c/1268339
    Commit-Queue: Sergio Villar <svillar@igalia.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607556}
    Former-commit-id: 1bdcf632f3345eae5f589a5280fb85b4d4f210b1

--- chrome/browser/profiles/renderer_updater.cc
+++ chrome/browser/profiles/renderer_updater.cc
@@ -8,7 +8,7 @@
 
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/renderer_configuration.mojom.h"
 #include "components/content_settings/core/browser/content_settings_utils.h"
@@ -52,9 +52,10 @@ void GetGuestViewDefaultContentSettingRules(
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 }  // namespace
 
-RendererUpdater::RendererUpdater(Profile* profile) : profile_(profile) {
-  signin_manager_ = SigninManagerFactory::GetForProfile(profile_);
-  signin_manager_->AddObserver(this);
+RendererUpdater::RendererUpdater(Profile* profile)
+    : profile_(profile), identity_manager_observer_(this) {
+  identity_manager_ = IdentityManagerFactory::GetForProfile(profile);
+  identity_manager_observer_.Add(identity_manager_);
   variations_http_header_provider_ =
       variations::VariationsHttpHeaderProvider::GetInstance();
   variations_http_header_provider_->AddObserver(this);
@@ -86,12 +87,12 @@ RendererUpdater::RendererUpdater(Profile* profile) : profile_(profile) {
 }
 
 RendererUpdater::~RendererUpdater() {
-  DCHECK(!signin_manager_);
+  DCHECK(!identity_manager_);
 }
 
 void RendererUpdater::Shutdown() {
-  signin_manager_->RemoveObserver(this);
-  signin_manager_ = nullptr;
+  identity_manager_observer_.RemoveAll();
+  identity_manager_ = nullptr;
   variations_http_header_provider_->RemoveObserver(this);
   variations_http_header_provider_ = nullptr;
 }
@@ -152,11 +153,11 @@ RendererUpdater::GetRendererConfiguration(
   return renderer_configuration;
 }
 
-void RendererUpdater::GoogleSigninSucceeded(const AccountInfo& account_info) {
+void RendererUpdater::OnPrimaryAccountSet(const AccountInfo& account_info) {
   UpdateAllRenderers();
 }
 
-void RendererUpdater::GoogleSignedOut(const AccountInfo& account_info) {
+void RendererUpdater::OnPrimaryAccountCleared(const AccountInfo& account_info) {
   UpdateAllRenderers();
 }
 
@@ -181,7 +182,7 @@ void RendererUpdater::UpdateRenderer(
           force_google_safesearch_.GetValue(),
           force_youtube_restrict_.GetValue(),
           allowed_domains_for_apps_.GetValue(),
-          signin_manager_->IsAuthenticated()
+          identity_manager_->HasPrimaryAccount()
               ? cached_variation_ids_header_signed_in_
               : cached_variation_ids_header_));
 }
