3fce74ddf5f2f9236afcaf45baf105f07f1a41cc
James Hawkins <jhawkins@chromium.org>
2018-11-13 02:06:14 +0000

    Reland crrev.com/c/1331573 w/ build fixes in place.
    Bug: 899324
    Test: none
    
    Original change description:
    
    > CryptAuth: Remove unused ConnectionFinder.
    >
    > Bug: 899324
    > Test: none
    > Change-Id: I30f07136ac7c3ed3ac49b678e5f45d250fcbe65f
    > Reviewed-on: https://chromium-review.googlesource.com/c/1331573
    > Reviewed-by: Jeremy Klein <jlklein@chromium.org>
    > Commit-Queue: James Hawkins <jhawkins@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#607302}
    
    R=hansberry@chromium.org
    
    Change-Id: Ia14bbe45b5b76a94c79aea5c4b687695df843163
    Reviewed-on: https://chromium-review.googlesource.com/c/1333109
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607436}
    Former-commit-id: a006576c66d9a17148fa56e81780ab29b8956689

--- components/cryptauth/ble/bluetooth_low_energy_weave_client_connection.cc
+++ components/cryptauth/ble/bluetooth_low_energy_weave_client_connection.cc
@@ -17,7 +17,6 @@
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/timer/timer.h"
 #include "chromeos/components/proximity_auth/logging/logging.h"
-#include "components/cryptauth/connection_finder.h"
 #include "components/cryptauth/wire_message.h"
 #include "device/bluetooth/bluetooth_gatt_connection.h"
 
