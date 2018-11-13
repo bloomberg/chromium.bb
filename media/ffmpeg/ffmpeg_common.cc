56e515f9ce3d0b4d0f3eaa4ba7e3f81faf0abaf7
Chrome Cunningham <chcunningham@chromium.org>
2018-11-13 23:49:26 +0000

    Revert "media: Add H264 profile extraction support"
    This reverts commit be364762e28bffb0f41d6bb08e9989ff1b9fda60 [formerly ffa5c05c6e68b8abfbe4f23f63292ae35d7772b6].
    
    Reason for revert: broke webkit_tests on Mojo Linux bot. Likely something to do with gn config that lacks proprietary codecs.
    
    Original change's description:
    > media: Add H264 profile extraction support
    > 
    > Without the ffmpeg decoder configured, libavformat is unable to get the
    > codec profile and chromium ffmpeg wrapper code choose some defaults.
    > This will mess up the decode scenarios where non-ffmpeg decoders like vaapi
    > are in use. Fortunately, we can still get hold of the ffmpeg's
    > AVStream extradata param which should contain the
    > H264 AVCDecoderConfigurationRecord (ISO/IEC 14496-15).
    > So we use internal parser for extracting profile information
    > from this AVCDecoderConfigurationRecord.
    > 
    > BUG=784610
    > TEST=media_unittests:FFmpegCommonTest.VerifyH264Profile
    > 
    > Change-Id: Id67c7026eefb5bb73e6c5fbbab7a362cb4553ab1
    > Reviewed-on: https://chromium-review.googlesource.com/c/1292605
    > Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#607769}
    
    TBR=dalecurtis@chromium.org,chcunningham@chromium.org,sreerenj.balachandran@intel.com
    
    Change-Id: I1cc9a3236d7187115566dff882c586b00dca713b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 784610
    Reviewed-on: https://chromium-review.googlesource.com/c/1334664
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607817}
    Former-commit-id: 1f57bfc33cce988ef3cdd57a5960ec86705260a6

--- media/ffmpeg/ffmpeg_common.cc
+++ media/ffmpeg/ffmpeg_common.cc
@@ -16,7 +16,6 @@
 #include "media/base/media_util.h"
 #include "media/base/video_decoder_config.h"
 #include "media/base/video_util.h"
-#include "media/formats/mp4/box_definitions.h"
 #include "media/media_buildflags.h"
 
 namespace media {
@@ -480,27 +479,12 @@ bool AVStreamToVideoDecoderConfig(const AVStream* stream,
   // actually handle capabilities requests correctly. http://crbug.com/784610
   VideoCodecProfile profile = VIDEO_CODEC_PROFILE_UNKNOWN;
   switch (codec) {
-    case kCodecH264: {
-      profile = ProfileIDToVideoCodecProfile(codec_context->profile);
-#if BUILDFLAG(USE_PROPRIETARY_CODECS)
-      // if the profile is still unknown, try to extract it from
-      // the extradata using the internal parser
-      if (profile == VIDEO_CODEC_PROFILE_UNKNOWN && codec_context->extradata &&
-          codec_context->extradata_size) {
-        mp4::AVCDecoderConfigurationRecord avc_config;
-        if (avc_config.Parse(codec_context->extradata,
-                             codec_context->extradata_size)) {
-          profile = ProfileIDToVideoCodecProfile(avc_config.profile_indication);
-        }
-      }
-#endif
-      // All the heuristics failed, let's assign a default profile
-      if (profile == VIDEO_CODEC_PROFILE_UNKNOWN)
-        profile = H264PROFILE_BASELINE;
-
+#if !BUILDFLAG(ENABLE_FFMPEG_VIDEO_DECODERS)
+    case kCodecH264:
       format = PIXEL_FORMAT_I420;
+      profile = H264PROFILE_BASELINE;
       break;
-    }
+#endif
     case kCodecVP8:
 #if !BUILDFLAG(ENABLE_FFMPEG_VIDEO_DECODERS)
       format = PIXEL_FORMAT_I420;
