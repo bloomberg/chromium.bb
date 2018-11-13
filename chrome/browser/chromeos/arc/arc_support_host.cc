a3bf8b2a95d0072467b7b5d9d17ab90a7f4f4f0b
Josh Horwich <jhorwich@chromium.org>
2018-11-13 01:41:37 +0000

    arc: Update and add child-account opt-in strings
    Update the ARC opt-in strings with minor tweaks, and use specific
    strings for child-account scenarios.
    
    Also remove the ability for a device owner to toggle the usage &
    diagnostics collection setting from within ARC opt-in flow.
    
    Bug: 805304
    Test: Run ARC opt-in during new-user setup (OOBE)
    Test: Run ARC opt-in for existing user ('in-product')
    Change-Id: Ic8dcbbaddd5b690baea469b4688d38f3c2378a98
    Reviewed-on: https://chromium-review.googlesource.com/c/1313764
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: Josh Horwich <jhorwich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607417}
    Former-commit-id: ec79c47c1753326cb9bed392e5ac75d46f38d3a5

--- chrome/browser/chromeos/arc/arc_support_host.cc
+++ chrome/browser/chromeos/arc/arc_support_host.cc
@@ -29,6 +29,7 @@
 #include "chrome/grit/generated_resources.h"
 #include "components/consent_auditor/consent_auditor.h"
 #include "components/user_manager/known_user.h"
+#include "components/user_manager/user_manager.h"
 #include "extensions/browser/extension_registry.h"
 #include "services/identity/public/cpp/identity_manager.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -451,6 +452,9 @@ void ArcSupportHost::SetRequestOpenAppCallbackForTesting(
 bool ArcSupportHost::Initialize() {
   DCHECK(message_host_);
 
+  const bool is_child =
+      user_manager::UserManager::Get()->IsLoggedInAsChildUser();
+
   auto loadtime_data = std::make_unique<base::DictionaryValue>();
   loadtime_data->SetString("appWindow", l10n_util::GetStringUTF16(
                                             IDS_ARC_PLAYSTORE_ICON_TITLE_BETA));
@@ -491,19 +495,29 @@ bool ArcSupportHost::Initialize() {
       l10n_util::GetStringUTF16(IDS_ARC_OPT_IN_DIALOG_TERMS_OF_SERVICE));
   loadtime_data->SetString(
       "textMetricsEnabled",
-      l10n_util::GetStringUTF16(IDS_ARC_OPT_IN_DIALOG_METRICS_ENABLED));
+      l10n_util::GetStringUTF16(
+          is_child ? IDS_ARC_OPT_IN_DIALOG_METRICS_ENABLED_CHILD
+                   : IDS_ARC_OPT_IN_DIALOG_METRICS_ENABLED));
   loadtime_data->SetString(
       "textMetricsDisabled",
-      l10n_util::GetStringUTF16(IDS_ARC_OPT_IN_DIALOG_METRICS_DISABLED));
+      l10n_util::GetStringUTF16(
+          is_child ? IDS_ARC_OPT_IN_DIALOG_METRICS_DISABLED_CHILD
+                   : IDS_ARC_OPT_IN_DIALOG_METRICS_DISABLED));
   loadtime_data->SetString(
       "textMetricsManagedEnabled",
-      l10n_util::GetStringUTF16(IDS_ARC_OPT_IN_DIALOG_METRICS_MANAGED_ENABLED));
-  loadtime_data->SetString("textMetricsManagedDisabled",
-                           l10n_util::GetStringUTF16(
-                               IDS_ARC_OPT_IN_DIALOG_METRICS_MANAGED_DISABLED));
+      l10n_util::GetStringUTF16(
+          is_child ? IDS_ARC_OPT_IN_DIALOG_METRICS_MANAGED_ENABLED_CHILD
+                   : IDS_ARC_OPT_IN_DIALOG_METRICS_MANAGED_ENABLED));
+  loadtime_data->SetString(
+      "textMetricsManagedDisabled",
+      l10n_util::GetStringUTF16(
+          is_child ? IDS_ARC_OPT_IN_DIALOG_METRICS_MANAGED_DISABLED_CHILD
+                   : IDS_ARC_OPT_IN_DIALOG_METRICS_MANAGED_DISABLED));
   loadtime_data->SetString(
       "textBackupRestore",
-      l10n_util::GetStringUTF16(IDS_ARC_OPT_IN_DIALOG_BACKUP_RESTORE));
+      l10n_util::GetStringUTF16(is_child
+                                    ? IDS_ARC_OPT_IN_DIALOG_BACKUP_RESTORE_CHILD
+                                    : IDS_ARC_OPT_IN_DIALOG_BACKUP_RESTORE));
   loadtime_data->SetString("textPaiService",
                            l10n_util::GetStringUTF16(IDS_ARC_OPT_IN_PAI));
   loadtime_data->SetString(
@@ -511,7 +525,8 @@ bool ArcSupportHost::Initialize() {
       l10n_util::GetStringUTF16(IDS_ARC_OPT_IN_GOOGLE_SERVICE_CONFIRMATION));
   loadtime_data->SetString(
       "textLocationService",
-      l10n_util::GetStringUTF16(IDS_ARC_OPT_IN_LOCATION_SETTING));
+      l10n_util::GetStringUTF16(is_child ? IDS_ARC_OPT_IN_LOCATION_SETTING_CHILD
+                                         : IDS_ARC_OPT_IN_LOCATION_SETTING));
   loadtime_data->SetString(
       "serverError",
       l10n_util::GetStringUTF16(IDS_ARC_SERVER_COMMUNICATION_ERROR));
@@ -520,13 +535,19 @@ bool ArcSupportHost::Initialize() {
       l10n_util::GetStringUTF16(IDS_CONTROLLED_SETTING_POLICY));
   loadtime_data->SetString(
       "learnMoreStatistics",
-      l10n_util::GetStringUTF16(IDS_ARC_OPT_IN_LEARN_MORE_STATISTICS));
+      l10n_util::GetStringUTF16(is_child
+                                    ? IDS_ARC_OPT_IN_LEARN_MORE_STATISTICS_CHILD
+                                    : IDS_ARC_OPT_IN_LEARN_MORE_STATISTICS));
   loadtime_data->SetString(
       "learnMoreBackupAndRestore",
-      l10n_util::GetStringUTF16(IDS_ARC_OPT_IN_LEARN_MORE_BACKUP_AND_RESTORE));
+      l10n_util::GetStringUTF16(
+          is_child ? IDS_ARC_OPT_IN_LEARN_MORE_BACKUP_AND_RESTORE_CHILD
+                   : IDS_ARC_OPT_IN_LEARN_MORE_BACKUP_AND_RESTORE));
   loadtime_data->SetString(
       "learnMoreLocationServices",
-      l10n_util::GetStringUTF16(IDS_ARC_OPT_IN_LEARN_MORE_LOCATION_SERVICES));
+      l10n_util::GetStringUTF16(
+          is_child ? IDS_ARC_OPT_IN_LEARN_MORE_LOCATION_SERVICES_CHILD
+                   : IDS_ARC_OPT_IN_LEARN_MORE_LOCATION_SERVICES));
   loadtime_data->SetString(
       "learnMorePaiService",
       l10n_util::GetStringUTF16(IDS_ARC_OPT_IN_LEARN_MORE_PAI_SERVICE));
@@ -642,6 +663,7 @@ void ArcSupportHost::OnMessage(const base::DictionaryValue& message) {
     auto* identity_manager = IdentityManagerFactory::GetForProfile(profile_);
     DCHECK(identity_manager->HasPrimaryAccount());
     std::string account_id = identity_manager->GetPrimaryAccountId();
+    bool is_child = user_manager::UserManager::Get()->IsLoggedInAsChildUser();
 
     // Record acceptance of ToS if it was shown to the user, otherwise simply
     // record acceptance of an empty ToS.
@@ -666,7 +688,8 @@ void ArcSupportHost::OnMessage(const base::DictionaryValue& message) {
       backup_and_restore_consent.set_confirmation_grd_id(
           IDS_ARC_OPT_IN_DIALOG_BUTTON_AGREE);
       backup_and_restore_consent.add_description_grd_ids(
-          IDS_ARC_OPT_IN_DIALOG_BACKUP_RESTORE);
+          is_child ? IDS_ARC_OPT_IN_DIALOG_BACKUP_RESTORE_CHILD
+                   : IDS_ARC_OPT_IN_DIALOG_BACKUP_RESTORE);
       backup_and_restore_consent.set_status(is_backup_restore_enabled
                                                 ? UserConsentTypes::GIVEN
                                                 : UserConsentTypes::NOT_GIVEN);
@@ -684,7 +707,8 @@ void ArcSupportHost::OnMessage(const base::DictionaryValue& message) {
       location_service_consent.set_confirmation_grd_id(
           IDS_ARC_OPT_IN_DIALOG_BUTTON_AGREE);
       location_service_consent.add_description_grd_ids(
-          IDS_ARC_OPT_IN_LOCATION_SETTING);
+          is_child ? IDS_ARC_OPT_IN_LOCATION_SETTING_CHILD
+                   : IDS_ARC_OPT_IN_LOCATION_SETTING);
       location_service_consent.set_status(is_location_service_enabled
                                               ? UserConsentTypes::GIVEN
                                               : UserConsentTypes::NOT_GIVEN);
