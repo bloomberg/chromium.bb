89c6e8ada28d5129608edfa6436dae266f26eb52
Lily Chen <chlily@chromium.org>
2018-11-13 00:40:58 +0000

    Revert "Add reprioritization to TransportConnectJobs"
    This reverts commit 13801a3b96d190517ecb3e47aaf8bf4b6b0d097c [formerly 668af6e315a4fe61d9ada4c7902dfa7c057e9f45].
    
    Reason for revert: Bug in RespectLimits where a ConnectJob with the IGNORE_LIMITS flag set is associated with a Request that does not have that flag set.
    
    Original change's description:
    > Add reprioritization to TransportConnectJobs
    > 
    > This change makes it possible to reprioritize socket pool ConnectJobs
    > to match their corresponding Requests. For TransportConnectJobs that are
    > reprioritized while undergoing host resolution, the corresponding host
    > resolver request is also reprioritized to match the connect job.
    > 
    > Bug: 166689
    > Change-Id: Icaed423c6f363fa15e9e379aa135a3b98330d1a1
    > Reviewed-on: https://chromium-review.googlesource.com/c/1320210
    > Commit-Queue: Lily Chen <chlily@chromium.org>
    > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#606129}
    
    TBR=mmenke@chromium.org,chlily@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 166689
    Change-Id: I02f58f99a9391871962b0c96df643e2a85403330
    Reviewed-on: https://chromium-review.googlesource.com/c/1331141
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607393}
    Former-commit-id: 74d2077221c6e67dc3c22127eebe7d95e783aa95

--- net/socket/client_socket_pool_base.h
+++ net/socket/client_socket_pool_base.h
@@ -98,14 +98,11 @@ class NET_EXPORT_PRIVATE ConnectJob {
   // Accessors
   const std::string& group_name() const { return group_name_; }
   const NetLogWithSource& net_log() { return net_log_; }
-  RequestPriority priority() const { return priority_; }
 
   // Releases ownership of the underlying socket to the caller.
   // Returns the released socket, or NULL if there was a connection
   // error.
   std::unique_ptr<StreamSocket> PassSocket();
-
-  void ChangePriority(RequestPriority priority);
 
   // Begins connecting the socket.  Returns OK on success, ERR_IO_PENDING if it
   // cannot complete synchronously without blocking, or another net error code
@@ -129,7 +126,7 @@ class NET_EXPORT_PRIVATE ConnectJob {
   const NetLogWithSource& net_log() const { return net_log_; }
 
  protected:
-  void set_priority(RequestPriority priority) { priority_ = priority; }
+  RequestPriority priority() const { return priority_; }
   const SocketTag& socket_tag() const { return socket_tag_; }
   ClientSocketPool::RespectLimits respect_limits() const {
     return respect_limits_;
@@ -145,8 +142,6 @@ class NET_EXPORT_PRIVATE ConnectJob {
  private:
   virtual int ConnectInternal() = 0;
 
-  virtual void ChangePriorityInternal(RequestPriority priority) {}
-
   void LogConnectStart();
   void LogConnectCompletion(int net_error);
 
@@ -155,7 +150,8 @@ class NET_EXPORT_PRIVATE ConnectJob {
 
   const std::string group_name_;
   const base::TimeDelta timeout_duration_;
-  RequestPriority priority_;
+  // TODO(akalin): Support reprioritization.
+  const RequestPriority priority_;
   const SocketTag socket_tag_;
   const ClientSocketPool::RespectLimits respect_limits_;
   // Timer to abort jobs that take too long.
