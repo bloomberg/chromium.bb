6d2ea623b343f73fec572cdefb2eeddabdf61819
Steve Anton <steveanton@chromium.org>
2018-11-13 19:45:23 +0000

    Implement RTCQuicStream.write()
    Bug: 874296
    Change-Id: I1786acb3b64fa0450e76fe27300c178081d1e191
    Reviewed-on: https://chromium-review.googlesource.com/c/1285528
    Commit-Queue: Steve Anton <steveanton@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607708}
    Former-commit-id: 61720441ceae8e19ba59d32e651b7c9354ac45f0

--- third_party/blink/renderer/modules/peerconnection/adapters/test/mock_p2p_quic_stream.h
+++ third_party/blink/renderer/modules/peerconnection/adapters/test/mock_p2p_quic_stream.h
@@ -12,11 +12,23 @@ namespace blink {
 
 class MockP2PQuicStream : public testing::NiceMock<P2PQuicStream> {
  public:
+  explicit MockP2PQuicStream(P2PQuicStream::Delegate** delegate_out = nullptr) {
+    if (delegate_out) {
+      // Ensure the caller has not left the delegate_out value floating.
+      DCHECK_EQ(nullptr, *delegate_out);
+      EXPECT_CALL(*this, SetDelegate(testing::_))
+          .WillOnce(testing::Invoke(
+              [delegate_out](P2PQuicStream::Delegate* delegate) {
+                *delegate_out = delegate;
+              }));
+    }
+  }
+
   // P2PQuicStream overrides.
   MOCK_METHOD0(Reset, void());
-  MOCK_METHOD1(SetDelegate, void(Delegate*));
-  MOCK_METHOD2(WriteData, void(Vector<uint8_t>, bool));
   MOCK_METHOD1(MarkReceivedDataConsumed, void(uint32_t));
+  MOCK_METHOD2(WriteData, void(Vector<uint8_t>, bool));
+  MOCK_METHOD1(SetDelegate, void(Delegate*));
 };
 
 }  // namespace blink
--- third_party/blink/renderer/modules/peerconnection/rtc_quic_stream_test.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_quic_stream_test.cc
@@ -16,13 +16,34 @@ namespace blink {
 namespace {
 
 using testing::_;
+using testing::ElementsAre;
 using testing::InvokeWithoutArgs;
 using testing::Return;
 using testing::SaveArg;
 
+NotShared<DOMUint8Array> CreateUint8Array(const Vector<uint8_t>& data) {
+  return NotShared<DOMUint8Array>(
+      DOMUint8Array::Create(data.data(), data.size()));
+}
+
+NotShared<DOMUint8Array> CreateUint8ArrayOfLength(uint32_t length) {
+  return NotShared<DOMUint8Array>(DOMUint8Array::Create(length));
+}
+
 }  // namespace
 
-class RTCQuicStreamTest : public RTCQuicTransportTest {};
+class RTCQuicStreamTest : public RTCQuicTransportTest {
+ public:
+  RTCQuicStream* CreateQuicStream(V8TestingScope& scope,
+                                  MockP2PQuicStream* mock_stream) {
+    auto p2p_quic_transport = std::make_unique<MockP2PQuicTransport>();
+    EXPECT_CALL(*p2p_quic_transport, CreateStream())
+        .WillOnce(Return(mock_stream));
+    Persistent<RTCQuicTransport> quic_transport =
+        CreateConnectedQuicTransport(scope, std::move(p2p_quic_transport));
+    return quic_transport->createStream(ASSERT_NO_EXCEPTION);
+  }
+};
 
 // Test that RTCQuicTransport.createStream causes CreateStream to be called on
 // the underlying transport and a P2PQuicStream::Delegate to be set on the
@@ -169,6 +190,148 @@ TEST_F(RTCQuicStreamTest, PendingOnRemoteResetIgnoredAfterReset) {
   RunUntilIdle();
 
   EXPECT_EQ("closed", quic_stream->state());
+}
+
+// The following group tests write(), finish(), writeBufferedAmount(), and
+// maxWriteBufferdAmount().
+
+// Test that write() adds to writeBufferedAmount().
+TEST_F(RTCQuicStreamTest, WriteAddsToWriteBufferedAmount) {
+  V8TestingScope scope;
+
+  auto p2p_quic_stream = std::make_unique<MockP2PQuicStream>();
+  Persistent<RTCQuicStream> stream =
+      CreateQuicStream(scope, p2p_quic_stream.get());
+
+  stream->write(CreateUint8Array({1, 2}), ASSERT_NO_EXCEPTION);
+  EXPECT_EQ(2u, stream->writeBufferedAmount());
+
+  stream->write(CreateUint8Array({3, 4, 5}), ASSERT_NO_EXCEPTION);
+  EXPECT_EQ(5u, stream->writeBufferedAmount());
+
+  RunUntilIdle();
+}
+
+// Test that write() calls WriteData() on the underlying P2PQuicStream.
+TEST_F(RTCQuicStreamTest, WriteCallsWriteData) {
+  V8TestingScope scope;
+
+  auto p2p_quic_stream = std::make_unique<MockP2PQuicStream>();
+  EXPECT_CALL(*p2p_quic_stream, WriteData(_, _))
+      .WillOnce(testing::Invoke([](Vector<uint8_t> data, bool fin) {
+        EXPECT_THAT(data, ElementsAre(1, 2, 3, 4));
+        EXPECT_FALSE(fin);
+      }));
+
+  Persistent<RTCQuicStream> stream =
+      CreateQuicStream(scope, p2p_quic_stream.get());
+  stream->write(CreateUint8Array({1, 2, 3, 4}), ASSERT_NO_EXCEPTION);
+
+  RunUntilIdle();
+}
+
+// Test that write() with no data succeeds but does not post a WriteData() to
+// the underlying P2PQuicStream.
+TEST_F(RTCQuicStreamTest, WriteWithEmptyArrayDoesNotCallWriteData) {
+  V8TestingScope scope;
+
+  auto p2p_quic_stream = std::make_unique<MockP2PQuicStream>();
+  EXPECT_CALL(*p2p_quic_stream, WriteData(_, _)).Times(0);
+
+  Persistent<RTCQuicStream> stream =
+      CreateQuicStream(scope, p2p_quic_stream.get());
+  stream->write(CreateUint8Array({}), ASSERT_NO_EXCEPTION);
+
+  RunUntilIdle();
+}
+
+// Test that finish() calls WriteData() on the underlying P2PQuicStream.
+TEST_F(RTCQuicStreamTest, FinishCallsWriteData) {
+  V8TestingScope scope;
+
+  auto p2p_quic_stream = std::make_unique<MockP2PQuicStream>();
+  EXPECT_CALL(*p2p_quic_stream, WriteData(_, _))
+      .WillOnce(testing::Invoke([](Vector<uint8_t> data, bool fin) {
+        EXPECT_THAT(data, ElementsAre());
+        EXPECT_TRUE(fin);
+      }));
+
+  Persistent<RTCQuicStream> stream =
+      CreateQuicStream(scope, p2p_quic_stream.get());
+  stream->finish();
+
+  RunUntilIdle();
+}
+
+// Test that writeBufferedAmount is decreased when receiving OnWriteDataConsumed
+// from the underlying P2PQuicStream.
+TEST_F(RTCQuicStreamTest, OnWriteDataConsumedSubtractsFromWriteBufferedAmount) {
+  V8TestingScope scope;
+
+  P2PQuicStream::Delegate* stream_delegate = nullptr;
+  auto p2p_quic_stream = std::make_unique<MockP2PQuicStream>(&stream_delegate);
+
+  Persistent<RTCQuicStream> stream =
+      CreateQuicStream(scope, p2p_quic_stream.get());
+  stream->write(CreateUint8ArrayOfLength(4), ASSERT_NO_EXCEPTION);
+
+  RunUntilIdle();
+
+  ASSERT_TRUE(stream_delegate);
+  stream_delegate->OnWriteDataConsumed(4);
+
+  RunUntilIdle();
+
+  EXPECT_EQ(0u, stream->writeBufferedAmount());
+}
+
+// Test that writeBufferedAmount is set to 0 if the stream was reset by the
+// remote peer.
+TEST_F(RTCQuicStreamTest, OnRemoteResetSetsWriteBufferedAmountToZero) {
+  V8TestingScope scope;
+
+  P2PQuicStream::Delegate* stream_delegate = nullptr;
+  auto p2p_quic_stream = std::make_unique<MockP2PQuicStream>(&stream_delegate);
+
+  Persistent<RTCQuicStream> stream =
+      CreateQuicStream(scope, p2p_quic_stream.get());
+  stream->write(CreateUint8ArrayOfLength(4), ASSERT_NO_EXCEPTION);
+
+  RunUntilIdle();
+
+  ASSERT_TRUE(stream_delegate);
+  stream_delegate->OnRemoteReset();
+
+  RunUntilIdle();
+
+  EXPECT_EQ(0u, stream->writeBufferedAmount());
+
+  RunUntilIdle();
+}
+
+// Test that write throws an InvalidStateError if the stream was reset by the
+// remote peer.
+TEST_F(RTCQuicStreamTest, WriteThrowsIfRemoteReset) {
+  V8TestingScope scope;
+
+  P2PQuicStream::Delegate* stream_delegate = nullptr;
+  auto p2p_quic_stream = std::make_unique<MockP2PQuicStream>(&stream_delegate);
+
+  Persistent<RTCQuicStream> stream =
+      CreateQuicStream(scope, p2p_quic_stream.get());
+
+  RunUntilIdle();
+
+  ASSERT_TRUE(stream_delegate);
+  stream_delegate->OnRemoteReset();
+
+  RunUntilIdle();
+
+  stream->write(CreateUint8ArrayOfLength(1), scope.GetExceptionState());
+  EXPECT_EQ(DOMExceptionCode::kInvalidStateError,
+            scope.GetExceptionState().CodeAs<DOMExceptionCode>());
+
+  RunUntilIdle();
 }
 
 }  // namespace blink
