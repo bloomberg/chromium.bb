75dc4fb637a242752e62c9ec55a5dec7a0ba054d
Shawn Gallea <sagallea@google.com>
2018-11-13 21:05:08 +0000

    EXO: Refactored text input interface into its own file
    Moved text_input_manager_v1 interface into another file,
    to be followed by refactoring of more protocols.
    
    Refactor wayland_text_input_delegate.* from server.cc
    with no new code.
    
    Modify server_util to add more utilities
    
    Bug: 896710
    Test: Compiled on ChromeOS.
    Change-Id: I2739c538a16640fdaebed1cd7493d300dd42a5a9
    Reviewed-on: https://chromium-review.googlesource.com/c/1306406
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Commit-Queue: Shawn Gallea <sagallea@google.com>
    Cr-Commit-Position: refs/heads/master@{#607738}
    Former-commit-id: aad8b28a8aade3673fc088a466d5af7b99c1bd33

--- components/exo/wayland/server_util.h
+++ components/exo/wayland/server_util.h
@@ -10,8 +10,26 @@
 #include <memory>
 
 #include "base/memory/ptr_util.h"
+#include "base/time/time.h"
+#include "components/exo/surface.h"
+#include "ui/base/class_property.h"
+
+struct wl_resource;
+
+namespace display {
+class Display;
+}
+
+namespace gfx {
+class Insets;
+class Rect;
+class Size;
+}  // namespace gfx
 
 namespace exo {
+
+class DataOffer;
+
 namespace wayland {
 
 template <class T>
@@ -38,6 +56,34 @@ void SetImplementation(wl_resource* resource,
   wl_resource_set_implementation(resource, implementation, user_data.release(),
                                  DestroyUserData<T>);
 }
+
+// Convert a timestamp to a time value that can be used when interfacing
+// with wayland. Note that we cast a int64_t value to uint32_t which can
+// potentially overflow.
+uint32_t TimeTicksToMilliseconds(base::TimeTicks ticks);
+uint32_t NowInMilliseconds();
+
+wl_resource* GetSurfaceResource(Surface* surface);
+void SetSurfaceResource(Surface* surface, wl_resource* resource);
+
+wl_resource* GetDataOfferResource(const DataOffer* data_offer);
+void SetDataOfferResource(DataOffer* data_offer,
+                          wl_resource* data_offer_resource);
+
+// Scale the |child_bounds| in such a way that if it should fill the
+// |parent_size|'s width/height, it returns the |parent_size_in_pixel|'s
+// width/height.
+gfx::Rect ScaleBoundsToPixelSnappedToParent(
+    const gfx::Size& parent_size_in_pixel,
+    const gfx::Size& parent_size,
+    float device_scale_factor,
+    const gfx::Rect& child_bounds);
+
+// Create the insets make sure that work area will be within the chrome's
+// work area when converted to the pixel on client side.
+// TODO(oshima): We should send these information in pixel so that
+// client do not have to convert it back.
+gfx::Insets GetAdjustedInsets(const display::Display& display);
 
 }  // namespace wayland
 }  // namespace exo
