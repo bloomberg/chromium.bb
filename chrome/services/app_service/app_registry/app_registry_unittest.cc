a4763e07e6d36899f7e6aeefff22e74c41203a36
Nigel Tao <nigeltao@chromium.org>
2018-11-14 02:03:33 +0000

    Remove obsolete AppRegistry.GetApps method
    Bug: 826982
    Change-Id: I19e9c4af2f3e53952d8ff8d697a43509d23eb809
    Reviewed-on: https://chromium-review.googlesource.com/c/1334668
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#607841}
    Former-commit-id: 7774f1d9bdc685e3102afdc6101b86d925f0b629

--- chrome/services/app_service/app_registry/app_registry_unittest.cc
+++ chrome/services/app_service/app_registry/app_registry_unittest.cc
@@ -27,36 +27,6 @@ class AppRegistryTest : public testing::Test {
     AppRegistry::RegisterPrefs(pref_service->registry());
     return std::make_unique<AppRegistry>(std::move(pref_service));
   }
-
-  void GetAppInfos(AppRegistry* app_registry) {
-    base::RunLoop run_loop;
-    app_registry->GetApps(base::BindOnce(&AppRegistryTest::GetAppsCallback,
-                                         base::Unretained(this),
-                                         run_loop.QuitClosure()));
-    run_loop.Run();
-  }
-
-  void GetAppsCallback(base::OnceClosure quit_closure,
-                       std::vector<apps::mojom::AppInfoPtr> app_infos) {
-    app_infos_ = std::move(app_infos);
-    std::move(quit_closure).Run();
-  }
-
-  const std::vector<apps::mojom::AppInfoPtr>& app_infos() const {
-    return app_infos_;
-  }
-
-  const apps::mojom::AppInfoPtr& FindWithId(const std::string& id) const {
-    return *(std::find_if(
-        app_infos_.begin(), app_infos_.end(),
-        [&id](const apps::mojom::AppInfoPtr& info) { return info->id == id; }));
-  }
-
- private:
-  // Required for RunLoop waiting.
-  base::MessageLoop message_loop_;
-
-  std::vector<apps::mojom::AppInfoPtr> app_infos_;
 };
 
 TEST_F(AppRegistryTest, SetAppPreferred) {
@@ -92,45 +62,6 @@ TEST_F(AppRegistryTest, SetAppPreferred) {
             registry->GetIfAppPreferredForTesting(kTestAppId1));
   EXPECT_EQ(apps::mojom::AppPreferred::kNotPreferred,
             registry->GetIfAppPreferredForTesting(kTestAppId2));
-}
-
-TEST_F(AppRegistryTest, GetPreferredApps) {
-  auto app_registry = CreateAppRegistry();
-  GetAppInfos(app_registry.get());
-  EXPECT_EQ(std::vector<apps::mojom::AppInfoPtr>{}, app_infos());
-
-  app_registry->SetAppPreferred(kTestAppId1,
-                                apps::mojom::AppPreferred::kPreferred);
-  GetAppInfos(app_registry.get());
-  EXPECT_EQ(1u, app_infos().size());
-  EXPECT_EQ(apps::mojom::AppPreferred::kPreferred, app_infos()[0]->preferred);
-
-  app_registry->SetAppPreferred(kTestAppId2,
-                                apps::mojom::AppPreferred::kPreferred);
-  GetAppInfos(app_registry.get());
-  EXPECT_EQ(2u, app_infos().size());
-  EXPECT_EQ(apps::mojom::AppPreferred::kPreferred,
-            FindWithId(kTestAppId1)->preferred);
-  EXPECT_EQ(apps::mojom::AppPreferred::kPreferred,
-            FindWithId(kTestAppId2)->preferred);
-
-  app_registry->SetAppPreferred(kTestAppId1,
-                                apps::mojom::AppPreferred::kNotPreferred);
-  GetAppInfos(app_registry.get());
-  EXPECT_EQ(2u, app_infos().size());
-  EXPECT_EQ(apps::mojom::AppPreferred::kNotPreferred,
-            FindWithId(kTestAppId1)->preferred);
-  EXPECT_EQ(apps::mojom::AppPreferred::kPreferred,
-            FindWithId(kTestAppId2)->preferred);
-
-  app_registry->SetAppPreferred(kTestAppId2,
-                                apps::mojom::AppPreferred::kNotPreferred);
-  GetAppInfos(app_registry.get());
-  EXPECT_EQ(2u, app_infos().size());
-  EXPECT_EQ(apps::mojom::AppPreferred::kNotPreferred,
-            FindWithId(kTestAppId1)->preferred);
-  EXPECT_EQ(apps::mojom::AppPreferred::kNotPreferred,
-            FindWithId(kTestAppId2)->preferred);
 }
 
 }  // namespace apps
