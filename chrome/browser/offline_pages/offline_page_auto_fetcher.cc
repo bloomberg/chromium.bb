9da52113a067355b53b115bf4575d9e9435e6ceb
Dan Harrington <harringtond@chromium.org>
2018-11-14 18:30:49 +0000

    auto-fetch: store tab ID in ClientId
    This changes how the ClientId is generated for this feature. The tabID
    is not used yet, but will be
    needed to implement the system notification correctly.
    
    I manually verified that FindTab() returns the correct ID.
    
    Bug: 883486
    Change-Id: I466fc5d3eb1b6f373d969a3d8eb88de7e96854ce
    Reviewed-on: https://chromium-review.googlesource.com/c/1327409
    Commit-Queue: Dan H <harringtond@google.com>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608057}
    Former-commit-id: 847e913a24a005925f33267c1ba40da5e053fb03

--- chrome/browser/offline_pages/offline_page_auto_fetcher.cc
+++ chrome/browser/offline_pages/offline_page_auto_fetcher.cc
@@ -6,14 +6,37 @@
 
 #include <utility>
 
+#include "chrome/browser/android/tab_android.h"
 #include "chrome/browser/offline_pages/offline_page_auto_fetcher_service.h"
 #include "chrome/browser/offline_pages/offline_page_auto_fetcher_service_factory.h"
+#include "chrome/browser/ui/android/tab_model/tab_model.h"
+#include "chrome/browser/ui/android/tab_model/tab_model_list.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
+#include "content/public/browser/web_contents.h"
 #include "mojo/public/cpp/bindings/strong_binding.h"
 #include "url/gurl.h"
 
 namespace offline_pages {
+namespace {
+
+TabAndroid* FindTab(content::RenderFrameHost* render_frame_host) {
+  content::WebContents* web_contents =
+      content::WebContents::FromRenderFrameHost(render_frame_host);
+  if (!web_contents)
+    return nullptr;
+  TabModel* tab_model = TabModelList::GetTabModelForWebContents(web_contents);
+  if (!tab_model)
+    return nullptr;
+  // For this use-case, it's OK to fail if the active tab doesn't match
+  // web_contents.
+  if (tab_model->GetActiveWebContents() != web_contents)
+    return nullptr;
+
+  return tab_model->GetTabAt(tab_model->GetActiveIndex());
+}
+
+}  // namespace
 
 OfflinePageAutoFetcher::OfflinePageAutoFetcher(
     content::RenderFrameHost* render_frame_host)
@@ -23,9 +46,14 @@ OfflinePageAutoFetcher::~OfflinePageAutoFetcher() = default;
 
 void OfflinePageAutoFetcher::TrySchedule(bool user_requested,
                                          TryScheduleCallback callback) {
+  TabAndroid* tab = FindTab(render_frame_host_);
+  if (!tab) {
+    std::move(callback).Run(OfflinePageAutoFetcherScheduleResult::kOtherError);
+    return;
+  }
   GetService()->TrySchedule(user_requested,
                             render_frame_host_->GetLastCommittedURL(),
-                            std::move(callback));
+                            tab->GetAndroidId(), std::move(callback));
 }
 
 void OfflinePageAutoFetcher::CancelSchedule() {
