e8e63acaa7f33566b8c7ac9371a22e481d66cd33
Vladislav Kaznacheev <kaznacheev@google.com>
2018-11-14 00:36:52 +0000

    Fix hidden shelf reveal on mouse hover
    On some scale factors the bottom most pixel is outside of
    the display bounds. Snapping shelf region to display edge fixes
    the problem.
    
    Bug: 899007
    Test: manual
    Change-Id: I1eeb342756b46e683d0f30f96f1c428fbb74d7ca
    Reviewed-on: https://chromium-review.googlesource.com/c/1320013
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Manu Cornet <manucornet@chromium.org>
    Commit-Queue: Vladislav Kaznacheev <kaznacheev@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#605748}(cherry picked from commit 26571eb8eb41ae222efbc8231dc39abba9c32e3a [formerly c034b9e7859bf3b3cb1c33a9ca1a89df23782356])
    Reviewed-on: https://chromium-review.googlesource.com/c/1334840
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3578@{#674}
    Cr-Branched-From: abbd18012032883c3113b3bfc5d59d25b4836c5f [formerly 4226ddf99103e493d7afb23a4c7902ee496108b6]-refs/heads/master@{#599034}
    Former-commit-id: 3514fd1dd0ceb4476036939277aba7ebab49ae71

--- ash/shelf/shelf_layout_manager.cc
+++ ash/shelf/shelf_layout_manager.cc
@@ -942,7 +942,8 @@ gfx::Rect ShelfLayoutManager::GetVisibleShelfBounds() const {
   gfx::Rect shelf_region = shelf_widget_->GetWindowBoundsInScreen();
   DCHECK(!display_.bounds().IsEmpty());
   shelf_region.Intersect(display_.bounds());
-  return shelf_region;
+  return screen_util::SnapBoundsToDisplayEdge(shelf_region,
+                                              shelf_widget_->GetNativeWindow());
 }
 
 gfx::Rect ShelfLayoutManager::GetAutoHideShowShelfRegionInScreen() const {
