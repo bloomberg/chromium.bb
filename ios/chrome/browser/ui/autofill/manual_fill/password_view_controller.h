883fbf3a427a59cebab6fc73098886c71e1ead4f
David Jean <djean@google.com>
2018-11-15 15:37:56 +0000

    [ios] use view controller base class for manual fill passwords
    Bug: 845472
    Change-Id: I012839e8ee5dee6ae54d66327f1de5f38ff96d71
    Reviewed-on: https://chromium-review.googlesource.com/c/1335599
    Commit-Queue: David Jean <djean@chromium.org>
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608376}
    Former-commit-id: e676b7b4c45de16791c3148da4214c7e88a1a4bd

--- ios/chrome/browser/ui/autofill/manual_fill/password_view_controller.h
+++ ios/chrome/browser/ui/autofill/manual_fill/password_view_controller.h
@@ -7,32 +7,24 @@
 
 #import <UIKit/UIKit.h>
 
+#import "ios/chrome/browser/ui/autofill/manual_fill/fallback_view_controller.h"
 #import "ios/chrome/browser/ui/autofill/manual_fill/password_consumer.h"
-#import "ios/chrome/browser/ui/table_view/chrome_table_view_controller.h"
 
 namespace manual_fill {
 
 extern NSString* const PasswordSearchBarAccessibilityIdentifier;
 extern NSString* const PasswordTableViewAccessibilityIdentifier;
-extern NSString* const PasswordDoneButtonAccessibilityIdentifier;
 
 }  // namespace manual_fill
 
 // This class presents a list of usernames and passwords in a table view.
 @interface PasswordViewController
-    : ChromeTableViewController<ManualFillPasswordConsumer>
+    : FallbackViewController<ManualFillPasswordConsumer>
 
 - (instancetype)initWithSearchController:(UISearchController*)searchController
     NS_DESIGNATED_INITIALIZER;
 
-- (instancetype)initWithTableViewStyle:(UITableViewStyle)style
-                           appBarStyle:
-                               (ChromeTableViewControllerStyle)appBarStyle
-    NS_UNAVAILABLE;
-
-// If set to YES, the controller will add negative content insets inverse to the
-// ones added by UITableViewController to accommodate for the keyboard.
-@property(nonatomic, assign) BOOL contentInsetsAlwaysEqualToSafeArea;
+- (instancetype)init NS_UNAVAILABLE;
 
 @end
 
