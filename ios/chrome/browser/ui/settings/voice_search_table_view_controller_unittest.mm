6d6fb01f9366561d7ee088c71c12043fcd20cefb
Yi Su <mrsuyi@chromium.org>
2018-11-15 11:59:36 +0000

    Migrate VoicesearchCollectionViewController from CollectionViewController to ChromeTableViewControllerStyleWithAppBar.
    This CL migrates the VoicesearchCollectionViewController from
    CollectionViewController, which is based on MDCCollectionViewController,
    to ChromeTableViewController, which is based on UITableViewController.
    
    Bug: 894791
    Change-Id: I3aee06d07573c2ca3560eb0dc093951382d7fb2c
    Reviewed-on: https://chromium-review.googlesource.com/c/1335934
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608329}
    Former-commit-id: e484b25d69d75f05ae4d43f2ac19f1cbbedc18ff

--- ios/chrome/browser/ui/settings/voice_search_table_view_controller_unittest.mm
+++ ios/chrome/browser/ui/settings/voice_search_table_view_controller_unittest.mm
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/settings/voicesearch_collection_view_controller.h"
+#import "ios/chrome/browser/ui/settings/voice_search_table_view_controller.h"
 
 #include <memory>
 
@@ -14,9 +14,8 @@
 #include "components/prefs/pref_member.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/testing_pref_service.h"
-#import "ios/chrome/browser/ui/collection_view/collection_view_controller.h"
-#import "ios/chrome/browser/ui/collection_view/collection_view_controller_test.h"
-#import "ios/chrome/browser/ui/settings/cells/legacy/legacy_settings_switch_item.h"
+#import "ios/chrome/browser/ui/settings/cells/settings_switch_item.h"
+#import "ios/chrome/browser/ui/table_view/chrome_table_view_controller_test.h"
 #include "ios/chrome/browser/voice/speech_input_locale_config_impl.h"
 #include "ios/public/provider/chrome/browser/voice/voice_search_prefs.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -30,20 +29,20 @@
 
 namespace {
 
-class VoicesearchCollectionViewControllerTest
-    : public CollectionViewControllerTest {
+class VoiceSearchTableViewControllerTest
+    : public ChromeTableViewControllerTest {
  protected:
-  VoicesearchCollectionViewControllerTest()
+  VoiceSearchTableViewControllerTest()
       : scoped_task_environment_(
             base::test::ScopedTaskEnvironment::MainThreadType::UI) {}
 
   void SetUp() override {
-    CollectionViewControllerTest::SetUp();
+    ChromeTableViewControllerTest::SetUp();
     pref_service_ = CreateLocalState();
   }
 
-  CollectionViewController* InstantiateController() override {
-    return [[VoicesearchCollectionViewController alloc]
+  ChromeTableViewController* InstantiateController() override {
+    return [[VoiceSearchTableViewController alloc]
         initWithPrefs:pref_service_.get()];
   }
 
@@ -55,18 +54,16 @@ class VoicesearchCollectionViewControllerTest
     return std::unique_ptr<PrefService>(prefs);
   }
 
-  LegacySettingsSwitchCell* GetSwitchCell() {
-    return base::mac::ObjCCastStrict<LegacySettingsSwitchCell>(
-        [controller().collectionView
-            cellForItemAtIndexPath:[NSIndexPath indexPathForItem:0
-                                                       inSection:0]]);
+  SettingsSwitchCell* GetSwitchCell() {
+    return base::mac::ObjCCastStrict<SettingsSwitchCell>([controller().tableView
+        cellForRowAtIndexPath:[NSIndexPath indexPathForItem:0 inSection:0]]);
   }
 
   base::test::ScopedTaskEnvironment scoped_task_environment_;
   std::unique_ptr<PrefService> pref_service_;
 };
 
-TEST_F(VoicesearchCollectionViewControllerTest, NumberOfSectionsAndItems) {
+TEST_F(VoiceSearchTableViewControllerTest, NumberOfSectionsAndItems) {
   CreateController();
   CheckController();
 
@@ -82,7 +79,7 @@ TEST_F(VoicesearchCollectionViewControllerTest, NumberOfSectionsAndItems) {
 
 // Taps the last item in the languages list to ensure that the app does not
 // crash.  This tests for a regression in crbug.com/661358.
-TEST_F(VoicesearchCollectionViewControllerTest, TapTheLastItemInTheList) {
+TEST_F(VoiceSearchTableViewControllerTest, TapTheLastItemInTheList) {
   CreateController();
 
   const std::vector<voice::SpeechInputLocale>& locales =
@@ -93,20 +90,20 @@ TEST_F(VoicesearchCollectionViewControllerTest, TapTheLastItemInTheList) {
             static_cast<unsigned int>(NumberOfItemsInSection(1)));
 
   // Simulate a tap on the last item in the list.
-  [controller() collectionView:[controller() collectionView]
-      didSelectItemAtIndexPath:[NSIndexPath indexPathForItem:locales.size()
-                                                   inSection:1]];
+  [controller() tableView:[controller() tableView]
+      didSelectRowAtIndexPath:[NSIndexPath indexPathForItem:locales.size()
+                                                  inSection:1]];
 }
 
-TEST_F(VoicesearchCollectionViewControllerTest,
+TEST_F(VoiceSearchTableViewControllerTest,
        TestModel_TextToSpeechOff_TTSSupported) {
   CreateController();
-  LegacySettingsSwitchItem* switchItem = GetCollectionViewItem(0, 0);
+  SettingsSwitchItem* switchItem = GetTableViewItem(0, 0);
   EXPECT_FALSE(switchItem.isOn);
   EXPECT_TRUE(switchItem.isEnabled);
 }
 
-TEST_F(VoicesearchCollectionViewControllerTest,
+TEST_F(VoiceSearchTableViewControllerTest,
        TestModel_TextToSpeechOn_TTSSupported) {
   // Enable the global TTS setting.
   BooleanPrefMember textToSpeechEnabled;
@@ -114,12 +111,12 @@ TEST_F(VoicesearchCollectionViewControllerTest,
   textToSpeechEnabled.SetValue(true);
 
   CreateController();
-  LegacySettingsSwitchItem* switchItem = GetCollectionViewItem(0, 0);
+  SettingsSwitchItem* switchItem = GetTableViewItem(0, 0);
   EXPECT_TRUE(switchItem.isOn);
   EXPECT_TRUE(switchItem.isEnabled);
 }
 
-TEST_F(VoicesearchCollectionViewControllerTest,
+TEST_F(VoiceSearchTableViewControllerTest,
        TestModel_TextToSpeechOff_TTSNotSupported) {
   // Set current language to a language that doesn't support TTS.
   StringPrefMember selectedLanguage;
@@ -127,12 +124,12 @@ TEST_F(VoicesearchCollectionViewControllerTest,
   selectedLanguage.SetValue("af-ZA");
 
   CreateController();
-  LegacySettingsSwitchItem* switchItem = GetCollectionViewItem(0, 0);
+  SettingsSwitchItem* switchItem = GetTableViewItem(0, 0);
   EXPECT_FALSE(switchItem.isOn);
   EXPECT_FALSE(switchItem.isEnabled);
 }
 
-TEST_F(VoicesearchCollectionViewControllerTest,
+TEST_F(VoiceSearchTableViewControllerTest,
        TestModel_TextToSpeechOn_TTSNotSupported) {
   // Set current language to a language that doesn't support TTS.
   StringPrefMember selectedLanguage;
@@ -145,7 +142,7 @@ TEST_F(VoicesearchCollectionViewControllerTest,
   textToSpeechEnabled.SetValue(true);
 
   CreateController();
-  LegacySettingsSwitchItem* switchItem = GetCollectionViewItem(0, 0);
+  SettingsSwitchItem* switchItem = GetTableViewItem(0, 0);
   EXPECT_FALSE(switchItem.isOn);
   EXPECT_FALSE(switchItem.isEnabled);
 }
