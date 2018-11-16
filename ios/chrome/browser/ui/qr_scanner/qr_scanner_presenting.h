f9f161983a9ede3c7d68966756e39e99bd655c91
edchin <edchin@chromium.org>
2018-11-16 16:56:08 +0000

    [ios] Migrate QR scanner from BVC
    In addition to migrating the QR scanner coordinator
    from BVC to BrowserCoordinator, this CL does a little
    refactoring on QR scanner coordinator.
    
    Previously, presentation/dismissal was delegate out to
    the BVC. Since coordinators are created with a baseViewController,
    the presentation/dismissal can happen inside the box.
    It does not need to be delegated outside the box.
    
    Bug: 905524
    
    Change-Id: I69dbe4eb4387bed7b42fc952dbee2a3af4059c71
    Reviewed-on: https://chromium-review.googlesource.com/c/1337635
    Commit-Queue: edchin <edchin@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608811}
    Former-commit-id: d018fef099ff59ccc6f766f09ed53920591ca3df

--- ios/chrome/browser/ui/qr_scanner/qr_scanner_presenting.h
+++ ios/chrome/browser/ui/qr_scanner/qr_scanner_presenting.h
@@ -2,15 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_UI_QR_SCANNER_REQUIREMENTS_QR_SCANNER_PRESENTING_H_
-#define IOS_CHROME_BROWSER_UI_QR_SCANNER_REQUIREMENTS_QR_SCANNER_PRESENTING_H_
+#ifndef IOS_CHROME_BROWSER_UI_QR_SCANNER_QR_SCANNER_PRESENTING_H_
+#define IOS_CHROME_BROWSER_UI_QR_SCANNER_QR_SCANNER_PRESENTING_H_
 
 // QRScannerPresenting contains methods that control how the QR scanner UI is
-// presented and dismissed on screen.
+// dismissed on screen.
 @protocol QRScannerPresenting
-
-// Asks the implementer to present the given |controller|.
-- (void)presentQRScannerViewController:(UIViewController*)controller;
 
 // Asks the implementer to dismiss the given |controller| and call the given
 // |completion| afterwards.
@@ -19,4 +16,4 @@
 
 @end
 
-#endif  // IOS_CHROME_BROWSER_UI_QR_SCANNER_REQUIREMENTS_QR_SCANNER_PRESENTING_H_
+#endif  // IOS_CHROME_BROWSER_UI_QR_SCANNER_QR_SCANNER_PRESENTING_H_
