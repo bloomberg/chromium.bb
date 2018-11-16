37a3efd1f05327d7b76439ba4119e7b61fcff5b5
Etienne Pierre-Doray <etiennep@chromium.org>
2018-11-16 16:57:50 +0000

    [TaskScheduler]: Migrate off of AssertBlockingAllowedDeprecated in /third_party/leveldatabase
    base::AssertBlockingAllowedDeprecated is deprecated in favor of
    ScopedBlockingCall, which serves as a precise annotation of
    the scope that may/will block.
    
    Please make sure of the following:
      - ScopedBlockingCall is instantiated in a scope with minimal CPU usage.
        If this is not the case, ScopedBlockingCall should be instantiated
        closer to the blocking call. See scoped_blocking_call.h for more
        info. Please let me know when/where the blocking call happens if this needs
        to be changed.
      - Parameter |blocking_type| matches expectation:
          MAY_BLOCK: The call might block (e.g. file I/O that might hit in memory cache).
          WILL_BLOCK: The call will definitely block (e.g. cache already checked and now pinging
            server synchronously).
        See BlockingType for more info. While I assumed MAY_BLOCK by default, that might
        not be the best fit if we know that this callsite is guaranteed to block.
      - The ScopedBlockingCall's scope covers the entirety of the blocking operation
        previously asserted against by the AssertBlockingAllowed().
      - Calls to blocking //base APIs don't need to be annotated
        with ScopedBlockingCall. All blocking //base APIs (e.g. base::ReadFileToString, base::File::Read,
        base::SysInfo::AmountOfFreeDiskSpace, base::WaitableEvent::Wait, etc.) have their
        own internal annotations.
    
    Refer to the top-level CL if necessary :
    https://chromium-review.googlesource.com/c/chromium/src/+/1338391
    
    Please CQ if LGTY!
    
    This CL was uploaded by git cl split.
    
    R=pwnall@chromium.org
    
    Bug: 903957
    Change-Id: I81021e9c637e588b0cdcc464218df1770cedc635
    Reviewed-on: https://chromium-review.googlesource.com/c/1338128
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608812}
    Former-commit-id: 0eed991aefb9dab90ac66f96d953323f0149962b

--- third_party/leveldatabase/env_chromium.cc
+++ third_party/leveldatabase/env_chromium.cc
@@ -29,7 +29,7 @@
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/system/sys_info.h"
-#include "base/threading/thread_restrictions.h"
+#include "base/threading/scoped_blocking_call.h"
 #include "base/trace_event/memory_dump_manager.h"
 #include "base/trace_event/memory_dump_provider.h"
 #include "base/trace_event/process_memory_dump.h"
@@ -73,7 +73,7 @@ static const char kDatabaseNameSuffixForRebuildDB[] = "__tmp_for_rebuild";
 static base::File::Error GetDirectoryEntries(const FilePath& dir_param,
                                              std::vector<FilePath>* result) {
   TRACE_EVENT0("leveldb", "ChromiumEnv::GetDirectoryEntries");
-  base::AssertBlockingAllowedDeprecated();
+  base::ScopedBlockingCall scoped_blocking_call(base::BlockingType::MAY_BLOCK);
   result->clear();
 #if defined(OS_WIN)
   FilePath dir_filepath = dir_param.Append(FILE_PATH_LITERAL("*"));
