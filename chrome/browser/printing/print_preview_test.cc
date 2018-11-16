ae1edf43bf1affb00d6e49ba857d6d133c5212ed
Evan Stade <estade@chromium.org>
2018-11-16 21:17:48 +0000

    Remove BrowserWithTestWindowTest's AtExitManager.
    It was added in 2012 for PrintPreviewTest but is no longer needed and is
    causing issues with single process mash tests, where OzonePlatform only
    expects to get initialized once. If this happens in a test's SetUp(),
    and parts of it get destroyed by the AtExitManager, they won't be re-
    initialized for later tests. Tests will still pass in isolation but not
    when run as a suite.
    
    This CL also simplifies PrintPreviewTest a bit, which is the test fixture
    for which the AtExitManager was originally added.
    
    Bug: 905430
    Change-Id: I4617aed098962f4eea6a9d1bfe052e3902278557
    Reviewed-on: https://chromium-review.googlesource.com/c/1340798
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608948}
    Former-commit-id: e8499d4449b8559c9734109c7f032f2f4811a55a

--- chrome/browser/printing/print_preview_test.cc
+++ chrome/browser/printing/print_preview_test.cc
@@ -5,18 +5,9 @@
 #include "chrome/browser/printing/print_preview_test.h"
 
 #include "chrome/test/base/dialog_test_browser_window.h"
-#include "content/public/browser/plugin_service.h"
 
 PrintPreviewTest::PrintPreviewTest() {}
 PrintPreviewTest::~PrintPreviewTest() {}
-
-void PrintPreviewTest::SetUp() {
-  BrowserWithTestWindowTest::SetUp();
-
-  // The PluginService will be destroyed at the end of the test (due to the
-  // ShadowingAtExitManager in our base class).
-  content::PluginService::GetInstance()->Init();
-}
 
 BrowserWindow* PrintPreviewTest::CreateBrowserWindow() {
   return new DialogTestBrowserWindow;
