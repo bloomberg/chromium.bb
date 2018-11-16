ec54fac1abbaf9ecb4e717e4b582fe0433ccee7f
Sky Malice <skym@google.com>
2018-11-16 18:24:12 +0000

    Reland "[Feed] When rescheduling, just set period to 1 day instead of going to native."
    This is a reland of 1c9a772b24bf0dc0491dfbc1b5169d2e3e92d3ca [formerly 0126fcebdc27586784929eb1f0fafc6666a0cfae]
    
    Original change's description:
    > [Feed] When rescheduling, just set period to 1 day instead of going to
    > native.
    > 
    > Bug: 901331
    > Change-Id: I65b3d748cb96669fa78a712552f82a44873debf3
    > Reviewed-on: https://chromium-review.googlesource.com/c/1315929
    > Commit-Queue: Sky Malice <skym@chromium.org>
    > Reviewed-by: Filip Gorski <fgorski@chromium.org>
    > Cr-Original-Commit-Position: refs/heads/master@{#606873}(cherry picked from commit b169a0df7fbb016e3b657f0c2c78eff3b949e47d [formerly 1dc99fe0bd2792457e4cc526e945df228ec47234])
    > Reviewed-on: https://chromium-review.googlesource.com/c/1338231
    > Reviewed-by: Sky Malice <skym@chromium.org>
    > Cr-Commit-Position: refs/branch-heads/3578@{#697}
    > Cr-Branched-From: abbd18012032883c3113b3bfc5d59d25b4836c5f [formerly 4226ddf99103e493d7afb23a4c7902ee496108b6]-refs/heads/master@{#599034}
    
    Bug: 901331
    Change-Id: Ie67142894a49e6410ca75f305fa35a3851529399
    Reviewed-on: https://chromium-review.googlesource.com/c/1338728
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3578@{#730}
    Cr-Branched-From: abbd18012032883c3113b3bfc5d59d25b4836c5f [formerly 4226ddf99103e493d7afb23a4c7902ee496108b6]-refs/heads/master@{#599034}
    Former-commit-id: 1c1912debafb92707a667e3858c1f113afaa00b2

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSchedulerBridge.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSchedulerBridge.java
@@ -117,12 +117,6 @@ public class FeedSchedulerBridge implements FeedScheduler {
     }
 
     @Override
-    public void onTaskReschedule() {
-        assert mNativeBridge != 0;
-        nativeOnTaskReschedule(mNativeBridge);
-    }
-
-    @Override
     public void onSuggestionConsumed() {
         assert mNativeBridge != 0;
         nativeOnSuggestionConsumed(mNativeBridge);
@@ -158,6 +152,5 @@ public class FeedSchedulerBridge implements FeedScheduler {
     private native void nativeOnForegrounded(long nativeFeedSchedulerBridge);
     private native void nativeOnFixedTimer(
             long nativeFeedSchedulerBridge, Callback<Void> onCompletion);
-    private native void nativeOnTaskReschedule(long nativeFeedSchedulerBridge);
     private native void nativeOnSuggestionConsumed(long nativeFeedSchedulerBridge);
 }
