94e8b2fad668f19f0fd2cf473a9d9b28b40b4d9e
Lei Zhang <thestig@chromium.org>
2018-11-16 19:28:07 +0000

    Verify the "pdf-form-save" is enabled in the saveData_() JS handler.
    Aside from adding the sanity check, this also hooks up loadTimeData in
    pdf_viewer.js, and demonstrates how to plumb a feature flag from the
    browser into pdf_viewer.js.
    
    BUG=61248
    
    Change-Id: I48850afaac17854af785d04a8ca93078ebd8532f
    Reviewed-on: https://chromium-review.googlesource.com/c/1339244
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608890}
    Former-commit-id: dffec5931c04893b07f790383287235a929ce7e9

--- chrome/browser/extensions/api/resources_private/resources_private_api.cc
+++ chrome/browser/extensions/api/resources_private/resources_private_api.cc
@@ -12,8 +12,13 @@
 #include "chrome/common/extensions/api/resources_private.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/strings/grit/components_strings.h"
+#include "pdf/buildflags.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/webui/web_ui_util.h"
+
+#if BUILDFLAG(ENABLE_PDF)
+#include "pdf/pdf_features.h"
+#endif
 
 // To add a new component to this API, simply:
 // 1. Add your component to the Component enum in
@@ -54,6 +59,14 @@ void AddStringsForPdf(base::DictionaryValue* dict) {
   SetL10nString(dict, "tooltipZoomOut", IDS_PDF_TOOLTIP_ZOOM_OUT);
 }
 
+void AddAdditionalDataForPdf(base::DictionaryValue* dict) {
+#if BUILDFLAG(ENABLE_PDF)
+  dict->SetKey("pdfFormSaveEnabled",
+               base::Value(base::FeatureList::IsEnabled(
+                   chrome_pdf::features::kSaveEditedPDFForm)));
+#endif
+}
+
 }  // namespace
 
 namespace get_strings = api::resources_private::GetStrings;
@@ -65,7 +78,7 @@ ResourcesPrivateGetStringsFunction::~ResourcesPrivateGetStringsFunction() {}
 ExtensionFunction::ResponseAction ResourcesPrivateGetStringsFunction::Run() {
   std::unique_ptr<get_strings::Params> params(
       get_strings::Params::Create(*args_));
-  std::unique_ptr<base::DictionaryValue> dict(new base::DictionaryValue);
+  auto dict = std::make_unique<base::DictionaryValue>();
 
   api::resources_private::Component component = params->component;
 
@@ -75,6 +88,7 @@ ExtensionFunction::ResponseAction ResourcesPrivateGetStringsFunction::Run() {
       break;
     case api::resources_private::COMPONENT_PDF:
       AddStringsForPdf(dict.get());
+      AddAdditionalDataForPdf(dict.get());
       break;
     case api::resources_private::COMPONENT_NONE:
       NOTREACHED();
