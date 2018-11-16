9f3dcbda7f55cf3b861d420691ffeba03d914f4b
Nico Weber <thakis@chromium.org>
2018-11-16 16:08:16 +0000

    Add start of script that checks subwaterfalls are consistent with main waterfall.
    And fix luci-milo.cfg to pass the check:
    
    - Add jumbo-rel bots that were on the subwaterfalls to the main waterfall
    - Add two Webkit builder bots to main waterfall
    - Add Deterministic Linux (dbg) to chromium.linux subwaterfall
    - Make sure each bot has a single name
    
    project_pb2.py was created by grabbing the current version of
    https://chromium.googlesource.com/infra/luci/luci-go/+/master/milo/api/config/project.proto
    (9a3aea9951f) and running
    
      out/gn/protoc --python_out=. project.proto
    
    I also want to check that the category names are consistent across main and
    subwaterfalls, but this CL is already large enough, so let's do that in a
    separate change.
    
    Bug: none
    Change-Id: I0088621e3b7e7fb9698dd3f0853460105bb8ccd0
    Reviewed-on: https://chromium-review.googlesource.com/c/1301795
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Ryan Tseng <hinoka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#608791}
    Former-commit-id: 082717b51abdb00ca4d52f1b9be4ea55310c99e9

---
+++ infra/config/global/lint-luci-milo.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Checks that the main console and subconsole configs are consistent."""
+
+import collections
+import difflib
+import os
+import sys
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+SRC_ROOT = os.path.join(THIS_DIR, '..', '..', '..')
+sys.path.insert(1, os.path.join(
+    SRC_ROOT, "third_party", "protobuf", "python"))
+
+import google.protobuf.text_format
+import project_pb2
+
+
+def compare_builders(name, main_builders, sub_builders):
+  # Checks that the builders on a subwaterfall on the main waterfall
+  # are consistent with the builders on that subwaterfall's main page.
+  # For example, checks that the builders on the "chromium.win" section
+  # are the same as on the dedicated standalone chromium.win waterfall.
+  main_names = [', '.join(builder.name) for builder in main_builders]
+  sub_names = [', '.join(builder.name) for builder in sub_builders]
+
+  if main_names != sub_names:
+    print '%s: bot name lists different:' % name
+    print '\n'.join(difflib.unified_diff(main_names, sub_names,
+                                         fromfile='main', tofile='sub',
+                                         lineterm=''))
+    print
+    return
+
+
+def main():
+  project = project_pb2.Project()
+  with open(os.path.join(THIS_DIR, 'luci-milo.cfg'), 'rb') as f:
+    google.protobuf.text_format.Parse(f.read(), project)
+
+  # Maps subwaterfall name to list of builders on that subwaterfall
+  # on the main waterfall.
+  subwaterfalls = collections.defaultdict(list)
+  for console in project.consoles:
+    if console.id == 'main':
+      # Chromium main waterfall console.
+      for builder in console.builders:
+        subwaterfall = builder.category.split('|', 1)[0]
+        subwaterfalls[subwaterfall].append(builder)
+
+  for console in project.consoles:
+    if console.id in subwaterfalls:
+      compare_builders(console.id, subwaterfalls[console.id], console.builders)
+
+
+if __name__ == '__main__':
+  sys.exit(main())
