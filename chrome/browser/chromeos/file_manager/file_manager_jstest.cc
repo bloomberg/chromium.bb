c2ff319134b082d140bafd86b4d71db6acd21b38
Noel Gordon <noel@chromium.org>
2018-11-19 23:05:41 +0000

    Closure compile providers_model_unittest
     - use let and const and add doc comment annotations.
     - update LoadTimeData fake to include DRIVE_OFFLINE_COLLECTION_LABEL
       and DRIVE_SHARED_WITH_ME_COLLECTION_LABEL to avoid JS exceptions
       about them being missing / undefined when running the unittest.
     - rm duplicate NOT_MOUNTED_SINGLE_PROVIDING_EXTENSION definition.
     - use VolumeManagerCommon.Source types rather than bare strings.
     - create & install mock chrome.fileManagerPrivate.
     - create & install mock VolumeManager.
     - run JS formatter over the code.
    
    Bug: 905933
    Change-Id: I7dabbb8ff72f79d290eaeffcb941280715f93660
    Reviewed-on: https://chromium-review.googlesource.com/c/1343397
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Stuart Langley <slangley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609487}
    Former-commit-id: 82e13c27cd285f191257d4c719c2ee82c77f6857

--- chrome/browser/chromeos/file_manager/file_manager_jstest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_jstest.cc
@@ -178,8 +178,7 @@ IN_PROC_BROWSER_TEST_F(FileManagerJsTest, ImageOrientation) {
 }
 
 IN_PROC_BROWSER_TEST_F(FileManagerJsTest, ProvidersModel) {
-  RunTest(base::FilePath(
-      FILE_PATH_LITERAL("foreground/js/providers_model_unittest.html")));
+  RunGeneratedTest("/foreground/js/providers_model_unittest.html");
 }
 
 IN_PROC_BROWSER_TEST_F(FileManagerJsTest, FilesTooltip) {
