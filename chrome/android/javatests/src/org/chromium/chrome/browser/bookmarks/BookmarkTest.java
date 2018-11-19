71dd23398a930dacf1ab257b6c09dceb7355498b
Donn Denman <donnd@chromium.org>
2018-11-19 23:32:32 +0000

    [Bookmarks] Disable testSearchBookmarks on Oreo.
    Conditionally disables this test due to flakiness on this configuration.
    
    BUG=906788
    
    TBR=twellington@chromium.org
    
    Change-Id: Ief4f08ae1d9f8c73b3055d136af655498a97892f
    Reviewed-on: https://chromium-review.googlesource.com/c/1343361
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609507}
    Former-commit-id: 1b4b09c669ad8035d3b26d13d956cf8a6838959b

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkTest.java
@@ -4,6 +4,7 @@
 
 package org.chromium.chrome.browser.bookmarks;
 
+import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.MediumTest;
@@ -273,7 +274,10 @@ public class BookmarkTest {
 
     @Test
     @MediumTest
-    public void testSearchBookmarks() throws Exception {
+    @DisableIf.Build(message = "Disable on Oreo which is flaky now.  https://crbug.com/906788",
+            sdk_is_less_than = Build.VERSION_CODES.P, sdk_is_greater_than = Build.VERSION_CODES.N)
+    public void
+    testSearchBookmarks() throws Exception {
         BookmarkPromoHeader.forcePromoStateForTests(BookmarkPromoHeader.PromoState.PROMO_SYNC);
         addBookmark(TEST_PAGE_TITLE_GOOGLE, mTestPage);
         addBookmark(TEST_PAGE_TITLE_FOO, mTestPageFoo);
