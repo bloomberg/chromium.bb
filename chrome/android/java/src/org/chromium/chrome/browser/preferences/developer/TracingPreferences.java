e57f265265bf1aa317dd9d2c489e82eb7abd6b93
Eric Seckler <eseckler@chromium.org>
2018-11-20 10:15:14 +0000

    tracing: Make tracing mode and categories configurable (android)
    Adds preferences for configuring the tracing mode and enabled tracing
    categories to the mobile tracing preferences.
    
    Bug: 898512
    Change-Id: I23583dd24868918ce29eebce68568344b7c3c2f0
    Binary-Size: feature implementation, strings will not be translated.
    Reviewed-on: https://chromium-review.googlesource.com/c/1340300
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609657}
    Former-commit-id: e53874394b9b2d4f1a031b471d064e7f6d34977c

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/developer/TracingPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/developer/TracingPreferences.java
@@ -5,26 +5,157 @@
 package org.chromium.chrome.browser.preferences.developer;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.support.annotation.IntDef;
 
+import org.chromium.base.ContextUtils;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.preferences.PreferenceUtils;
 import org.chromium.chrome.browser.tracing.TracingController;
 import org.chromium.chrome.browser.tracing.TracingNotificationManager;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Settings fragment that shows options for recording a performance trace.
  */
 public class TracingPreferences extends PreferenceFragment implements TracingController.Observer {
+    static final String NON_DEFAULT_CATEGORY_PREFIX = "disabled-by-default-";
+
+    @VisibleForTesting
+    static final String UI_PREF_DEFAULT_CATEGORIES = "default_categories";
+    @VisibleForTesting
+    static final String UI_PREF_NON_DEFAULT_CATEGORIES = "non_default_categories";
+    @VisibleForTesting
+    static final String UI_PREF_MODE = "mode";
     @VisibleForTesting
     static final String UI_PREF_START_RECORDING = "start_recording";
     @VisibleForTesting
     static final String UI_PREF_TRACING_STATUS = "tracing_status";
 
+    private static final String PREF_TRACING_CATEGORIES = "tracing_categories";
+    private static final String PREF_TRACING_MODE = "tracing_mode";
+
+    // Ordered map that maps tracing mode string to resource id for its description.
+    private static final Map<String, Integer> TRACING_MODES = createTracingModesMap();
+
+    private Preference mPrefDefaultCategories;
+    private Preference mPrefNondefaultCategories;
+    private ListPreference mPrefMode;
     private Preference mPrefStartRecording;
     private Preference mPrefTracingStatus;
+
+    /**
+     * Type of a tracing category indicating whether it is enabled by default or not.
+     */
+    @IntDef({CategoryType.DEFAULT, CategoryType.NON_DEFAULT})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface CategoryType {
+        int DEFAULT = 0;
+        int NON_DEFAULT = 1;
+    }
+
+    private static Map<String, Integer> createTracingModesMap() {
+        Map<String, Integer> map = new LinkedHashMap<>();
+        map.put("record-until-full", R.string.prefs_tracing_mode_record_until_full);
+        map.put("record-as-much-as-possible",
+                R.string.prefs_tracing_mode_record_as_much_as_possible);
+        map.put("record-continuously", R.string.prefs_tracing_mode_record_continuously);
+        return map;
+    }
+
+    /**
+     * @return the current set of all enabled categories, irrespective of their type.
+     */
+    public static Set<String> getEnabledCategories() {
+        Set<String> enabled =
+                ContextUtils.getAppSharedPreferences().getStringSet(PREF_TRACING_CATEGORIES, null);
+        if (enabled == null) {
+            enabled = new HashSet<>();
+            // By default, enable all default categories.
+            for (String category : TracingController.getInstance().getKnownCategories()) {
+                if (getCategoryType(category) == CategoryType.DEFAULT) enabled.add(category);
+            }
+        }
+        return enabled;
+    }
+
+    /**
+     * Get the set of enabled categories of a given category type.
+     *
+     * @param type the category type.
+     * @return the current set of enabled categories with |type|.
+     */
+    public static Set<String> getEnabledCategories(@CategoryType int type) {
+        Set<String> enabled = new HashSet<>();
+        for (String category : getEnabledCategories()) {
+            if (type == getCategoryType(category)) {
+                enabled.add(category);
+            }
+        }
+        return enabled;
+    }
+
+    /**
+     * Set the enabled categories of a given category type. The set of enabled categories with
+     * different types will not be changed.
+     *
+     * @param type the category type.
+     * @param enabledOfType the set of enabled categories with the given |type|.
+     */
+    public static void setEnabledCategories(@CategoryType int type, Set<String> enabledOfType) {
+        Set<String> enabled = new HashSet<>(enabledOfType);
+        for (String category : getEnabledCategories()) {
+            if (type != getCategoryType(category)) {
+                enabled.add(category);
+            }
+        }
+        ContextUtils.getAppSharedPreferences()
+                .edit()
+                .putStringSet(PREF_TRACING_CATEGORIES, enabled)
+                .apply();
+    }
+
+    /**
+     * Get the type of a category derived from its name.
+     * @param category the name of the category.
+     * @return the type of the category.
+     */
+    public static @CategoryType int getCategoryType(String category) {
+        return category.startsWith(NON_DEFAULT_CATEGORY_PREFIX) ? CategoryType.NON_DEFAULT
+                                                                : CategoryType.DEFAULT;
+    }
+
+    /**
+     * @return the current tracing mode stored in the preferences. Either "record-until-full",
+     *     "record-as-much-as-possible", or "record-continuously".
+     */
+    public static String getSelectedTracingMode() {
+        return ContextUtils.getAppSharedPreferences().getString(
+                PREF_TRACING_MODE, TRACING_MODES.keySet().iterator().next());
+    }
+
+    /**
+     * Select and store a new tracing mode in the preferences.
+     *
+     * @param tracingMode the new tracing mode, should be either "record-until-full",
+     *     "record-as-much-as-possible", or "record-continuously".
+     */
+    public static void setSelectedTracingMode(String tracingMode) {
+        assert TRACING_MODES.containsKey(tracingMode);
+        ContextUtils.getAppSharedPreferences()
+                .edit()
+                .putString(PREF_TRACING_MODE, tracingMode)
+                .apply();
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,8 +163,30 @@ public class TracingPreferences extends PreferenceFragment implements TracingCon
         getActivity().setTitle(R.string.prefs_tracing);
         PreferenceUtils.addPreferencesFromResource(this, R.xml.tracing_preferences);
 
+        mPrefDefaultCategories = findPreference(UI_PREF_DEFAULT_CATEGORIES);
+        mPrefNondefaultCategories = findPreference(UI_PREF_NON_DEFAULT_CATEGORIES);
+        mPrefMode = (ListPreference) findPreference(UI_PREF_MODE);
         mPrefStartRecording = findPreference(UI_PREF_START_RECORDING);
         mPrefTracingStatus = findPreference(UI_PREF_TRACING_STATUS);
+
+        mPrefDefaultCategories.getExtras().putInt(
+                TracingCategoriesPreferences.EXTRA_CATEGORY_TYPE, CategoryType.DEFAULT);
+
+        mPrefNondefaultCategories.getExtras().putInt(
+                TracingCategoriesPreferences.EXTRA_CATEGORY_TYPE, CategoryType.NON_DEFAULT);
+
+        mPrefMode.setEntryValues(TRACING_MODES.keySet().toArray(new String[TRACING_MODES.size()]));
+        String[] descriptions = new String[TRACING_MODES.size()];
+        int i = 0;
+        for (int resourceId : TRACING_MODES.values()) {
+            descriptions[i++] = getActivity().getResources().getString(resourceId);
+        }
+        mPrefMode.setEntries(descriptions);
+        mPrefMode.setOnPreferenceChangeListener((preference, newValue) -> {
+            setSelectedTracingMode((String) newValue);
+            updatePreferences();
+            return true;
+        });
 
         mPrefStartRecording.setOnPreferenceClickListener(preference -> {
             TracingController.getInstance().startRecording();
@@ -67,7 +220,33 @@ public class TracingPreferences extends PreferenceFragment implements TracingCon
         boolean idle = state == TracingController.State.IDLE || !initialized;
         boolean notificationsEnabled = TracingNotificationManager.browserNotificationsEnabled();
 
+        mPrefDefaultCategories.setEnabled(initialized);
+        mPrefNondefaultCategories.setEnabled(initialized);
+        mPrefMode.setEnabled(initialized);
         mPrefStartRecording.setEnabled(idle && initialized && notificationsEnabled);
+
+        if (initialized) {
+            int defaultTotal = 0;
+            int nondefaultTotal = 0;
+            for (String category : TracingController.getInstance().getKnownCategories()) {
+                if (getCategoryType(category) == CategoryType.DEFAULT) {
+                    defaultTotal++;
+                } else {
+                    nondefaultTotal++;
+                }
+            }
+
+            int defaultEnabled = getEnabledCategories(CategoryType.DEFAULT).size();
+            int nondefaultEnabled = getEnabledCategories(CategoryType.NON_DEFAULT).size();
+
+            mPrefDefaultCategories.setSummary(getActivity().getResources().getString(
+                    R.string.prefs_tracing_categories_summary, defaultEnabled, defaultTotal));
+            mPrefNondefaultCategories.setSummary(getActivity().getResources().getString(
+                    R.string.prefs_tracing_categories_summary, nondefaultEnabled, nondefaultTotal));
+
+            mPrefMode.setValue(getSelectedTracingMode());
+            mPrefMode.setSummary(TRACING_MODES.get(getSelectedTracingMode()));
+        }
 
         if (!notificationsEnabled) {
             mPrefStartRecording.setTitle(R.string.prefs_tracing_start);
