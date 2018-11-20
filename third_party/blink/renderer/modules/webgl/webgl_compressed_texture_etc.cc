b6e1950c69dd929542e0d593853a05990546e2cc
Geoff Lang <geofflang@chromium.org>
2018-11-20 19:03:07 +0000

    Reland "Expose GL_CHROMIUM_compressed_texture_etc when ANGLE supports it."
    This reverts commit 0a8f58bfc095123d1c441ef201915501264d0102 [formerly c60b7aa805bde46a4ef5749c6332aa4c503032c1].
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > Revert "Expose GL_CHROMIUM_compressed_texture_etc when ANGLE supports it."
    > 
    > This reverts commit cb6694bbe751bbf8ebf6e73fbdf926c082434815 [formerly 2d1708019c205e5d9e4070bdc62232eda5ef3c11].
    > 
    > Reason for revert: might have broken angle_perftests https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Linux%20FYI%20Release%20%28NVIDIA%29/6461
    > 
    > Original change's description:
    > > Expose GL_CHROMIUM_compressed_texture_etc when ANGLE supports it.
    > > 
    > > ANGLE now exposes this extension under the same conditions as Chrome: on top
    > > of a native ES driver.
    > > 
    > > BUG=angleproject:1552
    > > 
    > > Change-Id: I6552e51bccd5ece43b7405b26d63127283a90c46
    > > Reviewed-on: https://chromium-review.googlesource.com/c/1342497
    > > Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    > > Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    > > Commit-Queue: Geoff Lang <geofflang@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#609713}
    > 
    > TBR=zmo@chromium.org,geofflang@chromium.org,kainino@chromium.org
    > 
    > Change-Id: I92f0b2e98577af73cd23510e95868d29ac04e775
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: angleproject:1552
    > Reviewed-on: https://chromium-review.googlesource.com/c/1344582
    > Reviewed-by: Saman Sami <samans@chromium.org>
    > Commit-Queue: Saman Sami <samans@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#609758}
    
    TBR=zmo@chromium.org,geofflang@chromium.org,kainino@chromium.org,samans@chromium.org
    
    Change-Id: I639d66790d226f20f23965a59d29f1653e5c903d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: angleproject:1552
    Reviewed-on: https://chromium-review.googlesource.com/c/1344553
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Geoff Lang <geofflang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609760}
    Former-commit-id: 1bd80f9260b2553e488eb1248284fb5a8565489f

--- third_party/blink/renderer/modules/webgl/webgl_compressed_texture_etc.cc
+++ third_party/blink/renderer/modules/webgl/webgl_compressed_texture_etc.cc
@@ -11,6 +11,8 @@ namespace blink {
 WebGLCompressedTextureETC::WebGLCompressedTextureETC(
     WebGLRenderingContextBase* context)
     : WebGLExtension(context) {
+  context->ExtensionsUtil()->EnsureExtensionEnabled(
+      "GL_CHROMIUM_compressed_texture_etc");
   context->AddCompressedTextureFormat(GL_COMPRESSED_R11_EAC);
   context->AddCompressedTextureFormat(GL_COMPRESSED_SIGNED_R11_EAC);
   context->AddCompressedTextureFormat(GL_COMPRESSED_RGB8_ETC2);
