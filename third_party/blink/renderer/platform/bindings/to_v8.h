a4456d0cf34caa0b2b30effb4390290354101286
Yuki Shiino <yukishiino@chromium.org>
2018-11-20 10:52:50 +0000

    v8binding: Do not hold a cross origin ScriptState in IDL callback function
    Make IDL callback function not hold a ScriptState of its
    creation context when it's cross origin from the incumbent
    realm.
    
    Not holding a cross origin ScriptState, there is much
    less risk to access a cross origin context.
    
    IDL callback interface will follow the same approach in
    a separate patch.
    
    Bug: 892755, 886588, 904218
    Change-Id: Ie55b436fcc5f66f4ee053ef08ad98ea68fb3a2d6
    Reviewed-on: https://chromium-review.googlesource.com/c/1314023
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609662}
    Former-commit-id: 756bea38c853ce40e3daba7f7fadf85b0920785f

--- third_party/blink/renderer/platform/bindings/to_v8.h
+++ third_party/blink/renderer/platform/bindings/to_v8.h
@@ -46,10 +46,11 @@ inline v8::Local<v8::Value> ToV8(CallbackFunctionBase* callback,
                                  v8::Isolate* isolate) {
   // |creation_context| is intentionally ignored. Callback functions are not
   // wrappers nor clonable. ToV8 on a callback function must be used only when
-  // it's the same origin-domain in the same world.
-  DCHECK(!callback || (callback->CallbackRelevantScriptState()->GetContext() ==
-                       creation_context->CreationContext()));
-  return callback ? callback->CallbackFunction().As<v8::Value>()
+  // it's in the same world.
+  DCHECK(!callback ||
+         (&callback->GetWorld() ==
+          &ScriptState::From(creation_context->CreationContext())->World()));
+  return callback ? callback->CallbackObject().As<v8::Value>()
                   : v8::Null(isolate).As<v8::Value>();
 }
 
