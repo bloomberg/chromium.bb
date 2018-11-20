2b331427c139284af7b1d654e8c2eb165904e5a8
Henrik Boström <hbos@chromium.org>
2018-11-20 13:53:24 +0000

    CallSetupStateTracker and unittests added.
    This helper class keeps track of an offerer state and an answerer state
    and makes sure that the transitions between different states are valid.
    By having a single state that reports whether the call setup was
    successful or if (and in which step) it failed we can be sure to get
    trustworthy comparisons between % success and % failures, which is
    important for the Unified Plan experiments.
    
    Follow-up CL(s) will wire up RTCPeerConnection to use this helper class
    and make sure the states are reported with UMA.
    
    Bug: 906029
    Change-Id: Iafb77533c66c7625269e3c6e33ef954d88e54cdb
    Reviewed-on: https://chromium-review.googlesource.com/c/1340299
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609685}
    Former-commit-id: 87866bee9d6f4e9c20c6c528059ea2dfc22cc38c

---
+++ third_party/blink/renderer/modules/peerconnection/call_setup_state_tracker.cc
@@ -0,0 +1,98 @@
+// Copyright (c) 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/peerconnection/call_setup_state_tracker.h"
+
+namespace blink {
+
+CallSetupStateTracker::CallSetupStateTracker()
+    : valid_offerer_transitions_(
+          {std::make_pair(OffererState::kNotStarted,
+                          OffererState::kCreateOfferPending),
+           // createOffer()
+           std::make_pair(OffererState::kCreateOfferPending,
+                          OffererState::kCreateOfferRejected),
+           std::make_pair(OffererState::kCreateOfferPending,
+                          OffererState::kCreateOfferResolved),
+           std::make_pair(OffererState::kCreateOfferRejected,
+                          OffererState::kCreateOfferResolved),
+           std::make_pair(OffererState::kCreateOfferResolved,
+                          OffererState::kSetLocalOfferPending),
+           // setLocalDescription(offer)
+           std::make_pair(OffererState::kSetLocalOfferPending,
+                          OffererState::kSetLocalOfferRejected),
+           std::make_pair(OffererState::kSetLocalOfferPending,
+                          OffererState::kSetLocalOfferResolved),
+           std::make_pair(OffererState::kSetLocalOfferRejected,
+                          OffererState::kSetLocalOfferResolved),
+           std::make_pair(OffererState::kSetLocalOfferResolved,
+                          OffererState::kSetRemoteAnswerPending),
+           // setRemoteDescription(answer)
+           std::make_pair(OffererState::kSetRemoteAnswerPending,
+                          OffererState::kSetRemoteAnswerRejected),
+           std::make_pair(OffererState::kSetRemoteAnswerPending,
+                          OffererState::kSetRemoteAnswerResolved),
+           std::make_pair(OffererState::kSetRemoteAnswerRejected,
+                          OffererState::kSetRemoteAnswerResolved)}),
+      valid_answerer_transitions_({
+          std::make_pair(AnswererState::kNotStarted,
+                         AnswererState::kSetRemoteOfferPending),
+          // setRemoteDescription(answer)
+          std::make_pair(AnswererState::kSetRemoteOfferPending,
+                         AnswererState::kSetRemoteOfferRejected),
+          std::make_pair(AnswererState::kSetRemoteOfferPending,
+                         AnswererState::kSetRemoteOfferResolved),
+          std::make_pair(AnswererState::kSetRemoteOfferRejected,
+                         AnswererState::kSetRemoteOfferResolved),
+          std::make_pair(AnswererState::kSetRemoteOfferResolved,
+                         AnswererState::kCreateAnswerPending),
+          // createAnswer()
+          std::make_pair(AnswererState::kCreateAnswerPending,
+                         AnswererState::kCreateAnswerRejected),
+          std::make_pair(AnswererState::kCreateAnswerPending,
+                         AnswererState::kCreateAnswerResolved),
+          std::make_pair(AnswererState::kCreateAnswerRejected,
+                         AnswererState::kCreateAnswerResolved),
+          std::make_pair(AnswererState::kCreateAnswerResolved,
+                         AnswererState::kSetLocalAnswerPending),
+          // setLocalDescription(answer)
+          std::make_pair(AnswererState::kSetLocalAnswerPending,
+                         AnswererState::kSetLocalAnswerRejected),
+          std::make_pair(AnswererState::kSetLocalAnswerPending,
+                         AnswererState::kSetLocalAnswerResolved),
+          std::make_pair(AnswererState::kSetLocalAnswerRejected,
+                         AnswererState::kSetLocalAnswerResolved),
+      }),
+      offerer_state_(OffererState::kNotStarted),
+      answerer_state_(AnswererState::kNotStarted) {}
+
+OffererState CallSetupStateTracker::offerer_state() const {
+  return offerer_state_;
+}
+
+AnswererState CallSetupStateTracker::answerer_state() const {
+  return answerer_state_;
+}
+
+bool CallSetupStateTracker::NoteOffererStateEvent(OffererState event) {
+  auto transition = std::make_pair(offerer_state_, event);
+  if (valid_offerer_transitions_.find(transition) ==
+      valid_offerer_transitions_.end()) {
+    return false;
+  }
+  offerer_state_ = event;
+  return true;
+}
+
+bool CallSetupStateTracker::NoteAnswererStateEvent(AnswererState event) {
+  auto transition = std::make_pair(answerer_state_, event);
+  if (valid_answerer_transitions_.find(transition) ==
+      valid_answerer_transitions_.end()) {
+    return false;
+  }
+  answerer_state_ = event;
+  return true;
+}
+
+}  // namespace blink
