86d980023832eaae63876cdc44a5842f125f17ac
Anand K. Mistry <amistry@chromium.org>
2018-11-20 15:13:19 +0000

    Create fuzzer for the compact encoding detection library.
    BUG=None
    
    Change-Id: If9ca23541732251aa800c0335c8b094235e631a7
    Reviewed-on: https://chromium-review.googlesource.com/c/1343458
    Commit-Queue: Abhishek Arya <inferno@chromium.org>
    Reviewed-by: Abhishek Arya <inferno@chromium.org>
    Reviewed-by: Jungshik Shin <jshin@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#609707}
    Former-commit-id: 0a6007aa91830985c9200170ce9e66b36b72b82e

---
+++ third_party/ced/compact_enc_det_fuzzer.cc
@@ -0,0 +1,32 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <string>
+
+#include "base/test/fuzzed_data_provider.h"
+#include "third_party/ced/src/compact_enc_det/compact_enc_det.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  base::FuzzedDataProvider data_provider(data, size);
+
+  CompactEncDet::TextCorpusType corpus =
+      static_cast<CompactEncDet::TextCorpusType>(
+          data_provider.ConsumeInt32InRange(0, CompactEncDet::NUM_CORPA));
+  Encoding encoding_hint = static_cast<Encoding>(
+      data_provider.ConsumeInt32InRange(0, NUM_ENCODINGS));
+  Language langauge_hint = static_cast<Language>(
+      data_provider.ConsumeInt32InRange(0, NUM_LANGUAGES));
+  bool ignore_7bit_mail_encodings = data_provider.ConsumeBool();
+
+  std::string text = data_provider.ConsumeRemainingBytes();
+  int bytes_consumed = 0;
+  bool is_reliable = false;
+  CompactEncDet::DetectEncoding(
+      text.c_str(), text.length(), nullptr /* url_hint */,
+      nullptr /* http_charset_hint */, nullptr /* meta_charset_hint */,
+      encoding_hint, langauge_hint, corpus, ignore_7bit_mail_encodings,
+      &bytes_consumed, &is_reliable);
+
+  return 0;
+}
