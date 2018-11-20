abbce26765c3bd8eb8028363625ddd1d176a9a4c
Tibor Goldschwendt <tiborg@chromium.org>
2018-11-20 22:54:26 +0000

    Revert "[feature modules] Allow disk writes for SplitCompat.install"
    This reverts commit d897e920419d9d0ad59ed05aba53e2b50087c012 [formerly 4397d75c3bd54382c67ef875b6c69fd3115922a2].
    
    Reason for revert: only needed for bundles but we don't build bundles
    for M72.
    
    Original change's description:
    > [feature modules] Allow disk writes for SplitCompat.install
    >
    > Bug: 888602
    > Change-Id: I9ac5818460da30057a42fff007dbcceb52b6f6a2
    > Reviewed-on: https://chromium-review.googlesource.com/1244599
    > Reviewed-by: agrieve <agrieve@chromium.org>
    > Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#594302}
    
    TBR=agrieve@chromium.org
    
    Bug: 899574
    Change-Id: I87363d49be2c0657c41bbf97004c19ed86980018
    Reviewed-on: https://chromium-review.googlesource.com/c/1344907
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3578@{#779}
    Cr-Branched-From: abbd18012032883c3113b3bfc5d59d25b4836c5f [formerly 4226ddf99103e493d7afb23a4c7902ee496108b6]-refs/heads/master@{#599034}
    Former-commit-id: 396bfd307c865a4332529f1279ddc9ee23cac4f3

--- components/module_installer/android/java/src/org/chromium/components/module_installer/ModuleInstaller.java
+++ components/module_installer/android/java/src/org/chromium/components/module_installer/ModuleInstaller.java
@@ -13,7 +13,6 @@ import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
-import org.chromium.base.StrictModeContext;
 import org.chromium.base.ThreadUtils;
 
 import java.util.Arrays;
@@ -42,10 +41,7 @@ public class ModuleInstaller {
 
     /** Needs to be called before trying to access a module. */
     public static void init() {
-        // SplitCompat.install may copy modules into Chrome's internal folder or clean them up.
-        try (StrictModeContext unused = StrictModeContext.allowDiskWrites()) {
-            SplitCompat.install(ContextUtils.getApplicationContext());
-        }
+        SplitCompat.install(ContextUtils.getApplicationContext());
     }
 
     /**
