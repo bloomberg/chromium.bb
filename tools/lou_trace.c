205af5c8521f3ecc8e345b86543e138085281a4c
Bert Frees <bertfrees@gmail.com>
2018-11-21 14:19:05 +0100

    Omit the short options

--- tools/lou_trace.c
+++ tools/lou_trace.c
@@ -47,11 +47,11 @@ applied translation rules for a given input.\n\n",
   -v, --version           display version information and exit\n\
   -f, --forward           forward translation using the given table\n\
   -b, --backward          backward translation using the given table\n\
-  -n, --noContractions    Use no contractions\n\
-  -d, --dotsIO            Display dot patterns\n\
-  -u, --ucBrl             Use Unicode Braille patterns\n\
-  -N, --noUndefinedDots   Disable output of undefined dot numbers during back-translation\n\
-  -p, --partialTrans      Use partial back-translation\n\
+      --noContractions    Use no contractions\n\
+      --dotsIO            Display dot patterns\n\
+      --ucBrl             Use Unicode Braille patterns\n\
+      --noUndefinedDots   Disable output of undefined dot numbers during back-translation\n\
+      --partialTrans      Use partial back-translation\n\
                       If neither -f nor -b are specified forward translation\n\
                       is assumed\n",
 			stdout);
@@ -280,17 +280,22 @@ main(int argc, char **argv) {
 	int forward_flag = 0;
 	int backward_flag = 0;
 	int mode = 0;
+	int noContractionsMode, dotsIOMode, ucBrlMode, noUndefinedDotsMode,
+			partialTransMode = 0;
 
 	const struct option longopts[] = {
 		{ "help", no_argument, NULL, 'h' }, { "version", no_argument, NULL, 'v' },
 		{ "forward", no_argument, NULL, 'f' }, { "backward", no_argument, NULL, 'b' },
-		{ "noContractions", no_argument, NULL, 'n' }, { "dotsIO", no_argument, NULL, 'd' },
-		{ "ucBrl", no_argument, NULL, 'u' }, { "noUndefinedDots", no_argument, NULL, 'N' },
-		{ "partialTrans", no_argument, NULL, 'p' }, { NULL, 0, NULL, 0 },
+		{ "noContractions", no_argument, &noContractionsMode, noContractions },
+		{ "dotsIO", no_argument, &dotsIOMode, dotsIO },
+		{ "ucBrl", no_argument, &ucBrlMode, ucBrl },
+		{ "noUndefinedDots", no_argument, &noUndefinedDotsMode, noUndefinedDots },
+		{ "partialTrans", no_argument, &partialTransMode, partialTrans },
+		{ NULL, 0, NULL, 0 },
 	};
 
 	set_program_name(argv[0]);
-	while ((optc = getopt_long(argc, argv, "hvfbnduNp", longopts, NULL)) != -1) {
+	while ((optc = getopt_long(argc, argv, "hvfb", longopts, NULL)) != -1) {
 		switch (optc) {
 		case 'v':
 			version_etc(
@@ -307,20 +312,7 @@ main(int argc, char **argv) {
 		case 'b':
 			backward_flag = 1;
 			break;
-		case 'n':
-			mode |= noContractions;
-			break;
-		case 'd':
-			mode |= dotsIO;
-			break;
-		case 'u':
-			mode |= ucBrl;
-			break;
-		case 'N':
-			mode |= noUndefinedDots;
-			break;
-		case 'p':
-			mode |= partialTrans;
+		case 0:
 			break;
 		default:
 			fprintf(stderr, "Try `%s --help' for more information.\n", program_name);
@@ -328,6 +320,8 @@ main(int argc, char **argv) {
 			break;
 		}
 	}
+	mode |= noContractionsMode | dotsIOMode | ucBrlMode | noUndefinedDotsMode |
+			partialTransMode;
 	if (forward_flag && backward_flag) {
 		fprintf(stderr, "%s: specify either -f or -b but not both\n", program_name);
 		fprintf(stderr, "Try `%s --help' for more information.\n", program_name);
