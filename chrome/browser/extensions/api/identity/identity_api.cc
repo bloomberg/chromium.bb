a73efb4fe90431ab266da05a0dbe0b4b2ccd73e1
Antonio Gomes <tonikitoo@igalia.com>
2018-11-21 13:35:07 +0000

    [s13n] Port identity_api.cc away from observing ProfileOAuth2TokenService
    ... to observing IdentityManager
    
    CL also gets rid of the AccountTrackerService usage, driven-by.
    
    BUG=906579
    
    Change-Id: I1ec47f5817a665995a5c1e4c2a6ed21a49756f3e
    Reviewed-on: https://chromium-review.googlesource.com/c/1340890
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#610032}
    Former-commit-id: d22ab991da0abcd793f0cbbeb79ae29d9cbcb831

--- chrome/browser/extensions/api/identity/identity_api.cc
+++ chrome/browser/extensions/api/identity/identity_api.cc
@@ -28,10 +28,9 @@
 #include "chrome/browser/signin/account_consistency_mode_manager.h"
 #include "chrome/browser/signin/account_tracker_service_factory.h"
 #include "chrome/browser/signin/chrome_signin_client_factory.h"
-#include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/common/extensions/api/identity.h"
 #include "chrome/common/url_constants.h"
-#include "components/signin/core/browser/profile_oauth2_token_service.h"
 #include "extensions/browser/extension_function_dispatcher.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_l10n_util.h"
@@ -104,7 +103,7 @@ const base::Time& IdentityTokenCacheValue::expiration_time() const {
 IdentityAPI::IdentityAPI(content::BrowserContext* context)
     : profile_(Profile::FromBrowserContext(context)) {
   AccountTrackerServiceFactory::GetForProfile(profile_)->AddObserver(this);
-  ProfileOAuth2TokenServiceFactory::GetForProfile(profile_)->AddObserver(this);
+  IdentityManagerFactory::GetForProfile(profile_)->AddObserver(this);
 }
 
 IdentityAPI::~IdentityAPI() {}
@@ -147,8 +146,7 @@ const IdentityAPI::CachedTokens& IdentityAPI::GetAllCachedTokens() {
 void IdentityAPI::Shutdown() {
   on_shutdown_callback_list_.Notify();
   AccountTrackerServiceFactory::GetForProfile(profile_)->RemoveObserver(this);
-  ProfileOAuth2TokenServiceFactory::GetForProfile(profile_)->RemoveObserver(
-      this);
+  IdentityManagerFactory::GetForProfile(profile_)->RemoveObserver(this);
 }
 
 static base::LazyInstance<BrowserContextKeyedAPIFactory<IdentityAPI>>::
@@ -172,11 +170,9 @@ bool IdentityAPI::AreExtensionsRestrictedToPrimaryAccount() {
 #endif
 }
 
-void IdentityAPI::OnRefreshTokenAvailable(const std::string& account_id) {
-  const AccountInfo& account_info =
-      AccountTrackerServiceFactory::GetForProfile(profile_)->GetAccountInfo(
-          account_id);
-
+void IdentityAPI::OnRefreshTokenUpdatedForAccount(
+    const AccountInfo& account_info,
+    bool is_valid) {
   // Refresh tokens are sometimes made available in contexts where
   // AccountTrackerService is not tracking the account in question (one example
   // is SupervisedUserService::InitSync()). Bail out in these cases.
@@ -214,7 +210,7 @@ void BrowserContextKeyedAPIFactory<IdentityAPI>::DeclareFactoryDependencies() {
   DependsOn(ExtensionsBrowserClient::Get()->GetExtensionSystemFactory());
 
   DependsOn(ChromeSigninClientFactory::GetInstance());
-  DependsOn(ProfileOAuth2TokenServiceFactory::GetInstance());
+  DependsOn(IdentityManagerFactory::GetInstance());
 }
 
 }  // namespace extensions
