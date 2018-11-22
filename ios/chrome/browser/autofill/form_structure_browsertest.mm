d76083f4cf87f1c517cc7c7a40f0d14972dd5953
Olivier Robin <olivierrobin@chromium.org>
2018-11-22 14:26:15 +0000

    Reland "[Reland] Block resources in FormStructureBrowserTest."
    This is a reland of 67271a748a3d96a860519dbcb1a8b4e6b1119be0 [formerly d15dd85fbe28ee8991802c55314bc97c0ee8a7cf]
    
    Original change's description:
    > [Reland] Block resources in FormStructureBrowserTest.
    >
    > The files used for FormStructureBrowserTest are snapshots of real web
    > sites. They can have link to online resources but should not need them
    > for the test to pass.
    > But if the network is really bad, the page can wait for the resource
    > loading to finish and timeout.
    > Preventing the resource loading will make the test faster and more
    > robust.
    >
    > Bug: 896173
    > Change-Id: I16b02e1d7defd4d950e6ee4cd21bd0133b9ac957
    > Reviewed-on: https://chromium-review.googlesource.com/c/1346462
    > Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    > Reviewed-by: Eugene But <eugenebut@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#610366}
    
    Tbr: eugenebut@chromium.org
    Bug: 896173
    Change-Id: I713942404ec5ab47fb653f4fc2c04214f66fa866
    Reviewed-on: https://chromium-review.googlesource.com/c/1348032
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610407}
    Former-commit-id: 7cbbce326c5e6dfb6c8c14f6fe2e396b8997f015

--- ios/chrome/browser/autofill/form_structure_browsertest.mm
+++ ios/chrome/browser/autofill/form_structure_browsertest.mm
@@ -165,7 +165,7 @@ void FormStructureBrowserTest::TearDown() {
 
 void FormStructureBrowserTest::GenerateResults(const std::string& input,
                                                std::string* output) {
-  ASSERT_TRUE(LoadHtml(input));
+  ASSERT_TRUE(LoadHtmlWithoutSubresources(input));
   base::TaskScheduler::GetInstance()->FlushForTesting();
   web::WebFrame* frame = web::GetMainWebFrame(web_state());
   AutofillManager* autofill_manager =
