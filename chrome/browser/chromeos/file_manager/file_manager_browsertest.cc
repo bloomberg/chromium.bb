edd8970abba1791b99216cf8fd8f806cc41325fd
Joel Hockey <joelhockey@chromium.org>
2018-11-22 07:09:45 +0000

    FilesApp quickview: test to ensure that info box opens/closes on enter key
    * Added quickview integration test pressEnterOnInfoBoxToOpenClose.
    * Updated test.util.sync.deepQueryAllElements to accept targetQuery
      as either string or !Array<string>.
    * Changed RemoteCall waitForElement* functions to call
      deepQueryAllElements to allow for usage with nested elements.
    * Updated javadoc on RemoteCall functions to show which already
      support query as string|!Array<string>.
    
    
    Bug: 893093
    Change-Id: I7c542e7a2bc914e16fa8142fb696f5b3fe370752
    Reviewed-on: https://chromium-review.googlesource.com/c/1345693
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610326}
    Former-commit-id: fce468e7e8fb0511a7a7a503d036dec79e86aa25

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -444,6 +444,7 @@ WRAPPED_INSTANTIATE_TEST_CASE_P(
                       TestCase("openQuickViewCrostini"),
                       TestCase("openQuickViewUsb"),
                       TestCase("openQuickViewMtp"),
+                      TestCase("pressEnterOnInfoBoxToOpenClose"),
                       TestCase("closeQuickView")));
 
 WRAPPED_INSTANTIATE_TEST_CASE_P(
