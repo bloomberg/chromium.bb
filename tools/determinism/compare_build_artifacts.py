a1f115b4a77c7c41e20743db16dcfa5d75386dbc
Nico Weber <thakis@chromium.org>
2018-11-22 18:52:02 +0000

    Update deterministic build whitelist now that "Windows deterministic" uses separate build dirs.
    Also remove no-longer-needed debug logging.
    
    TBR=hans
    
    Bug: 899438
    Change-Id: I1759e7e2f6ee8ece3fcdddeeb286754c1cf6b60a
    Reviewed-on: https://chromium-review.googlesource.com/c/1348310
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610467}
    Former-commit-id: daa3cb99a97f9e52cce7d550e77f48ed519088c2

--- tools/determinism/compare_build_artifacts.py
+++ tools/determinism/compare_build_artifacts.py
@@ -190,12 +190,7 @@ def compare_files(first_filepath, second_filepath):
     with open(second_filepath, 'rb') as f:
       lhs_cwd = lhs['relative_cwd'].replace('\\', '\\\\')
       rhs_cwd = rhs['relative_cwd'].replace('\\', '\\\\')
-      json_contents = f.read()
-      print >>sys.stderr, 'raw contents', second_filepath, ':', json_contents
-      json_contents = json_contents.replace(rhs_cwd, lhs_cwd)
-      print >>sys.stderr, 'transformed', rhs_cwd, lhs_cwd, json_contents
-      rhs = json.loads(json_contents)
-      print >>sys.stderr, 'objects', lhs, rhs
+      rhs = json.loads(f.read().replace(rhs_cwd, lhs_cwd))
     diff = diff_dict(lhs, rhs)
     if diff:
       return '\n' + '\n'.join('  ' + line for line in diff.splitlines())
