493b21197e054a2aab8d2be6c50476f30cfcf12d
Shao-Chuan Lee <shaochuan@chromium.org>
2018-11-22 07:27:51 +0000

    Add ARC++ engagement time metrics and unittests
    Bug: 904282
    Change-Id: I5dfaed91060891069f317cf9eb3140f58c4d5c2f
    Reviewed-on: https://chromium-review.googlesource.com/c/1331095
    Commit-Queue: Shao-Chuan Lee <shaochuan@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Maajid <maajid@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610332}
    Former-commit-id: 678d55df5733c2a2034e0183e6a2446f31d4e4a8

--- components/arc/metrics/arc_metrics_service.cc
+++ components/arc/metrics/arc_metrics_service.cc
@@ -12,13 +12,22 @@
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_util.h"
+#include "base/system/sys_info.h"
+#include "base/time/default_clock.h"
+#include "base/time/default_tick_clock.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
+#include "chromeos/dbus/power_manager/idle.pb.h"
 #include "chromeos/dbus/session_manager_client.h"
 #include "components/arc/arc_bridge_service.h"
 #include "components/arc/arc_browser_context_keyed_service_factory_base.h"
+#include "components/arc/arc_prefs.h"
 #include "components/arc/arc_util.h"
 #include "components/arc/metrics/arc_metrics_constants.h"
 #include "components/exo/wm_helper.h"
+#include "components/prefs/pref_service.h"
+#include "components/session_manager/core/session_manager.h"
+#include "components/user_prefs/user_prefs.h"
+#include "content/public/browser/browser_context.h"
 
 namespace arc {
 
@@ -34,6 +43,11 @@ constexpr char kArcProcessNamePrefix[] = "org.chromium.arc.";
 constexpr char kGmsProcessNamePrefix[] = "com.google.android.gms";
 constexpr char kBootProgressEnableScreen[] = "boot_progress_enable_screen";
 
+constexpr base::TimeDelta kUpdateEngagementTimePeriod =
+    base::TimeDelta::FromMinutes(1);
+constexpr base::TimeDelta kSaveEngagementTimeToPrefsPeriod =
+    base::TimeDelta::FromMinutes(30);
+
 std::string BootTypeToString(mojom::BootType boot_type) {
   switch (boot_type) {
     case mojom::BootType::UNKNOWN:
@@ -47,6 +61,10 @@ std::string BootTypeToString(mojom::BootType boot_type) {
   }
   NOTREACHED();
   return "";
+}
+
+inline int GetDayId(const base::Clock* clock) {
+  return clock->Now().LocalMidnight().since_origin().InDays();
 }
 
 class ArcWindowDelegateImpl : public ArcMetricsService::ArcWindowDelegate {
@@ -112,23 +130,52 @@ ArcMetricsService* ArcMetricsService::GetForBrowserContextForTesting(
   return ArcMetricsServiceFactory::GetForBrowserContextForTesting(context);
 }
 
+// static
+BrowserContextKeyedServiceFactory* ArcMetricsService::GetFactory() {
+  return ArcMetricsServiceFactory::GetInstance();
+}
+
 ArcMetricsService::ArcMetricsService(content::BrowserContext* context,
                                      ArcBridgeService* bridge_service)
     : arc_bridge_service_(bridge_service),
       arc_window_delegate_(std::make_unique<ArcWindowDelegateImpl>(this)),
       process_observer_(this),
       native_bridge_type_(NativeBridgeType::UNKNOWN),
+      pref_service_(user_prefs::UserPrefs::Get(context)),
+      clock_(base::DefaultClock::GetInstance()),
+      tick_clock_(base::DefaultTickClock::GetInstance()),
+      last_update_ticks_(tick_clock_->NowTicks()),
       weak_ptr_factory_(this) {
   arc_bridge_service_->metrics()->SetHost(this);
   arc_bridge_service_->process()->AddObserver(&process_observer_);
   arc_window_delegate_->RegisterActivationChangeObserver();
+  session_manager::SessionManager::Get()->AddObserver(this);
+  chromeos::DBusThreadManager::Get()->GetPowerManagerClient()->AddObserver(
+      this);
+
+  DCHECK(pref_service_);
+  RestoreEngagementTimeFromPrefs();
+  update_engagement_time_timer_.Start(FROM_HERE, kUpdateEngagementTimePeriod,
+                                      this,
+                                      &ArcMetricsService::UpdateEngagementTime);
+  save_engagement_time_to_prefs_timer_.Start(
+      FROM_HERE, kSaveEngagementTimeToPrefsPeriod, this,
+      &ArcMetricsService::SaveEngagementTimeToPrefs);
 }
 
 ArcMetricsService::~ArcMetricsService() {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  save_engagement_time_to_prefs_timer_.Stop();
+  update_engagement_time_timer_.Stop();
+  UpdateEngagementTime();
+  SaveEngagementTimeToPrefs();
+
+  chromeos::DBusThreadManager::Get()->GetPowerManagerClient()->RemoveObserver(
+      this);
+  session_manager::SessionManager::Get()->RemoveObserver(this);
+  arc_window_delegate_->UnregisterActivationChangeObserver();
   arc_bridge_service_->process()->RemoveObserver(&process_observer_);
   arc_bridge_service_->metrics()->SetHost(nullptr);
-  arc_window_delegate_->UnregisterActivationChangeObserver();
 }
 
 void ArcMetricsService::SetArcWindowDelegateForTesting(
@@ -136,15 +183,23 @@ void ArcMetricsService::SetArcWindowDelegateForTesting(
   arc_window_delegate_ = std::move(delegate);
 }
 
+void ArcMetricsService::SetClockForTesting(base::Clock* clock) {
+  clock_ = clock;
+}
+
+void ArcMetricsService::SetTickClockForTesting(base::TickClock* tick_clock) {
+  tick_clock_ = tick_clock;
+}
+
 void ArcMetricsService::OnProcessConnectionReady() {
   VLOG(2) << "Start updating process list.";
-  timer_.Start(FROM_HERE, kRequestProcessListPeriod, this,
-               &ArcMetricsService::RequestProcessList);
+  request_process_list_timer_.Start(FROM_HERE, kRequestProcessListPeriod, this,
+                                    &ArcMetricsService::RequestProcessList);
 }
 
 void ArcMetricsService::OnProcessConnectionClosed() {
   VLOG(2) << "Stop updating process list.";
-  timer_.Stop();
+  request_process_list_timer_.Stop();
 }
 
 void ArcMetricsService::RequestProcessList() {
@@ -261,11 +316,143 @@ void ArcMetricsService::OnWindowActivated(
     wm::ActivationChangeObserver::ActivationReason reason,
     aura::Window* gained_active,
     aura::Window* lost_active) {
-  if (!arc_window_delegate_->IsArcAppWindow(gained_active))
+  UpdateEngagementTime();
+  was_arc_window_active_ = arc_window_delegate_->IsArcAppWindow(gained_active);
+  if (!was_arc_window_active_)
     return;
   UMA_HISTOGRAM_ENUMERATION(
       "Arc.UserInteraction",
       UserInteractionType::APP_CONTENT_WINDOW_INTERACTION);
+}
+
+void ArcMetricsService::OnSessionStateChanged() {
+  UpdateEngagementTime();
+  was_session_active_ =
+      session_manager::SessionManager::Get()->session_state() ==
+      session_manager::SessionState::ACTIVE;
+}
+
+void ArcMetricsService::ScreenIdleStateChanged(
+    const power_manager::ScreenIdleState& proto) {
+  UpdateEngagementTime();
+  was_screen_dimmed_ = proto.dimmed();
+}
+
+void ArcMetricsService::OnTaskCreated(int32_t task_id,
+                                      const std::string& package_name,
+                                      const std::string& activity,
+                                      const std::string& intent) {
+  UpdateEngagementTime();
+  task_ids_.push_back(task_id);
+}
+
+void ArcMetricsService::OnTaskDestroyed(int32_t task_id) {
+  UpdateEngagementTime();
+  auto it = std::find(task_ids_.begin(), task_ids_.end(), task_id);
+  DCHECK(it != task_ids_.end());
+  task_ids_.erase(it);
+}
+
+void ArcMetricsService::RestoreEngagementTimeFromPrefs() {
+  // Restore accumulated results only if they were recorded on the same OS
+  // version.
+  if (pref_service_->GetString(prefs::kEngagementTimeOsVersion) ==
+      base::SysInfo::OperatingSystemVersion()) {
+    day_id_ = pref_service_->GetInteger(prefs::kEngagementTimeDayId);
+    engagement_time_total_ =
+        pref_service_->GetTimeDelta(prefs::kEngagementTimeTotal);
+    engagement_time_foreground_ =
+        pref_service_->GetTimeDelta(prefs::kEngagementTimeForeground);
+    engagement_time_background_ =
+        pref_service_->GetTimeDelta(prefs::kEngagementTimeBackground);
+  } else {
+    ResetEngagementTimePrefs();
+  }
+
+  RecordEngagementTimeToUmaIfNeeded();
+}
+
+void ArcMetricsService::SaveEngagementTimeToPrefs() {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  DCHECK(pref_service_);
+
+  pref_service_->SetString(prefs::kEngagementTimeOsVersion,
+                           base::SysInfo::OperatingSystemVersion());
+  pref_service_->SetInteger(prefs::kEngagementTimeDayId, day_id_);
+  pref_service_->SetTimeDelta(prefs::kEngagementTimeTotal,
+                              engagement_time_total_);
+  pref_service_->SetTimeDelta(prefs::kEngagementTimeForeground,
+                              engagement_time_foreground_);
+  pref_service_->SetTimeDelta(prefs::kEngagementTimeBackground,
+                              engagement_time_background_);
+}
+
+void ArcMetricsService::UpdateEngagementTime() {
+  VLOG(2) << "last state: dimmed=" << was_screen_dimmed_
+          << " active=" << was_session_active_
+          << " focus=" << was_arc_window_active_
+          << " #tasks=" << task_ids_.size();
+
+  base::TimeTicks now = tick_clock_->NowTicks();
+  base::TimeDelta elapsed = now - last_update_ticks_;
+
+  if (ShouldAccumulateEngagementTotalTime()) {
+    VLOG(2) << "accumulate to total time " << elapsed;
+    engagement_time_total_ += elapsed;
+    if (ShouldAccumulateEngagementForegroundTime()) {
+      VLOG(2) << "accumulate to foreground time " << elapsed;
+      engagement_time_foreground_ += elapsed;
+    } else if (ShouldAccumulateEngagementBackgroundTime()) {
+      VLOG(2) << "accumulate to background time " << elapsed;
+      engagement_time_background_ += elapsed;
+    }
+  }
+
+  last_update_ticks_ = now;
+  RecordEngagementTimeToUmaIfNeeded();
+}
+
+void ArcMetricsService::RecordEngagementTimeToUmaIfNeeded() {
+  if (!ShouldRecordEngagementTimeToUma())
+    return;
+  VLOG(2) << "day changed, recording engagement time to UMA";
+  UMA_HISTOGRAM_CUSTOM_TIMES(
+      "Arc.EngagementTime.Total", engagement_time_total_,
+      base::TimeDelta::FromSeconds(1),
+      base::TimeDelta::FromDays(1) + kUpdateEngagementTimePeriod, 50);
+  UMA_HISTOGRAM_CUSTOM_TIMES(
+      "Arc.EngagementTime.Foreground", engagement_time_foreground_,
+      base::TimeDelta::FromSeconds(1),
+      base::TimeDelta::FromDays(1) + kUpdateEngagementTimePeriod, 50);
+  UMA_HISTOGRAM_CUSTOM_TIMES(
+      "Arc.EngagementTime.Background", engagement_time_background_,
+      base::TimeDelta::FromSeconds(1),
+      base::TimeDelta::FromDays(1) + kUpdateEngagementTimePeriod, 50);
+  ResetEngagementTimePrefs();
+}
+
+void ArcMetricsService::ResetEngagementTimePrefs() {
+  day_id_ = GetDayId(clock_);
+  engagement_time_total_ = base::TimeDelta();
+  engagement_time_foreground_ = base::TimeDelta();
+  engagement_time_background_ = base::TimeDelta();
+  SaveEngagementTimeToPrefs();
+}
+
+bool ArcMetricsService::ShouldAccumulateEngagementTotalTime() const {
+  return was_session_active_ && !was_screen_dimmed_;
+}
+
+bool ArcMetricsService::ShouldAccumulateEngagementForegroundTime() const {
+  return was_arc_window_active_;
+}
+
+bool ArcMetricsService::ShouldAccumulateEngagementBackgroundTime() const {
+  return task_ids_.size() > 0;
+}
+
+bool ArcMetricsService::ShouldRecordEngagementTimeToUma() const {
+  return day_id_ != GetDayId(clock_);
 }
 
 ArcMetricsService::ProcessObserver::ProcessObserver(
