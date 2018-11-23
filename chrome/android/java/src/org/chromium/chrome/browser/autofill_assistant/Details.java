25818b0564a95321da49d8adcd552c49197a62bb
Lukasz Suder <lsuder@google.com>
2018-11-23 14:16:19 +0000

    [Autofill Assistant] Checks the mId of details.
    Comparing just title is fragile (e.g. different languages),
    therefore we allow specifing the mid inside parameters.
    
    Bug: 806868
    Change-Id: Ie5790beb6cb06bc4c124ae7a969c2263a77fc2cf
    Reviewed-on: https://chromium-review.googlesource.com/c/1349335
    Commit-Queue: Lukasz Suder <lsuder@chromium.org>
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610598}
    Former-commit-id: 110e2dc7a6e1c4dbf44bf556a1b7287e4ccda404

--- chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/Details.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/Details.java
@@ -22,29 +22,31 @@ import java.util.Set;
  * Java side equivalent of autofill_assistant::DetailsProto.
  */
 class Details {
-    enum DetailsField { TITLE, URL, DATE, DESCRIPTION, IS_FINAL }
+    enum DetailsField { TITLE, URL, DATE, DESCRIPTION, MID, IS_FINAL }
 
     private final String mTitle;
     private final String mUrl;
     @Nullable
     private final Date mDate;
     private final String mDescription;
+    private final String mMid;
     private final boolean mIsFinal;
     /** Contains the fields that have changed when merging with other Details object. */
     private final Set<DetailsField> mFieldsChanged;
-    // NOTE: When adding a new field, update the isEmpty method.
+    // NOTE: When adding a new field, update the isEmpty and toJSONObject methods.
 
     static final Details EMPTY_DETAILS =
-            new Details("", "", null, "", false, Collections.emptySet());
+            new Details("", "", null, "", "", false, Collections.emptySet());
 
     private static final String RFC_3339_FORMAT_WITHOUT_TIMEZONE = "yyyy'-'MM'-'dd'T'HH':'mm':'ss";
 
-    Details(String title, String url, @Nullable Date date, String description, boolean isFinal,
-            Set<DetailsField> fieldsChanged) {
+    Details(String title, String url, @Nullable Date date, String description, String mId,
+            boolean isFinal, Set<DetailsField> fieldsChanged) {
         this.mTitle = title;
         this.mUrl = url;
         this.mDate = date;
         this.mDescription = description;
+        this.mMid = mId;
         this.mIsFinal = isFinal;
         this.mFieldsChanged = fieldsChanged;
     }
@@ -66,11 +68,16 @@ class Details {
         return mDescription;
     }
 
+    private String getMid() {
+        return mMid;
+    }
+
     JSONObject toJSONObject() {
         // Details are part of the feedback form, hence they need a JSON representation.
         Map<String, String> movieDetails = new HashMap<>();
         movieDetails.put("title", mTitle);
         movieDetails.put("url", mUrl);
+        movieDetails.put("mId", mMid);
         if (mDate != null) movieDetails.put("date", mDate.toString());
         movieDetails.put("description", mDescription);
         return new JSONObject(movieDetails);
@@ -89,7 +96,8 @@ class Details {
     }
 
     boolean isEmpty() {
-        return mTitle.isEmpty() && mUrl.isEmpty() && mDescription.isEmpty() && mDate == null;
+        return mTitle.isEmpty() && mUrl.isEmpty() && mDescription.isEmpty() && mDate == null
+                && mMid.isEmpty();
     }
 
     // TODO(crbug.com/806868): Create a fallback when there are no parameters for details.
@@ -97,14 +105,20 @@ class Details {
         String title = "";
         String description = "";
         Date date = null;
+        String mId = "";
         for (String key : parameters.keySet()) {
-            if (key.contains("E_NAME")) {
+            if (key.endsWith("E_NAME")) {
                 title = parameters.get(key);
                 continue;
             }
 
-            if (key.contains("R_NAME")) {
+            if (key.endsWith("R_NAME")) {
                 description = parameters.get(key);
+                continue;
+            }
+
+            if (key.endsWith("MID")) {
+                mId = parameters.get(key);
                 continue;
             }
 
@@ -120,7 +134,7 @@ class Details {
             }
         }
 
-        return new Details(title, /* url= */ "", date, description, /* isFinal= */ false,
+        return new Details(title, /* url= */ "", date, description, mId, /* isFinal= */ false,
                 /* fieldsChanged= */ Collections.emptySet());
     }
 
@@ -136,27 +150,40 @@ class Details {
     static Details merge(Details oldDetails, Details newDetails) {
         if (oldDetails.isEmpty()) {
             return new Details(newDetails.getTitle(), newDetails.getUrl(), newDetails.getDate(),
-                    newDetails.getDescription(), newDetails.isFinal(), Collections.emptySet());
+                    newDetails.getDescription(), newDetails.getMid(), newDetails.isFinal(),
+                    Collections.emptySet());
         }
 
         Set<DetailsField> changedFields = new HashSet<>();
 
-        // TODO(crbug.com/806868): Change title if mid doesn't match.
-        String title =
-                oldDetails.getTitle().isEmpty() ? newDetails.getTitle() : oldDetails.getTitle();
+        String title = oldDetails.getTitle();
+        String mId = oldDetails.getMid();
+        // Title and mId are tightly connected. If mId is different then title should also be
+        // different, but it's not true the other way (titles might slightly differ).
+        if (!oldDetails.getMid().isEmpty() && !newDetails.getMid().isEmpty()
+                && !oldDetails.getMid().equals(newDetails.getMid())
+                && !oldDetails.getTitle().equals(newDetails.getTitle())) {
+            changedFields.add(DetailsField.TITLE);
+            title = newDetails.getTitle();
+            mId = newDetails.getMid();
+        }
         String url = oldDetails.getUrl().isEmpty() ? newDetails.getUrl() : oldDetails.getUrl();
 
         Date date = oldDetails.getDate();
-        if (newDetails.getDate() != null && oldDetails.getDate() != null
-                && !newDetails.getDate().equals(oldDetails.getDate())) {
-            changedFields.add(DetailsField.DATE);
+        if (oldDetails.getDate() == null) {
+            // There was no date. Use the new one unconditionally.
             date = newDetails.getDate();
+        } else if (newDetails.getDate() != null
+                && !oldDetails.getDate().equals(newDetails.getDate())) {
+            // Only if new date is different than old (which wasn't null) mark it as changedField.
+            date = newDetails.getDate();
+            changedFields.add(DetailsField.DATE);
         }
 
         String description = oldDetails.getDescription().isEmpty() ? newDetails.getDescription()
                                                                    : oldDetails.getDescription();
 
         boolean isFinal = newDetails.isFinal();
-        return new Details(title, url, date, description, isFinal, changedFields);
+        return new Details(title, url, date, description, mId, isFinal, changedFields);
     }
 }
