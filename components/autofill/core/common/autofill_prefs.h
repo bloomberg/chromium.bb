582e7f61b35211397eb6b755d560537a2c21f971
sebsg <sebsg@chromium.org>
2018-11-23 16:03:58 +0000

    [AF] Add a wallet sync transport opt-in pref.
    This will be used to record whether a user has opted in to seeing their
    Google Account cards in the Autofill dropdown.
    
    The dictionary should not contain info about accounts that are not opted
    in.
    
    Bug: 907929
    Change-Id: Icc663b228792c58b00c02b204249fba9f557481b
    Reviewed-on: https://chromium-review.googlesource.com/c/1348792
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610621}
    Former-commit-id: affe4c454358628862b3dfd85e97e87eefa4e8f7

--- components/autofill/core/common/autofill_prefs.h
+++ components/autofill/core/common/autofill_prefs.h
@@ -35,11 +35,18 @@ extern const char kAutofillOrphanRowsRemoved[];
 // Do not get/set the value of this pref directly. Use provided getter/setter.
 extern const char kAutofillProfileEnabled[];
 extern const char kAutofillProfileValidity[];
+extern const char kAutofillSyncTransportOptIn[];
 extern const char kAutofillUploadEncodingSeed[];
 extern const char kAutofillUploadEvents[];
 extern const char kAutofillUploadEventsLastResetTimestamp[];
 extern const char kAutofillWalletImportEnabled[];
 extern const char kAutofillWalletImportStorageCheckboxState[];
+
+namespace sync_transport_opt_in {
+enum Flags {
+  kWallet = 1 << 0,
+};
+}  // namespace sync_transport_opt_in
 
 // Possible values for previous user decision when we displayed a save credit
 // card prompt.
@@ -86,6 +93,13 @@ bool IsPaymentsIntegrationEnabled(const PrefService* prefs);
 void SetPaymentsIntegrationEnabled(PrefService* prefs, bool enabled);
 
 std::string GetAllProfilesValidityMapsEncodedString(const PrefService* prefs);
+
+void SetUserOptedInWalletSyncTransport(PrefService* prefs,
+                                       const std::string& account_id,
+                                       bool opted_in);
+
+bool IsUserOptedInWalletSyncTransport(const PrefService* prefs,
+                                      const std::string& account_id);
 
 }  // namespace prefs
 }  // namespace autofill
