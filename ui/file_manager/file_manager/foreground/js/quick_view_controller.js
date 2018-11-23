aca10e26a776ebb4ec69ea1a925e4e5cf3222f8d
Sam McNally <sammc@chromium.org>
2018-11-23 03:05:05 +0000

    Only open quickview if exactly 1 file is selected.
    Always invoke preventDefault() to avoid the default page-down from
    triggering.
    
    Bug: 750403
    Change-Id: I7b8779a8bd51757192c9c1534f25d9d574ca5cc5
    Reviewed-on: https://chromium-review.googlesource.com/c/1348878
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610523}
    Former-commit-id: 9de4dc8e06f9794dff3d838f36b98ca32eb6ec32

--- ui/file_manager/file_manager/foreground/js/quick_view_controller.js
+++ ui/file_manager/file_manager/foreground/js/quick_view_controller.js
@@ -201,10 +201,11 @@ QuickViewController.prototype.onOpenInNewButtonTap_ = function(event) {
  * @private
  */
 QuickViewController.prototype.onKeyDownToOpen_ = function(event) {
-  if (this.entries_.length == 0)
-    return;
   if (event.key === ' ') {
     event.preventDefault();
+    if (this.entries_.length != 1) {
+      return;
+    }
     event.stopImmediatePropagation();
     this.display_(QuickViewUma.WayToOpen.SPACE_KEY);
   }
