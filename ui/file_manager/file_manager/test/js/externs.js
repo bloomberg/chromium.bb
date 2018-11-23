91ba46fc9891f3d3c97287205c913f54d3d1b89f
Joel Hockey <joelhockey@chromium.org>
2018-11-23 04:32:05 +0000

    FilesApp progress center: allow scrolling of items
    Bug: 627371
    Change-Id: I55e322bd36ff7eeb5699f9e2bd9842c01938d1da
    Reviewed-on: https://chromium-review.googlesource.com/c/1349131
    Reviewed-by: Stuart Langley <slangley@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610532}
    Former-commit-id: 5c5f86f2c1fda2d9ec2b947aa7b44cea985f0d03

--- ui/file_manager/file_manager/test/js/externs.js
+++ ui/file_manager/file_manager/test/js/externs.js
@@ -11,16 +11,34 @@ ChromeEvent.prototype.dispatchEvent = (var_args) => {};
 
 /** @constructor */
 function FileManager() {
-  /** @type {Crostini} */
+  /** @type {!Crostini} */
   this.crostini;
+
+  /**
+   * Declare as FileBrowserBackground rather than FileBrowserBackgroundFull to
+   * simplify deps.  Individual fields such as progressCenter below can be
+   * defined as needed.
+   * @type {!FileBrowserBackground}}
+   */
+  this.fileBrowserBackground_;
 }
 FileManager.prototype.setupCrostini_ = () => {};
 
 /** @type {string} */
 let FILE_MANAGER_ROOT;
 
-/** @type {FileManager} */
+/** @type {!FileManager} */
 let fileManager;
 
-/** @type {FileBrowserBackground} */
+/**
+ * Declare ProgressCenterPanel here rather than deal with its deps.  It is
+ * referenced in //ui/file_manager/externs/background/progress_center.js
+ * @constructor
+ */
+function ProgressCenterPanel() {}
+
+/** @type {!ProgressCenter} */
+fileManager.fileBrowserBackground_.progressCenter;
+
+/** @type {!FileBrowserBackground} */
 window.background;
