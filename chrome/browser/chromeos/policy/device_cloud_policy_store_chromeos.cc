06a700060b4015514ffd0c455d245455516f5cd2
Nikita Podguzov <nikitapodguzov@chromium.org>
2018-11-23 14:11:52 +0000

    Fetch External Policy for Device Settings.
    Add support for External Data referenced by Device Policy.
    Use the new support to implement the DeviceNativePrinters policy.
    Add browser test verifying external data cache is cleaned up after policy was changed or removed.
    
    Bug: 807068
    Change-Id: Ib98778a438e929a78214d1d9ab066a41a6bdcf09
    Reviewed-on: https://chromium-review.googlesource.com/c/1335592
    Commit-Queue: Nikita Podguzov <nikitapodguzov@google.com>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610596}
    Former-commit-id: 0a5685cf6d5f18857a3367c1731486b5e98997b3

--- chrome/browser/chromeos/policy/device_cloud_policy_store_chromeos.cc
+++ chrome/browser/chromeos/policy/device_cloud_policy_store_chromeos.cc
@@ -15,6 +15,7 @@
 #include "chrome/browser/chromeos/policy/value_validation/onc_device_policy_value_validator.h"
 #include "chromeos/settings/install_attributes.h"
 #include "components/ownership/owner_key_util.h"
+#include "components/policy/core/common/cloud/cloud_external_data_manager.h"
 #include "components/policy/core/common/cloud/cloud_policy_constants.h"
 #include "components/policy/proto/chrome_device_policy.pb.h"
 #include "components/policy/proto/device_management_backend.pb.h"
@@ -168,7 +169,7 @@ void DeviceCloudPolicyStoreChromeOS::UpdateFromService() {
     PolicyMap new_policy_map;
     if (is_managed()) {
       DecodeDevicePolicy(*device_settings_service_->device_settings(),
-                         &new_policy_map);
+                         external_data_manager(), &new_policy_map);
     }
     policy_map_.Swap(&new_policy_map);
 
