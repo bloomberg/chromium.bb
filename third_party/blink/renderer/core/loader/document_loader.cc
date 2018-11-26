35ccb6b0ea26ea5a8af6541f73f11a700534b740
Mounir Lamouri <mlamouri@chromium.org>
2018-11-26 20:19:56 +0000

    Revert "Inherit the navigation initiator when navigating instead of the parent/opener"
    This reverts commit 6507aaa7818edc6b3dc8286c2938353a9e94ae7a [formerly 108147dfd1ea159fd3632ef92ccc4ab8952980c7].
    
    Reason for revert:
    FindIt suggests a 74% chance that this is the cause of the following issues:
    https://ci.chromium.org/buildbot/chromium.webkit/WebKit%20Linux%20Trusty%20Leak/26833
    
    Original change's description:
    > Inherit the navigation initiator when navigating instead of the parent/opener
    > 
    > Spec PR: https://github.com/w3c/webappsec-csp/pull/358
    > 
    > Bug: 905301, 894228, 836148
    > Change-Id: I43ada2266d42d1cd56dbe3c6dd89d115e878a83a
    > Reviewed-on: https://chromium-review.googlesource.com/c/1314633
    > Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    > Reviewed-by: Mike West <mkwst@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#610850}
    
    TBR=mkwst@chromium.org,andypaicu@chromium.org
    
    Change-Id: If3ccf72cf8a4285926429f2855f32c1b0c606c5b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 905301, 894228, 836148
    Reviewed-on: https://chromium-review.googlesource.com/c/1351285
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610930}
    Former-commit-id: d72a833dad8deabbf0e49ef16073542142db88a6

--- third_party/blink/renderer/core/loader/document_loader.cc
+++ third_party/blink/renderer/core/loader/document_loader.cc
@@ -978,12 +978,13 @@ void DocumentLoader::StartLoading() {
                                         : fetch_params.GetResourceRequest();
 }
 
-void DocumentLoader::DidInstallNewDocument(Document* document) {
+void DocumentLoader::DidInstallNewDocument(
+    Document* document,
+    const ContentSecurityPolicy* previous_csp) {
   document->SetReadyState(Document::kLoading);
   if (content_security_policy_) {
-    document->InitContentSecurityPolicy(
-        content_security_policy_.Release(),
-        GetFrameLoader().GetLastOriginDocument());
+    document->InitContentSecurityPolicy(content_security_policy_.Release(),
+                                        nullptr, previous_csp);
   }
 
   if (history_item_ && IsBackForwardLoadType(load_type_))
@@ -1168,8 +1169,10 @@ void DocumentLoader::InstallNewDocument(
   }
 
   const SecurityOrigin* previous_security_origin = nullptr;
+  const ContentSecurityPolicy* previous_csp = nullptr;
   if (frame_->GetDocument()) {
     previous_security_origin = frame_->GetDocument()->GetSecurityOrigin();
+    previous_csp = frame_->GetDocument()->GetContentSecurityPolicy();
   }
 
   // In some rare cases, we'll re-use a LocalDOMWindow for a new Document. For
@@ -1192,7 +1195,8 @@ void DocumentLoader::InstallNewDocument(
           .WithDocumentLoader(this)
           .WithURL(url)
           .WithOwnerDocument(owner_document)
-          .WithNewRegistrationContext(),
+          .WithNewRegistrationContext()
+          .WithPreviousDocumentCSP(previous_csp),
       false);
 
   // Clear the user activation state.
@@ -1222,7 +1226,7 @@ void DocumentLoader::InstallNewDocument(
 
   if (!overriding_url.IsEmpty())
     document->SetBaseURLOverride(overriding_url);
-  DidInstallNewDocument(document);
+  DidInstallNewDocument(document, previous_csp);
 
   // This must be called before the document is opened, otherwise HTML parser
   // will use stale values from HTMLParserOption.
