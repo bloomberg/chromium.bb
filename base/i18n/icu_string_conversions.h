cfd03ef5de0c0b484f0dda41f246e7e97be27394
Claudio DeSouza <claudiomdsjr@gmail.com>
2018-11-26 17:51:16 +0000

    [base] Use StringPiece for codepage conversion
    The basic codepage converion functions in base, namely, CodepageToUTF16,
    CodepageToUTF16, and ConvertToUtf8AndNormalize are now taking
    StringPiece as the input type. This allows more flexibility when using
    theses interfaces, as well as providing the benefit of avoiding
    unnecessary string copies.
    
    Change-Id: I5922bd7cc3236f99fbac9393cba61afd9c475af3
    Reviewed-on: https://chromium-review.googlesource.com/c/1349652
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#610877}
    Former-commit-id: 32ce61668482f1ca9d9dcedab55081fc01049736

--- base/i18n/icu_string_conversions.h
+++ base/i18n/icu_string_conversions.h
@@ -10,6 +10,7 @@
 #include "base/i18n/base_i18n_export.h"
 #include "base/i18n/i18n_constants.h"
 #include "base/strings/string16.h"
+#include "base/strings/string_piece.h"
 
 namespace base {
 
@@ -37,18 +38,18 @@ class OnStringConversionError {
 // Converts between UTF-16 strings and the encoding specified.  If the
 // encoding doesn't exist or the encoding fails (when on_error is FAIL),
 // returns false.
-BASE_I18N_EXPORT bool UTF16ToCodepage(const string16& utf16,
+BASE_I18N_EXPORT bool UTF16ToCodepage(base::StringPiece16 utf16,
                                       const char* codepage_name,
                                       OnStringConversionError::Type on_error,
                                       std::string* encoded);
-BASE_I18N_EXPORT bool CodepageToUTF16(const std::string& encoded,
+BASE_I18N_EXPORT bool CodepageToUTF16(base::StringPiece encoded,
                                       const char* codepage_name,
                                       OnStringConversionError::Type on_error,
                                       string16* utf16);
 
 // Converts from any codepage to UTF-8 and ensures the resulting UTF-8 is
 // normalized.
-BASE_I18N_EXPORT bool ConvertToUtf8AndNormalize(const std::string& text,
+BASE_I18N_EXPORT bool ConvertToUtf8AndNormalize(base::StringPiece text,
                                                 const std::string& charset,
                                                 std::string* result);
 
