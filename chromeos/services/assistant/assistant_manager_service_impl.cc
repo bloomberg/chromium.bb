db35c1d4aa7056ab0d0141231417aa56fd85550e
yileili <yileili@google.com>
2018-11-26 19:55:40 +0000

    Reland "Use cast http connection in libassistant."
    This is a reland of 367cbc99d9431cbaf5dc9e5855ba467628818e7b [formerly 36f90a0e10de3a0ac87791db9ccb463246e6e867]
    
    Original change's description:
    > Use cast http connection in libassistant.
    >
    > Bug: b:118193766
    > Test: Manual
    >
    > Change-Id: Ia0cd58e42761b35b9f7281344266505074d59936
    > Reviewed-on: https://chromium-review.googlesource.com/c/1321956
    > Commit-Queue: Yilei Li <yileili@google.com>
    > Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#607318}
    
    Bug: b:118193766
    Change-Id: Ice6e98bea5c1d97f538d7de98c4c799dc72788ae
    Reviewed-on: https://chromium-review.googlesource.com/c/1337292
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Yilei Li <yileili@google.com>
    Cr-Commit-Position: refs/heads/master@{#610923}
    Former-commit-id: e813fa16c9a5ce4060f6ca7f6cd34a452285a189

--- chromeos/services/assistant/assistant_manager_service_impl.cc
+++ chromeos/services/assistant/assistant_manager_service_impl.cc
@@ -107,6 +107,7 @@ AssistantManagerServiceImpl::AssistantManagerServiceImpl(
     network::NetworkConnectionTracker* network_connection_tracker)
     : action_module_(std::make_unique<action::CrosActionModule>(this)),
       main_thread_task_runner_(base::ThreadTaskRunnerHandle::Get()),
+      chromium_api_delegate_(),
       assistant_settings_manager_(
           std::make_unique<AssistantSettingsManagerImpl>(this)),
       display_connection_(std::make_unique<CrosDisplayConnection>(this)),
@@ -784,6 +785,8 @@ AssistantManagerServiceImpl::StartAssistantInternal(
   assistant_manager_internal->SetDisplayConnection(display_connection_.get());
   assistant_manager_internal->RegisterActionModule(action_module_.get());
   assistant_manager_internal->SetAssistantManagerDelegate(this);
+  assistant_manager_internal->GetFuchsiaApiHelperOrDie()->SetFuchsiaApiDelegate(
+      &chromium_api_delegate_);
   assistant_manager->AddConversationStateListener(this);
   assistant_manager->AddDeviceStateListener(this);
 
