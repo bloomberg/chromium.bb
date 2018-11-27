f4652ec4b2999651952bb453928a8964c83d355e
kyle Ju <kyleju@chromium.org>
2018-11-27 17:26:57 +0000

    Add an edge case to PR clenaup tool.
    Address the edge case where when a CL no longer has exportable changes, the PR should
    be closed and the branch should be deleted.
    
    
    Bug: 852014, 750942
    Change-Id: I86adf85a3dcec7a9fac088fa42e2e357bbf1285a
    Reviewed-on: https://chromium-review.googlesource.com/c/1349471
    Commit-Queue: Robert Ma <robertma@chromium.org>
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611164}
    Former-commit-id: 46c9d5ef85ac910175eaeb1c4c44f708cde94f5f

--- third_party/blink/tools/blinkpy/w3c/pr_cleanup_tool_unittest.py
+++ third_party/blink/tools/blinkpy/w3c/pr_cleanup_tool_unittest.py
@@ -3,6 +3,7 @@ import json
 
 from blinkpy.common.host_mock import MockHost
 from blinkpy.common.system.log_testing import LoggingTestCase
+from blinkpy.common.path_finder import RELATIVE_WEB_TESTS
 from blinkpy.w3c.gerrit_mock import MockGerritAPI, MockGerritCL
 from blinkpy.w3c.pr_cleanup_tool import PrCleanupTool
 from blinkpy.w3c.wpt_github import PullRequest
@@ -24,7 +25,7 @@ class PrCleanupToolTest(LoggingTestCase):
             }))
         self.host = host
 
-    def test_main_successful_delete(self):
+    def test_main_successful_close_abandoned_cl(self):
         pr_cleanup = PrCleanupTool(self.host)
         pr_cleanup.wpt_github = MockWPTGitHub(pull_requests=[
             PullRequest(title='title1', number=1234, body='Change-Id: 88', state='open', labels=[]),
@@ -48,4 +49,36 @@ class PrCleanupToolTest(LoggingTestCase):
         self.assertEqual(pr_cleanup.gerrit.cls_queried, ['88'])
         self.assertEqual(pr_cleanup.wpt_github.calls, [
             'all_pull_requests',
-            'add_comment "Close this PR because the Chromium CL has been abandoned."', 'update_pr'])
+            'add_comment "Close this PR because the Chromium CL has been abandoned."',
+            'update_pr', 'get_pr_branch', 'delete_remote_branch'])
+
+    def test_main_successful_close_no_exportable_changes(self):
+        pr_cleanup = PrCleanupTool(self.host)
+        pr_cleanup.wpt_github = MockWPTGitHub(pull_requests=[
+            PullRequest(title='title1', number=1234, body='Change-Id: 99', state='open', labels=[]),
+        ])
+        pr_cleanup.gerrit = MockGerritAPI()
+        pr_cleanup.gerrit.cl = MockGerritCL(
+            data={
+                'change_id': 'I001',
+                'subject': 'subject',
+                '_number': 1234,
+                'status': 'MERGED',
+                'current_revision': '1',
+                'has_review_started': True,
+                'revisions': {
+                    '1': {'commit_with_footers': 'a commit with footers',
+                          'files': {
+                              RELATIVE_WEB_TESTS + 'foo/bar.html': '',
+                          }}
+                },
+                'owner': {'email': 'test@chromium.org'},
+            },
+            api=pr_cleanup.gerrit)
+        pr_cleanup.main(['--credentials-json', '/tmp/credentials.json'])
+        self.assertEqual(pr_cleanup.gerrit.cls_queried, ['99'])
+        self.assertEqual(pr_cleanup.wpt_github.calls, [
+            'all_pull_requests',
+            'add_comment "Close this PR because the Chromium'
+            ' CL does not have exportable changes."',
+            'update_pr', 'get_pr_branch', 'delete_remote_branch'])
