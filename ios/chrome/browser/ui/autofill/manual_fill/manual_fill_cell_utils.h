849ee655b76e0e4feecdebef4ecb26354ad00b47
David Jean <djean@google.com>
2018-11-27 13:53:12 +0000

    [ios] combine password cells for manual fallback
    Also gray out "No Username" label.
    
    Bug: 905663
    Change-Id: Iabbbf1ce7d20f62e72896ad08b5a239d0fbd14dc
    Reviewed-on: https://chromium-review.googlesource.com/c/1350616
    Commit-Queue: David Jean <djean@chromium.org>
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611091}
    Former-commit-id: b8f083570e6ba0840e53821dc9800f49d8fb4483

--- ios/chrome/browser/ui/autofill/manual_fill/manual_fill_cell_utils.h
+++ ios/chrome/browser/ui/autofill/manual_fill/manual_fill_cell_utils.h
@@ -7,16 +7,47 @@
 
 #import <UIKit/UIKit.h>
 
+namespace {
+
+// The multiplier for the base system spacing at the top margin.
+static const CGFloat TopSystemSpacingMultiplier = 1.58;
+
+// The multiplier for the base system spacing between elements (vertical).
+static const CGFloat MiddleSystemSpacingMultiplier = 1.83;
+
+// The multiplier for the base system spacing at the bottom margin.
+static const CGFloat BottomSystemSpacingMultiplier = 2.26;
+
+// Top and bottom margins for buttons.
+static const CGFloat ButtonVerticalMargin = 12;
+
+// Left and right margins of the cell content and buttons.
+static const CGFloat ButtonHorizontalMargin = 16;
+
+}  // namespace
+
 // Creates a blank button in fallback style, for the given |action| and
 // |target|.
 UIButton* CreateButtonWithSelectorAndTarget(SEL action, id target);
 
 // Sets vertical constraints on firstBaselineAnchor for the button or label rows
 // in |views| inside |container| starting at its topAnchor. Returns the applied
-// constrainst to allow caller to deactivate them later.
+// constrainst to allow caller to deactivate them later.  Defaults multipliers
+// are applied.
 NSArray<NSLayoutConstraint*>* VerticalConstraintsSpacingForViewsInContainer(
     NSArray<UIView*>* views,
     UIView* container);
+
+// Sets vertical constraints on firstBaselineAnchor for the button or label rows
+// in |views| inside |container| starting at its topAnchor. Returns the applied
+// constrainst to allow caller to deactivate them later.
+NSArray<NSLayoutConstraint*>*
+VerticalConstraintsSpacingForViewsInContainerWithMultipliers(
+    NSArray<UIView*>* views,
+    UIView* container,
+    CGFloat topSystemSpacingMultiplier,
+    CGFloat middleSystemSpacingMultiplier,
+    CGFloat BottomSystemSpacingMultiplier);
 
 // Sets constraints for the given |views|, so as to lay them out horizontally,
 // parallel to the given |guide| view, and applying the given constant |shift|
