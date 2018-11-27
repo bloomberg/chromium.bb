bf3a10cc8afc9844c1733d4b6839c2090323c4c2
Greg Kerr <kerrnel@chromium.org>
2018-11-27 18:25:46 +0000

    macOS Sandbox: Add unit test for pasteboard in sandboxed processes.
    This converts the V1 unit test, checking pasteboard access in sandboxed
    processes, to the V2 sandbox.
    
    Bug: 902597
    Change-Id: Id11e2c597a55f9e66f45d1edf6b7c7bcb8396f57
    Reviewed-on: https://chromium-review.googlesource.com/c/1327545
    Commit-Queue: Greg Kerr <kerrnel@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611202}
    Former-commit-id: b25043dd7b70be015d4f17e07910068a1f9feb08

--- content/browser/sandbox_mac_unittest.mm
+++ content/browser/sandbox_mac_unittest.mm
@@ -2,9 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import <Cocoa/Cocoa.h>
 #import <Foundation/Foundation.h>
 
+#include "base/bind.h"
+#include "base/callback.h"
 #include "base/command_line.h"
+#include "base/memory/ref_counted.h"
 #include "base/posix/eintr_wrapper.h"
 #include "base/process/kill.h"
 #include "base/strings/stringprintf.h"
@@ -14,11 +18,19 @@
 #include "content/browser/sandbox_parameters_mac.h"
 #include "sandbox/mac/seatbelt.h"
 #include "sandbox/mac/seatbelt_exec.h"
+#include "services/service_manager/sandbox/mac/audio.sb.h"
+#include "services/service_manager/sandbox/mac/cdm.sb.h"
 #include "services/service_manager/sandbox/mac/common_v2.sb.h"
+#include "services/service_manager/sandbox/mac/gpu_v2.sb.h"
+#include "services/service_manager/sandbox/mac/nacl_loader.sb.h"
+#include "services/service_manager/sandbox/mac/pdf_compositor.sb.h"
+#include "services/service_manager/sandbox/mac/ppapi_v2.sb.h"
 #include "services/service_manager/sandbox/mac/renderer_v2.sb.h"
+#include "services/service_manager/sandbox/mac/utility.sb.h"
 #include "services/service_manager/sandbox/switches.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/multiprocess_func_list.h"
+#import "ui/base/clipboard/clipboard_util_mac.h"
 
 namespace content {
 namespace {
@@ -27,6 +39,7 @@ namespace {
 // and is safe since this is only a unit test.
 constexpr char kTempDirSuffix[] =
     "(allow file* (subpath \"/private/var/folders\"))";
+constexpr char kClipboardArg[] = "pasteboard-name";
 
 class SandboxMacTest : public base::MultiProcessTest {
  protected:
@@ -37,7 +50,113 @@ class SandboxMacTest : public base::MultiProcessTest {
     return cl;
   }
 
+  std::string ProfileForSandbox(const std::string& sandbox_profile) {
+    return std::string(service_manager::kSeatbeltPolicyString_common_v2) +
+           sandbox_profile + kTempDirSuffix;
+  }
+
+  void ExecuteWithParams(const std::string& procname,
+                         const std::string& sub_profile,
+                         void (*setup)(sandbox::SeatbeltExecClient*)) {
+    std::string profile = ProfileForSandbox(sub_profile);
+    sandbox::SeatbeltExecClient client;
+    client.SetProfile(profile);
+    setup(&client);
+
+    pipe_ = client.GetReadFD();
+    ASSERT_GE(pipe_, 0);
+
+    base::LaunchOptions options;
+    options.fds_to_remap.push_back(std::make_pair(pipe_, pipe_));
+
+    base::Process process = SpawnChildWithOptions(procname, options);
+    ASSERT_TRUE(process.IsValid());
+    ASSERT_TRUE(client.SendProfile());
+
+    int rv = -1;
+    ASSERT_TRUE(base::WaitForMultiprocessTestChildExit(
+        process, TestTimeouts::action_timeout(), &rv));
+    EXPECT_EQ(0, rv);
+  }
+
+  void ExecuteInAudioSandbox(const std::string& procname) {
+    ExecuteWithParams(procname, service_manager::kSeatbeltPolicyString_audio,
+                      &content::SetupCommonSandboxParameters);
+  }
+
+  void ExecuteInCDMSandbox(const std::string& procname) {
+    ExecuteWithParams(procname, service_manager::kSeatbeltPolicyString_cdm,
+                      &content::SetupCDMSandboxParameters);
+  }
+
+  void ExecuteInGPUSandbox(const std::string& procname) {
+    ExecuteWithParams(procname, service_manager::kSeatbeltPolicyString_gpu_v2,
+                      &content::SetupCommonSandboxParameters);
+  }
+
+  void ExecuteInNaClSandbox(const std::string& procname) {
+    ExecuteWithParams(procname,
+                      service_manager::kSeatbeltPolicyString_nacl_loader,
+                      &content::SetupCommonSandboxParameters);
+  }
+
+  void ExecuteInPDFSandbox(const std::string& procname) {
+    ExecuteWithParams(procname,
+                      service_manager::kSeatbeltPolicyString_pdf_compositor,
+                      &content::SetupCommonSandboxParameters);
+  }
+
+  void ExecuteInPpapiSandbox(const std::string& procname) {
+    ExecuteWithParams(procname, service_manager::kSeatbeltPolicyString_ppapi_v2,
+                      &content::SetupPPAPISandboxParameters);
+  }
+
+  void ExecuteInRendererSandbox(const std::string& procname) {
+    ExecuteWithParams(procname,
+                      service_manager::kSeatbeltPolicyString_renderer_v2,
+                      &content::SetupCommonSandboxParameters);
+  }
+
+  void ExecuteInUtilitySandbox(const std::string& procname) {
+    ExecuteWithParams(procname, service_manager::kSeatbeltPolicyString_utility,
+                      [](sandbox::SeatbeltExecClient* client) -> void {
+                        content::SetupUtilitySandboxParameters(
+                            client, *base::CommandLine::ForCurrentProcess());
+                      });
+  }
+
+  void ExecuteInAllSandboxTypes(const std::string& multiprocess_main,
+                                base::RepeatingClosure after_each) {
+    using ExecuteFuncT = void (SandboxMacTest::*)(const std::string&);
+    constexpr ExecuteFuncT kExecuteFuncs[] = {
+        &SandboxMacTest::ExecuteInAudioSandbox,
+        &SandboxMacTest::ExecuteInCDMSandbox,
+        &SandboxMacTest::ExecuteInGPUSandbox,
+        &SandboxMacTest::ExecuteInNaClSandbox,
+        &SandboxMacTest::ExecuteInPDFSandbox,
+        &SandboxMacTest::ExecuteInPpapiSandbox,
+        &SandboxMacTest::ExecuteInRendererSandbox,
+        &SandboxMacTest::ExecuteInUtilitySandbox,
+    };
+
+    for (ExecuteFuncT execute_func : kExecuteFuncs) {
+      (this->*execute_func)(multiprocess_main);
+      after_each.Run();
+    }
+  }
+
   int pipe_{0};
+};
+
+class SandboxMacClipboardTest : public SandboxMacTest {
+ protected:
+  base::CommandLine MakeCmdLine(const std::string& procname) override {
+    base::CommandLine cl = SandboxMacTest::MakeCmdLine(procname);
+    cl.AppendSwitchASCII(kClipboardArg, pasteboard_name_);
+    return cl;
+  }
+
+  std::string pasteboard_name_{};
 };
 
 void CheckCreateSeatbeltServer() {
@@ -72,29 +191,35 @@ MULTIPROCESS_TEST_MAIN(RendererWriteProcess) {
 }
 
 TEST_F(SandboxMacTest, RendererCannotWriteHomeDir) {
-  std::string profile =
-      std::string(service_manager::kSeatbeltPolicyString_common_v2) +
-      service_manager::kSeatbeltPolicyString_renderer_v2 + kTempDirSuffix;
+  ExecuteInRendererSandbox("RendererWriteProcess");
+}
 
-  sandbox::SeatbeltExecClient client;
-  client.SetProfile(profile);
-  content::SetupCommonSandboxParameters(&client);
+MULTIPROCESS_TEST_MAIN(ClipboardAccessProcess) {
+  CheckCreateSeatbeltServer();
 
-  pipe_ = client.GetReadFD();
-  ASSERT_GE(pipe_, 0);
+  base::CommandLine* cl = base::CommandLine::ForCurrentProcess();
+  std::string pasteboard_name = cl->GetSwitchValueASCII(kClipboardArg);
+  CHECK(!pasteboard_name.empty());
+  CHECK([NSPasteboard pasteboardWithName:base::SysUTF8ToNSString(
+                                             pasteboard_name)] == nil);
+  CHECK([NSPasteboard generalPasteboard] == nil);
 
-  base::LaunchOptions options;
-  options.fds_to_remap.push_back(std::make_pair(pipe_, pipe_));
+  return 0;
+}
 
-  base::Process process =
-      SpawnChildWithOptions("RendererWriteProcess", options);
-  ASSERT_TRUE(process.IsValid());
-  ASSERT_TRUE(client.SendProfile());
+TEST_F(SandboxMacClipboardTest, ClipboardAccess) {
+  scoped_refptr<ui::UniquePasteboard> pb = new ui::UniquePasteboard;
+  ASSERT_TRUE(pb->get());
+  EXPECT_EQ([[pb->get() types] count], 0U);
 
-  int rv = -1;
-  ASSERT_TRUE(base::WaitForMultiprocessTestChildExit(
-      process, TestTimeouts::action_timeout(), &rv));
-  EXPECT_EQ(0, rv);
+  pasteboard_name_ = base::SysNSStringToUTF8([pb->get() name]);
+
+  ExecuteInAllSandboxTypes("ClipboardAccessProcess",
+                           base::BindRepeating(
+                               [](scoped_refptr<ui::UniquePasteboard> pb) {
+                                 ASSERT_EQ([[pb->get() types] count], 0U);
+                               },
+                               pb));
 }
 
 }  // namespace content
