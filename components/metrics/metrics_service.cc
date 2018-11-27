4ba0470533fc261e688f11615f23f4ea723082ba
Henrique Nakashima <hnakashima@chromium.org>
2018-11-27 19:29:57 +0000

    Reland "Flatten UpdateMetricsUsagePrefs - skip Metrics and Reporting service."
    This is a reland of b2c54c0be88ecd355ef4475fa3da01d74dd049ca [formerly a061c829b65ad5865f68112aac182c89cc622311]
    
    Reverted due to being a suspect in crbug.com/906242, but after reverting the
    bug persists and was bisected to another CL.
    
    Original change's description:
    > Flatten UpdateMetricsUsagePrefs - skip Metrics and Reporting service.
    >
    > UpdateMetricsUsagePrefs() is a chain of calls:
    > - ChromeDataUseMeasurement::UpdateDataUseToMetricsService()
    >   -> UpdateMetricsUsagePrefs()
    >   -> MetricsService::UpdateMetricsUsagePrefs()
    >   -> ReportingService::UpdateMetricsUsagePrefs()
    >   -> DataUseTracker::UpdateMetricsUsagePrefs()
    >
    > It can be reduced to:
    > - ChromeDataUseMeasurement::UpdateDataUseToMetricsService()
    >   -> UpdateMetricsUsagePrefs()
    >   -> DataUseTracker::UpdateMetricsUsagePrefs()
    >
    > This removes the dependency from ChromeDataUseMeasurement to
    > MetricsService and ReportingService.
    >
    > Bug: 902791
    > Change-Id: I38a6d22d1ff823134c79bc342ea32be0f89cda77
    > Reviewed-on: https://chromium-review.googlesource.com/c/1334267
    > Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    > Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    > Reviewed-by: rajendrant <rajendrant@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#608567}
    
    Bug: 902791
    Change-Id: I27abaac1d265e63ae5a921ab2839a77ae8582720
    Reviewed-on: https://chromium-review.googlesource.com/c/1348249
    Reviewed-by: rajendrant <rajendrant@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611250}
    Former-commit-id: 0ff7197996b04730cfbaa5801a57ef174a56ee3a

--- components/metrics/metrics_service.cc
+++ components/metrics/metrics_service.cc
@@ -452,14 +452,6 @@ void MetricsService::PushExternalLog(const std::string& log) {
   log_store()->StoreLog(log, MetricsLog::ONGOING_LOG);
 }
 
-void MetricsService::UpdateMetricsUsagePrefs(int message_size,
-                                             bool is_cellular,
-                                             bool is_metrics_service_usage) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  reporting_service_.UpdateMetricsUsagePrefs(message_size, is_cellular,
-                                             is_metrics_service_usage);
-}
-
 //------------------------------------------------------------------------------
 // private methods
 //------------------------------------------------------------------------------
