8f328e4e7758d8a7feefbdce0dd0444950bee04a
Darren Shen <shend@chromium.org>
2018-11-27 23:45:01 +0000

    [VK] Add feature flag for integrated keyboard settings.
    Bug: 895886
    Change-Id: I8e9bfd692e49cde029cb6fd8df1660713be45e05
    Reviewed-on: https://chromium-review.googlesource.com/c/1341772
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611420}
    Former-commit-id: 28fa329ceaf5909a8ad542ef458b31b12624cfab

--- ui/base/ui_base_features.h
+++ ui/base/ui_base_features.h
@@ -19,6 +19,9 @@ UI_BASE_EXPORT extern const base::Feature
 UI_BASE_EXPORT extern const base::Feature kEnableStylusVirtualKeyboard;
 UI_BASE_EXPORT extern const base::Feature kEnableVirtualKeyboardUkm;
 UI_BASE_EXPORT extern const base::Feature kExperimentalUi;
+#if defined(OS_CHROMEOS)
+UI_BASE_EXPORT extern const base::Feature kSettingsShowsPerKeyboardSettings;
+#endif  // defined(OS_CHROMEOS)
 UI_BASE_EXPORT extern const base::Feature kInputMethodSettingsUiUpdate;
 UI_BASE_EXPORT extern const base::Feature kSystemKeyboardLock;
 UI_BASE_EXPORT extern const base::Feature kTouchableAppContextMenu;
