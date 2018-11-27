40178ba473b4f204c662acdc0aa992fd4ce3eb9f
Mike Dougherty <michaeldo@chromium.org>
2018-11-27 19:26:50 +0000

    Remove usage of no-op macro DEFINE_WEB_STATE_USER_DATA_KEY.
    This CL removes macro usage from /ios/chrome/browser/webui.
    
    This CL was uploaded by git cl split.
    
    R=eugenebut@chromium.org
    
    Bug: 589840
    Change-Id: I047edb54ce7991b4aefd3af99a89affaa53f7a56
    Reviewed-on: https://chromium-review.googlesource.com/c/1351956
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611248}
    Former-commit-id: efb256d70759cffbf1c7f78e87b430d774bb39de

--- ios/chrome/browser/webui/net_export_tab_helper.mm
+++ ios/chrome/browser/webui/net_export_tab_helper.mm
@@ -12,8 +12,6 @@
 #error "This file requires ARC support."
 #endif
 
-DEFINE_WEB_STATE_USER_DATA_KEY(NetExportTabHelper);
-
 // static
 void NetExportTabHelper::CreateForWebState(
     web::WebState* web_state,
