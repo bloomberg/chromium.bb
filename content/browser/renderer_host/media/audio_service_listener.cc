0f49ae4dc7972a0f5044b23569f2f964fc2b159b
Ken Rockot <rockot@google.com>
2018-11-27 18:10:27 +0000

    Fix raciness in AudioServiceListener
    This fixes potential race conditions in AudioServiceListener resulting
    from the fact that a service instance can become unreachable (and may
    therefore be replaced with a new one) before it is officially designated
    as "stopped" by the Service Manager, even in the case of singleton
    services.
    
    The ServiceManagerListener API is augmented to convey a service
    instance's current running state via RunningServiceInfo. This is used
    by AudioServiceListener to more accurately track the running audio
    service instance.
    
    The listener now tracks the Identity of the most recently created
    audio service instance and responds to events accordingly.
    
    This also updates the logic and comments in resource_coordinator's
    ProcessMap to accurately reflect current guarantees of the
    ServiceManagerListener API, since these have changed slightly.
    
    NOTRY=true
    
    Bug: 818593,907898,908319
    Change-Id: If3e8a35aa90db72a34c8f5f2d458f0f24c3447eb
    Reviewed-on: https://chromium-review.googlesource.com/c/1350649
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611193}
    Former-commit-id: 3bdf26ad8f19ee9b629e09eb04ecc95c870fe772

--- content/browser/renderer_host/media/audio_service_listener.cc
+++ content/browser/renderer_host/media/audio_service_listener.cc
@@ -22,10 +22,12 @@ AudioServiceListener::Metrics::Metrics(const base::TickClock* clock)
 
 AudioServiceListener::Metrics::~Metrics() = default;
 
-void AudioServiceListener::Metrics::ServiceAlreadyRunning() {
+void AudioServiceListener::Metrics::ServiceAlreadyRunning(
+    service_manager::mojom::InstanceState state) {
   LogServiceStartStatus(ServiceStartStatus::kAlreadyStarted);
-  started_ = clock_->NowTicks();
   initial_downtime_start_ = base::TimeTicks();
+  if (state == service_manager::mojom::InstanceState::kStarted)
+    started_ = clock_->NowTicks();
 }
 
 void AudioServiceListener::Metrics::ServiceCreated() {
@@ -73,6 +75,8 @@ void AudioServiceListener::Metrics::ServiceStopped() {
   UMA_HISTOGRAM_CUSTOM_TIMES("Media.AudioService.ObservedUptime",
                              stopped_ - started_, base::TimeDelta(),
                              base::TimeDelta::FromDays(7), 50);
+
+  created_ = base::TimeTicks();
   started_ = base::TimeTicks();
 }
 
@@ -115,10 +119,17 @@ void AudioServiceListener::OnInit(
   DCHECK_CALLED_ON_VALID_SEQUENCE(owning_sequence_);
   for (const service_manager::mojom::RunningServiceInfoPtr& instance :
        running_services) {
-    if (instance->identity.name() == audio::mojom::kServiceName) {
-      process_id_ = instance->pid;
-      metrics_.ServiceAlreadyRunning();
+    if (instance->identity.name() == audio::mojom::kServiceName &&
+        instance->state !=
+            service_manager::mojom::InstanceState::kUnreachable) {
+      current_instance_identity_ = instance->identity;
+      current_instance_state_ = instance->state;
+      metrics_.ServiceAlreadyRunning(instance->state);
       MaybeSetLogFactory();
+
+      // NOTE: This may not actually be a valid PID yet. If not, we will
+      // receive OnServicePIDReceived soon.
+      process_id_ = instance->pid;
       break;
     }
   }
@@ -129,6 +140,24 @@ void AudioServiceListener::OnServiceCreated(
   DCHECK_CALLED_ON_VALID_SEQUENCE(owning_sequence_);
   if (service->identity.name() != audio::mojom::kServiceName)
     return;
+
+  if (current_instance_identity_) {
+    // If we were already tracking an instance of the service, it must be dying
+    // soon. We'll start tracking the new instance instead now, so simulate
+    // stoppage of the old one.
+    DCHECK(service->identity != current_instance_identity_);
+    if (current_instance_state_ ==
+        service_manager::mojom::InstanceState::kCreated) {
+      OnServiceFailedToStart(*current_instance_identity_);
+    } else {
+      DCHECK_EQ(service_manager::mojom::InstanceState::kStarted,
+                *current_instance_state_);
+      OnServiceStopped(*current_instance_identity_);
+    }
+  }
+
+  current_instance_identity_ = service->identity;
+  current_instance_state_ = service_manager::mojom::InstanceState::kCreated;
   metrics_.ServiceCreated();
   MaybeSetLogFactory();
 }
@@ -139,7 +168,11 @@ void AudioServiceListener::OnServiceStarted(
   DCHECK_CALLED_ON_VALID_SEQUENCE(owning_sequence_);
   if (identity.name() != audio::mojom::kServiceName)
     return;
-  process_id_ = pid;
+
+  DCHECK(identity == current_instance_identity_);
+  DCHECK(current_instance_state_ ==
+         service_manager::mojom::InstanceState::kCreated);
+  current_instance_state_ = service_manager::mojom::InstanceState::kStarted;
   metrics_.ServiceStarted();
 }
 
@@ -147,30 +180,41 @@ void AudioServiceListener::OnServicePIDReceived(
     const ::service_manager::Identity& identity,
     uint32_t pid) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(owning_sequence_);
-  if (identity.name() != audio::mojom::kServiceName)
+  if (identity != current_instance_identity_)
     return;
+
   process_id_ = pid;
 }
 
 void AudioServiceListener::OnServiceFailedToStart(
     const ::service_manager::Identity& identity) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(owning_sequence_);
-  if (identity.name() != audio::mojom::kServiceName)
+  if (identity != current_instance_identity_)
     return;
+
   metrics_.ServiceFailedToStart();
+  current_instance_identity_.reset();
+  current_instance_state_.reset();
+  process_id_ = base::kNullProcessId;
+  log_factory_is_set_ = false;
 }
 
 void AudioServiceListener::OnServiceStopped(
     const ::service_manager::Identity& identity) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(owning_sequence_);
-  if (identity.name() != audio::mojom::kServiceName)
+  if (identity != current_instance_identity_)
     return;
+
   metrics_.ServiceStopped();
+  current_instance_identity_.reset();
+  current_instance_state_.reset();
+  process_id_ = base::kNullProcessId;
   log_factory_is_set_ = false;
 }
 
 void AudioServiceListener::MaybeSetLogFactory() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(owning_sequence_);
+  DCHECK(current_instance_identity_);
   if (!base::FeatureList::IsEnabled(features::kAudioServiceOutOfProcess) ||
       !connector_ || log_factory_is_set_)
     return;
@@ -179,7 +223,7 @@ void AudioServiceListener::MaybeSetLogFactory() {
   mojo::MakeStrongBinding(std::make_unique<AudioLogFactory>(),
                           mojo::MakeRequest(&audio_log_factory_ptr));
   audio::mojom::LogFactoryManagerPtr log_factory_manager_ptr;
-  connector_->BindInterface(audio::mojom::kServiceName,
+  connector_->BindInterface(*current_instance_identity_,
                             mojo::MakeRequest(&log_factory_manager_ptr));
   log_factory_manager_ptr->SetLogFactory(std::move(audio_log_factory_ptr));
   log_factory_is_set_ = true;
