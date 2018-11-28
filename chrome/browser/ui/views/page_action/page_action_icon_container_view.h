310e3f08e257e4a55c0e4bdf65f51e297ad7b9a1
Alan Cutter <alancutter@chromium.org>
2018-11-28 03:23:23 +0000

    Use Params struct to construct PageActionIconContainerView
    The PageActionIconContainerView constructor takes 7 arguments.
    This CL replaces them with a single Params struct for readability at the
    call sites (in the absence of named function parameter passing in C++).
    
    This CL has no changes in behaviour.
    
    Bug: 788051
    Change-Id: I423faf616c0f06236479c01579dd971c86251361
    Reviewed-on: https://chromium-review.googlesource.com/c/1347640
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611545}
    Former-commit-id: 58906f9e202e2af8a7ba1de189df7defa3668d6a

--- chrome/browser/ui/views/page_action/page_action_icon_container_view.h
+++ chrome/browser/ui/views/page_action/page_action_icon_container_view.h
@@ -12,6 +12,7 @@
 #include "components/zoom/zoom_event_manager.h"
 #include "components/zoom/zoom_event_manager_observer.h"
 #include "third_party/skia/include/core/SkColor.h"
+#include "ui/gfx/color_palette.h"
 #include "ui/views/view.h"
 
 class Browser;
@@ -24,14 +25,24 @@ class PageActionIconContainerView : public views::View,
                                     public PageActionIconContainer,
                                     public zoom::ZoomEventManagerObserver {
  public:
-  PageActionIconContainerView(
-      const std::vector<PageActionIconType>& types_enabled,
-      int icon_size,
-      int between_icon_spacing,
-      Browser* browser,
-      CommandUpdater* command_updater,
-      PageActionIconView::Delegate* page_action_icon_delegate,
-      LocationBarView::Delegate* location_bar_delegate);
+  struct Params {
+    Params();
+    ~Params();
+
+    std::vector<PageActionIconType> types_enabled;
+    int icon_size = 0;
+    SkColor icon_color = gfx::kPlaceholderColor;
+    int between_icon_spacing = 0;
+    Browser* browser = nullptr;
+    CommandUpdater* command_updater = nullptr;
+    PageActionIconView::Delegate* page_action_icon_delegate = nullptr;
+    LocationBarView::Delegate* location_bar_delegate = nullptr;
+
+   private:
+    DISALLOW_COPY_AND_ASSIGN(Params);
+  };
+
+  explicit PageActionIconContainerView(const Params& params);
   ~PageActionIconContainerView() override;
 
   PageActionIconView* GetPageActionIconView(PageActionIconType type);
@@ -43,7 +54,7 @@ class PageActionIconContainerView : public views::View,
   // whether any icons were activated.
   bool ActivateFirstInactiveBubbleForAccessibility();
 
-  // Update the icons color, must be called before painting.
+  // Update the icons color.
   void SetIconColor(SkColor icon_color);
 
   // See comment in browser_window.h for more info.
