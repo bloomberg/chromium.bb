3bfea7861b1702c2561da52ca819082ae524b185
Garrett Beaty <gbeaty@chromium.org>
2018-11-28 22:42:10 +0000

    Remove dead code dealing with gyp.
    Bug: 889992
    Change-Id: I3cc9b095ecd19acda49e1d1d34b5b064668d7e26
    Reviewed-on: https://chromium-review.googlesource.com/c/1343367
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Garrett Beaty <gbeaty@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611908}
    Former-commit-id: 9e08eee5ba3bbb558113660818c37fdca486690d

--- build/gyp_environment.py
+++ build/gyp_environment.py
@@ -8,15 +8,12 @@ gyp_chromium and landmines.py which run at different stages of runhooks. To
 make sure settings are consistent between them, all setup should happen here.
 """
 
-import gyp_helper
 import os
 import sys
 import vs_toolchain
 
 def SetEnvironment():
   """Sets defaults for GYP_* variables."""
-  gyp_helper.apply_chromium_gyp_env()
-
   # Default to ninja on linux and windows, but only if no generator has
   # explicitly been set.
   # Also default to ninja on mac, but only when not building chrome/ios.
