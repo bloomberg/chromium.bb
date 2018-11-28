0c0ea5902ec012da9ec018d4af0d435d26accc5b
Dana Fried <dfried@chromium.org>
2018-11-28 19:45:44 +0000

    Create generalization for layout manager examples.
    Will be used in a followup CL to simplify making examples for a new
    layout manager without having to duplicate a bunch of code.
    
    Change-Id: I3a37dc1656f70015170434c9da80ccbc20c1db88
    Reviewed-on: https://chromium-review.googlesource.com/c/1351968
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611825}
    Former-commit-id: 490183dad9aa435694dff6330d14726749dea892

--- ui/views/examples/box_layout_example.h
+++ ui/views/examples/box_layout_example.h
@@ -8,9 +8,7 @@
 #include "base/macros.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/button/label_button.h"
-#include "ui/views/controls/combobox/combobox_listener.h"
-#include "ui/views/controls/textfield/textfield_controller.h"
-#include "ui/views/examples/example_base.h"
+#include "ui/views/examples/layout_example_base.h"
 #include "ui/views/layout/box_layout.h"
 
 namespace views {
@@ -21,26 +19,14 @@ class Textfield;
 
 namespace examples {
 
-namespace {
-
-class ChildPanel;
-}
-
-class VIEWS_EXAMPLES_EXPORT BoxLayoutExample : public ExampleBase,
-                                               public ButtonListener,
-                                               public ComboboxListener,
-                                               public TextfieldController {
+class VIEWS_EXAMPLES_EXPORT BoxLayoutExample : public LayoutExampleBase {
  public:
   BoxLayoutExample();
   ~BoxLayoutExample() override;
 
-  // ExampleBase
-  void CreateExampleView(View* container) override;
-
  private:
-  friend views::examples::ChildPanel;
-  // ButtonListener
-  void ButtonPressed(Button* sender, const ui::Event& event) override;
+  // Set the border insets on the current BoxLayout instance.
+  void UpdateBorderInsets();
 
   // ComboboxListener
   void OnPerformAction(Combobox* combobox) override;
@@ -49,45 +35,12 @@ class VIEWS_EXAMPLES_EXPORT BoxLayoutExample : public ExampleBase,
   void ContentsChanged(Textfield* sender,
                        const base::string16& new_contents) override;
 
-  // Force the box_layout_panel_ to layout and repaint.
-  void RefreshLayoutPanel();
-
-  // Set the border insets on the current BoxLayout instance.
-  void UpdateBorderInsets();
-
-  // Create a new BoxLayout and ensure all settings match the current state of
-  // the various control_panel_ controls.
-  void UpdateLayoutManager();
-
-  // Create a Combobox with a label with |label_text| to the left. Adjust
-  // |vertical_pos| to |vertical_pos| + combo_box->height() + kSpacing.
-  Combobox* CreateCombobox(const base::string16& label_text,
-                           const char** items,
-                           int count,
-                           int& vertical_pos);
-
-  // Create just a Textfield at the current position of |horizontal_pos| and
-  // |vertical_pos|. Update |horizontal_pos| to |horizontal_pos| +
-  // text_field->width() + kSpacing.
-  Textfield* CreateRawTextfield(int& horizontal_pos,
-                                int vertical_pos,
-                                bool add);
-
-  // Create a Textfield with a label with |label_text| to the left. Adjust
-  // |vertical_pos| to |vertical_pos| + combo_box->height() + kSpacing.
-  Textfield* CreateTextfield(const base::string16& label_text,
-                             int& vertical_pos);
-
-  // Returns the current values contained in the child_panel_size_[] Textfields
-  // as a gfx::Size. If either value is negative or not a valid integer,
-  // default values are returned, 180 x 90 for width and height, respectively.
-  gfx::Size GetChildPanelSize() const;
+  // LayoutExampleBase
+  void ButtonPressedImpl(Button* sender) override;
+  void CreateAdditionalControls(int vertical_start_pos) override;
+  void UpdateLayoutManager() override;
 
   BoxLayout* layout_ = nullptr;
-  View* full_panel_ = nullptr;
-  View* box_layout_panel_ = nullptr;
-  View* control_panel_ = nullptr;
-  LabelButton* add_button_ = nullptr;
   Combobox* orientation_ = nullptr;
   Combobox* main_axis_alignment_ = nullptr;
   Combobox* cross_axis_alignment_ = nullptr;
@@ -95,9 +48,7 @@ class VIEWS_EXAMPLES_EXPORT BoxLayoutExample : public ExampleBase,
   Textfield* default_flex_ = nullptr;
   Textfield* min_cross_axis_size_ = nullptr;
   Textfield* border_insets_[4] = {nullptr, nullptr, nullptr, nullptr};
-  Textfield* child_panel_size_[2] = {nullptr, nullptr};
   Checkbox* collapse_margins_ = nullptr;
-  int panel_count_ = 0;
 
   DISALLOW_COPY_AND_ASSIGN(BoxLayoutExample);
 };
