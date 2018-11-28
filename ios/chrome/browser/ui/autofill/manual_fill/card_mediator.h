827cf428b23adedece5feebe2de0ef829ec7c8bf
David Jean <djean@google.com>
2018-11-28 14:02:44 +0000

    Reland "[ios] add credit card test for manual fallback"
    This is a reland of eb386ba89bc405ee98eb38d2edffd4a9d79212df [formerly 30e1dd180408413c8fce6dbe5b856c048a8ca908]
    
    Adding feature flags to slimnav.
    Revert bug: https://bugs.chromium.org/p/chromium/issues/detail?id=908970
    
    Original change's description:
    > [ios] add credit card test for manual fallback
    >
    > Also adds AutofillManualFallbackPhaseTwo to test bots.
    >
    > Bug: 845472
    > Change-Id: I6bd05781c09c24c0768e24acfe59efa7ce1da05d
    > Reviewed-on: https://chromium-review.googlesource.com/c/1337347
    > Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    > Reviewed-by: Ben Pastene <bpastene@chromium.org>
    > Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    > Commit-Queue: David Jean <djean@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#611171}
    
    TBR=rohitrao@chromium.org
    
    Bug: 845472,908970
    Change-Id: I2cc0cddf0be8cea447f45f69457afbe19c840f29
    Reviewed-on: https://chromium-review.googlesource.com/c/1352328
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611679}
    Former-commit-id: 965f2dd775e784aa146f1905cf1013f84a6ab512

--- ios/chrome/browser/ui/autofill/manual_fill/card_mediator.h
+++ ios/chrome/browser/ui/autofill/manual_fill/card_mediator.h
@@ -20,7 +20,6 @@ class CreditCard;
 
 namespace manual_fill {
 extern NSString* const ManageCardsAccessibilityIdentifier;
-extern NSString* const OtherCardsAccessibilityIdentifier;
 }  // namespace manual_fill
 
 // Object in charge of getting the cards relevant for the manual fill
