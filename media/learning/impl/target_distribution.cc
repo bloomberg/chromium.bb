97269620ec5c0bf6b07151f7a91203ff7d1f05f9
liberato@chromium.org <liberato@chromium.org>
2018-11-28 05:18:34 +0000

    Add Distribution to replace Model::TargetDistribution.
    This adds the Distribution class, which will replace the
    TargetDistribution typedef in Model.  It provides some
    convenient functionality, such as += and max-finding.
    
    None of the uses of TargetDistribution are updated here.
    
    Change-Id: I0e86d9dbaddcd5421cbc65e73213329603346733
    Reviewed-on: https://chromium-review.googlesource.com/c/1329881
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Fredrik Hubinette <hubbe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611569}
    Former-commit-id: 05fcc7df44f852c5016a4f7c05b647ff27e00ee5

---
+++ media/learning/impl/target_distribution.cc
@@ -0,0 +1,70 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/learning/impl/target_distribution.h"
+
+namespace media {
+namespace learning {
+
+TargetDistribution::TargetDistribution() = default;
+TargetDistribution::~TargetDistribution() = default;
+
+TargetDistribution& TargetDistribution::operator+=(
+    const TargetDistribution& rhs) {
+  for (auto& rhs_pair : rhs.counts()) {
+    counts_[rhs_pair.first] += rhs_pair.second;
+    total_counts_ += rhs_pair.second;
+  }
+
+  return *this;
+}
+
+TargetDistribution& TargetDistribution::operator+=(const TargetValue& rhs) {
+  counts_[rhs]++;
+  total_counts_++;
+  return *this;
+}
+
+int TargetDistribution::operator[](const TargetValue& value) const {
+  auto iter = counts_.find(value);
+  if (iter == counts_.end())
+    return 0;
+
+  return iter->second;
+}
+
+void TargetDistribution::Add(const TargetValue& value, int counts) {
+  counts_[value] += counts;
+  total_counts_ += counts;
+}
+
+bool TargetDistribution::FindSingularMax(TargetValue* value_out,
+                                         int* counts_out) const {
+  if (!counts_.size())
+    return false;
+
+  int unused_counts;
+  if (!counts_out)
+    counts_out = &unused_counts;
+
+  auto iter = counts_.begin();
+  *value_out = iter->first;
+  *counts_out = iter->second;
+  bool singular_max = true;
+  for (iter++; iter != counts_.end(); iter++) {
+    if (iter->second > *counts_out) {
+      *value_out = iter->first;
+      *counts_out = iter->second;
+      singular_max = true;
+    } else if (iter->second == *counts_out) {
+      // If this turns out to be the max, then it's not singular.
+      singular_max = false;
+    }
+  }
+
+  return singular_max;
+}
+
+}  // namespace learning
+}  // namespace media
