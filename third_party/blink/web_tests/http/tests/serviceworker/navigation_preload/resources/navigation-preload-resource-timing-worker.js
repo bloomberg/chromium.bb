35e11f238e9aec39327ee0e79ccee5c4c0c4b1fc
Matt Falkenhagen <falken@chromium.org>
2018-11-29 02:31:36 +0000

    Deflake http test navigation-preload-resource-timing.html
    This is the same fix as r583483 did for issue 872078. As that commit
    description says: "Before this CL, this test tried to get performance
    entries right after Response#text() is resolved, but Chrome doesn't
    guarantee that there will be a performance entry for the response at
    that point. This CL changes the test to use PerformanceObserver so that
    we can make sure that there will be a performance entry for the
    response."
    
    Bug: 846631
    Change-Id: Ia8aaf82939f4457b7e2427bbefacce79150fbb20
    Reviewed-on: https://chromium-review.googlesource.com/c/1353032
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#611989}
    Former-commit-id: c157b57df3962bfe1ee4b21ef522a8cc496ec784

--- third_party/blink/web_tests/http/tests/serviceworker/navigation_preload/resources/navigation-preload-resource-timing-worker.js
+++ third_party/blink/web_tests/http/tests/serviceworker/navigation_preload/resources/navigation-preload-resource-timing-worker.js
@@ -1,13 +1,34 @@
-self.addEventListener('activate', event => {
-    event.waitUntil(self.registration.navigationPreload.enable());
+async function wait_for_performance_entries(url) {
+  const entries = performance.getEntriesByName(url);
+  if (entries.length > 0) {
+    return entries;
+  }
+  return new Promise(resolve => {
+    new PerformanceObserver(list => {
+      const entries = list.getEntriesByName(url);
+      if (entries.length > 0) {
+        resolve(entries);
+      }
+    }).observe({ entryTypes: ['resource'] });
   });
+}
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.registration.navigationPreload.enable());
+});
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-      event.preloadResponse
-          .then(response => response.text())
-          .then(text =>
-            new Response(
-              JSON.stringify({timingEntries: performance.getEntries()}),
-              {headers: {'Content-Type': 'text/html'}})));
-  });
+  let headers;
+  event.respondWith(
+    event.preloadResponse
+      .then(response => {
+        headers = response.headers;
+        return response.text();
+      })
+      .then(_ => wait_for_performance_entries(event.request.url))
+      .then(entries => {
+        return new Response(JSON.stringify({timingEntries: entries}),
+                            {headers: {'Content-Type': 'text/html'}});
+      })
+  );
+});
