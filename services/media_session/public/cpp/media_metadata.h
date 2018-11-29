4d628b393d708451eed6b77ed8cb8df74c6e9343
Becca Hughes <beccahughes@chromium.org>
2018-11-29 18:02:04 +0000

    Reland "[Media Session] [1/4] Move MediaMetadata to service"
    This is a reland of 45474edc506853b20cac7542b8a04c2364837fca [formerly 93ee1837006400a4fde789787d70a7f09905cce8]
    
    This includes a fix to //ash:ash_unittests which was throwing
    a linker error which resulted in the original being reverted.
    
    Original change's description:
    > [Media Session] [1/4] Move MediaMetadata to service
    >
    > This CL moves the MediaMetadata and MediaImage mojo types
    > from Blink to the Media Session service along with their
    > mapped types in content.
    >
    > This is the first of four parts.
    >
    > BUG=875004
    >
    > Change-Id: Ie862229bbeb474a60ccc4d2f82d8c4e1f8017b74
    > Reviewed-on: https://chromium-review.googlesource.com/c/1227401
    > Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#605481}
    
    Bug: 875004
    Change-Id: Id04143a89594a523f323ac16a54d93f1f2447367
    Reviewed-on: https://chromium-review.googlesource.com/c/1318802
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612269}
    Former-commit-id: c185420af301cda24b0a9e1951d04b4de31d601c

---
+++ services/media_session/public/cpp/media_metadata.h
@@ -0,0 +1,81 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_MEDIA_SESSION_PUBLIC_CPP_MEDIA_METADATA_H_
+#define SERVICES_MEDIA_SESSION_PUBLIC_CPP_MEDIA_METADATA_H_
+
+#include <vector>
+
+#include "base/component_export.h"
+#include "base/strings/string16.h"
+#include "build/build_config.h"
+#include "ui/gfx/geometry/size.h"
+#include "url/gurl.h"
+
+#if defined(OS_ANDROID)
+
+#include <jni.h>
+
+#include "base/android/scoped_java_ref.h"
+
+#endif  // defined(OS_ANDROID)
+
+namespace media_session {
+
+// The MediaMetadata is a structure carrying information associated to a
+// MediaSession.
+struct COMPONENT_EXPORT(MEDIA_SESSION_CPP) MediaMetadata {
+  // Structure representing an MediaImage as per the MediaSession API, see:
+  // https://wicg.github.io/mediasession/#dictdef-mediaimage
+  struct COMPONENT_EXPORT(MEDIA_SESSION_CPP) MediaImage {
+    MediaImage();
+    MediaImage(const MediaImage& other);
+    ~MediaImage();
+
+    bool operator==(const MediaImage& other) const;
+
+    // MUST be a valid url. If an icon doesn't have a valid URL, it will not be
+    // successfully parsed, thus will not be represented in the Manifest.
+    GURL src;
+
+    // Empty if the parsing failed or the field was not present. The type can be
+    // any string and doesn't have to be a valid image MIME type at this point.
+    // It is up to the consumer of the object to check if the type matches a
+    // supported type.
+    base::string16 type;
+
+    // Empty if the parsing failed, the field was not present or empty.
+    // The special value "any" is represented by gfx::Size(0, 0).
+    std::vector<gfx::Size> sizes;
+  };
+
+  MediaMetadata();
+  ~MediaMetadata();
+
+  MediaMetadata(const MediaMetadata& other);
+
+  bool operator==(const MediaMetadata& other) const;
+  bool operator!=(const MediaMetadata& other) const;
+
+#if defined(OS_ANDROID)
+  // Creates a Java MediaMetadata instance and returns the JNI ref.
+  base::android::ScopedJavaLocalRef<jobject> CreateJavaObject(JNIEnv* env);
+#endif
+
+  // Title associated to the MediaSession.
+  base::string16 title;
+
+  // Artist associated to the MediaSession.
+  base::string16 artist;
+
+  // Album associated to the MediaSession.
+  base::string16 album;
+
+  // Artwork associated to the MediaSession.
+  std::vector<MediaImage> artwork;
+};
+
+}  // namespace media_session
+
+#endif  // SERVICES_MEDIA_SESSION_PUBLIC_CPP_MEDIA_METADATA_H_
