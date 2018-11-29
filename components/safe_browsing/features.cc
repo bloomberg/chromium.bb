9d263dd7c30ebcf3b9ed1a1d2c663a02dde1a314
Varun Khaneja <vakh@chromium.org>
2018-11-29 21:38:34 +0000

    Features: kBillingInterstitial to ON, drop kInspectDownloadedRarFiles
    R=drubery
    
    Bug: 867518, 750327
    Change-Id: Id6ce3502a99a19767d0056a30cf626bc825ff56d
    Reviewed-on: https://chromium-review.googlesource.com/c/1355134
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612370}
    Former-commit-id: 9ec5fbb46df56910a0e10832457b6dd901477236

--- components/safe_browsing/features.cc
+++ components/safe_browsing/features.cc
@@ -24,7 +24,7 @@ const base::Feature kAdSamplerTriggerFeature{"SafeBrowsingAdSamplerTrigger",
 
 // Controls the billing interstitial UI.
 const base::Feature kBillingInterstitial{"BillingInterstitial",
-                                         base::FEATURE_DISABLED_BY_DEFAULT};
+                                         base::FEATURE_ENABLED_BY_DEFAULT};
 
 // If enabled in pre-network-service world, SafeBrowsing URL checks are done by
 // applying SafeBrowsing's URLLoaderThrottle subclasses to ThrottlingURLLoader.
@@ -38,9 +38,6 @@ const base::Feature kCheckByURLLoaderThrottle{
 
 const base::Feature kForceEnableResetPasswordWebUI{
     "ForceEnableResetPasswordWebUI", base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kInspectDownloadedRarFiles{
-    "InspectDownloadedRarFiles", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kSuspiciousSiteTriggerQuotaFeature{
     "SafeBrowsingSuspiciousSiteTriggerQuota", base::FEATURE_ENABLED_BY_DEFAULT};
@@ -68,7 +65,6 @@ constexpr struct {
     {&kBillingInterstitial, true},
     {&kCheckByURLLoaderThrottle, true},
     {&kForceEnableResetPasswordWebUI, true},
-    {&kInspectDownloadedRarFiles, true},
     {&kSuspiciousSiteTriggerQuotaFeature, true},
     {&kThreatDomDetailsTagAndAttributeFeature, false},
     {&kTriggerThrottlerDailyQuotaFeature, false},
