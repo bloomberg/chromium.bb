3e453b0a2957fcb80c742b1d9196df72efdeb3c5
CJ DiMeglio <lethalantidote@chromium.org>
2018-11-29 12:16:55 +0000

    [Accessibility] Volume slider reads out level in percentages.
    This CL makes it such that the volume level is read out as a percentage
    in aria. This CL also amplifies how the accessibility information is
    given to aria, by setting attributes instead of explicitly creating
    accessibility nodes.
    
    Bug: 905024
    Change-Id: I0ee5a91215669f39c7394d3c8f113ae83a74cf00
    Reviewed-on: https://chromium-review.googlesource.com/c/1337298
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: CJ DiMeglio <lethalantidote@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612160}
    Former-commit-id: cf0717195ed5798c1970f4897682a1517c229e75

--- third_party/blink/renderer/modules/media_controls/elements/media_control_volume_slider_element.cc
+++ third_party/blink/renderer/modules/media_controls/elements/media_control_volume_slider_element.cc
@@ -20,6 +20,9 @@ MediaControlVolumeSliderElement::MediaControlVolumeSliderElement(
     MediaControlsImpl& media_controls)
     : MediaControlSliderElement(media_controls, kMediaVolumeSlider) {
   setAttribute(html_names::kMaxAttr, "1");
+  setAttribute(html_names::kAriaValuemaxAttr, "100");
+  setAttribute(html_names::kAriaValueminAttr, "0");
+  setAttribute(html_names::kAriaLabelAttr, "volume");
   SetShadowPseudoId(AtomicString("-webkit-media-controls-volume-slider"));
   SetVolumeInternal(MediaElement().volume());
 
@@ -104,6 +107,9 @@ void MediaControlVolumeSliderElement::DefaultEventHandler(Event& event) {
 void MediaControlVolumeSliderElement::SetVolumeInternal(double volume) {
   SetupBarSegments();
   SetAfterSegmentPosition(MediaControlSliderElement::Position(0, volume));
+  int percent_vol = 100 * volume;
+  setAttribute(html_names::kAriaValuenowAttr,
+               WTF::AtomicString::Number(percent_vol));
 }
 
 bool MediaControlVolumeSliderElement::KeepEventInNode(
