c38eaca68654c9f0de5f35a0887df663ce1baafe
Xing Liu <xingliu@chromium.org>
2018-11-29 21:30:31 +0000

    Background download: Add trace events to debug jank.
    This CL adds 3 trace event in background download system to debug
    potential jank reporting through crash report.
    
    Bug: 878593
    Change-Id: Ibebca6ba331fb00b5b74a327b900c98e8b516151
    Reviewed-on: https://chromium-review.googlesource.com/c/1354638
    Reviewed-by: ssid <ssid@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612356}
    Former-commit-id: 04d6dc2f9f44c3a6a7b9b466715130cc605fe7a2

--- components/download/internal/background_service/android/battery_status_listener_android.cc
+++ components/download/internal/background_service/android/battery_status_listener_android.cc
@@ -5,6 +5,7 @@
 #include "components/download/internal/background_service/android/battery_status_listener_android.h"
 
 #include "base/android/jni_android.h"
+#include "base/trace_event/trace_event.h"
 #include "jni/BatteryStatusListenerAndroid_jni.h"
 
 namespace download {
@@ -16,6 +17,8 @@ BatteryStatusListenerAndroid::BatteryStatusListenerAndroid(
 BatteryStatusListenerAndroid::~BatteryStatusListenerAndroid() = default;
 
 int BatteryStatusListenerAndroid::GetBatteryPercentageInternal() {
+  TRACE_EVENT0("download_service",
+               "BatteryStatusListenerAndroid.GetBatteryPercentageInternal");
   return Java_BatteryStatusListenerAndroid_getBatteryPercentage(
       base::android::AttachCurrentThread());
 }
