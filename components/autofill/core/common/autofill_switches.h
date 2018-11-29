8cbd8dc9b09c7924b7d6fd6a6df4f5ee32726c4f
Vincent Boisselle <vincb@chromium.org>
2018-11-29 22:25:58 +0000

    Added a command line flag to pass a custom Autofill API key.
    Change-Id: Ic84e3b2fc4126937a4227d20f0acf03a2f8bcfba
    Reviewed-on: https://chromium-review.googlesource.com/c/1355425
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Commit-Queue: Vincent Boisselle <vincb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612407}
    Former-commit-id: ffed3636639da67f7d3203336e4d4e0508f57013

--- components/autofill/core/common/autofill_switches.h
+++ components/autofill/core/common/autofill_switches.h
@@ -12,6 +12,7 @@ namespace switches {
 
 // All switches in alphabetical order. The switches should be documented
 // alongside the definition of their values in the .cc file.
+extern const char kAutofillAPIKey[];
 extern const char kAutofillServerURL[];
 extern const char kAutofillMetadataUploadEncoding[];
 extern const char kDisableOfferStoreUnmaskedWalletCards[];
