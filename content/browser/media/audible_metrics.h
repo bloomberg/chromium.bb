cb91c1def3711903341a3ca12a05e3eb7441f103
Becca Hughes <beccahughes@chromium.org>
2018-11-29 04:06:16 +0000

    [Audible Metrics] Record audible tab closure
    Add a histogram to record when we close an audible
    tab whether it was the newest.
    
    BUG=907671
    
    Change-Id: Ie607223ee94b011389c1c7e3146c779f9cfce47c
    Reviewed-on: https://chromium-review.googlesource.com/c/1347492
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612029}
    Former-commit-id: 6acad712f95a40d12ffc83891ad29870c1d42947

--- content/browser/media/audible_metrics.h
+++ content/browser/media/audible_metrics.h
@@ -5,6 +5,7 @@
 #ifndef CONTENT_BROWSER_MEDIA_AUDIBLE_METRICS_H_
 #define CONTENT_BROWSER_MEDIA_AUDIBLE_METRICS_H_
 
+#include <list>
 #include <memory>
 #include <set>
 
@@ -16,18 +17,30 @@ namespace content {
 class WebContents;
 
 // This class handles metrics regarding audible WebContents.
-// It does register three different information:
+// It records four different histograms:
 // - how many WebContents are audible when a WebContents become audible.
 // - how long multiple WebContents are audible at the same time.
 // - for a browsing session, how often and how many WebContents get audible at
 //   the same time.
+// - if an audible tab was closed and we only have one audible tab open anymore
+//   then we should record whether we closed the newest or oldest tab.
 class CONTENT_EXPORT AudibleMetrics {
  public:
+  // This is used for recording whether the audible tab that exited concurrent
+  // playback was the most recent. It is used for recording a histogram so
+  // values should not be changed.
+  enum class ExitConcurrentPlaybackContents {
+    kMostRecent = 0,
+    kOlder = 1,
+    kMaxValue = kOlder,
+  };
+
   AudibleMetrics();
   ~AudibleMetrics();
 
   void UpdateAudibleWebContentsState(const WebContents* web_contents,
                                      bool audible);
+  void WebContentsDestroyed(const WebContents* web_contents);
 
   void SetClockForTest(const base::TickClock* test_clock);
 
@@ -42,6 +55,9 @@ class CONTENT_EXPORT AudibleMetrics {
   base::TimeTicks concurrent_web_contents_start_time_;
   size_t max_concurrent_audible_web_contents_in_session_;
   const base::TickClock* clock_;
+
+  // This stores the audible web contents in insertion order.
+  std::list<const WebContents*> last_audible_web_contents_;
 
   std::set<const WebContents*> audible_web_contents_;
 
