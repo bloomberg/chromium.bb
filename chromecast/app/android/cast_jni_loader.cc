ba69ed0dbbfeaf175109285a2ae80d39a7e2746c
Alex Leung <alexleung@google.com>
2018-11-29 20:09:55 +0000

    Revert "Android: Switch cast_shell to use implicit JNI registration"
    This reverts commit 9e5cad40ef18620a4ac92fee311e682f8d195e68 [formerly f1854090df71901e50c22e793daa0ffc91b6ae6e].
    
    Reason for revert: Causes internal b/120143260.
    
    Change-Id: I83286ee7cb9d5e96e63be66bc47efda802421232
    Reviewed-on: https://chromium-review.googlesource.com/c/1354326
    Commit-Queue: Alex Leung <alexleung@google.com>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612333}
    Former-commit-id: 78a36aa47b33d4b572fcf45c1cfc14fb74ad3f40

--- chromecast/app/android/cast_jni_loader.cc
+++ chromecast/app/android/cast_jni_loader.cc
@@ -3,6 +3,9 @@
 // found in the LICENSE file.
 
 #include "base/android/jni_android.h"
+#include "base/android/library_loader/library_loader_hooks.h"
+#include "base/bind.h"
+#include "chromecast/android/cast_shell_jni_registration.h"
 #include "chromecast/app/cast_main_delegate.h"
 #include "content/public/app/content_jni_onload.h"
 #include "content/public/app/content_main.h"
@@ -11,6 +14,10 @@
 // This is called by the VM when the shared library is first loaded.
 JNI_EXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
   base::android::InitVM(vm);
+  JNIEnv* env = base::android::AttachCurrentThread();
+  if (!RegisterMainDexNatives(env) || !RegisterNonMainDexNatives(env)) {
+    return -1;
+  }
 
   if (!content::android::OnJNIOnLoadInit())
     return false;
