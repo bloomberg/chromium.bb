74d4ca50aa2f1ae69378244d70e02a0f990c46bc
Joe Mason <joenotcharles@google.com>
2018-11-29 22:34:22 +0000

    Load the Chrome Cleaner partner logo SVG with <embed>.
    The logo now has a title attr in it for screen readers. But when its
    set as the background of a <span>, or as an <img> src, screen readers
    don't look at the tags inside. For the screen reader to view the tag,
    it seems the <svg> element must be part of the DOM tree, such as when
    it's added with <embed>.
    
    Bug: 767130
    Change-Id: I5d28030fc90c269aa6a13a605f24c698915f6c5d
    Reviewed-on: https://chromium-review.googlesource.com/c/1352822
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/master@{#612409}
    Former-commit-id: 3d20a937aa2b6e7ae8bb06d4e55cdcfcf9fc5e3d

--- chrome/browser/ui/webui/settings/md_settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/md_settings_localized_strings_provider.cc
@@ -983,9 +983,13 @@ void AddChromeCleanupStrings(content::WebUIDataSource* html_source) {
           .spec();
   html_source->AddString("chromeCleanupLearnMoreUrl", cleanup_learn_more_url);
 
-  const base::string16 powered_by_html =
-      l10n_util::GetStringFUTF16(IDS_SETTINGS_RESET_CLEANUP_FOOTER_POWERED_BY,
-                                 L"<span id='powered-by-logo'></span>");
+  // The "powered by" footer contains an HTML fragment with the SVG logo of the partner. The logo
+  // is added to the DOM using <embed>, rather than as an <img> src, to make sure that screen
+  // readers can find accessibility tags inside the svg.
+  const base::string16 powered_by_html = l10n_util::GetStringFUTF16(
+      IDS_SETTINGS_RESET_CLEANUP_FOOTER_POWERED_BY,
+      L"<embed type='image/svg+xml' id='powered-by-logo' "
+      L"src='chrome://settings/partner-logo.svg'></embed>");
   html_source->AddString("chromeCleanupPoweredByHtml", powered_by_html);
 
   const base::string16 cleanup_details_explanation =
