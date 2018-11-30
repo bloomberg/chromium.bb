41c669c22a892202714c07a00cb1b340ce61a20a
Greg Kerr <kerrnel@chromium.org>
2018-11-30 21:41:12 +0000

    Reland "macOS Sandbox: Port fontloading test to V2 sandbox."
    This is a reland of a5e5ad7b1dd0f7d2f6404979d34541b0a87d25b1 [formerly 1d979043f0a30d35f269fa60417d5f2f50e6b657]
    
    Original change's description:
    > macOS Sandbox: Port fontloading test to V2 sandbox.
    > 
    > This removes all of the obsolete unit testing code from the V1 sandbox.
    > 
    > Bug: 902597
    > Change-Id: I426e3b815e0bf23036815750aab350adbb22e068
    > Reviewed-on: https://chromium-review.googlesource.com/c/1327527
    > Commit-Queue: Greg Kerr <kerrnel@chromium.org>
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#612416}
    
    Bug: 902597
    Change-Id: I2088dd954956be6d704add7ee8e07bc629df8eb9
    Reviewed-on: https://chromium-review.googlesource.com/c/1357217
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Greg Kerr <kerrnel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612797}
    Former-commit-id: 95d0a57ff661b75917ba2d5706fee99222db7bda

--- content/browser/sandbox_mac_unittest.mm
+++ content/browser/sandbox_mac_unittest.mm
@@ -8,14 +8,20 @@
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/command_line.h"
+#include "base/files/file_util.h"
+#include "base/files/scoped_file.h"
+#include "base/mac/scoped_cftyperef.h"
 #include "base/memory/ref_counted.h"
+#include "base/memory/shared_memory.h"
 #include "base/posix/eintr_wrapper.h"
 #include "base/process/kill.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/sys_string_conversions.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/test/multiprocess_test.h"
 #include "base/test/test_timeouts.h"
 #include "content/browser/sandbox_parameters_mac.h"
+#include "content/common/mac/font_loader.h"
 #include "crypto/openssl_util.h"
 #include "sandbox/mac/seatbelt.h"
 #include "sandbox/mac/seatbelt_exec.h"
@@ -41,7 +47,7 @@ namespace {
 // and is safe since this is only a unit test.
 constexpr char kTempDirSuffix[] =
     "(allow file* (subpath \"/private/var/folders\"))";
-constexpr char kClipboardArg[] = "pasteboard-name";
+constexpr char kExtraDataArg[] = "extra-data";
 
 class SandboxMacTest : public base::MultiProcessTest {
  protected:
@@ -49,6 +55,9 @@ class SandboxMacTest : public base::MultiProcessTest {
     base::CommandLine cl = MultiProcessTest::MakeCmdLine(procname);
     cl.AppendArg(
         base::StringPrintf("%s%d", sandbox::switches::kSeatbeltClient, pipe_));
+    if (!extra_data_.empty()) {
+      cl.AppendSwitchASCII(kExtraDataArg, extra_data_);
+    }
     return cl;
   }
 
@@ -150,17 +159,7 @@ class SandboxMacTest : public base::MultiProcessTest {
   }
 
   int pipe_{0};
-};
-
-class SandboxMacClipboardTest : public SandboxMacTest {
- protected:
-  base::CommandLine MakeCmdLine(const std::string& procname) override {
-    base::CommandLine cl = SandboxMacTest::MakeCmdLine(procname);
-    cl.AppendSwitchASCII(kClipboardArg, pasteboard_name_);
-    return cl;
-  }
-
-  std::string pasteboard_name_{};
+  std::string extra_data_{};
 };
 
 void CheckCreateSeatbeltServer() {
@@ -176,6 +175,11 @@ void CheckCreateSeatbeltServer() {
   CHECK(result.sandbox_required);
   CHECK(result.server);
   CHECK(result.server->InitializeSandbox());
+}
+
+std::string GetExtraDataValue() {
+  base::CommandLine* cl = base::CommandLine::ForCurrentProcess();
+  return cl->GetSwitchValueASCII(kExtraDataArg);
 }
 
 }  // namespace
@@ -201,8 +205,7 @@ TEST_F(SandboxMacTest, RendererCannotWriteHomeDir) {
 MULTIPROCESS_TEST_MAIN(ClipboardAccessProcess) {
   CheckCreateSeatbeltServer();
 
-  base::CommandLine* cl = base::CommandLine::ForCurrentProcess();
-  std::string pasteboard_name = cl->GetSwitchValueASCII(kClipboardArg);
+  std::string pasteboard_name = GetExtraDataValue();
   CHECK(!pasteboard_name.empty());
   CHECK([NSPasteboard pasteboardWithName:base::SysUTF8ToNSString(
                                              pasteboard_name)] == nil);
@@ -211,12 +214,12 @@ MULTIPROCESS_TEST_MAIN(ClipboardAccessProcess) {
   return 0;
 }
 
-TEST_F(SandboxMacClipboardTest, ClipboardAccess) {
+TEST_F(SandboxMacTest, ClipboardAccess) {
   scoped_refptr<ui::UniquePasteboard> pb = new ui::UniquePasteboard;
   ASSERT_TRUE(pb->get());
   EXPECT_EQ([[pb->get() types] count], 0U);
 
-  pasteboard_name_ = base::SysNSStringToUTF8([pb->get() name]);
+  extra_data_ = base::SysNSStringToUTF8([pb->get() name]);
 
   ExecuteInAllSandboxTypes("ClipboardAccessProcess",
                            base::BindRepeating(
@@ -238,6 +241,76 @@ MULTIPROCESS_TEST_MAIN(SSLProcess) {
 
 TEST_F(SandboxMacTest, SSLInitTest) {
   ExecuteInAllSandboxTypes("SSLProcess", base::RepeatingClosure());
+}
+
+MULTIPROCESS_TEST_MAIN(FontLoadingProcess) {
+  // Create a shared memory handle to mimic what the browser process does.
+  std::string font_file_path = GetExtraDataValue();
+  CHECK(!font_file_path.empty());
+
+  std::string font_data;
+  CHECK(base::ReadFileToString(base::FilePath(font_file_path), &font_data));
+
+  size_t font_data_length = font_data.length();
+  CHECK(font_data_length > 0);
+
+  auto font_shmem = mojo::SharedBufferHandle::Create(font_data_length);
+  CHECK(font_shmem.is_valid());
+
+  mojo::ScopedSharedBufferMapping mapping = font_shmem->Map(font_data_length);
+  CHECK(mapping);
+
+  memcpy(mapping.get(), font_data.c_str(), font_data_length);
+
+  // Now init the sandbox.
+  CheckCreateSeatbeltServer();
+
+  mojo::ScopedSharedBufferHandle shmem_handle =
+      font_shmem->Clone(mojo::SharedBufferHandle::AccessMode::READ_ONLY);
+  CHECK(shmem_handle.is_valid());
+
+  base::ScopedCFTypeRef<CGFontRef> cgfont;
+  CHECK(FontLoader::CGFontRefFromBuffer(
+      std::move(shmem_handle), font_data_length, cgfont.InitializeInto()));
+  CHECK(cgfont);
+
+  base::ScopedCFTypeRef<CTFontRef> ctfont(
+      CTFontCreateWithGraphicsFont(cgfont.get(), 16.0, NULL, NULL));
+  CHECK(ctfont);
+
+  // Do something with the font to make sure it's loaded.
+  CGFloat cap_height = CTFontGetCapHeight(ctfont);
+  CHECK(cap_height > 0.0);
+
+  return 0;
+}
+
+TEST_F(SandboxMacTest, FontLoadingTest) {
+  base::FilePath temp_file_path;
+  FILE* temp_file = base::CreateAndOpenTemporaryFile(&temp_file_path);
+  ASSERT_TRUE(temp_file);
+  base::ScopedFILE temp_file_closer(temp_file);
+
+  std::unique_ptr<FontLoader::ResultInternal> result =
+      FontLoader::LoadFontForTesting(base::ASCIIToUTF16("Geeza Pro"), 16);
+  ASSERT_TRUE(result);
+  ASSERT_TRUE(result->font_data.is_valid());
+  uint64_t font_data_size = result->font_data->GetSize();
+  EXPECT_GT(font_data_size, 0U);
+  EXPECT_GT(result->font_id, 0U);
+
+  mojo::ScopedSharedBufferMapping mapping =
+      result->font_data->Map(font_data_size);
+  ASSERT_TRUE(mapping);
+
+  base::WriteFileDescriptor(fileno(temp_file),
+                            static_cast<const char*>(mapping.get()),
+                            font_data_size);
+
+  extra_data_ = temp_file_path.value();
+  ExecuteInRendererSandbox("FontLoadingProcess");
+  temp_file_closer.reset();
+  ASSERT_TRUE(base::DeleteFile(temp_file_path, false));
 }
 
 }  // namespace content
