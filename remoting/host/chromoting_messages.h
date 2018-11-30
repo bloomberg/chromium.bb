af127a5aee1ec5de4137f6e9cc1697978c0d0588
Erik Jensen <rkjnsn@chromium.org>
2018-11-30 22:51:10 +0000

    Switch chromoting_messages.h to new variadic macros.
    The IPC_MESSAGE_CONTROL[0-5] macros have been deprecated in favor of a
    new variadic IPC_MESSAGE_CONTROL macro.
    
    Change-Id: I64a0e8c7ec3c51b387de615950f93b6d5798d3a6
    Reviewed-on: https://chromium-review.googlesource.com/c/1354102
    Commit-Queue: Erik Jensen <rkjnsn@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612828}
    Former-commit-id: 6d6de592c0651e860949877b5bc1607dca13c773

--- remoting/host/chromoting_messages.h
+++ remoting/host/chromoting_messages.h
@@ -35,56 +35,56 @@
 // sends this message when a fatal error has been detected indicating that
 // the receiving process misbehaves. The daemon passes the location of the code
 // that detected the error.
-IPC_MESSAGE_CONTROL3(ChromotingDaemonMsg_Crash,
-                     std::string /* function_name */,
-                     std::string /* file_name */,
-                     int /* line_number */)
+IPC_MESSAGE_CONTROL(ChromotingDaemonMsg_Crash,
+                    std::string /* function_name */,
+                    std::string /* file_name */,
+                    int /* line_number */)
 
 //-----------------------------------------------------------------------------
 // Chromoting messages sent from the daemon to the network process.
 
 // Delivers the host configuration (and updates) to the network process.
-IPC_MESSAGE_CONTROL1(ChromotingDaemonNetworkMsg_Configuration, std::string)
+IPC_MESSAGE_CONTROL(ChromotingDaemonNetworkMsg_Configuration, std::string)
 
 // Initializes the pairing registry on Windows. The passed key handles are
 // already duplicated by the sender.
-IPC_MESSAGE_CONTROL2(ChromotingDaemonNetworkMsg_InitializePairingRegistry,
-                     IPC::PlatformFileForTransit /* privileged_key */,
-                     IPC::PlatformFileForTransit /* unprivileged_key */)
+IPC_MESSAGE_CONTROL(ChromotingDaemonNetworkMsg_InitializePairingRegistry,
+                    IPC::PlatformFileForTransit /* privileged_key */,
+                    IPC::PlatformFileForTransit /* unprivileged_key */)
 
 // Notifies the network process that the terminal |terminal_id| has been
 // disconnected from the desktop session.
-IPC_MESSAGE_CONTROL1(ChromotingDaemonNetworkMsg_TerminalDisconnected,
-                     int /* terminal_id */)
+IPC_MESSAGE_CONTROL(ChromotingDaemonNetworkMsg_TerminalDisconnected,
+                    int /* terminal_id */)
 
 // Notifies the network process that |terminal_id| is now attached to
 // a desktop integration process. |session_id| is the id of the desktop session
 // being attached. |desktop_pipe| is the client end of the desktop-to-network
 // pipe opened.
-IPC_MESSAGE_CONTROL3(ChromotingDaemonNetworkMsg_DesktopAttached,
-                     int /* terminal_id */,
-                     int /* session_id */,
-                     IPC::ChannelHandle /* desktop_pipe */)
+IPC_MESSAGE_CONTROL(ChromotingDaemonNetworkMsg_DesktopAttached,
+                    int /* terminal_id */,
+                    int /* session_id */,
+                    IPC::ChannelHandle /* desktop_pipe */)
 
 //-----------------------------------------------------------------------------
 // Chromoting messages sent from the network to the daemon process.
 
 // Connects the terminal |terminal_id| (i.e. a remote client) to a desktop
 // session.
-IPC_MESSAGE_CONTROL3(ChromotingNetworkHostMsg_ConnectTerminal,
-                     int /* terminal_id */,
-                     remoting::ScreenResolution /* resolution */,
-                     bool /* virtual_terminal */)
+IPC_MESSAGE_CONTROL(ChromotingNetworkHostMsg_ConnectTerminal,
+                    int /* terminal_id */,
+                    remoting::ScreenResolution /* resolution */,
+                    bool /* virtual_terminal */)
 
 // Disconnects the terminal |terminal_id| from the desktop session it was
 // connected to.
-IPC_MESSAGE_CONTROL1(ChromotingNetworkHostMsg_DisconnectTerminal,
-                     int /* terminal_id */)
+IPC_MESSAGE_CONTROL(ChromotingNetworkHostMsg_DisconnectTerminal,
+                    int /* terminal_id */)
 
 // Changes the screen resolution in the given desktop session.
-IPC_MESSAGE_CONTROL2(ChromotingNetworkDaemonMsg_SetScreenResolution,
-                     int /* terminal_id */,
-                     remoting::ScreenResolution /* resolution */)
+IPC_MESSAGE_CONTROL(ChromotingNetworkDaemonMsg_SetScreenResolution,
+                    int /* terminal_id */,
+                    remoting::ScreenResolution /* resolution */)
 
 // Serialized remoting::protocol::TransportRoute structure.
 IPC_STRUCT_BEGIN(SerializedTransportRoute)
@@ -100,27 +100,27 @@ IPC_ENUM_TRAITS_MAX_VALUE(remoting::protocol::TransportRoute::RouteType,
 
 // Hosts status notifications (see HostStatusObserver interface) sent by
 // IpcHostEventLogger.
-IPC_MESSAGE_CONTROL1(ChromotingNetworkDaemonMsg_AccessDenied,
-                     std::string /* jid */)
+IPC_MESSAGE_CONTROL(ChromotingNetworkDaemonMsg_AccessDenied,
+                    std::string /* jid */)
 
-IPC_MESSAGE_CONTROL1(ChromotingNetworkDaemonMsg_ClientAuthenticated,
-                     std::string /* jid */)
+IPC_MESSAGE_CONTROL(ChromotingNetworkDaemonMsg_ClientAuthenticated,
+                    std::string /* jid */)
 
-IPC_MESSAGE_CONTROL1(ChromotingNetworkDaemonMsg_ClientConnected,
-                     std::string /* jid */)
+IPC_MESSAGE_CONTROL(ChromotingNetworkDaemonMsg_ClientConnected,
+                    std::string /* jid */)
 
-IPC_MESSAGE_CONTROL1(ChromotingNetworkDaemonMsg_ClientDisconnected,
-                     std::string /* jid */)
+IPC_MESSAGE_CONTROL(ChromotingNetworkDaemonMsg_ClientDisconnected,
+                    std::string /* jid */)
 
-IPC_MESSAGE_CONTROL3(ChromotingNetworkDaemonMsg_ClientRouteChange,
-                     std::string /* jid */,
-                     std::string /* channel_name */,
-                     SerializedTransportRoute /* route */)
+IPC_MESSAGE_CONTROL(ChromotingNetworkDaemonMsg_ClientRouteChange,
+                    std::string /* jid */,
+                    std::string /* channel_name */,
+                    SerializedTransportRoute /* route */)
 
-IPC_MESSAGE_CONTROL1(ChromotingNetworkDaemonMsg_HostStarted,
-                     std::string /* xmpp_login */)
+IPC_MESSAGE_CONTROL(ChromotingNetworkDaemonMsg_HostStarted,
+                    std::string /* xmpp_login */)
 
-IPC_MESSAGE_CONTROL0(ChromotingNetworkDaemonMsg_HostShutdown)
+IPC_MESSAGE_CONTROL(ChromotingNetworkDaemonMsg_HostShutdown)
 
 //-----------------------------------------------------------------------------
 // Chromoting messages sent from the desktop to the daemon process.
@@ -128,25 +128,25 @@ IPC_MESSAGE_CONTROL0(ChromotingNetworkDaemonMsg_HostShutdown)
 // Notifies the daemon that a desktop integration process has been initialized.
 // |desktop_pipe| specifies the client end of the desktop pipe. It is to be
 // forwarded to the desktop environment stub.
-IPC_MESSAGE_CONTROL1(ChromotingDesktopDaemonMsg_DesktopAttached,
-                     IPC::ChannelHandle /* desktop_pipe */)
+IPC_MESSAGE_CONTROL(ChromotingDesktopDaemonMsg_DesktopAttached,
+                    IPC::ChannelHandle /* desktop_pipe */)
 
 // Asks the daemon to inject Secure Attention Sequence (SAS) in the session
 // where the desktop process is running.
-IPC_MESSAGE_CONTROL0(ChromotingDesktopDaemonMsg_InjectSas)
+IPC_MESSAGE_CONTROL(ChromotingDesktopDaemonMsg_InjectSas)
 
 //-----------------------------------------------------------------------------
 // Chromoting messages sent from the desktop to the network process.
 
 // Notifies the network process that a shared buffer has been created.
-IPC_MESSAGE_CONTROL3(ChromotingDesktopNetworkMsg_CreateSharedBuffer,
-                     int /* id */,
-                     base::SharedMemoryHandle /* handle */,
-                     uint32_t /* size */)
+IPC_MESSAGE_CONTROL(ChromotingDesktopNetworkMsg_CreateSharedBuffer,
+                    int /* id */,
+                    base::SharedMemoryHandle /* handle */,
+                    uint32_t /* size */)
 
 // Request the network process to stop using a shared buffer.
-IPC_MESSAGE_CONTROL1(ChromotingDesktopNetworkMsg_ReleaseSharedBuffer,
-                     int /* id */)
+IPC_MESSAGE_CONTROL(ChromotingDesktopNetworkMsg_ReleaseSharedBuffer,
+                    int /* id */)
 
 // Serialized webrtc::DesktopFrame.
 IPC_STRUCT_BEGIN(SerializedDesktopFrame)
@@ -179,79 +179,79 @@ IPC_ENUM_TRAITS_MAX_VALUE(webrtc::DesktopCapturer::Result,
                           webrtc::DesktopCapturer::Result::MAX_VALUE)
 
 // Notifies the network process that a shared buffer has been created.
-IPC_MESSAGE_CONTROL2(ChromotingDesktopNetworkMsg_CaptureResult,
-                     webrtc::DesktopCapturer::Result /* result */,
-                     SerializedDesktopFrame /* frame */)
+IPC_MESSAGE_CONTROL(ChromotingDesktopNetworkMsg_CaptureResult,
+                    webrtc::DesktopCapturer::Result /* result */,
+                    SerializedDesktopFrame /* frame */)
 
 // Notifies the network process that the host display layout has changed.
-IPC_MESSAGE_CONTROL1(ChromotingDesktopNetworkMsg_DisplayChanged,
-                     remoting::protocol::VideoLayout /* layout */)
+IPC_MESSAGE_CONTROL(ChromotingDesktopNetworkMsg_DisplayChanged,
+                    remoting::protocol::VideoLayout /* layout */)
 
 // Carries a cursor share update from the desktop session agent to the client.
-IPC_MESSAGE_CONTROL1(ChromotingDesktopNetworkMsg_MouseCursor,
-                     webrtc::MouseCursor /* cursor */ )
+IPC_MESSAGE_CONTROL(ChromotingDesktopNetworkMsg_MouseCursor,
+                    webrtc::MouseCursor /* cursor */)
 
 // Carries a clipboard event from the desktop session agent to the client.
 // |serialized_event| is a serialized protocol::ClipboardEvent.
-IPC_MESSAGE_CONTROL1(ChromotingDesktopNetworkMsg_InjectClipboardEvent,
-                     std::string /* serialized_event */ )
+IPC_MESSAGE_CONTROL(ChromotingDesktopNetworkMsg_InjectClipboardEvent,
+                    std::string /* serialized_event */)
 
 IPC_ENUM_TRAITS_MAX_VALUE(remoting::protocol::ErrorCode,
                           remoting::protocol::ERROR_CODE_MAX)
 
 // Requests the network process to terminate the client session.
-IPC_MESSAGE_CONTROL1(ChromotingDesktopNetworkMsg_DisconnectSession,
-                     remoting::protocol::ErrorCode /* error */)
+IPC_MESSAGE_CONTROL(ChromotingDesktopNetworkMsg_DisconnectSession,
+                    remoting::protocol::ErrorCode /* error */)
 
 // Carries an audio packet from the desktop session agent to the client.
 // |serialized_packet| is a serialized AudioPacket.
-IPC_MESSAGE_CONTROL1(ChromotingDesktopNetworkMsg_AudioPacket,
-                     std::string /* serialized_packet */ )
+IPC_MESSAGE_CONTROL(ChromotingDesktopNetworkMsg_AudioPacket,
+                    std::string /* serialized_packet */)
 
 //-----------------------------------------------------------------------------
 // Chromoting messages sent from the network to the desktop process.
 
 // Passes the client session data to the desktop session agent and starts it.
 // This must be the first message received from the host.
-IPC_MESSAGE_CONTROL3(ChromotingNetworkDesktopMsg_StartSessionAgent,
-                     std::string /* authenticated_jid */,
-                     remoting::ScreenResolution /* resolution */,
-                     remoting::DesktopEnvironmentOptions /* options */)
+IPC_MESSAGE_CONTROL(ChromotingNetworkDesktopMsg_StartSessionAgent,
+                    std::string /* authenticated_jid */,
+                    remoting::ScreenResolution /* resolution */,
+                    remoting::DesktopEnvironmentOptions /* options */)
 
-IPC_MESSAGE_CONTROL0(ChromotingNetworkDesktopMsg_CaptureFrame)
+IPC_MESSAGE_CONTROL(ChromotingNetworkDesktopMsg_CaptureFrame)
 
 // Carries a clipboard event from the client to the desktop session agent.
 // |serialized_event| is a serialized protocol::ClipboardEvent.
-IPC_MESSAGE_CONTROL1(ChromotingNetworkDesktopMsg_InjectClipboardEvent,
-                     std::string /* serialized_event */ )
+IPC_MESSAGE_CONTROL(ChromotingNetworkDesktopMsg_InjectClipboardEvent,
+                    std::string /* serialized_event */)
 
 // Carries a keyboard event from the client to the desktop session agent.
 // |serialized_event| is a serialized protocol::KeyEvent.
-IPC_MESSAGE_CONTROL1(ChromotingNetworkDesktopMsg_InjectKeyEvent,
-                     std::string /* serialized_event */ )
+IPC_MESSAGE_CONTROL(ChromotingNetworkDesktopMsg_InjectKeyEvent,
+                    std::string /* serialized_event */)
 
 // Carries a keyboard event from the client to the desktop session agent.
 // |serialized_event| is a serialized protocol::TextEvent.
-IPC_MESSAGE_CONTROL1(ChromotingNetworkDesktopMsg_InjectTextEvent,
-                     std::string /* serialized_event */ )
+IPC_MESSAGE_CONTROL(ChromotingNetworkDesktopMsg_InjectTextEvent,
+                    std::string /* serialized_event */)
 
 // Carries a mouse event from the client to the desktop session agent.
 // |serialized_event| is a serialized protocol::MouseEvent.
-IPC_MESSAGE_CONTROL1(ChromotingNetworkDesktopMsg_InjectMouseEvent,
-                     std::string /* serialized_event */ )
+IPC_MESSAGE_CONTROL(ChromotingNetworkDesktopMsg_InjectMouseEvent,
+                    std::string /* serialized_event */)
 
 // Carries a touch event from the client to the desktop session agent.
 // |serialized_event| is a serialized protocol::TouchEvent.
-IPC_MESSAGE_CONTROL1(ChromotingNetworkDesktopMsg_InjectTouchEvent,
-                     std::string /* serialized_event */ )
+IPC_MESSAGE_CONTROL(ChromotingNetworkDesktopMsg_InjectTouchEvent,
+                    std::string /* serialized_event */)
 
 // Changes the screen resolution in the desktop session.
-IPC_MESSAGE_CONTROL1(ChromotingNetworkDesktopMsg_SetScreenResolution,
-                     remoting::ScreenResolution /* resolution */)
+IPC_MESSAGE_CONTROL(ChromotingNetworkDesktopMsg_SetScreenResolution,
+                    remoting::ScreenResolution /* resolution */)
 
 // Carries an action request event from the client to the desktop session agent.
-IPC_MESSAGE_CONTROL1(ChromotingNetworkDesktopMsg_ExecuteActionRequest,
-                     remoting::protocol::ActionRequest /* request */)
+IPC_MESSAGE_CONTROL(ChromotingNetworkDesktopMsg_ExecuteActionRequest,
+                    remoting::protocol::ActionRequest /* request */)
 
 //---------------------------------------------------------------------
 // Chromoting messages sent from the remote_security_key process to the
@@ -259,31 +259,31 @@ IPC_MESSAGE_CONTROL1(ChromotingNetworkDesktopMsg_ExecuteActionRequest,
 
 // The array of bytes representing a security key request to be sent to the
 // remote client.
-IPC_MESSAGE_CONTROL1(ChromotingRemoteSecurityKeyToNetworkMsg_Request,
-                     std::string /* request bytes */)
+IPC_MESSAGE_CONTROL(ChromotingRemoteSecurityKeyToNetworkMsg_Request,
+                    std::string /* request bytes */)
 
 //---------------------------------------------------------
 // Chromoting messages sent from the network process to the remote_security_key
 // process.
 
 // The array of bytes representing the security key response from the client.
-IPC_MESSAGE_CONTROL1(ChromotingNetworkToRemoteSecurityKeyMsg_Response,
-                     std::string /* response bytes */)
+IPC_MESSAGE_CONTROL(ChromotingNetworkToRemoteSecurityKeyMsg_Response,
+                    std::string /* response bytes */)
 
 // Indicates the channel used for security key message passing is ready for use.
-IPC_MESSAGE_CONTROL0(ChromotingNetworkToRemoteSecurityKeyMsg_ConnectionReady)
+IPC_MESSAGE_CONTROL(ChromotingNetworkToRemoteSecurityKeyMsg_ConnectionReady)
 
 // Error indicating the request originated from outside the remoted session.
 // The IPC channel will be disconnected after this message has been sent.
-IPC_MESSAGE_CONTROL0(ChromotingNetworkToRemoteSecurityKeyMsg_InvalidSession)
+IPC_MESSAGE_CONTROL(ChromotingNetworkToRemoteSecurityKeyMsg_InvalidSession)
 
 // Starts to report process resource usage.
-IPC_MESSAGE_CONTROL1(ChromotingNetworkToAnyMsg_StartProcessStatsReport,
-                     base::TimeDelta /* interval */)
+IPC_MESSAGE_CONTROL(ChromotingNetworkToAnyMsg_StartProcessStatsReport,
+                    base::TimeDelta /* interval */)
 
 // Stops to report process resource usage.
-IPC_MESSAGE_CONTROL0(ChromotingNetworkToAnyMsg_StopProcessStatsReport)
+IPC_MESSAGE_CONTROL(ChromotingNetworkToAnyMsg_StopProcessStatsReport)
 
 // Reports process resource usage to network process.
-IPC_MESSAGE_CONTROL1(ChromotingAnyToNetworkMsg_ReportProcessStats,
-                     remoting::protocol::AggregatedProcessResourceUsage)
+IPC_MESSAGE_CONTROL(ChromotingAnyToNetworkMsg_ReportProcessStats,
+                    remoting::protocol::AggregatedProcessResourceUsage)
