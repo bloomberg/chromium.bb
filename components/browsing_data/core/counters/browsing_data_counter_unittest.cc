62de26dadab0fbd7ce3e770834df2ad89e01f0c5
Carlos Caballero <carlscab@google.com>
2018-11-30 14:19:01 +0000

    [scheduling] Use ScopedTaskEnvironment instead of MessageLoop in browsing_data
    MessageLoop will go away, eventually.
    
    BUG=891670
    
    Change-Id: I5911ca044c8632a77ed851a148892db34e8ed7c6
    Reviewed-on: https://chromium-review.googlesource.com/c/1352416
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#612639}
    Former-commit-id: 1bcce9b26597a42455b6be507f6361bda01e90e7

--- components/browsing_data/core/counters/browsing_data_counter_unittest.cc
+++ components/browsing_data/core/counters/browsing_data_counter_unittest.cc
@@ -8,8 +8,8 @@
 #include <vector>
 
 #include "base/bind_helpers.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "components/browsing_data/core/pref_names.h"
@@ -103,7 +103,7 @@ class BrowsingDataCounterTest : public testing::Test {
  private:
   std::unique_ptr<TestingPrefServiceSimple> pref_service_;
   std::unique_ptr<MockBrowsingDataCounter> counter_;
-  base::MessageLoop loop_;
+  base::test::ScopedTaskEnvironment task_environment_;
 };
 
 TEST_F(BrowsingDataCounterTest, NoResponse) {
