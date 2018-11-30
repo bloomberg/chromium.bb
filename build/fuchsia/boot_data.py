d2a09665b175a716f86f8e01a21809272d664c6a
Kevin Marshall <kmarshall@chromium.org>
2018-11-30 23:29:19 +0000

    Fuchsia: Generate SSH credentials for device boots.
    This fixes a bug in the runner scripts which prevented SSH cred
    generation for device boots, but allowed emulator run credentials to
    be re-used. The issue only manifested on clean builds with no prior
    emu runs.
    
    Bug: 899285
    Change-Id: Icfb2b734bf527b44cb671b08ee7d21563ae82a8c
    Reviewed-on: https://chromium-review.googlesource.com/c/1355850
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612839}
    Former-commit-id: 22d1b7117a6ee9a257d06a699ad257e3bf41be57

--- build/fuchsia/boot_data.py
+++ build/fuchsia/boot_data.py
@@ -40,14 +40,19 @@ def _TargetCpuToSdkBinPath(target_arch):
   return os.path.join(common.SDK_ROOT, 'target', target_arch)
 
 
-def _ProvisionSSH(output_dir):
-  """Generates a keypair and configuration data for the SSH client.
-  Returns a path to the client public key."""
+def _GetPubKeyPath(output_dir):
+  """Returns a path to the generated SSH public key."""
+
+  return os.path.join(output_dir, 'id_ed25519.pub')
+
+
+def ProvisionSSH(output_dir):
+  """Generates a keypair and config file for SSH."""
 
   host_key_path = os.path.join(output_dir, 'ssh_key')
   host_pubkey_path = host_key_path + '.pub'
   id_key_path = os.path.join(output_dir, 'id_ed25519')
-  id_pubkey_path = id_key_path + '.pub'
+  id_pubkey_path = _GetPubKeyPath(output_dir)
   known_hosts_path = os.path.join(output_dir, 'known_hosts')
   ssh_config_path = os.path.join(output_dir, 'ssh_config')
 
@@ -67,8 +72,6 @@ def _ProvisionSSH(output_dir):
 
   if os.path.exists(known_hosts_path):
     os.remove(known_hosts_path)
-
-  return id_pubkey_path
 
 
 def _MakeQcowDisk(output_dir, disk_path):
@@ -98,7 +101,8 @@ def GetBootImage(output_dir, target_arch):
   """"Gets a path to the Zircon boot image, with the SSH client public key
   added."""
 
-  pubkey_path = _ProvisionSSH(output_dir)
+  ProvisionSSH(output_dir)
+  pubkey_path = _GetPubKeyPath(output_dir)
   zbi_tool = os.path.join(common.SDK_ROOT, 'tools', 'zbi')
   image_source_path = GetTargetFile(target_arch, 'fuchsia.zbi')
   image_dest_path = os.path.join(output_dir, 'gen', 'fuchsia-with-keys.zbi')
