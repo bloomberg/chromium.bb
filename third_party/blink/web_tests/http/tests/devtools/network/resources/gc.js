088665f284f1fb056bd6df494591d85ff71834ce
Kent Tamura <tkent@chromium.org>
2018-11-30 04:32:29 +0000

    Remove some third_party/WebKit instances in third_party/blink/
    Bug: 622551
    Change-Id: I2d1911ef91081777978ea1a0fb45280434b2a5f5
    Reviewed-on: https://chromium-review.googlesource.com/c/1351983
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Quinten Yearsley <qyearsley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612562}
    Former-commit-id: 00ef3e476048edeb1b99538e42721302e29a3af5

--- third_party/blink/web_tests/http/tests/devtools/network/resources/gc.js
+++ third_party/blink/web_tests/http/tests/devtools/network/resources/gc.js
@@ -1,4 +1,4 @@
-// This file was copied from third_party/WebKit/LayoutTests/resources/gc.js
+// This file was copied from third_party/blink/web_tests/resources/gc.js
 // If there is no window.gc() already defined, define one using the best
 // method we can find.
 // The slow fallback should not hit in the actual test environment.
