a56dd9cc3c4ca5576ddab00c603794ef6178842a
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2018-11-30 01:41:41 +0000

    Revert "Handle Open/Verify android apps for Assistant"
    This reverts commit c156d4d8e1620fafc38f2440a1c4c1218b9bfa68 [formerly d20781e198ad7f985311737a00f7324c549f81b1].
    
    Reason for revert: Causes compile failure in Google Chrome ChromeOS
    
    https://ci.chromium.org/buildbot/chromium.chrome/Google%20Chrome%20ChromeOS/59164
    
    In file included from ../../chromeos/services/assistant/assistant_manager_service_impl.cc:5:
    ../../chromeos/services/assistant/assistant_manager_service_impl.h:142:31: error: no type named 'AndroidAppInfo' in namespace 'chromeos::assistant::action'; did you mean 'mojom::AndroidAppInfo'?
      void OnOpenAndroidApp(const action::AndroidAppInfo& app_info,
                                  ^~~~~~~~~~~~~~~~~~~~~~
                                  mojom::AndroidAppInfo
    gen/chromeos/services/assistant/public/mojom/assistant.mojom.h:1276:8: note: 'mojom::AndroidAppInfo' declared here
    class  AndroidAppInfo {
           ^
    
    Original change's description:
    > Handle Open/Verify android apps for Assistant
    > 
    > Bug: b/117793081
    > Test: Manual Test
    > Change-Id: If747d3317cb163715ff907839203e0c0eee57a1c
    > Reviewed-on: https://chromium-review.googlesource.com/c/1352341
    > Commit-Queue: Yue Li <updowndota@chromium.org>
    > Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    > Reviewed-by: Sam McNally <sammc@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#612448}
    
    TBR=sammc@chromium.org,xiaohuic@chromium.org,updowndota@chromium.org,llin@chromium.org
    
    Change-Id: If0edc6853dfdce343f6f52edd6bc059f8c7ec1aa
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: b/117793081
    Reviewed-on: https://chromium-review.googlesource.com/c/1356164
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612484}
    Former-commit-id: cf111e8e868a431216ad29a8b16d4c647b57ba1a

--- chrome/browser/ui/ash/assistant/device_actions.cc
+++ chrome/browser/ui/ash/assistant/device_actions.cc
@@ -9,38 +9,16 @@
 #include "ash/public/cpp/ash_pref_names.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/profiles/profile_manager.h"
-#include "chrome/browser/ui/app_list/arc/arc_app_list_prefs.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/power_manager/backlight.pb.h"
 #include "chromeos/dbus/power_manager_client.h"
 #include "chromeos/network/network_state_handler.h"
-#include "components/arc/arc_bridge_service.h"
-#include "components/arc/arc_service_manager.h"
-#include "components/arc/common/intent_helper.mojom.h"
 #include "components/prefs/pref_service.h"
 #include "components/user_manager/user_manager.h"
 
 using chromeos::NetworkHandler;
 using chromeos::NetworkStateHandler;
 using chromeos::NetworkTypePattern;
-using chromeos::assistant::mojom::AndroidAppInfoPtr;
-using chromeos::assistant::mojom::AppStatus;
-
-namespace {
-
-AppStatus GetAndroidAppStatus(const std::string& package_name) {
-  auto* prefs = ArcAppListPrefs::Get(ProfileManager::GetActiveUserProfile());
-  if (!prefs) {
-    LOG(ERROR) << "ArcAppListPrefs is not available.";
-    return AppStatus::UNKNOWN;
-  }
-  std::string app_id = prefs->GetAppIdByPackageName(package_name);
-
-  return app_id.empty() ? AppStatus::UNAVAILABLE : AppStatus::AVAILABLE;
-}
-
-}  // namespace
 
 DeviceActions::DeviceActions() {}
 
@@ -103,42 +81,4 @@ void DeviceActions::SetNightLightEnabled(bool enabled) {
   // Simply toggle the user pref, which is being observed by ash's night
   // light controller.
   profile->GetPrefs()->SetBoolean(ash::prefs::kNightLightEnabled, enabled);
-}
-
-void DeviceActions::OpenAndroidApp(AndroidAppInfoPtr app_info,
-                                   OpenAndroidAppCallback callback) {
-  app_info->status = GetAndroidAppStatus(app_info->package_name);
-
-  if (app_info->status != AppStatus::AVAILABLE) {
-    std::move(callback).Run(false);
-    return;
-  }
-
-  auto* helper = ARC_GET_INSTANCE_FOR_METHOD(
-      arc::ArcServiceManager::Get()->arc_bridge_service()->intent_helper(),
-      HandleIntent);
-  if (!helper) {
-    LOG(ERROR) << "Android container is not running.";
-    std::move(callback).Run(false);
-    return;
-  }
-
-  arc::mojom::ActivityNamePtr activity = arc::mojom::ActivityName::New();
-  activity->package_name = app_info->package_name;
-  auto intent = arc::mojom::IntentInfo::New();
-  if (!app_info->intent.empty()) {
-    intent->data = app_info->intent;
-  }
-  helper->HandleIntent(std::move(intent), std::move(activity));
-
-  std::move(callback).Run(true);
-}
-
-void DeviceActions::VerifyAndroidApp(
-    std::vector<chromeos::assistant::mojom::AndroidAppInfoPtr> apps_info,
-    VerifyAndroidAppCallback callback) {
-  for (const auto& app_info : apps_info) {
-    app_info->status = GetAndroidAppStatus(app_info->package_name);
-  }
-  std::move(callback).Run(std::move(apps_info));
 }
