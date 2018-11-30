6bd489d51bab2e872cc647bc965e425a722f2e99
Eric Robinson <ericrobinson@chromium.org>
2018-11-30 18:03:20 +0000

    Split out .cc classes for RulesetService.
    See also go/chromerulesetservicemerge.
    
    This change is the final step in the RulesetService change.
    Rather than merging the two classes, we've chosen to more
    appropriately name ContentRulesetService to reflect what it
    does (RulesetPublisherImpl).  In addition, the visibility of that
    class has been reduced, and it is now only referenced in/visible
    to the RulesetService class as well as tests.
    
    This change reflects splitting up the content of the .cc files,
    as the splitting of the headers was done in a previous CL.
    
    Change-Id: I2937ba2e2838669d3bf6a2f646acfefcd71f5f94
    Reviewed-on: https://chromium-review.googlesource.com/c/1354272
    Commit-Queue: Eric Robinson <ericrobinson@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#612695}
    Former-commit-id: af37afb41e18ab4ddba3024c192ba84c1718e5ab

--- components/subresource_filter/content/browser/ruleset_publisher_impl.h
+++ components/subresource_filter/content/browser/ruleset_publisher_impl.h
@@ -14,6 +14,7 @@
 #include "base/memory/ref_counted.h"
 #include "base/single_thread_task_runner.h"
 #include "components/subresource_filter/content/browser/ruleset_publisher.h"
+#include "components/subresource_filter/content/browser/ruleset_version.h"
 #include "components/subresource_filter/content/browser/verified_ruleset_dealer.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
--- components/subresource_filter/content/browser/ruleset_service.cc
+++ components/subresource_filter/content/browser/ruleset_service.cc
@@ -3,12 +3,10 @@
 // found in the LICENSE file.
 
 #include "components/subresource_filter/content/browser/ruleset_service.h"
-#include "components/subresource_filter/content/browser/ruleset_publisher_impl.h"
 
 #include <utility>
 
 #include "base/bind.h"
-#include "base/feature_list.h"
 #include "base/files/file_enumerator.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
@@ -17,7 +15,6 @@
 #include "base/macros.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/numerics/safe_conversions.h"
-#include "base/rand_util.h"
 #include "base/sequenced_task_runner.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/task/post_task.h"
@@ -27,6 +24,8 @@
 #include "base/trace_event/traced_value.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
+#include "components/subresource_filter/content/browser/ruleset_publisher.h"
+#include "components/subresource_filter/content/browser/ruleset_publisher_impl.h"
 #include "components/subresource_filter/content/common/subresource_filter_messages.h"
 #include "components/subresource_filter/core/browser/copying_file_stream.h"
 #include "components/subresource_filter/core/browser/subresource_filter_constants.h"
@@ -37,27 +36,12 @@
 #include "components/url_pattern_index/proto/rules.pb.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
-#include "content/public/browser/notification_service.h"
-#include "content/public/browser/notification_source.h"
-#include "content/public/browser/notification_types.h"
 #include "content/public/browser/render_process_host.h"
-#include "ipc/ipc_platform_file.h"
 #include "third_party/protobuf/src/google/protobuf/io/zero_copy_stream_impl_lite.h"
 
 namespace subresource_filter {
 
-// Constant definitions and helper functions ---------------------------------
-
 namespace {
-
-// Names of the preferences storing the most recent ruleset version that
-// was successfully stored to disk.
-const char kSubresourceFilterRulesetContentVersion[] =
-    "subresource_filter.ruleset_version.content";
-const char kSubresourceFilterRulesetFormatVersion[] =
-    "subresource_filter.ruleset_version.format";
-const char kSubresourceFilterRulesetChecksum[] =
-    "subresource_filter.ruleset_version.checksum";
 
 void RecordIndexAndWriteRulesetResult(
     RulesetService::IndexAndWriteRulesetResult result) {
@@ -100,88 +84,7 @@ class SentinelFile {
   DISALLOW_COPY_AND_ASSIGN(SentinelFile);
 };
 
-void SendRulesetToRenderProcess(base::File* file,
-                                content::RenderProcessHost* rph) {
-  DCHECK(rph);
-  DCHECK(file);
-  DCHECK(file->IsValid());
-  rph->Send(new SubresourceFilterMsg_SetRulesetForProcess(
-      IPC::TakePlatformFileForTransit(file->Duplicate())));
-}
-
-// The file handle is closed when the argument goes out of scope.
-void CloseFile(base::File) {}
-
-// Posts the |file| handle to the file thread so it can be closed.
-void CloseFileOnFileThread(base::File* file) {
-  if (!file->IsValid())
-    return;
-  base::PostTaskWithTraits(FROM_HERE,
-                           {base::TaskPriority::BEST_EFFORT, base::MayBlock()},
-                           base::BindOnce(&CloseFile, std::move(*file)));
-}
-
 }  // namespace
-
-// UnindexedRulesetInfo -------------------------------------------------------
-
-UnindexedRulesetInfo::UnindexedRulesetInfo() = default;
-UnindexedRulesetInfo::~UnindexedRulesetInfo() = default;
-
-// IndexedRulesetVersion ------------------------------------------------------
-
-IndexedRulesetVersion::IndexedRulesetVersion() = default;
-IndexedRulesetVersion::IndexedRulesetVersion(const std::string& content_version,
-                                             int format_version)
-    : content_version(content_version), format_version(format_version) {}
-IndexedRulesetVersion::~IndexedRulesetVersion() = default;
-IndexedRulesetVersion& IndexedRulesetVersion::operator=(
-    const IndexedRulesetVersion&) = default;
-
-// static
-void IndexedRulesetVersion::RegisterPrefs(PrefRegistrySimple* registry) {
-  registry->RegisterStringPref(kSubresourceFilterRulesetContentVersion,
-                               std::string());
-  registry->RegisterIntegerPref(kSubresourceFilterRulesetFormatVersion, 0);
-  registry->RegisterIntegerPref(kSubresourceFilterRulesetChecksum, 0);
-}
-
-// static
-int IndexedRulesetVersion::CurrentFormatVersion() {
-  return RulesetIndexer::kIndexedFormatVersion;
-}
-
-void IndexedRulesetVersion::ReadFromPrefs(PrefService* local_state) {
-  format_version =
-      local_state->GetInteger(kSubresourceFilterRulesetFormatVersion);
-  content_version =
-      local_state->GetString(kSubresourceFilterRulesetContentVersion);
-  checksum = local_state->GetInteger(kSubresourceFilterRulesetChecksum);
-}
-
-bool IndexedRulesetVersion::IsValid() const {
-  return format_version != 0 && !content_version.empty();
-}
-
-bool IndexedRulesetVersion::IsCurrentFormatVersion() const {
-  return format_version == CurrentFormatVersion();
-}
-
-void IndexedRulesetVersion::SaveToPrefs(PrefService* local_state) const {
-  local_state->SetInteger(kSubresourceFilterRulesetFormatVersion,
-                          format_version);
-  local_state->SetString(kSubresourceFilterRulesetContentVersion,
-                         content_version);
-  local_state->SetInteger(kSubresourceFilterRulesetChecksum, checksum);
-}
-
-std::unique_ptr<base::trace_event::TracedValue>
-IndexedRulesetVersion::ToTracedValue() const {
-  auto value = std::make_unique<base::trace_event::TracedValue>();
-  value->SetString("content_version", content_version);
-  value->SetInteger("format_version", format_version);
-  return value;
-}
 
 // IndexedRulesetLocator ------------------------------------------------------
 
@@ -549,92 +452,6 @@ void RulesetService::OnRulesetSet(base::File file) {
   }
 
   publisher_->PublishNewRulesetVersion(std::move(file));
-}
-
-// RulesetPublisherImpl ------------------------------------------------------
-
-RulesetPublisherImpl::RulesetPublisherImpl(
-    RulesetService* ruleset_service,
-    scoped_refptr<base::SequencedTaskRunner> blocking_task_runner)
-    : ruleset_service_(ruleset_service),
-      ruleset_dealer_(std::make_unique<VerifiedRulesetDealer::Handle>(
-          std::move(blocking_task_runner))) {
-  best_effort_task_runner_ = base::CreateSingleThreadTaskRunnerWithTraits(
-      {content::BrowserThread::UI, base::TaskPriority::BEST_EFFORT});
-  DCHECK(best_effort_task_runner_->BelongsToCurrentThread());
-  // Must rely on notifications as RenderProcessHostObserver::RenderProcessReady
-  // would only be called after queued IPC messages (potentially triggering a
-  // navigation) had already been sent to the new renderer.
-  notification_registrar_.Add(
-      this, content::NOTIFICATION_RENDERER_PROCESS_CREATED,
-      content::NotificationService::AllBrowserContextsAndSources());
-}
-
-RulesetPublisherImpl::~RulesetPublisherImpl() {
-  CloseFileOnFileThread(&ruleset_data_);
-}
-
-void RulesetPublisherImpl::SetRulesetPublishedCallbackForTesting(
-    base::OnceClosure callback) {
-  ruleset_published_callback_ = std::move(callback);
-}
-
-void RulesetPublisherImpl::TryOpenAndSetRulesetFile(
-    const base::FilePath& file_path,
-    int expected_checksum,
-    base::OnceCallback<void(base::File)> callback) {
-  GetRulesetDealer()->TryOpenAndSetRulesetFile(file_path, expected_checksum,
-                                               std::move(callback));
-}
-
-void RulesetPublisherImpl::PublishNewRulesetVersion(base::File ruleset_data) {
-  DCHECK(ruleset_data.IsValid());
-  CloseFileOnFileThread(&ruleset_data_);
-
-  // If Ad Tagging is running, then every request does a lookup and it's
-  // important that we verify the ruleset early on.
-  if (base::FeatureList::IsEnabled(kAdTagging)) {
-    // Even though the handle will immediately be destroyed, it will still
-    // validate the ruleset on its task runner.
-    VerifiedRuleset::Handle ruleset_handle(GetRulesetDealer());
-  }
-
-  ruleset_data_ = std::move(ruleset_data);
-  for (auto it = content::RenderProcessHost::AllHostsIterator(); !it.IsAtEnd();
-       it.Advance()) {
-    SendRulesetToRenderProcess(&ruleset_data_, it.GetCurrentValue());
-  }
-
-  if (!ruleset_published_callback_.is_null())
-    std::move(ruleset_published_callback_).Run();
-}
-
-scoped_refptr<base::SingleThreadTaskRunner>
-RulesetPublisherImpl::BestEffortTaskRunner() {
-  return best_effort_task_runner_;
-}
-
-VerifiedRulesetDealer::Handle* RulesetPublisherImpl::GetRulesetDealer() {
-  return ruleset_dealer_.get();
-}
-
-void RulesetPublisherImpl::IndexAndStoreAndPublishRulesetIfNeeded(
-    const UnindexedRulesetInfo& unindexed_ruleset_info) {
-  DCHECK(ruleset_service_);
-  ruleset_service_->IndexAndStoreAndPublishRulesetIfNeeded(
-      unindexed_ruleset_info);
-}
-
-void RulesetPublisherImpl::Observe(
-    int type,
-    const content::NotificationSource& source,
-    const content::NotificationDetails& details) {
-  DCHECK_EQ(type, content::NOTIFICATION_RENDERER_PROCESS_CREATED);
-  if (!ruleset_data_.IsValid())
-    return;
-  SendRulesetToRenderProcess(
-      &ruleset_data_,
-      content::Source<content::RenderProcessHost>(source).ptr());
 }
 
 }  // namespace subresource_filter
