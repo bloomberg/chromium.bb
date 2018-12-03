6c3c3ca3c7c184e9e3ea25707f1b7bcb765ed244
David Burger <dburger@chromium.org>
2018-12-03 14:22:01 -0700

    Remove now unused lib/const/waterfall.py constants
    Trim back the contents of lib/const/waterfall.py per
    other recent waterfall constant code reductions -
    removing all now unused constants from the file.
    
    TEST=./run_tests
    BUG=chromium:836551
    Change-Id: I152582e0763982f973b92533effd21d7b804e374
    Reviewed-on: https://chromium-review.googlesource.com/1359017
    Commit-Ready: David Burger <dburger@chromium.org>
    Tested-by: David Burger <dburger@chromium.org>
    Reviewed-by: Don Garrett <dgarrett@chromium.org>

--- lib/const/waterfall.py
+++ lib/const/waterfall.py
@@ -6,25 +6,13 @@
 """Constants related to waterfall names and behaviors."""
 
 WATERFALL_INTERNAL = 'chromeos'
-WATERFALL_INFRA = 'chromeos.infra'
 WATERFALL_RELEASE = 'chromeos_release'
-WATERFALL_BRANCH = 'chromeos.branch'
 # Used for all swarming builds.
 WATERFALL_SWARMING = 'go/legoland'
 
 # URLs to the various waterfalls.
 BUILD_INT_DASHBOARD = 'https://uberchromegw.corp.google.com/i/chromeos'
-RELEASE_DASHBOARD = 'https://uberchromegw.corp.google.com/i/chromeos_release'
-BRANCH_DASHBOARD = 'https://uberchromegw.corp.google.com/i/chromeos.branch'
 SWARMING_DASHBOARD = 'http://go/legoland'
-
-# Waterfall to dashboard URL mapping.
-WATERFALL_TO_DASHBOARD = {
-    WATERFALL_INTERNAL: BUILD_INT_DASHBOARD,
-    WATERFALL_RELEASE: RELEASE_DASHBOARD,
-    WATERFALL_BRANCH: BRANCH_DASHBOARD,
-    WATERFALL_SWARMING: SWARMING_DASHBOARD,
-}
 
 # Sheriff-o-Matic tree which Chrome OS alerts are posted to.
 SOM_TREE = 'chromeos'
