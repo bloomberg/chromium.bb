8fce044e040b04ae837992b4f04ea99194217246
Jan Wilken Dörrie <jdoerrie@chromium.org>
2018-12-03 21:38:30 +0000

    [Settings WebUI] Only show "Manage other people" on CrOS
    This change modifies the people section to only show the "Manage other
    people" row on Chrome OS. On other platforms this option will still be
    accessible from the Profile Chooser.
    
    Bug: 907035
    Change-Id: I7c2ab9ee97658e37a11f9d9a6d9ac0736ed2a1d2
    Reviewed-on: https://chromium-review.googlesource.com/c/1349336
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613258}
    Former-commit-id: c36d09a98363e906fe3aab009117fd86db49dd76

--- chrome/browser/ui/webui/settings/people_handler.cc
+++ chrome/browser/ui/webui/settings/people_handler.cc
@@ -67,7 +67,6 @@
 #include "components/signin/core/browser/signin_manager_base.h"
 #else
 #include "chrome/browser/signin/signin_util.h"
-#include "chrome/browser/ui/user_manager.h"
 #include "chrome/browser/ui/webui/profile_helper.h"
 #include "components/signin/core/browser/signin_manager.h"
 #endif
@@ -262,10 +261,6 @@ void PeopleHandler::RegisterMessages() {
   web_ui()->RegisterMessageCallback(
       "SyncSetupGetSyncStatus",
       base::BindRepeating(&PeopleHandler::HandleGetSyncStatus,
-                          base::Unretained(this)));
-  web_ui()->RegisterMessageCallback(
-      "SyncSetupManageOtherPeople",
-      base::BindRepeating(&PeopleHandler::HandleManageOtherPeople,
                           base::Unretained(this)));
 #if defined(OS_CHROMEOS)
   web_ui()->RegisterMessageCallback(
@@ -829,13 +824,6 @@ void PeopleHandler::HandleGetSyncStatus(const base::ListValue* args) {
   CHECK(args->Get(0, &callback_id));
 
   ResolveJavascriptCallback(*callback_id, *GetSyncStatusDictionary());
-}
-
-void PeopleHandler::HandleManageOtherPeople(const base::ListValue* /* args */) {
-#if !defined(OS_CHROMEOS)
-  UserManager::Show(base::FilePath(),
-                    profiles::USER_MANAGER_SELECT_PROFILE_NO_ACTION);
-#endif  // !defined(OS_CHROMEOS)
 }
 
 void PeopleHandler::CloseSyncSetup() {
