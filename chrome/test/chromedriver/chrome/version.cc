69a5e96845a9a3ac5e59abf0c2c5dd43ce2d85ac
khachatryan <khachatryan@chromium.org>
2018-12-03 21:04:30 +0000

    [ChromeDriver] Add Chrome v72, drop Chrome v69
    We are adding ChromeDriver support for Chrome v72
    and dropping support for Chrome v69
    
    Change-Id: I3424e451bf596eeab5052cbe6f1c79557d11649b
    Reviewed-on: https://chromium-review.googlesource.com/c/1359061
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Artur Khachatryan <khachatryan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613243}
    Former-commit-id: dd0f0eac96fa507606d151fbba0abf926ed775fe

--- chrome/test/chromedriver/chrome/version.cc
+++ chrome/test/chromedriver/chrome/version.cc
@@ -9,7 +9,7 @@
 namespace {
 
 // This variable must be able to be found and parsed by the upload script.
-const int kMinimumSupportedChromeVersion[] = {69, 0, 3497, 0};
+const int kMinimumSupportedChromeVersion[] = {70, 0, 3538, 0};
 
 }  // namespace
 
