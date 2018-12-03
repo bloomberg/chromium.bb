21b6d3223175603e16696444f2a0c637026de17c
Dana Fried <dfried@chromium.org>
2018-12-03 21:26:30 +0000

    Fix layout/repaint of tab on focus or alert change.
    Also removes code associated with "show close button on hover" in
    non-touch situations because showing of close button on inactive tabs is
    now purely a function of tab size and the code had no effect.
    
    Previously, tabs were relying on invalidation of the tabstrip to repaint
    when active status changed. This resulted in cases where when the number
    of tabs at minimum size exceded the size of the browser window, pinned
    tabs would not repaint when a new tab was added, resulting in them
    appearing to still be active (until something else invalidated the
    tabstrip, like a mouse event or resize).
    
    Now we're using InvalidateLayout() and SchedulePaint() correctly to make
    sure things which could change the render or layout state of a tab
    result in the tab being laid out and/or repainted.
    
    Bug: 896849
    Change-Id: If2fcdafe80a0d22dc1d7c5c2c4a703729b8ed55b
    Reviewed-on: https://chromium-review.googlesource.com/c/1357329
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613252}
    Former-commit-id: d80f1a220d9c95ab45afb8fdc44a2b926893132a

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -953,10 +953,6 @@ bool TabStrip::ShouldHideCloseButtonForTab(Tab* tab) const {
   return !!touch_layout_;
 }
 
-bool TabStrip::ShouldShowCloseButtonOnHover() {
-  return !touch_layout_;
-}
-
 bool TabStrip::MaySetClip() {
   // Only touch layout needs to restrict the clip.
   return touch_layout_ || IsStackingDraggedTabs();
