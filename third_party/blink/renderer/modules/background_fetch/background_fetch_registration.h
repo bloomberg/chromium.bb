cceba3998ddf68c5bf93edfa681c37f30cdd352e
Mugdha Lakhani <nator@chromium.org>
2018-12-03 14:35:30 +0000

    [Background Fetch] Remove records_ from BackgroundFetchRegistration.
    This had been introduced to simplify access to active fetches, but has
    the following issues:
    1. It makes match() and matchAll() inconsistent with Cache API's match()
    and matchAll(), which return new independently-consumable response(s) every
    time they're called.
    2. It breaks retries for responses, when downloading from a server that
    doesn't support resuming of downloads.
    
    For details, see
    https://docs.google.com/document/d/1CrbWrnnshhyp_SfiAeuODpnQX36GK3Bsi19rXQGez6Q/edit?usp=sharing
    
    The CL also updates the WPT test accordingly.
    
    Bug: 875201
    Change-Id: I2c33717cbee36f0d707e814506c0bae9b5a4f31f
    Reviewed-on: https://chromium-review.googlesource.com/c/1355121
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613079}
    Former-commit-id: 8b1cca92cd20df271685faeef8400c4e848761d9

--- third_party/blink/renderer/modules/background_fetch/background_fetch_registration.h
+++ third_party/blink/renderer/modules/background_fetch/background_fetch_registration.h
@@ -12,7 +12,6 @@
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
-#include "third_party/blink/renderer/platform/weborigin/kurl_hash.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
@@ -121,9 +120,6 @@ class BackgroundFetchRegistration final
   bool IsAborted();
 
   Member<ServiceWorkerRegistration> registration_;
-
-  // TODO(crbug.com/774054): Update the key once we support duplicate requests.
-  HeapHashMap<KURL, Member<BackgroundFetchRecord>> records_;
 
   // Corresponds to IDL 'id' attribute. Not unique - an active registration can
   // have the same |developer_id_| as one or more inactive registrations.
