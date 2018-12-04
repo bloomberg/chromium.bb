3d41a2daf1ba478d68183bfe8b34445ed98e8c5d
Alex Clarke <alexclarke@chromium.org>
2018-12-04 09:43:59 +0000

    [C++ Promises] Add base::TypeId
    This is a light weight alternative to RTTI based on using the linker to
    uniquely reserve a zero-initialised byte in .rodata. The cost here is
    the pointer-sized relocation that the linker must patch in for every
    type at program startup. We don't expect vast numbers of types to be
    used in practice so this should be OK.
    
    Note we can't use std::any when C++17 arrives because that is based on
    RTTI which is banned in Chromium, which motivates base::TypeId.
    
    Bug: 906125
    Change-Id: I03f7c14f0cc665d865eb6e029a23f441baf20a9a
    Reviewed-on: https://chromium-review.googlesource.com/c/1340317
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613486}
    Former-commit-id: 2fb11e0d8a1b9d8e3b46bf91844683848646cbe4

---
+++ base/test/type_id_test_support_a.h
@@ -0,0 +1,28 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_TEST_TYPE_ID_TEST_SUPPORT_A_H_
+#define BASE_TEST_TYPE_ID_TEST_SUPPORT_A_H_
+
+#include "base/type_id.h"
+
+namespace base {
+
+// BASE_EXPORT depends on COMPONENT_BUILD.
+// This will always be a separate shared library, so don't use BASE_EXPORT here.
+#if defined(WIN32)
+#define TEST_SUPPORT_EXPORT __declspec(dllexport)
+#else
+#define TEST_SUPPORT_EXPORT __attribute__((visibility("default")))
+#endif  // defined(WIN32)
+
+// This is here to help test base::TypeId.
+struct TEST_SUPPORT_EXPORT TypeIdTestSupportA {
+  static TypeId GetTypeIdForTypeInAnonymousNameSpace();
+  static TypeId GetTypeIdForUniquePtrInt();
+};
+
+}  // namespace base
+
+#endif  // BASE_TEST_TYPE_ID_TEST_SUPPORT_A_H_
