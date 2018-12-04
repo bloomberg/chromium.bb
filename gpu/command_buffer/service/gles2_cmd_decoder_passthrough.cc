1af51825bd629fbfc3d469e0176295821b3f149e
Geoff Lang <geofflang@chromium.org>
2018-12-04 18:46:58 +0000

    Log injected errors in the passthrough command decoder.
    Previously we only logged errors generated from ANGLE.
    
    BUG=738568
    
    Change-Id: Iac15b0223927355f450d726441575582d8df53af
    Reviewed-on: https://chromium-review.googlesource.com/c/1355240
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Geoff Lang <geofflang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613616}
    Former-commit-id: f1ce73cd073a5befd82645bfac412ba23ecb9247

--- gpu/command_buffer/service/gles2_cmd_decoder_passthrough.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder_passthrough.cc
@@ -1944,9 +1944,11 @@ GLES2DecoderPassthroughImpl::PatchGetFramebufferAttachmentParameter(
 }
 
 void GLES2DecoderPassthroughImpl::InsertError(GLenum error,
-                                              const std::string&) {
-  // Message ignored for now
+                                              const std::string& message) {
   errors_.insert(error);
+  LogGLDebugMessage(GL_DEBUG_SOURCE_API, GL_DEBUG_TYPE_ERROR, error,
+                    GL_DEBUG_SEVERITY_HIGH, message.length(), message.c_str(),
+                    GetLogger());
 }
 
 GLenum GLES2DecoderPassthroughImpl::PopError() {
