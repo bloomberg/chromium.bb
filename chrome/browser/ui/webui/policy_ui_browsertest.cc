35e67203446b245a4131e825a896a3a785dbdc35
Robert Flack <flackr@chromium.org>
2018-12-04 23:08:12 +0000

    Reland "Disable flaky timeouts when running with asan."
    Relands disabling flaky timeouts patch with DISABLED macro corrected for
    WRAPPED_INSTANTIATE_TEST_CASE_P
    
    TBR=lucmult@chromium.org,bartfab@chromium.org,pmarko@chromium.org
    
    Bug: 911661
    Change-Id: I16b7c76102854d398b8e130f66cef568bb5126b9
    Reviewed-on: https://chromium-review.googlesource.com/c/1361574
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613747}
    Former-commit-id: dee855ff4ba186b8a94f6f91a618dad10e9d1e0f

--- chrome/browser/ui/webui/policy_ui_browsertest.cc
+++ chrome/browser/ui/webui/policy_ui_browsertest.cc
@@ -542,7 +542,14 @@ IN_PROC_BROWSER_TEST_F(PolicyUITest, SendPolicyValues) {
   VerifyPolicies(expected_policies);
 }
 
-IN_PROC_BROWSER_TEST_F(PolicyUITest, ExtensionLoadAndSendPolicy) {
+// TODO(https://crbug.com/911661) Flaky time outs on Linux Chromium OS ASan
+// LSan bot.
+#if defined(ADDRESS_SANITIZER)
+#define MAYBE_ExtensionLoadAndSendPolicy DISABLED_ExtensionLoadAndSendPolicy
+#else
+#define MAYBE_ExtensionLoadAndSendPolicy ExtensionLoadAndSendPolicy
+#endif
+IN_PROC_BROWSER_TEST_F(PolicyUITest, MAYBE_ExtensionLoadAndSendPolicy) {
   ui_test_utils::NavigateToURL(browser(), GURL(chrome::kChromeUIPolicyURL));
   base::ScopedAllowBlockingForTesting allow_blocking;
   base::ScopedTempDir temp_dir_;
