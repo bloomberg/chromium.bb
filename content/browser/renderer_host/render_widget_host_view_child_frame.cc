78dfdfe71f4b221b1626f7720a062383c58dfaae
Yuichiro Hanada <yhanada@chromium.org>
2018-12-04 01:04:31 +0000

    Use GetSelectedText() to check whether any text is selected or not.
    Bug: 901439, 908762
    Test: content_browsertests
    Change-Id: Id8730f01db6781d7983ebbc1402176203e0b146a
    Reviewed-on: https://chromium-review.googlesource.com/c/1347634
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613366}
    Former-commit-id: 697c453e769909a6f50de7d183f1f34a03601eb4

--- content/browser/renderer_host/render_widget_host_view_child_frame.cc
+++ content/browser/renderer_host/render_widget_host_view_child_frame.cc
@@ -1077,22 +1077,6 @@ void RenderWidgetHostViewChildFrame::ResetCompositorFrameSinkSupport() {
   support_.reset();
 }
 
-bool RenderWidgetHostViewChildFrame::GetSelectionRange(
-    gfx::Range* range) const {
-  if (!text_input_manager_ || !GetFocusedWidget())
-    return false;
-
-  const TextInputManager::TextSelection* selection =
-      text_input_manager_->GetTextSelection(GetFocusedWidget()->GetView());
-  if (!selection)
-    return false;
-
-  range->set_start(selection->range().start());
-  range->set_end(selection->range().end());
-
-  return true;
-}
-
 ui::TextInputType RenderWidgetHostViewChildFrame::GetTextInputType() const {
   if (!text_input_manager_)
     return ui::TEXT_INPUT_TYPE_NONE;
