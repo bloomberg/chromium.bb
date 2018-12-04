26740c82a110fcebaaa1b4e121acdf37a6b833d0
Robert Flack <flackr@chromium.org>
2018-12-04 23:08:12 +0000

    Reland "Disable flaky timeouts when running with asan."
    Relands disabling flaky timeouts patch with DISABLED macro corrected for
    WRAPPED_INSTANTIATE_TEST_CASE_P
    
    TBR=lucmult@chromium.org,bartfab@chromium.org,pmarko@chromium.org
    
    Bug: 911661
    Change-Id: I16b7c76102854d398b8e130f66cef568bb5126b9
    Reviewed-on: https://chromium-review.googlesource.com/c/1361574
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613747}
    Former-commit-id: dee855ff4ba186b8a94f6f91a618dad10e9d1e0f

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -469,8 +469,15 @@ WRAPPED_INSTANTIATE_TEST_CASE_P(
                       TestCase("closeQuickView"),
                       TestCase("cantOpenQuickViewWithMultipleFiles")));
 
+// TODO(https://crbug.com/911661) Flaky time outs on Linux Chromium OS ASan
+// LSan bot.
+#if defined(ADDRESS_SANITIZER)
+#define MAYBE_DirectoryTreeContextMenu DISABLED_DirectoryTreeContextMenu
+#else
+#define MAYBE_DirectoryTreeContextMenu DirectoryTreeContextMenu
+#endif
 WRAPPED_INSTANTIATE_TEST_CASE_P(
-    DirectoryTreeContextMenu, /* directory_tree_context_menu.js */
+    MAYBE_DirectoryTreeContextMenu, /* directory_tree_context_menu.js */
     FilesAppBrowserTest,
     ::testing::Values(
         TestCase("dirCopyWithContextMenu"),
