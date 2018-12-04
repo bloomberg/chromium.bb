eefa194c6b926a5f85650ddb5deaaaa7fccea554
Zhiqiang Zhang <zqzhang@chromium.org>
2018-12-04 02:37:33 +0000

    Reland "[Android MR] Postpone route selection until session launch"
    This reverts commit e7f0c1c7a45a1825464a7be9be137092bf3acb24 [formerly 74e258c6b0861b76fb8a60b6149f30dd50947648].
    
    Reason for revert: The previous revert was wrong.
    This is still a necessary change
    
    Original change's description:
    > Revert "[Android MR] Postpone route selection until session launch"
    > 
    > This reverts commit b5e40d59e2b7c69fa06b1d0fd597b6a7c71b8eec [formerly 7c453583f0c622cf928bb5d628ae9def8f03279f].
    > 
    > Reason for revert: This introduces some regression in some cases. The old logic works just fine so it is safe to revert.
    > 
    > Original change's description:
    > > [Android MR] Postpone route selection until session launch
    > > 
    > > Previously, we unselect and re-select the route due to CAF limitation.
    > > The reason is that we need to set the receiver app ID after the user
    > > choose a device. However CAF won't be able to know a device was selected
    > > after the receiver app ID gets changed.
    > > 
    > > In this CL, to solve the issue, we intercept the click event in
    > > MediaRouteChooserDialog (which would select the route automatically).
    > > The route selection is postponed right before session launch. Therefore
    > > we are able to set the receiver app ID and select the route to get rid
    > > of CAF limitation.
    > > 
    > > Bug: 711860
    > > Change-Id: I5b1534d36d540b62199a6fb739c56ad5131c2711
    > > Reviewed-on: https://chromium-review.googlesource.com/c/1351893
    > > Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    > > Commit-Queue: Zhiqiang Zhang <zqzhang@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#611437}
    > 
    > TBR=zqzhang@chromium.org,tguilbert@chromium.org
    > 
    > Change-Id: I30322daecf8c866fdda9e846d0cb37d9a319f033
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 711860
    > Reviewed-on: https://chromium-review.googlesource.com/c/1352973
    > Reviewed-by: Zhiqiang Zhang <zqzhang@chromium.org>
    > Commit-Queue: Zhiqiang Zhang <zqzhang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#611572}
    
    TBR=zqzhang@chromium.org,tguilbert@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 711860
    Change-Id: I8bbeeb9e6917e07163bb0fbfacd1fa1c3f762a49
    Reviewed-on: https://chromium-review.googlesource.com/c/1359822
    Reviewed-by: Zhiqiang Zhang <zqzhang@chromium.org>
    Commit-Queue: Zhiqiang Zhang <zqzhang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613403}
    Former-commit-id: 37f67ef488ec98c04f8d2a4c5d5ae78e317b8fca

--- chrome/android/java/src/org/chromium/chrome/browser/media/router/caf/BaseSessionController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/router/caf/BaseSessionController.java
@@ -6,7 +6,6 @@ package org.chromium.chrome.browser.media.router.caf;
 
 import android.support.annotation.Nullable;
 import android.support.annotation.VisibleForTesting;
-import android.support.v7.media.MediaRouter;
 
 import com.google.android.gms.cast.CastDevice;
 import com.google.android.gms.cast.framework.CastSession;
@@ -31,7 +30,6 @@ public class BaseSessionController {
 
     private CastSession mCastSession;
     private final CafBaseMediaRouteProvider mProvider;
-    private final MediaRouter.Callback mMediaRouterCallbackForSessionLaunch;
     private CreateRouteRequestInfo mRouteCreationInfo;
     @VisibleForTesting
     CafNotificationController mNotificationController;
@@ -39,7 +37,6 @@ public class BaseSessionController {
 
     public BaseSessionController(CafBaseMediaRouteProvider provider) {
         mProvider = provider;
-        mMediaRouterCallbackForSessionLaunch = new MediaRouterCallbackForSessionLaunch();
         mNotificationController = new CafNotificationController(this);
         mRemoteMediaClientCallback = new RemoteMediaClientCallback();
     }
@@ -49,21 +46,10 @@ public class BaseSessionController {
         CastUtils.getCastContext().setReceiverApplicationId(
                 mRouteCreationInfo.source.getApplicationId());
 
-        if (mRouteCreationInfo.routeInfo.isSelected()) {
-            // If a route has just been selected, CAF might not be ready yet before setting the app
-            // ID. So unselect and select the route will let CAF be aware that the route has been
-            // selected thus it can start the session.
-            //
-            // An issue of this workaround is that if a route is unselected and selected in a very
-            // short time, the selection might be ignored by MediaRouter, so put the reselection in
-            // a callback.
-            mProvider.getAndroidMediaRouter().addCallback(
-                    mRouteCreationInfo.source.buildRouteSelector(),
-                    mMediaRouterCallbackForSessionLaunch);
-            mProvider.getAndroidMediaRouter().unselect(MediaRouter.UNSELECT_REASON_UNKNOWN);
-        } else {
-            mRouteCreationInfo.routeInfo.select();
-        }
+        // When the user clicks a route on the MediaRouteChooserDialog, we intercept the click event
+        // and do not select the route. Instead the route selection is postponed to here. This will
+        // trigger CAF to launch the session.
+        mRouteCreationInfo.routeInfo.select();
     }
 
     public MediaSource getSource() {
@@ -170,20 +156,6 @@ public class BaseSessionController {
                         + message + "\"");
         if (CastSessionUtil.MEDIA_NAMESPACE.equals(namespace)) {
             updateRemoteMediaClient(message);
-        }
-    }
-
-    private class MediaRouterCallbackForSessionLaunch extends MediaRouter.Callback {
-        @Override
-        public void onRouteUnselected(MediaRouter mediaRouter, MediaRouter.RouteInfo routeInfo) {
-            if (mProvider.getPendingCreateRouteRequestInfo() == null) return;
-
-            if (routeInfo.getId().equals(
-                        mProvider.getPendingCreateRouteRequestInfo().routeInfo.getId())) {
-                routeInfo.select();
-                mProvider.getAndroidMediaRouter().removeCallback(
-                        mMediaRouterCallbackForSessionLaunch);
-            }
         }
     }
 
