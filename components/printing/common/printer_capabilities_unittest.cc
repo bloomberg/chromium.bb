404c1c59976a23b4e15587468830c928b640a47f
Lei Zhang <thestig@chromium.org>
2018-12-04 01:30:36 +0000

    Fix some nits in PrinterCapabilitiesTest.
    Change-Id: I853a885dd09d523969353c698a7312272d5b0523
    Reviewed-on: https://chromium-review.googlesource.com/c/1357761
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613378}
    Former-commit-id: 3b7b671f510d1aafe6c9f08c1b2e3a71acb5f115

--- components/printing/common/printer_capabilities_unittest.cc
+++ components/printing/common/printer_capabilities_unittest.cc
@@ -17,8 +17,10 @@
 namespace printing {
 
 namespace {
+
 const char kDpi[] = "dpi";
-}
+
+}  // namespace
 
 class PrinterCapabilitiesTest : public testing::Test {
  public:
@@ -43,8 +45,8 @@ class PrinterCapabilitiesTest : public testing::Test {
 // Verify that we don't crash for a missing printer and a nullptr is never
 // returned.
 TEST_F(PrinterCapabilitiesTest, NonNullForMissingPrinter) {
-  PrinterBasicInfo basic_info;
   std::string printer_name = "missing_printer";
+  PrinterBasicInfo basic_info;
 
   std::unique_ptr<base::DictionaryValue> settings_dictionary =
       GetSettingsOnBlockingPool(printer_name, basic_info, nullptr);
@@ -55,24 +57,24 @@ TEST_F(PrinterCapabilitiesTest, NonNullForMissingPrinter) {
 TEST_F(PrinterCapabilitiesTest, ProvidedCapabilitiesUsed) {
   std::string printer_name = "test_printer";
   PrinterBasicInfo basic_info;
+
+  // Set a capability and add a valid printer.
   auto caps = std::make_unique<PrinterSemanticCapsAndDefaults>();
-
-  // set a capability
-  caps->dpis = {gfx::Size(600, 600)};
-
+  caps->dpis = {{600, 600}};
   print_backend()->AddValidPrinter(printer_name, std::move(caps));
 
   std::unique_ptr<base::DictionaryValue> settings_dictionary =
       GetSettingsOnBlockingPool(printer_name, basic_info, print_backend());
 
-  // verify settings were created
+  // Verify settings were created.
   ASSERT_TRUE(settings_dictionary);
 
-  // verify capabilities and have one entry
+  // Verify capabilities dict exists and has 2 entries. (printer and version)
   base::DictionaryValue* cdd;
   ASSERT_TRUE(settings_dictionary->GetDictionary(kSettingCapabilities, &cdd));
+  EXPECT_EQ(2U, cdd->size());
 
-  // read the CDD for the dpi attribute.
+  // Read the CDD for the "dpi" attribute.
   base::DictionaryValue* caps_dict;
   ASSERT_TRUE(cdd->GetDictionary(kPrinter, &caps_dict));
   EXPECT_TRUE(caps_dict->HasKey(kDpi));
@@ -84,16 +86,16 @@ TEST_F(PrinterCapabilitiesTest, NullCapabilitiesExcluded) {
   std::string printer_name = "test_printer";
   PrinterBasicInfo basic_info;
 
-  // return false when attempting to retrieve capabilities
+  // Return false when attempting to retrieve capabilities.
   print_backend()->AddValidPrinter(printer_name, nullptr);
 
   std::unique_ptr<base::DictionaryValue> settings_dictionary =
       GetSettingsOnBlockingPool(printer_name, basic_info, print_backend());
 
-  // verify settings were created
+  // Verify settings were created.
   ASSERT_TRUE(settings_dictionary);
 
-  // verify that capabilities is an empty dictionary
+  // Verify that capabilities is an empty dictionary.
   base::DictionaryValue* caps_dict;
   ASSERT_TRUE(
       settings_dictionary->GetDictionary(kSettingCapabilities, &caps_dict));
