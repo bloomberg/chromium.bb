03251106d2802fe0d572c64235a48f51d205c841
Daniel Nicoara <dnicoara@chromium.org>
2018-12-05 22:02:46 +0000

    EXO: Use Aura only TestBase for server unittests
    The server unittests do not need an Ash dependency. Move ExoTestBaseCast
    to ExoTestBaseAura and use it for server unittests.
    
    Bug: 896710
    Test: Run exo_unittests
    Change-Id: I7902624c74f85c2b9a9d0050d8c4e23c5bfde154
    Reviewed-on: https://chromium-review.googlesource.com/c/1345240
    Commit-Queue: Daniel Nicoara <dnicoara@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614118}
    Former-commit-id: 3e65fb569b4b509ff28813b6179ca5e2575ab52b

--- components/exo/test/exo_test_base_aura.h
+++ components/exo/test/exo_test_base_aura.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_EXO_TEST_EXO_TEST_BASE_CAST_H_
-#define COMPONENTS_EXO_TEST_EXO_TEST_BASE_CAST_H_
+#ifndef COMPONENTS_EXO_TEST_EXO_TEST_BASE_AURA_H_
+#define COMPONENTS_EXO_TEST_EXO_TEST_BASE_AURA_H_
 
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/aura/test/aura_test_base.h"
@@ -14,12 +14,12 @@ class WMHelper;
 
 namespace test {
 
-// Basic implementation of ExoTestBase without
-// ash dependencies
-class ExoTestBaseCast : public aura::test::AuraTestBase {
+// Implementation of a test::Base built on Aura without ChromeOS Ash
+// dependencies.
+class ExoTestBaseAura : public aura::test::AuraTestBase {
  public:
-  ExoTestBaseCast();
-  ~ExoTestBaseCast() override;
+  ExoTestBaseAura();
+  ~ExoTestBaseAura() override;
 
   // Overridden from test::Test.
   void SetUp() override;
@@ -32,4 +32,4 @@ class ExoTestBaseCast : public aura::test::AuraTestBase {
 }  // namespace test
 }  // namespace exo
 
-#endif  // COMPONENTS_EXO_TEST_EXO_TEST_BASE_CAST_H_
+#endif  // COMPONENTS_EXO_TEST_EXO_TEST_BASE_AURA_H_
