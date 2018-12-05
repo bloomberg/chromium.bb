48e7ffe06f9ec6c32b8d4098dc6d26fb29a717ec
Min Qin <qinmin@chromium.org>
2018-12-05 06:26:23 +0000

    Add instrumentation test for resuming a download with service manager only mode
    This CL adds an instrumentation test for resuming a download with only
    service manager running
    
    BUG=910734
    
    Change-Id: I0b8468b8db5920bdc8c3b57ad155865771f36fe0
    Reviewed-on: https://chromium-review.googlesource.com/c/1357600
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613886}
    Former-commit-id: 698f5fb8f05a44681382029746c3ea533caa0c9d

--- chrome/android/javatests/src/org/chromium/chrome/browser/ServicificationBackgroundServiceTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ServicificationBackgroundServiceTest.java
@@ -4,30 +4,21 @@
 
 package org.chromium.chrome.browser;
 
-import android.content.Context;
 import android.support.test.filters.MediumTest;
 
 import com.google.android.gms.gcm.TaskParams;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.chromium.base.ThreadUtils;
-import org.chromium.base.library_loader.ProcessInitException;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.RetryOnFailure;
-import org.chromium.chrome.browser.init.BrowserParts;
-import org.chromium.chrome.browser.init.ChromeBrowserInitializer;
-import org.chromium.chrome.browser.init.EmptyBrowserParts;
 import org.chromium.chrome.browser.init.ServiceManagerStartupUtils;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
-import org.chromium.content_public.browser.test.util.Criteria;
-import org.chromium.content_public.browser.test.util.CriteriaHelper;
 
 /**
  * Tests background tasks can be run in Service Manager only mode, i.e., without starting the full
@@ -36,61 +27,13 @@ import org.chromium.content_public.browser.test.util.CriteriaHelper;
 @RunWith(ChromeJUnit4ClassRunner.class)
 @RetryOnFailure
 public final class ServicificationBackgroundServiceTest {
-    private MockTaskService mTaskService;
-
-    static class MockTaskService extends ChromeBackgroundService {
-        private boolean mDidLaunchBrowser;
-        private boolean mDidStartServiceManager;
-
-        @Override
-        protected void launchBrowser(Context context, String tag) {
-            mDidLaunchBrowser = true;
-
-            final BrowserParts parts = new EmptyBrowserParts() {
-                @Override
-                public void finishNativeInitialization() {
-                    mDidStartServiceManager = true;
-                }
-
-                @Override
-                public boolean startServiceManagerOnly() {
-                    return true;
-                }
-            };
-
-            try {
-                ChromeBrowserInitializer.getInstance().handlePreNativeStartup(parts);
-                ChromeBrowserInitializer.getInstance().handlePostNativeStartup(true, parts);
-            } catch (ProcessInitException e) {
-                ChromeApplication.reportStartupErrorAndExit(e);
-            }
-        }
-
-        // Posts an assertion task to the UI thread. Since this is only called after the call
-        // to onRunTask, it will be enqueued after any possible call to launchBrowser, and we
-        // can reliably check whether launchBrowser was called.
-        protected void checkExpectations(final boolean expectedLaunchBrowser) {
-            ThreadUtils.runOnUiThread(() -> {
-                Assert.assertEquals("StartedService", expectedLaunchBrowser, mDidLaunchBrowser);
-            });
-        }
-
-        public void waitForServiceManagerStart() {
-            CriteriaHelper.pollUiThread(
-                    new Criteria("Failed while waiting for starting Service Manager.") {
-                        @Override
-                        public boolean isSatisfied() {
-                            return mDidStartServiceManager;
-                        }
-                    });
-        }
-    }
+    private ServicificationBackgroundService mServicificationBackgroundService;
 
     @Before
     public void setUp() {
         BackgroundSyncLauncher.setGCMEnabled(false);
         RecordHistogram.setDisabledForTests(true);
-        mTaskService = new MockTaskService();
+        mServicificationBackgroundService = new ServicificationBackgroundService();
     }
 
     @After
@@ -99,9 +42,9 @@ public final class ServicificationBackgroundServiceTest {
     }
 
     private void startOnRunTaskAndVerify(String taskTag, boolean shouldStart) {
-        mTaskService.onRunTask(new TaskParams(taskTag));
-        mTaskService.checkExpectations(shouldStart);
-        mTaskService.waitForServiceManagerStart();
+        mServicificationBackgroundService.onRunTask(new TaskParams(taskTag));
+        mServicificationBackgroundService.checkExpectations(shouldStart);
+        mServicificationBackgroundService.waitForServiceManagerStart();
     }
 
     @Test
