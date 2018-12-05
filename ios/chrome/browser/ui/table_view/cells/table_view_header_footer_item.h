a52b9e5eee51d6e22ffdff4de8305ece7f7cd1d4
Yi Su <mrsuyi@chromium.org>
2018-12-05 09:59:15 +0000

    Remove unused methods "headerHeightForWidth" in TableViewHeaderFooterItem and its descendants.
    This CLs removes all the "headerHeightForWidth" methods in
    TableViewHeaderFooterItem and its descendants since Chrome App doesn't
    support iOS10 anymore.
    
    Bug: 850814
    Change-Id: I8c77ca17dcb29f3fa8c004abb48397e7cb157073
    Reviewed-on: https://chromium-review.googlesource.com/c/1361105
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613922}
    Former-commit-id: 92bac9a669acaa68d5779828840887abed96838f

--- ios/chrome/browser/ui/table_view/cells/table_view_header_footer_item.h
+++ ios/chrome/browser/ui/table_view/cells/table_view_header_footer_item.h
@@ -23,10 +23,6 @@
                        withStyler:(ChromeTableViewStyler*)styler
     NS_REQUIRES_SUPER;
 
-// TODO(crbug.com/850814):Use only dynamic sizing once we stop supporting iOS10,
-// we can delete this method then. Returns the height this item's header would
-// need to be to fit within |width|.
-- (CGFloat)headerHeightForWidth:(CGFloat)width;
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_TABLE_VIEW_CELLS_TABLE_VIEW_HEADER_FOOTER_ITEM_H_
