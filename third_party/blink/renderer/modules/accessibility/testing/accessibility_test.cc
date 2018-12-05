e8fb84522d23588cc25e1ac69f491a0e31584a26
Nektarios Paisios <nektar@chromium.org>
2018-12-05 03:13:54 +0000

    AXSelection: Added the ability to select within the shadow DOM
    This includes both user agent shadow trees, e.g. the ones used for text fields and spin controls, as well as user generated shadow DOM trees, such as the ones created by web components.
    R=dmazzoni@chromium.org, aleventhal@chromium.org, aboxhall@chromium.org
    
    Change-Id: I4280035eb5f4e1f0bdcec251823266010a790ec0
    Bug: 639340
    Reviewed-on: https://chromium-review.googlesource.com/c/1355530
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#613842}
    Former-commit-id: 098ef8ec87b7f01f7b31846b0303d49fa49735ee

--- third_party/blink/renderer/modules/accessibility/testing/accessibility_test.cc
+++ third_party/blink/renderer/modules/accessibility/testing/accessibility_test.cc
@@ -5,6 +5,7 @@
 #include "third_party/blink/renderer/modules/accessibility/testing/accessibility_test.h"
 
 #include "third_party/blink/renderer/core/dom/document.h"
+#include "third_party/blink/renderer/core/dom/node.h"
 #include "third_party/blink/renderer/core/frame/local_frame_client.h"
 #include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
@@ -27,6 +28,10 @@ AXObjectCacheImpl& AccessibilityTest::GetAXObjectCache() const {
       ToAXObjectCacheImpl(GetDocument().ExistingAXObjectCache());
   DCHECK(ax_object_cache);
   return *ax_object_cache;
+}
+
+AXObject* AccessibilityTest::GetAXObject(const Node& node) const {
+  return GetAXObjectCache().GetOrCreate(&node);
 }
 
 AXObject* AccessibilityTest::GetAXRootObject() const {
