92de4aee966e52b4b2b2beadf9feae70d57b0580
Tanja Gornak <melandory@chromium.org>
2018-12-06 10:49:35 +0000

    [Tango->FCM] Add the API for enabling sync for session related datatypes.
    The CL is adding the API  to the ForeignSessionHelper for enabling sync for session related datatypes.
    
    
    * In the new invalidation infrastructure Clank isn't receiving the notifications
    for session sync related datatypes.
    * But in cases when the user opens history UI, sync should re-enable session sync related
    invalidations.
    * This patch adds the API which will be called from History UI. The API allows to enable or disable
    session sync related invalidations as needed.
    
    TBR=melandory@chromium.org
    
    (cherry picked from commit e005793bacb527b682c6a6c8033d1a4460d5657e [formerly 9086f1acab0606a174d6654e0f6312ac1da7077c])
    
    Bug: 909682
    Change-Id: I60755adc9bc8b6f063a59f40f1df016a0a39c809
    Reviewed-on: https://chromium-review.googlesource.com/c/1358492
    Commit-Queue: Tatiana Gornak <melandory@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#613908}
    Reviewed-on: https://chromium-review.googlesource.com/c/1365240
    Reviewed-by: Tatiana Gornak <melandory@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3626@{#104}
    Cr-Branched-From: 5302c6863bade38a5d283a7cce26a309d8e8e755 [formerly d897fb137fbaaa9355c0c93124cc048824eb1e65]-refs/heads/master@{#612437}
    Former-commit-id: 8f1c8d41cf56db641c791a58ab33b40f58c1a889

--- chrome/browser/android/foreign_session_helper.cc
+++ chrome/browser/android/foreign_session_helper.cc
@@ -304,3 +304,15 @@ void ForeignSessionHelper::DeleteForeignSession(
   if (open_tabs)
     open_tabs->DeleteForeignSession(ConvertJavaStringToUTF8(env, session_tag));
 }
+
+void ForeignSessionHelper::SetInvalidationsForSessionsEnabled(
+    JNIEnv* env,
+    const JavaParamRef<jobject>& obj,
+    jboolean enabled) {
+  browser_sync::ProfileSyncService* service =
+      ProfileSyncServiceFactory::GetInstance()->GetForProfile(profile_);
+  if (!service)
+    return;
+
+  service->SetInvalidationsForSessionsEnabled(enabled);
+}
