abcc2f0148dc844ef42e336a8b9210cdcf5cdb06
Justin Cohen <justincohen@google.com>
2018-12-06 19:36:59 +0000

    [ios] Add more metrics to iOS preload controller.
    Add histograms to track last committed index, time from
    prerender creation to release, and if it's from an NTP.
    
    Bug: 911763
    Change-Id: I3bfa423a8cb916f18002f0b6b25ba4f4b56eafa6
    Reviewed-on: https://chromium-review.googlesource.com/c/1361637
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614450}
    Former-commit-id: 118fee6c362144aaf3fb5672556a6a849a8560f6

--- ios/chrome/browser/prerender/preload_controller.mm
+++ ios/chrome/browser/prerender/preload_controller.mm
@@ -59,6 +59,9 @@ const char kPrerenderFinalStatusHistogramName[] = "Prerender.FinalStatus";
 // The name of the histogram for recording the number of successful prerenders.
 const char kPrerendersPerSessionCountHistogramName[] =
     "Prerender.PrerendersPerSessionCount";
+// The name of the histogram for recording time until a successful prerender.
+const char kPrerenderStartToReleaseContentsTime[] =
+    "Prerender.PrerenderStartToReleaseContentsTime";
 
 // Is this install selected for this particular experiment.
 bool IsPrerenderTabEvictionExperimentalGroup() {
@@ -90,6 +93,9 @@ bool IsPrerenderTabEvictionExperimentalGroup() {
 // Removes any scheduled prerender requests and resets |scheduledURL| to the
 // empty URL.
 - (void)removeScheduledPrerenderRequests;
+
+// Records metric on a successful prerender.
+- (void)recordReleaseMetrics;
 
 @end
 
@@ -149,6 +155,10 @@ bool IsPrerenderTabEvictionExperimentalGroup() {
   // Number of successful prerenders (i.e. the user viewed the prerendered page)
   // during the lifetime of this controller.
   int successfulPrerendersPerSessionCount_;
+
+  // Tracks the last time of the last attempt to load a |prerenderedURL_|. Used
+  // for UMA reporting of load durations.
+  base::TimeTicks startTime_;
 
   // Bridge to provide navigation policies for |webState_|.
   std::unique_ptr<web::WebStatePolicyDeciderBridge> policyDeciderBridge_;
@@ -244,12 +254,10 @@ bool IsPrerenderTabEvictionExperimentalGroup() {
 
 - (std::unique_ptr<web::WebState>)releasePrerenderContents {
   successfulPrerendersPerSessionCount_++;
-  UMA_HISTOGRAM_ENUMERATION(kPrerenderFinalStatusHistogramName,
-                            PRERENDER_FINAL_STATUS_USED,
-                            PRERENDER_FINAL_STATUS_MAX);
+  [self recordReleaseMetrics];
   [self removeScheduledPrerenderRequests];
   prerenderedURL_ = GURL();
-
+  startTime_ = base::TimeTicks();
   if (!webState_)
     return nullptr;
 
@@ -410,6 +418,8 @@ bool IsPrerenderTabEvictionExperimentalGroup() {
   // LoadIfNecessary is needed because the view is not created (but needed) when
   // loading the page. TODO(crbug.com/705819): Remove this call.
   webState_->GetNavigationManager()->LoadIfNecessary();
+
+  startTime_ = base::TimeTicks::Now();
 }
 
 - (void)destroyPreviewContents {
@@ -430,6 +440,7 @@ bool IsPrerenderTabEvictionExperimentalGroup() {
   webState_.reset();
 
   prerenderedURL_ = GURL();
+  startTime_ = base::TimeTicks();
 }
 
 - (void)schedulePrerenderCancel {
@@ -473,6 +484,16 @@ bool IsPrerenderTabEvictionExperimentalGroup() {
   if (handler) {
     handler(nil, nil);
   }
+}
+
+- (void)recordReleaseMetrics {
+  UMA_HISTOGRAM_ENUMERATION(kPrerenderFinalStatusHistogramName,
+                            PRERENDER_FINAL_STATUS_USED,
+                            PRERENDER_FINAL_STATUS_MAX);
+
+  DCHECK_NE(base::TimeTicks(), startTime_);
+  UMA_HISTOGRAM_TIMES(kPrerenderStartToReleaseContentsTime,
+                      base::TimeTicks::Now() - startTime_);
 }
 
 #pragma mark - CRWWebStateObserver
