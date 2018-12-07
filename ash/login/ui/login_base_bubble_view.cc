45bcbf76b85978c096e67bf65e316b320100a3a5
Takashi Sakamoto <tasak@google.com>
2018-12-07 03:49:55 +0000

    Revert "cros: Move most event/widget handling logic out of LoginBubble"
    This reverts commit dc49a52adde7a2cf5cfb2ed486b1c4153412de4d [formerly a61287973e8f4522ed673ac1a77de0dc56eb1bc8].
    
    Reason for revert: Suspect Failure single_process_mash_ash_unittests Failure ash_unittests:
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/linux-chromeos-rel/16943
    https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8927839167159249024/+/steps/single_process_mash_ash_unittests/0/logs/LoginExpandedPublicAccountViewTest.ChangeMenuSelection/0
    ---
    [ RUN      ] LoginExpandedPublicAccountViewTest.ChangeMenuSelection
    Received signal 11 SEGV_MAPERR ffffe03f000008fe
    #0 0x5631a3caedbf base::debug::StackTrace::StackTrace()
    #1 0x5631a3cae941 base::debug::(anonymous namespace)::StackDumpSignalHandler()
    #2 0x7faa9920f330 <unknown>
    #3 0x5631a3aafbbf ash::LoginBubble::~LoginBubble()
    #4 0x5631a3ab806c ash::RightPaneView::~RightPaneView()
    #5 0x5631a3ab810e ash::RightPaneView::~RightPaneView()
    #6 0x5631a40385d4 views::View::~View()
    #7 0x5631a3ab6df9 ash::LoginExpandedPublicAccountView::~LoginExpandedPublicAccountView()
    #8 0x5631a40385d4 views::View::~View()
    #9 0x5631a300afce ash::(anonymous namespace)::DragTestView::~DragTestView()
    #10 0x5631a4039b42 views::View::DoRemoveChildView()
    #11 0x5631a403a175 views::View::RemoveAllChildViews()
    #12 0x5631a4043b81 views::internal::RootView::~RootView()
    #13 0x5631a400ef0e views::MenuHostRootView::~MenuHostRootView()
    #14 0x5631a404615f views::Widget::~Widget()
    #15 0x5631a34d645e exo::(anonymous namespace)::ShellSurfaceWidget::~ShellSurfaceWidget()
    #16 0x5631a30c36f0 ash::LoginTestBase::TearDown()
    
    Original change's description:
    > cros: Move most event/widget handling logic out of LoginBubble
    > 
    > Changes made:
    > - Move the (keyboard/click/tap) event handling logic into a
    >   LoginBubbleHandler class.
    > - Move the widget handling into LoginBaseBubbleView itself
    > 
    > Bug: 912658
    > Change-Id: Ia1a9bf4b8d847ef74ef1716e73dfe1b16b8d19b2
    > Reviewed-on: https://chromium-review.googlesource.com/c/1366516
    > Commit-Queue: Quan Nguyen <qnnguyen@chromium.org>
    > Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#614544}
    
    TBR=jdufault@chromium.org,qnnguyen@chromium.org
    
    Change-Id: Ia9b9059f24f6aa6a8551c5c6bc5c76caafe592df
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 912658
    Reviewed-on: https://chromium-review.googlesource.com/c/1367070
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/master@{#614579}
    Former-commit-id: bb9ca465cac1a3ec4d0670844865b40efb003c11

--- ash/login/ui/login_base_bubble_view.cc
+++ ash/login/ui/login_base_bubble_view.cc
@@ -6,14 +6,7 @@
 
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/shell.h"
-#include "base/scoped_observer.h"
-#include "ui/aura/client/focus_change_observer.h"
-#include "ui/aura/client/focus_client.h"
-#include "ui/compositor/layer_animator.h"
-#include "ui/compositor/scoped_layer_animation_settings.h"
-#include "ui/events/event_handler.h"
 #include "ui/views/layout/box_layout.h"
-#include "ui/wm/core/coordinate_conversion.h"
 
 namespace ash {
 namespace {
@@ -30,105 +23,14 @@ constexpr int kBubbleTopMarginDp = 13;
 // Bottom margin of the bubble view.
 constexpr int kBubbleBottomMarginDp = 18;
 
-// The amount of time for bubble show/hide animation.
-constexpr base::TimeDelta kBubbleAnimationDuration =
-    base::TimeDelta::FromMilliseconds(300);
-
 }  // namespace
-
-// This class handles keyboard, mouse, and focus events, and dismisses the
-// associated bubble in response.
-class LoginBubbleHandler : public ui::EventHandler,
-                           public aura::client::FocusChangeObserver {
- public:
-  LoginBubbleHandler(LoginBaseBubbleView* bubble) : bubble_(bubble) {
-    Shell::Get()->AddPreTargetHandler(this);
-    focus_observer_.Add(
-        aura::client::GetFocusClient(Shell::GetPrimaryRootWindow()));
-  }
-
-  ~LoginBubbleHandler() override { Shell::Get()->RemovePreTargetHandler(this); }
-
-  // ui::EventHandler:
-  void OnMouseEvent(ui::MouseEvent* event) override {
-    if (event->type() == ui::ET_MOUSE_PRESSED)
-      ProcessPressedEvent(event->AsLocatedEvent());
-  }
-
-  void OnGestureEvent(ui::GestureEvent* event) override {
-    if (event->type() == ui::ET_GESTURE_TAP ||
-        event->type() == ui::ET_GESTURE_TAP_DOWN) {
-      ProcessPressedEvent(event->AsLocatedEvent());
-    }
-  }
-
-  void OnKeyEvent(ui::KeyEvent* event) override {
-    if (event->type() != ui::ET_KEY_PRESSED ||
-        event->key_code() == ui::VKEY_PROCESSKEY) {
-      return;
-    }
-
-    if (bubble_->GetBubbleOpener() && bubble_->GetBubbleOpener()->HasFocus())
-      return;
-
-    if (bubble_->GetWidget()->IsActive())
-      return;
-
-    if (!bubble_->IsPersistent())
-      bubble_->Hide();
-  }
-
-  // aura::client::FocusChangeObserver:
-  void OnWindowFocused(aura::Window* gained_focus,
-                       aura::Window* lost_focus) override {
-    if (!bubble_->GetWidget() || !bubble_->GetWidget()->IsVisible())
-      return;
-
-    if (gained_focus &&
-        bubble_->GetWidget()->GetNativeView()->Contains(gained_focus)) {
-      return;
-    }
-
-    if (!bubble_->IsPersistent())
-      bubble_->Hide();
-  }
-
- private:
-  void ProcessPressedEvent(const ui::LocatedEvent* event) {
-    gfx::Point screen_location = event->location();
-    ::wm::ConvertPointToScreen(static_cast<aura::Window*>(event->target()),
-                               &screen_location);
-
-    // Don't do anything with clicks inside the bubble.
-    if (bubble_->GetBoundsInScreen().Contains(screen_location))
-      return;
-
-    // Let the bubble opener handle clicks on itself.
-    if (bubble_->GetBubbleOpener() &&
-        bubble_->GetBubbleOpener()->GetBoundsInScreen().Contains(
-            screen_location)) {
-      return;
-    }
-
-    if (!bubble_->IsPersistent())
-      bubble_->Hide();
-  }
-
-  LoginBaseBubbleView* bubble_;
-
-  ScopedObserver<aura::client::FocusClient, aura::client::FocusChangeObserver>
-      focus_observer_{this};
-
-  DISALLOW_COPY_AND_ASSIGN(LoginBubbleHandler);
-};
 
 LoginBaseBubbleView::LoginBaseBubbleView(views::View* anchor_view)
     : LoginBaseBubbleView(anchor_view, nullptr) {}
 
 LoginBaseBubbleView::LoginBaseBubbleView(views::View* anchor_view,
                                          aura::Window* parent_window)
-    : BubbleDialogDelegateView(anchor_view, views::BubbleBorder::NONE),
-      bubble_handler_(std::make_unique<LoginBubbleHandler>(this)) {
+    : BubbleDialogDelegateView(anchor_view, views::BubbleBorder::NONE) {
   set_margins(gfx::Insets(kBubbleTopMarginDp, kBubbleHorizontalMarginDp,
                           kBubbleBottomMarginDp, kBubbleHorizontalMarginDp));
   set_color(SK_ColorBLACK);
@@ -143,27 +45,6 @@ LoginBaseBubbleView::LoginBaseBubbleView(views::View* anchor_view,
 }
 
 LoginBaseBubbleView::~LoginBaseBubbleView() = default;
-
-void LoginBaseBubbleView::Show() {
-  views::Widget* widget = GetWidget();
-
-  if (!widget)
-    widget = views::BubbleDialogDelegateView::CreateBubble(this);
-
-  widget->ShowInactive();
-  widget->StackAtTop();
-
-  ScheduleAnimation(true /*visible*/);
-
-  // Tell ChromeVox to read bubble contents.
-  NotifyAccessibilityEvent(ax::mojom::Event::kAlert,
-                           true /*send_native_event*/);
-}
-
-void LoginBaseBubbleView::Hide() {
-  if (GetWidget())
-    ScheduleAnimation(false /*visible*/);
-}
 
 LoginButton* LoginBaseBubbleView::GetBubbleOpener() const {
   return nullptr;
@@ -190,80 +71,11 @@ int LoginBaseBubbleView::GetDialogButtons() const {
   return ui::DIALOG_BUTTON_NONE;
 }
 
-void LoginBaseBubbleView::OnLayerAnimationEnded(
-    ui::LayerAnimationSequence* sequence) {
-  layer()->GetAnimator()->RemoveObserver(this);
-  GetWidget()->Hide();
-}
-
 gfx::Size LoginBaseBubbleView::CalculatePreferredSize() const {
   gfx::Size size;
   size.set_width(kBubbleTotalWidthDp);
   size.set_height(GetHeightForWidth(kBubbleTotalWidthDp));
   return size;
-}
-
-void LoginBaseBubbleView::OnWidgetVisibilityChanged(views::Widget* widget,
-                                                    bool visible) {
-  if (visible)
-    EnsureInScreen();
-}
-
-void LoginBaseBubbleView::OnWidgetBoundsChanged(views::Widget* widget,
-                                                const gfx::Rect& new_bounds) {
-  EnsureInScreen();
-}
-
-void LoginBaseBubbleView::ScheduleAnimation(bool visible) {
-  if (GetBubbleOpener()) {
-    GetBubbleOpener()->AnimateInkDrop(visible
-                                          ? views::InkDropState::ACTIVATED
-                                          : views::InkDropState::DEACTIVATED,
-                                      nullptr /*event*/);
-  }
-
-  layer()->GetAnimator()->StopAnimating();
-
-  float opacity_start = 0.0f;
-  float opacity_end = 1.0f;
-  if (!visible)
-    std::swap(opacity_start, opacity_end);
-
-  // We only need to handle animation ending if we're hiding the bubble.
-  if (!visible)
-    layer()->GetAnimator()->AddObserver(this);
-
-  layer()->SetOpacity(opacity_start);
-  {
-    ui::ScopedLayerAnimationSettings settings(layer()->GetAnimator());
-    settings.SetTransitionDuration(kBubbleAnimationDuration);
-    settings.SetTweenType(visible ? gfx::Tween::EASE_OUT : gfx::Tween::EASE_IN);
-    settings.SetPreemptionStrategy(
-        ui::LayerAnimator::IMMEDIATELY_ANIMATE_TO_NEW_TARGET);
-    layer()->SetOpacity(opacity_end);
-  }
-}
-
-void LoginBaseBubbleView::EnsureInScreen() {
-  DCHECK(GetWidget());
-
-  const gfx::Rect view_bounds = GetBoundsInScreen();
-  const gfx::Rect work_area =
-      display::Screen::GetScreen()
-          ->GetDisplayNearestWindow(GetWidget()->GetNativeWindow())
-          .work_area();
-
-  int horizontal_offset = 0;
-
-  // If the widget extends past the right side of the screen, make it go to
-  // the left instead.
-  if (work_area.right() < view_bounds.right()) {
-    horizontal_offset = -view_bounds.width();
-  }
-
-  set_anchor_view_insets(
-      anchor_view_insets().Offset(gfx::Vector2d(horizontal_offset, 0)));
-  OnAnchorBoundsChanged();
 }
 
 }  // namespace ash
