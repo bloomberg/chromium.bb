cd18b63ca76609489378b54c03fa965d6887112d
Toni Barzic <tbarzic@chromium.org>
2018-12-07 19:17:39 +0000

    Disable lockScreen permission on dev
    This was used for testing the lock screen apps apis before note taking
    app supported lock screen. It should not be needed anymore given that
    the actual note taking app can be used for testing.
    
    Change-Id: I3c678018bfb4e1dacb537fb7e3c58dcad2a4b92c
    Reviewed-on: https://chromium-review.googlesource.com/c/1347770
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614775}
    Former-commit-id: f5c236677bf6f399e47caa13e359cb19156bd865

--- chrome/browser/chromeos/lock_screen_apps/lock_screen_profile_creator_impl_unittest.cc
+++ chrome/browser/chromeos/lock_screen_apps/lock_screen_profile_creator_impl_unittest.cc
@@ -10,6 +10,7 @@
 
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/memory/ref_counted.h"
@@ -30,11 +31,13 @@
 #include "chrome/test/base/testing_profile.h"
 #include "components/arc/arc_service_manager.h"
 #include "components/arc/arc_session.h"
+#include "components/crx_file/id_util.h"
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing/common/safe_browsing_prefs.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_builder.h"
+#include "extensions/common/switches.h"
 #include "extensions/common/value_builder.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -208,6 +211,9 @@ class LockScreenProfileCreatorImplTest : public testing::Test {
   ~LockScreenProfileCreatorImplTest() override {}
 
   void SetUp() override {
+    base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+        extensions::switches::kWhitelistedExtensionID,
+        crx_file::id_util::GenerateId("test_app"));
     ASSERT_TRUE(user_data_dir_.CreateUniqueTempDir());
 
     auto profile_manager =
@@ -275,6 +281,7 @@ class LockScreenProfileCreatorImplTest : public testing::Test {
 
     return extensions::ExtensionBuilder()
         .SetManifest(manifest_builder.Build())
+        .SetID(crx_file::id_util::GenerateId("test_app"))
         .Build();
   }
 
