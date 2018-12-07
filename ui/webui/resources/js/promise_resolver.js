6b3dcd633d46658bb5383fad98c62878bc2db53c
Dan Beam <dbeam@chromium.org>
2018-12-07 01:28:22 +0000

    WebUI: Add PromiseResolver#isFulfilled to avoid double resolution/rejection
    R=dpapad@chromium.org
    BUG=855259
    
    Change-Id: I42160189ba60c849bcb9cd9578f5e27c47c02791
    Reviewed-on: https://chromium-review.googlesource.com/c/1364693
    Commit-Queue: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614561}
    Former-commit-id: 6eba080dcab3baea32ab38db4ced27c82eba927e

--- ui/webui/resources/js/promise_resolver.js
+++ ui/webui/resources/js/promise_resolver.js
@@ -27,14 +27,31 @@ function PromiseResolver() {
   /** @private {function(*=): void} */
   this.reject_;
 
+  /** @private {boolean} */
+  this.isFulfilled_ = false;
+
   /** @private {!Promise<T>} */
   this.promise_ = new Promise(function(resolve, reject) {
-    this.resolve_ = resolve;
-    this.reject_ = reject;
+    this.resolve_ = /** @param {T=} resolution */ function(resolution) {
+      resolve(resolution);
+      this.isFulfilled_ = true;
+    };
+    this.reject_ = /** @param {*=} reason */ function(reason) {
+      reject(reason);
+      this.isFulfilled_ = true;
+    };
   }.bind(this));
 }
 
 PromiseResolver.prototype = {
+  /** @return {boolean} Whether this resolver has been resolved or rejected. */
+  get isFulfilled() {
+    return this.isFulfilled_;
+  },
+  set isFulfilled(i) {
+    assertNotReached();
+  },
+
   /** @return {!Promise<T>} */
   get promise() {
     return this.promise_;
