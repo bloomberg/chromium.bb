1766bf7e64658e03083d2c6d01045df511e50d38
Bence Béky <bnc@chromium.org>
2018-12-07 13:03:45 +0000

    Introduce HTTP2_RELOADABLE_FLAG_COUNT.
    Introduce HTTP2_RELOADABLE_FLAG_COUNT and remove platform-specific
    prefix.  This way HTTP2_RELOADABLE_FLAG_COUNT syntax will be identical
    across platforms.
    
    Also remove unused HTTP2_FLAG_COUNT_N and HTTP2_CODE_COUNT* macros.
    
    This CL merges internal change 224404468 by bnc.
    
    Bug: 488484
    
    Change-Id: Ib1c29d1c59101af7a60c85805ed53ae612212b25
    Reviewed-on: https://chromium-review.googlesource.com/c/1366576
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614678}
    Former-commit-id: c24214e5cb5621134e1123f66795ca8fb4b992b5

--- net/third_party/http2/platform/impl/http2_flag_utils_impl.h
+++ net/third_party/http2/platform/impl/http2_flag_utils_impl.h
@@ -7,16 +7,7 @@
 
 #include "base/logging.h"
 
-#define HTTP2_FLAG_COUNT_IMPL(flag) \
+#define HTTP2_RELOADABLE_FLAG_COUNT_IMPL(flag) \
   DVLOG(1) << "FLAG_" #flag ": " << FLAGS_##flag
-#define HTTP2_FLAG_COUNT_N_IMPL(flag, instance, total) \
-  HTTP2_FLAG_COUNT_IMPL(flag)
-
-#define HTTP2_CODE_COUNT_IMPL(name) \
-  do {                              \
-  } while (0)
-#define HTTP2_CODE_COUNT_N_IMPL(name, instance, total) \
-  do {                                                 \
-  } while (0)
 
 #endif  // NET_THIRD_PARTY_HTTP2_PLATFORM_IMPL_HTTP2_FLAG_UTILS_IMPL_H_
