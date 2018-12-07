0959a8ceb9c3162ba207f76f33084a5213f5522b
Boris Sazonov <bsazonov@chromium.org>
2018-12-07 12:00:44 +0000

    [Signin][Android] Add tests for SigninManager.runAfterOperationInProgress
    This CL is a follow-up to https://crrev.com/c/1345069, which added
    isOperationInProgress and runAfterOperationInProgress methods to
    SigninManager. This CL modifies SigninManager to make it testable (by
    making some methods non-static and adding a constructor with explicit
    dependencies) and adds tests for the new methods.
    
    Bug: 903657, 903658, 906188, 906193, 906788
    Change-Id: Iba91ca038898d7abd015b4ab194bc26edbe34479
    Reviewed-on: https://chromium-review.googlesource.com/c/1350908
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#614673}
    Former-commit-id: 958e6783640e39cc1f5aa49cd17e21037e4bbb31

--- chrome/android/junit/src/org/chromium/chrome/browser/signin/SigninManagerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/signin/SigninManagerTest.java
@@ -4,15 +4,24 @@
 
 package org.chromium.chrome.browser.signin;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+
+import android.accounts.Account;
+import android.content.Context;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -20,9 +29,13 @@ import org.junit.runner.RunWith;
 import org.mockito.Spy;
 import org.robolectric.annotation.Config;
 
+import org.chromium.base.ContextUtils;
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.components.signin.AccountManagerFacade;
 import org.chromium.components.sync.AndroidSyncSettings;
 import org.chromium.components.sync.test.util.MockSyncContentResolverDelegate;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 /** Tests for {@link SigninManager}. */
 @RunWith(BaseRobolectricTestRunner.class)
@@ -33,6 +46,11 @@ public class SigninManagerTest {
      * Other native* methods can be overridden using Spy.
      */
     static class TestableSigninManager extends SigninManager {
+        TestableSigninManager(Context context, AccountTrackerService accountTrackerService,
+                AndroidSyncSettings androidSyncSettings) {
+            super(context, accountTrackerService, androidSyncSettings);
+        }
+
         @Override
         long nativeInit() {
             return 0;
@@ -44,12 +62,17 @@ public class SigninManagerTest {
         }
     }
 
+    private AccountTrackerService mAccountTrackerService;
     @Spy
     private TestableSigninManager mSigninManager;
 
     @Before
     public void setUp() {
-        mSigninManager = spy(new TestableSigninManager());
+        mAccountTrackerService = mock(AccountTrackerService.class);
+        AndroidSyncSettings androidSyncSettings = mock(AndroidSyncSettings.class);
+
+        mSigninManager = spy(new TestableSigninManager(
+                ContextUtils.getApplicationContext(), mAccountTrackerService, androidSyncSettings));
 
         // SinginManager interacts with AndroidSyncSettings, but its not the focus
         // of this test. Using MockSyncContentResolver reduces burden of test setup.
@@ -154,5 +177,61 @@ public class SigninManagerTest {
         // Sign-out should only clear the service worker cache when the domain is null.
         verify(mSigninManager, never()).nativeWipeProfileData(anyLong());
         verify(mSigninManager, times(1)).nativeWipeGoogleServiceWorkerCaches(anyLong());
+    }
+
+    @Test
+    public void callbackNotifiedWhenNoOperationIsInProgress() {
+        assertFalse(mSigninManager.isOperationInProgress());
+
+        AtomicInteger callCount = new AtomicInteger(0);
+        mSigninManager.runAfterOperationInProgress(callCount::incrementAndGet);
+        assertEquals(1, callCount.get());
+    }
+
+    @Test
+    public void callbackNotifiedOnSignout() {
+        doAnswer(invocation -> {
+            mSigninManager.onNativeSignOut();
+            return null;
+        }).when(mSigninManager).nativeSignOut(anyLong(), anyInt());
+        doReturn(null).when(mSigninManager).nativeGetManagementDomain(anyLong());
+        doNothing().when(mSigninManager).nativeWipeGoogleServiceWorkerCaches(anyLong());
+
+        mSigninManager.signOut(SignoutReason.SIGNOUT_TEST);
+        assertTrue(mSigninManager.isOperationInProgress());
+        AtomicInteger callCount = new AtomicInteger(0);
+        mSigninManager.runAfterOperationInProgress(callCount::incrementAndGet);
+        assertEquals(0, callCount.get());
+
+        mSigninManager.onProfileDataWiped();
+        assertFalse(mSigninManager.isOperationInProgress());
+        assertEquals(1, callCount.get());
+    }
+
+    @Test
+    public void callbackNotifiedOnSignin() {
+        // No need to seed accounts to the native code.
+        doReturn(true).when(mAccountTrackerService).checkAndSeedSystemAccounts();
+        // Request that policy is loaded. It will pause sign-in until onPolicyCheckedBeforeSignIn is
+        // invoked.
+        doReturn(true).when(mSigninManager).nativeShouldLoadPolicyForUser(any());
+        doNothing().when(mSigninManager).nativeCheckPolicyBeforeSignIn(anyLong(), any());
+
+        doReturn(true).when(mSigninManager).isSigninSupported();
+        doNothing().when(mSigninManager).nativeOnSignInCompleted(anyLong(), any());
+        doNothing().when(mSigninManager).logInSignedInUser();
+
+        mSigninManager.onFirstRunCheckDone(); // Allow sign-in.
+
+        Account account = new Account("test@gmail.com", AccountManagerFacade.GOOGLE_ACCOUNT_TYPE);
+        mSigninManager.signIn(account, null, null);
+        assertTrue(mSigninManager.isOperationInProgress());
+        AtomicInteger callCount = new AtomicInteger(0);
+        mSigninManager.runAfterOperationInProgress(callCount::incrementAndGet);
+        assertEquals(0, callCount.get());
+
+        mSigninManager.onPolicyCheckedBeforeSignIn(null); // Test user is unmanaged.
+        assertFalse(mSigninManager.isOperationInProgress());
+        assertEquals(1, callCount.get());
     }
 }
