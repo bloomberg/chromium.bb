82e5e351d9ae4d656f73d4cc41c23c1019149a91
Nicolas Ouellet-payeur <nicolaso@chromium.org>
2018-12-10 22:01:13 +0000

    [WebUI] Add <managed-footnote> component
    This Polymer component can be used on WebUI pages as a disclosure for
    managed users. It shows an enterprise icon and the text 'Your browser is
    managed by your organization'. It also has a hyperlink that points to a
    Help Center article.
    
    Bug: 893628
    Change-Id: Id9e5e0b3552943c260ac251c3b643dd963b5809a
    Reviewed-on: https://chromium-review.googlesource.com/c/1352438
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615273}
    Former-commit-id: 8516b846cd6f074986e1067837182c1041720f18

---
+++ chrome/test/data/webui/cr_components/managed_footnote_test.js
@@ -0,0 +1,60 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+suite('managed-footnote', function() {
+  suiteSetup(function() {
+    loadTimeData.data = {
+      isManaged: false,
+      managedByOrg: '',
+    };
+  });
+
+  setup(function() {
+    PolymerTest.clearBody();
+  });
+
+  /**
+   * Resets loadTimeData to the parameters, inserts a <managed-footnote> element
+   * in the DOM, and returns it.
+   * @param {boolean} isManaged Whether the footnote should be visible.
+   * @param {string} message String to display inside the element, before href
+   *     substitution.
+   * @return {HTMLElement}
+   */
+  function setupTestElement(isManaged, message) {
+    loadTimeData.overrideValues({
+      isManaged: isManaged,
+      managedByOrg: message,
+    });
+    const footnote = document.createElement('managed-footnote');
+    document.body.appendChild(footnote);
+    Polymer.dom.flush();
+    return footnote;
+  }
+
+  test('Hidden When isManaged Is False', function() {
+    const footnote = setupTestElement(false, '');
+    assertEquals('none', getComputedStyle(footnote).display);
+  });
+
+  test('Reads Attributes From loadTimeData', function() {
+    const message = 'the quick brown fox jumps over the lazy dog';
+    const footnote = setupTestElement(true, message);
+    assertNotEquals('none', getComputedStyle(footnote).display);
+    assertTrue(footnote.shadowRoot.textContent.includes(message));
+  });
+
+  test('Substitutes URL', function() {
+    const message =
+        'Your <a target="_blank" href="$1">browser is managed</a> by your ' +
+        'organization';
+    const targetMessage = 'Your browser is managed by your organization';
+    const supportUrl = 'https://support.google.com/chromebook/answer/1331549';
+
+    const footnote = setupTestElement(true, message);
+    assertTrue(footnote.shadowRoot.textContent.includes(targetMessage));
+    // The <a> element should have the right link.
+    assertEquals(supportUrl, footnote.$$('a').href);
+  });
+});
---
+++ ui/webui/resources/cr_components/managed_footnote/managed_footnote.js
@@ -0,0 +1,61 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Polymer element for indicating that this user is managed by
+ * their organization. This component uses the |isManaged| boolean in
+ * loadTimeData, and the |managedByOrg| i18n string.
+ *
+ * If |isManaged| is false, this component is hidden. If |isManaged| is true, it
+ * becomes visible.
+ */
+
+(function() {
+/**
+ * URL of the help article for the clickable link.
+ * @type {string}
+ */
+// TODO(nicolaso): Use a p-link instead, once it's available. b/117655761
+const HELP_ARTICLE_URL = 'https://support.google.com/chromebook/answer/1331549';
+
+Polymer({
+  is: 'managed-footnote',
+
+  behaviors: [I18nBehavior],
+
+  properties: {
+    /**
+     * Whether the browser is managed by their organization through enterprise
+     * policies.
+     * @private
+     */
+    isManaged_: {
+      reflectToAttribute: true,
+      type: Boolean,
+      value: function() {
+        return loadTimeData.getBoolean('isManaged');
+      },
+    },
+
+    /**
+     * Localized message to display in the footnote. May contain an <a>
+     * element.
+     * @private
+     */
+    message_: String,
+  },
+
+  /** @override */
+  ready: function() {
+    this.message_ = this.i18nAdvanced('managedByOrg', {
+      substitutions: [HELP_ARTICLE_URL],
+      tags: ['a'],
+      attrs: {
+        target: (node, v) => v === '_blank',
+        href: (node, v) => v === HELP_ARTICLE_URL,
+      },
+    });
+  },
+});
+})();
