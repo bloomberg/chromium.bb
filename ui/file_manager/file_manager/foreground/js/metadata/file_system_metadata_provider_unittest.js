ce4ed355cffd92a76d137007df0c68de7e5b02f5
Noel Gordon <noel@chromium.org>
2018-12-10 07:29:19 +0000

    Closure compile file_system_metadata_provider_unittest
     - remove file_system_metadata_provider_unittest.html
     - add BUILD rules to auto-generate and compile this unittest
     - fix Closure compiler errors
    
    No change in test behavior.
    
    Bug: 913307
    Change-Id: Ic8efcd922b9eab319e3369c9e334cd2e7365e9f4
    Reviewed-on: https://chromium-review.googlesource.com/c/1369484
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615064}
    Former-commit-id: df3ddf670536f2bfb7f100faa65d61a9dfe01730

--- ui/file_manager/file_manager/foreground/js/metadata/file_system_metadata_provider_unittest.js
+++ ui/file_manager/file_manager/foreground/js/metadata/file_system_metadata_provider_unittest.js
@@ -2,21 +2,27 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var entryA = {
-  toURL: function() { return "filesystem://A"; },
+/** @const {!Entry} */
+const entryA = /** @type {!Entry} */ ({
+  toURL: function() {
+    return "filesystem://A";
+  },
   getMetadata: function(fulfill, reject) {
     Promise.resolve({modificationTime: new Date(2015, 1, 1), size: 1024}).then(
         fulfill, reject);
   }
-};
+});
 
-var entryB = {
-  toURL: function() { return "filesystem://B"; },
+/** @const {!Entry} */
+const entryB = /** @type {!Entry} */ ({
+  toURL: function() {
+    return "filesystem://B";
+  },
   getMetadata: function(fulfill, reject) {
     Promise.resolve({modificationTime: new Date(2015, 2, 2), size: 2048}).then(
         fulfill, reject);
   }
-};
+});
 
 function testFileSystemMetadataProviderBasic(callback) {
   var provider = new FileSystemMetadataProvider();
@@ -32,14 +38,14 @@ function testFileSystemMetadataProviderBasic(callback) {
         new Date(2015, 1, 1).toString(),
         results[0].modificationTime.toString());
     assertEquals(1024, results[0].size);
-    assertTrue(results[0].present);
-    assertTrue(results[0].availableOffline);
+    assertTrue(!!results[0].present);
+    assertTrue(!!results[0].availableOffline);
     assertEquals(
         new Date(2015, 2, 2).toString(),
         results[1].modificationTime.toString());
     assertEquals(2048, results[1].size);
-    assertTrue(results[1].present);
-    assertTrue(results[1].availableOffline);
+    assertTrue(!!results[1].present);
+    assertTrue(!!results[1].availableOffline);
   }), callback);
 }
 
