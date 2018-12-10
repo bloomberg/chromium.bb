c27b3738f56dca8e0473df58463d51b9774c9436
Dana Fried <dfried@chromium.org>
2018-12-10 19:59:23 +0000

    Base CL for new near-universal "flex layout".
    As per previously discussed design:
    https://docs.google.com/document/d/1sOcsB2smt4pBzPgwAAtlh5CM4kSSYDxW4IIjx1aO010
    
    Has unit tests and example based on the existing BoxLayout example.
    
    Open questions:
     - Do we want to put the new layout stuff in its own namespace (views::layout)?
    
    Followup CLs in the pipe:
     - Use FlexLayout for toolbar layout, vastly simplifying toolbar_view.cc
     - Add concept of "overflow" child views that appear conditionally.
     - Use FlexLayout for tab handles, vastly simplifying tab.cc
    
    Future work:
     - Use layout manager for bookmarks bar instead of bespoke layout.
     - Replace existing uses of BoxLayout and GridLayout that lay out a single row
       or column with the new, unified layout manager.
     - Rename TOOLBAR_STANDARD_SPACING to something sensible.
     - Fix View::GetContentsBounds() to actually call GetInsets().
     - Remove ToolbarButton::SetLayoutInsetDelta() and make logic around switching
       to/from touch UI sane.
     - Enhance Views properties system to allow for larger value types (insets,
       flex rules, etc.) without having to explicitly store pointers.
    
    Bug: 898632
    Change-Id: I38698b01dc5afdd143ba350f2c53f1062baec3a3
    Reviewed-on: https://chromium-review.googlesource.com/c/1331040
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615207}
    Former-commit-id: 9367b837898b8e6960a6c916436094d0d9977b9b

--- ui/views/examples/examples_window.cc
+++ ui/views/examples/examples_window.cc
@@ -27,6 +27,7 @@
 #include "ui/views/examples/checkbox_example.h"
 #include "ui/views/examples/combobox_example.h"
 #include "ui/views/examples/dialog_example.h"
+#include "ui/views/examples/flex_layout_example.h"
 #include "ui/views/examples/label_example.h"
 #include "ui/views/examples/link_example.h"
 #include "ui/views/examples/menu_example.h"
@@ -68,6 +69,7 @@ ExampleVector CreateExamples() {
   examples.push_back(std::make_unique<CheckboxExample>());
   examples.push_back(std::make_unique<ComboboxExample>());
   examples.push_back(std::make_unique<DialogExample>());
+  examples.push_back(std::make_unique<FlexLayoutExample>());
   examples.push_back(std::make_unique<LabelExample>());
   examples.push_back(std::make_unique<LinkExample>());
   examples.push_back(std::make_unique<MenuExample>());
