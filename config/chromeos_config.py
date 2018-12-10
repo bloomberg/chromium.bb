768c73895b2f3e77b31396bd7adfcb5bf6ccd640
Zhuohao Lee <zhuohao@chromium.org>
2018-12-10 14:49:31 +0800

    rammus: add mapping for factory-rammus-11289.B
    BUG=b:111673473,b:113832925
    TEST=None
    
    Change-Id: I1fecbcf334189d308d958e94278c5dfa9908f8fb
    Reviewed-on: https://chromium-review.googlesource.com/1369472
    Commit-Ready: Zhuohao Lee <zhuohao@chromium.org>
    Tested-by: Zhuohao Lee <zhuohao@chromium.org>
    Reviewed-by: Don Garrett <dgarrett@chromium.org>
    Reviewed-by: YH Lin <yueherngl@chromium.org>

--- config/chromeos_config.py
+++ config/chromeos_config.py
@@ -2979,6 +2979,7 @@ def FactoryBuilders(site_config, boards_dict, ge_build_config):
       (None, 'factory-nami-10715.B', ['nami', 'kalista']),
       (None, 'factory-nocturne-11066.B', ['nocturne']),
       (None, 'factory-grunt-11164.B', ['grunt']),
+      (None, 'factory-rammus-11289.B', ['rammus']),
   ]
 
   # Requires that you set boards, and workspace_branch.
