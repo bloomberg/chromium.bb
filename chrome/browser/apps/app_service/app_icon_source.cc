33f79caa29da676feb7d41428bfce6a0f61b5eab
Melissa Zhang <melzhang@google.com>
2018-12-10 06:25:09 +0000

    Implement chrome://app-icon to serve app icons to WebUI.
    This CL adds an app icon page at
    chrome://app-icon/<app-id>/<icon-size> when the feature AppManagement
    is enabled.
    
    A request must be made to chrome://apps first to initialise the
    URLDataSource from which app icons are retrieved.
    
    This involves:
    - Adding AppIconSource
    - Initialising URLDataSource in App Management
    - Creating the url in webui urls
    
    BUG=912371
    
    Change-Id: Iba29dbd4fff35b60630fd2ec4337e8a94f9bd741
    Reviewed-on: https://chromium-review.googlesource.com/c/1361752
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Melissa Zhang <melzhang@google.com>
    Cr-Commit-Position: refs/heads/master@{#615054}
    Former-commit-id: 8f6d7316365629613e3e9b1635df64e1aedfe21e

---
+++ chrome/browser/apps/app_service/app_icon_source.cc
@@ -0,0 +1,121 @@
+// Copyright (c) 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/apps/app_service/app_icon_source.h"
+
+#include <cmath>
+#include <string>
+
+#include "base/bind.h"
+#include "base/callback.h"
+#include "base/numerics/safe_conversions.h"
+#include "base/strings/string_number_conversions.h"
+#include "base/strings/string_split.h"
+#include "base/strings/string_util.h"
+#include "chrome/browser/apps/app_service/app_service_proxy.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/common/url_constants.h"
+#include "extensions/grit/extensions_browser_resources.h"
+#include "ui/base/resource/resource_bundle.h"
+#include "ui/display/display.h"
+#include "ui/display/screen.h"
+#include "url/gurl.h"
+
+namespace apps {
+
+namespace {
+
+float GetDeviceScaleFactor() {
+  display::Screen* screen = display::Screen::GetScreen();
+  if (!screen) {
+    return 1.0f;
+  }
+  return screen->GetPrimaryDisplay().device_scale_factor();
+}
+
+int ConvertPxToDip(int px) {
+  return base::saturated_cast<int>(
+      std::floor(static_cast<float>(px) / GetDeviceScaleFactor()));
+}
+
+void LoadDefaultImage(const content::URLDataSource::GotDataCallback& callback) {
+  base::StringPiece contents =
+      ui::ResourceBundle::GetSharedInstance().GetRawDataResourceForScale(
+          IDR_APP_DEFAULT_ICON, ui::SCALE_FACTOR_100P);
+
+  base::RefCountedBytes* image_bytes = new base::RefCountedBytes();
+  image_bytes->data().assign(contents.data(),
+                             contents.data() + contents.size());
+  callback.Run(image_bytes);
+}
+
+void RunCallback(const content::URLDataSource::GotDataCallback& callback,
+                 apps::mojom::IconValuePtr iv) {
+  if (!iv->compressed.has_value()) {
+    LoadDefaultImage(callback);
+    return;
+  }
+  base::RefCountedBytes* image_bytes =
+      new base::RefCountedBytes(iv->compressed.value());
+  callback.Run(image_bytes);
+}
+
+}  // namespace
+
+AppIconSource::AppIconSource(Profile* profile) : profile_(profile) {}
+
+AppIconSource::~AppIconSource() = default;
+
+bool AppIconSource::AllowCaching() const {
+  // Should not be cached as caching is performed by proxy.
+  return false;
+}
+
+std::string AppIconSource::GetMimeType(const std::string&) const {
+  // We need to explicitly return a mime type, otherwise if the user tries to
+  // drag the image they get no extension.
+  return "image/png";
+}
+
+std::string AppIconSource::GetSource() const {
+  return chrome::kChromeUIAppIconHost;
+}
+
+void AppIconSource::StartDataRequest(
+    const std::string& path,
+    const content::ResourceRequestInfo::WebContentsGetter& wc_getter,
+    const content::URLDataSource::GotDataCallback& callback) {
+  std::string path_lower = base::ToLowerASCII(path);
+  std::vector<std::string> path_parts = base::SplitString(
+      path_lower, "/", base::TRIM_WHITESPACE, base::SPLIT_WANT_ALL);
+
+  // Check data exists, load default image if it doesn't.
+  if (path_lower.empty() || path_parts.size() < 2) {
+    LoadDefaultImage(callback);
+    return;
+  }
+
+  // Check data is correct type, load default image if not.
+  const std::string app_id = path_parts[0];
+  std::string size_param = path_parts[1];
+  int size = 0;
+  if (!base::StringToInt(size_param, &size)) {
+    LoadDefaultImage(callback);
+    return;
+  }
+  int size_in_dip = ConvertPxToDip(size);
+
+  apps::AppServiceProxy* app_service_proxy =
+      apps::AppServiceProxy::Get(profile_);
+  if (!app_service_proxy) {
+    LoadDefaultImage(callback);
+    return;
+  }
+
+  app_service_proxy->LoadIcon(app_id, apps::mojom::IconCompression::kCompressed,
+                              size_in_dip,
+                              base::BindOnce(&RunCallback, callback));
+}
+
+}  // namespace apps
