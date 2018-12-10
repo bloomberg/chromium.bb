9e0aef117ba8a10ebad10d64e0b646c0daeb84df
Steven Bennetts <stevenjb@chromium.org>
2018-12-10 18:35:33 +0000

    SetTimeDialog: Use SystemWebDialogDelegate
    This makes the SetTimeDialog consistent with other system dialogs
    when opened from the system tray, and simplifies the class.
    
    This CL also:
    * Removes the unused |use_minimal_style| arg to
      SystemWebDialogDelegate::ShowSystemDialog()
    * Adds a |parent| argument to ShowSystemDialog() to allow the time
      dialog to be parented when opened from the Settings UI, instead of
      AlwaysOnTop or modal.
    
    Bug: 912998
    Change-Id: I6f075773a04c862f58bd4be16e0878d7ea8f1d4f
    Reviewed-on: https://chromium-review.googlesource.com/c/1368281
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615182}
    Former-commit-id: 061816c5c3c86061fe4f61526b7be9756ebc4f31

--- chrome/browser/chromeos/set_time_dialog.cc
+++ chrome/browser/chromeos/set_time_dialog.cc
@@ -4,15 +4,10 @@
 
 #include "chrome/browser/chromeos/set_time_dialog.h"
 
-#include "ash/public/cpp/shell_window_ids.h"
 #include "base/metrics/user_metrics.h"
-#include "chrome/browser/profiles/profile_manager.h"
-#include "chrome/browser/ui/browser_dialogs.h"
+#include "base/strings/string16.h"
 #include "chrome/common/url_constants.h"
 #include "ui/gfx/geometry/size.h"
-
-using content::WebContents;
-using content::WebUIMessageHandler;
 
 namespace chromeos {
 
@@ -24,69 +19,20 @@ const int kDefaultHeight = 235;
 }  // namespace
 
 // static
-void SetTimeDialog::ShowDialogInParent(gfx::NativeWindow parent) {
-  DCHECK(parent);
+void SetTimeDialog::ShowDialog(gfx::NativeWindow parent) {
   base::RecordAction(base::UserMetricsAction("Options_SetTimeDialog_Show"));
-  chrome::ShowWebDialog(parent, ProfileManager::GetActiveUserProfile(),
-                        new SetTimeDialog());
+  auto* dialog = new SetTimeDialog();
+  dialog->ShowSystemDialog(parent);
 }
 
-// static
-void SetTimeDialog::ShowDialogInContainer(int container_id) {
-  DCHECK_NE(container_id, ash::kShellWindowId_Invalid);
-  base::RecordAction(base::UserMetricsAction("Options_SetTimeDialog_Show"));
-  chrome::ShowWebDialogInContainer(container_id,
-                                   ProfileManager::GetActiveUserProfile(),
-                                   new SetTimeDialog());
-}
+SetTimeDialog::SetTimeDialog()
+    : SystemWebDialogDelegate(GURL(chrome::kChromeUISetTimeURL),
+                              base::string16() /* title */) {}
 
-SetTimeDialog::SetTimeDialog() {
-}
-
-SetTimeDialog::~SetTimeDialog() {
-}
-
-ui::ModalType SetTimeDialog::GetDialogModalType() const {
-  return ui::MODAL_TYPE_SYSTEM;
-}
-
-base::string16 SetTimeDialog::GetDialogTitle() const {
-  return base::string16();
-}
-
-GURL SetTimeDialog::GetDialogContentURL() const {
-  return GURL(chrome::kChromeUISetTimeURL);
-}
-
-void SetTimeDialog::GetWebUIMessageHandlers(
-    std::vector<WebUIMessageHandler*>* handlers) const {
-}
+SetTimeDialog::~SetTimeDialog() = default;
 
 void SetTimeDialog::GetDialogSize(gfx::Size* size) const {
   size->SetSize(kDefaultWidth, kDefaultHeight);
-}
-
-std::string SetTimeDialog::GetDialogArgs() const {
-  return std::string();
-}
-
-void SetTimeDialog::OnDialogClosed(const std::string& json_retval) {
-  delete this;
-}
-
-void SetTimeDialog::OnCloseContents(WebContents* source,
-                                    bool* out_close_dialog) {
-  *out_close_dialog = true;
-}
-
-bool SetTimeDialog::ShouldShowDialogTitle() const {
-  return false;
-}
-
-bool SetTimeDialog::HandleContextMenu(
-    const content::ContextMenuParams& params) {
-  // Disable context menu.
-  return true;
 }
 
 }  // namespace chromeos
--- chrome/browser/chromeos/set_time_dialog.h
+++ chrome/browser/chromeos/set_time_dialog.h
@@ -7,44 +7,25 @@
 
 #include <string>
 
-#include "base/compiler_specific.h"
 #include "base/macros.h"
-#include "base/values.h"
+#include "chrome/browser/ui/webui/chromeos/system_web_dialog_delegate.h"
 #include "ui/gfx/native_widget_types.h"
-#include "ui/web_dialogs/web_dialog_delegate.h"
 
 namespace chromeos {
 
 // Set Time dialog for setting the system time, date and time zone.
-class SetTimeDialog : public ui::WebDialogDelegate {
+class SetTimeDialog : public SystemWebDialogDelegate {
  public:
-  // Shows the dialog as a child of |parent|, for example the webui settings
-  // window.
-  static void ShowDialogInParent(gfx::NativeWindow parent);
-
-  // Shows the dialog in a given container in the ash window hierarchy. Used
-  // when there is no explicit parent, for example at the login screen where
-  // the general settings window is not available. |container_id| is an
-  // an ash window container id. See ash/public/cpp/shell_window_ids.h.
-  static void ShowDialogInContainer(int container_id);
+  // Shows the set time/date dialog. If |parent| is not null, shows the dialog
+  // as a child of |parent|, e.g. the Settings window.
+  static void ShowDialog(gfx::NativeWindow parent = nullptr);
 
  private:
   SetTimeDialog();
   ~SetTimeDialog() override;
 
-  // ui::WebDialogDelegate:
-  ui::ModalType GetDialogModalType() const override;
-  base::string16 GetDialogTitle() const override;
-  GURL GetDialogContentURL() const override;
-  void GetWebUIMessageHandlers(
-      std::vector<content::WebUIMessageHandler*>* handlers) const override;
+  // SystemWebDialogDelegate
   void GetDialogSize(gfx::Size* size) const override;
-  std::string GetDialogArgs() const override;
-  void OnDialogClosed(const std::string& json_retval) override;
-  void OnCloseContents(content::WebContents* source,
-                       bool* out_close_dialog) override;
-  bool ShouldShowDialogTitle() const override;
-  bool HandleContextMenu(const content::ContextMenuParams& params) override;
 
   DISALLOW_COPY_AND_ASSIGN(SetTimeDialog);
 };
