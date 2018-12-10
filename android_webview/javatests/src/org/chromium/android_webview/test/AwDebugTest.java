67bea770a0ea96f06fb79443cbf1602542bcc70f
Samuel Huang <huangs@chromium.org>
2018-12-10 17:26:50 +0000

    [Sheriff] Disable 3 tests from org.chromium.android_webview.test.AwDebugTest
    The following tests have been consistently failing on "Android CFI":
    
      org.chromium.android_webview.test.AwDebugTest#testDump
      org.chromium.android_webview.test.AwDebugTest#testDumpContainsWhitelistedKey
      org.chromium.android_webview.test.AwDebugTest#testDumpDoesNotContainNonWhitelistedKey
    
    Culprit is likely
      https://chromium-review.googlesource.com/c/chromium/src/+/1357941
    but revert is likey painful, as seen in
      https://chromium-review.googlesource.com/c/chromium/src/+/1361286
    
    This CL disables the test to denoise the system.
    
    Tbr: jperaza@chromium.org
    Bug: 913515
    Change-Id: Ia62b796067294920164cfd37f662f40e467b2d07
    Reviewed-on: https://chromium-review.googlesource.com/c/1370191
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615157}
    Former-commit-id: fce879b44c0be70da8d2620c8ee49130c51c66c9

--- android_webview/javatests/src/org/chromium/android_webview/test/AwDebugTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwDebugTest.java
@@ -16,6 +16,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.android_webview.AwDebug;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 
 import java.io.File;
@@ -42,6 +43,7 @@ public class AwDebugTest {
     @Test
     @SmallTest
     @Feature({"AndroidWebView", "Debug"})
+    @DisabledTest(message = "crbug.com/913515")
     public void testDump() throws Throwable {
         File f = File.createTempFile("dump", ".dmp");
         try {
@@ -56,6 +58,7 @@ public class AwDebugTest {
     @Test
     @SmallTest
     @Feature({"AndroidWebView", "Debug"})
+    @DisabledTest(message = "crbug.com/913515")
     public void testDumpContainsWhitelistedKey() throws Throwable {
         File f = File.createTempFile("dump", ".dmp");
         try {
@@ -71,6 +74,7 @@ public class AwDebugTest {
     @Test
     @SmallTest
     @Feature({"AndroidWebView", "Debug"})
+    @DisabledTest(message = "crbug.com/913515")
     public void testDumpDoesNotContainNonWhitelistedKey() throws Throwable {
         File f = File.createTempFile("dump", ".dmp");
         try {
