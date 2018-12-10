dd618b181b16ea11aeb00511a1eada826ea596dd
Mitsuru Oshima <oshima@chromium.org>
2018-12-10 19:30:47 +0000

    Use ReentrantObserverList for aura::Window::observers_
    BUG=812109
    TEST=aura_unittests pass with "allow_reentrancy=false", which will
    be default in the future.
    
    Change-Id: I22cfc645bb15bfb7e9d6dc08c372124b216738a4
    Reviewed-on: https://chromium-review.googlesource.com/c/1171372
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615202}
    Former-commit-id: 01e8130c0824918abd6fe95af68d6a5100535b07

--- ui/aura/window.h
+++ ui/aura/window.h
@@ -663,7 +663,7 @@ class AURA_EXPORT Window : public ui::LayerDelegate,
   // Makes the window pass all events through to any windows behind it.
   ws::mojom::EventTargetingPolicy event_targeting_policy_;
 
-  base::ObserverList<WindowObserver, true> observers_;
+  base::ReentrantObserverList<WindowObserver, true> observers_;
 
   DISALLOW_COPY_AND_ASSIGN(Window);
 };
