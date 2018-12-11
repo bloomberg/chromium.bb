0de73efc1f41064bf563582e6fc98f20eaa366ff
Tim Volodine <timvolodine@chromium.org>
2018-12-11 12:20:27 +0000

    [AW NS] Initial AndroidStreamReaderURLLoader implementation and tests
    This patch implements the initial bits for the custom URLLoader that
    reads from an android InputStream and returns the results
    (instead of fetching response from an actual network).
    
    The general idea is to have this custom AndroidStreamReaderURLLoader
    take care of a number of things:
    1. provide response in the context of shouldInterceptRequest
    callback
    2. allow to read special url like file://android_asset/ to
    obtain app resources and assets.
    
    This patch aims to provide support for the shouldInterceptRequest
    callback. In particular it makes sure the stream is opened, correctly
    positioned (range seek), response headers are initialized, etc...
    Going forward some bits will be implemented further: i.e. returning
    and writing the actual body response via mojo pipes.
    
    Approximately speaking the AndroidStreamReaderURLLoader is the
    network service counterpart of AndroidStreamReaderURLRequestJob
    (which is used by the current net/ code path).
    
    The patch also adds some relevant corresponding unit tests for the
    loader.
    
    BUG=893566,841556
    
    Change-Id: I15247813f2b359445a2e9d45a18c8e595dfebdb2
    Reviewed-on: https://chromium-review.googlesource.com/c/1363184
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Tim Volodine <timvolodine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615498}
    Former-commit-id: 5a482ae50fa95b3aa7a792450b4c6b8bbfa1e4c2

---
+++ android_webview/browser/net_network_service/android_stream_reader_url_loader_unittest.cc
@@ -0,0 +1,170 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "android_webview/browser/net_network_service/android_stream_reader_url_loader.h"
+
+#include "android_webview/browser/input_stream.h"
+#include "base/test/scoped_feature_list.h"
+#include "base/test/scoped_task_environment.h"
+#include "content/public/common/resource_type.h"
+#include "mojo/core/embedder/embedder.h"
+#include "net/http/http_request_headers.h"
+#include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
+#include "services/network/public/cpp/features.h"
+#include "services/network/test/test_url_loader_client.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace android_webview {
+
+// Always succeeds
+class FakeInputStream : public InputStream {
+ public:
+  FakeInputStream() {}
+  ~FakeInputStream() override {}
+  bool BytesAvailable(int* bytes_available) const override { return true; }
+  bool Skip(int64_t n, int64_t* bytes_skipped) override {
+    *bytes_skipped = n;
+    return true;
+  }
+  bool Read(net::IOBuffer* dest, int length, int* bytes_read) override {
+    return true;
+  }
+};
+
+// Stream that always fails
+class FakeFailingInputStream : public InputStream {
+ public:
+  FakeFailingInputStream() {}
+  ~FakeFailingInputStream() override {}
+  bool BytesAvailable(int* bytes_available) const override { return false; }
+  bool Skip(int64_t n, int64_t* bytes_skipped) override { return false; }
+  bool Read(net::IOBuffer* dest, int length, int* bytes_read) override {
+    return false;
+  }
+};
+
+class TestResponseDelegate
+    : public AndroidStreamReaderURLLoader::ResponseDelegate {
+ public:
+  TestResponseDelegate(std::unique_ptr<InputStream> input_stream)
+      : input_stream_(std::move(input_stream)) {}
+  ~TestResponseDelegate() override {}
+
+  std::unique_ptr<android_webview::InputStream> OpenInputStream(
+      JNIEnv* env) override {
+    return std::move(input_stream_);
+  }
+
+ private:
+  std::unique_ptr<InputStream> input_stream_;
+};
+
+class AndroidStreamReaderURLLoaderTest : public ::testing::Test {
+ protected:
+  AndroidStreamReaderURLLoaderTest() {}
+  ~AndroidStreamReaderURLLoaderTest() override = default;
+
+  void SetUp() override {
+    mojo::core::Init();
+    feature_list_.InitAndEnableFeature(network::features::kNetworkService);
+  }
+
+  network::ResourceRequest CreateRequest(const GURL& url) {
+    network::ResourceRequest request;
+    request.url = url;
+    request.method = "GET";
+    request.resource_type = content::RESOURCE_TYPE_SUB_RESOURCE;
+    return request;
+  }
+
+  // helper method for creating loaders given a stream
+  AndroidStreamReaderURLLoader* CreateLoader(
+      const network::ResourceRequest& request,
+      network::TestURLLoaderClient* client,
+      std::unique_ptr<InputStream> input_stream) {
+    return new AndroidStreamReaderURLLoader(
+        request, client->CreateInterfacePtr(),
+        net::MutableNetworkTrafficAnnotationTag(TRAFFIC_ANNOTATION_FOR_TESTS),
+        std::make_unique<TestResponseDelegate>(std::move(input_stream)));
+  }
+
+  base::test::ScopedFeatureList feature_list_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
+
+  DISALLOW_COPY_AND_ASSIGN(AndroidStreamReaderURLLoaderTest);
+};
+
+TEST_F(AndroidStreamReaderURLLoaderTest, ReadFakeStream) {
+  network::ResourceRequest request =
+      CreateRequest(GURL("https://www.example.com/"));
+  std::unique_ptr<network::TestURLLoaderClient> client =
+      std::make_unique<network::TestURLLoaderClient>();
+  AndroidStreamReaderURLLoader* loader =
+      CreateLoader(request, client.get(), std::make_unique<FakeInputStream>());
+  loader->Start();
+  client->RunUntilComplete();
+  EXPECT_EQ(net::OK, client->completion_status().error_code);
+  EXPECT_EQ("HTTP/1.1 200 OK",
+            client->response_head().headers->GetStatusLine());
+}
+
+TEST_F(AndroidStreamReaderURLLoaderTest, ReadFailingStream) {
+  network::ResourceRequest request =
+      CreateRequest(GURL("https://www.example.com/"));
+  std::unique_ptr<network::TestURLLoaderClient> client =
+      std::make_unique<network::TestURLLoaderClient>();
+  AndroidStreamReaderURLLoader* loader = CreateLoader(
+      request, client.get(), std::make_unique<FakeFailingInputStream>());
+  loader->Start();
+  client->RunUntilComplete();
+  EXPECT_EQ(net::ERR_FAILED, client->completion_status().error_code);
+}
+
+TEST_F(AndroidStreamReaderURLLoaderTest, ValidRangeRequest) {
+  network::ResourceRequest request =
+      CreateRequest(GURL("https://www.example.com/"));
+  request.headers.SetHeader(net::HttpRequestHeaders::kRange, "bytes=10-200");
+
+  std::unique_ptr<network::TestURLLoaderClient> client =
+      std::make_unique<network::TestURLLoaderClient>();
+  AndroidStreamReaderURLLoader* loader =
+      CreateLoader(request, client.get(), std::make_unique<FakeInputStream>());
+  loader->Start();
+  client->RunUntilComplete();
+  EXPECT_EQ(net::OK, client->completion_status().error_code);
+  EXPECT_EQ("HTTP/1.1 200 OK",
+            client->response_head().headers->GetStatusLine());
+}
+
+TEST_F(AndroidStreamReaderURLLoaderTest, InvalidRangeRequest) {
+  network::ResourceRequest request =
+      CreateRequest(GURL("https://www.example.com/"));
+  request.headers.SetHeader(net::HttpRequestHeaders::kRange, "bytes=10-0");
+
+  std::unique_ptr<network::TestURLLoaderClient> client =
+      std::make_unique<network::TestURLLoaderClient>();
+  AndroidStreamReaderURLLoader* loader =
+      CreateLoader(request, client.get(), std::make_unique<FakeInputStream>());
+  loader->Start();
+  client->RunUntilComplete();
+  EXPECT_EQ(net::ERR_REQUEST_RANGE_NOT_SATISFIABLE,
+            client->completion_status().error_code);
+}
+
+TEST_F(AndroidStreamReaderURLLoaderTest, NullInputStream) {
+  network::ResourceRequest request =
+      CreateRequest(GURL("https://www.example.com/"));
+
+  std::unique_ptr<network::TestURLLoaderClient> client =
+      std::make_unique<network::TestURLLoaderClient>();
+  AndroidStreamReaderURLLoader* loader =
+      CreateLoader(request, client.get(), nullptr);
+  loader->Start();
+  client->RunUntilComplete();
+  EXPECT_EQ(net::ERR_FAILED, client->completion_status().error_code);
+  EXPECT_EQ("HTTP/1.1 404 Not Found",
+            client->response_head().headers->GetStatusLine());
+}
+
+}  // namespace android_webview
