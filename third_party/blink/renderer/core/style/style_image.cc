d33a18b6ba5bfb399ddd27372ca0758a1d19b4a7
Stephen Chenney <schenney@chromium.org>
2018-12-12 02:53:56 +0000

    Fix SVG-As-Background sizing
    Addresses a TODO where we were snapping the intrinsic size of an SVG image,
    even when that is no longer useful. This was posing issues with operations
    using aspect ratio, because the computed aspect ratio on the snapped value
    was wrong when then used on un-snapped values.
    
    R=fs@opera.com
    BUG=855882
    
    Change-Id: I0c14c0109da4c5ca5e52aebf408e9b702eda799c
    Reviewed-on: https://chromium-review.googlesource.com/c/1370486
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#615798}
    Former-commit-id: aed99d7e12bd036ab3ebb4fe522ce8faf347a32f

--- third_party/blink/renderer/core/style/style_image.cc
+++ third_party/blink/renderer/core/style/style_image.cc
@@ -33,11 +33,8 @@ FloatSize StyleImage::ImageSizeForSVGImage(
     const LayoutSize& default_object_size) const {
   FloatSize unzoomed_default_object_size(default_object_size);
   unzoomed_default_object_size.Scale(1 / multiplier);
-  // FIXME(schenney): Remove this rounding hack once background image
-  // geometry is converted to handle rounding downstream.
-  return FloatSize(RoundedIntSize(
-      ApplyZoom(svg_image->ConcreteObjectSize(unzoomed_default_object_size),
-                multiplier)));
+  return ApplyZoom(svg_image->ConcreteObjectSize(unzoomed_default_object_size),
+                   multiplier);
 }
 
 }  // namespace blink
