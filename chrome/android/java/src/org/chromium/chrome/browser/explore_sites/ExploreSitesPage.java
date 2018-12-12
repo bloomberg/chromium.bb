1a136460a77c5bd316dff0cb41506c5ef7f0504b
Cathy Li <chili@chromium.org>
2018-12-12 22:50:53 +0000

    [Explore sites]: Remove unneeded files and the title on the explore sites page.
    Change-Id: Iee0486cc74130e4b28f60cf9ed9fbddca915e7e3
    Reviewed-on: https://chromium-review.googlesource.com/c/1374330
    Commit-Queue: Cathy Li <chili@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616091}
    Former-commit-id: ec3933dde96b9729202b8689431ff3b84b265f48

--- chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesPage.java
+++ chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesPage.java
@@ -32,7 +32,6 @@ import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabObserver;
-import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.widget.RoundedIconGenerator;
 import org.chromium.content_public.browser.NavigationController;
 import org.chromium.content_public.browser.NavigationEntry;
@@ -69,7 +68,6 @@ public class ExploreSitesPage extends BasicNativePage {
         int LOADING_NET = 4; // Retrieving catalog resources from internet.
     }
 
-    private TabModelSelector mTabModelSelector;
     private NativePageHost mHost;
     private Tab mTab;
     private TabObserver mTabObserver;
@@ -107,7 +105,6 @@ public class ExploreSitesPage extends BasicNativePage {
             mTab.addObserver(mTabObserver);
         }
 
-        mTabModelSelector = activity.getTabModelSelector();
         mTitle = activity.getString(R.string.explore_sites_title);
         mView = (ViewGroup) activity.getLayoutInflater().inflate(
                 R.layout.explore_sites_page_layout, null);
@@ -128,8 +125,9 @@ public class ExploreSitesPage extends BasicNativePage {
                         context.getResources(), R.color.default_favicon_background_color),
                 context.getResources().getDimensionPixelSize(R.dimen.tile_view_icon_text_size));
 
-        NativePageNavigationDelegateImpl navDelegate =
-                new NativePageNavigationDelegateImpl(activity, mProfile, host, mTabModelSelector);
+        NativePageNavigationDelegateImpl navDelegate = new NativePageNavigationDelegateImpl(
+                activity, mProfile, host, activity.getTabModelSelector());
+
         // Don't direct reference activity because it might change if tab is reparented.
         Runnable closeContextMenuCallback =
                 () -> host.getActiveTab().getActivity().closeContextMenu();
@@ -149,8 +147,6 @@ public class ExploreSitesPage extends BasicNativePage {
 
         ExploreSitesBridge.getEspCatalog(mProfile, this::translateToModel);
         RecordUserAction.record("Android.ExploreSitesPage.Open");
-
-        // TODO(chili): Set layout to be an observer of list model
     }
 
     void translateToModel(@Nullable List<ExploreSitesCategory> categoryList) {
