c2911f9c8fa3725acc652bde8971edd68af44ebe
rbpotter <rbpotter@chromium.org>
2018-12-12 02:46:09 +0000

    CrFocusRowBehavior: Fix flaky test
    Locally reproduced the test failure and determined that async setup in
    FocusRowBehavior was often not actually completed before tests were
    started. Waiting for one more render to ensure async setup is complete
    prevents the flakes.
    
    Bug: 913197
    Change-Id: Ifb893927ecf83666f5edb201c81cb5ce49791fc0
    Reviewed-on: https://chromium-review.googlesource.com/c/1372831
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#615797}
    Former-commit-id: 5fbc5b2cf493013690c40ec5c27b539033c959eb

--- chrome/test/data/webui/cr_focus_row_behavior_test.js
+++ chrome/test/data/webui/cr_focus_row_behavior_test.js
@@ -47,15 +47,28 @@ suite('cr-focus-row-behavior-test', function() {
     });
   });
 
-  setup(function(done) {
+  /**
+   * @param {!HTMLElement} element
+   * @return {!Promise} Promise that resolves when an afterNextRender()
+   *     callback on |element| is run.
+   */
+  function afterNextRender(element) {
+    return new Promise(resolve => {
+      Polymer.RenderStatus.afterNextRender(element, resolve);
+    });
+  }
+
+  setup(function() {
     PolymerTest.clearBody();
 
     testElement = document.createElement('focus-row-element');
     document.body.appendChild(testElement);
 
-    // Block until FocusRowBehavior.attached finishes running async setup.
-    Polymer.RenderStatus.afterNextRender(this, function() {
-      done();
+    // Block so that FocusRowBehavior.attached can run.
+    return afterNextRender(testElement).then(() => {
+      // Wait one more time to ensure that async setup in FocusRowBehavior has
+      // executed.
+      return afterNextRender(testElement);
     });
   });
 
