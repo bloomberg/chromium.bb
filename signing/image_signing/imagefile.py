ccf97ef6bafc057b4a680e0e2842a820e8e8af1d
LaMont Jones <lamontjones@chromium.org>
2018-12-13 11:39:41 -0700

    signing: begin converting sign_official_build.sh
    Starting with SignImage.
    
    BUG=chromium:813829
    TEST=Unittests pass
    
    Change-Id: I22b89149274da7bbd5913ce96ba2c9230c9dece8
    Reviewed-on: https://chromium-review.googlesource.com/1361675
    Commit-Ready: LaMont Jones <lamontjones@chromium.org>
    Tested-by: LaMont Jones <lamontjones@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

---
+++ signing/image_signing/imagefile.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Chrome OS Image file signing."""
+
+from __future__ import print_function
+
+import os
+
+from chromite.lib import cros_build_lib
+from chromite.lib import cros_logging as logging
+from chromite.lib import image_lib
+from chromite.lib import osutils
+from chromite.signing.lib import firmware
+from chromite.signing.lib import keys
+
+
+class Error(Exception):
+  """Base exception for all exceptions in this module."""
+
+
+class SignImageError(Error):
+  """Error occurred within SignImage"""
+
+
+def SignImage(image_type, input_file, output_file, kernel_part_num, keydir,
+              keyA_prefix=''):
+  """Sign the image file.
+
+  A Chromium OS image file (INPUT) always contains 2 partitions (kernel A & B).
+  This function will rebuild hash data by DM_PARTNO, resign kernel partitions by
+  their KEYBLOCK and PRIVKEY files, and then write to OUTPUT file. Note some
+  special images (specified by IMAGE_TYPE, like 'recovery' or 'factory_install')
+  may have additional steps (ex, tweaking verity hash or not stripping files)
+  when generating output file.
+
+  Args:
+    image_type: Type of image (e.g., 'factory', 'recovery')
+    input_file: image to sign. (read-only: copied to output_file)
+    output_file: signed image. (file is created here)
+    kernel_part_num: partition number (or name) for the kernel (usually 2,
+        sometimes 4)
+    keydir: path of keyset dir to use.
+    keyA_prefix: prefix for kernA key (e.g., 'recovery_')
+
+  Raises SignImageException
+  """
+  # TODO(lamontjones): consider extending osutils to have a sparse file copy
+  # function.
+  logging.info('Preparing %s image...', image_type)
+  cros_build_lib.RunCommand(
+      ['cp', '--sparse=always', input_file, output_file])
+
+  keyset = keys.Keyset(keydir)
+
+  firmware.ResignImageFirmware(output_file, keyset)
+
+  with osutils.TempDir() as dest_dir:
+    with image_lib.LoopbackPartitions(output_file, dest_dir) as image:
+      rootfs_dir = image.Mount(('ROOT-A',))[0]
+      SignAndroidImage(rootfs_dir, keyset)
+      SignUefiBinaries(image, rootfs_dir, keyset)
+      image.Unmount(('ROOT-A',))
+
+      # TODO(lamontjones): From this point on, all we really want at the moment
+      # is the loopback devicefile names, but that may change as we implement
+      # more of the shell functions.  We do not actually want to have any
+      # filesystems mounted at this point.
+
+      loop_kernA = image.GetPartitionDevName('KERN-A')
+      loop_rootfs = image.GetPartitionDevName('ROOT-A')
+      kernA_config = cros_build_lib.SudoRunCommand(
+          ['dump_kernel_config', loop_kernA],
+          print_cmd=False, capture_output=True).output.split()
+      if (image_type != 'factory_install' and
+          'cros_legacy' not in kernA_config and
+          'cros_efi' not in kernA_config):
+        cros_build_lib.RunCommand(
+            ['strip_boot_from_image.sh', '--image', loop_rootfs])
+
+  # TODO(lamontjones): several remaining steps, that all need to be added:
+  # UpdateRootfsHash, UpdateStatefulPartitionVblock, UpdateRecoveryKernelHash,
+  # UpdateLegacyBootloader
+  logging.error('Not completely implemented.')
+  logging.debug('kernel_part_num=%d keyA_prefix=%s',
+                kernel_part_num, keyA_prefix)
+  logging.info('Signed %s image written to %s', image_type, output_file)
+  return True
+
+
+def SignAndroidImage(rootfs_dir, keyset):
+  """If there is an android image, sign it."""
+  system_img = os.path.join(
+      rootfs_dir, 'opt/google/containers/android/system.raw.img')
+  if not os.path.exists(system_img):
+    logging.info('ARC image not found.  Not signing Android APKs.')
+    return
+
+  arc_version = cros_build_lib.LoadKeyValueFile(os.path.join(
+      rootfs_dir, 'etc/lsb-release')).get('CHROMEOS_ARC_VERSION', '')
+  if not arc_version:
+    logging.warning('CHROMEOS_ARC_VERSION not found in lsb-release. '
+                    'Not signing Android APKs.')
+    return
+
+  logging.info('Found ARC image version %s, resigning APKs', arc_version)
+  # Sign the Android APKs using ${keyset.key_dir}/android keys.
+  android_keydir = os.path.join(keyset.key_dir, 'android')
+  logging.info('Using %s', android_keydir)
+
+  # TODO(lamontjones) migrate sign_android_image.sh.
+  cros_build_lib.RunCommand(
+      ['sign_android_image.sh', rootfs_dir, android_keydir])
+
+
+def SignUefiBinaries(image, rootfs_dir, keyset):
+  """Sign UEFI binaries if appropriate."""
+  # If there are no uefi keys in the keyset, we're done.
+  uefi_keydir = os.path.join(keyset.key_dir, 'uefi')
+  if not os.path.isdir(uefi_keydir):
+    logging.info('No UEFI keys in keyset. Skipping.')
+    return
+
+  # Mount the UEFI partition and sign the contents.
+  try:
+    uefi_fsdir = image.Mount(('EFI-SYSTEM',))[0]
+  except KeyError:
+    # Image has no EFI-SYSTEM partition.
+    logging.info('No EFI-SYSTEM partition found.')
+    return
+
+  # Sign the UEFI binaries on the EFI partition using
+  # ${keyset.key_dir}/uefi keys.
+  # TODO(lamontjones): convert install_gsetup_certs.sh to python.
+  cros_build_lib.RunCommand(
+      ['install_gsetup_certs.sh', uefi_fsdir, uefi_keydir])
+  # TODO(lamontjones): convert sign_uefi.sh to python.
+  cros_build_lib.RunCommand(
+      ['sign_uefi.sh', uefi_fsdir, uefi_keydir])
+
+  # TODO(lamontjones): convert sign_uefi.sh to python.
+  cros_build_lib.RunCommand(
+      ['sign_uefi.sh', os.path.join(rootfs_dir, 'boot'), uefi_keydir])
+
+  logging.info('Signed UEFI binaries.')
---
+++ signing/image_signing/imagefile_unittest.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Chrome OS imagefile signing unittests"""
+
+from __future__ import print_function
+
+import os
+
+import mock
+
+from chromite.lib import cros_build_lib
+from chromite.lib import cros_logging as logging
+from chromite.lib import cros_test_lib
+from chromite.lib import image_lib
+from chromite.lib import image_lib_unittest
+from chromite.lib import osutils
+from chromite.signing.lib import firmware
+from chromite.signing.lib import keys
+from chromite.signing.image_signing import imagefile
+
+
+# pylint: disable=protected-access
+
+class TestSignImage(cros_test_lib.RunCommandTempDirTestCase):
+  """Test SignImage function."""
+
+  def setUp(self):
+    self.rc.SetDefaultCmdResult()
+    self.image = image_lib_unittest.LoopbackPartitionsMock(
+        'outfile', self.tempdir)
+    self.PatchObject(image_lib, 'LoopbackPartitions', return_value=self.image)
+    self.fw_mock = self.PatchObject(firmware, 'ResignImageFirmware')
+    self.android_mock = self.PatchObject(imagefile, 'SignAndroidImage')
+    self.uefi_mock = self.PatchObject(imagefile, 'SignUefiBinaries')
+    self.keyset_return = mock.Mock()
+    self.keyset_mock = self.PatchObject(keys, 'Keyset',
+                                        return_value=self.keyset_return)
+
+  def testSimple(self):
+    """Test that USB case works, and strips boot."""
+    self.assertTrue(
+        imagefile.SignImage('USB', 'infile', 'outfile', 2, '/keydir'))
+    self.fw_mock.assert_called_once_with('outfile', self.keyset_return)
+    expected_rc = [
+        mock.call(['cp', '--sparse=always', 'infile', 'outfile']),
+        mock.call(['sudo', '--', 'dump_kernel_config', '/dev/loop9999p2'],
+                  capture_output=True, print_cmd=False),
+        mock.call(['strip_boot_from_image.sh', '--image', '/dev/loop9999p3'])
+    ]
+    self.assertEqual(expected_rc, self.rc.call_args_list)
+    self.android_mock.assert_called_once()
+    rootfs_dir, keyset = self.android_mock.call_args[0]
+    self.assertTrue(rootfs_dir.startswith(self.tempdir + '/'))
+    self.assertTrue(rootfs_dir.endswith('/dir-3'))
+    self.assertEqual(self.keyset_return, keyset)
+    self.uefi_mock.assert_called_once_with(self.image, rootfs_dir,
+                                           self.keyset_return)
+
+  def testNoStripOnNonFactrory(self):
+    """Verify that strip is not called on factory installs."""
+    self.assertTrue(
+        imagefile.SignImage(
+            'factory_install', 'infile', 'outfile', 2, '/keydir'))
+    self.assertNotIn(
+        mock.call(['strip_boot_from_image.sh', '--image', '/dev/loop9999p3']),
+        self.rc.call_args_list)
+
+  def testNoStripOnLegacy(self):
+    """Verify that strip is not called for legacy."""
+    self.rc.AddCmdResult(
+        ['sudo', '--', 'dump_kernel_config', '/dev/loop9999p2'],
+        output=' cros_legacy ')
+    self.assertTrue(
+        imagefile.SignImage(
+            'USB', 'infile', 'outfile', 2, '/keydir'))
+    self.assertNotIn(
+        mock.call(['strip_boot_from_image.sh', '--image', '/dev/loop9999p3']),
+        self.rc.call_args_list)
+
+  def testNoStripOnEFI(self):
+    """Verify that strip is not called for EFI."""
+    self.rc.AddCmdResult(
+        ['sudo', '--', 'dump_kernel_config', '/dev/loop9999p2'],
+        output=' cros_efi ')
+    self.assertTrue(
+        imagefile.SignImage(
+            'USB', 'infile', 'outfile', 2, '/keydir'))
+    self.assertNotIn(
+        mock.call(['strip_boot_from_image.sh', '--image', '/dev/loop9999p3']),
+        self.rc.call_args_list)
+
+
+class TestSignAndroidImage(cros_test_lib.RunCommandTempDirTestCase):
+  """Test SignAndroidImage function."""
+
+  def setUp(self):
+    self.keytempdir = osutils.TempDir()
+    self.keyset = keys.Keyset(self.keytempdir.tempdir)
+    self.info_mock = self.PatchObject(logging, 'info')
+    self.warn_mock = self.PatchObject(logging, 'warning')
+    self.rc.SetDefaultCmdResult()
+
+  def testNoImage(self):
+    """Test with no Android image."""
+    exists_mock = self.PatchObject(os.path, 'exists', return_value=False)
+    imagefile.SignAndroidImage(self.tempdir, self.keyset)
+    exists_mock.assert_called_once_with(
+        os.path.join(self.tempdir,
+                     'opt/google/containers/android/system.raw.img'))
+    self.info_mock.assert_called_once_with(
+        'ARC image not found.  Not signing Android APKs.')
+    self.assertEqual(0, self.rc.call_count)
+
+  def testNoVersion(self):
+    """Test: have Android image, but no ARC_VERSION info."""
+    exists_mock = self.PatchObject(os.path, 'exists', return_value=True)
+    self.PatchObject(cros_build_lib, 'LoadKeyValueFile', return_value={})
+    imagefile.SignAndroidImage(self.tempdir, self.keyset)
+    exists_mock.assert_called_once_with(
+        os.path.join(self.tempdir,
+                     'opt/google/containers/android/system.raw.img'))
+    self.warn_mock.assert_called_once_with(
+        'CHROMEOS_ARC_VERSION not found in lsb-release. '
+        'Not signing Android APKs.')
+    self.assertEqual(0, self.info_mock.call_count)
+    self.assertEqual(0, self.rc.call_count)
+
+  def testTriesToSign(self):
+    """Test: have Android image, and Android version."""
+    exists_mock = self.PatchObject(os.path, 'exists', return_value=True)
+    self.PatchObject(cros_build_lib, 'LoadKeyValueFile',
+                     return_value={'CHROMEOS_ARC_VERSION': '9'})
+    imagefile.SignAndroidImage(self.tempdir, self.keyset)
+    exists_mock.assert_called_once_with(
+        os.path.join(self.tempdir,
+                     'opt/google/containers/android/system.raw.img'))
+    self.assertEqual(0, self.warn_mock.call_count)
+    android_keydir = '%s/android' % self.keyset.key_dir
+    expected_info = [
+        mock.call('Found ARC image version %s, resigning APKs', '9'),
+        mock.call('Using %s', android_keydir),
+    ]
+    self.assertEqual(expected_info, self.info_mock.call_args_list)
+    self.assertEqual(
+        self.rc.call_args_list,
+        [mock.call(['sign_android_image.sh', self.tempdir, android_keydir])])
+
+
+class TestSignUefiBinaries(cros_test_lib.RunCommandTempDirTestCase):
+  """Test SignUefiBinaries function."""
+
+  def setUp(self):
+    self.keytempdir = osutils.TempDir()
+    self.keyset = keys.Keyset(self.keytempdir.tempdir)
+    self.info_mock = self.PatchObject(logging, 'info')
+    self.warn_mock = self.PatchObject(logging, 'warning')
+    self.rc.SetDefaultCmdResult()
+    self.image = image_lib_unittest.LoopbackPartitionsMock('meh', self.tempdir)
+
+  def testUefiKeydir(self):
+    """Test with no uefi keys."""
+    isdir_mock = self.PatchObject(os.path, 'isdir', return_value=False)
+    imagefile.SignUefiBinaries(self.image, self.tempdir, self.keyset)
+    uefi_keydir = os.path.join(self.keyset.key_dir, 'uefi')
+    isdir_mock.assert_called_once_with(uefi_keydir)
+    self.info_mock.assert_called_once_with('No UEFI keys in keyset. Skipping.')
+    self.assertEqual(0, self.rc.call_count)
+
+  def testNoEfiPartition(self):
+    """Test: have uefi keys, but no EFI-SYSTEM partition."""
+    isdir_mock = self.PatchObject(os.path, 'isdir', return_value=True)
+    self.PatchObject(image_lib_unittest.LoopbackPartitionsMock, '_Mount',
+                     side_effect=KeyError(repr('EFI_SYSTEM')))
+    imagefile.SignUefiBinaries(self.image, self.tempdir, self.keyset)
+    uefi_keydir = os.path.join(self.keyset.key_dir, 'uefi')
+    isdir_mock.assert_called_once_with(uefi_keydir)
+    self.info_mock.assert_called_once_with('No EFI-SYSTEM partition found.')
+    self.assertEqual(0, self.rc.call_count)
+
+  def testSigns(self):
+    """Test with uefi keys and EFI-SYSTEM partition."""
+    isdir_mock = self.PatchObject(os.path, 'isdir', return_value=True)
+    imagefile.SignUefiBinaries(self.image, self.tempdir, self.keyset)
+    uefi_keydir = os.path.join(self.keyset.key_dir, 'uefi')
+    isdir_mock.assert_called_once_with(uefi_keydir)
+    uefi_fsdir = os.path.join(self.tempdir, 'dir-12')
+    expected_rc = [
+        mock.call(['install_gsetup_certs.sh', uefi_fsdir, uefi_keydir]),
+        mock.call(['sign_uefi.sh', uefi_fsdir, uefi_keydir]),
+        mock.call([
+            'sign_uefi.sh', os.path.join(self.tempdir, 'boot'), uefi_keydir]),
+    ]
+    self.assertEqual(expected_rc, self.rc.call_args_list)
+    self.info_mock.assert_called_once_with('Signed UEFI binaries.')
