a76d5852d6fe7b9d8c041e884ab886ee451e7733
Mike Reed <reed@google.com>
2018-12-13 14:44:01 +0000

    add SK_SUPPORT_LEGACY_FONTMETRICS_IN_PAINT flag to stage skia api change
    Needs this to roll in first
    https://skia-review.googlesource.com/c/skia/+/177063
    
    Change-Id: I6ed43335c93e516f7a2f1fb7efc4756206e83bcc
    Reviewed-on: https://chromium-review.googlesource.com/c/1374166
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Mike Reed <reed@google.com>
    Commit-Queue: Mike Reed <reed@google.com>
    Cr-Commit-Position: refs/heads/master@{#616308}
    Former-commit-id: 5032ce65bc80f825f6f4b4d694cbb51786e96315

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -144,6 +144,14 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_USE_LEGACY_DISTANCE_FIELDS
 #endif
 
+#ifndef SK_SUPPORT_LEGACY_FONTMETRICS_IN_PAINT
+#define SK_SUPPORT_LEGACY_FONTMETRICS_IN_PAINT
+#endif
+
+#ifndef SK_SUPPORT_LEGACY_PAINT_TEXTMEASURE
+#define SK_SUPPORT_LEGACY_PAINT_TEXTMEASURE
+#endif
+
 // Skia is enabling this feature soon. Chrome probably does
 // not want it for M64
 #ifndef SK_DISABLE_EXPLICIT_GPU_RESOURCE_ALLOCATION
