5bef89e8c17bbc0efd7a66b60f998725de6bfe71
David Roger <droger@chromium.org>
2018-12-13 13:22:14 +0000

    [signin] Fix SigninProfileAttributesUpdaterTest
    This CL does a branch-specific fix for SigninProfileAttributesUpdaterTest.
    This test passes on trunk but not on the branch because it relies on a
    refactoring:
    https://chromium-review.googlesource.com/c/chromium/src/+/1070154
    
    This CL fixes the test so that it can pass even without the refactoring.
    
    Bug: 914454
    Change-Id: I08d4a81a44f0a57f057603dd291ce2e69aefb761
    Reviewed-on: https://chromium-review.googlesource.com/c/1375120
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3626@{#324}
    Cr-Branched-From: 5302c6863bade38a5d283a7cce26a309d8e8e755 [formerly d897fb137fbaaa9355c0c93124cc048824eb1e65]-refs/heads/master@{#612437}
    Former-commit-id: 5958ebb18d6c7d40fd4fa8969a287ca3bca1aaa9

--- chrome/browser/signin/signin_profile_attributes_updater_unittest.cc
+++ chrome/browser/signin/signin_profile_attributes_updater_unittest.cc
@@ -11,7 +11,6 @@
 #include "chrome/browser/profiles/profile_attributes_storage.h"
 #include "chrome/browser/signin/account_tracker_service_factory.h"
 #include "chrome/browser/signin/chrome_signin_client_factory.h"
-#include "chrome/browser/signin/fake_profile_oauth2_token_service_builder.h"
 #include "chrome/browser/signin/fake_signin_manager_builder.h"
 #include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
 #include "chrome/browser/signin/signin_manager_factory.h"
@@ -20,11 +19,12 @@
 #include "chrome/test/base/testing_profile.h"
 #include "chrome/test/base/testing_profile_manager.h"
 #include "components/signin/core/browser/account_tracker_service.h"
-#include "components/signin/core/browser/fake_profile_oauth2_token_service.h"
+#include "components/signin/core/browser/profile_oauth2_token_service.h"
 #include "components/signin/core/browser/signin_manager.h"
 #include "components/sync_preferences/pref_service_syncable.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "google_apis/gaia/google_service_auth_error.h"
+#include "google_apis/gaia/oauth2_token_service_delegate.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
@@ -56,9 +56,6 @@ class SigninProfileAttributesUpdaterTest : public testing::Test {
     testing_factories.emplace_back(
         ChromeSigninClientFactory::GetInstance(),
         base::BindRepeating(&signin::BuildTestSigninClient));
-    testing_factories.emplace_back(
-        ProfileOAuth2TokenServiceFactory::GetInstance(),
-        base::BindRepeating(&BuildFakeProfileOAuth2TokenService));
 #if defined(OS_CHROMEOS)
     testing_factories.emplace_back(
         SigninManagerFactory::GetInstance(),
@@ -111,9 +108,8 @@ TEST_F(SigninProfileAttributesUpdaterTest, AuthError) {
 
   AccountTrackerService* account_tracker =
       AccountTrackerServiceFactory::GetForProfile(profile_);
-  FakeProfileOAuth2TokenService* token_service =
-      static_cast<FakeProfileOAuth2TokenService*>(
-          ProfileOAuth2TokenServiceFactory::GetForProfile(profile_));
+  ProfileOAuth2TokenService* token_service =
+      ProfileOAuth2TokenServiceFactory::GetForProfile(profile_);
   std::string account_id =
       account_tracker->SeedAccountInfo("gaia", "example@email.com");
   token_service->UpdateCredentials(account_id, "token");
@@ -126,13 +122,13 @@ TEST_F(SigninProfileAttributesUpdaterTest, AuthError) {
   EXPECT_FALSE(entry->IsAuthError());
 
   // Set auth error.
-  token_service->UpdateAuthErrorForTesting(
+  token_service->GetDelegate()->UpdateAuthError(
       account_id,
       GoogleServiceAuthError(GoogleServiceAuthError::INVALID_GAIA_CREDENTIALS));
   EXPECT_TRUE(entry->IsAuthError());
 
   // Remove auth error.
-  token_service->UpdateAuthErrorForTesting(
+  token_service->GetDelegate()->UpdateAuthError(
       account_id, GoogleServiceAuthError::AuthErrorNone());
   EXPECT_FALSE(entry->IsAuthError());
 }
