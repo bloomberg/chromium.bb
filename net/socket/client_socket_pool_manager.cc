99c61f1c55d922a2404601f12eaba503073cbd83
Brad Lassey <lassey@chromium.org>
2018-12-13 04:21:09 +0000

    Disable the use of QUIC proxies for https:// URLs.
    This is a partial revert of https://chromium-review.googlesource.com/c/chromium/src/+/858603
    
    BUG=914497
    
    Change-Id: I378b42b01367aca8642d49b682b121f6f8873786
    Reviewed-on: https://chromium-review.googlesource.com/c/1375112
    Reviewed-by: Brad Lassey <lassey@chromium.org>
    Reviewed-by: Nick Harper <nharper@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Brad Lassey <lassey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616211}
    Former-commit-id: e62461ede4cdb45b0bfec2e75785d2fddde768e0

--- net/socket/client_socket_pool_manager.cc
+++ net/socket/client_socket_pool_manager.cc
@@ -131,7 +131,7 @@ int InitSocketPoolHelper(ClientSocketPoolManager::SocketGroupType group_type,
         new TransportSocketParams(*proxy_host_port, disable_resolver_cache,
                                   resolution_callback));
 
-    if (proxy_info.is_http() || proxy_info.is_https() || proxy_info.is_quic()) {
+    if (proxy_info.is_http() || proxy_info.is_https()) {
       // TODO(mmenke):  Would it be better to split these into two different
       //     socket pools?  And maybe socks4/socks5 as well?
       if (proxy_info.is_http()) {
@@ -144,7 +144,7 @@ int InitSocketPoolHelper(ClientSocketPoolManager::SocketGroupType group_type,
       request_extra_headers.GetHeader(HttpRequestHeaders::kUserAgent,
                                       &user_agent);
       scoped_refptr<SSLSocketParams> ssl_params;
-      if (!proxy_info.is_http()) {
+      if (proxy_info.is_https()) {
         proxy_tcp_params = new TransportSocketParams(
             *proxy_host_port, disable_resolver_cache, resolution_callback);
         // Set ssl_params, and unset proxy_tcp_params
