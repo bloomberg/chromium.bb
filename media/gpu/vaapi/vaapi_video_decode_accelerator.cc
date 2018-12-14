0f07f4ffd659ec305d260d1d5047a16173fef961
Hirokazu Honda <hiroh@chromium.org>
2018-12-14 00:34:41 +0000

    Revert "Vaapi: Use GUARDED_BY() static annotations on decoder"
    This reverts commit 75cb0f435f53f3bdccdac42f1ccb4faa1122b59c [formerly 99e2d9e3cdba89a15ebd4890b2de37336963a0b0].
    
    Reason for revert: This breaks Video Playback on intel devices
    
    Original change's description:
    > Vaapi: Use GUARDED_BY() static annotations on decoder
    >
    > This CL introduces GUARDED_BY() static annotation on VaVDA; not all
    > members variables need to be guarded, so it adds on a number of them
    > a comment about on which thread it is used.
    >
    > Bug: 909547
    > Change-Id: I437a82320795aebc56f77cd1254bb7c8c2f2f5da
    > Reviewed-on: https://chromium-review.googlesource.com/c/1368590
    > Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    > Commit-Queue: Miguel Casas <mcasas@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#616196}
    
    TBR=mcasas@chromium.org,hiroh@chromium.org,dstaessens@chromium.org
    
    Change-Id: I2e6496e9148f12d37f746a7d0b1e0e26cee70341
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 909547, 914954
    Reviewed-on: https://chromium-review.googlesource.com/c/1377470
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616522}
    Former-commit-id: 57d51cc17e0e2c806d1b03eb6ce0905ed25f4cda

--- media/gpu/vaapi/vaapi_video_decode_accelerator.cc
+++ media/gpu/vaapi/vaapi_video_decode_accelerator.cc
@@ -167,7 +167,6 @@ VaapiVideoDecodeAccelerator::VaapiVideoDecodeAccelerator(
     : state_(kUninitialized),
       input_ready_(&lock_),
       vaapi_picture_factory_(new VaapiPictureFactory()),
-      profile_(VIDEO_CODEC_PROFILE_UNKNOWN),
       surfaces_available_(&lock_),
       decode_using_client_picture_buffers_(false),
       task_runner_(base::ThreadTaskRunnerHandle::Get()),
@@ -177,6 +176,7 @@ VaapiVideoDecodeAccelerator::VaapiVideoDecodeAccelerator(
       requested_num_pics_(0),
       requested_num_reference_frames_(0),
       previously_requested_num_reference_frames_(0),
+      profile_(VIDEO_CODEC_PROFILE_UNKNOWN),
       make_context_current_cb_(make_context_current_cb),
       bind_image_cb_(bind_image_cb),
       weak_this_factory_(this) {
@@ -284,12 +284,9 @@ void VaapiVideoDecodeAccelerator::OutputPicture(
                                  PLATFORM_FAILURE, );
   }
 
-  {
-    base::AutoLock auto_lock(lock_);
-    TRACE_COUNTER_ID2("media,gpu", "Vaapi frames at client", this, "used",
-                      pictures_.size() - available_picture_buffers_.size(),
-                      "available", available_picture_buffers_.size());
-  }
+  TRACE_COUNTER_ID2("media,gpu", "Vaapi frames at client", this, "used",
+                    pictures_.size() - available_picture_buffers_.size(),
+                    "available", available_picture_buffers_.size());
 
   DVLOGF(4) << "Notifying output picture id " << output_id << " for input "
             << input_id
@@ -310,7 +307,6 @@ void VaapiVideoDecodeAccelerator::TryOutputPicture() {
   if (!client_)
     return;
 
-  base::AutoLock auto_lock(lock_);
   if (pending_output_cbs_.empty() || available_picture_buffers_.empty())
     return;
 
@@ -544,7 +540,6 @@ void VaapiVideoDecodeAccelerator::TryFinishSurfaceSetChange() {
   if (!awaiting_va_surfaces_recycle_)
     return;
 
-  base::AutoLock auto_lock(lock_);
   const size_t kExpectedMaxAvailableVASurfaces =
       decode_using_client_picture_buffers_
           ? pictures_.size()
@@ -584,7 +579,7 @@ void VaapiVideoDecodeAccelerator::TryFinishSurfaceSetChange() {
   VLOGF(2) << "Requesting " << requested_num_pics_
            << " pictures of size: " << requested_pic_size_.ToString();
 
-  const VideoPixelFormat format = GfxBufferFormatToVideoPixelFormat(
+  VideoPixelFormat format = GfxBufferFormatToVideoPixelFormat(
       vaapi_picture_factory_->GetBufferFormat());
   task_runner_->PostTask(
       FROM_HERE,
@@ -705,7 +700,6 @@ void VaapiVideoDecodeAccelerator::ImportBufferForPicture(
     return;
   }
 
-  base::AutoLock auto_lock(lock_);
   if (!pictures_.count(picture_buffer_id)) {
     CloseGpuMemoryBufferHandle(gpu_memory_buffer_handle);
 
@@ -740,24 +734,24 @@ void VaapiVideoDecodeAccelerator::ReusePictureBuffer(
   TRACE_EVENT1("media,gpu", "VAVDA::ReusePictureBuffer", "Picture id",
                picture_buffer_id);
 
+  if (!pictures_.count(picture_buffer_id)) {
+    // It's possible that we've already posted a DismissPictureBuffer for this
+    // picture, but it has not yet executed when this ReusePictureBuffer
+    // was posted to us by the client. In that case just ignore this (we've
+    // already dismissed it and accounted for that).
+    DVLOGF(3) << "got picture id=" << picture_buffer_id
+              << " not in use (anymore?).";
+    return;
+  }
+
   {
     base::AutoLock auto_lock(lock_);
-
-    if (!pictures_.count(picture_buffer_id)) {
-      // It's possible that we've already posted a DismissPictureBuffer for this
-      // picture, but it has not yet executed when this ReusePictureBuffer
-      // was posted to us by the client. In that case just ignore this (we've
-      // already dismissed it and accounted for that).
-      DVLOGF(3) << "got picture id=" << picture_buffer_id
-                << " not in use (anymore?).";
-      return;
-    }
-
     available_picture_buffers_.push_back(picture_buffer_id);
-    TRACE_COUNTER_ID2("media,gpu", "Vaapi frames at client", this, "used",
-                      pictures_.size() - available_picture_buffers_.size(),
-                      "available", available_picture_buffers_.size());
   }
+
+  TRACE_COUNTER_ID2("media,gpu", "Vaapi frames at client", this, "used",
+                    pictures_.size() - available_picture_buffers_.size(),
+                    "available", available_picture_buffers_.size());
 
   TryOutputPicture();
 }
@@ -970,10 +964,10 @@ void VaapiVideoDecodeAccelerator::SurfaceReady(
     if (state_ == kResetting || state_ == kDestroying)
       return;
   }
+
   pending_output_cbs_.push(
       base::BindOnce(&VaapiVideoDecodeAccelerator::OutputPicture, weak_this_,
                      dec_surface, bitstream_id, visible_rect, color_space));
-
   TryOutputPicture();
 }
 
