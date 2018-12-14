880d7b0e0bedf05fa38ef2ed3a1edfd5ee01cf53
Mike Reed <reed@google.com>
2018-12-14 16:38:25 +0000

    rm legacy fontmetrics flag
    Change-Id: Ie54911cd061d8343be0feabef8bad63fa6da9419
    Reviewed-on: https://chromium-review.googlesource.com/c/1376732
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Mike Reed <reed@google.com>
    Cr-Commit-Position: refs/heads/master@{#616707}
    Former-commit-id: f06cb3f86a989e0bfe2702641c855f2dbcfc5e73

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -144,10 +144,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_USE_LEGACY_DISTANCE_FIELDS
 #endif
 
-#ifndef SK_SUPPORT_LEGACY_FONTMETRICS_IN_PAINT
-#define SK_SUPPORT_LEGACY_FONTMETRICS_IN_PAINT
-#endif
-
 #ifndef SK_SUPPORT_LEGACY_PAINT_TEXTMEASURE
 #define SK_SUPPORT_LEGACY_PAINT_TEXTMEASURE
 #endif
