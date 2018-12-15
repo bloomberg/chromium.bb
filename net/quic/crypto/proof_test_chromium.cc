d8c940b99f4bf48a360d4a1224948cfcf8209efe
David Benjamin <davidben@chromium.org>
2018-12-15 04:47:15 +0000

    Enable ProofTest.Verify.
    Inspired by pkasting's email, I noticed this...
    
    It seems to work these days. Maybe the BoringSSL switch? The bug has
    some error coming out of NSS.
    
    Bug: 514468
    Change-Id: Ic0ba0af9b19019fea230e9ca171e2daf09fccbb2
    Reviewed-on: https://chromium-review.googlesource.com/c/1378817
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#616955}
    Former-commit-id: b28c476f724774d786a63a9e9e37e2ed925240c1

--- net/quic/crypto/proof_test_chromium.cc
+++ net/quic/crypto/proof_test_chromium.cc
@@ -127,9 +127,7 @@ INSTANTIATE_TEST_CASE_P(
     ProofTest,
     ::testing::ValuesIn(quic::AllSupportedTransportVersions()));
 
-// TODO(rtenneti): Enable testing of quic::ProofVerifier. See
-// http://crbug.com/514468.
-TEST_P(ProofTest, DISABLED_Verify) {
+TEST_P(ProofTest, Verify) {
   std::unique_ptr<quic::ProofSource> source(
       quic::test::crypto_test_utils::ProofSourceForTesting());
   std::unique_ptr<quic::ProofVerifier> verifier(
