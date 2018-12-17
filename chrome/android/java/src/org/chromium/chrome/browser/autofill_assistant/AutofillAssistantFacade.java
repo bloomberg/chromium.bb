5944d987b1ec681f47c753135bf7770abc23586a
Stephane Zermatten <szermatt@chromium.org>
2018-12-17 11:22:54 +0000

    [Autofill Assistant] Allow trusted apps to report acceptance of T&C.
    With this change, we can have different terms and conditions for
    different usages of the Autofill Assistant feature without ending up in
    a situation where a user needs to agree to two separate terms and
    conditions or onboarding screen before they're allowed to use the
    feature.
    
    This change allows trusted apps to tell Chrome that the user has agreed
    to the terms and conditions relevant to the current intent, which
    bypasses the generic terms and conditions screen hardcoded into the
    client.
    
    Trust is based on a PendingIntent included into the original Intent,
    used to get the package of the calling application.
    
    Bug: 806868
    Change-Id: Ia49891b3b9ab11010d49e3fac0e131c8b73abd11
    Reviewed-on: https://chromium-review.googlesource.com/c/1367724
    Commit-Queue: Stephane Zermatten <szermatt@chromium.org>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617083}
    Former-commit-id: ce098073ed9e4ec0ac24e36dbb22aa4b5a6b7771

--- chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantFacade.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantFacade.java
@@ -4,31 +4,56 @@
 
 package org.chromium.chrome.browser.autofill_assistant;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.VisibleForTesting;
 
+import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.EmptyTabModelObserver;
 import org.chromium.chrome.browser.tabmodel.TabModel;
 import org.chromium.chrome.browser.tabmodel.TabModel.TabSelectionType;
+import org.chromium.chrome.browser.util.IntentUtils;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /** Facade for starting Autofill Assistant on a custom tab. */
 public class AutofillAssistantFacade {
-    /** Prefix for Intent extras relevant to this feature. */
+    /**
+     * Prefix for Intent extras relevant to this feature.
+     *
+     * <p>Intent starting with this prefix are reported to the controller as parameters, except for
+     * the ones starting with {@code INTENT_SPECIAL_PREFIX}.
+     */
     private static final String INTENT_EXTRA_PREFIX =
             "org.chromium.chrome.browser.autofill_assistant.";
 
+    /** Prefix for intent extras which are not parameters. */
+    private static final String INTENT_SPECIAL_PREFIX = INTENT_EXTRA_PREFIX + "special.";
 
     /** Special parameter that enables the feature. */
     private static final String PARAMETER_ENABLED = "ENABLED";
 
-    /** Returns true if all conditions are satisfied to start Autofill Assistant. */
+    /**
+     * Boolean parameter that trusted apps can use to declare that the user has agreed to Terms and
+     * Conditions that cover the use of Autofill Assistant in Chrome for that specific invocation.
+     */
+    private static final String AGREED_TO_TC = "AGREED_TO_TC";
+
+    /** Pending intent sent by first-party apps. */
+    private static final String PENDING_INTENT_NAME = INTENT_SPECIAL_PREFIX + "PENDING_INTENT";
+
+    /** Package names of trusted first-party apps, from the pending intent. */
+    private static final String[] TRUSTED_CALLER_PACKAGES = {
+            "com.google.android.googlequicksearchbox", // GSA
+    };
+
+    /** Returns true if conditions are satisfied to attempt to start Autofill Assistant. */
     public static boolean isConfigured(@Nullable Bundle intentExtras) {
         return getBooleanParameter(intentExtras, PARAMETER_ENABLED);
     }
@@ -47,6 +72,10 @@ public class AutofillAssistantFacade {
         final AbstractAutofillAssistantUiController targetController = controller == null
                 ? new AutofillAssistantUiController(activity, parameters)
                 : controller;
+        if (canStart(activity.getInitialIntent())) {
+            initiateAutofillAssistant(activity, parameters, targetController);
+            return;
+        }
         if (AutofillAssistantPreferencesUtil.getShowOnboarding()) {
             FirstRunScreen.show(activity, (result) -> {
                 if (result) initiateAutofillAssistant(activity, parameters, targetController);
@@ -54,9 +83,6 @@ public class AutofillAssistantFacade {
             return;
         }
 
-        if (AutofillAssistantPreferencesUtil.isAutofillAssistantSwitchOn()) {
-            initiateAutofillAssistant(activity, parameters, targetController);
-        }
         // We don't have consent to start Autofill Assistant and cannot show initial screen.
         // Do nothing.
     }
@@ -101,7 +127,8 @@ public class AutofillAssistantFacade {
 
     /** Return the value if the given boolean parameter from the extras. */
     private static boolean getBooleanParameter(@Nullable Bundle extras, String parameterName) {
-        return extras != null && extras.getBoolean(INTENT_EXTRA_PREFIX + parameterName, false);
+        return extras != null
+                && IntentUtils.safeGetBoolean(extras, INTENT_EXTRA_PREFIX + parameterName, false);
     }
 
     /** Returns a map containing the extras starting with {@link #INTENT_EXTRA_PREFIX}. */
@@ -109,12 +136,45 @@ public class AutofillAssistantFacade {
         Map<String, String> result = new HashMap<>();
         if (extras != null) {
             for (String key : extras.keySet()) {
-                if (key.startsWith(INTENT_EXTRA_PREFIX)) {
+                if (key.startsWith(INTENT_EXTRA_PREFIX) && !key.startsWith(INTENT_SPECIAL_PREFIX)) {
                     result.put(key.substring(INTENT_EXTRA_PREFIX.length()),
                             extras.get(key).toString());
                 }
             }
         }
         return result;
+    }
+
+    /** Returns {@code true} if we can start right away. */
+    private static boolean canStart(Intent intent) {
+        return (AutofillAssistantPreferencesUtil.isAutofillAssistantSwitchOn()
+                       && !AutofillAssistantPreferencesUtil.getShowOnboarding())
+                || hasAgreedToTc(intent);
+    }
+
+    /**
+     * Returns {@code true} if the user has already agreed to specific terms and conditions for the
+     * current task, that cover the use of autofill assistant. There's no need to show the generic
+     * first-time screen for that call.
+     */
+    private static boolean hasAgreedToTc(Intent intent) {
+        return getBooleanParameter(intent.getExtras(), AGREED_TO_TC)
+                && callerIsOnWhitelist(intent, TRUSTED_CALLER_PACKAGES);
+    }
+
+    /** Returns {@code true} if the caller is on the given whitelist. */
+    private static boolean callerIsOnWhitelist(Intent intent, String[] whitelist) {
+        PendingIntent pendingIntent =
+                IntentUtils.safeGetParcelableExtra(intent, PENDING_INTENT_NAME);
+        if (pendingIntent == null) {
+            return false;
+        }
+        String packageName = ApiCompatibilityUtils.getCreatorPackage(pendingIntent);
+        for (String whitelistedPackage : whitelist) {
+            if (whitelistedPackage.equals(packageName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
