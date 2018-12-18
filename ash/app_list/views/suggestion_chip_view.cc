c431da4d6fc9db2ad4e96bb59d5649a50891e78a
weidongg@chromium.org <weidongg@chromium.org>
2018-12-18 06:25:45 +0000

    Make suggestion chip more visible
    Change the suggestion chip background color to make its contrast higher
    based on specs.
    
    Bug: 911749
    Change-Id: Iec7c66319cf8067b0207699e1444622262dc826b
    Reviewed-on: https://chromium-review.googlesource.com/c/1379592
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Weidong Guo <weidongg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617394}
    Former-commit-id: 20519a93bd8f22bc06f4f367942c17b91b53c2f2

--- ash/app_list/views/suggestion_chip_view.cc
+++ ash/app_list/views/suggestion_chip_view.cc
@@ -36,7 +36,7 @@ constexpr int kAssistantStrokeWidthDip = 1;
 
 // App list specific style:
 constexpr SkColor kAppListBackgroundColor =
-    SkColorSetA(gfx::kGoogleGrey900, 0x33);
+    SkColorSetA(gfx::kGoogleGrey100, 0x14);
 constexpr SkColor kAppListTextColor = gfx::kGoogleGrey100;
 constexpr SkColor kAppListRippleColor = SkColorSetA(gfx::kGoogleGrey100, 0x0F);
 constexpr SkColor kAppListFocusColor = SkColorSetA(gfx::kGoogleGrey100, 0x14);
@@ -159,13 +159,12 @@ void SuggestionChipView::OnPaintBackground(gfx::Canvas* canvas) {
   gfx::Rect bounds = GetContentsBounds();
 
   // Background.
+  flags.setColor(assistant_style_ ? kAssistantBackgroundColor
+                                  : kAppListBackgroundColor);
+  canvas->DrawRoundRect(bounds, height() / 2, flags);
   if (HasFocus()) {
     flags.setColor(assistant_style_ ? kAssistantFocusColor
                                     : kAppListFocusColor);
-    canvas->DrawRoundRect(bounds, height() / 2, flags);
-  } else {
-    flags.setColor(assistant_style_ ? kAssistantBackgroundColor
-                                    : kAppListBackgroundColor);
     canvas->DrawRoundRect(bounds, height() / 2, flags);
   }
 
