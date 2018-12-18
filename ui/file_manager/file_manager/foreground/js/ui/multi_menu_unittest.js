540b494ef17d30746c77eda9293d6b52761136ce
Alex Danilo <adanilo@chromium.org>
2018-12-18 23:42:23 +0000

    Added multiple unit tests for sub-menu behavior.
    Add unit tests to exercise the sub-menu functionalilty for
    various mouse events (over, out, mousedown). Also checking
    click outside the menu area hides all the displayed menus.
    
    Bug: 762662
    Change-Id: I6ecd7527567ca002a117a14694ba064684213942
    Reviewed-on: https://chromium-review.googlesource.com/c/1381318
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617672}
    Former-commit-id: 5ea1d6cc04ee3146d6866f8a6b3ee58d0e1c246e

--- ui/file_manager/file_manager/foreground/js/ui/multi_menu_unittest.js
+++ ui/file_manager/file_manager/foreground/js/ui/multi_menu_unittest.js
@@ -4,7 +4,14 @@
 
 'use strict';
 
-var menubutton;
+/** @type {cr.ui.MultiMenuButton} */
+let menubutton;
+
+/** @type {cr.ui.Menu} */
+let topMenu;
+
+/** @type {cr.ui.Menu} */
+let subMenu;
 
 // Set up test components.
 function setUp() {
@@ -16,7 +23,7 @@ function setUp() {
     '  }',
     '  cr-menu-item {',
     '    width: 10px;',
-    '    height: 10px',
+    '    height: 10px;',
     '    display: block;',
     '  }',
     '</style>',
@@ -27,7 +34,7 @@ function setUp() {
     '<cr-menu id="menu" hidden>',
     '  <cr-menu-item command="#default-task"></cr-menu-item>',
     '  <cr-menu-item command="#more-actions"></cr-menu-item>',
-    '  <cr-menu-item command="#show-submenu"',
+    '  <cr-menu-item id="host-sub-menu" command="#show-submenu"',
     'visibleif="full-page" class="hide-on-toolbar"',
     'sub-menu="#sub-menu" hidden></cr-menu-item>',
     '</cr-menu>',
@@ -40,14 +47,146 @@ function setUp() {
   cr.ui.decorate('command', cr.ui.Command);
   menubutton =
       util.queryDecoratedElement('#test-menu-button', cr.ui.MultiMenuButton);
+  topMenu = util.queryDecoratedElement('#menu', cr.ui.Menu);
+  subMenu = util.queryDecoratedElement('#sub-menu', cr.ui.Menu);
 }
 
 /**
- *  Tests that making the top level menu visible doesn't
- *  cause the sub-menu to become visible.
+ * Send a 'mouseover' event to the element target of a query.
+ * @param {string} targetQuery Query to specify the element.
+ */
+function sendMouseOver(targetQuery) {
+  const event = new MouseEvent('mouseover', {
+    bubbles: true,
+    composed: true,  // Allow the event to bubble past shadow DOM root.
+  });
+  const target = document.querySelector(targetQuery);
+  assertTrue(!!target);
+  return target.dispatchEvent(event);
+}
+
+/**
+ * Send a 'mouseover' event to the element target of a query.
+ * @param {string} targetQuery Query to specify the element.
+ * @param {number} x Position of the event in 'X'.
+ * @param {number} y Position of the event in 'X'.
+ */
+function sendMouseOut(targetQuery, x, y) {
+  const event = new MouseEvent('mouseout', {
+    bubbles: true,
+    composed: true,  // Allow the event to bubble past shadow DOM root.
+    clientX: x,
+    clientY: y,
+  });
+  const target = document.querySelector(targetQuery);
+  assertTrue(!!target);
+  return target.dispatchEvent(event);
+}
+
+/**
+ * Tests that making the top level menu visible doesn't
+ * cause the sub-menu to become visible.
  */
 function testShowMenuDoesntShowSubMenu() {
   menubutton.showMenu(true);
-  const subMenu = document.querySelector('#sub-menu');
+  // Check the top level menu is not hidden.
+  assertFalse(topMenu.hasAttribute('hidden'));
+  // Check the sub-menu is hidden
+  assertTrue(subMenu.hasAttribute('hidden'));
+}
+
+/**
+ * Tests that a 'mouseover' event on top of normal menu-items
+ * doesn't cause the sub-menu to become visible.
+ */
+function testMouseOverNormalItemsDoesntShowSubMenu() {
+  menubutton.showMenu(true);
+  sendMouseOver('#default-task');
+  assertTrue(subMenu.hasAttribute('hidden'));
+  sendMouseOver('#more-actions');
+  assertTrue(subMenu.hasAttribute('hidden'));
+}
+
+/**
+ * Tests that 'mouseover' on a menu-item with 'show-submenu' command
+ * causes the sub-menu to become visible.
+ */
+function testMouseOverHostMenuShowsSubMenu() {
+  menubutton.showMenu(true);
+  sendMouseOver('#host-sub-menu');
+  assertFalse(subMenu.hasAttribute('hidden'));
+}
+
+/**
+ * Tests that 'mouseout' with the mouse over the top level
+ * menu causes the sub-menu to hide.
+ */
+function testMouseoutFromHostMenuItemToHostMenu() {
+  menubutton.showMenu(true);
+  sendMouseOver('#host-sub-menu');
+  assertFalse(subMenu.hasAttribute('hidden'));
+  // Get the location of one of our menu-items to send with the event.
+  const item = document.querySelector('#default-task');
+  const loc = item.getBoundingClientRect();
+  sendMouseOut('#host-sub-menu', loc.left, loc.top);
+  assertTrue(subMenu.hasAttribute('hidden'));
+}
+
+/**
+ * Tests that 'mouseout' with the mouse over the sub-menu
+ * doesn't hide the sub-menu.
+ */
+function testMouseoutFromHostMenuToSubMenu() {
+  menubutton.showMenu(true);
+  sendMouseOver('#host-sub-menu');
+  assertFalse(subMenu.hasAttribute('hidden'));
+  // Get the location of our sub-menu to send with the event.
+  const loc = subMenu.getBoundingClientRect();
+  sendMouseOut('#host-sub-menu', loc.left, loc.top);
+  assertFalse(subMenu.hasAttribute('hidden'));
+}
+
+/**
+ * Tests that selecting a menu-item with a 'show-submenu' command
+ * doesn't cause the sub-menu to become visible.
+ */
+function testSelectHostMenuItem() {
+  menubutton.showMenu(true);
+  topMenu.selectedIndex = 2;
+  const hostItem = document.querySelector('#host-sub-menu');
+  assert(hostItem.hasAttribute('selected'));
+  assertEquals(hostItem.getAttribute('selected'), 'selected');
+  assertTrue(subMenu.hasAttribute('hidden'));
+}
+
+/**
+ * Tests that selecting a menu-item with a 'show-submenu' command
+ * followed by calling the showSubMenu() method causes the
+ * sub-menu to become visible.
+ * (Note: in an application, this would happen from a command
+ * being executed rather than a direct showSubMenu() call.)
+ */
+function testSelectHostMenuItemAndCallShowSubMenu() {
+  testSelectHostMenuItem();
+  menubutton.showSubMenu();
+  assertFalse(subMenu.hasAttribute('hidden'));
+}
+
+/**
+ * Tests that a mouse click outside of a menu and sub-menu causes
+ * both menus to hide.
+ */
+function testClickOutsideVisibleMenuAndSubMenu() {
+  testSelectHostMenuItemAndCallShowSubMenu();
+  const event = new MouseEvent('mousedown', {
+    bubbles: true,
+    cancelable: true,
+    view: window,
+    composed: true,  // Allow the event to bubble past shadow DOM root.
+    clientX: 0,      // 0, 0 is in the padding area of the viewport
+    clientY: 0,
+  });
+  menubutton.dispatchEvent(event);
+  assertTrue(topMenu.hasAttribute('hidden'));
   assertTrue(subMenu.hasAttribute('hidden'));
 }
