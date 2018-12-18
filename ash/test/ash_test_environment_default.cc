668f5f279f62fb02cfbc13fdd6c41a192e79af34
Scott Violet <sky@chromium.org>
2018-12-18 16:19:03 +0000

    chromeos: removes AshTestEnvironment::Get100PercentResourceFileName()
    It's no longer needed.
    
    BUG=none
    TEST=none
    
    Change-Id: I3d53901a809abf87cc93319672b43bf8b8b8eba7
    Reviewed-on: https://chromium-review.googlesource.com/c/1381729
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617513}
    Former-commit-id: e9d8df754128fd99db3228272b9e4747be607fb9

--- ash/test/ash_test_environment_default.cc
+++ ash/test/ash_test_environment_default.cc
@@ -41,9 +41,4 @@ std::unique_ptr<AshTestEnvironment> AshTestEnvironment::Create() {
   return std::make_unique<AshTestEnvironmentDefault>();
 }
 
-// static
-std::string AshTestEnvironment::Get100PercentResourceFileName() {
-  return "ash_test_resources_100_percent.pak";
-}
-
 }  // namespace ash
