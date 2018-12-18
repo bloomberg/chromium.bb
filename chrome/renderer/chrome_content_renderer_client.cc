d6f5aeb8331ef0919643f422ecd5da4e642ac431
Mounir Lamouri <mlamouri@chromium.org>
2018-12-18 13:10:49 +0000

    Flash Embed Rewrite: refactor code to make it simpler to add support for other websites.
    Bug: 877439
    Change-Id: I25c23fc0328bfeb5fbc9e1f09174120593b8ca42
    Reviewed-on: https://chromium-review.googlesource.com/c/1379522
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617461}
    Former-commit-id: a85eb7444ffd5f37205472aa55e2520cf96faa0a

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -49,6 +49,7 @@
 #include "chrome/renderer/chrome_render_view_observer.h"
 #include "chrome/renderer/content_settings_observer.h"
 #include "chrome/renderer/loadtimes_extension_bindings.h"
+#include "chrome/renderer/media/flash_embed_rewrite.h"
 #include "chrome/renderer/media/webrtc_logging_message_filter.h"
 #include "chrome/renderer/net/net_error_helper.h"
 #include "chrome/renderer/net_benchmarking_extension.h"
@@ -220,16 +221,7 @@ using content::WebPluginInfo;
 using content::WebPluginMimeType;
 using extensions::Extension;
 
-namespace internal {
-const char kFlashYouTubeRewriteUMA[] = "Plugin.Flash.YouTubeRewrite";
-}  // namespace internal
-
 namespace {
-
-void RecordYouTubeRewriteUMA(internal::YouTubeRewriteStatus status) {
-  UMA_HISTOGRAM_ENUMERATION(internal::kFlashYouTubeRewriteUMA, status,
-                            internal::NUM_PLUGIN_ERROR);
-}
 
 // Whitelist PPAPI for Android Runtime for Chromium. (See crbug.com/383937)
 #if BUILDFLAG(ENABLE_PLUGINS)
@@ -1616,57 +1608,7 @@ GURL ChromeContentRendererClient::OverrideFlashEmbedWithHTML(const GURL& url) {
   if (!url.is_valid())
     return GURL();
 
-  // We'll only modify YouTube Flash embeds. The URLs can be recognized since
-  // they're in the following form: youtube.com/v/VIDEO_ID. So, we check to see
-  // if the given URL does follow that format.
-  if (!url.DomainIs("youtube.com") && !url.DomainIs("youtube-nocookie.com"))
-    return GURL();
-  if (url.path().find("/v/") != 0)
-    return GURL();
-
-  std::string url_str = url.spec();
-  internal::YouTubeRewriteStatus result = internal::NUM_PLUGIN_ERROR;
-
-  // If the website is using an invalid YouTube URL, we'll try and
-  // fix the URL by ensuring that if there are multiple parameters,
-  // the parameter string begins with a "?" and then follows with a "&"
-  // for each subsequent parameter. We do this because the Flash video player
-  // has some URL correction capabilities so we don't want this move to HTML5
-  // to break webpages that used to work.
-  size_t index = url_str.find_first_of("&?");
-  bool invalid_url = index != std::string::npos && url_str.at(index) == '&';
-
-  if (invalid_url) {
-    // ? should appear first before all parameters
-    url_str.replace(index, 1, "?");
-
-    // Replace all instances of ? (after the first) with &
-    for (size_t pos = index + 1;
-         (pos = url_str.find("?", pos)) != std::string::npos; pos += 1) {
-      url_str.replace(pos, 1, "&");
-    }
-  }
-
-  GURL corrected_url = GURL(url_str);
-  // Chrome used to only rewrite embeds with enablejsapi=1 on mobile for
-  // backward compatibility but with Flash embeds deprecated by YouTube, they
-  // are rewritten on all platforms. However, a different result is used in
-  // order to keep track of how popular they are.
-  if (corrected_url.query().find("enablejsapi=1") != std::string::npos)
-    result = internal::SUCCESS_ENABLEJSAPI;
-
-  // Change the path to use the YouTube HTML5 API
-  std::string path = corrected_url.path();
-  path.replace(path.find("/v/"), 3, "/embed/");
-
-  url::Replacements<char> r;
-  r.SetPath(path.c_str(), url::Component(0, path.length()));
-
-  if (result == internal::NUM_PLUGIN_ERROR)
-    result = invalid_url ? internal::SUCCESS_PARAMS_REWRITE : internal::SUCCESS;
-
-  RecordYouTubeRewriteUMA(result);
-  return corrected_url.ReplaceComponents(r);
+  return FlashEmbedRewrite::RewriteFlashEmbedURL(url);
 }
 
 std::unique_ptr<base::TaskScheduler::InitParams>
