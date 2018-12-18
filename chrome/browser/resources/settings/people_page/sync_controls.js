8dad4159a190dbe3b091d91e1e896c8b660ad3fc
Thomas Tangl <tangltom@chromium.org>
2018-12-18 17:15:13 +0000

    [unified-consent] Create settings-sync-controls element
    All sync data type toggles are moved into one polymer
    element.
    
    Note: There is no structural change in this CL.
    In a follow-up CL the toggles are moved to a subpage
    which is accessible from the sync page.
    
    Bug: 912598
    Change-Id: I7d07a7051d34840d94a1229c7dae92fa2c1e680c
    Reviewed-on: https://chromium-review.googlesource.com/c/1367648
    Commit-Queue: Thomas Tangl <tangltom@chromium.org>
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#615490}(cherry picked from commit 064b3071f1ff5fc41a829dce3da2383388892200 [formerly 6c781857419f5209ff15bf5dca5208f39ab6a4a5])
    Reviewed-on: https://chromium-review.googlesource.com/c/1382913
    Reviewed-by: Thomas Tangl <tangltom@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3626@{#429}
    Cr-Branched-From: 5302c6863bade38a5d283a7cce26a309d8e8e755 [formerly d897fb137fbaaa9355c0c93124cc048824eb1e65]-refs/heads/master@{#612437}
    Former-commit-id: 19183d3f33259f7b83c9cbe3c557fe24d7f854d4

---
+++ chrome/browser/resources/settings/people_page/sync_controls.js
@@ -0,0 +1,149 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(function() {
+
+/**
+ * Names of the individual data type properties to be cached from
+ * settings.SyncPrefs when the user checks 'Sync All'.
+ * @type {!Array<string>}
+ */
+const SyncPrefsIndividualDataTypes = [
+  'appsSynced',
+  'extensionsSynced',
+  'preferencesSynced',
+  'autofillSynced',
+  'typedUrlsSynced',
+  'themesSynced',
+  'bookmarksSynced',
+  'passwordsSynced',
+  'tabsSynced',
+  'paymentsIntegrationEnabled',
+];
+
+/**
+ * @fileoverview
+ * 'settings-sync-controls' contains all sync data type controls.
+ */
+Polymer({
+  is: 'settings-sync-controls',
+
+  behaviors: [WebUIListenerBehavior],
+
+  properties: {
+    /**
+     * The current sync preferences, supplied by SyncBrowserProxy.
+     * @type {settings.SyncPrefs|undefined}
+     */
+    syncPrefs: Object,
+  },
+
+  /** @private {?settings.SyncBrowserProxy} */
+  browserProxy_: null,
+
+  /**
+   * Caches the individually selected synced data types. This is used to
+   * be able to restore the selections after checking and unchecking Sync All.
+   * @private {?Object}
+   */
+  cachedSyncPrefs_: null,
+
+  /** @override */
+  created: function() {
+    this.browserProxy_ = settings.SyncBrowserProxyImpl.getInstance();
+  },
+
+  /** @override */
+  attached: function() {
+    this.addWebUIListener(
+        'sync-prefs-changed', this.handleSyncPrefsChanged_.bind(this));
+  },
+
+  /**
+   * Handler for when the sync preferences are updated.
+   * @private
+   */
+  handleSyncPrefsChanged_: function(syncPrefs) {
+    this.syncPrefs = syncPrefs;
+
+    // If autofill is not registered or synced, force Payments integration off.
+    if (!this.syncPrefs.autofillRegistered || !this.syncPrefs.autofillSynced)
+      this.set('syncPrefs.paymentsIntegrationEnabled', false);
+  },
+
+  /**
+   * Handler for when the sync all data types checkbox is changed.
+   * @param {!Event} event
+   * @private
+   */
+  onSyncAllDataTypesChanged_: function(event) {
+    if (event.target.checked) {
+      this.set('syncPrefs.syncAllDataTypes', true);
+
+      // Cache the previously selected preference before checking every box.
+      this.cachedSyncPrefs_ = {};
+      for (const dataType of SyncPrefsIndividualDataTypes) {
+        // These are all booleans, so this shallow copy is sufficient.
+        this.cachedSyncPrefs_[dataType] = this.syncPrefs[dataType];
+
+        this.set(['syncPrefs', dataType], true);
+      }
+    } else if (this.cachedSyncPrefs_) {
+      // Restore the previously selected preference.
+      for (const dataType of SyncPrefsIndividualDataTypes) {
+        this.set(['syncPrefs', dataType], this.cachedSyncPrefs_[dataType]);
+      }
+    }
+
+    this.onSingleSyncDataTypeChanged_();
+  },
+
+  /**
+   * Handler for when any sync data type checkbox is changed (except autofill).
+   * @private
+   */
+  onSingleSyncDataTypeChanged_: function() {
+    assert(this.syncPrefs);
+    this.browserProxy_.setSyncDatatypes(this.syncPrefs);
+  },
+
+  /**
+   * Handler for when the autofill data type checkbox is changed.
+   * @private
+   */
+  onAutofillDataTypeChanged_: function() {
+    this.set(
+        'syncPrefs.paymentsIntegrationEnabled', this.syncPrefs.autofillSynced);
+
+    this.onSingleSyncDataTypeChanged_();
+  },
+
+  /**
+   * Handler for when the autofill data type checkbox is changed.
+   * @private
+   */
+  onTypedUrlsDataTypeChanged_: function() {
+    this.onSingleSyncDataTypeChanged_();
+  },
+
+  /**
+   * @param {boolean} syncAllDataTypes
+   * @param {boolean} enforced
+   * @return {boolean} Whether the sync checkbox should be disabled.
+   */
+  shouldSyncCheckboxBeDisabled_: function(syncAllDataTypes, enforced) {
+    return syncAllDataTypes || enforced;
+  },
+
+  /**
+   * @param {boolean} syncAllDataTypes
+   * @param {boolean} autofillSynced
+   * @return {boolean} Whether the sync checkbox should be disabled.
+   */
+  shouldPaymentsCheckboxBeDisabled_: function(
+      syncAllDataTypes, autofillSynced) {
+    return syncAllDataTypes || !autofillSynced;
+  },
+});
+})();
