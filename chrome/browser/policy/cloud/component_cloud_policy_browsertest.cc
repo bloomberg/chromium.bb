b66748b17ee0e85b3f7e99993625d4ca106bafa6
Antonio Gomes <tonikitoo@igalia.com>
2018-12-18 14:00:36 +0000

    [s13n] Convert c/b/policy/cloud/component_cloud_policy_browsertest.cc to IdentityManager
    Uses IdentityManager API instead of using the soon to be
    private SigninManager APIs.
    
    BUG=890780
    
    Change-Id: I7dbeb7b51b271f76118b59946e6de06cf7cc458e
    Reviewed-on: https://chromium-review.googlesource.com/c/1381031
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617475}
    Former-commit-id: f59e15e2eaaae59abacb8e93182137c64b4f1562

--- chrome/browser/policy/cloud/component_cloud_policy_browsertest.cc
+++ chrome/browser/policy/cloud/component_cloud_policy_browsertest.cc
@@ -45,9 +45,10 @@
 #else
 #include "chrome/browser/net/system_network_context_manager.h"
 #include "chrome/browser/policy/cloud/user_cloud_policy_manager_factory.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
+#include "chrome/browser/signin/identity_manager_factory.h"
 #include "components/policy/core/common/cloud/user_cloud_policy_manager.h"
-#include "components/signin/core/browser/signin_manager.h"
+#include "services/identity/public/cpp/identity_manager.h"
+#include "services/identity/public/cpp/primary_account_mutator.h"
 #endif
 
 using testing::InvokeWithoutArgs;
@@ -181,12 +182,12 @@ class ComponentCloudPolicyTest : public extensions::ExtensionBrowserTest {
 #else
     // Mock a signed-in user. This is used by the UserCloudPolicyStore to pass
     // the account id to the UserCloudPolicyValidator.
-    SigninManager* signin_manager =
-        SigninManagerFactory::GetForProfile(browser()->profile());
-    ASSERT_TRUE(signin_manager);
-    signin_manager->StartSignInWithRefreshToken(
+    auto* primary_account_mutator =
+        IdentityManagerFactory::GetForProfile(browser()->profile())
+            ->GetPrimaryAccountMutator();
+    primary_account_mutator->LegacyStartSigninWithRefreshTokenForPrimaryAccount(
         "", "account_id", "12345", PolicyBuilder::kFakeUsername,
-        SigninManager::OAuthTokenFetchedCallback());
+        base::OnceCallback<void(const std::string&)>());
 
     UserCloudPolicyManager* policy_manager =
         UserCloudPolicyManagerFactory::GetForBrowserContext(
@@ -220,11 +221,13 @@ class ComponentCloudPolicyTest : public extensions::ExtensionBrowserTest {
 
 #if !defined(OS_CHROMEOS)
   void SignOut() {
-    SigninManager* signin_manager =
-        SigninManagerFactory::GetForProfile(browser()->profile());
-    ASSERT_TRUE(signin_manager);
-    signin_manager->SignOut(signin_metrics::SIGNOUT_TEST,
-                            signin_metrics::SignoutDelete::IGNORE_METRIC);
+    auto* primary_account_mutator =
+        IdentityManagerFactory::GetForProfile(browser()->profile())
+            ->GetPrimaryAccountMutator();
+    primary_account_mutator->ClearPrimaryAccount(
+        identity::PrimaryAccountMutator::ClearAccountsAction::kDefault,
+        signin_metrics::SIGNOUT_TEST,
+        signin_metrics::SignoutDelete::IGNORE_METRIC);
   }
 #endif
 
