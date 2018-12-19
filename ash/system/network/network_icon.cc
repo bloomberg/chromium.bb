36c75da3e4351f622c276d3bebc0da096d118a21
Tony de Luna <tonydeluna@chromium.org>
2018-12-19 05:24:59 +0000

    Remove technology bade from network icons
    This cl removes technology badge from system tray, quick settings,
    pod button and settings.
    
    Bug: 914159
    Change-Id: I40bc06645e56b911ec0a2f7bb9e439e6c5113555
    Reviewed-on: https://chromium-review.googlesource.com/c/1382905
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Tony De Luna <tonydeluna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617733}
    Former-commit-id: eaba3d89c253a625482b5d148c6b8d619dca4995

--- ash/system/network/network_icon.cc
+++ ash/system/network/network_icon.cc
@@ -85,9 +85,6 @@ class NetworkIconImpl {
   // Cached strength index of the network when the icon was last generated.
   int strength_index_;
 
-  // Cached technology badge for the network when the icon was last generated.
-  Badge technology_badge_ = {};
-
   // Cached vpn badge for the network when the icon was last generated.
   Badge vpn_badge_ = {};
 
@@ -304,35 +301,6 @@ int StrengthIndex(int strength) {
   return zero_based_index + 1;
 }
 
-Badge BadgeForNetworkTechnology(const NetworkState* network,
-                                IconType icon_type) {
-  Badge badge = {nullptr, GetDefaultColorForIconType(icon_type)};
-  const std::string& technology = network->network_technology();
-  if (technology == shill::kNetworkTechnologyEvdo) {
-    badge.icon = &kNetworkBadgeTechnologyEvdoIcon;
-  } else if (technology == shill::kNetworkTechnology1Xrtt) {
-    badge.icon = &kNetworkBadgeTechnology1xIcon;
-  } else if (technology == shill::kNetworkTechnologyGprs ||
-             technology == shill::kNetworkTechnologyGsm) {
-    badge.icon = &kNetworkBadgeTechnologyGprsIcon;
-  } else if (technology == shill::kNetworkTechnologyEdge) {
-    badge.icon = &kNetworkBadgeTechnologyEdgeIcon;
-  } else if (technology == shill::kNetworkTechnologyUmts) {
-    badge.icon = &kNetworkBadgeTechnology3gIcon;
-  } else if (technology == shill::kNetworkTechnologyHspa) {
-    badge.icon = &kNetworkBadgeTechnologyHspaIcon;
-  } else if (technology == shill::kNetworkTechnologyHspaPlus) {
-    badge.icon = &kNetworkBadgeTechnologyHspaPlusIcon;
-  } else if (technology == shill::kNetworkTechnologyLte) {
-    badge.icon = &kNetworkBadgeTechnologyLteIcon;
-  } else if (technology == shill::kNetworkTechnologyLteAdvanced) {
-    badge.icon = &kNetworkBadgeTechnologyLteAdvancedIcon;
-  } else {
-    return {};
-  }
-  return badge;
-}
-
 gfx::ImageSkia GetIcon(const NetworkState* network,
                        IconType icon_type,
                        int strength_index) {
@@ -434,11 +402,6 @@ bool NetworkIconImpl::UpdateWirelessStrengthIndex(const NetworkState* network) {
 
 bool NetworkIconImpl::UpdateCellularState(const NetworkState* network) {
   bool dirty = false;
-  const Badge technology_badge = BadgeForNetworkTechnology(network, icon_type_);
-  if (technology_badge != technology_badge_) {
-    technology_badge_ = technology_badge;
-    dirty = true;
-  }
   std::string roaming_state = network->roaming();
   if (roaming_state != roaming_state_) {
     roaming_state_ = roaming_state;
@@ -488,10 +451,7 @@ void NetworkIconImpl::GetBadges(const NetworkState* network, Badges* badges) {
         !IsTrayIcon(icon_type_)) {
       badges->bottom_right = {&kUnifiedNetworkBadgeSecureIcon, icon_color};
     }
-  } else if (type == shill::kTypeWimax) {
-    technology_badge_ = {&kNetworkBadgeTechnology4gIcon, icon_color};
   } else if (type == shill::kTypeCellular) {
-    // technology_badge_ is set in UpdateCellularState.
     if (network->IsConnectedState() &&
         network->roaming() == shill::kRoamingStateRoaming) {
       // For networks that are always in roaming don't show roaming badge.
@@ -505,9 +465,8 @@ void NetworkIconImpl::GetBadges(const NetworkState* network, Badges* badges) {
       }
     }
   }
-  // Only show technology, VPN, and captive portal badges when connected.
+  // Only show VPN, and captive portal badges when connected.
   if (network->IsConnectedState()) {
-    badges->top_left = technology_badge_;
     badges->bottom_left = vpn_badge_;
     if (behind_captive_portal_)
       badges->bottom_right = {&kUnifiedNetworkBadgeCaptivePortalIcon,
