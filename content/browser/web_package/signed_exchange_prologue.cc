64d229022bb0951c67576767f275b9f883421302
Kunihiko Sakamoto <ksakamoto@chromium.org>
2018-12-19 08:58:05 +0000

    SignedExchange: Reject SXG if fallback URL has invalid UTF-8 sequence
    GURL parser happily accepts invalid UTF-8 path, so we need to validate
    the fallback URL string before parsing.
    
    Spec: https://github.com/WICG/webpackage/pull/346
    
    Bug: 916390
    Change-Id: Ife25621e2a41beef01cbf36a5ab523eaee1ea222
    Reviewed-on: https://chromium-review.googlesource.com/c/1382724
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617772}
    Former-commit-id: 772250a93f51155cb5f434b6fbe0e03c7261cf2d

--- content/browser/web_package/signed_exchange_prologue.cc
+++ content/browser/web_package/signed_exchange_prologue.cc
@@ -5,6 +5,7 @@
 #include "content/browser/web_package/signed_exchange_prologue.h"
 
 #include "base/strings/string_piece.h"
+#include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/trace_event/trace_event.h"
 #include "content/browser/web_package/signed_exchange_utils.h"
@@ -100,8 +101,13 @@ FallbackUrlAndAfter FallbackUrlAndAfter::Parse(
   base::StringPiece fallback_url_str(
       reinterpret_cast<const char*>(input.data()),
       before_fallback_url.fallback_url_length());
-  signed_exchange_utils::URLWithRawString fallback_url(fallback_url_str);
+  if (!base::IsStringUTF8(fallback_url_str)) {
+    signed_exchange_utils::ReportErrorAndTraceEvent(
+        devtools_proxy, "`fallbackUrl` is not a valid UTF-8 sequence.");
+    return FallbackUrlAndAfter();
+  }
 
+  signed_exchange_utils::URLWithRawString fallback_url(fallback_url_str);
   if (!fallback_url.url.is_valid()) {
     signed_exchange_utils::ReportErrorAndTraceEvent(
         devtools_proxy, "Failed to parse `fallbackUrl`.");
@@ -114,7 +120,7 @@ FallbackUrlAndAfter FallbackUrlAndAfter::Parse(
   }
   if (fallback_url.url.has_ref()) {
     signed_exchange_utils::ReportErrorAndTraceEvent(
-        devtools_proxy, ":url can't have a fragment.");
+        devtools_proxy, "`fallbackUrl` can't have a fragment.");
     return FallbackUrlAndAfter();
   }
 
