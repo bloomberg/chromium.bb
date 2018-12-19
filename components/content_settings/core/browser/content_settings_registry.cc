0326e25ae59649c84ff7fe35813dafd4d3c6f9d4
Xiaohan Wang <xhwang@chromium.org>
2018-12-19 17:27:23 +0000

    media: Update protected media identifier setting/permission
    When per-origin provisioning is not available, set the default setting
    for "protected media identifier" from "ALLOW" to "ASK". Also update the
    infobar text for this case. See Bug for more context.
    
    Bug: 904883
    Test: Manually tested on Android L and Android O.
    Change-Id: I7de895b592b06806330c90a2a6d0b32591dc4cb4
    Reviewed-on: https://chromium-review.googlesource.com/c/1343342
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617864}
    Former-commit-id: 82911884dd28d02779b0574a4a9d1f8e783db8c3

--- components/content_settings/core/browser/content_settings_registry.cc
+++ components/content_settings/core/browser/content_settings_registry.cc
@@ -16,6 +16,10 @@
 #include "components/content_settings/core/browser/website_settings_registry.h"
 #include "components/content_settings/core/common/content_settings.h"
 
+#if defined(OS_ANDROID)
+#include "media/base/android/media_drm_bridge.h"
+#endif
+
 namespace content_settings {
 
 namespace {
@@ -66,6 +70,19 @@ std::set<ContentSetting> ValidSettings(ContentSetting setting1,
                                        ContentSetting setting4) {
   ContentSetting settings[] = {setting1, setting2, setting3, setting4};
   return std::set<ContentSetting>(settings, settings + arraysize(settings));
+}
+
+ContentSetting GetInitialDefaultContentSettingForProtectedMediaIdentifier() {
+// On Android, the default value is ALLOW or ASK depending on whether per-origin
+// provisioning is used (https://crbug.com/854737 and https://crbug.com/904883).
+// On ChromeOS the default value is always ASK.
+#if defined(OS_ANDROID)
+  return media::MediaDrmBridge::IsPerOriginProvisioningSupported()
+             ? CONTENT_SETTING_ALLOW
+             : CONTENT_SETTING_ASK;
+#else
+  return CONTENT_SETTING_ASK;
+#endif  // defined(OS_ANDROID)
 }
 
 }  // namespace
@@ -264,26 +281,19 @@ void ContentSettingsRegistry::Init() {
            ContentSettingsInfo::PERSISTENT,
            ContentSettingsInfo::EXCEPTIONS_ON_SECURE_ORIGINS_ONLY);
 
+  const auto protected_media_identifier_setting =
+      GetInitialDefaultContentSettingForProtectedMediaIdentifier();
   Register(CONTENT_SETTINGS_TYPE_PROTECTED_MEDIA_IDENTIFIER,
-           "protected-media-identifier",
-// On Android, the default value is ALLOW. See https://crbug.com/854737 for
-// details. On ChromeOS the default value is still ASK.
-#if defined(OS_ANDROID)
-           CONTENT_SETTING_ALLOW,
-#else
-           CONTENT_SETTING_ASK,
-#endif  // defined(OS_ANDROID)
+           "protected-media-identifier", protected_media_identifier_setting,
            WebsiteSettingsInfo::UNSYNCABLE, WhitelistedSchemes(),
            ValidSettings(CONTENT_SETTING_ALLOW, CONTENT_SETTING_BLOCK,
                          CONTENT_SETTING_ASK),
            WebsiteSettingsInfo::REQUESTING_ORIGIN_AND_TOP_LEVEL_ORIGIN_SCOPE,
            WebsiteSettingsRegistry::PLATFORM_ANDROID |
                WebsiteSettingsRegistry::PLATFORM_CHROMEOS,
-#if defined(OS_ANDROID)
-           ContentSettingsInfo::INHERIT_IN_INCOGNITO,
-#else
-           ContentSettingsInfo::INHERIT_IF_LESS_PERMISSIVE,
-#endif  // defined(OS_ANDROID)
+           protected_media_identifier_setting == CONTENT_SETTING_ALLOW
+               ? ContentSettingsInfo::INHERIT_IN_INCOGNITO
+               : ContentSettingsInfo::INHERIT_IF_LESS_PERMISSIVE,
            ContentSettingsInfo::PERSISTENT,
            ContentSettingsInfo::EXCEPTIONS_ON_SECURE_ORIGINS_ONLY);
 
