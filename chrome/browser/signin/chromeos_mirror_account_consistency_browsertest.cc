56dc24b6d97a18966408664746762c63573aacbd
Robbie McElrath <rmcelrath@chromium.org>
2018-12-19 20:38:14 +0000

    Refactor ChromeOsMirrorAccountConsistencyTest to not use net/ directly.
    This is necessary to get the test working with the network service
    enabled.
    
    Bug: 914406
    Change-Id: I12cf941dfcdd6e7264a51c50e94bd026af45447e
    Reviewed-on: https://chromium-review.googlesource.com/c/1383591
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617936}
    Former-commit-id: 076c0bc8b5fc7a58c61ee948f48a4c7c29ee6b02

--- chrome/browser/signin/chromeos_mirror_account_consistency_browsertest.cc
+++ chrome/browser/signin/chromeos_mirror_account_consistency_browsertest.cc
@@ -2,93 +2,61 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/task/post_task.h"
 #include "chrome/browser/chromeos/login/login_manager_test.h"
 #include "chrome/browser/chromeos/login/startup_utils.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/prefs/incognito_mode_prefs.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/profiles/profile_io_data.h"
-#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/signin/account_consistency_mode_manager.h"
-#include "chrome/browser/signin/chrome_signin_helper.h"
 #include "chrome/browser/supervised_user/supervised_user_constants.h"
 #include "chrome/browser/supervised_user/supervised_user_settings_service.h"
 #include "chrome/browser/supervised_user/supervised_user_settings_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/common/pref_names.h"
-#include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/ui_test_utils.h"
 #include "components/account_id/account_id.h"
+#include "components/google/core/common/google_util.h"
+#include "components/network_session_configurator/common/network_switches.h"
 #include "components/prefs/pref_service.h"
-#include "components/signin/core/browser/account_consistency_method.h"
 #include "components/signin/core/browser/signin_header_helper.h"
 #include "components/signin/core/browser/signin_pref_names.h"
 #include "components/user_manager/user.h"
 #include "components/user_manager/user_manager.h"
-#include "content/public/browser/browser_task_traits.h"
-#include "content/public/browser/browser_thread.h"
-#include "content/public/browser/resource_context.h"
-#include "content/public/test/browser_test.h"
+#include "content/public/test/browser_test_utils.h"
+#include "net/test/embedded_test_server/default_handlers.h"
+#include "net/test/embedded_test_server/embedded_test_server.h"
 #include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
-#include "net/url_request/url_request.h"
-#include "net/url_request/url_request_context.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
 namespace {
 
-constexpr char kGaiaUrl[] = "https://accounts.google.com";
-constexpr char kChromeConnectedHeader[] = "X-Chrome-Connected";
+constexpr char kGaiaDomain[] = "accounts.google.com";
 constexpr char kUserEmail[] = "user@gmail.com";
 constexpr char kUserGaiaId[] = "1234567890";
 
-void CheckRequestHeader(net::URLRequest* url_request,
-                        const char* header_name,
-                        const std::string& expected_header_value) {
-  bool expected_has_header = !expected_header_value.empty();
-  std::string actual_header_value;
-  EXPECT_EQ(expected_has_header, url_request->extra_request_headers().GetHeader(
-                                     header_name, &actual_header_value))
-      << header_name << ": " << actual_header_value;
-  if (expected_has_header) {
-    EXPECT_EQ(expected_header_value, actual_header_value);
-  }
-}
-
-void TestMirrorRequestForProfileOnIOThread(
-    ProfileIOData* profile_io,
-    const std::string& expected_header_value) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
-
-  ASSERT_TRUE(profile_io->GetMainRequestContext());
-  std::unique_ptr<net::URLRequest> request =
-      profile_io->GetMainRequestContext()->CreateRequest(
-          GURL(kGaiaUrl), net::DEFAULT_PRIORITY, nullptr,
-          TRAFFIC_ANNOTATION_FOR_TESTS);
-  signin::ChromeRequestAdapter signin_request_adapter(request.get());
-  signin::FixAccountConsistencyRequestHeader(&signin_request_adapter, GURL(),
-                                             profile_io);
-
-  CheckRequestHeader(request.get(), kChromeConnectedHeader,
-                     expected_header_value);
-}
-
 // Checks whether the "X-Chrome-Connected" header of a new request to Google
 // contains |expected_header_value|.
-void TestMirrorRequestForProfile(Profile* profile,
+void TestMirrorRequestForProfile(net::EmbeddedTestServer* test_server,
+                                 Profile* profile,
                                  const std::string& expected_header_value) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  GURL gaia_url(test_server->GetURL("/echoheader?X-Chrome-Connected"));
+  GURL::Replacements replace_host;
+  replace_host.SetHostStr(kGaiaDomain);
+  gaia_url = gaia_url.ReplaceComponents(replace_host);
 
-  ProfileIOData* profile_io =
-      ProfileIOData::FromResourceContext(profile->GetResourceContext());
+  Browser* browser = new Browser(Browser::CreateParams(profile, true));
+  ui_test_utils::NavigateToURLWithDisposition(
+      browser, gaia_url, WindowOpenDisposition::SINGLETON_TAB,
+      ui_test_utils::BROWSER_TEST_WAIT_FOR_NAVIGATION);
 
-  base::RunLoop run_loop;
-  base::PostTaskWithTraitsAndReply(
-      FROM_HERE, {content::BrowserThread::IO},
-      base::BindOnce(TestMirrorRequestForProfileOnIOThread, profile_io,
-                     expected_header_value),
-      run_loop.QuitClosure());
-  run_loop.Run();
+  std::string inner_text;
+  ASSERT_TRUE(content::ExecuteScriptAndExtractString(
+      browser->tab_strip_model()->GetActiveWebContents(),
+      "domAutomationController.send(document.body.innerText);", &inner_text));
+  // /echoheader returns "None" if the header isn't set.
+  inner_text = (inner_text == "None") ? "" : inner_text;
+  EXPECT_EQ(expected_header_value, inner_text);
 }
 
 }  // namespace
@@ -103,7 +71,33 @@ class ChromeOsMirrorAccountConsistencyTest : public chromeos::LoginManagerTest {
       : LoginManagerTest(false, true /* should_initialize_webui */),
         account_id_(AccountId::FromUserEmailGaiaId(kUserEmail, kUserGaiaId)) {}
 
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    chromeos::LoginManagerTest::SetUpCommandLine(command_line);
+
+    // HTTPS server only serves a valid cert for localhost, so this is needed to
+    // load pages from "www.google.com" without an interstitial.
+    command_line->AppendSwitch(switches::kIgnoreCertificateErrors);
+  }
+
+  void SetUpOnMainThread() override {
+    // The production code only allows known ports (80 for http and 443 for
+    // https), but the test server runs on a random port.
+    google_util::IgnorePortNumbersForGoogleURLChecksForTesting();
+
+    // We can't use BrowserTestBase's EmbeddedTestServer because google.com
+    // URL's have to be https.
+    test_server_ = std::make_unique<net::EmbeddedTestServer>(
+        net::EmbeddedTestServer::TYPE_HTTPS);
+    net::test_server::RegisterDefaultHandlers(test_server_.get());
+    ASSERT_TRUE(test_server_->Start());
+
+    chromeos::LoginManagerTest::SetUpOnMainThread();
+  }
+
   const AccountId account_id_;
+
+ protected:
+  std::unique_ptr<net::EmbeddedTestServer> test_server_;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(ChromeOsMirrorAccountConsistencyTest);
@@ -146,7 +140,7 @@ IN_PROC_BROWSER_TEST_F(ChromeOsMirrorAccountConsistencyTest,
 
   ASSERT_EQ(3, signin::PROFILE_MODE_INCOGNITO_DISABLED |
                    signin::PROFILE_MODE_ADD_ACCOUNT_DISABLED);
-  TestMirrorRequestForProfile(profile,
+  TestMirrorRequestForProfile(test_server_.get(), profile,
                               "mode=3,enable_account_consistency=true");
 }
 
@@ -174,5 +168,5 @@ IN_PROC_BROWSER_TEST_F(ChromeOsMirrorAccountConsistencyTest,
   PrefService* prefs = profile->GetPrefs();
   ASSERT_FALSE(prefs->GetBoolean(prefs::kAccountConsistencyMirrorRequired));
 
-  TestMirrorRequestForProfile(profile, "");
+  TestMirrorRequestForProfile(test_server_.get(), profile, "");
 }
