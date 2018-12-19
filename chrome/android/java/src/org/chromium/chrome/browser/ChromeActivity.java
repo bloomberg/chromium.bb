930d05cee0cf1988046539fffbd41d90bd263ddd
Jinsuk Kim <jinsukkim@chromium.org>
2018-12-19 23:29:28 +0000

    EphemeralTab: Fix NPE in ChromeActivity.shouldShowAppMenu
    Null check against LaytoutManager was added when getting
    EphemeralTabPanel to prevent NPE.
    
    TBR=twellington@chromium.org
    
    Bug: 915950
    Change-Id: Id661f46df024bd97785cb343cd514723d374a1b3
    Reviewed-on: https://chromium-review.googlesource.com/c/1381352
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#617637}(cherry picked from commit 5a0464cc58ca6fa974d9d7532693a4342792ed8b [formerly f409f5b62b2d78e1436851640f1f1459a5e8244c])
    Reviewed-on: https://chromium-review.googlesource.com/c/1382654
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3626@{#476}
    Cr-Branched-From: 5302c6863bade38a5d283a7cce26a309d8e8e755 [formerly d897fb137fbaaa9355c0c93124cc048824eb1e65]-refs/heads/master@{#612437}
    Former-commit-id: 6263af258415ff794a11929b1d93289b21e080ce

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -1535,7 +1535,7 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
             return false;
         }
 
-        if (getEphemeralTabPanel() != null && getEphemeralTabPanel().isPanelOpened()) {
+        if (getEphemeralTabPanel() != null && getEphemeralTabPanel().isShowing()) {
             return false;
         }
 
@@ -1806,8 +1806,12 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
         return mReaderModeManager;
     }
 
+    /**
+     * @return The {@code EphemeralTabPanel} or {@code null} if none.
+     */
     public EphemeralTabPanel getEphemeralTabPanel() {
-        return getCompositorViewHolder().getLayoutManager().getEphemeralTabPanel();
+        LayoutManager layoutManager = getCompositorViewHolder().getLayoutManager();
+        return layoutManager != null ? layoutManager.getEphemeralTabPanel() : null;
     }
 
     /**
