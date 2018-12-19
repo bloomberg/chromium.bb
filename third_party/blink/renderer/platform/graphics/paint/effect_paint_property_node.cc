eacebc0033f443f7770c3b5d95100775f14b74a6
Vladimir Levin <vmpstr@chromium.org>
2018-12-19 22:21:08 +0000

    [IsolationNodes]: Ensure to generate raster invalidation on effect parent changes.
    This patch ensures that when an aliased effect node's parent changes, we
    detect that in the EffectPaintPropertyNode::Changed for the purposes
    of generating an appropriate raster invalidation.
    
    R=pdr@chromium.org, wangxianzhu@chromium.org
    
    Bug: 916510
    Change-Id: Ic46df93218eed21a5e868a22cbe72a7c5763aba7
    Reviewed-on: https://chromium-review.googlesource.com/c/1384661
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#617977}
    Former-commit-id: 1504ddd4db783b8ec999cf9b7c36e61c969ede6c

--- third_party/blink/renderer/platform/graphics/paint/effect_paint_property_node.cc
+++ third_party/blink/renderer/platform/graphics/paint/effect_paint_property_node.cc
@@ -32,21 +32,25 @@ FloatRect EffectPaintPropertyNode::MapRect(const FloatRect& input_rect) const {
 bool EffectPaintPropertyNode::Changed(
     const PropertyTreeState& relative_to_state,
     const TransformPaintPropertyNode* transform_not_to_check) const {
-  auto* relative_effect = relative_to_state.Effect()
-                              ? relative_to_state.Effect()->Unalias()
-                              : nullptr;
-  if (transform_not_to_check)
-    transform_not_to_check = transform_not_to_check->Unalias();
-  for (const auto* node = Unalias(); node && node != relative_effect;
-       node = node->Parent() ? node->Parent()->Unalias() : nullptr) {
+  auto* relative_effect = relative_to_state.Effect();
+  auto* relative_transform = relative_to_state.Transform();
+
+  // Note that we can't unalias nodes in the loop conditions, since we need to
+  // check NodeChanged() function on aliased nodes as well (since the parenting
+  // might change).
+  for (const auto* node = this; node && node != relative_effect;
+       node = node->Parent()) {
     if (node->NodeChanged())
       return true;
-    auto* local_transform = node->LocalTransformSpace()
-                                ? node->LocalTransformSpace()->Unalias()
-                                : nullptr;
+
+    // We shouldn't check state on aliased nodes, other than NodeChanged().
+    if (node->IsParentAlias())
+      continue;
+
+    auto* local_transform = node->LocalTransformSpace();
     if (node->HasFilterThatMovesPixels() &&
         local_transform != transform_not_to_check &&
-        local_transform->Changed(*relative_to_state.Transform()->Unalias())) {
+        local_transform->Changed(*relative_transform)) {
       return true;
     }
     // We don't check for change of OutputClip here to avoid N^3 complexity.
