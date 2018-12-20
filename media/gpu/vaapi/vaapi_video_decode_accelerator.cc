627b247af8544708f0b2a2479877b19f3e0d6ee3
Hirokazu Honda <hiroh@chromium.org>
2018-12-20 17:26:21 +0000

    Reland "Re-land "Vaapi: Use GUARDED_BY() static annotations on decoder""
    This reverts commit 58071b8fd6b69f08f0a0590d7748060b1cd727b8 [formerly cb5feb064ec4a02a8591e59c61e44920d7455a47].
    
    Bug: 909547
    Test: CTS and GTS
    Test: VDA unittest with --test_import
    
    Original change's description:
    > Revert "Re-land "Vaapi: Use GUARDED_BY() static annotations on decoder""
    >
    > This reverts commit 0e060ddba024d2734856c55d3fa046b86e533618 [formerly b8ad3f6a5f474abbbb395e89eb25cf76ed1f3c5c].
    >
    > Reason for revert: This causes inifite-loop in IMPORT mode.
    >
    > Original change's description:
    > > Re-land "Vaapi: Use GUARDED_BY() static annotations on decoder"
    > >
    > > Deadlocks in the original code are fixed now.
    > >
    > > TEST=ran VDA tests on Nocturne
    > >
    > > Bug=909547
    > >
    > > > Vaapi: Use GUARDED_BY() static annotations on decoder
    > > >
    > > > This CL introduces GUARDED_BY() static annotation on VaVDA; not all
    > > > members variables need to be guarded, so it adds on a number of them
    > > > a comment about on which thread it is used.
    > >
    > > > Bug: 909547
    > > > Change-Id: I437a82320795aebc56f77cd1254bb7c8c2f2f5da
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/1368590
    > > > Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    > > > Commit-Queue: Miguel Casas <mcasas@chromium.org>
    > > > Cr-Commit-Position: refs/heads/master@{#616196}
    > >
    > > Change-Id: If37bfd2d1f24b9b833bf8b291363b5c19861a6c7
    > > Reviewed-on: https://chromium-review.googlesource.com/c/1377479
    > > Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    > > Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#617781}
    >
    > TBR=mcasas@chromium.org,hiroh@chromium.org,dstaessens@chromium.org
    >
    > Change-Id: Ic4b1e7e8de8403173f71a415094ed23cab51f5e4
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/1385689
    > Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    > Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#618085}
    
    Change-Id: I96f758b3f99032db219ffce1d432c348e6a30e90
    Reviewed-on: https://chromium-review.googlesource.com/c/1385728
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618257}
    Former-commit-id: 5c66ed12a80d9dce342314fbc03240cb44cd3f0b

--- media/gpu/vaapi/vaapi_video_decode_accelerator.cc
+++ media/gpu/vaapi/vaapi_video_decode_accelerator.cc
@@ -294,9 +294,12 @@ void VaapiVideoDecodeAccelerator::OutputPicture(
                                  PLATFORM_FAILURE, );
   }
 
-  TRACE_COUNTER_ID2("media,gpu", "Vaapi frames at client", this, "used",
-                    pictures_.size() - available_picture_buffers_.size(),
-                    "available", available_picture_buffers_.size());
+  {
+    base::AutoLock auto_lock(lock_);
+    TRACE_COUNTER_ID2("media,gpu", "Vaapi frames at client", this, "used",
+                      pictures_.size() - available_picture_buffers_.size(),
+                      "available", available_picture_buffers_.size());
+  }
 
   DVLOGF(4) << "Notifying output picture id " << output_id << " for input "
             << input_id
@@ -317,8 +320,11 @@ void VaapiVideoDecodeAccelerator::TryOutputPicture() {
   if (!client_)
     return;
 
-  if (pending_output_cbs_.empty() || available_picture_buffers_.empty())
-    return;
+  {
+    base::AutoLock auto_lock(lock_);
+    if (pending_output_cbs_.empty() || available_picture_buffers_.empty())
+      return;
+  }
 
   auto output_cb = std::move(pending_output_cbs_.front());
   pending_output_cbs_.pop();
@@ -535,6 +541,7 @@ void VaapiVideoDecodeAccelerator::TryFinishSurfaceSetChange() {
   if (!awaiting_va_surfaces_recycle_)
     return;
 
+  base::AutoLock auto_lock(lock_);
   if (!pending_output_cbs_.empty() ||
       pictures_.size() != available_va_surfaces_.size()) {
     // Either:
@@ -567,7 +574,7 @@ void VaapiVideoDecodeAccelerator::TryFinishSurfaceSetChange() {
   VLOGF(2) << "Requesting " << requested_num_pics_
            << " pictures of size: " << requested_pic_size_.ToString();
 
-  VideoPixelFormat format = GfxBufferFormatToVideoPixelFormat(
+  const VideoPixelFormat format = GfxBufferFormatToVideoPixelFormat(
       vaapi_picture_factory_->GetBufferFormat());
   task_runner_->PostTask(
       FROM_HERE,
@@ -702,27 +709,30 @@ void VaapiVideoDecodeAccelerator::ImportBufferForPicture(
     return;
   }
 
-  if (!pictures_.count(picture_buffer_id)) {
-    CloseGpuMemoryBufferHandle(gpu_memory_buffer_handle);
+  {
+    base::AutoLock auto_lock(lock_);
+    if (!pictures_.count(picture_buffer_id)) {
+      CloseGpuMemoryBufferHandle(gpu_memory_buffer_handle);
 
-    // It's possible that we've already posted a DismissPictureBuffer for this
-    // picture, but it has not yet executed when this ImportBufferForPicture
-    // was posted to us by the client. In that case just ignore this (we've
-    // already dismissed it and accounted for that).
-    DVLOGF(3) << "got picture id=" << picture_buffer_id
-              << " not in use (anymore?).";
-    return;
-  }
+      // It's possible that we've already posted a DismissPictureBuffer for this
+      // picture, but it has not yet executed when this ImportBufferForPicture
+      // was posted to us by the client. In that case just ignore this (we've
+      // already dismissed it and accounted for that).
+      DVLOGF(3) << "got picture id=" << picture_buffer_id
+                << " not in use (anymore?).";
+      return;
+    }
 
-  VaapiPicture* picture = pictures_[picture_buffer_id].get();
-  if (!picture->ImportGpuMemoryBufferHandle(
-          VideoPixelFormatToGfxBufferFormat(pixel_format),
-          gpu_memory_buffer_handle)) {
-    // ImportGpuMemoryBufferHandle will close the handles even on failure, so
-    // we don't need to do this ourselves.
-    VLOGF(1) << "Failed to import GpuMemoryBufferHandle";
-    NotifyError(PLATFORM_FAILURE);
-    return;
+    VaapiPicture* picture = pictures_[picture_buffer_id].get();
+    if (!picture->ImportGpuMemoryBufferHandle(
+            VideoPixelFormatToGfxBufferFormat(pixel_format),
+            gpu_memory_buffer_handle)) {
+      // ImportGpuMemoryBufferHandle will close the handles even on failure, so
+      // we don't need to do this ourselves.
+      VLOGF(1) << "Failed to import GpuMemoryBufferHandle";
+      NotifyError(PLATFORM_FAILURE);
+      return;
+    }
   }
 
   ReusePictureBuffer(picture_buffer_id);
@@ -736,24 +746,24 @@ void VaapiVideoDecodeAccelerator::ReusePictureBuffer(
   TRACE_EVENT1("media,gpu", "VAVDA::ReusePictureBuffer", "Picture id",
                picture_buffer_id);
 
-  if (!pictures_.count(picture_buffer_id)) {
-    // It's possible that we've already posted a DismissPictureBuffer for this
-    // picture, but it has not yet executed when this ReusePictureBuffer
-    // was posted to us by the client. In that case just ignore this (we've
-    // already dismissed it and accounted for that).
-    DVLOGF(3) << "got picture id=" << picture_buffer_id
-              << " not in use (anymore?).";
-    return;
-  }
-
   {
     base::AutoLock auto_lock(lock_);
-    available_picture_buffers_.push_back(picture_buffer_id);
-  }
 
-  TRACE_COUNTER_ID2("media,gpu", "Vaapi frames at client", this, "used",
-                    pictures_.size() - available_picture_buffers_.size(),
-                    "available", available_picture_buffers_.size());
+    if (!pictures_.count(picture_buffer_id)) {
+      // It's possible that we've already posted a DismissPictureBuffer for this
+      // picture, but it has not yet executed when this ReusePictureBuffer
+      // was posted to us by the client. In that case just ignore this (we've
+      // already dismissed it and accounted for that).
+      DVLOGF(3) << "got picture id=" << picture_buffer_id
+                << " not in use (anymore?).";
+      return;
+    }
+
+    available_picture_buffers_.push_back(picture_buffer_id);
+    TRACE_COUNTER_ID2("media,gpu", "Vaapi frames at client", this, "used",
+                      pictures_.size() - available_picture_buffers_.size(),
+                      "available", available_picture_buffers_.size());
+  }
 
   TryOutputPicture();
 }
@@ -966,10 +976,10 @@ void VaapiVideoDecodeAccelerator::SurfaceReady(
     if (state_ == kResetting || state_ == kDestroying)
       return;
   }
-
   pending_output_cbs_.push(
       base::BindOnce(&VaapiVideoDecodeAccelerator::OutputPicture, weak_this_,
                      dec_surface, bitstream_id, visible_rect, color_space));
+
   TryOutputPicture();
 }
 
@@ -1017,14 +1027,16 @@ scoped_refptr<VASurface> VaapiVideoDecodeAccelerator::CreateSurface() {
 void VaapiVideoDecodeAccelerator::RecycleVASurfaceID(
     VASurfaceID va_surface_id) {
   DCHECK(task_runner_->BelongsToCurrentThread());
-  base::AutoLock auto_lock(lock_);
-  available_va_surfaces_.push_back(va_surface_id);
-  if (!decode_using_client_picture_buffers_) {
-    TRACE_COUNTER_ID2("media,gpu", "Vaapi VASurfaceIDs", this, "used",
-                      pictures_.size() - available_va_surfaces_.size(),
-                      "available", available_va_surfaces_.size());
+  {
+    base::AutoLock auto_lock(lock_);
+    available_va_surfaces_.push_back(va_surface_id);
+    if (!decode_using_client_picture_buffers_) {
+      TRACE_COUNTER_ID2("media,gpu", "Vaapi VASurfaceIDs", this, "used",
+                        pictures_.size() - available_va_surfaces_.size(),
+                        "available", available_va_surfaces_.size());
+    }
+    surfaces_available_.Signal();
   }
-  surfaces_available_.Signal();
 
   TryOutputPicture();
 }
