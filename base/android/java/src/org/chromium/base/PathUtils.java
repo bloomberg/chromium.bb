7c6806d5455b547c1506b6a77458de0d780bed40
Joshua Peraza <jperaza@chromium.org>
2018-12-20 22:45:24 +0000

    android: build package paths using chrome's package
    Monochrome uses /system/bin/app_process to load a Java class,
    CrashpadMain, to start the Crashpad handler process. In order for
    app_process to locate this class and for CrashpadMain to be able to
    load the native libraries for Crashpad, CLASSPATH and LD_LIBRARY_PATH
    need to be set with all the paths containing code that the Chrome
    package depends on.
    
    Add PackagePaths to build these paths.
    
    This work is normally done internally by the Android framework:
    https://cs.corp.google.com/android/frameworks/base/core/java/android/app/LoadedApk.java?rcl=7b2b4c838be94e6ca7cfbb255523c800da4a0cce&l=422
    
    Bug: 912739
    Change-Id: I30e4952fb17fea42bdc995296c47cf8a9f440866
    Reviewed-on: https://chromium-review.googlesource.com/c/1366763
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Joshua Peraza <jperaza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618363}
    Former-commit-id: ab42c0a8eb6515d48b4b3ed2325a1a60292be5fd

--- base/android/java/src/org/chromium/base/PathUtils.java
+++ base/android/java/src/org/chromium/base/PathUtils.java
@@ -262,13 +262,4 @@ public abstract class PathUtils {
     public static String getExternalStorageDirectory() {
         return Environment.getExternalStorageDirectory().getAbsolutePath();
     }
-
-    /**
-     * @ return the path to the base apk.
-     */
-    @SuppressWarnings("unused")
-    @CalledByNative
-    public static String getPathToBaseApk() {
-      return ContextUtils.getApplicationContext().getApplicationInfo().sourceDir;
-    }
 }
