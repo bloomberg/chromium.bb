1f219d08abfdc709416d8a8e590be8dc662dc34d
Xida Chen <xidachen@chromium.org>
2018-12-20 21:31:43 +0000

    Reland: Create a PaintWorkletStylePropertyMap
    This CL is a re-land of:
    https://chromium-review.googlesource.com/c/chromium/src/+/1281702
    
    Previous CL depends on the usage of default hash traits implementation
    of enum, which could crash.
    
    In this CL, instead of keeping two hash maps:
    1. HashMap<CSSPropertyID, String> native_values_
    2. HashMap<String, String> custom_values_
    
    We keep one hash map only:
    HashMap<String, String> values_
    
    Because we guarantee that CSS native and custom property name will
    never conflict, this is safe.
    
    PS#1 is the original CL so that we can easily see the diffs.
    
    TBR=pdr@chromium.org
    
    Bug: 884698
    Change-Id: Ifc74a13ba67fa74ff1a3dd9dbca628675b42c777
    Reviewed-on: https://chromium-review.googlesource.com/c/1386648
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618328}
    Former-commit-id: fabee74ba66a490bfdb6014bec5a9f2094a93a9e

---
+++ third_party/blink/renderer/core/css/cssom/paint_worklet_input.cc
@@ -0,0 +1,25 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/css/cssom/paint_worklet_input.h"
+
+namespace blink {
+
+PaintWorkletInput::PaintWorkletInput(
+    const std::string& name,
+    const FloatSize& container_size,
+    float effective_zoom,
+    const Document& document,
+    const ComputedStyle& style,
+    Node* styled_node,
+    const Vector<CSSPropertyID>& native_properties,
+    const Vector<AtomicString>& custom_properties)
+    : name_(name),
+      container_size_(container_size),
+      effective_zoom_(effective_zoom) {
+  style_map_ = MakeGarbageCollected<PaintWorkletStylePropertyMap>(
+      document, style, styled_node, native_properties, custom_properties);
+}
+
+}  // namespace blink
