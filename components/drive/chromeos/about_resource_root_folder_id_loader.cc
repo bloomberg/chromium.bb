f582cbea725f5991254736120b220e356520335d
Stuart Langley <slangley@google.com>
2018-12-20 03:26:23 +0000

    Store the root folder id after it's retrieved.
    As part of measuring the DriveFS requests vs the existing client
    requests I noticed that AboutResource calls seem higher than expected.
    This change should reduce that count. As we are deprecating this client
    I don't plan on making any large scale changes here, but this one seems
    small enough to do.
    
    Change-Id: I0223092ad7f8bdbe3dfe712e129277f7a05a108e
    Reviewed-on: https://chromium-review.googlesource.com/c/1385725
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Stuart Langley <slangley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618096}
    Former-commit-id: 486183bdc56d7a23c8a9acd3544ef10e51c5d210

--- components/drive/chromeos/about_resource_root_folder_id_loader.cc
+++ components/drive/chromeos/about_resource_root_folder_id_loader.cc
@@ -22,8 +22,12 @@ void AboutResourceRootFolderIdLoader::GetRootFolderId(
     const RootFolderIdCallback& callback) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
 
-  // After the initial load GetAboutResource will just return the cached value,
-  // avoiding any network calls.
+  // If we have already read the root folder id, then we can simply return it.
+  if (!root_folder_id_.empty()) {
+    callback.Run(FILE_ERROR_OK, root_folder_id_);
+    return;
+  }
+
   about_resource_loader_->GetAboutResource(
       base::BindRepeating(&AboutResourceRootFolderIdLoader::OnGetAboutResource,
                           weak_ptr_factory_.GetWeakPtr(), callback));
@@ -43,7 +47,9 @@ void AboutResourceRootFolderIdLoader::OnGetAboutResource(
 
   DCHECK(about_resource);
 
-  callback.Run(error, about_resource->root_folder_id());
+  // The root folder id is immutable so we can save it for subsequent calls.
+  root_folder_id_ = about_resource->root_folder_id();
+  callback.Run(error, root_folder_id_);
 }
 
 }  // namespace internal
