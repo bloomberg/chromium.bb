a07ecc3973a9af44e0d6921089045614be758621
Patrick Noland <pnoland@chromium.org>
2018-12-20 00:09:40 +0000

    [Chromeshine] Implement page view observation
    Implement PageViewObserver by observing url changes, tab hiding,
    tab switching, etc.
    Add Activity to PageViewObserver constructor(this will be needed in a
    follow-up change)
    Add PageViewObserverTest.
    
    Bug: 902500
    Change-Id: Ib5fd75849746ab4c9dc320c929ac28f11a4f9159
    Reviewed-on: https://chromium-review.googlesource.com/c/1377170
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618018}
    Former-commit-id: d8d5d63346bf1fc7c72d231a22ef109ce58e7fa8

--- chrome/android/java/src/org/chromium/chrome/browser/usage_stats/PageViewObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/usage_stats/PageViewObserver.java
@@ -4,20 +4,113 @@
 
 package org.chromium.chrome.browser.usage_stats;
 
+import android.app.Activity;
+import android.net.Uri;
+import android.webkit.URLUtil;
+
+import org.chromium.chrome.browser.tab.EmptyTabObserver;
+import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tab.Tab.TabHidingType;
+import org.chromium.chrome.browser.tab.TabObserver;
+import org.chromium.chrome.browser.tabmodel.TabModel.TabSelectionType;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
+import org.chromium.chrome.browser.tabmodel.TabModelSelectorTabModelObserver;
 
 /**
  * Class that observes url and tab changes in order to track when browsing stops and starts for each
- * visited fdqn.
+ * visited fully-qualified domain name (FQDN).
  */
 public class PageViewObserver {
-    private TabModelSelector mTabModelSelector;
-    private EventTracker mEventTracker;
+    private final Activity mActivity;
+    private final TabModelSelectorTabModelObserver mTabModelObserver;
+    private final TabModelSelector mTabModelSelector;
+    private final TabObserver mTabObserver;
+    private final EventTracker mEventTracker;
 
-    public PageViewObserver(TabModelSelector tabModelSelector, EventTracker eventTracker) {
+    private Tab mCurrentTab;
+    private String mLastFqdn;
+
+    public PageViewObserver(
+            Activity activity, TabModelSelector tabModelSelector, EventTracker eventTracker) {
+        mActivity = activity;
         mTabModelSelector = tabModelSelector;
         mEventTracker = eventTracker;
+        mTabObserver = new EmptyTabObserver() {
+            @Override
+            public void onShown(Tab tab, @TabSelectionType int type) {
+                updateUrl(tab.getUrl());
+            }
+
+            @Override
+            public void onHidden(Tab tab, @TabHidingType int type) {
+                updateUrl(null);
+            }
+
+            @Override
+            public void onUpdateUrl(Tab tab, String url) {
+                assert tab == mCurrentTab;
+
+                updateUrl(url);
+            }
+        };
+
+        mTabModelObserver = new TabModelSelectorTabModelObserver(tabModelSelector) {
+            @Override
+            public void didSelectTab(Tab tab, @TabSelectionType int type, int lastId) {
+                assert tab != null;
+                if (tab == mCurrentTab) return;
+
+                switchObserverToTab(tab);
+                updateUrl(tab.getUrl());
+            }
+
+            @Override
+            public void willCloseTab(Tab tab, boolean animate) {
+                assert tab != null;
+                if (tab != mCurrentTab) return;
+
+                updateUrl(null);
+                switchObserverToTab(null);
+            }
+
+            @Override
+            public void tabRemoved(Tab tab) {
+                assert tab != null;
+                if (tab != mCurrentTab) return;
+
+                updateUrl(null);
+                switchObserverToTab(null);
+            }
+        };
+
+        switchObserverToTab(tabModelSelector.getCurrentTab());
     }
 
-    // TODO(pnoland): implement page view observation and push events to EventTracker.
+    private void updateUrl(String newUrl) {
+        String newFqdn = newUrl == null ? "" : Uri.parse(newUrl).getHost();
+        if (mLastFqdn != null && mLastFqdn.equals(newFqdn)) return;
+
+        if (mLastFqdn != null) {
+            mEventTracker.addWebsiteEvent(new WebsiteEvent(
+                    System.currentTimeMillis(), mLastFqdn, WebsiteEvent.EventType.STOP));
+            mLastFqdn = null;
+        }
+
+        if (!URLUtil.isHttpUrl(newUrl) && !URLUtil.isHttpsUrl(newUrl)) return;
+
+        mLastFqdn = newFqdn;
+        mEventTracker.addWebsiteEvent(new WebsiteEvent(
+                System.currentTimeMillis(), mLastFqdn, WebsiteEvent.EventType.START));
+    }
+
+    private void switchObserverToTab(Tab tab) {
+        if (mCurrentTab != tab && mCurrentTab != null) {
+            mCurrentTab.removeObserver(mTabObserver);
+        }
+
+        mCurrentTab = tab;
+        if (mCurrentTab != null) {
+            mCurrentTab.addObserver(mTabObserver);
+        }
+    }
 }
