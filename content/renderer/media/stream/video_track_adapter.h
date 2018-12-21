804aa592498a7cfc5bb3eb047f20904180555d00
Marina Ciocea <marinaciocea@chromium.org>
2018-12-21 22:19:26 +0000

    Apply constraints for remote video tracks.
    Bug: 899789
    Change-Id: I28f538888f1bd041ffa31cc650d19245228e044f
    Reviewed-on: https://chromium-review.googlesource.com/c/1388436
    Commit-Queue: Marina Ciocea <marinaciocea@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618630}
    Former-commit-id: c3136103101141dfa259ae64294e18e46727ba01

--- content/renderer/media/stream/video_track_adapter.h
+++ content/renderer/media/stream/video_track_adapter.h
@@ -93,6 +93,7 @@ class VideoTrackAdapter
   void AddTrack(const MediaStreamVideoTrack* track,
                 VideoCaptureDeliverFrameCB frame_callback,
                 VideoTrackSettingsCallback settings_callback,
+                VideoTrackFormatCallback track_callback,
                 const VideoTrackAdapterSettings& settings);
   void RemoveTrack(const MediaStreamVideoTrack* track);
   void ReconfigureTrack(const MediaStreamVideoTrack* track,
@@ -134,6 +135,7 @@ class VideoTrackAdapter
   void AddTrackOnIO(const MediaStreamVideoTrack* track,
                     VideoCaptureDeliverFrameCB frame_callback,
                     VideoTrackSettingsCallback settings_callback,
+                    VideoTrackFormatCallback track_callback,
                     const VideoTrackAdapterSettings& settings);
   void RemoveTrackOnIO(const MediaStreamVideoTrack* track);
   void ReconfigureTrackOnIO(const MediaStreamVideoTrack* track,
