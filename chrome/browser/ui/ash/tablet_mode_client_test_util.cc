0ee9f201599deb3ebe79b547b471cd5404ab95e7
Jun Mukai <mukai@chromium.org>
2018-12-26 18:44:43 +0000

    Make a wait for Mash on SetAndWaitForTabletMode
    This fixes the following tests for issue 916177 along with others.
    TabDragging/DetachToBrowserTabDragControllerTest.DragToMinimizedOverviewWindow/0
    TabDragging/DetachToBrowserTabDragControllerTest.DragToMinimizedOverviewWindow/1
    
    Bug: 916177
    Test: interactive_ui_tests
    Change-Id: I9fbc8a81932d1a775bda30cbfb9ac61e901b8b40
    Reviewed-on: https://chromium-review.googlesource.com/c/1389439
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618944}
    Former-commit-id: c44c414dbf96ac6595b1500c2d0f0de8660ed828

--- chrome/browser/ui/ash/tablet_mode_client_test_util.cc
+++ chrome/browser/ui/ash/tablet_mode_client_test_util.cc
@@ -10,6 +10,7 @@
 #include "content/public/common/service_manager_connection.h"
 #include "services/service_manager/public/cpp/connector.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/aura/test/mus/change_completion_waiter.h"
 
 namespace test {
 
@@ -57,6 +58,7 @@ void SetAndWaitForTabletMode(bool enabled) {
 
   TestTabletModeClientObserver observer(enabled);
   observer.run_loop()->Run();
+  aura::test::WaitForAllChangesToComplete();
 
   ASSERT_EQ(enabled, TabletModeClient::Get()->tablet_mode_enabled());
 }
