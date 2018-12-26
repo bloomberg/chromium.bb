8ef18d9f2a8f1d4a0b8bd0a5027ab4f8e237479e
Avi Drissman <avi@chromium.org>
2018-12-26 21:39:43 +0000

    Use base::size rather than arraysize in mojo/.
    This is purely a mechanical change; there is no intended behavior change.
    
    BUG=837308
    
    Change-Id: I57f7923f0dd3c30f3115ab0b8d52f4038b30d8c6
    Reviewed-on: https://chromium-review.googlesource.com/c/1390720
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618964}
    Former-commit-id: 773f77c3af893257fdb6f71313ef51881d908902

--- mojo/core/data_pipe_unittest.cc
+++ mojo/core/data_pipe_unittest.cc
@@ -10,9 +10,9 @@
 #include "base/bind.h"
 #include "base/location.h"
 #include "base/logging.h"
-#include "base/macros.h"
 #include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "base/stl_util.h"
 #include "build/build_config.h"
 #include "mojo/core/embedder/embedder.h"
 #include "mojo/core/test/mojo_test_base.h"
@@ -168,7 +168,7 @@ TEST_F(DataPipeTest, Basic) {
   int32_t elements[10] = {};
   uint32_t num_bytes = 0;
 
-  num_bytes = static_cast<uint32_t>(arraysize(elements) * sizeof(elements[0]));
+  num_bytes = static_cast<uint32_t>(base::size(elements) * sizeof(elements[0]));
 
   elements[0] = 123;
   elements[1] = 456;
@@ -211,7 +211,7 @@ TEST_F(DataPipeTest, CreateAndMaybeTransfer) {
        100,                              // |element_num_bytes|.
        0}                                // |capacity_num_bytes|.
   };
-  for (size_t i = 0; i < arraysize(test_options); i++) {
+  for (size_t i = 0; i < base::size(test_options); i++) {
     MojoHandle producer_handle, consumer_handle;
     MojoCreateDataPipeOptions* options = i ? &test_options[i] : nullptr;
     ASSERT_EQ(MOJO_RESULT_OK,
@@ -236,7 +236,7 @@ TEST_F(DataPipeTest, SimpleReadWrite) {
   uint32_t num_bytes = 0;
 
   // Try reading; nothing there yet.
-  num_bytes = static_cast<uint32_t>(arraysize(elements) * sizeof(elements[0]));
+  num_bytes = static_cast<uint32_t>(base::size(elements) * sizeof(elements[0]));
   ASSERT_EQ(MOJO_RESULT_SHOULD_WAIT, ReadData(elements, &num_bytes));
 
   // Query; nothing there yet.
@@ -874,7 +874,7 @@ TEST_F(DataPipeTest, AllOrNone) {
   // Try writing more than the total capacity of the pipe.
   uint32_t num_bytes = 20u * sizeof(int32_t);
   int32_t buffer[100];
-  Seq(0, arraysize(buffer), buffer);
+  Seq(0, base::size(buffer), buffer);
   ASSERT_EQ(MOJO_RESULT_OUT_OF_RANGE, WriteData(buffer, &num_bytes, true));
 
   // Should still be empty.
@@ -884,7 +884,7 @@ TEST_F(DataPipeTest, AllOrNone) {
 
   // Write some data.
   num_bytes = 5u * sizeof(int32_t);
-  Seq(100, arraysize(buffer), buffer);
+  Seq(100, base::size(buffer), buffer);
   ASSERT_EQ(MOJO_RESULT_OK, WriteData(buffer, &num_bytes, true));
   ASSERT_EQ(5u * sizeof(int32_t), num_bytes);
 
@@ -910,7 +910,7 @@ TEST_F(DataPipeTest, AllOrNone) {
   // Try writing more than the available capacity of the pipe, but less than the
   // total capacity.
   num_bytes = 6u * sizeof(int32_t);
-  Seq(200, arraysize(buffer), buffer);
+  Seq(200, base::size(buffer), buffer);
   ASSERT_EQ(MOJO_RESULT_OUT_OF_RANGE, WriteData(buffer, &num_bytes, true));
 
   // Try reading too much.
@@ -927,13 +927,13 @@ TEST_F(DataPipeTest, AllOrNone) {
 
   // Just a little.
   num_bytes = 2u * sizeof(int32_t);
-  Seq(300, arraysize(buffer), buffer);
+  Seq(300, base::size(buffer), buffer);
   ASSERT_EQ(MOJO_RESULT_OK, WriteData(buffer, &num_bytes, true));
   ASSERT_EQ(2u * sizeof(int32_t), num_bytes);
 
   // Just right.
   num_bytes = 3u * sizeof(int32_t);
-  Seq(400, arraysize(buffer), buffer);
+  Seq(400, base::size(buffer), buffer);
   ASSERT_EQ(MOJO_RESULT_OK, WriteData(buffer, &num_bytes, true));
   ASSERT_EQ(3u * sizeof(int32_t), num_bytes);
 
@@ -1029,7 +1029,7 @@ TEST_F(DataPipeTest, AllOrNone) {
 // this.)
 TEST_F(DataPipeTest, WrapAround) {
   unsigned char test_data[1000];
-  for (size_t i = 0; i < arraysize(test_data); i++)
+  for (size_t i = 0; i < base::size(test_data); i++)
     test_data[i] = static_cast<unsigned char>(i);
 
   const MojoCreateDataPipeOptions options = {
@@ -1106,7 +1106,7 @@ TEST_F(DataPipeTest, WrapAround) {
 
   // Read as much as possible. We should read 100 bytes.
   num_bytes =
-      static_cast<uint32_t>(arraysize(read_buffer) * sizeof(read_buffer[0]));
+      static_cast<uint32_t>(base::size(read_buffer) * sizeof(read_buffer[0]));
   memset(read_buffer, 0, num_bytes);
   ASSERT_EQ(MOJO_RESULT_OK, ReadData(read_buffer, &num_bytes));
   ASSERT_EQ(100u, num_bytes);
