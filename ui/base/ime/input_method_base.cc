ad9a73a900a0a880eca991da21e84c23097966e7
Shu Chen <shuchen@google.com>
2018-12-27 08:32:26 +0000

    Makes sure the selection range is relative to the surrounding text.
    Change-Id: I1b51ba750e0ed01da2049f07e6610ea5ac53b64b
    Reviewed-on: https://chromium-review.googlesource.com/c/1390778
    Reviewed-by: Lan Wei <azurewei@chromium.org>
    Commit-Queue: Lan Wei <azurewei@chromium.org>
    Commit-Queue: Shu Chen <shuchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619015}
    Former-commit-id: 6b8f96a36988d23b503d039dacb24214a8a26ea3

--- ui/base/ime/input_method_base.cc
+++ ui/base/ime/input_method_base.cc
@@ -262,6 +262,10 @@ SurroundingTextInfo InputMethodBase::GetSurroundingTextInfo() {
       !client->GetEditableSelectionRange(&info.selection_range)) {
     return SurroundingTextInfo();
   }
+  // Makes the |selection_range| be relative to the |surrounding_text|.
+  info.selection_range.set_start(info.selection_range.start() -
+                                 text_range.start());
+  info.selection_range.set_end(info.selection_range.end() - text_range.start());
   return info;
 }
 
