ade81063e5ef60ba84d9e417f95ef35be523ad60
yoshiki iguchi <yoshiki@chromium.org>
2018-12-28 08:57:26 +0000

    Add the target mode for notification list.
    This CL introduces the target mode which decides the scroll position
    on opening the message center, and the logic to make it sure to be
    visible the target notification inside the viewport.
    
    Bug: b/111391270
    Change-Id: I8bd336bd53d2145d13e2b33cdfa260f8686d529d
    Reviewed-on: https://chromium-review.googlesource.com/c/1370242
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Manu Cornet <manucornet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619133}
    Former-commit-id: b8cc989ba64c562d64ca217e777e8258ed1c4b95

--- ash/system/unified/unified_system_tray.h
+++ ash/system/unified/unified_system_tray.h
@@ -78,6 +78,10 @@ class ASH_EXPORT UnifiedSystemTray : public TrayBackgroundView {
   // open when disabling, also close it.
   void SetTrayEnabled(bool enabled);
 
+  // Set the target notification, which is visible in the viewport when the
+  // message center opens.
+  void SetTargetNotification(const std::string& notification_id);
+
   // Sets the height of the system tray bubble from the edge of the work area
   // so that the notification popups don't overlap with the tray. Pass 0 if no
   // bubble is shown.
