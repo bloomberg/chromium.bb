90b8f5bdb360db03e49c899550c554283f3146ab
Raul Tambre <raul@tambre.ee>
2019-01-01 18:32:59 +0000

    AppCache: Fix typo owing_group -> owning_group
    Change-Id: I148879e6cda8e86fe8a7b07ef870392e68277534
    Reviewed-on: https://chromium-review.googlesource.com/c/1392337
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619350}
    Former-commit-id: 66b7786d6c0ea9a559e8668b0684617acf7b3ff0

--- content/browser/appcache/appcache_host.cc
+++ content/browser/appcache/appcache_host.cc
@@ -395,17 +395,17 @@ void AppCacheHost::FinishCacheSelection(
     DCHECK(cache->owning_group());
     DCHECK(new_master_entry_url_.is_empty());
     DCHECK_EQ(cache->owning_group()->manifest_url(), preferred_manifest_url_);
-    AppCacheGroup* owing_group = cache->owning_group();
+    AppCacheGroup* owning_group = cache->owning_group();
     const char* kFormatString =
         "Document was loaded from Application Cache with manifest %s";
     frontend_->OnLogMessage(
         host_id_, APPCACHE_LOG_INFO,
-        base::StringPrintf(
-            kFormatString, owing_group->manifest_url().spec().c_str()));
+        base::StringPrintf(kFormatString,
+                           owning_group->manifest_url().spec().c_str()));
     AssociateCompleteCache(cache);
-    if (!owing_group->is_obsolete() && !owing_group->is_being_deleted()) {
-      owing_group->StartUpdateWithHost(this);
-      ObserveGroupBeingUpdated(owing_group);
+    if (!owning_group->is_obsolete() && !owning_group->is_being_deleted()) {
+      owning_group->StartUpdateWithHost(this);
+      ObserveGroupBeingUpdated(owning_group);
     }
   } else if (group && !group->is_being_deleted()) {
     // If document was loaded using HTTP GET or equivalent, and, there is a
