f680f88a2aa29d0dcc147df220bab1eb48154182
Maxim Kolosovskiy <kolos@chromium.org>
2019-01-03 21:56:20 +0000

    [Autofill] Add |is_form_tag| to the server proto
    Whether an uploaded form is a <form> tag or a list of unowned <input>s may be useful for server side analysis.
    In particular, the list of unowned <input>s can contain several semantic forms (e.g. a search field,  sign-in form, sign-up form). The server side classifiers may apply special heuristics for the <form>less forms.
    
    Bug: 552420, 910546
    Change-Id: If4a5ec04eb1c0d87ec3a8cd399d84e3f5c31a692
    Reviewed-on: https://chromium-review.googlesource.com/c/1394583
    Commit-Queue: Maxim Kolosovskiy <kolos@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619772}
    Former-commit-id: 2b10d833d445b7fedb7901e7323af48538b62fa8

--- components/autofill/core/browser/form_structure.cc
+++ components/autofill/core/browser/form_structure.cc
@@ -575,6 +575,7 @@ bool FormStructure::EncodeUploadRequest(
   upload->set_autofill_used(form_was_autofilled);
   upload->set_data_present(EncodeFieldTypes(available_field_types));
   upload->set_passwords_revealed(passwords_were_revealed_);
+  upload->set_has_form_tag(is_form_tag_);
   if (!page_language_.empty() && randomized_encoder_ != nullptr) {
     upload->set_language(page_language_);
   }
