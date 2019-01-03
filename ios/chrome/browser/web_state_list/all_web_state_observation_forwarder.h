ac47fc413caa448a330be716b90071f37428b781
Rohit Rao <rohitrao@chromium.org>
2019-01-03 15:58:58 +0000

    [ios] Adds helper classes for observing WebStates in a WebStateList.
    AllWebStateObservationForwarder forwards observer methods for all the
    WebStates in a WebStateList, keeping track of WebStates as they are
    added, removed, and replaced.
    
    ActiveWebStateObservationForwarder forwards observer methods for just
    the currently-active WebState, even as the active WebState in the
    WebStateList changes.
    
    These classes are intended to remove some of the boilerplate bookkeeping
    that is necessary when trying to perform either of these common tasks.
    
    Change-Id: I8320f1d1c6aef180a79adf9cbe614cd2066b84fb
    Reviewed-on: https://chromium-review.googlesource.com/c/1393531
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619650}
    Former-commit-id: cc11e7bffa4d92221f92973d9b61645a800befcd

---
+++ ios/chrome/browser/web_state_list/all_web_state_observation_forwarder.h
@@ -0,0 +1,48 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_WEB_STATE_LIST_ALL_WEB_STATE_OBSERVATION_FORWARDER_H_
+#define IOS_CHROME_BROWSER_WEB_STATE_LIST_ALL_WEB_STATE_OBSERVATION_FORWARDER_H_
+
+#include "base/scoped_observer.h"
+#include "ios/chrome/browser/web_state_list/web_state_list_observer.h"
+
+class WebStateList;
+
+namespace web {
+class WebState;
+class WebStateObserver;
+}  // namespace web
+
+// AllWebStateObservationForwarder forwards WebStateObserver methods for all
+// WebStates in a WebStateList, handling cases where WebStates are added,
+// removed, or replaced.
+class AllWebStateObservationForwarder : public WebStateListObserver {
+ public:
+  // Creates an object which forwards observation methods to |observer| and
+  // tracks the set of WebStates in |web_state_list|. |web_state_list| and
+  // |observer| must both outlive this object.
+  AllWebStateObservationForwarder(WebStateList* web_state_list,
+                                  web::WebStateObserver* observer);
+  ~AllWebStateObservationForwarder() override;
+
+  // WebStateListObserver.
+  void WebStateInsertedAt(WebStateList* web_state_list,
+                          web::WebState* web_state,
+                          int index,
+                          bool activating) override;
+  void WebStateReplacedAt(WebStateList* web_state_list,
+                          web::WebState* old_web_state,
+                          web::WebState* new_web_state,
+                          int index) override;
+  void WebStateDetachedAt(WebStateList* web_state_list,
+                          web::WebState* web_state,
+                          int index) override;
+
+ private:
+  ScopedObserver<WebStateList, WebStateListObserver> web_state_list_observer_;
+  ScopedObserver<web::WebState, web::WebStateObserver> web_state_observer_;
+};
+
+#endif  // IOS_CHROME_BROWSER_WEB_STATE_LIST_ALL_WEB_STATE_OBSERVATION_FORWARDER_H_
