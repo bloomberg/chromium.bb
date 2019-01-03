1853f9f2839079e6519d666d4370bd2fccb5f4fb
James Hollyer <jameshollyer@chromium.org>
2019-01-03 00:07:24 +0000

    Stop duplicates WebUSB notification
    This change adds a list of notifications that are currently displayed and checks
    that list for duplicates before displaying a new notification.
    
    Bug: 789362
    Change-Id: I1f8025dbb237da35b088de5bd6f21e80a1bbec52
    Reviewed-on: https://chromium-review.googlesource.com/c/1373436
    Commit-Queue: James Hollyer <jameshollyer@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619541}
    Former-commit-id: d7cf3101dcae06f6d27524f055cd75de20c7fa33

--- chrome/browser/usb/web_usb_detector.h
+++ chrome/browser/usb/web_usb_detector.h
@@ -5,6 +5,8 @@
 #ifndef CHROME_BROWSER_USB_WEB_USB_DETECTOR_H_
 #define CHROME_BROWSER_USB_WEB_USB_DETECTOR_H_
 
+#include <map>
+
 #include "base/macros.h"
 #include "device/usb/public/mojom/device_manager.mojom.h"
 #include "mojo/public/cpp/bindings/associated_binding.h"
@@ -19,6 +21,7 @@ class WebUsbDetector : public device::mojom::UsbDeviceManagerClient {
 
   void SetDeviceManagerForTesting(
       device::mojom::UsbDeviceManagerPtr fake_device_manager);
+  void RemoveNotification(const std::string& id);
 
  private:
   // device::mojom::UsbDeviceManagerClient implementation.
@@ -26,11 +29,16 @@ class WebUsbDetector : public device::mojom::UsbDeviceManagerClient {
   void OnDeviceRemoved(device::mojom::UsbDeviceInfoPtr device_info) override;
 
   void OnDeviceManagerConnectionError();
+  bool IsDisplayingNotification(const GURL& url);
+
+  std::map<std::string, GURL> open_notifications_by_id_;
 
   // Connection to |device_manager_instance_|.
   device::mojom::UsbDeviceManagerPtr device_manager_;
   mojo::AssociatedBinding<device::mojom::UsbDeviceManagerClient>
       client_binding_;
+
+  base::WeakPtrFactory<WebUsbDetector> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(WebUsbDetector);
 };
