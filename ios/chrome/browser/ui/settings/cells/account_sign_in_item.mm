04d219bbb7c7b30976596a0fee4aabe30463c5a0
Jérôme Lebel <jlebel@chromium.org>
2019-01-04 17:45:34 +0000

    [iOS] Adding title for AccountSignInItem
    Setting "Sign in to Chrome" as the AccountSignInItem title.
    Related to crrev.com/c/1392962
    
    Change-Id: I2b2eef3320dd1455e0b1db6a564699ee9e0596a1
    Reviewed-on: https://chromium-review.googlesource.com/c/1396125
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#619994}
    Former-commit-id: 3457db5d005b6070bd9373abaf93721db877562e

--- ios/chrome/browser/ui/settings/cells/account_sign_in_item.mm
+++ ios/chrome/browser/ui/settings/cells/account_sign_in_item.mm
@@ -6,8 +6,10 @@
 
 #import "ios/chrome/browser/ui/settings/cells/settings_image_detail_text_cell.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
+#include "ios/chrome/grit/ios_chromium_strings.h"
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
 #include "ios/public/provider/chrome/browser/signin/signin_resources_provider.h"
+#include "ui/base/l10n/l10n_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -36,8 +38,8 @@ const CGFloat kAccountProfilePhotoDimension = 40.0f;
 - (void)configureCell:(SettingsImageDetailTextCell*)cell
            withStyler:(ChromeTableViewStyler*)styler {
   [super configureCell:cell withStyler:styler];
-  DCHECK(self.text);
-  cell.textLabel.text = self.text;
+  cell.textLabel.text =
+      l10n_util::GetNSString(IDS_IOS_SIGN_IN_TO_CHROME_SETTING_TITLE);
   cell.detailTextLabel.text = self.detailText;
   cell.image = CircularImageFromImage(ios::GetChromeBrowserProvider()
                                           ->GetSigninResourcesProvider()
