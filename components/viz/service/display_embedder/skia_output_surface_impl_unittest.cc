d425725f1ac32fed33e8ed50b805100d1ddb7597
Findit <findit-for-me@appspot.gserviceaccount.com>
2019-01-04 03:17:24 +0000

    Revert "Add reshape and copy outside case for SkiaOutputSurfaceImplTest"
    This reverts commit 303e2b68324fc479710a4e531e738c7fcc6eacf7 [formerly 8f13336d43ca10ee10bd22ad02aab11c7d799128].
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 619841 as the
    culprit for flakes in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/flake/flake-culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyQwsSDEZsYWtlQ3VscHJpdCIxY2hyb21pdW0vOGYxMzMzNmQ0M2NhMTBlZTEwYmQyMmFkMDJhYWIxMWM3ZDc5OTEyOAw
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.mac/Mac10.13%20Tests/8493
    
    Sample Failed Step: viz_unittests on (none) GPU on Mac
    
    Sample Flaky Test: SkiaOutputSurfaceImplTest.CopyOutOutside
    
    Original change's description:
    > Add reshape and copy outside case for SkiaOutputSurfaceImplTest
    > 
    > Bug: 894806
    > Change-Id: Id76b1a229b9583e7d7304b4f5262e2b8d9bbbbb3
    > Reviewed-on: https://chromium-review.googlesource.com/c/1388020
    > Commit-Queue: Xing Xu <xing.xu@intel.com>
    > Reviewed-by: Peng Huang <penghuang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#619841}
    
    Change-Id: Ic8105579dffefcf7f13cfb12e30e350c6ffee6e2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 894806, 919012
    Reviewed-on: https://chromium-review.googlesource.com/c/1395719
    Cr-Commit-Position: refs/heads/master@{#619874}
    Former-commit-id: 4c0d4779099e40a82e26e8d6e7517365f48f69c0

--- components/viz/service/display_embedder/skia_output_surface_impl_unittest.cc
+++ components/viz/service/display_embedder/skia_output_surface_impl_unittest.cc
@@ -18,7 +18,6 @@
 #include "mojo/public/cpp/bindings/strong_binding.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/skia/include/core/SkPixelRef.h"
 #include "ui/gl/init/gl_factory.h"
 
 namespace viz {
@@ -27,7 +26,6 @@ static void ExpectEquals(SkBitmap actual, SkBitmap expected) {
   EXPECT_EQ(actual.dimensions(), expected.dimensions());
   auto expected_url = cc::GetPNGDataUrl(expected);
   auto actual_url = cc::GetPNGDataUrl(actual);
-
   EXPECT_TRUE(actual_url == expected_url);
 }
 
@@ -48,10 +46,7 @@ class SkiaOutputSurfaceImplTest : public testing::Test {
   void TearDown() override;
   void BlockMainThread();
   void UnblockMainThread();
-  void DrawFrameAndVerifyResult(const SkColor output_color,
-                                const gfx::Rect& output_rect);
 
-  gfx::Size surface_size_;
   std::unique_ptr<base::Thread> gpu_thread_;
   std::unique_ptr<SkiaOutputSurfaceImpl> output_surface_;
   std::unique_ptr<GpuServiceImpl> gpu_service_;
@@ -75,43 +70,6 @@ void SkiaOutputSurfaceImplTest::BlockMainThread() {
 void SkiaOutputSurfaceImplTest::UnblockMainThread() {
   DCHECK(!wait_.IsSignaled());
   wait_.Signal();
-}
-
-void SkiaOutputSurfaceImplTest::DrawFrameAndVerifyResult(
-    const SkColor output_color,
-    const gfx::Rect& output_rect) {
-  SkCanvas* root_canvas = output_surface_->BeginPaintCurrentFrame();
-  SkPaint paint;
-  paint.setColor(output_color);
-  SkRect rect = SkRect::MakeXYWH(output_rect.x(), output_rect.y(),
-                                 output_rect.width(), output_rect.height());
-  root_canvas->drawRect(rect, paint);
-
-  gpu::SyncToken sync_token = output_surface_->SubmitPaint();
-  EXPECT_TRUE(sync_token.HasData());
-  base::OnceClosure closure =
-      base::BindOnce(&SkiaOutputSurfaceImplTest::CheckSyncTokenOnGpuThread,
-                     base::Unretained(this), sync_token);
-
-  std::vector<gpu::SyncToken> sync_tokens;
-  sync_tokens.push_back(sync_token);
-  auto sequence_id = gpu_service_->skia_output_surface_sequence_id();
-  gpu_service_->scheduler()->ScheduleTask(gpu::Scheduler::Task(
-      sequence_id, std::move(closure), std::move(sync_tokens)));
-  BlockMainThread();
-
-  // Copy the output
-  const gfx::ColorSpace color_space = gfx::ColorSpace::CreateSRGB();
-  auto request = std::make_unique<CopyOutputRequest>(
-      CopyOutputRequest::ResultFormat::RGBA_BITMAP,
-      base::BindOnce(&SkiaOutputSurfaceImplTest::CopyRequestCallbackOnGpuThread,
-                     base::Unretained(this), output_color, output_rect,
-                     color_space));
-  request->set_result_task_runner(gpu_thread_->task_runner());
-  gfx::Rect result_rect = output_rect;
-  output_surface_->CopyOutput(0, output_rect, color_space, result_rect,
-                              std::move(request));
-  BlockMainThread();
 }
 
 void SkiaOutputSurfaceImplTest::SetUpGpuServiceOnGpuThread() {
@@ -191,7 +149,7 @@ void SkiaOutputSurfaceImplTest::SetUpSkiaOutputSurfaceImpl() {
   output_surface_ = std::make_unique<SkiaOutputSurfaceImpl>(
       gpu_service_.get(), gpu::kNullSurfaceHandle,
       nullptr /* synthetic_begin_frame_source */,
-      false /* show_overdraw_feedback */);
+      false /*show_overdraw_feedback*/);
   output_surface_->BindToClient(output_surface_client_.get());
 }
 
@@ -212,149 +170,55 @@ void SkiaOutputSurfaceImplTest::CopyRequestCallbackOnGpuThread(
   EXPECT_EQ(result_bitmap->width(), output_rect.width());
   EXPECT_EQ(result_bitmap->height(), output_rect.height());
 
-  SkBitmap expected_bitmap;
-  expected_bitmap.allocN32Pixels(output_rect.width(), output_rect.height());
-  SkCanvas expected_canvas(expected_bitmap);
-  SkRect rect;
+  std::vector<SkPMColor> expected_pixels(
+      output_rect.width() * output_rect.height(),
+      SkPreMultiplyColor(output_color));
+  SkBitmap expected;
+  expected.installPixels(
+      SkImageInfo::MakeN32Premul(output_rect.width(), output_rect.height(),
+                                 color_space.ToSkColorSpace()),
+      expected_pixels.data(), output_rect.width() * sizeof(SkColor));
+  ExpectEquals(*result_bitmap.get(), expected);
 
-  SkRect surface_rect;
-  surface_rect =
-      SkRect::MakeXYWH(0, 0, surface_size_.width(), surface_size_.height());
-  bool intersect = surface_rect.intersect(
-      SkRect::MakeXYWH(output_rect.x(), output_rect.y(), output_rect.width(),
-                       output_rect.height()));
-
-  if (intersect) {
-    // When the x y of output_rect are both positive, the left-top of the
-    // output_rect remains in output surface, the copy out rect will be filled
-    // from left-top (0, 0).
-    // When the x y of output_rect are both negative, the left-top of the
-    // output_rect  will be shifted beyond the left-top of the output surface,
-    // which means the copy out rect is filled from
-    // (-output_rect.x(), -output_rect.y()).
-    // When the x of output_rect is negative, the left of the output_rect will
-    // be shifted beyond the left of the output surface, which means the copy
-    // out rect is filled from (-output_rect.x(), 0).
-    // When the y of output_rect is negative, the top of the output_rect  will
-    // be shifted beyond the top of the output surface, which means the copy
-    // out rect is filled from (0, -output_rect.y()).
-    if (output_rect.x() < 0 && output_rect.y() < 0)
-      rect = SkRect::MakeXYWH(-output_rect.x(), -output_rect.y(),
-                              surface_rect.width(), surface_rect.height());
-    else if (output_rect.x() < 0)
-      rect = SkRect::MakeXYWH(-output_rect.x(), 0, surface_rect.width(),
-                              surface_rect.height());
-    else if (output_rect.y() < 0)
-      rect = SkRect::MakeXYWH(0, -output_rect.y(), surface_rect.width(),
-                              surface_rect.height());
-    else
-      rect = SkRect::MakeWH(surface_rect.width(), surface_rect.height());
-  } else {
-    rect = SkRect::MakeEmpty();
-  }
-
-  SkPaint paint;
-  paint.setColor(output_color);
-  expected_canvas.drawRect(rect, paint);
-
-  sk_sp<SkPixelRef> pixels(SkSafeRef(expected_bitmap.pixelRef()));
-  SkIPoint origin = expected_bitmap.pixelRefOrigin();
-  expected_bitmap.setInfo(
-      expected_bitmap.info().makeColorSpace(color_space.ToSkColorSpace()),
-      expected_bitmap.rowBytes());
-  expected_bitmap.setPixelRef(std::move(pixels), origin.x(), origin.y());
-
-  ExpectEquals(*result_bitmap.get(), expected_bitmap);
   UnblockMainThread();
 }
 
 TEST_F(SkiaOutputSurfaceImplTest, SubmitPaint) {
-  surface_size_ = gfx::Size(100, 100);
-  output_surface_->Reshape(surface_size_, 1 /* device_scale_factor */,
-                           gfx::ColorSpace(), true /* has_alpha */,
-                           false /* use_stencil */);
-  DrawFrameAndVerifyResult(SK_ColorRED, gfx::Rect(0, 0, 10, 10));
-}
+  output_surface_->Reshape(gfx::Size(100.0, 100.0), 1, gfx::ColorSpace(), true,
+                           false);
+  SkCanvas* root_canvas = output_surface_->BeginPaintCurrentFrame();
+  SkPaint paint;
+  const SkColor output_color = SK_ColorRED;
+  const gfx::Rect output_rect(0, 0, 10, 10);
+  paint.setColor(output_color);
+  SkRect rect = SkRect::MakeWH(output_rect.width(), output_rect.height());
+  root_canvas->drawRect(rect, paint);
 
-TEST_F(SkiaOutputSurfaceImplTest, Reshape) {
-  // The first draw with reshape.
-  surface_size_ = gfx::Size(10, 10);
-  output_surface_->Reshape(surface_size_, 1 /* device_scale_factor */,
-                           gfx::ColorSpace(), true /* has_alpha */,
-                           false /* use_stencil */);
-  DrawFrameAndVerifyResult(SK_ColorRED, gfx::Rect(0, 0, 10, 10));
+  gpu::SyncToken sync_token = output_surface_->SubmitPaint();
+  EXPECT_TRUE(sync_token.HasData());
+  base::OnceClosure closure =
+      base::BindOnce(&SkiaOutputSurfaceImplTest::CheckSyncTokenOnGpuThread,
+                     base::Unretained(this), sync_token);
 
-  // The second and third draw without reshape.
-  DrawFrameAndVerifyResult(SK_ColorGREEN, gfx::Rect(0, 0, 10, 9));
-  DrawFrameAndVerifyResult(SK_ColorYELLOW, gfx::Rect(0, 0, 9, 10));
+  std::vector<gpu::SyncToken> resource_sync_tokens;
+  resource_sync_tokens.push_back(sync_token);
+  auto sequence_id = gpu_service_->skia_output_surface_sequence_id();
+  gpu_service_->scheduler()->ScheduleTask(gpu::Scheduler::Task(
+      sequence_id, std::move(closure), std::move(resource_sync_tokens)));
+  BlockMainThread();
 
-  // The last draw with reshape.
-  surface_size_ = gfx::Size(100, 100);
-  output_surface_->Reshape(surface_size_, 1 /* device_scale_factor */,
-                           gfx::ColorSpace(), true /* has_alpha */,
-                           false /* use_stencil */);
-  DrawFrameAndVerifyResult(SK_ColorRED, gfx::Rect(0, 0, 20, 20));
-}
-
-TEST_F(SkiaOutputSurfaceImplTest, CopyOutOutside) {
-  // The first draw, both width and height outside.
-  surface_size_ = gfx::Size(100, 100);
-  output_surface_->Reshape(surface_size_, 1 /* device_scale_factor */,
-                           gfx::ColorSpace(), true /* has_alpha */,
-                           false /* use_stencil */);
-  DrawFrameAndVerifyResult(SK_ColorRED, gfx::Rect(0, 0, 110, 110));
-
-  // The second draw, height outside.
-  output_surface_->Reshape(surface_size_, 1 /* device_scale_factor */,
-                           gfx::ColorSpace(), true /* has_alpha */,
-                           false /* use_stencil */);
-  DrawFrameAndVerifyResult(SK_ColorRED, gfx::Rect(0, 0, 90, 110));
-
-  // The last draw, width outside.
-  output_surface_->Reshape(surface_size_, 1 /* device_scale_factor */,
-                           gfx::ColorSpace(), true /* has_alpha */,
-                           false /* use_stencil */);
-  DrawFrameAndVerifyResult(SK_ColorRED, gfx::Rect(0, 0, 110, 90));
-}
-
-TEST_F(SkiaOutputSurfaceImplTest, CopyOutOffset) {
-  surface_size_ = gfx::Size(100, 100);
-  output_surface_->Reshape(surface_size_, 1 /* device_scale_factor */,
-                           gfx::ColorSpace(), true /* has_alpha */,
-                           false /* use_stencil */);
-  DrawFrameAndVerifyResult(SK_ColorRED, gfx::Rect(10, 10, 20, 20));
-  DrawFrameAndVerifyResult(SK_ColorRED, gfx::Rect(10, 10, 120, 120));
-  DrawFrameAndVerifyResult(SK_ColorRED, gfx::Rect(100, 100, 120, 120));
-
-  DrawFrameAndVerifyResult(SK_ColorRED, gfx::Rect(-10, -10, 20, 20));
-  DrawFrameAndVerifyResult(SK_ColorRED, gfx::Rect(-10, 10, 20, 20));
-  DrawFrameAndVerifyResult(SK_ColorRED, gfx::Rect(10, -10, 20, 20));
-  DrawFrameAndVerifyResult(SK_ColorRED, gfx::Rect(-10, -10, 120, 120));
-  DrawFrameAndVerifyResult(SK_ColorRED, gfx::Rect(-10, -10, 0, 0));
-}
-
-TEST_F(SkiaOutputSurfaceImplTest, CopyOutSmallSurface) {
-  // The  first draw with reshape.
-  surface_size_ = gfx::Size(10, 10);
-  output_surface_->Reshape(surface_size_, 1 /* device_scale_factor */,
-                           gfx::ColorSpace(), true /* has_alpha */,
-                           false /* use_stencil */);
-  DrawFrameAndVerifyResult(SK_ColorRED, gfx::Rect(0, 0, 10, 10));
-
-  // The last draw without reshape.
-  DrawFrameAndVerifyResult(SK_ColorGREEN, gfx::Rect(0, 0, 10, 9));
-}
-
-TEST_F(SkiaOutputSurfaceImplTest, CopyOutFullSurface) {
-  // The first draw with reshape.
-  surface_size_ = gfx::Size(10, 10);
-  output_surface_->Reshape(surface_size_, 1 /* device_scale_factor */,
-                           gfx::ColorSpace(), true /* has_alpha */,
-                           false /* use_stencil */);
-  DrawFrameAndVerifyResult(SK_ColorRED, gfx::Rect(0, 0, 10, 10));
-
-  // The last draw without reshape.
-  DrawFrameAndVerifyResult(SK_ColorGREEN, gfx::Rect(0, 0, 10, 10));
+  // Copy the output
+  const gfx::ColorSpace color_space = gfx::ColorSpace::CreateSRGB();
+  auto request = std::make_unique<CopyOutputRequest>(
+      CopyOutputRequest::ResultFormat::RGBA_BITMAP,
+      base::BindOnce(&SkiaOutputSurfaceImplTest::CopyRequestCallbackOnGpuThread,
+                     base::Unretained(this), output_color, output_rect,
+                     color_space));
+  request->set_result_task_runner(gpu_thread_->task_runner());
+  gfx::Rect result_rect = output_rect;
+  output_surface_->CopyOutput(0, output_rect, color_space, result_rect,
+                              std::move(request));
+  BlockMainThread();
 }
 
 }  // namespace viz
