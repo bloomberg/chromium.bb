52392208bb92c6cf0b640a78a58dac092e7e7d4c
Chris Harrelson <chrishtr@chromium.org>
2019-01-05 01:03:39 +0000

    [CI] Remove incorrect comments in visual overflow code.
    The calls to LayoutObject::RecalcVisualOverflow() are for
    recursion to children, not "adding in" children visual overflow.
    
    Change-Id: I8b5e44e49d1e1bea889afd5764d865a4342739dd
    Reviewed-on: https://chromium-review.googlesource.com/c/1396785
    Reviewed-by: Aleks Totic <atotic@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620136}
    Former-commit-id: 8942ec06dcb917f36df5c217dfd6c2d72b7ceb2a

--- third_party/blink/renderer/core/layout/layout_replaced.cc
+++ third_party/blink/renderer/core/layout/layout_replaced.cc
@@ -153,7 +153,6 @@ static inline bool LayoutObjectHasAspectRatio(
 }
 
 void LayoutReplaced::RecalcVisualOverflow() {
-  // Add in overflow from children.
   LayoutObject::RecalcVisualOverflow();
   ClearVisualOverflow();
   AddVisualEffectOverflow();
