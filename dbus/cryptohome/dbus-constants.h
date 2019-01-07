e409915310e13f7ec7cb4beb1c83cbe71c5e1939
Eric Caruso <ejcaruso@chromium.org>
2019-01-07 14:18:56 -0800

    cryptohome: add UnmountEx method
    This is a D-Bus method that allows us to unmount asynchronously
    and uses protos for easier expansion of the API in the future if
    we decide to support unmounting per user.
    
    BUG=chromium:809616
    TEST=using cryptohome command line
    
    Change-Id: I1cc2bc0364e944b85b57541faa9bafdeba989b17
    Reviewed-on: https://chromium-review.googlesource.com/1401342
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Eric Caruso <ejcaruso@chromium.org>
    Reviewed-by: Andrey Pronin <apronin@chromium.org>
    Cr-Mirrored-From: https://chromium.googlesource.com/chromiumos/platform2
    Cr-Mirrored-Commit: f211920105f7db3de8d5d407ef6fd0dc77638ef2

--- dbus/cryptohome/dbus-constants.h
+++ dbus/cryptohome/dbus-constants.h
@@ -24,6 +24,7 @@ const char kCryptohomeMount[] = "Mount";
 const char kCryptohomeMountGuest[] = "MountGuest";
 const char kCryptohomeMountGuestEx[] = "MountGuestEx";
 const char kCryptohomeUnmount[] = "Unmount";
+const char kCryptohomeUnmountEx[] = "UnmountEx";
 const char kCryptohomeTpmIsReady[] = "TpmIsReady";
 const char kCryptohomeTpmIsEnabled[] = "TpmIsEnabled";
 const char kCryptohomeTpmIsOwned[] = "TpmIsOwned";
