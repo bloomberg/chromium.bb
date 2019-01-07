aa6899c21df7c363af838dfd38656924b67abee9
Alexander Alekseev <alemate@chromium.org>
2019-01-07 20:08:19 +0000

    Chrome OS Discover App: Add test infrastructure.
    This Cl adds browser_test infrastructure to Discover App and tests for existing
    modules.
    
    Bug: 864684
    Change-Id: Ic20dc878d0435636d627d0b5917602f915deb204
    Reviewed-on: https://chromium-review.googlesource.com/c/1391362
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620442}
    Former-commit-id: 851b7c1f81a6e796e4f1ee6bbc5e05dbc9f03c3f

--- chrome/browser/ui/webui/chromeos/login/discover/discover_ui.cc
+++ chrome/browser/ui/webui/chromeos/login/discover/discover_ui.cc
@@ -4,17 +4,83 @@
 
 #include "chrome/browser/ui/webui/chromeos/login/discover/discover_ui.h"
 
+#include "base/logging.h"
 #include "chrome/browser/ui/webui/chromeos/login/discover/discover_handler.h"
 #include "chrome/browser/ui/webui/chromeos/login/discover/discover_manager.h"
+#include "chrome/browser/ui/webui/chromeos/login/discover/discover_window_manager.h"
+#include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
 
 namespace chromeos {
 
-DiscoverUI::DiscoverUI() {}
+namespace {
 
-DiscoverUI::~DiscoverUI() {}
+// Data to store a reference to DiscoverUI into WebContents.
+class DiscoverUIUserDataPointer : public base::SupportsUserData::Data {
+ public:
+  static DiscoverUI* Get(const content::WebContents* web_contents) {
+    DiscoverUIUserDataPointer* data = static_cast<DiscoverUIUserDataPointer*>(
+        web_contents->GetUserData(kKey));
+    if (data)
+      return data->ptr_;
+
+    return nullptr;
+  }
+
+  static void Attach(DiscoverUI* discover_ui,
+                     content::WebContents* web_contents) {
+    DCHECK(!Get(web_contents));
+
+    web_contents->SetUserData(
+        kKey, std::make_unique<DiscoverUIUserDataPointer>(discover_ui));
+  }
+
+  static void Detach(const DiscoverUI* discover_ui,
+                     content::WebContents* web_contents) {
+#if DCHECK_IS_ON()
+    if (Get(web_contents))
+      DCHECK_EQ(Get(web_contents), discover_ui);
+#else
+    ANALYZER_ALLOW_UNUSED(discover_ui);
+#endif
+
+    web_contents->RemoveUserData(kKey);
+  }
+
+  explicit DiscoverUIUserDataPointer(DiscoverUI* discover_ui)
+      : ptr_(discover_ui) {}
+
+  ~DiscoverUIUserDataPointer() override = default;
+
+ private:
+  DiscoverUI* ptr_ = nullptr;
+
+  static const char kKey[];
+
+  DISALLOW_COPY_AND_ASSIGN(DiscoverUIUserDataPointer);
+};
+
+const char DiscoverUIUserDataPointer::kKey[] = "DiscoverUIKey";
+}  // anonymous namespace
+
+DiscoverUI::DiscoverUI() = default;
+
+DiscoverUI::~DiscoverUI() {
+  if (web_ui_)
+    DiscoverUIUserDataPointer::Detach(this, web_ui_->GetWebContents());
+}
+
+DiscoverUI* DiscoverUI::GetDiscoverUI(
+    const content::WebContents* web_contents) {
+  return DiscoverUIUserDataPointer::Get(web_contents);
+}
 
 void DiscoverUI::RegisterMessages(content::WebUI* web_ui) {
+  DCHECK(!web_ui_);
+  web_ui_ = web_ui;
+
+  DiscoverUIUserDataPointer::Attach(this, web_ui->GetWebContents());
+
   std::vector<std::unique_ptr<DiscoverHandler>> handlers =
       DiscoverManager::Get()->CreateWebUIHandlers();
   for (auto& handler : handlers) {
@@ -31,8 +97,19 @@ void DiscoverUI::GetAdditionalParameters(base::DictionaryValue* dict) {
   }
 }
 
-void DiscoverUI::Show() {
-  // TODO.
+void DiscoverUI::Initialize() {
+  for (Observer& observer : observers_)
+    observer.OnInitialized();
+}
+
+void DiscoverUI::Show() {}
+
+void DiscoverUI::AddObserver(Observer* observer) {
+  observers_.AddObserver(observer);
+}
+
+void DiscoverUI::RemoveObserver(Observer* observer) {
+  observers_.RemoveObserver(observer);
 }
 
 }  // namespace chromeos
