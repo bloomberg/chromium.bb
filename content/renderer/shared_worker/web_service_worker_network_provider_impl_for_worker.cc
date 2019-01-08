69eff412de3e59f53d830c50245d58add5cc3fec
Hiroki Nakagawa <nhiroki@chromium.org>
2019-01-08 06:01:50 +0000

    Worker: Move WebServiceWorkerNetworkProviderForSharedWorker to its own file
    Before this CL, WebServiceWorkerNetworkProviderForSharedWorker is defined in
    embedded_shared_worker_stub.cc.
    
    This CL moves WebServiceWorkerNetworkProviderForSharedWorker into its own file,
    and renames it to WebServiceWorkerNetworkProviderImplForWorker so that it's
    reusable for dedicated workers(*).
    
    (*) Currently, dedicated workers use parent document's
    WebServiceWorkerNetworkProviderForFrame(**). After PlzDedicatedWorker is
    implemented, dedicated workers have their own network provider, that is,
    WebServiceWorkerNetworkProviderImplForWorker.
    
    (**) WebServiceWorkerNetworkProviderForFrame will be renamed to
    WebServiceWorkerNetworkProvider*Impl*ForFrame in a follow-up CL to clarify it's
    the implementation of WebServiceWorkerNetworkProvider for Frame.
    
    Bug: 906991
    Change-Id: I8c7ebd60aff2e20f4082a44eef43338ce23584ba
    Reviewed-on: https://chromium-review.googlesource.com/c/1391659
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620637}
    Former-commit-id: 4b48e1f44e84ddf5055ba10b8855dc39db0d0a15

---
+++ content/renderer/shared_worker/web_service_worker_network_provider_impl_for_worker.cc
@@ -0,0 +1,135 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/renderer/shared_worker/web_service_worker_network_provider_impl_for_worker.h"
+
+#include "base/feature_list.h"
+#include "content/public/common/origin_util.h"
+#include "content/renderer/loader/navigation_response_override_parameters.h"
+#include "content/renderer/loader/request_extra_data.h"
+#include "content/renderer/loader/web_url_loader_impl.h"
+#include "content/renderer/render_thread_impl.h"
+#include "content/renderer/service_worker/service_worker_network_provider.h"
+#include "services/network/public/cpp/features.h"
+#include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
+#include "third_party/blink/public/common/service_worker/service_worker_utils.h"
+
+namespace content {
+
+WebServiceWorkerNetworkProviderImplForWorker::
+    WebServiceWorkerNetworkProviderImplForWorker(
+        std::unique_ptr<ServiceWorkerNetworkProvider> provider,
+        bool is_secure_context,
+        std::unique_ptr<NavigationResponseOverrideParameters> response_override)
+    : provider_(std::move(provider)),
+      is_secure_context_(is_secure_context),
+      response_override_(std::move(response_override)) {}
+
+WebServiceWorkerNetworkProviderImplForWorker::
+    ~WebServiceWorkerNetworkProviderImplForWorker() = default;
+
+void WebServiceWorkerNetworkProviderImplForWorker::WillSendRequest(
+    blink::WebURLRequest& request) {
+  auto extra_data = std::make_unique<RequestExtraData>();
+  extra_data->set_service_worker_provider_id(provider_->provider_id());
+  extra_data->set_initiated_in_secure_context(is_secure_context_);
+  if (response_override_) {
+    DCHECK(base::FeatureList::IsEnabled(network::features::kNetworkService));
+    DCHECK_EQ(blink::mojom::RequestContextType::SHARED_WORKER,
+              request.GetRequestContext());
+    extra_data->set_navigation_response_override(std::move(response_override_));
+  }
+  request.SetExtraData(std::move(extra_data));
+
+  // If the provider does not have a controller at this point, the renderer
+  // expects subresource requests to never be handled by a controlling service
+  // worker, so set |skip_service_worker| to skip service workers here.
+  // Otherwise, a service worker that is in the process of becoming the
+  // controller (i.e., via claim()) on the browser-side could handle the
+  // request and break the assumptions of the renderer.
+  if (request.GetRequestContext() !=
+          blink::mojom::RequestContextType::SHARED_WORKER &&
+      provider_->IsControlledByServiceWorker() ==
+          blink::mojom::ControllerServiceWorkerMode::kNoController) {
+    request.SetSkipServiceWorker(true);
+  }
+}
+
+int WebServiceWorkerNetworkProviderImplForWorker::ProviderID() const {
+  return provider_->provider_id();
+}
+
+blink::mojom::ControllerServiceWorkerMode
+WebServiceWorkerNetworkProviderImplForWorker::IsControlledByServiceWorker() {
+  return provider_->IsControlledByServiceWorker();
+}
+
+int64_t
+WebServiceWorkerNetworkProviderImplForWorker::ControllerServiceWorkerID() {
+  if (provider_->context())
+    return provider_->context()->GetControllerVersionId();
+  return blink::mojom::kInvalidServiceWorkerVersionId;
+}
+
+std::unique_ptr<blink::WebURLLoader>
+WebServiceWorkerNetworkProviderImplForWorker::CreateURLLoader(
+    const blink::WebURLRequest& request,
+    std::unique_ptr<blink::scheduler::WebResourceLoadingTaskRunnerHandle>
+        task_runner_handle) {
+  // S13nServiceWorker:
+  // We only install our own URLLoader if Servicification is enabled.
+  if (!blink::ServiceWorkerUtils::IsServicificationEnabled())
+    return nullptr;
+
+  RenderThreadImpl* render_thread = RenderThreadImpl::current();
+  // RenderThreadImpl is nullptr in some tests.
+  if (!render_thread) {
+    return nullptr;
+  }
+  // If the request is for the main script, use the script_loader_factory.
+  if (provider_->script_loader_factory() &&
+      request.GetRequestContext() ==
+          blink::mojom::RequestContextType::SHARED_WORKER) {
+    // TODO(crbug.com/796425): Temporarily wrap the raw
+    // mojom::URLLoaderFactory pointer into SharedURLLoaderFactory.
+    return std::make_unique<WebURLLoaderImpl>(
+        render_thread->resource_dispatcher(), std::move(task_runner_handle),
+        base::MakeRefCounted<network::WeakWrapperSharedURLLoaderFactory>(
+            provider_->script_loader_factory()));
+  }
+
+  // Otherwise, it's an importScript. Use the subresource loader factory if it
+  // exists (we are controlled by a service worker).
+  if (!provider_->context() ||
+      !provider_->context()->GetSubresourceLoaderFactory()) {
+    return nullptr;
+  }
+
+  // If the URL is not http(s) or otherwise whitelisted, do not intercept the
+  // request. Schemes like 'blob' and 'file' are not eligible to be
+  // intercepted by service workers.
+  // TODO(falken): Let ServiceWorkerSubresourceLoaderFactory handle the
+  // request and move this check there (i.e., for such URLs, it should use
+  // its fallback factory).
+  if (!GURL(request.Url()).SchemeIsHTTPOrHTTPS() &&
+      !OriginCanAccessServiceWorkers(request.Url())) {
+    return nullptr;
+  }
+
+  // If GetSkipServiceWorker() returns true, do not intercept the request.
+  if (request.GetSkipServiceWorker())
+    return nullptr;
+
+  // Create our own SubresourceLoader to route the request to the controller
+  // ServiceWorker.
+  // TODO(crbug.com/796425): Temporarily wrap the raw mojom::URLLoaderFactory
+  // pointer into SharedURLLoaderFactory.
+  return std::make_unique<WebURLLoaderImpl>(
+      RenderThreadImpl::current()->resource_dispatcher(),
+      std::move(task_runner_handle),
+      base::MakeRefCounted<network::WeakWrapperSharedURLLoaderFactory>(
+          provider_->context()->GetSubresourceLoaderFactory()));
+}
+
+}  // namespace content
---
+++ content/renderer/shared_worker/web_service_worker_network_provider_impl_for_worker.h
@@ -0,0 +1,53 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_RENDERER_SHARED_WORKER_WEB_SERVICE_WORKER_NETWORK_PROVIDER_IMPL_FOR_WORKER_H_
+#define CONTENT_RENDERER_SHARED_WORKER_WEB_SERVICE_WORKER_NETWORK_PROVIDER_IMPL_FOR_WORKER_H_
+
+#include "third_party/blink/public/platform/modules/service_worker/web_service_worker_network_provider.h"
+
+namespace content {
+
+class ServiceWorkerNetworkProvider;
+struct NavigationResponseOverrideParameters;
+
+// This is the implementation of WebServiceWorkerNetworkProvider used for
+// shared workers and owned by Blink. All functions are called on the main
+// thread only.
+class WebServiceWorkerNetworkProviderImplForWorker final
+    : public blink::WebServiceWorkerNetworkProvider {
+ public:
+  WebServiceWorkerNetworkProviderImplForWorker(
+      std::unique_ptr<ServiceWorkerNetworkProvider> provider,
+      bool is_secure_context,
+      std::unique_ptr<NavigationResponseOverrideParameters> response_override);
+  ~WebServiceWorkerNetworkProviderImplForWorker() override;
+
+  // Blink calls this method for each request starting with the main script,
+  // we tag them with the provider id.
+  void WillSendRequest(blink::WebURLRequest& request) override;
+
+  int ProviderID() const override;
+
+  blink::mojom::ControllerServiceWorkerMode IsControlledByServiceWorker()
+      override;
+
+  int64_t ControllerServiceWorkerID() override;
+
+  std::unique_ptr<blink::WebURLLoader> CreateURLLoader(
+      const blink::WebURLRequest& request,
+      std::unique_ptr<blink::scheduler::WebResourceLoadingTaskRunnerHandle>
+          task_runner_handle) override;
+
+  ServiceWorkerNetworkProvider* provider() { return provider_.get(); }
+
+ private:
+  std::unique_ptr<ServiceWorkerNetworkProvider> provider_;
+  const bool is_secure_context_;
+  std::unique_ptr<NavigationResponseOverrideParameters> response_override_;
+};
+
+}  // namespace content
+
+#endif  // CONTENT_RENDERER_SHARED_WORKER_WEB_SERVICE_WORKER_NETWORK_PROVIDER_IMPL_FOR_WORKER_H_
