7f3684135273dc50b007a153e4f67e5c159c8381
Richard Knoll <knollr@chromium.org>
2019-01-08 11:41:51 +0000

    fix: only look for other RadioButtons under the same parent
    RadioButton used to iterate through all views in the same window to
    look for other buttons with the same group. This requires unique groups
    across the whole window and makes it quite difficult to use multiple
    components with radio buttons.
    
    This changes the behavior so that RadioButton only looks for other
    buttons that are below its parent().
    
    Bug: 917374
    Change-Id: I30776fd9dfd90f51caefd8d64b77edcf0841bb2b
    Reviewed-on: https://chromium-review.googlesource.com/c/1388154
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620684}
    Former-commit-id: 5ce9f46cbf3d593b6e71de0c1b1fe906c203f435

--- ui/views/controls/button/radio_button.cc
+++ ui/views/controls/button/radio_button.cc
@@ -38,7 +38,7 @@ void RadioButton::GetAccessibleNodeData(ui::AXNodeData* node_data) {
 
 View* RadioButton::GetSelectedViewForGroup(int group) {
   Views views;
-  GetWidget()->GetRootView()->GetViewsInGroup(group, &views);
+  GetViewsInGroupFromParent(group, &views);
   if (views.empty())
     return NULL;
 
@@ -66,7 +66,7 @@ void RadioButton::RequestFocusFromEvent() {
   Checkbox::RequestFocusFromEvent();
   // Take focus only if another radio button in the group has focus.
   Views views;
-  GetWidget()->GetRootView()->GetViewsInGroup(GetGroup(), &views);
+  GetViewsInGroupFromParent(GetGroup(), &views);
   if (std::find_if(views.begin(), views.end(), [](View* v) -> bool {
         return v->HasFocus();
       }) != views.end()) {
@@ -90,25 +90,20 @@ void RadioButton::SetChecked(bool checked) {
   if (checked == RadioButton::checked())
     return;
   if (checked) {
-    // We can't just get the root view here because sometimes the radio
-    // button isn't attached to a root view (e.g., if it's part of a tab page
-    // that is currently not active).
-    View* container = parent();
-    while (container && container->parent())
-      container = container->parent();
-    if (container) {
-      Views other;
-      container->GetViewsInGroup(GetGroup(), &other);
-      for (auto i(other.begin()); i != other.end(); ++i) {
-        if (*i != this) {
-          if (strcmp((*i)->GetClassName(), kViewClassName)) {
-            NOTREACHED() << "radio-button-nt has same group as other non "
-                            "radio-button-nt views.";
-            continue;
-          }
-          RadioButton* peer = static_cast<RadioButton*>(*i);
-          peer->SetChecked(false);
+    // We can't start from the root view because other parts of the UI might use
+    // radio buttons with the same group. This can happen when re-using the same
+    // component or even if views want to use the group for a different purpose.
+    Views other;
+    GetViewsInGroupFromParent(GetGroup(), &other);
+    for (auto i(other.begin()); i != other.end(); ++i) {
+      if (*i != this) {
+        if (strcmp((*i)->GetClassName(), kViewClassName)) {
+          NOTREACHED() << "radio-button-nt has same group as other non "
+                          "radio-button-nt views.";
+          continue;
         }
+        RadioButton* peer = static_cast<RadioButton*>(*i);
+        peer->SetChecked(false);
       }
     }
   }
@@ -123,6 +118,12 @@ SkPath RadioButton::GetFocusRingPath() const {
   SkPath path;
   path.addOval(gfx::RectToSkRect(image()->GetMirroredBounds()));
   return path;
+}
+
+void RadioButton::GetViewsInGroupFromParent(int group, Views* views) {
+  if (!parent())
+    return;
+  parent()->GetViewsInGroup(group, views);
 }
 
 }  // namespace views
