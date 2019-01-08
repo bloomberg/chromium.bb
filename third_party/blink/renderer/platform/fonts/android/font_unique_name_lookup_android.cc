0168d806d6c523b8248fada337958aee31ce1759
Dominik Röttsches <drott@chromium.org>
2019-01-08 15:56:03 +0000

    Move FontTableMatcher FontUniqueNameLookup implementation to base class
    Preparation for reusing this implementation on Windows.
    No functional changes for the existing Android implementation.
    End to end Blink to Browser tests in FontUniqueNameBrowserTest.
    
    Bug: 889864
    Change-Id: I5a42e2ed50b2d5a772643e8818981041c744b047
    Reviewed-on: https://chromium-review.googlesource.com/c/1400683
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620737}
    Former-commit-id: 421609e7af38ba79558a2ec822d6823e57a571a7

--- third_party/blink/renderer/platform/fonts/android/font_unique_name_lookup_android.cc
+++ third_party/blink/renderer/platform/fonts/android/font_unique_name_lookup_android.cc
@@ -4,39 +4,15 @@
 
 #include "third_party/blink/renderer/platform/fonts/android/font_unique_name_lookup_android.h"
 #include "mojo/public/mojom/base/shared_memory.mojom-blink.h"
-#include "third_party/blink/public/platform/interface_provider.h"
 #include "third_party/blink/public/platform/modules/font_unique_name_lookup/font_unique_name_lookup.mojom-blink.h"
-#include "third_party/blink/public/platform/platform.h"
 
 namespace blink {
 
 FontUniqueNameLookupAndroid::~FontUniqueNameLookupAndroid() = default;
 
-bool FontUniqueNameLookupAndroid::EnsureMatchingServiceConnected() {
-  if (font_table_matcher_)
-    return true;
-
-  mojom::blink::FontUniqueNameLookupPtr service;
-  Platform::Current()->GetInterfaceProvider()->GetInterface(
-      mojo::MakeRequest(&service));
-
-  base::ReadOnlySharedMemoryRegion region_ptr;
-  if (!service->GetUniqueNameLookupTable(&region_ptr)) {
-    // Tests like StyleEngineTest do not set up a full browser where Blink can
-    // connect to a browser side service for font lookups. Placing a DCHECK here
-    // is too strict for such a case.
-    LOG(ERROR) << "Unable to connect to browser side service for src: local() "
-                  "font lookup.";
-    return false;
-  }
-
-  font_table_matcher_ = std::make_unique<FontTableMatcher>(region_ptr.Map());
-  return true;
-}
-
 sk_sp<SkTypeface> FontUniqueNameLookupAndroid::MatchUniqueName(
     const String& font_unique_name) {
-  if (!EnsureMatchingServiceConnected())
+  if (!EnsureMatchingServiceConnected<mojom::blink::FontUniqueNameLookupPtr>())
     return nullptr;
   base::Optional<FontTableMatcher::MatchResult> match_result =
       font_table_matcher_->MatchName(font_unique_name.Utf8().data());
