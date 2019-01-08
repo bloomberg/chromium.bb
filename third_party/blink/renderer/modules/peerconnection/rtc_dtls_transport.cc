6a4bb241373262b5eee3c70930f90a1de3b887d0
Harald Alvestrand <hta@chromium.org>
2019-01-08 17:11:54 +0000

    Wiring for webrtc DtlsTransport events and state
    This is a somewhat simplified approach from the one used in
    the ICE transport adapters.
    
    Bug: chromium:907849
    Change-Id: I799735b5a2e92602fc303ca813466d762b7933a1
    Reviewed-on: https://chromium-review.googlesource.com/c/1392957
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Harald Alvestrand <hta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620767}
    Former-commit-id: 4d24417f4e32b141684e05ace391a43698cda137

--- third_party/blink/renderer/modules/peerconnection/rtc_dtls_transport.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_dtls_transport.cc
@@ -10,9 +10,9 @@
 #include "third_party/blink/renderer/core/dom/events/event.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/typed_arrays/dom_array_buffer.h"
+#include "third_party/blink/renderer/modules/peerconnection/adapters/dtls_transport_proxy.h"
 #include "third_party/blink/renderer/modules/peerconnection/adapters/ice_transport_adapter_cross_thread_factory.h"
 #include "third_party/blink/renderer/modules/peerconnection/adapters/ice_transport_adapter_impl.h"
-#include "third_party/blink/renderer/modules/peerconnection/adapters/ice_transport_proxy.h"
 #include "third_party/blink/renderer/modules/peerconnection/rtc_error_util.h"
 #include "third_party/blink/renderer/modules/peerconnection/rtc_ice_candidate.h"
 #include "third_party/blink/renderer/modules/peerconnection/rtc_ice_gather_options.h"
@@ -31,16 +31,54 @@
 
 namespace blink {
 
+namespace {
+String TransportStateToString(webrtc::DtlsTransportState state) {
+  switch (state) {
+    case webrtc::DtlsTransportState::kNew:
+      return String("new");
+      break;
+    case webrtc::DtlsTransportState::kConnecting:
+      return String("connecting");
+      break;
+    case webrtc::DtlsTransportState::kConnected:
+      return String("connected");
+      break;
+    case webrtc::DtlsTransportState::kClosed:
+      return String("closed");
+      break;
+    case webrtc::DtlsTransportState::kFailed:
+      return String("failed");
+      break;
+  }
+}
+
+std::unique_ptr<DtlsTransportProxy> CreateProxy(
+    ExecutionContext* context,
+    webrtc::DtlsTransportInterface* native_transport,
+    DtlsTransportProxy::Delegate* delegate) {
+  LocalFrame* frame = To<Document>(context)->GetFrame();
+  scoped_refptr<base::SingleThreadTaskRunner> proxy_thread =
+      frame->GetTaskRunner(TaskType::kNetworking);
+  scoped_refptr<base::SingleThreadTaskRunner> host_thread =
+      Platform::Current()->GetWebRtcWorkerThread();
+  return DtlsTransportProxy::Create(*frame, proxy_thread, host_thread,
+                                    native_transport, delegate);
+}
+
+}  // namespace
+
 RTCDtlsTransport::RTCDtlsTransport(
     ExecutionContext* context,
     rtc::scoped_refptr<webrtc::DtlsTransportInterface> native_transport)
-    : ContextClient(context), native_transport_(native_transport) {}
+    : ContextClient(context),
+      current_state_(webrtc::DtlsTransportState::kNew),
+      native_transport_(native_transport),
+      proxy_(CreateProxy(context, native_transport, this)) {}
 
 RTCDtlsTransport::~RTCDtlsTransport() {}
 
 String RTCDtlsTransport::state() const {
-  NOTIMPLEMENTED();
-  return "";
+  return TransportStateToString(current_state_.state());
 }
 
 const HeapVector<Member<DOMArrayBuffer>>&
@@ -58,12 +96,32 @@ webrtc::DtlsTransportInterface* RTCDtlsTransport::native_transport() {
   return native_transport_.get();
 }
 
+// Implementation of DtlsTransportProxy::Delegate
+void RTCDtlsTransport::OnStartCompleted(webrtc::DtlsTransportInformation info) {
+  current_state_ = info;
+}
+
+void RTCDtlsTransport::OnStateChange(webrtc::DtlsTransportInformation info) {
+  // We depend on closed only happening once for safe garbage collection.
+  DCHECK(current_state_.state() != webrtc::DtlsTransportState::kClosed);
+  current_state_ = info;
+  DispatchEvent(*Event::Create(event_type_names::kStatechange));
+}
+
 const AtomicString& RTCDtlsTransport::InterfaceName() const {
   return event_target_names::kRTCDtlsTransport;
 }
 
 ExecutionContext* RTCDtlsTransport::GetExecutionContext() const {
   return ContextClient::GetExecutionContext();
+}
+
+bool RTCDtlsTransport::HasPendingActivity() const {
+  // We have to keep the RTCDtlsTransport alive while new notifications
+  // may arrive.
+  // The closed state is final, so no more events will happen after
+  // seeing that state.
+  return current_state_.state() != webrtc::DtlsTransportState::kClosed;
 }
 
 void RTCDtlsTransport::Trace(Visitor* visitor) {
