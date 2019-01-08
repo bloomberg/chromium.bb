7aad5f2a89f39c4ab300acc3b28501a35d566445
LaMont Jones <lamontjones@chromium.org>
2019-01-08 11:47:45 -0700

    lib: add kernel_cmdline module
    BUG=None
    TEST=Unit tests pass.
    
    Change-Id: I5691a3681a7f7f2ad8d4f36d74b97b6be1c1ea70
    Reviewed-on: https://chromium-review.googlesource.com/1400973
    Commit-Ready: LaMont Jones <lamontjones@chromium.org>
    Tested-by: LaMont Jones <lamontjones@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

---
+++ lib/kernel_cmdline.py
@@ -0,0 +1,295 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Utilities for kernel commandline strings."""
+
+from __future__ import print_function
+
+import collections
+import re
+
+
+class KeyValue(object):
+  """Stores a key(=value)."""
+
+  def __init__(self, key, separator, value):
+    self.key = key
+    self.separator = separator
+    self.value = value
+
+  def __ne__(self, other):
+    return not self.__eq__(other)
+
+  def __eq__(self, other):
+    return (isinstance(other, KeyValue) and
+            self.key == other.key and
+            self.separator == other.separator and
+            self.value == other.value)
+
+  def __str__(self):
+    return self.Format()
+
+  def Format(self):
+    """Return the key(=value) as a string."""
+    if isinstance(self.value, basestring):
+      value = self.value
+    else:
+      value = '"%s"' % ' '.join(x.Format() for x in self.value)
+    return '%s%s%s' % (self.key, self.separator, value)
+
+
+_KEYVALUE_RE = r'(?:(--|[^\s=]+)(?:(=)("[^"]*"|\S*))?)'
+def _ParseKeyValue(cmd_string):
+  """Tokenize a string of key(=value) pairs.
+
+  Args:
+    cmd_string: Whitespace separated collection of 'key', 'key=', and/or
+        'key=value'.
+
+  Returns:
+    List of KeyValue objects.
+  """
+  args = re.findall(_KEYVALUE_RE, cmd_string)
+  if args:
+    return [KeyValue(*x) for x in args]
+  return []
+
+
+class CommandLine(object):
+  """Make parsing the kernel command line easier.
+
+  Attributes:
+    kern_args: Kernel arguments (before any '--')
+    init_args: Any arguments for init (after the first '--')
+  """
+
+  def __init__(self, cmdline):
+    args = _ParseKeyValue(cmdline)
+    self.kern_args = args
+    self.init_args = []
+    for idx, arg  in enumerate(args):
+      if arg.key == '--' and not arg.separator:
+        self.kern_args = args[:idx]
+        self.init_args = args[idx + 1:]
+        # We are done looking at args.
+        break
+
+  def __str__(self):
+    return self.Format()
+
+  def Format(self):
+    """Format the commandline for use."""
+    parts = [x.Format() for x in self.kern_args]
+    if self.init_args:
+      parts.append('--')
+      parts += [x.Format() for x in self.init_args]
+    return ' '.join(parts)
+
+  def GetKernelParameter(self, key=None, index=None, default=None):
+    """Get kernel argument.
+
+    Get an argument from kern_args.  If |key| and |index| are specified, then
+    |index| is used.
+
+    Args:
+      key: return the first value for |key|.
+      index: return the argument at index |index| (first argument is 0).
+      default: Value to return if nothing is found.
+
+    Returns:
+      If found, returns the KeyValue from the cmdline.
+      Otherwise returns the provided default.
+
+    Raises:
+      IndexError on non-None invalid |index|.
+      IndexError if neither |key| nor |index| is specifiexd.
+    """
+    if index is not None:
+      return self.kern_args[index]
+    if key is None:
+      raise IndexError('Neither key nor index specified')
+    for arg in self.kern_args:
+      if arg.key == key:
+        return arg
+    return default
+
+  def SetKernelParameter(self, arg, index=None):
+    """Set a kernel argument.
+
+    If |arg.value| is specified and |arg.separator| is not, '=' is used for the
+    separator.
+
+    Args:
+      arg: KeyValue to place in kernel cmdline.  If |arg.value| is unquoted and
+          has whitespace, double-quotes will be added to preserve spaces.
+      index: |index| to overwrite.  If not specified, the first argument
+          matching |arg.key| will be used.
+
+    Returns:
+      Index of argument that was set.
+    """
+    # Override arg.separator and/or arg.value as needed.
+    sep = '=' if not arg.separator and arg.value else arg.separator
+    val = arg.value
+    if not val.startswith('"') and re.search(r'\s', val):
+      val = '"%s"' % val
+    if sep != arg.separator or val != arg.value:
+      arg = KeyValue(arg.key, sep, val)
+
+    if index is None:
+      # index not given, set index to the first occurrence of arg.key.
+      for index in range(len(self.kern_args)):
+        if self.kern_args[index].key == arg.key:
+          break
+      else:
+        # We did not find the key at all.  Append this to the cmdline.
+        index = len(self.kern_args)
+    if index == len(self.kern_args):
+      self.kern_args.append(arg)
+    else:
+      self.kern_args[index] = arg
+    return index
+
+  def GetDmConfig(self):
+    """Return first dm= argument for processing."""
+    value = self.GetKernelParameter('dm').value[1:-1]
+    if value:
+      return DmConfig(value)
+    return None
+
+  def SetDmConfig(self, dm_config):
+    """Set the dm= argument to a DmConfig.
+
+    Args:
+      dm_config: DmConfig instance to use.
+
+    Returns:
+      Index of argument that was set.
+    """
+    return self.SetKernelParameter(KeyValue('dm', '=', dm_config.Format()))
+
+
+class DmConfig(object):
+  """Parse the dm= parameter.
+
+  Args:
+    boot_arg: contents of the quoted dm="..." kernel cmdline element, without
+        the quotes.
+
+  Attributes:
+    num_devices: Number of devices defined in this dmsetup config.
+    devices: OrderedDict of devices, by device name.
+  """
+
+  def __init__(self, boot_arg):
+    num_devices, devices = boot_arg.split(' ', 1)
+    self.num_devices = int(num_devices)
+    lines = devices.split(',')
+    self.devices = collections.OrderedDict()
+    idx = 0
+    for _ in range(self.num_devices):
+      dev = DmDevice(lines[idx:])
+      self.devices[dev.name] = dev
+      idx += dev.num_rows + 1
+
+  def __ne__(self, other):
+    return not self.__eq__(other)
+
+  def __eq__(self, other):
+    return (isinstance(other, DmConfig) and
+            self.num_devices == other.num_devices and
+            self.devices == other.devices)
+
+  def __str__(self):
+    return self.Format()
+
+  def Format(self):
+    """Format dm= value."""
+    return ''.join(
+        ['%d ' % self.num_devices] +
+        [', '.join(x.Format() for x in self.devices.values())])
+
+
+class DmDevice(object):
+  """A single device in the dm= kernel parameter.
+
+  Args:
+    config_lines: List of lines to process.  Excess elements are ignored.
+
+  Attributes:
+    name: Name of the device
+    uuid: Uuid of the device
+    flags: One of 'ro' or 'rw'
+    num_rows: Number of dmsetup config lines (|config_lines|) used by this
+        device.
+    rows: List of DmLine objects for the device.
+  """
+
+  def __init__(self, config_lines):
+    name, uuid, flags, rows = config_lines[0].split()
+    self.name = name
+    self.uuid = uuid
+    self.flags = flags
+    self.num_rows = int(rows)
+    self.rows = [DmLine(row) for row in config_lines[1:self.num_rows + 1]]
+
+  def __ne__(self, other):
+    return not self.__eq__(other)
+
+  def __eq__(self, other):
+    return (isinstance(other, DmDevice) and
+            self.name == other.name and
+            self.uuid == other.uuid and
+            self.flags == other.flags and
+            self.num_rows == other.num_rows and
+            self.rows == other.rows)
+
+  def __str__(self):
+    return self.Format()
+
+  def Format(self):
+    """Return the device formatted for the kernel."""
+    return ','.join(
+        ['%s %s %s %d' % (self.name, self.uuid, self.flags, self.num_rows)] +
+        [x.Format() for x in self.rows])
+
+
+class DmLine(object):
+  """A single line from the dmsetup config for a device.
+
+  Attributes:
+    start: Logical start sector
+    num: Number of sectors.
+    target_type: target_type.  See dmsetup(8).
+    args: list of KeyValue args for the line.
+  """
+
+  def __init__(self, line):
+    """Parse a single line of dmsetup config."""
+    # Allow leading whitespace.
+    start, num, target, args = line.strip().split(' ', 3)
+    self.start = int(start)
+    self.num = int(num)
+    self.target_type = target
+    self.args = _ParseKeyValue(args)
+
+  def __ne__(self, other):
+    return not self.__eq__(other)
+
+  def __eq__(self, other):
+    return (isinstance(other, DmLine) and
+            self.start == other.start and
+            self.num == other.num and
+            self.target_type == other.target_type and
+            self.args == other.args)
+
+  def __str__(self):
+    return self.Format()
+
+  def Format(self):
+    """Format this line of the dmsetup config."""
+    return ','.join(['%d %d %s %s' % (
+        self.start, self.num, self.target_type, ' '.join(
+            x.Format() for x in self.args))])
---
+++ lib/kernel_cmdline_unittest.py
@@ -0,0 +1,397 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Test the kernel_cmdline module."""
+
+from __future__ import print_function
+
+from chromite.lib import cros_test_lib
+from chromite.lib import kernel_cmdline
+
+# pylint: disable=protected-access
+
+# A partial, but sufficiently complicated command line.  DmConfigTest uses
+# more complicated configs, with multiple devices.  DM is split out here for
+# CommandLineTest.test*DmConfig().
+DM = (
+    '1 vroot none ro 1,0 3891200 verity payload=PARTUUID=%U/PARTNROFF=1 '
+    'hashtree=PARTUUID=%U/PARTNROFF=1 hashstart=3891200 alg=sha1 '
+    'root_hexdigest=9999999999999999999999999999999999999999 '
+    'salt=bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
+CMDLINE = (
+    'console= loglevel=7 init=/sbin/init cros_secure oops=panic panic=-1 '
+    'root=/dev/dm-0 rootwait ro dm_verity.error_behavior=3 '
+    'dm_verity.max_bios=-1 dm_verity.dev_wait=1 dm="' + DM +
+    '" noinitrd cros_debug vt.global_cursor_default=0 kern_guid=%U '
+    '-- run_level=3')
+
+
+class KeyValueTest(cros_test_lib.TestCase):
+  """Test KeyValue"""
+
+  def testKeyOnly(self):
+    """Expands key-only arg"""
+    self.assertEqual('key', kernel_cmdline.KeyValue('key', '', '').Format())
+
+  def testKeyEqual(self):
+    """Expands key= arg"""
+    self.assertEqual('key=', kernel_cmdline.KeyValue('key', '=', '').Format())
+
+  def testKeyValue(self):
+    """Expands key=value arg"""
+    self.assertEqual('key=value',
+                     kernel_cmdline.KeyValue('key', '=', 'value').Format())
+
+  def testRecursion(self):
+    """Expands key=list-of-KeyValue"""
+    self.assertEqual('a="b c= d=e"',
+                     kernel_cmdline.KeyValue('a', '=', [
+                         kernel_cmdline.KeyValue('b', '', ''),
+                         kernel_cmdline.KeyValue('c', '=', ''),
+                         kernel_cmdline.KeyValue('d', '=', 'e')]).Format())
+
+
+class ParseKeyValueTest(cros_test_lib.TestCase):
+  """Test _ParseKeyValue()."""
+
+  def testSimple(self):
+    """Test a simple command line string."""
+    expected = [
+        kernel_cmdline.KeyValue('a', '', ''),
+        kernel_cmdline.KeyValue('b', '=', ''),
+        kernel_cmdline.KeyValue('c', '=', 'd'),
+        kernel_cmdline.KeyValue('e.f', '=', 'd'),
+        kernel_cmdline.KeyValue('a', '', ''),
+        kernel_cmdline.KeyValue('--', '', ''),
+        kernel_cmdline.KeyValue('x', '', ''),
+        kernel_cmdline.KeyValue('y', '=', ''),
+        kernel_cmdline.KeyValue('z', '=', 'zz'),
+        kernel_cmdline.KeyValue('y', '', '')]
+    args = kernel_cmdline._ParseKeyValue('a b= c=d e.f=d a -- x y= z=zz y')
+    self.assertEqual(args, expected)
+
+  def testReturnsEmptyList(self):
+    """Test that strings with no arguments return an emtpy list."""
+    self.assertEqual([], kernel_cmdline._ParseKeyValue('  '))
+
+
+class CommandLineTest(cros_test_lib.TestCase):
+  """Test the CommandLine class"""
+
+  def testSimple(self):
+    """Test a simple command line string."""
+    expected_kern = [
+        kernel_cmdline.KeyValue('a', '', ''),
+        kernel_cmdline.KeyValue('b', '=', ''),
+        kernel_cmdline.KeyValue('c', '=', 'd'),
+        kernel_cmdline.KeyValue('e.f', '=', 'd'),
+        kernel_cmdline.KeyValue('a', '', '')]
+    expected_init = [
+        kernel_cmdline.KeyValue('x', '', ''),
+        kernel_cmdline.KeyValue('y', '=', ''),
+        kernel_cmdline.KeyValue('z', '=', 'zz'),
+        kernel_cmdline.KeyValue('y', '', '')]
+    cmd = kernel_cmdline.CommandLine('a b= c=d e.f=d a -- x y= z=zz y')
+    self.assertEqual(cmd.kern_args, expected_kern)
+    self.assertEqual(cmd.init_args, expected_init)
+
+  def testEmptyInit(self):
+    """Test that 'a --' behaves as expected."""
+    expected_kern = [kernel_cmdline.KeyValue('a', '', '')]
+    expected_init = []
+    cmd = kernel_cmdline.CommandLine('a --')
+    self.assertEqual(expected_kern, cmd.kern_args)
+    self.assertEqual(expected_init, cmd.init_args)
+
+  def testEmptyKern(self):
+    """Test that '-- a' behaves as expected."""
+    expected_kern = []
+    expected_init = [kernel_cmdline.KeyValue('a', '', '')]
+    cmd = kernel_cmdline.CommandLine('-- a')
+    self.assertEqual(expected_kern, cmd.kern_args)
+    self.assertEqual(expected_init, cmd.init_args)
+
+  def testEmptyArg(self):
+    """Test that '' behaves as expected."""
+    expected_kern = []
+    expected_init = []
+    cmd = kernel_cmdline.CommandLine('')
+    self.assertEqual(expected_kern, cmd.kern_args)
+    self.assertEqual(expected_init, cmd.init_args)
+
+  def testDashesOnly(self):
+    """Test that '--' behaves as expected."""
+    expected_kern = []
+    expected_init = []
+    cmd = kernel_cmdline.CommandLine('--')
+    self.assertEqual(expected_kern, cmd.kern_args)
+    self.assertEqual(expected_init, cmd.init_args)
+
+  def testExpandsDm(self):
+    """Test that we do not expand dm="..."."""
+    expected_kern = [
+        kernel_cmdline.KeyValue('a', '', ''),
+        kernel_cmdline.KeyValue('dm', '=', '"1 vroot b=c 1,0 1200"'),
+        kernel_cmdline.KeyValue('c.d', '=', 'e')]
+    expected_init = [
+        kernel_cmdline.KeyValue('dm', '=', '"not split"'),
+        kernel_cmdline.KeyValue('a', '', '')]
+    cmd = kernel_cmdline.CommandLine(
+        'a dm="1 vroot b=c 1,0 1200" c.d=e -- dm="not split" a')
+    self.assertEqual(expected_kern, cmd.kern_args)
+    self.assertEqual(expected_init, cmd.init_args)
+
+  def testGetKernelParameterIndex(self):
+    """Test GetKernelParameter() with index="""
+    cmd = kernel_cmdline.CommandLine('a b c b=3')
+    self.assertEqual(
+        kernel_cmdline.KeyValue('a', '', ''), cmd.GetKernelParameter(index=0))
+    self.assertEqual(
+        kernel_cmdline.KeyValue('b', '', ''), cmd.GetKernelParameter(index=1))
+    self.assertEqual(
+        kernel_cmdline.KeyValue('c', '', ''), cmd.GetKernelParameter(index=2))
+    self.assertEqual(
+        kernel_cmdline.KeyValue('b', '=', '3'), cmd.GetKernelParameter(index=3))
+    with self.assertRaises(IndexError):
+      cmd.GetKernelParameter(index=4)
+
+  def testGetKernelParameter(self):
+    """Test GetKernelParameter() with key"""
+    cmd = kernel_cmdline.CommandLine('a b c=1 b=3')
+    self.assertEqual(
+        kernel_cmdline.KeyValue('a', '', ''), cmd.GetKernelParameter('a'))
+    self.assertEqual(
+        kernel_cmdline.KeyValue('b', '', ''), cmd.GetKernelParameter('b'))
+    self.assertEqual(
+        kernel_cmdline.KeyValue('c', '=', '1'), cmd.GetKernelParameter('c'))
+    self.assertEqual('default', cmd.GetKernelParameter('d', default='default'))
+
+  def testSetKernelParameterIndex(self):
+    """Test SetKernelParameter() with index="""
+    cmd = kernel_cmdline.CommandLine('a b c')
+    self.assertEqual(0, cmd.SetKernelParameter(
+        kernel_cmdline.KeyValue('d', '=', ''), index=0))
+    self.assertEqual([
+        kernel_cmdline.KeyValue('d', '=', ''),
+        kernel_cmdline.KeyValue('b', '', ''),
+        kernel_cmdline.KeyValue('c', '', '')], cmd.kern_args)
+
+  def testSetKernelParameterIndexAppends(self):
+    """Test SetKernelParameter() appends with index = len+1"""
+    cmd = kernel_cmdline.CommandLine('a b c')
+    self.assertEqual(3, cmd.SetKernelParameter(
+        kernel_cmdline.KeyValue('d', '=', ''), index=3))
+    self.assertEqual([
+        kernel_cmdline.KeyValue('a', '', ''),
+        kernel_cmdline.KeyValue('b', '', ''),
+        kernel_cmdline.KeyValue('c', '', ''),
+        kernel_cmdline.KeyValue('d', '=', '')], cmd.kern_args)
+
+  def testSetKernelParameterIndexAssertsOnBadIndex(self):
+    """Test SetKernelParameter() appends with index > len+1"""
+    cmd = kernel_cmdline.CommandLine('a b c')
+    with self.assertRaises(IndexError):
+      cmd.SetKernelParameter(kernel_cmdline.KeyValue('d', '=', ''), index=5)
+
+  def testSetKernelParameter(self):
+    """Test SetKernelParameter() with key"""
+    cmd = kernel_cmdline.CommandLine('a b c')
+    self.assertEqual(
+        1, cmd.SetKernelParameter(kernel_cmdline.KeyValue('b', '=', 'f')))
+    self.assertEqual([
+        kernel_cmdline.KeyValue('a', '', ''),
+        kernel_cmdline.KeyValue('b', '=', 'f'),
+        kernel_cmdline.KeyValue('c', '', '')], cmd.kern_args)
+
+  def testSetKernelParameterAppends(self):
+    """Test SetKernelParameter() appends new key"""
+    cmd = kernel_cmdline.CommandLine('a b c')
+    self.assertEqual(
+        3, cmd.SetKernelParameter(kernel_cmdline.KeyValue('d', '=', '99')))
+    self.assertEqual([
+        kernel_cmdline.KeyValue('a', '', ''),
+        kernel_cmdline.KeyValue('b', '', ''),
+        kernel_cmdline.KeyValue('c', '', ''),
+        kernel_cmdline.KeyValue('d', '=', '99')], cmd.kern_args)
+
+  def testSetKernelParameterAddsQuotes(self):
+    """Test that SetKernelParameter adds double-quotes when needed."""
+    cmd = kernel_cmdline.CommandLine('a b c')
+    self.assertEqual(
+        3, cmd.SetKernelParameter(kernel_cmdline.KeyValue('d', '=', '9 9')))
+    self.assertEqual([
+        kernel_cmdline.KeyValue('a', '', ''),
+        kernel_cmdline.KeyValue('b', '', ''),
+        kernel_cmdline.KeyValue('c', '', ''),
+        kernel_cmdline.KeyValue('d', '=', '"9 9"')], cmd.kern_args)
+
+  def testFormat(self):
+    """Test that the output is correct"""
+    self.assertEqual(CMDLINE, kernel_cmdline.CommandLine(CMDLINE).Format())
+
+  def testGetDmConfig(self):
+    """Test that GetDmConfig returns the DmConfig we expect."""
+    cmd = kernel_cmdline.CommandLine(CMDLINE)
+    dm = kernel_cmdline.DmConfig(DM)
+    self.assertEqual(dm, cmd.GetDmConfig())
+
+  def testSetDmConfig(self):
+    """Test that SetDmConfig sets the dm= parameter."""
+    cmd = kernel_cmdline.CommandLine('a -- b')
+    dm = kernel_cmdline.DmConfig(DM)
+    cmd.SetDmConfig(dm)
+    expected = kernel_cmdline.KeyValue('dm', '=', '"%s"' % DM)
+    self.assertEqual(expected, cmd.kern_args[1])
+
+
+class DmConfigTest(cros_test_lib.TestCase):
+  """Test DmConfig."""
+
+  def testParses(self):
+    """Verify that DmConfig correctly parses the config from DmDevice."""
+    device_data = [
+        ['vboot uuid ro 1', '0 1 verity arg1 arg2'],
+        ['vblah uuid2 ro 2',
+         '100 9 stripe larg1=val1',
+         '200 10 stripe larg2=val2'],
+        ['vroot uuid3 ro 1', '99 3 linear larg1 larg2']]
+    dm_arg = ', '.join(','.join(x for x in data) for data in device_data)
+    devices = [kernel_cmdline.DmDevice(x) for x in device_data]
+    dc = kernel_cmdline.DmConfig('%d %s' % (len(device_data), dm_arg))
+    self.assertEqual(len(device_data), dc.num_devices)
+    self.assertEqual(devices, list(dc.devices.values()))
+
+  def testFormats(self):
+    """Verify that DmConfig recreates its input string."""
+    device_data = [
+        ['vboot uuid ro 1', '0 1 verity arg1 arg2'],
+        ['vblah uuid2 ro 2',
+         '100 9 stripe larg1=val1',
+         '200 10 stripe larg2=val2'],
+        ['vroot uuid3 ro 1', '99 3 linear larg1 larg2']]
+    dm_arg = ', '.join(','.join(x for x in data) for data in device_data)
+    dm_config_val = '%d %s' % (len(device_data), dm_arg)
+    dc = kernel_cmdline.DmConfig(dm_config_val)
+    self.assertEqual(dm_config_val, dc.Format())
+
+  def testEqual(self):
+    """Test that equal instances are equal."""
+    arg = '2 v1 u1 f1 1,0 1 t1 a1, v2 u2 f2 2,3 4 t2 a2,5 6 t3 a3'
+    dc = kernel_cmdline.DmConfig(arg)
+    self.assertEqual(dc, kernel_cmdline.DmConfig(arg))
+
+  def testNotEqual(self):
+    """Test that unequal instances are unequal."""
+    # Start with duplicated instances, and change fields to verify that all the
+    # fields matter.
+    arg = '2 v1 u1 f1 1,0 1 t1 a1, v2 u2 f2 2,3 4 t2 a2,5 6 t3 a3'
+    dc1 = kernel_cmdline.DmConfig(arg)
+    self.assertNotEqual(dc1, '')
+    dc2 = kernel_cmdline.DmConfig(arg)
+    dc2.num_devices = 1
+    self.assertNotEqual(dc1, dc2)
+    dc3 = kernel_cmdline.DmConfig(arg)
+    dc3.devices = []
+    self.assertNotEqual(dc1, dc3)
+
+
+class DmDeviceTest(cros_test_lib.TestCase):
+  """Test DmDevice."""
+
+  def testParsesOneLine(self):
+    """Verify that DmDevice correctly handles the results from DmLine."""
+    lines = ['vboot none ro 1', '0 1 verity arg']
+    dd = kernel_cmdline.DmDevice(lines)
+    self.assertEqual('vboot', dd.name)
+    self.assertEqual('none', dd.uuid)
+    self.assertEqual('ro', dd.flags)
+    self.assertEqual(1, dd.num_rows)
+    self.assertEqual([kernel_cmdline.DmLine('0 1 verity arg')], dd.rows)
+
+  def testParsesMultiLine(self):
+    """Verify that DmDevice correctly handles multiline device."""
+    lines = ['vboot none ro 2', '0 1 verity arg', '9 10 type a2']
+    dd = kernel_cmdline.DmDevice(lines)
+    self.assertEqual('vboot', dd.name)
+    self.assertEqual('none', dd.uuid)
+    self.assertEqual('ro', dd.flags)
+    self.assertEqual(2, dd.num_rows)
+    self.assertEqual([kernel_cmdline.DmLine('0 1 verity arg'),
+                      kernel_cmdline.DmLine('9 10 type a2')], dd.rows)
+
+  def testParsesIgnoresExcessRows(self):
+    """Verify that DmDevice ignores excess rows."""
+    lines = ['vboot none ro 2', '0 1 verity arg', '9 10 type a2', '4 5 t a3']
+    dd = kernel_cmdline.DmDevice(lines)
+    self.assertEqual('vboot', dd.name)
+    self.assertEqual('none', dd.uuid)
+    self.assertEqual('ro', dd.flags)
+    self.assertEqual(2, dd.num_rows)
+    self.assertEqual([kernel_cmdline.DmLine('0 1 verity arg'),
+                      kernel_cmdline.DmLine('9 10 type a2')], dd.rows)
+
+  def testEqual(self):
+    """Test that equal instances are equal."""
+    dd = kernel_cmdline.DmDevice(['v u f 1', '0 1 typ a'])
+    self.assertEqual(dd, kernel_cmdline.DmDevice(['v u f 1', '0 1 typ a']))
+
+  def testMultiLineEqual(self):
+    """Test that equal instances are equal."""
+    dd = kernel_cmdline.DmDevice(['v u f 2', '0 1 typ a', '2 3 t b'])
+    self.assertEqual(
+        dd, kernel_cmdline.DmDevice(['v u f 2', '0 1 typ a', '2 3 t b']))
+
+  def testNotEqual(self):
+    """Test that unequal instances are unequal."""
+    dd = kernel_cmdline.DmDevice(['v u f 2', '0 1 typ a', '2 3 t b'])
+    self.assertNotEqual(dd, '')
+    self.assertNotEqual(
+        dd, kernel_cmdline.DmDevice(['x u f 2', '0 1 typ a', '2 3 t b']))
+    self.assertNotEqual(
+        dd, kernel_cmdline.DmDevice(['v x f 2', '0 1 typ a', '2 3 t b']))
+    self.assertNotEqual(
+        dd, kernel_cmdline.DmDevice(['v u x 2', '0 1 typ a', '2 3 t b']))
+    self.assertNotEqual(
+        dd, kernel_cmdline.DmDevice(['v u f 1', '0 1 typ a', '2 3 t b']))
+    self.assertNotEqual(
+        dd, kernel_cmdline.DmDevice(['v u f 2', '9 1 typ a', '2 3 t b']))
+
+
+class DmLineTest(cros_test_lib.TestCase):
+  """Test DmLine."""
+
+  def testParses(self):
+    """Verify that DmLine correctly parses a line, and returns it."""
+    text = '0 1 verity a1 a2=v2 a3'
+    dl = kernel_cmdline.DmLine(text)
+    self.assertEqual(0, dl.start)
+    self.assertEqual(1, dl.num)
+    self.assertEqual('verity', dl.target_type)
+    self.assertEqual(kernel_cmdline._ParseKeyValue('a1 a2=v2 a3'), dl.args)
+    self.assertEqual(text, dl.Format())
+
+  def testAllowsWhitespace(self):
+    """Verify that leading/trailing whitespace is ignored."""
+    text = '0 1 verity a1 a2=v2 a3'
+    dl = kernel_cmdline.DmLine(' %s ' % text)
+    self.assertEqual(0, dl.start)
+    self.assertEqual(1, dl.num)
+    self.assertEqual('verity', dl.target_type)
+    self.assertEqual(kernel_cmdline._ParseKeyValue('a1 a2=v2 a3'), dl.args)
+    self.assertEqual(text, dl.Format())
+
+  def testEqual(self):
+    """Test that equal instances are equal."""
+    dl = kernel_cmdline.DmLine('0 1 verity a1 a2=v2 a3')
+    self.assertEqual(dl, kernel_cmdline.DmLine('0 1 verity a1 a2=v2 a3'))
+
+  def testNotEqual(self):
+    """Test that unequal instances are unequal."""
+    dl = kernel_cmdline.DmLine('0 1 verity a1 a2=v2 a3')
+    self.assertNotEqual(dl, '')
+    self.assertNotEqual(dl, kernel_cmdline.DmLine('1 1 verity a1 a2=v2 a3'))
+    self.assertNotEqual(dl, kernel_cmdline.DmLine('0 2 verity a1 a2=v2 a3'))
+    self.assertNotEqual(dl, kernel_cmdline.DmLine('0 1 verit  a1 a2=v2 a3'))
+    self.assertNotEqual(dl, kernel_cmdline.DmLine('0 1 verity aN a2=v2 a3'))
