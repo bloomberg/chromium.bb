c0d2879a46075a33410455fc4b62aa1b7f924941
Ioana Pandele <ioanap@chromium.org>
2019-01-08 09:50:31 +0000

    Simplify ManagePasswordsUIProvider code [1/5]
    This introduces a new interface, GooglePasswordManagerUIProvider,
    aiming to replace the current ManagePasswordsUIProvider code, but
    with a narrower scope.
    
    Bug: 912143
    Change-Id: Ia11d8a20195d0bddc6806866096b523681ccc22a
    Reviewed-on: https://chromium-review.googlesource.com/c/1367646
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#620667}
    Former-commit-id: e2c5a8455d2e376fbe3c8ea58be2eae420d36e4e

--- chrome/android/java/src/org/chromium/chrome/browser/AppHooks.java
+++ chrome/android/java/src/org/chromium/chrome/browser/AppHooks.java
@@ -41,6 +41,7 @@ import org.chromium.chrome.browser.omaha.RequestGenerator;
 import org.chromium.chrome.browser.partnerbookmarks.PartnerBookmark;
 import org.chromium.chrome.browser.partnerbookmarks.PartnerBookmarksProviderIterator;
 import org.chromium.chrome.browser.partnercustomizations.PartnerBrowserCustomizations;
+import org.chromium.chrome.browser.password_manager.GooglePasswordManagerUIProvider;
 import org.chromium.chrome.browser.password_manager.ManagePasswordsUIProvider;
 import org.chromium.chrome.browser.policy.PolicyAuditor;
 import org.chromium.chrome.browser.preferences.LocationSettings;
@@ -192,6 +193,14 @@ public abstract class AppHooks {
      */
     public ManagePasswordsUIProvider createManagePasswordsUIProvider() {
         return new ManagePasswordsUIProvider();
+    }
+
+    /**
+     * @return An instance of {@link GooglePasswordManagerUIProvider}. Will be null if one is not
+     *         available.
+     */
+    public GooglePasswordManagerUIProvider createGooglePasswordManagerUIProvider() {
+        return null;
     }
 
     /**
