f1645a5a1182cd4537a6042c301d32202fbaaaf2
Dan Sanders <sandersd@chromium.org>
2019-01-09 22:07:36 +0000

    [media] Add more documentation to VideoDecoderConfig members.
    Change-Id: I3704ec5c831498aa876ae3cea6ca458d77c0d275
    Reviewed-on: https://chromium-review.googlesource.com/c/1401608
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621311}
    Former-commit-id: cde4bfd1c1759aeda57914270cfa103a6b964119

--- media/base/video_decoder_config.h
+++ media/base/video_decoder_config.h
@@ -27,6 +27,8 @@ namespace media {
 MEDIA_EXPORT VideoCodec
 VideoCodecProfileToVideoCodec(VideoCodecProfile profile);
 
+// Describes the content of a video stream, as described by the media container
+// (or otherwise determined by the demuxer).
 class MEDIA_EXPORT VideoDecoderConfig {
  public:
   // Constructs an uninitialized object. Clients should call Initialize() with
@@ -77,13 +79,21 @@ class MEDIA_EXPORT VideoDecoderConfig {
 
   static std::string GetHumanReadableProfile(VideoCodecProfile profile);
 
+  // Video codec and profile.
   VideoCodec codec() const { return codec_; }
   VideoCodecProfile profile() const { return profile_; }
 
-  // Video format used to determine YUV buffer sizes.
+  // Encoded video pixel format. Lossy codecs rarely have actual pixel formats,
+  // this should usually be interpreted as a subsampling specification. (The
+  // decoder determines the actual pixel format.)
   VideoPixelFormat format() const { return format_; }
 
-  // Default is VIDEO_ROTATION_0.
+  // Difference between encoded and display orientation.
+  //
+  // Default is VIDEO_ROTATION_0. Note that rotation should be applied after
+  // scaling to natural_size().
+  //
+  // TODO(sandersd): Which direction is orientation measured in?
   VideoRotation video_rotation() const { return rotation_; }
 
   // Deprecated. TODO(wolenetz): Remove. See https://crbug.com/665539.
@@ -91,20 +101,28 @@ class MEDIA_EXPORT VideoDecoderConfig {
   // in this region are valid.
   const gfx::Size& coded_size() const { return coded_size_; }
 
-  // Region of |coded_size_| that is visible.
+  // Region of coded_size() that contains image data, also known as the clean
+  // aperture. Usually, but not always, origin-aligned (top-left).
   const gfx::Rect& visible_rect() const { return visible_rect_; }
 
   // Final visible width and height of a video frame with aspect ratio taken
-  // into account.
+  // into account. Image data in the visible_rect() should be scaled to this
+  // size for display.
   const gfx::Size& natural_size() const { return natural_size_; }
 
+  // The shape of encoded pixels. Given visible_rect() and a pixel aspect ratio,
+  // it is possible to compute natural_size() (see video_util.h).
+  //
   // TODO(crbug.com/837337): This should be explicitly set (replacing
   // |natural_size|). It should also be possible to determine whether it was set
   // at all, since in-stream information may override it if it was not.
   double GetPixelAspectRatio() const;
 
-  // Optional byte data required to initialize video decoders, such as H.264
-  // AVCC data.
+  // Optional video decoder initialization data, such as H.264 AVCC.
+  //
+  // Note: FFmpegVideoDecoder assumes that H.264 is in AVC format if there is
+  // |extra_data|, and in Annex B format if there is not. We should probably add
+  // explicit signaling of encoded format.
   void SetExtraData(const std::vector<uint8_t>& extra_data);
   const std::vector<uint8_t>& extra_data() const { return extra_data_; }
 
@@ -118,8 +136,10 @@ class MEDIA_EXPORT VideoDecoderConfig {
     return encryption_scheme_;
   }
 
+  // Color space of the image data.
   const VideoColorSpace& color_space_info() const;
 
+  // Dynamic range of the image data.
   void set_hdr_metadata(const HDRMetadata& hdr_metadata);
   const base::Optional<HDRMetadata>& hdr_metadata() const;
 
