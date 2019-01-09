e8f856c2561e623e3034b46920205dd948025340
Mohamed Amir Yosef <mamir@chromium.org>
2019-01-09 14:09:05 +0000

    [Sync::USS] Implement PasswordSyncBridge::GetStorageKey()
    Bug: 902349
    Change-Id: If5d3ff4cfee29561e735af66cca2453a104dba32
    Reviewed-on: https://chromium-review.googlesource.com/c/1402444
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621136}
    Former-commit-id: 0fad71db499e559926c900c6e5c2a90be5384c93

--- components/password_manager/core/browser/sync/password_sync_bridge.h
+++ components/password_manager/core/browser/sync/password_sync_bridge.h
@@ -44,6 +44,7 @@ class PasswordSyncBridge : public syncer::ModelTypeSyncBridge {
   void GetAllDataForDebugging(DataCallback callback) override;
   std::string GetClientTag(const syncer::EntityData& entity_data) override;
   std::string GetStorageKey(const syncer::EntityData& entity_data) override;
+  bool SupportsGetStorageKey() const override;
   ModelTypeSyncBridge::StopSyncResponse ApplyStopSyncChanges(
       std::unique_ptr<syncer::MetadataChangeList> delete_metadata_change_list)
       override;
