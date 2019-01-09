f043718aa77e60aa66315b4f1f6f4b04df50b60a
Evan Hernandez <evanhernandez@chromium.org>
2019-01-08 18:11:41 -0700

    cros_branch_unittest: Split tests.
    BranchCommandTest was beginning to look less like a
    unit test and more like a functional test. Split it
    up in anticipation of future CLs that add complexity.
    
    TEST=./run_tests cli/cros/cros_branch_unittest
    BUG=chromium:825241
    
    Change-Id: Ibe52c9d0e45351ff5816114574ae506747358c4c
    Reviewed-on: https://chromium-review.googlesource.com/1401644
    Commit-Ready: Evan Hernandez <evanhernandez@chromium.org>
    Tested-by: Evan Hernandez <evanhernandez@chromium.org>
    Reviewed-by: Lann Martin <lannm@chromium.org>

--- cli/cros/cros_branch_unittest.py
+++ cli/cros/cros_branch_unittest.py
@@ -9,6 +9,7 @@ from __future__ import print_function
 
 from chromite.cbuildbot.manifest_version import VersionInfo
 from chromite.cli import command_unittest
+from chromite.cli.cros.cros_branch import Branch
 from chromite.cli.cros.cros_branch import BranchCommand
 from chromite.cli.cros.cros_branch import FactoryBranch
 from chromite.cli.cros.cros_branch import FirmwareBranch
@@ -17,66 +18,105 @@ from chromite.cli.cros.cros_branch import StabilizeBranch
 from chromite.lib import cros_test_lib
 from chromite.lib import partial_mock
 from chromite.lib import repo_manifest
+from chromite.lib import repo_util
 
-BRANCH_NAME = 'branch'
-MILESTONE = '12'
-VERSION = '3.4.5'
-MANIFEST_OUTER_XML = '''\
+
+class BranchTest(cros_test_lib.MockTestCase):
+  """Establishes environment for testing sublcasses of Branch."""
+
+  MILESTONE = '12'
+  VERSION = '3.4.5'
+  MANIFEST = '''\
 <?xml version="1.0" encoding="UTF-8"?>
-<manifest>%s</manifest>
-'''
-REMOTES_XML = '''\
+<manifest>
   <remote fetch="https://chromium.googlesource.com"
-        name="cros"
-        review="chromium-review.googlesource.com"/>
-'''
-DEFAULTS_XML = '''\
+          name="cros"
+          review="chromium-review.googlesource.com"/>
   <default remote="cros" revision="refs/heads/master" sync-j="8"/>
-'''
-BRANCHABLE_PROJECTS_XML = '''\
-  <!-- A standard project that should always be branched. -->
-  <project groups="minilayout,buildtools"
-          name="chromiumos/chromite"
-          path="chromite"
-          revision="refs/heads/special-branch"/>
-
-  <!-- Explicitly specified branching strategy. -->
-  <project name="chromiumos/explicit-create"
-          path="explicit-create"
-          revision="refs/heads/special-branch">
-    <annotation name="branch-mode" value="create"/>
-  </project>
-'''
-NOT_BRANCHABLE_PROJECTS_XML = '''\
-  <!-- Explicitly specified branching strategies. -->
-  <project name="chromiumos/external-explicitly-pinned"
-          path="explicit-external"
-          revision="refs/heads/master">
-    <annotation name="branch-mode" value="pin"/>
-  </project>
-
-  <project name="chromiumos/external-explicitly-unpinned"
-          path="explicit-unpinned"
+  <project name="chromiumos/manifest"
+          path="path/manifest"
+          revision="refs/heads/manifest-branch"/>
+  <project name="chromiumos/chromite"
+          path="path/chromite"
+          revision="refs/heads/chromite-branch"/>
+  <project name="chromiumos/not-branchable"
+          path="path/not-branchable"
           revision="refs/heads/master">
     <annotation name="branch-mode" value="tot"/>
   </project>
+</manifest>
+  '''
+  BRANCH_NAME = 'branch'
 
-  <!-- The next two projects test legacy heristic to determine branching
-      strategy for projects -->
-  <project name="faraway/external"
-          path="external"
-          revision="refs/heads/master"/>
+  def CreateInstance(self):
+    return Branch('fake-kind', self.BRANCH_NAME)
 
-  <project name="faraway/unpinned"
-          path="unpinned"
-          revision="refs/heads/master"
-          pin="False"/>
-'''
-FULL_MANIFEST = MANIFEST_OUTER_XML % (
-    REMOTES_XML + DEFAULTS_XML + BRANCHABLE_PROJECTS_XML +
-    NOT_BRANCHABLE_PROJECTS_XML)
-BRANCHABLE_MANIFEST = MANIFEST_OUTER_XML % (
-    REMOTES_XML + DEFAULTS_XML + BRANCHABLE_PROJECTS_XML)
+  def setUp(self):
+    self.rc_mock = cros_test_lib.RunCommandMock()
+    self.rc_mock.SetDefaultCmdResult()
+    self.StartPatcher(self.rc_mock)
+
+    self.PatchObject(
+        repo_util.Repository,
+        'Manifest',
+        return_value=repo_manifest.Manifest.FromString(self.MANIFEST))
+    self.PatchObject(
+        VersionInfo,
+        'from_repo',
+        return_value=VersionInfo(self.VERSION, self.MILESTONE))
+
+    # Instance must be created last for patches to be applied.
+    self.inst = self.CreateInstance()
+
+  def testCreateLocal(self):
+    self.inst.Create(self.VERSION)
+
+    # Assert that local checkout was synced to correct version.
+    self.rc_mock.assertCommandContains([
+        partial_mock.ListRegex('.*/repo_sync_manifest'), '--version',
+        self.VERSION
+    ])
+
+    # Assert that all branchable projects were branched.
+    for project in ('manifest', 'chromite'):
+      self.rc_mock.assertCommandContains(
+          [
+              'git', 'checkout', '-B', self.BRANCH_NAME,
+              'refs/heads/%s-branch' % project
+          ],
+          cwd=partial_mock.ListRegex('.*/path/%s' % project))
+
+
+class ReleaseBranchTest(BranchTest):
+  """Tests and config specifically for ReleaseBranch."""
+  BRANCH_NAME = 'release-R12-3.B'
+
+  def CreateInstance(self):
+    return ReleaseBranch()
+
+
+class FactoryBranchTest(BranchTest):
+  """Tests and config specifically for FactoryBranch."""
+  BRANCH_NAME = 'factory-3.B'
+
+  def CreateInstance(self):
+    return FactoryBranch()
+
+
+class FirmwareBranchTest(BranchTest):
+  """Tests and config specifically for FirmwareBranch."""
+  BRANCH_NAME = 'firmware-3.B'
+
+  def CreateInstance(self):
+    return FirmwareBranch()
+
+
+class StabilizeBranchTest(BranchTest):
+  """Tests and config specifically for StabilizeBranch."""
+  BRANCH_NAME = 'stabilize-3.B'
+
+  def CreateInstance(self):
+    return StabilizeBranch()
 
 
 class MockBranchCommand(command_unittest.MockCommand):
@@ -94,36 +134,33 @@ class MockBranchCommand(command_unittest.MockCommand):
 
 class BranchCommandTest(cros_test_lib.MockTestCase):
   """Tests for BranchCommand functions."""
+  VERSION = '3.4.5'
+  BRANCH_NAME = 'branch'
 
   def SetUpCommandMock(self, args):
     cmd = MockBranchCommand(args)
     self.StartPatcher(cmd)
     return cmd
 
-  def testBranchNameGeneration(self):
-    self.PatchObject(
-        VersionInfo, 'from_repo', return_value=VersionInfo(VERSION, MILESTONE))
-    self.assertEqual(ReleaseBranch().GenerateName(), 'release-R12-3.B')
-    self.assertEqual(FactoryBranch().GenerateName(), 'factory-3.B')
-    self.assertEqual(FirmwareBranch().GenerateName(), 'firmware-3.B')
-    self.assertEqual(StabilizeBranch().GenerateName(), 'stabilize-3.B')
+  def TestCreateCommandParsesFor(self, cls, kind_flag):
+    cmd = self.SetUpCommandMock(['create', self.VERSION, kind_flag])
 
-  def testCreateBranchesLocal(self):
-    # Configure and run `cros branch`.
-    cmd = self.SetUpCommandMock(
-        ['create', VERSION, '--name', BRANCH_NAME, '--factory'])
-    cmd.rc_mock.AddCmdResult(
-        [partial_mock.ListRegex('.*/repo'), 'manifest'], output=FULL_MANIFEST)
-    cmd.inst.Run()
+    self.assertIs(cmd.inst.options.cls, cls)
 
-    # Assert that local checkout was synced to correct version, and that
-    # correct projects were branched.
-    cmd.rc_mock.assertCommandContains(
-        [partial_mock.ListRegex('.*/repo_sync_manifest'), '--version', VERSION])
-    branchable_projects = repo_manifest.Manifest.FromString(
-        BRANCHABLE_MANIFEST).Projects()
-    for project in branchable_projects:
-      cmd.rc_mock.assertCommandContains(
-          ['git', 'checkout', '-B', BRANCH_NAME,
-           project.Revision()],
-          cwd=partial_mock.ListRegex('.*%s' % project.Path()))
+    # It's important that `cros branch` does not perform dangerous
+    # operations by default.
+    self.assertIsNone(cmd.inst.options.name)
+    self.assertFalse(cmd.inst.options.force)
+    self.assertFalse(cmd.inst.options.push)
+
+  def testCreateReleaseCommandParses(self):
+    self.TestCreateCommandParsesFor(ReleaseBranch, '--release')
+
+  def testCreateFactoryCommandParses(self):
+    self.TestCreateCommandParsesFor(FactoryBranch, '--factory')
+
+  def testCreateFirmwareCommandParses(self):
+    self.TestCreateCommandParsesFor(FirmwareBranch, '--firmware')
+
+  def testCreateStabilizeCommandParses(self):
+    self.TestCreateCommandParsesFor(StabilizeBranch, '--stabilize')
