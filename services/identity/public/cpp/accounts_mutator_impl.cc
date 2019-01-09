585c0c6b72bc6774a6e43da23e17cea559135ebe
Mario Sanchez Prada <mario@igalia.com>
2019-01-09 18:41:57 +0000

    Implement methods to remove accounts in the AccountsMutator's API
    This patchset implements the RemoveAccount and RemoveAllAccounts methods
    as per the API described in the "Supporting account seeding and refresh
    token mutation in Identity Manager" document [1], along with new unit
    tests for both of them.
    
    A follow-up CL will implement the remaining methods to add or update
    accounts and refresh tokens, once the discussion in [1] is settled.
    
    [1] https://docs.google.com/document/d/1kLjnxPnBAX0G6W-3u6OF_VguuJc0pXdgCRVR0z3yn-g
    
    Bug: 887870, 907901
    Change-Id: I827cee4f339dd378b5aefe95b372ad2d115e71cc
    Reviewed-on: https://chromium-review.googlesource.com/c/1373828
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621241}
    Former-commit-id: c1485520dd0b14725862d7df7bc866e968610848

--- services/identity/public/cpp/accounts_mutator_impl.cc
+++ services/identity/public/cpp/accounts_mutator_impl.cc
@@ -5,14 +5,28 @@
 #include "services/identity/public/cpp/accounts_mutator_impl.h"
 
 #include "base/logging.h"
+#include "components/signin/core/browser/account_info.h"
+#include "components/signin/core/browser/profile_oauth2_token_service.h"
 
 namespace identity {
 
 AccountsMutatorImpl::AccountsMutatorImpl(
-    ProfileOAuth2TokenService* token_service) {
-  DCHECK(token_service);
+    ProfileOAuth2TokenService* token_service)
+    : token_service_(token_service) {
+  DCHECK(token_service_);
 }
 
 AccountsMutatorImpl::~AccountsMutatorImpl() {}
+
+void AccountsMutatorImpl::RemoveAccount(
+    const std::string& account_id,
+    signin_metrics::SourceForRefreshTokenOperation source) {
+  token_service_->RevokeCredentials(account_id, source);
+}
+
+void AccountsMutatorImpl::RemoveAllAccounts(
+    signin_metrics::SourceForRefreshTokenOperation source) {
+  token_service_->RevokeAllCredentials(source);
+}
 
 }  // namespace identity
--- services/identity/public/cpp/accounts_mutator_impl.h
+++ services/identity/public/cpp/accounts_mutator_impl.h
@@ -6,6 +6,7 @@
 #define SERVICES_IDENTITY_PUBLIC_CPP_ACCOUNTS_MUTATOR_IMPL_H_
 
 #include "base/macros.h"
+#include "components/signin/core/browser/signin_metrics.h"
 #include "services/identity/public/cpp/accounts_mutator.h"
 
 class ProfileOAuth2TokenService;
@@ -18,7 +19,21 @@ class AccountsMutatorImpl : public AccountsMutator {
   explicit AccountsMutatorImpl(ProfileOAuth2TokenService* token_service);
   ~AccountsMutatorImpl() override;
 
+  // Removes the account given by |account_id|. Also revokes the token
+  // server-side if needed.
+  void RemoveAccount(
+      const std::string& account_id,
+      signin_metrics::SourceForRefreshTokenOperation source =
+          signin_metrics::SourceForRefreshTokenOperation::kUnknown) override;
+
+  // Removes all accounts.
+  void RemoveAllAccounts(
+      signin_metrics::SourceForRefreshTokenOperation source =
+          signin_metrics::SourceForRefreshTokenOperation::kUnknown) override;
+
  private:
+  ProfileOAuth2TokenService* token_service_;
+
   DISALLOW_COPY_AND_ASSIGN(AccountsMutatorImpl);
 };
 
