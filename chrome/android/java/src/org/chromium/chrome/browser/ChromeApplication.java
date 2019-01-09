4d3334001c6d8162d07ff8f5135c47d87d224d6f
Becky Zhou <huayinz@chromium.org>
2019-01-09 16:29:14 +0000

    [Dark] Add build flag and experiment flag
    This patch adds a build flag and an experiment flag for the dark mode
    experiment. It will exclude the night- resources in the build for now.
    
    Bug: 919684
    Change-Id: I16f7dc0b076268863f6ec537d4ee41af124366df
    Reviewed-on: https://chromium-review.googlesource.com/c/1399457
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Becky Zhou <huayinz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621172}
    Former-commit-id: 8ef8c364dc0648b4905c4f9cfdf053e4279a47fb

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
@@ -10,6 +10,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatDelegate;
 
 import org.chromium.base.ActivityState;
 import org.chromium.base.ApplicationState;
@@ -40,6 +41,7 @@ import org.chromium.chrome.browser.dependency_injection.ModuleFactoryOverrides;
 import org.chromium.chrome.browser.init.InvalidStartupDialog;
 import org.chromium.chrome.browser.metrics.UmaUtils;
 import org.chromium.chrome.browser.preferences.ChromePreferenceManager;
+import org.chromium.chrome.browser.util.FeatureUtilities;
 import org.chromium.chrome.browser.vr.OnExitVrRequestListener;
 import org.chromium.chrome.browser.vr.VrModuleProvider;
 import org.chromium.components.module_installer.ModuleInstaller;
@@ -56,6 +58,12 @@ public class ChromeApplication extends Application {
 
     @Nullable
     private static ChromeAppComponent sComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initDefaultNightMode();
+    }
 
     // Called by the framework for ALL processes. Runs before ContentProviders are created.
     // Quirk: context.getApplicationContext() returns null during this method.
@@ -213,6 +221,16 @@ public class ChromeApplication extends Application {
             @Override
             public void onDenied() {}
         });
+    }
+
+    private void initDefaultNightMode() {
+        if (FeatureUtilities.isNightModeAvailable()) {
+            // TODO(huayinz): Initialize default night mode based on settings.
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            // Force not to use night mode.
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
     }
 
     /** Returns the application-scoped component. */
