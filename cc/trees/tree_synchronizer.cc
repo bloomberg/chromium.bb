178b12ab98221fcfb1d3b843d4ef37b49beb1a95
Adrienne Walker <enne@chromium.org>
2019-01-09 18:13:08 +0000

    cc: temporary debugging for issue 918126
    Bug: 918126
    Change-Id: I9e82d8ddd3145764849759a94c3c830a9c5dd73f
    Reviewed-on: https://chromium-review.googlesource.com/c/1401431
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621221}
    Former-commit-id: 50a0e60996285b5bf8a2f579738b3265e7536243

--- cc/trees/tree_synchronizer.cc
+++ cc/trees/tree_synchronizer.cc
@@ -9,6 +9,8 @@
 #include <set>
 
 #include "base/containers/flat_set.h"
+#include "base/debug/crash_logging.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/logging.h"
 #include "base/stl_util.h"
 #include "base/trace_event/trace_event.h"
@@ -154,6 +156,50 @@ void TreeSynchronizer::SynchronizeTrees(LayerTreeImpl* pending_tree,
 template <typename Iterator>
 static void PushLayerPropertiesInternal(Iterator source_layers_begin,
                                         Iterator source_layers_end,
+                                        LayerTreeHost* host_tree,
+                                        LayerTreeImpl* target_impl_tree) {
+  for (Iterator it = source_layers_begin; it != source_layers_end; ++it) {
+    auto* source_layer = *it;
+    LayerImpl* target_layer = target_impl_tree->LayerById(source_layer->id());
+    DCHECK(target_layer);
+    // TODO(enne): http://crbug.com/918126 debugging
+    if (!target_layer) {
+      bool host_set_on_source = source_layer->layer_tree_host() == host_tree;
+
+      bool source_found_by_iterator = false;
+      for (auto it = host_tree->begin(); it != host_tree->end(); ++it) {
+        if (*it == source_layer) {
+          source_found_by_iterator = true;
+          break;
+        }
+      }
+
+      bool root_layer_valid = !!host_tree->root_layer();
+      bool found_root = false;
+      Layer* layer = source_layer;
+      while (layer) {
+        if (layer == host_tree->root_layer()) {
+          found_root = true;
+          break;
+        }
+        layer = layer->parent();
+      }
+
+      auto str = base::StringPrintf(
+          "hs: %d, sf: %d, rlv: %d, fr: %d", host_set_on_source,
+          source_found_by_iterator, root_layer_valid, found_root);
+      static auto* crash_key = base::debug::AllocateCrashKeyString(
+          "cc_null_layer_sync", base::debug::CrashKeySize::Size32);
+      base::debug::SetCrashKeyString(crash_key, str);
+      base::debug::DumpWithoutCrashing();
+    }
+    source_layer->PushPropertiesTo(target_layer);
+  }
+}
+
+template <typename Iterator>
+static void PushLayerPropertiesInternal(Iterator source_layers_begin,
+                                        Iterator source_layers_end,
                                         LayerTreeImpl* target_impl_tree) {
   for (Iterator it = source_layers_begin; it != source_layers_end; ++it) {
     auto* source_layer = *it;
@@ -183,7 +229,8 @@ void TreeSynchronizer::PushLayerProperties(LayerTreeHost* host_tree,
   auto layers = host_tree->LayersThatShouldPushProperties();
   TRACE_EVENT1("cc", "TreeSynchronizer::PushLayerPropertiesTo.Main",
                "layer_count", layers.size());
-  PushLayerPropertiesInternal(layers.begin(), layers.end(), impl_tree);
+  PushLayerPropertiesInternal(layers.begin(), layers.end(), host_tree,
+                              impl_tree);
   // When using layer lists, we may not have layers for all property tree
   // node ids and need to synchronize the registered id list.
   if (host_tree->IsUsingLayerLists())
