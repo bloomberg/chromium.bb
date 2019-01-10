6247c1b85a3596514862a2c780f61dd617d83457
Dominik Röttsches <drott@chromium.org>
2019-01-10 09:13:15 +0000

    UMA metrics for detecting uniquely matched fonts outside system font dir
    Add UMA metrics for detecting whether a uniquely matched font can be
    instantiated, or whether access to it is restricted by the sandbox or
    other reasons. The expectation is that this is a highly rare case. If a
    font cannot be instantiated, Blink automatically falls back to the next
    font in the CSS font family list, or performs system fallback.
    
    With this metric, we would like to find out whether we need to cover the
    case of uniquely matched fonts outside of system locations, or not. For
    example, by opening those files on the browser process side and passing
    the handle to the renderer.
    
    Bug: 889864
    Change-Id: I27a242a1b2fca39be740897a5f270780f2c91a5a
    Reviewed-on: https://chromium-review.googlesource.com/c/1400694
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621513}
    Former-commit-id: c093321cee234053ffdf18958def34219a0f6467

--- third_party/blink/renderer/platform/fonts/win/font_unique_name_lookup_win.cc
+++ third_party/blink/renderer/platform/fonts/win/font_unique_name_lookup_win.cc
@@ -7,7 +7,22 @@
 #include "base/files/file_path.h"
 #include "mojo/public/mojom/base/shared_memory.mojom-blink.h"
 #include "third_party/blink/public/mojom/dwrite_font_proxy/dwrite_font_proxy.mojom-blink.h"
+#include "third_party/blink/renderer/platform/histogram.h"
 #include "third_party/skia/include/ports/SkTypeface_win.h"
+
+namespace {
+
+// These enum values correspond to the
+// "Blink.Fonts.WindowsUniqueLocalFontInstantiationResult" histogram, new values
+// can be added, but old values should never be reused.
+enum WindowsUniqueLocalFontInstantiationResult {
+  kSuccess = 0,
+  kErrorOutsideWindowsFontsDirectory = 1,
+  kErrorOther = 2,
+  kMaxWindowsUniqueLocalFontInstantiationResult = 3
+};
+
+}  // namespace
 
 namespace blink {
 
@@ -24,10 +39,37 @@ sk_sp<SkTypeface> FontUniqueNameLookupWin::MatchUniqueName(
       font_table_matcher_->MatchName(font_unique_name.Utf8().data());
   if (!match_result)
     return nullptr;
-  // TODO(drott): Add a UMA histogram for when this fails and recording whether
-  // the font file path was outside of the Windows Fonts directory.
-  return SkTypeface::MakeFromFile(match_result->font_path.c_str(),
-                                  match_result->ttc_index);
+  // Record here when a locally uniquely matched font could not be
+  // instantiated. One reason could be that the font was outside the
+  // C:\Windows\Fonts directory and thus not accessible due to sandbox
+  // restrictions.
+  sk_sp<SkTypeface> local_typeface = SkTypeface::MakeFromFile(
+      match_result->font_path.c_str(), match_result->ttc_index);
+
+  WindowsUniqueLocalFontInstantiationResult result = kSuccess;
+
+  // There is a chance that some systems have managed to register fonts into the
+  // Windows system font collection outside the C:\Windows\Fonts directory. For
+  // sandboxing reasons, we are unable to access them here. This histogram
+  // serves to quantify how often this case occurs and whether we need and
+  // additional sandbox helper to open the file handle on the browser process
+  // side.
+  if (!local_typeface) {
+    base::FilePath windows_fonts_path(L"C:\\WINDOWS\\FONTS");
+    if (!windows_fonts_path.IsParent(
+            base::FilePath::FromUTF8Unsafe(match_result->font_path.c_str())))
+      result = kErrorOutsideWindowsFontsDirectory;
+    else
+      result = kErrorOther;
+  }
+
+  DEFINE_THREAD_SAFE_STATIC_LOCAL(
+      EnumerationHistogram, windows_unique_local_font_instantiation_histogram,
+      ("Blink.Fonts.WindowsUniqueLocalFontInstantiationResult",
+       kMaxWindowsUniqueLocalFontInstantiationResult));
+  windows_unique_local_font_instantiation_histogram.Count(result);
+
+  return local_typeface;
 }
 
 }  // namespace blink
