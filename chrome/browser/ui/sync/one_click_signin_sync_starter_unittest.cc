00723e5fcda6bb7e8d13aaf4df7ad5c8958b5ce7
Antonio Gomes <tonikitoo@igalia.com>
2019-01-10 13:44:33 +0000

    [s13n] Port one_click_signin_sync_starter_unittest.cc away from SigninManager to IdentityManager
    CL uses IdentityTestEnvironmentProfileAdaptor to create the profile
    (hiding FakeSigninManager, PO2TS, GaiaCookieManagerServer, et al),
    and uses IdentityTestEnvironment directly to replace SigninManager
    API calls.
    
    BUG=920231
    
    Change-Id: I499cb3e2fb94387c765ef088a6f23d500639c5c8
    Reviewed-on: https://chromium-review.googlesource.com/c/1404234
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621566}
    Former-commit-id: fefd13936326516602394491dca553f0a020702a

--- chrome/browser/ui/sync/one_click_signin_sync_starter_unittest.cc
+++ chrome/browser/ui/sync/one_click_signin_sync_starter_unittest.cc
@@ -8,29 +8,16 @@
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
 #include "base/macros.h"
-#include "chrome/browser/signin/account_tracker_service_factory.h"
-#include "chrome/browser/signin/chrome_signin_client_factory.h"
-#include "chrome/browser/signin/gaia_cookie_manager_service_factory.h"
-#include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
+#include "chrome/browser/signin/identity_test_environment_profile_adaptor.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/browser_sync/browser_sync_switches.h"
-#include "components/signin/core/browser/account_tracker_service.h"
-#include "components/signin/core/browser/fake_signin_manager.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/test_browser_thread_bundle.h"
+#include "services/identity/public/cpp/identity_test_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
-
-namespace {
-
-const char kTestingGaiaId[] = "gaia_id";
-const char kTestingRefreshToken[] = "refresh_token";
-const char kTestingUsername[] = "fake_username";
-
-}  // namespace
 
 class OneClickSigninSyncStarterTest : public ChromeRenderViewHostTestHarness {
  public:
@@ -45,12 +32,10 @@ class OneClickSigninSyncStarterTest : public ChromeRenderViewHostTestHarness {
     base::CommandLine::ForCurrentProcess()->AppendSwitch(
         switches::kDisableSync);
 
-    SigninManagerBase* signin_manager =
-        SigninManagerFactory::GetForProfile(profile());
-
-    signin_manager->Initialize(nullptr);
-    signin_manager->SetAuthenticatedAccountInfo(kTestingGaiaId,
-                                                kTestingUsername);
+    IdentityTestEnvironmentProfileAdaptor identity_test_env_profile_adaptor(
+        profile());
+    account_info_ = identity_test_env_profile_adaptor.identity_test_env()
+                        ->MakePrimaryAccountAvailable("test@email.com");
   }
 
   void Callback(OneClickSigninSyncStarter::SyncSetupResult result) {
@@ -63,19 +48,21 @@ class OneClickSigninSyncStarterTest : public ChromeRenderViewHostTestHarness {
   // ChromeRenderViewHostTestHarness:
   content::BrowserContext* CreateBrowserContext() override {
     // Create the sign in manager required by OneClickSigninSyncStarter.
-    TestingProfile::Builder builder;
-    builder.AddTestingFactory(
-        SigninManagerFactory::GetInstance(),
-        base::BindRepeating(
-            &OneClickSigninSyncStarterTest::BuildSigninManager));
-    return builder.Build().release();
+    return IdentityTestEnvironmentProfileAdaptor::
+        CreateProfileForIdentityTestEnvironment()
+            .release();
   }
 
  protected:
   void CreateSyncStarter(OneClickSigninSyncStarter::Callback callback) {
+    std::string refresh_token =
+        "refresh_token_for_" +
+        account_info_.account_id;  // This matches the refresh token string set
+                                   // in identity::SetRefreshTokenForAccount.
     sync_starter_ = new OneClickSigninSyncStarter(
-        profile(), nullptr, kTestingGaiaId, kTestingUsername, std::string(),
-        kTestingRefreshToken, signin_metrics::AccessPoint::ACCESS_POINT_UNKNOWN,
+        profile(), nullptr, account_info_.gaia, account_info_.email,
+        std::string(), refresh_token,
+        signin_metrics::AccessPoint::ACCESS_POINT_UNKNOWN,
         signin_metrics::Reason::REASON_UNKNOWN_REASON,
         OneClickSigninSyncStarter::CURRENT_PROFILE,
         OneClickSigninSyncStarter::NO_CONFIRMATION, callback);
@@ -90,17 +77,10 @@ class OneClickSigninSyncStarterTest : public ChromeRenderViewHostTestHarness {
   // Number of times that the callback is called with SYNC_SETUP_SUCCESS.
   int succeeded_count_;
 
- private:
-  static std::unique_ptr<KeyedService> BuildSigninManager(
-      content::BrowserContext* context) {
-    Profile* profile = static_cast<Profile*>(context);
-    return std::make_unique<FakeSigninManager>(
-        ChromeSigninClientFactory::GetForProfile(profile),
-        ProfileOAuth2TokenServiceFactory::GetForProfile(profile),
-        AccountTrackerServiceFactory::GetForProfile(profile),
-        GaiaCookieManagerServiceFactory::GetForProfile(profile));
-  }
+  // Holds information for the account currently logged in.
+  AccountInfo account_info_;
 
+ private:
   DISALLOW_COPY_AND_ASSIGN(OneClickSigninSyncStarterTest);
 };
 
