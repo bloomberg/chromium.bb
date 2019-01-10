125d5425d6d8d032725aca188dd502d6731e528c
Sean Kau <skau@chromium.org>
2019-01-10 23:49:45 +0000

    Unbreak mDNS on older kernels
    setsockopt was failing on some older kernels where the desired
    option is not available.  Check errno for the unsupported error instead
    of the return value which is always -1 if there is an error.
    
    Bug: 917534
    
    Change-Id: Ie62cccdeab9ae2017ab1b5bf121825f97c1ccb2c
    Reviewed-on: https://chromium-review.googlesource.com/c/1403090
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621804}
    Former-commit-id: 2e49c94c24c1d93fe54b9549c9605b45e41cb8d7

--- net/dns/mdns_client_unittest.cc
+++ net/dns/mdns_client_unittest.cc
@@ -15,6 +15,7 @@
 #include "base/time/clock.h"
 #include "base/time/default_clock.h"
 #include "base/timer/mock_timer.h"
+#include "net/base/address_family.h"
 #include "net/base/completion_repeating_callback.h"
 #include "net/base/ip_address.h"
 #include "net/base/rand_callback.h"
@@ -22,6 +23,7 @@
 #include "net/dns/mdns_client_impl.h"
 #include "net/dns/mock_mdns_socket_factory.h"
 #include "net/dns/record_rdata.h"
+#include "net/log/net_log.h"
 #include "net/socket/udp_client_socket.h"
 #include "net/test/test_with_scoped_task_environment.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -1259,6 +1261,15 @@ TEST_F(MDnsConnectionSendTest, SendQueued) {
               SendToInternal(sample_packet_, "[ff02::fb]:5353", _))
       .WillOnce(Return(OK));
   callback.Run(OK);
+}
+
+TEST(MDnsSocketTest, CreateSocket) {
+  // Verifies that socket creation hasn't been broken.
+  NetLog net_log;
+  auto socket =
+      CreateAndBindMDnsSocket(AddressFamily::ADDRESS_FAMILY_IPV4, 1, &net_log);
+  EXPECT_TRUE(socket);
+  socket->Close();
 }
 
 }  // namespace net
