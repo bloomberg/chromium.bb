32a5282b429c7fa4a2b360d30f4331e4665a06c1
Ryan Hamilton <rch@chromium.org>
2019-01-10 00:22:09 +0000

    Fix InitGlobals() in fuzzer_test_support.cc to initialize test timeouts.
    Change-Id: Ie21849aa46dd12830a0cdc3036c210711c97456d
    Reviewed-on: https://chromium-review.googlesource.com/c/1403932
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621390}
    Former-commit-id: 64f9fa5153f9e73ca45193e54a01fa077e59131f

--- net/base/fuzzer_test_support.cc
+++ net/base/fuzzer_test_support.cc
@@ -7,6 +7,7 @@
 #include "base/i18n/icu_util.h"
 #include "base/logging.h"
 #include "base/test/scoped_task_environment.h"
+#include "base/test/test_timeouts.h"
 
 namespace {
 
@@ -15,10 +16,15 @@ namespace {
 // Note that in general static initializers are not allowed, however this is
 // just being used by test code.
 struct InitGlobals {
-  InitGlobals()
-      : scoped_task_environment(
-            base::test::ScopedTaskEnvironment::MainThreadType::IO) {
+  InitGlobals() {
     base::CommandLine::Init(0, nullptr);
+
+    // |test| instances uses ScopedTaskEnvironment, which needs TestTimeouts.
+    TestTimeouts::Initialize();
+
+    scoped_task_environment =
+        std::make_unique<base::test::ScopedTaskEnvironment>(
+            base::test::ScopedTaskEnvironment::MainThreadType::IO);
 
     // Set up ICU. ICU is used internally by GURL, which is used throughout the
     // //net code. Initializing ICU is important to prevent fuzztests from
@@ -33,7 +39,7 @@ struct InitGlobals {
   // A number of tests use async code which depends on there being a
   // ScopedTaskEnvironment.  Setting one up here allows tests to reuse the
   // ScopedTaskEnvironment between runs.
-  base::test::ScopedTaskEnvironment scoped_task_environment;
+  std::unique_ptr<base::test::ScopedTaskEnvironment> scoped_task_environment;
 
   base::AtExitManager at_exit_manager;
 };
