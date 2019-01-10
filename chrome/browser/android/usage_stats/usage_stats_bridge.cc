6095bb1a9ad409e584b7b51ab05a9d9fb7cbdb02
Natalie Chouinard <chouinard@chromium.org>
2019-01-10 21:55:22 +0000

    [Chromeshine] Define Usage Stats database interface. Initialize LevelDB instances.
    Bug: 919913
    Change-Id: I72c9401aa81b5cdce9e3ce1ff25d0a8e7973440b
    Reviewed-on: https://chromium-review.googlesource.com/c/1401516
    Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621761}
    Former-commit-id: b293c02a73a0e0a9b8df436dc0f7142050e83ea2

--- chrome/browser/android/usage_stats/usage_stats_bridge.cc
+++ chrome/browser/android/usage_stats/usage_stats_bridge.cc
@@ -13,19 +13,13 @@
 
 namespace usage_stats {
 
-using base::android::JavaParamRef;
-using base::android::JavaRef;
-
-const char kUsageStatsFolder[] = "usage_stats";
-
 static jlong JNI_UsageStatsBridge_Init(JNIEnv* env,
                                        const JavaParamRef<jobject>& j_this,
                                        const JavaParamRef<jobject>& j_profile) {
   Profile* profile = ProfileAndroid::FromProfileAndroid(j_profile);
-  base::FilePath usage_stats_dir(profile->GetPath().Append(kUsageStatsFolder));
 
   std::unique_ptr<UsageStatsDatabase> usage_stats_database =
-      std::make_unique<UsageStatsDatabase>(usage_stats_dir);
+      std::make_unique<UsageStatsDatabase>(profile);
 
   UsageStatsBridge* native_usage_stats_bridge =
       new UsageStatsBridge(std::move(usage_stats_database));
@@ -44,62 +38,54 @@ void UsageStatsBridge::Destroy(JNIEnv* env, const JavaRef<jobject>& j_this) {
   delete this;
 }
 
-void UsageStatsBridge::GetAllEvents(
-    JNIEnv* j_env,
-    const base::android::JavaRef<jobject>& j_this,
-    const base::android::JavaRef<jobject>& j_callback) {}
+void UsageStatsBridge::GetAllEvents(JNIEnv* j_env,
+                                    const JavaRef<jobject>& j_this,
+                                    const JavaRef<jobject>& j_callback) {}
 
-void UsageStatsBridge::QueryEventsInRange(
-    JNIEnv* j_env,
-    const base::android::JavaRef<jobject>& j_this,
-    const jlong j_start,
-    const jlong j_end,
-    const base::android::JavaRef<jobject>& j_callback) {}
+void UsageStatsBridge::QueryEventsInRange(JNIEnv* j_env,
+                                          const JavaRef<jobject>& j_this,
+                                          const jlong j_start,
+                                          const jlong j_end,
+                                          const JavaRef<jobject>& j_callback) {}
 
-void UsageStatsBridge::AddEvents(
-    JNIEnv* j_env,
-    const base::android::JavaRef<jobject>& j_this,
-    const base::android::JavaRef<jobjectArray>& j_events,
-    const base::android::JavaRef<jobject>& j_callback) {}
+void UsageStatsBridge::AddEvents(JNIEnv* j_env,
+                                 const JavaRef<jobject>& j_this,
+                                 const JavaRef<jobjectArray>& j_events,
+                                 const JavaRef<jobject>& j_callback) {}
 
-void UsageStatsBridge::DeleteAllEvents(
-    JNIEnv* j_env,
-    const base::android::JavaRef<jobject>& j_this,
-    const base::android::JavaRef<jobject>& j_callback) {}
+void UsageStatsBridge::DeleteAllEvents(JNIEnv* j_env,
+                                       const JavaRef<jobject>& j_this,
+                                       const JavaRef<jobject>& j_callback) {}
 
-void UsageStatsBridge::DeleteEventsInRange(
-    JNIEnv* j_env,
-    const base::android::JavaRef<jobject>& j_this,
-    const jlong j_start,
-    const jlong j_end,
-    const base::android::JavaRef<jobject>& j_callback) {}
+void UsageStatsBridge::DeleteEventsInRange(JNIEnv* j_env,
+                                           const JavaRef<jobject>& j_this,
+                                           const jlong j_start,
+                                           const jlong j_end,
+                                           const JavaRef<jobject>& j_callback) {
+}
 
 void UsageStatsBridge::DeleteEventsWithMatchingDomains(
     JNIEnv* j_env,
-    const base::android::JavaRef<jobject>& j_this,
-    const base::android::JavaRef<jobjectArray>& j_domains,
-    const base::android::JavaRef<jobject>& j_callback) {}
+    const JavaRef<jobject>& j_this,
+    const JavaRef<jobjectArray>& j_domains,
+    const JavaRef<jobject>& j_callback) {}
 
-void UsageStatsBridge::GetAllSuspensions(
-    JNIEnv* j_env,
-    const base::android::JavaRef<jobject>& j_this,
-    const base::android::JavaRef<jobject>& j_callback) {}
+void UsageStatsBridge::GetAllSuspensions(JNIEnv* j_env,
+                                         const JavaRef<jobject>& j_this,
+                                         const JavaRef<jobject>& j_callback) {}
 
-void UsageStatsBridge::SetSuspensions(
-    JNIEnv* j_env,
-    const base::android::JavaRef<jobject>& j_this,
-    const base::android::JavaRef<jobjectArray>& j_domains,
-    const base::android::JavaRef<jobject>& j_callback) {}
+void UsageStatsBridge::SetSuspensions(JNIEnv* j_env,
+                                      const JavaRef<jobject>& j_this,
+                                      const JavaRef<jobjectArray>& j_domains,
+                                      const JavaRef<jobject>& j_callback) {}
 
-void UsageStatsBridge::GetAllTokenMappings(
-    JNIEnv* j_env,
-    const base::android::JavaRef<jobject>& j_this,
-    const base::android::JavaRef<jobject>& j_callback) {}
+void UsageStatsBridge::GetAllTokenMappings(JNIEnv* j_env,
+                                           const JavaRef<jobject>& j_this,
+                                           const JavaRef<jobject>& j_callback) {
+}
 
-void UsageStatsBridge::SetTokenMappings(
-    JNIEnv* j_env,
-    const base::android::JavaRef<jobject>& j_this,
-    const base::android::JavaRef<jobject>& j_mappings,
-    const base::android::JavaRef<jobject>& j_callback) {}
-
+void UsageStatsBridge::SetTokenMappings(JNIEnv* j_env,
+                                        const JavaRef<jobject>& j_this,
+                                        const JavaRef<jobject>& j_mappings,
+                                        const JavaRef<jobject>& j_callback) {}
 }  // namespace usage_stats
