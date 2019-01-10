dddb00e86b31021ffcf84041672e570747ccb01f
Armando Miraglia <armax@chromium.org>
2019-01-10 14:20:53 +0000

    [CLEANUP] Remove cyclic dependency from header files.
    In preparation to add support for OverconstrainedErrorEvent, we need to
    remove a cyclic dependency between media_stream_constraints_util.h and
    media_stream_constraints_util_sets.h. To do so, I have moved the few
    trivial functions present in media_stream_constraints_util.h that were
    required by media_stream_constraints_util_sets.h, and cleaned up the
    forward declarations.
    
    BUG=854988
    
    Change-Id: Iddfd138c2a865af78704509affcabcbac759eb6a
    Reviewed-on: https://chromium-review.googlesource.com/c/1404086
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Armando Miraglia <armax@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621573}
    Former-commit-id: f23979ab348a6dec88ffadcbaf9e05dc07d1ed93

--- content/renderer/media/stream/media_stream_constraints_util.h
+++ content/renderer/media/stream/media_stream_constraints_util.h
@@ -10,6 +10,7 @@
 #include "base/logging.h"
 #include "content/common/content_export.h"
 #include "content/renderer/media/stream/media_stream_audio_processor_options.h"
+#include "content/renderer/media/stream/media_stream_constraints_util_sets.h"
 #include "content/renderer/media/stream/video_track_adapter.h"
 #include "media/base/video_facing.h"
 #include "media/capture/video_capture_types.h"
@@ -18,12 +19,6 @@
 #include "third_party/blink/public/platform/web_media_stream_source.h"
 
 namespace content {
-
-namespace media_constraints {
-class ResolutionSet;
-template <typename T>
-class NumericRangeSet;
-}  // namespace media_constraints
 
 extern const double kMinDeviceCaptureFrameRate;
 
@@ -289,30 +284,6 @@ CONTENT_EXPORT bool GetConstraintValueAsString(
     const blink::WebMediaConstraints& constraints,
     const blink::StringConstraint blink::WebMediaTrackConstraintSet::*picker,
     std::string* value);
-
-template <typename ConstraintType>
-bool ConstraintHasMax(const ConstraintType& constraint) {
-  return constraint.HasMax() || constraint.HasExact();
-}
-
-template <typename ConstraintType>
-bool ConstraintHasMin(const ConstraintType& constraint) {
-  return constraint.HasMin() || constraint.HasExact();
-}
-
-template <typename ConstraintType>
-auto ConstraintMax(const ConstraintType& constraint)
-    -> decltype(constraint.Max()) {
-  DCHECK(ConstraintHasMax(constraint));
-  return constraint.HasExact() ? constraint.Exact() : constraint.Max();
-}
-
-template <typename ConstraintType>
-auto ConstraintMin(const ConstraintType& constraint)
-    -> decltype(constraint.Min()) {
-  DCHECK(ConstraintHasMin(constraint));
-  return constraint.HasExact() ? constraint.Exact() : constraint.Min();
-}
 
 // If |value| is outside the range of |constraint|, returns the name of the
 // failed constraint. Otherwise, returns nullptr. The return value converts to
