36248fcd635b13c751d11e6722072bed2d57cb8a
Takuto Ikuta <tikuta@chromium.org>
2019-01-11 03:02:32 +0000

    [ninjalog] don't upload ninja log of no-op build
    Check timestamp of .ninja_log before upload and don't upload if mtime is very old.
    
    Bug: 900161
    Change-Id: I0cf00ae29e23a9f609564e4c12b4b518320ca23a
    Reviewed-on: https://chromium-review.googlesource.com/c/1404611
    Commit-Queue: Shinya Kawanaka <shinyak@chromium.org>
    Reviewed-by: Shinya Kawanaka <shinyak@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>

--- ninjalog_uploader.py
+++ ninjalog_uploader.py
@@ -26,6 +26,7 @@ import platform
 import socket
 import subprocess
 import sys
+import time
 
 from third_party import httplib2
 
@@ -191,6 +192,12 @@ def main():
     if not os.path.isfile(ninjalog):
         logging.warn("ninjalog is not found in %s", ninjalog)
         return 1
+
+    # We assume that each ninja invocation interval takes at least 2 seconds.
+    # This is not to have duplicate entry in server when current build is no-op.
+    if os.stat(ninjalog).st_mtime < time.time() - 2:
+        logging.info("ninjalog is not updated recently %s", ninjalog)
+        return 0
 
     output = cStringIO.StringIO()
 
