82f881a0e0670a9a4a4d0d921c805734a40bf17a
Alex Clarke <alexclarke@chromium.org>
2019-01-11 09:29:35 +0000

    Revert "Add a top level traits bag comment and improve unittest"
    This reverts commit fc4e09c0f9c9b60db63426d0a7e074639c382f62 [formerly 8e92e732734fb13b970dde6603ae6a178b18a38c].
    
    Reason for revert: Forgot to upload final patchset :(
    
    Original change's description:
    > Add a top level traits bag comment and improve unittest
    > 
    > Change-Id: Ica9aaefb29261df3e741f12880c12e4ebfed948d
    > Reviewed-on: https://chromium-review.googlesource.com/c/1403130
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#621521}
    
    TBR=dcheng@chromium.org,gab@chromium.org,alexclarke@chromium.org
    
    Change-Id: I39cc89dff8417fd7c7985ec668f239e9f0ff3be0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/1406245
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621953}
    Former-commit-id: deeb3b44a255eee1970b564c6a128522d6a23358

--- base/traits_bag_unittest.cc
+++ base/traits_bag_unittest.cc
@@ -27,7 +27,7 @@ struct TestTraits {
   template <class... ArgTypes,
             class CheckArgumentsAreValid = std::enable_if_t<
                 trait_helpers::AreValidTraits<ValidTrait, ArgTypes...>::value>>
-  constexpr TestTraits(ArgTypes... args)
+  TestTraits(ArgTypes... args)
       : has_example_trait(trait_helpers::HasTrait<ExampleTrait>(args...)),
         enum_trait_a(
             trait_helpers::GetEnum<EnumTraitA, EnumTraitA::A>(args...)),
@@ -40,7 +40,7 @@ struct TestTraits {
 };
 
 struct RequiredEnumTestTraits {
-  // List of traits that are required inputs for the constructor below.
+  // List of traits that are valid inputs for the constructor below.
   struct ValidTrait {
     ValidTrait(EnumTraitA);
   };
@@ -49,37 +49,36 @@ struct RequiredEnumTestTraits {
   template <class... ArgTypes,
             class CheckArgumentsAreValid = std::enable_if_t<
                 trait_helpers::AreValidTraits<ValidTrait, ArgTypes...>::value>>
-  constexpr RequiredEnumTestTraits(ArgTypes... args)
+  RequiredEnumTestTraits(ArgTypes... args)
       : enum_trait_a(trait_helpers::GetEnum<EnumTraitA>(args...)) {}
 
   const EnumTraitA enum_trait_a;
 };
-
 }  // namespace
 
 TEST(TraitsBagTest, DefaultConstructor) {
-  constexpr TestTraits trait_test_class;
+  TestTraits trait_test_class;
 
   EXPECT_FALSE(trait_test_class.has_example_trait);
 }
 
 TEST(TraitsBagTest, HasTrait) {
-  constexpr TestTraits with_trait(ExampleTrait{});
-  constexpr TestTraits without_trait;
+  TestTraits with_trait(ExampleTrait{});
+  TestTraits without_trait;
 
   EXPECT_TRUE(with_trait.has_example_trait);
   EXPECT_FALSE(without_trait.has_example_trait);
 }
 
 TEST(TraitsBagTest, GetEnumWithDefault) {
-  constexpr TestTraits defaults;
+  TestTraits defaults;
 
   EXPECT_EQ(defaults.enum_trait_a, EnumTraitA::A);
   EXPECT_EQ(defaults.enum_trait_b, EnumTraitB::ONE);
 
-  constexpr TestTraits a(EnumTraitA::A);
-  constexpr TestTraits b(EnumTraitA::B);
-  constexpr TestTraits c(EnumTraitA::C);
+  TestTraits a(EnumTraitA::A);
+  TestTraits b(EnumTraitA::B);
+  TestTraits c(EnumTraitA::C);
 
   EXPECT_EQ(a.enum_trait_a, EnumTraitA::A);
   EXPECT_EQ(a.enum_trait_b, EnumTraitB::ONE);
@@ -90,9 +89,9 @@ TEST(TraitsBagTest, GetEnumWithDefault) {
   EXPECT_EQ(c.enum_trait_a, EnumTraitA::C);
   EXPECT_EQ(c.enum_trait_b, EnumTraitB::ONE);
 
-  constexpr TestTraits a_one(EnumTraitA::A, EnumTraitB::ONE);
-  constexpr TestTraits b_one(EnumTraitA::B, EnumTraitB::ONE);
-  constexpr TestTraits c_one(EnumTraitA::C, EnumTraitB::ONE);
+  TestTraits a_one(EnumTraitA::A, EnumTraitB::ONE);
+  TestTraits b_one(EnumTraitA::B, EnumTraitB::ONE);
+  TestTraits c_one(EnumTraitA::C, EnumTraitB::ONE);
 
   EXPECT_EQ(a_one.enum_trait_a, EnumTraitA::A);
   EXPECT_EQ(a_one.enum_trait_b, EnumTraitB::ONE);
@@ -103,9 +102,9 @@ TEST(TraitsBagTest, GetEnumWithDefault) {
   EXPECT_EQ(c_one.enum_trait_a, EnumTraitA::C);
   EXPECT_EQ(c_one.enum_trait_b, EnumTraitB::ONE);
 
-  constexpr TestTraits a_two(EnumTraitA::A, EnumTraitB::TWO);
-  constexpr TestTraits b_two(EnumTraitA::B, EnumTraitB::TWO);
-  constexpr TestTraits c_two(EnumTraitA::C, EnumTraitB::TWO);
+  TestTraits a_two(EnumTraitA::A, EnumTraitB::TWO);
+  TestTraits b_two(EnumTraitA::B, EnumTraitB::TWO);
+  TestTraits c_two(EnumTraitA::C, EnumTraitB::TWO);
 
   EXPECT_EQ(a_two.enum_trait_a, EnumTraitA::A);
   EXPECT_EQ(a_two.enum_trait_b, EnumTraitB::TWO);
@@ -118,9 +117,9 @@ TEST(TraitsBagTest, GetEnumWithDefault) {
 }
 
 TEST(TraitsBagTest, RequiredEnum) {
-  constexpr RequiredEnumTestTraits a(EnumTraitA::A);
-  constexpr RequiredEnumTestTraits b(EnumTraitA::B);
-  constexpr RequiredEnumTestTraits c(EnumTraitA::C);
+  RequiredEnumTestTraits a(EnumTraitA::A);
+  RequiredEnumTestTraits b(EnumTraitA::B);
+  RequiredEnumTestTraits c(EnumTraitA::C);
 
   EXPECT_EQ(a.enum_trait_a, EnumTraitA::A);
   EXPECT_EQ(b.enum_trait_a, EnumTraitA::B);
