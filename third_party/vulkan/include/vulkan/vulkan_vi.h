2fbc12687e2ab28f1cc4bef07b95cab59b790280
Sergey Ulanov <sergeyu@chromium.org>
2019-01-11 18:24:05 +0000

    Update vulkan headers to 1.1.97
    The new version adds vulkan_fuchsia.h
    
    Bug: 861853
    Change-Id: Iaad12a3ce67fbdb5908abc0ea5aebce3e8d40b55
    Reviewed-on: https://chromium-review.googlesource.com/c/1406308
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622063}
    Former-commit-id: 2b4a80d34ee5bacbb342b419cab238a566f04338

--- third_party/vulkan/include/vulkan/vulkan_vi.h
+++ third_party/vulkan/include/vulkan/vulkan_vi.h
@@ -1,5 +1,5 @@
-#ifndef VULKAN_MACOS_H_
-#define VULKAN_MACOS_H_ 1
+#ifndef VULKAN_VI_H_
+#define VULKAN_VI_H_ 1
 
 #ifdef __cplusplus
 extern "C" {
@@ -27,26 +27,26 @@ extern "C" {
 */
 
 
-#define VK_MVK_macos_surface 1
-#define VK_MVK_MACOS_SURFACE_SPEC_VERSION 2
-#define VK_MVK_MACOS_SURFACE_EXTENSION_NAME "VK_MVK_macos_surface"
+#define VK_NN_vi_surface 1
+#define VK_NN_VI_SURFACE_SPEC_VERSION     1
+#define VK_NN_VI_SURFACE_EXTENSION_NAME   "VK_NN_vi_surface"
 
-typedef VkFlags VkMacOSSurfaceCreateFlagsMVK;
+typedef VkFlags VkViSurfaceCreateFlagsNN;
 
-typedef struct VkMacOSSurfaceCreateInfoMVK {
-    VkStructureType                 sType;
-    const void*                     pNext;
-    VkMacOSSurfaceCreateFlagsMVK    flags;
-    const void*                     pView;
-} VkMacOSSurfaceCreateInfoMVK;
+typedef struct VkViSurfaceCreateInfoNN {
+    VkStructureType             sType;
+    const void*                 pNext;
+    VkViSurfaceCreateFlagsNN    flags;
+    void*                       window;
+} VkViSurfaceCreateInfoNN;
 
 
-typedef VkResult (VKAPI_PTR *PFN_vkCreateMacOSSurfaceMVK)(VkInstance instance, const VkMacOSSurfaceCreateInfoMVK* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkSurfaceKHR* pSurface);
+typedef VkResult (VKAPI_PTR *PFN_vkCreateViSurfaceNN)(VkInstance instance, const VkViSurfaceCreateInfoNN* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkSurfaceKHR* pSurface);
 
 #ifndef VK_NO_PROTOTYPES
-VKAPI_ATTR VkResult VKAPI_CALL vkCreateMacOSSurfaceMVK(
+VKAPI_ATTR VkResult VKAPI_CALL vkCreateViSurfaceNN(
     VkInstance                                  instance,
-    const VkMacOSSurfaceCreateInfoMVK*          pCreateInfo,
+    const VkViSurfaceCreateInfoNN*              pCreateInfo,
     const VkAllocationCallbacks*                pAllocator,
     VkSurfaceKHR*                               pSurface);
 #endif
