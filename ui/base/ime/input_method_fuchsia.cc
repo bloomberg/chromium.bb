dc4db127999963b72349a4be8f96a968026fcdb3
Kevin Marshall <kmarshall@chromium.org>
2019-01-11 02:05:31 +0000

    [fuchsia] Fix Scenic focus & input handling.
    Implements a number of fixes to text editing across multiple Scenic
    windows.
    
    It wires up focus & blur events to the WindowTreeHost and InputMethod,
    so that the WTH's text editing modality can be started and stopped
    as appropriate.
    
    It also sets up and tears down IME service connections in response
    to focus and blur events, because Fuchsia temporarily requires
    applications to manage the lifetime of their input focus.
    
    
    Bug: 919242
    Change-Id: I7ed96f312bc65d179f2f8565a4475660b1547a5f
    Reviewed-on: https://chromium-review.googlesource.com/c/1403674
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621864}
    Former-commit-id: 8b9ddf8b8035507c18ef7b11e4af82c4047385a0

--- ui/base/ime/input_method_fuchsia.cc
+++ ui/base/ime/input_method_fuchsia.cc
@@ -10,22 +10,26 @@
 
 #include "base/bind_helpers.h"
 #include "base/fuchsia/component_context.h"
-#include "ui/base/ime/fuchsia/input_method_keyboard_controller_fuchsia.h"
 #include "ui/base/ime/text_input_client.h"
+#include "ui/events/base_event_utils.h"
+#include "ui/events/keycodes/dom/dom_code.h"
+#include "ui/events/keycodes/dom/keycode_converter.h"
 
 namespace ui {
 
 InputMethodFuchsia::InputMethodFuchsia(internal::InputMethodDelegate* delegate)
-    : InputMethodBase(delegate) {
-  virtual_keyboard_controller_ =
-      std::make_unique<InputMethodKeyboardControllerFuchsia>(this);
-}
+    : InputMethodBase(delegate),
+      event_converter_(this),
+      ime_client_binding_(this),
+      ime_service_(base::fuchsia::ComponentContext::GetDefault()
+                       ->ConnectToService<fuchsia::ui::input::ImeService>()),
+      virtual_keyboard_controller_(ime_service_.get()) {}
 
 InputMethodFuchsia::~InputMethodFuchsia() {}
 
 InputMethodKeyboardController*
 InputMethodFuchsia::GetInputMethodKeyboardController() {
-  return virtual_keyboard_controller_.get();
+  return &virtual_keyboard_controller_;
 }
 
 void InputMethodFuchsia::DispatchEvent(ui::Event* event) {
@@ -61,6 +65,48 @@ void InputMethodFuchsia::CancelComposition(const TextInputClient* client) {}
 
 bool InputMethodFuchsia::IsCandidatePopupOpen() const {
   return false;
+}
+
+void InputMethodFuchsia::OnFocus() {
+  DCHECK(!ime_);
+
+  // TODO(crbug.com/876934): Instantiate the IME with details about the text
+  // being edited.
+  fuchsia::ui::input::TextInputState state = {};
+  state.text = "";
+  ime_service_->GetInputMethodEditor(
+      fuchsia::ui::input::KeyboardType::TEXT,
+      fuchsia::ui::input::InputMethodAction::UNSPECIFIED, std::move(state),
+      ime_client_binding_.NewBinding(), ime_.NewRequest());
+}
+
+void InputMethodFuchsia::OnBlur() {
+  virtual_keyboard_controller_.DismissVirtualKeyboard();
+  ime_client_binding_.Unbind();
+  ime_.Unbind();
+}
+
+void InputMethodFuchsia::DidUpdateState(
+    fuchsia::ui::input::TextInputState state,
+    std::unique_ptr<fuchsia::ui::input::InputEvent> input_event) {
+  if (input_event->is_keyboard())
+    event_converter_.ProcessEvent(*input_event);
+  else
+    NOTIMPLEMENTED();
+}
+
+void InputMethodFuchsia::OnAction(
+    fuchsia::ui::input::InputMethodAction action) {
+  if (action != fuchsia::ui::input::InputMethodAction::UNSPECIFIED) {
+    NOTIMPLEMENTED();
+    return;
+  }
+
+  // Synthesize an ENTER keypress and send it to the Window.
+  KeyEvent key_event(ET_KEY_PRESSED, KeyboardCode::VKEY_RETURN,
+                     ui::DomCode::ENTER, ui::EF_NONE, ui::DomKey::ENTER,
+                     ui::EventTimeForNow());
+  DispatchKeyEvent(&key_event);
 }
 
 }  // namespace ui
