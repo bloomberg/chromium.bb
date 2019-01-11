677261d025200f9208613f53c9a4b668160ca80e
Roman Sorokin <rsorokin@chromium.org>
2019-01-11 12:33:56 +0000

    [Sheriff] Disable autofill tests due to flakiness
    Bug: 919988, 920901
    Change-Id: I50831b7a94c1b83027241f34b748048d2fabbc65
    Reviewed-on: https://chromium-review.googlesource.com/c/1406718
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Commit-Queue: Roman Sorokin <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621978}
    Former-commit-id: b11313eb88f81c456677cb93bdee85fb322ede32

--- chrome/android/javatests/src/org/chromium/chrome/browser/autofill/keyboard_accessory/PasswordAccessoryIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/autofill/keyboard_accessory/PasswordAccessoryIntegrationTest.java
@@ -37,6 +37,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeFeatureList;
@@ -149,6 +150,7 @@ public class PasswordAccessoryIntegrationTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/920901")
     @EnableFeatures({ChromeFeatureList.PASSWORDS_KEYBOARD_ACCESSORY})
     public void testPasswordSheetDisplaysNoPasswordsMessageAndOptions()
             throws InterruptedException, TimeoutException {
