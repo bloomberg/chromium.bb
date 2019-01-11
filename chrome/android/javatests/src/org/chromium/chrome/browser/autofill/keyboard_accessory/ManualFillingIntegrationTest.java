d52c66b55f0f3d8d72d2d6cc23db196aa66cd8bf
Roman Sorokin <rsorokin@chromium.org>
2019-01-11 12:33:56 +0000

    [Sheriff] Disable autofill tests due to flakiness
    Bug: 919988, 920901
    Change-Id: I50831b7a94c1b83027241f34b748048d2fabbc65
    Reviewed-on: https://chromium-review.googlesource.com/c/1406718
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Commit-Queue: Roman Sorokin <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621978}
    Former-commit-id: b11313eb88f81c456677cb93bdee85fb322ede32

--- chrome/android/javatests/src/org/chromium/chrome/browser/autofill/keyboard_accessory/ManualFillingIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/autofill/keyboard_accessory/ManualFillingIntegrationTest.java
@@ -34,6 +34,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeFeatureList;
@@ -255,6 +256,7 @@ public class ManualFillingIntegrationTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "https://crbug.com/919988")
     @Restriction(UiRestriction.RESTRICTION_TYPE_PHONE)
     public void testInvokingTabSwitcherHidesAccessory()
             throws InterruptedException, TimeoutException {
