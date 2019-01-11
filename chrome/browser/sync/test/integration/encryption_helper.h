ad46b39bdd0d9038f8a6b651eef836c74c54abc3
Marc Treib <treib@chromium.org>
2019-01-11 10:15:33 +0000

    Fix data races in SingleClientCustomPassphraseSyncTest
    Currently these tests are all very flaky on TSan because of a data race
    related to the global FeatureList instance. This CL changes the tests
    to avoid the problematic pattern (overriding the FeatureList from the
    test body).
    For one test (CannotDecryptScryptKeyEncryptedDataWhenScryptDisabled),
    it's not easy to avoid the bad pattern, so this test is disabled on
    TSan.
    
    Bug: 915219
    Change-Id: Ic04db73c6a40b426f52fbf4a170d661c2a1ebe93
    Reviewed-on: https://chromium-review.googlesource.com/c/1404084
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621960}
    Former-commit-id: 0f5c69b57657008f67a50bf7fc6a08feaf8f4415

--- chrome/browser/sync/test/integration/encryption_helper.h
+++ chrome/browser/sync/test/integration/encryption_helper.h
@@ -77,6 +77,10 @@ class PassphraseRequiredStateChecker : public SingleClientStatusChangeChecker {
   bool desired_state_;
 };
 
+// Helper for setting scrypt-related feature flags.
+// NOTE: DO NOT INSTANTIATE THIS CLASS IN THE TEST BODY FOR INTEGRATION TESTS!
+// That causes data races, see crbug.com/915219. Instead, instantiate it in the
+// test fixture class.
 class ScopedScryptFeatureToggler {
  public:
   ScopedScryptFeatureToggler(bool force_disabled, bool use_for_new_passphrases);
