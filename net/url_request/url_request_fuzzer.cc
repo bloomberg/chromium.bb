d39098a756caaeaf26dfd17ef44396d31f1f6aa1
Max Moroz <mmoroz@chromium.org>
2019-01-12 02:23:07 +0000

    [net] Re-enable net_url_request_fuzzer and enforce max input size.
    Bug: 820089
    Change-Id: Ie550f62a262d0b15fef4db4e0247a37b9a0f2a59
    Reviewed-on: https://chromium-review.googlesource.com/c/1405989
    Commit-Queue: Eric Roman <eroman@chromium.org>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622270}
    Former-commit-id: 9da1efcabd19d19023840bd1a6ca3271144b5ad9

--- net/url_request/url_request_fuzzer.cc
+++ net/url_request/url_request_fuzzer.cc
@@ -19,6 +19,11 @@
 #include "net/url_request/url_request_test_util.h"
 #include "url/gurl.h"
 
+
+// Restrict max input length to reject too long inputs that can be too slow to
+// process and may lead to an unbounded corpus growth.
+const size_t kMaxInputSize = 65536 + 257;
+
 // Integration fuzzer for URLRequest's handling of HTTP requests. Can follow
 // redirects, both on the same server (using a new socket or the old one) and
 // across servers.
@@ -27,6 +32,9 @@
 // QUIC, DNS failures (they all currently resolve to localhost), IPv6 DNS
 // results, URLs with IPs instead of hostnames (v4 and v6), etc.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  if (size > kMaxInputSize)
+    return 0;
+
   base::FuzzedDataProvider data_provider(data, size);
   net::TestURLRequestContext url_request_context(true);
   net::FuzzedSocketFactory fuzzed_socket_factory(&data_provider);
