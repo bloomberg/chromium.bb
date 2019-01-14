d87095cb183e4801fe9b333149ee394eb3077c3a
Daniel Erat <derat@chromium.org>
2019-01-14 23:36:13 +0000

    chromeos_config: Increase Tast VM timeout to an hour.
    Increase TastVMTestConfig's default timeout from 30 minutes
    to an hour. As more informational tests are added,
    betty-release is bumping up against the current timeout, and
    this is more in line other VM stages' defaults (90 minutes
    for VMTestConfig, 60 minutes for GCETestConfig).
    
    BUG=chromium:921641
    TEST=none
    
    Change-Id: I0a00c30498d3c18122fb6515c57fcea08b3188e6
    Reviewed-on: https://chromium-review.googlesource.com/1409625
    Commit-Ready: Dan Erat <derat@chromium.org>
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Ilja H. Friedel <ihf@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

--- lib/config_lib.py
+++ lib/config_lib.py
@@ -421,7 +421,7 @@ class TastVMTestConfig(object):
     timeout: Number of seconds to wait before timing out waiting for
              results.
   """
-  DEFAULT_TEST_TIMEOUT = 30 * 60
+  DEFAULT_TEST_TIMEOUT = 60 * 60
 
   def __init__(self, suite_name, test_exprs, timeout=DEFAULT_TEST_TIMEOUT):
     """Constructor -- see members above."""
