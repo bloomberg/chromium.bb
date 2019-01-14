a631c4bab9b6dfe1f84687ebc9d8739140da286c
Robbie McElrath <rmcelrath@chromium.org>
2019-01-14 21:23:20 +0000

    Enable the Network Service by default on ChromeOS.
    This enables the network service by default, and disables it at the
    beginning of unit tests, since they don't currently work with the
    network service enabled.
    
    This will be reverted after a few days, or earlier if it causes
    problems. We're only enabling it by default to make sure all test
    suites (in particular the hardware autotests) have a chance to run
    with it enabled, and to get bug reports from developers.
    
    Bug: 920038
    Change-Id: Iff9a2ab08b82d4446e327b685be1bca8cf61917b
    Reviewed-on: https://chromium-review.googlesource.com/c/1399297
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622606}
    Former-commit-id: 7112246b3dbeebf99659871f89c8e27fbeb7ec4b

--- content/public/test/content_test_suite_base.cc
+++ content/public/test/content_test_suite_base.cc
@@ -7,6 +7,7 @@
 #include <memory>
 
 #include "base/compiler_specific.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/test_suite.h"
 #include "build/build_config.h"
 #include "content/browser/gpu/gpu_main_thread_factory.h"
@@ -18,6 +19,7 @@
 #include "content/renderer/in_process_renderer_thread.h"
 #include "content/utility/in_process_utility_thread.h"
 #include "net/base/network_change_notifier.h"
+#include "services/network/public/cpp/features.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/material_design/material_design_controller.h"
 #include "ui/base/ui_base_paths.h"
@@ -74,12 +76,19 @@ ContentTestSuiteBase::ContentTestSuiteBase(int argc, char** argv)
     : base::TestSuite(argc, argv) {
 }
 
+ContentTestSuiteBase::~ContentTestSuiteBase() = default;
+
 void ContentTestSuiteBase::Initialize() {
   base::TestSuite::Initialize();
 
   testing::TestEventListeners& listeners =
       testing::UnitTest::GetInstance()->listeners();
   listeners.Append(new NetworkChangeNotifierDisabler());
+
+  // Disable the network service since a lot of unit tests don't work with it.
+  scoped_feature_list_ = std::make_unique<base::test::ScopedFeatureList>();
+  scoped_feature_list_->InitAndDisableFeature(
+      network::features::kNetworkService);
 
 #if defined(V8_USE_EXTERNAL_STARTUP_DATA)
   gin::V8Initializer::LoadV8Snapshot(kSnapshotType);
