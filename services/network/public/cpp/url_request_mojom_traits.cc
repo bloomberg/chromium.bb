014b6cc628916472ee932891d1872afe0d02967d
Leon Han <leon.han@intel.com>
2019-01-14 02:48:40 +0000

    [OnionSoup] Create mojom traits for typemapping network.mojom.URLRequest
    This CL makes network.mojom.URLRequest not a [Native] mojom struct:
      - define all its fields well.
      - create the mojom struct traits for keeping its typemap to
        network::ResourceRequest.
      - the legacy IPC struct traits of network::ResourceRequest becomes
        useless, remove it.
    
    This makes network.mojom.URLRequest no more than a mirror of
    network::ResourceRequest, by defining each field as almost the same type
    with the mirror filed in network::ResourceRequest, this is not the final
    definition but a start point for refining, which may happen in the
    future when we create the typemap to blink::ResourceRequest for Blink
    variant.
    
    The future plan would be:
     - make network.mojom.URLRequestBody not a [Native] one, keeping its
       typemap to network::ResourceRequestBody.
     - for Blink variant create a typemap to blink::EncodedFormData for
       network.mojom.URLRequestBody.
     - for Blink variant create a typemap to blink::ResourceRequest for
       network.mojom.URLRequest.
    
    BUG=860403
    
    Change-Id: I4b962cd42d093d3f03070a2eeb197dabfcbd9d01
    Reviewed-on: https://chromium-review.googlesource.com/c/1381735
    Commit-Queue: Leon Han <leon.han@intel.com>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622375}
    Former-commit-id: 9278acaca70fd85c7000969f5ecfec33a4badc17

--- services/network/public/cpp/url_request_mojom_traits.cc
+++ services/network/public/cpp/url_request_mojom_traits.cc
@@ -5,6 +5,11 @@
 #include "services/network/public/cpp/url_request_mojom_traits.h"
 
 #include "base/logging.h"
+#include "mojo/public/cpp/base/unguessable_token_mojom_traits.h"
+#include "services/network/public/cpp/http_request_headers_mojom_traits.h"
+#include "services/network/public/cpp/network_ipc_param_traits.h"
+#include "url/mojom/origin_mojom_traits.h"
+#include "url/mojom/url_gurl_mojom_traits.h"
 
 namespace mojo {
 
@@ -54,6 +59,151 @@ bool EnumTraits<network::mojom::RequestPriority, net::RequestPriority>::
 
   NOTREACHED();
   *out = static_cast<net::RequestPriority>(in);
+  return true;
+}
+
+network::mojom::URLRequestReferrerPolicy EnumTraits<
+    network::mojom::URLRequestReferrerPolicy,
+    net::URLRequest::ReferrerPolicy>::ToMojom(net::URLRequest::ReferrerPolicy
+                                                  policy) {
+  switch (policy) {
+    case net::URLRequest::ReferrerPolicy::
+        CLEAR_REFERRER_ON_TRANSITION_FROM_SECURE_TO_INSECURE:
+      return network::mojom::URLRequestReferrerPolicy::
+          kClearReferrerOnTransitionFromSecureToInsecure;
+    case net::URLRequest::ReferrerPolicy::
+        REDUCE_REFERRER_GRANULARITY_ON_TRANSITION_CROSS_ORIGIN:
+      return network::mojom::URLRequestReferrerPolicy::
+          kReduceReferrerGranularityOnTransitionCrossOrigin;
+    case net::URLRequest::ReferrerPolicy::
+        ORIGIN_ONLY_ON_TRANSITION_CROSS_ORIGIN:
+      return network::mojom::URLRequestReferrerPolicy::
+          kOriginOnlyOnTransitionCrossOrigin;
+    case net::URLRequest::ReferrerPolicy::NEVER_CLEAR_REFERRER:
+      return network::mojom::URLRequestReferrerPolicy::kNeverClearReferrer;
+    case net::URLRequest::ReferrerPolicy::ORIGIN:
+      return network::mojom::URLRequestReferrerPolicy::kOrigin;
+    case net::URLRequest::ReferrerPolicy::
+        CLEAR_REFERRER_ON_TRANSITION_CROSS_ORIGIN:
+      return network::mojom::URLRequestReferrerPolicy::
+          kClearReferrerOnTransitionCrossOrigin;
+    case net::URLRequest::ReferrerPolicy::
+        ORIGIN_CLEAR_ON_TRANSITION_FROM_SECURE_TO_INSECURE:
+      return network::mojom::URLRequestReferrerPolicy::
+          kOriginClearOnTransitionFromSecureToInsecure;
+    case net::URLRequest::ReferrerPolicy::NO_REFERRER:
+      return network::mojom::URLRequestReferrerPolicy::kNoReferrer;
+  }
+  NOTREACHED();
+  return static_cast<network::mojom::URLRequestReferrerPolicy>(policy);
+}
+
+bool EnumTraits<network::mojom::URLRequestReferrerPolicy,
+                net::URLRequest::ReferrerPolicy>::
+    FromMojom(network::mojom::URLRequestReferrerPolicy in,
+              net::URLRequest::ReferrerPolicy* out) {
+  switch (in) {
+    case network::mojom::URLRequestReferrerPolicy::
+        kClearReferrerOnTransitionFromSecureToInsecure:
+      *out = net::URLRequest::ReferrerPolicy::
+          CLEAR_REFERRER_ON_TRANSITION_FROM_SECURE_TO_INSECURE;
+      return true;
+    case network::mojom::URLRequestReferrerPolicy::
+        kReduceReferrerGranularityOnTransitionCrossOrigin:
+      *out = net::URLRequest::ReferrerPolicy::
+          REDUCE_REFERRER_GRANULARITY_ON_TRANSITION_CROSS_ORIGIN;
+      return true;
+    case network::mojom::URLRequestReferrerPolicy::
+        kOriginOnlyOnTransitionCrossOrigin:
+      *out = net::URLRequest::ReferrerPolicy::
+          ORIGIN_ONLY_ON_TRANSITION_CROSS_ORIGIN;
+      return true;
+    case network::mojom::URLRequestReferrerPolicy::kNeverClearReferrer:
+      *out = net::URLRequest::ReferrerPolicy::NEVER_CLEAR_REFERRER;
+      return true;
+    case network::mojom::URLRequestReferrerPolicy::kOrigin:
+      *out = net::URLRequest::ReferrerPolicy::ORIGIN;
+      return true;
+    case network::mojom::URLRequestReferrerPolicy::
+        kClearReferrerOnTransitionCrossOrigin:
+      *out = net::URLRequest::ReferrerPolicy::
+          CLEAR_REFERRER_ON_TRANSITION_CROSS_ORIGIN;
+      return true;
+    case network::mojom::URLRequestReferrerPolicy::
+        kOriginClearOnTransitionFromSecureToInsecure:
+      *out = net::URLRequest::ReferrerPolicy::
+          ORIGIN_CLEAR_ON_TRANSITION_FROM_SECURE_TO_INSECURE;
+      return true;
+    case network::mojom::URLRequestReferrerPolicy::kNoReferrer:
+      *out = net::URLRequest::ReferrerPolicy::NO_REFERRER;
+      return true;
+  }
+
+  NOTREACHED();
+  return false;
+}
+
+bool StructTraits<
+    network::mojom::URLRequestDataView,
+    network::ResourceRequest>::Read(network::mojom::URLRequestDataView data,
+                                    network::ResourceRequest* out) {
+  if (!data.ReadMethod(&out->method) || !data.ReadUrl(&out->url) ||
+      !data.ReadSiteForCookies(&out->site_for_cookies) ||
+      !data.ReadTopFrameOrigin(&out->top_frame_origin) ||
+      !data.ReadRequestInitiator(&out->request_initiator) ||
+      !data.ReadReferrer(&out->referrer) ||
+      !data.ReadReferrerPolicy(&out->referrer_policy) ||
+      !data.ReadHeaders(&out->headers) ||
+      !data.ReadRequestedWithHeader(&out->requested_with_header) ||
+      !data.ReadClientDataHeader(&out->client_data_header) ||
+      !data.ReadPriority(&out->priority) ||
+      !data.ReadCorsPreflightPolicy(&out->cors_preflight_policy) ||
+      !data.ReadFetchRequestMode(&out->fetch_request_mode) ||
+      !data.ReadFetchCredentialsMode(&out->fetch_credentials_mode) ||
+      !data.ReadFetchRedirectMode(&out->fetch_redirect_mode) ||
+      !data.ReadFetchIntegrity(&out->fetch_integrity) ||
+      !data.ReadFetchFrameType(&out->fetch_frame_type) ||
+      !data.ReadRequestBody(&out->request_body) ||
+      !data.ReadThrottlingProfileId(&out->throttling_profile_id) ||
+      !data.ReadCustomProxyPreCacheHeaders(
+          &out->custom_proxy_pre_cache_headers) ||
+      !data.ReadCustomProxyPostCacheHeaders(
+          &out->custom_proxy_post_cache_headers) ||
+      !data.ReadFetchWindowId(&out->fetch_window_id)) {
+    return false;
+  }
+
+  out->attach_same_site_cookies = data.attach_same_site_cookies();
+  out->update_first_party_url_on_redirect =
+      data.update_first_party_url_on_redirect();
+  out->is_prerendering = data.is_prerendering();
+  out->load_flags = data.load_flags();
+  out->allow_credentials = data.allow_credentials();
+  out->plugin_child_id = data.plugin_child_id();
+  out->resource_type = data.resource_type();
+  out->appcache_host_id = data.appcache_host_id();
+  out->should_reset_appcache = data.should_reset_appcache();
+  out->is_external_request = data.is_external_request();
+  out->service_worker_provider_id = data.service_worker_provider_id();
+  out->originated_from_service_worker = data.originated_from_service_worker();
+  out->skip_service_worker = data.skip_service_worker();
+  out->fetch_request_context_type = data.fetch_request_context_type();
+  out->keepalive = data.keepalive();
+  out->has_user_gesture = data.has_user_gesture();
+  out->enable_load_timing = data.enable_load_timing();
+  out->enable_upload_progress = data.enable_upload_progress();
+  out->do_not_prompt_for_login = data.do_not_prompt_for_login();
+  out->render_frame_id = data.render_frame_id();
+  out->is_main_frame = data.is_main_frame();
+  out->transition_type = data.transition_type();
+  out->allow_download = data.allow_download();
+  out->report_raw_headers = data.report_raw_headers();
+  out->previews_state = data.previews_state();
+  out->initiated_in_secure_context = data.initiated_in_secure_context();
+  out->upgrade_if_insecure = data.upgrade_if_insecure();
+  out->is_revalidating = data.is_revalidating();
+  out->custom_proxy_use_alternate_proxy_list =
+      data.custom_proxy_use_alternate_proxy_list();
   return true;
 }
 
