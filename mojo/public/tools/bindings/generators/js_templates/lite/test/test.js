1e23ec5b2e36703d024f47050664af52603d782f
Christopher Lam <calamity@chromium.org>
2019-01-14 03:37:24 +0000

    [Mojo Lite] Fix Map for stringable kinds and add Closure Compiler test.
    This CL makes stringable key types always return Objects, rather than a
    type union with Map, and adds better type information to response
    Promises.
    
    It also adds a simple Closure Compiler target to ensure that generated
    code can be accessed in certain ways.
    
    Bug: 849993
    Change-Id: I6002a577474e45b6b42185988968df8fa4ac6700
    Reviewed-on: https://chromium-review.googlesource.com/c/1395870
    Commit-Queue: calamity <calamity@chromium.org>
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#622378}
    Former-commit-id: 7661080854b5b01adbfbb06c02d74499beb09bfc

---
+++ mojo/public/tools/bindings/generators/js_templates/lite/test/test.js
@@ -0,0 +1,42 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+async function testFunction() {
+  /** @type {test.mojom.TestPageHandlerProxy} */
+  let proxy = test.mojom.TestPageHandler.getProxy()
+
+  /**
+   * @type {{values: !Array<!string>}}
+   * This type is not enforced, as Closure does not handle Promise return
+   * typing.
+   */
+  let result = await proxy.method1();
+  /**
+   * @type {Array<string>}
+   * This type is enforced because of the above cast.
+   */
+  let values = result.values;
+}
+
+/** @implements {test.mojom.TestPageInterface} */
+class TestPageImpl {
+  /** @override */
+  onEvent1(s) {
+    /** @type {test.mojom.TestStruct} */ let t = s;
+    /** @type {string} */ let id = t.id;
+    /** @type {string|undefined} */ let title = t.title;
+    /** @type {test.mojom.TestEnum} */ let enumValue = t.enums[0];
+
+    /** @type {string} */ let numberToStringMapValue = t.numberToStringMap[5];
+
+    /** @type {test.mojom.Message} */ let messageToMessageArrayValue =
+        t.messageToArrayMap.get({message: 'asdf'})[0];
+
+    /** @type {test.mojom.TestEnum} */ let enumToMapMapValue =
+        t.enumToMapMap[test.mojom.TestEnum.FIRST][test.mojom.TestEnum.SECOND];
+    /** @type {test.mojom.TestPageInterface} */ let handler =
+        t.numberToInterfaceProxyMap[3];
+    handler.onEvent1(t);
+  }
+}
