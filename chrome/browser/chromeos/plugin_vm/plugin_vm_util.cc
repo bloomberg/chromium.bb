ac59ea36d5b727c7c006942e7ea45d0fe4853ae4
Alex Oldemeier <aoldemeier@chromium.org>
2019-01-15 11:53:26 +0000

    Implement plugin_vm_util::IsPluginVmAllowedForProfile
    This adds the logic to the stub that has been introduced
    before. We check that PluginVm is allowed by device policy,
    that the current profile is affiliated, and that a VM image
    and a license key is set.
    
    Bug: 921634, b/122714766
    Test: unit_tests -gtest_filter="PluginVmUtilTest.IsPluginVmAllowedForProfileReturnsTrueOnceAllConditionsAreMet"
    Change-Id: I7f3d1784e5d54456ea36ced9bf0079c89667d51e
    Reviewed-on: https://chromium-review.googlesource.com/c/1380014
    Commit-Queue: Alex Oldemeier <aoldemeier@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622818}
    Former-commit-id: 7565e29f60c6103ba1e6289634c50fde55b1f4a4

--- chrome/browser/chromeos/plugin_vm/plugin_vm_util.cc
+++ chrome/browser/chromeos/plugin_vm/plugin_vm_util.cc
@@ -4,15 +4,54 @@
 
 #include "chrome/browser/chromeos/plugin_vm/plugin_vm_util.h"
 
+#include <string>
+
+#include "chrome/browser/chromeos/plugin_vm/plugin_vm_pref_names.h"
+#include "chrome/browser/chromeos/profiles/profile_helper.h"
+#include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chrome/browser/profiles/profile.h"
+#include "components/prefs/pref_service.h"
 
 namespace plugin_vm {
 
 bool IsPluginVmAllowedForProfile(Profile* profile) {
-  // TODO (okalitova): check that the right policies are in place.
-  // Right now, this always returns false so that no icon for
-  // PluginVm will ever be visible.
-  return false;
+  // Check that the profile is eligible.
+  if (!profile || profile->IsChild() || profile->IsLegacySupervised() ||
+      profile->IsOffTheRecord() ||
+      chromeos::ProfileHelper::IsEphemeralUserProfile(profile) ||
+      chromeos::ProfileHelper::IsLockScreenAppProfile(profile)) {
+    return false;
+  }
+
+  // Check that the user is affiliated.
+  const user_manager::User* const user =
+      chromeos::ProfileHelper::Get()->GetUserByProfile(profile);
+  if (user == nullptr || !user->IsAffiliated())
+    return false;
+
+  // Check that PluginVm is allowed to run by policy.
+  bool plugin_vm_allowed_for_device;
+  if (!chromeos::CrosSettings::Get()->GetBoolean(
+          chromeos::kPluginVmAllowed, &plugin_vm_allowed_for_device)) {
+    return false;
+  }
+  if (!plugin_vm_allowed_for_device)
+    return false;
+
+  // Check that a license key is set.
+  std::string plugin_vm_license_key;
+  if (!chromeos::CrosSettings::Get()->GetString(chromeos::kPluginVmLicenseKey,
+                                                &plugin_vm_license_key)) {
+    return false;
+  }
+  if (plugin_vm_license_key == std::string())
+    return false;
+
+  // Check that a VM image is set.
+  if (!profile->GetPrefs()->HasPrefPath(plugin_vm::prefs::kPluginVmImage))
+    return false;
+
+  return true;
 }
 
 }  // namespace plugin_vm
