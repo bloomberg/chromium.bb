f4ea709575a200aa3119cf117efce4fd1be61125
Xiaohan Wang <xhwang@chromium.org>
2019-01-15 02:24:33 +0000

    media: Misc cleanup in Decrypting{Audio,Video}Decoder
    No functionality change.
    
    Change-Id: I4c28ac1c1944cbb2cd8e274c578ff45ec6e715b8
    Reviewed-on: https://chromium-review.googlesource.com/c/1409848
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622684}
    Former-commit-id: 661e5229fbc3f71714d70505396e0e04b28142e0

--- media/filters/decrypting_video_decoder.cc
+++ media/filters/decrypting_video_decoder.cc
@@ -24,13 +24,7 @@ const char DecryptingVideoDecoder::kDecoderName[] = "DecryptingVideoDecoder";
 DecryptingVideoDecoder::DecryptingVideoDecoder(
     const scoped_refptr<base::SingleThreadTaskRunner>& task_runner,
     MediaLog* media_log)
-    : task_runner_(task_runner),
-      media_log_(media_log),
-      state_(kUninitialized),
-      decryptor_(NULL),
-      key_added_while_decode_pending_(false),
-      support_clear_content_(false),
-      weak_factory_(this) {}
+    : task_runner_(task_runner), media_log_(media_log), weak_factory_(this) {}
 
 std::string DecryptingVideoDecoder::GetDisplayName() const {
   return kDecoderName;
