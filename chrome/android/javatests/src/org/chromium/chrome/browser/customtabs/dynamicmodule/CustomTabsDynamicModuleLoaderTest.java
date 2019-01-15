5d483f770601fd485efd5867fe07057409a7efe7
Michael van Ouwerkerk <mvanouwerkerk@google.com>
2019-01-15 15:22:14 +0000

    Register cct module lifecycle state in a synthetic field trial.
    In the UMA dashboard we can use this to slice metrics like PrivateMemoryFootprint.
    
    This is a reland of https://chromium-review.googlesource.com/c/chromium/src/+/1405189
    
    It has the following fixes between patch sets 1 and 2:
    * Ensure the browser process is initialized in the test so we don't get a segfault due to g_browser_process being null.
    * Run the module loading code on the UI thread.
    
    Bug: 882370
    Change-Id: I080eb2ab1c9e3f2a4c5c6abc86caa49691b097f2
    Reviewed-on: https://chromium-review.googlesource.com/c/1409508
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Anna Malova <amalova@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622862}
    Former-commit-id: d82f404065061deeee12e9ab3750f4178eda7c35

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/dynamicmodule/CustomTabsDynamicModuleLoaderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/dynamicmodule/CustomTabsDynamicModuleLoaderTest.java
@@ -7,23 +7,19 @@ package org.chromium.chrome.browser.customtabs.dynamicmodule;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import static org.chromium.chrome.browser.customtabs.dynamicmodule
-        .CustomTabsDynamicModuleTestUtils.FAKE_MODULE_COMPONENT_NAME;
+import static org.chromium.base.ThreadUtils.runOnUiThreadBlocking;
+import static org.chromium.chrome.browser.customtabs.dynamicmodule.CustomTabsDynamicModuleTestUtils.FAKE_MODULE_COMPONENT_NAME;
 
 import android.support.test.filters.SmallTest;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.Callback;
-import org.chromium.base.library_loader.LibraryLoader;
-import org.chromium.base.library_loader.LibraryProcessType;
 import org.chromium.base.test.util.CallbackHelper;
-import org.chromium.chrome.browser.ChromeFeatureList;
+import org.chromium.chrome.browser.test.ChromeBrowserTestRule;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
-import org.chromium.chrome.test.util.browser.Features;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,13 +30,8 @@ import java.util.concurrent.TimeoutException;
  */
 @RunWith(ChromeJUnit4ClassRunner.class)
 public class CustomTabsDynamicModuleLoaderTest {
-    @Before
-    public void setUp() throws Exception {
-        LibraryLoader.getInstance().ensureInitialized(LibraryProcessType.PROCESS_BROWSER);
-    }
-
     @Rule
-    public Features.JUnitProcessor mProcessor = new Features.JUnitProcessor();
+    public final ChromeBrowserTestRule mBrowserTestRule = new ChromeBrowserTestRule();
 
     /**
      * Test fake dynamic module is correctly loaded.
@@ -48,15 +39,17 @@ public class CustomTabsDynamicModuleLoaderTest {
     @Test
     @SmallTest
     public void testModuleLoading() throws TimeoutException, InterruptedException {
-        ModuleLoader moduleLoader = new ModuleLoader(FAKE_MODULE_COMPONENT_NAME);
-        moduleLoader.loadModule();
-
         CallbackHelper onLoaded = new CallbackHelper();
-        moduleLoader.addCallbackAndIncrementUseCount(
-                result -> {
-                    assertNotNull(result);
-                    onLoaded.notifyCalled();
-                });
+
+        runOnUiThreadBlocking(() -> {
+            ModuleLoader moduleLoader = new ModuleLoader(FAKE_MODULE_COMPONENT_NAME);
+            moduleLoader.loadModule();
+
+            moduleLoader.addCallbackAndIncrementUseCount(result -> {
+                assertNotNull(result);
+                onLoaded.notifyCalled();
+            });
+        });
 
         onLoaded.waitForCallback();
     }
@@ -66,45 +59,48 @@ public class CustomTabsDynamicModuleLoaderTest {
      */
     @Test
     @SmallTest
-    @Features.EnableFeatures({ChromeFeatureList.CCT_MODULE, ChromeFeatureList.CCT_MODULE_CACHE})
     public void testModuleUseCounter() throws TimeoutException, InterruptedException {
         final int callbacksNumber = 3;
         ModuleLoader moduleLoader = new ModuleLoader(FAKE_MODULE_COMPONENT_NAME);
-
         CallbackHelper onLoaded = new CallbackHelper();
-
-        // Test we correctly unregister callbacks which were never notified.
         List<Callback<ModuleEntryPoint>> unusedCallbacks = new ArrayList<>();
-        for (int i = 0; i < callbacksNumber; i++) {
-            unusedCallbacks.add(result -> {});
-            moduleLoader.addCallbackAndIncrementUseCount(unusedCallbacks.get(i));
-        }
-        // module has not been loaded, therefore there is no usage
-        assertEquals(0, moduleLoader.getModuleUseCount());
-
-        // unregister all callbacks so they should not increment module usage
-        for (int i = 0; i < callbacksNumber; i++) {
-            moduleLoader.removeCallbackAndDecrementUseCount(unusedCallbacks.get(i));
-        }
-
-        assertEquals(0, moduleLoader.getModuleUseCount());
-
-        moduleLoader.loadModule();
-
         List<Callback<ModuleEntryPoint>> callbacks = new ArrayList<>();
-        // register callbacks and wait for the notification when module is loaded
-        for (int i = 0; i < callbacksNumber; i++) {
-            callbacks.add(result -> onLoaded.notifyCalled());
-            moduleLoader.addCallbackAndIncrementUseCount(callbacks.get(i));
-        }
+
+        runOnUiThreadBlocking(() -> {
+            // Test we correctly unregister callbacks which were never notified.
+            for (int i = 0; i < callbacksNumber; i++) {
+                unusedCallbacks.add(result -> {});
+                moduleLoader.addCallbackAndIncrementUseCount(unusedCallbacks.get(i));
+            }
+            // module has not been loaded, therefore there is no usage
+            assertEquals(0, moduleLoader.getModuleUseCount());
+
+            // unregister all callbacks so they should not increment module usage
+            for (int i = 0; i < callbacksNumber; i++) {
+                moduleLoader.removeCallbackAndDecrementUseCount(unusedCallbacks.get(i));
+            }
+
+            assertEquals(0, moduleLoader.getModuleUseCount());
+
+            moduleLoader.loadModule();
+
+            // register callbacks and wait for the notification when module is loaded
+            for (int i = 0; i < callbacksNumber; i++) {
+                callbacks.add(result -> onLoaded.notifyCalled());
+                moduleLoader.addCallbackAndIncrementUseCount(callbacks.get(i));
+            }
+        });
+
         onLoaded.waitForCallback(0, callbacksNumber);
 
-        assertEquals(callbacksNumber, moduleLoader.getModuleUseCount());
+        runOnUiThreadBlocking(() -> {
+            assertEquals(callbacksNumber, moduleLoader.getModuleUseCount());
 
-        // unregister already notified callbacks
-        for (int i = 0; i < callbacksNumber; i++) {
-            moduleLoader.removeCallbackAndDecrementUseCount(callbacks.get(i));
-        }
-        assertEquals(0, moduleLoader.getModuleUseCount());
+            // unregister already notified callbacks
+            for (int i = 0; i < callbacksNumber; i++) {
+                moduleLoader.removeCallbackAndDecrementUseCount(callbacks.get(i));
+            }
+            assertEquals(0, moduleLoader.getModuleUseCount());
+        });
     }
 }
