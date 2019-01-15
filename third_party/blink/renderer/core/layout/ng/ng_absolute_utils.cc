34d9f6409b62398819c17699638fd9e37af7ccb4
Fredrik Söderquist <fs@opera.com>
2019-01-15 13:18:47 +0000

    [LayoutNG] Avoid copying Lengths
    Copying/assigning a Length is not just a trivial copy, so when possible,
    use a const Length& to avoid making a copy (saving some small bit of
    footprint and hopefully a few CPU cycles too).
    
    Change-Id: Ib802f8e26c061e77bc8c0d8487a9dc8a8afe130b
    Reviewed-on: https://chromium-review.googlesource.com/c/1411921
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#622834}
    Former-commit-id: e52a43fd6a15604f674a016fc6d24054b37409be

--- third_party/blink/renderer/core/layout/ng/ng_absolute_utils.cc
+++ third_party/blink/renderer/core/layout/ng/ng_absolute_utils.cc
@@ -19,14 +19,14 @@ namespace blink {
 namespace {
 
 bool AbsoluteHorizontalNeedsEstimate(const ComputedStyle& style) {
-  Length width = style.Width();
+  const Length& width = style.Width();
   return width.IsIntrinsic() || style.MinWidth().IsIntrinsic() ||
          style.MaxWidth().IsIntrinsic() ||
          (width.IsAuto() && (style.Left().IsAuto() || style.Right().IsAuto()));
 }
 
 bool AbsoluteVerticalNeedsEstimate(const ComputedStyle& style) {
-  Length height = style.Height();
+  const Length& height = style.Height();
   return height.IsIntrinsic() || style.MinHeight().IsIntrinsic() ||
          style.MaxHeight().IsIntrinsic() ||
          (height.IsAuto() && (style.Top().IsAuto() || style.Bottom().IsAuto()));
