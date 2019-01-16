ea5fdd16d8dad4df4eccfd3ac3961d892b66b61f
Leonardo Padilha <ldaguilar@chromium.org>
2019-01-16 21:01:03 +0000

    Add session state change as a trigger for status report.
    The status report will be sent when child unlock or lock the device.
    
    Bug: 919132
    Change-Id: I2c0c2befede83dfeb65d76f28b54bf56760b83a9
    Reviewed-on: https://chromium-review.googlesource.com/c/1412853
    Commit-Queue: Leonardo Padilha <ldaguilar@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623354}
    Former-commit-id: 79a2f64e2d4ce1c00f415740d871d5b5435b55f0

--- chrome/browser/chromeos/child_accounts/event_based_status_reporting_service.cc
+++ chrome/browser/chromeos/child_accounts/event_based_status_reporting_service.cc
@@ -8,6 +8,7 @@
 #include "chrome/browser/chromeos/child_accounts/consumer_status_reporting_service.h"
 #include "chrome/browser/chromeos/child_accounts/consumer_status_reporting_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "components/session_manager/core/session_manager.h"
 
 namespace chromeos {
 
@@ -16,9 +17,9 @@ EventBasedStatusReportingService::EventBasedStatusReportingService(
     : context_(context) {
   ArcAppListPrefs* arc_app_prefs = ArcAppListPrefs::Get(context_);
   // arc_app_prefs may not available in some browser tests.
-  if (!arc_app_prefs)
-    return;
-  arc_app_prefs->AddObserver(this);
+  if (arc_app_prefs)
+    arc_app_prefs->AddObserver(this);
+  session_manager::SessionManager::Get()->AddObserver(this);
 }
 
 EventBasedStatusReportingService::~EventBasedStatusReportingService() = default;
@@ -37,12 +38,36 @@ void EventBasedStatusReportingService::OnPackageModified(
       ->RequestImmediateStatusReport();
 }
 
+void EventBasedStatusReportingService::OnSessionStateChanged() {
+  session_manager::SessionState session_state =
+      session_manager::SessionManager::Get()->session_state();
+
+  // Do not request status report when the device has just started the session
+  // because not all components may be ready yet, which may cause an incomplete
+  // status report.
+  if (session_just_started_ &&
+      session_state == session_manager::SessionState::ACTIVE) {
+    session_just_started_ = false;
+    return;
+  }
+
+  if (session_state == session_manager::SessionState::ACTIVE) {
+    VLOG(1) << "Request status report due to a unlock screen.";
+    ConsumerStatusReportingServiceFactory::GetForBrowserContext(context_)
+        ->RequestImmediateStatusReport();
+  } else if (session_state == session_manager::SessionState::LOCKED) {
+    VLOG(1) << "Request status report due to a lock screen.";
+    ConsumerStatusReportingServiceFactory::GetForBrowserContext(context_)
+        ->RequestImmediateStatusReport();
+  }
+}
+
 void EventBasedStatusReportingService::Shutdown() {
   ArcAppListPrefs* arc_app_prefs = ArcAppListPrefs::Get(context_);
   // arc_app_prefs may not available in some browser tests.
-  if (!arc_app_prefs)
-    return;
-  arc_app_prefs->RemoveObserver(this);
+  if (arc_app_prefs)
+    arc_app_prefs->RemoveObserver(this);
+  session_manager::SessionManager::Get()->RemoveObserver(this);
 }
 
 }  // namespace chromeos
