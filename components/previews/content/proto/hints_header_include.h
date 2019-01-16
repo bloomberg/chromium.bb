f3d6523369d7d54347743b24b97739caf1b9b047
Jered Gray <jegray@google.com>
2019-01-16 23:02:39 +0000

    Implement HintCacheStore and HintCacheLevelDBStore
    An abstract HintCacheStore base class and concrete HintCacheLevelDBStore
    derived class have been added to provide a hint backing store for the
    HintCache, which will enable the number of hints sent down to the client
    in hint components to significantly expand. The HintCacheStore provides
    the cache the ability to query the availability of a hint for a specific
    host suffix and to asynchronously load individual hints on demand.
    
    The HintCacheLevelDBStore database is composed of metadata entries that
    include basic store information, such as its schema version and its
    component version, and component hint entries. Eventually, the
    HintCacheLevelDBStore will be expanded to include OP API hints as well.
    
    Unittests have been added for HintCacheLevelDBStore.
    
    Bug: 888097
    
    Change-Id: Ibfeb52ada5cdf1a4a17395611f4bd85c711ccb62
    Reviewed-on: https://chromium-review.googlesource.com/c/1403335
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Commit-Queue: Jered Gray <jegray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623425}
    Former-commit-id: dedfb57f58577ece7bbd584049590ff094f4d7d5

---
+++ components/previews/content/proto/hints_header_include.h
@@ -0,0 +1,12 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PREVIEWS_CONTENT_PROTO_HINTS_HEADER_INCLUDE_H_
+#define COMPONENTS_PREVIEWS_CONTENT_PROTO_HINTS_HEADER_INCLUDE_H_
+
+// This is a bridge to include the generated proto header for the
+// optimization_guide proto.
+#include "components/optimization_guide/proto/hints.pb.h"
+
+#endif  // COMPONENTS_PREVIEWS_CONTENT_PROTO_HINTS_HEADER_INCLUDE_H_
