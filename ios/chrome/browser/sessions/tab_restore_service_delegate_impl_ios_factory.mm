b62c561faa172c65965795610dd67a3a82906c57
Sylvain Defresne <sdefresne@chromium.org>
2019-01-16 14:32:32 +0000

    Convert from base::{LazyInstance,Singleton} to base::NoDestructor
    The class base::NoDestructor is a simpler way to define a static
    global value (aka a singleton) than base::{LazyInstance,Singleton}
    and is the recommended pattern to use.
    
    The changes were automated using a custom one-shot script, then
    fine tuned by hand (to remove unnecessary classes, ...).
    
    This fixes usages in src/ios/chrome/browser/sessions.
    
    This CL was uploaded by git cl split.
    
    Bug: none
    Change-Id: Id5a3582f930b6073b3c42eb365d2deb204e102d9
    Reviewed-on: https://chromium-review.googlesource.com/c/1412175
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623231}
    Former-commit-id: 4a599ec77ed34748b2b686b0bd270b2ec8771299

--- ios/chrome/browser/sessions/tab_restore_service_delegate_impl_ios_factory.mm
+++ ios/chrome/browser/sessions/tab_restore_service_delegate_impl_ios_factory.mm
@@ -6,7 +6,7 @@
 
 #include <memory>
 
-#include "base/memory/singleton.h"
+#include "base/no_destructor.h"
 #include "components/keyed_service/ios/browser_state_dependency_manager.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/sessions/tab_restore_service_delegate_impl_ios.h"
@@ -26,7 +26,8 @@ TabRestoreServiceDelegateImplIOSFactory::GetForBrowserState(
 // static
 TabRestoreServiceDelegateImplIOSFactory*
 TabRestoreServiceDelegateImplIOSFactory::GetInstance() {
-  return base::Singleton<TabRestoreServiceDelegateImplIOSFactory>::get();
+  static base::NoDestructor<TabRestoreServiceDelegateImplIOSFactory> instance;
+  return instance.get();
 }
 
 TabRestoreServiceDelegateImplIOSFactory::
