8df11d8607cb2a04f9ba1d8be6ae1fae91c53528
Kevin Marshall <kmarshall@chromium.org>
2019-01-17 05:38:25 +0000

    [fuchsia] Implement Cast Channel API using Connector/MessagePort.
    Add a Cast Channel FIDL service, which bridges FIDL clients and the JS
    cast.__platform__.channel API.
    
    Injects cast.__platform__.channel to the document scripting context.
    The Cast Channel can be opened, written to, and closed by JS.
    The C++ side can write messages to an opened Channel.
    
    Modify OnLoadScriptInjector to use to page commit events as a trigger
    for script injection instead of DidClearWindowObject(). Page commits
    are guaranteed to occur only once per page load, which is a more useful
    event than window object clearing, which happens once for an initial
    page load, and then twice for all subsequent navigations.
    
    Enable "castrunner_browsertests" suite for testing bots.
    
    Actual integration into the Cast Runner will take place in a followup
    CL.
    
    Bug: 910799
    Change-Id: Ie7abdb22b5ed61b62fbbfceac9e466c8d36f10f6
    Reviewed-on: https://chromium-review.googlesource.com/c/1357719
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623601}
    Former-commit-id: dfd6eedb468f73fc55e28f02dad2c59a594ab03f

---
+++ webrunner/app/cast/bindings/cast_channel.h
@@ -0,0 +1,71 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WEBRUNNER_APP_CAST_BINDINGS_CAST_CHANNEL_H_
+#define WEBRUNNER_APP_CAST_BINDINGS_CAST_CHANNEL_H_
+
+#include <string>
+
+#include "base/callback.h"
+#include "base/macros.h"
+#include "base/strings/string_piece.h"
+#include "webrunner/common/webrunner_export.h"
+#include "webrunner/fidl/chromium/cast/cpp/fidl.h"
+#include "webrunner/fidl/chromium/web/cpp/fidl.h"
+
+namespace webrunner {
+class NamedMessagePortConnector;
+}
+
+namespace castrunner {
+
+// Handles the injection of cast.__platform__.channel bindings into pages'
+// scripting context, and establishes a bidirectional message pipe over
+// which the two communicate.
+class WEBRUNNER_EXPORT CastChannelImpl : public chromium::cast::CastChannel {
+ public:
+  // Attaches CastChannel bindings and port to a |frame|.
+  // |frame|: The frame to be provided with a CastChannel.
+  // |connector|: The NamedMessagePortConnector to use for establishing
+  // transport.
+  // Both |frame| and |connector| must outlive |this|.
+  CastChannelImpl(chromium::web::Frame* frame,
+                  webrunner::NamedMessagePortConnector* connector);
+  ~CastChannelImpl() override;
+
+  // chromium::cast::CastChannel implementation.
+  void Connect(ConnectCallback callback) override;
+
+ private:
+  // Receives a port used for receiving new Cast Channel ports.
+  void OnMasterPortReceived(chromium::web::MessagePortPtr port);
+
+  // Receives a message containing a newly opened Cast Channel from
+  // |master_port_|.
+  void OnCastChannelMessageReceived(chromium::web::WebMessage message);
+
+  // Handles error conditions on |master_port_|.
+  void OnMasterPortError();
+
+  chromium::web::Frame* const frame_;
+  webrunner::NamedMessagePortConnector* const connector_;
+
+  // A long-lived port, used to receive new Cast Channel ports when they are
+  // opened. Should be automatically  populated by the
+  // NamedMessagePortConnector whenever |frame| loads a new page.
+  chromium::web::MessagePortPtr master_port_;
+
+  fuchsia::mem::Buffer bindings_script_;
+  ConnectCallback pending_connect_cb_;
+
+  // A Cast Channel received from the webpage, waiting to be handled via
+  // ListenForChannel().
+  fidl::InterfaceHandle<chromium::web::MessagePort> pending_channel_;
+
+  DISALLOW_COPY_AND_ASSIGN(CastChannelImpl);
+};
+
+}  // namespace castrunner
+
+#endif  // WEBRUNNER_APP_CAST_BINDINGS_CAST_CHANNEL_H_
