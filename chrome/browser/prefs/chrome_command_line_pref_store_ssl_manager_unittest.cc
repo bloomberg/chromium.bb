219ec45627f8210ec7e85b42c54f85b8f6f31b47
Carlos Caballero <carlscab@google.com>
2019-01-17 15:38:55 +0000

    [scheduling] Use ScopedTaskEnvironment instead of MessageLoop in prefs
    MessageLoop will go away, eventually.
    
    BUG=891670
    
    Change-Id: I4810ecbef8d5b6a9f6d6d6a1b090df25afe1cffa
    Reviewed-on: https://chromium-review.googlesource.com/c/1417590
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623694}
    Former-commit-id: d77b95d3f6f5ec29acadd58267682659c47656e7

--- chrome/browser/prefs/chrome_command_line_pref_store_ssl_manager_unittest.cc
+++ chrome/browser/prefs/chrome_command_line_pref_store_ssl_manager_unittest.cc
@@ -4,7 +4,7 @@
 
 #include "base/command_line.h"
 #include "base/memory/ref_counted.h"
-#include "base/message_loop/message_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "chrome/browser/prefs/chrome_command_line_pref_store.h"
 #include "chrome/browser/ssl/ssl_config_service_manager.h"
 #include "chrome/common/chrome_switches.h"
@@ -22,7 +22,7 @@ class CommandLinePrefStoreSSLManagerTest : public testing::Test {
   CommandLinePrefStoreSSLManagerTest() {}
 
  protected:
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 };
 
 // Test that command-line settings for SSL versions are respected and that they
