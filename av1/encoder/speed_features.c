0b678bd4cfe52ce0fc37f3c65ca7e4b7cf99ad4b
Venkat <sanampudi.venkatarao@ittiam.com>
2019-01-17 19:15:13 +0530

    Refine tx domain dist speed feature for speeds 3,4
    MSE based threshold logic is been added to use transform domain distortion for speeds =3 and 4
    
    For speed = 3 and 4 presets, BD-rate impact is seen as -0.2% and -0.17% (as per AWCY runs),
    with encode time increase of 1.19% and 1.34% (averaged across multiple test cases) respectively.
    
    STATS_CHANGED
    
    Change-Id: Ia9a67d1cacd48aa33034c82b5056f65138378401

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -54,10 +54,9 @@ static uint8_t intrabc_max_mesh_pct[MAX_MESH_SPEED + 1] = { 100, 100, 100,
 
 // Threshold values to be used for pruning the txfm_domain_distortion
 // based on block MSE
-// TODO(any): Extend the threshold logic for faster presets and refine the
-// thresholds
+// TODO(any): Experiment the threshold logic based on variance metric
 static unsigned int tx_domain_dist_thresholds[MAX_TX_DOMAIN_EVAL_SPEED + 1] = {
-  UINT_MAX, 162754, 22026, 0, 0, 0
+  UINT_MAX, 162754, 22026, 22026, 22026, 0
 };
 // Threshold values to be used for disabling coeff RD-optimization
 // based on block MSE
