a82b4ef0b788a4d9b3a5bdd9d538fac8f9aae7af
Matthew Cary <mattcary@chromium.org>
2019-01-17 13:12:44 +0000

    Vr: Disable VrBrowser tests on pre-N.
    Bug: 897259
    Change-Id: I48194d4c01a3139db0183c3b7e843418510a51b3
    
    TBR: bsheedy@chromium.org
    Change-Id: I48194d4c01a3139db0183c3b7e843418510a51b3
    Reviewed-on: https://chromium-review.googlesource.com/c/1417532
    Reviewed-by: Matthew Cary <mattcary@chromium.org>
    Commit-Queue: Matthew Cary <mattcary@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623661}
    Former-commit-id: 3bac194f6b515796389eb9e23f1818e4bb4a0715

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr/VrBrowserCompositorViewHolderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr/VrBrowserCompositorViewHolderTest.java
@@ -6,6 +6,7 @@ package org.chromium.chrome.browser.vr;
 
 import static org.chromium.chrome.test.util.ChromeRestriction.RESTRICTION_TYPE_SVR;
 
+import android.os.Build;
 import android.support.test.filters.MediumTest;
 import android.view.ViewGroup;
 
@@ -16,6 +17,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeSwitches;
@@ -34,6 +36,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 @Restriction(RESTRICTION_TYPE_SVR)
+// TODO(crbug.com/897259): consistently failing on pre-N.
+@MinAndroidSdkLevel(Build.VERSION_CODES.N)
 public class VrBrowserCompositorViewHolderTest {
     // We explicitly instantiate a rule here instead of using parameterization since this class
     // only ever runs in ChromeTabbedActivity.
