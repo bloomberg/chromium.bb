05b0bcc066adeb5fed726fa700675db840636b5c
Ian Clelland <iclelland@chromium.org>
2019-01-17 17:12:20 +0000

    Disable two flaky NetworkContextConfiguration browser tests
    TBR: mmenke@chromium.org
    Bug: 922876
    Change-Id: I7aa180a67d2d8cae122b4472df80659914912b81
    Reviewed-on: https://chromium-review.googlesource.com/c/1417756
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Auto-Submit: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623727}
    Former-commit-id: 5fe5e42823d385819fdc6f9dc57d4df8780d17c2

--- chrome/browser/net/network_context_configuration_browsertest.cc
+++ chrome/browser/net/network_context_configuration_browsertest.cc
@@ -1191,7 +1191,9 @@ IN_PROC_BROWSER_TEST_P(NetworkContextConfigurationBrowserTest, SSLConfig) {
   EXPECT_EQ(net::ERR_SSL_VERSION_OR_CIPHER_MISMATCH, simple_loader->NetError());
 }
 
-IN_PROC_BROWSER_TEST_P(NetworkContextConfigurationBrowserTest, ProxyConfig) {
+// Flaky on several platforms: https://crbug.com/922876
+IN_PROC_BROWSER_TEST_P(NetworkContextConfigurationBrowserTest,
+                       DISABLED_ProxyConfig) {
   if (IsRestartStateWithInProcessNetworkService())
     return;
   SetProxyPref(embedded_test_server()->host_port_pair());
@@ -1574,8 +1576,9 @@ class NetworkContextConfigurationProxyOnStartBrowserTest
 
 // Test that when there's a proxy configuration at startup, the initial requests
 // use that configuration.
+// Flaky on several platforms: https://crbug.com/922876
 IN_PROC_BROWSER_TEST_P(NetworkContextConfigurationProxyOnStartBrowserTest,
-                       TestInitialProxyConfig) {
+                       DISABLED_TestInitialProxyConfig) {
   if (IsRestartStateWithInProcessNetworkService())
     return;
   TestProxyConfigured(/*expect_success=*/true);
