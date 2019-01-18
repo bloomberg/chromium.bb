98e4e31edf75ce314335e398807b886add1e8c85
John Budorick <jbudorick@chromium.org>
2019-01-18 17:38:04 +0000

    Disable TabRestoreTest.RestoreTabWithSpecialURL on windows.
    Tbr: chrisha@chromium.org,sky@chromium.org,gab@chromium.org
    Bug: 825305
    Change-Id: I662b4fa1e6ae45ef9c8864db0f3ddd15ea544591
    Reviewed-on: https://chromium-review.googlesource.com/c/1421679
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624184}
    Former-commit-id: f183fb456d036882ab326c771cc48b5a6174d12b

--- chrome/browser/sessions/tab_restore_browsertest.cc
+++ chrome/browser/sessions/tab_restore_browsertest.cc
@@ -719,10 +719,9 @@ IN_PROC_BROWSER_TEST_F(TabRestoreTest, RestoreWindow) {
   EXPECT_EQ(url1_, restored_tab->GetURL());
 }
 
-// https://crbug.com/835305: Timeout flakiness on Win7 Tests (dbg)(1) bot and
+// https://crbug.com/825305: Timeout flakiness on Win7 Tests (dbg)(1) bot and
 // PASS/FAIL flakiness on Linux Chromium OS ASan LSan Tests (1) bot.
-#if defined(ADDRESS_SANITIZER) || defined(MEMORY_SANITIZER) || \
-    (defined(OS_WIN) && !defined(NDEBUG))
+#if defined(ADDRESS_SANITIZER) || defined(MEMORY_SANITIZER) || defined(OS_WIN)
 #define MAYBE_RestoreTabWithSpecialURL DISABLED_RestoreTabWithSpecialURL
 #else
 #define MAYBE_RestoreTabWithSpecialURL RestoreTabWithSpecialURL
@@ -753,7 +752,7 @@ IN_PROC_BROWSER_TEST_F(TabRestoreTest, MAYBE_RestoreTabWithSpecialURL) {
 }
 
 // https://crbug.com/667932: Flakiness on linux_chromium_asan_rel_ng bot.
-// https://crbug.com/835305: Timeout flakiness on Win7 Tests (dbg)(1) bot.
+// https://crbug.com/825305: Timeout flakiness on Win7 Tests (dbg)(1) bot.
 #if defined(ADDRESS_SANITIZER) || defined(MEMORY_SANITIZER) || \
     (defined(OS_WIN) && !defined(NDEBUG))
 #define MAYBE_RestoreTabWithSpecialURLOnBack DISABLED_RestoreTabWithSpecialURLOnBack
