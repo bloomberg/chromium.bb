0414b529da27b4d065a4ee57491eec47a25104f5
John Chen <johnchen@chromium.org>
2019-01-18 17:50:24 +0000

    [ChromeDriver] Verify request host and origin headers
    Updating ChromeDriver to do some validation of host header and origin
    header of incoming requests, using an algorithm similar to the one
    used in DevTools (https://crrev.com/541547). Also add warning for
    users to protect ChromeDriver ports.
    
    TBR=johnchen@chromium.org
    
    (cherry picked from commit e55734b342dec74c160c5662796cf6e7b481c00f [formerly 2b9fe7b2097dc20e9abe723b9788b412ca43a1c5])
    
    Bug: 918232
    Change-Id: I8b38fd35573b0f1f5b342e665761176efbde36bd
    Reviewed-on: https://chromium-review.googlesource.com/c/1407383
    Commit-Queue: John Chen <johnchen@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#623807}
    Reviewed-on: https://chromium-review.googlesource.com/c/1422357
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3578@{#940}
    Cr-Branched-From: abbd18012032883c3113b3bfc5d59d25b4836c5f [formerly 4226ddf99103e493d7afb23a4c7902ee496108b6]-refs/heads/master@{#599034}
    Former-commit-id: 276b87bfbdf033b9ac9db4cdb7482a16be515541

--- chrome/test/chromedriver/server/chromedriver_server.cc
+++ chrome/test/chromedriver/server/chromedriver_server.cc
@@ -41,6 +41,7 @@
 #include "net/base/ip_address.h"
 #include "net/base/ip_endpoint.h"
 #include "net/base/net_errors.h"
+#include "net/base/url_util.h"
 #include "net/log/net_log_source.h"
 #include "net/server/http_server.h"
 #include "net/server/http_server_request_info.h"
@@ -73,15 +74,39 @@ int ListenOnIPv6(net::ServerSocket* socket, uint16_t port, bool allow_remote) {
   return socket->ListenWithAddressAndPort(binding_ip, port, 1);
 }
 
+bool RequestIsSafeToServe(const net::HttpServerRequestInfo& info) {
+  // To guard against browser-originating cross-site requests, when host header
+  // and/or origin header are present, serve only those coming from localhost.
+  std::string host_header = info.headers["host"];
+  if (!host_header.empty()) {
+    GURL url = GURL("http://" + host_header);
+    if (!net::IsLocalhost(url)) {
+      LOG(ERROR) << "Rejecting request with host: " << host_header;
+      return false;
+    }
+  }
+  std::string origin_header = info.headers["origin"];
+  if (!origin_header.empty()) {
+    GURL url = GURL(origin_header);
+    if (!net::IsLocalhost(url)) {
+      LOG(ERROR) << "Rejecting request with origin: " << origin_header;
+      return false;
+    }
+  }
+  return true;
+}
+
 class HttpServer : public net::HttpServer::Delegate {
  public:
   explicit HttpServer(const HttpRequestHandlerFunc& handle_request_func)
       : handle_request_func_(handle_request_func),
+        allow_remote_(false),
         weak_factory_(this) {}
 
   ~HttpServer() override {}
 
   int Start(uint16_t port, bool allow_remote, bool use_ipv4) {
+    allow_remote_ = allow_remote;
     std::unique_ptr<net::ServerSocket> server_socket(
         new net::TCPServerSocket(NULL, net::NetLogSource()));
     int status = use_ipv4
@@ -104,6 +129,13 @@ class HttpServer : public net::HttpServer::Delegate {
   }
   void OnHttpRequest(int connection_id,
                      const net::HttpServerRequestInfo& info) override {
+    if (!allow_remote_ && !RequestIsSafeToServe(info)) {
+      server_->Send500(
+          connection_id,
+          "Host header or origin header is specified and is not localhost.",
+          TRAFFIC_ANNOTATION_FOR_TESTS);
+      return;
+    }
     handle_request_func_.Run(
         info,
         base::Bind(&HttpServer::OnResponse,
@@ -131,6 +163,7 @@ class HttpServer : public net::HttpServer::Delegate {
 
   HttpRequestHandlerFunc handle_request_func_;
   std::unique_ptr<net::HttpServer> server_;
+  bool allow_remote_;
   base::WeakPtrFactory<HttpServer> weak_factory_;  // Should be last.
 };
 
@@ -454,6 +487,7 @@ int main(int argc, char *argv[]) {
     } else {
       printf("All remote connections are allowed. Use a whitelist instead!\n");
     }
+    printf("%s\n", kPortProtectionMessage);
     fflush(stdout);
   }
 
