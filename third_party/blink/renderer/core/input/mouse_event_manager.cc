5f9b0b7a6beea8914334c309f028e175ec17a586
Navid Zolghadr <nzolghadr@chromium.org>
2019-01-18 22:14:24 +0000

    Send click event to the nearest common ancestor
    Send click event even when the targets of down and up
    are different and across different interactive elements.
    
    This is another try to land this change and hopefully the
    previous regressions are now fixed.
    
    Bug: 869919
    Change-Id: I6161f734cb1c0513b11d82c635ea9af5c3eeee3a
    Reviewed-on: https://chromium-review.googlesource.com/c/1413232
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624315}
    Former-commit-id: 6cf0c4de8143be150844d9eb7f840fc652dd76c5

--- third_party/blink/renderer/core/input/mouse_event_manager.cc
+++ third_party/blink/renderer/core/input/mouse_event_manager.cc
@@ -926,15 +926,15 @@ bool MouseEventManager::HandleDrag(const MouseEventWithHitTestResults& event,
     return true;
   }
 
-  // Once we're past the drag threshold, we don't want to treat this gesture as
-  // a click.
-  InvalidateClick();
-
   if (!TryStartDrag(event)) {
     // Something failed to start the drag, clean up.
     ClearDragDataTransfer();
     ResetDragSource();
   } else {
+    // Once we're past the drag threshold, we don't want to treat this gesture
+    // as a click.
+    InvalidateClick();
+
     // Since drag operation started we need to send a pointercancel for the
     // corresponding pointer.
     if (initiator == DragInitiator::kMouse) {
