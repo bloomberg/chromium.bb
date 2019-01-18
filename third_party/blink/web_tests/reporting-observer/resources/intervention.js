42bcaeb7e192cd79db30c91cdaee5783eaa82843
Paul Meyer <paulmeyer@chromium.org>
2019-01-18 02:41:10 +0000

    Update ReportingObserver buffer to buffer per report type.
    The Reporting spec has been updated to allow 100 reports per report type
    in the buffer. This patch updated the implementation to match the spec.
    
    https://w3c.github.io/reporting/#notify-observers
    
    Bug: https://w3c.github.io/reporting/#notify-observers
    Change-Id: Ia49782156d74a334b6648d4c1f745117b8099b8d
    Reviewed-on: https://chromium-review.googlesource.com/c/1405537
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Paul Meyer <paulmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623969}
    Former-commit-id: 598a3541de56fb27807f6921ac9fbb1687b48e40

--- third_party/blink/web_tests/reporting-observer/resources/intervention.js
+++ third_party/blink/web_tests/reporting-observer/resources/intervention.js
@@ -21,7 +21,7 @@ function causeIntervention() {
   };
   var event = new TouchEvent('touchstart', touchEventInit);
 
-  var deadline = performance.now() + 100;
+  var deadline = performance.now() + 1;
   while (performance.now() < deadline) {};
 
   document.body.dispatchEvent(event);
