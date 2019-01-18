6a459e1657635acf3c9107277204023023a1b9b7
Lei Zhang <thestig@chromium.org>
2019-01-18 23:49:32 +0000

    DCHECK PrinterQuery runs on the IO thread.
    Sprinkle DCHECK_CURRENTLY_ON(content::BrowserThread::IO) everywhere. Get
    rid of |PrinterQuery::task_runner_| since it is always just the IO
    thread.
    
    Change-Id: I823a4b3a3359b3f66464561be142185cf48139f0
    Reviewed-on: https://chromium-review.googlesource.com/c/1419159
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624360}
    Former-commit-id: 3e1ac2bf7d1c6ab17fc8223f0a4d4e6af5c83d85

--- chrome/browser/printing/printer_query.h
+++ chrome/browser/printing/printer_query.h
@@ -17,7 +17,6 @@
 
 namespace base {
 class Location;
-class SequencedTaskRunner;
 }
 
 namespace printing {
@@ -65,7 +64,7 @@ class PrinterQuery : public base::RefCountedThreadSafe<PrinterQuery> {
 
 #if defined(OS_CHROMEOS)
   // Updates the current settings with |new_settings|.
-  void SetSettingsFromPOD(std::unique_ptr<printing::PrintSettings> new_settings,
+  void SetSettingsFromPOD(std::unique_ptr<PrintSettings> new_settings,
                           base::OnceClosure callback);
 #endif
 
@@ -80,10 +79,6 @@ class PrinterQuery : public base::RefCountedThreadSafe<PrinterQuery> {
 
   // Returns if a worker thread is still associated to this instance.
   bool is_valid() const;
-
-  // Returns true if tasks posted to this TaskRunner are sequenced
-  // with this call.
-  bool RunsTasksInCurrentSequence() const;
 
   // Posts the given task to be run.
   bool PostTask(const base::Location& from_here, base::OnceClosure task);
@@ -115,10 +110,6 @@ class PrinterQuery : public base::RefCountedThreadSafe<PrinterQuery> {
 
   // Callback waiting to be run.
   base::OnceClosure callback_;
-
-  // Task runner reference. Used to send notifications in the right
-  // thread.
-  scoped_refptr<base::SequencedTaskRunner> task_runner_;
 
   // All the UI is done in a worker thread because many Win32 print functions
   // are blocking and enters a message loop without your consent. There is one
