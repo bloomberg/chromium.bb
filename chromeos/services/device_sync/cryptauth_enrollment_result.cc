a6c61cd0b3208bb25dc95864dae2a157faf1f8f8
Josh Nohle <nohle@google.com>
2019-01-18 02:07:18 +0000

    Add CryptAuthEnrollmentResult class
    This class holds the status of a CryptAuth v2 Enrollment attempt and
    potentially a new ClientDirective from the SyncKeysResponse.
    
    The CryptAuthEnroller will construct this object and pass it to the
    CryptAuthEnrollmentManager when the enrollment finishes, successfully or
    not. The CryptAuthEnrollmentManager will forward this on the the
    CryptAuthEnrollmentScheduler to process the ClientDirective and schedule
    a retry attempt if necessary. The CryptAuthEnrollmentManager will use
    the CryptAuthEnrollmentResult::ResultCode for metrics.
    
    Bug: 899080
    Change-Id: Id70526ef9e02c9029ee174b7612711ac0f31c06f
    Reviewed-on: https://chromium-review.googlesource.com/c/1409805
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#623957}
    Former-commit-id: a96136463f8a55944c7ed296820cf9e6b711c062

---
+++ chromeos/services/device_sync/cryptauth_enrollment_result.cc
@@ -0,0 +1,53 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/services/device_sync/cryptauth_enrollment_result.h"
+
+namespace chromeos {
+
+namespace device_sync {
+
+// static
+CryptAuthEnrollmentResult::ResultCode
+CryptAuthEnrollmentResult::NetworkRequestErrorToResultCode(
+    NetworkRequestError error) {
+  switch (error) {
+    case NetworkRequestError::kOffline:
+      return CryptAuthEnrollmentResult::ResultCode::kNetworkRequestErrorOffline;
+    case NetworkRequestError::kEndpointNotFound:
+      return CryptAuthEnrollmentResult::ResultCode::
+          kNetworkRequestErrorEndpointNotFound;
+    case NetworkRequestError::kAuthenticationError:
+      return CryptAuthEnrollmentResult::ResultCode::
+          kNetworkRequestErrorAuthenticationError;
+    case NetworkRequestError::kBadRequest:
+      return CryptAuthEnrollmentResult::ResultCode::
+          kNetworkRequestErrorBadRequest;
+    case NetworkRequestError::kResponseMalformed:
+      return CryptAuthEnrollmentResult::ResultCode::
+          kNetworkRequestErrorResponseMalformed;
+    case NetworkRequestError::kInternalServerError:
+      return CryptAuthEnrollmentResult::ResultCode::
+          kNetworkRequestErrorInternalServerError;
+    case NetworkRequestError::kUnknown:
+      return CryptAuthEnrollmentResult::ResultCode::kNetworkRequestErrorUnknown;
+  }
+}
+
+CryptAuthEnrollmentResult::CryptAuthEnrollmentResult(
+    ResultCode result_code,
+    const base::Optional<cryptauthv2::ClientDirective>& client_directive)
+    : result_code_(result_code), client_directive_(client_directive) {}
+
+CryptAuthEnrollmentResult::~CryptAuthEnrollmentResult() = default;
+
+bool CryptAuthEnrollmentResult::IsSuccess() const {
+  return result_code_ == ResultCode::kSuccessNoSyncRequired ||
+         result_code_ == ResultCode::kSuccessNewKeysEnrolled ||
+         result_code_ == ResultCode::kSuccessNoNewKeysNeeded;
+}
+
+}  // namespace device_sync
+
+}  // namespace chromeos
---
+++ chromeos/services/device_sync/cryptauth_enrollment_result.h
@@ -0,0 +1,82 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_SERVICES_DEVICE_SYNC_CRYPTAUTH_ENROLLMENT_RESULT_H_
+#define CHROMEOS_SERVICES_DEVICE_SYNC_CRYPTAUTH_ENROLLMENT_RESULT_H_
+
+#include "base/optional.h"
+#include "chromeos/services/device_sync/network_request_error.h"
+#include "chromeos/services/device_sync/proto/cryptauth_directive.pb.h"
+
+namespace chromeos {
+
+namespace device_sync {
+
+// Information about the result of a CryptAuth v2 Enrollment attempt.
+class CryptAuthEnrollmentResult {
+ public:
+  // Enum class to denote the result of a CryptAuth v2 Enrollment attempt
+  enum class ResultCode {
+    // No SyncKeysRequest was made to CryptAuth because it was not necessary.
+    kSuccessNoSyncRequired,
+    // Successfully synced but no new keys were requested by CryptAuth, so no
+    // EnrollKeysRequest was made.
+    kSuccessNoNewKeysNeeded,
+    // Successfully synced and enrolled new key(s) with CryptAuth.
+    kSuccessNewKeysEnrolled,
+    // Request could not be completed because the device is offline or has
+    // issues sending the HTTP request.
+    kNetworkRequestErrorOffline,
+    // Server endpoint could not be found.
+    kNetworkRequestErrorEndpointNotFound,
+    // Authentication error contacting back-end.
+    kNetworkRequestErrorAuthenticationError,
+    // Network request was invalid.
+    kNetworkRequestErrorBadRequest,
+    // The server responded, but the response was not formatted correctly.
+    kNetworkRequestErrorResponseMalformed,
+    // Internal server error.
+    kNetworkRequestErrorInternalServerError,
+    // Unknown network request error.
+    kNetworkRequestErrorUnknown,
+    // The CryptAuth server indicated via SyncKeysResponse::server_status that
+    // it was overloaded and did not process the SyncKeysRequest.
+    kErrorCryptAuthServerOverloaded,
+    // An error occurred while performing a cryptographic operation, such as
+    // generating key proofs.
+    kErrorCryptographicOperationFailed,
+    // The size of SyncKeysResponse::sync_single_key_responses does not agree
+    // with the number of key bundle names.
+    kErrorSyncKeysResponseIncorrectNumberOfKeys,
+    // Failed to generate the new keys requested by CryptAuth or the ephemeral
+    // keys needed for intermediate cryptographic operations.
+    kErrorKeyCreationFailed
+  };
+
+  static ResultCode NetworkRequestErrorToResultCode(NetworkRequestError error);
+
+  CryptAuthEnrollmentResult(
+      ResultCode result_code,
+      const base::Optional<cryptauthv2::ClientDirective>& client_directive);
+
+  ~CryptAuthEnrollmentResult();
+
+  ResultCode result_code() const { return result_code_; }
+
+  const base::Optional<cryptauthv2::ClientDirective>& client_directive() const {
+    return client_directive_;
+  };
+
+  bool IsSuccess() const;
+
+ private:
+  ResultCode result_code_;
+  base::Optional<cryptauthv2::ClientDirective> client_directive_;
+};
+
+}  // namespace device_sync
+
+}  // namespace chromeos
+
+#endif  // CHROMEOS_SERVICES_DEVICE_SYNC_CRYPTAUTH_ENROLLMENT_RESULT_H_
