c04ea15b57549d376a72c7a25d81871a906b1579
Caleb Rouleau <crouleau@chromium.org>
2019-01-18 23:58:25 +0000

    Revert "[Chromeshine] Persist suspensions to LevelDB."
    This reverts commit e7e6aa7943da591b0f83d228a090bb8f83a98539 [formerly f53b2aa61d5e75ea4936d30a50acb39168265439].
    
    Reason for revert: https://bugs.chromium.org/p/chromium/issues/detail?id=923591
    
    Original change's description:
    > [Chromeshine] Persist suspensions to LevelDB.
    > 
    > Implement the Set- and GetAllSuspensions functions for
    > UsageStatsDatabase.
    > 
    > Bug: 902570
    > Change-Id: Iffc6cc1e2c8d271875834981c1b04f70c643c2a1
    > Reviewed-on: https://chromium-review.googlesource.com/c/1419243
    > Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    > Reviewed-by: Patrick Noland <pnoland@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#624353}
    
    TBR=pnoland@chromium.org,chouinard@chromium.org
    
    Change-Id: I8d1df190e7518574df033c9b902c133a0ad92fd4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 902570
    Reviewed-on: https://chromium-review.googlesource.com/c/1423578
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Commit-Queue: Caleb Rouleau <crouleau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624365}
    Former-commit-id: 90ccc0d7b34a3b2667bbf0f5c0b4d1205e51766e

--- chrome/browser/android/usage_stats/usage_stats_database.cc
+++ chrome/browser/android/usage_stats/usage_stats_database.cc
@@ -4,10 +4,7 @@
 
 #include "chrome/browser/android/usage_stats/usage_stats_database.h"
 
-#include <utility>
-
 #include "base/task/post_task.h"
-#include "chrome/browser/android/usage_stats/website_event.pb.h"
 #include "components/leveldb_proto/content/proto_database_provider_factory.h"
 #include "components/leveldb_proto/public/proto_database_provider.h"
 
@@ -15,9 +12,6 @@ namespace usage_stats {
 
 const char kNamespace[] = "usage_stats";
 const char kTypePrefix[] = "usage_stats";
-
-const char kKeySeparator = '.';
-const char kSuspensionPrefix[] = "suspension";
 
 UsageStatsDatabase::UsageStatsDatabase(Profile* profile)
     : weak_ptr_factory_(this) {
@@ -35,84 +29,6 @@ UsageStatsDatabase::UsageStatsDatabase(Profile* profile)
           {base::MayBlock(), base::TaskPriority::BEST_EFFORT}));
 }
 
-UsageStatsDatabase::UsageStatsDatabase(
-    std::unique_ptr<leveldb_proto::ProtoDatabase<UsageStat>> proto_db)
-    : proto_db_(std::move(proto_db)), weak_ptr_factory_(this) {}
-
 UsageStatsDatabase::~UsageStatsDatabase() = default;
-
-namespace {
-bool DoesNotContainFilter(const base::flat_set<std::string>& set,
-                          const std::string& key) {
-  return !set.contains(key);
-}
-
-UsageStatsDatabase::Error ToError(bool success) {
-  return success ? UsageStatsDatabase::Error::kNoError
-                 : UsageStatsDatabase::Error::kUnknownError;
-}
-}  // namespace
-
-void UsageStatsDatabase::GetAllSuspensions(SuspensionCallback callback) {
-  // Load all UsageStats with the suspension prefix.
-  proto_db_->LoadEntriesWithFilter(
-      leveldb_proto::LevelDB::KeyFilter(), leveldb::ReadOptions(),
-      kSuspensionPrefix,
-      base::BindOnce(&UsageStatsDatabase::OnLoadEntriesForGetAllSuspensions,
-                     weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
-}
-
-void UsageStatsDatabase::SetSuspensions(base::flat_set<std::string> domains,
-                                        StatusCallback callback) {
-  std::vector<std::string> keys;
-  keys.reserve(domains.size());
-
-  auto entries = std::make_unique<
-      leveldb_proto::ProtoDatabase<UsageStat>::KeyEntryVector>();
-
-  for (std::string domain : domains) {
-    // Prepend prefix to form key.
-    std::string key = std::string(kSuspensionPrefix) + kKeySeparator + domain;
-
-    keys.emplace_back(key);
-
-    UsageStat value;
-    Suspension* suspension = value.mutable_suspension();
-    suspension->set_fqdn(domain);
-
-    entries->emplace_back(key, value);
-  }
-
-  auto key_set = base::flat_set<std::string>(keys);
-
-  // Add all entries created from domain set, remove all entries not in the set.
-  proto_db_->UpdateEntriesWithRemoveFilter(
-      std::move(entries),
-      base::BindRepeating(&DoesNotContainFilter, std::move(key_set)),
-      base::BindOnce(&UsageStatsDatabase::OnUpdateEntriesForSetSuspensions,
-                     weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
-}
-
-void UsageStatsDatabase::OnLoadEntriesForGetAllSuspensions(
-    SuspensionCallback callback,
-    bool success,
-    std::unique_ptr<std::vector<UsageStat>> stats) {
-  std::vector<std::string> results;
-
-  if (stats) {
-    results.reserve(stats->size());
-    for (UsageStat stat : *stats) {
-      results.emplace_back(stat.suspension().fqdn());
-    }
-  }
-
-  std::move(callback).Run(ToError(success), std::move(results));
-}
-
-void UsageStatsDatabase::OnUpdateEntriesForSetSuspensions(
-    StatusCallback callback,
-    bool success) {
-  std::move(callback).Run(ToError(success));
-}
 
 }  // namespace usage_stats
