1feb97f0d4f4b0bf2c756f5a54e32011d6bec618
Christian Fremerey <chfremer@chromium.org>
2019-01-19 02:10:06 +0000

    [Video Capture Service] Multi-client: Add VideoSource implementation
    This CL fills interface VideoSource with methods and adds implementation.
    The implementation uses a class BroadcastingReceiver whose task is to
    distribute events to potentially multiple receivers.
    
    Design Doc: https://docs.google.com/document/d/1mYnsZfLBRmbsDpUtfb6C7dzhfw2Kcxg_-uiG_6MnWVQ/edit?usp=sharing
    Test: services_unittests --gtest_filter=MockVideoCaptureDeviceSharedAccessTest.*
    Bug: 783442
    
    Change-Id: Id9f6c497b0787dfba8caa15b085580ba0939f8b1
    Reviewed-on: https://chromium-review.googlesource.com/c/1263355
    Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Luke Sorenson <lasoren@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624418}
    Former-commit-id: 150ac101397f98d805f4b8cd88ec63ec9083ef8a

--- services/video_capture/test/mock_device_shared_access_unittest.cc
+++ services/video_capture/test/mock_device_shared_access_unittest.cc
@@ -5,6 +5,7 @@
 #include "base/run_loop.h"
 #include "base/test/mock_callback.h"
 #include "base/test/scoped_task_environment.h"
+#include "media/capture/mojom/image_capture_types.h"
 #include "media/capture/video/mock_device.h"
 #include "media/capture/video/mock_device_factory.h"
 #include "media/capture/video/video_capture_device.h"
@@ -17,6 +18,7 @@
 #include "testing/gtest/include/gtest/gtest.h"
 
 using testing::_;
+using testing::Invoke;
 using testing::InvokeWithoutArgs;
 using testing::Mock;
 
@@ -24,7 +26,11 @@ namespace video_capture {
 
 class MockDeviceSharedAccessTest : public ::testing::Test {
  public:
-  MockDeviceSharedAccessTest() : service_keepalive_(nullptr, base::nullopt) {}
+  MockDeviceSharedAccessTest()
+      : mock_receiver_1_(mojo::MakeRequest(&receiver_1_)),
+        mock_receiver_2_(mojo::MakeRequest(&receiver_2_)),
+        service_keepalive_(nullptr, base::nullopt),
+        next_arbitrary_frame_feedback_id_(123) {}
   ~MockDeviceSharedAccessTest() override {}
 
   void SetUp() override {
@@ -54,6 +60,149 @@ class MockDeviceSharedAccessTest : public ::testing::Test {
     wait_loop.Run();
     source_provider_->GetVideoSource(mock_descriptor.device_id,
                                      mojo::MakeRequest(&source_));
+
+    requestable_settings_.requested_format.frame_size = gfx::Size(800, 600);
+    requestable_settings_.requested_format.frame_rate = 15;
+    requestable_settings_.requested_format.pixel_format =
+        media::PIXEL_FORMAT_I420;
+    requestable_settings_.resolution_change_policy =
+        media::ResolutionChangePolicy::FIXED_RESOLUTION;
+    requestable_settings_.power_line_frequency =
+        media::PowerLineFrequency::FREQUENCY_DEFAULT;
+  }
+
+  void LetClient1ConnectWithRequestableSettingsAndExpectToGetThem() {
+    base::RunLoop run_loop;
+    source_->CreatePushSubscription(
+        std::move(receiver_1_), requestable_settings_,
+        false /*force_reopen_with_new_settings*/,
+        mojo::MakeRequest(&subscription_1_),
+        base::BindOnce(
+            [](base::RunLoop* run_loop,
+               media::VideoCaptureParams* requested_settings,
+               mojom::CreatePushSubscriptionResultCode result_code,
+               const media::VideoCaptureParams&
+                   settings_source_was_opened_with) {
+              ASSERT_EQ(mojom::CreatePushSubscriptionResultCode::
+                            kCreatedWithRequestedSettings,
+                        result_code);
+              ASSERT_EQ(*requested_settings, settings_source_was_opened_with);
+              run_loop->Quit();
+            },
+            &run_loop, &requestable_settings_));
+    run_loop.Run();
+  }
+
+  void LetClient2ConnectWithRequestableSettingsAndExpectToGetThem() {
+    base::RunLoop run_loop;
+    source_->CreatePushSubscription(
+        std::move(receiver_2_), requestable_settings_,
+        false /*force_reopen_with_new_settings*/,
+        mojo::MakeRequest(&subscription_2_),
+        base::BindOnce(
+            [](base::RunLoop* run_loop,
+               media::VideoCaptureParams* requested_settings,
+               mojom::CreatePushSubscriptionResultCode result_code,
+               const media::VideoCaptureParams&
+                   settings_source_was_opened_with) {
+              ASSERT_EQ(mojom::CreatePushSubscriptionResultCode::
+                            kCreatedWithRequestedSettings,
+                        result_code);
+              ASSERT_EQ(*requested_settings, settings_source_was_opened_with);
+              run_loop->Quit();
+            },
+            &run_loop, &requestable_settings_));
+    run_loop.Run();
+  }
+
+  void LetTwoClientsConnectWithSameSettingsOneByOne() {
+    LetClient1ConnectWithRequestableSettingsAndExpectToGetThem();
+    LetClient2ConnectWithRequestableSettingsAndExpectToGetThem();
+  }
+
+  void LetTwoClientsConnectWithDifferentSettings() {
+    base::RunLoop run_loop_1;
+    base::RunLoop run_loop_2;
+    source_->CreatePushSubscription(
+        std::move(receiver_1_), requestable_settings_,
+        false /*force_reopen_with_new_settings*/,
+        mojo::MakeRequest(&subscription_1_),
+        base::BindOnce(
+            [](base::RunLoop* run_loop_1,
+               media::VideoCaptureParams* requested_settings,
+               mojom::CreatePushSubscriptionResultCode result_code,
+               const media::VideoCaptureParams&
+                   settings_source_was_opened_with) {
+              ASSERT_EQ(mojom::CreatePushSubscriptionResultCode::
+                            kCreatedWithRequestedSettings,
+                        result_code);
+              ASSERT_EQ(*requested_settings, settings_source_was_opened_with);
+              run_loop_1->Quit();
+            },
+            &run_loop_1, &requestable_settings_));
+
+    auto different_settings = requestable_settings_;
+    // Change something arbitrary
+    different_settings.requested_format.frame_size = gfx::Size(123, 456);
+    ASSERT_FALSE(requestable_settings_ == different_settings);
+
+    source_->CreatePushSubscription(
+        std::move(receiver_2_), different_settings,
+        false /*force_reopen_with_new_settings*/,
+        mojo::MakeRequest(&subscription_2_),
+        base::BindOnce(
+            [](base::RunLoop* run_loop_2,
+               media::VideoCaptureParams* requested_settings,
+               mojom::CreatePushSubscriptionResultCode result_code,
+               const media::VideoCaptureParams&
+                   settings_source_was_opened_with) {
+              ASSERT_EQ(mojom::CreatePushSubscriptionResultCode::
+                            kCreatedWithDifferentSettings,
+                        result_code);
+              ASSERT_EQ(*requested_settings, settings_source_was_opened_with);
+              run_loop_2->Quit();
+            },
+            &run_loop_2, &requestable_settings_));
+
+    run_loop_1.Run();
+    run_loop_2.Run();
+  }
+
+  void SendFrameAndExpectToArriveAtBothSubscribers() {
+    const int32_t kArbitraryFrameFeedbackId =
+        next_arbitrary_frame_feedback_id_++;
+    const int32_t kArbitraryRotation = 0;
+    base::RunLoop wait_loop_1;
+    EXPECT_CALL(mock_receiver_1_,
+                DoOnFrameReadyInBuffer(_, kArbitraryFrameFeedbackId, _, _))
+        .WillOnce(InvokeWithoutArgs([&wait_loop_1]() { wait_loop_1.Quit(); }));
+    base::RunLoop wait_loop_2;
+    EXPECT_CALL(mock_receiver_2_,
+                DoOnFrameReadyInBuffer(_, kArbitraryFrameFeedbackId, _, _))
+        .WillOnce(InvokeWithoutArgs([&wait_loop_2]() { wait_loop_2.Quit(); }));
+    mock_device_.SendStubFrame(requestable_settings_.requested_format,
+                               kArbitraryRotation, kArbitraryFrameFeedbackId);
+    wait_loop_1.Run();
+    wait_loop_2.Run();
+    Mock::VerifyAndClearExpectations(&mock_receiver_1_);
+    Mock::VerifyAndClearExpectations(&mock_receiver_2_);
+  }
+
+  void SendFrameAndExpectToArriveOnlyAtSubscriber2() {
+    const int32_t kArbitraryFrameFeedbackId =
+        next_arbitrary_frame_feedback_id_++;
+    const int32_t kArbitraryRotation = 0;
+
+    base::RunLoop wait_loop;
+    EXPECT_CALL(mock_receiver_1_, DoOnFrameReadyInBuffer(_, _, _, _)).Times(0);
+    EXPECT_CALL(mock_receiver_2_,
+                DoOnFrameReadyInBuffer(_, kArbitraryFrameFeedbackId, _, _))
+        .WillOnce(InvokeWithoutArgs([&wait_loop]() { wait_loop.Quit(); }));
+    mock_device_.SendStubFrame(requestable_settings_.requested_format,
+                               kArbitraryRotation, kArbitraryFrameFeedbackId);
+    wait_loop.Run();
+    Mock::VerifyAndClearExpectations(&mock_receiver_1_);
+    Mock::VerifyAndClearExpectations(&mock_receiver_2_);
   }
 
  protected:
@@ -62,17 +211,183 @@ class MockDeviceSharedAccessTest : public ::testing::Test {
   std::unique_ptr<DeviceFactoryMediaToMojoAdapter> mock_device_factory_;
   std::unique_ptr<VideoSourceProviderImpl> source_provider_;
   mojom::VideoSourcePtr source_;
+  media::VideoCaptureParams requestable_settings_;
+
+  mojom::PushVideoStreamSubscriptionPtr subscription_1_;
+  mojom::ReceiverPtr receiver_1_;
+  MockReceiver mock_receiver_1_;
+  mojom::PushVideoStreamSubscriptionPtr subscription_2_;
+  mojom::ReceiverPtr receiver_2_;
+  MockReceiver mock_receiver_2_;
 
  private:
   service_manager::ServiceKeepalive service_keepalive_;
+  int32_t next_arbitrary_frame_feedback_id_;
 };
 
 // This alias ensures test output is easily attributed to this service's tests.
 // TODO(chfremer): Consider just renaming the type.
 using MockVideoCaptureDeviceSharedAccessTest = MockDeviceSharedAccessTest;
 
-TEST_F(MockVideoCaptureDeviceSharedAccessTest, SourceIsBound) {
-  ASSERT_TRUE(source_.is_bound());
+TEST_F(MockVideoCaptureDeviceSharedAccessTest,
+       TwoClientsCreatePushSubscriptionWithSameSettings) {
+  LetTwoClientsConnectWithSameSettingsOneByOne();
+}
+
+TEST_F(MockVideoCaptureDeviceSharedAccessTest,
+       TwoClientsCreatePushSubscriptionWithDifferentSettings) {
+  LetTwoClientsConnectWithDifferentSettings();
+}
+
+TEST_F(MockVideoCaptureDeviceSharedAccessTest,
+       NoFramesArePushedUntilSubscriptionIsActivated) {
+  LetTwoClientsConnectWithSameSettingsOneByOne();
+
+  subscription_2_->Activate();
+  SendFrameAndExpectToArriveOnlyAtSubscriber2();
+
+  subscription_1_->Activate();
+  SendFrameAndExpectToArriveAtBothSubscribers();
+}
+
+TEST_F(MockVideoCaptureDeviceSharedAccessTest,
+       DiscardingLastSubscriptionStopsTheDevice) {
+  EXPECT_CALL(mock_device_, DoAllocateAndStart(_, _)).Times(1);
+  EXPECT_CALL(mock_device_, DoStopAndDeAllocate()).Times(0);
+  LetTwoClientsConnectWithDifferentSettings();
+  subscription_1_.reset();
+  Mock::VerifyAndClearExpectations(&mock_device_);
+
+  base::RunLoop wait_loop;
+  EXPECT_CALL(mock_device_, DoStopAndDeAllocate())
+      .WillOnce(InvokeWithoutArgs([&wait_loop]() { wait_loop.Quit(); }));
+  subscription_2_.reset();
+  wait_loop.Run();
+}
+
+TEST_F(MockVideoCaptureDeviceSharedAccessTest,
+       DiscardingVideoSourceWithActiveSubscriptionsStopsTheDevice) {
+  LetTwoClientsConnectWithDifferentSettings();
+  subscription_1_->Activate();
+  subscription_2_->Activate();
+
+  base::RunLoop wait_loop;
+  EXPECT_CALL(mock_device_, DoStopAndDeAllocate())
+      .WillOnce(InvokeWithoutArgs([&wait_loop]() { wait_loop.Quit(); }));
+  source_.reset();
+  wait_loop.Run();
+}
+
+TEST_F(MockVideoCaptureDeviceSharedAccessTest,
+       NoMoreFramesArriveAfterClosingSubscription) {
+  LetTwoClientsConnectWithDifferentSettings();
+  subscription_1_->Activate();
+  subscription_2_->Activate();
+
+  SendFrameAndExpectToArriveAtBothSubscribers();
+
+  {
+    base::RunLoop wait_loop;
+    subscription_1_->Close(base::BindOnce(
+        [](base::RunLoop* wait_loop) { wait_loop->Quit(); }, &wait_loop));
+    wait_loop.Run();
+  }
+
+  SendFrameAndExpectToArriveOnlyAtSubscriber2();
+}
+
+TEST_F(MockVideoCaptureDeviceSharedAccessTest, SuspendAndResume) {
+  LetTwoClientsConnectWithDifferentSettings();
+  subscription_1_->Activate();
+  subscription_2_->Activate();
+
+  SendFrameAndExpectToArriveAtBothSubscribers();
+
+  {
+    base::RunLoop wait_loop;
+    subscription_1_->Suspend(base::BindOnce(
+        [](base::RunLoop* wait_loop) { wait_loop->Quit(); }, &wait_loop));
+    wait_loop.Run();
+  }
+
+  SendFrameAndExpectToArriveOnlyAtSubscriber2();
+
+  subscription_1_->Resume();
+  SendFrameAndExpectToArriveAtBothSubscribers();
+}
+
+TEST_F(MockVideoCaptureDeviceSharedAccessTest,
+       CreateNewSubscriptionAfterClosingExistingOneUsesNewSettings) {
+  LetClient1ConnectWithRequestableSettingsAndExpectToGetThem();
+  base::RunLoop run_loop;
+  subscription_1_->Close(base::BindOnce(
+      [](base::RunLoop* run_loop) { run_loop->Quit(); }, &run_loop));
+  run_loop.Run();
+
+  // modify settings to check if they will be applied
+  const int kArbitraryDifferentWidth = 753;
+  requestable_settings_.requested_format.frame_size.set_width(
+      kArbitraryDifferentWidth);
+  LetClient2ConnectWithRequestableSettingsAndExpectToGetThem();
+}
+
+TEST_F(MockVideoCaptureDeviceSharedAccessTest,
+       TakePhotoUsingOnPushSubscription) {
+  LetTwoClientsConnectWithDifferentSettings();
+
+  {
+    EXPECT_CALL(mock_device_, DoGetPhotoState(_))
+        .WillOnce(Invoke(
+            [](media::VideoCaptureDevice::GetPhotoStateCallback* callback) {
+              media::mojom::PhotoStatePtr state = mojo::CreateEmptyPhotoState();
+              std::move(*callback).Run(std::move(state));
+            }));
+    base::RunLoop run_loop;
+    subscription_1_->GetPhotoState(base::BindOnce(
+        [](base::RunLoop* run_loop, media::mojom::PhotoStatePtr state) {
+          run_loop->Quit();
+        },
+        &run_loop));
+
+    run_loop.Run();
+  }
+
+  {
+    EXPECT_CALL(mock_device_, DoSetPhotoOptions(_, _))
+        .WillOnce(Invoke(
+            [](media::mojom::PhotoSettingsPtr* settings,
+               media::VideoCaptureDevice::SetPhotoOptionsCallback* callback) {
+              media::mojom::BlobPtr blob = media::mojom::Blob::New();
+              std::move(*callback).Run(true);
+            }));
+    media::mojom::PhotoSettingsPtr settings =
+        media::mojom::PhotoSettings::New();
+    base::RunLoop run_loop;
+    subscription_1_->SetPhotoOptions(
+        std::move(settings), base::BindOnce(
+                                 [](base::RunLoop* run_loop, bool succeeded) {
+                                   ASSERT_TRUE(succeeded);
+                                   run_loop->Quit();
+                                 },
+                                 &run_loop));
+    run_loop.Run();
+  }
+
+  {
+    EXPECT_CALL(mock_device_, DoTakePhoto(_))
+        .WillOnce(
+            Invoke([](media::VideoCaptureDevice::TakePhotoCallback* callback) {
+              media::mojom::BlobPtr blob = media::mojom::Blob::New();
+              std::move(*callback).Run(std::move(blob));
+            }));
+    base::RunLoop run_loop;
+    subscription_1_->TakePhoto(
+        base::BindOnce([](base::RunLoop* run_loop,
+                          media::mojom::BlobPtr state) { run_loop->Quit(); },
+                       &run_loop));
+
+    run_loop.Run();
+  }
 }
 
 }  // namespace video_capture
--- services/video_capture/video_source_impl.cc
+++ services/video_capture/video_source_impl.cc
@@ -4,6 +4,8 @@
 
 #include "services/video_capture/video_source_impl.h"
 
+#include "services/video_capture/push_video_stream_subscription_impl.h"
+
 namespace video_capture {
 
 VideoSourceImpl::VideoSourceImpl(
@@ -13,9 +15,8 @@ VideoSourceImpl::VideoSourceImpl(
     : device_factory_(device_factory),
       device_id_(device_id),
       on_last_binding_closed_cb_(std::move(on_last_binding_closed_cb)),
+      device_status_(DeviceStatus::kNotStarted),
       weak_factory_(this) {
-  CHECK(device_factory_);
-  CHECK_GE(device_id_.size(), 0u);
   bindings_.set_connection_error_handler(base::BindRepeating(
       &VideoSourceImpl::OnClientDisconnected, base::Unretained(this)));
 }
@@ -28,12 +29,113 @@ void VideoSourceImpl::AddToBindingSet(mojom::VideoSourceRequest request) {
   bindings_.AddBinding(this, std::move(request));
 }
 
+void VideoSourceImpl::CreatePushSubscription(
+    mojom::ReceiverPtr subscriber,
+    const media::VideoCaptureParams& requested_settings,
+    bool force_reopen_with_new_settings,
+    mojom::PushVideoStreamSubscriptionRequest subscription_request,
+    CreatePushSubscriptionCallback callback) {
+  if (force_reopen_with_new_settings) {
+    NOTIMPLEMENTED();
+    return;
+  }
+  auto subscription = std::make_unique<PushVideoStreamSubscriptionImpl>(
+      std::move(subscription_request), std::move(subscriber),
+      requested_settings, std::move(callback), &broadcaster_, &device_);
+  subscription->SetOnClosedHandler(base::BindOnce(
+      &VideoSourceImpl::OnPushSubscriptionClosedOrDisconnectedOrDiscarded,
+      weak_factory_.GetWeakPtr(), subscription.get()));
+  auto* subscription_ptr = subscription.get();
+  push_subscriptions_.insert(
+      std::make_pair(subscription.get(), std::move(subscription)));
+  switch (device_status_) {
+    case DeviceStatus::kNotStarted:
+      device_start_settings_ = requested_settings;
+      device_status_ = DeviceStatus::kStartingAsynchronously;
+      device_factory_->CreateDevice(
+          device_id_, mojo::MakeRequest(&device_),
+          base::BindOnce(&VideoSourceImpl::OnCreateDeviceResponse,
+                         weak_factory_.GetWeakPtr()));
+      return;
+    case DeviceStatus::kStartingAsynchronously:
+      // No need to do anything. Response will be sent when
+      // OnCreateDeviceResponse() gets called.
+      return;
+    case DeviceStatus::kStarted:
+      subscription_ptr->NotifySubscriberCreateSubscriptionSucceededWithSettings(
+          device_start_settings_);
+      return;
+  }
+}
+
 void VideoSourceImpl::OnClientDisconnected() {
   if (bindings_.empty()) {
     // Note: Invoking this callback may synchronously trigger the destruction of
     // |this|, so no more member access should be done after it.
     on_last_binding_closed_cb_.Run();
   }
+}
+
+void VideoSourceImpl::OnCreateDeviceResponse(
+    mojom::DeviceAccessResultCode result_code) {
+  switch (result_code) {
+    case mojom::DeviceAccessResultCode::SUCCESS: {
+      mojom::ReceiverPtr broadcaster_as_receiver;
+      broadcaster_binding_ = std::make_unique<mojo::Binding<mojom::Receiver>>(
+          &broadcaster_, mojo::MakeRequest(&broadcaster_as_receiver));
+      device_->Start(device_start_settings_,
+                     std::move(broadcaster_as_receiver));
+      device_status_ = DeviceStatus::kStarted;
+      if (push_subscriptions_.empty()) {
+        StopDevice();
+        return;
+      }
+      for (auto& entry : push_subscriptions_) {
+        auto& subscription = entry.second;
+        subscription->NotifySubscriberCreateSubscriptionSucceededWithSettings(
+            device_start_settings_);
+      }
+      return;
+    }
+    case mojom::DeviceAccessResultCode::ERROR_DEVICE_NOT_FOUND:  // Fall through
+    case mojom::DeviceAccessResultCode::NOT_INITIALIZED:
+      for (auto& entry : push_subscriptions_) {
+        auto& subscription = entry.second;
+        subscription->NotifySubscriberCreateSubscriptionFailed();
+      }
+      push_subscriptions_.clear();
+      device_status_ = DeviceStatus::kNotStarted;
+      return;
+  }
+}
+
+void VideoSourceImpl::OnPushSubscriptionClosedOrDisconnectedOrDiscarded(
+    PushVideoStreamSubscriptionImpl* subscription,
+    base::OnceClosure done_cb) {
+  // We keep the subscription instance alive until after having called |done_cb|
+  // in order to allow it to send out a callback before being destroyed.
+  auto subscription_ownership = std::move(push_subscriptions_[subscription]);
+  push_subscriptions_.erase(subscription);
+  if (push_subscriptions_.empty()) {
+    switch (device_status_) {
+      case DeviceStatus::kNotStarted:
+        // Nothing to do here.
+        break;
+      case DeviceStatus::kStartingAsynchronously:
+        // We will check again in OnCreateDeviceResponse() whether or not there
+        // are any subscriptions.
+        break;
+      case DeviceStatus::kStarted:
+        StopDevice();
+        break;
+    }
+  }
+  std::move(done_cb).Run();
+}
+
+void VideoSourceImpl::StopDevice() {
+  device_.reset();
+  device_status_ = DeviceStatus::kNotStarted;
 }
 
 }  // namespace video_capture
