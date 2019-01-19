bb4ee5881fdea3dc8cf489c5542e3a2e35fa9764
Becca Hughes <beccahughes@chromium.org>
2019-01-19 00:55:58 +0000

    [Media Notification] Use source title as the app name
    Use the source title as the app name for the media
    notification.
    
    BUG=893296
    
    Change-Id: Ida8cb2ac5c140c6c5cdee0804294f7cd9f6fc254
    Reviewed-on: https://chromium-review.googlesource.com/c/1335663
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624387}
    Former-commit-id: 5a281cad0bb2e451e441067ade93a753693caee0

--- ui/message_center/views/notification_header_view.cc
+++ ui/message_center/views/notification_header_view.cc
@@ -407,6 +407,10 @@ std::unique_ptr<views::InkDrop> NotificationHeaderView::CreateInkDrop() {
   return std::make_unique<views::InkDropStub>();
 }
 
+const base::string16& NotificationHeaderView::app_name_for_testing() const {
+  return app_name_view_->text();
+}
+
 void NotificationHeaderView::UpdateSummaryTextVisibility() {
   const bool visible = has_progress_ || has_overflow_indicator_;
   summary_text_divider_->SetVisible(visible);
