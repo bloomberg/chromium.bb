c26bce1dd4ecc6d30d275e65c3ef7d366a9cb421
Hiroki Nakagawa <nhiroki@chromium.org>
2019-01-21 02:01:47 +0000

    Revert "splitview: Make split view work as expected with multiprofile."
    This reverts commit b49de83029160baf30399c9003d87dd560139e0d [formerly 3009eaced8125010fa7230d0c84674309eeac552].
    
    Reason for revert:
    
    This CL seems to cause heap-use-after-free failures on...
    
    - Linux Chromium OS ASan LSan Tests
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/31123
    - Linux ChromiumOS MSan Tests
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/Linux%20ChromiumOS%20MSan%20Tests/10815
    
    The failure happens in ash::SplitViewController::~SplitViewController().
    
    Original change's description:
    > splitview: Make split view work as expected with multiprofile.
    > 
    > BEFORE: On switching away from a user session in split view, the split
    > view divider is seen in the newly activated user session where any
    > windows are maximized. It looks horribly wrong.
    > AFTER: The split view state on switching away from a user session is
    > restored on switching back to it.
    > 
    > Test: manual
    > Bug: 833685
    > Change-Id: If5f1721d9015f59615e212425c2b8f21838572e7
    > Reviewed-on: https://chromium-review.googlesource.com/c/1389695
    > Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    > Reviewed-by: Dan Erat <derat@chromium.org>
    > Commit-Queue: Avery Musbach <amusbach@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#624363}
    
    TBR=derat@chromium.org,skuhne@chromium.org,xdai@chromium.org,afakhry@chromium.org,amusbach@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 833685
    Change-Id: I0e63f17f3dc781074e6a18768bc9fda611caeb7b
    Reviewed-on: https://chromium-review.googlesource.com/c/1424621
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624507}
    Former-commit-id: 0e505c4d47166acfca36b6137fb58962d1450c1c

--- ash/wm/splitview/split_view_controller.cc
+++ ash/wm/splitview/split_view_controller.cc
@@ -215,7 +215,6 @@ class SplitViewController::TabDraggedWindowObserver
 SplitViewController::SplitViewController() {
   Shell::Get()->accessibility_controller()->AddObserver(this);
   display::Screen::GetScreen()->AddObserver(this);
-  tablet_mode_observer_.Add(Shell::Get()->tablet_mode_controller());
 }
 
 SplitViewController::~SplitViewController() {
@@ -245,6 +244,7 @@ void SplitViewController::SnapWindow(aura::Window* window,
     // Add observers when the split view mode starts.
     Shell::Get()->AddShellObserver(this);
     Shell::Get()->activation_client()->AddObserver(this);
+    Shell::Get()->tablet_mode_controller()->AddObserver(this);
     Shell::Get()->NotifySplitViewModeStarting();
 
     divider_position_ = GetDefaultDividerPosition(window);
@@ -520,6 +520,7 @@ void SplitViewController::EndSplitView(EndReason end_reason) {
   // Remove observers when the split view mode ends.
   Shell::Get()->RemoveShellObserver(this);
   Shell::Get()->activation_client()->RemoveObserver(this);
+  Shell::Get()->tablet_mode_controller()->RemoveObserver(this);
 
   StopObserving(LEFT);
   StopObserving(RIGHT);
@@ -797,12 +798,7 @@ void SplitViewController::OnDisplayMetricsChanged(
   UpdateSnappedWindowsAndDividerBounds();
 }
 
-void SplitViewController::OnTabletModeStarted() {
-  Shell::Get()->session_controller()->AddObserver(this);
-}
-
 void SplitViewController::OnTabletModeEnding() {
-  Shell::Get()->session_controller()->RemoveObserver(this);
   EndSplitView();
 }
 
@@ -811,49 +807,6 @@ void SplitViewController::OnAccessibilityStatusChanged() {
   // they are compatible.
   if (Shell::Get()->accessibility_controller()->spoken_feedback_enabled())
     EndSplitView();
-}
-
-void SplitViewController::OnActiveUserSessionChanged(const AccountId&) {
-  // There is only one SplitViewController object for all user sessions, but
-  // functionally, each user session independently can be in split view or not.
-  // Here, a new user session has just been switched to, and if split view mode
-  // is active then it was for the previous user session. EndSplitView will
-  // perform some cleanup, including setting |left_window_| and |right_window_|
-  // to null, but the aura::Window objects will be left unchanged to facilitate
-  // switching back.
-  EndSplitView(EndReason::kActiveUserChanged);
-
-  // Search for snapped windows to detect if the now active user session was in
-  // split view. In case multiple windows were snapped to one side, one window
-  // after another, there may be multiple windows in a LEFT_SNAPPED state or
-  // multiple windows in a RIGHT_SNAPPED state. For each of those two state
-  // types that belongs to multiple windows, the relevant window will be listed
-  // first among those windows, and a null check in the loop body below will
-  // filter out the rest of them.
-  MruWindowTracker::WindowList windows =
-      Shell::Get()->mru_window_tracker()->BuildWindowListIgnoreModal();
-  for (aura::Window* window : windows) {
-    switch (wm::GetWindowState(window)->GetStateType()) {
-      case mojom::WindowStateType::LEFT_SNAPPED:
-        if (left_window_ == nullptr)
-          SnapWindow(window, LEFT);
-        break;
-      case mojom::WindowStateType::RIGHT_SNAPPED:
-        if (right_window_ == nullptr)
-          SnapWindow(window, RIGHT);
-        break;
-      default:
-        break;
-    }
-    if (state_ == BOTH_SNAPPED)
-      break;
-  }
-
-  // If there is a window snapped to one side but no window snapped to the other
-  // side, then overview should be started (to be seen on the side with no
-  // snapped window).
-  if (state_ == LEFT_SNAPPED || state_ == RIGHT_SNAPPED)
-    StartOverview();
 }
 
 void SplitViewController::StartObserving(aura::Window* window) {
