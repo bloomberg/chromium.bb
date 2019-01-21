25804af009af2e5ab69dc784ca82bbbcd1180b57
Mohsen Izadi <mohsen@chromium.org>
2019-01-21 17:59:46 +0000

    Remove double GMB re-allocation in HostGpuMemoryBufferManager
    When GPU service dies, HostGpuMemoryBufferManager tries to re-allocate
    pending buffer requests on the new GPU service; in OnConnectionError().
    So, if it later receives a buffer allocated by an old GPU service, it
    should not try to re-allocate as the re-allocation request is already
    sent.
    
    BUG=908460
    
    Change-Id: Ifdaccedc3a00d8fe92f24433788ea821bb0cb9b8
    Reviewed-on: https://chromium-review.googlesource.com/c/1413357
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624610}
    Former-commit-id: c470cedc635f679911b9c4ee8a66c19f1f1f34ee

--- components/viz/host/host_gpu_memory_buffer_manager_unittest.cc
+++ components/viz/host/host_gpu_memory_buffer_manager_unittest.cc
@@ -25,44 +25,52 @@ namespace {
 
 class TestGpuService : public mojom::GpuService {
  public:
-  TestGpuService() {}
-  ~TestGpuService() override {}
+  TestGpuService() = default;
+  ~TestGpuService() override = default;
 
-  HostGpuMemoryBufferManager::GpuServiceProvider CreateProvider() {
-    return base::BindRepeating(
-        [](mojom::GpuService* gpu_service,
-           base::OnceClosure connection_error_handler) { return gpu_service; },
-        base::Unretained(this));
+  mojom::GpuService* GetGpuService(base::OnceClosure connection_error_handler) {
+    DCHECK(!connection_error_handler_);
+    connection_error_handler_ = std::move(connection_error_handler);
+    return this;
   }
 
-  bool HasAllocationRequest(gfx::GpuMemoryBufferId id, int client_id) const {
-    for (const auto& req : allocation_requests_) {
-      if (req.id == id && req.client_id == client_id)
-        return true;
-    }
-    return false;
+  void SimulateConnectionError() {
+    if (connection_error_handler_)
+      std::move(connection_error_handler_).Run();
   }
 
-  bool HasDestructionRequest(gfx::GpuMemoryBufferId id, int client_id) const {
-    for (const auto& req : destruction_requests_) {
-      if (req.id == id && req.client_id == client_id)
-        return true;
-    }
-    return false;
+  int GetAllocationRequestsCount() const { return allocation_requests_.size(); }
+
+  bool IsAllocationRequestAt(size_t index,
+                             gfx::GpuMemoryBufferId id,
+                             int client_id) const {
+    DCHECK_LT(index, allocation_requests_.size());
+    const auto& req = allocation_requests_[index];
+    return req.id == id && req.client_id == client_id;
   }
 
-  void SatisfyAllocationRequest(gfx::GpuMemoryBufferId id, int client_id) {
-    for (auto& req : allocation_requests_) {
-      if (req.id == id && req.client_id == client_id) {
-        gfx::GpuMemoryBufferHandle handle;
-        handle.id = id;
-        handle.type = gfx::SHARED_MEMORY_BUFFER;
-        DCHECK(req.callback);
-        std::move(req.callback).Run(std::move(handle));
-        return;
-      }
-    }
-    NOTREACHED();
+  int GetDestructionRequestsCount() const {
+    return destruction_requests_.size();
+  }
+
+  bool IsDestructionRequestAt(size_t index,
+                              gfx::GpuMemoryBufferId id,
+                              int client_id) const {
+    DCHECK_LT(index, destruction_requests_.size());
+    const auto& req = destruction_requests_[index];
+    return req.id == id && req.client_id == client_id;
+  }
+
+  void SatisfyAllocationRequestAt(size_t index) {
+    DCHECK_LT(index, allocation_requests_.size());
+    auto& req = allocation_requests_[index];
+
+    gfx::GpuMemoryBufferHandle handle;
+    handle.id = req.id;
+    handle.type = gfx::SHARED_MEMORY_BUFFER;
+
+    DCHECK(req.callback);
+    std::move(req.callback).Run(std::move(handle));
   }
 
   // mojom::GpuService:
@@ -102,8 +110,7 @@ class TestGpuService : public mojom::GpuService {
                              int client_id,
                              gpu::SurfaceHandle surface_handle,
                              CreateGpuMemoryBufferCallback callback) override {
-    allocation_requests_.push_back(
-        {id, size, format, usage, client_id, std::move(callback)});
+    allocation_requests_.push_back({id, client_id, std::move(callback)});
   }
 
   void DestroyGpuMemoryBuffer(gfx::GpuMemoryBufferId id,
@@ -156,12 +163,11 @@ class TestGpuService : public mojom::GpuService {
   void Stop(StopCallback callback) override {}
 
  private:
+  base::OnceClosure connection_error_handler_;
+
   struct AllocationRequest {
-    const gfx::GpuMemoryBufferId id;
-    const gfx::Size size;
-    const gfx::BufferFormat format;
-    const gfx::BufferUsage usage;
-    const int client_id;
+    gfx::GpuMemoryBufferId id;
+    int client_id;
     CreateGpuMemoryBufferCallback callback;
   };
   std::vector<AllocationRequest> allocation_requests_;
@@ -182,8 +188,39 @@ class HostGpuMemoryBufferManagerTest : public ::testing::Test {
   HostGpuMemoryBufferManagerTest() = default;
   ~HostGpuMemoryBufferManagerTest() override = default;
 
-  std::unique_ptr<gfx::GpuMemoryBuffer> AllocateGpuMemoryBufferSync(
-      HostGpuMemoryBufferManager* manager) {
+  void SetUp() override {
+    gpu_service_ = std::make_unique<TestGpuService>();
+    auto gpu_service_provider = base::BindRepeating(
+        &TestGpuService::GetGpuService, base::Unretained(gpu_service_.get()));
+    auto gpu_memory_buffer_support =
+        std::make_unique<gpu::GpuMemoryBufferSupport>();
+    gpu_memory_buffer_manager_ = std::make_unique<HostGpuMemoryBufferManager>(
+        std::move(gpu_service_provider), 1,
+        std::move(gpu_memory_buffer_support),
+        base::ThreadTaskRunnerHandle::Get());
+  }
+
+  // Not all platforms support native configurations (currently only Windows,
+  // Mac and some Ozone platforms). Abort the test in those platforms.
+  bool IsNativePixmapConfigSupported() {
+    bool native_pixmap_supported = false;
+#if defined(USE_OZONE)
+    native_pixmap_supported =
+        ui::OzonePlatform::GetInstance()->IsNativePixmapConfigSupported(
+            gfx::BufferFormat::RGBA_8888, gfx::BufferUsage::GPU_READ);
+#elif defined(OS_MACOSX) || defined(OS_WIN)
+    native_pixmap_supported = true;
+#endif
+
+    if (native_pixmap_supported)
+      return true;
+
+    gpu::GpuMemoryBufferSupport support;
+    DCHECK(gpu::GetNativeGpuMemoryBufferConfigurations(&support).empty());
+    return false;
+  }
+
+  std::unique_ptr<gfx::GpuMemoryBuffer> AllocateGpuMemoryBufferSync() {
     base::Thread diff_thread("TestThread");
     diff_thread.Start();
     std::unique_ptr<gfx::GpuMemoryBuffer> buffer;
@@ -199,75 +236,63 @@ class HostGpuMemoryBufferManagerTest : public ::testing::Test {
                              gpu::kNullSurfaceHandle);
                          std::move(callback).Run();
                        },
-                       manager, &buffer, run_loop.QuitClosure()));
+                       gpu_memory_buffer_manager_.get(), &buffer,
+                       run_loop.QuitClosure()));
     run_loop.Run();
     return buffer;
   }
 
+  TestGpuService* gpu_service() const { return gpu_service_.get(); }
+
+  HostGpuMemoryBufferManager* gpu_memory_buffer_manager() const {
+    return gpu_memory_buffer_manager_.get();
+  }
+
  private:
+  std::unique_ptr<TestGpuService> gpu_service_;
+  std::unique_ptr<HostGpuMemoryBufferManager> gpu_memory_buffer_manager_;
+
   DISALLOW_COPY_AND_ASSIGN(HostGpuMemoryBufferManagerTest);
 };
 
 // Tests that allocation requests from a client that goes away before allocation
 // completes are cleaned up correctly.
 TEST_F(HostGpuMemoryBufferManagerTest, AllocationRequestsForDestroyedClient) {
-  // Not all platforms support native configurations (currently only Windows,
-  // Mac and some Ozone platforms). Abort the test in those platforms.
-  bool native_pixmap_supported = false;
-#if defined(USE_OZONE)
-  native_pixmap_supported =
-      ui::OzonePlatform::GetInstance()->IsNativePixmapConfigSupported(
-          gfx::BufferFormat::RGBA_8888, gfx::BufferUsage::GPU_READ);
-#elif defined(OS_MACOSX) || defined(OS_WIN)
-  native_pixmap_supported = true;
-#endif
-
-  if (!native_pixmap_supported) {
-    gpu::GpuMemoryBufferSupport support;
-    DCHECK(gpu::GetNativeGpuMemoryBufferConfigurations(&support).empty());
+  if (!IsNativePixmapConfigSupported())
     return;
-  }
 
   // Note: HostGpuMemoryBufferManager normally operates on a mojom::GpuService
-  // implementation over mojo. Which means the communication from SGMBManager to
+  // implementation over mojo. Which means the communication from HGMBManager to
   // GpuService is asynchronous. In this test, the mojom::GpuService is not
   // bound to a mojo pipe, which means those calls are all synchronous.
-  TestGpuService gpu_service;
-  auto gpu_memory_buffer_support =
-      std::make_unique<gpu::GpuMemoryBufferSupport>();
-  HostGpuMemoryBufferManager manager(gpu_service.CreateProvider(), 1,
-                                     std::move(gpu_memory_buffer_support),
-                                     base::ThreadTaskRunnerHandle::Get());
 
   const auto buffer_id = static_cast<gfx::GpuMemoryBufferId>(1);
   const int client_id = 2;
   const gfx::Size size(10, 20);
   const gfx::BufferFormat format = gfx::BufferFormat::RGBA_8888;
   const gfx::BufferUsage usage = gfx::BufferUsage::GPU_READ;
-  manager.AllocateGpuMemoryBuffer(buffer_id, client_id, size, format, usage,
-                                  gpu::kNullSurfaceHandle, base::DoNothing());
-  EXPECT_TRUE(gpu_service.HasAllocationRequest(buffer_id, client_id));
-  EXPECT_FALSE(gpu_service.HasDestructionRequest(buffer_id, client_id));
+  gpu_memory_buffer_manager()->AllocateGpuMemoryBuffer(
+      buffer_id, client_id, size, format, usage, gpu::kNullSurfaceHandle,
+      base::DoNothing());
+  EXPECT_EQ(1, gpu_service()->GetAllocationRequestsCount());
+  EXPECT_TRUE(gpu_service()->IsAllocationRequestAt(0, buffer_id, client_id));
+  EXPECT_EQ(0, gpu_service()->GetDestructionRequestsCount());
 
   // Destroy the client. Since no memory has been allocated yet, there will be
   // no request for freeing memory.
-  manager.DestroyAllGpuMemoryBufferForClient(client_id);
-  EXPECT_TRUE(gpu_service.HasAllocationRequest(buffer_id, client_id));
-  EXPECT_FALSE(gpu_service.HasDestructionRequest(buffer_id, client_id));
+  gpu_memory_buffer_manager()->DestroyAllGpuMemoryBufferForClient(client_id);
+  EXPECT_EQ(1, gpu_service()->GetAllocationRequestsCount());
+  EXPECT_EQ(0, gpu_service()->GetDestructionRequestsCount());
 
   // When the host receives the allocated memory for the destroyed client, it
   // should request the allocated memory to be freed.
-  gpu_service.SatisfyAllocationRequest(buffer_id, client_id);
-  EXPECT_TRUE(gpu_service.HasDestructionRequest(buffer_id, client_id));
+  gpu_service()->SatisfyAllocationRequestAt(0);
+  EXPECT_EQ(1, gpu_service()->GetAllocationRequestsCount());
+  EXPECT_EQ(1, gpu_service()->GetDestructionRequestsCount());
+  EXPECT_TRUE(gpu_service()->IsDestructionRequestAt(0, buffer_id, client_id));
 }
 
 TEST_F(HostGpuMemoryBufferManagerTest, RequestsFromUntrustedClientsValidated) {
-  TestGpuService gpu_service;
-  auto gpu_memory_buffer_support =
-      std::make_unique<gpu::GpuMemoryBufferSupport>();
-  HostGpuMemoryBufferManager manager(gpu_service.CreateProvider(), 1,
-                                     std::move(gpu_memory_buffer_support),
-                                     base::ThreadTaskRunnerHandle::Get());
   const auto buffer_id = static_cast<gfx::GpuMemoryBufferId>(1);
   const int client_id = 2;
   // SCANOUT cannot be used if native gpu memory buffer is not supported.
@@ -283,7 +308,7 @@ TEST_F(HostGpuMemoryBufferManagerTest, RequestsFromUntrustedClientsValidated) {
   for (const auto& config : configs) {
     gfx::GpuMemoryBufferHandle allocated_handle;
     base::RunLoop runloop;
-    manager.AllocateGpuMemoryBuffer(
+    gpu_memory_buffer_manager()->AllocateGpuMemoryBuffer(
         buffer_id, client_id, config.size, config.format, config.usage,
         gpu::kNullSurfaceHandle,
         base::BindOnce(
@@ -295,7 +320,7 @@ TEST_F(HostGpuMemoryBufferManagerTest, RequestsFromUntrustedClientsValidated) {
             &allocated_handle, runloop.QuitClosure()));
     // Since native gpu memory buffers are not supported, the mojom.GpuService
     // should not receive any allocation requests.
-    EXPECT_FALSE(gpu_service.HasAllocationRequest(buffer_id, client_id));
+    EXPECT_EQ(0, gpu_service()->GetAllocationRequestsCount());
     runloop.Run();
     if (config.expect_null_handle) {
       EXPECT_TRUE(allocated_handle.is_null());
@@ -308,32 +333,65 @@ TEST_F(HostGpuMemoryBufferManagerTest, RequestsFromUntrustedClientsValidated) {
 }
 
 TEST_F(HostGpuMemoryBufferManagerTest, GpuMemoryBufferDestroyed) {
-  TestGpuService gpu_service;
-  auto gpu_memory_buffer_support =
-      std::make_unique<gpu::GpuMemoryBufferSupport>();
-  HostGpuMemoryBufferManager manager(gpu_service.CreateProvider(), 1,
-                                     std::move(gpu_memory_buffer_support),
-                                     base::ThreadTaskRunnerHandle::Get());
-  auto buffer = AllocateGpuMemoryBufferSync(&manager);
+  auto buffer = AllocateGpuMemoryBufferSync();
   EXPECT_TRUE(buffer);
   buffer.reset();
 }
 
 TEST_F(HostGpuMemoryBufferManagerTest,
        GpuMemoryBufferDestroyedOnDifferentThread) {
-  TestGpuService gpu_service;
-  auto gpu_memory_buffer_support =
-      std::make_unique<gpu::GpuMemoryBufferSupport>();
-  HostGpuMemoryBufferManager manager(gpu_service.CreateProvider(), 1,
-                                     std::move(gpu_memory_buffer_support),
-                                     base::ThreadTaskRunnerHandle::Get());
-  auto buffer = AllocateGpuMemoryBufferSync(&manager);
+  auto buffer = AllocateGpuMemoryBufferSync();
   EXPECT_TRUE(buffer);
   // Destroy the buffer in a different thread.
   base::Thread diff_thread("DestroyThread");
   ASSERT_TRUE(diff_thread.Start());
   diff_thread.task_runner()->DeleteSoon(FROM_HERE, std::move(buffer));
   diff_thread.Stop();
+}
+
+// Tests that if an allocated buffer is received after the gpu service issuing
+// it has died, HGMBManager retries the allocation request properly.
+TEST_F(HostGpuMemoryBufferManagerTest, AllocationRequestFromDeadGpuService) {
+  if (!IsNativePixmapConfigSupported())
+    return;
+
+  // Request allocation. No allocation should happen yet.
+  gfx::GpuMemoryBufferHandle allocated_handle;
+  const auto buffer_id = static_cast<gfx::GpuMemoryBufferId>(1);
+  const int client_id = 2;
+  const gfx::Size size(10, 20);
+  const gfx::BufferFormat format = gfx::BufferFormat::RGBA_8888;
+  const gfx::BufferUsage usage = gfx::BufferUsage::GPU_READ;
+  gpu_memory_buffer_manager()->AllocateGpuMemoryBuffer(
+      buffer_id, client_id, size, format, usage, gpu::kNullSurfaceHandle,
+      base::BindOnce(
+          [](gfx::GpuMemoryBufferHandle* allocated_handle,
+             gfx::GpuMemoryBufferHandle handle) {
+            *allocated_handle = std::move(handle);
+          },
+          &allocated_handle));
+  EXPECT_EQ(1, gpu_service()->GetAllocationRequestsCount());
+  EXPECT_TRUE(gpu_service()->IsAllocationRequestAt(0, buffer_id, client_id));
+  EXPECT_TRUE(allocated_handle.is_null());
+
+  // Simulate a connection error from gpu. HGMBManager should retry allocation
+  // request.
+  gpu_service()->SimulateConnectionError();
+  EXPECT_EQ(2, gpu_service()->GetAllocationRequestsCount());
+  EXPECT_TRUE(gpu_service()->IsAllocationRequestAt(1, buffer_id, client_id));
+  EXPECT_TRUE(allocated_handle.is_null());
+
+  // Send an allocated buffer corresponding to the first request on the old gpu.
+  // This should not result in a buffer handle.
+  gpu_service()->SatisfyAllocationRequestAt(0);
+  EXPECT_EQ(2, gpu_service()->GetAllocationRequestsCount());
+  EXPECT_TRUE(allocated_handle.is_null());
+
+  // Send an allocated buffer corresponding to the retried request on the new
+  // gpu. This should result in a buffer handle.
+  gpu_service()->SatisfyAllocationRequestAt(1);
+  EXPECT_EQ(2, gpu_service()->GetAllocationRequestsCount());
+  EXPECT_FALSE(allocated_handle.is_null());
 }
 
 }  // namespace viz
