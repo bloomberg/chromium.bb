e66e896c9db6c2e758829e1058e2b948877ec426
Sam McNally <sammc@chromium.org>
2019-01-21 00:17:16 +0000

    Move Files app string fetching into a common location.
    The files app strings will soon be used while servicing requests for
    files app resources. To do so, the replacements will need to be
    accessible elsewhere. Extract the population of the unchanging strings
    (not affected by profile, enabled features etc.) into a common location.
    
    Bug: 923206
    Change-Id: I20897cd3903ee30bcc922c4652010b676f07df3d
    Reviewed-on: https://chromium-review.googlesource.com/c/1420511
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624497}
    Former-commit-id: 769fdf3e274e1d4663711cc96373ca5fb776e1ae

--- ui/file_manager/file_manager/test/scripts/create_test_main.py
+++ ui/file_manager/file_manager/test/scripts/create_test_main.py
@@ -175,8 +175,8 @@ for grdp in grdp_files:
       for ex in ph.findall('ex'):
         ph.remove(ex)
     resource_bundle[msg.attrib['name']] = ''.join(msg.itertext()).strip()
-private_api_strings = read('../../../chrome/browser/chromeos/extensions/'
-                           'file_manager/private_api_strings.cc')
+private_api_strings = read('../../../chrome/browser/chromeos/'
+                           'file_manager/file_manager_string_util.cc')
 for m in re.finditer(r'SET_STRING\(\"(.*?)\",\s+(\w+)\);', private_api_strings):
   strings[m.group(1)] = resource_bundle.get(m.group(2), m.group(2))
 
