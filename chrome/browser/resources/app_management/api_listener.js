a5801c17db2bc5bfd5b03104f6fe5fb315c16564
Cecilia Ni <ceciliani@google.com>
2019-01-21 12:24:39 +0000

    [App Management] UI Fix-up For High Priority Issue
    This CL fixes the most pressing issues of current UI layout:
    	1. Permission item doesn't have icons.
    	2. Permission item doesn't match mocks.
    	3. Line height is not correct, and it should be 1.54.
    	4. Font weight of header text is not right, and it should be 500.
    
    Bug: 906508
    Change-Id: I8c22cc3cd8175b2c61b335421a088f3702adaf1f
    Reviewed-on: https://chromium-review.googlesource.com/c/1411192
    Commit-Queue: Cecilia Ni <ceciliani@google.com>
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624564}
    Former-commit-id: 4507712ec42fdd91c0ef9bacbe092fce8a108f5d

--- chrome/browser/resources/app_management/api_listener.js
+++ chrome/browser/resources/app_management/api_listener.js
@@ -1,7 +1,7 @@
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-cr.define('app_management.ApiListener', function() {
+cr.define('app_management.apiListener', function() {
   let initialized = false;
 
   async function init() {
@@ -51,4 +51,6 @@ cr.define('app_management.ApiListener', function() {
   }
 
   init();
+
+  return {};
 });
