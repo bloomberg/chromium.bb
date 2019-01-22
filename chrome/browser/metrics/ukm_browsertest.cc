3545f9a91d38329d8b317ff98343039ba79236c9
Kyle Milka <kmilka@chromium.org>
2019-01-22 17:37:29 +0000

    Revert "Disable UkmBrowserTest.LogsTabId on ChromeOS"
    This reverts commit 3acf02e8677b70ca5d6119a577a46b3c35adef2c [formerly 1bcd3ed173a9aeea913a31b49036d6f148c72476].
    
    Reason for revert: reverted crrev.com/c/1426879, re-enabling this test
    
    Original change's description:
    > Disable UkmBrowserTest.LogsTabId on ChromeOS
    > 
    > No clue has found yet, while most suspected CLs have been once
    > reverted but relanded. Suppressing to reduce noise on builders.
    > 
    > TBR=gab@chromium.org
    > 
    > Bug: 923894
    > Change-Id: Ida043df3d46a73507ca1b31686d7497bc53d00ed
    > Reviewed-on: https://chromium-review.googlesource.com/c/1426206
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Commit-Queue: Kinuko Yasuda <kinuko@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#624735}
    
    TBR=kinuko@chromium.org,gab@chromium.org
    
    Change-Id: I10db608f00c9701fc14b4b97104c1b4ac802f607
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 923894
    Reviewed-on: https://chromium-review.googlesource.com/c/1426999
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624818}
    Former-commit-id: 646c49ba49591526ab3faeae18d7fbc3f70c2eca

--- chrome/browser/metrics/ukm_browsertest.cc
+++ chrome/browser/metrics/ukm_browsertest.cc
@@ -818,13 +818,7 @@ IN_PROC_BROWSER_TEST_P(UkmBrowserTest, MultiDisableExtensionsSyncCheck) {
   CloseBrowserSynchronously(browser1);
 }
 
-#if defined(OS_CHROMEOS)
-// Failing on multiple bots for ChromeOS. https://crbug.com/923894
-#define MAYBE_LogsTabId DISABLED_LogsTabId
-#else
-#define MAYBE_LogsTabId LogsTabId
-#endif
-IN_PROC_BROWSER_TEST_P(UkmBrowserTest, MAYBE_LogsTabId) {
+IN_PROC_BROWSER_TEST_P(UkmBrowserTest, LogsTabId) {
   ASSERT_TRUE(embedded_test_server()->Start());
   MetricsConsentOverride metrics_consent(true);
   Profile* profile = ProfileManager::GetActiveUserProfile();
