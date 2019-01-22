e9b5edd21a32aba8703248651f9257170342eff2
Paul Dyson <pdyson@chromium.org>
2019-01-22 03:18:21 +0000

    Add Arc++ logging; add logging of installed Chrome apps.
    For Chrome apps, use the list of installed apps instead of a
    whitelist.
    
    Only logs:
    * Chrome apps from the Chrome Store
    * Arc++ apps with sync enabled
    * PWAs on a whitelist
    
    This is logging for TopCat.
    
    Bug: 899123
    Change-Id: I5c3056224fe5bfb2eb6956b0ec8c36549d93f426
    Reviewed-on: https://chromium-review.googlesource.com/c/1379600
    Commit-Queue: Paul Dyson <pdyson@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Charles . <charleszhao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624717}
    Former-commit-id: 168ddefa92f7efee346aa5bc547da0c5295fd2cf

--- chrome/browser/ui/app_list/app_launch_event_logger.cc
+++ chrome/browser/ui/app_list/app_launch_event_logger.cc
@@ -8,13 +8,17 @@
 #include "base/feature_list.h"
 #include "base/macros.h"
 #include "base/metrics/histogram_macros.h"
-#include "base/no_destructor.h"
 #include "base/strings/string_util.h"
 #include "base/task/post_task.h"
 #include "base/task/task_traits.h"
 #include "base/time/time.h"
-#include "chrome/browser/ui/app_list/search/chrome_search_result.h"
+#include "chrome/browser/metrics/chrome_metrics_service_client.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_manager.h"
+#include "components/arc/arc_prefs.h"
+#include "components/prefs/pref_service.h"
 #include "components/ukm/app_source_url_recorder.h"
+#include "extensions/common/extension.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 #include "services/metrics/public/cpp/ukm_recorder.h"
 #include "url/gurl.h"
@@ -23,33 +27,16 @@ namespace app_list {
 
 const base::Feature kUkmAppLaunchEventLogging{"UkmAppLaunchEventLogging",
                                               base::FEATURE_ENABLED_BY_DEFAULT};
+
+// Keys for Arc app specific preferences. Defined in
+// chrome/browser/ui/app_list/arc/arc_app_list_prefs.cc.
+const char AppLaunchEventLogger::kPackageName[] = "package_name";
+const char AppLaunchEventLogger::kShouldSync[] = "should_sync";
+
 namespace {
 
+const char kArcScheme[] = "arc://";
 const char kExtensionSchemeWithDelimiter[] = "chrome-extension://";
-
-AppLaunchEvent GetAppLaunchEvent(const ChromeSearchResult* result) {
-  AppLaunchEvent event;
-  event.set_launched_from(AppLaunchEvent_LaunchedFrom_SUGGESTED);
-  std::string id = result->id();
-  // For chrome apps, result->id() returns "chrome-extension://|id|/" instead
-  // of just |id|, so remove any leading "chrome-extension://" and trailing "/".
-  if (!id.compare(0, strlen(kExtensionSchemeWithDelimiter),
-                  kExtensionSchemeWithDelimiter)) {
-    id.erase(0, strlen(kExtensionSchemeWithDelimiter));
-  }
-  if (id.size() && !id.compare(id.size() - 1, 1, "/")) {
-    id.pop_back();
-  }
-  event.set_app_id(id);
-  return event;
-}
-
-AppLaunchEvent GetAppLaunchEvent(const std::string& id) {
-  AppLaunchEvent event;
-  event.set_launched_from(AppLaunchEvent_LaunchedFrom_GRID);
-  event.set_app_id(id);
-  return event;
-}
 
 int HourOfDay(base::Time time) {
   base::Time::Exploded exploded;
@@ -66,151 +53,63 @@ int DayOfWeek(base::Time time) {
 }  // namespace
 
 AppLaunchEventLogger::AppLaunchEventLogger() : weak_factory_(this) {
-  CreateTaskRunner();
-  PopulateIdAppTypeMap();
+  task_runner_ = base::CreateSequencedTaskRunnerWithTraits(
+      {base::TaskPriority::BEST_EFFORT,
+       base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN});
   PopulatePwaIdUrlMap();
 }
 
 AppLaunchEventLogger::~AppLaunchEventLogger() {}
 
-void AppLaunchEventLogger::CreateTaskRunner() {
-  task_runner_ = base::CreateSequencedTaskRunnerWithTraits(
-      {base::TaskPriority::BEST_EFFORT,
-       base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN});
-}
-
-void AppLaunchEventLogger::OnSuggestionChipClicked(
-    const ChromeSearchResult* result) {
-  if (base::FeatureList::IsEnabled(kUkmAppLaunchEventLogging)) {
-    task_runner_->PostTask(
-        FROM_HERE,
-        base::BindOnce(&AppLaunchEventLogger::OnAppLaunched,
-                       weak_factory_.GetWeakPtr(), GetAppLaunchEvent(result)));
+void AppLaunchEventLogger::OnSuggestionChipClicked(const std::string& id) {
+  if (!base::FeatureList::IsEnabled(kUkmAppLaunchEventLogging)) {
+    return;
   }
+  AppLaunchEvent event;
+  event.set_launched_from(AppLaunchEvent_LaunchedFrom_SUGGESTED);
+  event.set_app_id(RemoveScheme(id));
+  SetAppInfo(&event);
+  task_runner_->PostTask(FROM_HERE,
+                         base::BindOnce(&AppLaunchEventLogger::Log,
+                                        weak_factory_.GetWeakPtr(), event));
 }
 
 void AppLaunchEventLogger::OnGridClicked(const std::string& id) {
-  if (base::FeatureList::IsEnabled(kUkmAppLaunchEventLogging)) {
-    task_runner_->PostTask(
-        FROM_HERE,
-        base::BindOnce(&AppLaunchEventLogger::OnAppLaunched,
-                       weak_factory_.GetWeakPtr(), GetAppLaunchEvent(id)));
-  }
-}
-
-AppLaunchEventLogger& AppLaunchEventLogger::GetInstance() {
-  static base::NoDestructor<AppLaunchEventLogger> instance;
-  return *instance;
-}
-
-void AppLaunchEventLogger::OnAppLaunched(AppLaunchEvent app_launch_event) {
-  AppLaunchEvent_AppType app_type(GetAppType(app_launch_event.app_id()));
-  UMA_HISTOGRAM_ENUMERATION("Apps.AppListAppTypeClicked", app_type,
-                            AppLaunchEvent_AppType_AppType_ARRAYSIZE);
-
-  ukm::SourceId source_id = 0;
-  if (app_type == AppLaunchEvent_AppType_CHROME) {
-    source_id = ukm::AppSourceUrlRecorder::GetSourceIdForChromeApp(
-        app_launch_event.app_id());
-  } else if (app_type == AppLaunchEvent_AppType_PWA) {
-    source_id = ukm::AppSourceUrlRecorder::GetSourceIdForPWA(
-        GURL(GetPwaUrl(app_launch_event.app_id())));
-  } else {
-    // TODO(pdyson): Handle Play apps.
+  if (!base::FeatureList::IsEnabled(kUkmAppLaunchEventLogging)) {
     return;
   }
-
-  ukm::builders::AppListAppLaunch app_launch(source_id);
-  base::Time now(base::Time::Now());
-  app_launch.SetAppType(app_type)
-      .SetLaunchedFrom(app_launch_event.launched_from())
-      .SetDayOfWeek(DayOfWeek(now))
-      .SetHourOfDay(HourOfDay(now))
-      .Record(ukm::UkmRecorder::Get());
+  AppLaunchEvent event;
+  event.set_launched_from(AppLaunchEvent_LaunchedFrom_GRID);
+  event.set_app_id(id);
+  SetAppInfo(&event);
+  task_runner_->PostTask(FROM_HERE,
+                         base::BindOnce(&AppLaunchEventLogger::Log,
+                                        weak_factory_.GetWeakPtr(), event));
 }
 
-void AppLaunchEventLogger::PopulateIdAppTypeMap() {
-  id_app_type_map_ = base::flat_map<std::string, AppLaunchEvent_AppType>{
-      // These apps are taken from extensions/common/constants.cc. They do not
-      // include the extensions in that file, only the apps.
-      // Camera
-      {"hfhhnacclhffhdffklopdkcgdhifgngh", AppLaunchEvent_AppType_CHROME},
-      // Chrome
-      {"mgndgikekgjfcpckkfioiadnlibdjbkf", AppLaunchEvent_AppType_CHROME},
-      // FilesManager
-      {"hhaomjibdihmijegdhdafkllkbggdgoj", AppLaunchEvent_AppType_CHROME},
-      // GoogleKeep
-      {"hmjkmjkepdijhoojdojkdfohbdgmmhki", AppLaunchEvent_AppType_CHROME},
-      // Youtube
-      {"blpcfgokakmgnkcojhhkbfbldkacnbeo", AppLaunchEvent_AppType_CHROME},
-      // Genius
-      {"ljoammodoonkhnehlncldjelhidljdpi", AppLaunchEvent_AppType_CHROME},
-      // Highlights
-      {"lpmakjfjcconjeehbidjclhdlpjmfjjj", AppLaunchEvent_AppType_CHROME},
-      // HighlightsAlt1
-      {"iggildboghmjpbjcpmobahnkmoefkike", AppLaunchEvent_AppType_CHROME},
-      // HighlightsAlt2
-      {"elhbopodaklenjkeihkdhhfaghalllba", AppLaunchEvent_AppType_CHROME},
-      // Screensaver
-      {"mnoijifedipmbjaoekhadjcijipaijjc", AppLaunchEvent_AppType_CHROME},
-      // ScreensaverAlt1
-      {"gdobaoeekhiklaljmhladjfdfkigampc", AppLaunchEvent_AppType_CHROME},
-      // ScreensaverAlt2
-      {"lminefdanffajachfahfpmphfkhahcnj", AppLaunchEvent_AppType_CHROME},
+void AppLaunchEventLogger::SetAppDataForTesting(
+    extensions::ExtensionRegistry* registry,
+    base::DictionaryValue* arc_apps,
+    base::DictionaryValue* arc_packages) {
+  testing_ = true;
+  registry_ = registry;
+  arc_apps_ = arc_apps;
+  arc_packages_ = arc_packages;
+}
 
-      // These apps are taken from
-      // chrome/common/extensions/extension_constants.cc. They do not include
-      // the extensions in that file, only the apps.
-      // Calculator
-      {"joodangkbfjnajiiifokapkpmhfnpleo", AppLaunchEvent_AppType_CHROME},
-      // Calendar
-      {"ejjicmeblgpmajnghnpcppodonldlgfn", AppLaunchEvent_AppType_CHROME},
-      // ChromeRemoteDesktop
-      {"gbchcmhmhahfdphkhkmpfmihenigjmpp", AppLaunchEvent_AppType_CHROME},
-      // CloudPrint
-      {"mfehgcgbbipciphmccgaenjidiccnmng", AppLaunchEvent_AppType_CHROME},
-      // DriveHosted
-      {"apdfllckaahabafndbhieahigkjlhalf", AppLaunchEvent_AppType_CHROME},
-      // EnterpriseWebStore
-      {"afchcafgojfnemjkcbhfekplkmjaldaa", AppLaunchEvent_AppType_CHROME},
-      // Gmail
-      {"pjkljhegncpnkpknbcohdijeoejaedia", AppLaunchEvent_AppType_CHROME},
-      // GoogleDoc
-      {"aohghmighlieiainnegkcijnfilokake", AppLaunchEvent_AppType_CHROME},
-      // GoogleMaps
-      {"lneaknkopdijkpnocmklfnjbeapigfbh", AppLaunchEvent_AppType_CHROME},
-      // GooglePhotos
-      {"hcglmfcclpfgljeaiahehebeoaiicbko", AppLaunchEvent_AppType_CHROME},
-      // GooglePlayBooks
-      {"mmimngoggfoobjdlefbcabngfnmieonb", AppLaunchEvent_AppType_CHROME},
-      // GooglePlayMovies
-      {"gdijeikdkaembjbdobgfkoidjkpbmlkd", AppLaunchEvent_AppType_CHROME},
-      // GooglePlayMusic
-      {"icppfcnhkcmnfdhfhphakoifcfokfdhg", AppLaunchEvent_AppType_CHROME},
-      // GooglePlus
-      {"dlppkpafhbajpcmmoheippocdidnckmm", AppLaunchEvent_AppType_CHROME},
-      // GoogleSheets
-      {"felcaaldnbdncclmgdcncolpebgiejap", AppLaunchEvent_AppType_CHROME},
-      // GoogleSlides
-      {"aapocclcgogkmnckokdopfmhonfmgoek", AppLaunchEvent_AppType_CHROME},
-      // HTerm
-      {"pnhechapfaindjhompbnflcldabbghjo", AppLaunchEvent_AppType_CHROME},
-      // HTermDev
-      {"okddffdblfhhnmhodogpojmfkjmhinfp", AppLaunchEvent_AppType_CHROME},
-      // IdentityApiUi
-      {"ahjaciijnoiaklcomgnblndopackapon", AppLaunchEvent_AppType_CHROME},
-      // CroshBuiltin
-      {"nkoccljplnhpfnfiajclkommnmllphnl", AppLaunchEvent_AppType_CHROME},
-      // TextEditor
-      {"mmfbcljfglbokpmkimbfghdkjmjhdgbg", AppLaunchEvent_AppType_CHROME},
-      // InAppPaymentsSupport
-      {"nmmhkkegccagdldgiimedpiccmgmieda", AppLaunchEvent_AppType_CHROME},
-
-      // Other
-      // Google+
-      {"dcdbodpaldbchkfinnjphocleggfceip", AppLaunchEvent_AppType_PWA},
-      // Photos
-      {"ncmjhecbjeaamljdfahankockkkdmedg", AppLaunchEvent_AppType_PWA}};
+std::string AppLaunchEventLogger::RemoveScheme(const std ::string& id) {
+  std::string app_id(id);
+  if (!app_id.compare(0, strlen(kExtensionSchemeWithDelimiter),
+                      kExtensionSchemeWithDelimiter)) {
+    app_id.erase(0, strlen(kExtensionSchemeWithDelimiter));
+  }
+  if (!app_id.compare(0, strlen(kArcScheme), kArcScheme)) {
+    app_id.erase(0, strlen(kArcScheme));
+  }
+  if (app_id.size() && !app_id.compare(app_id.size() - 1, 1, "/")) {
+    app_id.pop_back();
+  }
+  return app_id;
 }
 
 void AppLaunchEventLogger::PopulatePwaIdUrlMap() {
@@ -221,20 +120,121 @@ void AppLaunchEventLogger::PopulatePwaIdUrlMap() {
       {"ncmjhecbjeaamljdfahankockkkdmedg", "https://photos.google.com/"}};
 }
 
-AppLaunchEvent_AppType AppLaunchEventLogger::GetAppType(const std::string& id) {
-  auto search = id_app_type_map_.find(id);
-  if (search != id_app_type_map_.end()) {
-    return search->second;
-  }
-  return AppLaunchEvent_AppType_OTHER;
-}
-
 const std::string& AppLaunchEventLogger::GetPwaUrl(const std::string& id) {
   auto search = pwa_id_url_map_.find(id);
   if (search != pwa_id_url_map_.end()) {
     return search->second;
   }
   return base::EmptyString();
+}
+
+void AppLaunchEventLogger::SetAppInfo(AppLaunchEvent* event) {
+  LoadInstalledAppInformation();
+  if (IsChromeAppFromWebstore(event->app_id())) {
+    event->set_app_type(AppLaunchEvent_AppType_CHROME);
+    return;
+  }
+  const std::string& pwa_url = GetPwaUrl(event->app_id());
+  if (pwa_url != base::EmptyString()) {
+    event->set_app_type(AppLaunchEvent_AppType_PWA);
+    event->set_pwa_url(pwa_url);
+    return;
+  }
+  const std::string& arc_package_name(GetSyncedArcPackage(event->app_id()));
+  if (arc_package_name != base::EmptyString()) {
+    event->set_app_type(AppLaunchEvent_AppType_PLAY);
+    event->set_arc_package_name(arc_package_name);
+    return;
+  }
+  event->set_app_type(AppLaunchEvent_AppType_OTHER);
+}
+
+void AppLaunchEventLogger::LoadInstalledAppInformation() {
+  // Tests provide installed app information, so don't overwrite that.
+  if (testing_)
+    return;
+
+  Profile* profile = ProfileManager::GetLastUsedProfile();
+  if (!profile) {
+    LOG(DFATAL) << "No profile";
+    return;
+  }
+  registry_ = extensions::ExtensionRegistry::Get(profile);
+
+  PrefService* pref_service = profile->GetPrefs();
+  if (pref_service) {
+    arc_apps_ = pref_service->GetDictionary(arc::prefs::kArcApps);
+    arc_packages_ = pref_service->GetDictionary(arc::prefs::kArcPackages);
+  }
+}
+
+std::string AppLaunchEventLogger::GetSyncedArcPackage(const std::string& id) {
+  if (!arc_apps_ || !arc_packages_)
+    return base::EmptyString();
+
+  const base::Value* app = arc_apps_->FindKey(id);
+  if (!app) {
+    // App with |id| is not in the list of installed Arc apps.
+    return base::EmptyString();
+  }
+  const base::Value* package_name_value = app->FindKey(kPackageName);
+  if (!package_name_value) {
+    return base::EmptyString();
+  }
+  const base::Value* package =
+      arc_packages_->FindKey(package_name_value->GetString());
+  if (!package) {
+    return base::EmptyString();
+  }
+  if (!package->FindKey(kShouldSync)->GetBool()) {
+    return base::EmptyString();
+  }
+  return package_name_value->GetString();
+}
+
+bool AppLaunchEventLogger::IsChromeAppFromWebstore(const std::string& id) {
+  if (!registry_)
+    return false;
+  const extensions::Extension* extension =
+      registry_->GetExtensionById(id, extensions::ExtensionRegistry::ENABLED);
+  if (!extension) {
+    // App with |id| is not in the list of Chrome extensions.
+    return false;
+  }
+  return extension->from_webstore();
+}
+
+void AppLaunchEventLogger::Log(AppLaunchEvent app_launch_event) {
+  UMA_HISTOGRAM_ENUMERATION("Apps.AppListAppTypeClicked",
+                            app_launch_event.app_type(),
+                            AppLaunchEvent_AppType_AppType_ARRAYSIZE);
+
+  ukm::SourceId source_id = 0;
+  // SetAppInfo performed the checks to enforce logging policy. Apps that are
+  // not to be logged were assigned an |app_type| of OTHER and so are not logged
+  // here.
+  if (app_launch_event.app_type() == AppLaunchEvent_AppType_CHROME) {
+    source_id = ukm::AppSourceUrlRecorder::GetSourceIdForChromeApp(
+        app_launch_event.app_id());
+  } else if (app_launch_event.app_type() == AppLaunchEvent_AppType_PWA) {
+    source_id = ukm::AppSourceUrlRecorder::GetSourceIdForPWA(
+        GURL(app_launch_event.pwa_url()));
+  } else if (app_launch_event.app_type() == AppLaunchEvent_AppType_PLAY) {
+    source_id = ukm::AppSourceUrlRecorder::GetSourceIdForArc(
+        app_launch_event.arc_package_name());
+  } else {
+    // Either app is Crostini; or Chrome but not in app store; or Arc but not
+    // syncable; or PWA but not in whitelist.
+    return;
+  }
+
+  ukm::builders::AppListAppLaunch app_launch(source_id);
+  base::Time now(base::Time::Now());
+  app_launch.SetAppType(app_launch_event.app_type())
+      .SetLaunchedFrom(app_launch_event.launched_from())
+      .SetDayOfWeek(DayOfWeek(now))
+      .SetHourOfDay(HourOfDay(now))
+      .Record(ukm::UkmRecorder::Get());
 }
 
 }  // namespace app_list
