ca0858703bf86e0e3ca249b8a14aab58cbfdfcb4
Nick Diego Yamane <nickdiego@igalia.com>
2019-01-22 00:05:40 +0000

    [ozone/wayland] Fix typo in WaylandDataSource
    - Rename 'UpdataDataMap' to 'UpdateDataMap'
    
    Change-Id: I05bb1f88b4f4af59a6c62ab8ec460323a24b3d7b
    Reviewed-on: https://chromium-review.googlesource.com/c/1425437
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Auto-Submit: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#624697}
    Former-commit-id: b2225c8da4715c5ccc49cf54880c12afbe2d2638

--- ui/ozone/platform/wayland/wayland_data_source.h
+++ ui/ozone/platform/wayland/wayland_data_source.h
@@ -43,7 +43,7 @@ class WaylandDataSource {
   }
 
   void WriteToClipboard(const PlatformClipboard::DataMap& data_map);
-  void UpdataDataMap(const PlatformClipboard::DataMap& data_map);
+  void UpdateDataMap(const PlatformClipboard::DataMap& data_map);
   void Offer(const ui::OSExchangeData& data);
   void SetAction(int operation);
   void SetDragData(const DragDataMap& data_map);
