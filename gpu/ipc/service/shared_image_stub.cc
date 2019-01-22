d7f67bba9c8d945c4f828564fc371f29c3fbcf04
John Abd-El-Malek <jam@chromium.org>
2019-01-22 23:58:05 +0000

    Revert "Add SharedImage identifying bit to gpu::Mailbox"
    This reverts commit d764fa0ac6c5ab1f4013bb952c91a5f99b05c991 [formerly e4f5071361d8ccc668599d9a47afa8c4b0c7faeb].
    
    Causing failures on a bunch of builders: https://sheriff-o-matic.appspot.com/chromium/examine/gpu_unittests%20failing%20on%20multiple%20builders
    
    Original change's description:
    > Add SharedImage identifying bit to gpu::Mailbox
    > 
    > As we transition to SharedImage, certain places in code need to know
    > whether a mailbox they receive should be used with the SharedImage
    > system or legacy mailbox system.
    > 
    > This CL adds a flag to Mailbox to identify SharedImage usage, and
    > converts existing clients to check this flag.
    > 
    > Bug: 922834
    > Change-Id: I20900e302e2620d216377edc5d6f0017270c23d6
    > Reviewed-on: https://chromium-review.googlesource.com/c/1416836
    > Commit-Queue: Eric Karl <ericrk@chromium.org>
    > Reviewed-by: Antoine Labour <piman@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#624883}
    
    TBR=piman@chromium.org,ericrk@chromium.org
    
    Change-Id: I2126a4063cad202ddaa06c48b5fc8d1537f41d04
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 922834
    Reviewed-on: https://chromium-review.googlesource.com/c/1428097
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624993}
    Former-commit-id: 4e07ff5b89571cc76e6fd2f2cb0aecc08ac8ce48

--- gpu/ipc/service/shared_image_stub.cc
+++ gpu/ipc/service/shared_image_stub.cc
@@ -64,13 +64,6 @@ void SharedImageStub::OnCreateSharedImage(
     const GpuChannelMsg_CreateSharedImage_Params& params) {
   TRACE_EVENT2("gpu", "SharedImageStub::OnCreateSharedImage", "width",
                params.size.width(), "height", params.size.height());
-  if (!params.mailbox.IsSharedImage()) {
-    LOG(ERROR) << "SharedImageStub: Trying to create a SharedImage with a "
-                  "non-SharedImage mailbox.";
-    OnError();
-    return;
-  }
-
   if (!MakeContextCurrentAndCreateFactory()) {
     OnError();
     return;
@@ -95,13 +88,6 @@ void SharedImageStub::OnCreateSharedImageWithData(
     const GpuChannelMsg_CreateSharedImageWithData_Params& params) {
   TRACE_EVENT2("gpu", "SharedImageStub::OnCreateSharedImageWithData", "width",
                params.size.width(), "height", params.size.height());
-  if (!params.mailbox.IsSharedImage()) {
-    LOG(ERROR) << "SharedImageStub: Trying to create a SharedImage with a "
-                  "non-SharedImage mailbox.";
-    OnError();
-    return;
-  }
-
   if (!MakeContextCurrentAndCreateFactory()) {
     OnError();
     return;
@@ -153,13 +139,6 @@ void SharedImageStub::OnCreateGMBSharedImage(
     GpuChannelMsg_CreateGMBSharedImage_Params params) {
   TRACE_EVENT2("gpu", "SharedImageStub::OnCreateSharedImage", "width",
                params.size.width(), "height", params.size.height());
-  if (!params.mailbox.IsSharedImage()) {
-    LOG(ERROR) << "SharedImageStub: Trying to create a SharedImage with a "
-                  "non-SharedImage mailbox.";
-    OnError();
-    return;
-  }
-
   if (!MakeContextCurrentAndCreateFactory()) {
     OnError();
     return;
@@ -187,13 +166,6 @@ void SharedImageStub::OnCreateGMBSharedImage(
 void SharedImageStub::OnUpdateSharedImage(const Mailbox& mailbox,
                                           uint32_t release_id) {
   TRACE_EVENT0("gpu", "SharedImageStub::OnDestroySharedImage");
-  if (!mailbox.IsSharedImage()) {
-    LOG(ERROR) << "SharedImageStub: Trying to access a SharedImage with a "
-                  "non-SharedImage mailbox.";
-    OnError();
-    return;
-  }
-
   if (!MakeContextCurrentAndCreateFactory()) {
     OnError();
     return;
@@ -215,13 +187,6 @@ void SharedImageStub::OnUpdateSharedImage(const Mailbox& mailbox,
 
 void SharedImageStub::OnDestroySharedImage(const Mailbox& mailbox) {
   TRACE_EVENT0("gpu", "SharedImageStub::OnDestroySharedImage");
-  if (!mailbox.IsSharedImage()) {
-    LOG(ERROR) << "SharedImageStub: Trying to destroy a SharedImage with a "
-                  "non-SharedImage mailbox.";
-    OnError();
-    return;
-  }
-
   if (!MakeContextCurrentAndCreateFactory()) {
     OnError();
     return;
