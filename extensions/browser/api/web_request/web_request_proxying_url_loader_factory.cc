626f14cf240c7b0b10fa974e1f5316de16168ef8
Clark DuVall <cduvall@chromium.org>
2019-01-22 17:47:58 +0000

    Clear request body on redirect if new method is GET in webRequest proxy
    This is similar to what is done in CorsURLLoader:
    https://cs.chromium.org/chromium/src/services/network/cors/cors_url_loader.cc?l=138&rcl=04c9a69cbd34ded270dfee3073a7746cd3d346f4 [formerly cc2c755f47023edd66e89d836e467e505d124320]
    
    Bug: 923622
    Change-Id: I26e15c250b08a0fa0c1f6fcdd1ceb21b776cae89
    Reviewed-on: https://chromium-review.googlesource.com/c/1423919
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#624454}(cherry picked from commit 946155cce85e00635ba1722471f5c6cbf99ca75e [formerly 4922fd55ae6ed13071fc457529eb87f61ef8f25b])
    Reviewed-on: https://chromium-review.googlesource.com/c/1427019
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3626@{#752}
    Cr-Branched-From: 5302c6863bade38a5d283a7cce26a309d8e8e755 [formerly d897fb137fbaaa9355c0c93124cc048824eb1e65]-refs/heads/master@{#612437}
    Former-commit-id: d2893bb53a0bd5a0829d21b630d3a3266ede8037

--- extensions/browser/api/web_request/web_request_proxying_url_loader_factory.cc
+++ extensions/browser/api/web_request/web_request_proxying_url_loader_factory.cc
@@ -591,6 +591,12 @@ void WebRequestProxyingURLLoaderFactory::InProgressRequest::
   request_.site_for_cookies = redirect_info.new_site_for_cookies;
   request_.referrer = GURL(redirect_info.new_referrer);
   request_.referrer_policy = redirect_info.new_referrer_policy;
+
+  // The request method can be changed to "GET". In this case we need to
+  // reset the request body manually.
+  if (request_.method == net::HttpRequestHeaders::kGetMethod)
+    request_.request_body = nullptr;
+
   request_completed_ = true;
 }
 
