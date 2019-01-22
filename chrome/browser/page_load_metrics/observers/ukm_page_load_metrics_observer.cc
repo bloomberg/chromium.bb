e6cb05f6c471203fabec2f002300b6b4fcc26468
Annie Sullivan <sullivan@chromium.org>
2019-01-22 23:23:51 +0000

    Add a UserInitiated boolean to PageLoad UKM
    This counts a load as user-initiated if it's started in the browser, or
    it's started in the renderer and we can tie it back to the original
    input event. There are some notable missing cases in the renderer, like
    back/forward and reload.
    
    Bug: 921710
    Change-Id: Ibc034fbfa077a73148c9cc93032e8dc4a57dae5f
    Reviewed-on: https://chromium-review.googlesource.com/c/1423110
    Commit-Queue: Annie Sullivan <sullivan@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#624982}
    Former-commit-id: 496d10a4755317cfeac6349bf88d6270416c37e3

--- chrome/browser/page_load_metrics/observers/ukm_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/ukm_page_load_metrics_observer.cc
@@ -161,6 +161,16 @@ void UkmPageLoadMetricsObserver::RecordTimingMetrics(
     const page_load_metrics::mojom::PageLoadTiming& timing,
     const page_load_metrics::PageLoadExtraInfo& info) {
   ukm::builders::PageLoad builder(info.source_id);
+  bool is_user_initiated_navigation =
+      // All browser initiated page loads are user-initiated.
+      info.user_initiated_info.browser_initiated ||
+
+      // Renderer-initiated navigations are user-initiated if there is an
+      // associated input timestamp.
+      timing.input_to_navigation_start;
+
+  builder.SetExperimental_Navigation_UserInitiated(
+      is_user_initiated_navigation);
   if (timing.input_to_navigation_start) {
     builder.SetExperimental_InputToNavigationStart(
         timing.input_to_navigation_start.value().InMilliseconds());
