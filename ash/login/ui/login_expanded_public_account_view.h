1efafee41fc7e603ca8be4f9edb9fd5e06130176
Jun Mukai <mukai@chromium.org>
2019-01-23 19:59:53 +0000

    Fix the crash of View::OnTouchEvent in the login screen
    LoginExpandedPublicAccountView is a view subclass but also
    a pretarget handler of Shell. This is not great since any of
    touch events will invoke the final method View::OnTouchEvent().
    
    This CL creates an event handler for this purpose rather than
    using a view directly as a pretarget handler.
    
    I expanded the test cases to examine both mouse events and touches,
    and confirmed it causes the reported crashes with touches without
    this fix.
    
    Bug: 908890
    Test: the new test cases
    Change-Id: I45d7de7da1eca8f874f5774971a085af8f72c9eb
    Reviewed-on: https://chromium-review.googlesource.com/c/1428607
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625315}
    Former-commit-id: 0bc05783e856eb6e11e4fbf69a53df413b13e1a1

--- ash/login/ui/login_expanded_public_account_view.h
+++ ash/login/ui/login_expanded_public_account_view.h
@@ -5,6 +5,8 @@
 #ifndef ASH_LOGIN_UI_LOGIN_EXPANDED_PUBLIC_ACCOUNT_VIEW_H_
 #define ASH_LOGIN_UI_LOGIN_EXPANDED_PUBLIC_ACCOUNT_VIEW_H_
 
+#include <memory>
+
 #include "ash/ash_export.h"
 #include "ash/login/ui/login_menu_view.h"
 #include "ash/login/ui/non_accessible_view.h"
@@ -65,8 +67,6 @@ class ASH_EXPORT LoginExpandedPublicAccountView : public NonAccessibleView {
   void OnPaint(gfx::Canvas* canvas) override;
 
   // ui::EventHandler:
-  void OnMouseEvent(ui::MouseEvent* event) override;
-  void OnGestureEvent(ui::GestureEvent* event) override;
   void OnKeyEvent(ui::KeyEvent* event) override;
 
  private:
@@ -74,6 +74,7 @@ class ASH_EXPORT LoginExpandedPublicAccountView : public NonAccessibleView {
   RightPaneView* right_pane_ = nullptr;
   OnPublicSessionViewDismissed on_dismissed_;
   PublicAccountWarningDialog* warning_dialog_ = nullptr;
+  std::unique_ptr<ui::EventHandler> event_handler_;
 
   base::WeakPtrFactory<LoginExpandedPublicAccountView> weak_factory_{this};
 
