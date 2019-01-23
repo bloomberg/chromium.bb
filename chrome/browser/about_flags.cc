970512f9b33b2f6d3a78e1b9aafdf11fa0b0eb1a
Mitsuru Oshima <oshima@chromium.org>
2019-01-23 21:32:05 +0000

    Remove ash-enable-cursor-motion-blur from about:flags
    Bug: 924390
    Test: None
    Change-Id: I2aa1f8dc832bfbc1a7ce8ff1154adedac55a2af1
    Reviewed-on: https://chromium-review.googlesource.com/c/1429059
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625366}
    Former-commit-id: 2b7906e547cab4cd26ec472edb5005e4bf0140df

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1518,10 +1518,6 @@ const FeatureEntry kFeatureEntries[] = {
      SINGLE_VALUE_TYPE_AND_VALUE(switches::kAllowNaClSocketAPI, "*")},
 #endif  // ENABLE_PLUGINS
 #if defined(OS_CHROMEOS)
-    {"ash-enable-cursor-motion-blur",
-     flag_descriptions::kEnableCursorMotionBlurName,
-     flag_descriptions::kEnableCursorMotionBlurDescription, kOsCrOS,
-     SINGLE_VALUE_TYPE(ash::switches::kAshEnableCursorMotionBlur)},
     {"ash-enable-docked-magnifier",
      flag_descriptions::kEnableDockedMagnifierName,
      flag_descriptions::kEnableDockedMagnifierDescription, kOsCrOS,
