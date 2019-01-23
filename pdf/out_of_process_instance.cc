80d6939e7dd33f328682b8443146b331f040b1b2
Lei Zhang <thestig@chromium.org>
2019-01-23 07:03:23 +0000

    Add a unit test for PDF a11y info extraction.
    Move most of OutOfProcessInstance::SendNextAccessibilityPage() out into
    its own function. Then unit test it.
    
    Change-Id: I54590ac004f11d918ccb45a3e071d9cf19a927ed
    Reviewed-on: https://chromium-review.googlesource.com/c/1407265
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625117}
    Former-commit-id: a294e769337518323d11e84afa909276c958c04c

--- pdf/out_of_process_instance.cc
+++ pdf/out_of_process_instance.cc
@@ -21,6 +21,7 @@
 #include "base/values.h"
 #include "chrome/common/content_restriction.h"
 #include "net/base/escape.h"
+#include "pdf/accessibility.h"
 #include "pdf/pdf.h"
 #include "pdf/pdf_features.h"
 #include "ppapi/c/dev/ppb_cursor_control_dev.h"
@@ -936,61 +937,14 @@ void OutOfProcessInstance::LoadAccessibility() {
 }
 
 void OutOfProcessInstance::SendNextAccessibilityPage(int32_t page_index) {
-  int page_count = engine_->GetNumberOfPages();
-  if (page_index < 0 || page_index >= page_count)
-    return;
-
-  int char_count = engine_->GetCharCount(page_index);
-
-  // Treat a char count of -1 (error) as 0 (an empty page), since
-  // other pages might have valid content.
-  if (char_count < 0)
-    char_count = 0;
-
   PP_PrivateAccessibilityPageInfo page_info;
-  page_info.page_index = page_index;
-  page_info.bounds = engine_->GetPageBoundsRect(page_index);
-  page_info.char_count = char_count;
-
-  std::vector<PP_PrivateAccessibilityCharInfo> chars(page_info.char_count);
-  for (uint32_t i = 0; i < page_info.char_count; ++i) {
-    chars[i].unicode_character = engine_->GetCharUnicode(page_index, i);
-  }
-
   std::vector<PP_PrivateAccessibilityTextRunInfo> text_runs;
-  int char_index = 0;
-  while (char_index < char_count) {
-    PP_PrivateAccessibilityTextRunInfo text_run_info;
-    pp::FloatRect bounds;
-    engine_->GetTextRunInfo(page_index, char_index, &text_run_info.len,
-                            &text_run_info.font_size, &bounds);
-    DCHECK_LE(char_index + text_run_info.len,
-              static_cast<uint32_t>(char_count));
-    text_run_info.direction = PP_PRIVATEDIRECTION_LTR;
-    text_run_info.bounds = bounds;
-    text_runs.push_back(text_run_info);
-
-    // We need to provide enough information to draw a bounding box
-    // around any arbitrary text range, but the bounding boxes of characters
-    // we get from PDFium don't necessarily "line up". Walk through the
-    // characters in each text run and let the width of each character be
-    // the difference between the x coordinate of one character and the
-    // x coordinate of the next. The rest of the bounds of each character
-    // can be computed from the bounds of the text run.
-    pp::FloatRect char_bounds = engine_->GetCharBounds(page_index, char_index);
-    for (uint32_t i = 0; i < text_run_info.len - 1; i++) {
-      DCHECK_LT(char_index + i + 1, static_cast<uint32_t>(char_count));
-      pp::FloatRect next_char_bounds =
-          engine_->GetCharBounds(page_index, char_index + i + 1);
-      chars[char_index + i].char_width = next_char_bounds.x() - char_bounds.x();
-      char_bounds = next_char_bounds;
-    }
-    chars[char_index + text_run_info.len - 1].char_width = char_bounds.width();
-
-    char_index += text_run_info.len;
+  std::vector<PP_PrivateAccessibilityCharInfo> chars;
+  if (!GetAccessibilityInfo(engine_.get(), page_index, &page_info, &text_runs,
+                            &chars)) {
+    return;
   }
 
-  page_info.text_run_count = text_runs.size();
   pp::PDF::SetAccessibilityPageInfo(GetPluginInstance(), &page_info,
                                     text_runs.data(), chars.data());
 
