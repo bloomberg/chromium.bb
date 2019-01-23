77e8334d8c9a5b74a71f4078b255c8a92cf1c0e0
Tibor Goldschwendt <tiborg@chromium.org>
2019-01-23 16:31:07 +0000

    [DFMs] Track install status via UMA
    Bug: 923541
    Change-Id: Ic5e2dbc0c6053e4bb5f2df792c894f45c8619580
    Reviewed-on: https://chromium-review.googlesource.com/c/1426481
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: agrieve <agrieve@chromium.org>
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625223}
    Former-commit-id: f59576978631ce5ca586d17add3f25e9ae944960

--- components/module_installer/android/java/src-impl/org/chromium/components/module_installer/PlayCoreModuleInstallerBackend.java
+++ components/module_installer/android/java/src-impl/org/chromium/components/module_installer/PlayCoreModuleInstallerBackend.java
@@ -13,10 +13,11 @@ import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.components.module_installer.ModuleInstallerBackend.OnFinishedListener;
 
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * Backend that uses the Play Core SDK to download a module from Play and install it subsequently.
@@ -26,6 +27,16 @@ import java.util.Collections;
     private static final String TAG = "PlayCoreModInBackend";
     private final SplitInstallManager mManager;
     private boolean mIsClosed;
+
+    // FeatureModuleInstallStatus defined in //tools/metrics/histograms/enums.xml.
+    // These values are persisted to logs. Entries should not be renumbered and numeric values
+    // should never be reused.
+    private static final int INSTALL_STATUS_SUCCESS = 0;
+    private static final int INSTALL_STATUS_FAILURE = 1;
+    private static final int INSTALL_STATUS_REQUEST_ERROR = 2;
+    private static final int INSTALL_STATUS_CANCELLATION = 3;
+    // Keep this one at the end and increment appropriately when adding new status.
+    private static final int INSTALL_STATUS_COUNT = 4;
 
     public PlayCoreModuleInstallerBackend(OnFinishedListener listener) {
         super(listener);
@@ -42,7 +53,7 @@ import java.util.Collections;
             Log.e(TAG, "Failed to request module '%s': error code %s", moduleName, errorCode);
             // If we reach this error condition |onStateUpdate| won't be called. Thus, call
             // |onFinished| here.
-            onFinished(false, Arrays.asList(moduleName));
+            finish(false, Collections.singletonList(moduleName), INSTALL_STATUS_REQUEST_ERROR);
         });
     }
 
@@ -64,14 +75,25 @@ import java.util.Collections;
         assert !mIsClosed;
         switch (state.status()) {
             case SplitInstallSessionStatus.INSTALLED:
-                onFinished(true, state.moduleNames());
+                finish(true, state.moduleNames(), INSTALL_STATUS_SUCCESS);
                 break;
             case SplitInstallSessionStatus.CANCELED:
             case SplitInstallSessionStatus.FAILED:
                 Log.e(TAG, "Failed to install modules '%s': error code %s", state.moduleNames(),
                         state.status());
-                onFinished(false, state.moduleNames());
+                int status = state.status() == SplitInstallSessionStatus.CANCELED
+                        ? INSTALL_STATUS_CANCELLATION
+                        : INSTALL_STATUS_FAILURE;
+                finish(false, state.moduleNames(), status);
                 break;
         }
+    }
+
+    private void finish(boolean success, List<String> moduleNames, int eventId) {
+        for (String name : moduleNames) {
+            RecordHistogram.recordEnumeratedHistogram(
+                    "Android.FeatureModules.InstallStatus." + name, eventId, INSTALL_STATUS_COUNT);
+        }
+        onFinished(success, moduleNames);
     }
 }
