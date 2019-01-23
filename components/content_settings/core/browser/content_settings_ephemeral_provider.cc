de53eabe6ede9315178be697ce627774ed982c21
Marek Haranczyk <mharanczyk@opera.com>
2019-01-23 09:16:52 +0000

    Fix memory leaks in content settings introduced by linked_ptr removal.
    Bug: 556939, 914453
    Change-Id: Ibd8757967a051507e0fc2a9ae93ac789e51b005b
    Reviewed-on: https://chromium-review.googlesource.com/c/1425708
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Marek Haranczyk <mharanczyk@opera.com>
    Cr-Commit-Position: refs/heads/master@{#625144}
    Former-commit-id: 9418b9ace4e7574a0d81237999ffd88a83ad0dfd

--- components/content_settings/core/browser/content_settings_ephemeral_provider.cc
+++ components/content_settings/core/browser/content_settings_ephemeral_provider.cc
@@ -68,11 +68,11 @@ bool EphemeralProvider::SetWebsiteSetting(
     return false;
   }
 
-  if (in_value) {
+  std::unique_ptr<base::Value> value(in_value);
+  if (value) {
     content_settings_rules_.SetValue(
         primary_pattern, secondary_pattern, content_type, resource_identifier,
-        store_last_modified_ ? clock_->Now() : base::Time(),
-        std::move(*in_value));
+        store_last_modified_ ? clock_->Now() : base::Time(), std::move(*value));
     NotifyObservers(primary_pattern, secondary_pattern, content_type,
                     resource_identifier);
   } else {
