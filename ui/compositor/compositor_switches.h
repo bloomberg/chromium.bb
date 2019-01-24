4980492ab60e72b58ceb82911904498f5aff85a8
Mitsuru Oshima <oshima@chromium.org>
2019-01-24 07:25:12 +0000

    Add switch to configure gpu memory limit
    Bug: 923141
    Test: None
    Change-Id: I0c53c2272eda161a1d2d6ac16b66f260bd72dfd1
    Reviewed-on: https://chromium-review.googlesource.com/c/1431133
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625542}
    Former-commit-id: b03357d74f93f0754b49e91ca71c2bdacafed7ec

--- ui/compositor/compositor_switches.h
+++ ui/compositor/compositor_switches.h
@@ -18,6 +18,7 @@ COMPOSITOR_EXPORT extern const char kUIDisableZeroCopy[];
 COMPOSITOR_EXPORT extern const char kUIShowPaintRects[];
 COMPOSITOR_EXPORT extern const char kUISlowAnimations[];
 COMPOSITOR_EXPORT extern const char kDisableVsyncForTests[];
+COMPOSITOR_EXPORT extern const char kUiCompositorMemoryLimitWhenVisibleMB[];
 
 }  // namespace switches
 
