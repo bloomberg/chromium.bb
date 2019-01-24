ffb24bc40a87f0ee4900eed9638e978ae86ab667
Matthew Cary <mattcary@chromium.org>
2019-01-24 10:07:18 +0000

    Orderfile: add support for generating a 64 bit orderfile.
    Note that the orderfile will be produced in orderfile.arm64.out,
    which is currently not used when building (orderfile.arm.out is
    used regardless of target_cpu). This makes sense as we use the
    same orderfile on all architectures, but is potentially confusing.
    
    Bug: 922460
    Change-Id: I5f4b627529d3f9a4f20115a2609e512d83c4b88c
    Reviewed-on: https://chromium-review.googlesource.com/c/1417833
    Commit-Queue: Matthew Cary <mattcary@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Reviewed-by: Matthew Cary <mattcary@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625587}
    Former-commit-id: 53f74baf9ad2d42af6c04ec57305955bd24bdfb8

--- base/android/orderfile/orderfile_instrumentation.cc
+++ base/android/orderfile/orderfile_instrumentation.cc
@@ -32,9 +32,9 @@
 #include "base/trace_event/memory_dump_provider.h"
 #endif  // BUILDFLAG(DEVTOOLS_INSTRUMENTATION_DUMPING)
 
-#if !defined(ARCH_CPU_ARMEL)
-#error Only supported on ARM.
-#endif  // !defined(ARCH_CPU_ARMEL)
+#if !BUILDFLAG(SUPPORTS_CODE_ORDERING)
+#error Only supported on architectures supporting code ordering (arm/arm64).
+#endif  // !BUILDFLAG(SUPPORTS_CODE_ORDERING)
 
 // Must be applied to all functions within this file.
 #define NO_INSTRUMENT_FUNCTION __attribute__((no_instrument_function))
