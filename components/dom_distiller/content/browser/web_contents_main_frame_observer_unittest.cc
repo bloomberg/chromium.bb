eabd5a842c64eae51bdc83bfc122621c4bb0673a
Aran Gilman <gilmanmh@google.com>
2019-01-24 00:57:12 +0000

    Clean up dom_distiller::WebContentsMainFrameObserver and its unit test.
    Specific changes:
     * Provide constants to use in place of raw bools when calling Navigate() in the unit test. This makes it a bit easier to determine what the call does without scrolling up and looking at the method.
     * Replace ASSERT_* to EXPECT_*. None of these conditions prevent the test from continuing, so it should be allowed to do so in case it uncovers more errors.
     * Make getters const.
    
    Change-Id: I933daa3820d0b3dc957212a0d2a2fc88251538dc
    Reviewed-on: https://chromium-review.googlesource.com/c/1419438
    Reviewed-by: Mike Pinkerton <pinkerton@chromium.org>
    Commit-Queue: Aran Gilman <gilmanmh@google.com>
    Cr-Commit-Position: refs/heads/master@{#625452}
    Former-commit-id: 27080ab60fc1fbab24910cf37ed02190de1c5537

--- components/dom_distiller/content/browser/web_contents_main_frame_observer_unittest.cc
+++ components/dom_distiller/content/browser/web_contents_main_frame_observer_unittest.cc
@@ -13,6 +13,13 @@
 
 namespace dom_distiller {
 
+// Define named constants to make tests a bit easier to read.
+const bool kMainFrame = true;
+const bool kChildFrame = false;
+
+const bool kSameDocument = true;
+const bool kDifferentDocument = false;
+
 class WebContentsMainFrameObserverTest
     : public content::RenderViewHostTestHarness {
  public:
@@ -21,11 +28,10 @@ class WebContentsMainFrameObserverTest
     // This needed to keep the WebContentsObserverSanityChecker checks happy for
     // when AppendChild is called.
     NavigateAndCommit(GURL("about:blank"));
-    dom_distiller::WebContentsMainFrameObserver::CreateForWebContents(
-        web_contents());
+    WebContentsMainFrameObserver::CreateForWebContents(web_contents());
     main_frame_observer_ =
         WebContentsMainFrameObserver::FromWebContents(web_contents());
-    ASSERT_FALSE(main_frame_observer_->is_document_loaded_in_main_frame());
+    EXPECT_FALSE(main_frame_observer_->is_document_loaded_in_main_frame());
   }
 
   void Navigate(bool main_frame, bool same_document) {
@@ -45,58 +51,58 @@ class WebContentsMainFrameObserverTest
 };
 
 TEST_F(WebContentsMainFrameObserverTest, ListensForMainFrameNavigation) {
-  Navigate(true, false);
-  ASSERT_TRUE(main_frame_observer_->is_initialized());
-  ASSERT_FALSE(main_frame_observer_->is_document_loaded_in_main_frame());
+  Navigate(kMainFrame, kDifferentDocument);
+  EXPECT_TRUE(main_frame_observer_->is_initialized());
+  EXPECT_FALSE(main_frame_observer_->is_document_loaded_in_main_frame());
 
   main_frame_observer_->DocumentLoadedInFrame(main_rfh());
-  ASSERT_TRUE(main_frame_observer_->is_document_loaded_in_main_frame());
+  EXPECT_TRUE(main_frame_observer_->is_document_loaded_in_main_frame());
 }
 
 TEST_F(WebContentsMainFrameObserverTest, IgnoresChildFrameNavigation) {
-  Navigate(false, false);
-  ASSERT_FALSE(main_frame_observer_->is_initialized());
-  ASSERT_FALSE(main_frame_observer_->is_document_loaded_in_main_frame());
+  Navigate(kChildFrame, kDifferentDocument);
+  EXPECT_FALSE(main_frame_observer_->is_initialized());
+  EXPECT_FALSE(main_frame_observer_->is_document_loaded_in_main_frame());
 }
 
 TEST_F(WebContentsMainFrameObserverTest, IgnoresSameDocumentNavigation) {
-  Navigate(true, true);
-  ASSERT_FALSE(main_frame_observer_->is_initialized());
-  ASSERT_FALSE(main_frame_observer_->is_document_loaded_in_main_frame());
+  Navigate(kMainFrame, kSameDocument);
+  EXPECT_FALSE(main_frame_observer_->is_initialized());
+  EXPECT_FALSE(main_frame_observer_->is_document_loaded_in_main_frame());
 }
 
 TEST_F(WebContentsMainFrameObserverTest,
        IgnoresSameDocumentavigationUnlessMainFrameLoads) {
-  Navigate(true, true);
-  ASSERT_FALSE(main_frame_observer_->is_initialized());
-  ASSERT_FALSE(main_frame_observer_->is_document_loaded_in_main_frame());
+  Navigate(kMainFrame, kSameDocument);
+  EXPECT_FALSE(main_frame_observer_->is_initialized());
+  EXPECT_FALSE(main_frame_observer_->is_document_loaded_in_main_frame());
 
   // Even if we didn't acknowledge a same-document navigation, if the main
   // frame loads, consider a load complete.
   main_frame_observer_->DocumentLoadedInFrame(main_rfh());
-  ASSERT_TRUE(main_frame_observer_->is_document_loaded_in_main_frame());
+  EXPECT_TRUE(main_frame_observer_->is_document_loaded_in_main_frame());
 }
 
 TEST_F(WebContentsMainFrameObserverTest, ResetOnPageNavigation) {
-  Navigate(true, false);
+  Navigate(kMainFrame, kDifferentDocument);
   main_frame_observer_->DocumentLoadedInFrame(main_rfh());
-  ASSERT_TRUE(main_frame_observer_->is_document_loaded_in_main_frame());
+  EXPECT_TRUE(main_frame_observer_->is_document_loaded_in_main_frame());
 
   // Another navigation should result in waiting for a page load.
-  Navigate(true, false);
-  ASSERT_TRUE(main_frame_observer_->is_initialized());
-  ASSERT_FALSE(main_frame_observer_->is_document_loaded_in_main_frame());
+  Navigate(kMainFrame, kDifferentDocument);
+  EXPECT_TRUE(main_frame_observer_->is_initialized());
+  EXPECT_FALSE(main_frame_observer_->is_document_loaded_in_main_frame());
 }
 
 TEST_F(WebContentsMainFrameObserverTest, DoesNotResetOnInPageNavigation) {
-  Navigate(true, false);
+  Navigate(kMainFrame, kDifferentDocument);
   main_frame_observer_->DocumentLoadedInFrame(main_rfh());
-  ASSERT_TRUE(main_frame_observer_->is_document_loaded_in_main_frame());
+  EXPECT_TRUE(main_frame_observer_->is_document_loaded_in_main_frame());
 
   // Navigating withing the page should not result in waiting for a page load.
-  Navigate(true, true);
-  ASSERT_TRUE(main_frame_observer_->is_initialized());
-  ASSERT_TRUE(main_frame_observer_->is_document_loaded_in_main_frame());
+  Navigate(kMainFrame, kSameDocument);
+  EXPECT_TRUE(main_frame_observer_->is_initialized());
+  EXPECT_TRUE(main_frame_observer_->is_document_loaded_in_main_frame());
 }
 
 }  // namespace dom_distiller
