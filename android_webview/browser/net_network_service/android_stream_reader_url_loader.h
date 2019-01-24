abe9fb4757e614550d6f68664b8e546b7385b476
Tim Volodine <timvolodine@chromium.org>
2019-01-24 20:58:06 +0000

    [AW NS] Ensure correct threading for AndroidStreamReaderURLLoader
    Match the threading of AndroidStreamReaderURLLoader (NS code path)
    to that of AndroidStreamReaderURLRequestJob (current code path).
    
    In particular make sure to open the input stream on a background
    worker thread. The input stream can be provided by the embedding
    app, so it's important to respect the original thread model. The
    AndroidStreamReaderURLLoader is also used for loading android
    assets, android resources and content scheme urls.
    
    Add ThreadChecker same thread DCHECKs in
    AndroidStreamReaderURLLoader.
    
    Add OnInputStreamOpenFailed delegate method to be called on
    the IO thread with the necessary restart logic.
    
    Add threading checks for OpenInputStream and
    OnInputStreamOpenFailed in the unittest.
    
    BUG=913524,841556
    Cq-Include-Trybots: master.tryserver.chromium.android:android_mojo
    
    Change-Id: I23da72265ab0db8f0236bc1760d09826102317ef
    Reviewed-on: https://chromium-review.googlesource.com/c/1422698
    Commit-Queue: Tim Volodine <timvolodine@chromium.org>
    Reviewed-by: Tobias Sargeant <tobiasjs@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625802}
    Former-commit-id: 58e7a126560100b62c6306c7e497b47a7b676365

--- android_webview/browser/net_network_service/android_stream_reader_url_loader.h
+++ android_webview/browser/net_network_service/android_stream_reader_url_loader.h
@@ -6,6 +6,7 @@
 #define ANDROID_WEBVIEW_BROWSER_NET_NETWORK_SERVICE_ANDROID_STREAM_READER_URL_LOADER_H_
 
 #include "android_webview/browser/net/aw_web_resource_response.h"
+#include "base/threading/thread_checker.h"
 #include "mojo/public/cpp/system/simple_watcher.h"
 #include "net/http/http_byte_range.h"
 #include "services/network/public/cpp/net_adapters.h"
@@ -22,12 +23,18 @@ class InputStreamReaderWrapper;
 class AndroidStreamReaderURLLoader : public network::mojom::URLLoader {
  public:
   // Delegate abstraction for obtaining input streams.
-  // TODO(timvolodine): add other methods to this delegate
   class ResponseDelegate {
    public:
     virtual ~ResponseDelegate() {}
+
+    // This method is called from a worker thread, not from the IO thread.
     virtual std::unique_ptr<android_webview::InputStream> OpenInputStream(
         JNIEnv* env) = 0;
+
+    // This method is called on the URLLoader thread (IO thread) if the
+    // result of calling OpenInputStream was null. |restarted| parameter
+    // is set to true if the request was restarted with a new loader.
+    virtual void OnInputStreamOpenFailed(bool* restarted) = 0;
 
     virtual bool GetMimeType(JNIEnv* env,
                              const GURL& url,
@@ -65,6 +72,8 @@ class AndroidStreamReaderURLLoader : public network::mojom::URLLoader {
  private:
   bool ParseRange(const net::HttpRequestHeaders& headers);
   void OnInputStreamOpened(
+      std::unique_ptr<AndroidStreamReaderURLLoader::ResponseDelegate>
+          returned_delegate,
       std::unique_ptr<android_webview::InputStream> input_stream);
   void OnReaderSeekCompleted(int result);
   void HeadersComplete(int status_code, const std::string& status_text);
@@ -89,6 +98,7 @@ class AndroidStreamReaderURLLoader : public network::mojom::URLLoader {
   mojo::ScopedDataPipeProducerHandle producer_handle_;
   scoped_refptr<network::NetToMojoPendingBuffer> pending_buffer_;
   mojo::SimpleWatcher writable_handle_watcher_;
+  base::ThreadChecker thread_checker_;
 
   base::WeakPtrFactory<AndroidStreamReaderURLLoader> weak_factory_;
 
