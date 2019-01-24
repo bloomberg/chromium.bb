2a485c3818d04ed8ad0cba119f1429f5e0919633
Ken Rockot <rockot@google.com>
2019-01-24 07:41:53 +0000

    Revert "Move NoTouchActivity to touchless package"
    This reverts commit ba60999fc37cd68db4e6ce0506e7dc3c36798af7 [formerly 0ebabc86af52b18004b8feb1639ac2d9b07c8c31].
    
    Reason for revert: Speculative revert as I'm reasonably sure this made the android-binary-size CQ builder red for almost all builds (it's complaining about AndroidManifest changes and the failures coincide precisely with this CL landing). https://ci.chromium.org/p/chromium/builders/luci.chromium.try/android-binary-size?cursor=Ci0SJ2oQc35jci1idWlsZGJ1Y2tldHITCxIFQnVpbGQY0P3fi_a2pOt7DBgAIAA%3D&limit=200
    
    Original change's description:
    > Move NoTouchActivity to touchless package
    > 
    > Move the NoTouchActivity to the newly created touchless sub-package in order to
    > declutter and tidy up the browser package.
    > 
    > Bug: 922047
    > Change-Id: Ib7d9df3c89295e9e675b35a05e3613b390fbae5e
    > Reviewed-on: https://chromium-review.googlesource.com/c/1432473
    > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Commit-Queue: Mehran Mahmoudi <mahmoudi@google.com>
    > Cr-Commit-Position: refs/heads/master@{#625487}
    
    TBR=mthiesse@chromium.org,tedchoc@chromium.org,mahmoudi@google.com
    
    Change-Id: I93a20aeb64bb61fe798cf56118c1bb9e4b43e17a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 922047
    Reviewed-on: https://chromium-review.googlesource.com/c/1433597
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#625550}
    Former-commit-id: 3344a6df199b5d0861d46a5d88c8583cbde41a93

--- chrome/android/javatests/src/org/chromium/chrome/browser/NoTouchActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/NoTouchActivityTest.java
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.touchless;
+package org.chromium.chrome.browser;
 
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.MediumTest;
@@ -16,8 +16,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.chrome.browser.ChromeSwitches;
-import org.chromium.chrome.browser.MockSafeBrowsingApiHandler;
 import org.chromium.chrome.browser.ntp.NewTabPage;
 import org.chromium.chrome.test.ChromeActivityTestRule;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
