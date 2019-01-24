9e436bd0f26e12a3228046a3c4e88d6c1b353b4d
Vlad Tsyrklevich <vtsyrklevich@chromium.org>
2019-01-24 18:38:01 +0000

    Fix misc typos
    Change-Id: Id01a46f3ca42ef49a2ebbcb8bd161dfd88574581
    Reviewed-on: https://chromium-review.googlesource.com/c/1429383
    Auto-Submit: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625731}
    Former-commit-id: 4b5849fc4dd0707c558031f228e17fc7a7c8c520

--- components/version_info/version_info.h
+++ components/version_info/version_info.h
@@ -32,14 +32,14 @@ const base::Version& GetVersion();
 std::string GetLastChange();
 
 // Returns whether this is an "official" release of the current version, i.e.
-// whether kwnowing GetVersionNumber() is enough to completely determine what
+// whether knowing GetVersionNumber() is enough to completely determine what
 // GetLastChange() is.
 bool IsOfficialBuild();
 
-// Returns the OS type, e.g. "Windows", "Linux", "FreeBDS", ...
+// Returns the OS type, e.g. "Windows", "Linux", "FreeBSD", ...
 std::string GetOSType();
 
-// Returns a string equivalent of |channel|, indenpendent of whether the build
+// Returns a string equivalent of |channel|, independent of whether the build
 // is branded or not and without any additional modifiers.
 std::string GetChannelString(Channel channel);
 
