ed0b829ac50a983ac1b34822064fbd781797238f
Mason Freed <masonfreed@chromium.org>
2019-01-24 19:40:19 +0000

    Remove backdrop-filter crop rectangle from ui::Layer
    The prior code incorrectly cropped the backdrop-filtered content on
    the ChromeOS Shelf. This fixes that problem.
    
    Bug: 920296, 923022
    Change-Id: Idfee932a67b4f8baa2ad9351268384b3b059d72b
    Reviewed-on: https://chromium-review.googlesource.com/c/1415171
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625777}
    Former-commit-id: 8ba6b43b75f7cdceb48e2b50b8233881aa6571ff

--- ui/compositor/layer.cc
+++ ui/compositor/layer.cc
@@ -378,7 +378,6 @@ void Layer::SetBounds(const gfx::Rect& bounds) {
 void Layer::SetSubpixelPositionOffset(const gfx::Vector2dF& offset) {
   subpixel_position_offset_ = offset;
   RecomputePosition();
-  RecomputeBackdropBounds();
 }
 
 gfx::Rect Layer::GetTargetBounds() const {
@@ -540,7 +539,6 @@ void Layer::SetLayerBackgroundFilters() {
         background_blur_sigma_, SkBlurImageFilter::kClamp_TileMode));
   }
   cc_layer_->SetBackdropFilters(filters);
-  RecomputeBackdropBounds();
 }
 
 float Layer::GetTargetOpacity() const {
@@ -1007,7 +1005,6 @@ void Layer::OnDeviceScaleFactorChanged(float device_scale_factor) {
   device_scale_factor_ = device_scale_factor;
   RecomputeDrawsContentAndUVRect();
   RecomputePosition();
-  RecomputeBackdropBounds();
   if (nine_patch_layer_) {
     if (!nine_patch_layer_image_.isNull())
       UpdateNinePatchLayerImage(nine_patch_layer_image_);
@@ -1175,7 +1172,6 @@ void Layer::SetBoundsFromAnimation(const gfx::Rect& bounds,
 
   RecomputeDrawsContentAndUVRect();
   RecomputePosition();
-  RecomputeBackdropBounds();
 
   if (delegate_)
     delegate_->OnLayerBoundsChanged(old_bounds, reason);
@@ -1323,7 +1319,6 @@ void Layer::CreateCcLayer() {
   cc_layer_->SetLayerClient(weak_ptr_factory_.GetWeakPtr());
   cc_layer_->SetElementId(cc::ElementId(cc_layer_->id()));
   RecomputePosition();
-  RecomputeBackdropBounds();
 }
 
 void Layer::RecomputeDrawsContentAndUVRect() {
@@ -1345,13 +1340,6 @@ void Layer::RecomputeDrawsContentAndUVRect() {
 void Layer::RecomputePosition() {
   cc_layer_->SetPosition(gfx::PointF(bounds_.origin()) +
                          subpixel_position_offset_);
-}
-
-void Layer::RecomputeBackdropBounds() {
-  gfx::RectF backdrop_filter_bounds =
-      gfx::RectF(bounds().width(), bounds().height());
-  backdrop_filter_bounds.Scale(GetDeviceScaleFactor());
-  cc_layer_->SetBackdropFilterBounds(backdrop_filter_bounds);
 }
 
 void Layer::SetCompositorForAnimatorsInTree(Compositor* compositor) {
