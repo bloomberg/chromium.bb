3d6ca166560bd101522de3baf2d4992af3c5004f
Eliot Courtney <edcourtney@google.com>
2019-01-24 03:56:57 +0000

    Flag off PIP rounded corners by default.
    Bug: 923812
    Test: Rounded corners no longer used for PIP windows.
    Change-Id: Id2a09865d05ed1f51fb5a1ee6645db0bcf59f4e4
    Reviewed-on: https://chromium-review.googlesource.com/c/1424107
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625489}
    Former-commit-id: 493df9a93b888c7fab9b9310df507d96d8e02eb5

--- ash/public/cpp/ash_features.cc
+++ ash/public/cpp/ash_features.cc
@@ -38,7 +38,7 @@ const base::Feature kNotificationScrollBar{"NotificationScrollBar",
                                            base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kPipRoundedCorners{"PipRoundedCorners",
-                                       base::FEATURE_ENABLED_BY_DEFAULT};
+                                       base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kSeparateNetworkIcons{"SeparateNetworkIcons",
                                           base::FEATURE_DISABLED_BY_DEFAULT};
