e6f02da92b500127229d99012ffe6318e05c2db4
Lei Zhang <thestig@chromium.org>
2019-01-24 15:17:54 +0000

    Remove new usage in ResourceDispatcherHostImpl::MaybeInterceptAsStream()
    Switch to std::make_unique or base::MakeRefCounted. Fix some nits and
    lint errors as well.
    
    Change-Id: I2a75aa2c626ad3ea6523159edf4b981336771c8f
    Reviewed-on: https://chromium-review.googlesource.com/c/1433595
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625648}
    Former-commit-id: aff02e2d8c467d819ff9be87983a70b5e28866ff

--- content/browser/loader/resource_dispatcher_host_impl.cc
+++ content/browser/loader/resource_dispatcher_host_impl.cc
@@ -464,23 +464,23 @@ ResourceDispatcherHostImpl::MaybeInterceptAsStream(
     network::ResourceResponse* response,
     std::string* payload) {
   payload->clear();
-  ResourceRequestInfoImpl* info = ResourceRequestInfoImpl::ForRequest(request);
   const std::string& mime_type = response->head.mime_type;
 
   GURL origin;
   if (!delegate_ || !delegate_->ShouldInterceptResourceAsStream(
                         request, mime_type, &origin, payload)) {
-    return std::unique_ptr<ResourceHandler>();
+    return nullptr;
   }
 
+  ResourceRequestInfoImpl* info = ResourceRequestInfoImpl::ForRequest(request);
   StreamContext* stream_context =
       GetStreamContextForResourceContext(info->GetContext());
 
-  std::unique_ptr<StreamResourceHandler> handler(new StreamResourceHandler(
-      request, stream_context->registry(), origin, false));
+  auto handler = std::make_unique<StreamResourceHandler>(
+      request, stream_context->registry(), origin, false);
 
   info->set_is_stream(true);
-  std::unique_ptr<StreamInfo> stream_info(new StreamInfo);
+  auto stream_info = std::make_unique<StreamInfo>();
   stream_info->handle = handler->stream()->CreateHandle();
   stream_info->original_url = request->url();
   stream_info->mime_type = mime_type;
@@ -489,7 +489,8 @@ ResourceDispatcherHostImpl::MaybeInterceptAsStream(
   // ResourceDispatcherHostDelegate.
   if (response->head.headers.get()) {
     stream_info->response_headers =
-        new net::HttpResponseHeaders(response->head.headers->raw_headers());
+        base::MakeRefCounted<net::HttpResponseHeaders>(
+            response->head.headers->raw_headers());
   }
   delegate_->OnStreamCreated(request, std::move(stream_info));
   return std::move(handler);
@@ -1888,7 +1889,7 @@ void ResourceDispatcherHostImpl::UpdateLoadStateOnUI(
 
   std::unique_ptr<LoadInfoMap> info_map =
       PickMoreInterestingLoadInfos(std::move(infos));
-  for (const auto& load_info: *info_map) {
+  for (const auto& load_info : *info_map) {
     loader_delegate->LoadStateChanged(
         load_info.first, load_info.second.host, load_info.second.load_state,
         load_info.second.upload_position, load_info.second.upload_size);
