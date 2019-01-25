3a5176126257d7a5b1bd1dfc913e6b126c3b5695
Chris Lu <thegreenfrog@chromium.org>
2019-01-25 17:24:19 +0000

    [ios] Implement Find for FindInPageManager
    Implements logic for FindInPageSearch option to search and highlight matches.
    Searching for non-ascii not currently supported.
    
    Bug: 896867
    Change-Id: I362d3cebacfced1537742fbbab9ad323d6edeafd
    Reviewed-on: https://chromium-review.googlesource.com/c/1407665
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626108}
    Former-commit-id: ec44c35db800930a55a177f75d8cc3fd759b38ff

--- ios/web/web_state/find_in_page/find_in_page_manager_impl.h
+++ ios/web/web_state/find_in_page/find_in_page_manager_impl.h
@@ -41,12 +41,20 @@ class FindInPageManagerImpl : public FindInPageManager,
     // |pending_frame_calls_count| after it has been reset by the new find.
     int unique_id = 0;
     // Counter to keep track of pending frame JavaScript calls.
-    int pending_frame_calls_count = 0;
+    int pending_frame_call_count = 0;
     // Holds number of matches found for each frame keyed by frame_id.
-    std::map<std::string, int> frame_matches_count;
+    std::map<std::string, int> frame_match_count;
     // List of frame_ids used for sorting matches.
     std::list<std::string> frame_order;
   };
+
+  // Determines whether find is finished. If not, calls pumpSearch to continue.
+  // If it is, calls UpdateFrameMatchesCount(). If find returned null, then does
+  // nothing more.
+  void ProcessFindInPageResult(const std::string& query,
+                               const std::string& frame_id,
+                               const int request_id,
+                               const base::Value* result);
 
   // WebStateObserver overrides
   void WebFrameDidBecomeAvailable(WebState* web_state,
