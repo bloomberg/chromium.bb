8987bc85f999784c6c79864717476a3531c65248
Olivier Li <olivierli@chromium.org>
2019-01-25 21:24:39 +0000

    Make base::Feature variables const and not constexpr in safebrowsing::
    Bug: 925365
    Change-Id: Ie97bc90690811a865652e533c5ea6d540b5523b3
    Reviewed-on: https://chromium-review.googlesource.com/c/1437007
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Oliver Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626205}
    Former-commit-id: 2e1939cb542044e19891ac04601cf71cd31ae8b7

--- chrome/browser/safe_browsing/chrome_cleaner/srt_field_trial_win.cc
+++ chrome/browser/safe_browsing/chrome_cleaner/srt_field_trial_win.cc
@@ -39,19 +39,19 @@ namespace safe_browsing {
 
 constexpr char kSRTPromptTrial[] = "SRTPromptFieldTrial";
 
-constexpr base::Feature kRebootPromptDialogFeature{
+const base::Feature kRebootPromptDialogFeature{
     "RebootPromptDialog", base::FEATURE_DISABLED_BY_DEFAULT};
 
-constexpr base::Feature kUserInitiatedChromeCleanupsFeature{
+const base::Feature kUserInitiatedChromeCleanupsFeature{
     "UserInitiatedChromeCleanups", base::FEATURE_ENABLED_BY_DEFAULT};
 
-constexpr base::Feature kChromeCleanupDistributionFeature{
+const base::Feature kChromeCleanupDistributionFeature{
     "ChromeCleanupDistribution", base::FEATURE_DISABLED_BY_DEFAULT};
 
-constexpr base::Feature kChromeCleanupQuarantineFeature{
+const base::Feature kChromeCleanupQuarantineFeature{
     "ChromeCleanupQuarantine", base::FEATURE_DISABLED_BY_DEFAULT};
 
-constexpr base::Feature kChromeCleanupExtensionsFeature{
+const base::Feature kChromeCleanupExtensionsFeature{
     "ChromeCleanupExtensions", base::FEATURE_DISABLED_BY_DEFAULT};
 
 bool IsInSRTPromptFieldTrialGroups() {
