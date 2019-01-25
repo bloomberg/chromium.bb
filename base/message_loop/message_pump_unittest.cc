3541e64e01bbf24d032b1f196bfa5817f87a16f1
Sami Kyostila <skyostil@chromium.org>
2019-01-25 12:00:59 +0000

    base: Add workaround for iOS message pump timer slack bug
    This patch adds a workaround for an issue in the iOS message pump in the
    following scenario:
    
      1. The message pump timer slack is set to maximum.
      2. A delayed task is posted for far in the future (e.g., 1h).
      3. The system goes idle at least for a few seconds.
      4. Another delayed task is posted with a much smaller delay.
    
    It appears the setting the tolerance for a CFRunLoop timer *after* setting the
    next fire time, it has no effect and the previous tolerance remains active. In
    the above scenario it means the task posted in step #4 actually runs in about
    half the delay set in step #2.
    
    This bug was causing a net_unittest to fail because the TaskScheduler's
    periodic task to adjust the number of active threads wasn't running, causing
    the system to grind to a halt because all worker pool threads were blocking on
    a lock.
    
    The workaround is to always set the timer tolerance before adjusting the timer
    fire time.
    
    Bug: 891670
    Change-Id: I82e97fe993bbb8a71c417f01f4d6131cce0667ad
    Reviewed-on: https://chromium-review.googlesource.com/c/1434276
    Commit-Queue: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626053}
    Former-commit-id: 3f82735d69c34ff2d9c7a9b0abd9a90540723897

--- base/message_loop/message_pump_unittest.cc
+++ base/message_loop/message_pump_unittest.cc
@@ -4,6 +4,8 @@
 
 #include "base/message_loop/message_pump.h"
 #include "base/message_loop/message_loop.h"
+#include "base/test/bind_test_util.h"
+#include "base/threading/thread.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -82,6 +84,84 @@ TEST_P(MessagePumpTest, QuitStopsWorkWithNestedRunLoop) {
   }));
 
   message_pump_->ScheduleWork();
+  message_pump_->Run(&delegate);
+}
+
+class TimerSlackTestDelegate : public MessagePump::Delegate {
+ public:
+  TimerSlackTestDelegate(MessagePump* message_pump)
+      : message_pump_(message_pump) {
+    // We first schedule a delayed task far in the future with maximum timer
+    // slack.
+    message_pump_->SetTimerSlack(TIMER_SLACK_MAXIMUM);
+    message_pump_->ScheduleDelayedWork(TimeTicks::Now() +
+                                       TimeDelta::FromHours(1));
+
+    // Since we have no other work pending, the pump will initially be idle.
+    action_.store(NONE);
+  }
+
+  bool DoWork() override {
+    switch (action_.load()) {
+      case NONE:
+        break;
+      case SCHEDULE_DELAYED_WORK:
+        // After being woken up by the other thread, we schedule work after a
+        // short delay. If is workaround was successful, the pump will wake up
+        // shortly, finishing the test.
+        action_.store(QUIT);
+        message_pump_->ScheduleDelayedWork(TimeTicks::Now() +
+                                           TimeDelta::FromMilliseconds(50));
+        break;
+      case QUIT:
+        message_pump_->Quit();
+        break;
+    }
+    return false;
+  }
+  bool DoDelayedWork(base::TimeTicks*) override { return false; }
+  bool DoIdleWork() override { return false; }
+
+  void WakeUpFromOtherThread() {
+    action_.store(SCHEDULE_DELAYED_WORK);
+    message_pump_->ScheduleWork();
+  }
+
+ private:
+  enum Action {
+    NONE,
+    SCHEDULE_DELAYED_WORK,
+    QUIT,
+  };
+
+  MessagePump* const message_pump_;
+  std::atomic<Action> action_;
+};
+
+TEST_P(MessagePumpTest, TimerSlackWithLongDelays) {
+  // This is a regression test for an issue where the iOS message pump fails to
+  // run delayed work when timer slack is enabled. The steps needed to trigger
+  // this are:
+  //
+  //  1. The message pump timer slack is set to maximum.
+  //  2. A delayed task is posted for far in the future (e.g., 1h).
+  //  3. The system goes idle at least for a few seconds.
+  //  4. Another delayed task is posted with a much smaller delay.
+  //
+  // The following message pump test delegate automatically runs through this
+  // sequence.
+  TimerSlackTestDelegate delegate(message_pump_.get());
+
+  // We use another thread to wake up the pump after 2 seconds to allow the
+  // system to enter an idle state. This delay was determined experimentally on
+  // the iPhone 6S simulator.
+  Thread thread("Waking thread");
+  thread.StartAndWaitForTesting();
+  thread.task_runner()->PostDelayedTask(
+      FROM_HERE,
+      BindLambdaForTesting([&delegate] { delegate.WakeUpFromOtherThread(); }),
+      TimeDelta::FromSeconds(2));
+
   message_pump_->Run(&delegate);
 }
 
