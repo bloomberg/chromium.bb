57d61f8c6c0adeb8a21f996d5f93abeed3652ce8
Robbie McElrath <rmcelrath@chromium.org>
2019-01-25 04:36:49 +0000

    Revert "Enable NetworkService by default on ChromeOS."
    This reverts commit b6fe94d53ad9b147e7673e0d57cd93647d6f52a6 [formerly 82fd683b2229b596ddd198db167415da7aa471a7].
    
    Reason for revert: It looks like this caused flakiness in 2 tests:
    https://findit-for-me.appspot.com/waterfall/flake/flake-culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyQwsSDEZsYWtlQ3VscHJpdCIxY2hyb21pdW0vODJmZDY4M2IyMjI5YjU5NmRkZDE5OGRiMTY3NDE1ZGE3YWE0NzFhNww
    
    Original change's description:
    > Enable NetworkService by default on ChromeOS.
    > 
    > This also updates UnittestTestSuite to disable the Network Service at
    > the beginning of all unit tests, since many don't work with it enabled.
    > 
    > Change-Id: Ibff4333e4f80f34f7b26510cafafe3dc0eee6655
    > Bug: 920038
    > Reviewed-on: https://chromium-review.googlesource.com/c/1413414
    > Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    > Reviewed-by: John Budorick <jbudorick@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#625899}
    
    TBR=jam@chromium.org,jbudorick@chromium.org,rmcelrath@chromium.org
    
    Change-Id: Ifcaea7d25630a15dd8c38d7c838da8893419a42f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 920038
    Reviewed-on: https://chromium-review.googlesource.com/c/1436735
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#625993}
    Former-commit-id: dfc562b7ae7a21f39586a3d9a12bd1d36a174d08

--- services/network/public/cpp/features.cc
+++ services/network/public/cpp/features.cc
@@ -15,15 +15,8 @@ const base::Feature kExpectCTReporting{"ExpectCTReporting",
 const base::Feature kNetworkErrorLogging{"NetworkErrorLogging",
                                          base::FEATURE_ENABLED_BY_DEFAULT};
 // Enables the network service.
-const base::Feature kNetworkService {
-  "NetworkService",
-#if defined(OS_CHROMEOS)
-      base::FEATURE_ENABLED_BY_DEFAULT
-};
-#else
-      base::FEATURE_DISABLED_BY_DEFAULT
-};
-#endif
+const base::Feature kNetworkService{"NetworkService",
+                                    base::FEATURE_DISABLED_BY_DEFAULT};
 
 // Out of Blink CORS
 const base::Feature kOutOfBlinkCors{"OutOfBlinkCors",
