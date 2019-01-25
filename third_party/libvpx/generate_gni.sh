844fd33ceac53add9a746130d30af3cb609471d8
Jon Kunkee <jkunkee@microsoft.com>
2019-01-25 01:02:20 +0000

    [libvpx] Use HighBD config for ARM64 Windows
    Now that ARM64 HighBD has easily shared naming, switch to using it for
    ARM64 Windows builds.
    
    Bug: 893460
    Change-Id: Id39ccef906ed2c218669243bc01d8d300fb91bce
    Reviewed-on: https://chromium-review.googlesource.com/c/1432892
    Reviewed-by: Johann Koenig <johannkoenig@google.com>
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Cr-Commit-Position: refs/heads/master@{#625915}
    Former-commit-id: 7ecbaeeb681f2454dadb9204eeeb6f958917ffcd

--- third_party/libvpx/generate_gni.sh
+++ third_party/libvpx/generate_gni.sh
@@ -481,6 +481,8 @@ if [ -z $ONLY_CONFIGS ]; then
   make libvpx_srcs.txt target=libs $config > /dev/null
   convert_srcs_to_project_files libvpx_srcs.txt libvpx_srcs_arm64_highbd
 
+  echo "ARM64 Windows uses the ARM64 Linux HighBD source list. No need to generate it."
+
   echo "Generate MIPS source list."
   config=$(print_config_basic linux/mipsel)
   make_clean
