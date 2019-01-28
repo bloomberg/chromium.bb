3b766eff3462327dd87c10f3072bc0112c5e79bb
Tom Anderson <thomasanderson@chromium.org>
2019-01-28 21:31:31 +0000

    Fix TabStrip not laying out when touch mode changes
    BUG=b/122778991
    R=pkasting
    
    Change-Id: Ia8a50ebbcacd8ba9a3d526af2d0b8c645967cf39
    Reviewed-on: https://chromium-review.googlesource.com/c/1440073
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626739}
    Former-commit-id: b3ff31f31f02c92c3cc8ec8deda68400cd3828c5

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -2859,4 +2859,5 @@ void TabStrip::OnTouchUiChanged() {
   new_tab_button_bounds_.set_size(new_tab_button_->GetPreferredSize());
   new_tab_button_->SetBoundsRect(new_tab_button_bounds_);
   StopAnimating(true);
+  PreferredSizeChanged();
 }
