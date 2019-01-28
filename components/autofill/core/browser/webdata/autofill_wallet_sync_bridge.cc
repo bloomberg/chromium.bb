5c4ebe0fa76b3a846a7d3da833d2c836fefc4f49
Jan Krcal <jkrcal@chromium.org>
2019-01-28 17:17:44 +0000

    [Wallet sync] Report number of cleared entities when sync gets disabled
    This CL records how many entities have been cleared after disabling
    sync. As this is implemented differently in Directory / USS, the metric
    can help figuring out traffic discrepancies.
    
    Bug: 924447
    Change-Id: I6d9cdd7333a89bd5d8223d15baa7405de7c9617e
    Reviewed-on: https://chromium-review.googlesource.com/c/1437198
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Auto-Submit: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626582}
    Former-commit-id: 3a53012b4baa48045f0790a1c1ab165a2e1b7266

--- components/autofill/core/browser/webdata/autofill_wallet_sync_bridge.cc
+++ components/autofill/core/browser/webdata/autofill_wallet_sync_bridge.cc
@@ -21,6 +21,7 @@
 #include "components/autofill/core/browser/webdata/autofill_webdata_backend.h"
 #include "components/autofill/core/browser/webdata/autofill_webdata_service.h"
 #include "components/autofill/core/common/autofill_util.h"
+#include "components/sync/base/data_type_histogram.h"
 #include "components/sync/driver/sync_driver_switches.h"
 #include "components/sync/model/entity_data.h"
 #include "components/sync/model/mutable_data_batch.h"
@@ -262,6 +263,21 @@ AutofillWalletSyncBridge::ApplyStopSyncChanges(
     if (initial_sync_done_) {
       active_callback_.Run(false);
     }
+
+    // Report count of entities to delete. This use case should be pretty rare
+    // so it is okay to read it from DB again.
+    // TODO(crbug.com/853688): Remove when wallet data is launched on USS, incl.
+    // the helper function SyncWalletDataRecordClearedEntitiesCount().
+    std::vector<std::unique_ptr<AutofillProfile>> profiles;
+    std::vector<std::unique_ptr<CreditCard>> cards;
+    std::unique_ptr<PaymentsCustomerData> customer_data;
+    if (GetAutofillTable()->GetServerProfiles(&profiles) &&
+        GetAutofillTable()->GetServerCreditCards(&cards) &&
+        GetAutofillTable()->GetPaymentsCustomerData(&customer_data)) {
+      int count = profiles.size() + cards.size() + (customer_data ? 1 : 0);
+      SyncWalletDataRecordClearedEntitiesCount(count);
+    }
+
     SetSyncData(syncer::EntityChangeList());
 
     initial_sync_done_ = false;
