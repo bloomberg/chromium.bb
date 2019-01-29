dad0f350d19e16e21db10a3215a50161e79ac34a
Hirokazu Honda <hiroh@chromium.org>
2019-01-29 08:51:49 +0000

    media/capture/video: Follow GbmWrapper change, one file descriptor for each plane
    crrev.com/c/1437036 makes GbmWrapper create GbmPixmap which has one file
    descriptor for each plane. This CL follows the change in chrome os capture
    stack.
    
    Bug: 911370
    Test: capture_unittests
    Change-Id: Ia498f36d9f4edd074cba4dc8ea5bc863364297c1
    Reviewed-on: https://chromium-review.googlesource.com/c/1438858
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#626935}
    Former-commit-id: 47364f1e41747154d7af4f3c80c048a4383b3a7e

--- media/capture/video/mock_gpu_memory_buffer_manager.cc
+++ media/capture/video/mock_gpu_memory_buffer_manager.cc
@@ -37,12 +37,15 @@ class FakeGpuMemoryBuffer : public gfx::GpuMemoryBuffer {
 
 #if defined(OS_CHROMEOS)
     // Set a dummy fd since this is for testing only.
-    handle_.native_pixmap_handle.fds.push_back(base::FileDescriptor(0, false));
+    handle_.native_pixmap_handle.fds.push_back(base::FileDescriptor(0, true));
     handle_.native_pixmap_handle.planes.push_back(
         gfx::NativePixmapPlane(size_.width(), 0, y_plane_size));
-    handle_.native_pixmap_handle.planes.push_back(gfx::NativePixmapPlane(
-        size_.width(), handle_.native_pixmap_handle.planes[0].size,
-        uv_plane_size));
+    if (format == gfx::BufferFormat::YUV_420_BIPLANAR) {
+      handle_.native_pixmap_handle.fds.push_back(base::FileDescriptor(0, true));
+      handle_.native_pixmap_handle.planes.push_back(gfx::NativePixmapPlane(
+          size_.width(), handle_.native_pixmap_handle.planes[0].size,
+          uv_plane_size));
+    }
 
     // For faking a valid JPEG blob buffer.
     if (base::checked_cast<size_t>(size_.width()) >= sizeof(Camera3JpegBlob)) {
