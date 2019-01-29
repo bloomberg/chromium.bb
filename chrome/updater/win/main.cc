9443269305ba04490fd2eebd726438396fca43bc
Sorin Jianu <sorin@chromium.org>
2019-01-29 17:38:48 +0000

    Create a skeleton updater main for Windows.
    This change builds an executable program on Windows, which returns
    a hardcoded value. The value is tested by a unit test.
    
    The updater executable contains a valid resource file and manifest.
    
    An upcoming change will hook the test with the test bots.
    Then, another change will implement a similar function for macOS.
    
    Bug: 925489
    Change-Id: Id97f4740a2cd9e3d21c00e631a8e03c19757468a
    Reviewed-on: https://chromium-review.googlesource.com/c/1437266
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627053}
    Former-commit-id: 58a93adcba6d767d53909f4e02249d8b5921bf30

---
+++ chrome/updater/win/main.cc
@@ -0,0 +1,9 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <windows.h>
+
+int APIENTRY wWinMain(HINSTANCE instance, HINSTANCE prev, wchar_t*, int) {
+  return 0;
+}
