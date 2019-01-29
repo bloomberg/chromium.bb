88ba28d4726a8c84aca93f2815a2dfb7d161796e
Chris Hamilton <chrisha@chromium.org>
2019-01-29 22:27:02 +0000

    Fix SiteCharacteristicsDataReader OnDataLoaded callbacks.
    Previously, a deleted reader could have its registered callbacks incorrectly invoked.
    
    Change-Id: I3b2eade67e2f1171f078b88ea28b36223dbac2cb
    Reviewed-on: https://chromium-review.googlesource.com/c/1443861
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Auto-Submit: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627184}
    Former-commit-id: 3ce7915a72992d2e3e0585f736f9935c3e2d22fb

--- chrome/browser/resource_coordinator/local_site_characteristics_data_reader.h
+++ chrome/browser/resource_coordinator/local_site_characteristics_data_reader.h
@@ -8,6 +8,7 @@
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/resource_coordinator/site_characteristics_data_reader.h"
 
 namespace resource_coordinator {
@@ -43,15 +44,27 @@ class LocalSiteCharacteristicsDataReader
   friend class LocalSiteCharacteristicsDataStoreTest;
   friend class LocalSiteCharacteristicsDataStore;
   FRIEND_TEST_ALL_PREFIXES(LocalSiteCharacteristicsDataReaderTest,
+                           DestroyingReaderCancelsPendingCallbacks);
+  FRIEND_TEST_ALL_PREFIXES(LocalSiteCharacteristicsDataReaderTest,
                            FreeingReaderDoesntCauseWriteOperation);
+  FRIEND_TEST_ALL_PREFIXES(LocalSiteCharacteristicsDataReaderTest,
+                           OnDataLoadedCallbackInvoked);
 
   // Private constructor, these objects are meant to be created by a site
   // characteristics data store.
   explicit LocalSiteCharacteristicsDataReader(
       scoped_refptr<internal::LocalSiteCharacteristicsDataImpl> impl);
 
+  // Runs the provided closure. This is used as a wrapper so that callbacks
+  // registered with the |impl_| by this reader are invalidated when the
+  // reader is destroyed.
+  void RunClosure(base::OnceClosure&& closure);
+
   // The LocalSiteCharacteristicDataInternal object we delegate to.
   const scoped_refptr<internal::LocalSiteCharacteristicsDataImpl> impl_;
+
+  // Used for invalidating callbacks.
+  base::WeakPtrFactory<LocalSiteCharacteristicsDataReader> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(LocalSiteCharacteristicsDataReader);
 };
