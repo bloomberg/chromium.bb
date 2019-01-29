1f6e0389b45a3e9b76fc64759b42e42b38c94ed4
François Beaufort <beaufort.francois@gmail.com>
2019-01-29 11:34:30 +0000

    Add Auto Picture-in-Picture Origin Trial
    Intent to Experiment: https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/eFZ3h_A3VTY
    
    Bug: 917303
    Change-Id: I96e19600bad93f53b2014bacea6aa49c9d74a269
    Reviewed-on: https://chromium-review.googlesource.com/c/1426703
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#626971}
    Former-commit-id: 88e1412127c0c03880977cc5b7ac0a84c69364cb

--- third_party/blink/renderer/core/html/media/html_video_element.cc
+++ third_party/blink/renderer/core/html/media/html_video_element.cc
@@ -54,6 +54,7 @@
 #include "third_party/blink/renderer/core/layout/layout_image.h"
 #include "third_party/blink/renderer/core/layout/layout_video.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
+#include "third_party/blink/renderer/core/origin_trials/origin_trials.h"
 #include "third_party/blink/renderer/platform/graphics/canvas_resource_provider.h"
 #include "third_party/blink/renderer/platform/graphics/gpu/extensions_3d_util.h"
 #include "third_party/blink/renderer/platform/graphics/graphics_context.h"
@@ -238,7 +239,8 @@ void HTMLVideoElement::ParseAttribute(
         GetLayoutObject()->IsVideo())
       ToLayoutVideo(GetLayoutObject())->IntrinsicSizeChanged();
   } else if (params.name == kAutopictureinpictureAttr &&
-             RuntimeEnabledFeatures::AutoPictureInPictureEnabled()) {
+             origin_trials::AutoPictureInPictureEnabled(
+                 GetExecutionContext())) {
     if (!params.new_value.IsNull()) {
       PictureInPictureController::From(GetDocument())
           .AddToAutoPictureInPictureElementsList(this);
