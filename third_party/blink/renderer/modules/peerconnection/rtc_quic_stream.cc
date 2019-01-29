5b9ab485aeddc79d76f636cbd7f479072afc1b30
Seth Hampson <shampson@chromium.org>
2019-01-29 18:18:22 +0000

    Removes slow RTCQuicStream wpt test.
    Bug: 925600
    Change-Id: I211712e998061394a844207ae41126fc72eea92f
    Reviewed-on: https://chromium-review.googlesource.com/c/1441991
    Reviewed-by: Steve Anton <steveanton@chromium.org>
    Commit-Queue: Seth Hampson <shampson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627068}
    Former-commit-id: 805ab1b61c09aca4737ad88128c17fb35ddea844

--- third_party/blink/renderer/modules/peerconnection/rtc_quic_stream.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_quic_stream.cc
@@ -13,8 +13,6 @@ namespace blink {
 
 // 6 MB allows a reasonable amount to buffer on the read and write side.
 // TODO(https://crbug.com/874296): Consider exposing these configurations.
-// TODO(shampson): Investigate why wpt get slow throughput when this value
-// is higher (24 MB).
 const uint32_t RTCQuicStream::kWriteBufferSize = 6 * 1024 * 1024;
 const uint32_t RTCQuicStream::kReadBufferSize = 6 * 1024 * 1024;
 
