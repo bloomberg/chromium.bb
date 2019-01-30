a2616b902d0cf7bc8ba7dc8a9ea466760dcfcf85
Rouslan Solomakhin <rouslan@chromium.org>
2019-01-30 22:51:13 +0000

    [Payment Handler][Desktop] De-jank skip-UI flow
    Before this patch, Chrome momentarily showed the browser payment sheet
    contents with "Pay" and "Cancel" buttons and the Chrome logo immediately
    before showing the payment handler page in the skip-UI flow. The skip-UI
    flow, however, is supposed to hide the browser payment sheet contents
    and display the payment handler page directly. This happened because
    Chrome was animating the payment handler page into view as a slide-in
    from the right, which looks good when transitioning from the browser
    payment sheet, but is not necessary in the skip-UI flow.
    
    This patch turns off the payment handler page animation (slide-in) in
    the skip-UI flow.
    
    After this patch, Chrome shows the payment handler page directly in
    skip-UI flow, without the slide-in-from-the-right animation. If Chrome
    showed the browser payment sheet contents, however, (for payment handler
    selection, for example), then Chrome slides the selected payment handler
    from the right as before.
    
    Bug: 926297
    Change-Id: I7a212ccbd3092e60c4375b11e9930d8d30ca9c77
    Reviewed-on: https://chromium-review.googlesource.com/c/1447014
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627639}
    Former-commit-id: 02eee55c5af0b572e3f04d6c0c94d0759672a337

--- chrome/browser/ui/views/payments/payment_request_dialog_view.cc
+++ chrome/browser/ui/views/payments/payment_request_dialog_view.cc
@@ -185,7 +185,7 @@ void PaymentRequestDialogView::ShowPaymentHandlerScreen(
               request_->spec(), request_->state(), this,
               request_->web_contents(), GetProfile(), url, std::move(callback)),
           &controller_map_),
-      /* animate = */ true);
+      /* animate = */ !request_->skipped_payment_request_ui());
   HideProcessingSpinner();
 }
 
