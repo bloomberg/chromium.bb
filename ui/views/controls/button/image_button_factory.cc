1507efd9f456c170e721f6577c0d97f8136cd126
Leonard Grey <lgrey@chromium.org>
2019-01-30 20:34:19 +0000

    Use default icon color when creating an image button instead of deriving from GG200.
    Bug: 924505, 922847
    Change-Id: If47ded91767f086e70b348a1c477e4788aa55426
    Reviewed-on: https://chromium-review.googlesource.com/c/1440902
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627576}
    Former-commit-id: 1c60cbae8de50bc45a70f002ada7f33d47d97785

--- ui/views/controls/button/image_button_factory.cc
+++ ui/views/controls/button/image_button_factory.cc
@@ -6,6 +6,7 @@
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/gfx/vector_icon_types.h"
+#include "ui/native_theme/native_theme.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/button/image_button.h"
@@ -37,6 +38,13 @@ ToggleImageButton* CreateVectorToggleImageButton(ButtonListener* listener) {
   ToggleImageButton* button = new ToggleImageButton(listener);
   ConfigureVectorImageButton(button);
   return button;
+}
+
+void SetImageFromVectorIcon(ImageButton* button, const gfx::VectorIcon& icon) {
+  SetImageFromVectorIconWithColor(
+      button, icon,
+      ui::NativeTheme::GetInstanceForNativeUi()->GetSystemColor(
+          ui::NativeTheme::kColorId_DefaultIconColor));
 }
 
 void SetImageFromVectorIcon(ImageButton* button,
--- ui/views/controls/button/image_button_factory_unittest.cc
+++ ui/views/controls/button/image_button_factory_unittest.cc
@@ -7,6 +7,7 @@
 #include "components/vector_icons/vector_icons.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/gfx/color_utils.h"
+#include "ui/native_theme/native_theme.h"
 #include "ui/views/animation/test/ink_drop_host_view_test_api.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/button/image_button.h"
@@ -30,12 +31,15 @@ TEST_F(ImageButtonFactoryTest, SetImageFromVectorIcon) {
   EXPECT_FALSE(button->GetImage(Button::STATE_DISABLED).isNull());
   EXPECT_EQ(color_utils::DeriveDefaultIconColor(SK_ColorRED),
             button->GetInkDropBaseColor());
-
-  // Default to GoogleGrey900.
-  SetImageFromVectorIcon(button, vector_icons::kCloseRoundedIcon);
-  EXPECT_EQ(color_utils::DeriveDefaultIconColor(gfx::kGoogleGrey900),
-            button->GetInkDropBaseColor());
   delete button;
 }
 
+TEST_F(ImageButtonFactoryTest, SetImageFromVectorIcon_Default) {
+  ImageButton* button = CreateVectorImageButton(nullptr);
+  SetImageFromVectorIcon(button, vector_icons::kCloseRoundedIcon);
+  EXPECT_EQ(button->GetNativeTheme()->GetSystemColor(
+                ui::NativeTheme::kColorId_DefaultIconColor),
+            button->GetInkDropBaseColor());
+  delete button;
+}
 }  // views
