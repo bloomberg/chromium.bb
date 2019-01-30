9f1f49076453313969809eb89eef4a09ec2f78ac
Lowell Manners <lowell@chromium.org>
2019-01-30 17:06:59 +0000

    Remove child_account_service_unittest dependency on FakeGCMS.
    This CL removes child_account_service_unittest's dependency on
    FakeGaiaCookieManagerService.  With this change, the test uses a
    GaiaCookieManagerService directly, passing GCMS a TestUrlLoaderFactory for
    fake responses to cookie-related requests.
    
    This is part of step 5 of the plan to eliminate FakeGCMS entirely:
    https://docs.google.com/document/d/1t0ZtuV7h-znzdItFgBW0aKPscAwWXIBuNZnNlEGgi7g/edit
    
    TBR=treib@chromium.org
    
    Change-Id: I78cb51a9f40deb512a7e68e993983ad5d259ef2d
    Bug: 907782
    Reviewed-on: https://chromium-review.googlesource.com/c/1402768
    Commit-Queue: Lowell Manners <lowell@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627464}
    Former-commit-id: 2e2a1fb6e96d3d0bf07159afb82d23ea08804df0

--- chrome/browser/signin/fake_gaia_cookie_manager_service_builder.cc
+++ chrome/browser/signin/fake_gaia_cookie_manager_service_builder.cc
@@ -10,7 +10,23 @@
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/signin/core/browser/fake_gaia_cookie_manager_service.h"
 #include "components/signin/core/browser/profile_oauth2_token_service.h"
+#include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
 #include "services/network/test/test_url_loader_factory.h"
+
+std::unique_ptr<KeyedService> BuildGaiaCookieManagerServiceWithURLLoader(
+    network::TestURLLoaderFactory* test_url_loader_factory,
+    content::BrowserContext* context) {
+  Profile* profile = Profile::FromBrowserContext(context);
+  return std::make_unique<GaiaCookieManagerService>(
+      ProfileOAuth2TokenServiceFactory::GetForProfile(profile),
+      ChromeSigninClientFactory::GetForProfile(profile),
+      base::BindRepeating(
+          [](network::TestURLLoaderFactory* test_url_loader_factory)
+              -> scoped_refptr<network::SharedURLLoaderFactory> {
+            return test_url_loader_factory->GetSafeWeakWrapper();
+          },
+          test_url_loader_factory));
+}
 
 std::unique_ptr<KeyedService> BuildFakeGaiaCookieManagerServiceWithURLLoader(
     network::TestURLLoaderFactory* test_url_loader_factory,
--- chrome/browser/signin/fake_gaia_cookie_manager_service_builder.h
+++ chrome/browser/signin/fake_gaia_cookie_manager_service_builder.h
@@ -6,7 +6,6 @@
 #define CHROME_BROWSER_SIGNIN_FAKE_GAIA_COOKIE_MANAGER_SERVICE_BUILDER_H_
 
 #include <memory>
-#include "services/network/test/test_url_loader_factory.h"
 
 class KeyedService;
 
@@ -18,8 +17,21 @@ namespace network {
 class TestURLLoaderFactory;
 }
 
+// Creates a GaiaCookieManagerService using the supplied
+// |test_url_loader_factory| and |context|.
+std::unique_ptr<KeyedService> BuildGaiaCookieManagerServiceWithURLLoader(
+    network::TestURLLoaderFactory* test_url_loader_factory,
+    content::BrowserContext* context);
+
 // Builds a FakeGaiaCookieManagerService which uses the provided
 // |test_url_loader_factory| for cookie-related requests.
+//
+// TODO(https://crbug.com/907782): Convert all test code to use
+// GaiaCookieManagerService directly, passing a TestURLLoaderFactory when
+// fakes are needed.
+//
+// Once that's done, the method below can be deleted, and this file can be
+// renamed to something like gaia_cookie_manager_service_test_util.cc
 std::unique_ptr<KeyedService> BuildFakeGaiaCookieManagerServiceWithURLLoader(
     network::TestURLLoaderFactory* test_url_loader_factory,
     content::BrowserContext* context);
