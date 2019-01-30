9e9689a67de764088342d7106df94debb2039f41
Takuto Ikuta <tikuta@chromium.org>
2019-01-30 22:21:54 +0000

    [mojo] include mojo header in /chrome/browser/chromeos/printing
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    This CL was uploaded by git cl split.
    
    R=thestig@chromium.org
    
    CC: rockot@google.com
    Bug: 922875
    Change-Id: I6d24f779f6ae7c87b7b4eb907e5b6205318fcd2b
    Reviewed-on: https://chromium-review.googlesource.com/c/1446988
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627622}
    Former-commit-id: 44dd957408d824a21ef4da24a6a6aca4c403f6c1

--- chrome/browser/chromeos/printing/usb_printer_util.cc
+++ chrome/browser/chromeos/printing/usb_printer_util.cc
@@ -18,6 +18,7 @@
 #include "chrome/grit/generated_resources.h"
 #include "chromeos/printing/printer_configuration.h"
 #include "device/usb/public/cpp/usb_utils.h"
+#include "device/usb/public/mojom/device_enumeration_options.mojom.h"
 #include "device/usb/public/mojom/device_manager.mojom.h"
 #include "device/usb/usb_device.h"
 #include "ui/base/l10n/l10n_util.h"
