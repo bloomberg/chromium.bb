4691038d5489a82b1033c39431f29c833dbcf6f5
Lei Zhang <thestig@chromium.org>
2019-01-31 05:10:12 +0000

    Take the process limit into account in GetMaxRendererProcessCount() on Linux.
    On Linux, given sufficient RAM, GetMaxRendererProcessCount() can now
    return a value as high as (RLIMIT_NPROC / 2), instead of being capped to
    a hard-coded maximum. This lets the browser start more processes and
    have more process isolation between renderers if the environment can
    support it.
    
    This also makes the browser behave better when running in an environment
    that is constrained by the process limit.
    
    Fix the code to work on 32-bit Linux.
    
    BUG=104689
    
    Change-Id: I0e36ceac9acc0cb1036f8886d0e665fc3738f5c3
    Reviewed-on: https://chromium-review.googlesource.com/c/1442534
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627792}
    Former-commit-id: 535b3365d33f2f9f9782bc8c66718fc708245ed9

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -233,6 +233,11 @@
 #include "content/browser/compositor/image_transport_factory.h"
 #endif
 
+#if defined(OS_LINUX)
+#include <sys/resource.h>
+#include <sys/time.h>
+#endif
+
 #if defined(OS_MACOSX)
 #include "content/browser/mach_broker_mac.h"
 #endif
@@ -1264,6 +1269,28 @@ void AddCorbExceptionForPluginOnIOThread(int process_id) {
       base::BindOnce(&AddCorbExceptionForPluginOnUIThread, process_id));
 }
 
+#if !defined(OS_ANDROID) && !defined(OS_CHROMEOS)
+static constexpr size_t kUnknownPlatformProcessLimit = 0;
+
+// Returns the process limit from the system. Use |kUnknownPlatformProcessLimit|
+// to indicate failure and std::numeric_limits<size_t>::max() to indicate
+// unlimited.
+size_t GetPlatformProcessLimit() {
+#if defined(OS_LINUX)
+  struct rlimit limit;
+  if (getrlimit(RLIMIT_NPROC, &limit) != 0)
+    return kUnknownPlatformProcessLimit;
+
+  if (limit.rlim_cur == RLIM_INFINITY)
+    return std::numeric_limits<size_t>::max();
+  return base::saturated_cast<size_t>(limit.rlim_cur);
+#else
+  // TODO(https://crbug.com/104689): Implement on other platforms.
+  return kUnknownPlatformProcessLimit;
+#endif  // defined(OS_LINUX)
+}
+#endif  // !defined(OS_ANDROID) && !defined(OS_CHROMEOS)
+
 }  // namespace
 
 // Held by the RPH and used to control an (unowned) ConnectionFilterImpl from
@@ -1369,8 +1396,15 @@ RenderProcessHostImpl::GetInProcessRendererThreadTaskRunnerForTesting() {
 #if !defined(OS_ANDROID) && !defined(OS_CHROMEOS)
 // static
 size_t RenderProcessHostImpl::GetPlatformMaxRendererProcessCount() {
-  static constexpr size_t kMaxRendererProcessCount = 82;
-  return kMaxRendererProcessCount;
+  // Set the limit to half of the system limit to leave room for other programs.
+  size_t limit = GetPlatformProcessLimit() / 2;
+
+  // If the system limit is unavailable, use a fallback value instead.
+  if (limit == kUnknownPlatformProcessLimit) {
+    static constexpr size_t kMaxRendererProcessCount = 82;
+    limit = kMaxRendererProcessCount;
+  }
+  return limit;
 }
 #endif
 
@@ -1425,9 +1459,12 @@ size_t RenderProcessHost::GetMaxRendererProcessCount() {
     max_count /= kEstimatedWebContentsMemoryUsage;
 
     static constexpr size_t kMinRendererProcessCount = 3;
-    max_count = base::ClampToRange(
-        max_count, kMinRendererProcessCount,
-        RenderProcessHostImpl::GetPlatformMaxRendererProcessCount());
+    static const size_t kMaxRendererProcessCount =
+        RenderProcessHostImpl::GetPlatformMaxRendererProcessCount();
+    DCHECK_LE(kMinRendererProcessCount, kMaxRendererProcessCount);
+
+    max_count = base::ClampToRange(max_count, kMinRendererProcessCount,
+                                   kMaxRendererProcessCount);
   }
   return max_count;
 #endif
