7e5fbc28c486538ad08ee37e4568a580abd22587
khmel@chromium.org <khmel@chromium.org>
2019-01-31 22:01:25 +0000

    arc: Add visual information for ARC graphics tracing tool.
    This also fixes the issue when tool work incorrectly for NYC builds.
    
    BUG=b/122555793
    TEST=Locally
    
    Change-Id: I4e0f15f3e549190a7a2f208ccf72f5497e543a4d
    Reviewed-on: https://chromium-review.googlesource.com/c/1448696
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yury Khmel <khmel@chromium.org>
    Auto-Submit: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628102}
    Former-commit-id: 904877749b8ff0f466da6ee6479aabf9a9c460ad

--- chrome/browser/ui/webui/chromeos/arc_graphics_tracing/arc_graphics_tracing_handler.cc
+++ chrome/browser/ui/webui/chromeos/arc_graphics_tracing/arc_graphics_tracing_handler.cc
@@ -4,20 +4,77 @@
 
 #include "chrome/browser/ui/webui/chromeos/arc_graphics_tracing/arc_graphics_tracing_handler.h"
 
+#include <map>
 #include <string>
 
+#include "ash/public/cpp/shell_window_ids.h"
+#include "base/base64.h"
 #include "base/bind.h"
 #include "base/memory/ref_counted_memory.h"
 #include "base/task/post_task.h"
 #include "base/values.h"
 #include "chrome/browser/chromeos/arc/tracing/arc_tracing_graphics_model.h"
 #include "chrome/browser/chromeos/arc/tracing/arc_tracing_model.h"
+#include "components/exo/shell_surface_util.h"
+#include "components/exo/wm_helper_chromeos.h"
 #include "content/public/browser/tracing_controller.h"
 #include "content/public/browser/web_ui.h"
+#include "ui/aura/client/aura_constants.h"
+#include "ui/base/ui_base_features.h"
+#include "ui/gfx/codec/png_codec.h"
 
 namespace chromeos {
 
 namespace {
+
+constexpr char kKeyIcon[] = "icon";
+constexpr char kKeyTitle[] = "title";
+constexpr char kKeyTasks[] = "tasks";
+constexpr char kTaskIdPrefix[] = "org.chromium.arc.";
+
+// Scans for all ARC windows and and extracts the title and optionally icon.
+void CreateTaskMap(aura::Window* window, base::DictionaryValue* tasks) {
+  if (!window->IsVisible())
+    return;
+
+  // ARC window is top level window, all its parents have type not set.
+  if (window->type() == aura::client::WINDOW_TYPE_UNKNOWN) {
+    for (aura::Window* child_window : window->children())
+      CreateTaskMap(child_window, tasks);
+    return;
+  }
+
+  // Verifies if this is top-level ARC window.
+  const std::string* arc_app_id = exo::GetShellApplicationId(window);
+  if (!arc_app_id)
+    return;
+
+  // Root surface may not have task id.
+  const size_t prefix_pos = arc_app_id->find(kTaskIdPrefix);
+  if (prefix_pos)
+    return;
+
+  base::DictionaryValue task_information;
+  task_information.SetKey(kKeyTitle, base::Value(window->GetTitle()));
+
+  const gfx::ImageSkia* app_icon =
+      window->GetProperty(aura::client::kAppIconKey);
+  if (app_icon) {
+    std::vector<unsigned char> png_data;
+    if (gfx::PNGCodec::EncodeBGRASkBitmap(
+            app_icon->GetRepresentation(1.0f).GetBitmap(),
+            false /* discard_transparency */, &png_data)) {
+      const std::string png_data_as_string(
+          reinterpret_cast<const char*>(&png_data[0]), png_data.size());
+      std::string icon_content;
+      base::Base64Encode(png_data_as_string, &icon_content);
+      task_information.SetKey(kKeyIcon, base::Value(icon_content));
+    }
+  }
+
+  tasks->SetKey(arc_app_id->c_str() + strlen(kTaskIdPrefix),
+                std::move(task_information));
+}
 
 std::unique_ptr<base::Value> BuildGraphicsModel(const std::string& data) {
   arc::ArcTracingModel common_model;
@@ -32,7 +89,19 @@ std::unique_ptr<base::Value> BuildGraphicsModel(const std::string& data) {
     return nullptr;
   }
 
-  return graphics_model.Serialize();
+  std::unique_ptr<base::DictionaryValue> model = graphics_model.Serialize();
+  base::DictionaryValue tasks;
+  // Scan for ARC++ windows
+  // TODO(https://crbug.com/887156): Fix the mash.
+  if (!features::IsMultiProcessMash()) {
+    CreateTaskMap(
+        exo::WMHelperChromeOS::GetInstance()->GetPrimaryDisplayContainer(
+            ash::kShellWindowId_DefaultContainer),
+        &tasks);
+  }
+  model->SetKey(kKeyTasks, std::move(tasks));
+
+  return model;
 }
 
 }  // namespace
