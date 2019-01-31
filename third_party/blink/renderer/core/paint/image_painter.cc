c5199ba92eafc56306c3bae431ee714e9c75117b
Luna Lu <loonybear@chromium.org>
2019-01-31 11:29:47 +0000

    Reland "Move image policy violation report to paint"
    This is a reland of fffa67c938270fcca83bc5888d2e9cf6545a16a1 [formerly bcfc7924f94437f61fb599e19769fdcf9cd241ee]
    File edited:
    
    Added missing test expect for layout test images/feature-policy-oversized-images-resize.html
    
    Original change's description:
    > Move image policy violation report to paint
    >
    > Currently image policy (unoptimized-images, legacy-formats,
    > oversized-images) are reported every time when a image policy
    > is violated, essentially multiple times in layout.
    >
    > Layout can have multiple cycles before the image is being painted,
    > this could flush / spam the reports.
    >
    > This CL moves the report to image painter, which means the violation
    > will be reported every time when the image is being repainted.
    >
    > Bug: 926199
    > Change-Id: I8bbfe26ac1df7d57f4f4d7e077c2c194e8b1ea71
    > Reviewed-on: https://chromium-review.googlesource.com/c/1443035
    > Commit-Queue: Luna Lu <loonybear@chromium.org>
    > Reviewed-by: Ehsan Karamad <ekaramad@chromium.org>
    > Reviewed-by: Fredrik Söderquist <fs@opera.com>
    > Cr-Commit-Position: refs/heads/master@{#627412}
    
    Bug: 926199
    Change-Id: Id1e5fea120c7be2f2febda1d4833051de6d19e0c
    Reviewed-on: https://chromium-review.googlesource.com/c/1446217
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Luna Lu <loonybear@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627901}
    Former-commit-id: 35014229435cc6388ac2d15a34d84bf591e5e946

--- third_party/blink/renderer/core/paint/image_painter.cc
+++ third_party/blink/renderer/core/paint/image_painter.cc
@@ -197,6 +197,9 @@ void ImagePainter::PaintIntoRect(GraphicsContext& context,
     placeholder_image->SetIconAndTextScaleFactor(
         layout_image_.GetFrame()->PageZoomFactor());
     image = std::move(placeholder_image);
+
+    // Report image policy violation if needed.
+    layout_image_.ReportImagePolicyViolation();
   }
 
   context.DrawImage(
