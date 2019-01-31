c98081126e1656ad8ef242c979b0c17126b25e8b
Takuto Ikuta <tikuta@chromium.org>
2019-01-31 02:04:59 +0000

    [mojo] include mojo header in /chrome/browser/extensions/api/input_ime
    This is preparation CL to use forward declaration only header in mojo's
    generated code.
    If we include forward declaration only header in mojo's generated code, we need
    to include mojo header files in some places where definition is necessary.
    
    master CL is in
    https://chromium-review.googlesource.com/c/chromium/src/+/1438975
    
    This CL was uploaded by git cl split.
    
    R=komatsu@chromium.org
    
    CC: rockot@google.com
    Bug: 922875
    Change-Id: Ib2017f896774e5e5984b09dfb63a4e828c81d11c
    Reviewed-on: https://chromium-review.googlesource.com/c/1447053
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Shu Chen <shuchen@chromium.org>
    Reviewed-by: Shu Chen <shuchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#627741}
    Former-commit-id: 004b2eb7f3938e398fbf0c3fa0cfd0358944552b

--- chrome/browser/extensions/api/input_ime/input_ime_api_chromeos.cc
+++ chrome/browser/extensions/api/input_ime/input_ime_api_chromeos.cc
@@ -27,6 +27,7 @@
 #include "ui/base/ime/chromeos/input_method_manager.h"
 #include "ui/base/ime/ime_engine_handler_interface.h"
 #include "ui/base/ui_base_features.h"
+#include "ui/keyboard/public/keyboard_config.mojom.h"
 
 namespace input_ime = extensions::api::input_ime;
 namespace input_method_private = extensions::api::input_method_private;
