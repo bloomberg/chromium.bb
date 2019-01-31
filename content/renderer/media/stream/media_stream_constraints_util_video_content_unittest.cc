df2539dc5de7b2ea6f0741a93a4e7ae85a9d3d39
Guido Urdaneta <guidou@chromium.org>
2019-01-31 21:16:44 +0000

    Move mediastream blink exported implementation to a directory
    These files are intended to be in blink/public only temporarily.
    This organization facilitates ownership and avoids pollution of
    the main exported/ directory with files that do not match the
    style of the rest of the files in the directory.
    
    Drive-by: rename platform_media_stream_source.cc|h to
    web_platform_media_stream_source.cc|h. The types in these files
    have a Web prefix because they are exposed via methods in
    other Web classes. At some point they will no longer be exposed
    this way and will be removed from public/ too.
    
    Bug: 921006
    Change-Id: I488fd288ddc47e14e50ea5b1450d3c153959bbbc
    Reviewed-on: https://chromium-review.googlesource.com/c/1448411
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628077}
    Former-commit-id: 5849a7a7ef5555084247a8cf298f1a054872d271

--- content/renderer/media/stream/media_stream_constraints_util_video_content_unittest.cc
+++ content/renderer/media/stream/media_stream_constraints_util_video_content_unittest.cc
@@ -11,7 +11,7 @@
 #include "content/renderer/media/stream/mock_constraint_factory.h"
 #include "media/base/limits.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/blink/public/platform/modules/mediastream/platform_media_stream_source.h"
+#include "third_party/blink/public/platform/modules/mediastream/web_platform_media_stream_source.h"
 #include "third_party/blink/public/platform/web_media_constraints.h"
 
 namespace content {
