16e3ced346ef2b072d33e99edf0164254b093eb9
Matt Reynolds <mattreynolds@google.com>
2019-02-01 22:20:05 +0000

    Reland "Parse HID report descriptor for report info"
    This is a reland of 85dc2f184d974cfa06c9c01f47fd7033d95739de [formerly 6a235ad71f5f34c4b43a5c6be4a78c96cb748921]
    
    This reland includes a fix for a use-before-initialization
    error in HidReportDescriptor that caused devices that do not
    use report IDs to sometimes report that they did.
    
    Original change's description:
    > Parse HID report descriptor for report info
    >
    > The previous HID report descriptor parser was designed to only
    > extract the maximum size of each type of report and did not preserve
    > information about the individual fields within a report. This CL
    > adds a new parser that builds a hierarchical representation of the
    > reports supported by the HID device.
    >
    > BUG=890096
    >
    > Change-Id: I4ac3bf883a843bec00576bfad78958d42f53abc7
    > Reviewed-on: https://chromium-review.googlesource.com/c/1257897
    > Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#626816}
    
    BUG=890096
    
    Change-Id: I8991162f0bb8c442d4e7cdce5940dd801ab5bfa0
    Reviewed-on: https://chromium-review.googlesource.com/c/1446661
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628478}
    Former-commit-id: b47da7ddf8e3252428c67dc14231011fc5940c9d

--- services/device/public/cpp/hid/hid_device_filter.h
+++ services/device/public/cpp/hid/hid_device_filter.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef DEVICE_HID_HID_DEVICE_FILTER_H_
-#define DEVICE_HID_HID_DEVICE_FILTER_H_
+#ifndef SERVICES_DEVICE_PUBLIC_CPP_HID_HID_DEVICE_FILTER_H_
+#define SERVICES_DEVICE_PUBLIC_CPP_HID_HID_DEVICE_FILTER_H_
 
 #include <stdint.h>
 #include <vector>
@@ -41,4 +41,4 @@ class HidDeviceFilter {
 
 }  // namespace device
 
-#endif  // DEVICE_HID_HID_DEVICE_FILTER_H_
+#endif  // SERVICES_DEVICE_PUBLIC_CPP_HID_HID_DEVICE_FILTER_H_
