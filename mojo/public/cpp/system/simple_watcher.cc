12200858c72333783c91b6c8365802c935d1b566
ssid <ssid@chromium.org>
2019-02-01 18:07:04 +0000

    Add toplevel trace event for mojo message handlers
    The mojo message handlers are always grouped together as tasks posted by
    "Notify" function. To identify the root cause, add a trace event using
    the heap profiler tag. This only adds event to simple watcher, which
    seems most common.
    
    BUG=927206
    
    Change-Id: Id17c1d6ad396c994242a5a2ab92ed1ff0df42d48
    Reviewed-on: https://chromium-review.googlesource.com/c/1437727
    Commit-Queue: ssid <ssid@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#628385}
    Former-commit-id: 65df96be053a67b1b30104b7a4124db0f4c7385f

--- mojo/public/cpp/system/simple_watcher.cc
+++ mojo/public/cpp/system/simple_watcher.cc
@@ -11,6 +11,7 @@
 #include "base/synchronization/lock.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/trace_event/heap_profiler.h"
+#include "base/trace_event/trace_event.h"
 #include "mojo/public/c/system/trap.h"
 
 namespace mojo {
@@ -283,6 +284,10 @@ void SimpleWatcher::OnHandleReady(int watch_id,
   // NOTE: It's legal for |callback| to delete |this|.
   if (!callback.is_null()) {
     TRACE_HEAP_PROFILER_API_SCOPED_TASK_EXECUTION event(heap_profiler_tag_);
+    // Lot of janks caused are grouped to OnHandleReady tasks. This trace event helps identify the
+    // cause of janks. It is ok to pass |heap_profiler_tag_| here since it is a string literal.
+    // TODO(927206): Consider renaming |heap_profiler_tag_|.
+    TRACE_EVENT0("toplevel", heap_profiler_tag_);
 
     base::WeakPtr<SimpleWatcher> weak_self = weak_factory_.GetWeakPtr();
     callback.Run(result, state);
