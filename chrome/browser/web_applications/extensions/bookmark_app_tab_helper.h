5b909fd07236118bc10faa8f7bc23a0a410debb2
Alexey Baskakov <loyso@chromium.org>
2019-02-01 06:39:00 +0000

    WebApp: Maintain AppId in WebAppTabHelper on extension install and uninstall.
    "Create Shortcut" button may implicitly promote already opened tabs within the
    navigation scope to WebApp tab instances.
    
    "Remove from Chrome" in chrome://apps may demote already opened tabs within the
    navigation scope to regular web sites.
    
    Set or reset app_id using ExtensionRegistryObserver subscription.
    
    Bug: 915034
    Change-Id: I0bd3b8dc910f2ef3d516e64a0e6a628741a50fd0
    Reviewed-on: https://chromium-review.googlesource.com/c/1426247
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628253}
    Former-commit-id: 3dae106a7c82bfa8982a389d738b968bf39b5c77

--- chrome/browser/web_applications/extensions/bookmark_app_tab_helper.h
+++ chrome/browser/web_applications/extensions/bookmark_app_tab_helper.h
@@ -6,7 +6,9 @@
 #define CHROME_BROWSER_WEB_APPLICATIONS_EXTENSIONS_BOOKMARK_APP_TAB_HELPER_H_
 
 #include "base/macros.h"
+#include "base/scoped_observer.h"
 #include "chrome/browser/web_applications/components/web_app_tab_helper_base.h"
+#include "extensions/browser/extension_registry_observer.h"
 
 namespace content {
 class WebContents;
@@ -15,9 +17,11 @@ class WebContents;
 namespace extensions {
 
 class Extension;
+class ExtensionRegistry;
 
 // Allows to associate a tab with bookmark app.
-class BookmarkAppTabHelper : public web_app::WebAppTabHelperBase {
+class BookmarkAppTabHelper : public web_app::WebAppTabHelperBase,
+                             public ExtensionRegistryObserver {
  public:
   explicit BookmarkAppTabHelper(content::WebContents* web_contents);
   ~BookmarkAppTabHelper() override;
@@ -36,11 +40,23 @@ class BookmarkAppTabHelper : public web_app::WebAppTabHelperBase {
   bool IsUserInstalled() const override;
   bool IsFromInstallButton() const override;
 
+  // ExtensionRegistryObserver:
+  void OnExtensionInstalled(content::BrowserContext* browser_context,
+                            const Extension* extension,
+                            bool is_update) override;
+  void OnExtensionUninstalled(content::BrowserContext* browser_context,
+                              const Extension* extension,
+                              UninstallReason reason) override;
+  void OnShutdown(ExtensionRegistry* registry) override;
+
  private:
   // Get a pointer from app_id_. Semantically, we use app_id_ as a weak
   // reference. It might become nullptr in unforeseen circumstances (Uninstall).
   // TODO(loyso): Provide guarantees for app_id_. crbug.com/915034
   const Extension* GetExtension() const;
+
+  ScopedObserver<ExtensionRegistry, ExtensionRegistryObserver> scoped_observer_{
+      this};
 
   DISALLOW_COPY_AND_ASSIGN(BookmarkAppTabHelper);
 };
