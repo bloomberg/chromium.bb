e8103ba44eed7831ec9ffd2f91b6c9236d057ad3
sangwoo.ko <sangwoo.ko@navercorp.com>
2019-02-02 12:50:46 +0000

    Activate the next/previous tab on horizonal scroll
    This CL allows us to use mouse with horizontal scroll function,
    such as tilting wheel or separate wheel for h-scroll.
    
    Bug: 2384
    Change-Id: I770c629bb2391343ce66a398e4d73ad69dde569e
    Reviewed-on: https://chromium-review.googlesource.com/c/1414537
    Commit-Queue: Sang Woo Ko <sangwoo108@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628588}
    Former-commit-id: 90496b3b21c0aee9844e7eaf3b457abfa761092f

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -1341,7 +1341,7 @@ float TabStrip::GetHoverOpacityForRadialHighlight() const {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-// TabStrip, views::View overrides:
+// TabStrip, views::AccessiblePaneView overrides:
 
 void TabStrip::Layout() {
   // Only do a layout if our size changed.
@@ -1350,6 +1350,34 @@ void TabStrip::Layout() {
   if (IsDragSessionActive())
     return;
   DoLayout();
+}
+
+bool TabStrip::OnMouseWheel(const ui::MouseWheelEvent& event) {
+  // Change the selected tab on horizontal wheel scrolls.
+  // Honor wheel scrolls over the tabs themselves, but not the NTB/surrounding
+  // space.
+  if ((!event.x_offset() && !event.IsShiftDown()) || tab_count() < 2 ||
+      !FindTabHitByPoint(event.location()))
+    return false;
+
+  accumulated_horizontal_scroll_ +=
+      (event.x_offset() ? event.x_offset() : event.y_offset());
+
+  int horizontal_offset =
+      accumulated_horizontal_scroll_ / ui::MouseWheelEvent::kWheelDelta;
+  if (!horizontal_offset)
+    return true;
+
+  accumulated_horizontal_scroll_ %= ui::MouseWheelEvent::kWheelDelta;
+
+  int new_active_index =
+      (controller_->GetActiveIndex() + horizontal_offset) % tab_count();
+  if (new_active_index < 0)
+    new_active_index += tab_count();
+
+  DCHECK(IsValidModelIndex(new_active_index));
+  controller_->SelectTab(new_active_index);
+  return true;
 }
 
 void TabStrip::PaintChildren(const views::PaintInfo& paint_info) {
