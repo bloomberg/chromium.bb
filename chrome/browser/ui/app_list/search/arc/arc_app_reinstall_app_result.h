88b004417c45ef0325eaca99d2d80fc9276380e8
Rob Schonberger <robsc@chromium.org>
2019-02-04 08:43:25 +0000

    Changes to support fast app reinstall into the zero-state search results.
    Does so by:
    
    1. creating a new Search Provider
    2. Using this new search provider to call the fast app reinstall api in Mojom.
    3. Taking the results and turning them into new types of App Results.
    
    Note that this is intended to give precisely 1 result in the 6th
    position out of 5 in the search results. No attempt to re-rank is made right now.
    
    Adds UMA histogram logs for retrieval of JSON from the new MOJOM
    endpoint.
    
    Add UMA histogram for click on result.
    
    Change-Id: I5799870a931e4b1d9dfefbf0c0046154abc9a47a
    Bug: 911427
    Reviewed-on: https://chromium-review.googlesource.com/c/1260529
    Commit-Queue: Rob Schonberger <robsc@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628688}
    Former-commit-id: a3b8f0d50b69cc26826df7fb011e621ad88028b3

---
+++ chrome/browser/ui/app_list/search/arc/arc_app_reinstall_app_result.h
@@ -0,0 +1,35 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_APP_LIST_SEARCH_ARC_ARC_APP_REINSTALL_APP_RESULT_H_
+#define CHROME_BROWSER_UI_APP_LIST_SEARCH_ARC_ARC_APP_REINSTALL_APP_RESULT_H_
+
+#include "base/macros.h"
+#include "chrome/browser/ui/app_list/search/chrome_search_result.h"
+#include "components/arc/common/app.mojom.h"
+#include "ui/gfx/image/image_skia.h"
+
+namespace app_list {
+
+// A ChromeSearchResult that shows an App Reinstall candidate result. These are
+// Arc++ apps that can be installed on this device. Opens the app in the play
+// store when Open is called.
+class ArcAppReinstallAppResult : public ChromeSearchResult {
+ public:
+  ArcAppReinstallAppResult(
+      const arc::mojom::AppReinstallCandidatePtr& mojom_data,
+      const gfx::ImageSkia& skia_icon,
+      bool is_recommendation);
+  ~ArcAppReinstallAppResult() override;
+
+  // ArcAppResult:
+  void Open(int event_flags) override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(ArcAppReinstallAppResult);
+};
+
+}  // namespace app_list
+
+#endif  // CHROME_BROWSER_UI_APP_LIST_SEARCH_ARC_ARC_APP_REINSTALL_APP_RESULT_H_
