b0d7bc961a5a5114e61d412a8c506e387445a0ba
LaMont Jones <lamontjones@chromium.org>
2019-02-04 14:35:40 -0700

    portage_util: add yet more logging for chromeos-verison.sh failure.
    Yet more logging for chromium:917099.
    
    BUG=None
    TEST=None
    
    Change-Id: I737b59cc5ab26a7f202ccb290cd9d2a27344bf6c
    Reviewed-on: https://chromium-review.googlesource.com/1452878
    Commit-Ready: LaMont Jones <lamontjones@chromium.org>
    Tested-by: LaMont Jones <lamontjones@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- lib/portage_util.py
+++ lib/portage_util.py
@@ -911,17 +911,22 @@ class EBuild(object):
       # Crbug.com/917099 - Let's get more information for debugging, at least
       # temporarily.
       fsck_out = {}
+      packed_refs = ''
       for srcdir in srcdirs:
         fsck_out[srcdir] = self._RunCommand(['git', 'fsck', srcdir],
                                             error_code_ok=True)
       # TODO(crbug.com/917099): Remove this long-term because it's verbose.
-      ls_out = self._RunCommand(['ls', '-lRa'] + srcdirs, error_code_ok=True)
+      ls_out = self._RunCommand(['ls', '-lLRa'] + srcdirs, error_code_ok=True)
+      for srcdir in srcdirs + ['.']:
+        path = os.path.join(srcdir, '.git/packed-refs')
+        if os.path.exists(path):
+          packed_refs += '%s:\n%s\n' % (path, osutils.ReadFile(path))
       raise Error(
           'Package %s has a chromeos-version.sh script but failed:\n'
           'return code = %s\nstdout = %s\nstderr = %s\ndir listing = %s\n'
-          'git fsck = %s\nsrcdirs = %s\n',
+          'git fsck = %s\nsrcdirs = %s\npacked-refs file contents:\n%s\n',
           self.pkgname, result.returncode, result.output, result.error,
-          ls_out, fsck_out, srcdirs)
+          ls_out, fsck_out, srcdirs, packed_refs)
 
     # Sanity check: disallow versions that will be larger than the 9999 ebuild
     # used by cros-workon.
--- lib/portage_util_unittest.py
+++ lib/portage_util_unittest.py
@@ -730,6 +730,7 @@ class EBuildRevWorkonTest(cros_test_lib.MockTempDirTestCase):
                      return_value=portage_util.SourceInfo(
                          projects=None, srcdirs=[], subdirs=[], subtrees=[]))
     run = self.PatchObject(cros_build_lib, 'RunCommand')
+    readfile = self.PatchObject(osutils, 'ReadFile')
 
     # Reject no output.
     run.return_value = cros_build_lib.CommandResult(
@@ -737,8 +738,10 @@ class EBuildRevWorkonTest(cros_test_lib.MockTempDirTestCase):
     self.assertRaises(portage_util.Error,
                       self.m_ebuild.GetVersion, None, None, '1234')
     # Sanity check.
-    self.assertEqual(exists.call_count, 1)
+    self.assertEqual(exists.call_count, 2)
     exists.reset_mock()
+    self.assertEqual(readfile.call_count, 1)
+    readfile.reset_mock()
 
     # Reject simple output.
     run.return_value = cros_build_lib.CommandResult(
@@ -746,8 +749,10 @@ class EBuildRevWorkonTest(cros_test_lib.MockTempDirTestCase):
     self.assertRaises(portage_util.Error,
                       self.m_ebuild.GetVersion, None, None, '1234')
     # Sanity check.
-    self.assertEqual(exists.call_count, 1)
+    self.assertEqual(exists.call_count, 2)
     exists.reset_mock()
+    self.assertEqual(readfile.call_count, 1)
+    readfile.reset_mock()
 
     # Reject error.
     run.return_value = cros_build_lib.CommandResult(
@@ -755,7 +760,8 @@ class EBuildRevWorkonTest(cros_test_lib.MockTempDirTestCase):
     self.assertRaises(portage_util.Error,
                       self.m_ebuild.GetVersion, None, None, '1234')
     # Sanity check.
-    self.assertEqual(exists.call_count, 1)
+    self.assertEqual(exists.call_count, 2)
+    self.assertEqual(readfile.call_count, 1)
 
   def testVersionScriptTooHighVersion(self):
     """Reject scripts that output high version numbers."""
