bb9d37c29ca1e44b0726535bb0b645f8329e4858
Ross McIlroy <rmcilroy@chromium.org>
2019-02-04 11:02:46 +0000

    [Gin] Add feature flag to control V8's bytecode flushing.
    BUG=v8:8395
    
    Change-Id: I610ddd3318203811506a2b43ebfc3ef2bf81e95f
    Reviewed-on: https://chromium-review.googlesource.com/c/1418232
    Reviewed-by: Mythri Alle <mythria@chromium.org>
    Commit-Queue: Ross McIlroy <rmcilroy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628703}
    Former-commit-id: f4100321f00f544d64eefe9e01aa5dbe03f3df38

--- gin/v8_initializer.cc
+++ gin/v8_initializer.cc
@@ -219,6 +219,12 @@ void V8Initializer::Initialize(IsolateHolder::ScriptMode mode) {
     v8::V8::SetFlagsFromString(no_optimize, sizeof(no_optimize) - 1);
   }
 
+  if (!base::FeatureList::IsEnabled(features::kV8FlushBytecode)) {
+    static const char no_flush_bytecode[] = "--no-flush-bytecode";
+    v8::V8::SetFlagsFromString(no_flush_bytecode,
+                               sizeof(no_flush_bytecode) - 1);
+  }
+
   if (IsolateHolder::kStrictMode == mode) {
     static const char use_strict[] = "--use_strict";
     v8::V8::SetFlagsFromString(use_strict, sizeof(use_strict) - 1);
