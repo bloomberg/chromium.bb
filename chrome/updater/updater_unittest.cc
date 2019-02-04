dc32a4ae62798af8a1f8762f1ceb20fe46c57701
Boris Vidolov <borisv@chromium.org>
2019-02-04 23:27:42 +0000

    Mac shim for the updater. BUG: 928344 Change-Id: I0fb7854dcddbc5c248ac693ff5462d1f9f53c3f0 Reviewed-on: https://chromium-review.googlesource.com/c/1450410 Commit-Queue: Boris Vidolov <borisv@chromium.org> Reviewed-by: Sorin Jianu <sorin@chromium.org> Cr-Commit-Position: refs/heads/master@{#628930} Former-commit-id: c2dc789a7613f3eb06588621cbc095311ab6f0d8

--- chrome/updater/updater_unittest.cc
+++ chrome/updater/updater_unittest.cc
@@ -2,24 +2,34 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <windows.h>
-
 #include "base/command_line.h"
 #include "base/files/file_path.h"
+#include "base/path_service.h"
 #include "base/process/launch.h"
 #include "base/process/process.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+#if defined(OS_WIN)
+#define EXECUTABLE_EXTENSION ".exe"
+#else
+#define EXECUTABLE_EXTENSION ""
+#endif
+
 // Tests the updater process returns 0 when run with no arguments.
 TEST(UpdaterTest, UpdaterExitCode) {
-  base::FilePath::CharType buffer[MAX_PATH] = {0};
-  ASSERT_NE(0u, GetModuleFileName(0, buffer, base::size(buffer)));
-  const base::FilePath updater =
-      base::FilePath(buffer).DirName().Append(FILE_PATH_LITERAL("updater.exe"));
+  base::FilePath this_executable_path;
+
+  ASSERT_TRUE(base::PathService::Get(base::FILE_EXE, &this_executable_path));
+
+  const base::FilePath updater = this_executable_path.DirName().Append(
+      FILE_PATH_LITERAL("updater" EXECUTABLE_EXTENSION));
   base::LaunchOptions options;
+
+#if defined(OS_WIN)
   options.start_hidden = true;
+#endif
   auto process = base::LaunchProcess(base::CommandLine(updater), options);
   ASSERT_TRUE(process.IsValid());
   int exit_code = -1;
