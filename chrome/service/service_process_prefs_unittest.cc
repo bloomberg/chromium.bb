007bb7da5c260926771948e5342b024e52bb0f48
Carlos Caballero <carlscab@google.com>
2019-02-04 18:53:17 +0000

    [scheduling] Use ScopedTaskEnvironment instead of MessageLoop in service
    MessageLoop will go away, eventually.
    
    BUG=891670
    
    Change-Id: I6bf415fd06ed14bfce80a308b41694683d4623b2
    Reviewed-on: https://chromium-review.googlesource.com/c/1451899
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#628810}
    Former-commit-id: 4351bedbce9b43cba24255a278dbb5314043c9a6

--- chrome/service/service_process_prefs_unittest.cc
+++ chrome/service/service_process_prefs_unittest.cc
@@ -5,9 +5,10 @@
 #include <string>
 
 #include "base/files/scoped_temp_dir.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/sequenced_task_runner.h"
+#include "base/test/scoped_task_environment.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "chrome/service/service_process_prefs.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -19,15 +20,14 @@ class ServiceProcessPrefsTest : public testing::Test {
 
     prefs_.reset(new ServiceProcessPrefs(
         temp_dir_.GetPath().AppendASCII("service_process_prefs.txt"),
-        message_loop_.task_runner().get()));
+        base::ThreadTaskRunnerHandle::Get().get()));
   }
 
   void TearDown() override { prefs_.reset(); }
 
   // The path to temporary directory used to contain the test operations.
   base::ScopedTempDir temp_dir_;
-  // A message loop that we can use as the file thread message loop.
-  base::MessageLoop message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   std::unique_ptr<ServiceProcessPrefs> prefs_;
 };
 
@@ -37,7 +37,7 @@ TEST_F(ServiceProcessPrefsTest, RetrievePrefs) {
   prefs_->SetString("tests", "testvalue");
   prefs_->WritePrefs();
   base::RunLoop().RunUntilIdle();
-  prefs_->SetBoolean("testb", false);   // overwrite
+  prefs_->SetBoolean("testb", false);         // overwrite
   prefs_->SetString("tests", std::string());  // overwrite
   prefs_->ReadPrefs();
   EXPECT_EQ(prefs_->GetBoolean("testb", false), true);
