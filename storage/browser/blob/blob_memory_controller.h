a8d7f073b9656e65502f3f32ca0ccc1bb523b5bf
Joel Klinghed <the_jk@opera.com>
2019-02-04 10:41:51 +0000

    Avoid crashing on Android devices with only 256 MB RAM.
    On Android:
    max_blob_in_memory_space is calculated as 1% of total RAM
    min_page_file_size is hardcoded to 2.5MB
    
    If 2.5MB > 1% of total RAM the CHECK(limits.isValid()) fails.
    Avoid that by allowing max_blob_in_memory_space to be at least
    min_page_file_size regardless of amount of physical memory.
    
    R=dmurph@chromium.org, jianli@chromium.org, mek@chromium.org
    
    Change-Id: I6fe9717affeb4bffbcb094e264cde57314b1535f
    
    Bug: 841667
    Change-Id: I6fe9717affeb4bffbcb094e264cde57314b1535f
    Reviewed-on: https://chromium-review.googlesource.com/c/1448454
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628700}
    Former-commit-id: 51dce18b3dbcb3c9843bd987631abc8ea8ec95d2

--- storage/browser/blob/blob_memory_controller.h
+++ storage/browser/blob/blob_memory_controller.h
@@ -197,6 +197,10 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) BlobMemoryController {
   // synchronously.
   void CallWhenStorageLimitsAreKnown(base::OnceClosure callback);
 
+  void set_amount_of_physical_memory_for_testing(int64_t amount_of_memory) {
+    amount_of_memory_for_testing_ = amount_of_memory;
+  }
+
  private:
   class FileQuotaAllocationTask;
   class MemoryQuotaAllocationTask;
@@ -273,6 +277,8 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) BlobMemoryController {
   bool did_schedule_limit_calculation_ = false;
   bool did_calculate_storage_limits_ = false;
   std::vector<base::OnceClosure> on_calculate_limits_callbacks_;
+
+  base::Optional<int64_t> amount_of_memory_for_testing_;
 
   // Memory bookkeeping. These numbers are all disjoint.
   // This is the amount of memory we're using for blobs in RAM, including the
