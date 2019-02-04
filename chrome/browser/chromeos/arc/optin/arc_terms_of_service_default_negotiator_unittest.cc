e0c1d0ceffce56eeb17cfebf754afa9a1358aaec
Gyuyoung Kim <gyuyoung@igalia.com>
2019-02-04 21:31:04 +0000

    s13n: Remove unnecessary siginin_manager_factory.h in arc_terms_of_service_default_negotiator_unittest.cc
    There is no use of SigninManagerFactory class in arc_terms_of_service_default_negotiator_unittest.cc.
    It's unnecessary now. This CL removes the includes.
    
    Bug: 927237
    Change-Id: Iaddc44b4977c4ae4ca4a7e20deab12f5e0cd3cfb
    Reviewed-on: https://chromium-review.googlesource.com/c/1449475
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#628888}
    Former-commit-id: c7d9904321281293cfa8233a0c8834188fe3cb0b

--- chrome/browser/chromeos/arc/optin/arc_terms_of_service_default_negotiator_unittest.cc
+++ chrome/browser/chromeos/arc/optin/arc_terms_of_service_default_negotiator_unittest.cc
@@ -19,7 +19,6 @@
 #include "chrome/browser/consent_auditor/consent_auditor_factory.h"
 #include "chrome/browser/consent_auditor/consent_auditor_test_utils.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
 #include "chrome/test/base/testing_browser_process.h"
