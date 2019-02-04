7daacd5e17504fb433573876a7e4937b72ff8fdd
Marcin Wiącek <marcin@mwiacek.com>
2019-02-04 16:20:11 +0000

    Improve Notification @IntDef
    @IntDef/@StringDef annotation are preferred way for declaring
    set of String/int values
    
    1. they need less space in APK than enum, see
    https://developer.android.com/topic/performance/reduce-apk-size#remove-enums
    2. they give more control over allowed values than "static final" values
    
    Patch is migrating Notification @IntDef to form common with other Chrome @IntDef:
    
    1. with @IntDef first, @Retention second
       and related @interface third
    2. with values inside @interface
    3. with @Retention(RetentionPolicy.SOURCE)
    4. without "static final" in the @interface
    5. with NUM_ENTRIES
    
    BUG=919666
    
    Change-Id: I27e42fc73fdf2031568e7962871c0c7d63f83f94
    Reviewed-on: https://chromium-review.googlesource.com/c/1450674
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628760}
    Former-commit-id: 2af5645204cd77c94ea2f1ef0c2ca72157e82d67

--- base/android/java/src/org/chromium/base/memory/MemoryPressureUma.java
+++ base/android/java/src/org/chromium/base/memory/MemoryPressureUma.java
@@ -19,13 +19,11 @@ import java.lang.annotation.RetentionPolicy;
  * Centralizes UMA data collection for Android-specific memory conditions.
  */
 public class MemoryPressureUma implements ComponentCallbacks2 {
-    @IntDef({
-            Notification.UNKNOWN_TRIM_LEVEL, Notification.TRIM_MEMORY_COMPLETE,
+    @IntDef({Notification.UNKNOWN_TRIM_LEVEL, Notification.TRIM_MEMORY_COMPLETE,
             Notification.TRIM_MEMORY_MODERATE, Notification.TRIM_MEMORY_BACKGROUND,
             Notification.TRIM_MEMORY_UI_HIDDEN, Notification.TRIM_MEMORY_RUNNING_CRITICAL,
             Notification.TRIM_MEMORY_RUNNING_LOW, Notification.TRIM_MEMORY_RUNNING_MODERATE,
-            Notification.ON_LOW_MEMORY, Notification.NOTIFICATION_MAX,
-    })
+            Notification.ON_LOW_MEMORY})
     @Retention(RetentionPolicy.SOURCE)
     private @interface Notification {
         // WARNING: These values are persisted to logs. Entries should not be
@@ -42,7 +40,7 @@ public class MemoryPressureUma implements ComponentCallbacks2 {
         int ON_LOW_MEMORY = 8;
 
         // Must be the last one.
-        int NOTIFICATION_MAX = 9;
+        int NUM_ENTRIES = 9;
     }
 
     private final String mHistogramName;
@@ -108,6 +106,6 @@ public class MemoryPressureUma implements ComponentCallbacks2 {
 
     private void record(@Notification int notification) {
         RecordHistogram.recordEnumeratedHistogram(
-                mHistogramName, notification, Notification.NOTIFICATION_MAX);
+                mHistogramName, notification, Notification.NUM_ENTRIES);
     }
 }
