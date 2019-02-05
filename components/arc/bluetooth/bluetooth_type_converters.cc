9b2e99b1f5810031e78996be9b019f9796034bcb
Qiyu <qiyuh@google.com>
2019-02-05 20:09:21 +0000

    arc: bluetooth: json_value declared retirement today
    Bug: b:111367421
    Test: cts-tradefed run cts -m CtsBluetoothTestCases
    Change-Id: Ib8a3898404a5c9f7627495a29a77d816d6977399
    Reviewed-on: https://chromium-review.googlesource.com/c/1450750
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Commit-Queue: Qiyu Hu <qiyuh@google.com>
    Cr-Commit-Position: refs/heads/master@{#629281}
    Former-commit-id: dfd0cae04fa0263565b07aa2eab11abd2007c328

--- components/arc/bluetooth/bluetooth_type_converters.cc
+++ components/arc/bluetooth/bluetooth_type_converters.cc
@@ -85,14 +85,6 @@ TypeConverter<arc::mojom::BluetoothSdpAttributePtr,
   auto result = arc::mojom::BluetoothSdpAttribute::New();
   result->type = attr_bluez.type();
   result->type_size = attr_bluez.size();
-
-  // TODO(b/111367421): Remove after migration.
-  if (result->type != bluez::BluetoothServiceAttributeValueBlueZ::SEQUENCE) {
-    std::string json;
-    base::JSONWriter::Write(attr_bluez.value(), &json);
-    result->json_value = std::move(json);
-  }
-
   switch (result->type) {
     case bluez::BluetoothServiceAttributeValueBlueZ::NULLTYPE:
       result->value = base::Value();
@@ -127,15 +119,6 @@ TypeConverter<bluez::BluetoothServiceAttributeValueBlueZ,
               arc::mojom::BluetoothSdpAttributePtr>::
     Convert(const arc::mojom::BluetoothSdpAttributePtr& attr, size_t depth) {
   bluez::BluetoothServiceAttributeValueBlueZ::Type type = attr->type;
-
-  // TODO(b/111367421): Remove after migration.
-  if (type != bluez::BluetoothServiceAttributeValueBlueZ::SEQUENCE &&
-      attr->json_value.has_value()) {
-    return bluez::BluetoothServiceAttributeValueBlueZ(
-        type, static_cast<size_t>(attr->type_size),
-        base::JSONReader::Read(attr->json_value.value()));
-  }
-
   if (type != bluez::BluetoothServiceAttributeValueBlueZ::SEQUENCE &&
       !attr->value.has_value()) {
     return bluez::BluetoothServiceAttributeValueBlueZ();
