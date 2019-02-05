822478324861243e827608b8ed5ec926d6f7d86c
Xiaocheng Hu <xiaochengh@chromium.org>
2019-02-05 00:06:33 +0000

    [LayoutNG] Work around the crash with 'text-transform' on ::first-letter
    'text-transform' may make LayoutText::GetText() longer than the original
    DOM text node, and result in out-of-range offsets that are not properly
    handled.
    
    This patch works around that in one of the crash sites so that it
    doesn't crash.
    
    Bug: 923446
    Change-Id: Iadf7d3ee1e9dc5ac03f181cd3f37d11d65bca1fc
    Reviewed-on: https://chromium-review.googlesource.com/c/1450737
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628952}
    Former-commit-id: fd91d711b6669f62e4ffc3dce21e6ae88ab16caf

--- third_party/blink/renderer/core/layout/layout_text_fragment.cc
+++ third_party/blink/renderer/core/layout/layout_text_fragment.cc
@@ -202,12 +202,18 @@ void LayoutTextFragment::UpdateHitTestResult(HitTestResult& result,
 }
 
 Position LayoutTextFragment::PositionForCaretOffset(unsigned offset) const {
-  DCHECK_LE(offset, FragmentLength());
+  // TODO(layout-dev): Make the following DCHECK always enabled after we
+  // properly support 'text-transform' changing text length.
+#if DCHECK_IS_ON()
+  if (StyleRef().TextTransform() == ETextTransform::kNone)
+    DCHECK_LE(offset, FragmentLength());
+#endif
   const Text* node = AssociatedTextNode();
   if (!node)
     return Position();
-  // TODO(layout-dev): Support offset change due to text-transform.
-  return Position(node, Start() + offset);
+  // TODO(layout-dev): Properly support offset change due to text-transform.
+  const unsigned clamped_offset = std::min(offset, FragmentLength());
+  return Position(node, Start() + clamped_offset);
 }
 
 base::Optional<unsigned> LayoutTextFragment::CaretOffsetForPosition(
