3ef9c24afaa14e1d9c694fe5420f4c3a2137db9b
Xianzhu Wang <wangxianzhu@chromium.org>
2019-02-05 23:08:26 +0000

    [BlinkGenPropertyTrees] Move CHECK for crbug.com/923729 out of DCHECK_IS_ON()
    Bug: 923729
    Change-Id: Ib11c9ad5018fc3a04658dedd5893de7d13796e23
    Reviewed-on: https://chromium-review.googlesource.com/c/1455048
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629345}
    Former-commit-id: 291fd0676fae40ea88b07a2687dc3c0780cccc49

--- third_party/blink/renderer/platform/graphics/paint/paint_chunker.cc
+++ third_party/blink/renderer/platform/graphics/paint/paint_chunker.cc
@@ -49,11 +49,11 @@ bool PaintChunker::IncrementDisplayItemIndex(const DisplayItem& item) {
   // If this DCHECKs are hit we are missing a call to update the properties.
   // See: ScopedPaintChunkProperties.
   DCHECK(!IsInInitialState());
+#endif
   // TODO(crbug.com/923729): This CHECK is a temporary to determine the cause of
   // the referenced bug. At this point we should have all of the properties
   // given to us.
   CHECK(current_properties_.IsInitialized());
-#endif
 
   bool item_forces_new_chunk = item.IsForeignLayer() || item.IsScrollHitTest();
   if (item_forces_new_chunk)
