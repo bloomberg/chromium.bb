ab7cefc336f3e06f4bc01f281b95746a8896d614
Carlos IL <carlosil@chromium.org>
2019-02-05 00:32:03 +0000

    Add null check for delegate in MaybeSetupLoginPrompt
    TBR=carlosil@chromium.org
    
    (cherry picked from commit b119b582454e2811b13d5e21c0c83fc345a10124 [formerly c3e6bb6867956bea3620b9f67a5a965f7562aad7])
    
    Bug: 921760
    Change-Id: I7577e6e901463ef2823c415e672e749c284ff0ce
    Reviewed-on: https://chromium-review.googlesource.com/c/1444137
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Auto-Submit: Carlos IL <carlosil@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#627226}
    Reviewed-on: https://chromium-review.googlesource.com/c/1453577
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3626@{#818}
    Cr-Branched-From: 5302c6863bade38a5d283a7cce26a309d8e8e755 [formerly d897fb137fbaaa9355c0c93124cc048824eb1e65]-refs/heads/master@{#612437}
    Former-commit-id: 6eaeb5830d716fab779bada7c31f32e2424a903e

--- chrome/browser/ui/login/login_handler.cc
+++ chrome/browser/ui/login/login_handler.cc
@@ -651,7 +651,8 @@ void LoginHandler::MaybeSetUpLoginPrompt(
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
   WebContents* parent_contents = handler->GetWebContentsForLogin();
-  if (!parent_contents || handler->WasAuthHandled()) {
+  if (!parent_contents || !parent_contents()->GetDelegate() ||
+      handler->WasAuthHandled()) {
     // The request may have been canceled, or it may be for a renderer not
     // hosted by a tab (e.g. an extension). Cancel just in case (canceling twice
     // is a no-op).
