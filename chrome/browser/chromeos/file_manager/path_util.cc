af46625c05db213baaf0e9ca80baba37e70021f4
Joel Hockey <joelhockey@chromium.org>
2019-02-05 04:08:05 +0000

    Crostini shared files settings External storage label
    Use label 'External storage' for removalbe media rather than
    "/media/removable".
    
    BUG=928566
    
    Change-Id: I433baf235474397457608d8f0647fabdc4a9553f
    Reviewed-on: https://chromium-review.googlesource.com/c/1453557
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629036}
    Former-commit-id: 22f7ce6a17da36982179a122ae02ad1161e0154d

--- chrome/browser/chromeos/file_manager/path_util.cc
+++ chrome/browser/chromeos/file_manager/path_util.cc
@@ -498,6 +498,9 @@ std::string GetPathDisplayTextForSettings(Profile* profile,
   } else if (ReplacePrefix(&result, GetCrostiniMountDirectory(profile).value(),
                            l10n_util::GetStringUTF8(
                                IDS_FILE_BROWSER_LINUX_FILES_ROOT_LABEL))) {
+  } else if (ReplacePrefix(&result, kRemovableMediaPath,
+                           l10n_util::GetStringUTF8(
+                               IDS_FILE_BROWSER_EXTERNAL_STORAGE_ROOT_LABEL))) {
   }
 
   base::ReplaceChars(result, "/", " \u203a ", &result);
