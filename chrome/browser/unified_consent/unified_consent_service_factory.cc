a15cc8a6c5ae0ff03be5dd380f2ac74d14780478
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-02-05 11:40:57 +0000

    Remove unused include headers from unified_consent_service_factory.cc
    Remove unwanted headers related to SignInManager from
    unified_consent_service_factory.cc
    
    Bug: 927253
    Change-Id: I2b18b160d8c16eb309fa1d9592aa0aefcbb892bb
    Reviewed-on: https://chromium-review.googlesource.com/c/1447993
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#629110}
    Former-commit-id: 64f52d0faf76a7077c1c1d3dbfc34aa921084e1e

--- chrome/browser/unified_consent/unified_consent_service_factory.cc
+++ chrome/browser/unified_consent/unified_consent_service_factory.cc
@@ -6,7 +6,6 @@
 
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/unified_consent/chrome_unified_consent_service_client.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
