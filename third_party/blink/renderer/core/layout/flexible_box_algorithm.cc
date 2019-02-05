742884777c2e305ee2b10835284a74e5c5577b72
Christian Biesinger <cbiesinger@chromium.org>
2019-02-05 21:05:58 +0000

    Revert "[css-flexbox] Apply min-height: auto to nested flexboxes again"
    This reverts commit f9bd01a10d2fd0fb370ee96b233c1194478f663f [formerly 155c1f2bf45369bc7130c89b88c75c2d3c915410].
    
    Bug: 927066
    Change-Id: Icfa8248cf68dbffa581c4da7477f457a8ab8fd10
    Reviewed-on: https://chromium-review.googlesource.com/c/1455112
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3626@{#833}
    Cr-Branched-From: 5302c6863bade38a5d283a7cce26a309d8e8e755 [formerly d897fb137fbaaa9355c0c93124cc048824eb1e65]-refs/heads/master@{#612437}
    Former-commit-id: 032ef9666487db1d04b656a095b041de8c6d2b63

--- third_party/blink/renderer/core/layout/flexible_box_algorithm.cc
+++ third_party/blink/renderer/core/layout/flexible_box_algorithm.cc
@@ -470,11 +470,14 @@ FlexLayoutAlgorithm::ContentAlignmentNormalBehavior() {
 
 bool FlexLayoutAlgorithm::ShouldApplyMinSizeAutoForChild(
     const LayoutBox& child) const {
+  // TODO(cbiesinger): For now, we do not handle min-height: auto for nested
+  // column flexboxes. See crbug.com/596743
   // css-flexbox section 4.5
   Length min = IsHorizontalFlow() ? child.StyleRef().MinWidth()
                                   : child.StyleRef().MinHeight();
   return min.IsAuto() && !child.ShouldApplySizeContainment() &&
-         MainAxisOverflowForChild(child) == EOverflow::kVisible;
+         MainAxisOverflowForChild(child) == EOverflow::kVisible &&
+         !(IsColumnFlow() && child.IsFlexibleBox());
 }
 
 TransformedWritingMode FlexLayoutAlgorithm::GetTransformedWritingMode() const {
