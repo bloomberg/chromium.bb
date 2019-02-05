5739b8f1325aff51c6c0cdab93ee1a08fdfbda6a
Dan Sanders <sandersd@chromium.org>
2019-02-05 00:36:16 +0000

    [media] Add histograms to track CORS status of HLS manifests.
    When converting pages to use an MSE-based player, only requests that
    would not have opaque fetch() responses can be supported. These
    histograms will help understand the risk of such a requirement.
    
    Change-Id: I141202ec2ea07a35b67bbc9c8782a22662a1bb22
    Reviewed-on: https://chromium-review.googlesource.com/c/1450442
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#628962}
    Former-commit-id: d80b184e521adb5ddbed93333ee130d841e8852c

--- media/blink/webmediaplayer_impl.cc
+++ media/blink/webmediaplayer_impl.cc
@@ -1657,7 +1657,24 @@ void WebMediaPlayerImpl::OnError(PipelineStatus status) {
   // URL, since MediaPlayer doesn't support data:// URLs, fail playback now.
   const bool found_hls = status == PipelineStatus::DEMUXER_ERROR_DETECTED_HLS;
   if (found_hls && mb_data_source_) {
-    demuxer_found_hls_ = found_hls;
+    UMA_HISTOGRAM_BOOLEAN("Media.WebMediaPlayerImpl.HLS.IsCorsCrossOrigin",
+                          mb_data_source_->IsCorsCrossOrigin());
+    // Note: Does not consider the full redirect chain. Redirecting through
+    // another origin will set WouldTaintOrigin() though, assuming that the
+    // crossorigin attribute is not set.
+    bool frame_url_is_cryptographic = url::Origin(frame_->GetSecurityOrigin())
+                                          .GetURL()
+                                          .SchemeIsCryptographic();
+    bool manifest_url_is_cryptographic =
+        loaded_url_.SchemeIsCryptographic() &&
+        mb_data_source_->GetUrlAfterRedirects().SchemeIsCryptographic();
+    UMA_HISTOGRAM_BOOLEAN(
+        "Media.WebMediaPlayerImpl.HLS.IsMixedContent",
+        frame_url_is_cryptographic && !manifest_url_is_cryptographic);
+    UMA_HISTOGRAM_BOOLEAN("Media.WebMediaPlayerImpl.HLS.WouldTaintOrigin",
+                          WouldTaintOrigin());
+    // Note: Affects WouldTaintOrigin().
+    demuxer_found_hls_ = true;
 
     renderer_factory_selector_->SetUseMediaPlayer(true);
 
