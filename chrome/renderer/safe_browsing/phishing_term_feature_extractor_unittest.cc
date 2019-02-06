578a69ec3e0d169e4a52b464a51828dfb892b96d
Carlos Caballero <carlscab@google.com>
2019-02-06 10:48:09 +0000

    [scheduling] Use ScopedTaskEnvironment instead of MessageLoop in safe_browsing
    MessageLoop will go away, eventually.
    
    BUG=891670
    
    Change-Id: I8895f7978fa8269f46fb63a9cf39fe9c0ac7f859
    Reviewed-on: https://chromium-review.googlesource.com/c/1451882
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#629521}
    Former-commit-id: 6372ccbf0ca82081b639fb96aa34e156fd38c09a

--- chrome/renderer/safe_browsing/phishing_term_feature_extractor_unittest.cc
+++ chrome/renderer/safe_browsing/phishing_term_feature_extractor_unittest.cc
@@ -14,12 +14,13 @@
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/location.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/string16.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_task_environment.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "chrome/renderer/safe_browsing/features.h"
@@ -115,7 +116,7 @@ class PhishingTermFeatureExtractorTest : public ::testing::Test {
         shingle_hashes,
         base::Bind(&PhishingTermFeatureExtractorTest::ExtractionDone,
                    base::Unretained(this)));
-    msg_loop_.task_runner()->PostTask(
+    base::ThreadTaskRunnerHandle::Get()->PostTask(
         FROM_HERE,
         base::BindOnce(&PhishingTermFeatureExtractorTest::QuitExtraction,
                        base::Unretained(this)));
@@ -134,7 +135,7 @@ class PhishingTermFeatureExtractorTest : public ::testing::Test {
     active_run_loop_->QuitWhenIdle();
   }
 
-  base::MessageLoop msg_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
   std::unique_ptr<base::RunLoop> active_run_loop_;
   MockFeatureExtractorClock clock_;
   std::unique_ptr<PhishingTermFeatureExtractor> extractor_;
