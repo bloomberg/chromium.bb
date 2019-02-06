23b2d664df30d865a0deb513c5f9a744dbbe4c8f
Jon Kunkee <jkunkee@microsoft.com>
2019-02-06 22:57:24 +0000

    Add Windows support to build/symlink.py
    With the introduction of Windows 10 on ARM comes the need to cross-
    compile for it. The Chromium build regularly uses symlinks from the
    target output directory to the host output directory to make this
    happen.
    
    This change adds no-escalation symlink-ish functionality for Windows to
    the script that implements symlinking in the Chromium build. The
    biggest caveat to this support is that files are hard-linked, so any
    time that a symlink target is deleted and replaced without recreating
    the symlink, hard links will be stale. This is acceptable because the
    Chromium build system always re-makes links when the original changes.
    
    Bug: 893460,927113
    Change-Id: I451a4f353bf6bc976d244b3b08a09a646b278368
    Reviewed-on: https://chromium-review.googlesource.com/c/1447251
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629767}
    Former-commit-id: c89a09ee36661ed82f87078009100b4739665fe4

--- build/symlink.py
+++ build/symlink.py
@@ -3,25 +3,32 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Make a symlink and optionally touch a file (to handle dependencies).
-
-Usage:
-  symlink.py [options] sources... target
-
-A sym link to source is created at target. If multiple sources are specfied,
-then target is assumed to be a directory, and will contain all the links to
+description = """
+Make a symlink and optionally touch a file (to handle dependencies).
+"""
+usage = "%prog [options] source[ source ...] linkname"
+epilog = """
+A sym link to source is created at linkname. If multiple sources are specfied,
+then linkname is assumed to be a directory, and will contain all the links to
 the sources (basenames identical to their source).
+
+On Windows, this will use hard links (mklink /H) to avoid requiring elevation.
+This means that if the original is deleted and replaced, the link will still
+have the old contents. This is not expected to interfere with the Chromium
+build.
 """
 
 import errno
 import optparse
 import os.path
 import shutil
+import subprocess
 import sys
 
 
 def Main(argv):
-  parser = optparse.OptionParser()
+  parser = optparse.OptionParser(usage=usage, description=description,
+                                 epilog=epilog)
   parser.add_option('-f', '--force', action='store_true')
   parser.add_option('--touch')
 
@@ -36,10 +43,23 @@ def Main(argv):
     if len(sources) == 1 and not os.path.isdir(target):
       t = target
     t = os.path.expanduser(t)
-    if os.path.realpath(t) == s:
+    if os.path.realpath(t) == os.path.realpath(s):
       continue
     try:
-      os.symlink(s, t)
+      # N.B. Python 2.x does not have os.symlink for Windows.
+      #   Python 3 has os.symlink for Windows, but requires either the admin-
+      #   granted privilege SeCreateSymbolicLinkPrivilege or, as of Windows 10
+      #   1703, that Developer Mode be enabled. Hard links and junctions do not
+      #   require any extra privileges to create.
+      if os.name == 'nt':
+        # mklink does not tolerate /-delimited path names.
+        t = t.replace('/', '\\')
+        s = s.replace('/', '\\')
+        # N.B. This tool only handles file hardlinks, not directory junctions.
+        subprocess.check_output(['cmd.exe', '/c', 'mklink', '/H', t, s],
+                                stderr=subprocess.STDOUT)
+      else:
+        os.symlink(s, t)
     except OSError, e:
       if e.errno == errno.EEXIST and options.force:
         if os.path.isdir(t):
@@ -47,6 +67,18 @@ def Main(argv):
         else:
           os.remove(t)
         os.symlink(s, t)
+      else:
+        raise
+    except subprocess.CalledProcessError, e:
+      # Since subprocess.check_output does not return an easily checked error
+      # number, in the 'force' case always assume it is 'file already exists'
+      # and retry.
+      if options.force:
+        if os.path.isdir(t):
+          shutil.rmtree(t, ignore_errors=True)
+        else:
+          os.remove(t)
+        subprocess.check_output(e.cmd, stderr=subprocess.STDOUT)
       else:
         raise
 
