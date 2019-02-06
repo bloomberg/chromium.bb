a61ba4e4fa0155691f247a70f0d319482ea208df
Alex Clarke <alexclarke@chromium.org>
2019-02-06 20:24:33 +0000

    Remove redundant MessageLoop from audio_apitest.cc
    Bug: 863341
    Change-Id: I59bcc9eb6313bf23b9c4131c467d89eb72d59701
    Reviewed-on: https://chromium-review.googlesource.com/c/1456056
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Auto-Submit: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629707}
    Former-commit-id: 6e0c86f477265c3cfd5bd9f65acabd19220914fe

--- extensions/browser/api/audio/audio_apitest.cc
+++ extensions/browser/api/audio/audio_apitest.cc
@@ -9,7 +9,6 @@
 
 #include "base/auto_reset.h"
 #include "base/command_line.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "build/build_config.h"
 #include "extensions/common/features/feature_session_type.h"
@@ -121,7 +120,6 @@ class AudioApiTest : public ShellApiTest {
   }
 
  protected:
-  base::MessageLoopForUI message_loop_;
   std::unique_ptr<base::AutoReset<extensions::FeatureSessionType>>
       session_feature_type_;
   chromeos::CrasAudioHandler* cras_audio_handler_;  // Not owned.
