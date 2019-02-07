166ec36688b44edd0f2a3c25fa4b25f7ac6c859e
Etienne Pierre-doray <etiennep@chromium.org>
2019-02-07 15:18:23 +0000

    [TaskScheduler]: Cleanup Task.
    This CL removes redundant members in base::internal::Task:
    - delay: Redundant with PendingTask::delayed_run_time
    - sequenced_time: Redundant with PendingTask::queue_time (unused when sequenced_time is used).
    
    This clean up makes Task easier to read, and slightly more lightweight.
    
    Change-Id: I7ed16bd3458d61f98d717f2f3dd83124f9f0121f
    Reviewed-on: https://chromium-review.googlesource.com/c/1448977
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629969}
    Former-commit-id: 2d079037ff0c66178b91ec6b3f91e57c2e836573

--- base/task/task_scheduler/task.cc
+++ base/task/task_scheduler/task.cc
@@ -23,8 +23,7 @@ Task::Task(const Location& posted_from, OnceClosure task, TimeDelta delay)
     : PendingTask(posted_from,
                   std::move(task),
                   delay.is_zero() ? TimeTicks() : TimeTicks::Now() + delay,
-                  Nestable::kNonNestable),
-      delay(delay) {
+                  Nestable::kNonNestable) {
   // TaskScheduler doesn't use |sequence_num| but tracing (toplevel.flow) relies
   // on it being unique. While this subtle dependency is a bit overreaching,
   // TaskScheduler is the only task system that doesn't use |sequence_num| and
@@ -38,8 +37,6 @@ Task::Task(const Location& posted_from, OnceClosure task, TimeDelta delay)
 // this case.
 Task::Task(Task&& other) noexcept
     : PendingTask(std::move(other)),
-      delay(other.delay),
-      sequenced_time(other.sequenced_time),
       sequenced_task_runner_ref(std::move(other.sequenced_task_runner_ref)),
       single_thread_task_runner_ref(
           std::move(other.single_thread_task_runner_ref)) {}
