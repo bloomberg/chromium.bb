ee452f57c50ae5c63a8bf9e50e33e0fc8535079f
Carlos Caballero <carlscab@google.com>
2019-02-07 13:19:33 +0000

    Add tasks traits for BrowserUIThreadScheduler
    Add a new task trait to tell the future BrowserUIThreadScheduler which
    tasks are critical for startup and allow it to prioritize them
    accordingly.
    
    Design doc: https://docs.google.com/document/d/1z1BDq9vzcEpkhN9LSPF5XMnZ0kLJ8mWWkNAi4OI7cos/edit#
    
    Bug: 863341, 872372
    Change-Id: Ifb082762726fbda951325802faf82f1dacf219cc
    Reviewed-on: https://chromium-review.googlesource.com/c/1454632
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629944}
    Former-commit-id: 6dbe9498237691d966ffe28f188d187402071034

--- content/public/android/java/src/org/chromium/content/browser/UiThreadTaskTraitsImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/UiThreadTaskTraitsImpl.java
@@ -7,6 +7,7 @@ package org.chromium.content.browser;
 import org.chromium.base.task.TaskPriority;
 import org.chromium.base.task.TaskTraits;
 import org.chromium.content_public.browser.BrowserTaskExecutor;
+import org.chromium.content_public.browser.BrowserTaskType;
 
 /**
  * Provides the implementation needed in UiThreadTaskTraits.
@@ -18,11 +19,14 @@ public class UiThreadTaskTraitsImpl {
     public static final byte EXTENSION_ID = 1;
 
     // Keep in sync with content::BrowserTaskTraitsExtension::Serialize.
-    private static final byte NESTING_INDEX = 1;
+    private static final byte TASK_TYPE = 1;
+    private static final byte NESTING_INDEX = 2;
 
-    private static final byte[] sDefaultExtensionData = getDefaultExtesionData();
+    private static final byte[] sDefaultExtensionData = getDefaultExtensionData();
 
     public static final TaskTraits DEFAULT = new TaskTraits(EXTENSION_ID, sDefaultExtensionData);
+    public static final TaskTraits BOOTSTRAP = new TaskTraits(
+            EXTENSION_ID, getExtensionDataForBrowserTaskType(BrowserTaskType.BOOTSTRAP));
     public static final TaskTraits BEST_EFFORT = DEFAULT.taskPriority(TaskPriority.BEST_EFFORT);
     public static final TaskTraits USER_VISIBLE = DEFAULT.taskPriority(TaskPriority.USER_VISIBLE);
     public static final TaskTraits USER_BLOCKING = DEFAULT.taskPriority(TaskPriority.USER_BLOCKING);
@@ -31,14 +35,22 @@ public class UiThreadTaskTraitsImpl {
         BrowserTaskExecutor.register();
     }
 
-    private static byte[] getDefaultExtesionData() {
+    private static byte[] getDefaultExtensionData() {
         byte extensionData[] = new byte[TaskTraits.EXTENSION_STORAGE_SIZE];
 
         // Note we don't specify the UI thread directly here because it's ID 0 and the array is
         // initialized to zero.
 
+        // Similarly we don't specify BrowserTaskType.Default its ID is also 0.
+
         // TODO(crbug.com/876272) Remove this if possible.
         extensionData[NESTING_INDEX] = 1; // Allow the task to run in a nested RunLoop.
+        return extensionData;
+    }
+
+    private static byte[] getExtensionDataForBrowserTaskType(int browserTaskType) {
+        byte extensionData[] = getDefaultExtensionData();
+        extensionData[TASK_TYPE] = (byte) browserTaskType;
         return extensionData;
     }
 }
