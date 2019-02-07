8dd1a380dbfa904a8ecf141da5d21af881f14d2e
Alli Murray <alliemurray@google.com>
2019-02-07 04:17:17 +0000

    Add quick view test for removable partitions.
    Bug: 918795
    Change-Id: I3ce85373b987f572e901889082d638035acf1235
    Reviewed-on: https://chromium-review.googlesource.com/c/1457557
    Commit-Queue: Alli Murray <alliemurray@google.com>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629872}
    Former-commit-id: 3968c5449ec5f431fb2d4a4096bf38cd3c984e3f

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -500,6 +500,7 @@ WRAPPED_INSTANTIATE_TEST_CASE_P(
                       TestCase("openQuickViewAndroid"),
                       TestCase("openQuickViewCrostini"),
                       TestCase("openQuickViewUsb"),
+                      TestCase("openQuickViewRemovablePartitions"),
                       TestCase("openQuickViewMtp"),
                       TestCase("pressEnterOnInfoBoxToOpenClose"),
                       TestCase("closeQuickView"),
