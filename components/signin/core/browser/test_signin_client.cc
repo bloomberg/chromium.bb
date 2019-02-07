e7dea1d3979b70d950bb1d31f5d9eaeef7529c25
Antonio Gomes <tonikitoo@igalia.com>
2019-02-07 23:14:33 +0000

    Add a simple network::mojom::CookieManager implementation to services/network/test
    [1], [2] and [3] all have "fake" implementations of network::mojom::CookieManager,
    mostly stubbed out.
    
    This CL provides a simple implementation of this interface located in
    //services/network/test/test_cookie_manager.cc|h that can be either used
    as is (eg by [1] and [2]) or have the relevant methods overridden (eg in [3]).
    
    [1] //services/identity/public/cpp/identity_manager_unittests.cc,
    [2] //components/signin/core/browser/test_signin_client.cc
    [3] //c/b/chromeos/android_sms/android_sms_app_setup_controller_impl_unittest.cc
    
    For consistency, the newly added class is named TestCookieManager.
    
    This is a follow up of [4].
    
    [4] https://crrev.com/c/1452816/1/services/identity/public/cpp/identity_manager_unittest.cc#96
    
    BUG=926890
    
    Change-Id: Ie1b7b7e3dd6e432f61b91b310614645409294d89
    Reviewed-on: https://chromium-review.googlesource.com/c/1456916
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630093}
    Former-commit-id: e4d50406bd0f64db77218c72e4b9db4812107b41

--- components/signin/core/browser/test_signin_client.cc
+++ components/signin/core/browser/test_signin_client.cc
@@ -9,38 +9,8 @@
 #include "base/logging.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "services/network/test/test_cookie_manager.h"
 #include "testing/gtest/include/gtest/gtest.h"
-
-namespace {
-class FakeCookieManager : public network::mojom::CookieManager {
- public:
-  void SetCanonicalCookie(const net::CanonicalCookie& cookie,
-                          bool secure_source,
-                          bool modify_http_only,
-                          SetCanonicalCookieCallback callback) override;
-  void GetAllCookies(GetAllCookiesCallback callback) override {}
-  void GetCookieList(const GURL& url,
-                     const net::CookieOptions& cookie_options,
-                     GetCookieListCallback callback) override {}
-  void DeleteCanonicalCookie(const net::CanonicalCookie& cookie,
-                             DeleteCanonicalCookieCallback callback) override {}
-  void DeleteCookies(network::mojom::CookieDeletionFilterPtr filter,
-                     DeleteCookiesCallback callback) override {}
-  void AddCookieChangeListener(
-      const GURL& url,
-      const std::string& name,
-      network::mojom::CookieChangeListenerPtr listener) override {}
-  void AddGlobalChangeListener(
-      network::mojom::CookieChangeListenerPtr notification_pointer) override {}
-  void CloneInterface(
-      network::mojom::CookieManagerRequest new_interface) override {}
-  void FlushCookieStore(FlushCookieStoreCallback callback) override {}
-  void SetContentSettings(
-      const std::vector<::ContentSettingPatternSource>& settings) override {}
-  void SetForceKeepSessionState() override {}
-  void BlockThirdPartyCookies(bool block) override {}
-};
-}  // namespace
 
 TestSigninClient::TestSigninClient(PrefService* pref_service)
     : shared_factory_(
@@ -59,14 +29,6 @@ PrefService* TestSigninClient::GetPrefs() {
   return pref_service_;
 }
 
-void FakeCookieManager::SetCanonicalCookie(
-    const net::CanonicalCookie& cookie,
-    bool secure_source,
-    bool modify_http_only,
-    SetCanonicalCookieCallback callback) {
-  std::move(callback).Run(false);
-}
-
 void TestSigninClient::PreSignOut(
     base::OnceCallback<void(SignoutDecision)> on_signout_decision_reached,
     signin_metrics::ProfileSignout signout_source_metric) {
@@ -82,7 +44,7 @@ TestSigninClient::GetURLLoaderFactory() {
 
 network::mojom::CookieManager* TestSigninClient::GetCookieManager() {
   if (!cookie_manager_)
-    cookie_manager_ = std::make_unique<FakeCookieManager>();
+    cookie_manager_ = std::make_unique<network::TestCookieManager>();
   return cookie_manager_.get();
 }
 
