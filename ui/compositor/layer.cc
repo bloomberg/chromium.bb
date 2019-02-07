0815e934517eb3c2b2abaedd8941233d1b522e75
Malay Keshav <malaykeshav@chromium.org>
2019-02-07 04:19:58 +0000

    Propagate layer visibility changes to mirrors
    This patch aims at fixing one of the causes for the transparent chrome
    window bug. One of the reasons that caused this bug was that some of the
    property changes on the original ui layer were not being propagated to
    the mirror layer.
    
    The visibility of the original layer needs to be propagated to the
    mirror layers. This patch adds this functionality.
    
    Bug: 927909
    Change-Id: I3767bc513f199cb7c3dd7b3c0783894b99cd4293
    Component: ui layer,stale content,  solid color layer
    Reviewed-on: https://chromium-review.googlesource.com/c/1450611
    Commit-Queue: Malay Keshav <malaykeshav@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629874}
    Former-commit-id: df4c12cd00cdd9d959380d8c19ab13df07114ce4

--- ui/compositor/layer.cc
+++ ui/compositor/layer.cc
@@ -1212,6 +1212,10 @@ void Layer::SetOpacityFromAnimation(float opacity,
 
 void Layer::SetVisibilityFromAnimation(bool visible,
                                        PropertyChangeReason reason) {
+  // Sync changes with the mirror layers.
+  for (const auto& mirror : mirrors_)
+    mirror->dest()->SetVisible(visible);
+
   if (visible_ == visible)
     return;
 
