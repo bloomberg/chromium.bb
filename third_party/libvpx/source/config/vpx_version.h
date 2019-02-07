4a4352ed2fe30625ac4367a1689e821f1414ccdf
Jerome Jiang <jianj@google.com>
2019-02-07 17:37:47 +0000

    Roll src/third_party/libvpx/source/libvpx/ cde3da57b..ce4336c2a (26 commits)
    https://chromium.googlesource.com/webm/libvpx.git/+log/cde3da57b9a1..ce4336c2ab60
    
    $ git log cde3da57b..ce4336c2a --date=short --no-merges --format='%ad %ae %s'
    2019-02-05 jianj Fix VPX_KF_DISABLED.
    2019-02-04 jianj No vpx_img_alloc for y4m input in example encoders.
    2019-02-04 johannkoenig enforce some c89 restrictions
    2019-02-05 johannkoenig ppc: use c89 loop declaration
    2019-02-01 huisu Improve the partition split prediction model
    2019-02-04 chiyotsai Fix an inline varible declaration in temporal filter
    2019-02-04 marpan vp8: Add extra conditon for overshoot-drop
    2019-02-04 chiyotsai Add operator<< to a struct in yuv_temporal_filter_test.cc
    2019-01-31 chiyotsai Some cosmetic fixes to temporal filter
    2019-02-01 johannkoenig Release v1.8.0 Northern Shoveler
    (...)
    2019-01-31 angiebird Add dist scale in get_mv_dist
    2019-01-31 marpan vp9: Adjust intra check for short_circuit_flat_blocks
    2019-01-30 angiebird Implement get_mv_cost()
    2019-01-25 chiyotsai Add highbd yuv_temporal_filter
    2019-01-29 angiebird Add assertion in get_block_src_pred_buf
    2019-01-29 angiebird Fix bug in predict_mv_mode
    2019-01-29 angiebird Allocate memory for mv_mode_arr[]
    2019-01-15 jianj vp9: fix definition for VP9E_SET_POSTENCODE_DROP
    2019-01-11 deepa.kg Fix segmentation fault in hbd path
    2019-01-08 jianj vp8 dec: Add flag to bring up threads.
    
    Created with:
      roll-dep src/third_party/libvpx/source/libvpx
    R=johannkoenig@google.com
    
    Change-Id: Ife63df105d9925e81c8faa183ae61d31eed18568
    Reviewed-on: https://chromium-review.googlesource.com/c/1456937
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Reviewed-by: Johann Koenig <johannkoenig@google.com>
    Cr-Commit-Position: refs/heads/master@{#629995}
    Former-commit-id: f8d0510ca45404257fb6eb59ca1aad0148fb445b

--- third_party/libvpx/source/config/vpx_version.h
+++ third_party/libvpx/source/config/vpx_version.h
@@ -1,8 +1,8 @@
 // This file is generated. Do not edit.
 #define VERSION_MAJOR  1
-#define VERSION_MINOR  7
+#define VERSION_MINOR  8
 #define VERSION_PATCH  0
-#define VERSION_EXTRA  "1741-gcde3da57b9"
+#define VERSION_EXTRA  "142-gce4336c2ab"
 #define VERSION_PACKED ((VERSION_MAJOR<<16)|(VERSION_MINOR<<8)|(VERSION_PATCH))
-#define VERSION_STRING_NOSP "v1.7.0-1741-gcde3da57b9"
-#define VERSION_STRING      " v1.7.0-1741-gcde3da57b9"
+#define VERSION_STRING_NOSP "v1.8.0-142-gce4336c2ab"
+#define VERSION_STRING      " v1.8.0-142-gce4336c2ab"
