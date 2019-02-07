157a049d026ea0de44214b51a450d7f61e413be1
Olya Kalitova <okalitova@chromium.org>
2019-02-07 11:10:24 +0000

    Add unzipping functionality to PluginVmImageManager
    After PluginVm image is successfully downloaded it needs to be unzipped to a specific location. This CL adds unzipping functionality to the PluginVmImageManager so unzipping of the downloaded image could be called using manager and observer could receive events regarding status of the unzipping.
    
    Bug: 904851
    Test: unit_tests --gtest_filter="PluginVmImageManagerTest.*"
    Change-Id: I005929fccbeec191951a054e303bc816e853285a
    Reviewed-on: https://chromium-review.googlesource.com/c/1443171
    Commit-Queue: Olya Kalitova <okalitova@chromium.org>
    Reviewed-by: Igor <igorcov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#629931}
    Former-commit-id: 952e6e08f4110632eb206567121e56a3c69fd051

--- chrome/browser/chromeos/plugin_vm/plugin_vm_image_manager.cc
+++ chrome/browser/chromeos/plugin_vm/plugin_vm_image_manager.cc
@@ -4,38 +4,77 @@
 
 #include "chrome/browser/chromeos/plugin_vm/plugin_vm_image_manager.h"
 
+#include <memory>
 #include <string>
 
 #include "base/bind.h"
+#include "base/files/file.h"
+#include "base/files/file_util.h"
 #include "base/guid.h"
 #include "base/optional.h"
+#include "base/task/post_task.h"
+#include "base/task/task_traits.h"
 #include "chrome/browser/chromeos/plugin_vm/plugin_vm_pref_names.h"
 #include "chrome/browser/download/download_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/download/public/background_service/download_service.h"
 #include "components/prefs/pref_service.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
+#include "third_party/zlib/google/zip.h"
 
 namespace plugin_vm {
 
-bool PluginVmImageManager::IsDownloading() {
-  return processing_download_;
+bool PluginVmImageManager::IsProcessingImage() {
+  return processing_image_;
 }
 
 void PluginVmImageManager::StartDownload() {
-  if (IsDownloading())
-    return OnDownloadFailed();
-  processing_download_ = true;
+  if (IsProcessingImage()) {
+    LOG(ERROR) << "Download of a PluginVm image couldn't be started as"
+               << " another PluginVm image is currently being processed";
+    OnDownloadFailed();
+    return;
+  }
+  processing_image_ = true;
   download_service_->StartDownload(GetDownloadParams());
 }
 
 void PluginVmImageManager::CancelDownload() {
-  DCHECK(!current_download_guid_.empty());
   download_service_->CancelDownload(current_download_guid_);
+}
+
+void PluginVmImageManager::StartUnzipping() {
+  if (IsDownloading()) {
+    LOG(ERROR) << "Unzipping of PluginVm image couldn't be proceeded "
+               << "as image is still being downloaded";
+    OnUnzipped(false);
+    return;
+  }
+  if (!EnsureDirectoryForPluginVmImageIsPresent() ||
+      !EnsureDownloadedPluginVmImageArchiveIsPresent()) {
+    LOG(ERROR) << "Unzipping of PluginVm image couldn't be proceeded";
+    OnUnzipped(false);
+    return;
+  }
+
+  base::PostTaskWithTraitsAndReplyWithResult(
+      FROM_HERE, {base::TaskPriority::USER_VISIBLE, base::MayBlock()},
+      base::BindOnce(&PluginVmImageManager::UnzipDownloadedPluginVmImageArchive,
+                     base::Unretained(this)),
+      base::BindOnce(&PluginVmImageManager::OnUnzipped,
+                     weak_ptr_factory_.GetWeakPtr()));
+}
+
+void PluginVmImageManager::CancelUnzipping() {
+  unzipping_cancelled_ = true;
 }
 
 void PluginVmImageManager::SetObserver(Observer* observer) {
   observer_ = observer;
+}
+
+void PluginVmImageManager::RemoveObserver() {
+  observer_ = nullptr;
 }
 
 void PluginVmImageManager::OnDownloadStarted() {
@@ -50,29 +89,36 @@ void PluginVmImageManager::OnProgressUpdated(
 }
 
 void PluginVmImageManager::OnDownloadCompleted(base::FilePath file_path) {
-  if (observer_)
-    observer_->OnDownloadCompleted(file_path);
+  downloaded_plugin_vm_image_archive_ = file_path;
   current_download_guid_.clear();
-  processing_download_ = false;
+  if (observer_)
+    observer_->OnDownloadCompleted();
 }
 
 void PluginVmImageManager::OnDownloadCancelled() {
+  RemoveTemporaryPluginVmImageArchiveIfExists();
+  current_download_guid_.clear();
+  processing_image_ = false;
   if (observer_)
     observer_->OnDownloadCancelled();
-  current_download_guid_.clear();
-  processing_download_ = false;
 }
 
 void PluginVmImageManager::OnDownloadFailed() {
+  RemoveTemporaryPluginVmImageArchiveIfExists();
+  current_download_guid_.clear();
+  processing_image_ = false;
   if (observer_)
     observer_->OnDownloadFailed();
-  current_download_guid_.clear();
-  processing_download_ = false;
 }
 
 void PluginVmImageManager::SetDownloadServiceForTesting(
     download::DownloadService* download_service) {
   download_service_ = download_service;
+}
+
+void PluginVmImageManager::SetDownloadedPluginVmImageArchiveForTesting(
+    base::FilePath downloaded_plugin_vm_image_archive) {
+  downloaded_plugin_vm_image_archive_ = downloaded_plugin_vm_image_archive;
 }
 
 std::string PluginVmImageManager::GetCurrentDownloadGuidForTesting() {
@@ -81,8 +127,8 @@ std::string PluginVmImageManager::GetCurrentDownloadGuidForTesting() {
 
 PluginVmImageManager::PluginVmImageManager(Profile* profile)
     : profile_(profile),
-      download_service_(DownloadServiceFactory::GetForBrowserContext(profile)) {
-}
+      download_service_(DownloadServiceFactory::GetForBrowserContext(profile)),
+      weak_ptr_factory_(this) {}
 PluginVmImageManager::~PluginVmImageManager() = default;
 
 download::DownloadParams PluginVmImageManager::GetDownloadParams() {
@@ -91,8 +137,8 @@ download::DownloadParams PluginVmImageManager::GetDownloadParams() {
   // DownloadParams
   params.client = download::DownloadClient::PLUGIN_VM_IMAGE;
   params.guid = base::GenerateGUID();
-  params.callback = base::BindRepeating(&PluginVmImageManager::OnStarted,
-                                        base::Unretained(this));
+  params.callback = base::BindRepeating(&PluginVmImageManager::OnStartDownload,
+                                        weak_ptr_factory_.GetWeakPtr());
   // TODO(okalitova): Create annotation.
   params.traffic_annotation =
       net::MutableNetworkTrafficAnnotationTag(NO_TRAFFIC_ANNOTATION_YET);
@@ -117,13 +163,146 @@ download::DownloadParams PluginVmImageManager::GetDownloadParams() {
   return params;
 }
 
-void PluginVmImageManager::OnStarted(
+void PluginVmImageManager::OnStartDownload(
     const std::string& download_guid,
     download::DownloadParams::StartResult start_result) {
   if (start_result == download::DownloadParams::ACCEPTED)
     current_download_guid_ = download_guid;
   else
     OnDownloadFailed();
+}
+
+bool PluginVmImageManager::IsDownloading() {
+  return !current_download_guid_.empty();
+}
+
+bool PluginVmImageManager::UnzipDownloadedPluginVmImageArchive() {
+  base::File file(downloaded_plugin_vm_image_archive_,
+                  base::File::FLAG_OPEN | base::File::FLAG_READ);
+  if (!file.IsValid()) {
+    LOG(ERROR) << "Failed to open "
+               << downloaded_plugin_vm_image_archive_.value();
+    return false;
+  }
+
+  bool success = zip::UnzipWithFilterAndWriters(
+      file.GetPlatformFile(),
+      base::BindRepeating(
+          &PluginVmImageManager::CreatePluginVmImageWriterDelegate,
+          base::Unretained(this)),
+      base::BindRepeating(&PluginVmImageManager::CreateDirectory,
+                          base::Unretained(this)),
+      base::BindRepeating(
+          &PluginVmImageManager::FilterFilesInPluginVmImageArchive,
+          base::Unretained(this)),
+      true /* log_skipped_files */);
+  return success;
+}
+
+bool PluginVmImageManager::IsUnzippingCancelled() {
+  return unzipping_cancelled_;
+}
+
+PluginVmImageManager::PluginVmImageWriterDelegate::PluginVmImageWriterDelegate(
+    PluginVmImageManager* manager,
+    const base::FilePath& output_file_path)
+    : manager_(manager), output_file_path_(output_file_path) {}
+
+bool PluginVmImageManager::PluginVmImageWriterDelegate::PrepareOutput() {
+  // We can't rely on parent directory entries being specified in the
+  // zip, so we make sure they are created.
+  if (!base::CreateDirectory(output_file_path_.DirName()))
+    return false;
+
+  output_file_.Initialize(output_file_path_, base::File::FLAG_CREATE_ALWAYS |
+                                                 base::File::FLAG_WRITE);
+  return output_file_.IsValid();
+}
+
+bool PluginVmImageManager::PluginVmImageWriterDelegate::WriteBytes(
+    const char* data,
+    int num_bytes) {
+  return !manager_->IsUnzippingCancelled() &&
+         num_bytes == output_file_.WriteAtCurrentPos(data, num_bytes);
+}
+
+void PluginVmImageManager::PluginVmImageWriterDelegate::SetTimeModified(
+    const base::Time& time) {
+  output_file_.Close();
+  base::TouchFile(output_file_path_, base::Time::Now(), time);
+}
+
+std::unique_ptr<zip::WriterDelegate>
+PluginVmImageManager::CreatePluginVmImageWriterDelegate(
+    const base::FilePath& entry_path) {
+  return std::make_unique<PluginVmImageWriterDelegate>(
+      this, plugin_vm_image_dir_.Append(entry_path));
+}
+
+bool PluginVmImageManager::CreateDirectory(const base::FilePath& entry_path) {
+  return base::CreateDirectory(plugin_vm_image_dir_.Append(entry_path));
+}
+
+bool PluginVmImageManager::FilterFilesInPluginVmImageArchive(
+    const base::FilePath& file) {
+  return true;
+}
+
+void PluginVmImageManager::OnUnzipped(bool success) {
+  unzipping_cancelled_ = false;
+  RemoveTemporaryPluginVmImageArchiveIfExists();
+  if (!success) {
+    if (observer_)
+      observer_->OnUnzippingFailed();
+    RemovePluginVmImageDirectoryIfExists();
+    processing_image_ = false;
+    return;
+  }
+  if (observer_)
+    observer_->OnUnzipped();
+  processing_image_ = false;
+  // TODO(okalitova): Populate necessary preferences with information about
+  // PluginVm image that has been successfully downloaded and extracted.
+}
+
+bool PluginVmImageManager::EnsureDownloadedPluginVmImageArchiveIsPresent() {
+  return !downloaded_plugin_vm_image_archive_.empty();
+}
+
+bool PluginVmImageManager::EnsureDirectoryForPluginVmImageIsPresent() {
+  plugin_vm_image_dir_ = profile_->GetPath()
+                             .AppendASCII(kCrosvmDir)
+                             .AppendASCII(kPvmDir)
+                             .AppendASCII(kPluginVmImageDir);
+  if (!base::CreateDirectory(plugin_vm_image_dir_)) {
+    LOG(ERROR) << "Directory " << plugin_vm_image_dir_.value()
+               << " failed to be created";
+    plugin_vm_image_dir_.clear();
+    return false;
+  }
+  return true;
+}
+
+void PluginVmImageManager::RemoveTemporaryPluginVmImageArchiveIfExists() {
+  if (!downloaded_plugin_vm_image_archive_.empty()) {
+    if (!base::DeleteFile(downloaded_plugin_vm_image_archive_,
+                          false /* recursive */)) {
+      LOG(ERROR) << "Downloaded PluginVm image archive located in "
+                 << downloaded_plugin_vm_image_archive_.value()
+                 << " failed to be deleted";
+    }
+    downloaded_plugin_vm_image_archive_.clear();
+  }
+}
+
+void PluginVmImageManager::RemovePluginVmImageDirectoryIfExists() {
+  if (!plugin_vm_image_dir_.empty()) {
+    if (!base::DeleteFile(plugin_vm_image_dir_, true /* recursive */)) {
+      LOG(ERROR) << "Directory with PluginVm image "
+                 << plugin_vm_image_dir_.value() << " failed to be deleted";
+    }
+    plugin_vm_image_dir_.clear();
+  }
 }
 
 }  // namespace plugin_vm
