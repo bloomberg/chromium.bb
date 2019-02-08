fb7c3636f7e322deb292b39c305889bd50ce50e3
Pavel Feldman <pfeldman@chromium.org>
2019-02-08 04:38:11 +0000

    DevTools: roll third_party/inspector_protocol
    Bug: 929862
    
    Change-Id: I3c12584095a1d2a45a3b6181bece071a2cd34dae
    Reviewed-on: https://chromium-review.googlesource.com/c/1460186
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630185}
    Former-commit-id: 84b0216acfa753d91e4d41ebec8aa0e57cc216f2

--- content/browser/devtools/protocol_unittest.cc
+++ content/browser/devtools/protocol_unittest.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/browser/devtools/protocol_string.h"
+#include "content/browser/devtools/protocol/base_string_adapter.h"
 
 #include <vector>
 #include "testing/gtest/include/gtest/gtest.h"
