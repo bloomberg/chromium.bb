2adf8d39973990b8e276b84e3038a00191ae40b5
Reilly Grant <reillyg@chromium.org>
2019-02-08 04:49:25 +0000

    [serial] Implement feature policy checks
    This API should follow the example of WebUSB and require explicit
    authorization via feature policy when used in a cross-origin iframe.
    
    Bug: 884928
    Change-Id: Ia953c86a45e8b38b901b790b8aee912fd991e1a1
    Reviewed-on: https://chromium-review.googlesource.com/c/1392357
    Reviewed-by: Luna Lu <loonybear@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Oliver Chang <ochang@chromium.org>
    Commit-Queue: Luna Lu <loonybear@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630196}
    Former-commit-id: e4c83375219d9d370cb535deddff6bd4c3aa07c6

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -5682,6 +5682,11 @@ void RenderFrameHostImpl::BindNFCRequest(device::mojom::NFCRequest request) {
 #if !defined(OS_ANDROID)
 void RenderFrameHostImpl::BindSerialServiceRequest(
     blink::mojom::SerialServiceRequest request) {
+  if (!IsFeatureEnabled(blink::mojom::FeaturePolicyFeature::kSerial)) {
+    mojo::ReportBadMessage("Feature policy blocks access to Serial.");
+    return;
+  }
+
   if (!serial_service_)
     serial_service_ = std::make_unique<SerialService>(this);
 
