815e49195cceaf72d8a725f61ab3f8f346f75132
Marc Treib <treib@chromium.org>
2019-02-10 19:26:46 +0000

    Prefer SyncService over ProfileSyncService in foreign_session_helper
    SyncService is the interface, ProfileSyncService is the concrete
    implementation. Generally no clients should need to use the conrete
    implementation - for one, testing will be much easier once everyone
    uses the interface only.
    
    Bug: 924508
    Change-Id: Ia210665f8f02512053d1a60d627dea0f22758387
    Reviewed-on: https://chromium-review.googlesource.com/c/1461119
    Auto-Submit: Marc Treib <treib@chromium.org>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630662}
    Former-commit-id: 32a9879fc01c24f9216bb2975200ab8a4afac80c

--- chrome/browser/android/foreign_session_helper.cc
+++ chrome/browser/android/foreign_session_helper.cc
@@ -19,9 +19,9 @@
 #include "chrome/browser/ui/android/tab_model/tab_model_list.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
-#include "components/browser_sync/profile_sync_service.h"
 #include "components/prefs/pref_service.h"
 #include "components/prefs/scoped_user_pref_update.h"
+#include "components/sync/driver/sync_service.h"
 #include "components/sync_sessions/open_tabs_ui_delegate.h"
 #include "components/sync_sessions/session_sync_service.h"
 #include "content/public/browser/notification_details.h"
@@ -181,13 +181,12 @@ jboolean ForeignSessionHelper::IsTabSyncEnabled(
 void ForeignSessionHelper::TriggerSessionSync(
     JNIEnv* env,
     const JavaParamRef<jobject>& obj) {
-  browser_sync::ProfileSyncService* service =
-      ProfileSyncServiceFactory::GetInstance()->GetForProfile(profile_);
+  syncer::SyncService* service =
+      ProfileSyncServiceFactory::GetSyncServiceForProfile(profile_);
   if (!service)
     return;
 
-  const syncer::ModelTypeSet types(syncer::SESSIONS);
-  service->TriggerRefresh(types);
+  service->TriggerRefresh({syncer::SESSIONS});
 }
 
 void ForeignSessionHelper::SetOnForeignSessionCallback(
@@ -309,8 +308,8 @@ void ForeignSessionHelper::SetInvalidationsForSessionsEnabled(
     JNIEnv* env,
     const JavaParamRef<jobject>& obj,
     jboolean enabled) {
-  browser_sync::ProfileSyncService* service =
-      ProfileSyncServiceFactory::GetInstance()->GetForProfile(profile_);
+  syncer::SyncService* service =
+      ProfileSyncServiceFactory::GetSyncServiceForProfile(profile_);
   if (!service)
     return;
 
