f9b40b0081a4e64f32910963fd23a16bad96fd08
jdoerrie <jdoerrie@chromium.org>
2019-02-11 13:03:08 +0000

    [Sheriff] Disable SingleClientPreferencesSyncTest.ShouldRemoveBadDataWhenRegistering on Windows
    TBR=jkrcal@chromium.org
    
    Bug: 930482
    Change-Id: Ifbb07226d94f8b42cbdf7487d20bd8d72811d96b
    Reviewed-on: https://chromium-review.googlesource.com/c/1462897
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#630729}
    Former-commit-id: f86c4c9a8a99305a9aa805b9dca4f8ae3a534792

--- chrome/browser/sync/test/integration/single_client_preferences_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_preferences_sync_test.cc
@@ -6,6 +6,7 @@
 #include "base/strings/stringprintf.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/values.h"
+#include "build/build_config.h"
 #include "chrome/browser/sync/test/integration/feature_toggler.h"
 #include "chrome/browser/sync/test/integration/preferences_helper.h"
 #include "chrome/browser/sync/test/integration/profile_sync_service_harness.h"
@@ -71,8 +72,16 @@ IN_PROC_BROWSER_TEST_P(SingleClientPreferencesSyncTest, LateRegistration) {
   EXPECT_FALSE(BooleanPrefMatches(pref_name.c_str()));
 }
 
+// Flaky on Windows. https://crbug.com/930482
+#if defined(OS_WIN)
+#define MAYBE_ShouldRemoveBadDataWhenRegistering \
+  DISABLED_ShouldRemoveBadDataWhenRegistering
+#else
+#define MAYBE_ShouldRemoveBadDataWhenRegistering \
+  ShouldRemoveBadDataWhenRegistering
+#endif
 IN_PROC_BROWSER_TEST_P(SingleClientPreferencesSyncTest,
-                       ShouldRemoveBadDataWhenRegistering) {
+                       MAYBE_ShouldRemoveBadDataWhenRegistering) {
   // Populate the data store with data of type boolean but register as string.
   SetPreexistingPreferencesFileContents(
       0, "{\"testing\":{\"my-test-preference\":true}}");
