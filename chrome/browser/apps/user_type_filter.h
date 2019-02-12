3791e681ef0692f96779a8509a7e6ad51ad435a4
khmel@chromium.org <khmel@chromium.org>
2019-02-12 02:39:03 +0000

    apps: Fix accessing to profile from non-UI thread.
    In order to define type of profile (supervised and child case), it is
    required internally to request pref value that in general non-safe from
    non-UI thread. This CL switch to resolving user type on UI thread.
    
    TEST=Locally with DCHECK on + extra logging to confirm fix
    (eve + nocturne).
    BUG=930404
    
    Change-Id: I6f84c6e33d1b6c807b7a077393a35b0b9a10736b
    Reviewed-on: https://chromium-review.googlesource.com/c/1462417
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631053}
    Former-commit-id: 07072a5dbadb1b54bf8f1082b86f2652c0db91d9

--- chrome/browser/apps/user_type_filter.h
+++ chrome/browser/apps/user_type_filter.h
@@ -24,18 +24,26 @@ extern const char kUserTypeManaged[];
 extern const char kUserTypeSupervised[];
 extern const char kUserTypeUnmanaged[];
 
-// This filter is used to verify that testing profile |profile| matches user
-// type filter defined in root Json element |json_root|. |json_root| should have
-// the list of acceptable user types. Following values are valid: child, guest,
-// managed, supervised, unmanaged. Primary use of this filter is to determine if
-// particular default webapp or extension has to be installed for current user.
+// Returns user type based on |profile|. Must be called on UI thread. List of
+// possible values are |kUserTypeChild|, |kUserTypeGuest|, |kUserTypeManaged|,
+// |kUserTypeSupervised| and |kUserTypeUnmanaged|.
+std::string DetermineUserType(Profile* profile);
+
+// This filter is used to verify that profile's user type |user_type| matches
+// the user type filter defined in root Json element |json_root|. |json_root|
+// should have the list of acceptable user types. Following values are valid:
+// |kUserTypeChild|, |kUserTypeGuest|, |kUserTypeManaged|, |kUserTypeSupervised|
+// and |kUserTypeUnmanaged|. Primary use of this filter is to determine if
+// particular default webapp or extension has to be installed for the current
+// user.
 // Returns true if profile is accepted for this filter. |default_user_types| is
 // optional and used to support transition for the extension based default apps.
 // |app_id| is used for error logging purpose.
-bool ProfileMatchJsonUserType(Profile* profile,
-                              const std::string& app_id,
-                              const base::Value* json_root,
-                              const base::ListValue* default_user_types);
+// Safe to call on non-UI thread.
+bool UserTypeMatchesJsonUserType(const std::string& user_type,
+                                 const std::string& app_id,
+                                 const base::Value* json_root,
+                                 const base::ListValue* default_user_types);
 
 }  // namespace apps
 
