1a283e53d1fe43c25735ef2f65a6c35d4390788e
John Lee <johntlee@chromium.org>
2019-02-12 20:02:57 +0000

    Navi: Add grid of NTP backgrounds
    https://i.imgur.com/jTePPEf.png
    https://i.imgur.com/1iWYFnE.png
    Keyboard focused: https://i.imgur.com/pES0fsP.png
    
    Bug: 924176
    Change-Id: I60c90e0004c11cd6d31eb062ae367c0c89aa14bf
    Reviewed-on: https://chromium-review.googlesource.com/c/1459793
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631351}
    Former-commit-id: cea68d4b114b1db3fdc0a8001b9bca328509ad22

--- chrome/test/data/webui/welcome/nux_ntp_background_test.js
+++ chrome/test/data/webui/welcome/nux_ntp_background_test.js
@@ -9,12 +9,12 @@ cr.define('onboarding_ntp_background_test', function() {
       {
         id: 0,
         title: 'Cat',
-        image_url: 'some/cute/photo/of/a/cat',
+        imageUrl: 'some/cute/photo/of/a/cat',
       },
       {
         id: 1,
         title: 'Venice',
-        image_url: 'some/scenic/photo/of/a/beach',
+        imageUrl: 'some/scenic/photo/of/a/beach',
       },
     ];
 
@@ -25,6 +25,10 @@ cr.define('onboarding_ntp_background_test', function() {
     let testNtpBackgroundProxy;
 
     setup(function() {
+      loadTimeData.overrideValues({
+        ntpBackgroundDefault: 'Default',
+      });
+
       testNtpBackgroundProxy = new TestNtpBackgroundProxy();
       nux.NtpBackgroundProxyImpl.instance_ = testNtpBackgroundProxy;
       testNtpBackgroundProxy.setBackgroundsList(backgrounds);
@@ -43,9 +47,34 @@ cr.define('onboarding_ntp_background_test', function() {
       testElement.remove();
     });
 
-    test('test placeholder test', function() {
-      // TODO(johntlee): Add actual tests
-      assertTrue(true);
+    test('test displaying default and custom background', function() {
+      const options = testElement.shadowRoot.querySelectorAll(
+          '.ntp-background-grid-button');
+      assertEquals(3, options.length);
+
+      // the first option should be the 'Default' option
+      assertEquals(
+          options[0].querySelector('.ntp-background-title').innerText,
+          'Default');
+
+      for (let i = 0; i < backgrounds.length; i++) {
+        assertEquals(
+            options[i + 1].querySelector('.ntp-background-title').innerText,
+            backgrounds[i].title);
+      }
+    });
+
+    test('test activating a background', function() {
+      const options = testElement.shadowRoot.querySelectorAll(
+          '.ntp-background-grid-button');
+
+      options[1].click();
+      assertFalse(options[0].hasAttribute('active'));
+      assertEquals(options[0].getAttribute('aria-pressed'), 'false');
+      assertTrue(options[1].hasAttribute('active'));
+      assertEquals(options[1].getAttribute('aria-pressed'), 'true');
+      assertFalse(options[2].hasAttribute('active'));
+      assertEquals(options[2].getAttribute('aria-pressed'), 'false');
     });
   });
 });
