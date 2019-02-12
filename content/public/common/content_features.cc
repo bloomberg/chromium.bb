2b8d20b24fbaf62d86bfc8d3b51989c0abb2b309
Andreas Haas <ahaas@chromium.org>
2019-02-12 19:22:14 +0000

    [v8][wasm] Enable traphandler by default on Windows and macOS
    WebAssembly trap handler have been tested with a finch trial with 1% on
    stable on Windows and macOS, and the results look promising. We gained
    the expected performance improvements without significant regressions on
    crash rates and startup time. I think it is justified to do the next
    step to land this feature, which is enabling traphandler by default.
    
    The name of the two finch trials are V8WasmTrapHandlerWindows and
    V8WasmTrapHandlerWindows.
    
    R=titzer@chromium.org, hablich@chromium.org, piman@chromium.org
    
    Bug: chromium:903737, chromium:906565
    Change-Id: I00f55c91a628dc364cbe0526fa34ddf4ba92081c
    Reviewed-on: https://chromium-review.googlesource.com/c/1466602
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Michael Hablich <hablich@chromium.org>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631332}
    Former-commit-id: 0abbecf7d2588aef46abe6b46c13edf8704b5e0f

--- content/public/common/content_features.cc
+++ content/public/common/content_features.cc
@@ -524,7 +524,8 @@ const base::Feature kWebAssemblyThreads{"WebAssemblyThreads",
                                         base::FEATURE_DISABLED_BY_DEFAULT};
 
 // Enable WebAssembly trap handler.
-#if defined(OS_LINUX) && defined(ARCH_CPU_X86_64)
+#if (defined(OS_LINUX) || defined(OS_WIN) || defined(OS_MACOSX)) && \
+    defined(ARCH_CPU_X86_64)
 const base::Feature kWebAssemblyTrapHandler{"WebAssemblyTrapHandler",
                                             base::FEATURE_ENABLED_BY_DEFAULT};
 #else
