ca475f6c51199da3994351b9a0cd247795dee839
Tomasz Wiszkowski <ender@google.com>
2019-02-12 17:06:22 +0000

    Refer to Activity directly when presenting security dialog.
    Depending on Android version view context may be an Activity directly, or
    an instance of context wrapper that cannot be casted to Activity at all.
    This change ensures we always directly refer to Activity when presenting
    dialogs.
    
    Bug: 920998
    Change-Id: Iea8c16ef7da9e5bf2a9dddd20c1256a1134a0055
    Reviewed-on: https://chromium-review.googlesource.com/c/1464170
    Auto-Submit: Ender <ender@google.com>
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Ender <ender@google.com>
    Cr-Commit-Position: refs/heads/master@{#631274}
    Former-commit-id: 8003a855f086d58c82401977ca6f5d7fb36b5998

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/status/StatusViewCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/status/StatusViewCoordinator.java
@@ -4,8 +4,6 @@
 
 package org.chromium.chrome.browser.omnibox.status;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.res.Resources;
 import android.support.annotation.DrawableRes;
 import android.view.View;
@@ -152,18 +150,13 @@ public class StatusViewCoordinator implements View.OnClickListener {
     public void onClick(View view) {
         if (mUrlHasFocus) return;
 
-        // Get Activity from our managed view.
-        // TODO(ender): turn this into a property accessible via shared model.
-        Context context = view.getContext();
-        if (context == null || !(context instanceof Activity)) return;
-
         if (!mToolbarDataProvider.hasTab()
                 || mToolbarDataProvider.getTab().getWebContents() == null) {
             return;
         }
 
-        PageInfoController.show((Activity) context, mToolbarDataProvider.getTab(), null,
-                PageInfoController.OpenedFromSource.TOOLBAR);
+        PageInfoController.show(mToolbarDataProvider.getTab().getActivity(),
+                mToolbarDataProvider.getTab(), null, PageInfoController.OpenedFromSource.TOOLBAR);
     }
 
     /**
