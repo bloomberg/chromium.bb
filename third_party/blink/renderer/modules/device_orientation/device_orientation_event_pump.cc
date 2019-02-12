fb829ed27aef1a37faea87d3989b7e771170e6b0
Reilly Grant <reillyg@chromium.org>
2019-02-12 03:26:34 +0000

    [device_orientation] Split SensorEntry into a separate GCed class
    As the DeviceMotionEventPump and DeviceOrientationEventPump classes were
    refactored they became garbage collected. This is a problem because they
    own instances of SensorEntry which are not garbage collected and expect
    to be finalized.
    
    This change splits out SensorEntry into its own class which is now
    garbage collected with a prefinalizer to ensure that the Mojo binding
    is closed at the right time.
    
    Change-Id: I862918d80534c0375822d1e563928d9d1b59d53b
    Reviewed-on: https://chromium-review.googlesource.com/c/1464176
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631076}
    Former-commit-id: 68925f7f7c8f7c065118307bdf31c8d9f653f4b5

--- third_party/blink/renderer/modules/device_orientation/device_orientation_event_pump.cc
+++ third_party/blink/renderer/modules/device_orientation/device_orientation_event_pump.cc
@@ -2,14 +2,18 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "third_party/blink/renderer/modules/device_orientation/device_orientation_event_pump.h"
+
 #include <cmath>
 
+#include "services/device/public/cpp/generic_sensor/sensor_reading.h"
 #include "services/device/public/mojom/sensor.mojom-blink.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/modules/device_orientation/device_orientation_data.h"
-#include "third_party/blink/renderer/modules/device_orientation/device_orientation_event_pump.h"
+#include "third_party/blink/renderer/modules/device_orientation/device_sensor_entry.h"
 #include "third_party/blink/renderer/platform/heap/persistent.h"
+#include "third_party/blink/renderer/platform/wtf/functional.h"
 
 namespace {
 
@@ -41,19 +45,16 @@ const double DeviceOrientationEventPump::kOrientationThreshold = 0.1;
 DeviceOrientationEventPump::DeviceOrientationEventPump(
     scoped_refptr<base::SingleThreadTaskRunner> task_runner,
     bool absolute)
-    : DeviceSensorEventPump(task_runner),
-      relative_orientation_sensor_(
-          this,
-          device::mojom::SensorType::RELATIVE_ORIENTATION_EULER_ANGLES),
-      absolute_orientation_sensor_(
-          this,
-          device::mojom::SensorType::ABSOLUTE_ORIENTATION_EULER_ANGLES),
+    : DeviceSensorEventPump(std::move(task_runner)),
       absolute_(absolute),
-      fall_back_to_absolute_orientation_sensor_(!absolute) {}
-
-DeviceOrientationEventPump::~DeviceOrientationEventPump() {
-  StopIfObserving();
+      fall_back_to_absolute_orientation_sensor_(!absolute) {
+  relative_orientation_sensor_ = MakeGarbageCollected<DeviceSensorEntry>(
+      this, device::mojom::SensorType::RELATIVE_ORIENTATION_EULER_ANGLES);
+  absolute_orientation_sensor_ = MakeGarbageCollected<DeviceSensorEntry>(
+      this, device::mojom::SensorType::ABSOLUTE_ORIENTATION_EULER_ANGLES);
 }
+
+DeviceOrientationEventPump::~DeviceOrientationEventPump() = default;
 
 DeviceOrientationData*
 DeviceOrientationEventPump::LatestDeviceOrientationData() {
@@ -61,6 +62,8 @@ DeviceOrientationEventPump::LatestDeviceOrientationData() {
 }
 
 void DeviceOrientationEventPump::Trace(blink::Visitor* visitor) {
+  visitor->Trace(relative_orientation_sensor_);
+  visitor->Trace(absolute_orientation_sensor_);
   visitor->Trace(data_);
   PlatformEventDispatcher::Trace(visitor);
 }
@@ -80,15 +83,15 @@ void DeviceOrientationEventPump::SendStartMessage(LocalFrame* frame) {
         mojo::MakeRequest(&sensor_provider_));
     sensor_provider_.set_connection_error_handler(
         WTF::Bind(&DeviceSensorEventPump::HandleSensorProviderError,
-                  WrapPersistent(this)));
+                  WrapWeakPersistent(this)));
   }
 
   if (absolute_) {
-    absolute_orientation_sensor_.Start(sensor_provider_.get());
+    absolute_orientation_sensor_->Start(sensor_provider_.get());
   } else {
     fall_back_to_absolute_orientation_sensor_ = true;
     should_suspend_absolute_orientation_sensor_ = false;
-    relative_orientation_sensor_.Start(sensor_provider_.get());
+    relative_orientation_sensor_->Start(sensor_provider_.get());
   }
 }
 
@@ -103,7 +106,7 @@ void DeviceOrientationEventPump::SendStopMessage() {
   // the event listener is more rare than the page visibility changing,
   // Sensor::Suspend() is used to optimize this case for not doing extra work.
 
-  relative_orientation_sensor_.Stop();
+  relative_orientation_sensor_->Stop();
   // This is needed in case we fallback to using the absolute orientation
   // sensor. In this case, the relative orientation sensor is marked as
   // SensorState::SHOULD_SUSPEND, and if the relative orientation sensor
@@ -112,13 +115,13 @@ void DeviceOrientationEventPump::SendStopMessage() {
   // absolute_orientation_sensor_.Start() is called for initializing
   // the absolute orientation sensor in
   // DeviceOrientationEventPump::DidStartIfPossible().
-  if (relative_orientation_sensor_.sensor_state ==
-          SensorState::SHOULD_SUSPEND &&
+  if (relative_orientation_sensor_->state() ==
+          DeviceSensorEntry::State::SHOULD_SUSPEND &&
       fall_back_to_absolute_orientation_sensor_) {
     should_suspend_absolute_orientation_sensor_ = true;
   }
 
-  absolute_orientation_sensor_.Stop();
+  absolute_orientation_sensor_->Stop();
 
   // Reset the cached data because DeviceOrientationDispatcher resets its
   // data when stopping. If we don't reset here as well, then when starting back
@@ -137,16 +140,16 @@ void DeviceOrientationEventPump::FireEvent(TimerBase*) {
 }
 
 void DeviceOrientationEventPump::DidStartIfPossible() {
-  if (!absolute_ && !relative_orientation_sensor_.sensor &&
+  if (!absolute_ && !relative_orientation_sensor_->IsConnected() &&
       fall_back_to_absolute_orientation_sensor_ && sensor_provider_) {
     // When relative orientation sensor is not available fall back to using
     // the absolute orientation sensor but only on the first failure.
     fall_back_to_absolute_orientation_sensor_ = false;
-    absolute_orientation_sensor_.Start(sensor_provider_.get());
+    absolute_orientation_sensor_->Start(sensor_provider_.get());
     if (should_suspend_absolute_orientation_sensor_) {
       // The absolute orientation sensor needs to be marked as
       // SensorState::SUSPENDED when it is successfully initialized.
-      absolute_orientation_sensor_.sensor_state = SensorState::SHOULD_SUSPEND;
+      absolute_orientation_sensor_->Stop();
       should_suspend_absolute_orientation_sensor_ = false;
     }
     return;
@@ -155,14 +158,14 @@ void DeviceOrientationEventPump::DidStartIfPossible() {
 }
 
 bool DeviceOrientationEventPump::SensorsReadyOrErrored() const {
-  if (!relative_orientation_sensor_.ReadyOrErrored() ||
-      !absolute_orientation_sensor_.ReadyOrErrored()) {
+  if (!relative_orientation_sensor_->ReadyOrErrored() ||
+      !absolute_orientation_sensor_->ReadyOrErrored()) {
     return false;
   }
 
   // At most one sensor can be successfully initialized.
-  DCHECK(!relative_orientation_sensor_.sensor ||
-         !absolute_orientation_sensor_.sensor);
+  DCHECK(!relative_orientation_sensor_->IsConnected() ||
+         !absolute_orientation_sensor_->IsConnected());
 
   return true;
 }
@@ -172,47 +175,31 @@ DeviceOrientationData* DeviceOrientationEventPump::GetDataFromSharedMemory() {
   base::Optional<double> beta;
   base::Optional<double> gamma;
   bool absolute = false;
+  bool got_reading = false;
+  device::SensorReading reading;
 
-  if (!absolute_ && relative_orientation_sensor_.SensorReadingCouldBeRead()) {
-    // For DeviceOrientation Event, this provides relative orientation data.
-    if (relative_orientation_sensor_.reading.timestamp() == 0.0)
-      return nullptr;
-
-    if (!std::isnan(
-            relative_orientation_sensor_.reading.orientation_euler.z.value()))
-      alpha = relative_orientation_sensor_.reading.orientation_euler.z;
-
-    if (!std::isnan(
-            relative_orientation_sensor_.reading.orientation_euler.x.value()))
-      beta = relative_orientation_sensor_.reading.orientation_euler.x;
-
-    if (!std::isnan(
-            relative_orientation_sensor_.reading.orientation_euler.y.value()))
-      gamma = relative_orientation_sensor_.reading.orientation_euler.y;
-  } else if (absolute_orientation_sensor_.SensorReadingCouldBeRead()) {
-    // For DeviceOrientationAbsolute Event, this provides absolute orientation
-    // data.
-    //
-    // For DeviceOrientation Event, this provides absolute orientation data if
-    // relative orientation data is not available.
-    if (absolute_orientation_sensor_.reading.timestamp() == 0.0)
-      return nullptr;
-
-    if (!std::isnan(
-            absolute_orientation_sensor_.reading.orientation_euler.z.value()))
-      alpha = absolute_orientation_sensor_.reading.orientation_euler.z;
-
-    if (!std::isnan(
-            absolute_orientation_sensor_.reading.orientation_euler.x.value()))
-      beta = absolute_orientation_sensor_.reading.orientation_euler.x;
-
-    if (!std::isnan(
-            absolute_orientation_sensor_.reading.orientation_euler.y.value()))
-      gamma = absolute_orientation_sensor_.reading.orientation_euler.y;
-
+  if (!absolute_ && relative_orientation_sensor_->GetReading(&reading)) {
+    got_reading = true;
+  } else if (absolute_orientation_sensor_->GetReading(&reading)) {
+    got_reading = true;
     absolute = true;
   } else {
     absolute = absolute_;
+  }
+
+  if (got_reading) {
+    // For DeviceOrientation Event, this provides relative orientation data.
+    if (reading.timestamp() == 0.0)
+      return nullptr;
+
+    if (!std::isnan(reading.orientation_euler.z.value()))
+      alpha = reading.orientation_euler.z;
+
+    if (!std::isnan(reading.orientation_euler.x.value()))
+      beta = reading.orientation_euler.x;
+
+    if (!std::isnan(reading.orientation_euler.y.value()))
+      gamma = reading.orientation_euler.y;
   }
 
   return DeviceOrientationData::Create(alpha, beta, gamma, absolute);
