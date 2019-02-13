24ea38ba0df2addf9e1a93e73968c8cb29b4a776
Rayan Kanso <rayankans@chromium.org>
2019-02-13 13:11:43 +0000

    Add Logging/Querying functionality to DevToolsBackgroundServicesContext
    Set up the proto definitions and the read/write logic. Future features
    will define their own proto of want they want to store, integrate it
    here, and call `LogFeatureEvent` where necessary.
    
    Also adds a unit test.
    
    Bug: 927726
    
    Change-Id: Ic10b3412b4f4a1f8e95ffbbce73bd149dd8b80c6
    Reviewed-on: https://chromium-review.googlesource.com/c/1440421
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631662}
    Former-commit-id: 0698ca8ca9057cc48f240452e5ad2b8036774e66

--- content/browser/devtools/devtools_background_services_context.h
+++ content/browser/devtools/devtools_background_services_context.h
@@ -5,12 +5,23 @@
 #ifndef CONTENT_BROWSER_DEVTOOLS_DEVTOOLS_BACKGROUND_SERVICES_CONTEXT_H_
 #define CONTENT_BROWSER_DEVTOOLS_DEVTOOLS_BACKGROUND_SERVICES_CONTEXT_H_
 
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "base/callback_forward.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
+#include "content/browser/devtools/devtools_background_services.pb.h"
 #include "content/browser/service_worker/service_worker_context_wrapper.h"
 #include "content/common/content_export.h"
+
+namespace url {
+class Origin;
+}  // namespace url
 
 namespace content {
 
@@ -26,16 +37,49 @@ class ServiceWorkerContextWrapper;
 class CONTENT_EXPORT DevToolsBackgroundServicesContext
     : public base::RefCountedThreadSafe<DevToolsBackgroundServicesContext> {
  public:
+  using GetLoggedBackgroundServiceEventsCallback = base::OnceCallback<void(
+      std::vector<devtools::proto::BackgroundServiceState>)>;
+
   DevToolsBackgroundServicesContext(
       BrowserContext* browser_context,
       scoped_refptr<ServiceWorkerContextWrapper> service_worker_context);
 
+  // Queries all logged events for |service| and returns them in sorted order
+  // (by timestamp). |callback| is called with an empty vector if there was an
+  // error. Must be called from the IO thread.
+  void GetLoggedBackgroundServiceEvents(
+      devtools::proto::BackgroundService service,
+      GetLoggedBackgroundServiceEventsCallback callback);
+
  private:
+  friend class DevToolsBackgroundServicesContextTest;
   friend class base::RefCountedThreadSafe<DevToolsBackgroundServicesContext>;
   ~DevToolsBackgroundServicesContext();
 
+  // Entry point for logging a test event.
+  void LogTestBackgroundServiceEvent(
+      uint64_t service_worker_registration_id,
+      const url::Origin& origin,
+      devtools::proto::TestBackgroundServiceEvent event);
+
+  // Called after the Log*Event method creates the appropriate state proto.
+  void LogBackgroundServiceState(
+      uint64_t service_worker_registration_id,
+      const url::Origin& origin,
+      devtools::proto::BackgroundServiceState service_state);
+
+  void DidGetUserData(
+      GetLoggedBackgroundServiceEventsCallback callback,
+      const std::vector<std::pair<int64_t, std::string>>& user_data,
+      blink::ServiceWorkerStatusCode status);
+
   BrowserContext* browser_context_;
   scoped_refptr<ServiceWorkerContextWrapper> service_worker_context_;
+
+  // TODO(rayankans): Get this value from profile prefs.
+  bool should_record_ = false;
+
+  base::WeakPtrFactory<DevToolsBackgroundServicesContext> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(DevToolsBackgroundServicesContext);
 };
---
+++ content/browser/devtools/devtools_background_services_context_unittest.cc
@@ -0,0 +1,202 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/devtools/devtools_background_services_context.h"
+
+#include <string>
+#include <vector>
+
+#include "base/memory/scoped_refptr.h"
+#include "base/run_loop.h"
+#include "content/browser/devtools/devtools_background_services.pb.h"
+#include "content/browser/service_worker/embedded_worker_test_helper.h"
+#include "content/public/test/test_browser_context.h"
+#include "content/public/test/test_browser_thread_bundle.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "url/origin.h"
+
+namespace content {
+namespace {
+
+void DidRegisterServiceWorker(int64_t* out_service_worker_registration_id,
+                              base::OnceClosure quit_closure,
+                              blink::ServiceWorkerStatusCode status,
+                              const std::string& status_message,
+                              int64_t service_worker_registration_id) {
+  DCHECK(out_service_worker_registration_id);
+  EXPECT_EQ(blink::ServiceWorkerStatusCode::kOk, status) << status_message;
+
+  *out_service_worker_registration_id = service_worker_registration_id;
+
+  std::move(quit_closure).Run();
+}
+
+void DidFindServiceWorkerRegistration(
+    scoped_refptr<ServiceWorkerRegistration>* out_service_worker_registration,
+    base::OnceClosure quit_closure,
+    blink::ServiceWorkerStatusCode status,
+    scoped_refptr<ServiceWorkerRegistration> service_worker_registration) {
+  DCHECK(out_service_worker_registration);
+  EXPECT_EQ(blink::ServiceWorkerStatusCode::kOk, status)
+      << blink::ServiceWorkerStatusToString(status);
+
+  *out_service_worker_registration = service_worker_registration;
+
+  std::move(quit_closure).Run();
+}
+
+void DidGetLoggedBackgroundServiceEvents(
+    base::OnceClosure quit_closure,
+    std::vector<devtools::proto::BackgroundServiceState>* out_feature_states,
+    std::vector<devtools::proto::BackgroundServiceState> feature_states) {
+  *out_feature_states = std::move(feature_states);
+  std::move(quit_closure).Run();
+}
+
+}  // namespace
+
+class DevToolsBackgroundServicesContextTest : public ::testing::Test {
+ public:
+  DevToolsBackgroundServicesContextTest()
+      : thread_bundle_(TestBrowserThreadBundle::IO_MAINLOOP),
+        embedded_worker_test_helper_(base::FilePath() /* in memory */) {}
+
+  ~DevToolsBackgroundServicesContextTest() override = default;
+
+  void SetUp() override {
+    // Register Service Worker.
+    service_worker_registration_id_ = RegisterServiceWorker();
+    ASSERT_NE(service_worker_registration_id_,
+              blink::mojom::kInvalidServiceWorkerRegistrationId);
+
+    // Create |context_|.
+    context_ = base::MakeRefCounted<DevToolsBackgroundServicesContext>(
+        &browser_context_, embedded_worker_test_helper_.context_wrapper());
+    ASSERT_TRUE(context_);
+  }
+
+ protected:
+  std::vector<devtools::proto::BackgroundServiceState>
+  GetLoggedBackgroundServiceEvents() {
+    std::vector<devtools::proto::BackgroundServiceState> feature_states;
+
+    base::RunLoop run_loop;
+    context_->GetLoggedBackgroundServiceEvents(
+        devtools::proto::BackgroundService::TEST_BACKGROUND_SERVICE,
+        base::BindOnce(&DidGetLoggedBackgroundServiceEvents,
+                       run_loop.QuitClosure(), &feature_states));
+    run_loop.Run();
+
+    return feature_states;
+  }
+
+  void LogTestBackgroundServiceEvent(const std::string& log_message) {
+    devtools::proto::TestBackgroundServiceEvent event;
+    event.set_value(log_message);
+
+    context_->LogTestBackgroundServiceEvent(service_worker_registration_id_,
+                                            origin_, std::move(event));
+  }
+
+  void ToggleRecordingModeOn() { context_->should_record_ = true; }
+
+  TestBrowserThreadBundle thread_bundle_;  // Must be first member.
+  url::Origin origin_ = url::Origin::Create(GURL("https://example.com"));
+  int64_t service_worker_registration_id_ =
+      blink::mojom::kInvalidServiceWorkerRegistrationId;
+
+ private:
+  int64_t RegisterServiceWorker() {
+    GURL script_url(origin_.GetURL().spec() + "sw.js");
+    int64_t service_worker_registration_id =
+        blink::mojom::kInvalidServiceWorkerRegistrationId;
+
+    {
+      blink::mojom::ServiceWorkerRegistrationOptions options;
+      options.scope = origin_.GetURL();
+      base::RunLoop run_loop;
+      embedded_worker_test_helper_.context()->RegisterServiceWorker(
+          script_url, options,
+          base::BindOnce(&DidRegisterServiceWorker,
+                         &service_worker_registration_id,
+                         run_loop.QuitClosure()));
+
+      run_loop.Run();
+    }
+
+    if (service_worker_registration_id ==
+        blink::mojom::kInvalidServiceWorkerRegistrationId) {
+      ADD_FAILURE() << "Could not obtain a valid Service Worker registration";
+      return blink::mojom::kInvalidServiceWorkerRegistrationId;
+    }
+
+    {
+      base::RunLoop run_loop;
+      embedded_worker_test_helper_.context()->storage()->FindRegistrationForId(
+          service_worker_registration_id, origin_.GetURL(),
+          base::BindOnce(&DidFindServiceWorkerRegistration,
+                         &service_worker_registration_,
+                         run_loop.QuitClosure()));
+      run_loop.Run();
+    }
+
+    // Wait for the worker to be activated.
+    base::RunLoop().RunUntilIdle();
+
+    if (!service_worker_registration_) {
+      ADD_FAILURE() << "Could not find the new Service Worker registration.";
+      return blink::mojom::kInvalidServiceWorkerRegistrationId;
+    }
+
+    return service_worker_registration_id;
+  }
+
+  EmbeddedWorkerTestHelper embedded_worker_test_helper_;
+  TestBrowserContext browser_context_;
+  scoped_refptr<DevToolsBackgroundServicesContext> context_;
+  scoped_refptr<ServiceWorkerRegistration> service_worker_registration_;
+
+  DISALLOW_COPY_AND_ASSIGN(DevToolsBackgroundServicesContextTest);
+};
+
+TEST_F(DevToolsBackgroundServicesContextTest,
+       NothingStoredWithRecordingModeOff) {
+  // Initially there are no entries.
+  EXPECT_TRUE(GetLoggedBackgroundServiceEvents().empty());
+
+  // "Log" some events and wait for them to finish.
+  LogTestBackgroundServiceEvent("f1");
+  LogTestBackgroundServiceEvent("f2");
+
+  // There should still be nothing since recording mode is off.
+  EXPECT_TRUE(GetLoggedBackgroundServiceEvents().empty());
+}
+
+TEST_F(DevToolsBackgroundServicesContextTest, GetLoggedFeatures) {
+  ToggleRecordingModeOn();
+
+  // "Log" some events and wait for them to finish.
+  LogTestBackgroundServiceEvent("f1");
+  LogTestBackgroundServiceEvent("f2");
+
+  // Check the values.
+  auto feature_events = GetLoggedBackgroundServiceEvents();
+  ASSERT_EQ(feature_events.size(), 2u);
+
+  for (const auto& feature_event : feature_events) {
+    EXPECT_EQ(feature_event.background_service(),
+              devtools::proto::BackgroundService::TEST_BACKGROUND_SERVICE);
+    EXPECT_EQ(feature_event.origin(), origin_.GetURL().spec());
+    EXPECT_EQ(feature_event.service_worker_registration_id(),
+              service_worker_registration_id_);
+    ASSERT_TRUE(feature_event.has_test_event());
+  }
+
+  EXPECT_EQ(feature_events[0].test_event().value(), "f1");
+  EXPECT_EQ(feature_events[1].test_event().value(), "f2");
+
+  EXPECT_LE(feature_events[0].timestamp(), feature_events[1].timestamp());
+}
+
+}  // namespace content
