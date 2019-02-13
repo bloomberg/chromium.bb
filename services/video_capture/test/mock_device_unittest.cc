56bdfa087a6f802a832f32ba530995f34eaf4eed
Christian Fremerey <chfremer@chromium.org>
2019-02-13 01:01:23 +0000

    [Video Capture Service] Remove some unused methods
    Methods video_capture::mojom::Device::RequestRefreshFrame() and
    OnUtilizationReport() were initially added to match their
    counterpart in media::VideoCaptureDevice. However, the video
    capture service ended up being used for capture from devices
    exclusively, while the two above methods are only used in the
    context of capture from content, e.g. screen, window, or tab.
    
    This CL removes the unused methods from the video capture service
    API.
    
    Change-Id: Ie0c5aa5e3152d8c7d34cf0230eabef06d6feac55
    Reviewed-on: https://chromium-review.googlesource.com/c/1465141
    Reviewed-by: Weiyong Yao <braveyao@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631471}
    Former-commit-id: a3f9b35f9ba7831dfcfda55ad10b5a38af56fb1c

--- services/video_capture/test/mock_device_unittest.cc
+++ services/video_capture/test/mock_device_unittest.cc
@@ -59,43 +59,4 @@ TEST_F(MockVideoCaptureDeviceTest, DeviceIsStoppedWhenDiscardingDeviceClient) {
   }
 }
 
-// Tests that a utilization reported to a video_capture.mojom.Device via
-// OnReceiverReportingUtilization() arrives at the corresponding
-// media::VideoCaptureDevice.
-TEST_F(MockVideoCaptureDeviceTest, ReceiverUtilizationIsForwardedToDevice) {
-  base::RunLoop run_loop;
-  const media::VideoCaptureFormat stub_frame_format(gfx::Size(320, 200), 25.0f,
-                                                    media::PIXEL_FORMAT_I420);
-  const int arbitrary_rotation = 0;
-  const int arbitrary_frame_feedback_id = 654;
-  const double arbitrary_utilization = 0.12345;
-
-  EXPECT_CALL(*mock_receiver_, DoOnFrameReadyInBuffer(_, _, _, _))
-      .WillOnce(Invoke([this, &arbitrary_utilization](
-                           int32_t buffer_id, int32_t frame_feedback_id,
-                           mojom::ScopedAccessPermissionPtr*,
-                           media::mojom::VideoFrameInfoPtr*) {
-        device_proxy_->OnReceiverReportingUtilization(frame_feedback_id,
-                                                      arbitrary_utilization);
-      }));
-
-  EXPECT_CALL(mock_device_, OnUtilizationReport(arbitrary_frame_feedback_id,
-                                                arbitrary_utilization))
-      .Times(1);
-
-  device_proxy_->Start(requested_settings_, std::move(mock_receiver_proxy_));
-  run_loop.RunUntilIdle();
-
-  // Simulate device sending a frame, which should trigger |mock_receiver|
-  // DoOnFrameReadyInBuffer() getting called.
-  base::RunLoop run_loop_2;
-  mock_device_.SendStubFrame(stub_frame_format, arbitrary_rotation,
-                             arbitrary_frame_feedback_id);
-  run_loop_2.RunUntilIdle();
-
-  base::RunLoop run_loop_3;
-  mock_receiver_.reset();
-  run_loop_3.RunUntilIdle();
-}
-
 }  // namespace video_capture
