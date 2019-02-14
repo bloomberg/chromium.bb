ab56260ce411e4280c04ee8e1d1c2cee9ababbc9
Alexey Baskakov <loyso@chromium.org>
2019-02-14 06:03:57 +0000

    WebApp: Fix HostedAppPWAOnlyTest.InstallInstallableSite browser test.
    The order of events on Extenion installed:
    1) ExtensionRegistryObserver::OnExtensionInstalled
    2) CrxInstaller::NotifyCrxInstallComplete: NOTIFICATION_CRX_INSTALLER_DONE
    3) CrxInstaller::set_installer_callback fires.
    4) web_app::InstallManager::OnceInstallCallback fires.
    
    Previously the test used WaitForExtensionInstalled() which is a blocking
    wrapper for (1) plus flushing for all messages.
    
    The test waited for (1) while the real install completes after (2).
    It was working by accident: The extra message in a queue delayed the return
    from WaitForExtensionInstalled function.
    
    Solution: subscribe the test for the callback (4) and wait for the full
    completetion of the installation process.
    
    Stack traces:
    https://pastebin.com/wLi6RndL
    
    Bug: 915043
    Change-Id: I879d5260546a7d762e12588cd76063357389c72d
    Reviewed-on: https://chromium-review.googlesource.com/c/1470352
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632106}
    Former-commit-id: c826fc153564aa133ee41a27cc221bb64592413a

--- chrome/browser/ui/web_applications/web_app_dialog_utils.h
+++ chrome/browser/ui/web_applications/web_app_dialog_utils.h
@@ -5,9 +5,14 @@
 #ifndef CHROME_BROWSER_UI_WEB_APPLICATIONS_WEB_APP_DIALOG_UTILS_H_
 #define CHROME_BROWSER_UI_WEB_APPLICATIONS_WEB_APP_DIALOG_UTILS_H_
 
+#include "base/callback_forward.h"
+#include "chrome/browser/web_applications/components/web_app_helpers.h"
+
 class Browser;
 
 namespace web_app {
+
+enum class InstallResultCode;
 
 // Returns true if a WebApp installation is allowed for the current page.
 bool CanCreateWebApp(const Browser* browser);
@@ -15,6 +20,10 @@ bool CanCreateWebApp(const Browser* browser);
 // Initiates install of a WebApp for the current page.
 void CreateWebAppFromCurrentWebContents(Browser* browser,
                                         bool force_shortcut_app);
+
+using WebAppInstalledCallbackForTesting =
+    base::OnceCallback<void(const AppId& app_id, InstallResultCode code)>;
+void SetInstalledCallbackForTesting(WebAppInstalledCallbackForTesting callback);
 
 }  // namespace web_app
 
