3e3142e66778793355eed42c8a61e4116263df26
Daniel Rubery <drubery@chromium.org>
2019-02-14 00:26:13 +0000

    Use Block Mean Average for blurring image in PhishGuard Visual Features
    The None filter quality uses a nearest-neighbor approach for blurring,
    which leads to poor representation of the colors within the block.
    Instead, we use a block mean approach, for consistency with the backend.
    Also fixes some problems with RGB vs BGR byte ordering which can lead
    to incorrect colors on the backend.
    
    Bug: 922226
    Change-Id: I666d29c557d3c86870332394b9838951d04a2d21
    Reviewed-on: https://chromium-review.googlesource.com/c/1467462
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#631987}
    Former-commit-id: 5d8cfce3d5a4281b14fcba12b613f0bd14fdaf66

--- components/safe_browsing/password_protection/visual_utils.cc
+++ components/safe_browsing/password_protection/visual_utils.cc
@@ -92,8 +92,8 @@ bool GetBlurredImage(const SkBitmap& image,
       {2.22222f, 0.909672f, 0.0903276f, 0.222222f, 0.0812429f, 0, 0},
       SkNamedGamut::kRec2020);
 
-  // We scale down twice, once with medium quality, once with none quality to be
-  // consistent with the backend.
+  // We scale down twice, once with medium quality, then with a block mean
+  // average to be consistent with the backend.
   // TODO(drubery): Investigate whether this is necessary for performance or
   // not.
   SkImageInfo downsampled_info =
@@ -106,29 +106,70 @@ bool GetBlurredImage(const SkBitmap& image,
   image.pixmap().scalePixels(downsampled.pixmap(),
                              SkFilterQuality::kMedium_SkFilterQuality);
 
-  SkImageInfo blurred_info =
-      SkImageInfo::Make(kPHashDownsampleWidth / kPHashBlockSize,
-                        kPHashDownsampleHeight / kPHashBlockSize,
-                        SkColorType::kRGBA_8888_SkColorType,
-                        SkAlphaType::kUnpremul_SkAlphaType, rec2020);
-  SkBitmap blurred;
-  if (!blurred.tryAllocPixels(blurred_info))
-    return false;
-  downsampled.pixmap().scalePixels(blurred.pixmap(),
-                                   SkFilterQuality::kNone_SkFilterQuality);
+  std::unique_ptr<SkBitmap> blurred =
+      BlockMeanAverage(downsampled, kPHashBlockSize);
 
-  blurred_image->set_width(blurred.width());
-  blurred_image->set_height(blurred.height());
+  blurred_image->set_width(blurred->width());
+  blurred_image->set_height(blurred->height());
   blurred_image->clear_data();
 
-  const uint32_t* rgba = blurred.getAddr32(0, 0);
-  for (int i = 0; i < blurred.width() * blurred.height(); i++) {
+  const uint32_t* rgba = blurred->getAddr32(0, 0);
+  for (int i = 0; i < blurred->width() * blurred->height(); i++) {
+    // Data is stored in BGR order.
     *blurred_image->mutable_data() += static_cast<char>((rgba[i] >> 0) & 0xff);
     *blurred_image->mutable_data() += static_cast<char>((rgba[i] >> 8) & 0xff);
     *blurred_image->mutable_data() += static_cast<char>((rgba[i] >> 16) & 0xff);
   }
 
   return true;
+}
+
+std::unique_ptr<SkBitmap> BlockMeanAverage(const SkBitmap& image,
+                                           int block_size) {
+  // Compute the number of blocks in the target image, rounding up to account
+  // for partial blocks.
+  int num_blocks_high =
+      std::ceil(static_cast<float>(image.height()) / block_size);
+  int num_blocks_wide =
+      std::ceil(static_cast<float>(image.width()) / block_size);
+
+  SkImageInfo target_info = SkImageInfo::Make(
+      num_blocks_wide, num_blocks_high, SkColorType::kRGBA_8888_SkColorType,
+      SkAlphaType::kUnpremul_SkAlphaType, image.refColorSpace());
+  auto target = std::make_unique<SkBitmap>();
+  if (!target->tryAllocPixels(target_info))
+    return target;
+
+  for (int block_x = 0; block_x < num_blocks_wide; block_x++) {
+    for (int block_y = 0; block_y < num_blocks_high; block_y++) {
+      int r_total = 0, g_total = 0, b_total = 0, sample_count = 0;
+
+      // Compute boundary for the current block, taking into account the
+      // possibility of partial blocks near the edges.
+      int x_start = block_x * block_size;
+      int x_end = std::min(x_start + block_size, image.width());
+
+      int y_start = block_y * block_size;
+      int y_end = std::min(y_start + block_size, image.height());
+      for (int i = x_start; i < x_end; i++) {
+        for (int j = y_start; j < y_end; j++) {
+          r_total += SkColorGetR(image.getColor(i, j));
+          g_total += SkColorGetG(image.getColor(i, j));
+          b_total += SkColorGetB(image.getColor(i, j));
+          sample_count++;
+        }
+      }
+
+      int r_mean = r_total / sample_count;
+      int g_mean = g_total / sample_count;
+      int b_mean = b_total / sample_count;
+
+      *target->getAddr32(block_x, block_y) =
+          (255 << 24) | (b_mean << 16) | (g_mean << 8) | (r_mean << 0);
+    }
+  }
+
+  return target;
 }
 
 }  // namespace visual_utils
