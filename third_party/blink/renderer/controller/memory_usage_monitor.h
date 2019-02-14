04c41d26b19d6824cc16cb139973fe71bb2df851
Gyuyoung Kim <gyuyoung@igalia.com>
2019-02-14 03:43:59 +0000

    BlinkMemoryMgt: Apply USING_FAST_MALLOC to MemoryUsageMonitor
    Though none garbage-collectable Blink objects should be allocated by
    PartitionAlloc, MemoryUsageMonitor has not allocated by the PartitionAlloc
    so far. This CL adds USING_FAST_MALLOC to the classes to make them to allocate
    the memory from the FastMalloc partition of PartitionAlloc.
    
    Bug: 919389
    Change-Id: Ibf64f582f2c567189f96cc49e6957e34a5ef452b
    Reviewed-on: https://chromium-review.googlesource.com/c/1469756
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#632065}
    Former-commit-id: 507ee4ca8862031db831479ff85964618e93098e

--- third_party/blink/renderer/controller/memory_usage_monitor.h
+++ third_party/blink/renderer/controller/memory_usage_monitor.h
@@ -8,6 +8,7 @@
 #include "base/observer_list.h"
 #include "third_party/blink/renderer/controller/controller_export.h"
 #include "third_party/blink/renderer/platform/timer.h"
+#include "third_party/blink/renderer/platform/wtf/allocator.h"
 
 namespace blink {
 
@@ -24,6 +25,8 @@ struct MemoryUsage {
 // Periodically checks the memory usage and notifies its observers. Monitoring
 // automatically starts/stops depending on whether an observer exists.
 class CONTROLLER_EXPORT MemoryUsageMonitor {
+  USING_FAST_MALLOC(MemoryUsageMonitor);
+
  public:
   static MemoryUsageMonitor& Instance();
 
