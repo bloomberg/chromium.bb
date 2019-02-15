3801b9068a53f450b3cbbdb6b87ca4554b4f23eb
Torne (Richard Coles) <torne@google.com>
2019-02-15 19:24:59 +0000

    Remove MonochromeApplication from Trichrome.
    Remove the MonochromeApplication class from TrichromeChrome's manifest
    so that the regular ChromeApplication will be used instead, to avoid
    trying to set up Monochrome-specific things. Update the comment in
    MonochromeApplication to make it clear that it's not used by
    Trichrome and that it must only be used for things that are specific to
    Monochrome being a WebView implementation.
    
    This fixes the startup crash caused by Trichrome attempting to use
    BIND_EXTERNAL_SERVICE for its child processes when the Trichrome
    manifest does not declare the services as external.
    
    Bug: 917417
    Test: install and launch trichrome_chrome_apk
    Change-Id: If857dfb73b75e15c73398779a159ce7bb1ad56cb
    Reviewed-on: https://chromium-review.googlesource.com/c/1474997
    Commit-Queue: Richard Coles <torne@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Richard Coles <torne@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632722}
    Former-commit-id: dfb318704ba7d1ec22572c09f3157f3a55c9253d

--- chrome/android/java/src/org/chromium/chrome/browser/MonochromeApplication.java
+++ chrome/android/java/src/org/chromium/chrome/browser/MonochromeApplication.java
@@ -16,6 +16,11 @@ import org.chromium.content_public.browser.ChildProcessCreationParams;
  * You shouldn't add anything else in this file, this class is split off from
  * normal chrome in order to access Android system API through Android WebView
  * glue layer and have monochrome specific code.
+ *
+ * This class is NOT used by Trichrome. Do not add anything here which is only
+ * related to Monochrome's minimum SDK level or APK packaging decisions, because
+ * those are likely to apply to Trichrome as well - this must only be used for
+ * things specific to functioning as a WebView implementation.
  */
 public class MonochromeApplication extends ChromeApplication {
     @Override
@@ -23,7 +28,9 @@ public class MonochromeApplication extends ChromeApplication {
         super.onCreate();
         LibraryLoader.getInstance().setNativeLibraryPreloader(new MonochromeLibraryPreloader());
         // ChildProcessCreationParams is only needed for browser process, though it is
-        // created and set in all processes.
+        // created and set in all processes. We must set isExternalService to true for
+        // Monochrome because Monochrome's renderer services are shared with WebView
+        // and are external, and will fail to bind otherwise.
         boolean bindToCaller = false;
         boolean ignoreVisibilityForImportance = false;
         ChildProcessCreationParams.set(getPackageName(), true /* isExternalService */,
