a27181dbcc44b9483dd361f7c80d58e23dc0dc5a
Mugdha Lakhani <nator@chromium.org>
2019-02-15 10:35:23 +0000

    [Background Sync] Update UMA recording logic.
    Add the newly added Background Task to BackgroundTaskId enum
    and update the UMA recording logic to derive UMA value from TaskID.
    
    Bug: 924490,931795,931705
    Change-Id: Ibc2c2a1d01906752fc43e9cef6ebbf0a85176d7e
    Reviewed-on: https://chromium-review.googlesource.com/c/1472717
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632569}
    Former-commit-id: a1d6f49fe0ae638e45a61f7edc46d3489741b4a7

--- components/background_task_scheduler/android/junit/src/org/chromium/components/background_task_scheduler/BackgroundTaskSchedulerUmaTest.java
+++ components/background_task_scheduler/android/junit/src/org/chromium/components/background_task_scheduler/BackgroundTaskSchedulerUmaTest.java
@@ -96,7 +96,10 @@ public class BackgroundTaskSchedulerUmaTest {
         assertEquals(BackgroundTaskSchedulerUma.BACKGROUND_TASK_DEPRECATED_EXPLORE_SITES_REFRESH,
                 BackgroundTaskSchedulerUma.toUmaEnumValueFromTaskId(
                         TaskIds.DEPRECATED_EXPLORE_SITES_REFRESH_JOB_ID));
-        assertEquals(BackgroundTaskSchedulerUma.BACKGROUND_TASK_COUNT, 19);
+        assertEquals(BackgroundTaskSchedulerUma.BACKGROUND_TASK_ONE_SHOT_SYNC_WAKE_UP,
+                BackgroundTaskSchedulerUma.toUmaEnumValueFromTaskId(
+                        TaskIds.BACKGROUND_SYNC_ONE_SHOT_JOB_ID));
+        assertEquals(BackgroundTaskSchedulerUma.BACKGROUND_TASK_COUNT, 20);
     }
 
     @Test
