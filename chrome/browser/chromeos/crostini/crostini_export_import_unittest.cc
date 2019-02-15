070746e4edd8e85ee727a4af69f945fdd0938116
Joel Hockey <joelhockey@chromium.org>
2019-02-15 09:08:35 +0000

    Disable tests failing asan/lsan/msan
    Bug: 912638
    Change-Id: I1d4b17386e64c9c82de1f3f6d0d315f32c961add
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I1d4b17386e64c9c82de1f3f6d0d315f32c961add
    Reviewed-on: https://chromium-review.googlesource.com/c/1474886
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632551}
    Former-commit-id: ed6ef08678156e50ac742827bae6aa29b3901133

--- chrome/browser/chromeos/crostini/crostini_export_import_unittest.cc
+++ chrome/browser/chromeos/crostini/crostini_export_import_unittest.cc
@@ -106,7 +106,7 @@ class CrostiniExportImportTest : public testing::Test {
   DISALLOW_COPY_AND_ASSIGN(CrostiniExportImportTest);
 };
 
-TEST_F(CrostiniExportImportTest, TestExportSuccess) {
+TEST_F(CrostiniExportImportTest, DISABLED_TestExportSuccess) {
   crostini_export_import()->FileSelected(
       tarball_, 0, reinterpret_cast<void*>(ExportImportType::EXPORT));
   run_loop_->RunUntilIdle();
@@ -151,7 +151,7 @@ TEST_F(CrostiniExportImportTest, TestExportSuccess) {
             CrostiniExportImportNotification::Status::DONE);
 }
 
-TEST_F(CrostiniExportImportTest, TestExportFail) {
+TEST_F(CrostiniExportImportTest, DISABLED_TestExportFail) {
   crostini_export_import()->FileSelected(
       tarball_, 0, reinterpret_cast<void*>(ExportImportType::EXPORT));
   run_loop_->RunUntilIdle();
@@ -165,7 +165,7 @@ TEST_F(CrostiniExportImportTest, TestExportFail) {
             CrostiniExportImportNotification::Status::FAILED);
 }
 
-TEST_F(CrostiniExportImportTest, TestImportSuccess) {
+TEST_F(CrostiniExportImportTest, DISABLED_TestImportSuccess) {
   crostini_export_import()->FileSelected(
       tarball_, 0, reinterpret_cast<void*>(ExportImportType::IMPORT));
   run_loop_->RunUntilIdle();
@@ -211,7 +211,7 @@ TEST_F(CrostiniExportImportTest, TestImportSuccess) {
             CrostiniExportImportNotification::Status::DONE);
 }
 
-TEST_F(CrostiniExportImportTest, TestImportFail) {
+TEST_F(CrostiniExportImportTest, DISABLED_TestImportFail) {
   crostini_export_import()->FileSelected(
       tarball_, 0, reinterpret_cast<void*>(ExportImportType::IMPORT));
   run_loop_->RunUntilIdle();
