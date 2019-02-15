0ff32320ee1207ae50b143742ac1796b2df4abf1
Min Qin <qinmin@chromium.org>
2019-02-15 01:47:49 +0000

    Allow WebContentsImpl::FromRenderFrameHostId() to be called without UI thread
    In servicification mode, this could get called by NetworkServiceClient.
    An alternative change is to return nullptr in NetworkServiceClient if UI
    thread is not created, and we can also do a non-validity check on frame host Id
    
    BUG=930736
    
    Change-Id: I9a3b85706fa5f283485e29ba5254a54341f57569
    Reviewed-on: https://chromium-review.googlesource.com/c/1464351
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632447}
    Former-commit-id: a11501145cc9f82c83df87b06d52cdb2c9073fb5

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -821,7 +821,8 @@ WebContentsImpl* WebContentsImpl::FromFrameTreeNode(
 // static
 WebContents* WebContentsImpl::FromRenderFrameHostID(int render_process_host_id,
                                                     int render_frame_host_id) {
-  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI) ||
+         !BrowserThread::IsThreadInitialized(BrowserThread::UI));
   RenderFrameHost* render_frame_host =
       RenderFrameHost::FromID(render_process_host_id, render_frame_host_id);
   if (!render_frame_host)
