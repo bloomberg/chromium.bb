9174d7064dfecc4cd93eb6de7013628d062cded8
Alexander Alekseev <alemate@chromium.org>
2019-02-15 09:13:17 +0000

    Chrome OS: Remove html-echo from common elements.
    html-echo is a legacy element, and is currently used in a single place.
    So it's reasonable to move the dependency to that source.
    
    Bug: 926638
    Change-Id: I00fdc114dcc80743d4f3b23c8a8055e054cd13ad
    Reviewed-on: https://chromium-review.googlesource.com/c/1460130
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632554}
    Former-commit-id: c1220c770d92a71badb698842ef4a0703c5313af

--- chrome/browser/resources/chromeos/login/custom_elements_oobe.js
+++ chrome/browser/resources/chromeos/login/custom_elements_oobe.js
@@ -17,7 +17,6 @@
 // <include src="gaia_input.js">
 // <include src="gaia_password_changed.js">
 // <include src="hd-iron-icon.js">
-// <include src="html-echo.js">
 // <include src="network_select_login.js">
 // <include src="notification_card.js">
 // <include src="offline_gaia.js">
