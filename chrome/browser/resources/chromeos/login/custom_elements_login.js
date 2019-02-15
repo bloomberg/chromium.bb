add5b2d0ff73a79c47c79374b1cc61185e4a983e
Alexander Alekseev <alemate@chromium.org>
2019-02-15 09:08:50 +0000

    Chrome OS: Remove enrollment UI from md_login.
    Enrollment could not be started after device is configured. So this code
    is unused.
    
    Bug: 926638
    Change-Id: I7e70d79b3e438bd50b79754a6614a4e712c0573f
    Reviewed-on: https://chromium-review.googlesource.com/c/1460302
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632552}
    Former-commit-id: f3a330cdf5d4043ea4c8c900e63e31e10467ceb8

--- chrome/browser/resources/chromeos/login/custom_elements_login.js
+++ chrome/browser/resources/chromeos/login/custom_elements_login.js
@@ -25,13 +25,11 @@
 // <include src="oobe_buttons.js">
 // <include src="oobe_change_picture.js">
 // <include src="oobe_dialog.js">
-// <include src="oobe_enrollment.js">
 // <include src="arc_terms_of_service.js">
 // <include src="oobe_reset.js">
 // <include src="oobe_reset_confirmation_overlay.js">
 // <include src="encryption_migration.js">
 // <include src="oobe_supervision_transition.js">
-// <include src="enrollment_license_card.js">
 // <include src="sync_consent.js">
 // <include src="fingerprint_setup.js">
 // <include src="recommend_apps.js">
