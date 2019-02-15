79b12f7061dedee4a4d37d9445918598f83354ec
Hiroshige Hayashizaki <hiroshige@chromium.org>
2019-02-15 20:53:18 +0000

    Deflake http/tests/devtools/isolated-code-cache/same-origin-test.js
    The test was flaky because the ScriptResource can be
    garbage collected before the test finished.
    
    This CL keeps the ScriptResource alive, by loading the
    script as a parser-inserted script.
    This uses a Blink-specific behavior -- a parser-inserted
    <script> keeps alive its corresponding ScriptResource
    but a dynamically-inserted <script> doesn't.
    This is somehow hacky, but seems to work.
    
    Bug: 841466, 906320, 927296, 922951
    Change-Id: Ia45787d917191d81917e89f6843ad8b0cbcc7e76
    Reviewed-on: https://chromium-review.googlesource.com/c/1475077
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Mythri Alle <mythria@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632755}
    Former-commit-id: 46451301868653128f6b064e58edf18a8bb7ff7d

--- third_party/blink/web_tests/http/tests/devtools/isolated-code-cache/same-origin-test.js
+++ third_party/blink/web_tests/http/tests/devtools/isolated-code-cache/same-origin-test.js
@@ -11,51 +11,48 @@
   // scripts in the cache.
   SDK.multitargetNetworkManager.clearBrowserCache();
 
-  await TestRunner.evaluateInPagePromise(`
-      function loadScript() {
-        const url = 'http://localhost:8000/devtools/resources/v8-cache-script.js';
-        const frameId = 'frame_id';
-        let iframeWindow = document.getElementById(frameId).contentWindow;
-        return iframeWindow.loadScript(url)
-            .then(() => iframeWindow.loadScript(url))
-            .then(() => iframeWindow.loadScript(url));
-      }
-      function differentURLLoadScript() {
-        const url = 'http://127.0.0.1:8000/devtools/resources/v8-cache-script.js';
-        const frameId = 'diff_url_frame';
-        let iframeWindow = document.getElementById(frameId).contentWindow;
-        return iframeWindow.loadScript(url);
-      }
-      function sameURLLoadScript() {
-        const url = 'http://localhost:8000/devtools/resources/v8-cache-script.js';
-        const frameId = 'same_url_frame';
-        let iframeWindow = document.getElementById(frameId).contentWindow;
-        return iframeWindow.loadScript(url);
-      }
-  `);
+  // There are two scripts:
+  // [A] http://127.0.0.1:8000/devtools/resources/v8-cache-script.cgi
+  // [B] http://localhost:8000/devtools/resources/v8-cache-script.cgi
 
-  const scope = 'http://127.0.0.1:8000/devtools/service-workers/resources/v8-cache-iframe.html';
-  const frameId = 'frame_id';
-  const different_url_frameId = 'diff_url_frame';
-  const same_url_frameId = 'same_url_frame';
-
-  await TestRunner.addIframe(scope, {id: frameId});
-  await TestRunner.addIframe(scope, {id: different_url_frameId});
-  await TestRunner.addIframe(scope, {id: same_url_frameId});
+  // An iframe that loads [A].
+  // The script is executed as a parser-inserted script,
+  // to keep the ScriptResource on the MemoryCache.
+  // ScriptResources for dynamically-inserted <script>s can be
+  // garbage-collected and thus removed from MemoryCache after its execution.
+  const scope = 'resources/same-origin-script.html';
+  // An iframe that loads [B].
+  const scopeCrossOrigin = 'resources/cross-origin-script.html';
 
   TestRunner.addResult('--- Trace events related to code caches ------');
   await PerformanceTestRunner.startTimeline();
-  // This loads the same script thrice. With the current V8 heuristics (defined
+
+  // Load [A] thrice. With the current V8 heuristics (defined
   // in third_party/blink/renderer/bindings/core/v8/v8_code_cache.cc) we produce
   // cache on second fetch and consume it in the third fetch. This tests these
   // heuristics.
-  await TestRunner.callFunctionInPageAsync('loadScript');
+  // Note that addIframe() waits for iframe's load event, which waits for the
+  // <script> loading.
+  await TestRunner.addIframe(scope);
+  await TestRunner.addIframe(scope);
+  await TestRunner.addIframe(scope);
 
-  // Load from a different origin should not use the cached code
-  await TestRunner.callFunctionInPageAsync('differentURLLoadScript');
+  // Load [B]. Should not use the cached code.
+  await TestRunner.addIframe(scopeCrossOrigin);
 
-  // Loading from the same origin should use cached code
-  await TestRunner.callFunctionInPageAsync('sameURLLoadScript');
+  // Load [A] again from MemoryCache. Should use cached code.
+  await TestRunner.addIframe(scope);
+
+  // Clear [A] from MemoryCache. Blink evicts previous Resource when a
+  // new request to the same URL but with different resource type is started.
+  // We fetch() to the URL of [A], and thus evicts the previous ScriptResource
+  // of [A].
+  await TestRunner.evaluateInPageAsync(
+      `fetch('/devtools/resources/v8-cache-script.cgi')`);
+
+  // Load [A] from Disk Cache. As we cleared [A] from MemoryCache, this
+  // doesn't hit MemoryCache, but still hits Disk Cache.
+  await TestRunner.addIframe(scope);
 
   await PerformanceTestRunner.stopTimeline();
   PerformanceTestRunner.printTimelineRecordsWithDetails(
