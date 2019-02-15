310a06d4ba828c1104edccdd4e1a077a64d32cf4
Tommy C. Li <tommycli@chromium.org>
2019-02-15 20:07:49 +0000

    Omnibox: Experiment with a white background for the unfocused omnibox
    Sets the unfocused omnibox background to the same color as the results
    popup, which is conventionally white.
    
    Also enables the location bar border stroke in this mode, which already
    exists for high contrast mode.
    
    Bug: 931836
    Change-Id: I97e0a5c768c739f4f037d9e3abdb99115b14999d
    Reviewed-on: https://chromium-review.googlesource.com/c/1474281
    Reviewed-by: Kevin Bailey <krb@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Auto-Submit: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632737}
    Former-commit-id: a04e459e1cc1c2eec4f9052ebd5cfa8bca1856ec

--- components/omnibox/browser/omnibox_field_trial.h
+++ components/omnibox/browser/omnibox_field_trial.h
@@ -52,6 +52,7 @@ extern const base::Feature kUIExperimentShowSuggestionFavicons;
 extern const base::Feature kUIExperimentSwapTitleAndUrl;
 extern const base::Feature kUIExperimentVerticalMargin;
 extern const base::Feature kUIExperimentShowSuffixOnAllSearchSuggestions;
+extern const base::Feature kUIExperimentWhiteBackgroundOnBlur;
 extern const base::Feature kUIExperimentUseGenericSearchEngineIcon;
 extern const base::Feature kSpeculativeServiceWorkerStartOnQueryInput;
 extern const base::Feature kDocumentProvider;
