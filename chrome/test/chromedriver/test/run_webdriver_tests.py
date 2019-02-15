d97b62bb0023f98573c7e55325865e8c5dc6d1da
kyle Ju <kyleju@chromium.org>
2019-02-15 15:29:50 +0000

    Add WPT Serve to serve the content of tests.
    Fix WPT WebDriver test failures due to the lack of WPT Serve to serve the test content.
    Add a flag to Chrome to reroute web-platform.test to 127.0.0.1
    
    No-Export: True
    Bug: 881513
    Change-Id: I34398bc0eb7b3d8e288aae4358d2b195b46c324a
    Reviewed-on: https://chromium-review.googlesource.com/c/1462139
    Reviewed-by: John Chen <johnchen@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Kyle Ju <kyleju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632623}
    Former-commit-id: 0eabe31e6d834bedb7c6380915118719d1deb444

--- chrome/test/chromedriver/test/run_webdriver_tests.py
+++ chrome/test/chromedriver/test/run_webdriver_tests.py
@@ -10,6 +10,9 @@ import argparse
 import sys
 import json
 import time
+import logging
+
+_log = logging.getLogger(__name__)
 
 _TEST_DIR = os.path.abspath(os.path.dirname(__file__))
 _CHROMEDRIVER_DIR = os.path.join(_TEST_DIR, os.pardir)
@@ -17,15 +20,21 @@ SRC_DIR = os.path.join(_CHROMEDRIVER_DIR, os.pardir, os.pardir, os.pardir)
 _CLIENT_DIR = os.path.join(_CHROMEDRIVER_DIR, "client")
 _SERVER_DIR = os.path.join(_CHROMEDRIVER_DIR, "server")
 
-sys.path.insert(1, _CHROMEDRIVER_DIR)
+sys.path.insert(0, _CHROMEDRIVER_DIR)
 import util
 
-sys.path.insert(1, _SERVER_DIR)
+sys.path.insert(0, _SERVER_DIR)
 import server
 
-WD_CLIENT_PATH = ('third_party/blink/tools/blinkpy/'
-                  'third_party/wpt/wpt/tools/webdriver')
-WEBDRIVER_CLIENT_ABS_PATH = os.path.join(SRC_DIR, WD_CLIENT_PATH)
+BLINK_TOOLS_PATH = 'third_party/blink/tools'
+BLINK_TOOLS_ABS_PATH = os.path.join(SRC_DIR, BLINK_TOOLS_PATH)
+
+sys.path.insert(0, BLINK_TOOLS_ABS_PATH)
+from blinkpy.common.host import Host
+from blinkpy.common.system.log_utils import configure_logging
+
+WD_CLIENT_PATH = 'blinkpy/third_party/wpt/wpt/tools/webdriver'
+WEBDRIVER_CLIENT_ABS_PATH = os.path.join(BLINK_TOOLS_ABS_PATH, WD_CLIENT_PATH)
 
 
 class WebDriverTestResult(object):
@@ -84,15 +93,26 @@ def set_up_config(chromedriver_server):
                   }
               }
           },
+          "args": [
+              "--host-resolver-rules="
+              "MAP nonexistent.*.test ~NOTFOUND, "
+              "MAP *.test 127.0.0.1"
+          ]
+
       }
   })
+
+  # Port numbers are defined at
+  # https://cs.chromium.org/chromium/src/third_party/blink/tools
+  # /blinkpy/web_tests/servers/wptserve.py?l=23&rcl=375b34c6ba64
+  # 5d00c1413e4c6106c7bb74581c85
   os.environ["WD_SERVER_CONFIG"] = json.dumps({
     "browser_host": "web-platform.test",
-    "ports": {"http": [8000, 8001]}})
+    "ports": {"ws": [9001], "wss": [9444], "http": [8001], "https": [8444]}})
 
 def run_test(path):
   subtests = SubtestResultRecorder()
-  pytest.main(["--show-capture", "no", path], plugins=[subtests])
+  pytest.main(["-s", path], plugins=[subtests])
   return subtests.result
 
 if __name__ == '__main__':
@@ -116,8 +136,18 @@ if __name__ == '__main__':
   parser.add_argument(
       '--test-path',
       help='Path to the WPT WebDriver tests')
+  parser.add_argument(
+      '-v', '--verbose', action='store_true',
+      help='log extra details that may be helpful when debugging')
 
   options = parser.parse_args()
+
+  log_level = logging.DEBUG if options.verbose else logging.INFO
+  configure_logging(logging_level=log_level, include_time=True)
+
+  # Starts WPT Serve to serve the WPT WebDriver test content.
+  port = Host().port_factory.get()
+  port.start_wptserve()
 
   options.chromedriver = util.GetAbsolutePathOfUserPath(options.chromedriver)
   if (not os.path.exists(options.chromedriver) and
@@ -132,7 +162,7 @@ if __name__ == '__main__':
   chromedriver_server = server.Server(options.chromedriver, options.log_path)
 
   if not chromedriver_server.IsRunning():
-    print 'ChromeDriver is not running.'
+    _log.error('ChromeDriver is not running.')
     sys.exit(1)
 
   set_up_config(chromedriver_server)
@@ -140,22 +170,25 @@ if __name__ == '__main__':
   test_path = options.test_path
   start_time = time.time()
   test_results = []
-  sys.path.insert(1, WEBDRIVER_CLIENT_ABS_PATH)
-  if os.path.isfile(test_path):
-    test_results = run_test(test_path)
-  elif os.path.isdir(test_path):
-    for root, dirnames, filenames in os.walk(test_path):
-      for filename in filenames:
-        if '__init__' in filename:
-          continue
-        test_file = os.path.join(root, filename)
-        test_results += run_test(test_file)
-  else:
-    print '%s is not a file nor directory.' % test_path
-    sys.exit(1)
-  sys.path.remove(WEBDRIVER_CLIENT_ABS_PATH)
-
-  chromedriver_server.Kill()
+  sys.path.insert(0, WEBDRIVER_CLIENT_ABS_PATH)
+  try:
+    if os.path.isfile(test_path):
+      test_results = run_test(test_path)
+    elif os.path.isdir(test_path):
+      for root, dirnames, filenames in os.walk(test_path):
+        for filename in filenames:
+          if '__init__' in filename:
+            continue
+          test_file = os.path.join(root, filename)
+          test_results += run_test(test_file)
+    else:
+      _log.error('%s is not a file nor directory.' % test_path)
+      sys.exit(1)
+  except KeyboardInterrupt as e:
+    _log.error(e)
+  finally:
+    chromedriver_server.Kill()
+    port.stop_wptserve()
 
   if options.isolated_script_test_output:
     output = {
