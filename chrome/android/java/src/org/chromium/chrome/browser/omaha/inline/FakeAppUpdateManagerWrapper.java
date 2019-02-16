81b22a21628ad8adcdcc7eddf016711ee27f17f8
Tommy Nyquist <nyquist@chromium.org>
2019-02-16 03:47:57 +0000

    Add helpful debug messages for testing inline flow.
    When going through test scenarios for inline flows, this CL ensures
    that a system toast is shown for each interaction with the Google Play
    API.
    
    A specialized Handler is created so it is possible to send empty
    messages to it with specific Event numbers, instead of having to create
    an event object instance every time something happens. This event
    handler also removes the requirement for al lot of lambda statements.
    
    As part of this work, this CL also removes the triggerInstall() method,
    since that method only invoked downloadCompletes().
    
    BUG=922714
    
    Binary-Size: Using the minimum required APIs for inline update.
    Change-Id: I9cee165b9ce02aeaa9f4e145326642089190ef95
    Reviewed-on: https://chromium-review.googlesource.com/c/1475064
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#632894}
    Former-commit-id: 8c7d34bda7f4793acc8c0db9d4ca1ad1fb785c21

--- chrome/android/java/src/org/chromium/chrome/browser/omaha/inline/FakeAppUpdateManagerWrapper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omaha/inline/FakeAppUpdateManagerWrapper.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.omaha.inline;
 import android.app.Activity;
 import android.os.Handler;
 import android.os.Looper;
+import android.os.Message;
 import android.support.annotation.IntDef;
 
 import com.google.android.play.core.appupdate.AppUpdateInfo;
@@ -16,9 +17,11 @@ import com.google.android.play.core.tasks.Task;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.chrome.browser.ChromeActivity;
+import org.chromium.ui.widget.Toast;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.ref.WeakReference;
 
 /**
  * A wrapper of FakeAppUpdateManager meant to help automatically trigger more update scenarios.  The
@@ -28,6 +31,7 @@ import java.lang.annotation.RetentionPolicy;
 public class FakeAppUpdateManagerWrapper extends FakeAppUpdateManager {
     private static final int RESULT_IN_APP_UPDATE_FAILED = 1;
     private static final int STEP_DELAY_MS = 5000;
+    private static final int TOAST_DURATION_MS = 2000;
 
     /** A list of inline update end states that this class can simulate. */
     @IntDef({Type.NO_SIMULATION, Type.NONE, Type.SUCCESS, Type.FAIL_DIALOG_CANCEL,
@@ -60,8 +64,94 @@ public class FakeAppUpdateManagerWrapper extends FakeAppUpdateManager {
         int FAIL_INSTALL = 7;
     }
 
+    @IntDef({Event.UPDATE_AVAILABLE, Event.USER_ACCEPTS_UPDATE, Event.USER_REJECTS_UPDATE,
+            Event.TRIGGER_DOWNLOAD, Event.DOWNLOAD_STARTS, Event.DOWNLOAD_FAILS,
+            Event.USER_CANCELS_DOWNLOAD, Event.DOWNLOAD_COMPLETES, Event.INSTALL_FAILS,
+            Event.INSTALL_COMPLETES})
+    @Retention(RetentionPolicy.SOURCE)
+    @interface Event {
+        int UPDATE_AVAILABLE = 1;
+        int USER_ACCEPTS_UPDATE = 2;
+        int USER_REJECTS_UPDATE = 3;
+        int TRIGGER_DOWNLOAD = 4;
+        int DOWNLOAD_STARTS = 5;
+        int DOWNLOAD_FAILS = 6;
+        int USER_CANCELS_DOWNLOAD = 7;
+        int DOWNLOAD_COMPLETES = 8;
+        int INSTALL_FAILS = 9;
+        int INSTALL_COMPLETES = 10;
+    }
+
+    /**
+     * A helper class to wrap invocations to the Google Play API.
+     */
+    private static class EventHandler extends Handler {
+        // Need to use a weak reference to ensure the handler does not leak the outer object.
+        private final WeakReference<FakeAppUpdateManagerWrapper> mWeakWrapper;
+
+        EventHandler(FakeAppUpdateManagerWrapper wrapper) {
+            super(Looper.getMainLooper());
+            mWeakWrapper = new WeakReference<>(wrapper);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            execute(msg.what);
+        }
+
+        public void execute(@Event int event) {
+            FakeAppUpdateManagerWrapper w = mWeakWrapper.get();
+            if (w == null) return;
+            switch (event) {
+                case Event.UPDATE_AVAILABLE:
+                    w.toast("Making app update available.");
+                    w.setUpdateAvailable(10000 /* Figure out a better version? */);
+                    return;
+                case Event.USER_ACCEPTS_UPDATE:
+                    w.toast("User accepts update.");
+                    w.userAcceptsUpdate();
+                    return;
+                case Event.USER_REJECTS_UPDATE:
+                    w.toast("User rejects update.");
+                    w.userRejectsUpdate();
+                    return;
+                case Event.TRIGGER_DOWNLOAD:
+                    w.toast("Triggering download.");
+                    w.triggerDownload();
+                    return;
+                case Event.DOWNLOAD_STARTS:
+                    w.toast("Download has started.");
+                    w.downloadStarts();
+                    return;
+                case Event.DOWNLOAD_FAILS:
+                    w.toast("Triggering download failure.");
+                    w.downloadFails();
+                    return;
+                case Event.USER_CANCELS_DOWNLOAD:
+                    w.toast("Triggering cancellation of download.");
+                    w.userCancelsDownload();
+                    return;
+                case Event.DOWNLOAD_COMPLETES:
+                    w.toast("Download completes.");
+                    w.downloadCompletes();
+                    return;
+                case Event.INSTALL_FAILS:
+                    w.toast("Triggering install failure.");
+                    w.installFails();
+                    return;
+                case Event.INSTALL_COMPLETES:
+                    w.toast("Triggering install completion.");
+                    w.installCompletes();
+                    return;
+                default:
+                    w.toast("Unknown event.");
+            }
+        }
+    }
+
     private final Handler mHandler = new Handler(Looper.getMainLooper());
     private final @Type int mType;
+    private final EventHandler mEventHandler;
 
     /**
      * @param endState at which point should the inline update flow end.
@@ -69,14 +159,16 @@ public class FakeAppUpdateManagerWrapper extends FakeAppUpdateManager {
     FakeAppUpdateManagerWrapper(@Type int endState) {
         super(ContextUtils.getApplicationContext());
         mType = endState;
+        mEventHandler = new EventHandler(this);
 
-        if (mType != Type.NONE) setUpdateAvailable(10000 /* Figure out a better version? */);
+        if (mType != Type.NONE) execute(Event.UPDATE_AVAILABLE);
     }
 
     // FakeAppUpdateManager implementation.
     @Override
     public boolean startUpdateFlowForResult(AppUpdateInfo appUpdateInfo,
             @AppUpdateType int appUpdateType, Activity activity, int requestCode) {
+        toast("Starting update flow.");
         // TODO(dtrainor): Simulate exceptions being thrown or returning false from the super call.
         boolean success =
                 super.startUpdateFlowForResult(appUpdateInfo, appUpdateType, activity, requestCode);
@@ -102,12 +194,13 @@ public class FakeAppUpdateManagerWrapper extends FakeAppUpdateManager {
 
     @Override
     public Task<Void> completeUpdate() {
+        toast("Completing update.");
         Task<Void> result = super.completeUpdate();
 
         if (mType == Type.FAIL_INSTALL) {
-            mHandler.postDelayed(this::installFails, STEP_DELAY_MS);
+            postDelayedEvent(Event.INSTALL_FAILS);
         } else {
-            mHandler.postDelayed(this::installCompletes, STEP_DELAY_MS);
+            postDelayedEvent(Event.INSTALL_COMPLETES);
             // This doesn't actually restart Chrome in this case.
         }
 
@@ -116,30 +209,40 @@ public class FakeAppUpdateManagerWrapper extends FakeAppUpdateManager {
 
     private void triggerDialogResponse(ChromeActivity activity, int requestCode, int resultCode) {
         if (resultCode == Activity.RESULT_OK) {
-            userAcceptsUpdate();
+            execute(Event.USER_ACCEPTS_UPDATE);
         } else if (resultCode == Activity.RESULT_CANCELED) {
-            userRejectsUpdate();
+            execute(Event.USER_REJECTS_UPDATE);
         }
 
         activity.onActivityResult(requestCode, resultCode, null);
         if (resultCode == Activity.RESULT_OK) {
-            mHandler.postDelayed(this::triggerDownload, STEP_DELAY_MS);
+            postDelayedEvent(Event.TRIGGER_DOWNLOAD);
         }
     }
 
     private void triggerDownload() {
-        downloadStarts();
+        execute(Event.DOWNLOAD_STARTS);
 
         if (mType == Type.FAIL_DOWNLOAD) {
-            mHandler.postDelayed(this::downloadFails, STEP_DELAY_MS);
+            postDelayedEvent(Event.DOWNLOAD_FAILS);
         } else if (mType == Type.FAIL_DOWNLOAD_CANCEL) {
-            mHandler.postDelayed(this::userCancelsDownload, STEP_DELAY_MS);
+            postDelayedEvent(Event.USER_CANCELS_DOWNLOAD);
         } else {
-            mHandler.postDelayed(this::triggerInstall, STEP_DELAY_MS);
+            postDelayedEvent(Event.DOWNLOAD_COMPLETES);
         }
     }
 
-    private void triggerInstall() {
-        downloadCompletes();
+    private void execute(@Event int event) {
+        mEventHandler.execute(event);
+    }
+
+    private void postDelayedEvent(@Event int event) {
+        mEventHandler.sendEmptyMessageDelayed(event, STEP_DELAY_MS);
+    }
+
+    private void toast(CharSequence text) {
+        Toast.makeText(ContextUtils.getApplicationContext(), "Play Store Flow: " + text,
+                     TOAST_DURATION_MS)
+                .show();
     }
 }
