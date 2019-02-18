fd13bac9fc7ecc18708d0600249e6c85fe02a206
kdillon <kdillon@chromium.org>
2019-02-18 01:57:47 +0000

    Reland "Updating test from legacy harness"
    This reverts commit I0ccda4a23d6ac54c34fe60613ce6559d6681434d.
    
    Original change's description:
    > Updating test from legacy harness
    >
    > Updating some tests in fast/workers from the legacy test harness to
    > testharness.js in preparation to be upstreamed to external/wpt/workers.
    
    > Bug: 795636
    > Change-Id: I0ccda4a23d6ac54c34fe60613ce6559d6681434d
    > Reviewed-on: https://chromium-review.googlesource.com/c/1451424
    > Commit-Queue: Katie Dillon <kdillon@chromium.org>
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#631812}
    
    Bug: 795636
    Change-Id: I3cb1ec73ef7811d104b4d916ba59e7d55bb94087
    Reviewed-on: https://chromium-review.googlesource.com/c/1475004
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Katie Dillon <kdillon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633005}
    Former-commit-id: c85737bf3272a598264dc9e225842caffe638208

--- third_party/blink/web_tests/fast/workers/resources/worker-timeout-cancel-order.js
+++ third_party/blink/web_tests/fast/workers/resources/worker-timeout-cancel-order.js
@@ -1,8 +1,7 @@
-self.postMessage("Test started.");
-// The test will create 3 timeouts with their intervals decreasing.
-// If the timeouts execute in order then the test is PASS.
+// The test will create 2 timeouts and cancel the first one. If only the second
+// timeout executes then the test passes.
 self.addEventListener('message', function(e) {
     var t1 = setTimeout(function () { postMessage(1); }, 5);
-    setTimeout(function () { postMessage(2); postMessage("DONE"); }, 10);
+    setTimeout(function () { postMessage(2); }, 10);
     clearTimeout(t1);
 }, false);
