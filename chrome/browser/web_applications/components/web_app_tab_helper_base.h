9da920d94e6de025aef313ea62a0c0cdf1aee0d4
Alan Cutter <alancutter@chromium.org>
2019-02-18 01:47:34 +0000

    Fix comment for WebAppTabHelperBase::GetAppId()
    Not sure what the original comment meant.
    
    Bug: 907353
    Change-Id: I50d1465e63914607300caa38e81ce7b1ed4b67a9
    Reviewed-on: https://chromium-review.googlesource.com/c/1475558
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633001}
    Former-commit-id: 3b51fd3c395e7dc49b3d944a16147222d5ded94e

--- chrome/browser/web_applications/components/web_app_tab_helper_base.h
+++ chrome/browser/web_applications/components/web_app_tab_helper_base.h
@@ -69,8 +69,7 @@ class WebAppTabHelperBase
   virtual WebAppTabHelperBase* CloneForWebContents(
       content::WebContents* web_contents) const = 0;
 
-  // Gets AppId from derived platform-specific TabHelper and updates
-  // app_id_ with it.
+  // Gets AppId of app whose scope includes |url|.
   virtual AppId GetAppId(const GURL& url) = 0;
 
   // Returns whether the associated web contents belongs to an app window.
