e99a91cf0f372b89ed626004b25f40252ca81be0
Ulan Degenbaev <ulan@chromium.org>
2019-02-18 14:58:25 +0000

    gin: Add a feature flag for activating V8 memory reducer on small heaps
    Bug: chromium:933107
    Change-Id: I3aadaca807b291a3cf4a733cbd251f23d05528b0
    Reviewed-on: https://chromium-review.googlesource.com/c/1474943
    Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    Commit-Queue: Ulan Degenbaev <ulan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633095}
    Former-commit-id: 405b65e50ae60dc48602d69850d71fe31d9197e0

--- gin/v8_initializer.cc
+++ gin/v8_initializer.cc
@@ -225,6 +225,13 @@ void V8Initializer::Initialize(IsolateHolder::ScriptMode mode) {
                                sizeof(no_flush_bytecode) - 1);
   }
 
+  if (!base::FeatureList::IsEnabled(features::kV8MemoryReducerForSmallHeaps)) {
+    static const char no_memory_reducer[] =
+        "--no-memory-reducer-for-small-heaps";
+    v8::V8::SetFlagsFromString(no_memory_reducer,
+                               sizeof(no_memory_reducer) - 1);
+  }
+
   if (IsolateHolder::kStrictMode == mode) {
     static const char use_strict[] = "--use_strict";
     v8::V8::SetFlagsFromString(use_strict, sizeof(use_strict) - 1);
