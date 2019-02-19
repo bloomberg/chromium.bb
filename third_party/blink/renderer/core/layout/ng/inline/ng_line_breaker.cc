def4283e43c0dcd7ff454cd95d0caa75d4630153
Koji Ishii <kojii@chromium.org>
2019-02-19 08:42:28 +0000

    [LayoutNG] Avoid creating ShapeResult when collapsing trailing spaces
    This patch changes |TruncateLineEndResult| to create truncated
    |ShapeResultView| from the original |ShapeResult| in
    |NGInlineItem|, not from |ShapeResultView|. Doing so avoids
    creating intermediate |ShapeResult|, which saves CPU time and
    memory consumed by |RunInfo|.
    
    r632350 (CL:1469846) avoided reshaping when possible, but
    |TruncateLineEndResult| needs to create new results even when
    reshaping is not needed. This patch should improve both cases,
    when reshaping is needed and not needed.
    
    blink_perf.layout:
    https://pinpoint-dot-chromeperf.appspot.com/job/139c2aaa340000
    
    Bug: 636993, 931668
    Change-Id: Ia60b6c25d52d9999813ef9b37d2f4f38c61ad26a
    Reviewed-on: https://chromium-review.googlesource.com/c/1475327
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633215}
    Former-commit-id: a99e831a1256dfe40ee8634181d14667af78d36f

--- third_party/blink/renderer/core/layout/ng/inline/ng_line_breaker.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_line_breaker.cc
@@ -730,37 +730,33 @@ scoped_refptr<ShapeResultView> NGLineBreaker::TruncateLineEndResult(
     unsigned end_offset) {
   DCHECK(item_result.item);
   const NGInlineItem& item = *item_result.item;
-  DCHECK(item_result.shape_result);
-  // TODO(layout-dev): Add support for subsetting a ShapeResultView thereby
-  // avoiding extra copy to create a new full ShapeResult here.
-  scoped_refptr<ShapeResult> source_result =
-      item_result.shape_result->CreateShapeResult();
+  const ShapeResult* source_result = item.TextShapeResult();
+  DCHECK(source_result);
+
+  // Check given offsets require to truncate |item_result.shape_result|.
   const unsigned start_offset = item_result.start_offset;
-  DCHECK_GE(start_offset, source_result->StartIndex());
-  DCHECK_LE(end_offset, source_result->EndIndex());
-  DCHECK(start_offset > source_result->StartIndex() ||
-         end_offset < source_result->EndIndex());
+  DCHECK(item_result.shape_result);
+  DCHECK_GE(start_offset, item_result.shape_result->StartIndex());
+  DCHECK_LE(end_offset, item_result.shape_result->EndIndex());
+  DCHECK(start_offset > item_result.shape_result->StartIndex() ||
+         end_offset < item_result.shape_result->EndIndex());
 
   if (!NeedsAccurateEndPosition(*line_info_, item)) {
-    return ShapeResultView::Create(source_result.get(), start_offset,
-                                   end_offset);
+    return ShapeResultView::Create(source_result, start_offset, end_offset);
   }
 
   unsigned last_safe = source_result->PreviousSafeToBreakOffset(end_offset);
   DCHECK_LE(last_safe, end_offset);
   if (last_safe == end_offset || last_safe <= start_offset) {
-    return ShapeResultView::Create(source_result.get(), start_offset,
-                                   end_offset);
+    return ShapeResultView::Create(source_result, start_offset, end_offset);
   }
-
-  ShapeResultView::Segment segments[2];
-  segments[0] = {source_result.get(), start_offset, last_safe};
 
   scoped_refptr<ShapeResult> end_result =
       ShapeText(item, std::max(last_safe, start_offset), end_offset);
   DCHECK_EQ(end_result->Direction(), source_result->Direction());
+  ShapeResultView::Segment segments[2];
+  segments[0] = {source_result, start_offset, last_safe};
   segments[1] = {end_result.get(), 0, end_offset};
-
   return ShapeResultView::Create(&segments[0], 2);
 }
 
