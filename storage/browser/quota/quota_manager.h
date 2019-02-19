43d43871d7be7eabf089f55847748ee753ead1fb
Jarryd Goodman <jarrydg@chromium.org>
2019-02-19 23:35:44 +0000

    Revert "Quota: Add SequenceChecker to QuotaManager"
    This reverts commit 45238ea7cf75da91b6eb388f5b7c9e6e8563f9d3 [formerly d82ad3d0eee4f2b98d6f7be7fb2489c55713da69].
    
    Reason for revert: This change is suspected of causing the #1 browser process crash on  Windows canary 74.0.3709.0.
    
    Original change's description:
    > Quota: Add SequenceChecker to QuotaManager
    >
    > Change-Id: I7caa60bf82fdcd63469d58afa0e29f893795ae32
    > Reviewed-on: https://chromium-review.googlesource.com/c/1457000
    > Commit-Queue: Victor Costan <pwnall@chromium.org>
    > Reviewed-by: Victor Costan <pwnall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#631891}
    
    TBR=pwnall@chromium.org,jarrydg@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: Ie6833ffd46083e84fb0bcfa2d38c80050ffc2e8e
    Bug: 933014
    Reviewed-on: https://chromium-review.googlesource.com/c/1477764
    Commit-Queue: Jarryd Goodman <jarrydg@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633450}
    Former-commit-id: a4756050713637eaf608e76eb969a5c86e1a27a5

--- storage/browser/quota/quota_manager.h
+++ storage/browser/quota/quota_manager.h
@@ -21,10 +21,8 @@
 #include "base/files/file_path.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "base/memory/ref_counted_delete_on_sequence.h"
 #include "base/memory/weak_ptr.h"
 #include "base/optional.h"
-#include "base/sequence_checker.h"
 #include "base/stl_util.h"
 #include "base/timer/timer.h"
 #include "storage/browser/quota/quota_callbacks.h"
@@ -109,7 +107,7 @@ struct UsageInfo {
 class COMPONENT_EXPORT(STORAGE_BROWSER) QuotaManager
     : public QuotaTaskObserver,
       public QuotaEvictionHandler,
-      public base::RefCountedDeleteOnSequence<QuotaManager> {
+      public base::RefCountedThreadSafe<QuotaManager, QuotaManagerDeleter> {
  public:
   using UsageAndQuotaCallback = base::OnceCallback<
       void(blink::mojom::QuotaStatusCode, int64_t usage, int64_t quota)>;
@@ -180,7 +178,6 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) QuotaManager
   void NotifyOriginInUse(const url::Origin& origin);
   void NotifyOriginNoLongerInUse(const url::Origin& origin);
   bool IsOriginInUse(const url::Origin& origin) const {
-    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
     return base::ContainsKey(origins_in_use_, origin);
   }
 
@@ -266,7 +263,7 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) QuotaManager
 
  private:
   friend class base::DeleteHelper<QuotaManager>;
-  friend class base::RefCountedDeleteOnSequence<QuotaManager>;
+  friend class base::RefCountedThreadSafe<QuotaManager, QuotaManagerDeleter>;
   friend class content::QuotaManagerTest;
   friend class content::StorageMonitorTest;
   friend class content::MockQuotaManager;
@@ -426,8 +423,7 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) QuotaManager
   const bool is_incognito_;
   const base::FilePath profile_path_;
 
-  // proxy_ can be accessed by any thread so it must be thread-safe
-  const scoped_refptr<QuotaManagerProxy> proxy_;
+  scoped_refptr<QuotaManagerProxy> proxy_;
   bool db_disabled_;
   bool eviction_disabled_;
   scoped_refptr<base::SingleThreadTaskRunner> io_thread_;
@@ -481,11 +477,15 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) QuotaManager
 
   std::unique_ptr<StorageMonitor> storage_monitor_;
 
-  SEQUENCE_CHECKER(sequence_checker_);
-
   base::WeakPtrFactory<QuotaManager> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(QuotaManager);
+};
+
+struct QuotaManagerDeleter {
+  static void Destruct(const QuotaManager* manager) {
+    manager->DeleteOnCorrectThread();
+  }
 };
 
 }  // namespace storage
