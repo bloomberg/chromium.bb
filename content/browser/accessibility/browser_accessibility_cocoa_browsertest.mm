88019e3177a3acc198cba173a3e831fc834f238b
Dominic Mazzoni <dmazzoni@chromium.org>
2019-02-19 05:59:57 +0000

    Expose ImageAnnotationStatus via Mac accessibility APIs
    This is the Mac counterpart to http://crrev.com/c/1471353, which
    implemented the same functionality on Windows.
    
    When an image is annotated with a description of an image, the
    ImageAnnotationStatus enum field of AXNodeData keeps track of
    the current state, like available for annotation, waiting on
    an annotation, or successfully annotated. We want to expose
    this to VoiceOver users.
    
    For the NSAccessibility protocol on Mac, accessible text can
    be exposed in either AXTitle or AXDescription. Since image
    descriptions are not visible text, we want to expose them in
    AXDescription. They're appended to any existing description so
    nothing is ever overwritten.
    
    Bug: 905419
    Change-Id: I801ca80b3a1fd7c4744cd69a0681a705fc9e1a69
    Reviewed-on: https://chromium-review.googlesource.com/c/1476942
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633198}
    Former-commit-id: 617885959d6ffd38e106434b4d2954165a7ec793

--- content/browser/accessibility/browser_accessibility_cocoa_browsertest.mm
+++ content/browser/accessibility/browser_accessibility_cocoa_browsertest.mm
@@ -3,10 +3,12 @@
 // found in the LICENSE file.
 
 #include "base/logging.h"
+#include "base/strings/sys_string_conversions.h"
 #include "content/browser/accessibility/browser_accessibility.h"
 #include "content/browser/accessibility/browser_accessibility_cocoa.h"
 #include "content/browser/accessibility/browser_accessibility_mac.h"
 #include "content/browser/accessibility/browser_accessibility_manager.h"
+#include "content/browser/accessibility/browser_accessibility_manager_mac.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/content_browser_test.h"
@@ -120,6 +122,153 @@ IN_PROC_BROWSER_TEST_F(BrowserAccessibilityCocoaBrowserTest,
                          rangeValue]
                          .location);
     }
+  }
+}
+
+IN_PROC_BROWSER_TEST_F(BrowserAccessibilityCocoaBrowserTest,
+                       TestUnlabeledImageRoleDescription) {
+  ui::AXNodeData image;
+  image.id = 1;
+  image.role = ax::mojom::Role::kImage;
+  image.AddStringAttribute(ax::mojom::StringAttribute::kRoleDescription, "bar");
+  image.SetImageAnnotationStatus(
+      ax::mojom::ImageAnnotationStatus::kEligibleForAnnotation);
+
+  std::unique_ptr<BrowserAccessibilityManagerMac> manager(
+      new BrowserAccessibilityManagerMac(MakeAXTreeUpdate(image), nullptr));
+
+  base::scoped_nsobject<BrowserAccessibilityCocoa> ax_node(
+      [ToBrowserAccessibilityCocoa(manager->GetRoot()) retain]);
+
+  EXPECT_NSEQ(@"Unlabeled image", [ax_node roleDescription]);
+}
+
+IN_PROC_BROWSER_TEST_F(BrowserAccessibilityCocoaBrowserTest,
+                       TestAnnotatedImageDescription) {
+  std::vector<const char*> expected_descriptions;
+
+  ui::AXTreeUpdate tree;
+  tree.root_id = 1;
+  tree.nodes.resize(10);
+  tree.nodes[0].id = 1;
+  tree.nodes[0].child_ids = {2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+  // If the status is EligibleForAnnotation and there's no existing label,
+  // the description should be the discoverability string.
+  tree.nodes[1].id = 2;
+  tree.nodes[1].role = ax::mojom::Role::kImage;
+  tree.nodes[1].AddStringAttribute(ax::mojom::StringAttribute::kImageAnnotation,
+                                   "Annotation");
+  tree.nodes[1].SetImageAnnotationStatus(
+      ax::mojom::ImageAnnotationStatus::kEligibleForAnnotation);
+  expected_descriptions.push_back(
+      "To get missing image descriptions, open the context menu.");
+
+  // If the status is EligibleForAnnotation, the discoverability string
+  // should be appended to the existing name.
+  tree.nodes[2].id = 3;
+  tree.nodes[2].role = ax::mojom::Role::kImage;
+  tree.nodes[2].AddStringAttribute(ax::mojom::StringAttribute::kImageAnnotation,
+                                   "Annotation");
+  tree.nodes[2].SetName("ExistingLabel");
+  tree.nodes[2].SetImageAnnotationStatus(
+      ax::mojom::ImageAnnotationStatus::kEligibleForAnnotation);
+  expected_descriptions.push_back(
+      "ExistingLabel. To get missing image descriptions, open the context "
+      "menu.");
+
+  // If the status is IneligibleForAnnotation, nothing should be appended.
+  tree.nodes[3].id = 4;
+  tree.nodes[3].role = ax::mojom::Role::kImage;
+  tree.nodes[3].AddStringAttribute(ax::mojom::StringAttribute::kImageAnnotation,
+                                   "Annotation");
+  tree.nodes[3].SetName("ExistingLabel");
+  tree.nodes[3].SetImageAnnotationStatus(
+      ax::mojom::ImageAnnotationStatus::kIneligibleForAnnotation);
+  expected_descriptions.push_back("ExistingLabel");
+
+  // If the status is AnnotationPending, pending text should be appended
+  // to the name.
+  tree.nodes[4].id = 5;
+  tree.nodes[4].role = ax::mojom::Role::kImage;
+  tree.nodes[4].AddStringAttribute(ax::mojom::StringAttribute::kImageAnnotation,
+                                   "Annotation");
+  tree.nodes[4].SetName("ExistingLabel");
+  tree.nodes[4].SetImageAnnotationStatus(
+      ax::mojom::ImageAnnotationStatus::kAnnotationPending);
+  expected_descriptions.push_back("ExistingLabel. Getting description…");
+
+  // If the status is AnnotationSucceeded, and there's no annotation,
+  // nothing should be appended. (Ideally this shouldn't happen.)
+  tree.nodes[5].id = 6;
+  tree.nodes[5].role = ax::mojom::Role::kImage;
+  tree.nodes[5].SetName("ExistingLabel");
+  tree.nodes[5].SetImageAnnotationStatus(
+      ax::mojom::ImageAnnotationStatus::kAnnotationSucceeded);
+  expected_descriptions.push_back("ExistingLabel");
+
+  // If the status is AnnotationSucceeded, the annotation should be appended
+  // to the existing label.
+  tree.nodes[6].id = 7;
+  tree.nodes[6].role = ax::mojom::Role::kImage;
+  tree.nodes[6].AddStringAttribute(ax::mojom::StringAttribute::kImageAnnotation,
+                                   "Annotation");
+  tree.nodes[6].SetName("ExistingLabel");
+  tree.nodes[6].SetImageAnnotationStatus(
+      ax::mojom::ImageAnnotationStatus::kAnnotationSucceeded);
+  expected_descriptions.push_back("ExistingLabel. Annotation");
+
+  // If the status is AnnotationEmpty, failure text should be appended
+  // to the name.
+  tree.nodes[7].id = 8;
+  tree.nodes[7].role = ax::mojom::Role::kImage;
+  tree.nodes[7].AddStringAttribute(ax::mojom::StringAttribute::kImageAnnotation,
+                                   "Annotation");
+  tree.nodes[7].SetName("ExistingLabel");
+  tree.nodes[7].SetImageAnnotationStatus(
+      ax::mojom::ImageAnnotationStatus::kAnnotationEmpty);
+  expected_descriptions.push_back(
+      "ExistingLabel. No description is available.");
+
+  // If the status is AnnotationAdult, appropriate text should be appended
+  // to the name.
+  tree.nodes[8].id = 9;
+  tree.nodes[8].role = ax::mojom::Role::kImage;
+  tree.nodes[8].AddStringAttribute(ax::mojom::StringAttribute::kImageAnnotation,
+                                   "Annotation");
+  tree.nodes[8].SetName("ExistingLabel");
+  tree.nodes[8].SetImageAnnotationStatus(
+      ax::mojom::ImageAnnotationStatus::kAnnotationAdult);
+  expected_descriptions.push_back(
+      "ExistingLabel. Appears to be adult content.");
+
+  // If the status is AnnotationProcessFailed, appropriate text should be
+  // appended to the name.
+  tree.nodes[9].id = 10;
+  tree.nodes[9].role = ax::mojom::Role::kImage;
+  tree.nodes[9].AddStringAttribute(ax::mojom::StringAttribute::kImageAnnotation,
+                                   "Annotation");
+  tree.nodes[9].SetName("ExistingLabel");
+  tree.nodes[9].SetImageAnnotationStatus(
+      ax::mojom::ImageAnnotationStatus::kAnnotationProcessFailed);
+  expected_descriptions.push_back(
+      "ExistingLabel. Unable to get a description.");
+
+  // We should have one expected description per child of the root.
+  ASSERT_EQ(expected_descriptions.size(), tree.nodes[0].child_ids.size());
+  int child_count = static_cast<int>(expected_descriptions.size());
+
+  std::unique_ptr<BrowserAccessibilityManagerMac> manager(
+      new BrowserAccessibilityManagerMac(tree, nullptr));
+
+  for (int child_index = 0; child_index < child_count; child_index++) {
+    BrowserAccessibility* child =
+        manager->GetRoot()->PlatformGetChild(child_index);
+    base::scoped_nsobject<BrowserAccessibilityCocoa> child_obj(
+        [ToBrowserAccessibilityCocoa(child) retain]);
+
+    EXPECT_NSEQ(base::SysUTF8ToNSString(expected_descriptions[child_index]),
+                [child_obj description]);
   }
 }
 
