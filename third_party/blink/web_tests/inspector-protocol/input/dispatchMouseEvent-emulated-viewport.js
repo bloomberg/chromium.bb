2243dbbaf53c6fdbb0b48cece92b5a64bdf94d51
Joel Einbinder <einbinder@chromium.org>
2019-02-20 01:58:57 +0000

    DevTools: Reset scaling to 1 on a cross process navigation
    If a renderer is swapped out, it won't send a ScaleChanged message
    unless it has a non-1 scale. This patch resets the scale of
    input_handler to 1 on a cross process navigation.
    
    Bug: 929806
    Change-Id: I3d4a2a1fbf07286870a2c375b330d86caa9e38ea
    Reviewed-on: https://chromium-review.googlesource.com/c/1464115
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Joel Einbinder <einbinder@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633513}
    Former-commit-id: b511b2a3a854f1320a09db74ac7b97cb0fea7aaa

--- third_party/blink/web_tests/inspector-protocol/input/dispatchMouseEvent-emulated-viewport.js
+++ third_party/blink/web_tests/inspector-protocol/input/dispatchMouseEvent-emulated-viewport.js
@@ -1,38 +1,58 @@
 (async function(testRunner) {
   var {page, session, dp} = await testRunner.startBlank(`Tests Input.dispatchMouseEvent method after emulating a viewport.`);
 
-  await session.evaluate(`
-    window.result = 'Not Clicked';
-    window.button = document.createElement('button');
-    document.body.appendChild(button);
-    button.style.position = 'absolute';
-    button.style.top = '50px';
-    button.style.left = '50px';
-    button.style.width = '10px';
-    button.style.height = '10px';
-    button.onmousedown = () => window.result = 'Clicked';
-  `);
 
   function dumpError(message) {
     if (message.error)
       testRunner.log('Error: ' + message.error.message);
   }
+  async function testClick() {
+    await session.evaluate(`
+      window.result = 'Not Clicked';
+      window.button = document.createElement('button');
+      document.body.appendChild(button);
+      button.style.position = 'absolute';
+      button.style.top = '50px';
+      button.style.left = '50px';
+      button.style.width = '10px';
+      button.style.height = '10px';
+      button.onmousedown = () => window.result = 'Clicked';
+    `);
+    dumpError(await dp.Input.dispatchMouseEvent({
+      type: 'mousePressed',
+      button: 'left',
+      clickCount: 1,
+      x: 55,
+      y: 55
+    }));
 
+    testRunner.log(await session.evaluate(`window.result`));
+  }
+
+  testRunner.log('Emulate mobile viewport and click');
   dumpError(await dp.Emulation.setDeviceMetricsOverride({
     deviceScaleFactor: 5,
     width: 400,
     height: 300,
     mobile: true
   }));
+  await session.navigate('../resources/blank.html')
+  await testClick();
 
-  dumpError(await dp.Input.dispatchMouseEvent({
-    type: 'mousePressed',
-    button: 'left',
-    clickCount: 1,
-    x: 55,
-    y: 55
-  }));
+  testRunner.log('\nClick with viewport tag');
+  await session.navigate('data:text/html,<head><meta name="viewport" content="width=device-width, initial-scale=1"></head>');
+  await testClick();
 
-  testRunner.log(await session.evaluate(`window.result`));
+  testRunner.log('\nClick after adding a blank frame');
+  await session.navigate('../resources/blank.html');
+  await session.evaluateAsync(`new Promise(done => {
+    const frame = document.createElement('iframe');
+    frame.onload = done;
+    frame.src = 'about:blank';
+    document.body.appendChild(frame);
+  })`);
+  await testClick();
+
+
   testRunner.completeTest();
 })
