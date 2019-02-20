2aae7ee2c2939f85968676a71774a55fed3a113d
Tomasz Wiszkowski <ender@google.com>
2019-02-20 23:15:51 +0000

    Add support for events and bitmap icons for weather. Fix growing answer image view.
    This change
    - binds events to AnswerView, making it possible to select and refine queries
    - introduces weather bitmaps (views no longer expanding beyond limits)
    
    Bug: 920396
    Change-Id: Id9066f805c625094950dacac509d97be061e0274
    Reviewed-on: https://chromium-review.googlesource.com/c/1478355
    Commit-Queue: Ender <ender@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633936}
    Former-commit-id: b1a44a7bc311b7bd9230aeb992b8371f7753d332

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/answer/AnswerSuggestionView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/answer/AnswerSuggestionView.java
@@ -6,13 +6,19 @@ package org.chromium.chrome.browser.omnibox.suggestions.answer;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.DrawableRes;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.text.Spannable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.omnibox.suggestions.basic.SuggestionViewDelegate;
 
@@ -21,9 +27,11 @@ import org.chromium.chrome.browser.omnibox.suggestions.basic.SuggestionViewDeleg
  */
 public class AnswerSuggestionView extends RelativeLayout {
     private SuggestionViewDelegate mSuggestionDelegate;
+    private View mAnswerView;
     private TextView mTextView1;
     private TextView mTextView2;
-    private ImageView mAnswerIcon;
+    private ImageView mAnswerIconView;
+    private ImageView mRefineView;
 
     /** Creates new instance of AnswerSuggestionView. */
     public AnswerSuggestionView(Context context, AttributeSet attributes) {
@@ -35,19 +43,57 @@ public class AnswerSuggestionView extends RelativeLayout {
         super.onFinishInflate();
         mTextView1 = findViewById(R.id.omnibox_answer_line_1);
         mTextView2 = findViewById(R.id.omnibox_answer_line_2);
-        mAnswerIcon = findViewById(R.id.omnibox_answer_icon);
+        mAnswerIconView = findViewById(R.id.omnibox_answer_icon);
+        mAnswerView = findViewById(R.id.omnibox_answer);
+        mRefineView = findViewById(R.id.omnibox_answer_refine_icon);
+    }
+
+    @Override
+    public void setSelected(boolean selected) {
+        super.setSelected(selected);
+        if (selected && !isInTouchMode()) {
+            postDelegateAction(() -> mSuggestionDelegate.onSetUrlToSuggestion());
+        }
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        // Whenever the suggestion dropdown is touched, we dispatch onGestureDown which is
+        // used to let autocomplete controller know that it should stop updating suggestions.
+        switch (ev.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                mSuggestionDelegate.onGestureDown();
+                break;
+            case MotionEvent.ACTION_UP:
+                mSuggestionDelegate.onGestureUp(ev.getEventTime());
+                break;
+        }
+        return super.dispatchTouchEvent(ev);
     }
 
     /** Specify delegate receiving click/refine events. */
     void setDelegate(SuggestionViewDelegate delegate) {
         mSuggestionDelegate = delegate;
+        mAnswerView.setOnClickListener(
+                (View v) -> postDelegateAction(() -> mSuggestionDelegate.onSelection()));
+        mAnswerView.setOnLongClickListener((View v) -> {
+            postDelegateAction(() -> mSuggestionDelegate.onLongPress());
+            return true;
+        });
+        mRefineView.setOnClickListener(
+                (View v) -> postDelegateAction(() -> mSuggestionDelegate.onRefineSuggestion()));
     }
 
     /**
      * Toggles theme.
      * @param useDarkColors specifies whether UI should use dark theme.
      */
-    void setUseDarkColors(boolean useDarkColors) {}
+    void setUseDarkColors(boolean useDarkColors) {
+        Drawable drawable = mRefineView.getDrawable();
+        DrawableCompat.setTint(drawable,
+                ApiCompatibilityUtils.getColor(getContext().getResources(),
+                        useDarkColors ? R.color.dark_mode_tint : R.color.light_mode_tint));
+    }
 
     /**
      * Specifies text content of the first text line.
@@ -69,13 +115,25 @@ public class AnswerSuggestionView extends RelativeLayout {
      * Specifies image bitmap to be displayed as an answer icon.
      * @param bitmap Decoded image to be displayed as an answer icon.
      */
-    void setIconBitmap(Bitmap bitmap) {}
+    void setIconBitmap(Bitmap bitmap) {
+        BitmapDrawable drawable = new BitmapDrawable(bitmap);
+        mAnswerIconView.setImageDrawable(drawable);
+    }
 
     /**
      * Specifies fallback image to be presented if no image is available.
      * @param res Drawable resource ID to be used in place of answer image.
      */
     void setFallbackIconRes(@DrawableRes int res) {
-        mAnswerIcon.setImageResource(res);
+        mAnswerIconView.setImageResource(res);
+    }
+
+    /**
+     * Post delegate action to main thread. Invoked only if delegate is specified.
+     * @param action Delegate action to invoke on the UI thread.
+     */
+    private void postDelegateAction(Runnable action) {
+        if (mSuggestionDelegate == null) return;
+        if (!post(action)) action.run();
     }
 }
