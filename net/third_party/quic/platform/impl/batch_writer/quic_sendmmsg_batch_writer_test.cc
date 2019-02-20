a71676759c37cf13567bc163fbff82ff92053f1c
Bin Wu <wub@chromium.org>
2019-02-20 03:51:20 +0000

    Fix the issue where quic batch writer IO tests are double registered, by moving both instantiations of QuicUdpBatchWriterIOTest into a single quic_batch_writer_test.cc file.
    R=rch@chromium.org
    
    Change-Id: I2c88f54bdd51c692b571663c7dbe46b1637c1bda
    Reviewed-on: https://chromium-review.googlesource.com/c/1475209
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Bin Wu <wub@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633545}
    Former-commit-id: 67b5ec7661d1f6d89e3afc591a8d4b1216163e49

--- net/third_party/quic/platform/impl/batch_writer/quic_sendmmsg_batch_writer_test.cc
+++ net/third_party/quic/platform/impl/batch_writer/quic_sendmmsg_batch_writer_test.cc
@@ -3,32 +3,12 @@
 // found in the LICENSE file.
 
 #include "net/third_party/quic/platform/impl/batch_writer/quic_sendmmsg_batch_writer.h"
-#include "net/third_party/quic/platform/api/quic_ptr_util.h"
-#include "net/third_party/quic/platform/impl/batch_writer/quic_batch_writer_test.h"
 
 namespace quic {
 namespace test {
 namespace {
 
-class QuicSendmmsgBatchWriterIOTestDelegate
-    : public QuicUdpBatchWriterIOTestDelegate {
- public:
-  void ResetWriter(int fd) override {
-    writer_ = QuicMakeUnique<QuicSendmmsgBatchWriter>(
-        QuicMakeUnique<QuicBatchWriterBuffer>(), fd);
-  }
-
-  QuicUdpBatchWriter* GetWriter() override { return writer_.get(); }
-
- private:
-  std::unique_ptr<QuicSendmmsgBatchWriter> writer_;
-};
-
-INSTANTIATE_TEST_SUITE_P(
-    QuicSendmmsgBatchWriterTest,
-    QuicUdpBatchWriterIOTest,
-    testing::ValuesIn(MakeQuicBatchWriterTestParams<
-                      QuicSendmmsgBatchWriterIOTestDelegate>()));
+// Add tests here.
 
 }  // namespace
 }  // namespace test
