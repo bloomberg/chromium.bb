04d84baea4d9eb627858fe1cd4482b468cf2284d
Eric Orth <ericorth@chromium.org>
2019-02-20 18:58:27 +0000

    Remove legacy resolves from MappedHostResolverTest
    Bug: 922699
    Change-Id: Icb3454e12b0dcecae9aaa1773b39c206e9dd78b4
    Reviewed-on: https://chromium-review.googlesource.com/c/1479151
    Commit-Queue: Eric Orth <ericorth@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Auto-Submit: Eric Orth <ericorth@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633811}
    Former-commit-id: 919d2e82418e6114cdf0fa04acba271074ded2e8

--- net/dns/mapped_host_resolver_unittest.cc
+++ net/dns/mapped_host_resolver_unittest.cc
@@ -32,76 +32,6 @@ std::string FirstAddress(const AddressList& address_list) {
 TEST(MappedHostResolverTest, Inclusion) {
   base::test::ScopedTaskEnvironment scoped_task_environment;
 
-  // Outstanding request.
-  std::unique_ptr<HostResolver::Request> request;
-  // Create a mock host resolver, with specific hostname to IP mappings.
-  std::unique_ptr<MockHostResolver> resolver_impl(new MockHostResolver());
-  resolver_impl->rules()->AddSimulatedFailure("*google.com");
-  resolver_impl->rules()->AddRule("baz.com", "192.168.1.5");
-  resolver_impl->rules()->AddRule("foo.com", "192.168.1.8");
-  resolver_impl->rules()->AddRule("proxy", "192.168.1.11");
-
-  // Create a remapped resolver that uses |resolver_impl|.
-  std::unique_ptr<MappedHostResolver> resolver(
-      new MappedHostResolver(std::move(resolver_impl)));
-
-  int rv;
-  AddressList address_list;
-
-  // Try resolving "www.google.com:80". There are no mappings yet, so this
-  // hits |resolver_impl| and fails.
-  TestCompletionCallback callback;
-  rv = resolver->Resolve(
-      HostResolver::RequestInfo(HostPortPair("www.google.com", 80)),
-      DEFAULT_PRIORITY, &address_list, callback.callback(), &request,
-      NetLogWithSource());
-  EXPECT_THAT(rv, IsError(ERR_IO_PENDING));
-  rv = callback.WaitForResult();
-  EXPECT_THAT(rv, IsError(ERR_NAME_NOT_RESOLVED));
-
-  // Remap *.google.com to baz.com.
-  EXPECT_TRUE(resolver->AddRuleFromString("map *.google.com baz.com"));
-  request.reset();
-
-  // Try resolving "www.google.com:80". Should be remapped to "baz.com:80".
-  rv = resolver->Resolve(
-      HostResolver::RequestInfo(HostPortPair("www.google.com", 80)),
-      DEFAULT_PRIORITY, &address_list, callback.callback(), &request,
-      NetLogWithSource());
-  EXPECT_THAT(rv, IsError(ERR_IO_PENDING));
-  rv = callback.WaitForResult();
-  EXPECT_THAT(rv, IsOk());
-  EXPECT_EQ("192.168.1.5:80", FirstAddress(address_list));
-  request.reset();
-
-  // Try resolving "foo.com:77". This will NOT be remapped, so result
-  // is "foo.com:77".
-  rv = resolver->Resolve(HostResolver::RequestInfo(HostPortPair("foo.com", 77)),
-                         DEFAULT_PRIORITY, &address_list, callback.callback(),
-                         &request, NetLogWithSource());
-  EXPECT_THAT(rv, IsError(ERR_IO_PENDING));
-  rv = callback.WaitForResult();
-  EXPECT_THAT(rv, IsOk());
-  EXPECT_EQ("192.168.1.8:77", FirstAddress(address_list));
-  request.reset();
-
-  // Remap "*.org" to "proxy:99".
-  EXPECT_TRUE(resolver->AddRuleFromString("Map *.org proxy:99"));
-
-  // Try resolving "chromium.org:61". Should be remapped to "proxy:99".
-  rv = resolver->Resolve(
-      HostResolver::RequestInfo(HostPortPair("chromium.org", 61)),
-      DEFAULT_PRIORITY, &address_list, callback.callback(), &request,
-      NetLogWithSource());
-  EXPECT_THAT(rv, IsError(ERR_IO_PENDING));
-  rv = callback.WaitForResult();
-  EXPECT_THAT(rv, IsOk());
-  EXPECT_EQ("192.168.1.11:99", FirstAddress(address_list));
-}
-
-TEST(MappedHostResolverTest, Inclusion_ResolveHost) {
-  base::test::ScopedTaskEnvironment scoped_task_environment;
-
   // Create a mock host resolver, with specific hostname to IP mappings.
   std::unique_ptr<MockHostResolver> resolver_impl(new MockHostResolver());
   resolver_impl->rules()->AddSimulatedFailure("*google.com");
@@ -170,53 +100,6 @@ TEST(MappedHostResolverTest, Inclusion_ResolveHost) {
 TEST(MappedHostResolverTest, Exclusion) {
   base::test::ScopedTaskEnvironment scoped_task_environment;
 
-  // Outstanding request.
-  std::unique_ptr<HostResolver::Request> request;
-  // Create a mock host resolver, with specific hostname to IP mappings.
-  std::unique_ptr<MockHostResolver> resolver_impl(new MockHostResolver());
-  resolver_impl->rules()->AddRule("baz", "192.168.1.5");
-  resolver_impl->rules()->AddRule("www.google.com", "192.168.1.3");
-
-  // Create a remapped resolver that uses |resolver_impl|.
-  std::unique_ptr<MappedHostResolver> resolver(
-      new MappedHostResolver(std::move(resolver_impl)));
-
-  int rv;
-  AddressList address_list;
-  TestCompletionCallback callback;
-
-  // Remap "*.com" to "baz".
-  EXPECT_TRUE(resolver->AddRuleFromString("map *.com baz"));
-
-  // Add an exclusion for "*.google.com".
-  EXPECT_TRUE(resolver->AddRuleFromString("EXCLUDE *.google.com"));
-
-  // Try resolving "www.google.com". Should not be remapped due to exclusion).
-  rv = resolver->Resolve(
-      HostResolver::RequestInfo(HostPortPair("www.google.com", 80)),
-      DEFAULT_PRIORITY, &address_list, callback.callback(), &request,
-      NetLogWithSource());
-  EXPECT_THAT(rv, IsError(ERR_IO_PENDING));
-  rv = callback.WaitForResult();
-  EXPECT_THAT(rv, IsOk());
-  EXPECT_EQ("192.168.1.3:80", FirstAddress(address_list));
-  request.reset();
-
-  // Try resolving "chrome.com:80". Should be remapped to "baz:80".
-  rv = resolver->Resolve(
-      HostResolver::RequestInfo(HostPortPair("chrome.com", 80)),
-      DEFAULT_PRIORITY, &address_list, callback.callback(), &request,
-      NetLogWithSource());
-  EXPECT_THAT(rv, IsError(ERR_IO_PENDING));
-  rv = callback.WaitForResult();
-  EXPECT_THAT(rv, IsOk());
-  EXPECT_EQ("192.168.1.5:80", FirstAddress(address_list));
-}
-
-// Tests that exclusions are respected.
-TEST(MappedHostResolverTest, Exclusion_ResolveHost) {
-  base::test::ScopedTaskEnvironment scoped_task_environment;
-
   // Create a mock host resolver, with specific hostname to IP mappings.
   std::unique_ptr<MockHostResolver> resolver_impl(new MockHostResolver());
   resolver_impl->rules()->AddRule("baz", "192.168.1.5");
@@ -258,49 +141,6 @@ TEST(MappedHostResolverTest, Exclusion_ResolveHost) {
 }
 
 TEST(MappedHostResolverTest, SetRulesFromString) {
-  base::test::ScopedTaskEnvironment scoped_task_environment;
-
-  // Outstanding request.
-  std::unique_ptr<HostResolver::Request> request;
-  // Create a mock host resolver, with specific hostname to IP mappings.
-  std::unique_ptr<MockHostResolver> resolver_impl(new MockHostResolver());
-  resolver_impl->rules()->AddRule("baz", "192.168.1.7");
-  resolver_impl->rules()->AddRule("bar", "192.168.1.9");
-
-  // Create a remapped resolver that uses |resolver_impl|.
-  std::unique_ptr<MappedHostResolver> resolver(
-      new MappedHostResolver(std::move(resolver_impl)));
-
-  int rv;
-  AddressList address_list;
-  TestCompletionCallback callback;
-
-  // Remap "*.com" to "baz", and *.net to "bar:60".
-  resolver->SetRulesFromString("map *.com baz , map *.net bar:60");
-
-  // Try resolving "www.google.com". Should be remapped to "baz".
-  rv = resolver->Resolve(
-      HostResolver::RequestInfo(HostPortPair("www.google.com", 80)),
-      DEFAULT_PRIORITY, &address_list, callback.callback(), &request,
-      NetLogWithSource());
-  EXPECT_THAT(rv, IsError(ERR_IO_PENDING));
-  rv = callback.WaitForResult();
-  EXPECT_THAT(rv, IsOk());
-  EXPECT_EQ("192.168.1.7:80", FirstAddress(address_list));
-  request.reset();
-
-  // Try resolving "chrome.net:80". Should be remapped to "bar:60".
-  rv = resolver->Resolve(
-      HostResolver::RequestInfo(HostPortPair("chrome.net", 80)),
-      DEFAULT_PRIORITY, &address_list, callback.callback(), &request,
-      NetLogWithSource());
-  EXPECT_THAT(rv, IsError(ERR_IO_PENDING));
-  rv = callback.WaitForResult();
-  EXPECT_THAT(rv, IsOk());
-  EXPECT_EQ("192.168.1.9:60", FirstAddress(address_list));
-}
-
-TEST(MappedHostResolverTest, SetRulesFromString_ResolveHost) {
   base::test::ScopedTaskEnvironment scoped_task_environment;
 
   // Create a mock host resolver, with specific hostname to IP mappings.
@@ -359,45 +199,6 @@ TEST(MappedHostResolverTest, ParseInvalidRules) {
 
 // Test mapping hostnames to resolving failures.
 TEST(MappedHostResolverTest, MapToError) {
-  base::test::ScopedTaskEnvironment scoped_task_environment;
-
-  // Outstanding request.
-  std::unique_ptr<HostResolver::Request> request;
-  std::unique_ptr<MockHostResolver> resolver_impl(new MockHostResolver());
-  resolver_impl->rules()->AddRule("*", "192.168.1.5");
-
-  std::unique_ptr<MappedHostResolver> resolver(
-      new MappedHostResolver(std::move(resolver_impl)));
-
-  int rv;
-  AddressList address_list;
-
-  // Remap *.google.com to resolving failures.
-  EXPECT_TRUE(resolver->AddRuleFromString("MAP *.google.com ~NOTFOUND"));
-
-  // Try resolving www.google.com --> Should give an error.
-  TestCompletionCallback callback1;
-  rv = resolver->Resolve(
-      HostResolver::RequestInfo(HostPortPair("www.google.com", 80)),
-      DEFAULT_PRIORITY, &address_list, callback1.callback(), &request,
-      NetLogWithSource());
-  EXPECT_THAT(rv, IsError(ERR_NAME_NOT_RESOLVED));
-  request.reset();
-
-  // Try resolving www.foo.com --> Should succeed.
-  TestCompletionCallback callback2;
-  rv = resolver->Resolve(
-      HostResolver::RequestInfo(HostPortPair("www.foo.com", 80)),
-      DEFAULT_PRIORITY, &address_list, callback2.callback(), &request,
-      NetLogWithSource());
-  EXPECT_THAT(rv, IsError(ERR_IO_PENDING));
-  rv = callback2.WaitForResult();
-  EXPECT_THAT(rv, IsOk());
-  EXPECT_EQ("192.168.1.5:80", FirstAddress(address_list));
-}
-
-// Test mapping hostnames to resolving failures.
-TEST(MappedHostResolverTest, MapToError_ResolveHost) {
   base::test::ScopedTaskEnvironment scoped_task_environment;
 
   // Outstanding request.
