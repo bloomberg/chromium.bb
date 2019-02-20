0f0af59ae854bffd3cb6bdc8df6f4864653ac28b
Wez <wez@chromium.org>
2019-02-20 20:00:47 +0000

    [Fuchsia] Clean up CastRunner integration tests.
    Move StartCastComponent() into the test fixture, and break out the
    CastChannel implementation to a separate helper class, for clarity.
    
    Also fixes WebComponent to be usable even if the caller does not
    provide a service-directory request handle to CreateComponent().
    
    Bug: 918724, 893229
    Change-Id: I9fafd6536d550240d79ee7baa7f4e653db823066
    Reviewed-on: https://chromium-review.googlesource.com/c/1474647
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633847}
    Former-commit-id: ae340b4dc8c509ea5cbd64ffd04b60112b919eb0

--- fuchsia/runners/cast/cast_runner_integration_test.cc
+++ fuchsia/runners/cast/cast_runner_integration_test.cc
@@ -6,10 +6,12 @@
 #include <lib/zx/channel.h>
 
 #include "base/fuchsia/fuchsia_logging.h"
+#include "base/fuchsia/scoped_service_binding.h"
 #include "base/fuchsia/service_directory.h"
 #include "base/fuchsia/service_directory_client.h"
 #include "base/message_loop/message_loop.h"
 #include "base/strings/stringprintf.h"
+#include "base/test/bind_test_util.h"
 #include "base/test/test_timeouts.h"
 #include "fuchsia/base/fit_adapter.h"
 #include "fuchsia/base/result_receiver.h"
@@ -17,7 +19,6 @@
 #include "fuchsia/fidl/chromium/web/cpp/fidl.h"
 #include "fuchsia/runners/cast/cast_runner.h"
 #include "fuchsia/runners/cast/fake_application_config_manager.h"
-#include "fuchsia/runners/cast/test_common.h"
 #include "fuchsia/runners/common/web_component.h"
 #include "fuchsia/runners/common/web_content_runner.h"
 #include "net/test/embedded_test_server/default_handlers.h"
@@ -31,43 +32,72 @@ const char kTestServerRoot[] =
     FILE_PATH_LITERAL("fuchsia/runners/cast/testdata");
 
 void ComponentErrorHandler(zx_status_t status) {
-  ZX_LOG(ERROR, status) << "Component launch failed.";
+  ZX_LOG(ERROR, status) << "Component launch failed";
   ADD_FAILURE();
 }
 
+class FakeCastChannel : public chromium::cast::CastChannel {
+ public:
+  FakeCastChannel() = default;
+
+  // chromium::web::CastChannel implementation.
+  void OnOpened(fidl::InterfaceHandle<chromium::web::MessagePort> channel,
+                OnOpenedCallback callback_ignored) override {
+    // |callback_ignored| is dropped because these tests don't exercise multiple
+    // channel lifetimes.
+    connected_channel_ = channel.Bind();
+
+    if (on_channel_connected_)
+      std::move(on_channel_connected_).Run();
+  }
+
+  chromium::web::MessagePort* connected_channel() const {
+    // Return nullptr if |connected_channel_| is not currently bound.
+    return connected_channel_ ? connected_channel_.get() : nullptr;
+  }
+
+  void set_on_channel_connected(base::OnceClosure on_channel_connected) {
+    on_channel_connected_ = std::move(on_channel_connected);
+  }
+
+ protected:
+  // The connected Cast Channel.
+  chromium::web::MessagePortPtr connected_channel_;
+
+  // A pending on-connect callback, to be invoked once a Cast Channel is
+  // received.
+  base::OnceClosure on_channel_connected_;
+
+  DISALLOW_COPY_AND_ASSIGN(FakeCastChannel);
+};
+
 }  // namespace
 
-class CastRunnerIntegrationTest : public testing::Test,
-                                  public chromium::cast::CastChannel {
+class CastRunnerIntegrationTest : public testing::Test {
  public:
   CastRunnerIntegrationTest()
       : run_timeout_(TestTimeouts::action_timeout()),
-        cast_channel_binding_(this) {
+        app_config_binding_(&app_config_manager_) {
     // Create a new test ServiceDirectory, and ServiceDirectoryClient connected
     // to it, for tests to use to drive the CastRunner.
     fidl::InterfaceHandle<fuchsia::io::Directory> directory;
-    test_services_ = std::make_unique<base::fuchsia::ServiceDirectory>(
+    public_services_ = std::make_unique<base::fuchsia::ServiceDirectory>(
         directory.NewRequest());
-    test_services_client_ =
-        std::make_unique<base::fuchsia::ServiceDirectoryClient>(
-            std::move(directory));
 
     // Create the AppConfigManager.
-    app_config_binding_ = std::make_unique<
-        fidl::Binding<chromium::cast::ApplicationConfigManager>>(
-        &app_config_manager_);
-    chromium::cast::ApplicationConfigManagerPtr app_config_manager_interface;
-    app_config_binding_->Bind(app_config_manager_interface.NewRequest());
+    chromium::cast::ApplicationConfigManagerPtr app_config_manager_ptr;
+    app_config_binding_.Bind(app_config_manager_ptr.NewRequest());
 
     // Create the CastRunner, published into |test_services_|.
     cast_runner_ = std::make_unique<CastRunner>(
-        test_services_.get(), WebContentRunner::CreateDefaultWebContext(),
-        std::move(app_config_manager_interface),
-        cast_runner_run_loop_.QuitClosure());
+        public_services_.get(), WebContentRunner::CreateDefaultWebContext(),
+        std::move(app_config_manager_ptr), cast_runner_run_loop_.QuitClosure());
 
     // Connect to the CastRunner's fuchsia.sys.Runner interface.
+    base::fuchsia::ServiceDirectoryClient public_directory_client(
+        std::move(directory));
     cast_runner_ptr_ =
-        test_services_client_->ConnectToService<fuchsia::sys::Runner>();
+        public_directory_client.ConnectToService<fuchsia::sys::Runner>();
     cast_runner_ptr_.set_error_handler([this](zx_status_t status) {
       ZX_LOG(ERROR, status) << "CastRunner closed channel.";
       ADD_FAILURE();
@@ -88,23 +118,48 @@ class CastRunnerIntegrationTest : public testing::Test,
   }
 
   void WaitUntilCastChannelOpened() {
-    if (connected_channel_)
+    if (cast_channel_.connected_channel())
       return;
 
     base::RunLoop run_loop;
-    on_channel_connected_cb_ = run_loop.QuitClosure();
+    cast_channel_.set_on_channel_connected(run_loop.QuitClosure());
     run_loop.Run();
+
+    DCHECK(cast_channel_.connected_channel());
   }
 
-  // chromium::web::CastChannel implementation.
-  void OnOpened(fidl::InterfaceHandle<chromium::web::MessagePort> channel,
-                OnOpenedCallback callback_ignored) override {
-    // |callback_ignored| is dropped because these tests don't exercise multiple
-    // channel lifetimes.
-    connected_channel_ = channel.Bind();
+  fuchsia::sys::ComponentControllerPtr StartCastComponent(
+      base::StringPiece component_url) {
+    DCHECK(!component_services_);
 
-    if (on_channel_connected_cb_)
-      std::move(on_channel_connected_cb_).Run();
+    // Create a ServiceDirectory and publish the CastChannel into it.
+    fidl::InterfaceHandle<fuchsia::io::Directory> directory;
+    component_services_ = std::make_unique<base::fuchsia::ServiceDirectory>(
+        directory.NewRequest());
+    cast_channel_binding_ = std::make_unique<
+        base::fuchsia::ScopedServiceBinding<chromium::cast::CastChannel>>(
+        component_services_.get(), &cast_channel_);
+
+    // Configure the Runner, including a service directory channel to publish
+    // services to.
+    fuchsia::sys::StartupInfo startup_info;
+    startup_info.launch_info.url = component_url.as_string();
+
+    // Place the ServiceDirectory in the |flat_namespace|.
+    startup_info.flat_namespace.paths.emplace_back("/svc");
+    startup_info.flat_namespace.directories.emplace_back(
+        directory.TakeChannel());
+
+    fuchsia::sys::Package package;
+    package.resolved_url = component_url.as_string();
+
+    fuchsia::sys::ComponentControllerPtr component_controller;
+    cast_runner_ptr_->StartComponent(std::move(package),
+                                     std::move(startup_info),
+                                     component_controller.NewRequest());
+
+    EXPECT_TRUE(component_controller.is_bound());
+    return component_controller;
   }
 
  protected:
@@ -114,22 +169,17 @@ class CastRunnerIntegrationTest : public testing::Test,
 
   // Returns fake Cast application information to the CastRunner.
   FakeApplicationConfigManager app_config_manager_;
-  std::unique_ptr<fidl::Binding<chromium::cast::ApplicationConfigManager>>
-      app_config_binding_;
+  fidl::Binding<chromium::cast::ApplicationConfigManager> app_config_binding_;
 
-  // The connected Cast Channel.
-  chromium::web::MessagePortPtr connected_channel_;
-
-  // A pending on-connect callback, to be invoked once a Cast Channel is
-  // received.
-  base::OnceClosure on_channel_connected_cb_;
-
-  // Receives connected channels from the CastRunner.
-  fidl::Binding<chromium::cast::CastChannel> cast_channel_binding_;
+  // Incoming service directory, CastChannel and service bindings.
+  std::unique_ptr<base::fuchsia::ServiceDirectory> component_services_;
+  FakeCastChannel cast_channel_;
+  std::unique_ptr<
+      base::fuchsia::ScopedServiceBinding<chromium::cast::CastChannel>>
+      cast_channel_binding_;
 
   // ServiceDirectory into which the CastRunner will publish itself.
-  std::unique_ptr<base::fuchsia::ServiceDirectory> test_services_;
-  std::unique_ptr<base::fuchsia::ServiceDirectoryClient> test_services_client_;
+  std::unique_ptr<base::fuchsia::ServiceDirectory> public_services_;
 
   std::unique_ptr<CastRunner> cast_runner_;
   fuchsia::sys::RunnerPtr cast_runner_ptr_;
@@ -147,11 +197,9 @@ TEST_F(CastRunnerIntegrationTest, BasicRequest) {
                                     test_server_.GetURL(kBlankAppPath));
 
   // Launch the test-app component.
-  fuchsia::sys::ComponentControllerPtr component_controller_ptr;
-  base::fuchsia::ServiceDirectoryClient services_client(StartCastComponent(
-      base::StringPrintf("cast:%s", kBlankAppId), &cast_runner_ptr_,
-      component_controller_ptr.NewRequest(), &cast_channel_binding_));
-  component_controller_ptr.set_error_handler(&ComponentErrorHandler);
+  fuchsia::sys::ComponentControllerPtr component_controller =
+      StartCastComponent(base::StringPrintf("cast:%s", kBlankAppId));
+  component_controller.set_error_handler(&ComponentErrorHandler);
 
   // Access the NavigationController from the WebComponent. The test will hang
   // here if no WebComponent was created.
@@ -179,30 +227,32 @@ TEST_F(CastRunnerIntegrationTest, BasicRequest) {
     EXPECT_EQ(nav_entry->get()->url, test_server_.GetURL(kBlankAppPath).spec());
   }
 
-  // Verify that the component is torn down when |component_controller_ptr| is
+  // Verify that the component is torn down when |component_controller| is
   // unbound, by observing the destruction of its child Interfaces.
   base::RunLoop destruction_run_loop;
   nav_controller.set_error_handler(
       [&destruction_run_loop](zx_status_t) { destruction_run_loop.Quit(); });
-  component_controller_ptr.Unbind();
+  component_controller.Unbind();
   destruction_run_loop.Run();
 }
 
 TEST_F(CastRunnerIntegrationTest, IncorrectCastAppId) {
-  // Launch the test-app component.
-  fuchsia::sys::ComponentControllerPtr component_controller_ptr;
-  base::fuchsia::ServiceDirectoryClient services_client(StartCastComponent(
-      "cast:99999999", &cast_runner_ptr_, component_controller_ptr.NewRequest(),
-      &cast_channel_binding_));
-  component_controller_ptr.set_error_handler(&ComponentErrorHandler);
+  // Launch the a component with an invalid Cast app Id.
+  fuchsia::sys::ComponentControllerPtr component_controller =
+      StartCastComponent("cast:99999999");
 
-  // Ensure no WebComponent was created.
+  // Run the loop until the ComponentController is dropped, or a WebComponent is
+  // created.
   base::RunLoop run_loop;
+  component_controller.set_error_handler([&run_loop](zx_status_t status) {
+    EXPECT_EQ(status, ZX_ERR_PEER_CLOSED);
+    run_loop.Quit();
+  });
   cr_fuchsia::ResultReceiver<WebComponent*> web_component(
       run_loop.QuitClosure());
   cast_runner_->GetWebComponentForTest(web_component.GetReceiveCallback());
   run_loop.Run();
-  EXPECT_EQ(*web_component, nullptr);
+  EXPECT_FALSE(web_component.has_value());
 }
 
 TEST_F(CastRunnerIntegrationTest, CastChannel) {
@@ -212,11 +262,9 @@ TEST_F(CastRunnerIntegrationTest, CastChannel) {
                                     test_server_.GetURL(kCastChannelAppPath));
 
   // Launch the test-app component.
-  fuchsia::sys::ComponentControllerPtr component_controller_ptr;
-  base::fuchsia::ServiceDirectoryClient services_client(StartCastComponent(
-      base::StringPrintf("cast:%s", kCastChannelAppId), &cast_runner_ptr_,
-      component_controller_ptr.NewRequest(), &cast_channel_binding_));
-  component_controller_ptr.set_error_handler(&ComponentErrorHandler);
+  fuchsia::sys::ComponentControllerPtr component_controller =
+      StartCastComponent(base::StringPrintf("cast:%s", kCastChannelAppId));
+  component_controller.set_error_handler(&ComponentErrorHandler);
 
   // Access the NavigationController from the WebComponent. The test will hang
   // here if no WebComponent was created.
@@ -252,7 +300,7 @@ TEST_F(CastRunnerIntegrationTest, CastChannel) {
     base::RunLoop run_loop;
     cr_fuchsia::ResultReceiver<chromium::web::WebMessage> message(
         run_loop.QuitClosure());
-    connected_channel_->ReceiveMessage(
+    cast_channel_.connected_channel()->ReceiveMessage(
         cr_fuchsia::CallbackToFitFunction(message.GetReceiveCallback()));
     run_loop.Run();
 
@@ -260,10 +308,10 @@ TEST_F(CastRunnerIntegrationTest, CastChannel) {
               expected);
   }
 
-  component_controller_ptr.Unbind();
+  // Shutdown the component and wait for the CastChannel to close.
+  component_controller.Unbind();
   base::RunLoop run_loop;
-  cast_channel_binding_.set_error_handler(
-      [&run_loop](zx_status_t) { run_loop.Quit(); });
+  cast_channel_binding_->SetOnLastClientCallback(run_loop.QuitClosure());
   run_loop.Run();
 }
 
@@ -274,20 +322,18 @@ TEST_F(CastRunnerIntegrationTest, CastChannelConsumerDropped) {
                                     test_server_.GetURL(kCastChannelAppPath));
 
   // Launch the test-app component.
-  fuchsia::sys::ComponentControllerPtr component_controller_ptr;
-  base::fuchsia::ServiceDirectoryClient services_client(StartCastComponent(
-      base::StringPrintf("cast:%s", kCastChannelAppId), &cast_runner_ptr_,
-      component_controller_ptr.NewRequest(), &cast_channel_binding_));
+  fuchsia::sys::ComponentControllerPtr component_controller =
+      StartCastComponent(base::StringPrintf("cast:%s", kCastChannelAppId));
 
   // Expect that disconnecting the Cast Channel consumer service will trigger
   // the destruction of the Cast Component.
-  cast_channel_binding_.Unbind();
+  cast_channel_binding_.reset();
   base::RunLoop run_loop;
-  component_controller_ptr.set_error_handler(
+  component_controller.set_error_handler(
       [&run_loop](zx_status_t) { run_loop.Quit(); });
   run_loop.Run();
 
-  EXPECT_FALSE(component_controller_ptr.is_bound());
+  EXPECT_FALSE(component_controller.is_bound());
 }
 
 TEST_F(CastRunnerIntegrationTest, CastChannelComponentControllerDropped) {
@@ -297,18 +343,15 @@ TEST_F(CastRunnerIntegrationTest, CastChannelComponentControllerDropped) {
                                     test_server_.GetURL(kCastChannelAppPath));
 
   // Launch the test-app component.
-  fuchsia::sys::ComponentControllerPtr component_controller_ptr;
-  base::fuchsia::ServiceDirectoryClient services_client(StartCastComponent(
-      base::StringPrintf("cast:%s", kCastChannelAppId), &cast_runner_ptr_,
-      component_controller_ptr.NewRequest(), &cast_channel_binding_));
+  fuchsia::sys::ComponentControllerPtr component_controller =
+      StartCastComponent(base::StringPrintf("cast:%s", kCastChannelAppId));
 
   // Expect that disconnecting the ComponentController will kill the Cast
   // Component, which we verify indirectly by listening for the disconnection
   // of one of its CastChannel FIDL client.
-  component_controller_ptr.Unbind();
+  component_controller.Unbind();
   base::RunLoop run_loop;
-  cast_channel_binding_.set_error_handler(
-      [&run_loop](zx_status_t) { run_loop.Quit(); });
+  cast_channel_binding_->SetOnLastClientCallback(run_loop.QuitClosure());
   run_loop.Run();
 }
 
