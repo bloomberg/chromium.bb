167c829621d139ef4344815920d58353f77ada93
Mike Reed <reed@google.com>
2019-02-20 00:38:37 +0000

    add flag to stage API change
    See https://skia-review.googlesource.com/c/skia/+/193373
    
    Bug: skia:8773
    Change-Id: Ibd020259368e77f014cb3881f034c974c4392127
    Reviewed-on: https://chromium-review.googlesource.com/c/1478003
    Commit-Queue: Mike Reed <reed@google.com>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633491}
    Former-commit-id: 529c53dd111c65579e864942e027b3d4ebc9bd46

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -170,6 +170,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_AAA_CHOICE
 #endif
 
+#ifndef SK_SUPPORT_LEGACY_TEXTBLOBCACHEDIFFCANVAS_CONSTRUCTOR
+#define SK_SUPPORT_LEGACY_TEXTBLOBCACHEDIFFCANVAS_CONSTRUCTOR
+#endif
+
 // We're turning this off indefinitely,
 // until we can figure out some fundamental problems with its approach.
 //
