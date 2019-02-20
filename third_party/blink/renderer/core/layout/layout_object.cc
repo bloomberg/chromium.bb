a5586c9fce1b3fd450ebf3d0219f61cf26d274af
Stefan Zager <szager@chromium.org>
2019-02-20 21:23:50 +0000

    [IntersectionObserver] Assume non-zero opacity for unpainted iframes
    BUG=933694
    R=atotic@chromium.org
    
    Change-Id: I0a0aac1da5ab30ab09c1557e88387b1a2987bdf0
    Reviewed-on: https://chromium-review.googlesource.com/c/1478944
    Reviewed-by: Aleks Totic <atotic@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#633880}
    Former-commit-id: 43c87dfd59cbf8799f2827c86fd06a464106ce4f

--- third_party/blink/renderer/core/layout/layout_object.cc
+++ third_party/blink/renderer/core/layout/layout_object.cc
@@ -1544,10 +1544,17 @@ bool LayoutObject::HasDistortingVisualEffects() const {
 }
 
 bool LayoutObject::HasNonZeroEffectiveOpacity() const {
-  PropertyTreeState paint_properties = EnclosingLayer()
-                                           ->GetLayoutObject()
-                                           .FirstFragment()
-                                           .LocalBorderBoxProperties();
+  const FragmentData& fragment =
+      EnclosingLayer()->GetLayoutObject().FirstFragment();
+
+  // This can happen for an iframe element which is outside the viewport and has
+  // therefore never been painted. In that case, we do the safe thing -- report
+  // it as having non-zero opacity -- since this method is used by
+  // IntersectionObserver to detect occlusion.
+  if (!fragment.HasLocalBorderBoxProperties())
+    return true;
+
+  PropertyTreeState paint_properties = fragment.LocalBorderBoxProperties();
 
   for (const auto* effect = &paint_properties.Effect().Unalias(); effect;
        effect = SafeUnalias(effect->Parent())) {
