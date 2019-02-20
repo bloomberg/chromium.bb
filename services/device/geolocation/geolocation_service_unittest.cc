02d73ad700e69d75cf97833c4791fe7de95573c6
Maciej Pawlowski <mpawlowski@opera.com>
2019-02-20 12:10:59 +0000

    Extract PositionCache to separate file, make it persistent.
    PositionCache should be persistent and outlive NetworkLocationProvider,
    similarly to how LastPositionCache behaved. This lets us limit the
    number of API requests, since WifiData -> Geoposition mappings are
    stored longer.
    Also, LastPositionCache can now be merged with PositionCache.
    
    Bug: 672760
    Change-Id: Ibd4c30c9a58dd26deb5f67448b5e2bac48d634fc
    Reviewed-on: https://chromium-review.googlesource.com/c/1273055
    Commit-Queue: Maciej Pawlowski <mpawlowski@opera.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Auto-Submit: Maciej Pawlowski <mpawlowski@opera.com>
    Cr-Commit-Position: refs/heads/master@{#633681}
    Former-commit-id: 99ac5ffb9567e8f7dc2169736524679ff91ac36f

--- services/device/geolocation/geolocation_service_unittest.cc
+++ services/device/geolocation/geolocation_service_unittest.cc
@@ -11,6 +11,7 @@
 #include "chromeos/network/geolocation_handler.h"
 #endif
 #include "mojo/public/cpp/bindings/interface_ptr.h"
+#include "net/base/network_change_notifier.h"
 #include "services/device/device_service_test_base.h"
 #include "services/device/geolocation/geolocation_provider_impl.h"
 #include "services/device/geolocation/network_location_request.h"
@@ -42,7 +43,7 @@ class GeolocationServiceUnitTest : public DeviceServiceTestBase {
     chromeos::DBusThreadManager::Initialize();
     chromeos::NetworkHandler::Initialize();
 #endif
-
+    network_change_notifier_.reset(net::NetworkChangeNotifier::CreateMock());
     // We need to initialize the above *before* the base fixture instantiates
     // the device service.
     DeviceServiceTestBase::SetUp();
@@ -75,6 +76,7 @@ class GeolocationServiceUnitTest : public DeviceServiceTestBase {
     connector()->BindInterface(mojom::kServiceName, &geolocation_config_);
   }
 
+  std::unique_ptr<net::NetworkChangeNotifier> network_change_notifier_;
   mojom::GeolocationControlPtr geolocation_control_;
   mojom::GeolocationContextPtr geolocation_context_;
   mojom::GeolocationPtr geolocation_;
