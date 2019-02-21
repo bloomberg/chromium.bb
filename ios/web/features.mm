90543bedcb79df1168aba5eefbb5b793aee028d4
mrefaat <mrefaat@chromium.org>
2019-02-21 02:56:33 +0000

    Enable WKHTTPSystemCookieStore by default.
    Bug: 759229
    Change-Id: Icd6d8f29500ae29c59f6ab5bc00bbfeadb5f2ac7
    Reviewed-on: https://chromium-review.googlesource.com/c/1479275
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634038}
    Former-commit-id: 8da218a94fbc6da1bae7f5828f89f8e4aad274b5

--- ios/web/features.mm
+++ ios/web/features.mm
@@ -17,7 +17,7 @@ const base::Feature kSlimNavigationManager{"SlimNavigationManager",
                                            base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kWKHTTPSystemCookieStore{"WKHTTPSystemCookieStore",
-                                             base::FEATURE_DISABLED_BY_DEFAULT};
+                                             base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kCrashOnUnexpectedURLChange{
     "CrashOnUnexpectedURLChange", base::FEATURE_ENABLED_BY_DEFAULT};
