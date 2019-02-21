36141df66922e999d497311035efdf4b52a8cbcb
Sergey Poromov <poromov@chromium.org>
2019-02-21 10:58:08 +0000

    Do not crash on duplicate policy invalidation names.
    Due to server side issue, the same invalidation name could be sent for
    different kiosk apps, causing devices to constantly crash on signin
    screen.
    In order to not crash, we'll just silently stop receiving invalidations
    for one of "duplicate" apps, log the error and add UMA stats to track
    such behavior.
    
    Bug: 783428
    Test: Manual, trybots
    Change-Id: I11fd3b249a44699ce585e675bf697bee893c8438
    Reviewed-on: https://chromium-review.googlesource.com/c/1473990
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634143}
    Former-commit-id: fb8a794cf5ef80b41bac0538d8384f430ddd1f55

--- chrome/browser/policy/cloud/cloud_policy_invalidator.cc
+++ chrome/browser/policy/cloud/cloud_policy_invalidator.cc
@@ -16,6 +16,7 @@
 #include "base/time/time.h"
 #include "base/values.h"
 #include "components/invalidation/public/invalidation_service.h"
+#include "components/invalidation/public/invalidation_util.h"
 #include "components/invalidation/public/object_id_invalidation_map.h"
 #include "components/policy/core/common/cloud/cloud_policy_client.h"
 #include "components/policy/core/common/cloud/cloud_policy_refresh_scheduler.h"
@@ -308,7 +309,16 @@ void CloudPolicyInvalidator::Register(const invalidation::ObjectId& object_id) {
   // Update registration with the invalidation service.
   syncer::ObjectIdSet ids;
   ids.insert(object_id);
-  CHECK(invalidation_service_->UpdateRegisteredInvalidationIds(this, ids));
+  bool success =
+      invalidation_service_->UpdateRegisteredInvalidationIds(this, ids);
+  // Do not crash as server might send duplicate invalidation IDs due to
+  // http://b/119860379.
+  if (!success) {
+    LOG(ERROR) << "Failed to register " << syncer::ObjectIdToString(object_id)
+               << " for policy invalidations";
+  }
+  UMA_HISTOGRAM_BOOLEAN("Enterprise.PolicyInvalidationsRegistrationResult",
+                        success);
 }
 
 void CloudPolicyInvalidator::Unregister() {
