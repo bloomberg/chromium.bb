5515113d1fa01486335fc0998d06c5f020154efd
Eugene But <eugenebut@google.com>
2019-02-21 02:34:20 +0000

    Remove GURLOriginWithWKSecurityOrigin from ios/web/public.
    This function is only used by crw_web_controller.mm
    
    Bug: 616244
    Change-Id: I2dd87dc5798fc35901f0422c7df09690931ccbef
    Reviewed-on: https://chromium-review.googlesource.com/c/1474798
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634031}
    Former-commit-id: 6b6112c77445afa80f5de86bdc047f4615931a37

--- ios/web/public/origin_util.mm
+++ ios/web/public/origin_util.mm
@@ -4,16 +4,9 @@
 
 #import "ios/web/public/origin_util.h"
 
-#import <WebKit/WebKit.h>
-
-#include "base/numerics/safe_conversions.h"
 #include "base/stl_util.h"
-#include "base/strings/string_number_conversions.h"
-#include "base/strings/sys_string_conversions.h"
 #include "net/base/url_util.h"
 #include "url/gurl.h"
-#include "url/scheme_host_port.h"
-#include "url/url_canon.h"
 #include "url/url_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -38,24 +31,6 @@ bool IsOriginSecure(const GURL& url) {
     return true;
 
   return false;
-}
-
-GURL GURLOriginWithWKSecurityOrigin(WKSecurityOrigin* origin) {
-  if (!origin)
-    return GURL();
-  std::string scheme = base::SysNSStringToUTF8(origin.protocol);
-  std::string host = base::SysNSStringToUTF8(origin.host);
-  uint16_t port = base::checked_cast<uint16_t>(origin.port);
-  if (port == 0) {
-    // WKSecurityOrigin.port is 0 if the effective port of this origin is the
-    // default for its scheme.
-    int default_port = url::DefaultPortForScheme(scheme.c_str(), scheme.size());
-    if (default_port != url::PORT_UNSPECIFIED)
-      port = base::checked_cast<uint16_t>(default_port);
-  }
-
-  url::SchemeHostPort origin_tuple(scheme, host, port);
-  return origin_tuple.GetURL();
 }
 
 }  // namespace web
--- ios/web/public/origin_util_unittest.mm
+++ ios/web/public/origin_util_unittest.mm
@@ -14,19 +14,6 @@
 #error "This file requires ARC support."
 #endif
 
-// WKSecurityOrigin can not be manually created, hence stub is needed.
-@interface WKSecurityOriginStub : NSObject
-// Methods from WKSecurityOrigin class.
-@property(nonatomic, copy) NSString* protocol;
-@property(nonatomic, copy) NSString* host;
-@property(nonatomic) NSInteger port;
-@end
-
-@implementation WKSecurityOriginStub
-@synthesize protocol = _protocol;
-@synthesize host = _host;
-@synthesize port = _port;
-@end
 
 namespace web {
 
@@ -45,64 +32,5 @@ TEST_F(OriginUtilTest, IsInsecureOriginSecure) {
   EXPECT_FALSE(IsOriginSecure(GURL("bogus://bogus")));
 }
 
-// Tests calling GURLOriginWithWKSecurityOrigin with nil.
-TEST_F(OriginUtilTest, GURLOriginWithNilWKSecurityOrigin) {
-  GURL url(GURLOriginWithWKSecurityOrigin(nil));
-
-  EXPECT_FALSE(url.is_valid());
-  EXPECT_TRUE(url.spec().empty());
-}
-
-// Tests calling GURLOriginWithWKSecurityOrigin with valid origin.
-TEST_F(OriginUtilTest, GURLOriginWithValidWKSecurityOrigin) {
-  WKSecurityOriginStub* origin = [[WKSecurityOriginStub alloc] init];
-  [origin setProtocol:@"http"];
-  [origin setHost:@"chromium.org"];
-  [origin setPort:80];
-
-  GURL url(
-      GURLOriginWithWKSecurityOrigin(static_cast<WKSecurityOrigin*>(origin)));
-  EXPECT_EQ("http://chromium.org/", url.spec());
-  EXPECT_TRUE(url.port().empty());
-}
-
-// Tests calling GURLOriginWithWKSecurityOrigin with default port.
-TEST_F(OriginUtilTest, GURLOriginWithDefaultPort) {
-  WKSecurityOriginStub* origin = [[WKSecurityOriginStub alloc] init];
-  [origin setProtocol:@"http"];
-  [origin setHost:@"chromium.org"];
-  [origin setPort:0];
-
-  GURL url(
-      GURLOriginWithWKSecurityOrigin(static_cast<WKSecurityOrigin*>(origin)));
-  EXPECT_EQ("http://chromium.org/", url.spec());
-  EXPECT_TRUE(url.port().empty());
-}
-
-// Tests calling GURLOriginWithWKSecurityOrigin with valid origin.
-TEST_F(OriginUtilTest, GURLOriginWithNonDefaultPort) {
-  WKSecurityOriginStub* origin = [[WKSecurityOriginStub alloc] init];
-  [origin setProtocol:@"http"];
-  [origin setHost:@"chromium.org"];
-  [origin setPort:123];
-
-  GURL url(
-      GURLOriginWithWKSecurityOrigin(static_cast<WKSecurityOrigin*>(origin)));
-  EXPECT_EQ("http://chromium.org:123/", url.spec());
-  EXPECT_EQ("123", url.port());
-}
-
-// Tests calling GURLOriginWithWKSecurityOrigin with valid origin.
-TEST_F(OriginUtilTest, GURLOriginWithChromeProtocol) {
-  WKSecurityOriginStub* origin = [[WKSecurityOriginStub alloc] init];
-  [origin setProtocol:@"testwebui"];
-  [origin setHost:@"version"];
-  [origin setPort:0];
-
-  GURL url(
-      GURLOriginWithWKSecurityOrigin(static_cast<WKSecurityOrigin*>(origin)));
-  EXPECT_EQ("testwebui://version/", url.spec());
-  EXPECT_TRUE(url.port().empty());
-}
 
 }  // namespace web
