f6bddbdc7e1f524cccc6edf4e9277a339bb182e7
Shao-Chuan Lee <shaochuan@chromium.org>
2019-02-22 05:54:40 +0000

    arc: Fix ARC enabled state recording on browser shutdown
    ArcSessionManager state is reset before final UMA recording happens on
    browser shutdown, which result in missing Arc.State and incorrectly
    recorded Arc.StateByUserType.
    
    This CL adds fields to ArcSessionManager to persist state across
    Shutdown() for UMA recording purposes.
    
    Bug: 929583
    Change-Id: I5fdf58ec60f9720599fa15c997cd2a23ed524fd9
    Reviewed-on: https://chromium-review.googlesource.com/c/1481180
    Commit-Queue: Shao-Chuan Lee <shaochuan@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Auto-Submit: Shao-Chuan Lee <shaochuan@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634549}
    Former-commit-id: 09017c49ecaf13b8d47ef3b3d7443b923cb544e7

--- chrome/browser/chromeos/arc/arc_session_manager.h
+++ chrome/browser/chromeos/arc/arc_session_manager.h
@@ -349,6 +349,11 @@ class ArcSessionManager : public ArcSessionRunner::Observer,
   // chromeos::SessionManagerClient::Observer:
   void EmitLoginPromptVisibleCalled() override;
 
+  // Updates |should_record_legacy_enabled_state_| and |enabled_state_uma_|
+  // whenever |profile_| or |enable_requested_| is changed (except Shutdown()).
+  // TODO(crbug.com/929583): Remove this temporary fix.
+  void UpdatePersistentUMAState();
+
   std::unique_ptr<ArcSessionRunner> arc_session_runner_;
 
   // Unowned pointer. Keeps current profile.
@@ -384,6 +389,13 @@ class ArcSessionManager : public ArcSessionRunner::Observer,
   // The time when ARC was about to start.
   base::Time arc_start_time_;
   base::Closure attempt_user_exit_callback_;
+
+  // ARC state depends on |profile_| and |enable_requested_| which is reset in
+  // Shutdown(). Since UMA recording happens after Shutdown() on browser
+  // shutdown, here we persist relevant state for UMA recording purposes.
+  // TODO(crbug.com/929583): Remove this temporary fix.
+  bool should_record_legacy_enabled_state_ = false;
+  bool enabled_state_uma_ = false;
 
   // Must be the last member.
   base::WeakPtrFactory<ArcSessionManager> weak_ptr_factory_;
