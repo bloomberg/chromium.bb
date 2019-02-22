caeff23162f30f361880922ab1f2713a7a377529
Yutaka Hirano <yhirano@chromium.org>
2019-02-22 08:36:46 +0000

    Revert "[reland] Making Google Translate compatible with |request_initiator_site_lock|."
    This reverts commit 004ab2cf3fb5f606a9d50a62fb1e5e0908d8d330 [formerly 9405f0353825a3e8360d9e5606286472f8db5f58].
    
    Reason for revert: TranslateManagerBrowserTest.AppCacheInteraction is failing on some bots: see https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/linux-chromeos-rel/20438 for example.
    
    Original change's description:
    > [reland] Making Google Translate compatible with |request_initiator_site_lock|.
    > 
    > This is a reland of https://crrev.com/c/1441277 that
    > - Trims the test down in a speculative attempt to avoid test flakiness
    >   (I verified that the test still fails without the corresponding
    >   changes in TranslateHelper::Translate)
    > - Adds logging statements to the test, to help in case trimming of the
    >   test doesn't address the flakiness.
    > The original CL description follows below.
    > 
    > Google Translate executes its scripts inside "isolated worlds".  This is
    > quite similar to how extension content scripts execute and has a
    > consequence of using |request_initiator| set to the origin of the
    > isolated world (rather than to the origin of the page where the script
    > was injected).  This is problematic for 2 reasons:
    > 1. It leads to renderer kills, when an AppCache verifies
    >    |request_initiator| using CPSPI::CanAccessDataForOrigin.
    > 2. It leads to incompatibility between |request_initiator| and
    >    |request_initiator_site_lock| which can also cause wrong/harsh
    >    security decisions (e.g. CORB blocking of otherwise benign requests).
    > 
    > This CL addresses the problems above, by making sure that requests
    > initiated by the Translate's "isolated worls" will use a separate
    > URLLoaderFactory.
    > - For injecting and selecting the factory on the renderer side, the same
    >   mechanism is used as for extension content scripts (i.e.
    >   URLLoaderFactoryBundleInfo::initiator_specific_factory_infos is
    >   populated for the origin associated with the "isolated world").
    > - For simplicity and to avoid races, the separate URLLoaderFactory is
    >   sent from the browser to the renderer process in the same, single IPC
    >   that triggers script injection into an isolated world (i.e. in
    >   translate::mojom::Page::Translate method).
    > 
    > As a side effect, this CL also causes Translate's requests to go
    > directly to the NetworkService (rather than to the AppCache's
    > AppCacheSubresourceURLFactory).  This seems desirable.
    > 
    > Bug: 925457
    > Tbr: John Abd-El-Malek <jam@chromium.org>
    > Tbr: Daniel Cheng <dcheng@chromium.org>
    > Change-Id: If03198d98a3c78031718eecb21ec1ae39db5530f
    > Reviewed-on: https://chromium-review.googlesource.com/c/1481535
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#634432}
    
    TBR=dcheng@chromium.org,jam@chromium.org,anthonyvd@chromium.org,lukasza@chromium.org
    
    Change-Id: I02c798f40caa942c3a3f801ff83529e138a54057
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 925457
    Reviewed-on: https://chromium-review.googlesource.com/c/1482439
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634581}
    Former-commit-id: ab3bc83e31e3feaf3cbbfa40ff00516285244add

--- components/translate/content/renderer/translate_helper.cc
+++ components/translate/content/renderer/translate_helper.cc
@@ -8,7 +8,6 @@
 
 #include "base/bind.h"
 #include "base/compiler_specific.h"
-#include "base/feature_list.h"
 #include "base/json/string_escape.h"
 #include "base/location.h"
 #include "base/logging.h"
@@ -26,9 +25,7 @@
 #include "content/public/common/url_constants.h"
 #include "content/public/renderer/render_frame.h"
 #include "content/public/renderer/render_thread.h"
-#include "services/network/public/cpp/features.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
-#include "third_party/blink/public/common/loader/url_loader_factory_bundle.h"
 #include "third_party/blink/public/platform/web_isolated_world_info.h"
 #include "third_party/blink/public/web/web_document.h"
 #include "third_party/blink/public/web/web_language_detection_details.h"
@@ -285,20 +282,10 @@ int64_t TranslateHelper::ExecuteScriptAndGetIntegerResult(
 }
 
 // mojom::Page implementations.
-void TranslateHelper::Translate(
-    const std::string& translate_script,
-    network::mojom::URLLoaderFactoryPtr loader_factory_for_translate_script,
-    const std::string& source_lang,
-    const std::string& target_lang,
-    TranslateCallback callback) {
-  url::Origin translate_origin =
-      url::Origin::Create(GetTranslateSecurityOrigin());
-
-  if (base::FeatureList::IsEnabled(network::features::kNetworkService)) {
-    render_frame()->MarkInitiatorAsRequiringSeparateURLLoaderFactory(
-        translate_origin, std::move(loader_factory_for_translate_script));
-  }
-
+void TranslateHelper::Translate(const std::string& translate_script,
+                                const std::string& source_lang,
+                                const std::string& target_lang,
+                                TranslateCallback callback) {
   WebLocalFrame* main_frame = render_frame()->GetWebFrame();
   if (!main_frame) {
     // Cancelled.
@@ -335,7 +322,8 @@ void TranslateHelper::Translate(
   // Set up v8 isolated world with proper content-security-policy and
   // security-origin.
   blink::WebIsolatedWorldInfo info;
-  info.security_origin = WebSecurityOrigin::Create(translate_origin.GetURL());
+  info.security_origin =
+      WebSecurityOrigin::Create(GetTranslateSecurityOrigin());
   info.content_security_policy = WebString::FromUTF8(kContentSecurityPolicy);
   main_frame->SetIsolatedWorldInfo(world_id_, info);
 
