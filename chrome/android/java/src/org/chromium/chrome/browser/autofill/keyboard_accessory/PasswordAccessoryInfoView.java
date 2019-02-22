c7d3ae8aef673daf30f4c1efd102183a9c1c073c
Friedrich Horschig <fhorschig@chromium.org>
2019-02-22 11:57:43 +0000

    [Mfill Android] Fix spacing between footer commands and user infos
    This CL adds a space below the last UserInfo element and the first
    non-UserInfo element.
    Effectively, this means the FooterCommands have the same padding to the
    UserInfo as UserInfo elements have to each other.
    
    Screenshot before:
    https://photos.app.goo.gl/h3By5RN2zUiDGVLw8
    Screenshot after:
    https://photos.app.goo.gl/MVzu6uirjkkVhnBF9
    
    Alternatives considered:
    1. A static top margin for a footer command would not work here since
    footer commands are not supposed to have paddings between each other.
    2. Adding the space relative to the first Footer Command adds a padding
    between the "No saved passwords" text and the footer commands if no
    UserInfo is available. Such padding isn't supposed to exist either.
    3. Wrapping FooterCommands or UserInfos into a Layout and add the
    margin to the layout would work but degrades the effectiveness of the
    RecyclerView which wants to reuse containers.
    
    Change-Id: I63e08ab42215f81befb05bd5a26b96d27dc4b908
    Reviewed-on: https://chromium-review.googlesource.com/c/1481295
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634617}
    Former-commit-id: 1827194d338e97ce0a78492fe879d6cb1bdd2333

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/keyboard_accessory/PasswordAccessoryInfoView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/keyboard_accessory/PasswordAccessoryInfoView.java
@@ -6,11 +6,14 @@ package org.chromium.chrome.browser.autofill.keyboard_accessory;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.support.v7.content.res.AppCompatResources;
+import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -24,6 +27,26 @@ class PasswordAccessoryInfoView extends LinearLayout {
     private ImageView mIcon;
     private ChipView mUsername;
     private ChipView mPassword;
+
+    /**
+     * This decoration adds a space between the last PasswordAccessoryInfoView and the first
+     * non-PasswordAccessoryInfoView. This allows to define a margin below the whole list of
+     * PasswordAccessoryInfoViews without having to wrap them in a new layout. This would reduce
+     * the reusability of single info containers in a RecyclerView.
+     */
+    public static class DynamicBottomSpacer extends RecyclerView.ItemDecoration {
+        @Override
+        public void getItemOffsets(
+                Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            super.getItemOffsets(outRect, view, parent, state);
+            if (view instanceof PasswordAccessoryInfoView) return;
+            int previous = parent.indexOfChild(view) - 1;
+            if (previous < 0) return;
+            if (!(parent.getChildAt(previous) instanceof PasswordAccessoryInfoView)) return;
+            outRect.top = view.getContext().getResources().getDimensionPixelSize(
+                    R.dimen.keyboard_accessory_suggestion_padding);
+        }
+    }
 
     /**
      * Constructor for inflating from XML.
