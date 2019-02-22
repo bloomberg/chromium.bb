d21d8bb0d87b085baebb8ae70f53ca8bb4d20c3e
bsheedy <bsheedy@chromium.org>
2019-02-22 21:35:06 +0000

    Automate VR manual test for suggestion hovering
    Automates a manual VR regression test that ensures that the hover state
    and hit testing of search suggestions in the VR browser function
    as expected.
    
    In order to prevent the test from flaking due to the blinking
    omnibox cursor, also adds the ability to crop images before comparing
    them in via the RenderTestRule.
    
    Bug: 887561
    Change-Id: Ifddd18ff492b14bee08658251dc2191e7e481dcb
    Reviewed-on: https://chromium-review.googlesource.com/c/1479550
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634828}
    Former-commit-id: f1e50fa576ec70370d614f08e04790b0e624c06b

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr/util/RenderTestUtils.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr/util/RenderTestUtils.java
@@ -6,6 +6,7 @@ package org.chromium.chrome.browser.vr.util;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.RectF;
 
 import org.junit.Assert;
 
@@ -34,9 +35,18 @@ public class RenderTestUtils {
      */
     public static void dumpAndCompare(String suffix, String id, RenderTestRule rule)
             throws IOException, InterruptedException {
+        dumpAndCompareWithCrop(suffix, id, null /* bounds */, rule);
+    }
+
+    /**
+     * Helper function for running the general dumpAndCompare when only one image needs to be
+     * compared and it needs to be cropped before comparing.
+     */
+    public static void dumpAndCompareWithCrop(String suffix, String id, RectF bounds,
+            RenderTestRule rule) throws IOException, InterruptedException {
         HashMap<String, String> suffixToId = new HashMap<String, String>();
         suffixToId.put(suffix, id);
-        dumpAndCompare(suffixToId, rule);
+        dumpAndCompare(suffixToId, bounds, rule);
     }
 
     /**
@@ -45,10 +55,12 @@ public class RenderTestUtils {
      *
      * @param suffixToIds a map from framebuffer suffixes from NativeUiUtils to RenderTest image
      *        IDs.
+     * @param bounds a RectF defining the bounds [0, 1] with the origin in the top left corner to
+     *        crop the image to before comparing. Pass null to not crop.
      * @param rule the RenderTestRule to use for comparing images.
      */
-    public static void dumpAndCompare(HashMap<String, String> suffixToIds, RenderTestRule rule)
-            throws IOException, InterruptedException {
+    public static void dumpAndCompare(HashMap<String, String> suffixToIds, RectF bounds,
+            RenderTestRule rule) throws IOException, InterruptedException {
         File dumpDirectory = new File(UrlUtils.getIsolatedTestFilePath(IMAGE_DUMP_DIR));
         if (!dumpDirectory.exists() && !dumpDirectory.isDirectory()) {
             Assert.assertTrue("Failed to make framebuffer dump directory", dumpDirectory.mkdirs());
@@ -65,6 +77,27 @@ public class RenderTestUtils {
             BitmapFactory.Options options = new BitmapFactory.Options();
             options.inPreferredConfig = Bitmap.Config.ARGB_8888;
             Bitmap bitmap = BitmapFactory.decodeFile(filepath, options);
+
+            // Crop the image if necessary
+            if (bounds != null) {
+                Assert.assertTrue(
+                        "Given left bound is not in [0, 1)", bounds.left >= 0 && bounds.left < 1);
+                Assert.assertTrue("Given right bound is not in (0, 1]",
+                        bounds.right > 0 && bounds.right <= 1);
+                Assert.assertTrue(
+                        "Given horizontal bounds are not valid", bounds.left < bounds.right);
+                Assert.assertTrue(
+                        "Given top bound is not in [0, 1)", bounds.top >= 0 && bounds.top < 1);
+                Assert.assertTrue("Given bottom bound is not in (0, 1]",
+                        bounds.bottom > 0 && bounds.bottom <= 1);
+                Assert.assertTrue(
+                        "Given vertical bounds are not valid", bounds.top < bounds.bottom);
+
+                bitmap = Bitmap.createBitmap(bitmap, (int) (bounds.left * bitmap.getWidth()),
+                        (int) (bounds.top * bitmap.getHeight()),
+                        (int) (bounds.width() * bitmap.getWidth()),
+                        (int) (bounds.height() * bitmap.getHeight()));
+            }
 
             // The browser UI dump contains both eyes rendered, which is unnecessary for comparing
             // since any difference in one should show up in the other. So, take the left half of
