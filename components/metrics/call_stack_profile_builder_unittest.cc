b6e07d1de3a36e3f29f5f8ba8104d791e8bebfd3
Mike Wittman <wittman@chromium.org>
2019-02-22 02:50:13 +0000

    [Sampling profiler] Move Windows module cache into ModuleCache
    Moves the parallel module caching implementation for Windows into
    ModuleCache. This implementation will be merged with the existing module
    caching implementation over the course of later refactoring CLs.
    
    Bug: 931418
    Change-Id: I65309bca1edc2675e7e5d74e0305bf5cfe017fd1
    Reviewed-on: https://chromium-review.googlesource.com/c/1478035
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Charlie Andrews <charliea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634493}
    Former-commit-id: 9de3faa3e56c402da4c2009c4a277bcff56ea795

--- components/metrics/call_stack_profile_builder_unittest.cc
+++ components/metrics/call_stack_profile_builder_unittest.cc
@@ -311,11 +311,9 @@ TEST(CallStackProfileBuilderTest, DedupModules) {
   base::FilePath module_path("/some/path/to/chrome");
 #endif
 
-  Module module1 = {module_base_address, "1", module_path};
-  Frame frame1 = {module_base_address + 0x10, &module1};
-
-  Module module2 = {module_base_address, "1", module_path};
-  Frame frame2 = {module_base_address + 0x20, &module2};
+  Module module = {module_base_address, "1", module_path};
+  Frame frame1 = {module_base_address + 0x10, &module};
+  Frame frame2 = {module_base_address + 0x20, &module};
 
   std::vector<Frame> frames = {frame1, frame2};
 
@@ -334,8 +332,8 @@ TEST(CallStackProfileBuilderTest, DedupModules) {
   ASSERT_EQ(1, profile.stack_size());
   ASSERT_EQ(2, profile.stack(0).frame_size());
 
-  // Since module1 and module2 have the same base address, they are considered
-  // the same module and therefore deduped.
+  // The two frames share the same module, which should be deduped in the
+  // output.
   ASSERT_TRUE(profile.stack(0).frame(0).has_module_id_index());
   EXPECT_EQ(0, profile.stack(0).frame(0).module_id_index());
   ASSERT_TRUE(profile.stack(0).frame(0).has_address());
