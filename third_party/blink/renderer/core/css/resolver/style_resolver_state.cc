c2658dde2bc7223cf7777a344d94a559b615f866
Fredrik Söderquist <fs@opera.com>
2019-02-22 18:46:53 +0000

    Resolve stroke-width 'em' units against unzoomed font size
    When resolving style for the 'stroke-width' property we would use
    CSSToLengthConversionData::CopyWithAdjustedZoom() with an argument of 1
    to get an "unzoomed" length. The FontSizes object was however not
    adjusted, so still carried zoomed base font sizes.
    
    Make a new method on StyleResolverState that returns an
    CSSToLengthConversionData that uses unzoomed unit bases, basing it on
    the existing FontSizeConversionData() method which does roughly the
    same thing except it uses the parent style for unit bases.
    
    Bug: 933689
    Change-Id: Icfbc59a641f0dc2d94f00ba5f3e9a15b36f8c195
    Reviewed-on: https://chromium-review.googlesource.com/c/1481417
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#634750}
    Former-commit-id: 838c89ea3956c474d8c108e176978f30011b8c00

--- third_party/blink/renderer/core/css/resolver/style_resolver_state.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver_state.cc
@@ -94,15 +94,27 @@ scoped_refptr<ComputedStyle> StyleResolverState::TakeStyle() {
   return std::move(style_);
 }
 
-CSSToLengthConversionData StyleResolverState::FontSizeConversionData() const {
-  float em = ParentStyle()->SpecifiedFontSize();
+CSSToLengthConversionData StyleResolverState::UnzoomedLengthConversionData(
+    const ComputedStyle* font_style) const {
+  float em = font_style->SpecifiedFontSize();
   float rem = RootElementStyle() ? RootElementStyle()->SpecifiedFontSize() : 1;
+  // TODO(fs): Since 'ch' and 'ex' are still accessed directly from the font,
+  // they will still have zoom applied.
   CSSToLengthConversionData::FontSizes font_sizes(em, rem,
-                                                  &ParentStyle()->GetFont());
+                                                  &font_style->GetFont());
   CSSToLengthConversionData::ViewportSize viewport_size(
       GetDocument().GetLayoutView());
 
   return CSSToLengthConversionData(Style(), font_sizes, viewport_size, 1);
+}
+
+CSSToLengthConversionData StyleResolverState::FontSizeConversionData() const {
+  return UnzoomedLengthConversionData(ParentStyle());
+}
+
+CSSToLengthConversionData StyleResolverState::UnzoomedLengthConversionData()
+    const {
+  return UnzoomedLengthConversionData(Style());
 }
 
 void StyleResolverState::SetParentStyle(
