9d6caac555b780bfb3c75121bc7552086a8e3053
Christos Froussios <cfroussios@chromium.org>
2019-02-22 15:49:27 +0000

    Revert "Add DeviceWilcoDTCAllowed device policy"
    This reverts commit 23c94d7b4e3fd53bd745aa22014aa213c3ec920c [formerly 4c800eb7b092ba415535890066ccff4fbf3da871].
    
    Reason for revert: Suspected of breaking PolicyPrefsTest.PolicyToPrefsMapping
    on browser_tests
    on https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/linux-chromeos-rel
    
    First failure
    https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/linux-chromeos-rel/20456
    
    Original change's description:
    > Add DeviceWilcoDTCAllowed device policy
    > 
    > Create new boolean policy to control wilco diagnostics and telemetry
    > controller if it is present on the device.
    > 
    > BUG=b:123926018
    > TEST=unit_tests
    > 
    > Change-Id: I8161a36424310fbabfc580d3931121dc7a5c5492
    > Reviewed-on: https://chromium-review.googlesource.com/c/1456075
    > Commit-Queue: Polina Bondarenko <pbond@chromium.org>
    > Auto-Submit: Polina Bondarenko <pbond@chromium.org>
    > Reviewed-by: Bartosz Fabianowski <bartfab@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#634641}
    
    TBR=bartfab@chromium.org,pbond@chromium.org
    
    Change-Id: Iff0d905d9c103369822a5c88f90f877e2d289f2e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: b:123926018
    Reviewed-on: https://chromium-review.googlesource.com/c/1483021
    Reviewed-by: Christos Froussios <cfroussios@chromium.org>
    Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634672}
    Former-commit-id: 6fcf18f1adac2700ca2b73a393833d28403fe15d

--- chrome/browser/chromeos/settings/device_settings_provider.cc
+++ chrome/browser/chromeos/settings/device_settings_provider.cc
@@ -83,7 +83,6 @@ const char* const kKnownSettings[] = {
     kDeviceNativePrintersWhitelist,
     kDeviceQuirksDownloadEnabled,
     kDeviceUnaffiliatedCrostiniAllowed,
-    kDeviceWilcoDTCAllowed,
     kDeviceDisplayResolution,
     kDeviceRebootOnUserSignout,
     kDisplayRotationDefault,
@@ -761,16 +760,6 @@ void DecodeGenericPolicies(const em::ChromeDeviceSettingsProto& policy,
       new_values_cache->SetValue(
           kDeviceRebootOnUserSignout,
           base::Value(container.reboot_on_signout_mode()));
-    }
-  }
-
-  if (policy.has_device_wilco_dtc_allowed()) {
-    const em::DeviceWilcoDTCAllowedProto& container(
-        policy.device_wilco_dtc_allowed());
-    if (container.has_device_wilco_dtc_allowed()) {
-      new_values_cache->SetValue(
-          kDeviceWilcoDTCAllowed,
-          base::Value(container.device_wilco_dtc_allowed()));
     }
   }
 }
