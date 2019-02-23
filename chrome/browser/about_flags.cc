9e52ef2ab4e747c0f2fcb1eeb9af931ba2b1c4e6
Kristi Park <kristipark@chromium.org>
2019-02-23 01:30:40 +0000

    Revert "Changes to include fast app reinstall in chrome://flags."
    This reverts commit ebafb111a1b52bc73f1bbb6dd3397cac7da20260 [formerly 5a2d490901b78a58633f0c633187bd06fe2085b3].
    
    Reason for revert: Suspect for failing unit_tests, https://ci.chromium.org/p/chromium/builders/luci.chromium.ci/linux-chromeos-rel/20476
    
    Original change's description:
    > Changes to include fast app reinstall in chrome://flags.
    > 
    > Bug: 911427
    > Change-Id: Ic33fd7507d36d0e847598722067ce64802718f9e
    > Reviewed-on: https://chromium-review.googlesource.com/c/1484391
    > Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    > Commit-Queue: Rob Schonberger <robsc@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#634884}
    
    TBR=jennyz@chromium.org,robsc@chromium.org
    
    Change-Id: I69637a4f4064131eedf57f18f9a27769cf70a4f9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 911427
    Reviewed-on: https://chromium-review.googlesource.com/c/1484587
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#634907}
    Former-commit-id: d2a4056231f3d1bc532e5d7a8501792dd468f8bf

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -3815,10 +3815,6 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kEnableZeroStateSuggestionsName,
      flag_descriptions::kEnableZeroStateSuggestionsDescription, kOsCrOS,
      FEATURE_VALUE_TYPE(app_list_features::kEnableZeroStateSuggestions)},
-    {"enable-zero-state-app-reinstall-suggestions",
-     flag_descriptions::kEnableAppReinstallZeroStateName,
-     flag_descriptions::kEnableAppReinstallZeroStateDescription, kOsCrOS,
-     FEATURE_VALUE_TYPE(app_list_features::kEnableAppReinstallZeroState)},
 #endif  // OS_CHROMEOS
 
     {"enable-bloated-renderer-detection",
