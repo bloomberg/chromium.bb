f9475c98e6b751e38007bd7e44d4c8abfad437cd
Anthony Cui <cuianthony@google.com>
2019-02-25 15:07:02 +0000

    Delay JNI registration for GVR
    Previously GVR was registered when Chrome starts at JNI_OnLoad. Moving
    GVR Java to the VR DFM means that this registration fails and Chrome
    crashes.
    Move the registration of GVR to RegisterJni, so that it occurs only
    after GVR is already loaded.
    
    Bug: 862706
    Change-Id: If68fafa46859eeac62a6d4e633aa1f5519048164
    Reviewed-on: https://chromium-review.googlesource.com/c/1478990
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Anthony Cui <cuianthony@google.com>
    Cr-Commit-Position: refs/heads/master@{#635111}
    Former-commit-id: 80b00c0f7130e347ce79b63665e0e51fab54d18c

--- chrome/app/android/chrome_jni_onload.cc
+++ chrome/app/android/chrome_jni_onload.cc
@@ -4,43 +4,10 @@
 
 #include "chrome/app/android/chrome_jni_onload.h"
 
-#include "base/android/jni_android.h"
-#include "base/android/jni_registrar.h"
-#include "base/android/jni_utils.h"
-#include "base/stl_util.h"
 #include "chrome/app/android/chrome_android_initializer.h"
 #include "content/public/app/content_jni_onload.h"
-#include "device/vr/buildflags/buildflags.h"
-
-#if BUILDFLAG(ENABLE_VR)
-#include "third_party/gvr-android-sdk/display_synchronizer_jni.h"
-#include "third_party/gvr-android-sdk/gvr_api_jni.h"
-#include "third_party/gvr-android-sdk/native_callbacks_jni.h"
-#endif
 
 namespace android {
-
-// These VR native functions are not handled by the automatic registration, so
-// they are manually registered here.
-#if BUILDFLAG(ENABLE_VR)
-static const base::android::RegistrationMethod kChromeRegisteredMethods[] = {
-    {"DisplaySynchronizer",
-     DisplaySynchronizer::RegisterDisplaySynchronizerNatives},
-    {"GvrApi", GvrApi::RegisterGvrApiNatives},
-    {"NativeCallbacks", NativeCallbacks::RegisterNativeCallbacksNatives},
-};
-#endif
-
-bool OnJNIOnLoadRegisterJNI(JNIEnv* env) {
-#if BUILDFLAG(ENABLE_VR)
-  // Register manually when on the browser process.
-  if (!base::android::IsSelectiveJniRegistrationEnabled(env)) {
-    return RegisterNativeMethods(env, kChromeRegisteredMethods,
-                                 base::size(kChromeRegisteredMethods));
-  }
-#endif
-  return true;
-}
 
 bool OnJNIOnLoadInit() {
   if (!content::android::OnJNIOnLoadInit())
