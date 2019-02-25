70a7bd7da98419ca9944437f5dbd161b2bd3a175
Alex Clarke <alexclarke@chromium.org>
2019-02-25 09:22:49 +0000

    Revert "Disable WorkerThreadTest.TestTaskExecutedBeforeThreadDeletion on Linux"
    This reverts commit 731021fd38b522b4ca3297e98b88fdc4494f2dc7 [formerly 122a91aff1d73e2220e6a13bd8efd772d4a66b76].
    
    Speculative revert, the underlying problem may have been fixed by https://chromium-review.googlesource.com/c/chromium/src/+/1480462
    
    Original change's description:
    > Disable WorkerThreadTest.TestTaskExecutedBeforeThreadDeletion on Linux
    > 
    > The test is flaky
    > https://test-results.appspot.com/dashboards/flakiness_dashboard.html#testType=blink_platform_unittests&tests=WorkerThreadTest.TestTaskExecutedBeforeThreadDeletion
    > 
    > Bug: 934088
    > Change-Id: I4d783da07af305a094d41f32383e16e98ca48f43
    > Reviewed-on: https://chromium-review.googlesource.com/c/1481310
    > Reviewed-by: Christos Froussios <cfroussios@chromium.org>
    > Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    > Auto-Submit: Christos Froussios <cfroussios@chromium.org>
    > Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#634589}
    
    TBR=alexclarke@chromium.org,altimin@chromium.org,cfroussios@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 934088
    Change-Id: I502e52fad830c8403d431fbe4675bb3ea9bff6c1
    Reviewed-on: https://chromium-review.googlesource.com/c/1484308
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635052}
    Former-commit-id: e92ad529b4a4444626d52752f51d3634b9fce3c0

--- third_party/blink/renderer/platform/scheduler/worker/worker_thread_unittest.cc
+++ third_party/blink/renderer/platform/scheduler/worker/worker_thread_unittest.cc
@@ -119,12 +119,7 @@ TEST_F(WorkerThreadTest, TestDefaultTask) {
   completion.Wait();
 }
 
-#if defined(OS_LINUX)
-#define MAYBE_TestTaskExecutedBeforeThreadDeletion DISABLED_TestTaskExecutedBeforeThreadDeletion
-#else
-#define MAYBE_TestTaskExecutedBeforeThreadDeletion TestTaskExecutedBeforeThreadDeletion
-#endif
-TEST_F(WorkerThreadTest, MAYBE_TestTaskExecutedBeforeThreadDeletion) {
+TEST_F(WorkerThreadTest, TestTaskExecutedBeforeThreadDeletion) {
   MockTask task;
   base::WaitableEvent completion(
       base::WaitableEvent::ResetPolicy::AUTOMATIC,
