45603fe335bfca7767b9f59d87237be16396e95e
Tommy Steimel <steimel@chromium.org>
2019-02-25 22:50:54 +0000

    [Global Media Controls] Use MPRemoteCommandCenter to receive key events
    This CL adds a new component to interface with MPRemoteCommandCenter:
    https://developer.apple.com/documentation/mediaplayer/mpremotecommandcenter?language=objc
    
    This is used to receive media key presses instead of using an event tap
    so that we avoid an issue where the OS prompts the user for extra
    privileges when the event tap is created.
    
    
    Bug: 930180
    Change-Id: I3877499a49529c88427590cc013700b2e5e985ee
    Reviewed-on: https://chromium-review.googlesource.com/c/1480702
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635295}
    Former-commit-id: 6db83a6a677a89aa8ad568d04f6771a9f6ba1a28

--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -180,6 +180,7 @@
 #include "content/browser/renderer_host/browser_compositor_view_mac.h"
 #include "content/browser/theme_helper_mac.h"
 #include "ui/accelerated_widget_mac/window_resize_helper_mac.h"
+#include "ui/base/now_playing/remote_command_center_delegate.h"
 #endif
 
 #if defined(OS_WIN)
@@ -1444,6 +1445,10 @@ int BrowserMainLoop::BrowserThreadsStarted() {
 #endif
 
   if (MediaKeysListenerManager::IsMediaKeysListenerManagerEnabled()) {
+#if defined(OS_MACOSX)
+    remote_command_center_delegate_ =
+        now_playing::RemoteCommandCenterDelegate::Create();
+#endif
     media_keys_listener_manager_ =
         std::make_unique<MediaKeysListenerManagerImpl>(
             content::ServiceManagerConnection::GetForProcess()->GetConnector());
