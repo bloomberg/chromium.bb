915cacaa6e827549bb8163c3beec17fab94d6888
Hajime Hoshi <hajimehoshi@chromium.org>
2019-02-25 09:40:05 +0000

    Use a per-frame task runner at MIDIDispatcher's mojo binding
    This CL switches task runners at several places from per-thread to
    per-frame. The purpose of this change is to make it possible to freeze
    tasks for bfcache.
    
    The actual test that executes JavaScript on the task runner is:
    webmidi/add-port.html
    
    This design change is aligned with the future plan to call mojo
    interfaces directly from MIDIAccessor and remove MIDIDispatcher.
    
    BUG=913912, 582328
    
    Change-Id: I7be3f930b98ec5e256a5ec7447e0a6111255a692
    Reviewed-on: https://chromium-review.googlesource.com/c/1478653
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635053}
    Former-commit-id: 2654b62e134dadfc8ced52d95cc9352fbe8c922e

--- third_party/blink/renderer/modules/webmidi/midi_dispatcher.cc
+++ third_party/blink/renderer/modules/webmidi/midi_dispatcher.cc
@@ -4,6 +4,8 @@
 
 #include "third_party/blink/renderer/modules/webmidi/midi_dispatcher.h"
 
+#include <utility>
+
 #include "third_party/blink/public/platform/interface_provider.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/public/platform/web_string.h"
@@ -21,54 +23,24 @@ namespace {
 static const size_t kMaxUnacknowledgedBytesSent = 10 * 1024 * 1024;  // 10 MB.
 }  // namespace
 
-MIDIDispatcher::MIDIDispatcher() : binding_(this) {}
+MIDIDispatcher::MIDIDispatcher(
+    scoped_refptr<base::SingleThreadTaskRunner> task_runner,
+    MIDIAccessor* accessor)
+    : accessor_(accessor),
+      binding_(this),
+      task_runner_(std::move(task_runner)) {
+  TRACE_EVENT0("midi", "MIDIDispatcher::MIDIDispatcher");
+  DCHECK(accessor_);
+  midi::mojom::blink::MidiSessionClientPtr client_ptr;
+  binding_.Bind(mojo::MakeRequest(&client_ptr, task_runner_), task_runner_);
+
+  Platform::Current()->GetInterfaceProvider()->GetInterface(
+      mojo::MakeRequest(&midi_session_provider_, task_runner_));
+  midi_session_provider_->StartSession(mojo::MakeRequest(&midi_session_),
+                                       std::move(client_ptr));
+}
 
 MIDIDispatcher::~MIDIDispatcher() = default;
-
-MIDIDispatcher& MIDIDispatcher::Instance() {
-  DEFINE_STATIC_LOCAL(Persistent<MIDIDispatcher>, midi_dispatcher,
-                      (MakeGarbageCollected<MIDIDispatcher>()));
-  return *midi_dispatcher;
-}
-
-void MIDIDispatcher::Trace(Visitor* visitor) {}
-
-void MIDIDispatcher::AddAccessor(MIDIAccessor* accessor) {
-  TRACE_EVENT0("midi", "MIDIDispatcher::AddAccessor");
-  accessors_waiting_session_queue_.push_back(accessor);
-  if (session_result_ != midi::mojom::Result::NOT_INITIALIZED) {
-    SessionStarted(session_result_);
-  } else if (accessors_waiting_session_queue_.size() == 1u) {
-    midi::mojom::blink::MidiSessionClientPtr client_ptr;
-    binding_.Bind(mojo::MakeRequest(&client_ptr));
-    GetMidiSessionProvider().StartSession(mojo::MakeRequest(&midi_session_),
-                                          std::move(client_ptr));
-  }
-}
-
-void MIDIDispatcher::RemoveAccessor(MIDIAccessor* accessor) {
-  // |accessor| should either be in |accessors_| or
-  // |accessors_waiting_session_queue_|, but not both.
-  DCHECK_NE(accessors_.Contains(accessor),
-            accessors_waiting_session_queue_.Contains(accessor))
-      << "RemoveAccessor call was not balanced with AddAccessor call";
-  auto** it = std::find(accessors_.begin(), accessors_.end(), accessor);
-  if (it != accessors_.end()) {
-    accessors_.erase(it);
-  } else {
-    accessors_waiting_session_queue_.erase(
-        std::find(accessors_waiting_session_queue_.begin(),
-                  accessors_waiting_session_queue_.end(), accessor));
-  }
-  if (accessors_.IsEmpty() && accessors_waiting_session_queue_.IsEmpty()) {
-    session_result_ = midi::mojom::Result::NOT_INITIALIZED;
-    inputs_.clear();
-    outputs_.clear();
-    midi_session_.reset();
-    midi_session_provider_.reset();
-    binding_.Close();
-  }
-}
 
 void MIDIDispatcher::SendMidiData(uint32_t port,
                                   const uint8_t* data,
@@ -83,25 +55,22 @@ void MIDIDispatcher::SendMidiData(uint32_t port,
   unacknowledged_bytes_sent_ += length;
   Vector<uint8_t> v;
   v.Append(data, length);
-  GetMidiSession().SendData(port, std::move(v), timestamp);
+  midi_session_->SendData(port, std::move(v), timestamp);
 }
 
 void MIDIDispatcher::AddInputPort(midi::mojom::blink::PortInfoPtr info) {
   inputs_.push_back(*info);
-  // Iterating over a copy of |accessors_| as callback could result in
-  // |accessors_| being modified. Applies to for-loops later in this file as
-  // well.
-  for (auto* accessor : AccessorList(accessors_)) {
-    accessor->DidAddInputPort(info->id, info->manufacturer, info->name,
-                              info->version, info->state);
+  if (initialized_) {
+    accessor_->DidAddInputPort(info->id, info->manufacturer, info->name,
+                               info->version, info->state);
   }
 }
 
 void MIDIDispatcher::AddOutputPort(midi::mojom::blink::PortInfoPtr info) {
   outputs_.push_back(*info);
-  for (auto* accessor : AccessorList(accessors_)) {
-    accessor->DidAddOutputPort(info->id, info->manufacturer, info->name,
-                               info->version, info->state);
+  if (initialized_) {
+    accessor_->DidAddOutputPort(info->id, info->manufacturer, info->name,
+                                info->version, info->state);
   }
 }
 
@@ -110,8 +79,8 @@ void MIDIDispatcher::SetInputPortState(uint32_t port,
   if (inputs_[port].state == state)
     return;
   inputs_[port].state = state;
-  for (auto* accessor : AccessorList(accessors_))
-    accessor->DidSetInputPortState(port, state);
+  if (initialized_)
+    accessor_->DidSetInputPortState(port, state);
 }
 
 void MIDIDispatcher::SetOutputPortState(uint32_t port,
@@ -119,34 +88,29 @@ void MIDIDispatcher::SetOutputPortState(uint32_t port,
   if (outputs_[port].state == state)
     return;
   outputs_[port].state = state;
-  for (auto* accessor : AccessorList(accessors_))
-    accessor->DidSetOutputPortState(port, state);
+  if (initialized_)
+    accessor_->DidSetOutputPortState(port, state);
 }
 
 void MIDIDispatcher::SessionStarted(midi::mojom::blink::Result result) {
   TRACE_EVENT0("midi", "MIDIDispatcher::OnSessionStarted");
-  session_result_ = result;
 
-  // A for-loop using iterators does not work because |accessor| may touch
-  // |accessors_waiting_session_queue_| in callbacks.
-  while (!accessors_waiting_session_queue_.IsEmpty()) {
-    auto* accessor = accessors_waiting_session_queue_.back();
-    accessors_waiting_session_queue_.pop_back();
-    if (result == midi::mojom::blink::Result::OK) {
-      // Add the accessor's input and output ports.
-      for (const auto& info : inputs_) {
-        accessor->DidAddInputPort(info.id, info.manufacturer, info.name,
-                                  info.version, info.state);
-      }
+  DCHECK(!initialized_);
+  initialized_ = true;
 
-      for (const auto& info : outputs_) {
-        accessor->DidAddOutputPort(info.id, info.manufacturer, info.name,
-                                   info.version, info.state);
-      }
+  if (result == midi::mojom::blink::Result::OK) {
+    // Add the accessor's input and output ports.
+    for (const auto& info : inputs_) {
+      accessor_->DidAddInputPort(info.id, info.manufacturer, info.name,
+                                 info.version, info.state);
     }
-    accessor->DidStartSession(result);
-    accessors_.push_back(accessor);
+
+    for (const auto& info : outputs_) {
+      accessor_->DidAddOutputPort(info.id, info.manufacturer, info.name,
+                                  info.version, info.state);
+    }
   }
+  accessor_->DidStartSession(result);
 }
 
 void MIDIDispatcher::AcknowledgeSentData(uint32_t bytes_sent) {
@@ -161,22 +125,8 @@ void MIDIDispatcher::DataReceived(uint32_t port,
   TRACE_EVENT0("midi", "MIDIDispatcher::DataReceived");
   DCHECK(!data.IsEmpty());
 
-  for (auto* accessor : AccessorList(accessors_))
-    accessor->DidReceiveMIDIData(port, &data[0], data.size(), timestamp);
-}
-
-midi::mojom::blink::MidiSessionProvider&
-MIDIDispatcher::GetMidiSessionProvider() {
-  if (!midi_session_provider_) {
-    Platform::Current()->GetInterfaceProvider()->GetInterface(
-        mojo::MakeRequest(&midi_session_provider_));
-  }
-  return *midi_session_provider_;
-}
-
-midi::mojom::blink::MidiSession& MIDIDispatcher::GetMidiSession() {
-  DCHECK(midi_session_);
-  return *midi_session_;
+  if (initialized_)
+    accessor_->DidReceiveMIDIData(port, &data[0], data.size(), timestamp);
 }
 
 }  // namespace blink
