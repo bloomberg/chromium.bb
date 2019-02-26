9d909ee352c3364f6e4fd9d30a8c477e96299ee0
Josh Nohle <nohle@google.com>
2019-02-26 17:35:12 +0000

    Expand ResultCodes and overload <<
    Expand CryptAuthEnrollmentResult::ResultCode enum to accommodate more
    fine-grained error codes as well as error codes relevant to the
    enrollment manager, such as kErrorGcmRegistrationFailed and
    kErrorClientAppMetadataFetchFailed.
    
    We also overload the << operator for ResultCode printing.
    
    Bug: 899080
    Change-Id: I69a3e97e6987d4e3e54444febdecde3ef40137c9
    Reviewed-on: https://chromium-review.googlesource.com/c/1488116
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635592}
    Former-commit-id: 8868adcea16e28dd3dc57880f9f063f18292879c

--- chromeos/services/device_sync/cryptauth_enrollment_result.cc
+++ chromeos/services/device_sync/cryptauth_enrollment_result.cc
@@ -8,33 +8,6 @@ namespace chromeos {
 
 namespace device_sync {
 
-// static
-CryptAuthEnrollmentResult::ResultCode
-CryptAuthEnrollmentResult::NetworkRequestErrorToResultCode(
-    NetworkRequestError error) {
-  switch (error) {
-    case NetworkRequestError::kOffline:
-      return CryptAuthEnrollmentResult::ResultCode::kNetworkRequestErrorOffline;
-    case NetworkRequestError::kEndpointNotFound:
-      return CryptAuthEnrollmentResult::ResultCode::
-          kNetworkRequestErrorEndpointNotFound;
-    case NetworkRequestError::kAuthenticationError:
-      return CryptAuthEnrollmentResult::ResultCode::
-          kNetworkRequestErrorAuthenticationError;
-    case NetworkRequestError::kBadRequest:
-      return CryptAuthEnrollmentResult::ResultCode::
-          kNetworkRequestErrorBadRequest;
-    case NetworkRequestError::kResponseMalformed:
-      return CryptAuthEnrollmentResult::ResultCode::
-          kNetworkRequestErrorResponseMalformed;
-    case NetworkRequestError::kInternalServerError:
-      return CryptAuthEnrollmentResult::ResultCode::
-          kNetworkRequestErrorInternalServerError;
-    case NetworkRequestError::kUnknown:
-      return CryptAuthEnrollmentResult::ResultCode::kNetworkRequestErrorUnknown;
-  }
-}
-
 CryptAuthEnrollmentResult::CryptAuthEnrollmentResult(
     ResultCode result_code,
     const base::Optional<cryptauthv2::ClientDirective>& client_directive)
@@ -48,6 +21,129 @@ CryptAuthEnrollmentResult::~CryptAuthEnrollmentResult() = default;
 bool CryptAuthEnrollmentResult::IsSuccess() const {
   return result_code_ == ResultCode::kSuccessNewKeysEnrolled ||
          result_code_ == ResultCode::kSuccessNoNewKeysNeeded;
+}
+
+bool CryptAuthEnrollmentResult::operator==(
+    const CryptAuthEnrollmentResult& other) const {
+  return result_code_ == other.result_code_ &&
+         client_directive_.has_value() == other.client_directive_.has_value() &&
+         (!client_directive_ ||
+          client_directive_->SerializeAsString() ==
+              other.client_directive_->SerializeAsString());
+}
+
+bool CryptAuthEnrollmentResult::operator!=(
+    const CryptAuthEnrollmentResult& other) const {
+  return !(*this == other);
+}
+
+std::ostream& operator<<(
+    std::ostream& stream,
+    const CryptAuthEnrollmentResult::ResultCode result_code) {
+  using ResultCode = CryptAuthEnrollmentResult::ResultCode;
+
+  switch (result_code) {
+    case ResultCode::kSuccessNoNewKeysNeeded:
+      stream << "[Success: No new keys needed]";
+      break;
+    case ResultCode::kSuccessNewKeysEnrolled:
+      stream << "[Success: New keys enrolled]";
+      break;
+    case ResultCode::kErrorSyncKeysApiCallOffline:
+      stream << "[SyncKeys API call failed: Offline]";
+      break;
+    case ResultCode::kErrorSyncKeysApiCallEndpointNotFound:
+      stream << "[SyncKeys API call failed: Endpoint not found]";
+      break;
+    case ResultCode::kErrorSyncKeysApiCallAuthenticationError:
+      stream << "[SyncKeys API call failed: Authentication error]";
+      break;
+    case ResultCode::kErrorSyncKeysApiCallBadRequest:
+      stream << "[SyncKeys API call failed: Bad request]";
+      break;
+    case ResultCode::kErrorSyncKeysApiCallResponseMalformed:
+      stream << "[SyncKeys API call failed: Response malformed]";
+      break;
+    case ResultCode::kErrorSyncKeysApiCallInternalServerError:
+      stream << "[SyncKeys API call failed: Internal server error]";
+      break;
+    case ResultCode::kErrorSyncKeysApiCallUnknownError:
+      stream << "[SyncKeys API call failed: Unknown error]";
+      break;
+    case ResultCode::kErrorEnrollKeysApiCallOffline:
+      stream << "[EnrollKeys API call failed: Offline]";
+      break;
+    case ResultCode::kErrorEnrollKeysApiCallEndpointNotFound:
+      stream << "[EnrollKeys API call failed: Endpoint not found]";
+      break;
+    case ResultCode::kErrorEnrollKeysApiCallAuthenticationError:
+      stream << "[EnrollKeys API call failed: Authentication error]";
+      break;
+    case ResultCode::kErrorEnrollKeysApiCallBadRequest:
+      stream << "[EnrollKeys API call failed: Bad request]";
+      break;
+    case ResultCode::kErrorEnrollKeysApiCallResponseMalformed:
+      stream << "[EnrollKeys API call failed: Response malformed]";
+      break;
+    case ResultCode::kErrorEnrollKeysApiCallInternalServerError:
+      stream << "[EnrollKeys API call failed: Internal server error]";
+      break;
+    case ResultCode::kErrorEnrollKeysApiCallUnknownError:
+      stream << "[EnrollKeys API call failed: Unknown error]";
+      break;
+    case ResultCode::kErrorCryptAuthServerOverloaded:
+      stream << "[Error: CryptAuth server overloaded]";
+      break;
+    case ResultCode::kErrorSyncKeysResponseMissingRandomSessionId:
+      stream << "[Error: SyncKeysResponse missing session ID]";
+      break;
+    case ResultCode::kErrorSyncKeysResponseInvalidClientDirective:
+      stream << "[Error: Missing/invalid ClientDirective in SyncKeysResponse]";
+      break;
+    case ResultCode::kErrorWrongNumberOfSyncSingleKeyResponses:
+      stream << "[Error: Wrong number of SyncSingleKeyResponses]";
+      break;
+    case ResultCode::kErrorWrongNumberOfKeyActions:
+      stream << "[Error: Wrong number of key actions]";
+      break;
+    case ResultCode::kErrorInvalidKeyActionEnumValue:
+      stream << "[Error: Invalid KeyAction enum value]";
+      break;
+    case ResultCode::kErrorKeyActionsSpecifyMultipleActiveKeys:
+      stream << "[Error: KeyActions specify multiple active keys]";
+      break;
+    case ResultCode::kErrorKeyActionsDoNotSpecifyAnActiveKey:
+      stream << "[Error: KeyActions do not specify an active key]";
+      break;
+    case ResultCode::kErrorKeyCreationKeyTypeNotSupported:
+      stream << "[Error: KeyCreation instructions specify unsupported KeyType]";
+      break;
+    case ResultCode::kErrorSymmetricKeyCreationMissingServerDiffieHellman:
+      stream << "[Error: Cannot create symmetric key; missing server "
+             << "Diffie-Hellman key]";
+      break;
+    case ResultCode::kErrorTimeoutWaitingForSyncKeysResponse:
+      stream << "[Error: Timeout waiting for SyncKeys response]";
+      break;
+    case ResultCode::kErrorTimeoutWaitingForKeyCreation:
+      stream << "[Error: Timeout waiting for key creation]";
+      break;
+    case ResultCode::kErrorTimeoutWaitingForKeyProofComputation:
+      stream << "[Error: Timeout waiting for key proof computation]";
+      break;
+    case ResultCode::kErrorTimeoutWaitingForEnrollKeysResponse:
+      stream << "[Error: Timeout waiting for EnrollKeys response]";
+      break;
+    case ResultCode::kErrorGcmRegistrationFailed:
+      stream << "[Error: GCM registration failed]";
+      break;
+    case ResultCode::kErrorClientAppMetadataFetchFailed:
+      stream << "[Error: Could not retrieve ClientAppMetadata from "
+             << "ClientAppMetadataProvider]";
+      break;
+  }
+
+  return stream;
 }
 
 }  // namespace device_sync
