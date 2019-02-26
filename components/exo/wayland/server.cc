c143d6d946c96f78e40254ea873024cc12c4643c
Daniel Nicoara <dnicoara@chromium.org>
2019-02-26 14:58:45 +0000

    EXO: Refactor zcr_alpha_compositing interface
    Move the zcr_alpha_compositing interface into its own files. This
    change only moves code around, no functional changes.
    
    Bug: 896710
    Test: Build and run exo_unittests
    Change-Id: I6b576d9e686c8634dfa2a30c3718f9068628a9ea
    Reviewed-on: https://chromium-review.googlesource.com/c/1481733
    Commit-Queue: Daniel Nicoara <dnicoara@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635519}
    Former-commit-id: c583b47f277e2d0002cfafe812796290ab45a0ab

--- components/exo/wayland/server.cc
+++ components/exo/wayland/server.cc
@@ -77,6 +77,7 @@
 #include "components/exo/wayland/wl_subcompositor.h"
 #include "components/exo/wayland/wp_presentation.h"
 #include "components/exo/wayland/wp_viewporter.h"
+#include "components/exo/wayland/zcr_alpha_compositing.h"
 #include "components/exo/wayland/zcr_secure_output.h"
 #include "components/exo/wayland/zcr_vsync_feedback.h"
 #include "components/exo/wm_helper.h"
@@ -157,127 +158,6 @@ const base::FilePath::CharType kSocketName[] = FILE_PATH_LITERAL("wayland-0");
 
 // Group used for wayland socket.
 const char kWaylandSocketGroup[] = "wayland";
-
-// A property key containing a boolean set to true if a blending object is
-// associated with surface object.
-DEFINE_UI_CLASS_PROPERTY_KEY(bool, kSurfaceHasBlendingKey, false)
-
-////////////////////////////////////////////////////////////////////////////////
-// blending_interface:
-
-// Implements the blending interface to a Surface. The "blend mode" and
-// "alpha"-state is set to SrcOver and 1 upon destruction. A window property
-// will be set during the lifetime of this class to prevent multiple instances
-// from being created for the same Surface.
-class Blending : public SurfaceObserver {
- public:
-  explicit Blending(Surface* surface) : surface_(surface) {
-    surface_->AddSurfaceObserver(this);
-    surface_->SetProperty(kSurfaceHasBlendingKey, true);
-  }
-  ~Blending() override {
-    if (surface_) {
-      surface_->RemoveSurfaceObserver(this);
-      surface_->SetBlendMode(SkBlendMode::kSrcOver);
-      surface_->SetAlpha(1.0f);
-      surface_->SetProperty(kSurfaceHasBlendingKey, false);
-    }
-  }
-
-  void SetBlendMode(SkBlendMode blend_mode) {
-    if (surface_)
-      surface_->SetBlendMode(blend_mode);
-  }
-
-  void SetAlpha(float value) {
-    if (surface_)
-      surface_->SetAlpha(value);
-  }
-
-  // Overridden from SurfaceObserver:
-  void OnSurfaceDestroying(Surface* surface) override {
-    surface->RemoveSurfaceObserver(this);
-    surface_ = nullptr;
-  }
-
- private:
-  Surface* surface_;
-
-  DISALLOW_COPY_AND_ASSIGN(Blending);
-};
-
-void blending_destroy(wl_client* client, wl_resource* resource) {
-  wl_resource_destroy(resource);
-}
-
-void blending_set_blending(wl_client* client,
-                           wl_resource* resource,
-                           uint32_t equation) {
-  switch (equation) {
-    case ZCR_BLENDING_V1_BLENDING_EQUATION_NONE:
-      GetUserDataAs<Blending>(resource)->SetBlendMode(SkBlendMode::kSrc);
-      break;
-    case ZCR_BLENDING_V1_BLENDING_EQUATION_PREMULT:
-      GetUserDataAs<Blending>(resource)->SetBlendMode(SkBlendMode::kSrcOver);
-      break;
-    case ZCR_BLENDING_V1_BLENDING_EQUATION_COVERAGE:
-      NOTIMPLEMENTED();
-      break;
-    default:
-      DLOG(WARNING) << "Unsupported blending equation: " << equation;
-      break;
-  }
-}
-
-void blending_set_alpha(wl_client* client,
-                        wl_resource* resource,
-                        wl_fixed_t alpha) {
-  GetUserDataAs<Blending>(resource)->SetAlpha(wl_fixed_to_double(alpha));
-}
-
-const struct zcr_blending_v1_interface blending_implementation = {
-    blending_destroy, blending_set_blending, blending_set_alpha};
-
-////////////////////////////////////////////////////////////////////////////////
-// alpha_compositing_interface:
-
-void alpha_compositing_destroy(wl_client* client, wl_resource* resource) {
-  wl_resource_destroy(resource);
-}
-
-void alpha_compositing_get_blending(wl_client* client,
-                                    wl_resource* resource,
-                                    uint32_t id,
-                                    wl_resource* surface_resource) {
-  Surface* surface = GetUserDataAs<Surface>(surface_resource);
-  if (surface->GetProperty(kSurfaceHasBlendingKey)) {
-    wl_resource_post_error(resource,
-                           ZCR_ALPHA_COMPOSITING_V1_ERROR_BLENDING_EXISTS,
-                           "a blending object for that surface already exists");
-    return;
-  }
-
-  wl_resource* blending_resource =
-      wl_resource_create(client, &zcr_blending_v1_interface, 1, id);
-
-  SetImplementation(blending_resource, &blending_implementation,
-                    std::make_unique<Blending>(surface));
-}
-
-const struct zcr_alpha_compositing_v1_interface
-    alpha_compositing_implementation = {alpha_compositing_destroy,
-                                        alpha_compositing_get_blending};
-
-void bind_alpha_compositing(wl_client* client,
-                            void* data,
-                            uint32_t version,
-                            uint32_t id) {
-  wl_resource* resource =
-      wl_resource_create(client, &zcr_alpha_compositing_v1_interface, 1, id);
-
-  wl_resource_set_implementation(resource, &alpha_compositing_implementation,
-                                 data, nullptr);
-}
 
 ////////////////////////////////////////////////////////////////////////////////
 // touch_stylus interface:
