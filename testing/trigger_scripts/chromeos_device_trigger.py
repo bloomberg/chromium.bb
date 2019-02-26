7575d9ab55eecb5aba5dc28d8eca17be747320bb
Ben Pastene <bpastene@chromium.org>
2019-02-26 18:22:01 +0000

    Fix chromeos device trigger script when shards > 1.
    Change-Id: I1c2903e46e002a5a0f3bd3507563b56d66b6b7ea
    Reviewed-on: https://chromium-review.googlesource.com/c/1484589
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635608}
    Former-commit-id: 789bdca806065bfc1d318d465033797670a36d36

--- testing/trigger_scripts/chromeos_device_trigger.py
+++ testing/trigger_scripts/chromeos_device_trigger.py
@@ -49,7 +49,7 @@ def read_current_lkgm():
   return lkgm.split('.')[0]
 
 
-def parse_args():
+def parse_args(triggerer):
   # This script will do nothing but inspect and tweak the dimension args to
   # `swarming.py trigger`. So let's pull just those out.
   parser = argparse.ArgumentParser(description=__doc__)
@@ -68,7 +68,12 @@ def parse_args():
       '--primary-expiration', type=int, default=600,
       help='How long to wait (in seconds) for an available bot in the primary '
            'task slice.')
+  # BaseTestTriggerer's setup_parser_contract() takes care of adding needed
+  # swarming.py args if they're not already present.
+  base_test_triggerer.BaseTestTriggerer.setup_parser_contract(parser)
   args, additional_args = parser.parse_known_args()
+  additional_args = triggerer.modify_args(
+      additional_args, 0, args.shard_index, args.shards, args.dump_json)
 
   if additional_args[0] != 'trigger':
     parser.error(
@@ -91,7 +96,8 @@ def parse_args():
 
 
 def main():
-  args, additional_args = parse_args()
+  triggerer = base_test_triggerer.BaseTestTriggerer()
+  args, additional_args = parse_args(triggerer)
 
   current_lkgm = read_current_lkgm()
   if not current_lkgm:
@@ -120,7 +126,7 @@ def main():
   ])
   new_args += additional_args[1:]
 
-  return base_test_triggerer.BaseTestTriggerer().run_swarming(new_args, True)
+  return triggerer.run_swarming(new_args, True)
 
 
 if __name__ == '__main__':
