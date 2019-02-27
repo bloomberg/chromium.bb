3149b0ac93b89a4b3fd6e7451b1fd53bc98d5a4c
Paul Dyson <pdyson@chromium.org>
2019-02-27 02:19:41 +0000

    Add more click metrics for app launches.
    * Add more metrics to UKM event AppListAppLaunch.
    * Add a new UKM metric AppListAppClickData to send the
    accumulated click data for an app. On an app launch click,
    an AppListAppClickData event is sent for the app
    clicked on and for five other apps chosen at random.
    
    Also makes user_activity_ukm_logger_helpers a public_dep so
    it can be used here.
    
    Bug: 899123
    Change-Id: Ie1d6f17673b9548ef6823d05a362fea8992126b9
    Reviewed-on: https://chromium-review.googlesource.com/c/1460750
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Commit-Queue: Paul Dyson <pdyson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635827}
    Former-commit-id: 3cced99b05d4bc69b98f66acdfef0030ee380d99

--- chrome/browser/ui/app_list/app_launch_event_logger.h
+++ chrome/browser/ui/app_list/app_launch_event_logger.h
@@ -5,6 +5,8 @@
 #ifndef CHROME_BROWSER_UI_APP_LIST_APP_LAUNCH_EVENT_LOGGER_H_
 #define CHROME_BROWSER_UI_APP_LIST_APP_LAUNCH_EVENT_LOGGER_H_
 
+#include <memory>
+#include <string>
 #include <vector>
 
 #include "base/containers/flat_map.h"
@@ -14,6 +16,21 @@
 #include "base/values.h"
 #include "chrome/browser/ui/app_list/app_launch_event_logger.pb.h"
 #include "extensions/browser/extension_registry.h"
+#include "services/metrics/public/cpp/ukm_source_id.h"
+
+namespace chromeos {
+namespace power {
+namespace ml {
+class RecentEventsCounter;
+}  // namespace ml
+}  // namespace power
+}  // namespace chromeos
+
+namespace ukm {
+namespace builders {
+class AppListAppClickData;
+}  // namespace builders
+}  // namespace ukm
 
 namespace app_list {
 
@@ -24,15 +41,19 @@ namespace app_list {
 // apps the keys are based upon the app id, for Arc apps the keys are based upon
 // a hash of the package name and for PWAs the keys are the urls associated with
 // the PWA.
+// At the time of app launch this class logs metrics about the app clicked on
+// and another five apps that were not clicked on, chosen at random.
 class AppLaunchEventLogger {
  public:
   AppLaunchEventLogger();
   ~AppLaunchEventLogger();
   // Processes a click on an app in the suggestion chip and logs the resulting
-  // metrics in UKM.
+  // metrics in UKM. This method calls EnforceLoggingPolicy() to ensure the
+  // logging policy is complied with.
   void OnSuggestionChipClicked(const std::string& id, int suggestion_index);
   // Processes a click on an app located in the grid of apps in the launcher and
-  // logs the resulting metrics in UKM.
+  // logs the resulting metrics in UKM. This method calls EnforceLoggingPolicy()
+  // to ensure the logging policy is complied with.
   void OnGridClicked(const std::string& id);
   // Provides values to be used when testing.
   void SetAppDataForTesting(extensions::ExtensionRegistry* registry,
@@ -43,7 +64,7 @@ class AppLaunchEventLogger {
   static const char kShouldSync[];
 
  private:
-  // Remove any leading "chrome-extension://" or "arc://". Also remove any
+  // Removes any leading "chrome-extension://" or "arc://". Also remove any
   // trailing "/".
   std::string RemoveScheme(const std::string& id);
   // Creates the mapping from PWA app id to PWA url. This mapping also acts as a
@@ -52,18 +73,33 @@ class AppLaunchEventLogger {
   // Gets the PWA url from its app id. Returns base::EmptyString() if no match
   // found.
   const std::string& GetPwaUrl(const std::string& id);
-  // Sets the |event|'s app type based on the id. Also sets the url for PWA apps
-  // and package name for Arc apps.
-  // Logging policy is enforced here by only assigning a meaningful app type to
-  // apps that are allowed to be logged. An app type of OTHER is assigned to
-  // apps that cannot be logged by policy.
-  void SetAppInfo(AppLaunchEvent* event);
-  // Loads the information used to determine which apps can be logged.
-  void LoadInstalledAppInformation();
-  // Gets the Arc package name for synced apps. Returns base::EmptyString() if
-  // app not being synced or app id not found.
-  std::string GetSyncedArcPackage(const std::string& id);
-  bool IsChromeAppFromWebstore(const std::string&);
+  // Marks app as ok for policy compliance. If the app is not in
+  // |app_features_map_| then add it.
+  void OkApp(AppLaunchEvent_AppType app_type,
+             const std::string& app_id,
+             const std::string& arc_package_name,
+             const std::string& pwa_url);
+
+  // Enforces logging policy, ensuring that the |app_features_map_| only
+  // contains apps that are allowed to be logged. All apps are rechecked in case
+  // they have been uninstalled since the previous check.
+  void EnforceLoggingPolicy();
+  // Updates the app data following a click.
+  void ProcessClick(const AppLaunchEvent& event, const base::Time& now);
+  // Returns a source id. |arc_package_name| is only required for Arc apps,
+  // |pwa_url| is only required for PWA apps.
+  ukm::SourceId GetSourceId(AppLaunchEvent_AppType app_type,
+                            const std::string& app_id,
+                            const std::string& arc_package_name,
+                            const std::string& pwa_url);
+  // Randomly chooses up to five apps to log, plus the app clicked on.
+  std::vector<std::string> ChooseAppsToLog(const std::string clicked_app_id);
+  // Records a UMA histogram of the app type clicked on.
+  void RecordAppTypeClicked(AppLaunchEvent_AppType app_type);
+  // Helper function to log the clicks each hour metrics.
+  void LogClicksEachHour(
+      const AppLaunchFeatures& app_launch_features,
+      ukm::builders::AppListAppClickData* const app_click_data);
   // Logs the app click using UKM.
   void Log(AppLaunchEvent app_launch_event);
 
@@ -77,6 +113,31 @@ class AppLaunchEventLogger {
   const base::DictionaryValue* arc_packages_;
   // The Chrome extension registry.
   extensions::ExtensionRegistry* registry_;
+  // A map from app id to features. Only contains apps satisfying logging
+  // policy.
+  base::flat_map<std::string, AppLaunchFeatures> app_features_map_;
+  // A map from app id to a counter of the number of clicks in the last hour.
+  // Has a time resolution one minute.
+  base::flat_map<std::string,
+                 std::unique_ptr<chromeos::power::ml::RecentEventsCounter>>
+      app_clicks_last_hour_;
+  // A map from app id to a counter of the number of clicks in the last 24
+  // hours. Has a time resolution of 15 minutes.
+  base::flat_map<std::string,
+                 std::unique_ptr<chromeos::power::ml::RecentEventsCounter>>
+      app_clicks_last_24_hours_;
+
+  // The time this class was instantiated. Allows duration to be calculated.
+  base::Time start_time_;
+  // A counter for the click in the last hour. Has a time resolution of 1
+  // minute.
+  const std::unique_ptr<chromeos::power::ml::RecentEventsCounter>
+      all_clicks_last_hour_;
+  // A counter for the clicks in the last 24 hours. Has a time resolution of 15
+  // minutes.
+  const std::unique_ptr<chromeos::power::ml::RecentEventsCounter>
+      all_clicks_last_24_hours_;
+
   // Used to prevent overwriting of parameters that are set for tests.
   bool testing_ = false;
 
