baad19f23b908839f8480a8986498d2bed9e2065
John Chen <johnchen@chromium.org>
2019-02-27 04:41:52 +0000

    Reland "[ChromeDriver] Remove unused shadow DOM object cache"
    This is a reland of b63f52ab5b5613d1622739b4b487b07c72b7cc2a [formerly 4fe92e6c3e8e265712fd29eb9814ca4af79273ab]
    
    Original change's description:
    > [ChromeDriver] Remove unused shadow DOM object cache
    >
    > ChromeDriver uses object caches for use while calling JavaScript
    > functions. The data structure was designed to allow a separate cache for
    > each shadow DOM, but that feature was never used, and objects in shadow
    > DOMs have always been stored in the main page cache. This CL removes
    > unused code, and resolves differences between test expectancies and
    > actual code behavior. It prepares for future changes to make the object
    > cache standard compliant.
    >
    > Bug: chromedriver:1461
    > Change-Id: I10fcd94a7fd2359cc43fe6bc7241d36647e9d744
    > Reviewed-on: https://chromium-review.googlesource.com/c/1485011
    > Commit-Queue: John Chen <johnchen@chromium.org>
    > Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#635754}
    
    Bug: chromedriver:1461 chromedriver:2794
    Change-Id: Iedd6b7e43eb256d65bf71fec7e66454ceb0f776c
    Reviewed-on: https://chromium-review.googlesource.com/c/1490855
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#635876}
    Former-commit-id: 2ba63532ee73a52d857ee89acc448f3821914277

--- chrome/test/chromedriver/js/call_function.js
+++ chrome/test/chromedriver/js/call_function.js
@@ -232,6 +232,7 @@ function getNodeRootThroughAnyShadows(node) {
 function getPageCache(opt_doc, opt_w3c) {
   var doc = opt_doc || document;
   var w3c = opt_w3c || false;
+  // |key| is a long random string, unlikely to conflict with anything else.
   var key = '$cdc_asdjflasutopfhvcZLmcfl_';
   if (w3c) {
     if (!(key in doc))
@@ -323,8 +324,6 @@ function unwrap(value, cache) {
  * between cached object reference IDs and actual JS objects. The cache will
  * automatically be pruned each call to remove stale references.
  *
- * @param  {Array<string>} shadowHostIds The host ids of the nested shadow
- *     DOMs the function should be executed in the context of.
  * @param {function(...[*]) : *} func The function to invoke.
  * @param {!Array<*>} args The array of arguments to supply to the function,
  *     which will be unwrapped before invoking the function.
@@ -336,7 +335,7 @@ function unwrap(value, cache) {
  *     unwrapped return was specified, this will be the function's pure return
  *     value.
  */
-function callFunction(shadowHostIds, func, args, w3c, opt_unwrappedReturn) {
+function callFunction(func, args, w3c, opt_unwrappedReturn) {
   if (w3c) {
     w3cEnabled = true;
     ELEMENT_KEY = 'element-6066-11e4-a52e-4f735466cecf';
@@ -344,15 +343,6 @@ function callFunction(shadowHostIds, func, args, w3c, opt_unwrappedReturn) {
   }
   var cache = getPageCache(null, w3cEnabled);
   cache.clearStale();
-  if (shadowHostIds && SHADOW_DOM_ENABLED) {
-    for (var i = 0; i < shadowHostIds.length; i++) {
-      var host = cache.retrieveItem(shadowHostIds[i]);
-      // TODO(zachconrad): Use the olderShadowRoot API when available to check
-      // all of the shadow roots.
-      cache = getPageCache(host.webkitShadowRoot, w3cEnabled);
-      cache.clearStale();
-    }
-  }
 
   if (opt_unwrappedReturn)
     return func.apply(null, unwrap(args, cache));
