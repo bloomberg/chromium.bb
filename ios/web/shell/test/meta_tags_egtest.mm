b9645ae41c3f79c607d4e7fcff8c986844c7a7c5
Lindsay Pasricha <lindsayw@google.com>
2019-02-28 18:09:32 +0000

    [ios] Convert web view shell EarlGrey matchers to ObjC
    We need to port matchers to ObjC in order to be accessible in EG2 to
    the Test and the App processes. Also created
    shell_matchers_shorthand.h/mm to minimize the changes to call sites.
    
    Bug: 930859
    Change-Id: Ibff34f59962bac87fa50d48c1393451b63955271
    Reviewed-on: https://chromium-review.googlesource.com/c/1494896
    Commit-Queue: Lindsay Pasricha <lindsayw@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636472}
    Former-commit-id: e035cc778c016886192b6d0257a8317b00c896dd

--- ios/web/shell/test/meta_tags_egtest.mm
+++ ios/web/shell/test/meta_tags_egtest.mm
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import <EarlGrey/EarlGrey.h>
+
 #include <map>
 
 #include "base/strings/stringprintf.h"
@@ -10,6 +12,7 @@
 #include "ios/web/public/test/http_server/http_server_util.h"
 #import "ios/web/shell/test/earl_grey/shell_earl_grey.h"
 #import "ios/web/shell/test/earl_grey/shell_matchers.h"
+#import "ios/web/shell/test/earl_grey/shell_matchers_shorthand.h"
 #import "ios/web/shell/test/earl_grey/web_shell_test_case.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
