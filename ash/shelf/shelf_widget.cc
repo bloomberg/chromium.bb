bce5b0536e46d68c9d6e8da201a190d92f3d4ea9
Manu Cornet <manucornet@chromium.org>
2019-02-28 23:32:54 +0000

    CrOS Shelf: fix login shelf hit test rects in RTL
    Bug: 933044
    Change-Id: Ibeb706b1ea2aa91a4ddd6dd83bff35cb05139f4c
    Reviewed-on: https://chromium-review.googlesource.com/c/1492039
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Manu Cornet <manucornet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636607}
    Former-commit-id: 4f4df5f4cb6b79300f09018473d8166e23143c11

--- ash/shelf/shelf_widget.cc
+++ ash/shelf/shelf_widget.cc
@@ -295,7 +295,8 @@ bool ShelfWidget::GetHitTestRects(aura::Window* target,
   // of the shelf.
   DCHECK(login_shelf_view_->visible());
   gfx::Rect login_view_button_bounds =
-      login_shelf_view_->get_button_union_bounds();
+      login_shelf_view_->ConvertRectToWidget(login_shelf_view_->GetMirroredRect(
+          login_shelf_view_->get_button_union_bounds()));
   aura::Window* source = login_shelf_view_->GetWidget()->GetNativeWindow();
   aura::Window::ConvertRectToTarget(source, target->parent(),
                                     &login_view_button_bounds);
