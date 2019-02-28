f8d0fc4aad7d4f603478eb9c7690f85d2e97115b
Ryan Sturm <ryansturm@chromium.org>
2019-02-28 01:32:29 +0000

    Adding an about:flag for URL Loader HTTPS server previews
    This adds an about://flag entry for the new way of loading an HTTPS
    server preview using the network service.
    
    Bug: 921736
    Change-Id: Ib9c419abdcc09d1700a98854d7e3863f7b4edbc0
    Reviewed-on: https://chromium-review.googlesource.com/c/1490442
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636231}
    Former-commit-id: 5e4d442e376683743fa7e53afad11eaf65ec145e

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -399,6 +399,9 @@ extern const char kEnablePreviewsAndroidOmniboxUIDescription[];
 extern const char kEnableLitePageServerPreviewsName[];
 extern const char kEnableLitePageServerPreviewsDescription[];
 
+extern const char kEnableURLLoaderLitePageServerPreviewsName[];
+extern const char kEnableURLLoaderLitePageServerPreviewsDescription[];
+
 extern const char kLayeredAPIName[];
 extern const char kLayeredAPIDescription[];
 
