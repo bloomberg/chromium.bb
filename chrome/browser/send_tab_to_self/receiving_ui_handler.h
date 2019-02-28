fc18aeef4470b60fbd96d369539ccae012871220
Jonathan Metzman <metzman@chromium.org>
2019-02-28 17:52:57 +0000

    Make unimplemented virtual methods in ReceivingUiHandler pure
    Speculative fix for UBSAN vptr build breakage.
    
    Bug: 936859
    Change-Id: I8db59ddf0cc6bfc9b1bb6d15287cb6bd2eaafa42
    Reviewed-on: https://chromium-review.googlesource.com/c/1494977
    Reviewed-by: Max Moroz <mmoroz@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636462}
    Former-commit-id: b44709330ca60e36c0fadf3e25039dcd70af4864

--- chrome/browser/send_tab_to_self/receiving_ui_handler.h
+++ chrome/browser/send_tab_to_self/receiving_ui_handler.h
@@ -23,13 +23,13 @@ class ReceivingUiHandler {
 
   // Display the new entry passed in as an argument. The entry is owned by the
   // model and should not be modified.
-  virtual void DisplayNewEntry(const SendTabToSelfEntry* entry);
+  virtual void DisplayNewEntry(const SendTabToSelfEntry* entry) = 0;
   // Dismiss any UI associated with this entry.
   // Entry object is owned by the the model and should not be
   // modified by any implementors of this class.
   // TODO(crbug.com/935719): Figure out whether we need to pass the entire entry
   // or we can pass a smaller subset of information.
-  virtual void DismissEntry(const SendTabToSelfEntry* entry);
+  virtual void DismissEntry(const SendTabToSelfEntry* entry) = 0;
 };
 
 }  // namespace send_tab_to_self
