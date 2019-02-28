5af1284e95d6d0f0dcfaf684e1ca7c25b11c8dea
Christian Egli <christian.egli@sbs.ch>
2019-02-28 14:01:30 +0100

    Issue a warning if the locale opcode is used

--- liblouis/compileTranslationTable.c
+++ liblouis/compileTranslationTable.c
@@ -2507,6 +2507,8 @@ doOpcode:
 		break;
 	}
 	case CTO_Locale:
+		compileWarning(nested,
+				"The locale opcode is not implemented. Use the locale meta data instead.");
 		break;
 	case CTO_Undefined:
 		tmp_offset = (*table)->undefined;
