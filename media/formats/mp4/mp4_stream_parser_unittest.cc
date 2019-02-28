4bf3063aea66bf0189177ee3afdfbf3eced25001
Dale Curtis <dalecurtis@chromium.org>
2019-02-28 02:41:50 +0000

    Add ID3v2 metadata parsing for Shaka packager detection.
    This will help understand how frequently Shaka packager is used in
    the wild. If usage is high enough we may add a UKM to figure out
    whom it's most popular with.
    
    BUG=927952
    
    Change-Id: I144fa524fd2450e5fdd31f9baa24a9bc7b6bfbee
    Reviewed-on: https://chromium-review.googlesource.com/c/1464921
    Commit-Queue: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636273}
    Former-commit-id: c4328744d3f6da346720176196a43c3595796a09

--- media/formats/mp4/mp4_stream_parser_unittest.cc
+++ media/formats/mp4/mp4_stream_parser_unittest.cc
@@ -16,6 +16,7 @@
 #include "base/bind_helpers.h"
 #include "base/logging.h"
 #include "base/memory/ref_counted.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
 #include "media/base/audio_decoder_config.h"
@@ -256,6 +257,35 @@ TEST_F(MP4StreamParserTest, UnalignedAppend) {
   // incremental append system)
   InitializeParser();
   ParseMP4File("bear-1280x720-av_frag.mp4", 512);
+}
+
+constexpr char kShakaPackagerUMA[] = "Media.MSE.DetectedShakaPackagerInMp4";
+
+TEST_F(MP4StreamParserTest, DidNotUseShakaPackager) {
+  // Encrypted files have non-zero duration and are treated as recorded streams.
+  auto params = GetDefaultInitParametersExpectations();
+  params.duration = base::TimeDelta::FromMicroseconds(2736066);
+  params.liveness = DemuxerStream::LIVENESS_RECORDED;
+  params.detected_audio_track_count = 0;
+  InitializeParserWithInitParametersExpectations(params);
+
+  base::HistogramTester tester;
+
+  // Test file has ID32 box, but no shaka player metadata.
+  ParseMP4File("bear-640x360-v_frag-cenc-senc-no-saiz-saio.mp4", 512);
+  tester.ExpectUniqueSample(kShakaPackagerUMA, 0, 1);
+}
+
+TEST_F(MP4StreamParserTest, UsedShakaPackager) {
+  auto params = GetDefaultInitParametersExpectations();
+  params.duration = base::TimeDelta::FromMicroseconds(2736000);
+  params.liveness = DemuxerStream::LIVENESS_RECORDED;
+  params.detected_audio_track_count = 0;
+  InitializeParserWithInitParametersExpectations(params);
+
+  base::HistogramTester tester;
+  ParseMP4File("bear-320x240-v_frag-vp9.mp4", 512);
+  tester.ExpectUniqueSample(kShakaPackagerUMA, 1, 1);
 }
 
 TEST_F(MP4StreamParserTest, BytewiseAppend) {
