570bf251cdae716c0fb5ec3ea1c25b447fa369ba
rbpotter <rbpotter@chromium.org>
2019-03-01 04:30:31 +0000

    Refactor Print Preview tests: MediaSize, Dpi
    SetMediaSize and SetDpi are moved with small modifications to
    DpiSettingsSectionTest.UpdateFromSetting and
    MediaSizeSettingsSectionTest.UpdateFromSetting
    
    Since setting the setting based on the dropdown is actually the
    responsibility of the settings-select element, this aspect of the test
    is covered by a new SettingsSelect.SetSetting test.
    
    CustomMediaSize test is moved to:
    MediaSizeSettingsSectionTest.SettingsSelect
    
    Adding new
    DpiSettingsSectionTest.SettingsSelect
    (similar to media size test above).
    
    Change-Id: I7d6fe6b382b0fb5ac7ddb90c52bc3272425cf380
    Reviewed-on: https://chromium-review.googlesource.com/c/1493373
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Esmael El-Moslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636700}
    Former-commit-id: ca7a86662c3876331eaba5fcc62cb05a2c617b9f

--- chrome/test/data/webui/print_preview/settings_select_test.js
+++ chrome/test/data/webui/print_preview/settings_select_test.js
@@ -3,13 +3,7 @@
 // found in the LICENSE file.
 
 cr.define('settings_select_test', function() {
-  /** @enum {string} */
-  const TestNames = {
-    CustomMediaNames: 'custom media names',
-  };
-
-  const suiteName = 'SettingsSelectTest';
-  suite(suiteName, function() {
+  suite('SettingsSelectTest', function() {
     let settingsSelect = null;
 
     /** @override */
@@ -21,7 +15,7 @@ cr.define('settings_select_test', function() {
     });
 
     // Test that destinations are correctly displayed in the lists.
-    test(assert(TestNames.CustomMediaNames), function() {
+    test('custom media names', function() {
       // Set a capability with custom paper sizes.
       settingsSelect.settingName = 'mediaSize';
       settingsSelect.capability =
@@ -41,10 +35,56 @@ cr.define('settings_select_test', function() {
           customLocalizedMediaName, select.options[0].textContent.trim());
       assertEquals(customMediaName, select.options[1].textContent.trim());
     });
-  });
 
-  return {
-    suiteName: suiteName,
-    TestNames: TestNames,
-  };
+    test('set setting', async () => {
+      // Fake setting.
+      settingsSelect.settings = {
+        fruit: {
+          value: {},
+          unavailableValue: {},
+          valid: true,
+          available: true,
+          setByPolicy: false,
+          key: 'fruit',
+        },
+      };
+      settingsSelect.settingName = 'fruit';
+      settingsSelect.capability = {
+        option: [
+          {name: 'lime', color: 'green', size: 3},
+          {name: 'orange', color: 'orange', size: 5, is_default: true},
+        ],
+      };
+      Polymer.dom.flush();
+      const option0 = JSON.stringify(settingsSelect.capability.option[0]);
+      const option1 = JSON.stringify(settingsSelect.capability.option[1]);
+      const select = settingsSelect.$$('select');
+
+      // Normally done for initialization by the model and parent section.
+      settingsSelect.set(
+          'settings.fruit.value', settingsSelect.capability.option[0]);
+      settingsSelect.selectValue(option1);
+
+      // Verify that the selected option and names are as expected.
+      assertEquals(2, select.options.length);
+      assertEquals(1, select.selectedIndex);
+      assertEquals('lime', select.options[0].textContent.trim());
+      assertEquals('orange', select.options[1].textContent.trim());
+      assertEquals(option0, select.options[0].value);
+      assertEquals(option1, select.options[1].value);
+
+      // Verify that selecting an new option in the dropdown sets the setting.
+      await print_preview_test_utils.selectOption(settingsSelect, option0);
+      assertEquals(
+          option0, JSON.stringify(settingsSelect.getSettingValue('fruit')));
+      assertEquals(0, select.selectedIndex);
+
+      // Verify that selecting from outside works.
+      settingsSelect.selectValue(option1);
+      await test_util.eventToPromise('process-select-change', settingsSelect);
+      assertEquals(
+          option1, JSON.stringify(settingsSelect.getSettingValue('fruit')));
+      assertEquals(1, select.selectedIndex);
+    });
+  });
 });
