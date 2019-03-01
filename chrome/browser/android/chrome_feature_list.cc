e187d545eaed9757e440ba14c351a69671229026
Shakti Sahu <shaktisahu@chromium.org>
2019-03-01 18:59:17 +0000

    Download infobar : Enable by default
    Enabling download infobar by default on TOT. The feature will be 100%
    rolled out via finch in M73.
    Also assigned owners to the feature flag.
    
    Change-Id: I8b18ccae55678223045bd862f1d9cd98b8d211f6
    Reviewed-on: https://chromium-review.googlesource.com/c/1495046
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#636891}
    Former-commit-id: 87912eb4de55347cbe4a7cec9bfc0ee80e8b44fb

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -336,7 +336,7 @@ const base::Feature kDownloadAutoResumptionThrottling{
     "DownloadAutoResumptionThrottling", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kDownloadProgressInfoBar{"DownloadProgressInfoBar",
-                                             base::FEATURE_DISABLED_BY_DEFAULT};
+                                             base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kDownloadHomeV2{"DownloadHomeV2",
                                     base::FEATURE_DISABLED_BY_DEFAULT};
