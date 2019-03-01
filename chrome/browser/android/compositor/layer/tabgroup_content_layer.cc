cb98768a05156ed36785c5a6df9205662f5b42cd
Mei Liang <meiliang@chromium.org>
2019-03-01 23:28:40 +0000

    [TabGroupCard] Implement TabGroupContentLayer
    This CL defines TabGroupContentLayer#SetProperties and
    TabGroupTabContentLayer#SetProperties.
    
    Bug: 930954
    Change-Id: Ie1ca37ae45b49fcd73617a5b653cb99033133305
    Reviewed-on: https://chromium-review.googlesource.com/c/1476236
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637013}
    Former-commit-id: 5a062cdc47b2a2fdcc2ff54155d2b682df4cc1d6

--- chrome/browser/android/compositor/layer/tabgroup_content_layer.cc
+++ chrome/browser/android/compositor/layer/tabgroup_content_layer.cc
@@ -6,18 +6,21 @@
 
 #include <vector>
 
-#include "base/lazy_instance.h"
 #include "cc/layers/layer.h"
-#include "cc/layers/layer_collections.h"
-#include "cc/layers/nine_patch_layer.h"
-#include "cc/paint/filter_operations.h"
-#include "chrome/browser/android/compositor/layer/thumbnail_layer.h"
+#include "chrome/browser/android/compositor/layer/tabgroup_tab_content_layer.h"
 #include "chrome/browser/android/compositor/tab_content_manager.h"
-#include "content/public/browser/android/compositor.h"
 #include "ui/android/resources/nine_patch_resource.h"
+#include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/geometry/size.h"
 
 namespace android {
+
+// row x column of the tab group content grid is 2 x 2.
+const int THUMBNAIL_ROWS = 2;
+const int THUMBNAIL_COLS = THUMBNAIL_ROWS;
+const float DESIRED_SCALE = 0.475;
+const float GAP_SCALE = 0.05;
 
 // static
 scoped_refptr<TabGroupContentLayer> TabGroupContentLayer::Create(
@@ -35,12 +38,53 @@ void TabGroupContentLayer::SetProperties(
     float saturation,
     bool should_clip,
     const gfx::Rect& clip,
-    float width,
-    float height,
-    int inset_diff,
     ui::NinePatchResource* inner_shadow_resource,
     float inner_shadow_alpha) {
-  // TODO(meiliang): Override here;
+  if (group_tab_content_layers_.size() == 0) {
+    for (int i = 0; i < 4; i++) {
+      group_tab_content_layers_.emplace_back(
+          TabGroupTabContentLayer::Create(tab_content_manager_));
+      layer_->AddChild(group_tab_content_layers_.back()->layer());
+    }
+  }
+
+  const gfx::RectF border_inner_shadow_padding(
+      inner_shadow_resource->padding());
+
+  const gfx::Size border_inner_shadow_padding_size(
+      inner_shadow_resource->size().width() -
+          border_inner_shadow_padding.width(),
+      inner_shadow_resource->size().height() -
+          border_inner_shadow_padding.height());
+
+  int size = tab_ids.size();
+  for (int i = 0; i < size; i++) {
+    group_tab_content_layers_[i]->SetProperties(
+        tab_ids[i], can_use_live_layer, static_to_view_blend,
+        should_override_content_alpha, content_alpha_override, saturation,
+        should_clip, clip, inner_shadow_resource, tab_ids, inner_shadow_alpha);
+
+    gfx::Transform transform;
+    transform.Scale(DESIRED_SCALE, DESIRED_SCALE);
+    group_tab_content_layers_[i]->layer()->SetTransform(transform);
+
+    int height_offset_factor = i / THUMBNAIL_ROWS;
+    int width_offset_factor = i % THUMBNAIL_COLS;
+
+    float position_x =
+        clip.x() +
+        width_offset_factor *
+            (clip.width() * DESIRED_SCALE + clip.width() * GAP_SCALE -
+             border_inner_shadow_padding_size.width());
+
+    float position_y =
+        clip.y() +
+        height_offset_factor *
+            (clip.height() * DESIRED_SCALE + clip.height() * GAP_SCALE -
+             border_inner_shadow_padding_size.height());
+    gfx::PointF position(position_x, position_y);
+    group_tab_content_layers_[i]->layer()->SetPosition(position);
+  }
 }
 
 TabGroupContentLayer::TabGroupContentLayer(
