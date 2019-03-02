0846fb7167aefffb45c863083b7bee34e391858d
Luum Habtemariam <luum@chromium.org>
2019-03-02 00:23:59 +0000

    ipp_converter IPP APIs change
    Fix to correct handling IPP APIs. Also speculative fix for
    chromium:936121.
    
    Bug: 936121
    Test: still passes unittests
    Change-Id: Ic31d6d655d735e22d16dd11016de233d6243b1e7
    Reviewed-on: https://chromium-review.googlesource.com/c/1497298
    Commit-Queue: Luum Habtemariam <luum@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637032}
    Former-commit-id: 272a060ed51c49960aa793907ea4d8c3a537b7fb

--- chrome/services/cups_ipp_parser/public/cpp/ipp_converter.cpp
+++ chrome/services/cups_ipp_parser/public/cpp/ipp_converter.cpp
@@ -42,7 +42,7 @@ ssize_t IppWrite(base::span<uint8_t>* dst, ipp_uchar_t* source, size_t bytes) {
   uint8_t* src = static_cast<uint8_t*>(source);
 
   size_t num_to_write = std::min(dst->size(), bytes);
-  std::copy(src, src + bytes, dst->begin());
+  std::copy(src, src + num_to_write, dst->begin());
   *dst = dst->subspan(num_to_write);
 
   return num_to_write;
@@ -161,6 +161,9 @@ base::Optional<std::vector<uint8_t>> BuildHeaders(
                     {term.first, kHeaderDelimiter, term.second, kCarriage});
   }
 
+  // End-of-headers sentinel is a double carriage return; add the second one.
+  headers += kCarriage;
+
   std::vector<uint8_t> ret;
   std::copy(headers.begin(), headers.end(), std::back_inserter(ret));
   return ret;
@@ -189,11 +192,17 @@ printing::ScopedIppPtr ParseIppMessage(base::span<const uint8_t> ipp_slice) {
 base::Optional<std::vector<uint8_t>> BuildIppMessage(ipp_t* ipp) {
   std::vector<uint8_t> request(ippLength(ipp));
 
+  // Need to start in idle state for reading/writing.
+  if (!ippSetState(ipp, IPP_STATE_IDLE)) {
+    return base::nullopt;
+  }
+
   // Casting IppWrite callback to correct internal CUPS type
   // Note: This is safe since we essentially only cast the first argument from
-  // vector<const uint8_t> to base::span<const uint8_t>, which is well defined.
-  auto ret = ippWriteIO(&request, reinterpret_cast<ipp_iocb_t>(IppWrite), 1,
-                        nullptr, ipp);
+  // base::span<uint8_t> to void* and back, only accessing it from the former.
+  base::span<uint8_t> request_view(request);
+  auto ret = ippWriteIO(&request_view, reinterpret_cast<ipp_iocb_t>(IppWrite),
+                        1, nullptr, ipp);
 
   if (ret == IPP_STATE_ERROR) {
     // Write failed
