d9a19a2b76b22a43d854b6d2def8b20f1aae6f0d
dpapad <dpapad@chromium.org>
2019-03-04 05:44:59 +0000

    WebUI cleanup: Remove dead code cr.LinkController.
    Bug: None
    Change-Id: I4237e71fc9f5436f04f82c344380909c43838143
    Reviewed-on: https://chromium-review.googlesource.com/c/1497540
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637211}
    Former-commit-id: 8982dddaf386af4e964badec7c1b103c5fbf3d52

--- chrome/test/data/webui/webui_resource_browsertest.cc
+++ chrome/test/data/webui/webui_resource_browsertest.cc
@@ -139,12 +139,6 @@ IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, GridTest) {
   LoadFile(base::FilePath(FILE_PATH_LITERAL("grid_test.html")));
 }
 
-IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, LinkControllerTest) {
-  AddLibrary(IDR_WEBUI_JS_CR);
-  AddLibrary(IDR_WEBUI_JS_CR_LINK_CONTROLLER);
-  LoadFile(base::FilePath(FILE_PATH_LITERAL("link_controller_test.html")));
-}
-
 IN_PROC_BROWSER_TEST_F(WebUIResourceBrowserTest, ListSelectionModelTest) {
   AddLibrary(IDR_WEBUI_JS_CR);
   AddLibrary(IDR_WEBUI_JS_CR_EVENT_TARGET);
