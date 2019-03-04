aa13726d8f27d003293aa4aaeacb4f98ff904a75
Xiaocheng Hu <xiaochengh@chromium.org>
2019-03-04 16:45:27 +0000

    Fix caret rendering when at the end of a block after an ineditable inline
    AdjustBlockFlowPositionToInline() has an early-reject branch that avoids
    infinite recursion, introduced in crrev.com/664b6437.
    
    However, the early reject condition is too aggressive that, it also
    rejects some positions that don't enter an infinite recursion, like
    the one used in the new unit test added in this CL.
    
    As now AdjustBlockFlowPositionToInline() has a hard limit on recursion
    depth, this CL safely removes the over-aggressive reject condition, so
    that caret can be correctly rendered for more positions.
    
    Note: Unit test InlineBoxPositionTest.ComputeInlineBoxPositionMixedEditable
    uses another position that doesn't enter the infinite loop but is falsely
    rejected by the condition. This patch allows caret rendering also for that
    position, and therefore changes its test expectation.
    
    Bug: 936988
    Change-Id: Idef91ffaa412e67cddd5fcf0dd61f54055de7189
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1497466
    Auto-Submit: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637311}
    Former-commit-id: e9aa20fd6702db58135773a650bc4cbd1c542350

--- third_party/blink/renderer/core/editing/inline_box_position.cc
+++ third_party/blink/renderer/core/editing/inline_box_position.cc
@@ -217,8 +217,7 @@ PositionWithAffinityTemplate<Strategy> AdjustBlockFlowPositionToInline(
   }
   const PositionTemplate<Strategy>& upstream_equivalent =
       UpstreamIgnoringEditingBoundaries(position);
-  if (upstream_equivalent == position ||
-      DownstreamIgnoringEditingBoundaries(upstream_equivalent) == position)
+  if (upstream_equivalent == position)
     return PositionWithAffinityTemplate<Strategy>();
 
   return ComputeInlineAdjustedPositionAlgorithm(
