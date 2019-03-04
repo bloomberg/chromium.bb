e6e3e7d0f4084d8b2d1bbf8abe4196db80b12b58
Sadrul Habib Chowdhury <sadrul@chromium.org>
2019-03-04 22:34:15 +0000

    perf: Update test to use new API.
    Now that the CL to add the new API has landed and rolled in,
    remove the TODO (and update to use the new name of the API).
    
    BUG=932409
    
    Change-Id: Ica6351af03fbfd713647f73cc2fe6841fb112c6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1497864
    Commit-Queue: Caleb Rouleau <crouleau@chromium.org>
    Auto-Submit: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637450}
    Former-commit-id: 9f8b724af4b92f005cb96cd2a955c58a52d3aca8

--- tools/perf/benchmarks/system_health_smoke_test.py
+++ tools/perf/benchmarks/system_health_smoke_test.py
@@ -238,11 +238,8 @@ def GenerateBenchmarkOptions(benchmark_class):
   # all crashes and hence remove the need to enable logging in actual perf
   # benchmarks.
   options.browser_options.logging_verbosity = 'non-verbose'
-  # TODO(sadrul): Remove the check once catapult roll happens.
-  # https://crbug.com/932409
-  if "GetSupportedPlatforms" in dir(benchmark_class):
-    options.target_platforms = benchmark_class.GetSupportedPlatforms(
-        benchmark_class.SUPPORTED_PLATFORMS)
+  options.target_platforms = benchmark_class.GetSupportedPlatformNames(
+      benchmark_class.SUPPORTED_PLATFORMS)
   return options
 
 
