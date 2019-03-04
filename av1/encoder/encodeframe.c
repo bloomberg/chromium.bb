a4ef4471b780dd1dfc8bde816d00ed05a977c7e6
Debargha Mukherjee <debargha@google.com>
2019-03-04 15:42:49 -0800

    A small refactor for global motion computation
    Allows both feature based and disflow based global motion to be
    used.
    
    Change-Id: I858d8f44302d1888dad82c03b1b9eb7831fd5cc3

--- av1/encoder/encodeframe.c
+++ av1/encoder/encodeframe.c
@@ -6381,10 +6381,10 @@ static void encode_frame_internal(AV1_COMP *cpi) {
                    (MAX_PARAMDIM - 1) * sizeof(*params_by_motion));
           }
 
-          av1_compute_global_motion(model, cpi->source, ref_buf[frame],
-                                    cpi->common.seq_params.bit_depth,
-                                    inliers_by_motion, params_by_motion,
-                                    RANSAC_NUM_MOTIONS);
+          av1_compute_global_motion(
+              model, cpi->source, ref_buf[frame],
+              cpi->common.seq_params.bit_depth, GLOBAL_MOTION_FEATURE_BASED,
+              inliers_by_motion, params_by_motion, RANSAC_NUM_MOTIONS);
 
           for (i = 0; i < RANSAC_NUM_MOTIONS; ++i) {
             if (inliers_by_motion[i] == 0) continue;
