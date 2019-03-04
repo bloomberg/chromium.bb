701059687085b19338e214598e8aecb917f9cc00
Reilly Grant <reillyg@chromium.org>
2019-03-04 22:41:59 +0000

    Revert "[serial] Remove all code from Android Blink build"
    This reverts commit 3c3daac7cf50cd5eec39ce08ec821054340b583c [formerly 5cd5e69c82a5018288f282fb6d918ed0ce6e0f27].
    
    Reason for revert: Failure on android-rel.
    
    Original change's description:
    > [serial] Remove all code from Android Blink build
    > 
    > This reverts r629504 and instead removes all of the Serial API code from
    > the Blink build on Android to further reduce binary size.
    > 
    > Bug: 884928
    > Change-Id: I02cd550753d8020b6f8d796d576ca70caf378e39
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1497363
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    > Commit-Queue: Reilly Grant <reillyg@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#637439}
    
    TBR=kenrb@chromium.org,reillyg@chromium.org,haraken@chromium.org
    
    Change-Id: I2ff8c789992ceabfdaa856ec9c0bd6fae268fca0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 884928
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1500939
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637453}
    Former-commit-id: e4f4664139bfa9511c6b90c14826feb3a73d814a

--- content/browser/frame_host/render_frame_host_impl.h
+++ content/browser/frame_host/render_frame_host_impl.h
@@ -71,6 +71,7 @@
 #include "third_party/blink/public/mojom/frame/find_in_page.mojom.h"
 #include "third_party/blink/public/mojom/frame/navigation_initiator.mojom.h"
 #include "third_party/blink/public/mojom/presentation/presentation.mojom.h"
+#include "third_party/blink/public/mojom/serial/serial.mojom.h"
 #include "third_party/blink/public/mojom/worker/dedicated_worker_host_factory.mojom.h"
 #include "third_party/blink/public/platform/modules/bluetooth/web_bluetooth.mojom.h"
 #include "third_party/blink/public/platform/modules/webauthn/authenticator.mojom.h"
@@ -90,8 +91,6 @@
 
 #if defined(OS_ANDROID)
 #include "services/device/public/mojom/nfc.mojom.h"
-#else
-#include "third_party/blink/public/mojom/serial/serial.mojom.h"
 #endif
 
 class GURL;
