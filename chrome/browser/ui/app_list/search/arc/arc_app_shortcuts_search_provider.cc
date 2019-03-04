54c2faef87789531370e8e80f1889cc75397818c
tby <tby@chromium.org>
2019-03-04 23:21:33 +0000

    [Cros SR] Add Finch parameters to Roselle expansions.
    We've expanded Roselle in two ways: re-rank Arc app shortcuts, and
    re-rank the results for queries. Each of these is now controlled by a
    Finch parameter.
    
    Additionally, score coefficients for shortcuts and query-based apps
    are also controlled by parameters.
    
    Bug: 931149
    Change-Id: I791e247e95f6c7be7fb1f1e49a30c9aa51d9d753
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1496255
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637474}
    Former-commit-id: 39b511d4130412f0fb115c4f074f3f7e44f5f655

--- chrome/browser/ui/app_list/search/arc/arc_app_shortcuts_search_provider.cc
+++ chrome/browser/ui/app_list/search/arc/arc_app_shortcuts_search_provider.cc
@@ -10,6 +10,7 @@
 
 #include "ash/public/cpp/app_list/app_list_features.h"
 #include "base/bind.h"
+#include "base/metrics/field_trial_params.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_utils.h"
@@ -18,6 +19,18 @@
 #include "chrome/browser/ui/app_list/search/search_result_ranker/ranking_item_util.h"
 #include "components/arc/arc_bridge_service.h"
 #include "components/arc/arc_service_manager.h"
+
+namespace {
+
+// When ranking with the |AppSearchResultRanker| is enabled, this boost is
+// added to all shortcuts that the ranker knows about.
+constexpr float kDefaultRankerScoreBoost = 0.0f;
+
+// When ranking with the |AppSearchResultRanker| is enabled, its scores are
+// multiplied by this amount.
+constexpr float kDefaultRankerScoreCoefficient = 0.1f;
+
+}  // namespace
 
 namespace app_list {
 
@@ -66,9 +79,32 @@ void ArcAppShortcutsSearchProvider::OnGetAppShortcutGlobalQueryItems(
     std::vector<arc::mojom::AppShortcutItemPtr> shortcut_items) {
   const ArcAppListPrefs* arc_prefs = ArcAppListPrefs::Get(profile_);
   DCHECK(arc_prefs);
+
+  // To rerank shortcuts, both the main Roselle feature flag and the
+  // |rank_arc_app_shortcuts| parameter must be enabled. If so, then shortcuts
+  // are reranked for both zero-state and query-based results.
+  // TODO(crbug.com/931149): once zero-state arc shortcut results are
+  // implemented, split the |rank_arc_app_shortcuts| flag into two flags to
+  // control zero-state and query-based re-ranking individually.
+  const bool should_rerank =
+      app_list_features::IsAppSearchResultRankerEnabled() &&
+      base::GetFieldTrialParamByFeatureAsBool(
+          app_list_features::kEnableAppSearchResultRanker,
+          "rank_arc_app_shortcuts", false) &&
+      ranker_ != nullptr;
+  // Maps app IDs to their score according to |ranker_|.
   base::flat_map<std::string, float> ranker_scores;
-  if (app_list_features::IsAppSearchResultRankerEnabled() && ranker_ != nullptr)
+  float ranker_score_coefficient = kDefaultRankerScoreCoefficient;
+  float ranker_score_boost = kDefaultRankerScoreBoost;
+  if (should_rerank) {
     ranker_scores = ranker_->Rank();
+    ranker_score_coefficient = base::GetFieldTrialParamByFeatureAsDouble(
+        app_list_features::kEnableAppSearchResultRanker,
+        "arc_app_shortcuts_query_coefficient", ranker_score_coefficient);
+    ranker_score_boost = base::GetFieldTrialParamByFeatureAsDouble(
+        app_list_features::kEnableAppSearchResultRanker,
+        "arc_app_shortcuts_query_boost", ranker_score_boost);
+  }
 
   SearchProvider::Results search_results;
   for (auto& item : shortcut_items) {
@@ -81,12 +117,18 @@ void ArcAppShortcutsSearchProvider::OnGetAppShortcutGlobalQueryItems(
       continue;
     auto result = std::make_unique<ArcAppShortcutSearchResult>(
         std::move(item), profile_, list_controller_);
-    // TODO(crbug.com/931149): update the formula for relevance scores.
-    // This formula should be updated in the same way as query-based
-    // app search results
-    const auto find_in_ranker = ranker_scores.find(result->id());
-    if (find_in_ranker != ranker_scores.end())
-      result->set_relevance(result->relevance() + find_in_ranker->second / 10);
+    if (should_rerank) {
+      const auto find_in_ranker = ranker_scores.find(result->id());
+      if (find_in_ranker != ranker_scores.end()) {
+        // TODO(crbug.com/931149): currently, relevance scores for app shortcuts
+        // are always 0, but are included here in case this changes. If this
+        // changes, review their range and possibly update this formula.
+        result->set_relevance(result->relevance() +
+                              ranker_score_coefficient *
+                                  find_in_ranker->second +
+                              ranker_score_boost);
+      }
+    }
     search_results.emplace_back(std::move(result));
   }
   SwapResults(&search_results);
