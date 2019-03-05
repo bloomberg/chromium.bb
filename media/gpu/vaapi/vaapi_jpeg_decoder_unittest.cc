708a34dbfd915e1602c6cf5cf771543958f983ca
Miguel Casas <mcasas@chromium.org>
2019-03-05 14:46:26 +0000

    Parameterize vaapi_j_d_unittest, add a 420 file, use SSIM
    This CL parameterizes vaapi_j_d_unittest, deprecates the use of
    MD5Sums in favour of a SSIM+threshold (as used in other parts
    of Chrome) and takes the chance to add a 4:2:0 file to test with.
    
    Test: jpeg_decode_accelerator_unittest on Nocturne, Grunt,
    FTR the SSIMs values I see,
    nocturne i422 0.997971
    nocturne i420 0.998007
    grunt i422 0.997875
    grunt i420 0.998006
    
    CQ-DEPENDS=CL:1496187
    
    Bug: 868400
    Change-Id: I3d7b9badccb7ad15fdb864c24bba2224c3c7be2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1481114
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Auto-Submit: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637690}
    Former-commit-id: 44521bec650adc3b4cffac8eb4ad996a04566e6b

--- media/gpu/vaapi/vaapi_jpeg_decoder_unittest.cc
+++ media/gpu/vaapi/vaapi_jpeg_decoder_unittest.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <stddef.h>
 #include <stdint.h>
 
 #include <memory>
@@ -20,25 +21,36 @@
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/logging.h"
-#include "base/md5.h"
-#include "base/strings/string_piece.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/synchronization/lock.h"
 #include "base/test/gtest_util.h"
 #include "base/thread_annotations.h"
 #include "media/base/test_data_util.h"
+#include "media/base/video_types.h"
+#include "media/filters/jpeg_parser.h"
 #include "media/gpu/vaapi/vaapi_jpeg_decoder.h"
 #include "media/gpu/vaapi/vaapi_utils.h"
 #include "media/gpu/vaapi/vaapi_wrapper.h"
+#include "third_party/libyuv/include/libyuv.h"
 #include "ui/gfx/geometry/size.h"
 
 namespace media {
 namespace {
 
-// This file is supported by the VAAPI, so define expectations on the decode
-// result.
-constexpr const char* kSupportedFilename = "pixel-1280x720.jpg";
-constexpr const char* kExpectedMd5SumI420 = "6e9e1716073c9a9a1282e3f0e0dab743";
-constexpr const char* kExpectedMd5SumYUYV = "ff313a6aedbc4e157561e5c2d5c2e079";
+constexpr const char* kYuv422Filename = "pixel-1280x720.jpg";
+constexpr const char* kYuv420Filename = "pixel-1280x720-yuv420.jpg";
+
+struct TestParam {
+  const char* filename;
+};
+
+const TestParam kTestCases[] = {
+    {kYuv422Filename},
+    {kYuv420Filename},
+};
+
+// Any number above 99.5% should do, experimentally we like a wee higher.
+constexpr double kMinSsim = 0.997;
 
 // This file is not supported by the VAAPI, so we don't define expectations on
 // the decode result.
@@ -50,9 +62,120 @@ constexpr VAImageFormat kImageFormatI420 = {
     .bits_per_pixel = 12,
 };
 
+// Compares the result of sw decoding |encoded_image| with |decoded_image| using
+// SSIM. Returns true if all conversions work and SSIM is above a given
+// threshold (kMinSsim), or false otherwise.
+bool CompareImages(base::span<const uint8_t> encoded_image,
+                   const ScopedVAImage* decoded_image) {
+  JpegParseResult parse_result;
+  const bool result = ParseJpegPicture(encoded_image.data(),
+                                       encoded_image.size(), &parse_result);
+  if (!result)
+    return false;
+
+  const uint16_t width = parse_result.frame_header.visible_width;
+  const uint16_t height = parse_result.frame_header.visible_height;
+
+  if (width != decoded_image->image()->width ||
+      height != decoded_image->image()->height) {
+    DLOG(ERROR) << "Wrong expected decoded JPEG size, " << width << "x"
+                << height << " versus VaAPI provided "
+                << decoded_image->image()->width << "x"
+                << decoded_image->image()->height;
+    return false;
+  }
+  const uint16_t even_width = (width + 1) / 2;
+  const uint16_t even_height = (height + 1) / 2;
+
+  auto ref_y = std::make_unique<uint8_t[]>(width * height);
+  auto ref_u = std::make_unique<uint8_t[]>(even_width * even_height);
+  auto ref_v = std::make_unique<uint8_t[]>(even_width * even_height);
+
+  const int conversion_result = libyuv::ConvertToI420(
+      encoded_image.data(), encoded_image.size(), ref_y.get(), width,
+      ref_u.get(), even_width, ref_v.get(), even_width, 0, 0, width, height,
+      width, height, libyuv::kRotate0, libyuv::FOURCC_MJPG);
+  if (conversion_result != 0) {
+    DLOG(ERROR) << "libyuv conversion error";
+    return false;
+  }
+
+  const uint32_t va_fourcc = decoded_image->image()->format.fourcc;
+  if (!(va_fourcc == VA_FOURCC_I420 || va_fourcc == VA_FOURCC_YUY2 ||
+        va_fourcc == VA_FOURCC('Y', 'U', 'Y', 'V'))) {
+    DLOG(ERROR) << "Not supported FourCC: " << FourccToString(va_fourcc);
+    return false;
+  }
+  const uint32_t libyuv_fourcc =
+      (va_fourcc == VA_FOURCC_I420) ? libyuv::FOURCC_I420 : libyuv::FOURCC_YUY2;
+
+  if (libyuv_fourcc == libyuv::FOURCC_I420) {
+    const auto* decoded_data_y =
+        static_cast<const uint8_t*>(decoded_image->va_buffer()->data()) +
+        decoded_image->image()->offsets[0];
+    const auto* decoded_data_u =
+        static_cast<const uint8_t*>(decoded_image->va_buffer()->data()) +
+        decoded_image->image()->offsets[1];
+    const auto* decoded_data_v =
+        static_cast<const uint8_t*>(decoded_image->va_buffer()->data()) +
+        decoded_image->image()->offsets[2];
+
+    const double ssim = libyuv::I420Ssim(
+        ref_y.get(), width, ref_u.get(), even_width, ref_v.get(), even_width,
+        decoded_data_y,
+        base::checked_cast<int>(decoded_image->image()->pitches[0]),
+        decoded_data_u,
+        base::checked_cast<int>(decoded_image->image()->pitches[1]),
+        decoded_data_v,
+        base::checked_cast<int>(decoded_image->image()->pitches[2]), width,
+        height);
+    if (ssim < kMinSsim) {
+      DLOG(ERROR) << "Too low SSIM: " << ssim << " < " << kMinSsim;
+      return false;
+    }
+  } else {
+    auto temp_y = std::make_unique<uint8_t[]>(width * height);
+    auto temp_u = std::make_unique<uint8_t[]>(even_width * even_height);
+    auto temp_v = std::make_unique<uint8_t[]>(even_width * even_height);
+
+    // TODO(crbug.com/868400): support other formats/planarities/pitches.
+    constexpr uint32_t kNumPlanesYuv422 = 1u;
+    constexpr uint32_t kBytesPerPixelYuv422 = 2u;
+    if (decoded_image->image()->num_planes != kNumPlanesYuv422 ||
+        decoded_image->image()->pitches[0] != (width * kBytesPerPixelYuv422)) {
+      DLOG(ERROR) << "Too many planes (got "
+                  << decoded_image->image()->num_planes << ", expected "
+                  << kNumPlanesYuv422 << ") or rows not tightly packed (got "
+                  << decoded_image->image()->pitches[0] << ", expected "
+                  << (width * kBytesPerPixelYuv422) << "), aborting test";
+      return false;
+    }
+
+    const int conversion_result = libyuv::ConvertToI420(
+        static_cast<const uint8_t*>(decoded_image->va_buffer()->data()),
+        base::strict_cast<size_t>(decoded_image->image()->data_size),
+        temp_y.get(), width, temp_u.get(), even_width, temp_v.get(), even_width,
+        0, 0, width, height, width, height, libyuv::kRotate0, libyuv_fourcc);
+    if (conversion_result != 0) {
+      DLOG(ERROR) << "libyuv conversion error";
+      return false;
+    }
+
+    const double ssim = libyuv::I420Ssim(
+        ref_y.get(), width, ref_u.get(), even_width, ref_v.get(), even_width,
+        temp_y.get(), width, temp_u.get(), even_width, temp_v.get(), even_width,
+        width, height);
+    if (ssim < kMinSsim) {
+      DLOG(ERROR) << "Too low SSIM: " << ssim << " < " << kMinSsim;
+      return false;
+    }
+  }
+  return true;
+}
+
 }  // namespace
 
-class VaapiJpegDecoderTest : public ::testing::Test {
+class VaapiJpegDecoderTest : public testing::TestWithParam<TestParam> {
  protected:
   VaapiJpegDecoderTest() {
     const base::CommandLine* cmd_line = base::CommandLine::ForCurrentProcess();
@@ -67,7 +190,6 @@ class VaapiJpegDecoderTest : public ::testing::Test {
 
   base::FilePath FindTestDataFilePath(const std::string& file_name);
 
-  bool VerifyDecode(base::span<const uint8_t> encoded_image);
   std::unique_ptr<ScopedVAImage> Decode(
       base::span<const uint8_t> encoded_image);
 
@@ -100,31 +222,6 @@ base::FilePath VaapiJpegDecoderTest::FindTestDataFilePath(
   return GetTestDataFilePath(file_name);
 }
 
-bool VaapiJpegDecoderTest::VerifyDecode(
-    base::span<const uint8_t> encoded_image) {
-  std::unique_ptr<ScopedVAImage> scoped_image = Decode(encoded_image);
-  if (!scoped_image) {
-    LOG(ERROR) << "Cannot get VAImage";
-    return false;
-  }
-
-  const char* expected_md5sum = nullptr;
-  if (scoped_image->image()->format.fourcc == VA_FOURCC_I420) {
-    expected_md5sum = kExpectedMd5SumI420;
-  } else if (scoped_image->image()->format.fourcc == VA_FOURCC_YUY2 ||
-             scoped_image->image()->format.fourcc ==
-                 VA_FOURCC('Y', 'U', 'Y', 'V')) {
-    expected_md5sum = kExpectedMd5SumYUYV;
-  } else {
-    LOG(FATAL) << "Neither I420 nor YUY2 is supported.";
-  }
-
-  const auto* mem = static_cast<char*>(scoped_image->va_buffer()->data());
-  base::StringPiece result(mem, scoped_image->image()->data_size);
-  EXPECT_EQ(expected_md5sum, base::MD5String(result));
-  return true;
-}
-
 std::unique_ptr<ScopedVAImage> VaapiJpegDecoderTest::Decode(
     base::span<const uint8_t> encoded_image) {
   VaapiJpegDecodeStatus status;
@@ -134,13 +231,18 @@ std::unique_ptr<ScopedVAImage> VaapiJpegDecoderTest::Decode(
   return scoped_image;
 }
 
-TEST_F(VaapiJpegDecoderTest, DecodeSuccess) {
-  base::FilePath input_file = FindTestDataFilePath(kSupportedFilename);
+TEST_P(VaapiJpegDecoderTest, DecodeSuccess) {
+  base::FilePath input_file = FindTestDataFilePath(GetParam().filename);
   std::string jpeg_data;
   ASSERT_TRUE(base::ReadFileToString(input_file, &jpeg_data))
       << "failed to read input data from " << input_file.value();
-  EXPECT_TRUE(VerifyDecode(base::make_span<const uint8_t>(
-      reinterpret_cast<const uint8_t*>(jpeg_data.data()), jpeg_data.size())));
+
+  const auto encoded_image = base::make_span<const uint8_t>(
+      reinterpret_cast<const uint8_t*>(jpeg_data.data()), jpeg_data.size());
+  std::unique_ptr<ScopedVAImage> scoped_image = Decode(encoded_image);
+  ASSERT_TRUE(scoped_image);
+
+  ASSERT_TRUE(CompareImages(encoded_image, scoped_image.get()));
 }
 
 TEST_F(VaapiJpegDecoderTest, DecodeFail) {
@@ -225,5 +327,7 @@ TEST_F(VaapiJpegDecoderTest, BadScopedVABufferMapping) {
       GetVaapiWrapperLock(), GetVaapiWrapperVaDisplay(), VA_INVALID_ID - 1);
   EXPECT_FALSE(scoped_buffer->IsValid());
 }
+
+INSTANTIATE_TEST_SUITE_P(, VaapiJpegDecoderTest, testing::ValuesIn(kTestCases));
 
 }  // namespace media
