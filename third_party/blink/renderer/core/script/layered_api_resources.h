9161d40dcf868d2576c00b6a7c3aa300c2eb82c6
Hiroshige Hayashizaki <hiroshige@chromium.org>
2019-03-05 21:07:44 +0000

    [Layered API] Introduce layered_api::Module
    As a preparation for enabling/disabling individual built-in
    modules, this CL introduces layered_api::Module enum class
    that identifies each built-in module, and assigns its value
    in |kLayeredAPIResources|.
    
    generate_lapi_grdp.py is modified so that it associates each entry
    of resource files with a Module value based on the directory name,
    and layered_api_module.h is generated and added.
    
    Bug: 829084
    Change-Id: I401c120a49866b249725ddf9e93b3d1dd4e56549
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1501573
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637810}
    Former-commit-id: de7f3a7e34af6a2766b2634b740b1701734b53eb

--- third_party/blink/renderer/core/script/layered_api_resources.h
+++ third_party/blink/renderer/core/script/layered_api_resources.h
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 #include "third_party/blink/public/resources/grit/blink_resources.h"
-#include "third_party/blink/renderer/core/script/layered_api.h"
+#include "third_party/blink/renderer/core/script/layered_api_module.h"
 
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_SCRIPT_LAYERED_API_RESOURCES_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_SCRIPT_LAYERED_API_RESOURCES_H_
@@ -20,28 +20,42 @@ namespace layered_api {
 
 namespace {
 
+struct LayeredAPIResource {
+  const char* path;
+  int resource_id;
+  Module module;
+};
+
 const LayeredAPIResource kLayeredAPIResources[] = {
-    {"blank/index.js", IDR_LAYERED_API_BLANK_INDEX_JS},
+    {"blank/index.js", IDR_LAYERED_API_BLANK_INDEX_JS, Module::kBlank},
 
     {"kv-storage/async_iterator.js",
-     IDR_LAYERED_API_KV_STORAGE_ASYNC_ITERATOR_JS},
-    {"kv-storage/idb_utils.js", IDR_LAYERED_API_KV_STORAGE_IDB_UTILS_JS},
-    {"kv-storage/index.js", IDR_LAYERED_API_KV_STORAGE_INDEX_JS},
+     IDR_LAYERED_API_KV_STORAGE_ASYNC_ITERATOR_JS, Module::kKvStorage},
+    {"kv-storage/idb_utils.js", IDR_LAYERED_API_KV_STORAGE_IDB_UTILS_JS,
+     Module::kKvStorage},
+    {"kv-storage/index.js", IDR_LAYERED_API_KV_STORAGE_INDEX_JS,
+     Module::kKvStorage},
 
-    {"virtual-scroller/index.js", IDR_LAYERED_API_VIRTUAL_SCROLLER_INDEX_JS},
+    {"virtual-scroller/index.js", IDR_LAYERED_API_VIRTUAL_SCROLLER_INDEX_JS,
+     Module::kVirtualScroller},
     {"virtual-scroller/item-source.js",
-     IDR_LAYERED_API_VIRTUAL_SCROLLER_ITEM_SOURCE_JS},
+     IDR_LAYERED_API_VIRTUAL_SCROLLER_ITEM_SOURCE_JS, Module::kVirtualScroller},
     {"virtual-scroller/virtual-repeater.js",
-     IDR_LAYERED_API_VIRTUAL_SCROLLER_VIRTUAL_REPEATER_JS},
+     IDR_LAYERED_API_VIRTUAL_SCROLLER_VIRTUAL_REPEATER_JS,
+     Module::kVirtualScroller},
     {"virtual-scroller/virtual-scroller.js",
-     IDR_LAYERED_API_VIRTUAL_SCROLLER_VIRTUAL_SCROLLER_JS},
+     IDR_LAYERED_API_VIRTUAL_SCROLLER_VIRTUAL_SCROLLER_JS,
+     Module::kVirtualScroller},
 
     {"virtual-scroller/layouts/layout-1d-base.js",
-     IDR_LAYERED_API_VIRTUAL_SCROLLER_LAYOUTS_LAYOUT_1D_BASE_JS},
+     IDR_LAYERED_API_VIRTUAL_SCROLLER_LAYOUTS_LAYOUT_1D_BASE_JS,
+     Module::kVirtualScroller},
     {"virtual-scroller/layouts/layout-1d-grid.js",
-     IDR_LAYERED_API_VIRTUAL_SCROLLER_LAYOUTS_LAYOUT_1D_GRID_JS},
+     IDR_LAYERED_API_VIRTUAL_SCROLLER_LAYOUTS_LAYOUT_1D_GRID_JS,
+     Module::kVirtualScroller},
     {"virtual-scroller/layouts/layout-1d.js",
-     IDR_LAYERED_API_VIRTUAL_SCROLLER_LAYOUTS_LAYOUT_1D_JS},
+     IDR_LAYERED_API_VIRTUAL_SCROLLER_LAYOUTS_LAYOUT_1D_JS,
+     Module::kVirtualScroller},
 
 };
 
