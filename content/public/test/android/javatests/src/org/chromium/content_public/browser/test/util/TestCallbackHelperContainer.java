41eb3e4053c4f72ba6240c208fd0c1c775a1b557
Bo Liu <boliu@chromium.org>
2019-03-05 17:16:32 +0000

    android: Fix evaluateJavaScriptForTests thread
    WebContents methods needs to be called on the UI thread
    
    Change-Id: Id297470786ca85a3a02b9514fc3c957368dbfb09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1502032
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637722}
    Former-commit-id: 17babfb9d44cf8f8497a68284b317089b711e96a

--- content/public/test/android/javatests/src/org/chromium/content_public/browser/test/util/TestCallbackHelperContainer.java
+++ content/public/test/android/javatests/src/org/chromium/content_public/browser/test/util/TestCallbackHelperContainer.java
@@ -123,7 +123,8 @@ public class TestCallbackHelperContainer {
                 }
             };
             mJsonResult = null;
-            webContents.evaluateJavaScriptForTests(code, callback);
+            ThreadUtils.runOnUiThreadBlocking(
+                    () -> webContents.evaluateJavaScriptForTests(code, callback));
         }
 
         /**
