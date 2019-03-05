60dac4a9e54a6fcffbb078863ba0c7b51c06c51b
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-03-05 07:04:29 +0000

    Import wpt@7a54b4d831b271f678fac355f3a9c8285701512f
    Using wpt-import in Chromium 95c6a3d875970c5900e495f1b0b88b9c09b570db [formerly c46e9b4934a4a90c4d02366db5bc5009d373b61b].
    With Chromium commits locally applied on WPT:
    7b057eae5c [formerly 68d5240e06] "[ChromeDriver] Set key modifiers with mouse actions"
    cc9d2f2f78 [formerly 682affef9b] "Implemented setPlaybackRate for scroll-linked Worklet Animations."
    0df4cf2614 [formerly 2b037bdc25] "[Animation Worklet] Fix flaky worklet-animation-with-fill-mode.https.html"
    f75dea26bc [formerly 5a7d4abb1f] "IndexedDB: Fix WPT bindings-inject-key to clean up Object.prototype"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    guidou@chromium.org:
      external/wpt/mediacapture-streams
    kenneth.r.christiansen@intel.com, rijubrata.bhaumik@intel.com:
      external/wpt/web-nfc
    kojii@chromium.org:
      external/wpt/css/css-text/line-break
    meade@chromium.org:
      external/wpt/web-animations
    msisov@igalia.com:
      external/wpt/webvtt
    rouslan@chromium.org, mathp@chromium.org:
      external/wpt/payment-request
    skobes@chromium.org:
      external/wpt/css/css-scroll-anchoring
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    TBR=robertma
    
    No-Export: true
    Change-Id: I3fc26ef8e8f00f1d7d29d71cb7597845c975e445
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1501758
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#637618}
    Former-commit-id: 8eb6b70b5cff10d8aba28671a0ffd56c390a06f5

---
+++ third_party/blink/web_tests/external/wpt/tools/docker/start.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# This script is embedded in the docker image, and so the image must be updated when changes
+# to the script are made. To do this, assuming you have docker installed:
+# In tools/docker/ :
+#   docker build .
+#   docker ps # and look for the id of the image you just built
+#   docker tag <image> <tag>
+#   docker push <tag>
+# Update the `image` specified in the project's .taskcluster.yml file
+
+
+set -ex
+
+REMOTE=${1:-https://github.com/web-platform-tests/wpt}
+REF=${2:-master}
+REVISION=${3:-FETCH_HEAD}
+BROWSER=${4:-all}
+CHANNEL=${5:-nightly}
+
+cd ~
+
+mkdir web-platform-tests
+cd web-platform-tests
+
+git init
+git remote add origin ${REMOTE}
+
+# Initially we just fetch 50 commits in order to save several minutes of fetching
+retry git fetch --quiet --depth=50 --tags origin ${REF}
+
+if [[ ! `git rev-parse --verify -q ${REVISION}` ]];
+then
+    # But if for some reason the commit under test isn't in that range, we give in and
+    # fetch everything
+    retry git fetch -q --unshallow ${REMOTE}
+    git rev-parse --verify ${REVISION}
+fi
+git checkout -b build ${REVISION}
+
+source tools/ci/start.sh
