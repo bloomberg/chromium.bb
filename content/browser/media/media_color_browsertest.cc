c896a7658000e6048534ddaf163f4bf308c73f79
Tim Volodine <timvolodine@chromium.org>
2019-03-05 15:50:45 +0000

    [media_browsertests] Disable failing MediaColorTests
    In particular
    MediaColorTest.Yuv420pH264
    MediaColorTest.Yuv420pRec709H264
    which are failing on test-official-arm_64 bot.
    
    BUG=938320
    NOTRY=true
    TBR=hubbe@chromium.org,tedchoc@chromium.org,hanxi@chromium.org
    
    Change-Id: I079f2b263998c0f439b47b19bba9cca066420e06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1503418
    Reviewed-by: Tim Volodine <timvolodine@chromium.org>
    Commit-Queue: Tim Volodine <timvolodine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637698}
    Former-commit-id: a7ea4011716298ffcd34010fb67a15ad033d7886

--- content/browser/media/media_color_browsertest.cc
+++ content/browser/media/media_color_browsertest.cc
@@ -52,8 +52,13 @@ IN_PROC_BROWSER_TEST_F(MediaColorTest, Yuv444pVp9) {
 }
 
 #if BUILDFLAG(USE_PROPRIETARY_CODECS)
-
-IN_PROC_BROWSER_TEST_F(MediaColorTest, Yuv420pH264) {
+// This test fails on Android: http://crbug.com/938320
+#if defined(OS_ANDROID)
+#define MAYBE_Yuvj420pH264_2 DISABLED_Yuvj420pH264_2
+#else
+#define MAYBE_Yuvj420pH264_2 Yuvj420pH264_2
+#endif
+IN_PROC_BROWSER_TEST_F(MediaColorTest, MAYBE_Yuv420pH264_2) {
 #if defined(OS_ANDROID)
   // https://crbug.com/907572
   if (base::android::BuildInfo::GetInstance()->sdk_int() <=
@@ -76,7 +81,8 @@ IN_PROC_BROWSER_TEST_F(MediaColorTest, MAYBE_Yuvj420pH264) {
 }
 
 // This fails on ChromeOS: http://crbug.com/647400,
-#if defined(OS_CHROMEOS)
+// This fails on Android: http://crbug.com/938320,
+#if defined(OS_CHROMEOS) || defined(OS_ANDROID)
 #define MAYBE_Yuv420pRec709H264 DISABLED_Yuv420pRec709H264
 #else
 #define MAYBE_Yuv420pRec709H264 Yuv420pRec709H264
