f5aa424ae0b6bcb5a1680cbd6043f7efeff3e2e9
Don Garrett <dgarrett@google.com>
2019-03-05 12:23:16 -0800

    chromeos_config: Change packages for old factorybranches.
    Re-auditted all factory branches, after another missing one cropped up.
    
    Added:
      factory-veyron-7505.B
    
    Removed the non-existant:
      factory-glados-7857.B
    
    BUG=chromium:937026
    TEST=run_tests
    
    Change-Id: I13f0072de53fa40405b93039ef237873a49a7618
    Reviewed-on: https://chromium-review.googlesource.com/1503497
    Commit-Ready: Don Garrett <dgarrett@chromium.org>
    Tested-by: Don Garrett <dgarrett@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- config/chromeos_config.py
+++ config/chromeos_config.py
@@ -3213,9 +3213,9 @@ def FactoryBuilders(site_config, boards_dict, ge_build_config):
       'factory-whirlwind-6812.41.B',
       'factory-arkham-7077.113.B',
       'factory-strago-7458.B',
+      'factory-veyron-7505.B',
       'factory-glados-7657.B',
       'factory-glados-7828.B',
-      'factory-glados-7857.B',
   }
 
   for active, branch, boards in branch_builders:
