ce92804a92a23d60b12ee756804c93de7ff203e6
Xiaohan Wang <xhwang@chromium.org>
2019-03-05 20:38:22 +0000

    Revert "media: Use CHECK in CdmAdapter::SetTimer()"
    This reverts commit 78032eda3dba9610991ea09141b5f6c320c9f524 [formerly a104770b5a125b5be76ce2b2883bdc3aeff4afd6].
    
    Reason for revert: The original crash has been fixed in Chrome 72 but
    it is unclear how that's fixed. Even if we still need this checks for
    investigation, they have made into M73 beta/stable which should provide
    sufficient data.
    
    Original change's description:
    > media: Use CHECK in CdmAdapter::SetTimer()
    > 
    > This is for bug investigation and will be reverted after the bug
    > investigation is over. See Bug.
    > 
    > Bug: 887761
    > Test: No functionality change.
    > Change-Id: I98c7dff8a46ac4d7f4847711d24ef5e2ba45cc42
    > Reviewed-on: https://chromium-review.googlesource.com/c/1409350
    > Reviewed-by: John Rummell <jrummell@chromium.org>
    > Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#622571}
    
    TBR=xhwang@chromium.org,jrummell@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 887761
    Change-Id: I4dbfb86ea2eb656a358818e7721a489311c44065
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1503900
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Auto-Submit: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637792}
    Former-commit-id: 0e13ad92ac8fe24f26b091e3775e3d96da26f622

--- media/cdm/cdm_adapter.cc
+++ media/cdm/cdm_adapter.cc
@@ -677,10 +677,7 @@ cdm::Buffer* CdmAdapter::Allocate(uint32_t capacity) {
 }
 
 void CdmAdapter::SetTimer(int64_t delay_ms, void* context) {
-  // TODO(crbug.com/887761): Use CHECKs for bug investigation. Change back to
-  // DCHECK after it's completed.
-  CHECK(task_runner_);
-  CHECK(task_runner_->BelongsToCurrentThread());
+  DCHECK(task_runner_->BelongsToCurrentThread());
 
   auto delay = base::TimeDelta::FromMilliseconds(delay_ms);
   DVLOG(3) << __func__ << ": delay = " << delay << ", context = " << context;
