e150caff036e6f0382a7ac48340c7b86a9c04bb5
Miguel Casas-Sanchez <mcasas@chromium.org>
2019-03-06 21:03:13 +0000

    Barcode detection Mac: Reenable using Vision framework
    This CL reenables using the Vision framework for Barcode detection on
    Mac (this was broken in previous 10.14 OS updates and as expected is
    now fixed). It also takes the chance to fill in |corner_points| in
    DetectedBarcode.
    
    Tested by local compilation and running it via:
    ./out/gn/Chromium.app/Contents/MacOS/Chromium https://yellowdoge.github.io/shapedetection-polyfill/demo.bundle.html --enable-experimental-web-platform-features
    
    and verifying the QR (and other) codes, including slanted ones (!).
    
    Bug: 921968
    Change-Id: Ic485a41305d0290e8a35751a0de703bd674493fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1506011
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Auto-Submit: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Jao-ke Chin-Lee <jchinlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638272}
    Former-commit-id: 78c271f9e7a8329037dddc7b3f9c81d37b62e04d

--- services/shape_detection/barcode_detection_provider_mac.mm
+++ services/shape_detection/barcode_detection_provider_mac.mm
@@ -28,15 +28,8 @@ void BarcodeDetectionProviderMac::Create(
 void BarcodeDetectionProviderMac::CreateBarcodeDetection(
     mojom::BarcodeDetectionRequest request,
     mojom::BarcodeDetectorOptionsPtr options) {
-  // SDK 10.14 has troubles using the Vision Framework, fall back to CIDetector.
-  // TODO(crbug.com/921968) remove this extra case and use the Vision Framework.
-  if (@available(macOS 10.14, *)) {
-    mojo::MakeStrongBinding(std::make_unique<BarcodeDetectionImplMac>(),
-                            std::move(request));
-    return;
-  }
-
-  // Vision Framework needs at least MAC OS X 10.13.
+  // Vision Framework needs at least MAC OS X 10.13. Stay vigilant: past OS
+  // upgrades (e.g. to 10.14, see crbug.com/921968) have broken it.
   if (@available(macOS 10.13, *)) {
     auto impl =
         std::make_unique<BarcodeDetectionImplMacVision>(std::move(options));
