a132543eb78058f31b89c7846991f126d732e9ce
Boris Sazonov <bsazonov@chromium.org>
2019-03-06 12:56:17 +0000

    [Signin][Mice] Start 'Add account' flow while processing Mirror header
    If Mirror header has a service type GAIA_SERVICE_TYPE_SIGNUP or
    GAIA_SERVICE_TYPE_ADDSESSION, start an activity to add an account to the
    device.
    
    Bug: 938554
    Change-Id: Ib2d77afaa3434941c0597026211fa3d03292e456
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1504016
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638092}
    Former-commit-id: 2568baf98ee1d160cb05b0146d8517a1f278cbc5

--- chrome/android/java/src/org/chromium/chrome/browser/signin/AccountManagementScreenHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/AccountManagementScreenHelper.java
@@ -4,12 +4,19 @@
 
 package org.chromium.chrome.browser.signin;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
 import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.chrome.browser.ChromeFeatureList;
-import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.profiles.ProfileAccountManagementMetrics;
+import org.chromium.chrome.browser.util.IntentUtils;
+import org.chromium.components.signin.AccountManagerFacade;
 import org.chromium.components.signin.GAIAServiceType;
+import org.chromium.ui.base.WindowAndroid;
 
 /**
  * Stub entry points and implementation interface for the account management fragment delegate.
@@ -17,15 +24,40 @@ import org.chromium.components.signin.GAIAServiceType;
 public class AccountManagementScreenHelper {
     @CalledByNative
     private static void openAccountManagementScreen(
-            Profile profile, @GAIAServiceType int gaiaServiceType) {
+            WindowAndroid windowAndroid, @GAIAServiceType int gaiaServiceType) {
         ThreadUtils.assertOnUiThread();
 
         if (ChromeFeatureList.isEnabled(ChromeFeatureList.MOBILE_IDENTITY_CONSISTENCY)) {
+            if (gaiaServiceType == GAIAServiceType.GAIA_SERVICE_TYPE_SIGNUP
+                    || gaiaServiceType == GAIAServiceType.GAIA_SERVICE_TYPE_ADDSESSION) {
+                startAddAccountActivity(windowAndroid, gaiaServiceType);
+                return;
+            }
+
             SigninUtils.openSettingsForAllAccounts(ContextUtils.getApplicationContext());
             return;
         }
 
         AccountManagementFragment.openAccountManagementScreen(gaiaServiceType);
+    }
+
+    /**
+     * Tries starting an Activity to add a Google account to the device. If this activity cannot
+     * be started, opens "Accounts" page in the Android Settings app.
+     */
+    private static void startAddAccountActivity(
+            WindowAndroid windowAndroid, @GAIAServiceType int gaiaServiceTypeSignup) {
+        logEvent(ProfileAccountManagementMetrics.DIRECT_ADD_ACCOUNT, gaiaServiceTypeSignup);
+
+        AccountManagerFacade.get().createAddAccountIntent((@Nullable Intent intent) -> {
+            Activity activity = windowAndroid.getActivity().get();
+            if (intent == null || activity == null
+                    || !IntentUtils.safeStartActivity(activity, intent)) {
+                // Failed to create or show an intent, open settings for all accounts so
+                // the user has a chance to create an account manually.
+                SigninUtils.openSettingsForAllAccounts(ContextUtils.getApplicationContext());
+            }
+        });
     }
 
     /**
