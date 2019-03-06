0150b308449219099a01078880aff5dee99a3a5f
Darren Shen <shend@chromium.org>
2019-03-06 04:53:34 +0000

    [sheriff] Disable failing QuartcEndpointTest tests.
    Disabling:
    
    QuartcEndpointTest.ServerAcceptsOldVersion
    QuartcEndpointTest.ServerNegotiatesForOldVersion
    
    Culprit CL could not be reverted due to merge conflict, so disabling
    tests instead.
    
    TBR=vasilvv@chromium.org,rch@chromium.org
    NOTRY=true
    
    Bug: 938773
    Change-Id: Id0c78e8d88519de1cfe62e2e5822e5b7db236f9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1504598
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638012}
    Former-commit-id: fa6cb1c602bbc75b2d996db45008aa88a873f347

--- net/third_party/quic/quartc/quartc_endpoint_test.cc
+++ net/third_party/quic/quartc/quartc_endpoint_test.cc
@@ -80,7 +80,7 @@ TEST_F(QuartcEndpointTest, ClientCreatesSessionAsynchronously) {
 
 // Tests that the server can negotiate for an older QUIC version if the client
 // attempts to connect using a newer version.
-TEST_F(QuartcEndpointTest, ServerNegotiatesForOldVersion) {
+TEST_F(QuartcEndpointTest, DISABLED_ServerNegotiatesForOldVersion) {
   // Note: for this test, we need support for two versions.  Which two shouldn't
   // matter, but they must be enabled so that the version manager doesn't filter
   // them out.
@@ -124,7 +124,7 @@ TEST_F(QuartcEndpointTest, ServerNegotiatesForOldVersion) {
 
 // Tests that the server can accept connections from clients that use older
 // QUIC versions.
-TEST_F(QuartcEndpointTest, ServerAcceptsOldVersion) {
+TEST_F(QuartcEndpointTest, DISABLED_ServerAcceptsOldVersion) {
   // Note: for this test, we need support for two versions.  Which two shouldn't
   // matter, but they must be enabled so that the version manager doesn't filter
   // them out.
