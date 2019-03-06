0b4b04a9b81cf6a55e8d384fbf1c7a2e9feb9ed9
Tina Wang <tinazwang@chromium.org>
2019-03-06 02:16:56 +0000

    [SendTabToSelf] Add DesktopNotificationHandler class to handle notification interaction on desktop
    Add a new notification type: SEND_TAB_TO_SELF
    
    DesktopNotificaitonHandler inherits NotificationHandler and implements ReceivingUiHandler.
    
    This CL depends on https://chromium-review.googlesource.com/c/chromium/src/+/1477754
    
    Bug: 937693
    Change-Id: I96f8ee3c3de914c6742860b0f9f3650325fc8b07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1476103
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#637969}
    Former-commit-id: b02b5e03b07093e200dd71ac4d1d10a2ac9afb0e

---
+++ chrome/browser/send_tab_to_self/desktop_notification_handler.h
@@ -0,0 +1,53 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SEND_TAB_TO_SELF_DESKTOP_NOTIFICATION_HANDLER_H_
+#define CHROME_BROWSER_SEND_TAB_TO_SELF_DESKTOP_NOTIFICATION_HANDLER_H_
+
+#include <string>
+
+#include "chrome/browser/notifications/notification_handler.h"
+#include "chrome/browser/send_tab_to_self/receiving_ui_handler.h"
+
+class Profile;
+
+namespace send_tab_to_self {
+
+class SendTabToSelfEntry;
+
+// Handler for desktop notifications shown by SendTabToSelf.
+// Will only be used on desktop platform.
+// Will be created and owned by the NativeNotificationDisplayService.
+class DesktopNotificationHandler : public NotificationHandler,
+                                   public ReceivingUiHandler {
+ public:
+  explicit DesktopNotificationHandler(Profile* profile);
+  ~DesktopNotificationHandler() override;
+
+  // ReceivingUiHandler implementation.
+  void DisplayNewEntry(const SendTabToSelfEntry* entry) override;
+  void DismissEntry(const SendTabToSelfEntry* entry) override;
+
+  // NotificationHandler implementation.
+  void OnClose(Profile* profile,
+               const GURL& origin,
+               const std::string& notification_id,
+               bool by_user,
+               base::OnceClosure completed_closure) override;
+
+  void OnClick(Profile* profile,
+               const GURL& origin,
+               const std::string& notification_id,
+               const base::Optional<int>& action_index,
+               const base::Optional<base::string16>& reply,
+               base::OnceClosure completed_closure) override;
+
+ protected:
+  Profile* profile_;
+  DISALLOW_COPY_AND_ASSIGN(DesktopNotificationHandler);
+};
+
+}  // namespace send_tab_to_self
+
+#endif  // CHROME_BROWSER_SEND_TAB_TO_SELF_DESKTOP_NOTIFICATION_HANDLER_H_
