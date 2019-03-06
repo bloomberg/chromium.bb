8b3d73b0fdf57bda205b7859561443dfbab3950c
Stephen Martinis <martiniss@chromium.org>
2019-03-06 18:39:51 +0000

    Add merge script directory
    Most of these merge scripts lived in tools/build. These are moving
    source side so that:
    * Devs can find it easily
    * Tryjobs can test changes to it
    * Different trigger scripts can share and re-use code
    
    Bug: 928577
    Change-Id: I6a78164a3b5dfe142983ec111756bd60f838b2a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1479171
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Marc-Antoine Ruel <maruel@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Stephen Martinis <martiniss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638207}
    Former-commit-id: 3c3b0e538cf31dfe74b199e1935eae21b65731fc

--- testing/merge_scripts/PRESUBMIT.py
+++ testing/merge_scripts/PRESUBMIT.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Top-level presubmit script for testing/trigger_scripts.
+"""Top-level presubmit script for testing/merge_scripts.
 
 See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details about the presubmit API built into depot_tools.
@@ -10,7 +10,7 @@ for more details about the presubmit API built into depot_tools.
 
 def CommonChecks(input_api, output_api):
   return input_api.canned_checks.RunUnitTestsInDirectory(
-      input_api, output_api, '.', whitelist=['.*test.py'])
+      input_api, output_api, '.', [ r'^.+_unittest\.py$'])
 
 def CheckChangeOnUpload(input_api, output_api):
   return CommonChecks(input_api, output_api)
