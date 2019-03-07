7f920e04e1229867cf45eda12aa776ccca41a8f1
Jérôme Lebel <jlebel@chromium.org>
2019-03-07 00:17:09 +0000

    [iOS] Renaming TableViewImageCell.titleLabel to TableViewImageCell.textLabel
    Some of the code were using TableViewImageCell.textLabel, without compiler
    errors, since UITableView.textLabel exists. To avoid any errors, titleLabel
    is renamed into textLabel.
    
    Bug introduced:
    crrev.com/c/1478795
    
    Change-Id: I5c07d31565d7c23a212a44103371a311ef327e66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1503575
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638352}
    Former-commit-id: af3e9c30d10a2a578c83765c2ec4b0d31bb2f475

--- ios/chrome/browser/ui/table_view/cells/table_view_image_item.h
+++ ios/chrome/browser/ui/table_view/cells/table_view_image_item.h
@@ -15,7 +15,7 @@
 
 // The image in the cell. If nil, won't be added to the view hierarchy.
 @property(nonatomic, readwrite, strong) UIImage* image;
-// The title label in the cell.
+// The text label in the cell.
 @property(nonatomic, readwrite, copy) NSString* title;
 // UIColor for the cell's textLabel. ChromeTableViewStyler's |cellTitleColor|
 // takes precedence over black color, but not over |textColor|.
@@ -34,13 +34,14 @@
 
 @end
 
-// TableViewImageCell contains a favicon, a title, and an optional chevron.
+// TableViewImageCell contains a favicon, a text, an optional detail text and an
+// optional chevron.
 @interface TableViewImageCell : TableViewCell
 
 // The cell favicon imageView.
 @property(nonatomic, readonly, strong) UIImageView* imageView;
-// The cell title.
-@property(nonatomic, readonly, strong) UILabel* titleLabel;
+// The cell text.
+@property(nonatomic, readonly, strong) UILabel* textLabel;
 // The cell detail text.
 @property(nonatomic, readonly, strong) UILabel* detailTextLabel;
 
