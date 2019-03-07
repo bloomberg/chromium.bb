9fc7275f2214bc298927c3023f2f817f1c0d4ebe
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2019-03-07 02:15:58 +0000

    desktop-pwas: Introduce TestAppRegistrar in PendingBookmarkAppManager unit tests
    Now that PendingBookmarkAppManager doesn't access ExtensionRegistry and
    ExtensionPrefs directly for installations, we can avoid faking some cases
    in tests.
    
    PendingBookmarkAppManager still uses ExtensionRegistry for
    uninstallations so we still need to fake some calls to it. Once
    we abstract uninstallation, we can remove all usage of ExtensionRegistry
    from tests.
    
    Bug: 916381
    Change-Id: Ied92df67f8cb6ab7d07ffa5ea64b487e5d50d6e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1502258
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638389}
    Former-commit-id: 8784a0af522a9233c0fcc1b06579ade09d87e780

---
+++ chrome/browser/web_applications/test/test_app_registrar.cc
@@ -0,0 +1,41 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/web_applications/test/test_app_registrar.h"
+
+#include "base/callback.h"
+#include "base/stl_util.h"
+
+namespace web_app {
+
+TestAppRegistrar::TestAppRegistrar() = default;
+
+TestAppRegistrar::~TestAppRegistrar() = default;
+
+void TestAppRegistrar::AddAsInstalled(const AppId& app_id) {
+  installed_apps_.insert(app_id);
+}
+
+void TestAppRegistrar::RemoveAsInstalled(const AppId& app_id) {
+  DCHECK(base::ContainsKey(installed_apps_, app_id));
+  installed_apps_.erase(app_id);
+}
+
+void TestAppRegistrar::AddAsExternalAppUninstalledByUser(const AppId& app_id) {
+  DCHECK(!base::ContainsKey(uninstalled_external_apps_, app_id));
+  uninstalled_external_apps_.insert(app_id);
+}
+
+void TestAppRegistrar::Init(base::OnceClosure callback) {}
+
+bool TestAppRegistrar::IsInstalled(const AppId& app_id) const {
+  return base::ContainsKey(installed_apps_, app_id);
+}
+
+bool TestAppRegistrar::WasExternalAppUninstalledByUser(
+    const AppId& app_id) const {
+  return base::ContainsKey(uninstalled_external_apps_, app_id);
+}
+
+}  // namespace web_app
