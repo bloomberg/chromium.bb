1292a9806796abef1caeba837ed1c05a79e902d6
tzik <tzik@chromium.org>
2019-03-07 08:17:28 +0000

    Adopt to new MicrotasksCompletedCallback signature
    V8 updated the signature of MicrotasksCompletedCallback to take a void
    pointer user data, and is deprecating the old signature.
    This CL updates its user to use the new one.
    
    Change-Id: I24990f68b2964fa80554689947746636958f59cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1504660
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638471}
    Former-commit-id: 1bda93b7333e2b623f66537b84b902432057a89d

--- third_party/blink/renderer/platform/bindings/v8_per_isolate_data.cc
+++ third_party/blink/renderer/platform/bindings/v8_per_isolate_data.cc
@@ -67,7 +67,7 @@ static void BeforeCallEnteredCallback(v8::Isolate* isolate) {
   CHECK(!ScriptForbiddenScope::IsScriptForbidden());
 }
 
-static void MicrotasksCompletedCallback(v8::Isolate* isolate) {
+static void MicrotasksCompletedCallback(v8::Isolate* isolate, void*) {
   V8PerIsolateData::From(isolate)->RunEndOfScopeTasks();
 }
 
