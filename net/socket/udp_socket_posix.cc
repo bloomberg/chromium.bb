96760d36c0970c96754c49b14d0898887143f27c
Tom Anderson <thomasanderson@chromium.org>
2019-03-07 01:53:40 +0000

    Fix improper usage of std::vector in UDPSocketPosixSender
    The issue is that |msg_iov| and |msgvec| use std::vector as the underlying
    container, but the usage of the container is something like:
    
    msg_iov.reserve(kWriteAsyncMaxBuffersThreshold + 1);
    msg_iov[0].field = ...;  // Access an element that doesn't yet exist.
    
    This CL changes the usage to msg_iov->push_back(...) so that the underlying
    vector updates its size and operator[] will work.
    
    This is necessary to fix the build with _LIBCPP_DEBUG=0.  Otherwise we get
    errors of the form:
    
    buildtools/third_party/libc++/trunk/include/vector:1542: _LIBCPP_ASSERT '__n <
    size()' failed. vector[] index out of bounds
    
    BUG=923166
    R=davidben
    
    Change-Id: I364be618011c4c0e65627e530dde9e6dc3f7375f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1506384
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638382}
    Former-commit-id: d1a89a795854e6608588d8d7bcf2277fd31b2d87

--- net/socket/udp_socket_posix.cc
+++ net/socket/udp_socket_posix.cc
@@ -1196,17 +1196,12 @@ SendResult UDPSocketPosixSender::InternalSendmmsgBuffers(
     DatagramBuffers buffers) const {
   base::StackVector<struct iovec, kWriteAsyncMaxBuffersThreshold + 1> msg_iov;
   base::StackVector<struct mmsghdr, kWriteAsyncMaxBuffersThreshold + 1> msgvec;
-  int i = 0;
-  for (auto& buffer : buffers) {
-    msg_iov[i].iov_base = const_cast<char*>(buffer->data());
-    msg_iov[i].iov_len = buffer->length();
-    i++;
-  }
-  for (size_t j = 0; j < buffers.size(); j++) {
-    std::memset(&msgvec[j], 0, sizeof(msgvec[j]));
-    msgvec[j].msg_hdr.msg_iov = &msg_iov[j];
-    msgvec[j].msg_hdr.msg_iovlen = 1;
-  }
+  msg_iov->reserve(buffers.size());
+  for (auto& buffer : buffers)
+    msg_iov->push_back({const_cast<char*>(buffer->data()), buffer->length()});
+  msgvec->reserve(buffers.size());
+  for (size_t j = 0; j < buffers.size(); j++)
+    msgvec->push_back({{nullptr, 0, &msg_iov[j], 1, nullptr, 0, 0}, 0});
   int result = HANDLE_EINTR(Sendmmsg(fd, &msgvec[0], buffers.size(), 0));
   SendResult send_result(0, 0, std::move(buffers));
   if (result < 0) {
