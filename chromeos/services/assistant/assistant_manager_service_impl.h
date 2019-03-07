11c6beaa3539d99dff3e808759c2d83d306cd255
Sammie Quon <sammiequon@chromium.org>
2019-03-07 07:05:54 +0000

    Revert "Launch Android Intent for media playback."
    This reverts commit 4abad9dcec3491218b96643bccbade5288b04b6c [formerly fb0e628497c513215b458fc2236406f7ab7eafb0].
    
    Reason for revert: crbug.com/939241. Breaking chrome informationals.
    
    Original change's description:
    > Launch Android Intent for media playback.
    > 
    > Bug: b:124796759
    > Test: Manual
    > Change-Id: I197f984578e9f80096ada1701e0399058bf9e848
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1480726
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    > Commit-Queue: Li Lin <llin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#638359}
    
    TBR=dcheng@chromium.org,xiaohuic@chromium.org,updowndota@chromium.org,llin@chromium.org
    
    Change-Id: If851181e745e76170383e3355f231dda2fe1285e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: b:124796759
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1507255
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638456}
    Former-commit-id: 9769b3c4e692b57fe9ca6b4f40a36c6be81003b7

--- chromeos/services/assistant/assistant_manager_service_impl.h
+++ chromeos/services/assistant/assistant_manager_service_impl.h
@@ -196,8 +196,6 @@ class AssistantManagerServiceImpl
       const action::InteractionInfo& interaction,
       std::vector<mojom::AndroidAppInfoPtr> apps_info);
 
-  void HandleLaunchMediaIntentResponse(bool app_opened);
-
   void OnConversationTurnStartedOnMainThread(bool is_mic_open);
   void OnConversationTurnFinishedOnMainThread(
       assistant_client::ConversationStateListener::Resolution resolution);
@@ -218,8 +216,6 @@ class AssistantManagerServiceImpl
   void OnRespondingStartedOnMainThread(bool is_error_response);
   void OnSpeechLevelUpdatedOnMainThread(const float speech_level);
   void OnModifySettingsAction(const std::string& modify_setting_args_proto);
-  void OnOpenMediaAndroidIntent(const std::string play_media_args_proto,
-                                action::AndroidAppInfo* android_app_info);
 
   void RegisterFallbackMediaHandler();
 
