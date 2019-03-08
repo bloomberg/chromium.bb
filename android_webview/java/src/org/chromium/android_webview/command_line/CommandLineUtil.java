bd103b1e069b82b3a8c73af3886390d6217de16b
Ian Vollick <vollick@chromium.org>
2019-03-08 21:36:06 +0000

    Add plumbing for platform-specific command line flag initialization
    Attempts to do platform-specific init when appropriate.
    
    Bug: None
    Change-Id: I234ed64ed115b7110cade6dbced53ed27343061a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1487954
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Auto-Submit: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639173}
    Former-commit-id: 62af14a835efd50cb6528afadd8dee1cd3b11d14

--- android_webview/java/src/org/chromium/android_webview/command_line/CommandLineUtil.java
+++ android_webview/java/src/org/chromium/android_webview/command_line/CommandLineUtil.java
@@ -8,19 +8,37 @@ import android.os.StrictMode;
 
 import org.chromium.base.BuildInfo;
 import org.chromium.base.CommandLine;
+import org.chromium.base.ThreadUtils;
 
 /**
  * Utility class for WebView's CommandLine - this is compiled into a separate target that can be
  * reached from WebView's separate minidump-uploading Services.
  */
 public class CommandLineUtil {
-    private CommandLineUtil() {}
+    protected CommandLineUtil() {}
 
     public static final String WEBVIEW_COMMAND_LINE_FILE = "/data/local/tmp/webview-command-line";
 
     // same switch as kEnableCrashReporterForTesting in //base/base_switches.h
     public static final String CRASH_UPLOADS_ENABLED_FOR_TESTING_SWITCH =
             "enable-crash-reporter-for-testing";
+
+    private static final String COMMAND_LINE_UTIL_INTERNAL =
+            "org.chromium.android_webview.command_line.CommandLineUtilInternal";
+
+    private static CommandLineUtil sInstance;
+
+    private static CommandLineUtil getInstance() {
+        ThreadUtils.assertOnUiThread();
+        if (sInstance != null) return sInstance;
+        try {
+            sInstance = (CommandLineUtil) Class.forName(COMMAND_LINE_UTIL_INTERNAL).newInstance();
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+                | IllegalArgumentException e) {
+            sInstance = new CommandLineUtil();
+        }
+        return sInstance;
+    }
 
     /**
      * Initialize the CommandLine for WebView - this should be initialized on the same thread where
@@ -35,5 +53,8 @@ public class CommandLineUtil {
         } else {
             CommandLine.init(null);
         }
+        getInstance().initCommandLineInternal(CommandLine.getInstance());
     }
+
+    protected void initCommandLineInternal(CommandLine commandLine) {}
 }
