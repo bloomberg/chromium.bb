b235e97d8d679c14925bbf7d11186afe9602ad88
Donna Wu <donna.wu@intel.com>
2019-03-08 14:44:17 +0000

    Use USB mojo interfaces in ExtensionPrinterHandler.
    In extensions::UsbDeviceManager, USB device info list has been kept
    for a new GetDeviceInfo() method and testing support has been added
    to this class. With these enhancement, USB interfaces usage in
    ExtensionPrinterHandler have been converted to mojo version.
    
    Bug: 716628
    Change-Id: I06c3bd5144699e51c088c4d9a7bb67a3cc9013e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1496757
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Donna Wu <donna.wu@intel.com>
    Cr-Commit-Position: refs/heads/master@{#639003}
    Former-commit-id: dba62879c578e762921155379ced7e6007568e1f

--- chrome/browser/ui/webui/print_preview/extension_printer_handler.cc
+++ chrome/browser/ui/webui/print_preview/extension_printer_handler.cc
@@ -20,15 +20,12 @@
 #include "chrome/browser/ui/webui/print_preview/print_preview_utils.h"
 #include "components/cloud_devices/common/cloud_device_description.h"
 #include "components/cloud_devices/common/printer_description.h"
-#include "device/base/device_client.h"
-#include "device/usb/mojo/type_converters.h"
 #include "device/usb/public/mojom/device.mojom.h"
-#include "device/usb/usb_device.h"
-#include "device/usb/usb_service.h"
 #include "extensions/browser/api/device_permissions_manager.h"
 #include "extensions/browser/api/printer_provider/printer_provider_api.h"
 #include "extensions/browser/api/printer_provider/printer_provider_api_factory.h"
 #include "extensions/browser/api/printer_provider/printer_provider_print_job.h"
+#include "extensions/browser/api/usb/usb_device_manager.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/common/api/printer_provider/usb_printer_manifest_data.h"
 #include "extensions/common/permissions/permissions_data.h"
@@ -38,12 +35,12 @@
 #include "printing/print_job_constants.h"
 #include "printing/pwg_raster_settings.h"
 
-using device::UsbDevice;
 using extensions::DevicePermissionsManager;
 using extensions::DictionaryBuilder;
 using extensions::Extension;
 using extensions::ExtensionRegistry;
 using extensions::ListBuilder;
+using extensions::UsbDeviceManager;
 using extensions::UsbPrinterManifestData;
 
 namespace printing {
@@ -78,10 +75,11 @@ bool HasUsbPrinterProviderPermissions(const Extension* extension) {
             extensions::APIPermission::kUsb);
 }
 
-std::string GenerateProvisionalUsbPrinterId(const Extension* extension,
-                                            const UsbDevice* device) {
+std::string GenerateProvisionalUsbPrinterId(
+    const Extension* extension,
+    const device::mojom::UsbDeviceInfo& device) {
   return base::StringPrintf("%s:%s:%s", kProvisionalUsbLabel,
-                            extension->id().c_str(), device->guid().c_str());
+                            extension->id().c_str(), device.guid.c_str());
 }
 
 bool ParseProvisionalUsbPrinterId(const std::string& printer_id,
@@ -140,9 +138,10 @@ void ExtensionPrinterHandler::StartGetPrinters(
   }
 
   if (extension_supports_usb_printers) {
-    device::UsbService* service = device::DeviceClient::Get()->GetUsbService();
     pending_enumeration_count_++;
-    service->GetDevices(
+    UsbDeviceManager* usb_manager = UsbDeviceManager::Get(profile_);
+    DCHECK(usb_manager);
+    usb_manager->GetDevices(
         base::Bind(&ExtensionPrinterHandler::OnUsbDevicesEnumerated,
                    weak_ptr_factory_.GetWeakPtr(), callback));
   }
@@ -215,8 +214,11 @@ void ExtensionPrinterHandler::StartGrantPrinterAccess(
     return;
   }
 
-  device::UsbService* service = device::DeviceClient::Get()->GetUsbService();
-  scoped_refptr<UsbDevice> device = service->GetDevice(device_guid);
+  UsbDeviceManager* usb_manager = UsbDeviceManager::Get(profile_);
+  DCHECK(usb_manager);
+
+  const device::mojom::UsbDeviceInfo* device =
+      usb_manager->GetDeviceInfo(device_guid);
   if (!device) {
     std::move(callback).Run(base::DictionaryValue());
     return;
@@ -224,12 +226,10 @@ void ExtensionPrinterHandler::StartGrantPrinterAccess(
 
   DevicePermissionsManager* permissions_manager =
       DevicePermissionsManager::Get(profile_);
-  auto device_info = device::mojom::UsbDeviceInfo::From(*device);
-  DCHECK(device_info);
-  permissions_manager->AllowUsbDevice(extension_id, *device_info);
+  permissions_manager->AllowUsbDevice(extension_id, *device);
 
   GetPrinterProviderAPI(profile_)->DispatchGetUsbPrinterInfoRequested(
-      extension_id, *device_info,
+      extension_id, *device,
       base::BindOnce(&ExtensionPrinterHandler::WrapGetPrinterInfoCallback,
                      weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
 }
@@ -318,7 +318,7 @@ void ExtensionPrinterHandler::WrapGetPrinterInfoCallback(
 
 void ExtensionPrinterHandler::OnUsbDevicesEnumerated(
     const AddedPrintersCallback& callback,
-    const std::vector<scoped_refptr<UsbDevice>>& devices) {
+    std::vector<device::mojom::UsbDeviceInfoPtr> devices) {
   ExtensionRegistry* registry = ExtensionRegistry::Get(profile_);
   DevicePermissionsManager* permissions_manager =
       DevicePermissionsManager::Get(profile_);
@@ -337,8 +337,8 @@ void ExtensionPrinterHandler::OnUsbDevicesEnumerated(
       if (manifest_data->SupportsDevice(*device)) {
         std::unique_ptr<extensions::UsbDevicePermission::CheckParam> param =
             extensions::UsbDevicePermission::CheckParam::ForUsbDevice(
-                extension.get(), device.get());
-        if (device_permissions->FindUsbDeviceEntry(device) ||
+                extension.get(), *device);
+        if (device_permissions->FindUsbDeviceEntry(*device) ||
             extension->permissions_data()->CheckAPIPermissionWithParam(
                 extensions::APIPermission::kUsbDevice, param.get())) {
           // Skip devices the extension already has permission to access.
@@ -347,13 +347,14 @@ void ExtensionPrinterHandler::OnUsbDevicesEnumerated(
 
         printer_list.Append(
             DictionaryBuilder()
-                .Set("id", GenerateProvisionalUsbPrinterId(extension.get(),
-                                                           device.get()))
+                .Set("id",
+                     GenerateProvisionalUsbPrinterId(extension.get(), *device))
                 .Set("name",
                      DevicePermissionsManager::GetPermissionMessage(
-                         device->vendor_id(), device->product_id(),
-                         device->manufacturer_string(),
-                         device->product_string(), base::string16(), false))
+                         device->vendor_id, device->product_id,
+                         device->manufacturer_name.value_or(base::string16()),
+                         device->product_name.value_or(base::string16()),
+                         base::string16(), false))
                 .Set("extensionId", extension->id())
                 .Set("extensionName", extension->name())
                 .Set("provisional", true)
