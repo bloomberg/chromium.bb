a8abc863573df28b84390cf1626803aabb9e3692
Chia-hung Duan <chiahungduan@google.com>
2019-03-08 18:59:09 +0000

    Reland 1: Enable Heap Profiling Service in Chromecast.
    This reverts commit 208ad6e783a3eb9546ceba9740e51b5e11e9ebd7 [formerly 61b2f393207c02c4d19317a8c45cf4f62be8c2d6].
    
    Reason for revert: The original CL caused build break because of lacking
    sender/receiver pipes implementation for Fuchsia. Thus when we build the
    target os as Fuchsia, it'll bump into link error. Reland it by excluding
    Fuchsia, will bring it back, see crbug.com/933142
    
    Because the way we generate manifest files changed, this CL updates that
    as well. In summary, this CL excluds Fuchsia from supporting target and
    use manifest builder for generating manifests.
    
    Bug: crbug.com/938784
    Change-Id: Idbbef7e5f13b59fbfa9cf6a23e1c982155c88218
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1452397
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Commit-Queue: Chia-hung Duan <chiahungduan@google.com>
    Cr-Commit-Position: refs/heads/master@{#639094}
    Former-commit-id: 55226505e9a45ae301243627c1517851e112c458

--- chromecast/utility/cast_content_utility_client.h
+++ chromecast/utility/cast_content_utility_client.h
@@ -6,8 +6,10 @@
 #define CHROMECAST_UTILITY_CAST_CONTENT_UTILITY_CLIENT_H_
 
 #include <memory>
+#include <string>
 
 #include "base/macros.h"
+#include "build/build_config.h"
 #include "content/public/utility/content_utility_client.h"
 
 namespace chromecast {
@@ -18,6 +20,13 @@ class CastContentUtilityClient : public content::ContentUtilityClient {
   static std::unique_ptr<CastContentUtilityClient> Create();
 
   CastContentUtilityClient() {}
+
+#if !defined(OS_FUCHSIA)
+  // content::ContentUtilityClient implementation:
+  bool HandleServiceRequest(
+      const std::string& service_name,
+      service_manager::mojom::ServiceRequest request) override;
+#endif  // !defined(OS_FUCHSIA)
 
  private:
   DISALLOW_COPY_AND_ASSIGN(CastContentUtilityClient);
