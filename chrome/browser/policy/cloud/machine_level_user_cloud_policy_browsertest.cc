41934bdaa9f5313665aa2c8511fd469941884717
Owen Min <zmin@chromium.org>
2019-03-08 14:29:46 +0000

    Fix flaky MachineLevelUserCloudPolicyPolicyFetchTest
    The MachineLevelUserCloudPolicyPolicyFetchTest is timeout on Win7. It
    might be caused by the race condition that when we check the
    |last_policy_timestamp| of CloudPolicyClient, the policy has been fetched
    but not validated. Hence the OnPolicyFetched event is never triggered
    after observer created.
    
    The fix will be listen to the CloudPolicyStore for any valid DM token test
    case. Because OnStoreLoaded will always be called when the timestamp is set.
    And only observe to CloudPolicyClient iff the DM token is not valid in which
    case there is no CloudPolicyStore event.
    
    Bug: 939261
    Change-Id: I5e7698643474ca639bb516f89363ba32537f0782
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1509642
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639001}
    Former-commit-id: 5629ca7a1f66dfa333ba0705438aa0f3dd752e26

--- chrome/browser/policy/cloud/machine_level_user_cloud_policy_browsertest.cc
+++ chrome/browser/policy/cloud/machine_level_user_cloud_policy_browsertest.cc
@@ -237,12 +237,7 @@ class PolicyFetchClientObserver : public CloudPolicyClient::Observer {
   ~PolicyFetchClientObserver() override { client_->RemoveObserver(this); }
 
   void OnPolicyFetched(CloudPolicyClient* client) override {
-    // The policy is fetched, wait for the policy is validated and stored.
-    store_observer_ = std::make_unique<PolicyFetchStoreObserver>(
-        g_browser_process->browser_policy_connector()
-            ->machine_level_user_cloud_policy_manager()
-            ->store(),
-        std::move(quit_closure_));
+    std::move(quit_closure_).Run();
   }
 
   void OnRegistrationStateChanged(CloudPolicyClient* client) override {}
@@ -626,14 +621,7 @@ class MachineLevelUserCloudPolicyPolicyFetchTest
   DISALLOW_COPY_AND_ASSIGN(MachineLevelUserCloudPolicyPolicyFetchTest);
 };
 
-// Crashes on Win only.  http://crbug.com/939261
-#if defined(OS_WIN)
-#define MAYBE_Test DISABLED_Test
-#else
-#define MAYBE_Test Test
-#endif
-
-IN_PROC_BROWSER_TEST_P(MachineLevelUserCloudPolicyPolicyFetchTest, MAYBE_Test) {
+IN_PROC_BROWSER_TEST_P(MachineLevelUserCloudPolicyPolicyFetchTest, Test) {
   MachineLevelUserCloudPolicyManager* manager =
       g_browser_process->browser_policy_connector()
           ->machine_level_user_cloud_policy_manager();
@@ -641,8 +629,18 @@ IN_PROC_BROWSER_TEST_P(MachineLevelUserCloudPolicyPolicyFetchTest, MAYBE_Test) {
   // If the policy hasn't been updated, wait for it.
   if (manager->core()->client()->last_policy_timestamp().is_null()) {
     base::RunLoop run_loop;
-    PolicyFetchClientObserver observer(manager->core()->client(),
-                                       run_loop.QuitClosure());
+    // Listen to store event which is fired after policy validation if token is
+    // valid. Otherwise listen to the client event because there is no store
+    // event.
+    std::unique_ptr<PolicyFetchClientObserver> client_observer;
+    std::unique_ptr<PolicyFetchStoreObserver> store_observer;
+    if (dm_token() == kInvalidDMToken) {
+      client_observer = std::make_unique<PolicyFetchClientObserver>(
+          manager->core()->client(), run_loop.QuitClosure());
+    } else {
+      store_observer = std::make_unique<PolicyFetchStoreObserver>(
+          manager->store(), run_loop.QuitClosure());
+    }
     g_browser_process->browser_policy_connector()
         ->device_management_service()
         ->ScheduleInitialization(0);
