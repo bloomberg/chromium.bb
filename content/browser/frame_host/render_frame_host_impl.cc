5801fa2c9b1d49778e49eddd501d1a75f71dda7f
Adithya Srinivasan <adithyas@chromium.org>
2019-03-08 21:40:12 +0000

    Portals: Disallow Portals inside nested browsing contexts
    Bug: 938634
    Change-Id: Id9c46473a484531b5280c407e503948a2593f45f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1504298
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Lucas Gadani <lfg@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639176}
    Former-commit-id: d7fe884c4143c16129fa5e6d145d304c3efaf2c6

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -3826,6 +3826,12 @@ void RenderFrameHostImpl::CreateNewWindow(
 
 void RenderFrameHostImpl::CreatePortal(blink::mojom::PortalRequest request,
                                        CreatePortalCallback callback) {
+  // We don't support attaching a portal inside a nested browsing context.
+  if (frame_tree_node()->parent()) {
+    mojo::ReportBadMessage(
+        "RFHI::CreatePortal called in a nested browsing context");
+    return;
+  }
   Portal* portal = Portal::Create(this, std::move(request));
   RenderFrameProxyHost* proxy_host = portal->CreateProxyAndAttachPortal();
   std::move(callback).Run(proxy_host->GetRoutingID(), portal->portal_token());
