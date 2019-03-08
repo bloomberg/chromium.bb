5c3e129d3cbb370ec2bbd1e94c1ca243c23ca97c
Adithya Srinivasan <adithyas@chromium.org>
2019-03-08 21:40:12 +0000

    Portals: Disallow Portals inside nested browsing contexts
    Bug: 938634
    Change-Id: Id9c46473a484531b5280c407e503948a2593f45f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1504298
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Lucas Gadani <lfg@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639176}
    Former-commit-id: d7fe884c4143c16129fa5e6d145d304c3efaf2c6

--- third_party/blink/renderer/core/html/portal/html_portal_element.cc
+++ third_party/blink/renderer/core/html/portal/html_portal_element.cc
@@ -17,6 +17,7 @@
 #include "third_party/blink/renderer/core/html/parser/html_parser_idioms.h"
 #include "third_party/blink/renderer/core/html/portal/document_portals.h"
 #include "third_party/blink/renderer/core/html_names.h"
+#include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/core/layout/layout_iframe.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
@@ -76,6 +77,14 @@ HTMLPortalElement::InsertionNotificationRequest HTMLPortalElement::InsertedInto(
   auto result = HTMLFrameOwnerElement::InsertedInto(node);
 
   Document& document = GetDocument();
+
+  // We don't support embedding portals in nested browsing contexts.
+  if (!document.GetFrame()->IsMainFrame()) {
+    document.AddConsoleMessage(ConsoleMessage::Create(
+        kRenderingMessageSource, mojom::ConsoleMessageLevel::kWarning,
+        "Cannot use <portal> in a nested browsing context."));
+    return result;
+  }
 
   if (node.IsInDocumentTree() && document.IsHTMLDocument()) {
     std::tie(portal_frame_, portal_token_) =
