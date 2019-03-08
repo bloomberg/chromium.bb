4183d8ee93627939485a83c34a389a8de7107446
Robert Liao <robliao@chromium.org>
2019-03-08 21:33:29 +0000

    Revert "Reland "[DevTools] Add a flag --enable-devtools-binary-protocol for DevToolsSession.""
    This reverts commit 6a32b1e73bcfd4a0eb8f7154f59da33474030d24 [formerly a741fa4abd37baa4d8618c27b5c8a3b5e0c53fcd].
    
    Reason for revert: Implicated in breaking the win-rel bot
    https://ci.chromium.org/p/chromium/builders/ci/win-rel/12337
    
    ../../third_party/inspector_protocol/encoding/cbor_test.cc(253,25): error: no matching constructor for initialization of 'base::StringPiece16' (aka 'BasicStringPiece<basic_string<wchar_t, char_traits<wchar_t>, allocator<wchar_t> > >')
          base::UTF16ToUTF8(base::StringPiece16(decoded.data(), decoded.size()));
                            ^                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Original change's description:
    > Reland "[DevTools] Add a flag --enable-devtools-binary-protocol for DevToolsSession."
    > 
    > This is a reland of 4ef08f91e536610981396294289150fb5385c162 [formerly 6ab258258c8ba0c7ebda22756062abc720859b6a]
    > 
    > Original change's description:
    > > [DevTools] Add a flag --enable-devtools-binary-protocol for DevToolsSession.
    > > 
    > > When the flag is enabled, we send messages to agents / handlers as CBOR,
    > > even if the cient sends us JSON. We convert responses back to JSON if
    > > needed.
    > > 
    > > Change-Id: I6123e38262d6eeb82206d28a936a32f7db8235b8
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1507232
    > > Commit-Queue: Johannes Henkel <johannes@chromium.org>
    > > Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    > > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#638897}
    > 
    > Change-Id: I20975e3119d3ac65a3e4294ea7cf0bf2d9516389
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1512336
    > Reviewed-by: Johannes Henkel <johannes@chromium.org>
    > Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    > Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#639164}
    
    TBR=dgozman@chromium.org,caseq@chromium.org,pfeldman@chromium.org,johannes@chromium.org
    
    Change-Id: Ib5506d22bccdb58a4eb6abc50da2bd6609b3472a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1512141
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639169}
    Former-commit-id: 6874d49d3153dd8a77339bc66b2a4885aa14c2db

--- chrome/browser/devtools/chrome_devtools_session.cc
+++ chrome/browser/devtools/chrome_devtools_session.cc
@@ -11,7 +11,6 @@
 #include "content/public/browser/devtools_agent_host.h"
 #include "content/public/browser/devtools_agent_host_client.h"
 #include "content/public/browser/devtools_manager_delegate.h"
-#include "content/public/common/content_switches.h"
 
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/devtools/protocol/window_manager_handler.h"
@@ -53,12 +52,6 @@ void ChromeDevToolsSession::sendProtocolResponse(
   client_->DispatchProtocolMessage(agent_host_, message->serialize(binary));
 }
 
-static bool EnableInternalDevToolsBinaryProtocol() {
-  static bool enabled = base::CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kEnableInternalDevToolsBinaryProtocol);
-  return enabled;
-}
-
 void ChromeDevToolsSession::HandleCommand(
     const std::string& method,
     const std::string& message,
@@ -70,14 +63,9 @@ void ChromeDevToolsSession::HandleCommand(
 
   int call_id;
   std::string unused;
-  // We also check for --enable-internal-dev-tools-binary-protocol here,
-  // because if this flag is set, then content::DevToolsSession will
-  // send us binary even if the |client_| did not ask for it.
-  bool binary =
-      client_->UsesBinaryProtocol() || EnableInternalDevToolsBinaryProtocol();
   std::unique_ptr<protocol::DictionaryValue> value =
-      protocol::DictionaryValue::cast(
-          protocol::StringUtil::parseMessage(message, binary));
+      protocol::DictionaryValue::cast(protocol::StringUtil::parseMessage(
+          message, client_->UsesBinaryProtocol()));
   if (!dispatcher_->parseCommand(value.get(), &call_id, &unused))
     return;
   pending_commands_[call_id] = std::move(callback);
