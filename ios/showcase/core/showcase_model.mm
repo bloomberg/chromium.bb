3e4f27fccf3d47a4e6c7118a89c614b498f4fa06
Robbie Gibson <rkgibson@google.com>
2019-03-08 12:21:42 +0000

    [iOS] Add OmniboxPopupViewController to Showcase
    Bug: 938280
    Change-Id: I1a19b22609a0b63db48d3a1cdbe6178c690368f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1497140
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#638982}
    Former-commit-id: 2f58e76998c9fb0f702be96fed51b675a6bca878

--- ios/showcase/core/showcase_model.mm
+++ ios/showcase/core/showcase_model.mm
@@ -4,8 +4,6 @@
 
 #import "ios/showcase/core/showcase_model.h"
 
-#include "ios/chrome/browser/ui/util/ui_util.h"
-
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
@@ -103,6 +101,11 @@
       showcase::kClassForDisplayKey : @"RecentTabsTableViewController",
       showcase::kClassForInstantiationKey : @"SCDarkThemeRecentTabsCoordinator",
       showcase::kUseCaseKey : @"Dark theme recent tabs",
+    },
+    @{
+      showcase::kClassForDisplayKey : @"OmniboxPopupViewController",
+      showcase::kClassForInstantiationKey : @"SCOmniboxPopupCoordinator",
+      showcase::kUseCaseKey : @"Omnibox popup table view",
     },
   ];
 }
---
+++ ios/showcase/omnibox_popup/fake_autocomplete_suggestion.h
@@ -0,0 +1,15 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_SHOWCASE_OMNIBOX_POPUP_FAKE_AUTOCOMPLETE_SUGGESTION_H_
+#define IOS_SHOWCASE_OMNIBOX_POPUP_FAKE_AUTOCOMPLETE_SUGGESTION_H_
+
+#import <UIKit/UIKit.h>
+
+#import "ios/chrome/browser/ui/omnibox/autocomplete_suggestion.h"
+
+@interface FakeAutocompleteSuggestion : NSObject <AutocompleteSuggestion>
+@end
+
+#endif  // IOS_SHOWCASE_OMNIBOX_POPUP_FAKE_AUTOCOMPLETE_SUGGESTION_H_
