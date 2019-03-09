b4cbaa75fb98a437d91a0d72f66f012093bb2ad4
Nico Weber <thakis@chromium.org>
2019-03-09 02:32:02 +0000

    Reland "Reland "Enable -Wextra-semi in release builds that don't set dcheck_always_on=true.""
    This is a reland of 369b39ef47e546e14bd248854274431db6404e29 [formerly 5216f4babf120302852acf696a55b2c432c2cfdc]
    
    Original change's description:
    > Reland "Enable -Wextra-semi in release builds that don't set dcheck_always_on=true."
    >
    > The original change broke the iOS build because one Objective-C class used
    > SEQUENCE_CHECKER(), and static_assert() is currently not allowed in Objective-C
    > code. So don't enable -Wextra-semi for Objective-C code and don't expand
    > it to static_assert for now, until (if?) the compiler allows this.
    >
    > Original change's description:
    > > Revert "Enable -Wextra-semi in release builds that don't set dcheck_always_on=true."
    > >
    > > This reverts commit ebddbb820d2fe5d63f55c1aad2653eb23e6211c1 [formerly f6fa5cac2d777af3b53bf3ed699cd1da906664c8].
    > >
    > > Reason for revert: broke https://ci.chromium.org/p/chromium/builders/ci/ios-device/116376
    > >
    > > Original change's description:
    > > > Enable -Wextra-semi in release builds that don't set dcheck_always_on=true.
    > > >
    > > > Most of these are macros that expand to nothing when dchecks are off, but
    > > > to a declaration when dchecks are on. They are meant to be used to declare
    > > > member variables.
    > > >
    > > > Since clang-format gets very confused on class member declarations that
    > > > don't end in a semicolon and since we require clang-formatting,
    > > > we need some way to still permit a semicolon after these macros even
    > > > with dchecks off. To that end, add a dummy static_assert() at the end
    > > > of these macros.
    > > >
    > > > Alternatively, we could surround all uses of these macros with explicit
    > > > DCHECK_IS_ON() checks.
    > > >
    > > > Depends on https://skia-review.googlesource.com/c/skia/+/196421
    > > >
    > > > TBR=dalecurtis
    > > >
    > > > Bug: 926235,936856
    > > > Change-Id: I66ab08f383b2f27dc6a7617f67f33fa66ddfa00c
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/1495041
    > > > Commit-Queue: Nico Weber <thakis@chromium.org>
    > > > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > > > Cr-Commit-Position: refs/heads/master@{#636838}
    > >
    > > TBR=dalecurtis@chromium.org,gab@chromium.org,thakis@chromium.org
    > >
    > > Change-Id: Iba14b13129b16b569249e62afa4644209e1435c6
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Bug: 926235, 936856
    > > Reviewed-on: https://chromium-review.googlesource.com/c/1496308
    > > Reviewed-by: Nico Weber <thakis@chromium.org>
    > > Commit-Queue: Nico Weber <thakis@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#636850}
    >
    > TBR=dalecurtis@chromium.org,gab@chromium.org,thakis@chromium.org
    >
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    >
    > Bug: 926235, 936856
    > Change-Id: Ic6bd087026e8ba257862da1c493d9d54a5ca816d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1511734
    > Commit-Queue: Nico Weber <thakis@chromium.org>
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#639189}
    
    TBR=dalecurtis
    
    Bug: 926235, 936856
    Change-Id: Ie3276441b8a98aff446a348b2a71d1d81643ffb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1513074
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639302}
    Former-commit-id: dcacb338f217d60fb6f1b14cf6131b9d64de687a

--- base/threading/thread_checker.h
+++ base/threading/thread_checker.h
@@ -65,7 +65,7 @@
 #define DCHECK_CALLED_ON_VALID_THREAD(name) DCHECK((name).CalledOnValidThread())
 #define DETACH_FROM_THREAD(name) (name).DetachFromThread()
 #else  // DCHECK_IS_ON()
-#define THREAD_CHECKER(name)
+#define THREAD_CHECKER(name) static_assert(true, "")
 #define DCHECK_CALLED_ON_VALID_THREAD(name) EAT_STREAM_PARAMETERS
 #define DETACH_FROM_THREAD(name)
 #endif  // DCHECK_IS_ON()
