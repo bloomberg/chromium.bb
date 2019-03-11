1947695a25a57165a319b49c78a1b6ae76c982eb
Maksim Sisov <msisov@igalia.com>
2019-03-11 08:12:17 +0000

    [ozone/wayland] add CreateSurfaceCheckGbm test
    This test verifies that GLOzone does not try to create
    SurfacelessViewGL if GbmDevice is not available.
    
    R=rjkroege@chromium.org
    
    Bug: 934253
    Change-Id: I1ac7d5c7323b06e52badbeae95ec975610f38bc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1505332
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639422}
    Former-commit-id: 18969535f077a067c2709b39d3664e73dc902bb5

--- ui/ozone/platform/wayland/wayland_surface_factory_unittest.cc
+++ ui/ozone/platform/wayland/wayland_surface_factory_unittest.cc
@@ -7,6 +7,9 @@
 #include "base/run_loop.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkSurface.h"
+#include "ui/ozone/common/linux/gbm_buffer.h"
+#include "ui/ozone/common/linux/gbm_device.h"
+#include "ui/ozone/platform/wayland/gpu/wayland_connection_proxy.h"
 #include "ui/ozone/platform/wayland/test/mock_surface.h"
 #include "ui/ozone/platform/wayland/test/test_wayland_server_thread.h"
 #include "ui/ozone/platform/wayland/wayland_surface_factory.h"
@@ -21,9 +24,72 @@ using ::testing::_;
 
 namespace ui {
 
+namespace {
+
+class FakeGbmBuffer : public GbmBuffer {
+ public:
+  FakeGbmBuffer() = default;
+  ~FakeGbmBuffer() override = default;
+
+  uint32_t GetFormat() const override { return 0; }
+  uint64_t GetFormatModifier() const override { return 0; }
+  uint32_t GetFlags() const override { return 0; }
+  gfx::Size GetSize() const override { return gfx::Size(); }
+  gfx::BufferFormat GetBufferFormat() const override {
+    return gfx::BufferFormat::BGRA_8888;
+  }
+  bool AreFdsValid() const override { return false; }
+  size_t GetNumPlanes() const override { return 0; }
+  int GetPlaneFd(size_t plane) const override { return -1; }
+  uint32_t GetPlaneHandle(size_t plane) const override { return 0; }
+  int GetPlaneStride(size_t plane) const override { return -1; }
+  int GetPlaneOffset(size_t plane) const override { return -1; }
+  size_t GetPlaneSize(size_t plane) const override { return 0; }
+  uint32_t GetHandle() const override { return 0; }
+  gfx::NativePixmapHandle ExportHandle() const override {
+    return gfx::NativePixmapHandle();
+  }
+  sk_sp<SkSurface> GetSurface() override { return nullptr; }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(FakeGbmBuffer);
+};
+
+class FakeGbmDevice : public GbmDevice {
+ public:
+  FakeGbmDevice() = default;
+  ~FakeGbmDevice() override = default;
+
+  std::unique_ptr<GbmBuffer> CreateBuffer(uint32_t format,
+                                          const gfx::Size& size,
+                                          uint32_t flags) override {
+    return nullptr;
+  }
+
+  std::unique_ptr<GbmBuffer> CreateBufferWithModifiers(
+      uint32_t format,
+      const gfx::Size& size,
+      uint32_t flags,
+      const std::vector<uint64_t>& modifiers) override {
+    return nullptr;
+  }
+  std::unique_ptr<GbmBuffer> CreateBufferFromFds(
+      uint32_t format,
+      const gfx::Size& size,
+      std::vector<base::ScopedFD> fds,
+      const std::vector<gfx::NativePixmapPlane>& planes) override {
+    return nullptr;
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(FakeGbmDevice);
+};
+
+}  // namespace
+
 class WaylandSurfaceFactoryTest : public WaylandTest {
  public:
-  WaylandSurfaceFactoryTest() : surface_factory(connection_proxy_.get()) {}
+  WaylandSurfaceFactoryTest() : surface_factory_(connection_proxy_.get()) {}
 
   ~WaylandSurfaceFactoryTest() override {}
 
@@ -33,30 +99,35 @@ class WaylandSurfaceFactoryTest : public WaylandTest {
     auto connection_ptr = connection_->BindInterface();
     connection_proxy_->SetWaylandConnection(std::move(connection_ptr));
 
-    canvas = surface_factory.CreateCanvasForWidget(widget_);
-    ASSERT_TRUE(canvas);
-
     // Wait until initialization and mojo calls go through.
     base::RunLoop().RunUntilIdle();
   }
 
   void TearDown() override {
-    canvas.reset();
-
-    // The mojo call to destroy shared buffer goes after canvas is destroyed.
+    // The mojo call to destroy shared buffer goes after surfaces are destroyed.
     // Wait until it's done.
     base::RunLoop().RunUntilIdle();
   }
 
  protected:
-  WaylandSurfaceFactory surface_factory;
-  std::unique_ptr<SurfaceOzoneCanvas> canvas;
+  std::unique_ptr<SurfaceOzoneCanvas> CreateCanvas(
+      gfx::AcceleratedWidget widget) {
+    auto canvas = surface_factory_.CreateCanvasForWidget(widget_);
+    base::RunLoop().RunUntilIdle();
+
+    return canvas;
+  }
+
+  WaylandSurfaceFactory surface_factory_;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(WaylandSurfaceFactoryTest);
 };
 
 TEST_P(WaylandSurfaceFactoryTest, Canvas) {
+  auto canvas = CreateCanvas(widget_);
+  ASSERT_TRUE(canvas);
+
   canvas->ResizeCanvas(window_->GetBounds().size());
   canvas->GetSurface();
   canvas->PresentCanvas(gfx::Rect(5, 10, 20, 15));
@@ -83,6 +154,9 @@ TEST_P(WaylandSurfaceFactoryTest, Canvas) {
 }
 
 TEST_P(WaylandSurfaceFactoryTest, CanvasResize) {
+  auto canvas = CreateCanvas(widget_);
+  ASSERT_TRUE(canvas);
+
   canvas->ResizeCanvas(window_->GetBounds().size());
   canvas->GetSurface();
   canvas->ResizeCanvas(gfx::Size(100, 50));
@@ -104,6 +178,31 @@ TEST_P(WaylandSurfaceFactoryTest, CanvasResize) {
   ASSERT_TRUE(buffer);
   EXPECT_EQ(wl_shm_buffer_get_width(buffer), 100);
   EXPECT_EQ(wl_shm_buffer_get_height(buffer), 50);
+}
+
+TEST_P(WaylandSurfaceFactoryTest, CreateSurfaceCheckGbm) {
+  gl::SetGLImplementation(gl::kGLImplementationEGLGLES2);
+
+  // When gbm is not available, only canvas can be created with viz process
+  // used.
+  EXPECT_FALSE(connection_proxy_->gbm_device());
+
+  auto* gl_ozone = surface_factory_.GetGLOzone(gl::kGLImplementationEGLGLES2);
+  EXPECT_TRUE(gl_ozone);
+  auto gl_surface = gl_ozone->CreateSurfacelessViewGLSurface(widget_);
+  EXPECT_FALSE(gl_surface);
+
+  // Now, set gbm.
+  connection_proxy_->set_gbm_device(std::make_unique<FakeGbmDevice>());
+
+  gl_surface = gl_ozone->CreateSurfacelessViewGLSurface(widget_);
+  EXPECT_TRUE(gl_surface);
+
+  // Reset gbm now. WaylandConnectionProxy can reset it when zwp is not
+  // available. And factory must behave the same way as previously.
+  connection_proxy_->ResetGbmDevice();
+  gl_surface = gl_ozone->CreateSurfacelessViewGLSurface(widget_);
+  EXPECT_FALSE(gl_surface);
 }
 
 INSTANTIATE_TEST_SUITE_P(XdgVersionV5Test,
