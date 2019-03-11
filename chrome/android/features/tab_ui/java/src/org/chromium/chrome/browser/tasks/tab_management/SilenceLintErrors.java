e6ebe864fee15e73554188d6d563c1204cab1361
Yusuf Ozuysal <yusufo@google.com>
2019-03-11 20:02:45 +0000

    Move tab_list_ui/ code into its own DFM
    BUG=925126
    
    Change-Id: I7e0f676f4e9e332945d70bedcbcb6f84665a3c80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1475881
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639609}
    Former-commit-id: 02d5848947400d769890ab3bfdf8eb66cd731c4e

---
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/SilenceLintErrors.java
@@ -0,0 +1,31 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tasks.tab_management;
+
+import org.chromium.chrome.R;
+
+/**
+ * Hacky class to avoid lint errors for resources called on the module.
+ */
+/* package */ class SilenceLintErrors {
+    // TODO(yusufo): Add these resources to the DFM
+    private int[] mRes =
+            new int[] {R.dimen.tab_grid_favicon_size, R.string.tab_management_module_title,
+                    R.string.iph_tab_groups_quickly_compare_pages_text,
+                    R.string.iph_tab_groups_tap_to_see_another_tab_text,
+                    R.string.iph_tab_groups_your_tabs_together_text,
+                    R.string.bottom_tab_grid_description, R.string.bottom_tab_grid_opened_half,
+                    R.string.bottom_tab_grid_opened_full, R.string.bottom_tab_grid_closed,
+                    R.dimen.tab_list_selected_inset, R.layout.tab_strip_item,
+                    R.drawable.selected_tab_background, R.drawable.tab_grid_card_background,
+                    R.layout.tab_grid_card_item, R.layout.tab_list_recycler_view_layout,
+                    R.layout.bottom_tab_grid_toolbar, R.string.bottom_tab_grid_new_tab,
+                    R.string.bottom_tab_grid_new_tab, R.plurals.bottom_tab_grid_title_placeholder,
+                    R.string.iph_tab_groups_tap_to_see_another_tab_accessibility_text,
+                    R.string.accessibility_bottom_tab_strip_expand_tab_sheet,
+                    R.layout.bottom_tab_strip_toolbar};
+
+    private SilenceLintErrors() {}
+}
---
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUi.java
@@ -0,0 +1,15 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tasks.tab_management;
+
+import org.chromium.chrome.browser.ChromeActivity;
+
+/**
+ * Interface for the Tab Groups related UI.
+ */
+public interface TabGroupUi {
+    void initializeWithNative(ChromeActivity activity);
+    void destroy();
+}
