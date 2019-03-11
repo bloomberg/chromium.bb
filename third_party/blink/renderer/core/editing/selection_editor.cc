350af2165644b27413185a77d2628f4f4e3adcde
Kent Tamura <tkent@chromium.org>
2019-03-11 02:58:57 +0000

    selection: Updating textContent of a TEXTAREA element should not clear selection in its value.
    This fixes a regression by crrev.com/410976.
    ComputePositionForChildrenRemoval() should do nothing if the position is
    in a shadow tree of |container|.
    
    Bug: 751601
    Change-Id: I446d40f41712f1bded19ab9a29f1cf5b96c4f943
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1512984
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639382}
    Former-commit-id: c23ba76886bb26de27e671df21861c286eef21d4

--- third_party/blink/renderer/core/editing/selection_editor.cc
+++ third_party/blink/renderer/core/editing/selection_editor.cc
@@ -171,9 +171,17 @@ static Position ComputePositionForChildrenRemoval(const Position& position,
   if (!node)
     return position;
 #endif
-  if (container.ContainsIncludingHostElements(*node))
-    return Position::FirstPositionInNode(container);
-  return position;
+  if (!container.ContainsIncludingHostElements(*node))
+    return position;
+  if (auto* element = ToElementOrNull(container)) {
+    if (auto* shadow_root = element->GetShadowRoot()) {
+      // Removal of light children does not affect position in the
+      // shadow tree.
+      if (shadow_root->ContainsIncludingHostElements(*node))
+        return position;
+    }
+  }
+  return Position::FirstPositionInNode(container);
 }
 
 void SelectionEditor::NodeChildrenWillBeRemoved(ContainerNode& container) {
