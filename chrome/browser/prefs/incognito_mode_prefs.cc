e72a96d81382a67d16fc41559027756c03d935bd
Michael Thiessen <mthiesse@chromium.org>
2019-03-11 16:00:45 +0000

    Default-disable Incognito Mode for notouch builds.
    Touchless Android mode doesn't support incognito by default, so set the
    default availability to disabled.
    
    Bug: 939732
    Change-Id: Iae40962594559a4300e13a3165fbb5d15f6f2eb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1511829
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639518}
    Former-commit-id: 0f4e785c1021df3a40084599942923bd6ca5d0ab

--- chrome/browser/prefs/incognito_mode_prefs.cc
+++ chrome/browser/prefs/incognito_mode_prefs.cc
@@ -123,11 +123,20 @@ class PlatformParentalControlsValue {
 }  // namespace
 #endif  // OS_WIN
 
+namespace {
+static constexpr IncognitoModePrefs::Availability kDefaultAvailability =
+#if defined(INCOGNITO_DEFAULT_DISABLED)
+    IncognitoModePrefs::DISABLED;
+#else
+    IncognitoModePrefs::ENABLED;
+#endif
+}  // namespace
+
 // static
 bool IncognitoModePrefs::IntToAvailability(int in_value,
                                            Availability* out_value) {
   if (in_value < 0 || in_value >= AVAILABILITY_NUM_TYPES) {
-    *out_value = ENABLED;
+    *out_value = kDefaultAvailability;
     return false;
   }
   *out_value = static_cast<Availability>(in_value);
@@ -150,7 +159,7 @@ void IncognitoModePrefs::SetAvailability(PrefService* prefs,
 void IncognitoModePrefs::RegisterProfilePrefs(
     user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterIntegerPref(prefs::kIncognitoModeAvailability,
-                                IncognitoModePrefs::ENABLED);
+                                kDefaultAvailability);
 }
 
 // static
@@ -218,7 +227,7 @@ IncognitoModePrefs::Availability IncognitoModePrefs::GetAvailabilityInternal(
     GetAvailabilityMode mode) {
   DCHECK(pref_service);
   int pref_value = pref_service->GetInteger(prefs::kIncognitoModeAvailability);
-  Availability result = IncognitoModePrefs::ENABLED;
+  Availability result = kDefaultAvailability;
   bool valid = IntToAvailability(pref_value, &result);
   DCHECK(valid);
   if (result != IncognitoModePrefs::DISABLED &&
