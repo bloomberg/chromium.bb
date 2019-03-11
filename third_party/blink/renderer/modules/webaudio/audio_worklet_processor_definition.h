61d88e0af904c744062c9793dadb98a837e5e12a
Yuki Shiino <yukishiino@chromium.org>
2019-03-11 12:07:27 +0000

    v8binding: Make AudioWorkletGlobalScope use IDL callback function
    Replaces CallbackFunctionTreatedAsScriptValue (obsolete) in
    AudioWorkletGlobalScope with IDL callback function type.
    
    Change-Id: If23d6ae3714c594c53a8659805cef923a51dee3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1511754
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639453}
    Former-commit-id: 52dded60a7c8b0c66637c35a9450744d99ef1581

--- third_party/blink/renderer/modules/webaudio/audio_worklet_processor_definition.h
+++ third_party/blink/renderer/modules/webaudio/audio_worklet_processor_definition.h
@@ -8,12 +8,15 @@
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_param_descriptor.h"
 #include "third_party/blink/renderer/platform/bindings/name_client.h"
-#include "third_party/blink/renderer/platform/bindings/trace_wrapper_v8_reference.h"
+#include "third_party/blink/renderer/platform/bindings/trace_wrapper_member.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "v8/include/v8.h"
 
 namespace blink {
+
+class V8AudioWorkletProcessCallback;
+class V8BlinkAudioWorkletProcessorConstructor;
 
 // Represents a JavaScript class definition registered in the
 // AudioWorkletGlobalScope. After the registration, a definition class contains
@@ -27,20 +30,21 @@ class MODULES_EXPORT AudioWorkletProcessorDefinition final
       public NameClient {
  public:
   static AudioWorkletProcessorDefinition* Create(
-      v8::Isolate*,
       const String& name,
-      v8::Local<v8::Object> constructor,
-      v8::Local<v8::Function> process);
+      V8BlinkAudioWorkletProcessorConstructor* constructor,
+      V8AudioWorkletProcessCallback* process);
 
-  AudioWorkletProcessorDefinition(v8::Isolate*,
-                                  const String& name,
-                                  v8::Local<v8::Object> constructor,
-                                  v8::Local<v8::Function> process);
-  virtual ~AudioWorkletProcessorDefinition();
+  explicit AudioWorkletProcessorDefinition(
+      const String& name,
+      V8BlinkAudioWorkletProcessorConstructor* constructor,
+      V8AudioWorkletProcessCallback* process);
+  ~AudioWorkletProcessorDefinition();
 
   const String& GetName() const { return name_; }
-  v8::Local<v8::Object> ConstructorLocal(v8::Isolate*);
-  v8::Local<v8::Function> ProcessLocal(v8::Isolate*);
+  V8BlinkAudioWorkletProcessorConstructor* ConstructorFunction() const {
+    return constructor_;
+  }
+  V8AudioWorkletProcessCallback* ProcessFunction() const { return process_; }
   void SetAudioParamDescriptors(
       const HeapVector<Member<AudioParamDescriptor>>&);
   const Vector<String> GetAudioParamDescriptorNames() const;
@@ -51,11 +55,8 @@ class MODULES_EXPORT AudioWorkletProcessorDefinition final
   bool IsSynchronized() const { return is_synchronized_; }
   void MarkAsSynchronized() { is_synchronized_ = true; }
 
-  void Trace(blink::Visitor* visitor) {
-    visitor->Trace(constructor_);
-    visitor->Trace(process_);
-    visitor->Trace(audio_param_descriptors_);
-  }
+  void Trace(blink::Visitor* visitor);
+
   const char* NameInHeapSnapshot() const override {
     return "AudioWorkletProcessorDefinition";
   }
@@ -66,8 +67,8 @@ class MODULES_EXPORT AudioWorkletProcessorDefinition final
 
   // The definition is per global scope. The active instance of
   // |AudioProcessorWorklet| should be passed into these to perform JS function.
-  TraceWrapperV8Reference<v8::Object> constructor_;
-  TraceWrapperV8Reference<v8::Function> process_;
+  TraceWrapperMember<V8BlinkAudioWorkletProcessorConstructor> constructor_;
+  TraceWrapperMember<V8AudioWorkletProcessCallback> process_;
 
   HeapVector<Member<AudioParamDescriptor>> audio_param_descriptors_;
 };
