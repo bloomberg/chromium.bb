3046f849a48c293eaa6a674f823abe6acf5ab73f
Ken Rockot <rockot@chromium.org>
2019-03-11 19:59:41 +0000

    Convert Content Service to new Mojo+SM types
    Replaces usage of InterfacePtr etc with new Mojo endpoint types
    throughout the Content Service implementation, and uses some
    corresponding new Service Manager types/APIS as well.
    
    Clients are not yet updated, so some client APIs still support using old
    types and patterns.
    
    Bug: 875030
    Change-Id: Ic290c90404269e505cfbcb801a2c6c2bcbf4c7bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1507203
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639606}
    Former-commit-id: 53da09cf2393b8125cb4185bf0f02df6b5a16368

--- services/content/service.h
+++ services/content/service.h
@@ -8,7 +8,8 @@
 #include <map>
 
 #include "base/macros.h"
-#include "services/service_manager/public/cpp/binder_registry.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "services/service_manager/public/cpp/binder_map.h"
 #include "services/service_manager/public/cpp/service.h"
 #include "services/service_manager/public/cpp/service_binding.h"
 #include "services/service_manager/public/mojom/service.mojom.h"
@@ -52,13 +53,13 @@ class Service : public service_manager::Service {
   void RemoveNavigableContents(NavigableContentsImpl* contents);
 
   // service_manager::Service:
-  void OnBindInterface(const service_manager::BindSourceInfo& source,
-                       const std::string& interface_name,
-                       mojo::ScopedMessagePipeHandle pipe) override;
+  void OnConnect(const service_manager::ConnectSourceInfo& source,
+                 const std::string& interface_name,
+                 mojo::ScopedMessagePipeHandle pipe) override;
 
   ServiceDelegate* const delegate_;
   service_manager::ServiceBinding service_binding_;
-  service_manager::BinderRegistry binders_;
+  service_manager::BinderMap binders_;
 
   std::map<NavigableContentsFactoryImpl*,
            std::unique_ptr<NavigableContentsFactoryImpl>>
