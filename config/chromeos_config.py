070c12507b94e233d92dd1f3957c99f45e00abbe
Prathmesh Prabhu <pprabhu@chromium.org>
2019-03-12 15:06:28 -0700

    config: Migrate {peach_pit,tricky}-chrome-pfq to Skylab.
    They are marked experimental during the migration.
    
    BUG=chromium:938684
    TEST=unittests
    
    Change-Id: I75df1672a2c2f4cf7ba7999b575227b03b9fa640
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/1518989
    Trybot-Ready: Prathmesh Prabhu <pprabhu@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Don Garrett <dgarrett@chromium.org>
    Tested-by: Prathmesh Prabhu <pprabhu@chromium.org>

--- config/chromeos_config.py
+++ config/chromeos_config.py
@@ -2951,22 +2951,24 @@ def ChromePfqBuilders(site_config, boards_dict, ge_build_config):
       'kevin64',
       'nocturne',
       'nyan_big',
-      'peach_pit',
       'peppy',
       'reef',
       'scarlet',
       'terra',
-      'tricky',
       'veyron_minnie',
       'veyron_rialto',
   ])
 
   _chrome_pfq_experimental_boards = frozenset([
+      'peach_pit',
+      'tricky',
   ])
 
   _chrome_pfq_skylab_boards = frozenset([
+      'peach_pit',
       'peppy',
       'reef',
+      'tricky',
   ])
 
   _chrome_pfq_tryjob_boards = (
