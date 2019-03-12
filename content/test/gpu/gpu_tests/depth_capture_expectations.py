8c070d5bac65a5963294218f5415d3a7b1d1ceff
Aleksandar Stojiljkovic <aleksandar.stojiljkovic@intel.com>
2019-03-12 08:02:49 +0000

    Fix depthStreamToRGBAUint8Texture test logic.
    BUG=824438,819661
    
    Cq-Include-Trybots: luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:android_optional_gpu_tests_rel;luci.chromium.try:linux_optional_gpu_tests_rel;luci.chromium.try:mac_optional_gpu_tests_rel;luci.chromium.try:win_optional_gpu_tests_rel
    Change-Id: I25eab42d89c3b4ae76ccd012b3c52403d678b1b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/978251
    Commit-Queue: Aleksandar Stojiljkovic <aleksandar.stojiljkovic@intel.com>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639855}
    Former-commit-id: 2fd69c0ac9f0326af2c698511f66db61dfeb6864

--- content/test/gpu/gpu_tests/depth_capture_expectations.py
+++ content/test/gpu/gpu_tests/depth_capture_expectations.py
@@ -17,10 +17,6 @@ class DepthCaptureExpectations(GpuTestExpectations):
                ['linux', ('nvidia', 0x104a)], bug=737410)
     self.Flaky('DepthCapture_depthStreamToRGBAUint8Texture',
                ['linux', ('nvidia', 0x104a)], bug=737410)
-    self.Flaky('DepthCapture_depthStreamToRGBAUint8Texture',
-               ['highsierra', ('amd', 0x6821)], bug=819661)
-    self.Flaky('DepthCapture_depthStreamToRGBAUint8Texture',
-               ['highsierra', ('intel', 0x0a2e)], bug=824438)
     self.Fail('DepthCapture_depthStreamToR32FloatTexture',
               ['android', ('qualcomm', 'Adreno (TM) 330')], bug=765913)
     self.Flaky('DepthCapture_depthStreamToR32FloatTexture',
