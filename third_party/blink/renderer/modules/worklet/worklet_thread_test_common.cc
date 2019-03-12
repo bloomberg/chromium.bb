de414161e8c583dd670be49d07646dc11f0934c8
Xida Chen <xidachen@chromium.org>
2019-03-12 00:55:21 +0000

    [Code health] Move CreateAnimationAndPaintWorkletThread to a test_common.cc
    The worklet_thread_test_common.h has this function for AnimationWorklet
    already. This CL moves the PaintWorklet version into the same file.
    
    This CL should not change any behavior.
    
    Bug: None
    Change-Id: I0dc62af5cc6cc6c35e09cb67dc23eef520ba3d91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1515132
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639732}
    Former-commit-id: a4880aa8ecf3b746a03ff572987d218e126c644b

--- third_party/blink/renderer/modules/worklet/worklet_thread_test_common.cc
+++ third_party/blink/renderer/modules/worklet/worklet_thread_test_common.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <utility>
+
 #include "third_party/blink/renderer/modules/worklet/worklet_thread_test_common.h"
 
 #include "third_party/blink/public/mojom/script/script_type.mojom-blink.h"
@@ -12,19 +14,18 @@
 #include "third_party/blink/renderer/core/workers/global_scope_creation_params.h"
 #include "third_party/blink/renderer/core/workers/worker_reporting_proxy.h"
 #include "third_party/blink/renderer/modules/animationworklet/animation_worklet_proxy_client.h"
+#include "third_party/blink/renderer/modules/csspaint/paint_worklet_proxy_client.h"
 
 namespace blink {
 
 namespace {
 
 std::unique_ptr<AnimationAndPaintWorkletThread>
-CreateAnimationAndPaintWorkletThread(Document* document,
-                                     WorkerReportingProxy* reporting_proxy,
-                                     WorkerClients* clients) {
-  std::unique_ptr<AnimationAndPaintWorkletThread> thread =
-      AnimationAndPaintWorkletThread::CreateForAnimationWorklet(
-          *reporting_proxy);
-
+CreateAnimationAndPaintWorkletThread(
+    Document* document,
+    WorkerReportingProxy* reporting_proxy,
+    WorkerClients* clients,
+    std::unique_ptr<AnimationAndPaintWorkletThread> thread) {
   thread->Start(
       std::make_unique<GlobalScopeCreationParams>(
           document->Url(), mojom::ScriptType::kModule,
@@ -45,6 +46,22 @@ CreateAnimationAndPaintWorkletThread(Document* document,
 }  // namespace
 
 std::unique_ptr<AnimationAndPaintWorkletThread>
+CreateThreadAndProvidePaintWorkletProxyClient(
+    Document* document,
+    WorkerReportingProxy* reporting_proxy,
+    PaintWorkletProxyClient* proxy_client) {
+  if (!proxy_client)
+    proxy_client = MakeGarbageCollected<PaintWorkletProxyClient>();
+  WorkerClients* clients = WorkerClients::Create();
+  ProvidePaintWorkletProxyClientTo(clients, proxy_client);
+
+  std::unique_ptr<AnimationAndPaintWorkletThread> thread =
+      AnimationAndPaintWorkletThread::CreateForPaintWorklet(*reporting_proxy);
+  return CreateAnimationAndPaintWorkletThread(document, reporting_proxy,
+                                              clients, std::move(thread));
+}
+
+std::unique_ptr<AnimationAndPaintWorkletThread>
 CreateThreadAndProvideAnimationWorkletProxyClient(
     Document* document,
     WorkerReportingProxy* reporting_proxy,
@@ -60,8 +77,11 @@ CreateThreadAndProvideAnimationWorkletProxyClient(
   WorkerClients* clients = WorkerClients::Create();
   ProvideAnimationWorkletProxyClientTo(clients, proxy_client);
 
+  std::unique_ptr<AnimationAndPaintWorkletThread> thread =
+      AnimationAndPaintWorkletThread::CreateForAnimationWorklet(
+          *reporting_proxy);
   return CreateAnimationAndPaintWorkletThread(document, reporting_proxy,
-                                              clients);
+                                              clients, std::move(thread));
 }
 
 }  // namespace blink
