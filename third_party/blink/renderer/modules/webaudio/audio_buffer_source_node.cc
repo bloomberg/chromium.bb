94bb8c3d7f71771cb1627ed1687ad3bef7942c15
Raymond Toy <rtoy@chromium.org>
2019-03-12 01:18:33 +0000

    ABSN with null buffer should output silence
    If an AudioBufferSourceNode is set with a non-null AudioBuffer and is
    then set to null, the output from the buffer should be silence.
    
    Bug: 938713
    Test: audiobuffersource-null.html
    Change-Id: I4c5d3363f0176503b5ed8fb45f46e776b4e05b42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1507035
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#639744}
    Former-commit-id: 93b2cafe80ad9bb53e12731bc4983ed77de2a0ed

--- third_party/blink/renderer/modules/webaudio/audio_buffer_source_node.cc
+++ third_party/blink/renderer/modules/webaudio/audio_buffer_source_node.cc
@@ -411,7 +411,10 @@ void AudioBufferSourceHandler::SetBuffer(AudioBuffer* buffer,
   // This synchronizes with process().
   MutexLocker process_locker(process_lock_);
 
-  if (buffer) {
+  if (!buffer) {
+    // Clear out the shared buffer.
+    shared_buffer_.reset();
+  } else {
     buffer_has_been_set_ = true;
 
     // Do any necesssary re-configuration to the buffer's number of channels.
