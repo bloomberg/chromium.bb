ef01254543825f3f647c9f9b0763671b1b5adc02
Alex Betts <abetts3@bloomberg.net>
2019-03-12 17:33:40 -0400

    Renderer UI (6/22): Create a HWND for the `RenderWebView`
    The HWND will eventually host the compositor and accept user input events.
    
    Functions like `show()`, `hide()`, and `move()` are not implemented in terms
    of Win32 functions that manipulate the HWND.
    
    Currently, the HWND just delegates visibility and geometry changes to the
    proxy.

--- src/blpwtk2/private/blpwtk2_renderwebview.cc
+++ src/blpwtk2/private/blpwtk2_renderwebview.cc
@@ -42,10 +42,166 @@ RenderWebView::RenderWebView(WebViewDelegate          *delegate,
 #endif
     , d_pendingDestroy(false)
 {
+    initialize();
 }
 
 RenderWebView::~RenderWebView()
 {
+}
+
+LPCTSTR RenderWebView::GetWindowClass()
+{
+    static const LPCTSTR s_className    = L"blpwtk2-RenderWebView";
+    static ATOM          s_atom         = 0;
+
+    if (s_atom == 0) {
+        static WNDCLASSEX s_class = {};
+
+        s_class.cbSize        = sizeof(WNDCLASSEX);
+        s_class.style         = 0;
+        s_class.lpfnWndProc   = WindowProcedure;
+        s_class.cbClsExtra    = 0;
+        s_class.cbWndExtra    = 0;
+        s_class.hInstance     = GetModuleHandle(NULL);
+        s_class.hIcon         = NULL;
+        s_class.hCursor       = NULL;
+        s_class.hbrBackground = NULL;
+        s_class.lpszMenuName  = NULL;
+        s_class.lpszClassName = s_className;
+        s_class.hIconSm       = NULL;
+
+        s_atom = RegisterClassEx(&s_class);
+        DCHECK(s_atom);
+    }
+
+    return s_className;
+}
+
+LRESULT CALLBACK RenderWebView::WindowProcedure(HWND      hWnd,
+                                                UINT      uMsg,
+                                                WPARAM    wParam,
+                                                LPARAM    lParam)
+{
+    RenderWebView *that = reinterpret_cast<RenderWebView *>(
+        GetWindowLongPtr(hWnd, GWLP_USERDATA)
+    );
+
+    // GWL_USERDATA hasn't been set to anything yet:
+    if (!that) {
+        return DefWindowProc(hWnd, uMsg, wParam, lParam);
+    }
+    else {
+        // Otherwise:
+        DCHECK(that->d_hwnd == hWnd);
+
+        return that->windowProcedure(uMsg, wParam, lParam);
+    }
+}
+
+LRESULT RenderWebView::windowProcedure(UINT   uMsg,
+                                       WPARAM wParam,
+                                       LPARAM lParam)
+{
+    switch (uMsg) {
+    case WM_NCDESTROY: {
+        #pragma clang diagnostic push
+        #pragma clang diagnostic ignored "-Wunused"
+
+        HWND leaked_hwnd = d_hwnd.release();
+
+        #pragma clang diagnostic pop
+    } return 0;
+    case WM_WINDOWPOSCHANGED: {
+        WINDOWPOS *windowpos = reinterpret_cast<WINDOWPOS *>(lParam);
+
+        if (windowpos->flags & (SWP_SHOWWINDOW | SWP_HIDEWINDOW)) {
+            d_visible = (windowpos->flags & SWP_SHOWWINDOW)?
+                true : false;
+
+            updateVisibility();
+        }
+
+        gfx::Rect geometry(
+            windowpos->x, windowpos->y, windowpos->cx, windowpos->cy);
+
+        if ((geometry != d_geometry &&
+            (!(windowpos->flags & SWP_NOSIZE) ||
+             !(windowpos->flags & SWP_NOMOVE))) ||
+            windowpos->flags & SWP_FRAMECHANGED) {
+            d_geometry = geometry;
+
+            updateGeometry();
+        }
+    } return 0;
+    case WM_ERASEBKGND:
+        return 1;
+    default:
+        break;
+    }
+
+    return DefWindowProc(d_hwnd.get(), uMsg, wParam, lParam);
+}
+
+void RenderWebView::initialize()
+{
+    d_hwnd.reset(CreateWindowEx(
+#if defined(BLPWTK2_FEATURE_FOCUS)
+        d_properties.activateWindowOnMouseDown?
+            0 : WS_EX_NOACTIVATE,
+#else
+        0,
+#endif
+        GetWindowClass(),
+        L"blpwtk2-RenderWebView",
+        WS_OVERLAPPED | WS_CLIPCHILDREN | WS_CLIPSIBLINGS,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        1,
+        1,
+        NULL,
+        NULL,
+        NULL,
+        NULL));
+    DCHECK(d_hwnd.is_valid());
+
+    SetWindowLongPtr(d_hwnd.get(), GWLP_USERDATA, reinterpret_cast<LONG>(this));
+
+    SetWindowLong(
+        d_hwnd.get(),
+        GWL_STYLE,
+        GetWindowLong(d_hwnd.get(), GWL_STYLE) & ~WS_CAPTION);
+
+    RECT rect;
+    GetWindowRect(d_hwnd.get(), &rect);
+    d_geometry = gfx::Rect(rect);
+}
+
+void RenderWebView::updateVisibility()
+{
+    // TODO: forward this to the RenderWidget instead:
+    if (!d_proxy) {
+        return;
+    }
+
+    if (d_visible) {
+        d_proxy->show();
+    }
+    else {
+        d_proxy->hide();
+    }
+}
+
+void RenderWebView::updateGeometry()
+{
+    // TODO: forward this to the RenderWidget instead:
+    if (!d_proxy) {
+        return;
+    }
+
+    auto size = d_geometry.size();
+
+    d_proxy->move(
+        d_geometry.x(), d_geometry.y(), size.width(), size.height());
 }
 
 // blpwtk2::WebView overrides:
@@ -53,6 +209,10 @@ void RenderWebView::destroy()
 {
     DCHECK(Statics::isInApplicationMainThread());
     DCHECK(!d_pendingDestroy);
+
+    if (d_hwnd.is_valid()) {
+        d_hwnd.reset();
+    }
 
     if (d_proxy) {
         d_proxy->destroy();
@@ -139,25 +299,95 @@ void RenderWebView::setLogicalFocus(bool focused)
 void RenderWebView::show()
 {
     DCHECK(Statics::isInApplicationMainThread());
+    DCHECK(d_hwnd.is_valid());
 
-    // TODO: once the HWND is added, show it instead:
-    d_proxy->show();
+    if (d_shown) {
+        return;
+    }
+
+    d_shown = true;
+
+    if (d_hasParent) {
+        SetWindowPos(
+            d_hwnd.get(),
+            0,
+            0, 0, 0, 0,
+            SWP_SHOWWINDOW |
+            SWP_NOMOVE | SWP_NOSIZE |
+            SWP_NOACTIVATE | SWP_NOZORDER | SWP_NOOWNERZORDER);
+    }
 }
 
 void RenderWebView::hide()
 {
     DCHECK(Statics::isInApplicationMainThread());
+    DCHECK(d_hwnd.is_valid());
 
-    // TODO: once the HWND is added, hide it instead:
-    d_proxy->hide();
+    if (!d_shown) {
+        return;
+    }
+
+    d_shown = false;
+
+    if (d_hasParent) {
+        SetWindowPos(
+            d_hwnd.get(),
+            0,
+            0, 0, 0, 0,
+            SWP_HIDEWINDOW |
+            SWP_NOMOVE | SWP_NOSIZE |
+            SWP_NOACTIVATE | SWP_NOZORDER | SWP_NOOWNERZORDER);
+    }
 }
 
 void RenderWebView::setParent(NativeView parent)
 {
     DCHECK(Statics::isInApplicationMainThread());
+    DCHECK(d_hwnd.is_valid());
 
-    // TODO: once the HWND is added, set its parent instead:
-    d_proxy->setParent(parent);
+    auto shown = d_shown;
+
+    // The window is losing its parent:
+    if (!parent && d_hasParent) {
+        if (shown) {
+            SetWindowPos(
+                d_hwnd.get(),
+                0,
+                0, 0, 0, 0,
+                SWP_HIDEWINDOW |
+                SWP_NOMOVE | SWP_NOSIZE |
+                SWP_NOACTIVATE | SWP_NOZORDER | SWP_NOOWNERZORDER);
+        }
+    }
+    else if (parent && !d_hasParent) {
+        SetWindowLong(
+            d_hwnd.get(),
+            GWL_STYLE,
+            (GetWindowLong(d_hwnd.get(), GWL_STYLE) & ~WS_OVERLAPPED) | WS_CHILD);
+    }
+
+    SetParent(d_hwnd.get(), parent);
+
+    // The window is gaining a parent:
+    if (parent && !d_hasParent) {
+        if (shown) {
+            SetWindowPos(
+                d_hwnd.get(),
+                0,
+                0, 0, 0, 0,
+                SWP_SHOWWINDOW |
+                SWP_NOMOVE | SWP_NOSIZE |
+                SWP_NOACTIVATE | SWP_NOZORDER | SWP_NOOWNERZORDER);
+        }
+    }
+    else if (!parent && d_hasParent) {
+        SetWindowLong(
+            d_hwnd.get(),
+            GWL_STYLE,
+            (GetWindowLong(d_hwnd.get(), GWL_STYLE) & ~WS_CHILD) | WS_OVERLAPPED);
+    }
+
+    d_hasParent = !!parent;
 }
 
 void RenderWebView::move(int left, int top, int width, int height)
@@ -165,9 +395,13 @@ void RenderWebView::move(int left, int top, int width, int height)
     DCHECK(Statics::isInApplicationMainThread());
     DCHECK(0 <= width);
     DCHECK(0 <= height);
+    DCHECK(d_hwnd.is_valid());
 
-    // TODO: once the HWND is added, move it instead:
-    d_proxy->move(left, top, width, height);
+    SetWindowPos(
+        d_hwnd.get(),
+        0,
+        left, top, width, height,
+        SWP_NOACTIVATE | SWP_NOZORDER | SWP_NOOWNERZORDER);
 }
 
 void RenderWebView::cutSelection()
@@ -330,6 +564,9 @@ void RenderWebView::setSecurityToken(v8::Isolate *isolate,
 void RenderWebView::created(WebView *source)
 {
     d_proxy = source;
+
+    // TODO: eventually, do not do this:
+    d_proxy->setParent(d_hwnd.get());
 
     if (d_delegate) {
         d_delegate->created(this);
