32302261d5ec738a24a4ef7049ad33547a7c8cf9
John Budorick <jbudorick@chromium.org>
2019-03-13 19:42:06 +0000

    Revert "[OnionSoup] Build typemapping for blink::FormDataElement"
    This reverts commit ab818a89af72fb16dff9c72fa8306634b6b7695d [formerly be8dba9815ce95a1a5face0a982ea8a0be7d12fe].
    
    Reason for revert: Causing consistent MSAN failures in blink_platform_unittests, e.g. https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8919080506231539600/+/steps/blink_platform_unittests/0/logs/EncodedFormDataMojomTraitsTest.Roundtrips_FormDataElement__status_CRASH_/0
    
    Original change's description:
    > [OnionSoup] Build typemapping for blink::FormDataElement
    > 
    > This CL typemaps blink::FormDataElement with network.mojom.DataElement.
    > 
    > Bug: 860403
    > Change-Id: I7611e2cedf479f1da429fe8a026a290be447de5e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1444794
    > Commit-Queue: Richard Li <richard.li@intel.com>
    > Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#640163}
    
    TBR=falken@chromium.org,kinuko@chromium.org,leon.han@intel.com,richard.li@intel.com
    
    Change-Id: I260710c3d7f96f248bb9e0831791a6f7fc3d18a9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 860403
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1521284
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640449}
    Former-commit-id: 19868518d48f27db0f582343b0b02f8a24ff57c7

--- third_party/blink/renderer/platform/network/encoded_form_data_test.cc
+++ third_party/blink/renderer/platform/network/encoded_form_data_test.cc
@@ -2,17 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "third_party/blink/renderer/platform/network/encoded_form_data_mojom_traits.h"
-
-#include "base/sequenced_task_runner.h"
-#include "base/test/scoped_task_environment.h"
-#include "mojo/public/cpp/base/file_mojom_traits.h"
-#include "mojo/public/cpp/base/file_path_mojom_traits.h"
-#include "mojo/public/cpp/base/time_mojom_traits.h"
-#include "mojo/public/cpp/bindings/string_traits_wtf.h"
-#include "mojo/public/cpp/test_support/test_utils.h"
-#include "services/network/public/mojom/url_loader.mojom-blink.h"
-#include "third_party/blink/public/mojom/blob/blob.mojom-blink.h"
 #include "third_party/blink/renderer/platform/network/encoded_form_data.h"
 
 #include "testing/gtest/include/gtest/gtest.h"
@@ -105,62 +94,6 @@ TEST_F(EncodedFormDataTest, DeepCopy) {
   }
 }
 
-TEST(EncodedFormDataMojomTraitsTest, Roundtrips_FormDataElement) {
-  base::test::ScopedTaskEnvironment scoped_task_environment_;
-
-  FormDataElement original1;
-  original1.type_ = blink::FormDataElement::kData;
-  original1.data_ = {'a', 'b', 'c', 'd'};
-  FormDataElement copied1;
-  EXPECT_TRUE(
-      mojo::test::SerializeAndDeserialize<network::mojom::blink::DataElement>(
-          &original1, &copied1));
-  EXPECT_EQ(original1.type_, copied1.type_);
-  EXPECT_EQ(original1.data_, copied1.data_);
-
-  FormDataElement original2;
-  original2.type_ = blink::FormDataElement::kEncodedFile;
-  original2.file_start_ = 0;
-  original2.file_length_ = 4;
-  original2.filename_ = "file.name";
-  original2.expected_file_modification_time_ = base::Time::Now().ToDoubleT();
-  FormDataElement copied2;
-  EXPECT_TRUE(
-      mojo::test::SerializeAndDeserialize<network::mojom::blink::DataElement>(
-          &original2, &copied2));
-  EXPECT_EQ(original2.type_, copied2.type_);
-  EXPECT_EQ(original2.file_start_, copied2.file_start_);
-  EXPECT_EQ(original2.file_length_, copied2.file_length_);
-  EXPECT_EQ(original2.filename_, copied2.filename_);
-  EXPECT_EQ(original2.expected_file_modification_time_,
-            copied2.expected_file_modification_time_);
-
-  FormDataElement original3;
-  original3.type_ = blink::FormDataElement::kEncodedBlob;
-  original3.blob_uuid_ = "uuid-test";
-  mojo::MessagePipe pipe;
-  original3.optional_blob_data_handle_ = BlobDataHandle::Create(
-      original3.blob_uuid_, "type-test", 100,
-      mojom::blink::BlobPtrInfo(std::move(pipe.handle0), 0));
-  FormDataElement copied3;
-  EXPECT_TRUE(
-      mojo::test::SerializeAndDeserialize<network::mojom::blink::DataElement>(
-          &original3, &copied3));
-  EXPECT_EQ(copied3.type_, blink::FormDataElement::kDataPipe);
-
-  FormDataElement original4;
-  original4.type_ = blink::FormDataElement::kDataPipe;
-  network::mojom::blink::DataPipeGetterPtr data_pipe_getter;
-  auto request = mojo::MakeRequest(&data_pipe_getter);
-  original4.data_pipe_getter_ =
-      base::MakeRefCounted<blink::WrappedDataPipeGetter>(
-          std::move(data_pipe_getter));
-  FormDataElement copied4;
-  EXPECT_TRUE(
-      mojo::test::SerializeAndDeserialize<network::mojom::blink::DataElement>(
-          &original4, &copied4));
-  EXPECT_TRUE(copied4.data_pipe_getter_);
-}
-
 }  // namespace
+
 }  // namespace blink
