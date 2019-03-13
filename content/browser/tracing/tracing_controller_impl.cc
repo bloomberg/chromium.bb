20b3294fd9c928c7b17b8d7b423d99b992a7d405
Oystein Eftevaag <oysteine@chromium.org>
2019-03-13 17:27:10 +0000

    Fix enumeration of tracing categories for ARC tracing agents
    ARC tracing agents aren't owned by TracingControllerImpl;
    add a function in the base TracedProcess registry that all agents
    register with, to populate the tracing categories.
    
    R=ssid@chromium.org
    BUG=941295
    
    Change-Id: Ia2a2dd763be8961fe3f322499a1e3d8429bc249c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1519328
    Reviewed-by: ssid <ssid@chromium.org>
    Commit-Queue: oysteine <oysteine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640386}
    Former-commit-id: ad4923fbcaf6a728f4c2908212b73eef929e80f2

--- content/browser/tracing/tracing_controller_impl.cc
+++ content/browser/tracing/tracing_controller_impl.cc
@@ -340,10 +340,7 @@ TracingControllerImpl* TracingControllerImpl::GetInstance() {
 
 bool TracingControllerImpl::GetCategories(GetCategoriesDoneCallback callback) {
   std::set<std::string> category_set;
-
-  tracing::TraceEventAgent::GetInstance()->GetCategories(&category_set);
-  for (auto& agent : agents_)
-    agent->GetCategories(&category_set);
+  tracing::TracedProcessImpl::GetInstance()->GetCategories(&category_set);
 
   std::move(callback).Run(category_set);
   return true;
