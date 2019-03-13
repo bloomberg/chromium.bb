b881370cee99d76546d10410c0dfdb444fb3d655
danakj <danakj@chromium.org>
2019-03-13 22:14:21 +0000

    Fix comment typo in WebWidgetClient.
    TBR=dcheng
    
    Change-Id: I5c89d348b70c3558f8c1fe99e7db1a77c33f20ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1520302
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640516}
    Former-commit-id: 2d776b741865a55bc5651c8fbfefb1bb67232fe0

--- third_party/blink/public/web/web_widget_client.h
+++ third_party/blink/public/web/web_widget_client.h
@@ -5,7 +5,7 @@
  * modification, are permitted provided that the following conditions are
  * met:
  *
- r *     * Redistributions of source code must retain the above copyright
+ *     * Redistributions of source code must retain the above copyright
  * notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above
  * copyright notice, this list of conditions and the following disclaimer
