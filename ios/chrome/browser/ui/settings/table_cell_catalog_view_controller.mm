1dde67cc05e99ef54ba2cd7bd60d869e9784afa3
sczs <sczs@chromium.org>
2019-03-14 15:46:54 +0000

    [ios] Moves SettingsDetailItem to ios/c/b/ui/table_view/cells
    Moving this cell to table_view since its quite complex and
    didn't want to create a new one to use in the Infobars.
    Also, I can easily see this cell being used a lot in other places.
    
    I plan to rename this Item/Cell in a followup CL.
    
    Bug: 911864
    Change-Id: I3b516c8ee4e3b7d12df13d069561805ac5d3efaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1522467
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640767}
    Former-commit-id: 8d9708724008a29b9749068a574d73cdc3f08753

--- ios/chrome/browser/ui/settings/table_cell_catalog_view_controller.mm
+++ ios/chrome/browser/ui/settings/table_cell_catalog_view_controller.mm
@@ -13,10 +13,10 @@
 #import "ios/chrome/browser/ui/settings/cells/account_sign_in_item.h"
 #import "ios/chrome/browser/ui/settings/cells/autofill_data_item.h"
 #import "ios/chrome/browser/ui/settings/cells/copied_to_chrome_item.h"
-#import "ios/chrome/browser/ui/settings/cells/settings_detail_item.h"
 #import "ios/chrome/browser/ui/settings/cells/settings_image_detail_text_item.h"
 #import "ios/chrome/browser/ui/settings/cells/settings_switch_item.h"
 #import "ios/chrome/browser/ui/settings/cells/sync_switch_item.h"
+#import "ios/chrome/browser/ui/table_view/cells/settings_detail_item.h"
 #import "ios/chrome/browser/ui/table_view/cells/table_view_detail_text_item.h"
 #import "ios/chrome/browser/ui/table_view/cells/table_view_image_item.h"
 #import "ios/chrome/browser/ui/table_view/cells/table_view_link_header_footer_item.h"
@@ -174,26 +174,26 @@ typedef NS_ENUM(NSInteger, ItemType) {
   [model addItem:noDetailTextItem
       toSectionWithIdentifier:SectionIdentifierText];
 
-  // SectionIdentifierSettings.
-  TableViewTextHeaderFooterItem* settingsHeader =
-      [[TableViewTextHeaderFooterItem alloc] initWithType:ItemTypeTextHeader];
-  settingsHeader.text = @"Settings";
-  [model setHeader:settingsHeader
-      forSectionWithIdentifier:SectionIdentifierSettings];
-
   SettingsDetailItem* settingsDetailItem =
       [[SettingsDetailItem alloc] initWithType:ItemTypeTextSettingsDetail];
   settingsDetailItem.text = @"Settings cells";
   settingsDetailItem.detailText = @"Short";
   [model addItem:settingsDetailItem
-      toSectionWithIdentifier:SectionIdentifierSettings];
+      toSectionWithIdentifier:SectionIdentifierText];
 
   SettingsDetailItem* settingsDetailItemLong =
       [[SettingsDetailItem alloc] initWithType:ItemTypeTextSettingsDetail];
   settingsDetailItemLong.text = @"Very long text eating the other detail label";
   settingsDetailItemLong.detailText = @"A bit less short";
   [model addItem:settingsDetailItemLong
-      toSectionWithIdentifier:SectionIdentifierSettings];
+      toSectionWithIdentifier:SectionIdentifierText];
+
+  // SectionIdentifierSettings.
+  TableViewTextHeaderFooterItem* settingsHeader =
+      [[TableViewTextHeaderFooterItem alloc] initWithType:ItemTypeTextHeader];
+  settingsHeader.text = @"Settings";
+  [model setHeader:settingsHeader
+      forSectionWithIdentifier:SectionIdentifierSettings];
 
   AccountSignInItem* accountSignInItem =
       [[AccountSignInItem alloc] initWithType:ItemTypeAccountSignInItem];
--- ios/chrome/browser/ui/table_view/cells/settings_detail_item.mm
+++ ios/chrome/browser/ui/table_view/cells/settings_detail_item.mm
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/settings/cells/settings_detail_item.h"
+#import "ios/chrome/browser/ui/table_view/cells/settings_detail_item.h"
 
 #include <algorithm>
 
