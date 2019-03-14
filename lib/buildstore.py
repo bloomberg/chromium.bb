6e2bc8e528681965a7150f8ec6a39c87f0258dca
Dhanya Ganesh <dhanyaganesh@chromium.org>
2019-03-14 10:35:33 -0600

    GetBuildStatuses: sanitize inputs to fix PreCQLauncher
    BUG=None
    TEST=run_tests
    
    Change-Id: Ia943bed0b7371af56e7d62f9f80db7a2fa3028bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/1524026
    Commit-Queue: Dhanya Ganesh <dhanyaganesh@chromium.org>
    Tested-by: Dhanya Ganesh <dhanyaganesh@chromium.org>
    Trybot-Ready: Dhanya Ganesh <dhanyaganesh@chromium.org>
    Reviewed-by: Evan Hernandez <evanhernandez@chromium.org>

--- lib/buildstore.py
+++ lib/buildstore.py
@@ -525,11 +525,15 @@ class BuildStore(object):
                                   'buildbucket_ids and build_ids.')
       if buildbucket_ids:
         buildbucket_ids = [x for x in buildbucket_ids if x is not None]
+      if buildbucket_ids:
         return self.cidb_conn.GetBuildStatusesWithBuildbucketIds(
             buildbucket_ids)
       elif build_ids:
         build_ids = [x for x in build_ids if x is not None]
-        return self.cidb_conn.GetBuildStatuses(build_ids)
+        if build_ids:
+          return self.cidb_conn.GetBuildStatuses(build_ids)
+        else:
+          return []
       else:
         return []
 
