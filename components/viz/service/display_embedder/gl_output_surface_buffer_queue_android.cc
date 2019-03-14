b221c437251159c3c0d3f4020ed464db2388427b
Khushal <khushalsagar@chromium.org>
2019-03-14 21:26:48 +0000

    [M74] Surface Control: Set display and crop rects correctly for overlays.
    The framework does not handle display bounds at a negative offset and
    simply snaps them to [0,0]. This was resulting in the complete buffer
    getting scaled down to the visible rect size for the overlay. Avoid that
    by computing the correct source crop based on the visible subset of the
    overlay and adjusting the overlay's uv_rect and display_rect accordingly.
    
    R=enne@chromium.org
    TBR=khushalsagar@chromium.org
    
    (cherry picked from commit d60184b73d0af6d9f02a5f3b46a2f7bed73c44b9 [formerly 671f91154dc8d939bdd25c9b96d0f45a9d6f2e6f])
    
    Bug: 941278
    Change-Id: If911fc4a6fd087aca2fa285e1499380e7c27331e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1517159
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Auto-Submit: Khushal <khushalsagar@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#640489}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1525009
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3729@{#133}
    Cr-Branched-From: 19260737901ea731c9baac505269e4ad9638530d [formerly d4a8972e30b604f090aeda5dfff68386ae656267]-refs/heads/master@{#638880}
    Former-commit-id: c2678e78b84f3473e0301a8b6a95380304b730d5

--- components/viz/service/display_embedder/gl_output_surface_buffer_queue_android.cc
+++ components/viz/service/display_embedder/gl_output_surface_buffer_queue_android.cc
@@ -4,50 +4,10 @@
 
 #include "components/viz/service/display_embedder/gl_output_surface_buffer_queue_android.h"
 
-#include "components/viz/service/display/overlay_strategy_single_on_top.h"
-#include "components/viz/service/display/overlay_strategy_underlay.h"
-#include "components/viz/service/display_embedder/compositor_overlay_candidate_validator_android.h"
 #include "third_party/khronos/GLES2/gl2.h"
-#include "ui/gfx/geometry/rect_conversions.h"
-#include "ui/gl/android/android_surface_control_compat.h"
 
 namespace viz {
 namespace {
-class OverlayCandidateValidatorImpl : public OverlayCandidateValidator {
- public:
-  OverlayCandidateValidatorImpl() = default;
-  ~OverlayCandidateValidatorImpl() override = default;
-
-  void GetStrategies(OverlayProcessor::StrategyList* strategies) override {
-    strategies->push_back(std::make_unique<OverlayStrategyUnderlay>(
-        this, OverlayStrategyUnderlay::OpaqueMode::AllowTransparentCandidates));
-  }
-  bool AllowCALayerOverlays() override { return false; }
-  bool AllowDCLayerOverlays() override { return false; }
-  void CheckOverlaySupport(OverlayCandidateList* surfaces) override {
-    DCHECK(!surfaces->empty());
-
-    // Only update the last candidate that was added to the list. All previous
-    // overlays should have already been handled.
-    auto& candidate = surfaces->back();
-    if (!gl::SurfaceControl::SupportsColorSpace(candidate.color_space)) {
-      DCHECK(!candidate.use_output_surface_for_resource)
-          << "The main overlay must only use color space supported by the "
-             "device";
-      candidate.overlay_handled = false;
-      return;
-    }
-
-    candidate.display_rect =
-        gfx::RectF(gfx::ToEnclosingRect(candidate.display_rect));
-    candidate.overlay_handled = true;
-
-#if DCHECK_IS_ON()
-    for (auto& candidate : *surfaces)
-      DCHECK(candidate.overlay_handled);
-#endif
-  }
-};
 
 uint32_t TextureFormat(gfx::BufferFormat format) {
   return format == gfx::BufferFormat::BGR_565 ? GL_RGB : GL_RGBA;
@@ -67,16 +27,16 @@ GLOutputSurfaceBufferQueueAndroid::GLOutputSurfaceBufferQueueAndroid(
                                  gpu_memory_buffer_manager,
                                  GL_TEXTURE_2D,
                                  TextureFormat(buffer_format),
-                                 buffer_format),
-      overlay_candidate_validator_(
-          std::make_unique<OverlayCandidateValidatorImpl>()) {}
+                                 buffer_format) {}
 
 GLOutputSurfaceBufferQueueAndroid::~GLOutputSurfaceBufferQueueAndroid() =
     default;
 
 OverlayCandidateValidator*
 GLOutputSurfaceBufferQueueAndroid::GetOverlayCandidateValidator() const {
-  return overlay_candidate_validator_.get();
+  // TODO(khushalsagar): The API shouldn't be const.
+  return &const_cast<GLOutputSurfaceBufferQueueAndroid*>(this)
+              ->overlay_candidate_validator_;
 }
 
 }  // namespace viz
