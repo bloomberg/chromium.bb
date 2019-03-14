0af30b5efb4019c748b5a0c26900b6b654d7025b
Alice Boxhall <aboxhall@chromium.org>
2019-03-14 08:29:29 +0000

    Disable DumpAccessibilityEventsTest.AccessibilityEventsAddAlert
    Bug: 941919
    Change-Id: I8ac69ecee4dd6550fdccc57f9ea010c4611df448
    TBR=dmazzoni@chromium.org
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I8ac69ecee4dd6550fdccc57f9ea010c4611df448
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1522518
    Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    Auto-Submit: Alice Boxhall <aboxhall@chromium.org>
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#640620}
    Former-commit-id: 62a63158bbda28342f9ac7e597c2ddbff6a10056

--- content/browser/accessibility/dump_accessibility_events_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_events_browsertest.cc
@@ -281,8 +281,9 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest,
   RunEventTest(FILE_PATH_LITERAL("aria-spinbutton-valuetext-change.html"));
 }
 
+// https://crbug.com/941919
 IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest,
-                       AccessibilityEventsAddAlert) {
+                       DISABLED_AccessibilityEventsAddAlert) {
   RunEventTest(FILE_PATH_LITERAL("add-alert.html"));
 }
 
