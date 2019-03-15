405e2e402e6cbaab363b25bde3e1691817cf70e8
Anders Hartvoll Ruud <andruud@chromium.org>
2019-03-15 09:35:28 +0000

    [ribbon] Merge css_property_base_custom.cc into css_property.cc.
    Now that CSSProperty is not generated, we don't need this file.
    
    Note: The functions have been moved without any modification.
    Change-Id: I1ab12d34b9df452ecea74a7f977b0719e8bdc892
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1506514
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641136}
    Former-commit-id: e722f040f6f467b42aa897d4d26c0a28602f0789

--- third_party/blink/renderer/core/css/properties/css_property.cc
+++ third_party/blink/renderer/core/css/properties/css_property.cc
@@ -4,6 +4,7 @@
 
 #include "third_party/blink/renderer/core/css/properties/css_property.h"
 
+#include "base/stl_util.h"
 #include "third_party/blink/renderer/core/css/cssom/cross_thread_keyword_value.h"
 #include "third_party/blink/renderer/core/css/cssom/cross_thread_style_value.h"
 #include "third_party/blink/renderer/core/css/cssom/cross_thread_unit_value.h"
@@ -11,6 +12,9 @@
 #include "third_party/blink/renderer/core/css/cssom/css_keyword_value.h"
 #include "third_party/blink/renderer/core/css/cssom/css_unit_value.h"
 #include "third_party/blink/renderer/core/css/cssom/style_value_factory.h"
+#include "third_party/blink/renderer/core/style/computed_style.h"
+#include "third_party/blink/renderer/core/style/svg_computed_style.h"
+#include "third_party/blink/renderer/core/style_property_shorthand.h"
 
 namespace blink {
 
@@ -50,6 +54,93 @@ CSSProperty::CrossThreadStyleValueFromComputedStyle(
       // Make an isolated copy to ensure that it is safe to pass cross thread.
       return std::make_unique<CrossThreadUnsupportedValue>(
           css_value->CssText().IsolatedCopy());
+  }
+}
+
+const StylePropertyShorthand& CSSProperty::BorderDirections() {
+  static const CSSProperty* kProperties[4] = {
+      &GetCSSPropertyBorderTop(), &GetCSSPropertyBorderRight(),
+      &GetCSSPropertyBorderBottom(), &GetCSSPropertyBorderLeft()};
+  DEFINE_STATIC_LOCAL(
+      StylePropertyShorthand, border_directions,
+      (CSSPropertyBorder, kProperties, base::size(kProperties)));
+  return border_directions;
+}
+
+const CSSProperty& CSSProperty::ResolveAfterToPhysicalProperty(
+    TextDirection direction,
+    WritingMode writing_mode,
+    const StylePropertyShorthand& shorthand) {
+  const CSSProperty** shorthand_properties = shorthand.properties();
+  if (IsHorizontalWritingMode(writing_mode))
+    return *shorthand_properties[kBottomSide];
+  if (IsFlippedLinesWritingMode(writing_mode))
+    return *shorthand_properties[kRightSide];
+  return *shorthand_properties[kLeftSide];
+}
+
+const CSSProperty& CSSProperty::ResolveBeforeToPhysicalProperty(
+    TextDirection direction,
+    WritingMode writing_mode,
+    const StylePropertyShorthand& shorthand) {
+  const CSSProperty** shorthand_properties = shorthand.properties();
+  if (IsHorizontalWritingMode(writing_mode))
+    return *shorthand_properties[kTopSide];
+  if (IsFlippedLinesWritingMode(writing_mode))
+    return *shorthand_properties[kLeftSide];
+  return *shorthand_properties[kRightSide];
+}
+
+const CSSProperty& CSSProperty::ResolveEndToPhysicalProperty(
+    TextDirection direction,
+    WritingMode writing_mode,
+    const StylePropertyShorthand& shorthand) {
+  const CSSProperty** shorthand_properties = shorthand.properties();
+  if (direction == TextDirection::kLtr) {
+    if (IsHorizontalWritingMode(writing_mode))
+      return *shorthand_properties[kRightSide];
+    return *shorthand_properties[kBottomSide];
+  }
+  if (IsHorizontalWritingMode(writing_mode))
+    return *shorthand_properties[kLeftSide];
+  return *shorthand_properties[kTopSide];
+}
+
+const CSSProperty& CSSProperty::ResolveStartToPhysicalProperty(
+    TextDirection direction,
+    WritingMode writing_mode,
+    const StylePropertyShorthand& shorthand) {
+  const CSSProperty** shorthand_properties = shorthand.properties();
+  if (direction == TextDirection::kLtr) {
+    if (IsHorizontalWritingMode(writing_mode))
+      return *shorthand_properties[kLeftSide];
+    return *shorthand_properties[kTopSide];
+  }
+  if (IsHorizontalWritingMode(writing_mode))
+    return *shorthand_properties[kRightSide];
+  return *shorthand_properties[kBottomSide];
+}
+
+const CSSValue* CSSProperty::CSSValueFromComputedStyle(
+    const ComputedStyle& style,
+    const LayoutObject* layout_object,
+    Node* styled_node,
+    bool allow_visited_style) const {
+  const SVGComputedStyle& svg_style = style.SvgStyle();
+  const CSSProperty& resolved_property =
+      ResolveDirectionAwareProperty(style.Direction(), style.GetWritingMode());
+  return resolved_property.CSSValueFromComputedStyleInternal(
+      style, svg_style, layout_object, styled_node, allow_visited_style);
+}
+
+void CSSProperty::FilterEnabledCSSPropertiesIntoVector(
+    const CSSPropertyID* properties,
+    size_t propertyCount,
+    Vector<const CSSProperty*>& outVector) {
+  for (unsigned i = 0; i < propertyCount; i++) {
+    const CSSProperty& property = Get(properties[i]);
+    if (property.IsEnabled())
+      outVector.push_back(&property);
   }
 }
 
