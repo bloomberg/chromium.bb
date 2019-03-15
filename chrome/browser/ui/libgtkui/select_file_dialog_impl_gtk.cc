df5225aad80b02a2dea96d60a097c9f68303ba58
Robert Liao <robliao@chromium.org>
2019-03-15 01:06:54 +0000

    Convert DesktopWindowTreeHostX11::DisableEventListening to OnceClosure and Fix Memory Leak
    OnFilePickerDestroy() needs to also destroy the pointer it receives.
    
    BUG=714018,940130
    
    Change-Id: Ib0402575a7115b0e8b125e1131b61238561e95f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1524879
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641020}
    Former-commit-id: bb5c7b0beb6d6aca295cdc2ff32e91be1de8801f

--- chrome/browser/ui/libgtkui/select_file_dialog_impl_gtk.cc
+++ chrome/browser/ui/libgtkui/select_file_dialog_impl_gtk.cc
@@ -17,6 +17,7 @@
 #include <vector>
 
 #include "base/logging.h"
+#include "base/memory/ptr_util.h"
 #include "base/strings/string_util.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
@@ -64,8 +65,9 @@ void OnFileFilterDataDestroyed(std::string* file_extension) {
 
 // Runs DesktopWindowTreeHostX11::EnableEventListening() when the file-picker
 // is closed.
-void OnFilePickerDestroy(base::Closure* callback) {
-  callback->Run();
+void OnFilePickerDestroy(base::OnceClosure* callback_raw) {
+  std::unique_ptr<base::OnceClosure> callback = base::WrapUnique(callback_raw);
+  std::move(*callback).Run();
 }
 
 }  // namespace
@@ -190,10 +192,11 @@ void SelectFileDialogImplGTK::SelectFileImpl(
       // been captured and by turning off event listening, it is never
       // released. So we manually ensure there is no current capture.
       host->ReleaseCapture();
-      std::unique_ptr<base::Closure> callback =
-          views::DesktopWindowTreeHostX11::GetHostForXID(
-              host->GetAcceleratedWidget())
-              ->DisableEventListening();
+      std::unique_ptr<base::OnceClosure> callback =
+          std::make_unique<base::OnceClosure>(
+              views::DesktopWindowTreeHostX11::GetHostForXID(
+                  host->GetAcceleratedWidget())
+                  ->DisableEventListening());
       // OnFilePickerDestroy() is called when |dialog| destroyed, which allows
       // to invoke the callback function to re-enable event handling on the
       // owning window.
