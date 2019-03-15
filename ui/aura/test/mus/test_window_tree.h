98a8baad5ef8050baf3170554a34922a811c0309
Aleks Totic <atotic@chromium.org>
2019-03-15 22:49:31 +0000

    Revert "SPM: Fix EmbedRoot focus"
    This reverts commit 79a5351e577a82f9ac296db67ea3dfbc81b907d7 [formerly 45bcac82eb11e7701cfcfc70a0ccd673e2aeb516].
    
    Reason for revert: MSAN uninitialized memory use
    https://crbug.com/942741
    
    Original change's description:
    > SPM: Fix EmbedRoot focus
    > 
    > - Notify FocusSynchronizer when a window in EmbedRoot is focused
    >   so that WS side would focused its proxy window;
    > - Observe FocusSynchronizer to reset the focused window in EmbedRoot
    >   when the FocusSynchronizer changes to different focus client so
    >   that the observers of the focused window gets the expected "blur"
    >   call;
    > 
    > Bug: b/127864880
    > Change-Id: I00f76dd36040b3fdff05a8130093596aa5be6ffa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1525057
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#641213}
    
    TBR=xiyuan@chromium.org,sky@chromium.org
    
    Change-Id: I4994ae4526f67aa98f017dc72a42bf96f01003ff
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: b/127864880
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1526640
    Reviewed-by: Aleks Totic <atotic@chromium.org>
    Commit-Queue: Aleks Totic <atotic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641336}
    Former-commit-id: 82fb8ada44d5767278bcb0f99c1177cfff54113b

--- ui/aura/test/mus/test_window_tree.h
+++ ui/aura/test/mus/test_window_tree.h
@@ -141,7 +141,6 @@ class TestWindowTree : public ws::mojom::WindowTree {
   ws::Id last_cancelled_window_id() const { return last_cancelled_window_id_; }
   ws::Id last_transfer_current() const { return last_transfer_current_; }
   ws::Id last_transfer_new() const { return last_transfer_new_; }
-  ws::Id last_focused_window_id() const { return last_focused_window_id_; }
   bool last_transfer_should_cancel() const {
     return last_transfer_should_cancel_;
   }
@@ -353,7 +352,6 @@ class TestWindowTree : public ws::mojom::WindowTree {
   ws::Id last_cancelled_window_id_ = 0u;
   ws::Id last_transfer_current_ = 0u;
   ws::Id last_transfer_new_ = 0u;
-  ws::Id last_focused_window_id_ = 0u;
   bool last_transfer_should_cancel_ = false;
   bool last_accepts_drops_ = false;
   size_t can_focus_count_ = 0u;
