c995babf56f283a8fb1d667e1eb455f02dad12a9
Eugene But <eugenebut@chromium.org>
2019-03-15 22:02:23 +0000

    Create ios/web/common directory.
    This directory will contain common code shared between //ios/web and
    the rest of the layers (//components, //ios/chrome, //ios/web_view).
    This directory will not contain the public API to create and manage
    web contents (//ios/web//public is public API directory).
    
    Bug: 942244
    Change-Id: I22a93738863b82592edcebe6882e75ca7f61adb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1525008
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641308}
    Former-commit-id: 6b6b41bd83e2084f697505046c7afb96d3e0d40e

--- ios/web/common/crw_content_view.h
+++ ios/web/common/crw_content_view.h
@@ -2,14 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_WEB_PUBLIC_WEB_STATE_UI_CRW_CONTENT_VIEW_H_
-#define IOS_WEB_PUBLIC_WEB_STATE_UI_CRW_CONTENT_VIEW_H_
+#ifndef IOS_WEB_COMMON_CRW_CONTENT_VIEW_H_
+#define IOS_WEB_COMMON_CRW_CONTENT_VIEW_H_
 
 #import <UIKit/UIKit.h>
 
 // UIViews conforming to CRWScrollableContent (i.e. CRWContentViews) are used
 // to display content within a WebState.
-@protocol CRWScrollableContent<NSObject>
+@protocol CRWScrollableContent <NSObject>
 
 // The scroll view used to display the content.  If |scrollView| is non-nil,
 // it will be used to back the CRWContentViewScrollViewProxy and is expected to
@@ -38,4 +38,4 @@
 // Convenience type for content views.
 typedef UIView<CRWScrollableContent> CRWContentView;
 
-#endif  // IOS_WEB_PUBLIC_WEB_STATE_UI_CRW_CONTENT_VIEW_H_
+#endif  // IOS_WEB_COMMON_CRW_CONTENT_VIEW_H_
--- ios/web/public/web_state/ui/crw_web_view_content_view.h
+++ ios/web/public/web_state/ui/crw_web_view_content_view.h
@@ -5,7 +5,7 @@
 #ifndef IOS_WEB_PUBLIC_WEB_STATE_UI_CRW_WEB_VIEW_CONTENT_VIEW_H_
 #define IOS_WEB_PUBLIC_WEB_STATE_UI_CRW_WEB_VIEW_CONTENT_VIEW_H_
 
-#import "ios/web/public/web_state/ui/crw_content_view.h"
+#import "ios/web/common/crw_content_view.h"
 
 // Wraps a web vew in a CRWContentView.
 @interface CRWWebViewContentView : CRWContentView
