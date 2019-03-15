31d5004f88b4a9be18bb6bba45053c4a0c8a38ab
Vladislav Kaznacheev <kaznacheev@google.com>
2019-03-15 21:20:00 +0000

    Ash Tray: Fix tray bubble opening on another display.
    An off-by-one error caused the anchor bounds of the bubble to be inside
    the bounds of the next display rather than the current display, causing
    the bubble to be shown on the wrong display.
    
    This regression was caused by http://crrev/c/1474278.
    
    TEST=manually verified & new unit test case
    BUG=937420
    
    Change-Id: I6a248a785ea1ee36bdbb8ab75853efba71938272
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1526383
    Commit-Queue: Vladislav Kaznacheev <kaznacheev@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Auto-Submit: Vladislav Kaznacheev <kaznacheev@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641288}
    Former-commit-id: 464d6f6e5243d9b754cd2caa629fd83f3b3a6ec6

--- ash/system/unified/unified_slider_bubble_controller.cc
+++ ash/system/unified/unified_slider_bubble_controller.cc
@@ -168,7 +168,11 @@ void UnifiedSliderBubbleController::ShowBubble(SliderType slider_type) {
   init_params.anchor_view = nullptr;
   init_params.anchor_mode = TrayBubbleView::AnchorMode::kRect;
   init_params.anchor_rect = tray_->shelf()->GetSystemTrayAnchorRect();
-  init_params.insets = gfx::Insets(kUnifiedMenuPadding, kUnifiedMenuPadding);
+  // Decrease bottom and right insets to compensate for the adjustment of
+  // the respective edges in Shelf::GetSystemTrayAnchorRect().
+  init_params.insets =
+      gfx::Insets(kUnifiedMenuPadding, kUnifiedMenuPadding,
+                  kUnifiedMenuPadding - 1, kUnifiedMenuPadding - 1);
   init_params.corner_radius = kUnifiedTrayCornerRadius;
   init_params.has_shadow = false;
 
