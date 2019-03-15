0db4d39f74caec6fac82fc78683e57f5dee5c076
Lutz Justen <ljusten@chromium.org>
2019-03-15 15:50:22 +0000

    Extract KerberosFilesHandler
    Refactors AuthPolicyCredentialsManager and puts the part that handles
    Kerberos credential caches and config files into its own separate class
    KerberosFilesHandler. This functionality is used to set Kerberos
    credentials for GSSAPI on Active Directory managed Chrome OS devices.
    The refactoring will enable the functionality to be reused for the
    cloud-managed devices as well.
    
    BUG=chromium:933798
    TEST=Ran unit tests and ExistingUserControllerActiveDirectoryTest.*
         after temporarily enable disabled tests.
    
    Change-Id: I528b84674cfed8dd9e245498bd69976ab088202f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1511395
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Lutz Justen <ljusten@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641201}
    Former-commit-id: 4715eee49b169748a4a6e47c41f8cfd8564c7c0c

--- chrome/browser/chromeos/authpolicy/auth_policy_credentials_manager.h
+++ chrome/browser/chromeos/authpolicy/auth_policy_credentials_manager.h
@@ -10,12 +10,12 @@
 
 #include "base/cancelable_callback.h"
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/chromeos/authpolicy/kerberos_files_handler.h"
 #include "chromeos/dbus/authpolicy/active_directory_info.pb.h"
 #include "chromeos/network/network_state_handler_observer.h"
 #include "components/account_id/account_id.h"
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
 #include "components/keyed_service/core/keyed_service.h"
-#include "components/prefs/pref_member.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
 
 class Profile;
@@ -34,11 +34,6 @@ class Signal;
 }
 
 namespace chromeos {
-
-// Kerberos defaults for canonicalization SPN. (see
-// https://web.mit.edu/kerberos/krb5-1.12/doc/admin/conf_files/krb5_conf.html)
-// Exported for browsertests.
-extern const char* kKrb5CnameSettings;
 
 // A service responsible for tracking user credential status. Created for each
 // Active Directory user profile.
@@ -103,9 +98,6 @@ class AuthPolicyCredentialsManager
                                  const std::string& signal_name,
                                  bool success);
 
-  // Called whenever prefs::kDisableAuthNegotiateCnameLookup is changed.
-  void OnDisabledAuthNegotiateCnameLookupChanged();
-
   Profile* const profile_;
   AccountId account_id_;
   std::string display_name_;
@@ -113,13 +105,13 @@ class AuthPolicyCredentialsManager
   bool is_get_status_in_progress_ = false;
   bool rerun_get_status_on_error_ = false;
   bool is_observing_network_ = false;
+  KerberosFilesHandler kerberos_files_handler_;
 
   // Stores message ids of shown notifications. Each notification is shown at
   // most once.
   std::set<int> shown_notifications_;
   authpolicy::ErrorType last_error_ = authpolicy::ERROR_NONE;
   base::CancelableClosure scheduled_get_user_status_call_;
-  PrefMember<bool> negotiate_disable_cname_lookup_;
 
   base::WeakPtrFactory<AuthPolicyCredentialsManager> weak_factory_{this};
   DISALLOW_COPY_AND_ASSIGN(AuthPolicyCredentialsManager);
