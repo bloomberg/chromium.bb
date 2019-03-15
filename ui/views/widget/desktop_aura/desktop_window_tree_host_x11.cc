29c521c45d904f68039ad4ab614e6ad56cb74ff2
Robert Liao <robliao@chromium.org>
2019-03-15 23:37:44 +0000

    Convert CancelableCallback and Related Code to CancelableOnceCallback
    BUG=714018,940130
    TBR=thomasanderson@chromium.org
    
    Change-Id: I98c015e854c7eaebc0b664eeb1c0e08507aeabf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1526622
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641349}
    Former-commit-id: bbaa1bb799b0b0af5b571b4a3f1c41564cb47eb2

--- ui/views/widget/desktop_aura/desktop_window_tree_host_x11.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_x11.cc
@@ -2387,9 +2387,9 @@ void DesktopWindowTreeHostX11::EnableEventListening() {
 }
 
 void DesktopWindowTreeHostX11::RestartDelayedResizeTask() {
-  delayed_resize_task_.Reset(
-      base::Bind(&DesktopWindowTreeHostX11::DelayedResize,
-                 close_widget_factory_.GetWeakPtr(), bounds_in_pixels_.size()));
+  delayed_resize_task_.Reset(base::BindOnce(
+      &DesktopWindowTreeHostX11::DelayedResize,
+      close_widget_factory_.GetWeakPtr(), bounds_in_pixels_.size()));
   base::ThreadTaskRunnerHandle::Get()->PostTask(
       FROM_HERE, delayed_resize_task_.callback());
 }
