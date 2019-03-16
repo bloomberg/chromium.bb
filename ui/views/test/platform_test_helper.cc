9a9247aabfc94f721c42fca7d43f002943c77003
Robert Liao <robliao@chromium.org>
2019-03-16 04:25:12 +0000

    Convert PlatformTestHelper::Factory to RepeatingCallback with Miscellaneous Cleanup
    BUG=714018,940130
    TBR=sky@chromium.org
    
    Change-Id: I6fe5a4d82d299bc0676a802bc42c28086dc796bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1526742
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641405}
    Former-commit-id: 5d646e711bbdcacf5379c941ad23ce3c4c46f8d7

--- ui/views/test/platform_test_helper.cc
+++ ui/views/test/platform_test_helper.cc
@@ -4,6 +4,8 @@
 
 #include "ui/views/test/platform_test_helper.h"
 
+#include <utility>
+
 #include "base/callback.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
@@ -17,7 +19,7 @@
 namespace views {
 namespace {
 
-PlatformTestHelper::Factory test_helper_factory;
+PlatformTestHelper::Factory g_test_helper_factory;
 
 }  // namespace
 
@@ -25,16 +27,16 @@ PlatformTestHelper::~PlatformTestHelper() {
   ui::TerminateContextFactoryForTests();
 }
 
-void PlatformTestHelper::set_factory(const Factory& factory) {
-  DCHECK_NE(factory.is_null(), test_helper_factory.is_null());
-  test_helper_factory = factory;
+void PlatformTestHelper::set_factory(Factory factory) {
+  DCHECK_NE(factory.is_null(), g_test_helper_factory.is_null());
+  g_test_helper_factory = std::move(factory);
 }
 
 // static
 std::unique_ptr<PlatformTestHelper> PlatformTestHelper::Create() {
-  return !test_helper_factory.is_null()
-             ? test_helper_factory.Run()
-             : base::WrapUnique(new PlatformTestHelper);
+  return g_test_helper_factory.is_null()
+             ? base::WrapUnique(new PlatformTestHelper)
+             : g_test_helper_factory.Run();
 }
 
 #if defined(USE_AURA)
