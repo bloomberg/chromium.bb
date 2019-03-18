751f0ae7f484829f4428035a6a64bb9f56b0dcb0
Peter Wen <wnwen@chromium.org>
2019-03-18 14:40:31 +0000

    Android: Update android_deps for test targets
    - Add testonly targets through testCompile dependencies
    - Plumb through testOnly and licenseAndroidCompatible
    - Add logic allowing generated targets to depend on existing third_party
      libs (junit and hamcrest).
    
    Bug: 913062
    Change-Id: I9edc1f291f4979861370cf98bc311fd73dbb5fb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1524401
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641590}
    Former-commit-id: f3ca8671852c7c804aa5c59e75a12bca5e683e02

--- tools/android/roll/android_deps/buildSrc/src/main/groovy/ChromiumPlugin.groovy
+++ tools/android/roll/android_deps/buildSrc/src/main/groovy/ChromiumPlugin.groovy
@@ -17,6 +17,9 @@ class ChromiumPlugin implements Plugin<Project> {
             /** Main type of configuration, use it for libraries that the APK depends on. */
             compile
 
+            /** Libraries that are for testing only. */
+            testCompile
+
             /**
              * Marks that the dependency will only be used during building, as an annotation
              * processor. It will not be usable in the APK, and not marked as supporting android.
