61af4fec2a3578f99869175aac5959f32e7611ca
Sean Gilhuly <sgilhuly@chromium.org>
2019-03-18 16:02:13 +0000

    Triage GLRenderer pixel tests
    There are a lot of pixel tests running on GLRenderer, but not on
    SkiaRenderer. Parameterize tests, find out which ones pass with Skia,
    and add TODOs to the rest.
    
    Bug: 939442
    Change-Id: I315449c89cf903c1418fb5c42bc78ba89d7b6296
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1509454
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Commit-Queue: Sean Gilhuly <sgilhuly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641630}
    Former-commit-id: 2ace040cf5a6e958d1c9b7a61f67d09fcf10237c

--- components/viz/service/display_embedder/skia_output_surface_impl.cc
+++ components/viz/service/display_embedder/skia_output_surface_impl.cc
@@ -657,6 +657,17 @@ void SkiaOutputSurfaceImpl::RemoveContextLostObserver(
   observers_.RemoveObserver(observer);
 }
 
+void SkiaOutputSurfaceImpl::SetCapabilitiesForTesting(
+    bool flipped_output_surface) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  DCHECK(impl_on_gpu_);
+  capabilities_.flipped_output_surface = flipped_output_surface;
+  auto callback =
+      base::BindOnce(&SkiaOutputSurfaceImplOnGpu::SetCapabilitiesForTesting,
+                     base::Unretained(impl_on_gpu_.get()), capabilities_);
+  ScheduleGpuTask(std::move(callback), std::vector<gpu::SyncToken>());
+}
+
 void SkiaOutputSurfaceImpl::InitializeOnGpuThread(base::WaitableEvent* event) {
   base::Optional<base::ScopedClosureRunner> scoped_runner;
   if (event) {
