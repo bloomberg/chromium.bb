c722804f6cb5d45d3a2441150a06d156dff9ab37
Hirokazu Honda <hiroh@chromium.org>
2019-03-18 02:10:31 +0000

    Revert "media: Remove VaapiVP8Encoder media_feature"
    This reverts commit c16db91d3b9209e609893ae8b10d726dc0ca1f15 [formerly 169c1a699fcc58ee3bfb3cd30f02484794056adb].
    
    The VP8 stream generated by using VA-API causes some issues on targets. To
    disable HW VP8 encoding using VA-API via finch, we should recover this feature
    variable.
    
    TBR=beccahughes@chromium.org
    
    Bug: 942419
    Test: None
    Change-Id: I5617fa55e3cb1af74b1e4fe7eaef1ed84e681c5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1525787
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3729@{#196}
    Cr-Branched-From: 19260737901ea731c9baac505269e4ad9638530d [formerly d4a8972e30b604f090aeda5dfff68386ae656267]-refs/heads/master@{#638880}
    Former-commit-id: 1be5a5543f209f3cb659d481f64c4fa8193f6294

--- media/base/media_switches.h
+++ media/base/media_switches.h
@@ -135,6 +135,7 @@ MEDIA_EXPORT extern const base::Feature kUseNewMediaCache;
 MEDIA_EXPORT extern const base::Feature kUseR16Texture;
 MEDIA_EXPORT extern const base::Feature kUseSurfaceLayerForVideo;
 MEDIA_EXPORT extern const base::Feature kUseSurfaceLayerForVideoPIP;
+MEDIA_EXPORT extern const base::Feature kVaapiVP8Encoder;
 MEDIA_EXPORT extern const base::Feature kVaapiVP9Encoder;
 MEDIA_EXPORT extern const base::Feature kVideoBlitColorAccuracy;
 
