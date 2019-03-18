d0bac03afaef48d4452832f78641bf12511aedaa
Adam Rice <ricea@chromium.org>
2019-03-18 08:45:45 +0000

    Factor out some ReadableStream operations for sharing
    Factor some operations out of ReadableStreamWrapper that will be shared
    with ReadableStreamNative.
    
    * GetReaderValidateOptions() verifies the type parameter to getReader()
    * PipeThroughExtractReadableWritable() performs the first part of the
      pipeThrough() implementation.
    * PipeToCheckSourceAndDestination() performs the first part of pipeTo()
    * CallTeeAndReturnBranchArray() implements tee(), delegating to
      ReadableStream::Tee.
    
    BUG=902633
    
    Change-Id: Ia25c1ea01b4a1f7f1037397c4fe0e446dee93f3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1524601
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641532}
    Former-commit-id: 81e8a55afa040618c4b48236922fe2a72dd88a95

--- third_party/blink/renderer/core/streams/miscellaneous_operations.h
+++ third_party/blink/renderer/core/streams/miscellaneous_operations.h
@@ -5,6 +5,7 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_STREAMS_MISCELLANEOUS_OPERATIONS_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_STREAMS_MISCELLANEOUS_OPERATIONS_H_
 
+#include "third_party/blink/renderer/bindings/core/v8/script_value.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "v8/include/v8.h"
 
@@ -14,10 +15,12 @@ namespace blink {
 // https://streams.spec.whatwg.org/#misc-abstract-ops
 
 class ExceptionState;
+class ReadableStream;
 class ScriptState;
 class StrategySizeAlgorithm;
 class StreamAlgorithm;
 class StreamStartAlgorithm;
+class WritableStream;
 
 // This is slightly different than the version in the standard
 // https://streams.spec.whatwg.org/#create-algorithm-from-underlying-method as
@@ -90,6 +93,48 @@ CORE_EXPORT v8::Local<v8::Promise> PromiseResolve(ScriptState*,
 
 // Implements "a promise resolved with *undefined*".
 CORE_EXPORT v8::Local<v8::Promise> PromiseResolveWithUndefined(ScriptState*);
+
+// Validates the "options" argument to ReadableStream::getReader(). This
+// implementation is shared between ReadableStreamWrapper and
+// ReadableStreamNative. If an exception is thrown validation failed.
+// TODO(ricea): Move it into the native implementation once
+// ReadableStreamWrapper is deleted.
+CORE_EXPORT void GetReaderValidateOptions(ScriptState*,
+                                          ScriptValue options,
+                                          ExceptionState&);
+
+// Extracts the "readable" and "writable" streams from the |transform_stream|
+// dictionary, validates them, and returns them via the |readable_stream| and
+// |writable_stream| out parameters. The types of |readable_stream| and
+// |writable_stream| are asymmetric because |readable_stream| is returned
+// directly to JavaScript and so there is no point in converting it to an
+// internal type.
+// TODO(ricea): Move it into the native implementation once
+// ReadableStreamWrapper is deleted.
+CORE_EXPORT void PipeThroughExtractReadableWritable(
+    ScriptState*,
+    const ReadableStream* stream,
+    ScriptValue transform_stream,
+    ScriptValue* readable_stream,
+    WritableStream** writable_stream,
+    ExceptionState&);
+
+// Verifies that |destination_value| is a WritableStream and that both it and
+// |source| are unlocked. Returns the WritableStream that was wrapped by
+// |destination_value|.
+CORE_EXPORT WritableStream* PipeToCheckSourceAndDestination(
+    ScriptState*,
+    ReadableStream* source,
+    ScriptValue destination_value,
+    ExceptionState&);
+
+// Calls Tee() on |readable|, converts the two branches to a JavaScript array
+// and returns them.
+// TODO(ricea): Move it into the native implementation once
+// ReadableStreamWrapper is deleted.
+ScriptValue CallTeeAndReturnBranchArray(ScriptState* script_state,
+                                        ReadableStream* readable,
+                                        ExceptionState& exception_state);
 
 }  // namespace blink
 
