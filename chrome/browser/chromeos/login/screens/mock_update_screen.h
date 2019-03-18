5ea7d72ba46e4f79aea37c1dd71d05aa1a51043e
Jacob Dufault <jdufault@google.com>
2019-03-18 19:32:36 +0000

    cros: Remove ScreenContext usage in UpdateScreen.
    Bug: 926511
    Change-Id: I0be2550a23984481135a999fa989e2bd33d4c078
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1506661
    Commit-Queue: Jacob Dufault <jdufault@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#641724}
    Former-commit-id: a36fe0bcbda8b2718cd8b9bc72a078afe29df477

--- chrome/browser/chromeos/login/screens/mock_update_screen.h
+++ chrome/browser/chromeos/login/screens/mock_update_screen.h
@@ -39,6 +39,15 @@ class MockUpdateView : public UpdateView {
   MOCK_METHOD1(MockBind, void(UpdateScreen* screen));
   MOCK_METHOD0(MockUnbind, void());
 
+  MOCK_METHOD1(SetEstimatedTimeLeft, void(int value));
+  MOCK_METHOD1(SetShowEstimatedTimeLeft, void(bool value));
+  MOCK_METHOD1(SetUpdateCompleted, void(bool value));
+  MOCK_METHOD1(SetShowCurtain, void(bool value));
+  MOCK_METHOD1(SetProgressMessage, void(const base::string16& value));
+  MOCK_METHOD1(SetProgress, void(int value));
+  MOCK_METHOD1(SetRequiresPermissionForCellular, void(bool value));
+  MOCK_METHOD1(SetCancelUpdateShortcutEnabled, void(bool value));
+
  private:
   UpdateScreen* screen_ = nullptr;
 };
