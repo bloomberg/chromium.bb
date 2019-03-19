140ccb0c69f4a71c957589d6aa37803e3c7d7076
Min Chen <minch@google.com>
2019-03-19 22:09:43 +0000

    Remove |use_zero_animation_type_| in TabletModeWindowState.
    |use_zero_animation_type_| in TabletModeWindowState was added to
    avoid animation when exiting overview on ending window drag. This
    should already handled by overview itself, since overview only
    animate the windows until the first maximized/fullscreen/snapped
    window.
    
    Bug: 916331
    Change-Id: I8b0db04f23f246a51e5f52349ed648d4afaefabf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531286
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Min Chen <minch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642208}
    Former-commit-id: 8a98517d92aa293dcf8f8d7529825458249ece32

--- ash/wm/tablet_mode/tablet_mode_window_manager.h
+++ ash/wm/tablet_mode/tablet_mode_window_manager.h
@@ -63,7 +63,6 @@ class ASH_EXPORT TabletModeWindowManager
 
   // OverviewObserver:
   void OnOverviewModeStarting() override;
-  void OnOverviewModeEnding(OverviewSession* overview_session) override;
   void OnOverviewModeEnded() override;
 
   // aura::WindowObserver:
@@ -159,9 +158,6 @@ class ASH_EXPORT TabletModeWindowManager
   std::unordered_set<aura::Window*> added_windows_;
 
   std::unique_ptr<wm::TabletModeEventHandler> event_handler_;
-
-  // True if overview exit type is |kWindowDragged|.
-  bool exit_overview_by_window_drag_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(TabletModeWindowManager);
 };
--- ash/wm/tablet_mode/tablet_mode_window_state.h
+++ ash/wm/tablet_mode/tablet_mode_window_state.h
@@ -55,10 +55,6 @@ class TabletModeWindowState : public wm::WindowState::State {
                    wm::WindowState::State* previous_state) override;
   void DetachState(wm::WindowState* window_state) override;
 
-  void set_use_zero_animation_type(bool use_zero_animation_type) {
-    use_zero_animation_type_ = use_zero_animation_type;
-  }
-
  private:
   friend class TabletModeControllerTestApi;
 
@@ -114,10 +110,6 @@ class TabletModeWindowState : public wm::WindowState::State {
 
   // If true, do not update bounds.
   bool defer_bounds_updates_ = false;
-
-  // If true, the animation type will be set to ZERO, which means the bounds
-  // will be updated at the end of the animation.
-  bool use_zero_animation_type_ = false;
 
   // If true, the state will not process events.
   bool ignore_wm_events_ = false;
