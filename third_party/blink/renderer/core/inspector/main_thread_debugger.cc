d0e9b52c04936a8f9250924d1483d68926463318
Alexei Filippov <alph@chromium.org>
2019-03-19 17:40:05 +0000

    DevTools: Do not crash in Runtime.evaluate on provisional frames.
    Report a DevTools protocol error instead. This is a temporary fix until
    provisional frames concept is removed.
    
    BUG=897816,578349
    
    Change-Id: I88005df96d3aa31743d8aa543aec0e34516cb9de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1529871
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642056}
    Former-commit-id: 10268bd0cf3c118c33e4f784679c75a59ba3b661

--- third_party/blink/renderer/core/inspector/main_thread_debugger.cc
+++ third_party/blink/renderer/core/inspector/main_thread_debugger.cc
@@ -282,8 +282,20 @@ void MainThreadDebugger::unmuteMetrics(int context_group_id) {
 v8::Local<v8::Context> MainThreadDebugger::ensureDefaultContextInGroup(
     int context_group_id) {
   LocalFrame* frame = WeakIdentifierMap<LocalFrame>::Lookup(context_group_id);
-  ScriptState* script_state =
-      frame ? ToScriptStateForMainWorld(frame) : nullptr;
+  if (!frame)
+    return v8::Local<v8::Context>();
+
+  // This is a workaround code with a bailout to avoid crashing in
+  // LocalWindowProxy::Initialize().
+  // We cannot request a ScriptState on a provisional frame as it would lead
+  // to a context creation on it, which is not allowed. Remove this extra check
+  // when provisional frames concept gets eliminated. See crbug.com/897816
+  // The DCHECK is kept to catch additional regressions earlier.
+  DCHECK(!frame->IsProvisional());
+  if (frame->IsProvisional())
+    return v8::Local<v8::Context>();
+
+  ScriptState* script_state = ToScriptStateForMainWorld(frame);
   return script_state ? script_state->GetContext() : v8::Local<v8::Context>();
 }
 
