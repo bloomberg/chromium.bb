691baf89d463a1eeeb6be9bf38f10cbb68607571
Matt Menke <mmenke@chromium.org>
2019-03-19 16:34:00 +0000

    SocketPools: group_name to GroupId conversion part 1.
    Introduce ClientSocketPool::GroupId class, to replace string
    group_names.
    
    This CL does not make ClientSocketPool take GroupIds as an argument,
    instead it uses an implicit GroupId to string conversion, and updates
    some tests and ClientSocketPoolManager to pass in GroupIds to APIs that
    take in string group_names.
    
    Updating consumers first because conversion from GroupId to std::string
    is simpler than the other way around.  The implicit conversion operator
    will be removed in a followup CL in which the SocketPools themselves are
    updated to take GroupIds, after all consumers have been converted.
    
    Bug: 533571
    Change-Id: I013a23ae9211ac3d038f41abd13d0de42a229611
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1524977
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642012}
    Former-commit-id: 0734416591140b1370b7bd5837746eddb146d863

--- net/socket/client_socket_pool.cc
+++ net/socket/client_socket_pool.cc
@@ -4,7 +4,10 @@
 
 #include "net/socket/client_socket_pool.h"
 
+#include <utility>
+
 #include "base/logging.h"
+#include "net/socket/stream_socket.h"
 
 namespace {
 
@@ -14,6 +17,49 @@ int64_t g_used_idle_socket_timeout_s = 300;  // 5 minutes
 }  // namespace
 
 namespace net {
+
+ClientSocketPool::GroupId::GroupId()
+    : socket_type_(SocketType::kHttp), privacy_mode_(false) {}
+
+ClientSocketPool::GroupId::GroupId(const HostPortPair& destination,
+                                   SocketType socket_type,
+                                   bool privacy_mode)
+    : destination_(destination),
+      socket_type_(socket_type),
+      privacy_mode_(privacy_mode) {}
+
+ClientSocketPool::GroupId::GroupId(const GroupId& group_id) = default;
+
+ClientSocketPool::GroupId::~GroupId() = default;
+
+ClientSocketPool::GroupId& ClientSocketPool::GroupId::operator=(
+    const GroupId& group_id) = default;
+
+ClientSocketPool::GroupId& ClientSocketPool::GroupId::operator=(
+    GroupId&& group_id) = default;
+
+std::string ClientSocketPool::GroupId::ToString() const {
+  std::string result = destination_.ToString();
+  switch (socket_type_) {
+    case ClientSocketPool::SocketType::kHttp:
+      break;
+
+    case ClientSocketPool::SocketType::kSsl:
+      result = "ssl/" + result;
+      break;
+
+    case ClientSocketPool::SocketType::kSslVersionInterferenceProbe:
+      result = "version-interference-probe/ssl/" + result;
+      break;
+
+    case ClientSocketPool::SocketType::kFtp:
+      result = "ftp/" + result;
+      break;
+  }
+  if (privacy_mode_)
+    result = "pm/" + result;
+  return result;
+}
 
 // static
 base::TimeDelta ClientSocketPool::used_idle_socket_timeout() {
