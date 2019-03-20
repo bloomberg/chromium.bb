cddbf124e6d5e5cd01a955905af1e5e737c0e4ba
David Burger <dburger@chromium.org>
2019-03-20 09:31:08 -0600

    DeployLuciSchedulerStage updates luci-scheduler.cfg to concatenated result.
    Change the DeployLuciSchedulerStage to update the LUCI consumed
    luci-scheduler.cfg to the concatenation of the one produced by chromite
    and the one generated by lucicfg in the new config repo. This allows us
    to transition to a bit more of generated config and will allow us to
    drop the jobs / triggers coming out of luci-scheduler.cfg here:
    
    https://chromium.googlesource.com/chromiumos/manifest/+/refs/heads/infra/config
    
    It will also eliminate the chromiumos -> chromeos project scheduling
    boundary jump and thus remove the need for the  extra configuration
    here:
    
    https://chrome-internal.googlesource.com/chromeos/infra/config/+/refs/heads/master/main.star#40
    
    Note that I have not tested this yet but if this is seen as the correct
    step forward I will:
    
    cros tryjob -g CL# luci-scheduler-updater-tryjob
    
    before proceeding with any submit.
    
    BUG=chromium:718230
    TEST=will run cros tryjob -g CL# luci-scheduler-updater-tryjob
    
    Change-Id: I7349711428740505ae745a905b7e47c3bd4c8a24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/1531825
    Reviewed-by: Jason Clinton <jclinton@chromium.org>
    Reviewed-by: Andrew Lamb <andrewlamb@chromium.org>
    Trybot-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Commit-Queue: David Burger <dburger@chromium.org>
    Tested-by: David Burger <dburger@chromium.org>

--- cbuildbot/stages/config_stages.py
+++ cbuildbot/stages/config_stages.py
@@ -10,7 +10,6 @@ from __future__ import print_function
 import errno
 import os
 import re
-import shutil
 import textwrap
 import traceback
 
@@ -423,12 +422,17 @@ class DeployLuciSchedulerStage(generic_stages.BuilderStage):
                  self.PROJECT_URL, self.PROJECT_BRANCH, self.project_dir)
 
   def _UpdateLuciProject(self):
-    source_file = os.path.join(constants.CHROMITE_DIR, 'config',
-                               'luci-scheduler.cfg')
+    chromite_source_file = os.path.join(constants.CHROMITE_DIR, 'config',
+                                        'luci-scheduler.cfg')
+    generated_source_file = os.path.join(self.project_dir, 'generated',
+                                         'luci-scheduler.cfg')
 
     target_file = os.path.join(self.project_dir, 'luci', 'luci-scheduler.cfg')
 
-    if osutils.ReadFile(source_file) == osutils.ReadFile(target_file):
+    concatenated_content = (osutils.ReadFile(chromite_source_file) + "\n\n"
+                            + osutils.ReadFile(generated_source_file))
+
+    if concatenated_content == osutils.ReadFile(target_file):
       logging.PrintBuildbotStepText(
           'luci-scheduler.cfg current: No Update.')
       return
@@ -440,10 +444,11 @@ class DeployLuciSchedulerStage(generic_stages.BuilderStage):
     message = textwrap.dedent('''\
       luci-scheduler.cfg: Chromite %s
 
-      Auto update to match generated file in chromite.
+      Auto update to match generated file in chromite and luci config.
       ''' % chromite_rev)
 
-    shutil.copyfile(source_file, target_file)
+    with open(target_file, "w") as f:
+      f.write(concatenated_content)
 
     git.RunGit(self.project_dir, ['add', '-A'])
     git.RunGit(self.project_dir, ['commit', '-m', message])
