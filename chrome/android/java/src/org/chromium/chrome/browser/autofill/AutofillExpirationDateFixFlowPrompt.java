74d6703dd3a8bdb1c4e9e8883589faa628642b0b
siashah <siashah@chromium.org>
2019-03-20 19:38:34 +0000

    Fix dialog dismissed metric count for fix flows.
    Make the dismissed event count only when the cause of dismissal is
    either the user clicked on cancel or pressed the back button.
    
    Bug: 941768
    Change-Id: I5d533ff995d1cc44f032bf99c256ca4f604ebc68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1521983
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Becky Zhou <huayinz@chromium.org>
    Commit-Queue: Siddharth Shah <siashah@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642610}
    Former-commit-id: 2d738e15dd9aa4af1799ba1e483ca231a566e489

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/AutofillExpirationDateFixFlowPrompt.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/AutofillExpirationDateFixFlowPrompt.java
@@ -140,7 +140,12 @@ public class AutofillExpirationDateFixFlowPrompt
 
     @Override
     public void onDismiss(PropertyModel model, int dismissalCause) {
-        mDelegate.onPromptDismissed();
+        // Do not call dismissed on the delegate if dialog was dismissed either because the user
+        // accepted to save the card or was dismissed by native code.
+        if (dismissalCause != DialogDismissalCause.POSITIVE_BUTTON_CLICKED
+                && dismissalCause != DialogDismissalCause.DISMISSED_BY_NATIVE) {
+            mDelegate.onPromptDismissed();
+        }
     }
 
     /**
