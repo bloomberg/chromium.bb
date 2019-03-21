4d68df7f8eaa801c268f212d4e8fc8d1f8c90841
Jacob DeWitt <jacde@chromium.org>
2019-03-21 17:09:17 +0000

    Remove XRView.viewMatrix
    In tests, use XRView.transform.inverse().matrix instead.
    
    This should also fix the issue where XRView.transform was not properly updating on each frame.
    
    Bug: 939615, 944206
    Change-Id: Ic28eb75a22ca0f0d230a7f19952df65ce499b405
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1521803
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Jacob DeWitt <jacde@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642984}
    Former-commit-id: 256f87e197c0e64ff0748ea8011e296aef91479d

--- third_party/blink/renderer/modules/xr/xr_view.cc
+++ third_party/blink/renderer/modules/xr/xr_view.cc
@@ -14,8 +14,7 @@ namespace blink {
 XRView::XRView(XRSession* session, XREye eye)
     : eye_(eye),
       session_(session),
-      projection_matrix_(DOMFloat32Array::Create(16)),
-      view_matrix_(DOMFloat32Array::Create(16)) {
+      projection_matrix_(DOMFloat32Array::Create(16)) {
   eye_string_ = (eye_ == kEyeLeft ? "left" : "right");
 }
 
@@ -23,13 +22,11 @@ XRView::XRView()
     : eye_(XREye::kEyeLeft),
       eye_string_("left"),
       session_(nullptr),
-      projection_matrix_(DOMFloat32Array::Create(16)),
-      view_matrix_(DOMFloat32Array::Create(16)) {}
+      projection_matrix_(DOMFloat32Array::Create(16)) {}
 
 // deep copy
 XRView::XRView(const XRView& other)
-    : projection_matrix_(DOMFloat32Array::Create(16)),
-      view_matrix_(DOMFloat32Array::Create(16)) {
+    : projection_matrix_(DOMFloat32Array::Create(16)) {
   *this = other;
 }
 
@@ -57,14 +54,10 @@ XRView& XRView::operator=(const XRView& other) {
 
 void XRView::AssignMatrices(const XRView& other) {
   const float* src_projection_data = other.projection_matrix_->Data();
-  const float* src_view_data = other.view_matrix_->Data();
-
   float* dst_projection_data = projection_matrix_->Data();
-  float* dst_view_data = view_matrix_->Data();
 
   for (int i = 0; i < 16; ++i) {
     dst_projection_data[i] = src_projection_data[i];
-    dst_view_data[i] = src_view_data[i];
   }
 }
 
@@ -218,33 +211,20 @@ std::unique_ptr<TransformationMatrix> XRView::UnprojectPointer(
   return pointer;
 }
 
-// Pass inv_pose_matrix by value because this method modifies its offset, but
+// Pass pose_matrix by value because this method modifies its offset, but
 // the calling code doesn't want it changed.
-void XRView::UpdateViewMatrix(TransformationMatrix inv_pose_matrix) {
-  // Transform by the negative offset, since we're operating on the inverted
-  // matrix
-  inv_pose_matrix.PostTranslate3d(-offset_.X(), -offset_.Y(), -offset_.Z());
-  view_matrix_ = transformationMatrixToDOMFloat32Array(inv_pose_matrix);
-
-  // transform's matrix is the inverse of the view matrix
-  // can't use the original pose matrix because it has translation applied
-  // after taking the inverse
-  // compute the inverse lazily
-  DCHECK(inv_pose_matrix.IsInvertible());
-  inv_pose_ = std::make_unique<TransformationMatrix>(inv_pose_matrix);
+void XRView::UpdatePoseMatrix(TransformationMatrix pose_matrix) {
+  pose_matrix.Translate3d(offset_.X(), offset_.Y(), offset_.Z());
+  transform_ = MakeGarbageCollected<XRRigidTransform>(pose_matrix);
 }
 
-XRRigidTransform* XRView::transform() {
-  if (!transform_) {
-    transform_ = MakeGarbageCollected<XRRigidTransform>(inv_pose_->Inverse());
-  }
+XRRigidTransform* XRView::transform() const {
   return transform_;
 }
 
 void XRView::Trace(blink::Visitor* visitor) {
   visitor->Trace(session_);
   visitor->Trace(projection_matrix_);
-  visitor->Trace(view_matrix_);
   visitor->Trace(transform_);
   ScriptWrappable::Trace(visitor);
 }
--- third_party/blink/renderer/modules/xr/xr_viewer_pose.cc
+++ third_party/blink/renderer/modules/xr/xr_viewer_pose.cc
@@ -14,15 +14,12 @@ XRViewerPose::XRViewerPose(
     XRSession* session,
     std::unique_ptr<TransformationMatrix> pose_model_matrix)
     : XRPose(std::move(pose_model_matrix), session->EmulatedPosition()) {
-  // Can only update views with an invertible matrix.
-  TransformationMatrix inv_pose_matrix = transform_->InverseTransformMatrix();
-
   // session will update views if required
   // views array gets copied to views_
   views_ = session->views();
 
   for (Member<XRView>& view : views_) {
-    view->UpdateViewMatrix(inv_pose_matrix);
+    view->UpdatePoseMatrix(transform_->TransformMatrix());
   }
 }
 
