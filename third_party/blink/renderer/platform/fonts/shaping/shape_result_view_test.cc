77244edadc2a909ef291f94f58bcf7d9e38702a0
Emil A Eklund <eae@chromium.org>
2019-03-21 18:06:44 +0000

    Optimize bounds computation when splitting ShapeResults
    Use cached bounds when splitting ShapeResults instead of querying bounds
    from Skia which is, by far, the most expensive stage of range splitting.
    
    This uncovered a couple of bugs that this change fix and adds tests for.
    Namely incorrect overall bounds for ShapeResultViews due to missing data
    on the y-axis and a lack of support for vertical text.
    
    It also changes ShapeResultView to only use the bounds for glyphs at the
    very start and end of each partial run instead of accumulating all glyph
    bounds which is unnecessary. This matches the CopyRanges implementation.
    
    This change improves performance for many blink_perf.layout tests, e.g.:
    - linux / fit-content-change-available-size-text  +10%
    - linux / chapter-reflow                          +9%
    - linux / ArabicLineLayout                        +7%
    - linux / line-layout                             +7%
    
    Bug: 636993
    Test: shape_result_view_test.cc, harfbuzz_shaper_test.cc
    Change-Id: Iafe17d7c47cb806cc072caf4ed0cf878ee0f80fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1521482
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643015}
    Former-commit-id: 810082af4b27c76485914b731c6fab7ce481956f

--- third_party/blink/renderer/platform/fonts/shaping/shape_result_view_test.cc
+++ third_party/blink/renderer/platform/fonts/shaping/shape_result_view_test.cc
@@ -191,6 +191,17 @@ TEST_F(ShapeResultViewTest, LatinMultiRun) {
   EXPECT_TRUE(CompareResultGlyphs(view_glyphs, reference_glyphs, 0u, 16u));
   EXPECT_TRUE(
       CompareResultGlyphs(composite_copy_glyphs, reference_glyphs, 0u, 16u));
+  EXPECT_EQ(composite_view->Width(), composite_copy->Width());
+
+  // Rounding of x and width may be off by ~0.1 on Mac.
+  float tolerance = 0.1f;
+  EXPECT_NEAR(composite_view->Bounds().X(), composite_copy->Bounds().X(),
+              tolerance);
+  EXPECT_NEAR(composite_view->Bounds().Width(),
+              composite_copy->Bounds().Width(), tolerance);
+  EXPECT_EQ(composite_view->Bounds().Y(), composite_copy->Bounds().Y());
+  EXPECT_EQ(composite_view->Bounds().Height(),
+            composite_copy->Bounds().Height());
 }
 
 TEST_F(ShapeResultViewTest, LatinCompositeView) {
@@ -243,6 +254,8 @@ TEST_F(ShapeResultViewTest, LatinCompositeView) {
                                static_cast<void*>(&composite_glyphs));
   EXPECT_EQ(composite_glyphs.size(), 36u);
   EXPECT_TRUE(CompareResultGlyphs(composite_glyphs, reference_glyphs, 0u, 22u));
+  EXPECT_EQ(composite_view->Width(), composite_copy->Width());
+  EXPECT_EQ(composite_view->Bounds(), composite_copy->Bounds());
 }
 
 TEST_F(ShapeResultViewTest, MixedScriptsCompositeView) {
@@ -287,6 +300,19 @@ TEST_F(ShapeResultViewTest, MixedScriptsCompositeView) {
                                static_cast<void*>(&composite_glyphs));
   EXPECT_TRUE(CompareResultGlyphs(composite_glyphs, reference_glyphs, 0u,
                                   reference_glyphs.size()));
+  EXPECT_EQ(composite_view->Width(), composite_copy->Width());
+
+  // Rounding of x may be off by ~0.1 on Mac.
+  float tolerance = 0.1f;
+  EXPECT_NEAR(composite_view->Bounds().X(), composite_copy->Bounds().X(),
+              tolerance);
+  // The rounding for the x/width might be off slightly for composite views.
+  tolerance = composite_view->Bounds().Width() / 10;
+  EXPECT_NEAR(composite_view->Bounds().Width(),
+              composite_copy->Bounds().Width(), tolerance);
+  EXPECT_EQ(composite_view->Bounds().Y(), composite_copy->Bounds().Y());
+  EXPECT_EQ(composite_view->Bounds().Height(),
+            composite_copy->Bounds().Height());
 }
 
 TEST_F(ShapeResultViewTest, TrimEndOfView) {
