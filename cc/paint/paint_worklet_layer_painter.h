f4b38f1bcd974b69c1d74a64b95c73e48e5425d2
Xida Chen <xidachen@chromium.org>
2019-03-21 16:35:29 +0000

    Make PaintWorkletLayerPainter::Paint take a PaintWorkletInput* (1/6)
    This CL is part of a prototype:
    https://chromium-review.googlesource.com/c/chromium/src/+/1495445
    
    This CL makes the PaintWorkletLayerPainter takes a PaintWorkletInput*
    and eventually the input will be passed to the CSSPaintDefinition::Paint
    function which execute the JS paint callback on the worklet thread.
    
    Bug: 884698
    Change-Id: Icf90078889e535f33cf3376030155f92fc2f161b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1525316
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#642963}
    Former-commit-id: 7626ef7259737cad60393299840aa535125bba93

--- cc/paint/paint_worklet_layer_painter.h
+++ cc/paint/paint_worklet_layer_painter.h
@@ -10,11 +10,13 @@
 
 namespace cc {
 
+class PaintWorkletInput;
+
 class CC_EXPORT PaintWorkletLayerPainter {
  public:
   virtual ~PaintWorkletLayerPainter() {}
 
-  virtual sk_sp<PaintRecord> Paint() = 0;
+  virtual sk_sp<PaintRecord> Paint(PaintWorkletInput*) = 0;
 };
 
 }  // namespace cc
