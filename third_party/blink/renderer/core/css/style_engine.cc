ef9fa3e3d39067a452295cabc2931e4b0e1ea3c2
Rune Lillesveen <futhark@chromium.org>
2019-03-21 21:54:47 +0000

    Merge to 74: Only connected shadow roots can be active tree scopes.
    Stylesheet collection will be ensured, active scope added, and active
    style will be marked dirty on ShadowRootInsertedIntoDocument(). Skip on
    adopted stylesheets changes.
    
    Added DCHECK to make sure we don't mark disconnected tree scopes dirty.
    
    Bug: 942553
    Change-Id: I71b991dbeec540e69f85a1fef3961f3b9efaa1e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1532104
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#642746}(cherry picked from commit 78245876da70f935e8de0eff65399d9d02a580db [formerly 9f0af35b7a8f4664f712c7cbd815e78833589eaa])
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535008
    Cr-Commit-Position: refs/branch-heads/3729@{#363}
    Cr-Branched-From: 19260737901ea731c9baac505269e4ad9638530d [formerly d4a8972e30b604f090aeda5dfff68386ae656267]-refs/heads/master@{#638880}
    Former-commit-id: e608f7f32be1f2ca21b4d8c8125620ad1a438d57

--- third_party/blink/renderer/core/css/style_engine.cc
+++ third_party/blink/renderer/core/css/style_engine.cc
@@ -244,6 +244,7 @@ void StyleEngine::RemovePendingSheet(Node& style_sheet_candidate_node,
 }
 
 void StyleEngine::SetNeedsActiveStyleUpdate(TreeScope& tree_scope) {
+  DCHECK(tree_scope.RootNode().isConnected());
   if (GetDocument().IsActive() || !IsMaster())
     MarkTreeScopeDirty(tree_scope);
 }
@@ -316,6 +317,9 @@ void StyleEngine::AdoptedStyleSheetsWillChange(
   for (unsigned i = index; i < new_sheets_count; ++i) {
     new_sheets[i]->AddedAdoptedToTreeScope(tree_scope);
   }
+
+  if (!tree_scope.RootNode().isConnected())
+    return;
 
   if (new_sheets_count) {
     EnsureStyleSheetCollectionFor(tree_scope);
