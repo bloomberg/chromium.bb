57e60c2dccbb09585a7f608e33904d05a2b2a4a9
Dan Beam <dbeam@chromium.org>
2019-03-21 21:28:23 +0000

    WebUI Dark Mode: update ThemeHandler to safely update on native theme changes
    This affects:
     * incognito new tab page
     * chrome://apps
    
    R=dpapad@chromium.org
    BUG=925384
    
    Change-Id: Ia2a88a433a58a176127d3ae8a92338ac0909868f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1533314
    Commit-Queue: Dan Beam <dbeam@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643123}
    Former-commit-id: a744e5ab08cf59b2f7a868c3df4672bb8cadf216

--- chrome/browser/ui/webui/theme_handler.cc
+++ chrome/browser/ui/webui/theme_handler.cc
@@ -6,6 +6,7 @@
 
 #include <memory>
 
+#include "base/bind.h"
 #include "base/values.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/profiles/profile.h"
@@ -15,38 +16,64 @@
 #include "chrome/grit/theme_resources.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/web_ui.h"
+#include "ui/native_theme/native_theme.h"
 
 ///////////////////////////////////////////////////////////////////////////////
 // ThemeHandler
 
-ThemeHandler::ThemeHandler() {
-}
+ThemeHandler::ThemeHandler() : theme_observer_(this) {}
 
-ThemeHandler::~ThemeHandler() {
-}
+ThemeHandler::~ThemeHandler() {}
 
 void ThemeHandler::RegisterMessages() {
   // These are not actual message registrations, but can't be done in the
   // constructor since they need the web_ui value to be set, which is done
   // post-construction, but before registering messages.
   InitializeCSSCaches();
+  web_ui()->RegisterMessageCallback(
+      "observeThemeChanges",
+      base::BindRepeating(&ThemeHandler::HandleObserveThemeChanges,
+                          base::Unretained(this)));
+}
+
+void ThemeHandler::OnJavascriptAllowed() {
   // Listen for theme installation.
   registrar_.Add(this, chrome::NOTIFICATION_BROWSER_THEME_CHANGED,
                  content::Source<ThemeService>(
                      ThemeServiceFactory::GetForProfile(GetProfile())));
+  // Or native theme change.
+  theme_observer_.Add(ui::NativeTheme::GetInstanceForNativeUi());
+}
+
+void ThemeHandler::OnJavascriptDisallowed() {
+  registrar_.RemoveAll();
+  theme_observer_.RemoveAll();
 }
 
 void ThemeHandler::Observe(int type,
                            const content::NotificationSource& source,
                            const content::NotificationDetails& details) {
   DCHECK_EQ(chrome::NOTIFICATION_BROWSER_THEME_CHANGED, type);
+  SendThemeChanged();
+}
+
+void ThemeHandler::OnNativeThemeUpdated(ui::NativeTheme* observed_theme) {
+  DCHECK_EQ(observed_theme, ui::NativeTheme::GetInstanceForNativeUi());
+  SendThemeChanged();
+}
+
+void ThemeHandler::HandleObserveThemeChanges(const base::ListValue* /*args*/) {
+  AllowJavascript();
+}
+
+void ThemeHandler::SendThemeChanged() {
   InitializeCSSCaches();
   bool has_custom_bg = ThemeService::GetThemeProviderForProfile(GetProfile())
                            .HasCustomImage(IDR_THEME_NTP_BACKGROUND);
   // TODO(dbeam): why does this need to be a dictionary?
   base::DictionaryValue dictionary;
   dictionary.SetBoolean("hasCustomBackground", has_custom_bg);
-  web_ui()->CallJavascriptFunctionUnsafe("ntp.themeChanged", dictionary);
+  CallJavascriptFunction("ntp.themeChanged", dictionary);
 }
 
 void ThemeHandler::InitializeCSSCaches() {
