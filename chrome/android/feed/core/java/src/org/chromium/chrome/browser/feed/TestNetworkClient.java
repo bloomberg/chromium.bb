8543428548f4665cea6fdffa817b5f853b808cf4
Brandon Wylie <wylieb@chromium.org>
2019-03-21 19:19:42 +0000

    [Feed] Import feed@5edab49db9100f92f07e0c1b7f65a36d7c3cc97f
    ******************************Resource Sizes Diff******************************
    Specifics:
           +61,472 bytes main lib size
           +71,152 bytes main dex size
          +612,260 bytes secondary dex size
        +1,069,251 bytes normalized apk size
    InstallSize:
          +385,567 bytes APK size
        +1,185,159 bytes Estimated installed size (Android Go)
        +3,146,552 bytes Estimated installed size
    InstallBreakdown (+3,145,132 bytes):
            +3,782 bytes Native resources (no l10n) size
            +2,688 bytes unwind_cfi (dev and canary only) size
            +2,216 bytes Non-compiled Android resources size
           +61,472 bytes Native code size
              +586 bytes Package metadata size
        +3,057,604 bytes Java code size
           +16,784 bytes Compiled Android resources size
    Dex:
            +4,527 entries fields
            +6,667 entries methods
            +2,027 entries types
            +4,980 entries strings
    
    Binary-Size: Increase is due to library growth.
    Change-Id: I358143560a1040a83bea19aac0955d9e5cbe6ab1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1532730
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643060}
    Former-commit-id: 1f1f540322e1b8d1282b1483eefc2fb73b69e367

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/TestNetworkClient.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/TestNetworkClient.java
@@ -8,7 +8,7 @@ import android.util.Base64;
 
 import com.google.android.libraries.feed.common.functional.Consumer;
 import com.google.android.libraries.feed.common.logging.Logger;
-import com.google.android.libraries.feed.feedrequestmanager.FeedRequestManager;
+import com.google.android.libraries.feed.feedrequestmanager.RequestHelper;
 import com.google.android.libraries.feed.host.config.Configuration;
 import com.google.android.libraries.feed.host.network.HttpRequest;
 import com.google.android.libraries.feed.host.network.HttpRequest.HttpMethod;
@@ -126,10 +126,10 @@ public class TestNetworkClient implements NetworkClient {
     private Request getRequest(HttpRequest httpRequest) throws IOException {
         byte[] rawRequest = new byte[0];
         if (httpRequest.getMethod().equals(HttpMethod.GET)) {
-            if (httpRequest.getUri().getQueryParameter(FeedRequestManager.MOTHERSHIP_PARAM_PAYLOAD)
+            if (httpRequest.getUri().getQueryParameter(RequestHelper.MOTHERSHIP_PARAM_PAYLOAD)
                     != null) {
                 rawRequest = Base64.decode(httpRequest.getUri().getQueryParameter(
-                                                   FeedRequestManager.MOTHERSHIP_PARAM_PAYLOAD),
+                                                   RequestHelper.MOTHERSHIP_PARAM_PAYLOAD),
                         Base64.URL_SAFE);
             }
         } else {
