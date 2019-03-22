d975f8df8b3e7dc257a01fce6d41749e0454aefe
Anand K. Mistry <amistry@chromium.org>
2019-03-22 00:23:23 +0000

    Reject SMB URLs that don't have a share name.
    Although these are valid URLs, they cause Samba to browse the server,
    which isn't a supported use case (yet). Instead, reject them and show
    the user the same error as other invalid URLs.
    
    BUG=907207
    
    Change-Id: I287363cf71504f624c27e42e5a0af1adfe5c4c30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1532129
    Commit-Queue: Anand Mistry <amistry@chromium.org>
    Reviewed-by: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643189}
    Former-commit-id: 74061539448a4258327e2338adcfa14970c79a3d

--- chrome/browser/chromeos/smb_client/smb_service.h
+++ chrome/browser/chromeos/smb_client/smb_service.h
@@ -105,6 +105,8 @@ class SmbService : public KeyedService,
                        StartReadDirIfSuccessfulCallback reply);
 
  private:
+  friend class SmbServiceTest;
+
   // Calls SmbProviderClient::Mount(). |temp_file_manager_| must be initialized
   // before this is called.
   void CallMount(const file_system_provider::MountOptions& options,
