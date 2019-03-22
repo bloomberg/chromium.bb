fbc42ab1c9f980ef3ff15c1962a2469f60ad8927
Caitlin Fischer <caitlinfischer@google.com>
2019-03-22 13:59:39 +0000

    [Autofill] Implemented GetLabels() for AddressPhone formatter.
    Bug: 936168
    Change-Id: I8f07320b29b2ad228e6579a171be8a1ac7504dbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1529269
    Commit-Queue: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643346}
    Former-commit-id: 52a558e753c433f6bb55da9a196cca3b7438feb8

--- components/autofill/core/browser/address_phone_form_label_formatter.cc
+++ components/autofill/core/browser/address_phone_form_label_formatter.cc
@@ -4,21 +4,52 @@
 
 #include "components/autofill/core/browser/address_phone_form_label_formatter.h"
 
+#include "components/autofill/core/browser/label_formatter_utils.h"
+
 namespace autofill {
 
 AddressPhoneFormLabelFormatter::AddressPhoneFormLabelFormatter(
     const std::string& app_locale,
     ServerFieldType focused_field_type,
     const std::vector<ServerFieldType>& field_types)
-    : LabelFormatter(app_locale, focused_field_type, field_types) {}
+    : LabelFormatter(app_locale, focused_field_type, field_types),
+      form_has_street_address_(HasStreetAddress(field_types_for_labels())) {}
 
 AddressPhoneFormLabelFormatter::~AddressPhoneFormLabelFormatter() {}
 
 std::vector<base::string16> AddressPhoneFormLabelFormatter::GetLabels(
     const std::vector<AutofillProfile*>& profiles) const {
-  // TODO(crbug.com/936168): Implement GetLabels().
   std::vector<base::string16> labels;
+  for (const AutofillProfile* profile : profiles) {
+    labels.push_back(GetLabelForFocusedGroup(*profile, GetFocusedGroup()));
+  }
   return labels;
+}
+
+// Note that the order--phone, name, and address--in which parts of the label
+// are added ensures that the label is formatted correctly for the focused
+// group.
+base::string16 AddressPhoneFormLabelFormatter::GetLabelForFocusedGroup(
+    const AutofillProfile& profile,
+    FieldTypeGroup group) const {
+  std::vector<base::string16> label_parts;
+
+  if (group != PHONE_HOME) {
+    AddLabelPartIfNotEmpty(GetLabelPhone(profile, app_locale()), &label_parts);
+  }
+
+  if (group == PHONE_HOME || group == ADDRESS_HOME) {
+    AddLabelPartIfNotEmpty(GetLabelName(profile, app_locale()), &label_parts);
+  }
+
+  if (group != ADDRESS_HOME) {
+    AddLabelPartIfNotEmpty(
+        GetLabelAddress(form_has_street_address_, profile, app_locale(),
+                        field_types_for_labels()),
+        &label_parts);
+  }
+
+  return ConstructLabelLine(label_parts);
 }
 
 }  // namespace autofill
