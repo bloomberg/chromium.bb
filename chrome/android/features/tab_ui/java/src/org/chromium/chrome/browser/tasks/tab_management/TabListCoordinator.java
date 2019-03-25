149b7401ea9d6142936815f73d92baa148814b1e
Yusuf Ozuysal <yusufo@google.com>
2019-03-25 20:41:33 +0000

    Multi thumbnail support for GridTabSwitcher
    This pulls out the ThumbnailProvider so that individual parent
    components can create more customized ones. Makes sure we don't update
    thumbnails in the strip.
    
    Using the above plumbing adds a new MultiThumbnailCardProvider that
    builds a Bitmap laying out all related tabs in a single card.
    
    Also adjusts tab_grid_card_item so that it avoids resizing the image
    inside (which wouldn't work for these multi-thumbnail bitmaps).
    
    BUG=943876
    
    Change-Id: Ifb42d9c48ed8fe5b8ff1d7f263a6b33461bfb392
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531651
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#643193}(cherry picked from commit 17358e1e81a0093d777b5fa3154baf4dad8fcd2d [formerly 9a06dca620672138e6ff33399730f121d86adf9c])
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1536489
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3729@{#426}
    Cr-Branched-From: 19260737901ea731c9baac505269e4ad9638530d [formerly d4a8972e30b604f090aeda5dfff68386ae656267]-refs/heads/master@{#638880}
    Former-commit-id: c37cd8435033079cdec5c63dce9e972e402f8d3c

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListCoordinator.java
@@ -14,7 +14,6 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.compositor.layouts.content.TabContentManager;
 import org.chromium.chrome.browser.lifecycle.Destroyable;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tab.Tab;
@@ -55,7 +54,8 @@ public class TabListCoordinator implements Destroyable {
      * @param context The context to use for accessing {@link android.content.res.Resources}.
      * @param tabModelSelector {@link TabModelSelector} that will provide and receive signals about
      *                              the tabs concerned.
-     * @param tabContentManager {@link TabContentManager} to provide screenshot related details.
+     * @param thumbnailProvider Provider to provide screenshot related details.
+     * @param titleProvider Provider for a given tab's title.
      * @param parentView {@link ViewGroup} The root view of the UI.
      * @param attachToParent Whether the UI should attach to root view.
      * @param componentName A unique string uses to identify different components for UMA recording.
@@ -63,7 +63,8 @@ public class TabListCoordinator implements Destroyable {
      *                      through actions.xml file.
      */
     TabListCoordinator(@TabListMode int mode, Context context, TabModelSelector tabModelSelector,
-            TabContentManager tabContentManager, @NonNull ViewGroup parentView,
+            TabListMediator.ThumbnailProvider thumbnailProvider,
+            TabListMediator.TitleProvider titleProvider, @NonNull ViewGroup parentView,
             boolean attachToParent, String componentName) {
         TabListModel tabListModel = new TabListModel();
         mMode = mode;
@@ -106,11 +107,11 @@ public class TabListCoordinator implements Destroyable {
 
         mRecyclerView.setHasFixedSize(true);
 
-        TabListFaviconProvider mTabListFaviconProvider =
+        TabListFaviconProvider tabListFaviconProvider =
                 new TabListFaviconProvider(context, Profile.getLastUsedProfile());
 
-        mMediator = new TabListMediator(tabListModel, tabModelSelector, tabContentManager,
-                mTabListFaviconProvider, componentName);
+        mMediator = new TabListMediator(tabListModel, tabModelSelector, thumbnailProvider,
+                titleProvider, tabListFaviconProvider, componentName);
     }
 
     /**
