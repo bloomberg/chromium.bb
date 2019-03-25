965989b595cca25df05094483239ce0f3d5f5e55
Peter Kasting <pkasting@chromium.org>
2019-03-25 22:04:49 +0000

    Disable test which is flakily timing out.
    Bug: 945194
    Change-Id: I6e9c688abdb9fcf554b7963f060fb6267328292e
    TBR: sahel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1536805
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644027}
    Former-commit-id: c58168759340e0fda4db608d04e868f64dad1bc6

--- content/browser/frame_host/render_frame_host_manager_browsertest.cc
+++ content/browser/frame_host/render_frame_host_manager_browsertest.cc
@@ -1614,8 +1614,10 @@ IN_PROC_BROWSER_TEST_F(RenderFrameHostManagerTest,
 // Ensures that a pending navigation's URL  is no longer visible after the
 // speculative RFH is discarded due to a concurrent renderer-initiated
 // navigation.  See https://crbug.com/760342.
-IN_PROC_BROWSER_TEST_F(RenderFrameHostManagerTest,
-                       ResetVisibleURLOnCrossProcessNavigationInterrupted) {
+// TODO(https://crbug.com/945194): Disabled due to flaky timeouts.
+IN_PROC_BROWSER_TEST_F(
+    RenderFrameHostManagerTest,
+    DISABLED_ResetVisibleURLOnCrossProcessNavigationInterrupted) {
   const std::string kVictimPath = "/victim.html";
   const std::string kAttackPath = "/attack.html";
   net::test_server::ControllableHttpResponse victim_response(
