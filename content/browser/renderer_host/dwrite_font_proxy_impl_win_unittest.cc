88b993e8637ee8f0f1d410e60ec9d657c62bb85d
Dominik Röttsches <drott@chromium.org>
2019-03-25 17:33:09 +0000

    Reland: Persist font unique name lookup table to cache directory on disk
    Reland after fixing an issue where temp directory creation for a unit
    tests was enclosed in a DCHECK and not executed in all compilation
    configurations. Previously reverted in 8aee65b34d793b5e [formerly 4bb916e274df5b86].
    
    When implementing font unique name matching using DWrite (issue:889864)
    we found with UMA statistics that generating the lookup table takes 10s
    of milliseconds up 15 seconds and more. On the other hand, the
    information in the table changes rarely.
    
    Rebuild the table only after startup when the registry keys describing
    installed fonts have changed, the product version of the DWrite.dll has
    changed or - as a safety fallback - when the GetProduct() version from
    ContentClient has changed.
    
    When the persistence key computation has determined that the cache is
    out of date, construct a new font lookup table by scanning font files
    and retrieve the needed name metadata, then serialize to file from the
    protobuf that is constructed when scanning the font files. When done,
    store the protobuf to a cache directory in the user profile.
    
    Detailed design doc linked from issue:828317.
    
    Bug: 941462, 941434
    Change-Id: Ib4001fc613db8b1c2484869c37ab9edeba2eabcc
    Tbr: avi, eae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1524494
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643891}
    Former-commit-id: d7aefeb9506f1a7fd2c10a289254c8b637c1c8e9

--- content/browser/renderer_host/dwrite_font_proxy_impl_win_unittest.cc
+++ content/browser/renderer_host/dwrite_font_proxy_impl_win_unittest.cc
@@ -10,6 +10,7 @@
 #include <memory>
 
 #include "base/files/file.h"
+#include "base/files/scoped_temp_dir.h"
 #include "base/memory/ref_counted.h"
 #include "base/run_loop.h"
 #include "base/test/scoped_feature_list.h"
@@ -28,11 +29,15 @@ namespace content {
 
 namespace {
 
-std::vector<std::pair<std::string, uint32_t>> expected_test_fonts = {
-    {u8"CambriaMath", 1},
-    {u8"Ming-Lt-HKSCS-ExtB", 2},
-    {u8"NSimSun", 1},
-    {u8"calibri-bolditalic", 0}};
+struct FontExpectation {
+  const char font_name[64];
+  uint16_t ttc_index;
+};
+
+constexpr FontExpectation kExpectedTestFonts[] = {{u8"CambriaMath", 1},
+                                                  {u8"Ming-Lt-HKSCS-ExtB", 2},
+                                                  {u8"NSimSun", 1},
+                                                  {u8"calibri-bolditalic", 0}};
 
 class DWriteFontProxyImplUnitTest : public testing::Test {
  public:
@@ -54,13 +59,18 @@ class DWriteFontProxyUniqueNameMatchingTest
  public:
   DWriteFontProxyUniqueNameMatchingTest() {
     feature_list_.InitAndEnableFeature(features::kFontSrcLocalMatching);
-    DWriteFontLookupTableBuilder::GetInstance()->ResetLookupTableForTesting();
-    DWriteFontLookupTableBuilder::GetInstance()
-        ->ScheduleBuildFontUniqueNameTable();
+    DWriteFontLookupTableBuilder* table_builder_instance =
+        DWriteFontLookupTableBuilder::GetInstance();
+    DCHECK(scoped_temp_dir_.CreateUniqueTempDir());
+    table_builder_instance->SetCacheDirectoryForTesting(
+        scoped_temp_dir_.GetPath());
+    table_builder_instance->ResetLookupTableForTesting();
+    table_builder_instance->SchedulePrepareFontUniqueNameTable();
   }
 
  private:
   base::test::ScopedFeatureList feature_list_;
+  base::ScopedTempDir scoped_temp_dir_;
 };
 
 TEST_F(DWriteFontProxyImplUnitTest, GetFamilyCount) {
@@ -212,17 +222,17 @@ TEST_F(DWriteFontProxyUniqueNameMatchingTest, TestFindUniqueFont) {
   dwrite_font_proxy().GetUniqueNameLookupTable(&font_table_memory);
   blink::FontTableMatcher font_table_matcher(font_table_memory.Map());
 
-  for (auto& test_font_name_index : expected_test_fonts) {
+  for (auto& test_font_name_index : kExpectedTestFonts) {
     base::Optional<blink::FontTableMatcher::MatchResult> match_result =
-        font_table_matcher.MatchName(test_font_name_index.first);
+        font_table_matcher.MatchName(test_font_name_index.font_name);
     CHECK(match_result) << "No font matched for font name: "
-                        << test_font_name_index.first;
+                        << test_font_name_index.font_name;
     base::File unique_font_file(
         base::FilePath::FromUTF8Unsafe(match_result->font_path),
         base::File::FLAG_OPEN | base::File::FLAG_READ);
     CHECK(unique_font_file.IsValid());
     CHECK_GT(unique_font_file.GetLength(), 0);
-    CHECK_EQ(test_font_name_index.second, match_result->ttc_index);
+    CHECK_EQ(test_font_name_index.ttc_index, match_result->ttc_index);
   }
 }
 
