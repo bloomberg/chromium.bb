fc05894c67611c01185c5d3890320c7c908ea30d
Robbie Gibson <rkgibson@google.com>
2019-03-25 19:49:15 +0000

    Change clipboard timeout to use separate Feature
    Before, the clipboard timeout feature was in the OmniboxBundleExperiment.
    To make it easier to change the timeout, this CL adds a separate feature
    to control the timeout that can be set anyhwere. There are currently no
    experiments going on in OmniboxBundledExperiment to change the timeout,
    so this will not break any existing experiment.
    
    Bug: 943164
    Change-Id: If75c8caef62850cc081a6f82d35e4e68e25f77e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535878
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643964}
    Former-commit-id: a6cd54a3916e62432cdb76db366c98489fde63c7

--- components/open_from_clipboard/clipboard_recent_content.cc
+++ components/open_from_clipboard/clipboard_recent_content.cc
@@ -6,6 +6,7 @@
 
 #include "base/strings/string_number_conversions.h"
 #include "base/time/time.h"
+#include "components/open_from_clipboard/clipboard_recent_content_features.h"
 #include "components/variations/variations_associated_data.h"
 #include "url/url_constants.h"
 
@@ -33,18 +34,9 @@ void ClipboardRecentContent::SetInstance(
 
 // static
 base::TimeDelta ClipboardRecentContent::MaximumAgeOfClipboard() {
-  // Identify the current setting for this parameter from the omnibox field
-  // trial.
-  std::string value_str = variations::GetVariationParamValue(
-      "OmniboxBundledExperimentV1", "ClipboardURLMaximumAge");
-  // If the parameter is not set, use a 1 hour timeout.
-  if (value_str.empty())
-    return base::TimeDelta::FromHours(1);
-  // This is a best-effort conversion; we trust the hand-crafted parameters
-  // downloaded from the server to be perfect.  There's no need for handle
-  // errors smartly.
-  int value;
-  // The value in the parameter is stored in seconds.
-  base::StringToInt(value_str, &value);
+  // Identify the current setting for this parameter from the feature, using
+  // 3600 seconds (1 hour) as a default if the parameter is not set.
+  int value = variations::GetVariationParamByFeatureAsInt(
+      kClipboardMaximumAge, kClipboardMaximumAgeParam, 3600);
   return base::TimeDelta::FromSeconds(value);
 }
