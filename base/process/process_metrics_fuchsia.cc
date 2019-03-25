ffe684d9ca76bf09b86f4f6f5b09db679df730b1
Wez <wez@chromium.org>
2019-03-25 17:34:24 +0000

    [Fuchsia] Remove some NOTIMPLEMENTED() logging.
    Silently ignore calls to process priority and metrics APIs that cannot
    currently be implemented under Fuchsia.
    
    Bug: 944885
    Change-Id: I44e71171ef084cf3823372060cc5fdbd4b448833
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1536896
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#643893}
    Former-commit-id: 65450747882d5f5aced265e2eca126f0484885e2

--- base/process/process_metrics_fuchsia.cc
+++ base/process/process_metrics_fuchsia.cc
@@ -14,24 +14,23 @@ size_t GetMaxFds() {
 
 size_t GetSystemCommitCharge() {
   // TODO(https://crbug.com/926581): Fuchsia does not support this.
-  NOTIMPLEMENTED_LOG_ONCE();
   return 0;
 }
 
 // static
 std::unique_ptr<ProcessMetrics> ProcessMetrics::CreateProcessMetrics(
     ProcessHandle process) {
-  NOTIMPLEMENTED_LOG_ONCE();  // TODO(https://crbug.com/926581).
+  // TODO(https://crbug.com/926581).
   return nullptr;
 }
 
 TimeDelta ProcessMetrics::GetCumulativeCPUUsage() {
-  NOTIMPLEMENTED_LOG_ONCE();  // TODO(https://crbug.com/926581).
+  // TODO(https://crbug.com/926581).
   return TimeDelta();
 }
 
 bool GetSystemMemoryInfo(SystemMemoryInfoKB* meminfo) {
-  NOTIMPLEMENTED_LOG_ONCE();  // TODO(https://crbug.com/926581).
+  // TODO(https://crbug.com/926581).
   return false;
 }
 
