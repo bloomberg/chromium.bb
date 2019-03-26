b3c4efc1e8ea708b68276d91447e26339f42fd3b
Austin Eng <enga@chromium.org>
2019-03-26 12:28:13 +0000

    Add DawnCallback template to convert between base::OnceCallback and Dawn C callbacks.
    DawnCallback<Callback> is a heap-allocated version of
    base::OnceCallback or base::RepeatingCallback.
    It is allocated on the heap so that it can be reinterpret_cast to/from
    DawnCallbackUserdata and passed to Dawn C callbacks.
    
    Bug: 941536
    Change-Id: I8912b14c81a2a44d16718aa9f2aa464ff0f77c6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535573
    Commit-Queue: Austin Eng <enga@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644269}
    Former-commit-id: 3ee9eae4c5056909616b7534572494455887db73

---
+++ third_party/blink/renderer/modules/webgpu/dawn_callback.h
@@ -0,0 +1,111 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_WEBGPU_DAWN_CALLBACK_H_
+#define THIRD_PARTY_BLINK_RENDERER_MODULES_WEBGPU_DAWN_CALLBACK_H_
+
+#include <memory>
+
+#include "third_party/blink/renderer/platform/wtf/functional.h"
+
+using DawnCallbackUserdata = uint64_t;
+
+namespace blink {
+
+// DawnCallback<Callback> is a heap-allocated version of
+// base::OnceCallback or base::RepeatingCallback.
+// It is allocated on the heap so that it can be reinterpret_cast to/from
+// DawnCallbackUserdata and passed to Dawn C callbacks.
+//
+// Example:
+//   DawnCallback<F>* callback =
+//     CreateDawnCallback(WTF::Bind(func, arg1));
+//
+//   // |someDawnFunction| expects callback function with arguments:
+//   //    Args... args, DawnCallbackUserdata userdata.
+//   // When it is called, it will forward to func(arg1, args...).
+//   GetProcs().someDawnFunction(
+//     callback->UnboundCallback(), callback->AsUserdata());
+template <typename Callback>
+class DawnCallback;
+
+template <template <typename> class BaseCallbackTemplate,
+          typename R,
+          typename... Args>
+class DawnCallback<BaseCallbackTemplate<R(Args...)>> {
+  using BaseCallback = BaseCallbackTemplate<R(Args...)>;
+  using UnboundCallbackFunction = R (*)(Args..., DawnCallbackUserdata);
+
+  static_assert(
+      std::is_same<BaseCallback, base::OnceCallback<R(Args...)>>::value ||
+          std::is_same<BaseCallback,
+                       base::RepeatingCallback<R(Args...)>>::value,
+      "Callback must be base::OnceCallback or base::RepeatingCallback");
+
+ public:
+  explicit DawnCallback(BaseCallback callback)
+      : callback_(std::move(callback)) {}
+
+  R Run(Args... args) && {
+    return std::move(callback_).Run(std::forward<Args>(args)...);
+  }
+
+  R Run(Args... args) const& {
+    return callback_.Run(std::forward<Args>(args)...);
+  }
+
+  void Reset() { callback_.Reset(); }
+
+  UnboundCallbackFunction UnboundCallback() {
+    return [](Args... args, DawnCallbackUserdata handle) {
+      // After this non-repeating callback is run, it should delete itself.
+      auto callback =
+          std::unique_ptr<DawnCallback>(DawnCallback::FromUserdata(handle));
+      return std::move(*callback).Run(std::forward<Args>(args)...);
+    };
+  }
+
+  UnboundCallbackFunction UnboundRepeatingCallback() {
+    return [](Args... args, DawnCallbackUserdata handle) {
+      return DawnCallback::FromUserdata(handle)->Run(
+          std::forward<Args>(args)...);
+    };
+  }
+
+  DawnCallbackUserdata AsUserdata() {
+    return static_cast<DawnCallbackUserdata>(reinterpret_cast<uintptr_t>(this));
+  }
+
+  static DawnCallback* FromUserdata(DawnCallbackUserdata userdata) {
+    return reinterpret_cast<DawnCallback*>(static_cast<uintptr_t>(userdata));
+  }
+
+ private:
+  BaseCallback callback_;
+};
+
+template <typename CallbackType>
+DawnCallback<CallbackType>* CreateDawnCallback(CallbackType cb) {
+  return new DawnCallback<CallbackType>(std::move(cb));
+}
+
+template <typename FunctionType, typename... BoundParameters>
+auto BindDawnCallback(FunctionType&& function,
+                      BoundParameters&&... bound_parameters) {
+  return CreateDawnCallback(
+      WTF::Bind(std::forward<FunctionType>(function),
+                std::forward<BoundParameters>(bound_parameters)...));
+}
+
+template <typename FunctionType, typename... BoundParameters>
+auto BindRepeatingDawnCallback(FunctionType&& function,
+                               BoundParameters&&... bound_parameters) {
+  return CreateDawnCallback(
+      WTF::BindRepeating(std::forward<FunctionType>(function),
+                         std::forward<BoundParameters>(bound_parameters)...));
+}
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_MODULES_WEBGPU_DAWN_CALLBACK_H_
