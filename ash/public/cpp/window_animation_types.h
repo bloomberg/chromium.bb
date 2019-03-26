e8cef392008ca4ebd0029127bc050b601616e292
Avery Musbach <amusbach@chromium.org>
2019-03-26 22:27:33 +0000

    splitview: Fix fading out the split view drag indicators in overview.
    According to the specification, when you drag a window to snap it, the
    preview area (referred to as "Indicator") should fade out, and
    meanwhile its inset should animate to zero:
    https://mccanny.users.x20web.corp.google.com/www/splitscreen-motion/index.html#window-drop
    
    Also, it seems reasonable to expect that the highlights should fade out
    if you drag a window from the top and then back to the top, or if you
    drag a window from the top and drop it into overview. The animations
    have already been implemented in SplitViewDragIndicators, but in most
    cases, the SplitViewDragIndicators object goes out of scope and then
    you do not see the animations. See the videos linked below for details.
    The present CL gives animations a little extra time after the
    SplitViewDragIndicators object is destroyed.
    
    BEFORE:
    https://photos.app.goo.gl/81kc7xbxQgU67CvJA
    https://photos.app.goo.gl/evrzpBBoXuAqZadc8
    https://photos.app.goo.gl/cVqdxzVRdBzSYgNQ8
    https://photos.app.goo.gl/YMtUEPmYPBDpVkeZ8
    https://photos.app.goo.gl/aBsW1kYoLgvvNd4N8
    
    AFTER:
    https://photos.app.goo.gl/V7XG4TX7nmF98dnv5
    https://photos.app.goo.gl/oXWbDEhtij6FGwx5A
    https://photos.app.goo.gl/yUWToSHEH2u5k3kY7
    https://photos.app.goo.gl/5yTNQ2U4CkH4AG2aA
    https://photos.app.goo.gl/p1fW6n9edrod2SbeA
    
    Test: manual
    Bug: 934977
    Change-Id: I5ac3c6982f620dd90befe62849b4c60e5c7762bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1536463
    Commit-Queue: Avery Musbach <amusbach@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644561}
    Former-commit-id: ca25838b3a7e23b0072f87ccf7c17bddf86a2ac0

--- ash/public/cpp/window_animation_types.h
+++ ash/public/cpp/window_animation_types.h
@@ -24,8 +24,7 @@ enum WindowVisibilityAnimationType {
   // Animate a window out of the closest side of the screen. Fade in if it
   // re-appears.
   WINDOW_VISIBILITY_ANIMATION_TYPE_FADE_IN_SLIDE_OUT,
-  // Show the window at the end of animation. Note, this is only a type for
-  // showing window.
+  // Show or hide the window at the end of animation.
   WINDOW_VISIBILITY_ANIMATION_TYPE_STEP_END,
 };
 
