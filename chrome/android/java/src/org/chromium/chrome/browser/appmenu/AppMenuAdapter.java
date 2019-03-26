246a9f6b9caa0d9b7e283ae2ffe313efb10703e6
Shakti Sahu <shaktisahu@chromium.org>
2019-03-26 23:24:03 +0000

    Revert "IPH : Set focus to highlighted menu item after opening app menu"
    This reverts commit a97f3960e6ed1d842240ac0e5ba7ac7d7ae1394c [formerly d6a0a21a05f1cf0fa8bdf4aec4563627090589ef].
    
    Reason for revert: As per the discussion in the bug, it was decided to not set the accessibility focus on the highlighted menu item after opening the app menu.
    
    Original change's description:
    > IPH : Set focus to highlighted menu item after opening app menu
    > 
    > This CL adds the following:
    > 1 - After opening chrome menu, if there is a highlighted menu item due
    > to IPH (both for circle and rectangle), accessibility focus will be set
    > to the highlighted view. It still needs a delay of about 1 second after
    > the menu opens, without which focus is not set to the correct view.
    > 
    > Bug: 767075
    > Change-Id: I5f4418537e8922189a5fe9f19bfcc70eadcaa000
    > Reviewed-on: https://chromium-review.googlesource.com/c/1404566
    > Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#622565}
    
    TBR=dtrainor@chromium.org,shaktisahu@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 767075
    Change-Id: I2cf4a9d3a5c7e0d7aef2881030ca242512357470
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1540267
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644587}
    Former-commit-id: 4ff1171993774f609242b6eb517d94665d08b8ca

--- chrome/android/java/src/org/chromium/chrome/browser/appmenu/AppMenuAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/appmenu/AppMenuAdapter.java
@@ -106,7 +106,6 @@ class AppMenuAdapter extends BaseAdapter {
     private final int mNumMenuItems;
     private final Integer mHighlightedItemId;
     private final float mDpToPx;
-    private View mHighlightedView;
 
     public AppMenuAdapter(AppMenu appMenu, List<MenuItem> menuItems, LayoutInflater inflater,
             Integer highlightedItemId) {
@@ -293,19 +292,12 @@ class AppMenuAdapter extends BaseAdapter {
         }
 
         if (mHighlightedItemId != null && item.getItemId() == mHighlightedItemId) {
-            mHighlightedView = convertView;
             ViewHighlighter.turnOnHighlight(convertView, false);
         } else {
-            if (mHighlightedView == convertView) mHighlightedView = null;
             ViewHighlighter.turnOffHighlight(convertView);
         }
 
         return convertView;
-    }
-
-    /** @return The view currently highlighted. */
-    public View getHighlightedView() {
-        return mHighlightedView;
     }
 
     private void setupCheckBox(AppMenuItemIcon button, final MenuItem item) {
@@ -350,10 +342,8 @@ class AppMenuAdapter extends BaseAdapter {
         button.setOnLongClickListener(v -> mAppMenu.onItemLongClick(item, v));
 
         if (mHighlightedItemId != null && item.getItemId() == mHighlightedItemId) {
-            mHighlightedView = button;
             ViewHighlighter.turnOnHighlight(button, true);
         } else {
-            if (mHighlightedView == button) mHighlightedView = null;
             ViewHighlighter.turnOffHighlight(button);
         }
 
