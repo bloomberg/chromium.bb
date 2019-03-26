4f75176bcc65ce30b5c132a6b6a24e9620a0bf59
Mikel Astiz <mastiz@chromium.org>
2019-03-26 16:23:22 +0000

    Stop session sync when user signs out (sync paused)
    This is the most reliable way to make sure session-related information
    is not tracked for syncing purposes while sync is paused (DICe signout).
    
    It also means chrome://history and possibly other features will stop
    exposing foreign tabs are soon as the user signs out.
    
    Bug: 914747
    Change-Id: I97e0922632e51fd2b25cc9cca686e4db8d7b7794
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535889
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644354}
    Former-commit-id: e552f2ea7996f962ef7da5517c70ae3601ce454b

--- components/sync_sessions/session_model_type_controller.cc
+++ components/sync_sessions/session_model_type_controller.cc
@@ -7,10 +7,19 @@
 #include <utility>
 
 #include "base/bind.h"
+#include "base/feature_list.h"
 #include "components/prefs/pref_service.h"
+#include "components/sync/driver/sync_auth_util.h"
 #include "components/sync/driver/sync_service.h"
+#include "google_apis/gaia/google_service_auth_error.h"
 
 namespace sync_sessions {
+namespace {
+
+const base::Feature kStopSessionsIfSyncPaused{"StopSessionsIfSyncPaused",
+                                              base::FEATURE_ENABLED_BY_DEFAULT};
+
+}  // namespace
 
 SessionModelTypeController::SessionModelTypeController(
     syncer::SyncService* sync_service,
@@ -21,6 +30,9 @@ SessionModelTypeController::SessionModelTypeController(
       sync_service_(sync_service),
       pref_service_(pref_service),
       history_disabled_pref_name_(history_disabled_pref_name) {
+  // TODO(crbug.com/906995): remove this observing mechanism once all sync
+  // datatypes are stopped by ProfileSyncService, when sync is paused.
+  sync_service_->AddObserver(this);
   pref_registrar_.Init(pref_service);
   pref_registrar_.Add(
       history_disabled_pref_name_,
@@ -29,11 +41,21 @@ SessionModelTypeController::SessionModelTypeController(
           base::AsWeakPtr(this)));
 }
 
-SessionModelTypeController::~SessionModelTypeController() {}
+SessionModelTypeController::~SessionModelTypeController() {
+  sync_service_->RemoveObserver(this);
+}
 
 bool SessionModelTypeController::ReadyForStart() const {
   DCHECK(CalledOnValidThread());
-  return !pref_service_->GetBoolean(history_disabled_pref_name_);
+  return !pref_service_->GetBoolean(history_disabled_pref_name_) &&
+         !(syncer::IsWebSignout(sync_service_->GetAuthError()) &&
+           base::FeatureList::IsEnabled(kStopSessionsIfSyncPaused));
+}
+
+void SessionModelTypeController::OnStateChanged(syncer::SyncService* sync) {
+  DCHECK(CalledOnValidThread());
+  // Most of these calls will be no-ops but SyncService handles that just fine.
+  sync_service_->ReadyForStartChanged(type());
 }
 
 void SessionModelTypeController::OnSavingBrowserHistoryPrefChanged() {
