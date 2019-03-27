79acc37440c1161f9fc061cd6c6c5c09dc6cb033
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2019-03-27 21:05:07 +0000

    device_monitors: Move udev operations to a separate task runner
    udev API calls can block, even if for a short amount of time. UdevLinux
    does not annotate its calls to udev with ScopedBlockingCall, but
    UdevWatcher, which we want to migrate to, does.
    
    Lay out the groundwork for that migration by first making sure we run all
    UdevLinux operations in a separate, blocking task runner, rather than in the
    browser IO thread.
    
    This actually allows us to simplify and modernize the DeviceMonitorLinux:
    there is no need to have |io_task_runner_| around at all, and we also do not
    need to use the now-deprecated MessageLoop destructor observer code. All we
    need is to call the udev code in a separate task runner that can block, and
    notify base::SystemMonitor that something changed. base::SystemMonitor has
    all the infrastructure to properly notify its observers in their task
    runners, so we do not need to worry about that.
    
    Bug: 940514
    Change-Id: I0b19d3874cbc7518e54bdca5902ef6ef0109ec01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535870
    Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/master@{#644991}
    Former-commit-id: 1d3102350fe9eed56fe890814c0a2f492db8a07e

--- media/device_monitors/device_monitor_udev.cc
+++ media/device_monitors/device_monitor_udev.cc
@@ -6,13 +6,12 @@
 
 #include "media/device_monitors/device_monitor_udev.h"
 
-#include <stddef.h>
-
 #include <string>
 
 #include "base/bind.h"
-#include "base/macros.h"
+#include "base/sequence_checker.h"
 #include "base/system/system_monitor.h"
+#include "base/task/post_task.h"
 #include "device/udev_linux/udev.h"
 #include "device/udev_linux/udev_linux.h"
 
@@ -37,39 +36,47 @@ const SubsystemMap kSubsystemMap[] = {
 
 namespace media {
 
-DeviceMonitorLinux::DeviceMonitorLinux(
-    const scoped_refptr<base::SingleThreadTaskRunner>& io_task_runner)
-    : io_task_runner_(io_task_runner) {
-  io_task_runner_->PostTask(
-      FROM_HERE,
-      base::BindOnce(&DeviceMonitorLinux::Initialize, base::Unretained(this)));
+// Wraps a device::UdevLinux with an API that makes it easier to use from
+// DeviceMonitorLinux. Since it is essentially a wrapper around blocking udev
+// calls, Initialize() must be called from a task runner that can block.
+class DeviceMonitorLinux::BlockingTaskHelper {
+ public:
+  BlockingTaskHelper();
+  ~BlockingTaskHelper() = default;
+
+  void Initialize();
+
+ private:
+  void OnDevicesChanged(udev_device* device);
+
+  std::unique_ptr<device::UdevLinux> udev_;
+
+  SEQUENCE_CHECKER(sequence_checker_);
+
+  DISALLOW_COPY_AND_ASSIGN(BlockingTaskHelper);
+};
+
+DeviceMonitorLinux::BlockingTaskHelper::BlockingTaskHelper() {
+  // Detaches from the sequence on which this object was created. It will be
+  // bound to its owning sequence when Initialize() is called.
+  DETACH_FROM_SEQUENCE(sequence_checker_);
 }
 
-DeviceMonitorLinux::~DeviceMonitorLinux() = default;
-
-void DeviceMonitorLinux::Initialize() {
-  DCHECK(io_task_runner_->BelongsToCurrentThread());
-
-  // We want to be notified of IO message loop destruction to delete |udev_|.
-  base::MessageLoopCurrent::Get()->AddDestructionObserver(this);
-
+void DeviceMonitorLinux::BlockingTaskHelper::Initialize() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   std::vector<device::UdevLinux::UdevMonitorFilter> filters;
   for (const SubsystemMap& entry : kSubsystemMap) {
     filters.push_back(
         device::UdevLinux::UdevMonitorFilter(entry.subsystem, entry.devtype));
   }
-  udev_.reset(new device::UdevLinux(
-      filters, base::Bind(&DeviceMonitorLinux::OnDevicesChanged,
-                          base::Unretained(this))));
+  udev_ = std::make_unique<device::UdevLinux>(
+      filters, base::BindRepeating(&BlockingTaskHelper::OnDevicesChanged,
+                                   base::Unretained(this)));
 }
 
-void DeviceMonitorLinux::WillDestroyCurrentMessageLoop() {
-  DCHECK(io_task_runner_->BelongsToCurrentThread());
-  udev_.reset();
-}
-
-void DeviceMonitorLinux::OnDevicesChanged(udev_device* device) {
-  DCHECK(io_task_runner_->BelongsToCurrentThread());
+void DeviceMonitorLinux::BlockingTaskHelper::OnDevicesChanged(
+    udev_device* device) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(device);
 
   base::SystemMonitor::DeviceType device_type =
@@ -83,7 +90,26 @@ void DeviceMonitorLinux::OnDevicesChanged(udev_device* device) {
   }
   DCHECK_NE(device_type, base::SystemMonitor::DEVTYPE_UNKNOWN);
 
+  // base::SystemMonitor takes care of notifying each observer in their own task
+  // runner via base::ObserverListThreadSafe.
   base::SystemMonitor::Get()->ProcessDevicesChanged(device_type);
 }
+
+DeviceMonitorLinux::DeviceMonitorLinux()
+    : blocking_task_runner_(base::CreateSequencedTaskRunnerWithTraits(
+          {base::MayBlock(), base::TaskPriority::USER_VISIBLE,
+           base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN})),
+      blocking_task_helper_(new BlockingTaskHelper,
+                            base::OnTaskRunnerDeleter(blocking_task_runner_)) {
+  // Unretained() is safe because the deletion of |blocking_task_helper_|
+  // is scheduled on |blocking_task_runner_| when DeviceMonitorLinux is
+  // deleted.
+  blocking_task_runner_->PostTask(
+      FROM_HERE,
+      base::BindOnce(&DeviceMonitorLinux::BlockingTaskHelper::Initialize,
+                     base::Unretained(blocking_task_helper_.get())));
+}
+
+DeviceMonitorLinux::~DeviceMonitorLinux() = default;
 
 }  // namespace media
