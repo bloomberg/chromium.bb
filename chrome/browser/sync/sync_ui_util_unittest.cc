db3f0ffb0002c51b76b02c44761dc2b80bc5916a
Marc Treib <treib@chromium.org>
2019-03-27 16:01:31 +0000

    Revert "Disable SyncUIUtilTest.DistinctCasesReportUniqueMessageSets"
    This reverts commit 906d2ccbbafd4ca5649c7a01481cf5d13a2cb569 [formerly 3527922026746fbe6958a341a1a5a70f94b573e4].
    
    Reason for revert: It looks like all the flakes were caused by unsubmitted code (i.e. pending CLs).
    
    Original change's description:
    > Disable SyncUIUtilTest.DistinctCasesReportUniqueMessageSets
    > 
    > TBR=treib@chromium.org
    > 
    > Bug: 945628
    > Change-Id: I48f13c55991df57c29ae44e3db037b230de3f432
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535655
    > Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#644185}
    
    TBR=horo@chromium.org,treib@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 945628
    Change-Id: Iab4376dd047c482e43c6ef9105cc339415b3e418
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1541778
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644824}
    Former-commit-id: 7686bdcc2e4fcf28ab7d37a26ec90c1564097b3a

--- chrome/browser/sync/sync_ui_util_unittest.cc
+++ chrome/browser/sync/sync_ui_util_unittest.cc
@@ -163,8 +163,7 @@ std::unique_ptr<TestingProfile> BuildSignedInTestingProfile() {
 
 // This test ensures that each distinctive SyncService status will return a
 // unique combination of status and link messages from GetStatusLabels().
-// TODO(crbug.com/945628): Disabled due to flakiness.
-TEST_F(SyncUIUtilTest, DISABLED_DistinctCasesReportUniqueMessageSets) {
+TEST_F(SyncUIUtilTest, DistinctCasesReportUniqueMessageSets) {
   std::set<base::string16> messages;
   for (int idx = 0; idx != NUMBER_OF_STATUS_CASES; idx++) {
     std::unique_ptr<Profile> profile = BuildTestingProfile();
