93185d28b3003d868485460fef1074acb1b5f482
Yashar Dabiran <yashard@google.com>
2019-03-27 23:12:31 +0000

    Move feature policy feature declarations to feature_policy_features.json5
    This CL is part of the changes needed for this design doc:
    https://docs.google.com/document/d/1Qog5qwZ-6Rfx10AKkJxN-LrwhCw_GJ1xdLoQSnpsT1Q
    
    Changes in this CL:
    - Move all feature declarations from feature_policy.cc to feature_policy_features.json5.
      All the necessary code is generated in 'feature_policy_helper.cc'.
    - Make the parser use the generated functions defined in 'feature_policy_helper.h'.
    
    * This CL should not cause any behavioral changes.
    
    Bug: 934491
    Change-Id: I156737c3a9972027451f42d0eb81790bc3249573
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1535793
    Commit-Queue: Yashar Dabiran <yashard@google.com>
    Reviewed-by: Luna Lu <loonybear@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645068}
    Former-commit-id: 54946e9221e1ed1f89711d78064706bbb9525bef

--- third_party/blink/renderer/core/feature_policy/feature_policy_helper.h
+++ third_party/blink/renderer/core/feature_policy/feature_policy_helper.h
@@ -12,13 +12,15 @@
 
 namespace blink {
 
+using FeatureNameMap = HashMap<String, mojom::FeaturePolicyFeature>;
+
 class ExecutionContext;
 
 // This method defines the feature names which will be recognized by the parser
 // for the Feature-Policy HTTP header and the <iframe> "allow" attribute, as
 // well as the features which will be recognized by the document or iframe
 // policy object.
-const HashMap<String, mojom::FeaturePolicyFeature>& DefaultFeatureNameMap();
+const FeatureNameMap& GetDefaultFeatureNameMap();
 
 // Returns true if this feature is currently disabled by an origin trial (it is
 // origin trial controlled, and the origin trial is not enabled).
