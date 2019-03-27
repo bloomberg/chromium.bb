dcdcc1e8772b10ef47a269855e615cb519074a81
Avi Drissman <avi@chromium.org>
2019-03-27 16:44:12 +0000

    [Mac Updater] Add a staging-key-changed callback to the staging watcher.
    Also do some cleanup of naming, and use CFRunLoop calls rather
    than NSPorts to stop running NSRunLoops.
    
    BUG=496298
    
    Change-Id: Iaa862268ae2a0b40e2eedfbc482da2cf4c69e4e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1540099
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#644849}
    Former-commit-id: 35b27074c9a1b3b4424fb3ea039f46d51c52bd46

--- chrome/common/mac/staging_watcher.h
+++ chrome/common/mac/staging_watcher.h
@@ -12,24 +12,50 @@
 // this state of "update pending" is indicated outside of Keystone by a key in
 // the CFPreferences.
 
+using StagingKeyChangedObserver = void (^)(BOOL stagingKeySet);
+
+// An object to observe the staging key. It can be used in either of two ways:
+// 1. To wait for the staging key to clear.
+// 2. To notify when the staging key changes state.
 @interface CrStagingKeyWatcher : NSObject
 
-- (instancetype)init;
+// On macOS 10.11 and earlier, polling is used, and |pollingTime| specifies the
+// frequency of the polling. On macOS 10.12 and later, no polling is performed
+// and |pollingTime| is ignored.
+- (instancetype)initWithPollingTime:(NSTimeInterval)pollingTime;
+
+// Returns a boolean indicating whether or not the staging key is set.
+- (BOOL)isStagingKeySet;
 
 // Sleeps until the staging key is clear. If there is no staging key set,
 // returns immediately.
-- (void)waitForStagingKey;
+- (void)waitForStagingKeyToClear;
+
+// Sets a block to be called when the staging key changes, and starts observing.
+// Only one observer may be set for a given CrStagingKeyWatcher; calling this
+// method again replaces the current observer.
+- (void)setStagingKeyChangedObserver:(StagingKeyChangedObserver)block;
 
 @end
 
 @interface CrStagingKeyWatcher (TestingInterface)
 
-- (instancetype)initWithUserDefaults:(NSUserDefaults*)defaults;
+// The designated initializer. Allows a non-default NSUserDefaults to be
+// specified.
+- (instancetype)initWithUserDefaults:(NSUserDefaults*)defaults
+                         pollingTime:(NSTimeInterval)pollingTime;
 
+// KVO works for NSUserDefaults on 10.12 and later. This method turns off the
+// use of KVO to allow the macOS 10.11 and earlier code path to be tested on
+// 10.12 and later.
 - (void)disableKVOForTesting;
 
+// Returns whether the last call to -waitForStagingKeyToClear blocked or
+// returned immediately.
 - (BOOL)lastWaitWasBlockedForTesting;
 
+// Returns the NSUserDefaults key that is used to indicate staging. The value to
+// be used is an array of strings, each string being a file path to the bundle.
 + (NSString*)stagingKeyForTesting;
 
 @end
