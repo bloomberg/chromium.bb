c9c6a51075ab726cc731e357c0c34b257363bfcd
Jonathan Backer <backer@chromium.org>
2019-03-27 23:00:29 +0000

    Add SkiaOutputDeviceGL
    This CL moves some platform specific logic out of
    SkiaOutputSurfaceImplOnGpu and into SkiaOutputDeviceGL. I also added the
    hooks to support partial swaps and tested on an i7 gLinux laptop.
    
    Bug: 920344
    Change-Id: I77b86442fef2340094de9fa93057f1fa2af2f50b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1539792
    Commit-Queue: Jonathan Backer <backer@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645059}
    Former-commit-id: 78b558a11a7b661c9dfd30d0bd7e59ce5e92b89f

--- components/viz/service/display_embedder/skia_output_device.cc
+++ components/viz/service/display_embedder/skia_output_device.cc
@@ -4,9 +4,16 @@
 
 #include "components/viz/service/display_embedder/skia_output_device.h"
 
+#include <utility>
+
+#include "third_party/skia/include/core/SkSurface.h"
+#include "ui/gfx/presentation_feedback.h"
+
 namespace viz {
 
-SkiaOutputDevice::SkiaOutputDevice() = default;
+SkiaOutputDevice::SkiaOutputDevice(
+    DidSwapBufferCompleteCallback did_swap_buffer_complete_callback)
+    : did_swap_buffer_complete_callback_(did_swap_buffer_complete_callback) {}
 
 SkiaOutputDevice::~SkiaOutputDevice() = default;
 
@@ -14,9 +21,45 @@ bool SkiaOutputDevice::SupportPostSubBuffer() {
   return false;
 }
 
-gfx::SwapResult SkiaOutputDevice::PostSubBuffer(const gfx::Rect& rect) {
+gfx::SwapResponse SkiaOutputDevice::PostSubBuffer(
+    const gfx::Rect& rect,
+    BufferPresentedCallback feedback) {
   NOTREACHED();
-  return gfx::SwapResult::SWAP_FAILED;
+  StartSwapBuffers(std::move(feedback));
+  return FinishSwapBuffers(gfx::SwapResult::SWAP_FAILED);
+}
+
+void SkiaOutputDevice::StartSwapBuffers(
+    base::Optional<BufferPresentedCallback> feedback) {
+  DCHECK(!feedback_);
+  DCHECK(!params_);
+
+  feedback_ = std::move(feedback);
+  params_.emplace();
+  params_->swap_response.swap_id = ++swap_id_;
+  params_->swap_response.swap_start = base::TimeTicks::Now();
+}
+
+gfx::SwapResponse SkiaOutputDevice::FinishSwapBuffers(gfx::SwapResult result) {
+  DCHECK(params_);
+
+  params_->swap_response.result = result;
+  params_->swap_response.swap_end = base::TimeTicks::Now();
+  if (feedback_) {
+    std::move(*feedback_)
+        .Run(gfx::PresentationFeedback(
+            params_->swap_response.swap_start, base::TimeDelta() /* interval */,
+            params_->swap_response.result == gfx::SwapResult::SWAP_ACK
+                ? 0
+                : gfx::PresentationFeedback::Flags::kFailure));
+  }
+  did_swap_buffer_complete_callback_.Run(
+      *params_, gfx::Size(draw_surface_->width(), draw_surface_->height()));
+
+  feedback_.reset();
+  auto response = params_->swap_response;
+  params_.reset();
+  return response;
 }
 
 }  // namespace viz
