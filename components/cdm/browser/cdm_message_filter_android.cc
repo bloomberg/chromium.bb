2dd18acec3a6490b9919a85a6204e387842c1122
John Rummell <jrummell@chromium.org>
2019-03-27 22:58:20 +0000

    Check for key systems on a USER_VISIBLE thread
    Using a BEST_EFFORT thread causes the sync IPC to take 3 minutes,
    causing a long delay when first loading a site using EME. As the
    user is waiting for the page to load, this is user visible, so
    changing the thread priority to reflect that.
    
    BUG=946008
    TEST=loaded EME site after install on emulator, no delay
    
    Change-Id: I526d5258657a4dadc3d317edcd784b17ed789a12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1541984
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645057}
    Former-commit-id: a7ae450d0d465647eaff6dec086bd3f1aabd2a7d

--- components/cdm/browser/cdm_message_filter_android.cc
+++ components/cdm/browser/cdm_message_filter_android.cc
@@ -100,7 +100,7 @@ CdmMessageFilterAndroid::CdmMessageFilterAndroid(
     bool force_to_support_secure_codecs)
     : BrowserMessageFilter(EncryptedMediaMsgStart),
       task_runner_(base::CreateSequencedTaskRunnerWithTraits(
-          {base::MayBlock(), base::TaskPriority::BEST_EFFORT})),
+          {base::MayBlock(), base::TaskPriority::USER_VISIBLE})),
       can_persist_data_(can_persist_data),
       force_to_support_secure_codecs_(force_to_support_secure_codecs) {}
 
