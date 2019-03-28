a571ae5ef0d6e49fdd63788704f4a17ae1a9c7a5
Becca Hughes <beccahughes@chromium.org>
2019-03-28 21:22:34 +0000

    Reland "[Media Notification] Derive background color from artwork"
    This is a reland of f323fab2fea756011a868d1d682a9a4c550aeb77 [formerly e60e1e6e093757e27623a4c6e772500b2ce6c784]
    
    TBR=estade@chromium.org,sadrul@chromium.org
    
    Original change's description:
    > [Media Notification] Derive background color from artwork
    >
    > The media notification should derive its background
    > color from prominent colors in the artwork. This
    > uses as similar algorithm from Android.
    >
    > Also breaks out some of the logic of CalculateProminentColors
    > so we can use it without the reset of the function. This
    > is because we want uninteresting colors and do not want
    > the color profile matching.
    >
    > BUG=944598
    >
    > Change-Id: I60df7f600f6fc881dfb38d2057c903a5679845df
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1534923
    > Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    > Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    > Reviewed-by: Evan Stade <estade@chromium.org>
    > Reviewed-by: Tommy Steimel <steimel@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#644985}
    
    Bug: 944598
    Change-Id: I44f904ccfe2843cf2fbe9568e9121922fd012f03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1543466
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645507}
    Former-commit-id: bdd1b3a25cefb6380c054d8d66fbf839db3a8be5

--- ash/media/media_notification_background_unittest.cc
+++ ash/media/media_notification_background_unittest.cc
@@ -11,53 +11,173 @@
 
 namespace ash {
 
+namespace {
+
+gfx::ImageSkia CreateTestBackgroundImage(SkColor first_color,
+                                         SkColor second_color,
+                                         int second_height) {
+  constexpr SkColor kRightHandSideColor = SK_ColorMAGENTA;
+
+  DCHECK_NE(kRightHandSideColor, first_color);
+  DCHECK_NE(kRightHandSideColor, second_color);
+
+  SkBitmap bitmap;
+  bitmap.allocN32Pixels(100, 100);
+
+  int first_height = bitmap.height() - second_height;
+  int right_width = bitmap.width() / 2;
+
+  // Fill the right hand side of the image with a constant color. The color
+  // derivation algorithm does not look at the right hand side so we should
+  // never see |kRightHandSideColor|.
+  bitmap.erase(kRightHandSideColor,
+               {right_width, 0, bitmap.width(), bitmap.height()});
+
+  // Fill the left hand side with |first_color|.
+  bitmap.erase(first_color, {0, 0, right_width, first_height});
+
+  // Fill the left hand side with |second_color|.
+  bitmap.erase(second_color, {0, first_height, right_width, bitmap.height()});
+
+  return gfx::ImageSkia::CreateFrom1xBitmap(bitmap);
+}
+
+gfx::ImageSkia CreateTestBackgroundImage(SkColor color) {
+  return CreateTestBackgroundImage(color, SK_ColorTRANSPARENT, 0);
+}
+
+}  // namespace
+
 class MediaNotificationBackgroundTest : public AshTestBase {
  public:
   MediaNotificationBackgroundTest() = default;
   ~MediaNotificationBackgroundTest() override = default;
 
+  void SetUp() override {
+    AshTestBase::SetUp();
+
+    owner_ = std::make_unique<views::StaticSizedView>();
+    background_ = std::make_unique<MediaNotificationBackground>(owner_.get(),
+                                                                10, 10, 0.1);
+
+    EXPECT_FALSE(GetBackgroundColor().has_value());
+  }
+
+  void TearDown() override {
+    background_.reset();
+    owner_.reset();
+
+    AshTestBase::TearDown();
+  }
+
+  MediaNotificationBackground* background() const { return background_.get(); }
+
+  base::Optional<SkColor> GetBackgroundColor() const {
+    return background_->background_color_;
+  }
+
  private:
+  std::unique_ptr<views::StaticSizedView> owner_;
+  std::unique_ptr<MediaNotificationBackground> background_;
+
   DISALLOW_COPY_AND_ASSIGN(MediaNotificationBackgroundTest);
 };
 
 TEST_F(MediaNotificationBackgroundTest, BoundsSanityCheck) {
-  views::StaticSizedView owner;
-  MediaNotificationBackground background(&owner, 10, 10, 0.1);
-
   // The test notification will have a width of 200 and a height of 50.
   gfx::Rect bounds(0, 0, 200, 50);
 
   // Check the artwork is not visible by default.
-  EXPECT_EQ(0, background.GetArtworkWidth(bounds.size()));
-  EXPECT_EQ(0, background.GetArtworkVisibleWidth(bounds.size()));
-  EXPECT_EQ(gfx::Rect(200, 0, 0, 50), background.GetArtworkBounds(bounds));
+  EXPECT_EQ(0, background()->GetArtworkWidth(bounds.size()));
+  EXPECT_EQ(0, background()->GetArtworkVisibleWidth(bounds.size()));
+  EXPECT_EQ(gfx::Rect(200, 0, 0, 50), background()->GetArtworkBounds(bounds));
   EXPECT_EQ(gfx::Rect(0, 0, 200, 50),
-            background.GetFilledBackgroundBounds(bounds));
-  EXPECT_EQ(gfx::Rect(0, 0, 0, 0), background.GetGradientBounds(bounds));
+            background()->GetFilledBackgroundBounds(bounds));
+  EXPECT_EQ(gfx::Rect(0, 0, 0, 0), background()->GetGradientBounds(bounds));
 
   // The background artwork image will have an aspect ratio of 2:1.
   SkBitmap bitmap;
   bitmap.allocN32Pixels(20, 10);
-  background.UpdateArtwork(gfx::ImageSkia::CreateFrom1xBitmap(bitmap));
+  bitmap.eraseColor(SK_ColorWHITE);
+  background()->UpdateArtwork(gfx::ImageSkia::CreateFrom1xBitmap(bitmap));
 
   // The artwork width will be 2x the height of the notification and the visible
   // width will be limited to 10% the width of the notification.
-  EXPECT_EQ(100, background.GetArtworkWidth(bounds.size()));
-  EXPECT_EQ(20, background.GetArtworkVisibleWidth(bounds.size()));
+  EXPECT_EQ(100, background()->GetArtworkWidth(bounds.size()));
+  EXPECT_EQ(20, background()->GetArtworkVisibleWidth(bounds.size()));
 
   // Update the visible width % to be greater than the width of the image.
-  background.UpdateArtworkMaxWidthPct(0.6);
-  EXPECT_EQ(100, background.GetArtworkVisibleWidth(bounds.size()));
+  background()->UpdateArtworkMaxWidthPct(0.6);
+  EXPECT_EQ(100, background()->GetArtworkVisibleWidth(bounds.size()));
 
   // Check the artwork is positioned to the right.
-  EXPECT_EQ(gfx::Rect(100, 0, 100, 50), background.GetArtworkBounds(bounds));
+  EXPECT_EQ(gfx::Rect(100, 0, 100, 50), background()->GetArtworkBounds(bounds));
 
   // Check the filled background is to the left of the image.
   EXPECT_EQ(gfx::Rect(0, 0, 100, 50),
-            background.GetFilledBackgroundBounds(bounds));
+            background()->GetFilledBackgroundBounds(bounds));
 
   // Check the gradient is positioned above the artwork.
-  EXPECT_EQ(gfx::Rect(100, 0, 40, 50), background.GetGradientBounds(bounds));
+  EXPECT_EQ(gfx::Rect(100, 0, 40, 50), background()->GetGradientBounds(bounds));
+}
+
+// If we have no artwork then we should use the default background color.
+TEST_F(MediaNotificationBackgroundTest, DeriveBackgroundColor_NoArtwork) {
+  background()->UpdateArtwork(gfx::ImageSkia());
+  EXPECT_FALSE(GetBackgroundColor().has_value());
+}
+
+// If we have artwork with no popular color then we should use the default
+// background color.
+TEST_F(MediaNotificationBackgroundTest, DeriveBackgroundColor_NoPopularColor) {
+  background()->UpdateArtwork(CreateTestBackgroundImage(SK_ColorTRANSPARENT));
+  EXPECT_FALSE(GetBackgroundColor().has_value());
+}
+
+// If the most popular color is not white or black then we should use that
+// color.
+TEST_F(MediaNotificationBackgroundTest,
+       DeriveBackgroundColor_PopularNonWhiteBlackColor) {
+  constexpr SkColor kTestColor = SK_ColorYELLOW;
+  background()->UpdateArtwork(CreateTestBackgroundImage(kTestColor));
+  EXPECT_EQ(kTestColor, GetBackgroundColor());
+}
+
+// MediaNotificationBackgroundBlackWhiteTest will repeat these tests with a
+// parameter that is either black or white.
+class MediaNotificationBackgroundBlackWhiteTest
+    : public MediaNotificationBackgroundTest,
+      public testing::WithParamInterface<SkColor> {};
+
+INSTANTIATE_TEST_SUITE_P(,
+                         MediaNotificationBackgroundBlackWhiteTest,
+                         testing::Values(SK_ColorBLACK, SK_ColorWHITE));
+
+// If the most popular color is black or white but there is no secondary color
+// we should use the most popular color.
+TEST_P(MediaNotificationBackgroundBlackWhiteTest,
+       DeriveBackgroundColor_PopularBlackWhiteNoSecondaryColor) {
+  background()->UpdateArtwork(CreateTestBackgroundImage(GetParam()));
+  EXPECT_EQ(GetParam(), GetBackgroundColor());
+}
+
+// If the most popular color is black or white and there is a secondary color
+// that is very minor then we should use the most popular color.
+TEST_P(MediaNotificationBackgroundBlackWhiteTest,
+       DeriveBackgroundColor_VeryPopularBlackWhite) {
+  background()->UpdateArtwork(
+      CreateTestBackgroundImage(GetParam(), SK_ColorYELLOW, 20));
+  EXPECT_EQ(GetParam(), GetBackgroundColor());
+}
+
+// If the most popular color is black or white but it is not that popular then
+// we should use the secondary color.
+TEST_P(MediaNotificationBackgroundBlackWhiteTest,
+       DeriveBackgroundColor_NotVeryPopularBlackWhite) {
+  constexpr SkColor kTestColor = SK_ColorYELLOW;
+  background()->UpdateArtwork(
+      CreateTestBackgroundImage(GetParam(), kTestColor, 40));
+  EXPECT_EQ(kTestColor, GetBackgroundColor());
 }
 
 }  // namespace ash
