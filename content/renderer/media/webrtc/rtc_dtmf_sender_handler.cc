0cef78e4be7905250f28b4093f6320ee73533520
Miyoung Shin <myid.shin@igalia.com>
2019-03-28 10:45:25 +0000

    Change long to int32_t in third_party/blink/public/platform/web_rtc_dtmf_sender_handler.h
    - long -> int32_t considering that WebIDL long type is signed 32 bits.
    - no logic change.
    - Reference: https://google.github.io/styleguide/cppguide.html#Integer_Types
    
    Bug: 930251
    Change-Id: I7621e803acc30058351b168a05f7db5dcf127a82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1539331
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645247}
    Former-commit-id: 0d141f2cfc849e40322eec7d64201d5d713f6df4

--- content/renderer/media/webrtc/rtc_dtmf_sender_handler.cc
+++ content/renderer/media/webrtc/rtc_dtmf_sender_handler.cc
@@ -81,11 +81,10 @@ bool RtcDtmfSenderHandler::CanInsertDTMF() {
 }
 
 bool RtcDtmfSenderHandler::InsertDTMF(const blink::WebString& tones,
-                                      long duration,
-                                      long interToneGap) {
+                                      int duration,
+                                      int interToneGap) {
   std::string utf8_tones = tones.Utf8();
-  return dtmf_sender_->InsertDtmf(utf8_tones, static_cast<int>(duration),
-                                  static_cast<int>(interToneGap));
+  return dtmf_sender_->InsertDtmf(utf8_tones, duration, interToneGap);
 }
 
 void RtcDtmfSenderHandler::OnToneChange(const std::string& tone) {
