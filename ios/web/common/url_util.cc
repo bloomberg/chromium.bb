ccca76862b5fc8dc533a2c8b6a60032187b4156c
Eugene But <eugenebut@chromium.org>
2019-03-28 22:25:44 +0000

    Move url_util* files from ios/web/public to ios/web/common.
    ios/web/common directory contains common code shared between
    //ios/web and the rest of the layers (//components,
    //ios/chrome, //ios/web_view). This directory does not
    contain the public API to create and manage web contents
    (//ios/web//public is public API directory).
    
    
    Bug: 942244
    Change-Id: I9dd0cdf3413f0a7f353971a01833e091b7002817
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1542732
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645544}
    Former-commit-id: ed336ff4620909be040c9985fc56182631520905

--- ios/web/common/url_util.cc
+++ ios/web/common/url_util.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ios/web/public/url_util.h"
+#include "ios/web/common/url_util.h"
 
 namespace web {
 
