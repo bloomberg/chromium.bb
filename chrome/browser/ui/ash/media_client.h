f6f81ca413a9e3805ea10f6ce3e42a86bcfa02ce
Becca Hughes <beccahughes@chromium.org>
2019-03-28 22:28:05 +0000

    [Media Keys] Fix handling for extensions on CrOS
    Media keys are system keys that should be handled
    in ash on CrOS. This causes problems with extensions
    that want to use them. This fixes that by doing
    the following:
    
     1) Adds a state to ash::MediaController that will
        force the media keys to be handled in
        MediaClient.
     2) Adds support for MediaClient to have a custom
        media key handler that will be used if the
        associated browser is active.
     3) Adds code to extensions to add register the
        custom key handler if an extension is
        overriding the media keys.
    
    BUG=936519
    
    Change-Id: I8b8b7836bd0e840f304299069a1299e6ca94a605
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1512709
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645548}
    Former-commit-id: 5bffca3c418a324146255f473c74e59a82c3881f

--- chrome/browser/ui/ash/media_client.h
+++ chrome/browser/ui/ash/media_client.h
@@ -6,17 +6,26 @@
 #define CHROME_BROWSER_UI_ASH_MEDIA_CLIENT_H_
 
 #include "ash/public/interfaces/media.mojom.h"
+#include "base/containers/flat_map.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/media/webrtc/media_capture_devices_dispatcher.h"
-#include "content/public/browser/media_session.h"
+#include "chrome/browser/ui/browser_list_observer.h"
 #include "mojo/public/cpp/bindings/associated_binding.h"
+#include "ui/base/accelerators/media_keys_listener.h"
 
 class MediaClient : public ash::mojom::MediaClient,
+                    public BrowserListObserver,
                     MediaCaptureDevicesDispatcher::Observer {
  public:
   MediaClient();
   ~MediaClient() override;
+
+  // Initializes and connects to ash.
+  void Init();
+
+  // Tests can provide a mock mojo interface for the ash controller.
+  void InitForTesting(ash::mojom::MediaControllerPtr controller);
 
   // Returns a pointer to the singleton MediaClient, or nullptr if none exists.
   static MediaClient* Get();
@@ -34,19 +43,57 @@ class MediaClient : public ash::mojom::MediaClient,
                        blink::MediaStreamType stream_type,
                        const content::MediaRequestState state) override;
 
+  // BrowserListObserver:
+  void OnBrowserSetLastActive(Browser* browser) override;
+
+  // Enables/disables custom media key handling when |context| is the active
+  // browser. Media keys will be forwarded to |delegate|.
+  void EnableCustomMediaKeyHandler(content::BrowserContext* context,
+                                   ui::MediaKeysListener::Delegate* delegate);
+  void DisableCustomMediaKeyHandler(content::BrowserContext* context,
+                                    ui::MediaKeysListener::Delegate* delegate);
+
+  void FlushForTesting();
+
  private:
+  // Binds this object to its mojo interface and sets it as the ash client.
+  void BindAndSetClient();
+
+  // Sets |is_forcing_media_client_key_handling_| to true if
+  // |GetCurrentMediaKeyDelegate| returns a delegate. This will also mirror the
+  // value of |is_forcing_media_client_key_handling_| to Ash.
+  void UpdateForceMediaClientKeyHandling();
+
+  // Gets the current media key delegate that we should forward media keys to.
+  // This will be the delegate that is associated with the current active
+  // browser. If there is no delegate registered or there is no active browser
+  // then this will return |nullptr|.
+  ui::MediaKeysListener::Delegate* GetCurrentMediaKeyDelegate() const;
+
   // Returns the media capture state for the current user at
   // |user_index|. (Note that this isn't stable, see implementation comment on
   // RequestCaptureState()).
   ash::mojom::MediaCaptureState GetMediaCaptureStateByIndex(int user_index);
 
-  void ToggleMediaSessionPlayPause(content::MediaSession* media_session);
+  // Handles the media key action for the key with |code|. If there is a
+  // |GetCurrentMediaKeyDelegate| then the action will be forwarded to the
+  // delegate. Otherwise, we will forward the action to the extensions API.
+  void HandleMediaAction(ui::KeyboardCode code);
 
   ash::mojom::MediaControllerPtr media_controller_;
 
-  mojo::AssociatedBinding<ash::mojom::MediaClient> binding_;
+  base::flat_map<content::BrowserContext*, ui::MediaKeysListener::Delegate*>
+      media_key_delegates_;
 
-  base::WeakPtrFactory<MediaClient> weak_ptr_factory_;
+  // If true then ash will always forward media keys to |this| instead of trying
+  // to handle them first.
+  bool is_forcing_media_client_key_handling_ = false;
+
+  content::BrowserContext* active_context_ = nullptr;
+
+  mojo::AssociatedBinding<ash::mojom::MediaClient> binding_{this};
+
+  base::WeakPtrFactory<MediaClient> weak_ptr_factory_{this};
 
   DISALLOW_COPY_AND_ASSIGN(MediaClient);
 };
