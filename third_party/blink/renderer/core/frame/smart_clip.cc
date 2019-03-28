fc14103230f57c38edc128181881ed67f2e6e610
Rune Lillesveen <futhark@chromium.org>
2019-03-28 15:30:39 +0000

    Use GetComputedStyle in SmartClip.
    EnsureComputedStyle will force a ComputedStyle in display:none, but the
    bounding rect checked further down will be empty.
    
    Change-Id: Id92ea806ca445879149117c2b3e8f916edeb0949
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1536177
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645326}
    Former-commit-id: c843bc455be01502562291ebfd83f62aa71a1fd9

--- third_party/blink/renderer/core/frame/smart_clip.cc
+++ third_party/blink/renderer/core/frame/smart_clip.cc
@@ -32,6 +32,7 @@
 
 #include "third_party/blink/renderer/core/dom/container_node.h"
 #include "third_party/blink/renderer/core/dom/document.h"
+#include "third_party/blink/renderer/core/dom/node_computed_style.h"
 #include "third_party/blink/renderer/core/dom/node_traversal.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
@@ -233,8 +234,8 @@ String SmartClip::ExtractTextFromNode(Node* node) {
 
   StringBuilder result;
   for (Node& current_node : NodeTraversal::InclusiveDescendantsOf(*node)) {
-    const ComputedStyle* style = current_node.EnsureComputedStyle();
-    if (style && style->UserSelect() == EUserSelect::kNone)
+    const ComputedStyle* style = current_node.GetComputedStyle();
+    if (!style || style->UserSelect() == EUserSelect::kNone)
       continue;
 
     if (Node* node_from_frame = NodeInsideFrame(&current_node))
