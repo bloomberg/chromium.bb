93d3b5461c8f89de66e24895f0d56764be4f18b8
Yi Su <mrsuyi@chromium.org>
2019-03-29 08:50:13 +0000

    Fix crash of PasswordsVC in editing mode.
    Following operations will cause a crash:
    1. Open Settings->Passwords;
    2. Tap "Edit";
    3. Tap on the the "Save Passwords" switch item;
    4. The item will be selected in an invisible way, since
       PasswordsTableVC returns NO for it in |canEditRowAtIndexPath|;
    5. Tap "Delete" button in the bottom toolbar.
    
    This also happens in other Table VCs which support editing mode and
    have non-editable items, like those in Settings
    (e.g. Content-Settings->Block Pop-ups) and History.
    
    Stop the selecting events in |willSelectRowAtIndexPath| for
    non-editable items under editing mode fixes this.
    
    Bug: 943649
    Change-Id: I95e0e167c098686db9a9b3929a7e0f7f9bfcea00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1541118
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645700}
    Former-commit-id: 987661640971eca9912e8f50c6dde5eb69eba76a

--- ios/chrome/browser/ui/table_view/chrome_table_view_controller.mm
+++ ios/chrome/browser/ui/table_view/chrome_table_view_controller.mm
@@ -56,6 +56,44 @@ const CGFloat kTableViewSeparatorInsetWithIcon = 56;
                           appBarStyle:ChromeTableViewControllerStyleNoAppBar];
 }
 
+#pragma mark - UIViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  [self.tableView setBackgroundColor:self.styler.tableViewBackgroundColor];
+  [self.tableView setSeparatorColor:self.styler.cellSeparatorColor];
+  [self.tableView
+      setSeparatorInset:UIEdgeInsetsMake(0, kTableViewSeparatorInsetWithIcon, 0,
+                                         0)];
+
+  // Configure the app bar if needed.
+  if (_appBarViewController) {
+    ConfigureAppBarViewControllerWithCardStyle(self.appBarViewController);
+    self.appBarViewController.headerView.trackingScrollView = self.tableView;
+    // Add the AppBar's views after all other views have been registered.
+    [self addChildViewController:_appBarViewController];
+    CGRect frame = self.appBarViewController.view.frame;
+    frame.origin.x = 0;
+    frame.size.width =
+        self.appBarViewController.parentViewController.view.bounds.size.width;
+    self.appBarViewController.view.frame = frame;
+    [self.view addSubview:self.appBarViewController.view];
+    [self.appBarViewController didMoveToParentViewController:self];
+  }
+}
+
+#pragma mark - UITableViewDelegate
+
+- (NSIndexPath*)tableView:(UITableView*)tableView
+    willSelectRowAtIndexPath:(NSIndexPath*)indexPath {
+  if (self.editing && ![self tableView:tableView
+                          canEditRowAtIndexPath:indexPath]) {
+    return nil;
+  }
+  return indexPath;
+}
+
 #pragma mark - Accessors
 
 - (void)setStyler:(ChromeTableViewStyler*)styler {
@@ -84,31 +122,6 @@ const CGFloat kTableViewSeparatorInsetWithIcon = 56;
   // The safe area insets aren't propagated to the inner scroll view. Manually
   // set the content insets.
   self.emptyView.scrollViewContentInsets = self.view.safeAreaInsets;
-}
-
-- (void)viewDidLoad {
-  [super viewDidLoad];
-
-  [self.tableView setBackgroundColor:self.styler.tableViewBackgroundColor];
-  [self.tableView setSeparatorColor:self.styler.cellSeparatorColor];
-  [self.tableView
-      setSeparatorInset:UIEdgeInsetsMake(0, kTableViewSeparatorInsetWithIcon, 0,
-                                         0)];
-
-  // Configure the app bar if needed.
-  if (_appBarViewController) {
-    ConfigureAppBarViewControllerWithCardStyle(self.appBarViewController);
-    self.appBarViewController.headerView.trackingScrollView = self.tableView;
-    // Add the AppBar's views after all other views have been registered.
-    [self addChildViewController:_appBarViewController];
-    CGRect frame = self.appBarViewController.view.frame;
-    frame.origin.x = 0;
-    frame.size.width =
-        self.appBarViewController.parentViewController.view.bounds.size.width;
-    self.appBarViewController.view.frame = frame;
-    [self.view addSubview:self.appBarViewController.view];
-    [self.appBarViewController didMoveToParentViewController:self];
-  }
 }
 
 - (void)startLoadingIndicatorWithLoadingMessage:(NSString*)loadingMessage {
