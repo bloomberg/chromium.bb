e54cb861fe6cc4b8f47c1b08d20713d9bfa300ee
Peter K. Lee <pkl@chromium.org>
2019-03-29 19:11:55 +0000

    Removes IsUIRefreshPhase1Enabled() flag usage from some egtests
    This flag defaults to permanently ON.
    
    Bug: 884724
    Change-Id: I0640bd463781896a2471c527d2b861397e2bccc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1545181
    Commit-Queue: Peter Lee <pkl@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645887}
    Former-commit-id: eabfdb4f9accce73e6e8621ada3bbde3a7f02703

--- ios/chrome/test/app/tab_test_util.mm
+++ ios/chrome/test/app/tab_test_util.mm
@@ -7,7 +7,6 @@
 #import <Foundation/Foundation.h>
 
 #import "base/mac/foundation_util.h"
-#import "base/test/ios/wait_util.h"
 #import "ios/chrome/app/main_controller_private.h"
 #include "ios/chrome/browser/chrome_url_constants.h"
 #import "ios/chrome/browser/metrics/tab_usage_recorder.h"
@@ -17,7 +16,6 @@
 #import "ios/chrome/browser/ui/commands/browser_commands.h"
 #import "ios/chrome/browser/ui/commands/open_new_tab_command.h"
 #import "ios/chrome/browser/ui/main/tab_switcher.h"
-#include "ios/chrome/browser/ui/util/ui_util.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/web_state_list/web_usage_enabler/web_state_list_web_usage_enabler.h"
 #import "ios/chrome/browser/web_state_list/web_usage_enabler/web_state_list_web_usage_enabler_factory.h"
@@ -26,8 +24,6 @@
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
-
-using base::test::ios::WaitUntilConditionOrTimeout;
 
 namespace chrome_test_util {
 
@@ -195,14 +191,6 @@ BOOL CloseAllIncognitoTabs() {
       GetMainController().interfaceProvider.incognitoInterface.tabModel;
   DCHECK(tabModel);
   [tabModel closeAllTabs];
-  if (!IsIPadIdiom() && !IsUIRefreshPhase1Enabled()) {
-    // If the OTR BVC is active, wait until it isn't (since all of the
-    // tabs are now closed)
-    return WaitUntilConditionOrTimeout(
-        base::test::ios::kWaitForUIElementTimeout, ^{
-          return !IsIncognitoMode();
-        });
-  }
   return YES;
 }
 
