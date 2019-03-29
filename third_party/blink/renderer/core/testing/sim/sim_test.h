abefd24ea175ddbac43198e3b2424e7c883de7b6
David Bokan <bokan@chromium.org>
2019-03-29 15:15:25 +0000

    Rename SimTest::Page to prevent shadowing type
    This makes it impossible to use Page as a type in any SimTests.
    
    Change-Id: Ib108750afa13d94b17dcd83f883633951b2bab42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1545172
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645778}
    Former-commit-id: dccb756b4a84e6de8c5381fb5d7421a7c5f7675f

--- third_party/blink/renderer/core/testing/sim/sim_test.h
+++ third_party/blink/renderer/core/testing/sim/sim_test.h
@@ -32,7 +32,7 @@ class SimTest : public testing::Test {
   // web runtime features.
   // These methods should be accessed inside test body after a call to SetUp.
   LocalDOMWindow& Window();
-  SimPage& Page();
+  SimPage& GetPage();
   Document& GetDocument();
   WebViewImpl& WebView();
   WebLocalFrameImpl& MainFrame();
