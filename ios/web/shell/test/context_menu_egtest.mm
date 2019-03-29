4c003ea0cd6720b7e60e2e3c1a13d3db627791cb
Yi Su <mrsuyi@chromium.org>
2019-03-29 09:31:33 +0000

    Remove ios-http_server from ios_web_shell_egtests.
    This CLs replaces the deprecated ios/web/public/test/http_server with
    net/test/embedded_test_server in dep list of ios_web_shell_egtests.
    
    Bug: 930859
    Change-Id: I827a951bad5a57f13965162a9f27c62470446d88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1543633
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645712}
    Former-commit-id: a5258fa30fd9070c26cd1398d933719745da69ea

--- ios/web/shell/test/context_menu_egtest.mm
+++ ios/web/shell/test/context_menu_egtest.mm
@@ -10,8 +10,6 @@
 #import "base/ios/block_types.h"
 #import "ios/testing/earl_grey/matchers.h"
 #include "ios/web/public/test/element_selector.h"
-#import "ios/web/public/test/http_server/http_server.h"
-#include "ios/web/public/test/http_server/http_server_util.h"
 #import "ios/web/public/test/web_view_interaction_test_util.h"
 #import "ios/web/shell/test/app/web_shell_test_util.h"
 #include "ios/web/shell/test/app/web_view_interaction_test_util.h"
@@ -20,6 +18,7 @@
 #import "ios/web/shell/test/earl_grey/shell_matchers.h"
 #import "ios/web/shell/test/earl_grey/shell_matchers_shorthand.h"
 #import "ios/web/shell/test/earl_grey/web_shell_test_case.h"
+#include "net/test/embedded_test_server/embedded_test_server.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -28,34 +27,38 @@
 using testing::ButtonWithAccessibilityLabel;
 using testing::ElementToDismissAlert;
 
+namespace {
+const char kHtmlFile[] =
+    "/ios/testing/data/http_server_files/context_menu.html";
+}
+
 // Context menu test cases for the web shell.
-@interface ContextMenuTestCase : WebShellTestCase
+@interface ContextMenuTestCase : WebShellTestCase {
+  net::EmbeddedTestServer _server;
+}
+
 @end
 
 @implementation ContextMenuTestCase
 
+- (void)setUp {
+  [super setUp];
+
+  _server.ServeFilesFromSourceDirectory(base::FilePath(FILE_PATH_LITERAL(".")));
+  GREYAssert(_server.Start(), @"EmbeddedTestServer failed to start.");
+}
+
 // Tests context menu appears on a regular link.
 - (void)testContextMenu {
-  // Create map of canned responses and set up the test HTML server.
-  std::map<GURL, std::string> responses;
-  GURL initialURL = web::test::HttpServer::MakeUrl("http://contextMenuOpen");
-  GURL destinationURL = web::test::HttpServer::MakeUrl("http://destination");
-  // The initial page contains a link to the destination URL.
-  std::string linkID = "link";
-  std::string linkText = "link for context menu";
-  responses[initialURL] =
-      "<body>"
-      "<a href='" +
-      destinationURL.spec() + "' id='" + linkID + "'>" + linkText +
-      "</a>"
-      "</span></body>";
+  const char linkID[] = "normal-link";
+  const char linkText[] = "normal-link-text";
+  const GURL pageURL = _server.GetURL(kHtmlFile);
 
-  web::test::SetUpSimpleHttpServer(responses);
-  bool success = [ShellEarlGrey loadURL:initialURL];
+  bool success = [ShellEarlGrey loadURL:pageURL];
   GREYAssert(success, @"Page did not complete loading.");
+
   success = [ShellEarlGrey waitForWebViewContainingText:linkText];
-  GREYAssert(success, @"Failed waiting for web view containing %s",
-             linkText.c_str());
+  GREYAssert(success, @"Failed waiting for web view containing '%s'", linkText);
 
   [[EarlGrey selectElementWithMatcher:web::WebView()]
       performAction:web::LongPressElementForContextMenu(
@@ -78,29 +81,14 @@ using testing::ElementToDismissAlert;
 // Tests context menu on element that has WebkitTouchCallout set to none from an
 // ancestor and overridden.
 - (void)testContextMenuWebkitTouchCalloutOverride {
-  // Create map of canned responses and set up the test HTML server.
-  std::map<GURL, std::string> responses;
-  GURL initialURL =
-      web::test::HttpServer::MakeUrl("http://contextMenuDisabledByWebkit");
-  GURL destinationURL = web::test::HttpServer::MakeUrl("http://destination");
-  // The initial page contains a link to the destination URL that has an
-  // ancestor that disables the context menu via -webkit-touch-callout.
-  std::string linkID = "link";
-  std::string linkText = "override no-callout link";
-  responses[initialURL] =
-      "<body style='-webkit-touch-callout: none'>"
-      "<a href='" +
-      destinationURL.spec() + "' style='-webkit-touch-callout: default' id='" +
-      linkID + "'>" + linkText +
-      "</a>"
-      "</body>";
+  const char linkID[] = "no-webkit-link";
+  const char linkText[] = "no-webkit-link-text";
+  const GURL pageURL = _server.GetURL(kHtmlFile);
 
-  web::test::SetUpSimpleHttpServer(responses);
-  bool success = [ShellEarlGrey loadURL:initialURL];
+  bool success = [ShellEarlGrey loadURL:pageURL];
   GREYAssert(success, @"Page did not complete loading.");
   success = [ShellEarlGrey waitForWebViewContainingText:linkText];
-  GREYAssert(success, @"Failed waiting for web view containing %s",
-             linkText.c_str());
+  GREYAssert(success, @"Failed waiting for web view containing '%s'", linkText);
 
   [[EarlGrey selectElementWithMatcher:web::WebView()]
       performAction:web::LongPressElementForContextMenu(
