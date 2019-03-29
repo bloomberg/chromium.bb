833ce20a9ef78fb754083353d47aae63fb025720
Chris Mumford <cmumford@google.com>
2019-03-29 22:29:55 +0000

    Added temp UMA logging for renderer hangs.
    Adding temporary UMA statistics to determine what the
    browser was waiting for when it determined that the
    renderer was hung.
    
    This is similar to logging added in r406039 and removed in
    r455764, but the hang monitor is now different enough that
    the UMA values cannot be reused.
    
    Bug: 947559, 938647
    Change-Id: Ia4d44e09ec653f23fcc9d8ea0fcde05a887d21da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546250
    Commit-Queue: Chris Mumford <cmumford@google.com>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645979}
    Former-commit-id: ab20718c657194abb5e1a53257267e13c8b91714

--- components/metrics/stability_metrics_helper.cc
+++ components/metrics/stability_metrics_helper.cc
@@ -322,7 +322,8 @@ void StabilityMetricsHelper::IncrementLongPrefsValue(const char* path) {
   local_state_->SetInt64(path, value + 1);
 }
 
-void StabilityMetricsHelper::LogRendererHang() {
+void StabilityMetricsHelper::LogRendererHang(RendererHangCause hang_cause) {
+  UMA_HISTOGRAM_ENUMERATION("ChildProcess.HungRendererCause", hang_cause);
   IncrementPrefValue(prefs::kStabilityRendererHangCount);
 }
 
