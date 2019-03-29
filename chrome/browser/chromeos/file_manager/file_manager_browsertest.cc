43f290905517866790b61e5fef61b274319de8c3
Luciano Pacheco <lucmult@chromium.org>
2019-03-29 01:39:37 +0000

    [Files app] Fix condition to disable test  on ASAN and DEBUG
    Bug: 944697
    Change-Id: I4a8454f7faf4954a34f3112ae72a967686d05a4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1545330
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645626}
    Former-commit-id: b7313eacbe8281a0d09ec7f558db49669af1e5ce

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -571,7 +571,7 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("dirCreateWithKeyboard").EnableMyFilesVolume(),
         TestCase("dirCreateWithoutChangingCurrent").EnableMyFilesVolume(),
         TestCase("dirCreateWithoutChangingCurrent"),
-#if defined(ADDRESS_SANITIZER) || defined(DEBUG)
+#if !(defined(ADDRESS_SANITIZER) || defined(DEBUG))
         // Zip tests times out too often on ASAN and DEBUG.
         ZipCase("dirContextMenuZip"),
 #endif
