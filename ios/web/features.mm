35d4a1a594856adf30ef5bbea18bfc15a63c9509
Eugene But <eugenebut@google.com>
2019-03-29 19:30:22 +0000

    Enable IgnoresViewportScaleLimits flag by default.
    Always allow scaling of the web page, regardless of author intent. This
    will match Safari and Firefox.
    
    Bug: 946146, 719573, 836221, 877642
    Change-Id: I6298df34b81ca96fa65c55820f02ab3d65b1f05f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546358
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#645894}
    Former-commit-id: 2ca0d69ca5d8dba347454de45a59bf0905814fbf

--- ios/web/features.mm
+++ ios/web/features.mm
@@ -17,7 +17,7 @@ bool StorePendingItemInContext() {
 }
 
 const base::Feature kIgnoresViewportScaleLimits{
-    "IgnoresViewportScaleLimits", base::FEATURE_DISABLED_BY_DEFAULT};
+    "IgnoresViewportScaleLimits", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kSlimNavigationManager{"SlimNavigationManager",
                                            base::FEATURE_DISABLED_BY_DEFAULT};
