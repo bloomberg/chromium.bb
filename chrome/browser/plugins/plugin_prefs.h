a0b6172f8b3a0343b588e0ee1718b81954b7a428
Maksim Ivanov <emaxx@chromium.org>
2019-04-01 23:58:11 +0000

    Fix includes in PluginPrefs
    Remove unused includes and add missing ones.
    
    BUG=chromium:948309
    TEST=code compiles
    
    Change-Id: Idc271c561e0569ab4d87dd5b657bac1c5b460d49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1547798
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646569}
    Former-commit-id: ab376d502995fadf0dabd0ff02e50e5d2a53fb7e

--- chrome/browser/plugins/plugin_prefs.h
+++ chrome/browser/plugins/plugin_prefs.h
@@ -5,15 +5,12 @@
 #ifndef CHROME_BROWSER_PLUGINS_PLUGIN_PREFS_H_
 #define CHROME_BROWSER_PLUGINS_PLUGIN_PREFS_H_
 
-#include <map>
-#include <set>
-#include <vector>
+#include <string>
 
-#include "base/files/file_path.h"
 #include "base/macros.h"
+#include "base/memory/scoped_refptr.h"
+#include "base/strings/string16.h"
 #include "base/synchronization/lock.h"
-#include "chrome/browser/plugins/plugin_finder.h"
-#include "chrome/common/chrome_content_client.h"
 #include "components/keyed_service/core/refcounted_keyed_service.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_service.h"
