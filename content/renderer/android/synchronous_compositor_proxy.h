25264c9c335048a3f40a6cadc135d752681eb323
Alex Ilin <alexilin@chromium.org>
2019-04-01 17:35:51 +0000

    Convert SynchronousCompositor to the new shared memory API
    This is a refactoring CL, no behavioral change is expected.
    
    Deprecated base::SharedMemory is used in SynchronousCompositorProxy and
    SynchronousCompositorHost. This CL replaces usages of base::SharedMemory with
    base::WritableSharedMemoryRegion.
    
    Bug: 795291
    Change-Id: If9b5256aed77d2d36ee4c69163a56b13c46e98e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1545917
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646384}
    Former-commit-id: 28c8de1c8f7b8a5f6437428654dfd95c42fb9d1c

--- content/renderer/android/synchronous_compositor_proxy.h
+++ content/renderer/android/synchronous_compositor_proxy.h
@@ -10,6 +10,7 @@
 
 #include "base/callback.h"
 #include "base/macros.h"
+#include "base/memory/writable_shared_memory_region.h"
 #include "base/optional.h"
 #include "components/viz/common/presentation_feedback_map.h"
 #include "content/common/input/synchronous_compositor.mojom.h"
@@ -30,7 +31,6 @@ class SynchronousLayerTreeFrameSink;
 struct SyncCompositorCommonRendererParams;
 struct SyncCompositorDemandDrawHwParams;
 struct SyncCompositorDemandDrawSwParams;
-struct SyncCompositorSetSharedMemoryParams;
 
 class SynchronousCompositorProxy : public ui::SynchronousInputHandler,
                                    public SynchronousLayerTreeFrameSinkClient,
@@ -75,7 +75,7 @@ class SynchronousCompositorProxy : public ui::SynchronousInputHandler,
       const SyncCompositorDemandDrawHwParams& draw_params) final;
   void DemandDrawHw(const SyncCompositorDemandDrawHwParams& params,
                     DemandDrawHwCallback callback) final;
-  void SetSharedMemory(const SyncCompositorSetSharedMemoryParams& params,
+  void SetSharedMemory(base::WritableSharedMemoryRegion shm_region,
                        SetSharedMemoryCallback callback) final;
   void DemandDrawSw(const SyncCompositorDemandDrawSwParams& params,
                     DemandDrawSwCallback callback) final;
