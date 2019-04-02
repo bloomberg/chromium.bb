0bf7d08ace4ccf70069f15a9768be8da514bbe1f
Jeevan Shikaram <jshikaram@chromium.org>
2019-04-02 02:46:08 +0000

    [App Management] Set the favicon of App Management to be a gear.
    The favicon for the App Management page was changed from the default favicon
    to the favicon of the settings page.
    
    Bug: 947895, 906508
    Change-Id: Ibfec0f725024430ab883b52aa2231d810a326c2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546741
    Commit-Queue: Jeevan Shikaram <jshikaram@chromium.org>
    Auto-Submit: Jeevan Shikaram <jshikaram@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646653}
    Former-commit-id: 3f914a6853f33b745a47be8b9dd31080dfbfd7fe

--- chrome/common/webui_url_constants.h
+++ chrome/common/webui_url_constants.h
@@ -29,6 +29,7 @@ extern const char kChromeUIAccessibilityHost[];
 extern const char kChromeUIAppIconHost[];
 extern const char kChromeUIAppIconURL[];
 extern const char kChromeUIAppLauncherPageHost[];
+extern const char kChromeUIAppManagementHost[];
 extern const char kChromeUIAppsURL[];
 extern const char kChromeUIAutofillInternalsHost[];
 extern const char kChromeUIBluetoothInternalsHost[];
