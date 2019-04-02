259ebc00ababf1d79aa0e2b8116209e5bfb21a08
Lutz Justen <ljusten@chromium.org>
2019-04-02 08:35:56 +0000

    Reland "Fix ExistingUserControllerActiveDirectoryTest tests"
    Original CL:1482454
    Revert   CL:1546637
    
    The original CL was broken since WaitForKerberosFilesChanged() assumed
    that no RunLoop::Run() call is made after the Kerberos file writer task
    has been posted. However, the PolicyChangeTriggersFileUpdate test did
    exactly that in ApplyPolicyAndWaitFilesChanged -> UpdateProviderPolicy.
    
    The fix is to set the callback before the writer task is posed. This
    isn't always possible since the calls initialize the profile, but in
    these cases RunLoop::Run() isn't called.
    
    Also got rid of the fancy, yet pointless Lambda-Bind-Closure-Wrapper :-)
    
    BUG=chromium:865206
    TEST=ExistingUserControllerActiveDirectoryTest.* tests pass with ASAN.
    
    Change-Id: Ie9118550ba88eebb7ffbf77419ff5cf705aaf33c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1547837
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Lutz Justen <ljusten@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646711}
    Former-commit-id: a2de4422362e216c0bf9e8ebe937dc2942ed5e5b

--- chrome/browser/chromeos/authpolicy/kerberos_files_handler.h
+++ chrome/browser/chromeos/authpolicy/kerberos_files_handler.h
@@ -30,14 +30,23 @@ class KerberosFilesHandler {
   void SetFiles(base::Optional<std::string> krb5cc,
                 base::Optional<std::string> krb5conf);
 
+  // Sets a callback for when disk IO task posted by SetFiles has finished.
+  void SetFilesChangedForTesting(base::OnceClosure callback);
+
  private:
   // Called whenever prefs::kDisableAuthNegotiateCnameLookup is changed.
   void OnDisabledAuthNegotiateCnameLookupChanged();
+
+  // Forwards to |files_changed_for_testing_| if set.
+  void OnFilesChanged();
 
   PrefMember<bool> negotiate_disable_cname_lookup_;
 
   // Triggers a fetch of Kerberos files. Called when the watched pref changes.
   base::RepeatingClosure get_kerberos_files_;
+
+  // Called when disk IO queued by SetFiles has finished.
+  base::OnceClosure files_changed_for_testing_;
 
   base::WeakPtrFactory<KerberosFilesHandler> weak_factory_{this};
   DISALLOW_COPY_AND_ASSIGN(KerberosFilesHandler);
