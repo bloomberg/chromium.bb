a5911bb48d86ffab28e462a49dfe296430e2bc13
Esmael El-Moslimany <aee@chromium.org>
2019-04-02 20:55:12 +0000

    WebUI: FocusRowBehavior, focus on equivalent control only if up/down was pressed
    Bug: 901771
    Change-Id: Iea4fa9e711024f7f78ac363ed652b350e953e17a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1544978
    Reviewed-by: Scott Chen <scottchen@chromium.org>
    Commit-Queue: Esmael El-Moslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646951}
    Former-commit-id: dc931ca8d9dbb64840a7b4a16de96a4a9716b875

--- ui/webui/resources/js/cr/ui/focus_row_behavior.js
+++ ui/webui/resources/js/cr/ui/focus_row_behavior.js
@@ -3,6 +3,9 @@
 // found in the LICENSE file.
 
 cr.define('cr.ui', function() {
+  /** @type {string} */
+  let lastKeyDownKey = '';
+
   /** @implements {cr.ui.FocusRowDelegate} */
   class FocusRowBehaviorDelegate {
     /**
@@ -136,6 +139,7 @@ cr.define('cr.ui', function() {
         this.listen(this, 'dom-change', 'addItems_');
         this.listen(this, 'mousedown', 'onMouseDown_');
         this.listen(this, 'blur', 'onBlur_');
+        this.listen(this, 'keydown', 'onKeydown_');
       });
     },
 
@@ -145,6 +149,7 @@ cr.define('cr.ui', function() {
       this.unlisten(this, 'dom-change', 'addItems_');
       this.unlisten(this, 'mousedown', 'onMouseDown_');
       this.unlisten(this, 'blur', 'onBlur_');
+      this.unlisten(this, 'keydown', 'onKeydown_');
       this.removeObservers_();
       if (this.firstControl_) {
         this.unlisten(this.firstControl_, 'keydown', 'onFirstControlKeydown_');
@@ -275,11 +280,11 @@ cr.define('cr.ui', function() {
       const restoreFocusToFirst =
           this.listBlurred && e.composedPath()[0] === this;
 
-      if (this.lastFocused && !restoreFocusToFirst) {
-        this.row_.getEquivalentElement(this.lastFocused).focus();
-      } else {
+      if (!this.lastFocused || restoreFocusToFirst) {
         const firstFocusable = assert(this.firstControl_);
         firstFocusable.focus();
+      } else if (lastKeyDownKey == 'ArrowDown' || lastKeyDownKey == 'ArrowUp') {
+        this.row_.getEquivalentElement(this.lastFocused).focus();
       }
       this.listBlurred = false;
     },
@@ -289,6 +294,11 @@ cr.define('cr.ui', function() {
       if (e.shiftKey && e.key === 'Tab') {
         this.focus();
       }
+    },
+
+    /** @param {!KeyboardEvent} e */
+    onKeydown_: function(e) {
+      lastKeyDownKey = e.key;
     },
 
     /** @private */
