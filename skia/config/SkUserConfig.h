6a8658704f1d66ba77df1f96534f835d8a11cb41
Herb Derby <herb@google.com>
2019-04-02 23:00:24 +0000

    Text color handling flag
    SK_ENABLE_LEGACY_TEXT_COLOR
    
    Move from using 8-bit color components to 3-bit color
    components for better caching with changing colors.
    
    BUG=929858
    
    Change-Id: I271fe04830dee1f3a632e6eab7d2e88e0125650a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1549210
    Reviewed-by: Ben Wagner <bungeman@google.com>
    Commit-Queue: Herb Derby <herb@google.com>
    Cr-Commit-Position: refs/heads/master@{#647004}
    Former-commit-id: 2fc9cca177ffd954954d96708c72aed5958eb94a

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -165,6 +165,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_AAA_CHOICE
 #endif
 
+#ifndef SK_ENABLE_LEGACY_TEXT_COLOR
+#define SK_ENABLE_LEGACY_TEXT_COLOR
+#endif
+
 // We're turning this off indefinitely,
 // until we can figure out some fundamental problems with its approach.
 //
