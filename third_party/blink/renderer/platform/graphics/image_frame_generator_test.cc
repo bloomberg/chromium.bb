e62c6816dc586c9ad4ad50cdd9df28c076441005
Matthew Cary <mattcary@chromium.org>
2019-04-02 14:34:55 +0000

    Disable ImageFrameGeneratorTest.incompleteDecodeBecomesCompleteMultiThreaded
    On android and linux due to flakes.
    
    TBR: chrishtr@chromium.org
    Bug: 948641
    Change-Id: I12a6c4d5a27509a255b2bcc0d470fd21959271b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1547985
    Reviewed-by: Matthew Cary <mattcary@chromium.org>
    Commit-Queue: Matthew Cary <mattcary@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646799}
    Former-commit-id: 7e0d100ca255d27dedaf707a0e607d7153b05b5d

--- third_party/blink/renderer/platform/graphics/image_frame_generator_test.cc
+++ third_party/blink/renderer/platform/graphics/image_frame_generator_test.cc
@@ -27,6 +27,7 @@
 
 #include <memory>
 #include "base/location.h"
+#include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/renderer/platform/cross_thread_functional.h"
@@ -243,7 +244,16 @@ static void DecodeThreadMain(ImageFrameGenerator* generator,
                             cc::PaintImage::kDefaultGeneratorClientId);
 }
 
-TEST_F(ImageFrameGeneratorTest, incompleteDecodeBecomesCompleteMultiThreaded) {
+#if defined(OS_ANDROID) || defined(OS_LINUX)
+// TODO(crbug.com/948641)
+#define MAYBE_incompleteDecodeBecomesCompleteMultiThreaded \
+  DISABLED_incompleteDecodeBecomesCompleteMultiThreaded
+#else
+#define MAYBE_incompleteDecodeBecomesCompleteMultiThreaded \
+  incompleteDecodeBecomesCompleteMultiThreaded
+#endif  // defined(OS_ANDROID) || defined(OS_LINUX)
+TEST_F(ImageFrameGeneratorTest,
+       MAYBE_incompleteDecodeBecomesCompleteMultiThreaded) {
   SetFrameStatus(ImageFrame::kFramePartial);
 
   char buffer[100 * 100 * 4];
