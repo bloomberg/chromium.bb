2459c6d1e7561262cf34dea3fdf7ed4a6a9c0e87
Rakib M. Hasan <rmhasan@google.com>
2019-04-02 00:37:51 +0000

    Moves screenshot_sync tests onto the new test expectations format
    Bug: chromium:698902
    Change-Id: Ic2963f13439db4176e9650e6f111e42dbcc753d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1548377
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Rakib Hasan <rmhasan@google.com>
    Cr-Commit-Position: refs/heads/master@{#646589}
    Former-commit-id: bd43a439fcd8c87d6bb4216d2174f62b1775b016

--- content/test/gpu/gpu_tests/screenshot_sync_integration_test.py
+++ content/test/gpu/gpu_tests/screenshot_sync_integration_test.py
@@ -10,7 +10,6 @@ import sys
 from gpu_tests import color_profile_manager
 from gpu_tests import gpu_integration_test
 from gpu_tests import path_util
-from gpu_tests import screenshot_sync_expectations
 
 from telemetry.util import image_util
 from telemetry.util import rgba_color
@@ -74,7 +73,7 @@ class ScreenshotSyncIntegrationTest(gpu_integration_test.GpuIntegrationTest):
 
   @classmethod
   def _CreateExpectations(cls):
-    return screenshot_sync_expectations.ScreenshotSyncExpectations()
+    raise NotImplementedError
 
   @classmethod
   def GenerateGpuTests(cls, options):
@@ -144,6 +143,13 @@ class ScreenshotSyncIntegrationTest(gpu_integration_test.GpuIntegrationTest):
     repetitions = 20
     for _ in range(0, repetitions):
       self._CheckScreenshot()
+
+  @classmethod
+  def ExpectationsFiles(cls):
+    return [
+        os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                     'test_expectations',
+                     'screenshot_sync_expectations.txt')]
 
 def load_tests(loader, tests, pattern):
   del loader, tests, pattern  # Unused.
