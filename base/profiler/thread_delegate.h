a2472ba4c1c87949806f6e185c8124f73bb35dfa
Mike Wittman <wittman@chromium.org>
2019-04-02 13:46:40 +0000

    [Sampling profiler] Reland "Move context frame recording outside the native unwinders"
    Allows the native unwinders to be used to unwind from arbitrary frames,
    not just from the context frame. This is required to support restarting
    native frame unwinding after v8 unwinding.
    
    This is a reland of c360f0116e09b841aec4c8d8dd34e458f5431736 [formerly a92a56cb6ed4b7f7bb4fb19d94e00bf3b55fa417] with a fix
    to avoid calling GetModuleHandleEx() on invalid addresses, which was
    causing flaky test failures.
    
    Bug: 909957, 948302
    Change-Id: If4d246d74b68d509c3c7fac50a03d05588620f1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1540285
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#646333}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1548601
    Auto-Submit: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#646785}
    Former-commit-id: 8967586b85fb58ee3001f278894ae1e0614d7b44

--- base/profiler/thread_delegate.h
+++ base/profiler/thread_delegate.h
@@ -68,7 +68,9 @@ class BASE_EXPORT ThreadDelegate {
       RegisterContext* thread_context) = 0;
 
   // Walks the native frames on the stack pointed to by the stack pointer in
-  // |thread_context|, appending the frames to |stack|.
+  // |thread_context|, appending the frames to |stack|. When invoked
+  // stack->back() contains the frame corresponding to the state in
+  // |thread_context|.
   // TODO(wittman): Move the unwinding support into a separate UnwindDelegate.
   virtual UnwindResult WalkNativeFrames(
       RegisterContext* thread_context,
