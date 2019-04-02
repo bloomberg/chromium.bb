c68b8e30944dcff50e98b17a1a8c4112f0ac9410
Sinan Sahin <sinansahin@google.com>
2019-04-02 18:57:55 +0000

    Implement the Themes setting
    Add a "Themes" settings screen that allows users
    to select between system default, light and dark themes.
    
    Bug: 942771
    Change-Id: I76d0dd19dd61cac7bd579d5186a2e5d25305f3b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1541671
    Reviewed-by: Becky Zhou <huayinz@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/master@{#646902}
    Former-commit-id: 91dd25715b334f7ad66a33c9fa1fcc8e4df0d7ee

--- chrome/android/java/src/org/chromium/chrome/browser/widget/RadioButtonWithDescription.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/RadioButtonWithDescription.java
@@ -54,6 +54,10 @@ public class RadioButtonWithDescription extends RelativeLayout implements OnClic
 
         if (attrs != null) applyAttributes(attrs);
 
+        final int padding =
+                getResources().getDimensionPixelSize(R.dimen.radio_button_with_description_padding);
+        setPaddingRelative(padding, padding, padding, padding);
+
         // We want RadioButtonWithDescription to handle the clicks itself.
         setOnClickListener(this);
         // Make it focusable for navigation via key events (tab/up/down keys)
@@ -67,6 +71,15 @@ public class RadioButtonWithDescription extends RelativeLayout implements OnClic
 
         String titleText = a.getString(R.styleable.RadioButtonWithDescription_titleText);
         if (titleText != null) mTitle.setText(titleText);
+
+        String descriptionText =
+                a.getString(R.styleable.RadioButtonWithDescription_descriptionText);
+        if (descriptionText != null) {
+            mDescription.setText(descriptionText);
+            mDescription.setVisibility(View.VISIBLE);
+        } else {
+            ((LayoutParams) mTitle.getLayoutParams()).addRule(RelativeLayout.CENTER_VERTICAL);
+        }
 
         a.recycle();
     }
@@ -98,7 +111,14 @@ public class RadioButtonWithDescription extends RelativeLayout implements OnClic
      */
     public void setDescriptionText(CharSequence text) {
         mDescription.setText(text);
-        mDescription.setVisibility(TextUtils.isEmpty(text) ? View.GONE : View.VISIBLE);
+
+        if (TextUtils.isEmpty(text)) {
+            ((LayoutParams) mTitle.getLayoutParams()).addRule(RelativeLayout.CENTER_VERTICAL);
+            mDescription.setVisibility(View.GONE);
+        } else {
+            ((LayoutParams) mTitle.getLayoutParams()).removeRule(RelativeLayout.CENTER_VERTICAL);
+            mDescription.setVisibility(View.VISIBLE);
+        }
     }
 
     /**
