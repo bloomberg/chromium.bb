e5c7319d8f303ce10b4b178b3ec6036360deeab2
Robbie Gibson <rkgibson@google.com>
2019-04-03 10:06:29 +0000

    🦁Add a fake suggestion for each possible answer type to Showcase
    To make sure each suggestion looks correct in the new design, this CL
    creates a new fake suggestion for each possible answer type. It also
    moves the creation code to FakeAutocompleteSuggestion in case this
    needs to be used in other places.
    
    Bug: 945317
    Change-Id: I6718857edfce5593469e86a497d8d871075b5ad9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1545916
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#647207}
    Former-commit-id: d3876fcccdd1b709fa188ae4792b2bd053e486ae

--- ios/showcase/omnibox_popup/sc_omnibox_popup_coordinator.mm
+++ ios/showcase/omnibox_popup/sc_omnibox_popup_coordinator.mm
@@ -4,7 +4,7 @@
 
 #import "ios/showcase/omnibox_popup/sc_omnibox_popup_coordinator.h"
 
-#import "ios/chrome/browser/ui/omnibox/popup/omnibox_popup_legacy_view_controller.h"
+#import "ios/chrome/browser/ui/omnibox/popup/omnibox_popup_view_controller.h"
 #import "ios/showcase/common/coordinator.h"
 #import "ios/showcase/common/protocol_alerter.h"
 #import "ios/showcase/omnibox_popup/sc_omnibox_popup_container_view_controller.h"
@@ -36,7 +36,7 @@
   // Ignore didScroll because it's fired all the time.
   [self.alerter ignoreSelector:@selector(autocompleteResultConsumerDidScroll:)];
 
-  self.popupViewController = [[OmniboxPopupLegacyViewController alloc] init];
+  self.popupViewController = [[OmniboxPopupViewController alloc] init];
   self.popupViewController.delegate =
       static_cast<id<AutocompleteResultConsumerDelegate>>(self.alerter);
 
--- ios/showcase/omnibox_popup/sc_omnibox_popup_mediator.mm
+++ ios/showcase/omnibox_popup/sc_omnibox_popup_mediator.mm
@@ -12,121 +12,6 @@
 #error "This file requires ARC support."
 #endif
 
-namespace {
-// Spacer attributed string for dividing parts of an autocomplete suggestion's
-// text and detail text.
-NSAttributedString* spacer() {
-  return [[NSAttributedString alloc] initWithString:@"  "];
-}
-
-// Detail text for an autocomplete suggestion representing weather
-NSAttributedString* weatherDetailText() {
-  NSAttributedString* number = [[NSAttributedString alloc]
-      initWithString:@"18"
-          attributes:@{
-            NSFontAttributeName : [UIFont systemFontOfSize:24],
-            NSForegroundColorAttributeName : [UIColor grayColor],
-          }];
-  NSAttributedString* degreeSymbol = [[NSAttributedString alloc]
-      initWithString:@"°C"
-          attributes:@{
-            NSFontAttributeName : [UIFont systemFontOfSize:12],
-            NSBaselineOffsetAttributeName : @10.0f,
-            NSForegroundColorAttributeName : [UIColor grayColor],
-          }];
-  NSAttributedString* date = [[NSAttributedString alloc]
-      initWithString:@"ven."
-          attributes:@{
-            NSFontAttributeName : [UIFont systemFontOfSize:12],
-            NSForegroundColorAttributeName : [UIColor grayColor],
-          }];
-
-  NSMutableAttributedString* answer =
-      [[NSMutableAttributedString alloc] initWithAttributedString:number];
-  [answer appendAttributedString:degreeSymbol];
-  [answer appendAttributedString:spacer()];
-  [answer appendAttributedString:date];
-
-  return [answer copy];
-}
-
-// Main text for an autocomplete suggestion representing stock price
-NSAttributedString* stockText() {
-  NSAttributedString* search = [[NSAttributedString alloc]
-      initWithString:@"goog stock"
-          attributes:@{
-            NSFontAttributeName : [UIFont systemFontOfSize:16],
-          }];
-  NSAttributedString* priceSource = [[NSAttributedString alloc]
-      initWithString:@"GOOG (NASDAQ), 13:18 UTC−4"
-          attributes:@{
-            NSFontAttributeName : [UIFont systemFontOfSize:12],
-            NSForegroundColorAttributeName : [UIColor grayColor],
-          }];
-  NSMutableAttributedString* answer =
-      [[NSMutableAttributedString alloc] initWithAttributedString:search];
-  [answer appendAttributedString:spacer()];
-  [answer appendAttributedString:priceSource];
-  return [answer copy];
-}
-
-// Detail text for an autocomplete suggestion representing stock price
-NSAttributedString* stockDetailText() {
-  NSAttributedString* price = [[NSAttributedString alloc]
-      initWithString:@"1 209,29"
-          attributes:@{
-            NSFontAttributeName : [UIFont systemFontOfSize:24],
-            NSForegroundColorAttributeName : [UIColor grayColor],
-          }];
-  NSAttributedString* priceChange = [[NSAttributedString alloc]
-      initWithString:@"-22,25 (-1,81%)"
-          attributes:@{
-            NSFontAttributeName : [UIFont systemFontOfSize:16],
-            NSForegroundColorAttributeName : [UIColor colorWithRed:197 / 255.0
-                                                             green:57 / 255.0
-                                                              blue:41 / 255.0
-                                                             alpha:1.0],
-          }];
-  NSMutableAttributedString* answer =
-      [[NSMutableAttributedString alloc] initWithAttributedString:price];
-  [answer appendAttributedString:spacer()];
-  [answer appendAttributedString:priceChange];
-  return [answer copy];
-}
-
-// Main text for an autocomplete suggestion representing a word definition
-NSAttributedString* definitionText() {
-  NSAttributedString* searchText = [[NSAttributedString alloc]
-      initWithString:@"define government"
-          attributes:@{
-            NSFontAttributeName : [UIFont systemFontOfSize:16],
-          }];
-  NSAttributedString* pronunciation = [[NSAttributedString alloc]
-      initWithString:@"• /ˈɡʌv(ə)nˌm(ə)nt/"
-          attributes:@{
-            NSFontAttributeName : [UIFont systemFontOfSize:14],
-            NSForegroundColorAttributeName : [UIColor grayColor],
-          }];
-  NSMutableAttributedString* answer =
-      [[NSMutableAttributedString alloc] initWithAttributedString:searchText];
-  [answer appendAttributedString:spacer()];
-  [answer appendAttributedString:pronunciation];
-  return [answer copy];
-}
-
-// Detail text for an autocomplete suggestion representing a word definition
-NSAttributedString* definitionDetailText() {
-  return [[NSAttributedString alloc]
-      initWithString:@"the group of people with the authority to govern a "
-                     @"country or state; a particular ministry in office. "
-                     @"Let's expand this definition to get to three lines also."
-          attributes:@{
-            NSFontAttributeName : [UIFont systemFontOfSize:14],
-            NSForegroundColorAttributeName : [UIColor grayColor],
-          }];
-}
-}  // namespace
-
 @interface SCOmniboxPopupMediator ()
 
 @property(nonatomic, readonly, weak) id<AutocompleteResultConsumer> consumer;
@@ -146,80 +31,23 @@ NSAttributedString* definitionDetailText() {
 // Creates many fake suggestions and passes them along to the
 // AutocompleteResultConsumer.
 - (void)updateMatches {
-  FakeAutocompleteSuggestion* simpleSuggestion =
-      [[FakeAutocompleteSuggestion alloc] init];
-  simpleSuggestion.text =
-      [[NSAttributedString alloc] initWithString:@"Simple suggestion"];
-
-  FakeAutocompleteSuggestion* suggestionWithDetail =
-      [[FakeAutocompleteSuggestion alloc] init];
-  suggestionWithDetail.text =
-      [[NSAttributedString alloc] initWithString:@"Suggestion with detail"];
-  suggestionWithDetail.detailText =
-      [[NSAttributedString alloc] initWithString:@"Detail"];
-
-  FakeAutocompleteSuggestion* clippingSuggestion =
-      [[FakeAutocompleteSuggestion alloc] init];
-  clippingSuggestion.text = [[NSAttributedString alloc]
-      initWithString:@"Suggestion with text that clips because it is very long "
-                     @"and extends off the right end of the screen"];
-  clippingSuggestion.detailText = [[NSAttributedString alloc]
-      initWithString:
-          @"Detail about the suggestion that also clips because it is too long "
-          @"for the screen and extends off of the right edge."];
-
-  FakeAutocompleteSuggestion* appendableSuggestion =
-      [[FakeAutocompleteSuggestion alloc] init];
-  appendableSuggestion.text =
-      [[NSAttributedString alloc] initWithString:@"Appendable suggestion"];
-  appendableSuggestion.isAppendable = true;
-
-  FakeAutocompleteSuggestion* otherTabSuggestion =
-      [[FakeAutocompleteSuggestion alloc] init];
-  otherTabSuggestion.text =
-      [[NSAttributedString alloc] initWithString:@"Other tab suggestion"];
-  otherTabSuggestion.isTabMatch = true;
-
-  FakeAutocompleteSuggestion* deletableSuggestion =
-      [[FakeAutocompleteSuggestion alloc] init];
-  deletableSuggestion.text =
-      [[NSAttributedString alloc] initWithString:@"Deletable suggestion"];
-  deletableSuggestion.supportsDeletion = YES;
-
-  FakeAutocompleteSuggestion* weatherSuggestion =
-      [[FakeAutocompleteSuggestion alloc] init];
-  weatherSuggestion.text =
-      [[NSAttributedString alloc] initWithString:@"weather"];
-  weatherSuggestion.hasAnswer = YES;
-  weatherSuggestion.detailText = weatherDetailText();
-  // The image currently doesn't display because there is no fake
-  // Image Retriever, but leaving this here in case this is ever necessary.
-  weatherSuggestion.imageURL =
-      GURL("https://ssl.gstatic.com/onebox/weather/128/sunny.png");
-
-  FakeAutocompleteSuggestion* stockSuggestion =
-      [[FakeAutocompleteSuggestion alloc] init];
-  stockSuggestion.text = stockText();
-  stockSuggestion.hasAnswer = YES;
-  stockSuggestion.detailText = stockDetailText();
-
-  FakeAutocompleteSuggestion* definitionSuggestion =
-      [[FakeAutocompleteSuggestion alloc] init];
-  definitionSuggestion.text = definitionText();
-  definitionSuggestion.numberOfLines = 3;
-  definitionSuggestion.hasAnswer = YES;
-  definitionSuggestion.detailText = definitionDetailText();
-
   NSArray<id<AutocompleteSuggestion>>* suggestions = @[
-    simpleSuggestion,
-    suggestionWithDetail,
-    clippingSuggestion,
-    appendableSuggestion,
-    otherTabSuggestion,
-    deletableSuggestion,
-    stockSuggestion,
-    weatherSuggestion,
-    definitionSuggestion,
+    [FakeAutocompleteSuggestion simpleSuggestion],
+    [FakeAutocompleteSuggestion suggestionWithDetail],
+    [FakeAutocompleteSuggestion clippingSuggestion],
+    [FakeAutocompleteSuggestion appendableSuggestion],
+    [FakeAutocompleteSuggestion otherTabSuggestion],
+    [FakeAutocompleteSuggestion deletableSuggestion],
+    [FakeAutocompleteSuggestion stockSuggestion],
+    [FakeAutocompleteSuggestion weatherSuggestion],
+    [FakeAutocompleteSuggestion definitionSuggestion],
+    [FakeAutocompleteSuggestion sunriseSuggestion],
+    [FakeAutocompleteSuggestion knowledgeSuggestion],
+    [FakeAutocompleteSuggestion sportsSuggestion],
+    [FakeAutocompleteSuggestion whenIsSuggestion],
+    [FakeAutocompleteSuggestion currencySuggestion],
+    [FakeAutocompleteSuggestion translateSuggestion],
+    [FakeAutocompleteSuggestion calculatorSuggestion],
   ];
 
   [self.consumer updateMatches:suggestions withAnimation:YES];
