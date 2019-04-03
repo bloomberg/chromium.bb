1d42980341446d3eb0bb06f9b9f64005f834fd5a
David Bertoni <dbertoni@chromium.org>
2019-04-03 00:23:30 +0000

    [Extensions] Added more Web Request tests for Service Workers.
    Bug: 946604
    Change-Id: Ie2afed378e47f366fd6e48c12a46966f75a38e2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1545501
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647050}
    Former-commit-id: 4d9e21d3dabb21516be11729cfdae6b4d499048e

--- chrome/test/data/extensions/api_test/service_worker/worker_based_background/web_request/service_worker_background.js
+++ chrome/test/data/extensions/api_test/service_worker/worker_based_background/web_request/service_worker_background.js
@@ -2,26 +2,89 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var baseUrl;
+var tabUrl;
 
 chrome.test.runTests([
   function setup() {
     chrome.test.getConfig(function(config) {
-      baseUrl = `http://a.com:${config.testServer.port}/extensions` +
-          '/api_test/service_worker/worker_based_background/web_request/';
+      tabUrl = `http://a.com:${config.testServer.port}/extensions` +
+          '/api_test/service_worker/worker_based_background/web_request/' +
+          'empty.html';
       chrome.test.succeed();
     });
   },
-  // Test the onBeforeRequest listener.
   function testOnBeforeRequest() {
-    updateUrl = baseUrl + 'empty.html';
     chrome.webRequest.onBeforeRequest.addListener(
         function localListener(details) {
           chrome.webRequest.onBeforeRequest.removeListener(localListener);
+          chrome.test.assertNoLastError();
           chrome.test.succeed();
-          return {cancel: false};
         }, { urls: ['<all_urls>']});
     // Create the tab.
-    chrome.tabs.create({url: updateUrl});
+    chrome.tabs.create({url: tabUrl});
+  },
+  function testOnBeforeSendHeaders() {
+    chrome.webRequest.onBeforeSendHeaders.addListener(
+        function localListener(details) {
+          chrome.webRequest.onBeforeSendHeaders.removeListener(localListener);
+          chrome.test.assertNoLastError();
+          chrome.test.assertEq(tabUrl, details.url);
+          chrome.test.succeed();
+        }, { urls: [tabUrl]});
+    chrome.tabs.create({url: tabUrl});
+  },
+  function testOnSendHeaders() {
+    chrome.webRequest.onSendHeaders.addListener(
+        function localListener(details) {
+          chrome.webRequest.onSendHeaders.removeListener(localListener);
+          chrome.test.assertNoLastError();
+          chrome.test.assertEq(tabUrl, details.url);
+          chrome.test.succeed();
+          }, { urls: [tabUrl]});
+    chrome.tabs.create({url: tabUrl});
+  },
+  function testOnHeadersReceived() {
+    chrome.webRequest.onHeadersReceived.addListener(
+        function localListener(details) {
+          chrome.webRequest.onHeadersReceived.removeListener(localListener);
+          chrome.test.assertNoLastError();
+          chrome.test.assertEq(tabUrl, details.url);
+          chrome.test.succeed();
+        }, {urls: [tabUrl]});
+    chrome.tabs.create({url: tabUrl});
+  },
+  function testOnResponseStarted() {
+    chrome.webRequest.onResponseStarted.addListener(
+        function localListener(details) {
+          chrome.webRequest.onResponseStarted.removeListener(localListener);
+          chrome.test.assertNoLastError();
+          chrome.test.assertEq(tabUrl, details.url);
+          chrome.test.succeed();
+        }, {urls: [tabUrl]});
+    chrome.tabs.create({url: tabUrl});
+  },
+  function testOnCompleted() {
+    chrome.webRequest.onCompleted.addListener(
+        function localListener(details) {
+          chrome.webRequest.onCompleted.removeListener(localListener);
+          chrome.test.assertNoLastError();
+          chrome.test.assertEq(tabUrl, details.url);
+          chrome.test.succeed();
+        }, {urls: [tabUrl]});
+    chrome.tabs.create({url: tabUrl});
+  },
+  function testOnBeforeRequestBlocked() {
+    chrome.webRequest.onErrorOccurred.addListener(
+        function localListener(details) {
+          chrome.webRequest.onErrorOccurred.removeListener(localListener);
+          chrome.test.assertEq('net::ERR_BLOCKED_BY_CLIENT', details.error);
+          chrome.test.succeed();
+        }, {urls: [tabUrl]});
+    chrome.webRequest.onBeforeRequest.addListener(
+        function localListener(details) {
+          chrome.webRequest.onBeforeRequest.removeListener(localListener);
+          return {cancel: true};
+        }, {urls: [tabUrl]}, ['blocking']);
+    chrome.tabs.create({url: tabUrl});
   },
 ]);
