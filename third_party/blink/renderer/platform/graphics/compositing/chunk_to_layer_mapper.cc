8d480d20c9300aedc709647175352982da9f53a4
Xianzhu Wang <wangxianzhu@chromium.org>
2019-04-03 00:47:30 +0000

    [PE] Avoid DCHECK failure in ChunkToLayerMapper::MapVisualRect()
    Because of floating point errors, the fast path and slow path can't
    always create the same result. Previously we tolerated non-empty cases,
    now also tolerate empty cases.
    
    Bug: 947803
    Change-Id: I3b444ea1bb58ede0daa4546be6d161a65d6b4907
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1548546
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647064}
    Former-commit-id: 57deb7a46f50c44733957f81cb9e8b4ec65c60cc

--- third_party/blink/renderer/platform/graphics/compositing/chunk_to_layer_mapper.cc
+++ third_party/blink/renderer/platform/graphics/compositing/chunk_to_layer_mapper.cc
@@ -83,15 +83,12 @@ IntRect ChunkToLayerMapper::MapVisualRect(const FloatRect& rect) const {
   if (!mapped_rect.IsEmpty() && !clip_rect_.IsInfinite())
     mapped_rect.Intersect(clip_rect_.Rect());
 
-  if (mapped_rect.IsEmpty()) {
-    DCHECK(MapUsingGeometryMapper(rect).IsEmpty());
-    return IntRect();
+  IntRect result;
+  if (!mapped_rect.IsEmpty()) {
+    mapped_rect.Inflate(outset_for_raster_effects_);
+    AdjustVisualRectBySubpixelOffset(mapped_rect);
+    result = EnclosingIntRect(mapped_rect);
   }
-
-  mapped_rect.Inflate(outset_for_raster_effects_);
-  AdjustVisualRectBySubpixelOffset(mapped_rect);
-
-  auto result = EnclosingIntRect(mapped_rect);
 #if DCHECK_IS_ON()
   auto slow_result = MapUsingGeometryMapper(rect);
   if (result != slow_result) {
