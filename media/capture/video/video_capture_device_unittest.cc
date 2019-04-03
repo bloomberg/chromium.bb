5842e919da6628d83a290b8dce5b10bb21b2aefa
Christian Fremerey <chfremer@chromium.org>
2019-04-03 18:50:50 +0000

    Revert "Disables UsingRealWebcam_XXX tests on Mac OS X"
    This reverts commit b5b896afdee76485b4e6c36f2c9454597da7f891 [formerly 03ca5214d7334213c3ce73b011d6826a606f4af2].
    
    Reason for revert: Re-enable tests after infra issue has been resolved. https://bugs.chromium.org/p/chromium/issues/detail?id=927111
    
    Original change's description:
    > Disables UsingRealWebcam_XXX tests on Mac OS X
    > 
    > TBR=chfremer
    > 
    > No-Try: true
    > Bug: 923874
    > Change-Id: I083f947ffc6a1bf9bec987d2461e42b09aef38b6
    > Reviewed-on: https://chromium-review.googlesource.com/c/1424949
    > Commit-Queue: Henrik Andreasson <henrika@chromium.org>
    > Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#624580}
    
    TBR=mbonadei@chromium.org,henrika@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 923874
    Change-Id: Iba6423d22aa75bdc462d2ff06df9665b76c611b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1551442
    Reviewed-by: Christian Fremerey <chfremer@chromium.org>
    Commit-Queue: Christian Fremerey <chfremer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647391}
    Former-commit-id: c7b7d2c776519e6569581db287523ef8455b19cf

--- media/capture/video/video_capture_device_unittest.cc
+++ media/capture/video/video_capture_device_unittest.cc
@@ -63,12 +63,9 @@
   DISABLED_UsingRealWebcam_AllocateBadSize
 // We will always get YUYV from the Mac AVFoundation implementations.
 #define MAYBE_UsingRealWebcam_CaptureMjpeg DISABLED_UsingRealWebcam_CaptureMjpeg
-// TODO(crbug.com/923874).
-#define MAYBE_UsingRealWebcam_TakePhoto DISABLED_UsingRealWebcam_TakePhoto
-  // TODO(crbug.com/923874).
-#define MAYBE_UsingRealWebcam_GetPhotoState DISABLED_UsingRealWebcam_GetPhotoState
-  // TODO(crbug.com/923874).
-#define MAYBE_UsingRealWebcam_CaptureWithSize DISABLED_UsingRealWebcam_CaptureWithSize
+#define MAYBE_UsingRealWebcam_TakePhoto UsingRealWebcam_TakePhoto
+#define MAYBE_UsingRealWebcam_GetPhotoState UsingRealWebcam_GetPhotoState
+#define MAYBE_UsingRealWebcam_CaptureWithSize UsingRealWebcam_CaptureWithSize
 #define MAYBE_UsingRealWebcam_CheckPhotoCallbackRelease \
   UsingRealWebcam_CheckPhotoCallbackRelease
 #elif defined(OS_WIN)
