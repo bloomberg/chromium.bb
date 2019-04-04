6edcf0428919f9033f4b95d0f289d6938c494bc8
Sadrul Habib Chowdhury <sadrul@chromium.org>
2019-04-04 04:07:04 +0000

    interactive-ui-tests: Add support for using with speed infra.
    Add support for writing tests that produce traces for each test. The
    traces can then be processed to generate performance metrics. The test
    can also optionally specify some UMA histograms to be reported in the
    traces.
    
    The resulting traces can then be processed by a script, e.g.:
      ./third_party/catapult/bin/run_metric <trace-file> umaMetric
    
    BUG=945709
    
    Change-Id: I66d55e3f721ae2b65901db848581837b3f415a52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1536143
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647616}
    Former-commit-id: a431384ca9c4689f78d536a4c672fa9c0128aa03

---
+++ chrome/test/base/perf/performance_test.h
@@ -0,0 +1,36 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_TEST_BASE_PERF_PERFORMANCE_TEST_H_
+#define CHROME_TEST_BASE_PERF_PERFORMANCE_TEST_H_
+
+#include "chrome/test/base/in_process_browser_test.h"
+
+// PerformanceTest is an interactive-ui-test that can be used to collect traces.
+// The traces can then be post-processed, e.g. using catapult, to report
+// performance metrics. The GetUMAHistogramNames() and GetTracingCategories()
+// overrides can be used to specify which trace-events and which UMA histograms
+// should be collected in the trace files. The trace-file location must be set
+// using the --trace-dir=<path> command-line flag. If the flag is not given,
+// then the test will still run, but not produce any traces.
+class PerformanceTest : public InProcessBrowserTest {
+ public:
+  PerformanceTest();
+  ~PerformanceTest() override;
+
+  PerformanceTest(const PerformanceTest&) = delete;
+  PerformanceTest& operator=(const PerformanceTest&) = delete;
+
+  virtual std::vector<std::string> GetUMAHistogramNames() const;
+  virtual const std::string GetTracingCategories() const;
+
+  // InProcessBrowserTest:
+  void SetUpOnMainThread() override;
+  void TearDownOnMainThread() override;
+
+ private:
+  const bool should_start_trace_;
+};
+
+#endif  // CHROME_TEST_BASE_PERF_PERFORMANCE_TEST_H_
