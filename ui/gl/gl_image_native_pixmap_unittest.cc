5ae1734da149a959642f2bf240383b13e671f251
Miguel Casas <mcasas@chromium.org>
2019-04-04 01:25:26 +0000

    Correct native pixmap format mapping for RGBX_1010102
    ToT translates gfx::BufferFormat::RGBX_1010102 to DRM's AB30, where
    it should be DRM_FORMAT_XBGR2101010.  This CL corrects that and adds
    the GLImageOzoneNativePixmap unit tests entry.
    
    The unittests were landed in crrev.com/c/738555 but somehow got
    lost: this CL re-adds the format to it.
    
     (needs https://crrev.com/c/1552107)
    
    Test: ozone_gl_unittests --gtest_also_run_disabled_tests
    Bug: 776093
    Change-Id: I90c344e22049d4d6e6f0e003f81f1c19ef489fd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1551880
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647580}
    Former-commit-id: 824bceebb3ee49014f026f0ce9b5e1779dd2afe5

--- ui/gl/gl_image_native_pixmap_unittest.cc
+++ ui/gl/gl_image_native_pixmap_unittest.cc
@@ -101,7 +101,8 @@ using GLImageTestTypes = testing::Types<
     GLImageNativePixmapTestDelegate<gfx::BufferFormat::RGBX_8888>,
     GLImageNativePixmapTestDelegate<gfx::BufferFormat::RGBA_8888>,
     GLImageNativePixmapTestDelegate<gfx::BufferFormat::BGRX_8888>,
-    GLImageNativePixmapTestDelegate<gfx::BufferFormat::BGRA_8888>>;
+    GLImageNativePixmapTestDelegate<gfx::BufferFormat::BGRA_8888>,
+    GLImageNativePixmapTestDelegate<gfx::BufferFormat::RGBX_1010102>>;
 
 #if !defined(MEMORY_SANITIZER)
 // Fails under MSAN: crbug.com/886995
