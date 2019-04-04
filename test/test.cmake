cc78b70d6f3ab1f1c7d9e42dd01e03abef3e0cdb
Yue Chen <yuec@google.com>
2019-04-03 17:47:21 -0700

    Add a test to test rollover of int64 in encoder interface
    The current libaom encoder interface can potentially rollover an int64_t
    value used to calculate the current timestamp. If the timebase was set
    to microseconds and first timestamp was 0, then the rollover would
    occur in about 10.675 days.
    This test is migrated from a libvpx CL:
    https://chromium-review.googlesource.com/c/webm/libvpx/+/1539258
    
    BUG=aomedia:2053
    
    Change-Id: I49ce521045782737495859eafade5e44e82a8604

--- test/test.cmake
+++ test/test.cmake
@@ -79,7 +79,8 @@ list(APPEND AOM_UNIT_TEST_ENCODER_SOURCES
             "${AOM_ROOT}/test/scalability_test.cc"
             "${AOM_ROOT}/test/y4m_test.cc"
             "${AOM_ROOT}/test/y4m_video_source.h"
-            "${AOM_ROOT}/test/yuv_video_source.h")
+            "${AOM_ROOT}/test/yuv_video_source.h"
+            "${AOM_ROOT}/test/time_stamp_test.cc")
 
 list(APPEND AOM_DECODE_PERF_TEST_SOURCES "${AOM_ROOT}/test/decode_perf_test.cc")
 list(APPEND AOM_ENCODE_PERF_TEST_SOURCES "${AOM_ROOT}/test/encode_perf_test.cc")
