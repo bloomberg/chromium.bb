9e87448c636580fe6357d30990d32b11d41702aa
Sadrul Habib Chowdhury <sadrul@chromium.org>
2019-04-04 04:07:04 +0000

    interactive-ui-tests: Add support for using with speed infra.
    Add support for writing tests that produce traces for each test. The
    traces can then be processed to generate performance metrics. The test
    can also optionally specify some UMA histograms to be reported in the
    traces.
    
    The resulting traces can then be processed by a script, e.g.:
      ./third_party/catapult/bin/run_metric <trace-file> umaMetric
    
    BUG=945709
    
    Change-Id: I66d55e3f721ae2b65901db848581837b3f415a52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1536143
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647616}
    Former-commit-id: a431384ca9c4689f78d536a4c672fa9c0128aa03

---
+++ chrome/test/base/perf/performance_test.cc
@@ -0,0 +1,70 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/test/base/perf/performance_test.h"
+
+#include "base/command_line.h"
+#include "base/files/file_util.h"
+#include "base/trace_event/trace_event.h"
+#include "content/public/browser/tracing_controller.h"
+#include "services/tracing/public/cpp/trace_event_agent.h"
+#include "ui/compositor/compositor_switches.h"
+#include "ui/gl/gl_switches.h"
+
+static const char kTraceDir[] = "trace-dir";
+
+PerformanceTest::PerformanceTest()
+    : should_start_trace_(
+          base::CommandLine::ForCurrentProcess()->HasSwitch(kTraceDir)) {
+  if (should_start_trace_) {
+    auto* cmd = base::CommandLine::ForCurrentProcess();
+    cmd->AppendSwitch(switches::kUseGpuInTests);
+    cmd->AppendSwitch(switches::kEnablePixelOutputInTests);
+  }
+}
+
+PerformanceTest::~PerformanceTest() = default;
+
+std::vector<std::string> PerformanceTest::GetUMAHistogramNames() const {
+  return {};
+}
+
+const std::string PerformanceTest::GetTracingCategories() const {
+  return std::string();
+}
+
+void PerformanceTest::SetUpOnMainThread() {
+  InProcessBrowserTest::SetUpOnMainThread();
+  if (!should_start_trace_)
+    return;
+
+  auto* controller = content::TracingController::GetInstance();
+  base::trace_event::TraceConfig config(GetTracingCategories(),
+                                        base::trace_event::RECORD_CONTINUOUSLY);
+  for (const auto& histogram : GetUMAHistogramNames())
+    config.EnableHistogram(histogram);
+
+  base::RunLoop runloop;
+  bool result = controller->StartTracing(config, runloop.QuitClosure());
+  runloop.Run();
+  CHECK(result);
+}
+
+void PerformanceTest::TearDownOnMainThread() {
+  if (should_start_trace_) {
+    auto* controller = content::TracingController::GetInstance();
+    base::RunLoop runloop;
+    base::FilePath dir =
+        base::CommandLine::ForCurrentProcess()->GetSwitchValuePath(kTraceDir);
+    base::FilePath trace_file;
+    CHECK(base::CreateTemporaryFileInDir(dir, &trace_file));
+
+    auto trace_data_endpoint = content::TracingController::CreateFileEndpoint(
+        trace_file, runloop.QuitClosure());
+    bool result = controller->StopTracing(trace_data_endpoint);
+    runloop.Run();
+    CHECK(result);
+  }
+  InProcessBrowserTest::TearDownOnMainThread();
+}
