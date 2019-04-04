d63883d99d138d921f02e9b2fb1d0dc587040620
Dan Elphick <delphick@chromium.org>
2019-04-04 20:18:54 +0000

    [v8] Fix uses of deprecated Object::Set in content/renderer
    Uses Context version of Object::Set and calls Check on the result as
    these are cases where it cannot fail.
    
    Bug: v8:7283, v8:8834
    Change-Id: I9bc6cd25b761690753eaf7cc187e1ecb14685169
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1552876
    Auto-Submit: Dan Elphick <delphick@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#647880}
    Former-commit-id: 12bdce1d3ca269bbe29f5481d30084d47f62cfa3

--- content/renderer/web_ui_extension.cc
+++ content/renderer/web_ui_extension.cc
@@ -74,16 +74,20 @@ void WebUIExtension::Install(blink::WebLocalFrame* frame) {
   v8::Context::Scope context_scope(context);
 
   v8::Local<v8::Object> chrome = GetOrCreateChromeObject(isolate, context);
-  chrome->Set(
-      gin::StringToSymbol(isolate, "send"),
-      gin::CreateFunctionTemplate(isolate, base::Bind(&WebUIExtension::Send))
-          ->GetFunction(context)
-          .ToLocalChecked());
-  chrome->Set(gin::StringToSymbol(isolate, "getVariableValue"),
-              gin::CreateFunctionTemplate(
-                  isolate, base::Bind(&WebUIExtension::GetVariableValue))
-                  ->GetFunction(context)
-                  .ToLocalChecked());
+  chrome
+      ->Set(context, gin::StringToSymbol(isolate, "send"),
+            gin::CreateFunctionTemplate(isolate,
+                                        base::Bind(&WebUIExtension::Send))
+                ->GetFunction(context)
+                .ToLocalChecked())
+      .Check();
+  chrome
+      ->Set(context, gin::StringToSymbol(isolate, "getVariableValue"),
+            gin::CreateFunctionTemplate(
+                isolate, base::Bind(&WebUIExtension::GetVariableValue))
+                ->GetFunction(context)
+                .ToLocalChecked())
+      .Check();
 }
 
 // static
