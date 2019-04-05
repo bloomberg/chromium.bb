623098c23edc69bec5959734540d9e757a385bbc
Lucas Furukawa Gadani <lfg@chromium.org>
2019-04-05 14:56:10 +0000

    Portals: Add flag to enable/disable portals in about:flags.
    Bug: 865565
    Change-Id: I72563a04cb7fac4f7a17b9405237e83a8fd19f2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553658
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Lucas Gadani <lfg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648178}
    Former-commit-id: 9f57e19170ed93da4a2e308b356919ebbc301cc7

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -4037,6 +4037,10 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_VALUE_TYPE(features::kInSessionPasswordChange)},
 #endif  // OS_CHROMEOS
 
+    {"enable-portals", flag_descriptions::kEnablePortalsName,
+     flag_descriptions::kEnablePortalsDescription, kOsAll,
+     FEATURE_VALUE_TYPE(blink::features::kPortals)},
+
     // NOTE: Adding a new flag requires adding a corresponding entry to enum
     // "LoginCustomFlags" in tools/metrics/histograms/enums.xml. See "Flag
     // Histograms" in tools/metrics/histograms/README.md (run the
