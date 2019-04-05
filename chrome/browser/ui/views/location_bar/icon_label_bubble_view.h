c946d35c595a001011e514795422cc2be008b775
Collin Baker <collinbaker@chromium.org>
2019-04-05 19:59:45 +0000

    Revert "Move SeparatorView into its own file"
    This reverts commit 016a143f621057ead2db4376345cf67c78ef1bf4 [formerly 172df5f36a2167a7b945ff97cd6e9b9b5b7437d0].
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > Move SeparatorView into its own file
    > 
    > SeparatorView paints a separator for IconLabelBubbleView in certain
    > cases. This moves it into its own file and loosens the coupling
    > between the two, while trying not to change any behavior (yet).
    > 
    > This is in preparation for moving the separator out of
    > IconLabelBubbleView entirely.
    > 
    > Bug: 946293
    > Change-Id: Ia3b7c9c51c781dc75a7bcb33064d8cf0dc8c44ea
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1540682
    > Commit-Queue: Collin Baker <collinbaker@chromium.org>
    > Auto-Submit: Collin Baker <collinbaker@chromium.org>
    > Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#645150}
    
    TBR=bsep@chromium.org,collinbaker@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 946293
    Change-Id: I874381204cd63d11b63c740a84556b3656d9f3b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553934
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648301}
    Former-commit-id: 9e6da53e23fdacbe835c0dfd995dee8497991cd1

--- chrome/browser/ui/views/location_bar/icon_label_bubble_view.h
+++ chrome/browser/ui/views/location_bar/icon_label_bubble_view.h
@@ -35,8 +35,6 @@ namespace views {
 class ImageView;
 }
 
-class LocationBarSeparatorView;
-
 // View used to draw a bubble, containing an icon and a label. We use this as a
 // base for the classes that handle the location icon (including the EV bubble),
 // tab-to-search UI, and content settings.
@@ -45,6 +43,30 @@ class IconLabelBubbleView : public views::InkDropObserver,
                             public ui::MaterialDesignControllerObserver {
  public:
   static constexpr int kTrailingPaddingPreMd = 2;
+
+  // A view that draws the separator.
+  class SeparatorView : public views::View {
+   public:
+    explicit SeparatorView(IconLabelBubbleView* owner);
+
+    // views::View:
+    void OnPaint(gfx::Canvas* canvas) override;
+
+    // Updates the opacity based on the ink drop's state.
+    void UpdateOpacity();
+
+    void set_disable_animation_for_test(bool disable_animation_for_test) {
+      disable_animation_for_test_ = disable_animation_for_test;
+    }
+
+   private:
+    // Weak.
+    IconLabelBubbleView* owner_;
+
+    bool disable_animation_for_test_ = false;
+
+    DISALLOW_COPY_AND_ASSIGN(SeparatorView);
+  };
 
   explicit IconLabelBubbleView(const gfx::FontList& font_list);
   ~IconLabelBubbleView() override;
@@ -67,7 +89,7 @@ class IconLabelBubbleView : public views::InkDropObserver,
   SkColor GetParentBackgroundColor() const;
 
   // Exposed for testing.
-  LocationBarSeparatorView* separator_view() const { return separator_view_; }
+  SeparatorView* separator_view() const { return separator_view_; }
 
   // Exposed for testing.
   bool is_animating_label() const { return slide_animation_.is_animating(); }
@@ -199,10 +221,8 @@ class IconLabelBubbleView : public views::InkDropObserver,
   // bounds and the separator visibility.
   void UpdateHighlightPath();
 
-  void UpdateSeparator();
-
   // The contents of the bubble.
-  LocationBarSeparatorView* separator_view_;
+  SeparatorView* separator_view_;
 
   // The padding of the element that will be displayed after |this|. This value
   // is relevant for calculating the amount of space to reserve after the
