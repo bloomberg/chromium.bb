b81844b0b7d4d0d2bb580adf994cc9a32040f4f8
Austin Eng <enga@chromium.org>
2019-04-05 21:51:56 +0000

    Implement GPUUncapturedErrorEvent in Blink
    Bug: 877147
    Change-Id: I9bd31a9f9731e3cd50f43e246f9d0565f107bd9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546058
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648356}
    Former-commit-id: 11c116c894991ab16c394fdf66fbf28c3df66f39

--- third_party/blink/renderer/modules/webgpu/gpu_uncaptured_error_event.h
+++ third_party/blink/renderer/modules/webgpu/gpu_uncaptured_error_event.h
@@ -5,10 +5,12 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_WEBGPU_GPU_UNCAPTURED_ERROR_EVENT_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_WEBGPU_GPU_UNCAPTURED_ERROR_EVENT_H_
 
+#include "third_party/blink/renderer/bindings/modules/v8/gpu_out_of_memory_error_or_gpu_validation_error.h"
 #include "third_party/blink/renderer/modules/event_modules.h"
 
 namespace blink {
 
+class GPUOutOfMemoryErrorOrGPUValidationError;
 class GPUUncapturedErrorEventInit;
 
 class GPUUncapturedErrorEvent : public Event {
@@ -20,10 +22,14 @@ class GPUUncapturedErrorEvent : public Event {
   GPUUncapturedErrorEvent(const AtomicString& type,
                           const GPUUncapturedErrorEventInit*);
 
+  void Trace(Visitor*) override;
+
   // gpu_uncaptured_error_event.idl
-  // TODO(crbug.com/877147): implement GPUUncapturedErrorEvent.
+  void error(GPUOutOfMemoryErrorOrGPUValidationError&) const;
 
  private:
+  GPUOutOfMemoryErrorOrGPUValidationError error_;
+
   DISALLOW_COPY_AND_ASSIGN(GPUUncapturedErrorEvent);
 };
 
