5a7f6f5e3a871b8a4de09a8eeff1cbe98f2a0db3
Robbie Gibson <rkgibson@google.com>
2019-04-05 14:26:09 +0000

    🦁Load answer images in Omnibox popup
    This CL adds actually loading the answer images in the omnibox popup.
    It also fixes the issue from CL 1545886 and uses a separate image view
    for the answer image instead of using the UIButton for it.
    
    Bug: 945323
    Change-Id: Ibbe77deeac21ca31ecc35fe5db5047f272326686
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1549122
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#648171}
    Former-commit-id: e475ec7b906743e93d87c8b1d4abc162a204744b

--- ios/chrome/browser/ui/elements/extended_touch_target_button.mm
+++ ios/chrome/browser/ui/elements/extended_touch_target_button.mm
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/location_bar/extended_touch_target_button.h"
+#import "ios/chrome/browser/ui/elements/extended_touch_target_button.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
