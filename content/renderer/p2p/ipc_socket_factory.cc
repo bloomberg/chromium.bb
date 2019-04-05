5064e76cb73c55cff8decfd84163a2bb3be05f99
Evan Shrubsole <eshr@google.com>
2019-04-05 13:07:08 +0000

    Change P2PSendPacketMetrics send_time to be based on rtc::TimeMillis
    P2PSendPacketMetrics.send_time should be represented consistently
    by the webrtc clock. Various issues occur on webrtc otherwise, as
    different clocks are used for different event types.
    
    To illustrate the clock differences, I have a test comparing chrome's
    clock to webrtc's clock. It shows that there is a difference on
    Windows and iOS https://chromium-review.googlesource.com/c/chromium/src/+/1514840
    
    Bug: webrtc:10407
    Change-Id: I818dfc25a18337b896c382d77b70854020cff3fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1526045
    Commit-Queue: Evan Shrubsole <eshr@google.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648150}
    Former-commit-id: 2d7b5edb3646289432f80aba30d8e59e4d00bfae

--- content/renderer/p2p/ipc_socket_factory.cc
+++ content/renderer/p2p/ipc_socket_factory.cc
@@ -601,12 +601,8 @@ void IpcPacketSocket::OnSendComplete(
   in_flight_packet_records_.pop_front();
   TraceSendThrottlingState();
 
-  int64_t send_time_ms = -1;
-  if (send_metrics.rtc_packet_id >= 0) {
-    send_time_ms = send_metrics.send_time.since_origin().InMilliseconds();
-  }
   SignalSentPacket(this, rtc::SentPacket(send_metrics.rtc_packet_id,
-                                         send_time_ms));
+                                         send_metrics.send_time_ms));
 
   if (writable_signal_expected_ && send_bytes_available_ > 0) {
     WebRtcLogMessage(base::StringPrintf(
