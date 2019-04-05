246e4bb7cc9a603315fbdf3bd7902f204a68d8c3
Ben Kelly <wanderview@chromium.org>
2019-04-05 21:01:47 +0000

    CacheStorage: Move Legacy classes to separate dir.
    This is a follow-on to crrev.com/c/1542177 where we split
    CacheStorage/CacheStorageCache into separate interface and
    implementation classes.  This CL moves the new LegacyCacheStorage and
    LegacyCacheStorageCache classes into separate files in a new "legacy"
    directory.
    
    This CL only moves code and updates include paths.  It does not modify
    any functional code.
    
    Bug: 940449
    Change-Id: I41059a3545f716042ed55a03a011264684054d73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1548298
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648330}
    Former-commit-id: ab68c05beb790d04d1cb7fd8faa0a197fb40d399

--- content/browser/cache_storage/cache_storage_cache_unittest.cc
+++ content/browser/cache_storage/cache_storage_cache_unittest.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/browser/cache_storage/cache_storage_cache.h"
+#include "content/browser/cache_storage/legacy/legacy_cache_storage_cache.h"
 
 #include <stddef.h>
 #include <stdint.h>
