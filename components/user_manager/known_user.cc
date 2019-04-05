bd09f7638e6fa65ef8da8e10dd0a212075009887
Henrique Grandinetti <hgrandinetti@chromium.org>
2019-04-05 20:38:35 +0000

    Sync Parent Access Code config to known user storage.
    Bug: 946802
    Change-Id: I2154c3499924af299a9a52bad52b86abe473ec75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1542154
    Commit-Queue: Henrique Grandinetti <hgrandinetti@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648320}
    Former-commit-id: 07dca1401053d346e4d67814e4920199da987bb8

--- components/user_manager/known_user.cc
+++ components/user_manager/known_user.cc
@@ -25,7 +25,8 @@ namespace {
 // placed in this list.
 const char kKnownUsers[] = "KnownUsers";
 
-// Known user preferences keys (stored in Local State).
+// Known user preferences keys (stored in Local State). All keys should be
+// listed in kReservedKeys below.
 
 // Key of canonical e-mail value.
 const char kCanonicalEmail[] = "email";
@@ -64,6 +65,20 @@ const char kProfileRequiresPolicy[] = "profile_requires_policy";
 // Key of the boolean flag telling if user is ephemeral and should be removed
 // from the local state on logout.
 const char kIsEphemeral[] = "is_ephemeral";
+
+// List containing all the known user preferences keys.
+const char* kReservedKeys[] = {kCanonicalEmail,
+                               kGAIAIdKey,
+                               kObjGuidKey,
+                               kAccountTypeKey,
+                               kUsingSAMLKey,
+                               kDeviceId,
+                               kGAPSCookie,
+                               kReauthReasonKey,
+                               kGaiaIdMigration,
+                               kMinimalMigrationAttempted,
+                               kProfileRequiresPolicy,
+                               kIsEphemeral};
 
 PrefService* GetLocalState() {
   if (!UserManager::IsInitialized())
@@ -206,13 +221,6 @@ bool GetStringPref(const AccountId& account_id,
 void SetStringPref(const AccountId& account_id,
                    const std::string& path,
                    const std::string& in_value) {
-  PrefService* local_state = GetLocalState();
-
-  // Local State may not be initialized in tests.
-  if (!local_state)
-    return;
-
-  ListPrefUpdate update(local_state, kKnownUsers);
   base::DictionaryValue dict;
   dict.SetString(path, in_value);
   UpdatePrefs(account_id, dict, false);
@@ -231,13 +239,6 @@ bool GetBooleanPref(const AccountId& account_id,
 void SetBooleanPref(const AccountId& account_id,
                     const std::string& path,
                     const bool in_value) {
-  PrefService* local_state = GetLocalState();
-
-  // Local State may not be initialized in tests.
-  if (!local_state)
-    return;
-
-  ListPrefUpdate update(local_state, kKnownUsers);
   base::DictionaryValue dict;
   dict.SetBoolean(path, in_value);
   UpdatePrefs(account_id, dict, false);
@@ -255,16 +256,45 @@ bool GetIntegerPref(const AccountId& account_id,
 void SetIntegerPref(const AccountId& account_id,
                     const std::string& path,
                     const int in_value) {
-  PrefService* local_state = GetLocalState();
-
-  // Local State may not be initialized in tests.
-  if (!local_state)
-    return;
-
-  ListPrefUpdate update(local_state, kKnownUsers);
   base::DictionaryValue dict;
   dict.SetInteger(path, in_value);
   UpdatePrefs(account_id, dict, false);
+}
+
+bool GetPref(const AccountId& account_id,
+             const std::string& path,
+             const base::Value** out_value) {
+  const base::DictionaryValue* user_pref_dict = nullptr;
+  if (!FindPrefs(account_id, &user_pref_dict))
+    return false;
+
+  *out_value = user_pref_dict->FindPath(path);
+  return *out_value != nullptr;
+}
+
+void SetPref(const AccountId& account_id,
+             const std::string& path,
+             base::Value in_value) {
+  base::DictionaryValue dict;
+  dict.SetPath(path, std::move(in_value));
+  UpdatePrefs(account_id, dict, false);
+}
+
+void RemovePref(const AccountId& account_id, const std::string& path) {
+  // Prevent removing keys that are used internally.
+  for (const std::string& key : kReservedKeys)
+    CHECK_NE(path, key);
+
+  const base::DictionaryValue* user_pref_dict = nullptr;
+  if (!FindPrefs(account_id, &user_pref_dict))
+    return;
+
+  base::Value updated_user_pref = user_pref_dict->Clone();
+  base::DictionaryValue* updated_user_pref_dict;
+  updated_user_pref.GetAsDictionary(&updated_user_pref_dict);
+
+  updated_user_pref_dict->RemovePath(path);
+  UpdatePrefs(account_id, *updated_user_pref_dict, true);
 }
 
 AccountId GetAccountId(const std::string& user_email,
