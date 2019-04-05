ab7758a899c0089f40be8eb8473ba848d729215a
Christos Froussios <cfroussios@chromium.org>
2019-04-05 10:13:16 +0000

    Revert "Record quic proxy server status with servicification"
    This reverts commit ae2df88dddc5c131dc65510a7ed16dc3c1ccdd8c [formerly 758594644b2cc51bd1e858e5985dcfd383530780].
    
    Reason for revert: Suspected of breaking
    DataReductionProxyFallbackBrowsertest.ProxyBlockedOnAuthError
    DataReductionProxyFallbackBrowsertest.RedirectCycle
    on linux-chromeos-dbg
    
    Bug:949577
    
    Original change's description:
    > Record quic proxy server status with servicification
    > 
    > Also fixes a webdriver test when QUIC availability status of data saver
    > proxy is recorded.
    > 
    > Bug: 934875
    > Change-Id: I1320fb26b284bd23ae431f045e3e8724aa814cda
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1549927
    > Commit-Queue: rajendrant <rajendrant@chromium.org>
    > Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#647087}
    
    TBR=robertogden@chromium.org,rajendrant@chromium.org,ryansturm@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 934875
    Change-Id: Iddbf7208e399a78ea635e5520a7fc3cc0cc66917
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1554343
    Reviewed-by: Christos Froussios <cfroussios@chromium.org>
    Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648131}
    Former-commit-id: f014717763fa1689ae9a912f2a35ec30c9539b4c

--- components/data_reduction_proxy/content/common/data_reduction_proxy_url_loader_throttle.cc
+++ components/data_reduction_proxy/content/common/data_reduction_proxy_url_loader_throttle.cc
@@ -5,7 +5,6 @@
 #include "components/data_reduction_proxy/content/common/data_reduction_proxy_url_loader_throttle.h"
 
 #include "base/bind.h"
-#include "base/metrics/histogram_macros.h"
 #include "components/data_reduction_proxy/content/common/header_util.h"
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_bypass_protocol.h"
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_headers.h"
@@ -20,17 +19,6 @@ class HttpRequestHeaders;
 }
 
 namespace data_reduction_proxy {
-
-namespace {
-void RecordQuicProxyStatus(const net::ProxyServer& proxy_server) {
-  if (proxy_server.is_https()) {
-    RecordQuicProxyStatus(IsQuicProxy(proxy_server)
-                              ? QUIC_PROXY_STATUS_AVAILABLE
-                              : QUIC_PROXY_NOT_SUPPORTED);
-  }
-}
-
-}  // namespace
 
 DataReductionProxyURLLoaderThrottle::DataReductionProxyURLLoaderThrottle(
     const net::HttpRequestHeaders& post_cache_headers,
@@ -90,7 +78,6 @@ void DataReductionProxyURLLoaderThrottle::BeforeWillProcessResponse(
 
   before_will_process_response_received_ = true;
   MaybeRetry(proxy_server, response_head.headers.get(), net::OK, defer);
-  RecordQuicProxyStatus(proxy_server);
 }
 
 void DataReductionProxyURLLoaderThrottle::MaybeRetry(
