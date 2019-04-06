65d846dfe1ea11a08d5486e3a8eca721948df95a
David Tseng <dtseng@chromium.org>
2019-04-06 04:51:38 +0000

    Clear inapplicable disable reasons for component extensions
    This change ensures that all component extensions are allowed to be activated  despite having some inapplicable disable reasons set.
    
    In particular, this fixes any specific observed on-device instances where ChromeVox or tts had a disable reason of blocked by policy which prevented it from being activated.
    
    Bug: 946839,947305
    Test: manually. With a device enterprise enrolled, push down kiosk apps to the device from an admin console. Ensure that ChromeVox still works on all screens.
    Change-Id: I2cedafeb16d4420702188f1516825ce1bad52802
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553017
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648464}
    Former-commit-id: 07619eb11e0262154fd0bc07a338d45ce6c90489

--- extensions/browser/extension_registrar.cc
+++ extensions/browser/extension_registrar.cc
@@ -111,6 +111,7 @@ void ExtensionRegistrar::AddExtension(
 void ExtensionRegistrar::AddNewExtension(
     scoped_refptr<const Extension> extension) {
   if (extension_prefs_->IsExtensionBlacklisted(extension->id())) {
+    DCHECK(!Manifest::IsComponentLocation(extension->location()));
     // Only prefs is checked for the blacklist. We rely on callers to check the
     // blacklist before calling into here, e.g. CrxInstaller checks before
     // installation then threads through the install and pending install flow
@@ -118,6 +119,7 @@ void ExtensionRegistrar::AddNewExtension(
     // extensions.
     registry_->AddBlacklisted(extension);
   } else if (delegate_->ShouldBlockExtension(extension.get())) {
+    DCHECK(!Manifest::IsComponentLocation(extension->location()));
     registry_->AddBlocked(extension);
   } else if (extension_prefs_->IsExtensionDisabled(extension->id())) {
     registry_->AddDisabled(extension);
