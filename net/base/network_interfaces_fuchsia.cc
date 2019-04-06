0fb543dc7256df19e07fd3ae837008dfcf30bba7
Wez <wez@chromium.org>
2019-04-06 19:16:34 +0000

    [Fuchsia] Update FIDL-array call-sites from |count| to |size|.
    FIDL arrays are now using std::array<>, which has a size() method but no
    count() synonym for it.
    
    TBR=sergeyu
    
    Bug: 950279
    Change-Id: I4fb6677d72f5dd17c391fe477a7fd4577799115b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1556536
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648501}
    Former-commit-id: a5fa529d062c224c8659e2fc6dacb737709c50ca

--- net/base/network_interfaces_fuchsia.cc
+++ net/base/network_interfaces_fuchsia.cc
@@ -69,10 +69,10 @@ NetworkChangeNotifier::ConnectionType ConvertConnectionType(
 
 IPAddress FuchsiaIpAddressToIPAddress(const fuchsia::net::IpAddress& addr) {
   if (addr.is_ipv4()) {
-    return IPAddress(addr.ipv4().addr.data(), addr.ipv4().addr.count());
+    return IPAddress(addr.ipv4().addr.data(), addr.ipv4().addr.size());
   }
   if (addr.is_ipv6()) {
-    return IPAddress(addr.ipv6().addr.data(), addr.ipv6().addr.count());
+    return IPAddress(addr.ipv6().addr.data(), addr.ipv6().addr.size());
   }
   return IPAddress();
 }
