cc2c28c88452d2fbe4980b3d009e2ee2ab81c89d
Dhanya Ganesh <dhanyaganesh@chromium.org>
2019-04-06 20:17:13 -0600

    auth.py: Stop writing to log every time.
    BUG=None
    TEST=None
    
    Change-Id: Id2f36f7fcb8e1a0e0a5035dab606b5764a34e235
    Reviewed-on: https://chromium-review.googlesource.com/1556501
    Commit-Ready: Dhanya Ganesh <dhanyaganesh@chromium.org>
    Tested-by: Dhanya Ganesh <dhanyaganesh@chromium.org>
    Reviewed-by: Dhanya Ganesh <dhanyaganesh@chromium.org>

--- lib/auth.py
+++ lib/auth.py
@@ -73,7 +73,7 @@ def Login(service_account_json=None):
 
   result = cros_build_lib.RunCommand(
       cmd,
-      mute_output=False,
+      mute_output=True,
       error_code_ok=True)
 
   if result.returncode:
