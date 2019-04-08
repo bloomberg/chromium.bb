9c4fbe9ace72f0d2c745b6f3081e8033d5490055
Xiyuan Xia <xiyuan@chromium.org>
2019-04-08 15:13:38 +0000

    Reland "chromeos: Add test to drag a window/tab to overview in tablet mode"
    This is a reland of b20e48d4fdce745c3ab35bdccc9e82fb514ed9a9 [formerly ee9ec69109572f727fe50f0956ece2d7ecaa1e66]
    
    Fix the failure on classic because mouse move event is held by
    WindowTreeHost. Added WaitForNoPointerHoldLock before sending
    mouse move events.
    
    Original change's description:
    > chromeos: Add test to drag a window/tab to overview in tablet mode
    >
    > - Add tests to drag window/tab to overview in tablet mode;
    > - Fix UIControlsOzone to use host dips instead of host pixels
    >   with EventInjector and use PointF to avoid rounding errors;
    >
    > Bug: 942599
    > Change-Id: I0325749cdd95d4b0c213d09f349083336ec3f3b0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546718
    > Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#647147}
    
    Bug: 942599
    Change-Id: I4a609cad7b872353f2087914406840624111b1be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1550632
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648676}
    Former-commit-id: 65d4bbfd04c0251b810fb35e01325cecec90fa9f

--- ash/shell_test_api.h
+++ ash/shell_test_api.h
@@ -61,10 +61,12 @@ class ShellTestApi : public mojom::ShellTestApi {
                              SnapWindowInSplitViewCallback cb) override;
   void ToggleFullscreen(ToggleFullscreenCallback cb) override;
   void ToggleOverviewMode(ToggleOverviewModeCallback cb) override;
+  void IsOverviewSelecting(IsOverviewSelectingCallback callback) override;
   void AddRemoveDisplay() override;
   void SetMinFlingVelocity(float velocity) override;
   void WaitForNoPointerHoldLock(
       WaitForNoPointerHoldLockCallback callback) override;
+  void WaitForNextFrame(WaitForNextFrameCallback callback) override;
 
  private:
   Shell* shell_;  // not owned
