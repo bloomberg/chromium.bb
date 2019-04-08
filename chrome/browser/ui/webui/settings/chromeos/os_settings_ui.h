84e1d178ee5f3cf6c9949e2dc5561f71cbfd139a
James Cook <jamescook@chromium.org>
2019-04-08 23:20:01 +0000

    SplitSettings: Migrate Chrome OS WebUI handlers to OSSettingsUI
    This allows them to be shared between MdSettingsUI and OSSettingsUI
    during the migration.
    
    OS settings doesn't use these yet, but will do so as the HTML/JS is
    migrated.
    
    This CL does not migrate account management UI, as we're still
    sorting out how that will be split.
    
    Bug: 950007
    Test: browser_tests, browser settings and OS settings open normally
    Change-Id: Ic7cae2323b9a959df241f28a90017eab8a5dd6f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1554835
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#648925}
    Former-commit-id: b47cf74a28de3dbaa756aa0910fe5ea39c0cf994

--- chrome/browser/ui/webui/settings/chromeos/os_settings_ui.h
+++ chrome/browser/ui/webui/settings/chromeos/os_settings_ui.h
@@ -8,6 +8,13 @@
 #include "base/macros.h"
 #include "content/public/browser/web_ui_controller.h"
 
+class Profile;
+
+namespace content {
+class WebUI;
+class WebUIDataSource;
+}  // namespace content
+
 namespace chromeos {
 namespace settings {
 
@@ -16,6 +23,11 @@ class OSSettingsUI : public content::WebUIController {
  public:
   explicit OSSettingsUI(content::WebUI* web_ui);
   ~OSSettingsUI() override;
+
+  // Initializes the WebUI message handlers for OS-specific settings.
+  static void InitWebUIHandlers(Profile* profile,
+                                content::WebUI* web_ui,
+                                content::WebUIDataSource* html_source);
 
  private:
   DISALLOW_COPY_AND_ASSIGN(OSSettingsUI);
