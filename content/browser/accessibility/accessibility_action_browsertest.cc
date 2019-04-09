bloomberg/chromium.bb

commit 147b31e4f19acd37d682cca968f75a6eb554b750

Fire UIA_MenuOpenedEventId and UIA_MenuClosedEventId
1. Implementing support for UIA's `MenuOpened` and `MenuClosed` events.

   `BrowserAccessibilityManager::OnFocusLost` is introduced to handle
   firing of events caused by a node losing focus (i.e. MenuClosed).

2. Adding `DumpAccessibilityEventsTest` for both events.

3. Fixing a bug in `AccessibilityEventRecorderUia` where dump tests that
   run the `go()` script multiple times result in timeout failures.

   This happened because `AddAutomationEventHandler` hanged when called
   by `AccessibilityEventRecorderUia::Thread::ThreadMain`, and such hang
   was produced because we didn't expect to re-add the handlers in later
   runs of the `go()` script. To solve the issue we simply remove all
   event handlers at cleanup on `ThreadMain`.

4. Addressing an issue with the test infrastructure for Win7 where
   `AccessibilityEventRecorderUia::Thread::EventHandler::GetSenderInfo`
   crashed at requesting some properties from an UIA element. We suppose
   there is a bug in Win7 where such request is not properly marshalled
   to the main thread's STA and crosses thread apartment boundaries.

   This change introduces a work around for such issue by caching the
   `AriaRole` and `Name` properties queried in `GetSenderInfo`.

Bug: 928949
Change-Id: Ib720ae6cb1319473759db1c6b9786803eabe0469
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1518407
Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
Commit-Queue: Ethan Jimenez <ethavar@microsoft.com>
Cr-Commit-Position: refs/heads/master@{#645032}
Former-commit-id: 68bc8e104b88b97805e6be932bf5a2056dec856d

content/browser/accessibility/browser_accessibility_manager.cc
@@ -247,8 +247,12 @@ void BrowserAccessibilityManager::FireFocusEventsIfNeeded() {
     focus = nullptr;
   }
 
-  if (focus && focus != last_focused_node_)
-    FireFocusEvent(focus);
+  if (focus != last_focused_node_) {
+    if (last_focused_node_)
+      OnFocusLost(last_focused_node_);
+    if (focus)
+      FireFocusEvent(focus);
+  }
 
   last_focused_node_ = focus;
   last_focused_manager_ = focus ? focus->manager() : nullptr;
@@ -1151,36 +1155,40 @@ gfx::Rect BrowserAccessibilityManager::GetPageBoundsForRange(
 void BrowserAccessibilityManager::OnNodeWillBeDeleted(ui::AXTree* tree,
                                                       ui::AXNode* node) {
   DCHECK(node);
-  if (id_wrapper_map_.find(node->id()) == id_wrapper_map_.end())
-    return;
-  GetFromAXNode(node)->Destroy();
-  id_wrapper_map_.erase(node->id());
+  if (BrowserAccessibility* wrapper = GetFromAXNode(node)) {
+    if (wrapper == last_focused_node_) {
+      last_focused_node_ = nullptr;
+      last_focused_manager_ = nullptr;
+    }
+    wrapper->Destroy();
+    id_wrapper_map_.erase(node->id());
+  }
 }
 
 void BrowserAccessibilityManager::OnSubtreeWillBeDeleted(ui::AXTree* tree,
                                                          ui::AXNode* node) {
   DCHECK(node);
-  BrowserAccessibility* obj = GetFromAXNode(node);
-  if (obj)
-    obj->OnSubtreeWillBeDeleted();
+  if (BrowserAccessibility* wrapper = GetFromAXNode(node))
+    wrapper->OnSubtreeWillBeDeleted();
 }
 
 void BrowserAccessibilityManager::OnNodeCreated(ui::AXTree* tree,
                                                 ui::AXNode* node) {
+  DCHECK(node);
   BrowserAccessibility* wrapper = factory_->Create();
-  wrapper->Init(this, node);
   id_wrapper_map_[node->id()] = wrapper;
+  wrapper->Init(this, node);
   wrapper->OnDataChanged();
 }
 
 void BrowserAccessibilityManager::OnNodeReparented(ui::AXTree* tree,
                                                    ui::AXNode* node) {
-  BrowserAccessibility* wrapper = GetFromID(node->id());
+  DCHECK(node);
+  BrowserAccessibility* wrapper = GetFromAXNode(node);
   if (!wrapper) {
     wrapper = factory_->Create();
     id_wrapper_map_[node->id()] = wrapper;
   }
-
   wrapper->Init(this, node);
   wrapper->OnDataChanged();
 }
@@ -1188,7 +1196,8 @@ void BrowserAccessibilityManager::OnNodeReparented(ui::AXTree* tree,
 void BrowserAccessibilityManager::OnNodeChanged(ui::AXTree* tree,
                                                 ui::AXNode* node) {
   DCHECK(node);
-  GetFromAXNode(node)->OnDataChanged();
+  if (BrowserAccessibility* wrapper = GetFromAXNode(node))
+    wrapper->OnDataChanged();
 }
 
 void BrowserAccessibilityManager::OnAtomicUpdateFinished(

content/browser/accessibility/browser_accessibility_manager.h
@@ -156,6 +156,9 @@ class CONTENT_EXPORT BrowserAccessibilityManager : public ui::AXTreeObserver,
   // the frame tree has focus. If focus has changed, calls FireFocusEvent.
   void FireFocusEventsIfNeeded();
 
+  // Send the events triggered when a node lose focus.
+  virtual void OnFocusLost(BrowserAccessibility* node) {}
+
   // Return whether or not we are currently able to fire events.
   virtual bool CanFireEvents();
 

content/browser/accessibility/accessibility_event_recorder_uia_win.cc
@@ -128,13 +128,20 @@ void AccessibilityEventRecorderUia::Thread::ThreadMain() {
   uia_event_handler_->AddRef();
   uia_event_handler_->Init(this, root_);
 
+  // Create a cache request to avoid cross-thread issues when logging.
+  CHECK(SUCCEEDED(uia_->CreateCacheRequest(&cache_request_)));
+  CHECK(cache_request_.Get());
+  CHECK(SUCCEEDED(cache_request_->AddProperty(UIA_NamePropertyId)));
+  CHECK(SUCCEEDED(cache_request_->AddProperty(UIA_AriaRolePropertyId)));
+
   // Subscribe to the shutdown sentinel event
-  uia_->AddAutomationEventHandler(shutdown_sentinel_, root_.Get(),
-                                  TreeScope::TreeScope_Subtree, nullptr,
-                                  uia_event_handler_.Get());
+  uia_->AddAutomationEventHandler(
+      shutdown_sentinel_, root_.Get(), TreeScope::TreeScope_Subtree,
+      cache_request_.Get(), uia_event_handler_.Get());
 
   // Subscribe to focus events
-  uia_->AddFocusChangedEventHandler(nullptr, uia_event_handler_.Get());
+  uia_->AddFocusChangedEventHandler(cache_request_.Get(),
+                                    uia_event_handler_.Get());
 
   // Subscribe to all property-change events
   static const PROPERTYID kMinProp = UIA_RuntimeIdPropertyId;
@@ -142,16 +149,16 @@ void AccessibilityEventRecorderUia::Thread::ThreadMain() {
   std::array<PROPERTYID, (kMaxProp - kMinProp) + 1> property_list;
   std::iota(property_list.begin(), property_list.end(), kMinProp);
   uia_->AddPropertyChangedEventHandlerNativeArray(
-      root_.Get(), TreeScope::TreeScope_Subtree, nullptr,
+      root_.Get(), TreeScope::TreeScope_Subtree, cache_request_.Get(),
       uia_event_handler_.Get(), &property_list[0], property_list.size());
 
   // Subscribe to all automation events
   static const EVENTID kMinEvent = UIA_ToolTipOpenedEventId;
   static const EVENTID kMaxEvent = UIA_NotificationEventId;
   for (EVENTID event_id = kMinEvent; event_id <= kMaxEvent; ++event_id) {
-    uia_->AddAutomationEventHandler(event_id, root_.Get(),
-                                    TreeScope::TreeScope_Subtree, nullptr,
-                                    uia_event_handler_.Get());
+    uia_->AddAutomationEventHandler(
+        event_id, root_.Get(), TreeScope::TreeScope_Subtree,
+        cache_request_.Get(), uia_event_handler_.Get());
   }
 
   // Signal that initialization is complete; this will wake the main thread to
@@ -229,6 +236,7 @@ void AccessibilityEventRecorderUia::Thread::ThreadMain() {
   uia_->RemoveAllEventHandlers();
   uia_event_handler_->CleanUp();
   uia_event_handler_.Reset();
+  cache_request_.Reset();
   root_.Reset();
   uia_.Reset();
 
@@ -356,8 +364,8 @@ std::string AccessibilityEventRecorderUia::Thread::EventHandler::GetSenderInfo(
     }
   };
 
-  append_property("role", &IUIAutomationElement::get_CurrentAriaRole);
-  append_property("name", &IUIAutomationElement::get_CurrentName);
+  append_property("role", &IUIAutomationElement::get_CachedAriaRole);
+  append_property("name", &IUIAutomationElement::get_CachedName);
 
   if (!sender_info.empty())
     sender_info = "on " + sender_info;

content/browser/accessibility/accessibility_event_recorder_uia_win.h
@@ -66,6 +66,7 @@ class AccessibilityEventRecorderUia : public AccessibilityEventRecorder {
 
     Microsoft::WRL::ComPtr<IUIAutomation> uia_;
     Microsoft::WRL::ComPtr<IUIAutomationElement> root_;
+    Microsoft::WRL::ComPtr<IUIAutomationCacheRequest> cache_request_;
 
     // Thread synchronization members
     base::OnceClosure initialization_complete_;

content/browser/accessibility/browser_accessibility_manager_win.cc
@@ -89,6 +89,10 @@ void BrowserAccessibilityManagerWin::FireFocusEvent(
     BrowserAccessibility* node) {
   BrowserAccessibilityManager::FireFocusEvent(node);
   DCHECK(node);
+
+  if (node->GetRole() == ax::mojom::Role::kMenu)
+    FireUiaAccessibilityEvent(UIA_MenuOpenedEventId, node);
+
   FireWinAccessibilityEvent(EVENT_OBJECT_FOCUS, node);
   FireUiaAccessibilityEvent(UIA_AutomationFocusChangedEventId, node);
 }
@@ -304,6 +308,14 @@ void BrowserAccessibilityManagerWin::FireGeneratedEvent(
       // It's okay to skip them.
       break;
   }
+}
+
+void BrowserAccessibilityManagerWin::OnFocusLost(BrowserAccessibility* node) {
+  BrowserAccessibilityManager::OnFocusLost(node);
+  DCHECK(node);
+
+  if (node->GetRole() == ax::mojom::Role::kMenu)
+    FireUiaAccessibilityEvent(UIA_MenuClosedEventId, node);
 }
 
 void BrowserAccessibilityManagerWin::FireWinAccessibilityEvent(

content/browser/accessibility/browser_accessibility_manager_win.h
@@ -52,6 +52,8 @@ class CONTENT_EXPORT BrowserAccessibilityManagerWin
   void FireGeneratedEvent(ui::AXEventGenerator::Event event_type,
                           BrowserAccessibility* node) override;
 
+  void OnFocusLost(BrowserAccessibility* node) override;
+
   void FireWinAccessibilityEvent(LONG win_event, BrowserAccessibility* node);
   void FireUiaAccessibilityEvent(LONG uia_event, BrowserAccessibility* node);
   void FireUiaPropertyChangedEvent(LONG uia_property,

content/browser/accessibility/dump_accessibility_events_browsertest.cc
@@ -612,4 +612,9 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest, ValueValueChanged) {
   RunEventTest(FILE_PATH_LITERAL("value-value-changed.html"));
 }
 
+IN_PROC_BROWSER_TEST_F(DumpAccessibilityEventsTest,
+                       AccessibilityEventsMenuOpenedClosed) {
+  RunEventTest(FILE_PATH_LITERAL("menu-opened-closed.html"));
+}
+
 }  // namespace content

content/test/data/accessibility/event/menu-opened-closed-expected-uia-win.txt
@@ -1,0 +1,15 @@
+AutomationFocusChanged on role=menu, name=menu
+MenuOpened on role=menu, name=menu
+=== Start Continuation ===
+AutomationFocusChanged on role=menu, name=submenu1
+MenuClosed on role=menu, name=menu
+MenuOpened on role=menu, name=submenu1
+=== Start Continuation ===
+AutomationFocusChanged on role=document
+MenuClosed on role=menu, name=submenu1
+=== Start Continuation ===
+AutomationFocusChanged on role=menu, name=submenu2
+MenuOpened on role=menu, name=submenu2
+=== Start Continuation ===
+AutomationFocusChanged on role=menuitem, name=option
+MenuClosed on role=menu, name=submenu2

commit 3262f86c21f7ff1423ea90a004773306b9056a99

Fix crash in BrowserAccessibilityManagerWin::OnFocusLost
A bug was introduced in commit 147b31e4f19acd37d682cca968f75a6eb554b750 [formerly 68bc8e104b88b97805e6be932bf5a2056dec856d]
where in `BrowserAccessibilityManagerWin::OnFocusLost` we will try to
check the role of the last focused node in a tree in order to raise a
`UIA_MenuClosedEventId`, but by the time we query the role of such node,
it was already deleted from the accessibility tree, resulting in an
invalid access to a released `BrowserAccessibility`.

As pointed out by Dominic Mazzoni, the `last_focused_node_` pointer from
`BrowserAccessibilityManager` was only being used for comparison and was
not properly handled to be derefenced. As such, a check was introduced
in `BrowserAccessibilityManager::OnNodeWillBeDeleted` to clear the
pointer if the last focused node was destroyed.

This approach proved to be not enough to avoid invalid accesses. When
`OnNodeWillBeDeleted` is called, the subtree being deleted could be in
an "inconsistent state", meaning that it could be disconnected from the
root manager and thus unable to correctly clear its last focused node.

In order to fix such issue, this change removes the pointers as a mean
of comparison and introduces the use of the ids of the node and the tree
that contains such node, shared across all frames. Clearing the ids is
simple and when we need to dereference the `BrowserAccessibility` of the
last focused node, we recompute it from the ids, which guarantees its
validity if the node is still in a frame's tree.

Bug: 947129
Change-Id: I4a67930689f60b2d3543597dda7ac1c4679dad14
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1548574
Commit-Queue: Ethan Jimenez <ethavar@microsoft.com>
Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
Cr-Commit-Position: refs/heads/master@{#648993}
Former-commit-id: 46af21b15a1d8d4f05edf47d3ea7c1e3a14bd5c0

content/browser/accessibility/accessibility_action_browsertest.cc
@@ -643,4 +643,47 @@ IN_PROC_BROWSER_TEST_F(AccessibilityActionBrowserTest,
   EXPECT_NE(find_radio2, control_list.cend());
 }
 
+IN_PROC_BROWSER_TEST_F(AccessibilityActionBrowserTest, FocusLostOnDeletedNode) {
+  NavigateToURL(shell(), GURL(url::kAboutBlankURL));
+
+  GURL url(
+      "data:text/html,"
+      "<button id='1'>1</button>"
+      "<iframe id='iframe' srcdoc=\""
+      "<button id='2'>2</button>"
+      "\"></iframe>");
+
+  NavigateToURL(shell(), url);
+  EnableAccessibilityForWebContents(shell()->web_contents());
+
+  auto FocusNodeAndReload = [this, &url](const std::string& node_name,
+                                         const std::string& focus_node_script) {
+    WaitForAccessibilityTreeToContainNodeWithName(shell()->web_contents(),
+                                                  node_name);
+    BrowserAccessibility* node = FindNode(ax::mojom::Role::kButton, node_name);
+    ASSERT_NE(nullptr, node);
+
+    EXPECT_TRUE(ExecuteScript(shell(), focus_node_script));
+    WaitForAccessibilityFocusChange();
+
+    EXPECT_EQ(node->GetId(),
+              GetFocusedAccessibilityNodeInfo(shell()->web_contents()).id);
+
+    // Reloading the frames will achieve two things:
+    //   1. Force the deletion of the node being tested.
+    //   2. Lose focus on the node by focusing a new frame.
+    AccessibilityNotificationWaiter load_waiter(
+        shell()->web_contents(), ui::kAXModeComplete,
+        ax::mojom::Event::kLoadComplete);
+    NavigateToURL(shell(), url);
+    load_waiter.WaitForNotification();
+  };
+
+  FocusNodeAndReload("1", "document.getElementById('1').focus();");
+  FocusNodeAndReload("2",
+                     "var iframe = document.getElementById('iframe');"
+                     "var inner_doc = iframe.contentWindow.document;"
+                     "inner_doc.getElementById('2').focus();");
+}
+
 }  // namespace content

content/browser/accessibility/browser_accessibility_manager.cc
@@ -156,8 +156,6 @@ BrowserAccessibilityManager::BrowserAccessibilityManager(
       factory_(factory),
       tree_(new ui::AXSerializableTree()),
       user_is_navigating_away_(false),
-      last_focused_node_(nullptr),
-      last_focused_manager_(nullptr),
       connected_to_parent_tree_node_(false),
       ax_tree_id_(ui::AXTreeIDUnknown()),
       device_scale_factor_(1.0f),
@@ -174,8 +172,6 @@ BrowserAccessibilityManager::BrowserAccessibilityManager(
       factory_(factory),
       tree_(new ui::AXSerializableTree()),
       user_is_navigating_away_(false),
-      last_focused_node_(nullptr),
-      last_focused_manager_(nullptr),
       ax_tree_id_(ui::AXTreeIDUnknown()),
       device_scale_factor_(1.0f),
       use_custom_device_scale_factor_for_testing_(false),
@@ -214,6 +210,13 @@ bool BrowserAccessibilityManager::never_suppress_or_delay_events_for_testing_ =
     false;
 
 // static
+base::Optional<int32_t> BrowserAccessibilityManager::last_focused_node_id_ = {};
+
+// static
+base::Optional<ui::AXTreeID>
+    BrowserAccessibilityManager::last_focused_node_tree_id_ = {};
+
+// static
 ui::AXTreeUpdate BrowserAccessibilityManager::GetEmptyDocument() {
   ui::AXNodeData empty_document;
   empty_document.id = 0;
@@ -247,15 +250,14 @@ void BrowserAccessibilityManager::FireFocusEventsIfNeeded() {
     focus = nullptr;
   }
 
-  if (focus != last_focused_node_) {
-    if (last_focused_node_)
-      OnFocusLost(last_focused_node_);
+  BrowserAccessibility* last_focused_node = GetLastFocusedNode();
+  if (focus != last_focused_node) {
+    if (last_focused_node)
+      OnFocusLost(last_focused_node);
     if (focus)
       FireFocusEvent(focus);
   }
-
-  last_focused_node_ = focus;
-  last_focused_manager_ = focus ? focus->manager() : nullptr;
+  SetLastFocusedNode(focus);
 }
 
 bool BrowserAccessibilityManager::CanFireEvents() {
@@ -343,10 +345,8 @@ void BrowserAccessibilityManager::OnWindowFocused() {
 }
 
 void BrowserAccessibilityManager::OnWindowBlurred() {
-  if (IsRootTree()) {
-    last_focused_node_ = nullptr;
-    last_focused_manager_ = nullptr;
-  }
+  if (IsRootTree())
+    SetLastFocusedNode(nullptr);
 }
 
 void BrowserAccessibilityManager::UserIsNavigatingAway() {
@@ -402,7 +402,8 @@ void BrowserAccessibilityManager::OnAccessibilityEvents(
   }
 
   // If this page is hidden by an interstitial, suppress all events.
-  if (GetRootManager()->hidden_by_interstitial_page()) {
+  BrowserAccessibilityManager* root_manager = GetRootManager();
+  if (root_manager->hidden_by_interstitial_page()) {
     event_generator_.ClearEvents();
     return;
   }
@@ -425,7 +426,7 @@ void BrowserAccessibilityManager::OnAccessibilityEvents(
   // Screen readers might not do the right thing if they're not aware of what
   // has focus, so always try that first. Nothing will be fired if the window
   // itself isn't focused or if focus hasn't changed.
-  GetRootManager()->FireFocusEventsIfNeeded();
+  root_manager->FireFocusEventsIfNeeded();
 
   // Fire any events related to changes to the tree.
   for (auto targeted_event : event_generator_) {
@@ -457,7 +458,7 @@ void BrowserAccessibilityManager::OnAccessibilityEvents(
       continue;
 
     if (event.event_type == ax::mojom::Event::kHover)
-      GetRootManager()->CacheHitTestResult(event_target);
+      root_manager->CacheHitTestResult(event_target);
 
     FireBlinkEvent(event.event_type, event_target);
   }
@@ -1158,12 +1159,10 @@ void BrowserAccessibilityManager::OnNodeWillBeDeleted(ui::AXTree* tree,
                                                       ui::AXNode* node) {
   DCHECK(node);
   if (BrowserAccessibility* wrapper = GetFromAXNode(node)) {
-    if (wrapper == last_focused_node_) {
-      last_focused_node_ = nullptr;
-      last_focused_manager_ = nullptr;
-    }
-    wrapper->Destroy();
+    if (wrapper == GetLastFocusedNode())
+      SetLastFocusedNode(nullptr);
     id_wrapper_map_.erase(node->id());
+    wrapper->Destroy();
   }
 }
 
@@ -1206,6 +1205,12 @@ void BrowserAccessibilityManager::OnAtomicUpdateFinished(
     ui::AXTree* tree,
     bool root_changed,
     const std::vector<ui::AXTreeObserver::Change>& changes) {
+  // When the root changes and this is the root manager, we may need to
+  // fire a new focus event.
+  if (root_changed && last_focused_node_tree_id_ &&
+      ax_tree_id_ == last_focused_node_tree_id_.value())
+    SetLastFocusedNode(nullptr);
+
   bool ax_tree_id_changed = false;
   if (GetTreeData().tree_id != ui::AXTreeIDUnknown() &&
       GetTreeData().tree_id != ax_tree_id_) {
@@ -1220,13 +1225,6 @@ void BrowserAccessibilityManager::OnAtomicUpdateFinished(
   // we're properly connected.
   if (ax_tree_id_changed || root_changed)
     connected_to_parent_tree_node_ = false;
-
-  // When the root changes and this is the root manager, we may need to
-  // fire a new focus event.
-  if (root_changed && last_focused_manager_ == this) {
-    last_focused_node_ = nullptr;
-    last_focused_manager_ = nullptr;
-  }
 }
 
 ui::AXNode* BrowserAccessibilityManager::GetNodeFromTree(ui::AXTreeID tree_id,
@@ -1274,6 +1272,30 @@ BrowserAccessibilityManager::GetDelegateFromRootManager() {
 
 bool BrowserAccessibilityManager::IsRootTree() {
   return delegate()->AccessibilityIsMainFrame();
+}
+
+// static
+void BrowserAccessibilityManager::SetLastFocusedNode(
+    BrowserAccessibility* node) {
+  if (node) {
+    DCHECK(node->manager());
+    last_focused_node_id_ = node->GetId();
+    last_focused_node_tree_id_ = node->manager()->ax_tree_id();
+  } else {
+    last_focused_node_id_.reset();
+    last_focused_node_tree_id_.reset();
+  }
+}
+
+// static
+BrowserAccessibility* BrowserAccessibilityManager::GetLastFocusedNode() {
+  if (last_focused_node_id_) {
+    DCHECK(last_focused_node_tree_id_);
+    if (BrowserAccessibilityManager* last_focused_manager =
+            FromID(last_focused_node_tree_id_.value()))
+      return last_focused_manager->GetFromID(last_focused_node_id_.value());
+  }
+  return nullptr;
 }
 
 ui::AXTreeUpdate BrowserAccessibilityManager::SnapshotAXTreeForTesting() {

content/browser/accessibility/browser_accessibility_manager.h
@@ -431,6 +431,9 @@ class CONTENT_EXPORT BrowserAccessibilityManager : public ui::AXTreeObserver,
   virtual void SendLocationChangeEvents(
       const std::vector<AccessibilityHostMsg_LocationChangeParams>& params);
 
+  static void SetLastFocusedNode(BrowserAccessibility* node);
+  static BrowserAccessibility* GetLastFocusedNode();
+
  protected:
   // The object that can perform actions on our behalf.
   BrowserAccessibilityDelegate* delegate_;
@@ -452,16 +455,6 @@ class CONTENT_EXPORT BrowserAccessibilityManager : public ui::AXTreeObserver,
   bool hidden_by_interstitial_page_ = false;
 
   BrowserAccessibilityFindInPageInfo find_in_page_info_;
-
-  // These are only used by the root BrowserAccessibilityManager of a
-  // frame tree. Stores the last focused node and last focused manager so
-  // that when focus might have changed we can figure out whether we need
-  // to fire a focus event.
-  //
-  // NOTE: these pointers are not cleared, so they should never be
-  // dereferenced, only used for comparison.
-  BrowserAccessibility* last_focused_node_;
-  BrowserAccessibilityManager* last_focused_manager_;
 
   // These cache the AX tree ID, node ID, and global screen bounds of the
   // last object found by an asynchronous hit test. Subsequent hit test
@@ -495,6 +488,15 @@ class CONTENT_EXPORT BrowserAccessibilityManager : public ui::AXTreeObserver,
   // Fire all events regardless of focus and with no delay, to avoid test
   // flakiness. See NeverSuppressOrDelayEventsForTesting() for details.
   static bool never_suppress_or_delay_events_for_testing_;
+
+  // Stores the id of the last focused node across all frames, as well as the id
+  // of the tree that contains it, so that when focus might have changed we can
+  // figure out whether we need to fire a focus event.
+  //
+  // NOTE: Don't use or modify these properties directly, use the
+  // SetLastFocusedNode and GetLastFocusedNode methods instead.
+  static base::Optional<int32_t> last_focused_node_id_;
+  static base::Optional<ui::AXTreeID> last_focused_node_tree_id_;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(BrowserAccessibilityManager);