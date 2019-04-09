99a94d3ca3668ec62574f5288c6e8783fd2c15a2
Mike Reed <reed@google.com>
2019-04-09 20:49:47 +0000

    add flag to stage change to shader factories
    Bug: skia:8937
    Change-Id: I59be04380805a78b7a9e24721e4f4349797735f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1558685
    Commit-Queue: Mike Reed <reed@google.com>
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649254}
    Former-commit-id: e980ca505f330c64f33612490b7d4652f07e0831

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -154,6 +154,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_DISABLE_OPLIST_SORTING
 #endif
 
+#ifndef SK_SUPPORT_LEGACY_SHADER_FACTORIES
+#define SK_SUPPORT_LEGACY_SHADER_FACTORIES
+#endif
+
 #ifndef SK_IGNORE_LINEONLY_AA_CONVEX_PATH_OPTS
 #define SK_IGNORE_LINEONLY_AA_CONVEX_PATH_OPTS
 #endif
