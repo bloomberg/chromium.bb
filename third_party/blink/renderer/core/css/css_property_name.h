20edc77810005868667b9c9ddc45cd1b66d880ad
Anders Hartvoll Ruud <andruud@chromium.org>
2019-04-09 12:29:03 +0000

    [cascade] Provide HashTraits for CSSPropertyName.
    This is needed for the Cascade Project, which will use CSSPropertyName
    as the HashMap key.
    
    HashTraits requires the key object to support "empty" and "deleted"
    values. This is implemented by storing kEmptyValue and kDeletedValue
    in an integer which is shared with the CSSPropertyID.
    
    Constructing empty or deleted CSSPropertyNames is not advisable outside of
    HashTraits, hence the relevant constructor is exposed via friendship.
    (Inspired by PropertyHandle).
    
    BUG=947004
    R=futhark@chromium.org
    
    Change-Id: I48bfb3707f73f0cadd7696075fce622352f9dee0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553555
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649069}
    Former-commit-id: 4e41dd51aff6d508ae676a085417e796f27195a5

--- third_party/blink/renderer/core/css/css_property_name.h
+++ third_party/blink/renderer/core/css/css_property_name.h
@@ -16,16 +16,15 @@ namespace blink {
 // including custom properties.
 class CORE_EXPORT CSSPropertyName {
   DISALLOW_NEW();
-
  public:
   explicit CSSPropertyName(CSSPropertyID property_id)
-      : property_id_(property_id) {
-    DCHECK_NE(property_id, CSSPropertyID::kInvalid);
-    DCHECK_NE(property_id, CSSPropertyID::kVariable);
+      : value_(static_cast<int>(property_id)) {
+    DCHECK_NE(Id(), CSSPropertyID::kInvalid);
+    DCHECK_NE(Id(), CSSPropertyID::kVariable);
   }
 
   explicit CSSPropertyName(const AtomicString& custom_property_name)
-      : property_id_(CSSPropertyID::kVariable),
+      : value_(static_cast<int>(CSSPropertyID::kVariable)),
         custom_property_name_(custom_property_name) {
     DCHECK(!custom_property_name.IsNull());
   }
@@ -44,20 +43,78 @@ class CORE_EXPORT CSSPropertyName {
     return !(*this == other);
   }
 
-  CSSPropertyID Id() const { return property_id_; }
-
-  bool IsCustomProperty() const {
-    return property_id_ == CSSPropertyID::kVariable;
+  CSSPropertyID Id() const {
+    DCHECK(!IsEmptyValue() && !IsDeletedValue());
+    return static_cast<CSSPropertyID>(value_);
   }
+
+  bool IsCustomProperty() const { return Id() == CSSPropertyID::kVariable; }
 
   AtomicString ToAtomicString() const;
 
  private:
-  CSSPropertyID property_id_;
+  // For HashTraits::EmptyValue().
+  static constexpr int kEmptyValue = -1;
+  // For HashTraits::ConstructDeletedValue(...).
+  static constexpr int kDeletedValue = -2;
+
+  explicit CSSPropertyName(int value) : value_(value) {
+    DCHECK(value == kEmptyValue || value == kDeletedValue);
+  }
+
+  unsigned GetHash() const;
+  bool IsEmptyValue() const { return value_ == kEmptyValue; }
+  bool IsDeletedValue() const { return value_ == kDeletedValue; }
+
+  // The value_ field is either a CSSPropertyID, kEmptyValue, or
+  // kDeletedValue.
+  int value_;
   AtomicString custom_property_name_;
+
+  friend class CSSPropertyNameTest;
+  friend struct ::WTF::DefaultHash<blink::CSSPropertyName>;
+  friend struct ::WTF::HashTraits<blink::CSSPropertyName>;
 };
 
 }  // namespace blink
+
+namespace WTF {
+
+template <>
+struct DefaultHash<blink::CSSPropertyName> {
+  struct Hash {
+    STATIC_ONLY(Hash);
+    static unsigned GetHash(const blink::CSSPropertyName& name) {
+      return name.GetHash();
+    }
+
+    static bool Equal(const blink::CSSPropertyName& a,
+                      const blink::CSSPropertyName& b) {
+      return a == b;
+    }
+
+    static const bool safe_to_compare_to_empty_or_deleted = true;
+  };
+};
+
+template <>
+struct HashTraits<blink::CSSPropertyName>
+    : SimpleClassHashTraits<blink::CSSPropertyName> {
+  using CSSPropertyName = blink::CSSPropertyName;
+  static const bool kEmptyValueIsZero = false;
+  static const bool kNeedsDestruction = true;
+  static void ConstructDeletedValue(CSSPropertyName& slot, bool) {
+    new (NotNull, &slot) CSSPropertyName(CSSPropertyName::kDeletedValue);
+  }
+  static bool IsDeletedValue(CSSPropertyName value) {
+    return value.IsDeletedValue();
+  }
+  static blink::CSSPropertyName EmptyValue() {
+    return blink::CSSPropertyName(CSSPropertyName::kEmptyValue);
+  }
+};
+
+}  // namespace WTF
 
 WTF_ALLOW_MOVE_INIT_AND_COMPARE_WITH_MEM_FUNCTIONS(blink::CSSPropertyName)
 
