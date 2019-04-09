a19a1bbd6415d51d63c61873dbf23aed4007d4bc
Eugene But <eugenebut@google.com>
2019-04-09 22:22:05 +0000

    Disable HistoryStateOperationsTest.ReplaceStateThenReload only for slim-nav.
    The test is not failing if slim-nav is not enabled.
    
    Bug: 950263
    Change-Id: I71e226e6bc04dff6fa02553aeb69b202d9601007
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1560370
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649295}
    Former-commit-id: 1fd2b4c76ad7a69d43882cc783a498f0dbefb78d

--- ios/web/navigation/history_state_operations_inttest.mm
+++ ios/web/navigation/history_state_operations_inttest.mm
@@ -16,6 +16,7 @@
 #import "ios/web/public/test/http_server/http_server.h"
 #include "ios/web/public/test/http_server/http_server_util.h"
 #import "ios/web/public/test/web_view_interaction_test_util.h"
+#import "ios/web/public/web_client.h"
 #import "ios/web/public/web_state/web_state.h"
 #import "ios/web/test/web_int_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -475,8 +476,11 @@ TEST_P(HistoryStateOperationsTest, ReplaceStateNoHashChangeEvent) {
 }
 
 // Regression test for crbug.com/788464.
-// TODO(crbug.com/950263): Investigate culprit and reenable.
-TEST_P(HistoryStateOperationsTest, DISABLED_ReplaceStateThenReload) {
+TEST_P(HistoryStateOperationsTest, ReplaceStateThenReload) {
+  if (web::GetWebClient()->IsSlimNavigationManagerEnabled()) {
+    // TODO(crbug.com/950263): Investigate culprit and reenable.
+    return;
+  }
   GURL url = web::test::HttpServer::MakeUrl(
       "http://ios/testing/data/http_server_files/"
       "onload_replacestate_reload.html");
