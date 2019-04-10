4c2199b0d4573f28000ca2db81552fba135f8a88
Mikel Astiz <mastiz@chromium.org>
2019-04-10 19:55:13 +0000

    Decouple SESSIONS and DEVICE_INFO sync datatypes
    This simplifies the dependencies and the interactions across datatypes,
    which are quite fragile and rely on special-casing for DeviceInfo in
    ProfileSyncService (very early initialization via InitLocalCacheGuid()).
    
    Instead, let's make SESSIONS read the individual fields it needs (i.e.
    device name and type) directly from common utility libraries.
    
    Bug: 951051
    Change-Id: I99831ff5bc66003bc1142551f654b9fa387e4e28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1560151
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649612}
    Former-commit-id: 8680bb35e936f41cc40ee40c3da181e08ba72103

--- components/sync/device_info/local_device_info_provider_impl.cc
+++ components/sync/device_info/local_device_info_provider_impl.cc
@@ -5,33 +5,10 @@
 #include "components/sync/device_info/local_device_info_provider_impl.h"
 
 #include "base/bind.h"
-#include "build/build_config.h"
+#include "components/sync/device_info/local_device_info_util.h"
 #include "components/sync/driver/sync_util.h"
-#include "ui/base/device_form_factor.h"
 
 namespace syncer {
-
-namespace {
-
-sync_pb::SyncEnums::DeviceType GetLocalDeviceType() {
-#if defined(OS_CHROMEOS)
-  return sync_pb::SyncEnums_DeviceType_TYPE_CROS;
-#elif defined(OS_LINUX)
-  return sync_pb::SyncEnums_DeviceType_TYPE_LINUX;
-#elif defined(OS_ANDROID) || defined(OS_IOS)
-  return ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_TABLET
-             ? sync_pb::SyncEnums_DeviceType_TYPE_TABLET
-             : sync_pb::SyncEnums_DeviceType_TYPE_PHONE;
-#elif defined(OS_MACOSX)
-  return sync_pb::SyncEnums_DeviceType_TYPE_MAC;
-#elif defined(OS_WIN)
-  return sync_pb::SyncEnums_DeviceType_TYPE_WIN;
-#else
-  return sync_pb::SyncEnums_DeviceType_TYPE_OTHER;
-#endif
-}
-
-}  // namespace
 
 LocalDeviceInfoProviderImpl::LocalDeviceInfoProviderImpl(
     version_info::Channel channel,
