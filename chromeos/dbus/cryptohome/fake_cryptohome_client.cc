d3c1c354d91a1bd276ed595cfd420943dbef408c
Collin Baker <collinbaker@chromium.org>
2019-04-10 00:02:20 +0000

    Revert "Reland "Increase coverage for EULA browser tests""
    This reverts commit 9fa0602830bf682834fc9e0c27cd5b6341e65821 [formerly c3e3793b3e7b8ac42b35aea22ce28f69208132c1].
    
    Reason for revert: Still causing test failures: https://ci.chromium.org/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/32641?blamelist=1#blamelist-tab
    
    Original change's description:
    > Reland "Increase coverage for EULA browser tests"
    > 
    > This is a reland of d4ebabbcc72472e3ae0475a69e61d468b72b5b77 [formerly 426df2bedfa0c4c6bf9bcccaef6caf981c9a426c]
    > 
    > Original change's description:
    > > Increase coverage for EULA browser tests
    > >
    > > Adds tests for:
    > > * Clicking on "System security settings" button in EULA screen
    > > * Accepting and rejecting usage stats collection
    > > * Clicking on "Learn more" button in EULA screen
    > >
    > > Removes dead code from EulaScreen class.
    > >
    > > Adds a HelpApp extension manifest to use in tests and modifies
    > > HelpAppLauncher class to allow tests to set the extension id.
    > >
    > > Adds a HelpAppTestHelper class to facilitate writing tests that
    > > open HelpApp dialogs.
    > >
    > > Change-Id: Id7841be5bf3faabb067d3a75d3b034bdf0e00e32
    > > Reviewed-on:
    > https://chromium-review.googlesource.com/c/chromium/src/+/1554123
    > > Commit-Queue: Tony De Luna <tonydeluna@chromium.org>
    > > Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    > > Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#648759}
    > 
    > TBR=hashimoto
    > 
    > Bug: 947358
    > Change-Id: I23865c1957f300f55812f3275e830772aa27c32d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1560146
    > Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    > Commit-Queue: Tony De Luna <tonydeluna@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#649258}
    
    TBR=hashimoto@chromium.org,tbarzic@chromium.org,alemate@chromium.org,tonydeluna@chromium.org
    
    Change-Id: Id84e11a36c78c355cac699a1d9a4c48c5863659d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 947358
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1560493
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649326}
    Former-commit-id: 70f81ba53d2d88af0a11609c88f1fb5c4a79a706

--- chromeos/dbus/cryptohome/fake_cryptohome_client.cc
+++ chromeos/dbus/cryptohome/fake_cryptohome_client.cc
@@ -47,9 +47,6 @@ FakeCryptohomeClient* g_instance = nullptr;
 
 }  // namespace
 
-// static
-constexpr char FakeCryptohomeClient::kStubTpmPassword[] = "Stub-TPM-password";
-
 FakeCryptohomeClient::FakeCryptohomeClient()
     : service_is_available_(true),
       remove_firmware_management_parameters_from_tpm_call_count_(0),
@@ -187,6 +184,7 @@ bool FakeCryptohomeClient::CallTpmIsEnabledAndBlock(bool* enabled) {
 
 void FakeCryptohomeClient::TpmGetPassword(
     DBusMethodCallback<std::string> callback) {
+  constexpr char kStubTpmPassword[] = "Stub-TPM-password";
   base::ThreadTaskRunnerHandle::Get()->PostTask(
       FROM_HERE,
       base::BindOnce(std::move(callback), std::string(kStubTpmPassword)));
