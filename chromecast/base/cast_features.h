a5c5af7e15f9e38799eed12125e3f58016943797
Mao Huang <littlecvr@google.com>
2019-04-10 07:54:18 +0000

    Implement general audience browsing feature
    This feature blocks inappropriate content (such as adult content) to
    make the browsing experience suitable for general audience.
    
    Bug: b/110951484
    Test: Manually tested with
        '--enable-features=enable_general_audience_browsing' flag
    
    Change-Id: Iaba478ab353e6961de7106119d9fdd844cccf6c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1534588
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Drew Wilson <atwilson@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Mao Huang <littlecvr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649438}
    Former-commit-id: db20baed897e18683f50c7c18bef3f576782f92f

--- chromecast/base/cast_features.h
+++ chromecast/base/cast_features.h
@@ -28,6 +28,7 @@ extern const base::Feature kEnableQuic;
 extern const base::Feature kTripleBuffer720;
 extern const base::Feature kSingleBuffer;
 extern const base::Feature kDisableIdleSocketsCloseOnMemoryPressure;
+extern const base::Feature kEnableGeneralAudienceBrowsing;
 
 // Get an iterable list of all of the cast features for checking all features as
 // a collection.
