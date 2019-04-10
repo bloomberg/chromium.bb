8610b11d86b65a90a1a67f93f47fe0ee30147bb9
Hirokazu Honda <hiroh@chromium.org>
2019-04-10 07:00:04 +0000

    media/gpu: Move VideoFrameMappers to a production directory
    For LibyuvIP to support DMABUF input, it is necessary to map DMABUF-backed video
    frame inside LibyuvIP. We have VideoFrameMapper class which has been used in
    test code. We should make use of the classes to reduce code duplication.
    
    Bug: 944822
    Test: Build chrome and video tests
    Change-Id: I6f90ab9a9f515694153da5499f45529ebe2d22a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1538102
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: David Staessens <dstaessens@chromium.org>
    Reviewed-by: Alexandre Courbot <acourbot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649426}
    Former-commit-id: 1fcc09e2cf639602bd86fca33c838a2843b45b1f

--- media/gpu/video_frame_mapper_factory.h
+++ media/gpu/video_frame_mapper_factory.h
@@ -2,19 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef MEDIA_GPU_TEST_VIDEO_FRAME_MAPPER_FACTORY_H_
-#define MEDIA_GPU_TEST_VIDEO_FRAME_MAPPER_FACTORY_H_
+#ifndef MEDIA_GPU_VIDEO_FRAME_MAPPER_FACTORY_H_
+#define MEDIA_GPU_VIDEO_FRAME_MAPPER_FACTORY_H_
 
 #include <memory>
 
-#include "media/gpu/test/video_frame_mapper.h"
+#include "media/gpu/media_gpu_export.h"
+#include "media/gpu/video_frame_mapper.h"
 
 namespace media {
-namespace test {
 
 // A factory function for VideoFrameMapper.
 // The appropriate VideoFrameMapper is a platform-dependent.
-class VideoFrameMapperFactory {
+class MEDIA_GPU_EXPORT VideoFrameMapperFactory {
  public:
   // Create an instance of the frame mapper.
   static std::unique_ptr<VideoFrameMapper> CreateMapper();
@@ -25,7 +25,6 @@ class VideoFrameMapperFactory {
       bool force_linear_buffer_mapper);
 };
 
-}  // namespace test
 }  // namespace media
 
-#endif  // MEDIA_GPU_TEST_VIDEO_FRAME_MAPPER_FACTORY_H_
+#endif  // MEDIA_GPU_VIDEO_FRAME_MAPPER_FACTORY_H_
