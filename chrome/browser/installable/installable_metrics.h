f3838e55f2a1b67cccb0de834bec716d706fefe3
Alan Cutter <alancutter@chromium.org>
2019-04-10 11:05:16 +0000

    Record PWA installation from the omnibox icon
    Bug: 907351
    Change-Id: I6560402bd1dfdbb0a0a1f8296a3b40225cdcf56b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553131
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649462}
    Former-commit-id: 04efbc7b3ddfe31aff96ac48db27f0227ae7f3a1

--- chrome/browser/installable/installable_metrics.h
+++ chrome/browser/installable/installable_metrics.h
@@ -93,6 +93,9 @@ enum class WebappInstallSource {
   // A system app installed on Chrome OS.
   SYSTEM_DEFAULT = 14,
 
+  // Install icon in the Omnibox.
+  OMNIBOX_INSTALL_ICON = 15,
+
   // Add any new values above this one.
   COUNT,
 };
