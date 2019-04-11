2db81b17acd79edb5696a50e882ee4c07416466a
Tina Wang <tinazwang@chromium.org>
2019-04-11 22:02:54 +0000

    [SendTabToSelf] Fix the issue of showing notification on Windows
    Previously the SendTabToSelf on Window can't receive SendTabToSelf notifications.
    This CL add the send tab to self handler type to windows notification.
    
    Bug: 951640, 947515
    Change-Id: I1581f215ffc6af05db0e17fdef5d4b39b7d90693
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1546364
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650079}
    Former-commit-id: 7c155ffb6060e5b0ae3796dccb562bc0b12212fd

--- chrome/browser/send_tab_to_self/receiving_ui_handler_registry.cc
+++ chrome/browser/send_tab_to_self/receiving_ui_handler_registry.cc
@@ -11,7 +11,7 @@
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/send_tab_to_self/receiving_ui_handler.h"
 
-#if defined(OS_LINUX) || defined(OS_MACOSX)
+#if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_WIN)
 #include "chrome/browser/send_tab_to_self/desktop_notification_handler.h"
 #endif
 
@@ -32,7 +32,7 @@ ReceivingUiHandlerRegistry* ReceivingUiHandlerRegistry::GetInstance() {
 // Instantiates all the handlers relevant to this platform.
 void ReceivingUiHandlerRegistry::InstantiatePlatformSpecificHandlers(
     Profile* profile) {
-#if defined(OS_LINUX) || defined(OS_MACOSX)
+#if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_WIN)
   applicable_handlers_.push_back(
       std::make_unique<send_tab_to_self::DesktopNotificationHandler>(profile));
 #elif defined(OS_ANDROID)
