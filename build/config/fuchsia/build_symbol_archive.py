851af68ea292a363fff570fd4656071fb77ae0d4
Kevin Marshall <kmarshall@chromium.org>
2019-04-11 23:45:05 +0000

    [Fuchsia] Include SDK prebuilt symbols in generated symbol packages.
    Allows the Fuchsia stacktrace symbolizer to symbolize entries from
    *all* binaries, not just ones built from the Chromium tree.
    
    Bug: 926616
    Change-Id: Id835352d42b650c48941d41d00aba962ed026074
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1562629
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650124}
    Former-commit-id: 0dfb3859e0db1a48617ab920ed9a59c56ade567d

--- build/config/fuchsia/build_symbol_archive.py
+++ build/config/fuchsia/build_symbol_archive.py
@@ -18,16 +18,16 @@ import tempfile
 def main(args):
   parser = argparse.ArgumentParser()
   parser.add_argument('ids_txt', type=str, nargs=1,
-                      help='Path to the ids.txt file.')
-  parser.add_argument('output_tarball', nargs=1, type=str,
+                      help='Path to ids.txt files.')
+  parser.add_argument('-o', '--output_tarball', nargs=1, type=str,
                       help='Path which the tarball will be written to.')
   parser.add_argument('--eu-strip', required=True, type=str,
-                      help='Path to the the eu-strip tool.')
+                      help='Path to the eu-strip tool.')
+  parser.add_argument('--fuchsia-build-id-dir', type=str, required=True,
+                      help='Directory containing symbols for SDK prebuilts.')
   args = parser.parse_args(args)
 
-
   stripped_tempfile = tempfile.NamedTemporaryFile()
-
   ids_txt = args.ids_txt[0]
   build_ids_archive = tarfile.open(args.output_tarball[0], 'w:bz2')
   for line in open(ids_txt, 'r'):
@@ -37,29 +37,39 @@ def main(args):
     with tempfile.NamedTemporaryFile() as debug_tempfile, \
          tempfile.NamedTemporaryFile() as stripped_tempfile:
       build_id, binary_path = line.strip().split(' ')
-      binary_abspath = os.path.abspath(
-          os.path.join(os.path.dirname(ids_txt), binary_path))
 
-      # Extract debugging symbols from the binary into their own file.
-      # The stripped executable binary is written to |debug_tempfile| and
-      # deleted. Writing to /dev/null would be preferable, but eu-strip
-      # disallows writing output to /dev/null.
-      subprocess.check_call([args.eu_strip, '-g', binary_abspath,
-                             '-f', debug_tempfile.name,
-                             '-o', stripped_tempfile.name])
+      # Look for prebuilt symbols in the SDK first.
+      symbol_source_path = os.path.join(args.fuchsia_build_id_dir,
+                                        build_id[:2],
+                                        build_id[2:] + '.debug')
+      if not os.path.exists(symbol_source_path):
+        binary_abspath = os.path.abspath(
+            os.path.join(os.path.dirname(ids_txt), binary_path))
 
-      # An empty result means that the source binary (most likely a prebuilt)
-      # didn't have debugging data to begin with.
-      if os.path.getsize(debug_tempfile.name) == 0:
-        continue
+        # Extract debugging symbols from the binary into their own file.
+        # The stripped executable binary is written to |debug_tempfile| and
+        # deleted. Writing to /dev/null would be preferable, but eu-strip
+        # disallows writing output to /dev/null.
+        subprocess.check_call([args.eu_strip, '-g', binary_abspath,
+                               '-f', debug_tempfile.name,
+                               '-o', stripped_tempfile.name])
+
+        if os.path.getsize(debug_tempfile.name) == 0:
+          # This is a prebuilt which wasn't accompanied by SDK symbols.
+          continue
+
+        symbol_source_path = debug_tempfile.name
+
 
       # Archive the debugging symbols, placing them in a hierarchy keyed to the
       # GNU build ID. The symbols reside in directories whose names are the
       # first two characters of the build ID, with the symbol files themselves
       # named after the remaining characters of the build ID. So, a symbol file
       # with the build ID "deadbeef" would be located at the path 'de/adbeef'.
-      build_ids_archive.add(debug_tempfile.name,
+      build_ids_archive.add(symbol_source_path,
                             '%s/%s' % (build_id[:2], build_id[2:]))
+
+  return 0
 
 
 if __name__ == '__main__':
