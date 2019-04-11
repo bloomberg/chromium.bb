c2f1d9c6d7cac0c77776b19f9b286cc36156a0d8
Tim Volodine <timvolodine@chromium.org>
2019-04-11 21:25:26 +0000

    [AW NS] Make sure to execute onReceivedError callback for redirect errors and add new test.
    This patch makes sure to execute onReceivedError callback for redirect errors
    such as net::ERR_UNSAFE_REDIRECT and net::ERR_TOO_MANY_REDIRECTS. It also
    executes callback for 304 (net::HTTP_NOT_MODIFIED) responses (with
    net::ERR_ABORTED).
    
    This has also the effect that onPageFinished callback is properly called in such
    cases. This is further enforced by the added test (see below), which attempts
    to redirect to a file url.
    
    The approach in this patch uses a "side-channel" technique, where the
    NavigationURLLoader calls CancelWithError instead of OnComplete so that if there
    is an intercepting URLLoaderFactory (created through embedder's
    ContentBrowserClient::WillCreateURLLoaderFactory) it will get notified. In this
    case this is the AwProxyingURLLoaderFactory, which parses the error_code from
    the description string (in OnURLLoaderError method) and makes sure to invoke the
    error callback.
    
    Added test:
    ClientOnReceivedError2Test#testUnsafeRedirect_FileUrl
    
    BUG=944881,945862,841556
    
    Cq-Include-Trybots: luci.chromium.try:android_mojo
    
    Change-Id: Ia1d138d404000237b9af5f14c2c6ab20073b2233
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1536059
    Commit-Queue: Tim Volodine <timvolodine@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650051}
    Former-commit-id: 5a0dd4cf3c7bc49b92e57b6d7e56adef2fa9b78a

--- content/common/throttling_url_loader.h
+++ content/common/throttling_url_loader.h
@@ -73,6 +73,8 @@ class CONTENT_EXPORT ThrottlingURLLoader
   // datapipe endpoints.
   network::mojom::URLLoaderClientEndpointsPtr Unbind();
 
+  void CancelWithError(int error_code, base::StringPiece custom_reason);
+
   // Sets the forwarding client to receive all subsequent notifications.
   void set_forwarding_client(network::mojom::URLLoaderClient* client) {
     forwarding_client_ = client;
@@ -130,7 +132,6 @@ class CONTENT_EXPORT ThrottlingURLLoader
 
   void OnClientConnectionError();
 
-  void CancelWithError(int error_code, base::StringPiece custom_reason);
   void Resume();
   void SetPriority(net::RequestPriority priority);
   void UpdateDeferredRequestHeaders(
