a2b1d754558c4e0b6dab30866bdb60e80b1c8795
Andreea Costinas <acostinas@google.com>
2019-04-11 12:44:42 +0200

    system_api: add constants/vm_tools.h
    Add constant to system_api for the user that runs VMs on Chrome OS
    since it will be used in multiple projects.
    
    BUG=chromium:834585
    TEST=none
    
    Change-Id: I9617e7757b60b05e1c79b9040a5535b282f9c458
    Reviewed-on: https://chromium-review.googlesource.com/1564452
    Commit-Ready: Andreea-Elena Costinas <acostinas@google.com>
    Tested-by: Andreea-Elena Costinas <acostinas@google.com>
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Stephen Barber <smbarber@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Mirrored-From: https://chromium.googlesource.com/chromiumos/platform2
    Cr-Mirrored-Commit: 6d8b6b76959965854ac575f2f878b908d8afec78

---
+++ constants/vm_tools.h
@@ -0,0 +1,15 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SYSTEM_API_CONSTANTS_VM_TOOLS_H_
+#define SYSTEM_API_CONSTANTS_VM_TOOLS_H_
+
+namespace vm_tools {
+
+// Name of the user that runs unstrusted operating systems on Chrome OS.
+constexpr char kCrosVmUser[] = "crosvm";
+
+}  // namespace vm_tools
+
+#endif  // SYSTEM_API_CONSTANTS_VM_TOOLS_H_
