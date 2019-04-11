751b660cd09a46a44f4f76a802fdbce78ec1ef41
Lutz Justen <ljusten@chromium.org>
2019-04-11 10:28:43 +0000

    Add KerberosCredentialsManager
    The KerberosCredentialsManager is a layer on top of the Kerberos daemon
    D-Bus client (KerberosClient). It should be used to add/remove accounts,
    set configuration and authenticate. It makes sure that Kerberos tickets
    get put into a place where GSSAPI finds them, so Kerberos web
    authentication works.
    
    BUG=chromium:933798
    TEST=Ran unit tests and ExistingUserControllerActiveDirectoryTest.*
          after temporarily enable disabled tests.
    
    Change-Id: I4287c269677322436f6809d07cc14105390397b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1511397
    Commit-Queue: Lutz Justen <ljusten@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649826}
    Former-commit-id: b2ebecac80acd9fba74578c47d226225aea54ae6

--- chrome/browser/chromeos/authpolicy/kerberos_files_handler.cc
+++ chrome/browser/chromeos/authpolicy/kerberos_files_handler.cc
@@ -26,8 +26,14 @@ namespace chromeos {
 
 namespace {
 
+base::FilePath GetKerberosDir() {
+  base::FilePath dir;
+  base::PathService::Get(base::DIR_HOME, &dir);
+  return dir.Append(kKrb5Directory);
+}
+
 // Writes |blob| into file <UserPath>/kerberos/|file_name|. First writes into
-// temporary file and then replaces existing one.
+// temporary file and then replaces existing one. Prints an error or failure.
 void WriteFile(const base::FilePath& path, base::Optional<std::string> blob) {
   if (!blob.has_value())
     return;
@@ -35,13 +41,17 @@ void WriteFile(const base::FilePath& path, base::Optional<std::string> blob) {
     LOG(ERROR) << "Failed to write file " << path.value();
 }
 
+// Deletes file at |path|. Prints an error or failure.
+void RemoveFile(const base::FilePath& path) {
+  if (!base::DeleteFile(path, false /* recursive */))
+    LOG(ERROR) << "Failed to delete file " << path.value();
+}
+
 // Writes |krb5cc| to <DIR_HOME>/kerberos/krb5cc and |krb5config| to
 // <DIR_HOME>/kerberos/krb5.conf if set. Creates directories if necessary.
 void WriteFiles(base::Optional<std::string> krb5cc,
                 base::Optional<std::string> krb5config) {
-  base::FilePath dir;
-  base::PathService::Get(base::DIR_HOME, &dir);
-  dir = dir.Append(kKrb5Directory);
+  base::FilePath dir = GetKerberosDir();
   base::File::Error error;
   if (!base::CreateDirectoryAndGetError(dir, &error)) {
     LOG(ERROR) << "Failed to create '" << dir.value()
@@ -51,6 +61,13 @@ void WriteFiles(base::Optional<std::string> krb5cc,
 
   WriteFile(dir.Append(kKrb5CCFile), std::move(krb5cc));
   WriteFile(dir.Append(kKrb5ConfFile), std::move(krb5config));
+}
+
+// Deletes <DIR_HOME>/kerberos/krb5cc and <DIR_HOME>/kerberos/krb5.conf.
+void RemoveFiles() {
+  base::FilePath dir = GetKerberosDir();
+  RemoveFile(dir.Append(kKrb5CCFile));
+  RemoveFile(dir.Append(kKrb5ConfFile));
 }
 
 // If |config| has a value, puts canonicalization settings first depending on
@@ -129,11 +146,22 @@ void KerberosFilesHandler::SetFiles(base::Optional<std::string> krb5cc,
                      weak_factory_.GetWeakPtr()));
 }
 
+void KerberosFilesHandler::DeleteFiles() {
+  // These files contain user credentials, so use BLOCK_SHUTDOWN here to make
+  // sure they do get deleted.
+  base::PostTaskWithTraitsAndReply(
+      FROM_HERE,
+      {base::MayBlock(), base::TaskPriority::BEST_EFFORT,
+       base::TaskShutdownBehavior::BLOCK_SHUTDOWN},
+      base::BindOnce(&RemoveFiles),
+      base::BindOnce(&KerberosFilesHandler::OnFilesChanged,
+                     weak_factory_.GetWeakPtr()));
+}
+
 void KerberosFilesHandler::SetFilesChangedForTesting(
     base::OnceClosure callback) {
   files_changed_for_testing_ = std::move(callback);
 }
-
 void KerberosFilesHandler::OnDisabledAuthNegotiateCnameLookupChanged() {
   // Refresh kerberos files to adjust config for changed pref.
   get_kerberos_files_.Run();
