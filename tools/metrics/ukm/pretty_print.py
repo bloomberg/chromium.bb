e6181c2f89faa6beb6a39205b73b13774c2608f8
Elly Fong-Jones <ellyjones@chromium.org>
2019-04-11 14:55:17 +0000

    Revert "Specify relative order of nodes in ukm.xml"
    This reverts commit 916f6b8c8d8fe560446fc1bbc3a9f74cee8e16e0 [formerly 2657e7df8f42b10e8dd0e66ea3fd9bc98e28c52c].
    
    Reason for revert: broke linux-rel:
    
    FAILED: gen/services/metrics/public/cpp/ukm_builders.cc gen/services/metrics/public/cpp/ukm_builders.h gen/services/metrics/public/cpp/ukm_decode.cc gen/services/metrics/public/cpp/ukm_decode.h 
    python ../../tools/metrics/ukm/gen_builders.py --input ../../tools/metrics/ukm/ukm.xml --output gen/services/metrics/public/cpp
    Traceback (most recent call last):
      File "../../tools/metrics/ukm/gen_builders.py", line 35, in <module>
        sys.exit(main(sys.argv))
      File "../../tools/metrics/ukm/gen_builders.py", line 29, in main
        builders_template.WriteFiles(args.output, relpath, data)
      File "/b/swarming/w/ir/cache/builder/src/tools/metrics/ukm/builders_template.py", line 93, in WriteFiles
        HEADER.WriteFile(outdir, relpath, data)
      File "/b/swarming/w/ir/cache/builder/src/tools/metrics/ukm/codegen.py", line 84, in WriteFile
        output.write(self._StampFileCode(relpath, data))
      File "/b/swarming/w/ir/cache/builder/src/tools/metrics/ukm/codegen.py", line 70, in _StampFileCode
        for event in data['events'])
    KeyError: 'events'
    [24366/72009] ACTION //services/metric
    
    https://ci.chromium.org/p/chromium/builders/ci/linux-rel/23405
    
    Original change's description:
    > Specify relative order of nodes in ukm.xml
    > 
    > For each ObjectNodeType, specify alphabetization rules for all its child nodes types, which enforce their order relative to other types when pretty printed (sorting key is checked here: https://cs.chromium.org/chromium/src/tools/metrics/common/pretty_print_xml.py?q=_TransformByAlphabetizing&g=0&l=143). This patch fixes the issue where the pretty print script needs to be run more than once before the XML is sufficiently pretty for presubmit check.
    > 
    > 
    > Bug: 801614
    > Change-Id: I7e9076c8e55d300f346bde07db0b12d4c7533661
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1561610
    > Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    > Commit-Queue: Yue Ru Sun <yrsun@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#649863}
    
    TBR=rkaplow@chromium.org,yrsun@chromium.org
    
    Change-Id: I0a26eea9b2cbc8e25bb8200ef76d3a4ab4e77587
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 801614
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1564653
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#649869}
    Former-commit-id: 4e0430f076b0ac8ca903d4a2c0fc5043dea03102

--- tools/metrics/ukm/pretty_print.py
+++ tools/metrics/ukm/pretty_print.py
@@ -14,7 +14,7 @@ import presubmit_util
 
 def main(argv):
   presubmit_util.DoPresubmitMain(argv, 'ukm.xml', 'ukm.old.xml',
-                                 'pretty_print.py', ukm_model.PrettifyXML)
+                                 'pretty_print.py', ukm_model.UpdateXML)
 
 
 if '__main__' == __name__:
--- tools/metrics/ukm/ukm_model.py
+++ tools/metrics/ukm/ukm_model.py
@@ -14,11 +14,7 @@ _OBSOLETE_TYPE = models.TextNodeType('obsolete')
 _OWNER_TYPE = models.TextNodeType('owner', single_line=True)
 _SUMMARY_TYPE = models.TextNodeType('summary')
 
-# A key for sorting XML nodes by the value of |attribute|.
-_LOWERCASE_FN = lambda attribute: (lambda node: node.get(attribute).lower())
-# A constant function as the sorting key for nodes whose orderings should be
-# kept as given in the XML file within their parent node.
-_KEEP_ORDER = lambda node: 1
+_LOWERCASE_NAME_FN = lambda n: n.get('name').lower()
 
 _ENUMERATION_TYPE = models.ObjectNodeType(
     'enumeration',
@@ -46,28 +42,23 @@ _STATISTICS_TYPE =  models.ObjectNodeType(
     'statistics',
     attributes=[],
     children=[
-        models.ChildType(_QUANTILES_TYPE.tag, _QUANTILES_TYPE, multiple=False),
-        models.ChildType(
-            _ENUMERATION_TYPE.tag, _ENUMERATION_TYPE, multiple=False),
+        models.ChildType('quantiles', _QUANTILES_TYPE, multiple=False),
+        models.ChildType('enumeration', _ENUMERATION_TYPE, multiple=False),
     ])
 
 _HISTORY_TYPE =  models.ObjectNodeType(
     'history',
     attributes=[],
-    alphabetization=[
-        (_INDEX_TYPE.tag, _LOWERCASE_FN('fields')),
-        (_STATISTICS_TYPE.tag, _KEEP_ORDER),
-    ],
     children=[
-        models.ChildType(_INDEX_TYPE.tag, _INDEX_TYPE, multiple=True),
-        models.ChildType(_STATISTICS_TYPE.tag, _STATISTICS_TYPE, multiple=True),
+        models.ChildType('index', _INDEX_TYPE, multiple=True),
+        models.ChildType('statistics', _STATISTICS_TYPE, multiple=True),
     ])
 
 _AGGREGATION_TYPE =  models.ObjectNodeType(
     'aggregation',
     attributes=[],
     children=[
-        models.ChildType(_HISTORY_TYPE.tag, _HISTORY_TYPE, multiple=False),
+        models.ChildType('history', _HISTORY_TYPE, multiple=False),
     ])
 
 _METRIC_TYPE =  models.ObjectNodeType(
@@ -76,52 +67,40 @@ _METRIC_TYPE =  models.ObjectNodeType(
       ('name', unicode, r'^[A-Za-z0-9_.]+$'),
       ('semantic_type', unicode, None),
     ],
-    alphabetization=[
-        (_OBSOLETE_TYPE.tag, _KEEP_ORDER),
-        (_OWNER_TYPE.tag, _KEEP_ORDER),
-        (_SUMMARY_TYPE.tag, _KEEP_ORDER),
-        (_AGGREGATION_TYPE.tag, _KEEP_ORDER),
-    ],
     children=[
-        models.ChildType(_OBSOLETE_TYPE.tag, _OBSOLETE_TYPE, multiple=False),
-        models.ChildType(_OWNER_TYPE.tag, _OWNER_TYPE, multiple=True),
-        models.ChildType(_SUMMARY_TYPE.tag, _SUMMARY_TYPE, multiple=False),
-        models.ChildType(
-            _AGGREGATION_TYPE.tag, _AGGREGATION_TYPE, multiple=True),
+        models.ChildType('obsolete', _OBSOLETE_TYPE, multiple=False),
+        models.ChildType('owners', _OWNER_TYPE, multiple=True),
+        models.ChildType('summary', _SUMMARY_TYPE, multiple=False),
+        models.ChildType('aggregation', _AGGREGATION_TYPE, multiple=True),
     ])
 
 _EVENT_TYPE =  models.ObjectNodeType(
     'event',
+    alphabetization=[('metric', _LOWERCASE_NAME_FN)],
     attributes=[
       ('name', unicode, r'^[A-Za-z0-9.]+$'),
       ('singular', unicode, r'^(?i)(|true|false)$'),
     ],
-    alphabetization=[
-        (_OBSOLETE_TYPE.tag, _KEEP_ORDER),
-        (_OWNER_TYPE.tag, _KEEP_ORDER),
-        (_SUMMARY_TYPE.tag, _KEEP_ORDER),
-        (_METRIC_TYPE.tag, _LOWERCASE_FN('name')),
-    ],
     extra_newlines=(1, 1, 1),
     children=[
-        models.ChildType(_OBSOLETE_TYPE.tag, _OBSOLETE_TYPE, multiple=False),
-        models.ChildType(_OWNER_TYPE.tag, _OWNER_TYPE, multiple=True),
-        models.ChildType(_SUMMARY_TYPE.tag, _SUMMARY_TYPE, multiple=False),
-        models.ChildType(_METRIC_TYPE.tag, _METRIC_TYPE, multiple=True),
+        models.ChildType('obsolete', _OBSOLETE_TYPE, multiple=False),
+        models.ChildType('owners', _OWNER_TYPE, multiple=True),
+        models.ChildType('summary', _SUMMARY_TYPE, multiple=False),
+        models.ChildType('metrics', _METRIC_TYPE, multiple=True),
     ])
 
 _UKM_CONFIGURATION_TYPE = models.ObjectNodeType(
     'ukm-configuration',
-    alphabetization=[(_EVENT_TYPE.tag, _LOWERCASE_FN('name'))],
+    alphabetization=[('event', _LOWERCASE_NAME_FN)],
     extra_newlines=(2, 1, 1),
     indent=False,
     children=[
-        models.ChildType(_EVENT_TYPE.tag, _EVENT_TYPE, multiple=True),
+        models.ChildType('events', _EVENT_TYPE, multiple=True),
     ])
 
 UKM_XML_TYPE = models.DocumentType(_UKM_CONFIGURATION_TYPE)
 
-def PrettifyXML(original_xml):
+def UpdateXML(original_xml):
   """Parses the original xml and return a pretty printed version.
 
   Args:
