55aa76be0b3f29023f54c0111e7cd7df88a77b2e
Vlad Tsyrklevich <vtsyrklevich@chromium.org>
2019-04-12 20:41:07 +0000

    GWP-ASan: Expand stack trace test
    Test the higher-level AnalyzeCrashAllocator() function and have it check
    deallocation stack traces as well.
    
    Bug: 951410
    Change-Id: I2755c1e08de386290cd2ddc710269f7f60553e99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1566115
    Commit-Queue: Vitaly Buka <vitalybuka@chromium.org>
    Auto-Submit: Vlad Tsyrklevich <vtsyrklevich@chromium.org>
    Reviewed-by: Vitaly Buka <vitalybuka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650447}
    Former-commit-id: f7a0fb0eb39e51fe00cb52a948364bc1ff32c501

--- components/gwp_asan/crash_handler/crash_analyzer_unittest.cc
+++ components/gwp_asan/crash_handler/crash_analyzer_unittest.cc
@@ -10,25 +10,35 @@
 #include "components/gwp_asan/common/allocator_state.h"
 #include "components/gwp_asan/crash_handler/crash.pb.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/crashpad/crashpad/snapshot/test/test_exception_snapshot.h"
+#include "third_party/crashpad/crashpad/test/process_type.h"
+#include "third_party/crashpad/crashpad/util/process/process_memory_native.h"
 
 namespace gwp_asan {
 namespace internal {
 
 TEST(CrashAnalyzerTest, StackTraceCollection) {
-  GuardedPageAllocator gpa_;
-  gpa_.Init(1, 1, 1);
+  GuardedPageAllocator gpa;
+  gpa.Init(1, 1, 1);
 
-  void* ptr = gpa_.Allocate(10);
+  void* ptr = gpa.Allocate(10);
   ASSERT_NE(ptr, nullptr);
+  gpa.Deallocate(ptr);
 
-  uintptr_t addr = reinterpret_cast<uintptr_t>(ptr);
-  // gpa_ was initialized with only a single metadata slot so the allocation
-  // should have metadata index 0.
-  ASSERT_EQ(gpa_.metadata_[0].alloc_ptr, addr);
+  crashpad::ProcessMemoryNative memory;
+  ASSERT_TRUE(memory.Initialize(crashpad::test::GetSelfProcess()));
 
-  gwp_asan::Crash_AllocationInfo allocation_info;
-  CrashAnalyzer::ReadAllocationInfo(gpa_.metadata_[0].alloc, &allocation_info);
-  ASSERT_GT(allocation_info.stack_trace_size(), 0);
+  // Lets pretend a double free() occurred on the allocation we saw previously.
+  crashpad::test::TestExceptionSnapshot exception_snapshot;
+  gpa.state_.double_free_address = reinterpret_cast<uintptr_t>(ptr);
+
+  gwp_asan::Crash proto;
+  auto result = CrashAnalyzer::AnalyzeCrashedAllocator(
+      memory, exception_snapshot, reinterpret_cast<uintptr_t>(&gpa), &proto);
+  EXPECT_EQ(result, CrashAnalyzer::GwpAsanCrashAnalysisResult::kGwpAsanCrash);
+
+  ASSERT_TRUE(proto.has_allocation());
+  ASSERT_TRUE(proto.has_deallocation());
 
   base::debug::StackTrace st;
   size_t trace_len;
@@ -36,18 +46,35 @@ TEST(CrashAnalyzerTest, StackTraceCollection) {
   ASSERT_NE(trace, nullptr);
   ASSERT_GT(trace_len, 0U);
 
-  // Scan the stack trace we collected and the one from the allocator and make
-  // sure that all entries up to the current frame match.
-  for (size_t i = 1; i <= trace_len; i++) {
-    ASSERT_EQ(
-        allocation_info.stack_trace(allocation_info.stack_trace_size() - i),
-        reinterpret_cast<uintptr_t>(trace[trace_len - i]));
+  // Adjust the stack trace to point to the entry above the current frame.
+  while (trace_len > 0) {
+    if (trace[0] == __builtin_return_address(0))
+      break;
 
-    if (trace[trace_len - i] == __builtin_return_address(0))
-      return;
+    trace++;
+    trace_len--;
   }
 
-  FAIL() << "Return address not found in the collected stack trace.";
+  ASSERT_GT(proto.allocation().stack_trace_size(), (int)trace_len);
+  ASSERT_GT(proto.deallocation().stack_trace_size(), (int)trace_len);
+
+  // Ensure that the allocation and deallocation stack traces match the stack
+  // frames that we collected above the current frame.
+  for (size_t i = 1; i <= trace_len; i++) {
+    SCOPED_TRACE(i);
+    ASSERT_EQ(proto.allocation().stack_trace(
+                  proto.allocation().stack_trace_size() - i),
+              reinterpret_cast<uintptr_t>(trace[trace_len - i]));
+    ASSERT_EQ(proto.deallocation().stack_trace(
+                  proto.deallocation().stack_trace_size() - i),
+              reinterpret_cast<uintptr_t>(trace[trace_len - i]));
+  }
+
+  // Allocate() and Deallocate() were called from different points in this test.
+  ASSERT_NE(proto.allocation().stack_trace(
+                proto.allocation().stack_trace_size() - (trace_len + 1)),
+            proto.deallocation().stack_trace(
+                proto.deallocation().stack_trace_size() - (trace_len + 1)));
 }
 
 }  // namespace internal
