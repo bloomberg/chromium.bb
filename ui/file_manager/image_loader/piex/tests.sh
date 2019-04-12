09ad0d47b11cccf2f049a44f08b1e1d3aa8a61d6
Noel Gordon <noel@chromium.org>
2019-04-12 01:26:04 +0000

    [piexwasm] Make test.js control the test http server
    test.js can handle the test http server for less code and fewer moving
    parts (less is more or something like that).
    
    Bug: 935285
    Change-Id: I21d94f09ab16f4dc33a969b006db0e4e0494732a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1563536
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650158}
    Former-commit-id: f1b517a3f102f5cf241dca8730d7af2de836e10f

--- ui/file_manager/image_loader/piex/tests.sh
+++ ui/file_manager/image_loader/piex/tests.sh
@@ -4,19 +4,10 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-PORT=8123
-HTTP="http://localhost:${PORT}"
-
-# Start test http server.
-echo -e "test: starting test server ${HTTP}\n" > tests.log
-node node_modules/http-server/bin/http-server -p ${PORT} > /dev/null 2>&1 &
-HTTP_SERVER_PID=$!
-
 # Extract preview thumbnails from the raw test images.
-rm -f tests.result.txt
-node tests.js ${HTTP}/tests.html $* | tee -a tests.log | \
+rm -f tests.result.txt tests.log
+node tests tests.html $* | tee tests.log | \
   grep --text "^test: images/" > tests.result.txt
-kill ${HTTP_SERVER_PID} > /dev/null 2>&1
 
 # Compare their properties to the golden file values.
 if [[ $(cmp tests.result.txt images.golden.txt 2>&1) ]]; then
