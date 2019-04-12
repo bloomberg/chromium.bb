5977f63df149e57616f57fd777ba2511b68867f3
David Maunder <davidjm@chromium.org>
2019-04-12 16:25:44 +0000

    Add Task ID fields to SerializedNavigationEntry.
    Task IDs track navigations and relationships between navigations.
    They are added to SerializedNavigationEntry so they are persisted
    across restarts.
    
    Bug: 944322
    Change-Id: I189255de962d8bdadbb239801f9ef19a467de30d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1532999
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Nicolas Zea <zea@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: David Maunder <davidjm@google.com>
    Cr-Commit-Position: refs/heads/master@{#650327}
    Former-commit-id: 7d1c5db4a3ce793b83477e83384cf73cd225b807

--- components/sessions/content/content_serialized_navigation_builder_unittest.cc
+++ components/sessions/content/content_serialized_navigation_builder_unittest.cc
@@ -7,6 +7,7 @@
 #include "base/memory/ptr_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/sessions/content/content_record_password_state.h"
+#include "components/sessions/content/content_record_task_id.h"
 #include "components/sessions/content/content_serialized_navigation_driver.h"
 #include "components/sessions/content/extended_info_handler.h"
 #include "components/sessions/core/serialized_navigation_entry.h"
@@ -83,6 +84,14 @@ std::unique_ptr<content::NavigationEntry> MakeNavigationEntryForTest() {
   redirect_chain.push_back(test_data::kRedirectURL1);
   redirect_chain.push_back(test_data::kVirtualURL);
   navigation_entry->SetRedirectChain(redirect_chain);
+  ContextRecordTaskId::Get(navigation_entry.get())
+      ->set_task_id(test_data::kTaskId);
+  ContextRecordTaskId::Get(navigation_entry.get())
+      ->set_parent_task_id(test_data::kParentTaskId);
+  ContextRecordTaskId::Get(navigation_entry.get())
+      ->set_root_task_id(test_data::kRootTaskId);
+  ContextRecordTaskId::Get(navigation_entry.get())
+      ->set_children_task_ids(test_data::kChildrenTaskIds);
   return navigation_entry;
 }
 
@@ -163,6 +172,11 @@ TEST_F(ContentSerializedNavigationBuilderTest, FromNavigationEntry) {
   ASSERT_EQ(1U, navigation.extended_info_map().count(kExtendedInfoKey2));
   EXPECT_EQ(kExtendedInfoValue2,
             navigation.extended_info_map().at(kExtendedInfoKey2));
+
+  EXPECT_EQ(test_data::kTaskId, navigation.task_id());
+  EXPECT_EQ(test_data::kParentTaskId, navigation.parent_task_id());
+  EXPECT_EQ(test_data::kRootTaskId, navigation.root_task_id());
+  EXPECT_EQ(test_data::kChildrenTaskIds, navigation.children_task_ids());
 }
 
 // Test effect of the navigation serialization options.
