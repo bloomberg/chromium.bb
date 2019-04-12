b0c1bd452ff6ebeef161c063b5824c509b448528
Matthew Mourgos <mmourgos@chromium.org>
2019-04-12 22:41:51 +0000

    Disable window activation upon opening disambiguation menu for app shortcuts on the shelf.
    Bug: 951964
    Change-Id: I858d99304c09846581947a0ec17d6ff6ae497585
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1564549
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650494}
    Former-commit-id: 98fed5dd70e58e81d0b4cd47900dbf482527f6aa

--- chrome/browser/ui/ash/launcher/app_shortcut_launcher_item_controller.cc
+++ chrome/browser/ui/ash/launcher/app_shortcut_launcher_item_controller.cc
@@ -183,9 +183,16 @@ void AppShortcutLauncherItemController::ItemSelected(
     return;
   }
 
-  const ash::ShelfAction action = ActivateContent(content);
-  std::move(callback).Run(
-      action, GetAppMenuItems(event ? event->flags() : ui::EF_NONE));
+  ash::MenuItemList items =
+      GetAppMenuItems(event ? event->flags() : ui::EF_NONE);
+
+  if (items.size() == 1) {
+    std::move(callback).Run(ActivateContent(content), base::nullopt);
+  } else {
+    // Multiple items, a menu will be shown. No need to activate the most
+    // recently active item.
+    std::move(callback).Run(ash::SHELF_ACTION_NONE, std::move(items));
+  }
 }
 
 ash::MenuItemList AppShortcutLauncherItemController::GetAppMenuItems(
