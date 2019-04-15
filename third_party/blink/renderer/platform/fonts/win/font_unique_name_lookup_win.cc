09c7ac461ab9fdf62911ab25779cc470a8a93dfc
Dominik Röttsches <drott@chromium.org>
2019-04-15 21:12:06 +0000

    Reland: Use single lookups for font unique name matching on Windows 10
    When available (Windows 10, or DWriteFontFactory3 available), use
    individual font unique name matching Mojo IPC calls instead of the
    method of retrieving the shared memory lookup table.
    
    Bug: 951978
    Change-Id: I56ba2b1fadc25f15283e0c221a77434aaacea62c
    Tbr: eae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1567677
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650977}
    Former-commit-id: e844757159b8fd803fcc2a4b689fa3bd803bc96c

--- third_party/blink/renderer/platform/fonts/win/font_unique_name_lookup_win.cc
+++ third_party/blink/renderer/platform/fonts/win/font_unique_name_lookup_win.cc
@@ -35,6 +35,33 @@ FontUniqueNameLookupWin::~FontUniqueNameLookupWin() = default;
 
 sk_sp<SkTypeface> FontUniqueNameLookupWin::MatchUniqueName(
     const String& font_unique_name) {
+  if (lookup_mode_ == blink::mojom::UniqueFontLookupMode::kSingleLookups)
+    return MatchUniqueNameSingleLookup(font_unique_name);
+  return MatchUniqueNameLookupTable(font_unique_name);
+}
+
+sk_sp<SkTypeface> FontUniqueNameLookupWin::MatchUniqueNameSingleLookup(
+    const String& font_unique_name) {
+  DCHECK(lookup_mode_ == blink::mojom::UniqueFontLookupMode::kSingleLookups);
+  base::FilePath font_file_path;
+  uint32_t ttc_index = 0;
+
+  EnsureServiceConnected();
+
+  bool matching_mojo_success =
+      service_->MatchUniqueFont(font_unique_name, &font_file_path, &ttc_index);
+  DCHECK(matching_mojo_success);
+
+  if (!font_file_path.value().size())
+    return nullptr;
+
+  return InstantiateFromPathAndTtcIndex(font_file_path, ttc_index);
+}
+
+sk_sp<SkTypeface> FontUniqueNameLookupWin::MatchUniqueNameLookupTable(
+    const String& font_unique_name) {
+  DCHECK(lookup_mode_ == blink::mojom::UniqueFontLookupMode::kRetrieveTable);
+
   if (!IsFontUniqueNameLookupReadyForSyncLookup())
     return nullptr;
 
@@ -42,12 +69,21 @@ sk_sp<SkTypeface> FontUniqueNameLookupWin::MatchUniqueName(
       font_table_matcher_->MatchName(font_unique_name.Utf8().data());
   if (!match_result)
     return nullptr;
+
+  base::FilePath file_path =
+      base::FilePath::FromUTF8Unsafe(match_result->font_path.c_str());
+  return InstantiateFromPathAndTtcIndex(file_path, match_result->ttc_index);
+}
+
+sk_sp<SkTypeface> FontUniqueNameLookupWin::InstantiateFromPathAndTtcIndex(
+    base::FilePath font_file_path,
+    uint32_t ttc_index) {
   // Record here when a locally uniquely matched font could not be
   // instantiated. One reason could be that the font was outside the
   // C:\Windows\Fonts directory and thus not accessible due to sandbox
   // restrictions.
   sk_sp<SkTypeface> local_typeface = SkTypeface::MakeFromFile(
-      match_result->font_path.c_str(), match_result->ttc_index);
+      font_file_path.AsUTF8Unsafe().c_str(), ttc_index);
 
   WindowsUniqueLocalFontInstantiationResult result = kSuccess;
 
@@ -59,8 +95,7 @@ sk_sp<SkTypeface> FontUniqueNameLookupWin::MatchUniqueName(
   // side.
   if (!local_typeface) {
     base::FilePath windows_fonts_path(L"C:\\WINDOWS\\FONTS");
-    if (!windows_fonts_path.IsParent(
-            base::FilePath::FromUTF8Unsafe(match_result->font_path.c_str())))
+    if (!windows_fonts_path.IsParent(font_file_path))
       result = kErrorOutsideWindowsFontsDirectory;
     else
       result = kErrorOther;
@@ -76,11 +111,25 @@ sk_sp<SkTypeface> FontUniqueNameLookupWin::MatchUniqueName(
 }
 
 bool FontUniqueNameLookupWin::IsFontUniqueNameLookupReadyForSyncLookup() {
-  // If we have the table already, we're ready for sync lookups.
-  if (font_table_matcher_.get())
+  if (!RuntimeEnabledFeatures::FontSrcLocalMatchingEnabled())
     return true;
 
-  if (!RuntimeEnabledFeatures::FontSrcLocalMatchingEnabled())
+  EnsureServiceConnected();
+
+  if (!lookup_mode_.has_value()) {
+    blink::mojom::UniqueFontLookupMode lookup_mode_from_mojo;
+    service_->GetUniqueFontLookupMode(&lookup_mode_from_mojo);
+    lookup_mode_ = lookup_mode_from_mojo;
+  }
+
+  if (lookup_mode_ == blink::mojom::UniqueFontLookupMode::kSingleLookups) {
+    return true;
+  }
+
+  DCHECK(lookup_mode_ == blink::mojom::UniqueFontLookupMode::kRetrieveTable);
+
+  // If we have the table already, we're ready for sync lookups.
+  if (font_table_matcher_.get())
     return true;
 
   // We have previously determined via IPC whether the table is sync available.
@@ -90,8 +139,6 @@ bool FontUniqueNameLookupWin::IsFontUniqueNameLookupReadyForSyncLookup() {
 
   // If we haven't asked the browser before, probe synchronously - if the table
   // is available on the browser side, we can continue with sync operation.
-
-  EnsureServiceConnected();
 
   bool sync_available_from_mojo = false;
   base::ReadOnlySharedMemoryRegion shared_memory_region;
@@ -121,6 +168,7 @@ void FontUniqueNameLookupWin::PrepareFontUniqueNameLookup(
     NotifyFontUniqueNameLookupReady callback) {
   DCHECK(!font_table_matcher_.get());
   DCHECK(RuntimeEnabledFeatures::FontSrcLocalMatchingEnabled());
+  DCHECK(lookup_mode_ == blink::mojom::UniqueFontLookupMode::kRetrieveTable);
 
   pending_callbacks_.push_back(std::move(callback));
 
@@ -140,6 +188,7 @@ void FontUniqueNameLookupWin::PrepareFontUniqueNameLookup(
 
 void FontUniqueNameLookupWin::ReceiveReadOnlySharedMemoryRegion(
     base::ReadOnlySharedMemoryRegion shared_memory_region) {
+  DCHECK(lookup_mode_ == blink::mojom::UniqueFontLookupMode::kRetrieveTable);
   font_table_matcher_ =
       std::make_unique<FontTableMatcher>(shared_memory_region.Map());
   while (!pending_callbacks_.IsEmpty()) {
