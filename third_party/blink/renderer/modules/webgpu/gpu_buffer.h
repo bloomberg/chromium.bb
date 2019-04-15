94513f93c91d95541b457986de7f7fd7f31c8cfc
Austin Eng <enga@chromium.org>
2019-04-15 16:40:31 +0000

    Implement GPUBuffer in Blink
    Bug: 877147
    Change-Id: I41374c60ceed303b3e67aa70bba58e3a0187efb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1547934
    Commit-Queue: Austin Eng <enga@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650831}
    Former-commit-id: 95895b298045c439959fb6299b5a52c6733104ee

--- third_party/blink/renderer/modules/webgpu/gpu_buffer.h
+++ third_party/blink/renderer/modules/webgpu/gpu_buffer.h
@@ -5,11 +5,16 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_WEBGPU_GPU_BUFFER_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_WEBGPU_GPU_BUFFER_H_
 
+#include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
+#include "third_party/blink/renderer/core/typed_arrays/array_buffer_view_helpers.h"
 #include "third_party/blink/renderer/modules/webgpu/dawn_object.h"
+#include "third_party/blink/renderer/platform/bindings/exception_state.h"
 
 namespace blink {
 
+class DOMArrayBuffer;
 class GPUBufferDescriptor;
+class ScriptPromiseResolver;
 
 class GPUBuffer : public DawnObject<DawnBuffer> {
   DEFINE_WRAPPERTYPEINFO();
@@ -17,13 +22,35 @@ class GPUBuffer : public DawnObject<DawnBuffer> {
  public:
   static GPUBuffer* Create(GPUDevice* device,
                            const GPUBufferDescriptor* webgpu_desc);
-  explicit GPUBuffer(GPUDevice* device, DawnBuffer buffer);
+  explicit GPUBuffer(GPUDevice* device, uint64_t size, DawnBuffer buffer);
   ~GPUBuffer() override;
 
+  void Trace(blink::Visitor* visitor) override;
+
   // gpu_buffer.idl
+  void setSubData(uint64_t dst_byte_offset,
+                  const MaybeShared<DOMArrayBufferView>& src,
+                  uint64_t src_byte_offset,
+                  uint64_t byte_length,
+                  ExceptionState& exception_state);
+  ScriptPromise mapReadAsync(ScriptState* script_state,
+                             ExceptionState& exception_state);
+  ScriptPromise mapWriteAsync(ScriptState* script_state,
+                              ExceptionState& exception_state);
+  void unmap(ScriptState* script_state);
+  void destroy(ScriptState* script_state);
   // TODO(crbug.com/877147): implement GPUBuffer.
 
  private:
+  void OnMapAsyncCallback(ScriptPromiseResolver* resolver,
+                          DawnBufferMapAsyncStatus status,
+                          void* data,
+                          uint64_t data_length);
+  void DetachArrayBufferForCurrentMapping(ScriptState* script_state);
+
+  uint64_t size_;
+  Member<DOMArrayBuffer> mapped_buffer_;
+
   DISALLOW_COPY_AND_ASSIGN(GPUBuffer);
 };
 
