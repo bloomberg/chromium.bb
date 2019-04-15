b98c12c5f6f96cdf7bb419edc9c7972237c3b348
Stephen McGruer <smcgruer@chromium.org>
2019-04-15 14:34:02 +0000

    Reland "Ship the Animation constructor"
    This reverts commit 72613a57f61a0861c3f32a86058d1670a120a8d5 [formerly 173217e7a69032ce202db6f340e247c83551694c].
    
    Intent to ship with 3x LGTM:
    https://groups.google.com/a/chromium.org/forum/?utm_medium=email&utm_source=footer#!msg/blink-dev/UMxgGkxhdCo/CHZ2H9DsBgAJ
    
    This version removes a few incorrect DCHECKs for WebAnimationsAPIEnabled.
    
    Bug: 948657
    Change-Id: I2ba482946b44abd4e9b0b3dbdf76a587352bd4e8
    
    TBR=tkent@chromium.org
    
    Change-Id: I2ba482946b44abd4e9b0b3dbdf76a587352bd4e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1564553
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#650785}
    Former-commit-id: d399276bec3bd28fd64802af04570f76feab3f12

--- third_party/blink/renderer/core/animation/animation.cc
+++ third_party/blink/renderer/core/animation/animation.cc
@@ -53,7 +53,6 @@
 #include "third_party/blink/renderer/platform/bindings/script_forbidden_scope.h"
 #include "third_party/blink/renderer/platform/heap/persistent.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
-#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/wtf/math_extras.h"
 
 namespace blink {
@@ -94,8 +93,6 @@ Animation* Animation::Create(AnimationEffect* effect,
 Animation* Animation::Create(ExecutionContext* execution_context,
                              AnimationEffect* effect,
                              ExceptionState& exception_state) {
-  DCHECK(RuntimeEnabledFeatures::WebAnimationsAPIEnabled());
-
   Document* document = To<Document>(execution_context);
   return Create(effect, &document->Timeline(), exception_state);
 }
@@ -104,8 +101,6 @@ Animation* Animation::Create(ExecutionContext* execution_context,
                              AnimationEffect* effect,
                              AnimationTimeline* timeline,
                              ExceptionState& exception_state) {
-  DCHECK(RuntimeEnabledFeatures::WebAnimationsAPIEnabled());
-
   if (!timeline) {
     return Create(execution_context, effect, exception_state);
   }
