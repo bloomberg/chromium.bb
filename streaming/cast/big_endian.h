486c7aebca2058b542672124bcdbb041b65d1d16
Yuri Wiitala <miu@chromium.org>
2019-04-15 15:04:57 -0700

    Cast Streaming: Simple big-endian reader/writer functions.
    Adds functions that provide read/write of big-endian integers of various
    sizes. The functions are inlined, but consist of non-trivial
    implementations to adhere to "defined C++ behavior." Nevertheless, they
    will compile to a single-instruction when using any half-decent C++
    compiler optimizer (confirmed via godbolt.org).
    
    Change-Id: I0aca97396b17ebe23eed34179187e994370cb983
    Reviewed-on: https://chromium-review.googlesource.com/c/openscreen/+/1566570
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: Max Yakimakha <yakimakha@google.com>
    Reviewed-by: Ryan Keane <rwkeane@google.com>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>

---
+++ streaming/cast/big_endian.h
@@ -0,0 +1,108 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef STREAMING_CAST_BIG_ENDIAN_H_
+#define STREAMING_CAST_BIG_ENDIAN_H_
+
+#include <stdint.h>
+
+#include <cstring>
+#include <type_traits>
+
+namespace openscreen {
+namespace cast_streaming {
+
+////////////////////////////////////////////////////////////////////////////////
+// Note: All of the functions here are defined inline, as any half-decent
+// compiler will optimize them to a single integer constant or single
+// instruction on most architectures.
+////////////////////////////////////////////////////////////////////////////////
+
+// Returns true if this code is running on a big-endian architecture.
+inline bool IsBigEndianArchitecture() {
+  const uint16_t kTestWord = 0x0100;
+  uint8_t bytes[sizeof(kTestWord)];
+  memcpy(bytes, &kTestWord, sizeof(bytes));
+  return !!bytes[0];
+}
+
+// Returns the bytes of |x| in reverse order. This is only defined for 16-, 32-,
+// and 64-bit unsigned integers.
+template <typename Integer>
+Integer ByteSwap(Integer x);
+
+#if defined(__clang__) || defined(__GNUC__)
+
+template <>
+inline uint64_t ByteSwap(uint64_t x) {
+  return __builtin_bswap64(x);
+}
+template <>
+inline uint32_t ByteSwap(uint32_t x) {
+  return __builtin_bswap32(x);
+}
+template <>
+inline uint16_t ByteSwap(uint16_t x) {
+  return __builtin_bswap16(x);
+}
+
+#elif defined(_MSC_VER)
+
+template <>
+inline uint64_t ByteSwap(uint64_t x) {
+  return _byteswap_uint64(x);
+}
+template <>
+inline uint32_t ByteSwap(uint32_t x) {
+  return _byteswap_ulong(x);
+}
+template <>
+inline uint16_t ByteSwap(uint16_t x) {
+  return _byteswap_ushort(x);
+}
+
+#else
+
+#include <byteswap.h>
+
+template <>
+inline uint64_t ByteSwap(uint64_t x) {
+  return bswap_64(x);
+}
+template <>
+inline uint32_t ByteSwap(uint32_t x) {
+  return bswap_32(x);
+}
+template <>
+inline uint16_t ByteSwap(uint16_t x) {
+  return bswap_16(x);
+}
+
+#endif
+
+// Read a POD integer from |src| in big-endian byte order, returning the integer
+// in native byte order.
+template <typename Integer>
+inline Integer ReadBigEndian(const void* src) {
+  Integer result;
+  memcpy(&result, src, sizeof(result));
+  if (!IsBigEndianArchitecture()) {
+    result = ByteSwap<typename std::make_unsigned<Integer>::type>(result);
+  }
+  return result;
+}
+
+// Write a POD integer |val| to |dest| in big-endian byte order.
+template <typename Integer>
+inline void WriteBigEndian(Integer val, void* dest) {
+  if (!IsBigEndianArchitecture()) {
+    val = ByteSwap<typename std::make_unsigned<Integer>::type>(val);
+  }
+  memcpy(dest, &val, sizeof(val));
+}
+
+}  // namespace cast_streaming
+}  // namespace openscreen
+
+#endif  // STREAMING_CAST_BIG_ENDIAN_H_
---
+++ streaming/cast/big_endian_unittest.cc
@@ -0,0 +1,89 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "streaming/cast/big_endian.h"
+
+#include "third_party/googletest/src/googletest/include/gtest/gtest.h"
+
+namespace openscreen {
+namespace cast_streaming {
+namespace {
+
+// Tests that ReadBigEndian() correctly imports values from various offsets in
+// memory.
+TEST(BigEndianTest, ReadValues) {
+  const uint8_t kInput[] = {
+      0,    1,    2,    3,    4,    5,    6,    7,    8,    9,    0xa,
+      0xb,  0xc,  0xd,  0xe,  0xf,  0xff, 0xff, 0xfe, 0xff, 0xff, 0xff,
+      0xff, 0xfe, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfe,
+  };
+
+  EXPECT_EQ(UINT16_C(0x0001), ReadBigEndian<uint16_t>(kInput));
+  EXPECT_EQ(UINT16_C(0x0102), ReadBigEndian<uint16_t>(kInput + 1));
+  EXPECT_EQ(UINT16_C(0x0203), ReadBigEndian<uint16_t>(kInput + 2));
+  EXPECT_EQ(-1, ReadBigEndian<int16_t>(kInput + 16));
+  EXPECT_EQ(-2, ReadBigEndian<int16_t>(kInput + 17));
+
+  EXPECT_EQ(UINT32_C(0x03040506), ReadBigEndian<uint32_t>(kInput + 3));
+  EXPECT_EQ(UINT32_C(0x04050607), ReadBigEndian<uint32_t>(kInput + 4));
+  EXPECT_EQ(UINT32_C(0x05060708), ReadBigEndian<uint32_t>(kInput + 5));
+  EXPECT_EQ(-1, ReadBigEndian<int32_t>(kInput + 19));
+  EXPECT_EQ(-2, ReadBigEndian<int32_t>(kInput + 20));
+
+  EXPECT_EQ(UINT64_C(0x0001020304050607), ReadBigEndian<uint64_t>(kInput));
+  EXPECT_EQ(UINT64_C(0x0102030405060708), ReadBigEndian<uint64_t>(kInput + 1));
+  EXPECT_EQ(UINT64_C(0x0203040506070809), ReadBigEndian<uint64_t>(kInput + 2));
+  EXPECT_EQ(-1, ReadBigEndian<int64_t>(kInput + 24));
+  EXPECT_EQ(-2, ReadBigEndian<int64_t>(kInput + 25));
+}
+
+// Tests that WriteBigEndian() correctly writes-out values to various offsets in
+// memory. This test assumes ReadBigEndian() is working, using it to verify that
+// WriteBigEndian() is working.
+TEST(BigEndianTest, WriteValues) {
+  uint8_t scratch[16];
+
+  WriteBigEndian<uint16_t>(0x0102, scratch);
+  EXPECT_EQ(UINT16_C(0x0102), ReadBigEndian<uint16_t>(scratch));
+  WriteBigEndian<uint16_t>(0x0304, scratch + 1);
+  EXPECT_EQ(UINT16_C(0x0304), ReadBigEndian<uint16_t>(scratch + 1));
+  WriteBigEndian<uint16_t>(0x0506, scratch + 2);
+  EXPECT_EQ(UINT16_C(0x0506), ReadBigEndian<uint16_t>(scratch + 2));
+  WriteBigEndian<int16_t>(42, scratch + 3);
+  EXPECT_EQ(42, ReadBigEndian<int16_t>(scratch + 3));
+  WriteBigEndian<int16_t>(-1, scratch + 4);
+  EXPECT_EQ(-1, ReadBigEndian<int16_t>(scratch + 4));
+  WriteBigEndian<int16_t>(-2, scratch + 5);
+  EXPECT_EQ(-2, ReadBigEndian<int16_t>(scratch + 5));
+
+  WriteBigEndian<uint32_t>(UINT32_C(0x03040506), scratch);
+  EXPECT_EQ(UINT32_C(0x03040506), ReadBigEndian<uint32_t>(scratch));
+  WriteBigEndian<uint32_t>(UINT32_C(0x0708090a), scratch + 1);
+  EXPECT_EQ(UINT32_C(0x0708090a), ReadBigEndian<uint32_t>(scratch + 1));
+  WriteBigEndian<uint32_t>(UINT32_C(0x0b0c0d0e), scratch + 2);
+  EXPECT_EQ(UINT32_C(0x0b0c0d0e), ReadBigEndian<uint32_t>(scratch + 2));
+  WriteBigEndian<int32_t>(42, scratch + 3);
+  EXPECT_EQ(42, ReadBigEndian<int32_t>(scratch + 3));
+  WriteBigEndian<int32_t>(-1, scratch + 4);
+  EXPECT_EQ(-1, ReadBigEndian<int32_t>(scratch + 4));
+  WriteBigEndian<int32_t>(-2, scratch + 5);
+  EXPECT_EQ(-2, ReadBigEndian<int32_t>(scratch + 5));
+
+  WriteBigEndian<uint64_t>(UINT64_C(0x0f0e0d0c0b0a0908), scratch);
+  EXPECT_EQ(UINT64_C(0x0f0e0d0c0b0a0908), ReadBigEndian<uint64_t>(scratch));
+  WriteBigEndian<uint64_t>(UINT64_C(0x0708090a0b0c0d0e), scratch + 1);
+  EXPECT_EQ(UINT64_C(0x0708090a0b0c0d0e), ReadBigEndian<uint64_t>(scratch + 1));
+  WriteBigEndian<uint64_t>(UINT64_C(0x99aa88bb77cc66dd), scratch + 2);
+  EXPECT_EQ(UINT64_C(0x99aa88bb77cc66dd), ReadBigEndian<uint64_t>(scratch + 2));
+  WriteBigEndian<int64_t>(42, scratch + 3);
+  EXPECT_EQ(42, ReadBigEndian<int64_t>(scratch + 3));
+  WriteBigEndian<int64_t>(-1, scratch + 4);
+  EXPECT_EQ(-1, ReadBigEndian<int64_t>(scratch + 4));
+  WriteBigEndian<int64_t>(-2, scratch + 5);
+  EXPECT_EQ(-2, ReadBigEndian<int64_t>(scratch + 5));
+}
+
+}  // namespace
+}  // namespace cast_streaming
+}  // namespace openscreen
