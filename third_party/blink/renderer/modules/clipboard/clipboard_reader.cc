b00c5f97cfa560cc83e23c920d7b695a99ec480c
Darwin Huang <huangdarwin@chromium.org>
2019-04-16 01:31:00 +0000

    Async Clipboard: Use const ref args in Clipboard{Reader,Writer}
    Change-Id: Ib5048a1de824b4cf80fedb0f11125fab309b5597
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1568307
    Commit-Queue: Darwin Huang <huangdarwin@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Auto-Submit: Darwin Huang <huangdarwin@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651101}
    Former-commit-id: de44fe11dc136969def7775f4a8c4abac512450b

--- third_party/blink/renderer/modules/clipboard/clipboard_reader.cc
+++ third_party/blink/renderer/modules/clipboard/clipboard_reader.cc
@@ -62,7 +62,8 @@ class ClipboardTextReader final : public ClipboardReader {
 // ClipboardReader functions.
 
 // static
-std::unique_ptr<ClipboardReader> ClipboardReader::Create(String mime_type) {
+std::unique_ptr<ClipboardReader> ClipboardReader::Create(
+    const String& mime_type) {
   if (mime_type == kMimeTypeImagePng)
     return std::make_unique<ClipboardImageReader>();
   if (mime_type == kMimeTypeTextPlain)
