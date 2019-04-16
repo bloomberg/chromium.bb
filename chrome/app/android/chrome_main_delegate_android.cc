8a2e18f96e3c2921a15348a092f66190d7e37741
Michael Thiessen <mthiesse@chromium.org>
2019-04-16 16:16:35 +0000

    Fix test compile with local android safe browsing
    Note that these tests probably still won't pass - we aren't yet able to
    run most tests under the notouch configuration.
    
    Bug: 947659
    Change-Id: Ic8e7436ef9e267a61437e13acc08154175e2e590
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1551563
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Auto-Submit: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651297}
    Former-commit-id: 742011c05283d623cc4c69928e52af91e497ce02

--- chrome/app/android/chrome_main_delegate_android.cc
+++ chrome/app/android/chrome_main_delegate_android.cc
@@ -14,10 +14,13 @@
 #include "chrome/browser/android/chrome_startup_flags.h"
 #include "chrome/browser/android/metrics/uma_utils.h"
 #include "components/policy/core/browser/android/android_combined_policy_provider.h"
-#include "components/safe_browsing/android/safe_browsing_api_handler.h"
-#include "components/safe_browsing/android/safe_browsing_api_handler_bridge.h"
 #include "components/startup_metric_utils/browser/startup_metric_utils.h"
 #include "content/public/browser/browser_main_runner.h"
+
+#if defined(SAFE_BROWSING_DB_REMOTE)
+#include "components/safe_browsing/android/safe_browsing_api_handler.h"
+#include "components/safe_browsing/android/safe_browsing_api_handler_bridge.h"
+#endif
 
 using safe_browsing::SafeBrowsingApiHandler;
 
@@ -32,9 +35,11 @@ ChromeMainDelegateAndroid::~ChromeMainDelegateAndroid() {
 }
 
 bool ChromeMainDelegateAndroid::BasicStartupComplete(int* exit_code) {
+#if defined(SAFE_BROWSING_DB_REMOTE)
   safe_browsing_api_handler_.reset(
       new safe_browsing::SafeBrowsingApiHandlerBridge());
   SafeBrowsingApiHandler::SetInstance(safe_browsing_api_handler_.get());
+#endif
 
   policy::android::AndroidCombinedPolicyProvider::SetShouldWaitForPolicy(true);
   SetChromeSpecificCommandLineFlags();
@@ -90,5 +95,7 @@ int ChromeMainDelegateAndroid::RunProcess(
 
 void ChromeMainDelegateAndroid::ProcessExiting(
     const std::string& process_type) {
+#if defined(SAFE_BROWSING_DB_REMOTE)
   SafeBrowsingApiHandler::SetInstance(nullptr);
+#endif
 }
