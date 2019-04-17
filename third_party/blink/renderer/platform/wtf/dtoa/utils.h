f7711305c5e885e83a71cdcac1e8d40fc8b0fb2f
Shawn Anastasio <shawnanastasio@gmail.com>
2019-04-17 12:37:36 +0000

    [blink] Introduce support for ppc64
    This commit introduces support for the ppc64 architecture
    to blink.
    
    Included is an assembly implementation of PushAllRegisters
    written by Dan Horák and myself, a slight modification to
    the heap to disable guard pages on ppc64 written by
    Timothy Pearson, and minor BUILD.gn changes.
    
    This change is part of the effort to upstream a set of patches
    that allow chromium to build and run on ppc64le hosts available here:
    https://wiki.raptorcs.com/wiki/Porting/Chromium
    
    Change-Id: I0afb7b12c4cdb8b04012e672b2fa2409c3ef2a97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1554650
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651687}
    Former-commit-id: cac1e737825fafc0787b1b057381733027d5c9ea

--- third_party/blink/renderer/platform/wtf/dtoa/utils.h
+++ third_party/blink/renderer/platform/wtf/dtoa/utils.h
@@ -46,7 +46,7 @@
 // disabled.)
 // On Linux,x86 89255e-22 != Div_double(89255.0/1e22)
 #if defined(_M_X64) || defined(__x86_64__) || defined(__ARMEL__) || \
-    defined(__aarch64__) || defined(__MIPSEL__)
+    defined(__aarch64__) || defined(__MIPSEL__) || defined(__powerpc64__)
 #define DOUBLE_CONVERSION_CORRECT_DOUBLE_OPERATIONS 1
 #elif defined(_M_IX86) || defined(__i386__)
 #if defined(_WIN32)
