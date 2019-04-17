aa8ec6b20f7a74046542d92b113c27bc79a4736c
Sergey Ulanov <sergeyu@google.com>
2019-04-17 22:43:02 +0000

    [Fuchsia] Add VK_FUCHSIA_buffer_collection in vulkan_fuchsia_ext.h
    VK_FUCHSIA_buffer_collection Vulkan extension will be used on Fuchsia to
    create NativePixmap's backed by sysmem buffers.
    
    Bug: 852011
    Change-Id: I13dbe16be248e6279bd31874e27e18516bdbd0c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1572899
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651933}
    Former-commit-id: ca2b6b87e0de960137cb63f08c6749c61295dd70

--- gpu/vulkan/fuchsia/vulkan_fuchsia_ext.h
+++ gpu/vulkan/fuchsia/vulkan_fuchsia_ext.h
@@ -30,6 +30,18 @@ const VkStructureType
 const VkStructureType
     VK_STRUCTURE_TYPE_TEMP_SEMAPHORE_GET_ZIRCON_HANDLE_INFO_FUCHSIA =
         static_cast<VkStructureType>(1001006001);
+const VkStructureType VK_STRUCTURE_TYPE_BUFFER_COLLECTION_CREATE_INFO_FUCHSIA =
+    static_cast<VkStructureType>(1001004000);
+const VkStructureType VK_STRUCTURE_TYPE_FUCHSIA_IMAGE_FORMAT_FUCHSIA =
+    static_cast<VkStructureType>(1001004001);
+const VkStructureType
+    VK_STRUCTURE_TYPE_IMPORT_MEMORY_BUFFER_COLLECTION_FUCHSIA =
+        static_cast<VkStructureType>(1001004004);
+const VkStructureType
+    VK_STRUCTURE_TYPE_BUFFER_COLLECTION_IMAGE_CREATE_INFO_FUCHSIA =
+        static_cast<VkStructureType>(1001004005);
+const VkStructureType VK_STRUCTURE_TYPE_BUFFER_COLLECTION_PROPERTIES_FUCHSIA =
+    static_cast<VkStructureType>(1001004006);
 
 const VkExternalMemoryHandleTypeFlagBits
     VK_EXTERNAL_MEMORY_HANDLE_TYPE_TEMP_ZIRCON_VMO_BIT_FUCHSIA =
@@ -38,6 +50,9 @@ const VkExternalMemoryHandleTypeFlagBits
 const VkExternalSemaphoreHandleTypeFlagBits
     VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_TEMP_ZIRCON_EVENT_BIT_FUCHSIA =
         static_cast<VkExternalSemaphoreHandleTypeFlagBits>(0x00100000);
+
+const VkObjectType VK_OBJECT_TYPE_BUFFER_COLLECTION_FUCHSIA =
+    static_cast<VkObjectType>(1001004002);
 
 #define VK_FUCHSIA_external_memory 1
 #define VK_FUCHSIA_EXTERNAL_MEMORY_SPEC_VERSION 1
@@ -126,6 +141,88 @@ VKAPI_ATTR VkResult VKAPI_CALL vkGetSemaphoreZirconHandleFUCHSIA(
     VkDevice device,
     const VkSemaphoreGetZirconHandleInfoFUCHSIA* pGetZirconHandleInfo,
     zx_handle_t* pZirconHandle);
+#endif
+
+#define VK_FUCHSIA_buffer_collection 1
+VK_DEFINE_NON_DISPATCHABLE_HANDLE(VkBufferCollectionFUCHSIA)
+
+#define VK_FUCHSIA_BUFFER_COLLECTION_SPEC_VERSION 1
+#define VK_FUCHSIA_BUFFER_COLLECTION_EXTENSION_NAME \
+  "VK_FUCHSIA_buffer_collection"
+
+typedef struct VkBufferCollectionCreateInfoFUCHSIA {
+  VkStructureType sType;
+  const void* pNext;
+  zx_handle_t collectionToken;
+} VkBufferCollectionCreateInfoFUCHSIA;
+
+typedef struct VkFuchsiaImageFormatFUCHSIA {
+  VkStructureType sType;
+  const void* pNext;
+  const void* imageFormat;
+  uint32_t imageFormatSize;
+} VkFuchsiaImageFormatFUCHSIA;
+
+typedef struct VkImportMemoryBufferCollectionFUCHSIA {
+  VkStructureType sType;
+  const void* pNext;
+  VkBufferCollectionFUCHSIA collection;
+  uint32_t index;
+} VkImportMemoryBufferCollectionFUCHSIA;
+
+typedef struct VkBufferCollectionImageCreateInfoFUCHSIA {
+  VkStructureType sType;
+  const void* pNext;
+  VkBufferCollectionFUCHSIA collection;
+  uint32_t index;
+} VkBufferCollectionImageCreateInfoFUCHSIA;
+
+typedef struct VkBufferCollectionPropertiesFUCHSIA {
+  VkStructureType sType;
+  void* pNext;
+  uint32_t memoryTypeBits;
+  uint32_t count;
+} VkBufferCollectionPropertiesFUCHSIA;
+
+typedef VkResult(VKAPI_PTR* PFN_vkCreateBufferCollectionFUCHSIA)(
+    VkDevice device,
+    const VkBufferCollectionCreateInfoFUCHSIA* pImportInfo,
+    const VkAllocationCallbacks* pAllocator,
+    VkBufferCollectionFUCHSIA* pCollection);
+typedef VkResult(VKAPI_PTR* PFN_vkSetBufferCollectionConstraintsFUCHSIA)(
+    VkDevice device,
+    VkBufferCollectionFUCHSIA collection,
+    const VkImageCreateInfo* pImageInfo);
+typedef void(VKAPI_PTR* PFN_vkDestroyBufferCollectionFUCHSIA)(
+    VkDevice device,
+    VkBufferCollectionFUCHSIA collection,
+    const VkAllocationCallbacks* pAllocator);
+typedef VkResult(VKAPI_PTR* PFN_vkGetBufferCollectionPropertiesFUCHSIA)(
+    VkDevice device,
+    VkBufferCollectionFUCHSIA collection,
+    VkBufferCollectionPropertiesFUCHSIA* pProperties);
+
+#ifndef VK_NO_PROTOTYPES
+VKAPI_ATTR VkResult VKAPI_CALL vkCreateBufferCollectionFUCHSIA(
+    VkDevice device,
+    const VkBufferCollectionCreateInfoFUCHSIA* pImportInfo,
+    const VkAllocationCallbacks* pAllocator,
+    VkBufferCollectionFUCHSIA* pCollection);
+
+VKAPI_ATTR VkResult VKAPI_CALL
+vkSetBufferCollectionConstraintsFUCHSIA(VkDevice device,
+                                        VkBufferCollectionFUCHSIA collection,
+                                        const VkImageCreateInfo* pImageInfo);
+
+VKAPI_ATTR void VKAPI_CALL
+vkDestroyBufferCollectionFUCHSIA(VkDevice device,
+                                 VkBufferCollectionFUCHSIA collection,
+                                 const VkAllocationCallbacks* pAllocator);
+
+VKAPI_ATTR VkResult VKAPI_CALL vkGetBufferCollectionPropertiesFUCHSIA(
+    VkDevice device,
+    VkBufferCollectionFUCHSIA collection,
+    VkBufferCollectionPropertiesFUCHSIA* pProperties);
 #endif
 
 #ifdef __cplusplus
