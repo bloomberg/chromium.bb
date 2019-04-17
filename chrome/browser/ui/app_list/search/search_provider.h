9096cfcd56ce6ab17bd7563520c8a4080da8863f
Vladislav Kaznacheev <kaznacheev@google.com>
2019-04-17 15:23:03 +0000

    Do not update AppList from AppSearchProvider::ViewClosing()
    Calling ClearResults sends updated search results to Ash
    and ultimately causes a DCHECK (see linked bug).
    
    Introduce a version of this method that does not send an update.
    
    Bug: 948441
    Test: type "Set" in search box, hit Enter - no DCHECK
    Change-Id: I1f8fd9598841c34b17396e74be1dddb6ece1dbf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1570569
    Commit-Queue: Vladislav Kaznacheev <kaznacheev@chromium.org>
    Auto-Submit: Vladislav Kaznacheev <kaznacheev@chromium.org>
    Reviewed-by: Nigel Tao <nigeltao@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651722}
    Former-commit-id: b9d9f0c2b22df8be6ba597958a6afdf007478805

--- chrome/browser/ui/app_list/search/search_provider.h
+++ chrome/browser/ui/app_list/search/search_provider.h
@@ -51,7 +51,11 @@ class SearchProvider {
   // desired to be done only once when all results are added.
   void SwapResults(Results* new_results);
 
+  // Clear results and call the |result_changed_callback_|.
   void ClearResults();
+
+  // Clear the results without calling the |result_changed_callback_|.
+  void ClearResultsSilently();
 
  private:
   void FireResultChanged();
