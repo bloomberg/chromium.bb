62e2418bce0491db53babf448df05bc24c15aae5
Oleg Davydov <burunduk@chromium.org>
2019-04-17 13:17:33 +0000

    Increase fetch priority for policy-forced extensions
    When fetch extensions from chrome web store, there is a header which
    indicated pritority (or “interactivity”) of extension fetch. Usually it
    is high (FOREGROUND) when user manually clicked “Add to Chrome” in the
    store, and low (BACKGROUND) otherwise. Background fetches could be
    throttled on a store server-side (eg. due to bandwidth limit), which is
    perfectly OK for background updates, but is harmful for new installs
    (which can occur not only because of user click, but also because of
    extension forcelist policy). Therefore we have to set priority to
    foreground for policy-based extension downloads.
    
    Bug: 904600, 917700
    Change-Id: I25ac15e71398f833261cd0cb1116785fcf7a241d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1569019
    Commit-Queue: Oleg Davydov <burunduk@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651691}
    Former-commit-id: 6541a64f7801cd7f3560958334935cc06d63df07

--- chrome/browser/extensions/updater/extension_updater.h
+++ chrome/browser/extensions/updater/extension_updater.h
@@ -138,6 +138,10 @@ class ExtensionUpdater : public ExtensionDownloaderDelegate,
   // Overrides the extension cache with |extension_cache| for testing.
   void SetExtensionCacheForTesting(ExtensionCache* extension_cache);
 
+  // Overrides the extension downloader with |downloader| for testing.
+  void SetExtensionDownloaderForTesting(
+      std::unique_ptr<ExtensionDownloader> downloader);
+
  private:
   friend class ExtensionUpdaterTest;
   friend class ExtensionUpdaterFileHandler;
