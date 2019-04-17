bab8e581344b56d065857cc4e8a330d9a2d7e9f0
Will Cassella <cassew@google.com>
2019-04-17 18:19:37 +0000

    Account for inverted selection when determining if the user has backspaced.
    The omnibox disables autocompletion if the user has backspaced - detected
    by checking the current text size against the previous text size. An exception
    to this is trailing selected text, which is not considered when comparing the size.
    
    Previously only the selection_end field was used to determine the size of the selection,
    which is incorrect in the case of an inverted selection (where the selection start is
    greater than the end). This fixes that.
    
    Bug: 951375
    Change-Id: Ie245db06570e3b6df8d41a7645d60429973eecf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1570751
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Will Cassella <cassew@google.com>
    Cr-Commit-Position: refs/heads/master@{#651814}
    Former-commit-id: d3a4183811a810ed0a2a6bb546749461b959099b

--- chrome/browser/vr/elements/omnibox_text_field.cc
+++ chrome/browser/vr/elements/omnibox_text_field.cc
@@ -64,8 +64,10 @@ void OmniboxTextField::OnUpdateInput(const EditedText& info) {
     request.prevent_inline_autocomplete = true;
 
   size_t previous_base_size = info.previous.text.size();
-  if (info.previous.selection_end > info.previous.selection_start)
-    previous_base_size = info.previous.selection_start;
+  if (info.previous.selection_end != info.previous.selection_start) {
+    previous_base_size =
+        std::min(info.previous.selection_start, info.previous.selection_end);
+  }
 
   // If the new text is not larger than the previous base text, disable
   // autocomplete, as the user backspaced or removed a selection.
