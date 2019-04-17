ad6154b25a8bb65cd4ad83137f8f45bc6ad17db3
Olya Kalitova <okalitova@chromium.org>
2019-04-17 12:17:33 +0000

    Add handling of cancelled PluginVm image downloads
    PluginVm image downloads that are tracked by download service when it is
    initialized are cancelled once client receives information about them.
    Before this CL information that these downloads were cancelled was
    propagated to PluginVmImageManager which we do not want. So this Cl
    stops this information for being propagated.
    
    Bug: 951338
    Change-Id: I40b1a7251241cc99e7886dbb0437d6671e461ca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1564126
    Reviewed-by: Alex Oldemeier <aoldemeier@chromium.org>
    Commit-Queue: Olya Kalitova <okalitova@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651681}
    Former-commit-id: 341212ac6f0e86d8041c45b29bf6a508d6549b72

--- chrome/browser/chromeos/plugin_vm/plugin_vm_image_download_client.cc
+++ chrome/browser/chromeos/plugin_vm/plugin_vm_image_download_client.cc
@@ -33,6 +33,7 @@ void PluginVmImageDownloadClient::OnServiceInitialized(
   // TODO(okalitova): Manage downloads after sleep and log out.
   for (const auto& download : downloads) {
     VLOG(1) << "Download tracked by DownloadService: " << download.guid;
+    old_downloads_.insert(download.guid);
     DownloadServiceFactory::GetForBrowserContext(profile_)->CancelDownload(
         download.guid);
   }
@@ -47,7 +48,15 @@ download::Client::ShouldDownload PluginVmImageDownloadClient::OnDownloadStarted(
     const std::vector<GURL>& url_chain,
     const scoped_refptr<const net::HttpResponseHeaders>& headers) {
   VLOG(1) << __func__ << " called";
-  content_length_ = headers->GetContentLength();
+  // We do not want downloads that are tracked by download service from its
+  // initialization to proceed.
+  if (old_downloads_.find(guid) != old_downloads_.end())
+    return download::Client::ShouldDownload::ABORT;
+
+  if (headers)
+    content_length_ = headers->GetContentLength();
+  else
+    content_length_ = -1;
   GetManager()->OnDownloadStarted();
   return download::Client::ShouldDownload::CONTINUE;
 }
@@ -55,6 +64,7 @@ download::Client::ShouldDownload PluginVmImageDownloadClient::OnDownloadStarted(
 void PluginVmImageDownloadClient::OnDownloadUpdated(const std::string& guid,
                                                     uint64_t bytes_uploaded,
                                                     uint64_t bytes_downloaded) {
+  DCHECK(old_downloads_.find(guid) == old_downloads_.end());
   VLOG(1) << __func__ << " called";
   VLOG(1) << bytes_downloaded << " bytes downloaded";
   GetManager()->OnDownloadProgressUpdated(bytes_downloaded, content_length_);
@@ -86,6 +96,11 @@ void PluginVmImageDownloadClient::OnDownloadFailed(
       break;
   }
 
+  // We do not want to notify PluginVmImageManager about the status of
+  // downloads that are tracked by download service from its initialization.
+  if (old_downloads_.find(guid) != old_downloads_.end())
+    return;
+
   if (reason == download::Client::FailureReason::CANCELLED)
     GetManager()->OnDownloadCancelled();
   else
@@ -95,6 +110,7 @@ void PluginVmImageDownloadClient::OnDownloadFailed(
 void PluginVmImageDownloadClient::OnDownloadSucceeded(
     const std::string& guid,
     const download::CompletionInfo& completion_info) {
+  DCHECK(old_downloads_.find(guid) == old_downloads_.end());
   VLOG(1) << __func__ << " called";
   VLOG(1) << "Downloaded file is in " << completion_info.path.value();
   GetManager()->OnDownloadCompleted(completion_info);
@@ -110,6 +126,7 @@ bool PluginVmImageDownloadClient::CanServiceRemoveDownloadedFile(
 void PluginVmImageDownloadClient::GetUploadData(
     const std::string& guid,
     download::GetUploadDataCallback callback) {
+  DCHECK(old_downloads_.find(guid) == old_downloads_.end());
   VLOG(1) << __func__ << " called";
   base::SequencedTaskRunnerHandle::Get()->PostTask(
       FROM_HERE, base::BindOnce(std::move(callback), nullptr));
