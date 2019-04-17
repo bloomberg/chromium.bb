38fe096451e8be3a4f7b5b73f0d462d900e6eb2c
Yoav Weiss <yoavweiss@chromium.org>
2019-04-17 09:21:50 +0000

    Whitelist kApplicationCacheInstalledButNoManifest for UKM
    Change-Id: Iadd9d9dc0d88c7c1dc3564b4a4a57c8d0a74e0b6
    
    Bug: 788757
    Change-Id: Iadd9d9dc0d88c7c1dc3564b4a4a57c8d0a74e0b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1565506
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651655}
    Former-commit-id: 3ec8c2cd51d32bcac272551120ce16dce1930bcf

--- chrome/browser/page_load_metrics/observers/use_counter/ukm_features.cc
+++ chrome/browser/page_load_metrics/observers/use_counter/ukm_features.cc
@@ -34,6 +34,7 @@ UseCounterPageLoadMetricsObserver::GetAllowedUkmFeatures() {
           // kDataUriHasOctothorpe may not be recorded correctly for iframes.
           // See https://crbug.com/796173 for details.
           WebFeature::kDataUriHasOctothorpe,
+          WebFeature::kApplicationCacheInstalledButNoManifest,
           WebFeature::kApplicationCacheManifestSelectInsecureOrigin,
           WebFeature::kApplicationCacheManifestSelectSecureOrigin,
           WebFeature::kMixedContentAudio,
