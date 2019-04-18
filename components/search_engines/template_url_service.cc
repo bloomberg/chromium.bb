270926ba6818b1b2323fd96be11423799752b050
Mikel Astiz <mastiz@chromium.org>
2019-04-18 12:51:48 +0000

    Fix duplicated prepopulated search engines via sync
    Prior to this patch, random GUIDs were used for prepopulated search
    engines, which relied on a best-effort matching on the client side to
    avoid duplicates.
    
    This usually works well, except in rare cases like two clients enabling
    sync simultaneously, or slowness in the sync server.
    
    After this patch, the GUID for prepopulated search engines is generated
    deterministically based on the ID present in the JSON file. This means
    the more robust sync's built-in mechanisms for dedupping (namely the
    client tags) guarantee that prepopulated search engines do not get
    duplicated.
    
    The patch is believed to fix flakiness in sync integration tests,
    namely all tests in suite TwoClientSearchEngineSyncTest.
    
    Bug: 953711
    Change-Id: I8d96d81a25a51f16d57fd70fee10ce2eae633876
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1572345
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652129}
    Former-commit-id: b1c1caca2b80e7da6a9f49489b08a17cf905c0e0

--- components/search_engines/template_url_service.cc
+++ components/search_engines/template_url_service.cc
@@ -11,7 +11,6 @@
 #include "base/callback.h"
 #include "base/debug/crash_logging.h"
 #include "base/format_macros.h"
-#include "base/guid.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/stl_util.h"
 #include "base/strings/string_split.h"
@@ -2089,7 +2088,7 @@ void TemplateURLService::UpdateProvidersCreatedByPolicy(
     default_search_provider_source_ = DefaultSearchManager::FROM_POLICY;
     TemplateURLData new_data(*default_from_prefs);
     if (new_data.sync_guid.empty())
-      new_data.sync_guid = base::GenerateGUID();
+      new_data.GenerateSyncGUID();
     new_data.created_by_policy = true;
     std::unique_ptr<TemplateURL> new_dse_ptr =
         std::make_unique<TemplateURL>(new_data);
@@ -2321,7 +2320,7 @@ void TemplateURLService::PatchMissingSyncGUIDs(
     DCHECK(template_url);
     if (template_url->sync_guid().empty() &&
         (template_url->type() == TemplateURL::NORMAL)) {
-      template_url->data_.sync_guid = base::GenerateGUID();
+      template_url->data_.GenerateSyncGUID();
       if (web_data_service_)
         web_data_service_->UpdateKeyword(template_url->data());
     }
