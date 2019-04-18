bb100be69726b59bec4bea4575ee8d7fca8f031b
Varun Khaneja <vakh@chromium.org>
2019-04-18 20:21:18 +0000

    [M74] Make MaybeCaptureSafetyNetId() a NO-OP for now.
    This change needs to be merged back to M74 too :(
    For M75, I'll send a follow-upCL that enables this feature behind a
    Finch flag that's disabled by default.
    
    The relevant GmsCore release should be out soon so it should be safe for
    M75 release.
    
    R=drubery
    TBR=vakh@chromium.org
    
    (cherry picked from commit 204b62014847319bb4df634da93a8ca33959be4e [formerly 6298d31bc55dfef55fc2913256c8bba22e9f0bcd])
    
    Bug: 954255
    Change-Id: Iebb0f78aa4fd6e5219ffa3645c98fcf1b5d32244
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1574143
    Auto-Submit: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#652261}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1574679
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3729@{#883}
    Cr-Branched-From: 19260737901ea731c9baac505269e4ad9638530d [formerly d4a8972e30b604f090aeda5dfff68386ae656267]-refs/heads/master@{#638880}
    Former-commit-id: 8437b19e1bf5628f065429f05a9cd32018553a2d

--- chrome/browser/safe_browsing/telemetry/android/android_telemetry_service.cc
+++ chrome/browser/safe_browsing/telemetry/android/android_telemetry_service.cc
@@ -240,18 +240,9 @@ void AndroidTelemetryService::FillReferrerChain(
 
 void AndroidTelemetryService::MaybeCaptureSafetyNetId() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  DCHECK(sb_service_->database_manager());
-  if (!safety_net_id_on_ui_thread_.empty() ||
-      !sb_service_->database_manager()->IsSupported()) {
-    return;
-  }
-
-  base::PostTaskWithTraitsAndReplyWithResult(
-      FROM_HERE, {content::BrowserThread::IO},
-      base::BindOnce(&SafeBrowsingDatabaseManager::GetSafetyNetId,
-                     sb_service_->database_manager()),
-      base::BindOnce(&AndroidTelemetryService::SetSafetyNetIdOnUIThread,
-                     weak_ptr_factory_.GetWeakPtr()));
+  // NO-OP for now, similar to M73. Will be enabled via a Finch flag
+  // that's disabled by default later.
+  return;
 }
 
 void AndroidTelemetryService::MaybeSendApkDownloadReport(
