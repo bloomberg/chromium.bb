883fa7ddb430e2ee1353f9886fa482df4e255371
Matt Reynolds <mattreynolds@google.com>
2019-04-18 02:57:03 +0000

    Add IDL and stub implementations for WebHID types
    This CL adds IDL for types that will be used for the WebHID API.
    
    BUG=890096
    
    Change-Id: I4f4db2250aec05c38516fd663d52a3434c28c8b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1559344
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652018}
    Former-commit-id: 03006281aa823d5fa0b2e72ad12d514221433970

--- third_party/blink/renderer/modules/hid/hid.cc
+++ third_party/blink/renderer/modules/hid/hid.cc
@@ -5,7 +5,11 @@
 #include "third_party/blink/renderer/modules/hid/hid.h"
 
 #include "third_party/blink/public/mojom/feature_policy/feature_policy.mojom-blink.h"
+#include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
+#include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
+#include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/modules/event_target_modules.h"
+#include "third_party/blink/renderer/modules/hid/hid_connection_event.h"
 
 namespace blink {
 
@@ -13,12 +17,32 @@ HID::HID(ExecutionContext& context) : ContextLifecycleObserver(&context) {}
 
 HID::~HID() = default;
 
+ScriptPromise HID::getDevices(ScriptState* script_state) {
+  return ScriptPromise::RejectWithDOMException(
+      script_state, DOMException::Create(DOMExceptionCode::kNotSupportedError,
+                                         "Not supported."));
+}
+
+ScriptPromise HID::requestDevice(ScriptState* script_state,
+                                 const HIDDeviceRequestOptions* options) {
+  return ScriptPromise::RejectWithDOMException(
+      script_state, DOMException::Create(DOMExceptionCode::kNotSupportedError,
+                                         "Not supported."));
+}
+
 ExecutionContext* HID::GetExecutionContext() const {
   return ContextLifecycleObserver::GetExecutionContext();
 }
 
 const AtomicString& HID::InterfaceName() const {
   return event_target_names::kHID;
+}
+
+void HID::AddedEventListener(const AtomicString& event_type,
+                             RegisteredEventListener& listener) {
+  EventTargetWithInlineData::AddedEventListener(event_type, listener);
+  // TODO(mattreynolds): Connect to the HID service and register for connect
+  // and disconnect events.
 }
 
 FeatureEnabledState HID::GetFeatureEnabledState() const {
--- third_party/blink/renderer/modules/hid/hid.h
+++ third_party/blink/renderer/modules/hid/hid.h
@@ -5,26 +5,43 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_HID_HID_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_HID_HID_H_
 
+#include "services/device/public/mojom/hid.mojom-blink.h"
+#include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/core/dom/events/event_target.h"
 #include "third_party/blink/renderer/core/execution_context/context_lifecycle_observer.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/execution_context/security_context.h"
+#include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 
 namespace blink {
+
+class HIDDeviceRequestOptions;
+class ScriptState;
 
 class HID : public EventTargetWithInlineData, public ContextLifecycleObserver {
   DEFINE_WRAPPERTYPEINFO();
   USING_GARBAGE_COLLECTED_MIXIN(HID);
 
  public:
-  explicit HID(ExecutionContext&);
+  explicit HID(ExecutionContext& context);
   ~HID() override;
+
+  // hid.idl
+  ScriptPromise getDevices(ScriptState*);
+  ScriptPromise requestDevice(ScriptState*, const HIDDeviceRequestOptions*);
+  DEFINE_ATTRIBUTE_EVENT_LISTENER(connect, kConnect)
+  DEFINE_ATTRIBUTE_EVENT_LISTENER(disconnect, kDisconnect)
 
   // EventTarget overrides.
   ExecutionContext* GetExecutionContext() const override;
   const AtomicString& InterfaceName() const override;
 
   void Trace(blink::Visitor*) override;
+
+ protected:
+  // EventTarget protected overrides.
+  void AddedEventListener(const AtomicString& event_type,
+                          RegisteredEventListener&) override;
 
  private:
   FeatureEnabledState GetFeatureEnabledState() const;
