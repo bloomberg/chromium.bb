e47403fcaad369e05404b8c421cd09274e5100eb
Jeongwoo Park <skeksk91@gmail.com>
2019-04-18 11:16:25 +0000

    Change old comment that points nothing.
    No more listeners are used.
    The listeners were replaced by pending queue logic.
    
    Change-Id: I89c70a030a4ba5442fee4f7b42a253642816a6b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1569375
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652107}
    Former-commit-id: 38cfd073cbb0bb0b5eaaa7e91f7b3e98980cfeff

--- base/android/java/src/org/chromium/base/process_launcher/ChildConnectionAllocator.java
+++ base/android/java/src/org/chromium/base/process_launcher/ChildConnectionAllocator.java
@@ -45,7 +45,7 @@ public class ChildConnectionAllocator {
         }
     }
 
-    // Delay between the call to freeConnection and the connection actually beeing   freed.
+    // Delay between the call to freeConnection and the connection actually beeing freed.
     private static final long FREE_CONNECTION_DELAY_MILLIS = 1;
 
     // The handler of the thread on which all interations should happen.
@@ -225,7 +225,7 @@ public class ChildConnectionAllocator {
         return connection;
     }
 
-    /** Frees a connection and notifies listeners. */
+    /** Free connection allocated by this allocator. */
     private void free(ChildProcessConnection connection) {
         assert isRunningOnLauncherThread();
 
