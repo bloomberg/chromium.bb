5f70fb46bbdc9a5e573f2abf74a84f2d576b6e27
Takashi Toyoshima <toyoshim@chromium.org>
2019-04-18 11:10:34 +0000

    OOR-CORS: Store Purpose header value in cors_exempt_header_list
    Link prefetch sets 'Purpose: prefetch' header in Blink. This internal
    header should not affect CORS checks, but if OOR-CORS is enabled, it
    unexpectedly does.
    
    For a permanent fix, we are working on renaming the Purpose header to
    Sec-Purpose so that CORS specs can explicitly cover this header, but
    for m75 with OOR-CORS, we tentatively use cors_exempt_header_list
    so that it behaves in the same way regardless of the OOR-CORS flag.
    
    This change will be reverted once the spec discussion finishes, then
    http_names::kSecPurpose will be defined and used.
    
    Bug: 942976
    Change-Id: I392f4fc0bf7ee85696b27c9450215e7f52e64467
    Tbr: halliwell@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1573306
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652104}
    Former-commit-id: e22c9cb744ea22ca9007be753e59714083c7c3df

--- content/common/content_constants_internal.cc
+++ content/common/content_constants_internal.cc
@@ -37,6 +37,7 @@ const char kDoNotTrackHeader[] = "DNT";
 const char kMachBootstrapName[] = "rohitfork";
 #endif
 
+const char kCorsExemptPurposeHeaderName[] = "Purpose";
 const char kCorsExemptRequestedWithHeaderName[] = "X-Requested-With";
 
 } // namespace content
