2acc208dc32c7d97469a47ec1fba1236e7e96284
Brandon Tolsch <btolsch@chromium.org>
2019-04-18 04:28:33 +0000

    Revert "Improve presubmit support"
    This reverts commit f0be9de6149a73efbd98506541a6758c970f1b0f.
    
    Reason for revert: presubmit recipe is failing for an unknown reason, reverting until I can get more info from the infra team.
    
    Original change's description:
    > Improve presubmit support
    > 
    > This change improves our presubmit in two ways:
    >  1. It adds our PRESUBMIT.sh as a step in PRESUBMIT.py which is normally
    >     run by git-cl commands.
    >  2. Adds a presubmit bot which runs our PRESUBMIT.py as part of the
    >     commit queue.
    > 
    > Moving the PRESUBMIT.sh checks into python or adding other checks will
    > be left to future CLs.
    > 
    > Bug: openscreen:43
    > Change-Id: I7d95fe92b9b6c715344bc65868cd1eb91e7be643
    > Reviewed-on: https://chromium-review.googlesource.com/c/openscreen/+/1570449
    > Commit-Queue: Brandon Tolsch <btolsch@chromium.org>
    > Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    
    TBR=mfoltz@chromium.org,btolsch@chromium.org
    
    Change-Id: Ie7cf7688a74ee5168767428e48b5aeb4cdb00965
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: openscreen:43
    Reviewed-on: https://chromium-review.googlesource.com/c/openscreen/+/1573270
    Reviewed-by: Brandon Tolsch <btolsch@chromium.org>
    Commit-Queue: Brandon Tolsch <btolsch@chromium.org>

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -3,26 +3,5 @@
 # found in the LICENSE file.
 
 
-def _CommonChecks(input_api, output_api):
-  results = []
-  # TODO(issues/43): Probably convert this to python so we can give more
-  # detailed errors.
-  presubmit_sh_result = input_api.subprocess.call(
-      input_api.PresubmitLocalPath() + '/PRESUBMIT.sh')
-  if presubmit_sh_result != 0:
-    results.append(output_api.PresubmitError('PRESUBMIT.sh failed'))
-  return results
-
-
 def CheckChangeOnUpload(input_api, output_api):
-  results = []
-  results.extend(_CommonChecks(input_api, output_api))
-  results.extend(
-      input_api.canned_checks.CheckChangedLUCIConfigs(input_api, output_api))
-  return results
-
-
-def CheckChangeOnCommit(input_api, output_api):
-  results = []
-  results.extend(_CommonChecks(input_api, output_api))
-  return results
+  return input_api.canned_checks.CheckChangedLUCIConfigs(input_api, output_api)
