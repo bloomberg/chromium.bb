0b5165725531587137dabae9bc43d307b37e46a7
danakj <danakj@chromium.org>
2019-04-18 16:35:17 +0000

    Change dbus Get/Set Property callback to OnceCallback.
    These callbacks are only called once, so change them to the correct
    spelling. This will allow bluetooth code to be converted to OnceCallback
    as well, which will then allow content code to be converted.
    
    TBR=hashimoto,rkc
    
    Bug: 953861
    Change-Id: I49b0fcf6e00721e5e6bbb4439bad7c5cbb0d6463
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1572204
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652169}
    Former-commit-id: c78039d8ab7b89af14e67f8be69c1843f2be2097

--- dbus/property.cc
+++ dbus/property.cc
@@ -105,12 +105,9 @@ void PropertySet::Get(PropertyBase* property, GetCallback callback) {
   writer.AppendString(property->name());
 
   DCHECK(object_proxy_);
-  object_proxy_->CallMethod(&method_call,
-                            ObjectProxy::TIMEOUT_USE_DEFAULT,
-                            base::Bind(&PropertySet::OnGet,
-                                       GetWeakPtr(),
-                                       property,
-                                       callback));
+  object_proxy_->CallMethod(&method_call, ObjectProxy::TIMEOUT_USE_DEFAULT,
+                            base::BindOnce(&PropertySet::OnGet, GetWeakPtr(),
+                                           property, std::move(callback)));
 }
 
 void PropertySet::OnGet(PropertyBase* property, GetCallback callback,
@@ -132,7 +129,7 @@ void PropertySet::OnGet(PropertyBase* property, GetCallback callback,
   }
 
   if (!callback.is_null())
-    callback.Run(response);
+    std::move(callback).Run(response);
 }
 
 bool PropertySet::GetAndBlock(PropertyBase* property) {
@@ -196,12 +193,9 @@ void PropertySet::Set(PropertyBase* property, SetCallback callback) {
   property->AppendSetValueToWriter(&writer);
 
   DCHECK(object_proxy_);
-  object_proxy_->CallMethod(&method_call,
-                            ObjectProxy::TIMEOUT_USE_DEFAULT,
-                            base::Bind(&PropertySet::OnSet,
-                                       GetWeakPtr(),
-                                       property,
-                                       callback));
+  object_proxy_->CallMethod(&method_call, ObjectProxy::TIMEOUT_USE_DEFAULT,
+                            base::BindOnce(&PropertySet::OnSet, GetWeakPtr(),
+                                           property, std::move(callback)));
 }
 
 bool PropertySet::SetAndBlock(PropertyBase* property) {
@@ -224,7 +218,7 @@ void PropertySet::OnSet(PropertyBase* property,
                         Response* response) {
   LOG_IF(WARNING, !response) << property->name() << ": Set: failed.";
   if (!callback.is_null())
-    callback.Run(response);
+    std::move(callback).Run(response);
 }
 
 bool PropertySet::UpdatePropertiesFromReader(MessageReader* reader) {
