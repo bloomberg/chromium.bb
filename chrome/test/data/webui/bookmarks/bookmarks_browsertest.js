9fa6cc2df8892f8e1a8704b681b8bb8479bb5028
Christopher Lam <calamity@chromium.org>
2019-04-18 05:29:44 +0000

    [Bookmarks] Port bookmarkManagerPrivate ExtensionAPITests to WebUI.
    This CL removes the ExtensionAPITests for bookmarkManagerPrivate to
    WebUI as the WebUI is the only consumer of this API. In order to remove
    the API from Extension manifests, we need to move the test away from
    the ExtensionAPITest framework, so it has been moved to the
    chrome://bookmarks WebUI tests instead.
    
    Bug: 938695
    Change-Id: I11ef89876c73a73a0da64988d170e02f560223b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1566269
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652043}
    Former-commit-id: 1cb36120ecb030c5a209d638d6519c2bbe86865d

--- chrome/test/data/webui/bookmarks/bookmarks_browsertest.js
+++ chrome/test/data/webui/bookmarks/bookmarks_browsertest.js
@@ -242,3 +242,39 @@ BookmarksUtilTest.prototype = {
 TEST_F('BookmarksUtilTest', 'All', function() {
   mocha.run();
 });
+
+function BookmarksExtensionAPITest() {}
+
+BookmarksExtensionAPITest.prototype = {
+  __proto__: BookmarksBrowserTest.prototype,
+
+  extraLibraries: BookmarksBrowserTest.prototype.extraLibraries.concat([
+    'extension_api_test.js',
+  ]),
+
+  testGenPreamble: () => {
+    GEN('SetupExtensionAPITest();');
+  }
+};
+
+TEST_F('BookmarksExtensionAPITest', 'All', function() {
+  mocha.run();
+});
+
+function BookmarksExtensionAPIEditDisabledTest() {}
+
+BookmarksExtensionAPIEditDisabledTest.prototype = {
+  __proto__: BookmarksBrowserTest.prototype,
+
+  extraLibraries: BookmarksBrowserTest.prototype.extraLibraries.concat([
+    'extension_api_test_edit_disabled.js',
+  ]),
+
+  testGenPreamble: () => {
+    GEN('SetupExtensionAPIEditDisabledTest();');
+  }
+};
+
+TEST_F('BookmarksExtensionAPIEditDisabledTest', 'All', function() {
+  mocha.run();
+});
