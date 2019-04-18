231dc769a792e0f57e446bd1efbc0f7c0618cbec
Keishi Hattori <keishi@chromium.org>
2019-04-18 08:24:13 +0000

    Revert "[Image Fetcher] Replace AnswersImageFetcher with CachedImageFetcher"
    This reverts commit bc7dd996ff1b2b2a3c1654637afc43c4f3d5bc79 [formerly 9f25017463ee6cbfa63166b152212b5f705af328].
    
    Reason for revert: crbug.com/954086 Omnibox related tests are crashing on Android CFI
    
    Original change's description:
    > [Image Fetcher] Replace AnswersImageFetcher with CachedImageFetcher
    > 
    > Removing a duplicate cached image fetcher here.
    > 
    > Bug: 934950
    > Change-Id: I0063aa8e26cbd4c0a0acb7f3c59f4ee86116e2db
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1481955
    > Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    > Reviewed-by: Theresa <twellington@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#651905}
    
    TBR=rkaplow@chromium.org,tedchoc@chromium.org,twellington@chromium.org,wylieb@chromium.org,ender@google.com
    
    Change-Id: I298cf768003b4b034e0a340fa733250f146f8ce6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 934950
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1573437
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652076}
    Former-commit-id: 58f434938f0176e028eb187529318dbdee826f5b

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
@@ -193,12 +193,6 @@ public class LocationBarLayout extends FrameLayout
     }
 
     @Override
-    public void destroy() {
-        mAutocompleteCoordinator.destroy();
-        mAutocompleteCoordinator = null;
-    }
-
-    @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
 
--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediator.java
@@ -93,7 +93,7 @@ class AutocompleteMediator
     private final Handler mHandler;
     private final BasicSuggestionProcessor mBasicSuggestionProcessor;
     private EditUrlSuggestionProcessor mEditUrlProcessor;
-    private AnswerSuggestionProcessor mAnswerSuggestionProcessor;
+    private final AnswerSuggestionProcessor mAnswerSuggestionProcessor;
 
     private ToolbarDataProvider mDataProvider;
     private boolean mNativeInitialized;
@@ -155,11 +155,6 @@ class AutocompleteMediator
         mAnswerSuggestionProcessor = new AnswerSuggestionProcessor(mContext, this, textProvider);
         mEditUrlProcessor = new EditUrlSuggestionProcessor(
                 delegate, (suggestion) -> onSelection(suggestion, 0));
-    }
-
-    public void destroy() {
-        mAnswerSuggestionProcessor.destroy();
-        mAnswerSuggestionProcessor = null;
     }
 
     @Override
