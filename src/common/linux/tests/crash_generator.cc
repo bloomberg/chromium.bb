8c70c504b241f918e41f28a3102f5ca03475082b
Mark Mentovai <mark@chromium.org>
2019-04-19 17:08:56 -0400

    Make breakpad_unittests work with Chrome's test runner instead of gtest's
    Chrome's test runner on Linux installs its own StackDumpSignalHandler
    which swallows signals and doesn't re-raise them. This is sloppy, but
    apparently there are reasons (https://crbug.com/551681). For
    breakpad_unittests, it causes problems where a test process expects (via
    waitpid()) to observe a child crash.  Deal with those cases by
    explicitly restoring the default signal handler.
    
    In another case, Chrome's test runner seems to have been arriving at the
    conclusion that it was to expect output from a child. Transitioning from
    exit() to _exit() fixes this problem, and it's not necessarily a bad
    idea to do this in post-fork() children without an execve() anyway.
    
    Bug: chromium:949098
    Change-Id: I5a6af0c2a09cd8eac9998358f6d5ea665288236f
    Reviewed-on: https://chromium-review.googlesource.com/c/breakpad/breakpad/+/1575670
    Reviewed-by: Mark Mentovai <mark@chromium.org>

--- src/common/linux/tests/crash_generator.cc
+++ src/common/linux/tests/crash_generator.cc
@@ -184,6 +184,12 @@ bool CrashGenerator::CreateChildCrash(
 
   pid_t pid = fork();
   if (pid == 0) {
+    // Custom signal handlers, which may have been installed by a test launcher,
+    // are undesirable in this child.
+    if (signal(crash_signal, SIG_DFL) == SIG_ERR) {
+      perror("CrashGenerator: signal");
+      exit(1);
+    }
     if (chdir(temp_dir_.path().c_str()) == -1) {
       perror("CrashGenerator: Failed to change directory");
       exit(1);
