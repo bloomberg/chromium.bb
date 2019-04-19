e6a12ef8ba12b22a58ed32abf06c095338371bff
rajendrant <rajendrant@chromium.org>
2019-04-19 02:33:04 +0000

    Add better check for page ID in chrome-proxy header
    Change-Id: I1e38e6d983a0f46c8ace24bfe19a97600f90675b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1575127
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652452}
    Former-commit-id: 3d140e8c4599f567fcd0d6ed7010c23e16174851

--- chrome/browser/data_reduction_proxy/data_reduction_proxy_browsertest.cc
+++ chrome/browser/data_reduction_proxy/data_reduction_proxy_browsertest.cc
@@ -253,6 +253,7 @@ IN_PROC_BROWSER_TEST_F(DataReductionProxyBrowsertest, ChromeProxyHeaderSet) {
   std::string body = GetBody();
   EXPECT_THAT(body, HasSubstr(kSessionKey));
   EXPECT_THAT(body, HasSubstr("pid="));
+  EXPECT_THAT(body, Not(HasSubstr("pid=0")));
   EXPECT_THAT(body, HasSubstr("s="));
   EXPECT_THAT(body, HasSubstr("c="));
   EXPECT_THAT(body, HasSubstr("b="));
