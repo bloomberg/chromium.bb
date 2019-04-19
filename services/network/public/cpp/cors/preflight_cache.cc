460aa5a2e9c3f4570409d2a2ea064ae87a053edf
Takashi Toyoshima <toyoshim@chromium.org>
2019-04-19 03:11:55 +0000

    OOR-CORS: Introduce an entry limit to the CORS preflight cache
    Since CORS preflight cache can live for a longer time in OOR-CORS mode,
    we introduce an entry limit to the CORS preflight cache.
    Also this patch adds a metric to count cache entries.
    
    Bug: 736308
    Change-Id: Icd0ed0664d4bc592e921a632126ba88d1425f50b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1573519
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652458}
    Former-commit-id: 603bb8dfeeff79c16612ae9fe40e0c8a040ac3cc

--- services/network/public/cpp/cors/preflight_cache.cc
+++ services/network/public/cpp/cors/preflight_cache.cc
@@ -4,13 +4,26 @@
 
 #include "services/network/public/cpp/cors/preflight_cache.h"
 
+#include <iterator>
+
+#include "base/metrics/histogram_functions.h"
+#include "base/rand_util.h"
+#include "base/time/time.h"
 #include "url/gurl.h"
 
 namespace network {
 
 namespace cors {
 
-PreflightCache::PreflightCache() = default;
+namespace {
+constexpr size_t kMaxCacheEntries = 4096;
+}  // namespace
+
+PreflightCache::PreflightCache() {
+  timer_.Start(FROM_HERE, base::TimeDelta::FromHours(1), this,
+               &PreflightCache::ReportMetrics);
+}
+
 PreflightCache::~PreflightCache() = default;
 
 void PreflightCache::AppendEntry(
@@ -18,6 +31,12 @@ void PreflightCache::AppendEntry(
     const GURL& url,
     std::unique_ptr<PreflightResult> preflight_result) {
   DCHECK(preflight_result);
+
+  // Since one new entry is always added below, let's purge one cache entry
+  // if cache size is larger than kMaxCacheEntries - 1 so that the size to be
+  // kMaxCacheEntries at maximum.
+  MayPurge(kMaxCacheEntries - 1);
+
   cache_[origin][url.spec()] = std::move(preflight_result);
 }
 
@@ -58,10 +77,38 @@ size_t PreflightCache::CountOriginsForTesting() const {
 }
 
 size_t PreflightCache::CountEntriesForTesting() const {
+  return CountEntries();
+}
+
+void PreflightCache::MayPurgeForTesting(size_t max_entries) {
+  MayPurge(max_entries);
+}
+
+size_t PreflightCache::CountEntries() const {
   size_t entries = 0;
   for (auto const& cache_per_origin : cache_)
     entries += cache_per_origin.second.size();
   return entries;
+}
+
+void PreflightCache::MayPurge(size_t max_entries) {
+  if (CountEntries() <= max_entries)
+    return;
+  auto target_origin_cache = cache_.begin();
+  std::advance(target_origin_cache, base::RandInt(0, cache_.size() - 1));
+  if (target_origin_cache->second.size() == 1) {
+    cache_.erase(target_origin_cache);
+  } else {
+    auto target_cache_entry = target_origin_cache->second.begin();
+    std::advance(target_cache_entry,
+                 base::RandInt(0, target_origin_cache->second.size() - 1));
+    target_origin_cache->second.erase(target_cache_entry);
+  }
+}
+
+void PreflightCache::ReportMetrics() {
+  base::UmaHistogramCounts10000("Net.Cors.PreflightCacheEntries",
+                                CountEntries());
 }
 
 }  // namespace cors
