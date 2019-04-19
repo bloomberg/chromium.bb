edf08e47974594f962cdb8f001f3b5f8b4838a18
asteinha <asteinhauser@google.com>
2019-04-19 21:16:18 +0000

    Removing usages of a deleted flag. Deleted in https://chromium.googlesource.com/chromium/src.git/+/bc64dd0178b73a422ed5d3d10bd6296606ca2cdb [formerly ba49d00fddeb39c0816090dad7d359df0dce9a1a] Bug: 74848
    Change-Id: Ia3d5c796ad34857f4e84fdb3a6f53fce3f00d63b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1574829
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Commit-Queue: Anthony Steinhauser <asteinhauser@google.com>
    Cr-Commit-Position: refs/heads/master@{#652638}
    Former-commit-id: 7a7d567331594a11a9e3e2036256b2e3740b2bd6

--- ppapi/native_client/tools/browser_tester/browsertester/browserlauncher.py
+++ ppapi/native_client/tools/browser_tester/browsertester/browserlauncher.py
@@ -279,7 +279,6 @@ class ChromeLauncher(BrowserLauncher):
             '--no-first-run',
             '--no-default-browser-check',
             '--log-level=1',
-            '--safebrowsing-disable-auto-update',
             '--disable-default-apps',
             # Suppress metrics reporting.  This prevents misconfigured bots,
             # people testing at their desktop, etc from poisoning the UMA data.
