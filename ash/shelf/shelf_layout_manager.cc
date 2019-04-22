c5d7be6c5a01eefad5b031e4a39c584c409a5138
Aga Wronska <agawronska@chromium.org>
2019-04-22 23:38:34 +0000

    Fix status area color when immersive app is opened
    Status area background color was incorrect when switching from
    KioskNext home to immersive app, because shelf turns to auto-hide
    mode then and both shelf and status area are not visible.
    
    Bug: 949289
    Change-Id: I28213355ea0f65d7638bd59714e50fbaceef3371
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1577944
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653016}
    Former-commit-id: b026d4726473507cf849cb4982d3ce1e1c725f3a

--- ash/shelf/shelf_layout_manager.cc
+++ ash/shelf/shelf_layout_manager.cc
@@ -1299,8 +1299,6 @@ bool ShelfLayoutManager::CalculateStatusAreaVisibility(
 bool ShelfLayoutManager::ShouldRepaintStatusAreaOnStateChange(
     ShelfLayoutManager::State old_state,
     ShelfLayoutManager::State new_state) const {
-  if (!new_state.is_status_area_visible)
-    return false;
 
   if (old_state.visibility_state == new_state.visibility_state)
     return false;
