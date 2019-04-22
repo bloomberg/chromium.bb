b704e2a2317328be71a5c663577af33f8e608197
David Benjamin <davidben@chromium.org>
2019-04-22 17:19:19 +0000

    Fix invalid StringPiece to string conversion.
    Found by the newly-added fuzzer. Fortunately this is not a problem for
    any existing callers, which only ever passed a StringPiece derived from
    a full std::string (which promises to be NUL-terminated).
    
    Bug: 954703, 954760
    Change-Id: I274de66b785d9f272a25264980e21878c5a77872
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1577580
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#652868}
    Former-commit-id: 545e4cfa6437631e03ef0800b02893a2095cad73

--- components/domain_reliability/header.cc
+++ components/domain_reliability/header.cc
@@ -28,8 +28,8 @@ class DirectiveHeaderValueParser {
     SYNTAX_ERROR
   };
 
-  DirectiveHeaderValueParser(base::StringPiece value)
-      : value_(value.data()),
+  explicit DirectiveHeaderValueParser(base::StringPiece value)
+      : value_(value.as_string()),
         tokenizer_(value_.begin(), value_.end(), ";= "),
         stopped_with_error_(false) {
     tokenizer_.set_options(base::StringTokenizer::RETURN_DELIMS);
@@ -134,6 +134,8 @@ class DirectiveHeaderValueParser {
     return BEFORE_VALUE;
   }
 
+  // TODO(https://crbug.com/820198): This could take a StringPiece once
+  // StringTokenizer is made StringPiece-friendly.
   std::string value_;
   base::StringTokenizer tokenizer_;
 
