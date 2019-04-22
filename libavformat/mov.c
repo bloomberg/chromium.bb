d82e355b42a185683ce8c19ccb27e162c9ff66e8
Dan Sanders <sandersd@chromium.org>
2019-04-22 11:08:48 -0700

    libavformat/mov: limit nb_frames_for_fps to INT_MAX
    Part 2.
    
    Bug: 827894
    Change-Id: Icf597a26f817f342cf120f0c9c17a95122a45797
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/third_party/ffmpeg/+/1577698
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -4923,7 +4923,7 @@ static int mov_read_trun(MOVContext *c, AVIOContext *pb, MOVAtom atom)
         sc->data_size += sample_size;
 
         if (sample_duration <= INT64_MAX - sc->duration_for_fps &&
-            1 <= INT64_MAX - sc->nb_frames_for_fps
+            1 <= INT_MAX - sc->nb_frames_for_fps
         ) {
             sc->duration_for_fps += sample_duration;
             sc->nb_frames_for_fps ++;
