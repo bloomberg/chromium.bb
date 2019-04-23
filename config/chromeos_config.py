1b47c3c54ee51c09943ecd0fe8555c1c7ff4eb1b
Bob Moragues <moragues@google.com>
2019-04-23 10:52:38 -0700

    atlas: remove erroneous skylab configuration
    BUG=chromium:955473
    TEST=none
    
    Fix incorrect skylab configuration introduced by
    https://chromium-review.googlesource.com/c/chromiumos/chromite/+/1516421
    
    End goal is to get hardware tests running again and marked important.
    
    Change-Id: Ifb5f22ee0dd78cfaa9e12b5e83824b8dfdc9999b
    Reviewed-on: https://chromium-review.googlesource.com/1578592
    Commit-Ready: Bob Moragues <moragues@chromium.org>
    Tested-by: Bob Moragues <moragues@chromium.org>
    Reviewed-by: YH Lin <yueherngl@chromium.org>
    Reviewed-by: Nick Sanders <nsanders@chromium.org>

--- config/chromeos_config.py
+++ config/chromeos_config.py
@@ -2187,7 +2187,6 @@ def CqBuilders(site_config, boards_dict, ge_build_config):
   ])
 
   _paladin_enable_skylab_hwtest = frozenset([
-      'atlas',
       'auron_paine',
       'auron_yuna',
       'bob',
