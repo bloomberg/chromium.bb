c761e492ba8d1ce55d7fe65d282bef3327177ffd
Boris Sazonov <bsazonov@chromium.org>
2019-04-23 11:33:55 +0000

    [Signin][Android] Reland "Add Java side of ConsistencyCookieManager"
    Relands https://crrev.com/c/1565880 that broke UBSan build:
    https://crbug.com/954204. The only difference in this CL is that
    ConsistencyCookieManagerBase is now built on all platforms.
    
    Adds ConsistencyCookieManager Java class that listens to
    AccountManagerFacade.isUpdatePending and exposes it to the C++ code.
    
    TBR=droger@chromium.org
    
    Bug: 949562
    Change-Id: I28165658f298865a54942c056158950825d41090
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1578560
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653132}
    Former-commit-id: 6f00c97c68b1c9497cd5f950fd8322b2c6baed28

--- components/signin/core/browser/consistency_cookie_manager_unittest.cc
+++ components/signin/core/browser/consistency_cookie_manager_unittest.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/signin/core/browser/consistency_cookie_manager_android.h"
+#include "components/signin/core/browser/consistency_cookie_manager_base.h"
 
 #include <memory>
 #include <string>
@@ -49,9 +49,19 @@ class MockCookieManager
            network::mojom::CookieManager::SetCanonicalCookieCallback callback));
 };
 
-class ConsistencyCookieManagerAndroidTest : public ::testing::Test {
+class FakeConsistencyCookieManager
+    : public signin::ConsistencyCookieManagerBase {
  public:
-  ConsistencyCookieManagerAndroidTest()
+  FakeConsistencyCookieManager(SigninClient* signin_client,
+                               AccountReconcilor* reconcilor)
+      : ConsistencyCookieManagerBase(signin_client, reconcilor) {
+    UpdateCookie();
+  }
+};
+
+class ConsistencyCookieManagerTest : public ::testing::Test {
+ public:
+  ConsistencyCookieManagerTest()
       : signin_client_(&pref_service_),
         identity_test_env_(/*test_url_loader_factory=*/nullptr,
                            &pref_service_,
@@ -65,10 +75,12 @@ class ConsistencyCookieManagerAndroidTest : public ::testing::Test {
     reconcilor_ = std::make_unique<AccountReconcilor>(
         identity_test_env_.identity_manager(), &signin_client_,
         std::make_unique<AccountReconcilorDelegate>());
+    reconcilor_->Initialize(/*start_reconcile_if_tokens_available=*/false);
   }
 
-  ~ConsistencyCookieManagerAndroidTest() override { reconcilor_->Shutdown(); }
+  ~ConsistencyCookieManagerTest() override { reconcilor_->Shutdown(); }
 
+  SigninClient* signin_client() { return &signin_client_; }
   AccountReconcilor* reconcilor() { return reconcilor_.get(); }
 
   MockCookieManager* mock_cookie_manager() {
@@ -90,10 +102,12 @@ class ConsistencyCookieManagerAndroidTest : public ::testing::Test {
 };
 
 // Tests that the cookie is updated when the account reconcilor state changes.
-TEST_F(ConsistencyCookieManagerAndroidTest, AccountReconcilorState) {
+TEST_F(ConsistencyCookieManagerTest, AccountReconcilorState) {
   // AccountReconcilor::Initialize() creates the ConsistencyCookieManager.
   mock_cookie_manager()->ExpectSetCookieCall("Consistent");
-  reconcilor()->Initialize(/*start_reconcile_if_tokens_available=*/false);
+  reconcilor()->SetConsistencyCookieManager(
+      std::make_unique<FakeConsistencyCookieManager>(signin_client(),
+                                                     reconcilor()));
   testing::Mock::VerifyAndClearExpectations(mock_cookie_manager());
   ASSERT_EQ(signin_metrics::ACCOUNT_RECONCILOR_OK, reconcilor()->GetState());
 
