bloomberg/chromium.bb

commit 1bd006b397cfdb599d1a6e19588a3905ddac50c5

Renderer UI: Suppress `SynchronizeVisualProperties` sent from browser (#438)
Even though the browser-side HWND is never given a meaningful size or shown,
there are scenarios where the `WidgetMsg_SynchronizeVisualProperties` will
be sent by the browser which data that is inconsistent with the renderer.

This commit causes the `RenderViewObserver` created by the `RenderWebView` to
suppress such messages.

The size of the `WebViewProxy` is also immediately set to 0, 0 to further
convince the browser that its HWND isn't useful.

src/blpwtk2/private/blpwtk2_renderwebview.cc
@@ -97,12 +97,17 @@ namespace blpwtk2 {
 class RenderWebView::RenderViewObserver : public content::RenderViewObserver {
     RenderWebView *d_renderWebView;
 
+    bool d_dispatchingSynchronizeVisualProperties = false;
+
   public:
 
     RenderViewObserver(content::RenderViewImpl *rv, RenderWebView *renderWebView);
     ~RenderViewObserver() override;
 
     void OnDestruct() override;
+    bool OnMessageReceived(const IPC::Message& message) override;
+
+    void DispatchSynchronizeVisualProperties(content::VisualProperties);
 };
 
 RenderWebView::RenderViewObserver::RenderViewObserver(
@@ -123,6 +128,27 @@ void RenderWebView::RenderViewObserver::OnDestruct()
 {
     LOG(INFO) << "Destroyed RenderView, routingId=" << routing_id();
     delete this;
+}
+
+void RenderWebView::RenderViewObserver::DispatchSynchronizeVisualProperties(content::VisualProperties params)
+{
+    d_dispatchingSynchronizeVisualProperties = true;
+
+    d_renderWebView->dispatchToRenderWidget(
+        WidgetMsg_SynchronizeVisualProperties(
+            d_renderWebView->d_renderWidgetRoutingId,
+            params));
+
+    d_dispatchingSynchronizeVisualProperties = false;
+}
+
+bool RenderWebView::RenderViewObserver::OnMessageReceived(const IPC::Message& message)
+{
+    if (message.type() == WidgetMsg_SynchronizeVisualProperties::ID) {
+        return !d_dispatchingSynchronizeVisualProperties;
+    }
+
+    return false;
 }
 
                         // -------------------
@@ -780,9 +806,7 @@ void RenderWebView::updateGeometry()
     params.local_surface_id_allocation = d_compositor->GetLocalSurfaceIdAllocation();
     GetNativeViewScreenInfo(&params.screen_info, d_hwnd.get());
 
-    dispatchToRenderWidget(
-        WidgetMsg_SynchronizeVisualProperties(d_renderWidgetRoutingId,
-            params));
+    d_renderViewObserver->DispatchSynchronizeVisualProperties(params);
 }
 
 void RenderWebView::updateFocus()
@@ -1379,6 +1403,8 @@ void RenderWebView::setSecurityToken(v8::Isolate *isolate,
 void RenderWebView::created(WebView *source)
 {
     d_proxy = source;
+
+    d_proxy->move(0, 0, 0, 0);
 
     static_cast<WebViewProxy *>(d_proxy)->setProxyDelegate(this);
 

commit 1b685c6b84d9b97b3abb3ded5b921e2c336f5f4a

Renderer UI: Check 'd_renderViewObserver' before dereferencing it (#449)
This commit fixes a regression introduced by commit
1bd006b397cfdb599d1a6e19588a3905ddac50c5, which was itself made to suppress
a specific IPC message, `WidgetMsg_SynchronizeVisualProperties`, sent by
the browser.

Not every instance of `blpwtk2::RenderWebView` initializes
`d_renderViewObserver`; in particular, it will not be initializefd by instances
created for renderer-only webviews, such as dropdown menus created for <SELECT>
elements. Furthermore, such webviews do not have browser-side components, and
therefore do not need to suppress the IPC message.

Since `d_renderViewObserver` may by `null,
`blpwtk2::RenderWebView::updateGeometry()` must therefore check its value
before dereferencing it. If its value is null, then
`WidgetMsg_SynchronizeVisualProperties` will be dispatched directly to the
`content::RenderWidget` (which was the behavior before commit
1bd006b397cfdb599d1a6e19588a3905ddac50c5).

src/blpwtk2/private/blpwtk2_renderwebview.cc
@@ -806,7 +806,16 @@ void RenderWebView::updateGeometry()
     params.local_surface_id_allocation = d_compositor->GetLocalSurfaceIdAllocation();
     GetNativeViewScreenInfo(&params.screen_info, d_hwnd.get());
 
-    d_renderViewObserver->DispatchSynchronizeVisualProperties(params);
+    // Renderer-driven popups do not have a 'd_renderViewObserver', so check
+    // its value before dereferencing it:
+    if (d_renderViewObserver) {
+        d_renderViewObserver->DispatchSynchronizeVisualProperties(params);
+    }
+    else {
+        dispatchToRenderWidget(
+            WidgetMsg_SynchronizeVisualProperties(d_renderWidgetRoutingId,
+                params));
+    }
 }
 
 void RenderWebView::updateFocus()