ca2bb08127ddf1d5e52866689a49c772917ce4bb
Maksim Ivanov <emaxx@chromium.org>
2019-04-25 18:44:52 +0000

    Cert utils for challenge-response login keys
    Add a helper function that builds the ChallengeResponseKey object
    based on the public key information in the specified certificate.
    
    This will be used for challenge-response logins (a.k.a. smart card
    login) as higher-level APIs will identify the user with the
    certificate, while the lower-level APIs (such as the cryptohomed D-Bus
    API) only deal with public keys and signature algorithms.
    
    BUG=chromium:826417
    TEST=chromeos_unittests --gtest_filter=ChallengeResponseCertUtilsTest*
    
    Change-Id: Ie1ac1e9b13e44748f98aad4a0ac69df0e638e70f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1107058
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Jacob Dufault <jdufault@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654122}
    
    Former-commit-id: b92892fb34ddffaf6fc95ac0398164473195bad0

---
+++ chromeos/login/auth/challenge_response/cert_utils.cc
@@ -0,0 +1,54 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/login/auth/challenge_response/cert_utils.h"
+
+#include <string>
+
+#include "base/logging.h"
+#include "base/strings/string_piece.h"
+#include "net/cert/asn1_util.h"
+#include "net/cert/x509_certificate.h"
+#include "net/cert/x509_util.h"
+
+namespace chromeos {
+
+namespace {
+
+bool GetSubjectPublicKeyInfo(const net::X509Certificate& certificate,
+                             std::string* spki_der) {
+  base::StringPiece spki_der_piece;
+  if (!net::asn1::ExtractSPKIFromDERCert(
+          net::x509_util::CryptoBufferAsStringPiece(certificate.cert_buffer()),
+          &spki_der_piece)) {
+    return false;
+  }
+  *spki_der = spki_der_piece.as_string();
+  return !spki_der->empty();
+}
+
+}  // namespace
+
+bool ExtractChallengeResponseKeyFromCert(
+    const net::X509Certificate& certificate,
+    const std::vector<ChallengeResponseKey::SignatureAlgorithm>&
+        signature_algorithms,
+    ChallengeResponseKey* challenge_response_key) {
+  if (signature_algorithms.empty()) {
+    LOG(ERROR)
+        << "No signature algorithms provided for the challenge-response key";
+    return false;
+  }
+  std::string spki_der;
+  if (!GetSubjectPublicKeyInfo(certificate, &spki_der)) {
+    LOG(ERROR) << "Failed to extract Subject Public Key Information from the "
+                  "given certificate";
+    return false;
+  }
+  challenge_response_key->set_public_key_spki_der(spki_der);
+  challenge_response_key->set_signature_algorithms(signature_algorithms);
+  return true;
+}
+
+}  // namespace chromeos
