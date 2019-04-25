85ab68bfbb024e5d375dc9fc68a4d1d988b1c352
Adrienne Walker <enne@chromium.org>
2019-04-25 02:47:25 +0000

    Fix PIP window being blank after minimize/show
    DesktopWindowTreeHostX11::SetVisible only made the call into
    OnNativeWidgetVisibilityChanged when transitioning from shown
    to minimized and not vice versa.  This is because this change
    https://chromium-review.googlesource.com/c/chromium/src/+/1437263
    considered IsVisible to be true when minimized, which made
    IsVisible always true in this case.  This caused layers to be hidden
    but never shown again.
    
    Bug: 949199
    Change-Id: Ieb31ac5e6856645918aa337f34d47efb947c3910
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1580103
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653890}
    
    Former-commit-id: e0c1b7c048166a05cb1a07ef438fcca60261453f

--- ui/views/widget/desktop_aura/desktop_window_tree_host_x11.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_x11.cc
@@ -855,10 +855,13 @@ bool DesktopWindowTreeHostX11::IsAlwaysOnTop() const {
 }
 
 void DesktopWindowTreeHostX11::SetVisible(bool visible) {
+  if (is_compositor_set_visible_ == visible)
+    return;
+
+  is_compositor_set_visible_ = visible;
   if (compositor())
     compositor()->SetVisible(visible);
-  if (IsVisible() != visible)
-    native_widget_delegate_->OnNativeWidgetVisibilityChanged(visible);
+  native_widget_delegate_->OnNativeWidgetVisibilityChanged(visible);
 }
 
 void DesktopWindowTreeHostX11::SetVisibleOnAllWorkspaces(bool always_visible) {
