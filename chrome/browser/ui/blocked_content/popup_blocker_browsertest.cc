9c2835c2a4ae112c0cca4d1dedb3b093b4dc2db7
Elly Fong-Jones <ellyjones@google.com>
2019-04-25 23:11:46 +0000

    Disable no-popups-during-tab-closure.
    This is a deliberately minimal change intended for easy merging to earlier
    branches.
    
    TBR=mkwst@chromium.org,avi@chromium.org
    
    Bug: 936080
    Change-Id: Idbc20481303db551cba2994ed15670aecc163850
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1584618
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654249}
    
    Former-commit-id: 4dbf9efbfff61b0e27ce5277e826149b3c0bda0e

--- chrome/browser/ui/blocked_content/popup_blocker_browsertest.cc
+++ chrome/browser/ui/blocked_content/popup_blocker_browsertest.cc
@@ -434,6 +434,9 @@ IN_PROC_BROWSER_TEST_F(PopupBlockerBrowserTest,
   ASSERT_EQ(1, GetBlockedContentsCount());
 }
 
+// This feature is being reverted on trunk but an easily-merged CL is needed for
+// merging back to stable. https://crbug.com/936080
+#if 0
 IN_PROC_BROWSER_TEST_F(PopupBlockerBrowserTest, NoPopupsLaunchWhenTabIsClosed) {
   base::CommandLine::ForCurrentProcess()->AppendSwitch(
       switches::kDisablePopupBlocking);
@@ -447,6 +450,7 @@ IN_PROC_BROWSER_TEST_F(PopupBlockerBrowserTest, NoPopupsLaunchWhenTabIsClosed) {
   // Expect no popup.
   ASSERT_EQ(1u, chrome::GetBrowserCount(browser()->profile()));
 }
+#endif
 
 // This only exists for the AllowPopupsWhenTabIsClosedWithSpecialPolicy test.
 // Remove this in Chrome 82. https://crbug.com/937569
