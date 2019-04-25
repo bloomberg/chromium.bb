ce5b48a12cd53fb38cd744dbbd3e3b8dc9cbfe5d
Olivier Robin <olivierrobin@chromium.org>
2019-04-25 10:17:56 +0000

    Disable WKHTTPSystemCookieStore tests
    Tests are failing on device and locally on simulator.
    
    Bug: 941643
    Tbr: mrefaat
    Change-Id: I5226fc9dd22588e9dc7bd94ee7f3aa6833f460ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1583740
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#653967}
    
    Former-commit-id: b7f14a619d746a1d53b6b3305f755dccf3b97b7c

--- ios/net/cookies/system_cookie_store_unittest_template.h
+++ ios/net/cookies/system_cookie_store_unittest_template.h
@@ -154,7 +154,8 @@ class SystemCookieStoreTest : public PlatformTest {
 
 TYPED_TEST_SUITE_P(SystemCookieStoreTest);
 
-TYPED_TEST_P(SystemCookieStoreTest, SetCookieAsync) {
+// TODO(crbug.com/956477): Reenable the test.
+TYPED_TEST_P(SystemCookieStoreTest, DISABLED_SetCookieAsync) {
   NSHTTPCookie* system_cookie =
       CreateCookie(@"a", @"b", this->test_cookie_url1_);
   SystemCookieCallbackRunVerifier callback_verifier;
@@ -168,7 +169,8 @@ TYPED_TEST_P(SystemCookieStoreTest, SetCookieAsync) {
 }
 
 // Tests cases of GetAllCookiesAsync and GetCookiesForURLAsync.
-TYPED_TEST_P(SystemCookieStoreTest, GetCookiesAsync) {
+// TODO(crbug.com/956477): Reenable the test.
+TYPED_TEST_P(SystemCookieStoreTest, DISABLED_GetCookiesAsync) {
   SystemCookieStore* cookie_store = this->GetCookieStore();
   NSMutableDictionary* input_cookies = [[NSMutableDictionary alloc] init];
   NSHTTPCookie* system_cookie =
@@ -215,7 +217,8 @@ TYPED_TEST_P(SystemCookieStoreTest, GetCookiesAsync) {
 
 // Tests deleting cookies for different URLs and for different
 // cookie key/value pairs.
-TYPED_TEST_P(SystemCookieStoreTest, DeleteCookiesAsync) {
+// TODO(crbug.com/956477): Reenable the test.
+TYPED_TEST_P(SystemCookieStoreTest, DISABLED_DeleteCookiesAsync) {
   SystemCookieStore* cookie_store = this->GetCookieStore();
   NSHTTPCookie* system_cookie1 =
       CreateCookie(@"a", @"b", this->test_cookie_url1_);
@@ -248,7 +251,8 @@ TYPED_TEST_P(SystemCookieStoreTest, DeleteCookiesAsync) {
   EXPECT_EQ(0, this->CookiesCount());
 }
 
-TYPED_TEST_P(SystemCookieStoreTest, ClearCookiesAsync) {
+// TODO(crbug.com/956477): Reenable the test.
+TYPED_TEST_P(SystemCookieStoreTest, DISABLED_ClearCookiesAsync) {
   SystemCookieStore* cookie_store = this->GetCookieStore();
   SetCookieInStoreWithNoCallback(
       CreateCookie(@"a", @"b", this->test_cookie_url1_), cookie_store);
@@ -264,7 +268,8 @@ TYPED_TEST_P(SystemCookieStoreTest, ClearCookiesAsync) {
   EXPECT_EQ(0, this->CookiesCount());
 }
 
-TYPED_TEST_P(SystemCookieStoreTest, GetCookieAcceptPolicy) {
+// TODO(crbug.com/956477): Reenable the test.
+TYPED_TEST_P(SystemCookieStoreTest, DISABLED_GetCookieAcceptPolicy) {
   SystemCookieStore* cookie_store = this->GetCookieStore();
   EXPECT_EQ([NSHTTPCookieStorage sharedHTTPCookieStorage].cookieAcceptPolicy,
             cookie_store->GetCookieAcceptPolicy());
@@ -279,11 +284,11 @@ TYPED_TEST_P(SystemCookieStoreTest, GetCookieAcceptPolicy) {
 }
 
 REGISTER_TYPED_TEST_SUITE_P(SystemCookieStoreTest,
-                            SetCookieAsync,
-                            GetCookiesAsync,
-                            DeleteCookiesAsync,
-                            ClearCookiesAsync,
-                            GetCookieAcceptPolicy);
+                            DISABLED_SetCookieAsync,
+                            DISABLED_GetCookiesAsync,
+                            DISABLED_DeleteCookiesAsync,
+                            DISABLED_ClearCookiesAsync,
+                            DISABLED_GetCookieAcceptPolicy);
 
 }  // namespace net
 
