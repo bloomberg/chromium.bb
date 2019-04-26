2870622436c70f5cb120de633f1e81672ec642be
Xing Liu <xingliu@chromium.org>
2019-04-26 17:57:23 +0000

    added Rename Dialogs' Action UMA
    TBR=hesen@google.com
    
    (cherry picked from commit 76afcaeb5ad5ff7a2f91dd16b16469e18edc06ca [formerly b364990c6df69bca4a530e0d4dbb3601d8836429])
    
    Bug: 924751
    Change-Id: Ia4c841be540e8ff276c87b4512adbd9ef1699436
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1570236
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Hesen Zhang <hesen@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#652563}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1585975
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3770@{#111}
    Cr-Branched-From: 04e619e4964cac3f22e7ef787268f756c2b6c1c7 [formerly a9eee1c7c727ef42a15d86e9fa7b77ff0e63840a]-refs/heads/master@{#652427}
    
    Former-commit-id: a422d058d594d3d1a09f8e17d3098b29e39ab6cc

--- chrome/android/java/src/org/chromium/chrome/browser/download/home/rename/RenameDialogManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/home/rename/RenameDialogManager.java
@@ -3,11 +3,14 @@
 // found in the LICENSE file.
 package org.chromium.chrome.browser.download.home.rename;
 
+import static org.chromium.chrome.browser.download.home.metrics.UmaUtils.recordRenameAction;
+
 import android.content.Context;
 import android.support.annotation.IntDef;
 import android.text.TextUtils;
 
 import org.chromium.base.Callback;
+import org.chromium.chrome.browser.download.home.metrics.UmaUtils;
 import org.chromium.components.offline_items_collection.RenameResult;
 import org.chromium.ui.modaldialog.DialogDismissalCause;
 import org.chromium.ui.modaldialog.ModalDialogManager;
@@ -162,7 +165,9 @@ public class RenameDialogManager {
         });
     }
 
-    private void onRenameDialogDismiss(int dismissalCause) {}
+    private void onRenameDialogDismiss(int dismissalCause) {
+        recordRenameAction(getRenameAction(false, dismissalCause));
+    }
 
     private void onRenameDialogClick(boolean isPositiveButton) {
         if (isPositiveButton) {
@@ -198,6 +203,24 @@ public class RenameDialogManager {
     private void onRenameExtensionDialogDismiss(int dismissalCause) {
         if (dismissalCause == DialogDismissalCause.NAVIGATE_BACK_OR_TOUCH_OUTSIDE) {
             processDialogState(RenameDialogState.RENAME_EXTENSION_DIALOG_CANCEL, dismissalCause);
+        }
+        recordRenameAction(getRenameAction(true, dismissalCause));
+    }
+
+    private @UmaUtils.RenameDialogAction int getRenameAction(
+            boolean isExtensionDialog, int dismissalCause) {
+        switch (dismissalCause) {
+            case DialogDismissalCause.POSITIVE_BUTTON_CLICKED:
+                return isExtensionDialog
+                        ? UmaUtils.RenameDialogAction.RENAME_EXTENSION_DIALOG_CONFIRM
+                        : UmaUtils.RenameDialogAction.RENAME_DIALOG_CONFIRM;
+            case DialogDismissalCause.NEGATIVE_BUTTON_CLICKED:
+                return isExtensionDialog
+                        ? UmaUtils.RenameDialogAction.RENAME_EXTENSION_DIALOG_CANCEL
+                        : UmaUtils.RenameDialogAction.RENAME_DIALOG_CANCEL;
+            default:
+                return isExtensionDialog ? UmaUtils.RenameDialogAction.RENAME_EXTENSION_DIALOG_OTHER
+                                         : UmaUtils.RenameDialogAction.RENAME_DIALOG_OTHER;
         }
     }
 }
