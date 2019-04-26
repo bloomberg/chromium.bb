05489526771d6be6e2b1ef94095223efb5a11f86
Adrienne Walker <enne@chromium.org>
2019-04-26 20:26:38 +0000

    Fix tooltips being blank
    https://chromium-review.googlesource.com/c/chromium/src/+/1584617 caused
    this regression.
    
    The issue here is that DesktopWindowTreeHostX11::HideImpl doesn't call
    SetVisible directly and so the is_compositor_set_visible_ flag stays
    true and SetVisible(true) during Show does nothing.
    
    Bug: 956997
    Change-Id: I65174cb126debd5e10d68dbd237ddfa0e7ff8c14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1586235
    Auto-Submit: enne <enne@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654581}
    
    Former-commit-id: e81c55fd692ea725c3e7937dcf75a858af71fbd8

--- ui/views/widget/desktop_aura/desktop_window_tree_host_x11.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_x11.cc
@@ -1186,7 +1186,7 @@ void DesktopWindowTreeHostX11::HideImpl() {
   if (window_mapped_in_client_) {
     XWithdrawWindow(xdisplay_, xwindow_, 0);
     window_mapped_in_client_ = false;
-    native_widget_delegate_->OnNativeWidgetVisibilityChanged(false);
+    SetVisible(false);
   }
 }
 
