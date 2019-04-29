879d33751da94e89c13a40bc077e5518cc2a6fff
Marijn Kruisselbrink <mek@chromium.org>
2019-04-29 19:17:27 +0000

    [Blobs] Don't crash if null task runner is passed to FileReaderLoader.
    Unclear what or where is passing null, but if it happens we don't want
    to crash later when handle_watcher_ is armed.
    
    Bug: 859791, 957651
    Change-Id: Ibf75af611b933bcb36898654d2d15eff845a3b09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588683
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654964}
    
    Former-commit-id: 8e4846f4aa93d83fa1a1ffccc3f4d8a79438f41a

--- third_party/blink/renderer/core/fileapi/file_reader_loader.cc
+++ third_party/blink/renderer/core/fileapi/file_reader_loader.cc
@@ -68,9 +68,13 @@ FileReaderLoader::FileReaderLoader(
     scoped_refptr<base::SingleThreadTaskRunner> task_runner)
     : read_type_(read_type),
       client_(client),
-      handle_watcher_(FROM_HERE,
-                      mojo::SimpleWatcher::ArmingPolicy::AUTOMATIC,
-                      task_runner),
+      // TODO(https://crbug.com/957651): task_runner should never be null, but
+      // if it is make sure SimpleWatcher doesn't crash and just use a default
+      // task runner instead for now.
+      handle_watcher_(
+          FROM_HERE,
+          mojo::SimpleWatcher::ArmingPolicy::AUTOMATIC,
+          task_runner ? task_runner : base::SequencedTaskRunnerHandle::Get()),
       binding_(this),
       task_runner_(std::move(task_runner)),
       weak_factory_(this) {}
