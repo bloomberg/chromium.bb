6dbccdb0eeb18b69f838b7321752d56feced2b43
David Jean <djean@chromium.org>
2019-04-29 14:04:51 +0000

    [ios] Remove uses of designated initializer lists
    Remove banned initializer list usage in new password form manager.
    
    Bug: 956581
    Change-Id: I11c5bf077d003016baf2007d702aea29aa65a0e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1587566
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654834}
    
    Former-commit-id: b041a86e1deb7ba7dfccb3ec87246e061bd15d5d

--- components/password_manager/core/browser/new_password_form_manager.cc
+++ components/password_manager/core/browser/new_password_form_manager.cc
@@ -681,17 +681,16 @@ void NewPasswordFormManager::Fill() {
   if (observed_password_form->is_new_password_reliable && !IsBlacklisted()) {
 #if defined(OS_IOS)
     driver_->FormEligibleForGenerationFound(
-        {.form_name = observed_password_form->form_data.name,
-         .new_password_element = observed_password_form->new_password_element,
-         .confirmation_password_element =
-             observed_password_form->confirmation_password_element});
+        {/*form_name*/ observed_password_form->form_data.name,
+         /*new_password_element*/ observed_password_form->new_password_element,
+         /*confirmation_password_element*/
+         observed_password_form->confirmation_password_element});
 #else
     driver_->FormEligibleForGenerationFound(
-        {.new_password_renderer_id =
-             observed_password_form->new_password_element_renderer_id,
-         .confirmation_password_renderer_id =
-             observed_password_form
-                 ->confirmation_password_element_renderer_id});
+        {/*new_password_renderer_id*/
+         observed_password_form->new_password_element_renderer_id,
+         /*confirmation_password_renderer_id*/
+         observed_password_form->confirmation_password_element_renderer_id});
 #endif
   }
 
