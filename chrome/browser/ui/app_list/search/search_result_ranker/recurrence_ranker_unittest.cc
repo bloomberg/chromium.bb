09fb8bdbf348d275fb8b88f3cfd0b015af0a81c7
Carlos Caballero <carlscab@google.com>
2019-04-29 09:25:16 +0000

    Rename ScopedTaskEnvironment::ExecutionMode
    ScopedTaskEnvironment::ExecutionMode is becoming
    ScopedTaskEnvironment::ThreadPoolExecutionMode
    
    This CL was uploaded by git cl split.
    
    R=jiameng@chromium.org
    
    Bug: 708584
    Change-Id: If29ea495733f0a4131fbad08050f61f11d598541
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1583751
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#654814}
    
    Former-commit-id: 3b75e81e653dc326e270ed39b7203edb28e4045a

--- chrome/browser/ui/app_list/search/search_result_ranker/recurrence_ranker_unittest.cc
+++ chrome/browser/ui/app_list/search/search_result_ranker/recurrence_ranker_unittest.cc
@@ -69,7 +69,7 @@ class RecurrenceRankerTest : public testing::Test {
   RecurrenceRankerConfigProto config_;
   base::test::ScopedTaskEnvironment scoped_task_environment_{
       base::test::ScopedTaskEnvironment::MainThreadType::DEFAULT,
-      base::test::ScopedTaskEnvironment::ExecutionMode::QUEUED};
+      base::test::ScopedTaskEnvironment::ThreadPoolExecutionMode::QUEUED};
   base::ScopedTempDir temp_dir_;
   base::test::ScopedFeatureList scoped_feature_list_;
 
