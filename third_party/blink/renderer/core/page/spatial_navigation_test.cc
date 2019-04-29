5e33dff9fa851c2197c9b8c81a7dec4e44d7b0de
David Bokan <bokan@chromium.org>
2019-04-29 16:18:05 +0000

    [SpatNav] Account for clipping in z-ordering checks
    The spatial navigation algorithm attempts to prioritize targets fully
    contained within another target. However, the current implementation has
    a bug because it uses unclipped rects to determine containment.
    
    This CL fixes the issue by using MapToVisualRectInAncestorSpace which
    does account for clipping.
    
    Small cleanup: Remove the |ignore_border| option since it's always true.
    
    Bug: 957240
    Change-Id: Id032bb9d354ec06d075b2de6bd53a8799f7a8af1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1586660
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#654878}
    
    Former-commit-id: b72c9e10a746d28aef4e1c9b5cf669e3b4f8f637

--- third_party/blink/renderer/core/page/spatial_navigation_test.cc
+++ third_party/blink/renderer/core/page/spatial_navigation_test.cc
@@ -198,7 +198,7 @@ TEST_F(SpatialNavigationTest, StartAtVisibleFocusedElement) {
 
   EXPECT_EQ(SearchOrigin(RootViewport(&GetFrame()), b,
                          SpatialNavigationDirection::kDown),
-            NodeRectInRootFrame(b, true));
+            NodeRectInRootFrame(b));
 }
 
 TEST_F(SpatialNavigationTest, StartAtVisibleFocusedScroller) {
@@ -220,7 +220,7 @@ TEST_F(SpatialNavigationTest, StartAtVisibleFocusedScroller) {
   Element* scroller = GetDocument().getElementById("scroller");
   EXPECT_EQ(SearchOrigin(RootViewport(&GetFrame()), scroller,
                          SpatialNavigationDirection::kDown),
-            NodeRectInRootFrame(scroller, true));
+            NodeRectInRootFrame(scroller));
 }
 
 TEST_F(SpatialNavigationTest, StartAtVisibleFocusedIframe) {
@@ -241,7 +241,7 @@ TEST_F(SpatialNavigationTest, StartAtVisibleFocusedIframe) {
   Element* iframe = GetDocument().getElementById("iframe");
   EXPECT_EQ(SearchOrigin(RootViewport(&GetFrame()), iframe,
                          SpatialNavigationDirection::kDown),
-            NodeRectInRootFrame(iframe, true));
+            NodeRectInRootFrame(iframe));
 }
 
 TEST_F(SpatialNavigationTest, StartAtTopWhenGoingDownwardsWithoutFocus) {
@@ -317,7 +317,7 @@ TEST_F(SpatialNavigationTest, StartAtContainersEdge) {
 
   Element* b = GetDocument().getElementById("b");
   const Element* container = GetDocument().getElementById("container");
-  const LayoutRect container_box = NodeRectInRootFrame(container, true);
+  const LayoutRect container_box = NodeRectInRootFrame(container);
 
   // TODO(crbug.com/889840):
   // VisibleBoundsInVisualViewport does not (yet) take div-clipping into
@@ -433,7 +433,7 @@ TEST_F(SpatialNavigationTest, PartiallyVisible) {
 
   EXPECT_FALSE(IsOffscreen(b));  // <button> is not completely offscreen.
 
-  LayoutRect button_in_root_frame = NodeRectInRootFrame(b, true);
+  LayoutRect button_in_root_frame = NodeRectInRootFrame(b);
 
   EXPECT_EQ(SearchOrigin(RootViewport(&GetFrame()), b,
                          SpatialNavigationDirection::kUp),
@@ -442,7 +442,7 @@ TEST_F(SpatialNavigationTest, PartiallyVisible) {
   // Do some scrolling.
   ScrollableArea* root_scroller = GetDocument().View()->GetScrollableArea();
   root_scroller->SetScrollOffset(ScrollOffset(0, 600), kProgrammaticScroll);
-  LayoutRect button_after_scroll = NodeRectInRootFrame(b, true);
+  LayoutRect button_after_scroll = NodeRectInRootFrame(b);
   ASSERT_NE(button_in_root_frame,
             button_after_scroll);  // As we scrolled, the
                                    // <button>'s position in
@@ -559,7 +559,7 @@ TEST_F(SpatialNavigationTest, PartiallyVisibleIFrame) {
   EXPECT_TRUE(IsOffscreen(child_element));         // Completely offscreen.
   EXPECT_FALSE(IsOffscreen(enclosing_container));  // Partially visible.
 
-  LayoutRect iframe = NodeRectInRootFrame(enclosing_container, true);
+  LayoutRect iframe = NodeRectInRootFrame(enclosing_container);
 
   // When searching downwards we start at activeElement's
   // container's (here: the iframe's) topmost visible edge.
