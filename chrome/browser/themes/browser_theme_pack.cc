fafd723303c1f08b03ba8e3e73c15121d8da88dc
Gayane Petrosyan <gayane@chromium.org>
2019-04-29 16:47:52 +0000

    Increment theme browser pack version.
    This change is meant for forcing theme property recalculation in M75.
    
    (cherry picked from commit 60696c9c8866ed35bc182ca89e25868bdf273d27 [formerly 06f440b6c3158af1839d564358e45f47f174d42e])
    
    Bug: 942135
    Change-Id: Ie6c912234906702381ea5fc5fc5919eb1e14ad99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1585321
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#654673}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588369
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3770@{#158}
    Cr-Branched-From: 04e619e4964cac3f22e7ef787268f756c2b6c1c7 [formerly a9eee1c7c727ef42a15d86e9fa7b77ff0e63840a]-refs/heads/master@{#652427}
    
    Former-commit-id: 65acba3ce8275f8c17ebf0a52071be812ef99a8e

--- chrome/browser/themes/browser_theme_pack.cc
+++ chrome/browser/themes/browser_theme_pack.cc
@@ -60,7 +60,7 @@ constexpr int kTallestFrameHeight = kTallestTabHeight + 19;
 // change default theme assets, if you need themes to recreate their generated
 // images (which are cached), or if you changed how missing values are
 // generated.
-const int kThemePackVersion = 64;
+const int kThemePackVersion = 65;
 
 // IDs that are in the DataPack won't clash with the positive integer
 // uint16_t. kHeaderID should always have the maximum value because we want the
