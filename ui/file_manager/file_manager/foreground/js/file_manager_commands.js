f78ad29f6f5c07432182f5e0bd325f84c7455101
Dominik Röttsches <drott@chromium.org>
2019-04-30 07:55:35 +0000

    Revert "Reland "PluginVm in FilesApp: set icon and read-only status""
    This reverts commit d0c60c4ce007dcfbe3f14f952d971d240d7c0083 [formerly 7598941c72520da450640f8e2dfe62e2c23041f4].
    
    Reason for revert: Hits similar issues in as in previous revert,  FileManagerUITest.PluginVm fails on linux-chromeos-rel, compare https://ci.chromium.org/p/chromium/builders/ci/linux-chromeos-rel/23631
    
    Bug: 957512
    
    Original change's description:
    > Reland "PluginVm in FilesApp: set icon and read-only status"
    > 
    > This is a reland of 349bf336c5a2d80e2c6587d8276eab851f543313 [formerly bdefcc2d0ae7805a862c8111363a47044fdb2198]
    > 
    > I cannot reproduce failures which caused the revert,
    > and this has succeeded 10x on CQ.
    > 
    > The plan is to reland this as-is.  If/when flakes start again,
    > I have a potential fix in
    > https://chromium-review.googlesource.com/c/chromium/src/+/1588060
    > which can be applied, or this can be reverted again.
    > 
    > Original change's description:
    > > PluginVm in FilesApp: set icon and read-only status
    > >
    > > Bug: 955920
    > > Change-Id: Iff3e97b187368683d89cbf7200d200046c73e095
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1580988
    > > Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    > > Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#654793}
    > 
    > Bug: 955920
    > Change-Id: I1845a37a488b45987272caca5343ceb4b4d0bce4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1589075
    > Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    > Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    > Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    > Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#655122}
    
    TBR=joelhockey@chromium.org,lucmult@chromium.org
    
    Change-Id: I471204bca7df7f410ad7fcf67824594c4f3299ed
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 955920
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1588380
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655156}
    
    Former-commit-id: 66ae1f6d75a75c19042db0f27c352d77a2cb0527

--- ui/file_manager/file_manager/foreground/js/file_manager_commands.js
+++ ui/file_manager/file_manager/foreground/js/file_manager_commands.js
@@ -272,13 +272,12 @@ CommandUtil.shouldShowMenuItemsForEntry = (volumeManager, entry) => {
 };
 
 /**
- * If entry is MyFiles/Downloads or MyFiles/PluginVm, we don't allow
- * cut/delete/rename.
+ * If entry is MyFiles/Downloads, we don't allow cut/delete/rename.
  * @param {!VolumeManager} volumeManager
  * @param {(Entry|FakeEntry)} entry Entry or a fake entry.
  * @return {boolean}
  */
-CommandUtil.isReadOnly = (volumeManager, entry) => {
+CommandUtil.isDownloads = (volumeManager, entry) => {
   if (!entry) {
     return false;
   }
@@ -296,13 +295,10 @@ CommandUtil.isReadOnly = (volumeManager, entry) => {
     return false;
   }
 
-  if (volumeInfo.volumeType === VolumeManagerCommon.RootType.DOWNLOADS) {
-    if (util.isMyFilesVolumeEnabled() && entry.fullPath === '/Downloads') {
-      return true;
-    }
-    if (util.isPluginVmEnabled() && entry.fullPath === '/PluginVm') {
-      return true;
-    }
+  if (util.isMyFilesVolumeEnabled() &&
+      volumeInfo.volumeType === VolumeManagerCommon.RootType.DOWNLOADS &&
+      entry.fullPath === '/Downloads') {
+    return true;
   }
   return false;
 };
@@ -1034,7 +1030,7 @@ CommandHandler.COMMANDS_['delete'] = (() => {
 
     /**
      * Returns True if any entry belongs to a read-only volume or is
-     * forced to be read-only like MyFiles>Downloads.
+     * MyFiles>Downloads.
      * @param {!Array<!Entry>} entries
      * @param {!CommandHandlerDeps} fileManager
      * @return {boolean} True if entries contain read only entry.
@@ -1043,7 +1039,7 @@ CommandHandler.COMMANDS_['delete'] = (() => {
       return entries.some(entry => {
         const locationInfo = fileManager.volumeManager.getLocationInfo(entry);
         return (locationInfo && locationInfo.isReadOnly) ||
-            CommandUtil.isReadOnly(fileManager.volumeManager, entry);
+            CommandUtil.isDownloads(fileManager.volumeManager, entry);
       });
     }
   };
@@ -1201,8 +1197,8 @@ CommandHandler.cutCopyCommand_ = /** @type {Command} */ ({
         return false;
       }
 
-      // For MyFiles/Downloads and MyFiles/PluginVm we only allow copy.
-      if (isMove && CommandUtil.isReadOnly(volumeManager, entry)) {
+      // For MyFiles/Downloads we only allow copy.
+      if (isMove && CommandUtil.isDownloads(volumeManager, entry)) {
         return false;
       }
 
@@ -1240,7 +1236,7 @@ CommandHandler.cutCopyCommand_ = /** @type {Command} */ ({
       // For MyFiles/Downloads we only allow copy.
       if (isMove &&
           fileManager.getSelection().entries.some(
-              CommandUtil.isReadOnly.bind(null, volumeManager))) {
+              CommandUtil.isDownloads.bind(null, volumeManager))) {
         return false;
       }
 
@@ -1272,7 +1268,7 @@ CommandHandler.COMMANDS_['rename'] = /** @type {Command} */ ({
    */
   execute: function(event, fileManager) {
     const entry = CommandUtil.getCommandEntry(fileManager, event.target);
-    if (CommandUtil.isReadOnly(fileManager.volumeManager, entry)) {
+    if (CommandUtil.isDownloads(fileManager.volumeManager, entry)) {
       return;
     }
     if (event.target instanceof DirectoryTree ||
@@ -1347,7 +1343,7 @@ CommandHandler.COMMANDS_['rename'] = /** @type {Command} */ ({
         !CommandUtil.shouldShowMenuItemsForEntry(
             fileManager.volumeManager, entries[0]) ||
         entries.some(
-            CommandUtil.isReadOnly.bind(null, fileManager.volumeManager))) {
+            CommandUtil.isDownloads.bind(null, fileManager.volumeManager))) {
       event.canExecute = false;
       event.command.setHidden(true);
       return;
