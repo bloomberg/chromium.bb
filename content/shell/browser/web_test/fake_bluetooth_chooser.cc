b1eca9a5c00ac2b6dd1328dec595b840d85224be
Ovidio Henriquez <odejesush@chromium.org>
2019-04-30 00:07:32 +0000

    Reland "bluetooth: FakeBluetoothChooser impl."
    This is a reland of 47d08ba1733e415bf444df39953fde9a80c899cf [formerly 6a337f19713e049cd42969b0c909c59aff4146cd]
    
    This previous change caused tests to fail because of a Mojo validation
    error, which is now taken care of by allowing the Bluetooth tests to
    use the path to the generated Mojom files.
    
    Design doc:
    https://docs.google.com/document/d/1XFl_4ZAgO8ddM6U53A9AfUuZeWgJnlYD5wtbXqEpzeg
    
    Original change's description:
    > bluetooth: FakeBluetoothChooser impl.
    >
    > Implements the interfaces that FakeBluetoothChooser overrides. The
    > mojom::FakeBluetoothChooser overrides are implemented by running
    > |event_handler_| with the appropriate arguments. The BluetoothChooser
    > overrides do nothing at the moment, since the base class is only meant
    > to update the chooser user interface.
    >
    > This change also begins to partially convert a Bluetooth chooser test to test
    > the interface and deletes the temporary tests that were created to test this
    > API.
    >
    > This change reverts commit 93745bf6795ba7469a0c98efbc67cb663cd7ff0a [formerly 42624492cd5e80855f1dd3a5a8146b258def2c2a], which
    > reverted the original change. The original change caused the
    > new-scan-device-added.html test to fail because of a Mojo validation error
    > resulting from not updating the
    > third_party/.../external/wpt/resources/chromium/fake_bluetooth_chooser.mojom.js
    > file needed by the test.
    >
    > Bug: 719827, 719826, 824704, 826787
    > Change-Id: I5e5cce7e809b4d71216c2d074a417ea822d5e527
    > Reviewed-on: https://chromium-review.googlesource.com/985195
    > Commit-Queue: Ovidio Henriquez <odejesush@chromium.org>
    > Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#546729}
    
    Bug: 719827, 719826, 824704, 826787
    Change-Id: If63a681becbb2ac0f646a11ea20b1db18d42395d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/986593
    Commit-Queue: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655081}
    
    Former-commit-id: cb85732cb3be7cf442e5c3afe6a3625f7edee10b

--- content/shell/browser/web_test/fake_bluetooth_chooser.cc
+++ content/shell/browser/web_test/fake_bluetooth_chooser.cc
@@ -16,6 +16,8 @@ namespace content {
 FakeBluetoothChooser::~FakeBluetoothChooser() {
   SetTestBluetoothScanDuration(
       BluetoothTestScanDurationSetting::kImmediateTimeout);
+  // TODO(https://crbug.com/719826): Send a
+  // mojom::ChooserEventType::CHOOSER_CLOSED event to the client.
 }
 
 // static
@@ -41,34 +43,35 @@ void FakeBluetoothChooser::WaitForEvents(uint32_t num_of_events,
 }
 
 void FakeBluetoothChooser::SelectPeripheral(
-    const std::string& peripheral_address,
-    SelectPeripheralCallback callback) {
-  // TODO(https://crbug.com/719826): Record the event and send a
-  // BluetoothChooser::SELECTED event to |event_handler_|.
+    const std::string& peripheral_address) {
+  event_handler_.Run(BluetoothChooser::Event::SELECTED, peripheral_address);
+}
+
+void FakeBluetoothChooser::Cancel() {
+  // TODO(https://crbug.com/719826): Send a BluetoothChooser::CANCELLED event to
+  // |event_handler_| and a mojom::ChooserEventType::CHOOSER_CLOSED to the
+  // client.
   NOTREACHED();
 }
 
-void FakeBluetoothChooser::Cancel(CancelCallback callback) {
-  // TODO(https://crbug.com/719826): Record the event and send a
-  // BluetoothChooser::CANCELLED event to |event_handler_|.
-  NOTREACHED();
-}
-
-void FakeBluetoothChooser::Rescan(RescanCallback callback) {
-  // TODO(https://crbug.com/719826): Record the event and send a
-  // BluetoothChooser::RESCAN event to |event_handler_|.
+void FakeBluetoothChooser::Rescan() {
+  // TODO(https://crbug.com/719826): Send a BluetoothChooser::RESCAN event to
+  // |event_handler_| and the appropriate mojom::ChooserEventType to describe
+  // the discovery session.
   NOTREACHED();
 }
 
 // BluetoothChooser overrides
 
 void FakeBluetoothChooser::SetAdapterPresence(AdapterPresence presence) {
-  // TODO(https://crbug.com/719826): Record the event.
+  // TODO(https://crbug.com/719826): Send the appropriate
+  // mojom::ChooserEventType to the client.
   NOTREACHED();
 }
 
 void FakeBluetoothChooser::ShowDiscoveryState(DiscoveryState state) {
-  // TODO(https://crbug.com/719826): Record the event.
+  // TODO(https://crbug.com/719826): Send the appropriate
+  // mojom::ChooserEventType to the client.
   NOTREACHED();
 }
 
@@ -78,7 +81,8 @@ void FakeBluetoothChooser::AddOrUpdateDevice(const std::string& device_id,
                                              bool is_gatt_connected,
                                              bool is_paired,
                                              int signal_strength_level) {
-  // TODO(https://crbug.com/719826): Record the event.
+  // TODO(https://crbug.com/719826): Send a
+  // mojom::ChooserEventType::ADD_OR_UPDATE_DEVICE to the client.
   NOTREACHED();
 }
 
