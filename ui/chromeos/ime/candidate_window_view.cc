75f1be6a874f30482f35fb2883000e4ec139293a
Caroline Rising <corising@chromium.org>
2019-04-30 07:42:42 +0000

    Update theme when resetting bubble border for candidate window view.
    This fixes bug caused by https://chromium-review.googlesource.com/c/chromium/src/+/1561837
    
    Bug: 956847
    Change-Id: I8e53794522f477288d227bce53baf46304785f70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1586455
    Reviewed-by: Shu Chen <shuchen@chromium.org>
    Commit-Queue: Shu Chen <shuchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655151}
    
    Former-commit-id: 2d2e3283ed133bf7fd0df86ddfa345f796dbdc56

--- ui/chromeos/ime/candidate_window_view.cc
+++ ui/chromeos/ime/candidate_window_view.cc
@@ -317,6 +317,7 @@ void CandidateWindowView::UpdateCandidates(
     else
       border->set_offset(0);
     GetBubbleFrameView()->SetBubbleBorder(std::move(border));
+    GetBubbleFrameView()->OnNativeThemeChanged(GetNativeTheme());
   }
   // Update the current candidate window. We'll use candidate_window_ from here.
   // Note that SelectCandidateAt() uses candidate_window_.
