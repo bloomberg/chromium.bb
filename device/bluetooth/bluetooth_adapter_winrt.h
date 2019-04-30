73b6d6e79c63c7c4f2c6557a2a17f23e61a4d2af
Ken Buchanan <kenrb@chromium.org>
2019-04-30 21:41:00 +0000

    Move BluetoothAdapterWinrt initialization to lower priority thread
    BluetoothAdapterWinrt::Init() can cause Windows system libraries to be
    loaded, which causes jank because it has to be called on the main
    thread.
    
    This moves the heavy part of initialization to a lower priority thread
    and calls back to the main thread for completion.
    
    Bug: 929375
    Change-Id: I66cd1927b443cbe12991dceb87e508973aa8857f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1461353
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655394}
    
    Former-commit-id: b9e0ef8790d7c8df69922a24941fb81d464a562a

--- device/bluetooth/bluetooth_adapter_winrt.h
+++ device/bluetooth/bluetooth_adapter_winrt.h
@@ -79,6 +79,17 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothAdapterWinrt : public BluetoothAdapter {
   ~BluetoothAdapterWinrt() override;
 
   void Init(InitCallback init_cb);
+  // Allow tests to provide their own implementations of statics.
+  void InitForTests(
+      InitCallback init_cb,
+      Microsoft::WRL::ComPtr<
+          ABI::Windows::Devices::Bluetooth::IBluetoothAdapterStatics>
+          bluetooth_adapter_statics,
+      Microsoft::WRL::ComPtr<
+          ABI::Windows::Devices::Enumeration::IDeviceInformationStatics>
+          device_information_statics,
+      Microsoft::WRL::ComPtr<ABI::Windows::Devices::Radios::IRadioStatics>
+          radio_statics);
 
   // BluetoothAdapter:
   bool SetPoweredImpl(bool powered) override;
@@ -97,18 +108,7 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothAdapterWinrt : public BluetoothAdapter {
   void RemovePairingDelegateInternal(
       BluetoothDevice::PairingDelegate* pairing_delegate) override;
 
-  // These are declared virtual so that they can be overridden by tests.
-  virtual HRESULT GetBluetoothAdapterStaticsActivationFactory(
-      ABI::Windows::Devices::Bluetooth::IBluetoothAdapterStatics** statics)
-      const;
-
-  virtual HRESULT GetDeviceInformationStaticsActivationFactory(
-      ABI::Windows::Devices::Enumeration::IDeviceInformationStatics** statics)
-      const;
-
-  virtual HRESULT GetRadioStaticsActivationFactory(
-      ABI::Windows::Devices::Radios::IRadioStatics** statics) const;
-
+  // Declared virtual so that it can be overridden by tests.
   virtual HRESULT ActivateBluetoothAdvertisementLEWatcherInstance(
       ABI::Windows::Devices::Bluetooth::Advertisement::
           IBluetoothLEAdvertisementWatcher** instance) const;
@@ -120,6 +120,24 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothAdapterWinrt : public BluetoothAdapter {
       uint64_t raw_address);
 
  private:
+  struct StaticsInterfaces {
+    StaticsInterfaces(
+        Microsoft::WRL::ComPtr<IAgileReference>,   // IBluetoothStatics
+        Microsoft::WRL::ComPtr<IAgileReference>,   // IDeviceInformationStatics
+        Microsoft::WRL::ComPtr<IAgileReference>);  // IRadioStatics
+    StaticsInterfaces();
+    StaticsInterfaces(const StaticsInterfaces&);
+    ~StaticsInterfaces();
+
+    Microsoft::WRL::ComPtr<IAgileReference> adapter_statics;
+    Microsoft::WRL::ComPtr<IAgileReference> device_information_statics;
+    Microsoft::WRL::ComPtr<IAgileReference> radio_statics;
+  };
+
+  static StaticsInterfaces PerformSlowInitTasks();
+
+  void CompleteInit(InitCallback init_cb, StaticsInterfaces statics);
+
   void OnGetDefaultAdapter(
       base::ScopedClosureRunner on_init,
       Microsoft::WRL::ComPtr<
@@ -202,6 +220,15 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothAdapterWinrt : public BluetoothAdapter {
   Microsoft::WRL::ComPtr<ABI::Windows::Devices::Bluetooth::Advertisement::
                              IBluetoothLEAdvertisementWatcher>
       ble_advertisement_watcher_;
+
+  Microsoft::WRL::ComPtr<
+      ABI::Windows::Devices::Bluetooth::IBluetoothAdapterStatics>
+      bluetooth_adapter_statics_;
+  Microsoft::WRL::ComPtr<
+      ABI::Windows::Devices::Enumeration::IDeviceInformationStatics>
+      device_information_statics_;
+  Microsoft::WRL::ComPtr<ABI::Windows::Devices::Radios::IRadioStatics>
+      radio_statics_;
 
   THREAD_CHECKER(thread_checker_);
 
