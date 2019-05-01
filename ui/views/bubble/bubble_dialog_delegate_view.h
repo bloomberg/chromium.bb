1cbd0205b00a251b9f3a938f943b97c39ce71111
Caroline Rising <corising@chromium.org>
2019-05-01 19:42:54 +0000

    Change BubbleDialogDelegateView RTL mirroring to happen when setting the arrow.
    Previously this was done during creation of the nonclient view. This created issues in cases where the arrow was explicitly reset to a value fitting for only LTR after nonclient view creation.
    
    Fix GlobalErrorBubbleViewUnittest to use initialized values.
    
    Bug: 956001
    Change-Id: Ibfa5b598207f360f015de356e5cb01f5fb3eabb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1590224
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#655720}
    
    Former-commit-id: 428d420de0ef73ea87348326d5d683ceedaae666

--- ui/views/bubble/bubble_dialog_delegate_view.h
+++ ui/views/bubble/bubble_dialog_delegate_view.h
@@ -81,7 +81,7 @@ class VIEWS_EXPORT BubbleDialogDelegateView : public DialogDelegateView,
   // The anchor rect is used in the absence of an assigned anchor view.
   const gfx::Rect& anchor_rect() const { return anchor_rect_; }
 
-  BubbleBorder::Arrow arrow() const { return arrow_; }
+  // Set the desired arrow for the bubble. The arrow will be mirrored for RTL.
   void SetArrow(BubbleBorder::Arrow arrow);
 
   BubbleBorder::Shadow GetShadow() const;
@@ -139,6 +139,9 @@ class VIEWS_EXPORT BubbleDialogDelegateView : public DialogDelegateView,
       View* anchor_view,
       BubbleBorder::Arrow arrow,
       BubbleBorder::Shadow shadow = BubbleBorder::DIALOG_SHADOW);
+
+  // Returns the desired arrow post-RTL mirroring if needed.
+  BubbleBorder::Arrow arrow() const { return arrow_; }
 
   // Get bubble bounds from the anchor rect and client view's preferred size.
   virtual gfx::Rect GetBubbleBounds();
@@ -218,8 +221,8 @@ class VIEWS_EXPORT BubbleDialogDelegateView : public DialogDelegateView,
   // The anchor rect used in the absence of an anchor view.
   mutable gfx::Rect anchor_rect_;
 
-  // The arrow's location on the bubble.
-  BubbleBorder::Arrow arrow_;
+  // The arrow's default location on the bubble post-RTL mirroring if needed.
+  BubbleBorder::Arrow arrow_ = BubbleBorder::NONE;
 
   // Bubble border shadow to use.
   BubbleBorder::Shadow shadow_;
