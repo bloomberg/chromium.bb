eea4cdd542b4720280aea05bc185e276f6debc59
Gabriel Charette <gab@chromium.org>
2019-05-02 15:29:21 +0000

    [base] Add diagnosis stack traces to thread_restrictions assertions.
    This was extracted from https://chromium-review.googlesource.com/c/chromium/src/+/1283010
    where I realized that they'll be useful beyond their initial in-place
    diagnosis mandate.
    
    I hope this will help alleviate confusion about what set the current
    restrictions when a developer hits such an assert.
    
    Bug: 707362
    Change-Id: I01bc17b5dd97945d05860411dd2b68ed93f6907d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1379027
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656017}
    
    Former-commit-id: aab2c3d2688a636a9d704e4376620637dd5e6249

--- base/threading/thread_restrictions.cc
+++ base/threading/thread_restrictions.cc
@@ -6,25 +6,60 @@
 
 #if DCHECK_IS_ON()
 
+#include "base/debug/stack_trace.h"
 #include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/threading/thread_local.h"
+#include "build/build_config.h"
 
 namespace base {
 
 namespace {
 
-LazyInstance<ThreadLocalBoolean>::Leaky g_blocking_disallowed =
+#if defined(OS_NACL)
+using ThreadLocalBooleanWithStacks = ThreadLocalBoolean;
+#else
+class ThreadLocalBooleanWithStacks {
+ public:
+  ThreadLocalBooleanWithStacks() = default;
+
+  bool Get() const { return bool_.Get(); }
+
+  void Set(bool val) {
+    stack_.Set(std::make_unique<debug::StackTrace>());
+    bool_.Set(val);
+  }
+
+  friend std::ostream& operator<<(std::ostream& out,
+                                  const ThreadLocalBooleanWithStacks& tl) {
+    out << "currently set to " << (tl.bool_.Get() ? "true" : "false") << " by ";
+
+    if (!tl.stack_.Get())
+      return out << "default value\n";
+    out << "\n";
+    tl.stack_.Get()->OutputToStream(&out);
+    return out;
+  }
+
+ private:
+  ThreadLocalBoolean bool_;
+  ThreadLocalOwnedPointer<debug::StackTrace> stack_;
+
+  DISALLOW_COPY_AND_ASSIGN(ThreadLocalBooleanWithStacks);
+};
+#endif  // defined(OS_NACL)
+
+LazyInstance<ThreadLocalBooleanWithStacks>::Leaky g_blocking_disallowed =
     LAZY_INSTANCE_INITIALIZER;
 
-LazyInstance<ThreadLocalBoolean>::Leaky
-    g_singleton_disallowed = LAZY_INSTANCE_INITIALIZER;
-
-LazyInstance<ThreadLocalBoolean>::Leaky g_base_sync_primitives_disallowed =
+LazyInstance<ThreadLocalBooleanWithStacks>::Leaky g_singleton_disallowed =
     LAZY_INSTANCE_INITIALIZER;
 
-LazyInstance<ThreadLocalBoolean>::Leaky g_cpu_intensive_work_disallowed =
-    LAZY_INSTANCE_INITIALIZER;
+LazyInstance<ThreadLocalBooleanWithStacks>::Leaky
+    g_base_sync_primitives_disallowed = LAZY_INSTANCE_INITIALIZER;
+
+LazyInstance<ThreadLocalBooleanWithStacks>::Leaky
+    g_cpu_intensive_work_disallowed = LAZY_INSTANCE_INITIALIZER;
 
 }  // namespace
 
@@ -36,7 +71,11 @@ void AssertBlockingAllowed() {
          "blocking! If this task is running inside the ThreadPool, it needs "
          "to have MayBlock() in its TaskTraits. Otherwise, consider making "
          "this blocking work asynchronous or, as a last resort, you may use "
-         "ScopedAllowBlocking (see its documentation for best practices).";
+         "ScopedAllowBlocking (see its documentation for best practices).\n"
+#if !defined(OS_NACL)
+      << "g_blocking_disallowed " << g_blocking_disallowed.Get()
+#endif
+      ;
 }
 
 }  // namespace internal
@@ -73,7 +112,11 @@ ScopedAllowBaseSyncPrimitives::ScopedAllowBaseSyncPrimitives()
     : was_disallowed_(g_base_sync_primitives_disallowed.Get().Get()) {
   DCHECK(!g_blocking_disallowed.Get().Get())
       << "To allow //base sync primitives in a scope where blocking is "
-         "disallowed use ScopedAllowBaseSyncPrimitivesOutsideBlockingScope.";
+         "disallowed use ScopedAllowBaseSyncPrimitivesOutsideBlockingScope.\n"
+#if !defined(OS_NACL)
+      << "g_blocking_disallowed " << g_blocking_disallowed.Get()
+#endif
+      ;
   g_base_sync_primitives_disallowed.Get().Set(false);
 }
 
@@ -114,7 +157,14 @@ void AssertBaseSyncPrimitivesAllowed() {
          "prevent jank and deadlock. If waiting on a //base sync primitive is "
          "unavoidable, do it within the scope of a "
          "ScopedAllowBaseSyncPrimitives. If in a test, "
-         "use ScopedAllowBaseSyncPrimitivesForTesting.";
+         "use ScopedAllowBaseSyncPrimitivesForTesting.\n"
+#if !defined(OS_NACL)
+      << "g_base_sync_primitives_disallowed "
+      << g_base_sync_primitives_disallowed.Get()
+      << "It can be useful to know that g_blocking_disallowed is "
+      << g_blocking_disallowed.Get()
+#endif
+      ;
 }
 
 void ResetThreadRestrictionsForTesting() {
@@ -129,7 +179,13 @@ void ResetThreadRestrictionsForTesting() {
 void AssertLongCPUWorkAllowed() {
   DCHECK(!g_cpu_intensive_work_disallowed.Get().Get())
       << "Function marked as CPU intensive was called from a scope that "
-         "disallows this kind of work! Consider making this work asynchronous.";
+         "disallows this kind of work! Consider making this work "
+         "asynchronous.\n"
+#if !defined(OS_NACL)
+      << "g_cpu_intensive_work_disallowed "
+      << g_cpu_intensive_work_disallowed.Get()
+#endif
+      ;
 }
 
 void DisallowUnresponsiveTasks() {
@@ -161,16 +217,18 @@ bool ThreadRestrictions::SetSingletonAllowed(bool allowed) {
 
 // static
 void ThreadRestrictions::AssertSingletonAllowed() {
-  if (g_singleton_disallowed.Get().Get()) {
-    NOTREACHED() << "LazyInstance/Singleton is not allowed to be used on this "
-                 << "thread.  Most likely it's because this thread is not "
-                 << "joinable (or the current task is running with "
-                 << "TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN semantics), so "
-                 << "AtExitManager may have deleted the object on shutdown, "
-                 << "leading to a potential shutdown crash. If you need to use "
-                 << "the object from this context, it'll have to be updated to "
-                 << "use Leaky traits.";
-  }
+  DCHECK(!g_singleton_disallowed.Get().Get())
+      << "LazyInstance/Singleton is not allowed to be used on this thread. "
+         "Most likely it's because this thread is not joinable (or the current "
+         "task is running with TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN "
+         "semantics), so AtExitManager may have deleted the object on "
+         "shutdown, leading to a potential shutdown crash. If you need to use "
+         "the object from this context, it'll have to be updated to use Leaky "
+         "traits.\n"
+#if !defined(OS_NACL)
+      << "g_singleton_disallowed " << g_singleton_disallowed.Get()
+#endif
+      ;
 }
 
 // static
