9424338d4d9388074612b909b50c72d3737ebb65
kyslov <kyslov@google.com>
2019-05-02 15:33:32 -0700

    Disable screen content tools for RT mode
    Improper use of screen tools caused encoder to select a wrong entropy
    context and led to mismatch with decoder. Disabling screen tools for RT
    mode
    
    Change-Id: I559138a639c735aa1d946789620a175f7421d36f

--- av1/encoder/encoder.c
+++ av1/encoder/encoder.c
@@ -1031,7 +1031,7 @@ static void init_seq_coding_tools(SequenceHeader *seq, AV1_COMMON *cm,
   seq->still_picture = (oxcf->limit == 1);
   seq->reduced_still_picture_hdr = seq->still_picture;
   seq->reduced_still_picture_hdr &= !oxcf->full_still_picture_hdr;
-  seq->force_screen_content_tools = 2;
+  seq->force_screen_content_tools = (oxcf->mode == REALTIME) ? 0 : 2;
   seq->force_integer_mv = 2;
   seq->order_hint_info.enable_order_hint = oxcf->enable_order_hint;
   seq->frame_id_numbers_present_flag =
