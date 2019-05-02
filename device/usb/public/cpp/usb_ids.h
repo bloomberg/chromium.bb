9e0c8335be27b1381069add99d399016ba876745
Donna Wu <donna.wu@intel.com>
2019-05-02 15:15:38 +0000

    Expose usb_ids.h to public users.
    This is a preparation to limit the visibility of //device/usb.
    
    Bug: 699790
    Change-Id: I0c9a7ac5aa8b2f25f1c9757dd74fd2632a82155b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1574717
    Commit-Queue: Donna Wu <donna.wu@intel.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656012}
    
    Former-commit-id: 799cdec074cae70cbef7715f4e52ec709eecff28

---
+++ device/usb/public/cpp/usb_ids.h
@@ -0,0 +1,11 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DEVICE_USB_PUBLIC_CPP_USB_IDS_H_
+#define DEVICE_USB_PUBLIC_CPP_USB_IDS_H_
+
+// This header is just used to expose usb_ids.h to outside users.
+#include "device/usb/usb_ids.h"
+
+#endif  // DEVICE_USB_PUBLIC_CPP_USB_IDS_H_
