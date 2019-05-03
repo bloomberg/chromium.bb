d48b7d2f0094815e0c2cdc98b454eb4f244bbb85
Gayane Petrosyan <gayane@chromium.org>
2019-05-03 22:24:37 +0000

    [NTP] Don't treat GTK as default theme.
    Befor fix: https://screenshot.googleplex.com/k9mtdC35rWE.png
    After fix: https://screenshot.googleplex.com/AzjUjFH9Jtw.png
    
    Bug: 958538
    Change-Id: Id85f5a367bdd9b38293f5fd2f1577e7cf5513bc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1594926
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656555}
    
    Former-commit-id: 7be586d5142f5e97df37bfd27e002d38655d05de

--- chrome/browser/search/instant_service.cc
+++ chrome/browser/search/instant_service.cc
@@ -548,10 +548,9 @@ void InstantService::BuildThemeInfo() {
   // Get theme information from theme service.
   theme_info_.reset(new ThemeBackgroundInfo());
 
-  // Get if the current theme is the default theme (or GTK+ on linux).
+  // Get if the current theme is the default theme.
   ThemeService* theme_service = ThemeServiceFactory::GetForProfile(profile_);
-  theme_info_->using_default_theme =
-      theme_service->UsingDefaultTheme() || theme_service->UsingSystemTheme();
+  theme_info_->using_default_theme = theme_service->UsingDefaultTheme();
 
   theme_info_->using_dark_mode = dark_mode_observer_->InDarkMode();
 
