5dc5351308f1ed1bc383b3c8ef5e3db739f9ff74
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2019-05-03 17:28:18 +0000

    [Headless] Split browser/renderer process dependencies
    This CL splits //headless:headless into 2 components:
    - :headless_renderer
    - :headless_non_renderer
    
    :headless_renderer is intended for child processes, and has a smaller
    footprint than :headless_non_renderer.
    
    For release builds, this reduces the  size of chrome_child.dll by about
    9.7MB.
    
    Bug: 929408
    Change-Id: I514a1bb5604f8ab8d6c6031970b5c7a965a8393d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1570654
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656433}
    
    Former-commit-id: 9d58d48148490cacee5cdc2c48303aa8d66c6836

--- headless/app/headless_shell.cc
+++ headless/app/headless_shell.cc
@@ -32,7 +32,7 @@
 #include "content/public/common/content_switches.h"
 #include "headless/app/headless_shell.h"
 #include "headless/app/headless_shell_switches.h"
-#include "headless/lib/browser/headless_devtools.h"
+#include "headless/lib/headless_content_main_delegate.h"
 #include "headless/public/headless_devtools_target.h"
 #include "net/base/filename_util.h"
 #include "net/base/host_port_pair.h"
@@ -50,6 +50,11 @@
 #include "components/crash/content/app/crash_switches.h"
 #include "components/crash/content/app/run_as_crashpad_handler_win.h"
 #include "sandbox/win/src/sandbox_types.h"
+#endif
+
+#if !defined(CHROME_MULTIPLE_DLL_CHILD)
+#include "headless/lib/browser/headless_browser_impl.h"
+#include "headless/lib/browser/headless_devtools.h"
 #endif
 
 namespace headless {
@@ -136,6 +141,34 @@ base::FilePath GetSSLKeyLogFile(const base::CommandLine* command_line) {
 }
 
 #endif
+
+int RunContentMain(
+    HeadlessBrowser::Options options,
+    base::OnceCallback<void(HeadlessBrowser*)> on_browser_start_callback) {
+  content::ContentMainParams params(nullptr);
+#if defined(OS_WIN)
+  // Sandbox info has to be set and initialized.
+  CHECK(options.sandbox_info);
+  params.instance = options.instance;
+  params.sandbox_info = std::move(options.sandbox_info);
+#elif !defined(OS_ANDROID)
+  params.argc = options.argc;
+  params.argv = options.argv;
+#endif
+
+  // TODO(skyostil): Implement custom message pumps.
+  DCHECK(!options.message_pump);
+
+#if !defined(CHROME_MULTIPLE_DLL_CHILD)
+  std::unique_ptr<HeadlessBrowserImpl> browser(new HeadlessBrowserImpl(
+      std::move(on_browser_start_callback), std::move(options)));
+  HeadlessContentMainDelegate delegate(std::move(browser));
+#else
+  HeadlessContentMainDelegate delegate(std::move(options));
+#endif
+  params.delegate = &delegate;
+  return content::ContentMain(params);
+}
 
 }  // namespace
 
@@ -774,6 +807,52 @@ int HeadlessShellMain(const content::ContentMainParams& params) {
 #else
   return HeadlessShellMain(params.argc, params.argv);
 #endif
+}
+
+#if defined(OS_WIN)
+void RunChildProcessIfNeeded(HINSTANCE instance,
+                             sandbox::SandboxInterfaceInfo* sandbox_info) {
+  base::CommandLine::Init(0, nullptr);
+  HeadlessBrowser::Options::Builder builder(0, nullptr);
+  builder.SetInstance(instance);
+  builder.SetSandboxInfo(std::move(sandbox_info));
+#else
+void RunChildProcessIfNeeded(int argc, const char** argv) {
+  base::CommandLine::Init(argc, argv);
+  HeadlessBrowser::Options::Builder builder(argc, argv);
+#endif  // defined(OS_WIN)
+  const base::CommandLine& command_line(
+      *base::CommandLine::ForCurrentProcess());
+
+  if (!command_line.HasSwitch(::switches::kProcessType))
+    return;
+
+  if (command_line.HasSwitch(switches::kUserAgent)) {
+    std::string ua = command_line.GetSwitchValueASCII(switches::kUserAgent);
+    if (net::HttpUtil::IsValidHeaderValue(ua))
+      builder.SetUserAgent(ua);
+  }
+
+  exit(RunContentMain(builder.Build(),
+                      base::OnceCallback<void(HeadlessBrowser*)>()));
+}
+
+int HeadlessBrowserMain(
+    HeadlessBrowser::Options options,
+    base::OnceCallback<void(HeadlessBrowser*)> on_browser_start_callback) {
+  DCHECK(!on_browser_start_callback.is_null());
+#if DCHECK_IS_ON()
+  // The browser can only be initialized once.
+  static bool browser_was_initialized;
+  DCHECK(!browser_was_initialized);
+  browser_was_initialized = true;
+
+  // Child processes should not end up here.
+  DCHECK(!base::CommandLine::ForCurrentProcess()->HasSwitch(
+      ::switches::kProcessType));
+#endif
+  return RunContentMain(std::move(options),
+                        std::move(on_browser_start_callback));
 }
 
 }  // namespace headless
