e1978b70986dd2fd7cea75a292e456f1bb8a34aa
Robert Kaplow <rkaplow@chromium.org>
2019-05-03 23:11:07 +0000

    Minor cleanup of debug code in persisted_logs.
    Here is where they were added: https://codereview.chromium.org/2721883003. Seems reasonable to leave the other DCHECKS.
    
    Bug: 695433
    Change-Id: Icd08fbe9b113dcac3ff82ab94153bff3bdaa3cac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1594191
    Commit-Queue: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656573}
    
    Former-commit-id: 92271ab92a72c7da24f7dd61b6e5c84dfe3177c9

--- components/metrics/persisted_logs.cc
+++ components/metrics/persisted_logs.cc
@@ -30,8 +30,7 @@ const char kLogTimestampKey[] = "timestamp";
 const char kLogDataKey[] = "data";
 
 std::string EncodeToBase64(const std::string& to_convert) {
-  // CHECK to diagnose crbug.com/695433
-  CHECK(to_convert.data());
+  DCHECK(to_convert.data());
   std::string base64_result;
   base::Base64Encode(to_convert, &base64_result);
   return base64_result;
@@ -142,9 +141,7 @@ void PersistedLogs::StageNextLog() {
 }
 
 void PersistedLogs::DiscardStagedLog() {
-  // CHECK, rather than DCHECK, to diagnose cause of crashes from the field,
-  // for crbug.com/695433.
-  CHECK(has_staged_log());
+  DCHECK(has_staged_log());
   DCHECK_LT(static_cast<size_t>(staged_log_index_), list_.size());
   list_.erase(list_.begin() + staged_log_index_);
   staged_log_index_ = -1;
