f059e12a56183497c51f62ac1517d1ab8fae040f
Robert Ogden <robertogden@chromium.org>
2019-05-04 04:05:40 +0000

    Reland "Fix random_coin_flip_for_navigation_ not being initalized"
    This is a reland of 267d7e03ab1c51898f9b14b409cdef21eb361980 [formerly 278e36a415acdc022fc8459cfdd8ac8da4b55ae3]
    
    TBR=tbansal@chromium.org
    
    Original change's description:
    > Fix random_coin_flip_for_navigation_ not being initalized
    >
    > Bug: 959257, 955472
    > Change-Id: I6def667135b63e973c50221356e1648490a76406
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1594976
    > Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    > Commit-Queue: Robert Ogden <robertogden@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#656566}
    
    Bug: 959257, 955472
    Change-Id: I8fb63cea1ba3b4ab0f7ead571ff50b69e625c2a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1595429
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656640}
    
    Former-commit-id: 005c1470191b730e7608d2098dc6417842c4c568

--- components/previews/content/previews_user_data_unittest.cc
+++ components/previews/content/previews_user_data_unittest.cc
@@ -43,6 +43,7 @@ TEST(PreviewsUserDataTest, DeepCopy) {
 
   PreviewsUserData data_copy(*data);
   EXPECT_EQ(id, data_copy.page_id());
+  EXPECT_EQ(data->CoinFlipForNavigation(), data_copy.CoinFlipForNavigation());
   EXPECT_EQ(123, data_copy.data_savings_inflation_percent());
   EXPECT_TRUE(data_copy.cache_control_no_transform_directive());
   EXPECT_EQ(previews::PreviewsType::NOSCRIPT,
