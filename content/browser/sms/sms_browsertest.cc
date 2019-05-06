bde917027b910f8a1fcaff2e2f009fc4e3a13b2b
Ayu Ishii <ayui@chromium.org>
2019-05-06 18:13:24 +0000

    [sms] Sms Receiver browser test
    This change implements a browser end to end test that connects the
    JS bindings with a mocked SmsProvider protecting ourselves against
    regressions in the communection between the renderer process and the
    browser process.
    
    Bug: 958565
    Change-Id: Ie9e957d848b6c963c3635d682242101d2107a2db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1592647
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656873}
    
    Former-commit-id: 5267967fbbe28c24db38bbd2b15acfd1305b6db6

---
+++ content/browser/sms/sms_browsertest.cc
@@ -0,0 +1,86 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/time/time.h"
+#include "content/browser/renderer_host/render_process_host_impl.h"
+#include "content/browser/storage_partition_impl.h"
+#include "content/public/test/browser_test_utils.h"
+#include "content/public/test/content_browser_test.h"
+#include "content/public/test/content_browser_test_utils.h"
+#include "content/shell/browser/shell.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+using ::testing::_;
+using ::testing::Invoke;
+using ::testing::NiceMock;
+
+namespace content {
+
+namespace {
+
+class MockSmsProvider : public SmsProvider {
+ public:
+  MockSmsProvider() = default;
+  ~MockSmsProvider() override = default;
+
+  void Retrieve(base::TimeDelta timeout, SmsCallback callback) {
+    DoRetrieve(timeout, &callback);
+  }
+
+  MOCK_METHOD2(DoRetrieve, void(base::TimeDelta, SmsCallback*));
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(MockSmsProvider);
+};
+
+class SmsTest : public ContentBrowserTest {
+ public:
+  SmsTest() = default;
+  ~SmsTest() override = default;
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    ContentBrowserTest::SetUpCommandLine(command_line);
+    command_line->AppendSwitchASCII("enable-blink-features", "SmsReceiver");
+  }
+};
+
+}  // namespace
+
+IN_PROC_BROWSER_TEST_F(SmsTest, Start) {
+  NavigateToURL(shell(), GetTestUrl(nullptr, "simple_page.html"));
+
+  auto mock_sms_provider = std::make_unique<NiceMock<MockSmsProvider>>();
+  auto* rph = static_cast<RenderProcessHostImpl*>(
+      shell()->web_contents()->GetMainFrame()->GetProcess());
+  SmsManager* sms_mgr =
+      static_cast<StoragePartitionImpl*>(rph->GetStoragePartition())
+          ->GetSmsManager();
+
+  // Test that SMS content can be retrieved after SmsManager.start().
+  std::string script = R"(
+    (async () => {
+      let receiver = new SMSReceiver({timeout: 60});
+      await receiver.start();
+      return new Promise(function(resolve) {
+        receiver.addEventListener('change', e => {
+          resolve(receiver.sms.content);
+        })
+      });
+    }) ();
+  )";
+
+  EXPECT_CALL(*mock_sms_provider,
+              DoRetrieve(base::TimeDelta::FromSeconds(60), _))
+      .WillOnce(
+          Invoke([](base::TimeDelta timeout,
+                    base::OnceCallback<void(const std::string&)>* callback) {
+            std::move(*callback).Run("hello");
+          }));
+
+  sms_mgr->SetSmsProviderForTest(std::move(mock_sms_provider));
+
+  EXPECT_EQ("hello", EvalJs(shell(), script));
+}
+
+}  // namespace content
