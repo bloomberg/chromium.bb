64ac3b71c0dae101d4b8aea00f6cdf87afe75f7a
Jérôme Lebel <jlebel@chromium.org>
2019-05-06 16:19:45 +0000

    [iOS] Adding cancel button in Identity chooser for accessibility
    Adding transparent button in background for the identity chooser view.
    This button is used for accessibility.
    The button implementation (AccessibilityCloseButton) is shared between
    PopupMenuViewController and IdentityChooserPresentationController.
    
    Bug: 943174
    Change-Id: I8cce0d99e87fb51d9468d9eca9e24592b038542b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1592652
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656815}
    
    Former-commit-id: 75599c28c3c7bc3b53dffb0addf950fac43c5f12

--- ios/chrome/browser/ui/popup_menu/public/popup_menu_view_controller.mm
+++ ios/chrome/browser/ui/popup_menu/public/popup_menu_view_controller.mm
@@ -7,25 +7,12 @@
 #import "ios/chrome/browser/ui/image_util/image_util.h"
 #import "ios/chrome/browser/ui/popup_menu/public/popup_menu_ui_constants.h"
 #import "ios/chrome/browser/ui/popup_menu/public/popup_menu_view_controller_delegate.h"
+#import "ios/chrome/browser/ui/util/accessibility_close_menu_button.h"
 #import "ios/chrome/common/ui_util/constraints_ui_util.h"
-#include "ios/chrome/grit/ios_strings.h"
-#include "ui/base/l10n/l10n_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
-
-// UIButton allowing the user to close the menu with VoiceOver.
-@interface PopupMenuCloseButton : UIButton
-@end
-
-@implementation PopupMenuCloseButton
-// If accessibilityActivate isn't overriden, VoiceOver isn't able to close the
-// menu.
-- (BOOL)accessibilityActivate {
-  return [super accessibilityActivate];
-}
-@end
 
 namespace {
 const CGFloat kImageMargin = 196;
@@ -49,12 +36,10 @@ const CGFloat kBackgroundAlpha = 0.65;
   self = [super initWithNibName:nil bundle:nil];
   if (self) {
     UIButton* closeButton =
-        [PopupMenuCloseButton buttonWithType:UIButtonTypeCustom];
+        [AccessibilityCloseMenuButton buttonWithType:UIButtonTypeCustom];
     [closeButton addTarget:self
                     action:@selector(dismissPopup)
           forControlEvents:UIControlEventTouchUpInside];
-    closeButton.accessibilityLabel =
-        l10n_util::GetNSString(IDS_IOS_TOOLBAR_CLOSE_MENU);
     closeButton.translatesAutoresizingMaskIntoConstraints = NO;
     [self.view addSubview:closeButton];
     AddSameConstraints(self.view, closeButton);
