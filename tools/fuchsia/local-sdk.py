45ff6494bf44035b2fa5779cf1b96f9e6881277c
Sergey Ulanov <sergeyu@chromium.org>
2019-05-06 18:14:35 +0000

    [Fuchsia] Update local-sdk.py
    There is no longer a target for "topaz" sdk, but it can still be
    generated from "core" and "fuchsia_dart". Updated the script to build
    fuchsia_dart target and then merge core and fuchsia_dart packages to
    together to generate SDK.
    
    Change-Id: I6a624845aa5eb7acf6f1f45dc0e0a697ad6acdf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1595506
    Reviewed-by: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656875}
    
    Former-commit-id: 40f8d0d62bccaad93dde70d05d4e18b4cc41823a

--- tools/fuchsia/local-sdk.py
+++ tools/fuchsia/local-sdk.py
@@ -5,6 +5,7 @@
 # found in the LICENSE file.
 
 import hashlib
+import json
 import os
 import shutil
 import subprocess
@@ -36,7 +37,8 @@ def BuildForArch(arch):
   Run('scripts/fx', '--dir', build_dir, 'set', 'terminal.' + arch,
       '--with=//topaz/packages/sdk:topaz', '--with-base=//sdk/bundles:tools',
       '--args=is_debug=false', '--args=build_sdk_archives=true')
-  Run('scripts/fx', 'build', 'topaz/public/sdk:topaz', 'sdk:images_archive')
+  Run('scripts/fx', 'build', 'topaz/public/sdk:fuchsia_dart', 'sdk',
+      'sdk:images_archive')
 
 
 def main(args):
@@ -64,7 +66,9 @@ def main(args):
       os.path.join(fuchsia_root, 'out', 'release-' + arch, 'sdk', 'archive',
                    'images.tar.gz'),
       os.path.join(fuchsia_root, 'out', 'release-' + arch, 'sdk', 'archive',
-                   'topaz.tar.gz')
+                   'core.tar.gz'),
+      os.path.join(fuchsia_root, 'out', 'release-' + arch, 'sdk', 'archive',
+                   'fuchsia_dart.tar.gz'),
   ]
 
   # Nuke the SDK from DEPS, put our just-built one there, and set a fake .hash
@@ -73,8 +77,29 @@ def main(args):
   output_dir = os.path.join(REPOSITORY_ROOT, 'third_party', 'fuchsia-sdk',
                             'sdk')
   EnsureEmptyDir(output_dir)
+
+  # Extract tars merging manifests
+  manifest_path = os.path.join(output_dir, 'meta', 'manifest.json')
+  merged_manifest = None
+  parts = set()
   for sdk_tar in sdk_tars:
     tarfile.open(sdk_tar, mode='r:gz').extractall(path=output_dir)
+
+    # Merge the manifest ensuring that we don't have duplicate entries.
+    if os.path.isfile(manifest_path):
+      manifest = json.load(open(manifest_path))
+      os.remove(manifest_path)
+      if not merged_manifest:
+        merged_manifest = manifest
+      else:
+        for part in manifest['parts']:
+          if part['meta'] not in parts:
+            parts.add(part['meta'])
+            merged_manifest['parts'].append(part)
+
+  # Write merged manifest file.
+  with open(manifest_path, 'w') as manifest_file:
+    json.dump(merged_manifest, manifest_file)
 
   print 'Hashing sysroot...'
   # Hash the sysroot to catch updates to the headers, but don't hash the whole
