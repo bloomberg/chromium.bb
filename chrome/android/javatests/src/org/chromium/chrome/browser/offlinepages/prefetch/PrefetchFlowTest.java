9f9f2a589ad92f1cb175a5e89f9f2ed0a559aaf7
Christian Dullweber <dullweber@chromium.org>
2019-05-06 09:52:04 +0000

    Disable flaky PrefetchFlowTest#testPrefetchPageReadyLater
    This test is very flaky.
    
    TBR: carlosk@chromium.org
    Bug: 959733
    Change-Id: Ieb98b5373f9ce4744077c5e01e49434655d2f9ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1596564
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656748}
    
    Former-commit-id: 7b8341c5274899998f000cb8c513e3a9d5e8651f

--- chrome/android/javatests/src/org/chromium/chrome/browser/offlinepages/prefetch/PrefetchFlowTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/offlinepages/prefetch/PrefetchFlowTest.java
@@ -19,6 +19,7 @@ import org.chromium.base.CommandLine;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.browser.ChromeActivity;
@@ -252,6 +253,7 @@ public class PrefetchFlowTest implements WebServer.RequestHandler {
     @Test
     @MediumTest
     @Feature({"OfflinePrefetch"})
+    @DisabledTest
     public void testPrefetchPageReadyLater() throws Throwable {
         final String url1 = "http://suggestion1.com/main";
         mSuggestionsService.addSuggestion("Suggestion 1", url1, imageUrlBase() + "suggestion1");
