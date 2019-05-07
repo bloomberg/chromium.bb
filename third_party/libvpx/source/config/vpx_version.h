d863146fb143599996b73ad51dfdf8712f8b4c71
Jerome Jiang <jianj@google.com>
2019-05-07 01:03:07 +0000

    Roll src/third_party/libvpx/source/libvpx/ 3fd96f7d7..1cbcb820a (3 commits)
    https://chromium.googlesource.com/webm/libvpx.git/+log/3fd96f7d7d84..1cbcb820ace4
    
    $ git log 3fd96f7d7..1cbcb820a --date=short --no-merges --format='%ad %ae %s'
    2019-05-06 jianj Revert "vp9-rtc: tx_size selection for intra mode in nonrd"
    2019-05-02 johannkoenig android: do not attempt standalone builds
    2019-05-02 venkatarama.avadhani Exclude VP9 assemblies from VP8 builds
    
    Created with:
      roll-dep src/third_party/libvpx/source/libvpx
    R=johannkoenig@google.com
    BUG=b/131414589
    
    Change-Id: Ic099abeaeb07f60479f4c8b81852e0535ee50bee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1597347
    Reviewed-by: Johann Koenig <johannkoenig@google.com>
    Commit-Queue: Jerome Jiang <jianj@google.com>
    Cr-Commit-Position: refs/heads/master@{#657064}
    
    Former-commit-id: 3e28563ee3db782b31b3085ac306e8147b42ff0a

--- third_party/libvpx/source/config/vpx_version.h
+++ third_party/libvpx/source/config/vpx_version.h
@@ -2,7 +2,7 @@
 #define VERSION_MAJOR  1
 #define VERSION_MINOR  8
 #define VERSION_PATCH  0
-#define VERSION_EXTRA  "456-g3fd96f7d7d"
+#define VERSION_EXTRA  "462-g1cbcb820ac"
 #define VERSION_PACKED ((VERSION_MAJOR<<16)|(VERSION_MINOR<<8)|(VERSION_PATCH))
-#define VERSION_STRING_NOSP "v1.8.0-456-g3fd96f7d7d"
-#define VERSION_STRING      " v1.8.0-456-g3fd96f7d7d"
+#define VERSION_STRING_NOSP "v1.8.0-462-g1cbcb820ac"
+#define VERSION_STRING      " v1.8.0-462-g1cbcb820ac"
