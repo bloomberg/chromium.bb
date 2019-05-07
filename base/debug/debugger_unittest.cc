4d11b9b1e2ac2e74d224bb9d38d6f610a719a356
Richard Townsend <richard.townsend@arm.com>
2019-05-07 23:28:00 +0000

    fix: Debugger.DoesntExecuteBeyondBreakpoint failure on Windows ARM64
    Bug: 925584, 893460
    Change-Id: If3da3b316d2e60a36895301b45c165dd576a523f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1593360
    Commit-Queue: Richard Townsend <richard.townsend@arm.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657503}
    
    Former-commit-id: 0a350b7c5adf5be15a4b1cb5baf6c0ac67187a09

--- base/debug/debugger_unittest.cc
+++ base/debug/debugger_unittest.cc
@@ -3,8 +3,12 @@
 // found in the LICENSE file.
 
 #include "base/debug/debugger.h"
-
 #include "build/build_config.h"
+
+#if defined(OS_WIN)
+#include <windows.h>
+#endif
+
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
@@ -32,8 +36,14 @@ TEST(Debugger, CrashAtBreakpoint) {
 
 #if defined(OS_WIN)
 TEST(Debugger, DoesntExecuteBeyondBreakpoint) {
+#if defined(ARCH_CPU_ARM64)
+  // brk on aarch64 Windows seems to cause an illegal instruction exception
   EXPECT_EXIT(CrashWithBreakDebugger(),
-              ::testing::ExitedWithCode(0x80000003), "");
+              ::testing::ExitedWithCode(STATUS_ILLEGAL_INSTRUCTION), "");
+#else
+  EXPECT_EXIT(CrashWithBreakDebugger(),
+              ::testing::ExitedWithCode(STATUS_BREAKPOINT), "");
+#endif
 }
 #endif  // defined(OS_WIN)
 
