6cc8ea80b7c5c9e87d467ad74f04d9315133f6e7
Alex Danilo <adanilo@chromium.org>
2019-05-07 03:13:46 +0000

    Remove horizontal scroll on directory tree.
    CL:1593885 adds use of scrollIntoViewIfNeeded() and then checks
    if scrolling has happened and then does an additonal horizontal
    scroll if needed. CL:1591564 disables horizontal scrolling and
    as a result the horizontal scroll still happens causing a white
    line to appear on the right hand side of FileManager.
    
    This CL removes that additional scrolling call which triggers
    horizontal scroll behavior.
    
    Bug: 955566
    Tests: Manually checked, looks like its due to a CSS layout bug.
    Change-Id: I277a920521c427dd6f63916b3cf644d990bbe9b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1597929
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657105}
    
    Former-commit-id: 7b6d19748f4e5d67d0042af781b5374da11b6a29

--- ui/file_manager/file_manager/foreground/js/ui/directory_tree.js
+++ ui/file_manager/file_manager/foreground/js/ui/directory_tree.js
@@ -1994,14 +1994,8 @@ class DirectoryTree extends cr.ui.Tree {
    */
   onFocus_(e) {
     if (this.selectedItem && this.selectedItem.labelElement) {
-      // Grab the current location to check if it needs to move
       const element = this.selectedItem.labelElement;
-      const originalPos = element.getBoundingClientRect();
       element.scrollIntoViewIfNeeded();
-      const newPos = element.getBoundingClientRect();
-      if (newPos.x !== originalPos.x || newPos.y !== originalPos.y) {
-        element.scrollIntoView({inline: 'start'});
-      }
     }
   }
 
