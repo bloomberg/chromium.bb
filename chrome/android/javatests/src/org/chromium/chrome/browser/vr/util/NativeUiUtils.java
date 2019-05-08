ccbfcbea5f8c0c80825d1476ef7a93ed939bf4b9
Brian Sheedy <bsheedy@chromium.org>
2019-05-08 18:11:44 +0000

    Further decrease rate of VR content clicks
    Further decreases the rate at which clicks are sent to content nodes
    during instrumentation tests for the VR browser to ensure that all
    clicks are reliably picked up.
    
    Bug: 953990
    Change-Id: I6f903f14f2a6bb02fef7e7a76629fa5e038e2e55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1601414
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Klaus Weidner <klausw@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657764}
    
    Former-commit-id: d4075717a9fb7621c473283b5e09fbb6b1b34ea1

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr/util/NativeUiUtils.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr/util/NativeUiUtils.java
@@ -219,11 +219,13 @@ public class NativeUiUtils {
             for (int i = 0; i < numClicks; ++i) {
                 clickElement(UserFriendlyElementName.CONTENT_QUAD, clickCoordinates);
                 // Rarely, sending clicks back to back can cause the web contents to miss a click.
-                // So, if we're going to be sending more, introduce a couple of input-less frames
-                // to avoid this issue.
+                // So, if we're going to be sending more, introduce a few input-less frames to avoid
+                // this issue. 3 appears to currently be the magic number that lets the web contents
+                // reliably pick up all clicks.
                 if (i < numClicks - 1) {
-                    hoverElement(UserFriendlyElementName.CONTENT_QUAD, clickCoordinates);
-                    hoverElement(UserFriendlyElementName.CONTENT_QUAD, clickCoordinates);
+                    for (int j = 0; j < 3; ++j) {
+                        hoverElement(UserFriendlyElementName.CONTENT_QUAD, clickCoordinates);
+                    }
                 }
             }
         });
