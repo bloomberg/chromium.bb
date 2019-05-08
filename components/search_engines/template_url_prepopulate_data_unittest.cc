9ef7b650f638674382e0d63542d6579b0d1f3df1
Kyle Milka <kmilka@chromium.org>
2019-05-08 18:11:35 +0000

    Remove Google new_tab_url in prepopulated_engines.json
    When Google is the DSP we now use the local NTP built into Chrome
    instead of the remote NTP specified in prepopulated_engines.json.
    Remove the entry, as the loading of the local NTP is handled by
    logic in chrome/browser/search/search.cc.
    
    Bug: 583289
    Change-Id: Id28db805bc53eebab6d492be666f909e0ff0b800
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1591796
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#657763}
    
    Former-commit-id: 0b31015d18bd5a98e23739227484329beae04138

--- components/search_engines/template_url_prepopulate_data_unittest.cc
+++ components/search_engines/template_url_prepopulate_data_unittest.cc
@@ -243,7 +243,6 @@ TEST_F(TemplateURLPrepopulateDataTest, ClearProvidersFromPrefs) {
   EXPECT_EQ(ASCIIToUTF16("Google"), t_urls[default_index]->short_name());
   EXPECT_FALSE(t_urls[default_index]->suggestions_url.empty());
   EXPECT_FALSE(t_urls[default_index]->image_url.empty());
-  EXPECT_FALSE(t_urls[default_index]->new_tab_url.empty());
   EXPECT_FALSE(t_urls[default_index]->contextual_search_url.empty());
   EXPECT_FALSE(t_urls[default_index]->image_url_post_params.empty());
   EXPECT_EQ(SEARCH_ENGINE_GOOGLE,
@@ -278,7 +277,6 @@ TEST_F(TemplateURLPrepopulateDataTest, ProvidersFromPrepopulated) {
   EXPECT_EQ(ASCIIToUTF16("Google"), t_urls[default_index]->short_name());
   EXPECT_FALSE(t_urls[default_index]->suggestions_url.empty());
   EXPECT_FALSE(t_urls[default_index]->image_url.empty());
-  EXPECT_FALSE(t_urls[default_index]->new_tab_url.empty());
   EXPECT_FALSE(t_urls[default_index]->contextual_search_url.empty());
   EXPECT_FALSE(t_urls[default_index]->image_url_post_params.empty());
   // Expect at least 2 alternate_urls.
