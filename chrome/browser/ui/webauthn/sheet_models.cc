42d7208c4007f938f11f26478225dfdb969ba5ca
Adam Langley <agl@chromium.org>
2019-05-09 14:36:41 +0000

    chrome/browser/ui/webauthn: long domains may cause a line break.
    As requested by UX in [1], allow long host names to split a title into
    two lines. This allows us to show more of the name before eliding,
    although sufficiently long names will still trigger elision.
    
    Screenshot at
    https://drive.google.com/open?id=1_V6t2CeZDAVazy3Px-OET2LnB__aEW1r.
    
    [1] https://docs.google.com/presentation/d/1TtxkPUchyVZulqgdMcfui-68B0W-DWaFFVJEffGIbLA/edit#slide=id.g5913c4105f_1_12
    
    Change-Id: I70f6541e0db3e9942239304de43b487a7561ca34
    Bug: 870892
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1601812
    Auto-Submit: Adam Langley <agl@chromium.org>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658114}
    
    Former-commit-id: 2f19869af13bbfdcfd682a55c0d2c61c6e102475

--- chrome/browser/ui/webauthn/sheet_models.cc
+++ chrome/browser/ui/webauthn/sheet_models.cc
@@ -34,11 +34,7 @@ base::string16 GetRelyingPartyIdString(
   const auto& rp_id = dialog_model->relying_party_id();
   DCHECK(!rp_id.empty());
   GURL rp_id_url(kRpIdUrlPrefix + rp_id);
-  auto max_static_string_length = gfx::GetStringWidthF(
-      l10n_util::GetStringUTF16(IDS_WEBAUTHN_GENERIC_TITLE), gfx::FontList(),
-      gfx::Typesetter::DEFAULT);
-  return url_formatter::ElideHost(rp_id_url, gfx::FontList(),
-                                  kDialogWidth - max_static_string_length);
+  return url_formatter::ElideHost(rp_id_url, gfx::FontList(), kDialogWidth);
 }
 
 // Possibly returns a resident key warning if the model indicates that it's
