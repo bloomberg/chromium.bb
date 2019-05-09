318dc8bb868d5a41bf98aee8595ec2f210f5dc03
Devlin Cronin <rdevlin.cronin@chromium.org>
2019-05-09 20:37:56 +0000

    [Extensions Metrics] Stop recording Extensions.ExtensionCreationTime.*
    Stop recording Extensions.ExtensionCreationTime.* and mark it as
    obsolete in histograms.xml. We haven't been looking at it lately, so we
    shouldn't continue recording it. We can always add it back later if we
    need it again.
    
    Bug: 917738
    Change-Id: If3711f0e04de29fb5f4e00acdada2b97fbd37d8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1600854
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658253}
    
    Former-commit-id: f95a65c24e34bc50c86f6e5a740f5fc8f7f00c67

--- extensions/common/extension.cc
+++ extensions/common/extension.cc
@@ -17,7 +17,6 @@
 #include "base/json/json_writer.h"
 #include "base/logging.h"
 #include "base/memory/singleton.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/stl_util.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string16.h"
@@ -30,7 +29,6 @@
 #include "base/values.h"
 #include "base/version.h"
 #include "components/crx_file/id_util.h"
-#include "content/public/common/content_switches.h"
 #include "content/public/common/url_constants.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/error_utils.h"
@@ -192,27 +190,6 @@ scoped_refptr<Extension> Extension::Create(const base::FilePath& path,
   if (!extension->InitFromValue(flags, &error)) {
     *utf8_error = base::UTF16ToUTF8(error);
     return NULL;
-  }
-
-  const base::CommandLine& command_line =
-      *base::CommandLine::ForCurrentProcess();
-  std::string process_type =
-      command_line.GetSwitchValueASCII(::switches::kProcessType);
-  // Use microsecond accuracy for increased granularity. Max at 10 seconds.
-  base::TimeDelta elapsed_time = timer.Elapsed();
-  const int kMaxTimeInMicroseconds = 10000000;
-  if (process_type.empty()) {
-    UMA_HISTOGRAM_CUSTOM_COUNTS(
-        "Extensions.ExtensionCreationTime.BrowserProcess",
-        elapsed_time.InMicroseconds(), 1, kMaxTimeInMicroseconds, 100);
-  } else if (command_line.HasSwitch(switches::kExtensionProcess)) {
-    UMA_HISTOGRAM_CUSTOM_COUNTS(
-        "Extensions.ExtensionCreationTime.ExtensionProcess",
-        elapsed_time.InMicroseconds(), 1, kMaxTimeInMicroseconds, 100);
-  } else if (process_type == ::switches::kRendererProcess) {
-    UMA_HISTOGRAM_CUSTOM_COUNTS(
-        "Extensions.ExtensionCreationTime.RendererProcess",
-        elapsed_time.InMicroseconds(), 1, kMaxTimeInMicroseconds, 100);
   }
 
   return extension;
