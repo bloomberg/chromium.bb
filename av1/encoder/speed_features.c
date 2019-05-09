eeb8b20a732a5d016eb6605a9325ac6a85f9f928
kyslov <kyslov@google.com>
2019-05-09 14:06:13 -0700

    AV1 RT: Use QP-based CDEF for high speed RT
    This adds ~25% BDRate improvements with ~8% slowdown
    (on speed8 RTC set)
    
    Change-Id: Ia1aeab5775f18d68a16ed97b40fa44a168c2801c

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -613,6 +613,7 @@ static void set_rt_speed_features_framesize_independent(AV1_COMP *cpi,
     sf->use_nonrd_pick_mode = 1;
     sf->use_comp_ref_nonrd = 0;
     sf->inter_mode_rd_model_estimation = 2;
+    sf->cdef_pick_method = CDEF_PICK_FROM_Q;
   }
   if (speed >= 8) {
     sf->use_fast_nonrd_pick_mode = 1;
