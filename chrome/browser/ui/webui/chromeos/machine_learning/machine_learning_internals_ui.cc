aa9af74f8e836ddb125db5c7a35309d88d90d58a
alanlxl <alanlxl@chromium.org>
2019-05-10 11:07:08 +0000

    Add ML service internal page
    Add an internal page (chrome://machine-learning-internals) to debug ML Service models
    and test connectivity/performance.
    Currently it supports a built-in TEST model which add two floats.
    
    What is planned for this page:
    * Each built-in model can add a tab here, if it's worthwhile to help debugging of
      on-device issues.
    * Plan to add performance metrics (i.e. latency) of different operations.
    * Plan to add control operations like Disconnect/Connect from ML Service.
    
    Bug: 924738
    Change-Id: I23ff4ad93c532b7c1fc951a7ede1cb376b551f80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1411959
    Commit-Queue: Xinglong Luan <alanlxl@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#658513}
    
    Former-commit-id: 981e497baa9179085d6d16dc13f09ab45f24888b

--- chrome/browser/ui/webui/chromeos/machine_learning/machine_learning_internals_ui.cc
+++ chrome/browser/ui/webui/chromeos/machine_learning/machine_learning_internals_ui.cc
@@ -21,6 +21,30 @@ MachineLearningInternalsUI::MachineLearningInternalsUI(
   content::WebUIDataSource* const source = content::WebUIDataSource::Create(
       chrome::kChromeUIMachineLearningInternalsHost);
 
+  source->AddResourcePath(
+      "chromeos/services/machine_learning/public/mojom/tensor.mojom-lite.js",
+      IDR_MACHINE_LEARNING_INTERNALS_TENSOR_MOJO_JS);
+  source->AddResourcePath(
+      "chromeos/services/machine_learning/public/mojom/model.mojom-lite.js",
+      IDR_MACHINE_LEARNING_INTERNALS_MODEL_MOJO_JS);
+  source->AddResourcePath(
+      "chromeos/services/machine_learning/public/mojom/"
+      "graph_executor.mojom-lite.js",
+      IDR_MACHINE_LEARNING_INTERNALS_GRAPH_EXECUTOR_MOJO_JS);
+  source->AddResourcePath(
+      "chromeos/services/machine_learning/public/"
+      "mojom/machine_learning_service.mojom-lite.js",
+      IDR_MACHINE_LEARNING_INTERNALS_MACHINE_LEARNING_SERVICE_MOJO_JS);
+  source->AddResourcePath(
+      "chrome/browser/ui/webui/chromeos/machine_learning/"
+      "machine_learning_internals_page_handler.mojom-lite.js",
+      IDR_MACHINE_LEARNING_INTERNALS_PAGE_HANDLER_MOJO_JS);
+  source->AddResourcePath("machine_learning_internals.js",
+                          IDR_MACHINE_LEARNING_INTERNALS_JS);
+  source->AddResourcePath("machine_learning_internals_utils.js",
+                          IDR_MACHINE_LEARNING_INTERNALS_UTILS_JS);
+  source->AddResourcePath("test_model_tab.js",
+                          IDR_MACHINE_LEARNING_INTERNALS_TEST_MODEL_TAB_JS);
   source->SetDefaultResource(IDR_MACHINE_LEARNING_INTERNALS_HTML);
 
   content::WebUIDataSource::Add(Profile::FromWebUI(web_ui), source);
