686525938138a8f72381ecefda131c54acf28b8d
Michael Martis <martis@chromium.org>
2019-05-13 00:07:10 +0000

    A11y image labeling: disabled feature by default.
    We will roll out the feature incrementally in M75 to be sure that our servers
    can handle traffic from the entire user population. To do this, we disable the
    feature by default.
    
    This change will be merged into M75.
    
    (cherry picked from commit e14fdb9c9947b3896cdd598a9da073d33a08e294 [formerly 964a6c44e65e18c12d351bc6f25f12bda876c7e2])
    
    Change-Id: Ifb69da677686e6bd7613cec6bd23a32f66121ec5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1597898
    Reviewed-by: Charles . <charleszhao@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Michael Martis <martis@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#658011}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1607711
    Reviewed-by: Michael Martis <martis@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3770@{#547}
    Cr-Branched-From: 04e619e4964cac3f22e7ef787268f756c2b6c1c7 [formerly a9eee1c7c727ef42a15d86e9fa7b77ff0e63840a]-refs/heads/master@{#652427}
    
    Former-commit-id: b3dcc8db179b5e48a8036844cb64fe393eae8741

--- content/public/common/content_features.cc
+++ content/public/common/content_features.cc
@@ -140,7 +140,7 @@ const base::Feature kDesktopCaptureChangeSource{
 // When a screen reader is detected, allow users the option of letting
 // Google provide descriptions for unlabeled images.
 const base::Feature kExperimentalAccessibilityLabels{
-    "ExperimentalAccessibilityLabels", base::FEATURE_ENABLED_BY_DEFAULT};
+    "ExperimentalAccessibilityLabels", base::FEATURE_DISABLED_BY_DEFAULT};
 
 // Throttle tasks in Blink background timer queues based on CPU budgets
 // for the background tab. Bug: https://crbug.com/639852.
