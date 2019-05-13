057a126cf773de142e04bff065420d034413457f
Sinan Sahin <sinansahin@google.com>
2019-05-13 21:54:11 +0000

    Progress on the Revamped context menu
    This CL consists of:
    - Improvements for URL formatting (i18n and emphasizing)
    - Addition of grey background while loading
    - Addition of favicons, monograms, and thumbnails
    - Addition of checkered pattern behind the transparent PNGs
    - Some visual polish and fixes
    
    Bug: 655359
    Change-Id: I6ebf13b6097780db22c05804f2b9cdf6d1ee8461
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1595114
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/master@{#659233}
    
    Former-commit-id: 310bb105fad9bea01c17658062424b014d8a47d9

--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ContextMenuHelper.java
@@ -23,6 +23,8 @@ import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.task.PostTask;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeFeatureList;
+import org.chromium.chrome.browser.favicon.LargeIconBridge;
+import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.share.ShareHelper;
 import org.chromium.chrome.browser.share.ShareParams;
 import org.chromium.content_public.browser.UiThreadTaskTraits;
@@ -138,12 +140,13 @@ public class ContextMenuHelper implements OnCreateContextMenuListener {
                     mOnMenuShown, mOnMenuClosed);
             // TODO(sinansahin): This could be pushed in to the menuController.
             if (mCurrentContextMenuParams.isImage()) {
-                getThumbnail(menuController, new Callback<Bitmap>() {
-                    @Override
-                    public void onResult(Bitmap result) {
-                        menuController.onImageThumbnailRetrieved(result);
-                    }
-                });
+                getThumbnail(menuController, menuController::onImageThumbnailRetrieved);
+            } else {
+                LargeIconBridge iconBridge = new LargeIconBridge(Profile.getLastUsedProfile());
+                iconBridge.getLargeIconForUrl(mCurrentContextMenuParams.getUrl(),
+                        getActivity().getResources().getDimensionPixelSize(
+                                R.dimen.default_favicon_size),
+                        menuController::onFaviconAvailable);
             }
             return;
         }
