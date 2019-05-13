7259d6d779a6a23ff4dcc5703b971a7b17934b33
Alexander Timin <altimin@chromium.org>
2019-05-13 23:46:24 +0000

    Revert "[bfcache] Track permission requests for metrics"
    This reverts commit f16b4115fc129157ea4e386e5d3729f6267604ee [formerly ecde0931d6264033be78337e5ae95cd3b2026102].
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > [bfcache] Track permission requests for metrics
    > 
    > Record when page tries to request the following permissions:
    > - Geolocation
    > - MIDI
    > - Audio capture
    > - Video capture
    > - Sensors
    > - Background fetch or background sync
    > 
    > R=​​mlamouri@chromium.org
    > BUG=957632,933147
    > 
    > (cherry picked from commit 97fdb3f72a3a5a084a5b26915a6ca8ecb0ba6fe3 [formerly f417b362787887b4ff220b3b8dd412b47a4ec965])
    > 
    > Change-Id: If5ab85e45729870f1212a00ba0e2ec3becf01e29
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1601513
    > Auto-Submit: Alexander Timin <altimin@chromium.org>
    > Commit-Queue: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Cr-Original-Commit-Position: refs/heads/master@{#657967}
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1610759
    > Reviewed-by: Alexander Timin <altimin@chromium.org>
    > Cr-Commit-Position: refs/branch-heads/3770@{#578}
    > Cr-Branched-From: 04e619e4964cac3f22e7ef787268f756c2b6c1c7 [formerly a9eee1c7c727ef42a15d86e9fa7b77ff0e63840a]-refs/heads/master@{#652427}
    
    TBR=altimin@chromium.org
    
    Change-Id: Iff4acf8558e20ab2973cf11dc5f1c5f1f7f59cd0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 957632, 933147
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1610760
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3770@{#582}
    Cr-Branched-From: 04e619e4964cac3f22e7ef787268f756c2b6c1c7 [formerly a9eee1c7c727ef42a15d86e9fa7b77ff0e63840a]-refs/heads/master@{#652427}
    
    Former-commit-id: 1e2e15f94b08459f2417cd2636247bc409452c49

--- third_party/blink/public/common/scheduler/web_scheduler_tracked_feature.h
+++ third_party/blink/public/common/scheduler/web_scheduler_tracked_feature.h
@@ -44,20 +44,7 @@ enum class WebSchedulerTrackedFeature {
   // Recorded only for the main frame.
   kHasScriptableFramesInMultipleTabs = 18,
 
-  // Whether the page tried to request a permission regardless of the outcome.
-  // TODO(altimin): Track this more accurately depending on the data.
-  // See permission.mojom for more details.
-  kRequestedGeolocationPermission = 19,
-  kRequestedNotificationsPermission = 20,
-  kRequestedMIDIPermission = 21,
-  kRequestedAudioCapturePermission = 22,
-  kRequestedVideoCapturePermission = 23,
-  kRequestedSensorsPermission = 24,
-  // This covers all background-related permissions, including background sync,
-  // background fetch and others.
-  kRequestedBackgroundWorkPermission = 26,
-
-  kMaxValue = kRequestedBackgroundWorkPermission
+  kMaxValue = kHasScriptableFramesInMultipleTabs
 };
 
 }  // namespace scheduler
