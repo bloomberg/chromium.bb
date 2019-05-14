070af4b0374285260daef28697359e3c21ab9c71
Ian Vollick <vollick@chromium.org>
2019-05-14 19:54:46 +0000

    Reland "Plumb resource IDs for ActionNames"
    This is a reland of 89c38a61adf31c68dc5d723c933b448a5a5d8587 [formerly 05f301b301b51c588d9487a03027ab6dead08837]
    
    Original change's description:
    > Plumb resource IDs for ActionNames
    > 
    > These are easier to use than strings.
    > 
    > (cherry picked from commit d3f77619489278ee9a04fc4e38edcf54c5660477 [formerly e3dc0aafcc4e4ed1826319e5a8b6ca6bace6c2b0])
    > 
    > No-Presubmit: true
    > Bug: 961143
    > Change-Id: I00d9c83c7687a5e109bcc4def5fd7002908ad751
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1607320
    > Commit-Queue: Ian Vollick <vollick@chromium.org>
    > Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    > Reviewed-by: Matthew Jones <mdjones@chromium.org>
    > Cr-Original-Commit-Position: refs/heads/master@{#659171}
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1611506
    > Reviewed-by: Ian Vollick <vollick@chromium.org>
    > Cr-Commit-Position: refs/branch-heads/3770@{#590}
    > Cr-Branched-From: 04e619e4964cac3f22e7ef787268f756c2b6c1c7 [formerly a9eee1c7c727ef42a15d86e9fa7b77ff0e63840a]-refs/heads/master@{#652427}
    
    Bug: 961143
    Change-Id: Ia224a977c2e68ad404f8a61f8fe1e37eab07d910
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1611860
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3770@{#608}
    Cr-Branched-From: 04e619e4964cac3f22e7ef787268f756c2b6c1c7 [formerly a9eee1c7c727ef42a15d86e9fa7b77ff0e63840a]-refs/heads/master@{#652427}
    
    Former-commit-id: 655ad2f77f2bb1735b23c139e62c9938f56e8b45

--- chrome/android/java/src/org/chromium/chrome/browser/infobar/InfoBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/infobar/InfoBar.java
@@ -135,9 +135,9 @@ public abstract class InfoBar implements InfoBarView {
         }
 
         ActionNames names = new ActionNames();
-        names.cancel = mContext.getResources().getString(R.string.cancel);
-        names.select = mContext.getResources().getString(R.string.select);
-        names.alt = "";
+        names.cancel = R.string.cancel;
+        names.select = R.string.select;
+        names.alt = 0;
         PropertyModel model =
                 new PropertyModel.Builder(TouchlessDialogProperties.ALL_DIALOG_KEYS)
                         .with(TouchlessDialogProperties.IS_FULLSCREEN, false)
