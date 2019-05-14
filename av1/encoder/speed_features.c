4b3bb4a173ceb81e1ea06dee46a8e17790e1f193
Aniket Dhok <aniket.dhok@ittiam.com>
2019-05-14 18:47:51 +0530

    Disable prune_single_motion_modes_by_simple_trans
       Quality Improvement(AWCY)
    Preset  Overall  Encode time reduction
       1     0.12%    0.34%
       2     0.01%    1.80%
       3     0.05%    1.50%
       4     0.07%    2.13%
    Encode time is averaged across multiple test cases
    
    STATS_CHANGED
    
    Change-Id: I64eb6d97ae819d803ca9ceb50cc7ab8ea0ac41e3

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -254,11 +254,6 @@ static void set_good_speed_features_framesize_independent(
     sf->prune_comp_search_by_single_result = 1;
     sf->skip_repeated_newmv = 1;
     sf->obmc_full_pixel_search_level = 1;
-    // TODO(jianj): Following speed feature will be further explored to
-    // identify the appropriate tradeoff between encoder performance and its
-    // speed.
-    sf->prune_single_motion_modes_by_simple_trans = 1;
-
     sf->simple_motion_search_split = 1;
     sf->simple_motion_search_early_term_none = 1;
 
@@ -778,6 +773,7 @@ void av1_set_speed_features_framesize_independent(AV1_COMP *cpi, int speed) {
   sf->use_hash_based_trellis = 0;
   sf->prune_comp_search_by_single_result = 0;
   sf->skip_repeated_newmv = 0;
+  // TODO(any) Cleanup this speed feature
   sf->prune_single_motion_modes_by_simple_trans = 0;
 
   // Set decoder side speed feature to use less dual sgr modes
