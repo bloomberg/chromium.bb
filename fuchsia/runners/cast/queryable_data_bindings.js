345e1e4bb83304481711840c061d05fb04c4dbf5
Kevin Marshall <kmarshall@chromium.org>
2019-05-15 22:38:52 +0000

    [Fuchsia] Allow Cast Runner's APIs to be superceded by ApiBindings.
    Skips API definitions if they were previously defined in the page's
    scripting context.
    
    Enables an orderly transition from Cast Runner defined APIs to Agent-
    defined APIs, by deliberately injecting the Runner's APIs *after* the
    Agent's APIs are already in place, and yielding to the Agent's APIs
    in the event of collisions.
    
    Bug: 953958
    Change-Id: I14bf8f5afc48b2af319b2d24339aad48c738dae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1610649
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#660180}
    
    Former-commit-id: b1c229b14de6f755dc3d20605e7af1e43205047f

--- fuchsia/runners/cast/queryable_data_bindings.js
+++ fuchsia/runners/cast/queryable_data_bindings.js
@@ -8,23 +8,26 @@ if (!cast)
 if (!cast.__platform__)
   cast.__platform__ = new Object;
 
-cast.__platform__.__queryPlatformValueStore__ = new class {
-  mergeValues(values) {
-    for (var key in values)
-      this.values_[key] = values[key];
-  }
-
-  getValue(key) {
-    if (!this.values_.hasOwnProperty(key)) {
-      console.error('Unknown platformValue: ' + key);
-      return null;
+// Don't clobber the QueryableData API if it was previously injected.
+if (!cast.__platform__.queryPlatformValue) {
+  cast.__platform__.__queryPlatformValueStore__ = new class {
+    mergeValues(values) {
+      for (var key in values)
+        this.values_[key] = values[key];
     }
-    return this.values_[key];
-  }
 
-  values_ = {};
-};
+    getValue(key) {
+      if (!this.values_.hasOwnProperty(key)) {
+        console.error('Unknown platformValue: ' + key);
+        return null;
+      }
+      return this.values_[key];
+    }
 
-cast.__platform__.queryPlatformValue =
-    cast.__platform__.__queryPlatformValueStore__.getValue.bind(
-        cast.__platform__.__queryPlatformValueStore__);
+    values_ = {};
+  };
+
+  cast.__platform__.queryPlatformValue =
+      cast.__platform__.__queryPlatformValueStore__.getValue.bind(
+          cast.__platform__.__queryPlatformValueStore__);
+}
