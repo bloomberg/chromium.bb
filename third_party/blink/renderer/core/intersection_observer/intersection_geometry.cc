43601552c5c5ca5efc67ae9d0bfefdb8d698adda
Stefan Zager <szager@chromium.org>
2019-05-15 05:27:54 +0000

    [IntersectionObserver] Fix target rect for block inside inline
    As the spec requires, use the same logic as
    Element::getBoundingClientRect for the target rect.
    
    BUG=962605
    R=eae@chromium.org
    
    Change-Id: Ib5aaec30d9de79901d08f8fe3ce0a3998468839f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1612032
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#659836}
    
    Former-commit-id: bac4d6105c26197e47cd9fbf587e399c20c60177

--- third_party/blink/renderer/core/intersection_observer/intersection_geometry.cc
+++ third_party/blink/renderer/core/intersection_observer/intersection_geometry.cc
@@ -209,8 +209,14 @@ LayoutRect IntersectionGeometry::InitializeTargetRect(LayoutObject* target) {
   }
   if (target->IsBox())
     return LayoutRect(ToLayoutBoxModelObject(target)->BorderBoundingBox());
-  if (target->IsLayoutInline())
-    return ToLayoutInline(target)->PhysicalLinesBoundingBox();
+  if (target->IsLayoutInline()) {
+    return EnclosingLayoutRect(
+        target
+            ->AncestorToLocalQuad(
+                nullptr, FloatQuad(target->AbsoluteBoundingBoxFloatRect()),
+                kUseTransforms | kApplyContainerFlip)
+            .BoundingBox());
+  }
   return ToLayoutText(target)->PhysicalLinesBoundingBox();
 }
 
