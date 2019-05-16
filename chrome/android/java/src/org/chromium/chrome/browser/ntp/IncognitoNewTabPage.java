d8242fa5f2894f58683034a4cf018d320085fc89
Sinan Sahin <sinansahin@google.com>
2019-05-16 21:43:57 +0000

    [Dark theme] Work around default focus highlight bug on incognito NTP
    Also see https://crbug.com/943873.
    
    Bug: 963385
    Change-Id: Ie1849a0252771113d7cc89c13fc9a31177e7c020
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1615703
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/master@{#660565}
    
    Former-commit-id: d542d69b03db3c2daa298dbc2f01b3de084d07f6

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/IncognitoNewTabPage.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/IncognitoNewTabPage.java
@@ -6,6 +6,7 @@ package org.chromium.chrome.browser.ntp;
 
 import android.app.Activity;
 import android.graphics.Canvas;
+import android.os.Build;
 import android.support.v4.view.ViewCompat;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -56,6 +57,12 @@ public class IncognitoNewTabPage
 
         mIncognitoNTPBackgroundColor =
                 ApiCompatibilityUtils.getColor(activity.getResources(), R.color.ntp_bg_incognito);
+
+        // Work around https://crbug.com/943873 and https://crbug.com/963385 where default focus
+        // highlight shows up after toggling dark mode.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            getView().setDefaultFocusHighlightEnabled(false);
+        }
     }
 
     @Override
