b6f279c4a59e5fc7f4f5d8f96d1cadfee16afcac
Neil Birkbeck <birkbeck@google.com>
2019-05-16 17:16:35 -0700

    Set bit_depth on grain parameters in examples/noise_model.c
    When the bit_depth is unset, no noise will be added, limiting
    the debug info available.
    
    Change-Id: Ifbd6cb8c97a5435c1d46393f20d4d95ad2124460

--- examples/noise_model.c
+++ examples/noise_model.c
@@ -180,6 +180,7 @@ static void print_variance_y(FILE *debug_file, aom_image_t *raw,
   aom_image_t renoised;
   grain->apply_grain = 1;
   grain->random_seed = 7391;
+  grain->bit_depth = raw->bit_depth;
   aom_img_alloc(&renoised, raw->fmt, raw->w, raw->h, 1);
 
   if (av1_add_film_grain(grain, denoised, &renoised)) {
