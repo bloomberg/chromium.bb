e42eff57c0d507db4e1d81ca33b751847afbf9f9
Rayan Kanso <rayankans@chromium.org>
2019-05-20 18:31:41 +0000

    Reland "[DevTools] Enable PushMessaging & Notifications in handler."
    This is a reland of ceaa42ad2cc20f5ba822b56d19d2fad8aa76b6db [formerly 972aadda46536c2d26eda8c8a6d1098ce4bf961f]
    
    Original change's description:
    > [DevTools] Enable PushMessaging & Notifications in handler.
    >
    > Allows the Background Services handler to receive push & notification
    > calls from the frontend.
    >
    > Bug: 927726
    > Change-Id: Iaaca0205890f940d187f3b6e145e57fb1d3fdcb2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1609705
    > Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    > Reviewed-by: Alexei Filippov <alph@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#660806}
    
    TBR=alph@chromium.org
    
    Bug: 927726
    Change-Id: I6f4a0f43e4fc1299d5c6f7b2c7ee1c94ac837c06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1619702
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661418}
    
    Former-commit-id: a77a0638c628981035f20ad775be4de06d3e248d

--- content/browser/devtools/devtools_background_services_context_impl.cc
+++ content/browser/devtools/devtools_background_services_context_impl.cc
@@ -60,8 +60,7 @@ DevToolsBackgroundServicesContextImpl::DevToolsBackgroundServicesContextImpl(
     BrowserContext* browser_context,
     scoped_refptr<ServiceWorkerContextWrapper> service_worker_context)
     : browser_context_(browser_context),
-      service_worker_context_(std::move(service_worker_context)),
-      weak_ptr_factory_(this) {
+      service_worker_context_(std::move(service_worker_context)) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
   auto expiration_times =
@@ -148,7 +147,7 @@ void DevToolsBackgroundServicesContextImpl::GetLoggedBackgroundServiceEvents(
       FROM_HERE, {BrowserThread::IO},
       base::BindOnce(&DevToolsBackgroundServicesContextImpl::
                          GetLoggedBackgroundServiceEventsOnIO,
-                     weak_ptr_factory_.GetWeakPtr(), service,
+                     weak_ptr_factory_io_.GetWeakPtr(), service,
                      std::move(callback)));
 }
 
@@ -161,7 +160,7 @@ void DevToolsBackgroundServicesContextImpl::
   service_worker_context_->GetUserDataForAllRegistrationsByKeyPrefix(
       CreateEntryKeyPrefix(service),
       base::BindOnce(&DevToolsBackgroundServicesContextImpl::DidGetUserData,
-                     weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
+                     weak_ptr_factory_io_.GetWeakPtr(), std::move(callback)));
 }
 
 void DevToolsBackgroundServicesContextImpl::DidGetUserData(
@@ -209,7 +208,7 @@ void DevToolsBackgroundServicesContextImpl::ClearLoggedBackgroundServiceEvents(
       FROM_HERE, {BrowserThread::IO},
       base::BindOnce(&DevToolsBackgroundServicesContextImpl::
                          ClearLoggedBackgroundServiceEventsOnIO,
-                     weak_ptr_factory_.GetWeakPtr(), service));
+                     weak_ptr_factory_io_.GetWeakPtr(), service));
 }
 
 void DevToolsBackgroundServicesContextImpl::
@@ -234,7 +233,7 @@ void DevToolsBackgroundServicesContextImpl::LogBackgroundServiceEvent(
       FROM_HERE, {BrowserThread::IO},
       base::BindOnce(
           &DevToolsBackgroundServicesContextImpl::LogBackgroundServiceEventOnIO,
-          weak_ptr_factory_.GetWeakPtr(), service_worker_registration_id,
+          weak_ptr_factory_io_.GetWeakPtr(), service_worker_registration_id,
           origin, service, event_name, instance_id, event_metadata));
 }
 
@@ -257,7 +256,7 @@ void DevToolsBackgroundServicesContextImpl::LogBackgroundServiceEventOnIO(
         FROM_HERE, {BrowserThread::UI},
         base::BindOnce(
             &DevToolsBackgroundServicesContextImpl::OnRecordingTimeExpired,
-            weak_ptr_factory_.GetWeakPtr(), ServiceToProtoEnum(service)));
+            weak_ptr_factory_ui_.GetWeakPtr(), ServiceToProtoEnum(service)));
     return;
   }
 
@@ -281,7 +280,7 @@ void DevToolsBackgroundServicesContextImpl::LogBackgroundServiceEventOnIO(
       FROM_HERE, {BrowserThread::UI},
       base::BindOnce(
           &DevToolsBackgroundServicesContextImpl::NotifyEventObservers,
-          weak_ptr_factory_.GetWeakPtr(), std::move(event)));
+          weak_ptr_factory_ui_.GetWeakPtr(), std::move(event)));
 }
 
 void DevToolsBackgroundServicesContextImpl::NotifyEventObservers(
