e5916afad0a1bb1b17a2b6ab023303c77eb5fdd8
Boris Sazonov <bsazonov@chromium.org>
2019-05-20 18:36:06 +0000

    [Android] Disable flaky ProfilingProcessHostAndroidTest#testModeBrowser on L+
    TBR=alph@chromium.org
    
    Bug: 964502
    Change-Id: I48aad3a7a48b58a0d6ff94b4f9982e3f7714882d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1619947
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661422}
    
    Former-commit-id: fc05e3efb986c53efa3a58606a436f74a0854446

--- chrome/android/javatests/src/org/chromium/chrome/browser/profiling_host/ProfilingProcessHostAndroidTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/profiling_host/ProfilingProcessHostAndroidTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.ChromeSwitches;
@@ -38,6 +39,7 @@ public class ProfilingProcessHostAndroidTest {
 
     @Test
     @MediumTest
+    @DisableIf.Build(sdk_is_greater_than = 20, message = "https://crbug.com/964502")
     @CommandLineFlags.Add({"memlog=browser", "memlog-stack-mode=native-include-thread-names",
             "memlog-sampling-rate=1"})
     public void
