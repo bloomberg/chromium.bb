7f2d68248157c7571fff5c5a30eee8d8eec120d7
David Benjamin <davidben@chromium.org>
2019-05-20 16:57:22 +0000

    Remove stale SandboxIPCHandler::HandleLocaltime suppression.
    As of https://chromium-review.googlesource.com/c/chromium/src/+/882281/,
    that method no longer exists.
    
    Bug: 797998
    Change-Id: Iaeadf89b1c696aeec0e7792ee08c406e867a6031
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1618302
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661370}
    
    Former-commit-id: 73ecf0ffb3372ebc7330c4b1eced6dadb4d65768

--- build/sanitizers/tsan_suppressions.cc
+++ build/sanitizers/tsan_suppressions.cc
@@ -197,9 +197,6 @@ char kTSanDefaultSuppressions[] =
     // http://crbug.com/795110
     "race:third_party/fontconfig/*\n"
 
-    // http://crbug.com/797998
-    "race:content::SandboxIPCHandler::HandleLocaltime\n"
-
     // http://crbug.com/927330
     "race:net::(anonymous namespace)::g_network_change_notifier\n"
 
