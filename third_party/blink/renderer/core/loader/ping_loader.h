72e10cd414fa6232f6eb7bfea7da331c20adbb60
Karan Bhatia <karandeepb@chromium.org>
2019-05-21 17:36:39 +0000

    IsolatedWorldCSP: Enforce CSP checks for sendBeacon in isolated worlds.
    Enforce CSP checks for sendBeacon in isolated worlds. Also add tests for
    sendBeacon usage in isolated worlds and its interaction with CSP.
    
    Since the IsolatedWorldCSP feature is disabled by default, this CL will cause
    the CSP check to use an empty CSP instead of just bypassing the CSP checks for
    isolated worlds. In practice, this should cause no behavior change.
    
    BUG=896041
    
    Change-Id: Ica24d22d9384c7b50c9977f73c05a4893b0735d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1617203
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661795}
    
    Former-commit-id: 927ae69dc055b0351624e5f2ff8e569a8136166b

--- third_party/blink/renderer/core/loader/ping_loader.h
+++ third_party/blink/renderer/core/loader/ping_loader.h
@@ -54,7 +54,7 @@ class KURL;
 // any response. The request is made independent of any LocalFrame staying
 // alive, and must only stay alive until the transmission has completed
 // successfully (or not -- errors are not propagated back either.) Upon
-// transmission, the the load is cancelled and the loader cancels itself.
+// transmission, the load is cancelled and the loader cancels itself.
 //
 // The ping loader is used by audit pings, beacon transmissions and image loads
 // during page unloading.
@@ -78,6 +78,9 @@ class CORE_EXPORT PingLoader {
   // The last argument is guaranteed to be set to the size of payload if
   // these method return true. If these method returns false, the value
   // shouldn't be used.
+  // Note: To ensure the correct Javascript world is used for CSP checks, these
+  // should be called synchronously from the point navigator.sendBeacon is
+  // called.
   static bool SendBeacon(LocalFrame*, const KURL&, const String&);
   static bool SendBeacon(LocalFrame*, const KURL&, DOMArrayBufferView*);
   static bool SendBeacon(LocalFrame*, const KURL&, Blob*);
