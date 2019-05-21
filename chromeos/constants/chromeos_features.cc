756680a4ebb9b8374ff8a829b995b7605aa834b5
Noel Gordon <noel@chromium.org>
2019-05-21 12:27:26 +0000

    [piexwasm] FilesApp: enable piex wasm raw image previews
    Enable the piex wasm feature flag by default: to use use it to extract
    raw image preview images instead of peix nacl.
    
    Bug: 904630
    Change-Id: I26fccc36f07fa04d044a9b1dc7beb872d1dc5df9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1616836
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Stuart Langley <slangley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661697}
    
    Former-commit-id: e28b09219579dbb6a8223e1a8dbf007942b728eb

--- chromeos/constants/chromeos_features.cc
+++ chromeos/constants/chromeos_features.cc
@@ -51,9 +51,9 @@ const base::Feature kDriveFs{"DriveFS", base::FEATURE_ENABLED_BY_DEFAULT};
 const base::Feature kEnableFileManagerFeedbackPanel{
     "EnableFeedbackPanel", base::FEATURE_DISABLED_BY_DEFAULT};
 
-// Enable the piex-wasm module for raw image extraction. Offical builds only.
+// Enable the piex-wasm module for raw image preview image extraction.
 const base::Feature kEnableFileManagerPiexWasm{
-    "PiexWasm", base::FEATURE_DISABLED_BY_DEFAULT};
+    "PiexWasm", base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Enables or disables web push for background notifications in
 // Android Messages Integration on Chrome OS.
