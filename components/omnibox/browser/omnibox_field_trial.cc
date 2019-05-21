2c8a865967ee305fd5159571903084591cc39bea
manuk <manukh@chromium.org>
2019-05-21 16:07:58 +0000

    [omnibox]: Demote document suggestions from the fakebox.
    Unless finch configs specify demotion factors, by default, omnibox
    navSuggests for inputs initiated from the fakebox are demoted by a
    factor of .61. This includes history and bookmark suggestions.
    
    With this CL, document suggestions are also demoted for fakebox inputs.
    
    Bug: 965124
    Change-Id: I5f6a8aa635c7bd8071e5663ef6fceaed77b6c1e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1620685
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#661750}
    
    Former-commit-id: 943110894837e38a1d57358203cfb72302fa02e0

--- components/omnibox/browser/omnibox_field_trial.cc
+++ components/omnibox/browser/omnibox_field_trial.cc
@@ -314,7 +314,7 @@ void OmniboxFieldTrial::GetDemotionsByType(
     // enough to be inline autocompleted (1400+), even after demotion it will
     // score above 850 ( 1400 * 0.61 > 850).  850 is the maximum score for
     // queries when the input has been detected as URL-seeking.
-    constexpr char kDemoteURLs[] = "1:61,2:61,3:61,4:61,16:61";
+    constexpr char kDemoteURLs[] = "1:61,2:61,3:61,4:61,16:61,24:61";
 #if defined(OS_ANDROID)
     if (current_page_classification == OmniboxEventProto::
         SEARCH_RESULT_PAGE_NO_SEARCH_TERM_REPLACEMENT)
