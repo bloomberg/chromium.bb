887ac2075132c84d824b9afb199705900ed12965
Rouslan Solomakhin <rouslan@chromium.org>
2019-05-22 15:19:40 +0000

    [Payment Handler] Enable skip UI on localhost.
    Before this patch, developing on http://localhost would not skip the
    browser payment sheet, even though all requirements were met: single
    URL-based payment method was requested without any additional data from
    autofill, the method had a default payment app, and show() was called
    with a user gesture. If the hostname was changed to
    https://not-localhost.com, then skip UI would trigger.
    
    This patch adds an exception for http://localhost to be classified as a
    valid URL-based payment method for the purposes of the skip UI check.
    
    After this patch, developers on http://localhost have the ability to
    skip the browser payment sheet.
    
    Bug: 965141
    Change-Id: Idd639ceabf093ab8a82ccf52dd278bf142d42630
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1620511
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662191}
    
    Former-commit-id: 4f3306719f91f49ddfdda767891578da40af0ac4

--- components/payments/core/payment_request_data_util.cc
+++ components/payments/core/payment_request_data_util.cc
@@ -19,6 +19,7 @@
 #include "components/payments/core/basic_card_response.h"
 #include "components/payments/core/payment_method_data.h"
 #include "components/payments/core/payments_validators.h"
+#include "net/base/url_util.h"
 #include "url/url_constants.h"
 
 namespace payments {
@@ -147,22 +148,24 @@ void ParseSupportedMethods(
           }
         }
       }
-      } else {
-        // Here |method_data_entry.supported_method| could be a deprecated
-        // supported network (e.g., "visa"), some invalid string or a URL
-        // Payment Method Identifier. Capture this last category if the URL
-        // is valid. A valid URL must have an https scheme and its username and
-        // password must be empty:
-        // https://www.w3.org/TR/payment-method-id/#dfn-validate-a-url-based-payment-method-identifier
-        // Avoid duplicate URLs.
-        GURL url(method_data_entry.supported_method);
-        if (url.is_valid() && url.SchemeIs(url::kHttpsScheme) &&
-            !url.has_username() && !url.has_password()) {
-          const auto result = url_payment_method_identifiers.insert(url);
-          if (result.second)
-            out_url_payment_method_identifiers->push_back(url);
-        }
+    } else {
+      // Here |method_data_entry.supported_method| could be a deprecated
+      // supported network (e.g., "visa"), some invalid string or a URL
+      // Payment Method Identifier. Capture this last category if the URL
+      // is valid. A valid URL must have an https scheme (or http for localhost)
+      // and its username and password must be empty:
+      // https://www.w3.org/TR/payment-method-id/#dfn-validate-a-url-based-payment-method-identifier
+      // Avoid duplicate URLs.
+      GURL url(method_data_entry.supported_method);
+      if (url.is_valid() &&
+          (url.SchemeIs(url::kHttpsScheme) ||
+           (url.SchemeIsHTTPOrHTTPS() && net::IsLocalhost(url))) &&
+          !url.has_username() && !url.has_password()) {
+        const auto result = url_payment_method_identifiers.insert(url);
+        if (result.second)
+          out_url_payment_method_identifiers->push_back(url);
       }
+    }
   }
 }
 
