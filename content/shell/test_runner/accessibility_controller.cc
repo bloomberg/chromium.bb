bf926b76e073c0a4fa94e0b8b6e689aa0345e693
Ian Vollick <vollick@chromium.org>
2019-05-22 21:02:44 +0000

    Focusless snav should still move focus in accessibily mode
    Since the existing machinery is tied to the notion of focus,
    we need to move focus even when performing "focusless" spatial
    navigation. The rest of the "focusless" idioms apply, however.
    
    Layout tests are by default in accessibility mode, so I've
    had to provide a means to disable this for my tests and add
    extra tests to ensure that snav behavior is what we expect
    both in and out of accessibility mode.
    
    (cherry picked from commit 6a9479a690a094ffe14449bf23f260535f9efdc6 [formerly 507b17561373680a42acee706d02ade747336422])
    
    Bug: 941572
    Change-Id: I82890eb27e35c902c27632badf65c1a09ca0667d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1620345
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#662045}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626307
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3770@{#788}
    Cr-Branched-From: 04e619e4964cac3f22e7ef787268f756c2b6c1c7 [formerly a9eee1c7c727ef42a15d86e9fa7b77ff0e63840a]-refs/heads/master@{#652427}
    
    Former-commit-id: a441670ce429f80119e7bbc865f0330f94ed10c3

--- content/shell/test_runner/accessibility_controller.cc
+++ content/shell/test_runner/accessibility_controller.cc
@@ -45,6 +45,7 @@ class AccessibilityControllerBindings
   v8::Local<v8::Object> FocusedElement();
   v8::Local<v8::Object> RootElement();
   v8::Local<v8::Object> AccessibleElementById(const std::string& id);
+  void Reset();
 
   base::WeakPtr<AccessibilityController> controller_;
 
@@ -103,7 +104,8 @@ AccessibilityControllerBindings::GetObjectTemplateBuilder(
       .SetMethod("addNotificationListener",
                  &AccessibilityControllerBindings::SetNotificationListener)
       .SetMethod("removeNotificationListener",
-                 &AccessibilityControllerBindings::UnsetNotificationListener);
+                 &AccessibilityControllerBindings::UnsetNotificationListener)
+      .SetMethod("reset", &AccessibilityControllerBindings::Reset);
 }
 
 void AccessibilityControllerBindings::LogAccessibilityEvents() {
@@ -136,6 +138,11 @@ v8::Local<v8::Object> AccessibilityControllerBindings::AccessibleElementById(
                      : v8::Local<v8::Object>();
 }
 
+void AccessibilityControllerBindings::Reset() {
+  if (controller_)
+    controller_->Reset();
+}
+
 AccessibilityController::AccessibilityController(
     WebViewTestProxy* web_view_test_proxy)
     : log_accessibility_events_(false),
@@ -148,6 +155,7 @@ void AccessibilityController::Reset() {
   elements_.Clear();
   notification_callback_.Reset();
   log_accessibility_events_ = false;
+  ax_context_.reset();
 }
 
 void AccessibilityController::Install(blink::WebLocalFrame* frame) {
