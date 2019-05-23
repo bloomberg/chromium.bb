ab485cc11b6783c4d2f46817aee6ddbd581b8f28
Aran Gilman <gilmanmh@google.com>
2019-05-23 13:39:29 +0000

    Add missing copyright notice to DarkModeFilter files.
    Change-Id: Iee9c633bc0b0407cfff511f43735ea5b99e25d41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626394
    Auto-Submit: Aran Gilman <gilmanmh@google.com>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662604}
    
    Former-commit-id: c334eb0c908be57d0494fafe7b02f43e1d8763cc

--- third_party/blink/renderer/platform/graphics/dark_mode_filter.h
+++ third_party/blink/renderer/platform/graphics/dark_mode_filter.h
@@ -1,3 +1,7 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_DARK_MODE_FILTER_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_DARK_MODE_FILTER_H_
 
