d16e99874bff531d9a2e7f41d03254c3e1e0ac73
Azeem Arshad <azeemarshad@chromium.org>
2019-05-23 00:50:52 +0000

    [CrOS Cellular] Add Basic navigation buttons for Cellular Setup.
    This CL adds a button bar element containing navigation buttons that
    drive the cellular activation flow.
    See go/cros-cellular-activation-ui-design for UI design.
    
    Bug: 962702
    Change-Id: Iebbfddd1a6bb353b6f07f359923c3083a76e9f40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1611077
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662456}
    
    Former-commit-id: df551f09115160073aa8e7b4a0c472a98a31f5f2

--- chrome/browser/ui/webui/chromeos/cellular_setup/cellular_setup_localized_strings_provider.cc
+++ chrome/browser/ui/webui/chromeos/cellular_setup/cellular_setup_localized_strings_provider.cc
@@ -6,6 +6,7 @@
 
 #include "base/stl_util.h"
 #include "chrome/browser/ui/webui/localized_string.h"
+#include "chrome/grit/generated_resources.h"
 #include "components/login/localized_values_builder.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/web_ui_data_source.h"
@@ -16,10 +17,11 @@ namespace cellular_setup {
 
 namespace {
 
-// TODO(azeemarshad): Add localized strings for cellular setup flow.
 constexpr LocalizedString kLocalizedStringsWithoutPlaceholders[] = {
     {"cancel", IDS_CANCEL},
-};
+    {"back", IDS_CELLULAR_SETUP_BACK_LABEL},
+    {"finish", IDS_CELLULAR_SETUP_FINISH_LABEL},
+    {"tryAgain", IDS_CELLULAR_SETUP_TRY_AGAIN_LABEL}};
 
 }  //  namespace
 
--- ui/webui/resources/cr_components/chromeos/cellular_setup/cellular_setup.js
+++ ui/webui/resources/cr_components/chromeos/cellular_setup/cellular_setup.js
@@ -2,11 +2,21 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO(azeemarshad): Implement cellular setup UI.
+/**
+ * Root element for the cellular setup flow. This element interacts with the
+ * CellularSetup service to carry out the activation flow. It contains
+ * navigation buttons and sub-pages corresponding to each step of the flow.
+ */
 Polymer({
   is: 'cellular-setup',
 
   behaviors: [I18nBehavior],
+
+  listeners: {
+    'backward-nav-requested': 'onBackwardNavRequested_',
+    'retry-requested': 'onRetryRequested_',
+    'complete-flow-requested': 'onCompleteFlowRequested_',
+  },
 
   /**
    * Provides an interface to the CellularSetup Mojo service.
@@ -18,5 +28,20 @@ Polymer({
   created: function() {
     this.mojoInterfaceProvider_ =
         cellular_setup.MojoInterfaceProviderImpl.getInstance();
+  },
+
+  /** @private */
+  onBackwardNavRequested_: function() {
+    // TODO(azeemarshad): Add back navigation.
+  },
+
+  /** @private */
+  onRetryRequested_: function() {
+    // TODO(azeemarshad): Add try again logic.
+  },
+
+  /** @private */
+  onCompleteFlowRequested__: function() {
+    // TODO(azeemarshad): Add completion logic.
   },
 });
