7ba784ef20c992ef3bc771c0ead7774b39bd5875
Etienne Bergeron <etienneb@chromium.org>
2019-05-23 23:29:21 +0000

    Implement the GetFontList using DWrite instead of GDI API.
    This CL is modifying the font-settings API on Windows.
    
    The goal of this CL is to remove the
    gfx::PlatformFontWin::GetLocalizedFontName(...) calls since
    the gfx::PlatformFontWin will be replaced by
    gfx::PlatformFontSkia.
    
    The GDI API was listing the system fonts by using the system
    locale which means the font-name was the localized name.
    The DWrite API allows the caller to specify the locale and is
    not forced to be the system one.
    
    The font-settings API is shipping a list of pair of string to
    extensions when the GetList is called
    (e.g fontId and displayName).
    
    The fontId is used as an identifier for other font-settings API.
    The DisplayName is the name that is displayed to the user.
    
    On windows, both string were used to be the Localized name since
    the GDI API was returning the localized name. A extra conversion
    was needed when an other windows API returns a non-localized name
    to be sure it will be the same FontID.
    
    By enforcing fontId to be non-localized, these extra conversion
    are now useless. And only the displayName is localized.
    
    Bug: 944227
    
    Change-Id: Iab1bff832504fbf9abc8860ece31357148c01cee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1606661
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662886}
    
    Former-commit-id: 326e11b2bedfdbb2f92766c01bfca34d3661aad7

--- ui/gfx/win/direct_write.h
+++ ui/gfx/win/direct_write.h
@@ -7,6 +7,8 @@
 
 #include <dwrite.h>
 
+#include "base/optional.h"
+#include "base/strings/string_piece.h"
 #include "ui/gfx/gfx_export.h"
 
 namespace gfx {
@@ -19,6 +21,18 @@ GFX_EXPORT void CreateDWriteFactory(IDWriteFactory** factory);
 
 // Returns the global DirectWrite factory.
 GFX_EXPORT IDWriteFactory* GetDirectWriteFactory();
+
+// Retrieves the localized string for a given locale. If locale is empty,
+// retrieves the first element of |names|.
+GFX_EXPORT base::Optional<std::string> RetrieveLocalizedString(
+    IDWriteLocalizedStrings* names,
+    const std::string& locale);
+
+// Retrieves the localized font name for a given locale. If locale is empty,
+// retrieves the default native font name.
+GFX_EXPORT base::Optional<std::string> RetrieveLocalizedFontName(
+    base::StringPiece font_name,
+    const std::string& locale);
 
 }  // namespace win
 }  // namespace gfx
