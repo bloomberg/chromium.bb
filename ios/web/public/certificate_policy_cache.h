74e66d263401724222ca52640969596d1d308f1b
Rohit Rao <rohitrao@chromium.org>
2019-05-24 17:21:21 +0000

    Revert "Create //ios/web/public/security/BUILD.gn."
    This reverts commit bfb99a713bad40d998c98d80a0d3100232e92505 [formerly daad88b5393ed668072c9a8efe6ff299e614995f].
    
    Reason for revert: Breaks internal compile.
    
    Original change's description:
    > Create //ios/web/public/security/BUILD.gn.
    > 
    > This CL creates //ios/web/public/security/BUILD.gn and removes the
    > temporary header //ios/web/public/certificate_policy_cache.h
    > 
    > Bug: 956511,942244
    > Change-Id: I7f70539cb2ce2d504e058a94a585124d265e12a9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1624809
    > Reviewed-by: Christopher Thompson <cthomp@chromium.org>
    > Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    > Reviewed-by: Eugene But <eugenebut@chromium.org>
    > Commit-Queue: Yi Su <mrsuyi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#663093}
    
    TBR=marq@chromium.org,eugenebut@chromium.org,mahmadi@chromium.org,cthomp@chromium.org,mrsuyi@chromium.org
    
    Change-Id: I370fafb2a873946e396fac17344c163b44a763d4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 956511, 942244
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626491
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663140}
    
    Former-commit-id: 913e4bd1e955217c469eeb04023d5dddc71268e3

---
+++ ios/web/public/certificate_policy_cache.h
@@ -0,0 +1,10 @@
+// Copyright 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_WEB_PUBLIC_CERTIFICATE_POLICY_CACHE_H_
+#define IOS_WEB_PUBLIC_CERTIFICATE_POLICY_CACHE_H_
+
+#include "ios/web/public/security/certificate_policy_cache.h"
+
+#endif  // IOS_WEB_PUBLIC_CERTIFICATE_POLICY_CACHE_H_
