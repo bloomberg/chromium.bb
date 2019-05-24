50165c4a0a888f25c12269dce6218e27558d5eaa
Guohui Deng <guohuideng@chromium.org>
2019-05-24 21:54:36 +0000

    [Chromecast] Remove |audio_device_id_| from CastRenderer
    1. The |audio_device_id| in CastRenderer is always
    ::media::AudioDeviceDescription::kDefaultDeviceId, so it's
    not needed.
    
    2. Add CastMojoMediaClient::CreateCastRenderer() method, so when
    CastRendererFactory in the upstream is implemented and used, the
    CastRenderer can be created and used without breaking chromecast.
    
    No behavior change is expected.
    
    Bug: crbug.com/925450
    Test: 1. Manually Cast. 2. Internal CQ.
    
    Change-Id: Ia2234f721ba6b636ea87a67198894aeeba173bde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626389
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Commit-Queue: Guohui Deng <guohuideng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663282}
    
    Former-commit-id: d9e902347ce613b26e2cd9dc93c80a5ba3d0793b

--- chromecast/media/service/cast_mojo_media_client.cc
+++ chromecast/media/service/cast_mojo_media_client.cc
@@ -37,13 +37,31 @@ void CastMojoMediaClient::Initialize(service_manager::Connector* connector) {
   connector_ = connector;
 }
 
+#if BUILDFLAG(ENABLE_CAST_RENDERER)
+std::unique_ptr<::media::Renderer> CastMojoMediaClient::CreateCastRenderer(
+    service_manager::mojom::InterfaceProvider* host_interfaces,
+    scoped_refptr<base::SingleThreadTaskRunner> task_runner,
+    ::media::MediaLog* /* media_log */,
+    const base::UnguessableToken& /* overlay_plane_id */) {
+  // TODO(crbug.com/925450): 1) Make a VideoGeometrySetter within
+  // CastMojoMediaClient. 2) Before return the created CastRenderer, pass the
+  // CastRenderer and the |overlay_plane_id| to VideoGeometrySetter so it can
+  // maintain a map between the CastRenderers and their associated
+  // |overlay_plane_id|s.
+  return std::make_unique<CastRenderer>(
+      backend_factory_, task_runner, video_mode_switcher_,
+      video_resolution_policy_, media_resource_tracker_, connector_,
+      host_interfaces);
+}
+#endif
+
 std::unique_ptr<::media::Renderer> CastMojoMediaClient::CreateRenderer(
     service_manager::mojom::InterfaceProvider* host_interfaces,
     scoped_refptr<base::SingleThreadTaskRunner> task_runner,
     ::media::MediaLog* /* media_log */,
     const std::string& audio_device_id) {
   return std::make_unique<CastRenderer>(
-      backend_factory_, task_runner, audio_device_id, video_mode_switcher_,
+      backend_factory_, task_runner, video_mode_switcher_,
       video_resolution_policy_, media_resource_tracker_, connector_,
       host_interfaces);
 }
