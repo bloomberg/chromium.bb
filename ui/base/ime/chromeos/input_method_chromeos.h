d85642e69f771f9258f95e630a97b05ab6e5aae6
Shu Chen <shuchen@chromium.org>
2019-05-24 00:49:08 +0000

    Remove ime.mojom and the code on the client side for Mojo-based IMF.
    Bug: 965908
    Change-Id: I83aec36d10247ef7dd08ef58a04f0f2362a8d495
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626053
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Shu Chen <shuchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662925}
    
    Former-commit-id: b664d7326452786b3168f34d21ce24cbd3065dc6

--- ui/base/ime/chromeos/input_method_chromeos.h
+++ ui/base/ime/chromeos/input_method_chromeos.h
@@ -50,8 +50,6 @@ class COMPONENT_EXPORT(UI_BASE_IME_CHROMEOS) InputMethodChromeOS
   InputMethodKeyboardController* GetInputMethodKeyboardController() override;
 
   // Overridden from InputMethodBase:
-  void OnFocus() override;
-  void OnBlur() override;
   void OnWillChangeFocusedClient(TextInputClient* focused_before,
                                  TextInputClient* focused) override;
   void OnDidChangeFocusedClient(TextInputClient* focused_before,
@@ -79,7 +77,6 @@ class COMPONENT_EXPORT(UI_BASE_IME_CHROMEOS) InputMethodChromeOS
   void ResetContext();
 
  private:
-  class MojoHelper;
   class PendingKeyEvent;
   friend TestableInputMethodChromeOS;
 
@@ -203,8 +200,6 @@ class COMPONENT_EXPORT(UI_BASE_IME_CHROMEOS) InputMethodChromeOS
   // Indicates whether currently is handling a physical key event.
   // This is used in CommitText/UpdateCompositionText/etc.
   bool handling_key_event_;
-
-  std::unique_ptr<MojoHelper> mojo_helper_;
 
   // Used for making callbacks.
   base::WeakPtrFactory<InputMethodChromeOS> weak_ptr_factory_;
