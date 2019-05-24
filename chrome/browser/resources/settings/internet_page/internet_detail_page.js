cd8f8e91646feb0141ccc5147b3d69723968dff0
Azeem Arshad <azeemarshad@chromium.org>
2019-05-24 19:10:01 +0000

    [Settings Networking] Fix issue with toggle affecting all networks.
    This CL fixes issue with Auto connect toggle button toggling for
    other networks. This was caused because the updates to fake pref
    objects that control this toggle is not propagated down to the
    children, thus the toggle maintains same value from last network
    detail page. Fixed this by updating the entire pref object instead
    of just the value sub-property.
    
    (cherry picked from commit ff4c4bdfb90b00d9315a78f3bbfa49cbfee6d89e [formerly 4367d762ee756a2b7f13f9272d90d92dae6e40c0])
    
    Bug: 957224
    Change-Id: Ic95c2b0ed9f77d9547114fe01acfcc18023c38fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1622415
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Jeremy Klein <jlklein@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#662836}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1629147
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3770@{#839}
    Cr-Branched-From: 04e619e4964cac3f22e7ef787268f756c2b6c1c7 [formerly a9eee1c7c727ef42a15d86e9fa7b77ff0e63840a]-refs/heads/master@{#652427}
    
    Former-commit-id: c98cc9af7d8b39a8d4849c75f734855f00f9225a

--- chrome/browser/resources/settings/internet_page/internet_detail_page.js
+++ chrome/browser/resources/settings/internet_page/internet_detail_page.js
@@ -90,7 +90,7 @@ Polymer({
     globalPolicy: {
       type: Object,
       value: null,
-      observer: 'updateAutoConnectPref_'
+      observer: 'globalPolicyChanged_',
     },
 
     /**
@@ -278,6 +278,12 @@ Polymer({
     });
   },
 
+  /** @param {!chrome.networkingPrivate.GlobalPolicy} globalPolicy */
+  globalPolicyChanged_: function(globalPolicy) {
+    this.updateAutoConnectPref_(
+        !!(this.autoConnect_ && this.autoConnect_.value), globalPolicy);
+  },
+
   /** @private */
   networkPropertiesChanged_: function() {
     if (!this.networkProperties_) {
@@ -285,13 +291,8 @@ Polymer({
     }
 
     // Update autoConnect if it has changed. Default value is false.
-    const autoConnect = CrOnc.getAutoConnect(this.networkProperties_);
-    if (this.autoConnect_ === undefined) {
-      this.updateAutoConnectPref_();
-    }
-    if (autoConnect != this.autoConnect_.value) {
-      this.autoConnect_.value = autoConnect;
-    }
+    this.updateAutoConnectPref_(
+        CrOnc.getAutoConnect(this.networkProperties_), this.globalPolicy);
 
     // Update preferNetwork if it has changed. Default value is false.
     const priority = /** @type {number} */ (
@@ -340,18 +341,36 @@ Polymer({
     this.setNetworkProperties_(onc);
   },
 
-  /** @private */
-  updateAutoConnectPref_: function() {
+  /**
+   * Updates auto-connect pref value.
+   * @param {boolean} value
+   * @param {!chrome.networkingPrivate.GlobalPolicy|undefined} globalPolicy
+   * @private
+   */
+  updateAutoConnectPref_: function(value, globalPolicy) {
+    let enforcement;
+    let controlledBy;
+
+    if (this.isAutoConnectEnforcedByPolicy(
+            this.networkProperties_, globalPolicy)) {
+      enforcement = chrome.settingsPrivate.Enforcement.ENFORCED;
+      controlledBy = chrome.settingsPrivate.ControlledBy.DEVICE_POLICY;
+    }
+
+    if (this.autoConnect_ && this.autoConnect_.value == value &&
+        enforcement == this.autoConnect_.enforcement &&
+        controlledBy == this.autoConnect_.controlledBy) {
+      return;
+    }
+
     const newPrefValue = {
       key: 'fakeAutoConnectPref',
-      value: !!this.autoConnect_ && !!this.autoConnect_.value,
+      value: value,
       type: chrome.settingsPrivate.PrefType.BOOLEAN,
     };
-    if (this.isAutoConnectEnforcedByPolicy(
-            this.networkProperties_, this.globalPolicy)) {
-      newPrefValue.controlledBy =
-          chrome.settingsPrivate.ControlledBy.DEVICE_POLICY;
-      newPrefValue.enforcement = chrome.settingsPrivate.Enforcement.ENFORCED;
+    if (enforcement) {
+      newPrefValue.enforcement = enforcement;
+      newPrefValue.controlledBy = controlledBy;
     }
 
     this.autoConnect_ = newPrefValue;
