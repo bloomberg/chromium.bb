b75efb67f84f080a03cdee96f7232dbc8c041258
David Riley <davidriley@chromium.org>
2019-05-28 13:08:12 -0700

    minigbm: i915: Add support for AR30 DRM_FORMAT_ARGB2101010.
    BUG=chromium:963559
    TEST=freecad starts under crosvm
    
    Change-Id: I01c9bfcb0eb8419f06fff69edff6f5eb09232403
    Reviewed-on: https://chromium-review.googlesource.com/1631946
    Tested-by: David Riley <davidriley@chromium.org>
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>

--- i915.c
+++ i915.c
@@ -27,7 +27,7 @@ static const uint32_t render_target_formats[] = { DRM_FORMAT_ABGR8888,    DRM_FO
 						  DRM_FORMAT_ARGB8888,    DRM_FORMAT_RGB565,
 						  DRM_FORMAT_XBGR2101010, DRM_FORMAT_XBGR8888,
 						  DRM_FORMAT_XRGB1555,    DRM_FORMAT_XRGB2101010,
-						  DRM_FORMAT_XRGB8888 };
+						  DRM_FORMAT_XRGB8888,    DRM_FORMAT_ARGB2101010 };
 
 static const uint32_t tileable_texture_source_formats[] = { DRM_FORMAT_GR88, DRM_FORMAT_R8,
 							    DRM_FORMAT_UYVY, DRM_FORMAT_YUYV };
