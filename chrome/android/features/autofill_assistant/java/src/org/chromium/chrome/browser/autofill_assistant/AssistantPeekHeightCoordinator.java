6c7eede7dfb78d88b6539606e858df9b50e9f844
Jordan Demeulenaere <jdemeulenaere@google.com>
2019-05-28 08:19:10 +0000

    [Autofill Assistant] Fix peek height when peekMode shows carousels.
    This CL fixes the height of the BottomSheet when minimized and
    peekMode=HANDLE_HEADER_CAROUSELS. Before http://crrev/c/1624194, the
    vertical spacing before and after actions were set as layout margins
    (i.e. it was not considered when computing the actions height), so we
    had to add this spacing when computing the peek height. After that CL,
    the spacing is contained into the actions height so there is no need to
    add it when computing the peek height.
    
    Change-Id: Ic83a983c085d2dc7e2d91515c1d7389eb004c955
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1631373
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#663680}
    
    Former-commit-id: 423a19b6ecdf903dc639bef9425721044ee7f032

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/AssistantPeekHeightCoordinator.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/AssistantPeekHeightCoordinator.java
@@ -172,16 +172,12 @@ class AssistantPeekHeightCoordinator {
                 break;
             case PeekMode.HANDLE_HEADER_CAROUSELS:
                 mToolbarPaddingBottom = mHeaderHeight + mProgressBarHeight;
-                if (mActionsHeight > 0 || mSuggestionsHeight > 0) {
-                    mToolbarPaddingBottom += mChildrenVerticalSpacing;
-                }
-
                 if (mSuggestionsHeight > 0) {
                     mToolbarPaddingBottom += mSuggestionsHeight + mChildrenVerticalSpacing;
                 }
 
                 if (mActionsHeight > 0) {
-                    mToolbarPaddingBottom += mActionsHeight + mChildrenVerticalSpacing;
+                    mToolbarPaddingBottom += mActionsHeight;
                 }
 
                 // We decrease the artificial padding we add to the toolbar by 1 pixel to make sure
