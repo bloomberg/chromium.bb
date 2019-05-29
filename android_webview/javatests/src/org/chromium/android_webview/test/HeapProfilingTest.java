61e5b691026157f79a0ddbc873f77ee872ca890c
Shimi Zhang <ctzsm@chromium.org>
2019-05-29 20:51:58 +0000

    aw: Disable HeapProfilingTest.testModeBrowser() test
    Test is failing on M and L bots.
    
    Bug: 968043
    Change-Id: Ied66c5015d832f42d141734b8b3a272f05947f63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1635930
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664430}
    
    Former-commit-id: 88d1417fad563ab1411204a8328b58c63a520476

--- android_webview/javatests/src/org/chromium/android_webview/test/HeapProfilingTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/HeapProfilingTest.java
@@ -6,6 +6,7 @@ package org.chromium.android_webview.test;
 
 import static org.chromium.android_webview.test.OnlyRunIn.ProcessMode.MULTI_PROCESS;
 
+import android.os.Build;
 import android.support.test.filters.MediumTest;
 
 import org.junit.Assert;
@@ -15,6 +16,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.components.heap_profiling.HeapProfilingTestShim;
 
 /**
@@ -31,10 +33,12 @@ public class HeapProfilingTest {
 
     @Test
     @MediumTest
-    @CommandLineFlags.Add({"memlog=browser", "memlog-stack-mode=native-include-thread-names",
-            "memlog-sampling-rate=1"})
-    public void
-    testModeBrowser() throws Exception {
+    @DisableIf
+            .Build(sdk_is_less_than = Build.VERSION_CODES.N)
+            @CommandLineFlags.Add({"memlog=browser",
+                    "memlog-stack-mode=native-include-thread-names", "memlog-sampling-rate=1"})
+            public void
+            testModeBrowser() throws Exception {
         HeapProfilingTestShim shim = new HeapProfilingTestShim();
         Assert.assertTrue(
                 shim.runTestForMode("browser", false, "native-include-thread-names", false, false));
