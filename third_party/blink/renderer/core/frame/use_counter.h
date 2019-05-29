e7eb1011089ee317e97197d8a6417802e65cdd91
Yutaka Hirano <yhirano@chromium.org>
2019-05-29 07:03:13 +0000

    Move blink::UseCounter to platform/
    Move the class so as to platform/loader can use it.
    
    Bug: 963306, 914739
    Change-Id: I647fc6bfe13c7c895e48842861cb3b84d4e49a17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1634134
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664151}
    
    Former-commit-id: 88833f30349db81d9cf2506e276e50065f29ed01

--- third_party/blink/renderer/core/frame/use_counter.h
+++ third_party/blink/renderer/core/frame/use_counter.h
@@ -34,6 +34,7 @@
 #include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
+#include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
 
 namespace blink {
@@ -43,47 +44,6 @@ class Element;
 class EnumerationHistogram;
 class LocalFrame;
 class StyleSheetContents;
-
-// Definition for UseCounter features can be found in:
-// third_party/blink/public/mojom/web_feature/web_feature.mojom
-//
-// UseCounter is used for counting the number of times features of
-// Blink are used on real web pages and help us know commonly
-// features are used and thus when it's safe to remove or change them. It's
-// counting whether a feature is used in a context (e.g., a page), so calling
-// a counting function multiple times for the same UseCounter with the same
-// feature will be ignored.
-//
-// The Chromium Content layer controls what is done with this data.
-//
-// For instance, in Google Chrome, these counts are submitted anonymously
-// through the UMA histogram recording system in Chrome for users who have the
-// "Automatically send usage statistics and crash reports to Google" setting
-// enabled:
-// http://www.google.com/chrome/intl/en/privacy.html
-//
-// This is a pure virtual interface class with some utility static functions.
-class CORE_EXPORT UseCounter : public GarbageCollectedMixin {
- public:
-  static void Count(UseCounter* use_counter, mojom::WebFeature feature) {
-    if (use_counter) {
-      use_counter->CountUse(feature);
-    }
-  }
-  static void Count(UseCounter& use_counter, mojom::WebFeature feature) {
-    use_counter.CountUse(feature);
-  }
-
- public:
-  UseCounter() = default;
-  virtual ~UseCounter() = default;
-
-  // Counts a use of the given feature. Repeated calls are ignored.
-  virtual void CountUse(mojom::WebFeature feature) = 0;
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(UseCounter);
-};
 
 // Utility class for muting UseCounter, for instance ignoring attributes
 // constructed in user-agent shadow DOM. Once constructed, all UseCounting
