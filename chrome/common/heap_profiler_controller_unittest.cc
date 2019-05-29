0497e46c74baf8a12c2e9e27f1bcc2f20299cf88
Alexei Filippov <alph@chromium.org>
2019-05-29 17:43:41 +0000

    [heap profiler] Test that empty profiles are not emitted.
    Change-Id: I89f90fc6b8942afde5121b13fecc8f351691000c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1629335
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664335}
    
    Former-commit-id: 594481a351ba8f473e5cb40bdd0e98224b4f0feb

--- chrome/common/heap_profiler_controller_unittest.cc
+++ chrome/common/heap_profiler_controller_unittest.cc
@@ -5,29 +5,43 @@
 #include "chrome/common/heap_profiler_controller.h"
 
 #include "base/sampling_heap_profiler/sampling_heap_profiler.h"
+#include "base/test/bind_test_util.h"
 #include "base/test/test_mock_time_task_runner.h"
 #include "build/build_config.h"
 #include "components/metrics/call_stack_profile_builder.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/metrics_proto/sampled_profile.pb.h"
 
-constexpr size_t kAllocationSize = 42 * 1024;
-constexpr int kSnapshotsToCollect = 3;
+TEST(HeapProfilerControllerTest, EmptyProfileIsNotEmitted) {
+  auto task_runner = base::MakeRefCounted<base::TestMockTimeTaskRunner>();
+  base::TestMockTimeTaskRunner::ScopedContext scoped_context(task_runner.get());
 
-class HeapProfilerControllerTest : public testing::Test {
- public:
-  HeapProfilerControllerTest() = default;
+  HeapProfilerController controller;
+  metrics::CallStackProfileBuilder::SetBrowserProcessReceiverCallback(
+      base::BindLambdaForTesting(
+          [](base::TimeTicks time, metrics::SampledProfile profile) {
+            ADD_FAILURE();
+          }));
+  controller.SetTaskRunnerForTest(task_runner);
+  controller.Start();
 
-  void StartController(scoped_refptr<base::TaskRunner> task_runner) {
-    metrics::CallStackProfileBuilder::SetBrowserProcessReceiverCallback(
-        base::BindRepeating(&HeapProfilerControllerTest::CheckProfile,
-                            base::Unretained(this)));
-    controller_ = std::make_unique<HeapProfilerController>();
-    controller_->SetTaskRunnerForTest(std::move(task_runner));
-    controller_->Start();
-  }
+  task_runner->FastForwardBy(base::TimeDelta::FromDays(365));
+}
 
-  void CheckProfile(base::TimeTicks time, metrics::SampledProfile profile) {
+// Sampling profiler is not capable of unwinding stack on Android under tests.
+#if !defined(OS_ANDROID)
+TEST(HeapProfilerControllerTest, ProfileCollectionsScheduler) {
+  constexpr size_t kAllocationSize = 42 * 1024;
+  constexpr int kSnapshotsToCollect = 3;
+
+  auto task_runner = base::MakeRefCounted<base::TestMockTimeTaskRunner>();
+  base::TestMockTimeTaskRunner::ScopedContext scoped_context(task_runner.get());
+
+  auto controller = std::make_unique<HeapProfilerController>();
+  int profile_count = 0;
+
+  auto check_profile = [&](base::TimeTicks time,
+                           metrics::SampledProfile profile) {
     EXPECT_EQ(metrics::SampledProfile::PERIODIC_HEAP_COLLECTION,
               profile.trigger_event());
     EXPECT_LT(0, profile.call_stack_profile().stack_sample_size());
@@ -43,30 +57,20 @@ class HeapProfilerControllerTest : public testing::Test {
     }
     EXPECT_TRUE(found);
 
-    if (++snapshots_count_ == kSnapshotsToCollect)
-      controller_.reset();
-  }
-
- protected:
-  int snapshots_count_ = 0;
-  std::unique_ptr<HeapProfilerController> controller_;
-
-  DISALLOW_COPY_AND_ASSIGN(HeapProfilerControllerTest);
-};
-
-// Sampling profiler is not capable of unwinding stack on Android under tests.
-#if !defined(OS_ANDROID)
-TEST_F(HeapProfilerControllerTest, ProfileCollectionsScheduler) {
-  auto task_runner = base::MakeRefCounted<base::TestMockTimeTaskRunner>();
-  base::TestMockTimeTaskRunner::ScopedContext scoped_context(task_runner.get());
+    if (++profile_count == kSnapshotsToCollect)
+      controller.reset();
+  };
 
   base::SamplingHeapProfiler::Init();
   auto* profiler = base::SamplingHeapProfiler::Get();
   profiler->SetSamplingInterval(1024);
   profiler->Start();
 
-  base::PoissonAllocationSampler::Init();
-  StartController(task_runner);
+  metrics::CallStackProfileBuilder::SetBrowserProcessReceiverCallback(
+      base::BindLambdaForTesting(check_profile));
+  controller->SetTaskRunnerForTest(task_runner);
+  controller->Start();
+
   auto* sampler = base::PoissonAllocationSampler::Get();
   sampler->SuppressRandomnessForTest(true);
   sampler->RecordAlloc(reinterpret_cast<void*>(0x1337), kAllocationSize,
@@ -75,6 +79,6 @@ TEST_F(HeapProfilerControllerTest, ProfileCollectionsScheduler) {
                        base::PoissonAllocationSampler::kMalloc, nullptr);
 
   task_runner->FastForwardUntilNoTasksRemain();
-  EXPECT_GE(snapshots_count_, kSnapshotsToCollect);
+  EXPECT_LE(kSnapshotsToCollect, profile_count);
 }
 #endif
