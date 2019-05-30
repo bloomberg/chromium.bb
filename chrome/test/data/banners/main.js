a1b4a6946f6bd8ce54533830779ab271b6d68725
Michael Thiessen <mthiesse@chromium.org>
2019-05-30 00:46:55 +0000

    Allow use of fullscreen API from fullscreen PWAs
    Whatever issues caused us to initially disable the fullscreen API from
    fullscreen PWAs, they no longer seem to be present. In testing the
    sample in issue 720304 I couldn't find any issues.
    
    Bug: 963929
    Change-Id: I6fab2f4a558c2b7fdbd61b8ed92714f671bce7db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1627563
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664570}
    
    Former-commit-id: edaef9cb38a9d012374d682e5c5bcdd7a2cbabb4

--- chrome/test/data/banners/main.js
+++ chrome/test/data/banners/main.js
@@ -17,6 +17,7 @@ const Action = {
   CANCEL_PROMPT: 'cancel_prompt',
   STASH_EVENT: 'stash_event',
   STASH_EVENT_AND_PREVENT_DEFAULT: 'stash_event_and_prevent_default',
+  FULLSCREEN_ON_CLICK: 'fullscreen_on_click',
 };
 
 const LISTENER = "listener";
@@ -61,6 +62,18 @@ function callStashedPrompt() {
   callPrompt(stashedEvent);
 }
 
+function isBodyFullscreen() {
+  return document.fullscreenElement == document.body;
+}
+
+function toggleFullscreen() {
+  if (isBodyFullscreen()) {
+    document.exitFullscreen();
+  } else {
+    document.body.requestFullscreen();
+  }
+}
+
 function addClickListener(action) {
   switch (action) {
     case Action.CALL_STASHED_PROMPT_ON_CLICK:
@@ -69,6 +82,9 @@ function addClickListener(action) {
     case Action.CALL_STASHED_PROMPT_ON_CLICK_VERIFY_APPINSTALLED:
       window.addEventListener('click', callStashedPrompt);
       verifyEvents("appinstalled");
+      break;
+    case Action.FULLSCREEN_ON_CLICK:
+      window.addEventListener('click', toggleFullscreen);
       break;
   }
 }
@@ -156,6 +172,9 @@ function initialize() {
     case Action.CANCEL_PROMPT:
     case Action.STASH_EVENT:
       addPromptListener(action);
+      break;
+    case Action.FULLSCREEN_ON_CLICK:
+      addClickListener(action);
       break;
     default:
       throw new Error("Unrecognised action: " + action);
