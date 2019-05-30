5e02dc3ce294c98162d1b0c8728cca60c61dfcb8
John Abd-El-Malek <jam@chromium.org>
2019-05-30 21:22:20 +0000

    Enable network service feature flag for Chromecast.
    Bug: 966633
    Change-Id: I15cd56e2d36465e69d27a2c42b33d6a159873275
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636182
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Junbo Ke <juke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664903}
    
    Former-commit-id: e6d52d6c6cc3d2e30cd268c5ac1d72e16dce602c

--- chromecast/browser/cast_content_browser_client.cc
+++ chromecast/browser/cast_content_browser_client.cc
@@ -222,7 +222,6 @@ CastContentBrowserClient::CastContentBrowserClient(
       std::make_unique<CastNetworkContexts>(url_request_context_factory_.get());
   cast_feature_list_creator_->SetExtraEnableFeatures({
     ::media::kInternalMediaSession,
-    network::features::kNetworkService,
     features::kNetworkServiceInProcess,
 #if defined(OS_ANDROID)
         // TODO(awolter): Remove this once the feature is on by default.
