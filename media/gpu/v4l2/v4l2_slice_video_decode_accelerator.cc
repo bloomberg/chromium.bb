e56b4b93108a6bdad0fd5e0ba13dc5b839066cfe
Alexandre Courbot <acourbot@chromium.org>
2019-05-30 07:21:34 +0000

    Revert "media/gpu/v4l2svda: fix stateless API specification violation"
    This reverts commit 3b17181195068add30834a2d7cad5219598eca0a [formerly 91b0391fda87564fe352141cb804d0dfd410d8b6].
    
    Reason for revert: 
    
    Bug: chromium:968392
    Test: Checked that video_VideoSeek.vp8.switchres passed on Kevin.
    
    Original change's description:
    > media/gpu/v4l2svda: fix stateless API specification violation
    > 
    > The stateless API states that resolution of frames should be set on the
    > OUTPUT queue, and the coded resolution read back from the CAPTURE queue.
    > However we were setting the resolution on the CAPTURE queue and
    > expecting the ioctl to update it to the coded resolution.
    > 
    > This works with our current kernel drivers, but is not correct with
    > respect to the stateless codec API specification. The kernel drivers
    > have been updated to support the correct behavior, so carry that change
    > into Chromium as well.
    > 
    > Bug: 948534
    > Test: Checked that VDA unittest passes on Kevin and Minnie with kernel
    > driver fixes.
    > 
    > Change-Id: I8f589f81ae1a7d8223235739f0748e58ac25e342
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1553124
    > Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    > Commit-Queue: Alexandre Courbot <acourbot@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#659876}
    
    TBR=tfiga@chromium.org,hiroh@chromium.org,acourbot@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 948534
    Change-Id: I710af049273963ec34786633b0afb67736df2249
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636992
    Auto-Submit: Alexandre Courbot <acourbot@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Alexandre Courbot <acourbot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#664676}
    
    Former-commit-id: 28c9dc6755ce929863d59bf51b4bd9eaad083dd8

--- media/gpu/v4l2/v4l2_slice_video_decode_accelerator.cc
+++ media/gpu/v4l2/v4l2_slice_video_decode_accelerator.cc
@@ -508,28 +508,16 @@ bool V4L2SliceVideoDecodeAccelerator::CreateOutputBuffers() {
   DCHECK_GT(num_pictures, 0u);
   DCHECK(!pic_size.IsEmpty());
 
-  // Set the frame size on the OUTPUT queue
   struct v4l2_format format;
   memset(&format, 0, sizeof(format));
-  format.type = V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE;
-  if (device_->Ioctl(VIDIOC_G_FMT, &format) != 0) {
-    VPLOGF(1) << "Failed getting format of output queue";
-    NOTIFY_ERROR(PLATFORM_FAILURE);
-    return false;
-  }
+  format.type = V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE;
+  format.fmt.pix_mp.pixelformat = output_format_fourcc_;
   format.fmt.pix_mp.width = pic_size.width();
   format.fmt.pix_mp.height = pic_size.height();
-  if (device_->Ioctl(VIDIOC_S_FMT, &format) != 0) {
-    VPLOGF(1) << "Failed changing resolution of output queue";
-    NOTIFY_ERROR(PLATFORM_FAILURE);
-    return false;
-  }
+  format.fmt.pix_mp.num_planes = output_planes_count_;
 
-  // Get the coded size from the CAPTURE queue
-  memset(&format, 0, sizeof(format));
-  format.type = V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE;
-  if (device_->Ioctl(VIDIOC_G_FMT, &format) != 0) {
-    VPLOGF(1) << "Failed getting format of capture queue";
+  if (device_->Ioctl(VIDIOC_S_FMT, &format) != 0) {
+    VPLOGF(1) << "Failed setting format to: " << output_format_fourcc_;
     NOTIFY_ERROR(PLATFORM_FAILURE);
     return false;
   }
