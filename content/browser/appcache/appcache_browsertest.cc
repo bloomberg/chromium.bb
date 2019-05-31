f314ff2d2938f5002f4d79218f1bb52e659c8fa7
Victor Costan <pwnall@chromium.org>
2019-05-31 21:11:24 +0000

    AppCache: Reuse cacheable resources on cache update.
    Bug: 968179
    Change-Id: Ib07f23f14102e9bfed5f54c9ccf9a35fffceb258
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1636266
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665267}
    
    Former-commit-id: 4c91650bde69c7d0b7597295f38c93e272b0ddce

--- content/browser/appcache/appcache_browsertest.cc
+++ content/browser/appcache/appcache_browsertest.cc
@@ -4,13 +4,19 @@
 
 #include <stdint.h>
 #include "base/bind.h"
+#include "base/run_loop.h"
+#include "base/strings/strcat.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/bind_test_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "content/browser/appcache/appcache_subresource_url_factory.h"
 #include "content/public/browser/navigation_entry.h"
+#include "content/public/browser/render_frame_host.h"
+#include "content/public/browser/render_process_host.h"
 #include "content/public/browser/ssl_status.h"
+#include "content/public/browser/storage_partition.h"
+#include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/content_browser_test.h"
 #include "content/public/test/content_browser_test_utils.h"
@@ -23,6 +29,7 @@
 #include "net/test/embedded_test_server/http_request.h"
 #include "net/test/embedded_test_server/http_response.h"
 #include "services/network/public/cpp/features.h"
+#include "services/network/public/mojom/network_context.mojom.h"
 
 namespace content {
 
@@ -120,6 +127,73 @@ IN_PROC_BROWSER_TEST_F(AppCacheNetworkServiceBrowserTest,
       shell()->web_contents()->GetController().GetVisibleEntry();
   EXPECT_FALSE(net::IsCertStatusError(entry->GetSSL().cert_status));
   EXPECT_TRUE(entry->GetSSL().certificate);
+}
+
+// Regression test for crbug.com/968179.
+IN_PROC_BROWSER_TEST_F(AppCacheNetworkServiceBrowserTest,
+                       CacheableResourcesReuse) {
+  net::EmbeddedTestServer embedded_test_server;
+
+  std::string manifest_nonce = "# Version 1";
+  int resource_request_count = 0;
+  embedded_test_server.RegisterRequestHandler(base::BindLambdaForTesting(
+      [&](const net::test_server::HttpRequest& request)
+          -> std::unique_ptr<net::test_server::HttpResponse> {
+        if (request.GetURL().path() != "/appcache/cache_reuse.manifest") {
+          ++resource_request_count;
+          return nullptr;
+        }
+
+        // Return a dynamically generated manifest, to trigger AppCache updates.
+        auto http_response =
+            std::make_unique<net::test_server::BasicHttpResponse>();
+        http_response->set_content_type("text/cache-manifest");
+        http_response->set_content(base::StrCat({
+            "CACHE MANIFEST\n",
+            manifest_nonce,
+            "\n/appcache/cache_reuse.html\n",
+        }));
+        return http_response;
+      }));
+
+  embedded_test_server.SetSSLConfig(net::EmbeddedTestServer::CERT_OK,
+                                    net::SSLServerConfig());
+  embedded_test_server.ServeFilesFromSourceDirectory(GetTestDataFilePath());
+  ASSERT_TRUE(embedded_test_server.Start());
+
+  GURL main_url = embedded_test_server.GetURL("/appcache/cache_reuse.html");
+
+  // First navigation populates AppCache.
+  {
+    EXPECT_TRUE(NavigateToURL(shell(), main_url));
+    base::string16 expected_title = base::ASCIIToUTF16("AppCache primed");
+    TitleWatcher title_watcher(shell()->web_contents(), expected_title);
+    EXPECT_EQ(expected_title, title_watcher.WaitAndGetTitle());
+  }
+
+  // Flush the HTTP cache so cache_reuse.html won't be served from there.
+  base::RunLoop run_loop;
+  content::StoragePartition* storage_partition = shell()
+                                                     ->web_contents()
+                                                     ->GetMainFrame()
+                                                     ->GetProcess()
+                                                     ->GetStoragePartition();
+  storage_partition->GetNetworkContext()->ClearHttpCache(
+      base::Time(), base::Time::Max(), nullptr, run_loop.QuitClosure());
+  run_loop.Run();
+
+  // Second navigation triggers an AppCache update.
+  resource_request_count = 0;
+  manifest_nonce = "# Version 2";
+  {
+    EXPECT_TRUE(NavigateToURL(shell(), main_url));
+    base::string16 expected_title = base::ASCIIToUTF16("AppCache updated");
+    TitleWatcher title_watcher(shell()->web_contents(), expected_title);
+    EXPECT_EQ(expected_title, title_watcher.WaitAndGetTitle());
+  }
+
+  // The AppCache update should only reload the manifest.
+  EXPECT_EQ(0, resource_request_count);
 }
 
 }  // namespace content
