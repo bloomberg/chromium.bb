e8970fecc183c7edb3a3f014b3ce5d74f412179d
Victor Costan <pwnall@chromium.org>
2019-05-31 16:47:37 +0000

    AppCache: Clean up browsertest.
    Change-Id: I89e9c90a2a965dab70d27f6e23c47c4783ecd638
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1637242
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665155}
    
    Former-commit-id: f8ef43e449f4d54d21c2da2e886677d41dc6ed9d

--- content/browser/appcache/appcache_browsertest.cc
+++ content/browser/appcache/appcache_browsertest.cc
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include "base/bind.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/bind_test_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "content/browser/appcache/appcache_subresource_url_factory.h"
@@ -30,54 +31,36 @@ class AppCacheNetworkServiceBrowserTest : public ContentBrowserTest {
         network::features::kNetworkService);
   }
 
-  ~AppCacheNetworkServiceBrowserTest() override {}
-
-  // Handler to count the number of requests.
-  std::unique_ptr<net::test_server::HttpResponse> HandleRequest(
-      const net::test_server::HttpRequest& request) {
-    request_count_++;
-    return std::unique_ptr<net::test_server::HttpResponse>();
-  }
-
-  // Call this to reset the request_count_.
-  void Clear() {
-    request_count_ = 0;
-  }
-
-  int request_count() const { return request_count_; }
+  ~AppCacheNetworkServiceBrowserTest() override = default;
 
  private:
   base::test::ScopedFeatureList scoped_feature_list_;
 
-  // Tracks the number of requests.
-  int request_count_ = 0;
-
   DISALLOW_COPY_AND_ASSIGN(AppCacheNetworkServiceBrowserTest);
 };
 
-// The network service process launch DCHECK's on Android. The bug
-// here http://crbug.com/748764. It looks like unsandboxed utility
-// processes are not supported on Android.
-#if !defined(OS_ANDROID)
 // This test validates that navigating to a TLD which has an AppCache
 // associated with it and then navigating to another TLD within that
 // host clears the previously registered factory. We verify this by
 // validating that request count for the last navigation.
 IN_PROC_BROWSER_TEST_F(AppCacheNetworkServiceBrowserTest,
                        VerifySubresourceFactoryClearedOnNewNavigation) {
-  std::unique_ptr<net::EmbeddedTestServer> embedded_test_server(
-      new net::EmbeddedTestServer());
+  net::EmbeddedTestServer embedded_test_server;
 
-  embedded_test_server->RegisterRequestHandler(
-      base::BindRepeating(&AppCacheNetworkServiceBrowserTest::HandleRequest,
-                          base::Unretained(this)));
+  int request_count = 0;
+  embedded_test_server.RegisterRequestHandler(base::BindLambdaForTesting(
+      [&](const net::test_server::HttpRequest& request)
+          -> std::unique_ptr<net::test_server::HttpResponse> {
+        ++request_count;
+        return nullptr;
+      }));
 
-  embedded_test_server->AddDefaultHandlers(GetTestDataFilePath());
+  embedded_test_server.ServeFilesFromSourceDirectory(GetTestDataFilePath());
 
-  ASSERT_TRUE(embedded_test_server->Start());
+  ASSERT_TRUE(embedded_test_server.Start());
 
   GURL main_url =
-      embedded_test_server->GetURL("/appcache/simple_page_with_manifest.html");
+      embedded_test_server.GetURL("/appcache/simple_page_with_manifest.html");
 
   base::string16 expected_title = base::ASCIIToUTF16("AppCache updated");
 
@@ -92,17 +75,16 @@ IN_PROC_BROWSER_TEST_F(AppCacheNetworkServiceBrowserTest,
   EXPECT_EQ(main_url, observer.last_navigation_url());
   EXPECT_TRUE(observer.last_navigation_succeeded());
 
-  Clear();
+  request_count = 0;
   GURL page_no_manifest =
-      embedded_test_server->GetURL("/appcache/simple_page_no_manifest.html");
+      embedded_test_server.GetURL("/appcache/simple_page_no_manifest.html");
 
   EXPECT_TRUE(NavigateToURL(shell(), page_no_manifest));
   // We expect two requests for simple_page_no_manifest.html. The request
   // for the main page and the logo.
-  EXPECT_GT(request_count(), 1);
+  EXPECT_GT(request_count, 1);
   EXPECT_EQ(page_no_manifest, observer.last_navigation_url());
   EXPECT_TRUE(observer.last_navigation_succeeded());
 }
-#endif
 
 }  // namespace content
