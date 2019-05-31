05064aa23c78c68dbbed864a0f1d9588cfbaa1a9
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-05-31 19:35:23 +0000

    Replace HeapListHashSet with HeapLinkedHashSet in HitRegionManager
    ListHashSet is deprecated in favor of LinkedHashSet.
    
    HeapLinkedHashSet's invalidate iterators when the container is
    modified.  This does not happen in this usage, so replacement is
    simple.
    
    Change-Id: Iea0b4d86c5991b87baf20cd37ebf154c99a8fa64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1637002
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#665217}
    
    Former-commit-id: bd7533d4cba2c5d1204fbd38858b93b1d89b576f

--- third_party/blink/renderer/modules/canvas/canvas2d/hit_region.h
+++ third_party/blink/renderer/modules/canvas/canvas2d/hit_region.h
@@ -11,6 +11,7 @@
 #include "third_party/blink/renderer/modules/canvas/canvas2d/hit_region_options.h"
 #include "third_party/blink/renderer/platform/graphics/path.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
+#include "third_party/blink/renderer/platform/wtf/linked_hash_set.h"
 
 namespace blink {
 
@@ -36,9 +37,11 @@ class HitRegion final : public GarbageCollectedFinalized<HitRegion> {
   WindRule fill_rule_;
 };
 
-class HitRegionManager final : public GarbageCollected<HitRegionManager> {
+class HitRegionManager final
+    : public GarbageCollectedFinalized<HitRegionManager> {
  public:
   HitRegionManager() = default;
+  ~HitRegionManager() {}
 
   void AddHitRegion(HitRegion*);
 
@@ -57,7 +60,7 @@ class HitRegionManager final : public GarbageCollected<HitRegionManager> {
   void Trace(blink::Visitor*);
 
  private:
-  typedef HeapListHashSet<Member<HitRegion>> HitRegionList;
+  typedef HeapLinkedHashSet<Member<HitRegion>> HitRegionList;
   typedef HitRegionList::const_reverse_iterator HitRegionIterator;
   typedef HeapHashMap<String, Member<HitRegion>> HitRegionIdMap;
   typedef HeapHashMap<Member<const Element>, Member<HitRegion>>
