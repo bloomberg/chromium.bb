46cc4d380b067f5950657c441cd03d99ecf2c074
Juanmi Huertas <juanmihd@chromium.org>
2019-05-31 21:05:15 +0000

    Reland "Reland "Adding support to bitmaprenderer in OffscreenCanvas""
    Registering the factory on OffscreenCanvas in modules, adding
    ImageBitmapRenderer to the union type for OffscreenCanvas.
    Adding new functionality to bitmaprenderer to let it be used
    in offscreencanvas.
    Changing IDL and some tests to validate new functionality.
    Adding a new virtual test to guarantee testing validation
    of trasnsfertooffscreencanvas for bitmaprenderer.
    
    Added bug: 969151 for the two missing tests.
    
    TBR=chrishtr
    
    Bug: 907141
    Change-Id: I7c611952686aabdf2eb3df8ffe5ca73a69ae97f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1638919
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: Juanmi Huertas <juanmihd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665260}
    
    Former-commit-id: 3285c7d5e7d9698ad87ba3891d5dd2e2daa5a72d

--- third_party/blink/renderer/modules/modules_initializer.cc
+++ third_party/blink/renderer/modules/modules_initializer.cc
@@ -145,6 +145,8 @@ void ModulesInitializer::Initialize() {
       std::make_unique<WebGLRenderingContext::Factory>());
   OffscreenCanvas::RegisterRenderingContextFactory(
       std::make_unique<WebGL2RenderingContext::Factory>());
+  OffscreenCanvas::RegisterRenderingContextFactory(
+      std::make_unique<ImageBitmapRenderingContext::Factory>());
 #if defined(SUPPORT_WEBGL2_COMPUTE_CONTEXT)
   OffscreenCanvas::RegisterRenderingContextFactory(
       std::make_unique<WebGL2ComputeRenderingContext::Factory>());
