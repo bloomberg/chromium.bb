618e5b0b25f723033b944fe0884649e31d00f5a9
Peter Boström <pbos@chromium.org>
2019-06-03 20:06:34 +0000

    Enable NewTabLoadingAnimation by default
    Turns on a new tab-loading animation by default. This animation displays
    the favicon earlier in the loading process to communicate where a user
    is navigating to even while the page is still loading.
    
    Bug: chromium:941039
    Change-Id: I8a117243d963b87e75a875832529bcb35e9af035
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1639565
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#665637}
    
    Former-commit-id: 2a0840baa839599d6b7a3eb7d3c56dd73c507c93

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -439,7 +439,7 @@ const base::Feature kNewNetErrorPageUI{"NewNetErrorPageUI",
 
 #if !defined(OS_ANDROID)
 const base::Feature kNewTabLoadingAnimation{"NewTabLoadingAnimation",
-                                            base::FEATURE_DISABLED_BY_DEFAULT};
+                                            base::FEATURE_ENABLED_BY_DEFAULT};
 #endif  // !OS_ANDROID
 
 #if defined(OS_POSIX)
