bb0190d906709ac4335eff29d503280c91913732
Wojciech Dzierżanowski <wdzierzanowski@opera.com>
2019-06-04 16:23:22 +0000

    Remove invalid assertion in data_reduction_proxy_settings.cc
    Recreating the NetworkContext upon the network service process restart
    is a legitimate scenario where a new CustomProxyConfigClient ought to
    replace any old CustomProxyConfigClient set in
    DataReductionProxySettings.
    
    Bug: 970236
    Change-Id: I849b8aebd5f94cab50d11318101e96ffc4c51589
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1642639
    Reviewed-by: rajendrant <rajendrant@chromium.org>
    Commit-Queue: Wojciech Dzierżanowski <wdzierzanowski@opera.com>
    Cr-Commit-Position: refs/heads/master@{#665941}
    
    Former-commit-id: 1a9e4b32f68fb08856679f0728fb3b14be32739e

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_settings.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_settings.cc
@@ -354,7 +354,6 @@ void DataReductionProxySettings::RemoveDataReductionProxySettingsObserver(
 
 void DataReductionProxySettings::SetCustomProxyConfigClient(
     network::mojom::CustomProxyConfigClientPtrInfo proxy_config_client) {
-  DCHECK(!data_reduction_proxy_service_);
   proxy_config_client_ = std::move(proxy_config_client);
 }
 
