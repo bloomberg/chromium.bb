044a0095e608bca17ff2aea47607af3b1c54a1fe
Yuwei Huang <yuweih@chromium.org>
2019-06-05 14:12:56 +0000

    [remoting][FTL] Add unittest for FtlRegistrationManager
    Bug: 965746
    Change-Id: I0d9b31fea543ee0b9e661f2025bc9133863f9dce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1643786
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666271}
    
    Former-commit-id: a77193b5cf85689044c6df09f635a18b24e7b045

--- remoting/signaling/ftl_registration_manager.h
+++ remoting/signaling/ftl_registration_manager.h
@@ -12,13 +12,22 @@
 #include "base/macros.h"
 #include "base/timer/timer.h"
 #include "net/base/backoff_entry.h"
-#include "remoting/proto/ftl/v1/ftl_services.grpc.pb.h"
 #include "remoting/signaling/registration_manager.h"
+
+namespace grpc {
+class Status;
+}  // namespace grpc
 
 namespace remoting {
 
+namespace ftl {
+
+class SignInGaiaRequest;
+class SignInGaiaResponse;
+
+}  // namespace ftl
+
 class FtlDeviceIdProvider;
-class GrpcExecutor;
 class OAuthTokenGetter;
 
 // Class for registering the user with FTL service.
@@ -39,17 +48,28 @@ class FtlRegistrationManager final : public RegistrationManager {
   std::string GetFtlAuthToken() const override;
 
  private:
-  using Registration =
-      google::internal::communications::instantmessaging::v1::Registration;
+  using SignInGaiaResponseCallback =
+      base::OnceCallback<void(const grpc::Status&,
+                              const ftl::SignInGaiaResponse&)>;
+
+  friend class FtlRegistrationManagerTest;
+
+  class RegistrationClient {
+   public:
+    virtual ~RegistrationClient() = default;
+    virtual void SignInGaia(const ftl::SignInGaiaRequest& request,
+                            SignInGaiaResponseCallback on_done) = 0;
+    virtual void CancelPendingRequests() = 0;
+  };
+  class RegistrationClientImpl;
 
   void DoSignInGaia(DoneCallback on_done);
   void OnSignInGaiaResponse(DoneCallback on_done,
                             const grpc::Status& status,
                             const ftl::SignInGaiaResponse& response);
 
-  std::unique_ptr<GrpcExecutor> executor_;
+  std::unique_ptr<RegistrationClient> registration_client_;
   std::unique_ptr<FtlDeviceIdProvider> device_id_provider_;
-  std::unique_ptr<Registration::Stub> registration_stub_;
   base::OneShotTimer sign_in_backoff_timer_;
   base::OneShotTimer sign_in_refresh_timer_;
   std::string registration_id_;
