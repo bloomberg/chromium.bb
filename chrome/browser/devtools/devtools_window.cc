990a2539d5e8bb7529992b7a9e00a6350e888321
Robert Liao <robliao@chromium.org>
2019-06-05 17:32:13 +0000

    Convert Devtools Uses of base::RepeatingCallback<>::Equals to Use == or !=
    BUG=937566
    
    Change-Id: I412c5e383e454e375ba3b10fcec05058b7347422
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1644319
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666340}
    
    Former-commit-id: 632b4d3b86b150a3d4a65f4035aa50a97b01f103

--- chrome/browser/devtools/devtools_window.cc
+++ chrome/browser/devtools/devtools_window.cc
@@ -398,7 +398,7 @@ void DevToolsWindow::AddCreationCallbackForTest(
 void DevToolsWindow::RemoveCreationCallbackForTest(
     const CreationCallback& callback) {
   for (size_t i = 0; i < g_creation_callbacks.Get().size(); ++i) {
-    if (g_creation_callbacks.Get().at(i).Equals(callback)) {
+    if (g_creation_callbacks.Get().at(i) == callback) {
       g_creation_callbacks.Get().erase(g_creation_callbacks.Get().begin() + i);
       return;
     }
