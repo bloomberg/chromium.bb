70b0b07b479537480641167e15ad7fc2cc60ef4b
Vikas Soni <vikassoni@chromium.org>
2019-06-06 20:41:43 +0000

    [Merge to M75] Update presentation time calculations.
    As per the spec, the driver is expected to return a valid timestamp from
    the call eglGetFrameTimestampsANDROID() when its not
    EGL_TIMESTAMP_PENDING_ANDROID or EGL_TIMESTAMP_INVALID_ANDROID.
    But currently few buggy drivers returns an invalid timestamp 0.
    
    We fix/workaround this in chrome by returning the presentation time
    as TimeTicks::Now() (snapped to the next vsync) instead of 0.
    
    (cherry picked from commit cb124caca0d071ee5e2151c1311c888ac04ab907 [formerly 7746363d083e6b3451a99199bf3a3be1040d3175])
    
    Bug: 966638
    Change-Id: I5075dda0739f02029922ebbc20c09656fb0e87ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1631354
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#663918}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1648894
    Reviewed-by: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3770@{#971}
    Cr-Branched-From: 04e619e4964cac3f22e7ef787268f756c2b6c1c7 [formerly a9eee1c7c727ef42a15d86e9fa7b77ff0e63840a]-refs/heads/master@{#652427}
    
    Former-commit-id: 91cbb0c517a5e270eea247f23149a74925496829

--- ui/gl/gl_surface_egl.cc
+++ ui/gl/gl_surface_egl.cc
@@ -1475,6 +1475,15 @@ bool NativeViewGLSurfaceEGL::GetFrameTimestampInfoIfAvailable(
   // reporting purpose.
   std::vector<EGLnsecsANDROID> egl_timestamps(supported_egl_timestamps_.size(),
                                               EGL_TIMESTAMP_INVALID_ANDROID);
+
+  // TODO(vikassoni): File a driver bug for eglGetFrameTimestampsANDROID().
+  // See https://bugs.chromium.org/p/chromium/issues/detail?id=966638.
+  // As per the spec, the driver is expected to return a valid timestamp from
+  // the call eglGetFrameTimestampsANDROID() when its not
+  // EGL_TIMESTAMP_PENDING_ANDROID or EGL_TIMESTAMP_INVALID_ANDROID. But
+  // currently some buggy drivers an invalid timestamp 0.
+  // This is currentlt handled in chrome for by setting the presentation time to
+  // TimeTicks::Now() (snapped to the next vsync) instead of 0.
   if ((frame_id < 0) ||
       !eglGetFrameTimestampsANDROID(
           GetDisplay(), surface_, frame_id,
@@ -1502,7 +1511,6 @@ bool NativeViewGLSurfaceEGL::GetFrameTimestampInfoIfAvailable(
                          base::TimeDelta::FromNanoseconds(presentation_time_ns);
     *presentation_flags = presentation_flags_;
   }
-  DCHECK(!presentation_time->is_null());
   return true;
 }
 
