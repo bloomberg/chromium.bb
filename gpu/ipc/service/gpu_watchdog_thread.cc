3097518710457dd40810195a74be37fce32e900e
Maggie Chen <magchen@chromium.org>
2019-06-06 01:19:25 +0000

    Remove all unused header includes in gpu_watchdog_thread.cc
    Those header files are not used in gpu_watchdog_thread.h nor gpu_watch_thread.cc
    
    Bug: 949839
    Change-Id: I08c38f2edf9d3de5462f03280c2f087e0fae549e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1647175
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#666546}
    
    Former-commit-id: 0332cdde930391d891b4e6d0baf100619dae6639

--- gpu/ipc/service/gpu_watchdog_thread.cc
+++ gpu/ipc/service/gpu_watchdog_thread.cc
@@ -4,31 +4,20 @@
 
 #include "gpu/ipc/service/gpu_watchdog_thread.h"
 
-#include <errno.h>
-#include <stdint.h>
-
 #include "base/bind.h"
 #include "base/bind_helpers.h"
-#include "base/command_line.h"
-#include "base/compiler_specific.h"
 #include "base/debug/alias.h"
 #include "base/files/file_util.h"
 #include "base/format_macros.h"
-#include "base/location.h"
-#include "base/memory/ptr_util.h"
 #include "base/message_loop/message_loop_current.h"
 #include "base/power_monitor/power_monitor.h"
-#include "base/process/process.h"
 #include "base/single_thread_task_runner.h"
-#include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/system/sys_info.h"
-#include "base/threading/platform_thread.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
 #include "gpu/config/gpu_crash_keys.h"
-#include "gpu/config/gpu_finch_features.h"
 
 #if defined(OS_WIN)
 #include <windows.h>
