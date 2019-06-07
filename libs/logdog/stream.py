96f125709acfd0b48fc1e5dae7d6ea42291726ac
Ben Pastene <bpastene@chromium.org>
2019-06-07 18:02:33 +0000

    Revert "Fix logdog client library on windows."
    This reverts commit 90b6d0451314bb57c1e5e45f924478cc34c7f3cc.
    
    Reason for revert: Suspected to be causing frozen windows builds
    See crbug.com/971224#c7
    
    Original change's description:
    > Fix logdog client library on windows.
    > 
    > LOGDOG_STREAM_SERVER_PATH is set to e.g. `net.pipe:LUCILogDogKitchen_1176`
    > on bots, but the file to open is '\\.\pipe\LUCILogDogKitchen_1176'.
    > 
    > Additionally, specify no-buffering and '+' mode to prevent python from
    > hanging onto bytes it shouldn't keep and allow the pipe to be re-opened
    > for multiple streams in the same python process.
    > 
    > R=​tandrii@chromium.org
    > 
    > Bug: 959887
    > Change-Id: Iff15fb9de9e39552fc7b063c66e6fbeb6f677d61
    > Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/1642342
    > Auto-Submit: Robbie Iannucci <iannucci@chromium.org>
    > Commit-Queue: Andrii Shyshkalov <tandrii@chromium.org>
    > Reviewed-by: Andrii Shyshkalov <tandrii@chromium.org>
    
    TBR=iannucci@chromium.org,tandrii@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 959887, 971224
    Change-Id: Ib84744a6c370916199cce3f3f9e1e0e06defeba0
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/1650143
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Cr-Mirrored-From: https://chromium.googlesource.com/infra/luci/luci-py
    Cr-Mirrored-Commit: 2f8e5320de58739c4b8a5a6606746e377f148416

--- libs/logdog/stream.py
+++ libs/logdog/stream.py
@@ -528,14 +528,14 @@ class _NamedPipeStreamClient(StreamClient):
       name (str): The name of the Windows named pipe to use (e.g., "\\.\name")
     """
     super(_NamedPipeStreamClient, self).__init__(**kwargs)
-    self._name = '\\\\.\\pipe\\' + name
+    self._name = name
 
   @classmethod
   def _create(cls, value, **kwargs):
     return cls(value, **kwargs)
 
   def _connect_raw(self):
-    return open(self._name, 'wb+', buffering=0)
+    return open(self._name, 'wb')
 
 _default_registry.register_protocol('net.pipe', _NamedPipeStreamClient)
 
