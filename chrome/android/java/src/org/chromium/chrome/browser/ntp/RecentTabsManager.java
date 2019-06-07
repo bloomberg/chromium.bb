00a4ed56f73f7a1ef74255b38f671af6975c710b
Victor Hugo Vianna Silva <victorvianna@google.com>
2019-06-07 11:00:14 +0000

    Integrate android to FaviconRequestHandler
    (cherry picked from commit aecb3fcb81c9fb488b1fd0a80ac2b16e264ad5bd [formerly b65dd5ab836093ed539f659a890380e7ea9a851d])
    
    Bug: 955475
    Change-Id: I8a393c33dabe8a974acee4cf27acdc7f8a08c4d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1634695
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Original-Commit-Position: refs/heads/master@{#665846}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1648232
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3809@{#122}
    Cr-Branched-From: ce45438777a60f42f88228700b3968406c6404d8 [formerly d82dec1a818f378c464ba307ddd9c92133eac355]-refs/heads/master@{#665002}
    
    Former-commit-id: 34b3a3b4fee0bbbf1896a4ca9c6fe844f66a77d3

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/RecentTabsManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/RecentTabsManager.java
@@ -5,7 +5,6 @@
 package org.chromium.chrome.browser.ntp;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.support.annotation.IntDef;
 
 import org.chromium.base.VisibleForTesting;
@@ -248,13 +247,16 @@ public class RecentTabsManager implements AndroidSyncSettingsObserver, SignInSta
     }
 
     /**
-     * Returns a 16x16 favicon for a given synced url.
+     * Returns a favicon for a given foreign url.
      *
      * @param url The url to fetch the favicon for.
-     * @return 16x16 favicon or null if favicon unavailable.
+     * @param size the desired favicon size.
+     * @param faviconCallback the callback to be invoked when the favicon is available.
+     * @return favicon or null if favicon unavailable.
      */
-    public Bitmap getSyncedFaviconImageForURL(String url) {
-        return mFaviconHelper.getSyncedFaviconImageForURL(mProfile, url);
+    public boolean getForeignFaviconForUrl(
+            String url, int size, FaviconImageCallback faviconCallback) {
+        return mFaviconHelper.getForeignFaviconImageForURL(mProfile, url, size, faviconCallback);
     }
 
     /**
