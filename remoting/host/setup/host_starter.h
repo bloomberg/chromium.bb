0b631163cedbbb59fc9a240e110705d8eec80d08
Lambros Lambrou <lambroslambrou@chromium.org>
2019-06-10 18:19:49 +0000

    Use correct OAuth token in remoting_start_host.
    Calls to the Directory API (for RegisterHost/DeleteHost) should use the
    original OAuth token from the headless setup page, not the newly-minted
    service-account token for the new host. These tokens have different
    scopes, so this CL ensures that the correct token is used for the
    DeleteHost request.
    
    (cherry picked from commit 746162a67c29888be73827ca02dac8798b7c4bcb [formerly 6fb4a12eba52f2394b712007bf4f79b58cc985a1])
    
    Bug: 968326
    Change-Id: I255a46fb2cc8780bb7f07378d7808feaf4c87f20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1648711
    Auto-Submit: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#666892}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1650092
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3809@{#189}
    Cr-Branched-From: ce45438777a60f42f88228700b3968406c6404d8 [formerly d82dec1a818f378c464ba307ddd9c92133eac355]-refs/heads/master@{#665002}
    
    Former-commit-id: 1c67303cac50830776833130fad90a4a8b65c5b9

--- remoting/host/setup/host_starter.h
+++ remoting/host/setup/host_starter.h
@@ -72,6 +72,16 @@ class HostStarter : public gaia::GaiaOAuthClient::Delegate,
   void OnNetworkError(int response_code) override;
 
  private:
+  // GetTokensFromAuthCode() is used for getting an access token for the
+  // Directory API (to register/unregister a new host). It is also used for
+  // getting access+refresh tokens for the new host (for getting the robot
+  // email and for writing the new config file).
+  enum PendingGetTokensRequest {
+    GET_TOKENS_NONE,
+    GET_TOKENS_DIRECTORY,
+    GET_TOKENS_HOST
+  };
+
   HostStarter(std::unique_ptr<gaia::GaiaOAuthClient> oauth_client,
               std::unique_ptr<remoting::ServiceClient> service_client,
               scoped_refptr<remoting::DaemonController> daemon_controller);
@@ -89,8 +99,9 @@ class HostStarter : public gaia::GaiaOAuthClient::Delegate,
   bool consent_to_data_collection_;
   CompletionCallback on_done_;
   scoped_refptr<base::SingleThreadTaskRunner> main_task_runner_;
-  std::string refresh_token_;
-  std::string access_token_;
+  std::string host_refresh_token_;
+  std::string host_access_token_;
+  std::string directory_access_token_;
   std::string host_owner_;
   std::string xmpp_login_;
   scoped_refptr<remoting::RsaKeyPair> key_pair_;
@@ -100,6 +111,8 @@ class HostStarter : public gaia::GaiaOAuthClient::Delegate,
   // is set and a network/OAuth error occurs during unregistration, this will
   // be logged, but the error will still be reported as START_ERROR.
   bool unregistering_host_;
+
+  PendingGetTokensRequest pending_get_tokens_ = GET_TOKENS_NONE;
 
   base::WeakPtr<HostStarter> weak_ptr_;
   base::WeakPtrFactory<HostStarter> weak_ptr_factory_;
