ccd66fe04373c1ae73144c09c6c4b2a6706e46fe
Kelvin Jiang <kelvinjiang@chromium.org>
2019-06-10 23:15:04 +0000

    [DNR] restrict DNR feedback permission test to trunk
    This should fix the bug linked below as the test fails on official
    builds due to the declarativeNetRequestFeedback permission being
    unavailable on stable.
    
    Bug: 971565
    Change-Id: I97d578df1652b10fe4608872363fffb976e1070e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1652426
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667802}
    
    Former-commit-id: 4501d157968c9b4de0ee9629d4ab78da85d9f75d

--- chrome/browser/extensions/permission_message_combinations_unittest.cc
+++ chrome/browser/extensions/permission_message_combinations_unittest.cc
@@ -10,7 +10,9 @@
 #include "base/test/values_test_util.h"
 #include "chrome/browser/extensions/test_extension_environment.h"
 #include "chrome/common/extensions/permissions/chrome_permission_message_provider.h"
+#include "components/version_info/version_info.h"
 #include "extensions/common/extension.h"
+#include "extensions/common/features/feature_channel.h"
 #include "extensions/common/features/simple_feature.h"
 #include "extensions/common/permissions/permission_message_test_util.h"
 #include "extensions/common/permissions/permissions_data.h"
@@ -1181,6 +1183,9 @@ TEST_F(PermissionMessageCombinationsUnittest, NewTabPagePermissionMessages) {
 
 TEST_F(PermissionMessageCombinationsUnittest,
        DeclarativeNetRequestFeedbackPermissionMessages) {
+  // Set the current channel to trunk.
+  ScopedCurrentChannel scoped_channel(version_info::Channel::UNKNOWN);
+
   CreateAndInstall(
       "{"
       "  'permissions': ["
