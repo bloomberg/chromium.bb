fa68bcf9d5d18d7efd38655fb877562237316e4f
Jeffrey Cohen <jeffreycohen@chromium.org>
2019-06-11 00:50:21 +0000

    [SendTabToSelf] create a modal presentation controller to manage showing modal
    Bug: 970284
    Change-Id: I89e2e7a4894cc54c179e5ea8a9df6b4c64ae758e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1648941
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#667810}
    
    Former-commit-id: c423846ad481d13900c6f7b38da271e9c06933bb

--- ios/chrome/browser/ui/send_tab_to_self/send_tab_to_self_coordinator.mm
+++ ios/chrome/browser/ui/send_tab_to_self/send_tab_to_self_coordinator.mm
@@ -5,21 +5,55 @@
 #import "ios/chrome/browser/ui/send_tab_to_self/send_tab_to_self_coordinator.h"
 
 #include "base/logging.h"
+#import "ios/chrome/browser/ui/send_tab_to_self/send_tab_to_self_modal_presentation_controller.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+@interface SendTabToSelfCoordinator () <UIViewControllerTransitioningDelegate>
+
+// The presentationController that shows the Send Tab To Self UI.
+@property(nonatomic, strong) SendTabToSelfModalPresentationController*
+    sendTabToSelfModalPresentationController;
+
+@end
 
 @implementation SendTabToSelfCoordinator
 
 #pragma mark - ChromeCoordinator Methods
 
 - (void)start {
-  NOTIMPLEMENTED();
+  UITableViewController* tableViewController =
+      [[UITableViewController alloc] initWithStyle:UITableViewStylePlain];
+  UINavigationController* navigationController = [[UINavigationController alloc]
+      initWithRootViewController:tableViewController];
+
+  navigationController.transitioningDelegate = self;
+  navigationController.modalPresentationStyle = UIModalPresentationCustom;
+  [self.baseViewController presentViewController:navigationController
+                                        animated:YES
+                                      completion:nil];
 }
 
 - (void)stop {
   NOTIMPLEMENTED();
+}
+
+#pragma mark-- UIViewControllerTransitioningDelegate
+
+- (UIPresentationController*)
+    presentationControllerForPresentedViewController:
+        (UIViewController*)presented
+                            presentingViewController:
+                                (UIViewController*)presenting
+                                sourceViewController:(UIViewController*)source {
+  SendTabToSelfModalPresentationController* presentationController =
+      [[SendTabToSelfModalPresentationController alloc]
+          initWithPresentedViewController:presented
+                 presentingViewController:presenting];
+  // TODO(crbug.com/970284) flesh out presentationController.
+  return presentationController;
 }
 
 @end
