f3cf17d05b229e16079ce53f0100fca0034303fa
Jordan Bayles <jophba@chromium.org>
2019-06-11 15:29:43 -0700

    Add platform power save API
    This patch is an interface-only change, adding the API surface for power
    wait locking/save blocking. Basing somewhat on Chromium's
    services/device/wake_lock/power_save_blocker/power_save_blocker.h, with
    clearer naming and decoupling from the power manager.
    
    Change-Id: Ifb7b3c6d2d32ff940304dfc144df158df5871049
    Reviewed-on: https://chromium-review.googlesource.com/c/openscreen/+/1653724
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Max Yakimakha <yakimakha@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>

---
+++ platform/api/scoped_wake_lock.cc
@@ -0,0 +1,11 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/api/scoped_wake_lock.h"
+
+namespace openscreen {
+namespace platform {
+ScopedWakeLock::~ScopedWakeLock() = default;
+}  // namespace platform
+}  // namespace openscreen
