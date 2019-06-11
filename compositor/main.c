814335821eb0fb5e74ec031c0cf68399e1dd370a
Daniel Stone <daniels@collabora.com>
2019-06-11 11:32:14 +0100

    weston: Properly test for output-creation failure
    We were testing the wrong variable to see if output creation had failed:
    instead of testing the return of the function we'd just called, we were
    testing something we'd already checked earlier.
    
    Signed-off-by: Daniel Stone <daniels@collabora.com>

--- compositor/main.c
+++ compositor/main.c
@@ -1818,7 +1818,7 @@ wet_layoutput_create_output(struct wet_layoutput *lo, const char *name)
 	output->output =
 		weston_compositor_create_output(lo->compositor->compositor,
 						name);
-	if (!output) {
+	if (!output->output) {
 		free(output);
 		return NULL;
 	}
