c2467077b93523c9ab2b8a7548145201dc891328
Joshua Peraza <jperaza@chromium.org>
2019-06-11 11:48:14 -0700

    Enable truncation of module ranges
    ELF modules are loaded in memory in several, possibly discontiguous,
    segments. If the holes between segments are large enough, other things,
    possibly other ELF modules may be mapped in that space. Crashpad
    records the range of modules as the base address of the lowest mapped
    segment to the high address of the highest mapped segment. This means
    that when one module is mapped into a hole in another, it appears to
    the Breakpad processor as overlapping modules. Module ranges are
    relevant to the Breakpad processor during stackwalking for identifying
    which module a particular program counter belongs to (i.e. mapping the
    address to a module's text segment). This patch addresses this issue of
    overlapping modules by truncating the range of the module with the
    lower base address. A typical module's text segment is the first loaded
    segment which would leave the text segment range unaffected. Module
    producers can restrict the size of holes in their ELF modules with the
    flag "-Wl,-z,max-page-size=4096", preventing other modules from being
    mapped in their address range.
    
    Properly contemplating ELF module address ranges would require
    extensions to the minidump format to encode any holes.
    crbug.com/crashpad/298
    
    This patch also renames the concept of "shrinking down" (which
    truncated the upper of two overlapping ranges) to "truncate upper".
    
    Change-Id: I4599201f1e43918db036c390961f8b39e3af1849
    Reviewed-on: https://chromium-review.googlesource.com/c/breakpad/breakpad/+/1646932
    Reviewed-by: Mark Mentovai <mark@chromium.org>

--- src/processor/minidump.cc
+++ src/processor/minidump.cc
@@ -2667,7 +2667,14 @@ MinidumpModuleList::MinidumpModuleList(Minidump* minidump)
       range_map_(new RangeMap<uint64_t, unsigned int>()),
       modules_(NULL),
       module_count_(0) {
-  range_map_->SetEnableShrinkDown(minidump_->IsAndroid());
+  MDOSPlatform platform;
+  if (minidump_->GetPlatform(&platform)) {
+    if (platform == MD_OS_ANDROID) {
+      range_map_->SetMergeStrategy(MergeRangeStrategy::kTruncateUpper);
+    } else if (platform == MD_OS_LINUX) {
+      range_map_->SetMergeStrategy(MergeRangeStrategy::kTruncateLower);
+    }
+  }
 }
 
 
@@ -2931,16 +2938,12 @@ const MinidumpModule* MinidumpModuleList::GetModuleAtIndex(
 
 
 const CodeModules* MinidumpModuleList::Copy() const {
-  return new BasicCodeModules(this);
+  return new BasicCodeModules(this, range_map_->GetMergeStrategy());
 }
 
 vector<linked_ptr<const CodeModule> >
 MinidumpModuleList::GetShrunkRangeModules() const {
   return vector<linked_ptr<const CodeModule> >();
-}
-
-bool MinidumpModuleList::IsModuleShrinkEnabled() const {
-  return range_map_->IsShrinkDownEnabled();
 }
 
 void MinidumpModuleList::Print() {
@@ -3870,7 +3873,7 @@ MinidumpUnloadedModuleList::MinidumpUnloadedModuleList(Minidump* minidump)
     range_map_(new RangeMap<uint64_t, unsigned int>()),
     unloaded_modules_(NULL),
     module_count_(0) {
-  range_map_->SetEnableShrinkDown(true);
+  range_map_->SetMergeStrategy(MergeRangeStrategy::kTruncateLower);
 }
 
 MinidumpUnloadedModuleList::~MinidumpUnloadedModuleList() {
@@ -4048,16 +4051,12 @@ MinidumpUnloadedModuleList::GetModuleAtIndex(
 }
 
 const CodeModules* MinidumpUnloadedModuleList::Copy() const {
-  return new BasicCodeModules(this);
+  return new BasicCodeModules(this, range_map_->GetMergeStrategy());
 }
 
 vector<linked_ptr<const CodeModule>>
 MinidumpUnloadedModuleList::GetShrunkRangeModules() const {
   return vector<linked_ptr<const CodeModule> >();
-}
-
-bool MinidumpUnloadedModuleList::IsModuleShrinkEnabled() const {
-  return range_map_->IsShrinkDownEnabled();
 }
 
 
@@ -5386,6 +5385,11 @@ MinidumpLinuxMapsList *Minidump::GetLinuxMapsList() {
 }
 
 bool Minidump::IsAndroid() {
+  MDOSPlatform platform;
+  return GetPlatform(&platform) && platform == MD_OS_ANDROID;
+}
+
+bool Minidump::GetPlatform(MDOSPlatform* platform) {
   // Save the current stream position
   off_t saved_position = Tell();
   if (saved_position == -1) {
@@ -5400,7 +5404,11 @@ bool Minidump::IsAndroid() {
     return false;
   }
 
-  return system_info && system_info->platform_id == MD_OS_ANDROID;
+  if (!system_info) {
+    return false;
+  }
+  *platform = static_cast<MDOSPlatform>(system_info->platform_id);
+  return true;
 }
 
 MinidumpCrashpadInfo* Minidump::GetCrashpadInfo() {
--- src/processor/range_map_truncate_lower_unittest.cc
+++ src/processor/range_map_truncate_lower_unittest.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2016, Google Inc.
+// Copyright (c) 2019, Google Inc.
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -27,11 +27,6 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
 
-// range_map_shrink_down_unittest.cc: Unit tests for RangeMap that specifically
-// test shrink down when ranges overlap.
-//
-// Author: Ivan Penkov
-
 #include <limits.h>
 #include <stdio.h>
 
@@ -45,8 +40,9 @@
 namespace {
 
 using google_breakpad::linked_ptr;
-using google_breakpad::scoped_ptr;
+using google_breakpad::MergeRangeStrategy;
 using google_breakpad::RangeMap;
+using google_breakpad::scoped_ptr;
 
 // A CountedObject holds an int.  A global (not thread safe!) count of
 // allocated CountedObjects is maintained to help test memory management.
@@ -69,47 +65,47 @@ typedef int AddressType;
 typedef RangeMap<AddressType, linked_ptr<CountedObject>> TestMap;
 
 // Same range cannot be stored wice.
-TEST(RangeMap, TestShinkDown_SameRange) {
+TEST(RangeMapTruncateLower, SameRange) {
   TestMap range_map;
-  range_map.SetEnableShrinkDown(true);
+  range_map.SetMergeStrategy(MergeRangeStrategy::kTruncateLower);
   linked_ptr<CountedObject> object_1(new CountedObject(1));
-  EXPECT_TRUE(range_map.StoreRange(0 /* base address */, 100 /* size */,
-                                   object_1));
+  EXPECT_TRUE(
+      range_map.StoreRange(0 /* base address */, 100 /* size */, object_1));
 
   // Same range cannot be stored wice.
   linked_ptr<CountedObject> object_2(new CountedObject(2));
-  EXPECT_FALSE(range_map.StoreRange(0 /* base address */, 100 /* size */,
-                                    object_2));
+  EXPECT_FALSE(
+      range_map.StoreRange(0 /* base address */, 100 /* size */, object_2));
 }
 
 // If a range is completely contained by another range, then the larger range
-// should be shrinked down.
-TEST(RangeMap, TestShinkDown_CompletelyContained) {
+// should be truncated.
+TEST(RangeMapTruncateLower, CompletelyContained) {
   TestMap range_map;
-  range_map.SetEnableShrinkDown(true);
+  range_map.SetMergeStrategy(MergeRangeStrategy::kTruncateLower);
   // Larger range is added first.
   linked_ptr<CountedObject> object_1(new CountedObject(1));
-  EXPECT_TRUE(range_map.StoreRange(0 /* base address */, 100 /* size */,
-                                   object_1));
+  EXPECT_TRUE(
+      range_map.StoreRange(0 /* base address */, 100 /* size */, object_1));
   // Smaller (contained) range is added second.
   linked_ptr<CountedObject> object_2(new CountedObject(2));
-  EXPECT_TRUE(range_map.StoreRange(10 /* base address */, 80 /* size */,
-                                   object_2));
+  EXPECT_TRUE(
+      range_map.StoreRange(10 /* base address */, 80 /* size */, object_2));
   linked_ptr<CountedObject> object;
   AddressType retrieved_base = AddressType();
   AddressType retrieved_delta = AddressType();
   AddressType retrieved_size = AddressType();
   // The first range contains the second, so the first range should have been
-  // shrunk to [90, 99].  Range [0, 9] should be free.
-  EXPECT_FALSE(range_map.RetrieveRange(0, &object, &retrieved_base,
+  // shrunk to [0, 10].  Range [90, 99] should be free.
+  EXPECT_FALSE(range_map.RetrieveRange(90, &object, &retrieved_base,
                                        &retrieved_delta, &retrieved_size));
-  EXPECT_FALSE(range_map.RetrieveRange(9, &object, &retrieved_base,
+  EXPECT_FALSE(range_map.RetrieveRange(99, &object, &retrieved_base,
                                        &retrieved_delta, &retrieved_size));
-  EXPECT_TRUE(range_map.RetrieveRange(90, &object, &retrieved_base,
+  EXPECT_TRUE(range_map.RetrieveRange(9, &object, &retrieved_base,
                                       &retrieved_delta, &retrieved_size));
   EXPECT_EQ(1, object->id());
-  EXPECT_EQ(90, retrieved_base);
-  EXPECT_EQ(90, retrieved_delta);
+  EXPECT_EQ(0, retrieved_base);
+  EXPECT_EQ(0, retrieved_delta);
   EXPECT_EQ(10, retrieved_size);
   // Validate the properties of the smaller range (should be untouched).
   EXPECT_TRUE(range_map.RetrieveRange(10, &object, &retrieved_base,
@@ -121,32 +117,32 @@ TEST(RangeMap, TestShinkDown_CompletelyContained) {
 }
 
 // Same as the previous test, however the larger range is added second.
-TEST(RangeMap, TestShinkDown_CompletelyContained_LargerAddedSecond) {
+TEST(RangeMapTruncateLower, CompletelyContained_LargerAddedSecond) {
   TestMap range_map;
-  range_map.SetEnableShrinkDown(true);
+  range_map.SetMergeStrategy(MergeRangeStrategy::kTruncateLower);
   // Smaller (contained) range is added first.
   linked_ptr<CountedObject> object_1(new CountedObject(1));
-  EXPECT_TRUE(range_map.StoreRange(10 /* base address */, 80 /* size */,
-                                   object_1));
+  EXPECT_TRUE(
+      range_map.StoreRange(10 /* base address */, 80 /* size */, object_1));
   // Larger range is added second.
   linked_ptr<CountedObject> object_2(new CountedObject(2));
-  EXPECT_TRUE(range_map.StoreRange(0 /* base address */, 100 /* size */,
-                                   object_2));
+  EXPECT_TRUE(
+      range_map.StoreRange(0 /* base address */, 100 /* size */, object_2));
   linked_ptr<CountedObject> object;
   AddressType retrieved_base = AddressType();
   AddressType retrieved_delta = AddressType();
   AddressType retrieved_size = AddressType();
   // The second range contains the first, so the second range should have been
-  // shrunk to [90, 99].  Range [0, 9] should be free.
-  EXPECT_FALSE(range_map.RetrieveRange(0, &object, &retrieved_base,
+  // truncated to [0, 9].  Range [90, 99] should be free.
+  EXPECT_FALSE(range_map.RetrieveRange(90, &object, &retrieved_base,
                                        &retrieved_delta, &retrieved_size));
-  EXPECT_FALSE(range_map.RetrieveRange(9, &object, &retrieved_base,
+  EXPECT_FALSE(range_map.RetrieveRange(99, &object, &retrieved_base,
                                        &retrieved_delta, &retrieved_size));
-  EXPECT_TRUE(range_map.RetrieveRange(90, &object, &retrieved_base,
+  EXPECT_TRUE(range_map.RetrieveRange(9, &object, &retrieved_base,
                                       &retrieved_delta, &retrieved_size));
   EXPECT_EQ(2, object->id());
-  EXPECT_EQ(90, retrieved_base);
-  EXPECT_EQ(90, retrieved_delta);
+  EXPECT_EQ(0, retrieved_base);
+  EXPECT_EQ(0, retrieved_delta);
   EXPECT_EQ(10, retrieved_size);
   // Validate the properties of the smaller range (should be untouched).
   EXPECT_TRUE(range_map.RetrieveRange(10, &object, &retrieved_base,
@@ -157,138 +153,135 @@ TEST(RangeMap, TestShinkDown_CompletelyContained_LargerAddedSecond) {
   EXPECT_EQ(80, retrieved_size);
 }
 
-TEST(RangeMap, TestShinkDown_PartialOverlap_AtBeginning) {
+TEST(RangeMapTruncateLower, PartialOverlap_AtBeginning) {
   TestMap range_map;
-  range_map.SetEnableShrinkDown(true);
+  range_map.SetMergeStrategy(MergeRangeStrategy::kTruncateLower);
   linked_ptr<CountedObject> object_1(new CountedObject(1));
-  EXPECT_TRUE(range_map.StoreRange(0 /* base address */, 100 /* size */,
-                                   object_1));
+  EXPECT_TRUE(
+      range_map.StoreRange(0 /* base address */, 100 /* size */, object_1));
 
   // Partial overlap at the beginning of the new range.
   linked_ptr<CountedObject> object_2(new CountedObject(2));
-  EXPECT_TRUE(range_map.StoreRange(90 /* base address */, 110 /* size */,
-                                   object_2));
+  EXPECT_TRUE(
+      range_map.StoreRange(90 /* base address */, 110 /* size */, object_2));
 
   linked_ptr<CountedObject> object;
   AddressType retrieved_base = AddressType();
   AddressType retrieved_delta = AddressType();
   AddressType retrieved_size = AddressType();
-  // The second range is supposed to be shrunk down so the following address
-  // should resize in the first range.
+  // The first range should be truncated, so 99 should address the second range.
   EXPECT_TRUE(range_map.RetrieveRange(99, &object, &retrieved_base,
+                                      &retrieved_delta, &retrieved_size));
+  EXPECT_EQ(2, object->id());
+  EXPECT_EQ(90, retrieved_base);
+  EXPECT_EQ(0, retrieved_delta);
+  EXPECT_EQ(110, retrieved_size);
+  // Validate the properties of the truncated range.
+  EXPECT_TRUE(range_map.RetrieveRange(89, &object, &retrieved_base,
                                       &retrieved_delta, &retrieved_size));
   EXPECT_EQ(1, object->id());
   EXPECT_EQ(0, retrieved_base);
   EXPECT_EQ(0, retrieved_delta);
-  EXPECT_EQ(100, retrieved_size);
-  // Validate the properties of the shrunk down range.
-  EXPECT_TRUE(range_map.RetrieveRange(100, &object, &retrieved_base,
-                                      &retrieved_delta, &retrieved_size));
-  EXPECT_EQ(2, object->id());
-  EXPECT_EQ(100, retrieved_base);
-  EXPECT_EQ(10, retrieved_delta);
-  EXPECT_EQ(100, retrieved_size);
+  EXPECT_EQ(90, retrieved_size);
 }
 
-TEST(RangeMap, TestShinkDown_PartialOverlap_AtEnd) {
+TEST(RangeMapTruncateLower, PartialOverlap_AtEnd) {
   TestMap range_map;
-  range_map.SetEnableShrinkDown(true);
+  range_map.SetMergeStrategy(MergeRangeStrategy::kTruncateLower);
   linked_ptr<CountedObject> object_1(new CountedObject(1));
-  EXPECT_TRUE(range_map.StoreRange(50 /* base address */, 50 /* size */,
-                                   object_1));
+  EXPECT_TRUE(
+      range_map.StoreRange(50 /* base address */, 50 /* size */, object_1));
 
   // Partial overlap at the end of the new range.
   linked_ptr<CountedObject> object_2(new CountedObject(2));
-  EXPECT_TRUE(range_map.StoreRange(0 /* base address */, 70 /* size */,
-                                   object_2));
+  EXPECT_TRUE(
+      range_map.StoreRange(0 /* base address */, 70 /* size */, object_2));
 
   linked_ptr<CountedObject> object;
   AddressType retrieved_base = AddressType();
   AddressType retrieved_delta = AddressType();
   AddressType retrieved_size = AddressType();
-  // The first range is supposed to be shrunk down so the following address
-  // should resize in the first range.
+  // The second range should be truncated so 69 addresses the first range.
   EXPECT_TRUE(range_map.RetrieveRange(69, &object, &retrieved_base,
+                                      &retrieved_delta, &retrieved_size));
+  EXPECT_EQ(1, object->id());
+  EXPECT_EQ(50, retrieved_base);
+  EXPECT_EQ(0, retrieved_delta);
+  EXPECT_EQ(50, retrieved_size);
+  // Validate the properties of the truncated range.
+  EXPECT_TRUE(range_map.RetrieveRange(49, &object, &retrieved_base,
                                       &retrieved_delta, &retrieved_size));
   EXPECT_EQ(2, object->id());
   EXPECT_EQ(0, retrieved_base);
   EXPECT_EQ(0, retrieved_delta);
-  EXPECT_EQ(70, retrieved_size);
-  // Validate the properties of the shrunk down range.
-  EXPECT_TRUE(range_map.RetrieveRange(70, &object, &retrieved_base,
-                                      &retrieved_delta, &retrieved_size));
-  EXPECT_EQ(1, object->id());
-  EXPECT_EQ(70, retrieved_base);
-  EXPECT_EQ(20, retrieved_delta);
-  EXPECT_EQ(30, retrieved_size);
+  EXPECT_EQ(50, retrieved_size);
 }
 
 // A new range is overlapped at both ends.  The new range and the range
-// that overlaps at the end should be shrink.  The range that overlaps at the
-// beginning should be left untouched.
-TEST(RangeMap, TestShinkDown_OverlapAtBothEnds) {
+// that overlaps at the beginning should be truncated.  The range that overlaps
+// at the end should be left untouched.
+TEST(RangeMapTruncateLower, OverlapAtBothEnds) {
   TestMap range_map;
-  range_map.SetEnableShrinkDown(true);
+  range_map.SetMergeStrategy(MergeRangeStrategy::kTruncateLower);
   // This should overlap object_3 at the beginning.
   linked_ptr<CountedObject> object_1(new CountedObject(1));
-  EXPECT_TRUE(range_map.StoreRange(0 /* base address */, 100 /* size */,
-                                   object_1));
+  EXPECT_TRUE(
+      range_map.StoreRange(0 /* base address */, 100 /* size */, object_1));
 
   // This should overlap object_3 at the end.
   linked_ptr<CountedObject> object_2(new CountedObject(2));
-  EXPECT_TRUE(range_map.StoreRange(100 /* base address */, 100 /* size */,
-                                   object_2));
+  EXPECT_TRUE(
+      range_map.StoreRange(100 /* base address */, 100 /* size */, object_2));
 
   // This should be overlapped on both ends by object_1 and object_2.
   linked_ptr<CountedObject> object_3(new CountedObject(3));
-  EXPECT_TRUE(range_map.StoreRange(50 /* base address */, 100 /* size */,
-                                   object_3));
+  EXPECT_TRUE(
+      range_map.StoreRange(50 /* base address */, 100 /* size */, object_3));
 
   linked_ptr<CountedObject> object;
   AddressType retrieved_base = AddressType();
   AddressType retrieved_delta = AddressType();
   AddressType retrieved_size = AddressType();
-  // The first range should be intact.
+  // The first range should be truncated.
   EXPECT_TRUE(range_map.RetrieveRange(0, &object, &retrieved_base,
                                       &retrieved_delta, &retrieved_size));
   EXPECT_EQ(1, object->id());
   EXPECT_EQ(0, retrieved_base);
   EXPECT_EQ(0, retrieved_delta);
-  EXPECT_EQ(100, retrieved_size);
-  // The second range should be shrunk down by 50.
+  EXPECT_EQ(50, retrieved_size);
+  // The second range should be intact.
   EXPECT_TRUE(range_map.RetrieveRange(150, &object, &retrieved_base,
                                       &retrieved_delta, &retrieved_size));
   EXPECT_EQ(2, object->id());
-  EXPECT_EQ(150, retrieved_base);
-  EXPECT_EQ(50, retrieved_delta);
-  EXPECT_EQ(50, retrieved_size);
-  // The third range (in the middle) should be shrunk down by 50.
-  EXPECT_TRUE(range_map.RetrieveRange(100, &object, &retrieved_base,
+  EXPECT_EQ(100, retrieved_base);
+  EXPECT_EQ(0, retrieved_delta);
+  EXPECT_EQ(100, retrieved_size);
+  // The third range (in the middle) should be truncated.
+  EXPECT_TRUE(range_map.RetrieveRange(99, &object, &retrieved_base,
                                       &retrieved_delta, &retrieved_size));
   EXPECT_EQ(3, object->id());
-  EXPECT_EQ(100, retrieved_base);
-  EXPECT_EQ(50, retrieved_delta);
+  EXPECT_EQ(50, retrieved_base);
+  EXPECT_EQ(0, retrieved_delta);
   EXPECT_EQ(50, retrieved_size);
 }
 
-TEST(RangeMap, TestShinkDown_MultipleConflicts) {
+TEST(RangeMapTruncateLower, MultipleConflicts) {
   TestMap range_map;
-  range_map.SetEnableShrinkDown(true);
+  range_map.SetMergeStrategy(MergeRangeStrategy::kTruncateLower);
   // This should overlap with object_3.
   linked_ptr<CountedObject> object_1(new CountedObject(1));
-  EXPECT_TRUE(range_map.StoreRange(10 /* base address */, 90 /* size */,
-                                   object_1));
+  EXPECT_TRUE(
+      range_map.StoreRange(10 /* base address */, 90 /* size */, object_1));
 
   // This should also overlap with object_3 but after object_1.
   linked_ptr<CountedObject> object_2(new CountedObject(2));
-  EXPECT_TRUE(range_map.StoreRange(100 /* base address */, 100 /* size */,
-                                   object_2));
+  EXPECT_TRUE(
+      range_map.StoreRange(100 /* base address */, 100 /* size */, object_2));
 
-  // This should be overlapped on both object_1 and object_2.  Since
-  // object_3 ends with the higher address it must be shrunk.
+  // This should be overlapped on both object_1 and object_2.
   linked_ptr<CountedObject> object_3(new CountedObject(3));
-  EXPECT_TRUE(range_map.StoreRange(0 /* base address */, 300 /* size */,
-                                   object_3));
+  EXPECT_TRUE(
+      range_map.StoreRange(0 /* base address */, 300 /* size */, object_3));
 
   linked_ptr<CountedObject> object;
   AddressType retrieved_base = AddressType();
@@ -308,29 +301,29 @@ TEST(RangeMap, TestShinkDown_MultipleConflicts) {
   EXPECT_EQ(100, retrieved_base);
   EXPECT_EQ(0, retrieved_delta);
   EXPECT_EQ(100, retrieved_size);
-  // The third range should be shrunk down by 200.
-  EXPECT_TRUE(range_map.RetrieveRange(299, &object, &retrieved_base,
+  // The third range should be truncated.
+  EXPECT_TRUE(range_map.RetrieveRange(9, &object, &retrieved_base,
                                       &retrieved_delta, &retrieved_size));
   EXPECT_EQ(3, object->id());
-  EXPECT_EQ(200, retrieved_base);
-  EXPECT_EQ(200, retrieved_delta);
-  EXPECT_EQ(100, retrieved_size);
+  EXPECT_EQ(0, retrieved_base);
+  EXPECT_EQ(0, retrieved_delta);
+  EXPECT_EQ(10, retrieved_size);
 }
 
 // Adding two ranges without overlap should succeed and the ranges should
 // be left intact.
-TEST(RangeMap, TestShinkDown_NoConflicts) {
+TEST(RangeMapTruncateLower, NoConflicts) {
   TestMap range_map;
-  range_map.SetEnableShrinkDown(true);
+  range_map.SetMergeStrategy(MergeRangeStrategy::kTruncateLower);
   // Adding range 1.
   linked_ptr<CountedObject> object_1(new CountedObject(1));
-  EXPECT_TRUE(range_map.StoreRange(10 /* base address */, 90 /* size */,
-                                   object_1));
+  EXPECT_TRUE(
+      range_map.StoreRange(10 /* base address */, 90 /* size */, object_1));
 
   // Adding range 2 - no overlap with range 1.
   linked_ptr<CountedObject> object_2(new CountedObject(2));
-  EXPECT_TRUE(range_map.StoreRange(110 /* base address */, 90 /* size */,
-                                   object_2));
+  EXPECT_TRUE(
+      range_map.StoreRange(110 /* base address */, 90 /* size */, object_2));
 
   linked_ptr<CountedObject> object;
   AddressType retrieved_base = AddressType();
