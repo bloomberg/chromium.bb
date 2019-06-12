d37f2ed101541afddd230b8c265b44b535f617cb
Mehran Mahmoudi <mahmoudi@chromium.org>
2019-06-12 19:52:31 +0000

    [Touchless] Add splash screen for DinoActivity
    Mock: https://docs.google.com/presentation/d/1X29NR8smO_P3cNJCGHpASPOOyj20IpR4ztHPNGWFtFo/edit?hl=en#slide=id.g5b4b9a286c_0_24
    Implementation: https://drive.google.com/file/d/1lDS_tUembnk8qIu0VpJhLui_rO5ujFep/view?usp=sharing
    
    Bug: 957992
    Change-Id: I7764694b60ba15adcdf4f023a9c4d14f825423af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1647686
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Mehran Mahmoudi <mahmoudi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668529}
    
    Former-commit-id: 0513eb42308dec95662e04aced7c5bb14c81360c

--- chrome/android/touchless/java/src/org/chromium/chrome/browser/touchless/DinoActivity.java
+++ chrome/android/touchless/java/src/org/chromium/chrome/browser/touchless/DinoActivity.java
@@ -5,23 +5,51 @@
 package org.chromium.chrome.browser.touchless;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.SingleTabActivity;
 import org.chromium.chrome.browser.compositor.layouts.LayoutManager;
 import org.chromium.chrome.browser.fullscreen.ChromeFullscreenManager;
+import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabState;
 import org.chromium.chrome.browser.util.UrlConstants;
+import org.chromium.chrome.touchless.R;
 import org.chromium.content_public.browser.LoadUrlParams;
+import org.chromium.ui.UiUtils;
 import org.chromium.ui.base.PageTransition;
 
 /**
  * An Activity used to display the Dino Run game.
  */
 public class DinoActivity extends SingleTabActivity {
+    private View mSplashView;
+
+    @Override
+    protected void doLayoutInflation() {
+        super.doLayoutInflation();
+        mSplashView = LayoutInflater.from(this).inflate(R.layout.dino_splash_view, null);
+        mSplashView.addOnLayoutChangeListener(
+                (v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom) -> {
+                    ImageView dinoImage = v.findViewById(R.id.dino_animation_image);
+                    AnimationDrawable dinoAnimation = (AnimationDrawable) dinoImage.getDrawable();
+                    dinoAnimation.start();
+                });
+        ViewGroup coordinatorLayout = (ViewGroup) findViewById(R.id.coordinator);
+        CoordinatorLayout.LayoutParams layoutParams =
+                new CoordinatorLayout.LayoutParams(CoordinatorLayout.LayoutParams.MATCH_PARENT,
+                        CoordinatorLayout.LayoutParams.MATCH_PARENT);
+        mSplashView.setLayoutParams(layoutParams);
+        coordinatorLayout.addView(mSplashView);
+    }
+
     @Override
     public void finishNativeInitialization() {
         initializeCompositorContent(new LayoutManager(getCompositorViewHolder()), null /* urlBar */,
@@ -37,6 +65,12 @@ public class DinoActivity extends SingleTabActivity {
         loadUrlParams.setHasUserGesture(true);
         loadUrlParams.setTransitionType(PageTransition.LINK | PageTransition.FROM_API);
         tab.loadUrl(loadUrlParams);
+        tab.addObserver(new EmptyTabObserver() {
+            @Override
+            public void onPageLoadFinished(Tab tab, String url) {
+                UiUtils.removeViewFromParent(mSplashView);
+            }
+        });
         return tab;
     }
 
