633ef165310317b6cc614c91ea8d02a284a53eda
edchin <edchin@chromium.org>
2019-06-13 16:19:04 +0000

    M76 merge [ios] Fix crash when opening a non-supported URL bookmark
    This is an obscure crash in which the incognito BVC is destroyed when
    opening a non-supported URL bookmark in incognito. (See bug for repro
    steps).
    
    This CL ensures that the bubblePresenter is properly released when
    the browserState is reset in BVC and thereafter destroyed.
    
    The fix in https://crrev.com/c/1621311 is no longer necessary and is
    removed.
    
    TBR=eugenebut@chromium.org
    (cherry picked from commit e65bb47f746bbd0968fec8db32b0ac0ce8cd7a8c [formerly 84f24396fe399ed3eecd0b81b70446399fe2a37f])
    
    Bug: 966372
    Change-Id: I37a85a75cf1b7a6b0d6cada4df69938ffad4c8a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1650719
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Auto-Submit: edchin <edchin@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#668217}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1658509
    Cr-Commit-Position: refs/branch-heads/3809@{#289}
    Cr-Branched-From: ce45438777a60f42f88228700b3968406c6404d8 [formerly d82dec1a818f378c464ba307ddd9c92133eac355]-refs/heads/master@{#665002}
    
    Former-commit-id: a7b6954e4b7a311d6b815ca59f95621ff6a5892e

--- ios/chrome/browser/ui/browser_view/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view/browser_view_controller.mm
@@ -520,6 +520,9 @@ NSString* const kBrowserViewControllerSnackbarCategory =
 
   // Bridges C++ WebStateListObserver methods to this BrowserViewController.
   std::unique_ptr<WebStateListObserverBridge> _webStateListObserver;
+
+  // Presenter for in-product help bubbles.
+  BubblePresenter* _bubblePresenter;
 }
 
 // Activates/deactivates the object. This will enable/disable the ability for
@@ -1119,15 +1122,19 @@ NSString* const kBrowserViewControllerSnackbarCategory =
 }
 
 - (BubblePresenter*)bubblePresenter {
-  if (!_bubblePresenter) {
-    _bubblePresenter =
+  if (!_bubblePresenter && self.browserState) {
+    self.bubblePresenter =
         [[BubblePresenter alloc] initWithBrowserState:self.browserState
                                              delegate:self
                                    rootViewController:self];
-    _bubblePresenter.dispatcher = self.dispatcher;
-    self.popupMenuCoordinator.bubblePresenter = _bubblePresenter;
   }
   return _bubblePresenter;
+}
+
+- (void)setBubblePresenter:(BubblePresenter*)bubblePresenter {
+  _bubblePresenter = bubblePresenter;
+  _bubblePresenter.dispatcher = self.dispatcher;
+  self.popupMenuCoordinator.bubblePresenter = _bubblePresenter;
 }
 
 - (BOOL)isNTPActiveForCurrentWebState {
@@ -1447,6 +1454,8 @@ NSString* const kBrowserViewControllerSnackbarCategory =
   self.tabStripView = nil;
 
   _browserState = nullptr;
+  self.bubblePresenter = nil;
+
   [self.commandDispatcher stopDispatchingToTarget:self];
   self.commandDispatcher = nil;
 
@@ -1629,11 +1638,6 @@ NSString* const kBrowserViewControllerSnackbarCategory =
     // case, display the Long Press InProductHelp if needed.
     auto completion =
         ^(id<UIViewControllerTransitionCoordinatorContext> context) {
-          // Do not attempt to use the browserState if |-shutdown| was called
-          // during the BVC presentation animation. Attempting to present
-          // bubbles will crash since bubblePresenter requires a valid
-          // BrowserState.
-          if (!_isShutdown)
             [self.bubblePresenter presentLongPressBubbleIfEligible];
         };
 
