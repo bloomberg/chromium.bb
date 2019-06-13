34f136399b845001a6dd07fe716f50c0805a04b8
Scott Violet <sky@chromium.org>
2019-06-13 00:09:09 +0000

    chromeos: removes InputDeviceControllerClient usage in chrome
    InputDeviceControllerClient is going away (as are all the mojoms).
    Instead this code is going back to directly accessing ozone.
    
    BUG=964460
    TEST=none
    
    Change-Id: Ibfe55b727603cd17d05e1a53545f0dd619d00e1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1653040
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668620}
    
    Former-commit-id: 5d720cb87dd1dc50ba4e0b0a451dd5a73b0184db

--- chrome/browser/chromeos/system_logs/touch_log_source.cc
+++ chrome/browser/chromeos/system_logs/touch_log_source.cc
@@ -17,11 +17,10 @@
 #include "base/logging.h"
 #include "base/process/launch.h"
 #include "base/task/post_task.h"
-#include "chrome/browser/browser_process.h"
-#include "chrome/browser/browser_process_platform_part_chromeos.h"
 #include "content/public/browser/browser_thread.h"
-#include "services/ws/public/cpp/input_devices/input_device_controller_client.h"
 #include "ui/base/ui_base_features.h"
+#include "ui/ozone/public/input_controller.h"
+#include "ui/ozone/public/ozone_platform.h"
 
 using content::BrowserThread;
 
@@ -160,9 +159,9 @@ void OnStatusLogCollected(
 
   // Collect touch event logs.
   const base::FilePath kBaseLogPath(kTouchEventLogDir);
-  ws::InputDeviceControllerClient* input_device_controller_client =
-      g_browser_process->platform_part()->GetInputDeviceControllerClient();
-  input_device_controller_client->GetTouchEventLog(
+  ui::InputController* input_controller =
+      ui::OzonePlatform::GetInstance()->GetInputController();
+  input_controller->GetTouchEventLog(
       kBaseLogPath, base::BindOnce(&OnEventLogCollected, std::move(response),
                                    std::move(callback)));
 }
@@ -197,9 +196,9 @@ void TouchLogSource::Fetch(SysLogsSourceCallback callback) {
   CollectTouchHudDebugLog(response.get());
 
   // Collect touch device status logs.
-  ws::InputDeviceControllerClient* input_device_controller_client =
-      g_browser_process->platform_part()->GetInputDeviceControllerClient();
-  input_device_controller_client->GetTouchDeviceStatus(base::BindOnce(
+  ui::InputController* input_controller =
+      ui::OzonePlatform::GetInstance()->GetInputController();
+  input_controller->GetTouchDeviceStatus(base::BindOnce(
       &OnStatusLogCollected, std::move(response), std::move(callback)));
 }
 
