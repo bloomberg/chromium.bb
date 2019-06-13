600ee63c148af235903d6c9fe4b8cd7b33394ae2
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2019-06-13 07:15:36 +0000

    desktop-pwas: Reenable WebApp policy test
    The underlying code has changed a lot since the test was first written.
    So there is a chance the test is no longer flaky.
    
    Bug: 878797
    Change-Id: Ic31a7b4ad335a1748ac8c57eb9b601ad7dcc1548
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1651534
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#668729}
    
    Former-commit-id: 273c607460312fbfdfb950e27cfcf848a945dfc6

--- chrome/browser/policy/policy_browsertest.cc
+++ chrome/browser/policy/policy_browsertest.cc
@@ -6729,14 +6729,7 @@ class WebAppInstallForceListPolicyTest : public PolicyTest {
   DISALLOW_COPY_AND_ASSIGN(WebAppInstallForceListPolicyTest);
 };
 
-// TODO(crbug.com/878797): Flaky on windows
-#if defined(OS_WIN)
-#define MAYBE_StartUpInstallation DISABLED_StartUpInstallation
-#else
-#define MAYBE_StartUpInstallation StartUpInstallation
-#endif
-IN_PROC_BROWSER_TEST_F(WebAppInstallForceListPolicyTest,
-                       MAYBE_StartUpInstallation) {
+IN_PROC_BROWSER_TEST_F(WebAppInstallForceListPolicyTest, StartUpInstallation) {
   extensions::TestExtensionRegistryObserver observer(
       extensions::ExtensionRegistry::Get(browser()->profile()));
   const extensions::Extension* installed_extension =
