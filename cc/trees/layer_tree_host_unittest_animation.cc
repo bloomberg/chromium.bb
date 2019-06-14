326c1b762e949c08974dbbf395f0bb552b71e5f2
Robert Flack <flackr@chromium.org>
2019-06-14 17:41:30 +0000

    Fix flaky test LayerTreeHostAnimationTestAddKeyframeModelWithTimingFunction.
    Since the impl thread can start the second frame and the main thread may
    have committed that the animation is complete the keyframe model may not
    exist on the second impl frame for which AnimateLayers is called. Only
    test the assertions and end the test once.
    
    Bug: 974193
    Change-Id: I500f8691d22901640346c0fce1d7227cb4a5c19d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1660353
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669276}
    
    Former-commit-id: c565835c2c679efdd023f9d1fdf871bfb8c1af41

--- cc/trees/layer_tree_host_unittest_animation.cc
+++ cc/trees/layer_tree_host_unittest_animation.cc
@@ -369,6 +369,15 @@ class LayerTreeHostAnimationTestAddKeyframeModelWithTimingFunction
     if (host_impl->active_tree()->source_frame_number() != 0)
       return;
 
+    // The impl thread may start a second frame before the test finishes. This
+    // can lead to a race as if the main thread has committed a new sync tree
+    // the impl thread can now delete the KeyframeModel as the animation only
+    // lasts a single frame and no longer affects either tree. Only test the
+    // first frame the animation shows up for.
+    if (!first_animation_frame_)
+      return;
+    first_animation_frame_ = false;
+
     scoped_refptr<AnimationTimeline> timeline_impl =
         GetImplAnimationHost(host_impl)->GetTimelineById(timeline_id_);
     scoped_refptr<SingleKeyframeEffectAnimation> animation_child_impl =
@@ -397,10 +406,10 @@ class LayerTreeHostAnimationTestAddKeyframeModelWithTimingFunction
 
   FakeContentLayerClient client_;
   scoped_refptr<FakePictureLayer> picture_;
+  bool first_animation_frame_ = true;
 };
 
-// Flaky, see crbug.com/974193
-SINGLE_THREAD_TEST_F(
+SINGLE_AND_MULTI_THREAD_TEST_F(
     LayerTreeHostAnimationTestAddKeyframeModelWithTimingFunction);
 
 // Ensures that main thread animations have their start times synchronized with
