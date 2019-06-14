9af6efd0b1890ff5d2325939a35af695970f80db
Piotr Bialecki <bialpio@chromium.org>
2019-06-14 22:24:34 +0000

    Revert "Fix module install"
    This reverts commit 483a533a57ee54679a69085a94a4374435d7724e [formerly 8be5aea664dd5872079df13b36f34d9aa41a36cb].
    
    Reason for revert: The cherry-picked commit causes crashes on startup (assertion failure) when running on debug builds.
    
    26d54f26183a [formerly 801cc00b4ec4] Fix module install - crashes on startup
    51635afcfbd6 [formerly 05c5bcb74e2a] Do not block logon hours for AD accounts - works (parent of 26d54f26183a [formerly 801cc00b4ec4])
    
    Original change's description:
    > Fix module install
    > 
    > Previously, we created a SplitInstallManager _before_ calling
    > SplitCompat.install(-), which breaks the module install flow on Android
    > pre-Q.
    > 
    > (cherry picked from commit 26d54f26183ae6b6d9fdb7b9ced8108a5fc8b81e [formerly 801cc00b4ec48cfcfbbd48a441d59d48892f7c48])
    > 
    > Bug: b/133669491
    > Change-Id: I0b317732ed364a592047eeb7387c74dd1ea1b258
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1646903
    > Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    > Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    > Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    > Cr-Original-Commit-Position: refs/heads/master@{#668416}
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1658958
    > Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    > Cr-Commit-Position: refs/branch-heads/3809@{#295}
    > Cr-Branched-From: ce45438777a60f42f88228700b3968406c6404d8 [formerly d82dec1a818f378c464ba307ddd9c92133eac355]-refs/heads/master@{#665002}
    
    TBR=tiborg@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: b/133669491
    Change-Id: If3273a82806c5117611e56ce18c5de6961728b2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1661001
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3809@{#322}
    Cr-Branched-From: ce45438777a60f42f88228700b3968406c6404d8 [formerly d82dec1a818f378c464ba307ddd9c92133eac355]-refs/heads/master@{#665002}
    
    Former-commit-id: 288ef60d184f387611a9cda23f7df17320e8ac18

--- components/module_installer/android/java/src-impl/org/chromium/components/module_installer/PlayCoreModuleInstallerBackend.java
+++ components/module_installer/android/java/src-impl/org/chromium/components/module_installer/PlayCoreModuleInstallerBackend.java
@@ -91,8 +91,6 @@ import java.util.Set;
 
     /** Records via UMA all modules that have been requested and are currently installed. */
     public static void recordModuleAvailability() {
-        // MUST call init before creating a SplitInstallManager.
-        ModuleInstaller.init();
         SharedPreferences prefs = ContextUtils.getAppSharedPreferences();
         Set<String> requestedModules = new HashSet<>();
         requestedModules.addAll(
@@ -126,8 +124,6 @@ import java.util.Set;
 
     public PlayCoreModuleInstallerBackend(OnFinishedListener listener) {
         super(listener);
-        // MUST call init before creating a SplitInstallManager.
-        ModuleInstaller.init();
         mManager = SplitInstallManagerFactory.create(ContextUtils.getApplicationContext());
         mManager.registerListener(this);
     }
