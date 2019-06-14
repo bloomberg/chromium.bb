f859ed65c01a01e980eb3deaa455106d64676b08
Matthew Jones <mdjones@chromium.org>
2019-06-14 15:06:15 +0000

    Support ad block infobar for touchless
    TBR=mdjones@chromium.org
    
    (cherry picked from commit d4dc6a66c0533921fec3401a616b29cc99734076 [formerly f54b27814f5656232ecae5fc5bd5486963cd6a0f])
    
    Bug: 973550
    Change-Id: I65db611c07f035d657db1b54de12fe82df347762
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1656468
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#668559}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1660199
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3809@{#312}
    Cr-Branched-From: ce45438777a60f42f88228700b3968406c6404d8 [formerly d82dec1a818f378c464ba307ddd9c92133eac355]-refs/heads/master@{#665002}
    
    Former-commit-id: ae6615f33467a23a964c86c92916ff09c1361862

--- chrome/android/java/src/org/chromium/chrome/browser/infobar/AdsBlockedInfoBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/infobar/AdsBlockedInfoBar.java
@@ -4,6 +4,7 @@
 
 package org.chromium.chrome.browser.infobar;
 
+import android.content.res.Resources;
 import android.support.v7.widget.SwitchCompat;
 import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
@@ -12,11 +13,19 @@ import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 
+import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ResourceId;
+import org.chromium.chrome.browser.touchless.dialog.TouchlessDialogProperties;
+import org.chromium.chrome.browser.touchless.dialog.TouchlessDialogProperties.DialogListItemProperties;
+import org.chromium.chrome.browser.util.FeatureUtilities;
+import org.chromium.ui.modaldialog.ModalDialogProperties;
+import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.text.NoUnderlineClickableSpan;
 import org.chromium.ui.widget.ButtonCompat;
+
+import java.util.ArrayList;
 
 /**
  * This infobar appears when ads are being blocked on the page. This occurs after proceeding through
@@ -97,7 +106,7 @@ public class AdsBlockedInfoBar extends ConfirmInfoBar implements OnCheckedChange
         // If we aren't already showing the explanation, clicking the link should expand to show the
         // explanation. If we *are* already showing the explanation, clicking the link (which should
         // change to Learn more) should take us to the help page.
-        if (!mIsShowingExplanation) {
+        if (!mIsShowingExplanation && !FeatureUtilities.isNoTouchModeEnabled()) {
             mIsShowingExplanation = true;
             replaceView(createView());
         }
@@ -114,5 +123,53 @@ public class AdsBlockedInfoBar extends ConfirmInfoBar implements OnCheckedChange
         assert mButton != null;
         mButton.setText(isChecked ? mReloadButtonText : mOKButtonText);
         mReloadIsToggled = isChecked;
+    }
+
+    @Override
+    public boolean supportsTouchlessMode() {
+        return true;
+    }
+
+    @Override
+    public PropertyModel createModel() {
+        PropertyModel model = super.createModel();
+        Resources res = getContext().getResources();
+
+        model.set(ModalDialogProperties.TITLE, res.getString(R.string.blocked_ads_prompt_title));
+        model.set(ModalDialogProperties.MESSAGE, res.getString(R.string.intrusive_ads_information));
+
+        ArrayList<PropertyModel> options = new ArrayList<>();
+        options.add(new PropertyModel.Builder(DialogListItemProperties.ALL_KEYS)
+                            .with(DialogListItemProperties.TEXT, res.getString(R.string.learn_more))
+                            .with(DialogListItemProperties.CLICK_LISTENER, (v) -> onLinkClicked())
+                            .with(DialogListItemProperties.ICON,
+                                    ApiCompatibilityUtils.getDrawable(
+                                            res, R.drawable.ic_info_outline_grey))
+                            .build());
+        // TODO(973601): We should have a better string for "always allow"; at least one that is
+        //               specific to this feature.
+        options.add(
+                new PropertyModel.Builder(DialogListItemProperties.ALL_KEYS)
+                        .with(DialogListItemProperties.TEXT,
+                                res.getString(R.string.always_allow_redirects))
+                        .with(DialogListItemProperties.CLICK_LISTENER,
+                                (v) -> {
+                                    mReloadIsToggled = true;
+                                    onButtonClicked(true);
+                                })
+                        .with(DialogListItemProperties.ICON,
+                                ApiCompatibilityUtils.getDrawable(res, R.drawable.ic_check_circle))
+                        .build());
+
+        PropertyModel[] optionModels = new PropertyModel[options.size()];
+        options.toArray(optionModels);
+        model.set(TouchlessDialogProperties.LIST_MODELS, optionModels);
+
+        // The alt action matches cancel but with a different name.
+        model.get(TouchlessDialogProperties.ACTION_NAMES).alt = R.string.ok;
+        model.set(TouchlessDialogProperties.ALT_ACTION,
+                model.get(TouchlessDialogProperties.CANCEL_ACTION));
+
+        return model;
     }
 }
