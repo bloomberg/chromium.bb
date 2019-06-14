3511fdb15e93bba1f3d2acabe367c65cecc4fd57
Aaron Tagliaboschi <aarontag@chromium.org>
2019-06-14 19:48:18 +0000

    Add null check to main frame check in samesite deprecation messages
    There seems to be a race condition where cookies are called to be
    stored or sent on a page that somehow navigates to a interstitial page,
    causing WebContents::FromRenderFrameHost to return a null pointer that
    is then dereferenced. Because we definitely don't need messages from
    interstitial pages (and console messages shouldn't be sent after the
    page has navigated away anyways), simply returning early would be
    expected behaviour anyways.
    
    Bug: 973574
    Change-Id: I4777e36baf7737bcd4d73d36840fa671e34a2ed5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1658688
    Commit-Queue: Aaron Tagliaboschi <aarontag@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669331}
    
    Former-commit-id: 0485879974204691438021c15bf7e7df9ba03edc

--- content/browser/network_service_client.cc
+++ content/browser/network_service_client.cc
@@ -451,6 +451,11 @@ void DeprecateSameSiteCookies(int process_id,
   // returning early should this be the case.
   WebContents* web_contents = WebContents::FromRenderFrameHost(frame);
 
+  // |web_contents| will be null on interstitial pages, which means the frame
+  // has been navigated away from and the function should return early.
+  if (!web_contents)
+    return;
+
   RenderFrameHostImpl* root_frame_host = frame;
   while (root_frame_host->GetParent() != nullptr)
     root_frame_host = root_frame_host->GetParent();
