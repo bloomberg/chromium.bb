63c26add5d09a6faee9c7a8103d8c78ef6badcad
Daniel Bratell <bratell@opera.com>
2019-06-14 13:49:50 +0000

    Fix python3 compatibility for script/tools/clang/update.py
    Fixes by gmiazga@op...
    
    1. The two urllib imports used the same name so only the second import had any effect.
    
    2. getheader() is not available in Python3
    
    3. Integer division has to be done with // or the result won't be an integer
    
    Bug: 941669
    Change-Id: Ied1ccffd8de85c79a667a9b2c7f58308d973c74c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1660548
    Commit-Queue: Daniel Bratell <bratell@opera.com>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Auto-Submit: Daniel Bratell <bratell@opera.com>
    Cr-Commit-Position: refs/heads/master@{#669204}
    
    Former-commit-id: 62d2700b48561cd9d35bafe305203385a308d315

--- tools/clang/scripts/update.py
+++ tools/clang/scripts/update.py
@@ -14,6 +14,7 @@ near-tip-of-tree clang version:
 
 # TODO: Running stand-alone won't work on Windows due to the dia dll copying.
 
+from __future__ import division
 from __future__ import print_function
 import argparse
 import os
@@ -25,10 +26,10 @@ import tempfile
 import time
 
 try:
-  import urllib2 as urllib
+  from urllib2 import HTTPError, URLError, urlopen
 except ImportError: # For Py3 compatibility
-  import urllib.request as urllib
-  import urllib.error as urllib
+  from urllib.error import HTTPError, URLError
+  from urllib.request import urlopen
 
 import zipfile
 
@@ -99,8 +100,8 @@ def DownloadUrl(url, output_file):
     try:
       sys.stdout.write('Downloading %s ' % url)
       sys.stdout.flush()
-      response = urllib.urlopen(url)
-      total_size = int(response.info().getheader('Content-Length').strip())
+      response = urlopen(url)
+      total_size = int(response.info().get('Content-Length').strip())
       bytes_done = 0
       dots_printed = 0
       while True:
@@ -109,19 +110,19 @@ def DownloadUrl(url, output_file):
           break
         output_file.write(chunk)
         bytes_done += len(chunk)
-        num_dots = TOTAL_DOTS * bytes_done / total_size
+        num_dots = TOTAL_DOTS * bytes_done // total_size
         sys.stdout.write('.' * (num_dots - dots_printed))
         sys.stdout.flush()
         dots_printed = num_dots
       if bytes_done != total_size:
-        raise urllib.URLError("only got %d of %d bytes" %
-                              (bytes_done, total_size))
+        raise URLError("only got %d of %d bytes" %
+                       (bytes_done, total_size))
       print(' Done.')
       return
-    except urllib.URLError as e:
+    except URLError as e:
       sys.stdout.write('\n')
       print(e)
-      if num_retries == 0 or isinstance(e, urllib.HTTPError) and e.code == 404:
+      if num_retries == 0 or isinstance(e, HTTPError) and e.code == 404:
         raise e
       num_retries -= 1
       print('Retrying in %d s ...' % retry_wait_s)
@@ -171,7 +172,7 @@ def DownloadAndUnpackClangPackage(platform, output_dir, runtimes_only=False):
     if runtimes_only:
       path_prefix = 'lib/clang/' + RELEASE_VERSION + '/lib/'
     DownloadAndUnpack(cds_full_url, output_dir, path_prefix)
-  except urllib.URLError:
+  except URLError:
     print('Failed to download prebuilt clang %s' % cds_file)
     print('Use --force-local-build if you want to build locally.')
     print('Exiting.')
