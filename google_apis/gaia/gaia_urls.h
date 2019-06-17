a4f9f0d83f7fd53c6afd9de3b30197f115ac9f03
Boris Sazonov <bsazonov@chromium.org>
2019-06-17 23:45:56 +0000

    [Signin][Android] Add continue url to Logout request
    Adds GaiaAuthFetcher::StartLogOutWithBlankContinueURL that specifies
    a blank page as a continue URL for Logout requests. This continue URL
    is necessary as sometimes Gaia redirects Logout requests to large pages
    that exceed 1MB body limit for SimpleURLLoader::DownloadToString.
    
    (cherry picked from commit 21f5bbe514d89f0a7633fe3f27044e7cb91f8c2e [formerly 3d8eb95ae203c94c2455c542450a2371360cdf18])
    
    Bug: b/134618807, 975887
    Change-Id: Ie4d57ac959f8936077f98fce6a288ebf41122fc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1656488
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#669836}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1663492
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3809@{#394}
    Cr-Branched-From: ce45438777a60f42f88228700b3968406c6404d8 [formerly d82dec1a818f378c464ba307ddd9c92133eac355]-refs/heads/master@{#665002}
    
    Former-commit-id: 07998b5c8474c61d600c1660f47cb299687ddcd7

--- google_apis/gaia/gaia_urls.h
+++ google_apis/gaia/gaia_urls.h
@@ -52,6 +52,7 @@ class GaiaUrls {
 
   GURL ListAccountsURLWithSource(const std::string& source);
   GURL LogOutURLWithSource(const std::string& source);
+  GURL LogOutURLWithSourceAndContinueURL(const std::string& source);
   GURL GetCheckConnectionInfoURLWithSource(const std::string& source);
 
  private:
@@ -72,6 +73,7 @@ class GaiaUrls {
   GURL signin_chrome_sync_dice_;
   GURL service_login_auth_url_;
   GURL service_logout_url_;
+  GURL continue_url_for_logout_;
   GURL get_user_info_url_;
   GURL token_auth_url_;
   GURL merge_session_url_;
