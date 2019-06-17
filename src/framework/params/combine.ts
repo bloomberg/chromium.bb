1840678033d8ab518c41ba9b29b19da1f0379794
Kai Ninomiya <kainino@chromium.org>
2019-06-17 11:12:18 -0700

    [WIP] fix pcombine

--- src/framework/params/combine.ts
+++ src/framework/params/combine.ts
@@ -6,32 +6,32 @@ import {
 
 export function pcombine(params: ParamSpecIterable[]) { return new PCombine(params); }
 
+function merge(a: IParamsSpec, b: IParamsSpec): IParamsSpec {
+  for (const key of Object.keys(a)) {
+    if (b.hasOwnProperty(key)) {
+      throw new Error('Duplicate key: ' + key);
+    }
+  }
+  return {...a, ...b};
+}
+
+function * cartesian(iters: ParamSpecIterable[]): ParamSpecIterator {
+  if (iters.length === 0) {
+    return;
+  }
+  if (iters.length === 1) {
+    yield* iters[0];
+    return;
+  }
+  const [as, ...rest] = iters;
+  for (const a of as) {
+    for (const b of cartesian(rest)) {
+      yield merge(a, b);
+    }
+  }
+}
+
 class PCombine implements ParamSpecIterable {
-
-  private static merge(a: IParamsSpec, b: IParamsSpec): IParamsSpec {
-    for (const key of Object.keys(a)) {
-      if (b.hasOwnProperty(key)) {
-        throw new Error('Duplicate key: ' + key);
-      }
-    }
-    return {...a, ...b};
-  }
-
-  private static * cartesian(iters: ParamSpecIterable[]): ParamSpecIterator {
-    if (iters.length === 0) {
-      return;
-    }
-    if (iters.length === 1) {
-      yield* iters[0];
-      return;
-    }
-    const [as, ...rest] = iters;
-    for (const a of as) {
-      for (const b of PCombine.cartesian(rest)) {
-        yield PCombine.merge(a, b);
-      }
-    }
-  }
   private params: ParamSpecIterable[];
 
   constructor(params: ParamSpecIterable[]) {
@@ -39,6 +39,6 @@ class PCombine implements ParamSpecIterable {
   }
 
   public [Symbol.iterator](): ParamSpecIterator {
-    return PCombine.cartesian(this.params);
+    return cartesian(this.params);
   }
 }
