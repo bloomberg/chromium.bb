c85401c21027c2c2d570a2aa75e19d39c351cb6f
Andrew Lee <myungjaelee@chromium.org>
2019-06-18 00:34:20 +0000

    ui_devtools fails when you drag elements in the dom tree
    This is a small bug which is caused by this CL merged yesterday
    (https://chromium-review.googlesource.com/c/chromium/src/+/1636889).
    
    This error occurs because DCHECK(!property_trees->needs_rebuild) fails.
    While we try to drag elements on the dom tree UI, the property trees
    need to be updated again, hence property_trees->needs_rebuild = true.
    We can easily avoid this error by only calling GetRenderSurfaceReason()
    when rebuild is NOT required.
    
    Bug: 973092
    Change-Id: Ifdb49d7e81d32f6bcff73662ef34f0118d4abf50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1650640
    Commit-Queue: MyungJae Lee <myungjaelee@chromium.org>
    Reviewed-by: Wei Li <weili@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#669912}
    
    Former-commit-id: dc0c4efe4eb238c5fcd76922bc78d159baddc083

--- components/ui_devtools/views/element_utility.cc
+++ components/ui_devtools/views/element_utility.cc
@@ -15,6 +15,11 @@ void AppendLayerProperties(
                     layer->layer_mask_layer() ? "true" : "false");
   const cc::Layer* cc_layer = layer->cc_layer_for_testing();
   if (cc_layer) {
+    // Property trees must be updated in order to get valid render surface
+    // reasons.
+    if (!cc_layer->layer_tree_host() ||
+        cc_layer->layer_tree_host()->property_trees()->needs_rebuild)
+      return;
     cc::RenderSurfaceReason render_surface = cc_layer->GetRenderSurfaceReason();
     if (render_surface != cc::RenderSurfaceReason::kNone) {
       ret->emplace_back("render-surface-reason",
