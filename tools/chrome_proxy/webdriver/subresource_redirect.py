72352b62fb77f122dad87bd0f7aca34780d405d5
Sean Harrison <harrisonsean@chromium.org>
2019-06-19 20:10:50 +0000

    Add chromedriver test to check for server bypass
    Bug: 960513
    Change-Id: I02502f0c80794dce5c0133bea84eecaa43100433
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1663029
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670617}
    
    Former-commit-id: 0b169da96f1845e4f983d6d514196687043c28f5

--- tools/chrome_proxy/webdriver/subresource_redirect.py
+++ tools/chrome_proxy/webdriver/subresource_redirect.py
@@ -12,6 +12,8 @@ LITEPAGES_REGEXP = r'https://\w+\.litepages\.googlezip\.net/.*'
 
 class SubresourceRedirect(IntegrationTest):
 
+  # Verifies that image subresources on a page have been returned
+  # from the compression server.
   @ChromeVersionEqualOrAfterM(77)
   def testCompressImage(self):
     with TestDriver() as test_driver:
@@ -31,8 +33,36 @@ class SubresourceRedirect(IntegrationTest):
 
       self.assertEqual(5, image_responses)
 
-  # TODO(harrisonsean): Add compression server bypass test
+  # Verifies that when the image compression server serves a
+  # redirect, then Chrome fetches the image directly.
+  @ChromeVersionEqualOrAfterM(77)
+  def testOnRedirectImage(self):
+    with TestDriver() as test_driver:
+      test_driver.AddChromeArg('--enable-subresource-redirect')
+      # Image compression server returns a 307 for all images on this webpage.
+      test_driver.LoadURL(
+        'https://testsafebrowsing.appspot.com/s/image_small.html')
 
+      server_bypass = 0
+      image_responses = 0
+
+      for response in test_driver.GetHTTPResponses():
+        content_type = ''
+        if 'content-type' in response.response_headers:
+          content_type = response.response_headers['content-type']
+        if ('image/' in content_type
+          and re.match(LITEPAGES_REGEXP, response.url)
+          and 200 == response.status):
+          image_responses += 1
+        if ('https://testsafebrowsing.appspot.com/s/bad_assets/small.png'
+          == response.url and 200 == response.status):
+          server_bypass += 1
+
+      self.assertEqual(1, server_bypass)
+      self.assertEqual(0, image_responses)
+
+  # Verifies that non-image subresources aren't redirected to the
+  # compression server.
   @ChromeVersionEqualOrAfterM(77)
   def testNoCompressNonImage(self):
     with TestDriver() as test_driver:
@@ -52,6 +82,8 @@ class SubresourceRedirect(IntegrationTest):
 
       self.assertEqual(0, image_responses)
 
+  # Verifies that non-secure connections aren't redirected to the
+  # compression server.
   @ChromeVersionEqualOrAfterM(77)
   def testNoCompressNonHTTPS(self):
     with TestDriver() as test_driver:
