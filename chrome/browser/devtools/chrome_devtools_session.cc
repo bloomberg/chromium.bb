fc24d81c21e96ab63555a19df5dd91d68ee585d4
Johannes Henkel <johannes@chromium.org>
2019-06-19 22:37:34 +0000

    [DevTools] Flip chrome_devtools_session.cc to binary, for good.
    chrome_devtools_session has a pointer to the client, which may
    request that responses and notifications are provided as JSON.
    I'd like to switch this to serialization via CBOR, much like
    content/browser/devtools/devtools_session.cc already does it.
    Unfortunately this means that I need to transcode here.
    
    However, this should pave the path for removing the json support
    from Serializable, as it looks like this is the last remaining
    holdout.
    
    Change-Id: I8e4ef2d396973fa30a3bc64a4baa98d0db38a5ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1666464
    Commit-Queue: Johannes Henkel <johannes@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670688}
    
    Former-commit-id: 1f3669bfdb3201fe17df9ce315448632785c67d8

--- chrome/browser/devtools/chrome_devtools_session.cc
+++ chrome/browser/devtools/chrome_devtools_session.cc
@@ -4,6 +4,8 @@
 
 #include "chrome/browser/devtools/chrome_devtools_session.h"
 
+#include <memory>
+#include "base/strings/string_number_conversions.h"
 #include "chrome/browser/devtools/protocol/browser_handler.h"
 #include "chrome/browser/devtools/protocol/cast_handler.h"
 #include "chrome/browser/devtools/protocol/page_handler.h"
@@ -11,10 +13,46 @@
 #include "content/public/browser/devtools_agent_host.h"
 #include "content/public/browser/devtools_agent_host_client.h"
 #include "content/public/browser/devtools_manager_delegate.h"
+#include "third_party/inspector_protocol/encoding/encoding.h"
 
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/devtools/protocol/window_manager_handler.h"
 #endif
+
+namespace {
+// TODO(johannes): This is very similar to the code in
+// content/browser/devtools/devtools_protocol_encoding. Once we have
+// the error / status propagation story settled, move the common parts
+// into a content public API.
+
+using ::inspector_protocol_encoding::span;
+using ::inspector_protocol_encoding::SpanFrom;
+using ::inspector_protocol_encoding::json::ConvertCBORToJSON;
+using ::inspector_protocol_encoding::json::ConvertJSONToCBOR;
+using IPEStatus = ::inspector_protocol_encoding::Status;
+
+// Platform allows us to inject the string<->double conversion
+// routines from base:: into the inspector_protocol JSON parser / serializer.
+class Platform : public ::inspector_protocol_encoding::json::Platform {
+ public:
+  bool StrToD(const char* str, double* result) const override {
+    return base::StringToDouble(str, result);
+  }
+
+  // Prints |value| in a format suitable for JSON.
+  std::unique_ptr<char[]> DToStr(double value) const override {
+    std::string str = base::NumberToString(value);
+    std::unique_ptr<char[]> result(new char[str.size() + 1]);
+    memcpy(result.get(), str.c_str(), str.size() + 1);
+    return result;
+  }
+};
+
+IPEStatus ConvertCBORToJSON(span<uint8_t> cbor, std::string* json) {
+  Platform platform;
+  return ConvertCBORToJSON(platform, cbor, json);
+}
+}  // namespace
 
 ChromeDevToolsSession::ChromeDevToolsSession(
     content::DevToolsAgentHost* agent_host,
@@ -44,14 +82,6 @@ ChromeDevToolsSession::ChromeDevToolsSession(
 
 ChromeDevToolsSession::~ChromeDevToolsSession() = default;
 
-void ChromeDevToolsSession::sendProtocolResponse(
-    int call_id,
-    std::unique_ptr<protocol::Serializable> message) {
-  pending_commands_.erase(call_id);
-  bool binary = client_->UsesBinaryProtocol();
-  client_->DispatchProtocolMessage(agent_host_, message->serialize(binary));
-}
-
 void ChromeDevToolsSession::HandleCommand(
     const std::string& method,
     const std::string& message,
@@ -72,6 +102,37 @@ void ChromeDevToolsSession::HandleCommand(
   dispatcher_->dispatch(call_id, method, std::move(value), message);
 }
 
+// The following methods handle responses or notifications coming from
+// the browser to the client.
+static void SendProtocolResponseOrNotification(
+    content::DevToolsAgentHostClient* client,
+    content::DevToolsAgentHost* agent_host,
+    std::unique_ptr<protocol::Serializable> message) {
+  std::string cbor = message->serialize(/*binary=*/true);
+  if (client->UsesBinaryProtocol()) {
+    client->DispatchProtocolMessage(agent_host, cbor);
+    return;
+  }
+  std::string json;
+  IPEStatus status = ConvertCBORToJSON(SpanFrom(cbor), &json);
+  LOG_IF(ERROR, !status.ok()) << status.ToASCIIString();
+  client->DispatchProtocolMessage(agent_host, json);
+}
+
+void ChromeDevToolsSession::sendProtocolResponse(
+    int call_id,
+    std::unique_ptr<protocol::Serializable> message) {
+  pending_commands_.erase(call_id);
+  SendProtocolResponseOrNotification(client_, agent_host_, std::move(message));
+}
+
+void ChromeDevToolsSession::sendProtocolNotification(
+    std::unique_ptr<protocol::Serializable> message) {
+  SendProtocolResponseOrNotification(client_, agent_host_, std::move(message));
+}
+
+void ChromeDevToolsSession::flushProtocolNotifications() {}
+
 void ChromeDevToolsSession::fallThrough(int call_id,
                                         const std::string& method,
                                         const std::string& message) {
@@ -79,11 +140,3 @@ void ChromeDevToolsSession::fallThrough(int call_id,
   pending_commands_.erase(call_id);
   std::move(callback).Run(message);
 }
-
-void ChromeDevToolsSession::sendProtocolNotification(
-    std::unique_ptr<protocol::Serializable> message) {
-  bool binary = client_->UsesBinaryProtocol();
-  client_->DispatchProtocolMessage(agent_host_, message->serialize(binary));
-}
-
-void ChromeDevToolsSession::flushProtocolNotifications() {}
