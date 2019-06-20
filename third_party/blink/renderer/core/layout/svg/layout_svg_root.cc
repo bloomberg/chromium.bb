0db67fa55fe98aa0a269bb1c10c0825aa5b2f658
Vladimir Levin <vmpstr@chromium.org>
2019-06-20 01:57:38 +0000

    DL: Don't paint images and SVGs when locked.
    When images or SVGs are locked, we shouldn't paint their contents other
    than the self box (background/border). This patch ensures that this is
    true.
    
    R=chrishtr@chromium.org
    
    Change-Id: I87ab1b3fd219eeed7c4b8a2b7449dd254739da56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1666915
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670767}
    
    Former-commit-id: bbfca65901c2e4d662561a42958bd892b9fbd238

--- third_party/blink/renderer/core/layout/svg/layout_svg_root.cc
+++ third_party/blink/renderer/core/layout/svg/layout_svg_root.cc
@@ -264,6 +264,8 @@ LayoutRect LayoutSVGRoot::ComputeContentsVisualOverflow() const {
 
 void LayoutSVGRoot::PaintReplaced(const PaintInfo& paint_info,
                                   const PhysicalOffset& paint_offset) const {
+  if (PaintBlockedByDisplayLock(DisplayLockContext::kChildren))
+    return;
   SVGRootPainter(*this).PaintReplaced(paint_info, paint_offset);
 }
 
