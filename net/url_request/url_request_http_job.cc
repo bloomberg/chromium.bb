157b9460adf1a31d6bf71426dba54605aad348a0
Matt Menke <mmenke@chromium.org>
2019-06-20 02:25:39 +0000

    Remove Net.HttpTimeToFirstByte.LargeUpload.
    I don't believe anyone has actually used it for anything.
    
    Bug: 975284
    Change-Id: I6a58ff766898239dfb1d8b014e5cd05a009ee8dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1666233
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670771}
    
    Former-commit-id: 87b038c221927a5c3069ad1c0178ad7796a54124

--- net/url_request/url_request_http_job.cc
+++ net/url_request/url_request_http_job.cc
@@ -1416,10 +1416,6 @@ void URLRequestHttpJob::RecordTimer() {
   request_creation_time_ = base::Time();
 
   UMA_HISTOGRAM_MEDIUM_TIMES("Net.HttpTimeToFirstByte", to_start);
-  if (request_info_.upload_data_stream &&
-      request_info_.upload_data_stream->size() > 1024 * 1024) {
-    UMA_HISTOGRAM_MEDIUM_TIMES("Net.HttpTimeToFirstByte.LargeUpload", to_start);
-  }
 }
 
 void URLRequestHttpJob::ResetTimer() {
