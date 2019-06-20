153856afd3bc812c72a9e948c4e3f0741029ecc2
Etienne Bergeron <etienneb@chromium.org>
2019-06-20 14:43:33 +0000

    Implement the average character width for PlatformFontSkia
    This CL is adding a way to estimate the size of an average
    character when the SkiaMetrics doesn't provide it.
    
    Some SkiaFontMgr don't provide the average char width
    since the metric is not always available. As an example,
    the DWrite doesn't provide the average char widths
    (see structure DWRITE_FONT_METRICS).
    
    The blink code is using an approximation by looking for the
    glyph 'x' and getting the size of that glyph.
    
    We are proposing to use the same solution and fallback to
    the max char width if the glyph is not available.
    
    R=robliao@chromium.org, asvitkine@chromium.org
    CC=​​msw@chromium.org
    
    Bug: 969687, 944227
    
    Change-Id: I8cf23b5e02fba211edb321b2ac66306ff458993b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1666720
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670892}
    
    Former-commit-id: 5e563559d01610f5d3c9a90c2e8df54cc4b8d578

--- ui/gfx/platform_font_skia.cc
+++ ui/gfx/platform_font_skia.cc
@@ -37,6 +37,8 @@ const char* kFallbackFontFamilyName = "serif";
 const char* kFallbackFontFamilyName = "sans";
 #endif
 
+constexpr SkGlyphID kUnsupportedGlyph = 0;
+
 // The default font, used for the default constructor.
 base::LazyInstance<scoped_refptr<PlatformFontSkia>>::Leaky g_default_font =
     LAZY_INSTANCE_INITIALIZER;
@@ -336,10 +338,19 @@ void PlatformFontSkia::ComputeMetricsIfNecessary() {
       average_width_pixels_ = SkScalarToDouble(metrics.fAvgCharWidth);
     } else {
       // Some Skia fonts manager do not compute the average character size
-      // (e.g. Direct Write). The default behavior when the metric is not
-      // available is to use the max char width.
-      average_width_pixels_ = SkScalarToDouble(metrics.fMaxCharWidth);
+      // (e.g. Direct Write). The following code computes the average character
+      // width the same way blink (e.g. PlatformFont) does. Use the width of the
+      //  letter 'x' when available, otherwise use the max character width.
+      SkGlyphID glyph = typeface_->unicharToGlyph('x');
+      if (glyph != kUnsupportedGlyph) {
+        SkScalar sk_width;
+        font.getWidths(&glyph, 1, &sk_width);
+        average_width_pixels_ = SkScalarCeilToInt(sk_width);
+      }
+      if (!average_width_pixels_)
+        average_width_pixels_ = SkScalarCeilToInt(metrics.fMaxCharWidth);
     }
+    DCHECK_NE(average_width_pixels_, 0);
   }
 }
 
