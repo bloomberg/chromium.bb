c74c2c9a96573d458503520c4cf701a9af81ffb6
Matthew Jones <mdjones@chromium.org>
2019-06-20 21:10:49 +0000

    Implement downloads pause/resume flow
    This patch implements controls for downloads. The new UI exists as a
    separate activity and is triggered by the download notification. The
    UI is one-per download and contains the title of the affected
    download, a pause/resume button, and a cancel button.
    
    TBR=mdjones@chromium.org
    
    (cherry picked from commit b89ec80fa41bcde3d9c42d1de1eab3f116f842a6 [formerly 06d8dbc12c052594ca3b0faea49ffa7eeab0c6a7])
    
    Bug: 969220
    Change-Id: Id064a2b1a0b9159e3f9f8d1dba5badddd65920e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1663056
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#670960}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1670130
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3809@{#454}
    Cr-Branched-From: ce45438777a60f42f88228700b3968406c6404d8 [formerly d82dec1a818f378c464ba307ddd9c92133eac355]-refs/heads/master@{#665002}
    
    Former-commit-id: 811056022ce36b8ffb5b02088ba9996f8c0291db

---
+++ chrome/android/touchless/fallback/java/src/org/chromium/chrome/browser/download/TouchlessDownloadActivity.java
@@ -0,0 +1,11 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.download;
+
+/**
+ * Fallback class for accessing this class name in
+ * {@link org.chromium.chrome.browser.download.DownloadNotificationFactory}.
+ */
+public class TouchlessDownloadActivity {}
