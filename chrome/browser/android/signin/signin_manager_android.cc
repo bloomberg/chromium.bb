f2f6a35076037c5e6710e3e155951d4a1a58798a
Pâris MEULEMAN <pmeuleman@chromium.org>
2019-06-24 16:47:24 +0000

    Create ChromeSigninManagerDelegate JNI
    This introduces the Jni counterpart of the java
    ChromeSigninManagerDelegate. This interface will provide access to android
    specific dependencies on //chrome/browser to the componentized java
    SigninManager.
    
    This first iteration creates the new objects and implements
    stopApplyingCloudPolicy, fetchAndApplyCloudPolicy, isAccountManaged and
    getManagementDomain.
    
    As discussed in CLs on the java counterpart (bug 963400) this object
    will hold pointers to singletons such as the ActiveUserProfile.
    The native object is created by the java object.
    
    details in document:
    https://docs.google.com/document/d/18887XeZNJ9pmoTdJducssk5_yVU2CQ3EqaRFoMqWCCk/
    
    Bug: 963402
    Change-Id: Iccbb6b30d23f26fc5960b5d9db9c122df3e589f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1642547
    Commit-Queue: Pâris Meuleman <pmeuleman@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671684}
    
    Former-commit-id: bfe02993599efcef3491d36316d49957893bf331

--- chrome/browser/android/signin/signin_manager_android.cc
+++ chrome/browser/android/signin/signin_manager_android.cc
@@ -26,7 +26,6 @@
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/common/pref_names.h"
 #include "components/google/core/common/google_util.h"
-#include "components/policy/core/browser/browser_policy_connector.h"
 #include "components/policy/core/common/cloud/cloud_policy_core.h"
 #include "components/policy/core/common/cloud/cloud_policy_store.h"
 #include "components/policy/core/common/cloud/user_cloud_policy_manager.h"
@@ -48,11 +47,6 @@ namespace {
 void ClearLastSignedInUserForProfile(Profile* profile) {
   profile->GetPrefs()->ClearPref(prefs::kGoogleServicesLastAccountId);
   profile->GetPrefs()->ClearPref(prefs::kGoogleServicesLastUsername);
-}
-
-// Returns whether the user is a managed user or not.
-bool ShouldLoadPolicyForUser(const std::string& username) {
-  return !policy::BrowserPolicyConnector::IsNonEnterpriseUser(username);
 }
 
 // A BrowsingDataRemover::Observer that clears Profile data and then invokes
@@ -139,13 +133,8 @@ SigninManagerAndroid::SigninManagerAndroid(JNIEnv* env, jobject obj)
 
 SigninManagerAndroid::~SigninManagerAndroid() {
   IdentityManagerFactory::GetForProfile(profile_)->RemoveObserver(this);
-}
-
-void SigninManagerAndroid::AbortSignIn(JNIEnv* env,
-                                       const JavaParamRef<jobject>& obj) {
-  policy::UserPolicySigninService* service =
-      policy::UserPolicySigninServiceFactory::GetForProfile(profile_);
-  service->ShutdownUserCloudPolicyManager();
+  // TODO(crbug.com/963408) Call SigninManager.java Destroy once ownership is
+  // reversed.
 }
 
 void SigninManagerAndroid::OnSignInCompleted(
@@ -178,24 +167,6 @@ void SigninManagerAndroid::SignOut(JNIEnv* env,
       signin_metrics::SignoutDelete::IGNORE_METRIC);
 }
 
-base::android::ScopedJavaLocalRef<jstring>
-SigninManagerAndroid::GetManagementDomain(JNIEnv* env,
-                                          const JavaParamRef<jobject>& obj) {
-  base::android::ScopedJavaLocalRef<jstring> domain;
-
-  policy::UserCloudPolicyManager* manager =
-      profile_->GetUserCloudPolicyManager();
-  policy::CloudPolicyStore* store = manager->core()->store();
-
-  if (store && store->is_managed() && store->policy()->has_username()) {
-    domain.Reset(
-        base::android::ConvertUTF8ToJavaString(
-            env, gaia::ExtractDomainName(store->policy()->username())));
-  }
-
-  return domain;
-}
-
 void SigninManagerAndroid::WipeProfileData(
     JNIEnv* env,
     const JavaParamRef<jobject>& obj,
@@ -214,89 +185,6 @@ void SigninManagerAndroid::WipeGoogleServiceWorkerCaches(
       profile_, false /* only Google service worker caches */,
       base::BindOnce(base::android::RunRunnableAndroid,
                      base::android::ScopedJavaGlobalRef<jobject>(j_callback)));
-}
-
-void SigninManagerAndroid::RegisterPolicyWithAccount(
-    const CoreAccountInfo& account,
-    RegisterPolicyWithAccountCallback callback) {
-  if (!ShouldLoadPolicyForUser(account.email)) {
-    std::move(callback).Run(base::nullopt);
-    return;
-  }
-
-  policy::UserPolicySigninService* service =
-      policy::UserPolicySigninServiceFactory::GetForProfile(profile_);
-
-  service->RegisterForPolicyWithAccountId(
-      account.email, account.account_id,
-      base::AdaptCallbackForRepeating(base::BindOnce(
-          [](RegisterPolicyWithAccountCallback callback,
-             const std::string& dm_token, const std::string& client_id) {
-            base::Optional<ManagementCredentials> credentials;
-            if (!dm_token.empty()) {
-              credentials.emplace(dm_token, client_id);
-            }
-            std::move(callback).Run(credentials);
-          },
-          std::move(callback))));
-}
-
-void SigninManagerAndroid::FetchAndApplyCloudPolicy(
-    JNIEnv* env,
-    const JavaParamRef<jobject>& obj,
-    const JavaParamRef<jstring>& j_username,
-    const base::android::JavaParamRef<jobject>& j_callback) {
-  std::string username =
-      base::android::ConvertJavaStringToUTF8(env, j_username);
-  DCHECK(!username.empty());
-  // TODO(bsazonov): Remove after migrating the sign-in flow to CoreAccountId.
-  // ExtractDomainName Dchecks that username is a valid email, in practice
-  // this checks that @ is present and is not the last character.
-  gaia::ExtractDomainName(username);
-  CoreAccountInfo account =
-      IdentityManagerFactory::GetForProfile(profile_)
-          ->FindAccountInfoForAccountWithRefreshTokenByEmailAddress(username)
-          .value();
-
-  auto callback =
-      base::BindOnce(base::android::RunRunnableAndroid,
-                     base::android::ScopedJavaGlobalRef<jobject>(j_callback));
-
-  RegisterPolicyWithAccount(
-      account,
-      base::BindOnce(&SigninManagerAndroid::OnPolicyRegisterDone,
-                     weak_factory_.GetWeakPtr(), account, std::move(callback)));
-}
-
-void SigninManagerAndroid::OnPolicyRegisterDone(
-    const CoreAccountInfo& account,
-    base::OnceClosure policy_callback,
-    const base::Optional<ManagementCredentials>& credentials) {
-  if (credentials) {
-    FetchPolicyBeforeSignIn(account, std::move(policy_callback),
-                            credentials.value());
-  } else {
-    // User's account does not have a policy to fetch.
-    std::move(policy_callback).Run();
-  }
-}
-
-void SigninManagerAndroid::FetchPolicyBeforeSignIn(
-    const CoreAccountInfo& account,
-    base::OnceClosure policy_callback,
-    const ManagementCredentials& credentials) {
-  policy::UserPolicySigninService* service =
-      policy::UserPolicySigninServiceFactory::GetForProfile(profile_);
-  scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory =
-      content::BrowserContext::GetDefaultStoragePartition(profile_)
-          ->GetURLLoaderFactoryForBrowserProcess();
-  service->FetchPolicyForSignedInUser(
-      AccountIdFromAccountInfo(account), credentials.dm_token,
-      credentials.client_id, url_loader_factory,
-      base::AdaptCallbackForRepeating(
-          base::BindOnce([](base::OnceClosure callback,
-                            bool success) { std::move(callback).Run(); },
-                         std::move(policy_callback))));
 }
 
 void SigninManagerAndroid::ClearLastSignedInUser(
@@ -361,30 +249,6 @@ static jlong JNI_SigninManager_Init(JNIEnv* env,
   SigninManagerAndroid* signin_manager_android =
       new SigninManagerAndroid(env, obj);
   return reinterpret_cast<intptr_t>(signin_manager_android);
-}
-
-void SigninManagerAndroid::IsAccountManaged(
-    JNIEnv* env,
-    const JavaParamRef<jobject>& obj,
-    const JavaParamRef<jstring>& j_username,
-    const JavaParamRef<jobject>& j_callback) {
-  base::android::ScopedJavaGlobalRef<jobject> callback(env, j_callback);
-  std::string username =
-      base::android::ConvertJavaStringToUTF8(env, j_username);
-
-  base::Optional<CoreAccountInfo> account =
-      IdentityManagerFactory::GetForProfile(profile_)
-          ->FindAccountInfoForAccountWithRefreshTokenByEmailAddress(username);
-
-  RegisterPolicyWithAccount(
-      account.value_or(CoreAccountInfo{}),
-      base::BindOnce(
-          [](base::android::ScopedJavaGlobalRef<jobject> callback,
-             const base::Optional<ManagementCredentials>& credentials) {
-            base::android::RunBooleanCallbackAndroid(callback,
-                                                     credentials.has_value());
-          },
-          callback));
 }
 
 base::android::ScopedJavaLocalRef<jstring> JNI_SigninManager_ExtractDomainName(
