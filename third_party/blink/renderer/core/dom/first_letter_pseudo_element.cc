af56bd9d5a641bdbd26d798bba817324791978a2
Rune Lillesveen <futhark@chromium.org>
2019-06-24 11:29:42 +0000

    Handle null ::first-letter text layout object.
    Ideally, UpdateFirstLetterPseudoElement() will remove the ::first-letter
    pseudo element from the DOM when there is no text which can contain the
    first letter. Hence, we should not end up in a situation where we try to
    AttachLayoutTree for the FirstLetterPseudoElement with a null
    first_letter_text.
    
    However, the layout tree is different in AttachLayoutTree() than in
    UpdateFirstLetterPseudoElement() in the case where there already was a
    ::first-letter pseudo element, since we detached it right before
    AttachLayoutTree. This means we may have different results for
    FirstLetterTextLayoutObject() at those two points if the method has
    bugs.
    
    Such a bug was found here where a list item marker inside an anonymous
    block had an out-of-flow sibling, but we were not able to traverse out
    of the anonymous block to find the first letter text.
    
    Add a DCHECK to let us trigger cases where FirstLetterTextLayoutObject()
    is buggy, but also add if-tests to make sure we don't crash.
    
    Bug: 977044
    Change-Id: Id80f8aad5c04678d64927e7a7ca4d60cdb6a4b8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1672845
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671629}
    
    Former-commit-id: 9f72f084e6e90f9f43e5de949d0d6d0f7bcd5b0d

--- third_party/blink/renderer/core/dom/first_letter_pseudo_element.cc
+++ third_party/blink/renderer/core/dom/first_letter_pseudo_element.cc
@@ -101,7 +101,7 @@ LayoutText* FirstLetterPseudoElement::FirstLetterTextLayoutObject(
   LayoutObject* parent_layout_object = nullptr;
 
   // If we are looking at a first letter element then we need to find the
-  // first letter text layoutObject from the parent node, and not ourselves.
+  // first letter text LayoutObject from the parent node, and not ourselves.
   if (element.IsFirstLetterPseudoElement()) {
     parent_layout_object =
         element.ParentOrShadowHostElement()->GetLayoutObject();
@@ -147,9 +147,16 @@ LayoutText* FirstLetterPseudoElement::FirstLetterTextLayoutObject(
           first_letter_text_layout_object->NextSibling();
     } else if (first_letter_text_layout_object->IsListMarker() ||
                first_letter_text_layout_object == marker) {
-      first_letter_text_layout_object =
-          first_letter_text_layout_object->NextInPreOrderAfterChildren(
-              parent_layout_object);
+      // The list item marker may have out-of-flow siblings inside an anonymous
+      // block. Skip them to make sure we leave the anonymous block before
+      // continuing looking for the first letter text.
+      do {
+        first_letter_text_layout_object =
+            first_letter_text_layout_object->NextInPreOrderAfterChildren(
+                parent_layout_object);
+      } while (
+          first_letter_text_layout_object &&
+          first_letter_text_layout_object->IsFloatingOrOutOfFlowPositioned());
     } else if (first_letter_text_layout_object
                    ->IsFloatingOrOutOfFlowPositioned()) {
       if (first_letter_text_layout_object->Style()->StyleType() ==
@@ -261,14 +268,26 @@ void FirstLetterPseudoElement::ClearRemainingTextLayoutObject() {
 void FirstLetterPseudoElement::AttachLayoutTree(AttachContext& context) {
   LayoutText* first_letter_text =
       FirstLetterPseudoElement::FirstLetterTextLayoutObject(*this);
+  // The FirstLetterPseudoElement should have been removed in
+  // Element::UpdateFirstLetterPseudoElement(). However if there existed a first
+  // letter before updating it, the layout tree will be different after
+  // DetachLayoutTree() called right before this method.
+  // If there is a bug in FirstLetterTextLayoutObject(), we might end up with
+  // null here. DCHECKing here, but handling the null pointer below to avoid
+  // crashes.
+  DCHECK(first_letter_text);
+
   AttachContext first_letter_context(context);
   first_letter_context.next_sibling = first_letter_text;
   first_letter_context.next_sibling_valid = true;
-  first_letter_context.parent = first_letter_text->Parent();
-  if (first_letter_context.parent->ForceLegacyLayout())
-    first_letter_context.force_legacy_layout = true;
+  if (first_letter_text) {
+    first_letter_context.parent = first_letter_text->Parent();
+    if (first_letter_context.parent->ForceLegacyLayout())
+      first_letter_context.force_legacy_layout = true;
+  }
   PseudoElement::AttachLayoutTree(first_letter_context);
-  AttachFirstLetterTextLayoutObjects(first_letter_text);
+  if (first_letter_text)
+    AttachFirstLetterTextLayoutObjects(first_letter_text);
 }
 
 void FirstLetterPseudoElement::DetachLayoutTree(bool performing_reattach) {
