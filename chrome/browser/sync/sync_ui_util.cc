6ffef3167796711f23246e58cae380708d6da7aa
Marc Treib <treib@chromium.org>
2019-06-24 12:35:18 +0000

    Pre-Unity Sync settings: Fix non-clickable row after dashboard reset
    This fixes a regression in the pre-Unity version of Sync settings that
    was introduced by https://crrev.com/c/1559176: In the case where Sync
    was reset from the dashboard, the "Sync" row in chrome://settings was
    not clickable. The reason was that sync_ui_util::GetStatusLabels
    returned SYNC_ERROR, but no corresponding ActionType.
    This CL changes back to returning PRE_SYNCED in this case, specifically
    for pre-Unity. (It's important that the post-Unity UI continues to
    treat this case as an error.)
    
    Bug: 977980
    Change-Id: I6bfd116ee605dfbf1b673b2f8c0d9be05efb75df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1674006
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671640}
    
    Former-commit-id: 6f6c4729944166f2236bcdaa067bf7743c13c773

--- chrome/browser/sync/sync_ui_util.cc
+++ chrome/browser/sync/sync_ui_util.cc
@@ -13,6 +13,7 @@
 #include "components/strings/grit/components_strings.h"
 #include "components/sync/driver/sync_service.h"
 #include "components/sync/driver/sync_user_settings.h"
+#include "components/unified_consent/feature.h"
 #include "google_apis/gaia/google_service_auth_error.h"
 #include "services/identity/public/cpp/identity_manager.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -153,7 +154,11 @@ MessageType GetStatusLabelsImpl(
       *status_label =
           l10n_util::GetStringUTF16(IDS_SIGNED_IN_WITH_SYNC_SUPPRESSED);
     }
-    return SYNC_ERROR;
+    // Note: The pre-UnifiedConsent UI handles this case differently and does
+    // *not* treat it as an error. If we wanted to treat it as an error, we'd
+    // also have to set |link_label| and |action_type|, see crbug.com/977980.
+    return unified_consent::IsUnifiedConsentFeatureEnabled() ? SYNC_ERROR
+                                                             : PRE_SYNCED;
   }
 
   if (service->GetUserSettings()->IsFirstSetupComplete()) {
