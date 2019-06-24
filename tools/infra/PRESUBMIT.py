e808e636b4040500a2a9bccf6ca9bb4e5c8ef6b0
John Budorick <jbudorick@chromium.org>
2019-06-24 18:59:08 +0000

    infra: add a simple script to clobber luci builders.
    also add presubmit pylint & autoformatting to //tools/infra.
    
    Bug: 977664
    Change-Id: Ib08fc7f6ded5d565cb68a238a5f7839d59496ef4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1673081
    Auto-Submit: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Stephen Martinis <martiniss@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671736}
    
    Former-commit-id: 67c9d8a5215d64bd9e6e53d89f69b74209cfe80f

--- tools/infra/PRESUBMIT.py
+++ tools/infra/PRESUBMIT.py
@@ -10,15 +10,14 @@ for more details on the presubmit API built into depot_tools.
 
 
 def _CommonChecks(input_api, output_api):
-  commands = []
+  results = []
+  results.extend(input_api.canned_checks.RunPylint(input_api, output_api))
 
+  commands = []
   commands.extend(input_api.canned_checks.GetUnitTestsRecursively(
       input_api, output_api,
       input_api.os_path.join(input_api.PresubmitLocalPath()),
       whitelist=[r'.+_unittest\.py$'], blacklist=[]))
-
-  results = []
-
   results.extend(input_api.RunTests(commands))
 
   return results
