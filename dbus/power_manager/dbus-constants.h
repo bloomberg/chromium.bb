7e90794dfac8e32ce3ecc14029046d7e05fd91b2
alanlxl <alanlxl@chromium.org>
2019-06-24 10:45:45 +1000

    power_manager: Remove kScreenDimImminentSignal and kDeferScreenDimMethod
    Powerd was made dual-functional in https://crrev.com/c/1601992, to
    support both old Chrome which consumes kScreenDimImminentSignal, and new
    one(https://crrev.com/c/1598921) which exports kShouldDeferScreenDimMethod to
    powerd.
    Now the new chrome is uprevved, we can remove the
    kScreenDimImminentSignal related part from powerd.
    
    Besides, this CL introduces a new timestamp
    last_request_smart_dim_decision_time_ to avoid unexpected
    RequestSmartDimDecision method call.
    
    BUG=chromium:933199
    TEST=Unit test passed
    
    Change-Id: Id9a07840aa9460a1a86448a983d595c48bd15c56
    Reviewed-on: https://chromium-review.googlesource.com/1687002
    Tested-by: Xinglong Luan <alanlxl@chromium.org>
    Commit-Ready: Xinglong Luan <alanlxl@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Cr-Mirrored-From: https://chromium.googlesource.com/chromiumos/platform2
    Cr-Mirrored-Commit: c4a2c4cb75b2e9f5073906ac3e90fc0bf2e32963

--- dbus/power_manager/dbus-constants.h
+++ dbus/power_manager/dbus-constants.h
@@ -45,9 +45,6 @@ const char kHandlePowerButtonAcknowledgmentMethod[] =
 const char kIgnoreNextPowerButtonPressMethod[] = "IgnoreNextPowerButtonPress";
 const char kRecordDarkResumeWakeReasonMethod[] = "RecordDarkResumeWakeReason";
 const char kGetInactivityDelaysMethod[] = "GetInactivityDelays";
-// TODO(alanlxl): remove kDeferScreenDimMethod after chrome is uprevved.
-// https://crrev.com/c/1598921
-const char kDeferScreenDimMethod[] = "DeferScreenDim";
 const char kCreateArcTimersMethod[] = "CreateArcTimers";
 const char kStartArcTimerMethod[] = "StartArcTimer";
 const char kDeleteArcTimersMethod[] = "DeleteArcTimers";
@@ -61,9 +58,6 @@ const char kSuspendImminentSignal[] = "SuspendImminent";
 const char kDarkSuspendImminentSignal[] = "DarkSuspendImminent";
 const char kSuspendDoneSignal[] = "SuspendDone";
 const char kInputEventSignal[] = "InputEvent";
-// TODO(alanlxl): remove kScreenDimImminentSignal after chrome is uprevved.
-// https://crrev.com/c/1598921
-const char kScreenDimImminentSignal[] = "ScreenDimImminent";
 const char kIdleActionImminentSignal[] = "IdleActionImminent";
 const char kIdleActionDeferredSignal[] = "IdleActionDeferred";
 const char kScreenIdleStateChangedSignal[] = "ScreenIdleStateChanged";
