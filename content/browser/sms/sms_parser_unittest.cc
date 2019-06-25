a5315d659c134bffed9f9e4e0f08e4d84d84fba1
Ayu Ishii <ayui@chromium.org>
2019-06-25 00:17:16 +0000

    [sms] Change from using "From:" in SMS format to using "To:"
    This change updates the SMS formatting to use "To:" instead of
    "From:"[1]. This seems more appropriate because there isn't any
    guarantee that it is actually going to be coming from
    what the address states. The intent is to direct a message
    "to" an origin, independently of "where/from" it is coming from.
    
    [1] https://github.com/samuelgoto/sms-receiver#formatting
    
    Bug: 976346
    Change-Id: Iaf1358661b45a5a2066f4d8c3eac2bc294c10511
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1669985
    Reviewed-by: Jun Cai <juncai@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#671869}
    
    Former-commit-id: aa5c726068d431d18ca354033daea48c5d51059a

--- content/browser/sms/sms_parser_unittest.cc
+++ content/browser/sms/sms_parser_unittest.cc
@@ -16,32 +16,32 @@ TEST(SmsParserTest, NoToken) {
 }
 
 TEST(SmsParserTest, WithTokenInvalidUrl) {
-  ASSERT_FALSE(SmsParser::Parse("From: foo"));
+  ASSERT_FALSE(SmsParser::Parse("To: foo"));
 }
 
 TEST(SmsParserTest, NoSpace) {
-  ASSERT_FALSE(SmsParser::Parse("From:https://example.com"));
+  ASSERT_FALSE(SmsParser::Parse("To:https://example.com"));
 }
 
 TEST(SmsParserTest, InvalidUrl) {
-  ASSERT_FALSE(SmsParser::Parse("From: //example.com"));
+  ASSERT_FALSE(SmsParser::Parse("To: //example.com"));
 }
 
 TEST(SmsParserTest, FtpScheme) {
-  ASSERT_FALSE(SmsParser::Parse("From: ftp://example.com"));
+  ASSERT_FALSE(SmsParser::Parse("To: ftp://example.com"));
 }
 
 TEST(SmsParserTest, HttpScheme) {
-  ASSERT_FALSE(SmsParser::Parse("From: http://example.com"));
+  ASSERT_FALSE(SmsParser::Parse("To: http://example.com"));
 }
 
 TEST(SmsParserTest, Mailto) {
-  ASSERT_FALSE(SmsParser::Parse("From: mailto:goto@chromium.org"));
+  ASSERT_FALSE(SmsParser::Parse("To: mailto:goto@chromium.org"));
 }
 
 TEST(SmsParserTest, Basic) {
   base::Optional<url::Origin> origin =
-      SmsParser::Parse("From: https://example.com");
+      SmsParser::Parse("To: https://example.com");
 
   GURL url("https://example.com");
   ASSERT_EQ(origin, url::Origin::Create(url));
@@ -49,7 +49,7 @@ TEST(SmsParserTest, Basic) {
 
 TEST(SmsParserTest, Realistic) {
   base::Optional<url::Origin> origin = SmsParser::Parse(
-      "<#> Your OTP is 1234ABC.\nFrom: https://example.com?s3LhKBB0M33");
+      "<#> Your OTP is 1234ABC.\nTo: https://example.com?s3LhKBB0M33");
 
   GURL url("https://example.com");
   ASSERT_EQ(origin, url::Origin::Create(url));
@@ -57,7 +57,7 @@ TEST(SmsParserTest, Realistic) {
 
 TEST(SmsParserTest, Paths) {
   base::Optional<url::Origin> origin =
-      SmsParser::Parse("From: https://example.com/foobar");
+      SmsParser::Parse("To: https://example.com/foobar");
 
   GURL url("https://example.com");
   ASSERT_EQ(origin, url::Origin::Create(url));
@@ -65,7 +65,7 @@ TEST(SmsParserTest, Paths) {
 
 TEST(SmsParserTest, Message) {
   base::Optional<url::Origin> origin =
-      SmsParser::Parse("hello world\nFrom: https://example.com");
+      SmsParser::Parse("hello world\nTo: https://example.com");
 
   GURL url("https://example.com");
   ASSERT_EQ(origin, url::Origin::Create(url));
@@ -73,7 +73,7 @@ TEST(SmsParserTest, Message) {
 
 TEST(SmsParserTest, Whitespace) {
   base::Optional<url::Origin> origin =
-      SmsParser::Parse("hello world\nFrom: https://example.com ");
+      SmsParser::Parse("hello world\nTo: https://example.com ");
 
   GURL url("https://example.com");
   ASSERT_EQ(origin, url::Origin::Create(url));
@@ -81,7 +81,7 @@ TEST(SmsParserTest, Whitespace) {
 
 TEST(SmsParserTest, Newlines) {
   base::Optional<url::Origin> origin =
-      SmsParser::Parse("hello world\nFrom: https://example.com\n");
+      SmsParser::Parse("hello world\nTo: https://example.com\n");
 
   GURL url("https://example.com");
   ASSERT_EQ(origin, url::Origin::Create(url));
@@ -89,15 +89,15 @@ TEST(SmsParserTest, Newlines) {
 
 TEST(SmsParserTest, TwoTokens) {
   base::Optional<url::Origin> origin =
-      SmsParser::Parse("From: https://a.com From: https://b.com");
+      SmsParser::Parse("To: https://a.com To: https://b.com");
 
   GURL url("https://b.com");
   ASSERT_EQ(origin, url::Origin::Create(url));
 }
 
 TEST(SmsParserTest, AppHash) {
-  base::Optional<url::Origin> origin = SmsParser::Parse(
-      "<#> Hello World\nFrom: https://example.com?s3LhKBB0M33");
+  base::Optional<url::Origin> origin =
+      SmsParser::Parse("<#> Hello World\nTo: https://example.com?s3LhKBB0M33");
 
   GURL url("https://example.com");
   ASSERT_EQ(origin, url::Origin::Create(url));
