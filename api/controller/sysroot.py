6682ae1595bf15348ce7e851ff4638ec8b6bb820
Alex Klein <saklein@chromium.org>
2019-06-25 13:50:32 -0600

    Binhosts: enable PCQ postsubmit binhosts.
    BUG=chromium:963528
    TEST=run_tests, manual
    
    Change-Id: I3cd0727791f131e9c3934497112941cc32b031f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/1677133
    Tested-by: Alex Klein <saklein@chromium.org>
    Reviewed-by: David Burger <dburger@chromium.org>
    Auto-Submit: Alex Klein <saklein@chromium.org>

--- api/controller/sysroot.py
+++ api/controller/sysroot.py
@@ -54,8 +54,7 @@ def InstallToolchain(input_proto, output_proto):
 
   build_target = build_target_util.BuildTarget(name=build_target_name)
   target_sysroot = sysroot_lib.Sysroot(sysroot_path)
-  run_configs = sysroot.SetupBoardRunConfig(usepkg=not compile_source,
-                                            use_parallel_binhost=True)
+  run_configs = sysroot.SetupBoardRunConfig(usepkg=not compile_source)
 
   if not target_sysroot.Exists():
     cros_build_lib.Die('Sysroot has not been created. Run Create first.')
