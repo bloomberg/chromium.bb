dad2f4046d59fbb9477d9e7473a16d8c9d458c04
Daniel Stone <daniels@collabora.com>
2019-06-25 20:29:04 +0100

    gl-renderer: Rename border_damage variable
    Technically it is storing which areas of the border are damaged.
    However, we already have damage-region variables which need to be
    translated by the border region. Rename the variable to not contain the
    word 'damage' to reduce confusion.
    
    Signed-off-by: Daniel Stone <daniels@collabora.com>

--- libweston/renderer-gl/gl-renderer.c
+++ libweston/renderer-gl/gl-renderer.c
@@ -1435,7 +1435,7 @@ gl_renderer_repaint_output(struct weston_output *output,
 	EGLBoolean ret;
 	static int errored;
 	pixman_region32_t buffer_damage, total_damage;
-	enum gl_border_status border_damage = BORDER_STATUS_CLEAN;
+	enum gl_border_status border_status = BORDER_STATUS_CLEAN;
 	struct weston_view *view;
 
 	if (use_output(output) < 0)
@@ -1491,18 +1491,18 @@ gl_renderer_repaint_output(struct weston_output *output,
 	pixman_region32_init(&total_damage);
 	pixman_region32_init(&buffer_damage);
 
-	output_get_damage(output, &buffer_damage, &border_damage);
+	output_get_damage(output, &buffer_damage, &border_status);
 	output_rotate_damage(output, output_damage, go->border_status);
 
 	pixman_region32_union(&total_damage, &buffer_damage, output_damage);
-	border_damage |= go->border_status;
+	border_status |= go->border_status;
 
 	repaint_views(output, &total_damage);
 
 	pixman_region32_fini(&total_damage);
 	pixman_region32_fini(&buffer_damage);
 
-	draw_output_borders(output, border_damage);
+	draw_output_borders(output, border_status);
 
 	pixman_region32_copy(&output->previous_damage, output_damage);
 	wl_signal_emit(&output->frame_signal, output);
