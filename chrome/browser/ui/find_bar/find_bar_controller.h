e23379cc5ba5e500133d30aa7354098b08cd7bae
Rune Lillesveen <futhark@chromium.org>
2019-06-26 13:51:21 +0000

    Revert "Prepopulate find with selection."
    This reverts commit b30e442406c2f7804ca5acf40bcd780ed1e93174 [formerly febc547159134758df2c979342e742501d86d5e1].
    
    Reason for revert: FindInPageTest.SelectionDuringFind has been flaky
    linux-chromeos-rel since introduced.
    
    Original change's description:
    > Prepopulate find with selection.
    > 
    > Currently, the find box only considers its own history when opening,
    > but sometimes the user has selected a term on the page and wishes
    > to search for additional instances (especially when looking at source
    > code.)
    > 
    > To expedite this (and prevent needing to use the clipboard), this
    > change considers the active text selection (if any) when the find
    > widget is invoked.
    > 
    > Bug: 935157
    > Change-Id: Ibdc6a809471fed9aa2c6b6d31f010cbcefb5aa1f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1644229
    > Commit-Queue: Jeff Fisher <jeffish@microsoft.com>
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#672342}
    
    TBR=pkasting@chromium.org,jeffish@microsoft.com
    
    Change-Id: I33b7325140b30826653bf39ac7b51a926beb2273
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 935157, 978836
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1678557
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672494}
    
    Former-commit-id: 620cc18435c84fe0776291a886e36195ac95f5e9

--- chrome/browser/ui/find_bar/find_bar_controller.h
+++ chrome/browser/ui/find_bar/find_bar_controller.h
@@ -48,11 +48,7 @@ class FindBarController : public content::NotificationObserver {
   ~FindBarController() override;
 
   // Shows the find bar. Any previous search string will again be visible.
-  // The find operation will also be started depending on |find_next| and
-  // if there is currently a text selection. |find_next| means the user
-  // used a command to advance the search and |forward_direction| indicates if
-  // the find should be forward or backwards.
-  void Show(bool find_next = false, bool forward_direction = true);
+  void Show();
 
   // Ends the current session. |selection_action| specifies what to do with the
   // selection on the page created by the find operation. |results_action|
@@ -107,9 +103,6 @@ class FindBarController : public content::NotificationObserver {
   // tab. Mac has a global value for search, so this function does nothing on
   // Mac.
   void MaybeSetPrepopulateText();
-
-  // Gets the text that is selected in the current tab, or an empty string.
-  base::string16 GetSelectedText();
 
   content::NotificationRegistrar registrar_;
 
