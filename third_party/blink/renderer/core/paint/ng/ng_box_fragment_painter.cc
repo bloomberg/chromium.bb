dc292ff078669705bdaf3eafe6bb343cbe7948fb
Rune Lillesveen <futhark@chromium.org>
2019-06-26 08:33:56 +0000

    Revert "[LayoutNG] Snap bounds rect to pixel before hit testing inline boxes"
    This reverts commit 75062f9dc1bfdf02d40e9f19c26599f24d03c36c [formerly f2dd14b632881ebd7b2fb68747a1f16c8bd2ed96].
    
    Reason for revert: Suspected cause of test failures for intersection
    observer test on multiple Mac bots.
    
    For instance: https://ci.chromium.org/p/chromium/builders/ci/Mac10.11%20Tests/39520
    
    Original change's description:
    > [LayoutNG] Snap bounds rect to pixel before hit testing inline boxes
    > 
    > This patch fixes inline hit-testing by snapping bounds rect
    > only for inline boxes. The behavior matches to legacy.
    > 
    > Bug: 976606
    > Change-Id: I2532fd3d3439ec5e0bc3f73408b9cdc9b5454a7d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1676564
    > Commit-Queue: Koji Ishii <kojii@chromium.org>
    > Reviewed-by: Emil A Eklund <eae@chromium.org>
    > Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#672419}
    
    TBR=wangxianzhu@chromium.org,eae@chromium.org,kojii@chromium.org,xiaochengh@chromium.org
    
    Change-Id: I232519ecb6dfe5e0edaded9da11edcc67614e49c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 976606
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1678037
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672432}
    
    Former-commit-id: 46f5f2ef73446befd224c8e35b3930d2e1113b79

--- third_party/blink/renderer/core/paint/ng/ng_box_fragment_painter.cc
+++ third_party/blink/renderer/core/paint/ng/ng_box_fragment_painter.cc
@@ -986,7 +986,6 @@ bool NGBoxFragmentPainter::NodeAtPoint(HitTestResult& result,
                                        const HitTestLocation& hit_test_location,
                                        const PhysicalOffset& physical_offset,
                                        HitTestAction action) {
-  const NGPhysicalBoxFragment& fragment = PhysicalFragment();
   const PhysicalSize& size = box_fragment_.Size();
   const ComputedStyle& style = box_fragment_.Style();
 
@@ -1040,10 +1039,6 @@ bool NGBoxFragmentPainter::NodeAtPoint(HitTestResult& result,
       bounds_rect = box_fragment_.SelfInkOverflow();
       bounds_rect.Move(physical_offset);
     }
-    // TODO(kojii): Don't have good explanation why only inline box needs to
-    // snap, but matches to legacy and fixes crbug.com/976606.
-    if (fragment.IsInlineBox())
-      bounds_rect = PhysicalRect(PixelSnappedIntRect(bounds_rect));
     if (hit_test_location.Intersects(bounds_rect)) {
       Node* node = box_fragment_.NodeForHitTest();
       if (!result.InnerNode() && node) {
@@ -1087,9 +1082,6 @@ bool NGBoxFragmentPainter::HitTestTextFragment(
     rect.Move(border_rect.offset);
   }
 
-  // TODO(kojii): Don't have good explanation why only inline box needs to
-  // snap, but matches to legacy and fixes crbug.com/976606.
-  rect = PhysicalRect(PixelSnappedIntRect(rect));
   if (FragmentVisibleToHitTestRequest(text_paint_fragment,
                                       result.GetHitTestRequest()) &&
       hit_test_location.Intersects(rect)) {
@@ -1132,10 +1124,7 @@ bool NGBoxFragmentPainter::HitTestLineBoxFragment(
     return false;
 
   const PhysicalSize size = fragment.Size();
-  PhysicalRect bounds_rect(physical_offset, size);
-  // TODO(kojii): Don't have good explanation why only inline box needs to
-  // snap, but matches to legacy and fixes crbug.com/976606.
-  bounds_rect = PhysicalRect(PixelSnappedIntRect(bounds_rect));
+  const PhysicalRect bounds_rect(physical_offset, size);
   const ComputedStyle& containing_box_style = box_fragment_.Style();
   if (containing_box_style.HasBorderRadius() &&
       !hit_test_location.Intersects(containing_box_style.GetRoundedBorderFor(
