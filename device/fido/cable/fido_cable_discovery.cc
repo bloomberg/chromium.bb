98810a70a8b6f8578133f30365f6dc5c1cd5d1d6
Adam Langley <agl@chromium.org>
2019-06-27 15:50:10 +0000

    device/fido: also deduplicate caBLE handshakes by auth EID.
    Currently caBLE handshakes are limited to one per authenticator BLE
    address. However, authenticators can rotate their BLE address at will
    and so this isn't completely effective. This change also limits caBLE
    handshakes to at most one per authenticator EID to try and address this.
    
    Change-Id: I5d234da0dff6684a8d73c371e073fe385624ae10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1674687
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672915}
    
    Former-commit-id: db79e3f29b2ff05f7bd081ffd785d486c1f69271

--- device/fido/cable/fido_cable_discovery.cc
+++ device/fido/cable/fido_cable_discovery.cc
@@ -290,8 +290,13 @@ void FidoCableDiscovery::RecordAdvertisementResult(bool is_success) {
 void FidoCableDiscovery::CableDeviceFound(BluetoothAdapter* adapter,
                                           BluetoothDevice* device) {
   const auto* found_cable_device_data = GetCableDiscoveryData(device);
-  if (!found_cable_device_data)
+  const std::string device_address = device->GetAddress();
+  if (!found_cable_device_data ||
+      base::Contains(active_authenticator_eids_,
+                     found_cable_device_data->authenticator_eid) ||
+      base::Contains(active_devices_, device_address)) {
     return;
+  }
 
   FIDO_LOG(EVENT) << "Found new caBLE device.";
   // Nonce is embedded as first 8 bytes of client EID.
@@ -303,6 +308,8 @@ void FidoCableDiscovery::CableDeviceFound(BluetoothAdapter* adapter,
 
   auto cable_device =
       std::make_unique<FidoCableDevice>(adapter, device->GetAddress());
+  active_authenticator_eids_.insert(found_cable_device_data->authenticator_eid);
+  active_devices_.insert(device_address);
   StopAdvertisements(
       base::BindOnce(&FidoCableDiscovery::ConductEncryptionHandshake,
                      weak_factory_.GetWeakPtr(), std::move(cable_device),
@@ -313,17 +320,10 @@ void FidoCableDiscovery::ConductEncryptionHandshake(
     std::unique_ptr<FidoCableDevice> cable_device,
     base::span<const uint8_t, kSessionPreKeySize> session_pre_key,
     base::span<const uint8_t, 8> nonce) {
-  // At most one handshake messages should be exchanged for each Cable device.
-  if (base::Contains(cable_handshake_handlers_, cable_device->GetId())) {
-    FIDO_LOG(DEBUG) << "We've already exchanged a handshake with this device.";
-    return;
-  }
-
   auto handshake_handler =
       CreateHandshakeHandler(cable_device.get(), session_pre_key, nonce);
   auto* const handshake_handler_ptr = handshake_handler.get();
-  cable_handshake_handlers_.emplace(cable_device->GetId(),
-                                    std::move(handshake_handler));
+  cable_handshake_handlers_.emplace_back(std::move(handshake_handler));
 
   handshake_handler_ptr->InitiateCableHandshake(
       base::BindOnce(&FidoCableDiscovery::ValidateAuthenticatorHandshakeMessage,
