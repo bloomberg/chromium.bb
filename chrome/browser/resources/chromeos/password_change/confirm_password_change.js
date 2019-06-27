bfe0681647a9b915b331f848a83deb570ef5c156
A Olsen <olsen@chromium.org>
2019-06-27 17:06:18 +0000

    Add form validation to confirm-password-change
    Bug: 930109
    Change-Id: I06c74a168381fcff4dfd6e9da90a21c996832ae4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1680462
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: A Olsen <olsen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#672943}
    
    Former-commit-id: e574594918e18b6c2c87cce4bce2c24b1c72fa8b

--- chrome/browser/resources/chromeos/password_change/confirm_password_change.js
+++ chrome/browser/resources/chromeos/password_change/confirm_password_change.js
@@ -9,13 +9,48 @@
  */
 
 // TODO(https://crbug.com/930109): Logic is not done. Need to add logic to
-// show a spinner, to show only some of the password fields, to show errors,
+// show a spinner, to show only some of the password fields,
 // and to handle clicks on the save button.
+
+/** @enum{number} */
+const ValidationErrorType = {
+  NO_ERROR: 0,
+  MISSING_OLD_PASSWORD: 1,
+  MISSING_NEW_PASSWORD: 2,
+  MISSING_CONFIRM_NEW_PASSWORD: 3,
+  PASSWORDS_DO_NOT_MATCH: 4,
+};
 
 Polymer({
   is: 'confirm-password-change',
 
   behaviors: [I18nBehavior, WebUIListenerBehavior],
+
+  properties: {
+    /** @private {string} */
+    old_password_: {
+      type: String,
+      value: '',
+    },
+
+    /** @private {string} */
+    new_password_: {
+      type: String,
+      value: '',
+    },
+
+    /** @private {string} */
+    confirm_new_password_: {
+      type: String,
+      value: '',
+    },
+
+    /** @private {!ValidationErrorType} */
+    currentValidationError_: {
+      type: Number,
+      value: ValidationErrorType.NO_ERROR,
+    },
+  },
 
   /** @override */
   attached: function() {
@@ -25,5 +60,76 @@ Polymer({
   /** @private */
   cancel_: function() {
     this.$.dialog.cancel();
+  },
+
+  /**
+   * @private
+   */
+  onSaveTap_: function() {
+    this.currentValidationError_ = this.findFirstError_();
+    if (this.currentValidationError_ == ValidationErrorType.NO_ERROR) {
+      // TODO(olsen): Send a message to a handler to change the password,
+      // instead of just showing an alert.
+      alert(
+          'changePassword(' + this.old_password_ + ', ' + this.new_password_ +
+          ')');
+    }
+  },
+
+  /**
+   * @return {!ValidationErrorType}
+   * @private
+   */
+  findFirstError_: function() {
+    if (!this.old_password_) {
+      return ValidationErrorType.MISSING_OLD_PASSWORD;
+    }
+    if (!this.new_password_) {
+      return ValidationErrorType.MISSING_NEW_PASSWORD;
+    }
+    if (!this.confirm_new_password_) {
+      return ValidationErrorType.MISSING_CONFIRM_NEW_PASSWORD;
+    }
+    if (this.new_password_ != this.confirm_new_password_) {
+      return ValidationErrorType.PASSWORDS_DO_NOT_MATCH;
+    }
+    return ValidationErrorType.NO_ERROR;
+  },
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  invalidOldPassword_: function() {
+    return this.currentValidationError_ ==
+        ValidationErrorType.MISSING_OLD_PASSWORD;
+  },
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  invalidNewPassword_: function() {
+    return this.currentValidationError_ ==
+        ValidationErrorType.MISSING_NEW_PASSWORD;
+  },
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  invalidConfirmNewPassword_: function() {
+    return this.currentValidationError_ ==
+        ValidationErrorType.MISSING_CONFIRM_NEW_PASSWORD ||
+        this.passwordsDoNotMatch_();
+  },
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  passwordsDoNotMatch_: function() {
+    return this.currentValidationError_ ==
+        ValidationErrorType.PASSWORDS_DO_NOT_MATCH;
   },
 });
