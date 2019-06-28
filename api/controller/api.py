104b067af6a2eea63e010a4d2a3038c59f178436
Alex Klein <saklein@chromium.org>
2019-06-28 15:43:00 -0600

    Build API: Add version fetching endpoint.
    BUG=chromium:979814
    TEST=ran endpoint manually
    
    Change-Id: Ifd400e6d7f92286f8313927fa5200e011a1fa395
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/1682829
    Commit-Queue: Alex Klein <saklein@chromium.org>
    Tested-by: Alex Klein <saklein@chromium.org>
    Reviewed-by: Evan Hernandez <evanhernandez@chromium.org>
    Auto-Submit: Alex Klein <saklein@chromium.org>

--- api/controller/api.py
+++ api/controller/api.py
@@ -9,9 +9,24 @@ from __future__ import print_function
 
 from chromite.api import router as router_lib
 
+# API version number.
+# The major version MUST be updated on breaking changes.
+VERSION_MAJOR = 1
+# The minor and bug versions are not currently utilized, but put in place
+# to simplify future requirements.
+VERSION_MINOR = 0
+VERSION_BUG = 0
+
 
 def GetMethods(_input_proto, output_proto):
   """List all of the registered methods."""
   router = router_lib.GetRouter()
   for method in router.ListMethods():
     output_proto.methods.add().method = method
+
+
+def GetVersion(_input_proto, output_proto):
+  """Get the Build API major version number."""
+  output_proto.version.major = VERSION_MAJOR
+  output_proto.version.minor = VERSION_MINOR
+  output_proto.version.bug = VERSION_BUG
--- api/gen/chromite/api/api_pb2.py
+++ api/gen/chromite/api/api_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='chromite.api',
   syntax='proto3',
   serialized_options=_b('Z6go.chromium.org/chromiumos/infra/proto/go/chromite/api'),
-  serialized_pb=_b('\n\x16\x63hromite/api/api.proto\x12\x0c\x63hromite.api\x1a\x1c\x63hromite/api/build_api.proto\"\x12\n\x10MethodGetRequest\"f\n\x11MethodGetResponse\x12\x37\n\x07methods\x18\x01 \x03(\x0b\x32&.chromite.api.MethodGetResponse.Method\x1a\x18\n\x06Method\x12\x0e\n\x06method\x18\x01 \x01(\t2v\n\rMethodService\x12X\n\x03Get\x12\x1e.chromite.api.MethodGetRequest\x1a\x1f.chromite.api.MethodGetResponse\"\x10\xc2\xed\x1a\x0c\n\nGetMethods\x1a\x0b\xc2\xed\x1a\x07\n\x03\x61pi\x10\x02\x42\x38Z6go.chromium.org/chromiumos/infra/proto/go/chromite/apib\x06proto3')
+  serialized_pb=_b('\n\x16\x63hromite/api/api.proto\x12\x0c\x63hromite.api\x1a\x1c\x63hromite/api/build_api.proto\"\x12\n\x10MethodGetRequest\"f\n\x11MethodGetResponse\x12\x37\n\x07methods\x18\x01 \x03(\x0b\x32&.chromite.api.MethodGetResponse.Method\x1a\x18\n\x06Method\x12\x0e\n\x06method\x18\x01 \x01(\t\"\x13\n\x11VersionGetRequest\"\x85\x01\n\x12VersionGetResponse\x12\x39\n\x07version\x18\x01 \x01(\x0b\x32(.chromite.api.VersionGetResponse.Version\x1a\x34\n\x07Version\x12\r\n\x05major\x18\x01 \x01(\x05\x12\r\n\x05minor\x18\x02 \x01(\x05\x12\x0b\n\x03\x62ug\x18\x03 \x01(\x05\x32v\n\rMethodService\x12X\n\x03Get\x12\x1e.chromite.api.MethodGetRequest\x1a\x1f.chromite.api.MethodGetResponse\"\x10\xc2\xed\x1a\x0c\n\nGetMethods\x1a\x0b\xc2\xed\x1a\x07\n\x03\x61pi\x10\x02\x32y\n\x0eVersionService\x12Z\n\x03Get\x12\x1f.chromite.api.VersionGetRequest\x1a .chromite.api.VersionGetResponse\"\x10\xc2\xed\x1a\x0c\n\nGetVersion\x1a\x0b\xc2\xed\x1a\x07\n\x03\x61pi\x10\x02\x42\x38Z6go.chromium.org/chromiumos/infra/proto/go/chromite/apib\x06proto3')
   ,
   dependencies=[chromite_dot_api_dot_build__api__pb2.DESCRIPTOR,])
 
@@ -111,10 +111,113 @@ _METHODGETRESPONSE = _descriptor.Descriptor(
   serialized_end=192,
 )
 
+
+_VERSIONGETREQUEST = _descriptor.Descriptor(
+  name='VersionGetRequest',
+  full_name='chromite.api.VersionGetRequest',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=194,
+  serialized_end=213,
+)
+
+
+_VERSIONGETRESPONSE_VERSION = _descriptor.Descriptor(
+  name='Version',
+  full_name='chromite.api.VersionGetResponse.Version',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='major', full_name='chromite.api.VersionGetResponse.Version.major', index=0,
+      number=1, type=5, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='minor', full_name='chromite.api.VersionGetResponse.Version.minor', index=1,
+      number=2, type=5, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='bug', full_name='chromite.api.VersionGetResponse.Version.bug', index=2,
+      number=3, type=5, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=297,
+  serialized_end=349,
+)
+
+_VERSIONGETRESPONSE = _descriptor.Descriptor(
+  name='VersionGetResponse',
+  full_name='chromite.api.VersionGetResponse',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='version', full_name='chromite.api.VersionGetResponse.version', index=0,
+      number=1, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[_VERSIONGETRESPONSE_VERSION, ],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=216,
+  serialized_end=349,
+)
+
 _METHODGETRESPONSE_METHOD.containing_type = _METHODGETRESPONSE
 _METHODGETRESPONSE.fields_by_name['methods'].message_type = _METHODGETRESPONSE_METHOD
+_VERSIONGETRESPONSE_VERSION.containing_type = _VERSIONGETRESPONSE
+_VERSIONGETRESPONSE.fields_by_name['version'].message_type = _VERSIONGETRESPONSE_VERSION
 DESCRIPTOR.message_types_by_name['MethodGetRequest'] = _METHODGETREQUEST
 DESCRIPTOR.message_types_by_name['MethodGetResponse'] = _METHODGETRESPONSE
+DESCRIPTOR.message_types_by_name['VersionGetRequest'] = _VERSIONGETREQUEST
+DESCRIPTOR.message_types_by_name['VersionGetResponse'] = _VERSIONGETRESPONSE
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 MethodGetRequest = _reflection.GeneratedProtocolMessageType('MethodGetRequest', (_message.Message,), dict(
@@ -139,6 +242,28 @@ MethodGetResponse = _reflection.GeneratedProtocolMessageType('MethodGetResponse'
 _sym_db.RegisterMessage(MethodGetResponse)
 _sym_db.RegisterMessage(MethodGetResponse.Method)
 
+VersionGetRequest = _reflection.GeneratedProtocolMessageType('VersionGetRequest', (_message.Message,), dict(
+  DESCRIPTOR = _VERSIONGETREQUEST,
+  __module__ = 'chromite.api.api_pb2'
+  # @@protoc_insertion_point(class_scope:chromite.api.VersionGetRequest)
+  ))
+_sym_db.RegisterMessage(VersionGetRequest)
+
+VersionGetResponse = _reflection.GeneratedProtocolMessageType('VersionGetResponse', (_message.Message,), dict(
+
+  Version = _reflection.GeneratedProtocolMessageType('Version', (_message.Message,), dict(
+    DESCRIPTOR = _VERSIONGETRESPONSE_VERSION,
+    __module__ = 'chromite.api.api_pb2'
+    # @@protoc_insertion_point(class_scope:chromite.api.VersionGetResponse.Version)
+    ))
+  ,
+  DESCRIPTOR = _VERSIONGETRESPONSE,
+  __module__ = 'chromite.api.api_pb2'
+  # @@protoc_insertion_point(class_scope:chromite.api.VersionGetResponse)
+  ))
+_sym_db.RegisterMessage(VersionGetResponse)
+_sym_db.RegisterMessage(VersionGetResponse.Version)
+
 
 DESCRIPTOR._options = None
 
@@ -148,8 +273,8 @@ _METHODSERVICE = _descriptor.ServiceDescriptor(
   file=DESCRIPTOR,
   index=0,
   serialized_options=_b('\302\355\032\007\n\003api\020\002'),
-  serialized_start=194,
-  serialized_end=312,
+  serialized_start=351,
+  serialized_end=469,
   methods=[
   _descriptor.MethodDescriptor(
     name='Get',
@@ -164,5 +289,29 @@ _METHODSERVICE = _descriptor.ServiceDescriptor(
 _sym_db.RegisterServiceDescriptor(_METHODSERVICE)
 
 DESCRIPTOR.services_by_name['MethodService'] = _METHODSERVICE
+
+
+_VERSIONSERVICE = _descriptor.ServiceDescriptor(
+  name='VersionService',
+  full_name='chromite.api.VersionService',
+  file=DESCRIPTOR,
+  index=1,
+  serialized_options=_b('\302\355\032\007\n\003api\020\002'),
+  serialized_start=471,
+  serialized_end=592,
+  methods=[
+  _descriptor.MethodDescriptor(
+    name='Get',
+    full_name='chromite.api.VersionService.Get',
+    index=0,
+    containing_service=None,
+    input_type=_VERSIONGETREQUEST,
+    output_type=_VERSIONGETRESPONSE,
+    serialized_options=_b('\302\355\032\014\n\nGetVersion'),
+  ),
+])
+_sym_db.RegisterServiceDescriptor(_VERSIONSERVICE)
+
+DESCRIPTOR.services_by_name['VersionService'] = _VERSIONSERVICE
 
 # @@protoc_insertion_point(module_scope)
