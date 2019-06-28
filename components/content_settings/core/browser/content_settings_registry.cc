101943968da9eebafe490b6d31d627fe0c9b8392
Andy Paicu <andypaicu@chromium.org>
2019-06-28 15:41:17 +0000

    Inherit plugin content setting to incognito.
    This allows flash to be used in incognito as well if the user chooses
    to.
    
    Bug: 974721
    Change-Id: Ibbc825df65b2a0a6b48944cad2bad82cc733eb43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1681762
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673343}
    
    Former-commit-id: 514be7115863f9e40c79c0e4b7685c28aa1431b4

--- components/content_settings/core/browser/content_settings_registry.cc
+++ components/content_settings/core/browser/content_settings_registry.cc
@@ -176,7 +176,7 @@ void ContentSettingsRegistry::Init() {
                          CONTENT_SETTING_DETECT_IMPORTANT_CONTENT),
            WebsiteSettingsInfo::SINGLE_ORIGIN_WITH_EMBEDDED_EXCEPTIONS_SCOPE,
            WebsiteSettingsRegistry::DESKTOP,
-           ContentSettingsInfo::INHERIT_IF_LESS_PERMISSIVE,
+           ContentSettingsInfo::INHERIT_IN_INCOGNITO,
            ContentSettingsInfo::EPHEMERAL,
            ContentSettingsInfo::EXCEPTIONS_ON_SECURE_AND_INSECURE_ORIGINS);
 
