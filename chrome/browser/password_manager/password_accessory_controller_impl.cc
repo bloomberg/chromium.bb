2faf09cac97bbc4944d23310c7513d2d60f57deb
Friedrich Horschig <fhorschig@chromium.org>
2019-06-28 07:50:03 +0000

    [Mfill Android] Suppress passwords from PSL-origin in fallbacks
    This CL removes fallback from PSL origins from the fallbacks since the
    UI has no indication in the accessory V1 to distinguish them from
    regular saved passwords.
    
    (cherry picked from commit 24882cdbca22768d1e461a4724d96f051ff12aa6 [formerly e8dc84de9ae3f45e1b89360e25762946986eb47c])
    
    Bug: 976761
    Change-Id: I466c3396b5c440218e37484ab70909d68d4df5eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1674095
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#672460}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1681700
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3809@{#637}
    Cr-Branched-From: ce45438777a60f42f88228700b3968406c6404d8 [formerly d82dec1a818f378c464ba307ddd9c92133eac355]-refs/heads/master@{#665002}
    
    Former-commit-id: 2966d09434c9b65622a8c8bbbcc90e0008cdb36c

--- chrome/browser/password_manager/password_accessory_controller_impl.cc
+++ chrome/browser/password_manager/password_accessory_controller_impl.cc
@@ -20,6 +20,7 @@
 #include "chrome/browser/ui/passwords/manage_passwords_view_utils.h"
 #include "chrome/browser/vr/vr_tab_helper.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/autofill_util.h"
 #include "components/autofill/core/common/password_form.h"
 #include "components/autofill/core/common/password_generation_util.h"
@@ -157,6 +158,10 @@ void PasswordAccessoryControllerImpl::SavePasswordsForOrigin(
   suggestions->clear();
   for (const auto& pair : best_matches) {
     const PasswordForm* form = pair.second;
+    if (!url::IsSameOriginWith(origin.GetURL(), form->origin) &&
+        !base::FeatureList::IsEnabled(
+            autofill::features::kAutofillKeyboardAccessory))
+      continue;  // Skip matches for PSL origins in V1 which has no UI for that.
     suggestions->emplace_back(form->password_value, GetDisplayUsername(*form),
                               /*selectable=*/!form->username_value.empty());
   }
--- chrome/browser/password_manager/password_accessory_controller_impl_unittest.cc
+++ chrome/browser/password_manager/password_accessory_controller_impl_unittest.cc
@@ -59,6 +59,7 @@ CreateEntry(const std::string& username, const std::string& password) {
   PasswordForm form;
   form.username_value = ASCIIToUTF16(username);
   form.password_value = ASCIIToUTF16(password);
+  form.origin = GURL(kExampleSite);
   std::unique_ptr<const PasswordForm> form_ptr(
       new PasswordForm(std::move(form)));
   auto username_form_pair =
