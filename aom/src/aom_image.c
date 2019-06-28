c00d16904eb77be0b85d40d9fdff4894ac812dbf
Wan-Teh Chang <wtc@google.com>
2019-06-28 14:40:19 -0700

    Fix bits-per-sample (bps) value for I422 & I42216.
    AOM_IMG_FMT_I422 should be 16, not 24, bits per sample. Similarly,
    AOM_IMG_FMT_I42216 should be 32, not 48, bits per sample.
    
    Change-Id: I6b6b2e6dd7d6146fdeeed9c0282a089930ae9a27

--- aom/src/aom_image.c
+++ aom/src/aom_image.c
@@ -57,11 +57,11 @@ static aom_image_t *img_alloc_helper(
     case AOM_IMG_FMT_YV12:
     case AOM_IMG_FMT_AOMI420:
     case AOM_IMG_FMT_AOMYV12: bps = 12; break;
-    case AOM_IMG_FMT_I422:
+    case AOM_IMG_FMT_I422: bps = 16; break;
     case AOM_IMG_FMT_I444: bps = 24; break;
     case AOM_IMG_FMT_YV1216:
     case AOM_IMG_FMT_I42016: bps = 24; break;
-    case AOM_IMG_FMT_I42216:
+    case AOM_IMG_FMT_I42216: bps = 32; break;
     case AOM_IMG_FMT_I44416: bps = 48; break;
     default: bps = 16; break;
   }
