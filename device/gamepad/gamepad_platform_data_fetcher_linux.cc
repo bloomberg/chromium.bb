bloomberg/chromium.bb

commit 8132e83bde83b42efa56584d0144f6d2ae0e47dd

UdevWatcher: Merging UdevLinux into UdevWatcher
UdevLinux and UdevWatcher had similar functionality, this patch
is removing UdevLinux and adding its functionality to UdevWatcher.
It was decided to keep UdevWatcher, because of its ready-made method
to enumerate devices.
From UdevLinux, was kept the filters functionality to be able to select
the event from the subsystem we are interested to monitor.

Major change in UdevWatcher is at creation, by monitoring only devices
matching in the subsystem filter list.

UdevLinux is removed from code base.

All the users of deleted UdevLinux are modified to take into use
UdevWatcher.

Bug: 940514
Change-Id: I1ca191c82da32efc80d3308bb8ad7db4eff6d899
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1543252
Reviewed-by: Reilly Grant <reillyg@chromium.org>
Reviewed-by: Dan Sanders <sandersd@chromium.org>
Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
Cr-Commit-Position: refs/heads/master@{#645840}
Former-commit-id: 99d9fdb47d4e9f5e57e4122fc1d0c798532d442d

device/gamepad/gamepad_platform_data_fetcher_linux.cc
@@ -21,7 +21,6 @@
 #include "device/gamepad/gamepad_uma.h"
 #include "device/gamepad/nintendo_controller.h"
 #include "device/udev_linux/scoped_udev.h"
-#include "device/udev_linux/udev_linux.h"
 
 namespace device {
 
@@ -39,21 +38,26 @@ GamepadSource GamepadPlatformDataFetcherLinux::source() {
 }
 
 void GamepadPlatformDataFetcherLinux::OnAddedToProvider() {
-  std::vector<UdevLinux::UdevMonitorFilter> filters;
-  filters.push_back(
-      UdevLinux::UdevMonitorFilter(UdevGamepadLinux::kInputSubsystem, nullptr));
-  filters.push_back(UdevLinux::UdevMonitorFilter(
-      UdevGamepadLinux::kHidrawSubsystem, nullptr));
-  udev_.reset(new UdevLinux(
-      filters, base::Bind(&GamepadPlatformDataFetcherLinux::RefreshDevice,
-                          base::Unretained(this))));
+  std::vector<UdevWatcher::Filter> filters;
+  filters.emplace_back(UdevGamepadLinux::kInputSubsystem, nullptr);
+  filters.emplace_back(UdevGamepadLinux::kHidrawSubsystem, nullptr);
+  udev_watcher_ = UdevWatcher::StartWatching(this, filters);
 
   for (auto it = devices_.begin(); it != devices_.end(); ++it)
     it->get()->Shutdown();
   devices_.clear();
 
-  EnumerateSubsystemDevices(UdevGamepadLinux::kInputSubsystem);
-  EnumerateSubsystemDevices(UdevGamepadLinux::kHidrawSubsystem);
+  udev_watcher_->EnumerateExistingDevices();
+}
+
+void GamepadPlatformDataFetcherLinux::OnDeviceAdded(
+    ScopedUdevDevicePtr device) {
+  RefreshDevice(device.get());
+}
+
+void GamepadPlatformDataFetcherLinux::OnDeviceRemoved(
+    ScopedUdevDevicePtr device) {
+  RefreshDevice(device.get());
 }
 
 void GamepadPlatformDataFetcherLinux::GetGamepadData(bool) {
@@ -263,37 +267,6 @@ void GamepadPlatformDataFetcherLinux::RefreshHidrawDevice(
       Gamepad& pad = state->data;
       pad.vibration_actuator.not_null = device->SupportsVibration();
     }
-  }
-}
-
-void GamepadPlatformDataFetcherLinux::EnumerateSubsystemDevices(
-    const std::string& subsystem) {
-  if (!udev_->udev_handle())
-    return;
-  ScopedUdevEnumeratePtr enumerate(udev_enumerate_new(udev_->udev_handle()));
-  if (!enumerate)
-    return;
-  int ret =
-      udev_enumerate_add_match_subsystem(enumerate.get(), subsystem.c_str());
-  if (ret != 0)
-    return;
-  ret = udev_enumerate_scan_devices(enumerate.get());
-  if (ret != 0)
-    return;
-
-  udev_list_entry* devices = udev_enumerate_get_list_entry(enumerate.get());
-  for (udev_list_entry* dev_list_entry = devices; dev_list_entry != nullptr;
-       dev_list_entry = udev_list_entry_get_next(dev_list_entry)) {
-    // Get the filename of the /sys entry for the device and create a
-    // udev_device object (dev) representing it
-    const char* path = udev_list_entry_get_name(dev_list_entry);
-    if (!path)
-      continue;
-    ScopedUdevDevicePtr dev(
-        udev_device_new_from_syspath(udev_->udev_handle(), path));
-    if (!dev)
-      continue;
-    RefreshDevice(dev.get());
   }
 }
 

device/gamepad/gamepad_platform_data_fetcher_linux.h
@@ -16,19 +16,17 @@
 #include "device/gamepad/gamepad_device_linux.h"
 #include "device/gamepad/public/cpp/gamepads.h"
 #include "device/gamepad/udev_gamepad_linux.h"
+#include "device/udev_linux/udev_watcher.h"
 
 extern "C" {
 struct udev_device;
 }
 
 namespace device {
-class UdevLinux;
-}
-
-namespace device {
 
 class DEVICE_GAMEPAD_EXPORT GamepadPlatformDataFetcherLinux
-    : public GamepadDataFetcher {
+    : public GamepadDataFetcher,
+      public UdevWatcher::Observer {
  public:
   using Factory = GamepadDataFetcherFactoryImpl<GamepadPlatformDataFetcherLinux,
                                                 GAMEPAD_SOURCE_LINUX_UDEV>;
@@ -59,7 +57,6 @@ class DEVICE_GAMEPAD_EXPORT GamepadPlatformDataFetcherLinux
   void RefreshJoydevDevice(udev_device* dev, const UdevGamepadLinux& pad_info);
   void RefreshEvdevDevice(udev_device* dev, const UdevGamepadLinux& pad_info);
   void RefreshHidrawDevice(udev_device* dev, const UdevGamepadLinux& pad_info);
-  void EnumerateSubsystemDevices(const std::string& subsystem);
   void ReadDeviceData(size_t index);
 
   GamepadDeviceLinux* GetDeviceWithJoydevIndex(int joydev_index);
@@ -68,9 +65,13 @@ class DEVICE_GAMEPAD_EXPORT GamepadPlatformDataFetcherLinux
   void RemoveDevice(GamepadDeviceLinux* device);
   void RemoveDeviceAtIndex(int index);
 
+  // UdevWatcher::Observer overrides
+  void OnDeviceAdded(ScopedUdevDevicePtr device) override;
+  void OnDeviceRemoved(ScopedUdevDevicePtr device) override;
+
   std::unordered_set<std::unique_ptr<GamepadDeviceLinux>> devices_;
 
-  std::unique_ptr<device::UdevLinux> udev_;
+  std::unique_ptr<device::UdevWatcher> udev_watcher_;
 
   DISALLOW_COPY_AND_ASSIGN(GamepadPlatformDataFetcherLinux);
 };

device/udev_linux/udev_watcher.cc
@@ -16,7 +16,9 @@ void UdevWatcher::Observer::OnDeviceAdded(ScopedUdevDevicePtr device) {}
 
 void UdevWatcher::Observer::OnDeviceRemoved(ScopedUdevDevicePtr device) {}
 
-std::unique_ptr<UdevWatcher> UdevWatcher::StartWatching(Observer* observer) {
+std::unique_ptr<UdevWatcher> UdevWatcher::StartWatching(
+    Observer* observer,
+    const std::vector<Filter>& filters) {
   base::ScopedBlockingCall scoped_blocking_call(FROM_HERE,
                                                 base::BlockingType::MAY_BLOCK);
   ScopedUdevPtr udev(udev_new());
@@ -32,6 +34,12 @@ std::unique_ptr<UdevWatcher> UdevWatcher::StartWatching(Observer* observer) {
     return nullptr;
   }
 
+  for (const Filter& filter : filters) {
+    const int ret = udev_monitor_filter_add_match_subsystem_devtype(
+        udev_monitor.get(), filter.subsystem.c_str(), filter.devtype.c_str());
+    CHECK_EQ(0, ret);
+  }
+
   if (udev_monitor_enable_receiving(udev_monitor.get()) != 0) {
     LOG(ERROR) << "Failed to enable receiving udev events.";
     return nullptr;
@@ -44,7 +52,7 @@ std::unique_ptr<UdevWatcher> UdevWatcher::StartWatching(Observer* observer) {
   }
 
   return base::WrapUnique(new UdevWatcher(
-      std::move(udev), std::move(udev_monitor), monitor_fd, observer));
+      std::move(udev), std::move(udev_monitor), monitor_fd, observer, filters));
 }
 
 UdevWatcher::~UdevWatcher() {
@@ -59,6 +67,12 @@ void UdevWatcher::EnumerateExistingDevices() {
   if (!enumerate) {
     LOG(ERROR) << "Failed to initialize a udev enumerator.";
     return;
+  }
+
+  for (const Filter& filter : udev_filters_) {
+    const int ret = udev_enumerate_add_match_subsystem(
+        enumerate.get(), filter.subsystem.c_str());
+    CHECK_EQ(0, ret);
   }
 
   if (udev_enumerate_scan_devices(enumerate.get()) != 0) {
@@ -79,10 +93,12 @@ void UdevWatcher::EnumerateExistingDevices() {
 UdevWatcher::UdevWatcher(ScopedUdevPtr udev,
                          ScopedUdevMonitorPtr udev_monitor,
                          int monitor_fd,
-                         Observer* observer)
+                         Observer* observer,
+                         const std::vector<Filter>& filters)
     : udev_(std::move(udev)),
       udev_monitor_(std::move(udev_monitor)),
-      observer_(observer) {
+      observer_(observer),
+      udev_filters_(filters) {
   file_watcher_ = base::FileDescriptorWatcher::WatchReadable(
       monitor_fd,
       base::Bind(&UdevWatcher::OnMonitorReadable, base::Unretained(this)));

device/udev_linux/udev_watcher.h
@@ -6,10 +6,12 @@
 #define DEVICE_UDEV_LINUX_UDEV_WATCHER_H_
 
 #include <memory>
+#include <vector>
 
 #include "base/files/file_descriptor_watcher_posix.h"
 #include "base/macros.h"
 #include "base/sequence_checker.h"
+#include "base/strings/string_piece.h"
 #include "device/udev_linux/scoped_udev.h"
 
 namespace device {
@@ -25,7 +27,18 @@ class UdevWatcher {
     virtual void OnDeviceRemoved(ScopedUdevDevicePtr device);
   };
 
-  static std::unique_ptr<UdevWatcher> StartWatching(Observer* observer);
+  // subsystem and devtype parameter for
+  // udev_monitor_filter_add_match_subsystem_devtype().
+  struct Filter {
+    Filter(base::StringPiece subsystem_in, base::StringPiece devtype_in)
+        : subsystem(subsystem_in), devtype(devtype_in) {}
+    const std::string subsystem;
+    const std::string devtype;
+  };
+
+  static std::unique_ptr<UdevWatcher> StartWatching(
+      Observer* observer,
+      const std::vector<Filter>& filters = {});
 
   ~UdevWatcher();
 
@@ -37,13 +50,15 @@ class UdevWatcher {
   UdevWatcher(ScopedUdevPtr udev,
               ScopedUdevMonitorPtr udev_monitor,
               int monitor_fd,
-              Observer* observer);
+              Observer* observer,
+              const std::vector<Filter>& filters);
 
   void OnMonitorReadable();
 
   ScopedUdevPtr udev_;
   ScopedUdevMonitorPtr udev_monitor_;
   Observer* observer_;
+  const std::vector<Filter> udev_filters_;
   std::unique_ptr<base::FileDescriptorWatcher::Controller> file_watcher_;
   base::SequenceChecker sequence_checker_;
 

media/device_monitors/device_monitor_udev.cc
@@ -7,13 +7,14 @@
 #include "media/device_monitors/device_monitor_udev.h"
 
 #include <string>
+#include <vector>
 
 #include "base/bind.h"
 #include "base/sequence_checker.h"
 #include "base/system/system_monitor.h"
 #include "base/task/post_task.h"
 #include "device/udev_linux/udev.h"
-#include "device/udev_linux/udev_linux.h"
+#include "device/udev_linux/udev_watcher.h"
 
 namespace {
 
@@ -36,20 +37,25 @@ const SubsystemMap kSubsystemMap[] = {
 
 namespace media {
 
-// Wraps a device::UdevLinux with an API that makes it easier to use from
+// Wraps a device::UdevWatcher with an API that makes it easier to use from
 // DeviceMonitorLinux. Since it is essentially a wrapper around blocking udev
 // calls, Initialize() must be called from a task runner that can block.
-class DeviceMonitorLinux::BlockingTaskRunnerHelper {
+class DeviceMonitorLinux::BlockingTaskRunnerHelper
+    : public device::UdevWatcher::Observer {
  public:
   BlockingTaskRunnerHelper();
-  ~BlockingTaskRunnerHelper() = default;
+  ~BlockingTaskRunnerHelper() override = default;
 
   void Initialize();
 
  private:
-  void OnDevicesChanged(udev_device* device);
+  void OnDevicesChanged(device::ScopedUdevDevicePtr device);
 
-  std::unique_ptr<device::UdevLinux> udev_;
+  // device::UdevWatcher::Observer overrides
+  void OnDeviceAdded(device::ScopedUdevDevicePtr device) override;
+  void OnDeviceRemoved(device::ScopedUdevDevicePtr device) override;
+
+  std::unique_ptr<device::UdevWatcher> udev_watcher_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
@@ -64,24 +70,30 @@ DeviceMonitorLinux::BlockingTaskRunnerHelper::BlockingTaskRunnerHelper() {
 
 void DeviceMonitorLinux::BlockingTaskRunnerHelper::Initialize() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  std::vector<device::UdevLinux::UdevMonitorFilter> filters;
+  std::vector<device::UdevWatcher::Filter> filters;
   for (const SubsystemMap& entry : kSubsystemMap) {
-    filters.push_back(
-        device::UdevLinux::UdevMonitorFilter(entry.subsystem, entry.devtype));
+    filters.emplace_back(entry.subsystem, entry.devtype);
   }
-  udev_ = std::make_unique<device::UdevLinux>(
-      filters, base::BindRepeating(&BlockingTaskRunnerHelper::OnDevicesChanged,
-                                   base::Unretained(this)));
+  udev_watcher_ = device::UdevWatcher::StartWatching(this, filters);
+}
+
+void DeviceMonitorLinux::BlockingTaskRunnerHelper::OnDeviceAdded(
+    device::ScopedUdevDevicePtr device) {
+  OnDevicesChanged(std::move(device));
+}
+
+void DeviceMonitorLinux::BlockingTaskRunnerHelper::OnDeviceRemoved(
+    device::ScopedUdevDevicePtr device) {
+  OnDevicesChanged(std::move(device));
 }
 
 void DeviceMonitorLinux::BlockingTaskRunnerHelper::OnDevicesChanged(
-    udev_device* device) {
+    device::ScopedUdevDevicePtr device) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  DCHECK(device);
 
   base::SystemMonitor::DeviceType device_type =
       base::SystemMonitor::DEVTYPE_UNKNOWN;
-  const std::string subsystem(device::udev_device_get_subsystem(device));
+  const std::string subsystem(device::udev_device_get_subsystem(device.get()));
   for (const SubsystemMap& entry : kSubsystemMap) {
     if (subsystem == entry.subsystem) {
       device_type = entry.device_type;

AUTHORS
@@ -89,6 +89,7 @@ Anuj Kumar Sharma <anujk.sharma@samsung.com>
 Arjun Karthik <arjunkar@amazon.com>
 Arman Ghotb <armanghotb@gmail.com>
 Armin Burgmeier <aburgmeier@bloomberg.net>
+Arnaud Mandy <arnaud.mandy@intel.com>
 Arnaud Renevier <a.renevier@samsung.com>
 Arpita Bahuguna <a.bah@samsung.com>
 Arthur Lussos <developer0420@gmail.com>

device/gamepad/gamepad_device_linux.cc
@@ -17,7 +17,7 @@
 #include "base/strings/string_piece.h"
 #include "base/strings/stringprintf.h"
 #include "device/gamepad/gamepad_data_fetcher.h"
-#include "device/udev_linux/udev_linux.h"
+#include "device/udev_linux/udev.h"
 
 namespace device {
 

device/gamepad/gamepad_platform_data_fetcher.h
@@ -27,7 +27,7 @@
 #include "device/gamepad/gamepad_platform_data_fetcher_mac.h"
 #include "device/gamepad/nintendo_data_fetcher.h"
 #include "device/gamepad/xbox_data_fetcher_mac.h"
-#elif defined(OS_LINUX)
+#elif defined(OS_LINUX) && defined(USE_UDEV)
 #include "device/gamepad/gamepad_platform_data_fetcher_linux.h"
 #include "device/gamepad/nintendo_data_fetcher.h"
 #endif

commit 456f5196a81491b832af21b685b14c6cd78d6688

UdevWatcher: Handle "change" events besides "add" and "remove" ones.
This fixes a regression on Linux introduced in commit
8132e83bde [formerly 99d9fdb47d] ("UdevWatcher: Merging UdevLinux into UdevWatcher") that caused
removing and reinserting a pluggable audio device (e.g. a microphone) to
trigger navigator.mediaDevices.ondevicechange only on removal.

Before commit 8132e83bde [formerly 99d9fdb47d], the UdevLinux class would invoke a given callback
whenever a given udev event happened, regardless of what the actual event
was. UdevWatcher, on the other hand, filters udev events to invoke different
functions depending on the event type. However, it only handled "add" and
"remove" events and ignored the third existing udev event type, "change".

The DeviceMonitorLinux class was one of the only 2 users of UdevLinux in the
tree, and it turns out that not acting on the "change" event causes the
entire media code path to miss an important event and consequently cause
MediaDevicesManager::UpdateSnapshot() to decide there is no need to notify
subscribers of device changes.

Make UdevWatcher call a new OnDeviceChanged() method on observers when there
is a "change" event, and add implementations to DeviceMonitorLinux and
GamepadPlatformDataFetcherLinux, the two classes that used to use UdevLinux
and thus presumably relied on this missing event.

Other users of UdevWatcher that were _not_ converted from UdevLinux have
been given empty implementations of that method: the event was never
received (or missed) by these classes, and each user needs to figure out
what to do with it in the future should it start to become important.

Bug: 940514, 977992
Change-Id: I70187a3b2367f34815a4c5a7b24f7dfab221e2b4
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1684172
Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
Reviewed-by: Reilly Grant <reillyg@chromium.org>

device/gamepad/gamepad_platform_data_fetcher_linux.cc
@@ -82,6 +82,11 @@ void GamepadPlatformDataFetcherLinux::OnDeviceRemoved(
   RefreshDevice(device.get());
 }
 
+void GamepadPlatformDataFetcherLinux::OnDeviceChanged(
+    ScopedUdevDevicePtr device) {
+  RefreshDevice(device.get());
+}
+
 void GamepadPlatformDataFetcherLinux::GetGamepadData(bool) {
   TRACE_EVENT0("GAMEPAD", "GetGamepadData");
 

device/gamepad/gamepad_platform_data_fetcher_linux.h
@@ -80,6 +80,7 @@ class DEVICE_GAMEPAD_EXPORT GamepadPlatformDataFetcherLinux
   // UdevWatcher::Observer overrides
   void OnDeviceAdded(ScopedUdevDevicePtr device) override;
   void OnDeviceRemoved(ScopedUdevDevicePtr device) override;
+  void OnDeviceChanged(ScopedUdevDevicePtr device) override;
 
   std::unordered_set<std::unique_ptr<GamepadDeviceLinux>> devices_;
 

device/udev_linux/udev_watcher.cc
@@ -131,6 +131,10 @@ void UdevWatcher::OnMonitorReadable() {
     observer_->OnDeviceAdded(std::move(device));
   else if (action == "remove")
     observer_->OnDeviceRemoved(std::move(device));
+  else if (action == "change")
+    observer_->OnDeviceChanged(std::move(device));
+  else
+    DVLOG(1) << "Unknown udev action: " << action;
 }
 
 }  // namespace device

device/udev_linux/udev_watcher.h
@@ -25,6 +25,7 @@ class UdevWatcher {
    public:
     virtual void OnDeviceAdded(ScopedUdevDevicePtr device) = 0;
     virtual void OnDeviceRemoved(ScopedUdevDevicePtr device) = 0;
+    virtual void OnDeviceChanged(ScopedUdevDevicePtr device) = 0;
 
    protected:
     virtual ~Observer();

media/device_monitors/device_monitor_udev.cc
@@ -54,6 +54,7 @@ class DeviceMonitorLinux::BlockingTaskRunnerHelper
   // device::UdevWatcher::Observer overrides
   void OnDeviceAdded(device::ScopedUdevDevicePtr device) override;
   void OnDeviceRemoved(device::ScopedUdevDevicePtr device) override;
+  void OnDeviceChanged(device::ScopedUdevDevicePtr device) override;
 
   std::unique_ptr<device::UdevWatcher> udev_watcher_;
 
@@ -83,6 +84,11 @@ void DeviceMonitorLinux::BlockingTaskRunnerHelper::OnDeviceAdded(
 }
 
 void DeviceMonitorLinux::BlockingTaskRunnerHelper::OnDeviceRemoved(
+    device::ScopedUdevDevicePtr device) {
+  OnDevicesChanged(std::move(device));
+}
+
+void DeviceMonitorLinux::BlockingTaskRunnerHelper::OnDeviceChanged(
     device::ScopedUdevDevicePtr device) {
   OnDevicesChanged(std::move(device));
 }

services/device/generic_sensor/linux/sensor_device_manager.cc
@@ -177,4 +177,8 @@ void SensorDeviceManager::OnDeviceRemoved(ScopedUdevDevicePtr dev) {
                                 delegate_, type, device_node));
 }
 
+void SensorDeviceManager::OnDeviceChanged(ScopedUdevDevicePtr) {
+  NOTIMPLEMENTED();
+}
+
 }  // namespace device

services/device/generic_sensor/linux/sensor_device_manager.h
@@ -66,6 +66,7 @@ class SensorDeviceManager : public UdevWatcher::Observer {
   // UdevWatcher::Observer overrides
   void OnDeviceAdded(ScopedUdevDevicePtr udev_device) override;
   void OnDeviceRemoved(ScopedUdevDevicePtr device) override;
+  void OnDeviceChanged(ScopedUdevDevicePtr device) override;
 
   // Represents a map of sensors that are already known to this manager after
   // initial enumeration.

services/device/hid/hid_service_linux.cc
@@ -180,6 +180,8 @@ class HidServiceLinux::BlockingTaskRunnerHelper : public UdevWatcher::Observer {
     }
   }
 
+  void OnDeviceChanged(ScopedUdevDevicePtr) override { NOTIMPLEMENTED(); }
+
   SEQUENCE_CHECKER(sequence_checker_);
   std::unique_ptr<UdevWatcher> watcher_;
 

services/device/usb/usb_service_linux.cc
@@ -76,6 +76,7 @@ class UsbServiceLinux::BlockingTaskRunnerHelper : public UdevWatcher::Observer {
   // UdevWatcher::Observer
   void OnDeviceAdded(ScopedUdevDevicePtr device) override;
   void OnDeviceRemoved(ScopedUdevDevicePtr device) override;
+  void OnDeviceChanged(ScopedUdevDevicePtr device) override;
 
   std::unique_ptr<UdevWatcher> watcher_;
 
@@ -200,6 +201,11 @@ void UsbServiceLinux::BlockingTaskRunnerHelper::OnDeviceRemoved(
         FROM_HERE, base::BindOnce(&UsbServiceLinux::OnDeviceRemoved, service_,
                                   std::string(device_path)));
   }
+}
+
+void UsbServiceLinux::BlockingTaskRunnerHelper::OnDeviceChanged(
+    ScopedUdevDevicePtr) {
+  NOTIMPLEMENTED();
 }
 
 UsbServiceLinux::UsbServiceLinux()