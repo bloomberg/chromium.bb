3d50ddb8185ca59e83eb7c796be6ff5aa2509cf7
Andy Paicu <andypaicu@chromium.org>
2019-07-01 08:36:54 +0000

    Ensure that navigation initiator binding_set is closed when garbage...
    ...collecting the document object.
    
    I believe this makes sense but both oilpan and mojo are things that I
    would not consider myself an expert in.
    
    (cherry picked from commit 1170bdda612167657f4024dc32254fcc85cfd1de [formerly 3550c46dbb1f0d3cbb358440d321179072a0f3d7])
    
    Bug: 962572
    Change-Id: I9fd231ff41d43493f74db1cdae7511c2378714fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1634840
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#670418}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1682574
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3809@{#666}
    Cr-Branched-From: ce45438777a60f42f88228700b3968406c6404d8 [formerly d82dec1a818f378c464ba307ddd9c92133eac355]-refs/heads/master@{#665002}
    
    Former-commit-id: 575c42047bc662f14391b7df389073a5034fa83c

--- third_party/blink/renderer/core/dom/document.h
+++ third_party/blink/renderer/core/dom/document.h
@@ -250,6 +250,7 @@ class CORE_EXPORT Document : public ContainerNode,
                              public mojom::blink::NavigationInitiator {
   DEFINE_WRAPPERTYPEINFO();
   USING_GARBAGE_COLLECTED_MIXIN(Document);
+  USING_PRE_FINALIZER(Document, Dispose);
 
  public:
   // Factory for web-exposed Document constructor. The argument document must be
@@ -1727,6 +1728,8 @@ class CORE_EXPORT Document : public ContainerNode,
   void NotifyFocusedElementChanged(Element* old_focused_element,
                                    Element* new_focused_element);
   void DisplayNoneChangedForFrame();
+
+  void Dispose();
 
   DocumentLifecycle lifecycle_;
 
