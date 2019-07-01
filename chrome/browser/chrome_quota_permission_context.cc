c849b90d1e93fdb7c9a550ff7afb3faa382d86ec
Evan Stade <estade@chromium.org>
2019-07-01 20:09:50 +0000

    Clean up folder vector icons in //chrome/app
    kFolderSupervisedIcon was unused, and is deleted.
    kFolderIcon is redundant with the one in //components/vector_icons, so
      it is moved and its only reference is updated.
    
    Bug: 522168
    Change-Id: Idec712f2994b31fb5ff5503ca614b497e8c6d3f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1682796
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Evan Stade <estade@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#673792}
    
    Former-commit-id: 0d40b357f940c7e8ccb553aa478d0b908c5fa7a2

--- chrome/browser/chrome_quota_permission_context.cc
+++ chrome/browser/chrome_quota_permission_context.cc
@@ -30,7 +30,6 @@
 #if defined(OS_ANDROID)
 #include "chrome/browser/android/android_theme_resources.h"
 #else
-#include "chrome/app/vector_icons/vector_icons.h"
 #include "components/vector_icons/vector_icons.h"
 #endif
 
@@ -94,7 +93,7 @@ PermissionRequest::IconId QuotaPermissionRequest::GetIconId() const {
 #if defined(OS_ANDROID)
   return IDR_ANDROID_INFOBAR_FOLDER;
 #else
-  return kFolderIcon;
+  return vector_icons::kFolderIcon;
 #endif
 }
 
