bloomberg/chromium.bb

commit adc6a15eb70528dc50c9a5f1c6232b69af43ec8c

Add DataReductionProxy UKM event
Adds two fields
* data_saver_page_uuid
* estimated_original_network_bytes

Bug: 957753
Change-Id: Icf632f6d200229cc5bbcc5a505f088b6176bba60
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1654275
Reviewed-by: Robert Kaplow (slow) <rkaplow@chromium.org>
Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
Commit-Queue: Robert Ogden <robertogden@chromium.org>
Cr-Commit-Position: refs/heads/master@{#670487}

Former-commit-id: da9e774b86cd8816d661c1e4674ef9a99725b745

chrome/browser/page_load_metrics/observers/data_reduction_proxy_metrics_observer_base.cc
@@ -4,9 +4,15 @@
 
 #include "chrome/browser/page_load_metrics/observers/data_reduction_proxy_metrics_observer_base.h"
 
+#include <stdint.h>
+#include <algorithm>
+#include <iterator>
 #include <string>
+#include <vector>
 
+#include "base/big_endian.h"
 #include "base/bind.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/optional.h"
 #include "base/strings/string_piece.h"
 #include "base/time/time.h"
@@ -29,7 +35,11 @@
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/child_process_host.h"
+#include "crypto/sha2.h"
 #include "services/metrics/public/cpp/metrics_utils.h"
+#include "services/metrics/public/cpp/ukm_builders.h"
+#include "services/metrics/public/cpp/ukm_recorder.h"
+#include "services/metrics/public/cpp/ukm_source.h"
 #include "url/gurl.h"
 
 namespace data_reduction_proxy {
@@ -60,6 +70,33 @@ PageloadMetrics_PageEndReason ConvertPLMPageEndReasonToProto(
     default:
       return PageloadMetrics_PageEndReason_END_OTHER;
   }
+}
+
+uint64_t ComputeDataReductionProxyUUID(
+    data_reduction_proxy::DataReductionProxyData* data) {
+  if (!data || data->session_key().empty() || !data->page_id().has_value() ||
+      data->page_id().value() == 0) {
+    return 0;
+  }
+
+  std::string session_key = data->session_key();
+  uint64_t page_id = data->page_id().value();
+
+  char buf[8];
+  base::WriteBigEndian<uint64_t>(buf, page_id);
+
+  std::vector<char> to_hash;
+  std::copy(session_key.begin(), session_key.end(),
+            std::back_inserter(to_hash));
+  to_hash.insert(to_hash.end(), buf, buf + 8);
+
+  char hash[32];
+  crypto::SHA256HashString(base::StringPiece(to_hash.begin(), to_hash.end()),
+                           hash, 32);
+
+  uint64_t uuid;
+  base::ReadBigEndian<uint64_t>(hash, &uuid);
+  return uuid;
 }
 
 }  // namespace
@@ -182,6 +219,7 @@ DataReductionProxyMetricsObserverBase::FlushMetricsOnAppEnterBackground(
   // notification, so we send a pingback with data collected up to this point.
   if (info.did_commit) {
     SendPingback(timing, info, true /* app_background_occurred */);
+    RecordUKM(info);
   }
   return STOP_OBSERVING;
 }
@@ -191,6 +229,7 @@ void DataReductionProxyMetricsObserverBase::OnComplete(
     const page_load_metrics::PageLoadExtraInfo& info) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   SendPingback(timing, info, false /* app_background_occurred */);
+  RecordUKM(info);
 }
 
 // static
@@ -201,6 +240,27 @@ int64_t DataReductionProxyMetricsObserverBase::ExponentiallyBucketBytes(
     return 0;
   }
   return ukm::GetExponentialBucketMin(bytes, 1.16);
+}
+
+void DataReductionProxyMetricsObserverBase::RecordUKM(
+    const page_load_metrics::PageLoadExtraInfo& info) const {
+  if (!data())
+    return;
+
+  if (!data()->used_data_reduction_proxy())
+    return;
+
+  int64_t original_network_bytes =
+      insecure_original_network_bytes() + secure_original_network_bytes();
+  uint64_t uuid = ComputeDataReductionProxyUUID(data());
+
+  ukm::builders::DataReductionProxy builder(info.source_id);
+
+  builder.SetEstimatedOriginalNetworkBytes(
+      ExponentiallyBucketBytes(original_network_bytes));
+  builder.SetDataSaverPageUUID(uuid);
+
+  builder.Record(ukm::UkmRecorder::Get());
 }
 
 void DataReductionProxyMetricsObserverBase::SendPingback(

chrome/browser/page_load_metrics/observers/data_reduction_proxy_metrics_observer.cc
@@ -123,7 +123,7 @@ void DataReductionProxyMetricsObserver::RecordPageSizeUMA() const {
   const int64_t original_network_bytes =
       insecure_original_network_bytes() + secure_original_network_bytes();
 
-  // TODO(ryansturm): Evaluate if any of the below histograms are unncessary
+  // TODO(ryansturm): Evaluate if any of the below histograms are unnecessary
   // once data is available. crbug.com/682782
 
   // The percent of resources that went through the data reduction proxy.

chrome/browser/page_load_metrics/observers/data_reduction_proxy_metrics_observer_base.h
@@ -104,6 +104,9 @@ class DataReductionProxyMetricsObserverBase
   ObservePolicy OnCommit(content::NavigationHandle* navigation_handle,
                          ukm::SourceId source_id) final;
 
+  // Records UKM for the data_reduction_proxy event.
+  void RecordUKM(const page_load_metrics::PageLoadExtraInfo& info) const;
+
   // Sends the page load information to the pingback client.
   void SendPingback(const page_load_metrics::mojom::PageLoadTiming& timing,
                     const page_load_metrics::PageLoadExtraInfo& info,

chrome/browser/page_load_metrics/observers/data_reduction_proxy_metrics_observer_base_unittest.cc
@@ -30,6 +30,9 @@
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/web_contents_tester.h"
 #include "net/base/ip_endpoint.h"
+#include "services/metrics/public/cpp/ukm_builders.h"
+#include "services/metrics/public/cpp/ukm_recorder.h"
+#include "services/metrics/public/cpp/ukm_source.h"
 #include "services/resource_coordinator/public/cpp/memory_instrumentation/memory_instrumentation.h"
 #include "services/resource_coordinator/public/mojom/memory_instrumentation/memory_instrumentation.mojom.h"
 #include "third_party/blink/public/platform/web_input_event.h"
@@ -50,13 +53,17 @@ class TestDataReductionProxyMetricsObserverBase
       bool data_reduction_proxy_used,
       bool cached_data_reduction_proxy_used,
       bool lite_page_used,
-      bool black_listed)
+      bool black_listed,
+      std::string session_key,
+      uint64_t page_id)
       : web_contents_(web_contents),
         pingback_client_(pingback_client),
         data_reduction_proxy_used_(data_reduction_proxy_used),
         cached_data_reduction_proxy_used_(cached_data_reduction_proxy_used),
         lite_page_used_(lite_page_used),
-        black_listed_(black_listed) {}
+        black_listed_(black_listed),
+        session_key_(session_key),
+        page_id_(page_id) {}
 
   ~TestDataReductionProxyMetricsObserverBase() override {}
 
@@ -71,6 +78,8 @@ class TestDataReductionProxyMetricsObserverBase
         cached_data_reduction_proxy_used_);
     data->set_request_url(GURL(kDefaultTestUrl));
     data->set_lite_page_received(lite_page_used_);
+    data->set_session_key(session_key_);
+    data->set_page_id(page_id_);
     DataReductionProxyChromeSettingsFactory::GetForBrowserContext(
         web_contents_->GetBrowserContext())
         ->SetDataForNextCommitForTesting(std::move(data));
@@ -113,19 +122,45 @@ class TestDataReductionProxyMetricsObserverBase
   bool cached_data_reduction_proxy_used_;
   bool lite_page_used_;
   bool black_listed_;
+  std::string session_key_;
+  uint64_t page_id_;
 
   DISALLOW_COPY_AND_ASSIGN(TestDataReductionProxyMetricsObserverBase);
 };
 
 class DataReductionProxyMetricsObserverBaseTest
     : public DataReductionProxyMetricsObserverTestBase {
+ public:
+  void ValidateUKM(bool want_ukm,
+                   uint64_t want_original_bytes,
+                   uint64_t want_uuid) {
+    using UkmEntry = ukm::builders::DataReductionProxy;
+    auto entries = test_ukm_recorder().GetEntriesByName(UkmEntry::kEntryName);
+
+    if (!want_ukm) {
+      EXPECT_EQ(0u, entries.size());
+      return;
+    }
+
+    EXPECT_EQ(1u, entries.size());
+    for (const auto* const entry : entries) {
+      test_ukm_recorder().ExpectEntrySourceHasUrl(entry, GURL(kDefaultTestUrl));
+
+      test_ukm_recorder().ExpectEntryMetric(
+          entry, UkmEntry::kEstimatedOriginalNetworkBytesName,
+          want_original_bytes);
+      test_ukm_recorder().ExpectEntryMetric(
+          entry, UkmEntry::kDataSaverPageUUIDName, want_uuid);
+    }
+  }
+
  protected:
   void RegisterObservers(page_load_metrics::PageLoadTracker* tracker) override {
     tracker->AddObserver(
         std::make_unique<TestDataReductionProxyMetricsObserverBase>(
             web_contents(), pingback_client(), data_reduction_proxy_used(),
             cached_data_reduction_proxy_used(), is_using_lite_page(),
-            black_listed()));
+            black_listed(), session_key(), page_id()));
   }
 };
 
@@ -347,6 +382,62 @@ TEST_F(DataReductionProxyMetricsObserverBaseTest,
 
   // When the renderer does not crash, the pingback should report that.
   ValidateRendererCrash(false);
+}
+
+TEST_F(DataReductionProxyMetricsObserverBaseTest,
+       ValidateUKM_DataSaverNotUsed) {
+  ResetTest();
+  RunTestAndNavigateToUntrackedUrl(false, false, false);
+  ValidateUKM(false, 0U, 0U);
+}
+
+TEST_F(DataReductionProxyMetricsObserverBaseTest, ValidateUKM_DataSaverUsed) {
+  ResetTest();
+  RunTestAndNavigateToUntrackedUrl(true, false, false);
+  ValidateUKM(true, 0U, 0U);
+}
+
+TEST_F(DataReductionProxyMetricsObserverBaseTest,
+       ValidateUKM_DataSaverPopulated_Lower) {
+  ResetTest();
+
+  session_key_ = "abc";
+  page_id_ = 1U;
+
+  RunTest(true, false, false, false);
+
+  std::vector<page_load_metrics::mojom::ResourceDataUpdatePtr> resources;
+  resources.push_back(CreateDataReductionProxyResource(
+      false /* was_cached */, 2 * 1024 /* delta_bytes */,
+      true /* is_complete */, true /* proxy_used*/, 5 /* compression_ratio */));
+
+  SimulateResourceDataUseUpdate(resources);
+
+  NavigateToUntrackedUrl();
+  // Use https://play.golang.org/p/XnMDcTiPzt8 to generate an updated uuid.
+  ValidateUKM(true, 9918U, 5261012271403106530);
+}
+
+TEST_F(DataReductionProxyMetricsObserverBaseTest,
+       ValidateUKM_DataSaverPopulated_Upper) {
+  ResetTest();
+
+  session_key_ = "xyz";
+  // Same as 1<<63, but keeps clang happy.
+  page_id_ = 0x8000000000000000;
+
+  RunTest(true, false, false, false);
+
+  std::vector<page_load_metrics::mojom::ResourceDataUpdatePtr> resources;
+  resources.push_back(CreateDataReductionProxyResource(
+      false /* was_cached */, 2 * 1024 /* delta_bytes */,
+      true /* is_complete */, true /* proxy_used*/, 5 /* compression_ratio */));
+
+  SimulateResourceDataUseUpdate(resources);
+
+  NavigateToUntrackedUrl();
+  // Use https://play.golang.org/p/XnMDcTiPzt8 to generate an updated uuid.
+  ValidateUKM(true, 9918U, 7538012597823726222);
 }
 
 }  // namespace data_reduction_proxy

chrome/browser/page_load_metrics/observers/data_reduction_proxy_metrics_observer_test_utils.h
@@ -138,11 +138,15 @@ class DataReductionProxyMetricsObserverTestBase
   bool is_using_lite_page() const { return is_using_lite_page_; }
   bool opt_out_expected() const { return opt_out_expected_; }
   bool black_listed() const { return black_listed_; }
+  std::string session_key() const { return session_key_; }
+  uint64_t page_id() const { return page_id_; }
 
  protected:
   std::unique_ptr<TestPingbackClient> pingback_client_;
   page_load_metrics::mojom::PageLoadTiming timing_;
   bool cached_data_reduction_proxy_used_ = false;
+  std::string session_key_;
+  uint64_t page_id_ = 0;
 
  private:
   previews::PreviewsUserData::ServerLitePageInfo* preview_info_;

chrome/browser/page_load_metrics/observers/data_reduction_proxy_metrics_observer_unittest.cc
@@ -257,7 +257,7 @@ TEST_F(DataReductionProxyMetricsObserverTest, DataReductionProxyOff) {
 TEST_F(DataReductionProxyMetricsObserverTest, DataReductionProxyOn) {
   ResetTest();
   // Verify that when the data reduction proxy was used, but lite page was not
-  // used, the correpsonding UMA is reported.
+  // used, the corresponding UMA is reported.
   RunTest(true, false, false, false);
   ValidateHistograms();
 }
@@ -274,11 +274,6 @@ TEST_F(DataReductionProxyMetricsObserverTest, ByteInformationCompression) {
   ResetTest();
 
   RunTest(true, false, false, false);
-
-  std::unique_ptr<DataReductionProxyData> data =
-      std::make_unique<DataReductionProxyData>();
-  data->set_used_data_reduction_proxy(true);
-  data->set_request_url(GURL(kDefaultTestUrl));
 
   std::vector<page_load_metrics::mojom::ResourceDataUpdatePtr> resources;
   // Cached resource.
@@ -342,11 +337,6 @@ TEST_F(DataReductionProxyMetricsObserverTest, ByteInformationInflation) {
   ResetTest();
 
   RunTest(true, false, false, false);
-
-  std::unique_ptr<DataReductionProxyData> data =
-      std::make_unique<DataReductionProxyData>();
-  data->set_used_data_reduction_proxy(true);
-  data->set_request_url(GURL(kDefaultTestUrl));
 
   std::vector<page_load_metrics::mojom::ResourceDataUpdatePtr> resources;
   // Cached resource.

tools/metrics/ukm/ukm.xml
@@ -2479,6 +2479,27 @@ be describing additional metrics about the same event.
   </metric>
 </event>
 
+<event name="DataReductionProxy" singular="True">
+  <owner>robertogden@chromium.org</owner>
+  <summary>
+    Data Reduction Proxy related metrics associated with a page load. See
+    //components/data_reduction_proxy/.
+  </summary>
+  <metric name="DataSaverPageUUID">
+    <summary>
+      A unique identifier of this page load to the proxy server. Recorded for
+      each navigation that uses the proxy.
+    </summary>
+  </metric>
+  <metric name="EstimatedOriginalNetworkBytes">
+    <summary>
+      The estimated number of bytes that would have been used if that proxy was
+      disabled on this page load. Recorded for each navigation that uses the
+      proxy.
+    </summary>
+  </metric>
+</event>
+
 <event name="Document.OutliveTimeAfterShutdown">
   <owner>hajimehoshi@chromium.org</owner>
   <owner>keishi@chromium.org</owner>

commit dad0a6c7f19304d3c12a38f7642b1487dd4dd208

DataReductionProxy: Simplify ComputeDataReductionProxyUUID().
According to the standard, |iterator| and |const_iterator| are
implementation-defined for std::basic_string and std::vector, so we cannot
rely on std::string::iterator and std::vector<char>::iterator being
compatible. In fact, the libstdc++ build has been broken since commit
adc6a15eb70 [formerly da9e774b86c] ("Add DataReductionProxy UKM event"):

    ../../chrome/browser/page_load_metrics/observers/data_reduction_proxy_metrics_observer_base.cc:94:28: error: no matching constructor for initialization of 'base::StringPiece' (aka 'BasicStringPiece<basic_string<char> >')
      crypto::SHA256HashString(base::StringPiece(to_hash.begin(), to_hash.end()),
                               ^                 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    ../../base/strings/string_piece.h:189:13: note: candidate constructor not viable: no known conversion from 'std::vector<char, std::allocator<char> >::iterator' (aka '__normal_iterator<char *, std::vector<char, std::allocator<char> > >') to 'const base::BasicStringPiece<std::__cxx11::basic_string<char> >::value_type *' (aka 'const char *') for 1st argument
      constexpr BasicStringPiece(const value_type* offset, size_type len)
                ^
    ../../base/strings/string_piece.h:191:3: note: candidate constructor not viable: no known conversion from '__normal_iterator<std::vector<char, std::allocator<char> >::pointer, std::vector<char, std::allocator<char> >>' to 'const __normal_iterator<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >::const_pointer, std::__cxx11::basic_string<char>>' for 1st argument
      BasicStringPiece(const typename STRING_TYPE::const_iterator& begin,
      ^
    ../../base/strings/string_piece.h:185:13: note: candidate constructor not viable: requires single argument 'str', but 2 arguments were provided
      constexpr BasicStringPiece(const value_type* str)
                ^

chrome/browser/page_load_metrics/observers/data_reduction_proxy_metrics_observer_base.cc
@@ -79,20 +79,15 @@ uint64_t ComputeDataReductionProxyUUID(
     return 0;
   }
 
-  std::string session_key = data->session_key();
   uint64_t page_id = data->page_id().value();
-
   char buf[8];
   base::WriteBigEndian<uint64_t>(buf, page_id);
 
-  std::vector<char> to_hash;
-  std::copy(session_key.begin(), session_key.end(),
-            std::back_inserter(to_hash));
-  to_hash.insert(to_hash.end(), buf, buf + 8);
+  std::string to_hash(data->session_key());
+  to_hash.append(std::begin(buf), std::end(buf));
 
   char hash[32];
-  crypto::SHA256HashString(base::StringPiece(to_hash.begin(), to_hash.end()),
-                           hash, 32);
+  crypto::SHA256HashString(base::StringPiece(to_hash), hash, 32);
 
   uint64_t uuid;
   base::ReadBigEndian<uint64_t>(hash, &uuid);