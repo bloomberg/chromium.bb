b7e2bf4f26eedd1cc01a74029655a6ec70e5d843
Samuel Huang <huangs@chromium.org>
2019-07-03 16:01:54 +0000

    [DevUI] Rename Devtools DFM to DevUI DFM; fix build errors.
    The name "devtools" already means something specific in Chrome. To
    avoid confusion, this CL renames the upcoming DFM for chrome://
    internal pages to "Developer UI module", or dev_ui / DevUi for short,
    and essentially redoes the CL:
    https://chromium-review.googlesource.com/1613558
    
    Other changes:
    * To fix build errors, update dev_ui_module_tmpl.gni to adapt to recent
      changes re. manifest package, from
      https://chromium-review.googlesource.com//1667687
    * Rename DevtoolsUI.java to DevUiModuleProvider.java, which will play
      a greater role for DFM installation than initially thought.
    * Change GN enablement flag to |dfmify_dev_ui| (was |dfmify_devtools|).
    
    Bug: 927131
    Change-Id: I500451658e7fd575e6b499e9eafbb221d0e30ad6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1681872
    Reviewed-by: Robert Kaplow (slow) <rkaplow@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#674489}
    
    Former-commit-id: fda3778d08843323c49cdfe14a6eb85b51b7a529

---
+++ chrome/android/features/dev_ui/java/src/org/chromium/chrome/browser/dev_ui/DevUiModuleProvider.java
@@ -0,0 +1,14 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.dev_ui;
+
+import org.chromium.base.annotations.UsedByReflection;
+
+/**
+ * Provider for the Developer UI DFM. Currently this is a placeholder that serves as a kludge, so
+ * Java code exists somewhere.
+ */
+@UsedByReflection("DevUiModuleProvider.java")
+public interface DevUiModuleProvider {}
