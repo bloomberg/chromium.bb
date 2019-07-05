9ea17a4e9c485ce56f5fe4144140ff35519f7158
Yann Dago <ydago@chromium.org>
2019-07-05 22:14:52 +0000

    Replace placeholders by value in policy localized json files
    The localized files generated from policy_templates.json contain
    placeholders such as $1, the placeholders should have their real
    values.
    
    Bug: 976254
    Change-Id: I8b7ed1a61bceeef1d7443736f3f0176acaaee233
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1666702
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Lutz Justen <ljusten@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675094}
    
    Former-commit-id: e77aa3235f059b44aba0ddc7194d9bbe56513852

--- tools/grit/grit/gather/policy_json.py
+++ tools/grit/grit/gather/policy_json.py
@@ -57,9 +57,15 @@ class PolicyJson(skeleton_gatherer.SkeletonGatherer):
     if example_text == []:
       # In such cases the original text is okay for an example.
       example_text = text
+
+    replacedText = self.Escape(''.join(text).strip())
+    replacedText = replacedText.replace('$1', self._config['app_name'])
+    replacedText = replacedText.replace('$2', self._config['os_name'])
+    replacedText = replacedText.replace('$3', self._config['frame_name'])
+
     msg.AppendPlaceholder(tclib.Placeholder(
         placeholder.attributes['name'].value,
-        self.Escape(''.join(text).strip()),
+        replacedText,
         ''.join(example_text).strip()))
 
   def _ParseMessage(self, string, desc):
@@ -293,3 +299,21 @@ class PolicyJson(skeleton_gatherer.SkeletonGatherer):
 
   def Escape(self, text):
     return json.dumps(text, ensure_ascii=False)[1:-1]
+
+  def SetDefines(self, defines):
+    if '_chromium' in defines:
+      self._config = {
+        'build': 'chromium',
+        'app_name': 'Chromium',
+        'frame_name': 'Chromium Frame',
+        'os_name': 'Chromium OS',
+      }
+    elif '_google_chrome' in defines:
+      self._config = {
+        'build': 'chrome',
+        'app_name': 'Google Chrome',
+        'frame_name': 'Google Chrome Frame',
+        'os_name': 'Google Chrome OS',
+      }
+    else:
+      raise Exception('Unknown build')
