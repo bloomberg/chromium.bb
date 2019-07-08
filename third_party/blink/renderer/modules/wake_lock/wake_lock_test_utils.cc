e2890b2ea7f1819f0632dc82a687a74f5fc9036f
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2019-07-08 21:44:48 +0000

    wake lock: Move most of wake_lock_test_utils.h to wake_lock_test_utils.cc
    wake_lock_test_utils.h has grown too long; move most of the method
    implementations to wake_lock_test_utils.cc and keep only declarations and
    small getters in the header.
    
    Change-Id: I2d2ae622ceda00c3567b2d2891a34038d0eeb2c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1688830
    Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#675383}
    
    Former-commit-id: c4977bb0b696146d44630e7165349423eee13460

--- third_party/blink/renderer/modules/wake_lock/wake_lock_test_utils.cc
+++ third_party/blink/renderer/modules/wake_lock/wake_lock_test_utils.cc
@@ -4,13 +4,142 @@
 
 #include "third_party/blink/renderer/modules/wake_lock/wake_lock_test_utils.h"
 
+#include <utility>
+
+#include "base/logging.h"
+#include "base/run_loop.h"
+#include "services/service_manager/public/cpp/interface_provider.h"
+#include "third_party/blink/renderer/bindings/core/v8/script_function.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_dom_exception.h"
+#include "third_party/blink/renderer/core/dom/document.h"
+#include "third_party/blink/renderer/modules/wake_lock/wake_lock_type.h"
 #include "third_party/blink/renderer/platform/bindings/v8_binding.h"
+#include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
+#include "third_party/blink/renderer/platform/wtf/functional.h"
 
 namespace blink {
 
 using mojom::blink::PermissionDescriptorPtr;
 using mojom::blink::PermissionStatus;
+
+namespace {
+
+// Helper class for WaitForPromise{Fulfillment,Rejection}(). It provides a
+// function that invokes |callback| when a ScriptPromise is resolved.
+class ClosureRunnerFunction final : public ScriptFunction {
+ public:
+  static v8::Local<v8::Function> CreateFunction(
+      ScriptState* script_state,
+      base::RepeatingClosure callback) {
+    auto* function = MakeGarbageCollected<ClosureRunnerFunction>(
+        script_state, std::move(callback));
+    return function->BindToV8Function();
+  }
+
+  ClosureRunnerFunction(ScriptState* script_state,
+                        base::RepeatingClosure callback)
+      : ScriptFunction(script_state), callback_(std::move(callback)) {}
+
+ private:
+  ScriptValue Call(ScriptValue) override {
+    if (callback_)
+      std::move(callback_).Run();
+    return ScriptValue();
+  }
+
+  base::RepeatingClosure callback_;
+};
+
+WakeLockType ToBlinkWakeLockType(device::mojom::blink::WakeLockType type) {
+  switch (type) {
+    case device::mojom::blink::WakeLockType::kPreventDisplaySleep:
+      return WakeLockType::kScreen;
+    case device::mojom::blink::WakeLockType::kPreventAppSuspension:
+      return WakeLockType::kSystem;
+    default:
+      NOTREACHED();
+      return WakeLockType::kMaxValue;
+  }
+}
+
+}  // namespace
+
+// MockWakeLock
+
+MockWakeLock::MockWakeLock() = default;
+MockWakeLock::~MockWakeLock() = default;
+
+void MockWakeLock::Bind(device::mojom::blink::WakeLockRequest request) {
+  DCHECK(!binding_.is_bound());
+  binding_.Bind(std::move(request));
+  binding_.set_connection_error_handler(
+      WTF::Bind(&MockWakeLock::OnConnectionError, WTF::Unretained(this)));
+}
+
+void MockWakeLock::Unbind() {
+  OnConnectionError();
+}
+
+void MockWakeLock::WaitForRequest() {
+  DCHECK(!request_wake_lock_callback_);
+  base::RunLoop run_loop;
+  request_wake_lock_callback_ = run_loop.QuitClosure();
+  run_loop.Run();
+}
+
+void MockWakeLock::WaitForCancelation() {
+  DCHECK(!cancel_wake_lock_callback_);
+  base::RunLoop run_loop;
+  cancel_wake_lock_callback_ = run_loop.QuitClosure();
+  run_loop.Run();
+}
+
+void MockWakeLock::OnConnectionError() {
+  if (binding_.is_bound())
+    binding_.Unbind();
+  CancelWakeLock();
+}
+
+void MockWakeLock::RequestWakeLock() {
+  is_acquired_ = true;
+  if (request_wake_lock_callback_)
+    std::move(request_wake_lock_callback_).Run();
+}
+
+void MockWakeLock::CancelWakeLock() {
+  is_acquired_ = false;
+  if (cancel_wake_lock_callback_)
+    std::move(cancel_wake_lock_callback_).Run();
+}
+
+void MockWakeLock::AddClient(device::mojom::blink::WakeLockRequest) {}
+void MockWakeLock::ChangeType(device::mojom::blink::WakeLockType,
+                              ChangeTypeCallback) {}
+void MockWakeLock::HasWakeLockForTests(HasWakeLockForTestsCallback) {}
+
+// MockWakeLockService
+
+MockWakeLockService::MockWakeLockService() = default;
+MockWakeLockService::~MockWakeLockService() = default;
+
+void MockWakeLockService::BindRequest(mojo::ScopedMessagePipeHandle handle) {
+  bindings_.AddBinding(this,
+                       mojom::blink::WakeLockServiceRequest(std::move(handle)));
+}
+
+MockWakeLock& MockWakeLockService::get_wake_lock(WakeLockType type) {
+  size_t pos = static_cast<size_t>(type);
+  return mock_wake_lock_[pos];
+}
+
+void MockWakeLockService::GetWakeLock(
+    device::mojom::blink::WakeLockType type,
+    device::mojom::blink::WakeLockReason reason,
+    const String& description,
+    device::mojom::blink::WakeLockRequest request) {
+  size_t pos = static_cast<size_t>(ToBlinkWakeLockType(type));
+  mock_wake_lock_[pos].Bind(std::move(request));
+}
 
 // MockPermissionService
 
@@ -108,6 +237,63 @@ void MockPermissionService::AddPermissionObserver(
     PermissionStatus last_known_status,
     mojom::blink::PermissionObserverPtr) {
   NOTREACHED();
+}
+
+// WakeLockTestingContext
+
+WakeLockTestingContext::WakeLockTestingContext(
+    MockWakeLockService* mock_wake_lock_service) {
+  service_manager::InterfaceProvider::TestApi test_api(
+      GetDocument()->GetInterfaceProvider());
+  test_api.SetBinderForName(
+      mojom::blink::WakeLockService::Name_,
+      WTF::BindRepeating(&MockWakeLockService::BindRequest,
+                         WTF::Unretained(mock_wake_lock_service)));
+  test_api.SetBinderForName(
+      mojom::blink::PermissionService::Name_,
+      WTF::BindRepeating(&MockPermissionService::BindRequest,
+                         WTF::Unretained(&permission_service_)));
+}
+
+Document* WakeLockTestingContext::GetDocument() {
+  return &testing_scope_.GetDocument();
+}
+
+LocalFrame* WakeLockTestingContext::Frame() {
+  return &testing_scope_.GetFrame();
+}
+
+ScriptState* WakeLockTestingContext::GetScriptState() {
+  return testing_scope_.GetScriptState();
+}
+
+MockPermissionService& WakeLockTestingContext::GetPermissionService() {
+  return permission_service_;
+}
+
+ScriptPromise WakeLockTestingContext::WaitForPromiseFulfillment(
+    ScriptPromise promise) {
+  base::RunLoop run_loop;
+  ScriptPromise return_promise =
+      promise.Then(ClosureRunnerFunction::CreateFunction(
+          GetScriptState(), run_loop.QuitClosure()));
+  // Execute pending microtasks, otherwise it can take a few seconds for the
+  // promise to resolve.
+  v8::MicrotasksScope::PerformCheckpoint(GetScriptState()->GetIsolate());
+  run_loop.Run();
+  return return_promise;
+}
+
+// Synchronously waits for |promise| to be rejected.
+void WakeLockTestingContext::WaitForPromiseRejection(ScriptPromise promise) {
+  base::RunLoop run_loop;
+  promise.Then(v8::Local<v8::Function>(),
+               ClosureRunnerFunction::CreateFunction(GetScriptState(),
+                                                     run_loop.QuitClosure()));
+  // Execute pending microtasks, otherwise it can take a few seconds for the
+  // promise to resolve.
+  v8::MicrotasksScope::PerformCheckpoint(GetScriptState()->GetIsolate());
+  run_loop.Run();
 }
 
 // ScriptPromiseUtils
--- third_party/blink/renderer/modules/wake_lock/wake_lock_test_utils.h
+++ third_party/blink/renderer/modules/wake_lock/wake_lock_test_utils.h
@@ -5,136 +5,80 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_WAKE_LOCK_WAKE_LOCK_TEST_UTILS_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_WAKE_LOCK_WAKE_LOCK_TEST_UTILS_H_
 
-#include <memory>
-#include <utility>
-
 #include "base/callback.h"
-#include "base/logging.h"
 #include "base/optional.h"
-#include "base/run_loop.h"
 #include "mojo/public/cpp/bindings/binding.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
 #include "services/device/public/mojom/wake_lock.mojom-blink.h"
-#include "services/service_manager/public/cpp/interface_provider.h"
 #include "third_party/blink/public/mojom/permissions/permission.mojom-blink.h"
 #include "third_party/blink/public/mojom/wake_lock/wake_lock.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_function.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_testing.h"
-#include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/modules/wake_lock/wake_lock_type.h"
-#include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
-#include "third_party/blink/renderer/platform/wtf/functional.h"
-#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "v8/include/v8.h"
 
 namespace blink {
+
+class Document;
 
 // Mock WakeLock implementation that tracks whether it's bound or acquired, and
 // provides a few helper methods to synchronously wait for RequestWakeLock()
 // and CancelWakeLock() to be called.
 class MockWakeLock : public device::mojom::blink::WakeLock {
  public:
-  MockWakeLock() : binding_(this) {}
-  ~MockWakeLock() override = default;
+  MockWakeLock();
+  ~MockWakeLock() override;
 
   bool is_acquired() const { return is_acquired_; }
 
-  void Bind(device::mojom::blink::WakeLockRequest request) {
-    DCHECK(!binding_.is_bound());
-    binding_.Bind(std::move(request));
-    binding_.set_connection_error_handler(
-        WTF::Bind(&MockWakeLock::OnConnectionError, WTF::Unretained(this)));
-  }
+  void Bind(device::mojom::blink::WakeLockRequest request);
 
   // Forcefully terminate a binding to test connection errors.
-  void Unbind() { OnConnectionError(); }
+  void Unbind();
 
   // Synchronously wait for RequestWakeLock() to be called.
-  void WaitForRequest() {
-    DCHECK(!request_wake_lock_callback_);
-    base::RunLoop run_loop;
-    request_wake_lock_callback_ = run_loop.QuitClosure();
-    run_loop.Run();
-  }
+  void WaitForRequest();
 
   // Synchronously wait for CancelWakeLock() to be called.
-  void WaitForCancelation() {
-    DCHECK(!cancel_wake_lock_callback_);
-    base::RunLoop run_loop;
-    cancel_wake_lock_callback_ = run_loop.QuitClosure();
-    run_loop.Run();
-  }
+  void WaitForCancelation();
 
  private:
-  void OnConnectionError() {
-    if (binding_.is_bound())
-      binding_.Unbind();
-    CancelWakeLock();
-  }
+  void OnConnectionError();
 
   // device::mojom::blink::WakeLock implementation
-  void RequestWakeLock() override {
-    is_acquired_ = true;
-    if (request_wake_lock_callback_)
-      std::move(request_wake_lock_callback_).Run();
-  }
-  void CancelWakeLock() override {
-    is_acquired_ = false;
-    if (cancel_wake_lock_callback_)
-      std::move(cancel_wake_lock_callback_).Run();
-  }
-  void AddClient(device::mojom::blink::WakeLockRequest) override {}
+  void RequestWakeLock() override;
+  void CancelWakeLock() override;
+  void AddClient(device::mojom::blink::WakeLockRequest) override;
   void ChangeType(device::mojom::blink::WakeLockType,
-                  ChangeTypeCallback) override {}
-  void HasWakeLockForTests(HasWakeLockForTestsCallback) override {}
+                  ChangeTypeCallback) override;
+  void HasWakeLockForTests(HasWakeLockForTestsCallback) override;
 
   bool is_acquired_ = false;
 
   base::OnceClosure request_wake_lock_callback_;
   base::OnceClosure cancel_wake_lock_callback_;
 
-  mojo::Binding<device::mojom::blink::WakeLock> binding_;
+  mojo::Binding<device::mojom::blink::WakeLock> binding_{this};
 };
 
 // Mock WakeLockService implementation that creates a MockWakeLock in its
 // GetWakeLock() implementation.
 class MockWakeLockService : public mojom::blink::WakeLockService {
  public:
-  MockWakeLockService() = default;
-  ~MockWakeLockService() override = default;
+  MockWakeLockService();
+  ~MockWakeLockService() override;
 
-  void BindRequest(mojo::ScopedMessagePipeHandle handle) {
-    bindings_.AddBinding(
-        this, mojom::blink::WakeLockServiceRequest(std::move(handle)));
-  }
+  void BindRequest(mojo::ScopedMessagePipeHandle handle);
 
-  MockWakeLock& get_wake_lock(WakeLockType type) {
-    size_t pos = static_cast<size_t>(type);
-    return mock_wake_lock_[pos];
-  }
+  MockWakeLock& get_wake_lock(WakeLockType type);
 
  private:
-  WakeLockType ToBlinkWakeLockType(device::mojom::blink::WakeLockType type) {
-    switch (type) {
-      case device::mojom::blink::WakeLockType::kPreventDisplaySleep:
-        return WakeLockType::kScreen;
-      case device::mojom::blink::WakeLockType::kPreventAppSuspension:
-        return WakeLockType::kSystem;
-      default:
-        NOTREACHED();
-        return WakeLockType::kMaxValue;
-    }
-  }
-
   // mojom::blink::WakeLockService implementation
   void GetWakeLock(device::mojom::blink::WakeLockType type,
                    device::mojom::blink::WakeLockReason reason,
                    const String& description,
-                   device::mojom::blink::WakeLockRequest request) override {
-    size_t pos = static_cast<size_t>(ToBlinkWakeLockType(type));
-    mock_wake_lock_[pos].Bind(std::move(request));
-  }
+                   device::mojom::blink::WakeLockRequest request) override;
 
   MockWakeLock mock_wake_lock_[kWakeLockTypeCount];
   mojo::BindingSet<mojom::blink::WakeLockService> bindings_;
@@ -182,9 +126,7 @@ class MockPermissionService final : public mojom::blink::PermissionService {
   base::Optional<mojom::blink::PermissionStatus>
       permission_responses_[kWakeLockTypeCount];
 
-  base::OnceClosure request_permission_callbacks_[static_cast<size_t>(
-                                                      WakeLockType::kMaxValue) +
-                                                  1];
+  base::OnceClosure request_permission_callbacks_[kWakeLockTypeCount];
 };
 
 // Overrides requests for WakeLockService with MockWakeLockService instances.
@@ -202,77 +144,20 @@ class WakeLockTestingContext final {
   STACK_ALLOCATED();
 
  public:
-  WakeLockTestingContext(MockWakeLockService* mock_wake_lock_service) {
-    service_manager::InterfaceProvider::TestApi test_api(
-        GetDocument()->GetInterfaceProvider());
-    test_api.SetBinderForName(
-        mojom::blink::WakeLockService::Name_,
-        WTF::BindRepeating(&MockWakeLockService::BindRequest,
-                           WTF::Unretained(mock_wake_lock_service)));
-    test_api.SetBinderForName(
-        mojom::blink::PermissionService::Name_,
-        WTF::BindRepeating(&MockPermissionService::BindRequest,
-                           WTF::Unretained(&permission_service_)));
-  }
+  WakeLockTestingContext(MockWakeLockService* mock_wake_lock_service);
 
-  Document* GetDocument() { return &testing_scope_.GetDocument(); }
-  LocalFrame* Frame() { return &testing_scope_.GetFrame(); }
-  ScriptState* GetScriptState() { return testing_scope_.GetScriptState(); }
-  MockPermissionService& GetPermissionService() { return permission_service_; }
+  Document* GetDocument();
+  LocalFrame* Frame();
+  ScriptState* GetScriptState();
+  MockPermissionService& GetPermissionService();
 
   // Synchronously waits for |promise| to be fulfilled.
-  ScriptPromise WaitForPromiseFulfillment(ScriptPromise promise) {
-    base::RunLoop run_loop;
-    ScriptPromise return_promise =
-        promise.Then(ClosureRunnerFunction::CreateFunction(
-            GetScriptState(), run_loop.QuitClosure()));
-    // Execute pending microtasks, otherwise it can take a few seconds for the
-    // promise to resolve.
-    v8::MicrotasksScope::PerformCheckpoint(GetScriptState()->GetIsolate());
-    run_loop.Run();
-    return return_promise;
-  }
+  ScriptPromise WaitForPromiseFulfillment(ScriptPromise promise);
 
   // Synchronously waits for |promise| to be rejected.
-  void WaitForPromiseRejection(ScriptPromise promise) {
-    base::RunLoop run_loop;
-    promise.Then(v8::Local<v8::Function>(),
-                 ClosureRunnerFunction::CreateFunction(GetScriptState(),
-                                                       run_loop.QuitClosure()));
-    // Execute pending microtasks, otherwise it can take a few seconds for the
-    // promise to resolve.
-    v8::MicrotasksScope::PerformCheckpoint(GetScriptState()->GetIsolate());
-    run_loop.Run();
-  }
+  void WaitForPromiseRejection(ScriptPromise promise);
 
  private:
-  // Helper class for WaitForPromise{Fulfillment,Rejection}(). It provides a
-  // function that is invoked when a ScriptPromise is rejected that invokes
-  // |callback|.
-  class ClosureRunnerFunction final : public ScriptFunction {
-   public:
-    static v8::Local<v8::Function> CreateFunction(
-        ScriptState* script_state,
-        base::RepeatingClosure callback) {
-      auto* function = MakeGarbageCollected<ClosureRunnerFunction>(
-          script_state, std::move(callback));
-      return function->BindToV8Function();
-    }
-
-    ClosureRunnerFunction(ScriptState* script_state,
-                          base::RepeatingClosure callback)
-        : ScriptFunction(script_state), callback_(std::move(callback)) {}
-
-   private:
-    ScriptValue Call(ScriptValue) override {
-      if (callback_)
-        std::move(callback_).Run();
-      return ScriptValue();
-    }
-
-    base::RepeatingClosure callback_;
-  };
-
   MockPermissionService permission_service_;
   V8TestingScope testing_scope_;
 };
