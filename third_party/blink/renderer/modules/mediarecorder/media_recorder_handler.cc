3f7f6f868fa5c26ef945b822cc4ac98904daf15d
Miyoung Shin <myid.shin@igalia.com>
2019-07-08 09:05:58 +0000

    Remove WeakPtrFactory from MediaRecorderHandler
    This CL removes WeakPtrFactory from MediaRecorderHandler class that
    is garbage collected and replaces the weak pointer with
    WeakPersistent references.
    
    Bug: 981572
    Change-Id: I5dbfc268177cac290b85246ec029833e6baf64ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1688966
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#675183}
    
    Former-commit-id: 2a4f7981b942733a3e3f14385666ff10e695a263

--- third_party/blink/renderer/modules/mediarecorder/media_recorder_handler.cc
+++ third_party/blink/renderer/modules/mediarecorder/media_recorder_handler.cc
@@ -119,8 +119,7 @@ MediaRecorderHandler::MediaRecorderHandler(
       audio_codec_id_(AudioTrackRecorder::CodecId::LAST),
       recording_(false),
       recorder_(nullptr),
-      task_runner_(std::move(task_runner)),
-      weak_factory_(this) {}
+      task_runner_(std::move(task_runner)) {}
 
 MediaRecorderHandler::~MediaRecorderHandler() = default;
 
@@ -259,7 +258,7 @@ bool MediaRecorderHandler::Start(int timeslice) {
                            CodecIdToMediaAudioCodec(audio_codec_id_),
                            use_video_tracks, use_audio_tracks,
                            WTF::BindRepeating(&MediaRecorderHandler::WriteData,
-                                              weak_factory_.GetWeakPtr())));
+                                              WrapWeakPersistent(this))));
 
   if (use_video_tracks) {
     // TODO(mcasas): The muxer API supports only one video track. Extend it to
@@ -272,7 +271,7 @@ bool MediaRecorderHandler::Start(int timeslice) {
 
     const VideoTrackRecorder::OnEncodedVideoCB on_encoded_video_cb =
         media::BindToCurrentLoop(WTF::BindRepeating(
-            &MediaRecorderHandler::OnEncodedVideo, weak_factory_.GetWeakPtr()));
+            &MediaRecorderHandler::OnEncodedVideo, WrapWeakPersistent(this)));
 
     video_recorders_.emplace_back(MakeGarbageCollected<VideoTrackRecorder>(
         video_codec_id_, video_tracks_[0], on_encoded_video_cb,
@@ -290,7 +289,7 @@ bool MediaRecorderHandler::Start(int timeslice) {
 
     const AudioTrackRecorder::OnEncodedAudioCB on_encoded_audio_cb =
         media::BindToCurrentLoop(base::Bind(
-            &MediaRecorderHandler::OnEncodedAudio, weak_factory_.GetWeakPtr()));
+            &MediaRecorderHandler::OnEncodedAudio, WrapWeakPersistent(this)));
 
     audio_recorders_.emplace_back(MakeGarbageCollected<AudioTrackRecorder>(
         audio_codec_id_, audio_tracks_[0], std::move(on_encoded_audio_cb),
@@ -305,7 +304,6 @@ void MediaRecorderHandler::Stop() {
   DCHECK(IsMainThread());
   // Don't check |recording_| since we can go directly from pause() to stop().
 
-  weak_factory_.InvalidateWeakPtrs();
   recording_ = false;
   timeslice_ = base::TimeDelta::FromMilliseconds(0);
   video_recorders_.clear();
@@ -465,6 +463,9 @@ void MediaRecorderHandler::OnEncodedVideo(
     bool is_key_frame) {
   DCHECK(IsMainThread());
 
+  if (video_recorders_.IsEmpty())
+    return;
+
   if (UpdateTracksAndCheckIfChanged()) {
     recorder_->OnError("Amount of tracks in MediaStream has changed.");
     return;
@@ -483,6 +484,9 @@ void MediaRecorderHandler::OnEncodedAudio(const media::AudioParameters& params,
                                           std::string encoded_data,
                                           base::TimeTicks timestamp) {
   DCHECK(IsMainThread());
+
+  if (audio_recorders_.IsEmpty())
+    return;
 
   if (UpdateTracksAndCheckIfChanged()) {
     recorder_->OnError("Amount of tracks in MediaStream has changed.");
