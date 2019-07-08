7b735ae9b973a3c44e1f05f3b0a914096e4eec1f
Morten Stenshorne <mstensho@chromium.org>
2019-07-08 21:16:25 +0000

    Don't consider fieldsets as replaced content.
    They should be treated as regular block containers, as far as sizing is
    concerned. The LayoutNG out-of-flow positioning machinery treated
    fieldsets as replaced (since it was told to), and ended up with an
    incorrect inline-size.
    
    A "side-effect" of this fix is that now that fieldsets are no longer
    treated as replaced content, they can can also fragment inside multicol
    containers or paged media - just like any other block-level container.
    This is good. There should be no reason to prohibit fragmentainer
    breaks inside fieldsets. Added a test for it, to assert the new
    behavior.
    
    (cherry picked from commit 9afb7c55f260c4976a89673fd9980a2048d134bb [formerly a5837f4be0f68ff9155ab6a0c46d6afdb1991a44])
    
    Bug: 979523
    Change-Id: I2fd73f8324d3207c5d12c500b5632f631468ab34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1685096
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#674096}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1690853
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3809@{#770}
    Cr-Branched-From: ce45438777a60f42f88228700b3968406c6404d8 [formerly d82dec1a818f378c464ba307ddd9c92133eac355]-refs/heads/master@{#665002}
    
    Former-commit-id: 45de8a21f32376ba5348f496aff9087d6b81411a

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -5364,14 +5364,28 @@ bool LayoutBox::ShrinkToAvoidFloats() const {
 
 DISABLE_CFI_PERF
 bool LayoutBox::ShouldBeConsideredAsReplaced() const {
-  // Checkboxes and radioboxes are not isAtomicInlineLevel() nor do they have
-  // their own layoutObject in which to override avoidFloats().
   if (IsAtomicInlineLevel())
     return true;
-  Node* node = GetNode();
-  return node && node->IsElementNode() &&
-         (ToElement(node)->IsFormControlElement() ||
-          IsHTMLImageElement(ToElement(node)));
+  // We need to detect all types of objects that should be treated as replaced.
+  // Callers of this method will use the result for various things, such as
+  // determining how to size the object, or whether it needs to avoid adjacent
+  // floats, just like objects that establish a new formatting context.
+  // IsAtomicInlineLevel() will not catch all the cases. Objects may be
+  // block-level and still replaced, and we cannot deduce this from the
+  // LayoutObject type. Checkboxes and radio buttons are such examples. We need
+  // to check the Element type. This also applies to images, since we may have
+  // created a block-flow LayoutObject for the ALT text (which still counts as
+  // replaced).
+  auto* element = ToElementOrNull(GetNode());
+  if (!element)
+    return false;
+  if (element->IsFormControlElement()) {
+    // Form control elements are generally replaced objects. Fieldsets are not,
+    // though. A fieldset is (almost) a regular block container, and should be
+    // treated as such.
+    return !IsHTMLFieldSetElement(element);
+  }
+  return IsHTMLImageElement(element);
 }
 
 bool LayoutBox::HasNonCompositedScrollbars() const {
