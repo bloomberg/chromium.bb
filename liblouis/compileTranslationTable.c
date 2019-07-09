368d26c7ff65cc7cbce198e6e7471b4d1ae9589f
Bert Frees <bertfrees@gmail.com>
2019-07-09 20:08:16 +0200

    Eliminate global variable "lastTrans"
    The last table used in now always put at the top of tableChain.

--- liblouis/compileTranslationTable.c
+++ liblouis/compileTranslationTable.c
@@ -4081,7 +4081,6 @@ cleanup:
 	return table;
 }
 
-static ChainEntry *lastTrans = NULL;
 /* Return the emphasis classes declared in tableList. */
 char const **EXPORT_CALL
 lou_getEmphClasses(const char *tableList) {
@@ -4114,43 +4113,39 @@ lou_getTable(const char *tableList) {
 	/* Keep track of which tables have already been compiled */
 	int tableListLen;
 	ChainEntry *currentEntry = NULL;
-	ChainEntry *lastEntry = NULL;
+	ChainEntry *prevEntry = NULL;
 	TranslationTableHeader *newTable;
 	if (tableList == NULL || *tableList == 0) return NULL;
 	errorCount = fileCount = 0;
 	tableListLen = (int)strlen(tableList);
-	/* See if this is the last table used. */
-	if (lastTrans != NULL)
-		if (tableListLen == lastTrans->tableListLength &&
-				(memcmp(&lastTrans->tableList[0], tableList, tableListLen)) == 0)
-			return (gTable = lastTrans->table);
 	/* See if Table has already been compiled */
 	currentEntry = tableChain;
 	while (currentEntry != NULL) {
 		if (tableListLen == currentEntry->tableListLength &&
 				(memcmp(&currentEntry->tableList[0], tableList, tableListLen)) == 0) {
-			lastTrans = currentEntry;
+			/* Move the table to the top of the table chain. */
+			if (prevEntry != NULL) {
+				prevEntry->next = currentEntry->next;
+				currentEntry->next = tableChain;
+				tableChain = currentEntry;
+			}
 			return (gTable = currentEntry->table);
 		}
-		lastEntry = currentEntry;
+		prevEntry = currentEntry;
 		currentEntry = currentEntry->next;
 	}
 	if ((newTable = compileTranslationTable(tableList, &gCharacterClasses,
 				 &gCharacterClassAttribute, gOpcodeLengths, &gNewRuleOffset, &gNewRule,
 				 &gRuleNames))) {
-		/* Add a new entry to the table chain. */
+		/* Add a new entry to the top of the table chain. */
 		int entrySize = sizeof(ChainEntry) + tableListLen;
 		ChainEntry *newEntry = malloc(entrySize);
 		if (!newEntry) _lou_outOfMemory();
-		if (tableChain == NULL)
-			tableChain = newEntry;
-		else
-			lastEntry->next = newEntry;
-		newEntry->next = NULL;
+		newEntry->next = tableChain;
 		newEntry->table = newTable;
 		newEntry->tableListLength = tableListLen;
 		memcpy(&newEntry->tableList[0], tableList, tableListLen);
-		lastTrans = newEntry;
+		tableChain = newEntry;
 		return (gTable = newEntry->table);
 	}
 	_lou_logMessage(LOU_LOG_ERROR, "%s could not be compiled", tableList);
@@ -4300,7 +4295,6 @@ lou_free(void) {
 			free(previousEntry);
 		}
 		tableChain = NULL;
-		lastTrans = NULL;
 	}
 	if (typebuf != NULL) free(typebuf);
 	typebuf = NULL;
