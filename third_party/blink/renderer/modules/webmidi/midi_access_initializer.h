79948ba11e61321f6f32ab56ce45bdf210ec8738
Omer Katz <omerkatz@chromium.org>
2019-07-10 16:05:43 +0000

    Converting MIDIAccessInitializer from EagerSweeping to PreFinalizer
    Created a pre-finalizer to reset accessor_ and permission_service_.
    
    Bug: 981043
    Change-Id: Ie91a044f5991f4b082f8a6c3092a0f8f6a80e029
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1693009
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676019}
    
    Former-commit-id: b0e400ed499bbcd195d1b3745d1c2c432a9aeaf4

--- third_party/blink/renderer/modules/webmidi/midi_access_initializer.h
+++ third_party/blink/renderer/modules/webmidi/midi_access_initializer.h
@@ -24,6 +24,8 @@ class ScriptState;
 
 class MODULES_EXPORT MIDIAccessInitializer : public ScriptPromiseResolver,
                                              public MIDIAccessorClient {
+  USING_PRE_FINALIZER(MIDIAccessInitializer, Dispose);
+
  public:
   struct PortDescriptor {
     DISALLOW_NEW();
@@ -59,9 +61,7 @@ class MODULES_EXPORT MIDIAccessInitializer : public ScriptPromiseResolver,
   MIDIAccessInitializer(ScriptState*, const MIDIOptions*);
   ~MIDIAccessInitializer() override = default;
 
-  // Eager finalization to allow dispose() operation access
-  // other (non eager) heap objects.
-  EAGERLY_FINALIZE();
+  void Dispose();
 
   // MIDIAccessorClient
   void DidAddInputPort(const String& id,
