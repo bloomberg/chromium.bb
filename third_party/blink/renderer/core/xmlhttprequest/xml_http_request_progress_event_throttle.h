f64b69439816d30765c389f0828f98eb2aa64bc6
Omer Katz <omerkatz@chromium.org>
2019-07-10 15:39:16 +0000

    Converting XMLHttpRequestProgressEventThrottle to PreFinalizer
    Because XMLHttpRequestProgressEventThrottle is a subclass of TimerBase,
    the pre-finalizer needs to stop the timer.
    Also added a is_stopped_ flag to make sure that if Stop is called twice,
    the second time will be noop.
    
    Bug: 981043
    Change-Id: Iefc1b92e00a42f2b4ea134a7da121ab838c2aa4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1692411
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676012}
    
    Former-commit-id: 596a7be5741c431ce3ab64797d091395083eabe7

--- third_party/blink/renderer/core/xmlhttprequest/xml_http_request_progress_event_throttle.h
+++ third_party/blink/renderer/core/xmlhttprequest/xml_http_request_progress_event_throttle.h
@@ -52,6 +52,9 @@ class XMLHttpRequest;
 class XMLHttpRequestProgressEventThrottle final
     : public GarbageCollectedFinalized<XMLHttpRequestProgressEventThrottle>,
       public TimerBase {
+  // Need to promptly stop this timer when it is deemed finalizable.
+  USING_PRE_FINALIZER(XMLHttpRequestProgressEventThrottle, Stop);
+
  public:
   explicit XMLHttpRequestProgressEventThrottle(XMLHttpRequest*);
   ~XMLHttpRequestProgressEventThrottle() override;
@@ -80,8 +83,6 @@ class XMLHttpRequestProgressEventThrottle final
   // depending on the value of the ProgressEventAction argument.
   void DispatchReadyStateChangeEvent(Event*, DeferredEventAction);
 
-  // Need to promptly stop this timer when it is deemed finalizable.
-  EAGERLY_FINALIZE();
   void Trace(blink::Visitor*);
 
  private:
