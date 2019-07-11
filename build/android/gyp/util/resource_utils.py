6aeba3a7dca89198258c633e67a0c1bf5d24ee14
Eric Stevenson <estevenson@chromium.org>
2019-07-11 19:34:44 +0000

    Android: Make R.java inheritance work with Trichrome proguard.
    bda5cdeb0ce66103dfec0b08c53b309e787d174f [formerly 212f516e112565c703f9ae7f206f58be5ebbb3e2] changed the way R.java files
    work so that all resources are stored in a base R.java file, but
    accessed through R.java classes that subclass the base R.java.
    
    Currently for synchronized proguard, we combine all of the jars
    produced by the APK's java_library_impl() step. This dedupes class
    files, but also requires classes with the same name to be identical.
    With R.java inheritance, each APK will have a base module R.java
    with different contents, and Chrome will crash at runtime due to
    missing resources.
    
    We can't just rename the base module name in the various APKs that
    are part of synchronized proguarding because the subclass R.java
    files can only be included once, and can only have one direct
    superclass.
    
    Instead of creating a more complex R.java class hierarchy, this CL
    changes the compile_resources() step of the static library APK to
    generate a base R.java that contains the resources of both Chrome
    and Webview.
    
    Bug: 901465
    Change-Id: Iaa2a05c75cd431b95afe6318da8c7982fffbc242
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1695699
    Commit-Queue: Eric Stevenson <estevenson@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676552}
    
    Former-commit-id: 852d646f8f598dd1377b0ee2e1228a2beb932b2c

--- build/android/gyp/util/resource_utils.py
+++ build/android/gyp/util/resource_utils.py
@@ -335,7 +335,8 @@ def CreateRJavaFiles(srcjar_dir,
                      rjava_build_options,
                      srcjar_out,
                      custom_root_package_name=None,
-                     grandparent_custom_package_name=None):
+                     grandparent_custom_package_name=None,
+                     extra_main_r_text_files=None):
   """Create all R.java files for a set of packages and R.txt files.
 
   Args:
@@ -358,6 +359,7 @@ def CreateRJavaFiles(srcjar_dir,
       as the grandparent_custom_package_name. The format of this package name
       is identical to custom_root_package_name.
       (eg. for vr grandparent_custom_package_name would be "base")
+    extra_main_r_text_files: R.txt files to be added to the root R.java file.
   Raises:
     Exception if a package name appears several times in |extra_res_packages|
   """
@@ -378,12 +380,23 @@ def CreateRJavaFiles(srcjar_dir,
   all_resources = {}
   all_resources_by_type = collections.defaultdict(list)
 
-  for entry in _ParseTextSymbolsFile(main_r_txt_file, fix_package_ids=True):
-    all_resources[(entry.resource_type, entry.name)] = entry
-    all_resources_by_type[entry.resource_type].append(entry)
-    assert entry.resource_type in _ALL_RESOURCE_TYPES, (
-        'Unknown resource type: %s, add to _ALL_RESOURCE_TYPES!' %
-        entry.resource_type)
+  main_r_text_files = [main_r_txt_file]
+  if extra_main_r_text_files:
+    main_r_text_files.extend(extra_main_r_text_files)
+  for r_txt_file in main_r_text_files:
+    for entry in _ParseTextSymbolsFile(r_txt_file, fix_package_ids=True):
+      entry_key = (entry.resource_type, entry.name)
+      if entry_key in all_resources:
+        assert entry == all_resources[entry_key], (
+            'Input R.txt %s provided a duplicate resource with a different '
+            'entry value. Got %s, expected %s.' % (r_txt_file, entry,
+                                                   all_resources[entry_key]))
+      else:
+        all_resources[entry_key] = entry
+        all_resources_by_type[entry.resource_type].append(entry)
+        assert entry.resource_type in _ALL_RESOURCE_TYPES, (
+            'Unknown resource type: %s, add to _ALL_RESOURCE_TYPES!' %
+            entry.resource_type)
 
   if custom_root_package_name:
     # Custom package name is available, thus use it for root_r_java_package.
