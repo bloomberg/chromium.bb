c749451b6a43c8ff0b420796a1d03e522af05b47
Lukasz Anforowicz <lukasza@chromium.org>
2019-07-11 02:12:15 +0000

    Removing SiteIsolation.Isolate* UMA.
    Bug: 975222
    Change-Id: I8b7f7b1bc67c4b90c9431d6f648d389f8ffbe8b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1686613
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676256}
    
    Former-commit-id: c2004b0ca3f44b66dace86e065dfee632e27aacd

--- chrome/browser/site_isolation/site_details.h
+++ chrome/browser/site_isolation/site_details.h
@@ -8,23 +8,12 @@
 #include <stdint.h>
 
 #include <map>
+#include <set>
 
 #include "base/macros.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/site_instance.h"
 #include "content/public/browser/web_contents.h"
-
-// Collects information for a browsing instance assuming some alternate
-// isolation scenario.
-struct ScenarioBrowsingInstanceInfo {
-  ScenarioBrowsingInstanceInfo();
-  ScenarioBrowsingInstanceInfo(const ScenarioBrowsingInstanceInfo& other);
-  ~ScenarioBrowsingInstanceInfo();
-
-  std::set<GURL> sites;
-};
-using ScenarioBrowsingInstanceMap =
-    std::map<int32_t, ScenarioBrowsingInstanceInfo>;
 
 // Collects metrics about an actual browsing instance in the current session.
 struct BrowsingInstanceInfo {
@@ -38,37 +27,12 @@ struct BrowsingInstanceInfo {
 using BrowsingInstanceMap =
     std::map<content::SiteInstance*, BrowsingInstanceInfo>;
 
-// This enum represents various alternative process model policies that we want
-// to evaluate. We'll estimate the process cost of each scenario.
-enum IsolationScenarioType {
-  ISOLATE_NOTHING,
-  ISOLATE_ALL_SITES,
-  ISOLATE_HTTPS_SITES,
-  ISOLATE_EXTENSIONS,
-  ISOLATION_SCENARIO_LAST = ISOLATE_EXTENSIONS
-};
-
-// Contains the state required to estimate the process count under a particular
-// process model. We have one of these per IsolationScenarioType.
-struct IsolationScenario {
-  IsolationScenario();
-  IsolationScenario(const IsolationScenario& other);
-  ~IsolationScenario();
-
-  IsolationScenarioType policy = ISOLATE_NOTHING;
-  std::set<GURL> all_sites;
-  ScenarioBrowsingInstanceMap browsing_instances;
-};
-
 // Information about the sites and SiteInstances in each BrowsingInstance, for
 // use in estimating the number of processes needed for various process models.
 struct SiteData {
   SiteData();
   SiteData(const SiteData& other);
   ~SiteData();
-
-  // One IsolationScenario object per IsolationScenarioType.
-  IsolationScenario scenarios[ISOLATION_SCENARIO_LAST + 1];
 
   // This map groups related SiteInstances together into BrowsingInstances. The
   // first SiteInstance we see in a BrowsingInstance is designated as the
@@ -96,9 +60,9 @@ class SiteDetails {
                                int non_renderer_process_count);
 
  private:
-  // Never needs to be constructed.
-  SiteDetails();
-  ~SiteDetails();
+  // Only static methods - never needs to be constructed.
+  SiteDetails() = delete;
+  ~SiteDetails() = delete;
 
   DISALLOW_COPY_AND_ASSIGN(SiteDetails);
 };
