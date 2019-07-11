7dfbdf08acb809b26bcc31d8698c0869f5038231
Nate Fischer <ntfschr@google.com>
2019-07-11 19:40:46 +0000

    AW: write console.log into logcat during tests
    No change to production logic.
    
    Although writing JavaScript console.log() messages (and related methods)
    to android logcat is a privacy issue if done in production (see
    https://crbug.com/905251), it's perfectly fine to do during automated
    tests, where all the web content is written by chromium contributors.
    
    This changes onConsoleMessage() to print the actual text passed to
    console.log() into logcat, including other attributes (ex. source code
    file the log message came from).
    
    This also changes the behavior to log every console message to logcat,
    rather than the previous implementation which was gated behind the TRACE
    variable. This is to help catch JavaScript errors (which also generate
    console messages).
    
    Bug: 983005, 964471
    Test: used this patch when investigating another test, verified logs in logcat
    Change-Id: Ied627d106f6ef9af4b05c67556d62d80c6462b49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1696908
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676555}
    
    Former-commit-id: 102433aedb32b806cf519b82cbf9c229e52b8c01

--- android_webview/javatests/src/org/chromium/android_webview/test/TestAwContentsClient.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/TestAwContentsClient.java
@@ -392,9 +392,33 @@ public class TestAwContentsClient extends NullContentsClient {
 
     @Override
     public boolean onConsoleMessage(AwConsoleMessage consoleMessage) {
-        if (TRACE) Log.i(TAG, "onConsoleMessage " + consoleMessage);
+        // Log unconditionally, because JavaScript errors also generate ConsoleMessages (and
+        // developers generally expect logcat to show such errors).
+        logConsoleMessage(consoleMessage);
         mAddMessageToConsoleHelper.notifyCalled(consoleMessage);
         return false;
+    }
+
+    private void logConsoleMessage(AwConsoleMessage consoleMessage) {
+        String formattedMessage = "[" + consoleMessage.sourceId() + ":"
+                + consoleMessage.lineNumber() + "] " + consoleMessage.message();
+        switch (consoleMessage.messageLevel()) {
+            case AwConsoleMessage.MESSAGE_LEVEL_TIP:
+            case AwConsoleMessage.MESSAGE_LEVEL_LOG:
+                Log.i(TAG, "onConsoleMessage " + formattedMessage);
+                break;
+            case AwConsoleMessage.MESSAGE_LEVEL_WARNING:
+                Log.w(TAG, "onConsoleMessage " + formattedMessage);
+                break;
+            case AwConsoleMessage.MESSAGE_LEVEL_ERROR:
+                Log.e(TAG, "onConsoleMessage " + formattedMessage);
+                break;
+            default:
+                // Should not be reached, but fall-through anyway.
+            case AwConsoleMessage.MESSAGE_LEVEL_DEBUG:
+                Log.d(TAG, "onConsoleMessage " + formattedMessage);
+                break;
+        }
     }
 
     /**
