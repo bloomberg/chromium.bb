b2f44d90c942d537cf8801c55c71cd454fc7f409
Alex Danilo <adanilo@chromium.org>
2019-07-11 04:17:39 +0000

    Make feedback panel resources load lazily
    Moves the feedback panel JS files out of the main script bundle, into
    the lazily loaded set used for Polymer elements.
    
    Also inline the background image resources for the button for now: we
    will replace them in future CL with a cr-icon-button. Remove the icon
    pause/expand/cancel SVG images: no longer used.
    
    Bug: 947388
    Change-Id: I0064b1b35a62d5f06674eada04cb0cd85191dc3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1692110
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#676289}
    
    Former-commit-id: eb4d334db2ba5083ca3e4219c6bc32c4c9eaaeeb

--- ui/file_manager/file_manager/foreground/elements/xf_button.js
+++ ui/file_manager/file_manager/foreground/elements/xf_button.js
@@ -34,17 +34,33 @@ class PanelButton extends HTMLElement {
               }
 
               :host([data-category='pause']) {
-                background: url(../images/files/ui/pause.svg) no-repeat center;
+                background: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0naHR\
+                  0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHdpZHRoPSczNnB4JyBoZWlnaHQ\
+                  9JzM2cHgnIHZpZXdCb3g9JzAgMCAzNiAzNic+CiAgICA8ZyBzdHJva2U9JyM\
+                  1RjYzNjgnIHN0cm9rZS13aWR0aD0nMyc+CiAgICAgICAgPGxpbmUgeDE9JzE\
+                  1JyB5MT0nMTInIHgyPScxNScgeTI9JzI0Jy8+CiAgICAgICAgPGxpbmUgeDE\
+                  9JzIxJyB5MT0nMTInIHgyPScyMScgeTI9JzI0Jy8+CiAgICA8L2c+Cjwvc3Z\
+                  nPg==') no-repeat center;
               }
 
-              :host([data-category='cancel']),
-              :host([data-category='dismiss']) {
-                background: url(../images/files/ui/cancel.svg) no-repeat center;
+              :host([data-category='cancel']) {
+                background: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0naHR\
+                  0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHdpZHRoPSczNnB4JyBoZWlnaHQ\
+                  9JzM2cHgnIHZpZXdCb3g9JzAgMCAzNiAzNic+CiAgICA8ZyBzdHJva2U9JyM\
+                  1RjYzNjgnIHN0cm9rZS13aWR0aD0nMic+CiAgICAgICAgPGxpbmUgeDE9JzE\
+                  yJyB5MT0nMTInIHgyPScyNCcgeTI9JzI0Jy8+CiAgICAgICAgPGxpbmUgeDE\
+                  9JzI0JyB5MT0nMTInIHgyPScxMicgeTI9JzI0Jy8+CiAgICA8L2c+Cjwvc3Z\
+                  nPg==') no-repeat center;
               }
 
               :host([data-category='expand']),
               :host([data-category='collapse']) {
-                background: url(../images/files/ui/expand.svg) no-repeat center;
+                background: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0naHR\
+                  0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHdpZHRoPSczNnB4JyBoZWlnaHQ\
+                  9JzM2cHgnIHZpZXdCb3g9JzAgMCAzNiAzNic+CiAgICA8ZyBzdHJva2U9JyM\
+                  1RjYzNjgnIHN0cm9rZS13aWR0aD0nMic+CiAgICAgICAgPHBhdGggZmlsbD0\
+                  ibm9uZSIgZD0nTTEyLDIxbDYsLTYgNiw2Jy8+CiAgICA8L2c+Cjwvc3ZnPg=='
+                  ) no-repeat center;
               }
 
               @keyframes setcollapse {
