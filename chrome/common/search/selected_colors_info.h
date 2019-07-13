782a6010c2116fe4cc2ac731f83e546a3de83131
Gayane Petrosyan <gayane@chromium.org>
2019-07-13 03:58:44 +0000

    [Chrome Colors] New sizing, template and pallet for Colors menu.
    screencasts:
    https://drive.google.com/file/d/1tTODUlntVr5Wv50vlNkZClrZIlr9Ks9U/view?usp=sharing
    https://drive.google.com/file/d/1-IjVlNx8ytgrc5ikXN-PrEcomlY-cv4g/view?usp=sharing
    
    Bug: 960982, 976413
    Change-Id: Ib82b09ad0a89079c4de3fec79dc4d93af3d87523
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1699948
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677180}
    
    Former-commit-id: 923ecf2c2b5a9450ea63cb0f2293ca36cf99ec4c

--- chrome/common/search/selected_colors_info.h
+++ chrome/common/search/selected_colors_info.h
@@ -28,10 +28,25 @@ struct ColorInfo {
 // TODO(gayane): Add colors selected by UX.
 // List of preselected colors to show in Chrome Colors menu.
 constexpr ColorInfo kSelectedColorsInfo[] = {
-    ColorInfo(0, SkColorSetRGB(120, 0, 120), "purple"),
-    ColorInfo(1, SkColorSetRGB(0, 100, 100), "teal"),
-    ColorInfo(2, SkColorSetRGB(200, 50, 100), "pretty"),
-    ColorInfo(3, SkColorSetRGB(70, 50, 170), "bluish")};
+    ColorInfo(0, SkColorSetRGB(192, 230, 181), "Pistachio"),
+    ColorInfo(1, SkColorSetRGB(158, 228, 233), "Seafoam"),
+    ColorInfo(2, SkColorSetRGB(219, 237, 254), "Mist"),
+    ColorInfo(3, SkColorSetRGB(255, 174, 189), "Flamingo"),
+    ColorInfo(4, SkColorSetRGB(221, 179, 197), "Wine"),
+    ColorInfo(5, SkColorSetRGB(210, 175, 248), "Lavender"),
+    ColorInfo(6, SkColorSetRGB(217, 213, 213), "Elephant"),
+    ColorInfo(7, SkColorSetRGB(246, 97, 12), "Sunset"),
+    ColorInfo(8, SkColorSetRGB(247, 194, 12), "Banana"),
+    ColorInfo(9, SkColorSetRGB(109, 180, 87), "Kiwi"),
+    ColorInfo(10, SkColorSetRGB(25, 157, 169), "Robins Egg"),
+    ColorInfo(11, SkColorSetRGB(93, 147, 228), "Blue"),
+    ColorInfo(12, SkColorSetRGB(23, 94, 26), "Pine"),
+    ColorInfo(13, SkColorSetRGB(10, 110, 119), "Oceanic"),
+    ColorInfo(14, SkColorSetRGB(126, 24, 39), "Bordeaux"),
+    ColorInfo(15, SkColorSetRGB(91, 55, 137), "Eggplant"),
+    ColorInfo(16, SkColorSetRGB(45, 65, 109), "Navy"),
+    ColorInfo(17, SkColorSetRGB(33, 33, 33), "Midnight"),
+    ColorInfo(18, SkColorSetRGB(189, 22, 82), "Raspberry")};
 
 }  // namespace chrome_colors
 
