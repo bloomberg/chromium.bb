38a8a28fe03ddfb01a4af001def308466f3146c6
Mohsen Izadi <mohsen@chromium.org>
2019-07-15 20:09:18 +0000

    Disable ImageAnnotationBrowserTest.AnnotationLanguages
    The test is flaky on all platforms, not just Windows.
    
    TBR=katie@chromium.org
    BUG=983404
    
    Change-Id: I05200a2aa7fea4be8af45cfebfb19963981f8455
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1702490
    Reviewed-by: Mohsen Izadi <mohsen@chromium.org>
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677450}
    
    Former-commit-id: dff1f12768079d51098c110ff2f9ca91f8bbaed3

--- chrome/browser/accessibility/image_annotation_browsertest.cc
+++ chrome/browser/accessibility/image_annotation_browsertest.cc
@@ -401,13 +401,9 @@ IN_PROC_BROWSER_TEST_F(ImageAnnotationBrowserTest, ImageWithSrcSet) {
       "Appears to say: red.png Annotation. Appears to be: red.png 'en' Label");
 }
 
-// Flaky on Windows.  http://crbug.com/983404
-#if defined(OS_WIN)
-#define MAYBE_AnnotationLanguages DISABLED_AnnotationLanguages
-#else
-#define MAYBE_AnnotationLanguages AnnotationLanguages
-#endif
-IN_PROC_BROWSER_TEST_F(ImageAnnotationBrowserTest, MAYBE_AnnotationLanguages) {
+// Disabled due to flakiness. http://crbug.com/983404
+IN_PROC_BROWSER_TEST_F(ImageAnnotationBrowserTest,
+                       DISABLED_AnnotationLanguages) {
   FakeAnnotator::SetReturnOcrResults(true);
   FakeAnnotator::SetReturnLabelResults(true);
 
