49e0d3f3dc1b667199c45ad22b9a4f5c91d2cf8b
Dominic Battre <battre@chromium.org>
2019-07-15 17:45:27 +0000

    Move PasswordManagerInternalsServiceTest
    Move PasswordManagerInternalsServiceTests from chrome/browser into component as
    there are no necesary dependencies on the full browser.
    
    Bug: 928595
    Change-Id: Ib72b96375a17997e5f51b6d35325a77c61877b8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1701865
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677385}
    
    Former-commit-id: 1c8bc56b6478733a950fdb21d63430568c1f3c28

--- components/password_manager/content/browser/password_manager_internals_service_factory_unittest.cc
+++ components/password_manager/content/browser/password_manager_internals_service_factory_unittest.cc
@@ -2,12 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/password_manager/core/browser/password_manager_internals_service.h"
-
-#include "chrome/test/base/testing_profile.h"
-#include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/password_manager/content/browser/password_manager_internals_service_factory.h"
+
+#include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/password_manager/core/browser/log_receiver.h"
+#include "components/password_manager/core/browser/password_manager_internals_service.h"
+#include "content/public/test/test_browser_context.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -26,29 +26,33 @@ class MockLogReceiver : public password_manager::LogReceiver {
   MOCK_METHOD1(LogSavePasswordProgress, void(const std::string&));
 };
 
-enum ProfileType { NORMAL_PROFILE, INCOGNITO_PROFILE };
-
-std::unique_ptr<TestingProfile> CreateProfile(ProfileType type) {
-  TestingProfile::Builder builder;
-  return builder.Build();
-}
-
 }  // namespace
 
-class PasswordManagerInternalsServiceTest : public testing::Test {
+class PasswordManagerInternalsServiceFactoryTest : public testing::Test {
+ public:
   content::TestBrowserThreadBundle thread_bundle_;
+  content::TestBrowserContext browser_context_;
+
+  void SetUp() override {
+    BrowserContextDependencyManager::GetInstance()->MarkBrowserContextLive(
+        &browser_context_);
+  }
+
+  void TearDown() override {
+    BrowserContextDependencyManager::GetInstance()
+        ->DestroyBrowserContextServices(&browser_context_);
+  }
 };
 
 // When the profile is not incognito, it should be possible to activate the
 // service.
-TEST_F(PasswordManagerInternalsServiceTest, ServiceActiveNonIncognito) {
-  std::unique_ptr<TestingProfile> profile(CreateProfile(NORMAL_PROFILE));
+TEST_F(PasswordManagerInternalsServiceFactoryTest, ServiceActiveNonIncognito) {
+  browser_context_.set_is_off_the_record(false);
   PasswordManagerInternalsService* service =
       PasswordManagerInternalsServiceFactory::GetForBrowserContext(
-          profile.get());
+          &browser_context_);
   testing::StrictMock<MockLogReceiver> receiver;
 
-  ASSERT_TRUE(profile);
   ASSERT_TRUE(service);
   EXPECT_EQ(std::string(), service->RegisterReceiver(&receiver));
 
@@ -60,16 +64,13 @@ TEST_F(PasswordManagerInternalsServiceTest, ServiceActiveNonIncognito) {
 
 // When the browser profile is incognito, it should not be possible to activate
 // the service.
-TEST_F(PasswordManagerInternalsServiceTest, ServiceNotActiveIncognito) {
-  std::unique_ptr<TestingProfile> profile(CreateProfile(INCOGNITO_PROFILE));
-  ASSERT_TRUE(profile);
-
-  Profile* incognito_profile = profile->GetOffTheRecordProfile();
+TEST_F(PasswordManagerInternalsServiceFactoryTest, ServiceNotActiveIncognito) {
+  browser_context_.set_is_off_the_record(true);
   PasswordManagerInternalsService* service =
       PasswordManagerInternalsServiceFactory::GetForBrowserContext(
-          incognito_profile);
+          &browser_context_);
   // BrowserContextKeyedServiceFactory::GetBrowserContextToUse should return
-  // NULL for |profile|, because |profile| is incognito. Therefore the returned
-  // |service| should also be NULL.
+  // nullptr for |browser_context|, because |browser_context| is incognito.
+  // Therefore the returned |service| should also be nullptr.
   EXPECT_FALSE(service);
 }
