81119e7d229319ce717f3459e3156081ff455b36
Imran Haider <ihaider3@bloomberg.net>
2019-07-16 17:45:01 -0400

    Consider a niceness factor when deciding to work (#560)
    We are accumulating a budget on every iteration and once the budget
    reaches a certain value, we allow Chromium to work.  In a steady
    state system, you can consider the 'allowance to work' as the value
    in a square PWM wave with a fixed duty cycle.  Note that when our
    wave is at its trough, we allow low priority messages to be
    processed; however, this can only happen if all message pumps on
    the same thread are simultanously at their trough.
    
    It can be very unlikely for the PWM waves for all message pumps to
    be at their trough at the same time and so the low priority messages
    might be starved.  To get around this, we can change the duty cycle
    of our wave and expand the time we spent at its trough.  This will
    make it more likely for all the message pumps to simultanously spend
    some time at their trough and that will allow low priority messages
    to be processed.
    
    This commit introduces a 'niceness' factor that is multiplied by
    the duty cycle of the wave.  Over every message pump iteration, we
    increase the niceness level by a small amount, which then alters the
    duty cycle of our message pump.

--- src/blpwtk2/private/blpwtk2_queuepumpscheduler.cc
+++ src/blpwtk2/private/blpwtk2_queuepumpscheduler.cc
@@ -41,6 +41,8 @@ QueuePumpScheduler::QueuePumpScheduler()
     d_tunables.push_back({"idle-work-budget", 50});
     d_tunables.push_back({"important-work-charge", 100});
     d_tunables.push_back({"idle-work-charge", 120});
+    d_tunables.push_back({"niceness-increment", 110});
+    d_tunables.push_back({"max-niceness", 500});
 }
 
 QueuePumpScheduler::~QueuePumpScheduler()
@@ -56,6 +58,8 @@ void QueuePumpScheduler::isReadyToWork(bool *allowNormalWork, bool *allowIdleWor
     unsigned int idleWorkBudget = d_tunables[1].second;
     unsigned int importantWorkCharge = d_tunables[2].second;
     unsigned int idleWorkCharge = d_tunables[3].second;
+    unsigned int nicenessIncrement = d_tunables[4].second;
+    unsigned int maxNiceness = d_tunables[5].second;
 
     if (GetInputState() || queueStatus & (QS_TIMER | QS_PAINT)) {
         // The input messages are only fetched from the input queue when
@@ -85,14 +89,49 @@ void QueuePumpScheduler::isReadyToWork(bool *allowNormalWork, bool *allowIdleWor
         // state where the chrome pump message is starved by these low
         // priority  messages.  For this reason, we only preempt ourselves a
         // fraction of the time.
-        d_importantWorkBudget += importantWorkBudget;
-        d_idleWorkBudget += idleWorkBudget;
+        d_importantWorkBudget += importantWorkBudget / d_niceness;
+        d_idleWorkBudget += idleWorkBudget / d_niceness;
+
+        // We are accumulating a budget on every iteration and once the budget
+        // reaches a certain value, we allow Chromium to work.  In a steady
+        // state system, you can consider the 'allowance to work' as the value
+        // in a square PWM wave with a fixed duty cycle.  Note that when our
+        // wave is at its trough, we allow low priority messages to be
+        // processed; however, this can only happen if all message pumps on
+        // the same thread are simultanously at their trough.
+        //
+        // It can be very unlikely for the PWM waves for all message pumps to
+        // be at their trough at the same time and so the low priority messages
+        // might be starved.  To get around this, we can change the duty cycle
+        // of our wave and expand the time we spent at its trough.  This will
+        // make it more likely for all the message pumps to simultanously spend
+        // some time at their trough and that will allow low priority messages
+        // to be processed.
+        //
+        // We change the duty cycle by introducing a 'niceness' factor.  Over
+        // every message pump iteration, we increase the niceness by a small
+        // amount, which alters the duty cycle of our PWM wave.
+        d_niceness *= (nicenessIncrement * 0.01);
+
+        // Enforce a maximum value for the 'niceness'.  It is possible for the
+        // main thread to be kept sufficiently busy by the embedder such that
+        // there is always a timer or paint message.  In that case, we would
+        // bump up our niceness on every iteration and end up not doing any
+        // chromium work.  We solve this problem by applying a maximum to the
+        // 'niceness' level.
+        double maxNicenessOver100 = maxNiceness * 0.01;
+        if (d_niceness > maxNicenessOver100) {
+            d_niceness = maxNicenessOver100;
+        }
     }
     else {
         // Since there are no pending low-priority messages, we
         // conditionally allow chromium to do work by adding the charge
         // to the budget.
         d_importantWorkBudget += importantWorkCharge;
+
+        // Reset the niceness to its default value.
+        d_niceness = 1.0;
 
         if (0 == (queueStatus & QS_SENDMESSAGE)) {
             // No sent messages are in the Windows queue so we allow Chromium
