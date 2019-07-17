b9f1c3916497b920b2c99aa6b6e417c2b069c2b0
Reid Kleckner <rnk@google.com>
2019-07-17 22:56:31 +0000

    Clang update script: bump gnuwin version to include cut.exe
    LLVM r365162 added a lit tests that uses cut. The new package has
    cut.exe from UnxUtils.zip.
    
    R=hans@chromium.org, pcc@chromium.org, thakis@chromium.org
    
    Bug: 984065
    Change-Id: I35c42c33cc3c621ad5881caffc50d80f2cf11ed9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1707474
    Commit-Queue: Reid Kleckner <rnk@chromium.org>
    Auto-Submit: Reid Kleckner <rnk@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678487}
    
    Former-commit-id: ac6afaf292679c501328a0d454bc8b478dc0e0cb

--- tools/clang/scripts/build.py
+++ tools/clang/scripts/build.py
@@ -212,7 +212,7 @@ def AddGnuWinToPath():
     return
 
   gnuwin_dir = os.path.join(LLVM_BUILD_TOOLS_DIR, 'gnuwin')
-  GNUWIN_VERSION = '11'
+  GNUWIN_VERSION = '12'
   GNUWIN_STAMP = os.path.join(gnuwin_dir, 'stamp')
   if ReadStampFile(GNUWIN_STAMP) == GNUWIN_VERSION:
     print('GNU Win tools already up to date.')
