7dc75b0c0037dddd50be9da7fc763d207ca763f6
Chase Phillips <cmp@chromium.org>
2019-07-19 17:19:00 +0000

    IndexedDB: Return after detecting a bad mojo message
    (cherry picked from commit d110d4fc0f02ce189799f0cdc5c7ab3f70e8056d [formerly 227859e5d0e3d03b2fb7f8314110aecd062ea387])
    
    Bug: 984521
    Change-Id: I7b89323f4fcb8fadcc9926387729fa508463c6a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1704858
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Chase Phillips <cmp@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#677999}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1709742
    Reviewed-by: Chase Phillips <cmp@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3809@{#916}
    Cr-Branched-From: ce45438777a60f42f88228700b3968406c6404d8 [formerly d82dec1a818f378c464ba307ddd9c92133eac355]-refs/heads/master@{#665002}
    
    Former-commit-id: 16954c99cba9ac608ae56064dd4274f5d3fc65e3

--- content/browser/indexed_db/transaction_impl.cc
+++ content/browser/indexed_db/transaction_impl.cc
@@ -99,6 +99,7 @@ void TransactionImpl::CreateObjectStore(int64_t object_store_id,
   if (transaction_->mode() != blink::mojom::IDBTransactionMode::VersionChange) {
     mojo::ReportBadMessage(
         "CreateObjectStore must be called from a version change transaction.");
+    return;
   }
 
   IndexedDBConnection* connection = transaction_->connection();
@@ -119,6 +120,7 @@ void TransactionImpl::DeleteObjectStore(int64_t object_store_id) {
   if (transaction_->mode() != blink::mojom::IDBTransactionMode::VersionChange) {
     mojo::ReportBadMessage(
         "DeleteObjectStore must be called from a version change transaction.");
+    return;
   }
 
   IndexedDBConnection* connection = transaction_->connection();
