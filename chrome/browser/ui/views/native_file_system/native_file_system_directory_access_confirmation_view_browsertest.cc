e40ace22845a9b3cdeb7671bf446bbfea2bc8bcc
Marijn Kruisselbrink <mek@chromium.org>
2019-07-19 20:28:22 +0000

    [Native File System] Make sure tests are in the correctly named file.
    Somehow I managed to swap these tests/test file names.
    
    Bug: none
    Change-Id: Ic060ca68271f89e8c9438f7ebaf3bb5f9b510c90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1709757
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Auto-Submit: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679239}
    
    Former-commit-id: 9ebe4d421e3dad04ee15074e5150cc297829df83

--- chrome/browser/ui/views/native_file_system/native_file_system_directory_access_confirmation_view_browsertest.cc
+++ chrome/browser/ui/views/native_file_system/native_file_system_directory_access_confirmation_view_browsertest.cc
@@ -2,15 +2,18 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/views/native_file_system/native_file_system_restricted_directory_dialog_view.h"
+#include "chrome/browser/ui/views/native_file_system/native_file_system_directory_access_confirmation_view.h"
 
 #include "base/files/file_path.h"
+#include "base/test/bind_test_util.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
 #include "chrome/grit/generated_resources.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "ui/views/controls/button/label_button.h"
+#include "ui/views/window/dialog_client_view.h"
 
-class NativeFileSystemRestrictedDirectoryDialogViewTest
+class NativeFileSystemDirectoryAccessConfirmationViewTest
     : public DialogBrowserTest {
  public:
   void SetUpOnMainThread() override {
@@ -21,31 +24,73 @@ class NativeFileSystemRestrictedDirectoryDialogViewTest
     // are referenced from the Chrome binary.
     auto& shared_resource_bundle = ui::ResourceBundle::GetSharedInstance();
     shared_resource_bundle.OverrideLocaleStringResource(
-        IDS_NATIVE_FILE_SYSTEM_RESTRICTED_DIRECTORY_TITLE,
-        base::ASCIIToUTF16("Can't save to this folder"));
+        IDS_NATIVE_FILE_SYSTEM_DIRECTORY_ACCESS_CONFIRMATION_TITLE,
+        base::ASCIIToUTF16("Let site read this folder?"));
     shared_resource_bundle.OverrideLocaleStringResource(
-        IDS_NATIVE_FILE_SYSTEM_RESTRICTED_DIRECTORY_TEXT,
-        base::ASCIIToUTF16("$1 can't save your changes to this folder because "
-                           "it contains system files."));
-    shared_resource_bundle.OverrideLocaleStringResource(
-        IDS_NATIVE_FILE_SYSTEM_RESTRICTED_DIRECTORY_BUTTON,
-        base::ASCIIToUTF16("Choose a different folder"));
+        IDS_NATIVE_FILE_SYSTEM_DIRECTORY_ACCESS_CONFIRMATION_TEXT,
+        base::ASCIIToUTF16("$1 will be able to read all files in the following "
+                           "folder. This site can see any changes to the "
+                           "folder only while this tab is open."));
   }
 
   // DialogBrowserTest:
   void ShowUi(const std::string& name) override {
-    NativeFileSystemRestrictedDirectoryDialogView::ShowDialog(
+    widget_ = NativeFileSystemDirectoryAccessConfirmationView::ShowDialog(
         kTestOrigin, base::FilePath(FILE_PATH_LITERAL("/foo/bar")),
-        base::DoNothing(),
+        base::BindLambdaForTesting([&](PermissionAction result) {
+          callback_called_ = true;
+          callback_result_ = result;
+        }),
         browser()->tab_strip_model()->GetActiveWebContents());
   }
 
  protected:
   const url::Origin kTestOrigin =
       url::Origin::Create(GURL("https://example.com"));
+
+  views::Widget* widget_ = nullptr;
+
+  bool callback_called_ = false;
+  PermissionAction callback_result_ = PermissionAction::IGNORED;
 };
 
-IN_PROC_BROWSER_TEST_F(NativeFileSystemRestrictedDirectoryDialogViewTest,
+IN_PROC_BROWSER_TEST_F(NativeFileSystemDirectoryAccessConfirmationViewTest,
+                       AcceptIsntDefaultFocused) {
+  ShowUi(std::string());
+  EXPECT_NE(widget_->client_view()->AsDialogClientView()->ok_button(),
+            widget_->GetFocusManager()->GetFocusedView());
+  widget_->Close();
+  base::RunLoop().RunUntilIdle();
+}
+
+IN_PROC_BROWSER_TEST_F(NativeFileSystemDirectoryAccessConfirmationViewTest,
+                       AcceptRunsCallback) {
+  ShowUi(std::string());
+  widget_->client_view()->AsDialogClientView()->AcceptWindow();
+  EXPECT_TRUE(callback_called_);
+  EXPECT_EQ(PermissionAction::GRANTED, callback_result_);
+  base::RunLoop().RunUntilIdle();
+}
+
+IN_PROC_BROWSER_TEST_F(NativeFileSystemDirectoryAccessConfirmationViewTest,
+                       CancelRunsCallback) {
+  ShowUi(std::string());
+  widget_->client_view()->AsDialogClientView()->CancelWindow();
+  EXPECT_TRUE(callback_called_);
+  EXPECT_EQ(PermissionAction::DISMISSED, callback_result_);
+  base::RunLoop().RunUntilIdle();
+}
+
+IN_PROC_BROWSER_TEST_F(NativeFileSystemDirectoryAccessConfirmationViewTest,
+                       CancelsWhenClosed) {
+  ShowUi(std::string());
+  widget_->Close();
+  EXPECT_TRUE(callback_called_);
+  EXPECT_EQ(PermissionAction::DISMISSED, callback_result_);
+  base::RunLoop().RunUntilIdle();
+}
+
+IN_PROC_BROWSER_TEST_F(NativeFileSystemDirectoryAccessConfirmationViewTest,
                        InvokeUi_default) {
   ShowAndVerifyUi();
 }
