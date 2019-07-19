d43bc7aec93d990d15a1a043b35e51e3ddcf1523
Mehran Mahmoudi <mahmoudi@google.com>
2019-07-19 14:16:23 +0000

    [Touchless] Fix failing test
    TBR=mdjones
    
    Bug: 984004
    Change-Id: Ic26def4fe0c2e429c24ea9030204f8407e3b6d71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1710533
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Commit-Queue: Mehran Mahmoudi <mahmoudi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679088}
    
    Former-commit-id: ada0c8e9c3c7b1ef3d592119688d8e8b9244f69d

--- chrome/android/touchless/javatests/src/org/chromium/chrome/browser/touchless/dialog/TouchlessDialogPresenterTest.java
+++ chrome/android/touchless/javatests/src/org/chromium/chrome/browser/touchless/dialog/TouchlessDialogPresenterTest.java
@@ -25,7 +25,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.touchless.NoTouchActivity;
@@ -58,7 +57,6 @@ public class TouchlessDialogPresenterTest {
     }
 
     @Test
-    @DisabledTest(message = "crbug.com/984004")
     @SmallTest
     @Feature({"TouchlessModalDialog"})
     public void testItemSelection() throws Exception {
@@ -69,16 +67,14 @@ public class TouchlessDialogPresenterTest {
                 new View.OnClickListener[] {
                         (v) -> item1Callback.notifyCalled(), (v) -> item2Callback.notifyCalled()});
 
-        // Enable 'keyboard mode' by sending a key press event.
-        // Doing so, the first item will be focused when the dialog appears.
-        mUiDevice.pressDPadDown();
         showDialog(mManager, dialog);
         mUiDevice.wait(Until.findObject(By.text("dialog")), TIMEOUT_MS);
 
         UiObject2 firstItem = mUiDevice.findObject(By.text("1")).getParent();
         UiObject2 secondItem = mUiDevice.findObject(By.text("2")).getParent();
 
-        Assert.assertTrue("First item is not selected", firstItem.isFocused());
+        mUiDevice.pressDPadDown();
+        firstItem.wait(Until.focused(true), TIMEOUT_MS);
         Assert.assertFalse("Second item is selected", secondItem.isFocused());
 
         Assert.assertEquals(0, item1Callback.getCallCount());
