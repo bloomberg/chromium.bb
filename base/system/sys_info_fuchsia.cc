503b08927e228d889d4a167449f51c2cf3d0a39e
arun manickam <arunmanickam@google.com>
2019-07-19 02:42:35 +0000

    [Fuchsia] Stop using the POSIX SysInfo implementation.
    Adding Fuchsia specific SysInfo APIs and removing OS_FUCHSIA conditions from POXIS SysInfo Implementation (Code Clean-up)
    
    Bug: b/136176387
    
    Test: base_unittests
    
    Change-Id: I90a431bae3fe814d1aa7f14071a0ac68a1dce8f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1693741
    Commit-Queue: Arun Manickam <arunmanickam@google.com>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#678958}
    
    Former-commit-id: 731c16df697e02c871d334176a3ff498216617ea

--- base/system/sys_info_fuchsia.cc
+++ base/system/sys_info_fuchsia.cc
@@ -4,9 +4,11 @@
 
 #include "base/system/sys_info.h"
 
+#include <sys/utsname.h>
 #include <zircon/syscalls.h>
 
 #include "base/logging.h"
+#include "base/threading/scoped_blocking_call.h"
 
 namespace base {
 
@@ -17,7 +19,8 @@ int64_t SysInfo::AmountOfPhysicalMemoryImpl() {
 
 // static
 int64_t SysInfo::AmountOfAvailablePhysicalMemoryImpl() {
-  // TODO(fuchsia): https://crbug.com/706592 This is not exposed.
+  // TODO(https://crbug.com/706592): This method doesn't have an Fuchsia API to
+  // consume it
   NOTREACHED();
   return 0;
 }
@@ -35,6 +38,70 @@ int64_t SysInfo::AmountOfVirtualMemory() {
 // static
 std::string SysInfo::OperatingSystemName() {
   return "Fuchsia";
+}
+
+// static
+int64_t SysInfo::AmountOfFreeDiskSpace(const FilePath& path) {
+  base::ScopedBlockingCall scoped_blocking_call(FROM_HERE,
+                                                base::BlockingType::MAY_BLOCK);
+  NOTIMPLEMENTED_LOG_ONCE();
+  return -1;
+}
+
+// static
+int64_t SysInfo::AmountOfTotalDiskSpace(const FilePath& path) {
+  base::ScopedBlockingCall scoped_blocking_call(FROM_HERE,
+                                                base::BlockingType::MAY_BLOCK);
+  NOTIMPLEMENTED_LOG_ONCE();
+  return -1;
+}
+
+// static
+std::string SysInfo::OperatingSystemVersion() {
+  struct utsname info;
+  if (uname(&info) < 0) {
+    NOTREACHED();
+    return std::string();
+  }
+
+  return std::string(info.release);
+}
+
+// static
+void SysInfo::OperatingSystemVersionNumbers(int32_t* major_version,
+                                            int32_t* minor_version,
+                                            int32_t* bugfix_version) {
+  struct utsname info;
+  if (uname(&info) < 0) {
+    *major_version = 0;
+    *minor_version = 0;
+    *bugfix_version = 0;
+    return;
+  }
+  int num_read = sscanf(info.release, "%d.%d.%d", major_version, minor_version,
+                        bugfix_version);
+  if (num_read < 1)
+    *major_version = 0;
+  if (num_read < 2)
+    *minor_version = 0;
+  if (num_read < 3)
+    *bugfix_version = 0;
+}
+
+// static
+std::string SysInfo::OperatingSystemArchitecture() {
+  struct utsname info;
+  if (uname(&info) < 0) {
+    NOTREACHED();
+    return std::string();
+  }
+
+  return info.machine;
+}
+
+// static
+size_t SysInfo::VMAllocationGranularity() {
+  return getpagesize();
 }
 
 }  // namespace base
