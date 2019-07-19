40299932cf546b1ad5abcf012562fbb08d597d73
Ivana Zuzic <izuzic@google.com>
2019-07-19 12:14:39 +0000

    [PWD Editing Android] Create a layout for editing passwords
    A layout similar to the one for Addresses and Payment methods is added
    for Passwords. It contains fields for site, username and password. The
    fields are not populated with data yet.
    
    Bug: 377410
    Change-Id: If3b0157766f9049e08e0019503a5fa6f0faf43c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1707362
    Commit-Queue: Ivana Zuzic <izuzic@google.com>
    Reviewed-by: Theresa <twellington@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679059}
    
    Former-commit-id: 4fd45d20c4521ea1cd1358ab761d21702ef76e34

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/password/PasswordEntryEditor.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/password/PasswordEntryEditor.java
@@ -1,14 +1,29 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 package org.chromium.chrome.browser.preferences.password;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.chromium.chrome.R;
 
 /**
  * Password entry editor that allows editing passwords stored in Chrome.
  */
 public class PasswordEntryEditor extends Fragment {
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.password_entry_editor, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
