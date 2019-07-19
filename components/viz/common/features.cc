81b01787693766a928e28fed9c9f4c0e384d1ab1
kylechar <kylechar@chromium.org>
2019-07-19 21:50:58 +0000

    Enable VizDisplayCompositor on CrOS.
    1. Enable feature on CrOS. We are preparing for an M77 launch. Rollout
       will still be finch controlled but we need to enable the feature in
       code for Chrome OS infra to start running with it.
    2. Flip viz_* tests to be non_viz_* since tests will now run with
       VizDisplayCompositor enabled by default.
    3. Remove viz_content_unittests from FYI bots since the same test is
       already in CQ.
    
    Note that the VizDisplayCompositor feature shifts a significant amount
    of work from the browser to GPU process, which has both memory and CPU
    time implications. Once this change is picked up in Chrome OS and hits
    the perf bots it will likely trigger numerous alerts. Please assign bugs
    to kylechar@chromium.org so I can investigate if it's a real regression
    or just shifting work between processes.
    
    Bug: 732805
    Change-Id: I628aba5efcb0d1c8499753695e4c915aa5344044
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1708415
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679283}
    
    Former-commit-id: bd5d5fe84c358e1edbfeab03dbb475423d34d29c

--- components/viz/common/features.cc
+++ components/viz/common/features.cc
@@ -22,7 +22,7 @@ const base::Feature kEnableSurfaceSynchronization{
 // (OOP-D).
 // TODO(dnicoara): Look at enabling Chromecast support when ChromeOS support is
 // ready.
-#if defined(OS_CHROMEOS) || defined(IS_CHROMECAST)
+#if defined(IS_CHROMECAST)
 const base::Feature kVizDisplayCompositor{"VizDisplayCompositor",
                                           base::FEATURE_DISABLED_BY_DEFAULT};
 #else
