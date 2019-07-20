9b0d5b48c1f7e3a70bac3640fc38d6476ce1a0fc
Gayane Petrosyan <gayane@chromium.org>
2019-07-20 04:01:04 +0000

    [Chrome Colors] Change the templates and sizing.
    Following changes in this CL:
    - Change the template to have an outline
    - Change default theme icons,
    - Change sizing of the color tiles
    - Use new check mark svg wihtout gaps in the sides(the prev one has 2px
      gap from sides that makes it complicated to position).
    
    Screenshot:
    https://screenshot.googleplex.com/74DKQbwTcJ9.png
    https://screenshot.googleplex.com/dop0K3LBfnw.png
    
    Bug: 960982
    Change-Id: I10242c7b8381748cc2a8901bd3a456efcb9049ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1709839
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#679362}
    
    Former-commit-id: 1ebff4c0cbc64d4f120e5ef648c9261ad71fc655

--- chrome/common/search/chrome_colors_icon_template.h
+++ chrome/common/search/chrome_colors_icon_template.h
@@ -9,20 +9,15 @@
 // $1 - primary color
 // $2 - secondary color
 const char kChromeColorsIconTemplate[] =
-    "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><!DOCTYPE svg "
-    "PUBLIC \"-//W3C//DTD SVG 1.1//EN\" "
-    "\"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\"><svg version=\"1.1\" "
-    "xmlns=\"http://www.w3.org/2000/svg\" "
-    "xmlns:xlink=\"http://www.w3.org/1999/xlink\" "
-    "preserveAspectRatio=\"xMidYMid meet\" viewBox=\"0 0 80 80\" width=\"80\" "
-    "height=\"80\"><defs><path d=\"M80 40C80 62.08 62.07 80 40 80C17.92 80 0 "
-    "62.08 0 40C0 17.93 17.92 0 40 0C62.07 0 80 17.93 80 40Z\" "
-    "id=\"a2GlMRZYm7\"></path><linearGradient id=\"gradientb48881kEO\" "
-    "gradientUnits=\"userSpaceOnUse\" x1=\"40\" y1=\"40\" x2=\"40.1\" "
-    "y2=\"40\"><stop style=\"stop-color: $1;stop-opacity: 1\" "
-    "offset=\"0\%\"></stop><stop style=\"stop-color: $2;stop-opacity: 1\" "
-    "offset=\"100\%\"></stop></linearGradient></defs><g><use "
-    "xlink:href=\"#a2GlMRZYm7\" opacity=\"1\" "
-    "fill=\"url(#gradientb48881kEO)\"></use></g></svg>";
+    "<svg xmlns=\"http://www.w3.org/2000/svg\" "
+    "xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"64\" "
+    "height=\"64\"><defs><path d=\"M32 64C14.34 64 0 49.66 0 32S14.34 0 32 "
+    "0s32 14.34 32 32-14.34 32-32 32z\" id=\"a\"/><linearGradient id=\"b\" "
+    "gradientUnits=\"userSpaceOnUse\" x1=\"32\" y1=\"32\" x2=\"32.08\" "
+    "y2=\"32\"><stop offset=\"0\%\" stop-color=\"$2\"/><stop offset=\"100\%\" "
+    "stop-color=\"$1\"/></linearGradient><clipPath id=\"c\"><use "
+    "xlink:href=\"#a\"/></clipPath></defs><use xlink:href=\"#a\" "
+    "fill=\"url(#b)\"/><g clip-path=\"url(#c)\"><use xlink:href=\"#a\" "
+    "fill-opacity=\"0\" stroke=\"$1\" stroke-width=\"2\"/></g></svg>";
 
 #endif  // CHROME_COMMON_SEARCH_CHROME_COLORS_ICON_TEMPLATE_H_
