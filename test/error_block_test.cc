58f2feb0b1c5bb24ddfe45aa65780cdbf01e77de
Jerome Jiang <jianj@google.com>
2019-07-22 16:29:51 -0700

    Add sse2 for av1_block_error and tests.
    Change-Id: Ide1cfa5c70d83071d72d0910abeaf7b5d565aa50

--- test/error_block_test.cc
+++ test/error_block_test.cc
@@ -234,7 +234,7 @@ TEST_P(ErrorBlockTest, DISABLED_Speed) {
 
 using ::testing::make_tuple;
 
-#if (HAVE_SSE2 || HAVE_AVX)
+#if (HAVE_SSE2)
 INSTANTIATE_TEST_CASE_P(
     SSE2, ErrorBlockTest,
     ::testing::Values(make_tuple(&av1_highbd_block_error_sse2,
@@ -242,7 +242,10 @@ INSTANTIATE_TEST_CASE_P(
                       make_tuple(&av1_highbd_block_error_sse2,
                                  &av1_highbd_block_error_c, AOM_BITS_12),
                       make_tuple(&av1_highbd_block_error_sse2,
-                                 &av1_highbd_block_error_c, AOM_BITS_8)));
+                                 &av1_highbd_block_error_c, AOM_BITS_8),
+                      make_tuple(&BlockError8BitWrapper<av1_block_error_sse2>,
+                                 &BlockError8BitWrapper<av1_block_error_c>,
+                                 AOM_BITS_8)));
 #endif  // HAVE_SSE2
 
 #if (HAVE_AVX2)
