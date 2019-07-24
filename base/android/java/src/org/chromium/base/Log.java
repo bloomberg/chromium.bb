3cfbd8bff32f07460dcd55a8d6ca625730a98d80
Andrew Grieve <agrieve@google.com>
2019-07-24 03:04:47 +0000

    Reland "ProGuard: Move -checkdiscard for Log.* into @RemovableInRelease annotation"
    Was reverted in: 563e9c4539c65f65ec3d531f38051e4e8923e86b [formerly 54f75614d20f23f5ff5da505343f28a82323d654].
    
    Reason for reland: Includes fix for downstream targets.
    
    Change-Id: I2a5f77cdbd40934ae206dd91b2e12e7b5e6553e1
    Bug: 986693, 986693
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715004
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680307}
    
    Former-commit-id: 885aa0205473882798718ff3951146620bcd3e24

--- base/android/java/src/org/chromium/base/Log.java
+++ base/android/java/src/org/chromium/base/Log.java
@@ -87,6 +87,7 @@ public class Log {
      * Note: Has no effect on whether logs are sent or not. Use a method with
      * {@link RemovableInRelease} to log something in Debug builds only.
      */
+    @RemovableInRelease
     public static boolean isLoggable(String tag, int level) {
         return android.util.Log.isLoggable(tag, level);
     }
@@ -105,6 +106,7 @@ public class Log {
      * @param args Arguments referenced by the format specifiers in the format string. If the last
      *             one is a {@link Throwable}, its trace will be printed.
      */
+    @RemovableInRelease
     private static void verbose(String tag, String messageTemplate, Object... args) {
         String message = formatLogWithStack(messageTemplate, args);
         Throwable tr = getThrowableToLog(args);
@@ -190,6 +192,7 @@ public class Log {
      * @param args Arguments referenced by the format specifiers in the format string. If the last
      *             one is a {@link Throwable}, its trace will be printed.
      */
+    @RemovableInRelease
     private static void debug(String tag, String messageTemplate, Object... args) {
         String message = formatLogWithStack(messageTemplate, args);
         Throwable tr = getThrowableToLog(args);
@@ -362,6 +365,7 @@ public class Log {
     }
 
     /** Returns a string form of the origin of the log call, to be used as secondary tag.*/
+    @RemovableInRelease
     private static String getCallOrigin() {
         StackTraceElement[] st = Thread.currentThread().getStackTrace();
 
