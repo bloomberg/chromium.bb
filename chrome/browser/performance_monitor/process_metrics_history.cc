25516c03350f45f411dfce5ce6f1088e088e2b41
Leonard Grey <lgrey@chromium.org>
2019-07-24 15:32:35 +0000

    Revert "Mac: Record process “energy impact” to UMA"
    This reverts commit 1a97e5c7e661a23da68b92ae67e93471892f1f0d [formerly 9bc33932c9ba1b8446d8f9df85241254e2803edb].
    
    Reason for revert: https://crbug.com/987044
    
    Original change's description:
    > Mac: Record process “energy impact” to UMA
    > 
    > This is a synthetic estimate displayed to users in Activity Monitor, the battery menu, and top.
    > More info at https://blog.mozilla.org/nnethercote/2015/08/26/what-does-the-os-x-activity-monitors-energy-impact-actually-measure/
    > 
    > Bug: 969105
    > Change-Id: Ie3438307226c3f9a46459948ea88efb6479de0c8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1707472
    > Commit-Queue: Leonard Grey <lgrey@chromium.org>
    > Reviewed-by: Mark Mentovai <mark@chromium.org>
    > Reviewed-by: Ilya Sherman <isherman@chromium.org>
    > Reviewed-by: oysteine <oysteine@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#679557}
    
    TBR=isherman@chromium.org,oysteine@chromium.org,lgrey@chromium.org,mark@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 969105
    Change-Id: Iec95be777c3816183dc92fd87797bbb0cc890ef6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715509
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680436}
    
    Former-commit-id: 40a955a540960a9e14965db7b7b2faa99b71d08f

--- chrome/browser/performance_monitor/process_metrics_history.cc
+++ chrome/browser/performance_monitor/process_metrics_history.cc
@@ -52,7 +52,6 @@ void ProcessMetricsHistory::SampleMetrics() {
 #endif
 #if defined(OS_MACOSX)
   package_idle_wakeups_ = process_metrics_->GetPackageIdleWakeupsPerSecond();
-  energy_impact_ = process_metrics_->GetEnergyImpact();
 #endif
 }
 
@@ -96,9 +95,6 @@ void ProcessMetricsHistory::RunPerformanceTriggers() {
       UMA_HISTOGRAM_COUNTS_1000(
           "PerformanceMonitor.PackageExitIdleWakeups.BrowserProcess",
           package_idle_wakeups_);
-      UMA_HISTOGRAM_COUNTS_100000(
-          "PerformanceMonitor.EnergyImpact.BrowserProcess", energy_impact_);
-
 #endif
       break;
     case content::PROCESS_TYPE_RENDERER:
@@ -117,9 +113,6 @@ void ProcessMetricsHistory::RunPerformanceTriggers() {
       UMA_HISTOGRAM_COUNTS_1000(
           "PerformanceMonitor.PackageExitIdleWakeups.RendererProcess",
           package_idle_wakeups_);
-      UMA_HISTOGRAM_COUNTS_100000(
-          "PerformanceMonitor.EnergyImpact.RendererProcess", energy_impact_);
-
 #endif
 
       break;
@@ -137,9 +130,6 @@ void ProcessMetricsHistory::RunPerformanceTriggers() {
       UMA_HISTOGRAM_COUNTS_1000(
           "PerformanceMonitor.PackageExitIdleWakeups.GPUProcess",
           package_idle_wakeups_);
-      UMA_HISTOGRAM_COUNTS_100000("PerformanceMonitor.EnergyImpact.GPUProcess",
-                                  energy_impact_);
-
 #endif
 
       break;
