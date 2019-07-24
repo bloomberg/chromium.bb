614a888314edad0d5c0f9125c3e089cf7c27a7e7
Balazs Engedy <engedy@chromium.org>
2019-07-24 14:09:29 +0000

    Deprecate histogram ProfileReset.SendFeedback.
    Bug: 975621
    Change-Id: Id4431de0fc8400ee2c2e7a6350631deede2dc332
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1714592
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Auto-Submit: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680413}
    
    Former-commit-id: 791a48b8b8977b7bafdea9459ae65f3847790b7b

--- chrome/browser/ui/webui/settings/reset_settings_handler.cc
+++ chrome/browser/ui/webui/settings/reset_settings_handler.cc
@@ -268,7 +268,6 @@ void ResetSettingsHandler::ResetProfile(
                  callback_weak_ptr_factory_.GetWeakPtr(), callback_id,
                  send_settings, request_origin));
   base::RecordAction(base::UserMetricsAction("ResetProfile"));
-  UMA_HISTOGRAM_BOOLEAN("ProfileReset.SendFeedback", send_settings);
   UMA_HISTOGRAM_ENUMERATION(
       "ProfileReset.ResetRequestOrigin", request_origin,
       reset_report::ChromeResetReport::ResetRequestOrigin_MAX + 1);
