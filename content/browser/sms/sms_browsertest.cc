2c69cbba318a59652c2924ca854534e551695273
Sam Goto <goto@chromium.org>
2019-07-25 01:08:02 +0000

    [sms] Assert behavior when reloading, closing and opening multiple tabs
    To protect ourselves against regressions, we introduce tests that
    assert the expected behavior when users interact with the browser.
    
    Bug: 969801
    Change-Id: I3988996acd80bf1d577c90461ce7e1b401e3a3df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1678282
    Commit-Queue: Sam Goto <goto@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680691}
    
    Former-commit-id: b53c430b82d82dd4c826c6ba8bbea706217632bf

--- content/browser/sms/sms_browsertest.cc
+++ content/browser/sms/sms_browsertest.cc
@@ -12,7 +12,10 @@
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/content_browser_test.h"
 #include "content/public/test/content_browser_test_utils.h"
+#include "content/public/test/content_mock_cert_verifier.h"
 #include "content/shell/browser/shell.h"
+#include "net/cert/mock_cert_verifier.h"
+#include "net/dns/mock_host_resolver.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
 using ::testing::_;
@@ -72,9 +75,27 @@ class SmsBrowserTest : public ContentBrowserTest {
     command_line->AppendSwitchASCII("enable-blink-features", "SmsReceiver");
     command_line->AppendSwitch(
         switches::kEnableExperimentalWebPlatformFeatures);
+    cert_verifier_.SetUpCommandLine(command_line);
+  }
+
+ protected:
+  void SetUpOnMainThread() override {
+    host_resolver()->AddRule("*", "127.0.0.1");
+    cert_verifier_.mock_cert_verifier()->set_default_result(net::OK);
+  }
+
+  void SetUpInProcessBrowserTestFixture() override {
+    cert_verifier_.SetUpInProcessBrowserTestFixture();
+  }
+
+  void TearDownInProcessBrowserTestFixture() override {
+    cert_verifier_.TearDownInProcessBrowserTestFixture();
   }
 
   NiceMock<MockWebContentsDelegate> delegate_;
+  // Similar to net::MockCertVerifier, but also updates the CertVerifier
+  // used by the NetworkService.
+  ContentMockCertVerifier cert_verifier_;
 };
 
 }  // namespace
@@ -121,6 +142,8 @@ IN_PROC_BROWSER_TEST_F(SmsBrowserTest, Receive) {
   }));
 
   EXPECT_EQ("hello", EvalJs(shell(), script));
+
+  ASSERT_FALSE(provider->HasObservers());
 }
 
 IN_PROC_BROWSER_TEST_F(SmsBrowserTest, ReceiveMultiple) {
@@ -191,6 +214,250 @@ IN_PROC_BROWSER_TEST_F(SmsBrowserTest, ReceiveMultiple) {
   ASSERT_EQ(2u, result.GetList().size());
   EXPECT_EQ("hello1", result.GetList()[0].GetString());
   EXPECT_EQ("hello2", result.GetList()[1].GetString());
+
+  ASSERT_FALSE(provider->HasObservers());
+}
+
+IN_PROC_BROWSER_TEST_F(SmsBrowserTest, Reload) {
+  GURL url = GetTestUrl(nullptr, "simple_page.html");
+  NavigateToURL(shell(), url);
+
+  auto* provider = new NiceMock<MockSmsProvider>();
+  BrowserMainLoop::GetInstance()->SetSmsProviderForTesting(
+      base::WrapUnique(provider));
+
+  std::string script = R"(
+    // kicks off the sms receiver, adding the service
+    // to the observer's list.
+    navigator.sms.receive({timeout: 60});
+    true
+  )";
+
+  base::RunLoop loop;
+
+  EXPECT_CALL(*provider, Retrieve()).WillOnce(Invoke([&loop]() {
+    // deliberately avoid calling NotifyReceive() to simulate
+    // a request that has been received but not fulfilled.
+    loop.Quit();
+  }));
+
+  EXPECT_EQ(true, EvalJs(shell(), script));
+
+  loop.Run();
+
+  ASSERT_TRUE(provider->HasObservers());
+
+  // reload the page.
+  NavigateToURL(shell(), url);
+
+  ASSERT_FALSE(provider->HasObservers());
+}
+
+IN_PROC_BROWSER_TEST_F(SmsBrowserTest, Close) {
+  GURL url = GetTestUrl(nullptr, "simple_page.html");
+  NavigateToURL(shell(), url);
+
+  auto* provider = new NiceMock<MockSmsProvider>();
+  BrowserMainLoop::GetInstance()->SetSmsProviderForTesting(
+      base::WrapUnique(provider));
+
+  std::string script = R"(
+    navigator.sms.receive({timeout: 60});
+    true
+  )";
+
+  base::RunLoop loop;
+
+  EXPECT_CALL(*provider, Retrieve()).WillOnce(Invoke([&loop]() {
+    loop.Quit();
+  }));
+
+  EXPECT_EQ(true, EvalJs(shell(), script));
+
+  loop.Run();
+
+  ASSERT_TRUE(provider->HasObservers());
+
+  shell()->Close();
+
+  ASSERT_FALSE(provider->HasObservers());
+}
+
+IN_PROC_BROWSER_TEST_F(SmsBrowserTest, TwoTabsSameOrigin) {
+  auto* provider = new NiceMock<MockSmsProvider>();
+  BrowserMainLoop::GetInstance()->SetSmsProviderForTesting(
+      base::WrapUnique(provider));
+
+  Shell* tab1 = CreateBrowser();
+  Shell* tab2 = CreateBrowser();
+
+  GURL url = GetTestUrl(nullptr, "simple_page.html");
+
+  NavigateToURL(tab1, url);
+  NavigateToURL(tab2, url);
+
+  std::string script = R"(
+    navigator.sms.receive({timeout: 60}).then(({content}) => {
+      sms = content;
+    });
+    true
+  )";
+
+  base::OnceClosure on_continue1, on_continue2;
+
+  {
+    base::RunLoop loop;
+
+    auto* dialog = new NiceMock<MockSmsDialog>();
+
+    tab1->web_contents()->SetDelegate(&delegate_);
+
+    EXPECT_CALL(delegate_, CreateSmsDialog())
+        .WillOnce(Return(ByMove(base::WrapUnique(dialog))));
+
+    EXPECT_CALL(*provider, Retrieve()).WillOnce(Invoke([&loop]() {
+      loop.Quit();
+    }));
+
+    EXPECT_CALL(*dialog, Open(_, _, _))
+        .WillOnce(Invoke([&on_continue1](content::RenderFrameHost*,
+                                         base::OnceClosure on_continue,
+                                         base::OnceClosure on_cancel) {
+          on_continue1 = std::move(on_continue);
+        }));
+
+    // First tab registers an observer.
+    EXPECT_EQ(true, EvalJs(tab1, script));
+
+    loop.Run();
+  }
+
+  {
+    base::RunLoop loop;
+
+    auto* dialog = new NiceMock<MockSmsDialog>();
+
+    tab2->web_contents()->SetDelegate(&delegate_);
+
+    EXPECT_CALL(delegate_, CreateSmsDialog())
+        .WillOnce(Return(ByMove(base::WrapUnique(dialog))));
+
+    EXPECT_CALL(*provider, Retrieve()).WillOnce(Invoke([&loop]() {
+      loop.Quit();
+    }));
+
+    EXPECT_CALL(*dialog, Open(_, _, _))
+        .WillOnce(Invoke([&on_continue2](content::RenderFrameHost*,
+                                         base::OnceClosure on_continue,
+                                         base::OnceClosure on_cancel) {
+          on_continue2 = std::move(on_continue);
+        }));
+
+    // Second tab registers an observer.
+    EXPECT_EQ(true, EvalJs(tab2, script));
+
+    loop.Run();
+  }
+
+  ASSERT_TRUE(provider->HasObservers());
+
+  provider->NotifyReceive(url::Origin::Create(url), "hello1");
+
+  std::move(on_continue1).Run();
+
+  EXPECT_EQ("hello1", EvalJs(tab1, "sms"));
+
+  ASSERT_TRUE(provider->HasObservers());
+
+  provider->NotifyReceive(url::Origin::Create(url), "hello2");
+
+  std::move(on_continue2).Run();
+
+  EXPECT_EQ("hello2", EvalJs(tab2, "sms"));
+
+  ASSERT_FALSE(provider->HasObservers());
+}
+
+IN_PROC_BROWSER_TEST_F(SmsBrowserTest, TwoTabsDifferentOrigin) {
+  auto* provider = new NiceMock<MockSmsProvider>();
+  BrowserMainLoop::GetInstance()->SetSmsProviderForTesting(
+      base::WrapUnique(provider));
+
+  Shell* tab1 = CreateBrowser();
+  Shell* tab2 = CreateBrowser();
+
+  net::EmbeddedTestServer https_server(net::EmbeddedTestServer::TYPE_HTTPS);
+  https_server.ServeFilesFromSourceDirectory(GetTestDataFilePath());
+  ASSERT_TRUE(https_server.Start());
+
+  GURL url1 = https_server.GetURL("a.com", "/simple_page.html");
+  GURL url2 = https_server.GetURL("b.com", "/simple_page.html");
+
+  NavigateToURL(tab1, url1);
+  NavigateToURL(tab2, url2);
+
+  std::string script = R"(
+    navigator.sms.receive({timeout: 60}).then(({content}) => {
+      sms = content;
+    });
+    true;
+  )";
+
+  base::RunLoop loop;
+
+  EXPECT_CALL(*provider, Retrieve())
+      .WillOnce(testing::Return())
+      .WillOnce(Invoke([&loop]() { loop.Quit(); }));
+
+  tab1->web_contents()->SetDelegate(&delegate_);
+  tab2->web_contents()->SetDelegate(&delegate_);
+
+  auto* dialog1 = new NiceMock<MockSmsDialog>();
+  auto* dialog2 = new NiceMock<MockSmsDialog>();
+
+  base::OnceClosure on_continue_callback1;
+  base::OnceClosure on_continue_callback2;
+
+  EXPECT_CALL(delegate_, CreateSmsDialog())
+      .WillOnce(Return(ByMove(base::WrapUnique(dialog1))))
+      .WillOnce(Return(ByMove(base::WrapUnique(dialog2))));
+
+  EXPECT_CALL(*dialog1, Open(_, _, _))
+      .WillOnce(Invoke([&on_continue_callback1](content::RenderFrameHost*,
+                                                base::OnceClosure on_continue,
+                                                base::OnceClosure on_cancel) {
+        on_continue_callback1 = std::move(on_continue);
+      }));
+
+  EXPECT_CALL(*dialog2, Open(_, _, _))
+      .WillOnce(Invoke([&on_continue_callback2](content::RenderFrameHost*,
+                                                base::OnceClosure on_continue,
+                                                base::OnceClosure on_cancel) {
+        on_continue_callback2 = std::move(on_continue);
+      }));
+
+  EXPECT_EQ(true, EvalJs(tab1, script));
+  EXPECT_EQ(true, EvalJs(tab2, script));
+
+  loop.Run();
+
+  ASSERT_TRUE(provider->HasObservers());
+
+  provider->NotifyReceive(url::Origin::Create(url1), "hello1");
+
+  std::move(on_continue_callback1).Run();
+
+  EXPECT_EQ("hello1", EvalJs(tab1, "sms"));
+
+  ASSERT_TRUE(provider->HasObservers());
+
+  provider->NotifyReceive(url::Origin::Create(url2), "hello2");
+
+  std::move(on_continue_callback2).Run();
+
+  EXPECT_EQ("hello2", EvalJs(tab2, "sms"));
+
+  ASSERT_FALSE(provider->HasObservers());
 }
 
 IN_PROC_BROWSER_TEST_F(SmsBrowserTest, SmsReceivedAfterTabIsClosed) {
