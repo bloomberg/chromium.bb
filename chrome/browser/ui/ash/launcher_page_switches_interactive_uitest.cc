41b128defb8ce34948c0ecafc13a19b38c6e030c
Jun Mukai <mukai@chromium.org>
2019-07-25 18:59:14 +0000

    Add LauncherPageDragTest
    This new test case checks the presentation time of dragging the
    app-list item grid.
    
    This histogram data would be important for the smoothness of
    dragging and I hope it would be helpful to further investigate
    the bug reports like crbug.com/987311.
    
    Right now, however, it only tests clamshell mode, since
    the same test scenario is very flaky in tablet mode; I'll check
    this later.
    
    The result for 10 runs on ToT (r680552) on nocturne is:
    Apps.PaginationTransition.DragScroll.PresentationTime.ClamshellMode:
      56.8654, 51.3962, 53.6226, 55.6154, 52.6792, 59.9636, 55.537, 57.6792,
      56.1321, 58.283
    Apps.PaginationTransition.DragScroll.PresentationTime.MaxLatency.ClamshellMode:
        77, 56, 78, 72, 72, 77, 75, 77, 74, 78
    
    Bug: 987311
    Test: the new test case covers
    Change-Id: Ib0b257efa5b7e4c87a49da0c5f1a75eb9cd867d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1717585
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#680930}
    
    Former-commit-id: 10c212a2ae4cce7e071b95bcc0e742b8d46ba66d

--- chrome/browser/ui/ash/launcher_page_switches_interactive_uitest.cc
+++ chrome/browser/ui/ash/launcher_page_switches_interactive_uitest.cc
@@ -12,8 +12,11 @@
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/test/base/perf/drag_event_generator.h"
 #include "chrome/test/base/perf/performance_test.h"
 #include "ui/base/test/ui_controls.h"
+#include "ui/display/display.h"
+#include "ui/display/screen.h"
 
 namespace {
 
@@ -116,3 +119,74 @@ IN_PROC_BROWSER_TEST_P(LauncherPageSwitchesTest, SwitchToFarPage) {
 INSTANTIATE_TEST_SUITE_P(,
                          LauncherPageSwitchesTest,
                          /*tablet_mode=*/::testing::Bool());
+
+// The test gets very flaky in tablet-mode, so it's in clamshell mode only for
+// now.
+// TODO(mukai): investigate why and enable this test case with tablet-mode too.
+class LauncherPageDragTest : public UIPerformanceTest {
+ public:
+  LauncherPageDragTest() = default;
+  ~LauncherPageDragTest() override = default;
+
+  // UIPerformanceTest:
+  void SetUpOnMainThread() override {
+    UIPerformanceTest::SetUpOnMainThread();
+
+    test::PopulateDummyAppListItems(100);
+
+    ash::ShellTestApi shell_test_api;
+
+    // Open the fullscreen app; required for page switching.
+    BrowserView* browser_view =
+        BrowserView::GetBrowserViewForBrowser(browser());
+    aura::Window* browser_window = browser_view->GetWidget()->GetNativeWindow();
+    ui_controls::SendKeyPress(browser_window, ui::VKEY_BROWSER_SEARCH,
+                              /*control=*/false,
+                              /*shift=*/true,
+                              /*alt=*/false,
+                              /* command = */ false);
+    shell_test_api.WaitForLauncherAnimationState(
+        ash::AppListViewState::kFullscreenAllApps);
+
+    if (base::SysInfo::IsRunningOnChromeOS()) {
+      base::RunLoop run_loop;
+      base::PostDelayedTask(FROM_HERE, run_loop.QuitClosure(),
+                            base::TimeDelta::FromSeconds(5));
+      run_loop.Run();
+    }
+  }
+
+  // UIPerformanceTest:
+  std::vector<std::string> GetUMAHistogramNames() const override {
+    return {
+        "Apps.PaginationTransition.DragScroll.PresentationTime.ClamshellMode",
+        "Apps.PaginationTransition.DragScroll.PresentationTime.MaxLatency."
+        "ClamshellMode",
+    };
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(LauncherPageDragTest);
+};
+
+IN_PROC_BROWSER_TEST_F(LauncherPageDragTest, Run) {
+  ash::ShellTestApi shell_test_api;
+
+  gfx::Rect display_bounds =
+      display::Screen::GetScreen()
+          ->GetDisplayNearestWindow(browser()->window()->GetNativeWindow())
+          .bounds();
+  gfx::Point start_point = display_bounds.CenterPoint();
+  gfx::Point end_point(start_point);
+  end_point.set_y(10);
+  ui_test_utils::DragEventGenerator generator(
+      std::make_unique<ui_test_utils::InterpolatedProducer>(
+          start_point, end_point, base::TimeDelta::FromMilliseconds(1000)),
+      /*touch=*/true);
+
+  ash::PaginationModel* model = ash::ShellTestApi().GetAppListPaginationModel();
+  ASSERT_TRUE(model);
+  PageSwitchWaiter waiter(model);
+  generator.Wait();
+  waiter.Wait();
+}
