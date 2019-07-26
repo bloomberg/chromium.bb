22762d94d75846cef988f8b96da221ccfb571165
Jason D. Clinton <jclinton@chromium.org>
2019-07-26 09:31:06 -0600

    Mark mistral experimental
    BUG=chromium:987989
    TEST=./run_tests
    
    Change-Id: Iaa6b8f193650b58bafbc40e97a1bf2c105512567
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/1720899
    Tested-by: Jason Clinton <jclinton@chromium.org>
    Auto-Submit: Jason Clinton <jclinton@chromium.org>
    Reviewed-by: Mike Nichols <mikenichols@chromium.org>
    Commit-Queue: Mike Nichols <mikenichols@chromium.org>

--- config/chromeos_config.py
+++ config/chromeos_config.py
@@ -2249,7 +2249,6 @@ def CqBuilders(site_config, boards_dict, ge_build_config):
       'lakitu-st',
       'lasilla-ground',
       'littlejoe',
-      'mistral',
       'monroe',
       'nami',
       'nocturne',
@@ -2315,6 +2314,7 @@ def CqBuilders(site_config, boards_dict, ge_build_config):
   # Paladin configs that exist and should stay as experimental until further
   # notice, preferably with a comment indicating why and a bug.
   _paladin_experimental_boards = _paladin_new_boards | frozenset([
+      'mistral', # crbug.com/987989
       'moblab-generic-vm', # crbug.com/960998, crbug.com/976297
       'betty', # crbug.com/984316
       'betty-pi-arc', # Promote when we replace betty-arcnext. b/129410042
