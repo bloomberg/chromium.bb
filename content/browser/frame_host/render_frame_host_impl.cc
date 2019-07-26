6c74f31a919cf79878f5dd0506013bf75a26cc99
Mario Sanchez Prada <mario@igalia.com>
2019-07-26 11:34:08 +0000

    Revert "Move implementation of Mojo method GetTextSurroundingSelection() to Blink"
    This reverts commit d11522291a6ab871ea7a78a603ae4e6b08e716d9 [formerly 506aea9166170a6ecb7ab5ecbf30b21626d5e14b].
    
    Reason for revert: It's possible that this patch has caused the renderer
    process to crash on Android so let's revert it for now and I will investigate
    a better patch if needed once I'm back from OoO, in August 5th. 
    
    Original change's description:
    > Move implementation of Mojo method GetTextSurroundingSelection() to Blink
    > 
    > Move this method from the content.mojom.Frame Mojo interface into Blink,
    > as part of a new blink.mojom.SurroundingText Mojo interface, and add a new
    > class there to implement such interface (i.e. blink::SurroundingTextImpl),
    > to replace usages of content.mojom.Frame's GetTextSurroundingSelection()
    > from the browser process.
    > 
    > Note that this SurroundingTextImpl class still relies on WebSurroundingText
    > and WebLocalFrame to keep the change smaller, but this is a temporary step
    > that will be corrected once WebSurroundingText has been moved out of the
    > public API and into renderer/core, which will happen on a follow-up CL.
    > 
    > Bug: 980151
    > Change-Id: I62fb2d5b24a98773c1acddbe1c58217c1a2d53e4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1696968
    > Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    > Reviewed-by: Mike West <mkwst@chromium.org>
    > Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#679981}
    
    TBR=dcheng@chromium.org,kenrb@chromium.org,haraken@chromium.org,dominickn@chromium.org,mkwst@chromium.org,mario@igalia.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 980151
    Change-Id: Ic3869e4d6fad9ca06d8d730d5202112b01cc47e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1719489
    Reviewed-by: Mario Sanchez Prada <mario@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#681243}
    
    Former-commit-id: ef82d5ffded4325e05d1d99c3cbfdaaa1c3a819e

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -2918,8 +2918,7 @@ void RenderFrameHostImpl::RequestTextSurroundingSelection(
     TextSurroundingSelectionCallback callback,
     int max_length) {
   DCHECK(!callback.is_null());
-  GetSurroundingText()->GetTextSurroundingSelection(max_length,
-                                                    std::move(callback));
+  frame_->GetTextSurroundingSelection(max_length, std::move(callback));
 }
 
 void RenderFrameHostImpl::AllowBindings(int bindings_flags) {
@@ -5485,14 +5484,6 @@ RenderFrameHostImpl::GetFindInPage() {
       !find_in_page_.is_connected())
     GetRemoteAssociatedInterfaces()->GetInterface(&find_in_page_);
   return find_in_page_;
-}
-
-const mojo::AssociatedRemote<blink::mojom::SurroundingText>&
-RenderFrameHostImpl::GetSurroundingText() {
-  if (!surrounding_text_ || !surrounding_text_.is_bound() ||
-      !surrounding_text_.is_connected())
-    GetRemoteAssociatedInterfaces()->GetInterface(&surrounding_text_);
-  return surrounding_text_;
 }
 
 void RenderFrameHostImpl::ResetLoadingState() {
