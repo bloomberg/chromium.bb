77bc43506c21c4fd36ffdc6cf1a8a072ccd4039c
Miyoung Shin <myid.shin@igalia.com>
2019-07-26 11:30:42 +0000

    Reland "Remove WeakPtrFactory from MediaRecorderHandler"
    This is a reland of 4444eaf10cc706ee5c27b4274f4595efdeef5c3d [formerly 129650cb78882d2657d77cbd56799c58bb16e493] with
    fixing the reverted reason that CCA video playback couldn't work
    after 2nd video recording on ChromeOS.
    
    The original CL removed WeakPtrFactory in MediaRecorderHandler class
    and the encoding callback was fired consistently even after
    MediaRecorderHandler::Stop was called, so this CL calls Pause function
    in MediaRecorderHandler::Stop to stop the encoding callback.
    
    Original change's description:
    > Remove WeakPtrFactory from MediaRecorderHandler
    >
    > This CL removes WeakPtrFactory from MediaRecorderHandler class that
    > is garbage collected and replaces the weak pointer with
    > WeakPersistent references.
    >
    > Bug: 981572
    > Change-Id: I5dbfc268177cac290b85246ec029833e6baf64ce
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1688966
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    > Cr-Commit-Position: refs/heads/master@{#675183}
    
    Bug: 981572
    Change-Id: I81938a2751564a6c38301b918f5ed72f6db4a019
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1718474
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#681241}
    
    Former-commit-id: a3a5bc12b9b8a302d7180b2d6f10eaee7d84f090

--- third_party/blink/renderer/modules/mediarecorder/media_recorder_handler.h
+++ third_party/blink/renderer/modules/mediarecorder/media_recorder_handler.h
@@ -8,7 +8,6 @@
 #include <memory>
 
 #include "base/macros.h"
-#include "base/memory/weak_ptr.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/string_piece.h"
 #include "base/threading/thread_checker.h"
@@ -139,8 +138,6 @@ class MODULES_EXPORT MediaRecorderHandler
   std::unique_ptr<media::WebmMuxer> webm_muxer_;
 
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
-
-  base::WeakPtrFactory<MediaRecorderHandler> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(MediaRecorderHandler);
 };
