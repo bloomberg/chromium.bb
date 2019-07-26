6a99285a394c7da665336fa54f53d64204faa142
Rohan Pavone <rohpavone@chromium.org>
2019-07-26 17:48:36 +0000

    [Permissions] Creates DevToolsPermissionOverrides.
    Creates class for DevTools permission overrides to maintain logic
    of overrides as they get more complicated. This maintains a mapping
    from URL (origin only) to PermissionTypes and their corresponding
    override status. If no override status is defined, the interface
    indicates this - it is up to the caller to determine what status to give
    if no override is given.
    
    Design doc: go/permission-overrides-refactor
    
    Tested: Unit tests.
    BUG: 986605
    Change-Id: I0ec28e08afbcb97fb9e18f8798cf95f8a63f972d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1713436
    Commit-Queue: Rohan Pavone <rohpavone@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681372}
    
    Former-commit-id: caa6637993ccc2d5fcbc37dc36e82ec9e95be6ad

---
+++ content/public/browser/devtools_permission_overrides.cc
@@ -0,0 +1,70 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/public/browser/devtools_permission_overrides.h"
+
+#include "base/no_destructor.h"
+
+namespace content {
+using PermissionOverrides = DevToolsPermissionOverrides::PermissionOverrides;
+using PermissionStatus = blink::mojom::PermissionStatus;
+
+DevToolsPermissionOverrides::DevToolsPermissionOverrides() = default;
+DevToolsPermissionOverrides::~DevToolsPermissionOverrides() = default;
+DevToolsPermissionOverrides::DevToolsPermissionOverrides(
+    DevToolsPermissionOverrides&& other) = default;
+DevToolsPermissionOverrides& DevToolsPermissionOverrides::operator=(
+    DevToolsPermissionOverrides&& other) = default;
+
+void DevToolsPermissionOverrides::Set(const url::Origin& origin,
+                                      const PermissionType& permission,
+                                      const PermissionStatus status) {
+  overrides_[origin][permission] = status;
+}
+
+base::Optional<PermissionStatus> DevToolsPermissionOverrides::Get(
+    const url::Origin& origin,
+    const PermissionType& type) const {
+  auto current_override = overrides_.find(origin);
+  if (current_override == overrides_.end())
+    return base::nullopt;
+
+  auto new_status = current_override->second.find(type);
+  if (new_status == current_override->second.end())
+    return base::nullopt;
+
+  return base::make_optional(new_status->second);
+}
+
+const PermissionOverrides& DevToolsPermissionOverrides::GetAll(
+    const url::Origin& origin) const {
+  static const base::NoDestructor<PermissionOverrides> empty_overrides;
+  auto it = overrides_.find(origin);
+  if (it == overrides_.end())
+    return *empty_overrides;
+  return it->second;
+}
+
+void DevToolsPermissionOverrides::GrantPermissions(
+    const url::Origin& origin,
+    const std::vector<PermissionType>& permissions) {
+  const std::vector<PermissionType>& kAllPermissionTypes =
+      GetAllPermissionTypes();
+  PermissionOverrides granted_overrides;
+  for (const auto& permission : kAllPermissionTypes)
+    granted_overrides[permission] = PermissionStatus::DENIED;
+  for (const auto& permission : permissions)
+    granted_overrides[permission] = PermissionStatus::GRANTED;
+  Reset(origin);
+  SetAll(origin, granted_overrides);
+}
+
+void DevToolsPermissionOverrides::SetAll(const url::Origin& origin,
+                                         const PermissionOverrides& overrides) {
+  PermissionOverrides& current_override = overrides_[origin];
+  for (const auto& setting : overrides)
+    current_override[setting.first] = setting.second;
+}
+
+}  // namespace content
