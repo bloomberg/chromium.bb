b2ac30e79868f7764f6fb3300824da3369e5d40c
Stepan Khapugin <stkhapugin@chromium.org>
2019-07-26 15:45:02 +0000

    Convert popup shortcut tests to EG2.
    * Ensures omnibox popup shortcuts egtests compat with EG2
    * Adds a new EG2 target and adds shortcuts tests to ios_chrome_eg2tests
    * Cleans up shortcut egtest deps
    
    Bug: 866147
    Change-Id: I3c004889b230e7590a62d5f4ab062cb6103456f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1698420
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681302}
    
    Former-commit-id: 77e29684aa1628406461063efbfca64d94f4a590

--- ios/chrome/browser/ui/omnibox/popup/shortcuts/shortcuts_egtest.mm
+++ ios/chrome/browser/ui/omnibox/popup/shortcuts/shortcuts_egtest.mm
@@ -2,21 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <EarlGrey/EarlGrey.h>
 #import <XCTest/XCTest.h>
 
 #include "base/bind.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/test/scoped_feature_list.h"
-#include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/ui/content_suggestions/ntp_home_constant.h"
 #import "ios/chrome/browser/ui/omnibox/omnibox_constants.h"
 #include "ios/chrome/browser/ui/ui_feature_flags.h"
-#import "ios/chrome/test/app/chrome_test_util.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
+#import "ios/testing/earl_grey/earl_grey_test.h"
 #include "net/test/embedded_test_server/http_request.h"
 #include "net/test/embedded_test_server/http_response.h"
 
@@ -170,8 +168,7 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
                                    nil)]
       assertWithMatcher:grey_sufficientlyVisible()];
 
-  [[EarlGrey
-      selectElementWithMatcher:[GREYMatchers matcherForButtonTitle:@"Done"]]
+  [[EarlGrey selectElementWithMatcher:grey_buttonTitle(@"Done")]
       performAction:grey_tap()];
 
   // Verify that after tapping Done the omnibox is defocused.
@@ -198,8 +195,7 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
                                    nil)]
       assertWithMatcher:grey_sufficientlyVisible()];
 
-  [[EarlGrey
-      selectElementWithMatcher:[GREYMatchers matcherForButtonTitle:@"Done"]]
+  [[EarlGrey selectElementWithMatcher:grey_buttonTitle(@"Done")]
       performAction:grey_tap()];
 
   // Verify that after tapping Done the omnibox is defocused.
@@ -225,8 +221,7 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
                                           nil)]
       assertWithMatcher:grey_sufficientlyVisible()];
 
-  [[EarlGrey
-      selectElementWithMatcher:[GREYMatchers matcherForButtonTitle:@"Done"]]
+  [[EarlGrey selectElementWithMatcher:grey_buttonTitle(@"Done")]
       performAction:grey_tap()];
 
   // Verify that after tapping Done the omnibox is defocused.
