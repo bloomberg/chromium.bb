cd0ce7531fa7a8e5bec7630872b2fe8d62a01c18
Kai Ninomiya <kainino@chromium.org>
2019-07-27 22:50:07 -0700

    add pbool as a shortcut for poptions [false,true]

--- src/framework/params/options.ts
+++ src/framework/params/options.ts
@@ -4,6 +4,10 @@ export function poptions(name: string, values: ParamArgument[]): POptions {
   return new POptions(name, values);
 }
 
+export function pbool(name: string): POptions {
+  return new POptions(name, [false, true]);
+}
+
 class POptions implements ParamSpecIterable {
   private name: string;
   private values: ParamArgument[];
