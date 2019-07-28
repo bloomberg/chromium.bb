0a244ef268de2735054cfb26a4f85ec787f461f1
Kai Ninomiya <kainino@chromium.org>
2019-07-27 23:28:24 -0700

    Remove unnecessary fence descriptors

--- src/suites/cts/fences.spec.ts
+++ src/suites/cts/fences.spec.ts
@@ -7,21 +7,24 @@ import { GPUTest } from './gpu_test.js';
 
 export const g = new TestGroup(GPUTest);
 
-// Test default initial value.
-g.test('initial/default', t => {
+g.test('initial/no descriptor', t => {
+  const fence = t.queue.createFence();
+  t.expect(fence.getCompletedValue() === 0);
+});
+
+g.test('initial/empty descriptor', t => {
   const fence = t.queue.createFence({});
   t.expect(fence.getCompletedValue() === 0);
 });
 
-// Test initial value.
-g.test('initial/initialValue', t => {
+g.test('initial/descriptor with initialValue', t => {
   const fence = t.queue.createFence({ initialValue: 2 });
   t.expect(fence.getCompletedValue() === 2);
 });
 
 // Promise resolves when onCompletion value is less than signal value.
 g.test('wait/less than signaled', async t => {
-  const fence = t.queue.createFence({});
+  const fence = t.queue.createFence();
   t.queue.signal(fence, 2);
   await fence.onCompletion(1);
   t.expect(fence.getCompletedValue() === 2);
@@ -29,7 +32,7 @@ g.test('wait/less than signaled', async t => {
 
 // Promise resolves when onCompletion value is equal to signal value.
 g.test('wait/equal to signaled', async t => {
-  const fence = t.queue.createFence({});
+  const fence = t.queue.createFence();
   t.queue.signal(fence, 2);
   await fence.onCompletion(2);
   t.expect(fence.getCompletedValue() === 2);
@@ -37,7 +40,7 @@ g.test('wait/equal to signaled', async t => {
 
 // Test it is illegal to wait on a value greater than the signaled value.
 g.test('wait/greater than signaled', async t => {
-  const fence = t.queue.createFence({});
+  const fence = t.queue.createFence();
   t.queue.signal(fence, 2);
   const promise = fence.onCompletion(3);
 
@@ -46,7 +49,7 @@ g.test('wait/greater than signaled', async t => {
 
 // Promise resolves when signal is called multiple times.
 g.test('wait/signaled multiple times', async t => {
-  const fence = t.queue.createFence({});
+  const fence = t.queue.createFence();
   t.queue.signal(fence, 1);
   t.queue.signal(fence, 2);
   await fence.onCompletion(2);
@@ -55,7 +58,7 @@ g.test('wait/signaled multiple times', async t => {
 
 // Promise resolves if fence has already completed.
 g.test('wait/already completed', async t => {
-  const fence = t.queue.createFence({});
+  const fence = t.queue.createFence();
   t.queue.signal(fence, 2);
 
   // Wait for value to update.
@@ -73,7 +76,7 @@ g.test('wait/already completed', async t => {
 
 // Test it is illegal to wait on a fence without signaling the value.
 g.test('wait/without signal', async t => {
-  const fence = t.queue.createFence({});
+  const fence = t.queue.createFence();
   const promise = fence.onCompletion(2);
 
   await t.shouldReject(promise, 'OperationError');
@@ -81,7 +84,7 @@ g.test('wait/without signal', async t => {
 
 // Test it is illegal to wait on a fence before it is signaled.
 g.test('wait/before signaled', async t => {
-  const fence = t.queue.createFence({});
+  const fence = t.queue.createFence();
   const promise = fence.onCompletion(2);
   t.queue.signal(fence, 2);
 
@@ -90,7 +93,7 @@ g.test('wait/before signaled', async t => {
 
 // Test many calls to signal and wait on fence values one at a time.
 g.test('wait/many/serially', async t => {
-  const fence = t.queue.createFence({});
+  const fence = t.queue.createFence();
   for (let i = 1; i <= 20; ++i) {
     t.queue.signal(fence, i);
     await fence.onCompletion(i);
@@ -100,7 +103,7 @@ g.test('wait/many/serially', async t => {
 
 // Test many calls to signal and wait on all fence values.
 g.test('wait/many/parallel', async t => {
-  const fence = t.queue.createFence({});
+  const fence = t.queue.createFence();
   const promises = [];
   for (let i = 1; i <= 20; ++i) {
     t.queue.signal(fence, i);
@@ -117,7 +120,7 @@ g.test('wait/many/parallel', async t => {
 // Test dropping references to the fence and onCompletion promise does not crash.
 g.test('drop/fence and promise', t => {
   {
-    const fence = t.queue.createFence({});
+    const fence = t.queue.createFence();
     t.queue.signal(fence, 2);
     fence.onCompletion(2);
   }
@@ -128,7 +131,7 @@ g.test('drop/fence and promise', t => {
 g.test('drop/promise', async t => {
   let promise;
   {
-    const fence = t.queue.createFence({});
+    const fence = t.queue.createFence();
     t.queue.signal(fence, 2);
     promise = fence.onCompletion(2);
   }
