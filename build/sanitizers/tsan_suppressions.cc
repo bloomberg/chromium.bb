3ec47313984414f0c71fe07875f9fb203485ef1c
Takuto Ikuta <tikuta@chromium.org>
2019-07-29 14:15:57 +0000

    [build] add libdconfsettings.so to tsan blacklist
    Bug: 977804
    Change-Id: Ib6d2a783bd053ef1554cb5a133b03d6fb894ace5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1722272
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Max Moroz <mmoroz@chromium.org>
    Reviewed-by: Max Moroz <mmoroz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#681796}
    
    Former-commit-id: a2d06c9da8e96c796d5298f9f1cc0b5b0ec71327

--- build/sanitizers/tsan_suppressions.cc
+++ build/sanitizers/tsan_suppressions.cc
@@ -15,10 +15,11 @@
 // See http://dev.chromium.org/developers/testing/threadsanitizer-tsan-v2
 // for the instructions on writing suppressions.
 char kTSanDefaultSuppressions[] =
-    // False positives in libflashplayer.so, libgio.so, libglib.so and
-    // libgobject.so.
+    // False positives in libdconfsettings.so, libflashplayer.so, libgio.so,
+    // libglib.so and libgobject.so.
     // Since we don't instrument them, we cannot reason about the
     // synchronization in them.
+    "race:libdconfsettings*.so\n"
     "race:libflashplayer.so\n"
     "race:libgio*.so\n"
     "race:libglib*.so\n"
