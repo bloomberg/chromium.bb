a93b90288c8e6099a55e22f393f379c96c6e5b01
Kristi Park <kristipark@chromium.org>
2019-07-29 23:38:06 +0000

    [NTP] Fix ricker picker dimensions to match spec
    Adjust positioning of elements to match spec:
    - Reduce width of dialog to 568px to match spec.
    - Remove hidden spacing between some inline-block elements due to font
      size.
    - Simplify header by removing some extra div wrappers.
    - Hide the back button using "display: none" in order to avoid messy
      margin adjustments.
    - Remove side margin of the last background tile per row.
    - Align footer and daily refresh elements using flexbox instead of
      inline-block.
    
    Misc. fixes:
    - Hide daily refresh toggle when background image panel is not open.
    - Fix back button focus color in dark mode.
    - Hide scrollbar when not required.
    
    Screenshots: https://drive.google.com/open?id=1p5VB83jYaUqk0Pz3mzlO8NTKbd9dgiDG
    
    Bug: 987076
    Change-Id: I12d9132f66361029b1b644a91f9de639ee74c2d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1721375
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682037}
    
    Former-commit-id: cee035b5eaee335fbe6bd4de70032d4f3641da6e

--- chrome/browser/resources/local_ntp/customize.js
+++ chrome/browser/resources/local_ntp/customize.js
@@ -138,7 +138,6 @@ customize.IDS = {
   LINK_ICON: 'link-icon',
   MENU: 'bg-sel-menu',
   OPTIONS_TITLE: 'edit-bg-title',
-  REFRESH_DAILY_WRAPPER: 'refresh-daily-wrapper',
   RESTORE_DEFAULT: 'edit-bg-restore-default',
   RESTORE_DEFAULT_TEXT: 'edit-bg-restore-default-text',
   SHORTCUTS_BUTTON: 'shortcuts-button',
@@ -477,7 +476,6 @@ customize.richerPicker_resetImageMenu = function() {
       customize.IDS.BACKGROUNDS_MENU;
   customize.richerPicker_openBackgroundSubmenu.title = '';
   backgroundMenu.scrollTop = 0;
-  $(customize.IDS.REFRESH_DAILY_WRAPPER).hidden = true;
 };
 
 /**
@@ -1208,7 +1206,6 @@ customize.showImageSelectionDialog = function(dialogTitle, collIndex) {
   } else {
     $(customize.IDS.TILES).focus();
   }
-  $(customize.IDS.REFRESH_DAILY_WRAPPER).hidden = false;
 };
 
 /**
