a0d57432f53db1b4e43a4c9e4f0d192927965fe9
David Bokan <bokan@chromium.org>
2019-07-29 15:36:38 +0000

    [TextFragmentAnchor] Add UMA metrics
    This CL adds two metrics. The first is an UMA histogram tracking how
    often we try to use an element-id based fragment and succeed vs fail.
    Note that if the TextFragmentAnchor feature is enabled, we won't count
    this metric if the fragment successfully parses to a |targetText|
    anchor.
    
    The second metric measures how often we see a hash ("#") character in
    the fragment. This is technically an invalid URL but we'd like to
    evaluate how often it happens in practice. Similarly to above, if
    targetText is enabled, we won't count cases of ##targetText as being
    part of this metric.
    
    TBR=bokan@chromium.org
    
    (cherry picked from commit 3ee00424fdc460dc9d743c228a841eee9f3fc514 [formerly 427b2ca2ff003fed78074d8f54b6548ed2757f48])
    
    Bug: 987027
    Change-Id: Ic3b6cf1a85e590449af12c131edc60b13bad0dfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1719651
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#681420}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1723298
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3865@{#18}
    Cr-Branched-From: 4e0147fd4ab868c78ff6bb1ef42f86871cde1461 [formerly 0cdcc6158160790658d1f033d3db873603250124]-refs/heads/master@{#681094}
    
    Former-commit-id: 6051dc9d48d4a4fed7d57e2b770429c70b6c6097

--- third_party/blink/renderer/core/page/scrolling/fragment_anchor.cc
+++ third_party/blink/renderer/core/page/scrolling/fragment_anchor.cc
@@ -4,6 +4,7 @@
 
 #include "third_party/blink/renderer/core/page/scrolling/fragment_anchor.h"
 
+#include "base/metrics/histogram_macros.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/page/scrolling/element_fragment_anchor.h"
 #include "third_party/blink/renderer/core/page/scrolling/text_fragment_anchor.h"
@@ -14,23 +15,57 @@ namespace blink {
 FragmentAnchor* FragmentAnchor::TryCreate(const KURL& url,
                                           LocalFrame& frame,
                                           bool same_document_navigation) {
+  DCHECK(frame.GetDocument());
+
   FragmentAnchor* anchor = nullptr;
   const bool text_fragment_identifiers_enabled =
       RuntimeEnabledFeatures::TextFragmentIdentifiersEnabled(
           frame.GetDocument());
 
+  // The text fragment anchor will be created if we successfully parsed the
+  // targetText but we only do the text matching later on.
+  bool text_fragment_anchor_created = false;
   if (text_fragment_identifiers_enabled) {
     anchor = TextFragmentAnchor::TryCreateFragmentDirective(
         url, frame, same_document_navigation);
+    text_fragment_anchor_created = anchor;
   }
 
+  // Count how often we see a # in the fragment (i.e. after the # delimiting
+  // the hash). We do this after trying to find a ##targetText so that we don't
+  // pollute this metric with our own feature since we're trying to determine
+  // how prevalent ## is. If a ##targetText is found, it'll strip off the ##
+  // and any text following it.
+  if (url.HasFragmentIdentifier()) {
+    size_t hash_pos = url.FragmentIdentifier().Find("#");
+    if (hash_pos != kNotFound)
+      UseCounter::Count(frame.GetDocument(), WebFeature::kFragmentDoubleHash);
+  }
+
+  bool element_id_anchor_found = false;
   if (!anchor) {
     anchor = ElementFragmentAnchor::TryCreate(url, frame);
+    element_id_anchor_found = anchor;
   }
 
-  if (!anchor && text_fragment_identifiers_enabled) {
-    anchor =
+  if (text_fragment_identifiers_enabled) {
+    FragmentAnchor* text_anchor =
         TextFragmentAnchor::TryCreate(url, frame, same_document_navigation);
+    text_fragment_anchor_created |= static_cast<bool>(text_anchor);
+    // We parse the anchor to determine if we should report the UMA metric
+    // below but we should only use it if we didn't find an element-id based
+    // fragment.
+    if (!anchor)
+      anchor = text_anchor;
+  }
+
+  // Track how often we have a element fragment that we can't find. Only track
+  // if we didn't match a text fragment since we expect those would inflate the
+  // "failed" case.
+  if (frame.GetDocument()->IsHTMLDocument() && url.HasFragmentIdentifier() &&
+      !text_fragment_anchor_created) {
+    UMA_HISTOGRAM_BOOLEAN("TextFragmentAnchor.ElementIdFragmentFound",
+                          element_id_anchor_found);
   }
 
   return anchor;
