c12f1c894f89aa7d942f45f2c8cd6fca0f3f0809
Alexey Baskakov <loyso@chromium.org>
2019-07-30 07:00:46 +0000

    WebApp: Get rid of web_applications/bookmark_apps/ directory.
    Move all extension-based tests into web_applications/extensions/.
    
    TBR=bsep@chromium.org
    
    Bug: 973324
    Change-Id: I3a2238ad37f9d540e2970e2d3a5bb4341f3e1ccb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1724520
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682172}
    
    Former-commit-id: dcab67d7d0909221f391600162f150dd45a295bb

--- chrome/browser/web_applications/extensions/system_web_app_manager_browsertest.cc
+++ chrome/browser/web_applications/extensions/system_web_app_manager_browsertest.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/web_applications/bookmark_apps/system_web_app_manager_browsertest.h"
+#include "chrome/browser/web_applications/extensions/system_web_app_manager_browsertest.h"
 
 #include <vector>
 
