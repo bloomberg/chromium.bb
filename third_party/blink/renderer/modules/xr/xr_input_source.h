671ae0b1ce889857549ed5c25e90a5700decfb28
Jacob DeWitt <jacde@chromium.org>
2019-07-30 16:59:33 +0000

    Add XRInputSource.profiles
    For now, it will always be an empty array. Populating the array will
    come in a later CL.
    
    Bug: 979250
    Change-Id: I7dabbec261f280d6937d57a9076033b3b7e09f3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1724277
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Jacob DeWitt <jacde@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682324}
    
    Former-commit-id: 583c5131caafb5dc3c8e7ea5dc928eed05eca47d

--- third_party/blink/renderer/modules/xr/xr_input_source.h
+++ third_party/blink/renderer/modules/xr/xr_input_source.h
@@ -55,6 +55,7 @@ class XRInputSource : public ScriptWrappable, public Gamepad::Client {
   XRSpace* targetRaySpace() const;
   XRSpace* gripSpace() const;
   Gamepad* gamepad() const { return gamepad_; }
+  Vector<String> profiles() const { return profiles_; }
 
   uint32_t source_id() const { return state_.source_id; }
 
@@ -129,6 +130,7 @@ class XRInputSource : public ScriptWrappable, public Gamepad::Client {
   Member<XRTargetRaySpace> target_ray_space_;
   Member<XRGripSpace> grip_space_;
   Member<Gamepad> gamepad_;
+  Vector<String> profiles_;
 
   std::unique_ptr<TransformationMatrix> base_pose_matrix_;
 
