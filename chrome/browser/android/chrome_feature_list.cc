a99ebcbb87ca608612f770552d5e164aea84bbda
Shakti Sahu <shaktisahu@chromium.org>
2019-07-30 23:06:36 +0000

    Enable Download Home V2 by default
    This CL enabled download home V2 by default on trunk.
    
    TBR=dtrainor@chromium.org
    
    (cherry picked from commit e964918c9ab86ad95ded680be31a492e11ab664d [formerly 6779e4256300fd0a3eb51ce81e9705f4a7f6eaf8])
    
    Bug: 988189
    Change-Id: I1a8be463e1603ed6abf7ceb4dbffe4f9ae23b738
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1721252
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#681571}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1726767
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3865@{#52}
    Cr-Branched-From: 4e0147fd4ab868c78ff6bb1ef42f86871cde1461 [formerly 0cdcc6158160790658d1f033d3db873603250124]-refs/heads/master@{#681094}
    
    Former-commit-id: 443ce71c57b97f100717deb7c6efdc5f457a06f1

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -390,7 +390,7 @@ const base::Feature kDownloadFileProvider{"DownloadFileProvider",
                                           base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kDownloadHomeV2{"DownloadHomeV2",
-                                    base::FEATURE_DISABLED_BY_DEFAULT};
+                                    base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kDownloadHomeShowStorageInfo{
     "DownloadHomeShowStorageInfo", base::FEATURE_DISABLED_BY_DEFAULT};
