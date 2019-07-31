3af9088b9bdd03ef09c1ae1105f77e053b72b7a7
vasilii <vasilii@chromium.org>
2019-07-31 11:04:35 +0000

    Deprecate some iOS histograms in favor of cross-platform ones.
    PasswordManager.SuggestionClicked and PasswordManager.SuggestionShown are gone.
    They are replaced with cross-platform ones:
    - PasswordManager.PasswordDropdownItemSelected and
    - PasswordManager.PasswordDropdownShown
    
    Bug: 988730
    Change-Id: I8be52f9ad89efdbe574dfada697619a2ec400487
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1724688
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682653}
    
    Former-commit-id: e96a043aafc2bd02f60aeedf55700a11447d65d5

--- ios/chrome/browser/passwords/password_controller.mm
+++ ios/chrome/browser/passwords/password_controller.mm
@@ -81,6 +81,8 @@ using base::SysNSStringToUTF16;
 using base::SysUTF16ToNSString;
 using l10n_util::GetNSString;
 using l10n_util::GetNSStringF;
+using password_manager::metrics_util::LogPasswordDropdownShown;
+using password_manager::metrics_util::PasswordDropdownState;
 using password_manager::AccountSelectFillData;
 using password_manager::FillData;
 using password_manager::GetPageURLAndCheckTrustLevel;
@@ -96,18 +98,6 @@ namespace {
 // Types of password infobars to display.
 enum class PasswordInfoBarType { SAVE, UPDATE };
 
-// Types of password suggestion in the keyboard accessory. Used for metrics
-// collection.
-enum class PasswordSuggestionType {
-  // Credentials are listed.
-  CREDENTIALS = 0,
-  // "Show All" is listed.
-  SHOW_ALL = 1,
-  // "Suggest Password" is listed.
-  SUGGESTED = 2,
-  COUNT = 3,
-};
-
 // Password is considered not generated when user edits it below 4 characters.
 constexpr int kMinimumLengthForEditedPassword = 4;
 
@@ -116,16 +106,6 @@ constexpr int kNotifyAutoSigninDuration = 3;  // seconds
 
 // The string ' •••' appended to the username in the suggestion.
 NSString* const kSuggestionSuffix = @" ••••••••";
-
-void LogSuggestionClicked(PasswordSuggestionType type) {
-  UMA_HISTOGRAM_ENUMERATION("PasswordManager.SuggestionClicked", type,
-                            PasswordSuggestionType::COUNT);
-}
-
-void LogSuggestionShown(PasswordSuggestionType type) {
-  UMA_HISTOGRAM_ENUMERATION("PasswordManager.SuggestionShown", type,
-                            PasswordSuggestionType::COUNT);
-}
 }  // namespace
 
 @interface PasswordController ()<PasswordSuggestionHelperDelegate>
@@ -423,8 +403,9 @@ void LogSuggestionShown(PasswordSuggestionType type) {
                                      icon:nil
                                identifier:0]];
   }
+  base::Optional<PasswordDropdownState> suggestion_state;
   if (suggestions.count) {
-    LogSuggestionShown(PasswordSuggestionType::CREDENTIALS);
+    suggestion_state = PasswordDropdownState::kStandard;
   }
 
   if ([self canGeneratePasswordForForm:formName
@@ -440,7 +421,12 @@ void LogSuggestionShown(PasswordSuggestionType type) {
                                icon:nil
                          identifier:autofill::
                                         POPUP_ITEM_ID_GENERATE_PASSWORD_ENTRY]];
-    LogSuggestionShown(PasswordSuggestionType::SUGGESTED);
+    suggestion_state = PasswordDropdownState::kStandardGenerate;
+  }
+
+  if (suggestion_state) {
+    LogPasswordDropdownShown(*suggestion_state,
+                             _passwordManagerClient->IsIncognito());
   }
 
   completion([suggestions copy], self);
@@ -456,7 +442,10 @@ void LogSuggestionShown(PasswordSuggestionType type) {
       // Navigate to the settings list.
       [self.delegate displaySavedPasswordList];
       completion();
-      LogSuggestionClicked(PasswordSuggestionType::SHOW_ALL);
+      password_manager::metrics_util::LogPasswordDropdownItemSelected(
+          password_manager::metrics_util::PasswordDropdownSelectedOption::
+              kShowAll,
+          _passwordManagerClient->IsIncognito());
       return;
     }
     case autofill::POPUP_ITEM_ID_GENERATE_PASSWORD_ENTRY: {
@@ -464,11 +453,17 @@ void LogSuggestionShown(PasswordSuggestionType type) {
       // whether user injects a generated password or cancels.
       [self generatePasswordForFormName:formName
                         fieldIdentifier:fieldIdentifier];
-      LogSuggestionClicked(PasswordSuggestionType::SUGGESTED);
+      password_manager::metrics_util::LogPasswordDropdownItemSelected(
+          password_manager::metrics_util::PasswordDropdownSelectedOption::
+              kGenerate,
+          _passwordManagerClient->IsIncognito());
       return;
     }
     default: {
-      LogSuggestionClicked(PasswordSuggestionType::CREDENTIALS);
+      password_manager::metrics_util::LogPasswordDropdownItemSelected(
+          password_manager::metrics_util::PasswordDropdownSelectedOption::
+              kPassword,
+          _passwordManagerClient->IsIncognito());
       DCHECK([suggestion.value hasSuffix:kSuggestionSuffix]);
       NSString* username = [suggestion.value
           substringToIndex:suggestion.value.length - kSuggestionSuffix.length];
