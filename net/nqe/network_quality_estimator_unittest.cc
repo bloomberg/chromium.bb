f90282d12f9a29ecc3bb5c77522bf605016a9465
Owen Min <zmin@chromium.org>
2019-07-31 19:40:48 +0000

    Disable NetworkQualityEstimatorTest.TestTCPSocketRTT on ChromeOS
    The test is flaky.
    
    Bug: 986904
    Change-Id: I670a6b99b5ec6448b4cc8cbf7f1569112d2d8bcb
    Tbr: tbansal@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1729396
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682869}
    
    Former-commit-id: dc87c31d1c864a3d4d8729710e1cc92f7884b93b

--- net/nqe/network_quality_estimator_unittest.cc
+++ net/nqe/network_quality_estimator_unittest.cc
@@ -2143,7 +2143,9 @@ TEST_F(NetworkQualityEstimatorTest, TestGlobalSocketWatcherThrottle) {
 
 // TestTCPSocketRTT requires kernel support for tcp_info struct, and so it is
 // enabled only on certain platforms.
-#if defined(TCP_INFO) || defined(OS_LINUX) || defined(OS_ANDROID)
+// ChromeOS is disabled due to crbug.com/986904
+#if (defined(TCP_INFO) || defined(OS_LINUX) || defined(OS_ANDROID)) && \
+    !defined(OS_CHROMEOS)
 #define MAYBE_TestTCPSocketRTT TestTCPSocketRTT
 #else
 #define MAYBE_TestTCPSocketRTT DISABLED_TestTCPSocketRTT
