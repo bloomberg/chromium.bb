ef0086cb1730faec13ab5e47eeea84dee851e53d
Marc Treib <treib@chromium.org>
2019-07-31 22:04:04 +0000

    Remove dead PasswordStoreFactory code related to Linux backends
    Bug: 950269
    Change-Id: I3fd1e103b675166b4bbc69205d8f92b02ce63c9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1720832
    Reviewed-by: Christos Froussios <cfroussios@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Auto-Submit: Marc Treib <treib@chromium.org>
    Commit-Queue: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682949}
    
    Former-commit-id: 8f17c67770320c393ead7fa9df40777e1fda1e44

--- chrome/browser/password_manager/password_store_factory.h
+++ chrome/browser/password_manager/password_store_factory.h
@@ -13,10 +13,6 @@
 #include "components/keyed_service/content/refcounted_browser_context_keyed_service_factory.h"
 #include "components/keyed_service/core/service_access_type.h"
 
-#if defined(USE_X11)
-#include "base/nix/xdg_util.h"
-#endif
-
 class PrefService;
 class Profile;
 
@@ -65,40 +61,6 @@ class PasswordStoreFactory
   content::BrowserContext* GetBrowserContextToUse(
       content::BrowserContext* context) const override;
   bool ServiceIsNULLWhileTesting() const override;
-
-#if defined(USE_X11)
-  enum LinuxBackendUsage {
-    KDE_NOFLAG_PLAINTEXT,
-    KDE_NOFLAG_KWALLET,
-    KDE_KWALLETFLAG_PLAINTEXT,
-    KDE_KWALLETFLAG_KWALLET,
-    KDE_GNOMEFLAG_PLAINTEXT,
-    KDE_GNOMEFLAG_KEYRING,
-    KDE_GNOMEFLAG_LIBSECRET,
-    KDE_BASICFLAG_PLAINTEXT,
-    GNOME_NOFLAG_PLAINTEXT,
-    GNOME_NOFLAG_KEYRING,
-    GNOME_NOFLAG_LIBSECRET,
-    GNOME_GNOMEFLAG_PLAINTEXT,
-    GNOME_GNOMEFLAG_KEYRING,
-    GNOME_GNOMEFLAG_LIBSECRET,
-    GNOME_KWALLETFLAG_PLAINTEXT,
-    GNOME_KWALLETFLAG_KWALLET,
-    GNOME_BASICFLAG_PLAINTEXT,
-    OTHER_PLAINTEXT,
-    OTHER_KWALLET,
-    OTHER_KEYRING,
-    OTHER_LIBSECRET,
-    MAX_BACKEND_USAGE_VALUE
-  };
-
-  enum LinuxBackendUsed { PLAINTEXT, GNOME_KEYRING, LIBSECRET, KWALLET };
-
-  static base::nix::DesktopEnvironment GetDesktopEnvironment();
-  static void RecordBackendStatistics(base::nix::DesktopEnvironment desktop_env,
-                                      const std::string& command_line_flag,
-                                      LinuxBackendUsed used_backend);
-#endif
 
   DISALLOW_COPY_AND_ASSIGN(PasswordStoreFactory);
 };
