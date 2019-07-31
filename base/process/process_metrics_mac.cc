80b488984551906551107ba8ecc214bbdb3a4bad
Leonard Grey <lgrey@chromium.org>
2019-07-31 17:43:00 +0000

    [RELAND] Mac: Record process “energy impact” to UMA
    (This was reverted because network sampling was hanging. This change opts us
    out of network sampling, which makes what we record slightly less accurate.
    The alternative approach is to gather energy impact asynchronously, which
    means we would need to lag behind one sample interval and requires lots of
    bookkeeping).
    
    Original description:
    This is a synthetic estimate displayed to users in Activity Monitor, the battery menu, and top.
    More info at https://blog.mozilla.org/nnethercote/2015/08/26/what-does-the-os-x-activity-monitors-energy-impact-actually-measure/
    
    TBR=oysteine@chromium.org
    
    Bug: 969105
    Change-Id: I8af6b03f9714aa2e6d6302726b0392fa287fab5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1718791
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682794}
    
    Former-commit-id: 392cf2bc4f5f024410490c4b159bd60169a24c7b

--- base/process/process_metrics_mac.cc
+++ base/process/process_metrics_mac.cc
@@ -6,6 +6,7 @@
 
 #include <libproc.h>
 #include <mach/mach.h>
+#include <mach/mach_time.h>
 #include <mach/mach_vm.h>
 #include <mach/shared_region.h>
 #include <stddef.h>
@@ -20,6 +21,34 @@
 #include "base/numerics/safe_conversions.h"
 #include "base/numerics/safe_math.h"
 #include "base/process/process_metrics_iocounters.h"
+#include "base/time/time.h"
+
+namespace {
+
+// This is a standin for the private pm_task_energy_data_t struct.
+struct OpaquePMTaskEnergyData {
+  // Empirical size of the private struct.
+  uint8_t data[384];
+};
+
+// Sample everything but network usage, since fetching network
+// usage can hang.
+static constexpr uint8_t kPMSampleFlags = 0xff & ~0x8;
+
+}  // namespace
+
+extern "C" {
+
+// From libpmsample.dylib
+int pm_sample_task(mach_port_t task,
+                   OpaquePMTaskEnergyData* pm_energy,
+                   uint64_t mach_time,
+                   uint8_t flags);
+
+// From libpmenergy.dylib
+double pm_energy_impact(OpaquePMTaskEnergyData* pm_energy);
+
+}  // extern "C"
 
 namespace base {
 
@@ -57,6 +86,14 @@ bool GetPowerInfo(mach_port_t task, task_power_info* power_info_data) {
                                &power_info_count);
   // Most likely cause for failure: |task| is a zombie.
   return kr == KERN_SUCCESS;
+}
+
+double GetEnergyImpactInternal(mach_port_t task, uint64_t mach_time) {
+  OpaquePMTaskEnergyData energy_info{};
+
+  if (pm_sample_task(task, &energy_info, mach_time, kPMSampleFlags) != 0)
+    return 0.0;
+  return pm_energy_impact(&energy_info);
 }
 
 }  // namespace
@@ -146,6 +183,31 @@ int ProcessMetrics::GetIdleWakeupsPerSecond() {
   return CalculateIdleWakeupsPerSecond(power_info_data.task_interrupt_wakeups);
 }
 
+int ProcessMetrics::GetEnergyImpact() {
+  uint64_t now = mach_absolute_time();
+  if (last_energy_impact_ == 0) {
+    last_energy_impact_ = GetEnergyImpactInternal(TaskForPid(process_), now);
+    last_energy_impact_time_ = now;
+    return 0;
+  }
+
+  double total_energy_impact =
+      GetEnergyImpactInternal(TaskForPid(process_), now);
+  uint64_t delta = now - last_energy_impact_time_;
+  if (delta == 0)
+    return 0;
+
+  // Scale by 100 since the histogram is integral.
+  double seconds_since_last_measurement =
+      base::TimeTicks::FromMachAbsoluteTime(delta).since_origin().InSecondsF();
+  int energy_impact = 100 * (total_energy_impact - last_energy_impact_) /
+                      seconds_since_last_measurement;
+  last_energy_impact_ = total_energy_impact;
+  last_energy_impact_time_ = now;
+
+  return energy_impact;
+}
+
 int ProcessMetrics::GetOpenFdCount() const {
   // In order to get a true count of the open number of FDs, PROC_PIDLISTFDS
   // is used. This is done twice: first to get the appropriate size of a
@@ -183,6 +245,7 @@ ProcessMetrics::ProcessMetrics(ProcessHandle process,
     : process_(process),
       last_absolute_idle_wakeups_(0),
       last_absolute_package_idle_wakeups_(0),
+      last_energy_impact_(0),
       port_provider_(port_provider) {}
 
 mach_port_t ProcessMetrics::TaskForPid(ProcessHandle process) const {
