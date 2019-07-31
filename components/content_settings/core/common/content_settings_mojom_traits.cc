fad8f6a40c48a72e0523de130b4eee90af11198b
Miyoung Shin <myid.shin@igalia.com>
2019-07-31 23:20:06 +0000

    Rename traits in //components/content_settings
    This CL renames *_struct_traits.* to *_mojom_traits.* in
    //components/content_settings.
    
    *_struct_traits.* => *_mojom_traits.*
    
    Bug: 806965
    Change-Id: I35cd4278ff10e95c7b62bcf38c4881f4b22aaee1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1728891
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#682982}
    
    Former-commit-id: 04d3d3cdba04b8c11438a6c28356efe942758feb

--- components/content_settings/core/common/content_settings_mojom_traits.cc
+++ components/content_settings/core/common/content_settings_mojom_traits.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/content_settings/core/common/content_settings_struct_traits.h"
+#include "components/content_settings/core/common/content_settings_mojom_traits.h"
 
 namespace mojo {
 
