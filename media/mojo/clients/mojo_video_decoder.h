557de85d4924c3443657bbb9677c037a567d38bc
Miguel Casas <mcasas@chromium.org>
2019-07-31 01:11:18 +0000

    media: add Media.MojoVideoDecoder.Decode UMA histogram
    This CL adds a histogram for Media.MojoVideoDecoder.Decode;
    this is already tracked as an async trace, for the purpose
    of CrOs Tast, a histogram is needed.(This is basically the
    same case as crrev.com/c/1680764)
    
    Bug: 953253
    Change-Id: I4cab76dd739f7b8196b29ec5870e141f04b1076c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1725313
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682543}
    
    Former-commit-id: dda8d41f0507c21d7c9f69a166e1bd7c660b9645

--- media/mojo/clients/mojo_video_decoder.h
+++ media/mojo/clients/mojo_video_decoder.h
@@ -5,6 +5,7 @@
 #ifndef MEDIA_MOJO_CLIENTS_MOJO_VIDEO_DECODER_H_
 #define MEDIA_MOJO_CLIENTS_MOJO_VIDEO_DECODER_H_
 
+#include "base/containers/mru_cache.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
@@ -74,7 +75,7 @@ class MojoVideoDecoder final : public VideoDecoder,
   void OnInitializeDone(bool status,
                         bool needs_bitstream_conversion,
                         int32_t max_decode_requests);
-  void OnDecodeDone(uint64_t decode_id, int64_t timestamp, DecodeStatus status);
+  void OnDecodeDone(uint64_t decode_id, DecodeStatus status);
   void OnResetDone();
 
   void BindRemoteDecoder();
@@ -103,6 +104,10 @@ class MojoVideoDecoder final : public VideoDecoder,
   uint64_t decode_counter_ = 0;
   std::map<uint64_t, DecodeCB> pending_decodes_;
   base::OnceClosure reset_cb_;
+
+  // DecodeBuffer/VideoFrame timestamps for histogram/tracing purposes. Must be
+  // large enough to account for any amount of frame reordering.
+  base::MRUCache<int64_t, base::TimeTicks> timestamps_;
 
   mojom::VideoDecoderPtr remote_decoder_;
   std::unique_ptr<MojoDecoderBufferWriter> mojo_decoder_buffer_writer_;
