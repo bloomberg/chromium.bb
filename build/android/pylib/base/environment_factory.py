d71376e63a1090ec44c9c0a3356b10a3aba06997
John Budorick <jbudorick@chromium.org>
2019-07-31 17:23:35 +0000

    android: add a local emulator environment.
    This allows //build/android/test_runner.py to run an AVD on which
    it can run tests.
    
    Change-Id: I653a9e2e351ced1914c471e83aab3f6c0dc8d508
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1699274
    Reviewed-by: Yun Liu <yliuyliu@google.com>
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#682784}
    
    Former-commit-id: 21db2d8b13e294dfd0bc5a7640873d52298c1046

--- build/android/pylib/base/environment_factory.py
+++ build/android/pylib/base/environment_factory.py
@@ -4,12 +4,16 @@
 
 from pylib import constants
 from pylib.local.device import local_device_environment
+from pylib.local.emulator import local_emulator_environment
 from pylib.local.machine import local_machine_environment
 
 def CreateEnvironment(args, output_manager, error_func):
 
   if args.environment == 'local':
     if args.command not in constants.LOCAL_MACHINE_TESTS:
+      if args.avd_name:
+        return local_emulator_environment.LocalEmulatorEnvironment(
+            args, output_manager, error_func)
       return local_device_environment.LocalDeviceEnvironment(
           args, output_manager, error_func)
     else:
