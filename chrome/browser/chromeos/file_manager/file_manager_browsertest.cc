10352a73baf1f0a1201bf8c65269d6b868c802a4
Alex Danilo <adanilo@chromium.org>
2019-08-01 05:54:17 +0000

    Hide feedback panels for file picker
    Feedback panels were visible on all active File Manager windows.
    
    Disable display of the panels if the File Picker is active
    which is deduced by using the 'type' of the main window body
    and using the existing CSS rule for visibility when the main
    body type is 'full-page' only.
    
    Also moves the panel location in the DOM to be contained by the
    dialog main panel in all cases so that positioning for layout
    will be relative to the dialog-main <div> rather than
    the 'dialog-container'.
    
    No-try: true
    Bug: 988193
    Tests: browser_tests --gtest_filter="*PanelsDisabled".
    Change-Id: I5d152ab8a0d2cd7bf299c706223505da97ca7a27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1723572
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683080}
    
    Former-commit-id: ad88a54947c25ffb10047f7e0b7d85dc0924f44c

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -838,6 +838,7 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
             .WithBrowser()
             .InIncognito()
             .EnableMyFilesVolume(),
+        TestCase("openFileDialogPanelsDisabled").WithBrowser(),
         TestCase("saveFileDialogDownloads").WithBrowser(),
         TestCase("saveFileDialogDownloads").WithBrowser().EnableMyFilesVolume(),
         TestCase("saveFileDialogDownloads").WithBrowser().InGuestMode(),
@@ -853,6 +854,7 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("saveFileDialogDownloadsNewFolderButton")
             .WithBrowser()
             .EnableMyFilesVolume(),
+        TestCase("saveFileDialogPanelsDisabled").WithBrowser(),
         TestCase("openFileDialogCancelDownloads").WithBrowser(),
         TestCase("openFileDialogCancelDownloads")
             .WithBrowser()
