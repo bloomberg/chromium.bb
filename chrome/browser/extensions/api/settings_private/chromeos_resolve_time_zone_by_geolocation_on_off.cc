391cf4e521f78c303cfa31a2f28f969f502ab94a
Henrique Grandinetti <hgrandinetti@chromium.org>
2019-08-01 13:20:16 +0000

    Disable timezone changes for children based on feature flag value
    Bug: 988490
    Change-Id: Ib26f0315a6fcebec7e25b6f2b3139b34f5221c38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1724697
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Commit-Queue: Henrique Grandinetti <hgrandinetti@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683142}
    
    Former-commit-id: 3a32093d1b32b986f07cbbc5a2c15146d489f225

--- chrome/browser/extensions/api/settings_private/chromeos_resolve_time_zone_by_geolocation_on_off.cc
+++ chrome/browser/extensions/api/settings_private/chromeos_resolve_time_zone_by_geolocation_on_off.cc
@@ -4,12 +4,14 @@
 
 #include "chrome/browser/extensions/api/settings_private/chromeos_resolve_time_zone_by_geolocation_on_off.h"
 
+#include "base/feature_list.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chromeos/system/timezone_resolver_manager.h"
 #include "chrome/browser/extensions/api/settings_private/generated_pref.h"
 #include "chrome/browser/extensions/api/settings_private/generated_time_zone_pref_base.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/extensions/api/settings_private.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
@@ -71,6 +73,13 @@ SetPrefResult GeneratedResolveTimezoneByGeolocationOnOff::SetPref(
   if (chromeos::system::TimeZoneResolverManager::
           IsTimeZoneResolutionPolicyControlled() ||
       !profile_->IsSameProfile(ProfileManager::GetPrimaryUserProfile())) {
+    return SetPrefResult::PREF_NOT_MODIFIABLE;
+  }
+
+  // If the Parent Access Code feature is not available, children must use
+  // automatic timezone.
+  if (profile_->IsChild() &&
+      !base::FeatureList::IsEnabled(features::kParentAccessCodeForTimeChange)) {
     return SetPrefResult::PREF_NOT_MODIFIABLE;
   }
 
