959717b9660b9f16ff4ad7b856c81c91c6545be5
Erik Jensen <rkjnsn@chromium.org>
2019-08-02 17:40:19 +0000

    remoting: Obtain max message size dynamically.
    Previously, clipboard messages were capped at the default safe size of
    64KiB. This patchset raises the limit when the session descriptor
    indicates that a larger size is supported.
    
    (cherry picked from commit 20ef88fc76bcaf8d2f690bafc6b975d981b3f3df [formerly 8627e9e8f1b9ac9543d702372c83bfe86dc37a20])
    
    Bug: 988865
    Change-Id: I651c4e0a5aa96b84950eb4fa8f798af7579eff85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1726277
    Commit-Queue: Erik Jensen <rkjnsn@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Auto-Submit: Erik Jensen <rkjnsn@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#682400}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1731549
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3865@{#142}
    Cr-Branched-From: 4e0147fd4ab868c78ff6bb1ef42f86871cde1461 [formerly 0cdcc6158160790658d1f033d3db873603250124]-refs/heads/master@{#681094}
    
    Former-commit-id: 334e45e4029278ac963797685e245e91bc82fc17

--- remoting/protocol/webrtc_connection_to_client.cc
+++ remoting/protocol/webrtc_connection_to_client.cc
@@ -26,6 +26,7 @@
 #include "remoting/protocol/webrtc_video_stream.h"
 #include "third_party/webrtc/api/media_stream_interface.h"
 #include "third_party/webrtc/api/peer_connection_interface.h"
+#include "third_party/webrtc/api/sctp_transport_interface.h"
 
 namespace remoting {
 namespace protocol {
@@ -179,6 +180,14 @@ void WebrtcConnectionToClient::OnWebrtcTransportConnecting() {
 
 void WebrtcConnectionToClient::OnWebrtcTransportConnected() {
   DCHECK(thread_checker_.CalledOnValidThread());
+  auto sctp_transport = transport_->peer_connection()->GetSctpTransport();
+  if (sctp_transport) {
+    absl::optional<double> max_message_size =
+        sctp_transport->Information().MaxMessageSize();
+    if (max_message_size && *max_message_size > 0) {
+      control_dispatcher_->set_max_message_size(*max_message_size);
+    }
+  }
 }
 
 void WebrtcConnectionToClient::OnWebrtcTransportError(ErrorCode error) {
