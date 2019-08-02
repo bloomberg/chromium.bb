9d2939948ce269cb8fca5d0ba2f1e60cc9483794
Tricia Crichton <triciac@chromium.org>
2019-08-02 00:20:35 +0000

    [ChromeDriver] Fix unable to delete temp dirs on exit test
    When running run_py_tests.py, chmod on temp dirs can fail occasionally.
    Wrapped the command with try/except to suppress the error and
    allow tests process to complete.
    
    Change-Id: I896f10021348f45a065a66787a4bad5dcb01b70f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1732150
    Commit-Queue: Tricia Crichton <triciac@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683406}
    
    Former-commit-id: b480184243a4f4194230f91881ed5c8ddf64a0ce

--- chrome/test/chromedriver/util.py
+++ chrome/test/chromedriver/util.py
@@ -87,7 +87,10 @@ def _DeleteDir(path):
   for root, dirs, files in os.walk(path, topdown=False):
     for name in files:
       filename = os.path.join(root, name)
-      os.chmod(filename, stat.S_IWRITE)
+      try:
+        os.chmod(filename, stat.S_IWRITE)
+      except OSError:
+        pass
       os.remove(filename)
     for name in dirs:
       os.rmdir(os.path.join(root, name))
