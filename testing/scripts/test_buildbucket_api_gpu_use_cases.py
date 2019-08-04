28a93f14c847be668f8982e9ecb95c063d21ebe1
Yuly Novikov <ynovikov@chromium.org>
2019-08-04 01:17:51 +0000

    Follow up on "Win10 FYI x64 Release (Intel HD 630)" bot rename
    TBR=kbr
    
    Bug: 988833
    Change-Id: Ifdd007e7fc3bfb84375d61c62e56dc0602f60357
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1734023
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#683869}
    
    Former-commit-id: 9505b00aafc7c689263479b2c568789ada35f325

--- testing/scripts/test_buildbucket_api_gpu_use_cases.py
+++ testing/scripts/test_buildbucket_api_gpu_use_cases.py
@@ -32,9 +32,7 @@ class BuildBucketApiGpuUseCaseTests:
     # Verify we can get power measurement test data from latest successful
     # build, including the swarming bot that runs the test, and actual test
     # results.
-    # TODO(ynovikov): rename to 'Win10 FYI x64 Release (Intel HD 630)'
-    # once it has a successful build. crbug.com/988833
-    bot = 'Win10 FYI Release (Intel HD 630)'
+    bot = 'Win10 FYI x64 Release (Intel HD 630)'
     step = 'power_measurement_test'
     build_id = gather_power_measurement_results.GetLatestGreenBuild(bot)
     build_json = gather_power_measurement_results.GetJsonForBuildSteps(
