6e764057d5c9ce560818296cf36b487ec02fb27a
Jordan Demeulenaere <jdemeulenaere@google.com>
2019-08-05 12:33:52 +0000

    [Autofill Assistant] Allow to navigate backward/forward.
    (cherry picked from commit 2d743035904beb0996ef4e43bf8dc8e27695e3bf [formerly 20b653eb780d24f1b55c97bdc2ce2abccb87f72d])
    
    Bug: 990336
    Change-Id: I69a14a6e77e53e6eea77aa1c5e4e905b59ae6720
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1724761
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Commit-Queue: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#682271}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1735584
    Reviewed-by: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3865@{#183}
    Cr-Branched-From: 4e0147fd4ab868c78ff6bb1ef42f86871cde1461 [formerly 0cdcc6158160790658d1f033d3db873603250124]-refs/heads/master@{#681094}
    
    Former-commit-id: 49550537c468443898bcd0ac293ec7344aa4e49b

--- components/autofill_assistant/browser/actions/navigate_action.cc
+++ components/autofill_assistant/browser/actions/navigate_action.cc
@@ -9,6 +9,7 @@
 
 #include "base/callback.h"
 #include "components/autofill_assistant/browser/actions/action_delegate.h"
+#include "content/public/browser/web_contents.h"
 #include "url/gurl.h"
 
 namespace autofill_assistant {
@@ -27,9 +28,31 @@ void NavigateAction::InternalProcessAction(ProcessActionCallback callback) {
   // without having to add an expect_navigation first.
   delegate_->ExpectNavigation();
 
-  GURL url(proto_.navigate().url());
-  delegate_->LoadURL(url);
-  UpdateProcessedAction(ACTION_APPLIED);
+  auto& proto = proto_.navigate();
+  if (!proto.url().empty()) {
+    GURL url(proto_.navigate().url());
+    delegate_->LoadURL(url);
+    UpdateProcessedAction(ACTION_APPLIED);
+  } else if (proto.go_backward()) {
+    auto& controller = delegate_->GetWebContents()->GetController();
+    if (controller.CanGoBack()) {
+      controller.GoBack();
+      UpdateProcessedAction(ACTION_APPLIED);
+    } else {
+      UpdateProcessedAction(PRECONDITION_FAILED);
+    }
+  } else if (proto.go_forward()) {
+    auto& controller = delegate_->GetWebContents()->GetController();
+    if (controller.CanGoForward()) {
+      controller.GoForward();
+      UpdateProcessedAction(ACTION_APPLIED);
+    } else {
+      UpdateProcessedAction(PRECONDITION_FAILED);
+    }
+  } else {
+    UpdateProcessedAction(UNSUPPORTED);
+  }
+
   std::move(callback).Run(std::move(processed_action_proto_));
 }
 
