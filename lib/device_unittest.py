3cfdd83e16c09222b735df6d9dfd6d8bcede5e87
Eashan Bhatt <eashanbhatt@google.com>
2019-08-05 11:28:18 -0700

    device_unittest: Add Create Unittests
    * testCreate tests the device/vm creation.
    * New helper function CreateDevice.
    
    BUG=chromium:987753
    TEST=This is the test.
    
    Change-Id: Ia07ab2705b3cd1e5966b96cb460426a7716614ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/1737266
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Eashan Bhatt <eashanbhatt@google.com>
    Tested-by: Eashan Bhatt <eashanbhatt@google.com>

--- lib/device_unittest.py
+++ lib/device_unittest.py
@@ -9,6 +9,7 @@ from __future__ import print_function
 
 from chromite.lib import cros_test_lib
 from chromite.lib import device
+from chromite.lib import vm
 
 
 class DeviceTester(cros_test_lib.RunCommandTestCase):
@@ -18,6 +19,19 @@ class DeviceTester(cros_test_lib.RunCommandTestCase):
     """Common set up method for all tests."""
     opts = device.Device.GetParser().parse_args([])
     self._device = device.Device(opts)
+
+  def CreateDevice(self, device_name, is_vm):
+    """Creates a device.
+
+    Args:
+      device_name: Name of the device.
+      is_vm: If True, then created device should be a VM.
+    """
+    self._device.device = device_name
+    created_device = device.Device.Create(
+        vm.VM.GetParser().parse_args(['--device', device_name]))
+    self.assertEqual(isinstance(created_device, vm.VM), is_vm)
+    self.assertEqual(self._device.is_vm, is_vm)
 
   def testRunCmd(self):
     """Verify that a command is run via RunCommand."""
@@ -38,3 +52,12 @@ class DeviceTester(cros_test_lib.RunCommandTestCase):
     self._device.use_sudo = True
     self._device.RunCommand(['mount', '-o', 'remount,rw', '/'])
     self.assertCommandCalled(['sudo', '--', 'mount', '-o', 'remount,rw', '/'])
+
+  def testCreate(self):
+    """Verify Device/VM creation."""
+    # Verify a VM is created by default.
+    self.CreateDevice(None, True)
+    # Verify a VM is created when IP is localhost.
+    self.CreateDevice('localhost', True)
+    # Verify a Device is created when an IP is specified.
+    self.CreateDevice('190.0.2.130', False)
