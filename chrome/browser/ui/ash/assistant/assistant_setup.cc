3d3706d88633567620128ba18f803e11cf2a478c
Yue Li <updowndota@chromium.org>
2019-08-06 23:23:29 +0000

    Remove Assistant hotword notification for user migration
    The notification is for AGSA migration and is confusing now.
    
    Bug: b/138612873
    Test: Manual Test
    Change-Id: Ib25d2133395a5a4a346d1fda47c0a1c8490e6e9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1740072
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684559}
    
    Former-commit-id: f7e09a71cfc60363201a0f82b4d69d012fdac6ee

--- chrome/browser/ui/ash/assistant/assistant_setup.cc
+++ chrome/browser/ui/ash/assistant/assistant_setup.cc
@@ -8,79 +8,22 @@
 #include <utility>
 
 #include "ash/public/cpp/notification_utils.h"
-#include "ash/public/cpp/vector_icons/vector_icons.h"
 #include "base/bind.h"
 #include "base/bind_helpers.h"
-#include "base/metrics/histogram_macros.h"
-#include "base/strings/utf_string_conversions.h"
 #include "base/threading/sequenced_task_runner_handle.h"
-#include "chrome/browser/chromeos/login/ui/login_display_host.h"
-#include "chrome/browser/notifications/notification_display_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/settings_window_manager_chromeos.h"
 #include "chrome/browser/ui/webui/chromeos/assistant_optin/assistant_optin_ui.h"
 #include "chrome/common/webui_url_constants.h"
-#include "chrome/grit/generated_resources.h"
 #include "chromeos/services/assistant/public/cpp/assistant_prefs.h"
 #include "chromeos/services/assistant/public/mojom/constants.mojom.h"
 #include "chromeos/services/assistant/public/proto/settings_ui.pb.h"
 #include "components/arc/arc_prefs.h"
 #include "components/prefs/pref_service.h"
-#include "components/user_manager/user_manager.h"
 #include "services/service_manager/public/cpp/connector.h"
-#include "ui/base/l10n/l10n_util.h"
-#include "ui/gfx/paint_vector_icon.h"
-#include "ui/message_center/public/cpp/notification.h"
-#include "ui/message_center/public/cpp/notification_delegate.h"
 
 using chromeos::assistant::ConsentFlowUi;
-
-namespace {
-
-constexpr char kAssistantDisplaySource[] = "Assistant";
-constexpr char kHotwordNotificationId[] = "assistant/hotword";
-constexpr char kNotifierAssistant[] = "assistant";
-
-// Delegate for assistant hotword notification.
-class AssistantHotwordNotificationDelegate
-    : public message_center::NotificationDelegate {
- public:
-  explicit AssistantHotwordNotificationDelegate(Profile* profile)
-      : profile_(profile) {}
-
-  // message_center::NotificationDelegate:
-  void Close(bool by_user) override {
-    if (!by_user)
-      return;
-
-    HandleHotwordEnableNotificationResult(false /* enable */);
-  }
-
-  void Click(const base::Optional<int>& button_index,
-             const base::Optional<base::string16>& reply) override {
-    HandleHotwordEnableNotificationResult(true /* enable */);
-    NotificationDisplayService::GetForProfile(profile_)->Close(
-        NotificationHandler::Type::TRANSIENT, kHotwordNotificationId);
-  }
-
- private:
-  ~AssistantHotwordNotificationDelegate() override = default;
-
-  void HandleHotwordEnableNotificationResult(bool enable) {
-    if (enable) {
-      chrome::SettingsWindowManager::GetInstance()->ShowOSSettings(
-          ProfileManager::GetActiveUserProfile(), chrome::kAssistantSubPage);
-    }
-    UMA_HISTOGRAM_BOOLEAN("Assistant.HotwordEnableNotification", enable);
-  }
-
-  Profile* profile_;
-
-  DISALLOW_COPY_AND_ASSIGN(AssistantHotwordNotificationDelegate);
-};
-
-}  // namespace
 
 AssistantSetup::AssistantSetup(service_manager::Connector* connector)
     : connector_(connector), weak_factory_(this) {
@@ -104,41 +47,6 @@ void AssistantSetup::OnStateChanged(ash::mojom::VoiceInteractionState state) {
   // Sync settings state when Assistant service started.
   if (!settings_manager_)
     SyncSettingsState();
-
-  // If the OOBE flow is active, no need to show the notification since it is
-  // included in the flow.
-  if (user_manager::UserManager::Get()->IsCurrentUserNew() &&
-      chromeos::LoginDisplayHost::default_host()) {
-    return;
-  }
-
-  Profile* profile = ProfileManager::GetActiveUserProfile();
-  PrefService* prefs = profile->GetPrefs();
-  if (!prefs->FindPreference(arc::prefs::kVoiceInteractionHotwordEnabled)
-           ->IsDefaultValue()) {
-    return;
-  }
-  // TODO(xiaohuic): need better ways to decide when to show the notification.
-  // Avoid the notification from keep showing up.
-  prefs->SetBoolean(arc::prefs::kVoiceInteractionHotwordEnabled, false);
-
-  const base::string16 title =
-      l10n_util::GetStringUTF16(IDS_ASSISTANT_HOTWORD_NOTIFICATION_TITLE);
-  const base::string16 display_source =
-      base::UTF8ToUTF16(kAssistantDisplaySource);
-
-  auto notification = ash::CreateSystemNotification(
-      message_center::NOTIFICATION_TYPE_SIMPLE, kHotwordNotificationId, title,
-      base::string16(), display_source, GURL(),
-      message_center::NotifierId(message_center::NotifierType::SYSTEM_COMPONENT,
-                                 kNotifierAssistant),
-      {}, base::MakeRefCounted<AssistantHotwordNotificationDelegate>(profile),
-      ash::kNotificationAssistantIcon,
-      message_center::SystemNotificationWarningLevel::NORMAL);
-
-  NotificationDisplayService::GetForProfile(profile)->Display(
-      NotificationHandler::Type::TRANSIENT, *notification,
-      /*metadata=*/nullptr);
 }
 
 void AssistantSetup::SyncSettingsState() {
