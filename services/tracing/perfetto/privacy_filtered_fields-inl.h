651cd09c722b565ef98516f5dda416acf38074c7
ssid <ssid@chromium.org>
2019-08-06 23:53:25 +0000

    Fix test failure caused by new trace stats field
    BUG=991282
    
    Change-Id: I7b815ed53acdec98ca7e8b1d50c92d92414988b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1740330
    Reviewed-by: ssid <ssid@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Commit-Queue: ssid <ssid@chromium.org>
    Commit-Queue: oysteine <oysteine@chromium.org>
    Auto-Submit: ssid <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684571}
    
    Former-commit-id: c0f5c13c0dbb0317f7d1c40addfe6783ff3025b1

--- services/tracing/perfetto/privacy_filtered_fields-inl.h
+++ services/tracing/perfetto/privacy_filtered_fields-inl.h
@@ -84,8 +84,8 @@ constexpr MessageInfo kInternedData = {kInternedDataIndices,
                                        kInternedDataComplexMessages};
 
 // Proto Message: BufferStats
-constexpr int kBufferStatsIndices[] = {1,  2,  3,  4,  5,  6,  7,  8,  9, 10,
-                                       11, 12, 13, 14, 15, 16, 17, 18, -1};
+constexpr int kBufferStatsIndices[] = {1,  2,  3,  4,  5,  6,  7,  8,  9,  10,
+                                       11, 12, 13, 14, 15, 16, 17, 18, 19, -1};
 constexpr MessageInfo kBufferStats = {kBufferStatsIndices, nullptr};
 
 // Proto Message: TraceStats
