b474c7f0e0cf7d35037d15b69a3f6b020ef92540
Ian Kilpatrick <ikilpatrick@chromium.org>
2019-08-07 22:54:38 +0000

    [cleanup] Remove dead V8ObjectParser code.
    Change-Id: I9a356248514602de265bfd3d575d09d76507528a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1739539
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#684991}
    
    Former-commit-id: 877ee29f73bf5fdb94404ecbd022a0f0308eca37

--- third_party/blink/renderer/bindings/core/v8/v8_object_parser.h
+++ third_party/blink/renderer/bindings/core/v8/v8_object_parser.h
@@ -19,29 +19,6 @@ class CORE_EXPORT V8ObjectParser final {
   STATIC_ONLY(V8ObjectParser);
 
  public:
-  // Retrieves the prototype object off a class constructor function. Returns
-  // true if the prototype exists, and is an object.
-  static bool ParsePrototype(v8::Local<v8::Context>,
-                             v8::Local<v8::Function> constructor,
-                             v8::Local<v8::Object>* prototype,
-                             ExceptionState*);
-
-  // Retrieves the specified function off a class prototype. Returns true if
-  // the object exists, and is a function.
-  static bool ParseFunction(v8::Local<v8::Context>,
-                            v8::Local<v8::Object> prototype,
-                            const AtomicString function_name,
-                            v8::Local<v8::Function>*,
-                            ExceptionState*);
-
-  // Retrieves the specified generator function off a class prototype. Returns
-  // true if the object exists, and is a generator function.
-  static bool ParseGeneratorFunction(v8::Local<v8::Context>,
-                                     v8::Local<v8::Object> prototype,
-                                     const AtomicString function_name,
-                                     v8::Local<v8::Function>*,
-                                     ExceptionState*);
-
   // Retrieves and parses a CSS property list off the class constructor
   // function. Returns true if the list exists, and successfully converted to a
   // Vector<String> type. It does not fail if the list contains invalid CSS
