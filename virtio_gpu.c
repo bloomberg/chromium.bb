9f7897f47f82790fbabbe36fe5515b5f050bcb74
David Stevens <stevensd@google.com>
2019-08-09 20:20:23 +0900

    minigbm: virtio_gpu: Add camera usage to all NV12 combinations
    This change adds camera usage to all NV12 combinations after all
    combinations have been added. This fixes an issue where a virtio_gpu
    with 3d support would not have any camera compatible NV12 combinations.
    
    BUG=b:132939858
    TEST=Camera apps run on arcvm with no gralloc errors (although they
    don't actually work for separate reasons).
    
    Change-Id: Icc868398d6e036f8430afb9a351809bb7965202e
    Reviewed-on: https://chromium-review.googlesource.com/1746346
    Tested-by: Tomasz Figa <tfiga@chromium.org>
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>

--- virtio_gpu.c
+++ virtio_gpu.c
@@ -218,13 +218,14 @@ static int virtio_gpu_init(struct driver *drv)
 				     ARRAY_SIZE(dumb_texture_source_formats), &LINEAR_METADATA,
 				     BO_USE_TEXTURE_MASK);
 		drv_add_combination(drv, DRM_FORMAT_NV12, &LINEAR_METADATA,
-				    BO_USE_CAMERA_READ | BO_USE_CAMERA_WRITE | BO_USE_SW_MASK |
-					BO_USE_LINEAR);
+				    BO_USE_SW_MASK | BO_USE_LINEAR);
 	}
 
 	/* Android CTS tests require this. */
 	drv_add_combination(drv, DRM_FORMAT_BGR888, &LINEAR_METADATA, BO_USE_SW_MASK);
 
+	drv_modify_combination(drv, DRM_FORMAT_NV12, &LINEAR_METADATA,
+			       BO_USE_CAMERA_READ | BO_USE_CAMERA_WRITE);
 	drv_modify_combination(drv, DRM_FORMAT_R8, &LINEAR_METADATA,
 			       BO_USE_CAMERA_READ | BO_USE_CAMERA_WRITE);
 
