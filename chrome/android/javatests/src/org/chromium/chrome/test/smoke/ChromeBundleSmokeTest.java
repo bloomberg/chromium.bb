fe076662f8e622e55cd4fed73cc218c298cafad3
Tibor Goldschwendt <tiborg@chromium.org>
2019-08-09 16:45:48 +0000

    [android] Add bundle smoke test executing test dummy module
    Adds a smoke test that has a Chrome bundle as the target. Sends an
    intent to Chrome so that Chrome installs the test dummy module and
    runs a test case.
    
    Bug: 989652
    Change-Id: If220f1892c509b2f78bde1ac87e5f2fe3ff502a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1728040
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Andrew Luo <aluo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685600}
    
    Former-commit-id: 8e1db3832accbf15d27737fa89b326c23804348d

---
+++ chrome/android/javatests/src/org/chromium/chrome/test/smoke/ChromeBundleSmokeTest.java
@@ -0,0 +1,64 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.test.smoke;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SmallTest;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+import org.junit.runner.RunWith;
+
+import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.chrome.test.pagecontroller.rules.ChromeUiApplicationTestRule;
+import org.chromium.chrome.test.pagecontroller.rules.ChromeUiAutomatorTestRule;
+import org.chromium.chrome.test.pagecontroller.utils.Ui2Locators;
+import org.chromium.chrome.test.pagecontroller.utils.UiAutomatorUtils;
+
+/** Smoke Test for Chrome bundles. */
+@SmallTest
+@RunWith(BaseJUnit4ClassRunner.class)
+public class ChromeBundleSmokeTest {
+    private static final String PACKAGE_NAME_ARG = "PackageUnderTest";
+    private static final String TARGET_ACTIVITY =
+            "org.chromium.chrome.features.test_dummy.TestDummyActivity";
+
+    public ChromeUiAutomatorTestRule mRule = new ChromeUiAutomatorTestRule();
+    public ChromeUiApplicationTestRule mChromeUiRule = new ChromeUiApplicationTestRule();
+    @Rule
+    public final TestRule mChain = RuleChain.outerRule(mChromeUiRule).around(mRule);
+
+    private String mPackageName;
+
+    @Before
+    public void setUp() {
+        // TODO (tiborg): Adjust this as needed according to https://crrev.com/c/1585142.
+        mPackageName = InstrumentationRegistry.getArguments().getString(PACKAGE_NAME_ARG);
+        Assert.assertNotNull("Must specify bundle under test", mPackageName);
+        mChromeUiRule.launchIntoNewTabPageOnFirstRun();
+    }
+
+    @Test
+    public void testModuleInstall() {
+        // Send intent that makes Chrome install the test dummy module.
+        Context context = InstrumentationRegistry.getContext();
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName(mPackageName, TARGET_ACTIVITY));
+        intent.putExtra("test_case", 0); // Test case EXECUTE_JAVA.
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+
+        // Wait for done dialog to show up.
+        Assert.assertTrue(UiAutomatorUtils.getInstance().getLocatorHelper().isOnScreen(
+                Ui2Locators.withText("Test Case 0: done")));
+    }
+}
