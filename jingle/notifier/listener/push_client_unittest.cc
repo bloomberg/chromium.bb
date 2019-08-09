dcdf0a8d0f0a53dbbcb6ba0a1e9293821dbf07a1
Carlos Caballero <carlscab@google.com>
2019-08-09 14:54:51 +0000

    Remove references of MessageLoopForIO in /jingle
    MessageLoopForIO will go away soon use ScopedTaskEnvironment instead.
    
    ScopedTaskEnvironment will per default start a ThreadPool, which should
    be fine in most of the cases. If you believe your test needs to make sure
    that no ThreadPool runs let me know and I will update the patch.
    
    BUG=891670
    This CL was uploaded by git cl split.
    
    R=zea@chromium.org
    
    Change-Id: I85626fd1fddb7d36b61b60574b5a6868630d4ed5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1733550
    Reviewed-by: Nicolas Zea <zea@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#685560}
    
    Former-commit-id: 9798801db56b94e1afc845750d95ace0a226f7a4

--- jingle/notifier/listener/push_client_unittest.cc
+++ jingle/notifier/listener/push_client_unittest.cc
@@ -10,8 +10,8 @@
 #include "base/bind_helpers.h"
 #include "base/compiler_specific.h"
 #include "base/location.h"
-#include "base/message_loop/message_loop.h"
 #include "base/single_thread_task_runner.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/threading/thread.h"
 #include "jingle/glue/network_service_config_test_util.h"
 #include "jingle/notifier/base/notifier_options.h"
@@ -27,14 +27,15 @@ class PushClientTest : public testing::Test {
   PushClientTest()
       : net_config_helper_(
             base::MakeRefCounted<net::TestURLRequestContextGetter>(
-                message_loop_.task_runner())) {
+                scoped_task_environment_.GetMainThreadTaskRunner())) {
     net_config_helper_.FillInNetworkConfig(&notifier_options_.network_config);
   }
 
   ~PushClientTest() override {}
 
   // The sockets created by the XMPP code expect an IO loop.
-  base::MessageLoopForIO message_loop_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_{
+      base::test::ScopedTaskEnvironment::MainThreadType::IO};
   jingle_glue::NetworkServiceConfigTestUtil net_config_helper_;
   NotifierOptions notifier_options_;
 };
