2dbaa7843ca9b62746acdd9f8d8e23a039727749
Sergey Ulanov <sergeyu@chromium.org>
2019-08-10 06:14:44 +0000

    [Fuchsia] Add FuchsiaVideoDecoder in WatchTimeRecodered
    WatchTimeRecorder was not recognizing FuchsiaVideoDecoder name, so it
    was logging warnings in debug builds. This change should solve this
    problem.
    
    Change-Id: Ib21a7d2c8a19363e2f84b60a483c759255524216
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1746632
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685855}
    
    Former-commit-id: 992d28de6ba32e2010c16ea6b4cb3c04e9f9542d

--- media/mojo/services/watch_time_recorder_unittest.cc
+++ media/mojo/services/watch_time_recorder_unittest.cc
@@ -1245,7 +1245,7 @@ TEST_F(WatchTimeRecorderTest, DISABLED_PrintExpectedDecoderNameHashes) {
       "FFmpegAudioDecoder", "FFmpegVideoDecoder",     "GpuVideoDecoder",
       "MojoVideoDecoder",   "MojoAudioDecoder",       "VpxVideoDecoder",
       "AomVideoDecoder",    "DecryptingAudioDecoder", "DecryptingVideoDecoder",
-      "Dav1dVideoDecoder"};
+      "Dav1dVideoDecoder",  "FuchsiaVideoDecoder"};
   printf("%18s = 0\n", "None");
   for (const auto& name : kDecoderNames)
     printf("%18s = 0x%x\n", name.c_str(), base::PersistentHash(name));
