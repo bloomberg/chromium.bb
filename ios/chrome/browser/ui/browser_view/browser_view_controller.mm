d702e27296a3c61cae7f90e79aba2a4651b25f12
Kurt Horimoto <kkhorimoto@chromium.org>
2019-08-10 00:02:19 +0000

    [iOS] Update FullscreenWebStateListObserver logic.
    The FullscreenWebStateListObserver's WebStateList is reset to nullptr
    when in the tab switcher, so it was missing changes to the active
    WebState that occurs when switching tabs using the tab grid.  This CL
    updates SetWebStateList() to move the page content below the header if
    the active WebState in the list has not previously been activated.
    
    Also in this CL:
    - Decomposes repeated FullscreenWebStateListObserver logic.
    - Sets the FullscreenController's WebStateList after updating broadcast
      values in BVC so that the toolbar insets are correct when the content
      is moved below the header.
    
    Bug: 992354
    Change-Id: I4ab9d2245ec662dcd167b5178d4abf2395889ed9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1745809
    Auto-Submit: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#685800}
    
    Former-commit-id: 58978f3386f4837e79fba852338cdacdc3bca4a0

--- ios/chrome/browser/ui/browser_view/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view/browser_view_controller.mm
@@ -956,8 +956,6 @@ NSString* const kBrowserViewControllerSnackbarCategory =
           _browserState);
   ChromeBroadcaster* broadcaster = fullscreenController->broadcaster();
   if (_broadcasting) {
-    fullscreenController->SetWebStateList(self.tabModel.webStateList);
-
     _toolbarUIUpdater = [[LegacyToolbarUIUpdater alloc]
         initWithToolbarUI:[[ToolbarUIState alloc] init]
              toolbarOwner:self
@@ -971,6 +969,8 @@ NSString* const kBrowserViewControllerSnackbarCategory =
         initWithUpdater:_mainContentUIUpdater
            webStateList:self.tabModel.webStateList];
     StartBroadcastingMainContentUI(self, broadcaster);
+
+    fullscreenController->SetWebStateList(self.tabModel.webStateList);
 
     _fullscreenUIUpdater = std::make_unique<FullscreenUIUpdater>(self);
     fullscreenController->AddObserver(_fullscreenUIUpdater.get());
