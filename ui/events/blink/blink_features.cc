83210c69c7c9501b84a84ff703bcc581f6047daf
Lan Wei <lanwei@chromium.org>
2019-08-13 23:11:09 +0000

    Revert "Disable flag "UpdateHoverAtBeginFrame""
    This reverts commit 4a6bdf9ade592daccb8e278ab208f4ca423426ad [formerly 95343512afe668473285c825323dade378c3ee3e].
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > Disable flag "UpdateHoverAtBeginFrame"
    > 
    > Blink.MainFrame.StyleAndLayoutRatio metrics has a significant
    > regression increase, we will disable flag "UpdateHoverAtBeginFrame" for
    > now in M78 to help identify what is the reason to cause this
    > regression.
    > 
    > TBR=bokan@chromium.org
    > 
    > Change-Id: Ic85d4c142a2a25928c3ec25a61de20649144ef50
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1733037
    > Reviewed-by: Lan Wei <lanwei@chromium.org>
    > Commit-Queue: Lan Wei <lanwei@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#684518}
    
    TBR=bokan@chromium.org,lanwei@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: Id6ad8e351ed45dc60d21984ef49bcbbe45dd5b2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1752765
    Reviewed-by: Lan Wei <lanwei@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686620}
    
    Former-commit-id: bf4664e7d16bed6245cc9b2d0d5b2c7321f91c6f

--- ui/events/blink/blink_features.cc
+++ ui/events/blink/blink_features.cc
@@ -16,7 +16,7 @@ const base::Feature kSendMouseLeaveEvents{"SendMouseLeaveEvents",
                                           base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kUpdateHoverAtBeginFrame{"UpdateHoverAtBeginFrame",
-                                             base::FEATURE_DISABLED_BY_DEFAULT};
+                                             base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kCompositorTouchAction{"CompositorTouchAction",
                                            base::FEATURE_ENABLED_BY_DEFAULT};
