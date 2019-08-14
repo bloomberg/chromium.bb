8d6e3490443cb7f17e2fced9712f781b471c7224
Antonio Gomes <tonikitoo@igalia.com>
2019-08-14 02:57:45 +0000

    Remove PostCrossThreadBind variant that operates over repeating callbacks
    All users have been switched to using the variant of PostCrossThreadTask
    and PostDelayedCrossThreadTask that operate on WTF's once-callbacks
    (aka CrossThreadOnceFunction).
    
    BUG=963574
    R=haraken@chromium.org, hiroshige@chromium.org
    
    Change-Id: I2ff3732924192f910ae26b810353efa84eafaf0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1626489
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#686681}
    
    Former-commit-id: e553e089386975d1d96978484dcc8fcca3948df5

--- third_party/blink/renderer/platform/scheduler/public/post_cross_thread_task.h
+++ third_party/blink/renderer/platform/scheduler/public/post_cross_thread_task.h
@@ -16,24 +16,9 @@ namespace blink {
 // For cross-thread posting. Can be called from any thread.
 inline void PostCrossThreadTask(base::SequencedTaskRunner& task_runner,
                                 const base::Location& location,
-                                WTF::CrossThreadClosure task) {
-  task_runner.PostDelayedTask(location, ConvertToBaseCallback(std::move(task)),
-                              base::TimeDelta());
-}
-
-inline void PostCrossThreadTask(base::SequencedTaskRunner& task_runner,
-                                const base::Location& location,
                                 WTF::CrossThreadOnceClosure task) {
   task_runner.PostDelayedTask(
       location, ConvertToBaseOnceCallback(std::move(task)), base::TimeDelta());
-}
-
-inline void PostDelayedCrossThreadTask(base::SequencedTaskRunner& task_runner,
-                                       const base::Location& location,
-                                       WTF::CrossThreadClosure task,
-                                       base::TimeDelta delay) {
-  task_runner.PostDelayedTask(location, ConvertToBaseCallback(std::move(task)),
-                              delay);
 }
 
 inline void PostDelayedCrossThreadTask(base::SequencedTaskRunner& task_runner,
