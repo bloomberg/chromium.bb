951febc81a84d5f4fbdec8f72c3c6e66cf60e97a
Nicolas Arciniega <niarci@microsoft.com>
2019-08-14 19:03:47 +0000

    Add test for Group Policy: RequireOnlineRevocationChecksForLocalAnchors
    This does not directly test the effects of setting revocation checks in
    the network service. The mojo pipe as well as direct testing of this
    behavior occurs in other code. Instead, this test ensures that
    prefs::kCertRevocationCheckingRequiredLocalAnchors, which maps to this
    group policy, gets picked up by the SSLConfig used to create and update
    the network service.
    
    Bug: 981598
    Change-Id: I8c363f6359bfbe25b9b79eadf82dd49abb9c00ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1745712
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Commit-Queue: Nicolas Arciniega <niarci@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#686926}
    
    Former-commit-id: 29e361565c353f1df49d235e397c1483f87add62

--- chrome/browser/ssl/ssl_config_service_manager_pref_unittest.cc
+++ chrome/browser/ssl/ssl_config_service_manager_pref_unittest.cc
@@ -271,3 +271,29 @@ TEST_F(SSLConfigServiceManagerPrefTest, H2ClientCertCoalescingPref) {
   EXPECT_EQ("127.0.0.1", observed_patterns[3]);
   EXPECT_EQ("128.2.1.10", observed_patterns[4]);
 }
+
+// Tests that the cert revocation checking pref correctly sets the corresponding
+// value in SSL configs.
+TEST_F(SSLConfigServiceManagerPrefTest,
+       RequireOnlineRevocationChecksForLocalAnchors) {
+  scoped_refptr<TestingPrefStore> local_state_store(new TestingPrefStore());
+
+  TestingPrefServiceSimple local_state;
+  local_state.SetUserPref(prefs::kCertRevocationCheckingRequiredLocalAnchors,
+                          std::make_unique<base::Value>(false));
+  SSLConfigServiceManager::RegisterPrefs(local_state.registry());
+
+  std::unique_ptr<SSLConfigServiceManager> config_manager =
+      SetUpConfigServiceManager(&local_state);
+
+  EXPECT_FALSE(initial_config_->rev_checking_required_local_anchors);
+
+  local_state.SetUserPref(prefs::kCertRevocationCheckingRequiredLocalAnchors,
+                          std::make_unique<base::Value>(true));
+
+  // Wait for the SSLConfigServiceManagerPref to be notified of the preferences
+  // being changed, and for it to notify the test fixture of the change.
+  ASSERT_NO_FATAL_FAILURE(WaitForUpdate());
+
+  EXPECT_TRUE(observed_configs_[0]->rev_checking_required_local_anchors);
+}
