0f923a3f255c79307fc8354aaacdc6470fb3fd0d
Xi Han <hanxi@google.com>
2019-08-14 13:56:26 +0000

    Fix ServicificationBackgroundServiceTest::testHistogramsPersistedWithServiceManagerOnlyStart fails on official bot.
    The test fails on the official bot due to the active field trial group size is
    0. In this CL, we force a field trial "Foo/Bar" in the test. Thus, it guarantees
    at least there is one active field trial.
    
    (cherry picked from commit 47c7c563c319b2677cc2f9d5bc8d312839dc4ddc [formerly a0fb7719ffd56f6ca36dd111349f612da8f37b85])
    
    Bug: 965482
    Change-Id: Icdd84dd2e4ff22a17da3d7a7c9dd048529ac9ae0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1752245
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#686456}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1753544
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3865@{#386}
    Cr-Branched-From: 4e0147fd4ab868c78ff6bb1ef42f86871cde1461 [formerly 0cdcc6158160790658d1f033d3db873603250124]-refs/heads/master@{#681094}
    
    Former-commit-id: d195e23720d6e4db80f1b2ce8ef74abb57710e3c

--- chrome/browser/android/servicification_background_service_jni.cc
+++ chrome/browser/android/servicification_background_service_jni.cc
@@ -52,36 +52,58 @@ JNI_ServicificationBackgroundService_TestPersistentHistogramsOnDiskSystemProfile
           std::move(mapped), 0, 0, base::StringPiece(), /* read_only */ true);
   if (memory_allocator->GetMemoryState() ==
       base::PersistentMemoryAllocator::MEMORY_DELETED) {
+    LOG(ERROR) << "The memory allocator state shouldn't be MEMORY_DELETED!";
     return false;
   }
-  if (memory_allocator->IsCorrupt())
-    return false;
 
-  if (!metrics::PersistentSystemProfile::HasSystemProfile(*memory_allocator))
+  if (memory_allocator->IsCorrupt()) {
+    LOG(ERROR) << "The memory allocator is corrupt!";
     return false;
+  }
+
+  if (!metrics::PersistentSystemProfile::HasSystemProfile(*memory_allocator)) {
+    LOG(ERROR) << "There isn't a System Profile!";
+    return false;
+  }
 
   metrics::SystemProfileProto system_profile_proto;
   if (!metrics::PersistentSystemProfile::GetSystemProfile(
-          *memory_allocator, &system_profile_proto))
+          *memory_allocator, &system_profile_proto)) {
+    LOG(ERROR) << "Failed to get the System Profile!";
     return false;
+  }
 
-  if (!system_profile_proto.has_os())
+  if (!system_profile_proto.has_os()) {
+    LOG(ERROR) << "The os info isn't set!";
     return false;
+  }
 
   const metrics::SystemProfileProto_OS& os = system_profile_proto.os();
-  if (!os.has_version())
+  if (!os.has_version()) {
+    LOG(ERROR) << "The os version isn't set!";
     return false;
+  }
 
-  if (base::SysInfo::OperatingSystemVersion().compare(os.version()) != 0)
+  if (base::SysInfo::OperatingSystemVersion().compare(os.version()) != 0) {
+    LOG(ERROR) << "The os version doesn't match!";
     return false;
+  }
 
   std::vector<variations::ActiveGroupId> field_trial_ids;
   variations::GetFieldTrialActiveGroupIds("", &field_trial_ids);
+  int expected_size = static_cast<int>(field_trial_ids.size());
 
-  int expeceted_size = static_cast<int>(field_trial_ids.size());
-  // The active field trial "PersistentHistograms" is guaranteed in the list.
-  if (expeceted_size <= 0)
+  // The active field trial "Foo" is guaranteed in the list.
+  if (expected_size <= 0) {
+    LOG(ERROR) << "Expect at least one active field trial!";
     return false;
+  }
 
-  return system_profile_proto.field_trial_size() == expeceted_size;
+  if (system_profile_proto.field_trial_size() != expected_size) {
+    LOG(ERROR) << "The size of field trials recorded in the System Profile"
+               << " doesn't match the size of active field trials!";
+    return false;
+  }
+
+  return true;
 }
