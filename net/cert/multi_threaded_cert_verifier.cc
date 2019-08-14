088232e4625c0d95c06c4493db35c9b8ccf2a26f
Matt Mueller <mattm@chromium.org>
2019-08-14 23:27:37 +0000

    [net] Make CertVerifyResult subclass SupportsUserData.
    CertVerifyProc implementations can use this to add arbitrary extra
    debug information onto the result.
    
    Bug: 991247
    Change-Id: Iea2447ef513ab3b7be7fcd45836a84f4d16dc438
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1738630
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687064}
    
    Former-commit-id: cc42ea10d370cf6a3e0763452d92aae1251002dc

--- net/cert/multi_threaded_cert_verifier.cc
+++ net/cert/multi_threaded_cert_verifier.cc
@@ -203,6 +203,10 @@ std::unique_ptr<ResultHelper> DoVerifyOnWorkerThread(
   verify_result->error = verify_proc->Verify(
       cert.get(), hostname, ocsp_response, sct_list, flags, crl_set.get(),
       additional_trust_anchors, &verify_result->result);
+  // The CertVerifyResult is created and populated on the worker thread and
+  // then returned to the network thread. Detach now before returning the
+  // result, since any further access will be on the network thread.
+  verify_result->result.DetachFromSequence();
   return verify_result;
 }
 
