34f051dfe57169749e96ffd75b423702faf9010a
Adam Rice <ricea@chromium.org>
2019-08-15 08:51:13 +0000

    Enable "StreamsNative" by default
    In preparation for launch of the C++ implementation of WHATWG streams,
    enable the "StreamsNative" feature flag by default.
    
    The flag will be removed, and the old JavaScript implementation will be
    deleted, in approximately 6 weeks' time.
    
    BUG=902633
    
    Change-Id: Iaa6fd4d436675eff5e032c9652b04ced6473a0a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1745683
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687199}
    
    Former-commit-id: 3c4fc7be009090e3ec0cb89f19e86edf02878035

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -339,7 +339,7 @@ const base::Feature kCanvasAlwaysDeferral{"CanvasAlwaysDeferral",
 // Use the new C++ implementation of WHATWG Streams. See
 // https://crbug.com/977500.
 const base::Feature kStreamsNative{"StreamsNative",
-                                   base::FEATURE_DISABLED_BY_DEFAULT};
+                                   base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Blink garbage collection.
 // Enables compaction of backing stores on Blink's heap.
