f20ea4056c2610f3d52adb5aba7566835d142fea
Ali Juma <ajuma@chromium.org>
2019-08-15 18:19:02 +0000

    Make cache_egtest.mm EG2 compatible
    This adds the file as an EG2 target, and updates #includes.
    
    Bug: 987646
    Change-Id: Iece7bdc789b49e36443566f6fc00fb9be6902fa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1756213
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687347}
    
    Former-commit-id: 0fc9202db4f967ed4094bb9e69f888654649ff60

--- ios/chrome/browser/web/cache_egtest.mm
+++ ios/chrome/browser/web/cache_egtest.mm
@@ -2,19 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <EarlGrey/EarlGrey.h>
-
 #include <memory>
 
 #include "base/ios/ios_util.h"
 #include "base/strings/stringprintf.h"
-#include "ios/chrome/browser/ui/util/ui_util.h"
-#import "ios/chrome/test/app/chrome_test_util.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #include "ios/chrome/test/earl_grey/scoped_block_popups_pref.h"
+#import "ios/testing/earl_grey/earl_grey_test.h"
 #include "ios/web/public/test/http_server/html_response_provider.h"
 #import "ios/web/public/test/http_server/http_server.h"
 #include "ios/web/public/test/http_server/http_server_util.h"
@@ -24,7 +21,6 @@
 #error "This file requires ARC support."
 #endif
 
-using chrome_test_util::GetOriginalBrowserState;
 using web::test::HttpServer;
 
 namespace {
