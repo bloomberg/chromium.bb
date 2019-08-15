e43fd9e2d6a2de5e1e2436cba4dd92bd1cd0d9da
Kai Ninomiya <kainino@chromium.org>
2019-08-15 13:43:56 -0700

    babel: filter out tslint comments

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['@babel/preset-typescript'],
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-syntax-dynamic-import',
+      'macros',
+      [
+        'add-header-comment',
+        {
+          header: ['AUTO-GENERATED - DO NOT EDIT. Source: https://github.com/gpuweb/cts'],
+        },
+      ],
+    ],
+    compact: false,
+    shouldPrintComment: val => !/tslint:/.test(val),
+  };
+};
