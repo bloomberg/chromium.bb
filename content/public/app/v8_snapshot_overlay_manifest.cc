579b1b5a5f7d006795d7f5f6e6e2ac93dee96494
Jinsong Fan <fanjinsong@sogou-inc.com>
2019-08-16 00:39:11 +0000

    Fix warning: Ignoring invalid file assets/snapshot_blob_32|64.bin
    On Android, V8 will need a snapshot file to speed up initialization, the
    Ninja will generate different snapshot files according to 32-bit or 64-bit
    systems, and only one of them is included in the Chrome apk. Therefore,
    only need to give the service access to the preloaded snapshot file it needs
    on startup.
    
    Bug: None
    Change-Id: Ib5a1d0733d073c6b6d657c6b6a1e32df32d573ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1753985
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Jinsong Fan <fanjinsong@sogou-inc.com>
    Cr-Commit-Position: refs/heads/master@{#687513}
    
    Former-commit-id: 516c53cdd7941fd39abc6264ab63df9d0337a293

--- content/public/app/v8_snapshot_overlay_manifest.cc
+++ content/public/app/v8_snapshot_overlay_manifest.cc
@@ -31,19 +31,25 @@ const service_manager::Manifest& GetV8SnapshotOverlayManifest() {
             kV8NativesDataDescriptor,
             base::FilePath(FILE_PATH_LITERAL("assets/natives_blob.bin")))
 #if defined(USE_V8_CONTEXT_SNAPSHOT)
-        .PreloadFile(kV8Snapshot32DataDescriptor,
-                     base::FilePath(FILE_PATH_LITERAL(
-                         "assets/v8_context_snapshot_32.bin")))
+#if defined(ARCH_CPU_64_BITS)
         .PreloadFile(kV8Snapshot64DataDescriptor,
                      base::FilePath(FILE_PATH_LITERAL(
                          "assets/v8_context_snapshot_64.bin")))
 #else
-        .PreloadFile(
-            kV8Snapshot32DataDescriptor,
-            base::FilePath(FILE_PATH_LITERAL("assets/snapshot_blob_32.bin")))
+        .PreloadFile(kV8Snapshot32DataDescriptor,
+                     base::FilePath(FILE_PATH_LITERAL(
+                         "assets/v8_context_snapshot_32.bin")))
+#endif
+#else
+#if defined(ARCH_CPU_64_BITS)
         .PreloadFile(
             kV8Snapshot64DataDescriptor,
             base::FilePath(FILE_PATH_LITERAL("assets/snapshot_blob_64.bin")))
+#else
+        .PreloadFile(
+            kV8Snapshot32DataDescriptor,
+            base::FilePath(FILE_PATH_LITERAL("assets/snapshot_blob_32.bin")))
+#endif
 #endif  // defined(USE_V8_CONTEXT_SNAPSHOT)
 #endif
         .Build()
