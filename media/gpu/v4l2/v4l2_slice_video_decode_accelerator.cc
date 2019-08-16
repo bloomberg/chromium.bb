379dd7297a2f63755ea4853cb485fce0966c96a2
Francois Buergisser <fbuergisser@chromium.org>
2019-08-16 06:05:03 +0000

    media/gpu/v4l2svda: Set picture size to OUTPUT buffer
    Currently, the picture size is set to the CAPTURE buffer but CAPTURE buffer
    references the picture size of the OUTPUT buffer.
    If the picture size if set to CAPTURE, Hantro will ignore it and use the
    OUTPUT picture size which is not set, therefore using the minimum size
    available (48x48).
    This patch sets the picture size to the OUTPUT buffer instead of the
    CAPTURE buffer following the initialization specification for m2m stateless
    video decoder [1].
    
    [1] https://hverkuil.home.xs4all.nl/codec-api/uapi/v4l/dev-stateless-decoder.html#initialization
    
    Bug: 994077
    Test: VDA H264 unittest with Hantro.
    Change-Id: I42c82c5e814f99fa027ef612cb95b1d025e9c393
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1755702
    Commit-Queue: Francois Buergisser <fbuergisser@chromium.org>
    Reviewed-by: Alexandre Courbot <acourbot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687579}
    
    Former-commit-id: 28ae19c277431494a34a8f4e6d4d39e47a9a06a5

--- media/gpu/v4l2/v4l2_slice_video_decode_accelerator.cc
+++ media/gpu/v4l2/v4l2_slice_video_decode_accelerator.cc
@@ -532,18 +532,32 @@ bool V4L2SliceVideoDecodeAccelerator::CreateOutputBuffers() {
   DCHECK_GT(num_pictures, 0u);
   DCHECK(!pic_size.IsEmpty());
 
-  // Since VdaVideoDeecoder doesn't allocate PictureBuffer with size adjusted by
+  // Since VdaVideoDecoder doesn't allocate PictureBuffer with size adjusted by
   // itself, we have to adjust here.
   struct v4l2_format format;
   memset(&format, 0, sizeof(format));
-  format.type = V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE;
-  format.fmt.pix_mp.pixelformat = output_format_fourcc_;
+  format.type = V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE;
+
+  if (device_->Ioctl(VIDIOC_G_FMT, &format) != 0) {
+    VPLOGF(1) << "Failed getting OUTPUT format";
+    NOTIFY_ERROR(PLATFORM_FAILURE);
+    return false;
+  }
+
   format.fmt.pix_mp.width = pic_size.width();
   format.fmt.pix_mp.height = pic_size.height();
-  format.fmt.pix_mp.num_planes = output_planes_count_;
 
   if (device_->Ioctl(VIDIOC_S_FMT, &format) != 0) {
-    VPLOGF(1) << "Failed setting format to: " << output_format_fourcc_;
+    VPLOGF(1) << "Failed setting OUTPUT format to: " << input_format_fourcc_;
+    NOTIFY_ERROR(PLATFORM_FAILURE);
+    return false;
+  }
+
+  // Get the coded size from the CAPTURE queue
+  memset(&format, 0, sizeof(format));
+  format.type = V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE;
+  if (device_->Ioctl(VIDIOC_G_FMT, &format) != 0) {
+    VPLOGF(1) << "Failed getting CAPTURE format";
     NOTIFY_ERROR(PLATFORM_FAILURE);
     return false;
   }
