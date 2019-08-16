399424f71b842a0e5e3a99a282427e57fd31855b
Tomasz Wiszkowski <ender@google.com>
2019-08-16 00:27:54 +0000

    Introducing Base suggestion component.
    This change introduces a base suggestion component, that currently
    decorates Omnibox suggestion with a "refine" icon (↖).
    
    This component will also be responsible for presenting decorative icon
    beside the suggestion.
    
    The component is currently not attached to any code.
    
    Change-Id: Ie286ff5d7f8e7881a03df75978849a131297e46d
    Doc: http://doc/1aL_UcW1gdeSLqNzuJZlQPBqMG0XAwVaoa05UTyVulp8#heading=h.6a92f2gfl9le
    Bug: 982818
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1695993
    Commit-Queue: Ender <ender@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#687509}
    
    Former-commit-id: 2faf9ec31d44afda13a9f3b9e6ccfb818b16b54f

---
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionViewBinder.java
@@ -0,0 +1,37 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.omnibox.suggestions.base;
+
+import android.support.annotation.CallSuper;
+import android.support.v4.view.ViewCompat;
+
+import org.chromium.chrome.browser.omnibox.suggestions.SuggestionCommonProperties;
+import org.chromium.chrome.browser.util.ColorUtils;
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel;
+import org.chromium.ui.modelutil.PropertyModelChangeProcessor.ViewBinder;
+
+/**
+ * Binds base suggestion view properties.
+ */
+public class BaseSuggestionViewBinder
+        implements ViewBinder<PropertyModel, BaseSuggestionView, PropertyKey> {
+    @Override
+    @CallSuper
+    public void bind(PropertyModel model, BaseSuggestionView view, PropertyKey propertyKey) {
+        if (BaseSuggestionViewProperties.SUGGESTION_DELEGATE == propertyKey) {
+            view.setDelegate(model.get(BaseSuggestionViewProperties.SUGGESTION_DELEGATE));
+        } else if (BaseSuggestionViewProperties.REFINE_VISIBLE == propertyKey) {
+            view.setRefineVisible(model.get(BaseSuggestionViewProperties.REFINE_VISIBLE));
+        } else if (SuggestionCommonProperties.LAYOUT_DIRECTION == propertyKey) {
+            ViewCompat.setLayoutDirection(
+                    view, model.get(SuggestionCommonProperties.LAYOUT_DIRECTION));
+        } else if (SuggestionCommonProperties.USE_DARK_COLORS == propertyKey) {
+            boolean useDarkColors = model.get(SuggestionCommonProperties.USE_DARK_COLORS);
+            int tint = ColorUtils.getIconTint(view.getContext(), !useDarkColors).getDefaultColor();
+            view.updateIconTint(tint);
+        }
+    }
+}
