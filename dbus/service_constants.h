a9e6090ac59d024aa9687f51f6c2191990cdbe3b
Allen Webb <allenwebb@google.com>
2019-08-19 09:53:46 -0700

    system_api: Add D-Bus constants for usbguard device notifications.
    This adds D-Bus constants defined by upstream usbguard that are needed
    to display lock-screen notifications for blocked devices.
    
    BUG=chromium:987351
    TEST=PreCQ Passes
    
    Change-Id: I1f8c510f11d3e562b2707340017664ef85d12be4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1760322
    Tested-by: Allen Webb <allenwebb@google.com>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Commit-Queue: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Cr-Mirrored-From: https://chromium.googlesource.com/chromiumos/platform2
    Cr-Mirrored-Commit: 8752ad3624bc0d1cb7809e94b3927aed4c37cfc5

--- dbus/service_constants.h
+++ dbus/service_constants.h
@@ -32,6 +32,7 @@
 #include "shill/dbus-constants.h"
 #include "smbprovider/dbus-constants.h"
 #include "update_engine/dbus-constants.h"
+#include "usbguard/dbus-constants.h"
 #include "vm_applications/dbus-constants.h"
 #include "vm_cicerone/dbus-constants.h"
 #include "vm_concierge/dbus-constants.h"
