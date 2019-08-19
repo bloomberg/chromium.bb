94be07b6235ed6f815e56713876e3c0ff4e17c61
Alexey Baskakov <loyso@chromium.org>
2019-08-19 07:51:24 +0000

    Sheriff: Disable flaky BrowserFocusTest.TabsRememberFocus on Linux TSan.
    TBR=tapted@chromium.org
    
    Bug: 995181
    Change-Id: I53d37e56cd1a0b8992ef62e504d3e78e3ecd2d60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1760628
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688028}
    
    Former-commit-id: 08fd6cb64505986855bc7cb1394004467c8d02df

--- chrome/browser/ui/browser_focus_uitest.cc
+++ chrome/browser/ui/browser_focus_uitest.cc
@@ -262,7 +262,13 @@ IN_PROC_BROWSER_TEST_F(BrowserFocusTest, BrowsersRememberFocus) {
 }
 
 // Tabs remember focus.
-IN_PROC_BROWSER_TEST_F(BrowserFocusTest, TabsRememberFocus) {
+#if defined(THREAD_SANITIZER)
+// TODO(crbug.com/995181): This test is flaky on Linux TSan.
+#define MAYBE_TabsRememberFocus DISABLED_TabsRememberFocus
+#else
+#define MAYBE_TabsRememberFocus TabsRememberFocus
+#endif
+IN_PROC_BROWSER_TEST_F(BrowserFocusTest, MAYBE_TabsRememberFocus) {
   ASSERT_TRUE(ui_test_utils::BringBrowserWindowToFront(browser()));
   const GURL url = embedded_test_server()->GetURL(kSimplePage);
   ui_test_utils::NavigateToURL(browser(), url);
