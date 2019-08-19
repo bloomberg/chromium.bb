d77ed6c792ce3519bead7e6c2c33675cfbb3b8e1
Robbie Gibson <rkgibson@google.com>
2019-08-19 10:02:05 +0000

    [iOS][Dark mode] Add dark mode support to download manager
    Bug: 976674
    Change-Id: I4a8c084c66cbaa77445124affa35ef879286d2df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1720710
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688050}
    
    Former-commit-id: c383ba05c5f945b5e0721341c9f86a52d7bfea4d

--- ios/chrome/browser/ui/download/download_manager_state_view.mm
+++ ios/chrome/browser/ui/download/download_manager_state_view.mm
@@ -60,6 +60,11 @@ const CGFloat kInProgressScale = 0.65f;
   return CGSizeMake(kViewSize, kViewSize);
 }
 
+- (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
+  [super traitCollectionDidChange:previousTraitCollection];
+  [self updateUIAnimated:NO];
+}
+
 #pragma mark - Public
 
 - (void)setState:(DownloadManagerState)state {
@@ -86,6 +91,21 @@ const CGFloat kInProgressScale = 0.65f;
 
 // Updates CoreAnimation layers (icon and badge).
 - (void)updateUIAnimated:(BOOL)animated {
+#if defined(__IPHONE_13_0) && (__IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_13_0)
+  if (@available(iOS 13, *)) {
+    [self.traitCollection performAsCurrentTraitCollection:^{
+      [self updateUIWithCurrentTraitCollectionAnimated:animated];
+    }];
+    return;
+  }
+#endif
+  [self updateUIWithCurrentTraitCollectionAnimated:animated];
+}
+
+// Updates the CoreAnimation layers assuming currentTraitCollection is set
+// correctly, so any .CGColor property returns the correct color for the current
+// mode (light vs. dark).
+- (void)updateUIWithCurrentTraitCollectionAnimated:(BOOL)animated {
   NSTimeInterval animationDuration =
       animated ? kDownloadManagerAnimationDuration : 0.0;
 
