44c1bdca1587543421c92e79680db04481d43ad2
Hiroshige Hayashizaki <hiroshige@chromium.org>
2019-08-20 09:16:06 +0000

    [Import Maps] Reject values not ending with "/" for keys ending with "/"
    Bug: 990561
    Change-Id: Id9eae41b435aca6ce97a2fa5317d63e5b0d52d86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1734210
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688453}
    
    Former-commit-id: 93a9bfed0e1810b67aab3b10e7b6b24a920b631c

--- third_party/blink/renderer/core/script/import_map.cc
+++ third_party/blink/renderer/core/script/import_map.cc
@@ -117,8 +117,18 @@ KURL NormalizeValue(const String& key,
     case ParsedSpecifier::Type::kURL:
       // <spec step="3.3.4">If specifierKey ends with U+002F (/), and the
       // serialization of addressURL does not end with U+002F (/), then:</spec>
-      //
-      // TODO(hiroshige): Implement this.
+      if (key.EndsWith("/") && !value.GetUrl().GetString().EndsWith("/")) {
+        // <spec step="3.3.4.1">Report a warning to the console that an invalid
+        // address was given for the specifier key specifierKey; since
+        // specifierKey ended in a slash, so must the address.</spec>
+        AddIgnoredValueMessage(
+            logger, key,
+            "Since specifierKey ended in a slash, so must the address: " +
+                value_string);
+
+        // <spec step="3.3.4.2">Continue.</spec>
+        return NullURL();
+      }
 
       // <spec step="3.3.5">Append addressURL to
       // validNormalizedAddresses.</spec>
@@ -241,7 +251,7 @@ ImportMap::SpecifierMap ImportMap::SortAndNormalizeSpecifierMap(
 
       case JSONValue::ValueType::kTypeString: {
         // <spec step="2.3">If value is a string, then set
-        // normalized[normalizedSpecifierKey] to ≪value≫.</spec>
+        // normalized[normalizedSpecifierKey] to «value».</spec>
         String value_string;
         if (!imports->GetString(entry.first, &value_string)) {
           AddIgnoredValueMessage(logger, entry.first,
