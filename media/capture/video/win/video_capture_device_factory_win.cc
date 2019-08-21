e17df6e55fb5dd52b3ec967bac9267163b205f78
Guido Urdaneta <guidou@chromium.org>
2019-08-21 16:14:43 +0000

    Update MediaFoundation camera blacklist
    Add camera 05c8:0369 known to cause problems with MediaFoundation.
    
    Bug: 978852
    Change-Id: Idb4b02e04514affa9e0d429aaf85b310435b2fa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1757937
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Marina Ciocea <marinaciocea@chromium.org>
    Auto-Submit: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Marina Ciocea <marinaciocea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689014}
    
    Former-commit-id: a2ca8d5f61f30f47388573678bfa4ce10e199a49

--- media/capture/video/win/video_capture_device_factory_win.cc
+++ media/capture/video/win/video_capture_device_factory_win.cc
@@ -92,7 +92,7 @@ const char* const kModelIdsBlacklistedForMediaFoundation[] = {
     // also https://crbug.com/924528
     "04ca:7047", "04ca:7048",
     // HP Elitebook 840 G1
-    "04f2:b3ed", "04f2:b3ca", "05c8:035d",
+    "04f2:b3ed", "04f2:b3ca", "05c8:035d", "05c8:0369",
     // RBG/IR camera for Windows Hello Face Auth. See https://crbug.com/984864.
     "13d3:5257"};
 
