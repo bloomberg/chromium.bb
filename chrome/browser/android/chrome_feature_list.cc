0e0d86fc92102d5c98971d51c7dc3a85475742fa
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2019-08-21 00:11:21 +0000

    Enable flag TabGroupsUiImprovementsAndroid by default
    Bug: 956205
    Change-Id: I632d5f303fe9bb11b7c9e80f643b65e52ac56c63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1742524
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688810}
    
    Former-commit-id: 4d9e93edfa0e5a6e3c51baa8b6d49ed5cfcbb418

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -533,7 +533,7 @@ const base::Feature kTabGroupsAndroid{"TabGroupsAndroid",
                                       base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kTabGroupsUiImprovementsAndroid{
-    "TabGroupsUiImprovementsAndroid", base::FEATURE_DISABLED_BY_DEFAULT};
+    "TabGroupsUiImprovementsAndroid", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kTabGridLayoutAndroid{"TabGridLayoutAndroid",
                                           base::FEATURE_DISABLED_BY_DEFAULT};
