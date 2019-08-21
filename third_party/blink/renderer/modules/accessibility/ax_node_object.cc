21458488aa9ada980873da8df81ba350a6c5efb0
Alice Boxhall <aboxhall@chromium.org>
2019-08-21 11:30:32 +0000

    Fix duplicate text in accessibility tree caused by multiple text nodes
    Bug: 995603
    Change-Id: I7e402104a729aa0fdaf525530be98f00b239f123
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1763547
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#688947}
    
    Former-commit-id: 3fe481c20e0479dea71f5a7c7ff2243c27dee3a2

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -2038,7 +2038,7 @@ String AXNodeObject::TextAlternative(bool recursive,
       }
 
       if (auto* text_node = DynamicTo<Text>(node))
-        text_alternative = text_node->wholeText();
+        text_alternative = text_node->data();
       else if (IsA<HTMLBRElement>(node))
         text_alternative = String("\n");
       else
