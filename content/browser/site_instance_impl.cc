4899dd87982cd4bcf4d0bbcdef8a5753dec0bfb7
Aaron Colwell <acolwell@google.com>
2019-08-22 18:25:12 +0000

    Require dedicated process for all WebUI schemes.
    This changes SiteInstanceImpl::DoesSiteURLRequireDedicatedProcess() to
    return true for all WebUI schemes instead of just singling out the
    chrome: scheme. This ensures that these URLs get placed in dedicated
    processes even if site isolation is disabled.
    
    Bug: 991153,991888
    Change-Id: I1af3b87ac39d93f6e45587a5b3845a176f98b7bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1763643
    Commit-Queue: Aaron Colwell <acolwell@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689561}
    
    Former-commit-id: 7be7426134cc4978a253f3be6dcdbf77ee25702f

--- content/browser/site_instance_impl.cc
+++ content/browser/site_instance_impl.cc
@@ -18,6 +18,7 @@
 #include "content/browser/isolation_context.h"
 #include "content/browser/renderer_host/render_process_host_impl.h"
 #include "content/browser/storage_partition_impl.h"
+#include "content/browser/webui/url_data_manager_backend.h"
 #include "content/public/browser/browser_or_resource_context.h"
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/render_process_host_factory.h"
@@ -905,10 +906,11 @@ bool SiteInstanceImpl::DoesSiteURLRequireDedicatedProcess(
   if (site_url.SchemeIs(kChromeErrorScheme))
     return true;
 
-  // Isolate kChromeUIScheme pages from one another and from other kinds of
-  // schemes.
-  if (site_url.SchemeIs(content::kChromeUIScheme))
-    return true;
+  // Isolate WebUI pages from one another and from other kinds of schemes.
+  for (const auto& webui_scheme : URLDataManagerBackend::GetWebUISchemes()) {
+    if (site_url.SchemeIs(webui_scheme))
+      return true;
+  }
 
   // Let the content embedder enable site isolation for specific URLs. Use the
   // canonical site url for this check, so that schemes with nested origins
