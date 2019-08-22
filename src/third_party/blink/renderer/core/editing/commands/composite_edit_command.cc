be2279ba28894c082697edddb686d02627f414fa
Zhen Yin <zyin@bloomberg.net>
2019-08-22 16:00:32 -0400

    feature/indent-outdent-block: fix more cr76 build error (#602)

--- src/third_party/blink/renderer/core/editing/commands/composite_edit_command.cc
+++ src/third_party/blink/renderer/core/editing/commands/composite_edit_command.cc
@@ -1980,7 +1980,7 @@ void CompositeEditCommand::FinishBlockCommand(ContainerNode* start_scope,
                                               int start_index,
                                               int end_index)
 {
-  GetDocument().UpdateStyleAndLayoutIgnorePendingStylesheets();
+  GetDocument().UpdateStyleAndLayout();
 
   DCHECK_EQ(start_scope, end_scope);
   DCHECK_GE(start_index, 0);
