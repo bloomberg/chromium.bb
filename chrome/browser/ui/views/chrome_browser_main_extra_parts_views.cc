94c0f7d9c2e8f0f9729b5e8fef855f16d973d409
Andrew Lee <myungjaelee@chromium.org>
2019-08-22 21:51:43 +0000

    Reland "UI_DevTools: Backend for ui_devtools performance panel."
    Fixed clobber bots failure by specifying all domains in the protocol config file.
    Without this change, builds that build from scratch will fail to generate the domain output
    files and will fail (clobber builds).
    
    This is a reland of 70b34c9e8fbb9026257885648eb37767678dbdd8 [formerly 591bf020329e99b6e00b583b836f289d50acaa3d]
    
    Original change's description:
    > UI_DevTools: Backend for ui_devtools performance panel.
    >
    > The goal is to enable and create the performance tool for ui_devtools.
    > The performance panel will record and show metrics such as FPS, CPU time utilization
    > for Aura/Views UI. This CL is for the backend only. The TracingAgent is very similar
    > to the TracingHandler from
    > https://cs.chromium.org/chromium/src/content/browser/devtools/protocol/tracing_handler.cc.
    >
    > Bug: 989052
    > Change-Id: Ic696e09f700e93bcd0842fdbda575ef1f54af63a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1726651
    > Commit-Queue: MyungJae Lee <myungjaelee@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Reviewed-by: oysteine <oysteine@chromium.org>
    > Reviewed-by: Leonard Grey <lgrey@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#689296}
    
    Bug: 989052
    Change-Id: I6392c40d5babcd74558186c80ebc618c22aa4d7b
    
    This is a reland CL and the only file difference is
    components/ui_devtools/inspector_protocol_config.json, which has been reviewed by ui_devtools
    OWNER lgrey@.
    
    TBR=rockot@google.com,oysteine@chromium.org,dfried@chromium.org
    
    Change-Id: I6392c40d5babcd74558186c80ebc618c22aa4d7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1765005
    Commit-Queue: MyungJae Lee <myungjaelee@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689686}
    
    Former-commit-id: 197daae623ce5cfc857bac07f49dc9b43de77762

--- chrome/browser/ui/views/chrome_browser_main_extra_parts_views.cc
+++ chrome/browser/ui/views/chrome_browser_main_extra_parts_views.cc
@@ -15,9 +15,12 @@
 #include "chrome/browser/ui/views/chrome_views_delegate.h"
 #include "chrome/browser/ui/views/relaunch_notification/relaunch_notification_controller.h"
 #include "components/constrained_window/constrained_window_views.h"
+#include "components/ui_devtools/connector_delegate.h"
 #include "components/ui_devtools/switches.h"
 #include "components/ui_devtools/views/devtools_server_util.h"
+#include "content/public/browser/system_connector.h"
 #include "services/service_manager/sandbox/switches.h"
+#include "services/tracing/public/mojom/constants.mojom.h"
 #include "ui/base/material_design/material_design_controller.h"
 
 #if defined(USE_AURA)
@@ -43,6 +46,20 @@
 #include "content/public/common/content_switches.h"
 #include "ui/base/l10n/l10n_util.h"
 #endif  // defined(OS_LINUX) && !defined(OS_CHROMEOS)
+
+// This connector is used in ui_devtools's TracingAgent to hook up with the
+// tracing service.
+class UiDevtoolsConnector : public ui_devtools::ConnectorDelegate {
+ public:
+  UiDevtoolsConnector() {}
+  ~UiDevtoolsConnector() override = default;
+
+  void BindTracingConsumerHost(
+      mojo::PendingReceiver<tracing::mojom::ConsumerHost> receiver) override {
+    content::GetSystemConnector()->Connect(tracing::mojom::kServiceName,
+                                           std::move(receiver));
+  }
+};
 
 ChromeBrowserMainExtraPartsViews::ChromeBrowserMainExtraPartsViews() {}
 
@@ -83,8 +100,10 @@ void ChromeBrowserMainExtraPartsViews::PreProfileInit() {
   if (ui_devtools::UiDevToolsServer::IsUiDevToolsEnabled(
           ui_devtools::switches::kEnableUiDevTools)) {
     // Starts the UI Devtools server for browser UI (and Ash UI on Chrome OS).
+    auto connector = std::make_unique<UiDevtoolsConnector>();
     devtools_server_ = ui_devtools::CreateUiDevToolsServerForViews(
-        g_browser_process->system_network_context_manager()->GetContext());
+        g_browser_process->system_network_context_manager()->GetContext(),
+        std::move(connector));
   }
 
 #if defined(OS_LINUX) && !defined(OS_CHROMEOS)
--- components/ui_devtools/devtools_protocol_encoding.h
+++ components/ui_devtools/devtools_protocol_encoding.h
@@ -5,10 +5,14 @@
 #ifndef COMPONENTS_UI_DEVTOOLS_DEVTOOLS_PROTOCOL_ENCODING_H_
 #define COMPONENTS_UI_DEVTOOLS_DEVTOOLS_PROTOCOL_ENCODING_H_
 
+#include <string>
+#include <vector>
+
 #include "third_party/inspector_protocol/encoding/encoding.h"
 
 // Convenience adaptation of the conversion function
 // ::inspector_protocol_encoding::json::ConvertCBORToJSON,
+// ::inspector_protocol_encoding::json::ConvertJSONToCBOR.
 // using an implementation of
 // ::inspector_protocol_encoding::json::Platform that
 // delegates to base/strings/string_number_conversions.h.
@@ -16,6 +20,14 @@ namespace ui_devtools {
 ::inspector_protocol_encoding::Status ConvertCBORToJSON(
     ::inspector_protocol_encoding::span<uint8_t> cbor,
     std::string* json);
+
+::inspector_protocol_encoding::Status ConvertJSONToCBOR(
+    ::inspector_protocol_encoding::span<uint8_t> json,
+    std::string* cbor);
+
+::inspector_protocol_encoding::Status ConvertJSONToCBOR(
+    ::inspector_protocol_encoding::span<uint8_t> json,
+    std::vector<uint8_t>* cbor);
 }  // namespace ui_devtools
 
 #endif  // COMPONENTS_UI_DEVTOOLS_DEVTOOLS_PROTOCOL_ENCODING_H_
