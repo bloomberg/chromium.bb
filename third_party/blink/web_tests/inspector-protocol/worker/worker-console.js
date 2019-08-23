54e69db98a1916cd1706a4416f3cd616003e5f93
Johannes Henkel <johannes@chromium.org>
2019-08-23 23:54:31 +0000

    [DevTools] Rework the console-worker.js test for flatten=true.
    In my rework, I think there could be some race conditions
    in the order of the output. I'll do the dry-run for now
    and see whether anything manifests. Perhaps it's worth
    doing some sorting of the logs, e.g. we could make the
    test insensitive to the order of some of the events in
    the golden file, by keeping two or three separate logging
    streams for each section.
    
    Bug: 991325
    Change-Id: Ib9b8fb0732f5efbf9ad619ff12d5a36bccb02a16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1762823
    Commit-Queue: Johannes Henkel <johannes@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690125}
    
    Former-commit-id: 90914b5f43204dde81d11b0124ef62bff6935afe

--- third_party/blink/web_tests/inspector-protocol/worker/worker-console.js
+++ third_party/blink/web_tests/inspector-protocol/worker/worker-console.js
@@ -1,267 +1,150 @@
 (async function(testRunner) {
-  var {page, session, dp} = await testRunner.startBlank(`Tests how console messages from worker get into page's console once worker is destroyed.`);
+  var {page, session, dp} = await testRunner.startBlank(
+      `Tests how console messages from worker get into page's console once worker is destroyed.`);
 
   await session.evaluate(`
-    var worker = null;
-    var onMessageCallbacks = {};
+    let worker;
 
+    // This function returns a promise, which, if awaited (e.g. with
+    // await session.evaluateAsync("startWorker()") ensures that the
+    // worker has been created. This works by worker-console-worker.js posting
+    // a message upon which worker.onmessage resolves the promise.
     function startWorker() {
-      var callback;
-      var promise = new Promise((fulfill) => callback = fulfill);
-      worker = new Worker('${testRunner.url('../resources/worker-console-worker.js')}');
-      worker.onmessage = function(event) {
-        worker.onmessage = onMessageFromWorker;
-        callback();
-      };
-      return promise;
+      return new Promise(resolve => {
+        worker = new Worker(
+            '${testRunner.url('../resources/worker-console-worker.js')}');
+        worker.onmessage = () => { resolve(); }
+      });
     }
 
-    function logInWorkerFromPage(message, callback) {
-      onMessageCallbacks[message] = callback;
-      worker.postMessage(message);
-    }
-
-    function onMessageFromWorker(event) {
-      var callback = onMessageCallbacks[event.data];
-      delete onMessageCallbacks[event.data];
-      if (callback)
-        callback();
-    }
-
-    function stopWorker() {
-      worker.terminate();
-      worker = null;
+    // Similar to the above method, this method returns a promise such that
+    // awaiting it ensures that the worker has posted a message back to the
+    // page. In this way, we know that worker-console-worker.js gets a chance
+    // to call console.log.
+    // We can terminate the worker after awaiting the promise returned by this
+    // method, and then observe that the console.log events were registered
+    // anyway. This is much of the point of this test.
+    function postToWorker(message) {
+      return new Promise(resolve => {
+        worker.onmessage = () => { resolve(); };
+        worker.postMessage(message);
+      });
     }
   `);
 
-  var workerEventHandler = {};
-  dp.Target.onAttachedToTarget(onWorkerCreated);
-  dp.Target.onReceivedMessageFromTarget(onWorkerMessage);
-  workerEventHandler['Runtime.consoleAPICalled'] = onConsoleAPICalledFromWorker;
+  await dp.Log.enable();
+  dp.Log.onEntryAdded((event) => {
+    testRunner.log('<- Log from page: ' + event.params.entry.text);
+  });
 
-  var workerId;
-  var workerCreateCallback;
-
-  function onWorkerCreated(payload) {
-    testRunner.log('Worker.created');
-    workerId = payload.params.targetInfo.targetId;
-    if (workerCreateCallback) {
-      var tmp = workerCreateCallback;
-      workerCreateCallback = null;
-      tmp();
-    }
+  function postToWorker(message) {
+    testRunner.log('-> Posting to worker: ' + message);
+    return session.evaluateAsync('postToWorker("' + message + '")');
   }
 
-  function waitForWorkerCreated(next) {
-    workerCreateCallback = next;
+  {
+    testRunner.log(
+        "\n=== console.log event won't get lost despite worker.terminate. ===");
+    testRunner.log('Starting worker');
+    await session.evaluateAsync('startWorker()');
+    await postToWorker('message0 (posted after starting worker)');
+    testRunner.log('Terminating worker');
+    await session.evaluate('worker.terminate()');
+    // The key part of this test is that its expectation contains
+    // "<- Log from page: message1", even though we terminated the
+    // worker without awaiting the log event (postToWorker only ensures
+    // that the message was echoed back to the page).
   }
 
-  var requestId = 0;
-  var dispatchTable = [];
+  {
+    testRunner.log(
+        '\n=== Scenario with autoattach enabled and stopped. ===');
+    testRunner.log('Starting worker');
+    await session.evaluateAsync('startWorker()');
+    await postToWorker('message1');
 
-  function sendCommandToWorker(method, params, callback) {
-    dispatchTable[++requestId] = callback;
-    var messageObject = {
-      'method': method,
-      'params': params,
-      'id': requestId
-    };
-    dp.Target.sendMessageToTarget({
-      targetId: workerId,
-      message: JSON.stringify(messageObject)
+    // Now we call Target.setAutoAttach, which will immediately generate
+    // an event that we're attached; which we receive below to create the
+    // childSession instance.
+    testRunner.log('Starting autoattach');
+    dp.Target.setAutoAttach({
+      autoAttach: true, waitForDebuggerOnStart: false, flatten: true});
+    const childSession = session.createChild(
+        (await dp.Target.onceAttachedToTarget()).params.sessionId);
+    childSession.protocol.Runtime.onConsoleAPICalled((event) => {
+      testRunner.log('<-- Console API from worker: ' +
+                     event.params.args[0].value);
     });
+    testRunner.log('child session for worker created');
+
+    testRunner.log('Sending Runtime.enable to worker');
+    childSession.protocol.Runtime.enable();
+
+    await postToWorker('message2 (posted after runtime enabled)');
+    await postToWorker('throw1 (posted after runtime enabled; ' +
+                       'yields exception in worker)');
+
+    // This unregisters the child session, so we stop getting the console API
+    // calls, but still receive the log messages for the page.
+    testRunner.log('Stopping autoattach');
+    await dp.Target.setAutoAttach({autoAttach: false,
+                                   waitForDebuggerOnStart: false});
+    await postToWorker('message3 (posted after auto-attach)');
+    testRunner.log('Terminating worker');
+    await session.evaluate('worker.terminate()');
   }
 
-  function onWorkerMessage(payload) {
-    if (payload.params.targetId !== workerId)
-      testRunner.log('targetId mismatch');
-    var messageObject = JSON.parse(payload.params.message);
-    var messageId = messageObject['id'];
-    if (typeof messageId === 'number') {
-      var handler = dispatchTable[messageId];
-      dispatchTable[messageId] = null;
-      if (handler && typeof handler === 'function')
-        handler(messageObject);
-    } else {
-      var eventName = messageObject['method'];
-      var eventHandler = workerEventHandler[eventName];
-      if (eventHandler)
-        eventHandler(messageObject);
-    }
+  {
+    testRunner.log(
+        '\n=== Scenario with autoattach from the get-go. ===');
+    // This time we start the worker only after Target.setAutoAttach, so
+    // we may await the autoattach response.
+    testRunner.log('Starting autoattach');
+    await dp.Target.setAutoAttach({
+      autoAttach: true, waitForDebuggerOnStart: false, flatten: true});
+
+    testRunner.log('Starting worker');
+    session.evaluate('startWorker()');
+    const childSession = session.createChild(
+        (await dp.Target.onceAttachedToTarget()).params.sessionId);
+    childSession.protocol.Runtime.onConsoleAPICalled((event) => {
+      testRunner.log('<-- Console API from worker: ' +
+                     event.params.args[0].value);
+    });
+    testRunner.log('child session for worker created');
+
+    // Here, we test the behavior of posting before / after Runtime.enable.
+    await postToWorker(
+        "message4 (posted before worker's runtime agent enabled)");
+    testRunner.log('Sending Runtime.enable to worker');
+    childSession.protocol.Runtime.enable();
+    await postToWorker(
+        "message5 (posted after worker's runtime agent enabled)");
+    testRunner.log('Terminating worker');
+    await session.evaluate('worker.terminate()');
   }
 
-  var lastPageLog = '';
-  function logInWorker(message, next) {
-    lastPageLog = '';
-    testRunner.log('Logging in worker: ' + message);
-    dp.Log.onEntryAdded(onLogEntry);
-    session.evaluate('logInWorkerFromPage(\'' + message + '\')');
+  {
+    testRunner.log(
+        '\n=== New worker, with auto-attach still enabled. ===');
+    testRunner.log('Starting worker');
+    session.evaluate('startWorker()');
+    const childSession = session.createChild(
+        (await dp.Target.onceAttachedToTarget()).params.sessionId);
+    childSession.protocol.Runtime.onConsoleAPICalled((event) => {
+      testRunner.log('<-- Console API from worker: ' +
+                     event.params.args[0].value);
+    });
+    testRunner.log('child session for worker created');
 
-    function onLogEntry(payload) {
-      lastPageLog = payload.params.entry.text;
-      dp.Log.offEntryAdded(onLogEntry);
-      next();
-    }
+    await postToWorker('message6 (posted just before worker termination)');
+
+    testRunner.log('Terminating worker');
+    await session.evaluate('worker.terminate()');
+
+    testRunner.log('Stopping autoattach');
+    await dp.Target.setAutoAttach({autoAttach: false,
+                                   waitForDebuggerOnStart: false});
   }
-
-  var gotMessages = [];
-  var waitingForMessage;
-  var waitingForMessageCallback;
-
-  function onConsoleAPICalledFromWorker(payload) {
-    var message = payload.params.args[0].value;
-    testRunner.log('Got console API call from worker: ' + message);
-    gotMessages.push(message);
-    if (message === waitingForMessage)
-      waitingForMessageCallback();
-  }
-
-  function waitForMessage(message, next) {
-    if (gotMessages.indexOf(message) !== -1) {
-      next();
-      return;
-    }
-    waitingForMessage = message;
-    waitingForMessageCallback = next;
-  }
-
-  var steps = [
-    function listenToConsole(next) {
-      dp.Log.enable().then(next);
-    },
-
-    function start0(next) {
-      testRunner.log('Starting worker');
-      session.evaluateAsync('startWorker()').then(next);
-    },
-
-    function log0(next) {
-      logInWorker('message0', next);
-    },
-
-    function stop0(next) {
-      testRunner.log('Got log message from page: ' + lastPageLog);
-      testRunner.log('Stopping worker');
-      session.evaluate('stopWorker()').then(next);
-    },
-
-    function start1(next) {
-      testRunner.log('Starting worker');
-      session.evaluateAsync('startWorker()').then(next);
-    },
-
-    function log1(next) {
-      logInWorker('message1', next);
-    },
-
-    function enable1(next) {
-      testRunner.log('Got log message from page: ' + lastPageLog);
-      testRunner.log('Starting autoattach');
-      dp.Target.setAutoAttach({autoAttach: true, waitForDebuggerOnStart: false});
-      waitForWorkerCreated(next);
-    },
-
-    function consoleEnable1(next) {
-      testRunner.log('Sending Runtime.enable to worker');
-      waitForMessage('message1', next);
-      sendCommandToWorker('Runtime.enable', {});
-    },
-
-    function log2(next) {
-      logInWorker('message2', next);
-    },
-
-    function waitForMessage2(next) {
-      waitForMessage('message2', next);
-    },
-
-    function throw1(next) {
-      testRunner.log('Got log message from page: ' + lastPageLog);
-      logInWorker('throw1', next);
-    },
-
-    function disable1(next) {
-      testRunner.log('Got log message from page: ' + lastPageLog);
-      testRunner.log('Stopping autoattach');
-      dp.Target.setAutoAttach({autoAttach: false, waitForDebuggerOnStart: false}).then(next);
-    },
-
-    function log3(next) {
-      logInWorker('message3', next);
-    },
-
-    function stop1(next) {
-      testRunner.log('Got log message from page: ' + lastPageLog);
-      testRunner.log('Stopping worker');
-      session.evaluate('stopWorker()').then(next);
-    },
-
-    function enable2(next) {
-      testRunner.log('Starting autoattach');
-      dp.Target.setAutoAttach({autoAttach: true, waitForDebuggerOnStart: false}).then(next);
-    },
-
-    function start2(next) {
-      testRunner.log('Starting worker');
-      session.evaluateAsync('startWorker()');
-      waitForWorkerCreated(next);
-    },
-
-    function log4(next) {
-      logInWorker('message4', next);
-    },
-
-    function consoleEnable2(next) {
-      testRunner.log('Sending Runtime.enable to worker');
-      waitForMessage('message4', next);
-      sendCommandToWorker('Runtime.enable', {});
-    },
-
-    function log5(next) {
-      testRunner.log('Got log message from page: ' + lastPageLog);
-      logInWorker('message5', next);
-    },
-
-    function waitForMessage5(next) {
-      waitForMessage('message5', next);
-    },
-
-    function stop2(next) {
-      testRunner.log('Got log message from page: ' + lastPageLog);
-      testRunner.log('Stopping worker');
-      session.evaluate('stopWorker()').then(next);
-    },
-
-    function start3(next) {
-      testRunner.log('Starting worker');
-      session.evaluateAsync('startWorker()');
-      waitForWorkerCreated(next);
-    },
-
-    function log6(next) {
-      logInWorker('message6', next);
-    },
-
-    function stop3(next) {
-      testRunner.log('Got log message from page: ' + lastPageLog);
-      testRunner.log('Stopping worker');
-      session.evaluate('stopWorker()').then(next);
-    },
-
-    function disable2(next) {
-      testRunner.log('Stopping autoattach');
-      dp.Target.setAutoAttach({autoAttach: false, waitForDebuggerOnStart: false}).then(next);
-    }
-  ];
-
-  function runNextStep() {
-    if (!steps.length) {
-      testRunner.completeTest();
-      return;
-    }
-    var nextStep = steps.shift();
-    nextStep(runNextStep);
-  }
-
-  runNextStep();
+  testRunner.completeTest();
 })
