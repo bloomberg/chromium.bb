c1eee1b436a3819e62c831c343c8824553975a7f
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2019-08-23 05:48:48 +0000

    Revert "Hide invisible Tab in GTS layout better"
    This reverts commit bb29ff1711bb491a5120f533e2d3ae38df016792 [formerly f9355db345457fdab987846d5474c7e076100bf6].
    
    Reason for revert: possible cause of crbug/997065
    
    Original change's description:
    > Hide invisible Tab in GTS layout better
    > 
    > After switching to another tab from GTS, a thumbnail of the original
    > tab is taken. However, the thumbnail taking would be blocked if the
    > web contents of the original tab is detached. Therefore, when showing
    > the GTS-to-Tab transition animation to another tab, we cannot detach
    > the original tab.
    > 
    > The original tab is supposed to be invisible, but the tab decoration
    > was not properly hidden.
    > 
    > This CL hides the decoration of that invisible tab.
    > 
    > Bug: 995992
    > Change-Id: Ia55cc21ddcbbc59674fa8c8268faaddc458cd4a3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1764660
    > Reviewed-by: Mei Liang <meiliang@chromium.org>
    > Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#689621}
    
    TBR=wychen@chromium.org,meiliang@chromium.org
    
    Change-Id: I4d0a26b33b89bd98b6d6693e50ce368f4ec45b74
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 995992
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1767607
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#689814}
    
    Former-commit-id: ef04913d880876a9e0c190d10cf6983f3ecfc5c0

--- chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/StartSurfaceLayout.java
+++ chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/StartSurfaceLayout.java
@@ -205,7 +205,6 @@ public class StartSurfaceLayout extends Layout implements StartSurface.OverviewM
             LayoutTab originalTab = createLayoutTab(mTabModelSelector.getCurrentTabId(),
                     mTabModelSelector.isIncognitoSelected(), NO_CLOSE_BUTTON, NO_TITLE);
             originalTab.setScale(0);
-            originalTab.setDecorationAlpha(0);
             layoutTabs.add(originalTab);
         }
         mLayoutTabs = layoutTabs.toArray(new LayoutTab[0]);
