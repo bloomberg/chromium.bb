085d34716317b2a93d4b6ebb87ab4c1676af7bee
Anastasia Helfinstein <anastasi@google.com>
2019-08-26 20:18:28 +0000

    [Switch Access] Add focus ring manager
    This change adds a class to manage focus rings. A follow-up change will
    replace all existing focus ring logic with calls to this class.
    
    Bug: None
    Change-Id: I035aaaa7ff4ce0a462321bd286a6254dc33b284b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1769301
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690480}
    
    Former-commit-id: 898f36875448e02db418496be8275b35e1f310f4

---
+++ chrome/browser/resources/chromeos/switch_access/focus_ring_manager.js
@@ -0,0 +1,156 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Class to handle focus rings.
+ */
+class FocusRingManager {
+  /**
+   * @param {!BackButtonManager} backButtonManager
+   */
+  constructor(backButtonManager) {
+    /**
+     * A map of all the focus rings.
+     * @private {!Map<SAConstants.Focus.ID,
+     *     chrome.accessibilityPrivate.FocusRingInfo>}
+     */
+    this.rings_;
+
+    /**
+     * Regex pattern to verify valid colors. Checks that the first character
+     * is '#', followed by between 3 and 8 valid hex characters, and no other
+     * characters (ignoring case).
+     */
+    this.colorPattern_ = /^#[0-9A-F]{3,8}$/i;
+
+    /**
+     * Keeps track of the scope node currently being focused.
+     * @private {chrome.automation.AutomationNode}
+     */
+    this.currentScope_;
+
+    /**
+     * The back button manager.
+     */
+    this.backButtonManager_ = backButtonManager;
+  }
+
+  /** Finishes setup of focus rings once the preferences are loaded. */
+  onPrefsReady() {
+    // Currently all focus rings share the same color.
+    const color = this.switchAccess_.getStringPreference(
+        SAConstants.Preference.PRIMARY_FOCUS_COLOR);
+
+    // Create each focus ring.
+    this.rings_[SAConstants.Focus.ID.PRIMARY] = {
+      id: SAConstants.Focus.ID.PRIMARY,
+      rects: [],
+      type: chrome.accessibilityPrivate.FocusType.SOLID,
+      color: color,
+      secondaryColor: SAConstants.Focus.SECONDARY_COLOR
+    };
+    this.rings_[SAConstants.Focus.ID.SCOPE] = {
+      id: SAConstants.Focus.ID.SCOPE,
+      rects: [],
+      type: chrome.accessibilityPrivate.FocusType.DASHED,
+      color: color,
+      secondaryColor: SAConstants.Focus.SECONDARY_COLOR
+    };
+    this.rings_[SAConstants.Focus.ID.TEXT] = {
+      id: SAConstants.Focus.ID.TEXT,
+      rects: [],
+      type: chrome.accessibilityPrivate.FocusType.DASHED,
+      color: color,
+      secondaryColor: SAConstants.Focus.SECONDARY_COLOR
+    };
+  }
+
+  /**
+   * Sets the focus ring color.
+   * @param {!string} color
+   */
+  setColor(color) {
+    if (this.colorPattern_.test(color) !== true) {
+      new Error(
+          'Problem setting focus ring color: color is not a valid' +
+          'CSS color string.');
+      return;
+    }
+    this.rings_.forEach((ring) => ring.color = color);
+  }
+
+  /**
+   * Sets the primary and scope focus rings to be around the given nodes.
+   * Saves the scope for future comparison.
+   * @param {!chrome.automation.AutomationNode} primary
+   * @param {!chrome.automation.AutomationNode} scope
+   */
+  setFocusNodes(primary, scope) {
+    const focusRect = primary.location;
+
+    // If the scope element has not changed, we want to use the previously
+    // calculated rect as the current scope rect.
+    let scopeRect = scope.location;
+    const currentScopeRects = this.rings_[SAConstants.Focus.ID.SCOPE].rects;
+    if (currentScopeRects.length && scope === this.currentScope_)
+      scopeRect = currentScopeRects[0];
+    this.currentScope_ = scope;
+
+    if (primary === this.backButtonManager_.buttonNode()) {
+      this.backButtonManager_.show(scopeRect);
+
+      this.rings_[SAConstants.Focus.ID.PRIMARY].rects = [];
+      this.rings_[SAConstants.Focus.ID.SCOPE].rects = [scopeRect];
+      this.updateFocusRings_();
+      return;
+    }
+    this.backButtonManager_.hide();
+
+    // If the current element is not the back button, the scope rect should
+    // expand to contain the focus rect.
+    scopeRect = RectHelper.expandToFitWithPadding(
+        SAConstants.Focus.SCOPE_BUFFER, scopeRect, focusRect);
+
+    this.rings_[SAConstants.Focus.ID.PRIMARY].rects = [focusRect];
+    this.rings_[SAConstants.Focus.ID.SCOPE].rects = [scopeRect];
+    this.updateFocusRings_();
+  }
+
+  /**
+   * Clears the focus ring with the given ID.
+   * @param {!SAConstants.Focus.ID} id
+   */
+  clearRing(id) {
+    this.rings_[id].rects = [];
+    this.updateFocusRings_();
+  }
+
+  /**
+   * Clears all focus rings.
+   */
+  clearAll() {
+    this.rings_.forEach((ring) => ring.rects = []);
+    updateFocusRings_();
+  }
+
+  /**
+   * Sets the indicated focus ring to highlight the given rects.
+   * @param {!SAConstants.Focus.ID} id
+   * @param {!Array<chrome.accessibilityPrivate.ScreenRect>} rects
+   */
+  setRing(id, rects) {
+    this.rings_[id].rects = rects;
+    this.updateFocusRings_();
+  }
+
+  /**
+   * Updates all focus rings to reflect new location, color, style, or other
+   * changes.
+   */
+  updateFocusRings_() {
+    let focusRings = [];
+    this.rings_.forEach((ring) => focusRings.push(ring));
+    chrome.accessibilityPrivate.setFocusRings(focusRings);
+  }
+}
