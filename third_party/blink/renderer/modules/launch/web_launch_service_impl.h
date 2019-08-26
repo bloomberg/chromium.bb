0ea1b74cae9d0c840e033570ecfacca4f6ec5645
Miyoung Shin <myid.shin@igalia.com>
2019-08-26 02:17:59 +0000

    Convert WebLaunchService to new Mojo types
    This CL converts WebLaunchServiceAssociatedPtr in chrome and
    WebLaunchServiceAssociatedRequest in blink to the new Mojo types.
    
    Bug: 955171, 978694
    Change-Id: Ic73752fe3f3525c631d9665cbc7cb967ce8c39ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1767446
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Jay Harris <harrisjay@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#690255}
    
    Former-commit-id: 57edfda058ecaf8324ee462686458574e6ac5d2d

--- third_party/blink/renderer/modules/launch/web_launch_service_impl.h
+++ third_party/blink/renderer/modules/launch/web_launch_service_impl.h
@@ -5,6 +5,7 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_LAUNCH_WEB_LAUNCH_SERVICE_IMPL_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_LAUNCH_WEB_LAUNCH_SERVICE_IMPL_H_
 
+#include "mojo/public/cpp/bindings/pending_associated_receiver.h"
 #include "third_party/blink/public/mojom/native_file_system/native_file_system_directory_handle.mojom-blink-forward.h"
 #include "third_party/blink/public/mojom/web_launch/web_launch.mojom-blink.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
@@ -22,8 +23,9 @@ class LocalDOMWindow;
 class MODULES_EXPORT WebLaunchServiceImpl final
     : public mojom::blink::WebLaunchService {
  public:
-  static void Create(LocalFrame* frame,
-                     mojom::blink::WebLaunchServiceAssociatedRequest);
+  static void Create(
+      LocalFrame* frame,
+      mojo::PendingAssociatedReceiver<mojom::blink::WebLaunchService>);
   explicit WebLaunchServiceImpl(LocalDOMWindow& frame);
   ~WebLaunchServiceImpl() override;
 
