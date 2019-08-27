fe9150b9da356e96480eea5d6463f1c4bb2169b3
Devlin Cronin <rdevlin.cronin@chromium.org>
2019-08-27 17:47:21 +0000

    [Merge M77][Extensions] Remove downloads.drag
    The downloads.drag API has exceedingly low usage, and has other issues.
    Remove it.
    
    Bug: 986043
    
    TBR=rdevlin.cronin@chromium.org
    
    (cherry picked from commit 1ade90ff477dd7ea59c2179427253cf1dc31580a [formerly 8c3e2c3c1fd30b3fcc4dff17e33e4a00c09438bf])
    
    Change-Id: I3f0f41fba02a8d729feee9af036a1f383a0e1b0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1742547
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#686508}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1773203
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3865@{#609}
    Cr-Branched-From: 4e0147fd4ab868c78ff6bb1ef42f86871cde1461 [formerly 0cdcc6158160790658d1f033d3db873603250124]-refs/heads/master@{#681094}
    
    Former-commit-id: c50370cd40d57ee67f257fa7e65bda402266af2f

--- chrome/browser/extensions/api/downloads/downloads_api.h
+++ chrome/browser/extensions/api/downloads/downloads_api.h
@@ -275,19 +275,6 @@ class DownloadsSetShelfEnabledFunction : public UIThreadExtensionFunction {
   DISALLOW_COPY_AND_ASSIGN(DownloadsSetShelfEnabledFunction);
 };
 
-class DownloadsDragFunction : public UIThreadExtensionFunction {
- public:
-  DECLARE_EXTENSION_FUNCTION("downloads.drag", DOWNLOADS_DRAG)
-  DownloadsDragFunction();
-  ResponseAction Run() override;
-
- protected:
-  ~DownloadsDragFunction() override;
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(DownloadsDragFunction);
-};
-
 class DownloadsGetFileIconFunction : public ChromeAsyncExtensionFunction {
  public:
   DECLARE_EXTENSION_FUNCTION("downloads.getFileIcon", DOWNLOADS_GETFILEICON)
--- chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
+++ chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
@@ -1235,14 +1235,6 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
   RunFunction(new DownloadsShowDefaultFolderFunction(),
               DownloadItemIdAsArgList(item.get()));
 }
-
-IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
-                       DownloadsDragFunction) {
-  ScopedCancellingItem item(CreateFirstSlowTestDownload());
-  ASSERT_TRUE(item.get());
-
-  RunFunction(new DownloadsDragFunction(), DownloadItemIdAsArgList(item.get()));
-}
 #endif
 
 
