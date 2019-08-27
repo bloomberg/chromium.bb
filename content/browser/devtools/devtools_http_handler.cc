94975e950ada58381a5f39c1be3cd6ad48328825
Jacques Chen <jacqueschen@google.com>
2019-08-27 20:23:21 +0000

    Make use of grit's brotli support for brotli-compressed protocol.json
    -Change protocol.json to be Brotli compressed by grit
    -Have protocol.json be decompressed by GetDataResourceBytes
    -Remove brotli.gni that is no longer used
    
    Bug: 826858
    Change-Id: I2c49715f85508f494a57d65b09ada446c2efefec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1728183
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690865}
    
    Former-commit-id: 277f819d03cf4d45aa8d44661333122ed08ac6c8

--- content/browser/devtools/devtools_http_handler.cc
+++ content/browser/devtools/devtools_http_handler.cc
@@ -48,7 +48,6 @@
 #include "net/server/http_server_response_info.h"
 #include "net/socket/server_socket.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
-#include "third_party/brotli/include/brotli/decode.h"
 #include "v8/include/v8-version-string.h"
 
 #if defined(OS_ANDROID)
@@ -657,36 +656,10 @@ void DevToolsHttpHandler::DecompressAndSendJsonProtocol(int connection_id) {
 #if defined(OS_FUCHSIA)
   NOTREACHED();
 #else
-  scoped_refptr<base::RefCountedMemory> raw_bytes =
+  scoped_refptr<base::RefCountedMemory> bytes =
       GetContentClient()->GetDataResourceBytes(COMPRESSED_PROTOCOL_JSON);
-  const uint8_t* next_encoded_byte = raw_bytes->front();
-  size_t input_size_remaining = raw_bytes->size();
-  BrotliDecoderState* decoder = BrotliDecoderCreateInstance(
-      nullptr /* no custom allocator */, nullptr /* no custom deallocator */,
-      nullptr /* no custom memory handle */);
-  CHECK(!!decoder);
-  std::vector<std::string> decoded_parts;
-  size_t decompressed_size = 0;
-  while (!BrotliDecoderIsFinished(decoder)) {
-    size_t output_size_remaining = 0;
-    CHECK(BrotliDecoderDecompressStream(
-              decoder, &input_size_remaining, &next_encoded_byte,
-              &output_size_remaining, nullptr,
-              nullptr) != BROTLI_DECODER_RESULT_ERROR);
-    const uint8_t* output_buffer =
-        BrotliDecoderTakeOutput(decoder, &output_size_remaining);
-    decoded_parts.emplace_back(reinterpret_cast<const char*>(output_buffer),
-                               output_size_remaining);
-    decompressed_size += output_size_remaining;
-  }
-  BrotliDecoderDestroyInstance(decoder);
-
-  // Ideally we'd use a StringBuilder here but there isn't one in base/.
-  std::string json_protocol;
-  json_protocol.reserve(decompressed_size);
-  for (const std::string& part : decoded_parts) {
-    json_protocol.append(part);
-  }
+  std::string json_protocol(reinterpret_cast<const char*>(bytes->front()),
+                            bytes->size());
 
   net::HttpServerResponseInfo response(net::HTTP_OK);
   response.SetBody(json_protocol, "application/json; charset=UTF-8");
