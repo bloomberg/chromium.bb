b36ecc0af5ece326202c5914e1f9a904ede9aed1
Clark DuVall <cduvall@chromium.org>
2019-08-27 18:48:04 +0000

    [Merge M77] Log network service ping time on navigation commit timeouts
    This will be useful in telling if the network service is responding or
    not when a commit timeout happens.
    
    (cherry picked from commit 40f8426fd8ec55af494d3eadcabfbaffbb64b304 [formerly 1f94e9f455e24907957b10c0f308fc3239332325])
    
    Bug: 934317
    Change-Id: I6ea88147bb2ad4e0d393aa8354004b7bdc53fd77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1761340
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#688864}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1773526
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3865@{#621}
    Cr-Branched-From: 4e0147fd4ab868c78ff6bb1ef42f86871cde1461 [formerly 0cdcc6158160790658d1f033d3db873603250124]-refs/heads/master@{#681094}
    
    Former-commit-id: 6731dde34fbe89b413ec98eebf34a0dcd49c74a9

--- content/browser/frame_host/navigation_handle_impl.cc
+++ content/browser/frame_host/navigation_handle_impl.cc
@@ -517,6 +517,13 @@ void NavigationHandleImpl::OnCommitTimeout() {
   }
 #endif
 
+  PingNetworkService(base::BindOnce(
+      [](base::Time start_time) {
+        UMA_HISTOGRAM_MEDIUM_TIMES(
+            "Navigation.CommitTimeout.NetworkServicePingTime",
+            base::Time::Now() - start_time);
+      },
+      base::Time::Now()));
   UMA_HISTOGRAM_ENUMERATION(
       "Navigation.CommitTimeout.NetworkServiceAvailability",
       GetNetworkServiceAvailability());
