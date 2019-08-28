451e383c03303db9e77af5d0d3e932e7ee9c1347
Tarun Bansal <tbansal@chromium.org>
2019-08-28 01:50:07 +0000

    Detects a client redirect loop with DeferAllScript
    Navigating to some websites with DeferAllScript enabled for that site
    could cause a client redirect loop. A previous CL
    (https://chromium-review.googlesource.com/c/chromium/src/+/1739493)
    implemented this in a different way but that CL is not
    mergeable back.
    
    This CL alternates it in a different way which also fixes the problem
    and can be merged back.
    
    All the logic is behind a finch parameter and so can be easil turned off
    using finch.
    
    Tested by commenting out the older code. This will also
    be tested by beta builders using browser tests.
    
    Change-Id: Icb1075580d12890fe0ee9e902925eaf212744d37
    Bug: 987062
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1760667
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Auto-Submit: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690983}
    
    Former-commit-id: 62dc00e25fb8138be4dce09d63b493ed82457048

--- chrome/browser/previews/previews_service_unittest.cc
+++ chrome/browser/previews/previews_service_unittest.cc
@@ -118,3 +118,46 @@ TEST_F(PreviewsServiceTest, TestDeferAllScriptPreviewsEnabledByFeature) {
                 static_cast<int>(previews::PreviewsType::DEFER_ALL_SCRIPT)),
             allowed_types_and_versions2.end());
 }
+
+TEST_F(PreviewsServiceTest, HasURLRedirectCycle) {
+  base::MRUCache<GURL, GURL> redirect_history(100u);
+
+  // URL redirect cycle of length 3.
+  redirect_history.Put(GURL("https://a.com/"), GURL("https://b.com/"));
+  redirect_history.Put(GURL("https://b.com/"), GURL("https://c.com/"));
+  redirect_history.Put(GURL("https://c.com/"), GURL("https://a.com/"));
+
+  // Not a cycle.
+  redirect_history.Put(GURL("https://m.com/"), GURL("https://n.com/"));
+  redirect_history.Put(GURL("https://n.com/"), GURL("https://o.com/"));
+  redirect_history.Put(GURL("https://o.com/"), GURL("https://p.com/"));
+
+  // URL redirect cycle of length 3.
+  redirect_history.Put(GURL("https://q.com/"), GURL("https://r.com/"));
+  redirect_history.Put(GURL("https://r.com/"), GURL("https://s.com/"));
+  redirect_history.Put(GURL("https://s.com/"), GURL("https://s.com/"));
+
+  // URL redirect cycle of length 2.
+  redirect_history.Put(GURL("https://x.com/"), GURL("https://y.com/"));
+  redirect_history.Put(GURL("https://y.com/"), GURL("https://x.com/"));
+
+  const struct {
+    const char* start_url;
+    bool expect_redirect_cycle;
+  } kTestCases[] = {
+      {"https://a.com/", true},  {"https://b.com/", true},
+      {"https://c.com/", true},  {"https://d.com/", false},
+      {"https://m.com/", false}, {"https://n.com/", false},
+      {"https://o.com/", false}, {"https://p.com/", false},
+      {"https://q.com/", true},  {"https://r.com/", true},
+      {"https://s.com/", true},  {"https://x.com/", true},
+      {"https://y.com/", true},
+  };
+
+  for (const auto& test : kTestCases) {
+    EXPECT_EQ(test.expect_redirect_cycle,
+              PreviewsService::HasURLRedirectCycle(GURL(test.start_url),
+                                                   redirect_history))
+        << " test.start_url=" << test.start_url;
+  }
+}
