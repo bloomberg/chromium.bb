f64842835c01815c9b6a86b80935aefe86948c3c
Tricia Crichton <triciac@chromium.org>
2019-08-29 23:27:03 +0000

    [ChromeDriver] Refactor SwitchToWindow page refresh
    Refactor to remove duplicate code that connects to a page for several
    session types.
    
    Bug: chromedriver:2126
    Change-Id: I37713cce11082fd28761018c71c69f3b400b1071
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1776798
    Commit-Queue: Tricia Crichton <triciac@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691853}
    
    Former-commit-id: 0158eac1c8bf64ab49bb3b4b2aa34471b0b2e80b

--- chrome/test/chromedriver/session_commands.cc
+++ chrome/test/chromedriver/session_commands.cc
@@ -731,7 +731,11 @@ Status ExecuteSwitchToWindow(Session* session,
   if (!found)
     return Status(kNoSuchWindow);
 
-  if (session->overridden_geoposition) {
+  if (session->overridden_geoposition ||
+      session->overridden_network_conditions ||
+      session->headless_download_directory ||
+      session->chrome->IsMobileEmulationEnabled()) {
+    // Connect to new window to apply session configuration
     WebView* web_view;
     Status status = session->chrome->GetWebViewById(web_view_id, &web_view);
     if (status.IsError())
@@ -739,46 +743,25 @@ Status ExecuteSwitchToWindow(Session* session,
     status = web_view->ConnectIfNecessary();
     if (status.IsError())
       return status;
-    status = web_view->OverrideGeolocation(*session->overridden_geoposition);
-    if (status.IsError())
-      return status;
-  }
 
-  if (session->overridden_network_conditions) {
-    WebView* web_view;
-    Status status = session->chrome->GetWebViewById(web_view_id, &web_view);
-    if (status.IsError())
-      return status;
-    status = web_view->ConnectIfNecessary();
-    if (status.IsError())
-      return status;
-    status = web_view->OverrideNetworkConditions(
-        *session->overridden_network_conditions);
-    if (status.IsError())
-      return status;
-  }
-  if (session->headless_download_directory) {
-    WebView* web_view;
-    Status status = session->chrome->GetWebViewById(web_view_id, &web_view);
-    if (status.IsError())
-      return status;
-    status = web_view->ConnectIfNecessary();
-    if (status.IsError())
-      return status;
-    status = web_view->OverrideDownloadDirectoryIfNeeded(
-        *session->headless_download_directory);
-    if (status.IsError())
-      return status;
-  }
-
-  if (session->chrome->IsMobileEmulationEnabled()) {
-    WebView* web_view;
-    Status status = session->chrome->GetWebViewById(web_view_id, &web_view);
-    if (status.IsError())
-      return status;
-    status = web_view->ConnectIfNecessary();
-    if (status.IsError())
-      return status;
+    // apply type specific configurations:
+    if (session->overridden_geoposition) {
+      status = web_view->OverrideGeolocation(*session->overridden_geoposition);
+      if (status.IsError())
+        return status;
+    }
+    if (session->overridden_network_conditions) {
+      status = web_view->OverrideNetworkConditions(
+          *session->overridden_network_conditions);
+      if (status.IsError())
+        return status;
+    }
+    if (session->headless_download_directory) {
+      status = web_view->OverrideDownloadDirectoryIfNeeded(
+          *session->headless_download_directory);
+      if (status.IsError())
+        return status;
+    }
   }
 
   status = session->chrome->ActivateWebView(web_view_id);
