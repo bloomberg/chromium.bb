2594341677ccf19e9017a5fcd44ae6ffea6df48e
Finnur Thorarinsson <finnur@chromium.org>
2019-08-30 15:43:32 +0000

    Contacts Picker: Fix plural bug in "+ %d more" string.
    Bug: 999102, 860467
    Change-Id: Ie56145c9e838f3942ac0ce04b4bb4e996e162b6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1778050
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692068}
    
    Former-commit-id: 4e9a2b48b920dc30c65a582061add74b5744c54d

--- chrome/android/java/src/org/chromium/chrome/browser/contacts_picker/ContactDetails.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contacts_picker/ContactDetails.java
@@ -142,9 +142,11 @@ public class ContactDetails implements Comparable<ContactDetails> {
             results.primaryEmail = "";
         } else {
             results.primaryEmail = mEmails.get(0);
-            if (mEmails.size() > 1) {
+            int totalAddresses = mEmails.size();
+            if (totalAddresses > 1) {
+                int hiddenAddresses = totalAddresses - 1;
                 results.overflowEmailCount = resources.getQuantityString(
-                        R.plurals.contacts_picker_more_details, (mEmails.size() - 1));
+                        R.plurals.contacts_picker_more_details, hiddenAddresses, hiddenAddresses);
             }
         }
 
@@ -153,9 +155,11 @@ public class ContactDetails implements Comparable<ContactDetails> {
             results.primaryTelephoneNumber = "";
         } else {
             results.primaryTelephoneNumber = mPhoneNumbers.get(0);
-            if (mPhoneNumbers.size() > 1) {
+            int totalNumbers = mPhoneNumbers.size();
+            if (totalNumbers > 1) {
+                int hiddenNumbers = totalNumbers - 1;
                 results.overflowTelephoneNumberCount = resources.getQuantityString(
-                        R.plurals.contacts_picker_more_details, (mPhoneNumbers.size() - 1));
+                        R.plurals.contacts_picker_more_details, hiddenNumbers, hiddenNumbers);
             }
         }
 
