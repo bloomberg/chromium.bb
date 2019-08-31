a01ad082e6769b190a74c916e6abd50d3f606f61
Wez <wez@chromium.org>
2019-08-31 19:49:49 +0000

    [Fuchsia] Remove MSG_PEEK workaround from SocketPosix.
    Fuchsia's FDIO layer now implements recv(..., MSG_PEEK), so remove the
    work-around implementations of SocketPosix::IsConnected[AndIdle]() used
    under Fuchsia.
    
    Bug: 887587
    Change-Id: If43530527ec5cbc313e321846fb97f0e45bcb71f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1769683
    Auto-Submit: Wez <wez@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692363}
    
    Former-commit-id: 7acee7ba237db6175a22c18183556c1858b5fcb8

--- net/socket/socket_posix.cc
+++ net/socket/socket_posix.cc
@@ -237,7 +237,6 @@ bool SocketPosix::IsConnected() const {
   if (socket_fd_ == kInvalidSocket || waiting_connect_)
     return false;
 
-#if !defined(OS_FUCHSIA)
   // Checks if connection is alive.
   char c;
   int rv = HANDLE_EINTR(recv(socket_fd_, &c, 1, MSG_PEEK));
@@ -247,34 +246,6 @@ bool SocketPosix::IsConnected() const {
     return false;
 
   return true;
-
-#else   // OS_FUCHSIA
-  // TODO(crbug.com/887587): Remove once MSG_PEEK is implemented on Fuchsia.
-  // Fuchsia currently doesn't support MSG_PEEK flag in recv(), so the code
-  // above doesn't work on Fuchsia.
-
-  // If there is no peer address then this socket was never connected.
-  if (!HasPeerAddress())
-    return false;
-
-  // Use poll(POLLRDHUP) to check whether the socket is disconnected.
-  struct pollfd pollfd;
-  pollfd.fd = socket_fd_;
-  pollfd.events = POLLRDHUP;
-  pollfd.revents = 0;
-  const int poll_result = HANDLE_EINTR(poll(&pollfd, 1, 0));
-
-  if (poll_result == 1) {
-    // The socket is disconnected, so check whether it has data to read.
-    int bytes_available;
-    int ioctl_result =
-        HANDLE_EINTR(ioctl(socket_fd_, FIONREAD, &bytes_available));
-    return ioctl_result == 0 && bytes_available > 0;
-  }
-
-  // The socket is still connected, or poll() reported an error.
-  return poll_result == 0;
-#endif  // OS_FUCHSIA
 }
 
 bool SocketPosix::IsConnectedAndIdle() const {
@@ -283,7 +254,6 @@ bool SocketPosix::IsConnectedAndIdle() const {
   if (socket_fd_ == kInvalidSocket || waiting_connect_)
     return false;
 
-#if !defined(OS_FUCHSIA)
   // Check if connection is alive and we haven't received any data
   // unexpectedly.
   char c;
@@ -294,25 +264,6 @@ bool SocketPosix::IsConnectedAndIdle() const {
     return false;
 
   return true;
-#else   // OS_FUCHSIA
-  // TODO(crbug.com/887587): Remove once MSG_PEEK is implemented.
-  // Fuchsia currently doesn't support MSG_PEEK flag in recv(), so the code
-  // above doesn't work on Fuchsia.
-
-  // If there is no peer address then this socket was never connected.
-  if (!HasPeerAddress())
-    return false;
-
-  // Use poll(POLLIN) to check state of the socket. POLLIN is signaled if the
-  // socket is readable or if it was closed by the peer, i.e. the socket is
-  // connected and idle if and only if POLLIN is not signaled.
-  struct pollfd pollfd;
-  pollfd.fd = socket_fd_;
-  pollfd.events = POLLIN;
-  pollfd.revents = 0;
-  const int poll_result = HANDLE_EINTR(poll(&pollfd, 1, 0));
-  return poll_result == 0;
-#endif  // OS_FUCHSIA
 }
 
 int SocketPosix::Read(IOBuffer* buf,
