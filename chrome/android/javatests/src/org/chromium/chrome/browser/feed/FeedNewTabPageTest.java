1659e97563498e79a4e5430b45d7b617efaa6f82
Alex Ilin <alexilin@chromium.org>
2019-09-03 17:46:24 +0000

    Mark FeedNewTabPageTest#testSignInPromo_AccountsNotReady as flaky
    Bug: 996716
    TBR: skym@chromium.org
    Change-Id: Ic86fd8c06a8841dc691137c5c24bf01c87604ed1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1782857
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692721}
    
    Former-commit-id: 0cd0a1dbfdebb690cdf140987ba04928d9df01f9

--- chrome/android/javatests/src/org/chromium/chrome/browser/feed/FeedNewTabPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/feed/FeedNewTabPageTest.java
@@ -35,6 +35,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.ChromeSwitches;
@@ -199,6 +200,7 @@ public class FeedNewTabPageTest {
     @Test
     @MediumTest
     @Feature({"FeedNewTabPage"})
+    @FlakyTest(message = "https://crbug.com/996716")
     @AccountManagerTestRule.BlockGetAccounts
     public void testSignInPromo_AccountsNotReady() {
         // Check that the sign-in promo is not shown if accounts are not ready.
