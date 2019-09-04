8214fae0e7955629841694d6eda873045f2074f0
dalyk <dalyk@google.com>
2019-09-04 12:40:26 +0000

    Display the chrome://flags DoH entry for non-managed users.
    The flag will only be shown on Mac OS, Windows, Chrome OS, and Android.
    
    Bug: 985589
    Change-Id: I816e70d3cd2941cb10a041c4c2403782ed5efc74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1779078
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Commit-Queue: Katharine Daly <dalyk@google.com>
    Cr-Commit-Position: refs/heads/master@{#693130}
    
    Former-commit-id: 348087fed9a5aa3fb260013da35e7a71186d8b8e

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -30,6 +30,7 @@
 #include "cc/base/switches.h"
 #include "chrome/browser/browser_features.h"
 #include "chrome/browser/flag_descriptions.h"
+#include "chrome/browser/net/dns_util.h"
 #include "chrome/browser/notifications/scheduler/public/features.h"
 #include "chrome/browser/performance_manager/graph/policies/policy_features.h"
 #include "chrome/browser/permissions/permission_features.h"
@@ -4427,7 +4428,8 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_VALUE_TYPE(blink::features::kDecodeLossyWebPImagesToYUV)},
 
     {"dns-over-https", flag_descriptions::kDnsOverHttpsName,
-     flag_descriptions::kDnsOverHttpsDescription, kOsAll,
+     flag_descriptions::kDnsOverHttpsDescription,
+     kOsMac | kOsWin | kOsCrOS | kOsAndroid,
      FEATURE_VALUE_TYPE(features::kDnsOverHttps)},
 
 #if defined(OS_ANDROID)
@@ -4565,9 +4567,8 @@ bool SkipConditionalFeatureEntry(const FeatureEntry& entry) {
   }
 #endif  // OS_WIN
 
-  // TODO(crbug.com/988078): Make the DoH entry visible for non-enterprise
-  // users.
-  if (!strcmp("dns-over-https", entry.internal_name)) {
+  if (!strcmp("dns-over-https", entry.internal_name) &&
+      chrome_browser_net::ShouldDisableDohForManaged()) {
     return true;
   }
 
