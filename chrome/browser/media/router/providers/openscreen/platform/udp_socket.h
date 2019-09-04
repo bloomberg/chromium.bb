25986a29c0b882c53d358af07075a5386eb693f6
mark a. foltz <mfoltz@chromium.org>
2019-09-04 16:50:22 +0000

    [Open Screen] Temporarily make udp_socket a no-op.
    The upstream API for openscreen::platform::UdpSocket has changed in multiple
    incompatible ways.  Since the Chrome implementation doesn't have any usages yet,
    it is simpler to just comment it out, rather than devise a complicated sequence
    of multi-sided patches to revert and re-land the upstream API while migrating
    the Chrome socket implementation.
    
    When the upstream API is fully stabilized, this can be reverted and the Chrome
    socket re-implemented on top of it.
    
    Bug: openscreen:68
    Change-Id: I05165a409076df9862aa8309da2ef9152beddf00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1783879
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693223}
    
    Former-commit-id: 4095c5dbec90dec8bfb5d48a3c5cd4d8fbdcba9a

--- chrome/browser/media/router/providers/openscreen/platform/udp_socket.h
+++ chrome/browser/media/router/providers/openscreen/platform/udp_socket.h
@@ -5,57 +5,57 @@
 #ifndef CHROME_BROWSER_MEDIA_ROUTER_PROVIDERS_OPENSCREEN_PLATFORM_UDP_SOCKET_H_
 #define CHROME_BROWSER_MEDIA_ROUTER_PROVIDERS_OPENSCREEN_PLATFORM_UDP_SOCKET_H_
 
-#include <memory>
+// #include <memory>
 
-#include "base/memory/weak_ptr.h"
-#include "services/network/udp_socket.h"
-#include "third_party/openscreen/src/platform/api/udp_socket.h"
-#include "third_party/openscreen/src/platform/base/error.h"
+// #include "base/memory/weak_ptr.h"
+// #include "services/network/udp_socket.h"
+// #include "third_party/openscreen/src/platform/api/udp_socket.h"
+// #include "third_party/openscreen/src/platform/base/error.h"
 
-namespace net {
-class IPEndPoint;
-}
+// namespace net {
+// class IPEndPoint;
+// }
 
 namespace openscreen {
 namespace platform {
 
-struct ChromeUdpSocket : public UdpSocket {
- public:
-  ChromeUdpSocket(std::unique_ptr<network::UDPSocket> udp_socket,
-                  TaskRunner* task_runner,
-                  Client* client,
-                  const IPEndpoint& local_endpoint);
-  ~ChromeUdpSocket() final;
+struct ChromeUdpSocket {
+  // public:
+  //  ChromeUdpSocket(std::unique_ptr<network::UDPSocket> udp_socket,
+  //                  TaskRunner* task_runner,
+  //                  Client* client,
+  //                  const IPEndpoint& local_endpoint);
+  //  ~ChromeUdpSocket() final;
 
-  ErrorOr<UdpPacket> ReceiveMessage();
+  //  ErrorOr<UdpPacket> ReceiveMessage();
 
-  // Implementations of UdpSocket methods.
-  bool IsIPv4() const final;
-  bool IsIPv6() const final;
-  Error Bind() final;
-  Error SetMulticastOutboundInterface(NetworkInterfaceIndex ifindex) final;
-  Error JoinMulticastGroup(const IPAddress& address,
-                           NetworkInterfaceIndex ifindex) final;
-  Error SendMessage(const void* data,
-                    size_t length,
-                    const IPEndpoint& dest) final;
-  Error SetDscp(DscpMode state) final;
+  //  // Implementations of UdpSocket methods.
+  //  bool IsIPv4() const final;
+  //  bool IsIPv6() const final;
+  //  Error Bind() final;
+  //  Error SetMulticastOutboundInterface(NetworkInterfaceIndex ifindex) final;
+  //  Error JoinMulticastGroup(const IPAddress& address,
+  //                           NetworkInterfaceIndex ifindex) final;
+  //  Error SendMessage(const void* data,
+  //                    size_t length,
+  //                    const IPEndpoint& dest) final;
+  //  Error SetDscp(DscpMode state) final;
 
-  // Method that allows for getting the last async error, set by internal
-  // callback methods.
-  int32_t GetLastError();
+  //  // Method that allows for getting the last async error, set by internal
+  //  // callback methods.
+  //  int32_t GetLastError();
 
- private:
-  void BindCallback(int32_t result,
-                    const base::Optional<net::IPEndPoint>& address);
-  void JoinGroupCallback(int32_t result);
-  void SendToCallback(int32_t result);
+  // private:
+  //  void BindCallback(int32_t result,
+  //                    const base::Optional<net::IPEndPoint>& address);
+  //  void JoinGroupCallback(int32_t result);
+  //  void SendToCallback(int32_t result);
 
-  bool is_bound_ = false;
-  int32_t last_error_ = 0;
-  const net::IPEndPoint local_endpoint_;
-  std::unique_ptr<network::UDPSocket> udp_socket_;
-  base::WeakPtrFactory<ChromeUdpSocket> weak_ptr_factory_{this};
+  //  bool is_bound_ = false;
+  //  int32_t last_error_ = 0;
+  //  const net::IPEndPoint local_endpoint_;
+  //  std::unique_ptr<network::UDPSocket> udp_socket_;
+  //  base::WeakPtrFactory<ChromeUdpSocket> weak_ptr_factory_{this};
 };
 
 }  // namespace platform
