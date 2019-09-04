426adc834b32590172fc42700cd102926e839b1c
Andrii Shyshkalov <tandrii@google.com>
2019-09-04 21:05:38 +0000

    git cl try: stop supporting '-m|--master'.
    Use "git cl try -B <bucket>" instead.
    
    R=smut
    
    Bug: 570733
    Change-Id: Id5fedadede9fd8305e4567028ce4a4326d775cf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/1779545
    Commit-Queue: Andrii Shyshkalov <tandrii@google.com>
    Reviewed-by: smut <smut@google.com>

--- git_cl.py
+++ git_cl.py
@@ -432,10 +432,6 @@ def _get_bucket_map(changelist, options, option_parser):
 
   if options.bucket:
     return {options.bucket: {b: [] for b in options.bot}}
-  if options.master:
-    print(
-        'WARNING: "-m <master>" option is deprecated. Use -B <bucket> instead.')
-    return {_prefix_master(options.master): {b: [] for b in options.bot}}
   option_parser.error(
       'Please specify the bucket, e.g. "-B luci.chromium.try".')
 
@@ -4925,9 +4921,6 @@ def CMDtry(parser, args):
       '-B', '--bucket', default='',
       help=('Buildbucket bucket to send the try requests.'))
   group.add_option(
-      '-m', '--master', default='',
-      help=('DEPRECATED, use -B. The try master where to run the builds.'))
-  group.add_option(
       '-r', '--revision',
       help='Revision to use for the tryjob; default: the revision will '
            'be determined by the try recipe that builder runs, which usually '
@@ -4960,9 +4953,6 @@ def CMDtry(parser, args):
   _process_codereview_select_options(parser, options)
   auth_config = auth.extract_auth_config_from_options(options)
 
-  if options.master and options.master.startswith('luci.'):
-    parser.error(
-        '-m option does not support LUCI. Please pass -B %s' % options.master)
   # Make sure that all properties are prop=value pairs.
   bad_params = [x for x in options.properties if '=' not in x]
   if bad_params:
@@ -4981,9 +4971,6 @@ def CMDtry(parser, args):
   error_message = cl.CannotTriggerTryJobReason()
   if error_message:
     parser.error('Can\'t trigger tryjobs: %s' % error_message)
-
-  if options.bucket and options.master:
-    parser.error('Only one of --bucket and --master may be used.')
 
   buckets = _get_bucket_map(cl, options, parser)
 
