afeb978c050208b93733860752422698b4a27343
Alexandre Courbot <acourbot@chromium.org>
2019-09-04 08:37:55 +0000

    media/gpu/v4l2: add VP8 accelerator supporting upstream UAPI
    Add a new VP8 accelerator that supports the upstream UAPI for VP8.
    
    This is just a port of the legacy VP8 accelerator to use the upstream
    headers and request API. It has not been thoroughly tested yet.
    
    Bug: 965375
    Test: None yet.
    
    Change-Id: Ib06b2cc39a32beefa7314ae03d9ea15f8abc90f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1761883
    Commit-Queue: Alexandre Courbot <acourbot@chromium.org>
    Auto-Submit: Alexandre Courbot <acourbot@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693059}
    
    Former-commit-id: a562377de6012ca3a0e3983c0bce313597365d1f

--- media/gpu/v4l2/v4l2_slice_video_decode_accelerator.cc
+++ media/gpu/v4l2/v4l2_slice_video_decode_accelerator.cc
@@ -41,6 +41,7 @@
 #include "media/gpu/v4l2/v4l2_h264_accelerator_legacy.h"
 #include "media/gpu/v4l2/v4l2_image_processor.h"
 #include "media/gpu/v4l2/v4l2_vda_helpers.h"
+#include "media/gpu/v4l2/v4l2_vp8_accelerator.h"
 #include "media/gpu/v4l2/v4l2_vp8_accelerator_legacy.h"
 #include "media/gpu/v4l2/v4l2_vp9_accelerator.h"
 #include "ui/gl/gl_context.h"
@@ -287,8 +288,13 @@ bool V4L2SliceVideoDecodeAccelerator::Initialize(const Config& config,
     }
   } else if (video_profile_ >= VP8PROFILE_MIN &&
              video_profile_ <= VP8PROFILE_MAX) {
-    decoder_.reset(new VP8Decoder(
-        std::make_unique<V4L2LegacyVP8Accelerator>(this, device_.get())));
+    if (supports_requests_) {
+      decoder_.reset(new VP8Decoder(
+          std::make_unique<V4L2VP8Accelerator>(this, device_.get())));
+    } else {
+      decoder_.reset(new VP8Decoder(
+          std::make_unique<V4L2LegacyVP8Accelerator>(this, device_.get())));
+    }
   } else if (video_profile_ >= VP9PROFILE_MIN &&
              video_profile_ <= VP9PROFILE_MAX) {
     decoder_.reset(new VP9Decoder(
