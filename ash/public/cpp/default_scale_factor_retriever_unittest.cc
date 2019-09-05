48801913678a78aa6b253e5c8f25398ff6be3291
Gabriel Charette <gab@chromium.org>
2019-09-05 17:31:34 +0000

    Backport some tests in /ash to SingleThreadTaskEnvironment
    These tests were previously migrated from single-threaded MessageLoop to
    a multi-threaded TaskEnvironment (then named ScopedTaskEnvironment) as
    part of crbug.com/891670.
    
    //base OWNERS decided in retrospect that it was better to keep a
    single-threaded option for TaskEnvironment and introduced
    SingleThreadTaskEnvironment. This CL retrofits that decision for
    /ash.
    
    This CL is a no-op if it passes CQ.
    
    This CL was uploaded by git cl split.
    
    R=jamescook@chromium.org
    
    Bug: 891670
    Change-Id: Ia6c9e5450e044adca6f225e44796d5f58923e893
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787101
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693828}
    
    Former-commit-id: bd4308caa656943b76ea1bc952910632ba67976e

--- ash/public/cpp/default_scale_factor_retriever_unittest.cc
+++ ash/public/cpp/default_scale_factor_retriever_unittest.cc
@@ -73,7 +73,7 @@ class DefaultScaleFactorRetrieverTest : public testing::Test {
   ~DefaultScaleFactorRetrieverTest() override = default;
 
  private:
-  base::test::TaskEnvironment task_environment_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
   DISALLOW_COPY_AND_ASSIGN(DefaultScaleFactorRetrieverTest);
 };
 
