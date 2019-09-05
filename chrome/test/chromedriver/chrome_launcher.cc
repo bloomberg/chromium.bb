b8df9c6b6645cf9dcbda2b02d61f14e0408e58f7
John Chen <johnchen@chromium.org>
2019-09-05 19:01:46 +0000

    [ChromeDriver] Remove reference to obsolete trial
    Remove reference to field trial SiteIsolationExtensions/Control,
    no longer exists. However, have to keep force-fieldtrials switch
    due to https://crbug.com/996865.
    
    Change-Id: If6b644d0cb6d6c6e860a1be22b33a37ec6a3b4ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787989
    Auto-Submit: John Chen <johnchen@chromium.org>
    Reviewed-by: Rohan Pavone <rohpavone@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693896}
    
    Former-commit-id: ce580f7366129c8c1d5cd7fb801cecfcee82bcc3

--- chrome/test/chromedriver/chrome_launcher.cc
+++ chrome/test/chromedriver/chrome_launcher.cc
@@ -88,7 +88,10 @@ const char* const kDesktopSwitches[] = {
     "password-store=basic",
     "use-mock-keychain",
     "test-type=webdriver",
-    "force-fieldtrials=SiteIsolationExtensions/Control",
+    // In theory the following switch should have no effects, but its absence
+    // would cause VizHitTestSurfaceLayer feature to be enabled on Chromium
+    // builds, causing a test failure due to https://crbug.com/996865.
+    "force-fieldtrials",
     // TODO(yoichio): This is temporary switch to support chrome internal
     // components migration from the old web APIs.
     // After completion of the migration, we should remove this.
