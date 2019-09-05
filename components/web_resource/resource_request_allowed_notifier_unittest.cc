cfac167e71c687d936507b39a9d081a04abb134a
Gabriel Charette <gab@chromium.org>
2019-09-05 17:41:05 +0000

    Backport some tests in /components/web_resource/resource_request_allowed_notifier_unittest.cc to SingleThreadTaskEnvironment
    These tests were previously migrated from single-threaded MessageLoop to
    a multi-threaded TaskEnvironment (then named ScopedTaskEnvironment) as
    part of crbug.com/891670.
    
    //base OWNERS decided in retrospect that it was better to keep a
    single-threaded option for TaskEnvironment and introduced
    SingleThreadTaskEnvironment. This CL retrofits that decision for
    /components/web_resource/resource_request_allowed_notifier_unittest.cc.
    
    This CL is a no-op if it passes CQ.
    
    This CL was uploaded by git cl split.
    
    R=asvitkine@chromium.org
    
    Bug: 891670
    Change-Id: I13509fa01b3fadb6cffa203cba15975ac553e8db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786777
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693837}
    
    Former-commit-id: d7006cb65c188d129ef9fb7b426f516b643d5fef

--- components/web_resource/resource_request_allowed_notifier_unittest.cc
+++ components/web_resource/resource_request_allowed_notifier_unittest.cc
@@ -119,8 +119,8 @@ class ResourceRequestAllowedNotifierTest
   }
 
  private:
-  base::test::TaskEnvironment task_environment_{
-      base::test::TaskEnvironment::MainThreadType::UI};
+  base::test::SingleThreadTaskEnvironment task_environment_{
+      base::test::SingleThreadTaskEnvironment::MainThreadType::UI};
   TestRequestAllowedNotifier resource_request_allowed_notifier_;
   TestingPrefServiceSimple prefs_;
   TestEulaAcceptedNotifier* eula_notifier_;  // Weak, owned by RRAN.
