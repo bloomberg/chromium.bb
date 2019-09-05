5e97e8abc5906a6496aae3e5a4731e8741397714
Gabriel Charette <gab@chromium.org>
2019-09-05 17:14:45 +0000

    Backport some tests in /content/renderer/media to SingleThreadTaskEnvironment
    These tests were previously migrated from single-threaded MessageLoop to
    a multi-threaded TaskEnvironment (then named ScopedTaskEnvironment) as
    part of crbug.com/891670.
    
    //base OWNERS decided in retrospect that it was better to keep a
    single-threaded option for TaskEnvironment and introduced
    SingleThreadTaskEnvironment. This CL retrofits that decision for
    /content/renderer/media.
    
    This CL is a no-op if it passes CQ.
    
    This CL was uploaded by git cl split.
    
    R=hbos@chromium.org
    
    Bug: 891670
    Change-Id: Id30d6abf1228394025cf994fc3a92a7a31b3ecd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787182
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693805}
    
    Former-commit-id: afc84c3a20eb539402e640a971c4bb8f3c2d657d

--- content/renderer/media/android/stream_texture_wrapper_impl_unittest.cc
+++ content/renderer/media/android/stream_texture_wrapper_impl_unittest.cc
@@ -18,7 +18,7 @@ class StreamTextureWrapperImplTest : public testing::Test {
   StreamTextureWrapperImplTest() {}
 
   // Necessary, or else GetSingleThreadTaskRunnerForTesting() fails.
-  base::test::TaskEnvironment task_environment_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(StreamTextureWrapperImplTest);
