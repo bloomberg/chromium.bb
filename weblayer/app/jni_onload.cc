cc2c7e2219ebd7efe88bb9a925f83b08c3794ca4
Clark DuVall <cduvall@chromium.org>
2019-09-06 19:28:34 +0000

    [WebLayer] Refactor WebLayer native init
    This pulls out some changes from http://crrev.com/c/1783727 that are
    needed to make the WebLayer lib work when used with monochrome.
    
    Change-Id: Ia749d0b9c7efdfd5906a684346cce30ad9b2c7b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1788236
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694369}
    
    Former-commit-id: 0348602380a3080286aad898a78d0dfe53ded66e

--- weblayer/app/jni_onload.cc
+++ weblayer/app/jni_onload.cc
@@ -2,31 +2,34 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/android/jni_android.h"
+#include "base/android/library_loader/library_loader_hooks.h"
 #include "base/logging.h"
+#include "components/version_info/version_info_values.h"
 #include "content/public/app/content_jni_onload.h"
 #include "content/public/app/content_main.h"
 #include "weblayer/app/content_main_delegate_impl.h"
 
 namespace weblayer {
+
 class MainDelegateImpl : public MainDelegate {
  public:
   void PreMainMessageLoopRun() override {}
   void SetMainMessageLoopQuitClosure(base::OnceClosure quit_closure) override {}
 };
-}  // namespace weblayer
 
 // This is called by the VM when the shared library is first loaded.
-JNI_EXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
-  base::android::InitVM(vm);
+bool OnJNIOnLoadInit() {
   if (!content::android::OnJNIOnLoadInit())
-    return -1;
+    return false;
   weblayer::MainParams params;
   params.delegate = new weblayer::MainDelegateImpl;
   params.pak_name = "weblayer_support.pak";
   params.brand = "WebLayer";
 
+  base::android::SetVersionNumber(PRODUCT_VERSION);
   content::SetContentMainDelegate(
       new weblayer::ContentMainDelegateImpl(params));
-  return JNI_VERSION_1_4;
+  return true;
 }
+
+}  // namespace weblayer
