4ebfe4643bf06794bbf2620e4d2d8dda21f62987
Edward Lemur <ehmaldonado@chromium.org>
2019-09-06 20:34:03 +0000

    depot_tools: Fix flaky gclient_test test.
    Dependencies are processed in order, so they don't have to be sorted.
    target_os for each dependency is a set [1], so it should be sorted to avoid flaky failures.
    
    [1] https://cs.chromium.org/chromium/tools/depot_tools/gclient.py?l=368
    
    Bug: 984182
    Change-Id: I3e84f3cb85f9cb1920b7abf7a45766f3a518f4c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/1789278
    Auto-Submit: Edward Lesmes <ehmaldonado@chromium.org>
    Commit-Queue: Andrii Shyshkalov <tandrii@google.com>
    Reviewed-by: Andrii Shyshkalov <tandrii@google.com>

--- tests/gclient_test.py
+++ tests/gclient_test.py
@@ -565,8 +565,8 @@ class GclientTest(trial_dir.TestCase):
     obj = gclient.GClient.LoadCurrentConfig(options)
     obj.RunOnDeps('None', [])
     self.assertEqual(['unix'], sorted(obj.enforced_os))
-    self.assertEqual([('unix',), ('unix', 'baz')],
-                     sorted(dep.target_os for dep in obj.dependencies))
+    self.assertEqual([['baz', 'unix'], ['unix']],
+                     [sorted(dep.target_os) for dep in obj.dependencies])
     self.assertEqual([('foo', 'svn://example.com/foo'),
                       ('bar', 'svn://example.com/bar')],
                      self._get_processed())
