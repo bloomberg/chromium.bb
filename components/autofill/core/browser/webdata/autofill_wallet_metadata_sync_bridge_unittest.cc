3948ea90d4d6841dbb42813759adf67f2c07c03b
Gabriel Charette <gab@chromium.org>
2019-09-06 13:25:58 +0000

    Backport some tests in /components/autofill/core/browser/webdata/autofill_wallet_metadata_sync_bridge_unittest.cc to SingleThreadTaskEnvironment
    These tests were previously migrated from single-threaded MessageLoop to
    a multi-threaded TaskEnvironment (then named ScopedTaskEnvironment) as
    part of crbug.com/891670.
    
    //base OWNERS decided in retrospect that it was better to keep a
    single-threaded option for TaskEnvironment and introduced
    SingleThreadTaskEnvironment. This CL retrofits that decision for
    /components/autofill/core/browser/webdata/autofill_wallet_metadata_sync_bridge_unittest.cc.
    
    This CL is a no-op if it passes CQ.
    
    This CL was uploaded by git cl split.
    
    R=vitaliii@chromium.org
    
    Bug: 891670
    Change-Id: I2716a45c81b330e553938972f2d856dc96198ac3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786751
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: vitaliii <vitaliii@chromium.org>
    Commit-Queue: vitaliii <vitaliii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694222}
    
    Former-commit-id: 5a53ba6e5b9f20bfdadf25927080bfe5011583ce

--- components/autofill/core/browser/webdata/autofill_wallet_metadata_sync_bridge_unittest.cc
+++ components/autofill/core/browser/webdata/autofill_wallet_metadata_sync_bridge_unittest.cc
@@ -451,7 +451,7 @@ class AutofillWalletMetadataSyncBridgeTest : public testing::Test {
   int response_version = 0;
   autofill::TestAutofillClock test_clock_;
   ScopedTempDir temp_dir_;
-  base::test::TaskEnvironment task_environment_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
   testing::NiceMock<MockAutofillWebDataBackend> backend_;
   AutofillTable table_;
   WebDatabase db_;
