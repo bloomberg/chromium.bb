035074898a77f04488cd566925ff9de11672e868
Ahmed Fakhry <afakhry@google.com>
2019-09-06 18:23:27 +0000

    Desks: Exclude its shortcuts from the KSH if the feature is disabled
    Also, remove any references to the word "virtual", as from now on
    the feature will be just called "Desks".
    
    BUG=1001568
    TEST=Manually.
    
    Change-Id: If3832d2b58fb0b4c1c20ecd85887e86cc20dad58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1790285
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694327}
    
    Former-commit-id: 80ebbf82b241cb00a45616be841f4174f0a8d78d

--- ash/components/shortcut_viewer/views/keyboard_shortcut_view.cc
+++ ash/components/shortcut_viewer/views/keyboard_shortcut_view.cc
@@ -16,6 +16,7 @@
 #include "ash/components/strings/grit/ash_components_strings.h"
 #include "ash/public/cpp/app_list/internal_app_id_constants.h"
 #include "ash/public/cpp/app_types.h"
+#include "ash/public/cpp/ash_features.h"
 #include "ash/public/cpp/resources/grit/ash_public_unscaled_resources.h"
 #include "ash/public/cpp/shelf_item.h"
 #include "ash/public/cpp/window_properties.h"
@@ -130,6 +131,22 @@ void UpdateAXNodeDataPosition(
     shortcut_items.at(i)->GetViewAccessibility().OverridePosInSet(
         i + 1, number_shortcut_items);
   }
+}
+
+// Returns true if the given |item| should be excluded from the view, since
+// certain shortcuts can be associated with a disabled feature behind a flag.
+bool ShouldExcludeItem(const KeyboardShortcutItem& item) {
+  switch (item.description_message_id) {
+    case IDS_KSV_DESCRIPTION_DESKS_NEW_DESK:
+    case IDS_KSV_DESCRIPTION_DESKS_REMOVE_CURRENT_DESK:
+    case IDS_KSV_DESCRIPTION_DESKS_ACTIVATE_LEFT_DESK:
+    case IDS_KSV_DESCRIPTION_DESKS_ACTIVATE_RIGHT_DESK:
+    case IDS_KSV_DESCRIPTION_DESKS_MOVE_ACTIVE_ITEM_LEFT_DESK:
+    case IDS_KSV_DESCRIPTION_DESKS_MOVE_ACTIVE_ITEM_RIGHT_DESK:
+      return !ash::features::IsVirtualDesksEnabled();
+  }
+
+  return false;
 }
 
 }  // namespace
@@ -356,6 +373,9 @@ void KeyboardShortcutView::InitViews() {
   // clear the cache.
   KeyboardShortcutItemView::ClearKeycodeToString16Cache();
   for (const auto& item : GetKeyboardShortcutItemList()) {
+    if (ShouldExcludeItem(item))
+      continue;
+
     for (auto category : item.categories) {
       shortcut_views_.emplace_back(
           std::make_unique<KeyboardShortcutItemView>(item, category));
