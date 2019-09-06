246e6556921e64360f8c92604654f71b97b8bf57
Gabriel Charette <gab@chromium.org>
2019-09-06 18:04:24 +0000

    Backport some tests in /chrome/browser/android/contextualsearch to SingleThreadTaskEnvironment
    These tests were previously migrated from single-threaded MessageLoop to
    a multi-threaded TaskEnvironment (then named ScopedTaskEnvironment) as
    part of crbug.com/891670.
    
    //base OWNERS decided in retrospect that it was better to keep a
    single-threaded option for TaskEnvironment and introduced
    SingleThreadTaskEnvironment. This CL retrofits that decision for
    /chrome/browser/android/contextualsearch.
    
    This CL is a no-op if it passes CQ.
    
    This CL was uploaded by git cl split.
    
    R=donnd@chromium.org
    
    Bug: 891670
    Change-Id: I4eafad717c740b5168d1f61dd7ad65fdafd756fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786755
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694315}
    
    Former-commit-id: 657599446962f987c38b1c54fd0457f233ef1a3d

--- chrome/browser/android/contextualsearch/contextual_search_delegate_unittest.cc
+++ chrome/browser/android/contextualsearch/contextual_search_delegate_unittest.cc
@@ -288,8 +288,8 @@ class ContextualSearchDelegateTest : public testing::Test {
   std::string search_url_preload_;
   int coca_card_tag_;
 
-  base::test::TaskEnvironment task_environment_{
-      base::test::TaskEnvironment::MainThreadType::IO};
+  base::test::SingleThreadTaskEnvironment task_environment_{
+      base::test::SingleThreadTaskEnvironment::MainThreadType::IO};
   std::unique_ptr<TemplateURLService> template_url_service_;
   scoped_refptr<network::SharedURLLoaderFactory>
       test_shared_url_loader_factory_;
