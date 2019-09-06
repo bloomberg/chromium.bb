ce6583c9b1a6971b9e9af53ec77dd1afdb2308ef
Thanh Nguyen <thanhdng@chromium.org>
2019-09-06 04:44:37 +0000

    [Cros-fuzzy-app] Add block matching ratio.
    Implementation of ratio calculation using block matching algorithm.
    
    Bug: 990684
    Change-Id: I98a05ca64e1acd58add8a6d15dc32d075a357969
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1778895
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Thanh Nguyen <thanhdng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694131}
    
    Former-commit-id: d9e38a4638f10e0855d6cece1d3a731626da04b4

--- chrome/browser/ui/app_list/search/search_utils/sequence_matcher.h
+++ chrome/browser/ui/app_list/search/search_utils/sequence_matcher.h
@@ -6,6 +6,8 @@
 #define CHROME_BROWSER_UI_APP_LIST_SEARCH_SEARCH_UTILS_SEQUENCE_MATCHER_H_
 
 #include <string>
+#include <unordered_map>
+#include <vector>
 
 #include "base/logging.h"
 #include "base/macros.h"
@@ -17,11 +19,21 @@ namespace app_list {
 // (https://docs.python.org/2/library/difflib.html#difflib.SequenceMatcher)
 class SequenceMatcher {
  public:
+  // Representing a common substring between |first_string_| and
+  // |second_string_|.
+  struct Match {
+    Match();
+    Match(int pos_first, int pos_second, int len);
+    // Starting position of the common substring in |first_string_|.
+    int pos_first_string;
+    // Starting position of the common substring in |second_string_|.
+    int pos_second_string;
+    // Length of the common substring.
+    int length;
+  };
   SequenceMatcher(const std::string& first_string,
-                  const std::string& second_string)
-      : first_string_(first_string), second_string_(second_string) {
-    DCHECK(!first_string_.empty() || !second_string_.empty());
-  }
+                  const std::string& second_string);
+
   ~SequenceMatcher() = default;
 
   // Calculates similarity ratio of |first_string_| and |second_string_|.
@@ -33,13 +45,33 @@ class SequenceMatcher {
   // See https://en.wikipedia.org/wiki/Damerau–Levenshtein_distance for more
   // details.
   int EditDistance();
+  // Finds the longest common substring between
+  // |first_string_[first_start:first_end]| and
+  // |second_string_[second_start:second_end]|.
+  Match FindLongestMatch(int first_start,
+                         int first_end,
+                         int second_start,
+                         int second_end);
+  // Get all matching blocks of |first_string_| and |second_string_|.
+  // All blocks will be sorted by starting position of them in the
+  // |first_string_|. The last matching block will always be
+  // Match(first_string_.size(), second_string_.size(), 0).
+  std::vector<Match> GetMatchingBlocks();
 
  private:
-  const std::string first_string_;
-  const std::string second_string_;
+  std::string first_string_;
+  std::string second_string_;
   double edit_distance_ratio_ = -1.0;
   double block_matching_ratio_ = -1.0;
+  std::vector<Match> matching_blocks_;
+
   int edit_distance_ = -1;
+  // For each character |c| in |second_string_|, this variable
+  // |char_to_positions_| stores all positions where |c| occurs in
+  // |second_string_|.
+  std::unordered_map<char, std::vector<int>> char_to_positions_;
+  // Memory for dynamic programming algorithm used in FindLongestMatch().
+  std::vector<int> dp_common_string_;
   DISALLOW_COPY_AND_ASSIGN(SequenceMatcher);
 };
 
--- chrome/browser/ui/app_list/search/search_utils/sequence_matcher_unittest.cc
+++ chrome/browser/ui/app_list/search/search_utils/sequence_matcher_unittest.cc
@@ -10,6 +10,15 @@
 
 namespace app_list {
 
+namespace {
+using Match = SequenceMatcher::Match;
+bool MatchEqual(const Match& match1, const Match& match2) {
+  return match1.pos_first_string == match2.pos_first_string &&
+         match1.pos_second_string == match2.pos_second_string &&
+         match1.length == match2.length;
+}
+}  // namespace
+
 class SequenceMatcherTest : public testing::Test {};
 
 TEST_F(SequenceMatcherTest, TestEditDistance) {
@@ -30,6 +39,36 @@ TEST_F(SequenceMatcherTest, TestEditDistance) {
   // Combination
   ASSERT_EQ(SequenceMatcher("caclulation", "calculator").EditDistance(), 3);
   ASSERT_EQ(SequenceMatcher("sunday", "saturday").EditDistance(), 3);
+}
+
+TEST_F(SequenceMatcherTest, TestFindLongestMatch) {
+  SequenceMatcher sequence_match("miscellanious", "miscellaneous");
+  ASSERT_TRUE(MatchEqual(sequence_match.FindLongestMatch(0, 13, 0, 13),
+                         Match(0, 0, 9)));
+  ASSERT_TRUE(MatchEqual(sequence_match.FindLongestMatch(7, 13, 7, 13),
+                         Match(10, 10, 3)));
+
+  ASSERT_TRUE(
+      MatchEqual(SequenceMatcher("", "abcd").FindLongestMatch(0, 0, 0, 4),
+                 Match(0, 0, 0)));
+  ASSERT_TRUE(MatchEqual(
+      SequenceMatcher("abababbababa", "ababbaba").FindLongestMatch(0, 12, 0, 8),
+      Match(2, 0, 8)));
+  ASSERT_TRUE(MatchEqual(
+      SequenceMatcher("aaaaaa", "aaaaa").FindLongestMatch(0, 6, 0, 5),
+      Match(0, 0, 5)));
+}
+
+TEST_F(SequenceMatcherTest, TestGetMatchingBlocks) {
+  SequenceMatcher sequence_match("This is a demo sentence!!!",
+                                 "This demo sentence is good!!!");
+  const std::vector<Match> true_matches = {Match(0, 0, 4), Match(9, 4, 14),
+                                           Match(23, 26, 3), Match(26, 29, 0)};
+  const std::vector<Match> matches = sequence_match.GetMatchingBlocks();
+  ASSERT_EQ(matches.size(), 4ul);
+  for (int i = 0; i < 4; i++) {
+    ASSERT_TRUE(MatchEqual(matches[i], true_matches[i]));
+  }
 }
 
 }  // namespace app_list
