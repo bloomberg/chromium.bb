53959c3666e9ccf312bf07bc6c4b916a12a74087
Yannic Bonenberger <contact@yannic-bonenberger.com>
2019-09-07 17:32:07 +0000

    Apply base_bind_rewriters to //base/callback.h
    *** Note: There is no behavior change from this patch. ***
    
    Bug: 714018
    Change-Id: I9969eec66114931542e927758553118ebe3a4f2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1778494
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Yannic Bonenberger <contact@yannic-bonenberger.com>
    Auto-Submit: Yannic Bonenberger <contact@yannic-bonenberger.com>
    Cr-Commit-Position: refs/heads/master@{#694592}
    
    Former-commit-id: c93f9a04be3e476974c46c6473bebe1705135c0a

--- base/callback.h
+++ base/callback.h
@@ -140,7 +140,7 @@ class RepeatingCallback<R(Args...)> : public internal::CallbackBaseCopyable {
     RepeatingCallback cb = std::move(*this);
     PolymorphicInvoke f =
         reinterpret_cast<PolymorphicInvoke>(cb.polymorphic_invoke());
-    return f(cb.bind_state_.get(), std::forward<Args>(args)...);
+    return f(std::move(cb).bind_state_.get(), std::forward<Args>(args)...);
   }
 };
 
