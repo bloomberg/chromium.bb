7fa1bc6811190bb4e55d5a2b0b2591497e002815
Ted Meyer <tmathmeyer@chromium.org>
2019-09-08 06:36:49 +0000

    The one time tricium could have been useful
    Change-Id: I1e48eb0a2a0a42052fc078682a4766a43422ce45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1790585
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694613}
    
    Former-commit-id: b76f4893588adbcb00bd837857205dfb832ba66e

--- media/base/media_switches.cc
+++ media/base/media_switches.cc
@@ -271,7 +271,7 @@ const base::Feature kD3D11VideoDecoderIgnoreWorkarounds{
 
 // Enable D3D11VideoDecoder to decode VP9 profile 2 (10 bit) video.
 const base::Feature kD3D11VideoDecoderVP9Profile2{
-    "D3D11VideoDecoderEnablbleVP9Profile2", base::FEATURE_DISABLED_BY_DEFAULT};
+    "D3D11VideoDecoderEnableVP9Profile2", base::FEATURE_DISABLED_BY_DEFAULT};
 
 // Falls back to other decoders after audio/video decode error happens. The
 // implementation may choose different strategies on when to fallback. See
