63ec0aec5f40b2bae5e34a7f60e2ff986a06c8b3
Gabriel Charette <gab@chromium.org>
2019-09-09 07:40:03 +0000

    Backport some tests in /chromeos/cryptohome to SingleThreadTaskEnvironment
    These tests were previously migrated from single-threaded MessageLoop to
    a multi-threaded TaskEnvironment (then named ScopedTaskEnvironment) as
    part of crbug.com/891670.
    
    //base OWNERS decided in retrospect that it was better to keep a
    single-threaded option for TaskEnvironment and introduced
    SingleThreadTaskEnvironment. This CL retrofits that decision for
    /chromeos/cryptohome.
    
    This CL is a no-op if it passes CQ.
    
    This CL was uploaded by git cl split.
    
    R=hashimoto@chromium.org
    
    Bug: 891670
    Change-Id: I682201afc209ceed93ea2f248580506db382fe5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787076
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694661}
    
    Former-commit-id: 6316bf0da131d06718e5b21f6ad2fd748fed6930

--- chromeos/cryptohome/system_salt_getter_unittest.cc
+++ chromeos/cryptohome/system_salt_getter_unittest.cc
@@ -24,7 +24,8 @@ void CopySystemSalt(std::string* out_system_salt,
 class SystemSaltGetterTest : public testing::Test {
  protected:
   SystemSaltGetterTest()
-      : task_environment_(base::test::TaskEnvironment::MainThreadType::UI) {}
+      : task_environment_(
+            base::test::SingleThreadTaskEnvironment::MainThreadType::UI) {}
 
   void SetUp() override {
     CryptohomeClient::InitializeFake();
@@ -40,7 +41,7 @@ class SystemSaltGetterTest : public testing::Test {
     CryptohomeClient::Shutdown();
   }
 
-  base::test::TaskEnvironment task_environment_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
 };
 
 TEST_F(SystemSaltGetterTest, GetSystemSalt) {
