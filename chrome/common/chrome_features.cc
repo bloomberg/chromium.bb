ab4f308345de7edd59cc90f4859a57713c9e1c50
Ilya Bizyaev <ilyabiz@google.com>
2019-09-09 12:08:34 +0000

    Add feature flag for Crostini Ansible software management playbooks
    This adds a feature flag that will be used in the near future
    to test experimental functionality for Ansible software management
    playbook generation from JSON schema.
    
    This is intentionally separated in meaning from the
    CrostiniAnsibleInfrastructure flag, which controls Ansible
    infrastructure as a whole.
    
    Test: compile
    Bug: 998124
    Change-Id: I38fc61a4bf2c717386019ddabe8e9691c3c78be7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787552
    Reviewed-by: Olya Kalitova <okalitova@chromium.org>
    Commit-Queue: Ilya Bizyaev <ilyabiz@google.com>
    Cr-Commit-Position: refs/heads/master@{#694707}
    
    Former-commit-id: c87d91141fc89a2a3ae3f26dd6a18ada68526540

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -197,6 +197,11 @@ const base::Feature kCrostiniAdvancedAccessControls{
 const base::Feature kCrostiniAnsibleInfrastructure{
     "CrostiniAnsibleInfrastructure", base::FEATURE_DISABLED_BY_DEFAULT};
 
+// Enables infrastructure for generating Ansible playbooks for the default
+// Crostini container from software configurations in JSON schema.
+const base::Feature kCrostiniAnsibleSoftwareManagement{
+    "CrostiniAnsibleSoftwareManagement", base::FEATURE_DISABLED_BY_DEFAULT};
+
 // Enables or disables the UI overhaul for Cups Printers in settings page.
 const base::Feature kCupsPrintersUiOverhaul{"CupsPrintersUiOverhaul",
                                             base::FEATURE_ENABLED_BY_DEFAULT};
--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -125,6 +125,8 @@ extern const base::Feature kCrostiniAdvancedAccessControls;
 COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kCrostiniAnsibleInfrastructure;
 COMPONENT_EXPORT(CHROME_FEATURES)
+extern const base::Feature kCrostiniAnsibleSoftwareManagement;
+COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kCupsPrintersUiOverhaul;
 COMPONENT_EXPORT(CHROME_FEATURES) extern const base::Feature kPluginVm;
 COMPONENT_EXPORT(CHROME_FEATURES)
