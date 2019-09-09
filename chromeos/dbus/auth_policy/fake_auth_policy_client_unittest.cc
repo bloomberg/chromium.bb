a86d53e787fad08fd4334e5267f34f5d98b5c47f
Gabriel Charette <gab@chromium.org>
2019-09-09 15:18:37 +0000

    Backport some tests in /chromeos/dbus/auth_policy to SingleThreadTaskEnvironment
    These tests were previously migrated from single-threaded MessageLoop to
    a multi-threaded TaskEnvironment (then named ScopedTaskEnvironment) as
    part of crbug.com/891670.
    
    //base OWNERS decided in retrospect that it was better to keep a
    single-threaded option for TaskEnvironment and introduced
    SingleThreadTaskEnvironment. This CL retrofits that decision for
    /chromeos/dbus/auth_policy.
    
    This CL is a no-op if it passes CQ.
    
    This CL was uploaded by git cl split.
    
    R=tnagel@chromium.org
    
    Bug: 891670
    Change-Id: I4b88ebcfc2c108c02a6ff8987d121f24bb7c5195
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786827
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Thiemo Nagel <tnagel@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694771}
    
    Former-commit-id: 772a281dcc22993f10810ab8460ae804755023d2

--- chromeos/dbus/auth_policy/fake_auth_policy_client_unittest.cc
+++ chromeos/dbus/auth_policy/fake_auth_policy_client_unittest.cc
@@ -98,7 +98,7 @@ class FakeAuthPolicyClientTest : public ::testing::Test {
 
  private:
   ScopedStubInstallAttributes install_attributes_;
-  base::test::TaskEnvironment task_environment_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
 
   DISALLOW_COPY_AND_ASSIGN(FakeAuthPolicyClientTest);
 };
