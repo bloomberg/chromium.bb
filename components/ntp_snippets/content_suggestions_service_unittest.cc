490aea00de150c9badef1a02029988fe14660532
Gabriel Charette <gab@chromium.org>
2019-09-10 21:19:12 +0000

    Backport some tests in /components/ntp_snippets to SingleThreadTaskEnvironment
    These tests were previously migrated from single-threaded MessageLoop to
    a multi-threaded TaskEnvironment (then named ScopedTaskEnvironment) as
    part of crbug.com/891670.
    
    //base OWNERS decided in retrospect that it was better to keep a
    single-threaded option for TaskEnvironment and introduced
    SingleThreadTaskEnvironment. This CL retrofits that decision for
    /components/ntp_snippets.
    
    This CL is a no-op if it passes CQ.
    
    This CL was uploaded by git cl split.
    
    R=noyau@chromium.org
    
    Bug: 891670
    Change-Id: Ib3206c1a7c5241a2f8f6d515f7f321be5058519f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787205
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Eric Noyau <noyau@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695326}
    
    Former-commit-id: c30de0a2df6468993942158d1ce0dbcd5a044b90

--- components/ntp_snippets/content_suggestions_service_unittest.cc
+++ components/ntp_snippets/content_suggestions_service_unittest.cc
@@ -259,7 +259,7 @@ TEST_F(ContentSuggestionsServiceTest, ShouldRedirectFetchSuggestionImage) {
 
 TEST_F(ContentSuggestionsServiceTest,
        ShouldCallbackEmptyImageForUnavailableProvider) {
-  base::test::TaskEnvironment task_environment;
+  base::test::SingleThreadTaskEnvironment task_environment;
 
   base::RunLoop run_loop;
   // Assuming there will never be a category with the id below.
