f4776794d95dc967fc72597b59709683e02dce21
Takashi Toyoshima <toyoshim@chromium.org>
2019-09-10 10:52:39 +0000

    Revert "Reland "Notification scheduler: Add a browser test.""
    This reverts commit 2cf7a93a5087f8f4349c4dec107723926f75e1b7 [formerly 2eda3ac7ced48a1ff28e38a2d0bf18b4f4028b2b].
    
    Reason for revert: continues to fail on Linux Tests Builder.
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20Tests
    
    Original change's description:
    > Reland "Notification scheduler: Add a browser test."
    > 
    > Reverted in
    > https://chromium-review.googlesource.com/c/chromium/src/+/1787277
    > due to flakiness on chromeos builders.
    > 
    > This CL tries to reland the browser test that the test seems no longer
    > failed on those trybots. Also can't reproduce the test failure on
    > local machine with linux-chromeos build.
    > 
    > TBR=dtrainor@chromium.org
    > 
    > Bug: 1001003,963295
    > Change-Id: Ia1b553aa46f9256b9a94004d2426cf35bc9bf4e6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1793821
    > Reviewed-by: Xing Liu <xingliu@chromium.org>
    > Commit-Queue: Xing Liu <xingliu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#695001}
    
    TBR=dtrainor@chromium.org,xingliu@chromium.org,hesen@chromium.org
    
    Change-Id: I3bf84db4e976a7c79a79122dd14282d7ee73f61d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1001003, 963295
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1795522
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695099}
    
    Former-commit-id: feae6e0ee85b889a915b2e33cc714dc6d045d45f

--- chrome/browser/notifications/scheduler/public/notification_schedule_service.h
+++ chrome/browser/notifications/scheduler/public/notification_schedule_service.h
@@ -42,10 +42,9 @@ class NotificationScheduleService : public KeyedService {
   // Returns the user action handler to process notification events.
   virtual UserActionHandler* GetUserActionHandler() = 0;
 
-  ~NotificationScheduleService() override = default;
-
  protected:
   NotificationScheduleService() = default;
+  ~NotificationScheduleService() override = default;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(NotificationScheduleService);
