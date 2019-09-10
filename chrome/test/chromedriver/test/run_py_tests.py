6eed38b128420be8d183d8db926725ec1d932181
Lan Wei <lanwei@chromium.org>
2019-09-10 16:33:30 +0000

    Enable chromedriver and webdriver tests
    These tests are flaky when we send the events asynchronously, now we
    change to send the last action in one tick synchronously by default.
    Now the test should not be flaky any more.
    
    Bug: 999814, 1000530
    Change-Id: I3fbc438b74c1912983c6767e77fc94c386aea256
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1795745
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695205}
    
    Former-commit-id: e010e4774e45584ba3e6510266e7d46361366f63

--- chrome/test/chromedriver/test/run_py_tests.py
+++ chrome/test/chromedriver/test/run_py_tests.py
@@ -101,8 +101,6 @@ _OS_SPECIFIC_FILTER['win'] = [
     'ChromeDownloadDirTest.testFileDownloadWithGetHeadless',
 ]
 _OS_SPECIFIC_FILTER['linux'] = [
-    # https://bugs.chromium.org/p/chromium/issues/detail?id=1000530
-    'ChromeDriverTest.testActionsMouseMove',
 ]
 _OS_SPECIFIC_FILTER['mac'] = [
     # https://bugs.chromium.org/p/chromedriver/issues/detail?id=1927
@@ -112,8 +110,6 @@ _OS_SPECIFIC_FILTER['mac'] = [
     'ChromeDownloadDirTest.testFileDownloadAfterTabHeadless',
     'ChromeDownloadDirTest.testFileDownloadWithClickHeadless',
     'ChromeDownloadDirTest.testFileDownloadWithGetHeadless',
-    # https://bugs.chromium.org/p/chromium/issues/detail?id=1000530
-    'ChromeDriverTest.testActionsMouseMove',
 ]
 
 _DESKTOP_NEGATIVE_FILTER = [
