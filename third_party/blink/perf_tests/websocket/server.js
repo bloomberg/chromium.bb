20920b2dc824f30d82f03e08143df663cf1ce907
Yoichi Osato <yoichio@chromium.org>
2019-09-10 10:03:52 +0000

    [WebSocket] Extend WebSocket npm server to send 1MB of ASCII text
    So that we can test with query:
    websocket/receive-arraybuffer-1MBx100.html?iteration=10&server=ws://127.0.0.1:8001/text
    
    Result
    Chrome M78:   144 MB/s
    Firefox 69.0: 189 MB/s
    
    Bug: 1001009
    Change-Id: I41089a0098de51d9b0ea12695eb7e82450f3675e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1794324
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Auto-Submit: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695091}
    
    Former-commit-id: 811d4910e705acf0e1b3b794e695a8f9f63d127c

--- third_party/blink/perf_tests/websocket/server.js
+++ third_party/blink/perf_tests/websocket/server.js
@@ -7,11 +7,31 @@ const getNow = () => {
 console.log(getNow() + " WebSocket server started.");
 const arrayBuf = 1000*1000; // 1MB
 const totalIter = 100;
-ws_server.on('connection', function(ws_socket) {
-  console.log(getNow() + " Connection established.");
-  const data = new ArrayBuffer(arrayBuf);
-  for (let i = 0; i < totalIter; i++) {
-    ws_socket.send(data, {binary: true});
+
+const charArray1000 = [];
+for (i = 0; i < 1000; i++) {
+  charArray1000.push(i % 128);
+}
+const asciiArray1K = String.fromCharCode.apply(this, charArray1000);
+const textArray1M = [];
+for (i = 0; i < 1000; i++) {
+  textArray1M.push(asciiArray1K);
+}
+const asciiText1MB = textArray1M.join('');
+
+ws_server.on('connection', function(ws_socket, request) {
+  console.log(getNow() + ' Connection established. url=' + request.url);
+  if (request.url === '/') {
+    const data = new ArrayBuffer(arrayBuf);
+    for (let i = 0; i < totalIter; i++) {
+      ws_socket.send(data, {binary: true});
+    }
+  } else if (request.url === '/text') {
+    for (let i = 0; i < totalIter; i++) {
+      ws_socket.send(asciiText1MB);
+    }
+  } else {
+    console.log('Invalid request: ' + request.url);
   }
   ws_socket.close();
   console.log(getNow() + " Connection closed.");
