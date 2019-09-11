6e1dd3e065aac3f346b4df9dad846a5a58ed12b5
Tiancong Wang <tcwang@google.com>
2019-09-11 10:36:04 -0700

    afdo: Increase AFDO_ALLOWED_STALE_DAYS when checking AFDO freshness
    In legacy PFQ, we check AFDO freshness to make sure the profile to
    be within AFDO_ALLOWED_STALE_DAYS old. This blocks the PFQ right now
    with no new profiles uploaded recently. Increase the limit as the
    legacy PFQ going away soon and new mechanism is not using the same
    checking.
    
    BUG=chromium:1002803
    TEST=None
    
    Change-Id: Icf92ebdc6c9a2fd2b1e412c904964231509c2cb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/1798744
    Tested-by: Tiancong Wang <tcwang@google.com>
    Reviewed-by: Caroline Tice <cmtice@chromium.org>
    Reviewed-by: Manoj Gupta <manojgupta@chromium.org>

--- cbuildbot/afdo.py
+++ cbuildbot/afdo.py
@@ -64,7 +64,7 @@ AFDO_GENERATE_LLVM_PROF = '/usr/bin/create_llvm_prof'
 # away, it is considered valid.
 
 # How old can the AFDO data be? (in days).
-AFDO_ALLOWED_STALE_DAYS = 14
+AFDO_ALLOWED_STALE_DAYS = 30
 
 # How old can the AFDO data be? (in difference of builds).
 AFDO_ALLOWED_STALE_BUILDS = 7
