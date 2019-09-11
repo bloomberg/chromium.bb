6dfd07f71bdd794077d68ba2a272b743db429f4a
Lucas Gadani <lfg@chromium.org>
2019-09-11 17:16:09 +0000

    Revert "Add DumpWithoutCrashing to debug https://crbug.com/996337."
    This reverts commit 47f90e6abc374c185a028cbbc44bb4f3270b5358 [formerly d631bcd2526fd28e1a4ba0ea6bb69b45b014ef1f].
    
    Reason for revert: We have enough data from this dump.
    
    Original change's description:
    > Add DumpWithoutCrashing to debug https://crbug.com/996337.
    > 
    > Bug: 996337
    > Change-Id: I18d8fee2f27842abd3ab48cb5bde90815afba5ca
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1785078
    > Commit-Queue: Lucas Gadani <lfg@chromium.org>
    > Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#694873}
    
    TBR=alexmos@chromium.org,lfg@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 996337
    Change-Id: I8ccca0add4031683153f2407b2c1120bcb05eaec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1797695
    Reviewed-by: Lucas Gadani <lfg@chromium.org>
    Commit-Queue: Lucas Gadani <lfg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695684}
    
    Former-commit-id: 68db488dda804d80bfea7be08861ad06b6acfa34

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -7032,11 +7032,6 @@ void RenderFrameHostImpl::SendCommitNavigation(
     mojo::PendingRemote<network::mojom::URLLoaderFactory>
         prefetch_loader_factory,
     const base::UnguessableToken& devtools_navigation_token) {
-  // TODO(lfg): Temporary to debug https://crbug.com/996337.
-  if (common_params->url.SchemeIsCryptographic() &&
-      response_head.ssl_info.has_value() && !response_head.ssl_info->cert) {
-    base::debug::DumpWithoutCrashing();
-  }
   if (navigation_client) {
     navigation_client->CommitNavigation(
         std::move(common_params), std::move(commit_params), response_head,
