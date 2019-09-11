26b0ff9b2929846b0b3c00551442e2bc79f57400
Khushal <khushalsagar@chromium.org>
2019-09-11 23:15:04 +0000

    gpu: Disallow use of IOSurfaces for half-float format with swiftshader.
    R=kbr@chromium.org
    
    Bug: 998038
    Change-Id: Ic31d28938ef205b36657fc7bd297fe8a63d08543
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1798052
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Auto-Submit: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695826}
    
    Former-commit-id: cbc5d5153b18ea387f4769caa01d1339261f6ed6

--- gpu/command_buffer/service/feature_info.cc
+++ gpu/command_buffer/service/feature_info.cc
@@ -426,6 +426,11 @@ void FeatureInfo::EnableOESTextureHalfFloatLinear() {
     return;
   AddExtensionString("GL_OES_texture_half_float_linear");
   feature_flags_.enable_texture_half_float_linear = true;
+
+  // TODO(capn) : Re-enable this once we have ANGLE+SwiftShader supporting
+  // IOSurfaces.
+  if (workarounds_.disable_half_float_for_gmb)
+    return;
   feature_flags_.gpu_memory_buffer_formats.Add(gfx::BufferFormat::RGBA_F16);
 }
 
