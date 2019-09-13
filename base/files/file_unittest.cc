dd302287774be297b78775a126c0ce5f100b345a
Wei Li <weili@chromium.org>
2019-09-13 00:32:57 +0000

    Fix File.GetInfo() to return real creation time on Mac/iOS
    Previously, all POSIX platform returns a file/path's last status change
    time as the creation time. On Mac/iOS, the actual birth time is actually
    returned along with other timestamps. This CL fixes to use the real
    birth time as the creation time for files.
    
    For the associated bug, we planned to launch Happiness Tracking Survey
    (HaTS) to users who have used Chrome for longer than 30 days. We rely on
    using a user profile's creation time to approximate the time this user
    started to use Chrome. However, due to aformentioned bug, the creation
    time of a user's profile is always updated to the current time, so we
    had no chance to bring up the survey on Mac.
    
    BUG=1002756
    
    Change-Id: I29436b520087b8e4d358869279edfce0250890a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1799308
    Commit-Queue: Wei Li <weili@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696247}
    
    Former-commit-id: 80fd52f138e4379b64237c11625d30763a064176

--- base/files/file_unittest.cc
+++ base/files/file_unittest.cc
@@ -432,6 +432,29 @@ TEST(FileTest, DISABLED_TouchGetInfo) {
             creation_time.ToInternalValue());
 }
 
+// Test we can retrieve the file's creation time through File::GetInfo().
+TEST(FileTest, GetInfoForCreationTime) {
+  int64_t before_creation_time_s =
+      base::Time::Now().ToDeltaSinceWindowsEpoch().InSeconds();
+
+  base::ScopedTempDir temp_dir;
+  ASSERT_TRUE(temp_dir.CreateUniqueTempDir());
+  FilePath file_path = temp_dir.GetPath().AppendASCII("test_file");
+  File file(file_path, base::File::FLAG_CREATE | base::File::FLAG_READ |
+                           base::File::FLAG_WRITE);
+  EXPECT_TRUE(file.IsValid());
+
+  int64_t after_creation_time_s =
+      base::Time::Now().ToDeltaSinceWindowsEpoch().InSeconds();
+
+  base::File::Info info;
+  EXPECT_TRUE(file.GetInfo(&info));
+  EXPECT_GE(info.creation_time.ToDeltaSinceWindowsEpoch().InSeconds(),
+            before_creation_time_s);
+  EXPECT_LE(info.creation_time.ToDeltaSinceWindowsEpoch().InSeconds(),
+            after_creation_time_s);
+}
+
 TEST(FileTest, ReadAtCurrentPosition) {
   base::ScopedTempDir temp_dir;
   ASSERT_TRUE(temp_dir.CreateUniqueTempDir());
