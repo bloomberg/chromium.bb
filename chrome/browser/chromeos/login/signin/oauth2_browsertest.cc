25c5d7f892e679840194e24d3e6812bab0a3b188
Ioana Pandele <ioanap@chromium.org>
2019-09-13 13:43:10 +0000

    Disable flaky OAuth2.MergeSession browser test
    TBR=antrim@chromium.org
    
    Bug: 1003772
    Change-Id: I7ff8a25128d60e667c235165ea833de6a3ea4cd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1803292
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696371}
    
    Former-commit-id: c6fc73dae635b8f840e407aa15e6c932c1dc929b

--- chrome/browser/chromeos/login/signin/oauth2_browsertest.cc
+++ chrome/browser/chromeos/login/signin/oauth2_browsertest.cc
@@ -568,7 +568,8 @@ IN_PROC_BROWSER_TEST_F(OAuth2Test, PRE_MergeSession) {
 // MergeSession test is attempting to merge session for an existing profile
 // that was generated in PRE_PRE_MergeSession test. This attempt should fail
 // since FakeGaia instance isn't configured to return relevant tokens/cookies.
-IN_PROC_BROWSER_TEST_F(OAuth2Test, MergeSession) {
+// Disabling because of flakiness. Details in crbug.com/1003772.
+IN_PROC_BROWSER_TEST_F(OAuth2Test, DISABLED_MergeSession) {
   SimulateNetworkOnline();
 
   content::WindowedNotificationObserver(
