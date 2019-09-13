83d7f8785180c19c33a0b424be1b629b6929a1bd
Sebastien Marchand <sebmarchand@chromium.org>
2019-09-13 17:03:49 +0000

    [PM] Remove some dead code from ProcessMetricsDecorator
    Change-Id: Ic01b48864914f13e16f02441ac6e89a8f0bc4fc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1792854
    Auto-Submit: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696432}
    
    Former-commit-id: 1064d1a322104a8b27d7e64d0e577b4ad15a6ea4

--- chrome/browser/performance_manager/decorators/process_metrics_decorator.h
+++ chrome/browser/performance_manager/decorators/process_metrics_decorator.h
@@ -12,14 +12,10 @@
 
 namespace performance_manager {
 
-class ProcessNode;
-
 // The ProcessMetricsDecorator is responsible for adorning process nodes with
 // performance metrics.
 class ProcessMetricsDecorator : public GraphOwned {
  public:
-  class Data;
-
   ProcessMetricsDecorator();
   ~ProcessMetricsDecorator() override;
 
@@ -62,18 +58,6 @@ class ProcessMetricsDecorator : public GraphOwned {
 
   base::WeakPtrFactory<ProcessMetricsDecorator> weak_factory_{this};
   DISALLOW_COPY_AND_ASSIGN(ProcessMetricsDecorator);
-};
-
-class ProcessMetricsDecorator::Data {
- public:
-  // This is roughly private, anonymous, non-discardable, resident or swapped
-  // memory in kilobytes. For more details, see https://goo.gl/3kPb9S.
-  uint32_t private_footprint_kb_ = 0;
-
-  // The resident set of this process.
-  uint32_t resident_set_kb_ = 0;
-
-  static Data* GetForTesting(ProcessNode* process_node);
 };
 
 }  // namespace performance_manager
