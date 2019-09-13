aa337eef4ff484743dcb6c669fd3524c31bcae2e
Kyle Horimoto <khorimoto@chromium.org>
2019-09-13 00:33:01 +0000

    [CrOS Tether] Fix connection settings UI.
    This CL fixes two regressions:
    (1) The confirmation dialog was not properly displayed; this was due to
        using this.$.tetherDialog when the dialog is not always available
        when the Polymer element loaded.
    (2) The "Connect" button was not visible; this was due to using the
        managed properties, which did not explicitly set the connectable
        property.
    
    Bug: 1003568
    Change-Id: I426c5f935b45dce49c82392ea5e841d877c4f08f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1802514
    Auto-Submit: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696248}
    
    Former-commit-id: b8c81fd78a44f7b5bbc68d9fd16a7c8e107a7a9f

--- chrome/browser/resources/settings/internet_page/internet_detail_page.js
+++ chrome/browser/resources/settings/internet_page/internet_detail_page.js
@@ -990,7 +990,8 @@ Polymer({
    * @private
    */
   getTetherDialog_: function() {
-    return /** @type {!TetherConnectionDialogElement} */ (this.$.tetherDialog);
+    return /** @type {!TetherConnectionDialogElement} */ (
+        this.$$('#tetherDialog'));
   },
 
   /** @private */
