1266a13f173d12a470c7c8a094659ff3f1e0f6e5
Ioana Pandele <ioanap@chromium.org>
2019-09-16 11:56:21 +0000

    Revert "Use computed font-size for em/rem resolution."
    This reverts commit bb20d42a71f35b596869f7d48d77b3e870e84ece [formerly 41edd5e12e83f6ac7ce7cfd2fc979b6b35d672ee].
    
    Reason for revert:
    external/wpt/orientation-sensor/AbsoluteOrientationSensor.https.html 
    external/wpt/orientation-sensor/RelativeOrientationSensor.https.html
    are consistently failing on Mac.
    
    Bug:1004253
    
    Original change's description:
    > Use computed font-size for em/rem resolution.
    > 
    > This makes us match how Firefox applies minimum font-size and makes us
    > not break relative length computation when pages use a font-size less
    > than the user's minimum font-size as a basis for widths, heights,
    > margins, padding, etc.
    > 
    > This is a behavioral change for relative lengths for the following
    > cases:
    > 
    > - minimum-font-size does not affect em lengths
    > - minimum-logical-font-size does not affect em lengths
    > - text autosizing does not affect em lengths
    > - text scaling accessibility setting in Chrome for Android does not
    >   affect em lengths
    > 
    > Test expectation changes and reasons:
    > 
    > Text scaling no longer applies to relative lengths (text zoom set from testRunner):
    > 
    > svg/zoom/text/zoom-hixie-mixed-008.xml
    > svg/zoom/text/zoom-hixie-rendering-model-004.xhtml
    > svg/zoom/text/zoom-svg-float-border-padding.xml
    > 
    > Minimum logical font-size not applying to relative lengths (minimum logical font-size set to
    > 9px when running layout tests):
    > 
    > fast/backgrounds/background-position-parsing.html
    > virtual/controls-refresh/fast/forms/controls-new-ui/password/password-eye-icon-zoom-0.5x.html
    > http/tests/misc/object-embedding-svg-delayed-size-negotiation-2.htm
    > 
    > Bug: 308862
    > Change-Id: I5e5fb99e69b516c9c0c3758d232cbae8f7f5894a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1803281
    > Reviewed-by: Emil A Eklund <eae@chromium.org>
    > Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#696694}
    
    TBR=pdr@chromium.org,eae@chromium.org,drott@chromium.org,futhark@chromium.org
    
    Change-Id: Iaca7d3c69056f1b8f77ea4f212bf4abd08c613d8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 308862
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1804478
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696759}
    
    Former-commit-id: 73ecbd2f6a18eb5523ad2f7b9bec474174083368

--- third_party/blink/renderer/core/css/css_to_length_conversion_data.cc
+++ third_party/blink/renderer/core/css/css_to_length_conversion_data.cc
@@ -47,8 +47,8 @@ CSSToLengthConversionData::FontSizes::FontSizes(float em,
 
 CSSToLengthConversionData::FontSizes::FontSizes(const ComputedStyle* style,
                                                 const ComputedStyle* root_style)
-    : FontSizes(style->SpecifiedFontSize(),
-                root_style ? root_style->SpecifiedFontSize() : 1.0f,
+    : FontSizes(style->ComputedFontSize(),
+                root_style ? root_style->ComputedFontSize() : 1.0f,
                 &style->GetFont(),
                 style->EffectiveZoom()) {}
 
@@ -175,13 +175,13 @@ double CSSToLengthConversionData::ZoomedComputedPixels(
     // as well as enforcing the implicit "smart minimum."
     case CSSPrimitiveValue::UnitType::kEms:
     case CSSPrimitiveValue::UnitType::kQuirkyEms:
-      return value * EmFontSize() * Zoom();
+      return value * EmFontSize();
 
     case CSSPrimitiveValue::UnitType::kExs:
       return value * ExFontSize();
 
     case CSSPrimitiveValue::UnitType::kRems:
-      return value * RemFontSize() * Zoom();
+      return value * RemFontSize();
 
     case CSSPrimitiveValue::UnitType::kChs:
       return value * ChFontSize();
