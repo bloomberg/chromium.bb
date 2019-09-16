fdb887d4fe9f12d77b04c02ad30a9ffa33928a77
Adrienne Walker <enne@chromium.org>
2019-09-16 22:44:06 +0000

    Add Storage.Blob.BlobUrlLoader.FailureType UMA
    This parallels Storage.Blob.FileReaderLoader.FailureType, but for blobs
    loaded by url instead.
    
    (cherry picked from commit 78cff80b1ccdccdefddd8865e34dfff609472a37 [formerly 02e8cd5e20634dd9015b2aee454f3fcb6cecc2b0])
    
    Bug: 1001677
    Change-Id: I9fd5a75d71fe03b80acacd55fd82b6569df9c985
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1788457
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: enne <enne@chromium.org>
    Auto-Submit: enne <enne@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#695358}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1808163
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3904@{#245}
    Cr-Branched-From: 9035e7d56a589711638d7ffdc83ffa6d674a06ec [formerly 675968a8c657a3bd9c1c2c20c5d2935577bbc5e6]-refs/heads/master@{#693954}
    
    Former-commit-id: 4ae246560f21da33fa3aec1c64daf0bd1c76cff7

--- storage/browser/blob/blob_url_loader.cc
+++ storage/browser/blob/blob_url_loader.cc
@@ -11,6 +11,7 @@
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/weak_ptr.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/threading/thread_task_runner_handle.h"
@@ -192,6 +193,9 @@ void BlobURLLoader::DidReadSideData(net::IOBufferWithSize* data) {
 
 void BlobURLLoader::OnComplete(net::Error error_code,
                                uint64_t total_written_bytes) {
+  base::UmaHistogramSparse("Storage.Blob.BlobUrlLoader.FailureType",
+                           error_code);
+
   network::URLLoaderCompletionStatus status(error_code);
   status.encoded_body_length = total_written_bytes;
   status.decoded_body_length = total_written_bytes;
