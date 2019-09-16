6d973c884c046024d990a21c42b6353c2f873092
François Beaufort <beaufort.francois@gmail.com>
2019-09-16 19:55:32 +0200

    Add test for maximum number of BGL in pipeline layout (#34)

--- src/suites/cts/validation/createPipelineLayout.spec.ts
+++ src/suites/cts/validation/createPipelineLayout.spec.ts
@@ -2,7 +2,7 @@ export const description = `
 createPipelineLayout validation tests.
 `;
 
-import { TestGroup } from '../../../framework/index.js';
+import { TestGroup, poptions } from '../../../framework/index.js';
 
 import { ValidationTest } from './validation_test.js';
 
@@ -69,4 +69,40 @@ g.test('number of dynamic buffers exceeds the maximum value', async t => {
   { type: 'uniform-buffer', maxDynamicBufferCount: 8 },
 ]);
 
-// TODO: test max BGLs (4 works, 5 doesn't)
+g.test('number of bind group layouts exceeds the maximum value', async t => {
+  const { type } = t.params;
+
+  const bindGroupLayoutDescriptor: GPUBindGroupLayoutDescriptor = {
+    bindings: [
+      {
+        binding: 0,
+        visibility: GPUShaderStage.COMPUTE,
+        type,
+      },
+    ],
+  };
+
+  // 4 is the maximum number of bind group layouts.
+  const maxBindGroupLayouts = [1, 2, 3, 4].map(() =>
+    t.device.createBindGroupLayout(bindGroupLayoutDescriptor)
+  );
+
+  const goodPipelineLayoutDescriptor = {
+    bindGroupLayouts: maxBindGroupLayouts,
+  };
+
+  // Control case
+  t.device.createPipelineLayout(goodPipelineLayoutDescriptor);
+
+  // Check bind group layouts exceed maximum in pipeline layout.
+  const badPipelineLayoutDescriptor = {
+    bindGroupLayouts: [
+      ...maxBindGroupLayouts,
+      t.device.createBindGroupLayout(bindGroupLayoutDescriptor),
+    ],
+  };
+
+  await t.expectValidationError(() => {
+    t.device.createPipelineLayout(badPipelineLayoutDescriptor);
+  });
+}).params(poptions('type', ['storage-buffer', 'uniform-buffer']));
