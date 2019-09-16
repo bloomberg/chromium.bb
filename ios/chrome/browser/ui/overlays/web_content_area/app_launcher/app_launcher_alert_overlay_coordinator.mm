f8d5dc593112eba1964a97b304e4955b35c6c3d5
Kurt Horimoto <kkhorimoto@google.com>
2019-09-16 22:19:07 +0000

    [iOS] Use non-modal contained presenter for app launcher dialogs
    App launcher dialogs are now presented as child view controllers.  This
    allows for usage of NonModalViewControllerPresenter, which has the
    durations provided by UX.
    
    Also, since the view controllers can be presented immediately, this
    CL also prevents the flicker from occurring when exiting the tab
    switcher by avoiding the asynchronicity of view controller
    presentation.
    
    Bug: 979030, 999960, 999962
    Change-Id: I8b2c967e9cbe393f107bcfb68124ed9f214a7125
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1788246
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696965}
    
    Former-commit-id: b546938bf925a810584762927b80533317725c59

--- ios/chrome/browser/ui/overlays/web_content_area/app_launcher/app_launcher_alert_overlay_coordinator.mm
+++ ios/chrome/browser/ui/overlays/web_content_area/app_launcher/app_launcher_alert_overlay_coordinator.mm
@@ -9,17 +9,21 @@
 #import "ios/chrome/browser/ui/alert_view_controller/alert_view_controller.h"
 #import "ios/chrome/browser/ui/overlays/overlay_request_coordinator_delegate.h"
 #import "ios/chrome/browser/ui/overlays/web_content_area/app_launcher/app_launcher_alert_overlay_mediator.h"
+#import "ios/chrome/browser/ui/presenters/contained_presenter_delegate.h"
+#import "ios/chrome/browser/ui/presenters/non_modal_view_controller_presenter.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
 @interface AppLauncherAlertOverlayCoordinator () <
-    AppLauncherAlertOverlayMediatorDelegate>
+    AppLauncherAlertOverlayMediatorDelegate,
+    ContainedPresenterDelegate>
 // Whether the coordinator has been started.
 @property(nonatomic, getter=isStarted) BOOL started;
 
 @property(nonatomic) AlertViewController* alertViewController;
+@property(nonatomic) NonModalViewControllerPresenter* presenter;
 @property(nonatomic) AppLauncherAlertOverlayMediator* mediator;
 @end
 
@@ -42,10 +46,26 @@
   [self stopAnimated:YES];
 }
 
+#pragma mark - ContainedPresenterDelegate
+
+- (void)containedPresenterDidPresent:(id<ContainedPresenter>)presenter {
+  self.delegate->OverlayUIDidFinishPresentation(self.request);
+}
+
+- (void)containedPresenterDidDismiss:(id<ContainedPresenter>)presenter {
+  self.alertViewController = nil;
+  self.presenter = nil;
+  self.delegate->OverlayUIDidFinishDismissal(self.request);
+}
+
 #pragma mark - OverlayCoordinator
 
 + (BOOL)supportsRequest:(OverlayRequest*)request {
   return !!request->GetConfig<AppLauncherAlertOverlayRequestConfig>();
+}
+
++ (BOOL)usesChildViewController {
+  return YES;
 }
 
 - (UIViewController*)viewController {
@@ -63,31 +83,19 @@
   self.mediator =
       [[AppLauncherAlertOverlayMediator alloc] initWithRequest:self.request];
   self.mediator.consumer = self.alertViewController;
-  __weak __typeof__(self) weakSelf = self;
-  [self.baseViewController
-      presentViewController:self.alertViewController
-                   animated:animated
-                 completion:^{
-                   weakSelf.delegate->OverlayUIDidFinishPresentation(
-                       weakSelf.request);
-                 }];
+  self.presenter = [[NonModalViewControllerPresenter alloc] init];
+  self.presenter.delegate = self;
+  self.presenter.baseViewController = self.baseViewController;
+  self.presenter.presentedViewController = self.alertViewController;
+  [self.presenter prepareForPresentation];
+  [self.presenter presentAnimated:animated];
   self.started = YES;
 }
 
 - (void)stopAnimated:(BOOL)animated {
   if (!self.started)
     return;
-  __weak __typeof__(self) weakSelf = self;
-  [self.baseViewController
-      dismissViewControllerAnimated:animated
-                         completion:^{
-                           __typeof__(self) strongSelf = weakSelf;
-                           if (!strongSelf)
-                             return;
-                           strongSelf.alertViewController = nil;
-                           strongSelf.delegate->OverlayUIDidFinishDismissal(
-                               weakSelf.request);
-                         }];
+  [self.presenter dismissAnimated:animated];
   self.started = NO;
 }
 
