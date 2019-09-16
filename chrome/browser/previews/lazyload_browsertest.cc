b37cd3e49bad39f8206284d54b6cea446e26ba60
rajendrant <rajendrant@chromium.org>
2019-09-16 20:55:13 +0000

    Improve lazyload browser test
    Adds a combination of in viewport and below viewport images and frames
    with auto and lazy values for loading attribute.
    
    (cherry picked from commit 86f0f3e17dbdaa08361153db641af418d6e2fa6b [formerly 10499c2591581f51cca262b7c29b5c31693145cc])
    
    Bug: 1000814
    Change-Id: Ie7239bb66b1d23ab33615d90cd0391c61e853aa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1788252
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Scott Little <sclittle@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#696575}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1807833
    Cr-Commit-Position: refs/branch-heads/3904@{#227}
    Cr-Branched-From: 9035e7d56a589711638d7ffdc83ffa6d674a06ec [formerly 675968a8c657a3bd9c1c2c20c5d2935577bbc5e6]-refs/heads/master@{#693954}
    
    Former-commit-id: 3e07d86eed573df5f7c382a2a6cf21ce3c126c8a

--- chrome/browser/previews/lazyload_browsertest.cc
+++ chrome/browser/previews/lazyload_browsertest.cc
@@ -6,25 +6,109 @@
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
+#include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/data_use_measurement/core/data_use_user_data.h"
+#include "content/public/browser/content_browser_client.h"
+#include "content/public/browser/web_contents.h"
 #include "content/public/common/content_features.h"
+#include "content/public/common/referrer.h"
 #include "content/public/test/browser_test_base.h"
 #include "content/public/test/browser_test_utils.h"
+#include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
+#include "net/test/embedded_test_server/http_request.h"
+#include "net/test/embedded_test_server/http_response.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 class LazyLoadBrowserTest : public InProcessBrowserTest {
  protected:
+  void SetUpOnMainThread() override {
+    InProcessBrowserTest::SetUpOnMainThread();
+    host_resolver()->AddRule("*", "127.0.0.1");
+  }
   void SetUp() override {
-    scoped_feature_list_.InitAndEnableFeatureWithParameters(
-        features::kLazyImageLoading,
-        {{"restrict-lazy-load-images-to-data-saver-only", "false"},
-         {"automatic-lazy-load-images-enabled", "true"}});
+    scoped_feature_list_.InitWithFeaturesAndParameters(
+        {{features::kLazyImageLoading,
+          {{"automatic-lazy-load-images-enabled", "true"},
+           {"restrict-lazy-load-images-to-data-saver-only", "false"}}},
+         {features::kLazyFrameLoading,
+          {{"automatic-lazy-load-frames-enabled", "true"},
+           {"restrict-lazy-load-frames-to-data-saver-only", "false"}}}},
+        {});
     InProcessBrowserTest::SetUp();
   }
+
+  void ScrollToAndWaitForScroll(unsigned int scroll_offset) {
+    ASSERT_TRUE(content::ExecuteScript(
+        browser()->tab_strip_model()->GetActiveWebContents(),
+        base::StringPrintf("window.scrollTo(0, %d);", scroll_offset)));
+    content::RenderFrameSubmissionObserver observer(
+        browser()->tab_strip_model()->GetActiveWebContents());
+    observer.WaitForScrollOffset(gfx::Vector2dF(0, scroll_offset));
+  }
+
+  // Sets up test pages with in-viewport and below-viewport cross-origin frames.
+  void SetUpLazyLoadFrameTestPage() {
+    cross_origin_server_.ServeFilesFromSourceDirectory(GetChromeTestDataDir());
+    ASSERT_TRUE(cross_origin_server_.Start());
+
+    embedded_test_server()->RegisterRequestHandler(base::Bind(
+        [](uint16_t cross_origin_port,
+           const net::test_server::HttpRequest& request)
+            -> std::unique_ptr<net::test_server::HttpResponse> {
+          auto response =
+              std::make_unique<net::test_server::BasicHttpResponse>();
+          if (request.relative_url == "/mainpage.html") {
+            response->set_content(base::StringPrintf(
+                R"HTML(
+          <body onload="on_load('document')">
+            <script>
+            function on_load(msg) {
+              console.log("LAZY_LOAD CONSOLE", msg, "ON_LOAD FOR TEST");
+            }
+            </script>
+
+            <iframe src="http://bar.com:%d/simple.html?auto"
+              width="100" height="100"
+              onload="on_load('in-viewport iframe')"></iframe>
+            <iframe src="http://bar.com:%d/simple.html?lazy"
+              width="100" height="100" loading="lazy"
+              onload="on_load('in-viewport loading=lazy iframe')">
+            </iframe>
+            <iframe src="http://bar.com:%d/simple.html?eager"
+              width="100" height="100" loading="eager"
+              onload="on_load('in-viewport loading=eager iframe')">
+            </iframe>
+
+            <div style="height:11000px;"></div>
+            Below the viewport cross-origin iframe <br>
+            <iframe src="http://bar.com:%d/simple.html?auto&belowviewport"
+              width="100" height="100"
+              onload="on_load('below-viewport iframe')"></iframe>
+            <iframe src="http://bar.com:%d/simple.html?lazy&belowviewport"
+              width="100" height="100" loading="lazy"
+              onload="on_load('below-viewport loading=lazy iframe')">
+            </iframe>
+            <iframe src="http://bar.com:%d/simple.html?eager&belowviewport"
+              width="100" height="100" loading="eager"
+              onload="on_load('below-viewport loading=eager iframe')">
+            </iframe>
+          </body>)HTML",
+                cross_origin_port, cross_origin_port, cross_origin_port,
+                cross_origin_port, cross_origin_port, cross_origin_port));
+          }
+          return response;
+        },
+        cross_origin_server_.port()));
+    ASSERT_TRUE(embedded_test_server()->Start());
+  }
+
+ private:
   base::test::ScopedFeatureList scoped_feature_list_;
+  net::EmbeddedTestServer cross_origin_server_;
 };
 
 IN_PROC_BROWSER_TEST_F(LazyLoadBrowserTest, CSSBackgroundImageDeferred) {
@@ -44,9 +128,6 @@ IN_PROC_BROWSER_TEST_F(LazyLoadBrowserTest, CSSBackgroundImageDeferred) {
                    data_use_measurement::DataUseUserData::IMAGE));
 }
 
-// TODO(rajendrant): Add a test that checks if the deferred image is loaded when
-// user scrolls near it.
-
 IN_PROC_BROWSER_TEST_F(LazyLoadBrowserTest, CSSPseudoBackgroundImageLoaded) {
   ASSERT_TRUE(embedded_test_server()->Start());
   base::HistogramTester histogram_tester;
@@ -62,4 +143,87 @@ IN_PROC_BROWSER_TEST_F(LazyLoadBrowserTest, CSSPseudoBackgroundImageLoaded) {
   EXPECT_GE(30 /* KB */, histogram_tester.GetBucketCount(
                              "DataUse.ContentType.UserTrafficKB",
                              data_use_measurement::DataUseUserData::IMAGE));
+}
+
+IN_PROC_BROWSER_TEST_F(LazyLoadBrowserTest,
+                       LazyLoadImage_DeferredAndLoadedOnScroll) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+  GURL test_url(embedded_test_server()->GetURL("/lazyload/img.html"));
+
+  auto* contents = browser()->tab_strip_model()->GetActiveWebContents();
+  content::ConsoleObserverDelegate console_observer(
+      contents, "LAZY_LOAD CONSOLE * ON_LOAD FOR TEST");
+  contents->SetDelegate(&console_observer);
+  ui_test_utils::NavigateToURL(browser(), test_url);
+
+  // Wait for the four images (3 in-viewport, 1 eager below-viewport) and the
+  // document to load.
+  while (console_observer.messages().size() < 5) {
+    base::RunLoop().RunUntilIdle();
+  }
+  console_observer.Wait();
+
+  EXPECT_THAT(
+      console_observer.messages(),
+      testing::UnorderedElementsAre(
+          "LAZY_LOAD CONSOLE document ON_LOAD FOR TEST",
+          "LAZY_LOAD CONSOLE in-viewport img ON_LOAD FOR TEST",
+          "LAZY_LOAD CONSOLE in-viewport loading=lazy img ON_LOAD FOR TEST",
+          "LAZY_LOAD CONSOLE in-viewport loading=eager img ON_LOAD FOR TEST",
+          "LAZY_LOAD CONSOLE below-viewport loading=eager img ON_LOAD FOR "
+          "TEST"));
+
+  // Scroll down and verify the below-viewport images load.
+  ScrollToAndWaitForScroll(10000);
+  while (console_observer.messages().size() < 7) {
+    base::RunLoop().RunUntilIdle();
+  }
+  const auto messages = console_observer.messages();
+  EXPECT_THAT(messages,
+              testing::Contains(
+                  "LAZY_LOAD CONSOLE below-viewport img ON_LOAD FOR TEST"));
+  EXPECT_THAT(messages, testing::Contains("LAZY_LOAD CONSOLE below-viewport "
+                                          "loading=lazy img ON_LOAD FOR TEST"));
+}
+
+IN_PROC_BROWSER_TEST_F(LazyLoadBrowserTest,
+                       LazyLoadFrame_DeferredAndLoadedOnScroll) {
+  SetUpLazyLoadFrameTestPage();
+  GURL test_url(embedded_test_server()->GetURL("/mainpage.html"));
+
+  auto* contents = browser()->tab_strip_model()->GetActiveWebContents();
+  content::ConsoleObserverDelegate console_observer(
+      contents, "LAZY_LOAD CONSOLE * ON_LOAD FOR TEST");
+  contents->SetDelegate(&console_observer);
+  ui_test_utils::NavigateToURL(browser(), test_url);
+
+  // Wait for the four iframes (3 in-viewport, 1 eager below-viewport) and the
+  // document to load.
+  while (console_observer.messages().size() < 5) {
+    base::RunLoop().RunUntilIdle();
+  }
+  console_observer.Wait();
+
+  EXPECT_THAT(
+      console_observer.messages(),
+      testing::UnorderedElementsAre(
+          "LAZY_LOAD CONSOLE document ON_LOAD FOR TEST",
+          "LAZY_LOAD CONSOLE in-viewport iframe ON_LOAD FOR TEST",
+          "LAZY_LOAD CONSOLE in-viewport loading=lazy iframe ON_LOAD FOR TEST",
+          "LAZY_LOAD CONSOLE in-viewport loading=eager iframe ON_LOAD FOR TEST",
+          "LAZY_LOAD CONSOLE below-viewport loading=eager iframe ON_LOAD FOR "
+          "TEST"));
+
+  // Scroll down and verify the below-viewport iframes load.
+  ScrollToAndWaitForScroll(10000);
+  while (console_observer.messages().size() < 7) {
+    base::RunLoop().RunUntilIdle();
+  }
+  const auto messages = console_observer.messages();
+  EXPECT_THAT(messages,
+              testing::Contains(
+                  "LAZY_LOAD CONSOLE below-viewport iframe ON_LOAD FOR TEST"));
+  EXPECT_THAT(messages,
+              testing::Contains("LAZY_LOAD CONSOLE below-viewport "
+                                "loading=lazy iframe ON_LOAD FOR TEST"));
 }
