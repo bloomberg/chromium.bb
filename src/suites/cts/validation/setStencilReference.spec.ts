6af4f5f576bec57dd0dbd6e9973ef54461d08f49
François Beaufort <beaufort.francois@gmail.com>
2019-09-17 22:43:10 +0200

    Add dynamic state validation tests (#30)

---
+++ src/suites/cts/validation/setStencilReference.spec.ts
@@ -0,0 +1,39 @@
+export const description = `
+setStencilReference validation tests.
+`;
+
+import { TestGroup, poptions } from '../../../framework/index.js';
+
+import { ValidationTest } from './validation_test.js';
+
+// TODO: Move this fixture class to a common file.
+class F extends ValidationTest {
+  beginRenderPass(commandEncoder: GPUCommandEncoder): GPURenderPassEncoder {
+    const attachmentTexture = this.device.createTexture({
+      format: 'rgba8unorm',
+      size: { width: 16, height: 16, depth: 1 },
+      usage: GPUTextureUsage.OUTPUT_ATTACHMENT,
+    });
+
+    return commandEncoder.beginRenderPass({
+      colorAttachments: [
+        {
+          attachment: attachmentTexture.createView(),
+          loadValue: { r: 1.0, g: 0.0, b: 0.0, a: 1.0 },
+        },
+      ],
+    });
+  }
+}
+
+export const g = new TestGroup(F);
+
+g.test('use of setStencilReference', t => {
+  const { reference } = t.params;
+
+  const commandEncoder = t.device.createCommandEncoder();
+  const renderPass = t.beginRenderPass(commandEncoder);
+  renderPass.setStencilReference(reference);
+  renderPass.endPass();
+  commandEncoder.finish();
+}).params(poptions('reference', [0, 0xffffffff]));
