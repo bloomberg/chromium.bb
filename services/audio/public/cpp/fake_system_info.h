2a8c089c92bd8a10b5a27c0ed9b028c86c8b93f6
Miyoung Shin <myid.shin@igalia.com>
2019-09-18 11:20:42 +0000

    Convert SystemInfo to new Mojo types
    This CL converts SystemInfo{Ptr, Request} in services to the
    new Mojo types.
    
    Bug: 955171
    Change-Id: Ie5d39d40751a95a20cafbddcc118d76c2090b01a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787636
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697558}
    
    Former-commit-id: 3e0000af8b8c5b8f36e3648185514ac88215db2a

--- services/audio/public/cpp/fake_system_info.h
+++ services/audio/public/cpp/fake_system_info.h
@@ -8,6 +8,7 @@
 #include <string>
 
 #include "base/macros.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
 #include "mojo/public/cpp/system/message_pipe.h"
 #include "services/audio/public/mojom/system_info.mojom.h"
