7e5763fa21d56d33b1a01510c2cff097410ab819
Tina Wang <tinazwang@chromium.org>
2019-09-18 19:48:20 +0000

    [SendTabToSelf] Format the attributes' name in enum class
    - Rename the DeviceIconType attributes.
    - Remove default case of CreateDeviceIcon because of using desktop icon
    by default.
    
    Bug: 1003812
    Change-Id: Ie1e1a9cc9ef397769c8b37d25b867dfd1018cf37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1802500
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697735}
    
    Former-commit-id: f89f952059b65d6c0033b59a2cde321b4b2c1abf

--- chrome/browser/ui/views/send_tab_to_self/send_tab_to_self_bubble_device_button.cc
+++ chrome/browser/ui/views/send_tab_to_self/send_tab_to_self_bubble_device_button.cc
@@ -23,9 +23,8 @@ namespace send_tab_to_self {
 namespace {
 
 enum class DeviceIconType {
-  DESKTOP = 0,
-  PHONE = 1,
-  TOTAL_COUNT = 2  // Add new types above this line.
+  kDesktop = 0,
+  kPhone = 1,
 };
 
 SkColor GetColorfromTheme() {
@@ -38,14 +37,12 @@ SkColor GetColorfromTheme() {
 gfx::ImageSkia CreateDeviceIcon(DeviceIconType icon_type) {
   const gfx::VectorIcon* vector_icon;
   switch (icon_type) {
-    case DeviceIconType::DESKTOP:
+    case DeviceIconType::kDesktop:
       vector_icon = &kHardwareComputerIcon;
       break;
-    case DeviceIconType::PHONE:
+    case DeviceIconType::kPhone:
       vector_icon = &kHardwareSmartphoneIcon;
       break;
-    default:
-      vector_icon = &kSendTabToSelfIcon;
   }
 
   constexpr int kPrimaryIconSize = 20;
@@ -55,12 +52,9 @@ gfx::ImageSkia CreateDeviceIcon(DeviceIconType icon_type) {
 
 std::unique_ptr<views::ImageView> CreateIconView(
     const sync_pb::SyncEnums::DeviceType device_type) {
-  gfx::ImageSkia image;
-  if (device_type == sync_pb::SyncEnums::TYPE_PHONE) {
-    image = CreateDeviceIcon(DeviceIconType::PHONE);
-  } else {
-    image = CreateDeviceIcon(DeviceIconType::DESKTOP);
-  }
+  gfx::ImageSkia image = CreateDeviceIcon(
+      device_type == sync_pb::SyncEnums::TYPE_PHONE ? DeviceIconType::kPhone
+                                                    : DeviceIconType::kDesktop);
   auto icon_view = std::make_unique<views::ImageView>();
   icon_view->SetImage(image);
   constexpr auto kPrimaryIconBorder = gfx::Insets(6);
