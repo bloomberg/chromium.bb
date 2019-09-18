4a86b3bd33b1b2b34057880aae73bb6f5a855019
Henrique Ferreiro <hferreiro@igalia.com>
2019-09-18 10:25:02 +0000

    Migrate explore_sites_internals.mojom to the new Mojo types
    Convert the implementation and all users of the
    explore_sites_internals::mojom::PageHandler interface.
    
    Bug: 955171
    Change-Id: I10c6afa8c22eb3ccac5bba728cb90b4b023ebe7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1807319
    Reviewed-by: Cathy Li <chili@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#697547}
    
    Former-commit-id: b249e614c5971bcc05f8fed1d9209d664e6a9c21

--- chrome/browser/ui/webui/explore_sites_internals/explore_sites_internals_ui.cc
+++ chrome/browser/ui/webui/explore_sites_internals/explore_sites_internals_ui.cc
@@ -9,10 +9,12 @@
 #include "chrome/browser/android/chrome_feature_list.h"
 #include "chrome/browser/android/explore_sites/explore_sites_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/webui/explore_sites_internals/explore_sites_internals.mojom.h"
 #include "chrome/browser/ui/webui/explore_sites_internals/explore_sites_internals_page_handler.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/browser_resources.h"
 #include "content/public/browser/web_ui_data_source.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
 
 namespace explore_sites {
 
@@ -44,9 +46,11 @@ ExploreSitesInternalsUI::ExploreSitesInternalsUI(content::WebUI* web_ui)
 ExploreSitesInternalsUI::~ExploreSitesInternalsUI() {}
 
 void ExploreSitesInternalsUI::BindExploreSitesInternalsPageHandler(
-    explore_sites_internals::mojom::PageHandlerRequest request) {
+    mojo::PendingReceiver<explore_sites_internals::mojom::PageHandler>
+        receiver) {
   page_handler_ = std::make_unique<ExploreSitesInternalsPageHandler>(
-      std::move(request), explore_sites_service_, Profile::FromWebUI(web_ui()));
+      std::move(receiver), explore_sites_service_,
+      Profile::FromWebUI(web_ui()));
 }
 
 }  // namespace explore_sites
