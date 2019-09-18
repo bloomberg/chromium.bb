2332f437e59ea89e559d4d87113760a1e80a5a64
Naoki Fukino <fukino@chromium.org>
2019-09-18 00:58:32 +0000

    Fix Android path to request media scan for removable media.
    In Android, a removable media labelled as "UNTITLED" is mounted
    at /storage/removable_UNTITLED instead of /storage/UNTITLED.
    This behavior change was made by ag/6513597, but Chrome side
    didn't follow the changed paths.
    
    *Example*
    CrOS path: /removable/media/UNTITLED/foo.jpg
    Wrong path to request media scan (today's behavior): /storage/UNTITLED/foo.jpg
    Correct path to request media scan: /storage/removable_UNTITLED/foo.jpg
    
    This CL alters the path in Chrome side, since Android side
    can't tell if the requested path is on a removable device or not.
    
    (cherry picked from commit e9a2c7a42c9cf0651a674b9e68b38b03086cc9e0 [formerly 92e5f97fe57fc06e48d3a09e74768f44f90c33d5])
    
    Bug: 1000163
    Test: Manually tested on Kevin.
    Change-Id: I318735135900ed47f144bff13a51965b0bcb0ee0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787620
    Commit-Queue: Naoki Fukino <fukino@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#694192}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1810137
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3904@{#291}
    Cr-Branched-From: 9035e7d56a589711638d7ffdc83ffa6d674a06ec [formerly 675968a8c657a3bd9c1c2c20c5d2935577bbc5e6]-refs/heads/master@{#693954}
    
    Former-commit-id: 4c9283d663d55983b08da08c83055ef1e0c71fea

--- chrome/browser/chromeos/arc/file_system_watcher/arc_file_system_watcher_service.cc
+++ chrome/browser/chromeos/arc/file_system_watcher/arc_file_system_watcher_service.cc
@@ -66,6 +66,11 @@ constexpr base::FilePath::CharType kCrosRemovableMediaDir[] =
 constexpr base::FilePath::CharType kAndroidRemovableMediaDir[] =
     FILE_PATH_LITERAL("/storage");
 
+// The prefix for device label used in Android paths for removable media.
+// A removable device mounted at /media/removable/UNTITLED is mounted at
+// /storage/removable_UNTITLED in Android.
+constexpr char kRemovableMediaLabelPrefix[] = "removable_";
+
 // How long to wait for new inotify events before building the updated timestamp
 // map.
 const base::TimeDelta kBuildTimestampMapDelay =
@@ -126,7 +131,11 @@ TimestampMap BuildTimestampMap(base::FilePath cros_dir,
     // Android file path can be obtained by replacing |cros_dir|
     // prefix with |android_dir|.
     base::FilePath android_path(android_dir);
-    cros_dir.AppendRelativePath(cros_path, &android_path);
+    if (cros_dir.value() == kCrosRemovableMediaDir) {
+      AppendRelativePathForRemovableMedia(cros_path, &android_path);
+    } else {
+      cros_dir.AppendRelativePath(cros_path, &android_path);
+    }
     const base::FileEnumerator::FileInfo& info = enumerator.GetInfo();
     timestamp_map[android_path] = info.GetLastModifiedTime();
   }
@@ -249,6 +258,35 @@ bool HasAndroidSupportedMediaExtension(const base::FilePath& path) {
       kAndroidSupportedMediaExtensions,
       kAndroidSupportedMediaExtensions + kAndroidSupportedMediaExtensionsSize,
       extension.c_str(), less_comparator);
+}
+
+bool AppendRelativePathForRemovableMedia(const base::FilePath& cros_path,
+                                         base::FilePath* android_path) {
+  std::vector<base::FilePath::StringType> parent_components;
+  base::FilePath(kCrosRemovableMediaDir).GetComponents(&parent_components);
+  std::vector<base::FilePath::StringType> child_components;
+  cros_path.GetComponents(&child_components);
+  auto child_itr = child_components.begin();
+  for (const auto& parent_component : parent_components) {
+    if (child_itr == child_components.end() || parent_component != *child_itr) {
+      LOG(WARNING) << "|cros_path| is not under kCrosRemovableMediaDir.";
+      return false;
+    }
+    ++child_itr;
+  }
+  if (child_itr == child_components.end()) {
+    LOG(WARNING) << "The CrOS path doesn't have a component for device label.";
+    return false;
+  }
+  // The device label (e.g. "UNTITLED" for /media/removable/UNTITLED/foo.jpg)
+  // should be converted to removable_UNTITLED, since the prefix "removable_"
+  // is appended to paths for removable media in Android.
+  *android_path = android_path->Append(kRemovableMediaLabelPrefix + *child_itr);
+  ++child_itr;
+  for (; child_itr != child_components.end(); ++child_itr) {
+    *android_path = android_path->Append(*child_itr);
+  }
+  return true;
 }
 
 // The core part of ArcFileSystemWatcherService to watch for file changes in
