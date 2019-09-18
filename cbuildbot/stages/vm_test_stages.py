3e31228bdf51aaa6a6d44ab06098d9dd80f0ac03
Keith Haddow <haddowk@chromium.org>
2019-09-18 15:55:13 -0700

    vm_test_stages:  Increase the moblab_vm timeout
    moblab_vm is not marked as imporant in the CQ so this should
    not be able to break anything.
    
    BUG=chromium:1005534
    TEST=None
    
    Change-Id: Ifabba1676a094d462602f3ea445ccd3eacd004de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/1811788
    Tested-by: Keith Haddow <haddowk@chromium.org>
    Auto-Submit: Keith Haddow <haddowk@chromium.org>
    Reviewed-by: Jason Clinton <jclinton@chromium.org>
    Commit-Queue: Jason Clinton <jclinton@chromium.org>

--- cbuildbot/stages/vm_test_stages.py
+++ cbuildbot/stages/vm_test_stages.py
@@ -432,7 +432,7 @@ class MoblabVMTestStage(generic_stages.BoardSpecificBuilderStage,
 
   # This includes the time we expect to take to prepare and run the tests. It
   # excludes the time required to archive the results at the end.
-  _PERFORM_TIMEOUT_S = 90 * 60
+  _PERFORM_TIMEOUT_S = 110 * 60
 
   def __str__(self):
     return type(self).__name__
