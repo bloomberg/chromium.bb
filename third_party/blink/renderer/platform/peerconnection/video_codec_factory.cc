305f7ef54af9dd4705cf1ac5e680ed55c2bca7b4
Antonio Gomes <tonikitoo@igalia.com>
2019-09-18 19:16:42 +0000

    Move video_codec_factory.h out of the Blink API
    BUG=919392, 787254
    R=guidou@chromium.org, haraken@chromium.org
    
    Change-Id: Ie40d53355cbcfeecd65e30f0bd25334e86ebf5cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1807716
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697715}
    
    Former-commit-id: 2fa285a7c64c1f1405f82023c95a13126be80d24

--- third_party/blink/renderer/platform/peerconnection/video_codec_factory.cc
+++ third_party/blink/renderer/platform/peerconnection/video_codec_factory.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "third_party/blink/public/platform/modules/peerconnection/video_codec_factory.h"
+#include "third_party/blink/renderer/platform/peerconnection/video_codec_factory.h"
 
 #include "base/memory/ptr_util.h"
 #include "build/build_config.h"
--- third_party/blink/renderer/platform/peerconnection/video_codec_factory.h
+++ third_party/blink/renderer/platform/peerconnection/video_codec_factory.h
@@ -2,10 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef THIRD_PARTY_BLINK_PUBLIC_PLATFORM_MODULES_PEERCONNECTION_VIDEO_CODEC_FACTORY_H_
-#define THIRD_PARTY_BLINK_PUBLIC_PLATFORM_MODULES_PEERCONNECTION_VIDEO_CODEC_FACTORY_H_
+#ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_PEERCONNECTION_VIDEO_CODEC_FACTORY_H_
+#define THIRD_PARTY_BLINK_RENDERER_PLATFORM_PEERCONNECTION_VIDEO_CODEC_FACTORY_H_
 
-#include "third_party/blink/public/platform/web_common.h"
+#include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/webrtc/api/video_codecs/video_decoder_factory.h"
 #include "third_party/webrtc/api/video_codecs/video_encoder_factory.h"
 
@@ -15,15 +15,13 @@ class GpuVideoAcceleratorFactories;
 
 namespace blink {
 
-// TODO(crbug.com/787254): Remove these APIs when their clients are Onion
-// souped.
-BLINK_PLATFORM_EXPORT std::unique_ptr<webrtc::VideoEncoderFactory>
+PLATFORM_EXPORT std::unique_ptr<webrtc::VideoEncoderFactory>
 CreateWebrtcVideoEncoderFactory(
     media::GpuVideoAcceleratorFactories* gpu_factories);
-BLINK_PLATFORM_EXPORT std::unique_ptr<webrtc::VideoDecoderFactory>
+PLATFORM_EXPORT std::unique_ptr<webrtc::VideoDecoderFactory>
 CreateWebrtcVideoDecoderFactory(
     media::GpuVideoAcceleratorFactories* gpu_factories);
 
 }  // namespace blink
 
-#endif  // THIRD_PARTY_BLINK_PUBLIC_PLATFORM_MODULES_PEERCONNECTION_VIDEO_CODEC_FACTORY_H_
+#endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_PEERCONNECTION_VIDEO_CODEC_FACTORY_H_
