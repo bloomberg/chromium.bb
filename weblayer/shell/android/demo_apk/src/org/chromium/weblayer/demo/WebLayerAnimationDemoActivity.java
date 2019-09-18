c4dd86230d1d397d029ee4d7ba7b6cc5bc08c907
Bo Liu <boliu@chromium.org>
2019-09-18 00:45:12 +0000

    weblayer: webview animation demo
    Implement the animation demo using webview behind a compile time
    variable. There is no url bar. Useful for comparison purpose. Duplicates
    a bunch of code, which is ok assuming demo will not be long lived.
    
    Change-Id: I2280255e4d5f546afe1f7c5d054474d228cafe0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1809623
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697429}
    
    Former-commit-id: 2b1c19711de78f56e3c7d4e897597b7d6006f33c

--- weblayer/shell/android/demo_apk/src/org/chromium/weblayer/demo/WebLayerAnimationDemoActivity.java
+++ weblayer/shell/android/demo_apk/src/org/chromium/weblayer/demo/WebLayerAnimationDemoActivity.java
@@ -17,6 +17,9 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.TextView;
@@ -34,9 +37,12 @@ import org.chromium.weblayer.WebLayer;
 public class WebLayerAnimationDemoActivity extends FragmentActivity {
     private static final String TAG = "AnimationDemo";
 
+    private static final boolean USE_WEBVIEW = false;
+
     private Profile mProfile;
     private final BrowserFragmentImpl mBrowserFragments[] = new BrowserFragmentImpl[4];
     private final ContainerFrameLayout mContainerViews[] = new ContainerFrameLayout[4];
+    private final MyWebView mWebViews[] = new MyWebView[4];
     private FrameLayout mMainView;
 
     public static class ShellFragment extends Fragment {
@@ -58,6 +64,38 @@ public class WebLayerAnimationDemoActivity extends FragmentActivity {
         private boolean mInterceptTouchEvent;
 
         public ContainerFrameLayout(Context context, int index) {
+            super(context);
+            mIndex = index;
+        }
+
+        public void setInterceptTouchEvent(boolean intercept) {
+            mInterceptTouchEvent = intercept;
+        }
+
+        public int getIndex() {
+            return mIndex;
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(MotionEvent ev) {
+            return mInterceptTouchEvent;
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            if (!mInterceptTouchEvent) return super.onTouchEvent(event);
+            if (event.getAction() == KeyEvent.ACTION_UP) {
+                performClick();
+            }
+            return true;
+        }
+    }
+
+    private static class MyWebView extends WebView {
+        private int mIndex;
+        private boolean mInterceptTouchEvent;
+
+        public MyWebView(Context context, int index) {
             super(context);
             mIndex = index;
         }
@@ -131,27 +169,53 @@ public class WebLayerAnimationDemoActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
-        // Only call init for main process.
-        WebLayer.getInstance().init(getApplication());
+        if (USE_WEBVIEW) {
+            super.onCreate(savedInstanceState);
 
-        super.onCreate(savedInstanceState);
+            FrameLayout mainView = new FrameLayout(this);
+            mMainView = mainView;
+            setContentView(mainView);
 
-        FrameLayout mainView = new FrameLayout(this);
-        mMainView = mainView;
-        setContentView(mainView);
+            for (int i = 0; i < 3; ++i) {
+                mWebViews[i] = new MyWebView(this, i);
+                mWebViews[i].setWebViewClient(new WebViewClient() {
+                    @Override
+                    public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+                        return false;
+                    }
+                });
+                mMainView.addView(mWebViews[i]);
+                WebSettings settings = mWebViews[i].getSettings();
+                settings.setJavaScriptEnabled(true);
 
-        mProfile = WebLayer.getInstance().createProfile(null);
+                // Default layout behavior for chrome on android.
+                settings.setUseWideViewPort(true);
+                settings.setLoadWithOverviewMode(true);
+                settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.TEXT_AUTOSIZING);
+            }
+        } else {
+            // Only call init for main process.
+            WebLayer.getInstance().init(getApplication());
 
-        createNewFragment(0);
-        createNewFragment(1);
-        createNewFragment(2);
+            super.onCreate(savedInstanceState);
 
-        mBrowserFragments[0].getBrowserController().getNavigationController().navigate(
-                Uri.parse(sanitizeUrl("https://www.google.com")));
-        mBrowserFragments[1].getBrowserController().getNavigationController().navigate(
-                Uri.parse(sanitizeUrl("https://en.wikipedia.org")));
-        mBrowserFragments[2].getBrowserController().getNavigationController().navigate(
-                Uri.parse(sanitizeUrl("https://www.chromium.org")));
+            FrameLayout mainView = new FrameLayout(this);
+            mMainView = mainView;
+            setContentView(mainView);
+
+            mProfile = WebLayer.getInstance().createProfile(null);
+
+            createNewFragment(0);
+            createNewFragment(1);
+            createNewFragment(2);
+
+            mBrowserFragments[0].getBrowserController().getNavigationController().navigate(
+                    Uri.parse(sanitizeUrl("https://www.google.com")));
+            mBrowserFragments[1].getBrowserController().getNavigationController().navigate(
+                    Uri.parse(sanitizeUrl("https://en.wikipedia.org")));
+            mBrowserFragments[2].getBrowserController().getNavigationController().navigate(
+                    Uri.parse(sanitizeUrl("https://www.chromium.org")));
+        }
     }
 
     @Override
@@ -174,34 +238,61 @@ public class WebLayerAnimationDemoActivity extends FragmentActivity {
     protected void onResume() {
         super.onResume();
 
-        mContainerViews[0].setOnClickListener(new OnClickImpl());
-        mContainerViews[1].setOnClickListener(new OnClickImpl());
-        mContainerViews[2].setOnClickListener(new OnClickImpl());
-        mContainerViews[0].post(() -> {
-            mBrowserFragments[0].setSupportsEmbedding(true);
-            mBrowserFragments[1].setSupportsEmbedding(true);
-            mBrowserFragments[2].setSupportsEmbedding(true);
-            animateDown(mContainerViews[0]);
-            animateDown(mContainerViews[1]);
-            animateDown(mContainerViews[2]);
-        });
+        if (USE_WEBVIEW) {
+            mWebViews[0].post(() -> {
+                mWebViews[0].loadUrl("https://www.google.com");
+                mWebViews[1].loadUrl("https://en.wikipedia.org");
+                mWebViews[2].loadUrl("https://www.chromium.org");
+                for (int i = 0; i < 3; ++i) {
+                    mWebViews[i].setOnClickListener(new OnClickImpl());
+                    animateDown(mWebViews[i]);
+                }
+            });
+        } else {
+            mContainerViews[0].setOnClickListener(new OnClickImpl());
+            mContainerViews[1].setOnClickListener(new OnClickImpl());
+            mContainerViews[2].setOnClickListener(new OnClickImpl());
+            mContainerViews[0].post(() -> {
+                mBrowserFragments[0].setSupportsEmbedding(true);
+                mBrowserFragments[1].setSupportsEmbedding(true);
+                mBrowserFragments[2].setSupportsEmbedding(true);
+                animateDown(mContainerViews[0]);
+                animateDown(mContainerViews[1]);
+                animateDown(mContainerViews[2]);
+            });
+        }
     }
 
     private ContainerFrameLayout mFullscreenContainer;
+    private MyWebView mFullscreenWebView;
     public class OnClickImpl implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-            ContainerFrameLayout container = (ContainerFrameLayout) v;
-            if (mFullscreenContainer == container) return;
+            if (USE_WEBVIEW) {
+                MyWebView webview = (MyWebView) v;
+                if (mFullscreenWebView == webview) return;
 
-            mMainView.removeView(container);
-            mMainView.addView(container, 0);
+                mMainView.removeView(webview);
+                mMainView.addView(webview, 0);
 
-            if (mFullscreenContainer != null) {
-                animateDown(mFullscreenContainer);
+                if (mFullscreenWebView != null) {
+                    animateDown(mFullscreenWebView);
+                }
+                animateUp(webview);
+                mFullscreenWebView = webview;
+            } else {
+                ContainerFrameLayout container = (ContainerFrameLayout) v;
+                if (mFullscreenContainer == container) return;
+
+                mMainView.removeView(container);
+                mMainView.addView(container, 0);
+
+                if (mFullscreenContainer != null) {
+                    animateDown(mFullscreenContainer);
+                }
+                animateUp(container);
+                mFullscreenContainer = container;
             }
-            animateUp(container);
-            mFullscreenContainer = container;
         }
     }
 
@@ -226,6 +317,29 @@ public class WebLayerAnimationDemoActivity extends FragmentActivity {
                 .alpha(1.0f)
                 .setDuration(500);
         container.setInterceptTouchEvent(false);
+    }
+
+    private static void animateDown(MyWebView webview) {
+        int index = webview.getIndex();
+        webview.animate()
+                .scaleX(1.0f / 3)
+                .scaleY(1.0f / 3)
+                .translationX(-webview.getWidth() / 3.0f + (webview.getWidth() / 3.0f * index))
+                .translationY(webview.getHeight() / 3.0f)
+                .alpha(0.8f)
+                .setDuration(500);
+        webview.setInterceptTouchEvent(true);
+    }
+
+    private static void animateUp(MyWebView webview) {
+        webview.animate()
+                .scaleX(1.0f)
+                .scaleY(1.0f)
+                .translationX(0)
+                .translationY(0)
+                .alpha(1.0f)
+                .setDuration(500);
+        webview.setInterceptTouchEvent(false);
     }
 
     /**
