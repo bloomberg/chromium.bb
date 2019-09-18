523c647765cede0ca14fe5a338566120688491bf
Takuto Ikuta <tikuta@chromium.org>
2019-09-18 02:53:34 +0000

    client: reduce disk access when deploy files to destination
    This CL changed not to check fs.exists just after file writing and skip
    ensure_tree/chmod if function calls are not necessary.
    
    Added Profiler for _map_file for granular performance investigation.
    
    This CL reduced the time to download isolated input of
    https://chromium-swarm.appspot.com/task?id=4756b682385c4d10
    from around 300 seconds to 150 seconds in my P920 Win10 when all isolate
    files are cached in locally.
    
    Bug: 908483
    Change-Id: I112c29c5f3a15aca03b2ed89a70274df3e5d6fc0
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/1808999
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Mirrored-From: https://chromium.googlesource.com/infra/luci/luci-py
    Cr-Mirrored-Commit: 8aacd42f0e2ede371d3a5ffe66b53409723b6fe4

--- isolateserver.py
+++ isolateserver.py
@@ -138,7 +138,7 @@ def fileobj_path(fileobj):
   # fs.exists requires an absolute path, otherwise it will fail with an
   # assertion error.
   if not os.path.isabs(name):
-      return None
+    return None
 
   if fs.exists(name):
     return name
@@ -226,12 +226,16 @@ def putfile(srcfileobj, dstpath, file_mode=None, size=-1, use_symlink=False):
       link_mode = file_path.COPY
 
     file_path.link_file(dstpath, srcpath, link_mode)
+    assert fs.exists(dstpath)
   else:
     # Need to write out the file
     with fs.open(dstpath, 'wb') as dstfileobj:
       fileobj_copy(dstfileobj, srcfileobj, size)
 
-  assert fs.exists(dstpath)
+    if sys.platform == 'win32' and file_mode and file_mode & stat.S_IWRITE:
+      # On windows, mode other than removing stat.S_IWRITE is ignored. Returns
+      # early to skip slow/unnecessary chmod call.
+      return
 
   # file_mode of 0 is actually valid, so need explicit check.
   if file_mode is not None:
@@ -1232,47 +1236,47 @@ def _map_file(dst, digest, props, cache, read_only, use_symlinks):
   """Put downloaded file to destination path. This function is used for multi
   threaded file putting.
   """
-  with cache.getfileobj(digest) as srcfileobj:
-    filetype = props.get('t', 'basic')
+  with tools.Profiler("_map_file for %s" % dst):
+    with cache.getfileobj(digest) as srcfileobj:
+      filetype = props.get('t', 'basic')
 
-    if filetype == 'basic':
-      # Ignore all bits apart from the user.
-      file_mode = (props.get('m') or 0o500) & 0o700
-      if read_only:
-        # Enforce read-only if the root bundle does.
-        file_mode &= 0o500
-      putfile(srcfileobj, dst, file_mode,
-              use_symlink=use_symlinks)
+      if filetype == 'basic':
+        # Ignore all bits apart from the user.
+        file_mode = (props.get('m') or 0o500) & 0o700
+        if read_only:
+          # Enforce read-only if the root bundle does.
+          file_mode &= 0o500
+        putfile(srcfileobj, dst, file_mode, use_symlink=use_symlinks)
 
-    elif filetype == 'tar':
-      basedir = os.path.dirname(dst)
-      with tarfile.TarFile(fileobj=srcfileobj, encoding='utf-8') as t:
-        for ti in t:
-          if not ti.isfile():
-            logging.warning(
-                'Path(%r) is nonfile (%s), skipped',
-                ti.name, ti.type)
-            continue
-          # Handle files created on Windows fetched on POSIX and the
-          # reverse.
-          other_sep = '/' if os.path.sep == '\\' else '\\'
-          name = ti.name.replace(other_sep, os.path.sep)
-          fp = os.path.normpath(os.path.join(basedir, name))
-          if not fp.startswith(basedir):
-            logging.error(
-                'Path(%r) is outside root directory',
-                fp)
-          ifd = t.extractfile(ti)
-          file_path.ensure_tree(os.path.dirname(fp))
-          file_mode = ti.mode & 0o700
-          if read_only:
-            # Enforce read-only if the root bundle does.
-            file_mode &= 0o500
-          putfile(ifd, fp, file_mode, ti.size)
+      elif filetype == 'tar':
+        basedir = os.path.dirname(dst)
+        with tarfile.TarFile(fileobj=srcfileobj, encoding='utf-8') as t:
+          ensured_dirs = set()
+          for ti in t:
+            if not ti.isfile():
+              logging.warning('Path(%r) is nonfile (%s), skipped', ti.name,
+                              ti.type)
+              continue
+            # Handle files created on Windows fetched on POSIX and the
+            # reverse.
+            other_sep = '/' if os.path.sep == '\\' else '\\'
+            name = ti.name.replace(other_sep, os.path.sep)
+            fp = os.path.normpath(os.path.join(basedir, name))
+            if not fp.startswith(basedir):
+              logging.error('Path(%r) is outside root directory', fp)
+            ifd = t.extractfile(ti)
+            fp_dir = os.path.dirname(fp)
+            if fp_dir not in ensured_dirs:
+              file_path.ensure_tree(fp_dir)
+              ensured_dirs.add(fp_dir)
+            file_mode = ti.mode & 0o700
+            if read_only:
+              # Enforce read-only if the root bundle does.
+              file_mode &= 0o500
+            putfile(ifd, fp, file_mode, ti.size)
 
-    else:
-      raise isolated_format.IsolatedError(
-            'Unknown file type %r' % filetype)
+      else:
+        raise isolated_format.IsolatedError('Unknown file type %r' % filetype)
 
 
 def fetch_isolated(isolated_hash, storage, cache, outdir, use_symlinks,
