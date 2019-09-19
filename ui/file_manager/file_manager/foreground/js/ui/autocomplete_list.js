766439d5cbfd4d7b811f0e032ae91643b17eb23f
Luciano Pacheco <lucmult@chromium.org>
2019-09-19 06:01:44 +0000

    Files app: ES6 class for autocomplete_list.js
    Manually migrated. Since this file has been donated to Files app and
    we're the only users I've removed AutocompleteListItem since we have it
    completely replaced in search_box.js thus wasn't been used.
    
    Test: Manually and browser_tests --gtest_filter="Search/FilesApp*"
    Bug: 778674
    Change-Id: Ibcfbc9fc1439fdfe5d81c02b4294ef47aeea7b2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1807642
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697913}
    
    Former-commit-id: fc93d91ef0d2b88c5282161c56b4938ac04e9e43

--- ui/file_manager/file_manager/foreground/js/ui/autocomplete_list.js
+++ ui/file_manager/file_manager/foreground/js/ui/autocomplete_list.js
@@ -3,98 +3,54 @@
 // found in the LICENSE file.
 
 cr.define('cr.ui', function() {
-  /** @const */ const ArrayDataModel = cr.ui.ArrayDataModel;
-  /** @const */ const List = cr.ui.List;
-  /** @const */ const ListItem = cr.ui.ListItem;
-
-  /**
-   * Creates a new autocomplete list item.
-   * This is suitable for selecting a web site, and used by default.
-   * A different behavior can be set by AutocompleteListItem.itemConstructor.
-   * @param {*} pageInfo The page this item represents.
-   * @constructor
-   * @extends {cr.ui.ListItem}
-   */
-  function AutocompleteListItem(pageInfo) {
-    const el = cr.doc.createElement('div');
-    el.pageInfo_ = pageInfo;
-    AutocompleteListItem.decorate(el);
-    return el;
-  }
-
-  /**
-   * Decorates an element as an autocomplete list item.
-   * @param {!HTMLElement} el The element to decorate.
-   */
-  AutocompleteListItem.decorate = function(el) {
-    el.__proto__ = AutocompleteListItem.prototype;
-    /** @type {AutocompleteListItem} */ (el).decorate();
-  };
-
-  AutocompleteListItem.prototype = {
-    __proto__: ListItem.prototype,
-
-    /** @override */
-    decorate: function() {
-      ListItem.prototype.decorate.call(this);
-
-      const title = this.pageInfo_['title'];
-      const url = this.pageInfo_['displayURL'];
-      const titleEl = this.ownerDocument.createElement('span');
-      titleEl.className = 'title';
-      titleEl.textContent = title || url;
-      this.appendChild(titleEl);
-
-      if (title && title.length > 0 && url != title) {
-        const separatorEl = this.ownerDocument.createTextNode(' - ');
-        this.appendChild(separatorEl);
-
-        const urlEl = this.ownerDocument.createElement('span');
-        urlEl.className = 'url';
-        urlEl.textContent = url;
-        this.appendChild(urlEl);
-      }
-    },
-  };
-
   /**
    * Creates a new autocomplete list popup.
-   * @constructor
    * @extends {cr.ui.List}
    */
-  const AutocompleteList = cr.ui.define('list');
+  class AutocompleteList extends cr.ui.List {
+    constructor() {
+      super();
+      this.__proto__ = AutocompleteList.prototype;
 
-  AutocompleteList.prototype = {
-    __proto__: List.prototype,
+      /**
+       * The text field the autocomplete popup is currently attached to, if any.
+       * @type {HTMLElement}
+       * @private
+       */
+      this.targetInput_ = null;
 
-    /**
-     * The text field the autocomplete popup is currently attached to, if any.
-     * @type {HTMLElement}
-     * @private
-     */
-    targetInput_: null,
+      /**
+       * Keydown event listener to attach to a text field.
+       * @type {Function}
+       * @private
+       */
+      this.textFieldKeyHandler_ = null;
 
-    /**
-     * Keydown event listener to attach to a text field.
-     * @type {Function}
-     * @private
-     */
-    textFieldKeyHandler_: null,
+      /**
+       * Input event listener to attach to a text field.
+       * @type {Function}
+       * @private
+       */
+      this.textFieldInputHandler_ = null;
 
-    /**
-     * Input event listener to attach to a text field.
-     * @type {Function}
-     * @private
-     */
-    textFieldInputHandler_: null,
+      /**
+       * syncWidthAndPositionToInput function bound to |this|.
+       * @type {!Function|undefined}
+       * @private
+       */
+      this.boundSyncWidthAndPositionToInput_ = undefined;
+
+      this.decorate();
+    }
 
     /** @override */
-    decorate: function() {
-      List.prototype.decorate.call(this);
+    decorate() {
+      cr.ui.List.prototype.decorate.call(this);
+
       this.classList.add('autocomplete-suggestions');
       this.selectionModel = new cr.ui.ListSingleSelectionModel;
 
-      this.itemConstructor = AutocompleteListItem;
+      this.itemConstructor = cr.ui.ListItem;
       this.textFieldKeyHandler_ = this.handleAutocompleteKeydown_.bind(this);
       const self = this;
       this.textFieldInputHandler_ = function(e) {
@@ -107,36 +63,36 @@ cr.define('cr.ui', function() {
       });
       // Start hidden; adding suggestions will unhide.
       this.hidden = true;
-    },
+    }
 
     /** @override */
-    createItem: function(pageInfo) {
+    createItem(pageInfo) {
       return new this.itemConstructor(pageInfo);
-    },
+    }
 
     /**
      * The suggestions to show.
      * @type {Array}
      */
     set suggestions(suggestions) {
-      this.dataModel = new ArrayDataModel(suggestions);
+      this.dataModel = new cr.ui.ArrayDataModel(suggestions);
       this.hidden = !this.targetInput_ || suggestions.length == 0;
-    },
+    }
 
     /**
      * Requests new suggestions. Called when new suggestions are needed.
      * @param {string} query the text to autocomplete from.
      */
-    requestSuggestions: function(query) {},
+    requestSuggestions(query) {}
 
     /**
      * Handles the Enter keydown event.
      * By default, clears and hides the autocomplete popup. Note that the
      * keydown event bubbles up, so the input field can handle the event.
      */
-    handleEnterKeydown: function() {
+    handleEnterKeydown() {
       this.suggestions = [];
-    },
+    }
 
     /**
      * Handles the selected suggestion. Called when a suggestion is selected.
@@ -144,7 +100,7 @@ cr.define('cr.ui', function() {
      * of the selected suggestion.
      * @param {Object} selectedSuggestion
      */
-    handleSelectedSuggestion: function(selectedSuggestion) {
+    handleSelectedSuggestion(selectedSuggestion) {
       const input = this.targetInput_;
       if (!input) {
         return;
@@ -153,14 +109,14 @@ cr.define('cr.ui', function() {
       // Programatically change the value won't trigger a change event, but
       // clients are likely to want to know when changes happen, so fire one.
       cr.dispatchSimpleEvent(input, 'change', true);
-    },
+    }
 
     /**
      * Attaches the popup to the given input element. Requires
      * that the input be wrapped in a block-level container of the same width.
      * @param {HTMLElement} input The input element to attach to.
      */
-    attachToInput: function(input) {
+    attachToInput(input) {
       if (this.targetInput_ == input) {
         return;
       }
@@ -183,12 +139,12 @@ cr.define('cr.ui', function() {
       // We need to call syncWidthAndPositionToInput whenever page zoom level or
       // page size is changed.
       window.addEventListener('resize', this.boundSyncWidthAndPositionToInput_);
-    },
+    }
 
     /**
      * Detaches the autocomplete popup from its current input element, if any.
      */
-    detach: function() {
+    detach() {
       const input = this.targetInput_;
       if (!input) {
         return;
@@ -202,27 +158,20 @@ cr.define('cr.ui', function() {
         window.removeEventListener(
             'resize', this.boundSyncWidthAndPositionToInput_);
       }
-    },
+    }
 
     /**
      * Makes sure that the suggestion list matches the width and the position
      * of the input it is attached to. Should be called any time the input is
      * resized.
      */
-    syncWidthAndPositionToInput: function() {
+    syncWidthAndPositionToInput() {
       const input = this.targetInput_;
       if (input) {
         this.style.width = input.getBoundingClientRect().width + 'px';
         cr.ui.positionPopupAroundElement(input, this, cr.ui.AnchorType.BELOW);
       }
-    },
-
-    /**
-     * syncWidthAndPositionToInput function bound to |this|.
-     * @type {!Function|undefined}
-     * @private
-     */
-    boundSyncWidthAndPositionToInput_: undefined,
+    }
 
     /**
      * @return {HTMLElement} The text field the autocomplete popup is currently
@@ -230,7 +179,7 @@ cr.define('cr.ui', function() {
      */
     get targetInput() {
       return this.targetInput_;
-    },
+    }
 
     /**
      * Handles input field key events that should be interpreted as autocomplete
@@ -238,7 +187,7 @@ cr.define('cr.ui', function() {
      * @param {Event} event The keydown event.
      * @private
      */
-    handleAutocompleteKeydown_: function(event) {
+    handleAutocompleteKeydown_(event) {
       if (this.hidden) {
         return;
       }
@@ -266,8 +215,9 @@ cr.define('cr.ui', function() {
         event.preventDefault();
         event.stopPropagation();
       }
-    },
-  };
+    }
+  }
+  AutocompleteList.prototype.__proto__ = cr.ui.List.prototype;
 
   return {AutocompleteList: AutocompleteList};
 });
