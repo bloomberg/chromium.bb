ce690c5215e3e141315429151c3f46505d61db8a
Andrew Xu <andrewxu@chromium.org>
2019-09-19 20:10:09 +0000

    Fix a broken browser test caused by enabling the scrollable shelf
    The scrollable shelf is notified of adding new item at the end of
    shelf view's bounds animation. So wait for that animation to finish in
    the test.
    
    Bug: 1002576
    Change-Id: Ic91a2ff22fb8206dbbf1b248616e48e850176012
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1814643
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698157}
    
    Former-commit-id: 17731d6c64680f506796808bc397d5e3b05e93dd

--- chrome/browser/ui/ash/launcher/arc_app_launcher_browsertest.cc
+++ chrome/browser/ui/ash/launcher/arc_app_launcher_browsertest.cc
@@ -331,6 +331,13 @@ IN_PROC_BROWSER_TEST_F(ArcAppDeferredLauncherBrowserTest,
   aura::Window* const root_window = ash::Shell::GetPrimaryRootWindow();
   ash::ShelfViewTestAPI test_api(
       ash::Shelf::ForWindow(root_window)->GetShelfViewForTesting());
+
+  // In this test, we need the shelf button's bounds. The scrollable shelf
+  // is notified of the added shelf button and layouts its child views
+  // during the bounds animation. So wait for the bounds animation to finish
+  // then get the final bounds of the shelf button.
+  test_api.RunMessageLoopUntilAnimationsDone();
+
   const int item_index =
       controller->shelf_model()->ItemIndexByID(ash::ShelfID(app_id));
   ASSERT_GE(item_index, 0);
