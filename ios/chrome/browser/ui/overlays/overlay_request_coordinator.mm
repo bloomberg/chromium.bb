918723113db221e2ca4b646cafcd43484ef9384a
Kurt Horimoto <kkhorimoto@google.com>
2019-09-19 21:18:29 +0000

    [iOS] Add superclass for overlays that use AlertViewController.
    This is a preliminary step for consolidating implementations of HTTP
    auth, JavaScript, and App Launcher dialog overlays.
    
    Bug: 990070
    Change-Id: I31edbfad36db7c27d2b1e6db25c0de4b23fc9a31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1809613
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698190}
    
    Former-commit-id: 89c7eadd59a84293f9cf1a4ec108500df24e2ff6

--- ios/chrome/browser/ui/overlays/overlay_request_coordinator.mm
+++ ios/chrome/browser/ui/overlays/overlay_request_coordinator.mm
@@ -13,6 +13,14 @@
 
 @implementation OverlayRequestCoordinator
 
+- (void)dealloc {
+  // ChromeCoordinator's |-dealloc| calls |-stop|, which defaults to an animated
+  // dismissal.  OverlayRequestCoordinators should instead stop without
+  // animation so that the OverlayRequestCoordinatorDelegate can be notified of
+  // the dismissal immediately.
+  [self stopAnimated:NO];
+}
+
 + (BOOL)supportsRequest:(OverlayRequest*)request {
   return NO;
 }
