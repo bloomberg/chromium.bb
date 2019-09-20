ee3a1551614b7cbeebddb56a330d8f072dfbaaff
Fritz Koenig <frkoenig@chromium.org>
2019-09-20 01:48:39 +0000

    gpu: generate enum for GL_COMMANDS_ISSUED_TIMESTAMP_CHROMIUM
    Run ./generate_bindings.py
    
    Bug: 995155
    Tests: none
    
    Change-Id: I0f1b1662d3b42314695da8c31bfeb1df48714d06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1815280
    Auto-Submit: Fritz Koenig <frkoenig@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698303}
    
    Former-commit-id: bc1c33f47895bc7328d148ce71694a8924fff314

--- ui/gl/gl_enums_implementation_autogen.h
+++ ui/gl/gl_enums_implementation_autogen.h
@@ -1001,6 +1001,10 @@ static const GLEnums::EnumToString enum_to_string_table[] = {
         "GL_COMMANDS_ISSUED_CHROMIUM",
     },
     {
+        0x6005,
+        "GL_COMMANDS_ISSUED_TIMESTAMP_CHROMIUM",
+    },
+    {
         0x6006,
         "GL_ASYNC_PIXEL_PACK_COMPLETED_CHROMIUM",
     },
