bacecdaaa2b7b1e305720acd37aad41c9451a247
Peter Kasting <pkasting@chromium.org>
2019-09-20 18:37:54 +0000

    Remove using directives ("using namespace x") from mojo/.
    Bug: 82078
    Change-Id: Iede861b1cf4680d52f3dc1f298ca5f6ec8f398f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1815574
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698565}
    
    Former-commit-id: 7fac4a17d1a3031d194acae7bc7a901037f31053

--- mojo/core/channel_fuzzer.cc
+++ mojo/core/channel_fuzzer.cc
@@ -19,13 +19,11 @@
 #include <windows.h>
 #endif
 
-using namespace mojo::core;
-
 // A fake delegate for each Channel endpoint. By the time an incoming message
 // reaches a Delegate, all interesting message parsing at the lowest protocol
 // layer has already been done by the receiving Channel implementation, so this
 // doesn't need to do any work.
-class FakeChannelDelegate : public Channel::Delegate {
+class FakeChannelDelegate : public mojo::core::Channel::Delegate {
  public:
   FakeChannelDelegate() = default;
   ~FakeChannelDelegate() override = default;
@@ -33,14 +31,14 @@ class FakeChannelDelegate : public Channel::Delegate {
   void OnChannelMessage(const void* payload,
                         size_t payload_size,
                         std::vector<mojo::PlatformHandle> handles) override {}
-  void OnChannelError(Channel::Error error) override {}
+  void OnChannelError(mojo::core::Channel::Error error) override {}
 };
 
 // Message deserialization may register handles in the global handle table. We
 // need to initialize Core for that to be OK.
 struct Environment {
   Environment() : main_thread_task_executor(base::MessagePumpType::IO) {
-    InitializeCore();
+    mojo::core::InitializeCore();
   }
 
   base::SingleThreadTaskExecutor main_thread_task_executor;
@@ -54,9 +52,10 @@ extern "C" int LLVMFuzzerTestOneInput(const unsigned char* data, size_t size) {
   mojo::PlatformChannel channel;
 
   FakeChannelDelegate receiver_delegate;
-  auto receiver = Channel::Create(
-      &receiver_delegate, ConnectionParams(channel.TakeLocalEndpoint()),
-      Channel::HandlePolicy::kRejectHandles,
+  auto receiver = mojo::core::Channel::Create(
+      &receiver_delegate,
+      mojo::core::ConnectionParams(channel.TakeLocalEndpoint()),
+      mojo::core::Channel::HandlePolicy::kRejectHandles,
       environment->main_thread_task_executor.task_runner());
 
 #if defined(OS_WIN)
@@ -78,14 +77,15 @@ extern "C" int LLVMFuzzerTestOneInput(const unsigned char* data, size_t size) {
   receiver->Start();
 
   FakeChannelDelegate sender_delegate;
-  auto sender = Channel::Create(
-      &sender_delegate, ConnectionParams(channel.TakeRemoteEndpoint()),
-      Channel::HandlePolicy::kRejectHandles,
+  auto sender = mojo::core::Channel::Create(
+      &sender_delegate,
+      mojo::core::ConnectionParams(channel.TakeRemoteEndpoint()),
+      mojo::core::Channel::HandlePolicy::kRejectHandles,
       environment->main_thread_task_executor.task_runner());
   sender->Start();
 
-  sender->Write(
-      Channel::Message::CreateRawForFuzzing(base::make_span(data, size)));
+  sender->Write(mojo::core::Channel::Message::CreateRawForFuzzing(
+      base::make_span(data, size)));
 
   // Make sure |receiver| does whatever work it's gonna do in response to our
   // message. By the time the loop goes idle, all parsing will be done.
