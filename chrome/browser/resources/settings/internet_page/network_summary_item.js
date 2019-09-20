51551a1e6aef4e478b8afef25852b925f8c2aaa0
James Hawkins <jhawkins@chromium.org>
2019-09-20 00:16:01 +0000

    OS Settings/Network: Always show the Wi-Fi subpage if enabled.
    Previous functionality did nothing when the user clicked on the Wi-Fi
    list item while Wi-Fi was connecting, which felt like a broken
    experience.
    
    The new functionality shows the Wi-Fi subpage in this case.
    
    R=stevenjb@chromium.org
    
    Bug: 998389
    Test: none
    Change-Id: I6e0baddac5ab1a7a3897bf7a1f7a2118f3764a53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1814538
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Auto-Submit: James Hawkins <jhawkins@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698261}
    
    Former-commit-id: ff850b92cfd5a48b5ae8b5a0d65693fe5baf65eb

--- chrome/browser/resources/settings/internet_page/network_summary_item.js
+++ chrome/browser/resources/settings/internet_page/network_summary_item.js
@@ -287,14 +287,22 @@ Polymer({
     const type = deviceState.type;
     if (type == mojom.NetworkType.kTether ||
         (type == mojom.NetworkType.kCellular && this.tetherDeviceState)) {
-      // The "Mobile data" subpage should always be shown if Tether networks are
+      // The "Mobile data" subpage should always be shown if Tether is
       // available, even if there are currently no associated networks.
       return true;
     }
-    const minlen =
-        (type == mojom.NetworkType.kWiFi || type == mojom.NetworkType.kVPN) ?
-        1 :
-        2;
+    let minlen;
+    if (type == mojom.NetworkType.kVPN) {
+      // VPN subpage provides provider info so show if there are any networks.
+      minlen = 1;
+    } else if (type == mojom.NetworkType.kWiFi) {
+      // WiFi subpage includes 'Known Networks' so always show, even if the
+      // technology is still enabling / scanning, or none are visible.
+      minlen = 0;
+    } else {
+      // By default, only show the subpage if there are 2+ networks
+      minlen = 2;
+    }
     return networkStateList.length >= minlen;
   },
 
