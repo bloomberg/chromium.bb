1b94a2e82a94baa81036b87fe197026b52e58af5
Nate Chapin <japhet@chromium.org>
2019-09-20 20:20:34 +0000

    Revert "media/gpu/BUILD.gn: restrict image_processor, video_frame_mapper to CrOs, Chromecast"
    This reverts commit 4e1ef558f50f9df07deb6ddeb0371f073d831ea7 [formerly ad6024161c38ad31a65b3161b792aebab8fc7452].
    
    Reason for revert: Compile breaks: https://ci.chromium.org/p/chromium/builders/ci/ChromiumOS%20ASAN%20Release/104250
    
    Original change's description:
    > media/gpu/BUILD.gn: restrict image_processor, video_frame_mapper to CrOs, Chromecast
    > 
    > This CL restricts //media/gpu:image_processor and :video_frame_mapper
    > to ChromeOS and ChromeCast, since they are only implemented for these
    > platforms.  Same for image_processor_test.
    > 
    > video_frame_file_writer.cc sandwiched some uses of |video_frame_mapper_|
    > in an OS_LINUX and left others unprotected, which confused OS_WIN
    > builds. This CL changes those to if defined(OS_CHROMEOS), since I
    > don't think this file is meant to be compiled nor run on OS_LINUX.
    > 
    > Change-Id: I8098d55859c50a752b40a63955684e558db3582a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787467
    > Commit-Queue: Miguel Casas <mcasas@chromium.org>
    > Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    > Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#698566}
    
    TBR=deanliao@chromium.org,mcasas@chromium.org,akahuang@chromium.org,hiroh@chromium.org,acourbot@chromium.org,yucliu@chromium.org,dstaessens@chromium.org
    
    Change-Id: I1f70a99e52b6ca640e4f8c9fcc79ba71dbb7f5f0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1816972
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698595}
    
    Former-commit-id: de1720a73e1ee6b3a1d4df2f74baa621808336d3

--- media/gpu/test/video_frame_file_writer.cc
+++ media/gpu/test/video_frame_file_writer.cc
@@ -109,7 +109,6 @@ void VideoFrameFileWriter::ProcessVideoFrameTask(
   base::SStringPrintf(&filename, FILE_PATH_LITERAL("frame_%04zu_%dx%d"),
                       frame_index, visible_size.width(), visible_size.height());
 
-#if defined(OS_CHROMEOS)
   // Create VideoFrameMapper if not yet created. The decoder's output pixel
   // format is not known yet when creating the VideoFrameWriter. We can only
   // create the VideoFrameMapper upon receiving the first video frame.
@@ -118,7 +117,6 @@ void VideoFrameFileWriter::ProcessVideoFrameTask(
         VideoFrameMapperFactory::CreateMapper(video_frame->format());
     LOG_ASSERT(video_frame_mapper_) << "Failed to create VideoFrameMapper";
   }
-#endif
 
   switch (output_format_) {
     case OutputFormat::kPNG:
@@ -141,7 +139,7 @@ void VideoFrameFileWriter::WriteVideoFramePNG(
   DCHECK(video_frame_mapper_);
 
   auto mapped_frame = video_frame;
-#if defined(OS_CHROMEOS)
+#if defined(OS_LINUX)
   if (video_frame->storage_type() == VideoFrame::STORAGE_DMABUFS)
     mapped_frame = video_frame_mapper_->Map(std::move(video_frame));
 #endif
@@ -183,7 +181,7 @@ void VideoFrameFileWriter::WriteVideoFrameYUV(
   DCHECK(video_frame_mapper_);
 
   auto mapped_frame = video_frame;
-#if defined(OS_CHROMEOS)
+#if defined(OS_LINUX)
   if (video_frame->storage_type() == VideoFrame::STORAGE_DMABUFS)
     mapped_frame = video_frame_mapper_->Map(std::move(video_frame));
 #endif
