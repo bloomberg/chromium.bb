6e632d3c4566ae03a668645cf67094afe4e7095c
James Zern <jzern@google.com>
2019-09-20 23:04:52 -0700

    error_block_test: fix visual studio build
    move conditional test parameters to arrays to avoid a #if within a
    macro
    
    since:
    7683ed5f7 Exclude hbd functions in av1_rtcd_defs.pl from HBD flag.
    
    Change-Id: I9d01bda9e485f8246e2d5b2955e5748b1bfc4826

--- test/error_block_test.cc
+++ test/error_block_test.cc
@@ -235,35 +235,39 @@ TEST_P(ErrorBlockTest, DISABLED_Speed) {
 using ::testing::make_tuple;
 
 #if (HAVE_SSE2)
-INSTANTIATE_TEST_CASE_P(
-    SSE2, ErrorBlockTest,
-    ::testing::Values(
+const ErrorBlockParam kErrorBlockTestParamsSse2[] = {
 #if CONFIG_AV1_HIGHBITDEPTH
-        make_tuple(&av1_highbd_block_error_sse2, &av1_highbd_block_error_c,
-                   AOM_BITS_10),
-        make_tuple(&av1_highbd_block_error_sse2, &av1_highbd_block_error_c,
-                   AOM_BITS_12),
-        make_tuple(&av1_highbd_block_error_sse2, &av1_highbd_block_error_c,
-                   AOM_BITS_8),
+  make_tuple(&av1_highbd_block_error_sse2, &av1_highbd_block_error_c,
+             AOM_BITS_10),
+  make_tuple(&av1_highbd_block_error_sse2, &av1_highbd_block_error_c,
+             AOM_BITS_12),
+  make_tuple(&av1_highbd_block_error_sse2, &av1_highbd_block_error_c,
+             AOM_BITS_8),
 #endif
-        make_tuple(&BlockError8BitWrapper<av1_block_error_sse2>,
-                   &BlockError8BitWrapper<av1_block_error_c>, AOM_BITS_8)));
+  make_tuple(&BlockError8BitWrapper<av1_block_error_sse2>,
+             &BlockError8BitWrapper<av1_block_error_c>, AOM_BITS_8)
+};
+
+INSTANTIATE_TEST_CASE_P(SSE2, ErrorBlockTest,
+                        ::testing::ValuesIn(kErrorBlockTestParamsSse2));
 #endif  // HAVE_SSE2
 
 #if (HAVE_AVX2)
-INSTANTIATE_TEST_CASE_P(
-    AVX2, ErrorBlockTest,
-    ::testing::Values(
+const ErrorBlockParam kErrorBlockTestParamsAvx2[] = {
 #if CONFIG_AV1_HIGHBITDEPTH
-        make_tuple(&av1_highbd_block_error_avx2, &av1_highbd_block_error_c,
-                   AOM_BITS_10),
-        make_tuple(&av1_highbd_block_error_avx2, &av1_highbd_block_error_c,
-                   AOM_BITS_12),
-        make_tuple(&av1_highbd_block_error_avx2, &av1_highbd_block_error_c,
-                   AOM_BITS_8),
+  make_tuple(&av1_highbd_block_error_avx2, &av1_highbd_block_error_c,
+             AOM_BITS_10),
+  make_tuple(&av1_highbd_block_error_avx2, &av1_highbd_block_error_c,
+             AOM_BITS_12),
+  make_tuple(&av1_highbd_block_error_avx2, &av1_highbd_block_error_c,
+             AOM_BITS_8),
 #endif
-        make_tuple(&BlockError8BitWrapper<av1_block_error_avx2>,
-                   &BlockError8BitWrapper<av1_block_error_c>, AOM_BITS_8)));
+  make_tuple(&BlockError8BitWrapper<av1_block_error_avx2>,
+             &BlockError8BitWrapper<av1_block_error_c>, AOM_BITS_8)
+};
+
+INSTANTIATE_TEST_CASE_P(AVX2, ErrorBlockTest,
+                        ::testing::ValuesIn(kErrorBlockTestParamsAvx2));
 #endif  // HAVE_AVX2
 
 #if (HAVE_MSA)
