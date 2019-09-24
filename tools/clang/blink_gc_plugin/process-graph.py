0670b2ef33c45ce50dd41c5a91260ba9bb94d83e
Hans Wennborg <hans@chromium.org>
2019-09-24 10:56:54 +0000

    Fix process-graph.py after #698787, take 2
    After the Python 3 conversion, there is now an extra space in the output. This
    shows the problem:
    
    >>> print "a\n", "b"
    a
    b
    >>> from __future__ import print_function
    >>> print("a\n", "b")
    a
     b
    
    Fix it by specifying sep="".
    
    TBR=thakis
    
    Bug: 1007195
    Change-Id: I20fea51951d552fcffee4704bd515c54bb84ef63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1821598
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699286}
    
    Former-commit-id: 68f78607ed53b28799749e02fb27970e85ba8292

--- tools/clang/blink_gc_plugin/process-graph.py
+++ tools/clang/blink_gc_plugin/process-graph.py
@@ -313,7 +313,7 @@ def report_cycle(root_edge):
   sout = out.getvalue()
   if not is_ignored_cycle(sout):
     print("\nFound a potentially leaking cycle starting from a GC root:\n",
-          sout)
+          sout, sep="")
     set_reported_error(True)
 
 def load_graph():
