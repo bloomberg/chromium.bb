f9e9c09df00d04450ae0bd3d0b9bb706de909f01
Peter Kasting <pkasting@chromium.org>
2019-09-24 19:51:45 +0000

    Remove using directives ("using namespace x") from android_webview/.
    Bug: 82078
    Change-Id: I3bbe1dbbf33aaf3c700b35b55576a8f41334aab0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1820538
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699462}
    
    Former-commit-id: 59633ee4ef35a39c4c589958b0db8a098d799adf

--- android_webview/browser/aw_variations_seed_bridge.cc
+++ android_webview/browser/aw_variations_seed_bridge.cc
@@ -19,28 +19,27 @@
 namespace android_webview {
 
 std::unique_ptr<variations::SeedResponse> GetAndClearJavaSeed() {
-  using namespace base::android;
-
-  JNIEnv* env = AttachCurrentThread();
+  JNIEnv* env = base::android::AttachCurrentThread();
   if (!Java_AwVariationsSeedBridge_haveSeed(env))
     return nullptr;
 
-  ScopedJavaLocalRef<jstring> j_signature =
+  base::android::ScopedJavaLocalRef<jstring> j_signature =
       Java_AwVariationsSeedBridge_getSignature(env);
-  ScopedJavaLocalRef<jstring> j_country =
+  base::android::ScopedJavaLocalRef<jstring> j_country =
       Java_AwVariationsSeedBridge_getCountry(env);
-  ScopedJavaLocalRef<jstring> j_date = Java_AwVariationsSeedBridge_getDate(env);
+  base::android::ScopedJavaLocalRef<jstring> j_date =
+      Java_AwVariationsSeedBridge_getDate(env);
   jboolean j_is_gzip_compressed =
       Java_AwVariationsSeedBridge_getIsGzipCompressed(env);
-  ScopedJavaLocalRef<jbyteArray> j_data =
+  base::android::ScopedJavaLocalRef<jbyteArray> j_data =
       Java_AwVariationsSeedBridge_getData(env);
   Java_AwVariationsSeedBridge_clearSeed(env);
 
   auto java_seed = std::make_unique<variations::SeedResponse>();
   base::android::JavaByteArrayToString(env, j_data, &java_seed->data);
-  java_seed->signature = ConvertJavaStringToUTF8(j_signature);
-  java_seed->country = ConvertJavaStringToUTF8(j_country);
-  java_seed->date = ConvertJavaStringToUTF8(j_date);
+  java_seed->signature = base::android::ConvertJavaStringToUTF8(j_signature);
+  java_seed->country = base::android::ConvertJavaStringToUTF8(j_country);
+  java_seed->date = base::android::ConvertJavaStringToUTF8(j_date);
   java_seed->is_gzip_compressed = static_cast<bool>(j_is_gzip_compressed);
   return java_seed;
 }
