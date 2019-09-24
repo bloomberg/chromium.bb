df9de660a0e76a1df96d616b25c611bad057c71e
Eugene But <eugenebut@google.com>
2019-09-24 19:50:02 +0000

    Mark testOpeningIncognitoTabsDoNotLeak as flaky.
    The flakiness was observed on iOS 13/13, iPhone/iPad, device/simulator.
    
    TBR=javierrobles@chromium.org
    
    Bug: 1007421
    Change-Id: I75ee2ff2f2c6fc8c04be7d6307db03445c28fc51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1822979
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699461}
    
    Former-commit-id: 072d3ec536377617f3e94787a663310ac1bf0e1f

--- ios/chrome/browser/ui/autofill/manual_fill/fallback_coordinator_egtest.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/fallback_coordinator_egtest.mm
@@ -708,7 +708,8 @@ void DockKeyboard() {
 // Tests the mediator stops observing objects when the incognito BVC is
 // destroyed. Waiting for dealloc was causing a race condition with the
 // autorelease pool, and some times a DCHECK will be hit.
-- (void)testOpeningIncognitoTabsDoNotLeak {
+// TODO(crbug.com/1007421): This test is flaky on all configurations.
+- (void)FLAKY_testOpeningIncognitoTabsDoNotLeak {
   const GURL URL = self.testServer->GetURL(kFormHTMLFile);
   std::string webViewText("Profile form");
   AddAutofillProfile(_personalDataManager);
