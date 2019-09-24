b2962c249df5f0a12ecfbcab49d9a8d6b9afa786
Donn Denman <donnd@chromium.org>
2019-09-24 02:48:44 +0000

    [TTS] limit sheet size and update highlight.
    Limits the sheet height to 95% of a tab's full height.
    Updates the touch highlight area of the Bar to limit height
    under the new layout.
    
    BUG=1005558
    
    Change-Id: I57f4bfb6cca53096e93e00679098ef6b3933671b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1812245
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Auto-Submit: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699169}
    
    Former-commit-id: faa9f796b79a93029bd14390961a000be63993cd

--- chrome/browser/android/compositor/layer/contextual_search_layer.cc
+++ chrome/browser/android/compositor/layer/contextual_search_layer.cc
@@ -223,11 +223,12 @@ void ContextualSearchLayer::SetProperties(
   // ---------------------------------------------------------------------------
   // Search Term, Context and Search Caption
   // ---------------------------------------------------------------------------
-  SetupTextLayer(search_bar_top, search_bar_height,
-                 search_text_layer_min_height, search_caption_resource_id,
-                 search_caption_visible, search_caption_animation_percentage,
-                 search_term_opacity, search_context_resource_id,
-                 search_context_opacity, search_term_caption_spacing);
+  int text_layer_height = SetupTextLayer(
+      search_bar_top, search_bar_height, search_text_layer_min_height,
+      search_caption_resource_id, search_caption_visible,
+      search_caption_animation_percentage, search_term_opacity,
+      search_context_resource_id, search_context_opacity,
+      search_term_caption_spacing);
 
   // ---------------------------------------------------------------------------
   // Arrow Icon
@@ -348,10 +349,17 @@ void ContextualSearchLayer::SetProperties(
   if (touch_highlight_visible) {
     if (touch_highlight_layer_->parent() != layer_)
       layer_->AddChild(touch_highlight_layer_);
-    gfx::Size background_size(touch_highlight_width, search_bar_height);
+    bool is_overlay_new_layout =
+        rounded_bar_top_resource_id != kInvalidResourceID;
+    int highlight_height =
+        is_overlay_new_layout ? text_layer_height : search_bar_height;
+    int highlight_top = search_bar_top;
+    highlight_top +=
+        is_overlay_new_layout ? (search_bar_height - text_layer_height) / 2 : 0;
+    gfx::Size background_size(touch_highlight_width, highlight_height);
     touch_highlight_layer_->SetBounds(background_size);
-    touch_highlight_layer_->SetPosition(gfx::PointF(
-        touch_highlight_x_offset, search_bar_top));
+    touch_highlight_layer_->SetPosition(
+        gfx::PointF(touch_highlight_x_offset, highlight_top));
   } else {
     touch_highlight_layer_->RemoveFromParent();
   }
@@ -458,16 +466,16 @@ void ContextualSearchLayer::SetCustomImageProperties(
       gfx::PointF(side_margin, custom_image_y_offset));
 }
 
-void ContextualSearchLayer::SetupTextLayer(float bar_top,
-                                           float bar_height,
-                                           float search_text_layer_min_height,
-                                           int caption_resource_id,
-                                           bool caption_visible,
-                                           float animation_percentage,
-                                           float search_term_opacity,
-                                           int context_resource_id,
-                                           float context_opacity,
-                                           float term_caption_spacing) {
+int ContextualSearchLayer::SetupTextLayer(float bar_top,
+                                          float bar_height,
+                                          float search_text_layer_min_height,
+                                          int caption_resource_id,
+                                          bool caption_visible,
+                                          float animation_percentage,
+                                          float search_term_opacity,
+                                          int context_resource_id,
+                                          float context_opacity,
+                                          float term_caption_spacing) {
   // ---------------------------------------------------------------------------
   // Setup the Drawing Hierarchy
   // ---------------------------------------------------------------------------
@@ -566,7 +574,7 @@ void ContextualSearchLayer::SetupTextLayer(float bar_top,
   if (!caption_visible || animation_percentage == 0.f || !caption_resource) {
     bar_text_->SetPosition(gfx::PointF(0.f, term_top));
     search_context_->SetPosition(gfx::PointF(0.f, term_top));
-    return;
+    return layer_height;
   }
 
   // Calculate the positions for the Term and Caption when the Caption
@@ -591,6 +599,7 @@ void ContextualSearchLayer::SetupTextLayer(float bar_top,
   bar_text_->SetPosition(gfx::PointF(0.f, term_top));
   search_context_->SetPosition(gfx::PointF(0.f, term_top));
   search_caption_->SetPosition(gfx::PointF(0.f, caption_top));
+  return layer_height;
 }
 
 void ContextualSearchLayer::SetThumbnail(const SkBitmap* thumbnail) {
