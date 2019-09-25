04540b9b4573797c96d4c0a3585253d9c274a8a1
Sergey Ulanov <sergeyu@chromium.org>
2019-09-25 21:51:37 +0000

    Revert "[Fuchsia] Reenable protected mode in WebEngine"
    This reverts commit 3da0d4d59c6617f8d8c33b9488d07a744858bfbe [formerly a2c0ced36d5f45f1cc59a92cf7c483607993ce0e].
    
    Reason for revert: Fuchsia doesn't provide enough protected memory. https://fuchsia-review.googlesource.com/c/fuchsia/+/322346 was reverted.
    
    Original change's description:
    > [Fuchsia] Reenable protected mode in WebEngine
    > 
    > Bug: 982922, fuchsia:24570
    > Change-Id: If9fb495249488c43f97e4451a14f0193911d600f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1804527
    > Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    > Commit-Queue: Emircan Uysaler <emircan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#697683}
    
    TBR=sergeyu@chromium.org,emircan@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 982922, fuchsia:24570
    Change-Id: I7f52d2fbfdabc14de398df68409dfd6d07f1fc9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1825725
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699970}
    
    Former-commit-id: 325cce09364a602f8027eaee45ac7b7b06fc0415

--- fuchsia/engine/context_provider_impl.cc
+++ fuchsia/engine/context_provider_impl.cc
@@ -227,10 +227,6 @@ void ContextProviderImpl::Create(
                                      gl::kGLImplementationStubName);
   }
 
-  if (enable_protected_graphics) {
-    launch_command.AppendSwitch(switches::kEnforceVulkanProtectedMemory);
-  }
-
   if (enable_widevine) {
     launch_command.AppendSwitch(switches::kEnableWidevine);
   }
