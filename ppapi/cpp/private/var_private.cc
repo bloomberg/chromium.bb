310535549369c11b3c9033c1c6cf542750fe4c7b
Sebastien Lalancette <seblalancette@chromium.org>
2019-09-25 16:34:49 +0000

    Revert "Remove using directives ("using namespace x") from ppapi/, take 2."
    This reverts commit 2ca9e95a6770192973594d7ff95514e9b6f3fe62 [formerly 147e1d3baeef37665fb5a2ef81571c5506c1f19a].
    
    Reason for revert: compare_build_artifacts step is flaky on Windows deterministic builder.
    
    File that failed diff check: ppapi_nacl_tests_pnacl_newlib_x64.nexe
    
    (Linux looks good though.)
    
    Original change's description:
    > Remove using directives ("using namespace x") from ppapi/, take 2.
    > 
    > Testing to see if this route avoids breaking the Linux Deterministic bot.
    > 
    > TBR=bbudge
    > 
    > Bug: 82078
    > Change-Id: I1512141cc886dafa6683f5ff38c6a50f35be067a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1818998
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Commit-Queue: Peter Kasting <pkasting@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#698817}
    
    TBR=pkasting@chromium.org,bbudge@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1007979
    Change-Id: I6b24cc229ac48f5838b230b74d5022a34e1c6415
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1824673
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699825}
    
    Former-commit-id: c10ded6dd270cbeaaf048714617ae1dd07b8cbb2

--- ppapi/cpp/private/var_private.cc
+++ ppapi/cpp/private/var_private.cc
@@ -24,7 +24,7 @@ template <> const char* interface_name<PPB_Var_Deprecated>() {
 
 }  // namespace
 
-using deprecated::ScriptableObject;
+using namespace deprecated;
 
 VarPrivate::VarPrivate(const InstanceHandle& instance,
                        ScriptableObject* object) {
