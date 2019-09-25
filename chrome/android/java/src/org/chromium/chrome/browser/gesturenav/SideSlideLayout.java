0e7294d3ea041d908a49bc6fa142e4aba1a012c2
Jinsuk Kim <jinsukkim@chromium.org>
2019-09-25 23:50:20 +0000

    Android: Fix gesture navigation arrow offset bug w/ notch
    This CL fixes a bug that happens only on Pixel 3 XL in landscape mode.
    
    - Chrome does not use the large notch area around the top that goes to
      left side in landscape mode. This makes screen/view x coordinate
      different. The correct coordinate to use for touch event is view
      coordinate, not the screen one being used in OverscrollScroller.
    
    - SideSlideLayout calls getMeasuredWidth() at the beginning (right
      after being attached) to find correct position of the navigation
      bubble when swiped from right. It somehow still returns the width
      in portrait mode (== height in landscape) and causes the arrow to
      position somewhere in the middle of the screen. Got around this
      problem by using the width of the parent view instead. This makes
      some init logic simpler.
    
    (cherry picked from commit cb0cae9c8f3d6b1ca31d8bc9655b5e01053bdfd6 [formerly 4d5efd25edd427b1f0b0725219ed2ba5faa4a412])
    
    Bug: 999572
    Change-Id: I36ea8f56531686877a9cd5831570959e85788b67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786391
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#694135}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1826057
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3904@{#462}
    Cr-Branched-From: 9035e7d56a589711638d7ffdc83ffa6d674a06ec [formerly 675968a8c657a3bd9c1c2c20c5d2935577bbc5e6]-refs/heads/master@{#693954}
    
    Former-commit-id: efc32857d2fe26a980f29105e6c951c5fe50fa4b

--- chrome/android/java/src/org/chromium/chrome/browser/gesturenav/SideSlideLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/gesturenav/SideSlideLayout.java
@@ -82,9 +82,6 @@ public class SideSlideLayout extends ViewGroup {
     private int mCurrentTargetOffset;
     private float mTotalMotion;
 
-    // Whether or not the starting offset has been determined.
-    private boolean mOriginalOffsetCalculated;
-
     // True while side gesture is in progress.
     private boolean mIsBeingDragged;
 
@@ -246,14 +243,11 @@ public class SideSlideLayout extends ViewGroup {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         mArrowView.measure(MeasureSpec.makeMeasureSpec(mArrowViewWidth, MeasureSpec.EXACTLY),
                 MeasureSpec.makeMeasureSpec(mCircleWidth, MeasureSpec.EXACTLY));
-        if (!mOriginalOffsetCalculated) {
-            initializeOffset();
-            mOriginalOffsetCalculated = true;
-        }
     }
 
     private void initializeOffset() {
-        mCurrentTargetOffset = mOriginalOffset = mIsForward ? getMeasuredWidth() : -mArrowViewWidth;
+        int offset = mIsForward ? ((View) getParent()).getWidth() : -mArrowViewWidth;
+        mCurrentTargetOffset = mOriginalOffset = offset;
     }
 
     /**
