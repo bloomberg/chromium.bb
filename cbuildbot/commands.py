475a4c1d064054f53acaebef42e7823725a42e77
LaMont Jones <lamontjones@chromium.org>
2019-09-25 15:42:37 -0600

    cbuildbot/commands: fix typo
    BUG=chromium:928894
    TEST=verified.
    
    Change-Id: Icb0c3ed84ac08ff5b190510c1be2542d17b13acc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/1825947
    Reviewed-by: LaMont Jones <lamontjones@chromium.org>
    Tested-by: LaMont Jones <lamontjones@chromium.org>

--- cbuildbot/commands.py
+++ cbuildbot/commands.py
@@ -577,7 +577,7 @@ def RunCrosSigningTests(buildroot, network=False):
   test_runner = path_util.ToChrootPath(
       os.path.join(buildroot, 'src', 'platform', 'signing',
                    'signer-production', 'signer', 'run_tests.py'))
-  if not os.exists(test_runner):
+  if not os.path.exists(test_runner):
     test_runner = path_util.ToChrootPath(
         os.path.join(buildroot, 'cros-signing', 'signer', 'run_tests.py'))
   cmd = [test_runner]
