ed175c740c4506f5efa4f7d02c417718efbf9a7c
Allen Webb <allenwebb@google.com>
2019-09-25 10:36:34 -0700

    system_api: Add rust D-Bus bindings generation.
    BUG=chromium:1007474
    TEST=cargo build, sudo emerge system_api-rust crosh
    
    Change-Id: I9b7aa57fd3bdc04428325f42e90520dcda36633d
    Reviewed-on: https://chromium-review.googlesource.com/1825641
    Tested-by: Allen Webb <allenwebb@google.com>
    Commit-Ready: Allen Webb <allenwebb@google.com>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Stephen Barber <smbarber@chromium.org>
    Cr-Mirrored-From: https://chromium.googlesource.com/chromiumos/platform2
    Cr-Mirrored-Commit: 09093e934e0bc43c3e6d3cc23d63cd020ca42908

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Imports the Rust D-Bus bindings for system_api.
+
+include!(concat!(env!("OUT_DIR"), "/include_modules.rs"));
