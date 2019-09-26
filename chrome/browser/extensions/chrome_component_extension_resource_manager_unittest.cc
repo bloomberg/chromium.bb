b465f225bcbf56fef236bf10e6cc6da55c8910de
dpapad <dpapad@chromium.org>
2019-09-26 19:37:13 +0000

    Allow chrome-extension:// URL to serve generated component extension files.
    This is necessary to allow migrating the PDF viewer from Polymer2 to Polymer3,
    since the latter uses generated files.
    
    Added automated test. Can also verify manually as follows
    1) Visit chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai/elements/shared-vars.m.js
    2) Should see the contents of the generated file.
    
    Bug: 1005029
    Change-Id: I762672a0078f00b62500391f2d880d5e6432befe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1814840
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700383}
    
    Former-commit-id: b9d0dbf7bb00424f26956c615f211e78680b470f

--- chrome/browser/extensions/chrome_component_extension_resource_manager_unittest.cc
+++ chrome/browser/extensions/chrome_component_extension_resource_manager_unittest.cc
@@ -4,8 +4,11 @@
 
 #include "base/files/file_path.h"
 #include "base/path_service.h"
+#include "base/strings/string_util.h"
 #include "build/build_config.h"
 #include "chrome/common/chrome_paths.h"
+#include "chrome/grit/component_extension_resources.h"
+#include "chrome/grit/component_extension_resources_map.h"
 #include "extensions/browser/component_extension_resource_manager.h"
 #include "extensions/browser/extensions_browser_client.h"
 #include "extensions/common/constants.h"
@@ -70,6 +73,40 @@ TEST_F(ChromeComponentExtensionResourceManagerTest,
       extension->path(), resource.relative_path(), &resource_id));
   ASSERT_EQ(IDR_FILE_MANAGER_ICON_16, resource_id);
 #endif
+}
+
+TEST_F(ChromeComponentExtensionResourceManagerTest,
+       IsComponentExtensionResource_Generated) {
+  // Check that the file being used for testing is indeed a generated resource.
+  int generated_resource_id = IDR_PDF_SHARED_VARS_M_JS;
+  bool found_resource = false;
+  for (size_t i = 0; i < kComponentExtensionResourcesSize; ++i) {
+    if (kComponentExtensionResources[i].value == generated_resource_id) {
+      ASSERT_TRUE(base::StartsWith(kComponentExtensionResources[i].name,
+                                   "@out_folder@",
+                                   base::CompareCase::SENSITIVE));
+      found_resource = true;
+      break;
+    }
+  }
+  ASSERT_TRUE(found_resource);
+
+  const ComponentExtensionResourceManager* resource_manager =
+      ExtensionsBrowserClient::Get()->GetComponentExtensionResourceManager();
+  ASSERT_TRUE(resource_manager);
+
+  base::FilePath resources_dir;
+  base::PathService::Get(chrome::DIR_RESOURCES, &resources_dir);
+
+  base::FilePath extension_path = resources_dir.AppendASCII("pdf");
+  base::FilePath resource_path =
+      base::FilePath().AppendASCII("elements/shared-vars.m.js");
+
+  // Check that the resource is classified as a component resource.
+  int resource_id = 0;
+  ASSERT_TRUE(resource_manager->IsComponentExtensionResource(
+      extension_path, resource_path, &resource_id));
+  ASSERT_EQ(generated_resource_id, resource_id);
 }
 
 }  // namespace extensions
