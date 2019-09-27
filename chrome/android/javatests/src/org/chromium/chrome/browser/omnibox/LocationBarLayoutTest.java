2d271314fbd18b96961e5a5b7350138a28b29601
Tomasz Wiszkowski <ender@google.com>
2019-09-27 16:59:45 +0000

    Disable failing testOmniboxSearchEngineLogo_goneWhenIncognito test.
    TBR=twellington@chromium.org,wylieb@chromium.org
    
    Bug: 1008912
    Change-Id: Iefe74c0b6babac0cf6f01b0832e7a484fb776c1c
    Logs: https://chromium-swarm.appspot.com/task?id=478af97affe4db10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1829460
    Reviewed-by: Ender <ender@google.com>
    Commit-Queue: Ender <ender@google.com>
    Cr-Commit-Position: refs/heads/master@{#700759}
    
    Former-commit-id: 0510a6b59acf211083024e188607310207ea017e

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/LocationBarLayoutTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/LocationBarLayoutTest.java
@@ -21,6 +21,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.R;
@@ -241,6 +242,7 @@ public class LocationBarLayoutTest {
 
     @Test
     @SmallTest
+    @DisabledTest
     @EnableFeatures(ChromeFeatureList.OMNIBOX_SEARCH_ENGINE_LOGO)
     @Feature({"OmniboxSearchEngineLogo"})
     public void testOmniboxSearchEngineLogo_goneWhenIncognito() throws Exception {
