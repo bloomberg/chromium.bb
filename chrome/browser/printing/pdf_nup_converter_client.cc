b5d8f10fd3cb16bf2db8cfc4826ec8f33fd55dcf
Julie Jeongeun Kim <jkim@igalia.com>
2019-09-27 01:59:26 +0000

    Convert PdfNupConverter to new Mojo types
    This CL converts PdfNupConverterPtr to new Mojo types using Remote.
    
    Bug: 955171
    Change-Id: I04b12fac6e4195297930388d50f473c7c8908a84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1827260
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700544}
    
    Former-commit-id: 87f6d993749582f802eae1e00167b3fe7e28bfdf

--- chrome/browser/printing/pdf_nup_converter_client.cc
+++ chrome/browser/printing/pdf_nup_converter_client.cc
@@ -24,7 +24,7 @@ void PdfNupConverterClient::DoNupPdfConvert(
     const gfx::Rect& printable_area,
     std::vector<base::ReadOnlySharedMemoryRegion> pdf_page_regions,
     mojom::PdfNupConverter::NupPageConvertCallback callback) {
-  auto& nup_converter = GetPdfNupConverterRequest(document_cookie);
+  auto& nup_converter = GetPdfNupConverterRemote(document_cookie);
   nup_converter->NupPageConvert(pages_per_sheet, page_size, printable_area,
                                 std::move(pdf_page_regions),
                                 std::move(callback));
@@ -37,7 +37,7 @@ void PdfNupConverterClient::DoNupPdfDocumentConvert(
     const gfx::Rect& printable_area,
     base::ReadOnlySharedMemoryRegion src_pdf_document,
     mojom::PdfNupConverter::NupDocumentConvertCallback callback) {
-  auto& nup_converter = GetPdfNupConverterRequest(document_cookie);
+  auto& nup_converter = GetPdfNupConverterRemote(document_cookie);
   nup_converter->NupDocumentConvert(
       pages_per_sheet, page_size, printable_area, std::move(src_pdf_document),
       base::BindOnce(&PdfNupConverterClient::OnDidNupPdfDocumentConvert,
@@ -50,12 +50,12 @@ void PdfNupConverterClient::OnDidNupPdfDocumentConvert(
     mojom::PdfNupConverter::NupDocumentConvertCallback callback,
     mojom::PdfNupConverter::Status status,
     base::ReadOnlySharedMemoryRegion region) {
-  RemovePdfNupConverterRequest(document_cookie);
+  RemovePdfNupConverterRemote(document_cookie);
   std::move(callback).Run(status, std::move(region));
 }
 
-mojom::PdfNupConverterPtr& PdfNupConverterClient::GetPdfNupConverterRequest(
-    int cookie) {
+mojo::Remote<mojom::PdfNupConverter>&
+PdfNupConverterClient::GetPdfNupConverterRemote(int cookie) {
   auto iter = pdf_nup_converter_map_.find(cookie);
   if (iter != pdf_nup_converter_map_.end()) {
     DCHECK(iter->second.is_bound());
@@ -63,21 +63,21 @@ mojom::PdfNupConverterPtr& PdfNupConverterClient::GetPdfNupConverterRequest(
   }
 
   auto iterator =
-      pdf_nup_converter_map_.emplace(cookie, CreatePdfNupConverterRequest())
+      pdf_nup_converter_map_.emplace(cookie, CreatePdfNupConverterRemote())
           .first;
   return iterator->second;
 }
 
-void PdfNupConverterClient::RemovePdfNupConverterRequest(int cookie) {
+void PdfNupConverterClient::RemovePdfNupConverterRemote(int cookie) {
   size_t erased = pdf_nup_converter_map_.erase(cookie);
   DCHECK_EQ(erased, 1u);
 }
 
-mojom::PdfNupConverterPtr
-PdfNupConverterClient::CreatePdfNupConverterRequest() {
-  mojom::PdfNupConverterPtr pdf_nup_converter;
+mojo::Remote<mojom::PdfNupConverter>
+PdfNupConverterClient::CreatePdfNupConverterRemote() {
+  mojo::Remote<mojom::PdfNupConverter> pdf_nup_converter;
   GetPrintingService()->BindPdfNupConverter(
-      mojo::MakeRequest(&pdf_nup_converter));
+      pdf_nup_converter.BindNewPipeAndPassReceiver());
   pdf_nup_converter->SetWebContentsURL(web_contents_->GetLastCommittedURL());
   return pdf_nup_converter;
 }
