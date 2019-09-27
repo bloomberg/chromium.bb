f066f28eaa300f583a73dac2b8d70b3e6a7cbf12
Patti <patricialor@chromium.org>
2019-09-27 07:19:46 +0000

    Revert "Add drop shadow support for popups on Windows."
    This reverts commit 335f2f2cb0ed22879be2523e504fe7f13dd3f5a8 [formerly f8ead841edf2feaa48e2021e1a28a990d8526b99].
    
    Reason for revert: Causing compile errors in win-jumbo-rel builder. See crbug.com/1008725.
    
    Original change's description:
    > Add drop shadow support for popups on Windows.
    > 
    > This CL adds drop shadow support for popups on Windows when
    > the FormControlsRefresh feature is enabled.
    > 
    > In order to add the drop shadow, the Window used for popups (TYPE_MENU)
    > is decorated with the WS_THICKFRAME style. Since WS_THICKFRAME also
    > provides a frame, the remove_standard_frame flag is set as we are interested
    > in keeping just the drop shadow.
    > 
    > Since drop shadow is not supported on Windows versions without DWM composition,
    > then the drop shadow is replaced by a thin-line border by using the
    > WS_BORDER style.
    > 
    > Bug: 1000898
    > Change-Id: I04588d8d1703cb98f4b7a239f9cf46d742324a1c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786247
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Mason Freed <masonfreed@chromium.org>
    > Commit-Queue: Ionel Popescu <iopopesc@microsoft.com>
    > Cr-Commit-Position: refs/heads/master@{#695655}
    
    TBR=sky@chromium.org,tkent@chromium.org,masonfreed@chromium.org,iopopesc@microsoft.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1000898, 1008725
    Change-Id: I938e15242e6cab5423305db75596c17f2c5812f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1826753
    Reviewed-by: Patti <patricialor@chromium.org>
    Commit-Queue: Patti <patricialor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700607}
    
    Former-commit-id: 3f599b400a5b14a54c1b75b6df91518a22986e5f

--- ui/views/widget/widget_hwnd_utils.cc
+++ ui/views/widget/widget_hwnd_utils.cc
@@ -9,7 +9,6 @@
 #include "base/command_line.h"
 #include "build/build_config.h"
 #include "ui/base/l10n/l10n_util_win.h"
-#include "ui/base/ui_base_features.h"
 #include "ui/base/ui_base_switches.h"
 #include "ui/views/widget/widget_delegate.h"
 #include "ui/views/win/hwnd_message_handler.h"
@@ -109,15 +108,6 @@ void CalculateWindowStylesFromInitParams(
       break;
     case Widget::InitParams::TYPE_MENU:
       *style |= WS_POPUP;
-      if (features::IsFormControlsRefreshEnabled() &&
-          params.remove_standard_frame) {
-        // If the platform doesn't support drop shadow, decorate the Window
-        // with just a border.
-        if (ui::win::IsAeroGlassEnabled())
-          *style |= WS_THICKFRAME;
-        else
-          *style |= WS_BORDER;
-      }
       break;
     case Widget::InitParams::TYPE_TOOLTIP:
       *style |= WS_POPUP;
