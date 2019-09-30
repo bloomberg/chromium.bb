04e1b1a58daeb6247c057ec6f288cab344650206
Raul Tambre <raul@tambre.ee>
2019-09-30 16:42:27 +0000

    tools/json_schema_compiler/feature_compiler.py: Fix use of 'is' for comparison
    [290/24365] ACTION //chrome/common/extensions/api:manifest_features_json_features(//build/toolchain/win:win_clang_x64)
    ../../tools/json_schema_compiler/feature_compiler.py:681: SyntaxWarning: "is" with a literal. Did you mean "=="?
      if sep is -1 or no_parent:
    
    Bug: 958874
    Change-Id: I7114523a95c3300a0a077732868f523d666fd826
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1829207
    Auto-Submit: Raul Tambre <raul@tambre.ee>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701147}
    
    Former-commit-id: 4f5df588b4fca4d07578bbc842be46c1aef3fc3a

--- tools/json_schema_compiler/feature_compiler.py
+++ tools/json_schema_compiler/feature_compiler.py
@@ -678,7 +678,7 @@ class FeatureCompiler(object):
     else:
       no_parent = 'noparent' in feature_value
     sep = feature_name.rfind('.')
-    if sep is -1 or no_parent:
+    if sep == -1 or no_parent:
       return None
 
     parent_name = feature_name[:sep]
