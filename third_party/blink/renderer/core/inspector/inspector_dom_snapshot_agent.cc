c342ce5fad9a0d3f013a362f53ba4d832bea2f45
Erik Hendriks <hendriks@chromium.org>
2019-09-30 22:52:24 +0000

    Add title and document content width/height to DOMSnapshot.captureSnapshot results.
    Add a test to see that misc details are showing up ok. Also, make
    stabilization of the string table in the pseudo element test more
    robust.
    
    Change-Id: I3e5500c8095f41e4dc15ce3d6181c9fa8c5d33a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1827180
    Commit-Queue: Erik Hendriks <hendriks@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701328}
    
    Former-commit-id: a4406936510af7a0612608d91b636478ad3bf2c8

--- third_party/blink/renderer/core/inspector/inspector_dom_snapshot_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_dom_snapshot_agent.cc
@@ -324,6 +324,7 @@ void InspectorDOMSnapshotAgent::VisitDocument(Document* document) {
               AddString(InspectorDOMAgent::DocumentURLString(document)))
           .setBaseURL(
               AddString(InspectorDOMAgent::DocumentBaseURLString(document)))
+          .setTitle(AddString(document->title()))
           .setContentLanguage(AddString(document->ContentLanguage()))
           .setEncodingName(AddString(document->EncodingName()))
           .setFrameId(
@@ -373,6 +374,9 @@ void InspectorDOMSnapshotAgent::VisitDocument(Document* document) {
     auto offset = document->View()->LayoutViewport()->GetScrollOffset();
     document_->setScrollOffsetX(offset.Width());
     document_->setScrollOffsetY(offset.Height());
+    auto contents_size = document->View()->LayoutViewport()->ContentsSize();
+    document_->setContentWidth(contents_size.Width());
+    document_->setContentHeight(contents_size.Height());
   }
 
   if (paint_order_map_) {
--- third_party/blink/web_tests/inspector-protocol/dom-snapshot/dom-snapshot-captureSnapshot-pseudo-element.js
+++ third_party/blink/web_tests/inspector-protocol/dom-snapshot/dom-snapshot-captureSnapshot-pseudo-element.js
@@ -1,21 +1,17 @@
 (async function(testRunner) {
   var {page, session, dp} = await testRunner.startURL('../resources/dom-snapshot-pseudo-element.html', 'Tests DOMSnapshot.getSnapshot exports layout tree nodes associated with pseudo elements.');
 
-  function stabilize(key, value) {
-    var unstableKeys = ['documentURL', 'baseURL', 'frameId', 'backendNodeId'];
-    if (unstableKeys.indexOf(key) !== -1)
-      return '<' + typeof(value) + '>';
-    return value;
-  }
-
   var response = await dp.DOMSnapshot.captureSnapshot({'computedStyles': ['font-weight', 'color'], 'includeEventListeners': true});
   if (response.error) {
     testRunner.log(response);
   } else {
-    response.result.strings[0] = '';
-    response.result.strings[1] = '';
-    response.result.strings[2] = '';
-    testRunner.log(JSON.stringify(response.result, stabilize, 2));
+    // Remove unstable strings from the string table.
+    response.result.strings[response.result.documents[0].documentURL] = '';
+    response.result.strings[response.result.documents[0].baseURL] = '';
+    response.result.strings[response.result.documents[0].frameId] = '';
+    testRunner.log(
+        response.result, undefined,
+        ['documentURL', 'baseURL', 'frameId', 'backendNodeId']);
   }
   testRunner.completeTest();
 })
