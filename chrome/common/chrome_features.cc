b03fd3114b5666344501606cb762287e531e9a52
Zentaro Kavanagh <zentaro@chromium.org>
2019-10-01 20:57:35 +0000

    Mouse acceleration: Show UI to disable it
    - Enables the flag that shows the UI for mouse acceleration
    
    BUG=chromium:589190
    TEST=visual confirmation
    
    Change-Id: I75de3655b559f99729b81d210766b6e7c71eca96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832285
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701754}
    
    Former-commit-id: e2fa187e3f0ef9904f0504ae2b18a9725e771aad

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -177,7 +177,7 @@ const base::Feature kImmersiveFullscreen{"ImmersiveFullscreen",
 #if defined(OS_CHROMEOS)
 // Shows a setting that allows disabling mouse acceleration.
 const base::Feature kAllowDisableMouseAcceleration{
-    "AllowDisableMouseAcceleration", base::FEATURE_DISABLED_BY_DEFAULT};
+    "AllowDisableMouseAcceleration", base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Enable project Crostini, Linux VMs on Chrome OS.
 const base::Feature kCrostini{"Crostini", base::FEATURE_DISABLED_BY_DEFAULT};
