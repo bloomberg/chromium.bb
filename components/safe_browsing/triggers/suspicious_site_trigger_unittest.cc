b416d745fb50c61889b63dc981daba2e2ee6a55e
Xida Chen <xidachen@chromium.org>
2019-10-01 14:58:58 +0000

    Disable SuspiciousSiteTriggerTest.SuspiciousHitDuringLoad
    TBR=hbos@chromium.org
    NOTRY=true
    
    Bug: 1010037
    Change-Id: I96c92b2adf04e076d8785521610102988c17ddd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832691
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701573}
    
    Former-commit-id: 85fe334ff778faa90e5afb010963db663858c4b5

--- components/safe_browsing/triggers/suspicious_site_trigger_unittest.cc
+++ components/safe_browsing/triggers/suspicious_site_trigger_unittest.cc
@@ -8,6 +8,7 @@
 
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/test_simple_task_runner.h"
+#include "build/build_config.h"
 #include "components/prefs/testing_pref_service.h"
 #include "components/safe_browsing/common/safe_browsing_prefs.h"
 #include "components/safe_browsing/triggers/mock_trigger_manager.h"
@@ -191,7 +192,13 @@ TEST_F(SuspiciousSiteTriggerTest, RegularPageNonSuspicious) {
   ExpectNoReportRejection();
 }
 
-TEST_F(SuspiciousSiteTriggerTest, SuspiciousHitDuringLoad) {
+// crbug.com/1010037: fails on win.
+#if defined(OS_WIN)
+#define MAYBE_SuspiciousHitDuringLoad DISABLED_SuspiciousHitDuringLoad
+#else
+#define MAYBE_SuspiciousHitDuringLoad SuspiciousHitDuringLoad
+#endif
+TEST_F(SuspiciousSiteTriggerTest, MAYBE_SuspiciousHitDuringLoad) {
   // When a suspicious site is detected in the middle of a page load, a report
   // is created after the page load has finished.
   CreateTrigger(/*monitor_mode=*/false);
