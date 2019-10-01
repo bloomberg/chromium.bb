33cd67fec657317f0b9ad9ba6fbb81c0c69ecde2
Mario Sanchez Prada <mario@igalia.com>
2019-10-01 20:32:57 +0000

    Remove InterfaceRegistry & InterfaceProvider's methods for old Mojo types
    We no longer need to have these templated methods declared as we can now
    rely on the new Mojo types, so let's drop them to reduce the amount of
    references to the old types inside Blink.
    
    Bug: 955171, 978694
    Change-Id: I7cfc4987ae41916b678e071e13e892f52b014913
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832205
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701739}
    
    Former-commit-id: 02badc14b859638f05804b36365e6bc0b1e9d47c

--- third_party/blink/public/platform/interface_registry.h
+++ third_party/blink/public/platform/interface_registry.h
@@ -14,8 +14,6 @@
 #include "third_party/blink/public/platform/web_common.h"
 
 #if INSIDE_BLINK
-#include "mojo/public/cpp/bindings/associated_interface_request.h"
-#include "mojo/public/cpp/bindings/interface_request.h"
 #include "mojo/public/cpp/bindings/pending_associated_receiver.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "third_party/blink/renderer/platform/wtf/cross_thread_functional.h"  // nogncheck
@@ -49,35 +47,12 @@ class BLINK_PLATFORM_EXPORT InterfaceRegistry {
 #if INSIDE_BLINK
   template <typename Interface>
   void AddInterface(
-      base::RepeatingCallback<void(mojo::InterfaceRequest<Interface>)>
-          factory) {
-    AddInterface(
-        Interface::Name_,
-        WTF::BindRepeating(&InterfaceRegistry::ForwardToInterfaceFactory<
-                               mojo::InterfaceRequest<Interface>>,
-                           std::move(factory)));
-  }
-
-  template <typename Interface>
-  void AddInterface(
       base::RepeatingCallback<void(mojo::PendingReceiver<Interface>)> factory) {
     AddInterface(
         Interface::Name_,
         WTF::BindRepeating(&InterfaceRegistry::ForwardToInterfaceFactory<
                                mojo::PendingReceiver<Interface>>,
                            std::move(factory)));
-  }
-
-  template <typename Interface>
-  void AddInterface(WTF::CrossThreadRepeatingFunction<
-                        void(mojo::InterfaceRequest<Interface>)> factory,
-                    scoped_refptr<base::SingleThreadTaskRunner> task_runner) {
-    AddInterface(Interface::Name_,
-                 ConvertToBaseCallback(CrossThreadBind(
-                     &InterfaceRegistry::ForwardToCrossThreadInterfaceFactory<
-                         mojo::InterfaceRequest<Interface>>,
-                     std::move(factory))),
-                 std::move(task_runner));
   }
 
   template <typename Interface>
@@ -90,18 +65,6 @@ class BLINK_PLATFORM_EXPORT InterfaceRegistry {
                          mojo::PendingReceiver<Interface>>,
                      std::move(factory))),
                  std::move(task_runner));
-  }
-
-  template <typename Interface>
-  void AddAssociatedInterface(
-      base::RepeatingCallback<void(mojo::AssociatedInterfaceRequest<Interface>)>
-          factory) {
-    AddAssociatedInterface(
-        Interface::Name_,
-        WTF::BindRepeating(
-            &InterfaceRegistry::ForwardToAssociatedInterfaceFactory<
-                mojo::AssociatedInterfaceRequest<Interface>>,
-            std::move(factory)));
   }
 
   template <typename Interface>
