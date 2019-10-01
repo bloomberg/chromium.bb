e6a8c0a2960554744acc9fa3f760efe669dbe043
rbpotter <rbpotter@chromium.org>
2019-10-01 00:22:58 +0000

    Extensions Web UI: Split file to break circular dependency
    Split out KeyboardShortcutDelegate into a separate file to break the
    following circular dependency, created when we migrate to Polymer 3:
    
    - shortcut_input.* depends on the KeyboardShortcutDelegate class from
    keyboard_shortcuts.*
    - keyboard_shortcuts.* imports shortcut_input.*, since it uses
    shortcut-input.
    
    Bug: 1004967
    Change-Id: I9b75267eddea8eb4555b2080a90ba7e8ffca0a6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832792
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701372}
    
    Former-commit-id: 384bf5d94a14e324a4cf497e94407c2297689505

--- chrome/browser/resources/extensions/keyboard_shortcuts.js
+++ chrome/browser/resources/extensions/keyboard_shortcuts.js
@@ -5,36 +5,6 @@
 cr.define('extensions', function() {
   'use strict';
 
-  /** @interface */
-  class KeyboardShortcutDelegate {
-    /**
-     * Called when shortcut capturing changes in order to suspend or re-enable
-     * global shortcut handling. This is important so that the shortcuts aren't
-     * processed normally as the user types them.
-     * TODO(devlin): From very brief experimentation, it looks like preventing
-     * the default handling on the event also does this. Investigate more in the
-     * future.
-     * @param {boolean} isCapturing
-     */
-    setShortcutHandlingSuspended(isCapturing) {}
-
-    /**
-     * Updates an extension command's keybinding.
-     * @param {string} extensionId
-     * @param {string} commandName
-     * @param {string} keybinding
-     */
-    updateExtensionCommandKeybinding(extensionId, commandName, keybinding) {}
-
-    /**
-     * Updates an extension command's scope.
-     * @param {string} extensionId
-     * @param {string} commandName
-     * @param {chrome.developerPrivate.CommandScope} scope
-     */
-    updateExtensionCommandScope(extensionId, commandName, scope) {}
-  }
-
   // The UI to display and manage keyboard shortcuts set for extension commands.
   const KeyboardShortcuts = Polymer({
     is: 'extensions-keyboard-shortcuts',
@@ -127,7 +97,6 @@ cr.define('extensions', function() {
   });
 
   return {
-    KeyboardShortcutDelegate: KeyboardShortcutDelegate,
     KeyboardShortcuts: KeyboardShortcuts,
   };
 });
