93df9cb23a612649a90c6b88b350405c0ea64bbc
Kent Tamura <tkent@chromium.org>
2019-10-02 09:18:23 +0000

    std-switch: Implement a way to toggle a platform-agnostic theme and a platform-matching theme
    This CL implements the option F in [1].
    Add Material-like theme as a platform-matching theme.
    
    [1] https://github.com/tkent-google/std-switch/issues/6#issuecomment-499842423
    
    Bug: 972476
    Change-Id: I643d36b0a4750191d45e7e6cfe2ae320826e3f48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832128
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701952}
    
    Former-commit-id: 6461c864f743d1e85ecb956d704c81bfa7ec23c7

--- third_party/blink/renderer/core/script/resources/layered_api/elements/switch/style.mjs
+++ third_party/blink/renderer/core/script/resources/layered_api/elements/switch/style.mjs
@@ -11,6 +11,10 @@ const THUMB_WIDTH = '22px';
 const THUMB_MARGIN_START = '2px';
 const THUMB_MARGIN_END = '2px';
 
+const MATERIAL_THUMB_SIZE = '20px';
+const RIPPLE_COLOR = 'rgba(100,100,100,0.3)';
+const RIPPLE_MAX_SIZE = '48px';
+
 // Returns a function returning a CSSStyleSheet().
 // TODO(tkent): Share this stylesheet factory feature with elements/toast/.
 export function styleSheetFactory() {
@@ -143,6 +147,122 @@ export function styleSheetFactory() {
  */
 :host([hidden]) {
   display: none;
+}
+
+`);
+    }
+    return styleSheet;
+  };
+}
+
+export function materialStyleSheetFactory() {
+  let styleSheet;
+  return () => {
+    if (!styleSheet) {
+      styleSheet = new CSSStyleSheet();
+      styleSheet.replaceSync(`
+:host {
+  block-size: 20px;
+  inline-size: 36px;
+}
+
+#track,
+:host(:active) #track {
+  background: rgba(0,0,0,0.4);
+  block-size: 14px;
+  border: none;
+  box-shadow: none;
+}
+
+:host([on]) #track,
+:host([on]:active) #track,
+:host([on]:focus) #track {
+  border: none;
+  box-shadow: none;
+}
+
+#trackFill,
+:host([on]:active) #trackFill {
+  background: rgba(63,81,181,0.5);
+}
+
+#thumb,
+:host(:focus) #thumb {
+  block-size: ${MATERIAL_THUMB_SIZE};
+  border-radius: calc(${MATERIAL_THUMB_SIZE} / 2);
+  border: none;
+  box-shadow: 0 1px 5px 0 rgba(0,0,0,0.6);
+  inline-size: ${MATERIAL_THUMB_SIZE};
+  margin-inline-start: -100%;
+}
+
+:host([on]) #thumb,
+:host([on]:focus) #thumb,
+:host([on]:not(:disabled):hover) #thumb {
+  background: rgb(63,81,181);
+  border: none;
+  margin-inline-start: calc(0px - 20px);
+}
+
+:host(:not(:disabled):hover) #thumb {
+  inline-size: ${MATERIAL_THUMB_SIZE};
+}
+
+/*
+ * Ripple effect
+ *
+ * Translucent circle is painted on the thumb if the element is :active or
+ * :focus-visible.  It has
+ *   - Size transition when it appears
+ *   - Opacity transition when it disappears
+ * part(thumb)::before represents the former, and part(thumb)::after represents
+ * the latter.
+ */
+#thumb::before {
+  background: ${RIPPLE_COLOR};
+  block-size: 0px;
+  border-radius: 0px;
+  content: "";
+  display: inline-block;
+  inline-size: 0px;
+  left: calc(${MATERIAL_THUMB_SIZE} / 2);
+  position: relative;
+  top: calc(${MATERIAL_THUMB_SIZE} / 2);
+  transition: none;
+}
+
+:host(:active) #thumb::before,
+:host(:focus-visible) #thumb::before {
+  block-size: ${RIPPLE_MAX_SIZE};
+  border-radius: calc(${RIPPLE_MAX_SIZE} / 2);
+  inline-size: ${RIPPLE_MAX_SIZE};
+  left: calc((${MATERIAL_THUMB_SIZE} - ${RIPPLE_MAX_SIZE}) / 2);
+  top: calc((${MATERIAL_THUMB_SIZE} - ${RIPPLE_MAX_SIZE}) / 2);
+  transition: all linear 0.1s;
+}
+
+#thumb::after {
+  background: ${RIPPLE_COLOR};
+  block-size: ${RIPPLE_MAX_SIZE};
+  border-radius: calc(${RIPPLE_MAX_SIZE} / 2);
+  content: "";
+  display: inline-block;
+  inline-size: ${RIPPLE_MAX_SIZE};
+  left: calc((${MATERIAL_THUMB_SIZE} - ${RIPPLE_MAX_SIZE}) / 2);
+  opacity: 0;
+  position: relative;
+  /* Why 18px? */
+  top: calc((${MATERIAL_THUMB_SIZE} - ${RIPPLE_MAX_SIZE}) / 2 - 18px);
+  transition: opacity linear 0.3s;
+}
+
+:host(:active) #thumb::after,
+:host(:focus-visible) #thumb::after {
+  block-size: 0px;
+  content: "";
+  inline-size: 0px;
+  opacity: 1;
+  transition: none;
 }
 
 `);
