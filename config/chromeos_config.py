880a3f812b033051094c5b1a65016cde0ee561be
Amin Hassani <ahassani@chromium.org>
2019-10-02 23:28:28 +0000

    Revert "chromeos_config: Mark lakitu paladin builders experimental"
    This reverts commit 773f0cf12b52d60c500941f1613ba8d6baada358.
    
    Reason for revert: <builders are green>
    
    Original change's description:
    > chromeos_config: Mark lakitu paladin builders experimental
    > 
    > BUG=chromium:1010132
    > TEST=None
    > 
    > Change-Id: I5dd85fa702cd45e179f392b6da71a3e79dcd117f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/1834102
    > Tested-by: Amin Hassani <ahassani@chromium.org>
    > Auto-Submit: Amin Hassani <ahassani@chromium.org>
    > Reviewed-by: Jason Clinton <jclinton@chromium.org>
    > Commit-Queue: Jason Clinton <jclinton@chromium.org>
    
    Bug: chromium:1010132
    Change-Id: Icf1c0f9dc7c160914707238e50d7e7ca916bae6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/1836537
    Reviewed-by: Amin Hassani <ahassani@chromium.org>
    Reviewed-by: Jason Clinton <jclinton@chromium.org>
    Tested-by: Amin Hassani <ahassani@chromium.org>
    Commit-Queue: Amin Hassani <ahassani@chromium.org>

--- config/chromeos_config.py
+++ config/chromeos_config.py
@@ -2280,6 +2280,9 @@ def CqBuilders(site_config, boards_dict, ge_build_config):
       'kevin64',
       'kip',
       'kukui',
+      'lakitu',
+      'lakitu-gpu',
+      'lakitu-st',
       'lars',
       'lasilla-ground',
       'littlejoe',
@@ -2372,9 +2375,6 @@ def CqBuilders(site_config, boards_dict, ge_build_config):
   _paladin_experimental_boards = _paladin_new_boards | frozenset([
       'auron_paine', # crbug.com/1008410
       'fizz-moblab', # crbug.com/995170
-      'lakitu', # crbug.com/1010132
-      'lakitu-gpu', # crbug.com/1010132
-      'lakitu-st', # crbug.com/1010132
       'mistral', # b/140536001
       'moblab-generic-vm', # crbug.com/960998, crbug.com/976297
       'samus-kernelnext', # https://crbug.com/1004618
