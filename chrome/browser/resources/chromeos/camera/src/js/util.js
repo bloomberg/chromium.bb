7d601f5e6a5fd2e1698dd0303f3cc8127eee2173
Kuo Jen Wei <inker@google.com>
2019-10-02 02:28:05 +0000

    [CCA] Fix logic of judging isWindowFullSize.
    Consider shelf on left/right or top/bottom side, either width or
    height of window outer bound equal screen size should be fullscreen.
    
    Bug: 1008347
    Test: Cases of shelf on left/right or bottom, window resizing related
    function works as expected.
    
    (cherry picked from commit 6ac9ea0eb05323eab2d720c05980bce7596dddfc [formerly 17e710e32158cd1ee58efe1fe9b7645cf597e049])
    
    Change-Id: Id381bac95c9287e3febfdebb6ac74333283f7071
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1828784
    Auto-Submit: Kuo Jen Wei <inker@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Shik Chen <shik@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#700670}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1833169
    Cr-Commit-Position: refs/branch-heads/3904@{#573}
    Cr-Branched-From: 9035e7d56a589711638d7ffdc83ffa6d674a06ec [formerly 675968a8c657a3bd9c1c2c20c5d2935577bbc5e6]-refs/heads/master@{#693954}
    
    Former-commit-id: 32c13b06ab47c7865511e6379cc7d32134643671

--- chrome/browser/resources/chromeos/camera/src/js/util.js
+++ chrome/browser/resources/chromeos/camera/src/js/util.js
@@ -857,7 +857,8 @@ cca.util.isWindowFullSize = function() {
   // App-window's isFullscreen, isMaximized state and window's outer-size may
   // not be updated immediately during resizing. Use if app-window's outerBounds
   // width matches screen width here as workarounds.
-  return chrome.app.window.current().outerBounds.width >= screen.width;
+  return chrome.app.window.current().outerBounds.width >= screen.width ||
+      chrome.app.window.current().outerBounds.height >= screen.height;
 };
 
 /**
