adcf04873ca235a81f8ab489de8686457bff2300
Chandani Shrestha <chshrest@microsoft.com>
2019-10-02 17:26:16 +0000

    Devtools: Fix regex to show error for inputs which expects number
    Before this change, latitude and longitude inputs in Geolocations and pixel ratio in Devices Settings which expects a number wasn't throwing error when invalid input was entered.
    E.g. An input starting with digits and ending with any non-digit character like 1.1a.
    This change uses Number.isNaN for validation instead of regex. Using this method is better instead of regex considering localization and also fixes the current bug.
    
    Before: !/^[\d]+(\.\d+)?|\.\d+$/.test(value)
    After:
            parsedValue = Number(value.trim())
            Number.isNaN(parsedValue);
    
    Gif before the change: https://imgur.com/sFOBWfd
    Gif after the change: https://imgur.com/BiUll5C
    
    Bug: 990451
    Change-Id: Id41ccee141ff36257505e451b7f44b950cb833a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1776594
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Chandani Shrestha <chshrest@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#702081}
    
    Former-commit-id: b585a36804ab12a14232fe970392476aab5fc60d

--- third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/settings/geolocations-a11y-test.js
+++ third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/settings/geolocations-a11y-test.js
@@ -24,10 +24,27 @@
     const locationsEditor = geolocationsWidget._list._editor;
     const newLocationInputs = locationsEditor._controls;
     const nameInput = newLocationInputs[0];
+    const latitudeInput = newLocationInputs[1];
+    const longitudeInput = newLocationInputs[2];
+    let errorMessage;
+
     TestRunner.addResult(`Invalidating the ${nameInput.getAttribute('aria-label')} input`);
     nameInput.blur();
+    errorMessage = locationsEditor._errorMessageContainer.textContent;
+    TestRunner.addResult(`Error message: ${errorMessage}`);
 
-    const errorMessage = locationsEditor._errorMessageContainer.textContent;
+    TestRunner.addResult(`Invalidating the ${latitudeInput.getAttribute('aria-label')} input`);
+    nameInput.value = 'location';
+    latitudeInput.value = 'a.a';
+    latitudeInput.dispatchEvent(new Event('input'));
+    errorMessage = locationsEditor._errorMessageContainer.textContent;
+    TestRunner.addResult(`Error message: ${errorMessage}`);
+
+    TestRunner.addResult(`Invalidating the ${longitudeInput.getAttribute('aria-label')} input`);
+    latitudeInput.value = '1.1';
+    longitudeInput.value = '1a.1';
+    longitudeInput.dispatchEvent(new Event('input'));
+    errorMessage = locationsEditor._errorMessageContainer.textContent;
     TestRunner.addResult(`Error message: ${errorMessage}`);
 
     await AxeCoreTestRunner.runValidation(geolocationsWidget.contentElement);
