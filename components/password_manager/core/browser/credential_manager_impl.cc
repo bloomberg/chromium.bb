fd5faee0ef6a8e13c77daf0632dc04b7df588406
Justin Cohen <justincohen@chromium.org>
2019-10-03 15:29:11 +0000

    Revert "[iOS] Make the leak detection backend available on iOS"
    This reverts commit 2571324ddfce95da0bf2bef37ec07ba0d624f091 [formerly 74640a8991aeed70928fef6c25df738127855bef].
    
    Reason for revert: Indirectly broke official builds. See
    crbug.com/1010930.
    
    Original change's description:
    > [iOS] Make the leak detection backend available on iOS
    > 
    > Bug: 986317
    > 
    > Change-Id: I9d799cd24551f13bc58eac91c843d2da19aa7851
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832267
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#702059}
    
    TBR=vasilii@chromium.org,ioanap@chromium.org
    
    Change-Id: I0ea2f9c848cbcc4f60e10351f47fd317d4bca6ae
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 986317
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1836994
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702419}
    
    Former-commit-id: a246ac7a0022953db3aea4c67695925a95bb974c

--- components/password_manager/core/browser/credential_manager_impl.cc
+++ components/password_manager/core/browser/credential_manager_impl.cc
@@ -24,7 +24,12 @@ void RunGetCallback(GetCallback callback, const CredentialInfo& info) {
 }  // namespace
 
 CredentialManagerImpl::CredentialManagerImpl(PasswordManagerClient* client)
-    : client_(client), leak_delegate_(client) {
+    : client_(client)
+#if !defined(OS_IOS)
+      ,
+      leak_delegate_(client)
+#endif  // !defined(OS_IOS)
+{
   auto_signin_enabled_.Init(prefs::kCredentialsEnableAutosignin,
                             client_->GetPrefs());
 }
@@ -54,8 +59,10 @@ void CredentialManagerImpl::Store(const CredentialInfo& credential,
       CreatePasswordFormFromCredentialInfo(credential, origin));
 
   // Check whether a stored password credential was leaked.
+#if !defined(OS_IOS)
   if (credential.type == CredentialType::CREDENTIAL_TYPE_PASSWORD)
     leak_delegate_.StartLeakCheck(*form);
+#endif
 
   std::string signon_realm = origin.GetOrigin().spec();
   PasswordStore::FormDigest observed_digest(
