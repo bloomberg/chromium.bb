76037ed383b2e91239e3f6af4fbf1331612a1b7f
Samuel Huang <huangs@chromium.org>
2019-10-04 05:14:07 +0000

    [DevUI DFM] Associate all chosen WebUI hosts with the DFM.
    This CL performs a "horizontal" update to IsWebUiHostInDevUiDfm(), so
    that all WebUI hosts chosen to be in the DevUI DFM become affected by
    its flow. Specifically (on Android):
    * Accessing the (chrome://) URLs of these hosts triggers DevUI DFM
      install and/or load.
    * These hosts are not accessible unless the DevUI DFM is installed.
    
    The updates here are quite mechanical, so it makes sense to do them
    all at once (at least in draft), thereby reducing churn and edit /
    review burden.
    
    Hereafter, we can focus on moving resources from resources.pak to
    dev_ui_page_resources.pak.
    
    Bug: 927131
    Change-Id: I9fc1471e84bb1219eaafad6e751b90cc425a1b23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1834426
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702735}
    
    Former-commit-id: aa4ea572e014ea5d2eee946a439100800d561f4a

--- chrome/browser/android/dev_ui/dev_ui_url_handler.cc
+++ chrome/browser/android/dev_ui/dev_ui_url_handler.cc
@@ -8,22 +8,71 @@
 
 #include "chrome/browser/android/dev_ui/dev_ui_module_provider.h"
 #include "chrome/common/webui_url_constants.h"
+#include "components/safe_browsing/web_ui/constants.h"
+#include "content/public/common/url_constants.h"
 #include "net/base/url_util.h"
 #include "url/gurl.h"
-
-namespace chrome {
-namespace android {
 
 namespace {
 
 // Decides whether WebUI |host| (assumed for chrome://) is in the DevUI DFM.
 bool IsWebUiHostInDevUiDfm(const std::string& host) {
-  // Each WebUI host in the DevUI DFM must have an entry here.
-  return host == kChromeUIBluetoothInternalsHost ||
-         host == kChromeUIUsbInternalsHost;
+  // Each WebUI host (including synonyms) in the DevUI DFM must have an entry.
+  // Assume linear search is fast enough. Can optimize later if needed.
+  return host == chrome::kChromeUIAccessibilityHost ||
+         host == chrome::kChromeUIAutofillInternalsHost ||
+         host == chrome::kChromeUIBluetoothInternalsHost ||
+         host == chrome::kChromeUIComponentsHost ||
+         host == chrome::kChromeUICrashesHost ||
+         host == chrome::kChromeUIDeviceLogHost ||
+         host == chrome::kChromeUIDomainReliabilityInternalsHost ||
+         host == chrome::kChromeUIDownloadInternalsHost ||
+         host == chrome::kChromeUIGCMInternalsHost ||
+         host == chrome::kChromeUIInterstitialHost ||
+         host == chrome::kChromeUIInterventionsInternalsHost ||
+         host == chrome::kChromeUIInvalidationsHost ||
+         host == chrome::kChromeUILocalStateHost ||
+         host == chrome::kChromeUIMediaEngagementHost ||
+         host == chrome::kChromeUIMemoryInternalsHost ||
+         host == chrome::kChromeUINTPTilesInternalsHost ||
+         host == chrome::kChromeUINetExportHost ||
+         host == chrome::kChromeUINetInternalsHost ||
+         host == chrome::kChromeUINotificationsInternalsHost ||
+         host == chrome::kChromeUIOmniboxHost ||
+         host == chrome::kChromeUIPasswordManagerInternalsHost ||
+         host == chrome::kChromeUIPolicyHost ||
+         host == chrome::kChromeUIPredictorsHost ||
+         host == chrome::kChromeUIQuotaInternalsHost ||
+         host == chrome::kChromeUISandboxHost ||
+         host == chrome::kChromeUISignInInternalsHost ||
+         host == chrome::kChromeUISiteEngagementHost ||
+         host == chrome::kChromeUISnippetsInternalsHost ||
+         host == chrome::kChromeUISuggestionsHost ||
+         host == chrome::kChromeUISupervisedUserInternalsHost ||
+         host == chrome::kChromeUISyncInternalsHost ||
+         host == chrome::kChromeUITranslateInternalsHost ||
+         host == chrome::kChromeUIUkmHost ||
+         host == chrome::kChromeUIUsbInternalsHost ||
+         host == chrome::kChromeUIUserActionsHost ||
+         host == chrome::kChromeUIWebApksHost ||
+         host == chrome::kChromeUIWebRtcLogsHost ||
+         host == content::kChromeUIAppCacheInternalsHost ||
+         host == content::kChromeUIBlobInternalsHost ||
+         host == content::kChromeUIGpuHost ||
+         host == content::kChromeUIHistogramHost ||
+         host == content::kChromeUIIndexedDBInternalsHost ||
+         host == content::kChromeUIMediaInternalsHost ||
+         host == content::kChromeUINetworkErrorsListingHost ||
+         host == content::kChromeUIProcessInternalsHost ||
+         host == content::kChromeUIServiceWorkerInternalsHost ||
+         host == content::kChromeUIWebRTCInternalsHost ||
+         host == safe_browsing::kChromeUISafeBrowsingHost;
 }
 
 }  // namespace
+
+namespace chrome {
+namespace android {
 
 bool HandleDfmifiedDevUiPageURL(
     GURL* url,
