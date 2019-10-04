3a62085a3fdbb433aa29a08a1132d5b1d70bd944
John Abd-El-Malek <jam@chromium.org>
2019-10-04 00:40:31 +0000

    Reland "Add weblayer browser_tests APK so we can run the tests on Android."
    This is a reland of e355b04fe677f1519692db6b9fe17aa9aa811468 [formerly aaa2ec5c4603bfe5e423b8e3a49025c5fe75bd9f] with fix from patchset 1 to 2 due to conflict with r702520.
    
    Original change's description:
    > Add weblayer browser_tests APK so we can run the tests on Android.
    >
    > The Shell C++ class isn't used on Android (unlike in content_shell) because we want to consume WebLayer there using the idiomatic Java API. At the same time, we want to write browser tests the same way for all platforms. So fake this by creating a Shell that reuses the existing BrowserController class that was created by Java.
    >
    > Change-Id: I946957f39eb79cff2e84c8abda56eed98b28d0a0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1815884
    > Reviewed-by: Clark DuVall <cduvall@chromium.org>
    > Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#702563}
    
    TBR=cduvall@chromium.org
    
    Change-Id: I425b7b15d9f742a25a5b7c9dd306067ca29e551b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1838953
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702657}
    
    Former-commit-id: 4a9ab5950da8a289b4a390d9da7756ce3955a2ae

--- weblayer/browser/browser_controller_impl.cc
+++ weblayer/browser/browser_controller_impl.cc
@@ -24,8 +24,19 @@
 
 namespace weblayer {
 
+namespace {
+
+#if defined(OS_ANDROID)
+BrowserController* g_last_browser_controller;
+#endif
+
+}  // namespace
+
 BrowserControllerImpl::BrowserControllerImpl(ProfileImpl* profile)
     : profile_(profile) {
+#if defined(OS_ANDROID)
+  g_last_browser_controller = this;
+#endif
   content::WebContents::CreateParams create_params(
       profile_->GetBrowserContext());
   web_contents_ = content::WebContents::Create(create_params);
@@ -149,5 +160,11 @@ std::unique_ptr<BrowserController> BrowserController::Create(Profile* profile) {
   return std::make_unique<BrowserControllerImpl>(
       static_cast<ProfileImpl*>(profile));
 }
+
+#if defined(OS_ANDROID)
+BrowserController* BrowserController::GetLastControllerForTesting() {
+  return g_last_browser_controller;
+}
+#endif
 
 }  // namespace weblayer
--- weblayer/shell/browser/shell.h
+++ weblayer/shell/browser/shell.h
@@ -38,6 +38,9 @@ class Profile;
 
 // This represents one window of the Web Shell, i.e. all the UI including
 // buttons and url bar, as well as the web content area.
+// On desktop this is used for the demo Shell application and also
+// weblayer_browsertests. On Android this is only used for
+// weblayer_browsertests.
 class Shell : public BrowserObserver {
  public:
   ~Shell() override;
@@ -67,7 +70,7 @@ class Shell : public BrowserObserver {
   // instance is destroyed.
   static void SetMainMessageLoopQuitClosure(base::OnceClosure quit_closure);
 
-  BrowserController* browser_controller() { return browser_controller_.get(); }
+  BrowserController* browser_controller();
 
   gfx::NativeWindow window() { return window_; }
 
