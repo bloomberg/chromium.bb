a8df779f3f7fecbdf12f808dd474cd8fee2d5b05
Nate Fischer <ntfschr@chromium.org>
2019-10-05 02:09:25 +0000

    AW: prefer androidx annotations
    No change to logic.
    
    This swaps android.support.annotation.* for androidx.annotation.* in
    //android_webview/. Both of these are @Retention(SOURCE), so they'll
    have no effect on the product's behavior.
    
    No BUILD file changes because we were depending on android.support.*
    transitively (which was a bug we've now fixed) and already depended on
    androidx.annotation.* (so now our folder is consistent).
    
    Test: N/A
    Change-Id: I9cb65d5eb2cc0d52f56403d804b0b4f6b4acf49d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1842614
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Tao Bai <michaelbai@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703124}
    
    Former-commit-id: a0c28bd15cb7dfc7223bd861835afe65908fd8f5

--- android_webview/java/src/org/chromium/android_webview/WebMessageListenerHolder.java
+++ android_webview/java/src/org/chromium/android_webview/WebMessageListenerHolder.java
@@ -5,7 +5,8 @@
 package org.chromium.android_webview;
 
 import android.net.Uri;
-import android.support.annotation.NonNull;
+
+import androidx.annotation.NonNull;
 
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
