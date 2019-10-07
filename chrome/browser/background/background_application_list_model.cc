bloomberg/chromium.bb

commit 7aa2eb91f25047bab9026b3c3af02f7c759f1ecd

Remove NOTIFICATION_BACKGROUND_CONTENTS_*
Replace with a new observer interface and by extending
BackgroundService::Delegate.

Bug: 268984
Change-Id: I3952ae91064a171f88574987a0cef7bacccd663c
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1659595
Reviewed-by: Scott Violet <sky@chromium.org>
Commit-Queue: Evan Stade <estade@chromium.org>
Cr-Commit-Position: refs/heads/master@{#670198}

Former-commit-id: 6670c336fbeefc2d854550a926fd5ae993eb2d3c

chrome/browser/background/background_application_list_model.cc
@@ -142,20 +142,16 @@ void BackgroundApplicationListModel::Application::RequestIcon(
       base::Bind(&Application::OnImageLoaded, AsWeakPtr()));
 }
 
-BackgroundApplicationListModel::~BackgroundApplicationListModel() {
-}
+BackgroundApplicationListModel::~BackgroundApplicationListModel() = default;
 
 BackgroundApplicationListModel::BackgroundApplicationListModel(Profile* profile)
-    : profile_(profile),
-      ready_(false),
-      extension_registry_observer_(this),
-      weak_ptr_factory_(this) {
+    : profile_(profile) {
   DCHECK(profile_);
+  background_contents_service_observer_.Add(
+      BackgroundContentsServiceFactory::GetForProfile(profile));
+
   registrar_.Add(this,
                  extensions::NOTIFICATION_EXTENSION_PERMISSIONS_UPDATED,
-                 content::Source<Profile>(profile));
-  registrar_.Add(this,
-                 chrome::NOTIFICATION_BACKGROUND_CONTENTS_SERVICE_CHANGED,
                  content::Source<Profile>(profile));
   extensions::ExtensionSystem::Get(profile_)->ready().Post(
       FROM_HERE,
@@ -280,25 +276,11 @@ void BackgroundApplicationListModel::Observe(
     int type,
     const content::NotificationSource& source,
     const content::NotificationDetails& details) {
-  extensions::ExtensionService* service =
-      extensions::ExtensionSystem::Get(profile_)->extension_service();
-  if (!service || !service->is_ready())
-    return;
-
-  switch (type) {
-    case extensions::NOTIFICATION_EXTENSION_PERMISSIONS_UPDATED:
-      OnExtensionPermissionsUpdated(
-          content::Details<UpdatedExtensionPermissionsInfo>(details)->extension,
-          content::Details<UpdatedExtensionPermissionsInfo>(details)->reason,
-          content::Details<UpdatedExtensionPermissionsInfo>(details)->
-              permissions);
-      break;
-    case chrome::NOTIFICATION_BACKGROUND_CONTENTS_SERVICE_CHANGED:
-      Update();
-      break;
-    default:
-      NOTREACHED() << "Received unexpected notification";
-  }
+  DCHECK_EQ(type, extensions::NOTIFICATION_EXTENSION_PERMISSIONS_UPDATED);
+  OnExtensionPermissionsUpdated(
+      content::Details<UpdatedExtensionPermissionsInfo>(details)->extension,
+      content::Details<UpdatedExtensionPermissionsInfo>(details)->reason,
+      content::Details<UpdatedExtensionPermissionsInfo>(details)->permissions);
 }
 
 void BackgroundApplicationListModel::SendApplicationDataChangedNotifications() {
@@ -327,6 +309,7 @@ void BackgroundApplicationListModel::OnExtensionUnloaded(
 }
 
 void BackgroundApplicationListModel::OnExtensionSystemReady() {
+  ready_ = true;
   // All initial extensions will be loaded when extension system ready. So we
   // can get everything here.
   Update();
@@ -338,12 +321,19 @@ void BackgroundApplicationListModel::OnExtensionSystemReady() {
   // for the extension system, which isn't a guarantee. Thus, register here and
   // associate all initial extensions.
   extension_registry_observer_.Add(ExtensionRegistry::Get(profile_));
-  ready_ = true;
 }
 
 void BackgroundApplicationListModel::OnShutdown(ExtensionRegistry* registry) {
   DCHECK_EQ(ExtensionRegistry::Get(profile_), registry);
   extension_registry_observer_.Remove(registry);
+}
+
+void BackgroundApplicationListModel::OnBackgroundContentsServiceChanged() {
+  Update();
+}
+
+void BackgroundApplicationListModel::OnBackgroundContentsServiceDestroying() {
+  background_contents_service_observer_.RemoveAll();
 }
 
 void BackgroundApplicationListModel::OnExtensionPermissionsUpdated(
@@ -377,6 +367,9 @@ void BackgroundApplicationListModel::RemoveObserver(Observer* observer) {
 // differs from the old list, it generates OnApplicationListChanged events for
 // each observer.
 void BackgroundApplicationListModel::Update() {
+  if (!ready_)
+    return;
+
   extensions::ExtensionService* service =
       extensions::ExtensionSystem::Get(profile_)->extension_service();
 

chrome/browser/background/background_application_list_model.h
@@ -15,6 +15,8 @@
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "base/scoped_observer.h"
+#include "chrome/browser/background/background_contents_service.h"
+#include "chrome/browser/background/background_contents_service_observer.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
 #include "extensions/browser/extension_registry_observer.h"
@@ -35,7 +37,8 @@ class ExtensionRegistry;
 // BackgroundContents).
 class BackgroundApplicationListModel
     : public content::NotificationObserver,
-      public extensions::ExtensionRegistryObserver {
+      public extensions::ExtensionRegistryObserver,
+      public BackgroundContentsServiceObserver {
  public:
   // Observer is informed of changes to the model.  Users of the
   // BackgroundApplicationListModel should anticipate that associated data,
@@ -120,18 +123,22 @@ class BackgroundApplicationListModel
   // Returns the Application associated with |extension| or NULL.
   Application* FindApplication(const extensions::Extension* extension);
 
-  // content::NotificationObserver implementation.
+  // content::NotificationObserver:
   void Observe(int type,
                const content::NotificationSource& source,
                const content::NotificationDetails& details) override;
 
-  // extensions::ExtensionRegistryObserver implementation.
+  // extensions::ExtensionRegistryObserver:
   void OnExtensionLoaded(content::BrowserContext* browser_context,
                          const extensions::Extension* extension) override;
   void OnExtensionUnloaded(content::BrowserContext* browser_context,
                            const extensions::Extension* extension,
                            extensions::UnloadedExtensionReason reason) override;
   void OnShutdown(extensions::ExtensionRegistry* registry) override;
+
+  // BackgroundContentsServiceObserver:
+  void OnBackgroundContentsServiceChanged() override;
+  void OnBackgroundContentsServiceDestroying() override;
 
   // Intended to be called when extension system is ready.
   void OnExtensionSystemReady();
@@ -160,14 +167,17 @@ class BackgroundApplicationListModel
   base::ObserverList<Observer, true>::Unchecked observers_;
   Profile* const profile_;
   content::NotificationRegistrar registrar_;
-  bool ready_;
+  bool ready_{false};
 
   // Listens to extension load, unload notifications.
   ScopedObserver<extensions::ExtensionRegistry,
                  extensions::ExtensionRegistryObserver>
-      extension_registry_observer_;
+      extension_registry_observer_{this};
 
-  base::WeakPtrFactory<BackgroundApplicationListModel> weak_ptr_factory_;
+  ScopedObserver<BackgroundContentsService, BackgroundContentsServiceObserver>
+      background_contents_service_observer_{this};
+
+  base::WeakPtrFactory<BackgroundApplicationListModel> weak_ptr_factory_{this};
 
   DISALLOW_COPY_AND_ASSIGN(BackgroundApplicationListModel);
 };

chrome/browser/background/background_mode_manager.cc
@@ -617,11 +617,6 @@ void BackgroundModeManager::StartBackgroundMode() {
   in_background_mode_ = true;
 
   UpdateKeepAliveAndTrayIcon();
-
-  content::NotificationService::current()->Notify(
-      chrome::NOTIFICATION_BACKGROUND_MODE_CHANGED,
-      content::Source<BackgroundModeManager>(this),
-      content::Details<bool>(&in_background_mode_));
 }
 
 void BackgroundModeManager::EndBackgroundMode() {
@@ -630,11 +625,6 @@ void BackgroundModeManager::EndBackgroundMode() {
   in_background_mode_ = false;
 
   UpdateKeepAliveAndTrayIcon();
-
-  content::NotificationService::current()->Notify(
-      chrome::NOTIFICATION_BACKGROUND_MODE_CHANGED,
-      content::Source<BackgroundModeManager>(this),
-      content::Details<bool>(&in_background_mode_));
 }
 
 void BackgroundModeManager::EnableBackgroundMode() {

chrome/browser/background/background_application_list_model_unittest.cc
@@ -304,24 +304,24 @@ TEST_F(BackgroundApplicationListModelTest, LateExtensionSystemReady) {
 
   scoped_refptr<Extension> bgapp =
       CreateExtension("background_application", true);
-  ASSERT_TRUE(
+  EXPECT_TRUE(
       bgapp->permissions_data()->HasAPIPermission(APIPermission::kBackground));
-  ASSERT_TRUE(registry()->enabled_extensions().is_empty());
-  ASSERT_EQ(0U, model()->size());
+  EXPECT_TRUE(registry()->enabled_extensions().is_empty());
+  EXPECT_EQ(0U, model()->size());
 
   extensions::TestExtensionRegistryObserver load_observer(registry());
   // extensions can be loaded before ExtensionSystem::ready() is dispatched.
   service()->AddExtension(bgapp.get());
   load_observer.WaitForExtensionLoaded();
-  ASSERT_EQ(1U, registry()->enabled_extensions().size());
+  EXPECT_EQ(1U, registry()->enabled_extensions().size());
   // Model still has 0 item. since OnExtensionSystemReady is not called yet.
-  ASSERT_EQ(0U, model()->size());
+  EXPECT_EQ(0U, model()->size());
 
   // Wait Until OnExtensionSystemReady called.
   base::RunLoop().RunUntilIdle();
   // Make sure background model holds extensions.
-  ASSERT_TRUE(model()->is_ready());
-  ASSERT_EQ(1U, model()->size());
+  EXPECT_TRUE(model()->is_ready());
+  EXPECT_EQ(1U, model()->size());
 }
 
 typedef std::set<scoped_refptr<Extension> > ExtensionCollection;

chrome/browser/BUILD.gn
@@ -2836,6 +2836,7 @@ jumbo_split_static_library("browser") {
       "apps/intent_helper/page_transition_util.h",
       "background/background_contents.cc",
       "background/background_contents.h",
+      "background/background_contents_service_observer.h",
       "badging/badge_manager.cc",
       "badging/badge_manager.h",
       "badging/badge_manager_delegate.h",

chrome/browser/background/background_contents.cc
@@ -101,10 +101,7 @@ BackgroundContents::~BackgroundContents() {
   // (http://crbug.com/237781).
   registrar_.RemoveAll();
 
-  content::NotificationService::current()->Notify(
-      chrome::NOTIFICATION_BACKGROUND_CONTENTS_DELETED,
-      content::Source<Profile>(profile_),
-      content::Details<BackgroundContents>(this));
+  delegate_->OnBackgroundContentsDeleted(this);
   for (auto& observer : deferred_start_render_host_observer_list_)
     observer.OnDeferredStartRenderHostDestroyed(this);
 
@@ -121,10 +118,7 @@ void BackgroundContents::CreateRenderViewSoon(const GURL& url) {
 }
 
 void BackgroundContents::CloseContents(WebContents* source) {
-  content::NotificationService::current()->Notify(
-      chrome::NOTIFICATION_BACKGROUND_CONTENTS_CLOSED,
-      content::Source<Profile>(profile_),
-      content::Details<BackgroundContents>(this));
+  delegate_->OnBackgroundContentsClosed(this);
   delete this;
 }
 
@@ -140,10 +134,7 @@ void BackgroundContents::DidNavigateMainFramePostCommit(WebContents* tab) {
   // some way to scope navigation of a background page to its opener's security
   // origin. Note: if the first navigation is to a URL outside the app's
   // extent a background page will be opened but will remain at about:blank.
-  content::NotificationService::current()->Notify(
-      chrome::NOTIFICATION_BACKGROUND_CONTENTS_NAVIGATED,
-      content::Source<Profile>(profile_),
-      content::Details<BackgroundContents>(this));
+  delegate_->OnBackgroundContentsNavigated(this);
 }
 
 // Forward requests to add a new WebContents to our delegate.
@@ -165,10 +156,7 @@ bool BackgroundContents::IsNeverVisible(content::WebContents* web_contents) {
 }
 
 void BackgroundContents::RenderProcessGone(base::TerminationStatus status) {
-  content::NotificationService::current()->Notify(
-      chrome::NOTIFICATION_BACKGROUND_CONTENTS_TERMINATED,
-      content::Source<Profile>(profile_),
-      content::Details<BackgroundContents>(this));
+  delegate_->OnBackgroundContentsTerminated(this);
 
   // Our RenderView went away, so we should go away also, so killing the process
   // via the TaskManager doesn't permanently leave a BackgroundContents hanging

chrome/browser/background/background_contents.h
@@ -51,6 +51,14 @@ class BackgroundContents : public extensions::DeferredStartRenderHost,
         const gfx::Rect& initial_rect,
         bool* was_blocked) = 0;
 
+    // Informs the delegate of lifetime events.
+    virtual void OnBackgroundContentsNavigated(
+        BackgroundContents* contents) = 0;
+    virtual void OnBackgroundContentsTerminated(
+        BackgroundContents* contents) = 0;
+    virtual void OnBackgroundContentsClosed(BackgroundContents* contents) = 0;
+    virtual void OnBackgroundContentsDeleted(BackgroundContents* contents) = 0;
+
    protected:
     virtual ~Delegate() {}
   };

commit 2faed3494dfd81f8d0995dea43a61f6d2919f319

Don't send background app notifications during Browser startup.
This restores behavior broken by 7aa2eb91f25047bab9026b3c3af02f7c7 [formerly 6670c336fbeefc2d854550a926fd5ae99]

Change the name of BackgroundApplicationListModel::is_ready() to
startup_done() to disambiguate from ExtensionService::is_ready().

Move BackgroundContentsService observation to OnExtensionSystemReady()
so we don't run BackgroundApplicationListModel::Update() too early.
Note that it's not sufficient to check whether the ExtensionService is
ready because it may return true before
BackgroundApplicationListModel::OnExtensionSystemReady runs.

Bug: 1008890
Change-Id: Ib3e5013e7857e7a9aa6f94ed6b43f3839e08fe00
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1837486
Reviewed-by: Drew Wilson <atwilson@chromium.org>
Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
Commit-Queue: Evan Stade <estade@chromium.org>
Cr-Commit-Position: refs/heads/master@{#703319}

Former-commit-id: f35770b2c363357645bf12da6bde29a487a62b18

chrome/browser/background/background_application_list_model.cc
@@ -147,9 +147,6 @@ BackgroundApplicationListModel::~BackgroundApplicationListModel() = default;
 BackgroundApplicationListModel::BackgroundApplicationListModel(Profile* profile)
     : profile_(profile) {
   DCHECK(profile_);
-  background_contents_service_observer_.Add(
-      BackgroundContentsServiceFactory::GetForProfile(profile));
-
   registrar_.Add(this,
                  extensions::NOTIFICATION_EXTENSION_PERMISSIONS_UPDATED,
                  content::Source<Profile>(profile));
@@ -309,7 +306,6 @@ void BackgroundApplicationListModel::OnExtensionUnloaded(
 }
 
 void BackgroundApplicationListModel::OnExtensionSystemReady() {
-  ready_ = true;
   // All initial extensions will be loaded when extension system ready. So we
   // can get everything here.
   Update();
@@ -321,6 +317,11 @@ void BackgroundApplicationListModel::OnExtensionSystemReady() {
   // for the extension system, which isn't a guarantee. Thus, register here and
   // associate all initial extensions.
   extension_registry_observer_.Add(ExtensionRegistry::Get(profile_));
+
+  background_contents_service_observer_.Add(
+      BackgroundContentsServiceFactory::GetForProfile(profile_));
+
+  startup_done_ = true;
 }
 
 void BackgroundApplicationListModel::OnShutdown(ExtensionRegistry* registry) {
@@ -367,11 +368,9 @@ void BackgroundApplicationListModel::RemoveObserver(Observer* observer) {
 // differs from the old list, it generates OnApplicationListChanged events for
 // each observer.
 void BackgroundApplicationListModel::Update() {
-  if (!ready_)
-    return;
-
   extensions::ExtensionService* service =
       extensions::ExtensionSystem::Get(profile_)->extension_service();
+  DCHECK(service->is_ready());
 
   // Discover current background applications, compare with previous list, which
   // is consistently sorted, and notify observers if they differ.

chrome/browser/background/background_application_list_model.h
@@ -101,9 +101,7 @@ class BackgroundApplicationListModel
   }
 
   // Returns true if all startup notifications have already been issued.
-  bool is_ready() const {
-    return ready_;
-  }
+  bool startup_done() const { return startup_done_; }
 
  private:
   // Contains data associated with a background application that is not
@@ -167,7 +165,7 @@ class BackgroundApplicationListModel
   base::ObserverList<Observer, true>::Unchecked observers_;
   Profile* const profile_;
   content::NotificationRegistrar registrar_;
-  bool ready_{false};
+  bool startup_done_ = false;
 
   // Listens to extension load, unload notifications.
   ScopedObserver<extensions::ExtensionRegistry,

chrome/browser/background/background_mode_manager.cc
@@ -206,7 +206,7 @@ BackgroundModeManager::BackgroundModeData::GetNewBackgroundApps() {
       current_extensions_.insert(id);
       // If this application has been newly loaded after the initial startup,
       // notify the user.
-      if (applications_->is_ready())
+      if (applications_->startup_done())
         new_apps.insert(application.get());
     }
   }
@@ -744,6 +744,7 @@ void BackgroundModeManager::OnBackgroundClientInstalled(
   EnableBackgroundMode();
   ResumeBackgroundMode();
 
+  ++client_installed_notifications_;
   // Notify the user that a background client has been installed.
   DisplayClientInstalledNotification(name);
 }

chrome/browser/background/background_mode_manager.h
@@ -108,6 +108,10 @@ class BackgroundModeManager : public content::NotificationObserver,
   // For testing purposes.
   size_t NumberOfBackgroundModeData();
 
+  int client_installed_notifications_for_test() {
+    return client_installed_notifications_;
+  }
+
  private:
   friend class AppBackgroundPageApiTest;
   friend class BackgroundModeManagerTest;
@@ -394,6 +398,10 @@ class BackgroundModeManager : public content::NotificationObserver,
   // (used for testing background mode without having to install a background
   // app).
   bool keep_alive_for_test_ = false;
+
+  // Tracks the number of "background app installed" notifications shown to the
+  // user. Used for testing.
+  int client_installed_notifications_ = 0;
 
   // Set to true when background mode is suspended.
   bool background_mode_suspended_ = false;

chrome/browser/extensions/background_app_browsertest.cc
@@ -9,71 +9,54 @@
 #include "chrome/browser/background/background_mode_manager.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
+#include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "extensions/browser/test_extension_registry_observer.h"
 
-class TestBackgroundModeManager : public BackgroundModeManager {
- public:
-  TestBackgroundModeManager(const base::CommandLine& command_line,
-                            ProfileAttributesStorage* profile_storage)
-      : BackgroundModeManager(command_line, profile_storage),
-        showed_background_app_installed_notification_for_test_(false) {}
+namespace extensions {
 
-  ~TestBackgroundModeManager() override {}
-
-  void DisplayClientInstalledNotification(const base::string16& name) override {
-    showed_background_app_installed_notification_for_test_ = true;
-  }
-
-  bool showed_background_app_installed_notification_for_test() {
-    return showed_background_app_installed_notification_for_test_;
-  }
-
-  void set_showed_background_app_installed_notification_for_test(
-      bool showed) {
-    showed_background_app_installed_notification_for_test_ = showed;
-  }
-
- private:
-  // Tracks if we have shown a "Background App Installed" notification to the
-  // user.  Used for unit tests only.
-  bool showed_background_app_installed_notification_for_test_;
-
-  FRIEND_TEST_ALL_PREFIXES(BackgroundAppBrowserTest,
-                           ReloadBackgroundApp);
-
-  DISALLOW_COPY_AND_ASSIGN(TestBackgroundModeManager);
-};
-
-using BackgroundAppBrowserTest = extensions::ExtensionBrowserTest;
+using BackgroundAppBrowserTest = ExtensionBrowserTest;
 
 // Tests that if we reload a background app, we don't get a popup bubble
 // telling us that a new background app has been installed.
 IN_PROC_BROWSER_TEST_F(BackgroundAppBrowserTest, ReloadBackgroundApp) {
-  // Pass this in to the browser test.
-  std::unique_ptr<BackgroundModeManager> test_background_mode_manager(
-      new TestBackgroundModeManager(*base::CommandLine::ForCurrentProcess(),
-                                    &(g_browser_process->profile_manager()
-                                          ->GetProfileAttributesStorage())));
-  g_browser_process->set_background_mode_manager_for_test(
-      std::move(test_background_mode_manager));
-  TestBackgroundModeManager* manager =
-      reinterpret_cast<TestBackgroundModeManager*>(
-          g_browser_process->background_mode_manager());
-
+  BackgroundModeManager* manager = g_browser_process->background_mode_manager();
   // Load our background extension
-  ASSERT_FALSE(
-      manager->showed_background_app_installed_notification_for_test());
-  const extensions::Extension* extension = LoadExtension(
-      test_data_dir_.AppendASCII("background_app"));
+  EXPECT_EQ(0, manager->client_installed_notifications_for_test());
+  const Extension* extension =
+      LoadExtension(test_data_dir_.AppendASCII("background_app"));
+  EXPECT_EQ(1, manager->client_installed_notifications_for_test());
   ASSERT_FALSE(extension == NULL);
-
-  // Set the test flag to not shown.
-  manager->set_showed_background_app_installed_notification_for_test(false);
 
   // Reload our background extension
   ReloadExtension(extension->id());
 
-  // Ensure that we did not see a "Background extension loaded" dialog.
-  EXPECT_FALSE(
-      manager->showed_background_app_installed_notification_for_test());
+  // Ensure that we did not see another "Background extension loaded" dialog.
+  EXPECT_EQ(1, manager->client_installed_notifications_for_test());
 }
+
+// Make sure that the background mode notification is sent for an app install,
+// but not again on browser restart. Regression test for
+// https://crbug.com/1008890
+IN_PROC_BROWSER_TEST_F(BackgroundAppBrowserTest, PRE_InstallBackgroundApp) {
+  InstallExtension(test_data_dir_.AppendASCII("background_app"), 1);
+  EXPECT_EQ(1, g_browser_process->background_mode_manager()
+                   ->client_installed_notifications_for_test());
+}
+
+IN_PROC_BROWSER_TEST_F(BackgroundAppBrowserTest, InstallBackgroundApp) {
+  // Verify the installed extension is still here.
+  const ExtensionSet& extensions = extension_registry()->enabled_extensions();
+  EXPECT_TRUE(
+      std::any_of(extensions.begin(), extensions.end(),
+                  [](scoped_refptr<const Extension> extension) {
+                    return extension->description() ==
+                           "A simple app with background permission set.";
+                  }));
+  // Verify the installed extension did not pop up a background mode
+  // notification.
+  EXPECT_EQ(0, g_browser_process->background_mode_manager()
+                   ->client_installed_notifications_for_test());
+}
+
+}  // namespace extensions

chrome/browser/background/background_application_list_model_unittest.cc
@@ -156,7 +156,7 @@ TEST_F(BackgroundApplicationListModelTest, DISABLED_ExplicitTest) {
   service()->Init();
   base::RunLoop().RunUntilIdle();
   ASSERT_TRUE(service()->is_ready());
-  ASSERT_TRUE(model()->is_ready());
+  ASSERT_TRUE(model()->startup_done());
 
   ASSERT_TRUE(registry()->enabled_extensions().is_empty());
   ASSERT_EQ(0U, model()->size());
@@ -296,11 +296,11 @@ TEST_F(BackgroundApplicationListModelTest, ExtensionLoadAndUnload) {
 
 TEST_F(BackgroundApplicationListModelTest, LateExtensionSystemReady) {
   ASSERT_FALSE(service()->is_ready());
-  ASSERT_FALSE(model()->is_ready());
+  ASSERT_FALSE(model()->startup_done());
   service()->Init();
   // Model is not ready yet since ExtensionSystem::ready() is dispatched using
   // PostTask to UI Thread. and OnExtensionSystemReady is not called yet.
-  ASSERT_FALSE(model()->is_ready());
+  ASSERT_FALSE(model()->startup_done());
 
   scoped_refptr<Extension> bgapp =
       CreateExtension("background_application", true);
@@ -314,13 +314,13 @@ TEST_F(BackgroundApplicationListModelTest, LateExtensionSystemReady) {
   service()->AddExtension(bgapp.get());
   load_observer.WaitForExtensionLoaded();
   EXPECT_EQ(1U, registry()->enabled_extensions().size());
-  // Model still has 0 item. since OnExtensionSystemReady is not called yet.
+  // Model still has 0 items since OnExtensionSystemReady is not called yet.
   EXPECT_EQ(0U, model()->size());
 
   // Wait Until OnExtensionSystemReady called.
   base::RunLoop().RunUntilIdle();
   // Make sure background model holds extensions.
-  EXPECT_TRUE(model()->is_ready());
+  EXPECT_TRUE(model()->startup_done());
   EXPECT_EQ(1U, model()->size());
 }
 