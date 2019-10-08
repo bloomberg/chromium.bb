c1657d077843fb8dfd74538eb8aad287ed97818c
ckitagawa <ckitagawa@chromium.org>
2019-10-08 15:22:45 +0000

    [Paint Preview] Inject PaintPreviewTracker
    Adds the ability to inject paint_preview::PaintPreviewTracker into
    blink::GraphicsContext to act similarly to printing::MetafileSkia.
    This class will track various metadata about the Paint Preview
    capture process.
    
    Design Doc: go/fdt-design (internal)
    Part of Landing: crrev/1786583
    
    Bug: 1008885
    Change-Id: Iba2819491bee7b284de62a1bf888aa9486c788cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1829277
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703720}
    
    Former-commit-id: 2c483e9385818283717b451f1027f9c571ac5c46

--- third_party/blink/renderer/platform/graphics/paint/paint_record_builder.h
+++ third_party/blink/renderer/platform/graphics/paint/paint_record_builder.h
@@ -19,12 +19,15 @@ namespace cc {
 class PaintCanvas;
 }
 
+namespace paint_preview {
+class PaintPreviewTracker;
+}
+
 namespace blink {
 class GraphicsContext;
 class PaintController;
 
 class PLATFORM_EXPORT PaintRecordBuilder final : public DisplayItemClient {
-
  public:
   // Constructs a new builder for the resulting paint record. If |metadata|
   // is specified, that metadata is propagated to the builder's internal canvas.
@@ -39,7 +42,8 @@ class PLATFORM_EXPORT PaintRecordBuilder final : public DisplayItemClient {
   // CompositeAfterPaint.
   PaintRecordBuilder(printing::MetafileSkia* metafile = nullptr,
                      GraphicsContext* containing_context = nullptr,
-                     PaintController* = nullptr);
+                     PaintController* = nullptr,
+                     paint_preview::PaintPreviewTracker* tracker = nullptr);
   ~PaintRecordBuilder() override;
 
   GraphicsContext& Context() { return *context_; }
