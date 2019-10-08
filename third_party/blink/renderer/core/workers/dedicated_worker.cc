cce6697838d49e939f0a6d677f89bd16cb7f658d
Gyuyoung Kim <gyuyoung@igalia.com>
2019-10-08 04:33:35 +0000

    Reland "Replace .mojom-blink.h with .mojom-blink-forward.h in blink - 5"
    This is a reland of 719e0c8a1cb2f4a8951c750c87ea9843d8ff349f [formerly 5d024fd729e50afc50ad190083dcf140fc2787f3]
    
    Missed including "ip_address_space.mojom-blink.h"
    in worker_module_script_fetcher.cc
    
    Original change's description:
    > Replace .mojom-blink.h with .mojom-blink-forward.h in blink - 5
    >
    > To reduce pre-processed size of header files have been including
    > foo.mojom-blink.h, this CL replaces .mojom-blink.h with
    > .mojom-blink-forward.h in blink header files.
    >
    > This CL has no behavior changes and following CLs will
    > continue replacing them more.
    >
    > Bug: 1001360
    > Change-Id: I49cfab31ec57b183570cf4551e4cd1acd20a1a75
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1844752
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    > Cr-Commit-Position: refs/heads/master@{#703549}
    
    Bug: 1001360
    Change-Id: I5279e2fba33e46fa7eb002deeda34421c4176f53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1846416
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#703592}
    
    Former-commit-id: 5ccc40c0bbd4e6dbeeef327b8cdad02923b344b1

--- third_party/blink/renderer/core/workers/dedicated_worker.cc
+++ third_party/blink/renderer/core/workers/dedicated_worker.cc
@@ -12,6 +12,7 @@
 #include "services/service_manager/public/mojom/interface_provider.mojom-blink.h"
 #include "third_party/blink/public/common/blob/blob_utils.h"
 #include "third_party/blink/public/common/features.h"
+#include "third_party/blink/public/mojom/browser_interface_broker.mojom-blink.h"
 #include "third_party/blink/public/mojom/script/script_type.mojom-blink.h"
 #include "third_party/blink/public/mojom/worker/dedicated_worker_host_factory.mojom-blink.h"
 #include "third_party/blink/public/platform/web_content_settings_client.h"
