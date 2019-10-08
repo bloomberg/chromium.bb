7522f879e38a5f6f77718d543eaef2410e752899
Jack Rosenthal <jrosenth@chromium.org>
2019-10-08 15:04:02 +0000

    Revert "chromeos_config: mark veyron_jerry experimental"
    This reverts commit 40324d671c257be329238223356da4e77ab16540.
    
    Reason for revert: jerry RO space issue resolved
    
    BUG=chromium:1011621
    
    Original change's description:
    > chromeos_config: mark veyron_jerry experimental
    > 
    > The EC RO image on ToT for jerry is out of space on flash. I removed
    > jerry from chromeos-ec unit tests, but it will still be built in the
    > BuildPackages phase for the veyron_jerry builder. The best temporary
    > solution I can think of for now is to mark experimental.
    > 
    > BUG=chromium:1011621
    > TEST=./run_tests
    > 
    > Change-Id: I4b3b2fc242358914c07dd335bbe9e5042e89ddb0
    > Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/1844448
    > Reviewed-by: George Engelbrecht <engeg@google.com>
    > Commit-Queue: George Engelbrecht <engeg@google.com>
    > Tested-by: George Engelbrecht <engeg@google.com>
    
    Bug: chromium:1011621
    Change-Id: I45679fd07ca5fda7d19aeba1b8b03a03dee5d35a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/1847772
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    Commit-Queue: Jack Rosenthal <jrosenth@chromium.org>
    Tested-by: Jack Rosenthal <jrosenth@chromium.org>

--- config/chromeos_config.py
+++ config/chromeos_config.py
@@ -2337,6 +2337,7 @@ def CqBuilders(site_config, boards_dict, ge_build_config):
       'ultima',
       'veyron_fievel',
       'veyron_jaq',
+      'veyron_jerry',
       'veyron_mickey',
       'veyron_minnie',
       'veyron_rialto',
@@ -2391,7 +2392,6 @@ def CqBuilders(site_config, boards_dict, ge_build_config):
       'mistral', # b/140536001
       'moblab-generic-vm', # crbug.com/960998, crbug.com/976297
       'samus-kernelnext', # https://crbug.com/1004618
-      'veyron_jerry', # https://crbug.com/1011621
       'veyron_mighty', # https://crbug.com/1001290
       'whirlwind', # https://crbug.com/1001714
       'wizpig', # crbug.com/996944
