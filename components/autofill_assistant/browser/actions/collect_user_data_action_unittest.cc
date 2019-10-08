5f416dc5e83ca7b7c841c96e905ccf63eb0d9b02
Clemens Arbesser <arbesser@google.com>
2019-10-08 09:31:31 +0000

    Reland "[Autofill Assistant] Fix bugs in user data form."
    This is a reland of 56d316e072a9342ec46f06c253e261b5110fd66b [formerly 814ee2b9b4e3940b8a672a143ff7c1d3145ba9a2]
    Missing includes were leading to build failure. This issue should be fixed now.
    
    Bug: 1012001
    
    Original change's description:
    > [Autofill Assistant] Fix bugs in user data form.
    >
    > This CL changes the way autofill-assistant verifies the user data form. It removes redundant code between |controller| and |collect_user_data_action|. Unifying the verification code allows for easier testing and also fixes several bugs. Most importantly, prior to this CL, it was possible for |collect_user_data_action| to store a nullptr in client memory for the billing address. This would eventually lead to a PRECONDITION_FAILED error in a future |autofill_action|.
    >
    > This CL also adds unit tests to both the controller and the action to prevent future regression of the same bugs.
    >
    > (cherry picked from commit 393bcd74cf663db619cfb6db5339e346971fea54 [formerly 31588ebb94ef6864b9cd35227aa6fcf7ec2ef17c])
    >
    > Bug: 1011328
    > Bug: b/136788056
    > Change-Id: I04515875ba3b8320680a538625e8c08adbbc5bf0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1808866
    > Commit-Queue: Clemens Arbesser <arbesser@google.com>
    > Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    > Cr-Original-Commit-Position: refs/heads/master@{#697999}
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1844986
    > Reviewed-by: Clemens Arbesser <arbesser@google.com>
    > Cr-Commit-Position: refs/branch-heads/3904@{#636}
    > Cr-Branched-From: 9035e7d56a589711638d7ffdc83ffa6d674a06ec [formerly 675968a8c657a3bd9c1c2c20c5d2935577bbc5e6]-refs/heads/master@{#693954}
    
    Bug: 1011328, b/136788056
    Change-Id: I6622abcfaedcb30c581a60b62c596d35971406ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1845767
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/branch-heads/3904@{#663}
    Cr-Branched-From: 9035e7d56a589711638d7ffdc83ffa6d674a06ec [formerly 675968a8c657a3bd9c1c2c20c5d2935577bbc5e6]-refs/heads/master@{#693954}
    
    Former-commit-id: eb60ad91e95ce443d0adda4635c4ad38c68f1018

--- components/autofill_assistant/browser/actions/collect_user_data_action_unittest.cc
+++ components/autofill_assistant/browser/actions/collect_user_data_action_unittest.cc
@@ -25,9 +25,6 @@ const char kFakeUsername[] = "user@example.com";
 const char kFakePassword[] = "example_password";
 
 const char kMemoryLocation[] = "billing";
-const char kName[] = "John Doe";
-const char kEmail[] = "john@doe.com";
-const char kPhone[] = "+1 234-567-8901";
 }  // namespace
 
 namespace autofill_assistant {
@@ -184,14 +181,27 @@ TEST_F(CollectUserDataActionTest, SelectContactDetails) {
   contact_details_proto->set_request_payer_email(true);
   contact_details_proto->set_request_payer_phone(true);
 
+  autofill::AutofillProfile contact_profile;
+  contact_profile.SetRawInfo(autofill::ServerFieldType::NAME_FULL,
+                             base::UTF8ToUTF16("Marion Mitchell Morrison"));
+  contact_profile.SetRawInfo(autofill::ServerFieldType::NAME_FIRST,
+                             base::UTF8ToUTF16("Marion"));
+  contact_profile.SetRawInfo(autofill::ServerFieldType::NAME_MIDDLE,
+                             base::UTF8ToUTF16("Mitchell"));
+  contact_profile.SetRawInfo(autofill::ServerFieldType::NAME_LAST,
+                             base::UTF8ToUTF16("Morrison"));
+  contact_profile.SetRawInfo(autofill::ServerFieldType::EMAIL_ADDRESS,
+                             base::UTF8ToUTF16("marion@me.xyz"));
+  contact_profile.SetRawInfo(autofill::ServerFieldType::PHONE_HOME_WHOLE_NUMBER,
+                             base::UTF8ToUTF16("16505678910"));
+
   ON_CALL(mock_action_delegate_, CollectUserData(_, _))
       .WillByDefault(Invoke(
-          [](std::unique_ptr<CollectUserDataOptions> collect_user_data_options,
-             std::unique_ptr<UserData> user_data) {
+          [=](std::unique_ptr<CollectUserDataOptions> collect_user_data_options,
+              std::unique_ptr<UserData> user_data) {
             user_data->succeed = true;
-            user_data->payer_name.assign(kName);
-            user_data->payer_phone.assign(kPhone);
-            user_data->payer_email.assign(kEmail);
+            user_data->contact_profile =
+                std::make_unique<autofill::AutofillProfile>(contact_profile);
             std::move(collect_user_data_options->confirm_callback)
                 .Run(std::move(user_data));
           }));
@@ -201,17 +211,19 @@ TEST_F(CollectUserDataActionTest, SelectContactDetails) {
                   Property(&ProcessedActionProto::status, ACTION_APPLIED),
                   Property(&ProcessedActionProto::collect_user_data_result,
                            Property(&CollectUserDataResultProto::payer_email,
-                                    kEmail))))));
+                                    "marion@me.xyz"))))));
+
   CollectUserDataAction action(&mock_action_delegate_, action_proto);
   action.ProcessAction(callback_.Get());
 
   EXPECT_EQ(client_memory_.has_selected_address(kMemoryLocation), true);
   auto* profile = client_memory_.selected_address(kMemoryLocation);
-  EXPECT_EQ(profile->GetRawInfo(autofill::NAME_FULL), base::UTF8ToUTF16(kName));
+  EXPECT_EQ(profile->GetRawInfo(autofill::NAME_FULL),
+            base::UTF8ToUTF16("Marion Mitchell Morrison"));
   EXPECT_EQ(profile->GetRawInfo(autofill::PHONE_HOME_WHOLE_NUMBER),
-            base::UTF8ToUTF16(kPhone));
+            base::UTF8ToUTF16("16505678910"));
   EXPECT_EQ(profile->GetRawInfo(autofill::EMAIL_ADDRESS),
-            base::UTF8ToUTF16(kEmail));
+            base::UTF8ToUTF16("marion@me.xyz"));
 }
 
 TEST_F(CollectUserDataActionTest, SelectPaymentMethod) {
@@ -272,10 +284,6 @@ TEST_F(CollectUserDataActionTest, MandatoryPostalCodeWithoutErrorMessageFails) {
 }
 
 TEST_F(CollectUserDataActionTest, ContactDetailsCanHandleUtf8) {
-  // String literal = 艾丽森 in UTF-8.
-  const std::string kNonAsciiName("\xE8\x89\xBE\xE4\xB8\xBD\xE6\xA3\xAE");
-  const std::string kNonAsciiEmail(
-      "\xE8\x89\xBE\xE4\xB8\xBD\xE6\xA3\xAE@example.com");
   ActionProto action_proto;
   auto* collect_user_data_proto = action_proto.mutable_collect_user_data();
   collect_user_data_proto->set_request_terms_and_conditions(false);
@@ -285,32 +293,194 @@ TEST_F(CollectUserDataActionTest, ContactDetailsCanHandleUtf8) {
   contact_details_proto->set_request_payer_name(true);
   contact_details_proto->set_request_payer_email(true);
 
+  // Name = 艾丽森 in UTF-8.
+  autofill::AutofillProfile contact_profile;
+  contact_profile.SetRawInfo(
+      autofill::ServerFieldType::NAME_FULL,
+      base::UTF8ToUTF16("\xE8\x89\xBE\xE4\xB8\xBD\xE6\xA3\xAE"));
+  contact_profile.SetRawInfo(
+      autofill::ServerFieldType::EMAIL_ADDRESS,
+      base::UTF8ToUTF16("\xE8\x89\xBE\xE4\xB8\xBD\xE6\xA3\xAE@example.com"));
+
   ON_CALL(mock_action_delegate_, CollectUserData(_, _))
       .WillByDefault(Invoke(
-          [&](std::unique_ptr<CollectUserDataOptions> collect_user_data_options,
+          [=](std::unique_ptr<CollectUserDataOptions> collect_user_data_options,
               std::unique_ptr<UserData> user_data) {
             user_data->succeed = true;
-            user_data->payer_name.assign(kNonAsciiName);
-            user_data->payer_email.assign(kNonAsciiEmail);
+            user_data->contact_profile =
+                std::make_unique<autofill::AutofillProfile>(contact_profile);
             std::move(collect_user_data_options->confirm_callback)
                 .Run(std::move(user_data));
           }));
 
-  EXPECT_CALL(callback_,
-              Run(Pointee(AllOf(
-                  Property(&ProcessedActionProto::status, ACTION_APPLIED),
-                  Property(&ProcessedActionProto::collect_user_data_result,
-                           Property(&CollectUserDataResultProto::payer_email,
-                                    kNonAsciiEmail))))));
+  EXPECT_CALL(
+      callback_,
+      Run(Pointee(AllOf(
+          Property(&ProcessedActionProto::status, ACTION_APPLIED),
+          Property(
+              &ProcessedActionProto::collect_user_data_result,
+              Property(&CollectUserDataResultProto::payer_email,
+                       "\xE8\x89\xBE\xE4\xB8\xBD\xE6\xA3\xAE@example.com"))))));
   CollectUserDataAction action(&mock_action_delegate_, action_proto);
   action.ProcessAction(callback_.Get());
 
   EXPECT_EQ(client_memory_.has_selected_address(kMemoryLocation), true);
   auto* profile = client_memory_.selected_address(kMemoryLocation);
   EXPECT_EQ(profile->GetRawInfo(autofill::NAME_FULL),
-            base::UTF8ToUTF16(kNonAsciiName));
-  EXPECT_EQ(profile->GetRawInfo(autofill::EMAIL_ADDRESS),
-            base::UTF8ToUTF16(kNonAsciiEmail));
+            base::UTF8ToUTF16("\xE8\x89\xBE\xE4\xB8\xBD\xE6\xA3\xAE"));
+  EXPECT_EQ(
+      profile->GetRawInfo(autofill::EMAIL_ADDRESS),
+      base::UTF8ToUTF16("\xE8\x89\xBE\xE4\xB8\xBD\xE6\xA3\xAE@example.com"));
+}
+
+TEST_F(CollectUserDataActionTest, UserDataComplete_Contact) {
+  UserData user_data;
+  CollectUserDataOptions options;
+  EXPECT_TRUE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+
+  user_data.contact_profile = std::make_unique<autofill::AutofillProfile>(
+      base::GenerateGUID(), kFakeUrl);
+  options.request_payer_email = true;
+  EXPECT_FALSE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+
+  user_data.contact_profile->SetRawInfo(
+      autofill::ServerFieldType::EMAIL_ADDRESS,
+      base::UTF8ToUTF16("joedoe@example.com"));
+  EXPECT_TRUE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+
+  options.request_payer_name = true;
+  EXPECT_FALSE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+
+  user_data.contact_profile->SetRawInfo(autofill::ServerFieldType::NAME_FULL,
+                                        base::UTF8ToUTF16("Joe Doe"));
+  EXPECT_TRUE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+
+  options.request_payer_phone = true;
+  EXPECT_FALSE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+
+  user_data.contact_profile->SetRawInfo(
+      autofill::ServerFieldType::PHONE_HOME_WHOLE_NUMBER,
+      base::UTF8ToUTF16("+1 23 456 789 01"));
+  EXPECT_TRUE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+}
+
+TEST_F(CollectUserDataActionTest, UserDataComplete_Payment) {
+  UserData user_data;
+  CollectUserDataOptions options;
+
+  options.request_payment_method = true;
+  EXPECT_FALSE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+
+  // Valid credit card, but no billing address.
+  user_data.card =
+      std::make_unique<autofill::CreditCard>(base::GenerateGUID(), kFakeUrl);
+  autofill::test::SetCreditCardInfo(user_data.card.get(), "Marion Mitchell",
+                                    "4111 1111 1111 1111", "01", "2020",
+                                    /* billing_address_id = */ "");
+  EXPECT_FALSE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+
+  // Valid credit card, but invalid billing address.
+  user_data.card->set_billing_address_id("invalid");
+  ON_CALL(mock_personal_data_manager_, GetProfileByGUID("invalid"))
+      .WillByDefault(Return(nullptr));
+  EXPECT_FALSE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+
+  // Incomplete billing address.
+  user_data.billing_address = std::make_unique<autofill::AutofillProfile>(
+      base::GenerateGUID(), kFakeUrl);
+  autofill::test::SetProfileInfo(user_data.billing_address.get(), "Marion",
+                                 "Mitchell", "Morrison", "marion@me.xyz", "Fox",
+                                 "123 Zoo St.", "unit 5", "Hollywood", "CA",
+                                 /* zipcode = */ "", "US", "16505678910");
+  ON_CALL(mock_personal_data_manager_,
+          GetProfileByGUID(user_data.billing_address->guid()))
+      .WillByDefault(Return(user_data.billing_address.get()));
+  user_data.card->set_billing_address_id(user_data.billing_address->guid());
+  EXPECT_FALSE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+
+  user_data.billing_address->SetRawInfo(autofill::ADDRESS_HOME_ZIP,
+                                        base::UTF8ToUTF16("91601"));
+  EXPECT_TRUE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+
+  // Zip code is optional in Argentinian address.
+  user_data.billing_address->SetRawInfo(autofill::ADDRESS_HOME_ZIP,
+                                        base::UTF8ToUTF16(""));
+  user_data.billing_address->SetRawInfo(autofill::ADDRESS_HOME_COUNTRY,
+                                        base::UTF8ToUTF16("AR"));
+  EXPECT_TRUE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+
+  options.require_billing_postal_code = true;
+  EXPECT_FALSE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+
+  user_data.billing_address->SetRawInfo(autofill::ADDRESS_HOME_ZIP,
+                                        base::UTF8ToUTF16("B1675"));
+  EXPECT_TRUE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+}
+
+TEST_F(CollectUserDataActionTest, UserDataComplete_Terms) {
+  UserData user_data;
+  CollectUserDataOptions options;
+  options.accept_terms_and_conditions_text.assign("Accept T&C");
+  EXPECT_FALSE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+
+  user_data.terms_and_conditions = REQUIRES_REVIEW;
+  EXPECT_TRUE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+
+  user_data.terms_and_conditions = ACCEPTED;
+  EXPECT_TRUE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+}
+
+TEST_F(CollectUserDataActionTest, UserDataComplete_Login) {
+  UserData user_data;
+  CollectUserDataOptions options;
+  options.request_login_choice = true;
+  EXPECT_FALSE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+
+  user_data.login_choice_identifier.assign("1");
+  EXPECT_TRUE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+}
+
+TEST_F(CollectUserDataActionTest, UserDataComplete_ShippingAddress) {
+  UserData user_data;
+  CollectUserDataOptions options;
+  options.request_shipping = true;
+  EXPECT_FALSE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+
+  // Incomplete address.
+  user_data.shipping_address = std::make_unique<autofill::AutofillProfile>(
+      base::GenerateGUID(), kFakeUrl);
+  autofill::test::SetProfileInfo(user_data.shipping_address.get(), "Marion",
+                                 "Mitchell", "Morrison", "marion@me.xyz", "Fox",
+                                 "123 Zoo St.", "unit 5", "Hollywood", "CA",
+                                 /* zipcode = */ "", "US", "16505678910");
+  EXPECT_FALSE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
+
+  user_data.shipping_address->SetRawInfo(autofill::ADDRESS_HOME_ZIP,
+                                         base::UTF8ToUTF16("91601"));
+  EXPECT_TRUE(CollectUserDataAction::IsUserDataComplete(
+      &mock_personal_data_manager_, user_data, options));
 }
 
 }  // namespace
