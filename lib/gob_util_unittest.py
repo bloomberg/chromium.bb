3cf0a3b0963b786c32a695f514b7157d6974d601
Jack Rosenthal <jrosenth@chromium.org>
2019-10-08 17:12:53 -0600

    lib/gob_util_unittest: remove HTTP 409 test
    Gerrit seems to no longer respond with code 409 for this condition
    anymore. Remove the corresponding network test.
    
    BUG=none
    TEST=lib/gob_util_unittest --network
    
    Change-Id: I951f321d2d8c45218eef6415b0e7f7c2af006a9a
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/1848543
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- lib/gob_util_unittest.py
+++ lib/gob_util_unittest.py
@@ -160,17 +160,6 @@ class NetworkGobTest(cros_test_lib.TestCase):
                             'foo/bar/baz', ignore_404=False)
     self.assertEqual(ex.exception.http_status, 404)
 
-  def test409Exception(self):
-    """Test FetchUrlJson raises 409 errors with response body message."""
-    with self.assertRaises(gob_util.GOBError) as ex:
-      gob_util.FetchUrlJson(
-          config_lib.GetSiteParams().EXTERNAL_GERRIT_HOST,
-          'changes/422652/revisions/901b4ee349a9395ba23a7a1e8597a35050f741e4/'
-          'review', reqtype='POST', body={'labels': {'Commit-Ready': '+1'}})
-
-    self.assertEqual(ex.exception.http_status, 409)
-    self.assertEqual(ex.exception.reason,
-                     gob_util.GOB_ERROR_REASON_CLOSED_CHANGE)
 
 def main(_argv):
   gob_util.TRY_LIMIT = 1
