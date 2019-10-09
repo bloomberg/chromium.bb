623581709db3cc417f240fad2dadbe443b5ce302
Makoto Shimazu <shimazu@chromium.org>
2019-10-09 13:33:44 +0000

    Convert base::Bind to base::Bind{Once,Repeating} in content/browser
    This CL converts base::Bind to BindOnce or BindRepeating, and also changes
    relevant base::Callbacks or base::Closures to appropriate types.
    
    This CL is split from https://crrev.com/1831621.
    
    This CL was uploaded by git cl split.
    
    R=pwnall@chromium.org
    
    Bug: 1007760
    Change-Id: I23b431cb679d87b3d22fd2cd1c510ac867b5bb7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1849543
    Auto-Submit: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704163}
    
    Former-commit-id: 9c9fa37dee85b3c13ef78caa02d73f00d3da61f8

--- content/browser/appcache/appcache_group.cc
+++ content/browser/appcache/appcache_group.cc
@@ -232,7 +232,7 @@ bool AppCacheGroup::FindObserver(
 void AppCacheGroup::ScheduleUpdateRestart(int delay_ms) {
   DCHECK(restart_update_task_.IsCancelled());
   restart_update_task_.Reset(
-      base::Bind(&AppCacheGroup::RunQueuedUpdates, this));
+      base::BindOnce(&AppCacheGroup::RunQueuedUpdates, this));
   base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
       FROM_HERE, restart_update_task_.callback(),
       base::TimeDelta::FromMilliseconds(delay_ms));
