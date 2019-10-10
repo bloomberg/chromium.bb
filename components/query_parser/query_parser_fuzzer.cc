4e6259df7e9a2a08eb3fd07e2b872dbd6369cf39
Daniel McArdle <dmcardle@chromium.org>
2019-10-10 13:58:52 +0000

    Add query_parser_fuzzer for component query_parser::QueryParser
    Bug: 1012848
    Change-Id: If0b9c22bcf3d8341d826dfbc718fe47ebb6328ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1850432
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704609}
    
    Former-commit-id: f4d4c68787bb920eff8c9265d3cff6948ff8187b

---
+++ components/query_parser/query_parser_fuzzer.cc
@@ -0,0 +1,25 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <fuzzer/FuzzedDataProvider.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include "base/strings/utf_string_conversions.h"
+#include "components/query_parser/query_parser.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  FuzzedDataProvider data_provider(data, size);
+
+  const query_parser::MatchingAlgorithm matching_alg =
+      data_provider.ConsumeEnum<query_parser::MatchingAlgorithm>();
+  const base::string16 query16(base::UTF8ToUTF16(
+      data_provider.ConsumeBytesAsString(data_provider.remaining_bytes())));
+
+  query_parser::QueryParser parser;
+  std::vector<base::string16> words;
+  parser.ParseQueryWords(query16, matching_alg, &words);
+
+  return 0;
+}
