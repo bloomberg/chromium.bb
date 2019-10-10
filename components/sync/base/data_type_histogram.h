81e651935c778e30f958699ae538b05bdd1f152c
Marc Treib <treib@chromium.org>
2019-10-10 15:20:21 +0000

    Sync: Remove deprecated *NOTIFICATION* types (again)
    This is a partial reland of crrev.com/c/1821520 and crrev.com/c/1824261
    (or a partial revert of crrev.com/c/1847290).
    It removed the ModelType enum entries (again), but leaves the actual
    proto files intact (other than marking related fields as deprecated),
    since they're still needed by the server for now.
    
    Bug: 1012648
    Change-Id: Icd963d6e244d424643377f8defb3aedcce81f763
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1852446
    Auto-Submit: Marc Treib <treib@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: Tatiana Gornak <melandory@chromium.org>
    Commit-Queue: Tatiana Gornak <melandory@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704638}
    
    Former-commit-id: 87416af9f6af00a30ea09d4de10dd64db6b0f5a9

--- components/sync/base/data_type_histogram.h
+++ components/sync/base/data_type_histogram.h
@@ -89,17 +89,8 @@ void SyncRecordModelTypeCountHistogram(syncer::ModelType model_type,
       case ::syncer::EXTENSION_SETTINGS:                       \
         PER_DATA_TYPE_MACRO("ExtensionSettings");              \
         break;                                                 \
-      case ::syncer::DEPRECATED_APP_NOTIFICATIONS:             \
-        PER_DATA_TYPE_MACRO("AppNotifications");               \
-        break;                                                 \
       case ::syncer::HISTORY_DELETE_DIRECTIVES:                \
         PER_DATA_TYPE_MACRO("HistoryDeleteDirectives");        \
-        break;                                                 \
-      case ::syncer::DEPRECATED_SYNCED_NOTIFICATIONS:          \
-        PER_DATA_TYPE_MACRO("SyncedNotifications");            \
-        break;                                                 \
-      case ::syncer::DEPRECATED_SYNCED_NOTIFICATION_APP_INFO:  \
-        PER_DATA_TYPE_MACRO("SyncedNotificationAppInfo");      \
         break;                                                 \
       case ::syncer::DICTIONARY:                               \
         PER_DATA_TYPE_MACRO("Dictionary");                     \
