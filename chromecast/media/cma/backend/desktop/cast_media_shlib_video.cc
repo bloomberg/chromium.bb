ecf3702828ff1fb714cde73e8f00b2f1a74e4e65
Bryan Clark <bryanclark@google.com>
2019-10-10 23:16:09 +0000

    Create a video plane in the cast desktop media library.
    This allows callers to deference the result of
    CastMediaShlib::GetVideoPlane() without crashing cast_shell.
    
    Change-Id: I09b8a761359736195e42bec1708174a4e939a69f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1854186
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Commit-Queue: Bryan Clark <bryanclark@google.com>
    Cr-Commit-Position: refs/heads/master@{#704876}
    
    Former-commit-id: 9e773cc7021dd65015351c1ae652bd23d22ced9f

--- chromecast/media/cma/backend/desktop/cast_media_shlib_video.cc
+++ chromecast/media/cma/backend/desktop/cast_media_shlib_video.cc
@@ -5,16 +5,26 @@
 #include <vector>
 #include <string>
 
+#include "base/no_destructor.h"
 #include "chromecast/public/cast_media_shlib.h"
 #include "chromecast/public/media/media_capabilities_shlib.h"
+#include "chromecast/public/video_plane.h"
 
 namespace chromecast {
 namespace media {
 
+class DesktopVideoPlane : public VideoPlane {
+ public:
+  ~DesktopVideoPlane() override = default;
+
+  void SetGeometry(const RectF& display_rect, Transform transform) override {}
+};
+
 void CastMediaShlib::Initialize(const std::vector<std::string>& argv) {}
 
 VideoPlane* CastMediaShlib::GetVideoPlane() {
-  return nullptr;
+  static base::NoDestructor<DesktopVideoPlane> g_video_plane;
+  return g_video_plane.get();
 }
 
 bool MediaCapabilitiesShlib::IsSupportedVideoConfig(VideoCodec codec,
