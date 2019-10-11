ae3b20e87a8424a94462a9c92c6b7eeac11f44b6
ckitagawa <ckitagawa@chromium.org>
2019-10-11 20:17:14 +0000

    [Zucchini] Add ELF fuzzer
    This CL introduces a fuzzer for the ELF disassemblers in Zucchini. I
    have already uploaded some corpus files to the clusterfuzz-corpus
    Google Storage bucket.
    
    Achieves ~700 exec/s locally. This is on-par with the DEX and Win32
    disassemblers as it requires a largish representative file to serve
    as a test. (Recommendation is ~1000 exec/s).
    
    Brings up coverage of ELF related code from 0-30% to 80-100%
    
    I expect this will find quite a few crashes early on but should
    stabilize within a couple of weeks; est. 1-2 bugfixes per day
    based on how DEX and Win32 went. Mostly checked_casts and bounds
    issues.
    
    Bug: 1013641
    Change-Id: I205135547cad2a95e59f99d7f040c13d72c45b59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1856624
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705245}
    
    Former-commit-id: b00aaabae0e86b84d0007f76f7736fe2db397aaf

--- components/zucchini/fuzzers/disassembler_elf_fuzzer.cc
+++ components/zucchini/fuzzers/disassembler_elf_fuzzer.cc
@@ -1,31 +1,24 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
+// Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include <stddef.h>
 #include <stdint.h>
 
-#include <map>
-#include <memory>
-#include <vector>
-
 #include "base/logging.h"
 #include "components/zucchini/buffer_view.h"
 #include "components/zucchini/disassembler.h"
-#include "components/zucchini/disassembler_win32.h"
+#include "components/zucchini/disassembler_elf.h"
 #include "components/zucchini/fuzzers/fuzz_utils.h"
 
 namespace {
 
 struct Environment {
-  Environment() {
-    logging::SetMinLogLevel(logging::LOG_FATAL);  // Disable console spamming.
-  }
+  Environment() { logging::SetMinLogLevel(logging::LOG_FATAL); }
 };
 
 }  // namespace
 
-// Entry point for LibFuzzer.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   static Environment env;
   if (!size)
@@ -34,19 +27,19 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   std::vector<uint8_t> mutable_data(data, data + size);
   zucchini::ConstBufferView image(mutable_data.data(), mutable_data.size());
 
-  // One of x86 or x64 should return a non-nullptr if the data is valid.
-  auto disassembler_win32x86 =
-      zucchini::Disassembler::Make<zucchini::DisassemblerWin32X86>(image);
-  if (disassembler_win32x86) {
-    zucchini::ReadAndWriteReferences(std::move(disassembler_win32x86),
+  // Create disassembler. Early exit on failure.
+  auto disassembler_elf_x64 =
+      zucchini::Disassembler::Make<zucchini::DisassemblerElfX64>(image);
+  if (disassembler_elf_x64) {
+    zucchini::ReadAndWriteReferences(std::move(disassembler_elf_x64),
                                      &mutable_data);
     return 0;
   }
 
-  auto disassembler_win32x64 =
-      zucchini::Disassembler::Make<zucchini::DisassemblerWin32X64>(image);
-  if (disassembler_win32x64)
-    zucchini::ReadAndWriteReferences(std::move(disassembler_win32x64),
+  auto disassembler_elf_x86 =
+      zucchini::Disassembler::Make<zucchini::DisassemblerElfX86>(image);
+  if (disassembler_elf_x86)
+    zucchini::ReadAndWriteReferences(std::move(disassembler_elf_x86),
                                      &mutable_data);
   return 0;
 }
