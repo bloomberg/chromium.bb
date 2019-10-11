e8d0b74cc1f63eb743d17fe1c902e67d6d9a7439
Lei Zhang <thestig@chromium.org>
2019-10-11 21:54:20 +0000

    Disable flaky TabStackTraceTest.testCrashSymbols on Linux.
    Also disable TabStackTraceTest.testCrashMinimalSymbols for the same
    reason.
    
    TBR=crouleau@chromium.org
    
    Bug: 820282
    Change-Id: Ib7096d1fdb97b3e870b82b3d41deb188989d8727
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1856850
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705294}
    
    Former-commit-id: f8c62422794ff23a10b139b8ea43a7ceefeb1f6b

--- tools/perf/core/stacktrace_unittest.py
+++ tools/perf/core/stacktrace_unittest.py
@@ -23,8 +23,8 @@ class TabStackTraceTest(tab_test_case.TabTestCase):
     self.assertTrue(c.exception.is_valid_dump)
 
   # Stack traces aren't working on Android yet.
-  # Disabled on mac, flaky: https://crbug.com/820282.
-  @decorators.Enabled('linux')
+  # Disabled on Linux and Mac, flaky: https://crbug.com/820282.
+  @decorators.Disabled('all')
   def testCrashSymbols(self):
     with self.assertRaises(exceptions.DevtoolsTargetCrashException) as c:
       self._tab.Navigate('chrome://crash', timeout=5)
@@ -33,9 +33,8 @@ class TabStackTraceTest(tab_test_case.TabTestCase):
   # Some platforms do not support full stack traces, this test requires only
   # minimal symbols to be available.
   # Disabled on win due to crbug.com/706328.
-  # Disabled on mac, flaky: https://crbug.com/820282.
-  @decorators.Enabled('linux')
-  @decorators.Disabled('mac', 'win')
+  # Disabled on Linux and Mac, flaky: https://crbug.com/820282.
+  @decorators.Disabled('all')
   def testCrashMinimalSymbols(self):
     with self.assertRaises(exceptions.DevtoolsTargetCrashException) as c:
       self._tab.Navigate('chrome://crash', timeout=5)
