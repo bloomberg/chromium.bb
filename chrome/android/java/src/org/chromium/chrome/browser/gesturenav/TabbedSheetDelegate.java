015b4adff12bb57706658d65bb2b83cb92f7be5e
Jinsuk Kim <jinsukkim@chromium.org>
2019-10-13 22:49:10 +0000

    Android: Close gesture navigation sheet upon tab closure
    Navigation Sheet should be closed when its Tab is closed. In most cases
    the sheet is close prior to Tab, but the opposite can happen if incognito
    tabs are closed via Android notification. This CL ensures the sheet gets
    closed upon tab closure.
    
    (cherry picked from commit 58d0d5c52f35bbb3f1b4e884aa64b78dffdf9e98 [formerly f21ce8810cd2e9aafc8f173699ae1571fffc4da8])
    
    Bug: 1011073
    Change-Id: Id9729c2d8987bfe37ff81f47a8070d40a86e00e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1842134
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#704483}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1858544
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3904@{#743}
    Cr-Branched-From: 9035e7d56a589711638d7ffdc83ffa6d674a06ec [formerly 675968a8c657a3bd9c1c2c20c5d2935577bbc5e6]-refs/heads/master@{#693954}
    
    Former-commit-id: c7e28be4ccebc59eb2a0c974a6875e1920b6908b

--- chrome/android/java/src/org/chromium/chrome/browser/gesturenav/TabbedSheetDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/gesturenav/TabbedSheetDelegate.java
@@ -6,6 +6,7 @@ package org.chromium.chrome.browser.gesturenav;
 
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.history.HistoryManagerUtils;
+import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.util.UrlConstants;
 import org.chromium.content_public.browser.NavigationEntry;
@@ -21,6 +22,25 @@ public class TabbedSheetDelegate implements NavigationSheet.Delegate {
 
     private final Tab mTab;
     private final String mFullHistoryMenu;
+
+    // TabObserver that monitors tab closing event to close the navigation sheet
+    // together if open. For now this is necessary when closing all incognito tabs
+    // through Android notification.
+    private static class CloseTabObserver extends EmptyTabObserver {
+        private Runnable mCloseRunnable;
+
+        public void setCloseRunnable(Runnable runnable) {
+            mCloseRunnable = runnable;
+        }
+
+        @Override
+        public void onDestroyed(Tab tab) {
+            mCloseRunnable.run();
+            tab.removeObserver(this);
+        }
+    }
+
+    private final CloseTabObserver mCloseTabObserver = new CloseTabObserver();
 
     public TabbedSheetDelegate(Tab tab) {
         mTab = tab;
@@ -43,6 +63,16 @@ public class TabbedSheetDelegate implements NavigationSheet.Delegate {
             HistoryManagerUtils.showHistoryManager(mTab.getActivity(), mTab);
         } else {
             mTab.getWebContents().getNavigationController().goToNavigationIndex(index);
+        }
+    }
+
+    @Override
+    public void setTabCloseRunnable(Runnable runnable) {
+        if (runnable != null) {
+            mCloseTabObserver.setCloseRunnable(runnable);
+            mTab.addObserver(mCloseTabObserver);
+        } else {
+            mTab.removeObserver(mCloseTabObserver);
         }
     }
 }
