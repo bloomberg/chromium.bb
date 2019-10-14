69c2ba87dde0e473ac2d8ee2796f76ab75c5ea0d
Mike Frysinger <vapier@chromium.org>
2019-10-14 11:41:14 -0600

    cros lint: Update utf-8 BOM check
    Now that ReadFile returns a UTF-8 string, update the logic to operate
    on Unicode codepoints instead of bytes.
    
    BUG=chromium:1014125
    TEST=unittests pass.
    
    Change-Id: Iad70de47160937b37e48cfedf57684ce0146f5fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/1860367
    Tested-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: LaMont Jones <lamontjones@chromium.org>
    Commit-Queue: LaMont Jones <lamontjones@chromium.org>

--- cli/cros/cros_lint.py
+++ cli/cros/cros_lint.py
@@ -193,8 +193,8 @@ def _JsonLintFile(path, _output_format, _debug):
   data = osutils.ReadFile(path)
 
   # Strip off leading UTF-8 BOM if it exists.
-  if data.startswith(b'\xef\xbb\xbf'):
-    data = data[3:]
+  if data.startswith(u'\ufeff'):
+    data = data[1:]
 
   # Strip out comments for JSON parsing.
   stripped_data = re.sub(r'^\s*#.*', '', data, flags=re.M)
--- cli/cros/cros_lint_unittest.py
+++ cli/cros/cros_lint_unittest.py
@@ -7,8 +7,14 @@
 
 from __future__ import print_function
 
+import os
+
 from chromite.cli.cros import cros_lint
 from chromite.lib import cros_test_lib
+from chromite.lib import osutils
+
+
+# pylint: disable=protected-access
 
 
 class LintCommandTest(cros_test_lib.TestCase):
@@ -19,3 +25,28 @@ class LintCommandTest(cros_test_lib.TestCase):
     self.assertEqual(
         set(cros_lint.LintCommand.OUTPUT_FORMATS),
         set(cros_lint.CPPLINT_OUTPUT_FORMAT_MAP.keys() + ['default']))
+
+
+class JsonTest(cros_test_lib.TempDirTestCase):
+  """Tests for _JsonLintFile."""
+
+  def testValid(self):
+    """Verify valid json file is accepted."""
+    path = os.path.join(self.tempdir, 'x.json')
+    osutils.WriteFile(path, '{}\n')
+    ret = cros_lint._JsonLintFile(path, None, None)
+    self.assertEqual(ret.returncode, 0)
+
+  def testInvalid(self):
+    """Verify invalid json file is rejected."""
+    path = os.path.join(self.tempdir, 'x.json')
+    osutils.WriteFile(path, '{')
+    ret = cros_lint._JsonLintFile(path, None, None)
+    self.assertEqual(ret.returncode, 1)
+
+  def testUnicodeBom(self):
+    """Verify we skip the Unicode BOM."""
+    path = os.path.join(self.tempdir, 'x.json')
+    osutils.WriteFile(path, b'\xef\xbb\xbf{}\n', mode='wb')
+    ret = cros_lint._JsonLintFile(path, None, None)
+    self.assertEqual(ret.returncode, 0)
