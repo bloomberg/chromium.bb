09594608cb9e1f94e76d7c0759412dffe0b9d91e
Hans Wennborg <hans@chromium.org>
2019-10-14 11:54:49 +0000

    Reduce include bloat in content/public/common/previews_state.h
    Reducing the preprocessed include size from 514,119 to 101 tokens.
    
    Bug: 1014009
    Change-Id: I2eba8848545dd95dcd2da105580b45d6be0e21f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1856964
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705519}
    
    Former-commit-id: 5ffc4694495b26a6abf0a7bd3ad4102c69f670d3

--- content/public/common/previews_state.h
+++ content/public/common/previews_state.h
@@ -5,13 +5,6 @@
 #ifndef CONTENT_PUBLIC_COMMON_PREVIEWS_STATE_H_
 #define CONTENT_PUBLIC_COMMON_PREVIEWS_STATE_H_
 
-#include "content/common/content_export.h"
-#include "third_party/blink/public/platform/web_url_request.h"
-
-#define STATIC_ASSERT_PREVIEWS_ENUM(a, b)                   \
-  static_assert(static_cast<int>(a) == static_cast<int>(b), \
-                "mismatching enums: " #a)
-
 namespace content {
 
 typedef int PreviewsState;
@@ -64,31 +57,6 @@ enum PreviewsTypes {
 const PreviewsState ALL_SUPPORTED_PREVIEWS =
     SERVER_LITE_PAGE_ON | NOSCRIPT_ON | RESOURCE_LOADING_HINTS_ON |
     OFFLINE_PAGE_ON | LITE_PAGE_REDIRECT_ON;
-
-// Ensure that content::PreviewsState and blink::WebURLRequest::PreviewsState
-// are kept in sync.
-STATIC_ASSERT_PREVIEWS_ENUM(PREVIEWS_UNSPECIFIED,
-                            blink::WebURLRequest::kPreviewsUnspecified);
-STATIC_ASSERT_PREVIEWS_ENUM(CLIENT_LOFI_AUTO_RELOAD,
-                            blink::WebURLRequest::kClientLoFiAutoReload);
-STATIC_ASSERT_PREVIEWS_ENUM(SERVER_LITE_PAGE_ON,
-                            blink::WebURLRequest::kServerLitePageOn);
-STATIC_ASSERT_PREVIEWS_ENUM(PREVIEWS_NO_TRANSFORM,
-                            blink::WebURLRequest::kPreviewsNoTransform);
-STATIC_ASSERT_PREVIEWS_ENUM(PREVIEWS_OFF, blink::WebURLRequest::kPreviewsOff);
-STATIC_ASSERT_PREVIEWS_ENUM(NOSCRIPT_ON, blink::WebURLRequest::kNoScriptOn);
-STATIC_ASSERT_PREVIEWS_ENUM(RESOURCE_LOADING_HINTS_ON,
-                            blink::WebURLRequest::kResourceLoadingHintsOn);
-STATIC_ASSERT_PREVIEWS_ENUM(OFFLINE_PAGE_ON,
-                            blink::WebURLRequest::kOfflinePageOn);
-STATIC_ASSERT_PREVIEWS_ENUM(LITE_PAGE_REDIRECT_ON,
-                            blink::WebURLRequest::kLitePageRedirectOn);
-STATIC_ASSERT_PREVIEWS_ENUM(LAZY_IMAGE_LOAD_DEFERRED,
-                            blink::WebURLRequest::kLazyImageLoadDeferred);
-STATIC_ASSERT_PREVIEWS_ENUM(LAZY_IMAGE_AUTO_RELOAD,
-                            blink::WebURLRequest::kLazyImageAutoReload);
-STATIC_ASSERT_PREVIEWS_ENUM(PREVIEWS_STATE_LAST,
-                            blink::WebURLRequest::kPreviewsStateLast);
 
 }  // namespace content
 
