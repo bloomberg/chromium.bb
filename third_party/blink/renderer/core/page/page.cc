36e77e84517f4e4748c458cc235390eecd544b26
Dave Tapuska <dtapuska@chromium.org>
2019-10-15 19:59:40 +0000

    Fix crashes from bindings checks that the agent clusters are the same
    If a Webview app delays setting the allowUniversalAccessFromFileURLs it is
    possible that further accesses will fail because they are different
    agent clusters. Grant corss agent cluster access.
    
    BUG=1006684
    TBR=dtapuska@chromium.org
    
    (cherry picked from commit 884168d65754b45d45ab5725ac90714048df4c8e [formerly ecfcd3c35fb4aa0e27d488582bf11d638fb704e1])
    
    Change-Id: Ic19b5ae26ffc03747f5d352f824a00e2deda776a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1857301
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#706096}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1863117
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3904@{#773}
    Cr-Branched-From: 9035e7d56a589711638d7ffdc83ffa6d674a06ec [formerly 675968a8c657a3bd9c1c2c20c5d2935577bbc5e6]-refs/heads/master@{#693954}
    
    Former-commit-id: e47f2d3964b69ea91e2bc2b62df5572403eec299

--- third_party/blink/renderer/core/page/page.cc
+++ third_party/blink/renderer/core/page/page.cc
@@ -754,6 +754,22 @@ void Page::SettingsChanged(SettingsDelegate::ChangeType change_type) {
         GetSpatialNavigationController().OnSpatialNavigationSettingChanged();
       }
       break;
+    case SettingsDelegate::kUniversalAccessChange: {
+      if (!GetSettings().GetAllowUniversalAccessFromFileURLs())
+        break;
+      for (Frame* frame = MainFrame(); frame;
+           frame = frame->Tree().TraverseNext()) {
+        // If we got granted universal access from file urls we need to grant
+        // any outstanding security origin cross agent cluster access since
+        // newly allocated agent clusters will be the universal agent.
+        if (auto* local_frame = DynamicTo<LocalFrame>(frame)) {
+          local_frame->GetDocument()
+              ->GetMutableSecurityOrigin()
+              ->GrantCrossAgentClusterAccess();
+        }
+      }
+      break;
+    }
   }
 }
 
