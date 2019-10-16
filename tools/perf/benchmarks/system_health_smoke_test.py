346c9729311de9eaa0d578cd82789cf41e1dc8e7
Parastoo Geranmayeh <parastoog@google.com>
2019-10-16 12:27:04 +0000

    [Sheriff] Disable SystemHealthBenchmarkSmokeTest
    Test is flaky for search/india and social. See bug.
    
    TBR=sullivan@chromium.org
    
    Bug: 1014661
    Change-Id: I2c098f543ed2f6acd2575e821a21328e474735c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1863379
    Reviewed-by: Parastoo Geranmayeh <parastoog@google.com>
    Commit-Queue: Parastoo Geranmayeh <parastoog@google.com>
    Cr-Commit-Position: refs/heads/master@{#706403}
    
    Former-commit-id: 2c305063d687a25797247465bbe2bbc1625a799f

--- tools/perf/benchmarks/system_health_smoke_test.py
+++ tools/perf/benchmarks/system_health_smoke_test.py
@@ -170,6 +170,10 @@ _DISABLED_TESTS = frozenset({
   'system_health.memory_desktop/browse:tools:sheets:2019',
   'system_health.memory_desktop/browse:tools:maps:2019',
 
+  # crbug.com/1014661
+  'system_health.memory_desktop/browse:social:tumblr_infinite_scroll:2018'
+  'system_health.memory_desktop/browse:search:google_india:2018'
+
   # The following tests are disabled because they are disabled on the perf
   # waterfall (using tools/perf/expectations.config) on one platform or another.
   # They may run fine on the CQ, but it isn't worth the bot time to run them.
