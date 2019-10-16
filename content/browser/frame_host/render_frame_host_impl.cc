8b0ae965c74d8a2af0f825e28d4601338da22a0f
Andrey Kosyakov <caseq@chromium.org>
2019-10-16 20:02:14 +0000

    Do not reset NS connection error holder upon navigation
    network_service_connection_error_handler_holder_ is used to
    update loader factories in page upon network service failure,
    so it should not be cleared upon navigation.
    
    Change-Id: I2103d633e47525ee8d8b33aa91324cb6c11ce9c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1863857
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706578}
    
    Former-commit-id: f5464fe929b75e1ed57b34e1197775e477192ccf

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -4693,7 +4693,6 @@ void RenderFrameHostImpl::ResetWaitingState() {
   }
   send_before_unload_start_time_ = base::TimeTicks();
   render_view_host_->is_waiting_for_close_ack_ = false;
-  network_service_connection_error_handler_holder_.reset();
 }
 
 CanCommitStatus RenderFrameHostImpl::CanCommitOriginAndUrl(
