bb7258c68b18acf30968717b40cc94b7577937ba
Ken Rockot <rockot@google.com>
2019-10-16 23:21:24 +0000

    Introduce StandaloneConnectorImpl
    This is an implementation of the Service Manager's Connector mojom
    interface which can be used in isolation without any dependency on an
    actual Service Manager instance. It serves as a transitional helper for
    code to migrate away from Service Manager APIs incrementally.
    
    Bug: 977637
    Change-Id: I98e7edabfe36f98f0d9e817e3b8d8c4de23cb67c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1854210
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#706694}
    
    Former-commit-id: 2e4c0dc75d67822917e1f858418a955d48aee870

---
+++ services/service_manager/public/cpp/standalone_connector_impl.cc
@@ -0,0 +1,74 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/service_manager/public/cpp/standalone_connector_impl.h"
+
+#include "base/logging.h"
+
+namespace service_manager {
+
+StandaloneConnectorImpl::StandaloneConnectorImpl(Delegate* delegate)
+    : delegate_(delegate) {
+  DCHECK(delegate_);
+}
+
+StandaloneConnectorImpl::~StandaloneConnectorImpl() = default;
+
+mojo::PendingRemote<mojom::Connector> StandaloneConnectorImpl::MakeRemote() {
+  mojo::PendingRemote<mojom::Connector> remote;
+  receivers_.Add(this, remote.InitWithNewPipeAndPassReceiver());
+  return remote;
+}
+
+void StandaloneConnectorImpl::BindInterface(
+    const ServiceFilter& filter,
+    const std::string& interface_name,
+    mojo::ScopedMessagePipeHandle interface_pipe,
+    mojom::BindInterfacePriority priority,
+    BindInterfaceCallback callback) {
+  delegate_->OnConnect(
+      filter.service_name(),
+      mojo::GenericPendingReceiver(interface_name, std::move(interface_pipe)));
+  std::move(callback).Run(mojom::ConnectResult::SUCCEEDED, base::nullopt);
+}
+
+void StandaloneConnectorImpl::QueryService(const std::string& service_name,
+                                           QueryServiceCallback callback) {
+  NOTIMPLEMENTED()
+      << "QueryService is not supported by StandaloneConnectorImpl.";
+  std::move(callback).Run(nullptr);
+}
+
+void StandaloneConnectorImpl::WarmService(const ServiceFilter& filter,
+                                          WarmServiceCallback callback) {
+  NOTIMPLEMENTED()
+      << "WarmService is not supported by StandaloneConnectorImpl.";
+  std::move(callback).Run(mojom::ConnectResult::INVALID_ARGUMENT,
+                          base::nullopt);
+}
+
+void StandaloneConnectorImpl::RegisterServiceInstance(
+    const Identity& identity,
+    mojo::ScopedMessagePipeHandle service_pipe,
+    mojo::PendingReceiver<mojom::ProcessMetadata> metadata_receiver,
+    RegisterServiceInstanceCallback callback) {
+  NOTIMPLEMENTED()
+      << "RegisterServiceInstance is not supported by StandaloneConnectorImpl.";
+  std::move(callback).Run(mojom::ConnectResult::INVALID_ARGUMENT);
+}
+
+void StandaloneConnectorImpl::Clone(mojom::ConnectorRequest request) {
+  receivers_.Add(this, std::move(request));
+}
+
+void StandaloneConnectorImpl::FilterInterfaces(
+    const std::string& spec,
+    const Identity& source,
+    mojom::InterfaceProviderRequest source_request,
+    mojom::InterfaceProviderPtr target) {
+  NOTIMPLEMENTED()
+      << "FilterInterfaces is not supported by StandaloneConnectorImpl.";
+}
+
+}  // namespace service_manager
