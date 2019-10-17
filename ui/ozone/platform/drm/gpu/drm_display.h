e03c89e5121e2c7f482235bb9bcf78442fae9251
Daniele Castagna <dcastagna@chromium.org>
2019-10-17 20:30:56 +0000

    ozone/drm: Cache connector drm_display
    There is some stuttering when viewing protected content with a DP MST
    dongle attached.
    
    Looking at a trace it looks like we spend a lot of time (200ms)
    in DrmDisplay::GetHDCPState, in particular in drm_->GetConnector.
    
    This patch caches the connector used to check if HDCP is available.
    
    It additionally downgrades log severity when the HDCP properties
    is not found to INFO, since it can happen every 2 seconds.
    
    Bug: 1001211
    Change-Id: I77eb86b2e55f0fd4640560ae2e38d64d5e2b2174
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1864354
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Commit-Queue: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707065}
    
    Former-commit-id: ad7dbe97a1b3b8e203b51462ddbbd6b076903544

--- ui/ozone/platform/drm/gpu/drm_display.h
+++ ui/ozone/platform/drm/gpu/drm_display.h
@@ -15,6 +15,7 @@
 #include "ui/display/types/display_constants.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/ozone/common/gpu/ozone_gpu_message_params.h"
+#include "ui/ozone/platform/drm/common/scoped_drm_types.h"
 
 typedef struct _drmModeModeInfo drmModeModeInfo;
 
@@ -38,7 +39,7 @@ class DrmDisplay {
   int64_t display_id() const { return display_id_; }
   scoped_refptr<DrmDevice> drm() const { return drm_; }
   uint32_t crtc() const { return crtc_; }
-  uint32_t connector() const { return connector_; }
+  uint32_t connector() const;
   const std::vector<drmModeModeInfo>& modes() const { return modes_; }
 
   std::unique_ptr<display::DisplaySnapshot> Update(
@@ -60,7 +61,7 @@ class DrmDisplay {
   int64_t display_id_ = -1;
   const scoped_refptr<DrmDevice> drm_;
   uint32_t crtc_ = 0;
-  uint32_t connector_ = 0;
+  ScopedDrmConnectorPtr connector_;
   std::vector<drmModeModeInfo> modes_;
   gfx::Point origin_;
 
