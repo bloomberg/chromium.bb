59a53704f508ce05bd9d4679d7d203b4cc73e676
Justin Cohen <justincohen@google.com>
2019-10-17 04:15:10 +0000

    [ios] Migrate ui/ntp to EG2.
    Bug: 987646
    Change-Id: Iaa13ee085aec001158ac46151a50d5ab3cbfa46c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1862994
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706781}
    
    Former-commit-id: 67407619b20c95aab26f3b9ce28da4158901e109

--- ios/chrome/browser/ui/ntp/new_tab_page_egtest.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_egtest.mm
@@ -2,22 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <EarlGrey/EarlGrey.h>
-#import <XCTest/XCTest.h>
-
 #import "base/test/ios/wait_util.h"
 #include "base/test/scoped_command_line.h"
 #include "base/test/scoped_feature_list.h"
 #include "components/strings/grit/components_strings.h"
-#include "ios/chrome/browser/chrome_switches.h"
-#import "ios/chrome/browser/ui/commands/browser_commands.h"
 #import "ios/chrome/browser/ui/content_suggestions/ntp_home_constant.h"
-#include "ios/chrome/browser/ui/util/ui_util.h"
 #include "ios/chrome/grit/ios_strings.h"
-#import "ios/chrome/test/app/chrome_test_util.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
+#import "ios/testing/earl_grey/earl_grey_test.h"
 #include "ui/base/l10n/l10n_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -28,8 +22,8 @@ namespace {
 
 // Pauses until the history label has disappeared.  History should not show on
 // incognito.
-void WaitForHistoryToDisappear() {
-  [[GREYCondition
+BOOL WaitForHistoryToDisappear() {
+  return [[GREYCondition
       conditionWithName:@"Wait for history to disappear"
                   block:^BOOL {
                     NSError* error = nil;
@@ -60,7 +54,7 @@ void WaitForHistoryToDisappear() {
 // Tests that all items are accessible on the incognito page.
 - (void)testAccessibilityOnIncognitoTab {
   [ChromeEarlGrey openNewIncognitoTab];
-  WaitForHistoryToDisappear();
+  GREYAssert(WaitForHistoryToDisappear(), @"History did not disappear.");
   [ChromeEarlGrey verifyAccessibilityForCurrentScreen];
   [ChromeEarlGrey closeAllIncognitoTabs];
 }
