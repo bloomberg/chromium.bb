efa99158bbbceb12b98b60fac58853a4e2440e99
Tommy Steimel <steimel@chromium.org>
2019-10-17 03:25:02 +0000

    GMC: Add separator lines between notifications
    This CL uses borders to separate notifications in the media dialog.
    
    Bug: 1004707
    Change-Id: I3c129e46ac1c855a906ac0577fcd092f9087c576
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1864096
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Jazz Xu <jazzhsu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706770}
    
    Former-commit-id: 024fabd585c8f2ce81f4657059bdc44e557c2445

--- chrome/browser/ui/views/global_media_controls/media_notification_list_view.cc
+++ chrome/browser/ui/views/global_media_controls/media_notification_list_view.cc
@@ -5,12 +5,26 @@
 #include "chrome/browser/ui/views/global_media_controls/media_notification_list_view.h"
 
 #include "chrome/browser/ui/views/global_media_controls/media_notification_container_impl_view.h"
+#include "ui/views/border.h"
 #include "ui/views/controls/scrollbar/overlay_scroll_bar.h"
 #include "ui/views/layout/box_layout.h"
 
 namespace {
 
 constexpr int kMediaListMaxHeight = 478;
+
+// Color for border between notifications.
+constexpr SkColor kMediaListSeparatorColor = SkColorSetA(SK_ColorBLACK, 31);
+
+// Thickness of separator border.
+constexpr int kMediaListSeparatorThickness = 2;
+
+std::unique_ptr<views::Border> CreateMediaListSeparatorBorder() {
+  return views::CreateSolidSidedBorder(/*top=*/kMediaListSeparatorThickness,
+                                       /*left=*/0,
+                                       /*bottom=*/0,
+                                       /*right=*/0, kMediaListSeparatorColor);
+}
 
 }  // anonymous namespace
 
@@ -35,6 +49,11 @@ void MediaNotificationListView::ShowNotification(
   DCHECK(!base::Contains(notifications_, id));
   DCHECK_NE(nullptr, notification.get());
 
+  // If this isn't the first notification, then create a top-sided separator
+  // border.
+  if (!notifications_.empty())
+    notification->SetBorder(CreateMediaListSeparatorBorder());
+
   notifications_[id] = contents()->AddChildView(std::move(notification));
 
   contents()->InvalidateLayout();
@@ -45,7 +64,18 @@ void MediaNotificationListView::HideNotification(const std::string& id) {
   if (!base::Contains(notifications_, id))
     return;
 
+  // If we're removing the topmost notification and there are others, then we
+  // need to remove the top-sided separator border from the new topmost
+  // notification.
+  if (contents()->children().size() > 1 &&
+      contents()->children().at(0) == notifications_[id]) {
+    contents()->children().at(1)->SetBorder(nullptr);
+  }
+
+  // Remove the notification. Note that since |RemoveChildView()| does not
+  // delete the notification, we must do so manually here.
   contents()->RemoveChildView(notifications_[id]);
+  delete notifications_[id];
   notifications_.erase(id);
 
   contents()->InvalidateLayout();
