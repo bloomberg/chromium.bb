wjdz99/aom

test/*.cc: remove use of GTEST_ARRAY_SIZE_()
this macro is deprecated and will be removed in a future release.

fwd_kf_test.cc: instead of an index, just pass the values to the test as
                parameters
{intrabc_test,resize_test}.cc: use range based for loops now that c++11
                               is mandatory

Change-Id: I4e31cca01682c1f5c00b873400ed4839b40acc7e

test/intrabc_test.cc
@@ -156,13 +156,12 @@ TEST(IntrabcTest, DvValidation) {
   AV1_COMMON cm;
   memset(&cm, 0, sizeof(cm));
 
-  for (int i = 0; i < static_cast<int>(GTEST_ARRAY_SIZE_(kDvCases)); ++i) {
-    EXPECT_EQ(static_cast<int>(kDvCases[i].valid),
-              av1_is_dv_valid(kDvCases[i].dv, &cm, &xd,
-                              xd.tile.mi_row_start + kDvCases[i].mi_row_offset,
-                              xd.tile.mi_col_start + kDvCases[i].mi_col_offset,
-                              kDvCases[i].bsize, MAX_MIB_SIZE_LOG2))
-        << "DvCases[" << i << "]";
+  for (const DvTestCase &dvCase : kDvCases) {
+    EXPECT_EQ(static_cast<int>(dvCase.valid),
+              av1_is_dv_valid(dvCase.dv, &cm, &xd,
+                              xd.tile.mi_row_start + dvCase.mi_row_offset,
+                              xd.tile.mi_col_start + dvCase.mi_col_offset,
+                              dvCase.bsize, MAX_MIB_SIZE_LOG2));
   }
 }
 }  // namespace

bloomberg/chromium.bb

test/intrabc_test.cc
@@ -156,13 +156,12 @@ TEST(IntrabcTest, DvValidation) {
   AV1_COMMON cm;
   memset(&cm, 0, sizeof(cm));
 
-  for (int i = 0; i < static_cast<int>(GTEST_ARRAY_SIZE_(kDvCases)); ++i) {
-    EXPECT_EQ(static_cast<int>(kDvCases[i].valid),
-              av1_is_dv_valid(kDvCases[i].dv, &cm, &xd,
-                              xd.tile.mi_row_start + kDvCases[i].mi_row_offset,
-                              xd.tile.mi_col_start + kDvCases[i].mi_col_offset,
-                              kDvCases[i].bsize, MAX_MIB_SIZE_LOG2))
-        << "DvCases[" << i << "]";
+  for (const DvTestCase &dv_case : kDvCases) {
+    EXPECT_EQ(static_cast<int>(dv_case.valid),
+              av1_is_dv_valid(dv_case.dv, &cm, &xd,
+                              xd.tile.mi_row_start + dv_case.mi_row_offset,
+                              xd.tile.mi_col_start + dv_case.mi_col_offset,
+                              dv_case.bsize, MAX_MIB_SIZE_LOG2));
   }
 }
 }  // namespace