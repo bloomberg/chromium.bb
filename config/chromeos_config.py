973a36942fb66975502627ad17c25012b1addce6
LaMont Jones <lamontjones@chromium.org>
2019-10-18 11:59:01 -0600

    config: update bug# on (non-crit) jetstream
    BUG=chromium:981002
    TEST=regen
    
    Change-Id: Id2ca1769987fdb0680be85a6b884dee916cefd8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/1869615
    Tested-by: LaMont Jones <lamontjones@chromium.org>
    Auto-Submit: LaMont Jones <lamontjones@chromium.org>
    Reviewed-by: David Burger <dburger@chromium.org>
    Commit-Queue: LaMont Jones <lamontjones@chromium.org>

--- config/chromeos_config.py
+++ config/chromeos_config.py
@@ -2376,16 +2376,16 @@ def CqBuilders(site_config, boards_dict, ge_build_config):
       'cave',  # temporary hack for crbug.com/1008674
       'elm',  # temporary hack for crbug.com/1008674
       'fizz-moblab', # crbug.com/995170
-      'gale',  # crbug.com/1014100
+      'gale',  # crbug.com/1014100, crbug.com/981002
       'lakitu',  # crbug.com/1015040  review on 2019-10-17
       'lakitu-gpu',  # crbug.com/1015040  review on 2019-10-17
       'lakitu-st',  # crbug.com/1015040  review on 2019-10-17
-      'mistral', # b/140536001
+      'mistral', # b/140536001, crbug.com/981002
       'moblab-generic-vm', # crbug.com/960998, crbug.com/976297
       'samus-kernelnext', # https://crbug.com/1004618
       'veyron_mighty', # https://crbug.com/1001290
       'veyron_rialto',  # https://crbug.com/1007997
-      'whirlwind', # https://crbug.com/1001714
+      'whirlwind', # https://crbug.com/981002
       'wizpig', # crbug.com/996944
   ])
 
