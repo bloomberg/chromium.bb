3b0be1f052e773daaee22e5ba545b48eef27b6d4
Caleb Rouleau <crouleau@chromium.org>
2019-10-18 08:19:59 +0000

    Revert "[run perf tests] Output to swarming log again."
    This reverts commit 4ea6ca07dc44ac31e5f2d7a99bd3a7abf4d64c17 [formerly 7bfe9152bded5066e32d743ecdb0367dced59af4].
    
    Reason for revert: crbug.com/1000426 is fixed now, so this is no longer needed.
    
    Original change's description:
    > [run perf tests] Output to swarming log again.
    > 
    > We used to do this before
    > https://chromium-review.googlesource.com/c/chromium/src/+/1629432
    > We changed it because the swarming was cutting off the logs
    > because they were too long (and also loading them was difficult).
    > 
    > This change is a temporary trial for figuring out crbug.com/1000426
    > while we wait for email responses to
    > https://groups.google.com/a/google.com/forum/#!topic/luci-eng/3uLbRYVecuc
    > 
    > 
    > Bug: 1000426
    > Change-Id: I788d385a665604030f0df93a24e6aab3606929cd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1790243
    > Reviewed-by: John Chen <johnchen@chromium.org>
    > Commit-Queue: Caleb Rouleau <crouleau@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#694372}
    
    TBR=crouleau@chromium.org,johnchen@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1000426, 1015505
    Change-Id: Ic5b43be621c0719eaec981b54a9a89f22623857f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1864622
    Reviewed-by: John Chen <johnchen@chromium.org>
    Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Auto-Submit: Caleb Rouleau <crouleau@chromium.org>
    Commit-Queue: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707283}
    
    Former-commit-id: c97cd0497308b95330070ea9e59d5be851466ab1

--- testing/scripts/run_performance_tests.py
+++ testing/scripts/run_performance_tests.py
@@ -348,8 +348,9 @@ def execute_telemetry_benchmark(
       return_code = xvfb.run_executable(
           command, env=env, stdoutfile=output_paths.logs)
     else:
-      return_code = test_env.run_command_with_output(
-          command, env=env, stdoutfile=output_paths.logs)
+      with open(output_paths.logs, 'w') as handle:
+        return_code = test_env.run_command_output_to_handle(
+            command, handle, env=env)
     expected_results_filename = os.path.join(temp_dir, 'test-results.json')
     if os.path.exists(expected_results_filename):
       shutil.move(expected_results_filename, output_paths.test_results)
