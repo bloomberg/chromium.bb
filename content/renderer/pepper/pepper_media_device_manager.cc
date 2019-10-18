c4c566b09a267a5cb3894a04314521468e75bb97
Ken Rockot <rockot@google.com>
2019-10-18 20:19:44 +0000

    Fix plugin access to media devices
    This was broken by https://crrev.com/8d654258 where
    PepperMediaDeviceManager was not updated to acquire the relevant
    interfaces from their new location.
    
    Bug: 1013652
    Change-Id: I040943975c4638159f788b47e24aa3a20ee729d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1869019
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#707503}
    
    Former-commit-id: 0f3adac2a1df856f95cb9d560edef411fe3eca97

--- content/renderer/pepper/pepper_media_device_manager.cc
+++ content/renderer/pepper/pepper_media_device_manager.cc
@@ -273,8 +273,7 @@ blink::mojom::MediaStreamDispatcherHost*
 PepperMediaDeviceManager::GetMediaStreamDispatcherHost() {
   if (!dispatcher_host_) {
     CHECK(render_frame());
-    CHECK(render_frame()->GetRemoteInterfaces());
-    render_frame()->GetRemoteInterfaces()->GetInterface(
+    render_frame()->GetBrowserInterfaceBroker()->GetInterface(
         dispatcher_host_.BindNewPipeAndPassReceiver());
   }
   return dispatcher_host_.get();
@@ -294,8 +293,7 @@ blink::mojom::MediaDevicesDispatcherHost*
 PepperMediaDeviceManager::GetMediaDevicesDispatcher() {
   if (!media_devices_dispatcher_) {
     CHECK(render_frame());
-    CHECK(render_frame()->GetRemoteInterfaces());
-    render_frame()->GetRemoteInterfaces()->GetInterface(
+    render_frame()->GetBrowserInterfaceBroker()->GetInterface(
         media_devices_dispatcher_.BindNewPipeAndPassReceiver());
   }
 
