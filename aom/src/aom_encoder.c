wjdz99/aom

add config file options support
clean up and update config file options support and allow passing control
flags through config file to turn on/off individual coding tool for tool
on and tool off testing

Change-Id: I2c68773d3bce6f7eb70990d4fbdc34fab32dcc80

aom/src/aom_encoder.c
@@ -22,6 +22,11 @@
 #include <fenv.h>
 #endif
 
+#if CONFIG_FILEOPTIONS
+#include "aom/aom_codec.h"
+extern void init_config(cfg_options_t *pConfig);
+#endif
+
 #include <limits.h>
 #include <string.h>
 #include "aom/internal/aom_codec_internal.h"
@@ -167,10 +172,12 @@ aom_codec_err_t aom_codec_enc_config_default(aom_codec_iface_t *iface,
     }
   }
 
+#if CONFIG_FILEOPTIONS
   /* default values */
   if (cfg) {
-    cfg->cfg.ext_partition = 1;
+    init_config(&cfg->encoder_cfg);
   }
+#endif
 
   return res;
 }

av1/av1_cx_iface.c
@@ -584,9 +584,61 @@ static void disable_superres(AV1EncoderConfig *const oxcf) {
   oxcf->superres_kf_qthresh = 255;
 }
 
-static aom_codec_err_t set_encoder_config(
-    AV1EncoderConfig *oxcf, const aom_codec_enc_cfg_t *cfg,
-    const struct av1_extracfg *extra_cfg) {
+#if CONFIG_FILEOPTIONS
+static void update_default_encoder_config(const cfg_options_t *cfg,
+                                          struct av1_extracfg *extra_cfg) {
+  extra_cfg->enable_cdef = (cfg->DisableCDEF == 0);
+  extra_cfg->enable_restoration = (cfg->DisableLR == 0);
+  extra_cfg->superblock_size = (cfg->SuperBlockSize == 64)
+                                   ? AOM_SUPERBLOCK_SIZE_64X64
+                                   : (cfg->SuperBlockSize == 128)
+                                         ? AOM_SUPERBLOCK_SIZE_128X128
+                                         : AOM_SUPERBLOCK_SIZE_DYNAMIC;
+  extra_cfg->enable_warped_motion = (cfg->DisableWarpMotion == 0);
+  extra_cfg->enable_dist_wtd_comp = (cfg->DisableDistWtdComp == 0);
+  extra_cfg->enable_diff_wtd_comp = (cfg->DisableDiffWtdComp == 0);
+  extra_cfg->enable_dual_filter = (cfg->DisableDualFilter == 0);
+  extra_cfg->enable_angle_delta = (cfg->DisableIntraAngleDelta == 0);
+  extra_cfg->enable_rect_partitions = (cfg->DisableRectPartitionType == 0);
+  extra_cfg->enable_ab_partitions = (cfg->DisableABPartitionType == 0);
+  extra_cfg->enable_1to4_partitions = (cfg->Disable1to4PartitionType == 0);
+  extra_cfg->max_partition_size = cfg->MaxPartitionSize;
+  extra_cfg->min_partition_size = cfg->MinPartitionSize;
+  extra_cfg->enable_intra_edge_filter = (cfg->DisableIntraEdgeFilter == 0);
+  extra_cfg->enable_tx64 = (cfg->DisableTx64x64 == 0);
+  extra_cfg->enable_flip_idtx = (cfg->DisableFlipIdtx == 0);
+  extra_cfg->enable_masked_comp = (cfg->DisableMaskedComp == 0);
+  extra_cfg->enable_interintra_comp = (cfg->DisableInterIntraComp == 0);
+  extra_cfg->enable_smooth_interintra = (cfg->DisableSmoothInterIntra == 0);
+  extra_cfg->enable_interinter_wedge = (cfg->DisableInterInterWedge == 0);
+  extra_cfg->enable_interintra_wedge = (cfg->DisableInterIntraWedge == 0);
+  extra_cfg->enable_global_motion = (cfg->DisableGlobalMotion == 0);
+  extra_cfg->enable_filter_intra = (cfg->DisableFilterIntra == 0);
+  extra_cfg->enable_smooth_intra = (cfg->DisableSmoothIntra == 0);
+  extra_cfg->enable_paeth_intra = (cfg->DisablePaethIntra == 0);
+  extra_cfg->enable_cfl_intra = (cfg->DisableCFL == 0);
+  extra_cfg->enable_obmc = (cfg->DisableOBMC == 0);
+  extra_cfg->enable_palette = (cfg->DisablePalette == 0);
+  extra_cfg->enable_intrabc = (cfg->DisableIBC == 0);
+  extra_cfg->disable_trellis_quant = cfg->DisableTrellisQuant;
+  extra_cfg->allow_ref_frame_mvs = (cfg->DisableRefFrameMV == 0);
+  extra_cfg->enable_ref_frame_mvs = (cfg->DisableRefFrameMV == 0);
+  extra_cfg->enable_onesided_comp = (cfg->DisableOneSidedComp == 0);
+  extra_cfg->tx_size_search_method = cfg->TxSizeSearchMethod;
+  extra_cfg->enable_reduced_reference_set = cfg->ReducedReferenceSet;
+  extra_cfg->reduced_tx_type_set = cfg->ReducedTxTypeSet;
+}
+#endif
+
+static aom_codec_err_t set_encoder_config(AV1EncoderConfig *oxcf,
+                                          const aom_codec_enc_cfg_t *cfg,
+                                          struct av1_extracfg *extra_cfg) {
+#if CONFIG_FILEOPTIONS
+  if (cfg->encoder_cfg.InitByCfgFile) {
+    update_default_encoder_config(&cfg->encoder_cfg, extra_cfg);
+  }
+#endif
+
   const int is_vbr = cfg->rc_end_usage == AOM_VBR;
   oxcf->profile = cfg->g_profile;
   oxcf->fwd_kf_enabled = cfg->fwd_kf_enabled;
@@ -634,7 +686,9 @@ static aom_codec_err_t set_encoder_config(
     oxcf->init_framerate = 30;
     oxcf->timing_info_present = 0;
   }
-  oxcf->cfg = &cfg->cfg;
+#if CONFIG_FILEOPTIONS
+  oxcf->encoder_cfg = &cfg->encoder_cfg;
+#endif
 
   switch (cfg->g_pass) {
     case AOM_RC_ONE_PASS: oxcf->pass = 0; break;
@@ -668,6 +722,7 @@ static aom_codec_err_t set_encoder_config(
   oxcf->enable_intrabc = extra_cfg->enable_intrabc;
   oxcf->enable_angle_delta = extra_cfg->enable_angle_delta;
   oxcf->disable_trellis_quant = extra_cfg->disable_trellis_quant;
+  oxcf->allow_ref_frame_mvs = extra_cfg->enable_ref_frame_mvs;
   oxcf->using_qm = extra_cfg->enable_qm;
   oxcf->qm_y = extra_cfg->qm_y;
   oxcf->qm_u = extra_cfg->qm_u;
@@ -969,7 +1024,7 @@ static aom_codec_err_t ctrl_get_quantizer64(aom_codec_alg_priv_t *ctx,
 }
 
 static aom_codec_err_t update_extra_cfg(aom_codec_alg_priv_t *ctx,
-                                        const struct av1_extracfg *extra_cfg) {
+                                        struct av1_extracfg *extra_cfg) {
   const aom_codec_err_t res = validate_config(ctx, &ctx->cfg, extra_cfg);
   if (res == AOM_CODEC_OK) {
     ctx->extra_cfg = *extra_cfg;
@@ -2531,7 +2586,8 @@ static aom_codec_enc_cfg_map_t encoder_usage_cfg_map[] = {
         0,            // tile_height_count
         { 0 },        // tile_widths
         { 0 },        // tile_heights
-        { 1 },        // config file
+        { 0, 128, 128, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0,   0,   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  // cfg
     } },
   { 1,
     {
@@ -2599,7 +2655,8 @@ static aom_codec_enc_cfg_map_t encoder_usage_cfg_map[] = {
         0,            // tile_height_count
         { 0 },        // tile_widths
         { 0 },        // tile_heights
-        { 1 },        // config file
+        { 0, 128, 128, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0,   0,   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  // cfg
     } },
 };
 

common/args.c
@@ -17,6 +17,7 @@
 
 #include "aom/aom_integer.h"
 #include "aom_ports/msvc.h"
+#include "aom/aom_codec.h"
 
 #if defined(__GNUC__) && __GNUC__
 extern void die(const char *fmt, ...) __attribute__((noreturn));
@@ -48,12 +49,32 @@ void ignore_end_spaces(char *str) {
   if (end >= str) end[1] = '\0';
 }
 
-int arg_cfg(int *argc, char ***argv, const char *file) {
-  char **argv_local = (char **)*argv;
-  char **argv_org = (char **)*argv;
+#if CONFIG_FILEOPTIONS
+void init_config(cfg_options_t *pConfig) {
+  memset(pConfig, 0, sizeof(cfg_options_t));
+  pConfig->SuperBlockSize = 0;  // Dynamic
+  pConfig->MaxPartitionSize = 128;
+  pConfig->MinPartitionSize = 4;
+  pConfig->DisableTrellisQuant = 3;
+}
+
+static const char sbsize_warning_string[] =
+    "SuperBlockSize has to be 64 or 128.";
+static const char minpart_warning_string[] =
+    "MinPartitionSize has to be smaller or equal to MaxPartitionSize.";
+static const char maxpart_warning_string[] =
+    "MaxPartitionSize has to be smaller or equal to SuperBlockSize.";
+
+int parse_cfg(const char *file, cfg_options_t *pConfig) {
   char line[1024 * 10];
   FILE *f = fopen(file, "r");
   if (!f) return 1;
+
+#define GET_PARAMS(field)          \
+  if (strcmp(left, #field) == 0) { \
+    pConfig->field = atoi(right);  \
+    continue;                      \
+  }
 
   while (fgets(line, sizeof(line) - 1, f)) {
     char *actual_line = ignore_front_spaces(line);
@@ -61,7 +82,7 @@ int arg_cfg(int *argc, char ***argv, const char *file) {
     size_t length = strlen(actual_line);
 
     if (length == 0 || actual_line[0] == '#') continue;
-    right = strchr(actual_line, ':');
+    right = strchr(actual_line, '=');
     if (right == NULL) continue;
     right[0] = '\0';
 
@@ -74,25 +95,65 @@ int arg_cfg(int *argc, char ***argv, const char *file) {
     ignore_end_spaces(left);
     ignore_end_spaces(right);
 
-    char **new_args = argv_dup(*argc, (const char **)argv_local);
-    char *new_line = (char *)malloc(sizeof(*new_line) * 128);
+    GET_PARAMS(SuperBlockSize);
+    GET_PARAMS(MaxPartitionSize);
+    GET_PARAMS(MinPartitionSize);
+    GET_PARAMS(DisableABPartitionType);
+    GET_PARAMS(DisableRectPartitionType);
+    GET_PARAMS(Disable1to4PartitionType);
+    GET_PARAMS(DisableFlipIdtx);
+    GET_PARAMS(DisableCDEF);
+    GET_PARAMS(DisableLR);
+    GET_PARAMS(DisableOBMC);
+    GET_PARAMS(DisableWarpMotion);
+    GET_PARAMS(DisableGlobalMotion);
+    GET_PARAMS(DisableDistWtdComp);
+    GET_PARAMS(DisableDiffWtdComp);
+    GET_PARAMS(DisableInterIntraComp);
+    GET_PARAMS(DisableMaskedComp);
+    GET_PARAMS(DisableOneSidedComp);
+    GET_PARAMS(DisablePalette);
+    GET_PARAMS(DisableIBC);
+    GET_PARAMS(DisableCFL);
+    GET_PARAMS(DisableSmoothIntra);
+    GET_PARAMS(DisableFilterIntra);
+    GET_PARAMS(DisableDualFilter);
+    GET_PARAMS(DisableIntraAngleDelta);
+    GET_PARAMS(TxSizeSearchMethod);
+    GET_PARAMS(DisableIntraEdgeFilter);
+    GET_PARAMS(DisableTx64x64);
+    GET_PARAMS(DisableSmoothInterIntra);
+    GET_PARAMS(DisableInterInterWedge);
+    GET_PARAMS(DisableInterIntraWedge);
+    GET_PARAMS(DisablePaethIntra);
+    GET_PARAMS(DisableTrellisQuant);
+    GET_PARAMS(DisableRefFrameMV);
+    GET_PARAMS(ReducedReferenceSet);
+    GET_PARAMS(ReducedTxTypeSet);
 
-    if (argv_local != argv_org) free(argv_local);
-
-    if (!strcmp(right, "ON"))
-      snprintf(new_line, sizeof(*new_line) * 128, "--%s", left);
-    else
-      snprintf(new_line, sizeof(*new_line) * 128, "--%s=%s", left, right);
-
-    new_args[(*argc) - 1] = new_args[(*argc) - 2];
-    new_args[(*argc) - 2] = new_line;
-    argv_local = new_args;
-    *argv = new_args;
-    (*argc)++;
+    fprintf(stderr, "\nInvalid parameter: %s", left);
+    exit(-1);
   }
+
+  if (pConfig->SuperBlockSize != 128 && pConfig->SuperBlockSize != 64) {
+    fprintf(stderr, "\n%s", sbsize_warning_string);
+    exit(-1);
+  }
+  if (pConfig->MinPartitionSize > pConfig->MaxPartitionSize) {
+    fprintf(stderr, "\n%s", minpart_warning_string);
+    exit(-1);
+  }
+  if (pConfig->MaxPartitionSize > pConfig->SuperBlockSize) {
+    fprintf(stderr, "\n%s", maxpart_warning_string);
+    exit(-1);
+  }
+
   fclose(f);
+  pConfig->InitByCfgFile = 1;
+
   return 0;
 }
+#endif
 
 int arg_match(struct arg *arg_, const struct arg_def *def, char **argv) {
   struct arg arg;

bloomberg/chromium.bb

aom/src/aom_encoder.c
@@ -24,7 +24,10 @@
 
 #include <limits.h>
 #include <string.h>
+#include "aom/aom_encoder.h"
 #include "aom/internal/aom_codec_internal.h"
+
+extern void init_config(cfg_options_t *pConfig);
 
 #define SAVE_STATUS(ctx, var) (ctx ? (ctx->err = var) : var)
 
@@ -167,10 +170,12 @@ aom_codec_err_t aom_codec_enc_config_default(aom_codec_iface_t *iface,
     }
   }
 
+#if CONFIG_FILEOPTIONS
   /* default values */
   if (cfg) {
-    cfg->cfg.ext_partition = 1;
+    init_config(&cfg->encoder_cfg);
   }
+#endif
 
   return res;
 }

av1/av1_cx_iface.c
@@ -586,9 +586,61 @@ static void disable_superres(AV1EncoderConfig *const oxcf) {
   oxcf->superres_kf_qthresh = 255;
 }
 
-static aom_codec_err_t set_encoder_config(
-    AV1EncoderConfig *oxcf, const aom_codec_enc_cfg_t *cfg,
-    const struct av1_extracfg *extra_cfg) {
+#if CONFIG_FILEOPTIONS
+static void update_default_encoder_config(const cfg_options_t *cfg,
+                                          struct av1_extracfg *extra_cfg) {
+  extra_cfg->enable_cdef = (cfg->disable_cdef == 0);
+  extra_cfg->enable_restoration = (cfg->disable_lr == 0);
+  extra_cfg->superblock_size = (cfg->super_block_size == 64)
+                                   ? AOM_SUPERBLOCK_SIZE_64X64
+                                   : (cfg->super_block_size == 128)
+                                         ? AOM_SUPERBLOCK_SIZE_128X128
+                                         : AOM_SUPERBLOCK_SIZE_DYNAMIC;
+  extra_cfg->enable_warped_motion = (cfg->disable_warp_motion == 0);
+  extra_cfg->enable_dist_wtd_comp = (cfg->disable_dist_wtd_comp == 0);
+  extra_cfg->enable_diff_wtd_comp = (cfg->disable_diff_wtd_comp == 0);
+  extra_cfg->enable_dual_filter = (cfg->disable_dual_filter == 0);
+  extra_cfg->enable_angle_delta = (cfg->disable_intra_angle_delta == 0);
+  extra_cfg->enable_rect_partitions = (cfg->disable_rect_partition_type == 0);
+  extra_cfg->enable_ab_partitions = (cfg->disable_ab_partition_type == 0);
+  extra_cfg->enable_1to4_partitions = (cfg->disable_1to4_partition_type == 0);
+  extra_cfg->max_partition_size = cfg->max_partition_size;
+  extra_cfg->min_partition_size = cfg->min_partition_size;
+  extra_cfg->enable_intra_edge_filter = (cfg->disable_intra_edge_filter == 0);
+  extra_cfg->enable_tx64 = (cfg->disable_tx_64x64 == 0);
+  extra_cfg->enable_flip_idtx = (cfg->disable_flip_idtx == 0);
+  extra_cfg->enable_masked_comp = (cfg->disable_masked_comp == 0);
+  extra_cfg->enable_interintra_comp = (cfg->disable_inter_intra_comp == 0);
+  extra_cfg->enable_smooth_interintra = (cfg->disable_smooth_inter_intra == 0);
+  extra_cfg->enable_interinter_wedge = (cfg->disable_inter_inter_wedge == 0);
+  extra_cfg->enable_interintra_wedge = (cfg->disable_inter_intra_wedge == 0);
+  extra_cfg->enable_global_motion = (cfg->disable_global_motion == 0);
+  extra_cfg->enable_filter_intra = (cfg->disable_filter_intra == 0);
+  extra_cfg->enable_smooth_intra = (cfg->disable_smooth_intra == 0);
+  extra_cfg->enable_paeth_intra = (cfg->disable_paeth_intra == 0);
+  extra_cfg->enable_cfl_intra = (cfg->disable_cfl == 0);
+  extra_cfg->enable_obmc = (cfg->disable_obmc == 0);
+  extra_cfg->enable_palette = (cfg->disable_palette == 0);
+  extra_cfg->enable_intrabc = (cfg->disable_ibc == 0);
+  extra_cfg->disable_trellis_quant = cfg->disable_trellis_quant;
+  extra_cfg->allow_ref_frame_mvs = (cfg->disable_ref_frame_mv == 0);
+  extra_cfg->enable_ref_frame_mvs = (cfg->disable_ref_frame_mv == 0);
+  extra_cfg->enable_onesided_comp = (cfg->disable_one_sided_comp == 0);
+  extra_cfg->tx_size_search_method = cfg->tx_size_search_method;
+  extra_cfg->enable_reduced_reference_set = cfg->reduced_reference_set;
+  extra_cfg->reduced_tx_type_set = cfg->reduced_tx_type_set;
+}
+#endif
+
+static aom_codec_err_t set_encoder_config(AV1EncoderConfig *oxcf,
+                                          const aom_codec_enc_cfg_t *cfg,
+                                          struct av1_extracfg *extra_cfg) {
+#if CONFIG_FILEOPTIONS
+  if (cfg->encoder_cfg.init_by_cfg_file) {
+    update_default_encoder_config(&cfg->encoder_cfg, extra_cfg);
+  }
+#endif
+
   const int is_vbr = cfg->rc_end_usage == AOM_VBR;
   oxcf->profile = cfg->g_profile;
   oxcf->fwd_kf_enabled = cfg->fwd_kf_enabled;
@@ -636,7 +688,9 @@ static aom_codec_err_t set_encoder_config(
     oxcf->init_framerate = 30;
     oxcf->timing_info_present = 0;
   }
-  oxcf->cfg = &cfg->cfg;
+#if CONFIG_FILEOPTIONS
+  oxcf->encoder_cfg = &cfg->encoder_cfg;
+#endif
 
   switch (cfg->g_pass) {
     case AOM_RC_ONE_PASS: oxcf->pass = 0; break;
@@ -670,6 +724,7 @@ static aom_codec_err_t set_encoder_config(
   oxcf->enable_intrabc = extra_cfg->enable_intrabc;
   oxcf->enable_angle_delta = extra_cfg->enable_angle_delta;
   oxcf->disable_trellis_quant = extra_cfg->disable_trellis_quant;
+  oxcf->allow_ref_frame_mvs = extra_cfg->enable_ref_frame_mvs;
   oxcf->using_qm = extra_cfg->enable_qm;
   oxcf->qm_y = extra_cfg->qm_y;
   oxcf->qm_u = extra_cfg->qm_u;
@@ -972,7 +1027,7 @@ static aom_codec_err_t ctrl_get_quantizer64(aom_codec_alg_priv_t *ctx,
 }
 
 static aom_codec_err_t update_extra_cfg(aom_codec_alg_priv_t *ctx,
-                                        const struct av1_extracfg *extra_cfg) {
+                                        struct av1_extracfg *extra_cfg) {
   const aom_codec_err_t res = validate_config(ctx, &ctx->cfg, extra_cfg);
   if (res == AOM_CODEC_OK) {
     ctx->extra_cfg = *extra_cfg;
@@ -2543,7 +2598,8 @@ static aom_codec_enc_cfg_map_t encoder_usage_cfg_map[] = {
         0,            // tile_height_count
         { 0 },        // tile_widths
         { 0 },        // tile_heights
-        { 1 },        // config file
+        { 0, 128, 128, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0,   0,   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  // cfg
     } },
   { 1,
     {
@@ -2611,7 +2667,8 @@ static aom_codec_enc_cfg_map_t encoder_usage_cfg_map[] = {
         0,            // tile_height_count
         { 0 },        // tile_widths
         { 0 },        // tile_heights
-        { 1 },        // config file
+        { 0, 128, 128, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0,   0,   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  // cfg
     } },
 };
 

common/args.c
@@ -17,6 +17,7 @@
 
 #include "aom/aom_integer.h"
 #include "aom_ports/msvc.h"
+#include "aom/aom_codec.h"
 
 #if defined(__GNUC__) && __GNUC__
 extern void die(const char *fmt, ...) __attribute__((noreturn));
@@ -48,12 +49,31 @@ void ignore_end_spaces(char *str) {
   if (end >= str) end[1] = '\0';
 }
 
-int arg_cfg(int *argc, char ***argv, const char *file) {
-  char **argv_local = (char **)*argv;
-  char **argv_org = (char **)*argv;
+void init_config(cfg_options_t *pConfig) {
+  memset(pConfig, 0, sizeof(cfg_options_t));
+  pConfig->super_block_size = 0;  // Dynamic
+  pConfig->max_partition_size = 128;
+  pConfig->min_partition_size = 4;
+  pConfig->disable_trellis_quant = 3;
+}
+
+static const char sbsize_warning_string[] =
+    "super_block_size has to be 64 or 128.";
+static const char minpart_warning_string[] =
+    "min_partition_size has to be smaller or equal to max_partition_size.";
+static const char maxpart_warning_string[] =
+    "max_partition_size has to be smaller or equal to super_block_size.";
+
+int parse_cfg(const char *file, cfg_options_t *pConfig) {
   char line[1024 * 10];
   FILE *f = fopen(file, "r");
   if (!f) return 1;
+
+#define GET_PARAMS(field)          \
+  if (strcmp(left, #field) == 0) { \
+    pConfig->field = atoi(right);  \
+    continue;                      \
+  }
 
   while (fgets(line, sizeof(line) - 1, f)) {
     char *actual_line = ignore_front_spaces(line);
@@ -61,7 +81,7 @@ int arg_cfg(int *argc, char ***argv, const char *file) {
     size_t length = strlen(actual_line);
 
     if (length == 0 || actual_line[0] == '#') continue;
-    right = strchr(actual_line, ':');
+    right = strchr(actual_line, '=');
     if (right == NULL) continue;
     right[0] = '\0';
 
@@ -74,23 +94,62 @@ int arg_cfg(int *argc, char ***argv, const char *file) {
     ignore_end_spaces(left);
     ignore_end_spaces(right);
 
-    char **new_args = argv_dup(*argc, (const char **)argv_local);
-    char *new_line = (char *)malloc(sizeof(*new_line) * 128);
+    GET_PARAMS(super_block_size);
+    GET_PARAMS(max_partition_size);
+    GET_PARAMS(min_partition_size);
+    GET_PARAMS(disable_ab_partition_type);
+    GET_PARAMS(disable_rect_partition_type);
+    GET_PARAMS(disable_1to4_partition_type);
+    GET_PARAMS(disable_flip_idtx);
+    GET_PARAMS(disable_cdef);
+    GET_PARAMS(disable_lr);
+    GET_PARAMS(disable_obmc);
+    GET_PARAMS(disable_warp_motion);
+    GET_PARAMS(disable_global_motion);
+    GET_PARAMS(disable_dist_wtd_comp);
+    GET_PARAMS(disable_diff_wtd_comp);
+    GET_PARAMS(disable_inter_intra_comp);
+    GET_PARAMS(disable_masked_comp);
+    GET_PARAMS(disable_one_sided_comp);
+    GET_PARAMS(disable_palette);
+    GET_PARAMS(disable_ibc);
+    GET_PARAMS(disable_cfl);
+    GET_PARAMS(disable_smooth_intra);
+    GET_PARAMS(disable_filter_intra);
+    GET_PARAMS(disable_dual_filter);
+    GET_PARAMS(disable_intra_angle_delta);
+    GET_PARAMS(tx_size_search_method);
+    GET_PARAMS(disable_intra_edge_filter);
+    GET_PARAMS(disable_tx_64x64);
+    GET_PARAMS(disable_smooth_inter_intra);
+    GET_PARAMS(disable_inter_inter_wedge);
+    GET_PARAMS(disable_inter_intra_wedge);
+    GET_PARAMS(disable_paeth_intra);
+    GET_PARAMS(disable_trellis_quant);
+    GET_PARAMS(disable_ref_frame_mv);
+    GET_PARAMS(reduced_reference_set);
+    GET_PARAMS(reduced_tx_type_set);
 
-    if (argv_local != argv_org) free(argv_local);
-
-    if (!strcmp(right, "ON"))
-      snprintf(new_line, sizeof(*new_line) * 128, "--%s", left);
-    else
-      snprintf(new_line, sizeof(*new_line) * 128, "--%s=%s", left, right);
-
-    new_args[(*argc) - 1] = new_args[(*argc) - 2];
-    new_args[(*argc) - 2] = new_line;
-    argv_local = new_args;
-    *argv = new_args;
-    (*argc)++;
+    fprintf(stderr, "\nInvalid parameter: %s", left);
+    exit(-1);
   }
+
+  if (pConfig->super_block_size != 128 && pConfig->super_block_size != 64) {
+    fprintf(stderr, "\n%s", sbsize_warning_string);
+    exit(-1);
+  }
+  if (pConfig->min_partition_size > pConfig->max_partition_size) {
+    fprintf(stderr, "\n%s", minpart_warning_string);
+    exit(-1);
+  }
+  if (pConfig->max_partition_size > pConfig->super_block_size) {
+    fprintf(stderr, "\n%s", maxpart_warning_string);
+    exit(-1);
+  }
+
   fclose(f);
+  pConfig->init_by_cfg_file = 1;
+
   return 0;
 }
 
@@ -209,10 +268,6 @@ int arg_parse_int(const struct arg *arg) {
   return 0;
 }
 
-struct aom_rational {
-  int num; /**< fraction numerator */
-  int den; /**< fraction denominator */
-};
 struct aom_rational arg_parse_rational(const struct arg *arg) {
   long int rawval;
   char *endptr;