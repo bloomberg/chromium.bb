d87ac051ddf075f6a71f13c175d7649e27467262
James Zern <jzern@google.com>
2019-10-20 04:04:19 +0000

    threadpool: use std::condition_variable on iOS
    this avoids an intermittent deadlock on Shutdown(); performance is neutral for
    480/720p
    
    Change-Id: Ia7614f56b8b4b86938aa62d970df42c1e22cb17d

--- src/utils/threadpool.h
+++ src/utils/threadpool.h
@@ -20,13 +20,18 @@
 #include <functional>
 #include <memory>
 
-#if defined(__ANDROID__)
+#if defined(__APPLE__)
+#include <TargetConditionals.h>
+#endif
+
+#if defined(__ANDROID__) || (defined(TARGET_OS_IPHONE) && TARGET_OS_IPHONE)
 #include <condition_variable>  // NOLINT (unapproved c++11 header)
 #include <mutex>               // NOLINT (unapproved c++11 header)
 #define LIBGAV1_THREADPOOL_USE_STD_MUTEX 1
 #else
 // absl::Mutex & absl::CondVar are significantly faster than the pthread
-// variants on platforms other than Android.
+// variants on platforms other than Android. iOS may deadlock on Shutdown()
+// using absl, see b/142251739.
 #include "third_party/abseil-cpp/absl/base/thread_annotations.h"
 #include "third_party/abseil-cpp/absl/synchronization/mutex.h"
 #define LIBGAV1_THREADPOOL_USE_STD_MUTEX 0
