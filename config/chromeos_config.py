6efb3466e0f278bda4ad4ca73f26ba56f90f5ee8
Zhuohao Lee <zhuohao@chromium.org>
2019-10-21 10:16:30 +0800

    kukui: add factory builder
    BUG=b:120114802
    TEST=None
    
    Change-Id: Id63c604c053d3a371af3129d9925e5b449bc87a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/1869092
    Reviewed-by: Wei-Han Chen <stimim@chromium.org>
    Commit-Queue: Zhuohao Lee <zhuohao@chromium.org>
    Tested-by: Zhuohao Lee <zhuohao@chromium.org>

--- config/chromeos_config.py
+++ config/chromeos_config.py
@@ -3499,6 +3499,7 @@ def FactoryBuilders(site_config, _boards_dict, _ge_build_config):
       (ACTIVE, 'factory-atlas-11907.B', ['atlas']),
       (ACTIVE, 'factory-sarien-12033.B', ['sarien']),
       (ACTIVE, 'factory-mistral-12361.B', ['mistral']),
+      (ACTIVE, 'factory-kukui-12587.B', ['kukui']),
       # This is intended to create master branch tryjobs, NOT for production
       # builds. Update the associated list of boards as needed.
       (None, 'master', ['atlas', 'octopus', 'rammus', 'coral', 'eve',
