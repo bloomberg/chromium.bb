69e1fcc17802696fde7a372e12386842a8ed6b89
Maksim Moskvitin <mmoskvitin@google.com>
2019-10-21 14:35:52 +0000

    Enable CannotDecryptScryptKeyEncryptedDataWhenScryptDisabled test
    The test was disabled (crrev.com/c/1849426) because it instantiates
    FeatureLists after the browser threads have been created.
    
    It was needed to inject NigoriSpecifics with scrypt key derivation
    method, while test assumes that scrypt is disabled. The fix is just
    initialization of NigoriSpecifics in test fixture constructor, where
    it's safe to override features.
    
    The test also used to inject an encrypted bookmark to the server, but
    this part wasn't exercised further in test, so this CL just drop it.
    
    Bug: 1013073
    Change-Id: I8d2947484a0192cf99fd2b84e44325ea1d6220fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1872190
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707801}
    
    Former-commit-id: 133c6d7db1714959b00ae2e6bfcaa13bd895d4fb

--- chrome/browser/sync/test/integration/single_client_custom_passphrase_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_custom_passphrase_sync_test.cc
@@ -212,10 +212,21 @@ class SingleClientCustomPassphraseForceDisableScryptSyncTest
     : public SingleClientCustomPassphraseSyncTest {
  public:
   SingleClientCustomPassphraseForceDisableScryptSyncTest()
-      : features_(/*force_disabled=*/true, /*use_for_new_passphrases=*/false) {}
+      : features_(/*force_disabled=*/true, /*use_for_new_passphrases=*/false) {
+    // Creation of NigoriSpecifics with scrypt derivation method requires
+    // |force_disabled| temporary set to false.
+    ScopedScryptFeatureToggler toggler(/*force_disabled=*/false,
+                                       /*use_for_new_passphrases_=*/false);
+    KeyParams key_params = {
+        KeyDerivationParams::CreateForScrypt("someConstantSalt"), "hunter2"};
+    scrypt_nigori_ = CreateCustomPassphraseNigori(key_params);
+  }
+
+  const sync_pb::NigoriSpecifics& scrypt_nigori() { return scrypt_nigori_; }
 
  private:
   ScopedScryptFeatureToggler features_;
+  sync_pb::NigoriSpecifics scrypt_nigori_;
 };
 
 class SingleClientCustomPassphraseDoNotUseScryptSyncTest
@@ -373,27 +384,9 @@ IN_PROC_BROWSER_TEST_P(SingleClientCustomPassphraseDoNotUseScryptSyncTest,
   EXPECT_TRUE(WaitForClientBookmarkWithTitle("scypt-encrypted bookmark"));
 }
 
-// See crbug.com/915219. This test needs to be rewritten to not rely on
-// FeatureList mutation after browser threads have started.
-IN_PROC_BROWSER_TEST_F(
-    SingleClientCustomPassphraseForceDisableScryptSyncTest,
-    DISABLED_CannotDecryptScryptKeyEncryptedDataWhenScryptDisabled) {
-  sync_pb::NigoriSpecifics nigori;
-  {
-    // Creating a Nigori and injecting an encrypted bookmark both require key
-    // derivation using scrypt, so temporarily enable it. This overrides the
-    // feature toggle in the test fixture, which will be restored after this
-    // block.
-    ScopedScryptFeatureToggler toggler(/*force_disabled=*/false,
-                                       /*use_for_new_passphrases_=*/false);
-    KeyParams key_params = {
-        KeyDerivationParams::CreateForScrypt("someConstantSalt"), "hunter2"};
-    nigori = CreateCustomPassphraseNigori(key_params);
-    InjectEncryptedServerBookmark("scypt-encrypted bookmark",
-                                  GURL("http://example.com/doesnt-matter"),
-                                  key_params);
-  }
-  SetNigoriInFakeServer(GetFakeServer(), nigori);
+IN_PROC_BROWSER_TEST_F(SingleClientCustomPassphraseForceDisableScryptSyncTest,
+                       CannotDecryptScryptKeyEncryptedDataWhenScryptDisabled) {
+  SetNigoriInFakeServer(GetFakeServer(), scrypt_nigori());
   SetDecryptionPassphraseForClient(/*index=*/0, "hunter2");
 
   SetupSyncNoWaitingForCompletion();
