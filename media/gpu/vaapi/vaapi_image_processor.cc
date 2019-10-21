890fa825b6288f88eb9cb3b215128a56457eb9c5
Hirokazu Honda <hiroh@chromium.org>
2019-10-21 01:45:39 +0000

    media/gpu/VaapiIP: Support GpuMemoryBuffer-based VideoFrame
    Bug: 1011281
    Test: image_processor_test on atlas (crrev.com/c/1864758)
    Change-Id: I0ec6e5baee9a77ba5598b6f89d005ed41f16fc0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1864501
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707700}
    
    Former-commit-id: 82b7a60435bb7fcec3e9b54bc2e25af789da7c24

--- media/gpu/vaapi/vaapi_image_processor.cc
+++ media/gpu/vaapi/vaapi_image_processor.cc
@@ -106,11 +106,19 @@ std::unique_ptr<VaapiImageProcessor> VaapiImageProcessor::Create(
   }
 
   if (!base::Contains(input_config.preferred_storage_types,
-                      VideoFrame::STORAGE_DMABUFS) ||
+                      VideoFrame::STORAGE_DMABUFS) &&
+      !base::Contains(input_config.preferred_storage_types,
+                      VideoFrame::STORAGE_GPU_MEMORY_BUFFER)) {
+    VLOGF(2) << "VaapiImageProcessor supports Dmabuf-backed or GpuMemoryBuffer"
+             << " based VideoFrame only for input";
+    return nullptr;
+  }
+  if (!base::Contains(output_config.preferred_storage_types,
+                      VideoFrame::STORAGE_DMABUFS) &&
       !base::Contains(output_config.preferred_storage_types,
-                      VideoFrame::STORAGE_DMABUFS)) {
-    VLOGF(2) << "VaapiImageProcessor supports Dmabuf-backed VideoFrame only "
-             << "for both input and output";
+                      VideoFrame::STORAGE_GPU_MEMORY_BUFFER)) {
+    VLOGF(2) << "VaapiImageProcessor supports Dmabuf-backed or GpuMemoryBuffer"
+             << " based VideoFrame only for output";
     return nullptr;
   }
 
