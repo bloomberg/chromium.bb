4abf72961e7424b5ade98306e539d50fa8f41f63
Mikhail Khokhlov <khokhlov@google.com>
2019-10-21 15:57:46 +0000

    [tools/perf] Fix unit name in rasterize_and_record_micro_ct benchmark
    'pixels' is not a valid unit name, should use 'count' instead.
    
    Bug: 1016295
    Change-Id: Ib24aef1c912b28d2ca5377490a2bc3bfb1de4b94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1871875
    Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Cr-Commit-Position: refs/heads/master@{#707828}
    
    Former-commit-id: b528279c974b22c116ee4b03683cb3e24be041b4

--- tools/perf/measurements/rasterize_and_record_micro.py
+++ tools/perf/measurements/rasterize_and_record_micro.py
@@ -71,8 +71,8 @@ class RasterizeAndRecordMicro(legacy_page_test.LegacyPageTest):
     paint_op_memory_usage = data.get('paint_op_memory_usage', 0)
     paint_op_count = data.get('paint_op_count', 0)
 
-    results.AddMeasurement('pixels_recorded', 'pixels', pixels_recorded)
-    results.AddMeasurement('pixels_rasterized', 'pixels', pixels_rasterized)
+    results.AddMeasurement('pixels_recorded', 'count', pixels_recorded)
+    results.AddMeasurement('pixels_rasterized', 'count', pixels_rasterized)
     results.AddMeasurement('rasterize_time', 'ms', rasterize_time)
     results.AddMeasurement('record_time', 'ms', record_time)
     results.AddMeasurement('painter_memory_usage', 'bytes',
@@ -111,8 +111,8 @@ class RasterizeAndRecordMicro(legacy_page_test.LegacyPageTest):
       total_picture_layers_off_screen = data['total_picture_layers_off_screen']
 
       results.AddMeasurement('pixels_rasterized_with_non_solid_color',
-                             'pixels', pixels_rasterized_with_non_solid_color)
-      results.AddMeasurement('pixels_rasterized_as_opaque', 'pixels',
+                             'count', pixels_rasterized_with_non_solid_color)
+      results.AddMeasurement('pixels_rasterized_as_opaque', 'count',
                              pixels_rasterized_as_opaque)
       results.AddMeasurement('total_layers', 'count', total_layers)
       results.AddMeasurement('total_picture_layers', 'count',
