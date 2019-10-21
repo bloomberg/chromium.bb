3fd50003cbb284de763b67ce7d9c0d3e3cff530a
Jordan Bayles <jophba@chromium.org>
2019-10-21 21:00:08 +0000

    (Re)implement Open Screen's ChromeUdpSocket class
    This patch uncomments out and updates/fixes the ChromeUdpSocket class,
    part of Open Screen's embedder platform code.
    
    Change-Id: Ia66f2e92336f99202b33be7c2636f805e2e0dca3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1851230
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707953}
    
    Former-commit-id: a23e2e59fc614a7d3700c3fb12ed14510da428e9

---
+++ chrome/browser/media/router/providers/openscreen/platform/chrome_platform_client.cc
@@ -0,0 +1,44 @@
+// Copyright (c) 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/media/router/providers/openscreen/platform/chrome_platform_client.h"
+
+#include <mutex>
+
+namespace media_router {
+
+ChromePlatformClient::~ChromePlatformClient() {
+  // TODO(jophba, rwkeane): Handle threads and safely shutting down singletons.
+}
+
+// static
+void ChromePlatformClient::Create(
+    std::unique_ptr<network::mojom::NetworkContext> network_context,
+    scoped_refptr<base::SequencedTaskRunner> task_runner) {
+  PlatformClient::SetInstance(new ChromePlatformClient(
+      std::move(network_context), std::move(task_runner)));
+}
+
+// static
+ChromePlatformClient* ChromePlatformClient::GetInstance() {
+  return reinterpret_cast<ChromePlatformClient*>(PlatformClient::GetInstance());
+}
+
+// static
+void ChromePlatformClient::ShutDown() {
+  PlatformClient::ShutDown();
+}
+
+openscreen::platform::TaskRunner* ChromePlatformClient::GetTaskRunner() {
+  return task_runner_.get();
+}
+
+ChromePlatformClient::ChromePlatformClient(
+    std::unique_ptr<network::mojom::NetworkContext> network_context,
+    scoped_refptr<base::SequencedTaskRunner> task_runner)
+    : network_context_(std::move(network_context)),
+      task_runner_(
+          new openscreen::platform::ChromeTaskRunner(std::move(task_runner))) {}
+
+}  // namespace media_router
---
+++ chrome/browser/media/router/providers/openscreen/platform/chrome_platform_client.h
@@ -0,0 +1,47 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_MEDIA_ROUTER_PROVIDERS_OPENSCREEN_PLATFORM_CHROME_PLATFORM_CLIENT_H_
+#define CHROME_BROWSER_MEDIA_ROUTER_PROVIDERS_OPENSCREEN_PLATFORM_CHROME_PLATFORM_CLIENT_H_
+
+#include <memory>
+
+#include "chrome/browser/media/router/providers/openscreen/platform/task_runner.h"
+#include "services/network/public/mojom/network_context.mojom.h"
+#include "third_party/openscreen/src/platform/api/platform_client.h"
+
+namespace media_router {
+
+class ChromePlatformClient : public openscreen::platform::PlatformClient {
+ public:
+  // NOTE: The initialization and shutdown routines are not threadsafe.
+  static void Create(
+      std::unique_ptr<network::mojom::NetworkContext> network_context,
+      scoped_refptr<base::SequencedTaskRunner> task_runner);
+  static ChromePlatformClient* GetInstance();
+  static void ShutDown();
+
+  // These methods are threadsafe.
+  network::mojom::NetworkContext* network_context() const {
+    return network_context_.get();
+  }
+
+  // PlatformClient overrides.
+  openscreen::platform::TaskRunner* GetTaskRunner() override;
+
+ protected:
+  ~ChromePlatformClient() override;
+
+ private:
+  ChromePlatformClient(
+      std::unique_ptr<network::mojom::NetworkContext> network_context,
+      scoped_refptr<base::SequencedTaskRunner> task_runner);
+
+  const std::unique_ptr<network::mojom::NetworkContext> network_context_;
+  const std::unique_ptr<openscreen::platform::ChromeTaskRunner> task_runner_;
+};
+
+}  // namespace media_router
+
+#endif  // CHROME_BROWSER_MEDIA_ROUTER_PROVIDERS_OPENSCREEN_PLATFORM_CHROME_PLATFORM_CLIENT_H_
