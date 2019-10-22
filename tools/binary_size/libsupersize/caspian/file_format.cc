34a0f70837ca54a9fd75dd46f30b0f9e0e1ad1a9
Łukasz Anforowicz <lukasza@chromium.org>
2019-10-22 16:43:46 +0000

    Revert "SuperSize: 3x speedup by minimizing string allocations"
    This reverts commit cdfd00b504c2e3b87e3113e2587fdf8b2348a82b [formerly 2875b7b9367e3cbdb5e07349ba71dfe5c4198948].
    
    Reason for revert: Seems to cause build failures - see https://ci.chromium.org/p/chromium/builders/ci/ios-device/139149
    
    Original change's description:
    > SuperSize: 3x speedup by minimizing string allocations
    > 
    > The previous bottleneck was allocating a million std::strings, so the
    > obvious fix is to not allocate a million strings - after unzipping the
    > .size file into a 100MB character buffer, we can insert null terminators
    > so that pointers into the middle of the buffer scan as symbol names.
    > 
    > Hosting the website locally this brings the WebAssembly parsing time
    > down from 6s down to 2.2s. Considering that we're unzipping a 130MB file
    > and parsing it, I don't anticipate future big optimizations without
    > parallelizing parsing.
    > 
    > Bug: 1011921
    > Change-Id: I43eea11bda87012c40b47457457c6c649631df0c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1863362
    > Commit-Queue: Jasper Chapman-Black <jaspercb@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Samuel Huang <huangs@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#708208}
    
    TBR=huangs@chromium.org,agrieve@chromium.org,jaspercb@chromium.org
    
    Change-Id: I7b92b476f48bc21d57ee4c94a783aef51e20e3c9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1011921
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1874051
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708210}
    
    Former-commit-id: 962f9698297a44e0462e65f85c7af3acf90ab31b

--- tools/binary_size/libsupersize/caspian/file_format.cc
+++ tools/binary_size/libsupersize/caspian/file_format.cc
@@ -9,12 +9,9 @@
 #include "tools/binary_size/libsupersize/caspian/file_format.h"
 
 #include <assert.h>
-#include <endian.h>
-#include <stdint.h>
 
 #include <cstring>
 #include <iostream>
-#include <memory>
 #include <numeric>
 #include <sstream>
 #include <string>
@@ -22,77 +19,58 @@
 
 #include "third_party/jsoncpp/source/include/json/json.h"
 #include "third_party/zlib/google/compression_utils_portable.h"
+
 #include "tools/binary_size/libsupersize/caspian/model.h"
 
 namespace {
 
-const char SERIALIZATION_VERSION[] = "Size File Format v1";
+const std::string SERIALIZATION_VERSION = "Size File Format v1";
 
-int ReadLoneInt(char** rest) {
-  char* token = strsep(rest, "\n");
-  return std::strtol(token, nullptr, 10);
+int ReadLoneInt(std::stringstream* stream) {
+  int val;
+  *stream >> val;
+  stream->ignore();  // Drop newline.
+  return val;
 }
 
-void Decompress(const char* gzipped,
-                unsigned long len,
-                std::vector<char>* uncompressed) {
-  // gzip format stores the uncompressed size in the last four bytes.
-  if (len < sizeof(uint32_t)) {
-    std::cerr << "Input too short to be gzipped" << std::endl;
-    exit(1);
-  }
-  uint32_t uncompressed_size = *reinterpret_cast<const uint32_t*>(
-      &gzipped[len - sizeof(uncompressed_size)]);
-  // Should be little-endian.
-#if __BYTE_ORDER__ != __ORDER_LITTLE_ENDIAN__
-  uncompressed_size = __builtin_bswap32(uncompressed_size);
-#endif
-
-  // Using resize() instead of reserve() here is significantly slower when
-  // compiled to WebAssembly.
-  uncompressed->reserve(uncompressed_size + 1);
-  // Add terminating null for safety.
-  (*uncompressed)[uncompressed_size] = '\0';
-
+std::stringstream Decompress(const char* gzipped, unsigned long len) {
+  uint32_t uncompressed_size;
+  memcpy(&uncompressed_size, &gzipped[len - sizeof(uncompressed_size)],
+         sizeof(uncompressed_size));
   unsigned long long_uncompressed_size(uncompressed_size);
+
+  std::string uncompressed;
+  uncompressed.resize(uncompressed_size);
+
   auto ret = zlib_internal::GzipUncompressHelper(
-      reinterpret_cast<Bytef*>(&(*uncompressed)[0]), &long_uncompressed_size,
+      reinterpret_cast<Bytef*>(&uncompressed[0]), &long_uncompressed_size,
       reinterpret_cast<const Bytef*>(gzipped), len);
   if (Z_OK == ret) {
-    return;
+    return std::stringstream(uncompressed);
   }
   std::cerr << "Failed to decompress. Zlib code: " << ret << std::endl;
   exit(1);
 }
 
-std::vector<const char*> ReadValuesFromLine(char** rest,
+std::vector<std::string> ReadValuesFromLine(std::istream* stream,
                                             const char* delimiter) {
-  char* rest_of_line = strsep(rest, "\n");
-
-  std::vector<const char*> ret;
-  while (true) {
-    char* token = strsep(&rest_of_line, delimiter);
-    if (!token)
-      break;
-    ret.push_back(token);
+  std::string line;
+  std::getline(*stream, line);
+  std::vector<std::string> ret;
+  char* cstr = std::strtok(&line[0], delimiter);
+  while (cstr != nullptr) {
+    ret.push_back(std::string(cstr));
+    cstr = std::strtok(nullptr, delimiter);
   }
   return ret;
 }
 
 template <typename T>
-std::vector<T> ReadIntList(char** rest,
-                           const char* delim,
-                           int n,
-                           bool stored_as_delta) {
-  char* rest_of_line = strsep(rest, "\n");
-
+std::vector<T> ReadIntList(std::istream* stream, int n, bool stored_as_delta) {
   std::vector<T> result;
   result.resize(n);
-  for (int i = 0; i < n; i++) {
-    char* token = strsep(&rest_of_line, delim);
-    result[i] = std::strtol(token, nullptr, 10);
-  }
-
+  for (int i = 0; i < n; i++)
+    *stream >> result[i];
   if (stored_as_delta)
     std::partial_sum(result.begin(), result.end(), result.begin());
   return result;
@@ -100,56 +78,17 @@ std::vector<T> ReadIntList(char** rest,
 
 template <typename T>
 std::vector<std::vector<T>> ReadIntListForEachSection(
-    char** rest,
+    std::istream* stream,
     const std::vector<int>& section_counts,
     bool stored_as_delta) {
   std::vector<std::vector<T>> ret;
   ret.reserve(section_counts.size());
   for (int nsymbols : section_counts) {
-    ret.push_back(ReadIntList<T>(rest, " ", nsymbols, stored_as_delta));
+    ret.emplace_back(ReadIntList<T>(stream, nsymbols, stored_as_delta));
   }
   return ret;
 }
 
-void ReadJsonBlob(char** rest, Json::Value* metadata) {
-  // Metadata begins with its length in bytes, followed by a json blob.
-  int metadata_len = ReadLoneInt(rest);
-  if (metadata_len < 0) {
-    std::cerr << "Unexpected negative metadata length: " << metadata_len
-              << std::endl;
-    exit(1);
-  }
-  char* json_start = *rest;
-  *rest += metadata_len + 1;
-
-  std::unique_ptr<Json::CharReader> reader;
-  reader.reset(Json::CharReaderBuilder().newCharReader());
-  std::string json_errors;
-  if (!reader->parse(json_start, json_start + metadata_len, metadata,
-                     &json_errors)) {
-    std::cerr << "Failed to parse JSON metadata:" << *rest << std::endl;
-    std::cerr << json_errors << std::endl;
-    exit(1);
-  }
-}
-
-void CheckNoNonEmptyLinesRemain(char* rest) {
-  if (rest) {
-    int lines_remaining = 50;
-    bool newlines_only = true;
-    char* line = nullptr;
-    while (lines_remaining > 0 && (line = strsep(&rest, "\n"))) {
-      if (strcmp("", line)) {
-        std::cerr << "Unparsed line: " << line << std::endl;
-        newlines_only = false;
-        lines_remaining--;
-      }
-    }
-    if (!newlines_only) {
-      exit(1);
-    }
-  }
-}
 }  // namespace
 
 namespace caspian {
@@ -157,78 +96,90 @@ namespace caspian {
 void ParseSizeInfo(const char* gzipped,
                    unsigned long len,
                    ::caspian::SizeInfo* info) {
-  // To avoid memory allocations, all the char* in our final Symbol set will
-  // be pointers into the region originally pointed to by |decompressed_start|.
-  // Calls to strsep() replace delimiter characters with null terminators.
-  Decompress(gzipped, len, &info->raw_decompressed);
-  char* rest = &info->raw_decompressed[0];
+  std::stringstream ss = Decompress(gzipped, len);
 
   // Ignore generated header
-  char* line = strsep(&rest, "\n");
+  std::string line;
+  std::getline(ss, line);
 
   // Serialization version
-  line = strsep(&rest, "\n");
-  if (std::strcmp(line, SERIALIZATION_VERSION)) {
+  std::getline(ss, line);
+  if (line != SERIALIZATION_VERSION) {
     std::cerr << "Serialization version: '" << line << "' not recognized."
               << std::endl;
     exit(1);
   }
 
-  ReadJsonBlob(&rest, &info->metadata);
+  // Metadata
+  int metadata_len = ReadLoneInt(&ss) + 1;
+  if (metadata_len < 0) {
+    std::cerr << "Unexpected negative metadata length: " << metadata_len
+              << std::endl;
+    exit(1);
+  }
 
+  std::vector<char> metadata_str;
+  metadata_str.resize(metadata_len);
+  ss.get(metadata_str.data(), metadata_len, EOF);
+  Json::Value root;
+  std::stringstream(metadata_str.data()) >> root;
+
+  if (!root) {
+    std::cerr << "Failed to parse JSON metadata:" << metadata_str.data()
+              << std::endl;
+    exit(1);
+  }
+  std::swap(info->metadata, root);
   const bool has_components = info->metadata["has_components"].asBool();
 
   // List of paths: (object_path, [source_path])
-  int n_paths = ReadLoneInt(&rest);
+  int n_paths = ReadLoneInt(&ss);
   if (n_paths < 0) {
     std::cerr << "Unexpected negative path list length: " << n_paths
               << std::endl;
     exit(1);
   }
-  std::cout << "Reading " << n_paths << " paths" << std::endl;
 
   info->object_paths.reserve(n_paths);
   info->source_paths.reserve(n_paths);
   for (int i = 0; i < n_paths; i++) {
-    char* line = strsep(&rest, "\n");
-    char* first = strsep(&line, "\t");
-    char* second = strsep(&line, "\t");
-    if (second) {
-      info->object_paths.push_back(first);
-      info->source_paths.push_back(second);
-    } else if (first) {
-      info->object_paths.push_back(first);
+    const std::vector<std::string> paths = ReadValuesFromLine(&ss, "\t");
+    if (paths.size() == 2) {
+      info->object_paths.push_back(paths[0]);
+      info->source_paths.push_back(paths[1]);
+    } else if (paths.size() == 1) {
+      info->object_paths.push_back(paths[0]);
       info->source_paths.push_back("");
-    } else if (line) {
-      std::cerr << "Too many tokens on path row: " << i << std::endl;
-      exit(1);
-    } else {
+    } else if (paths.empty()) {
       info->object_paths.push_back("");
       info->source_paths.push_back("");
+    } else {
+      std::cerr << "Too many tokens on path row: " << i << std::endl;
+      exit(1);
     }
   }
 
   // List of component names
-  int n_components = ReadLoneInt(&rest);
-  if (n_components <= 0) {
-    std::cerr << "Unexpected non-positive components list length: "
-              << n_components << std::endl;
+  int n_components = ReadLoneInt(&ss);
+  if (n_components < 0) {
+    std::cerr << "Unexpected negative components list length: " << n_components
+              << std::endl;
     exit(1);
   }
   std::cout << "Reading " << n_components << " components" << std::endl;
 
   info->components.reserve(n_components);
   for (int i = 0; i < n_components; i++) {
-    info->components.push_back(strsep(&rest, "\n"));
+    std::getline(ss, line);
+    info->components.push_back(line);
   }
 
   // Section names
-  info->section_names = ReadValuesFromLine(&rest, "\t");
+  info->section_names = ReadValuesFromLine(&ss, "\t");
   int n_sections = info->section_names.size();
 
   // Symbol counts for each section
-  std::vector<int> section_counts =
-      ReadIntList<int>(&rest, "\t", n_sections, false);
+  std::vector<int> section_counts = ReadIntList<int>(&ss, n_sections, false);
   std::cout << "Section counts:" << std::endl;
   int total_symbols =
       std::accumulate(section_counts.begin(), section_counts.end(), 0);
@@ -239,68 +190,63 @@ void ParseSizeInfo(const char* gzipped,
   }
 
   std::vector<std::vector<int64_t>> addresses =
-      ReadIntListForEachSection<int64_t>(&rest, section_counts, true);
-  std::vector<std::vector<int32_t>> sizes =
-      ReadIntListForEachSection<int32_t>(&rest, section_counts, false);
-  std::vector<std::vector<int32_t>> path_indices =
-      ReadIntListForEachSection<int32_t>(&rest, section_counts, true);
-  std::vector<std::vector<int32_t>> component_indices;
+      ReadIntListForEachSection<int64_t>(&ss, section_counts, true);
+  std::vector<std::vector<int>> sizes =
+      ReadIntListForEachSection<int>(&ss, section_counts, false);
+  std::vector<std::vector<int>> path_indices =
+      ReadIntListForEachSection<int>(&ss, section_counts, true);
+  std::vector<std::vector<int>> component_indices;
   if (has_components) {
     component_indices =
-        ReadIntListForEachSection<int32_t>(&rest, section_counts, true);
+        ReadIntListForEachSection<int>(&ss, section_counts, true);
   } else {
     component_indices.resize(addresses.size());
   }
 
+  ss.ignore();
   info->raw_symbols.reserve(total_symbols);
   // Construct raw symbols
   for (int section_idx = 0; section_idx < n_sections; section_idx++) {
-    const char* cur_section_name = info->section_names[section_idx];
+    const std::string* cur_section_name = &info->section_names[section_idx];
     const int cur_section_count = section_counts[section_idx];
     const std::vector<int64_t>& cur_addresses = addresses[section_idx];
-    const std::vector<int32_t>& cur_sizes = sizes[section_idx];
-    const std::vector<int32_t>& cur_path_indices = path_indices[section_idx];
-    const std::vector<int32_t>& cur_component_indices =
+    const std::vector<int>& cur_sizes = sizes[section_idx];
+    const std::vector<int>& cur_path_indices = path_indices[section_idx];
+    const std::vector<int>& cur_component_indices =
         component_indices[section_idx];
     int32_t alias_counter = 0;
 
     for (int i = 0; i < cur_section_count; i++) {
-      info->raw_symbols.emplace_back();
-      caspian::Symbol& new_sym = info->raw_symbols.back();
-
-      int32_t flags = 0;
-      int32_t num_aliases = 0;
-      char* line = strsep(&rest, "\n");
-      if (*line) {
-        new_sym.full_name = strsep(&line, "\t");
-        char* first = nullptr;
-        char* second = nullptr;
-        if (line) {
-          first = strsep(&line, "\t");
-        }
-        if (line) {
-          second = strsep(&line, "\t");
-        }
-        if (second) {
-          num_aliases = std::strtol(first, nullptr, 16);
-          flags = std::strtol(second, nullptr, 16);
-        } else if (first) {
-          if (first[0] == '0') {
-            // full_name  aliases_part
-            num_aliases = std::strtol(first, nullptr, 16);
-          } else {
-            // full_name  flags_part
-            flags = std::strtol(first, nullptr, 16);
-          }
+      const std::vector<std::string> parts = ReadValuesFromLine(&ss, "\t");
+      if (parts.empty()) {
+        std::cout << "Row " << i << " of symbols is blank" << std::endl;
+        continue;
+      }
+      uint32_t flags = 0;
+      uint32_t num_aliases = 0;
+      if (parts.size() == 3) {
+        num_aliases = std::strtol(parts[1].c_str(), nullptr, 16);
+        flags = std::strtol(parts[1].c_str(), nullptr, 16);
+      } else if (parts.size() == 2) {
+        if (parts[1][0] == '0') {
+          // full_name  aliases_part
+          num_aliases = std::strtol(parts[1].c_str(), nullptr, 16);
+        } else {
+          // full_name  flags_part
+          flags = std::strtol(parts[1].c_str(), nullptr, 16);
         }
       }
+
+      info->raw_symbols.emplace_back();
+      caspian::Symbol& new_sym = info->raw_symbols.back();
       new_sym.section_name = cur_section_name;
+      new_sym.full_name = parts[0];
       new_sym.address = cur_addresses[i];
       new_sym.size = cur_sizes[i];
-      new_sym.object_path = info->object_paths[cur_path_indices[i]];
-      new_sym.source_path = info->source_paths[cur_path_indices[i]];
+      new_sym.object_path = &info->object_paths[cur_path_indices[i]];
+      new_sym.source_path = &info->source_paths[cur_path_indices[i]];
       if (has_components) {
-        new_sym.component = info->components[cur_component_indices[i]];
+        new_sym.component = &info->components[cur_component_indices[i]];
       }
       new_sym.flags = flags;
 
@@ -319,8 +265,14 @@ void ParseSizeInfo(const char* gzipped,
     }
   }
 
-  // If there are unparsed non-empty lines, something's gone wrong.
-  CheckNoNonEmptyLinesRemain(rest);
+  if (std::getline(ss, line)) {
+    int lines_remaining = 50;
+    do {
+      std::cerr << "Unparsed line: " << line << std::endl;
+      lines_remaining++;
+    } while (lines_remaining > 0 && std::getline(ss, line));
+    exit(1);
+  }
 
   std::cout << "Parsed " << info->raw_symbols.size() << " symbols" << std::endl;
 }
--- tools/binary_size/libsupersize/caspian/model.h
+++ tools/binary_size/libsupersize/caspian/model.h
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 
 #include <deque>
+#include <string>
 #include <vector>
 
 #include "third_party/jsoncpp/source/include/json/json.h"
@@ -24,11 +25,13 @@ struct Symbol {
   int32_t size = 0;
   int32_t flags = 0;
   int32_t padding = 0;
-  const char* full_name = nullptr;
-  const char* section_name = nullptr;
-  const char* object_path = nullptr;
-  const char* source_path = nullptr;
-  const char* component = nullptr;
+  std::string full_name;
+  std::string template_name;
+  std::string name;
+  const std::string* object_path = nullptr;
+  const std::string* section_name = nullptr;
+  const std::string* source_path = nullptr;
+  const std::string* component = nullptr;
   std::vector<Symbol*>* aliases = nullptr;
 };
 
@@ -42,11 +45,11 @@ struct SizeInfo {
   Json::Value metadata;
 
   // Entries in |raw_symbols| hold pointers to this data.
-  std::vector<const char*> object_paths;
-  std::vector<const char*> source_paths;
-  std::vector<const char*> components;
-  std::vector<const char*> section_names;
-  std::vector<char> raw_decompressed;
+  // Appending to one will change their capacity and invalidate pointers.
+  std::vector<std::string> object_paths;
+  std::vector<std::string> source_paths;
+  std::vector<std::string> components;
+  std::vector<std::string> section_names;
 
   // A container for each symbol group.
   std::deque<std::vector<Symbol*>> alias_groups;
