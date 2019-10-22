f33f1d903a22e6fcb4b1458609b85ef9eea6ad70
Olivier Li <olivierli@google.com>
2019-10-22 17:13:35 +0000

    Prepare ShortcutsProviderExtensionTest for imminent switching on of the HistoryServiceUsesTaskScheduler experiment.
    The feature will soon be flipped to ENABLED_BY_DEFAULT. This will make HistoryServiceBackend's task runner
    dispatch to the ThreadPool instead of a dedicated base::Thread. The tests need to be
    prepared to stay functional when that happens.
    
    This is done as the last mile of the chrome-wide effort to coalesce as much work as possible
    to the ThreadPool.
    
    Bug: 661143
    Change-Id: Ic17ce4468b3de601763e1ebe5c36f1120313fb57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1873354
    Reviewed-by: Kevin Bailey <krb@chromium.org>
    Commit-Queue: Oliver Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708224}
    
    Former-commit-id: 879c849926d0ca03d1786cad12045f394837400d

--- chrome/browser/autocomplete/shortcuts_provider_extension_unittest.cc
+++ chrome/browser/autocomplete/shortcuts_provider_extension_unittest.cc
@@ -13,6 +13,7 @@
 #include "base/run_loop.h"
 #include "base/stl_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/autocomplete/chrome_autocomplete_provider_client.h"
 #include "chrome/browser/autocomplete/chrome_autocomplete_scheme_classifier.h"
 #include "chrome/browser/autocomplete/shortcuts_backend_factory.h"
@@ -58,6 +59,7 @@ class ShortcutsProviderExtensionTest : public testing::Test {
   void TearDown() override;
 
   content::BrowserTaskEnvironment task_environment_;
+  base::test::ScopedFeatureList feature_list_;
   TestingProfile profile_;
   ChromeAutocompleteProviderClient client_;
   scoped_refptr<ShortcutsBackend> backend_;
@@ -68,6 +70,9 @@ ShortcutsProviderExtensionTest::ShortcutsProviderExtensionTest()
     : client_(&profile_) {}
 
 void ShortcutsProviderExtensionTest::SetUp() {
+  feature_list_.InitWithFeatures(
+      {history::HistoryService::kHistoryServiceUsesTaskScheduler}, {});
+
   ShortcutsBackendFactory::GetInstance()->SetTestingFactoryAndUse(
       &profile_,
       base::BindRepeating(
@@ -85,6 +90,8 @@ void ShortcutsProviderExtensionTest::TearDown() {
   // Run all pending tasks or else some threads hold on to the message loop
   // and prevent it from being deleted.
   base::RunLoop().RunUntilIdle();
+
+  profile_.BlockUntilHistoryBackendDestroyed();
 }
 
 // Actual tests ---------------------------------------------------------------
