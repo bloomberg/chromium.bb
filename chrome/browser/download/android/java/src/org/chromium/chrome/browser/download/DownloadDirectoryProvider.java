7f0a8b8810841fda0225dfc80eacc880156dc3d8
Min Qin <qinmin@chromium.org>
2019-10-22 19:53:04 +0000

    Reland "Move some java download files to chrome/browser/download/android"
    This reverts commit 1897f174b42314535f8a45994105041da81bb604 [formerly d6c0806398734da5ebfecd413fffbb8d54ec5e7f].
    
    Reason for revert: This CL is coupled with
    https://chrome-internal-review.googlesource.com/c/clank/internal/apps/+/1999437,
    so please wait for both to land
    
    Original change's description:
    > Revert "Move some java download files to chrome/browser/download/android"
    >
    > This reverts commit e341ad44c2f46ff6b4634d9735313a19a9988ef3 [formerly 4812124d0d19660f63d12e6b4204cc6304fb1b30].
    >
    > Reason for revert: Suspect causing build failure android_arm64-builder-perf
    > https://ci.chromium.org/p/chrome/builders/ci/android_arm64-builder-perf/122367
    >
    > Original change's description:
    > > Move some java download files to chrome/browser/download/android
    > >
    > > This CL moves the DirectoryOption and DownloadDirectoryProvider.java
    > >
    > > BUG=1013759
    > >
    > > Change-Id: I78bef333bed85a4e34a70843caaf37ef1dbfa7de
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1867192
    > > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > > Commit-Queue: Min Qin <qinmin@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#708254}
    >
    > TBR=dtrainor@chromium.org,qinmin@chromium.org
    >
    > Change-Id: I6f219a56fe4ad9e4e5cde3b8acfca4c045a5bfc7
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1013759
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1873281
    > Reviewed-by: Ella Ge <eirage@chromium.org>
    > Commit-Queue: Ella Ge <eirage@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#708299}
    
    TBR=dtrainor@chromium.org,qinmin@chromium.org,eirage@chromium.org
    
    Change-Id: Ie92a0b729d234ff50fabc4cd4ae0affaf51e9ce9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1013759
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1874327
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708309}
    
    Former-commit-id: 23c9b57eb0f0d3c74f86a1b6c86a8bb4ebac6850

--- chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/DownloadDirectoryProvider.java
+++ chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/DownloadDirectoryProvider.java
@@ -131,8 +131,9 @@ public class DownloadDirectoryProvider {
                 hasAddtionalDirectory = true;
             }
 
-            if (hasAddtionalDirectory)
+            if (hasAddtionalDirectory) {
                 recordDirectoryType(DirectoryOption.DownloadLocationDirectoryType.ADDITIONAL);
+            }
 
             return dirs;
         }
@@ -203,7 +204,7 @@ public class DownloadDirectoryProvider {
     private AllDirectoriesTask mAllDirectoriesTask;
     private ArrayList<DirectoryOption> mDirectoryOptions;
     private String mExternalStorageDirectory;
-    private ArrayList < Callback < ArrayList<DirectoryOption>>> mCallbacks = new ArrayList<>();
+    private ArrayList<Callback<ArrayList<DirectoryOption>>> mCallbacks = new ArrayList<>();
 
     protected DownloadDirectoryProvider() {
         registerSDCardReceiver();
