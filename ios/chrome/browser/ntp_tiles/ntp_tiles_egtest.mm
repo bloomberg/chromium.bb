b3b99edd789ebff25b0c5a47d9cfd7d6e6e0df53
Gauthier Ambard <gambard@chromium.org>
2019-10-23 09:11:21 +0000

    [iOS] Convert NTPTiles to EG2
    Bug: 987646
    Change-Id: I5fd6d0828a2c22e613d97d34689f864c809d93a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1871598
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708546}
    
    Former-commit-id: 1fd8cf5729d0247e7c5e4845af549fc417564159

--- ios/chrome/browser/ntp_tiles/ntp_tiles_egtest.mm
+++ ios/chrome/browser/ntp_tiles/ntp_tiles_egtest.mm
@@ -2,13 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <EarlGrey/EarlGrey.h>
-
-#import "ios/chrome/test/app/chrome_test_util.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
+#import "ios/testing/earl_grey/earl_grey_test.h"
 #import "ios/web/public/test/http_server/html_response_provider.h"
 #import "ios/web/public/test/http_server/html_response_provider_impl.h"
 #import "ios/web/public/test/http_server/http_server.h"
