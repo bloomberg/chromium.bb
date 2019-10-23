bd3e2c519620924cc26faa92215ff37a68b1d231
Mitsuru Oshima <oshima@chromium.org>
2019-10-23 17:09:49 +0000

    Blurred wallapper image on UI side for lock screen.
    I could cleanly apply this change to 78.
    
    (cherry picked from commit d0692d16934fa2af57a638b031b5890b814a5ba6 [formerly 85be12155d5fb8d4012b736a6a12c853fde5cbcf])
    
    Bug: 987819
    Test: manually tested on 4k display device
    Change-Id: I9ed0c3a0f1007b46a213030fa19edf25dcb3810b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1868840
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Mitsuru Oshima (slow) <oshima@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#708015}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1876748
    Reviewed-by: Mitsuru Oshima (slow) <oshima@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3904@{#806}
    Cr-Branched-From: 9035e7d56a589711638d7ffdc83ffa6d674a06ec [formerly 675968a8c657a3bd9c1c2c20c5d2935577bbc5e6]-refs/heads/master@{#693954}
    
    Former-commit-id: 94ba5efe7a3b0458a0dd4dd3d2d8c62c816b1573

--- ash/wallpaper/wallpaper_widget_controller.cc
+++ ash/wallpaper/wallpaper_widget_controller.cc
@@ -8,6 +8,7 @@
 
 #include "ash/ash_export.h"
 #include "ash/root_window_controller.h"
+#include "ash/wallpaper/wallpaper_view.h"
 #include "base/scoped_observer.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_observer.h"
@@ -29,10 +30,13 @@ class WallpaperWidgetController::WidgetHandler
       public views::WidgetObserver,
       public aura::WindowObserver {
  public:
-  WidgetHandler(WallpaperWidgetController* controller, views::Widget* widget)
+  WidgetHandler(WallpaperWidgetController* controller,
+                views::Widget* widget,
+                WallpaperView* wallpaper_view)
       : controller_(controller),
         widget_(widget),
-        parent_window_(widget->GetNativeWindow()->parent()) {
+        parent_window_(widget->GetNativeWindow()->parent()),
+        wallpaper_view_(wallpaper_view) {
     DCHECK(controller_);
     DCHECK(widget_);
     widget_observer_.Add(widget_);
@@ -105,7 +109,6 @@ class WallpaperWidgetController::WidgetHandler
 
   void SetBlur(float blur_sigma) {
     widget_->GetLayer()->SetLayerBlur(blur_sigma);
-
     const bool old_has_blur_cache = has_blur_cache_;
     has_blur_cache_ = blur_sigma > 0.0f;
     if (!old_has_blur_cache && has_blur_cache_) {
@@ -116,6 +119,16 @@ class WallpaperWidgetController::WidgetHandler
   }
 
   void StopAnimating() { widget_->GetLayer()->GetAnimator()->StopAnimating(); }
+
+  void SwitchToNonLayerBlur() {
+    float blur = widget_->GetLayer()->layer_blur();
+    if (has_blur_cache_) {
+      parent_window_->layer()->RemoveCacheRenderSurfaceRequest();
+      has_blur_cache_ = false;
+    }
+    widget_->GetLayer()->SetLayerBlur(0.f);
+    wallpaper_view_->RepaintBlurAndOpacity(blur, 1.f);
+  }
 
  private:
   void Reset(bool close) {
@@ -143,6 +156,7 @@ class WallpaperWidgetController::WidgetHandler
   WallpaperWidgetController* controller_;
   views::Widget* widget_;
   aura::Window* parent_window_;
+  WallpaperView* wallpaper_view_;
 
   bool reset_ = false;
   bool has_blur_cache_ = false;
@@ -200,7 +214,8 @@ void WallpaperWidgetController::SetWallpaperWidget(
     active_widget_->StopAnimating();
   }
 
-  animating_widget_ = std::make_unique<WidgetHandler>(this, widget);
+  animating_widget_ =
+      std::make_unique<WidgetHandler>(this, widget, wallpaper_view);
   animating_widget_->SetBlur(blur_sigma);
   animating_widget_->Show();
 
@@ -258,6 +273,7 @@ void WallpaperWidgetController::SetAnimatingWidgetAsActive() {
 
   // Notify observers that animation finished.
   RunAnimationEndCallbacks();
+  active_widget_->SwitchToNonLayerBlur();
 }
 
 void WallpaperWidgetController::RunAnimationEndCallbacks() {
