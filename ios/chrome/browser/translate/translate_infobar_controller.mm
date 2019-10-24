86f2a9df985691d392d1fc1aaf7be0698136de27
Chris Lu <thegreenfrog@chromium.org>
2019-10-24 01:42:05 +0000

    [ios] Implement BannerViewController presentation for Translate Messages
    This change implements presenting InfobarBannerViewController when
    InfobarTranslateCoordinator starts and the button action. Some constants
    from TranslateInfobarController are moved to a constants so that they
    can be shared.
    
    Screenshot of what it will look like when activated: https://drive.google.com/open?id=11W6HznODlJduMCZgNOSR1b8Gi_crfQl0
    
    Bug: 1014959
    Change-Id: I4e5e2859d20e3f04cd477f65e23889d2aa5b37e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1872013
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708878}
    
    Former-commit-id: 4447e9f627d9574553c28d1a12558cf22b542079

--- ios/chrome/browser/translate/translate_infobar_controller.mm
+++ ios/chrome/browser/translate/translate_infobar_controller.mm
@@ -23,6 +23,7 @@
 #include "ios/chrome/browser/translate/language_selection_context.h"
 #include "ios/chrome/browser/translate/language_selection_delegate.h"
 #include "ios/chrome/browser/translate/language_selection_handler.h"
+#import "ios/chrome/browser/translate/translate_constants.h"
 #import "ios/chrome/browser/translate/translate_infobar_delegate_observer_bridge.h"
 #include "ios/chrome/browser/translate/translate_option_selection_delegate.h"
 #include "ios/chrome/browser/translate/translate_option_selection_handler.h"
@@ -47,17 +48,6 @@ typedef NS_ENUM(NSInteger, LanguageSelectionState) {
   LanguageSelectionStateTarget,
 };
 
-// Various user actions to keep track of.
-typedef NS_OPTIONS(NSUInteger, UserAction) {
-  UserActionNone = 0,
-  UserActionTranslate = 1 << 0,
-  UserActionRevert = 1 << 1,
-  UserActionAlwaysTranslate = 1 << 2,
-  UserActionNeverTranslateLanguage = 1 << 3,
-  UserActionNeverTranslateSite = 1 << 4,
-  UserActionExpandMenu = 1 << 5,
-};
-
 // UMA histogram names.
 // Note: These string constants are repeated in TranslateCompactInfoBar.java.
 const char kLanguageHistogramTranslate[] =
@@ -73,41 +63,6 @@ const char kLanguageHistogramNeverTranslate[] =
 const char kEventHistogram[] = "Translate.CompactInfobar.Event";
 const char kTranslationCountHistogram[] =
     "Translate.CompactInfobar.TranslationsPerPage";
-
-// Enum for the Translate.CompactInfobar.Event UMA histogram.
-// Note: These values are repeated as constants in TranslateCompactInfoBar.java.
-// Note: This enum is used to back an UMA histogram, and should be treated as
-// append-only.
-// TODO(crbug.com/933371): Share these enums with Java.
-enum class InfobarEvent {
-  INFOBAR_IMPRESSION = 0,
-  INFOBAR_TARGET_TAB_TRANSLATE = 1,
-  INFOBAR_DECLINE = 2,
-  INFOBAR_OPTIONS = 3,
-  INFOBAR_MORE_LANGUAGES = 4,
-  INFOBAR_MORE_LANGUAGES_TRANSLATE = 5,
-  INFOBAR_PAGE_NOT_IN = 6,
-  INFOBAR_ALWAYS_TRANSLATE = 7,
-  INFOBAR_NEVER_TRANSLATE = 8,
-  INFOBAR_NEVER_TRANSLATE_SITE = 9,
-  INFOBAR_SCROLL_HIDE = 10,
-  INFOBAR_SCROLL_SHOW = 11,
-  INFOBAR_REVERT = 12,
-  INFOBAR_SNACKBAR_ALWAYS_TRANSLATE_IMPRESSION = 13,
-  INFOBAR_SNACKBAR_NEVER_TRANSLATE_IMPRESSION = 14,
-  INFOBAR_SNACKBAR_NEVER_TRANSLATE_SITE_IMPRESSION = 15,
-  INFOBAR_SNACKBAR_CANCEL_ALWAYS = 16,
-  INFOBAR_SNACKBAR_CANCEL_NEVER_SITE = 17,
-  INFOBAR_SNACKBAR_CANCEL_NEVER = 18,
-  INFOBAR_ALWAYS_TRANSLATE_UNDO = 19,
-  INFOBAR_CLOSE_DEPRECATED = 20,
-  INFOBAR_SNACKBAR_AUTO_ALWAYS_IMPRESSION = 21,
-  INFOBAR_SNACKBAR_AUTO_NEVER_IMPRESSION = 22,
-  INFOBAR_SNACKBAR_CANCEL_AUTO_ALWAYS = 23,
-  INFOBAR_SNACKBAR_CANCEL_AUTO_NEVER = 24,
-  INFOBAR_HISTOGRAM_BOUNDARY = 25,
-  kMaxValue = INFOBAR_HISTOGRAM_BOUNDARY,
-};
 
 }  // namespace
 
--- ios/chrome/browser/ui/infobars/coordinators/infobar_translate_coordinator.mm
+++ ios/chrome/browser/ui/infobars/coordinators/infobar_translate_coordinator.mm
@@ -4,12 +4,16 @@
 
 #import "ios/chrome/browser/ui/infobars/coordinators/infobar_translate_coordinator.h"
 
+#include "base/strings/sys_string_conversions.h"
 #include "components/translate/core/browser/translate_infobar_delegate.h"
 #include "ios/chrome/browser/infobars/infobar_controller_delegate.h"
 #import "ios/chrome/browser/infobars/infobar_type.h"
+#import "ios/chrome/browser/translate/translate_constants.h"
 #import "ios/chrome/browser/translate/translate_infobar_delegate_observer_bridge.h"
 #import "ios/chrome/browser/ui/infobars/banners/infobar_banner_view_controller.h"
 #import "ios/chrome/browser/ui/infobars/infobar_container.h"
+#include "ios/chrome/grit/ios_strings.h"
+#include "ui/base/l10n/l10n_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -25,9 +29,17 @@
 @property(nonatomic, readonly)
     translate::TranslateInfoBarDelegate* translateInfoBarDelegate;
 
+// InfobarBannerViewController owned by this Coordinator.
+@property(nonatomic, strong) InfobarBannerViewController* bannerViewController;
+
+// Tracks user actions taken throughout Translate lifetime.
+@property(nonatomic, assign) UserAction userAction;
+
 @end
 
 @implementation TranslateInfobarCoordinator
+// Synthesize since readonly property from superclass is changed to readwrite.
+@synthesize bannerViewController = _bannerViewController;
 
 - (instancetype)initWithInfoBarDelegate:
     (translate::TranslateInfoBarDelegate*)infoBarDelegate {
@@ -39,6 +51,7 @@
     _translateInfobarDelegateObserver =
         std::make_unique<TranslateInfobarDelegateObserverBridge>(
             infoBarDelegate, self);
+    _userAction = UserActionNone;
   }
   return self;
 }
@@ -53,8 +66,7 @@
 
 - (BOOL)translateInfoBarDelegateDidDismissWithoutInteraction:
     (translate::TranslateInfoBarDelegate*)delegate {
-  // TODO(crbug.com/1014959): implement
-  return YES;
+  return self.userAction == UserActionNone;
 }
 
 #pragma mark - ChromeCoordinator
@@ -62,7 +74,16 @@
 - (void)start {
   if (!self.started) {
     self.started = YES;
-    // TODO(crbug.com/1014959): Configure BannerViewController.
+    self.bannerViewController = [[InfobarBannerViewController alloc]
+        initWithDelegate:self
+           presentsModal:self.hasBadge
+                    type:InfobarType::kInfobarTypeTranslate];
+    self.bannerViewController.titleText = [self titleText];
+    self.bannerViewController.buttonText = [self buttonText];
+    self.bannerViewController.iconImage =
+        [UIImage imageNamed:@"infobar_translate_icon"];
+    self.bannerViewController.optionalAccessibilityLabel =
+        self.bannerViewController.titleText;
   }
 }
 
@@ -80,11 +101,18 @@
 #pragma mark - InfobarCoordinatorImplementation
 
 - (void)performInfobarAction {
-  // TODO(crbug.com/1014959): implement
+  self.userAction |= UserActionTranslate;
+
+  // TODO(crbug.com/1014959): Add metrics
+  if (self.translateInfoBarDelegate->ShouldAutoAlwaysTranslate()) {
+    self.translateInfoBarDelegate->ToggleAlwaysTranslate();
+  }
+  self.translateInfoBarDelegate->Translate();
 }
 
 - (void)infobarWasDismissed {
-  // TODO(crbug.com/1014959): implement
+  // Release these strong ViewControllers at the time of infobar dismissal.
+  self.bannerViewController = nil;
 }
 
 #pragma mark - Banner
@@ -94,7 +122,7 @@
 }
 
 - (void)dismissBannerWhenInteractionIsFinished {
-  // TODO(crbug.com/1014959): implement
+  [self.bannerViewController dismissWhenInteractionIsFinished];
 }
 
 - (void)infobarBannerWillBeDismissed:(BOOL)userInitiated {
@@ -115,6 +143,25 @@
 - (CGFloat)infobarModalHeightForWidth:(CGFloat)width {
   // TODO(crbug.com/1014959): implement
   return 0.0;
+}
+
+#pragma mark - Private
+
+- (NSString*)titleText {
+  // TODO(crbug.com/1014959): Return different strings depending on
+  // TranslateStep. Also, consider looking into base::i18n::MessageFormatter to
+  // ensure the dstring can be translated properly with a dynamic variable
+  // inserted.
+  return [NSString
+      stringWithFormat:@"%@ %@?", @"Translate",
+                       base::SysUTF16ToNSString(self.translateInfoBarDelegate
+                                                    ->target_language_name())];
+}
+
+- (NSString*)buttonText {
+  // TODO(crbug.com/1014959): Return different strings depending on
+  // TranslateStep.
+  return l10n_util::GetNSString(IDS_IOS_TRANSLATE_INFOBAR_TRANSLATE_ACTION);
 }
 
 @end
