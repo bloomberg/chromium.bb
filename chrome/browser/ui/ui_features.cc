9a1bb550b26847938d14857731c8c3956c25464e
Caroline Rising <corising@chromium.org>
2019-10-24 19:24:28 +0000

    Disable tab hover cards for mac on TOT and set default group to Enabled C.
    Bug: 1017821
    Change-Id: I39fc6148524086fb45437911580a98d896263600
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1879694
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709156}
    
    Former-commit-id: 69e95cc2ba0bf7bc67ae7665f4219acd67e8230f

--- chrome/browser/ui/ui_features.cc
+++ chrome/browser/ui/ui_features.cc
@@ -65,8 +65,15 @@ const base::Feature kTabGroups{"TabGroups", base::FEATURE_DISABLED_BY_DEFAULT};
 
 // Enables popup cards containing tab information when hovering over a tab.
 // https://crbug.com/910739
-const base::Feature kTabHoverCards{"TabHoverCards",
-                                   base::FEATURE_ENABLED_BY_DEFAULT};
+const base::Feature kTabHoverCards {
+  "TabHoverCards",
+#if defined(OS_MACOSX)
+      base::FEATURE_DISABLED_BY_DEFAULT
+#else
+      base::FEATURE_ENABLED_BY_DEFAULT
+#endif  // defined(OS_MACOSX)
+};
+
 // Parameter name used for tab hover cards user study.
 // TODO(corising): Removed this after tab hover cards user study.
 const char kTabHoverCardsFeatureParameterName[] = "setting";
--- chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.cc
+++ chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.cc
@@ -71,7 +71,7 @@ bool AreHoverCardImagesEnabled() {
 base::TimeDelta GetMinimumTriggerDelay() {
   int delay_group = base::GetFieldTrialParamByFeatureAsInt(
       features::kTabHoverCards, features::kTabHoverCardsFeatureParameterName,
-      0);
+      2);
   switch (delay_group) {
     case 2:
       return base::TimeDelta::FromMilliseconds(150);
@@ -86,7 +86,7 @@ base::TimeDelta GetMinimumTriggerDelay() {
 base::TimeDelta GetMaximumTriggerDelay() {
   int delay_group = base::GetFieldTrialParamByFeatureAsInt(
       features::kTabHoverCards, features::kTabHoverCardsFeatureParameterName,
-      0);
+      2);
   switch (delay_group) {
     case 2:
       return base::TimeDelta::FromMilliseconds(500);
