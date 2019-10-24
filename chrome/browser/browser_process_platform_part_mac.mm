fb4ef565a7a7d16163ab262d7965f7f71fcb9563
Christian Dullweber <dullweber@chromium.org>
2019-10-24 12:16:03 +0000

    Revert "Add browser process Metal shader compile histogram"
    This reverts commit 93e9f80341eb0bd15463be549f960117d8a09dbb [formerly d4365c02eaad376b882520139709091c65d8a726].
    
    Reason for revert: Suspected to break lots of tests in https://ci.chromium.org/p/chromium/builders/ci/Mac%20ASan%2064%20Tests%20%281%29/57830
    
    Original change's description:
    > Add browser process Metal shader compile histogram
    > 
    > We are already doing this in the GPU process as the histogram
    > Gpu.Metal.TestShaderCompileSucceeded. That histogram indicates that
    > we fail to compile the shader from the GPU process within 6 seconds 2%
    > of the time.
    > 
    > The new histogram Browser.Metal.TestShaderCompileTime histograms the
    > shader compile time, up to 1 minute, whereupon it uses the max bucket
    > (3 minutes).
    > 
    > Also add a DumpWithoutCrashing on timeout. This will have to be removed
    > after one promotion to dev (to not drown out real crashers), but will
    > help gather more information about crashing machine profiles.
    > 
    > Move the test shader code into the metal_util component so that it can
    > be used by both locations (the previous implementation was in /viz/).
    > 
    > Bug: 974219
    > Change-Id: Ieeac67d7dcbef53d7e3cc257eca6ba9fccf94286
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1872624
    > Commit-Queue: ccameron <ccameron@chromium.org>
    > Reviewed-by: Ilya Sherman <isherman@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#708870}
    
    TBR=avi@chromium.org,ccameron@chromium.org,isherman@chromium.org
    
    Change-Id: I8c737c18e8e56d851e71474e27877a0f7333c7a7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 974219
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1878375
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708991}
    
    Former-commit-id: 5c27815488c7330d5131e308aecbbd2b46ee272e

--- chrome/browser/browser_process_platform_part_mac.mm
+++ chrome/browser/browser_process_platform_part_mac.mm
@@ -5,49 +5,8 @@
 #include "chrome/browser/browser_process_platform_part_mac.h"
 
 #include "base/mac/foundation_util.h"
-#include "base/metrics/histogram_macros.h"
-#include "base/time/time.h"
 #import "chrome/browser/app_controller_mac.h"
 #include "chrome/browser/chrome_browser_application_mac.h"
-#include "components/metal_util/test_shader.h"
-
-namespace {
-
-// Allow up to 1 minute for shader compilation. This is much longer than the
-// GPU process limit of 6 seconds.
-constexpr base::TimeDelta kTestShaderCompileTimeout =
-    base::TimeDelta::FromMinutes(1);
-
-void TestShaderCallback(const base::TimeTicks& start_time,
-                        metal::TestShaderResult result) {
-  base::TimeDelta delta;
-  switch (result) {
-    case metal::TestShaderResult::kNotAttempted:
-      // Don't include data if no Metal device was created (e.g, due to hardware
-      // or macOS version reasons).
-      return;
-    case metal::TestShaderResult::kTimedOut:
-      // Use a single histogram for both "how long did compile take" and "did
-      // compile complete in 1 minute" by pushing timeouts into the maximum
-      // bucket of UMA_HISTOGRAM_MEDIUM_TIMES.
-      delta = base::TimeDelta::FromMinutes(3);
-      return;
-    case metal::TestShaderResult::kSucceeded:
-      delta = base::TimeTicks::Now() - start_time;
-      break;
-  }
-  UMA_HISTOGRAM_MEDIUM_TIMES("Browser.Metal.TestShaderCompileTime", delta);
-}
-
-// Launch a test metal shader compile to see how long it takes to complete (if
-// it ever completes).
-// https://crbug.com/974219
-void TestShaderLaunch() {
-  metal::TestShader(base::BindOnce(TestShaderCallback, base::TimeTicks::Now()),
-                    kTestShaderCompileTimeout);
-}
-
-}  // namespace
 
 BrowserProcessPlatformPart::BrowserProcessPlatformPart() {
 }
@@ -87,10 +46,6 @@ void BrowserProcessPlatformPart::PreMainMessageLoopRun() {
   // domain socket will cause the just-created socket to be unlinked.
   DCHECK(!app_shim_listener_.get());
   app_shim_listener_ = new AppShimListener;
-
-  // Launch a test Metal shader compile once the run loop starts.
-  base::ThreadTaskRunnerHandle::Get()->PostTask(
-      FROM_HERE, base::BindOnce(TestShaderLaunch));
 }
 
 AppShimListener* BrowserProcessPlatformPart::app_shim_listener() {
--- components/viz/common/gpu/metal_context_provider.mm
+++ components/viz/common/gpu/metal_context_provider.mm
@@ -4,47 +4,83 @@
 
 #include "components/viz/common/gpu/metal_context_provider.h"
 
-#import <Metal/Metal.h>
-
-#include "base/bind.h"
 #include "base/mac/scoped_nsobject.h"
 #include "base/memory/ref_counted.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/synchronization/waitable_event.h"
-#include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "components/metal_util/device.h"
-#include "components/metal_util/test_shader.h"
 #include "components/viz/common/gpu/metal_api_proxy.h"
 #include "third_party/skia/include/gpu/GrContext.h"
+
+#import <Metal/Metal.h>
 
 namespace viz {
 
 namespace {
+
+const char* kTestShaderSource =
+    ""
+    "#include <metal_stdlib>\n"
+    "#include <simd/simd.h>\n"
+    "typedef struct {\n"
+    "    float4 clipSpacePosition [[position]];\n"
+    "    float4 color;\n"
+    "} RasterizerData;\n"
+    "\n"
+    "vertex RasterizerData vertexShader(\n"
+    "    uint vertexID [[vertex_id]],\n"
+    "    constant vector_float2 *positions[[buffer(0)]],\n"
+    "    constant vector_float4 *colors[[buffer(1)]]) {\n"
+    "  RasterizerData out;\n"
+    "  out.clipSpacePosition = vector_float4(0.0, 0.0, 0.0, 1.0);\n"
+    "  out.clipSpacePosition.xy = positions[vertexID].xy;\n"
+    "  out.color = colors[vertexID];\n"
+    "  return out;\n"
+    "}\n"
+    "\n"
+    "fragment float4 fragmentShader(RasterizerData in [[stage_in]]) {\n"
+    "    return in.color;\n"
+    "}\n"
+    "";
 
 // The timeout for compiling the test shader. Set to be much longer than any
 // shader compile should take, but still less than the watchdog timeout.
 constexpr base::TimeDelta kTestShaderCompileTimeout =
     base::TimeDelta::FromSeconds(6);
 
+// State shared between the compiler callback and the caller.
+class API_AVAILABLE(macos(10.11)) TestShaderState
+    : public base::RefCountedThreadSafe<TestShaderState> {
+ public:
+  TestShaderState() {}
+  base::WaitableEvent event;
+
+ protected:
+  friend class base::RefCountedThreadSafe<TestShaderState>;
+  virtual ~TestShaderState() {}
+};
+
 // Attempt to asynchronously compile a test shader. If the compile doesn't
 // complete within a timeout, then return false.
-bool CompileTestShader() {
-  // Compile a test shader, to see if the MTLCompilerService is responding
-  // or not. If the compile times out, then don't try to use Metal.
-  // https://crbug.com/974219
-  metal::TestShaderResult result = metal::TestShaderResult::kNotAttempted;
-  base::WaitableEvent event;
-  auto lambda = [](base::WaitableEvent* event,
-                   metal::TestShaderResult* out_result,
-                   metal::TestShaderResult result) {
-    *out_result = result;
-    event->Signal();
-  };
-  metal::TestShader(base::BindOnce(lambda, &event, &result),
-                    kTestShaderCompileTimeout);
-  event.Wait();
-  return result == metal::TestShaderResult::kSucceeded;
+bool CompileTestShader(id<MTLDevice> device) API_AVAILABLE(macos(10.11)) {
+  auto state = base::MakeRefCounted<TestShaderState>();
+
+  base::scoped_nsobject<NSString> source([[NSString alloc]
+      initWithCString:kTestShaderSource
+             encoding:NSASCIIStringEncoding]);
+  base::scoped_nsobject<MTLCompileOptions> options(
+      [[MTLCompileOptions alloc] init]);
+  MTLNewLibraryCompletionHandler completion_handler =
+      ^(id<MTLLibrary> library, NSError* error) {
+        DCHECK(library);
+        state->event.Signal();
+      };
+  [device newLibraryWithSource:source
+                       options:options
+             completionHandler:completion_handler];
+
+  return state->event.TimedWait(kTestShaderCompileTimeout);
 }
 
 struct API_AVAILABLE(macos(10.11)) MetalContextProviderImpl
@@ -90,7 +126,7 @@ std::unique_ptr<MetalContextProvider> MetalContextProvider::Create() {
     // Compile a test shader, to see if the MTLCompilerService is responding
     // or not. If the compile times out, then don't try to use Metal.
     // https://crbug.com/974219
-    bool did_compile = CompileTestShader();
+    bool did_compile = CompileTestShader(device_to_use);
     UMA_HISTOGRAM_BOOLEAN("Gpu.Metal.TestShaderCompileSucceeded", did_compile);
     if (!did_compile) {
       DLOG(ERROR) << "Compiling test MTLLibrary timed out.";
