fa43ccde6a5c27a20c604abdeca257a1a004bcbe
Stephen McGruer <smcgruer@chromium.org>
2019-10-24 15:44:56 +0000

    [Sheriff] Disable BookmarksCommandManagerTest.All on Linux Tests (dbg)
    Because this file has multiple tests with the same name ('All') and an
    existing MAYBE_All macro, rename the test to 'AllBCM' to avoid macro
    collision.
    
    TBR=tommycli@chromium.org
    
    Bug: 1010381
    Change-Id: I475153dc2ec05fb737fe59a7568c619ebfdc68da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1878530
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709052}
    
    Former-commit-id: 9bb4073b603e5775f74a130ea95b575d87ac1b77

--- chrome/test/data/webui/bookmarks/bookmarks_browsertest.js
+++ chrome/test/data/webui/bookmarks/bookmarks_browsertest.js
@@ -72,7 +72,14 @@ BookmarksCommandManagerTest.prototype = {
   ]),
 };
 
-TEST_F('BookmarksCommandManagerTest', 'All', function() {
+// https://crbug.com/1010381: Flaky on Linux Tests (dbg)
+GEN('#if defined(OS_LINUX) && !defined(NDEBUG)');
+GEN('#define MAYBE_AllBCM DISABLED_AllBCM');
+GEN('#else');
+GEN('#define MAYBE_AllBCM AllBCM');
+GEN('#endif');
+
+TEST_F('BookmarksCommandManagerTest', 'MAYBE_AllBCM', function() {
   mocha.run();
 });
 
