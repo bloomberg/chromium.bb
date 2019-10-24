32ea537090368c3f29d03ddfa1110022f8567aa0
Ravi Chaudhary <ravi.chaudhary@ittiam.com>
2019-10-24 10:03:02 +0530

    Enable prune_ref_frame_for_gm_search for speed 1
    Enabled speed feature 'prune_ref_frame_for_gm_search'
    for cpu-used=1 for non-boosted frames.
    
               Instruction Count
    cpu-used       Reduction        Quality Loss
       1             3.63%             -0.002%
    
    STATS_CHANGED
    
    Change-Id: Ie01d65f8fa6d600e62401328488974d0966a79cd

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -276,6 +276,7 @@ static void set_good_speed_features_framesize_independent(
   if (speed >= 1) {
     sf->selective_ref_frame = 2;
 
+    sf->prune_ref_frame_for_gm_search = boosted ? 0 : 1;
     sf->intra_tx_size_search_init_depth_rect = 1;
 
     sf->skip_repeat_interpolation_filter_search = 1;
@@ -323,7 +324,6 @@ static void set_good_speed_features_framesize_independent(
 
   if (speed >= 2) {
     sf->gm_erroradv_type = GM_ERRORADV_TR_2;
-    sf->prune_ref_frame_for_gm_search = boosted ? 0 : 1;
 
     sf->selective_ref_frame = 3;
 
