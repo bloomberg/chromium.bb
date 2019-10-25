b90b35022bc23e7719f9da021918bbfeab1b19e0
Igor Kobylin <ikobylin@yandex-team.ru>
2019-10-25 05:33:26 +0000

    Move node_utils to c/b/r/bluetooth_internals
    Since node_utils is used only by cr.ui.pageManager.Page move it to the
    same place.
    
    This also fixes an error with missing dependency in
    c/b/r/bluetooth_internals/page.js.
    
    R=dpapad@chromium.org
    
    Bug: None
    Change-Id: I9048038a1df853cf2cddf11b6728c4780fa1267f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1878070
    Commit-Queue: Igor Kobylin <ikobylin@yandex-team.ru>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709367}
    
    Former-commit-id: 829cc13ab1d1e35727c8b1b2a70e4f2bee641b11

--- chrome/browser/resources/bluetooth_internals/page.js
+++ chrome/browser/resources/bluetooth_internals/page.js
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// <include src="../node_utils.js">
-
 cr.define('cr.ui.pageManager', function() {
   const PageManager = cr.ui.pageManager.PageManager;
 
