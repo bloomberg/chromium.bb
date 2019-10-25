cb63e0438da0f7bc02755ae73e38d58c4fcb79e9
Sven Zheng <svenzheng@chromium.org>
2019-10-25 21:04:48 +0000

    Fix flakiness pixeltests on bots
    On bot, often we can only catch black screen for dialog. I found a similar
    bug crbug.com/687387 related to gpu. To repro the same result locally,
    pass in --disable-gpu when running test locally.
    To fix the flakiness, we don't catch the screen from HDC. Instead, we
    catch from Layer::RequestCopyOfOutput.
    This change also refactor some code to reuse some existing lib.
    
    browser_tests.exe
      --build-revision=0e1a0c2d0616e3f040c67e15b3bcefaf9aba6361 [formerly fd1bb4431a0ab096271373fe0ad315fe90bfc1e0]
      --test-launcher-filter-file=testing/buildbot/filters/pixel_browser_tests.filter
      --browser-ui-tests-verify-pixels
      --enable-pixel-output-in-tests
      --disable-gpu
      --no-luci-auth
    can pass.
    
    TEST: tested multiple times the tests won't catch black screen anymore.
    Bug: 958242
    Change-Id: Ie7df0e304fb74eb1d2b6c013612529dcdf549605
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1879506
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709634}
    
    Former-commit-id: c8a95bb8641c3614349a164b8ce21fef57597a76

--- chrome/browser/ui/test/test_browser_dialog.cc
+++ chrome/browser/ui/test/test_browser_dialog.cc
@@ -23,10 +23,10 @@
 
 #if defined(TOOLKIT_VIEWS)
 #include "base/strings/strcat.h"
+#include "ui/compositor/test/draw_waiter_for_test.h"
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
 #include "ui/views/test/widget_test.h"
-#include "ui/views/widget/widget_observer.h"
 #endif
 
 namespace {
@@ -56,18 +56,6 @@ class WidgetCloser {
   DISALLOW_COPY_AND_ASSIGN(WidgetCloser);
 };
 
-class CompositorObserverEnd : public ui::CompositorObserver {
- public:
-  void OnCompositingEnded(ui::Compositor* compositor) override {
-    base::ThreadTaskRunnerHandle::Get()->PostTask(FROM_HERE,
-                                                  run_loop_.QuitClosure());
-  }
-  void WaitForComplete() { run_loop_.Run(); }
-
- private:
-  base::RunLoop run_loop_;
-};
-
 #endif  // defined(TOOLKIT_VIEWS)
 
 }  // namespace
@@ -83,13 +71,6 @@ TestBrowserDialog::~TestBrowserDialog() = default;
 
 void TestBrowserDialog::PreShow() {
   UpdateWidgets();
-}
-
-void CompareAsync(const BrowserSkiaGoldPixelDiff* pixel_diff,
-                  const std::string& name,
-                  const views::View* view,
-                  bool* result) {
-  *result = pixel_diff->CompareScreenshot(name, view);
 }
 
 // This returns true if exactly one views widget was shown that is a dialog or
@@ -120,26 +101,15 @@ bool TestBrowserDialog::VerifyUi() {
 #if !defined(OS_MACOSX)
   if (pixel_diff_) {
     // Wait for painting complete.
-    auto* compositor = dialog_widget->GetNativeView()->layer()->GetCompositor();
-    CompositorObserverEnd obend;
-    compositor->AddObserver(&obend);
-    obend.WaitForComplete();
-    compositor->RemoveObserver(&obend);
+    auto* compositor = dialog_widget->GetCompositor();
+    ui::DrawWaiterForTest::WaitForCompositingEnded(compositor);
 
-    base::ScopedAllowBlockingForTesting allow_blocking;
     pixel_diff_->Init(dialog_widget, "BrowserUiDialog");
     auto* test_info = testing::UnitTest::GetInstance()->current_test_info();
     const std::string test_name =
         base::StrCat({test_info->test_case_name(), "_", test_info->name()});
-    bool result = false;
-    base::RunLoop run_loop;
-    base::ThreadTaskRunnerHandle::Get()->PostTaskAndReply(
-        FROM_HERE,
-        base::BindOnce(CompareAsync, pixel_diff_.get(), test_name,
-                       dialog_widget->GetContentsView(), &result),
-        run_loop.QuitClosure());
-    run_loop.Run();
-    if (!result)
+    if (!pixel_diff_->CompareScreenshot(test_name,
+                                        dialog_widget->GetContentsView()))
       return false;
   }
 #endif  // OS_MACOSX
