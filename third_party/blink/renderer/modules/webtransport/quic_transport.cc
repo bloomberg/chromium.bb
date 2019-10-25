c226834cf8f7decbfe0fa6d511d77d3cde9283e4
Yutaka Hirano <yhirano@chromium.org>
2019-10-25 08:41:05 +0000

    Reland "Introduce QuicTransport"
    This is a reland of df72adac85475412556c594f0b0ed6c895facf7e [formerly 93e10d4752d3cfd41accd9ef5986463d4b37b104]. The original
    change was reverted due to a clusterfuzz report, but this CL seems innocent so
    I'm relanding this.
    
    Original change's description:
    > Introduce QuicTransport
    >
    > This is the first CL to implement QuicTransport. This CL
    >  - creates a directory in blink,
    >  - introduces a new blink runtime feature flag, and
    >  - defines QuicTransport IDL interface behind the flag,
    >
    > Bug: 1011392
    > Change-Id: Id432cee2b6d5df6ab4a4c3e8aa6828d5a7937dd9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1868569
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Reviewed-by: Adam Rice <ricea@chromium.org>
    > Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#707303}
    
    TBR=kinuko@chromium.org,ricea@chromium.org,yhirano@chromium.org,tkent@chromium.org
    
    Bug: 1011392, 1016436
    Change-Id: Iad9e785cd5886c7b2d2e25fe55f3b5f327727f87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1880502
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709401}
    
    Former-commit-id: 0174e616aac443e7b57e2518cb1a4553c40fee65

---
+++ third_party/blink/renderer/modules/webtransport/quic_transport.cc
@@ -0,0 +1,13 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/webtransport/quic_transport.h"
+
+namespace blink {
+
+QuicTransport::QuicTransport(ScriptState* script_state, const String& url) {}
+
+void QuicTransport::Dispose() {}
+
+}  // namespace blink
