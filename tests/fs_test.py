1ca6159744f4e24656c1348bf13198ffbc5577d0
Takuto Ikuta <tikuta@chromium.org>
2019-10-25 08:39:48 +0000

    client: update shebang for fs_test.py
    I forget to update shebang in crrev.com/c/1878207
    
    Bug: 1010816
    Change-Id: I87d98f33feb2bc4d14cbdcf9544d2d138024515e
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/1880422
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Cr-Mirrored-From: https://chromium.googlesource.com/infra/luci/luci-py
    Cr-Mirrored-Commit: 0900383420f47db4d7cdefaec6776b6c242347cd

--- tests/fs_test.py
+++ tests/fs_test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env vpython
+#!/usr/bin/env vpython3
 # coding=utf-8
 # Copyright 2019 The LUCI Authors. All rights reserved.
 # Use of this source code is governed under the Apache License, Version 2.0
