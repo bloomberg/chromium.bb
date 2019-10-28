2e6004a79dc9ed766859a4f05a5baa9a5434d9bd
Tom Finegan <tomfinegan@google.com>
2019-10-28 11:45:08 -0700

    metadata test: Fix MSVC warnings.
    Silence type mismatch warnings in MSVC, and do a little
    tidying up.
    
    BUG=aomedia:2507
    
    Change-Id: I62e1d0345b23247447dc6c18d5deedece3cbb093

--- test/metadata_memory_handling_test.cc
+++ test/metadata_memory_handling_test.cc
@@ -5,30 +5,24 @@
 #include "aom_scale/yv12config.h"
 
 TEST(MetadataMemoryHandlingTest, MetadataAllocation) {
-  aom_metadata_t *metadata;
   uint8_t data[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-  metadata = aom_img_metadata_alloc(OBU_METADATA_TYPE_ITUT_T35, data, 10);
-  ASSERT_TRUE(metadata != NULL);
-
-  int status = aom_img_metadata_free(metadata);
-  EXPECT_EQ(status, 0);
+  aom_metadata_t *metadata =
+      aom_img_metadata_alloc(OBU_METADATA_TYPE_ITUT_T35, data, 10);
+  ASSERT_NE(metadata, nullptr);
+  EXPECT_EQ(aom_img_metadata_free(metadata), 0);
 }
 
 TEST(MetadataMemoryHandlingTest, MetadataArrayAllocation) {
-  aom_metadata_array_t *metadata_array;
   uint8_t data[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-  metadata_array = aom_img_metadata_array_alloc(2);
-  ASSERT_TRUE(metadata_array != NULL);
+  aom_metadata_array_t *metadata_array = aom_img_metadata_array_alloc(2);
+  ASSERT_NE(metadata_array, nullptr);
 
   metadata_array->metadata_array[0] =
       aom_img_metadata_alloc(OBU_METADATA_TYPE_ITUT_T35, data, 10);
   metadata_array->metadata_array[1] =
       aom_img_metadata_alloc(OBU_METADATA_TYPE_ITUT_T35, data, 10);
 
-  int status = aom_img_metadata_array_free(metadata_array);
-  EXPECT_EQ(status, 2);
+  EXPECT_EQ(aom_img_metadata_array_free(metadata_array), 2u);
 }
 
 TEST(MetadataMemoryHandlingTest, AddMetadataToImage) {
@@ -36,16 +30,11 @@ TEST(MetadataMemoryHandlingTest, AddMetadataToImage) {
   image.metadata = NULL;
 
   uint8_t data[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-  int status =
-      aom_img_add_metadata(&image, OBU_METADATA_TYPE_ITUT_T35, data, 10);
-  ASSERT_EQ(status, 0);
-
-  status = aom_img_metadata_array_free(image.metadata);
-  EXPECT_EQ(status, 1);
-
-  status = aom_img_add_metadata(NULL, OBU_METADATA_TYPE_ITUT_T35, data, 10);
-  EXPECT_EQ(status, -1);
+  ASSERT_EQ(aom_img_add_metadata(&image, OBU_METADATA_TYPE_ITUT_T35, data, 10),
+            0);
+  EXPECT_EQ(aom_img_metadata_array_free(image.metadata), 1u);
+  EXPECT_EQ(aom_img_add_metadata(NULL, OBU_METADATA_TYPE_ITUT_T35, data, 10),
+            -1);
 }
 
 TEST(MetadataMemoryHandlingTest, RemoveMetadataFromImage) {
@@ -54,26 +43,19 @@ TEST(MetadataMemoryHandlingTest, RemoveMetadataFromImage) {
 
   uint8_t data[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-  int status =
-      aom_img_add_metadata(&image, OBU_METADATA_TYPE_ITUT_T35, data, 10);
-  ASSERT_EQ(status, 0);
-
-  status = aom_img_remove_metadata(&image);
-  EXPECT_EQ(status, 1);
-
-  status = aom_img_remove_metadata(NULL);
-  EXPECT_EQ(status, 0);
+  ASSERT_EQ(aom_img_add_metadata(&image, OBU_METADATA_TYPE_ITUT_T35, data, 10),
+            0);
+  EXPECT_EQ(aom_img_remove_metadata(&image), 1u);
+  EXPECT_EQ(aom_img_remove_metadata(NULL), 0u);
 }
 
 TEST(MetadataMemoryHandlingTest, CopyMetadataToFrameBUffer) {
   YV12_BUFFER_CONFIG yvBuf;
   yvBuf.metadata = NULL;
-  aom_metadata_array_t *metadata_array;
-  aom_metadata_array_t *metadata_array_2;
   uint8_t data[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-  metadata_array = aom_img_metadata_array_alloc(1);
-  ASSERT_TRUE(metadata_array != NULL);
+  aom_metadata_array_t *metadata_array = aom_img_metadata_array_alloc(1);
+  ASSERT_NE(metadata_array, nullptr);
 
   metadata_array->metadata_array[0] =
       aom_img_metadata_alloc(OBU_METADATA_TYPE_ITUT_T35, data, 10);
@@ -81,30 +63,20 @@ TEST(MetadataMemoryHandlingTest, CopyMetadataToFrameBUffer) {
   // Metadata_array
   int status = aom_copy_metadata_to_frame_buffer(&yvBuf, metadata_array);
   EXPECT_EQ(status, 0);
-
   status = aom_copy_metadata_to_frame_buffer(NULL, metadata_array);
   EXPECT_EQ(status, -1);
-
-  status = aom_img_metadata_array_free(metadata_array);
-  EXPECT_EQ(status, 1);
+  EXPECT_EQ(aom_img_metadata_array_free(metadata_array), 1u);
 
   // Metadata_array_2
-  metadata_array_2 = aom_img_metadata_array_alloc(0);
-  ASSERT_TRUE(metadata_array_2 != NULL);
-
+  aom_metadata_array_t *metadata_array_2 = aom_img_metadata_array_alloc(0);
+  ASSERT_NE(metadata_array_2, nullptr);
   status = aom_copy_metadata_to_frame_buffer(&yvBuf, metadata_array_2);
   EXPECT_EQ(status, -1);
-
-  status = aom_img_metadata_array_free(metadata_array_2);
-  EXPECT_EQ(status, 0);
+  EXPECT_EQ(aom_img_metadata_array_free(metadata_array_2), 0u);
 
   // YV12_BUFFER_CONFIG
   status = aom_copy_metadata_to_frame_buffer(&yvBuf, NULL);
   EXPECT_EQ(status, -1);
-
-  status = aom_remove_metadata_from_frame_buffer(NULL);
-  EXPECT_EQ(status, 0);
-
-  status = aom_remove_metadata_from_frame_buffer(&yvBuf);
-  EXPECT_EQ(status, 1);
+  EXPECT_EQ(aom_remove_metadata_from_frame_buffer(NULL), 0u);
+  EXPECT_EQ(aom_remove_metadata_from_frame_buffer(&yvBuf), 1u);
 }
