08b4544109b9eccb39704240a7208ff96cf5fc27
David Schinazi <dschinazi@chromium.org>
2019-10-29 02:52:30 +0000

    Add QUIC_NO_EXPORT
    Change-Id: Ic6e48be74428496bf989f638056880531f3ad06e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1885392
    Commit-Queue: David Schinazi <dschinazi@chromium.org>
    Auto-Submit: David Schinazi <dschinazi@chromium.org>
    Reviewed-by: Nick Harper <nharper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710165}
    
    Former-commit-id: 55e18ed69fd965e213337efd20f32fa3c59d71d5

--- net/quic/platform/impl/quic_export_impl.h
+++ net/quic/platform/impl/quic_export_impl.h
@@ -7,7 +7,11 @@
 
 #include "net/base/net_export.h"
 
+// These macros are documented in:
+// net/third_party/quiche/src/quic/platform/api/quic_export.h
+
 #define QUIC_EXPORT NET_EXPORT
 #define QUIC_EXPORT_PRIVATE NET_EXPORT_PRIVATE
+#define QUIC_NO_EXPORT
 
 #endif  // NET_QUIC_PLATFORM_IMPL_QUIC_EXPORT_IMPL_H_
