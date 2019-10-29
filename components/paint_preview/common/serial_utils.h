fdab262f456f433d003655f1bc6a726199580ad1
Kamila Hasanbega <hkamila@chromium.org>
2019-10-29 08:22:09 +0000

    Revert "[Paint Preview] Compositing Service"
    This reverts commit d4a767b8d28423f1944b7ec2a56291f27ac83e49 [formerly ba5c7d6f8470b86064e35ed6b617f09278c869fd].
    
    Reason for revert: It breaks several tests 
    https://ci.chromium.org/p/chromium/builders/ci/Win7%20Tests%20%28dbg%29%281%29
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20Tests%20%28dbg%29%281%29
    
    
    Original change's description:
    > [Paint Preview] Compositing Service
    > 
    > Adds a paint preview compositor service for compositing collections of
    > SkPictures representing frames into bitmaps. This is very similar
    > in principle to the PDF compositor service, but produces tileable
    > bitmaps rather than a PDF.
    > 
    > A client to this service should start the compositing collection
    > portion of the service in a utility process. The client can then
    > create and delete dedicated compositor instances for each group of
    > SkPictures. Realistically only one compositor will be actively
    > communicating at a time. However, multiple compositors can be
    > "warm" (data is deserialized and in memory for fast switching).
    > 
    > Bug: 1011430
    > Change-Id: I21cb594cd94982f8d4fc5800d9ae4d74b5d9973b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1842273
    > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    > Reviewed-by: Ian Vollick <vollick@chromium.org>
    > Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    > Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#709946}
    
    TBR=vollick@chromium.org,sky@chromium.org,kenrb@chromium.org,blundell@chromium.org,jochen@chromium.org,blundell@google.com,mahmoudi@chromium.org,ckitagawa@chromium.org
    
    Change-Id: Idbeb4bb3989d803f1efcde86f1066b791731b08b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1011430
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1886610
    Reviewed-by: Kamila Hasanbega <hkamila@chromium.org>
    Commit-Queue: Kamila Hasanbega <hkamila@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710251}
    
    Former-commit-id: 29b91f26fe176627115499a161753792add726ad

--- components/paint_preview/common/serial_utils.h
+++ components/paint_preview/common/serial_utils.h
@@ -15,7 +15,6 @@
 #include "third_party/skia/include/core/SkRefCnt.h"
 #include "third_party/skia/include/core/SkSerialProcs.h"
 #include "third_party/skia/include/core/SkTypeface.h"
-#include "ui/gfx/geometry/rect.h"
 
 namespace paint_preview {
 
@@ -34,8 +33,8 @@ struct TypefaceSerializationContext {
   base::flat_set<SkFontID> finished;  // Should be empty on first use.
 };
 
-// Maps a content ID to a clip rect.
-using DeserializationContext = base::flat_map<uint32_t, gfx::Rect>;
+// Maps a content ID to a SkPicture.
+using DeserializationContext = base::flat_map<uint32_t, sk_sp<SkPicture>>;
 
 // Creates a no-op SkPicture.
 sk_sp<SkPicture> MakeEmptyPicture();
