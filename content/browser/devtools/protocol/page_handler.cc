e9ba7e92fd569f705402f523a5b99a7cb0b1bfc2
Yang Guo <yangguo@chromium.org>
2019-10-29 14:37:23 +0000

    Revert "[devtools] Allow reloading of extension with devtools"
    This reverts commit f772116c5a53145b798225ffced88a4043b54325 [formerly 6089b2114b5c5a355759ac226f76839aaab21a4a].
    
    Reason for revert: Failure here: https://ci.chromium.org/p/chromium/builders/ci/linux-trusty-rel/4085
    
    I'm half assuming that this test is a bit flaky?
    
    Original change's description:
    > [devtools] Allow reloading of extension with devtools
    > 
    > This patches restricts the changes from [1] to pdf embeds.
    > 
    > [1]: https://chromium-review.googlesource.com/c/chromium/src/+/1659983
    > 
    > Bug: chromium:997521
    > Change-Id: I8a94c9c1eecf132388508ceed47b426f2c842ed0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1798350
    > Reviewed-by: Yang Guo <yangguo@chromium.org>
    > Reviewed-by: James MacLean <wjmaclean@chromium.org>
    > Commit-Queue: Jan Scheffler <janscheffler@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#708538}
    
    TBR=yangguo@chromium.org,wjmaclean@chromium.org,luoe@chromium.org,janscheffler@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: chromium:997521
    Change-Id: I79849fb39d89d84c4e7a5a9eb72494da13bf5119
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1886817
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Yang Guo <yangguo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710336}
    
    Former-commit-id: 6e72acaf9f2ed84210755c7e890dff2dd14383c2

--- content/browser/devtools/protocol/page_handler.cc
+++ content/browser/devtools/protocol/page_handler.cc
@@ -404,14 +404,11 @@ void PageHandler::Reload(Maybe<bool> bypassCache,
     return;
   }
 
-  // In the case of inspecting a PDF GuestView, we should reload
+  // In the case of inspecting a GuestView (e.g. a PDF), we should reload
   // the outer web contents (embedder), since otherwise reloading the guest by
   // itself will fail.
-  auto* outer_web_contents = web_contents->GetOuterWebContents();
-  if (outer_web_contents &&
-      outer_web_contents->GetContentsMimeType() == "application/pdf") {
-    web_contents = outer_web_contents;
-  }
+  if (web_contents->GetOuterWebContents())
+    web_contents = web_contents->GetOuterWebContents();
 
   // It is important to fallback before triggering reload, so that
   // renderer could prepare beforehand.
