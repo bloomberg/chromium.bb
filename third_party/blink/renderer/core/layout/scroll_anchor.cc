f32969170120fc9098d7624568fc7950debe1103
Nick Burris <nburris@chromium.org>
2019-10-30 22:23:33 +0000

    Consider scroll-padding when determining scroll anchor node
    Scroll anchoring should not anchor to a node that is behind scroll
    padding.
    
    Bug: 1010002
    Change-Id: Icbd89fb85ea2c97a6de635930a9896f6a87b8f07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1887745
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711020}
    
    Former-commit-id: a0ae9ed7bc545314dfcd4882db3498fda1808b4f

--- third_party/blink/renderer/core/layout/scroll_anchor.cc
+++ third_party/blink/renderer/core/layout/scroll_anchor.cc
@@ -292,6 +292,15 @@ ScrollAnchor::ExamineResult ScrollAnchor::Examine(
   LayoutRect visible_rect =
       ScrollerLayoutBox(scroller_)->OverflowClipRect(LayoutPoint());
 
+  const ComputedStyle* style = ScrollerLayoutBox(scroller_)->Style();
+  LayoutRectOutsets scroll_padding(
+      MinimumValueForLength(style->ScrollPaddingTop(), visible_rect.Height()),
+      MinimumValueForLength(style->ScrollPaddingRight(), visible_rect.Width()),
+      MinimumValueForLength(style->ScrollPaddingBottom(),
+                            visible_rect.Height()),
+      MinimumValueForLength(style->ScrollPaddingLeft(), visible_rect.Width()));
+  visible_rect.Contract(scroll_padding);
+
   bool occupies_space =
       candidate_rect.Width() > 0 && candidate_rect.Height() > 0;
   if (occupies_space && visible_rect.Intersects(candidate_rect)) {
