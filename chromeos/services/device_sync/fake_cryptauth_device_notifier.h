dee7b804428c8411f7532317fd1d8290425d372b
Josh Nohle <nohle@google.com>
2019-10-30 16:30:21 +0000

    [DeviceSync v2] Add CryptAuthDeviceNotifier class
    Handles the BatchNotifyGroupDevices portion of the CryptAuth v2
    DeviceSync protocol, which sends a GCM message--via CryptAuth--to
    a subset of devices in the "DeviceSync:BetterTogether" group.
    
    The implementation queues requests made via NotifyDevices() and
    processes the requests sequentially. The implementation also handles
    timeouts internally, so a callback passed to NotifyDevices() is always
    guaranteed to be invoked.
    
    CryptAuthDeviceNotifier::NotifyDevices() will replace the CryptAuth v1
    method SoftwareFeatureManager::FindEligibleDevices(), which was used to
    notify a phone that it was selected as the multi-device host.
    
    Bug: 951969
    Change-Id: I155e35847f5d6d9d99d8cf4477e70819d82d8e21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1888734
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710817}
    
    Former-commit-id: ab93f9f0bafc5d53353e831993afc97cd7d69193

---
+++ chromeos/services/device_sync/fake_cryptauth_device_notifier.h
@@ -0,0 +1,110 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_SERVICES_DEVICE_SYNC_FAKE_CRYPTAUTH_DEVICE_NOTIFIER_H_
+#define CHROMEOS_SERVICES_DEVICE_SYNC_FAKE_CRYPTAUTH_DEVICE_NOTIFIER_H_
+
+#include <memory>
+#include <string>
+
+#include "base/callback.h"
+#include "base/containers/flat_set.h"
+#include "base/containers/queue.h"
+#include "base/macros.h"
+#include "base/timer/timer.h"
+#include "chromeos/services/device_sync/cryptauth_device_notifier.h"
+#include "chromeos/services/device_sync/cryptauth_device_notifier_impl.h"
+#include "chromeos/services/device_sync/network_request_error.h"
+#include "chromeos/services/device_sync/proto/cryptauth_common.pb.h"
+
+namespace chromeos {
+
+namespace device_sync {
+
+class ClientAppMetadataProvider;
+class CryptAuthClientFactory;
+class CryptAuthGCMManager;
+
+class FakeCryptAuthDeviceNotifier : public CryptAuthDeviceNotifier {
+ public:
+  struct Request {
+    Request(const base::flat_set<std::string>& device_ids,
+            cryptauthv2::TargetService target_service,
+            CryptAuthFeatureType feature_type,
+            base::OnceClosure success_callback,
+            base::OnceCallback<void(NetworkRequestError)> error_callback);
+
+    Request(Request&& request);
+
+    ~Request();
+
+    base::flat_set<std::string> device_ids;
+    cryptauthv2::TargetService target_service;
+    CryptAuthFeatureType feature_type;
+    base::OnceClosure success_callback;
+    base::OnceCallback<void(NetworkRequestError)> error_callback;
+  };
+
+  FakeCryptAuthDeviceNotifier();
+  ~FakeCryptAuthDeviceNotifier() override;
+
+  base::queue<Request>& requests() { return requests_; }
+
+ private:
+  // CryptAuthDeviceNotifier:
+  void NotifyDevices(
+      const base::flat_set<std::string>& device_ids,
+      cryptauthv2::TargetService target_service,
+      CryptAuthFeatureType feature_type,
+      base::OnceClosure success_callback,
+      base::OnceCallback<void(NetworkRequestError)> error_callback) override;
+
+  base::queue<Request> requests_;
+
+  DISALLOW_COPY_AND_ASSIGN(FakeCryptAuthDeviceNotifier);
+};
+
+class FakeCryptAuthDeviceNotifierFactory
+    : public CryptAuthDeviceNotifierImpl::Factory {
+ public:
+  FakeCryptAuthDeviceNotifierFactory();
+  ~FakeCryptAuthDeviceNotifierFactory() override;
+
+  const std::vector<FakeCryptAuthDeviceNotifier*>& instances() const {
+    return instances_;
+  }
+
+  const ClientAppMetadataProvider* last_client_app_metadata_provider() const {
+    return last_client_app_metadata_provider_;
+  }
+
+  const CryptAuthClientFactory* last_client_factory() const {
+    return last_client_factory_;
+  }
+
+  const CryptAuthGCMManager* last_gcm_manager() const {
+    return last_gcm_manager_;
+  }
+
+ private:
+  // CryptAuthDeviceNotifierImpl::Factory:
+  std::unique_ptr<CryptAuthDeviceNotifier> BuildInstance(
+      ClientAppMetadataProvider* client_app_metadata_provider,
+      CryptAuthClientFactory* client_factory,
+      CryptAuthGCMManager* gcm_manager,
+      std::unique_ptr<base::OneShotTimer> timer = nullptr) override;
+
+  std::vector<FakeCryptAuthDeviceNotifier*> instances_;
+  ClientAppMetadataProvider* last_client_app_metadata_provider_ = nullptr;
+  CryptAuthClientFactory* last_client_factory_ = nullptr;
+  CryptAuthGCMManager* last_gcm_manager_ = nullptr;
+
+  DISALLOW_COPY_AND_ASSIGN(FakeCryptAuthDeviceNotifierFactory);
+};
+
+}  // namespace device_sync
+
+}  // namespace chromeos
+
+#endif  //  CHROMEOS_SERVICES_DEVICE_SYNC_FAKE_CRYPTAUTH_DEVICE_NOTIFIER_H_
