cf6e973f8582b2108c69ad486ce960a7c87bf45c
Daniel Rubery <drubery@chromium.org>
2019-10-31 00:32:01 +0000

    Preserve original Safe Browsing verdict for download deep scanning.
    In cases where deep scanning fails, we currently supply a SAFE verdict.
    If the original verdict from Safe Browsing was UNCOMMON, we will
    therefore downgrade the warning. This CL preserves the original verdict
    and returns it in cases where deep scanning fails. It also adds a test
    implementation of the BinaryUploadService, for easier testing of code
    using uploads.
    
    Fixed: 1017921
    Change-Id: I444b301c3fe75ebbe0561bc6b201945242f09dc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1887736
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Bettina Dea <bdea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711049}
    
    Former-commit-id: 1a90cb5cfac69f14aaeda25fc4fb78c2a698baa4

--- chrome/browser/safe_browsing/download_protection/check_client_download_request_base.h
+++ chrome/browser/safe_browsing/download_protection/check_client_download_request_base.h
@@ -140,7 +140,8 @@ class CheckClientDownloadRequestBase {
 
   // If ShouldUploadBinary is true, actually performs the upload to Safe
   // Browsing for deep scanning.
-  virtual void UploadBinary(DownloadCheckResultReason reason) = 0;
+  virtual void UploadBinary(DownloadCheckResult result,
+                            DownloadCheckResultReason reason) = 0;
 
   // Called whenever a request has completed.
   virtual void NotifyRequestFinished(DownloadCheckResult result,
---
+++ chrome/browser/safe_browsing/download_protection/test_binary_upload_service.h
@@ -0,0 +1,31 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SAFE_BROWSING_DOWNLOAD_PROTECTION_TEST_BINARY_UPLOAD_SERVICE_H_
+#define CHROME_BROWSER_SAFE_BROWSING_DOWNLOAD_PROTECTION_TEST_BINARY_UPLOAD_SERVICE_H_
+
+#include <memory>
+
+#include "chrome/browser/safe_browsing/download_protection/binary_upload_service.h"
+#include "chrome/browser/safe_browsing/services_delegate.h"
+#include "components/safe_browsing/proto/webprotect.pb.h"
+
+namespace safe_browsing {
+
+class TestBinaryUploadService : public BinaryUploadService {
+ public:
+  TestBinaryUploadService();
+  ~TestBinaryUploadService() override = default;
+
+  void MaybeUploadForDeepScanning(std::unique_ptr<Request> request) override;
+  void SetResponse(Result result, DeepScanningClientResponse response);
+
+ private:
+  Result saved_result_;
+  DeepScanningClientResponse saved_response_;
+};
+
+}  // namespace safe_browsing
+
+#endif  // CHROME_BROWSER_SAFE_BROWSING_DOWNLOAD_PROTECTION_TEST_BINARY_UPLOAD_SERVICE_H_
