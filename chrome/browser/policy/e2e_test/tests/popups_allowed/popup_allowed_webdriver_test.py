998a6f1d8b9561dd58fe8d3306be922ffc94aed2
Xiang Ji <jxiang@google.com>
2019-10-31 19:44:18 +0000

    Replace test URL for fixing test failure.
    TODO: crbug/1020231 add customized IIS site support in CEL
    
    Bug: 1018819
    Change-Id: I0502f5f7d686e77ae1779a94d8debd385efbd130
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1884990
    Commit-Queue: Xiang Ji <jxiang@google.com>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711404}
    
    Former-commit-id: d9e48f6388d05b4186c2fa5a0b4f4c095b4b7c62

--- chrome/browser/policy/e2e_test/tests/popups_allowed/popup_allowed_webdriver_test.py
+++ chrome/browser/policy/e2e_test/tests/popups_allowed/popup_allowed_webdriver_test.py
@@ -2,16 +2,23 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from absl import app
 from selenium import webdriver
 
 import test_util
 
-testSite = "http://www.popuptest.com/popuptest1.html"
-options = webdriver.ChromeOptions()
-options.add_experimental_option('excludeSwitches', ['disable-popup-blocking'])
-driver = test_util.create_chrome_webdriver(chrome_options=options)
-driver.implicitly_wait(5)
-driver.get(testSite)
-handles = driver.window_handles
-print len(handles)
-driver.quit()
+
+def main(argv):
+  testSite = "http://www.dummysoftware.com/popupdummy_testpage.html"
+  options = webdriver.ChromeOptions()
+  options.add_experimental_option('excludeSwitches', ['disable-popup-blocking'])
+  driver = test_util.create_chrome_webdriver(chrome_options=options)
+  driver.implicitly_wait(5)
+  driver.get(testSite)
+  handles = driver.window_handles
+  print len(handles)
+  driver.quit()
+
+
+if __name__ == '__main__':
+  app.run(main)
