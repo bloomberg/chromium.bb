73e27229f4c625171ceea34b8fe525d04b7b8778
Jeremy Apthorp <jeremya@chromium.org>
2019-10-31 21:19:15 +0000

    qualify base::data in make_span
    this fixes an ambiguous reference error when compiling with MSSTL
    
    Change-Id: I19a56e9268cac76afc8dd3fcf391b3a2d5fe7d2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1891762
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Auto-Submit: Jeremy Apthorp <jeremya@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711456}
    
    Former-commit-id: 9d4a758a49c51e723c71b0c54a62ae83c6f8e3fd

--- base/containers/span.h
+++ base/containers/span.h
@@ -509,7 +509,7 @@ template <
         decltype(base::data(std::declval<const Container&>()))>,
     typename = internal::EnableIfSpanCompatibleContainer<const Container&, T>>
 constexpr span<T, N> make_span(const Container& container) noexcept {
-  return span<T, N>(data(container), size(container));
+  return span<T, N>(base::data(container), base::size(container));
 }
 
 template <int&... ExplicitArgumentBarrier, typename T, size_t X>
