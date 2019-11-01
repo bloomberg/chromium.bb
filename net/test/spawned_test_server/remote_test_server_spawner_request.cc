dfc6ef6f6e9786dea8d0f112be7e21168b5dd35d
Andrii Shyshkalov <tandrii@google.com>
2019-11-01 15:35:22 +0000

    Revert "Remove SpawnedTestServer timeouts."
    This reverts commit ff1554656c0bb262513d48da2149bb72ff6fa42e [formerly 8e4bd23845503ebf9969124b8ab10bb69ef738f4].
    
    Reason for revert: want to verify that workarounds to https://crbug.com/869227 inside vpython itself are actually useful
    plus to read more logs.
    
    The original can re-landed any time if new occurrences of https://crbug.com/869227 manifest themselves.
    
    Original change's description:
    > Remove SpawnedTestServer timeouts.
    > 
    > These timeouts are ancient. The test infrastructure has its own
    > timeouts, which should make these obsolete. Also, this may help with
    > https://crbug.com/869227, where starting the Python test server
    > times out.
    > 
    > Bug: 869227
    > Change-Id: I9ea4f3174bcaa86daf09b965c82921432d8d556a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1869717
    > Commit-Queue: Matt Menke <mmenke@chromium.org>
    > Reviewed-by: David Benjamin <davidben@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#707894}
    
    TBR=davidben@chromium.org,mmenke@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 869227
    Change-Id: Ic11c2e9a78568738a7712a1141876840f8a63740
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1893789
    Auto-Submit: Andrii Shyshkalov <tandrii@google.com>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711706}
    
    Former-commit-id: f6ec475d5cfd5b24a54370cbc51ca5a6fb81c3f2

--- net/test/spawned_test_server/remote_test_server_spawner_request.cc
+++ net/test/spawned_test_server/remote_test_server_spawner_request.cc
@@ -12,7 +12,10 @@
 #include "base/macros.h"
 #include "base/single_thread_task_runner.h"
 #include "base/synchronization/waitable_event.h"
+#include "base/test/test_timeouts.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "build/build_config.h"
 #include "net/base/elements_upload_data_stream.h"
 #include "net/base/io_buffer.h"
@@ -47,6 +50,7 @@ class RemoteTestServerSpawnerRequest::Core : public URLRequest::Delegate {
 
   void ReadResponse();
   void OnCommandCompleted(int net_error);
+  void OnTimeout();
 
   // Request results.
   int result_code_ = 0;
@@ -59,6 +63,8 @@ class RemoteTestServerSpawnerRequest::Core : public URLRequest::Delegate {
   std::unique_ptr<URLRequest> request_;
 
   scoped_refptr<IOBuffer> read_buffer_;
+
+  std::unique_ptr<base::OneShotTimer> timeout_timer_;
 
   THREAD_CHECKER(thread_checker_);
 
@@ -96,6 +102,10 @@ void RemoteTestServerSpawnerRequest::Core::SendRequest(
                                           /*override=*/true);
   }
 
+  timeout_timer_ = std::make_unique<base::OneShotTimer>();
+  timeout_timer_->Start(FROM_HERE, TestTimeouts::action_max_timeout(),
+                        base::Bind(&Core::OnTimeout, base::Unretained(this)));
+
   request_->Start();
 }
 
@@ -112,6 +122,13 @@ bool RemoteTestServerSpawnerRequest::Core::WaitForCompletion(
   if (response)
     *response = data_received_;
   return result_code_ == OK;
+}
+
+void RemoteTestServerSpawnerRequest::Core::OnTimeout() {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+
+  int result = request_->CancelWithError(ERR_TIMED_OUT);
+  OnCommandCompleted(result);
 }
 
 void RemoteTestServerSpawnerRequest::Core::OnCommandCompleted(int net_error) {
@@ -135,6 +152,7 @@ void RemoteTestServerSpawnerRequest::Core::OnCommandCompleted(int net_error) {
 
   request_.reset();
   context_.reset();
+  timeout_timer_.reset();
 
   event_.Signal();
 }
