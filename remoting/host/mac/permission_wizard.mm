328b53f13411e0f990ac0ff041d9d2d25f49a8c5
Lambros Lambrou <lambroslambrou@chromium.org>
2019-11-01 23:08:22 +0000

    Add pages to PermissionWizard
    This adds internal state to the PermissionWizard class, and logic to
    show the UI for all pages and states of the wizard.
    
    This also fixes the button action selectors and targets.
    
    Bug: 1015201
    Change-Id: I49690bceba7f9f55ae07ddde171b5a4eda4faa3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1895786
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711917}
    
    Former-commit-id: 92bafbfe0ad424ffee75a556e26b0d72f22c3432

--- remoting/host/mac/permission_wizard.mm
+++ remoting/host/mac/permission_wizard.mm
@@ -14,17 +14,44 @@
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/l10n/l10n_util_mac.h"
 
+namespace {
+
+// The steps of the wizard.
+enum class WizardPage {
+  ACCESSIBILITY,
+  SCREEN_RECORDING,
+  ALL_SET,
+};
+
+}  // namespace
+
 @interface PermissionWizardController : NSWindowController
 
+- (instancetype)initWithWindow:(NSWindow*)window;
 - (void)hide;
 - (void)initializeWindow;
-- (void)onNextButton;
 
 @end
 
 @implementation PermissionWizardController {
   NSTextField* _instructionText;
+  NSButton* _cancelButton;
   NSButton* _nextButton;
+
+  // The page of the wizard being shown.
+  WizardPage _page;
+
+  // Whether the relevant permission has been granted for the current page. If
+  // YES, the user will be able to advance to the next page of the wizard.
+  BOOL _hasPermission;
+}
+
+- (instancetype)initWithWindow:(NSWindow*)window {
+  self = [super initWithWindow:(NSWindow*)window];
+  if (self) {
+    _page = WizardPage::ACCESSIBILITY;
+  }
+  return self;
 }
 
 - (void)hide {
@@ -36,15 +63,8 @@
       l10n_util::GetNSStringF(IDS_MAC_PERMISSION_WIZARD_TITLE,
                               l10n_util::GetStringUTF16(IDS_PRODUCT_NAME));
 
-  // TODO(lambroslambrou): Parameterize the name of the app needing permission.
   _instructionText = [[[NSTextField alloc] init] autorelease];
   _instructionText.translatesAutoresizingMaskIntoConstraints = NO;
-  _instructionText.stringValue = l10n_util::GetNSStringF(
-      IDS_ACCESSIBILITY_PERMISSION_DIALOG_BODY_TEXT,
-      l10n_util::GetStringUTF16(IDS_PRODUCT_NAME),
-      l10n_util::GetStringUTF16(
-          IDS_ACCESSIBILITY_PERMISSION_DIALOG_OPEN_BUTTON),
-      base::SysNSStringToUTF16(@"ChromeRemoteDesktopHost"));
   _instructionText.drawsBackground = NO;
   _instructionText.bezeled = NO;
   _instructionText.editable = NO;
@@ -54,21 +74,21 @@
   icon.translatesAutoresizingMaskIntoConstraints = NO;
   icon.image = [[NSApplication sharedApplication] applicationIconImage];
 
-  NSButton* cancelButton = [[[NSButton alloc] init] autorelease];
-  cancelButton.translatesAutoresizingMaskIntoConstraints = NO;
-  cancelButton.buttonType = NSButtonTypeMomentaryPushIn;
-  cancelButton.bezelStyle = NSBezelStyleRegularSquare;
-  cancelButton.title =
+  _cancelButton = [[[NSButton alloc] init] autorelease];
+  _cancelButton.translatesAutoresizingMaskIntoConstraints = NO;
+  _cancelButton.buttonType = NSButtonTypeMomentaryPushIn;
+  _cancelButton.bezelStyle = NSBezelStyleRegularSquare;
+  _cancelButton.title =
       l10n_util::GetNSString(IDS_MAC_PERMISSION_WIZARD_CANCEL_BUTTON);
-  cancelButton.action = @selector(hide:);
+  _cancelButton.action = @selector(onCancel:);
+  _cancelButton.target = self;
 
   _nextButton = [[[NSButton alloc] init] autorelease];
   _nextButton.translatesAutoresizingMaskIntoConstraints = NO;
   _nextButton.buttonType = NSButtonTypeMomentaryPushIn;
   _nextButton.bezelStyle = NSBezelStyleRegularSquare;
-  _nextButton.title =
-      l10n_util::GetNSString(IDS_ACCESSIBILITY_PERMISSION_DIALOG_OPEN_BUTTON);
-  _nextButton.action = @selector(onNextButton:);
+  _nextButton.action = @selector(onNext:);
+  _nextButton.target = self;
 
   NSStackView* iconAndTextStack = [[[NSStackView alloc] init] autorelease];
   iconAndTextStack.translatesAutoresizingMaskIntoConstraints = NO;
@@ -81,7 +101,7 @@
   NSStackView* buttonsStack = [[[NSStackView alloc] init] autorelease];
   buttonsStack.translatesAutoresizingMaskIntoConstraints = NO;
   buttonsStack.orientation = NSUserInterfaceLayoutOrientationHorizontal;
-  [buttonsStack addView:cancelButton inGravity:NSStackViewGravityTrailing];
+  [buttonsStack addView:_cancelButton inGravity:NSStackViewGravityTrailing];
   [buttonsStack addView:_nextButton inGravity:NSStackViewGravityTrailing];
 
   NSStackView* mainStack = [[[NSStackView alloc] init] autorelease];
@@ -127,12 +147,78 @@
                                              metrics:nil
                                                views:views]];
 
+  [self updateUI];
+
   [self.window makeKeyAndOrderFront:NSApp];
   [self.window center];
 }
 
-- (void)onNextButton {
+- (void)onCancel:(id)sender {
+  [self hide];
+}
+
+- (void)onNext:(id)sender {
   NOTIMPLEMENTED();
+}
+
+// Updates the dialog controls according to the object's state.
+- (void)updateUI {
+  // TODO(lambroslambrou): Parameterize the name of the app needing permission.
+  switch (_page) {
+    case WizardPage::ACCESSIBILITY:
+      _instructionText.stringValue = l10n_util::GetNSStringF(
+          IDS_ACCESSIBILITY_PERMISSION_DIALOG_BODY_TEXT,
+          l10n_util::GetStringUTF16(IDS_PRODUCT_NAME),
+          l10n_util::GetStringUTF16(
+              IDS_ACCESSIBILITY_PERMISSION_DIALOG_OPEN_BUTTON),
+          base::SysNSStringToUTF16(@"ChromeRemoteDesktopHost"));
+      break;
+    case WizardPage::SCREEN_RECORDING:
+      _instructionText.stringValue = l10n_util::GetNSStringF(
+          IDS_SCREEN_RECORDING_PERMISSION_DIALOG_BODY_TEXT,
+          l10n_util::GetStringUTF16(IDS_PRODUCT_NAME),
+          l10n_util::GetStringUTF16(
+              IDS_SCREEN_RECORDING_PERMISSION_DIALOG_OPEN_BUTTON),
+          base::SysNSStringToUTF16(@"ChromeRemoteDesktopHost"));
+      break;
+    case WizardPage::ALL_SET:
+      _instructionText.stringValue =
+          l10n_util::GetNSString(IDS_MAC_PERMISSION_WIZARD_FINAL_TEXT);
+      break;
+    default:
+      NOTREACHED();
+  }
+  _cancelButton.hidden = (_page == WizardPage::ALL_SET);
+  [self updateNextButton];
+}
+
+// Updates |_nextButton| according to the object's state.
+- (void)updateNextButton {
+  if (_page == WizardPage::ALL_SET) {
+    _nextButton.title =
+        l10n_util::GetNSString(IDS_MAC_PERMISSION_WIZARD_OK_BUTTON);
+    return;
+  }
+
+  if (_hasPermission) {
+    _nextButton.title =
+        l10n_util::GetNSString(IDS_MAC_PERMISSION_WIZARD_NEXT_BUTTON);
+    return;
+  }
+
+  // Permission is not granted, so show the appropriate launch text.
+  switch (_page) {
+    case WizardPage::ACCESSIBILITY:
+      _nextButton.title = l10n_util::GetNSString(
+          IDS_ACCESSIBILITY_PERMISSION_DIALOG_OPEN_BUTTON);
+      break;
+    case WizardPage::SCREEN_RECORDING:
+      _nextButton.title = l10n_util::GetNSString(
+          IDS_SCREEN_RECORDING_PERMISSION_DIALOG_OPEN_BUTTON);
+      break;
+    default:
+      NOTREACHED();
+  }
 }
 
 @end
