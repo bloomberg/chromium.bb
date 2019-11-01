dbac7644c398bdd4097c50d450134066271f3c61
Ahmed Fakhry <afakhry@google.com>
2019-11-01 00:43:11 +0000

    Desks: Add animation smoothness stats
    This CL adds UMA states to track the smoothness of the desk
    activation, and desk removal animations.
    
    BUG=1016994
    TEST=No behavior changes, all tests should pass.
    
    Change-Id: Ia0630d5c9a9265c41bfb05d5aa0eb182e438e036
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1891766
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711554}
    
    Former-commit-id: bb5c66871bf08c69c5e910269ab05a3173073e10

--- ash/wm/desks/desks_controller.cc
+++ ash/wm/desks/desks_controller.cc
@@ -7,6 +7,7 @@
 #include <utility>
 
 #include "ash/accessibility/accessibility_controller_impl.h"
+#include "ash/public/cpp/fps_counter.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
@@ -50,6 +51,10 @@ constexpr char kNumberOfWindowsOnDesk_3_HistogramName[] =
     "Ash.Desks.NumberOfWindowsOnDesk_3";
 constexpr char kNumberOfWindowsOnDesk_4_HistogramName[] =
     "Ash.Desks.NumberOfWindowsOnDesk_4";
+constexpr char kDeskActivationSmoothnessHistogramName[] =
+    "Ash.Desks.AnimationSmoothness.DeskActivation";
+constexpr char kDeskRemovalSmoothnessHistogramName[] =
+    "Ash.Desks.AnimationSmoothness.DeskRemoval";
 
 // Appends the given |windows| to the end of the currently active overview mode
 // session such that the most-recently used window is added first. If
@@ -85,6 +90,19 @@ void RemoveWindowsFromOverview(const base::flat_set<aura::Window*>& windows) {
   }
 }
 
+// Selects and returns the compositor that the FpsCounter will use to measure
+// the animation smoothness.
+ui::Compositor* GetSelectedCompositorForAnimationSmoothness() {
+  // Favor the compositor associated with the active window's root window (if
+  // any), or that of the primary root window.
+  auto* active_window = window_util::GetActiveWindow();
+  auto* selected_root = active_window && active_window->GetRootWindow()
+                            ? active_window->GetRootWindow()
+                            : Shell::GetPrimaryRootWindow();
+  DCHECK(selected_root);
+  return selected_root->layer()->GetCompositor();
+}
+
 }  // namespace
 
 // -----------------------------------------------------------------------------
@@ -107,6 +125,9 @@ class DesksController::DeskAnimationBase
   void Launch() {
     for (auto& observer : controller_->observers_)
       observer.OnDeskSwitchAnimationLaunching();
+
+    fps_counter_ = std::make_unique<FpsCounter>(
+        GetSelectedCompositorForAnimationSmoothness());
 
     DCHECK(!desk_switch_animators_.empty());
     for (auto& animator : desk_switch_animators_)
@@ -169,10 +190,13 @@ class DesksController::DeskAnimationBase
 
     desk_switch_animators_.clear();
 
+    ComputeAnimationSmoothnessAndReport();
+
     for (auto& observer : controller_->observers_)
       observer.OnDeskSwitchAnimationFinished();
 
     controller_->OnAnimationFinished(this);
+    // `this` is now deleted.
   }
 
  protected:
@@ -187,6 +211,12 @@ class DesksController::DeskAnimationBase
       const Desk* ending_desk) = 0;
   virtual void OnDeskSwitchAnimationFinishedInternal() = 0;
 
+  // Since performance here matters, we have to use the UMA histograms macros to
+  // report the smoothness histograms, but each macro use has to be associated
+  // with exactly one histogram name. This function allows subclasses to report
+  // the histogram using the macro with their desired name.
+  virtual void ReportSmoothness(int smoothness) const = 0;
+
   DesksController* const controller_;
 
   // An animator object per each root. Once all the animations are complete,
@@ -195,6 +225,18 @@ class DesksController::DeskAnimationBase
       desk_switch_animators_;
 
  private:
+  // Computes the animation smoothness and reports an UMA stat for it.
+  void ComputeAnimationSmoothnessAndReport() {
+    DCHECK(fps_counter_);
+    const int smoothness = fps_counter_->ComputeSmoothness();
+    if (smoothness < 0)
+      return;
+    ReportSmoothness(smoothness);
+  }
+
+  // The FPS counter used for measuring this animation smoothness.
+  std::unique_ptr<FpsCounter> fps_counter_;
+
   DISALLOW_COPY_AND_ASSIGN(DeskAnimationBase);
 };
 
@@ -249,6 +291,11 @@ class DesksController::DeskActivationAnimation
   }
 
   void OnDeskSwitchAnimationFinishedInternal() override {}
+
+  void ReportSmoothness(int smoothness) const override {
+    UMA_HISTOGRAM_PERCENTAGE(kDeskActivationSmoothnessHistogramName,
+                             smoothness);
+  }
 
  private:
   DISALLOW_COPY_AND_ASSIGN(DeskActivationAnimation);
@@ -307,6 +354,10 @@ class DesksController::DeskRemovalAnimation
     controller_->RemoveDeskInternal(desk_to_remove_, request_source_);
 
     MaybeRestoreSplitView(/*refresh_snapped_windows=*/true);
+  }
+
+  void ReportSmoothness(int smoothness) const override {
+    UMA_HISTOGRAM_PERCENTAGE(kDeskRemovalSmoothnessHistogramName, smoothness);
   }
 
  private:
