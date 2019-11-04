0dbb69d71674e0038d53ac072e3665784969e502
nancy <nancylingwang@chromium.org>
2019-11-04 10:07:46 +0000

    Remove kAppServiceAsh from sync test.
    kAppServiceAsh is the flag to integrate AppService in UI app_list.
    Since it has been enabled for a few months for M78, M79 release, it
    should not be disabled, so the flag kAppServiceAsh can be removed.
    
    BUG=1016159
    
    Change-Id: I5ebab190007370286738308e80d8354ea651493f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1892978
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712067}
    
    Former-commit-id: 9a30f42a7c6c877599ae370718b9609c769db341

--- chrome/browser/sync/test/integration/apps_helper.cc
+++ chrome/browser/sync/test/integration/apps_helper.cc
@@ -15,7 +15,6 @@
 #include "chrome/browser/sync/test/integration/sync_datatype_helper.h"
 #include "chrome/browser/sync/test/integration/sync_extension_helper.h"
 #include "chrome/browser/sync/test/integration/sync_extension_installer.h"
-#include "chrome/common/chrome_features.h"
 #include "content/public/browser/notification_service.h"
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/extension_registry.h"
@@ -116,9 +115,6 @@ void WaitForAppService(Profile* profile) {
   // (because they are potentially IPC calls). When the tests install and
   // uninstall apps, they may need to pump the run loop so that those async
   // calls settle.
-  if (!base::FeatureList::IsEnabled(features::kAppServiceAsh))
-    return;
-
   apps::AppServiceProxyFactory::GetForProfile(profile)
       ->FlushMojoCallsForTesting();
 }
