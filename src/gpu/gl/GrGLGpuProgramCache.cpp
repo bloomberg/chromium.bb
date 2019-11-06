91d99230844557a53e5ecb75f9588d86e53b5a18
Imran Haider <ihaider3@bloomberg.net>
2019-11-05 23:46:23 -0500

    Squashed 'src/third_party/skia/' content from commit a40a8a58755f9
    git-subtree-dir: src/third_party/skia
    git-subtree-split: a40a8a58755f952280cac9dfdcbf761aa7d29a52

---
+++ src/gpu/gl/GrGLGpuProgramCache.cpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include "src/gpu/gl/GrGLGpu.h"
+
+#include "src/gpu/GrProcessor.h"
+#include "src/gpu/GrProgramDesc.h"
+#include "src/gpu/gl/builders/GrGLProgramBuilder.h"
+#include "src/gpu/glsl/GrGLSLFragmentProcessor.h"
+
+struct GrGLGpu::ProgramCache::Entry {
+    Entry(sk_sp<GrGLProgram> program) : fProgram(std::move(program)) {}
+
+    sk_sp<GrGLProgram> fProgram;
+};
+
+GrGLGpu::ProgramCache::ProgramCache(GrGLGpu* gpu)
+    : fMap(kMaxEntries)
+    , fGpu(gpu) {}
+
+GrGLGpu::ProgramCache::~ProgramCache() {}
+
+void GrGLGpu::ProgramCache::abandon() {
+    fMap.foreach([](std::unique_ptr<Entry>* e) {
+        (*e)->fProgram->abandon();
+    });
+
+    this->reset();
+}
+
+void GrGLGpu::ProgramCache::reset() {
+    fMap.reset();
+}
+
+GrGLProgram* GrGLGpu::ProgramCache::refProgram(GrGLGpu* gpu,
+                                               GrRenderTarget* renderTarget,
+                                               GrSurfaceOrigin origin,
+                                               const GrPrimitiveProcessor& primProc,
+                                               const GrTextureProxy* const primProcProxies[],
+                                               const GrPipeline& pipeline,
+                                               bool isPoints) {
+    // Get GrGLProgramDesc
+    GrProgramDesc desc;
+    if (!GrProgramDesc::Build(&desc, renderTarget, primProc, isPoints, pipeline, gpu)) {
+        GrCapsDebugf(gpu->caps(), "Failed to gl program descriptor!\n");
+        return nullptr;
+    }
+    // If we knew the shader won't depend on origin, we could skip this (and use the same program
+    // for both origins). Instrumenting all fragment processors would be difficult and error prone.
+    desc.setSurfaceOriginKey(GrGLSLFragmentShaderBuilder::KeyForSurfaceOrigin(origin));
+
+    std::unique_ptr<Entry>* entry = fMap.find(desc);
+    if (!entry) {
+        // We have a cache miss
+        GrGLProgram* program = GrGLProgramBuilder::CreateProgram(renderTarget, origin,
+                                                                 primProc, primProcProxies,
+                                                                 pipeline, &desc, fGpu);
+        if (nullptr == program) {
+            return nullptr;
+        }
+        entry = fMap.insert(desc, std::unique_ptr<Entry>(new Entry(sk_sp<GrGLProgram>(program))));
+    }
+
+    return SkRef((*entry)->fProgram.get());
+}
