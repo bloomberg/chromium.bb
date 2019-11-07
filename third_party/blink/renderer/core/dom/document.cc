ac7c607b48c5f46bc1f14a5ad583015ce7076c29
Dave Tapuska <dtapuska@chromium.org>
2019-11-07 14:41:45 +0000

    Adjust DCHECK to include inactive documents for scrollingElement.
    The DCHECK already covers the stopped/stopping states, so the only states it
    doesn't cover are kInactive and kUninitialized. Since JS is calling
    this API, the kUninitialized state is impossible. So the only state that
    this occurs in is kInactive. The method can return null in quirks mode
    so adjusting the DCHECK for inactive documents seems best.
    
    BUG=1020787, 1002472
    
    Change-Id: Ief51eb7783ea7ed506107646a258f24e274127b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1902007
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713427}
    
    Former-commit-id: f2cad857624d48a1c74cdaf5d2eb39abfacd7210

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -2048,7 +2048,8 @@ Element* Document::scrollingElement() {
 Element* Document::ScrollingElementNoLayout() {
   if (RuntimeEnabledFeatures::ScrollTopLeftInteropEnabled()) {
     if (InQuirksMode()) {
-      DCHECK(lifecycle_.GetState() >= DocumentLifecycle::kStyleClean);
+      DCHECK(!IsActive() ||
+             lifecycle_.GetState() >= DocumentLifecycle::kStyleClean);
       HTMLBodyElement* body = FirstBodyElement();
       if (body && body->GetLayoutObject() &&
           body->GetLayoutObject()->HasOverflowClip())
