9acd4088874cfcbc25b608b1c1119af050450420
Hongchan Choi <hongchan@chromium.org>
2019-11-07 19:22:18 +0000

    Disabled DragWindowFromShelfControllerTest.CancelDragDismissOverview
    See details in:
    https://analysis.chromium.org/p/chromium/flake-portal/flakes/occurrences?key=ag9zfmZpbmRpdC1mb3ItbWVyXQsSBUZsYWtlIlJjaHJvbWl1bUBhc2hfdW5pdHRlc3RzQERyYWdXaW5kb3dGcm9tU2hlbGZDb250cm9sbGVyVGVzdC5DYW5jZWxEcmFnRGlzbWlzc092ZXJ2aWV3DA
    
    TBR=sky@chromium.org
    
    Bug: 1022319
    Change-Id: I772b15cae2066696ae719af76034c6a4b497dc12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1903756
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Auto-Submit: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713529}
    
    Former-commit-id: 8924339a69472034b4b61afddc1de4869c390c15

--- ash/home_screen/drag_window_from_shelf_controller_unittest.cc
+++ ash/home_screen/drag_window_from_shelf_controller_unittest.cc
@@ -518,7 +518,8 @@ TEST_F(DragWindowFromShelfControllerTest, NoBackdropDuringWindowScaleDown) {
 
 // Test that if drag is cancelled, overview should be dismissed and other
 // hidden windows should restore to its previous visibility state.
-TEST_F(DragWindowFromShelfControllerTest, CancelDragDismissOverview) {
+// Flaky on ChromeOS. https://crbug.com/1022319
+TEST_F(DragWindowFromShelfControllerTest, DISABLED_CancelDragDismissOverview) {
   auto window3 = CreateTestWindow();
   auto window2 = CreateTestWindow();
   auto window1 = CreateTestWindow();
