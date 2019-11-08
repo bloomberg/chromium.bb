ec23f9df61aaf2707ee465b4b234952de8804d23
John Budorick <jbudorick@chromium.org>
2019-11-08 14:32:26 +0000

    Revert "Avoid explicit conversion for CoreAccountId on desktop platforms."
    This reverts commit d9402660ca10167b473bba9c4cda5a5f15dfbc67 [formerly 75be7e879474d75d78d25ed38c193c952e58c7af].
    
    Reason for revert: breaks compilation on fuchsia: https://ci.chromium.org/p/chromium/builders/ci/Fuchsia%20ARM64/90020
    
    Original change's description:
    > Avoid explicit conversion for CoreAccountId on desktop platforms.
    > 
    > Bug: 959157
    > Change-Id: I8d6a6c80a4f2aea5a84264f26b4047ca46f6e687
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1904173
    > Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    > Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#713817}
    
    TBR=msarda@chromium.org,triploblastic@chromium.org
    
    Change-Id: Ic950b9b7b4bd800b0d7c71c4ab46f17f68922137
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 959157
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1906307
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713819}
    
    Former-commit-id: 2e670a95cf0ec572bc7822ce1078054633b0b9e0

--- google_apis/gaia/core_account_id.cc
+++ google_apis/gaia/core_account_id.cc
@@ -22,11 +22,9 @@ CoreAccountId::CoreAccountId(std::string&& id) : id(std::move(id)) {}
 
 CoreAccountId::CoreAccountId(const std::string& id) : id(id) {}
 
-#if defined(OS_CHROMEOS) || defined(OS_IOS) || defined(OS_ANDROID)
 CoreAccountId::operator std::string() const {
   return id;
 }
-#endif
 
 bool CoreAccountId::empty() const {
   return id.empty();
