b6214a1306af8e96909ce3548d6217238c27bfe6
Jasper Chapman-Black <jaspercb@chromium.org>
2019-11-08 16:08:48 +0000

    SuperSize: Caspian: Add native symbol parsing
    Bug: 1011921
    Change-Id: Ica2e5a7a9222d80a81bd70299219f82798c19855
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1901427
    Commit-Queue: Jasper Chapman-Black <jaspercb@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713842}
    
    Former-commit-id: 930b4b0906ec78b3bd6141aa32b919cb6349ce3b

--- tools/binary_size/libsupersize/caspian/function_signature.cc
+++ tools/binary_size/libsupersize/caspian/function_signature.cc
@@ -1,19 +1,35 @@
 // Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file
+// found in the LICENSE file.
 
 // Much of this logic is duplicated at
 // tools/binary_size/libsupersize/function_signature.py.
 
 #include <stddef.h>
 
+#include <algorithm>
 #include <deque>
+#include <iostream>
 #include <string>
 #include <string_view>
 #include <tuple>
 #include <vector>
 
 #include "tools/binary_size/libsupersize/caspian/function_signature.h"
+
+namespace {
+bool EndsWith(std::string_view str,
+              std::string_view suffix,
+              size_t pos = std::string_view::npos) {
+  pos = std::min(pos, str.size());
+  size_t span = suffix.size();
+  return pos >= span && str.substr(pos - span, span) == suffix;
+}
+
+std::string_view Slice(std::string_view sv, size_t lo, size_t hi) {
+  return sv.substr(lo, hi - lo);
+}
+}  // namespace
 
 namespace caspian {
 std::vector<std::string_view> SplitBy(std::string_view str, char delim) {
@@ -32,6 +48,8 @@ std::vector<std::string_view> SplitBy(std::string_view str, char delim) {
 std::tuple<std::string_view, std::string_view, std::string_view> ParseJava(
     std::string_view full_name,
     std::deque<std::string>* owned_strings) {
+  // |owned_strings| is used as an allocator, the relative order of its
+  // elements can be arbitrary.
   std::string maybe_member_type;
   size_t hash_idx = full_name.find('#');
   std::string_view full_class_name;
@@ -42,7 +60,7 @@ std::tuple<std::string_view, std::string_view, std::string_view> ParseJava(
     // Format: Class#symbol: type
     full_class_name = full_name.substr(0, hash_idx);
     size_t colon_idx = full_name.find(':');
-    member = full_name.substr(hash_idx + 1, colon_idx - hash_idx - 1);
+    member = Slice(full_name, hash_idx + 1, colon_idx);
     if (colon_idx != std::string_view::npos) {
       member_type = full_name.substr(colon_idx);
     }
@@ -79,6 +97,234 @@ std::tuple<std::string_view, std::string_view, std::string_view> ParseJava(
                            std::string(member));
   std::string_view template_name = owned_strings->back();
 
+  return std::make_tuple(full_name, template_name, name);
+}
+
+size_t FindLastCharOutsideOfBrackets(std::string_view name,
+                                     char target_char,
+                                     size_t prev_idx) {
+  int paren_balance_count = 0;
+  int angle_balance_count = 0;
+  std::string_view prefix = name.substr(0, prev_idx);
+  while (true) {
+    size_t idx = prefix.rfind(target_char);
+    if (idx == std::string_view::npos) {
+      return std::string_view::npos;
+    }
+    for (char c : prefix.substr(idx)) {
+      switch (c) {
+        case '<':
+          angle_balance_count++;
+          break;
+        case '>':
+          angle_balance_count--;
+          break;
+        case '(':
+          paren_balance_count++;
+          break;
+        case ')':
+          paren_balance_count--;
+          break;
+      }
+    }
+    if (angle_balance_count == 0 && paren_balance_count == 0) {
+      return idx;
+    }
+    prefix = prefix.substr(0, idx);
+  }
+}
+
+size_t FindReturnValueSpace(std::string_view name, size_t paren_idx) {
+  size_t space_idx = paren_idx;
+  // Special case: const cast operators (see tests).
+  if (EndsWith(name, " const", paren_idx)) {
+    space_idx = paren_idx - 6;
+  }
+  while (true) {
+    space_idx = FindLastCharOutsideOfBrackets(name, ' ', space_idx);
+    // Special cases: "operator new", "operator< <templ>", "operator<< <tmpl>".
+    // No space is added for operator>><tmpl>.
+    // Currently does not handle operator->, operator->*
+    if (std::string_view::npos == space_idx) {
+      break;
+    }
+    if (EndsWith(name, "operator", space_idx)) {
+      space_idx -= 8;
+    } else if (EndsWith(name, "operator<", space_idx)) {
+      space_idx -= 9;
+    } else if (EndsWith(name, "operator<<", space_idx)) {
+      space_idx -= 10;
+    } else {
+      break;
+    }
+  }
+  return space_idx;
+}
+
+std::string StripTemplateArgs(std::string name) {
+  size_t last_right_idx = std::string::npos;
+  while (true) {
+    last_right_idx = name.substr(0, last_right_idx).rfind('>');
+    if (last_right_idx == std::string_view::npos) {
+      return name;
+    }
+    size_t left_idx =
+        FindLastCharOutsideOfBrackets(name, '<', last_right_idx + 1);
+    if (left_idx != std::string_view::npos) {
+      // Leave in empty <>s to denote that it's a template.
+      name = std::string(name.substr(0, left_idx + 1)) +
+             std::string(name.substr(last_right_idx));
+      last_right_idx = left_idx;
+    }
+  }
+}
+
+std::string NormalizeTopLevelGccLambda(std::string_view name,
+                                       size_t left_paren_idx) {
+  // cc::{lambda(PaintOp*)#63}::_FUN(cc:PaintOp*)
+  // -> cc::$lambda#63(cc:PaintOp*)
+
+  size_t left_brace_idx = name.find('{');
+  if (left_brace_idx == std::string_view::npos) {
+    exit(1);
+  }
+  size_t hash_idx = name.find('#', left_brace_idx + 1);
+  if (hash_idx == std::string_view::npos) {
+    exit(1);
+  }
+  size_t right_brace_idx = name.find('}', hash_idx + 1);
+  if (right_brace_idx == std::string_view::npos) {
+    exit(1);
+  }
+  std::string_view number = Slice(name, hash_idx + 1, right_brace_idx);
+
+  std::string ret;
+  ret += name.substr(0, left_brace_idx);
+  ret += "$lambda#";
+  ret += number;
+  ret += name.substr(left_paren_idx);
+  return ret;
+}
+
+std::string NormalizeTopLevelClangLambda(std::string_view name,
+                                         size_t left_paren_idx) {
+  // cc::$_21::__invoke(int) -> cc::$lambda#21(int)
+  size_t dollar_idx = name.find('$');
+  if (dollar_idx == std::string_view::npos) {
+    exit(1);
+  }
+  size_t colon_idx = name.find(':', dollar_idx + 1);
+  if (colon_idx == std::string_view::npos) {
+    exit(1);
+  }
+  std::string_view number = Slice(name, dollar_idx + 2, colon_idx);
+
+  std::string ret;
+  ret += name.substr(0, dollar_idx);
+  ret += "$lambda#";
+  ret += number;
+  ret += name.substr(left_paren_idx);
+  return ret;
+}
+
+size_t FindParameterListParen(std::string_view name) {
+  size_t start_idx = 0;
+  int angle_balance_count = 0;
+  int paren_balance_count = 0;
+  while (true) {
+    size_t idx = name.find('(', start_idx);
+    if (idx == std::string_view::npos) {
+      return std::string_view::npos;
+    }
+    for (char c : Slice(name, start_idx, idx)) {
+      switch (c) {
+        case '<':
+          angle_balance_count++;
+          break;
+        case '>':
+          angle_balance_count--;
+          break;
+        case '(':
+          paren_balance_count++;
+          break;
+        case ')':
+          paren_balance_count--;
+          break;
+      }
+    }
+    size_t operator_offset = Slice(name, start_idx, idx).find("operator<");
+    if (operator_offset != std::string_view::npos) {
+      if (name[start_idx + operator_offset + 9] == '<') {
+        // Handle operator<<, <<=
+        angle_balance_count -= 2;
+      } else {
+        // Handle operator<=
+        angle_balance_count -= 1;
+      }
+    } else {
+      operator_offset = Slice(name, start_idx, idx).find("operator>");
+      if (operator_offset != std::string_view::npos) {
+        if (name[start_idx + operator_offset + 9] == '>') {
+          // Handle operator>>,>>=
+          angle_balance_count += 2;
+        } else {
+          // Handle operator>=
+          angle_balance_count += 1;
+        }
+      }
+    }
+
+    // Adjust paren
+    if (angle_balance_count == 0 && paren_balance_count == 0) {
+      // Special case: skip "(anonymous namespace)".
+      if (name.substr(idx, 21) == "(anonymous namespace)") {
+        start_idx = idx + 21;
+        continue;
+      }
+      // Special case: skip "decltype (...)"
+      // Special case: skip "{lambda(PaintOp*)#63}"
+      if (idx && name[idx - 1] != ' ' && !EndsWith(name, "{lambda", idx)) {
+        return idx;
+      }
+    }
+
+    start_idx = idx + 1;
+    paren_balance_count++;
+  }
+}
+
+std::tuple<std::string, std::string, std::string> ParseCpp(
+    const std::string& input_name) {
+  std::string name = input_name;
+  size_t left_paren_idx = FindParameterListParen(input_name);
+  std::string full_name = input_name;
+  if (left_paren_idx != std::string::npos && left_paren_idx > 0) {
+    size_t right_paren_idx = name.rfind(')');
+    if (right_paren_idx <= left_paren_idx) {
+      std::cerr << "ParseCpp() received bad symbol: " << name << std::endl;
+      exit(1);
+    }
+    size_t space_idx = FindReturnValueSpace(name, left_paren_idx);
+    std::string name_no_params =
+        std::string(Slice(name, space_idx + 1, left_paren_idx));
+    // Special case for top-level lambdas.
+    if (EndsWith(name_no_params, "}::_FUN")) {
+      // Don't use |name_no_params| in here since prior _idx will be off if
+      // there was a return value.
+      name = NormalizeTopLevelGccLambda(name, left_paren_idx);
+      return ParseCpp(name);
+    } else if (EndsWith(name_no_params, "::__invoke") &&
+               name_no_params.find('$') != std::string::npos) {
+      name = NormalizeTopLevelClangLambda(name, left_paren_idx);
+      return ParseCpp(name);
+    }
+
+    full_name = name.substr(space_idx + 1);
+    name = name_no_params + name.substr(right_paren_idx + 1);
+  }
+
+  std::string template_name = name;
+  name = StripTemplateArgs(name);
   return std::make_tuple(full_name, template_name, name);
 }
 }  // namespace caspian
--- tools/binary_size/libsupersize/caspian/function_signature.h
+++ tools/binary_size/libsupersize/caspian/function_signature.h
@@ -23,6 +23,37 @@ std::vector<std::string_view> SplitBy(std::string_view str, char delim);
 std::tuple<std::string_view, std::string_view, std::string_view> ParseJava(
     std::string_view full_name,
     std::deque<std::string>* owned_strings);
+
+// Strips return type and breaks function signature into parts.
+// See unit tests for example signatures.
+// Returns:
+//  A tuple of:
+//   * name without return type (symbol.full_name),
+//   * full_name without params (symbol.template_name),
+//   * full_name without params and template args (symbol.name)
+std::tuple<std::string, std::string, std::string> ParseCpp(
+    const std::string& name);
+
+// Returns the last index of |target_char| that is not within ()s nor <>s.
+size_t FindLastCharOutsideOfBrackets(std::string_view name,
+                                     char target_char,
+                                     size_t prev_idx = std::string::npos);
+
+// Finds index of the "(" that denotes the start of a parameter list.
+size_t FindParameterListParen(std::string_view name);
+
+// Returns the index of the space that comes after the return type.
+size_t FindReturnValueSpace(std::string_view name, size_t paren_idx);
+
+// Different compilers produce different lambda symbols. These utility
+// functions standardize the two, so we can compare between compilers.
+std::string NormalizeTopLevelGccLambda(std::string_view name,
+                                       size_t left_paren_idx);
+std::string NormalizeTopLevelClangLambda(std::string_view name,
+                                         size_t left_paren_idx);
+
+// Strips the contents of <>, leaving empty <>s to denote that it's a template.
+std::string StripTemplateArgs(std::string name);
 }  // namespace caspian
 
 #endif  // TOOLS_BINARY_SIZE_LIBSUPERSIZE_CASPIAN_FUNCTION_SIGNATURE_H_
