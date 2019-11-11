811d935c6e73d1d4dd2df86239216e3a2aeff5b3
Yuki Shiino <yukishiino@chromium.org>
2019-11-11 09:01:44 +0000

    bind-gen: Expose upper/lower case functions from name_style
    name_style.arg is preferred to snake_case, but there are cases that
    a bindings generator needs to control case directly.  This patch
    exposes such low-level controls into name_style.raw namespace.
    
    Bug: 839389
    Change-Id: I3892238ada61625c36e1cff993640146758089b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1909048
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714172}
    
    Former-commit-id: 9b76cd582be6aca884e90d104506242e60726767

--- third_party/blink/renderer/bindings/scripts/bind_gen/name_style.py
+++ third_party/blink/renderer/bindings/scripts/bind_gen/name_style.py
@@ -19,114 +19,114 @@ from blinkbuild.name_style_converter import NameStyleConverter
 
 def api_func(*args):
     """Applies the style of Blink implementation function names for Web API."""
-    return _concat(_lower_camel_case, args)
+    return _concat(raw.lower_camel_case, args)
 
 
 def api_func_f(format_string, *args, **kwargs):
     """Applies the style of Blink implementation function names for Web API."""
-    return _lower_camel_case(
-        _format(_upper_camel_case, format_string, *args, **kwargs))
+    return raw.lower_camel_case(
+        _format(raw.upper_camel_case, format_string, *args, **kwargs))
 
 
 def arg(*args):
     """Applies the style of argument variable names."""
-    return _concat(_snake_case, args)
+    return _concat(raw.snake_case, args)
 
 
 def arg_f(format_string, *args, **kwargs):
     """Applies the style of argument variable names."""
-    return _format(_snake_case, format_string, *args, **kwargs)
+    return _format(raw.snake_case, format_string, *args, **kwargs)
 
 
 def class_(*args):
     """Applies the style of class names."""
-    return _concat(_upper_camel_case, args)
+    return _concat(raw.upper_camel_case, args)
 
 
 def class_f(format_string, *args, **kwargs):
     """Applies the style of class names."""
-    return _format(_upper_camel_case, format_string, *args, **kwargs)
+    return _format(raw.upper_camel_case, format_string, *args, **kwargs)
 
 
 def constant(*args):
     """Applies the style of constant names."""
-    return "k" + _concat(_upper_camel_case, args)
+    return "k" + _concat(raw.upper_camel_case, args)
 
 
 def constant_f(format_string, *args, **kwargs):
     """Applies the style of constant names."""
-    return "k" + _upper_camel_case(
-        _format(_upper_camel_case, format_string, *args, **kwargs))
+    return "k" + raw.upper_camel_case(
+        _format(raw.upper_camel_case, format_string, *args, **kwargs))
 
 
 def file(*args):
     """Applies the style of filenames."""
-    return _concat(_snake_case, args)
+    return _concat(raw.snake_case, args)
 
 
 def file_f(format_string, *args, **kwargs):
     """Applies the style of filenames."""
-    return _format(_snake_case, format_string, *args, **kwargs)
+    return _format(raw.snake_case, format_string, *args, **kwargs)
 
 
 def func(*args):
     """Applies the style of general Blink function names."""
-    return _concat(_upper_camel_case, args)
+    return _concat(raw.upper_camel_case, args)
 
 
 def func_f(format_string, *args, **kwargs):
     """Applies the style of general Blink function names."""
-    return _format(_upper_camel_case, format_string, *args, **kwargs)
+    return _format(raw.upper_camel_case, format_string, *args, **kwargs)
 
 
 def header_guard(*args):
     """Applies the style of header guard names."""
-    return _concat(_macro_case, args) + "_"
+    return _concat(raw.macro_case, args) + "_"
 
 
 def header_guard_f(format_string, *args, **kwargs):
     """Applies the style of header guard names."""
-    return _format(_macro_case, format_string, *args, **kwargs) + "_"
+    return _format(raw.macro_case, format_string, *args, **kwargs) + "_"
 
 
 def local_var(*args):
     """Applies the style of function local variable names."""
-    return _concat(_snake_case, args)
+    return _concat(raw.snake_case, args)
 
 
 def local_var_f(format_string, *args, **kwargs):
     """Applies the style of function local variable names."""
-    return _format(_snake_case, format_string, *args, **kwargs)
+    return _format(raw.snake_case, format_string, *args, **kwargs)
 
 
 def macro(*args):
     """Applies the style of macro names."""
-    return _concat(_macro_case, args)
+    return _concat(raw.macro_case, args)
 
 
 def macro_f(format_string, *args, **kwargs):
     """Applies the style of macro names."""
-    return _format(_macro_case, format_string, *args, **kwargs)
+    return _format(raw.macro_case, format_string, *args, **kwargs)
 
 
 def member_var(*args):
     """Applies the style of member variable names."""
-    return _concat(_snake_case, args) + "_"
+    return _concat(raw.snake_case, args) + "_"
 
 
 def member_var_f(format_string, *args, **kwargs):
     """Applies the style of member variable names."""
-    return _format(_snake_case, format_string, *args, **kwargs) + "_"
+    return _format(raw.snake_case, format_string, *args, **kwargs) + "_"
 
 
 def namespace(*args):
     """Applies the style of namespace names."""
-    return _concat(_snake_case, args)
+    return _concat(raw.snake_case, args)
 
 
 def namespace_f(format_string, *args, **kwargs):
     """Applies the style of namespace names."""
-    return _format(_snake_case, format_string, *args, **kwargs)
+    return _format(raw.snake_case, format_string, *args, **kwargs)
 
 
 def _concat(style_func, args):
@@ -145,17 +145,28 @@ def _format(style_func, format_string, *args, **kwargs):
     return format_string.format(*args, **kwargs)
 
 
-def _snake_case(name):
-    return NameStyleConverter(name).to_snake_case()
+class raw(object):
+    """
+    Namespace to provide (unrecommended) raw controls on case conversions.
 
+    This class is pretending to be a module.
+    """
 
-def _upper_camel_case(name):
-    return NameStyleConverter(name).to_upper_camel_case()
+    def __init__(self):
+        assert False
 
+    @staticmethod
+    def snake_case(name):
+        return NameStyleConverter(name).to_snake_case()
 
-def _lower_camel_case(name):
-    return NameStyleConverter(name).to_lower_camel_case()
+    @staticmethod
+    def upper_camel_case(name):
+        return NameStyleConverter(name).to_upper_camel_case()
 
+    @staticmethod
+    def lower_camel_case(name):
+        return NameStyleConverter(name).to_lower_camel_case()
 
-def _macro_case(name):
-    return NameStyleConverter(name).to_macro_case()
+    @staticmethod
+    def macro_case(name):
+        return NameStyleConverter(name).to_macro_case()
