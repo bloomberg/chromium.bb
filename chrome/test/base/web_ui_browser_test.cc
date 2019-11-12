f7be4fccc1e154980a52e96171e564391a52a1d3
Stephen Nusko <nuskos@chromium.org>
2019-11-12 12:08:44 +0000

    Revert "Capture test_api.js javascript dependencies in 2 places (rather than ~13)."
    This reverts commit 38aaff8fde10ce5323e32e7e9c808e628df30e6f [formerly f8dd1ecf73ca005482d2da450b4f28947a3eb25f].
    
    Reason for revert: Seems to be causing the mac-builder-perf to fail (see linked bug).
    
    Original change's description:
    > Capture test_api.js javascript dependencies in 2 places (rather than ~13).
    > 
    > test_api.js depends on a bunch of JS libraries that appear in many lists.
    > 
    > This CL consolidates those dependencies to two places:
    >  - js2gtest.gni, and
    >  - A JsTestApiConfig class declared in chrome/test/base/js_test_api.h
    > 
    > Note not all lists captured the axs_testing.js dependency, but this change
    > uses the same dependency list in all locations for consistency.
    > 
    > TBR=aboxhall@chromium.org (mechanical change)
    > 
    > Bug: 1000989
    > Change-Id: If6e0716c03e42783a2d3529baa70475a9d305045
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1900512
    > Commit-Queue: Trent Apted <tapted@chromium.org>
    > Reviewed-by: Dan Beam <dbeam@chromium.org>
    > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#714421}
    
    TBR=tapted@chromium.org,dbeam@chromium.org,aboxhall@chromium.org,blundell@chromium.org
    
    Change-Id: I3d93382a9833f85aabe2eb0e0469c16e451c65ed
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1000989,1023795
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1911748
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714447}
    
    Former-commit-id: a0829135f51b120470ab845efc586058d1d96d57

--- chrome/test/base/web_ui_browser_test.cc
+++ chrome/test/base/web_ui_browser_test.cc
@@ -28,7 +28,6 @@
 #include "chrome/browser/ui/webui/web_ui_test_handler.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/url_constants.h"
-#include "chrome/test/base/js_test_api.h"
 #include "chrome/test/base/test_chrome_web_ui_controller_factory.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/url_data_source.h"
@@ -443,6 +442,8 @@ void BaseWebUIBrowserTest::SetUpOnMainThread() {
 
   logging::SetLogMessageHandler(&LogHandler);
 
+  AddLibrary(base::FilePath(kA11yAuditLibraryJSPath));
+
   content::WebUIControllerFactory::UnregisterFactoryForTesting(
       ChromeWebUIControllerFactory::GetInstance());
 
@@ -544,7 +545,9 @@ bool BaseWebUIBrowserTest::RunJavascriptUsingHandler(
 GURL BaseWebUIBrowserTest::WebUITestDataPathToURL(
     const base::FilePath::StringType& path) {
   base::ScopedAllowBlockingForTesting allow_blocking;
-  base::FilePath test_path(JsTestApiConfig().search_path);
+  base::FilePath dir_test_data;
+  EXPECT_TRUE(base::PathService::Get(chrome::DIR_TEST_DATA, &dir_test_data));
+  base::FilePath test_path(dir_test_data.Append(kWebUITestFolder).Append(path));
   EXPECT_TRUE(base::PathExists(test_path));
   return net::FilePathToFileURL(test_path);
 }
