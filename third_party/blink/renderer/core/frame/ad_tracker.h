4aee17cc5ab00387de2119ed8bc508881bc855c9
Josh Karlin <jkarlin@chromium.org>
2019-11-12 22:21:33 +0000

    [AdTagging] Experiment with only looking at the top of the stack
    What: Add a feature to cause AdTagging to only tag a v8 stack as an ad
    stack if the top of the stack is an ad script. E.g., ignore the
    bottom.
    
    Why: Because sometimes an event on an ad script is used to create
    content (e.g., the ad finishes showing so now reveal the page
    content by calling content script).
    
    Bug: 1022802
    Change-Id: I54ef3f0930f886df01320a714e72e8e4554a0357
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1906467
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714675}
    
    Former-commit-id: b959e58a5a7ef398a6cafb6b535d0a6779d95a21

--- third_party/blink/renderer/core/frame/ad_tracker.h
+++ third_party/blink/renderer/core/frame/ad_tracker.h
@@ -28,6 +28,7 @@ class ExecuteScript;
 
 namespace features {
 CORE_EXPORT extern const base::Feature kAsyncStackAdTagging;
+CORE_EXPORT extern const base::Feature kTopOfStackAdTagging;
 }
 
 // Tracker for tagging resources as ads based on the call stack scripts.
@@ -107,6 +108,7 @@ class CORE_EXPORT AdTracker : public GarbageCollected<AdTracker> {
   uint32_t running_ad_async_tasks_ = 0;
 
   const bool async_stack_enabled_;
+  const bool top_of_stack_only_;
 
   DISALLOW_COPY_AND_ASSIGN(AdTracker);
 };
