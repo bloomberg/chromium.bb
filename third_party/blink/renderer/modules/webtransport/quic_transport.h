d9e065db477b0f8b2a42b8b3250c7975703c61ae
Adam Rice <ricea@chromium.org>
2019-11-14 19:08:33 +0000

    Implement connection and close for QuicTransport
    QuicTransport is a low-level JavaScript interface to permit
    communication via the QUIC protocol. This CL implements only the
    connection and close parts of the API, along with unit tests.
    
    Bug: 1011392
    Change-Id: I7383680e04c83a45f483e5d542e9227f8288ed65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1899631
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715349}
    
    Former-commit-id: 43d4baee9a1e6a21e38d1d0f1ce5a978bd48d65b

--- third_party/blink/renderer/modules/webtransport/quic_transport.h
+++ third_party/blink/renderer/modules/webtransport/quic_transport.h
@@ -9,7 +9,10 @@
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver.h"
+#include "mojo/public/cpp/bindings/remote.h"
 #include "services/network/public/mojom/quic_transport.mojom-blink.h"
+#include "third_party/blink/renderer/bindings/core/v8/active_script_wrappable.h"
+#include "third_party/blink/renderer/core/execution_context/context_lifecycle_observer.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
@@ -17,20 +20,32 @@
 
 namespace blink {
 
+class ExceptionState;
+class ExecutionContext;
 class ScriptState;
+class WebTransportCloseInfo;
 
 class MODULES_EXPORT QuicTransport final
     : public ScriptWrappable,
-      public network::mojom::blink::QuicTransportHandshakeClient {
+      public ActiveScriptWrappable<QuicTransport>,
+      public ContextLifecycleObserver,
+      public network::mojom::blink::QuicTransportHandshakeClient,
+      public network::mojom::blink::QuicTransportClient {
   DEFINE_WRAPPERTYPEINFO();
   USING_PRE_FINALIZER(QuicTransport, Dispose);
+  USING_GARBAGE_COLLECTED_MIXIN(QuicTransport);
 
  public:
   using PassKey = util::PassKey<QuicTransport>;
-  static QuicTransport* Create(ScriptState* script_state, const String& url);
+  static QuicTransport* Create(ScriptState* script_state,
+                               const String& url,
+                               ExceptionState&);
 
   QuicTransport(PassKey, ScriptState*, const String& url);
-  ~QuicTransport() override = default;
+  ~QuicTransport() override;
+
+  // QuicTransport IDL implementation.
+  void close(const WebTransportCloseInfo*);
 
   // QuicTransportHandshakeClient implementation
   void OnConnectionEstablished(
@@ -39,19 +54,28 @@ class MODULES_EXPORT QuicTransport final
       override;
   void OnHandshakeFailed() override;
 
+  // Implementation of ContextLifecycleObserver
+  void ContextDestroyed(ExecutionContext*) final;
+
+  // Implementation of ActiveScriptWrappable
+  bool HasPendingActivity() const final;
+
   // ScriptWrappable implementation
   void Trace(Visitor* visitor) override;
 
  private:
-  void Init();
+  void Init(ExceptionState&);
   void Dispose();
+  void OnConnectionError();
 
-  const Member<ScriptState> script_state_;
   const KURL url_;
+  mojo::Remote<network::mojom::blink::QuicTransport> quic_transport_;
   mojo::Receiver<network::mojom::blink::QuicTransportHandshakeClient>
-      handshake_client_receiver_;
+      handshake_client_receiver_{this};
+  mojo::Receiver<network::mojom::blink::QuicTransportClient> client_receiver_{
+      this};
 };
 
 }  // namespace blink
 
-#endif
+#endif  // THIRD_PARTY_BLINK_RENDERER_MODULES_WEBTRANSPORT_QUIC_TRANSPORT_H_
