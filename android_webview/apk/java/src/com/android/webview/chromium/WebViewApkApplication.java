538d20d1498f98b37a5b37741805c21d8e9d54f7
Robbie McElrath <rmcelrath@chromium.org>
2019-11-14 21:38:13 +0000

    [AW] Delete //aw/apk now that downstream doesn't depend on it
    crrev.com/c/1900130 renamed //aw/apk to //aw/nonembedded, and
    crrev.com/i/2103749 removed the last //aw/apk dependencies. Now that the
    latter CL has landed, we can delete //aw/apk.
    
    Test: ninja -C out/aw
    Bug: 934152
    Change-Id: I1e5322adfda9813d296ca0f027a75463db18efc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913597
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715429}
    
    Former-commit-id: c98dded53a937be724a1a60fe30313c60fd9a37f

--- android_webview/apk/java/src/com/android/webview/chromium/WebViewApkApplication.java
+++
@@ -1,9 +0,0 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-package com.android.webview.chromium;
-
-// TODO(crbug.com/934152): Delete this once Clank points to the new WebViewApkApplication package.
-public class WebViewApkApplication
-        extends org.chromium.android_webview.nonembedded.WebViewApkApplication {}
