90c88f097366f33f7d251ffa70b9f15c27479686
Garrett Beaty <gbeaty@chromium.org>
2019-11-14 21:45:14 +0000

    Reland "Fetch the input commit ref when both ref and id are specified."
    This is a reland of 72a245e4c678f478bf7e1961cae8b2ba5bfa93bf
    
    The "git checkout master --detach" operation has been modified to run in the solution
    directory.
    
    Original change's description:
    > Fetch the input commit ref when both ref and id are specified.
    >
    > This causes --refs refs/head/master to be added to bot_update calls when
    > the input build message contains a commit id and the refs/head/master
    > ref. This is an effective no-op.
    >
    > When bot_update clones a git repository, it results in HEAD referring to
    > master in contrast to the non-clone case which results in a detached
    > HEAD. This prevents fetching refs/heads/master, so detach the HEAD after
    > cloning.
    >
    > Bug: 922150
    > Change-Id: I357a78305020218e9f5d350832a706b8f2b27967
    > Recipe-Nontrivial-Roll: build
    > Recipe-Nontrivial-Roll: build_limited_scripts_slave
    > Recipe-Nontrivial-Roll: infra
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/1900735
    > Commit-Queue: Garrett Beaty <gbeaty@chromium.org>
    > Reviewed-by: Edward Lesmes <ehmaldonado@chromium.org>
    
    Bug: 922150
    Change-Id: I7dc6ebed2dcbd880ff4d515f7e9fc6428ff24099
    Recipe-Nontrivial-Roll: build
    Recipe-Nontrivial-Roll: build_limited_scripts_slave
    Recipe-Nontrivial-Roll: infra
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/1910260
    Reviewed-by: Edward Lesmes <ehmaldonado@chromium.org>
    Commit-Queue: Garrett Beaty <gbeaty@chromium.org>

--- recipes/recipe_modules/bot_update/resources/bot_update.py
+++ recipes/recipe_modules/bot_update/resources/bot_update.py
@@ -724,6 +724,8 @@ def _git_checkout(sln, sln_dir, revisions, refs, no_fetch_tags, git_cache_dir,
       if not path.isdir(sln_dir):
         git('clone', '--no-checkout', '--local', '--shared', mirror_dir,
             sln_dir)
+        # Detach HEAD to be consistent with the non-clone case
+        git('checkout', 'master', '--detach', cwd=sln_dir)
         _git_disable_gc(sln_dir)
       else:
         _git_disable_gc(sln_dir)
