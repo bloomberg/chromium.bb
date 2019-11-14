e6a02e825bd2821f2be1286d57f5e6e66a8e522a
nancy <nancylingwang@chromium.org>
2019-11-14 11:33:29 +0000

    Use the unified time format function in the app pause dialog.
    Use the unified time format function to show the time limit in the
    app pause dialog.
    
    For example,
    hour = 1 minute = 1
    Show: You can use it for 1 hour and 1 minute tomorrow.
    
    hour = 11 minute = 59
    Show: You can use it for 11 hours and 59 minutes tomorrow.
    
    hour = 10 minute = 0
    Show: You can use it for 10 hours tomorrow.
    
    hour = 0 minute = 10
    Show: You can use it for 10 minutes tomorrow.
    
    hour = 0 minute = 0
    Show: You can use it for 0 seconds tomorrow.
    
    BUG=1011235
    
    Change-Id: I27619b2b7f999c68eabff6a337e9d54eb661db86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913360
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715233}
    
    Former-commit-id: b8711233502ce151005196c8f987872b01fd081b

--- chrome/browser/ui/views/apps/app_pause_dialog_view.cc
+++ chrome/browser/ui/views/apps/app_pause_dialog_view.cc
@@ -8,6 +8,7 @@
 #include <utility>
 
 #include "base/strings/utf_string_conversions.h"
+#include "base/time/time.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/chrome_typography.h"
 #include "chrome/grit/chromium_strings.h"
@@ -15,12 +16,12 @@
 #include "components/constrained_window/constrained_window_views.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/l10n/time_format.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/image/image_skia_operations.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/box_layout.h"
-
 // static
 void apps::AppServiceProxy::CreatePauseDialog(
     const std::string& app_name,
@@ -50,25 +51,14 @@ AppPauseDialogView::AppPauseDialogView(
   auto* icon_view = AddChildView(std::make_unique<views::ImageView>());
   icon_view->SetImage(image);
 
-  base::string16 pause_hours = l10n_util::GetPluralStringFUTF16(
-      IDS_APP_PAUSE_HEADING_HOURS, pause_data.hours);
-  base::string16 pause_minutes = l10n_util::GetPluralStringFUTF16(
-      IDS_APP_PAUSE_HEADING_MINUTES, pause_data.minutes);
-
-  base::string16 heading_text;
-  if (pause_data.hours != 0 && pause_data.minutes != 0) {
-    heading_text = l10n_util::GetStringFUTF16(
-        IDS_APP_PAUSE_HEADING_HOURS_AND_MINUTES, base::UTF8ToUTF16(app_name),
-        pause_hours, pause_minutes);
-  } else if (pause_data.hours != 0) {
-    heading_text =
-        l10n_util::GetStringFUTF16(IDS_APP_PAUSE_HEADING_HOURS_ONLY,
-                                   base::UTF8ToUTF16(app_name), pause_hours);
-  } else if (pause_data.minutes != 0) {
-    heading_text =
-        l10n_util::GetStringFUTF16(IDS_APP_PAUSE_HEADING_MINUTES_ONLY,
-                                   base::UTF8ToUTF16(app_name), pause_minutes);
-  }
+  const int cutoff = pause_data.minutes == 0 || pause_data.hours == 0 ? 0 : -1;
+  base::string16 heading_text = l10n_util::GetStringFUTF16(
+      IDS_APP_PAUSE_HEADING, base::UTF8ToUTF16(app_name),
+      ui::TimeFormat::Detailed(
+          ui::TimeFormat::Format::FORMAT_DURATION,
+          ui::TimeFormat::Length::LENGTH_LONG, cutoff,
+          base::TimeDelta::FromHours(pause_data.hours) +
+              base::TimeDelta::FromMinutes(pause_data.minutes)));
 
   auto* label = AddChildView(std::make_unique<views::Label>(heading_text));
   label->SetMultiLine(true);
