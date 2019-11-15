eb94c08445081b7d237bcb68a290bbbf49ed2f63
Hajime Hoshi <hajimehoshi@chromium.org>
2019-11-15 10:15:12 +0000

    Disable testRunPerformanceTestsTelemetrySharded_end2end on Linux
    TBR=dpranke@chromium.org
    
    Bug: 1024767
    Change-Id: I8fc0d6e2acbc7fbd723770cf8e5dce5b14feb3c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1918913
    Reviewed-by: Hajime Hoshi <hajimehoshi@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715637}
    
    Former-commit-id: ed091f8d3efd3056e6bc4ba123a0e214d7fd16a0

--- tools/perf/scripts_smoke_unittest.py
+++ tools/perf/scripts_smoke_unittest.py
@@ -162,7 +162,8 @@ class ScriptsSmokeTest(unittest.TestCase):
 
   # Android: crbug.com/932301
   # ChromeOS: crbug.com/754913
-  @decorators.Disabled('chromeos', 'android')
+  # Linux: crbug.com/1024767
+  @decorators.Disabled('chromeos', 'android', 'linux')
   def testRunPerformanceTestsTelemetrySharded_end2end(self):
     tempdir = tempfile.mkdtemp()
     env = os.environ.copy()
