0b4664a3e51e6deb74a6720073d8436f144f0fcd
Peter Kasting <pkasting@chromium.org>
2019-11-15 19:48:53 +0000

    Add a dump_colors utility to dump the output values of the color pipeline.
    Bug: 1003612
    TBR: sky
    Change-Id: I1078017072fcf921dd40002f04dcb2dd4419cf86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913605
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715806}
    
    Former-commit-id: 4e6326f64cd00013176064f71d27dc810e87d78f

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -34,10 +34,17 @@ enum ChromeColorIds : ui::ColorId {
   kColorOmniboxTextDimmed,
 
   kColorToolbar,  // TODO(pkasting): Add a recipe
+
+  kChromeColorsEnd,
 };
+
+static_assert(ui::ColorId{kChromeColorsEnd} <= ui::ColorId{ui::kUiColorsLast},
+              "Embedder colors must not exceed allowed space");
 
 enum ChromeColorSetIds : ui::ColorSetId {
   kColorSetCustomTheme = ui::kUiColorSetsEnd,
+
+  kChromeColorSetsEnd,
 };
 
 #endif  // CHROME_BROWSER_UI_COLOR_CHROME_COLOR_ID_H_
