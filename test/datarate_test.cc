f112a35e3b8a125ae69538f3ffe37a91b0076971
Fyodor Kyslov <kyslov@google.com>
2019-11-18 10:41:17 -0800

    AV1 RT: Fix datarate test failure
    With the new speed feature two data rate tests are failing with a very
    small overshoot of the threshold. Extendign the threshold
    
    BUG=aomedia:2543
    
    Change-Id: Ic87db7b6b2069d9b7f16364c1fc41d9b6d23a6cf

--- test/datarate_test.cc
+++ test/datarate_test.cc
@@ -167,7 +167,7 @@ class DatarateTestLarge
       ASSERT_NO_FATAL_FAILURE(RunLoop(&video));
       ASSERT_GE(effective_datarate_, cfg_.rc_target_bitrate * 0.85)
           << " The datarate for the file is lower than target by too much!";
-      ASSERT_LE(effective_datarate_, cfg_.rc_target_bitrate * 1.16)
+      ASSERT_LE(effective_datarate_, cfg_.rc_target_bitrate * 1.17)
           << " The datarate for the file is greater than target by too much!";
       if (last_drop > 0) {
         ASSERT_LE(first_drop_, last_drop)
