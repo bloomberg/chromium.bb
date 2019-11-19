16d94916695a028054783d1ef00a0fbf55ca5b33
Andrew Xu <andrewxu@chromium.org>
2019-11-19 00:27:14 +0000

    Invalidate ShelfContainerView's layout explicitly
    Now in ShelfContainerView::ChildPreferredSizeChanged, layout is
    updated implicitly by calling PreferredSizeChanged(). However, due
    to the CL (https://crrev.com/c/1876128), when the parent view calls
    ChildPreferredSizeChanged(), the child view is not labeled as
    |needs_layout_|. As a result, ShelfContainerView's layout may fail to
    update after calling PreferredSizeChanged(). To solve this issue,
    ShelfContainerView's layout is invalidated explicitly.
    
    Bug: 1025567
    Change-Id: Iee6c8cbebd038b78ae229a0f28d1f08924a68fa1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1921340
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716394}
    
    Former-commit-id: b1faf0c5b2f4d86a1c45ec862ab35ffe4c3136f2

--- ash/shelf/shelf_container_view.cc
+++ ash/shelf/shelf_container_view.cc
@@ -28,6 +28,15 @@ gfx::Size ShelfContainerView::CalculatePreferredSize() const {
 }
 
 void ShelfContainerView::ChildPreferredSizeChanged(views::View* child) {
+  // The CL (https://crrev.com/c/1876128) modifies View::PreferredSizeChanged
+  // by moving InvalidateLayout() after ChildPreferredSizeChanged(). Meanwhile,
+  // the parent view of ShelfContainerView overrides ChildPreferredSizeChanged
+  // with calling Layout(). Due to the CL above, ShelfContainerView is not
+  // labeled as |needs_layout_| when the parent view updates the layout. As a
+  // result, Calling Layout() in the parent view may not trigger the update in
+  // child view. So we have to invalidate the layout here explicitly.
+  InvalidateLayout();
+
   PreferredSizeChanged();
 }
 
