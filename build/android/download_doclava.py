f4ab3bb5143c726d5548c6a8e195e109c2388d82
Yngve N. Pettersen <yngve@vivaldi.com>
2019-11-19 12:03:14 +0000

    Use script relative paths in download_doclava.py
    Rather than using a hardcoded path from the working directory, specify
    the file location relative to the script's directory. This allows
    downstream projects to have different source code organizations.
    
    Change-Id: I5449770d73210504c69b92bd746c5e31e65cab76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1921907
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Yngve Pettersen <yngve@vivaldi.com>
    Cr-Commit-Position: refs/heads/master@{#716584}
    
    Former-commit-id: 4c2c5c60500563d545916029915fc7d9140e557f

--- build/android/download_doclava.py
+++ build/android/download_doclava.py
@@ -24,7 +24,8 @@ def main():
       '--bucket', 'chromium-doclava',
       '--extract',
       '-s',
-      os.path.join('src', 'buildtools', 'android', 'doclava.tar.gz.sha1')])
+      os.path.join(os.path.dirname(__file__), '..', '..', 'buildtools',
+                   'android', 'doclava.tar.gz.sha1')])
   return 0
 
 if __name__ == '__main__':
