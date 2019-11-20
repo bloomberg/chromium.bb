f436c1e7e837e45d975e577b98dc1eeee1c241a5
Ilia Samsonov <isamsonov@google.com>
2019-11-20 20:21:20 +0000

    Add prefix to empty param tests.
    googletest will no longer support empty prefix
    for parameterized INSTANTIATE_TEST_SUITE_P.
    Adding "All" prefix to existing empty prefix test suites.
    
    This CL was uploaded by git cl split.
    
    R=bengr@chromium.org
    
    Bug: 1023677
    Change-Id: I87328b9c43d4a225e16d5748ddc705416c723ae6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1925743
    Auto-Submit: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717165}
    
    Former-commit-id: a16a3bc30d19dbbf1d2e6f18dc79364be94b0db5

--- chrome/browser/data_reduction_proxy/data_reduction_proxy_browsertest.cc
+++ chrome/browser/data_reduction_proxy/data_reduction_proxy_browsertest.cc
@@ -757,7 +757,7 @@ IN_PROC_BROWSER_TEST_P(DataReductionProxyWithHoldbackBrowsertest,
 // enabled. Second parameter is true if lite page redirect preview is enabled.
 // Third parameter is true if data reduction proxy config should always be
 // fetched.
-INSTANTIATE_TEST_SUITE_P(,
+INSTANTIATE_TEST_SUITE_P(All,
                          DataReductionProxyWithHoldbackBrowsertest,
                          ::testing::Combine(::testing::Bool(),
                                             ::testing::Bool(),
@@ -1462,7 +1462,7 @@ IN_PROC_BROWSER_TEST_P(
 // Second parameter is true if the test proxy server should set via header
 // correctly on the response headers.
 INSTANTIATE_TEST_SUITE_P(
-    ,
+    All,
     DataReductionProxyWarmupURLBrowsertest,
     ::testing::Combine(
         testing::Values(ProxyServer_ProxyScheme_HTTP,
