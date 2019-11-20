c6a19df0c621794ed07c4c1aa88d42d40815c39c
Mike Wittman <wittman@chromium.org>
2019-11-20 19:24:17 +0000

    Revert "[Sampling profiler] Add profiler browser tests"
    This reverts commit cf73f7f18876752bff5cf1164d9928cd613a7a01 [formerly 3fc2bdd04a94edf0369131a2d1fb55ef44492ff5].
    
    Reason for revert: Tests are generating consistent failures on official build.
    
    Original change's description:
    > [Sampling profiler] Add profiler browser tests
    > 
    > 
    > 
    > Implements browser tests to verify that startup profiles taken in the
    > 
    > browser and child processes are received by the metrics provider.
    > 
    > 
    > 
    > To work within the test timeouts and to avoid non-determinism this
    > 
    > requires a couple adaptations to be made to the code when run under
    > 
    > these tests. The profiling period is reduced from 30 seconds to one
    > 
    > second, and profiles are taken in all renderer processes. The behavior
    > 
    > is enabled by providing a 'browser-test' argument to the existing
    > 
    > 'start-stack-profiler' switch.
    > 
    > 
    > 
    > The main thread profiler for browser tests is moved later, after the
    > 
    > command line is set up, to be able to observe the added switch
    > 
    > argument. StackSamplingConfiguration is made responsible for the
    > 
    > sampling parameters, reusing the GetSamplingParamsForCurrentProcess()
    > 
    > function which was previously unused.
    > 
    > 
    > 
    > 
    > 
    > 
    > Bug: 1011877
    > 
    > Change-Id: Ibad55178ebd3ff5ea497971868c9619210c2c8a1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1900262
    > Commit-Queue: Mike Wittman <wittman@chromium.org>
    > Auto-Submit: Mike Wittman <wittman@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#716372}
    
    TBR=sadrul@chromium.org,thestig@chromium.org,wittman@chromium.org,kylechar@chromium.org,etiennep@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1011877, 1026575
    Change-Id: Iccbfc4ea4efcf2b2be268165fba1ddd26da37b01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1925557
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717113}
    
    Former-commit-id: b6a9f384fc39548e6d187f02513f7d243b664203

--- chrome/common/thread_profiler.cc
+++ chrome/common/thread_profiler.cc
@@ -39,6 +39,12 @@ ThreadProfiler* g_main_thread_instance = nullptr;
 // Run continuous profiling 2% of the time.
 constexpr const double kFractionOfExecutionTimeToSample = 0.02;
 
+constexpr struct StackSamplingProfiler::SamplingParams kSamplingParams = {
+    /* initial_delay= */ base::TimeDelta::FromMilliseconds(0),
+    /* samples_per_profile= */ 300,
+    /* sampling_interval= */ base::TimeDelta::FromMilliseconds(100),
+    /* keep_consistent_sampling_interval= */ true};
+
 CallStackProfileParams::Process GetProcess() {
   const base::CommandLine* command_line =
       base::CommandLine::ForCurrentProcess();
@@ -231,10 +237,8 @@ ThreadProfiler::ThreadProfiler(
   if (!StackSamplingConfiguration::Get()->IsProfilerEnabledForCurrentProcess())
     return;
 
-  const base::StackSamplingProfiler::SamplingParams sampling_params =
-      StackSamplingConfiguration::Get()->GetSamplingParams();
   startup_profiler_ = std::make_unique<StackSamplingProfiler>(
-      base::GetSamplingProfilerCurrentThreadToken(), sampling_params,
+      base::GetSamplingProfilerCurrentThreadToken(), kSamplingParams,
       std::make_unique<CallStackProfileBuilder>(
           CallStackProfileParams(GetProcess(), thread,
                                  CallStackProfileParams::PROCESS_STARTUP),
@@ -248,10 +252,10 @@ ThreadProfiler::ThreadProfiler(
   // profiling.
   base::TimeTicks startup_profiling_completion_time =
       base::TimeTicks::Now() +
-      sampling_params.samples_per_profile * sampling_params.sampling_interval;
+      kSamplingParams.samples_per_profile * kSamplingParams.sampling_interval;
 
   periodic_sampling_scheduler_ = std::make_unique<PeriodicSamplingScheduler>(
-      sampling_params.samples_per_profile * sampling_params.sampling_interval,
+      kSamplingParams.samples_per_profile * kSamplingParams.sampling_interval,
       kFractionOfExecutionTimeToSample, startup_profiling_completion_time);
 
   if (owning_thread_task_runner_)
@@ -294,8 +298,7 @@ void ThreadProfiler::StartPeriodicSamplingCollection() {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   // NB: Destroys the previous profiler as side effect.
   periodic_profiler_ = std::make_unique<StackSamplingProfiler>(
-      base::GetSamplingProfilerCurrentThreadToken(),
-      StackSamplingConfiguration::Get()->GetSamplingParams(),
+      base::GetSamplingProfilerCurrentThreadToken(), kSamplingParams,
       std::make_unique<CallStackProfileBuilder>(
           CallStackProfileParams(GetProcess(), thread_,
                                  CallStackProfileParams::PERIODIC_COLLECTION),
