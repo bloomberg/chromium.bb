1476bcc2d9f460c503cd1a92882ea8cd92c57480
Azeem Arshad <azeemarshad@chromium.org>
2019-11-20 02:05:02 +0000

    [Network Details] Fix issue with cellular APN select list in OOBE.
    This CL fixes issue with APN select drop down showing incorrect value
    in OOBE screen. The issue was caused because for some reason, the select
    value was not being set properly when we update the list and then set
    the value in a this.async call. Fixed this by using the option selected
    attribute so that we don't have to wait to update the value.
    
    Fixed: 1011772
    Change-Id: I54028651d2a8f4a663fb301ea75fab614bce2266
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1925440
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716844}
    
    Former-commit-id: 5a19deff05e66ff57075613280f840d4680296a2

--- ui/webui/resources/cr_components/chromeos/network/network_apnlist.js
+++ ui/webui/resources/cr_components/chromeos/network/network_apnlist.js
@@ -151,12 +151,8 @@ Polymer({
     apnList.push(otherApn);
 
     this.apnSelectList_ = apnList;
-
-    // Set selectedApn_ after dom-repeat has been stamped.
-    this.async(() => {
-      this.selectedApn_ =
-          (activeApn && activeApn.accessPointName) || otherApn.accessPointName;
-    });
+    this.selectedApn_ =
+        (activeApn && activeApn.accessPointName) || otherApn.accessPointName;
   },
 
   /**
@@ -287,6 +283,16 @@ Polymer({
     return apnList.find(function(a) {
       return a.accessPointName == accessPointName;
     });
+  },
+
+  /**
+   * @param {chromeos.networkConfig.mojom.ApnProperties} item
+   * @return {boolean} Boolean indicating whether |item| is the current selected
+   *     apn item.
+   * @private
+   */
+  isApnItemSelected_: function(item) {
+    return item.accessPointName == this.selectedApn_;
   }
 });
 })();
