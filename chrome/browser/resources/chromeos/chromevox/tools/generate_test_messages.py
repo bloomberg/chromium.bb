4cde6932f2d601c6633167ab0fe6c9f0ceb7fa6a
David Tseng <dtseng@chromium.org>
2019-11-20 07:40:24 +0000

    Revert "Support compressed .json grit files and compress chromevox messages"
    This reverts commit 57f1341eb7a8fc5afd5298c114775a774e38e70c [formerly 00f512ce02fd9e8cc945b21b9c7ef038ac3061d9].
    
    Reason for revert: <INSERT REASONING HERE>
    Speculative fix for crbug.com/1026473
    Tests are unable to find any message strings.
    Bug: 1026473
    Original change's description:
    > Support compressed .json grit files and compress chromevox messages
    >
    > This provides support for --gzip-output in grit/build.py, which:
    > 1. Compresses individual output files.
    > 2. Appends .gz to the compressed output files
    >    (e.g. messages.json -> messages.json.gz).
    >
    > This makes it fairly simple to convert an extension (e.g. chromevox)
    > to use compressed l10n files. Other than setting gzip_output = true
    > in the .gn file, the only other required change is updating any
    > test dependencies.
    >
    > Bug: 1023568
    > Change-Id: I8318b1d87b828b512f890c6f2cf6d4d22462ed7d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913610
    > Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    > Reviewed-by: David Tseng <dtseng@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#716284}
    
    TBR=stevenjb@chromium.org,dtseng@chromium.org,agrieve@chromium.org
    
    NOTRY=true
    
    Bug: 1023568
    Change-Id: I4b506ef6d574a1e300383359aa2fbdc81b332252
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1925888
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716900}
    
    Former-commit-id: e510762b5961ba3db93c8610aea88c2359f5f233

--- chrome/browser/resources/chromeos/chromevox/tools/generate_test_messages.py
+++ chrome/browser/resources/chromeos/chromevox/tools/generate_test_messages.py
@@ -6,7 +6,6 @@
 
 '''Generates test_messages.js from an extension message json file.'''
 
-import gzip
 import optparse
 import sys
 
@@ -36,11 +35,7 @@ def main():
   if len(args) != 1:
     Die('Exactly one input file must be specified')
   in_file_name = args[0]
-  def _OpenFile(filename):
-    if filename.endswith('.gz'):
-      return gzip.open(filename)
-    return open(filename)
-  with _OpenFile(in_file_name) as in_file:
+  with open(in_file_name) as in_file:
     json = in_file.read().strip()
   with open(options.output_file, 'w') as out_file:
     out_file.write(_JS_TEMPLATE % {'in_file': in_file_name, 'json': json})
--- tools/grit/grit/tool/build.py
+++ tools/grit/grit/tool/build.py
@@ -10,7 +10,6 @@ from __future__ import print_function
 import codecs
 import filecmp
 import getopt
-import gzip
 import os
 import shutil
 import sys
@@ -35,7 +34,6 @@ _format_modules = {
   'android': 'android_xml',
   'c_format': 'c_format',
   'chrome_messages_json': 'chrome_messages_json',
-  'chrome_messages_json_gzip': 'chrome_messages_json',
   'data_package': 'data_pack',
   'policy_templates': 'policy_templates_json',
   'rc_all': 'rc',
@@ -361,7 +359,7 @@ are exported to translation interchange files (e.g. XMB files), etc.
       return 'cp1252'
     if output_type in ('android', 'c_format',  'plist', 'plist_strings', 'doc',
                        'json', 'android_policy', 'chrome_messages_json',
-                       'chrome_messages_json_gzip', 'policy_templates'):
+                       'policy_templates'):
       return 'utf_8'
     # TODO(gfeher) modify here to set utf-8 encoding for admx/adml
     return 'utf_16'
@@ -394,34 +392,24 @@ are exported to translation interchange files (e.g. XMB files), etc.
 
       # Write the results to a temporary file and only overwrite the original
       # if the file changed.  This avoids unnecessary rebuilds.
-      out_filename = output.GetOutputFilename()
-      tmp_filename = out_filename + '.tmp'
-      tmpfile = self.fo_create(tmp_filename, 'wb')
+      outfile = self.fo_create(output.GetOutputFilename() + '.tmp', 'wb')
 
-      output_type = output.GetType()
-      if output_type != 'data_package':
-        encoding = self._EncodingForOutputType(output_type)
-        tmpfile = util.WrapOutputStream(tmpfile, encoding)
+      if output.GetType() != 'data_package':
+        encoding = self._EncodingForOutputType(output.GetType())
+        outfile = util.WrapOutputStream(outfile, encoding)
 
       # Iterate in-order through entire resource tree, calling formatters on
       # the entry into a node and on exit out of it.
-      with tmpfile:
-        self.ProcessNode(self.res, output, tmpfile)
-
-      if output_type == 'chrome_messages_json_gzip':
-        gz_filename = tmp_filename + '.gz'
-        with open(tmp_filename, 'rb') as tmpfile, open(gz_filename, 'wb') as f:
-          with gzip.GzipFile(filename='', mode='wb', fileobj=f, mtime=0) as fgz:
-            shutil.copyfileobj(tmpfile, fgz)
-        os.remove(tmp_filename)
-        tmp_filename = gz_filename
+      with outfile:
+        self.ProcessNode(self.res, output, outfile)
 
       # Now copy from the temp file back to the real output, but on Windows,
       # only if the real output doesn't exist or the contents of the file
       # changed.  This prevents identical headers from being written and .cc
       # files from recompiling (which is painful on Windows).
-      if not os.path.exists(out_filename):
-        os.rename(tmp_filename, out_filename)
+      if not os.path.exists(output.GetOutputFilename()):
+        os.rename(output.GetOutputFilename() + '.tmp',
+                  output.GetOutputFilename())
       else:
         # CHROMIUM SPECIFIC CHANGE.
         # This clashes with gyp + vstudio, which expect the output timestamp
@@ -430,11 +418,13 @@ are exported to translation interchange files (e.g. XMB files), etc.
         if not self.write_only_new:
           write_file = True
         else:
-          files_match = filecmp.cmp(out_filename, tmp_filename)
+          files_match = filecmp.cmp(output.GetOutputFilename(),
+              output.GetOutputFilename() + '.tmp')
           write_file = not files_match
         if write_file:
-          shutil.copy2(tmp_filename, out_filename)
-        os.remove(tmp_filename)
+          shutil.copy2(output.GetOutputFilename() + '.tmp',
+                       output.GetOutputFilename())
+        os.remove(output.GetOutputFilename() + '.tmp')
 
       self.VerboseOut(' done.\n')
 
