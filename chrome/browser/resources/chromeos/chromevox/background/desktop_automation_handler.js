b885cbcb17dcf022738f7e2f8d2e0b25171eecaa
David Tseng <dtseng@chromium.org>
2019-11-20 18:02:49 +0000

    Ignore selection events when using ChromeVox with touch
    Change-Id: I5aefa9a356ddbbdf473a21eb74d0470cdc61be9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1925588
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717074}
    
    Former-commit-id: 3acb22809d3d7b456d5f54eba222832ff3d65610

--- chrome/browser/resources/chromeos/chromevox/background/desktop_automation_handler.js
+++ chrome/browser/resources/chromeos/chromevox/background/desktop_automation_handler.js
@@ -656,6 +656,10 @@ DesktopAutomationHandler.prototype = {
    * @param {!AutomationEvent} evt
    */
   onSelection: function(evt) {
+    // Never process selection when interacting with gestures.
+    if (EventSourceState.get() == EventSourceType.TOUCH_GESTURE)
+      return;
+
     // Invalidate any previous editable text handler state since some nodes,
     // like menuitems, can receive selection while focus remains on an editable
     // leading to braille output routing to the editable.
