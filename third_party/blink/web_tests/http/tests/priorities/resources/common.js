e2129c1c20d9239f13feb36c82231b6485153014
Dominic Farolino <dom@chromium.org>
2019-11-20 06:53:34 +0000

    Re-add disabled audio/video preload tests
    This CL adds back the resource load priority preload audio and video
    tests so they are not forgotten, but disables them which was the
    intention of crrev.com/c/1440522. Also adds a note indicating pointing
    to the spec complexity around why they are disabled.
    
    R=yoavweiss@chromium.org
    
    Bug: 735014,977033
    Change-Id: I2707a1bd39102e894b803f10f3305b8b8efea796
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1902481
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716885}
    
    Former-commit-id: 6404bad318a664960761e3fce75f53314fed6549

--- third_party/blink/web_tests/http/tests/priorities/resources/common.js
+++ third_party/blink/web_tests/http/tests/priorities/resources/common.js
@@ -11,6 +11,33 @@ const kVeryLow = 0,
       kHigh = 3,
       kVeryHigh = 4;
 
+function openWindow(url) {
+  const win = window.open(url, '_blank');
+  add_result_callback(() => win.close());
+}
+
+function resource_load_priority_test(windowURL, expected_priority,
+                                     description) {
+  promise_test(async () => {
+    openWindow('resources/' + windowURL);
+
+    // The order in which these two events are sent can't be relied upon.
+    const priority_event_promise =
+       new Promise(resolve => window.onRequestPriorityUpdated = resolve);
+    const subresource_finished_loading_event_promise =
+       new Promise(resolve => window.onRequestStatusChanged = resolve);
+
+    const priority_event = await priority_event_promise;
+    assert_equals(priority_event, expected_priority);
+
+    const subresource_finished_loading_event =
+        await subresource_finished_loading_event_promise;
+    assert_equals(subresource_finished_loading_event, 'LOADED',
+                  'The resource loaded successfully');
+
+  }, description);
+}
+
 function observeAndReportResourceLoadPriority(url, optionalDoc, message) {
   const documentToUse = optionalDoc ? optionalDoc : document;
   return internals.getResourcePriority(url, documentToUse)
