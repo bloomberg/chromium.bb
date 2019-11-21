f9103306ea06dd880c4ded64f1d0437a6179930d
Eric Seckler <eseckler@chromium.org>
2019-11-21 23:36:42 +0000

    perfetto: Use a smaller StringPool block size for TraceProcessor
    TraceProcessor attempts to allocate a 4GB block on 64bit, which likely
    fails due to sandbox limits. This patch decreases the block size to
    32MB.
    
    Bug: 1026682
    Change-Id: If42128238fb522325f1eeb838630e663b5e4f595
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1929814
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: oysteine <oysteine@chromium.org>
    Auto-Submit: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717891}
    
    Former-commit-id: 4616c405c4158e631a2fb0bb13b853232dea7f7b

--- services/tracing/perfetto/consumer_host.cc
+++ services/tracing/perfetto/consumer_host.cc
@@ -393,9 +393,14 @@ void ConsumerHost::TracingSession::DisableTracingAndEmitJson(
 
   json_agent_label_filter_ = agent_label_filter;
 
+  perfetto::trace_processor::Config processor_config;
+  // Chrome uses a smaller block size than the default even on 64-bit machines,
+  // because the sandbox for our utility process may restrict our address space.
+  // The string pool can still grow larger across multiple blocks if necessary.
+  processor_config.string_pool_block_size_bytes = 32 * 1024 * 1024;  // 32 mB.
   trace_processor_ =
       perfetto::trace_processor::TraceProcessorStorage::CreateInstance(
-          perfetto::trace_processor::Config());
+          processor_config);
 
   DisableTracing();
 }
