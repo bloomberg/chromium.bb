e455ffa35cf8e62685bbaf9919a93b59a4c196ee
Tim Volodine <timvolodine@chromium.org>
2019-11-22 18:41:15 +0000

    [WebLayer] Enable safebrowsing by default and update test targets deps.
    Set the WebLayerSafeBrowsing feature to be enabled
    by default in weblayer.
    
    Also adds components/safebrowsing java dependency
    to the following targets:
    - weblayer_instrumentation_test_apk
    - weblayer_browsertests_java
    in order to make test bots work (e.g. otherwise
    missing SafeBrowsingApiBridge java class on the
    android-marshmallow-arm64-rel bot).
    
    Note:
    depends on https://crrev.com/c/1926321 (for M80+)
    
    BUG=1015418
    
    Change-Id: Ibcf44d6f4ad249001813c29a082292d3d46fd73e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1926471
    Commit-Queue: Tim Volodine <timvolodine@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718216}
    
    Former-commit-id: 2a246cc872a04ef259ec99e129d3f56595970d6e

--- weblayer/common/features.cc
+++ weblayer/common/features.cc
@@ -11,7 +11,7 @@ namespace features {
 
 // Safebrowsing support for weblayer.
 const base::Feature kWebLayerSafeBrowsing{"WebLayerSafeBrowsing",
-                                          base::FEATURE_DISABLED_BY_DEFAULT};
+                                          base::FEATURE_ENABLED_BY_DEFAULT};
 
 }  // namespace features
 }  // namespace weblayer
