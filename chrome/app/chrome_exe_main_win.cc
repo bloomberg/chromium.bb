23bb6939479d67d13db0d72d75789a0fec00042f
Rafael Cintron <rafael.cintron@microsoft.com>
2019-11-22 00:18:18 +0000

    Only enable high DPI mode for browser and GPU process
    The following commit switched to only enabling high DPI mode for the
    browser process. However, that regressed multi-monitor setups like my
    own where a high DPI monitor sits to the left of a low DPI monitor.
    
    Expanding the restriction to be the browser process and GPU process
    fixes the bug.
    
    > commit 094d77ba1112455c5b27b72012a31e39482c5b0e [formerly f31b41f507a8898f4b9edb3afe22df7a3448e3b8]
    > Author: Will Harris <wfh@chromium.org>
    > Date:   Wed Nov 13 16:13:56 2019 +0000
    >
    >     Only enable high DPI support for browser process.
    >
    >     BUG=978133
    >
    >    Change-Id: I9bdad7579ceedc467b029fcc65f8e3c5d2dae1de
    >    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1912502
    >    Reviewed-by: Greg Thompson <grt@chromium.org>
    >    Commit-Queue: Will Harris <wfh@chromium.org>
    >    Cr-Commit-Position: refs/heads/master@{#714939}
    
    Bug: 1026794
    Change-Id: Ic23d74ea1e3e4d3fc17f3de460e78e2ed0cac1fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1929444
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717919}
    
    Former-commit-id: 95c9acf0b254bedae5128b16ad2354969331a887

--- chrome/app/chrome_exe_main_win.cc
+++ chrome/app/chrome_exe_main_win.cc
@@ -221,8 +221,8 @@ int main() {
   // The exit manager is in charge of calling the dtors of singletons.
   base::AtExitManager exit_manager;
 
-  // Only enable High DPI support for browser process.
-  if (process_type.empty())
+  // Only enable High DPI support for browser and GPU process.
+  if (process_type.empty() || process_type == switches::kGpuProcess)
     base::win::EnableHighDPISupport();
 
   if (AttemptFastNotify(*command_line))
