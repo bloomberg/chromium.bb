a8d20d9cbb1e1b66de58dc0b47c74c3508e494ea
Shakti Sahu <shaktisahu@chromium.org>
2019-11-22 02:36:18 +0000

    Explore offline card on NTP
    Updated the explore offline card on NTP to match the new UX specs
    https://docs.google.com/presentation/d/1fXWP8nxQjNbE2pdNkHPje6ss_KazaIgxfX04QgeyA6s/edit#slide=id.g6596215654_0_66
    
    Bug: 1012267
    Change-Id: Ifb5bd3073be6e80521110dbaf820131c2b2946b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1910702
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717991}
    
    Former-commit-id: fe4536352b6576655923722df052792627203271

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/ExploreOfflineCard.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/ExploreOfflineCard.java
@@ -4,18 +4,14 @@
 
 package org.chromium.chrome.browser.ntp.cards;
 
-import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.view.ViewStub;
-import android.widget.TextView;
+import android.widget.Button;
 
-import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.download.DownloadUtils;
-import org.chromium.chrome.browser.feed.FeedConfiguration;
 import org.chromium.net.NetworkChangeNotifier;
-import org.chromium.ui.text.SpanApplier;
 
 /**
  * Shows a card prompting the user to view offline content when they are offline. This class is
@@ -25,6 +21,7 @@ public class ExploreOfflineCard {
     private final View mRootView;
     private final Runnable mOpenDownloadHomeCallback;
     private NetworkChangeNotifier.ConnectionTypeObserver mConnectionTypeObserver;
+    private boolean mCardDismissed;
 
     /**
      * Constructor.
@@ -50,34 +47,31 @@ public class ExploreOfflineCard {
     }
 
     private void setCardViewVisibility() {
-        boolean isVisible = DownloadUtils.shouldShowOfflineHome();
+        boolean isVisible = !mCardDismissed && DownloadUtils.shouldShowOfflineHome();
         View cardView = mRootView.findViewById(R.id.explore_offline_card);
 
         if (cardView == null) {
             if (!isVisible) return;
             cardView = inflateCardLayout();
         }
-        cardView.setVisibility(DownloadUtils.shouldShowOfflineHome() ? View.VISIBLE : View.GONE);
+        cardView.setVisibility(isVisible ? View.VISIBLE : View.GONE);
     }
 
     private View inflateCardLayout() {
         View cardView =
                 ((ViewStub) mRootView.findViewById(R.id.explore_offline_card_stub)).inflate();
-        TextView messageView = cardView.findViewById(R.id.explore_offline_text);
-        messageView.setText(SpanApplier.applySpans(cardView.getContext().getResources().getString(
-                                                           R.string.explore_offline_card_message),
-                new SpanApplier.SpanInfo("<link>", "</link>",
-                        new ForegroundColorSpan(
-                                ApiCompatibilityUtils.getColor(cardView.getContext().getResources(),
-                                        R.color.blue_when_enabled)))));
+        Button confirmButton = cardView.findViewById(R.id.button_primary);
+        Button cancelButton = cardView.findViewById(R.id.button_secondary);
 
-        View imageView = cardView.findViewById(R.id.explore_offline_image);
-        imageView.setBackground(imageView.getContext().getResources().getDrawable(
-                FeedConfiguration.getFeedUiEnabled()
-                        ? R.drawable.card_background_rounded_right_half_with_border
-                        : R.drawable.card_background_rounded_right_half_no_border));
-
-        cardView.setOnClickListener(v -> mOpenDownloadHomeCallback.run());
+        confirmButton.setOnClickListener(v -> {
+            mCardDismissed = true;
+            setCardViewVisibility();
+            mOpenDownloadHomeCallback.run();
+        });
+        cancelButton.setOnClickListener(v -> {
+            mCardDismissed = true;
+            setCardViewVisibility();
+        });
         return cardView;
     }
 }
