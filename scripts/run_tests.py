f6f9d579105b76dbd538ba968c14b173596b4578
Mike Frysinger <vapier@chromium.org>
2019-11-24 04:43:19 -0500

    run_tests: enable build_stages_unittest
    We fixed this at some point but missed enabling it.
    
    BUG=chromium:997354
    TEST=`./run_tests` passes
    
    Change-Id: If2cf5d5defb95ae301c666647b437301cc8676f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/1933552
    Reviewed-by: Michael Mortensen <mmortensen@google.com>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -156,7 +156,6 @@ PYTHON2_ONLY_TESTS = {
     'cbuildbot/lkgm_manager_unittest',
     'cbuildbot/manifest_version_unittest',
     'cbuildbot/stages/artifact_stages_unittest',
-    'cbuildbot/stages/build_stages_unittest',
     'cbuildbot/stages/completion_stages_unittest',
     'cbuildbot/stages/release_stages_unittest',
     'cbuildbot/stages/report_stages_unittest',
