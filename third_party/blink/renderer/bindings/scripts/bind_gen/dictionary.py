e29a4a9f05123a48b541a4a911f6e8913550d7e4
Hitoshi Yoshida <peria@chromium.org>
2019-11-26 09:42:38 +0000

    CodeGen: Generate IDL dictionary (3/N)
    Implements Create() that creates a Blink object from a V8 object.
    In case we have an exception internally, Create() returns a nullptr.
    
    
    Bug: 839389
    Change-Id: Iaebd4c1b2a7e9f510eed7a0d13bb5cd7999e9591
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1911799
    Commit-Queue: Hitoshi Yoshida <peria@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719074}
    
    Former-commit-id: a5fa28a691f5542fb0b32f0c9db55e05da658f21

--- third_party/blink/renderer/bindings/scripts/bind_gen/dictionary.py
+++ third_party/blink/renderer/bindings/scripts/bind_gen/dictionary.py
@@ -4,9 +4,12 @@
 
 import os.path
 
+import web_idl
+
 from . import name_style
 from .blink_v8_bridge import blink_class_name
 from .blink_v8_bridge import blink_type_info
+from .blink_v8_bridge import make_v8_to_blink_value
 from .code_node import CodeNode
 from .code_node import FunctionDefinitionNode
 from .code_node import SymbolScopeNode
@@ -93,6 +96,36 @@ def make_dict_member_set_def(cg_context):
     return func_def
 
 
+def make_get_v8_dict_member_names_def(cg_context):
+    assert isinstance(cg_context, CodeGenContext)
+
+    T = TextNode
+
+    dictionary = cg_context.dictionary
+
+    func_name = _format("{}::GetV8MemberNames",
+                        blink_class_name(cg_context.dictionary))
+    func_def = FunctionDefinitionNode(
+        name=T(func_name),
+        arg_decls=[T("v8::Isolate* isolate")],
+        return_type=T("const v8::Eternal<v8::Name>*"),
+        comment=T("// static"))
+
+    body = func_def.body
+
+    pattern = ("static const char* kKeyStrings[] = {{{_1}}};")
+    _1 = ", ".join(
+        _format("\"{}\"", member.identifier) for member in dictionary.members)
+    body.extend([
+        T(_format(pattern, _1=_1)),
+        T("return V8PerIsolateData::From(isolate)"
+          "->FindOrCreateEternalNameCache("
+          "kKeyStrings, kKeyStrings, base::size(kKeyStrings));")
+    ])
+
+    return func_def
+
+
 def make_fill_with_dict_members_def(cg_context):
     assert isinstance(cg_context, CodeGenContext)
 
@@ -117,8 +150,9 @@ def make_fill_with_dict_members_def(cg_context):
     if dictionary.inherited:
         _1 = blink_class_name(dictionary.inherited)
         pattern = ("if (!{_1}::FillWithMembers("
-                   "isolate, creation_context, v8_dictionary))\n"
-                   "  return false;")
+                   "isolate, creation_context, v8_dictionary)) {{\n"
+                   "  return false;\n"
+                   "}}")
         body.append(T(_format(pattern, _1=_1)))
 
     body.append(
@@ -150,11 +184,10 @@ def make_fill_with_own_dict_members_def(cg_context):
     body = func_def.body
     body.add_template_vars(cg_context.template_bindings())
 
-    body.extend([
-        T("const v8::Eternal<v8::Name>* member_keys = EternalKeys(isolate);"),
-        T("v8::Local<v8::Context> current_context = "
-          "isolate->GetCurrentContext();"),
-    ])
+    text = """\
+const v8::Eternal<v8::Name>* member_names = GetV8MemberNames(isolate);
+v8::Local<v8::Context> current_context = isolate->GetCurrentContext();"""
+    body.append(T(text))
 
     # TODO(peria): Support runtime enabled / origin trial members.
     for key_index, member in enumerate(own_members):
@@ -164,7 +197,7 @@ def make_fill_with_own_dict_members_def(cg_context):
         pattern = ("""
 if ({_1}()) {{
   v8::Local<v8::Value> v8_value = ToV8({_2}(), creation_context, isolate);
-  v8::Local<v8::Name> key = member_keys[{_3}].Get(isolate);
+  v8::Local<v8::Name> key = member_names[{_3}].Get(isolate);
   if (!v8_dictionary->CreateDataProperty(current_context, key, v8_value)
            .ToChecked()) {{
     return false;
@@ -172,15 +205,197 @@ if ({_1}()) {{
 }}\
 """)
         body.append(T(_format(pattern, _1=_1, _2=_2, _3=_3)))
-
     body.append(T("return true;"))
 
     return func_def
 
 
+def make_dict_create_def(cg_context):
+    assert isinstance(cg_context, CodeGenContext)
+
+    T = TextNode
+
+    dictionary = cg_context.dictionary
+    class_name = blink_class_name(dictionary)
+
+    func_def = FunctionDefinitionNode(
+        name=T(_format("{_1}::Create", _1=class_name)),
+        arg_decls=[
+            T("v8::Isolate* isolate"),
+            T("v8::Local<v8::Object> v8_dictionary"),
+            T("ExceptionState& exception_state"),
+        ],
+        return_type=T(_format("{_1}*", _1=class_name)),
+        comment=T("// static"))
+
+    body = func_def.body
+    body.add_template_vars(cg_context.template_bindings())
+
+    pattern = """\
+{_1}* dictionary = MakeGarbageCollected<{_1}>();
+dictionary->FillMembers(isolate, v8_dictionary, exception_state);
+if (exception_state.HadException()) {
+  return nullptr;
+}
+return dictionary;"""
+    body.append(T(_format(pattern, _1=class_name)))
+
+    return func_def
+
+
+def make_fill_dict_members_def(cg_context):
+    assert isinstance(cg_context, CodeGenContext)
+
+    T = TextNode
+
+    dictionary = cg_context.dictionary
+    class_name = blink_class_name(dictionary)
+    own_members = sorted(dictionary.own_members, key=lambda m: m.identifier)
+    required_own_members = list(
+        member for member in own_members if member.is_required)
+
+    func_name = _format("{_1}::FillMembers", _1=class_name)
+    func_def = FunctionDefinitionNode(
+        name=T(func_name),
+        arg_decls=[
+            T("v8::Isolate* isolate"),
+            T("v8::Local<v8::Object> v8_dictionary"),
+            T("ExceptionState& exception_state"),
+        ],
+        return_type=T("void"))
+
+    body = func_def.body
+    body.add_template_vars(cg_context.template_bindings())
+
+    text = "if (v8_dictionary->IsUndefinedOrNull()) { return; }"
+    if len(required_own_members) > 0:
+        text = """\
+if (v8_dictionary->IsUndefinedOrNull()) {
+  exception_state.ThrowError(ExceptionMessages::FailedToConstruct(
+      "${dictionary.identifier}",
+      "has required members, but null/undefined was passed."));
+  return;
+}"""
+    body.append(T(text))
+
+    # [PermissiveDictionaryConversion]
+    if "PermissiveDictionaryConversion" in dictionary.extended_attributes:
+        text = """\
+if (!v8_dictionary->IsObject()) {
+  // [PermissiveDictionaryConversion]
+  return;
+}"""
+    else:
+        text = """\
+if (!v8_dictionary->IsObject()) {
+  exception_state.ThrowTypeError(
+      ExceptionMessages::FailedToConstruct(
+          "${dictionary.identifier}", "The value is not of type Object"));
+  return;
+}"""
+    body.append(T(text))
+
+    body.append(
+        T("FillMembersInternal(isolate, v8_dictionary, exception_state);"))
+
+    return func_def
+
+
+def make_fill_dict_members_internal_def(cg_context):
+    assert isinstance(cg_context, CodeGenContext)
+
+    T = TextNode
+
+    dictionary = cg_context.dictionary
+    own_members = sorted(dictionary.own_members, key=lambda m: m.identifier)
+    class_name = blink_class_name(dictionary)
+
+    func_name = _format("{_1}::FillMembersInternal", _1=class_name)
+    func_def = FunctionDefinitionNode(
+        name=T(func_name),
+        arg_decls=[
+            T("v8::Isolate* isolate"),
+            T("v8::Local<v8::Object> v8_dictionary"),
+            T("ExceptionState& exception_state"),
+        ],
+        return_type=T("void"))
+
+    body = func_def.body
+    body.add_template_vars(cg_context.template_bindings())
+    body.add_template_var("isolate", "isolate")
+    body.add_template_var("exception_state", "exception_state")
+
+    if dictionary.inherited:
+        pattern = """\
+{_1}::FillMembersInternal(${isolate}, v8_dictionary, ${exception_state});
+if (${exception_state}.HadException()) {{
+  return;
+}}
+"""
+        _1 = blink_class_name(dictionary.inherited)
+        body.append(T(_format(pattern, _1=_1)))
+
+    body.extend([
+        T("const v8::Eternal<v8::Name>* member_names = "
+          "GetV8MemberNames(${isolate});"),
+        T("v8::TryCatch try_block(${isolate});"),
+        T("v8::Local<v8::Context> current_context = "
+          "${isolate}->GetCurrentContext();"),
+        T("v8::Local<v8::Value> v8_value;"),
+    ])
+
+    # TODO(peria): Support origin-trials and runtime enabled features.
+    for key_index, member in enumerate(own_members):
+        body.append(make_fill_own_dict_member(key_index, member))
+
+    return func_def
+
+
+def make_fill_own_dict_member(key_index, member):
+    assert isinstance(key_index, int)
+    assert isinstance(member, web_idl.DictionaryMember)
+
+    T = TextNode
+
+    member_set_func = name_style.api_func("set", member.identifier)
+
+    node = SymbolScopeNode()
+
+    pattern = """
+if (!v8_dictionary->Get(current_context, member_names[{_1}].Get(${isolate}))
+         .ToLocal(&v8_memer)) {{
+  ${exception_state}.RethrowV8Exception(try_block.Exception());
+  return;
+}}"""
+    node.append(T(_format(pattern, _1=key_index)))
+
+    internal_node = SymbolScopeNode()
+    internal_node.register_code_symbol(
+        make_v8_to_blink_value("blink_value", "v8_value", member.idl_type))
+    internal_node.append(
+        T(_format("{_1}(${blink_value});", _1=member_set_func)))
+    node.extend([
+        T("if (!v8_value->IsUndefined()) {"),
+        internal_node,
+        T("}"),
+    ])
+
+    if member.is_required:
+        pattern = """\
+else {{
+  ${exception_state}.ThrowTypeError(
+      ExceptionMessages::FailedToGet(
+          "{_1}", "${{dictionary.identifier}}",
+          "Required member is undefined."));
+}}"""
+        node.append(T(_format(pattern, _1=member.identifier)))
+
+    return node
+
+
 def generate_dictionaries(web_idl_database, output_dirs):
-    dictionary = web_idl_database.find("ExampleDictionary")
-    filename = "{}.cc".format(name_style.file(dictionary.identifier))
+    dictionary = web_idl_database.find("RTCTrackEventInit")
+    filename = "example_dictionary.cc"
     filepath = os.path.join(output_dirs['core'], filename)
 
     cg_context = CodeGenContext(dictionary=dictionary)
@@ -190,14 +405,22 @@ def generate_dictionaries(web_idl_database, output_dirs):
 
     code_node = SymbolScopeNode()
 
-    code_node.append(make_fill_with_dict_members_def(cg_context))
-    code_node.append(make_fill_with_own_dict_members_def(cg_context))
+    code_node.extend([
+        make_get_v8_dict_member_names_def(cg_context),
+        make_dict_create_def(cg_context),
+        make_fill_dict_members_def(cg_context),
+        make_fill_dict_members_internal_def(cg_context),
+        make_fill_with_dict_members_def(cg_context),
+        make_fill_with_own_dict_members_def(cg_context),
+    ])
+
     for member in sorted(dictionary.own_members, key=lambda x: x.identifier):
         code_node.extend([
             make_dict_member_get_def(cg_context.make_copy(dict_member=member)),
             make_dict_member_has_def(cg_context.make_copy(dict_member=member)),
             make_dict_member_set_def(cg_context.make_copy(dict_member=member)),
         ])
+
     root_node.extend([
         make_copyright_header(),
         TextNode(""),
