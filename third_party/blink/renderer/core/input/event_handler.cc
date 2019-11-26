b1ead64103b03aa67e9668e84306ed6f814af670
Jinsong Fan <fanjinsong@sogou-inc.com>
2019-11-26 01:06:59 +0000

    Remove diagnose CHECKs for the issue has been fixed
    No change to logical.
    
    Bug: 1020939
    Change-Id: Ic0d98e8f6a2626da9a82007a7410d0f85918b3d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1934131
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Jinsong Fan <fanjinsong@sogou-inc.com>
    Cr-Commit-Position: refs/heads/master@{#718933}
    
    Former-commit-id: e0e27d970119bef4c5448647990c625e7026253e

--- third_party/blink/renderer/core/input/event_handler.cc
+++ third_party/blink/renderer/core/input/event_handler.cc
@@ -2023,8 +2023,6 @@ WebInputEventResult EventHandler::ShowNonLocatedContextMenu(
   Document* doc = frame_->GetDocument();
   if (!doc)
     return WebInputEventResult::kNotHandled;
-  // TODO(szager): Remove CHECK after diagnosing crash in crbug.com/1020939
-  CHECK(doc->IsActive());
 
   static const int kContextMenuMargin = 1;
 
@@ -2032,8 +2030,6 @@ WebInputEventResult EventHandler::ShowNonLocatedContextMenu(
 
   Element* focused_element =
       override_target_element ? override_target_element : doc->FocusedElement();
-  // TODO(szager): Remove CHECK after diagnosing crash in crbug.com/1020939
-  CHECK(!focused_element || focused_element->GetLayoutObject());
   FrameSelection& selection = frame_->Selection();
   VisualViewport& visual_viewport = frame_->GetPage()->GetVisualViewport();
 
