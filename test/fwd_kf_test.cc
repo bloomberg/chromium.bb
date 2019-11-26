3001dee0eb208996d7281da4305f42d90702b194
Sarah Parker <sarahparker@google.com>
2019-11-26 15:35:33 -0600

    Adjust psnr threshold in fwd-kf test
    Change-Id: I0b13da3d92482738806f4178a6c0afa78bdd7627

--- test/fwd_kf_test.cc
+++ test/fwd_kf_test.cc
@@ -23,7 +23,7 @@ typedef struct {
 } FwdKfTestParam;
 
 const FwdKfTestParam kTestParams[] = {
-  { 4, 34.0 },  { 6, 33.3 },  { 8, 33.0 },
+  { 4, 33.4 },  { 6, 32.9 },  { 8, 32.6 },
   { 12, 32.4 }, { 16, 32.3 }, { 18, 32.1 }
 };
 
