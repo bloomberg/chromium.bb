a529e50daf146e4fa264b9c03931915dbc44cfd4
Tim Schumann <tschumann@chromium.org>
2019-11-26 08:40:34 +0000

    Revert "Re-enable DeleteService_HandleHeld test"
    This reverts commit a94dfa62c12c42a4b021490274510d47566b7306 [formerly 492c759113c40c9aa315e647f5bc431d05f390c7].
    
    Reason for revert: Times out on Win7 tests (dbg)
    
    Link to instance: https://ci.chromium.org/p/chromium/builders/ci/Win7%20Tests%20%28dbg%29%281%29/80319
    
    From the logs:
    [ RUN      ] CleanerSandboxInterface.DeleteService_HandleHeld
    ../../chrome/chrome_cleaner/engines/broker/cleaner_sandbox_interface_unittest.cc(809): error: Value of: service_handle.StartService()
      Actual: false (Failed to start 2E684A79ADD8581E0F0A57FB8FBF785B:The service did not respond to the start or control request in a timely fashion. (0x41D))
    Expected: true
    Stack trace:
    Backtrace:
    	base::debug::StackTrace::StackTrace [0x73FFA092+34] (o:\base\debug\stack_trace.cc:203)
    	StackTraceGetter::CurrentStackTrace [0x0070905A+90] (o:\third_party\googletest\custom\gtest\internal\custom\stack_trace_getter.cc:24)
    	testing::internal::UnitTestImpl::CurrentOsStackTraceExceptTop [0x0071DDCA+74] (o:\third_party\googletest\src\googletest\src\gtest.cc:820)
    	testing::internal::AssertHelper::operator= [0x0071D93B+75] (o:\third_party\googletest\src\googletest\src\gtest.cc:397)
    
    [  FAILED  ] CleanerSandboxInterface.DeleteService_HandleHeld (312 ms)
    
    Original change's description:
    > Re-enable DeleteService_HandleHeld test
    > 
    > crrev.com/c/1919544 is the potential fix for the test.
    > I have to enable the test to see if it really can fix
    > the test and then enable more disabled tests.
    > 
    > Bug: 956016
    > Change-Id: I9448cb928edf50e131e8c1c5886e7d26437b0aaa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1928129
    > Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    > Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#718748}
    
    TBR=joenotcharles@chromium.org,svenzheng@chromium.org
    
    Change-Id: Ie64b3260ce0cefc21a62c9736bc869a120fb7733
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 956016
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1936267
    Reviewed-by: Tim Schumann <tschumann@chromium.org>
    Commit-Queue: Tim Schumann <tschumann@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719063}
    
    Former-commit-id: c36abe8100b7e93f41933693ed10118aecccfa2e

--- chrome/chrome_cleaner/engines/broker/cleaner_sandbox_interface_unittest.cc
+++ chrome/chrome_cleaner/engines/broker/cleaner_sandbox_interface_unittest.cc
@@ -801,7 +801,8 @@ TEST(CleanerSandboxInterface, DISABLED_DeleteService_Running) {
   EXPECT_FALSE(chrome_cleaner::DoesServiceExist(service_handle.service_name()));
 }
 
-TEST(CleanerSandboxInterface, DeleteService_HandleHeld) {
+// Disabled: https://crbug.com/956016
+TEST(CleanerSandboxInterface, DISABLED_DeleteService_HandleHeld) {
   ASSERT_TRUE(chrome_cleaner::EnsureNoTestServicesRunning());
 
   chrome_cleaner::TestScopedServiceHandle service_handle;
