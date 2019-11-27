eed15f21e5087125e294ea311f6467cb88d71f7e
Noel Gordon <noel@chromium.org>
2019-11-27 05:40:48 +0000

    [directorytree] Improve the keyboard horizontal scroll test
     - use keyboard arrow-right key to expand the nested folders
     - speed: a smaller number of entries (still repros the bug)
     - make comments consistent across the test fixtures
     - add files-ng TODO
    
    Bug: 1025581
    Fixed: 1025581
    Change-Id: Ia239b2ce194322df4894902f2a6522bc99f92be3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1937957
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719512}
    
    Former-commit-id: 503f800b6fea27c30547a3d177a11733be87b585

--- ui/file_manager/integration_tests/file_manager/directory_tree.js
+++ ui/file_manager/integration_tests/file_manager/directory_tree.js
@@ -3,13 +3,15 @@
 // found in the LICENSE file.
 
 'use strict';
+
 (() => {
   /**
    * Tests that the directory tree can be vertically scrolled.
    */
   testcase.directoryTreeVerticalScroll = async () => {
-    // Add directory entries to overflow the directory tree container.
     const folders = [ENTRIES.photos];
+
+    // Add enough test entries to overflow the #directory-tree container.
     for (let i = 0; i < 30; i++) {
       folders.push(new TestEntryInfo({
         type: EntryType.DIRECTORY,
@@ -21,11 +23,11 @@
       }));
     }
 
-    // Open FilesApp on Downloads. Expand the directory tree view of Downloads.
+    // Open FilesApp on Downloads and expand the tree view of Downloads.
     const appId = await setupAndWaitUntilReady(RootPath.DOWNLOADS, folders, []);
     await expandRoot(appId, TREEITEM_DOWNLOADS);
 
-    // Get the directory tree and verify it is not scrolled.
+    // Verify the directory tree is not vertically scrolled.
     const directoryTree = '#directory-tree';
     const original = await remoteCall.waitForElementStyles(
         appId, directoryTree, ['scrollTop']);
@@ -35,28 +37,29 @@
     await remoteCall.callRemoteTestUtil(
         'setScrollTop', appId, [directoryTree, 100]);
 
-    // Check: the directory tree should be scrolled.
+    // Check: the directory tree should be vertically scrolled.
     const scrolled = await remoteCall.waitForElementStyles(
         appId, directoryTree, ['scrollTop']);
-    chrome.test.assertTrue(scrolled.scrollTop === 100, 'Tree should scroll');
+    const scrolledDown = scrolled.scrollTop === 100;
+    chrome.test.assertTrue(scrolledDown, 'Tree should scroll down');
   };
 
   /**
    * Tests that the directory tree does not horizontally scroll.
    */
   testcase.directoryTreeHorizontalScroll = async () => {
-    // Open FilesApp on Downloads. Expand the navigation list view of Downloads.
+    // Open FilesApp on Downloads and expand the tree view of Downloads.
     const appId = await setupAndWaitUntilReady(
         RootPath.DOWNLOADS, BASIC_LOCAL_ENTRY_SET, []);
     await expandRoot(appId, TREEITEM_DOWNLOADS);
 
-    // Get the directory tree and verify it is not scrolled.
+    // Verify the directory tree is not horizontally scrolled.
     const directoryTree = '#directory-tree';
     const original = await remoteCall.waitForElementStyles(
         appId, directoryTree, ['scrollLeft']);
     chrome.test.assertTrue(original.scrollLeft === 0);
 
-    // Shrink the navigation list tree to 50px.
+    // Shrink the tree to 50px. TODO(files-ng): consider using 150px?
     const navigationList = '.dialog-navigation-list';
     await remoteCall.callRemoteTestUtil(
         'setElementStyles', appId, [navigationList, {width: '50px'}]);
@@ -65,10 +68,11 @@
     await remoteCall.callRemoteTestUtil(
         'setScrollLeft', appId, [directoryTree, 100]);
 
-    // Check; the directory tree should not be scrolled.
+    // Check: the directory tree should not be horizontally scrolled.
     const scrolled = await remoteCall.waitForElementStyles(
         appId, directoryTree, ['scrollLeft']);
-    chrome.test.assertTrue(scrolled.scrollLeft === 0, 'Tree should not scroll');
+    const noScrollLeft = scrolled.scrollLeft === 0;
+    chrome.test.assertTrue(noScrollLeft, 'Tree should not scroll left');
   };
 
   /**
@@ -95,7 +99,7 @@
 
     // Build an array of nested folder test entries.
     const nestedFolderTestEntries = [];
-    for (let path = 'nested-folder0', i = 0; i < 8; ++i) {
+    for (let path = 'nested-folder0', i = 0; i < 6; ++i) {
       nestedFolderTestEntries.push(createTestFolder(path));
       path += `/nested-folder${i + 1}`;
     }
@@ -115,23 +119,23 @@
     await remoteCall.callRemoteTestUtil(
         'setElementStyles', appId, [navigationList, {width: '150px'}]);
 
-    // Select Downloads folder.
-    chrome.test.assertTrue(await remoteCall.callRemoteTestUtil(
-        'selectFolderInTree', appId, ['Downloads']));
+    // Focus the tree and prepare to send it ArrowRight keys in pairs.
+    await remoteCall.callRemoteTestUtil('focus', appId, ['#directory-tree']);
+    const key = [directoryTree, 'ArrowRight', false, false, false];
 
-    // Keyboard expand Downloads > nested-folder0 > nested-folder1 > ...
+    // Emit keyboard events to select, then expand, the nested folders.
     for (let i = 0; i < nestedFolderTestEntries.length; ++i) {
-      chrome.test.assertTrue(await remoteCall.callRemoteTestUtil(
-          'expandSelectedFolderInTree', appId, []));
-      chrome.test.assertTrue(await remoteCall.callRemoteTestUtil(
-          'expandSelectedFolderInTree', appId, []));
+      chrome.test.assertTrue(
+          await remoteCall.callRemoteTestUtil('fakeKeyDown', appId, key));
+      chrome.test.assertTrue(
+          await remoteCall.callRemoteTestUtil('fakeKeyDown', appId, key));
       chrome.test.assertTrue(await remoteCall.callRemoteTestUtil(
           'requestAnimationFrame', appId, []));
     }
 
     // Wait until the directory tree has selected the last folder.
     await remoteCall.waitForElement(
-        appId, '.tree-item[selected][entry-label="nested-folder7"]');
+        appId, '.tree-item[selected][entry-label="nested-folder5"]');
 
     // Check: the directory tree should not be horizontally scrolled.
     const scrolled = await remoteCall.waitForElementStyles(
