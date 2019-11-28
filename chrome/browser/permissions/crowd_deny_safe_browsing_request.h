9ec18baeb261133e813156c2ee106a49b7cccfcd
Balazs Engedy <engedy@chromium.org>
2019-11-28 09:25:13 +0000

    Implement CrowdDenySafeBrowsingRequest.
    CrowdDenySafeBrowsingRequest interfaces with Safe Browsing to fetch
    whether a certain origin is known to show unsolicited notification
    permission requests to users.
    
    Bug: 1028642
    Change-Id: I9942350500e88115b0d2f42954222423d0eddd63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1940253
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719913}
    
    Former-commit-id: 893ce4b53e7908b17316cda7baee73a1b918dd23

---
+++ chrome/browser/permissions/crowd_deny_safe_browsing_request.h
@@ -0,0 +1,68 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_PERMISSIONS_CROWD_DENY_SAFE_BROWSING_REQUEST_H_
+#define CHROME_BROWSER_PERMISSIONS_CROWD_DENY_SAFE_BROWSING_REQUEST_H_
+
+#include <memory>
+
+#include "base/callback.h"
+#include "base/macros.h"
+#include "base/memory/ref_counted.h"
+#include "base/memory/weak_ptr.h"
+#include "base/optional.h"
+#include "content/public/browser/browser_thread.h"
+
+namespace url {
+class Origin;
+}
+
+namespace safe_browsing {
+class SafeBrowsingDatabaseManager;
+}
+
+// Represents a single request to the Safe Browsing service to fetch the crowd
+// deny verdict for a given origin. Can be created and used on any one thread.
+class CrowdDenySafeBrowsingRequest {
+ public:
+  // The crowd deny verdict for a given origin.
+  enum class Verdict {
+    kAcceptable,
+    kKnownToShowUnsolicitedNotificationPermissionRequests
+  };
+
+  using VerdictCallback = base::OnceCallback<void(Verdict)>;
+
+  // Constructs a request that fetches the verdict for |origin| by consulting
+  // the |database_manager|, and invokes |callback| when done.
+  //
+  // It is guaranteed that |callback| will never be invoked synchronously, and
+  // it will not be invoked after |this| goes out of scope.
+  CrowdDenySafeBrowsingRequest(
+      scoped_refptr<safe_browsing::SafeBrowsingDatabaseManager>
+          database_manager,
+      const url::Origin& origin,
+      VerdictCallback callback);
+  ~CrowdDenySafeBrowsingRequest();
+
+ private:
+  class SafeBrowsingClient;
+
+  CrowdDenySafeBrowsingRequest(const CrowdDenySafeBrowsingRequest&) = delete;
+  CrowdDenySafeBrowsingRequest& operator=(const CrowdDenySafeBrowsingRequest&) =
+      delete;
+
+  // Posted by the |client_| from the IO thread when it gets a response.
+  void OnReceivedResult(Verdict verdict);
+
+  // The client interfacing with Safe Browsing. Created on |this| thread, but
+  // used on the IO thread for the rest of its life.
+  std::unique_ptr<SafeBrowsingClient, content::BrowserThread::DeleteOnIOThread>
+      client_;
+
+  VerdictCallback callback_;
+  base::WeakPtrFactory<CrowdDenySafeBrowsingRequest> weak_factory_{this};
+};
+
+#endif  // CHROME_BROWSER_PERMISSIONS_CROWD_DENY_SAFE_BROWSING_REQUEST_H_
