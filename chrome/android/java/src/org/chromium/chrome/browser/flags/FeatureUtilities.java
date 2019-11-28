3230c8139337152a0b4245b2cf7becdefc5d87f0
Yue Zhang <yuezhanggg@chromium.org>
2019-11-28 00:15:02 +0000

    Support turning on Duet-TabStrip integration for testing
    Bug: 1022827
    Change-Id: Ie3ee8e5f3ced8c694cf964eb9b596cb30a19c36f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1941195
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719817}
    
    Former-commit-id: 2637a5d221ec08e6ab5d106fdd090c916a73053b

--- chrome/android/java/src/org/chromium/chrome/browser/flags/FeatureUtilities.java
+++ chrome/android/java/src/org/chromium/chrome/browser/flags/FeatureUtilities.java
@@ -496,6 +496,17 @@ public class FeatureUtilities {
         sFlags.put(ChromePreferenceKeys.START_SURFACE_ENABLED_KEY, isEnabled);
     }
 
+    /**
+     * Toggles whether the Duet-TabStrip integration is enabled for testing. Should be reset back to
+     * null after the test has finished. Notice that TabGroup should also be turned on in order to
+     * really get the feature.
+     */
+    @VisibleForTesting
+    public static void setDuetTabStripIntegrationAndroidEnabledForTesting(
+            @Nullable Boolean isEnabled) {
+        sFlags.put(ChromePreferenceKeys.DUET_TABSTRIP_INTEGRATION_ANDROID_ENABLED_KEY, isEnabled);
+    }
+
     private static boolean isHighEndPhone() {
         return !SysUtils.isLowEndDevice()
                 && !DeviceFormFactor.isNonMultiDisplayContextOnTablet(
