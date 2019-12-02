1112106a25c3d352b1bb27c5280f8b9833547003
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2019-12-02 16:28:23 +0000

    chromedriver: Include <memory> for std::make_unique().
    IWYU. Follow-up to commit 376a18cd9 ("events: ozone: Remove implicit init
    from GetKeyboardLayoutEngine"), which broke the non-libc++ non-ozone build:
    
    ../../chrome/test/chromedriver/keycode_text_conversion_ozone.cc:34:12: error: no member named 'make_unique' in namespace 'std'
          std::make_unique<ui::StubKeyboardLayoutEngine>();
          ~~~~~^
    ../../chrome/test/chromedriver/keycode_text_conversion_ozone.cc:34:52: error: expected '(' for function-style cast or type construction
          std::make_unique<ui::StubKeyboardLayoutEngine>();
                           ~~~~~~~~~~~~~~~~~~~~~~~~~~~~^
    ../../chrome/test/chromedriver/keycode_text_conversion_ozone.cc:34:54: error: expected expression
          std::make_unique<ui::StubKeyboardLayoutEngine>();
                                                         ^
    
    Bug: 957519
    Change-Id: I54d132875ffc65495cde651d40b1ea87b5b0b284
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1945176
    Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720517}
    
    Former-commit-id: 8a55b42c7077358b80d8c969e424b6fa5a7020b0

--- chrome/test/chromedriver/keycode_text_conversion_ozone.cc
+++ chrome/test/chromedriver/keycode_text_conversion_ozone.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <memory>
+
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversion_utils.h"
 #include "base/strings/utf_string_conversions.h"
