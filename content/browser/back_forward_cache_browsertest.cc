40cf39e3c30411c0983c98dd8b96ded63340607a
Guido Urdaneta <guidou@chromium.org>
2019-12-02 16:17:09 +0000

    Disable SensorBackForwardCacheBrowserTest.SensorPausedWhileCached
    Due to flakiness, disabling this test on Mac and Linux ASAN/TSAN
    
    Bug: 1029238
    Change-Id: I49d27dffff9b54b03308e1586822f148e9044e44
    TBR: altimin@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1945177
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720514}
    
    Former-commit-id: d7e0ca7cd3f6e390dcf4c5784d8275fd1f278368

--- content/browser/back_forward_cache_browsertest.cc
+++ content/browser/back_forward_cache_browsertest.cc
@@ -4248,8 +4248,15 @@ IN_PROC_BROWSER_TEST_F(SensorBackForwardCacheBrowserTest, OrientationCached) {
 // resets the reasing back to have alpha=0 and navigates back to the a-page and
 // catpures 3 more events and verifies that all events on the a-page have
 // alpha=1.
+// Flaky on Mac and Linux ASAN/TSAN. https://crbug.com/1029238
+#if defined(OS_MACOSX) || (defined(OS_LINUX) && (defined(ADDRESS_SANITIZER) || \
+                                                 defined(THREAD_SANITIZER)))
+#define MAYBE_SensorPausedWhileCached DISABLED_SensorPausedWhileCached
+#else
+#define MAYBE_SensorPausedWhileCached SensorPausedWhileCached
+#endif
 IN_PROC_BROWSER_TEST_F(SensorBackForwardCacheBrowserTest,
-                       SensorPausedWhileCached) {
+                       MAYBE_SensorPausedWhileCached) {
   ASSERT_TRUE(CreateHttpsServer()->Start());
   GURL url_a(https_server()->GetURL("a.com", "/title1.html"));
   GURL url_b(https_server()->GetURL("b.com", "/title1.html"));
