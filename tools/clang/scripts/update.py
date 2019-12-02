1a4497202d3e601caf428eba7b37bf8c7168e5c9
Hans Wennborg <hans@chromium.org>
2019-12-02 12:45:02 +0000

    Roll clang n332854-5190cf8f-1 : n332890-c2443155-1.
    Ran `tools/clang/scripts/upload_revision.py c2443155a0fb245c8f17f2c1c72b6ea391e86e81`.
    
    Bug: 1028108, 1029385
    Change-Id: I1222d343130a0c159c1283f20c41f60cb1b6db14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1944343
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720454}
    
    Former-commit-id: 505160defd6dabc9097c38773bf441bbe1632a5a

--- tools/clang/scripts/update.py
+++ tools/clang/scripts/update.py
@@ -39,8 +39,8 @@ import zipfile
 # Do NOT CHANGE this if you don't know what you're doing -- see
 # https://chromium.googlesource.com/chromium/src/+/master/docs/updating_clang.md
 # Reverting problematic clang rolls is safe, though.
-CLANG_REVISION = '5190cf8ffd9a7878d79fd92a5e682891f0693377'
-CLANG_SVN_REVISION = 'n332854'
+CLANG_REVISION = 'c2443155a0fb245c8f17f2c1c72b6ea391e86e81'
+CLANG_SVN_REVISION = 'n332890'
 CLANG_SUB_REVISION = 1
 
 PACKAGE_VERSION = '%s-%s-%s' % (CLANG_SVN_REVISION, CLANG_REVISION[:8],
