ffddaa00a6f81ff5901de7c3f7e7cf6be5d57489
Friedrich Horschig <fhorschig@chromium.org>
2019-12-02 17:40:58 +0000

    [Android] Assert that TintedDrawables accept only bitmaps
    This assert prevents the usage of VectorDrawables in TintedDrawable.
    
    Before this CL, TintedDrawable would fail silently and return an empty
    image (which leads to regression like the linked bug).
    
    Bug: 1028925
    Change-Id: I65ff97599c8dba5b90a07087335a63a39903c2d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1946332
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720543}
    
    Former-commit-id: befd0248ba09099340cd72a1ef31d7de1fb55b82

--- chrome/browser/ui/android/widget/java/src/org/chromium/chrome/browser/ui/widget/TintedDrawable.java
+++ chrome/browser/ui/android/widget/java/src/org/chromium/chrome/browser/ui/widget/TintedDrawable.java
@@ -10,7 +10,14 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.VectorDrawable;
+import android.os.Build;
+import android.support.annotation.DrawableRes;
+import android.support.graphics.drawable.VectorDrawableCompat;
 import android.support.v7.content.res.AppCompatResources;
+
+import org.chromium.base.annotations.RemovableInRelease;
 
 /**
  * Implementation of BitmapDrawable that allows to tint the color of the drawable for all
@@ -53,6 +60,9 @@ public class TintedDrawable extends BitmapDrawable {
      * Factory method for creating a {@link TintedDrawable} with a resource id.
      */
     public static TintedDrawable constructTintedDrawable(Context context, int drawableId) {
+        assert !isVectorDrawable(context, drawableId)
+            : "TintedDrawable doesn't support "
+              + "VectorDrawables! Please use UiUtils.getTintedDrawable() instead.";
         Bitmap icon = BitmapFactory.decodeResource(context.getResources(), drawableId);
         return new TintedDrawable(context, icon);
     }
@@ -71,5 +81,23 @@ public class TintedDrawable extends BitmapDrawable {
         if (mTint == null) return false;
         setColorFilter(mTint.getColorForState(getState(), 0), PorterDuff.Mode.SRC_IN);
         return true;
+    }
+
+    /**
+     * Only called in debug builds to ensure that TintedDrawable isn't constructed for a vector
+     * grahpic.
+     * @param context A {@link Context} used to load the Drawable.
+     * @param drawableId A {@link DrawableRes} to load and check whether it's a vector graphic.
+     * @return True iff the loaded resource is either a {@link VectorDrawableCompat} or
+     * a {@link VectorDrawable}. The latter is only checked for Android L and later.
+     */
+    @RemovableInRelease
+    private static boolean isVectorDrawable(Context context, @DrawableRes int drawableId) {
+        Drawable drawable = AppCompatResources.getDrawable(context, drawableId);
+        if (drawable instanceof VectorDrawableCompat) return true;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return drawable instanceof VectorDrawable;
+        }
+        return false;
     }
 }
