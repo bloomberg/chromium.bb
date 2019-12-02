70f775e5baa2c13d23ddc3ffe1aa4edf9761e41b
Steven Bennetts <stevenjb@chromium.org>
2019-12-02 22:24:15 +0000

    Support compressed .json grit files, compress chromevox msgs (Take 2)
    This provides support for type="chrome_messages_json_gzip" in
    grit/build.py, which compresses the output files (which should have
    .gz appended to the name, e.g. messages.json.gz).
    
    This makes it fairly simple to convert an extension (e.g. chromevox)
    to use compressed l10n files.
    
    Original CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/1913610
    
    For changes reviewed in the original CL:
    TBR=agrieve@chromium.org
    
    Bug: 1023568
    Change-Id: Ibf8dfa374f5e759b3b459d007fde2423f83873a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1931097
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720686}
    
    Former-commit-id: f878a705444031c736414415c11534174db5c130

--- extensions/common/extension_l10n_util.h
+++ extensions/common/extension_l10n_util.h
@@ -11,6 +11,7 @@
 #include <string>
 #include <vector>
 
+#include "base/auto_reset.h"
 #include "base/strings/string_piece.h"
 
 namespace base {
@@ -31,6 +32,10 @@ enum class GzippedMessagesPermission {
   // e.g. component extensions from the Chrome OS rootfs.
   kAllowForTrustedSource,
 };
+
+// Called from tests to temporarily allow loading gzipped messages for non
+// component test extensions.
+base::AutoReset<bool> AllowGzippedMessagesAllowedForTest();
 
 // Set the locale for this process to a fixed value, rather than using the
 // normal file-based lookup mechanisms. This is used to set the locale inside
