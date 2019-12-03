07925b9701c77b6c00770e19f45734a5bfa3a4b6
Antonio Gomes <tonikitoo@igalia.com>
2019-12-03 10:35:07 +0000

    Move track_observer.h out of the Blink API
    BUG=787254, 919392
    R=guidou@chromium.org, haraken@chromium.org
    
    Change-Id: I1125698a24e20fda4e6fbfecc0b2d52b5272d0c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1946633
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#720900}
    
    Former-commit-id: 3542bd2ac212aabe592f569dc29da2ed42936d26

--- third_party/blink/renderer/platform/webrtc/track_observer.cc
+++ third_party/blink/renderer/platform/webrtc/track_observer.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "third_party/blink/public/platform/modules/webrtc/track_observer.h"
+#include "third_party/blink/renderer/platform/webrtc/track_observer.h"
 
 #include "base/bind.h"
 #include "base/location.h"
--- third_party/blink/renderer/platform/webrtc/track_observer.h
+++ third_party/blink/renderer/platform/webrtc/track_observer.h
@@ -2,21 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef THIRD_PARTY_BLINK_PUBLIC_PLATFORM_MODULES_WEBRTC_TRACK_OBSERVER_H_
-#define THIRD_PARTY_BLINK_PUBLIC_PLATFORM_MODULES_WEBRTC_TRACK_OBSERVER_H_
+#ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_WEBRTC_TRACK_OBSERVER_H_
+#define THIRD_PARTY_BLINK_RENDERER_PLATFORM_WEBRTC_TRACK_OBSERVER_H_
 
 #include "base/callback.h"
 #include "base/macros.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/single_thread_task_runner.h"
-#include "third_party/blink/public/platform/web_common.h"
+#include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/webrtc/api/media_stream_interface.h"
 
 namespace blink {
 
-// TODO(crbug.com/704136): Move the classes below out of the Blink exposed
-// API when all users of it have been Onion souped.
-class BLINK_PLATFORM_EXPORT TrackObserver {
+class PLATFORM_EXPORT TrackObserver {
  public:
   TrackObserver(const scoped_refptr<base::SingleThreadTaskRunner>& main_thread,
                 const scoped_refptr<webrtc::MediaStreamTrackInterface>& track);
@@ -36,4 +34,4 @@ class BLINK_PLATFORM_EXPORT TrackObserver {
 
 }  // namespace blink
 
-#endif  // THIRD_PARTY_BLINK_PUBLIC_PLATFORM_MODULES_WEBRTC_TRACK_OBSERVER_H_
+#endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_WEBRTC_TRACK_OBSERVER_H_
