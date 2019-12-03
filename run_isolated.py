d7e2c7abee7f0e775bd700e45ecd9fd3d0d12cf4
Takuto Ikuta <tikuta@chromium.org>
2019-12-03 07:07:16 +0000

    run_isolated: update isolated client
    This is to pick up
    crrev.com/b265971d2cfd995f9c6803f8e5871c5a06ca6a5a
    
    Bug: 932396
    Change-Id: I9ede903711f312d578aa667ed97be938590ffa14
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/1948012
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Cr-Mirrored-From: https://chromium.googlesource.com/infra/luci/luci-py
    Cr-Mirrored-Commit: 40b2cd35f651e581fa3e87bffe2ff181c0a0e4ab

--- run_isolated.py
+++ run_isolated.py
@@ -113,7 +113,7 @@ ISOLATED_CLIENT_DIR = u'ic'
 # Take revision from
 # https://ci.chromium.org/p/infra-internal/g/infra-packagers/console
 ISOLATED_PACKAGE = 'infra/tools/luci/isolated/${platform}'
-ISOLATED_REVISION = 'git_revision:1190afd45e12e2adbdde61b24a14365f0dc415e3'
+ISOLATED_REVISION = 'git_revision:7776f29c956edaf8c5881760332a6cf3e98f4a0b'
 
 # Keep synced with task_request.py
 CACHE_NAME_RE = re.compile(r'^[a-z0-9_]{1,4096}$')
