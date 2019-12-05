2590ca9fdf00b2a3badc85212678c8e94a9dd382
Elly Fong-Jones <ellyjones@chromium.org>
2019-12-05 22:40:57 +0000

    remoting: remove MessageBox::Core GetDialogButtonLabel
    This change migrates MessageBox::Core away from the deprecated
    GetDialogButtonLabel customization point to the label setters, which
    means this class no longer needs to store the button labels.
    
    Bug: 1011446
    Change-Id: I71e937f3d016a1cb58ca5b905e16b64e8bfa7c0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1954187
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722256}
    
    Former-commit-id: eb52a9a3d71c0c6a2ff062bc0cb4f16e6c8c0c5c

--- remoting/host/chromeos/message_box.cc
+++ remoting/host/chromeos/message_box.cc
@@ -41,7 +41,6 @@ class MessageBox::Core : public views::DialogDelegateView {
   bool Cancel() override;
   ui::ModalType GetModalType() const override;
   base::string16 GetWindowTitle() const override;
-  base::string16 GetDialogButtonLabel(ui::DialogButton button) const override;
 
   // views::WidgetDelegate interface.
   views::View* GetContentsView() override;
@@ -54,8 +53,6 @@ class MessageBox::Core : public views::DialogDelegateView {
 
  private:
   const base::string16 title_label_;
-  const base::string16 ok_label_;
-  const base::string16 cancel_label_;
   ResultCallback result_callback_;
   MessageBox* message_box_;
 
@@ -72,13 +69,13 @@ MessageBox::Core::Core(const base::string16& title_label,
                        ResultCallback result_callback,
                        MessageBox* message_box)
     : title_label_(title_label),
-      ok_label_(ok_label),
-      cancel_label_(cancel_label),
       result_callback_(result_callback),
       message_box_(message_box),
       message_box_view_(new views::MessageBoxView(
           views::MessageBoxView::InitParams(message_label))) {
   DCHECK(message_box_);
+  DialogDelegate::set_button_label(ui::DIALOG_BUTTON_OK, ok_label);
+  DialogDelegate::set_button_label(ui::DIALOG_BUTTON_CANCEL, cancel_label);
 }
 
 void MessageBox::Core::Show() {
@@ -119,19 +116,6 @@ ui::ModalType MessageBox::Core::GetModalType() const {
 
 base::string16 MessageBox::Core::GetWindowTitle() const {
   return title_label_;
-}
-
-base::string16 MessageBox::Core::GetDialogButtonLabel(
-    ui::DialogButton button) const {
-  switch (button) {
-    case ui::DIALOG_BUTTON_OK:
-      return ok_label_;
-    case ui::DIALOG_BUTTON_CANCEL:
-      return cancel_label_;
-    default:
-      NOTREACHED();
-      return base::string16();
-  }
 }
 
 views::View* MessageBox::Core::GetContentsView() {
