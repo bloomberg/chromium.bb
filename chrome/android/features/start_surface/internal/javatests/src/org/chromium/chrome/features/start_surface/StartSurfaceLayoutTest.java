84518d852871e1f7ac4464562b3da75689269e45
Yun Liu <yliuyliu@google.com>
2019-12-05 19:56:43 +0000

    Allow DisableAnimationsTestRule to also turn on animation and fix bugs.
    Android trybots already turned off animations by default,
    while local runs didn't.
    
    DisableAnimationsTestRule introduced a bug before this change that
    a test with DisableAnimationsTestRule will turn on animation
    after test finished, which is not correct behavior on trybots.
    
    Also throw exception when test rule failed to better debug issues.
    
    Bug: 1025725
    Change-Id: I90b38484fc4a7fbe9dc144372fe8375e586733ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1925423
    Commit-Queue: Yun Liu <yliuyliu@google.com>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722166}
    
    Former-commit-id: 4828633a917e4e6ee8c3c1139bd99fd2f258a587

--- chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceLayoutTest.java
+++ chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceLayoutTest.java
@@ -32,6 +32,7 @@ import android.text.TextUtils;
 import android.view.View;
 
 import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestRule;
@@ -58,6 +59,7 @@ import org.chromium.chrome.browser.tasks.tab_management.TabUiTestHelper;
 import org.chromium.chrome.tab_ui.R;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
+import org.chromium.chrome.test.ui.DisableAnimationsTestRule;
 import org.chromium.chrome.test.util.ApplicationTestUtils;
 import org.chromium.chrome.test.util.ChromeTabUtils;
 import org.chromium.chrome.test.util.MenuUtils;
@@ -89,6 +91,11 @@ public class StartSurfaceLayoutTest {
     private static final String BASE_PARAMS = "force-fieldtrial-params="
             + "Study.Group:soft-cleanup-delay/0/cleanup-delay/0/skip-slow-zooming/false"
             + "/zooming-min-sdk-version/19/zooming-min-memory-mb/512";
+
+    // Tests need animation on.
+    @ClassRule
+    public static DisableAnimationsTestRule sEnableAnimationsRule =
+            new DisableAnimationsTestRule(true);
 
     @Rule
     public ChromeTabbedActivityTestRule mActivityTestRule = new ChromeTabbedActivityTestRule();
