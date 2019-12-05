d0f83bdb26cdee1be6b67482e1524c61b77b0e6f
ckitagawa <ckitagawa@chromium.org>
2019-12-05 15:05:04 +0000

    Reland "[Paint Preview] Add font access for Linux"
    This is a reland of 42f4dc3eed36daf9bbe257db3cfc3471b89b17fc [formerly 4951143a5ff64cff365c7532fc26c101ce194d9b]
    
    The revert was due to downstream bots not successfully running a the
    new browsertest as they use is_official_build=true. Since the compositor
    service is disabled for official builds the test shouldn't be run. Using
    the same buildflag that enables the service to choose whether to build
    the test should avoid this issue.
    
    Tested locally on an official Linux build (can't run downstream bots)
    
    Original change's description:
    > [Paint Preview] Add font access for Linux
    >
    > Testing on Cluster Telemetry with Linux found an issue where SkFontMgr
    > wasn't initialized properly resulting in inability to start the
    > compositor. This is due to the lack of font access since we didn't
    > call EnsureBlinkInitializedWithSandbox. However, the compositor only
    > needs a fraction of the features that come with that call. This CL
    > starts the font_service which enables the SkFontMgr to initialize. Note
    > that we don't require WebSandboxSupport to be initialized (at least for
    > Linux) as all the fonts are stored in the provided SkPictures.
    >
    > Test coverage for this will be added as part of the end-to-end tests
    > for the player. That should catch any issues with other platforms.
    >
    > Bug: 1023377
    > Change-Id: I95a224f6f3759b1b8f8df2989222757ebbd4f12e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1908062
    > Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > Reviewed-by: Dominik Röttsches <drott@chromium.org>
    > Reviewed-by: Ian Vollick <vollick@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#718149}
    
    Bug: 1023377
    Change-Id: I5ededf77fd02bdd4f2e1248a42366d584634fd11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1933731
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722032}
    
    Former-commit-id: 6c80da46daa9a3715b9fd698817d5022d1a533f5

--- components/services/paint_preview_compositor/paint_preview_compositor_collection_impl.cc
+++ components/services/paint_preview_compositor/paint_preview_compositor_collection_impl.cc
@@ -9,10 +9,14 @@
 #include "base/memory/discardable_memory.h"
 #include "base/memory/discardable_memory_allocator.h"
 #include "build/build_config.h"
+#include "content/public/utility/utility_thread.h"
 #include "third_party/skia/include/core/SkFontMgr.h"
+#include "third_party/skia/include/ports/SkFontConfigInterface.h"
 
 #if defined(OS_WIN)
 #include "content/public/child/dwrite_font_proxy_init_win.h"
+#elif defined(OS_LINUX)
+#include "components/services/font/public/cpp/font_loader.h"
 #endif
 
 namespace paint_preview {
@@ -28,10 +32,21 @@ PaintPreviewCompositorCollectionImpl::PaintPreviewCompositorCollectionImpl(
   if (!initialize_environment)
     return;
 
+    // Initialize font access for Skia.
 #if defined(OS_WIN)
-  // Initialize direct write font proxy so skia can use it.
   content::InitializeDWriteFontProxy();
+#elif defined(OS_LINUX)
+  mojo::PendingRemote<font_service::mojom::FontService> font_service;
+  content::UtilityThread::Get()->BindHostReceiver(
+      font_service.InitWithNewPipeAndPassReceiver());
+  font_loader_ = sk_make_sp<font_service::FontLoader>(std::move(font_service));
+  SkFontConfigInterface::SetGlobal(font_loader_);
 #endif
+  // TODO(crbug/1023377): Determine if EnsureBlinkInitialized*() does any other
+  // initialization we require. Possibly for other platforms (e.g. MacOS,
+  // Android). In theory, WebSandboxSupport isn't required since we subset and
+  // load all required fonts into the Skia Pictures for portability so they are
+  // all local; however, this may be required for initialization on MacOS?
 
   // TODO(crbug/1013585): PDF compositor initializes Blink to leverage some
   // codecs for images. This is a huge overhead and shouldn't be necessary for
@@ -39,7 +54,15 @@ PaintPreviewCompositorCollectionImpl::PaintPreviewCompositorCollectionImpl(
   // encoding to PNG or we could provide our own codec implementations.
 
   // Sanity check that fonts are working.
+#if defined(OS_LINUX)
+  // No WebSandbox is provided on Linux so the local fonts aren't accessible.
+  // This is fine since since the subsetted fonts are provided in the SkPicture.
+  // However, we still need to check that the SkFontMgr starts as it is used by
+  // Skia when handling the SkPicture.
+  DCHECK(SkFontMgr::RefDefault());
+#else
   DCHECK(SkFontMgr::RefDefault()->countFamilies());
+#endif
 }
 
 PaintPreviewCompositorCollectionImpl::~PaintPreviewCompositorCollectionImpl() {
