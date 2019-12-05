93822b74e7ba9f01e80d95a68e1da17a0cad1eca
Andy Paicu <andypaicu@chromium.org>
2019-12-05 09:24:22 +0000

    Update the android quiet notifications strings and the settings UI
    This CL updates the existing strings to match the current UI deck:
    https://docs.google.com/presentation/d/1PhHoVnWczG2Xgh53-8q-Ux-WNL4HnOS9G4_KrrIsqNA/edit#slide=id.g758f186fdd_2_60
    
    Also it makes slight tweaks to the layout to match the UI deck
    * the "quiet prompts" now only has a title, not a title and subtitle
    * the main toggle on the notifications category page now has custom
    text for the "disabled" state as well
    
    Bug: 1014874
    Change-Id: Iee2c4d8ce4b9c592da2b5803c09066964be3e15b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1950506
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Kamila Hasanbega <hkamila@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721935}
    
    Former-commit-id: d1c0b7a9ee9a5e99fc4e47bc1267aed05d089f3d

--- chrome/android/java/src/org/chromium/chrome/browser/settings/website/ContentSettingsResources.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/website/ContentSettingsResources.java
@@ -166,7 +166,8 @@ public class ContentSettingsResources {
                             R.string.push_notifications_permission_title,
                             R.string.push_notifications_permission_title, ContentSettingValues.ASK,
                             ContentSettingValues.BLOCK,
-                            R.string.website_settings_category_notifications_ask, 0));
+                            R.string.website_settings_category_notifications_ask,
+                            R.string.website_settings_category_notifications_block));
             localMap.put(ContentSettingsType.POPUPS,
                     new ResourceItem(R.drawable.permission_popups, R.string.popup_permission_title,
                             R.string.popup_permission_title, ContentSettingValues.ALLOW,
