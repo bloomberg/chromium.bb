a6abf7761783ef0c5294ce6bce1f3b9eed30cd6d
Tommy Li <tommycli@chromium.org>
2019-12-05 17:11:16 +0000

    Revert "[omnibox] ZeroSuggestProvider: classify search results w/ input text"
    This reverts commit e5092bfad86e20685a16a0d43940cd1913147133 [formerly 197dd2b5f4f5a12f41071e5847dd33b29dc5c96f].
    
    Reason for revert: Reverting because this breaks ChromeOS
    
    Original change's description:
    > [omnibox] ZeroSuggestProvider: classify search results w/ input text
    > 
    > Currently, when we get search suggest results back from
    > ZeroSuggestProvider, nothing is bolded. This is inconsistent with
    > as-you-type search suggestions.
    > 
    > The root cause is that we purposely set AutocompleteInput::input_text
    > to an empty string for all ZeroSuggest queries, to avoid leaking the
    > user's current URL. (The URL is separately included in eligible).
    > 
    > The lack of bolding is an unintended side effect.
    > 
    > This CL fixes that, by re-classifying all the search suggest results
    > that come back with the real input text on the client side.
    > 
    > This is primarily intended to improve On Focus Query Refinements,
    > but this will affect all ZeroSuggest modes that return search results
    > from a non-empty omnibox. (Should not affect NTP).
    > 
    > Note nothing is bolded at all when the input text is empty (on NTP).
    > 
    > Navigational suggestions are not affected by this CL. They are
    > currently not bolded at all.
    > 
    > Bug: 963173
    > Change-Id: I7982c780f4b03f3d2b4992051d291fe1cd86f4b7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1947765
    > Reviewed-by: manuk hovanesian <manukh@chromium.org>
    > Commit-Queue: Tommy Li <tommycli@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#721114}
    
    TBR=tommycli@chromium.org,manukh@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 963173, 1030741
    Change-Id: Icfae9425fb4348913bc283b52f57ac47ca5c7b35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1953333
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722074}
    
    Former-commit-id: bff4793cf817d24fe1fed647d440809d59ce410a

--- components/omnibox/browser/zero_suggest_provider.cc
+++ components/omnibox/browser/zero_suggest_provider.cc
@@ -35,7 +35,6 @@
 #include "components/omnibox/browser/omnibox_pref_names.h"
 #include "components/omnibox/browser/remote_suggestions_service.h"
 #include "components/omnibox/browser/search_provider.h"
-#include "components/omnibox/browser/search_suggestion_parser.h"
 #include "components/omnibox/browser/verbatim_match.h"
 #include "components/omnibox/common/omnibox_features.h"
 #include "components/pref_registry/pref_registry_syncable.h"
@@ -452,6 +451,13 @@ bool ZeroSuggestProvider::UpdateResults(const std::string& json_data) {
   return results_updated;
 }
 
+void ZeroSuggestProvider::AddSuggestResultsToMap(
+    const SearchSuggestionParser::SuggestResults& results,
+    MatchMap* map) {
+  for (size_t i = 0; i < results.size(); ++i)
+    AddMatchToMap(results[i], std::string(), i, false, false, map);
+}
+
 AutocompleteMatch ZeroSuggestProvider::NavigationToMatch(
     const SearchSuggestionParser::NavigationResult& navigation) {
   AutocompleteMatch match(this, navigation.relevance(), false,
@@ -518,16 +524,7 @@ void ZeroSuggestProvider::ConvertResultsToAutocompleteMatches() {
     return;
 
   MatchMap map;
-
-  // Add all the SuggestResults to the map, re-classifying based on the
-  // permanent text as we go. This is to make ZeroSuggest results formatted in
-  // a congruent way with as-you-type search suggestions.
-  for (size_t i = 0; i < results_.suggest_results.size(); ++i) {
-    results_.suggest_results[i].ClassifyMatchContents(true, permanent_text_);
-
-    AddMatchToMap(results_.suggest_results[i], std::string(), i, false, false,
-                  &map);
-  }
+  AddSuggestResultsToMap(results_.suggest_results, &map);
 
   const int num_query_results = map.size();
   const int num_nav_results = results_.navigation_results.size();
