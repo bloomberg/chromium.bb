86c6f695526f5f25bbe6de0d0bf91a4d4821cd82
Takuto Ikuta <tikuta@chromium.org>
2019-12-06 06:20:19 +0000

    Reland "Reland "task_runner: enable go isolated client""
    This is a reland of 02d93c43b05ed71afbeca7879b1fcedc67bef208
    
    I confirmed that updated client does not failed due to too many open files.
    android: https://chromium-swarm-dev.appspot.com/task?d=true&id=48f29858bba10010
    
    Original change's description:
    > Reland "task_runner: enable go isolated client"
    >
    > This is a reland of 2c071733b79e6929299b5704283c14771658e0c4
    >
    > I confirmed this works on linux and win.
    > linux: https://chromium-swarm-dev.appspot.com/task?id=48ec118072711e10
    > win:   https://chromium-swarm-dev.appspot.com/task?id=48ec164e68cbd110
    >
    >
    > Original change's description:
    > > task_runner: enable go isolated client
    > >
    > > Let me see what happens on dev instances.
    > >
    > > Bug: 932396
    > > Change-Id: I636243dd350111fd2d4acbaa42f264b34ddf21d8
    > > Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/1943743
    > > Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    > > Reviewed-by: Junji Watanabe <jwata@google.com>
    > > Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@google.com>
    >
    > Bug: 932396
    > Change-Id: I1a378341261f42825ed38dc752dfe254d98ded48
    > Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/1946289
    > Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    > Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    > Reviewed-by: Marc-Antoine Ruel <maruel@chromium.org>
    
    Bug: 932396
    Change-Id: Icf4ffab17e34807cf408790973d2cce9025aed38
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/1954826
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Cr-Mirrored-From: https://chromium.googlesource.com/infra/luci/luci-py
    Cr-Mirrored-Commit: 425eb650bbed12b634ce831cb9f7798cf8ea4a2c

--- run_isolated.py
+++ run_isolated.py
@@ -113,7 +113,7 @@ ISOLATED_CLIENT_DIR = u'ic'
 # Take revision from
 # https://ci.chromium.org/p/infra-internal/g/infra-packagers/console
 ISOLATED_PACKAGE = 'infra/tools/luci/isolated/${platform}'
-ISOLATED_REVISION = 'git_revision:98a836e8b4eec64d41a6bfbadc80e5515c45a6f3'
+ISOLATED_REVISION = 'git_revision:ee185d59d9892bf235d574c88e3782c42d2873d9'
 
 # Keep synced with task_request.py
 CACHE_NAME_RE = re.compile(r'^[a-z0-9_]{1,4096}$')
