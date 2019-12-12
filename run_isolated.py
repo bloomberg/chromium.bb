977e17225b01ed2dd2bbbc6a0d8d6a8cfa839054
Takuto Ikuta <tikuta@chromium.org>
2019-12-12 00:46:27 +0000

    4th Reland "task_runner: enable go isolated client"
    This is reland of
    crrev.com/c/1958353
    
    This has fallback to python and retry in go client.
    
    Bug: 932396
    Change-Id: I3efc8a348e9dc534ba746be90c60dbe082d160d8
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/1961945
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Cr-Mirrored-From: https://chromium.googlesource.com/infra/luci/luci-py
    Cr-Mirrored-Commit: c66128143d6b073b74dc4d85234aef2bb8cfbc6a

--- run_isolated.py
+++ run_isolated.py
@@ -114,7 +114,7 @@ ISOLATED_CLIENT_DIR = u'ic'
 # Take revision from
 # https://ci.chromium.org/p/infra-internal/g/infra-packagers/console
 ISOLATED_PACKAGE = 'infra/tools/luci/isolated/${platform}'
-ISOLATED_REVISION = 'git_revision:98a836e8b4eec64d41a6bfbadc80e5515c45a6f3'
+ISOLATED_REVISION = 'git_revision:bbbfc325f87f18c03116dba39ba3e7cf00daa1ee'
 
 # Keep synced with task_request.py
 CACHE_NAME_RE = re.compile(r'^[a-z0-9_]{1,4096}$')
