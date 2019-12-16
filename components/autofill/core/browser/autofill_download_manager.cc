bloomberg/chromium.bb

commit 3216916cb58e814b7c7cf494ad8531bcfb1154ca

Fill in the network isolation key for autofill request
Fill in the key through resource_request->trusted_params, as the
factory (StoragePartitionImpl::GetURLLoaderFactoryForBrowserProcess())
is used for many other purposes. The test
AutofillInteractiveTestWithHistogramTester.BasicFormFill covers this path.

The tests All/AutofillQueryTest/* are using a separate testing factory and
autofill driver, so also fill in the key there and set |is_trusted| to
true in the test factory.

Bug: 1009600
Change-Id: I4465e6c3f26f27ee1c7416d57ee508d26e0068a3
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1836456
Reviewed-by: Matt Menke <mmenke@chromium.org>
Reviewed-by: Shivani Sharma <shivanisha@chromium.org>
Reviewed-by: Dominic Battré <battre@chromium.org>
Commit-Queue: Yao Xiao <yaoxia@chromium.org>
Cr-Commit-Position: refs/heads/master@{#708362}

Former-commit-id: fc59bb21057d0a077f421157e9bc2d2a8c3208e1

components/autofill/core/browser/autofill_download_manager.cc
@@ -852,6 +852,9 @@ bool AutofillDownloadManager::StartRequest(FormRequestData request_data) {
   resource_request->url = request_url;
   resource_request->credentials_mode = network::mojom::CredentialsMode::kOmit;
   resource_request->method = method;
+  resource_request->trusted_params = network::ResourceRequest::TrustedParams();
+  resource_request->trusted_params->network_isolation_key =
+      driver_->NetworkIsolationKey();
 
   // Add Chrome experiment state to the request headers.
   variations::AppendVariationsHeaderUnknownSignedIn(

chrome/browser/autofill/autofill_interactive_uitest.cc
@@ -21,6 +21,7 @@
 #include "base/strings/string_split.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/bind_test_util.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/mock_entropy_provider.h"
 #include "base/test/scoped_feature_list.h"
@@ -68,6 +69,7 @@
 #include "content/public/test/content_mock_cert_verifier.h"
 #include "content/public/test/test_renderer_host.h"
 #include "content/public/test/test_utils.h"
+#include "content/public/test/url_loader_interceptor.h"
 #include "net/base/net_errors.h"
 #include "net/cert/mock_cert_verifier.h"
 #include "net/dns/mock_host_resolver.h"
@@ -82,6 +84,7 @@
 #include "ui/events/keycodes/keyboard_codes.h"
 
 using base::ASCIIToUTF16;
+using content::URLLoaderInterceptor;
 
 namespace autofill {
 
@@ -721,8 +724,43 @@ class AutofillInteractiveTest : public AutofillInteractiveTestBase {
   }
 };
 
+class AutofillInteractiveTestWithHistogramTester
+    : public AutofillInteractiveTest {
+ public:
+  void SetUp() override {
+    // Only allow requests to be loaded that are necessary for the test. This
+    // allows a histogram to test properties of some specific requests.
+    std::vector<std::string> allowlist = {
+        "/internal/test_url_path", "https://clients1.google.com/tbproxy"};
+    url_loader_interceptor_ =
+        std::make_unique<URLLoaderInterceptor>(base::BindLambdaForTesting(
+            [&](URLLoaderInterceptor::RequestParams* params) {
+              for (const auto& s : allowlist) {
+                const bool is_match =
+                    params->url_request.url.spec().find(s) != std::string::npos;
+                if (is_match)
+                  return false;  // Do not intercept.
+              }
+              return true;  // Intercept
+            }));
+    AutofillInteractiveTest::SetUp();
+  }
+
+  void TearDownOnMainThread() override {
+    url_loader_interceptor_.reset();
+    AutofillInteractiveTest::TearDownOnMainThread();
+  }
+
+  base::HistogramTester& histogram_tester() { return histogram_tester_; }
+
+ private:
+  base::HistogramTester histogram_tester_;
+  std::unique_ptr<URLLoaderInterceptor> url_loader_interceptor_;
+};
+
 // Test that basic form fill is working.
-IN_PROC_BROWSER_TEST_F(AutofillInteractiveTest, BasicFormFill) {
+IN_PROC_BROWSER_TEST_F(AutofillInteractiveTestWithHistogramTester,
+                       BasicFormFill) {
   LOG(ERROR) << "crbug/967588: In case of flakes, report log statements to "
                 "crbug.com/967588";
   CreateTestProfile();
@@ -737,6 +775,13 @@ IN_PROC_BROWSER_TEST_F(AutofillInteractiveTest, BasicFormFill) {
   // Invoke Autofill.
   TryBasicFormFill();
   LOG(ERROR) << "crbug/967588: Basic form filling completed";
+
+  SubprocessMetricsProvider::MergeHistogramDeltasForTesting();
+  // Assert that the network isolation key is populated for 2 requests:
+  // - Navigation: /internal/test_url_path
+  // - Autofill query: https://clients1.google.com/tbproxy/af/query?...
+  histogram_tester().ExpectBucketCount("HttpCache.NetworkIsolationKeyPresent2",
+                                       2 /*kPresent*/, 2 /*count*/);
 }
 
 IN_PROC_BROWSER_TEST_F(AutofillInteractiveTest, BasicClear) {

components/autofill/content/browser/content_autofill_driver.cc
@@ -202,6 +202,14 @@ gfx::RectF ContentAutofillDriver::TransformBoundingBoxToViewportCoordinates(
                     bounding_box.width(), bounding_box.height());
 }
 
+net::NetworkIsolationKey ContentAutofillDriver::NetworkIsolationKey() {
+  content::RenderFrameHost* top_frame_host = render_frame_host_;
+  while (top_frame_host->GetParent())
+    top_frame_host = top_frame_host->GetParent();
+  return net::NetworkIsolationKey(top_frame_host->GetLastCommittedOrigin(),
+                                  render_frame_host_->GetLastCommittedOrigin());
+}
+
 void ContentAutofillDriver::FormsSeen(const std::vector<FormData>& forms,
                                       base::TimeTicks timestamp) {
   autofill_handler_->OnFormsSeen(forms, timestamp);

components/autofill/content/browser/content_autofill_driver.h
@@ -83,6 +83,7 @@ class ContentAutofillDriver : public AutofillDriver,
   void PopupHidden() override;
   gfx::RectF TransformBoundingBoxToViewportCoordinates(
       const gfx::RectF& bounding_box) override;
+  net::NetworkIsolationKey NetworkIsolationKey() override;
 
   // mojom::AutofillDriver:
   void FormsSeen(const std::vector<FormData>& forms,

components/autofill/core/browser/autofill_download_manager_unittest.cc
@@ -1353,9 +1353,13 @@ class AutofillServerCommunicationTest
 
     // Intialize the autofill driver.
     shared_url_loader_factory_ =
-        base::MakeRefCounted<network::TestSharedURLLoaderFactory>();
+        base::MakeRefCounted<network::TestSharedURLLoaderFactory>(
+            nullptr /* network_service */, true /* is_trusted */);
     driver_ = std::make_unique<TestAutofillDriver>();
     driver_->SetSharedURLLoaderFactory(shared_url_loader_factory_);
+    driver_->SetNetworkIsolationKey(
+        net::NetworkIsolationKey(url::Origin::Create(GURL("https://abc.com")),
+                                 url::Origin::Create(GURL("https://xyz.com"))));
 
     // Configure the autofill server communications channel.
     switch (GetParam()) {

components/autofill/core/browser/autofill_driver.h
@@ -11,6 +11,7 @@
 #include "build/build_config.h"
 #include "components/autofill/core/common/form_data.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "net/base/network_isolation_key.h"
 
 #if !defined(OS_IOS)
 #include "third_party/blink/public/mojom/webauthn/internal_authenticator.mojom.h"
@@ -119,6 +120,8 @@ class AutofillDriver {
   // renderers cannot do this transformation themselves.
   virtual gfx::RectF TransformBoundingBoxToViewportCoordinates(
       const gfx::RectF& bounding_box) = 0;
+
+  virtual net::NetworkIsolationKey NetworkIsolationKey() = 0;
 };
 
 }  // namespace autofill

components/autofill/core/browser/test_autofill_driver.cc
@@ -89,12 +89,21 @@ gfx::RectF TestAutofillDriver::TransformBoundingBoxToViewportCoordinates(
   return bounding_box;
 }
 
+net::NetworkIsolationKey TestAutofillDriver::NetworkIsolationKey() {
+  return network_isolation_key_;
+}
+
 void TestAutofillDriver::SetIsIncognito(bool is_incognito) {
   is_incognito_ = is_incognito;
 }
 
 void TestAutofillDriver::SetIsInMainFrame(bool is_in_main_frame) {
   is_in_main_frame_ = is_in_main_frame;
+}
+
+void TestAutofillDriver::SetNetworkIsolationKey(
+    const net::NetworkIsolationKey& network_isolation_key) {
+  network_isolation_key_ = network_isolation_key;
 }
 
 void TestAutofillDriver::SetSharedURLLoaderFactory(

components/autofill/core/browser/test_autofill_driver.h
@@ -51,12 +51,15 @@ class TestAutofillDriver : public AutofillDriver {
   void PopupHidden() override;
   gfx::RectF TransformBoundingBoxToViewportCoordinates(
       const gfx::RectF& bounding_box) override;
+  net::NetworkIsolationKey NetworkIsolationKey() override;
 
   // Methods unique to TestAutofillDriver that tests can use to specialize
   // functionality.
 
   void SetIsIncognito(bool is_incognito);
   void SetIsInMainFrame(bool is_in_main_frame);
+  void SetNetworkIsolationKey(
+      const net::NetworkIsolationKey& network_isolation_key);
 
   void SetSharedURLLoaderFactory(
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory);
@@ -66,6 +69,7 @@ class TestAutofillDriver : public AutofillDriver {
   scoped_refptr<network::SharedURLLoaderFactory> test_shared_loader_factory_;
   bool is_incognito_ = false;
   bool is_in_main_frame_ = false;
+  net::NetworkIsolationKey network_isolation_key_;
 
   DISALLOW_COPY_AND_ASSIGN(TestAutofillDriver);
 };

commit 804de1016a2371434c8d8646196df0cf16e68ac1

Fix communication with autofill server on iOS
This CL restores the communication of Chrome on iOS with the Autofill
server. It broke by commit 3216916cb58e814b7c7cf494ad8531bcfb1154ca [formerly fc59bb21057d0a077f421157e9bc2d2a8c3208e1]
which assigned trusted_params to the network request. However, the
URLLoaderFactory originated from BrowserState and was therefore shared
and not trusted. As per the documentaiton of trusted_params:
"Setting these from an untrusted URLLoader will cause the request to
fail."

This CL removes the trusted_params to bring Autofill back to a working
situation. Maybe the iOS or network stack folks can see whether
we want to have special URLLoaderFactories for components on iOS.
I don't see a precedence for that.

(cherry picked from commit d523a3dc24e3044b220a15554d702d567d5749fe [formerly 87d33b5b6575a12367605d29fe7fbf8c85a7bfd0])
TBR=mmenke@chromium.org

Bug: 1031136
Change-Id: Ic2e04f37f3a30e8adb98e23f19205be09fa5375a
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1953707
Reviewed-by: Matt Menke <mmenke@chromium.org>
Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
Commit-Queue: Dominic Battré <battre@chromium.org>
Cr-Original-Commit-Position: refs/heads/master@{#722493}
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1969337
Reviewed-by: Dominic Battré <battre@chromium.org>
Cr-Commit-Position: refs/branch-heads/3987@{#154}
Cr-Branched-From: f8ec429d946866f630bf422440e162a16c8b97d1 [formerly c4e8da9871cc266be74481e212f3a5252972509d]-refs/heads/master@{#722274}

Former-commit-id: c24742113c94364f92a09c5916848a50bea54585

components/autofill/core/browser/autofill_download_manager.cc
@@ -24,6 +24,7 @@
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "build/build_config.h"
 #include "components/autofill/core/browser/autofill_driver.h"
 #include "components/autofill/core/browser/autofill_metrics.h"
 #include "components/autofill/core/browser/form_structure.h"
@@ -852,9 +853,15 @@ bool AutofillDownloadManager::StartRequest(FormRequestData request_data) {
   resource_request->url = request_url;
   resource_request->credentials_mode = network::mojom::CredentialsMode::kOmit;
   resource_request->method = method;
+
+  // On iOS we have a single, shared URLLoaderFactory provided by BrowserState.
+  // As it is shared, it is not trusted and we cannot assign trusted_params
+  // to the network request.
+#if !defined(OS_IOS)
   resource_request->trusted_params = network::ResourceRequest::TrustedParams();
   resource_request->trusted_params->network_isolation_key =
       driver_->NetworkIsolationKey();
+#endif
 
   // Add Chrome experiment state to the request headers.
   variations::AppendVariationsHeaderUnknownSignedIn(