45d77d2c2cb92bc6ce59a1b6451083751af6bb5d
Yutaka Hirano <yhirano@chromium.org>
2019-12-16 04:10:06 +0000

    Fix WebRequestProxyingURLLoaderFactory::MaybeRemoveProxy
    The existing logic allows WebRequestProxyingURLLoaderFactory to be
    deleted while some requests are being processed (in hooks provided
    via WebRequest APIs for example). This makes fetch keepalive
    fragile. Fix that.
    
    TBR=yhirano@chromium.org
    
    (cherry picked from commit 083ad6bc223b1be2500a4149cdf3cfdc33049e5d [formerly e1f867e27d3c5069ab913b56b078660dd43c7b50])
    
    Bug: 1033062
    Change-Id: I76d86d4275e752c922f9eacec7f07e71d339b61e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1967093
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#724673}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1969084
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3987@{#147}
    Cr-Branched-From: f8ec429d946866f630bf422440e162a16c8b97d1 [formerly c4e8da9871cc266be74481e212f3a5252972509d]-refs/heads/master@{#722274}
    
    Former-commit-id: 9a18c338ee2fc5b5c518b806c311da2bcd012db0

--- extensions/browser/api/web_request/web_request_proxying_url_loader_factory.cc
+++ extensions/browser/api/web_request/web_request_proxying_url_loader_factory.cc
@@ -1106,9 +1106,6 @@ void WebRequestProxyingURLLoaderFactory::OnTargetFactoryError() {
 
 void WebRequestProxyingURLLoaderFactory::OnProxyBindingError() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  if (proxy_receivers_.empty())
-    target_factory_.reset();
-
   MaybeRemoveProxy();
 }
 
@@ -1127,10 +1124,12 @@ void WebRequestProxyingURLLoaderFactory::RemoveRequest(
 }
 
 void WebRequestProxyingURLLoaderFactory::MaybeRemoveProxy() {
-  // Even if all URLLoaderFactory pipes connected to this object have been
-  // closed it has to stay alive until all active requests have completed.
-  if (target_factory_.is_bound() || !requests_.empty())
+  // We can delete this factory only when
+  //  - there are no existing requests, and
+  //  - it is impossible for a new request to arrive in the future.
+  if (!requests_.empty() || !proxy_receivers_.empty()) {
     return;
+  }
 
   // Deletes |this|.
   proxies_->RemoveProxy(this);
