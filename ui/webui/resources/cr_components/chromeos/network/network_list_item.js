e721ab55710c8b65142d615370def4074bbec254
Jon Mann <jonmann@chromium.org>
2019-12-18 03:01:45 +0000

    Apply preserve-focus to network-list.
    This is a more robust method of re-applying focus to an
    iron-list after the underlying data changes.   The current
    method breaks during oobe and updates the list more often
    than needed.
    
    (cherry picked from commit a045fac1aa6f3ee01c7e081c022d7fc75182e061 [formerly f64bee441f87d4815afcb8ebf96a3b5ef0007bab])
    
    Bug: 1031994, 1032265
    Change-Id: Ibc115fa099f80c1da0fd872f00a84472b9ff8c20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1966581
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#725421}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1972898
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3987@{#245}
    Cr-Branched-From: f8ec429d946866f630bf422440e162a16c8b97d1 [formerly c4e8da9871cc266be74481e212f3a5252972509d]-refs/heads/master@{#722274}
    
    Former-commit-id: 0d7f87ead8647c2a220bacf46a763fb613839e7c

--- ui/webui/resources/cr_components/chromeos/network/network_list_item.js
+++ ui/webui/resources/cr_components/chromeos/network/network_list_item.js
@@ -13,6 +13,7 @@ Polymer({
   behaviors: [
     CrPolicyNetworkBehaviorMojo,
     I18nBehavior,
+    cr.ui.FocusRowBehavior,
   ],
 
   properties: {
@@ -45,7 +46,6 @@ Polymer({
     tabindex: {
       type: Number,
       value: -1,
-      reflectToAttribute: true,
     },
 
     /**
@@ -53,11 +53,10 @@ Polymer({
      * added as an attribute on this top-level network-list-item, and can
      * be used by any sub-element which applies it.
      */
-    ariaLabel: {
+    rowLabel: {
       type: String,
       notify: true,
-      reflectToAttribute: true,
-      computed: 'getAriaLabel_(item, networkState)',
+      computed: 'getRowLabel_(item, networkState)',
     },
 
     buttonLabel: {
@@ -155,15 +154,16 @@ Polymer({
    * @return {string}
    * @private
    */
-  getAriaLabel_: function() {
+  getRowLabel_: function() {
     const NetworkType = chromeos.networkConfig.mojom.NetworkType;
     const OncSource = chromeos.networkConfig.mojom.OncSource;
     const SecurityType = chromeos.networkConfig.mojom.SecurityType;
     const status = this.getNetworkStateText_();
     const isManaged = this.item.source === OncSource.kDevicePolicy ||
         this.item.source === OncSource.kUserPolicy;
-    const index = this.parentElement.items.indexOf(this.item) + 1;
-    const total = this.parentElement.items.length;
+    const list = this.parentElement;
+    const index = list.items.indexOf(this.item) + 1;
+    const total = list.items.length;
     switch (this.item.type) {
       case NetworkType.kCellular:
         if (isManaged) {
@@ -324,19 +324,33 @@ Polymer({
   onKeydown_: function(event) {
     // The only key event handled by this element is pressing Enter when the
     // subpage arrow is focused.
-    if (event.key != 'Enter' ||
-        !this.isSubpageButtonVisible_(this.networkState, this.showButtons) ||
-        this.$$('#subpage-button') != this.shadowRoot.activeElement) {
+    if (event.key != 'Enter' && event.key != ' ') {
       return;
     }
 
-    this.fireShowDetails_(event);
+    this.onSelected_(event);
 
     // The default event for pressing Enter on a focused button is to simulate a
     // click on the button. Prevent this action, since it would navigate a
     // second time to the details page and cause an unnecessary entry to be
     // added to the back stack. See https://crbug.com/736963.
     event.preventDefault();
+  },
+
+  /**
+   * @param {!Event} event
+   * @private
+   */
+  onSelected_: function(event) {
+    if (this.isSubpageButtonVisible_(this.networkState, this.showButtons) &&
+        this.$$('#subpage-button') == this.shadowRoot.activeElement) {
+      this.fireShowDetails_(event);
+    } else if (this.item.hasOwnProperty('customItemName')) {
+      this.fire('custom-item-selected', this.item);
+    } else {
+      this.fire('selected', this.item);
+      this.focusRequested_ = true;
+    }
   },
 
   /**
