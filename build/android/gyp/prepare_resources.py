bloomberg/chromium.bb

commit a90636a9e776e46577389ccfacede2fa93f30f0d

Stop same name resource clobbering on android_resources targets
android_resources targets are zipped into one directory with the contents of a
res/ dir. However android_resources targets can take multiple res/ dirs as
inputs. Files of the same name in multiple input dirs conflict and the last to
be listed ellides the others. This cl changes that by including all input res/
directories as separate res/ directories inside the zip. Resource clobbering
can then follow android aapt2 rules instead.

Bug: 935982
Change-Id: I453622093ebe5fabb91cc147a5bc9030f98c7889
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1934649
Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
Reviewed-by: Andrew Grieve <agrieve@chromium.org>
Cr-Commit-Position: refs/heads/master@{#718979}

Former-commit-id: 29c97e18b1d7580a453eb51a7a97fbdd620bdb49

build/android/gyp/prepare_resources.py
@@ -13,6 +13,7 @@ import os
 import re
 import shutil
 import sys
+import zipfile
 
 from util import build_utils
 from util import manifest_utils
@@ -108,16 +109,26 @@ def _ZipResources(resource_dirs, zip_path, ignore_pattern):
   # files that should not be part of the final resource zip.
   files_to_zip = dict()
   files_to_zip_without_generated = dict()
-  for path, archive_path in resource_utils.IterResourceFilesInDirectories(
-      resource_dirs, ignore_pattern):
-    # We want the original resource dirs in the .info file rather than the
-    # generated overridden path.
-    if not path.startswith('/tmp'):
-      files_to_zip_without_generated[archive_path] = path
-    files_to_zip[archive_path] = path
+  for index, resource_dir in enumerate(resource_dirs):
+    for path, archive_path in resource_utils.IterResourceFilesInDirectories(
+        [resource_dir], ignore_pattern):
+      resource_dir_name = os.path.basename(resource_dir)
+      archive_path = '{}_{}/{}'.format(index, resource_dir_name, archive_path)
+      # We want the original resource dirs in the .info file rather than the
+      # generated overridden path.
+      if not path.startswith('/tmp'):
+        files_to_zip_without_generated[archive_path] = path
+      files_to_zip[archive_path] = path
   resource_utils.CreateResourceInfoFile(files_to_zip_without_generated,
                                         zip_path)
-  build_utils.DoZip(files_to_zip.iteritems(), zip_path)
+  with zipfile.ZipFile(zip_path, 'w') as z:
+    # This magic comment signals to resource_utils.ExtractDeps that this zip is
+    # not just the contents of a single res dir, without the encapsulating res/
+    # (like the outputs of android_generated_resources targets), but instead has
+    # the contents of possibly multiple res/ dirs each within an encapsulating
+    # directory within the zip.
+    z.comment = resource_utils.MULTIPLE_RES_MAGIC_STRING
+    build_utils.DoZip(files_to_zip.iteritems(), z)
 
 
 def _GenerateRTxt(options, dep_subdirs, gen_dir):

build/android/gyp/compile_resources.py
@@ -570,11 +570,15 @@ def _CompileDeps(aapt2_path, dep_subdirs, temp_dir):
       # '--no-crunch',
   ]
   pool = multiprocessing.pool.ThreadPool(10)
-  def compile_partial(directory):
-    dirname = os.path.basename(directory)
-    partial_path = os.path.join(partials_dir, dirname + '.zip')
-    compile_command = (partial_compile_command +
-                       ['--dir', directory, '-o', partial_path])
+
+  def compile_partial(params):
+    index, dep_path = params
+    basename = os.path.basename(dep_path)
+    unique_name = '{}_{}'.format(index, basename)
+    partial_path = os.path.join(partials_dir, '{}.zip'.format(unique_name))
+    compile_command = (
+        partial_compile_command + ['--dir', dep_path, '-o', partial_path])
+
     # There are resources targeting API-versions lower than our minapi. For
     # various reasons it's easier to let aapt2 ignore these than for us to
     # remove them from our build (e.g. it's from a 3rd party library).
@@ -586,12 +590,13 @@ def _CompileDeps(aapt2_path, dep_subdirs, temp_dir):
 
     # Sorting the files in the partial ensures deterministic output from the
     # aapt2 link step which uses order of files in the partial.
-    sorted_partial_path = os.path.join(partials_dir, dirname + '.sorted.zip')
+    sorted_partial_path = os.path.join(partials_dir,
+                                       unique_name + '.sorted.zip')
     _SortZip(partial_path, sorted_partial_path)
 
     return sorted_partial_path
 
-  partials = pool.map(compile_partial, dep_subdirs)
+  partials = pool.map(compile_partial, enumerate(dep_subdirs))
   pool.close()
   pool.join()
   return partials
@@ -935,11 +940,11 @@ def main(args):
   debug_temp_resources_dir = os.environ.get(_ENV_DEBUG_VARIABLE)
   if debug_temp_resources_dir:
     path = os.path.join(debug_temp_resources_dir, os.path.basename(path))
-    build_utils.DeleteDirectory(path)
   else:
     # Use a deterministic temp directory since .pb files embed the absolute
     # path of resources: crbug.com/939984
     path = path + '.tmpdir'
+  build_utils.DeleteDirectory(path)
   build_utils.MakeDirectory(path)
 
   with resource_utils.BuildContext(

build/android/gyp/util/resource_utils.py
@@ -11,6 +11,7 @@ import shutil
 import subprocess
 import sys
 import tempfile
+import zipfile
 from xml.etree import ElementTree
 
 import util.build_utils as build_utils
@@ -55,6 +56,8 @@ AAPT_IGNORE_PATTERN = ':'.join([
     '.*',  # Never makes sense to include dot(files/dirs).
     '*.d.stamp',  # Ignore stamp files
 ])
+
+MULTIPLE_RES_MAGIC_STRING = b'magic'
 
 
 def ToAndroidLocaleName(chromium_locale):
@@ -699,6 +702,31 @@ def ExtractArscPackage(aapt2_path, apk_path):
   raise Exception('Failed to find arsc package name')
 
 
+def _RenameSubdirsWithPrefix(dir_path, prefix):
+  subdirs = [
+      d for d in os.listdir(dir_path)
+      if os.path.isdir(os.path.join(dir_path, d))
+  ]
+  renamed_subdirs = []
+  for d in subdirs:
+    old_path = os.path.join(dir_path, d)
+    new_path = os.path.join(dir_path, '{}_{}'.format(prefix, d))
+    renamed_subdirs.append(new_path)
+    os.rename(old_path, new_path)
+  return renamed_subdirs
+
+
+def _HasMultipleResDirs(zip_path):
+  """Checks for magic comment set by prepare_resources.py
+
+  Returns: True iff the zipfile has the magic comment that means it contains
+  multiple res/ dirs inside instead of just contents of a single res/ dir
+  (without a wrapping res/).
+  """
+  with zipfile.ZipFile(zip_path) as z:
+    return z.comment == MULTIPLE_RES_MAGIC_STRING
+
+
 def ExtractDeps(dep_zips, deps_dir):
   """Extract a list of resource dependency zip files.
 
@@ -720,7 +748,16 @@ def ExtractDeps(dep_zips, deps_dir):
     if os.path.exists(subdir):
       raise Exception('Resource zip name conflict: ' + subdirname)
     build_utils.ExtractAll(z, path=subdir)
-    dep_subdirs.append(subdir)
+    if _HasMultipleResDirs(z):
+      # basename of the directory is used to create a zip during resource
+      # compilation, include the path in the basename to help blame errors on
+      # the correct target. For example directory 0_res may be renamed
+      # chrome_android_chrome_app_java_resources_0_res pointing to the name and
+      # path of the android_resources target from whence it came.
+      subdir_subdirs = _RenameSubdirsWithPrefix(subdir, subdirname)
+      dep_subdirs.extend(subdir_subdirs)
+    else:
+      dep_subdirs.append(subdir)
   return dep_subdirs
 
 
@@ -773,11 +810,13 @@ class _ResourceBuildContext(object):
 @contextlib.contextmanager
 def BuildContext(temp_dir=None, keep_files=False):
   """Generator for a _ResourceBuildContext instance."""
+  context = None
   try:
     context = _ResourceBuildContext(temp_dir, keep_files)
     yield context
   finally:
-    context.Close()
+    if context:
+      context.Close()
 
 
 def ResourceArgsParser():

commit aeb5e8da742b08c24cf9bc3afb4322df0f948f75

Android: Fix .res.info file entries for supersize
This broke in a90636a9e776e46577389ccfacede2fa93f30f0d [formerly 29c97e18b1d7580a453eb51a7a97fbdd620bdb49].
It's hard to notice because supersize diffs don't consider symbols
to be changed when only their source directory changes.

(cherry picked from commit 8320d70b9ff019403a829c3ab700256429813012 [formerly 89fd507bc7f0702b0a9e817fdce21a310a6b1b1b])

Bug: 1034651
Change-Id: I06ff5ab9bbc3362b3e945800eaad5d6b0f516b14
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1968255
Commit-Queue: Andrew Grieve <agrieve@chromium.org>
Commit-Queue: Peter Wen <wnwen@chromium.org>
Auto-Submit: Andrew Grieve <agrieve@chromium.org>
Reviewed-by: Peter Wen <wnwen@chromium.org>
Cr-Original-Commit-Position: refs/heads/master@{#725542}
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1974391
Reviewed-by: Andrew Grieve <agrieve@chromium.org>
Cr-Commit-Position: refs/branch-heads/3987@{#262}
Cr-Branched-From: f8ec429d946866f630bf422440e162a16c8b97d1 [formerly c4e8da9871cc266be74481e212f3a5252972509d]-refs/heads/master@{#722274}

Former-commit-id: e877f550a9674c683ab36c51767c898248662f9b

build/android/gyp/prepare_resources.py
@@ -112,12 +112,9 @@ def _ZipResources(resource_dirs, zip_path, ignore_pattern):
   for index, resource_dir in enumerate(resource_dirs):
     for path, archive_path in resource_utils.IterResourceFilesInDirectories(
         [resource_dir], ignore_pattern):
+      files_to_zip_without_generated[archive_path] = path
       resource_dir_name = os.path.basename(resource_dir)
       archive_path = '{}_{}/{}'.format(index, resource_dir_name, archive_path)
-      # We want the original resource dirs in the .info file rather than the
-      # generated overridden path.
-      if not path.startswith('/tmp'):
-        files_to_zip_without_generated[archive_path] = path
       files_to_zip[archive_path] = path
   resource_utils.CreateResourceInfoFile(files_to_zip_without_generated,
                                         zip_path)