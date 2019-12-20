ab5fd1dc144c0f74c1880259d9c88f8c74542b45
Alex Ilin <alexilin@chromium.org>
2019-12-20 23:08:10 +0000

    [TTS] Mark testTapMultipleSwipeOnlyLoadsContentOnce as flaky
    The test was already disabled to Android versions less than M, but it
    started failing on M as well.
    
    (cherry picked from commit a75db7812ada6ca93c436eead5fb24b1b590f547 [formerly 11f272cf357750f4f44dcac3e2b2dae3172bfd0a])
    
    Bug: 1032955
    TBR: donnd@chromium.org
    Change-Id: I6dd62f0360ff3b3405d53fb8f80faea8ef47ffcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1962065
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#723787}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1978841
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3987@{#321}
    Cr-Branched-From: f8ec429d946866f630bf422440e162a16c8b97d1 [formerly c4e8da9871cc266be74481e212f3a5252972509d]-refs/heads/master@{#722274}
    
    Former-commit-id: 048d46be95715d416a65d810490a8300910c5ad9

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
@@ -43,6 +43,7 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.base.test.util.RetryOnFailure;
@@ -2294,7 +2295,7 @@ public class ContextualSearchManagerTest {
     @SmallTest
     @Feature({"ContextualSearch"})
     @Restriction(UiRestriction.RESTRICTION_TYPE_PHONE)
-    @DisableIf.Build(sdk_is_less_than = Build.VERSION_CODES.M, message = "crbug.com/965706")
+    @FlakyTest(message = "crbug.com/1032955")
     public void testTapMultipleSwipeOnlyLoadsContentOnce()
             throws InterruptedException, TimeoutException {
         // Simulate a tap and make sure Content is not visible.
