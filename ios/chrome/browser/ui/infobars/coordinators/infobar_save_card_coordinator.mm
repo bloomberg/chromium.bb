96fee61e482cec5d18a2afd7e15d7cf97640b56b
sczs <sczs@chromium.org>
2020-01-06 20:56:49 +0000

    [ios] Uses the local Card icon for Save Card banner.
    TBR=sczs@chromium.org
    
    (cherry picked from commit 34dc299333cd6f33467db08bb48f5c8c46b0dcad [formerly 19038779b48a9a1ee3473c65f3e5670cbf9e6343])
    
    Bug: 1032624
    Change-Id: I0a4edcbf26806da4448e2aa102df5b988c896711
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1970088
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#726037}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1988672
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3987@{#411}
    Cr-Branched-From: f8ec429d946866f630bf422440e162a16c8b97d1 [formerly c4e8da9871cc266be74481e212f3a5252972509d]-refs/heads/master@{#722274}
    
    Former-commit-id: 1d60c3e7ad36c85a0a7f1d8c98da521e5fd814f4

--- ios/chrome/browser/ui/infobars/coordinators/infobar_save_card_coordinator.mm
+++ ios/chrome/browser/ui/infobars/coordinators/infobar_save_card_coordinator.mm
@@ -78,9 +78,8 @@
         self.saveCardInfoBarDelegate->GetMessageText());
     self.bannerViewController.subTitleText =
         base::SysUTF16ToNSString(self.saveCardInfoBarDelegate->card_label());
-    gfx::Image icon = self.saveCardInfoBarDelegate->GetIcon();
-    if (!icon.IsEmpty())
-      self.bannerViewController.iconImage = icon.ToUIImage();
+    self.bannerViewController.iconImage =
+        [UIImage imageNamed:@"infobar_save_card_icon"];
   }
 }
 
