b92d20526b83edda0ccb9e79d00f916a61e16103
Edward Lesmes <ehmaldonado@chromium.org>
2020-01-09 17:06:50 +0000

    vpython3: Add Python3 bin dir to PATH on Windows
    As a workaround, add the Python3 bin dir to PATH so that the necessary DLLs are available.
    
    Bug: 1003139
    Change-Id: I7b3e05922be3fcb7b0ec2b06783a86f25484310b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/1992270
    Reviewed-by: smut <smut@google.com>
    Commit-Queue: Edward Lesmes <ehmaldonado@chromium.org>

--- vpython3.bat
+++ vpython3.bat
@@ -3,5 +3,10 @@
 :: Use of this source code is governed by a BSD-style license that can be
 :: found in the LICENSE file.
 
+:: TODO(crbug.com/1003139): Remove.
+:: Add Python 3 to PATH to work around crbug.com/1003139.
+for /f %%i in (%~dp0python3_bin_reldir.txt) do set PYTHON3_BIN_RELDIR=%%i
+set PATH=%~dp0%PYTHON3_BIN_RELDIR%;%PATH%
+
 call "%~dp0\cipd_bin_setup.bat" > nul 2>&1
 "%~dp0\.cipd_bin\vpython3.exe" %*
