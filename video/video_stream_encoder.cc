252371db88bcdbe87a59211adac9f2f7c2e05cb3
Ilya Nikolaevskiy <ilnik@webrtc.org>
2020-01-20 10:03:20 +0100

    Merge to M80: Propagate is_bw_limited flag with bw allocation everywhere it's copied
    // Because ios_sim_x64_dbg_ios10 is broken on release brunch:
    // https://bugs.chromium.org/p/webrtc/issues/detail?id=11277
    
    (cherry picked from commit 8b1338bf8e05af991632447f5eed7b11548cbbda)
    
    No-Try: true
    Bug: webrtc:11015,chromium:1043883
    Change-Id: Ie971d29d8a8d140ba120a51dd3920291034a4d48
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/166526
    Reviewed-by: Erik Språng <sprang@webrtc.org>
    Commit-Queue: Ilya Nikolaevskiy <ilnik@webrtc.org>
    Cr-Original-Commit-Position: refs/heads/master@{#30325}
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/166537
    Cr-Commit-Position: refs/branch-heads/3987@{#4}
    Cr-Branched-From: 1256d9bcac500d962e884231b0360d8c3eb3ef02-refs/heads/master@{#30022}

--- video/video_stream_encoder.cc
+++ video/video_stream_encoder.cc
@@ -208,6 +208,7 @@ VideoBitrateAllocation UpdateAllocationFromEncoderInfo(
       }
     }
   }
+  new_allocation.set_bw_limited(allocation.is_bw_limited());
   return new_allocation;
 }
 }  //  namespace
