502a5179e7d9a44d29b17e7a59b0e587959960f0
edchin <edchin@chromium.org>
2020-01-21 20:05:56 +0000

    Merge M80 [ios] Do not snapshot for rapid blocking of javascript popups
    Rapid blocking of javascript popups can cause a crash as simultaneous
    snapshots are triggered. Do not take snapshots when consecutive infobars
    are popup blocking infobars.
    
    TBR=sczs@chromium.org
    (cherry picked from commit 7a6521c2301904c6fcc6b7c8e176ecd36e7614ce [formerly ce1ea8aadbc9cd4b91cce025bf232bcd05f3b1bf])
    
    Bug: 1018285
    Change-Id: I46bca7fd8cc1f4d3f43ff28800b37c41a247f7e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1999084
    Commit-Queue: edchin <edchin@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Auto-Submit: edchin <edchin@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#731597}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2013309
    Cr-Commit-Position: refs/branch-heads/3987@{#647}
    Cr-Branched-From: f8ec429d946866f630bf422440e162a16c8b97d1 [formerly c4e8da9871cc266be74481e212f3a5252972509d]-refs/heads/master@{#722274}
    
    Former-commit-id: 6b4f40d8f9d1ad3d139143b93fe473cdd1ee19e0

--- ios/chrome/browser/snapshots/snapshot_tab_helper.mm
+++ ios/chrome/browser/snapshots/snapshot_tab_helper.mm
@@ -8,8 +8,11 @@
 #include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/task/post_task.h"
+#include "components/infobars/core/infobar.h"
+#include "components/infobars/core/infobar_delegate.h"
 #include "ios/chrome/browser/infobars/infobar_manager_impl.h"
 #import "ios/chrome/browser/snapshots/snapshot_generator.h"
+#import "ios/chrome/browser/ui/infobars/infobar_feature.h"
 #include "ios/chrome/browser/ui/util/ui_util.h"
 #include "ios/web/public/thread/web_task_traits.h"
 #include "ios/web/public/thread/web_thread.h"
@@ -177,17 +180,41 @@ void SnapshotTabHelper::WebStateDestroyed(web::WebState* web_state) {
 }
 
 void SnapshotTabHelper::OnInfoBarAdded(infobars::InfoBar* infobar) {
-  UpdateSnapshotWithCallback(nil);
+  // TODO(crbug.com/961343): Remove snapshotting for infobars when
+  // MessagesUI is permanent for all infobars.
+  if (!IsConfirmInfobarMessagesUIEnabled()) {
+    UpdateSnapshotWithCallback(nil);
+  }
 }
 
 void SnapshotTabHelper::OnInfoBarRemoved(infobars::InfoBar* infobar,
                                          bool animate) {
-  UpdateSnapshotWithCallback(nil);
+  // TODO(crbug.com/961343): Remove snapshotting for infobars when
+  // MessagesUI is permanent for all infobars.
+  if (!IsConfirmInfobarMessagesUIEnabled()) {
+    UpdateSnapshotWithCallback(nil);
+  }
 }
 
 void SnapshotTabHelper::OnInfoBarReplaced(infobars::InfoBar* old_infobar,
                                           infobars::InfoBar* new_infobar) {
-  UpdateSnapshotWithCallback(nil);
+  // TODO(crbug.com/961343): Remove snapshotting for infobars when
+  // MessagesUI is permanent for all infobars.
+  // TODO(crbug.com/1018285): Rapid blocking of javascript popups can cause a
+  // crash as simultaneous snapshots are triggered. Do not take snapshots when
+  // consecutive infobars are popup blocking infobars.
+  infobars::InfoBarDelegate::InfoBarIdentifier
+      popup_blocked_infobar_identifier =
+          infobars::InfoBarDelegate::POPUP_BLOCKED_INFOBAR_DELEGATE_MOBILE;
+  bool consecutive_popup_blocked_infobars =
+      old_infobar->delegate()->GetIdentifier() ==
+          popup_blocked_infobar_identifier &&
+      new_infobar->delegate()->GetIdentifier() ==
+          popup_blocked_infobar_identifier;
+  if (!consecutive_popup_blocked_infobars &&
+      !IsConfirmInfobarMessagesUIEnabled()) {
+    UpdateSnapshotWithCallback(nil);
+  }
 }
 
 void SnapshotTabHelper::OnManagerShuttingDown(
