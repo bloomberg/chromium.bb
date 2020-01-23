ddb8871b9bfefa206b8003498ac34f9d45097b90
Yunqing Wang <yunqingwang@google.com>
2020-01-23 14:01:23 -0800

    Revert "Move MAX_REF_MV_SEARCH to rdopt_utils.h"
    This reverts commit 54d59245cbae714b870056afb86e574db4122f5e.
    It causes a build failure after merging into the code base.
    
    Change-Id: I3ad841d8322eeb3855e6bbd93006f91ad79555e1

--- av1/encoder/rdopt_utils.h
+++ av1/encoder/rdopt_utils.h
@@ -20,8 +20,6 @@
 extern "C" {
 #endif
 
-#define MAX_REF_MV_SEARCH 3
-
 static AOM_INLINE void restore_dst_buf(MACROBLOCKD *xd, const BUFFER_SET dst,
                                        const int num_planes) {
   for (int i = 0; i < num_planes; i++) {
