46a8dc8c1873226d7a83b357051bd009bb3e4e70
Alex Peterson <petersonab@google.com>
2020-01-24 11:06:20 -0800

    Add NEON export for dsp/weight_mask
    PiperOrigin-RevId: 291404889
    Change-Id: I690d147ee87bb1e47942fb6ca5fc8f81196c5e95

--- src/dsp/weight_mask.h
+++ src/dsp/weight_mask.h
@@ -20,6 +20,12 @@
 // Pull in LIBGAV1_DspXXX defines representing the implementation status
 // of each function. The resulting value of each can be used by each module to
 // determine whether an implementation is needed at compile time.
+// IWYU pragma: begin_exports
+
+// ARM:
+#include "src/dsp/arm/super_res_neon.h"
+
+// IWYU pragma: end_exports
 
 namespace libgav1 {
 namespace dsp {
