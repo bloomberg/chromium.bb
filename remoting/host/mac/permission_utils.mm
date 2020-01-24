0b63a1ce201da25b7eff9d0d3c14a5dab0eb9689
Gary Kacmarcik <garykac@chromium.org>
2020-01-24 22:21:07 +0000

    [Chromoting] Update the heuristic that checks if Screen Capture is available
    (cherry picked from commit 601ce5e2a0ce9a660f29415200920b8defd83204 [formerly 83c2b50728a2e30dc74780424586ca3ab3af9b3c])
    TBR=garykac@chromium.org
    BUG=1040198
    
    Change-Id: I453e48c84bba191a63f524d81a3817c3c4f5a961
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1994673
    Auto-Submit: Gary Kacmarcik <garykac@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#730239}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2020607
    Cr-Commit-Position: refs/branch-heads/3987@{#707}
    Cr-Branched-From: f8ec429d946866f630bf422440e162a16c8b97d1 [formerly c4e8da9871cc266be74481e212f3a5252972509d]-refs/heads/master@{#722274}
    
    Former-commit-id: 1839eb5773eb52ef5bde07fe7e9843ebf083cbc5

--- remoting/host/mac/permission_utils.mm
+++ remoting/host/mac/permission_utils.mm
@@ -118,24 +118,42 @@ bool CanInjectInput() {
 }
 
 // Heuristic to check screen capture permission. See http://crbug.com/993692
+// Screen capture is considered allowed if the name of at least one normal
+// or dock window running on another process is visible.
 // Copied from
 // chrome/browser/media/webrtc/system_media_capture_permissions_mac.mm
 // TODO(garykac) Move webrtc version where it can be shared.
 bool CanRecordScreen() {
   if (@available(macOS 10.15, *)) {
-    base::ScopedCFTypeRef<CFArrayRef> window_list(CGWindowListCopyWindowInfo(
-        kCGWindowListOptionOnScreenOnly, kCGNullWindowID));
-    NSUInteger num_windows = CFArrayGetCount(window_list);
-    NSUInteger num_windows_with_name = 0;
-    for (NSDictionary* dict in base::mac::CFToNSCast(window_list.get())) {
-      if ([dict objectForKey:base::mac::CFToNSCast(kCGWindowName)]) {
-        num_windows_with_name++;
-      } else {
-        // No kCGWindowName detected implies no permission.
-        break;
+    base::ScopedCFTypeRef<CFArrayRef> window_list(
+        CGWindowListCopyWindowInfo(kCGWindowListOptionAll, kCGNullWindowID));
+    int current_pid = [[NSProcessInfo processInfo] processIdentifier];
+    for (NSDictionary* window in base::mac::CFToNSCast(window_list.get())) {
+      NSNumber* window_pid =
+          [window objectForKey:base::mac::CFToNSCast(kCGWindowOwnerPID)];
+      if (!window_pid || [window_pid integerValue] == current_pid)
+        continue;
+
+      NSString* window_name =
+          [window objectForKey:base::mac::CFToNSCast(kCGWindowName)];
+      if (!window_name)
+        continue;
+
+      NSNumber* layer =
+          [window objectForKey:base::mac::CFToNSCast(kCGWindowLayer)];
+      if (!layer)
+        continue;
+
+      NSInteger layer_integer = [layer integerValue];
+      if (layer_integer == CGWindowLevelForKey(kCGNormalWindowLevelKey) ||
+          layer_integer == CGWindowLevelForKey(kCGDockWindowLevelKey)) {
+        return true;
       }
+      return false;
     }
-    return num_windows == num_windows_with_name;
+
+    // Screen capture is always allowed in older macOS versions.
+    return true;
   }
 
   // Previous to 10.15, screen capture was always allowed.
