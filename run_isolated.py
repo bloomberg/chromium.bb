a45481c9342d4e8637f47563f27a9def73f56eb3
Takuto Ikuta <tikuta@chromium.org>
2020-01-28 00:31:56 +0000

    [client] use go isolated client other than win7 32bit bots
    Bug: 1045281, 932396
    Change-Id: Ifa569faa04609aa394ac05e7e5a80469ec5a67d9
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/2022127
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Cr-Mirrored-From: https://chromium.googlesource.com/infra/luci/luci-py
    Cr-Mirrored-Commit: 290769ff238b25d339958f31386c3c662d849f40

--- run_isolated.py
+++ run_isolated.py
@@ -1339,8 +1339,9 @@ def main(args):
   # TODO(crbug.com/932396): Remove this.
   use_go_isolated = (
       options.cipd_enabled and
-      # TODO(crbug.com/932396): win7 has flaky connection issue.
-      sys.platform == 'win32' and platform.release() == '10')
+      # TODO(crbug.com/1045281): 32bit win7 has flaky connection issue.
+      not (sys.platform == 'win32' and platform.release() == '7' and
+           platform.architecture()[0] == '32bit'))
 
   # TODO(maruel): CIPD caches should be defined at an higher level here too, so
   # they can be cleaned the same way.
