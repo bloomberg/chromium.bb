094c157fc3f09123825c2fbf1bc44002733f1b48
Mike Frysinger <vapier@chromium.org>
2020-01-31 16:45:14 -0500

    gs: upgrade gsutil to 4.47
    BUG=chromium:1019337
    TEST=`./gs_unittest --network` passes
    
    Change-Id: Id78dd83620054bdbed5dc461617a9ed4ed6c8625
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/2033670
    Reviewed-by: Chris McDonald <cjmcdonald@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>

--- lib/gs.py
+++ lib/gs.py
@@ -325,7 +325,7 @@ class GSContext(object):
   # (1*sleep) the first time, then (2*sleep), continuing via attempt * sleep.
   DEFAULT_SLEEP_TIME = 60
 
-  GSUTIL_VERSION = '4.46'
+  GSUTIL_VERSION = '4.47'
   GSUTIL_TAR = 'gsutil_%s.tar.gz' % GSUTIL_VERSION
   GSUTIL_URL = (PUBLIC_BASE_HTTPS_URL +
                 'chromeos-mirror/gentoo/distfiles/%s' % GSUTIL_TAR)
