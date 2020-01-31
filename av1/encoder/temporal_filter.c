bloomberg/chromium.bb

commit 4895d98bef216814462126209dd08036e7eca6d9

Temporal filter: new function to model control parameter h
h = decay_control * (0.7 + ln(sigma + 0.5))
This change leads to larger decay, i.e., stronger smoothing when
sigma (noise) is larger.

It applies to frame sizes (min of width and height) >= 480.
Lowres and ugc360 set are therefore the same.

Net metric gains are observed.

speed4    avg_psnr    ovr_psnr     ssim      vmaf
midres     -0.08       -0.12      -0.11    -0.12
hdres      -0.34       -0.31      -0.43    -0.23

speed1    avg_psnr    ovr_psnr     ssim      vmaf
midres     -0.02       -0.03      -0.10    -0.14
hdres      -0.25       -0.22      -0.35    -0.00

STATS_CHANGED

Change-Id: Ia232c55412ffee4aa23e9b21890d6bae593154df

av1/encoder/temporal_filter.c
@@ -688,8 +688,7 @@ void av1_temporal_filter_plane_c(uint8_t *frame1, unsigned int stride,
   (void)strength;
   (void)blk_fw;
   (void)use_32x32;
-  const double decay = decay_control * exp(1 - sigma);
-  const double h = AOMMAX(decay * sigma, 0.1);
+  const double h = decay_control * (0.7 + log(sigma + 0.5));
   const double beta = 1.0;
   for (int i = 0, k = 0; i < block_height; i++) {
     for (int j = 0; j < block_width; j++, k++) {
@@ -734,8 +733,7 @@ void av1_highbd_temporal_filter_plane_c(
   (void)use_32x32;
   uint16_t *frame1 = CONVERT_TO_SHORTPTR(frame1_8bit);
   uint16_t *frame2 = CONVERT_TO_SHORTPTR(frame2_8bit);
-  const double decay = decay_control * exp(1 - sigma);
-  const double h = decay * sigma;
+  const double h = decay_control * (0.7 + log(sigma + 0.5));
   const double beta = 1.0;
   for (int i = 0, k = 0; i < block_height; i++) {
     for (int j = 0; j < block_width; j++, k++) {
@@ -786,9 +784,9 @@ void apply_temporal_filter_block(YV12_BUFFER_CONFIG *frame, MACROBLOCKD *mbd,
       int decay_control;
       // The decay is obtained empirically, subject to better tuning.
       if (frame_height >= 720) {
-        decay_control = 7;
+        decay_control = 4;
       } else if (frame_height >= 480) {
-        decay_control = 5;
+        decay_control = 4;
       } else {
         decay_control = 3;
       }
@@ -838,9 +836,9 @@ void apply_temporal_filter_block(YV12_BUFFER_CONFIG *frame, MACROBLOCKD *mbd,
     int decay_control;
     // The decay is obtained empirically, subject to better tuning.
     if (frame_height >= 720) {
-      decay_control = 7;
+      decay_control = 4;
     } else if (frame_height >= 480) {
-      decay_control = 5;
+      decay_control = 4;
     } else {
       decay_control = 3;
     }
@@ -1035,8 +1033,8 @@ static FRAME_DIFF temporal_filter_iterate_c(
   const int mb_uv_width = BW >> mbd->plane[1].subsampling_x;
 #if EXPERIMENT_TEMPORAL_FILTER
   const int is_screen_content_type = cm->allow_screen_content_tools != 0;
-  const int use_new_temporal_mode = AOMMIN(cm->width, cm->height) >= 480 &&
-                                    !is_screen_content_type && !is_key_frame;
+  const int use_new_temporal_mode =
+      !is_screen_content_type && AOMMIN(cm->width, cm->height) >= 480;
 #else
   (void)sigma;
   const int use_new_temporal_mode = 0;

av1/encoder/x86/temporal_filter_avx2.c
@@ -138,8 +138,7 @@ void av1_temporal_filter_plane_avx2(uint8_t *frame1, unsigned int stride,
   (void)strength;
   (void)blk_fw;
   (void)use_32x32;
-  const double decay = decay_control * exp(1 - sigma);
-  const double h = AOMMAX(decay * sigma, 0.1);
+  const double h = decay_control * (0.7 + log(sigma + 0.5));
   const double beta = 1.0;
 
   uint16_t frame_sse[SSE_STRIDE * BH];

av1/encoder/x86/temporal_filter_sse2.c
@@ -109,8 +109,7 @@ void av1_temporal_filter_plane_sse2(uint8_t *frame1, unsigned int stride,
   (void)strength;
   (void)blk_fw;
   (void)use_32x32;
-  const double decay = decay_control * exp(1 - sigma);
-  const double h = AOMMAX(decay * sigma, 0.1);
+  const double h = decay_control * (0.7 + log(sigma + 0.5));
   const double beta = 1.0;
 
   uint16_t frame_sse[SSE_STRIDE * BH];

commit fefe21ddc7dabb71e6236ee40263d2746d548cb7

Fix 10bit quality regression
The commit 4895d98bef216814462126209dd08036e7eca6d9 caused 2% - 3%
quality drop in 10bit encoding. This patch reverted the changes to
bring back the 10bit encoding quality.

BUG=aomedia:2558

Change-Id: Ica5b64400e771afeb6fcb1c57281c90e6c9e1f56

av1/encoder/temporal_filter.c
@@ -799,9 +799,11 @@ void av1_apply_temporal_filter_planewise_c(
   // Hyper-parameter for filter weight adjustment.
   const int frame_height = frame_to_filter->heights[0]
                            << mbd->plane[0].subsampling_y;
-  const int decay_control = frame_height >= 480 ? 4 : 3;
+  const int decay_control =
+      frame_height >= 720 ? 7 : (frame_height >= 480 ? 5 : 3);
   // Control factor for non-local mean approach.
-  const double r = (double)decay_control * (0.7 + log(noise_level + 0.5));
+  const double decay = decay_control * exp(1 - noise_level);
+  const double r = AOMMAX(decay * noise_level, 0.1);
 
   // Block information.
   const int mb_height = block_size_high[block_size];
@@ -1096,7 +1098,7 @@ static FRAME_DIFF tf_do_filtering(
   const int use_planewise_strategy =
       TF_ENABLE_PLANEWISE_STRATEGY &&
       (cpi->common.allow_screen_content_tools == 0) &&
-      AOMMIN(frame_height, frame_width) >= 480;
+      AOMMIN(frame_height, frame_width) >= 480 && !is_key_frame;
   // Perform temporal filtering block by block.
   for (int mb_row = 0; mb_row < mb_rows; mb_row++) {
     mb->mv_limits.row_min = get_min_mv(mb_row, mb_height);

av1/encoder/x86/temporal_filter_avx2.c
@@ -132,7 +132,8 @@ static void apply_temporal_filter_planewise(
     const unsigned int stride2, const int block_width, const int block_height,
     const double sigma, const int decay_control, unsigned int *accumulator,
     uint16_t *count) {
-  const double h = decay_control * (0.7 + log(sigma + 0.5));
+  const double decay = decay_control * exp(1 - sigma);
+  const double h = AOMMAX(decay * sigma, 0.1);
   const double beta = 1.0;
 
   uint16_t frame_sse[SSE_STRIDE * BH];
@@ -226,7 +227,8 @@ void av1_apply_temporal_filter_planewise_avx2(
   }
 
   const int frame_height = ref_frame->heights[0] << mbd->plane[0].subsampling_y;
-  const int decay_control = frame_height >= 480 ? 4 : 3;
+  const int decay_control =
+      frame_height >= 720 ? 7 : (frame_height >= 480 ? 5 : 3);
 
   const int mb_height = block_size_high[block_size];
   const int mb_width = block_size_wide[block_size];

av1/encoder/x86/temporal_filter_sse2.c
@@ -103,7 +103,8 @@ static void apply_temporal_filter_planewise(
     const unsigned int stride2, const int block_width, const int block_height,
     const double sigma, const int decay_control, unsigned int *accumulator,
     uint16_t *count) {
-  const double h = decay_control * (0.7 + log(sigma + 0.5));
+  const double decay = decay_control * exp(1 - sigma);
+  const double h = AOMMAX(decay * sigma, 0.1);
   const double beta = 1.0;
 
   uint16_t frame_sse[SSE_STRIDE * BH];
@@ -199,7 +200,8 @@ void av1_apply_temporal_filter_planewise_sse2(
   }
 
   const int frame_height = ref_frame->heights[0] << mbd->plane[0].subsampling_y;
-  const int decay_control = frame_height >= 480 ? 4 : 3;
+  const int decay_control =
+      frame_height >= 720 ? 7 : (frame_height >= 480 ? 5 : 3);
 
   const int mb_height = block_size_high[block_size];
   const int mb_width = block_size_wide[block_size];