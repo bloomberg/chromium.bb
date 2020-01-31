b46f3601e3e90a2145bf400561d8e5b73c9e66cc
Sergey Silkin <ssilkin@google.com>
2020-01-31 16:20:26 +0000

    Reland "Remove deprecated enable-webrtc-hw-{h264,vp8,vp9}-encoding flags"
    This reverts commit e9b52df695e909ee4a26a7c0db81d16b781c1b64 [formerly b8cd99139ac8afc8c98ed8ab2fd764378fd92ee9].
    
    Reason for revert: M80 release blocker. Bug 1046501.
    
    Original change's description:
    > Revert "Remove deprecated enable-webrtc-hw-{h264,vp8,vp9}-encoding flags"
    > 
    > This reverts commit 0284312f18b900b5e3cba29dcb02b7b26525e545 [formerly a387ca32aef0aa52f3ce7f19507fc0ee10ed1d74].
    > 
    > Reason for revert: M80 Stable blocker bug 1046501
    > 
    > Original change's description:
    > > Remove deprecated enable-webrtc-hw-{h264,vp8,vp9}-encoding flags
    > > 
    > > This CL removes the 3 flags  enable-webrtc-hw-{h264,vp8,vp9}-encoding
    > > because they were deprecated in M76 and will be hidden in M78.  These
    > > flags are used in peerconnection/rtc_video_encoder_factory.cc to
    > > filter the supported codecs to be added to the SDP.
    > > 
    > > The same filtering effect can be obtained by using the still existing
    > > flag --disable-webrtc-hw-encoding which is also surfaced to
    > > chrome:flags and used in peerconnection/video_codec_factory.cc [1],
    > > albeit at a coarser granularity: no codec-level enable/disable is
    > > available.
    > > 
    > > [1] https://cs.chromium.org/chromium/src/third_party/blink/renderer/platform/peerconnection/video_codec_factory.cc?type=cs&q=IsWebRtcHWEncodingEnabled&g=0&l=180
    > > 
    > > Bug: 599650
    > > Change-Id: I35bde6cd50c88ca8641dd9a6bd5a3e44394cde43
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1863153
    > > Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    > > Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    > > Auto-Submit: Miguel Casas <mcasas@chromium.org>
    > > Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#708063}
    > 
    > TBR=dgozman@chromium.org,dcheng@google.com,mcasas@chromium.org,pfeldman@chromium.org,foolip@chromium.org,hiroh@chromium.org
    > 
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    > 
    > Bug: 599650
    > Change-Id: I6473fb68d3aac1e937da9ded8138befdd857e762
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2029452
    > Reviewed-by: Ben Mason <benmason@chromium.org>
    > Commit-Queue: Ben Mason <benmason@chromium.org>
    > Cr-Commit-Position: refs/branch-heads/3987@{#762}
    > Cr-Branched-From: f8ec429d946866f630bf422440e162a16c8b97d1 [formerly c4e8da9871cc266be74481e212f3a5252972509d]-refs/heads/master@{#722274}
    
    TBR=govind@chromium.org,benmason@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 599650
    Change-Id: Ifc6bae0ea18c735f460ff7777b6a6914fdcaf984
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2033249
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3987@{#779}
    Cr-Branched-From: f8ec429d946866f630bf422440e162a16c8b97d1 [formerly c4e8da9871cc266be74481e212f3a5252972509d]-refs/heads/master@{#722274}
    
    Former-commit-id: aa5c20c8456f713530c799a6996f40207024ed2e

--- third_party/blink/renderer/platform/peerconnection/rtc_video_encoder_factory.cc
+++ third_party/blink/renderer/platform/peerconnection/rtc_video_encoder_factory.cc
@@ -29,11 +29,10 @@ base::Optional<webrtc::SdpVideoFormat> VEAToWebRTCFormat(
 
   if (profile.profile >= media::VP8PROFILE_MIN &&
       profile.profile <= media::VP8PROFILE_MAX) {
-    if (base::FeatureList::IsEnabled(blink::features::kWebRtcHWVP8Encoding)) {
-      return webrtc::SdpVideoFormat("VP8");
-    }
-  } else if (profile.profile >= media::H264PROFILE_MIN &&
-             profile.profile <= media::H264PROFILE_MAX) {
+    return webrtc::SdpVideoFormat("VP8");
+  }
+  if (profile.profile >= media::H264PROFILE_MIN &&
+      profile.profile <= media::H264PROFILE_MAX) {
     // Enable H264 HW encode for WebRTC when SW fallback is available, which is
     // checked by kWebRtcH264WithOpenH264FFmpeg flag. This check should be
     // removed when SW implementation is fully enabled.
@@ -42,58 +41,57 @@ base::Optional<webrtc::SdpVideoFormat> VEAToWebRTCFormat(
     webrtc_h264_sw_enabled = base::FeatureList::IsEnabled(
         blink::features::kWebRtcH264WithOpenH264FFmpeg);
 #endif  // BUILDFLAG(RTC_USE_H264) && BUILDFLAG(ENABLE_FFMPEG_VIDEO_DECODERS)
-    if (webrtc_h264_sw_enabled ||
-        base::FeatureList::IsEnabled(blink::features::kWebRtcHWH264Encoding)) {
-      webrtc::H264::Profile h264_profile;
-      switch (profile.profile) {
-        case media::H264PROFILE_BASELINE:
+    if (!webrtc_h264_sw_enabled)
+      return base::nullopt;
+
+    webrtc::H264::Profile h264_profile;
+    switch (profile.profile) {
+      case media::H264PROFILE_BASELINE:
 #if defined(OS_ANDROID)
-          // Force HW H264 on Android to be CBP for most compatibility, since:
-          // - Only HW H264 is available on Android at present.
-          // - MediaCodec only advise BP, which works same as CBP in most cases.
-          // - Some peers only expect CBP in negotiation.
-          h264_profile = webrtc::H264::kProfileConstrainedBaseline;
+        // Force HW H264 on Android to be CBP for most compatibility, since:
+        // - Only HW H264 is available on Android at present.
+        // - MediaCodec only advise BP, which works same as CBP in most cases.
+        // - Some peers only expect CBP in negotiation.
+        h264_profile = webrtc::H264::kProfileConstrainedBaseline;
 #else
-          h264_profile = webrtc::H264::kProfileBaseline;
+        h264_profile = webrtc::H264::kProfileBaseline;
 #endif
-          break;
-        case media::H264PROFILE_MAIN:
-          h264_profile = webrtc::H264::kProfileMain;
-          break;
-        case media::H264PROFILE_HIGH:
-          h264_profile = webrtc::H264::kProfileHigh;
-          break;
-        default:
-          // Unsupported H264 profile in WebRTC.
-          return base::nullopt;
-      }
-
-      const int width = profile.max_resolution.width();
-      const int height = profile.max_resolution.height();
-      const int fps = profile.max_framerate_numerator;
-      DCHECK_EQ(1u, profile.max_framerate_denominator);
-
-      const absl::optional<webrtc::H264::Level> h264_level =
-          webrtc::H264::SupportedLevel(width * height, fps);
-      const webrtc::H264::ProfileLevelId profile_level_id(
-          h264_profile, h264_level.value_or(webrtc::H264::kLevel1));
-
-      webrtc::SdpVideoFormat format("H264");
-      format.parameters = {
-          {cricket::kH264FmtpProfileLevelId,
-           *webrtc::H264::ProfileLevelIdToString(profile_level_id)},
-          {cricket::kH264FmtpLevelAsymmetryAllowed, "1"},
-          {cricket::kH264FmtpPacketizationMode, "1"}};
-      return format;
+        break;
+      case media::H264PROFILE_MAIN:
+        h264_profile = webrtc::H264::kProfileMain;
+        break;
+      case media::H264PROFILE_HIGH:
+        h264_profile = webrtc::H264::kProfileHigh;
+        break;
+      default:
+        // Unsupported H264 profile in WebRTC.
+        return base::nullopt;
     }
-  } else if (profile.profile >= media::VP9PROFILE_MIN &&
-             profile.profile <= media::VP9PROFILE_MAX) {
-    if (base::FeatureList::IsEnabled(blink::features::kWebRtcHWVP9Encoding)) {
-      return webrtc::SdpVideoFormat("VP9");
-    }
+
+    const int width = profile.max_resolution.width();
+    const int height = profile.max_resolution.height();
+    const int fps = profile.max_framerate_numerator;
+    DCHECK_EQ(1u, profile.max_framerate_denominator);
+
+    const absl::optional<webrtc::H264::Level> h264_level =
+        webrtc::H264::SupportedLevel(width * height, fps);
+    const webrtc::H264::ProfileLevelId profile_level_id(
+        h264_profile, h264_level.value_or(webrtc::H264::kLevel1));
+
+    webrtc::SdpVideoFormat format("H264");
+    format.parameters = {
+        {cricket::kH264FmtpProfileLevelId,
+         *webrtc::H264::ProfileLevelIdToString(profile_level_id)},
+        {cricket::kH264FmtpLevelAsymmetryAllowed, "1"},
+        {cricket::kH264FmtpPacketizationMode, "1"}};
+    return format;
+  }
+  if (profile.profile >= media::VP9PROFILE_MIN &&
+      profile.profile <= media::VP9PROFILE_MAX) {
+    return webrtc::SdpVideoFormat("VP9");
   }
   return base::nullopt;
-}
+}  // namespace
 
 bool IsSameFormat(const webrtc::SdpVideoFormat& format1,
                   const webrtc::SdpVideoFormat& format2) {
