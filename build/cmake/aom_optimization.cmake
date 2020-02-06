9f7d6a250dd60dd60110f4b584a54635acbc8954
Tom Finegan <tomfinegan@google.com>
2020-02-06 14:33:07 -0800

    cmake: Improve object format handling.
    Treat Darwin and Windows as the special cases, and fall through to use of
    elf[32,64] format otherwise.
    
    Patch from FreeBSD:
    https://svnweb.freebsd.org/ports/head/multimedia/aom/files/patch-build_cmake_aom__optimization.cmake?revision=502497&view=markup
    
    BUG=aomedia:2472
    
    Change-Id: I3a634302deb2854af0184f53c8da961c004d79c1

--- build/cmake/aom_optimization.cmake
+++ build/cmake/aom_optimization.cmake
@@ -85,24 +85,20 @@ function(get_asm_obj_format out_format)
   if("${AOM_TARGET_CPU}" STREQUAL "x86_64")
     if("${AOM_TARGET_SYSTEM}" STREQUAL "Darwin")
       set(objformat "macho64")
-    elseif("${AOM_TARGET_SYSTEM}" STREQUAL "Linux")
-      set(objformat "elf64")
     elseif("${AOM_TARGET_SYSTEM}" STREQUAL "MSYS"
            OR "${AOM_TARGET_SYSTEM}" STREQUAL "Windows")
       set(objformat "win64")
     else()
-      message(FATAL_ERROR "Unknown obj format: ${AOM_TARGET_SYSTEM}")
+      set(objformat "elf64")
     endif()
   elseif("${AOM_TARGET_CPU}" STREQUAL "x86")
     if("${AOM_TARGET_SYSTEM}" STREQUAL "Darwin")
       set(objformat "macho32")
-    elseif("${AOM_TARGET_SYSTEM}" STREQUAL "Linux")
-      set(objformat "elf32")
     elseif("${AOM_TARGET_SYSTEM}" STREQUAL "MSYS"
            OR "${AOM_TARGET_SYSTEM}" STREQUAL "Windows")
       set(objformat "win32")
     else()
-      message(FATAL_ERROR "Unknown obj format: ${AOM_TARGET_SYSTEM}")
+      set(objformat "elf32")
     endif()
   else()
     message(
@@ -177,7 +173,13 @@ function(test_nasm)
         message(
           FATAL_ERROR "Unsupported nasm: macho32 object format not supported.")
       endif()
-    elseif("${AOM_TARGET_SYSTEM}" STREQUAL "Linux")
+    elseif("${AOM_TARGET_SYSTEM}" STREQUAL "MSYS"
+           OR "${AOM_TARGET_SYSTEM}" STREQUAL "Windows")
+      if(NOT "${nasm_helptext}" MATCHES "win32")
+        message(
+          FATAL_ERROR "Unsupported nasm: win32 object format not supported.")
+      endif()
+    else()
       if(NOT "${nasm_helptext}" MATCHES "elf32")
         message(
           FATAL_ERROR "Unsupported nasm: elf32 object format not supported.")
@@ -189,7 +191,13 @@ function(test_nasm)
         message(
           FATAL_ERROR "Unsupported nasm: macho64 object format not supported.")
       endif()
-    elseif("${AOM_TARGET_SYSTEM}" STREQUAL "Linux")
+    elseif("${AOM_TARGET_SYSTEM}" STREQUAL "MSYS"
+           OR "${AOM_TARGET_SYSTEM}" STREQUAL "Windows")
+      if(NOT "${nasm_helptext}" MATCHES "win64")
+        message(
+          FATAL_ERROR "Unsupported nasm: win64 object format not supported.")
+      endif()
+    else()
       if(NOT "${nasm_helptext}" MATCHES "elf64")
         message(
           FATAL_ERROR "Unsupported nasm: elf64 object format not supported.")
