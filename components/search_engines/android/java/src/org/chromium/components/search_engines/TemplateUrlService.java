a8b00c73365a8b8eae39e47723e2672536967a37
Pavel Yatsuk <pavely@google.com>
2020-02-10 20:46:50 +0000

    Reland "[SEC] Add a parameter to control if TemplateUrlService.setPlayAPISearchEngine should set the search engine as default"
    This is a reland of c8f7770c65597aab2d03dd70a5cce12371f07bdb [formerly 71bb8ac174ba649bcfba83a15efae80f8424fc97]
    
    Original change's description:
    > [SEC] Add a parameter to control if TemplateUrlService.setPlayAPISearchEngine should set the search engine as default
    > 
    > This change is needed to fix clients, affected by crbug.com/1048713.
    > If Play API search engine was overwritten by DefaultSearchManager then
    > this function could be used to reapply Play API search engine data.
    > This time though the search engine should not be set as default.
    > 
    > BUG=1048713
    > R=​pkasting@chromium.org
    > 
    > (cherry picked from commit b536bde987705b91e2bac04879ff3a38d3549f99 [formerly 6d2a7362795b11b1175234d9ffa8ff678c5ec0f9])
    > 
    > Change-Id: Id6df0bbcb21b652d9d384ffc446796d2abdafe49
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2042747
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Reviewed-by: Tommy Li <tommycli@chromium.org>
    > Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    > Cr-Original-Commit-Position: refs/heads/master@{#739459}
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2047088
    > Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    > Cr-Commit-Position: refs/branch-heads/3987@{#853}
    > Cr-Branched-From: f8ec429d946866f630bf422440e162a16c8b97d1 [formerly c4e8da9871cc266be74481e212f3a5252972509d]-refs/heads/master@{#722274}
    
    Bug: 1048713
    Change-Id: Ida4fb998f96af867e672125bd5cedece35ebcd40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2047495
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3987@{#860}
    Cr-Branched-From: f8ec429d946866f630bf422440e162a16c8b97d1 [formerly c4e8da9871cc266be74481e212f3a5252972509d]-refs/heads/master@{#722274}
    
    Former-commit-id: 20dbc420176de7015d0e8e3b490e2a253c74d5e0

--- components/search_engines/android/java/src/org/chromium/components/search_engines/TemplateUrlService.java
+++ components/search_engines/android/java/src/org/chromium/components/search_engines/TemplateUrlService.java
@@ -308,20 +308,22 @@ public class TemplateUrlService {
      * @param searchUrl Search url of the search engine to be added.
      * @param suggestUrl Url for retrieving search suggestions.
      * @param faviconUrl Favicon url of the search engine to be added.
+     * @param setAsDefault If true, set as default search provider.
      * @return True if search engine was successfully added, false if search engine from Play API
      *         with such keyword already existed (e.g. from previous attempt to set search engine).
      */
-    public boolean setPlayAPISearchEngine(
-            String name, String keyword, String searchUrl, String suggestUrl, String faviconUrl) {
+    public boolean setPlayAPISearchEngine(String name, String keyword, String searchUrl,
+            String suggestUrl, String faviconUrl, boolean setAsDefault) {
         return TemplateUrlServiceJni.get().setPlayAPISearchEngine(mNativeTemplateUrlServiceAndroid,
-                TemplateUrlService.this, name, keyword, searchUrl, suggestUrl, faviconUrl);
+                TemplateUrlService.this, name, keyword, searchUrl, suggestUrl, faviconUrl,
+                setAsDefault);
     }
     // TODO(crbug/1002271): This API is called from clank repo. Helper function below will be
     // removed once clank repo is updated.
     public boolean setPlayAPISearchEngine(
-            String name, String keyword, String searchUrl, String faviconUrl) {
+            String name, String keyword, String searchUrl, String suggestUrl, String faviconUrl) {
         return TemplateUrlServiceJni.get().setPlayAPISearchEngine(mNativeTemplateUrlServiceAndroid,
-                TemplateUrlService.this, name, keyword, searchUrl, null, faviconUrl);
+                TemplateUrlService.this, name, keyword, searchUrl, suggestUrl, faviconUrl, true);
     }
 
     @VisibleForTesting
@@ -359,7 +361,7 @@ public class TemplateUrlService {
                 TemplateUrlService caller, String keyword, int offset);
         boolean setPlayAPISearchEngine(long nativeTemplateUrlServiceAndroid,
                 TemplateUrlService caller, String name, String keyword, String searchUrl,
-                String suggestUrl, String faviconUrl);
+                String suggestUrl, String faviconUrl, boolean setAsDefault);
         void getTemplateUrls(long nativeTemplateUrlServiceAndroid, TemplateUrlService caller,
                 List<TemplateUrl> templateUrls);
         TemplateUrl getDefaultSearchEngine(
