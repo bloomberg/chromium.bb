aece5e541af0f095be19e661ab328cac0d527471
henrika <henrika@chromium.org>
2020-02-14 10:07:42 +0000

    Fixes issue with buzzing noise in combination with WebRTC
    Avoids usage of an invalid number of input channels (=0) on machines
    with a discrete multi channel input channel configuration. Without this
    change, the user can experience a loud (buzzing) distorted audio signal.
    
    (cherry picked from commit 65aec1bf6c01c20beda688ac1b299d4ab0955f92 [formerly d454afdce23fdf2c1f24d68b509683f62348f439])
    
    Bug: 1051043, 1051900
    Change-Id: I8ebaee33959ae0da50bf93f0756810bc7f524349
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2054089
    Commit-Queue: Henrik Andreasson <henrika@chromium.org>
    Reviewed-by: Tommi <tommi@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#741046}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2056723
    Reviewed-by: Henrik Andreasson <henrika@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3987@{#889}
    Cr-Branched-From: f8ec429d946866f630bf422440e162a16c8b97d1 [formerly c4e8da9871cc266be74481e212f3a5252972509d]-refs/heads/master@{#722274}
    
    Former-commit-id: dba7a64c095ee5f4561e658121c7e16cbc40905b

--- media/webrtc/helpers.cc
+++ media/webrtc/helpers.cc
@@ -7,11 +7,17 @@
 namespace media {
 
 webrtc::StreamConfig CreateStreamConfig(const AudioParameters& parameters) {
+  int channels = parameters.channels();
+
+  // Mapping all discrete channel layouts to max two channels assuming that any
+  // required channel remix takes place in the native audio layer.
+  if (parameters.channel_layout() == media::CHANNEL_LAYOUT_DISCRETE) {
+    channels = std::min(parameters.channels(), 2);
+  }
   const int rate = parameters.sample_rate();
   const bool has_keyboard = parameters.channel_layout() ==
                             media::CHANNEL_LAYOUT_STEREO_AND_KEYBOARD_MIC;
-  int channels =
-      media::ChannelLayoutToChannelCount(parameters.channel_layout());
+
   // webrtc::StreamConfig requires that the keyboard mic channel is not included
   // in the channel count. It may still be used.
   if (has_keyboard)
