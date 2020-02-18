cf322d9f91c14df6f5ed849887faadf2e09ef091
Junji Watanabe <jwata@google.com>
2020-02-18 08:48:57 +0000

    [swarming] fix tools.clear_cache_all
    clear_cache_all() doesn't invoke clear_cache() as expected, and caused flaky test failures.
    https://logs.chromium.org/logs/infra/buildbucket/cr-buildbucket.appspot.com/8888134910469147232/+/steps/swarming_bot/0/steps/run_tests_python3/0/stdout
    
    Change-Id: I30e869462a9ed0c202946b727f3bb97af8996131
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/2061828
    Auto-Submit: Junji Watanabe <jwata@google.com>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Mirrored-From: https://chromium.googlesource.com/infra/luci/luci-py
    Cr-Mirrored-Commit: 9e13db14f41fa64c84bfc1ca7bec6e015d26697d

--- utils/tools.py
+++ utils/tools.py
@@ -185,7 +185,8 @@ def report_cache_stats_at_exit(func, cache):
 _CACHED_FUNCS = set()
 
 def clear_cache_all():
-  map(clear_cache, _CACHED_FUNCS)
+  for f in _CACHED_FUNCS:
+    clear_cache(f)
 
 
 def cached(func):
