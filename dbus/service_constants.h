f5f93699e52ea395f5d6793a4cc6b835f1ae3cf9
Hsin-Yu Chao <hychao@google.com>
2020-02-26 18:08:47 +0800

    system_api: Add CRAS dbus API NextHandsfreeProfile
    This new API will allows end user to try out wideband speech
    feature from chrome://flags
    
    BUG=b:149188379
    TEST=emerge-octopus system_api
    
    Change-Id: I10f845f1743e3d9e324d2da617f315b137d017bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2073979
    Tested-by: Hsinyu Chao <hychao@chromium.org>
    Commit-Queue: Hsinyu Chao <hychao@chromium.org>
    Reviewed-by: En-Shuo Hsu <enshuo@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Cr-Mirrored-From: https://chromium.googlesource.com/chromiumos/platform2
    Cr-Mirrored-Commit: 38cf236b7c523c94166b24a50288ef246c83d000

--- dbus/service_constants.h
+++ dbus/service_constants.h
@@ -363,6 +363,7 @@ const char kSetPlayerPlaybackStatus[] = "SetPlayerPlaybackStatus";
 const char kSetPlayerIdentity[] = "SetPlayerIdentity";
 const char kSetPlayerPosition[] = "SetPlayerPosition";
 const char kSetPlayerMetadata[] = "SetPlayerMetadata";
+const char kNextHandsfreeProfile[] = "NextHandsfreeProfile";
 
 // Names of properties returned by GetNodes()
 const char kIsInputProperty[] = "IsInput";
