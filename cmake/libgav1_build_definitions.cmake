7766c3e090ac59f1fb86cbdfa70cd952c5ae3e1d
Wan-Teh Chang <wtc@google.com>
2020-02-26 09:21:26 -0800

    Add IfThisThenThat annotations to LIBGAV1_VERSION
    Also change LIBGAV1_VERSION to "0.10.1".
    
    PiperOrigin-RevId: 297377025
    Change-Id: I6250ab0dd43b63bd8c33897adc3b359895430858

--- cmake/libgav1_build_definitions.cmake
+++ cmake/libgav1_build_definitions.cmake
@@ -23,7 +23,7 @@ macro(libgav1_set_build_definitions)
   # TODO(tomfinegan): Neither LIBGAV1_VERSION nor LIBGAV1_SOVERSION should be
   # defined here. This data should come from outside the build system and be
   # loaded by the build system when needed.
-  set(LIBGAV1_VERSION "0.0.0")
+  set(LIBGAV1_VERSION "0.10.1")
   set(LIBGAV1_SOVERSION 0)
 
   list(APPEND libgav1_include_paths "${libgav1_root}" "${libgav1_root}/src"
