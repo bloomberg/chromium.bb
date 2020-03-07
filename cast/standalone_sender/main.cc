31d567818e11429bd9055107e9da7db4f8af0d9d
Yuri Wiitala <miu@chromium.org>
2020-03-06 16:00:19 -0800

    Cast Streaming Standalone Sender (1 of 3): SimulatedCapturer
    Implements the components that reads media from a file (audio or video),
    decodes/resamples it to the required format, and emits it at a normal
    playback speed. This simulates live, real-time capture.
    
    Later commits will introduce the components that encode+stream the media
    with a Cast Streaming Sender, and plumb it all together.
    
    Also: initial BUILD.gn, DUPS, main.cc skeleton.
    Change-Id: Iaf6792e7340477c8a999e3ec1484a55f92e04317
    Reviewed-on: https://chromium-review.googlesource.com/c/openscreen/+/2086691
    Reviewed-by: Max Yakimakha <yakimakha@chromium.org>
    Commit-Queue: Yuri Wiitala <miu@chromium.org>

---
+++ cast/standalone_sender/main.cc
@@ -0,0 +1,8 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+int main(int argc, char* argv[]) {
+  // TODO(miu): The rest is coming soon!
+  return 0;
+}
