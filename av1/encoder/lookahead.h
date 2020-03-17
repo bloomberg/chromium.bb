wjdz99/aom

Correct frames_to_key in single_pass for last GOP
Correct the value of frames_to_key when flush condition
is reached(towards the last GOP). Because flush has begun
we can be sure that no more input frames remain, and we can
use the depth of the lookahead buffer to determine frames_to_key.

Change-Id: I6afbdee1a795f0d286437e4abb08d84322f8ef1e

av1/encoder/lookahead.h
@@ -113,6 +113,8 @@ struct lookahead_entry *av1_lookahead_peek(struct lookahead_ctx *ctx, int index,
 unsigned int av1_lookahead_depth(struct lookahead_ctx *ctx,
                                  COMPRESSOR_STAGE stage);
 
+int av1_lookahead_is_flush(struct lookahead_ctx *ctx, COMPRESSOR_STAGE stage);
+
 #ifdef __cplusplus
 }  // extern "C"
 #endif

bloomberg/chromium.bb

(cherry picked from commit 8f7c0acf538fbbcb90873c8b3f3d45630237f21b)

av1/encoder/lookahead.h
@@ -113,6 +113,8 @@ struct lookahead_entry *av1_lookahead_peek(struct lookahead_ctx *ctx, int index,
 unsigned int av1_lookahead_depth(struct lookahead_ctx *ctx,
                                  COMPRESSOR_STAGE stage);
 
+int av1_lookahead_pop_sz(struct lookahead_ctx *ctx, COMPRESSOR_STAGE stage);
+
 #ifdef __cplusplus
 }  // extern "C"
 #endif