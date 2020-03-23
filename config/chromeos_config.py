11f363017933f28f822def6cf239ff59166b4efe
Fergus Dall <sidereal@google.com>
2020-03-23 18:23:55 +1100

    chromite: Upload debug symbols for tatl/tael-full builds
    tatl-full and tael-full are used to build production termina
    images. As we now collect crash reports from these images, we also
    need to upload debug symbols for them.
    
    BUG=chromium:703926
    TEST=./chromeos_config_unittest --update
    
    Change-Id: Ie6a52eefda5ec02014487e59a3287710baeba1d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/2114603
    Reviewed-by: Sean Abraham <seanabraham@chromium.org>
    Commit-Queue: Fergus Dall <sidereal@google.com>
    Tested-by: Fergus Dall <sidereal@google.com>

--- config/chromeos_config.py
+++ config/chromeos_config.py
@@ -1749,6 +1749,13 @@ def FullBuilders(site_config, boards_dict, ge_build_config):
       'tatl',
   ])
 
+  # These full builders are used to produce released VMs and so need to upload
+  # debug symbols to the crash server.
+  symbol_upload_builders = frozenset([
+      'tael',
+      'tatl',
+  ])
+
   # Move the following builders to active_builders once they are consistently
   # green.
   unstable_builders = frozenset([
@@ -1780,6 +1787,12 @@ def FullBuilders(site_config, boards_dict, ge_build_config):
       overlays=constants.PUBLIC_OVERLAYS,
       slave_configs=[],
       schedule='0 */3 * * *',
+  )
+
+  site_config.ApplyForBoards(
+      config_lib.CONFIG_TYPE_FULL,
+      symbol_upload_builders,
+      upload_symbols=True,
   )
 
   master_config.AddSlaves(
