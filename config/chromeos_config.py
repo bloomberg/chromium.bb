07fb1bff62673f86d4a43b90cd63c065a18c23b8
Federico 'Morg' Pareschi <morg@google.com>
2020-03-24 10:52:23 +0900

    Add kukui and atlas to vmpi hwtest boards
    BUG=b:152237067
    TEST=`./chromeos_config_unittest --update`
    
    Change-Id: I22b65543cdea2df11b3618f937e64bf1355b0c9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/2117191
    Tested-by: Morg <morg@chromium.org>
    Auto-Submit: Morg <morg@chromium.org>
    Reviewed-by: Lepton Wu <lepton@chromium.org>
    Reviewed-by: Alex Zamorzaev <zamorzaev@chromium.org>
    Reviewed-by: Junichi Uekawa <uekawa@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Morg <morg@chromium.org>

--- config/chromeos_config.py
+++ config/chromeos_config.py
@@ -1410,7 +1410,10 @@ def AndroidPfqBuilders(site_config, boards_dict, ge_build_config):
       'eve-arcvm-mesa-virgl-next',
   ])
   _vmpi_no_hwtest_experimental_boards = frozenset([])
-  _vmpi_hwtest_boards = frozenset([])
+  _vmpi_hwtest_boards = frozenset([
+      'atlas',
+      'kukui',
+  ])
   _vmpi_hwtest_experimental_boards = frozenset([])
   _vmpi_vmtest_boards = frozenset([])
   _vmpi_vmtest_experimental_boards = frozenset([])
