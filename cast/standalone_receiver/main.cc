2e33129995df5277e5100187c56f543d4b019cdd
Jordan Bayles <jophba@chromium.org>
2020-03-25 16:04:52 -0700

    Add discovery to standalone receiver
    This patch adds DNS-SD publishing to the standalone receiver, with
    default values for fields in the service info. Future patches may make
    this more configurable, but for now everything is set from constants.
    
    Change-Id: I48412a2bb5e587cc08b3aa7f8aff76717eb8ea43
    Reviewed-on: https://chromium-review.googlesource.com/c/openscreen/+/2120290
    Reviewed-by: Ryan Keane <rwkeane@google.com>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>

--- cast/standalone_receiver/main.cc
+++ cast/standalone_receiver/main.cc
@@ -7,8 +7,13 @@
 #include <array>
 #include <chrono>  // NOLINT
 
+#include "cast/common/public/service_info.h"
 #include "cast/standalone_receiver/cast_agent.h"
 #include "cast/streaming/ssrc.h"
+#include "discovery/common/config.h"
+#include "discovery/common/reporting_client.h"
+#include "discovery/public/dns_sd_service_factory.h"
+#include "discovery/public/dns_sd_service_publisher.h"
 #include "platform/api/time.h"
 #include "platform/api/udp_socket.h"
 #include "platform/base/error.h"
@@ -18,11 +23,73 @@
 #include "platform/impl/platform_client_posix.h"
 #include "platform/impl/task_runner.h"
 #include "platform/impl/text_trace_logging_platform.h"
+#include "util/stringprintf.h"
 #include "util/trace_logging.h"
 
 namespace openscreen {
 namespace cast {
 namespace {
+
+// TODO(jophba): set this based on actual values when the cast agent is
+// complete.
+static constexpr int kCastTlsPort = 80;
+
+class DiscoveryReportingClient : public discovery::ReportingClient {
+  void OnFatalError(Error error) override {
+    OSP_LOG_FATAL << "Encountered fatal discovery error: " << error;
+  }
+
+  void OnRecoverableError(Error error) override {
+    OSP_LOG_ERROR << "Encountered recoverable discovery error: " << error;
+  }
+};
+
+struct DiscoveryState {
+  SerialDeletePtr<discovery::DnsSdService> service;
+  std::unique_ptr<DiscoveryReportingClient> reporting_client;
+  std::unique_ptr<discovery::DnsSdServicePublisher<ServiceInfo>> publisher;
+};
+
+ErrorOr<std::unique_ptr<DiscoveryState>> StartDiscovery(
+    TaskRunner* task_runner,
+    const InterfaceInfo& interface) {
+  discovery::Config config;
+
+  config.interface = interface;
+
+  auto state = std::make_unique<DiscoveryState>();
+  state->reporting_client = std::make_unique<DiscoveryReportingClient>();
+  state->service = discovery::CreateDnsSdService(
+      task_runner, state->reporting_client.get(), config);
+
+  // TODO(jophba): update after ServiceInfo update patch lands.
+  ServiceInfo info;
+  if (interface.GetIpAddressV4()) {
+    info.v4_endpoint = IPEndpoint{interface.GetIpAddressV4(), kCastTlsPort};
+  }
+  if (interface.GetIpAddressV6()) {
+    info.v6_endpoint = IPEndpoint{interface.GetIpAddressV6(), kCastTlsPort};
+  }
+
+  OSP_CHECK(std::any_of(interface.hardware_address.begin(),
+                        interface.hardware_address.end(),
+                        [](int e) { return e > 0; }));
+  info.unique_id = HexEncode(interface.hardware_address);
+
+  // TODO(jophba): add command line arguments to set these fields.
+  info.model_name = "cast_standalone_receiver";
+  info.friendly_name = "Cast Standalone Receiver";
+
+  state->publisher =
+      std::make_unique<discovery::DnsSdServicePublisher<ServiceInfo>>(
+          state->service.get(), kCastV2ServiceId, ServiceInfoToDnsSdRecord);
+
+  auto error = state->publisher->Register(info);
+  if (!error.ok()) {
+    return error;
+  }
+  return state;
+}
 
 void RunStandaloneReceiver(TaskRunnerImpl* task_runner,
                            InterfaceInfo interface) {
@@ -44,14 +111,12 @@ namespace {
 void LogUsage(const char* argv0) {
   constexpr char kExecutableTag[] = "argv[0]";
   constexpr char kUsageMessage[] = R"(
-    usage: argv[0] <options>
+    usage: argv[0] <options> <interface>
 
-      -i, --interface= <interface, e.g. wlan0, eth0>
-           Specify the network interface to bind to. The interface is looked
-           up from the system interface registry. This argument is optional, and
-           omitting it causes the standalone receiver to attempt to bind to
-           ANY (0.0.0.0) on default port 2344. Note that this mode does not
-           work reliably on some platforms.
+    options:
+      <interface>: Specify the network interface to bind to. The interface is
+          looked up from the system interface registry. This argument is
+          mandatory, as it must be known for publishing discovery.
 
       -t, --tracing: Enable performance tracing logging.
 
@@ -59,7 +124,7 @@ void LogUsage(const char* argv0) {
   )";
   std::string message = kUsageMessage;
   message.replace(message.find(kExecutableTag), strlen(kExecutableTag), argv0);
-  OSP_LOG_ERROR << message;
+  OSP_LOG_INFO << message;
 }
 
 }  // namespace
@@ -77,7 +142,6 @@ int main(int argc, char* argv[]) {
   openscreen::SetLogLevel(openscreen::LogLevel::kInfo);
 
   const struct option argument_options[] = {
-      {"interface", required_argument, nullptr, 'i'},
       {"tracing", no_argument, nullptr, 't'},
       {"help", no_argument, nullptr, 'h'},
       {nullptr, 0, nullptr, 0}};
@@ -85,24 +149,9 @@ int main(int argc, char* argv[]) {
   InterfaceInfo interface_info;
   std::unique_ptr<openscreen::TextTraceLoggingPlatform> trace_logger;
   int ch = -1;
-  while ((ch = getopt_long(argc, argv, "i:th", argument_options, nullptr)) !=
+  while ((ch = getopt_long(argc, argv, "th", argument_options, nullptr)) !=
          -1) {
     switch (ch) {
-      case 'i': {
-        std::vector<InterfaceInfo> network_interfaces =
-            openscreen::GetNetworkInterfaces();
-        for (auto& interface : network_interfaces) {
-          if (interface.name == optarg) {
-            interface_info = std::move(interface);
-          }
-        }
-
-        if (interface_info.name.empty()) {
-          OSP_LOG_ERROR << "Invalid interface specified: " << optarg;
-          return 1;
-        }
-        break;
-      }
       case 't':
         trace_logger = std::make_unique<openscreen::TextTraceLoggingPlatform>();
         break;
@@ -111,14 +160,34 @@ int main(int argc, char* argv[]) {
         return 1;
     }
   }
+  char* interface_argument = argv[optind];
+  OSP_CHECK(interface_argument != nullptr)
+      << "Missing mandatory argument: interface.";
+  std::vector<InterfaceInfo> network_interfaces =
+      openscreen::GetNetworkInterfaces();
+  for (auto& interface : network_interfaces) {
+    if (interface.name == interface_argument) {
+      interface_info = std::move(interface);
+      break;
+    }
+  }
+  OSP_CHECK(!interface_info.name.empty()) << "Invalid interface specified.";
 
   auto* const task_runner = new TaskRunnerImpl(&Clock::now);
   PlatformClientPosix::Create(Clock::duration{50}, Clock::duration{50},
                               std::unique_ptr<TaskRunnerImpl>(task_runner));
 
+  auto discovery_state =
+      openscreen::cast::StartDiscovery(task_runner, interface_info);
+  OSP_CHECK(discovery_state.is_value()) << "Failed to start discovery.";
+
   // Runs until the process is interrupted.  Safe to pass |task_runner| as it
   // will not be destroyed by ShutDown() until this exits.
   openscreen::cast::RunStandaloneReceiver(task_runner, interface_info);
+
+  // The task runner must be deleted after all serial delete pointers, such
+  // as the one stored in the discovery state.
+  discovery_state.value().reset();
   PlatformClientPosix::ShutDown();
   return 0;
 }
