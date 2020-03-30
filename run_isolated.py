cd53c0d3d308baba6dfc4cb66f0d049cc2b4376b
Ye Kuang <yekuang@google.com>
2020-03-30 08:56:14 +0000

    client: update isolated client
    This is to include https://crrev.com/c/2124444
    See https://chromium.googlesource.com/infra/infra/+/a1f1a26ca99c19aada812968e02928e4de4c7bd3
    
    Bug: 1064012
    Change-Id: I312b861d93e589d45ff3c74f256490f92e0772ca
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/luci/luci-py/+/2126651
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Ye Kuang <yekuang@google.com>
    Auto-Submit: Ye Kuang <yekuang@google.com>
    Cr-Mirrored-From: https://chromium.googlesource.com/infra/luci/luci-py
    Cr-Mirrored-Commit: 01f428894ad513dc22008bc4f70a200628868c8c

--- run_isolated.py
+++ run_isolated.py
@@ -114,7 +114,7 @@ ISOLATED_CLIENT_DIR = u'ic'
 # Take revision from
 # https://ci.chromium.org/p/infra-internal/g/infra-packagers/console
 ISOLATED_PACKAGE = 'infra/tools/luci/isolated/${platform}'
-ISOLATED_REVISION = 'git_revision:d6deed26e36bb924a2d13c6090546d99147d68c7'
+ISOLATED_REVISION = 'git_revision:a1f1a26ca99c19aada812968e02928e4de4c7bd3'
 
 # Keep synced with task_request.py
 CACHE_NAME_RE = re.compile(r'^[a-z0-9_]{1,4096}$')
