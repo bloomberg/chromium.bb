7e0b333328abe9ad499660e9931becba1b4e4e2d
Josip Sokcevic <sokcevic@google.com>
2020-04-01 17:01:10 +0000

    Fix git-* commands when using python3 on Win
    R=ehmaldonado@chromium.org
    
    Bug: 1065582
    Change-Id: I434db17d4113cdf623ad733b65aa66b770766fb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/2131468
    Reviewed-by: Edward Lesmes <ehmaldonado@chromium.org>
    Commit-Queue: Josip Sokcevic <sokcevic@google.com>

--- vpython3.bat
+++ vpython3.bat
@@ -9,4 +9,4 @@ for /f %%i in (%~dp0python3_bin_reldir.txt) do set PYTHON3_BIN_RELDIR=%%i
 set PATH=%~dp0%PYTHON3_BIN_RELDIR%;%~dp0%PYTHON3_BIN_RELDIR%\Scripts;%~dp0%PYTHON3_BIN_RELDIR%\DLLs;%PATH%
 
 call "%~dp0\cipd_bin_setup.bat" > nul 2>&1
-"%~dp0\.cipd_bin\vpython3.exe" -vpython-interpreter "%PYTHON3_BIN_RELDIR%\python3.exe" %*
+"%~dp0\.cipd_bin\vpython3.exe" -vpython-interpreter "%~dp0\%PYTHON3_BIN_RELDIR%\python3.exe" %*
