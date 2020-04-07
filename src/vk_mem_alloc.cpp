ec44c3121c73ae243fe59acfcc0ce1ba19e43947
Jamie Madill <jmadill@chromium.org>
2020-04-07 10:21:31 -0400

    Update location modifications.
    Removes vk_mem_alloc.cc because of linking errors from volk. Removes
    the fallthrough workaround because it didn't work correctly.
    
    Change-Id: Iea26eee27bb6f4744e697a34bc915fd506bcbe10
    Bug: angleproject:2162

--- src/vk_mem_alloc.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Copyright 2019 The ANGLE Project Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-//
-// Simple no-content file to link against to build the VMA allocator.
-
-#include "vk_mem_alloc.h"
--- src/vk_mem_alloc.h
+++ src/vk_mem_alloc.h
@@ -1993,13 +1993,6 @@ available through VmaAllocatorCreateInfo::pRecordSettings.
     #define VMA_CALL_POST
 #endif
 
-// Used to silence warnings for implicit fallthrough.
-#if __has_cpp_attribute(clang::fallthrough)
-#define VMA_FALLTHROUGH [[clang::fallthrough]];
-#else
-#define VMA_FALLTHROUGH
-#endif
-
 /** \struct VmaAllocator
 \brief Represents main object of this library initialized.
 
@@ -11840,8 +11833,6 @@ void VmaDeviceMemoryBlock::Init(
         break;
     default:
         VMA_ASSERT(0);
-        // Fall-through.
-        VMA_FALLTHROUGH
     case 0:
         m_pMetadata = vma_new(hAllocator, VmaBlockMetadata_Generic)(hAllocator);
     }
