c9c5fe43cdcc7e71908cebd67e0eccea92ecced4
LaMont Jones <lamontjones@chromium.org>
2020-04-14 10:00:00 -0600

    SigningStage: print more detail on failure
    Since the error seems to get lost in the long line of status detail,
    explicitly print that in the log.
    
    BUG=chromium:1069819
    TEST=CQ
    
    Change-Id: I7a95688c8c4d65a0d9973e4299398acd88020fad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/2149270
    Reviewed-by: George Engelbrecht <engeg@google.com>
    Commit-Queue: LaMont Jones <lamontjones@chromium.org>
    Tested-by: LaMont Jones <lamontjones@chromium.org>

--- cbuildbot/stages/release_stages.py
+++ cbuildbot/stages/release_stages.py
@@ -265,6 +265,9 @@ class SigningStage(generic_stages.BoardSpecificBuilderStage):
         if status != constants.SIGNER_STATUS_PASSED:
           failures.append(result_description)
           logging.error('Signing failed for: %s', result_description)
+          details = signer_result.get('status', {}).get('details')
+          if details:
+            logging.info('Details:\n%s', details)
 
     if failures:
       logging.error('Failure summary:')
