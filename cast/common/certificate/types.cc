22486ab1cdb8f56b69b9c0a5138663c577c0eb66
btolsch <btolsch@chromium.org>
2020-04-16 11:48:18 -0700

    Add windows path for gmtime
    This change adds a preprocessor branch for calling the Windows version
    of gmtime.  This is specifically to allow building with Chrome.
    
    Bug: 1050913
    Change-Id: I230c674fc93b6b83b63c4a1019d2f4505af233d9
    Reviewed-on: https://chromium-review.googlesource.com/c/openscreen/+/2152211
    Commit-Queue: Brandon Tolsch <btolsch@chromium.org>
    Reviewed-by: Ryan Keane <rwkeane@google.com>

--- cast/common/certificate/types.cc
+++ cast/common/certificate/types.cc
@@ -52,9 +52,17 @@ bool DateTimeFromSeconds(uint64_t seconds, DateTime* time) {
   time_t sec = static_cast<time_t>(seconds);
   OSP_DCHECK_GE(sec, 0);
   OSP_DCHECK_EQ(static_cast<uint64_t>(sec), seconds);
+#if defined(_WIN32)
+  // NOTE: This is for compiling in Chromium and is not validated in any direct
+  // libcast Windows build.
+  if (!gmtime_s(&tm, &sec)) {
+    return false;
+  }
+#else
   if (!gmtime_r(&sec, &tm)) {
     return false;
   }
+#endif
 
   time->second = tm.tm_sec;
   time->minute = tm.tm_min;
