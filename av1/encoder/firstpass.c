ca1c9fb6e82ab1118bcc266ab4e2a419ef1e1b7f
Mufaddal Chakera <mufaddal.chakera@ittiam.com>
2020-04-23 16:39:56 +0530

    Modifications in firstpass to facilitate multi-threading
    In function av1_first_pass last_mv is now set to Top MB's MV
    if the left MB's mv is not available(in case of first column
    in a row).
    
    Change-Id: I002914b7834dfb98439e21eb316be82c1decb42e

--- av1/encoder/firstpass.c
+++ av1/encoder/firstpass.c
@@ -898,7 +898,7 @@ void av1_first_pass(AV1_COMP *cpi, const int64_t ts_duration) {
   MACROBLOCKD *const xd = &x->e_mbd;
   PICK_MODE_CONTEXT *ctx =
       av1_alloc_pmc(cm, BLOCK_16X16, &cpi->td.shared_coeff_buf);
-  MV last_mv = kZeroMv;
+  MV first_top_mv = kZeroMv;
   const int qindex = find_fp_qindex(seq_params->bit_depth);
   // Detect if the key frame is screen content type.
   if (frame_is_intra_only(cm)) {
@@ -997,6 +997,7 @@ void av1_first_pass(AV1_COMP *cpi, const int64_t ts_duration) {
   for (int mb_row = 0; mb_row < mi_params->mb_rows; ++mb_row) {
     MV best_ref_mv = kZeroMv;
     FRAME_STATS *mb_stat = &mb_stats[mb_row * mi_params->mb_cols];
+    MV last_mv = first_top_mv;
 
     // Reset above block coeffs.
     xd->up_available = (mb_row != 0);
@@ -1025,6 +1026,9 @@ void av1_first_pass(AV1_COMP *cpi, const int64_t ts_duration) {
             recon_yoffset, recon_uvoffset, src_yoffset, alt_ref_frame_yoffset,
             fp_block_size, this_intra_error, raw_motion_err_counts,
             raw_motion_err_list, &best_ref_mv, &last_mv, mb_stat);
+        if (mb_col == 0) {
+          first_top_mv = last_mv;
+        }
         mb_stat->coded_error += this_inter_error;
         ++raw_motion_err_counts;
       } else {
