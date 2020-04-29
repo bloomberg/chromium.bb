30586a1ca1ee94abda751a3fe60007df09c39977
Mike Frysinger <vapier@chromium.org>
2020-04-29 01:31:37 -0400

    patch_unittest: unify sample git repo setup
    The overhead of initializing a new git repo for each test case is
    pretty high: by moving this to a class setup and then duplicating
    the source repo, this cuts the execution time of this module from
    about ~140 seconds to ~115 seconds.
    
    BUG=None
    TEST=`./run_tests` passes
    
    Change-Id: Ic449d542962353ddf86a79038f7b35b9b0c2aa05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/2172628
    Reviewed-by: Chris McDonald <cjmcdonald@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- lib/patch_unittest.py
+++ lib/patch_unittest.py
@@ -12,6 +12,7 @@ import functools
 import itertools
 import os
 import shutil
+import tempfile
 import time
 
 import mock
@@ -214,22 +215,35 @@ I am the first commit.
   DEFAULT_TRACKING = (
       'refs/remotes/%s/master' % config_lib.GetSiteParams().EXTERNAL_REMOTE)
 
-  def _CreateSourceRepo(self, path):
+  @staticmethod
+  def _CreateSourceRepo(tmp_path):
     """Generate a new repo with a single commit."""
-    tmp_path = '%s-tmp' % path
-    os.mkdir(path)
-    os.mkdir(tmp_path)
-    self._run(['git', 'init', '--separate-git-dir', path], cwd=tmp_path)
+    bare_path = os.path.join(tmp_path, 'bare.git')
+    checkout_path = os.path.join(tmp_path, 'checkout')
+    cros_build_lib.run(
+        ['git', 'init', '--separate-git-dir', bare_path, checkout_path],
+        cwd=tmp_path, print_cmd=False, capture_output=True)
 
     # Add an initial commit then wipe the working tree.
-    self._run(['git', 'commit', '--allow-empty', '-m', 'initial commit'],
-              cwd=tmp_path)
-    shutil.rmtree(tmp_path)
+    cros_build_lib.run(
+        ['git', 'commit', '--allow-empty', '-m', 'initial commit'],
+        cwd=checkout_path, print_cmd=False, capture_output=True)
+
+    return bare_path
+
+  @classmethod
+  def setUpClass(cls):
+    # Generate the same git tree once as it's a bit expensive.
+    cls._cache_root = tempfile.mkdtemp(prefix='chromite-patch-unittest')
+    cls._cache_bare = cls._CreateSourceRepo(cls._cache_root)
+
+  @classmethod
+  def tearDownClass(cls):
+    shutil.rmtree(cls._cache_root)
 
   def setUp(self):
     # Create an empty repo to work from.
-    self.source = os.path.join(self.tempdir, 'source.git')
-    self._CreateSourceRepo(self.source)
+    self.source = self._cache_bare
     self.default_cwd = os.path.join(self.tempdir, 'unwritable')
     # Disallow write so as to smoke out any invalid writes to cwd.
     os.mkdir(self.default_cwd, 0o500)
