b27c92cc315d033caaae1a30c5216a77e668a449
Amin Hassani <ahassani@chromium.org>
2020-05-05 14:32:52 -0700

    dlcservice: Add InstallDlc() DBus method.
    We want to move away from allowing users to install multiple DLCs in one
    DBus call. That has proven to make the entire install logic utterly
    complicated. This CL adds the InstallDlc() method to that installs only
    a single DLC. This allows us to change all the users (chrome
    specifically) to the new API before we remove the multi install logic.
    
    BUG=chromium:1078556
    TEST=cros_workon_make --board reef --test dlcservice
    
    Change-Id: I3c4a14e58fc1ced53c85c946c6768d4b77aea55e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2183229
    Tested-by: Amin Hassani <ahassani@chromium.org>
    Reviewed-by: Jae Hoon Kim <kimjae@chromium.org>
    Reviewed-by: Stephen Barber <smbarber@chromium.org>
    Commit-Queue: Amin Hassani <ahassani@chromium.org>
    Cr-Mirrored-From: https://chromium.googlesource.com/chromiumos/platform2
    Cr-Mirrored-Commit: e8a51b1183e9a6fc76231f63085e4cda2631e248

--- dbus/dlcservice/dbus-constants.h
+++ dbus/dlcservice/dbus-constants.h
@@ -13,6 +13,7 @@ constexpr char kDlcServiceServiceName[] = "org.chromium.DlcService";
 
 constexpr char kGetInstalledMethod[] = "GetInstalled";
 constexpr char kInstallMethod[] = "Install";
+constexpr char kInstallDlcMethod[] = "InstallDlc";
 constexpr char kUninstallMethod[] = "Uninstall";
 constexpr char kPurgeMethod[] = "Purge";
 constexpr char kGetExistingDlcsMethod[] = "GetExistingDlcs";
