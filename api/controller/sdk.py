ba4b8d2d04aaf9bb465b0a5e056285f20b04d1b1
Alex Klein <saklein@chromium.org>
2020-05-07 11:08:29 -0600

    SDK cleanup: Also clean images.
    SDK is running out of space. Clean the images to free more space.
    
    BUG=chromium:1078979
    TEST=cq
    
    Change-Id: Ib44f895c78342a1964c2fe528022e63585a40497
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/2187925
    Commit-Queue: Alex Klein <saklein@chromium.org>
    Tested-by: Alex Klein <saklein@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- api/controller/sdk.py
+++ api/controller/sdk.py
@@ -117,7 +117,7 @@ def Unmount(input_proto, _output_proto, _config):
 def Clean(input_proto, _output_proto, _config):
   """Clean unneeded files from a chroot."""
   chroot = controller_util.ParseChroot(input_proto.chroot)
-  sdk.Clean(chroot, sysroots=True, tmp=True)
+  sdk.Clean(chroot, images=True, sysroots=True, tmp=True)
 
 
 @faux.all_empty
