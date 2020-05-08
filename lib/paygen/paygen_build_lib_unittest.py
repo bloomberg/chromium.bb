aae1787454dbc42a6ec66e5e68bc26bceb885a45
Michael Mortensen <mmortensen@google.com>
2020-05-08 14:24:23 -0600

    Fix failing chromite test (paygen_build_lib_unittest.py).
    It appears that underlying changes have affected the tar files used.
    With this working again, re-enable the test.
    
    BUG=chromium:1079484
    TEST=manually run lib/paygen/paygen_build_lib_unittest.py
    
    Change-Id: I8eb9fbd0b885ae2ef5595f5ac9432523ec78db8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/2190868
    Reviewed-by: Michael Mortensen <mmortensen@google.com>
    Reviewed-by: Amin Hassani <ahassani@chromium.org>
    Tested-by: Michael Mortensen <mmortensen@google.com>
    Tested-by: Amin Hassani <ahassani@chromium.org>
    Commit-Queue: Amin Hassani <ahassani@chromium.org>

--- lib/paygen/paygen_build_lib_unittest.py
+++ lib/paygen/paygen_build_lib_unittest.py
@@ -11,7 +11,6 @@ import json
 import os
 import sys
 import tarfile
-import unittest
 
 import mock
 
@@ -1236,7 +1235,6 @@ class TestAutotestPayloadsPayloads(BasePaygenBuildLibTestWithBuilds):
     self.test_image.uri = 'test_image_uri'
     self.prev_test_image.uri = 'prev_test_image_uri'
 
-  @unittest.skip('Flaking in CQ https://crbug.com/1079484')
   def testAutotestPayloadsSuccess(self):
     payload_tests = [
         self.full_payload_test,
@@ -1300,29 +1298,16 @@ from autotest_lib.client.common_lib import error, utils
 from autotest_lib.client.cros import constants
 from autotest_lib.server import host_attributes
 
-AUTHOR = "Chromium OS"
+AUTHOR = "dhaddock@, ahassani@"
 NAME = "autoupdate_EndToEndTest_paygen_au_foo_delta_1.0.0_omaha"
 TIME = "MEDIUM"
 TEST_CATEGORY = "Functional"
 TEST_CLASS = "platform"
 TEST_TYPE = "server"
-JOB_RETRIES = 1
-BUG_TEMPLATE = {
-    'cc': ['chromeos-installer-alerts@google.com'],
-    'components': ['Internals>Installer'],
-}
 
 # Skip provision special task for AU tests.
 DEPENDENCIES = "skip_provision"
-
-# Disable server-side packaging support for this test.
-# This control file is used as the template for paygen_au_canary suite, which
-# creates the control files during paygen. Therefore, autotest server package
-# does not have these test control files for paygen_au_canary suite.
-REQUIRE_SSP = False
-
-DOC ="""))
-
+"""))
     # We only checking the beginning to avoid the very long doc string.
     self.assertTrue(full_contents.startswith(b"""name = 'paygen_au_foo'
 update_type = 'full'
@@ -1343,28 +1328,16 @@ from autotest_lib.client.common_lib import error, utils
 from autotest_lib.client.cros import constants
 from autotest_lib.server import host_attributes
 
-AUTHOR = "Chromium OS"
+AUTHOR = "dhaddock@, ahassani@"
 NAME = "autoupdate_EndToEndTest_paygen_au_foo_full_1.2.3_n2n"
 TIME = "MEDIUM"
 TEST_CATEGORY = "Functional"
 TEST_CLASS = "platform"
 TEST_TYPE = "server"
-JOB_RETRIES = 1
-BUG_TEMPLATE = {
-    'cc': ['chromeos-installer-alerts@google.com'],
-    'components': ['Internals>Installer'],
-}
 
 # Skip provision special task for AU tests.
 DEPENDENCIES = "skip_provision"
-
-# Disable server-side packaging support for this test.
-# This control file is used as the template for paygen_au_canary suite, which
-# creates the control files during paygen. Therefore, autotest server package
-# does not have these test control files for paygen_au_canary suite.
-REQUIRE_SSP = False
-
-DOC ="""))
+"""))
 
 
 class HWTest(cros_test_lib.MockTestCase):
