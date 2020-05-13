93081ddc88103ea59e75c3972977bad678b135e5
George Engelbrecht <engeg@google.com>
2020-05-13 12:19:42 -0600

    cbuildbot: stop retrying builds
    BUG=None
    TEST=./run_tests.py
    
    Change-Id: I426e19a34b4cea3ddf89d155b20b0d2c11ba96d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/2198610
    Reviewed-by: Sean Abraham <seanabraham@chromium.org>
    Reviewed-by: Mike Nichols <mikenichols@chromium.org>
    Reviewed-by: Chris McDonald <cjmcdonald@chromium.org>
    Tested-by: George Engelbrecht <engeg@google.com>
    Commit-Queue: George Engelbrecht <engeg@google.com>

--- lib/constants.py
+++ lib/constants.py
@@ -966,7 +966,11 @@ ACTIVE_BUCKETS = [
 ]
 
 # Build retry limit on buildbucket
-BUILDBUCKET_BUILD_RETRY_LIMIT = 2
+#
+# 2->1 on 2020-05-13 by engeg@: This is rarely effective, causes confusion,
+# higher bot utilization, and if the initial try was past uploading artifacts
+# then the retry is destined to fail with a difficult to parse error.
+BUILDBUCKET_BUILD_RETRY_LIMIT = 1
 
 # TODO(nxia): consolidate all run.metadata key constants,
 # add a unit test to avoid duplicated keys in run_metadata
