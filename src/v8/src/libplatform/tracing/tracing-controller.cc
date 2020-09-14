17441e4edcf77464ad64a9ac8ee744401df3028b
Zhen Yin <zyin@bloomberg.net>
2020-09-14 18:18:43 -0400

    turn off recording for build that don't have v8 (#840)
    e.g. mksnapshot will crash if recording is on.
    Using MSVC_2015_PLUS which is defined in v8 when _MSC_VER >= 1900.

--- src/v8/src/libplatform/tracing/tracing-controller.cc
+++ src/v8/src/libplatform/tracing/tracing-controller.cc
@@ -70,7 +70,7 @@ const int g_num_builtin_categories = 3;
 v8::base::AtomicWord g_category_index = g_num_builtin_categories;
 #endif  // !defined(V8_USE_PERFETTO)
 
-#if defined(_MSC_VER) && _MSC_VER >= 1900
+#if defined(MSVC_2015_PLUS)
 TracingController::TracingController() { mutex_.reset(new base::Mutex()); }
 #else
 TracingController::TracingController():recording_(false) { mutex_.reset(new base::Mutex()); }
