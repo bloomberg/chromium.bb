b87c519b6fcacc190362cb16918d64202b4b60bc
Imran Haider <ihaider3@bloomberg.net>
2021-03-29 11:07:42 -0400

    blpwtk2/memory-diagnostic: Fix build (#1053)

--- src/cc/trees/proxy.h
+++ src/cc/trees/proxy.h
@@ -9,6 +9,7 @@
 #include <string>
 
 #include "base/memory/ref_counted.h"
+#include "base/memory/shared_memory_mapping.h"
 #include "base/threading/platform_thread.h"
 #include "base/time/time.h"
 #include "base/values.h"
