91b973b65cf913fdc683567c05f2112c11df15d6
Shezan Baig <sbaig1@bloomberg.net>
2022-04-11 11:18:55 -0400

    blpwtk2_shell: log the process/thread id (#479)
    These are useful for debugging multithreaded issues.

--- src/blpwtk2/shell/main.cc
+++ src/blpwtk2/shell/main.cc
@@ -849,7 +849,8 @@ void logMessageHandler(blpwtk2::ToolkitCreateParams::LogMessageSeverity severity
                        int line,
                        const char* message)
 {
-    std::cout << "[" << file << ":" << line << "] " << message << std::endl;
+    std::cout << "[" << ::GetCurrentProcessId() << ":" << GetCurrentThreadId()
+              << "][" << file << ":" << line << "]: " << message << std::endl;
 }
 
 void consoleLogMessageHandler(blpwtk2::ToolkitCreateParams::LogMessageSeverity severity,
@@ -859,7 +860,8 @@ void consoleLogMessageHandler(blpwtk2::ToolkitCreateParams::LogMessageSeverity s
                               const blpwtk2::StringRef& message,
                               const blpwtk2::StringRef& stack_trace)
 {
-    std::cout << "[" << std::string(file.data(), file.length()) << ":"
+    std::cout << "[" << ::GetCurrentProcessId() << ":" << GetCurrentThreadId()
+              << "][" << std::string(file.data(), file.length()) << ":"
               << line << ":" << column << "] "
               << std::string(message.data(), message.length()) << std::endl;
      if (!stack_trace.isEmpty()) {
